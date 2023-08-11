932fcd7bd3441c7965356590a7e22adc570c40b8
Marijn Kruisselbrink <mek@chromium.org>
2023-08-11 21:24:21 +0000

    Support some commands in PWAs when no windows are open on Mac.
    Currently it is not possible to have a PWA app shim running
    without any windows open, but this changes with the
    AppShimNewCloseBehavior feature. With that feature enabled and
    no app windows open, it is a bit weird if the "new window" and
    "app settings" menu items are disabled/don't work, so this CL
    enables those commands and makes them work even when no windows
    are open.
    
    In case of multi-profile PWAs, opening settings currently picks
    a profile fairly arbitrary (using similar logic to what is used
    to determine what profile to open a new app launch in), which
    feels like pretty reasonable behavior to me.
    
    Bug: 1466216, 1466217
    Change-Id: Ia84e59d7b684c91d50ab92b7e204a04d6eb84145
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4752995
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1182788}

--- chrome/app_shim/app_shim_delegate.mm
+++ chrome/app_shim/app_shim_delegate.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/mac/foundation_util.h"
 #include "base/memory/raw_ptr.h"
+#include "chrome/app/chrome_command_ids.h"
 #include "chrome/app_shim/app_shim_controller.h"
 #include "net/base/mac/url_conversions.h"
 
@@ -58,7 +59,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (BOOL)validateUserInterfaceItem:(id<NSValidatedUserInterfaceItem>)item {
+  SEL action = [item action];
+  if (action == @selector(commandDispatch:)) {
+    NSInteger tag = [item tag];
+    if (tag == IDC_WEB_APP_SETTINGS || tag == IDC_NEW_WINDOW) {
+      return YES;
+    }
+  }
   return NO;
+}
+
+- (void)commandDispatch:(id)sender {
+  NSInteger tag = [sender tag];
+  NSLog(@"Dispatching: %d", int(tag));
+  _appShimController->CommandDispatch(tag);
 }
 
 - (NSMenu*)applicationDockMenu:(NSApplication*)sender {
--- chrome/browser/apps/app_shim/app_shim_manager_mac.cc
+++ chrome/browser/apps/app_shim/app_shim_manager_mac.cc
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profiles_state.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_navigator.h"
+#include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/profile_picker.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
@@ -53,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/os_integration/web_app_shortcut_mac.h"
 #include "chrome/browser/web_applications/web_app_helpers.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
+#include "chrome/browser/web_applications/web_app_utils.h"
 #include "chrome/common/chrome_features.h"
 #include "components/crash/core/common/crash_key.h"
 #include "components/crx_file/id_util.h"
@@ -1179,6 +1181,33 @@ void AppShimManager::OnShimSelectedProfile(AppShimHost* host,
   LoadAndLaunchAppParams params;
   params.app_id = host->GetAppId();
   LoadAndLaunchApp(profile_path, params, base::DoNothing());
+}
+
+void AppShimManager::OnShimOpenedAppSettings(AppShimHost* host) {
+  // Retrieve the list of last-active profiles. If there are no last-active
+  // profiles (which is rare -- e.g, when the last-active profiles were
+  // removed), then use all profiles for which the app is installed.
+  std::set<base::FilePath> last_active_profile_paths =
+      AppShimRegistry::Get()->GetLastActiveProfilesForApp(host->GetAppId());
+  if (last_active_profile_paths.empty()) {
+    last_active_profile_paths =
+        AppShimRegistry::Get()->GetInstalledProfilesForApp(host->GetAppId());
+  }
+  if (last_active_profile_paths.empty()) {
+    return;
+  }
+  // Open settings in the first of these profiles.
+  LoadProfileAsync(
+      *last_active_profile_paths.begin(),
+      base::BindOnce(
+          [](const web_app::AppId& app_id, Profile* profile) {
+            if (profile) {
+              chrome::ShowWebAppSettings(
+                  profile, app_id,
+                  web_app::AppSettingsPageEntryPoint::kBrowserCommand);
+            }
+          },
+          host->GetAppId()));
 }
 
 void AppShimManager::OnShimOpenedUrls(AppShimHost* host,
