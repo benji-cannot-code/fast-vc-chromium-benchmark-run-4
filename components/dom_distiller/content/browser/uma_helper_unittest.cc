9be4fd7b7e69c6bf7538a97dcf02ed8026a08cdd
Ho Cheung <uioptt24@gmail.com>
2023-08-11 17:08:08 +0000

    [a11y] Remove expired histograms ActivelyViewingArticleBeforeDistilling
    1. The following histograms have been removed and obsoleted in this CL:
    DomDistiller.Time.ActivelyViewingArticleBeforeDistilling.
    
    OBSOLETE_HISTOGRAMS=No longer needed.
    
    2. Remove some code that no longer works.
    
    Bug: 1061928
    Fixed: 1448187
    Change-Id: Ie8763b105459a4d715e5fe05f7b99025cbd5329d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4764650
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Ho Cheung <uioptt24@gmail.com>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1182643}

--- components/dom_distiller/content/browser/uma_helper_unittest.cc
+++ components/dom_distiller/content/browser/uma_helper_unittest.cc
@@ -28,7 +28,6 @@ TEST_F(UMAHelperTest, TestTimerBasics) {
   UMAHelper::DistillabilityDriverTimer timer;
   ASSERT_FALSE(timer.HasStarted());
   timer.Start(false);
-  ASSERT_FALSE(timer.IsTimingDistilledPage());
   ASSERT_TRUE(timer.HasStarted());
 
   FastForwardBy(100);
@@ -84,7 +83,6 @@ TEST_F(UMAHelperTest, TestTimerForDistilledPage) {
 
   timer.Start(true);
   ASSERT_TRUE(timer.HasStarted());
-  ASSERT_TRUE(timer.IsTimingDistilledPage());
   FastForwardBy(100);
   ASSERT_EQ(timer.GetElapsedTime().InMilliseconds(), 100);
   timer.Start(true);
