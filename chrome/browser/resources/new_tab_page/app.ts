7fa7ca6cec65cd3782e1f773a335d8273b7d51f9
Roman Arora <romanarora@chromium.org>
2023-08-11 20:49:31 +0000

    Revert "[ntp] Fix webstore toast scroll flicker bug"
    This reverts commit 7ffff8d0a52d00808304bd7fddbf7298aa1f7791.
    
    Reason for revert: Introduces a different type of flicker issue.
    
    Original change's description:
    > [ntp] Fix webstore toast scroll flicker bug
    >
    > Fixes a scroll flicker bug and lazy renders the webstore toast.
    >
    > Bug: 1472228
    > Change-Id: Ie462107bb06cb2626aff73a2bb9ed0161fbf0a39
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4775458
    > Commit-Queue: Roman Arora <romanarora@chromium.org>
    > Reviewed-by: Riley Tatum <rtatum@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1182639}
    
    Bug: 1472228
    Change-Id: I32d893fc2305946f4dd06ae2ca53be910e5d7c60
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4776120
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1182761}

--- chrome/browser/resources/new_tab_page/app.ts
+++ chrome/browser/resources/new_tab_page/app.ts
@@ -108,6 +108,7 @@ export interface AppElement {
     customizeDialogIf: DomIf,
     oneGoogleBarClipPath: HTMLElement,
     logo: LogoElement,
+    webstoreToast: CrToastElement,
   };
 }
 
@@ -428,7 +429,7 @@ export class AppElement extends AppElementBase {
     this.showWebstoreToastListenerId_ =
         NewTabPageProxy.getInstance()
             .callbackRouter.showWebstoreToast.addListener(() => {
-              $$<CrToastElement>(this, '#webstoreToast')!.show();
+              this.$.webstoreToast.show();
             });
 
     // Open Customize Chrome if there are Customize Chrome URL params.
