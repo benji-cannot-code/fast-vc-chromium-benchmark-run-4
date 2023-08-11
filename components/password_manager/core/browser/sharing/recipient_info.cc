5a980f17081ef2a3096cb563563b98dfb0658326
Rushan Suleymanov <rushans@google.com>
2023-08-11 15:03:35 +0000

    Move PublicKey into a separate struct
    Bug: 1456309
    Change-Id: Ic1f684bfc982521b50ed1a5393b2038a83c73a6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4771576
    Reviewed-by: Elias Khsheibun <eliaskh@chromium.org>
    Auto-Submit: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1182579}

--- components/password_manager/core/browser/sharing/recipient_info.cc
+++ components/password_manager/core/browser/sharing/recipient_info.cc
@@ -3,9 +3,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/password_manager/core/browser/sharing/recipients_fetcher.h"
+#include "components/password_manager/core/browser/sharing/recipient_info.h"
 
 namespace password_manager {
+
+bool PublicKey::operator==(const PublicKey& other) const {
+  return key == other.key && key_version == other.key_version;
+}
+
+// static
+PublicKey PublicKey::FromProto(
+    const sync_pb::CrossUserSharingPublicKey& proto_public_key) {
+  PublicKey result;
+  result.key = proto_public_key.x25519_public_key();
+  result.key_version = proto_public_key.version();
+  return result;
+}
 
 RecipientInfo::RecipientInfo() = default;
 
@@ -17,7 +30,8 @@ RecipientInfo::~RecipientInfo() = default;
 
 bool RecipientInfo::operator==(const RecipientInfo& other) const {
   return user_id == other.user_id && user_name == other.user_name &&
-         email == other.email && profile_image_url == other.profile_image_url;
+         email == other.email && profile_image_url == other.profile_image_url &&
+         public_key == other.public_key;
 }
 
 }  // namespace password_manager
