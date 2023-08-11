aaf61e0f8fb2c85f9f41b5fbcd3cb7e8ad4addfc
Carlos IL <carlosil@chromium.org>
2023-08-11 16:48:05 +0000

    Enable OmniboxUpdatedConnectionSecurityIndicators by default
    This change is controlled in other platforms by the Chrome 2023 refresh
    which doesn't exist on Android. This will launch default-on on Android
    (with the flag as a backup killswitch).
    
    This also removes the IDS_PAGE_INFO_SECURE_DETAILS_V2 string, which was
    shown on desktop when the flag was enabled. This was part of experiment
    behavior that did not launch.
    
    Bug: 1468591
    Change-Id: I2c76e73bcdf7c647926b3b34a2e535887b4a01f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4761690
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1182632}

--- components/page_info/page_info_ui.cc
+++ components/page_info/page_info_ui.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "components/content_settings/core/common/content_settings.h"
-#include "components/omnibox/common/omnibox_features.h"
 #include "components/page_info/core/features.h"
 #include "components/page_info/page_info.h"
 #include "components/page_info/page_info_ui_delegate.h"
@@ -547,10 +546,7 @@ PageInfoUI::GetSecurityDescription(const IdentityInfo& identity_info) const {
 
           auto description = CreateSecurityDescription(
               SecuritySummaryColor::GREEN, IDS_PAGE_INFO_SECURE_SUMMARY,
-              base::FeatureList::IsEnabled(
-                  omnibox::kUpdatedConnectionSecurityIndicators)
-                  ? IDS_PAGE_INFO_SECURE_DETAILS_V2
-                  : IDS_PAGE_INFO_SECURE_DETAILS,
+              IDS_PAGE_INFO_SECURE_DETAILS,
               SecurityDescriptionType::CONNECTION);
           if (identity_info.identity_status ==
               PageInfo::SITE_IDENTITY_STATUS_ADMIN_PROVIDED_CERT) {
