adb97a97046a9f78e9e67be420897c845cf588b0
Ruma Kesh <rkesh@google.com>
2023-08-11 16:59:22 +0000

    CrOS Settings: Update import for CrDrawerElement in the test files.
    The element should be accessed from the 'os_settings' file in order to
    avoid the element from potentially being registered twice.
    
    Bug: b/270728282
    Test: browser_tests --gtest_filter="OSSettings*"
    Change-Id: I783329d2ed52a5229f0c40619774d1e8b0dba759
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4771916
    Commit-Queue: Ruma Kesh <rkesh@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1182637}

--- chrome/test/data/webui/settings/chromeos/os_settings_ui/os_settings_ui_test.ts
+++ chrome/test/data/webui/settings/chromeos/os_settings_ui/os_settings_ui_test.ts
@@ -3,8 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {CrSettingsPrefs, MainPageContainerElement, OsSettingsMainElement, OsSettingsUiElement} from 'chrome://os-settings/os_settings.js';
-import {CrDrawerElement} from 'chrome://resources/cr_elements/cr_drawer/cr_drawer.js';
+import 'chrome://os-settings/os_settings.js';
+
+import {CrDrawerElement, CrSettingsPrefs, MainPageContainerElement, OsSettingsMainElement, OsSettingsUiElement} from 'chrome://os-settings/os_settings.js';
 import {assert} from 'chrome://resources/js/assert_ts.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
