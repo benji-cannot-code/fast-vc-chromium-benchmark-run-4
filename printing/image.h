5fbb0b77ca0e7fac5f4d76892f7c8ea52754aec3
Morten Stenshorne <mstensho@chromium.org>
2023-08-11 09:31:36 +0000

    Remove unused parts from printing::Image.
    Change-Id: Ie8bc5abf3bde9a02b40e3e514e78d2d29350e8da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4767318
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1182491}

--- printing/image.h
+++ printing/image.h
@@ -15,16 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "ui/gfx/geometry/size.h"
 
-namespace base {
-class FilePath;
-}
-
 namespace printing {
 
 class Metafile;
 
 // Lightweight raw-bitmap management. The image, once initialized, is immutable.
-// The main purpose is testing image contents.
+// The only purpose is testing image contents.
 class Image {
  public:
   // Creates the image from the metafile.  Deduces bounds based on bounds in
@@ -41,18 +37,9 @@ class Image {
   // Return a checksum of the image (MD5 over the internal data structure).
   std::string checksum() const;
 
-  // Save image as PNG.
-  bool SaveToPng(const base::FilePath& filepath) const;
-
-  // Returns % of pixels different
-  double PercentageDifferent(const Image& rhs) const;
-
-  // Returns the 0x0RGB or 0xARGB value of the pixel at the given location.
+  // Returns the 0x0RGB value of the pixel at the given location.
   uint32_t Color(uint32_t color) const {
-    if (ignore_alpha_)
-      return color & 0xFFFFFF;  // Strip out A.
-    else
-      return color;
+    return color & 0xFFFFFF;  // Strip out alpha channel.
   }
 
   uint32_t pixel_at(int x, int y) const {
@@ -64,12 +51,6 @@ class Image {
   }
 
  private:
-  // Construct from metafile.  This is kept internal since it's ambiguous what
-  // kind of data is used (png, bmp, metafile etc).
-  Image(const void* data, size_t size);
-
-  bool LoadPng(const std::string& compressed);
-
   // Loads the first page from `metafile`.
   bool LoadMetafile(const Metafile& metafile);
 
@@ -82,9 +63,6 @@ class Image {
   // Actual bitmap data in arrays of RGBAs (so when loaded as uint32_t, it's
   // 0xABGR).
   std::vector<unsigned char> data_;
-
-  // Flag to signal if the comparison functions should ignore the alpha channel.
-  const bool ignore_alpha_;  // Currently always true.
 
   // Prevent operator= (this function has no implementation)
   Image& operator=(const Image& image);
