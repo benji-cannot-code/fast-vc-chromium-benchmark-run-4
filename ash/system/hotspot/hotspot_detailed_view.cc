d18fd36c1d83c79ede4753244748bb271bc19c5a
Jason Zhang <jiajunz@google.com>
2023-08-11 20:20:44 +0000

    [CrOS Hotspot] Enable hotspot toggle when enabling in QS
    This CL enables the hotspot toggle in detailed view when enabling to
    support aborting hotspot enable operation.
    
    Bug: b/295190831
    Change-Id: Ie04be3766705b6ef2184d0e09b6dfca952222ca2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4771045
    Reviewed-by: Nikhil Nayunigari <nikhilcn@google.com>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1182743}

--- ash/system/hotspot/hotspot_detailed_view.cc
+++ ash/system/hotspot/hotspot_detailed_view.cc
@@ -43,10 +43,6 @@ namespace {
 // Used for setting the insets of broader hotspot entry row.
 constexpr auto kToggleRowTriViewInsets = gfx::Insets::VH(8, 24);
 
-bool IsIntermediateState(HotspotState state) {
-  return state == HotspotState::kDisabling || state == HotspotState::kEnabling;
-}
-
 bool IsEnabledOrEnabling(HotspotState state) {
   return state == HotspotState::kEnabled || state == HotspotState::kEnabling;
 }
@@ -184,8 +180,8 @@ void HotspotDetailedView::UpdateToggleState(
     const HotspotAllowStatus& allow_status) {
   toggle_->SetIsOn(IsEnabledOrEnabling(state));
 
-  bool enabled = !IsIntermediateState(state) &&
-                 allow_status == HotspotAllowStatus::kAllowed;
+  bool enabled = allow_status == HotspotAllowStatus::kAllowed &&
+                 state != HotspotState::kDisabling;
   entry_row_->SetEnabled(enabled);
 }
 
