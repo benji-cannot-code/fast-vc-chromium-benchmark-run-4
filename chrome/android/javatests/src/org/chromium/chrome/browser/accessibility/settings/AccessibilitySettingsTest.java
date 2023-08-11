cc07453885e69e1a01cb7946814bdc9784e5769b
Mark Schillaci <mschillaci@google.com>
2023-08-11 21:49:43 +0000

    [Auto-disable Accessibility] Deprecate the old isTouchExplorationEnabled method
    This CL continues the work of consolidating accessibility state to a
    single source in Clank.
    
    With this CL we update all callers of isTouchExplorationEnabled to the
    equivalent method in AccessibilityState. For some methods we instead
    migrate to isScreenReaderEnabled, since it more accurately represents
    the condition that was meant to be checked. We keep the original method
    in place but add a @Deprecated annotation to discourage use.
    
    AX-Relnotes: N/A
    Bug: 1430202, b/265493191
    Change-Id: Ib0c6194bca1ae734c30dc96b41d12de69e9b2145
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4748989
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1182796}

--- chrome/android/javatests/src/org/chromium/chrome/browser/accessibility/settings/AccessibilitySettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/accessibility/settings/AccessibilitySettingsTest.java
@@ -34,7 +34,6 @@ import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.settings.SettingsActivityTestRule;
-import org.chromium.chrome.browser.util.ChromeAccessibilityUtil;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.R;
 import org.chromium.chrome.test.util.browser.Features.DisableFeatures;
@@ -45,6 +44,7 @@ import org.chromium.components.browser_ui.settings.ChromeSwitchPreference;
 import org.chromium.content_public.browser.ContentFeatureList;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.content_public.browser.test.util.UiUtils;
+import org.chromium.ui.accessibility.AccessibilityState;
 
 import java.text.NumberFormat;
 
@@ -64,10 +64,8 @@ public class AccessibilitySettingsTest {
 
     @After
     public void tearDown() {
-        TestThreadUtils.runOnUiThreadBlocking(() -> {
-            ChromeAccessibilityUtil.get().setAccessibilityEnabledForTesting(false);
-            ChromeAccessibilityUtil.get().setTouchExplorationEnabledForTesting(false);
-        });
+        TestThreadUtils.runOnUiThreadBlocking(
+                () -> { AccessibilityState.setIsScreenReaderEnabledForTesting(false); });
     }
 
     /**
@@ -199,13 +197,12 @@ public class AccessibilitySettingsTest {
     @SmallTest
     @Feature({"Accessibility"})
     public void testImageDescriptionsPreferences_Enabled() {
-        // Enable touch exploration to display settings option.
-        TestThreadUtils.runOnUiThreadBlocking(() -> {
-            ChromeAccessibilityUtil.get().setAccessibilityEnabledForTesting(true);
-            ChromeAccessibilityUtil.get().setTouchExplorationEnabledForTesting(true);
-        });
+        // Enable screen reader to display settings option.
+        TestThreadUtils.runOnUiThreadBlocking(
+                () -> { AccessibilityState.setIsScreenReaderEnabledForTesting(true); });
 
         mSettingsActivityTestRule.startSettingsActivity();
+
         AccessibilitySettings accessibilitySettings = mSettingsActivityTestRule.getFragment();
 
         Preference imageDescriptionsPref =
