af5c5fa7885edf27590ec51ebc5045e4569c795c
Sean Kau <skau@chromium.org>
2023-08-11 21:04:38 +0000

    Fix focus rings in ash/style
    Browser recently updated focus ring styling. Revert it for ChromeOS as
    we already updated our styling.
    
    Bug: b:288161056
    Change-Id: I330d9fd834fe5fd85a10d6c2ccc1791ff400f01a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4775193
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Xiaodan Zhu <zxdan@chromium.org>
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1182780}

--- ash/style/icon_button.cc
+++ ash/style/icon_button.cc
@@ -161,6 +161,7 @@ IconButton::IconButton(PressedCallback callback,
   UpdateVectorIcon();
 
   auto* focus_ring = views::FocusRing::Get(this);
+  focus_ring->SetOutsetFocusRingDisabled(true);
   focus_ring->SetColorId(
       chromeos::features::IsJellyrollEnabled()
           ? cros_tokens::kCrosSysFocusRing
--- ash/style/style_util.cc
+++ ash/style/style_util.cc
@@ -191,6 +191,7 @@ views::FocusRing* StyleUtil::SetUpFocusRingForView(
   DCHECK(view);
   views::FocusRing::Install(view);
   views::FocusRing* focus_ring = views::FocusRing::Get(view);
+  focus_ring->SetOutsetFocusRingDisabled(true);
   focus_ring->SetColorId(ui::kColorAshFocusRing);
   if (halo_inset)
     focus_ring->SetHaloInset(*halo_inset);
