32cadf5ba26da754dbfea0c64ca9ed37ea324e19
Jan Keitel <jkeitel@google.com>
2023-08-14 06:15:40 +0000

    Attempt to fix dangling pointer in TestAutofillManagerWaiter.
    This CL is a speculative fix for a flaky dangling pointer inside
    TestAutofillManagerWaiter.
    
    While TestAutofillManagerWaiter observes AutofillManager destruction
    events, it currently does not null its reference to the AutofillManager
    immediately. Instead, it calls RunLoop::Quit(). It appears that the
    call to Quit() can spawn another task that actually quits the run
    loop - see the code of RunLoop::Quit().
    This CL therefore replaces the reference by a pointer and nulls it
    explicitly when the AutofillManager it is observing is destroyed.
    
    Bug: 1468967
    Change-Id: I6411872cc436feb5fc49f14e90b40e66ff40a928
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4770941
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1182981}

--- components/autofill/core/browser/test_autofill_manager_waiter.cc
+++ components/autofill/core/browser/test_autofill_manager_waiter.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/logging.h"
 #include "base/memory/raw_ptr.h"
-#include "base/memory/raw_ref.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/scoped_run_loop_timeout.h"
@@ -287,7 +286,7 @@ const FormStructure* WaitForMatchingForm(
    public:
     explicit Waiter(AutofillManager* manager,
                     base::RepeatingCallback<bool(const FormStructure&)> pred)
-        : manager_(*manager), pred_(std::move(pred)) {
+        : manager_(manager), pred_(std::move(pred)) {
       observation_.Observe(manager);
     }
 
@@ -314,20 +313,22 @@ const FormStructure* WaitForMatchingForm(
 
    private:
     void OnAutofillManagerDestroyed(AutofillManager& manager) override {
-      DCHECK_EQ(&manager, &manager_.get());
+      DCHECK_EQ(&manager, manager_.get());
+      manager_ = nullptr;
       run_loop_.Quit();
       observation_.Reset();
     }
 
     void OnAutofillManagerReset(AutofillManager& manager) override {
-      DCHECK_EQ(&manager, &manager_.get());
+      DCHECK_EQ(&manager, manager_.get());
+      manager_ = nullptr;
       run_loop_.Quit();
       observation_.Reset();
     }
 
     void OnAfterFormsSeen(AutofillManager& manager,
                           base::span<const FormGlobalId> forms) override {
-      DCHECK_EQ(&manager, &manager_.get());
+      DCHECK_EQ(&manager, manager_.get());
       if (const auto* form = FindForm()) {
         matching_form_ = form;
         run_loop_.Quit();
@@ -344,7 +345,7 @@ const FormStructure* WaitForMatchingForm(
 
     base::ScopedObservation<AutofillManager, AutofillManager::Observer>
         observation_{this};
-    const raw_ref<AutofillManager, FlakyDanglingUntriaged> manager_;
+    raw_ptr<AutofillManager> manager_;
     base::RepeatingCallback<bool(const FormStructure&)> pred_;
     base::RunLoop run_loop_;
     raw_ptr<const FormStructure> matching_form_ = nullptr;
