3dd9bf746d842f03bd28cad72e2bf0c001aaf32f
Theresa Sullivan <twellington@chromium.org>
2023-08-14 02:45:15 +0000

    Only allow supported download types on Automotive
    On Android Automotive, only downloads that can be opened by Chrome will
    be initially allowed to be downloaded. Intercept downloads with a mime
    type that can't be handled by Blink and show a snackbar.
    
    Bug: 1469477
    Low-Coverage-Reason: DownloadMessageBridge contains very simple code to call existing APIs; not interesting to unit test.
    Change-Id: I03617c8345beb81a22e12a8e246d8c6e018f4772
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4775333
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1182921}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadMessageBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadMessageBridge.java
@@ -5,8 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.download;
 
+import android.content.Context;
+
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.NativeMethods;
+import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ui.messages.snackbar.Snackbar;
+import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
+import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManagerProvider;
+import org.chromium.ui.base.WindowAndroid;
 
 public class DownloadMessageBridge {
     private long mNativeDownloadMessageBridge;
@@ -31,6 +38,19 @@ public class DownloadMessageBridge {
                         /*otrProfileID=*/null);
         messageUiController.showIncognitoDownloadMessage(
                 (accepted) -> { onConfirmed(callbackId, accepted); });
+    }
+
+    @CalledByNative
+    public void showUnsupportedDownloadMessage(WindowAndroid window) {
+        SnackbarManager snackbarManager = SnackbarManagerProvider.from(window);
+        if (snackbarManager == null) return;
+
+        Context context = window.getContext().get();
+        Snackbar snackbar =
+                Snackbar.make(context.getString(R.string.download_file_type_not_supported), null,
+                        Snackbar.TYPE_NOTIFICATION, Snackbar.UMA_AUTO_LOGIN);
+        snackbar.setAction(context.getString(R.string.ok), null);
+        snackbarManager.showSnackbar(snackbar);
     }
 
     @CalledByNative
--- chrome/browser/download/chrome_download_manager_delegate.cc
+++ chrome/browser/download/chrome_download_manager_delegate.cc
@@ -93,6 +93,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 
 #if BUILDFLAG(IS_ANDROID)
+#include "base/android/build_info.h"
 #include "base/android/content_uri_utils.h"
 #include "base/android/path_utils.h"
 #include "chrome/browser/download/android/chrome_duplicate_download_infobar_delegate.h"
@@ -108,6 +109,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/flags/android/chrome_feature_list.h"
 #include "components/infobars/content/content_infobar_manager.h"
 #include "net/http/http_content_disposition.h"
+#include "third_party/blink/public/common/mime_util/mime_util.h"
 #include "ui/android/window_android.h"
 #else
 #include "chrome/browser/download/download_item_web_app_data.h"
@@ -504,6 +506,11 @@ void ChromeDownloadManagerDelegate::SetDownloadDialogBridgeForTesting(
     DownloadDialogBridge* bridge) {
   download_dialog_bridge_.reset(bridge);
 }
+
+void ChromeDownloadManagerDelegate::SetDownloadMessageBridgeForTesting(
+    DownloadMessageBridge* bridge) {
+  download_message_bridge_.reset(bridge);
+}
 #endif  // BUILDFLAG(IS_ANDROID)
 
 void ChromeDownloadManagerDelegate::Shutdown() {
@@ -828,6 +835,16 @@ bool ChromeDownloadManagerDelegate::InterceptDownloadIfApplicable(
     return true;
   }
 #endif
+
+#if BUILDFLAG(IS_ANDROID)
+  if (base::android::BuildInfo::GetInstance()->is_automotive()) {
+    if (!blink::IsSupportedMimeType(mime_type)) {
+      download_message_bridge_->ShowUnsupportedDownloadMessage(web_contents);
+      return true;
+    }
+  }
+#endif  // BUILDFLAG(IS_ANDROID)
+
   return false;
 }
 
