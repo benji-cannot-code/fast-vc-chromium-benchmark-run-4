07ea116aa09c6d174677bcb5873c51303a6a03f4
Sam Davis Omekara <samomekarajr@microsoft.com>
2023-08-14 23:21:10 +0000

    Complete the definition of Emulated Forced Colors Color Provider
    This CL is the first step of moving the forced colors state to the
    color pipeline.
    
    This change does not affect the current behavior of the forced colors
    mode. The main change is to add color definitions for system colors to
    the Emulated Forced Colors Color Provider. The provider currently only
    has color definitions for scrollbars, this change adds color
    definitions for system colors. In the subsequent CLs, we will
    implement the logic to switch between the Emulated Forced Colors Color
    Provider and the default Forced Colors Color Provider based on the
    forced colors state and apply the system colors to web content and
    other controls accordingly.
    
    Bug: 1430181
    Change-Id: If0827a97e4d9e16fa9e4a8703b5a021c3f8e7596
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4758021
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Sam Davis Omekara <samomekarajr@microsoft.com>
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1183406}

--- ui/color/win/native_color_mixers_win.cc
+++ ui/color/win/native_color_mixers_win.cc
@@ -207,8 +207,9 @@ void AddNativeUiColorMixer(ColorProvider* provider,
   // Window Background
   mixer[kColorBubbleFooterBackground] = {kColorNativeWindow};
   mixer[kColorButtonBackgroundProminentDisabled] = {kColorNativeWindow};
-  mixer[kColorFrameActive] = {ui::kColorNativeWindow};
-  mixer[kColorFrameInactive] = {ui::kColorNativeWindow};
+  mixer[kColorForcedWindow] = {kColorNativeWindow};
+  mixer[kColorFrameActive] = {kColorNativeWindow};
+  mixer[kColorFrameInactive] = {kColorNativeWindow};
   mixer[kColorPrimaryBackground] = {kColorNativeWindow};
   mixer[kColorTooltipBackground] = {kColorNativeWindow};
 
@@ -217,6 +218,7 @@ void AddNativeUiColorMixer(ColorProvider* provider,
   mixer[kColorAlertMediumSeverityIcon] = {kColorNativeWindowText};
   mixer[kColorAlertMediumSeverityText] = {kColorNativeWindowText};
   mixer[kColorAlertHighSeverity] = {kColorNativeWindowText};
+  mixer[kColorForcedWindowText] = {kColorNativeWindowText};
   mixer[kColorIcon] = {kColorNativeWindowText};
   mixer[kColorMidground] = {kColorNativeWindowText};
   mixer[kColorPrimaryForeground] = {kColorNativeWindowText};
@@ -226,12 +228,14 @@ void AddNativeUiColorMixer(ColorProvider* provider,
   mixer[kColorTooltipForeground] = {kColorNativeWindowText};
 
   // Hyperlinks
+  mixer[kColorForcedHotlight] = {kColorNativeHotlight};
   mixer[kColorLinkForegroundDefault] = {kColorNativeHotlight};
   mixer[kColorLinkForegroundPressedDefault] = {kColorNativeHotlight};
   mixer[kColorMenuItemForegroundHighlighted] = {kColorNativeHotlight};
 
   // Gray/Disabled Text
   mixer[kColorDisabledForeground] = {kColorNativeGrayText};
+  mixer[kColorForcedGrayText] = {kColorNativeGrayText};
   mixer[kColorMenuItemForegroundDisabled] = {kColorNativeGrayText};
   mixer[kColorLinkForegroundDisabled] = {kColorNativeGrayText};
   mixer[kColorLabelForegroundDisabled] = {kColorNativeGrayText};
@@ -241,6 +245,7 @@ void AddNativeUiColorMixer(ColorProvider* provider,
   // Button Background
   mixer[kColorButtonBackground] = {kColorNativeBtnFace};
   mixer[kColorComboboxBackground] = {kColorNativeBtnFace};
+  mixer[kColorForcedBtnFace] = {kColorNativeBtnFace};
   mixer[kColorMenuBackground] = {kColorNativeBtnFace};
   mixer[kColorSliderTrack] = {kColorNativeBtnFace};
   mixer[kColorSliderTrackMinimal] = {kColorNativeBtnFace};
@@ -253,6 +258,7 @@ void AddNativeUiColorMixer(ColorProvider* provider,
   mixer[kColorCheckboxForegroundChecked] = {kColorNativeBtnText};
   mixer[kColorFocusableBorderFocused] = {kColorNativeBtnText};
   mixer[kColorFocusableBorderUnfocused] = {kColorNativeBtnText};
+  mixer[kColorForcedBtnText] = {kColorNativeBtnText};
   mixer[kColorMenuBorder] = {kColorNativeBtnText};
   mixer[kColorMenuItemForeground] = {kColorNativeBtnText};
   mixer[kColorMenuItemForegroundSecondary] = {kColorNativeBtnText};
@@ -272,6 +278,7 @@ void AddNativeUiColorMixer(ColorProvider* provider,
   mixer[kColorButtonBackgroundProminent] = {kColorNativeHighlight};
   mixer[kColorButtonBorder] = {kColorNativeHighlight};
   mixer[kColorButtonBackgroundProminentFocused] = {kColorNativeHighlight};
+  mixer[kColorForcedHighlight] = {kColorNativeHighlight};
   mixer[kColorHelpIconActive] = {kColorNativeHighlight};
   mixer[kColorItemSelectionBackground] = {kColorNativeHighlight};
   mixer[kColorMenuSelectionBackground] = {kColorNativeHighlight};
@@ -281,6 +288,7 @@ void AddNativeUiColorMixer(ColorProvider* provider,
 
   // Highlight/Selected Text Foreground
   mixer[kColorButtonForegroundProminent] = {kColorNativeHighlightText};
+  mixer[kColorForcedHighlightText] = {kColorNativeHighlightText};
   mixer[kColorMenuItemForegroundSelected] = {kColorNativeHighlightText};
   mixer[kColorNotificationInputForeground] = {kColorNativeHighlightText};
   mixer[kColorTableForegroundSelectedFocused] = {kColorNativeHighlightText};
@@ -290,6 +298,12 @@ void AddNativeUiColorMixer(ColorProvider* provider,
   mixer[kColorTreeNodeForegroundSelectedUnfocused] = {
       kColorNativeHighlightText};
   mixer[kColorButtonForegroundProminent] = {kColorNativeHighlightText};
+
+  // Menu Highlight
+  mixer[kColorForcedMenuHilight] = {kColorNativeMenuHilight};
+
+  // Scrollbar
+  mixer[kColorForcedScrollbar] = {kColorNativeScrollbar};
 }
 
 }  // namespace ui
