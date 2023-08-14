8798ee30382c2efe1467610fb0622e6cedefa04b
Vikas Soni <vikassoni@chromium.org>
2023-08-14 18:45:02 +0000

    [MappableSI] Get GpuMemoryBufferHandle from backings.
    Implement a method to query GpuMemoryBufferHandle from the backings
    which supports native buffers as well as shared memory.
    
    Bug: 1472340
    Change-Id: I28d6ed151b94a092e1293005f9a2af7f1c97f9f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4771625
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Auto-Submit: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183218}

--- gpu/command_buffer/service/shared_image/shared_image_backing.h
+++ gpu/command_buffer/service/shared_image/shared_image_backing.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/color_space.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
+#include "ui/gfx/gpu_memory_buffer.h"
 #include "ui/gfx/native_pixmap.h"
 
 #if BUILDFLAG(IS_WIN)
@@ -168,7 +169,7 @@ class GPU_GLES2_EXPORT SharedImageBacking {
   // Marks the provided rect as cleared.
   virtual void SetClearedRect(const gfx::Rect& cleared_rect) = 0;
 
-  // Indicate that the image is purgable. When an image is purgeable, its
+  // Indicate that the image is purgeable. When an image is purgeable, its
   // contents may be discarded at any time. Before the image can be used again,
   // it must be set to be not-purgeable. This is intended to be lighter-weight
   // than allocating and freeing the image. See investigation in
@@ -229,6 +230,9 @@ class GPU_GLES2_EXPORT SharedImageBacking {
   // Returns the NativePixmap backing the SharedImageBacking. Returns null if
   // the SharedImage is not backed by a NativePixmap.
   virtual scoped_refptr<gfx::NativePixmap> GetNativePixmap();
+
+  // Returns the GpuMemoryBufferHandle if present.
+  virtual gfx::GpuMemoryBufferHandle GetGpuMemoryBufferHandle();
 
   // Helper to determine if the entire SharedImage is cleared.
   bool IsCleared() const { return ClearedRect() == gfx::Rect(size()); }
