ed25ba1a4445abdbbd58d8f9eda2d31367133541
Ruma Kesh <rkesh@google.com>
2023-08-14 20:11:21 +0000

    CrOS Settings: Update import for AppManagementToggleRowElement in the test files.
    The element should be accessed from the 'os_settings' file in order to
    avoid the element from potentially being registered twice.
    
    Bug: b/270728282
    Test: browser_tests --gtest_filter="OSSettings*"
    Change-Id: I3732f59bed4c0e7e1ffc5c613969d1a4cb583b1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4775620
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Ruma Kesh <rkesh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1183282}

--- chrome/test/data/webui/settings/chromeos/app_management/file_handling_item_test.ts
+++ chrome/test/data/webui/settings/chromeos/app_management/file_handling_item_test.ts
@@ -3,9 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {AppManagementFileHandlingItemElement, AppManagementStore, updateSelectedAppId} from 'chrome://os-settings/os_settings.js';
+import {AppManagementFileHandlingItemElement, AppManagementStore, AppManagementToggleRowElement, updateSelectedAppId} from 'chrome://os-settings/os_settings.js';
 import {AppType} from 'chrome://resources/cr_components/app_management/app_management.mojom-webui.js';
-import {AppManagementToggleRowElement} from 'chrome://resources/cr_components/app_management/toggle_row.js';
 import {assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
 
--- chrome/test/data/webui/settings/chromeos/os_apps_page/app_management_page/pwa_detail_view_test.ts
+++ chrome/test/data/webui/settings/chromeos/os_apps_page/app_management_page/pwa_detail_view_test.ts
@@ -6,10 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://os-settings/lazy_load.js';
 
 import {AppManagementPinToShelfItemElement, AppManagementPwaDetailViewElement, AppManagementSubAppsItemElement} from 'chrome://os-settings/lazy_load.js';
-import {AppManagementStore, CrToggleElement, updateSelectedAppId, updateSubAppToParentAppId} from 'chrome://os-settings/os_settings.js';
+import {AppManagementStore, AppManagementToggleRowElement, CrToggleElement, updateSelectedAppId, updateSubAppToParentAppId} from 'chrome://os-settings/os_settings.js';
 import {App, InstallReason} from 'chrome://resources/cr_components/app_management/app_management.mojom-webui.js';
 import {PermissionTypeIndex} from 'chrome://resources/cr_components/app_management/permission_constants.js';
-import {AppManagementToggleRowElement} from 'chrome://resources/cr_components/app_management/toggle_row.js';
 import {convertOptionalBoolToBool, getPermissionValueBool} from 'chrome://resources/cr_components/app_management/util.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 
