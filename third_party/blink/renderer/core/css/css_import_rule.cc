d5b2e88108d3ce61fa2e10bd07994b8187f28612
Steinar H. Gunderson <sesse@chromium.org>
2023-08-14 12:14:09 +0000

    Fix quoting of @import URLs.
    Fixed: 1472259
    Change-Id: If53ff9f4634550f3a24476c4d777bebd46816009
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4777124
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183049}

--- third_party/blink/renderer/core/css/css_import_rule.cc
+++ third_party/blink/renderer/core/css/css_import_rule.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/css/css_import_rule.h"
 
+#include "third_party/blink/renderer/core/css/css_markup.h"
 #include "third_party/blink/renderer/core/css/css_style_sheet.h"
 #include "third_party/blink/renderer/core/css/media_list.h"
 #include "third_party/blink/renderer/core/css/style_rule_import.h"
@@ -51,9 +52,8 @@ MediaList* CSSImportRule::media() {
 
 String CSSImportRule::cssText() const {
   StringBuilder result;
-  result.Append("@import url(\"");
-  result.Append(import_rule_->Href());
-  result.Append("\")");
+  result.Append("@import ");
+  result.Append(SerializeURI(import_rule_->Href()));
 
   if (import_rule_->IsLayered()) {
     result.Append(" layer");
