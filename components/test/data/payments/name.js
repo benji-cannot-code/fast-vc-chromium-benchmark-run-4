cdeba13bb39bb64c28e766e1e75d692e2400887a
Slobodan Pejic <slobodan@chromium.org>
2023-08-14 18:52:26 +0000

    Fix PaymentRequestNameTest by migrating from basic-card.
    Update tests to use the test-only payment app instead of the removed
    basic-card payment method.
    
    Use HistogramWatcher to assert histogram records since
    RecordHistogram.getHistogramValueCountForTesting is deprecated.
    
    Test: https://chromium-swarm.appspot.com/task?id=640da60577e28310
    Bug: 1182234
    Bug: 1331489
    Bug: 1331469
    Bug: 1339009
    Bug: 1331496
    Bug: 1331475
    Change-Id: I9317a35bc166a2e6d5243cd2b5a59b6b39fbe2b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4774617
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183223}

--- components/test/data/payments/name.js
+++ components/test/data/payments/name.js
@@ -6,16 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 /**
- * Launches the PaymentRequest UI that requests payer name.
- *
- * Legacy entry function until basic-card is removed.
- */
-function buy() {
-  buyWithMethods(
-      [{supportedMethods: 'basic-card', data: {supportedNetworks: ['visa']}}]);
-}
-
-/**
  * Launches the PaymentRequest UI that offers free shipping worldwide.
  *
  * @param {sequence<PaymentMethodData>} methodData - An array of payment method
