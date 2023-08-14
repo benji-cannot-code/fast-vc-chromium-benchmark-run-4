9216f6ffd78e81870b33a50617ad12158ffdf4a5
Steve Kobes <skobes@chromium.org>
2023-08-14 14:02:43 +0000

    Remove uses of --disable-threaded-scrolling in content browsertests.
    Bug: 1163246,1087725,1369739
    Change-Id: Iaf251bae23de1fd5c110e3877d9e05c44b748ef0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4754673
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183076}

--- content/browser/renderer_host/input/composited_scrolling_browsertest.cc
+++ content/browser/renderer_host/input/composited_scrolling_browsertest.cc
@@ -194,7 +194,7 @@ class CompositedScrollingMetricTest : public CompositedScrollingBrowserTest,
     if (enable_composited_scrolling)
       cmd->AppendSwitch(blink::switches::kEnablePreferCompositingToLCDText);
     else
-      cmd->AppendSwitch(blink::switches::kDisableThreadedScrolling);
+      cmd->AppendSwitch(blink::switches::kDisablePreferCompositingToLCDText);
   }
 
   bool CompositingEnabled() { return GetParam(); }
