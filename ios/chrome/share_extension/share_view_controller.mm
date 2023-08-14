64a609123b65e7b661464dab4b8e67fbcef24c61
Cheick Cisse <cheickcisse@google.com>
2023-08-14 16:54:44 +0000

    [iOS] Fix sharing extension hanging caller app
    This CL adds showing the error view when no match has been found for the sharing extension. This will stop the caller app from hanging when no match has been found.
    
    Bug: 1469386
    
    Change-Id: Ibeca33870574386d8132849f5f62e8b2b26aebba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4761224
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1183140}

--- ios/chrome/share_extension/share_view_controller.mm
+++ ios/chrome/share_extension/share_view_controller.mm
@@ -218,9 +218,12 @@ const CGFloat kMediumAlpha = 0.5;
 
 - (void)loadElementsFromContext {
   NSString* typeURL = UTTypeURL.identifier;
+  // TODO(crbug.com/1472758): Reorganize sharing extension handler.
+  BOOL foundMatch = false;
   for (NSExtensionItem* item in self.extensionContext.inputItems) {
     for (NSItemProvider* itemProvider in item.attachments) {
       if ([itemProvider hasItemConformingToTypeIdentifier:typeURL]) {
+        foundMatch = true;
         ItemBlock URLCompletion = ^(id idURL, NSError* error) {
           NSURL* URL = base::mac::ObjCCast<NSURL>(idURL);
           if (!URL) {
@@ -261,6 +264,11 @@ const CGFloat kMediumAlpha = 0.5;
                                 completionHandler:imageCompletion];
       }
     }
+  }
+
+  // Display the error view when no match have been found.
+  if (!foundMatch) {
+    [self displayErrorView];
   }
 }
 
