272a79190fcb95e54f96e779559356d17e11e039
Jun W <jungoog@google.com>
2023-08-14 06:24:07 +0000

    Add stub Diagnostics API to `chromeos`
    This CL adds the `chromeos.diagnostics` Blink extension and creates a
    corresponding browser test to verify its existence.
    
    Bug: b/294775364
    Change-Id: Icbd766617d304a4a6118e9374e25c89373b96505
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4761504
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Junguo (Jun) W. <jungoog@google.com>
    Cr-Commit-Position: refs/heads/main@{#1182983}

--- third_party/blink/renderer/extensions/chromeos/chromeos.cc
+++ third_party/blink/renderer/extensions/chromeos/chromeos.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/extensions/chromeos/chromeos.h"
 
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
+#include "third_party/blink/renderer/extensions/chromeos/diagnostics/cros_diagnostics.h"
 #include "third_party/blink/renderer/extensions/chromeos/system_extensions/hid/cros_hid.h"
 #include "third_party/blink/renderer/extensions/chromeos/system_extensions/managed_device_health_services/telemetry/cros_telemetry.h"
 #include "third_party/blink/renderer/extensions/chromeos/system_extensions/window_management/cros_window_management.h"
@@ -26,6 +27,10 @@ CrosHID* ChromeOS::hid(ExecutionContext* execution_context) {
 
 CrosTelemetry* ChromeOS::telemetry(ExecutionContext* execution_context) {
   return &CrosTelemetry::From(*execution_context);
+}
+
+CrosDiagnostics* ChromeOS::diagnostics(ExecutionContext* execution_context) {
+  return &CrosDiagnostics::From(*execution_context);
 }
 
 void ChromeOS::Trace(Visitor* visitor) const {
