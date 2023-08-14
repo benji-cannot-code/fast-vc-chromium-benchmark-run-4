20aed48e11df3426bc7684c53ef4158060d23e5d
David Pennington <dpenning@chromium.org>
2023-08-14 22:27:41 +0000

    [TabOrganization] Log up to the 3rd std deviation of tab groups in UKM.
    Bug: 1469128
    Change-Id: Iec081bd0f8c1b3211c8ae69a824f6a7a06120c98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4779450
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183370}

--- chrome/browser/ui/tabs/organization/metrics.cc
+++ chrome/browser/ui/tabs/organization/metrics.cc
@@ -19,8 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-int kMaxLoggedTabs = 30;
-
+// Max number of tabs within 3 std deviations of mean.
+int kMaxLoggedTabs = 115;
 }
 
 void LogTabStripOrganizationUKM(
