50b21163f1b69c7c03046868909a0880b3657dc5
Stephen McGruer <smcgruer@chromium.org>
2023-08-14 19:07:29 +0000

    Re-enable PaymentRequestNoShippingTest tests
    This test fixes and re-enables a number of PaymentRequestNoShippingTest
    tests, as follows:
    
    - testCloseDialog: migrated from basic-card to payment apps
    - testEditAndCloseDialog: migrated from basic-card to payment apps
    - testEditAndCancelDialog: migrated from basic-card to payment apps
    - testPay: deleted, as there was nothing to test after basic-card was
      removed
    - testCancelUnmaskAndRetry: deleted, as there was nothing to test after
      basic-card was removed
    - testPaymentRequestEventsMetric: migrated from basic-card to payment
      apps
    
    Also migrated tests away from expectResultContains to wait on JavaScript
    promises instead.
    
    Bug: 1331715, 1331515, 1331497, 1331494, 1331473
    Change-Id: Ia35a65fe007bd01c0db471a2cb46a684a429a24f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4774541
    Auto-Submit: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183238}

--- components/test/data/payments/no_shipping.js
+++ components/test/data/payments/no_shipping.js
@@ -7,18 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * Launches the PaymentRequest UI that does not require a shipping address.
- */
-function buy() {
-  buyWithMethods([
-    {
-      supportedMethods: 'basic-card',
-      data: {supportedNetworks: ['visa', 'mastercard']},
-    },
-  ]);
-}
-
-/**
- * Launches the PaymentRequest UI that does not require a shipping address.
  * @param {String} methodData - An array of payment method objects.
  * @return {string} - The error message, if any.
  */
