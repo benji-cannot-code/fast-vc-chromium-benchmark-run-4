baaa73b3023a5f05b57c5ca66897b89ebaa67f31
Charles William Dick <cwd@google.com>
2023-08-14 08:34:49 +0000

    Hide Battery Saver toolbar icon on cros
    Hide the Battery Saver toolbar icon when ChromeOS Battery Saver is
    available. The icon only controls a subset of ChromeOS's battery Saver
    features, so we should hide it to avoid confusing users. Battery Saver
    status will be shown on the battery icon in the shelf on ChromeOS
    instead.
    
    Bug: b:294167101
    Test: Manually check both lacros and ash chrome don't have the icon
    Change-Id: I423b181dd568e781230c0ed4d11676630ce86238
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4764651
    Commit-Queue: Charles Dick <cwd@google.com>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183006}

--- chromeos/startup/browser_params_proxy.h
+++ chromeos/startup/browser_params_proxy.h
@@ -147,6 +147,8 @@ class COMPONENT_EXPORT(CHROMEOS_STARTUP) BrowserParamsProxy {
 
   bool IsSysUiDownloadsIntegrationV2Enabled() const;
 
+  bool IsCrosBatterySaverAvailable() const;
+
  private:
   friend base::NoDestructor<BrowserParamsProxy>;
 
