4224f1f71b7b4511f9fc7b5f8e60bf2cb0f2b549
Aishwarya Rajesh <aishwaryarj@google.com>
2023-08-14 19:38:33 +0000

    [APS] Rename/refactor gesture event filter classes
    The gesture event filter classes were repurposed in crrev.com/c/4699042
    to support detecting and handling hover events that are not generally
    categorized as gesture events. This CL renames the following classes to
    reflect this:
    
    * GestureEventFilter.java
    * AreaGestureEventFilter.java
    * AreaGestureEventFilterUnitTest.java
    * GestureHandler.java
    
    Bug: 1451925
    Change-Id: I92cf6c97953bdc2e8cacbfe29649afab50ca162e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4778083
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1183258}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/eventfilter/MotionEventHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/eventfilter/MotionEventHandler.java
@@ -6,10 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.compositor.layouts.eventfilter;
 
 /**
- * Interface that describes gesture callbacks.
+ * Interface that describes motion event callbacks.
  */
-public interface GestureHandler {
-
+public interface MotionEventHandler {
     /**
      * Called on down touch event.
      *
@@ -21,8 +20,8 @@ public interface GestureHandler {
     void onDown(float x, float y, boolean fromMouse, int buttons);
 
     /**
-    * Called on up or cancel touch event.
-    */
+     * Called on up or cancel touch event.
+     */
     void onUpOrCancel();
 
     /**
