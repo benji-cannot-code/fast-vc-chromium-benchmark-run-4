70d031b9520b9add52e9cc904fea47835e776108
Theodore Olsauskas-Warren <sauski@google.com>
2023-08-14 15:16:50 +0000

    Cookie Controls: Report medium confidence on stateful bounces
    When there have been any stateful bounces, indicating that DIPS has been
    involved in potentially clearing data, report a medium confidence
    signal for the new Cookie Controls. This ensures that the entry point
    is available.
    
    This does not change the number of sites blocked, as it's not clear how
    we should best represent DIPS interventions in that count - but at least
    the "site not working?" entry point is available.
    
    Bug: 1472685
    Change-Id: Ie99a8a393ca6c428da050c56e9253681fdc47075
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4774204
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183111}

--- components/content_settings/browser/ui/cookie_controls_controller.cc
+++ components/content_settings/browser/ui/cookie_controls_controller.cc
@@ -140,14 +140,16 @@ void CookieControlsController::Update(content::WebContents* web_contents) {
   if (base::FeatureList::IsEnabled(content_settings::features::kUserBypassUI)) {
     int third_party_allowed_sites = GetAllowedThirdPartyCookiesSitesCount();
     int third_party_blocked_sites = GetBlockedThirdPartyCookiesSitesCount();
+    int bounce_count = GetStatefulBounceCount();
 
     for (auto& observer : observers_) {
       observer.OnStatusChanged(status.status, status.enforcement,
                                status.expiration);
       observer.OnSitesCountChanged(third_party_allowed_sites,
                                    third_party_blocked_sites);
-      observer.OnBreakageConfidenceLevelChanged(GetConfidenceLevel(
-          status.status, third_party_allowed_sites, third_party_blocked_sites));
+      observer.OnBreakageConfidenceLevelChanged(
+          GetConfidenceLevel(status.status, third_party_allowed_sites,
+                             third_party_blocked_sites, bounce_count));
     }
   } else {
     int allowed_cookies = GetAllowedCookieCount();
@@ -216,7 +218,8 @@ CookieControlsController::Status CookieControlsController::GetStatus(
 CookieControlsBreakageConfidenceLevel
 CookieControlsController::GetConfidenceLevel(CookieControlsStatus status,
                                              int allowed_sites,
-                                             int blocked_sites) {
+                                             int blocked_sites,
+                                             int bounce_count) {
   // If 3PC cookies are not blocked by default:
   switch (status) {
     case CookieControlsStatus::kDisabled:
@@ -229,11 +232,12 @@ CookieControlsController::GetConfidenceLevel(CookieControlsStatus status,
       break;
   }
 
-  // If no 3P sites have attempted to access site data:
-  // (taking into account both allow and blocked counts, since the breakage
-  // might be related to storage partitioning. Partitioned site will be allowed
-  // to access partitioned storage)
-  if (allowed_sites + blocked_sites == 0) {
+  // If no 3P sites have attempted to access site data, nor were any stateful
+  // bounces recorded, return low confidence. Take into account both allow and
+  // blocked counts, since the breakage might be related to storage
+  // partitioning. Partitioned site will be allowed to access partitioned
+  // storage.
+  if (allowed_sites + blocked_sites + bounce_count == 0) {
     return CookieControlsBreakageConfidenceLevel::kLow;
   }
 
@@ -337,7 +341,9 @@ CookieControlsController::GetBreakageConfidenceLevel() {
     auto status = GetStatus(GetWebContents());
     int allowed_sites = GetAllowedSitesCount();
     int blocked_sites = GetBlockedSitesCount();
-    return GetConfidenceLevel(status.status, allowed_sites, blocked_sites);
+    int bounce_count = GetStatefulBounceCount();
+    return GetConfidenceLevel(status.status, allowed_sites, blocked_sites,
+                              bounce_count);
   } else {
     return CookieControlsBreakageConfidenceLevel::kUninitialized;
   }
@@ -436,12 +442,14 @@ void CookieControlsController::PresentBlockedCookieCounter() {
     auto status = GetStatus(GetWebContents());
     int third_party_allowed_sites = GetAllowedThirdPartyCookiesSitesCount();
     int third_party_blocked_sites = GetBlockedThirdPartyCookiesSitesCount();
+    int bounce_count = GetStatefulBounceCount();
 
     for (auto& observer : observers_) {
       observer.OnSitesCountChanged(third_party_allowed_sites,
                                    third_party_blocked_sites);
-      observer.OnBreakageConfidenceLevelChanged(GetConfidenceLevel(
-          status.status, third_party_allowed_sites, third_party_blocked_sites));
+      observer.OnBreakageConfidenceLevelChanged(
+          GetConfidenceLevel(status.status, third_party_allowed_sites,
+                             third_party_blocked_sites, bounce_count));
     }
   } else {
     int allowed_cookies = GetAllowedCookieCount();
@@ -494,10 +502,11 @@ void CookieControlsController::OnPageReloadDetected(int recent_reloads_count) {
 
   int allowed_sites = GetAllowedSitesCount();
   int blocked_sites = GetBlockedSitesCount();
+  int bounce_count = GetStatefulBounceCount();
 
   for (auto& observer : observers_) {
-    observer.OnBreakageConfidenceLevelChanged(
-        GetConfidenceLevel(status.status, allowed_sites, blocked_sites));
+    observer.OnBreakageConfidenceLevelChanged(GetConfidenceLevel(
+        status.status, allowed_sites, blocked_sites, bounce_count));
   }
 }
 
