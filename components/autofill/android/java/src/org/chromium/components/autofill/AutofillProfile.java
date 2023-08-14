8044ab92ef9534a21a53a5a4bd0492a383770e63
Timofey Chudakov <tchudakov@google.com>
2023-08-14 02:54:17 +0000

    [Android][I18n] Remove field resetting logic.
    Settings invisible fields will be reset by AutofillProfile function.
    Platform code should know nothing about this logic.
    
    Bug: 1441904
    Change-Id: Ieb04f8859b509fb5b59ee0c9b3d5d7fa910a00c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4767817
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1182935}

--- components/autofill/android/java/src/org/chromium/components/autofill/AutofillProfile.java
+++ components/autofill/android/java/src/org/chromium/components/autofill/AutofillProfile.java
@@ -13,7 +13,6 @@ import org.chromium.base.annotations.JNINamespace;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * Autofill address information.
@@ -315,10 +314,6 @@ public class AutofillProfile {
 
     public String getLabel() {
         return mLabel;
-    }
-
-    public void removeAllDataExceptGivenFields(Set<Integer> allowedFields) {
-        mFields.keySet().removeIf(key -> !allowedFields.contains(key));
     }
 
     @CalledByNative
