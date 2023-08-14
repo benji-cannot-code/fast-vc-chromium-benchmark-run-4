f0b397beddc4dd07afe0233750cc58ab758f4f94
Sammie Quon <sammiequon@chromium.org>
2023-08-14 22:33:29 +0000

    desks: Remove extra blur background from saved desk buttons
    It has blurred background from overriding pill button now, so its not
    necessary to double blur. Also add blur quality to blurred background
    for when skia renderer supports it.
    
    Test: manual
    Bug: b/278534054
    Change-Id: I159a2a309f834fdc5eae5e979e294a113e4e976e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4777016
    Reviewed-by: Xiaodan Zhu <zxdan@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183373}

--- ash/style/blurred_background_shield.cc
+++ ash/style/blurred_background_shield.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/style/blurred_background_shield.h"
 
+#include "ash/public/cpp/style/color_provider.h"
 #include "ui/color/color_provider.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/views/view.h"
@@ -106,6 +107,8 @@ void BlurredBackgroundShield::UpdateBackgroundColor() {
   background_layer_.SetColor(background_color);
   if (SkColorGetA(background_color) != SK_AlphaOPAQUE && blur_sigma_) {
     background_layer_.SetBackgroundBlur(blur_sigma_);
+    background_layer_.SetBackdropFilterQuality(
+        ColorProvider::kBackgroundBlurQuality);
   } else {
     background_layer_.SetBackgroundBlur(0.0f);
   }
--- ash/wm/desks/templates/saved_desk_save_desk_button.cc
+++ ash/wm/desks/templates/saved_desk_save_desk_button.cc
@@ -5,13 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wm/desks/templates/saved_desk_save_desk_button.h"
 
-#include "ash/public/cpp/style/color_provider.h"
 #include "ash/style/style_util.h"
 #include "ash/wm/desks/templates/saved_desk_constants.h"
 #include "ash/wm/overview/overview_utils.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
-#include "ui/compositor/layer.h"
 #include "ui/gfx/vector_icon_types.h"
 #include "ui/views/controls/focus_ring.h"
 #include "ui/views/highlight_border.h"
@@ -47,15 +45,6 @@ SavedDeskSaveDeskButton::SavedDeskSaveDeskButton(
           : views::HighlightBorder::Type::kHighlightBorder2));
 
   SetEnableBackgroundBlur(true);
-
-  View* background_view = AddChildView(std::make_unique<views::View>());
-  background_view->SetPaintToLayer();
-
-  background_view->layer()->SetRoundedCornerRadius(
-      gfx::RoundedCornersF{kSaveDeskCornerRadius});
-  background_view->layer()->SetBackgroundBlur(
-      ColorProvider::kBackgroundBlurSigma);
-  background_view->layer()->SetFillsBoundsOpaquely(false);
 }
 
 SavedDeskSaveDeskButton::~SavedDeskSaveDeskButton() = default;
