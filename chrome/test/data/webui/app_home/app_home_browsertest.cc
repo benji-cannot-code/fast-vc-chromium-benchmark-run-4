05675fae862e6412d921a412389764c6a97abecf
dpapad <dpapad@chromium.org>
2023-08-14 18:42:51 +0000

    WebUI: Migrate c/b/r/app_home/ tests to WebUIMochaBrowserTest.
    WebUIMochaBrowserTest is the new way of running WebUI tests without any
    dependency to the obsolete js2gtest and test_api.js mechanism.
    
    Bug: 1457360
    Change-Id: Ifaa18022d833f2a175982a08eda9ca110ded2271
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4778920
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183216}

---
+++ chrome/test/data/webui/app_home/app_home_browsertest.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/test/scoped_feature_list.h"
+#include "chrome/browser/ui/ui_features.h"
+#include "chrome/common/webui_url_constants.h"
+#include "chrome/test/base/web_ui_mocha_browser_test.h"
+#include "content/public/test/browser_test.h"
+
+class AppHomeTest : public WebUIMochaBrowserTest {
+ protected:
+  AppHomeTest() { set_test_loader_host(chrome::kChromeUIAppLauncherPageHost); }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_{
+      features::kDesktopPWAsAppHomePage};
+};
+
+IN_PROC_BROWSER_TEST_F(AppHomeTest, AppList) {
+  RunTest("app_home/app_list_test.js", "mocha.run()");
+}
--- chrome/test/data/webui/app_home/app_list_test.ts
+++ chrome/test/data/webui/app_home/app_list_test.ts
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'chrome://webui-test/mojo_webui_test_support.js';
 import 'chrome://apps/app_list.js';
 import 'chrome://apps/app_item.js';
 import 'chrome://apps/deprecated_apps_link.js';
