77b7f22cfc9f1a7f27b135bf280e35164c7112c2
David Pennington <dpenning@chromium.org>
2023-08-14 16:31:47 +0000

    [TabOrganization] Adding UKM Metrics for collecting sourceID groups
    collection review doc: https://docs.google.com/document/d/1qjvBDM0-ZI4C8RfeS6NjSnFKHjKNgy64_6y6N2_-OPU/edit
    
    Bug: 1469128
    Change-Id: I229639dff2c5e987b573151919f2e3ad3d0249d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4734972
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183129}

--- chrome/browser/ui/views/tabs/tab_drag_controller.cc
+++ chrome/browser/ui/views/tabs/tab_drag_controller.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/layout_constants.h"
 #include "chrome/browser/ui/sad_tab_helper.h"
+#include "chrome/browser/ui/tabs/organization/metrics.h"
 #include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_keyed_service.h"
 #include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_service_factory.h"
 #include "chrome/browser/ui/tabs/tab_group.h"
@@ -2129,6 +2130,12 @@ void TabDragController::CompleteDrag() {
       selection.set_anchor(static_cast<size_t>(index));
       model->SetSelectionFromModel(selection);
     }
+  }
+
+  if (source_context_ == attached_context_) {
+    LogTabStripOrganizationUKM(
+        attached_context_->GetTabStripModel(),
+        SuggestedTabStripOrganizationReason::DRAGGED_WITHIN_SAME_TABSTRIP);
   }
 }
 
