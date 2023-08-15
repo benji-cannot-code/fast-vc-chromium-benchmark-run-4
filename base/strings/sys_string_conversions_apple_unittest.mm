07c08724cbba0a219b815ac05e6836249b71e17d
Avi Drissman <avi@chromium.org>
2023-08-15 16:47:23 +0000

    Rename sys_string_conversions_mac to "apple" because iOS uses it too
    Bug: 1444927
    No-Presubmit: true
    Change-Id: I27093e25897ab945733afc5e5bccf6824f81e74b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4775753
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183661}

--- base/strings/sys_string_conversions_apple_unittest.mm
+++ base/strings/sys_string_conversions_apple_unittest.mm
@@ -3,11 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/strings/sys_string_conversions.h"
+
 #import <Foundation/Foundation.h>
 
 #include <string>
 
-#include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
