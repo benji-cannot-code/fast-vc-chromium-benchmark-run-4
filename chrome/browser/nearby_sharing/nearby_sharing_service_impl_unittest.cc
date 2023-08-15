c4bbd78ff6f1aefd3cdacd7be6cfc538d4edabe1
Thomas Guilbert <tguilbert@chromium.org>
2023-08-15 21:25:08 +0000

    Revert "[Nearby][Self Share] Your Devices Visibility On Screen Lock"
    This reverts commit 57e0f3045325ac30bfb92d5eed5177d36214ade7.
    
    Reason for revert: MSAN build failure, see crbug.com/1473145
    
    Original change's description:
    > [Nearby][Self Share] Your Devices Visibility On Screen Lock
    >
    > When Self Share is enabled, the Chromebook switches to Your Devices
    > visibility on screen lock and reverts back to its previous visibility
    > when the screen is unlocked again.
    >
    > Test: utest in this and manual test with DUT
    > Change-Id: If93129076c750029b870cde16a3089d37d0d5360
    > Fixed: b/279939480
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4779249
    > Reviewed-by: Daniel Classon <dclasson@google.com>
    > Commit-Queue: Brando Socarras <brandosocarras@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1183680}
    
    Change-Id: I08193187371c7299c3401ad51533d19578f911c0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4781623
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Owners-Override: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1183822}

--- chrome/browser/nearby_sharing/nearby_sharing_service_impl_unittest.cc
+++ chrome/browser/nearby_sharing/nearby_sharing_service_impl_unittest.cc
@@ -642,11 +642,6 @@ class NearbySharingServiceImplTestBase : public testing::Test {
     base::RunLoop().RunUntilIdle();
   }
 
-  nearby_share::mojom::Visibility GetVisibility() {
-    NearbyShareSettings settings(&prefs_, local_device_data_manager());
-    return settings.GetVisibility();
-  }
-
   void SetIsEnabled(bool is_enabled) {
     NearbyShareSettings settings(&prefs_, local_device_data_manager());
     if (is_enabled) {
@@ -2329,7 +2324,7 @@ TEST_P(NearbySharingServiceImplTest,
   NearbySharingService::StatusCodes result = service_->RegisterReceiveSurface(
       &callback, NearbySharingService::ReceiveSurfaceState::kForeground);
   EXPECT_EQ(result, NearbySharingService::StatusCodes::kOk);
-  if (features::IsSelfShareEnabled()) {
+  if (base::FeatureList::IsEnabled(features::kNearbySharingSelfShare)) {
     EXPECT_TRUE(fake_nearby_connections_manager_->IsAdvertising());
   } else {
     EXPECT_FALSE(fake_nearby_connections_manager_->IsAdvertising());
@@ -2347,7 +2342,7 @@ TEST_P(NearbySharingServiceImplTest, ScreenLocksDuringAdvertising) {
   EXPECT_FALSE(fake_nearby_connections_manager_->is_shutdown());
 
   session_controller_->SetScreenLocked(true);
-  if (features::IsSelfShareEnabled()) {
+  if (base::FeatureList::IsEnabled(features::kNearbySharingSelfShare)) {
     EXPECT_TRUE(fake_nearby_connections_manager_->IsAdvertising());
   } else {
     EXPECT_FALSE(fake_nearby_connections_manager_->IsAdvertising());
@@ -5260,7 +5255,7 @@ TEST_P(NearbySharingServiceImplTest, CreateShareTarget) {
   ASSERT_TRUE(share_target.has_value());
   EXPECT_EQ(kDeviceName, share_target->device_name);
   EXPECT_EQ(kDeviceType, share_target->type);
-  if (features::IsSelfShareEnabled()) {
+  if (base::FeatureList::IsEnabled(features::kNearbySharingSelfShare)) {
     EXPECT_EQ(certificate_proto.for_self_share(), share_target->for_self_share);
   }
 
@@ -5290,7 +5285,7 @@ TEST_P(NearbySharingServiceImplTest, SelfShareAutoAccept) {
       SetUpIncomingConnection(callback, /*for_self_share=*/true);
 
   // If Self Share is not enabled, we should just time out.
-  if (!features::IsSelfShareEnabled()) {
+  if (!base::FeatureList::IsEnabled(features::kNearbySharingSelfShare)) {
     base::RunLoop run_loop;
     EXPECT_CALL(callback, OnTransferUpdate(testing::_, testing::_))
         .WillOnce(testing::Invoke(
@@ -5334,21 +5329,6 @@ TEST_P(NearbySharingServiceImplTest, SelfShareAutoAccept) {
 
   // To avoid UAF in OnIncomingTransferUpdate().
   service_->UnregisterReceiveSurface(&callback);
-}
-
-TEST_P(NearbySharingServiceImplTest, YourDevicesVisibilityOnScreenLock) {
-  if (features::IsSelfShareEnabled()) {
-    SetIsEnabled(true);
-    SetVisibility(nearby_share::mojom::Visibility::kAllContacts);
-
-    // Lock screen, expect Your Devices visibility.
-    session_controller_->SetScreenLocked(true);
-    EXPECT_EQ(GetVisibility(), nearby_share::mojom::Visibility::kYourDevices);
-
-    // Unlock screen, expect visibility to return to All Contacts.
-    session_controller_->SetScreenLocked(false);
-    EXPECT_EQ(GetVisibility(), nearby_share::mojom::Visibility::kAllContacts);
-  }
 }
 
 INSTANTIATE_TEST_SUITE_P(NearbySharingServiceImplTest,
