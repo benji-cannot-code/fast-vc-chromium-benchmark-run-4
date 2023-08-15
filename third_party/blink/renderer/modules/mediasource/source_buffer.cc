c99afcb246926c9751e36b279a766717bde19557
Jeffrey Kardatzke <jkardatzke@google.com>
2023-08-15 20:10:16 +0000

    media: Add DecoderBufferSideData
    This adds a formalized side data struct to media::DecoderBuffer and
    removes existing usages of the raw array for side data.
    
    There are two existing cases of side data and this CL also adds a
    third new one. The two existing ones are for VP9 spatial layers and
    alpha data. The new one is for a secure handle which is used for
    secure world content decryption on ARM in ChromeOS.
    
    This also removes some TextRenderer support since that is deprecated
    and will be removed. Changes made here break some of the functionality
    so the unit test for it is removed as well.
    
    In order to deal with Lacros compatibility, this does translate
    between the existing raw data format and the new formalized format and
    vice-versa. At M120, the corresponding code for that should be removed.
    
    BUG=b:248610722,b:269383891,1471504
    TEST=CQ
    
    Change-Id: I0f90c3417fcd72bb25738ca045e527fbc1371ab7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4757645
    Reviewed-by: Chris Bookholt <bookholt@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1183777}

--- third_party/blink/renderer/modules/mediasource/source_buffer.cc
+++ third_party/blink/renderer/modules/mediasource/source_buffer.cc
@@ -156,8 +156,7 @@ scoped_refptr<media::StreamParserBuffer> MakeAudioStreamParserBuffer(
       kWebCodecsAudioTrackId);
 
   // Currently, we do not populate any side_data in these converters.
-  DCHECK_EQ(0U, stream_parser_buffer->side_data_size());
-  DCHECK_EQ(nullptr, stream_parser_buffer->side_data());
+  DCHECK(!stream_parser_buffer->has_side_data());
 
   stream_parser_buffer->set_timestamp(audio_chunk.buffer()->timestamp());
   // TODO(crbug.com/1144908): Get EncodedAudioChunk to have an optional duration
@@ -182,8 +181,7 @@ scoped_refptr<media::StreamParserBuffer> MakeVideoStreamParserBuffer(
       kWebCodecsVideoTrackId);
 
   // Currently, we do not populate any side_data in these converters.
-  DCHECK_EQ(0U, stream_parser_buffer->side_data_size());
-  DCHECK_EQ(nullptr, stream_parser_buffer->side_data());
+  DCHECK(!stream_parser_buffer->has_side_data());
 
   stream_parser_buffer->set_timestamp(video_chunk.buffer()->timestamp());
   // TODO(crbug.com/1144908): Get EncodedVideoChunk to have an optional decode
@@ -655,8 +653,7 @@ ScriptPromise SourceBuffer::appendEncodedChunks(
     case V8EncodedChunks::ContentType::kEncodedAudioChunk:
       buffer_queue->emplace_back(
           MakeAudioStreamParserBuffer(*(chunks->GetAsEncodedAudioChunk())));
-      size += buffer_queue->back()->data_size() +
-              buffer_queue->back()->side_data_size();
+      size += buffer_queue->back()->data_size();
       break;
     case V8EncodedChunks::ContentType::kEncodedVideoChunk: {
       const auto& video_chunk = *(chunks->GetAsEncodedVideoChunk());
@@ -668,8 +665,7 @@ ScriptPromise SourceBuffer::appendEncodedChunks(
         return ScriptPromise();
       }
       buffer_queue->emplace_back(MakeVideoStreamParserBuffer(video_chunk));
-      size += buffer_queue->back()->data_size() +
-              buffer_queue->back()->side_data_size();
+      size += buffer_queue->back()->data_size();
       break;
     }
     case V8EncodedChunks::ContentType::
@@ -682,8 +678,7 @@ ScriptPromise SourceBuffer::appendEncodedChunks(
               kEncodedAudioChunk:
             buffer_queue->emplace_back(MakeAudioStreamParserBuffer(
                 *(av_chunk->GetAsEncodedAudioChunk())));
-            size += buffer_queue->back()->data_size() +
-                    buffer_queue->back()->side_data_size();
+            size += buffer_queue->back()->data_size();
             break;
           case V8UnionEncodedAudioChunkOrEncodedVideoChunk::ContentType::
               kEncodedVideoChunk: {
@@ -697,8 +692,7 @@ ScriptPromise SourceBuffer::appendEncodedChunks(
             }
             buffer_queue->emplace_back(
                 MakeVideoStreamParserBuffer(video_chunk));
-            size += buffer_queue->back()->data_size() +
-                    buffer_queue->back()->side_data_size();
+            size += buffer_queue->back()->data_size();
             break;
           }
         }
