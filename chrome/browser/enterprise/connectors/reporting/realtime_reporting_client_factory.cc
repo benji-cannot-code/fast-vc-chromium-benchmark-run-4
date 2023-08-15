965701ba8fbbcd96cb18133052088963414d6c0e
Dominique Fauteux-Chapleau <domfc@chromium.org>
2023-08-15 19:10:47 +0000

    Cleanup RealtimeReportingClientFactory
    Follow-up to crrev.com/c/4770314
    
    Change-Id: If647981e8c0d9d66fb0858a108b72c45880a6736
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4775002
    Reviewed-by: Shanthanu Bhardwaj <xanth@google.com>
    Commit-Queue: Shanthanu Bhardwaj <xanth@google.com>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183743}

--- chrome/browser/enterprise/connectors/reporting/realtime_reporting_client_factory.cc
+++ chrome/browser/enterprise/connectors/reporting/realtime_reporting_client_factory.cc
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/enterprise/connectors/reporting/realtime_reporting_client.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "content/public/browser/browser_context.h"
-#include "extensions/browser/extension_system_provider.h"
-#include "extensions/browser/extensions_browser_client.h"
 
 namespace enterprise_connectors {
 
@@ -36,8 +34,6 @@ RealtimeReportingClientFactory::RealtimeReportingClientFactory()
               .WithGuest(ProfileSelection::kOwnInstance)
               .WithSystem(ProfileSelection::kNone)
               .Build()) {
-  DependsOn(
-      extensions::ExtensionsBrowserClient::Get()->GetExtensionSystemFactory());
   DependsOn(IdentityManagerFactory::GetInstance());
   DependsOn(ConnectorsServiceFactory::GetInstance());
 }
