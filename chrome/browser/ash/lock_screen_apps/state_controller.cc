c991bf1b82b9601da8c2f86089cb094354fd6890
Alexander Alekseev <alemate@chromium.org>
2023-08-15 22:37:44 +0000

    Add ChromeOS trace events in ['/c/b/ash/lock_screen_apps', '/c/b/ash/login/ui']
    Investigating ChromeOS login performance revealed that many
    potentially slow UI thread tasks are not instrumented.
    This Cl adds some events around slow tasks that we saw in
    our tests.
    
    Bug: b:293198304
    
    CC=​achuith@chromium.org
    
    This CL was uploaded by git cl split.
    
    R=tbarzic@chromium.org
    
    Change-Id: I81df7cc9194c8c0f9d034f48f29c869fd62938e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4763167
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Auto-Submit: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183870}

--- chrome/browser/ash/lock_screen_apps/state_controller.cc
+++ chrome/browser/ash/lock_screen_apps/state_controller.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/path_service.h"
 #include "base/time/default_tick_clock.h"
+#include "base/trace_event/trace_event.h"
 #include "chrome/browser/ash/lock_screen_apps/app_manager.h"
 #include "chrome/browser/ash/lock_screen_apps/app_manager_impl.h"
 #include "chrome/browser/ash/lock_screen_apps/first_app_run_toast_manager.h"
@@ -320,6 +321,7 @@ void StateController::CloseLockScreenNote(CloseLockScreenNoteReason reason) {
 }
 
 void StateController::OnSessionStateChanged() {
+  TRACE_EVENT0("ui", "StateController::OnSessionStateChanged");
   if (!session_manager::SessionManager::Get()->IsScreenLocked()) {
     lock_screen_data_->SetSessionLocked(false);
     app_manager_->Stop();
