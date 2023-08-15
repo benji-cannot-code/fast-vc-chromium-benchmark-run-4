3347d42adbd208466734e3fdc490da8417d61c65
Devlin Cronin <rdevlin.cronin@chromium.org>
2023-08-15 21:25:56 +0000

    [Extensions] Add a mojom struct for ServiceWorkerContext
    Add a mojom struct representing a ServiceWorkerContext that includes
    service worker scope URL, service worker version ID, and worker
    thread ID, and use this struct when registering service worker-related
    listeners in the EventRouter.
    
    This reduces the number of parameters to these IPC messages and obviates
    the need for the internal ServiceWorkerIdentifier struct in EventRouter.
    
    This CL has no behavior change.
    
    Bug: 1470045
    Change-Id: Icfd9fa51aaef9343e7ac0f0168505a392102606c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4762976
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183823}

--- extensions/renderer/worker_thread_dispatcher.cc
+++ extensions/renderer/worker_thread_dispatcher.cc
@@ -53,8 +53,9 @@ void AddEventListenerOnIO(const std::string& extension_id,
                           int worker_thread_id) {
   auto* dispatcher = WorkerThreadDispatcher::Get();
   dispatcher->GetEventRouterOnIO()->AddListenerForServiceWorker(
-      extension_id, scope, event_name, service_worker_version_id,
-      worker_thread_id);
+      extension_id, event_name,
+      mojom::ServiceWorkerContext::New(scope, service_worker_version_id,
+                                       worker_thread_id));
 }
 
 // Calls mojom::EventRouter::RemoveListenerForServiceWorker(). It should be
@@ -66,8 +67,9 @@ void RemoveEventListenerOnIO(const std::string& extension_id,
                              int worker_thread_id) {
   auto* dispatcher = WorkerThreadDispatcher::Get();
   dispatcher->GetEventRouterOnIO()->RemoveListenerForServiceWorker(
-      extension_id, scope, event_name, service_worker_version_id,
-      worker_thread_id);
+      extension_id, event_name,
+      mojom::ServiceWorkerContext::New(scope, service_worker_version_id,
+                                       worker_thread_id));
 }
 
 // Calls mojom::EventRouter::AddLazyListenerForServiceWorker(). It should be
@@ -101,8 +103,10 @@ void AddEventFilteredListenerOnIO(const std::string& extension_id,
                                   bool add_lazy_listener) {
   auto* dispatcher = WorkerThreadDispatcher::Get();
   dispatcher->GetEventRouterOnIO()->AddFilteredListenerForServiceWorker(
-      extension_id, scope, event_name, service_worker_version_id,
-      worker_thread_id, std::move(filter), add_lazy_listener);
+      extension_id, event_name,
+      mojom::ServiceWorkerContext::New(scope, service_worker_version_id,
+                                       worker_thread_id),
+      std::move(filter), add_lazy_listener);
 }
 
 // Calls mojom::EventRouter::RemoveFilteredListenerForServiceWorker(). It should
@@ -116,8 +120,10 @@ void RemoveEventFilteredListenerOnIO(const std::string& extension_id,
                                      bool remove_lazy_listener) {
   auto* dispatcher = WorkerThreadDispatcher::Get();
   dispatcher->GetEventRouterOnIO()->RemoveFilteredListenerForServiceWorker(
-      extension_id, scope, event_name, service_worker_version_id,
-      worker_thread_id, std::move(filter), remove_lazy_listener);
+      extension_id, event_name,
+      mojom::ServiceWorkerContext::New(scope, service_worker_version_id,
+                                       worker_thread_id),
+      std::move(filter), remove_lazy_listener);
 }
 
 }  // namespace
