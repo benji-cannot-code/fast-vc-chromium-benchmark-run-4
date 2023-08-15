748cd5c0ae6748a5f5d51c3acd32ebd4fe0d379a
Patrick Noland <pnoland@google.com>
2023-08-15 20:02:47 +0000

    [Omnibox] Make sAvoidRelayoutDuringFocusAnimation default true
    The default of the feature is true, so we should default to true
    pre-native. This might cause issues in rare cases for the disabled
    group, but the enabled group matters more since the disabled group is
    only for holdback purposes.
    
    Bug: 1468910, 1467309
    Change-Id: Ic3b4a0a64f7d3e3558176b7b4540681f84bf86e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4781025
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1183770}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/OmniboxFeatures.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/OmniboxFeatures.java
@@ -82,7 +82,7 @@ public class OmniboxFeatures {
 
     private static final MutableFlagWithSafeDefault sAvoidRelayoutDuringFocusAnimation =
             new MutableFlagWithSafeDefault(
-                    ChromeFeatureList.AVOID_RELAYOUT_DURING_FOCUS_ANIMATION, false);
+                    ChromeFeatureList.AVOID_RELAYOUT_DURING_FOCUS_ANIMATION, true);
 
     private static final MutableFlagWithSafeDefault sShortCircuitUnfocusAnimation =
             new MutableFlagWithSafeDefault(
