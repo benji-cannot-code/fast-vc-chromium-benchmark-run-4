81b28cdb75ed8683713a056aae83c2cc18b7d763
Rahul Yadav <rahul.yadav@samsung.com>
2023-08-15 19:56:38 +0000

    Moving common method LatencyToString to one place.
    Bug: None
    Change-Id: I65743ee0c2706b586afef0d03964904553796b48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4777742
    Commit-Queue: Rahul yadav <rahul.yadav@samsung.com>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183768}

--- media/audio/audio_device_stats_reporter.cc
+++ media/audio/audio_device_stats_reporter.cc
@@ -16,25 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 
 namespace media {
-namespace {
-
-const char* LatencyToString(AudioLatency::Type latency) {
-  switch (latency) {
-    case AudioLatency::Type::kExactMS:
-      return "LatencyExactMs";
-    case AudioLatency::Type::kInteractive:
-      return "LatencyInteractive";
-    case AudioLatency::Type::kRtc:
-      return "LatencyRtc";
-    case AudioLatency::Type::kPlayback:
-      return "LatencyPlayback";
-    case AudioLatency::Type::kUnknown:
-      return "LatencyUnknown";
-  }
-}
-
-}  // namespace
-
 AudioDeviceStatsReporter::AudioDeviceStatsReporter(
     const AudioParameters& params,
     Type type)
@@ -145,9 +126,9 @@ AudioDeviceStatsReporter::CreateAggregateCallback(
   }
 
   std::string short_with_latency_name(
-      base::StrCat({short_name, ".", LatencyToString(latency)}));
+      base::StrCat({short_name, ".", AudioLatency::ToString(latency)}));
   std::string intervals_with_latency_name(
-      base::StrCat({intervals_name, ".", LatencyToString(latency)}));
+      base::StrCat({intervals_name, ".", AudioLatency::ToString(latency)}));
 
   return base::BindRepeating(
       [](int max_value, size_t bucket_count, const std::string& short_name,
@@ -188,7 +169,7 @@ AudioDeviceStatsReporter::CreateRealtimeCallback(
                              : "Media.AudioInputDevice.AudioService",
        stat_name}));
   std::string base_with_latency_name(
-      base::StrCat({base_name, ".", LatencyToString(latency)}));
+      base::StrCat({base_name, ".", AudioLatency::ToString(latency)}));
 
   // Since this callback will be called on every call to ReportCallback(), we
   // pre-fetch the histograms for efficiency, like the histogram macros do. Note
