c658acf30abe0071139663ca2c3be4543593f601
Chuong Ho <hdchuong@google.com>
2023-08-15 01:28:43 +0000

    orca: add consent store
    Add the skeleton for reading/writing orca consent status from profile
    prefs.
    
    Note: My apologies for asking to review a duplicated CL.
    This CL is a duplication of crrev/c/4755110 (up to patchset 11). I
    made a mistake when rebasing and re-committing crrev/c/4755110, and the CL now became polluted with so many other unrelated changes (and losing +1 as well).
    
    Bug: b/294916716
    Change-Id: Iecf48a8449d04358e9dc33c39dee25018ceb977b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4770597
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: Chuong Ho <hdchuong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1183457}

---
+++ chrome/browser/ash/input_method/editor_consent_store_unittest.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/input_method/editor_consent_store.h"
+
+#include "ash/constants/ash_pref_names.h"
+#include "chrome/test/base/testing_profile.h"
+#include "content/public/test/browser_task_environment.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace ash::input_method {
+namespace {
+
+class EditorConsentStoreTest : public ::testing::Test {
+ public:
+  EditorConsentStoreTest() = default;
+  ~EditorConsentStoreTest() override = default;
+
+ private:
+  content::BrowserTaskEnvironment task_environment_;
+};
+
+TEST_F(EditorConsentStoreTest, SettingConsentStatusShouldUpdateUserPrefs) {
+  TestingProfile profile_;
+  EditorConsentStore store(profile_.GetPrefs());
+
+  store.SetConsentStatus(ConsentStatus::kApproved);
+
+  EXPECT_EQ(store.GetConsentStatus(), ConsentStatus::kApproved);
+}
+
+}  // namespace
+}  // namespace ash::input_method
