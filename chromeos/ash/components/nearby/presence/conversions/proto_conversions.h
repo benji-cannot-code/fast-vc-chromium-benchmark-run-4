e2b1cde6c9ff54b63ca0c1a2d0be5688258fa1cb
Mark Polner <polner@google.com>
2023-08-15 21:27:04 +0000

    [Nearby] Create new build target for Credential mojo/proto conversion.
    To avoid circular dependency issues, this CL moves the
    //chromeos/.../presence/credentials/proto_conversions file
    target to its own build target. It also updates any
    downstream files that already depend on this target.
    
    Bug: b:287334195
    Change-Id: I59e79dab7b9236d6be2e42c9d1b91cd9dfd7bdf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4762045
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Mark Polner <polner@google.com>
    Cr-Commit-Position: refs/heads/main@{#1183825}

--- chromeos/ash/components/nearby/presence/conversions/proto_conversions.h
+++ chromeos/ash/components/nearby/presence/conversions/proto_conversions.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROMEOS_ASH_COMPONENTS_NEARBY_PRESENCE_CREDENTIALS_PROTO_CONVERSIONS_H_
-#define CHROMEOS_ASH_COMPONENTS_NEARBY_PRESENCE_CREDENTIALS_PROTO_CONVERSIONS_H_
+#ifndef CHROMEOS_ASH_COMPONENTS_NEARBY_PRESENCE_CONVERSIONS_PROTO_CONVERSIONS_H_
+#define CHROMEOS_ASH_COMPONENTS_NEARBY_PRESENCE_CONVERSIONS_PROTO_CONVERSIONS_H_
 
 #include "chromeos/ash/components/nearby/common/proto/timestamp.pb.h"
 #include "chromeos/ash/components/nearby/presence/proto/update_device_rpc.pb.h"
@@ -60,4 +60,4 @@ int64_t SecondsToMilliseconds(int64_t seconds);
 
 }  // namespace ash::nearby::presence::proto
 
-#endif  // CHROMEOS_ASH_COMPONENTS_NEARBY_PRESENCE_CREDENTIALS_PROTO_CONVERSIONS_H_
+#endif  // CHROMEOS_ASH_COMPONENTS_NEARBY_PRESENCE_CONVERSIONS_PROTO_CONVERSIONS_H_
