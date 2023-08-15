0aa6ec56934d7d551109a0aefbf9fa4c4c4fd4e5
Wes Okuhara <wesokuhara@google.com>
2023-08-15 20:48:28 +0000

    CrOS Settings: Fix night light slider legend labels
    Fixes the overlapping legend labels for the night light slider by
    using the attribute binding Polymer syntax "$=" for binding the style
    attribute.
    
    Before: http://screen/6dFCQ3QKrKKGYPi.png
    After: http://screen/KJWxaiXsjbQhGQG.png
    
    Bug: b/295420051
    Change-Id: I56498c56de88269b78dd8097ef7ddec4e9975dae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4781196
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Jerry Liu <pzliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1183795}

--- chrome/browser/resources/ash/settings/settings_scheduler_slider/settings_scheduler_slider.ts
+++ chrome/browser/resources/ash/settings/settings_scheduler_slider/settings_scheduler_slider.ts
@@ -275,8 +275,8 @@ export class SettingsSchedulerSliderElement extends
    * @return The CSS style of the legend div.
    */
   private getLegendStyle_(percent: number, isRTL: boolean): string {
-    percent = isRTL ? 100 - percent : percent;
-    return 'left: ' + percent + '%';
+    const percentage = isRTL ? 100 - percent : percent;
+    return `left: ${percentage}%`;
   }
 
   /**
