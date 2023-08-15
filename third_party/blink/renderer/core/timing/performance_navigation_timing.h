468211649b21a06ca60ef0c5e44ce376c433c7fd
Domenic Denicola <domenic@chromium.org>
2023-08-15 23:22:34 +0000

    Rename NavigationType IDL enum to NavigationTimingType
    Bug: 1312586
    Change-Id: I1a36e27e04fe74e7ac9a73a7f3fcdb324db6b922
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4780371
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Domenic Denicola <domenic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183893}

--- third_party/blink/renderer/core/timing/performance_navigation_timing.h
+++ third_party/blink/renderer/core/timing/performance_navigation_timing.h
@@ -71,7 +71,7 @@ class CORE_EXPORT PerformanceNavigationTiming final
  private:
   friend class PerformanceNavigationTimingActivationStart;
 
-  static AtomicString GetNavigationType(WebNavigationType);
+  static AtomicString GetNavigationTimingType(WebNavigationType);
 
   const DocumentTiming* GetDocumentTiming() const;
 
