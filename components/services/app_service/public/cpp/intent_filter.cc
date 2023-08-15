7f92b88cdd816a619ea05a07f70fada5698407df
Alan Cutter <alancutter@chromium.org>
2023-08-15 02:31:02 +0000

    [App Service] Add optional port to intent filter authority condition
    This CL updates the kAuthority (previously named kHost) intent filter
    condition type to accept an optional port in its string value.
    This condition accepts either a host by itself or host:port as a string
    for backwards compatibility.
    
    Callsites that add a kAuthority condition with a port must use
    apps_utils::AuthorityView::Encode() to set the condition value.
    
    This change updates web apps to only intent filter their specific
    host:port combo rather than all ports on their host. This is in line
    with the origin security model where different ports are considered
    different isolated origins.
    
    Bug: 1468870
    Change-Id: I3728fca3c7aad21d8708fa7dc2a0b042093965a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4736858
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183479}

--- components/services/app_service/public/cpp/intent_filter.cc
+++ components/services/app_service/public/cpp/intent_filter.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sstream>
 
 #include "components/services/app_service/public/cpp/intent_filter.h"
+
 #include "url/url_constants.h"
 
 namespace apps {
