6b6933afa41bbaa7f6e458c486262962f85e8996
Piotr Pawliczek <pawliczek@google.com>
2023-08-15 22:48:50 +0000

    Change a way the printer is set up when viewing PPD file
    This patch changes the method that is used to set a printer up when the
    user wants to see the printer's PPD file. In the previous version, the
    method ash::printing::SetUpPrinter() is used for setting the printer up.
    Unfortunately, this method was designed to be called from PrintPreview
    only. This patch solves this problem by calling SetUpPrinter() directly
    from CupsPrintersManager.
    
    BUG=b:253246486
    TEST=unit_tests
    
    Change-Id: I5a0b48a094f532f03f1247fd20a160c2b50585e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4767996
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Piotr Pawliczek <pawliczek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183875}

--- chrome/browser/ui/webui/settings/ash/cups_printers_handler.cc
+++ chrome/browser/ui/webui/settings/ash/cups_printers_handler.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/printing/printer_event_tracker.h"
 #include "chrome/browser/ash/printing/printer_event_tracker_factory.h"
 #include "chrome/browser/ash/printing/printer_info.h"
-#include "chrome/browser/ash/printing/printer_setup_util.h"
 #include "chrome/browser/ash/printing/server_printers_fetcher.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/download/download_prefs.h"
@@ -459,18 +458,24 @@ void CupsPrintersHandler::HandleRetrieveCupsPrinterPpd(
     return;
   }
 
-  ash::printing::SetUpPrinter(
-      printers_manager_, *printer,
+  printers_manager_->SetUpPrinter(
+      *printer, /*is_automatic_installation=*/true,
       base::BindOnce(&CupsPrintersHandler::OnSetUpPrinter,
                      weak_factory_.GetWeakPtr(), printer_id, printer_name,
                      eula));
 }
 
-void CupsPrintersHandler::OnSetUpPrinter(
-    const std::string& printer_id,
-    const std::string& printer_name,
-    const std::string& eula,
-    const absl::optional<::printing::PrinterSemanticCapsAndDefaults>& caps) {
+void CupsPrintersHandler::OnSetUpPrinter(const std::string& printer_id,
+                                         const std::string& printer_name,
+                                         const std::string& eula,
+                                         PrinterSetupResult result) {
+  if (result != PrinterSetupResult::kSuccess) {
+    PRINTER_LOG(ERROR) << "Cannot setup a printer " << printer_id << " ("
+                       << printer_name << ")";
+    OnRetrievePpdError(printer_name);
+    return;
+  }
+
   // Once the printer has been setup we can request the PPD.
   const std::vector<uint8_t> empty_ppd;
 
