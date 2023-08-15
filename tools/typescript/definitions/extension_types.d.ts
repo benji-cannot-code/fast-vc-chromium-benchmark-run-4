ae5591fa78ca79ba316c677503961e3443acc16a
Courtney Wong <courtneywong@chromium.org>
2023-08-15 18:31:45 +0000

    [TS Migration] Create d.ts files for APIs used by Families team
    The d.ts files were generated using the script in crrev.com/c/4679213,
    and manual adjustments were made to include objects used by Families
    code that are available in the corresponding externs JS files, but have
    diverged from the JSON schemas.
    
    Tested: Checked that externs dependencies compile in WIP TS migrations:
    crrev.com/c/4754622
    
    Bug: b:295569841
    Change-Id: Ie31f54a8616ccdf13ddf542410924f2ea5601bcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4775157
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Reviewed-by: Zork <zork@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183728}

---
+++ tools/typescript/definitions/extension_types.d.ts
@@ -0,0 +1,51 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Definitions for chrome.extensionTypes API
+ * Generated from: extensions/common/api/extension_types.json
+ * run `tools/json_schema_compiler/compiler.py
+ * extensions/common/api/extension_types.json -g ts_definitions` to regenerate.
+ */
+
+declare namespace chrome {
+  export namespace extensionTypes {
+
+    export enum ImageFormat {
+      JPEG = 'jpeg',
+      PNG = 'png',
+    }
+
+    export interface ImageDetails {
+      format?: ImageFormat;
+      quality?: number;
+    }
+
+    export enum RunAt {
+      DOCUMENT_START = 'document_start',
+      DOCUMENT_END = 'document_end',
+      DOCUMENT_IDLE = 'document_idle',
+    }
+
+    export enum FrameType {
+      OUTERMOST_FRAME = 'outermost_frame',
+      FENCED_FRAME = 'fenced_frame',
+      SUB_FRAME = 'sub_frame',
+    }
+
+    export enum DocumentLifecycle {
+      PRERENDER = 'prerender',
+      ACTIVE = 'active',
+      CACHED = 'cached',
+      PENDING_DELETION = 'pending_deletion',
+    }
+
+    export enum ExecutionWorld {
+      ISOLATED = 'ISOLATED',
+      MAIN = 'MAIN',
+    }
+
+  }
+}
