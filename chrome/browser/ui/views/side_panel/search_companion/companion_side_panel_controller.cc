2c918c90414e6f6591cf5e2a6cf6f2a879511a18
Duncan Mercer <mercerd@google.com>
2023-08-15 00:34:48 +0000

    [CSC] Give Lens server third party cookie permissions
    Bug: b:295589292
    Change-Id: I776c139fd83bd65ea81674f1f89e328772b8917a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4779403
    Reviewed-by: Ali Stanfield <stanfield@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1183432}

--- chrome/browser/ui/views/side_panel/search_companion/companion_side_panel_controller.cc
+++ chrome/browser/ui/views/side_panel/search_companion/companion_side_panel_controller.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/google/core/common/google_util.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 #include "third_party/blink/public/mojom/frame/user_activation_notification_type.mojom.h"
+#include "ui/webui/webui_allowlist.h"
 
 namespace companion {
 
@@ -57,6 +58,14 @@ void CompanionSidePanelController::CreateAndRegisterEntry() {
           base::Unretained(this)));
   registry->Register(std::move(entry));
   AddObserver();
+
+  // Give Search Companion Server 3P Cookie Permissions
+  auto* webui_allowlist = WebUIAllowlist::GetOrCreate(browser->profile());
+  const url::Origin companion_origin = url::Origin::Create(
+      GURL(chrome::kChromeUIUntrustedCompanionSidePanelURL));
+  webui_allowlist->RegisterAutoGrantedThirdPartyCookies(
+      companion_origin,
+      {ContentSettingsPattern::FromURL(GURL(GetHomepageURLForCompanion()))});
 }
 
 void CompanionSidePanelController::DeregisterEntry() {
