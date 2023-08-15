2aac2593cf9b27bf91cfa20a17b9f929c1b54de1
Domenic Denicola <domenic@chromium.org>
2023-08-15 23:22:06 +0000

    Refactor SpeculationRuleSet::Source constructor to factory functions
    As we are contemplating a third type of source (see issue 1472970),
    continuing to add overloaded constructors is probably not a good idea.
    Refactor to use named factory functions instead.
    
    Bug: 1472970
    Change-Id: I46392841b76b05be1c9b07e8be4fdaedfa484e1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4780038
    Commit-Queue: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183892}

--- third_party/blink/renderer/core/speculation_rules/speculation_rule_set.cc
+++ third_party/blink/renderer/core/speculation_rules/speculation_rule_set.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/speculation_rules/speculation_rules_features.h"
 #include "third_party/blink/renderer/core/speculation_rules/speculation_rules_metrics.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
+#include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
 #include "third_party/blink/renderer/platform/json/json_parser.h"
 #include "third_party/blink/renderer/platform/json/json_values.h"
@@ -430,23 +431,35 @@ SpeculationRule* ParseSpeculationRule(JSONObject* input,
 
 // ---- SpeculationRuleSet::Source implementation ----
 
-SpeculationRuleSet::Source::Source(const String& source_text,
-                                   Document& document,
-                                   DOMNodeId node_id)
+SpeculationRuleSet::Source::Source(base::PassKey<SpeculationRuleSet::Source>,
+                                   const String& source_text,
+                                   Document* document,
+                                   absl::optional<DOMNodeId> node_id,
+                                   absl::optional<KURL> base_url,
+                                   absl::optional<uint64_t> request_id)
     : source_text_(source_text),
       document_(document),
       node_id_(node_id),
-      base_url_(absl::nullopt),
-      request_id_(absl::nullopt) {}
-
-SpeculationRuleSet::Source::Source(const String& source_text,
-                                   const KURL& base_url,
-                                   uint64_t request_id)
-    : source_text_(source_text),
-      document_(nullptr),
-      node_id_(absl::nullopt),
       base_url_(base_url),
       request_id_(request_id) {}
+
+SpeculationRuleSet::Source* SpeculationRuleSet::Source::FromInlineScript(
+    const String& source_text,
+    Document& document,
+    DOMNodeId node_id) {
+  return MakeGarbageCollected<Source>(base::PassKey<Source>(), source_text,
+                                      &document, node_id, absl::nullopt,
+                                      absl::nullopt);
+}
+
+SpeculationRuleSet::Source* SpeculationRuleSet::Source::FromRequest(
+    const String& source_text,
+    const KURL& base_url,
+    uint64_t request_id) {
+  return MakeGarbageCollected<Source>(base::PassKey<Source>(), source_text,
+                                      nullptr, absl::nullopt, base_url,
+                                      request_id);
+}
 
 const String& SpeculationRuleSet::Source::GetSourceText() const {
   return source_text_;
