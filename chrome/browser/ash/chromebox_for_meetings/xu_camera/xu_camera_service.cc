67c12115fbb3a3f7bac99225db37780d355c8661
Kyle Williams <kdgwill@google.com>
2023-08-15 23:09:29 +0000

    chromebox_for_meetings: xu_camera: fix errno logging
    QueryXuControl returns correct system error code on failure.
    
    Bug: b:296021617
    Test: CQ
    Change-Id: I2d749d788c5a64b97946197ad82b3eee043883fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4779085
    Auto-Submit: Kyle Williams <kdgwill@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183886}

--- chrome/browser/ash/chromebox_for_meetings/xu_camera/xu_camera_service.cc
+++ chrome/browser/ash/chromebox_for_meetings/xu_camera/xu_camera_service.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <base/notreached.h>
 #include <base/posix/eintr_wrapper.h>
-#include <errno.h>
 #include <fcntl.h>
 #include <libudev.h>
 #include <linux/usb/video.h>
@@ -357,8 +356,9 @@ uint8_t XuCameraService::QueryXuControl(const base::ScopedFD& file_descriptor,
       delegate_->Ioctl(file_descriptor, UVCIOC_CTRL_QUERY, &control_query);
 
   if (error < 0) {
-    LOG(ERROR) << "ioctl call failed. error: " << errno;
-    return errno;
+    logging::SystemErrorCode err = logging::GetLastSystemErrorCode();
+    LOG(ERROR) << "ioctl call failed. error: " << logging::SystemErrorCodeToString(err);
+    return err;
   }
   return error;
 }
