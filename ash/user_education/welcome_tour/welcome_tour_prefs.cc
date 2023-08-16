dfaa218fcde270b7e2adf794170e2bb31524417a
Angus L. M. McLean IV <angusmclean@google.com>
2023-08-16 18:20:05 +0000

    welcome tour: Store reason for prevention in prefs
    Stores the reason the tour was prevented for the first time in a pref,
    so that it can be used for metrics, e.g., if the user is part of the
    counterfactual arm of the experiment.
    
    Bug: b:293200505
    Change-Id: Ic02dad1d5a8469ed3196b9f996a5d24db26951d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4761688
    Commit-Queue: Angus McLean <angusmclean@google.com>
    Reviewed-by: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1184276}

--- ash/user_education/welcome_tour/welcome_tour_prefs.cc
+++ ash/user_education/welcome_tour/welcome_tour_prefs.cc
@@ -22,8 +22,10 @@ constexpr char kTimeOfFirstInteractionPrefPrefix[] =
     "ash.welcome_tour.interaction_time.";
 constexpr char kTimeOfFirstTourCompletion[] =
     "ash.welcome_tour.completed.first_time";
-constexpr char kTimeOfFirstTourPrevention[] =
+static constexpr char kTimeOfFirstTourPrevention[] =
     "ash.welcome_tour.prevented.first_time";
+static constexpr char kReasonForFirstTourPrevention[] =
+    "ash.welcome_tour.prevented.first_reason";
 
 // Helpers ---------------------------------------------------------------------
 
@@ -55,6 +57,38 @@ absl::optional<base::Time> GetTimeOfFirstTourPrevention(PrefService* prefs) {
   return GetTimePrefIfSet(prefs, kTimeOfFirstTourPrevention);
 }
 
+absl::optional<welcome_tour_metrics::PreventedReason>
+GetReasonForFirstTourPrevention(PrefService* prefs) {
+  using welcome_tour_metrics::PreventedReason;
+
+  CHECK(features::IsWelcomeTourEnabled());
+
+  auto* pref = prefs->FindPreference(kReasonForFirstTourPrevention);
+  if (!pref || pref->IsDefaultValue() || !pref->GetValue()->is_int()) {
+    return absl::nullopt;
+  }
+
+  auto reason = static_cast<PreventedReason>(pref->GetValue()->GetInt());
+  if (welcome_tour_metrics::kAllPreventedReasonsSet.Has(reason)) {
+    return reason;
+  }
+
+  return PreventedReason::kUnknown;
+}
+
+bool MarkFirstTourPrevention(PrefService* prefs,
+                             welcome_tour_metrics::PreventedReason reason) {
+  CHECK(features::IsWelcomeTourEnabled());
+
+  if (prefs->FindPreference(kTimeOfFirstTourPrevention)->IsDefaultValue()) {
+    prefs->SetTime(kTimeOfFirstTourPrevention, base::Time::Now());
+    prefs->SetInteger(kReasonForFirstTourPrevention, static_cast<int>(reason));
+    return true;
+  }
+
+  return false;
+}
+
 bool MarkTimeOfFirstInteraction(PrefService* prefs,
                                 welcome_tour_metrics::Interaction interaction) {
   CHECK(features::IsWelcomeTourEnabled());
@@ -75,18 +109,10 @@ bool MarkTimeOfFirstTourCompletion(PrefService* prefs) {
   return false;
 }
 
-bool MarkTimeOfFirstTourPrevention(PrefService* prefs) {
-  CHECK(features::IsWelcomeTourEnabled());
-  if (prefs->FindPreference(kTimeOfFirstTourPrevention)->IsDefaultValue()) {
-    prefs->SetTime(kTimeOfFirstTourPrevention, base::Time::Now());
-    return true;
-  }
-  return false;
-}
-
 void RegisterProfilePrefs(PrefRegistrySimple* registry) {
   registry->RegisterTimePref(kTimeOfFirstTourCompletion, base::Time());
   registry->RegisterTimePref(kTimeOfFirstTourPrevention, base::Time());
+  registry->RegisterIntegerPref(kReasonForFirstTourPrevention, -1);
 
   for (const auto interaction : welcome_tour_metrics::AllInteractionsSet()) {
     registry->RegisterTimePref(GetTimeOfFirstInteractionPrefName(interaction),
