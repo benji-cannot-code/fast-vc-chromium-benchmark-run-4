a559d615f4745c73970403c587adbf670761854b
Norge Vizcay <vizcay@google.com>
2023-08-16 10:23:45 +0000

    [AutofillDataModel] Add overflow to Account table.
    The CL adds ADDRESS_HOME_OVERFLOW to the list of supported ServerFieldType by the contact-info table.
    
    Bug: 1441904
    Change-Id: Id36836fee6dfc305f1c05ec03cf39fa164b6dcf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4775685
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1184071}

--- components/autofill/core/browser/webdata/autofill_table.cc
+++ components/autofill/core/browser/webdata/autofill_table.cc
@@ -1031,6 +1031,12 @@ bool AddAutofillProfileToTable(sql::Database* db,
   for (ServerFieldType type :
        AutofillTable::GetStoredTypesForAutofillProfile()) {
     if (!base::FeatureList::IsEnabled(
+            features::kAutofillEnableSupportForAddressOverflow) &&
+        type == ADDRESS_HOME_OVERFLOW) {
+      continue;
+    }
+
+    if (!base::FeatureList::IsEnabled(
             features::kAutofillEnableSupportForLandmark) &&
         type == ADDRESS_HOME_LANDMARK) {
       continue;
@@ -1143,6 +1149,7 @@ AutofillTable::GetStoredTypesForAutofillProfile() {
       ADDRESS_HOME_COUNTRY,
       ADDRESS_HOME_APT_NUM,
       ADDRESS_HOME_FLOOR,
+      ADDRESS_HOME_OVERFLOW,
       ADDRESS_HOME_LANDMARK,
       ADDRESS_HOME_BETWEEN_STREETS,
       ADDRESS_HOME_ADMIN_LEVEL2,
