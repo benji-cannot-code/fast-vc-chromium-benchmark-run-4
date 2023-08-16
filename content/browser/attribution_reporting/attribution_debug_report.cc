ecd2c11d9c4c83a10663f2e377bb00ac1f6ced3a
Nan Lin <linnan@chromium.org>
2023-08-16 13:11:33 +0000

    Remove obsolete TODO
    The interop test was added in https://crrev.com/c/4604908.
    
    Change-Id: I389e6e86b708d104e566ba495604fd5c82000cf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4779532
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184102}

--- content/browser/attribution_reporting/attribution_debug_report.cc
+++ content/browser/attribution_reporting/attribution_debug_report.cc
@@ -47,8 +47,6 @@ enum class DebugDataType {
   kSourceStorageLimit = 2,
   kSourceSuccess = 3,
   kSourceUnknownError = 4,
-  // TODO(tquintanilla): Add interop test for `kSourceDestinationRateLimit`
-  // case.
   kSourceDestinationRateLimit = 5,
   kTriggerNoMatchingSource = 6,
   kTriggerAttributionsPerSourceDestinationLimit = 7,
