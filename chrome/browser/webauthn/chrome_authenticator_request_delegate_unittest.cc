4b7626ce88f2d125af5423f3366daddbfb48b991
Anders Hartvoll Ruud <andruud@chromium.org>
2023-08-16 13:15:57 +0000

    Revert "webauthn: makeCredential requests always set an attachment."
    This reverts commit 4a32a9e62c025f615ce7e5f25382e9978aa1bff5.
    
    Reason for revert: suspected of causing crbug.com/1473305
    
    Bug: 1473305
    
    Original change's description:
    > webauthn: makeCredential requests always set an attachment.
    >
    > Fixed: 1426628
    > Change-Id: I79fb5b921e398fa20e16e2619c76d9f3487030ba
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4778986
    > Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    > Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    > Auto-Submit: Adam Langley <agl@chromium.org>
    > Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1184047}
    
    Change-Id: Iee928b567c66b1756d3f899b41e49217d6b66525
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4783590
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Owners-Override: Anders Hartvoll Ruud <andruud@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1184106}

--- chrome/browser/webauthn/chrome_authenticator_request_delegate_unittest.cc
+++ chrome/browser/webauthn/chrome_authenticator_request_delegate_unittest.cc
@@ -451,9 +451,8 @@ TEST_F(ChromeAuthenticatorRequestDelegateTest, ConditionalUI) {
     model->AddObserver(&observer);
     EXPECT_EQ(observer.last_step(),
               AuthenticatorRequestDialogModel::Step::kNotStarted);
-    AuthenticatorRequestDialogModel::TransportAvailabilityInfo tai;
-    tai.request_type = device::FidoRequestType::kGetAssertion;
-    delegate.OnTransportAvailabilityEnumerated(tai);
+    delegate.OnTransportAvailabilityEnumerated(
+        AuthenticatorRequestDialogModel::TransportAvailabilityInfo());
     EXPECT_EQ(observer.last_step() ==
                   AuthenticatorRequestDialogModel::Step::kConditionalMediation,
               conditional_ui);
@@ -571,7 +570,6 @@ TEST_F(ChromeAuthenticatorRequestDelegateTest, GpmPasskeys) {
   passkey_model->AddNewPasskeyForTesting(std::move(passkey_other_rp_id));
 
   AuthenticatorRequestDialogModel::TransportAvailabilityInfo tai;
-  tai.request_type = device::FidoRequestType::kGetAssertion;
   EXPECT_CALL(observer_, OnTransportAvailabilityEnumerated)
       .WillOnce([&tai](const auto* _, const auto* new_tai) {
         tai = std::move(*new_tai);
@@ -627,7 +625,6 @@ TEST_F(ChromeAuthenticatorRequestDelegateTest, GpmPasskeys_NoSyncPairedPhones) {
   passkey_model->AddNewPasskeyForTesting(std::move(passkey));
 
   AuthenticatorRequestDialogModel::TransportAvailabilityInfo tai;
-  tai.request_type = device::FidoRequestType::kGetAssertion;
   EXPECT_CALL(observer_, OnTransportAvailabilityEnumerated)
       .WillOnce([&tai](const auto* _, const auto* new_tai) {
         tai = std::move(*new_tai);
@@ -689,7 +686,6 @@ TEST_F(ChromeAuthenticatorRequestDelegateTest, GpmPasskeys_ShadowedPasskeys) {
   passkey_model->AddNewPasskeyForTesting(std::move(shadowed_passkey));
 
   AuthenticatorRequestDialogModel::TransportAvailabilityInfo tai;
-  tai.request_type = device::FidoRequestType::kGetAssertion;
   EXPECT_CALL(observer_, OnTransportAvailabilityEnumerated)
       .WillOnce([&tai](const auto* _, const auto* new_tai) {
         tai = std::move(*new_tai);
