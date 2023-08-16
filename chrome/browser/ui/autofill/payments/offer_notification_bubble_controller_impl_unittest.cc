8d53d9e22beb09b1de40bb0146c1d7ea0aefa309
Qihui Zhao <qihuizhao@google.com>
2023-08-16 20:24:57 +0000

    Cleanup AutofillFillMerchantPromoCodeFields as it's fully launched.
    Bug: 1007974
    Change-Id: Id9f49d18c9674f33a2c68419b93aa24fd3c496b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4775618
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1184343}

--- chrome/browser/ui/autofill/payments/offer_notification_bubble_controller_impl_unittest.cc
+++ chrome/browser/ui/autofill/payments/offer_notification_bubble_controller_impl_unittest.cc
@@ -72,8 +72,6 @@ class OfferNotificationBubbleControllerImplTest
   }
 
  protected:
-  base::test::ScopedFeatureList feature_list_;
-
   class MockCouponService : public CouponService {
    public:
     MOCK_METHOD(void,
@@ -281,8 +279,6 @@ TEST_F(OfferNotificationBubbleControllerImplTest,
 // Tests that the offer notification bubble will be shown, and coupon service
 // will not be called for a GPay promo code offer.
 TEST_F(OfferNotificationBubbleControllerImplTest, GPayPromoCode_BubbleShown) {
-  feature_list_.InitAndEnableFeature(
-      autofill::features::kAutofillFillMerchantPromoCodeFields);
   AutofillOfferData offer = CreateTestGPayPromoCodeOffer(
       /*merchant_origins=*/{GURL("https://www.example.com/first/")
                                 .DeprecatedGetOriginAsURL()},
@@ -294,26 +290,6 @@ TEST_F(OfferNotificationBubbleControllerImplTest, GPayPromoCode_BubbleShown) {
   EXPECT_EQ(controller()->GetWindowTitle(),
             l10n_util::GetStringUTF16(
                 IDS_AUTOFILL_GPAY_PROMO_CODE_OFFERS_REMINDER_TITLE));
-}
-
-// Tests that the offer notification bubble will be shown as a free-listing
-// coupon notification bubble when the feature is disabled.
-TEST_F(OfferNotificationBubbleControllerImplTest,
-       GPayPromoCode_BubbleShownWithFLCTitle) {
-  feature_list_.InitAndDisableFeature(
-      autofill::features::kAutofillFillMerchantPromoCodeFields);
-
-  AutofillOfferData offer = CreateTestGPayPromoCodeOffer(
-      /*merchant_origins=*/{GURL("https://www.example.com/first/")
-                                .DeprecatedGetOriginAsURL()},
-      /*promo_code=*/"FREEFALL5678");
-  ShowBubble(&offer);
-
-  EXPECT_CALL(mock_coupon_service_, GetCouponDisplayTimestamp).Times(0);
-  EXPECT_TRUE(controller()->GetOfferNotificationBubbleView());
-  EXPECT_EQ(
-      controller()->GetWindowTitle(),
-      l10n_util::GetStringUTF16(IDS_AUTOFILL_PROMO_CODE_OFFERS_REMINDER_TITLE));
 }
 
 }  // namespace autofill
