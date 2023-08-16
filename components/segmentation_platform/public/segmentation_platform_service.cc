febeaf1a89b007035598fd380ecafa03e941b6ce
ssid <ssid@chromium.org>
2023-08-16 20:39:42 +0000

    Fix multiple small issues with tab ranking system
    1. Support default models in training trigger API, this will move to
       database soon after default migration.
    2. Fix RecordClassificationResultComputed() to not record when result is
       not classification.
    3. Fix build error because of TrainingLabel missing copy constructor.
    
    Bug: b/280619653
    Change-Id: I51bae200a89acb2de549a266de0ca07df324209a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4690938
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Cr-Commit-Position: refs/heads/main@{#1184361}

--- components/segmentation_platform/public/segmentation_platform_service.cc
+++ components/segmentation_platform/public/segmentation_platform_service.cc
@@ -10,6 +10,8 @@ namespace segmentation_platform {
 TrainingLabels::TrainingLabels() = default;
 TrainingLabels::~TrainingLabels() = default;
 
+TrainingLabels::TrainingLabels(const TrainingLabels& other) = default;
+
 ServiceProxy* SegmentationPlatformService::GetServiceProxy() {
   return nullptr;
 }
