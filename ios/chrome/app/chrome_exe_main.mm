07ea757d445b529a4c81ac2696e566e93d82f615
Ali Juma <ajuma@chromium.org>
2023-08-16 20:36:33 +0000

    [iOS] Don't delete the preferred language setting on startup
    Starting in iOS 13, iOS lets users choose a preferred language for
    each app, possibly different from their system language, in the
    Settings app. This value is stored in NSUserDefaults, in an
    "AppleLanguages" key.
    
    Prior to iOS 13, there was no supported way to make an app's language
    different from the system language, so Chrome had an Experimental
    Setting whose value, if set, was used to overwrite the value of
    "AppleLanguages" in NSUserDefaults, and if unset, caused
    "AppleLanguages" to get cleared, resetting its effective value to
    the system language.
    
    However, this means that setting the preferred language through the
    iOS-provided UI has been broken ever since this UI was added in iOS
    13, since "AppleLanguages" gets deleted on startup when Chrome's
    experimental setting has a default value, which it will always have
    in Stable builds (which don't have experimental settings).
    
    Since there is an iOS-supported way to set Chrome's language,
    there's no longer a need for the Experimental setting, particularly
    given that the Experimental setting breaks the iOS-supported
    approach.
    
    This CL removes the Experimental setting and its associated logic.
    
    Bug: 1472278
    Change-Id: I70339935601028a4277f33cbcf6c126a7de0a638
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4784529
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184357}

--- ios/chrome/app/chrome_exe_main.mm
+++ ios/chrome/app/chrome_exe_main.mm
@@ -62,18 +62,6 @@ void SetTextDirectionIfPseudoRTLEnabled() {
   }
 }
 
-void SetUILanguageIfLanguageIsSelected() {
-  @autoreleasepool {
-    NSUserDefaults* standard_defaults = [NSUserDefaults standardUserDefaults];
-    NSString* language = [standard_defaults valueForKey:@"UILanguageOverride"];
-    if (!language || [language length] == 0) {
-      [standard_defaults removeObjectForKey:@"AppleLanguages"];
-    } else {
-      [standard_defaults setObject:@[ language ] forKey:@"AppleLanguages"];
-    }
-  }
-}
-
 int RunUIApplicationMain(int argc, char* argv[]) {
   @autoreleasepool {
     // Fetch the name of the UIApplication delegate stored in the application
@@ -118,9 +106,6 @@ int ChromeMain(int argc, char* argv[]) {
 
   // Set NSUserDefaults keys to force pseudo-RTL if needed.
   SetTextDirectionIfPseudoRTLEnabled();
-
-  // Set NSUserDefaults keys to force the UI language if needed.
-  SetUILanguageIfLanguageIsSelected();
 
   // Create this here since it's needed to start the crash handler.
   base::AtExitManager at_exit;
