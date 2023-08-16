50991bed869161e3a4daef94a2e170db6a7e5b8f
Kristi Saney <kristislee@google.com>
2023-08-16 00:10:38 +0000

    Add callbacks for updating all the theme values in the PageHandler.
    Currently this is a no-op, but represents the final stage of the path of updating the prefs from the webui. See crrev.com/c/4767261 for an example of how the whole path gets connected in the end.
    
    This CL is separate so that we don't have to have multiple CLs (or one enormous CL) that rely on IPC reviewers, and they can review all these related changes at once.
    
    Bug: 1465029
    Change-Id: I7a9f7a77134fa68e7188c40eba9bd6545fd8177c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4771941
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1183913}

--- chrome/browser/ui/webui/side_panel/read_anything/read_anything_untrusted_page_handler.cc
+++ chrome/browser/ui/webui/side_panel/read_anything/read_anything_untrusted_page_handler.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/side_panel/read_anything/read_anything_untrusted_page_handler.h"
 
+#include <algorithm>
 #include <string>
 #include <utility>
 #include <vector>
@@ -100,6 +101,27 @@ void ReadAnythingUntrustedPageHandler::OnLineSpaceChange(
   browser_->profile()->GetPrefs()->SetInteger(
       prefs::kAccessibilityReadAnythingLineSpacing,
       static_cast<size_t>(line_spacing));
+}
+
+void ReadAnythingUntrustedPageHandler::OnLetterSpaceChange(
+    read_anything::mojom::LetterSpacing letter_spacing) {
+  browser_->profile()->GetPrefs()->SetInteger(
+      prefs::kAccessibilityReadAnythingLetterSpacing,
+      static_cast<size_t>(letter_spacing));
+}
+void ReadAnythingUntrustedPageHandler::OnFontChange(const std::string& font) {
+  browser_->profile()->GetPrefs()->SetString(
+      prefs::kAccessibilityReadAnythingFontName, font);
+}
+void ReadAnythingUntrustedPageHandler::OnFontSizeChange(double font_size) {
+  double saved_font_size = std::min(font_size, kReadAnythingMaximumFontScale);
+  browser_->profile()->GetPrefs()->SetDouble(
+      prefs::kAccessibilityReadAnythingFontScale, saved_font_size);
+}
+void ReadAnythingUntrustedPageHandler::OnColorChange(
+    read_anything::mojom::Colors color) {
+  browser_->profile()->GetPrefs()->SetInteger(
+      prefs::kAccessibilityReadAnythingColorInfo, static_cast<size_t>(color));
 }
 
 void ReadAnythingUntrustedPageHandler::OnLinkClicked(
