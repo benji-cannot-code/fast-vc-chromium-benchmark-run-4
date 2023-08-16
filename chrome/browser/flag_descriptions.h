a140af028d0a6035a3f2df9c4f10ec0c43e4a497
Andrew Wolfers <aswolfers@chromium.org>
2023-08-16 14:40:35 +0000

    Add VRR always on flag
    This CL adds a new feature flag `enable-variable-refresh-rate-always-on`
    for unilaterally enabling variable refresh rates. The new flag is
    separate from the existing flag `enable-variable-refresh-rate` which
    will be used for enabling VRR only in limited contexts (borealis gaming
    is currently the only intended context, although the exact scope may
    change later).
    
    BUG=b:290265668
    
    Change-Id: I5e7fc34a6c781c399ee792a6a16993a2f6259394
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4778097
    Commit-Queue: Andrew Wolfers <aswolfers@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184150}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3497,6 +3497,9 @@ extern const char kPanelSelfRefresh2Description[];
 extern const char kEnableVariableRefreshRateName[];
 extern const char kEnableVariableRefreshRateDescription[];
 
+extern const char kEnableVariableRefreshRateAlwaysOnName[];
+extern const char kEnableVariableRefreshRateAlwaysOnDescription[];
+
 extern const char kEnforceAshExtensionKeeplistName[];
 extern const char kEnforceAshExtensionKeeplistDescription[];
 
