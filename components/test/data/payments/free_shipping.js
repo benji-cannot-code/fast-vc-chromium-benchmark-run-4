5ae67da4fa614a44c3f4b90e7e402444c086b0b1
“Kavita <kavitasoni@chromium.org>
2023-08-16 13:39:31 +0000

    Fix PaymentRequestShippingAddressAndOptionTest by migrating from basic-card payment method.
    These tests have been updated to use test-only payment app method
    i.e 'bobpay', since basic-card method has been removed.
    
    Bug: https://crbug.com/1331767
    Bug: https://crbug.com/1331769
    Bug: https://crbug.com/1331731
    Bug: https://crbug.com/1331723
    Bug: https://crbug.com/1331726
    Bug: https://crbug.com/1331729
    Change-Id: I499fda6cafb85bd8e4e39e3290bd0d6c596f4091
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4770017
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Kavita Soni <kavitasoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184117}

--- components/test/data/payments/free_shipping.js
+++ components/test/data/payments/free_shipping.js
@@ -7,29 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * Launches the PaymentRequest UI that offers free shipping worldwide.
- *
- * Legacy entry function until basic-card is removed.
- */
-function buy() {
-  buyWithMethods([{
-    supportedMethods: 'basic-card',
-    data: {
-      supportedNetworks: [
-        'visa',
-        'unionpay',
-        'mir',
-        'mastercard',
-        'jcb',
-        'discover',
-        'diners',
-        'amex',
-      ],
-    },
-  }]);
-}
-
-/**
- * Launches the PaymentRequest UI that offers free shipping worldwide.
  * @param {sequence<PaymentMethodData>} methodData An array of payment method
  *        objects.
  */
