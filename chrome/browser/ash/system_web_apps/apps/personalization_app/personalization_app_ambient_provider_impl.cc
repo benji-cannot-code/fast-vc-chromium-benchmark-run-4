57fe6842187aeffddf92055c049975abf0159353
Erica Lee <ericamlee@google.com>
2023-08-16 23:58:05 +0000

    p13n: reset backoff in ambient provider
    ResetLocalSettings resets many of the local values. It should also reset
    the backoff count, so that future requests can be sent to the ambient
    backend.
    
    Also cleaned up some comments.
    
    BUG=b:296114840
    TEST=unit_tests *AmbientProviderImpl*
    
    Change-Id: I033de230cb45448c79e9d6001507da2f3158547f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4785808
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Commit-Queue: Erica Lee <ericamlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1184466}

--- chrome/browser/ash/system_web_apps/apps/personalization_app/personalization_app_ambient_provider_impl.cc
+++ chrome/browser/ash/system_web_apps/apps/personalization_app/personalization_app_ambient_provider_impl.cc
@@ -346,8 +346,6 @@ void PersonalizationAppAmbientProviderImpl::FetchSettingsAndAlbums() {
     return;
   }
 
-  // TODO(b/161044021): Add a helper function to get all the albums. Currently
-  // only load 100 latest modified albums.
   ash::AmbientBackendController::Get()->FetchSettingsAndAlbums(
       kBannerWidthPx, kBannerHeightPx, /*num_albums=*/100,
       base::BindOnce(
@@ -643,7 +641,7 @@ void PersonalizationAppAmbientProviderImpl::OnSettingsAndAlbumsFetched(
 
 void PersonalizationAppAmbientProviderImpl::SyncSettingsAndAlbums() {
   // Clear the `selected` field, which will be populated with new value below.
-  // It is neceessary if `UpdateSettings()` failed and we need to reset the
+  // It is necessary if `UpdateSettings()` failed and we need to reset the
   // cached settings.
   for (auto& album : personal_albums_.albums) {
     album.selected = false;
@@ -749,6 +747,8 @@ void PersonalizationAppAmbientProviderImpl::ResetLocalSettings() {
   settings_.reset();
   cached_settings_.reset();
   settings_sent_for_update_.reset();
+  update_settings_retry_backoff_.Reset();
+  fetch_settings_retry_backoff_.Reset();
   has_pending_fetch_request_ = false;
   is_updating_backend_ = false;
   has_pending_updates_for_backend_ = false;
