095ca970a0a28ca793edb69808bcb413414c73dc
Rune Lillesveen <futhark@chromium.org>
2023-08-16 09:05:46 +0000

    ::backdrop is tree-abiding and should be allowed after ::slotted()
    When ::backdrop was moved into css-position, it was made tree-abiding:
    
    https://drafts.csswg.org/css-position-4/#backdrop
    
    That means ::slotted()::backdrop should be allowed.
    
    Bug: 1473233
    Change-Id: I4079c38c2efa53f411b157ad6e8956e271fdd6d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4778566
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184038}

--- third_party/blink/renderer/core/css/css_selector.cc
+++ third_party/blink/renderer/core/css/css_selector.cc
@@ -1391,7 +1391,8 @@ bool CSSSelector::IsTreeAbidingPseudoElement() const {
          (GetPseudoType() == kPseudoBefore || GetPseudoType() == kPseudoAfter ||
           GetPseudoType() == kPseudoMarker ||
           GetPseudoType() == kPseudoPlaceholder ||
-          GetPseudoType() == kPseudoFileSelectorButton);
+          GetPseudoType() == kPseudoFileSelectorButton ||
+          GetPseudoType() == kPseudoBackdrop);
 }
 
 bool CSSSelector::IsAllowedAfterPart() const {
