54ea67610b40702b35017c13f756d9cb4d7f6f3f
Sasha McIntosh <sashamcintosh@chromium.org>
2023-08-16 17:23:11 +0000

    display,viz: HDR10 for external displays
    Limit HDR10 capability to external displays, as this format conflicts
    with piecewise linear usage on current HDR CrOS devices.
    
    TEST=
      All/DisplayChangeObserverTest.HDRDisplayColorSpaces*,
      OverlayProcessorOzoneTest.ColorSpaceMismatch
    BUG=b:264947967
    
    Change-Id: Icbe512d98773c570e1654159e39f2bc8c46eb421
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4500261
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Auto-Submit: Sasha McIntosh <sashamcintosh@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184221}

--- ui/ozone/platform/drm/common/drm_util.h
+++ ui/ozone/platform/drm/common/drm_util.h
@@ -49,6 +49,8 @@ const char kHdcpContentType[] = "HDCP Content Type";
 
 const char kColorSpace[] = "Colorspace";
 const char kColorSpaceBT2020RGBEnumName[] = "BT2020_RGB";
+const char kColorSpaceDefaultEnumName[] = "Default";
+
 const char kHdrOutputMetadata[] = "HDR_OUTPUT_METADATA";
 
 constexpr char kPrivacyScreenPropertyNameLegacy[] = "privacy-screen";
@@ -174,6 +176,7 @@ display::VariableRefreshRateState GetVariableRefreshRateState(
     const DrmWrapper& drm,
     HardwareDisplayControllerInfo* info);
 
+const char* GetNameForColorspace(const gfx::ColorSpace color_space);
 uint64_t GetEnumValueForName(const DrmWrapper& drm,
                              int property_id,
                              const char* str);
