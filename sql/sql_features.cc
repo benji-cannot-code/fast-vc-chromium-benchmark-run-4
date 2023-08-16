e1dc3f3cbeb770d64414163ccb04a4a2609e38cb
Austin Sullivan <asully@chromium.org>
2023-08-16 18:32:37 +0000

    sql: Consider poisoned databases closed
    This feature is enabled by default, but is gated behind a new base
    feature flag. It builds on the changes behind the kClearDbIfCloseFails
    flag. Rather than adding a new flag whose value would depend on this
    older flag, this CL simply removes the older flag, which has been on
    stable for almost two milestones and is therefore removable according
    to the flag-guarding guidelines.
    
    Bug: 1441955
    Change-Id: Idabc75fc69250976a907dfe4e554cc9f204b21fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4499396
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184284}

--- sql/sql_features.cc
+++ sql/sql_features.cc
@@ -7,10 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace sql::features {
 
-// Clears the Database.db_ member if sqlite3_open_v2() fails.
+// When enabled, the `Database::is_open()` method return false for poisoned
+// databases.
 // TODO(https://crbug.com/1441955): Remove this flag eventually.
-BASE_FEATURE(kClearDbIfCloseFails,
-             "ClearDbIfCloseFails",
+BASE_FEATURE(kConsiderPoisonedDatabasesClosed,
+             "ConsiderPoisonedDatabasesClosed",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Enable WAL mode for all SQLite databases.
