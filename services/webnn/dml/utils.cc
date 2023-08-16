a71257792bc07e3a815b8ff0e3085122143c52dd
junwei <junwei.fu@intel.com>
2023-08-16 05:39:11 +0000

    WebNN: Define async computation in WebNNGraph mojo interface.
    The `Compute` method is called by the renderer process to carry out the
    computational workload of the compiled graph. It is one-to-one mapping
    between `BigBuffer` and ArrayBuffer of input/output to transfer data
    from renderer process to GPU process.
    
    Add unit tests for the `Compute` method and verify the inputs array
    buffer information match as the graph expected.
    
    Fuchsia-Binary-Size: Size increase is unavoidable.
    Bug: 1273291
    Change-Id: I8e5d7ed05522916ede818d84457e80271d4ce7bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4664465
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Commit-Queue: Junwei Fu <junwei.fu@intel.com>
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183991}

--- services/webnn/dml/utils.cc
+++ services/webnn/dml/utils.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/webnn/dml/utils.h"
 
+#include "base/bits.h"
 #include "base/check.h"
 #include "base/check_op.h"
 #include "base/notreached.h"
@@ -70,7 +71,8 @@ uint64_t CalculateDMLBufferTensorSize(
   // the nearest 4 bytes according to the alignment requirement:
   // https://learn.microsoft.com/en-us/windows/ai/directml/dml-helper-functions#dmlcalcbuffertensorsize
   base::CheckedNumeric<uint64_t> buffer_tensor_size =
-      (CalculateElementCount(dimensions, strides) * element_size + 3) & ~3ull;
+      base::bits::AlignUp<uint64_t>(
+          CalculateElementCount(dimensions, strides) * element_size, 4);
 
   return buffer_tensor_size.ValueOrDie();
 }
