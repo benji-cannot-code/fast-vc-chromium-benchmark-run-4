5ecb3077db6ffb638807259f47ae7138f957c03a
Rafał Godlewski <rgod@google.com>
2023-08-16 09:12:02 +0000

    [iOS] Cap username and email at one line in family picker view
    Additionally adds a unit test for the view layout.
    
    Bug: 1463882
    Change-Id: Ibbdcaa9ba14573cf236b50856a1ded49d3eca10a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4775022
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184045}

--- ios/chrome/browser/ui/settings/password/password_sharing/family_picker_view_controller.mm
+++ ios/chrome/browser/ui/settings/password/password_sharing/family_picker_view_controller.mm
@@ -113,6 +113,8 @@ const CGFloat kAccessorySymbolSize = 22;
                      cellForRowAtIndexPath:indexPath];
   cell.selectionStyle = UITableViewCellSelectionStyleNone;
   cell.userInteractionEnabled = YES;
+  cell.textLabel.numberOfLines = 1;
+  cell.detailTextLabel.numberOfLines = 1;
   if (_recipients[indexPath.row].isEligible) {
     cell.accessoryView = [[UIImageView alloc] initWithImage:[self circleIcon]];
   } else {
@@ -125,6 +127,15 @@ const CGFloat kAccessorySymbolSize = 22;
   }
 
   return cell;
+}
+
+#pragma mark - FamilyPickerConsumer
+
+- (void)setRecipients:(NSArray<RecipientInfoForIOSDisplay*>*)recipients {
+  _recipients = recipients;
+
+  [self loadModel];
+  [self.tableView reloadData];
 }
 
 #pragma mark - Items
