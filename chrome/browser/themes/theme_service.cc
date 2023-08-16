aafe3e7a1d1a36d48f29925cce7ca676f9b712d4
Gavin Mak <gavinmak@google.com>
2023-08-16 21:51:02 +0000

    [Code Health] Remove Browser.ThemeService.WritePackToDisk histogram
    As part of the Chrome Code Health rotation effort, remove the expired
    Browser.ThemeService.WritePackToDisk histogram.
    
    OBSOLETE_HISTOGRAMS=Expired in 2022
    
    Bug: 1448154
    Change-Id: I75d3b79d9db43bd528c23a8e9afe7ee51919c264
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4781376
    Commit-Queue: Gavin Mak <gavinmak@google.com>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184403}

--- chrome/browser/themes/theme_service.cc
+++ chrome/browser/themes/theme_service.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted_memory.h"
-#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics.h"
 #include "base/observer_list.h"
@@ -101,9 +100,7 @@ void WritePackToDiskCallback(BrowserThemePack* pack,
   if (g_dont_write_theme_pack_for_testing)
     return;
 
-  const bool success =
-      pack->WriteToDisk(directory.Append(chrome::kThemePackFilename));
-  base::UmaHistogramBoolean("Browser.ThemeService.WritePackToDisk", success);
+  pack->WriteToDisk(directory.Append(chrome::kThemePackFilename));
 }
 
 }  // namespace
