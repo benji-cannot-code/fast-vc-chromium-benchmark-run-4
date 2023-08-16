1d8ccb75546e7c8c6ddda0e849ee3e0659380d9b
Nick Diego Yamane <nickdiego@igalia.com>
2023-08-16 21:49:36 +0000

    exo: send ash/exo version to wayland clients
    As discussed in [1], as Lacros launching approaches, it becomes more and
    more desirable to have mechanisms in place to handle Exo-side issues,
    missing features at Lacros side. So, in addition to aura-shell's
    bugfixes, this CL adds an event to let Lacros (and other clients) know
    the current Ash version, such that it can decide how to deal with each
    situation, eg: buggy or incomplete feature, etc.
    
    R=petermcneeley, sky
    
    Bug: 1457008
    Change-Id: Ic264fbe1ce456ebd75a08abcd25bf70d0b520505
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4778958
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184401}

--- components/exo/wayland/zaura_shell.cc
+++ components/exo/wayland/zaura_shell.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/wayland/wayland_display_observer.h"
 #include "components/exo/wayland/wl_output.h"
 #include "components/exo/wayland/xdg_shell.h"
+#include "components/version_info/version_info.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/env.h"
 #include "ui/aura/window_occlusion_tracker.h"
@@ -1173,6 +1174,12 @@ class WaylandAuraShell : public ash::DesksController::Observer,
                              ? ZAURA_SHELL_LAYOUT_MODE_TABLET
                              : ZAURA_SHELL_LAYOUT_MODE_WINDOWED;
       zaura_shell_send_layout_mode(aura_shell_resource_, layout_mode);
+    }
+    if (wl_resource_get_version(aura_shell_resource_) >=
+        ZAURA_SHELL_COMPOSITOR_VERSION_SINCE_VERSION) {
+      const base::StringPiece ash_version = version_info::GetVersionNumber();
+      zaura_shell_send_compositor_version(aura_shell_resource_,
+                                          ash_version.data());
     }
     if (wl_resource_get_version(aura_shell_resource_) >=
         ZAURA_SHELL_BUG_FIX_SINCE_VERSION) {
