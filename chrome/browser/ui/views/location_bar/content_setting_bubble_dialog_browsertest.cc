901f3ca41264dffc812dc946a969eaa338702ef9
Christian Dullweber <dullweber@chromium.org>
2023-08-16 13:13:13 +0000

    SAA: Adjust bottom margin of omnibox bubble
    Reduce bottom margin of the StorageAccess ContentSettingBubbleContents
    to 8 to match the PageInfo and CookieControls bubble.
    
    Screenshot: https://crbug.com/1433644#c92
    Bug: 1433644
    Change-Id: Ie9a0e1ab95cc47c510404a696ad7aab562b87a00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4780968
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184105}

--- chrome/browser/ui/views/location_bar/content_setting_bubble_dialog_browsertest.cc
+++ chrome/browser/ui/views/location_bar/content_setting_bubble_dialog_browsertest.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/blocked_content/popup_blocker_tab_helper.h"
 #include "components/content_settings/browser/page_specific_content_settings.h"
+#include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings_types.h"
 #include "components/content_settings/core/test/content_settings_mock_provider.h"
 #include "components/content_settings/core/test/content_settings_test_utils.h"
@@ -113,7 +114,8 @@ class ContentSettingBubbleDialogTest
                       GetPopupNavigationDelegateFactoryForTesting(),
                   &CreateTestPopupNavigationDelegate) {
     scoped_feature_list_.InitWithFeatures(
-        {features::kQuietNotificationPrompts},
+        {features::kQuietNotificationPrompts,
+         permissions::features::kPermissionStorageAccessAPI},
         {permissions::features::kPermissionQuietChip});
   }
 
@@ -210,7 +212,14 @@ void ContentSettingBubbleDialogTest::ApplyContentSettingsForType(
               custom_handlers::ProtocolHandler::CreateProtocolHandler(
                   "mailto", GURL("https://example.com/")));
       break;
-
+    case ContentSettingsType::STORAGE_ACCESS:
+      // Set a fake URL so the UI displays a consistent string for pixel tests.
+      web_contents->GetController().GetVisibleEntry()->SetVirtualURL(
+          GURL("http://example.com/"));
+      content_settings->OnTwoSitePermissionChanged(
+          content_type, net::SchemefulSite(GURL("https://embedded.com")),
+          CONTENT_SETTING_BLOCK);
+      break;
     default:
       // For all other content_types passed in, mark them as blocked.
       content_settings->OnContentBlocked(content_type);
@@ -333,6 +342,8 @@ void ContentSettingBubbleDialogTest::ShowUi(const std::string& name) {
        ImageType::AUTOMATIC_DOWNLOADS},
       {"midi_sysex", ContentSettingsType::MIDI_SYSEX, ImageType::MIDI_SYSEX},
       {"ads", ContentSettingsType::ADS, ImageType::ADS},
+      {"storage_access", ContentSettingsType::STORAGE_ACCESS,
+       ImageType::STORAGE_ACCESS},
   };
   for (auto content_settings : content_settings_values) {
     if (base::StartsWith(name, content_settings.name,
@@ -424,6 +435,11 @@ IN_PROC_BROWSER_TEST_P(ContentSettingBubbleDialogTest,
 
 IN_PROC_BROWSER_TEST_P(ContentSettingBubbleDialogTest,
                        InvokeUi_notifications_quiet_predicted_very_unlikely) {
+  ShowAndVerifyUi();
+}
+
+IN_PROC_BROWSER_TEST_P(ContentSettingBubbleDialogTest,
+                       InvokeUi_storage_access) {
   ShowAndVerifyUi();
 }
 
