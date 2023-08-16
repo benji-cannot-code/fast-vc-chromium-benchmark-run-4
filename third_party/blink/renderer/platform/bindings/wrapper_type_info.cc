5f2d3879a5cd559469e707790ea641876ee2287a
Yuki Shiino <yukishiino@chromium.org>
2023-08-16 11:49:31 +0000

    bindings: Support Web IDL async iterable (3 of 4)
    Supports the code generation of AsyncIterator<Foo> and
    V8AsyncIteratorFoo for interface Foo that declares async iterable.
    
    AsyncIterator<Foo> and V8AsyncIteratorFoo are mostly the same as
    SyncIterator<Foo> and V8SyncIterableBar. The key differences are:
    1. prototype chain (v8::Intrinsic::kAsyncIteratorPrototype)
    2. @@asyncIterator property (instead of @@iterator property)
    
    Bug: 1087157
    Change-Id: I8c48ef730b953eadca1046ac69f5aeeb6752310a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4666961
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1184089}

--- third_party/blink/renderer/platform/bindings/wrapper_type_info.cc
+++ third_party/blink/renderer/platform/bindings/wrapper_type_info.cc
@@ -44,7 +44,7 @@ v8::Local<v8::Template> WrapperTypeInfo::GetV8ClassTemplate(
     case kIdlObservableArray:
       v8_template = v8::FunctionTemplate::New(isolate);
       break;
-    case kIdlSyncIterator:
+    case kIdlAsyncOrSyncIterator:
       v8_template = v8::FunctionTemplate::New(isolate);
       break;
     case kCustomWrappableKind:
