4cc89ede2b8751ca83b4bde2bd9814b7597c3205
Tanmoy Mollik <triploblastic@chromium.org>
2023-08-16 09:06:56 +0000

    [iOS] Convert kSync usage to kSignin in push_notifications
    Bug: 1462286
    Change-Id: I2aee17f392f0d2fe9c6bce9f0230008aecbb3e90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4765348
    Reviewed-by: Daniel White <danieltwhite@google.com>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184040}

--- ios/chrome/browser/push_notification/push_notification_browser_state_service.mm
+++ ios/chrome/browser/push_notification/push_notification_browser_state_service.mm
@@ -73,7 +73,7 @@ void PushNotificationBrowserStateService::OnPrimaryAccountChanged(
   if (!service->DeviceTokenIsSet()) {
     return;
   }
-  const signin::ConsentLevel consent_level = signin::ConsentLevel::kSync;
+  const signin::ConsentLevel consent_level = signin::ConsentLevel::kSignin;
 
   switch (event.GetEventTypeFor(consent_level)) {
     case signin::PrimaryAccountChangeEvent::Type::kSet: {
