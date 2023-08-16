2e478e826b3ca0d1554857221f2f60f0443af468
Fiona Macintosh <fmacintosh@google.com>
2023-08-16 17:58:02 +0000

    Remove features::PreloadingDesktopSettingsSubPage flag.
    Bug: 1385176
    Change-Id: I786d25197a50eaaec59352077f087567ccee7e3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4781408
    Reviewed-by: Max Curran <curranmax@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1184252}

--- chrome/browser/preloading/preloading_features.cc
+++ chrome/browser/preloading/preloading_features.cc
@@ -7,11 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace features {
 
-// Forces Chrome to use the Preload pages settings sub page on desktop
-// platforms. This allows a user to choose between no preloading, standard
-// preloading, and extended preloading.
-BASE_FEATURE(kPreloadingDesktopSettingsSubPage,
-             "PreloadingDesktopSettingsSubPage",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+// TODO(1446230): Add feature for moving preloading subpage to
+// performance settings.
 
 }  // namespace features
