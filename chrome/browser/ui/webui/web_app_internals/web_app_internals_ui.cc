027242c87da2581ae8bd0a7783be2d0adada3b85
Chase Phillips <cmp@chromium.org>
2023-08-17 00:50:42 +0000

    iwa: Modify web-app-internals to allow installing an IWA
    This change adds a form (only visible when IWAs are enabled) to
    web-app-internals that allows installing a DevModeProxy IWA directly
    from that page.
    
    Before installation: https://screenshot.googleplex.com/9t82r8U8sFNToBa
    After installation: https://screenshot.googleplex.com/7uQuBWkxAmDmoeJ
    Installation failed: https://screenshot.googleplex.com/5qZjbW9c6LQAYJq
    
    Bug: 1472373, b/264423148
    Change-Id: I3a9fda68acc1b54dad9872887886ba5840684ea4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4775823
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Auto-Submit: Chase Phillips <cmp@chromium.org>
    Commit-Queue: Chase Phillips <cmp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184491}

--- chrome/browser/ui/webui/web_app_internals/web_app_internals_ui.cc
+++ chrome/browser/ui/webui/web_app_internals/web_app_internals_ui.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/web_app_internals/web_app_internals_handler.h"
 #include "chrome/browser/ui/webui/webui_util.h"
+#include "chrome/browser/web_applications/isolated_web_apps/isolated_web_app_dev_mode.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/web_app_internals_resources.h"
 #include "chrome/grit/web_app_internals_resources_map.h"
@@ -30,9 +31,12 @@ WebAppInternalsUI::WebAppInternalsUI(content::WebUI* web_ui)
       internals,
       base::make_span(kWebAppInternalsResources, kWebAppInternalsResourcesSize),
       IDR_WEB_APP_INTERNALS_WEB_APP_INTERNALS_HTML);
+  internals->UseStringsJs();
+  internals->AddBoolean(
+      "experimentalIsIwaDevModeEnabled",
+      web_app::IsIwaDevModeEnabled(Profile::FromWebUI(web_ui)));
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
-  internals->UseStringsJs();
   internals->AddBoolean(
       "experimentalIsolationEnabled",
       web_app::ResolveExperimentalWebAppIsolationFeature() !=
