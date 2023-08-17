05f0e67613c0fd2cf83b1e5f32d735b250c30357
Luke Warlow <luke@warlow.dev>
2023-08-17 16:15:08 +0000

    Fix issues with scrollbar-color invalidation with currentColor
    scrollbar-color property that uses currentColor now correctly repaints
    when color value updates.
    
    This only fixes non-viewports as viewports have a separate issue where
     scrollbar-color invalidation doesn't work at all.
    
    Bug: 891944
    Change-Id: Iac513cbadbfc589280621b864d1d0b5d68bf9e98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4774160
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Luke <lukewarlow156@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1184751}

--- third_party/blink/renderer/core/paint/paint_layer_scrollable_area.cc
+++ third_party/blink/renderer/core/paint/paint_layer_scrollable_area.cc
@@ -1405,8 +1405,10 @@ void PaintLayerScrollableArea::UpdateAfterStyleChange(
       old_style->UsedColorScheme() != UsedColorSchemeScrollbars() ||
       old_style->ScrollbarWidth() !=
           GetLayoutBox()->StyleRef().ScrollbarWidth() ||
-      old_style->ScrollbarColor() !=
-          GetLayoutBox()->StyleRef().ScrollbarColor()) {
+      old_style->ScrollbarThumbColorResolved() !=
+          GetLayoutBox()->StyleRef().ScrollbarThumbColorResolved() ||
+      old_style->ScrollbarTrackColorResolved() !=
+          GetLayoutBox()->StyleRef().ScrollbarTrackColorResolved()) {
     SetScrollControlsNeedFullPaintInvalidation();
   }
 }
