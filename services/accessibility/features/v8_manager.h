68b937f9397f1c044d92ad4789c74ef19e26219e
Jacob Francis <francisjp@google.com>
2023-08-17 22:01:47 +0000

    [ATP] Add ATP OS DevTools Agent and Session
    This CL adds the classes required to allow communication with the
    agent host in the browser process and the v8_inspector.
    
    At this point we can interact with ATP via the devtools console. However
    the devtools ui will need to be updated to remove unnecessary
    features, and we will eventually have follow on work to enable
    breakpoints.
    
    AX-RelNotes: N/A
    Bug: b/266767410
    Change-Id: I2745a33de8ea85f6d7c81713e71125e2b3c9c7df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4671308
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Jacob Francis <francisjp@google.com>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184931}

--- services/accessibility/features/v8_manager.h
+++ services/accessibility/features/v8_manager.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/sequenced_task_runner_helpers.h"
-#include "base/task/single_thread_task_runner.h"
 #include "base/threading/sequence_bound.h"
 #include "mojo/public/cpp/bindings/generic_pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_associated_receiver.h"
@@ -21,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/accessibility/features/bindings_isolate_holder.h"
 #include "services/accessibility/public/mojom/accessibility_service.mojom-forward.h"
 #include "services/accessibility/public/mojom/automation.mojom-forward.h"
+#include "third_party/blink/public/mojom/devtools/devtools_agent.mojom.h"
 #include "v8/include/v8-context.h"
 #include "v8/include/v8-local-handle.h"
 
@@ -38,6 +38,7 @@ namespace ax {
 class AutomationInternalBindings;
 class InterfaceBinder;
 class V8Manager;
+class OSDevToolsAgent;
 
 // A V8Environment owns a V8 context within the Accessibility Service, the
 // bindings that belong to that context, as well as loading the Javascript that
@@ -50,6 +51,11 @@ class V8Manager;
 // TODO(dcheng): Move this into v8_environment.h.
 class V8Environment : public BindingsIsolateHolder {
  public:
+  // The default Context ID to use. We currently will have one context per
+  // isolate. In the future we may need to switch this to an incrementing
+  // system.
+  static const int kDefaultContextId = 1;
+
   // Creates a new V8Environment with its own isolate and context.
   static base::SequenceBound<V8Environment> Create(
       base::WeakPtr<V8Manager> manager);
@@ -59,6 +65,10 @@ class V8Environment : public BindingsIsolateHolder {
 
   V8Environment(const V8Environment&) = delete;
   V8Environment& operator=(const V8Environment&) = delete;
+
+  // Creates a devtools agent to debug javascript running in this environment.
+  void ConnectDevToolsAgent(
+      mojo::PendingAssociatedReceiver<blink::mojom::DevToolsAgent> agent);
 
   // All of the APIs needed for this V8Manager (based on the AT type) should be
   // installed before adding V8 bindings.
@@ -100,6 +110,8 @@ class V8Environment : public BindingsIsolateHolder {
   // Holders for isolate and context.
   std::unique_ptr<gin::IsolateHolder> isolate_holder_;
   std::unique_ptr<gin::ContextHolder> context_holder_;
+
+  std::unique_ptr<OSDevToolsAgent> devtools_agent_;
 };
 
 // Owns the V8Environment and any Mojo interfaces exposed to that V8Environment.
@@ -123,6 +135,10 @@ class V8Manager {
       mojom::AccessibilityServiceClient* ax_service_client);
 
   void FinishContextSetUp();
+
+  // Instructs V8Environment to create a devtools agent.
+  void ConnectDevToolsAgent(
+      mojo::PendingAssociatedReceiver<blink::mojom::DevToolsAgent> agent);
 
   // Called by V8Environment when JS wants to bind a Mojo interface.
   void BindInterface(const std::string& interface_name,
--- services/accessibility/os_accessibility_service.cc
+++ services/accessibility/os_accessibility_service.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "services/accessibility/assistive_technology_controller_impl.h"
+#include "services/accessibility/features/v8_manager.h"
 
 namespace ax {
 
@@ -35,9 +36,11 @@ void OSAccessibilityService::BindAssistiveTechnologyController(
 }
 
 void OSAccessibilityService::ConnectDevToolsAgent(
-    ::mojo::PendingAssociatedReceiver<::blink::mojom::DevToolsAgent> agent,
+    mojo::PendingAssociatedReceiver<::blink::mojom::DevToolsAgent> agent,
     mojom::AssistiveTechnologyType type) {
-  // TODO(francisjp): b/266767410 implement.
+  auto* manager = at_controller_->GetV8Manager(type);
+  CHECK(manager);
+  manager->ConnectDevToolsAgent(std::move(agent));
 }
 
 }  // namespace ax
