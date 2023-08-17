5093bfdd6a37d55e5901a9a8a2e0413f65663d35
Evan Stade <estade@chromium.org>
2023-08-17 16:44:53 +0000

    IndexedDB: make leveldb encoding/decoding slightly stricter
    Keys with type IDBKeyType::Min should not be considered a valid
    IndexedDBKey. This key type is never encoded in the LevelDB backing
    store as an IndexedDBKey, nor should it ever be passed from renderer
    to browser.
    
    Likewise, we never write kIndexedDBKeyNullTypeByte for an IndexedDBKey
    and therefore should never need to decode one. (If data were corrupted,
    we might run into this, but then decoding should fail rather than
    returning an invalid IndexedDBKey.)
    
    kIndexedDBKeyNullTypeByte and kIndexedDBKeyMinKeyTypeByte are used as
    placeholder values when encoding/decoding cursors and indexes.
    
    This fixes an issue flagged by the fuzzer.
    
    Bug: 1470500
    Change-Id: Ic66836b9c23cd01403f0ae8cadb78f0aeeb40584
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4786530
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184764}

--- content/browser/indexed_db/indexed_db_leveldb_coding.cc
+++ content/browser/indexed_db/indexed_db_leveldb_coding.cc
@@ -239,7 +239,6 @@ void EncodeDouble(double value, std::string* into) {
 
 void EncodeIDBKey(const IndexedDBKey& value, std::string* into) {
   size_t previous_size = into->size();
-  CHECK(value.IsValid());
   switch (value.type()) {
     case blink::mojom::IDBKeyType::Array: {
       EncodeByte(kIndexedDBKeyArrayTypeByte, into);
@@ -274,15 +273,12 @@ void EncodeIDBKey(const IndexedDBKey& value, std::string* into) {
     case blink::mojom::IDBKeyType::Invalid:
     case blink::mojom::IDBKeyType::Min:
     default:
-      NOTREACHED();
-      EncodeByte(kIndexedDBKeyNullTypeByte, into);
-      return;
+      NOTREACHED_NORETURN();
   }
 }
 
 void EncodeSortableIDBKey(const IndexedDBKey& value, std::string* into) {
   size_t previous_size = into->size();
-  CHECK(value.IsValid());
   switch (value.type()) {
     case blink::mojom::IDBKeyType::Array: {
       EncodeByte(kOrderedArrayTypeByte, into);
@@ -313,9 +309,7 @@ void EncodeSortableIDBKey(const IndexedDBKey& value, std::string* into) {
     case blink::mojom::IDBKeyType::Invalid:
     case blink::mojom::IDBKeyType::Min:
     default:
-      NOTREACHED();
-      EncodeByte(kSentinel, into);
-      return;
+      NOTREACHED_NORETURN();
   }
 }
 
@@ -484,8 +478,7 @@ bool DecodeIDBKeyRecursive(StringPiece* slice,
 
   switch (type) {
     case kIndexedDBKeyNullTypeByte:
-      *value = std::make_unique<IndexedDBKey>();
-      return true;
+      return false;
 
     case kIndexedDBKeyArrayTypeByte: {
       int64_t length = 0;
@@ -532,8 +525,7 @@ bool DecodeIDBKeyRecursive(StringPiece* slice,
       return true;
     }
     case kIndexedDBKeyMinKeyTypeByte: {
-      *value = std::make_unique<IndexedDBKey>(blink::mojom::IDBKeyType::Min);
-      return true;
+      return false;
     }
   }
 
