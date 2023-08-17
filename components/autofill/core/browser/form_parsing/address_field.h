11db24f57b3e6ffc1dd5a2742a9648d847fffb1b
Norge Vizcay <vizcay@google.com>
2023-08-17 14:14:54 +0000

    [AutofillDataModel] Add parsing for address overflow fields.
    The CL adds type field parsing for address overflow fields. The parsing logic is implemented behind kAutofillEnableSupportForAddressOverflow flag.
    
    It also moves the token "complemento" from address line 2 parsing
    expression into address overflow.
    
    Bug: 1441904
    Change-Id: I38cbcc12f78103b943c2c910c684f41466301804
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4783769
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1184702}

--- components/autofill/core/browser/form_parsing/address_field.h
+++ components/autofill/core/browser/form_parsing/address_field.h
@@ -135,6 +135,11 @@ class AddressField : public FormField {
       const LanguageCode& page_language,
       PatternSource pattern_source);
 
+  ParseNameLabelResult ParseNameAndLabelForOverflow(
+      AutofillScanner* scanner,
+      const LanguageCode& page_language,
+      PatternSource pattern_source);
+
   ParseNameLabelResult ParseNameAndLabelForState(
       AutofillScanner* scanner,
       const LanguageCode& page_language,
@@ -160,6 +165,7 @@ class AddressField : public FormField {
   raw_ptr<AutofillField> landmark_ = nullptr;
   raw_ptr<AutofillField> between_streets_ = nullptr;
   raw_ptr<AutofillField> admin_level2_ = nullptr;
+  raw_ptr<AutofillField> overflow_ = nullptr;
 };
 
 }  // namespace autofill
