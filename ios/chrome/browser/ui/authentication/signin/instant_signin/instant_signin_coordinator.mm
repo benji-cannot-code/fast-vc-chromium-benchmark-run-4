c497a4e89307c05f4590409796fc39e1bfc17eb0
Jérôme Lebel <jlebel@chromium.org>
2023-08-17 15:09:06 +0000

    [iOS] Swap CHECK to DCHECK in -[InstantSigninCoordinator dealloc]
    Replace CHECK by DCHECK. This CHECK can fail when using double tap.
    Once the double tap issue is fixed, we can replace this DCHECK by
    DUMP_WILL_BE_CHECK to see if there is still an issue.
    
    This patch needs to be merged in to M117.
    
    Fixed: 1473636
    Bug: 1470859
    Change-Id: Iead99ee49055f69a12a43616ef372a187f354bf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4790103
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Menghan Yang <myuu@google.com>
    Reviewed-by: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1184721}

--- ios/chrome/browser/ui/authentication/signin/instant_signin/instant_signin_coordinator.mm
+++ ios/chrome/browser/ui/authentication/signin/instant_signin/instant_signin_coordinator.mm
@@ -61,7 +61,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)dealloc {
-  CHECK(!_mediator);
+  // TODO(crbug.com/1464966): Replace to DUMP_WILL_BE_CHECK(), when double tap
+  // is fixed.
+  DCHECK(!_mediator);
 }
 
 #pragma mark - ChromeCoordinator
