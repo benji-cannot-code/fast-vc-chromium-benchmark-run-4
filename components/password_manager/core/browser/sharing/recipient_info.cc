554317097571be281ffd34b34001ae8fc3354e24
Andrii Natiahlyi <natiahlyi@google.com>
2023-08-17 12:06:22 +0000

    [Passwords] Convert sharing public key to base64
    Bytes from x25519_public_key proto may not match valid utf-8 characters.
    Conversion to Base64 prevents further errors while converting string to
    different types. Also, this helps with debugging by verifying the public keys from `chrome.passwordsPrivate.fetchFamilyMembers` match with ones
    in chrome://sync-internals.
    
    Bug: 1445526
    Change-Id: Icb1d5f67c73f2ce0c904d5c6820ce2bf92658107
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4788712
    Reviewed-by: Elias Khsheibun <eliaskh@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Andrii Natiahlyi <natiahlyi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1184652}

--- components/password_manager/core/browser/sharing/recipient_info.cc
+++ components/password_manager/core/browser/sharing/recipient_info.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/password_manager/core/browser/sharing/recipient_info.h"
 
+#include "base/base64.h"
+
 namespace password_manager {
 
 bool PublicKey::operator==(const PublicKey& other) const {
@@ -13,7 +15,10 @@ bool PublicKey::operator==(const PublicKey& other) const {
 
 sync_pb::CrossUserSharingPublicKey PublicKey::ToProto() const {
   sync_pb::CrossUserSharingPublicKey proto_result;
-  proto_result.set_x25519_public_key(key);
+  // Bytes from x25519_public_key proto may not match valid utf-8 characters.
+  // Conversion to Base64 prevents further errors while converting string to
+  // different types.
+  base::Base64Decode(key, proto_result.mutable_x25519_public_key());
   proto_result.set_version(key_version);
   return proto_result;
 }
@@ -22,7 +27,7 @@ sync_pb::CrossUserSharingPublicKey PublicKey::ToProto() const {
 PublicKey PublicKey::FromProto(
     const sync_pb::CrossUserSharingPublicKey& proto_public_key) {
   PublicKey result;
-  result.key = proto_public_key.x25519_public_key();
+  base::Base64Encode(proto_public_key.x25519_public_key(), &result.key);
   result.key_version = proto_public_key.version();
   return result;
 }
