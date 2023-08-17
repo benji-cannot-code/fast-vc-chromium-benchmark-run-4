49a00ef4bd337feb123fe3576ef39575a208ec94
Simon Zünd <szuend@chromium.org>
2023-08-17 14:03:38 +0000

    [devtools] Replace Common.Object global with import
    R=wolfi@chromium.org
    
    Bug: 1442410
    Change-Id: Ic18cd11512999f2b4f8eed505c8adbf4398cb54a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4789069
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184693}

--- third_party/blink/web_tests/http/tests/devtools/unit/object-events.js
+++ third_party/blink/web_tests/http/tests/devtools/unit/object-events.js
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {TestRunner} from 'test_runner';
 
+import * as Common from 'devtools/core/common/common.js';
+
 (async function() {
   TestRunner.addResult(`The test verifies that DevTools events work.\n`);
-  var object = new Common.Object();
+  var object = new Common.ObjectWrapper.ObjectWrapper();
   var eventSymbol = Symbol('Event');
 
   addListener('original listener');
