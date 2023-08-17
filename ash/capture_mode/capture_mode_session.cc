89a137866ff9c021ac8645ceb092ed7606c9936a
Michele Fan <michelefan@chromium.org>
2023-08-17 19:40:56 +0000

    capture-mode-polish: Correct confine bounds for widget
    This cl fixed the issue that the widget may not have the correct
    bounds on window bounds been transformed by always use the selected
    window bounds.
    
    Fixed: b/295592190
    Test: Manual
    Change-Id: I5ea14b7b434d586f63623c033f28dacb17370fce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4779364
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184861}

--- ash/capture_mode/capture_mode_session.cc
+++ ash/capture_mode/capture_mode_session.cc
@@ -1558,7 +1558,7 @@ void CaptureModeSession::PaintCaptureRegion(gfx::Canvas* canvas) {
       break;
 
     case CaptureModeSource::kWindow:
-      region = GetSelectedWindowBounds();
+      region = GetSelectedWindowTargetBounds();
       break;
 
     case CaptureModeSource::kRegion:
@@ -2414,7 +2414,7 @@ gfx::Rect CaptureModeSession::CalculateCaptureLabelWidgetBounds() {
 
   gfx::Rect bounds(current_root_->bounds());
   const gfx::Rect capture_region = controller_->user_capture_region();
-  const gfx::Rect window_bounds = GetSelectedWindowBounds();
+  const gfx::Rect window_bounds = GetSelectedWindowTargetBounds();
   const CaptureModeSource source = controller_->source();
 
   // For fullscreen mode, the capture label is placed in the middle of the
