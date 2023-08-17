31b35af825ae602f5c4e94c6f67b1c8514605f62
Anders Hartvoll Ruud <andruud@chromium.org>
2023-08-17 10:03:01 +0000

    [@scope] Place constructed stylesheets directly in completed_clients_
    Currently they're placed in loading_clients_, and never leave
    (because they don't actually load).
    
    There is no test in this CL, because I don't think this is
    an observable change. However, it will make the next CL (CL:4778499)
    more reasonable, since we can avoiding looking at loading_clients_
    in StyleSheetContents::HasOwnerParentNode.
    
    Bug: 1379844
    Change-Id: I93a78da38a3986a6433c7e8b65de01b1b42e82cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4777246
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184629}

--- third_party/blink/renderer/core/css/style_sheet_contents.cc
+++ third_party/blink/renderer/core/css/style_sheet_contents.cc
@@ -524,9 +524,7 @@ void StyleSheetContents::CheckLoaded() {
     if (loading_clients[i]->LoadCompleted()) {
       continue;
     }
-    if (loading_clients[i]->IsConstructed()) {
-      continue;
-    }
+    DCHECK(!loading_clients[i]->IsConstructed());
 
     // sheetLoaded might be invoked after its owner node is removed from
     // document.
@@ -701,7 +699,17 @@ void StyleSheetContents::RegisterClient(CSSStyleSheet* sheet) {
       has_single_owner_document_ = false;
     }
   }
-  loading_clients_.insert(sheet);
+
+  if (sheet->IsConstructed()) {
+    // Constructed stylesheets don't need loading. Note that @import is ignored
+    // in both CSSStyleSheet.replaceSync and CSSStyleSheet.replace.
+    //
+    // https://drafts.csswg.org/cssom/#dom-cssstylesheet-replacesync
+    // https://drafts.csswg.org/cssom/#dom-cssstylesheet-replace
+    completed_clients_.insert(sheet);
+  } else {
+    loading_clients_.insert(sheet);
+  }
 }
 
 void StyleSheetContents::UnregisterClient(CSSStyleSheet* sheet) {
