dc2e69d4ec3f56b32398ae58e2c340be49b2430c
Jeongwoo Park <skeksk91@gmail.com>
2023-08-17 13:38:49 +0000

    Remove unused header
    Change-Id: I4c17051fd6422d0ac05c643a783aad6fd8306965
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4784327
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Jeongwoo Park <skeksk91@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1184682}

--- services/network/restricted_cookie_manager.cc
+++ services/network/restricted_cookie_manager.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/features.h"
 #include "services/network/public/mojom/cookie_manager.mojom.h"
 #include "services/network/public/mojom/network_context.mojom.h"
-#include "services/network/public/mojom/network_service.mojom.h"
 #include "url/gurl.h"
 
 namespace network {
