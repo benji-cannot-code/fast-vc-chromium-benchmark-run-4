66056af224ed6fd171045982a581f66ce78ad525
Chuong Ho <hdchuong@google.com>
2023-08-17 09:49:02 +0000

    add skeleton for switch
    This CL adds a skeleton for the centralized switch component.
    This CL also adds an input check for the demonstration of the API.
    
    Bug:b/295440693
    
    Change-Id: I5aa5dfb4222f8bf4fe00171336400f12ac49cd79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4776012
    Commit-Queue: Chuong Ho <hdchuong@google.com>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184624}

---
+++ chrome/browser/ash/input_method/editor_switch.h
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_INPUT_METHOD_EDITOR_SWITCH_H_
+#define CHROME_BROWSER_ASH_INPUT_METHOD_EDITOR_SWITCH_H_
+
+#include "ui/base/ime/ash/text_input_method.h"
+
+namespace ash::input_method {
+
+// EditorSwitch is the centralized switch that decides whether the feature is
+// available for use, and if it is available, further decides whether the
+// feature should be popped up given a particular input context.
+class EditorSwitch {
+ public:
+  EditorSwitch();
+  EditorSwitch(const EditorSwitch&) = delete;
+  EditorSwitch& operator=(const EditorSwitch&) = delete;
+  ~EditorSwitch();
+
+  // Determines if the feature trace is ever allowed to be visible.
+  bool IsAllowedForUse();
+
+  // Determines if the feature can be triggered from an input context. If it is
+  // not allowed for use, then returns false.
+  bool CanBeTriggered();
+
+  // Handles the change in input context.
+  void OnInputContextUpdated(
+      const TextInputMethod::InputContext& input_context);
+
+ private:
+  void UpdateTriggerableCache();
+
+  bool is_allowed_for_use_;
+  bool can_be_triggered_;
+
+  ui::TextInputType input_type_ = ui::TEXT_INPUT_TYPE_NONE;
+};
+
+}  // namespace ash::input_method
+
+#endif  // CHROME_BROWSER_ASH_INPUT_METHOD_EDITOR_SWITCH_H_
