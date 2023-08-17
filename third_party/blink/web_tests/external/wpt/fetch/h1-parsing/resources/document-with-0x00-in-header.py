c320b7a3929fe3bb7690d5c20107b2fee84dff21
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2023-08-17 16:27:07 +0000

    Import wpt@555c330701cd1b45deb08e0681c62792183e1df0
    Using wpt-import in Chromium d63430737754316a64e818758570ea1bc88c544f.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    japhet@chromium.org:
      external/wpt/fetch
    rego@igalia.com:
      external/wpt/css/selectors
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: Ia410126d8ba487aa95de238c9398eab1dc6dc381
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4789087
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1184759}

--- third_party/blink/web_tests/external/wpt/fetch/h1-parsing/resources/document-with-0x00-in-header.py
+++ third_party/blink/web_tests/external/wpt/fetch/h1-parsing/resources/document-with-0x00-in-header.py
@@ -2,4 +2,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 def main(request, response):
     response.headers.set(b"Content-Type", b"text/html")
     response.headers.set(b"Custom", b"\0")
-    return b"<!doctype html><b>This is a document.</b>"
+    return b"<!doctype html><script>top.frameLoaded=true</script><b>This is a document.</b>"
