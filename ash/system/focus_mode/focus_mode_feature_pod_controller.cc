e94931cd8df00c18f0a3c27e4cb37145ac8e83e5
Richard Chui <richui@chromium.org>
2023-08-17 18:20:03 +0000

    focus-mode: Add controller getter and move strings
    - Add a getter for the controller for convenience
    - Move some temporary strings to ash_strings.grd
    
    BUG=b/288975135
    
    Change-Id: Ia25803d02281bd6138a8923fe960183364b6022d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4786893
    Commit-Queue: Richard Chui <richui@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184807}

--- ash/system/focus_mode/focus_mode_feature_pod_controller.cc
+++ ash/system/focus_mode/focus_mode_feature_pod_controller.cc
@@ -9,32 +9,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/quick_settings_catalogs.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shell.h"
+#include "ash/strings/grit/ash_strings.h"
 #include "ash/system/focus_mode/focus_mode_controller.h"
 #include "ash/system/unified/feature_pod_button.h"
 #include "ash/system/unified/feature_tile.h"
 #include "ash/system/unified/unified_system_tray_controller.h"
 #include "base/check.h"
 #include "base/functional/bind.h"
+#include "base/i18n/number_formatting.h"
 #include "base/i18n/time_formatting.h"
+#include "ui/base/l10n/l10n_util.h"
 
 namespace ash {
-
-namespace {
-
-// TODO(b/288975135): replace these placeholders with string ids later.
-constexpr const char16_t kLabelText[] = u"Focus Mode";
-constexpr const char16_t kSubLabelText[] = u"30 mins";
-
-}  // namespace
 
 FocusModeFeaturePodController::FocusModeFeaturePodController(
     UnifiedSystemTrayController* tray_controller)
     : tray_controller_(tray_controller) {
-  Shell::Get()->focus_mode_controller()->AddObserver(this);
+  FocusModeController::Get()->AddObserver(this);
 }
 
 FocusModeFeaturePodController::~FocusModeFeaturePodController() {
-  Shell::Get()->focus_mode_controller()->RemoveObserver(this);
+  FocusModeController::Get()->RemoveObserver(this);
 }
 
 FeaturePodButton* FocusModeFeaturePodController::CreateButton() {
@@ -45,9 +40,10 @@ FeaturePodButton* FocusModeFeaturePodController::CreateButton() {
   button_ = button.get();
   button_->ShowDetailedViewArrow();
   button_->SetVectorIcon(kCaptureModeIcon);
-  button_->SetLabel(kLabelText);
-  button_->icon_button()->SetTooltipText(kLabelText);
-  OnFocusModeChanged(Shell::Get()->focus_mode_controller()->in_focus_session());
+  button_->SetLabel(l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_FOCUS_MODE));
+  button_->icon_button()->SetTooltipText(
+      l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_FOCUS_MODE));
+  OnFocusModeChanged(FocusModeController::Get()->in_focus_session());
   return button.release();
 }
 
@@ -64,10 +60,12 @@ std::unique_ptr<FeatureTile> FocusModeFeaturePodController::CreateTile(
                           weak_factory_.GetWeakPtr()));
   tile_->CreateDecorativeDrillInArrow();
   tile_->SetVectorIcon(kCaptureModeIcon);
-  tile_->SetLabel(kLabelText);
-  tile_->SetIconButtonTooltipText(kLabelText);
-  tile_->SetTooltipText(kLabelText);
-  OnFocusModeChanged(Shell::Get()->focus_mode_controller()->in_focus_session());
+  tile_->SetLabel(l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_FOCUS_MODE));
+  tile_->SetIconButtonTooltipText(
+      l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_FOCUS_MODE));
+  tile_->SetTooltipText(
+      l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_FOCUS_MODE));
+  OnFocusModeChanged(FocusModeController::Get()->in_focus_session());
   return tile;
 }
 
@@ -76,7 +74,7 @@ QsFeatureCatalogName FocusModeFeaturePodController::GetCatalogName() {
 }
 
 void FocusModeFeaturePodController::OnIconPressed() {
-  Shell::Get()->focus_mode_controller()->ToggleFocusMode();
+  FocusModeController::Get()->ToggleFocusMode();
 }
 
 void FocusModeFeaturePodController::OnLabelPressed() {
@@ -101,14 +99,12 @@ void FocusModeFeaturePodController::OnTimerTick() {
 }
 
 void FocusModeFeaturePodController::UpdateUI() {
-  FocusModeController* focus_mode_controller =
-      Shell::Get()->focus_mode_controller();
-  CHECK(focus_mode_controller);
+  auto* controller = FocusModeController::Get();
+  CHECK(controller);
 
   std::u16string sub_text;
-  if (focus_mode_controller->in_focus_session()) {
-    base::TimeDelta time_remaining =
-        focus_mode_controller->end_time() - base::Time::Now();
+  if (controller->in_focus_session()) {
+    base::TimeDelta time_remaining = controller->end_time() - base::Time::Now();
     std::u16string remaining_time;
     if (!base::TimeDurationFormatWithSeconds(
             time_remaining, base::DURATION_WIDTH_SHORT, &remaining_time)) {
@@ -117,7 +113,9 @@ void FocusModeFeaturePodController::UpdateUI() {
     }
     sub_text = remaining_time;
   } else {
-    sub_text = kSubLabelText;
+    sub_text = l10n_util::GetStringFUTF16(
+        IDS_ASH_STATUS_TRAY_FOCUS_MODE_TIME_SUBLABEL,
+        base::FormatNumber(controller->session_duration().InMinutes()));
   }
 
   if (features::IsQsRevampEnabled()) {
