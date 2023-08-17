fb895ee295508fab432c499845061bf85c990247
Jong Ahn <jongahn@google.com>
2023-08-17 20:06:52 +0000

    Add KeyDataProvider interface to provide project keys.
    This refactor will be used to decouple the requirement for a user to
    login to record and use StructuredMetrics in Chromium by using the
    device key.
    
    Bug: b/290096302
    Change-Id: I160281df9f9d4c37da56507c652daf63adcac6a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4662392
    Reviewed-by: Hirthanan Subenderan <hirthanan@google.com>
    Commit-Queue: Jong Ahn <jongahn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184868}

--- chrome/browser/metrics/structured/test/structured_metrics_mixin.h
+++ chrome/browser/metrics/structured/test/structured_metrics_mixin.h
@@ -31,6 +31,7 @@ class StructuredMetricsMixin : public InProcessBrowserTestMixin {
 
   // InProcessBrowserTestMixin:
   void SetUpCommandLine(base::CommandLine* command_line) override;
+  void SetUpOnMainThread() override;
 
  private:
   std::unique_ptr<TestStructuredMetricsProvider>
