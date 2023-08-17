87cbf83f1ef72de37266c4355336ad06a2ba8c68
Nina Satragno <nsatragno@chromium.org>
2023-08-17 15:45:40 +0000

    [webauthn] i18n new UI strings
    Replace string placeholders in the new WebAuthn modal & conditional UI
    with internationalized strings that have the UXW seal of approval.
    
    UXW document: go/passkey-metadata-sync-uxw
    
    Bug: 1459273
    Change-Id: I21b6c8dd1a01bb9c046182a60be68ec973acc641
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4781349
    Commit-Queue: Adam Langley <agl@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184735}

--- chrome/browser/ui/webauthn/sheet_models_unittest.cc
+++ chrome/browser/ui/webauthn/sheet_models_unittest.cc
@@ -9,9 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/webauthn/authenticator_request_dialog_model.h"
 #include "chrome/browser/webauthn/authenticator_transport.h"
+#include "chrome/grit/generated_resources.h"
 #include "device/fido/fido_types.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/base/l10n/l10n_util.h"
 
 namespace {
 
@@ -130,7 +132,9 @@ TEST_F(AuthenticatorMultiSourcePickerSheetModelTest, GPMPasskeysOnly) {
   AuthenticatorMultiSourcePickerSheetModel model(&dialog_model);
   EXPECT_THAT(model.primary_passkey_indices(), testing::ElementsAre(0, 1));
   EXPECT_THAT(model.secondary_passkey_indices(), testing::ElementsAre(2));
-  EXPECT_EQ(model.primary_passkeys_label(), u"From \"pixel 7\" (UNTRANSLATED)");
+  EXPECT_EQ(
+      model.primary_passkeys_label(),
+      l10n_util::GetStringFUTF16(IDS_WEBAUTHN_FROM_PHONE_LABEL, kPhoneName));
 }
 
 TEST_F(AuthenticatorMultiSourcePickerSheetModelTest, GPMAndLocalPasskeys) {
@@ -153,7 +157,8 @@ TEST_F(AuthenticatorMultiSourcePickerSheetModelTest, GPMAndLocalPasskeys) {
   AuthenticatorMultiSourcePickerSheetModel model(&dialog_model);
   EXPECT_THAT(model.primary_passkey_indices(), testing::ElementsAre(1));
   EXPECT_THAT(model.secondary_passkey_indices(), testing::ElementsAre(0, 2));
-  EXPECT_EQ(model.primary_passkeys_label(), u"From this device (UNTRANSLATED)");
+  EXPECT_EQ(model.primary_passkeys_label(),
+            l10n_util::GetStringUTF16(IDS_WEBAUTHN_THIS_DEVICE_LABEL));
 }
 
 TEST_F(AuthenticatorMultiSourcePickerSheetModelTest, NoDiscoveredPasskeys) {
