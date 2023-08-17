c8e887fcb7d44433f404d2aff1d228e1e0cb28b0
Bastian Kersting <bkersting@google.com>
2023-08-17 09:45:39 +0000

    [Telemetry] Add MemoryRoutineFinished info and event
    This change adds the MemoryRoutineFinished info and event to
    the Chrome Extension layer.
    
    Bug: b/286031590, b/294819343
    Test: browser_tests
    --gtest_filter="*TelemetryExtensionDiagnosticRoutineObserverBrowserTest*"
    
    NO_IFTTT=Documentation for this change will be done in one batch
    
    Change-Id: I65566634ed6068fa1648a7a39a713f4159b57fe2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4762694
    Reviewed-by: Ethan Cheng <yycheng@google.com>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Cr-Commit-Position: refs/heads/main@{#1184623}

--- chrome/browser/chromeos/extensions/telemetry/api/routines/diagnostic_routine_observation_browsertest.cc
+++ chrome/browser/chromeos/extensions/telemetry/api/routines/diagnostic_routine_observation_browsertest.cc
@@ -147,6 +147,26 @@ IN_PROC_BROWSER_TEST_F(TelemetryExtensionDiagnosticRoutineObserverBrowserTest,
   )");
 }
 
+IN_PROC_BROWSER_TEST_F(
+    TelemetryExtensionDiagnosticRoutineObserverBrowserTest,
+    CanObserveOnMemoryRoutineFinishedWithoutFeatureFlagFail) {
+  CreateExtensionAndRunServiceWorker(R"(
+    chrome.test.runTests([
+      function canObserveOnMemoryRoutineFinishedFail() {
+        chrome.test.assertThrows(() => {
+          chrome.os.diagnostics.onMemoryRoutineFinished.addListener((event) => {
+            // unreachable
+          });
+        }, [],
+          'Cannot read properties of undefined (reading \'addListener\')'
+        );
+
+        chrome.test.succeed();
+      }
+    ]);
+  )");
+}
+
 class PendingApprovalTelemetryExtensionDiagnosticRoutineObserverBrowserTest
     : public TelemetryExtensionDiagnosticRoutineObserverBrowserTest {
  public:
@@ -252,6 +272,69 @@ IN_PROC_BROWSER_TEST_F(
             percentage: 50,
             reason: "waiting_to_be_scheduled",
             uuid: "%s",
+          });
+
+          chrome.test.succeed();
+        });
+      }
+    ]);
+  )",
+                         uuid_.AsLowercaseString().c_str()));
+}
+
+IN_PROC_BROWSER_TEST_F(
+    PendingApprovalTelemetryExtensionDiagnosticRoutineObserverBrowserTest,
+    CanObserveOnMemoryRoutineFinished) {
+  RegisterEventObserver(
+      api::os_diagnostics::OnMemoryRoutineFinished::kEventName,
+      base::BindLambdaForTesting([this] {
+        auto memtester_result =
+            crosapi::TelemetryDiagnosticMemtesterResult::New();
+        memtester_result->passed_items = {
+            crosapi::TelemetryDiagnosticMemtesterTestItemEnum::kCompareDIV,
+            crosapi::TelemetryDiagnosticMemtesterTestItemEnum::kCompareMUL};
+        memtester_result->failed_items = {
+            crosapi::TelemetryDiagnosticMemtesterTestItemEnum::kCompareAND,
+            crosapi::TelemetryDiagnosticMemtesterTestItemEnum::kCompareSUB};
+
+        auto memory_detail =
+            crosapi::TelemetryDiagnosticMemoryRoutineDetail::New();
+        memory_detail->bytes_tested = 500;
+        memory_detail->result = std::move(memtester_result);
+
+        auto finished_detail =
+            crosapi::TelemetryDiagnosticRoutineDetail::NewMemory(
+                std::move(memory_detail));
+
+        auto finished_state = crosapi::TelemetryDiagnosticRoutineState::New();
+        finished_state->state_union =
+            crosapi::TelemetryDiagnosticRoutineStateUnion::NewFinished(
+                crosapi::TelemetryDiagnosticRoutineStateFinished::New(
+                    /*has_passed=*/true, std::move(finished_detail)));
+        finished_state->percentage = 100;
+
+        remote_->OnRoutineStateChange(std::move(finished_state));
+      }));
+
+  CreateExtensionAndRunServiceWorker(
+      base::StringPrintf(R"(
+    chrome.test.runTests([
+      async function canObserveOnMemoryRoutineFinished() {
+        chrome.os.diagnostics.onMemoryRoutineFinished.addListener((event) => {
+          chrome.test.assertEq(event, {
+            "bytesTested": 500,
+            "has_passed": true,
+            "result": {
+              "failed_items": [
+                "compare_and",
+                "compare_sub"
+              ],
+              "passed_items": [
+                "compare_div",
+                "compare_mul"
+              ]
+            },
+            "uuid":"%s"
           });
 
           chrome.test.succeed();
