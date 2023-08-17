590fb25aa9854ae5bd5cb5a17b4d0dcccdf8b8ee
Anastasia Helfinstein <anastasi@google.com>
2023-08-17 18:38:55 +0000

    [Switch Access] Separate init() and start()
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: b:261227533
    Test: cq
    Change-Id: If2579970c933511541c6f5b1c0cf3ebefea0dd07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4286141
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1184821}

--- chrome/browser/resources/chromeos/accessibility/switch_access/switch_access.js
+++ chrome/browser/resources/chromeos/accessibility/switch_access/switch_access.js
@@ -18,14 +18,20 @@ const EventType = chrome.automation.EventType;
 const FindParams = chrome.automation.FindParams;
 const RoleType = chrome.automation.RoleType;
 
+let readyCallback;
+const readyPromise = new Promise(resolve => readyCallback = resolve);
+
 /**
  * The top-level class for the Switch Access accessibility feature. Handles
  * initialization and small matters that don't fit anywhere else in the
  * codebase.
  */
 export class SwitchAccess {
-  static async initialize() {
+  static async init() {
     await Flags.init();
+    if (SwitchAccess.instance) {
+      throw new Error('Cannot create two SwitchAccess.instances');
+    }
     SwitchAccess.instance = new SwitchAccess();
 
     const desktop = await AsyncUtil.getDesktop();
@@ -37,8 +43,18 @@ export class SwitchAccess {
     Navigator.initializeSingletonInstances(desktop);
 
     SwitchAccess.commands = new SACommands();
-    KeyboardRootNode.startWatchingVisibility();
     PreferenceManager.initialize();
+  }
+
+  /** Starts Switch Access behavior. */
+  static start() {
+    KeyboardRootNode.startWatchingVisibility();
+    readyCallback();
+  }
+
+  /** @return {!Promise} */
+  static async ready() {
+    return readyPromise;
   }
 
   /**
@@ -158,3 +174,6 @@ export class SwitchAccess {
     return new Error(errorString);
   }
 }
+
+/** @type {SwitchAccess} */
+SwitchAccess.instance;
