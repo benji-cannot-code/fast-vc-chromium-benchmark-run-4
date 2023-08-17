5c029468550460a252caaa638087904d9d40745e
Lei Zhang <thestig@chromium.org>
2023-08-17 19:06:26 +0000

    Remove the supports_alpha_blend parameter in printing Mojom files
    It is not used for anything. https://crrev.com/292501 removed the last
    use.
    
    Change-Id: I572ee2f1f63a9c1d2edd06fe412fd8a371e508d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4787730
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184835}

--- printing/mojom/printing_context_mojom_traits_unittest.cc
+++ printing/mojom/printing_context_mojom_traits_unittest.cc
@@ -141,9 +141,6 @@ constexpr bool kPrintSettingsRasterizePdf2 = false;
 constexpr bool kPrintSettingsLandscape1 = false;
 constexpr bool kPrintSettingsLandscape2 = true;
 
-constexpr bool kPrintSettingsSupportsAlphaBlend1 = false;
-constexpr bool kPrintSettingsSupportsAlphaBlend2 = true;
-
 #if BUILDFLAG(IS_WIN)
 constexpr mojom::PrinterLanguageType kPrintSettingsPrinterLanguageType1 =
     mojom::PrinterLanguageType::kTextOnly;
@@ -207,7 +204,6 @@ PrintSettings GenerateSamplePrintSettingsDefaultMargins() {
   settings.set_scale_factor(kPrintSettingsScaleFactor1);
   settings.set_rasterize_pdf(kPrintSettingsRasterizePdf1);
   settings.SetOrientation(kPrintSettingsLandscape1);
-  settings.set_supports_alpha_blend(kPrintSettingsSupportsAlphaBlend1);
 
 #if BUILDFLAG(IS_WIN)
   settings.set_printer_language_type(kPrintSettingsPrinterLanguageType1);
@@ -239,7 +235,6 @@ PrintSettings GenerateSamplePrintSettingsCustomMarginsWithParams(
   settings.set_scale_factor(kPrintSettingsScaleFactor2);
   settings.set_rasterize_pdf(kPrintSettingsRasterizePdf2);
   settings.SetOrientation(kPrintSettingsLandscape2);
-  settings.set_supports_alpha_blend(kPrintSettingsSupportsAlphaBlend2);
 
   settings.SetCustomMargins(kPrintSettingsCustomMarginsInPoints);
 
@@ -498,7 +493,6 @@ TEST(PrintingContextMojomTraitsTest,
   EXPECT_EQ(output.scale_factor(), kPrintSettingsScaleFactor1);
   EXPECT_EQ(output.rasterize_pdf(), kPrintSettingsRasterizePdf1);
   EXPECT_EQ(output.landscape(), kPrintSettingsLandscape1);
-  EXPECT_EQ(output.supports_alpha_blend(), kPrintSettingsSupportsAlphaBlend1);
 
 #if BUILDFLAG(IS_WIN)
   EXPECT_EQ(output.printer_language_type(), kPrintSettingsPrinterLanguageType1);
@@ -554,7 +548,6 @@ TEST(PrintingContextMojomTraitsTest,
   EXPECT_EQ(output.scale_factor(), kPrintSettingsScaleFactor2);
   EXPECT_EQ(output.rasterize_pdf(), kPrintSettingsRasterizePdf2);
   EXPECT_EQ(output.landscape(), kPrintSettingsLandscape2);
-  EXPECT_EQ(output.supports_alpha_blend(), kPrintSettingsSupportsAlphaBlend2);
 
 #if BUILDFLAG(IS_WIN)
   EXPECT_EQ(output.printer_language_type(), kPrintSettingsPrinterLanguageType2);
