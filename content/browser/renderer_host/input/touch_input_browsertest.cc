b2b1a23773c88b89ea09c6e9a07339b05598f3bb
Omar Elmekkawy <mekk@google.com>
2023-08-17 14:53:40 +0000

    Add field trial configs for input fixing bug finch experiment
    Adding field trial config and handling changed test expectations from the old kNotConsumed instead of kNotConsumedBlocking.
    
    Bug: b/292051312
    Change-Id: I24ecafa5939bd11d4ae77a9b5b14f604174bf1f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4783347
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Omar Elmekkawy <mekk@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184717}

--- content/browser/renderer_host/input/touch_input_browsertest.cc
+++ content/browser/renderer_host/input/touch_input_browsertest.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/content_browser_test_utils.h"
 #include "content/public/test/hit_test_region_observer.h"
 #include "content/shell/browser/shell.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/input/synthetic_web_input_event_builders.h"
 #include "third_party/blink/public/common/input/web_input_event.h"
 #include "ui/latency/latency_info.h"
@@ -71,6 +72,13 @@ const char kTouchEventDataURL[] =
     "    third.ontouchstart = e => e.preventDefault();"
     "  }"
     "</script>";
+
+blink::mojom::InputEventResultState ExpectedNotConsumedOrNotConsumedBlocking() {
+  return base::FeatureList::IsEnabled(
+             blink::features::kFixGestureScrollQueuingBug)
+             ? blink::mojom::InputEventResultState::kNotConsumedBlocking
+             : blink::mojom::InputEventResultState::kNotConsumed;
+}
 
 }  // namespace
 
@@ -155,7 +163,7 @@ IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, TouchNoHandler) {
       base::FeatureList::IsEnabled(
           blink::features::kDroppedTouchSequenceIncludesTouchEnd)
           ? blink::mojom::InputEventResultState::kNoConsumerExists
-          : blink::mojom::InputEventResultState::kNotConsumed;
+          : ExpectedNotConsumedOrNotConsumedBlocking();
   EXPECT_EQ(expected_touchend_result, filter->WaitForAck());
 }
 
@@ -168,8 +176,7 @@ IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, TouchStartNoConsume) {
   touch.PressPoint(125, 25);
   auto filter = AddFilter(WebInputEvent::Type::kTouchStart);
   SendTouchEvent(&touch);
-  EXPECT_EQ(blink::mojom::InputEventResultState::kNotConsumed,
-            filter->WaitForAck());
+  EXPECT_EQ(ExpectedNotConsumedOrNotConsumedBlocking(), filter->WaitForAck());
 
   // Even though there is no touch-end handler there, the touch-end is still
   // dispatched for state consistency in downstream event-path.  That event
@@ -177,8 +184,7 @@ IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, TouchStartNoConsume) {
   filter = AddFilter(WebInputEvent::Type::kTouchEnd);
   touch.ReleasePoint(0);
   SendTouchEvent(&touch);
-  EXPECT_EQ(blink::mojom::InputEventResultState::kNotConsumed,
-            filter->WaitForAck());
+  EXPECT_EQ(ExpectedNotConsumedOrNotConsumedBlocking(), filter->WaitForAck());
 }
 
 IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, TouchStartConsume) {
@@ -199,8 +205,7 @@ IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, TouchStartConsume) {
   touch.ReleasePoint(0);
   filter = AddFilter(WebInputEvent::Type::kTouchEnd);
   SendTouchEvent(&touch);
-  EXPECT_EQ(blink::mojom::InputEventResultState::kNotConsumed,
-            filter->WaitForAck());
+  EXPECT_EQ(ExpectedNotConsumedOrNotConsumedBlocking(), filter->WaitForAck());
 }
 
 IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, MultiPointTouchPress) {
