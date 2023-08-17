d405a124b7345eb3aa0a57e653a18d6aec6223eb
Jose Dapena Paz <jdapena@igalia.com>
2023-08-17 15:19:24 +0000

    IWYU: usage of nan() in cstring_builder_pa_unittests.cc requires including cmath
    Fix build error with libstdc++:
        ../../base/allocator/partition_allocator/partition_alloc_base/strings/cstring_builder_pa_unittest.cc: In member function ‘virtual void partition_alloc::internal::base::strings::CStringBuilderTestPA_FloatingPoint_Test::TestBody()’:
        ../../base/allocator/partition_allocator/partition_alloc_base/strings/cstring_builder_pa_unittest.cc:98:15: error: ‘nan’ was not declared in this scope
           98 |   builder9 << nan("");
              |               ^~~
    
    Bug: 957519
    Change-Id: I2383db46be24c0bed06e4b0c6a79d628d166b635
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4790501
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1184725}

--- base/allocator/partition_allocator/partition_alloc_base/strings/cstring_builder_pa_unittest.cc
+++ base/allocator/partition_allocator/partition_alloc_base/strings/cstring_builder_pa_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/allocator/partition_allocator/partition_alloc_base/strings/cstring_builder.h"
 
+#include <cmath>
+
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
