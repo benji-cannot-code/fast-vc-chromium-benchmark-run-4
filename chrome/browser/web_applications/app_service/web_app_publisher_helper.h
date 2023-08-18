c9595f2798cb1db7fc1ceeddcc2aff82a243b1bd
Glen Robertson <glenrob@chromium.org>
2023-08-18 07:57:09 +0000

    Add test for StopApp and make PauseApp use same "close app" logic.
    - Make PauseApp, UnpauseApp, IsPaused, PausedApps CrOS-only (they were
      used in CrOS only). StopApp is already CrOS-only and is seemingly used
      in both Ash (for SWAs) and Lacros (for PWAs), at least in tests.
    - Make PauseApp and StopApp use BrowserAppInstanceTracker if available
      (when CrosApi is enabled) and otherwise just close app windows
      directly using WebAppUiManager. This makes their behaviour
      consistent and can be simplified once CrosApi is always enabled.
    - Fix includes in lacros_web_apps_controller_browsertest.cc per IWYU.
    - Remove the need for FRIEND_TEST_ALL_PREFIXES by upcasting instead.
    - Add a few TestFutures when I touched code nearby.
    
    
    Change-Id: I1b95e000abec37ed43d40fef2a224979fdb590bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4780368
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185067}

--- chrome/browser/web_applications/app_service/web_app_publisher_helper.h
+++ chrome/browser/web_applications/app_service/web_app_publisher_helper.h
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/app_icon/icon_effects.h"
 #include "chrome/browser/apps/app_service/app_icon/icon_key_util.h"
 #include "chrome/browser/apps/app_service/launch_result_type.h"
-#include "chrome/browser/apps/app_service/paused_apps.h"
 #include "chrome/browser/web_applications/mojom/user_display_mode.mojom.h"
 #include "chrome/browser/web_applications/web_app_constants.h"
 #include "chrome/browser/web_applications/web_app_id.h"
@@ -51,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_CHROMEOS)
 #include "chrome/browser/apps/app_service/app_notifications.h"
 #include "chrome/browser/apps/app_service/media_requests.h"
+#include "chrome/browser/apps/app_service/paused_apps.h"
 #include "chrome/browser/badging/badge_manager_delegate.h"
 #include "chrome/browser/media/webrtc/media_stream_capture_indicator.h"
 #include "chrome/browser/notifications/notification_common.h"
@@ -184,11 +184,13 @@ class WebAppPublisherHelper : public WebAppRegistrarObserver,
 
   void SetIconEffect(const std::string& app_id);
 
+#if BUILDFLAG(IS_CHROMEOS)
   void PauseApp(const std::string& app_id);
 
   void UnpauseApp(const std::string& app_id);
 
   bool IsPaused(const std::string& app_id);
+#endif  // BUILDFLAG(IS_CHROMEOS)
 
   void LoadIcon(const std::string& app_id,
                 apps::IconType icon_type,
@@ -438,9 +440,9 @@ class WebAppPublisherHelper : public WebAppRegistrarObserver,
 
   apps_util::IncrementingIconKeyFactory icon_key_factory_;
 
+#if BUILDFLAG(IS_CHROMEOS)
   apps::PausedApps paused_apps_;
 
-#if BUILDFLAG(IS_CHROMEOS)
   base::ScopedObservation<NotificationDisplayService,
                           NotificationDisplayService::Observer>
       notification_display_service_{this};
@@ -454,7 +456,7 @@ class WebAppPublisherHelper : public WebAppRegistrarObserver,
       media_indicator_observation_{this};
 
   apps::MediaRequests media_requests_;
-#endif
+#endif  // BUILDFLAG(IS_CHROMEOS)
 
   std::map<std::string, WebAppShortcutsMenuItemInfo> shortcut_id_map_;
   ShortcutId::Generator shortcut_id_generator_;
