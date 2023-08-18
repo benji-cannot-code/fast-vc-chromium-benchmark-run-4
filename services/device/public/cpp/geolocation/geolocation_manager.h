791258358926223b6d958ef29abfa7f6faba498a
Jan Lanik <janlanik@google.com>
2023-08-18 07:41:33 +0000

    Use buildflag for os geolocation permissions
    Introduces macros that can be used to write code specific for platforms
    that suppor os level geolocation premissions.
    
    Bug: b/274404854
    Change-Id: I9a6dd65fc562d9e25a302d33802038aa59d24fc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4787489
    Commit-Queue: Jan Láník <janlanik@google.com>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185063}

--- services/device/public/cpp/geolocation/geolocation_manager.h
+++ services/device/public/cpp/geolocation/geolocation_manager.h
@@ -12,8 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/component_export.h"
 #include "build/build_config.h"
 #include "build/buildflag.h"
+#include "services/device/public/cpp/geolocation/buildflags.h"
 
-#if BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_APPLE) || BUILDFLAG(OS_LEVEL_GEOLOCATION_PERMISSION_SUPPORTED)
 
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list_threadsafe.h"
@@ -39,11 +40,12 @@ class COMPONENT_EXPORT(GEOLOCATION) GeolocationManager {
   void TrackGeolocationRelinquished();
   void RequestSystemPermission();
 
-#if !BUILDFLAG(IS_APPLE) && !BUILDFLAG(IS_CHROMEOS)
-// Default empty implementation of Geolocation Manager. It is used on operation
-// systems for which we don't support system-level geolocation. A separate class
-// (as opposed to nullptr) makes sure no unsupported calls are made in such
-// context.
+#if !BUILDFLAG(IS_APPLE) && \
+    !BUILDFLAG(OS_LEVEL_GEOLOCATION_PERMISSION_SUPPORTED)
+  // Default empty implementation of Geolocation Manager. It is used on
+  // operation systems for which we don't support system-level geolocation. A
+  // separate class (as opposed to nullptr) makes sure no unsupported calls are
+  // made in such context.
 };  // class GeolocationManager
 
 #else
