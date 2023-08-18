7b4ceb943f992ac0d76a1d65d773a51bebd5f6ef
Jason Thai <jasontt@chromium.org>
2023-08-18 19:13:55 +0000

    wallpaper: rm unneeded friend class / tests
    BUG=none
    TEST=ash_unittests --gtest_filter="WallpaperControllerTest.*"
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I02c778eb1ce4911e60bd6a4cbbc27678398ba824
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4791623
    Reviewed-by: Erica Lee <ericamlee@google.com>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185325}

--- ash/wallpaper/wallpaper_controller_impl.h
+++ ash/wallpaper/wallpaper_controller_impl.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/overview_observer.h"
 #include "base/containers/flat_map.h"
 #include "base/files/file_path.h"
-#include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/observer_list.h"
 #include "base/scoped_observation.h"
@@ -417,12 +416,8 @@ class ASH_EXPORT WallpaperControllerImpl
   }
 
  private:
-  FRIEND_TEST_ALL_PREFIXES(WallpaperControllerTest, BasicReparenting);
-  FRIEND_TEST_ALL_PREFIXES(WallpaperControllerTest,
-                           WallpaperMovementDuringUnlock);
   friend class WallpaperControllerTestBase;
   friend class WallpaperControllerTestApi;
-  friend class KeyboardBacklightColorControllerTest;
 
   enum WallpaperMode { WALLPAPER_NONE, WALLPAPER_IMAGE };
 
