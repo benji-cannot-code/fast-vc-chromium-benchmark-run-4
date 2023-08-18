fda1bd3a3c8e2079827f47a103148b22e9aff9db
Yue Zhang <yuezhanggg@chromium.org>
2023-08-18 17:22:58 +0000

    [Discounts] Add discount chip in Quest 2.0
    This CL adds the discount chip to Quest 2.0. The chip is never visible
    now as the plumbing for the data has not been created yet. For demo
    please see https://docs.google.com/document/d/10jasHx-TJgMpklldmIPGkzcJXlr74j3Sp7xsUDJZ46Y/edit#heading=h.qmrbhtpvzy0g.
    
    Bug: b/295065105
    Change-Id: I4204e7e62a82aaca9d7e82c8012dbbaaf93e4c88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4786568
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185266}

--- chrome/browser/resources/new_tab_page/modules/v2/history_clusters/visit_tile.ts
+++ chrome/browser/resources/new_tab_page/modules/v2/history_clusters/visit_tile.ts
@@ -65,12 +65,19 @@ export class VisitTileModuleElement extends I18nMixin
         type: String,
         reflectToAttribute: true,
       },
+
+      hasDiscount: {
+        type: Boolean,
+        value: false,
+        reflectToAttribute: true,
+      },
     };
   }
 
   format: string;
   imagesEnabled: boolean;
   visit: URLVisit;
+  hasDiscount: boolean;
   private imageUrl_: Url|null;
 
   hasImageUrl(): boolean {
