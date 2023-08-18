a9b5d3101a15bd01738ba07ee5c541b8d95ec898
Bruno Braga <brunobraga@google.com>
2023-08-18 10:37:19 +0000

    [autofill] Fix AutofillTriggerSource usages.
    Some usages of AutofillTriggerSource is not correct, this CL fixed them.
    
    1. Virtual cards can also be used in android, therefore we should not
    hard code it as popup.
    2. Scanning credit card can only triggered from mobile, therefore we
    hardcode it as KA.
    
    Bug: 1459990
    Change-Id: Ibaaee069c71303a8aaa099d6362eec29c483d789
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4790448
    Auto-Submit: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1185114}

--- components/autofill/core/browser/autofill_external_delegate.cc
+++ components/autofill/core/browser/autofill_external_delegate.cc
@@ -272,7 +272,8 @@ void AutofillExternalDelegate::DidSelectSuggestion(
     case PopupItemId::kVirtualCreditCardEntry:
       manager_->FillOrPreviewVirtualCardInformation(
           mojom::AutofillActionPersistence::kPreview, backend_id.value(),
-          query_form_, query_field_, AutofillTriggerSource::kKeyboardAccessory);
+          query_form_, query_field_,
+          TriggerSourceFromSuggestionTriggerSource(trigger_source));
       break;
     default:
       break;
@@ -346,9 +347,9 @@ void AutofillExternalDelegate::DidAcceptSuggestion(
                                                 query_form_, query_field_);
       break;
     case PopupItemId::kScanCreditCard:
-      manager_->client().ScanCreditCard(
-          base::BindOnce(&AutofillExternalDelegate::OnCreditCardScanned,
-                         GetWeakPtr(), AutofillTriggerSource::kPopup));
+      manager_->client().ScanCreditCard(base::BindOnce(
+          &AutofillExternalDelegate::OnCreditCardScanned, GetWeakPtr(),
+          AutofillTriggerSource::kKeyboardAccessory));
       break;
     case PopupItemId::kShowAccountCards:
       manager_->OnUserAcceptedCardsFromAccountOption();
