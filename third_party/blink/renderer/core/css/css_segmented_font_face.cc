5140e4f0b5dee54380148558b43aed077880cdce
Rune Lillesveen <futhark@chromium.org>
2023-08-18 07:12:14 +0000

    Remove DumpWithoutCrashing from CascadePriorityHigherThan
    There are more than 90000 dumps now. Mainly on Android. Should be enough
    to inspect on crash.corp.
    
    Bug: 1468721, 1250831
    Change-Id: Ife7d449f7b8b466bdb0bbad8b9227c042a1aa1e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4790726
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185058}

--- third_party/blink/renderer/core/css/css_segmented_font_face.cc
+++ third_party/blink/renderer/core/css/css_segmented_font_face.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/css/css_segmented_font_face.h"
 
-#include "base/debug/dump_without_crashing.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "third_party/blink/renderer/core/css/cascade_layer_map.h"
@@ -267,8 +266,9 @@ bool CascadePriorityHigherThan(const FontFace& new_font_face,
   // owner document. However, there are cases where we don't have a document
   // here, possibly caused by ExecutionContext or Document lifecycle issues.
   // TODO(crbug.com/1250831): Find out the root cause and fix it.
+  // Used to have base::debug::DumpWithoutCrashing(), but caused a lot of
+  // crashes, particularly on Android (crbug.com/1468721).
   if (!new_font_face.GetDocument() || !existing_font_face.GetDocument()) {
-    base::debug::DumpWithoutCrashing();
     // In the buggy case, to ensure a stable ordering, font faces without a
     // document are considered higher priority.
     return !new_font_face.GetDocument();
