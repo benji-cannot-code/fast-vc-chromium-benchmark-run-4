d7021a9c39bbabc8dca2b73864d0ab06af1df434
Jood Hajeer <jood@google.com>
2023-08-18 09:43:12 +0000

    [iOS] Increase space after Sync toggles with SyncDisabled policy.
    In the new unified settings page, when SyncDisabled policy is applied,
    the AdvancedSettingsSection is not shown and thus its header space is
    not shown.
    Only in this case, a space should be added before the SignOutSection;
    below the Sync toggles section, to fill for that space.
    
    Screenshot in crbug.com/1472240#c9
    
    Fixed: 1472240
    Change-Id: I7929bf67e1a1c50ee180abaadaf92468a2f526c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4790144
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1185097}

--- ios/chrome/browser/ui/settings/google_services/manage_sync_settings_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/google_services/manage_sync_settings_table_view_controller.mm
@@ -26,6 +26,7 @@ namespace {
 CGFloat kAccountSectionHeaderHeightPointSize = 22.17;
 CGFloat kSyncDataTypeSectionHeaderHeightPointSize = 60.;
 CGFloat kAdvancedSettingsSectionHeaderHeightPointSize = 26.;
+CGFloat kSignOutSectionHeaderHeightPointSize = 26.;
 
 // Table view customized footer heights.
 CGFloat kAccountSectionFooterHeightPointSize = 16.;
@@ -191,8 +192,13 @@ CGFloat kDefaultSectionFooterHeightPointSize = 10.;
         return kSyncDataTypeSectionHeaderHeightPointSize;
       case AdvancedSettingsSectionIdentifier:
         return kAdvancedSettingsSectionHeaderHeightPointSize;
-      case SyncErrorsSectionIdentifier:
       case SignOutSectionIdentifier:
+        if (![self.tableViewModel hasSectionForSectionIdentifier:
+                                      AdvancedSettingsSectionIdentifier]) {
+          return kSignOutSectionHeaderHeightPointSize;
+        }
+        break;
+      case SyncErrorsSectionIdentifier:
         break;
     }
   }
