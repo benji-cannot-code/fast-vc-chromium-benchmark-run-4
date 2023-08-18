8144d8250f222ee05377723f26f24a2d7de19cef
Andrew Williams <awillia@chromium.org>
2023-08-18 02:31:41 +0000

    [IP Protection] Add VLOGs, DUMP_WILL_BE_CHECK, and optimization
    This CL has several small changes, including:
     - Adding VLOGs that are consistent with the ones we are adding in the
       network service
    
     - Adding a DUMP_WILL_BE_CHECK so we can safely determine whether we can
       CHECK that IpProtectionAuthTokenProvider::SetReceiver isn't called
       more than once.
    
     - Instantiating the IpProtectionAuthTokenProvider on profile creation
    
    Bug: 1473026, 1473734
    Change-Id: I9cb94ac9c984b0f4bc16fb30b7bca20e271fd77c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4790806
    Reviewed-by: Dustin Mitchell <djmitche@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Andrew Williams <awillia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185024}

--- chrome/browser/ip_protection/ip_protection_auth_token_provider.cc
+++ chrome/browser/ip_protection/ip_protection_auth_token_provider.cc
@@ -88,8 +88,8 @@ void IpProtectionAuthTokenProvider::OnRequestOAuthTokenCompleted(
   // If we fail to get an OAuth token don't attempt to fetch from Phosphor as
   // the request is guaranteed to fail.
   if (error.state() != GoogleServiceAuthError::NONE) {
-    VLOG(1) << "IP Protection OAuth token fetch failed with error: "
-            << base::NumberToString(static_cast<int>(error.state()));
+    VLOG(2) << "IPATP::OnRequestOAuthTokenCompleted got an error: "
+            << static_cast<int>(error.state());
     TryGetAuthTokensComplete(
         absl::nullopt, IpProtectionTryGetAuthTokensResult::kFailedOAuthToken);
     return;
@@ -131,11 +131,14 @@ void IpProtectionAuthTokenProvider::OnFetchBlindSignedTokenCompleted(
         result = IpProtectionTryGetAuthTokensResult::kFailedBSAOther;
         break;
     }
+    VLOG(2) << "IPATP::OnFetchBlindSignedTokenCompleted got an error: "
+            << static_cast<int>(result);
     TryGetAuthTokensComplete(absl::nullopt, result);
     return;
   }
 
   if (tokens.value().size() == 0) {
+    VLOG(2) << "IPATP::OnFetchBlindSignedTokenCompleted called with no tokens";
     TryGetAuthTokensComplete(
         absl::nullopt, IpProtectionTryGetAuthTokensResult::kFailedBSAOther);
     return;
@@ -242,11 +245,12 @@ void IpProtectionAuthTokenProvider::SetReceiver(
   if (is_shutting_down_) {
     return;
   }
+  // TODO(https://crbug.com/1473734): I'm not sure if this case is possible
+  // since a receiver should only be added when a NetworkContext is created, but
+  // maybe this can occur if the network service crashes and is restarted? If
+  // this can't happen, just replace this if statement with a CHECK.
+  DUMP_WILL_BE_CHECK(!receiver_.is_bound());
   if (receiver_.is_bound()) {
-    // TODO(awillia): I'm not sure if this case is possible since a receiver
-    // should only be added when a NetworkContext is created, but maybe this can
-    // occur if the network service crashes and is restarted? If this can't
-    // happen, just replace this if statement with a CHECK.
     receiver_.reset();
     // Reset any pending callbacks as well since this class only expects to have
     // only one pending call to `TryGetAuthTokens()` at any given time.
