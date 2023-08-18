3ad94e5f119ab2fd2ad3d2f935b0f97e152983fc
hmare <hmare@google.com>
2023-08-18 13:58:52 +0000

    [DTC-KeyManagement] Create KeyLoader
    Creates the KeyLoader class that loads and synchronizes the signing key.
    
    Design: go/dtc-temp-key-storage
    
    Bug: b/290076221
    Change-Id: I22ecdc60077d5c6b5a6f8ae16dd7bd6dd22c305a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4727890
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Hamda Mare <hmare@google.com>
    Cr-Commit-Position: refs/heads/main@{#1185162}

---
+++ chrome/browser/enterprise/connectors/device_trust/key_management/browser/mock_key_loader.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/enterprise/connectors/device_trust/key_management/browser/mock_key_loader.h"
+
+namespace enterprise_connectors::test {
+
+MockKeyLoader::MockKeyLoader() = default;
+MockKeyLoader::~MockKeyLoader() = default;
+
+}  // namespace enterprise_connectors::test
