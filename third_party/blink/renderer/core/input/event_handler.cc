f5e3a2bf21f755041c3075b2c2edb48344cea5ba
Takumi Fujimoto <takumif@chromium.org>
2023-08-18 02:37:49 +0000

    Revert "Add tooltip to date/time control picker indicator element"
    This reverts commit a1d53f199285c38b3f84df528b7c7b667598d2da.
    
    Reason for revert: Suspected culprit of bot failures:
    https://ci.chromium.org/ui/p/chromium/builders/ci/fuchsia-x64-rel/8193/overview
    
    Original change's description:
    > Add tooltip to date/time control picker indicator element
    >
    > Prior to this change, the picker indicator element (the "icon" that can
    > be clicked on to open the popup) did not have a tooltip. With this CL,
    > we add a tooltip where the tooltip text matches the indicator element's
    > aria-label (e.g. "Show X picker").
    >
    > Bug: 1473774
    > Change-Id: I4a156753cea4d44722e8636492c90bce7e94fd87
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4767916
    > Commit-Queue: Stephanie Zhang <stephanie.zhang@microsoft.com>
    > Reviewed-by: Mason Freed <masonf@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1184963}
    
    Bug: 1473774
    Change-Id: I967b16f649e52ce3003cfc2be2041a5141d5e649
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4790324
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Owners-Override: Takumi Fujimoto <takumif@chromium.org>
    Auto-Submit: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185026}

--- third_party/blink/renderer/core/input/event_handler.cc
+++ third_party/blink/renderer/core/input/event_handler.cc
@@ -91,7 +91,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/style/cursor_data.h"
 #include "third_party/blink/renderer/core/svg/graphics/svg_image.h"
 #include "third_party/blink/renderer/core/svg/graphics/svg_image_for_container.h"
-#include "third_party/blink/renderer/core/svg/svg_use_element.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/cursors.h"
 #include "third_party/blink/renderer/platform/graphics/image_orientation.h"
@@ -173,30 +172,6 @@ gfx::Point DetermineHotSpot(const Image& image,
 
   // If neither is provided, use a default value of (0, 0).
   return gfx::Point();
-}
-
-// Returns whether the hit element contains a title and isn't a SVGUseElement or
-// part of an SVGUseElement.
-bool HasTitleAndNotSVGUseElement(const HitTestResult& hovered_node_result) {
-  // TODO(crbug.com/1473774): Remove flag check if no issues arise.
-  if (!RuntimeEnabledFeatures::SkipShadowHostWhenHoveringForTooltipEnabled()) {
-    return false;
-  }
-  Node* inner_node = hovered_node_result.InnerNode();
-  if (!inner_node) {
-    return false;
-  }
-  auto* element = DynamicTo<Element>(inner_node);
-  if (!element || element->title().IsNull()) {
-    return false;
-  }
-  ShadowRoot* containing_shadow_root = inner_node->ContainingShadowRoot();
-  if (IsA<SVGUseElement>(element) ||
-      (containing_shadow_root &&
-       IsA<SVGUseElement>(containing_shadow_root->host()))) {
-    return false;
-  }
-  return true;
 }
 
 }  // namespace
@@ -981,11 +956,7 @@ WebInputEventResult EventHandler::HandleMouseMoveEvent(
       layer_scrollable_area->MouseMovedInContentArea();
   }
 
-  // Should not convert the hit shadow element to its shadow host, so that
-  // tooltips in the shadow tree appear correctly.
-  if (!HasTitleAndNotSVGUseElement(hovered_node_result)) {
-    hovered_node_result.SetToShadowHostIfInUAShadowRoot();
-  }
+  hovered_node_result.SetToShadowHostIfInUAShadowRoot();
   page->GetChromeClient().MouseDidMoveOverElement(*frame_, location,
                                                   hovered_node_result);
 
