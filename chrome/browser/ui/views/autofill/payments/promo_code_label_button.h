3f7c2bdc2ab4b4dbf2a5f9995dfaf725b113a2b4
Jan Keitel <jkeitel@google.com>
2023-08-18 08:48:35 +0000

    Eliminate PromoCodeLabelButton::OnThemeChanged().
    This CL moves PromoCodeLabelButton from using SkColor to using
    ui::ColorId. This also avoids having to restyle the entire button on
    theme change.
    
    Bug: 1469871
    Change-Id: Ia4e48231db7fbb2e10e248d9b869953518e9ef4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4789129
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1185084}

--- chrome/browser/ui/views/autofill/payments/promo_code_label_button.h
+++ chrome/browser/ui/views/autofill/payments/promo_code_label_button.h
@@ -24,9 +24,6 @@ class PromoCodeLabelButton : public views::LabelButton {
   PromoCodeLabelButton& operator=(const PromoCodeLabelButton&) = delete;
 
   ~PromoCodeLabelButton() override;
-
-  // views::Button:
-  void OnThemeChanged() override;
 };
 
 }  // namespace autofill
