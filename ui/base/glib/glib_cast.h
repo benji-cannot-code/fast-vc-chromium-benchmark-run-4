453c1310760f97cfdf8c10f8202e25246544c135
Tom Anderson <thomasanderson@chromium.org>
2023-08-18 00:04:57 +0000

    [GTK] Fix invalid type-class reference in GTK4
    The `g_type_class_ref` hack was added to prevent the compiler from
    optimizing away `g_type` so that libgtk.so would be identical
    when built with GTK3 or GTK4.  However, this is
      1. Not necessary for correctness
      2. Causes log-spam when the type is not convertable to a
         type-class (which can happen with GdkPaintable on GTK4
         for reasons I don't understand).
    Now that it's recommended to use CHECK() instead of DCHECK() when
    possible, this CL removes `g_type_class_ref` and replaces with
    CHECK().
    
    R=thestig
    
    Bug: None
    Change-Id: I329b58b012d36c41fde8c89ad5de574b10e2f6a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4788527
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1184967}

--- ui/base/glib/glib_cast.h
+++ ui/base/glib/glib_cast.h
@@ -12,11 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 template <typename T, typename U>
 T* GlibCast(U* instance, GType g_type) {
-  // Ensure |g_type| is used so the compiler doesn't optimize it away
-  // in release builds.
-  if (auto* type_class = g_type_class_ref(g_type))
-    g_type_class_unref(type_class);
-  DCHECK(G_TYPE_CHECK_INSTANCE_TYPE(instance, g_type));
+  CHECK(G_TYPE_CHECK_INSTANCE_TYPE(instance, g_type));
   return reinterpret_cast<T*>(instance);
 }
 
