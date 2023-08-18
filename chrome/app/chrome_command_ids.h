dd088651689a969bc7cb84ed36b2aac90eade06f
John Palmer <jopalmer@chromium.org>
2023-08-18 00:47:20 +0000

    Add orca to enum map
    Prevents a crash when we try to show it - this was missed in https://chromium-review.googlesource.com/c/chromium/src/+/4671285
    
    Change-Id: Ica8b7444bfd7695f17db65f94acbeaa9f45cc54c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4736938
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Auto-Submit: John Palmer <jopalmer@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184986}

--- chrome/app/chrome_command_ids.h
+++ chrome/app/chrome_command_ids.h
@@ -493,9 +493,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Quick Answers context menu items.
 #define IDC_CONTENT_CONTEXT_QUICK_ANSWERS_INLINE_ANSWER 52413
 #define IDC_CONTENT_CONTEXT_QUICK_ANSWERS_INLINE_QUERY 52414
+#endif
+
 // Orca feature context menu items.
 #define IDC_CONTENT_CONTEXT_ORCA 52415
-#endif
 
 // Screen AI layout extraction.
 #define IDC_CONTENT_CONTEXT_RUN_LAYOUT_EXTRACTION 52420
