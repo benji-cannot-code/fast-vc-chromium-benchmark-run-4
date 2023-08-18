03c580f4c841e54f4444f70a59a4ca3de9cbf01c
Chris Lu <thegreenfrog@chromium.org>
2023-08-18 15:06:16 +0000

    [ios] Rename ContentSuggestionsMediator's reloadAllData
    The logic should only be called once after setting the mediator's
    consumer.
    
    Change-Id: I3bcff85ec6a3429ce3947a5ce0b85be993906b21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4791290
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1185194}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
@@ -324,10 +324,6 @@ BASE_FEATURE(kNoRecentTabIfNullWebState,
   return self.contentSuggestionsViewController.view;
 }
 
-- (void)reload {
-  [self.contentSuggestionsMediator reloadAllData];
-}
-
 #pragma mark - ContentSuggestionsMenuProvider
 
 - (UIContextMenuConfiguration*)contextMenuConfigurationForItem:
