891fe2f5856f863e7f7a360cf6a262ba8e6f4ed4
Yi Gu <yigu@chromium.org>
2023-08-18 23:04:35 +0000

    [FedCM] Prototype AutoReauthnFlag behind a flag
    ChromeStatus: https://chromestatus.com/guide/edit/5384360374566912
    
    Bug: 1473976
    Change-Id: Idb4f702f23ef96c3ebd08c642609f2a7fa2ead44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4792440
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185429}

--- content/browser/webid/test/federated_auth_request_request_token_callback_helper.h
+++ content/browser/webid/test/federated_auth_request_request_token_callback_helper.h
@@ -63,7 +63,8 @@ class FederatedAuthRequestRequestTokenCallbackHelper {
  private:
   void ReceiverMethod(blink::mojom::RequestTokenStatus status,
                       const absl::optional<GURL>& selected_idp_config_url,
-                      const absl::optional<std::string>& token);
+                      const absl::optional<std::string>& token,
+                      bool is_auto_reauthn);
 
   void Quit();
 
@@ -72,6 +73,7 @@ class FederatedAuthRequestRequestTokenCallbackHelper {
   absl::optional<blink::mojom::RequestTokenStatus> status_;
   absl::optional<GURL> selected_idp_config_url_;
   absl::optional<std::string> token_;
+  bool is_auto_reauthn_{false};
 };
 
 }  // namespace content
