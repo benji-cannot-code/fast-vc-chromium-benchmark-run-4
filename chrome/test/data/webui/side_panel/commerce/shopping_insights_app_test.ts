939a2e2390f50a979855a8cfd9a4cdf7957d7992
Zhiyuan Cai <zhiyuancai@chromium.org>
2023-08-18 18:05:36 +0000

    [PriceInsights] Show "Price History" as the graph title in all cases
    This CL updates to always show "Price History" as the graph title no
    matter what price bucket it is.
    
    Bug: 1473840, b:296470966
    Change-Id: I49edf8a2be31832445cbda32d871683f1fd207e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4792383
    Commit-Queue: Zhiyuan Cai <zhiyuancai@chromium.org>
    Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185290}

--- chrome/test/data/webui/side_panel/commerce/shopping_insights_app_test.ts
+++ chrome/test/data/webui/side_panel/commerce/shopping_insights_app_test.ts
@@ -146,7 +146,7 @@ suite('ShoppingInsightsAppTest', () => {
     assertTrue(!!historyTitle);
     assertTrue(isVisible(historyTitle));
     assertEquals(
-        loadTimeData.getString('lowPriceMultipleOptions'),
+        loadTimeData.getString('historyTitle'),
         historyTitle.textContent!.trim());
 
     const attributesRow =
@@ -282,7 +282,7 @@ suite('ShoppingInsightsAppTest', () => {
         shoppingInsightsApp.shadowRoot!.querySelector('#historyTitle');
     assertTrue(!!historyTitle);
     assertEquals(
-        loadTimeData.getString('highPriceSingleOption'),
+        loadTimeData.getString('historyTitle'),
         historyTitle.textContent!.trim());
     assertFalse(
         isVisible(historySection.querySelector('catalog-attributes-row')));
