149291aea355f7aa7622196da81c6a3b31cb3be8
Nan Lin <linnan@chromium.org>
2023-08-18 19:31:39 +0000

    Move getMeasurementApiStatus JNI call off the main thread
    This is to avoid additional latency on startup.
    
    The Java getMeasurementApiStatus method is changed to static to avoid
    using the same Java objects in different threads.
    
    Bug: 1473968
    Change-Id: If890bf6bfe74b545ae0d9d5edfa382de6be1f13b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4790805
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185334}

--- content/public/android/java/src/org/chromium/content/browser/AttributionOsLevelManager.java
+++ content/public/android/java/src/org/chromium/content/browser/AttributionOsLevelManager.java
@@ -27,6 +27,7 @@ import com.google.common.util.concurrent.ListenableFuture;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
+import org.chromium.base.ThreadUtils;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.NativeMethods;
@@ -336,7 +337,9 @@ public class AttributionOsLevelManager {
      * https://developer.android.com/reference/androidx/privacysandbox/ads/adservices/java/measurement/MeasurementManagerFutures.
      */
     @CalledByNative
-    private void getMeasurementApiStatus() {
+    private static void getMeasurementApiStatus() {
+        ThreadUtils.assertOnBackgroundThread();
+
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
             AttributionOsLevelManagerJni.get().onMeasurementStateReturned(0);
             return;
@@ -348,7 +351,8 @@ public class AttributionOsLevelManager {
             AttributionOsLevelManagerJni.get().onMeasurementStateReturned(0);
             return;
         }
-        MeasurementManagerFutures mm = getManager();
+        MeasurementManagerFutures mm =
+                MeasurementManagerFutures.from(ContextUtils.getApplicationContext());
         if (mm == null) {
             AttributionOsLevelManagerJni.get().onMeasurementStateReturned(0);
             return;
