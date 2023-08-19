ff6935d50928e31202888415f4e1ba976efc616f
Steve Kobes <skobes@chromium.org>
2023-08-19 00:49:30 +0000

    Remove the "threadedScrollingEnabled" Blink setting.
    Bug: 1087725,1369739
    Change-Id: I2680607eb27451b556d9ef1544f8784250cda2e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4774603
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: David Awogbemila <awogbemila@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185466}

--- cc/input/main_thread_scrolling_reason.h
+++ cc/input/main_thread_scrolling_reason.h
@@ -39,6 +39,7 @@ struct CC_EXPORT MainThreadScrollingReason {
 
     // Non-transient scrolling reasons. These are set on the ScrollNode.
     kHasBackgroundAttachmentFixedObjects = 1 << 2,
+    // TODO(crbug.com/1087725): Remove kThreadedScrollingDisabled.
     kThreadedScrollingDisabled = 1 << 3,
     kPopupNoThreadedInput = 1 << 4,
     kPreferNonCompositedScrolling = 1 << 15,
