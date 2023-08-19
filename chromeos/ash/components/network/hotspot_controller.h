375d508b4eec93258324b4d550005c3765e96841
Jason Zhang <jiajunz@google.com>
2023-08-19 00:42:25 +0000

    [CrOS Hotspot] Restore WiFi status when hotspot if off
    This CL restores WiFi status to previous status when hotspot is turned
    off.
    
    Bug: b/295213726
    Change-Id: Icced517c3aca533c0d5d18f8ba7cd000cd39fbc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4766456
    Reviewed-by: Nikhil Nayunigari <nikhilcn@google.com>
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1185463}

--- chromeos/ash/components/network/hotspot_controller.h
+++ chromeos/ash/components/network/hotspot_controller.h
@@ -31,7 +31,8 @@ class HotspotFeatureUsageMetrics;
 // Enable or disable requests executed as they come in but are ignored if there
 // is already a pending request.
 class COMPONENT_EXPORT(CHROMEOS_NETWORK) HotspotController
-    : public TechnologyStateController::HotspotOperationDelegate {
+    : public TechnologyStateController::HotspotOperationDelegate,
+      public HotspotStateHandler::Observer {
  public:
   class Observer : public base::CheckedObserver {
    public:
@@ -45,7 +46,7 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) HotspotController
   HotspotController();
   HotspotController(const HotspotController&) = delete;
   HotspotController& operator=(const HotspotController&) = delete;
-  virtual ~HotspotController();
+  ~HotspotController() override;
 
   void Init(HotspotCapabilitiesProvider* hotspot_capabilities_provider,
             HotspotFeatureUsageMetrics* hotspot_feature_usage_metrics,
@@ -88,7 +89,6 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) HotspotController
     ~HotspotControlRequest();
 
     bool enabled;
-    bool wifi_turned_off = false;
     bool abort = false;
     // Set for disable requests and will be nullopt for enable requests.
     absl::optional<hotspot_config::mojom::DisableReason> disable_reason;
@@ -101,6 +101,9 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) HotspotController
   // TechnologyStateController::HotspotOperationDelegate:
   void PrepareEnableWifi(
       base::OnceCallback<void(bool prepare_success)> callback) override;
+
+  // HotspotStateHandler::Observer:
+  void OnHotspotStatusChanged() override;
 
   void CheckTetheringReadiness();
   void OnCheckTetheringReadiness(
@@ -133,6 +136,10 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) HotspotController
   std::unique_ptr<HotspotControlRequest> current_disable_request_;
 
   bool allow_hotspot_ = true;
+  // Store whether the WiFi was turned off due to the start of hotspot. Need to
+  // restore the WiFi status once hotspot is turned off.
+  bool wifi_turned_off_ = false;
+
   raw_ptr<HotspotCapabilitiesProvider, ExperimentalAsh>
       hotspot_capabilities_provider_ = nullptr;
   raw_ptr<HotspotFeatureUsageMetrics, ExperimentalAsh>
