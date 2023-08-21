2ccf0a9081722e4ed4561480710124cfb12c01d8
Ilya Nikolaevskiy <ilnik@chromium.org>
2023-08-21 10:45:15 +0000

    Reduce ID3D11VideoProcessor churn in DCLayerTree
    Before it stored one VideoProcessor for HDR and one for SDR and recreated
    it each time an input or output texture size exceeded current in any dimension.
    This could lead to flip-flopping between two states: one with big input texture
    and another with big output texture. Each time VideoProcessor would be recreated.
    
    Now it will store one VideoProcessor and recreate it if any of 4 dimensions
    (input/output width/height) increases.
    
    To reduce resource usage, a moving window max is used on each of 4 dimensions
    separately, so after a prolonged period of using VideoProcessor on smaller textures
    a new smaller sizes would be used.
    
    A small utility MovingMax class is also added in this CL.
    
    Bug: 1472975
    Change-Id: I16ac9dfa0e3a64f0c10ff895f232a22cb87bb3a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4790222
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Commit-Queue: Ilya Nikolaevskiy <ilnik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185786}

--- ui/gl/dc_layer_tree.h
+++ ui/gl/dc_layer_tree.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_map.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "ui/base/moving_max.h"
 #include "ui/gfx/color_space_win.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gl/dc_layer_overlay_params.h"
@@ -33,8 +34,6 @@ namespace gl {
 
 class DirectCompositionChildSurfaceWin;
 class SwapChainPresenter;
-
-enum class VideoProcessorType { kSDR, kHDR };
 
 // Cache video processor and its size.
 struct VideoProcessorWrapper {
@@ -64,8 +63,6 @@ struct VideoProcessorWrapper {
 // CommitAndClearPendingOverlays().
 class GL_EXPORT DCLayerTree {
  public:
-  using VideoProcessorMap =
-      base::flat_map<VideoProcessorType, VideoProcessorWrapper>;
   using DelegatedInkRenderer =
       DelegatedInkPointRendererGpu<IDCompositionInkTrailDevice,
                                    IDCompositionDelegatedInkTrail,
@@ -100,8 +97,7 @@ class GL_EXPORT DCLayerTree {
   // layers so the same one can be reused if it's large enough.  Returns true on
   // success.
   VideoProcessorWrapper* InitializeVideoProcessor(const gfx::Size& input_size,
-                                                  const gfx::Size& output_size,
-                                                  bool is_hdr_output);
+                                                  const gfx::Size& output_size);
 
   bool disable_nv12_dynamic_textures() const {
     return disable_nv12_dynamic_textures_;
@@ -122,8 +118,6 @@ class GL_EXPORT DCLayerTree {
   bool no_downscaled_overlay_promotion() const {
     return no_downscaled_overlay_promotion_;
   }
-
-  VideoProcessorWrapper& GetOrCreateVideoProcessor(bool is_hdr);
 
   Microsoft::WRL::ComPtr<IDXGISwapChain1> GetLayerSwapChainForTesting(
       size_t index) const;
@@ -455,9 +449,16 @@ class GL_EXPORT DCLayerTree {
   // A IDCompositionSurface cleared to white, used for solid color overlays.
   Microsoft::WRL::ComPtr<IDCompositionSurface> solid_color_texture_;
 
-  // Store video processor for SDR/HDR mode separately, which could avoid
-  // problem in (http://crbug.com/1121061).
-  VideoProcessorMap video_processor_map_;
+  // Store the largest video processor to avoid problems in
+  // (http://crbug.com/1121061) and (http://crbug.com/1472975).
+  VideoProcessorWrapper video_processor_wrapper_;
+  // To reduce resource usage, we keep track of the largest input/output
+  // dimensions for several last VideoProcessor usages. All 4 dimensions must be
+  // tracked separately.
+  ui::MovingMax max_video_processor_input_height_;
+  ui::MovingMax max_video_processor_input_width_;
+  ui::MovingMax max_video_processor_output_height_;
+  ui::MovingMax max_video_processor_output_width_;
 
   // Current video processor input and output colorspace.
   gfx::ColorSpace video_input_color_space_;
