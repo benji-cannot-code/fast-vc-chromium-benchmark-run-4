f48b4fdffebd413e3662680b492536bd65831118
Noam Rosenthal <nrosenthal@chromium.org>
2023-08-21 06:22:10 +0000

    LoAF: Make sure callbacks don't cancel promise resolver attribution
    Some callbacks (e.g. URLSearchParams.forEach) can be invoked
    synchronously. If there is a pending promise resolver, those callbacks
    should not cancel it as pending.
    
    Fixed by using the existing user_callback_counter to also account
    for promise resolvers.
    
    The callback in the bug is TrustedTypePolicy.CreateHTML, but using
    URLSearchParams.forEach in the test as it's more widely supported.
    
    Bug: 1473663
    Change-Id: Id0036304d9f8a434de6d0a8a06be3087a1b21e24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4790449
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185715}

--- third_party/blink/renderer/core/frame/animation_frame_timing_monitor.cc
+++ third_party/blink/renderer/core/frame/animation_frame_timing_monitor.cc
@@ -392,6 +392,7 @@ ScriptTimingInfo* AnimationFrameTimingMonitor::DidExecuteScript(
 
 void AnimationFrameTimingMonitor::OnMicrotasksCompleted(
     ExecutionContext* context) {
+  user_callback_depth_--;
   if (!ShouldAddScript(context)) {
     pending_script_info_ = absl::nullopt;
     return;
@@ -441,6 +442,8 @@ void AnimationFrameTimingMonitor::WillHandlePromise(
       .execution_start_time = now,
       .class_like_name = class_like_name,
       .property_like_name = property_like_name};
+
+  user_callback_depth_++;
 }
 
 void AnimationFrameTimingMonitor::Will(
