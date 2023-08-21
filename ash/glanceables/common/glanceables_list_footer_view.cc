f0c789741afdc0698d5b614c1e4f191f7a3933a3
Artsiom Mitrokhin <amitrokhin@google.com>
2023-08-21 20:04:50 +0000

    Update focus ring color for "See all" button
    Bug: b/296472951
    Change-Id: Iefffb509f5140c00c4bd9c8fc2bf33c77cec8df8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4794572
    Commit-Queue: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186037}

--- ash/glanceables/common/glanceables_list_footer_view.cc
+++ ash/glanceables/common/glanceables_list_footer_view.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/text_constants.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/button/label_button.h"
+#include "ui/views/controls/focus_ring.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/flex_layout_types.h"
 #include "ui/views/layout/layout_types.h"
@@ -56,6 +57,7 @@ class SeeAllButton : public views::LabelButton {
     TypographyProvider::Get()->StyleLabel(TypographyToken::kCrosButton2,
                                           *label());
     SetAccessibilityProperties(ax::mojom::Role::kLink, see_all_accessible_name);
+    views::FocusRing::Get(this)->SetColorId(cros_tokens::kCrosSysFocusRing);
   }
 
   SeeAllButton(const SeeAllButton&) = delete;
