0b0be0ccb0b57a9be0b7f11402dc017e89468b69
Elizaveta Sheremetyeva <sheremetyeva@google.com>
2023-08-21 10:33:58 +0000

    [DBSC] Release blocked requests after timeout
    Requests are throttled until the cookie rotation request completes or
    bound cookies are fresh in the cookie jar. In this CL we add a timeout
    after which requests are resumed, to avoid blocking the requests for too
    long.
    
    Bug: b/293560545
    Change-Id: I8a85ccca026a9afc1f23d32ec57a2e971ee2a944
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4732691
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Elizaveta Sheremetyeva <sheremetyeva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1185784}

--- chrome/browser/signin/bound_session_credentials/bound_session_cookie_controller_impl.h
+++ chrome/browser/signin/bound_session_credentials/bound_session_cookie_controller_impl.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "base/containers/span.h"
 #include "base/functional/callback_forward.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
@@ -40,17 +39,18 @@ class BoundSessionCookieControllerImpl : public BoundSessionCookieController {
       const base::flat_set<std::string>& cookie_names,
       Delegate* delegate);
 
-  void Initialize() override;
-
-  void OnRequestBlockedOnCookie(
-      base::OnceClosure resume_blocked_request) override;
-
   ~BoundSessionCookieControllerImpl() override;
 
   BoundSessionCookieControllerImpl(const BoundSessionCookieControllerImpl&) =
       delete;
   BoundSessionCookieControllerImpl& operator=(
       const BoundSessionCookieControllerImpl&) = delete;
+
+  // BoundSessionCookieController:
+  void Initialize() override;
+
+  void OnRequestBlockedOnCookie(
+      base::OnceClosure resume_blocked_request) override;
 
  private:
   friend class BoundSessionCookieControllerImplTest;
@@ -95,6 +95,8 @@ class BoundSessionCookieControllerImpl : public BoundSessionCookieController {
   std::vector<base::OnceClosure> resume_blocked_requests_;
   // Used to schedule preemptive cookie refresh.
   base::OneShotTimer cookie_refresh_timer_;
+  // Used to release blocked requests after a timeout.
+  base::OneShotTimer resume_blocked_requests_timer_;
 
   RefreshCookieFetcherFactoryForTesting
       refresh_cookie_fetcher_factory_for_testing_;
