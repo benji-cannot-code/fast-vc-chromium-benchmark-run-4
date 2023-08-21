7552d18e6332953a8dec6702f76fc07104cbc42b
Federica Germinario <fedegermi@google.com>
2023-08-21 13:19:37 +0000

    [ios] Set TranslateOverlayTabHelper's observer list check empty to true
    This CL checks that observer list in TranslateOverlayTabHelper is
    empty when it is destroyed.
    
    Bug: 1430392
    Change-Id: If08923d9a69fadde7a9b428209e7075aeccdd8a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4767824
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185823}

--- ios/chrome/browser/infobars/overlays/translate_overlay_tab_helper.h
+++ ios/chrome/browser/infobars/overlays/translate_overlay_tab_helper.h
@@ -46,7 +46,7 @@ class TranslateOverlayTabHelper
   TranslateOverlayTabHelper(web::WebState* web_state);
 
   // Observers to listen to translation completions.
-  base::ObserverList<Observer> observers_;
+  base::ObserverList<Observer, true> observers_;
 
  private:
   friend class web::WebStateUserData<TranslateOverlayTabHelper>;
