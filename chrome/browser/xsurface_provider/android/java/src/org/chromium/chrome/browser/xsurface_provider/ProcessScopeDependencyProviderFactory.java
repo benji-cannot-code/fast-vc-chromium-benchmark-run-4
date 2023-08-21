007375e24fc12befd59ae430eef88c694c89549a
Edmund Wright <edmundw@google.com>
2023-08-21 18:27:03 +0000

    [gbb-cct] Create XSurfaceProcessScopeProvider.
    This will be used to retrieve ProcessScope instead of retrieving it via FeedServiceBridge. It is being created to allow this retrieval from non-Feed features like Page Insights Hub, without depending on Feed code.
    
    Most files in this CL duplicate existing Feed files, which will be deleted once Feed moves over to using this.
    
    Low-Coverage-Reason: Uncovered lines in XSurfaceProcessScopeProvider infeasible to test. Uncovered single line in XSurfaceFeedHooksImpl is not useful to test.
    Bug: b/286003870
    Change-Id: Iacc420357d7afa945cee9df74a41ce63211a1f65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4793571
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Edmund Wright <edmundw@google.com>
    Auto-Submit: Edmund Wright <edmundw@google.com>
    Cr-Commit-Position: refs/heads/main@{#1185975}

---
+++ chrome/browser/xsurface_provider/android/java/src/org/chromium/chrome/browser/xsurface_provider/ProcessScopeDependencyProviderFactory.java
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.xsurface_provider;
+
+import org.chromium.chrome.browser.xsurface.ProcessScopeDependencyProvider;
+
+/**
+ * Provides instance of ProcessScopeDependencyProvider needed in response to Native calls.
+ *
+ * Note that ProcessScopeDependencyProvider is sometimes needed in response to a Native call,
+ * we use this through reflection rather than simply injecting an instance.
+ */
+public interface ProcessScopeDependencyProviderFactory {
+    /** Constructs and returns a ProcessScopeDependencyProvider instance. */
+    ProcessScopeDependencyProvider create();
+}
