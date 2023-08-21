3696559c0363c267205e0c90f8f95df5ce89d36a
Sky Malice <skym@chromium.org>
2023-08-21 19:36:41 +0000

    Add x button to bookmark search.
    Bug: 1467376
    Change-Id: Ibbcede260551ff096ea92a5c35fe9e8d8cfc1cce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4791633
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186013}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerMediator.java
@@ -182,7 +182,7 @@ class BookmarkManagerMediator
             clearHighlight();
 
             if (getCurrentUiMode() == BookmarkUiMode.SEARCHING
-                    && TextUtils.isEmpty(getCurrentQueryString())
+                    && TextUtils.isEmpty(getCurrentSearchText())
                     && getCurrentSearchPowerFilter().isEmpty()) {
                 onEndSearch();
             } else {
@@ -534,7 +534,7 @@ class BookmarkManagerMediator
      * @param query The query text to search for.
      */
     void search(@Nullable String query) {
-        onQueryCallback(query);
+        onSearchTextChangeCallback(query);
     }
 
     public void setOrder() {
@@ -676,7 +676,7 @@ class BookmarkManagerMediator
 
     @Override
     public void openSearchUi() {
-        onQueryCallback("");
+        onSearchTextChangeCallback("");
         mSelectableListLayout.onStartSearch(R.string.bookmark_no_result);
     }
 
@@ -826,10 +826,10 @@ class BookmarkManagerMediator
                 mNativePage.onStateChange(state.mUrl, false);
             }
         } else if (state.mUiMode == BookmarkUiMode.SEARCHING) {
-            String queryString = getCurrentQueryString();
-            if (!preserveFolderBookmarksOnEmptySearch || !TextUtils.isEmpty(queryString)) {
+            String searchText = getCurrentSearchText();
+            if (!preserveFolderBookmarksOnEmptySearch || !TextUtils.isEmpty(searchText)) {
                 setBookmarks(mBookmarkQueryHandler.buildBookmarkListForSearch(
-                        queryString, getCurrentSearchPowerFilter()));
+                        searchText, getCurrentSearchPowerFilter()));
             }
         }
 
@@ -1121,7 +1121,12 @@ class BookmarkManagerMediator
         boolean hasAnyShopping = true;
         PropertyModel propertyModel =
                 new PropertyModel.Builder(BookmarkSearchBoxRowProperties.ALL_KEYS)
-                        .with(BookmarkSearchBoxRowProperties.QUERY_CALLBACK, this::onQueryCallback)
+                        .with(BookmarkSearchBoxRowProperties.SEARCH_TEXT_CHANGE_CALLBACK,
+                                this::onSearchTextChangeCallback)
+                        .with(BookmarkSearchBoxRowProperties.CLEAR_SEARCH_TEXT_RUNNABLE,
+                                this::onClearSearchTextRunnable)
+                        .with(BookmarkSearchBoxRowProperties.FOCUS_CHANGE_CALLBACK,
+                                this::onSearchBoxFocusChange)
                         .with(BookmarkSearchBoxRowProperties.SHOPPING_CHIP_VISIBILITY,
                                 hasAnyShopping)
                         .with(BookmarkSearchBoxRowProperties.SHOPPING_CHIP_START_ICON_RES,
@@ -1130,8 +1135,6 @@ class BookmarkManagerMediator
                                 R.string.price_tracking_bookmarks_filter_title)
                         .with(BookmarkSearchBoxRowProperties.SHOPPING_CHIP_TOGGLE_CALLBACK,
                                 this::onShoppingFilterToggle)
-                        .with(BookmarkSearchBoxRowProperties.FOCUS_CHANGE_CALLBACK,
-                                this::onSearchBoxFocusChange)
                         .build();
         return new ListItem(ViewType.SEARCH_BOX, propertyModel);
     }
@@ -1358,9 +1361,19 @@ class BookmarkManagerMediator
         return true;
     }
 
-    private void onQueryCallback(String query) {
-        query = query == null ? "" : query.trim();
-        onSearchChange(query, getCurrentSearchPowerFilter());
+    private void onSearchTextChangeCallback(String searchText) {
+        searchText = searchText == null ? "" : searchText.trim();
+        if (BookmarkFeatures.isAndroidImprovedBookmarksEnabled()) {
+            getSearchBoxPropertyModel().set(BookmarkSearchBoxRowProperties.SEARCH_TEXT, searchText);
+            getSearchBoxPropertyModel().set(
+                    BookmarkSearchBoxRowProperties.CLEAR_SEARCH_TEXT_BUTTON_VISIBILITY,
+                    !TextUtils.isEmpty(searchText));
+        }
+        onSearchChange(searchText, getCurrentSearchPowerFilter());
+    }
+
+    private void onClearSearchTextRunnable() {
+        getSearchBoxPropertyModel().set(BookmarkSearchBoxRowProperties.SEARCH_TEXT, "");
     }
 
     private void onSearchBoxFocusChange(Boolean hasFocus) {
@@ -1374,22 +1387,22 @@ class BookmarkManagerMediator
         final @NonNull Set<PowerBookmarkType> powerFilter = isFiltering
                 ? Collections.singleton(PowerBookmarkType.SHOPPING)
                 : Collections.emptySet();
-        onSearchChange(getCurrentQueryString(), powerFilter);
+        onSearchChange(getCurrentSearchText(), powerFilter);
     }
 
     private void onSearchChange(
-            @Nullable String query, @NonNull Set<PowerBookmarkType> powerFilter) {
-        if (TextUtils.isEmpty(query) && powerFilter.isEmpty()
+            @Nullable String searchText, @NonNull Set<PowerBookmarkType> powerFilter) {
+        if (TextUtils.isEmpty(searchText) && powerFilter.isEmpty()
                 && getCurrentUiMode() == BookmarkUiMode.SEARCHING) {
             onEndSearch();
         } else {
-            query = query == null ? "" : query;
-            setState(BookmarkUiState.createSearchState(query, powerFilter));
+            searchText = searchText == null ? "" : searchText;
+            setState(BookmarkUiState.createSearchState(searchText, powerFilter));
         }
     }
 
-    private @Nullable String getCurrentQueryString() {
-        return mStateStack.isEmpty() ? "" : mStateStack.peek().mQueryString;
+    private @Nullable String getCurrentSearchText() {
+        return mStateStack.isEmpty() ? "" : mStateStack.peek().mSearchText;
     }
 
     private @NonNull Set<PowerBookmarkType> getCurrentSearchPowerFilter() {
--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkSearchBoxRow.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkSearchBoxRow.java
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.bookmarks;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -19,6 +21,7 @@ import org.chromium.base.Callback;
 import org.chromium.chrome.R;
 import org.chromium.ui.KeyboardVisibilityDelegate;
 import org.chromium.ui.text.EmptyTextWatcher;
+import org.chromium.ui.widget.ChromeImageButton;
 
 /**
  * Row in a {@link androidx.recyclerview.widget.RecyclerView} for querying and filtering shown
@@ -26,6 +29,7 @@ import org.chromium.ui.text.EmptyTextWatcher;
  */
 public class BookmarkSearchBoxRow extends LinearLayout {
     private EditText mSearchText;
+    private ChromeImageButton mClearSearchTextButton;
 
     public BookmarkSearchBoxRow(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -36,15 +40,22 @@ public class BookmarkSearchBoxRow extends LinearLayout {
         super.onFinishInflate();
         mSearchText = findViewById(R.id.search_text);
         mSearchText.setOnEditorActionListener(this::onEditorAction);
+        mClearSearchTextButton = findViewById(R.id.clear_text_button);
     }
 
-    void setQueryCallback(Callback<String> queryCallback) {
+    void setSearchTextCallback(Callback<String> searchTextCallback) {
         mSearchText.addTextChangedListener(new EmptyTextWatcher() {
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                queryCallback.onResult(charSequence.toString());
+                searchTextCallback.onResult(charSequence.toString());
             }
         });
+    }
+
+    void setSearchText(String modelText) {
+        if (!TextUtils.equals(modelText, mSearchText.getText())) {
+            mSearchText.setText(modelText);
+        }
     }
 
     void setFocusChangeCallback(Callback<Boolean> focusChangeCallback) {
@@ -62,6 +73,17 @@ public class BookmarkSearchBoxRow extends LinearLayout {
         } else {
             mSearchText.clearFocus();
         }
+    }
+
+    void setClearSearchTextButtonRunnable(Runnable onClearSearchTextButtonRunnable) {
+        mClearSearchTextButton.setOnClickListener((view) -> {
+            assert view == mClearSearchTextButton;
+            onClearSearchTextButtonRunnable.run();
+        });
+    }
+
+    void setClearSearchTextButtonVisibility(boolean isVisible) {
+        mClearSearchTextButton.setVisibility(isVisible ? View.VISIBLE : View.INVISIBLE);
     }
 
     private boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
