159f84c6b8e60e8fa8abd1a34d2c7ca0b5eac7a2
Theresa Sullivan <twellington@chromium.org>
2023-08-21 23:04:17 +0000

    Reland "Introduce DrivingRestrictionsManager to monitor for driver optimizations"
    This reverts commit abe49396ac51673cbdf267cb21bec78224940ea5.
    
    Call #finish on all Activity's when the vehicle starts driving.
    
    Bug: b/269382065
    Change-Id: I9db6aa3b7ef0942bc6706924037626f95da7d8a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4799678
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186111}

--- chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
@@ -16,6 +16,7 @@ import android.view.inputmethod.InputMethodSubtype;
 
 import androidx.annotation.WorkerThread;
 
+import org.chromium.base.BuildInfo;
 import org.chromium.base.CommandLine;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
@@ -82,6 +83,7 @@ import org.chromium.chrome.browser.searchwidget.SearchWidgetProvider;
 import org.chromium.chrome.browser.signin.SigninCheckerProvider;
 import org.chromium.chrome.browser.tab.state.PersistedTabData;
 import org.chromium.chrome.browser.tab.state.ShoppingPersistedTabData;
+import org.chromium.chrome.browser.ui.cars.DrivingRestrictionsManager;
 import org.chromium.chrome.browser.ui.searchactivityutils.SearchActivityPreferencesManager;
 import org.chromium.chrome.browser.usb.UsbNotificationManager;
 import org.chromium.chrome.browser.util.AfterStartupTaskUtils;
@@ -256,6 +258,10 @@ public class ProcessInitializationHandler {
         setProcessStateSummaryForAnrs(true);
 
         AccessibilityState.registerObservers();
+
+        if (BuildInfo.getInstance().isAutomotive) {
+            DrivingRestrictionsManager.initialize();
+        }
     }
 
     /**
---
+++ chrome/browser/ui/android/cars/java/src/org/chromium/chrome/browser/ui/cars/DrivingRestrictionsDelegate.java
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ui.cars;
+
+import org.chromium.base.Callback;
+
+/**
+ * Base class that exposes methods for starting and stopping monitoring.
+ */
+class DrivingRestrictionsDelegate {
+    private Callback<Boolean> mRequiresDrivingOptimizationsCallback;
+
+    DrivingRestrictionsDelegate(Callback<Boolean> requiresDrivingOptimizationsCallback) {
+        mRequiresDrivingOptimizationsCallback = requiresDrivingOptimizationsCallback;
+    }
+
+    void startMonitoring() {}
+
+    void stopMonitoring() {}
+
+    protected void notifyCallback(boolean requiresOptimization) {
+        mRequiresDrivingOptimizationsCallback.onResult(requiresOptimization);
+    }
+}
