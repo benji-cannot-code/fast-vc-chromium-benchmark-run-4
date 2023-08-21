30c30d1d6ef5f9376a1512a3d22763f308d45bb4
Ankush Singh <ankushkush@google.com>
2023-08-21 16:49:06 +0000

    [sync] Remove kSyncAllowClearingMetadataWhenDataTypeIsStopped flag
    This flag enables clearing of metadata when a data type is stopped. The
    last changes behind this flag are on stable now, it is safe to remove
    the feature flag.
    
    Bug: 1454040
    Change-Id: I4ba82786e40e18ed57529b0915ee07487aaa25c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4707788
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1185914}

--- components/sync/service/model_load_manager_unittest.cc
+++ components/sync/service/model_load_manager_unittest.cc
@@ -508,8 +508,6 @@ TEST_F(SyncModelLoadManagerTest,
 }
 
 TEST_F(SyncModelLoadManagerTest, ShouldClearMetadataAfterStopped) {
-  base::test::ScopedFeatureList feature_list(
-      syncer::kSyncAllowClearingMetadataWhenDataTypeIsStopped);
   controllers_[BOOKMARKS] = std::make_unique<FakeDataTypeController>(BOOKMARKS);
   ModelLoadManager model_load_manager(&controllers_, &delegate_);
   ModelTypeSet types;
@@ -529,8 +527,6 @@ TEST_F(SyncModelLoadManagerTest, ShouldClearMetadataAfterStopped) {
 }
 
 TEST_F(SyncModelLoadManagerTest, ShouldClearMetadataIfNotRunning) {
-  base::test::ScopedFeatureList feature_list(
-      syncer::kSyncAllowClearingMetadataWhenDataTypeIsStopped);
   controllers_[BOOKMARKS] = std::make_unique<FakeDataTypeController>(BOOKMARKS);
   ModelLoadManager model_load_manager(&controllers_, &delegate_);
 
@@ -544,8 +540,6 @@ TEST_F(SyncModelLoadManagerTest, ShouldClearMetadataIfNotRunning) {
 }
 
 TEST_F(SyncModelLoadManagerTest, ShouldClearMetadataIfFailed) {
-  base::test::ScopedFeatureList feature_list(
-      syncer::kSyncAllowClearingMetadataWhenDataTypeIsStopped);
   controllers_[BOOKMARKS] = std::make_unique<FakeDataTypeController>(BOOKMARKS);
   EXPECT_CALL(delegate_, OnSingleDataTypeWillStop(BOOKMARKS, _)).Times(2);
 
--- components/sync/service/sync_service_impl_unittest.cc
+++ components/sync/service/sync_service_impl_unittest.cc
@@ -1549,9 +1549,6 @@ TEST_F(SyncServiceImplTest,
 }
 
 TEST_F(SyncServiceImplTest, ShouldCallStopUponResetEngineIfAlreadyShutDown) {
-  base::test::ScopedFeatureList feature_list(
-      syncer::kSyncAllowClearingMetadataWhenDataTypeIsStopped);
-
   // The intention here is to stop sync without clearing metadata by getting to
   // a sync paused state by simulating a credential rejection error.
 
