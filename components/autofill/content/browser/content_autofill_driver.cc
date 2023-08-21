63a368b0302c84005baf75d930c6484d5b4b23fd
Christoph Schwering <schwering@google.com>
2023-08-21 07:16:03 +0000

    [Autofill] Remove unused OnContextMenuShownInFieldCallback()
    The function is a leftover from the deleted manual fallbacks
    feature.
    
    Bug: 1007974, 1443645
    Change-Id: I06ed3b8cfd8eef024fc427a074a78f960cb4f5c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4793955
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1185725}

--- components/autofill/content/browser/content_autofill_driver.cc
+++ components/autofill/content/browser/content_autofill_driver.cc
@@ -631,12 +631,6 @@ void ContentAutofillDriver::JavaScriptChangedAutofilledValue(
       });
 }
 
-void ContentAutofillDriver::OnContextMenuShownInFieldCallback(
-    const FormGlobalId& form_global_id,
-    const FieldGlobalId& field_global_id) {
-  autofill_manager_->OnContextMenuShownInField(form_global_id, field_global_id);
-}
-
 void ContentAutofillDriver::OnContextMenuShownInField(
     const FormGlobalId& form_global_id,
     const FieldGlobalId& field_global_id) {
@@ -644,8 +638,8 @@ void ContentAutofillDriver::OnContextMenuShownInField(
       this, form_global_id, field_global_id,
       [](ContentAutofillDriver* target, const FormGlobalId& form_global_id,
          const FieldGlobalId& field_global_id) {
-        target->OnContextMenuShownInFieldCallback(form_global_id,
-                                                  field_global_id);
+        target->autofill_manager_->OnContextMenuShownInField(form_global_id,
+                                                             field_global_id);
       });
 }
 
