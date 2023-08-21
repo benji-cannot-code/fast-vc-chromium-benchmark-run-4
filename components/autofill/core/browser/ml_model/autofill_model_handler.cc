d85011bd69506847e802284cf788a6d4a93ed84f
Mariam Mostafa <mariammostafa@google.com>
2023-08-21 14:24:40 +0000

    Change Model handler to implement KeyedService and create new service factory.
    This will be used to create a new model handler and ensure it is instantiated once. The model handler will also be unique for every profile.
    
    The model handler dependency is added to BUILD_WITH_TFLITE_LIB target so throughout the implementation we would need to use #ifdef BUILD_WITH_TFLITE_LIB.
    
    Bug: 1465926
    Low-Coverage-Reason: The factory class will be used in a follow-up CL.
    Change-Id: Ie90b242dfcabca5cedd72b8439e9cefee25b18ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4793491
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Mariam Mostafa <mariammostafa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1185857}

--- components/autofill/core/browser/ml_model/autofill_model_handler.cc
+++ components/autofill/core/browser/ml_model/autofill_model_handler.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
-#include "base/files/file_path.h"
 #include "base/functional/bind.h"
 #include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/ml_model/autofill_model_executor.h"
