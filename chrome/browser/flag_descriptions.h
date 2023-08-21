ac2bb5be820cd5490470fd82c11e77315d1660c8
cranelw <cranelw@chromium.org>
2023-08-21 04:50:25 +0000

    Adds flag: audio-hfp-mic-sr-toggle
    This flag will be used to control the display of the toggle in audio
    setting page and quick settings.
    
    Design doc:http://docs/document/d/1Cv1DRAsC_XDA66LuD-4lmzMsUrWTk2EXQUDuCirzja4?resourcekey=0--aRStqr4ykw3dtHy-M8YWg
    
    BUG=b:292018871
    TEST=autoninja -C out/Default unit_tests && out/Default/unit_tests
    
    Change-Id: If29f762f87d8ca7120e2e391c4712421ca0514d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4792265
    Commit-Queue: Cranel W <cranelw@chromium.org>
    Reviewed-by: Li-Yu Yu <aaronyu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1185701}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3136,6 +3136,9 @@ extern const char kAudioFlexibleLoopbackDescription[];
 extern const char kAudioHFPMicSRName[];
 extern const char kAudioHFPMicSRDescription[];
 
+extern const char kAudioHFPMicSRToggleName[];
+extern const char kAudioHFPMicSRToggleDescription[];
+
 extern const char kAudioHFPNbsWarningName[];
 extern const char kAudioHFPNbsWarningDescription[];
 
