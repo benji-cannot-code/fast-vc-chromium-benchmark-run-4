baf74844adbe385eabaa067c774036834014c0df
Chad Duffin <chadduffin@chromium.org>
2023-08-21 17:14:04 +0000

    cellular: Minor UI/string changes for SM-DS Support
    This change makes a few small UI/string changes to the eSIM installation
    flow UI.
    
    Bug: b/278134084
    Change-Id: Ifdfddaaa4efe195d7b71aea6c0285a21e23cbeff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4727066
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185930}

--- ash/webui/common/resources/cellular_setup/esim_flow_ui.js
+++ ash/webui/common/resources/cellular_setup/esim_flow_ui.js
@@ -884,7 +884,8 @@ Polymer({
    */
   noProfilesFound_() {
     if (this.smdsSupportEnabled_) {
-      return this.pendingProfileProperties_ &&
+      return this.hasConsentedForDiscovery_ &&
+          !!this.pendingProfileProperties_ &&
           this.pendingProfileProperties_.length === 0;
     } else {
       return (this.pendingProfiles_ && this.pendingProfiles_.length === 0);
@@ -894,7 +895,8 @@ Polymer({
   /** @private*/
   profilesFound_() {
     if (this.smdsSupportEnabled_) {
-      return this.pendingProfileProperties_ &&
+      return this.hasConsentedForDiscovery_ &&
+          !!this.pendingProfileProperties_ &&
           this.pendingProfileProperties_.length > 0;
     } else {
       return (this.pendingProfiles_ && this.pendingProfiles_.length > 0);
