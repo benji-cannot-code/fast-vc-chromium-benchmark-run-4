77a79676771c9aa1611e23ae26e55757753cca28
Federica Germinario <fedegermi@google.com>
2023-08-21 15:38:57 +0000

    [ios] Check that NotificationTrampoline has no observer on destruction
    This CL checks that observer list in NotificationTrampoline is
    empty when it is destroyed.
    
    Bug: 1430392
    Change-Id: I22461da96ac852f6a688249ae26c8294c731fcc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4797609
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1185891}

--- ios/net/cookies/cookie_store_ios.mm
+++ ios/net/cookies/cookie_store_ios.mm
@@ -59,10 +59,10 @@ class NotificationTrampoline {
   void NotifyCookiesChanged();
 
  private:
-  NotificationTrampoline();
-  ~NotificationTrampoline();
+  NotificationTrampoline() = default;
+  ~NotificationTrampoline() = default;
 
-  base::ObserverList<CookieNotificationObserver> observer_list_;
+  base::ObserverList<CookieNotificationObserver, true> observer_list_;
 
   static NotificationTrampoline* g_notification_trampoline;
 };
@@ -86,12 +86,6 @@ void NotificationTrampoline::RemoveObserver(CookieNotificationObserver* obs) {
 void NotificationTrampoline::NotifyCookiesChanged() {
   for (auto& observer : observer_list_)
     observer.OnSystemCookiesChanged();
-}
-
-NotificationTrampoline::NotificationTrampoline() {
-}
-
-NotificationTrampoline::~NotificationTrampoline() {
 }
 
 // Global instance of NotificationTrampoline.
