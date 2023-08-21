1f03b1a28b1ce39035c398f2a9e66b407649abd3
Olivier ROBIN <olivierrobin@google.com>
2023-08-21 15:34:54 +0000

    Remove call to applyShowWidgetContainerBackground
    Accessing the showsWidgetContainerBackground property is
    crashing on iOS 17 beta 6, so remove the call in our widgets.
    
    At the moment, the call only has effect on StandBy mode
    and those widgets are too big to be compatible anyway.
    
    Bug: 1473190
    Change-Id: I4038308ecf380db57bea68729dc66209f2dbccb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4797949
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185887}

--- ios/chrome/widget_kit_extension/shortcuts_widget.swift
+++ ios/chrome/widget_kit_extension/shortcuts_widget.swift
@@ -238,7 +238,6 @@ struct ShortcutsWidgetEntryView: View {
           .foregroundColor(Colors.widgetMostVisitedSitesRow)
           .frame(minWidth: 0, maxWidth: .infinity)
           .accessibilityLabel(Strings.widgetDisplayName)
-          .applyShowWidgetContainerBackground()
         HStack {
           let ntpTiles = Array(entry.mostVisitedSites.values).sorted()
 
