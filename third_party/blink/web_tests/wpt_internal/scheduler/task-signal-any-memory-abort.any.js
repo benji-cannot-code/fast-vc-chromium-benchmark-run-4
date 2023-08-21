97832d58c7f55c15610e50a59f8c18e34417fa64
Yoshisato Yanagisawa <yyanagisawa@google.com>
2023-08-21 01:16:44 +0000

    Revert "AbortSignal: Deflake memory tests"
    This reverts commit 22d5e228c6c4deed553010e195583fe23971c172.
    
    Reason for revert: This CL seems to cause continuous WPT failures.
    https://bugs.chromium.org/p/chromium/issues/detail?id=1474279
    
    Original change's description:
    > AbortSignal: Deflake memory tests
    >
    > The abort signal memory tests rely on a single synchronous gc() to
    > collect signals and trigger finalization registry callbacks, which has
    > become flaky on Mac (worker tests only). The implementation was changed
    > recently, but the tests were reliably passing from 8/10 -- 8/16. The
    > change to use a per-Worker microtask queue in crrev.com/c/4781406 may
    > have caused the tests to start flaking (timing lines up, reverting it
    > locally fixes things), but it's not clear how that's affecting GC.
    >
    > Testing on a Mac locally I found the third test fails fairly reliably, but attempts to debug it (heap snapshots w/ GC removed, console logs, tracing, etc.) removed the flakiness. Adding a second gc() reliably
    > fixes the third test, but other tests still fail (less often).
    >
    > This CL refactors the tests to speculatively fix the flakes:
    >  1. Run GC asynchronously. It's possible something on the stack is
    >     holding onto the signal. This probably needs to be done anyways for
    >     conservative stack scanning (other tests have been similarly
    >     refactored to use.
    >  2. Because of (1), change tests to use WeakRef instead of finalization
    >     registry. This is cleaner and will fail before the timeout, giving
    >     more insight into failures.
    >  3. Do multiple rounds of gc. I found doing gc() multiple times fixed
    >     test 3, but that could have also just changed timing. But blink
    >     unit tests run GC in a loop, so it seems reasonable to do this to
    >     ensure collection occurs in case multiple cycles are needed.
    >
    > Locally on mac, the memory tests pass with these changes with
    > --repeat-each=1000, and I didn't find any flakes on the Mac bots.
    >
    > Bug: 1473373
    > Change-Id: Ib6a65ce52372c5220126b490aee79e8f4b33e183
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4787951
    > Commit-Queue: Scott Haseley <shaseley@chromium.org>
    > Reviewed-by: Mason Freed <masonf@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1185373}
    
    Bug: 1473373
    Change-Id: Ic395066fb034d7dbfcca77a4b62752a1ffe5c4dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4796929
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1185678}

--- third_party/blink/web_tests/wpt_internal/scheduler/task-signal-any-memory-abort.any.js
+++ third_party/blink/web_tests/wpt_internal/scheduler/task-signal-any-memory-abort.any.js
@@ -1,5 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// META: script=../dom/abort/resources/run-async-gc.js
 // META: script=../dom/abort/resources/abort-signal-any-memory-tests.js
 
 abortSignalAnyMemoryTests(TaskSignal, AbortController);
