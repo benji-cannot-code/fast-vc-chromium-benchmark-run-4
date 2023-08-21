caf62dc4c229910c57919c0d603c1b5b8202bdea
Bruno Braga <brunobraga@google.com>
2023-08-21 13:33:19 +0000

    [autofill in devtools]: Add formFilled event to the manager observer.
    As suggested in crbug.com/4632806, introducing the form filling
    notification in this separate CL.
    
    This will be used by protocol/autofill_handler.h
    to emit devtools events about filled fields of a form.
    
    1. The FormFieldData and AutofillField are needed to know both
    the value and the field server type of a filled field.
    2. The profile or credit card are needed so that devtools
    will know how to build the address UI.
    I will use AutofillAddressUIComponent to do this, there is no privacy
    concerns since websites do not have access to devtools.
    3. We do not emit this notification for webview because
    we cannot have all required params, namely the profile and credit card.
    
    Bug: 1451267
    Change-Id: I0ab513fe25bfc428b97c6f7fd81e0b36dd22febf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4753133
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1185831}

--- components/autofill/core/browser/mock_autofill_manager_observer.h
+++ components/autofill/core/browser/mock_autofill_manager_observer.h
@@ -13,6 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
+class AutofillProfile;
+class CreditCard;
+
 class MockAutofillManagerObserver : public AutofillManager::Observer {
  public:
   MockAutofillManagerObserver();
@@ -102,6 +105,16 @@ class MockAutofillManagerObserver : public AutofillManager::Observer {
   MOCK_METHOD(void,
               OnFieldTypesDetermined,
               (AutofillManager&, FormGlobalId, FieldTypeSource),
+              (override));
+
+  MOCK_METHOD(void,
+              OnAutofillProfileOrCreditCardFormFilled,
+              (AutofillManager&,
+               FormGlobalId,
+               (base::span<const std::pair<const FormFieldData*,
+                                           const AutofillField*>>),
+               (absl::variant<const AutofillProfile*, const CreditCard*>
+                    profile_or_credit_card)),
               (override));
 
   MOCK_METHOD(void,
