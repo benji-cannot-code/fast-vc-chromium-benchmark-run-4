9a086c408dbae88a81a5a18d7943d4f6b4f9273e
Jose Dapena Paz <jdapena@igalia.com>
2023-08-21 18:25:39 +0000

    IWYU: ColorChooser include to convert from nullptr in ShellPlatformDelegate
    libstdc++ implementation of unique_ptr requires declaration of
    contained type.
    
    Bug: 957519
    Change-Id: I239c60703e69ea7d4070c57b9fd75873e6d18f50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4790541
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185973}

--- content/shell/browser/shell_platform_delegate.cc
+++ content/shell/browser/shell_platform_delegate.cc
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/shell/browser/shell.h"
 
+#if !BUILDFLAG(IS_IOS)
+#include "content/public/browser/color_chooser.h"
+#endif
+
 namespace content {
 
 void ShellPlatformDelegate::DidCreateOrAttachWebContents(
