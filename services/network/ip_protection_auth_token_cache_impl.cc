2dcb3d3875cc9f28a6bfdb703beec3ab9a3fccd2
Dustin J. Mitchell <djmitche@chromium.org>
2023-08-21 16:46:38 +0000

    Add VLOG()'s for IP Protection
    These VLOGs will help with debugging integration issues that occur in
    local builds and canary builds.  VERBOSE2 is used for most messages,
    with per-request messages at VERBOSE3.
    
    Bug: 1473026
    Change-Id: I5f2256e47eaf8434db2f395e21482be3435d075a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4784308
    Reviewed-by: Andrew Williams <awillia@chromium.org>
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185912}

--- services/network/ip_protection_auth_token_cache_impl.cc
+++ services/network/ip_protection_auth_token_cache_impl.cc
@@ -77,6 +77,7 @@ void IpProtectionAuthTokenCacheImpl::MaybeRefillCache() {
 
   if (cache_.size() < cache_low_water_mark_) {
     currently_getting_ = true;
+    VLOG(2) << "IPPATC::MaybeRefillCache calling TryGetAuthTokens";
     auth_token_getter_->TryGetAuthTokens(
         batch_size_,
         base::BindOnce(&IpProtectionAuthTokenCacheImpl::OnGotAuthTokens,
@@ -128,6 +129,7 @@ void IpProtectionAuthTokenCacheImpl::OnGotAuthTokens(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   currently_getting_ = false;
   if (tokens.has_value()) {
+    VLOG(2) << "IPPATC::OnGotAuthTokens got " << tokens->size() << " tokens";
     try_get_auth_tokens_after_ = base::Time();
     cache_.insert(cache_.end(), std::make_move_iterator(tokens->begin()),
                   std::make_move_iterator(tokens->end()));
@@ -137,6 +139,7 @@ void IpProtectionAuthTokenCacheImpl::OnGotAuthTokens(
                 return a->expiration < b->expiration;
               });
   } else {
+    VLOG(2) << "IPPATC::OnGotAuthTokens back off until " << *try_again_after;
     try_get_auth_tokens_after_ = *try_again_after;
   }
 
@@ -154,6 +157,8 @@ IpProtectionAuthTokenCacheImpl::GetAuthToken() {
 
   base::UmaHistogramBoolean("NetworkService.IpProtection.GetAuthTokenResult",
                             cache_.size() > 0);
+  VLOG(2) << "IPPATC::GetAuthToken with " << cache_.size()
+          << " tokens available";
 
   absl::optional<network::mojom::BlindSignedAuthTokenPtr> result;
   if (cache_.size() > 0) {
