1ece31a453a7880ae0e530d093c7f355fb8225a8
Alex Kingsborough <akingsb@google.com>
2023-08-21 19:29:51 +0000

    [NP DebugUI] Add functionality to execute actions.
    Add an execute button to perform the selected debug action.
    
    Screenshot with just current changes:
    https://screenshot.googleplex.com/72mLPe887oErxhq
    
    End result screenshot for whole chain:
    https://screenshot.googleplex.com/3owPQSDSo7vnYn3
    
    Test: Manually tested on DUT
    Change-Id: If99a0acfd97304e8d4d0fc6b8a608569a2e61e07
    Bug: b/288444022
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4778767
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Alex Kingsborough <akingsb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1186009}

--- chrome/browser/resources/nearby_internals/types.js
+++ chrome/browser/resources/nearby_internals/types.js
@@ -4,6 +4,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 /**
+ * Enum of values to use for the feature select dropdown. If a new feature is
+ * added, add it here.
+ * @enum {string}
+ */
+export const FeatureValues = {
+  NS: '0',
+  NC: '1',
+  NP: '2',
+  FP: '3',
+};
+
+/**
+ * Enum of values to use for the action select dropdown. If a new action is
+ * added, add it here.
+ * @enum {string}
+ */
+export const ActionValues = {
+  STARTSCAN: '0',
+  STOPSCAN: '1',
+  SYNCCREDENTIALS: '2',
+  FIRSTTIMEFLOW: '3',
+};
+
+/**
  * Severity enum based on LogMessage format. Needs to stay in sync with the
  * NearbyInternalsLogsHandler.
  * @enum {number}
