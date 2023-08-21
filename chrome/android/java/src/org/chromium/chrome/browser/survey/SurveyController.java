df508dc29a88dcf29d932866b772942170254c5f
Wenyu Fu <wenyufu@google.com>
2023-08-21 21:13:41 +0000

    [Survey] Modularize upstream SurveyController (3/5)
    Remove the non-interface instance of SurveyController created by AppHooks, so the downstream override can be removed.
    
    Note that the internal build target //chrome/browser/ui/android/hats/internal:controller_java is temporarily added as DEP to //chrome/android/BUILD.gn - this is temporarily, and once SurveyClientImpl is complete, the deps will be removed.
    
    SurveyController will be removed in a follow up patch, so there's no plans adding test to expand the test coverage.
    
    #1: crrev.com/c/4778974
    #2: crrev.com/i/6319390
    #3: crrev.com/c/4781023 (current)
    #4: crrev.com/i/6323390
    #5: crrev.com/c/4781664
    
    
    Low-Coverage-Reason: See description
    Bug: 1231681, 1400731
    Change-Id: Idff8540ad9c3a167e1056cedf995ad1660498efe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4781023
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186077}

--- chrome/android/java/src/org/chromium/chrome/browser/survey/SurveyController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/survey/SurveyController.java
@@ -11,8 +11,8 @@ import android.content.Context;
 import androidx.annotation.Nullable;
 
 import org.chromium.base.ResettersForTesting;
-import org.chromium.chrome.browser.AppHooks;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
+import org.chromium.chrome.browser.ui.hats.SurveyControllerProvider;
 
 import java.util.Map;
 
@@ -24,6 +24,12 @@ import java.util.Map;
 public class SurveyController {
     private static SurveyController sTestInstance;
 
+    private final org.chromium.chrome.browser.ui.hats.SurveyController mDelegate;
+
+    public SurveyController() {
+        mDelegate = SurveyControllerProvider.create();
+    }
+
     /**
      * @return The SurveyController to use during the lifetime of the browser process.
      */
@@ -31,7 +37,7 @@ public class SurveyController {
         if (sTestInstance != null) {
             return sTestInstance;
         }
-        return AppHooks.get().createSurveyController();
+        return new SurveyController();
     }
 
     /** Set the instance to use for survey related tests. Reset back to null after tests. */
@@ -49,7 +55,9 @@ public class SurveyController {
      *                          survey does not exist.
      */
     protected void downloadSurvey(Context context, String triggerId, Runnable onSuccessRunnable,
-            Runnable onFailureRunnable) {}
+            Runnable onFailureRunnable) {
+        mDelegate.downloadSurvey(context, triggerId, onSuccessRunnable, onFailureRunnable);
+    }
 
     /**
      * Show the survey.
@@ -66,7 +74,10 @@ public class SurveyController {
     @Deprecated
     protected void showSurveyIfAvailable(Activity activity, String siteId,
             boolean showAsBottomSheet, int displayLogoResId,
-            @Nullable ActivityLifecycleDispatcher lifecycleDispatcher) {}
+            @Nullable ActivityLifecycleDispatcher lifecycleDispatcher) {
+        mDelegate.showSurveyIfAvailable(
+                activity, siteId, displayLogoResId, lifecycleDispatcher, null);
+    }
 
     /**
      * Show the survey.
@@ -80,7 +91,8 @@ public class SurveyController {
     protected void showSurveyIfAvailable(Activity activity, String triggerId, int displayLogoResId,
             @Nullable ActivityLifecycleDispatcher lifecycleDispatcher,
             @Nullable Map<String, String> psd) {
-        throw new UnsupportedOperationException();
+        mDelegate.showSurveyIfAvailable(
+                activity, triggerId, displayLogoResId, lifecycleDispatcher, psd);
     }
 
     /**
@@ -89,6 +101,6 @@ public class SurveyController {
      * @return true if the survey has expired, false if the survey is valid.
      */
     protected boolean isSurveyExpired(String triggerId) {
-        return false;
+        return mDelegate.isSurveyExpired(triggerId);
     }
 }
