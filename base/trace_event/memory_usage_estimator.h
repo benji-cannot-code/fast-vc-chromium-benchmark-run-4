f81b845095e57604d950f3baf450eab93474219f
Charlie Harrison <csharrison@chromium.org>
2023-08-21 18:05:54 +0000

    Clarify documentation in memory_usage_estimator
    Change-Id: Ib95fa8ed01c6cb372dfe59927485c2592cb46fad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4799449
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185959}

--- base/trace_event/memory_usage_estimator.h
+++ base/trace_event/memory_usage_estimator.h
@@ -77,6 +77,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The approach is simple: first call EstimateMemoryUsage() on all members,
 // then recursively fix compilation errors that are caused by types not
 // implementing EstimateMemoryUsage().
+//
+// Note that in the above example, the memory estimates for `id_` and `success_` are
+// intentionally omitted. This is because these members do not allocate any _dynamic_ memory.
+// If, for example, `MyClass` is declared as a heap-allocated `unique_ptr` member in some parent
+// class, then `EstimateMemoryUsage` on the `unique_ptr` will automatically take into account
+// `sizeof(MyClass)`.
 
 namespace base {
 namespace trace_event {
