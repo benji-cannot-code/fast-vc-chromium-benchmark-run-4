82c1e531bded06edd23709bc2b474a38cd911c4c
Kevin Lubick <kjlubick@google.com>
2023-08-21 19:31:15 +0000

    Add more codecs to SkPicture de/serialization
    Bug: chromium:1474162
    Change-Id: Ia13c941632b2f636b123175069b2269c3d9f6e1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4799389
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Kevin Lubick <kjlubick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186011}

--- components/paint_preview/common/serial_utils.cc
+++ components/paint_preview/common/serial_utils.cc
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "components/paint_preview/common/subset_font.h"
+#include "third_party/skia/include/codec/SkBmpDecoder.h"
 #include "third_party/skia/include/codec/SkCodec.h"
+#include "third_party/skia/include/codec/SkGifDecoder.h"
 #include "third_party/skia/include/codec/SkJpegDecoder.h"
 #include "third_party/skia/include/codec/SkPngDecoder.h"
 #include "third_party/skia/include/codec/SkWebpDecoder.h"
@@ -104,6 +106,15 @@ sk_sp<SkData> SerializeTypeface(SkTypeface* typeface, void* ctx) {
   return subset_data;
 }
 
+static bool is_supported_codec(sk_sp<SkData> data) {
+  CHECK(data);
+  return SkBmpDecoder::IsBmp(data->data(), data->size()) ||
+         SkGifDecoder::IsGif(data->data(), data->size()) ||
+         SkPngDecoder::IsPng(data->data(), data->size()) ||
+         SkJpegDecoder::IsJpeg(data->data(), data->size()) ||
+         SkWebpDecoder::IsWebp(data->data(), data->size());
+}
+
 sk_sp<SkData> SerializeImage(SkImage* image, void* ctx) {
   ImageSerializationContext* context =
       reinterpret_cast<ImageSerializationContext*>(ctx);
@@ -125,7 +136,7 @@ sk_sp<SkData> SerializeImage(SkImage* image, void* ctx) {
 
   // If there already exists encoded data use it directly.
   sk_sp<SkData> encoded_data = image->refEncodedData();
-  if (!encoded_data) {
+  if (!encoded_data || !is_supported_codec(encoded_data)) {
     // Use the default PNG at quality 100 as it is safe.
     // TODO(crbug/1198304): Investigate supporting JPEG at quality 100 for
     // opaque images.
@@ -135,10 +146,6 @@ sk_sp<SkData> SerializeImage(SkImage* image, void* ctx) {
   if (!encoded_data) {
     return SkData::MakeEmpty();
   }
-
-  CHECK(SkPngDecoder::IsPng(encoded_data->data(), encoded_data->size()) ||
-        SkJpegDecoder::IsJpeg(encoded_data->data(), encoded_data->size()) ||
-        SkWebpDecoder::IsWebp(encoded_data->data(), encoded_data->size()));
 
   // Ensure the encoded data fits in the size restriction if present.
   // OOM Prevention: This avoids creating/keeping large serialized images
@@ -171,6 +178,12 @@ sk_sp<SkImage> DeserializeImage(const void* bytes, size_t length, void*) {
   };
   if (SkPngDecoder::IsPng(bytes, length)) {
     return get_image(SkPngDecoder::Decode(data, nullptr));
+  }
+  if (SkBmpDecoder::IsBmp(bytes, length)) {
+    return get_image(SkBmpDecoder::Decode(data, nullptr));
+  }
+  if (SkGifDecoder::IsGif(bytes, length)) {
+    return get_image(SkGifDecoder::Decode(data, nullptr));
   }
   if (SkJpegDecoder::IsJpeg(bytes, length)) {
     return get_image(SkJpegDecoder::Decode(data, nullptr));
