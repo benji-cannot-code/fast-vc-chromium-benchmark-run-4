8523fe400256db7e2959d6d6277182efff8c70a4
Sebastien Lalancette <seblalancette@chromium.org>
2023-08-22 18:51:31 +0000

    [EV] Support showing the consent flow for unaffiliated profiles
    Adds support for showing the consent flow when in an unaffiliated
    context based on the UnmanagedDeviceSignalsConsentFlowEnabled policy
    value, behind a disabled feature flag.
    
    Also, this adds support for returning the device signals through the
    new enterprise.reportingPrivate extension APIs when consent has been
    given in those unaffiliated contexts.
    
    Bug: b:296920433
    Change-Id: I5eda536e87f0651a3fa9a4ecd4bec8afe98587c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4799089
    Reviewed-by: Zonghan Xu <xzonghan@chromium.org>
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1186708}

--- components/device_signals/core/common/signals_features.cc
+++ components/device_signals/core/common/signals_features.cc
@@ -61,4 +61,8 @@ bool IsConsentDialogEnabled() {
 #endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) ||
         // BUILDFLAG(IS_CHROMEOS_ASH)
 
+BASE_FEATURE(kNewEvSignalsUnaffiliatedEnabled,
+             "NewEvSignalsUnaffiliatedEnabled",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 }  // namespace enterprise_signals::features
