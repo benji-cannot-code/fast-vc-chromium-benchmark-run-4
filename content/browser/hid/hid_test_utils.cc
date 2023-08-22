14c525d34b9c4da28d3bf58a3b4cc24d91427492
Jack Hsieh <chengweih@chromium.org>
2023-08-22 22:42:27 +0000

    Shutdown process manager after resetting the context core
    This CL moves the step of shutdown the process manager to the end of the
    shutdown so the object that needs to have a valid browser context can
    remove itself from observing other modules properly. For example, it
    would allow the ServiceWorkerHidDelegateObserver destructor to remove
    itself properly from the HidDelegate using a valid browser context.
    
    Bug: 1473204
    Change-Id: I2921aeff6f70bc9104d5201a3230b56c2d3fbf77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4786932
    Commit-Queue: Jack Hsieh <chengweih@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186887}

--- content/browser/hid/hid_test_utils.cc
+++ content/browser/hid/hid_test_utils.cc
@@ -27,11 +27,17 @@ std::unique_ptr<HidChooser> MockHidDelegate::RunChooser(
 
 void MockHidDelegate::AddObserver(BrowserContext* browser_context,
                                   Observer* observer) {
+  if (assert_browser_context_) {
+    ASSERT_TRUE(browser_context);
+  }
   observer_list_.AddObserver(observer);
 }
 
 void MockHidDelegate::RemoveObserver(BrowserContext* browser_context,
                                      Observer* observer) {
+  if (assert_browser_context_) {
+    ASSERT_TRUE(browser_context);
+  }
   observer_list_.RemoveObserver(observer);
 }
 
@@ -62,6 +68,10 @@ void MockHidDelegate::OnHidManagerConnectionError() {
   for (auto& observer : observer_list_) {
     observer.OnHidManagerConnectionError();
   }
+}
+
+void MockHidDelegate::SetAssertBrowserContext(bool assert_browser_context) {
+  assert_browser_context_ = assert_browser_context;
 }
 
 HidTestContentBrowserClient::HidTestContentBrowserClient() = default;
