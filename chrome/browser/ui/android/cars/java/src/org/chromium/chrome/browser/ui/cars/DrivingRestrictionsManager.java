e76c79814f17902c9dd37394dccadcd2f138e263
Theresa Sullivan <twellington@chromium.org>
2023-08-22 22:20:23 +0000

    Revert "Update build to pick right DrivingRestrictionsDelegateImpl"
    This reverts commit e64cfbcc4544a6a566dd422942bcb12a8e63449c.
    
    Reason for revert: compilation broke on missing Google3 classes; need to add proguard keep rules
    
    Original change's description:
    > Update build to pick right DrivingRestrictionsDelegateImpl
    >
    > Low-Coverage-Reason: Uncovered lines flagged are test-only methods. Integration test covering these lines being added downstream.
    > Bug: b/269382065
    > Change-Id: I3e3f623fbb52d9d9874812308d6feb25eeda84a9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4799931
    > Reviewed-by: Charles Hager <clhager@google.com>
    > Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1186765}
    
    Bug: b/269382065
    Change-Id: I528ade9dbe47889d76e9ec3e1877f1e1f1d40bdd
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4803282
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186864}

--- chrome/browser/ui/android/cars/java/src/org/chromium/chrome/browser/ui/cars/DrivingRestrictionsManager.java
+++ chrome/browser/ui/android/cars/java/src/org/chromium/chrome/browser/ui/cars/DrivingRestrictionsManager.java
@@ -16,7 +16,7 @@ import org.chromium.base.ApplicationStatus;
 public class DrivingRestrictionsManager {
     private static DrivingRestrictionsManager sInstance;
 
-    private DrivingRestrictionsDelegateImpl mDelegate;
+    private DrivingRestrictionsDelegate mDelegate;
     private boolean mMonitoring;
 
     /**
@@ -27,8 +27,7 @@ public class DrivingRestrictionsManager {
     }
 
     DrivingRestrictionsManager() {
-        mDelegate =
-                new DrivingRestrictionsDelegateImpl(this::onRequiresDistractionOptimizationChanged);
+        mDelegate = new DrivingRestrictionsDelegate(this::onRequiresDistractionOptimizationChanged);
 
         updateMonitoring(ApplicationStatus.getStateForApplication());
         ApplicationStatus.registerApplicationStateListener(newState -> updateMonitoring(newState));
@@ -52,15 +51,11 @@ public class DrivingRestrictionsManager {
         }
     }
 
-    void setDelegateForTesting(DrivingRestrictionsDelegateImpl delegate) {
+    void setDelegateForTesting(DrivingRestrictionsDelegate delegate) {
         mDelegate = delegate;
     }
 
-    DrivingRestrictionsDelegateImpl getDelegateForTesting() {
+    DrivingRestrictionsDelegate getDelegateForTesting() {
         return mDelegate;
-    }
-
-    static DrivingRestrictionsManager getInstanceForTesting() {
-        return sInstance;
     }
 }
