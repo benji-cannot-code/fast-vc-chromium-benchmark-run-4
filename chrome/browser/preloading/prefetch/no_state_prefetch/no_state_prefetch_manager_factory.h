e8858b3c6efb03497e7d0d9202242b6f51f4aaf2
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 14:31:37 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: Ifede37a1ccb3e6650330b5463a6b4b0f5fe4a7e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800327
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186512}

--- chrome/browser/preloading/prefetch/no_state_prefetch/no_state_prefetch_manager_factory.h
+++ chrome/browser/preloading/prefetch/no_state_prefetch/no_state_prefetch_manager_factory.h
@@ -37,7 +37,7 @@ class NoStatePrefetchManagerFactory : public ProfileKeyedServiceFactory {
   ~NoStatePrefetchManagerFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* profile) const override;
 };
 
