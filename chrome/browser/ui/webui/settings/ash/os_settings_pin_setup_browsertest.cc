8a28e6196270a8edac5b02b205d85fbfeee86be1
Xiaohui Chen <xiaohuic@google.com>
2023-08-22 01:02:02 +0000

    os settings: move cpp files to new location (1/N)
    * Create the new ash directory outside of browser settings
    * Move the first small set of files to test the setup
    
    Bug: b/284187997
    Change-Id: I47fa1f8db357f5ae2c4368d4bafecbd08db0d18a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4795371
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186203}

--- chrome/browser/ui/webui/settings/ash/os_settings_pin_setup_browsertest.cc
+++ chrome/browser/ui/webui/settings/ash/os_settings_pin_setup_browsertest.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/quick_unlock/quick_unlock_utils.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/webui/settings/ash/os_settings_lock_screen_browser_test_base.h"
+#include "chrome/browser/ui/webui/ash/settings/test_support/os_settings_lock_screen_browser_test_base.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/data/webui/settings/chromeos/os_people_page/pin_settings_api.test-mojom-test-utils.h"
 #include "chrome/test/data/webui/settings/chromeos/test_api.test-mojom-test-utils.h"
