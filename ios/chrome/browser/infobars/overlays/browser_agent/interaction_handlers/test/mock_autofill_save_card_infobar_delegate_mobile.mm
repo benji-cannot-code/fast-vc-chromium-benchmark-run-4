a4aad4c2701056e35dcb543c38c559f7aec19595
Slobodan Pejic <slobodan@chromium.org>
2023-08-22 13:57:41 +0000

    Extract ui info and delegate from AutofillSaveCardInfoBarDelegateMobile
    * Move non-infobar code out of AutofillSaveCardInfoBarDelegateMobile to
      be reusable for the upcoming save card bottom sheet on Chrome for
      Android.
    
    Bug: 1454271
    Low-Coverage-Reason: Coverage not run in CQ.
    Change-Id: I968e4683b77615d5318dc22479d9b869e6f7d91c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4721765
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186485}

--- ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/test/mock_autofill_save_card_infobar_delegate_mobile.mm
+++ ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/test/mock_autofill_save_card_infobar_delegate_mobile.mm
@@ -6,8 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/test/mock_autofill_save_card_infobar_delegate_mobile.h"
 
 #import "base/functional/bind.h"
+#import "base/memory/ptr_util.h"
 #import "base/uuid.h"
 #import "components/autofill/core/browser/autofill_test_utils.h"
+#import "components/autofill/core/browser/payments/autofill_save_card_delegate.h"
+#import "components/autofill/core/browser/payments/autofill_save_card_ui_info.h"
 #import "components/autofill/core/browser/payments/test_legal_message_line.h"
 #import "components/signin/public/identity_manager/account_info.h"
 
@@ -20,11 +23,19 @@ MockAutofillSaveCardInfoBarDelegateMobile::
             callback,
         const autofill::LegalMessageLines& legal_message_lines,
         const AccountInfo& displayed_target_account)
-    : AutofillSaveCardInfoBarDelegateMobile(options,
-                                            card,
-                                            std::move(callback),
-                                            legal_message_lines,
-                                            displayed_target_account) {}
+    : AutofillSaveCardInfoBarDelegateMobile(
+          absl::holds_alternative<
+              autofill::AutofillClient::UploadSaveCardPromptCallback>(callback)
+              ? autofill::AutofillSaveCardUiInfo::CreateForUploadSave(
+                    options,
+                    card,
+                    legal_message_lines,
+                    displayed_target_account)
+              : autofill::AutofillSaveCardUiInfo::CreateForLocalSave(options,
+                                                                     card),
+          std::make_unique<autofill::AutofillSaveCardDelegate>(
+              std::move(callback),
+              options)) {}
 
 MockAutofillSaveCardInfoBarDelegateMobile::
     ~MockAutofillSaveCardInfoBarDelegateMobile() = default;
