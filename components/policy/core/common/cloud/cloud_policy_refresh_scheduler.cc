2e6a6e9857328a0faae44ceee2a79a4361ead2c1
Roland Bock <rbock@google.com>
2023-08-22 08:38:41 +0000

    [Policy] Remove superfluous includes and fix comment
    This CL removes unnecessary includes and an in-comment
    reference to CloudPolicyRefreshScheduler::RefreshNow
    which does not exist any more.
    
    Bug: None
    Change-Id: Ibb45248d4d89fb787902f93020483bafd4fb81fe
    Tests: Still compiles (non-functional change)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798217
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1186349}

--- components/policy/core/common/cloud/cloud_policy_refresh_scheduler.cc
+++ components/policy/core/common/cloud/cloud_policy_refresh_scheduler.cc
@@ -6,13 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/cloud/cloud_policy_refresh_scheduler.h"
 
 #include <algorithm>
-#include <memory>
 
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
-#include "base/no_destructor.h"
 #include "base/rand_util.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/time/clock.h"
--- components/policy/core/common/cloud/cloud_policy_refresh_scheduler.h
+++ components/policy/core/common/cloud/cloud_policy_refresh_scheduler.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/cancelable_callback.h"
 #include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
-#include "base/memory/ref_counted.h"
 #include "base/time/clock.h"
 #include "base/time/tick_clock.h"
 #include "base/time/time.h"
@@ -185,8 +184,7 @@ class POLICY_EXPORT CloudPolicyRefreshScheduler
   // The delayed refresh callback.
   base::CancelableOnceClosure refresh_callback_;
 
-  // Whether the refresh is scheduled for soon (using |RefreshSoon| or
-  // |RefreshNow|).
+  // Whether the refresh is scheduled for soon (using |RefreshSoon|).
   bool is_scheduled_for_soon_ = false;
 
   // The last time a policy fetch was attempted or completed.
