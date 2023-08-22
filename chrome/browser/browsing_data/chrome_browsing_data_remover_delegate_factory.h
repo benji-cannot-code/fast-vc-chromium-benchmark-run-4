984f60ec3fa1a1c05ff9d78a7793274723d282a5
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 09:33:02 +0000

    Implements the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one being deprecated,
    this CL replaces it by the first one.
    
    This also ensures that, instead of applying `WrapUnique` to a `new` raw
    ptr, it directly uses `make_unique`.
    
    Change-Id: I5cc56c889d5e5f706accfaee9b91eebb3c4e6c3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798703
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186376}

--- chrome/browser/browsing_data/chrome_browsing_data_remover_delegate_factory.h
+++ chrome/browser/browsing_data/chrome_browsing_data_remover_delegate_factory.h
@@ -37,7 +37,7 @@ class ChromeBrowsingDataRemoverDelegateFactory
   ~ChromeBrowsingDataRemoverDelegateFactory() override;
 
   // BrowserContextKeyedServiceFactory overrides:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
