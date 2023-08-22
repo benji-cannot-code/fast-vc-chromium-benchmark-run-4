6f3279e3b52b604519e52b0d8852641000039173
Juliet Levesque <julietlevesque@google.com>
2023-08-22 23:17:27 +0000

    [Nearby Presence] Record the duration of a successful cred upload
    Adds a metric to log the duration of a successful credential upload
    from the Nearby Presence Web Server by recording the difference
    between the server request and the server response.
    
    Bug: b/276642472
    Change-Id: I601026f4fc338baaf27270e16a32cafd0f9a0945
    Tested: Verified on DUT via chrome://histograms and unit tests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4685490
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186908}

--- chromeos/ash/components/nearby/presence/credentials/nearby_presence_credential_manager_impl_unittest.cc
+++ chromeos/ash/components/nearby/presence/credentials/nearby_presence_credential_manager_impl_unittest.cc
@@ -406,6 +406,8 @@ TEST_F(NearbyPresenceCredentialManagerImplTest, RegistrationSuccess) {
   histogram_tester_.ExpectBucketCount(
       "Nearby.Presence.Credentials.Upload.AttemptsNeededCount",
       /*bucket: attempt_count=*/1, 1);
+  histogram_tester_.ExpectTotalCount(
+      "Nearby.Presence.Credentials.Upload.ServerRequestDuration", 1);
   histogram_tester_.ExpectBucketCount(
       "Nearby.Presence.Credentials.Download.Result", /*bucket: success=*/true,
       1);
@@ -528,6 +530,8 @@ TEST_F(NearbyPresenceCredentialManagerImplTest,
       "Nearby.Presence.Credentials.Upload.FailureReason",
       /*bucket: NearbyHttpResult::kTimeout*/
       ash::nearby::NearbyHttpResult::kTimeout, 1);
+  histogram_tester_.ExpectTotalCount(
+      "Nearby.Presence.Credentials.Upload.ServerRequestDuration", 0);
 }
 
 TEST_F(NearbyPresenceCredentialManagerImplTest, UploadCredentialsFailure) {
@@ -568,6 +572,8 @@ TEST_F(NearbyPresenceCredentialManagerImplTest, UploadCredentialsFailure) {
       "Nearby.Presence.Credentials.Upload.FailureReason",
       /*bucket: NearbyHttpResult::kHttpErrorInternalServerError*/
       ash::nearby::NearbyHttpResult::kHttpErrorInternalServerError, 1);
+  histogram_tester_.ExpectTotalCount(
+      "Nearby.Presence.Credentials.Upload.ServerRequestDuration", 0);
 }
 
 TEST_F(NearbyPresenceCredentialManagerImplTest, DownloadCredentialsFailure) {
@@ -721,6 +727,8 @@ TEST_F(NearbyPresenceCredentialManagerImplTest,
   histogram_tester_.ExpectBucketCount(
       "Nearby.Presence.Credentials.Upload.AttemptsNeededCount",
       /*bucket: attempt_count=*/1, 1);
+  histogram_tester_.ExpectTotalCount(
+      "Nearby.Presence.Credentials.Upload.ServerRequestDuration", 1);
   histogram_tester_.ExpectBucketCount(
       "Nearby.Presence.Credentials.Download.Result", /*bucket: success=*/true,
       1);
@@ -770,6 +778,8 @@ TEST_F(NearbyPresenceCredentialManagerImplTest,
       "Nearby.Presence.Credentials.Upload.FailureReason", 0);
   histogram_tester_.ExpectTotalCount(
       "Nearby.Presence.Credentials.Upload.AttemptsNeededCount", 0);
+  histogram_tester_.ExpectTotalCount(
+      "Nearby.Presence.Credentials.Upload.ServerRequestDuration", 0);
   histogram_tester_.ExpectBucketCount(
       "Nearby.Presence.Credentials.Download.Result", /*bucket: success=*/true,
       1);
@@ -833,6 +843,8 @@ TEST_F(NearbyPresenceCredentialManagerImplTest,
       ash::nearby::NearbyHttpResult::kHttpErrorInternalServerError, 1);
   histogram_tester_.ExpectTotalCount(
       "Nearby.Presence.Credentials.Upload.AttemptsNeededCount", 0);
+  histogram_tester_.ExpectTotalCount(
+      "Nearby.Presence.Credentials.Upload.ServerRequestDuration", 0);
 }
 
 TEST_F(NearbyPresenceCredentialManagerImplTest,
@@ -929,6 +941,8 @@ TEST_F(NearbyPresenceCredentialManagerImplTest,
       "Nearby.Presence.Credentials.Upload.FailureReason", 0);
   histogram_tester_.ExpectTotalCount(
       "Nearby.Presence.Credentials.Upload.AttemptsNeededCount", 0);
+  histogram_tester_.ExpectTotalCount(
+      "Nearby.Presence.Credentials.Upload.ServerRequestDuration", 0);
   histogram_tester_.ExpectBucketCount(
       "Nearby.Presence.Credentials.Download.Result", /*bucket: success=*/true,
       7);
