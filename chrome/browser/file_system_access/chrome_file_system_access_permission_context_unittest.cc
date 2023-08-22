893fe8c08a30cc1e472c9bae579fb2a7a98603f0
Christine Smith <christinesm@chromium.org>
2023-08-22 21:58:03 +0000

    FSA: Update `PersistedPermissionOptions` enum
    Updates the `PersistedPermissionOptions` enum to be private,
    as it is no longer needed publicly.
    
    This cleanup results from the Persistent Permissions
    implementation.
    
    Bug: 1011533
    Change-Id: Ia460a31b49979f11f8b47fdc3a6336a9d1471d91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4803821
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Christine Smith <christinesm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186853}

--- chrome/browser/file_system_access/chrome_file_system_access_permission_context_unittest.cc
+++ chrome/browser/file_system_access/chrome_file_system_access_permission_context_unittest.cc
@@ -55,8 +55,6 @@ using PathType = ChromeFileSystemAccessPermissionContext::PathType;
 using UserAction = ChromeFileSystemAccessPermissionContext::UserAction;
 using PermissionStatus =
     content::FileSystemAccessPermissionGrant::PermissionStatus;
-using PersistedPermissionOptions =
-    ChromeFileSystemAccessPermissionContext::PersistedPermissionOptions;
 using PermissionRequestOutcome =
     content::FileSystemAccessPermissionGrant::PermissionRequestOutcome;
 using SensitiveDirectoryResult =
