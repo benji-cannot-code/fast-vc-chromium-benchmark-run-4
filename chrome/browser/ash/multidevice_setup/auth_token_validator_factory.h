e24d35020cce272f4d979e9afc25763281659b78
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 22:30:40 +0000

    Implements the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one being deprecated,
    this CL replaces it by the first one.
    
    This also ensures that, instead of applying `WrapUnique` to a `new` raw
    ptr, it directly uses `make_unique`.
    
    Change-Id: Id9b93a871d8d98aaa311c279c94e38e119b4916d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4797901
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186873}

--- chrome/browser/ash/multidevice_setup/auth_token_validator_factory.h
+++ chrome/browser/ash/multidevice_setup/auth_token_validator_factory.h
@@ -35,7 +35,7 @@ class AuthTokenValidatorFactory : public ProfileKeyedServiceFactory {
   ~AuthTokenValidatorFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
