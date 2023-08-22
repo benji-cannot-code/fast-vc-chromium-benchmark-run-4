aef46ad03120219dc4fe7f40413f9d85650346e4
Jeffrey Kardatzke <jkardatzke@google.com>
2023-08-22 17:07:09 +0000

    Secure buffer allocation for ARM TrustZone
    Adds the Mojo IPC call to the cdm-oemcrypto daemon on ChromeOS for
    allocating secure buffer in ARM TrustZone. These buffers are then used
    as the target for decryption of content in the secure world.
    
    This call will originate in the browser process and end up in the
    cdm-oemcrypto daemon, except for OOP video decoding where it will
    originate in the utility process and end up in the daemon
    
    The corresponding ChromeOS change is: crrev.com/i/6345151
    
    BUG=b:249588091
    TEST=Allocation + playback works w/ full prototype
    
    Change-Id: I03a63f7e7aa0ff396766ef61bbe9d8a850a9a9aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4799677
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186632}

--- chromeos/components/cdm_factory_daemon/chromeos_cdm_context.h
+++ chromeos/components/cdm_factory_daemon/chromeos_cdm_context.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "media/base/cdm_context.h"
 #include "media/base/decryptor.h"
+#include "mojo/public/cpp/platform/platform_handle.h"
 #include "ui/gfx/geometry/size.h"
 
 namespace media {
@@ -35,6 +36,9 @@ class ChromeOsCdmContext {
                               const std::vector<uint8_t>& config_data)>;
   using GetScreenResolutionsCB =
       base::OnceCallback<void(const std::vector<gfx::Size>& resolutions)>;
+
+  using AllocateSecureBufferCB =
+      base::OnceCallback<void(mojo::PlatformHandle secure_fd)>;
 
   // Gets the HW specific key information for the key specified in
   // |decrypt_config| and returns it via |callback|.
@@ -60,6 +64,11 @@ class ChromeOsCdmContext {
   // operating system. This is used to determine if certain processing has
   // already been done on the data such as transcryption.
   virtual bool IsRemoteCdm() const = 0;
+
+  // Used to allocate a secure buffer for the target of decryption on ARM Trust
+  // Zone implementations.
+  virtual void AllocateSecureBuffer(uint32_t size,
+                                    AllocateSecureBufferCB callback) = 0;
 
  protected:
   virtual ~ChromeOsCdmContext() = default;
