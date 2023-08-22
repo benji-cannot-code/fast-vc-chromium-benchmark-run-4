f276221f903541a53174872d9bf1f2a849656747
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 12:05:48 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: I888d0a13dd0aab9cde0ba75de23a7128942605ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800983
    Reviewed-by: Anunoy Ghosh <anunoy@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186424}

--- chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_service_factory.cc
+++ chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_service_factory.cc
@@ -49,7 +49,8 @@ ExtensionTelemetryServiceFactory::ExtensionTelemetryServiceFactory()
   DependsOn(extensions::ExtensionSystemFactory::GetInstance());
 }
 
-KeyedService* ExtensionTelemetryServiceFactory::BuildServiceInstanceFor(
+std::unique_ptr<KeyedService>
+ExtensionTelemetryServiceFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
   if (!base::FeatureList::IsEnabled(kExtensionTelemetry))
     return nullptr;
@@ -58,8 +59,9 @@ KeyedService* ExtensionTelemetryServiceFactory::BuildServiceInstanceFor(
   if (!network_service) {
     return nullptr;
   }
-  return new ExtensionTelemetryService(Profile::FromBrowserContext(context),
-                                       network_service->GetURLLoaderFactory());
+  return std::make_unique<ExtensionTelemetryService>(
+      Profile::FromBrowserContext(context),
+      network_service->GetURLLoaderFactory());
 }
 
 bool ExtensionTelemetryServiceFactory::ServiceIsCreatedWithBrowserContext()
--- chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_service_factory.h
+++ chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_service_factory.h
@@ -44,7 +44,7 @@ class ExtensionTelemetryServiceFactory : public ProfileKeyedServiceFactory {
   ~ExtensionTelemetryServiceFactory() override = default;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
   bool ServiceIsCreatedWithBrowserContext() const override;
   bool ServiceIsNULLWhileTesting() const override;
