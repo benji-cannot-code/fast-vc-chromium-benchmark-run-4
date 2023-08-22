aa3d8cee339d7a7b5e772b87a418b80b896205f4
Tim Sergeant <tsergeant@chromium.org>
2023-08-22 04:36:29 +0000

    App Provisioning Service: Add a BUILD.gn file
    App Discovery and App Deduplication both depend on Provisioning, so
    creating BUILD.gn files for those directories requires a BUILD.gn file
    here first.
    
    Bug: None
    Change-Id: I4367ef0baed78f7bac2bedb8096990991ae94d64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800829
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186271}

--- chrome/browser/apps/app_provisioning_service/app_provisioning_data_manager.cc
+++ chrome/browser/apps/app_provisioning_service/app_provisioning_data_manager.cc
@@ -7,10 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "base/feature_list.h"
 #include "base/logging.h"
 #include "chrome/browser/apps/app_provisioning_service/proto/app_data.pb.h"
-#include "chrome/common/chrome_features.h"
 
 namespace apps {
 
