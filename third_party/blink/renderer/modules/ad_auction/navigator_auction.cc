da85e18201411ea89653a4cfc18b0a32102b89b7
Maks Orlovich <morlovich@chromium.org>
2023-08-22 20:47:02 +0000

    FLEDGE: Fix browser/renderer secure context mismatch on join/leave
    The renderer checked only [SecureContext], while the browser checked for
    https specifically; the former also accepts http://127.0.0.1, meaning a
    joinInterestGroup on http://127.0.0.1 would be a BadMessage renderer
    kill. This changes tightens up the renderer checks to match.
    
    Change-Id: Ibd48b63a66ca6fe98d70bfbc0ab7edc06abdd763
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4803166
    Reviewed-by: Qingxin Wu <qingxinwu@google.com>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186796}

--- third_party/blink/renderer/modules/ad_auction/navigator_auction.cc
+++ third_party/blink/renderer/modules/ad_auction/navigator_auction.cc
@@ -77,6 +77,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/text/string_operators.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
+#include "url/url_constants.h"
 #include "v8/include/v8-primitive.h"
 #include "v8/include/v8-value.h"
 
@@ -2365,6 +2366,11 @@ ScriptPromise JoinAdInterestGroupInternal(
   }
   RecordCommonFledgeUseCounters(navigator.DomWindow()->document());
   const ExecutionContext* context = ExecutionContext::From(script_state);
+  if (context->GetSecurityOrigin()->Protocol() != url::kHttpsScheme) {
+    exception_state.ThrowSecurityError(
+        "May only joinAdInterestGroup from an https origin.");
+    return ScriptPromise();
+  }
   if (!context->IsFeatureEnabled(
           mojom::blink::PermissionsPolicyFeature::kJoinAdInterestGroup)) {
     exception_state.ThrowDOMException(
@@ -2974,6 +2980,13 @@ ScriptPromise NavigatorAuction::leaveAdInterestGroup(
     return ScriptPromise();
   }
 
+  if (ExecutionContext::From(script_state)->GetSecurityOrigin()->Protocol() !=
+      url::kHttpsScheme) {
+    exception_state.ThrowSecurityError(
+        "May only leaveAdInterestGroup from an https origin.");
+    return ScriptPromise();
+  }
+
   bool is_cross_origin = !ExecutionContext::From(script_state)
                               ->GetSecurityOrigin()
                               ->IsSameOriginWith(owner.get());
@@ -3001,11 +3014,16 @@ ScriptPromise NavigatorAuction::leaveAdInterestGroupForDocument(
     ScriptState* script_state,
     ExceptionState& exception_state) {
   LocalDOMWindow* window = GetSupplementable()->DomWindow();
-
   if (!window) {
     exception_state.ThrowSecurityError(
         "May not leaveAdInterestGroup from a Document that is not fully "
         "active");
+    return ScriptPromise();
+  }
+  if (ExecutionContext::From(script_state)->GetSecurityOrigin()->Protocol() !=
+      url::kHttpsScheme) {
+    exception_state.ThrowSecurityError(
+        "May only leaveAdInterestGroup from an https origin.");
     return ScriptPromise();
   }
   if (!window->GetFrame()->IsInFencedFrameTree()) {
