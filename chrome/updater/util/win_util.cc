7a001adcb0e5f79d3a2fe44b0931b1c7d6e95cef
Sorin Jianu <sorin@chromium.org>
2023-08-22 21:38:51 +0000

    updater: dump the return value and args when expecting the STA apt.
    Bug: 1473487
    Change-Id: Idd5e65ed5133c26a4f807a8869e530dc982ea4e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4804403
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186843}

--- chrome/updater/util/win_util.cc
+++ chrome/updater/util/win_util.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/cxx20_erase.h"
 #include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
+#include "base/debug/alias.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/functional/callback_helpers.h"
@@ -1056,6 +1057,19 @@ bool IsSTA() {
   APTTYPEQUALIFIER apt_type_qualifier = APTTYPEQUALIFIER_NONE;
   return SUCCEEDED(::CoGetApartmentType(&apt_type, &apt_type_qualifier)) &&
          (apt_type == APTTYPE_STA || apt_type == APTTYPE_MAINSTA);
+}
+
+void ExpectIsSTA() {
+  VLOG(2) << __func__;
+  HRESULT hr = S_OK;
+  base::debug::Alias(&hr);
+  APTTYPE apt_type = APTTYPE_CURRENT;
+  base::debug::Alias(&apt_type);
+  APTTYPEQUALIFIER apt_type_qualifier = APTTYPEQUALIFIER_NONE;
+  base::debug::Alias(&apt_type_qualifier);
+  hr = ::CoGetApartmentType(&apt_type, &apt_type_qualifier);
+  DUMP_WILL_BE_CHECK(SUCCEEDED(hr) &&
+                     (apt_type == APTTYPE_STA || apt_type == APTTYPE_MAINSTA));
 }
 
 }  // namespace updater
