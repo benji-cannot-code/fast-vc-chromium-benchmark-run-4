d7afc0df74d46e6ae03498ca743cf55e2becd25f
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 12:27:38 +0000

    Implements the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one being deprecated,
    this CL replaces it by the first one.
    
    This also ensures that, instead of applying `WrapUnique` to a `new` raw
    ptr, it directly uses `make_unique`.
    
    Change-Id: Ic6ee395b8fe3eced35c8b8c4f7a3d3000e2bfb3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4797784
    Commit-Queue: Alexander Hendrich <hendrich@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186434}

--- chrome/browser/ash/login/extensions/login_screen_extensions_content_script_manager_factory.cc
+++ chrome/browser/ash/login/extensions/login_screen_extensions_content_script_manager_factory.cc
@@ -44,9 +44,9 @@ LoginScreenExtensionsContentScriptManagerFactory::
 LoginScreenExtensionsContentScriptManagerFactory::
     ~LoginScreenExtensionsContentScriptManagerFactory() = default;
 
-KeyedService*
-LoginScreenExtensionsContentScriptManagerFactory::BuildServiceInstanceFor(
-    content::BrowserContext* context) const {
+std::unique_ptr<KeyedService> LoginScreenExtensionsContentScriptManagerFactory::
+    BuildServiceInstanceForBrowserContext(
+        content::BrowserContext* context) const {
   Profile* profile = Profile::FromBrowserContext(context);
   if (!profile)
     return nullptr;
@@ -54,7 +54,7 @@ LoginScreenExtensionsContentScriptManagerFactory::BuildServiceInstanceFor(
     // The manager should only be created for the sign-in profile.
     return nullptr;
   }
-  return new LoginScreenExtensionsContentScriptManager(profile);
+  return std::make_unique<LoginScreenExtensionsContentScriptManager>(profile);
 }
 
 bool LoginScreenExtensionsContentScriptManagerFactory::
--- chrome/browser/ash/login/extensions/login_screen_extensions_content_script_manager_factory.h
+++ chrome/browser/ash/login/extensions/login_screen_extensions_content_script_manager_factory.h
@@ -36,7 +36,7 @@ class LoginScreenExtensionsContentScriptManagerFactory
   ~LoginScreenExtensionsContentScriptManagerFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
   bool ServiceIsCreatedWithBrowserContext() const override;
 };
