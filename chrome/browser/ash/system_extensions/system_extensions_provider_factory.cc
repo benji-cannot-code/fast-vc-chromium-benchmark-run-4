8d07bdcb1cc4412c74e5843ca84da5939c80ec39
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 00:26:08 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: Id9635b8ba7d4422b3d71eb79a0f8e60c4fbb197e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800110
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186188}

--- chrome/browser/ash/system_extensions/system_extensions_provider_factory.cc
+++ chrome/browser/ash/system_extensions/system_extensions_provider_factory.cc
@@ -39,9 +39,11 @@ SystemExtensionsProviderFactory::SystemExtensionsProviderFactory()
 
 SystemExtensionsProviderFactory::~SystemExtensionsProviderFactory() = default;
 
-KeyedService* SystemExtensionsProviderFactory::BuildServiceInstanceFor(
+std::unique_ptr<KeyedService>
+SystemExtensionsProviderFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
-  return new SystemExtensionsProvider(Profile::FromBrowserContext(context));
+  return std::make_unique<SystemExtensionsProvider>(
+      Profile::FromBrowserContext(context));
 }
 
 bool SystemExtensionsProviderFactory::ServiceIsCreatedWithBrowserContext()
