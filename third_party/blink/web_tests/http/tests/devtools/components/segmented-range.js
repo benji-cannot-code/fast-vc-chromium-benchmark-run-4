a8788672a02767285c813efbfc831dce4dabe34f
Simon Zünd <szuend@chromium.org>
2023-08-22 06:12:55 +0000

    [DevTools] Replace `Common.Segment` global with import
    R=wolfi@chromium.org
    
    Bug: chromium:1442410
    Change-Id: I6905c303b044c3b7c9069b1258b8cdfeedfda465
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4797771
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186305}

--- third_party/blink/web_tests/http/tests/devtools/components/segmented-range.js
+++ third_party/blink/web_tests/http/tests/devtools/components/segmented-range.js
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {TestRunner} from 'test_runner';
 import {DataGridTestRunner} from 'data_grid_test_runner';
 
+import * as Common from 'devtools/core/common/common.js';
+
 (async function() {
   TestRunner.addResult(`Tests SegmentedRange\n`);
 
@@ -13,13 +15,13 @@ import {DataGridTestRunner} from 'data_grid_test_runner';
     TestRunner.addResult('Test case: ' + testName);
     TestRunner.addResult('Input Segments: ' + JSON.stringify(data));
 
-    var forwardRange = new Common.SegmentedRange(merge);
-    data.map(entry => new Common.Segment(entry[0], entry[1], entry[2])).forEach(forwardRange.append, forwardRange);
+    var forwardRange = new Common.SegmentedRange.SegmentedRange(merge);
+    data.map(entry => new Common.SegmentedRange.Segment(entry[0], entry[1], entry[2])).forEach(forwardRange.append, forwardRange);
     var forward = forwardRange.segments();
 
-    var backwardRange = new Common.SegmentedRange(merge);
+    var backwardRange = new Common.SegmentedRange.SegmentedRange(merge);
     data.reverse()
-        .map(entry => new Common.Segment(entry[0], entry[1], entry[2]))
+        .map(entry => new Common.SegmentedRange.Segment(entry[0], entry[1], entry[2]))
         .forEach(backwardRange.append, backwardRange);
     var backward = backwardRange.segments();
 
