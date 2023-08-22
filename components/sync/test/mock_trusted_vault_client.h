461ee89bd85465e5b6203a7a036b0ea0088c90db
Maksim Moskvitin <mmoskvitin@google.com>
2023-08-22 16:10:39 +0000

    [TrustedVault] Add reusable FakeTrustedVaultClient
    This CL introduces FakeTrustedVaultClient, based on the code from
    TestTrustedVaultClient located in sync_service_crypto_unittest with
    minimal changes (making FakeServer nested class).
    
    Motivation:
    1. There are more places coming that needs FakeTrustedVaultClient,
    multiple cases for Lacros implementation.
    2. There is a mock implementation, that can be replaced with fake one
    (in follow up CL).
    
    Bug: 1434667
    Change-Id: I1076db297c69fdef61a8ef7df4e284d2fb825375
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4802129
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1186583}

--- components/sync/test/mock_trusted_vault_client.h
+++ components/sync/test/mock_trusted_vault_client.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace syncer {
 
+// TODO(crbug.com/1434667): replace usages with
+// trusted_vault::FakeTrustedVaultClient and delete this mock.
 class MockTrustedVaultClient : public trusted_vault::TrustedVaultClient {
  public:
   MockTrustedVaultClient();
