71758f31fb5759dcfa16adb1011f00de33530355
Greg Thompson <grt@chromium.org>
2023-08-22 08:59:44 +0000

    Disable flaky CookieControlsBubbleViewPixelTest on Windows
    Bug: 1472744
    Change-Id: I4e8c1251fdd95c060f64e897c45480a732bbc27d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798456
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1186363}

--- chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_bubble_view_pixel_test.cc
+++ chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_bubble_view_pixel_test.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
+#include "build/build_config.h"
 #include "chrome/browser/content_settings/cookie_settings_factory.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/dips/dips_service.h"
@@ -204,7 +205,13 @@ class CookieControlsBubbleViewPixelTest
   raw_ptr<CookieControlsBubbleCoordinator> cookie_controls_coordinator_;
 };
 
-IN_PROC_BROWSER_TEST_P(CookieControlsBubbleViewPixelTest, InvokeUi) {
+// Disabled on Windows due to flakes; see https://crbug.com/1472744.
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_InvokeUi DISABLED_InvokeUi
+#else
+#define MAYBE_InvokeUi InvokeUi
+#endif
+IN_PROC_BROWSER_TEST_P(CookieControlsBubbleViewPixelTest, MAYBE_InvokeUi) {
   ShowAndVerifyUi();
 }
 
