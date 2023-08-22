b2379dc64b5ba052eab77fcc4c43040a79e5fb6a
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 07:37:34 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: Ic332c48b1bd9bd78349d2f977bee3a39177ad35d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798506
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186329}

--- chrome/browser/extensions/api/passwords_private/passwords_private_delegate_factory.h
+++ chrome/browser/extensions/api/passwords_private/passwords_private_delegate_factory.h
@@ -63,7 +63,7 @@ class PasswordsPrivateDelegateFactory : public ProfileKeyedServiceFactory {
   ~PasswordsPrivateDelegateFactory() override;
 
   // BrowserContextKeyedServiceFactory implementation.
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* profile) const override;
 };
 
