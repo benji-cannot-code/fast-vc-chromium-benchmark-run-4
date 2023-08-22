bf785bb51064c0c4c6db3a7abeb35cd7e5b29f32
Tim Judkins <tjudkins@chromium.org>
2023-08-22 00:31:01 +0000

    [Extensions] Refactor APIBindingUnittest to prefer returns over globals
    This CL does some refactoring in the test to rely on returns from
    running JS where possible, rather than relying on JS globals being set
    and the test retrieving them.
    
    Bug: N/A
    Change-Id: Ic20d6247e74b954ca31ce3db4d3b0a0ba38ba2f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4794459
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186190}

--- extensions/renderer/bindings/api_binding_unittest.cc
+++ extensions/renderer/bindings/api_binding_unittest.cc
@@ -2289,21 +2289,14 @@ TEST_F(APIBindingUnittest, PromiseBasedAPIs) {
   // request is completed with a value, the promise will be resolved with that
   // value.
   {
-    constexpr char kFunctionCall[] =
-        R"((function(api) {
-             this.apiResult = api.supportsPromises(3);
-             this.apiResult.then((strResult) => {
-               this.promiseResult = strResult;
-             });
-           }))";
-    v8::Local<v8::Function> promise_api_call =
-        FunctionFromString(context, kFunctionCall);
+    v8::Local<v8::Function> promise_api_call = FunctionFromString(
+        context, "(function(api) { return api.supportsPromises(3); })");
     v8::Local<v8::Value> args[] = {binding_object};
-    RunFunctionOnGlobal(promise_api_call, context, std::size(args), args);
+    v8::Local<v8::Value> result =
+        RunFunctionOnGlobal(promise_api_call, context, std::size(args), args);
 
     v8::Local<v8::Promise> promise;
-    ASSERT_TRUE(GetPropertyFromObjectAs(context->Global(), context, "apiResult",
-                                        &promise));
+    ASSERT_TRUE(GetValueAs(result, &promise));
     EXPECT_EQ(v8::Promise::kPending, promise->State());
 
     ASSERT_TRUE(last_request());
@@ -2313,8 +2306,6 @@ TEST_F(APIBindingUnittest, PromiseBasedAPIs) {
 
     EXPECT_EQ(v8::Promise::kFulfilled, promise->State());
     EXPECT_EQ(R"("foo")", V8ToString(promise->Result(), context));
-    EXPECT_EQ(R"("foo")", GetStringPropertyFromObject(
-                              context->Global(), context, "promiseResult"));
   }
   // Also test that promise-based APIs still support passing a callback.
   {
@@ -2390,12 +2381,8 @@ TEST_F(APIBindingUnittest, PromiseBasedAPIs) {
   // required callback isn't supplied.
   context_allows_promises = false;
   {
-    constexpr char kPromiseFunctionCall[] =
-        R"((function(api) {
-             this.apiResult = api.supportsPromises(3);
-           }))";
-    v8::Local<v8::Function> promise_api_call =
-        FunctionFromString(context, kPromiseFunctionCall);
+    v8::Local<v8::Function> promise_api_call = FunctionFromString(
+        context, "(function(api) { return api.supportsPromises(3) });");
     v8::Local<v8::Value> args[] = {binding_object};
     auto expected_error =
         "Uncaught TypeError: " +
@@ -2430,19 +2417,13 @@ TEST_F(APIBindingUnittest, PromiseBasedAPIs) {
   // If a returns_async field is marked as optional, then a context which
   // doesn't support promises should be able to leave it off of the call.
   {
-    constexpr char kCallbackOptionalFunctionCall[] =
-        R"((function(api) {
-             this.callbackOptionalResult = api.callbackOptional(3);
-           }))";
-    v8::Local<v8::Function> promise_api_call =
-        FunctionFromString(context, kCallbackOptionalFunctionCall);
+    v8::Local<v8::Function> promise_api_call = FunctionFromString(
+        context, "(function(api) { return api.callbackOptional(3) });");
     v8::Local<v8::Value> args[] = {binding_object};
-    RunFunctionOnGlobal(promise_api_call, context, std::size(args), args);
+    v8::Local<v8::Value> api_result =
+        RunFunctionOnGlobal(promise_api_call, context, std::size(args), args);
 
     ASSERT_TRUE(last_request());
-
-    v8::Local<v8::Value> api_result = GetPropertyFromObject(
-        context->Global(), context, "callbackOptionalResult");
     ASSERT_TRUE(api_result->IsNullOrUndefined());
   }
 }
