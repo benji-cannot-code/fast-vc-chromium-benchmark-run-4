f9b8882d18662ebe6e6e9d51bc8b6c8cad8af393
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 14:34:24 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: I4748fc1a4cfd3e0d494934732000145ddd70bf10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798397
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186513}

--- chrome/browser/webid/federated_identity_auto_reauthn_permission_context_factory.cc
+++ chrome/browser/webid/federated_identity_auto_reauthn_permission_context_factory.cc
@@ -43,8 +43,10 @@ FederatedIdentityAutoReauthnPermissionContextFactory::
 FederatedIdentityAutoReauthnPermissionContextFactory::
     ~FederatedIdentityAutoReauthnPermissionContextFactory() = default;
 
-KeyedService*
-FederatedIdentityAutoReauthnPermissionContextFactory::BuildServiceInstanceFor(
-    content::BrowserContext* profile) const {
-  return new FederatedIdentityAutoReauthnPermissionContext(profile);
+std::unique_ptr<KeyedService>
+FederatedIdentityAutoReauthnPermissionContextFactory::
+    BuildServiceInstanceForBrowserContext(
+        content::BrowserContext* profile) const {
+  return std::make_unique<FederatedIdentityAutoReauthnPermissionContext>(
+      profile);
 }
