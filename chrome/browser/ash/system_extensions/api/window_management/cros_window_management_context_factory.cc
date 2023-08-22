d38b20d95e4b226e383fde259d2d8561da203fd3
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 00:10:19 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: Ie70a060f6ebfb0e0b40467cf1928f13a3975c184
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4797942
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186174}

--- chrome/browser/ash/system_extensions/api/window_management/cros_window_management_context_factory.cc
+++ chrome/browser/ash/system_extensions/api/window_management/cros_window_management_context_factory.cc
@@ -39,9 +39,11 @@ CrosWindowManagementContextFactory::CrosWindowManagementContextFactory()
 CrosWindowManagementContextFactory::~CrosWindowManagementContextFactory() =
     default;
 
-KeyedService* CrosWindowManagementContextFactory::BuildServiceInstanceFor(
+std::unique_ptr<KeyedService>
+CrosWindowManagementContextFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
-  return new CrosWindowManagementContext(Profile::FromBrowserContext(context));
+  return std::make_unique<CrosWindowManagementContext>(
+      Profile::FromBrowserContext(context));
 }
 
 bool CrosWindowManagementContextFactory::ServiceIsCreatedWithBrowserContext()
