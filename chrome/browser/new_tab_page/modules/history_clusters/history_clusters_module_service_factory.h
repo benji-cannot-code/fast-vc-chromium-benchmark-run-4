aa8f947071117b572cb94821668aa1c1c2775880
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 16:04:31 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: Ib83d6eed97c8ab7238db260e467160006178e38c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800321
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186579}

--- chrome/browser/new_tab_page/modules/history_clusters/history_clusters_module_service_factory.h
+++ chrome/browser/new_tab_page/modules/history_clusters/history_clusters_module_service_factory.h
@@ -25,7 +25,7 @@ class HistoryClustersModuleServiceFactory : public ProfileKeyedServiceFactory {
   ~HistoryClustersModuleServiceFactory() override;
 
   // ProfileKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
   bool ServiceIsCreatedWithBrowserContext() const override;
   bool ServiceIsNULLWhileTesting() const override;
