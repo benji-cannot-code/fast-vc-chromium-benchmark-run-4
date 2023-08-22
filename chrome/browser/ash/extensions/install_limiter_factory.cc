c58e7ebae22cb78e7f10f4b64755c664aa34b562
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 23:30:06 +0000

    Implement unique_ptr builder for InstallLimiterFactory
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: I8dd0e81e1e75d69e50ee24bd983eae0322e34c8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4799913
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186920}

--- chrome/browser/ash/extensions/install_limiter_factory.cc
+++ chrome/browser/ash/extensions/install_limiter_factory.cc
@@ -38,9 +38,10 @@ InstallLimiterFactory::InstallLimiterFactory()
 
 InstallLimiterFactory::~InstallLimiterFactory() = default;
 
-KeyedService* InstallLimiterFactory::BuildServiceInstanceFor(
+std::unique_ptr<KeyedService>
+InstallLimiterFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* profile) const {
-  return new InstallLimiter();
+  return std::make_unique<InstallLimiter>();
 }
 
 }  // namespace extensions
--- chrome/browser/ash/extensions/install_limiter_factory.h
+++ chrome/browser/ash/extensions/install_limiter_factory.h
@@ -32,7 +32,7 @@ class InstallLimiterFactory : public ProfileKeyedServiceFactory {
   ~InstallLimiterFactory() override;
 
   // BrowserContextKeyedServiceFactory overrides:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* profile) const override;
 };
 
