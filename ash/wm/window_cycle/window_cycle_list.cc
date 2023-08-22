c45d388ee95366b431cfa62d987c64e8acdd48d1
Michele Fan <michelefan@chromium.org>
2023-08-22 23:11:42 +0000

    snap-group: Group cycle item creation and removal
    This cl creates `GroupContainerView` for snap group in window
    cycle view, which will be the direct child view of the
    `mirror_container_`, which will be used as the focus target
    for snap group.
    This cl also handles window destruction, for free forms windows
    the `WindowCycleItemView` will be removed. For group item,
    however, we will only remove the child view that represents
    the destroying window. With the newly added `TryRemovingItem`,
    `WindowCycleView` would know whether to clean up the corresponding
    list or not.
    
    Fixed: b/293366018
    Demo: http://b/293366018#comment2
    Test: Added new unit tests + manual
    Change-Id: Ib6c207a5d88dff34b57d028e1b31967c1f272c47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4786951
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186902}

--- ash/wm/window_cycle/window_cycle_list.cc
+++ ash/wm/window_cycle/window_cycle_list.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/accessibility/accessibility_controller_impl.h"
 #include "ash/app_list/app_list_controller_impl.h"
-#include "ash/constants/ash_features.h"
 #include "ash/frame_throttler/frame_throttling_controller.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/public/cpp/window_properties.h"
@@ -17,12 +16,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/tray_background_view.h"
 #include "ash/wm/mru_window_tracker.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
+#include "ash/wm/window_cycle/window_cycle_view.h"
 #include "ash/wm/window_state.h"
 #include "ash/wm/window_util.h"
 #include "base/check.h"
 #include "base/location.h"
 #include "base/metrics/histogram_functions.h"
-#include "base/metrics/user_metrics.h"
 #include "base/ranges/algorithm.h"
 #include "ui/aura/scoped_window_targeter.h"
 #include "ui/aura/window.h"
@@ -284,7 +283,6 @@ void WindowCycleList::OnWindowDestroying(aura::Window* window) {
   window->RemoveObserver(this);
 
   WindowList::iterator i = base::ranges::find(windows_, window);
-  // TODO(oshima): Change this back to DCHECK once crbug.com/483491 is fixed.
   CHECK(i != windows_.end());
   int removed_index = static_cast<int>(i - windows_.begin());
   windows_.erase(i);
--- ash/wm/window_cycle/window_cycle_list.h
+++ ash/wm/window_cycle/window_cycle_list.h
@@ -11,14 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/wm/window_cycle/window_cycle_controller.h"
-#include "ash/wm/window_cycle/window_cycle_view.h"
 #include "base/memory/raw_ptr.h"
 #include "base/timer/timer.h"
 #include "ui/aura/window_observer.h"
 #include "ui/display/display_observer.h"
-#include "ui/display/screen.h"
-#include "ui/views/controls/label.h"
-#include "ui/views/view.h"
+#include "ui/events/event.h"
 
 namespace aura {
 class ScopedWindowTargeter;
@@ -30,6 +27,8 @@ class Widget;
 }
 
 namespace ash {
+
+class WindowCycleView;
 
 // Tracks a set of Windows that can be stepped through. This class is used by
 // the WindowCycleController.
