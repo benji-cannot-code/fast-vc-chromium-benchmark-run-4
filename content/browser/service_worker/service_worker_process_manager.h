ff1e09ad1286a9ae5e345a3a0d699629ffc6dc66
Nidhi Jaju <nidhijaju@chromium.org>
2023-08-22 02:09:41 +0000

    Revert "service worker: ServiceWorkerContextWrapper to maintain browser context"
    This reverts commit b53126e0ddd699682d688e18dd1f5aa18aec69a3.
    
    Reason for revert: Causing consistent test failures on Linux Tests (Wayland) builder
    i.e. https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests%20(Wayland)/41977/test-results?sortby=&groupby=
    
    Original change's description:
    > service worker: ServiceWorkerContextWrapper to maintain browser context
    >
    > This CL makes ServiceWorkerContextWrapper to maintain the browser
    > context by itself rather than having the dependency of
    > ServiceWorkerProcessManager. This is to avoid the complication when
    > destroying its own members. For example, ServiceWorkerContextCore
    > requires a valid browser context during destruction and
    > ServiceWorkerContextWrapper ends up needing to be careful on the
    > sequence of shutting down ServiceWorkerProcessManager and destroying
    > ServiceWorkerContextCore.
    >
    > Bug: 1473204
    > Change-Id: I9a8a82f3e228fda41041be74f88b308e776d8f53
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4791907
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    > Commit-Queue: Jack Hsieh <chengweih@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1186148}
    
    Bug: 1473204
    Change-Id: Ifdef33cfd43fdfe3539ee81a549d8ed63bc3efad
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800630
    Owners-Override: Nidhi Jaju <nidhijaju@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186227}

--- content/browser/service_worker/service_worker_process_manager.h
+++ content/browser/service_worker/service_worker_process_manager.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/synchronization/lock.h"
 #include "content/browser/service_worker/service_worker_metrics.h"
 #include "content/common/content_export.h"
 #include "services/network/public/mojom/cross_origin_embedder_policy.mojom-forward.h"
@@ -25,6 +26,7 @@ class GURL;
 
 namespace content {
 
+class BrowserContext;
 class SiteInstance;
 class StoragePartitionImpl;
 
@@ -45,11 +47,15 @@ class CONTENT_EXPORT ServiceWorkerProcessManager {
     ServiceWorkerMetrics::StartSituation start_situation;
   };
 
-  // |*this| must be owned by a ServiceWorkerContextWrapper.
-  ServiceWorkerProcessManager();
+  // |*this| must be owned by a ServiceWorkerContextWrapper in a
+  // StoragePartition within |browser_context|.
+  explicit ServiceWorkerProcessManager(BrowserContext* browser_context);
 
   // Shutdown must be called before the ProcessManager is destroyed.
   ~ServiceWorkerProcessManager();
+
+  // Called on the UI thread.
+  BrowserContext* browser_context();
 
   // Synchronously prevents new processes from being allocated
   // and drops references to RenderProcessHosts. Called on the UI thread.
@@ -117,6 +123,14 @@ class CONTENT_EXPORT ServiceWorkerProcessManager {
  private:
   friend class ServiceWorkerProcessManagerTest;
 
+  // Guarded by |browser_context_lock_|.
+  // Written only on the UI thread, so the UI thread doesn't need to acquire the
+  // lock when reading. Can be read from other threads with the lock.
+  raw_ptr<BrowserContext, DanglingUntriaged> browser_context_;
+
+  // Protects |browser_context_|.
+  base::Lock browser_context_lock_;
+
   //////////////////////////////////////////////////////////////////////////////
   // All fields below are only accessed on the UI thread.
 
@@ -136,9 +150,6 @@ class CONTENT_EXPORT ServiceWorkerProcessManager {
   int new_process_id_for_test_;
 
   bool force_new_process_for_test_;
-
-  // If it has been shut down.
-  bool is_shutdown_ = false;
 
   // Used to double-check that we don't access *this after it's destroyed.
   base::WeakPtr<ServiceWorkerProcessManager> weak_this_;
