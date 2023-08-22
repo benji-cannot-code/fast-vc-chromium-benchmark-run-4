2145ef7064941d8f25eedd658c2ad98b49502c9e
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 14:10:51 +0000

    Implements the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one being deprecated,
    this CL replaces it by the first one.
    
    Bug: 1396138
    Change-Id: Icc8076389e262878738402532d31ac1b48c0af1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798103
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186496}

--- chrome/browser/autocomplete/remote_suggestions_service_factory.h
+++ chrome/browser/autocomplete/remote_suggestions_service_factory.h
@@ -30,7 +30,7 @@ class RemoteSuggestionsServiceFactory : public ProfileKeyedServiceFactory {
   ~RemoteSuggestionsServiceFactory() override;
 
   // Overrides from BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
