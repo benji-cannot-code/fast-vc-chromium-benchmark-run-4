95ff7ba219751607ba4e8a89cfeec89c26489638
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 16:04:09 +0000

    a11y: migrate live captions to new KeyedService builder API
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one being deprecated,
    this CL replaces it by the first one.
    
    Bug: 1396138
    Change-Id: I793f8f87ae7cad3e43e7ebf0835bc2fd00f73c61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798591
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186578}

--- chrome/browser/accessibility/live_caption/live_caption_controller_factory.h
+++ chrome/browser/accessibility/live_caption/live_caption_controller_factory.h
@@ -32,7 +32,7 @@ class LiveCaptionControllerFactory : public ProfileKeyedServiceFactory {
 
   // BrowserContextKeyedServiceFactory:
   bool ServiceIsCreatedWithBrowserContext() const override;
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* profile) const override;
 };
 
