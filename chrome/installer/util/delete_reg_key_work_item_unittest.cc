61bcb74e9fb93bc0eb1337293a3ad8d6cf7ac1d1
Greg Thompson <grt@chromium.org>
2023-08-22 06:47:51 +0000

    [windows] Switch from ATL to base::win::SecurityDescriptor
    Bug: 5027
    Change-Id: Ie8aa612bf5c0ef9ad08760be8c1a5c1596ebfaa7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4790201
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186314}

--- chrome/installer/util/delete_reg_key_work_item_unittest.cc
+++ chrome/installer/util/delete_reg_key_work_item_unittest.cc
@@ -5,15 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/installer/util/delete_reg_key_work_item.h"
 
-#include <windows.h>
-
-#include <atlsecurity.h>  // NOLINT
 #include <stddef.h>
+#include <windows.h>
 
 #include <memory>
 
 #include "base/logging.h"
 #include "base/win/registry.h"
+#include "base/win/security_descriptor.h"
 #include "chrome/installer/util/registry_test_data.h"
 #include "chrome/installer/util/work_item.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -104,17 +103,21 @@ TEST_F(DeleteRegKeyWorkItemTest, DISABLED_TestUndeletableKey) {
   EXPECT_EQ(ERROR_SUCCESS,
             subkey2.Create(subkey.Handle(), L"Subkey2", KEY_WRITE | WRITE_DAC));
   EXPECT_EQ(ERROR_SUCCESS, subkey2.WriteValue(L"", 2U));
-  CSecurityDesc sec_desc;
-  sec_desc.FromString(L"D:PAI(A;OICI;KR;;;BU)");  // builtin users read
-  EXPECT_EQ(ERROR_SUCCESS,
-            RegSetKeySecurity(subkey.Handle(), DACL_SECURITY_INFORMATION,
-                              const_cast<SECURITY_DESCRIPTOR*>(
-                                  sec_desc.GetPSECURITY_DESCRIPTOR())));
-  sec_desc.FromString(L"D:PAI(A;OICI;KA;;;BU)");  // builtin users all access
+  // builtin users read.
+  auto sd = base::win::SecurityDescriptor::FromSddl(L"D:PAI(A;OICI;KR;;;BU)");
+  ASSERT_TRUE(sd.has_value());
+  SECURITY_DESCRIPTOR sec_desc;
+  sd->ToAbsolute(sec_desc);
+  EXPECT_EQ(
+      ERROR_SUCCESS,
+      RegSetKeySecurity(subkey.Handle(), DACL_SECURITY_INFORMATION, &sec_desc));
+  // builtin users all access.
+  sd = base::win::SecurityDescriptor::FromSddl(L"D:PAI(A;OICI;KA;;;BU)");
+  ASSERT_TRUE(sd.has_value());
+  sd->ToAbsolute(sec_desc);
   EXPECT_EQ(ERROR_SUCCESS,
             RegSetKeySecurity(subkey2.Handle(), DACL_SECURITY_INFORMATION,
-                              const_cast<SECURITY_DESCRIPTOR*>(
-                                  sec_desc.GetPSECURITY_DESCRIPTOR())));
+                              &sec_desc));
   subkey2.Close();
   subkey.Close();
   key.Close();
@@ -136,10 +139,9 @@ TEST_F(DeleteRegKeyWorkItemTest, DISABLED_TestUndeletableKey) {
   EXPECT_EQ(ERROR_SUCCESS, key.ReadValueDW(L"SomeValue", &dw_value));
   EXPECT_EQ(1U, dw_value);
   // Give users all access to the subkey so it can be deleted.
-  EXPECT_EQ(ERROR_SUCCESS,
-            RegSetKeySecurity(key.Handle(), DACL_SECURITY_INFORMATION,
-                              const_cast<SECURITY_DESCRIPTOR*>(
-                                  sec_desc.GetPSECURITY_DESCRIPTOR())));
+  EXPECT_EQ(
+      ERROR_SUCCESS,
+      RegSetKeySecurity(key.Handle(), DACL_SECURITY_INFORMATION, &sec_desc));
   EXPECT_EQ(ERROR_SUCCESS, key.OpenKey(L"Subkey2", KEY_QUERY_VALUE));
   EXPECT_EQ(ERROR_SUCCESS, key.ReadValueDW(L"", &dw_value));
   EXPECT_EQ(2U, dw_value);
