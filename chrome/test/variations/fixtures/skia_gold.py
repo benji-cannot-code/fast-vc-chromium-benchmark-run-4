5ba49f5830974779087ed9c57fe05f7683bb1188
Hao Wu <haowoo@google.com>
2023-08-22 17:13:11 +0000

    Format test result and report additional info to result_sink.
    - skia failure will be reported as a tag with a link
    - test file source location is recorded
    - test failure is shortened and details will be posted as an artifact
    
    Bug: b/280622260
    Change-Id: I8f3f00b27d3b6701cd0a3b3e123ec2d0e415de8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4787450
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Hao Wu <haowoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1186635}

--- chrome/test/variations/fixtures/skia_gold.py
+++ chrome/test/variations/fixtures/skia_gold.py
@@ -8,7 +8,6 @@ import base64
 import functools
 import os
 import shutil
-import subprocess
 import sys
 
 import attr
@@ -41,6 +40,7 @@ class VariationsSkiaGoldUtil:
   skia_gold_session: SkiaGoldSession = attr.attrib()
   test_name: str = attr.attrib()
   use_luci: bool = attr.attrib()
+  request: pytest.FixtureRequest = attr.attrib()
 
   def _png_file_for_name(self, name: str) -> str:
     """Returns a file name that should be used for diff comparison."""
@@ -65,10 +65,14 @@ class VariationsSkiaGoldUtil:
     image_name = f'{self.test_name}:{name}'
     status, error_msg = self.skia_gold_session.RunComparison(
       name=image_name, png_file=png_file, use_luci=self.use_luci)
-    return status, (
-      f'{error_msg}'
-      f'{self.skia_gold_session.GetTriageLinks(image_name)}'
-    )
+
+    # Screenshots for variations are in chrome-gold.skia.org
+    triage_link = self.skia_gold_session.GetTriageLinks(image_name)[1]
+    if triage_link:
+      self.request.node.user_properties.append(
+        ('tag', (f'skiagold_link/{name}', triage_link)))
+
+    return status, f'{error_msg} \n{triage_link}'
 
 @pytest.fixture
 def skia_gold_util(
@@ -100,6 +104,7 @@ def skia_gold_util(
 
   try:
     util = VariationsSkiaGoldUtil(
+      request=request,
       img_dir=skia_img_dir,
       skia_gold_session=session,
       test_name=f'{test_file}:{request.node.name}',
