dc90efde603755c5055b849d3653bb49c756f5ca
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 02:17:43 +0000

    Implements the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one being deprecated,
    this CL replaces it by the first one.
    
    Bug: 1396138
    Change-Id: I2233aea8af6caacc6a805608834c3e82b1a5bc33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798601
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186232}

--- chrome/browser/drive/drive_notification_manager_factory.h
+++ chrome/browser/drive/drive_notification_manager_factory.h
@@ -40,7 +40,7 @@ class DriveNotificationManagerFactory : public ProfileKeyedServiceFactory {
   ~DriveNotificationManagerFactory() override;
 
   // BrowserContextKeyedServiceFactory implementation.
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
