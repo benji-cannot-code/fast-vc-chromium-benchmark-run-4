59c72b7426304314c0624c3d03e184d1d825f070
Wayne Jackson Jr. <wbjacksonjr@google.com>
2023-08-22 10:10:18 +0000

    [webview][multi-profile] Fix Lifetime of ServiceWorker classes + AwInit Renames
    This CL does a few things related to multi-profile.
    
    - Fixes class Lifetime for AwServiceWorkerController &
    AwServiceWorkerSettings
    - Removes @JNINamespace annotation for AwServiceWorkerSettings as it
    calls no native code
    - Renames browser context specific methods and variables in AwInit to
    reflect default profile
    
    Bug: 1446913
    Change-Id: I47a02b6c339ec1c48e118d060ee6d81aa3077fea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4783369
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Wayne Jackson Jr. <wbjacksonjr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186385}

--- android_webview/support_library/java/src/org/chromium/support_lib_glue/SupportLibWebViewChromiumFactory.java
+++ android_webview/support_library/java/src/org/chromium/support_lib_glue/SupportLibWebViewChromiumFactory.java
@@ -383,7 +383,7 @@ class SupportLibWebViewChromiumFactory implements WebViewProviderFactoryBoundary
                     mServiceWorkerController =
                             BoundaryInterfaceReflectionUtil.createInvocationHandlerFor(
                                     new SupportLibServiceWorkerControllerAdapter(
-                                            mAwInit.getServiceWorkerController()));
+                                            mAwInit.getDefaultServiceWorkerController()));
                 }
             }
             return mServiceWorkerController;
