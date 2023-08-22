a4212fca9448056f37e2f55519c8294713e4d793
Joemer Ramos <joemerramos@chromium.org>
2023-08-22 12:11:59 +0000

    [iOS] Fix tab + sign being misplaced after multiwindow exit
    There is logic that changes the + sign frame when we use multiwindow. To
    account for the repositioning, we need to force an update for the
    configuration attached to the UIButton when entering and exiting
    multiwindow mode.
    
    Fixed: 1473739
    Change-Id: I376d1d8247e9cc8a5f2a3fbeeec7ed89e733d033
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4799575
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1186426}

--- ios/chrome/browser/ui/tabs/tab_strip_controller.mm
+++ ios/chrome/browser/ui/tabs/tab_strip_controller.mm
@@ -1353,6 +1353,7 @@ const CGFloat kSymbolSize = 18;
 - (void)updateContentSizeAndRepositionViews {
   // TODO(rohitrao): The following lines are duplicated in
   // layoutTabStripSubviews.  Find a way to consolidate this logic.
+  [_buttonNewTab setNeedsUpdateConfiguration];
   const NSUInteger tabCount = [_tabArray count] - [_closingTabs count];
   if (!tabCount)
     return;
