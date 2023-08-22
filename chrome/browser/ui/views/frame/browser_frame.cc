e82f73d40670909dcd127717cd0fa9363e2f62b0
Yuzhu Shen <yzshen@chromium.org>
2023-08-22 06:57:54 +0000

    BrowserFrame: remove duplicate ThemeServiceFactory::GetForProfile() calls.
    Evaluations show that there are excessive calls to this
    function. For a test run of
    ui.LoginPerf.lacros_chrome_root_fs_only tast test on atlas, there
    are ~12k calls of this function from
    BrowserFrame::GetColorProviderKey(). (They contributes to
    ~40% calls into KeyedServiceFactory::GetServiceForContext().)
    Merging the 3 calls into 1 could cut roughly 65ms thread duration.
    
    Bug: b/295939581
    Change-Id: I66c256734cbd3f6f26907be343b563803f63495f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4794983
    Commit-Queue: Yuzhu Shen <yzshen@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186315}

--- chrome/browser/ui/views/frame/browser_frame.cc
+++ chrome/browser/ui/views/frame/browser_frame.cc
@@ -438,16 +438,17 @@ ui::ColorProviderKey BrowserFrame::GetColorProviderKey() const {
   }
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
+  const auto* theme_service =
+      ThemeServiceFactory::GetForProfile(browser_view_->browser()->profile());
+
   // color_mode.
-  [this, &key]() {
+  [this, &key, theme_service]() {
     // Currently the incognito browser is implemented as unthemed dark mode.
     if (IsIncognitoBrowser()) {
       key.color_mode = ui::ColorProviderKey::ColorMode::kDark;
       return;
     }
 
-    const auto* theme_service =
-        ThemeServiceFactory::GetForProfile(browser_view_->browser()->profile());
     const auto browser_color_scheme = theme_service->GetBrowserColorScheme();
 
     if (browser_color_scheme != ThemeService::BrowserColorScheme::kSystem) {
@@ -458,9 +459,6 @@ ui::ColorProviderKey BrowserFrame::GetColorProviderKey() const {
     }
   }();
 
-  const auto* theme_service =
-      ThemeServiceFactory::GetForProfile(browser_view_->browser()->profile());
-
   // is_grayscale.
   // Incognito mode browser should be forced to grayscale.
   key.is_grayscale = IsIncognitoBrowser() ||
@@ -468,7 +466,7 @@ ui::ColorProviderKey BrowserFrame::GetColorProviderKey() const {
                       theme_service->GetIsGrayscale());
 
   // user_color.
-  [this, &key]() {
+  [&key, theme_service]() {
     // The grayscale theme also assumes that the baseline palette is used.
     if (key.is_grayscale) {
       // Baseline palette is used when `ColorProviderKey::user_color` is empty.
@@ -476,8 +474,6 @@ ui::ColorProviderKey BrowserFrame::GetColorProviderKey() const {
       return;
     }
 
-    const auto* theme_service =
-        ThemeServiceFactory::GetForProfile(browser_view_->browser()->profile());
     if (!theme_service) {
       return;
     }
