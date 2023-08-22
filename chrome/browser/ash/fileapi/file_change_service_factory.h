9ff9de4a4361dd2e8b920273cf8f121e6b37b2b0
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 00:11:44 +0000

    Implements the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one being deprecated,
    this CL replaces it by the first one.
    
    This also ensures that, instead of applying `WrapUnique` to a `new` raw
    ptr, it directly uses `make_unique`.
    
    Change-Id: I3e038fc08ac51bb51ef68e4435eeb717511d81cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4797783
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Reviewed-by: Austin Tankiang <austinct@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186176}

--- chrome/browser/ash/fileapi/file_change_service_factory.h
+++ chrome/browser/ash/fileapi/file_change_service_factory.h
@@ -38,7 +38,7 @@ class FileChangeServiceFactory : public ProfileKeyedServiceFactory {
   ~FileChangeServiceFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
