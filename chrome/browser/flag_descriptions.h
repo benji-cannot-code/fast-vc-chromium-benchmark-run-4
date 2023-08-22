bb4bdf10605b03daf987650890afa6e4be84ed7f
mark a. foltz <mfoltz@chromium.org>
2023-08-22 00:24:56 +0000

    [Capture] Remove features::kLacrosAuraCapture
    This feature shipped by default in M109.
    
    Bug: 1273189
    Change-Id: I3aff59f0cd8eb98b14ea87aff33e39ecaa8c8d9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798861
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186186}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4043,9 +4043,6 @@ extern const char kExperimentalWebAppProfileIsolationDescription[];
 extern const char kExperimentalWebAppStoragePartitionIsolationName[];
 extern const char kExperimentalWebAppStoragePartitionIsolationDescription[];
 
-extern const char kLacrosAuraCaptureName[];
-extern const char kLacrosAuraCaptureDescription[];
-
 extern const char kLacrosMergeIcuDataFileName[];
 extern const char kLacrosMergeIcuDataFileDescription[];
 #endif  // #if BUILDFLAG(IS_CHROMEOS_LACROS)
