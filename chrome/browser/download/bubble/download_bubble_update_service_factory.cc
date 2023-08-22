2b728e4b9e7cc30321d8d876ff5dd5dc82288a9e
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 14:30:12 +0000

    Implements the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one being deprecated,
    this CL replaces it by the first one.
    
    Bug: 1396138
    Change-Id: I61283e067e8dfef5849f2b0f3110f3f909d6a6f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798704
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186510}

--- chrome/browser/download/bubble/download_bubble_update_service_factory.cc
+++ chrome/browser/download/bubble/download_bubble_update_service_factory.cc
@@ -42,7 +42,9 @@ DownloadBubbleUpdateServiceFactory::DownloadBubbleUpdateServiceFactory()
 DownloadBubbleUpdateServiceFactory::~DownloadBubbleUpdateServiceFactory() =
     default;
 
-KeyedService* DownloadBubbleUpdateServiceFactory::BuildServiceInstanceFor(
+std::unique_ptr<KeyedService>
+DownloadBubbleUpdateServiceFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
-  return new DownloadBubbleUpdateService(Profile::FromBrowserContext(context));
+  return std::make_unique<DownloadBubbleUpdateService>(
+      Profile::FromBrowserContext(context));
 }
--- chrome/browser/download/bubble/download_bubble_update_service_factory.h
+++ chrome/browser/download/bubble/download_bubble_update_service_factory.h
@@ -32,7 +32,7 @@ class DownloadBubbleUpdateServiceFactory : public ProfileKeyedServiceFactory {
   ~DownloadBubbleUpdateServiceFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
