b444c0124e37ec02679c6585226c2221cfb9b036
Ewann Pelle <ewannpv@chromium.org>
2023-08-22 12:57:56 +0000

    [iOS] Update IOSMagicStackModuleType enum
    Change-Id: I180df6b1a17b3a071372627e9eace09ee8ac64dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4801337
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186454}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_constants.h
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_constants.h
@@ -10,8 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ntp/set_up_list_item_type.h"
 
-// Enum specifying the type of Content Suggestions a module is showing. Entries
-// should not be renumbered and numeric values should never be reused.
+// Enum specifying the type of Content Suggestions a module is showing.
+// Entries should always keep synced with the IOSMagicStackModuleType histogram
+// enum. Entries should not be renumbered and numeric values should never be
+// reused.
 enum class ContentSuggestionsModuleType {
   kMostVisited = 0,
   kShortcuts = 1,
