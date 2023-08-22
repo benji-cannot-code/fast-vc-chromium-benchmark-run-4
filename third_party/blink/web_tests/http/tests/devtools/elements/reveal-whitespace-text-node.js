b3ba56175178968c6412602a65f922fdeee4e42d
Simon Zünd <szuend@chromium.org>
2023-08-22 08:17:12 +0000

    [DevTools] Replace `Common.Revealer.reveal` global with import
    Note that the global usage is equal to the import usage, so the CL
    only adds the necessary import statement.
    
    R=mathias@chromium.org
    
    Bug: chromium:1442410
    Change-Id: Ib863d2c5756ac7e998a62a87c17148464cbfe702
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798688
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186340}

--- third_party/blink/web_tests/http/tests/devtools/elements/reveal-whitespace-text-node.js
+++ third_party/blink/web_tests/http/tests/devtools/elements/reveal-whitespace-text-node.js
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {TestRunner} from 'test_runner';
 import {ElementsTestRunner} from 'elements_test_runner';
 
+import * as Common from 'devtools/core/common/common.js';
+
 (async function() {
   TestRunner.addResult(
       `This test verifies that revealing a whitespace text node RemoteObject reveals its parentElement DIV.\n`);
--- third_party/blink/web_tests/http/tests/devtools/elements/styles-3/styles-add-new-rule.js
+++ third_party/blink/web_tests/http/tests/devtools/elements/styles-3/styles-add-new-rule.js
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {TestRunner} from 'test_runner';
 import {ElementsTestRunner} from 'elements_test_runner';
 
+import * as Common from 'devtools/core/common/common.js';
+
 (async function() {
   TestRunner.addResult(`Tests that adding a new rule works after switching nodes.\n`);
   await TestRunner.loadLegacyModule('elements');
