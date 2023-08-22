b81ea2781f5fedb358278c2d35c78ceec5e17419
Emily Shack <emshack@chromium.org>
2023-08-22 21:20:31 +0000

    Revert "Make the Reading mode side panel a local side panel instead of a global"
    This reverts commit d5dacbd5bc28d84bbe948719c9f47e91465502ac.
    
    Reason for revert: Likely culprit for builder failures https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/35308/overview
    
    Original change's description:
    > Make the Reading mode side panel a local side panel instead of a global
    > one.
    >
    > Bug: 1439905
    > Change-Id: Icdcabd2ddade61c4a32805c9a41668d621993aef
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4756472
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Jocelyn Tran <jocelyntran@google.com>
    > Commit-Queue: Xiang Xiao <xiangxiao@google.com>
    > Reviewed-by: David Tseng <dtseng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1186669}
    
    Bug: 1439905
    Change-Id: I1fd68271e0fe69901e4834c12df84acf104cefe8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4804108
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Owners-Override: Emily Shack <emshack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1186828}

--- chrome/browser/ui/side_panel/read_anything/read_anything_side_panel_controller_utils.h
+++ chrome/browser/ui/side_panel/read_anything/read_anything_side_panel_controller_utils.h
@@ -8,13 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class Browser;
 
-namespace content {
-class WebContents;
-}  // namespace content
-
 // Used for reading mode option in context menu.
 void ShowReadAnythingSidePanel(Browser* browser);
 bool IsReadAnythingEntryShowing(Browser* browser);
-void CreateAndRegisterEntry(content::WebContents* web_contents);
 
 #endif  // CHROME_BROWSER_UI_SIDE_PANEL_READ_ANYTHING_READ_ANYTHING_SIDE_PANEL_CONTROLLER_UTILS_H_
--- chrome/browser/ui/views/side_panel/read_anything/read_anything_coordinator.h
+++ chrome/browser/ui/views/side_panel/read_anything/read_anything_coordinator.h
@@ -52,7 +52,7 @@ class ReadAnythingCoordinator : public BrowserUserData<ReadAnythingCoordinator>,
   ReadAnythingCoordinator& operator=(const ReadAnythingCoordinator&) = delete;
   ~ReadAnythingCoordinator() override;
 
-  void CreateAndRegisterSidePanelEntry(SidePanelRegistry* registry);
+  void CreateAndRegisterEntry(SidePanelRegistry* global_registry);
   ReadAnythingController* GetController();
   ReadAnythingModel* GetModel();
 
@@ -64,10 +64,6 @@ class ReadAnythingCoordinator : public BrowserUserData<ReadAnythingCoordinator>,
  private:
   friend class BrowserUserData<ReadAnythingCoordinator>;
   friend class ReadAnythingCoordinatorTest;
-
-  void CreateAndRegisterEntriesForExistingWebContents(
-      TabStripModel* tab_strip_model);
-  void DeregisterEntriesForExistingWebContents(TabStripModel* tab_strip_model);
 
   // Used during construction to initialize the model with saved user prefs.
   void InitModelWithUserPrefs();
