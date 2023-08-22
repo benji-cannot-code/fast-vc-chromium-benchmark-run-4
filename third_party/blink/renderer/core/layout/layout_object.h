d5bd08731c000beffe080b1881eef627c110ce86
Ian Kilpatrick <ikilpatrick@chromium.org>
2023-08-22 14:29:02 +0000

    [flex] Remove intrinsic sizing algorithms that aren't web compatible.
    These algorithms caused too many regressions - remove :(
    
    See:
    https://github.com/w3c/csswg-drafts/issues/8884#issuecomment-1641069059
    
    Bug: 240765
    Change-Id: Id5c5d0817d778ff8094e606d5d111c808c6c6c5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4799320
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186508}

--- third_party/blink/renderer/core/layout/layout_object.h
+++ third_party/blink/renderer/core/layout/layout_object.h
@@ -1591,15 +1591,6 @@ class CORE_EXPORT LayoutObject : public GarbageCollected<LayoutObject>,
     bitfields_.SetIntrinsicLogicalWidthsChildDependsOnBlockConstraints(b);
   }
 
-  void SetIntrinsicLogicalWidthsInFlexIntrinsicSizing(bool b) {
-    NOT_DESTROYED();
-    bitfields_.SetIntrinsicLogicalWidthsInFlexIntrinsicSizing(b);
-  }
-  bool IntrinsicLogicalWidthsInFlexIntrinsicSizing() const {
-    NOT_DESTROYED();
-    return bitfields_.IntrinsicLogicalWidthsInFlexIntrinsicSizing();
-  }
-
   bool NeedsLayoutOverflowRecalc() const {
     NOT_DESTROYED();
     return bitfields_.SelfNeedsLayoutOverflowRecalc() ||
@@ -3997,9 +3988,6 @@ class CORE_EXPORT LayoutObject : public GarbageCollected<LayoutObject>,
     ADD_BOOLEAN_BITFIELD(
         intrinsic_logical_widths_child_depends_on_block_constraints_,
         IntrinsicLogicalWidthsChildDependsOnBlockConstraints);
-
-    ADD_BOOLEAN_BITFIELD(intrinsic_logical_widths_in_flex_intrinsic_sizing_,
-                         IntrinsicLogicalWidthsInFlexIntrinsicSizing);
 
     // This flag is set on inline container boxes that need to run the
     // Pre-layout phase in LayoutNG. See NGInlineNode::CollectInlines().
