2e305cc8e6321b4ffcdf16e5f82224ddaa766549
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 12:46:39 +0000

    DiceWebSigninInterceptorFactory uses the new builder
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: I14c963a9273e06aa00210eb388c9fe16572c38b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4801175
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186451}

--- chrome/browser/signin/dice_web_signin_interceptor_factory.h
+++ chrome/browser/signin/dice_web_signin_interceptor_factory.h
@@ -30,7 +30,7 @@ class DiceWebSigninInterceptorFactory : public ProfileKeyedServiceFactory {
   // BrowserContextKeyedServiceFactory:
   void RegisterProfilePrefs(
       user_prefs::PrefRegistrySyncable* registry) override;
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* profile) const override;
 };
 
