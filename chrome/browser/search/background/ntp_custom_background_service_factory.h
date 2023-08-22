fa53b689cf410ee17cddc2b0cf6a6c4cfe02e499
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 14:37:53 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: I54ec91e7af02f993c1135e110c74b7af116088f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800960
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186517}

--- chrome/browser/search/background/ntp_custom_background_service_factory.h
+++ chrome/browser/search/background/ntp_custom_background_service_factory.h
@@ -30,7 +30,7 @@ class NtpCustomBackgroundServiceFactory : public ProfileKeyedServiceFactory {
   ~NtpCustomBackgroundServiceFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* profile) const override;
 };
 
