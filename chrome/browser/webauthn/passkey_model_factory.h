1a9518428eacc19864c265f8dc391127b7e46958
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 14:40:25 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: Ifc9992a0ad290d7a191d7cf092c3931803241a74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4801359
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186519}

--- chrome/browser/webauthn/passkey_model_factory.h
+++ chrome/browser/webauthn/passkey_model_factory.h
@@ -27,7 +27,7 @@ class PasskeyModelFactory : public ProfileKeyedServiceFactory {
   ~PasskeyModelFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
