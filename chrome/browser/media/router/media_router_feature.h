279053b1ce8ee5f435ad4303a63c5ea7fe08d529
Benjamin Zielinski <bzielinski@google.com>
2023-08-22 19:19:25 +0000

    Add a feature to control cast mirroring playout delay.
    This change adds a feature kCastMirroringPlayoutDelay, with a
    corresponding param kCastMirroringPlayoutDelayMs, which controls the
    target playout delay during cast mirroring sessions, as long as the
    playout delay has not been set by a site-initiated mirroring source.
    
    We are adding this feature to run an experiment to test whether the
    current default playout delay may be lowered without harshly degrading
    the current mirroring experience.
    
    Previously, we wanted to run this experiment only on AccessCodeCast
    users, and thus previous patchsets included a new policy instead of a
    new feature.
    
    Bug: b:291616456
    Change-Id: Ic61041007719226254df678b914b14ae16ffe3d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4725242
    Commit-Queue: Benjamin Zielinski <bzielinski@google.com>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186725}

--- chrome/browser/media/router/media_router_feature.h
+++ chrome/browser/media/router/media_router_feature.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_MEDIA_ROUTER_MEDIA_ROUTER_FEATURE_H_
 
 #include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
+#include "base/time/time.h"
 #include "build/build_config.h"
 
 class PrefRegistrySimple;
@@ -70,6 +72,11 @@ BASE_DECLARE_FEATURE(kFallbackToAudioTabMirroring);
 // dialog instead of the entire sink button being a stop button.
 BASE_DECLARE_FEATURE(kCastDialogStopButton);
 
+// If enabled, mirroring sessions use the playout delay specified by
+// `kCastMirroringPlayoutDelayMs`.
+BASE_DECLARE_FEATURE(kCastMirroringPlayoutDelay);
+extern const base::FeatureParam<int> kCastMirroringPlayoutDelayMs;
+
 // Registers |kMediaRouterCastAllowAllIPs| with local state pref |registry|.
 void RegisterLocalStatePrefs(PrefRegistrySimple* registry);
 
@@ -92,6 +99,10 @@ bool DialMediaRouteProviderEnabled();
 // Returns true if global media controls are used to start and stop casting and
 // Media Router is enabled for |context|.
 bool GlobalMediaControlsCastStartStopEnabled(content::BrowserContext* context);
+
+// Returns the optional value to use for mirroring playout delay from the
+// relevant command line flag or feature, if any are set.
+absl::optional<base::TimeDelta> GetCastMirroringPlayoutDelay();
 #endif  // !BUILDFLAG(IS_ANDROID)
 
 }  // namespace media_router
