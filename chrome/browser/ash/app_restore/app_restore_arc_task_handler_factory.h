4cbfa50e604aae8159c252305d7f33772ecf7ab3
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 00:26:38 +0000

    Implements the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one being deprecated,
    this CL
    replaces it by the first one.
    
    This also ensures that, instead of applying `WrapUnique` to a `new` raw
    ptr, it directly uses `make_unique`.
    
    Change-Id: I457261d87f37a548cb7705acf19ad2be87668d2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798095
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186189}

--- chrome/browser/ash/app_restore/app_restore_arc_task_handler_factory.h
+++ chrome/browser/ash/app_restore/app_restore_arc_task_handler_factory.h
@@ -32,7 +32,7 @@ class AppRestoreArcTaskHandlerFactory : public ProfileKeyedServiceFactory {
   ~AppRestoreArcTaskHandlerFactory() override;
 
   // BrowserContextKeyedServiceFactory.
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
