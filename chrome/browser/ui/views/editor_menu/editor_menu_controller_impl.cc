17372e777306180bd5cf4e61282ba230b396684f
Michelle <michellegc@google.com>
2023-08-22 05:23:46 +0000

    Add basic editor menu promo card view.
    Add a basic EditorMenuPromoCardView which just consists of a promo card
    title and uses the same bounds as EditorMenuView for now. Will work on
    buttons, text, icon, styling etc. in followups.
    
    Bug: b:295061567
    Change-Id: I924426e5c1609acf9773eb98f34d9afce237f872
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4796695
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186285}

--- chrome/browser/ui/views/editor_menu/editor_menu_controller_impl.cc
+++ chrome/browser/ui/views/editor_menu/editor_menu_controller_impl.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/editor_menu/editor_menu_controller_impl.h"
 
+#include "chrome/browser/ui/views/editor_menu/editor_menu_promo_card_view.h"
 #include "chrome/browser/ui/views/editor_menu/editor_menu_view.h"
 #include "ui/gfx/geometry/rect.h"
 
@@ -33,7 +34,8 @@ void EditorMenuControllerImpl::MaybeShowEditorMenu(
   }
 
   if (status == ConsentStatus::kPending) {
-    // TODO(b/295061567): Implement the consent view.
+    editor_menu_widget_ = EditorMenuPromoCardView::CreateWidget(anchor_bounds);
+    editor_menu_widget_->ShowInactive();
     return;
   }
 
