6951c11cb72337c003b95ac5ce7c6793fcb3b505
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 10:33:44 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: If89a15c3f622c921fb49374d6d2b57bd95bbcb56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798223
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186393}

--- chrome/browser/extensions/menu_manager_factory.h
+++ chrome/browser/extensions/menu_manager_factory.h
@@ -33,7 +33,7 @@ class MenuManagerFactory : public ProfileKeyedServiceFactory {
   MenuManagerFactory();
   ~MenuManagerFactory() override;
 
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
   bool ServiceIsCreatedWithBrowserContext() const override;
   bool ServiceIsNULLWhileTesting() const override;
