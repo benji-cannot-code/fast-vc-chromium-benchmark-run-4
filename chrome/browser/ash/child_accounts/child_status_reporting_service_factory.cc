694effe7907e2c891bf60840583bc59fa83b86b5
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 14:16:36 +0000

    Implement the non-deprecated builder
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: I2c03597d1ea2d06dcc67118492c6fee1495c856b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4797950
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186501}

--- chrome/browser/ash/child_accounts/child_status_reporting_service_factory.cc
+++ chrome/browser/ash/child_accounts/child_status_reporting_service_factory.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/child_accounts/child_status_reporting_service_factory.h"
 
+#include <memory>
+
 #include "base/no_destructor.h"
 #include "chrome/browser/ash/child_accounts/child_status_reporting_service.h"
 
@@ -38,9 +40,10 @@ ChildStatusReportingServiceFactory::ChildStatusReportingServiceFactory()
 ChildStatusReportingServiceFactory::~ChildStatusReportingServiceFactory() =
     default;
 
-KeyedService* ChildStatusReportingServiceFactory::BuildServiceInstanceFor(
+std::unique_ptr<KeyedService>
+ChildStatusReportingServiceFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
-  return new ChildStatusReportingService(context);
+  return std::make_unique<ChildStatusReportingService>(context);
 }
 
 }  // namespace ash
