9f22c8b7ae3c76f71aa2e1fba064c99cbd09fd9d
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 22:19:12 +0000

    Implements the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one being deprecated,
    this CL replaces it by the first one.
    
    This also ensures that, instead of applying `WrapUnique` to a `new` raw
    ptr, it directly uses `make_unique`.
    
    Change-Id: I091f6232c9bddc951abcd29dcd5a9bf1645af58f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4799873
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186863}

--- chrome/browser/ash/device_sync/device_sync_client_factory.h
+++ chrome/browser/ash/device_sync/device_sync_client_factory.h
@@ -34,7 +34,7 @@ class DeviceSyncClientFactory : public ProfileKeyedServiceFactory {
   ~DeviceSyncClientFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
   bool ServiceIsNULLWhileTesting() const override;
 };
