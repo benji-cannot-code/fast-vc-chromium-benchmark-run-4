325e41716578cbf950a2812fb7d5e9ef9fc6debf
Emilia Paz <emiliapaz@chromium.org>
2023-08-22 21:11:43 +0000

    [Extensions UserScripts] Add userScripts.getScripts()
    Add userScripts.getScripts() API function that returns dynamically
    registered user scripts from an extension.
    
    Bug: 1385165
    Change-Id: I6de8438daa46e76ec583fdafc97dada98a7c755c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4781218
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186818}

--- extensions/browser/extension_function_histogram_value.h
+++ extensions/browser/extension_function_histogram_value.h
@@ -1882,6 +1882,7 @@ enum HistogramValue {
   OS_DIAGNOSTICS_RUNBLUETOOTHPAIRINGROUTINE = 1819,
   FILEMANAGERPRIVATE_DISMISSIOTASK = 1820,
   ACCESSIBILITY_PRIVATE_SHOWTOAST = 1821,
+  USERSCRIPTS_GETSCRIPTS = 1822,
   // Last entry: Add new entries above, then run:
   // tools/metrics/histograms/update_extension_histograms.py
   ENUM_BOUNDARY
