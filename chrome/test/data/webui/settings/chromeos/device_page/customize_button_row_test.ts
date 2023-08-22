1d7861b785eb8e3739272f2209a914a798015edf
YuhanYang <yyhyyh@google.com>
2023-08-22 20:37:06 +0000

    customization: Display key combination in dropdown
    When a button is remapped with a keyEvent, calculate the key combination
    from the keyEvent and display it in the dropdown menu.
    
    Screenshot: https://screenshot.googleplex.com/4X4EFhneibaZCMn
    
    Bug: b/241965717
    Test: Verified on DUT and browser_tests.
    Change-Id: I7c5aa377d8b1a52b1d6d4c3b897e5e45bb0a06df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4791313
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1186789}

--- chrome/test/data/webui/settings/chromeos/device_page/customize_button_row_test.ts
+++ chrome/test/data/webui/settings/chromeos/device_page/customize_button_row_test.ts
@@ -63,10 +63,10 @@ suite('<customize-button-row>', () => {
         expectedRemapping!.remappingAction?.action!.toString());
 
     // Change buttonRemapping data to display.
+    customizeButtonRow.set('remappingIndex', 1);
     customizeButtonRow.set(
         'buttonRemappingList',
         fakeGraphicsTablets[1]!.settings.tabletButtonRemappings);
-    customizeButtonRow.set('remappingIndex', 1);
     await flushTasks();
     expectedRemapping =
         fakeGraphicsTablets[1]!.settings.tabletButtonRemappings[1];
@@ -77,5 +77,27 @@ suite('<customize-button-row>', () => {
     assertEquals(
         getSelectedValue(),
         expectedRemapping!.remappingAction?.action!.toString());
+  });
+
+  test('Initialize key combination string', async () => {
+    await initializeCustomizeButtonRow();
+    customizeButtonRow.set(
+        'buttonRemappingList',
+        fakeGraphicsTablets[0]!.settings.penButtonRemappings);
+    customizeButtonRow.set('remappingIndex', 0);
+    await flushTasks();
+
+    assertEquals(getSelectedValue(), 'key combination');
+    assertEquals(customizeButtonRow.get('keyCombinationLabel_'), 'ctrl + z');
+
+    // Switch to another button remapping.
+    customizeButtonRow.set(
+        'buttonRemappingList',
+        fakeGraphicsTablets[1]!.settings.penButtonRemappings);
+    customizeButtonRow.set('remappingIndex', 1);
+    await flushTasks();
+
+    assertEquals(getSelectedValue(), 'key combination');
+    assertEquals(customizeButtonRow.get('keyCombinationLabel_'), 'ctrl + v');
   });
 });
