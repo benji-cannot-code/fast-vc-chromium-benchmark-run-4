937b4ede4f2e9b779d9990cfce7e5bda55729411
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 12:32:18 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: I3eb2f155ee2b8593d5539ac0667614e0d396ef67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800456
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186436}

--- components/password_manager/content/browser/password_change_success_tracker_factory.h
+++ components/password_manager/content/browser/password_change_success_tracker_factory.h
@@ -27,7 +27,7 @@ class PasswordChangeSuccessTrackerFactory
       content::BrowserContext* browser_context);
 
  private:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* browser_context) const override;
   content::BrowserContext* GetBrowserContextToUse(
       content::BrowserContext* context) const override;
