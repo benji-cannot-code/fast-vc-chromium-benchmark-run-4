fda52d3f67aa274374ee757a00176fb284321908
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-23 02:45:53 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: I246c80225f5f8f20deab9a96cca0ab44e65507a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798524
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186996}

--- chrome/browser/extensions/forced_extensions/install_stage_tracker_factory.cc
+++ chrome/browser/extensions/forced_extensions/install_stage_tracker_factory.cc
@@ -35,9 +35,10 @@ InstallStageTrackerFactory::InstallStageTrackerFactory()
 
 InstallStageTrackerFactory::~InstallStageTrackerFactory() = default;
 
-KeyedService* InstallStageTrackerFactory::BuildServiceInstanceFor(
+std::unique_ptr<KeyedService>
+InstallStageTrackerFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
-  return new InstallStageTracker(context);
+  return std::make_unique<InstallStageTracker>(context);
 }
 
 }  // namespace extensions
--- chrome/browser/extensions/forced_extensions/install_stage_tracker_factory.h
+++ chrome/browser/extensions/forced_extensions/install_stage_tracker_factory.h
@@ -30,7 +30,7 @@ class InstallStageTrackerFactory : public ProfileKeyedServiceFactory {
   InstallStageTrackerFactory();
   ~InstallStageTrackerFactory() override;
 
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
