3df44ec1e568669c2dc920ae6cae48162db6a4ad
Noureldien Refae <noureldien@google.com>
2023-08-23 17:15:19 +0000

    Privacy Guide: Add metrics to the privacy guide 3 flow
    Testing and adding metrics to the privacy guide 3 flow.
    
    Bug: 1215630
    Change-Id: Ic41e660f0f058f2c71b7bd700dbeeefe93c7851a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4802111
    Commit-Queue: Nour Eldien Ayman <noureldien@google.com>
    Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1187339}

--- chrome/browser/privacy_guide/android/java/src/org/chromium/chrome/browser/privacy_guide/SearchSuggestionsFragment.java
+++ chrome/browser/privacy_guide/android/java/src/org/chromium/chrome/browser/privacy_guide/SearchSuggestionsFragment.java
@@ -33,6 +33,7 @@ public class SearchSuggestionsFragment extends Fragment {
         searchSuggestionsSwitch.setChecked(PrivacyGuideUtils.isSearchSuggestionsEnabled());
 
         searchSuggestionsSwitch.setOnCheckedChangeListener((button, isChecked) -> {
+            PrivacyGuideMetricsDelegate.recordMetricsOnSearchSuggestionsChange(isChecked);
             UserPrefs.get(Profile.getLastUsedRegularProfile())
                     .setBoolean(Pref.SEARCH_SUGGEST_ENABLED, isChecked);
         });
