57eb2c2c29bd3d4bb3a66962d51ec383d7b69fea
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-23 00:03:35 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: I81c59a436d1bbfc5ddad7c853d97bde9c6e40d8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4801177
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186937}

--- chrome/browser/ui/cocoa/screentime/history_bridge_factory.mm
+++ chrome/browser/ui/cocoa/screentime/history_bridge_factory.mm
@@ -29,7 +29,8 @@ bool HistoryBridgeFactory::IsEnabled() {
   return base::FeatureList::IsEnabled(kScreenTime);
 }
 
-KeyedService* HistoryBridgeFactory::BuildServiceInstanceFor(
+std::unique_ptr<KeyedService>
+HistoryBridgeFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
   auto* profile = Profile::FromBrowserContext(context);
   auto* service = HistoryServiceFactory::GetForProfile(
@@ -37,7 +38,7 @@ KeyedService* HistoryBridgeFactory::BuildServiceInstanceFor(
 
   auto deleter = HistoryDeleterImpl::Create();
 
-  return new HistoryBridge(service, std::move(deleter));
+  return std::make_unique<HistoryBridge>(service, std::move(deleter));
 }
 
 bool HistoryBridgeFactory::ServiceIsCreatedWithBrowserContext() const {
