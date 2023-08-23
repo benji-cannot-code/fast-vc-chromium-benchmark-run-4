62784da419e3c5d2008dc04e913cc87dff83b7c4
Dustin J. Mitchell <djmitche@chromium.org>
2023-08-23 18:14:28 +0000

    Use os.path.sep in GetThirdPartyDepsFromGNDepsOutput
    This should be more reliable than matching `[/\\]`.
    
    Change-Id: Ib07f36b140bd790cec10315b59072b8a4e6c7ac3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4790801
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187384}

--- tools/licenses/licenses.py
+++ tools/licenses/licenses.py
@@ -783,6 +783,7 @@ def GetThirdPartyDepsFromGNDepsOutput(
 
   # Use non-capturing group with or's for all possible options.
   allowed_paths = '|'.join([re.escape(x) for x in allowed_paths_list])
+  sep = re.escape(os.path.sep)
   path_regex = re.compile(
       r'''^
             (                                     # capture
@@ -795,7 +796,7 @@ def GetThirdPartyDepsFromGNDepsOutput(
               {sep}
             )
             (.+{sep})?BUILD\.gn$                  # with filename BUILD.gn
-  '''.format(allowed_paths=allowed_paths, sep=r'[/\\]', nonsep=r'[^/\\]'),
+  '''.format(allowed_paths=allowed_paths, sep=sep, nonsep=f'[^{sep}]'),
       re.VERBOSE)
 
   third_party_deps = set()
