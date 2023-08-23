def7298a6c0a6f8862acf8a48a32f2b2c66a56e0
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-23 19:53:53 +0000

    Implements the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one being deprecated,
    this CL replaces it by the first one.
    
    This also ensures that, instead of applying `WrapUnique` to a `new` raw
    ptr, it directly uses `make_unique`.
    
    Change-Id: I9dcea11f654782785487527ed616abfba853954f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4797785
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187444}

--- chrome/browser/ash/nearby/quick_start_connectivity_service_factory.h
+++ chrome/browser/ash/nearby/quick_start_connectivity_service_factory.h
@@ -30,7 +30,7 @@ class QuickStartConnectivityServiceFactory : public ProfileKeyedServiceFactory {
   QuickStartConnectivityServiceFactory();
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
   bool ServiceIsCreatedWithBrowserContext() const override;
 };
