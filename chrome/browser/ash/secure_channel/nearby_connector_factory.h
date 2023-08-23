1e2642de9f31d628000dcdc9482d2f16213c3856
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-23 20:20:12 +0000

    NearbyConnectorFactory implements the non-deprecated builder
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one being deprecated, this CL
    replaces it by the first one.
    
    Bug: 1396138
    Change-Id: I411c9ff4f15e2f3b34f7602707f2c6351e9b1af6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798600
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187457}

--- chrome/browser/ash/secure_channel/nearby_connector_factory.h
+++ chrome/browser/ash/secure_channel/nearby_connector_factory.h
@@ -31,7 +31,7 @@ class NearbyConnectorFactory : public ProfileKeyedServiceFactory {
   ~NearbyConnectorFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
   bool ServiceIsCreatedWithBrowserContext() const override;
 };
