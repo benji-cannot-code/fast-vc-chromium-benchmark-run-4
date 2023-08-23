314170e869acb5c3b7fa9204f7ea92dde8a5bc09
dljames <dljames@google.com>
2023-08-23 18:44:13 +0000

    [ SavedTabGroups ] Add feature to chrome labs
    Change-Id: I6d297c40cb3fa9383dbef905831e0d6705157cda
    Bug: 1474716
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4802520
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187406}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -3259,6 +3259,7 @@ const char kTabGroupsContinuationAndroidName[] = "Tab Groups Continuation";
 const char kTabGroupsContinuationAndroidDescription[] =
     "Allows users to access continuation features in Tab Group on phones.";
 
+const char kTabGroupsSaveId[] = "tab-groups-save";
 const char kTabGroupsSaveName[] = "Tab Groups Save and Sync";
 const char kTabGroupsSaveDescription[] =
     "Enable saving and recalling of tab groups. Right click a tab group to "
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1872,6 +1872,7 @@ extern const char kCommerceMerchantViewerAndroidDescription[];
 extern const char kTabGroupsContinuationAndroidName[];
 extern const char kTabGroupsContinuationAndroidDescription[];
 
+extern const char kTabGroupsSaveId[];
 extern const char kTabGroupsSaveName[];
 extern const char kTabGroupsSaveDescription[];
 
