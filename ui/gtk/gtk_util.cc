5ff59ecd22320ac6ba79276cb4a834df801c2178
Tom Anderson <thomasanderson@chromium.org>
2023-08-23 00:10:09 +0000

    [GTK] Disable parent window input handling while print dialog is active
    R=thestig
    
    Change-Id: Ieb70998a9634d813989555065a4b372a732dc27e
    Bug: 1473839
    Low-Coverage-Reason: There are no GTK tests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798383
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186940}

--- ui/gtk/gtk_util.cc
+++ ui/gtk/gtk_util.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/linux/linux_ui.h"
 #include "ui/native_theme/common_theme.h"
 #include "ui/ozone/public/ozone_platform.h"
+#include "ui/views/widget/desktop_aura/desktop_window_tree_host_linux.h"
 
 namespace gtk {
 
@@ -175,6 +176,13 @@ double GetOpacityFromRenderNode(GskRenderNode* node) {
   return GetOpacityFromRenderNode(GetRenderNodeChild(node));
 }
 
+// Runs DesktopWindowTreeHostLinux::EnableEventListening() when the dialog is
+// closed.
+void OnDialogDestroy(base::OnceClosure* callback_raw) {
+  std::unique_ptr<base::OnceClosure> callback = base::WrapUnique(callback_raw);
+  std::move(*callback).Run();
+}
+
 }  // namespace
 
 const char* GtkCssMenu() {
@@ -223,6 +231,30 @@ void ClearAuraTransientParent(GtkWidget* dialog, aura::Window* parent) {
   g_object_set_data(G_OBJECT(dialog), kAuraTransientParent, nullptr);
   GtkUi::GetPlatform()->ClearTransientFor(
       parent->GetHost()->GetAcceleratedWidget());
+}
+
+void DisableHostInputHandling(GtkWidget* dialog, aura::Window* parent) {
+  if (!parent) {
+    return;
+  }
+  auto* host =
+      static_cast<views::DesktopWindowTreeHostLinux*>(parent->GetHost());
+  if (!host) {
+    return;
+  }
+
+  // In some circumstances the mouse has been captured and by turning off event
+  // listening, it is never released. So we manually ensure there is no current
+  // capture.
+  host->ReleaseCapture();
+  auto callback =
+      std::make_unique<base::OnceClosure>(host->DisableEventListening());
+  // OnDialogDestroy() is called when |dialog| destroyed, which allows
+  // to invoke the callback function to re-enable event handling on the
+  // owning window.
+  g_object_set_data_full(G_OBJECT(dialog), "callback", callback.release(),
+                         reinterpret_cast<GDestroyNotify>(OnDialogDestroy));
+  gtk_window_set_modal(GTK_WINDOW(dialog), TRUE);
 }
 
 void ParseButtonLayout(const std::string& button_string,
