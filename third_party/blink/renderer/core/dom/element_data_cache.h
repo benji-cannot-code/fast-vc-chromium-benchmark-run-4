afb89468831563f260152b0c0155a48844f5b379
Scott Violet <sky@chromium.org>
2023-08-23 05:03:05 +0000

    Revert "blink: avoid unnecessary processing when class attribute changes"
    This reverts commit c6284999328750376d9925332fbcb0dd7c0f6e8a.
    
    Reason for revert: See bug 1474382 for regression.
    
    Original change's description:
    > blink: avoid unnecessary processing when class attribute changes
    >
    > When the class attribute changes Element does the following:
    >
    > 1. Updates `class` on the ElementData.
    > 2. Calls to the StyleEngine.
    >
    > If the ElementData is shared, and the class data is already up
    > to date date, then 1 is unnecessary.
    >
    > Additionally, if in the inactive document, then 2 is unnecessary.
    > 2 is unnecessary in this case because of an early out in the
    > StyleEngine:
    > https://source.chromium.org/chromium/chromium/src/+/refs/heads/main:third_party/blink/renderer/core/css/style_engine.cc;l=1155;drc=90b428dcab63b652cc91107b81d2758270e92ac0
    >
    > This takes one of the bits in ElementData to track whether the
    > class is dirty. As the bitfield is 32 bits, I'm reducing the
    > number of attributes by half (2^28th to 2^27th). This also ensures
    > we only create ShareableElementData if the number of attributes
    > fits in the bitmask.
    >
    > Bug: 1449329
    > Change-Id: Ia661bff5b1a7399be86c6fe0df1eb14fed63982b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4559646
    > Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    > Commit-Queue: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1149848}
    
    Bug: 1449329
    Change-Id: I88187e5972fb747f041ec253f66250065b4a5426
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4803704
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187041}

--- third_party/blink/renderer/core/dom/element_data_cache.h
+++ third_party/blink/renderer/core/dom/element_data_cache.h
@@ -35,17 +35,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class ElementData;
 class ShareableElementData;
 
 class ElementDataCache final : public GarbageCollected<ElementDataCache> {
  public:
   ElementDataCache();
 
-  // Returns an ElementData representing the specified attributes. This prefers
-  // returning a ShareableElementData, but in some cases returns an
-  // ElementData.
-  ElementData* ElementDataWithAttributes(
+  ShareableElementData* CachedShareableElementDataWithAttributes(
       const Vector<Attribute, kAttributePrealloc>&);
 
   void Trace(Visitor*) const;
