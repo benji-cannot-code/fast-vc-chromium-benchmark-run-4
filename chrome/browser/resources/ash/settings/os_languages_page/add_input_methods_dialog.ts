09a400014ffaf8eb74fd58684412885e61a9e9f1
mlcui <mlcui@google.com>
2023-08-23 05:02:54 +0000

    add_input_methods_dialog: Prioritise 3P Vietnamese IMEs based on 1P flag
    3P Vietnamese IMEs were prioritised as 1P Vietnamese IMEs were not
    suitable for widespread use. As 1P Vietnamese IMEs now have a feature
    flag, conditionally prioritise 3P Vietnamese IMEs depending on it.
    
    Bug: b:237492047
    Change-Id: I1355d2453a1e43d604f09d23629f18de7275d576
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4805383
    Reviewed-by: Bao-Duy Tran <tranbaoduy@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1187040}

--- chrome/browser/resources/ash/settings/os_languages_page/add_input_methods_dialog.ts
+++ chrome/browser/resources/ash/settings/os_languages_page/add_input_methods_dialog.ts
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import './add_items_dialog.js';
 
+import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {recordSettingChange} from '../metrics_recorder.js';
@@ -42,6 +43,10 @@ class OsSettingsAddInputMethodsDialogElement extends PolymerElement {
   languages: LanguagesModel|undefined;
   languageHelper: LanguageHelper;
 
+  // Internal state.
+  private readonly shouldPrioritiseVietnameseExtensions_ =
+      !loadTimeData.getBoolean('allowFirstPartyVietnameseInput');
+
   /**
    * Get suggested input methods based on user's enabled languages and ARC IMEs
    */
@@ -52,17 +57,20 @@ class OsSettingsAddInputMethodsDialogElement extends PolymerElement {
     ];
     let inputMethods =
         this.languageHelper.getInputMethodsForLanguages(languageCodes);
-    // Temporary solution for b/237492047: move Vietnamese extension input
-    // methods to the top of the suggested list.
-    // TODO(b/237492047): Remove this once 1P Vietnamese input methods are
-    // suitable for widespread use.
-    const isVietnameseExtension =
-        (inputMethod: chrome.languageSettingsPrivate.InputMethod): boolean =>
-            (inputMethod.id.startsWith('_ext_ime_') &&
-             inputMethod.languageCodes.includes('vi'));
-    inputMethods = inputMethods.filter(isVietnameseExtension)
-                       .concat(inputMethods.filter(
-                           inputMethod => !isVietnameseExtension(inputMethod)));
+    if (this.shouldPrioritiseVietnameseExtensions_) {
+      // Temporary solution for b/237492047: move Vietnamese extension input
+      // methods to the top of the suggested list.
+      // TODO(b/237492047): Remove this once 1P Vietnamese input methods are
+      // launched.
+      const isVietnameseExtension =
+          (inputMethod: chrome.languageSettingsPrivate.InputMethod): boolean =>
+              (inputMethod.id.startsWith('_ext_ime_') &&
+               inputMethod.languageCodes.includes('vi'));
+      inputMethods =
+          inputMethods.filter(isVietnameseExtension)
+              .concat(inputMethods.filter(
+                  inputMethod => !isVietnameseExtension(inputMethod)));
+    }
     return inputMethods.map(inputMethod => inputMethod.id);
   }
 
