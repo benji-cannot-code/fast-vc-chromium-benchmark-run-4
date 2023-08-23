28ba02177387ca2541bac09bde1349fbd4fbce79
Cassy Chun-Crogan <cassycc@google.com>
2023-08-23 03:22:24 +0000

    Reland "[Office Fallback] Update check for QuickOffice extension on Lacros"
    This is a reland of commit 54083c462e736a6f6cfd767d579a7ab2431fe07e
    
    Fix broken test FallbackFailsNoQuickOffice by uninstalling QuickOffice
    via the ExtensionService::RemoveComponentExtension(), instead of the
    previously used ExtensionRegistry::RemoveEnabled() which didn't notify
    the App Service. RemoveComponentExtension() notifies the observers,
    including the App Service [1] which will update the AppRegistryCache
    [2].
    
    Also move the checks for the Fallback succeeding to a separate test
    FallbackSucceedsWithQuickOffice.
    
    Change IsExtensionInstalled() to IsQuickOfficeInstalled() as using
    the AppRegistryCache to check if the QuickOffice extension is installed
    is correct, but other extensions are not necessarily found on the
    AppRegistryCache.
    
    Thus, remove the IsExtensionInstalled test and change the
    IsExtensionInstalledQuickOffice test to become IsQuickOfficeInstalled.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/apps/app_service/publishers/extension_apps_base.h;l=159
    [2] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/apps/app_service/publishers/extension_apps_base.cc;l=527;drc=ef54bf45d50e6f9990713781e788c48102639d5e;bpv=1;bpt=1
    
    Original change's description:
    > [Office Fallback] Update check for QuickOffice extension on Lacros
    >
    > Before displaying the Office Fallback dialog, there is a check to make
    > sure the QuickOffice extension is installed.
    >
    > Previously, the ExtensionRegistry was checked but this does not contain
    > QuickOffice installed on Lacros.
    >
    > Instead use the AppRegistryCache which contains QuickOffice whether Ash
    > or Lacros is being used http://crrev.com/c/3193930.
    >
    > Bug: b/295767856
    > Change-Id: I9f677fbf6cfede629c5a1f549272ed98fe05b03c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4787011
    > Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    > Reviewed-by: Alexander Bolodurin <alexbn@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1185042}
    
    Bug: b/295767856
    Change-Id: Iad98025a401a37bbd1fc6192a5d1d3c69699a35a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4794311
    Reviewed-by: Alexander Bolodurin <alexbn@chromium.org>
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1187012}

--- chrome/browser/ash/file_manager/file_tasks.cc
+++ chrome/browser/ash/file_manager/file_tasks.cc
@@ -82,6 +82,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/app_service/public/cpp/app_update.h"
 #include "components/services/app_service/public/cpp/file_handler.h"
 #include "components/services/app_service/public/cpp/file_handler_info.h"
+#include "components/services/app_service/public/cpp/types_util.h"
 #include "content/public/browser/network_service_instance.h"
 #include "extensions/browser/api/file_handlers/mime_util.h"
 #include "extensions/browser/entry_info.h"
@@ -1068,8 +1069,7 @@ bool GetUserFallbackChoice(
     gfx::NativeWindow modal_parent,
     ash::office_fallback::FallbackReason fallback_reason) {
   // If QuickOffice is not installed, don't launch dialog.
-  if (!IsExtensionInstalled(profile,
-                            extension_misc::kQuickOfficeComponentExtensionId)) {
+  if (!IsQuickOfficeInstalled(profile)) {
     LOG(ERROR) << "Cannot fallback to QuickOffice when it is not installed";
     return false;
   }
@@ -1268,11 +1268,21 @@ bool IsOpenInOfficeTask(const TaskDescriptor& task) {
   return IsFilesAppId(task.app_id) && action_id == kActionIdOpenInOffice;
 }
 
-bool IsExtensionInstalled(Profile* profile, const std::string& extension_id) {
-  extensions::ExtensionRegistry* registry =
-      extensions::ExtensionRegistry::Get(profile);
-  return registry->GetExtensionById(extension_id,
-                                    extensions::ExtensionRegistry::ENABLED);
+bool IsQuickOfficeInstalled(Profile* profile) {
+  apps::AppServiceProxy* proxy =
+      apps::AppServiceProxyFactory::GetForProfile(profile);
+  if (!proxy) {
+    return false;
+  }
+  // The AppRegistryCache will contain the QuickOffice extension whether on Ash
+  // or Lacros.
+  bool installed = false;
+  proxy->AppRegistryCache().ForOneApp(
+      extension_misc::kQuickOfficeComponentExtensionId,
+      [&installed](const apps::AppUpdate& update) {
+        installed = apps_util::IsInstalled(update.Readiness());
+      });
+  return installed;
 }
 
 bool IsHtmlFile(const base::FilePath& path) {
