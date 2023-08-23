410e05e5646b111c5f9bbbd0bd6a09fa658246c5
Christian Flach <cmfcmf@chromium.org>
2023-08-23 12:30:27 +0000

    web-bundles: Format code to reduce conflicts in CLs
    This CL runs `clang-format` over `components/web_package`, which mostly
    adds missing brackets on if statements. This makes the code easier to
    read and extend, and also avoids conflicts with other CLs that modify
    this code.
    
    Bug: None
    Change-Id: I070153665ae4e27a5784ee7887726c07e23eb573
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4803870
    Auto-Submit: Christian Flach <cmfcmf@chromium.org>
    Reviewed-by: Sonja Laurila <laurila@google.com>
    Commit-Queue: Christian Flach <cmfcmf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187211}

--- components/web_package/web_bundle_parser_factory.cc
+++ components/web_package/web_bundle_parser_factory.cc
@@ -56,8 +56,7 @@ WebBundleParserFactory::WebBundleParserFactory() = default;
 WebBundleParserFactory::~WebBundleParserFactory() = default;
 
 std::unique_ptr<mojom::BundleDataSource>
-WebBundleParserFactory::CreateFileDataSourceForTesting(
-    base::File file) {
+WebBundleParserFactory::CreateFileDataSourceForTesting(base::File file) {
   return std::make_unique<FileDataSource>(std::move(file));
 }
 
