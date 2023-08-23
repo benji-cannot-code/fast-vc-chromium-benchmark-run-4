6a88ac3488e6c5a15cc6969d159a75c6e3ce5256
Brandon Nguyen <bpnguyen@google.com>
2023-08-23 00:29:23 +0000

    Sync pip-double-tap chrome flag to ARC pip-double-tap flag
    Corresponding Android CL: ag/24439388
    
    Bug: b:284377213
    Test: ash_components_unittests
    Change-Id: Ia2050c367d99d4ed5c3dd8d86d05f1ebb698c0ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4781769
    Commit-Queue: Kazuki Takise <takise@chromium.org>
    Reviewed-by: Kazuki Takise <takise@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Josh Horwich <jhorwich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186946}

--- ash/components/arc/chrome_feature_flags/arc_chrome_feature_flags_bridge.cc
+++ ash/components/arc/chrome_feature_flags/arc_chrome_feature_flags_bridge.cc
@@ -92,6 +92,7 @@ void ArcChromeFeatureFlagsBridge::NotifyFeatureFlags() {
           : mojom::RoundedWindowCompatStrategy::kDisabled;
   flags->rounded_window_radius = chromeos::features::RoundedWindowsRadius();
   flags->xdg_mode = base::FeatureList::IsEnabled(kXdgMode);
+  flags->enable_pip_double_tap = ash::features::IsPipDoubleTapToResizeEnabled();
 
   chrome_feature_flags_instance->NotifyFeatureFlags(std::move(flags));
 }
