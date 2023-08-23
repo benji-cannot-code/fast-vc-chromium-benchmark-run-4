2a54a00b4eecb937fc0b5411fcd5f3be2b64f5fd
Philip Pfaffe <pfaffe@chromium.org>
2023-08-23 08:07:06 +0000

    Fix renderer crash with nested sticky containers and layers agent
    Fixed: 1420408
    Change-Id: Ic37158fd4de20f86d21fbfc6d38fd7bde24cffa7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800462
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Auto-Submit: Philip Pfaffe <pfaffe@chromium.org>
    Reviewed-by: Changhao Han <changhaohan@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187096}

---
+++ third_party/blink/web_tests/inspector-protocol/layers/layer-sticky.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function(testRunner) {
+  const {page, session, dp} = await testRunner.startHTML(`
+  <style>
+    .tall {
+      height: 200vh;
+    }
+    .sticky {
+      height: 50px;
+      position: sticky;
+      top: 0;
+    }
+  </style>
+
+  <div class="sticky">
+    <div class="sticky">
+    </div>
+  </div>
+  <div class="tall">
+  </div>
+`, 'Tests nested sticky containers');
+
+  await dp.LayerTree.enable();
+  await dp.LayerTree.onceLayerTreeDidChange();
+
+  testRunner.log('Didn\'t crash');
+  testRunner.completeTest();
+});
