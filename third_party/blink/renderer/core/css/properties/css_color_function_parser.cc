2cd96b9244797cc539cbedf78d929577ab192931
Aaron Krajeski <aaronhk@chromium.org>
2023-08-23 14:15:24 +0000

    Add usage counter for Relative Color Syntax
    Bug: 1475009
    Change-Id: I72e5c07abd86f789801e9786be8a7e0bfdeb602e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4804364
    Commit-Queue: Aaron Krajeski <aaronhk@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187258}

--- third_party/blink/renderer/core/css/properties/css_color_function_parser.cc
+++ third_party/blink/renderer/core/css/properties/css_color_function_parser.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "third_party/blink/renderer/core/css/properties/css_color_function_parser.h"
+#include "third_party/blink/public/mojom/use_counter/metrics/web_feature.mojom-shared.h"
 #include "third_party/blink/renderer/core/css/css_color.h"
 #include "third_party/blink/renderer/core/css/css_identifier_value.h"
 #include "third_party/blink/renderer/core/css/css_math_function_value.h"
@@ -516,6 +517,11 @@ bool ColorFunctionParser::ConsumeFunctionalSyntaxColor(
                                  channels_[2], alpha_);
   // The parsing was successful, so we need to consume the input.
   input_range = range;
+
+  if (is_relative_color_) {
+    context.Count(WebFeature::kCSSRelativeColor);
+  }
+
   return true;
 }
 
