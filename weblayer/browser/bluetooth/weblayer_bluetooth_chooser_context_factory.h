77b9bfeeb3ddcb5f06eaca2b542e442a1988d7af
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-23 19:52:45 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: Icd7d5d228c1624e0e74f8d967114d2fb2b516019
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4802107
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187442}

--- weblayer/browser/bluetooth/weblayer_bluetooth_chooser_context_factory.h
+++ weblayer/browser/bluetooth/weblayer_bluetooth_chooser_context_factory.h
@@ -36,7 +36,7 @@ class WebLayerBluetoothChooserContextFactory
   ~WebLayerBluetoothChooserContextFactory() override;
 
   // BrowserContextKeyedServiceFactory implementation:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
   content::BrowserContext* GetBrowserContextToUse(
       content::BrowserContext* context) const override;
