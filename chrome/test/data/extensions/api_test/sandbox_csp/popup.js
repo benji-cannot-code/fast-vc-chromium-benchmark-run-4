50768d53240d1e4dd913e05b4577fe4dd3ec5d33
Illia Klimov <elklm@google.com>
2023-08-23 11:16:07 +0000

    Add tests for CSP sandbox extension fix
    Bug: 1407986
    Change-Id: Ie0ed70c0caddc5243725889bfb04bd2ee4767a05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4803043
    Commit-Queue: Elias Klim <elklm@chromium.org>
    Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187186}

---
+++ chrome/test/data/extensions/api_test/sandbox_csp/popup.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.runTests([
+  function checkExecuteScript() {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      chrome.tabs.executeScript(
+        tabs[0].id,
+        { code: 'var x = 1;' },
+        () => {
+          let lastError = chrome.runtime.lastError;
+          if (lastError) {
+            chrome.test.fail();
+          } else {
+            chrome.test.succeed();
+          }
+        }
+      );
+    });
+  },
+]);
