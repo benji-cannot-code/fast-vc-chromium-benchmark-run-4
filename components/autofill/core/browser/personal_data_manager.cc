46d7a149c97df76f638cb798cd69da879a9589df
Matthias Körber <koerber@google.com>
2023-08-23 17:13:33 +0000

    Enabled Leipzig Migration by default
    This CL enables the feature by default and removes the field
    trial config.
    
    On top, this CL addresses a small bug in the test logic which
    only surfaces for account profiles: The ClearAllLocalData()
    function in the PDM is used to reset all previously imported
    profiles. However, account profiles are not considered local
    in the database. As a result, they are loaded again after
    clearing which breaks the test.
    
    The test is fixed by not relying on clearing but to explicitly
    test for the existence of both profiles.
    
    Change-Id: I2fcc912d11b95045a293dab8037b144f33a470e9
    Bug: 1475277, 1423319
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4803828
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Auto-Submit: Matthias Körber <koerber@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Dominic Battre <battre@chromium.org>
    Commit-Queue: Matthias Körber <koerber@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1187338}

--- components/autofill/core/browser/personal_data_manager.cc
+++ components/autofill/core/browser/personal_data_manager.cc
@@ -1251,6 +1251,8 @@ void PersonalDataManager::ClearAllLocalData() {
   // Even though `account_profiles_` are not "local", the local/server
   // distinction in the PersonalDataManager only exists for historical reasons
   // and all AutofillProfiles fall in the local category.
+  // TODO(crbug.com/1475277): This call needs to be removed since account
+  // profiles are not cleared in the database.
   account_profiles_.clear();
 }
 
