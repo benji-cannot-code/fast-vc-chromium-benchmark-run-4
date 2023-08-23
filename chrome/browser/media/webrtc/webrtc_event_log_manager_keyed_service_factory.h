100f12f24b57202a53c5ece89cd841e797c4adbf
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-23 03:06:43 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: I44f83999d809d24d2912dbe7a137121877bcb466
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800123
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187005}

--- chrome/browser/media/webrtc/webrtc_event_log_manager_keyed_service_factory.h
+++ chrome/browser/media/webrtc/webrtc_event_log_manager_keyed_service_factory.h
@@ -37,7 +37,7 @@ class WebRtcEventLogManagerKeyedServiceFactory
   WebRtcEventLogManagerKeyedServiceFactory();
   ~WebRtcEventLogManagerKeyedServiceFactory() override;
 
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
