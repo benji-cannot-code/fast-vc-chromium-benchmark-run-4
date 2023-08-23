5003e0e215741060a4089124a7fd0c0e562d1d5e
Paul Adedeji <pauladedeji@google.com>
2023-08-23 16:43:07 +0000

    [ntp][modules] Add action menu to Drive v2
    *Demo and specs in bug description*
    
    Change-Id: Ic9dda5f7e79d9f512832e003e172b4ae5e086ad5
    Bug: 1467935
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4795140
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187320}

--- chrome/browser/resources/new_tab_page/modules/v2/drive/module.ts
+++ chrome/browser/resources/new_tab_page/modules/v2/drive/module.ts
@@ -14,6 +14,7 @@ import {I18nMixin, loadTimeData} from '../../../i18n_setup.js';
 import {DriveProxy} from '../../drive/drive_module_proxy.js';
 import {InfoDialogElement} from '../../info_dialog.js';
 import {ModuleDescriptor} from '../../module_descriptor.js';
+import {MenuItem, ModuleHeaderElementV2} from '../module_header.js';
 
 import {getTemplate} from './module.html.js';
 
@@ -22,6 +23,7 @@ export interface DriveModuleElement {
     fileRepeat: DomRepeat,
     files: HTMLElement,
     infoDialogRender: CrLazyRenderElement<InfoDialogElement>,
+    moduleHeaderElementV2: ModuleHeaderElementV2,
   };
 }
 
@@ -52,6 +54,31 @@ export class DriveModuleElement extends I18nMixin
         file.mimeType;
   }
 
+  private getMenuItemGroups_(): MenuItem[][] {
+    return [
+      [
+        {
+          action: 'disable',
+          icon: 'modules:block',
+          text: this.i18nRecursive(
+              '', 'modulesDisableButtonTextV2', 'modulesDriveSentence'),
+        },
+        {
+          action: 'info',
+          icon: 'modules:info',
+          text: this.i18n('moduleInfoButtonTitle'),
+        },
+      ],
+      [
+        {
+          action: 'customize-module',
+          icon: 'modules:tune',
+          text: this.i18n('modulesCustomizeButtonText'),
+        },
+      ],
+    ];
+  }
+
   private onDisableButtonClick_() {
     const disableEvent = new CustomEvent('disable-module', {
       composed: true,
@@ -73,6 +100,10 @@ export class DriveModuleElement extends I18nMixin
 
   private onInfoButtonClick_() {
     this.$.infoDialogRender.get().showModal();
+  }
+
+  private onMenuButtonClick_(e: Event) {
+    this.$.moduleHeaderElementV2.showAt(e);
   }
 }
 
