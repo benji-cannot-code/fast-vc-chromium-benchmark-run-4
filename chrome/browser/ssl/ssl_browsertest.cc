456f4516486a22d8461d5fa8d454f6ede6394b19
Erik Chen <erikchen@chromium.org>
2023-08-23 21:53:28 +0000

    Don't let WebUI events cross the incognito barrier.
    WebUI events use the same dispatch mechanism as extensions. Extensions
    have a mechanism to allow some extension events to cross the incognito
    barrier. These checks were inadvertently being skipped for WebUI and
    webpages.
    
    This CL adds the check for WebUI and webpages to enforce that no events
    cross the incognito boundary. This CL updates SettingsPrivateEventRouter
    to attach a browser context to the generated event.
    
    Bug: 1381219
    Change-Id: Iffa6fc40de737b353ab2705919f4863c583d2c5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4786992
    Reviewed-by: Emily Stark <estark@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187516}

--- chrome/browser/ssl/ssl_browsertest.cc
+++ chrome/browser/ssl/ssl_browsertest.cc
@@ -2336,7 +2336,8 @@ IN_PROC_BROWSER_TEST_F(SSLUITest, TestExtensionEvents) {
       event_names_.push_back(event.event_name);
     }
 
-    void OnDidDispatchEventToProcess(const extensions::Event& event) override {}
+    void OnDidDispatchEventToProcess(const extensions::Event& event,
+                                     int process_id) override {}
 
     const std::vector<std::string>& event_names() const { return event_names_; }
 
--- extensions/browser/api/web_request/permission_helper.h
+++ extensions/browser/api/web_request/permission_helper.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "extensions/browser/browser_context_keyed_api_factory.h"
+#include "extensions/browser/process_map.h"
 
 namespace extensions {
 class ExtensionRegistry;
@@ -34,7 +35,9 @@ class PermissionHelper : public BrowserContextKeyedAPI {
   bool ShouldHideBrowserNetworkRequest(const WebRequestInfo& request) const;
   bool CanCrossIncognito(const Extension* extension) const;
 
-  const ProcessMap* process_map() const { return process_map_; }
+  const ProcessMap* process_map() const {
+    return ProcessMap::Get(browser_context_);
+  }
 
   const ExtensionRegistry* extension_registry() const {
     return extension_registry_;
@@ -44,7 +47,6 @@ class PermissionHelper : public BrowserContextKeyedAPI {
   friend class BrowserContextKeyedAPIFactory<PermissionHelper>;
 
   const raw_ptr<content::BrowserContext> browser_context_;
-  const raw_ptr<ProcessMap> process_map_;
   const raw_ptr<ExtensionRegistry> extension_registry_;
 
   // BrowserContextKeyedAPI implementation.
