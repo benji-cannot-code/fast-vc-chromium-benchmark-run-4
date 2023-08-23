b5127395c1e0c6868a8379b4f3aab6b3509917ab
Guido Urdaneta <guidou@chromium.org>
2023-08-23 14:00:26 +0000

    Reland "Add prefinalizer to UserMediaProcessor"
    This reverts commit 13d8042a591354035b3f2e5bf1ddab332aee0920.
    
    Reason for revert: Found the reason for the crash. Amended patch.
    Adds null check in StopLocalSource.
    
    Original change's description:
    > Revert "Add prefinalizer to UserMediaProcessor"
    >
    > This reverts commit 0f7d865fa0f345238ec9d5f1c3616d9052d7ab6c.
    >
    > Reason for revert: Causing build failures: crbug.com/1475188
    >
    > Original change's description:
    > > Add prefinalizer to UserMediaProcessor
    > >
    > > Invoke StopAllProcessing() as prefinalizer to ensure clean shutdown.
    > >
    > > Bug: 1282467, b/297017450
    > > Change-Id: I1cea50b889cba4b45042aca3d03113eed8d145eb
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4802883
    > > Reviewed-by: Palak Agarwal <agpalak@chromium.org>
    > > Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1187104}
    >
    > Bug: 1282467, b/297017450
    > Change-Id: Ifd6247ac180bc6bee6f96c38a7a87e63ab72ef6f
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4806485
    > Owners-Override: Andy Paicu <andypaicu@google.com>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Andy Paicu <andypaicu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1187163}
    
    Bug: 1282467, b/297017450
    Change-Id: I0d2f9c401e150d9709f28ac00bba7a68cd76b0b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4803831
    Reviewed-by: Palak Agarwal <agpalak@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187251}

--- third_party/blink/renderer/modules/mediastream/user_media_processor.cc
+++ third_party/blink/renderer/modules/mediastream/user_media_processor.cc
@@ -1982,6 +1982,9 @@ void UserMediaProcessor::OnLocalSourceStopped(
 void UserMediaProcessor::StopLocalSource(MediaStreamSource* source,
                                          bool notify_dispatcher) {
   WebPlatformMediaStreamSource* source_impl = source->GetPlatformSource();
+  if (!source_impl) {
+    return;
+  }
   SendLogMessage(base::StringPrintf(
       "StopLocalSource({session_id=%s})",
       source_impl->device().session_id().ToString().c_str()));
--- third_party/blink/renderer/modules/mediastream/user_media_processor.h
+++ third_party/blink/renderer/modules/mediastream/user_media_processor.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/mediastream/user_media_request.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
+#include "third_party/blink/renderer/platform/heap/prefinalizer.h"
 #include "third_party/blink/renderer/platform/mediastream/media_stream_descriptor.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_wrapper_mode.h"
@@ -48,6 +49,8 @@ class WebString;
 // render thread. There should be only one UserMediaProcessor per frame.
 class MODULES_EXPORT UserMediaProcessor
     : public GarbageCollected<UserMediaProcessor> {
+  USING_PRE_FINALIZER(UserMediaProcessor, StopAllProcessing);
+
  public:
   using MediaDevicesDispatcherCallback = base::RepeatingCallback<
       blink::mojom::blink::MediaDevicesDispatcherHost*()>;
