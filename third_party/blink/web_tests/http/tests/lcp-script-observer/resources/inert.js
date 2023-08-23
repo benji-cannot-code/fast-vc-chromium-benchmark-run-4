184c6cab87e8db5a58978aef3bb270a080d6326a
Alex N. Jose <alexnj@chromium.org>
2023-08-23 08:17:17 +0000

    [LCPP] LCP ScriptObserver web_tests.
    This CL adds test cases for crrev.com/c/4651499. The tests assert
    that LCPScriptObserver's HTMLImageElement script dependency feature
    works as expected in different scenarios that involve async scripts.
    
    Bug: 1419756
    Change-Id: Id60df6649cccaf35f51acbd86995c96a08fcd897
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4767918
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187099}

---
+++ third_party/blink/web_tests/http/tests/lcp-script-observer/resources/inert.js
@@ -0,0 +1,5 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// this is a script that's not contributing anything to lcp
+window.addEventListener('load', () => {
+  console.log('inert.js');
+});
