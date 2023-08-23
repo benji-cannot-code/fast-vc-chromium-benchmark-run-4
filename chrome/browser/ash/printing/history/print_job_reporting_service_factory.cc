ad56aebd9759bd26456bb251fd8d1d426749d014
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-23 01:09:38 +0000

    Implements the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one being deprecated, this CL
    replaces it by the first one.
    
    Bug: 1396138
    Change-Id: I99f0034e311081e1da1497101c9eda3283145d35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798499
    Reviewed-by: Piotr Pawliczek <pawliczek@chromium.org>
    Commit-Queue: Piotr Pawliczek <pawliczek@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186960}

--- chrome/browser/ash/printing/history/print_job_reporting_service_factory.cc
+++ chrome/browser/ash/printing/history/print_job_reporting_service_factory.cc
@@ -36,11 +36,10 @@ PrintJobReportingServiceFactory::PrintJobReportingServiceFactory()
 
 PrintJobReportingServiceFactory::~PrintJobReportingServiceFactory() = default;
 
-KeyedService* PrintJobReportingServiceFactory::BuildServiceInstanceFor(
+std::unique_ptr<KeyedService>
+PrintJobReportingServiceFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
-  auto reporting_service = PrintJobReportingService::Create();
-
-  return reporting_service.release();
+  return PrintJobReportingService::Create();
 }
 
 bool PrintJobReportingServiceFactory::ServiceIsNULLWhileTesting() const {
--- chrome/browser/ash/printing/history/print_job_reporting_service_factory.h
+++ chrome/browser/ash/printing/history/print_job_reporting_service_factory.h
@@ -38,7 +38,7 @@ class PrintJobReportingServiceFactory : public ProfileKeyedServiceFactory {
   ~PrintJobReportingServiceFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
   bool ServiceIsNULLWhileTesting() const override;
 };
