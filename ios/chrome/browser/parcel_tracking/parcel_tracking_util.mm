60147e343f3ff994992e71302a1092b24f1ae56a
Hira Mahmood <hiramahmood@google.com>
2023-08-23 21:40:26 +0000

    [iOS] Create IsUserEligibleParcelTrackingOptInPrompt util method
    This CL creates a util method that will be used to check a
    user's eligibility for the parcel tracking opt-in prompt.
    
    Bug: 1473449
    Change-Id: I74460cc747f21bc49238d985ce58302d0ca28cbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4803184
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187511}

--- ios/chrome/browser/parcel_tracking/parcel_tracking_util.mm
+++ ios/chrome/browser/parcel_tracking/parcel_tracking_util.mm
@@ -5,10 +5,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/parcel_tracking/parcel_tracking_util.h"
 
+#import "components/prefs/pref_service.h"
+#import "components/signin/public/base/consent_level.h"
+#import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
+#import "ios/chrome/browser/shared/model/prefs/pref_names.h"
+#import "ios/chrome/browser/signin/authentication_service.h"
+#import "ios/chrome/browser/signin/authentication_service_factory.h"
+
 BASE_FEATURE(kIOSParcelTracking,
              "IOSParcelTracking",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 bool IsIOSParcelTrackingEnabled() {
   return base::FeatureList::IsEnabled(kIOSParcelTracking);
+}
+
+bool IsUserEligibleParcelTrackingOptInPrompt(
+    ChromeBrowserState* browser_state) {
+  PrefService* pref_service = browser_state->GetPrefs();
+  AuthenticationService* authentication_service =
+      AuthenticationServiceFactory::GetForBrowserState(browser_state);
+  if (!authentication_service || !authentication_service->initialized()) {
+    return false;
+  }
+  bool signed_in =
+      authentication_service->HasPrimaryIdentity(signin::ConsentLevel::kSignin);
+  return IsIOSParcelTrackingEnabled() &&
+         !pref_service->GetBoolean(
+             prefs::kIosParcelTrackingOptInPromptDisplayed) &&
+         signed_in;
 }
