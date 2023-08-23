2ccc2f6f06bd5736f497ffb83cae82e8494cd12b
Vovo Yang <vovoy@google.com>
2023-08-23 23:03:27 +0000

    sandbox: Remove AdjustLowMemoryMargin declaration
    AdjustLowMemoryMargin declaration is not used.
    
    Bug: b/273672971
    Change-Id: I19f6b8e759870ed84a6a4afa460150c2d695941f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4802632
    Commit-Queue: Vovo Yang <vovoy@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187540}

--- sandbox/linux/suid/process_util.h
+++ sandbox/linux/suid/process_util.h
@@ -23,10 +23,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // values, of course.
 bool AdjustOOMScore(pid_t process, int score);
 
-// This adjusts /sys/kernel/mm/chromeos-low_mem/margin so that
-// the kernel notifies us that we are low on memory when less than
-// |margin_mb| megabytes are available.  Setting |margin_mb| to -1
-// turns off low memory notification.
-bool AdjustLowMemoryMargin(int64_t margin_mb);
-
 #endif  // SANDBOX_LINUX_SUID_PROCESS_UTIL_H_
