a971dfd42d5a24deaf10a866497f04ed568a5e37
Yuki Shiino <yukishiino@chromium.org>
2023-08-23 06:05:51 +0000

    Revert "[bfcache] Introduce NotRestoredReasons interface"
    This reverts commit c5d373e378134b9594f80045f747f5b5b8966402.
    
    Reason for revert: https://crbug.com/1474767
    
    Original change's description:
    > [bfcache] Introduce NotRestoredReasons interface
    >
    > This CL introduces NotRestoredReasons interface to make the implementation match the spec draft[1].
    > This also changes NotRestoredReasons attribute's name from `blocked` to `preventedBackForwardCache` and its return value, thus changing WPT as well.
    >
    > [1]: https://github.com/whatwg/html/pull/9360
    >
    > Bug: 1472637
    > Change-Id: I4dcecd5a012c36c883933ed3d04c776bfe844f6c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4770594
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1185723}
    
    Bug: 1472637
    Change-Id: Ie2e4322abd062a4cdff14f9f78c8651c06453ae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4805368
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187060}

--- third_party/blink/renderer/core/timing/performance_navigation_timing.h
+++ third_party/blink/renderer/core/timing/performance_navigation_timing.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/dom_high_res_time_stamp.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
-#include "third_party/blink/renderer/core/timing/not_restored_reasons.h"
 #include "third_party/blink/renderer/core/timing/performance_resource_timing.h"
 
 namespace blink {
@@ -51,7 +50,7 @@ class CORE_EXPORT PerformanceNavigationTiming final
   DOMHighResTimeStamp loadEventEnd() const;
   AtomicString type() const;
   uint16_t redirectCount() const;
-  NotRestoredReasons* notRestoredReasons() const;
+  ScriptValue notRestoredReasons(ScriptState* script_state) const;
   AtomicString systemEntropy() const;
   DOMHighResTimeStamp criticalCHRestart(ScriptState* script_state) const;
 
@@ -82,7 +81,8 @@ class CORE_EXPORT PerformanceNavigationTiming final
 
   bool AllowRedirectDetails() const;
 
-  NotRestoredReasons* BuildNotRestoredReasons(
+  ScriptValue NotRestoredReasonsBuilder(
+      ScriptState* script_state,
       const mojom::blink::BackForwardCacheNotRestoredReasonsPtr& reasons) const;
 };
 }  // namespace blink
