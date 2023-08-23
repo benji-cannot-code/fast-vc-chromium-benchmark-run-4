2cb4b218e528cd52918dde2cbe66dafb3c26a9d7
Marcello Salomao <msalomao@google.com>
2023-08-23 00:18:37 +0000

    [Files app] Ducks: reducer migration: ui entries
    Bug: b:295777015
    Test: CQ passes
    Change-Id: I9e063f9dd71e46d5baba950f54f62bc2baf9eba3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4801163
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Marcello Salomao <msalomao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1186941}

--- ui/file_manager/file_manager/state/reducers/root.ts
+++ ui/file_manager/file_manager/state/reducers/root.ts
@@ -7,6 +7,7 @@ import {State} from '../../externs/ts/state.js';
 import {Action, ActionType} from '../actions.js';
 import {bulkPinningReducersMap} from '../ducks/bulk_pinning.js';
 import {searchReducersMap} from '../ducks/search.js';
+import {uiEntriesReducersMap} from '../ducks/ui_entries.js';
 import {volumesReducersMap} from '../ducks/volumes.js';
 
 import {addChildEntries, cacheEntries, clearCachedEntries, updateMetadata} from './all_entries.js';
@@ -15,11 +16,14 @@ import {changeDirectory, updateDirectoryContent, updateFileTasks, updateSelectio
 import {addFolderShortcut, refreshFolderShortcut, removeFolderShortcut} from './folder_shortcuts.js';
 import {refreshNavigationRoots, updateNavigationEntry} from './navigation.js';
 import {updatePreferences} from './preferences.js';
-import {addUiEntry, removeUiEntry} from './ui_entries.js';
 
 // Reducers map created from merging together each slice's exported reducersMap.
-const rootReducersMap = new Map(
-    [...searchReducersMap, ...volumesReducersMap, ...bulkPinningReducersMap]);
+const rootReducersMap = new Map([
+  ...searchReducersMap,
+  ...volumesReducersMap,
+  ...bulkPinningReducersMap,
+  ...uiEntriesReducersMap,
+]);
 
 /**
  * Root reducer for the State for Files app.
@@ -57,10 +61,6 @@ export function rootReducer(currentState: State, action: Action): State {
       return refreshNavigationRoots(currentState, action);
     case ActionType.UPDATE_NAVIGATION_ENTRY:
       return updateNavigationEntry(currentState, action);
-    case ActionType.ADD_UI_ENTRY:
-      return addUiEntry(currentState, action);
-    case ActionType.REMOVE_UI_ENTRY:
-      return removeUiEntry(currentState, action);
     case ActionType.REFRESH_FOLDER_SHORTCUT:
       return refreshFolderShortcut(currentState, action);
     case ActionType.ADD_FOLDER_SHORTCUT:
