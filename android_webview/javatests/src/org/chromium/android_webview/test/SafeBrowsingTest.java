ace4ea52d65ad9a394e1a5580d6cc6e53ba7de1c
Wayne Jackson Jr. <wbjacksonjr@google.com>
2023-08-23 11:53:35 +0000

    [webview][multi-profile 1/n] Expose Browser Context Name & Path to Java Layer
    This CL exposes the profile name to the Java layer. This name will be
    used by the glue Profile class to expose the name to the public API.
    
    - The name will be used for the public Profile API.
    - The relative path will be used to construct the HTTP auth db & shared
      preferences file names.
    
    Bug: 1446913
    Change-Id: I44db4166f27bd84bd216dfb012ad593479b9f4b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4790122
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Wayne Jackson Jr. <wbjacksonjr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187200}

--- android_webview/javatests/src/org/chromium/android_webview/test/SafeBrowsingTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/SafeBrowsingTest.java
@@ -196,7 +196,7 @@ public class SafeBrowsingTest {
      */
     private static class MockAwBrowserContext extends AwBrowserContext {
         public MockAwBrowserContext(SharedPreferences sharedPreferences) {
-            super(sharedPreferences, 0, true);
+            super(sharedPreferences, 0);
             SafeBrowsingApiBridge.setHandler(new MockSafetyNetApiHandler());
         }
     }
