7ee7bbef573645ca0cf4df1a1eb737607f5b8a09
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-23 01:15:08 +0000

    Implement unique_ptr builder for UsbConnectionTrackerFactory
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one is being deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: I45c129765d9e5543bf5e0f4fdf90df30a8c1d3ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800453
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186962}

--- chrome/browser/usb/usb_connection_tracker_factory.h
+++ chrome/browser/usb/usb_connection_tracker_factory.h
@@ -27,7 +27,7 @@ class UsbConnectionTrackerFactory : public ProfileKeyedServiceFactory {
   ~UsbConnectionTrackerFactory() override;
 
   // BrowserContextKeyedBaseFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* profile) const override;
   void BrowserContextShutdown(content::BrowserContext* context) override;
 };
