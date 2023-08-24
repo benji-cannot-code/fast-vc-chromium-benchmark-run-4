99f3cb5a43e4e5e9451c8ecf3e16ce43ab8b0d97
Maria Kazinova <kazinova@google.com>
2023-08-24 15:19:29 +0000

    [Passwords] Refactor setting SingleUsernameVoteData in VotesUploader.
    This CL requires an already constructed SingleUsernameVoteData to be
    passed to VotesUploader. A default SingleUsernameVoteData constructor
    is added for convenience.
    This CL is pure refactoring, no behavior is changed.
    
    Bug: 1468297
    Change-Id: I2a87c0b3a08fc7b990ea6a75771e19a0b802b1a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4807994
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1187830}

--- components/password_manager/core/browser/votes_uploader_unittest.cc
+++ components/password_manager/core/browser/votes_uploader_unittest.cc
@@ -679,10 +679,10 @@ TEST_F(VotesUploaderTest, UploadSingleUsernameMultipleFieldsInUsernameForm) {
   form_predictions.fields.back().signature = kSingleUsernameFieldSignature;
 
   std::u16string single_username_candidate_value = u"username_candidate_value";
-  votes_uploader.set_single_username_vote_data(
+  votes_uploader.set_single_username_vote_data(SingleUsernameVoteData(
       kSingleUsernameRendererId, single_username_candidate_value,
       form_predictions,
-      /*stored_credentials=*/{}, /*password_form_had_username_field=*/false);
+      /*stored_credentials=*/{}, /*password_form_had_username_field=*/false));
   votes_uploader.set_suggested_username(single_username_candidate_value);
 #if !BUILDFLAG(IS_ANDROID)
   votes_uploader.CalculateUsernamePromptEditState(
@@ -714,11 +714,11 @@ TEST_F(VotesUploaderTest, UploadNotSingleUsernameForWhitespaces) {
       features::kUsernameFirstFlowFallbackCrowdsourcing);
 
   VotesUploader votes_uploader(&client_, false);
-  votes_uploader.set_single_username_vote_data(
+  votes_uploader.set_single_username_vote_data(SingleUsernameVoteData(
       kSingleUsernameRendererId,
-      /*username_candidate_value=*/u"some search query",
+      /*username_value=*/u"some search query",
       MakeSimpleSingleUsernamePredictions(),
-      /*stored_credentials=*/{}, /*password_form_had_username_field=*/false);
+      /*stored_credentials=*/{}, /*password_form_had_username_field=*/false));
 #if !BUILDFLAG(IS_ANDROID)
   votes_uploader.CalculateUsernamePromptEditState(
       /*saved_username=*/u"saved_value");
@@ -768,10 +768,10 @@ TEST_F(VotesUploaderTest, SingleUsernameValueSuggestedAndAccepted) {
 
   VotesUploader votes_uploader(&client_, false);
   std::u16string single_username_candidate_value = u"username_candidate_value";
-  votes_uploader.set_single_username_vote_data(
+  votes_uploader.set_single_username_vote_data(SingleUsernameVoteData(
       kSingleUsernameRendererId, single_username_candidate_value,
       MakeSimpleSingleUsernamePredictions(), /*stored_credentials=*/{},
-      /*password_form_had_username_field=*/false);
+      /*password_form_had_username_field=*/false));
   votes_uploader.set_suggested_username(single_username_candidate_value);
 #if !BUILDFLAG(IS_ANDROID)
   votes_uploader.CalculateUsernamePromptEditState(
@@ -828,10 +828,10 @@ TEST_F(VotesUploaderTest, SingleUsernameOtherValueSuggestedAndAccepted) {
 
   VotesUploader votes_uploader(&client_, false);
   std::u16string single_username_candidate_value = u"username_candidate_value";
-  votes_uploader.set_single_username_vote_data(
+  votes_uploader.set_single_username_vote_data(SingleUsernameVoteData(
       kSingleUsernameRendererId, single_username_candidate_value,
       MakeSimpleSingleUsernamePredictions(), /*stored_credentials=*/{},
-      /*password_form_had_username_field=*/false);
+      /*password_form_had_username_field=*/false));
   std::u16string suggested_value = u"other_value";
   votes_uploader.set_suggested_username(suggested_value);
 #if !BUILDFLAG(IS_ANDROID)
@@ -888,10 +888,10 @@ TEST_F(VotesUploaderTest, SingleUsernameValueSetInPrompt) {
 
   VotesUploader votes_uploader(&client_, false);
   std::u16string single_username_candidate_value = u"username_candidate_value";
-  votes_uploader.set_single_username_vote_data(
+  votes_uploader.set_single_username_vote_data(SingleUsernameVoteData(
       kSingleUsernameRendererId, single_username_candidate_value,
       MakeSimpleSingleUsernamePredictions(), /*stored_credentials=*/{},
-      /*password_form_had_username_field=*/false);
+      /*password_form_had_username_field=*/false));
   std::u16string suggested_value = u"other_value";
   votes_uploader.set_suggested_username(suggested_value);
 #if !BUILDFLAG(IS_ANDROID)
@@ -947,10 +947,10 @@ TEST_F(VotesUploaderTest, SingleUsernameValueDeletedInPrompt) {
 
   VotesUploader votes_uploader(&client_, false);
   std::u16string single_username_candidate_value = u"username_candidate_value";
-  votes_uploader.set_single_username_vote_data(
+  votes_uploader.set_single_username_vote_data(SingleUsernameVoteData(
       kSingleUsernameRendererId, single_username_candidate_value,
       MakeSimpleSingleUsernamePredictions(), /*stored_credentials=*/{},
-      /*password_form_had_username_field=*/false);
+      /*password_form_had_username_field=*/false));
   votes_uploader.set_suggested_username(single_username_candidate_value);
 #if !BUILDFLAG(IS_ANDROID)
   votes_uploader.CalculateUsernamePromptEditState(/*saved_username=*/u"");
@@ -1005,10 +1005,10 @@ TEST_F(VotesUploaderTest, NotSingleUsernameValueDeletedInPrompt) {
 
   VotesUploader votes_uploader(&client_, false);
   std::u16string single_username_candidate_value = u"username_candidate_value";
-  votes_uploader.set_single_username_vote_data(
+  votes_uploader.set_single_username_vote_data(SingleUsernameVoteData(
       kSingleUsernameRendererId, single_username_candidate_value,
       MakeSimpleSingleUsernamePredictions(), /*stored_credentials=*/{},
-      /*password_form_had_username_field=*/false);
+      /*password_form_had_username_field=*/false));
   std::u16string other_value = u"other_value";
   votes_uploader.set_suggested_username(other_value);
 #if !BUILDFLAG(IS_ANDROID)
@@ -1045,9 +1045,7 @@ TEST_F(VotesUploaderTest, SingleUsernameNoUsernameCandidate) {
       features::kUsernameFirstFlowFallbackCrowdsourcing);
 
   VotesUploader votes_uploader(&client_, false);
-  votes_uploader.set_single_username_vote_data(
-      FieldRendererId(), std::u16string(), FormPredictions(),
-      /*stored_credentials=*/{}, /*password_form_had_username_field=*/false);
+  votes_uploader.set_single_username_vote_data(SingleUsernameVoteData());
   votes_uploader.set_suggested_username(u"");
 #if !BUILDFLAG(IS_ANDROID)
   votes_uploader.CalculateUsernamePromptEditState(/*saved_username=*/u"");
