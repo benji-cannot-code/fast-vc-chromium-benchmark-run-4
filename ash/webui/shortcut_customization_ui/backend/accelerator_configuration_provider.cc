ef0975bb41f23eaffdc5b4564b4f0f2968a17da5
Jimmy <jimmyxgong@chromium.org>
2023-08-24 22:55:53 +0000

    shortcuts: Track add/remove accelerators
    Track with more granularity of which accelerators have
    been added or removed.
    
    Bug: b/216049298
    Test: ash_webui_unittests
    Change-Id: Id752fb2526074738e940bd37332cb18a6a82e4b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800431
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1188068}

--- ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.cc
+++ ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/accelerators/ash_accelerator_configuration.h"
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_pref_names.h"
+#include "ash/public/cpp/accelerator_actions.h"
 #include "ash/public/cpp/accelerators_util.h"
 #include "ash/public/mojom/accelerator_configuration.mojom-shared.h"
 #include "ash/public/mojom/accelerator_configuration.mojom.h"
@@ -63,6 +64,10 @@ constexpr size_t kMaxAcceleratorsAllowed = 5;
 
 constexpr char kShortcutCustomizationHistogramName[] =
     "Ash.ShortcutCustomization.CustomizationAction";
+constexpr char kAddAcceleratorHistogramName[] =
+    "Ash.ShortcutCustomization.AddAccelerator.";
+constexpr char kRemoveDefaultAcceleratorHistogramName[] =
+    "Ash.ShortcutCustomization.RemoveDefaultAccelerator.";
 
 // The following map are accelerators that will not appear in the app and cannot
 // be used as a custom accelerator. For example, if you have an accelerator
@@ -551,6 +556,16 @@ void LogAddAccelerator(mojom::AcceleratorSource source,
           << " with error: " << error;
 }
 
+void RecordEncodedAcceleratorHistogram(const std::string& histogram_name,
+                                       uint32_t action_id,
+                                       const ui::Accelerator& accelerator) {
+  base::UmaHistogramSparse(
+      base::StrCat(
+          {histogram_name, GetAcceleratorActionName(
+                               static_cast<AcceleratorAction>(action_id))}),
+      GetEncodedShortcut(accelerator));
+}
+
 }  // namespace
 
 namespace shortcut_ui {
@@ -890,6 +905,8 @@ void AcceleratorConfigurationProvider::AddAccelerator(
   LogAddAccelerator(source, accelerator, result_data->result);
   base::UmaHistogramEnumeration(kShortcutCustomizationHistogramName,
                                 ShortcutCustomizationAction::kAddAccelerator);
+  RecordEncodedAcceleratorHistogram(kAddAcceleratorHistogramName, action_id,
+                                    accelerator);
   std::move(callback).Run(std::move(result_data));
 }
 
@@ -921,6 +938,15 @@ void AcceleratorConfigurationProvider::RemoveAccelerator(
   base::UmaHistogramEnumeration(
       kShortcutCustomizationHistogramName,
       ShortcutCustomizationAction::kRemoveAccelerator);
+
+  // Only record this metric if the removed accelerator is a default accelerator
+  // for `action_id`.
+  absl::optional<AcceleratorAction> default_id =
+      ash_accelerator_configuration_->GetIdForDefaultAccelerator(accelerator);
+  if (default_id == action_id) {
+    RecordEncodedAcceleratorHistogram(kRemoveDefaultAcceleratorHistogramName,
+                                      action_id, accelerator);
+  }
   std::move(callback).Run(std::move(result_data));
 }
 
