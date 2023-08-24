a4ddd9ad330b2d16273898e07d936fca75182047
ginnnnnnny <ginnyhuang@chromium.org>
2023-08-24 18:12:20 +0000

    [iOS][finch-first-run] Full launch 🎉
    Bug: launch/4218266
    Change-Id: Ifc614dd3bfd98c10390ff6e47ca4a4a1076399d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4808901
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187941}

--- ios/chrome/app/variations_app_state_agent.mm
+++ ios/chrome/app/variations_app_state_agent.mm
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/variations/service/variations_field_trial_creator.h"
 #import "components/variations/service/variations_service_utils.h"
 #import "components/variations/variations_seed_store.h"
-#import "components/version_info/version_info.h"
 #import "ios/chrome/app/application_delegate/app_state.h"
 #import "ios/chrome/app/application_delegate/startup_information.h"
 #import "ios/chrome/app/launch_screen_view_controller.h"
@@ -25,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/first_run/first_run_util.h"
 #import "ios/chrome/browser/variations/ios_chrome_variations_seed_fetcher.h"
 #import "ios/chrome/browser/variations/ios_chrome_variations_seed_store.h"
-#import "ios/chrome/common/channel_info.h"
 
 // Name of trial and experiment groups.
 const char kIOSChromeVariationsTrialName[] = "kIOSChromeVariationsTrial";
@@ -41,7 +39,6 @@ using ::variations::HasSeedExpiredSinceTime;
 using ::variations::SeedApplicationStage;
 using ::variations::VariationsSeedExpiry;
 using ::variations::VariationsSeedStore;
-using ::version_info::Channel;
 
 // The NSUserDefault key to store the time the last seed is fetched.
 NSString* kLastVariationsSeedFetchTimeKey = @"kLastVariationsSeedFetchTime";
@@ -61,20 +58,6 @@ enum class IOSChromeVariationsGroup {
 };
 
 #pragma mark - Helpers
-
-// Group weight for both enabled and control experiment groups.
-// NOTE: The value will be updated during the incremental rollout period.
-int GetGroupWeight() {
-  switch (GetChannel()) {
-    case Channel::UNKNOWN:
-    case Channel::CANARY:
-    case Channel::DEV:
-    case Channel::BETA:
-      return 50;
-    case Channel::STABLE:
-      return 50;
-  }
-}
 
 // Returns the fetch time of the variations seed store fetched by a previous
 // run, and null if such seed doesn't exist.
@@ -197,16 +180,14 @@ void SaveFetchTimeOfLatestSeedInLocalState() {
 @implementation VariationsAppStateAgent
 
 - (instancetype)init {
-  int groupWeight = GetGroupWeight();
-  DCHECK_LE(groupWeight, 50);
   // Note: `ShouldPresentFirstRunExperience()` will return YES as long as the
   // user has not completed a first run experience.
   return [self
       initWithFirstRunExperience:ShouldPresentFirstRunExperience()
                lastSeedFetchTime:GetLastVariationsSeedFetchTime()
                          fetcher:[[IOSChromeVariationsSeedFetcher alloc] init]
-              enabledGroupWeight:groupWeight
-              controlGroupWeight:groupWeight];
+              enabledGroupWeight:100
+              controlGroupWeight:0];
 }
 
 - (instancetype)initWithFirstRunExperience:(BOOL)shouldPresentFRE
