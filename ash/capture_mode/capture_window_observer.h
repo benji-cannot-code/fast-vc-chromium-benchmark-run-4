d08706e1f5634a8a4d4c5dcd6f2c08ab988eb033
Elijah Hewer <hewer@chromium.org>
2023-08-24 03:19:31 +0000

    GD: Stop session when active desk changes
    Stop the GD capture session when the selected window loses visibility
    due to the active desk changing.
    
    Bug: b/286556422
    Test: manual + unit
    Change-Id: I1f04596e154729c2c9ea6fc93252ed59a660272d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800430
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Elijah Hewer <hewer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187643}

--- ash/capture_mode/capture_window_observer.h
+++ ash/capture_mode/capture_window_observer.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_CAPTURE_MODE_CAPTURE_WINDOW_OBSERVER_H_
 
 #include "ash/ash_export.h"
+#include "ash/wm/desks/desks_controller.h"
 #include "base/memory/raw_ptr.h"
 #include "ui/aura/window_observer.h"
 #include "ui/gfx/geometry/point.h"
@@ -23,7 +24,8 @@ class CaptureModeSession;
 
 // Class to observe the current selected to-be-captured window.
 class ASH_EXPORT CaptureWindowObserver : public aura::WindowObserver,
-                                         public ::wm::ActivationChangeObserver {
+                                         public ::wm::ActivationChangeObserver,
+                                         public DesksController::Observer {
  public:
   explicit CaptureWindowObserver(CaptureModeSession* capture_mode_session);
   CaptureWindowObserver(const CaptureWindowObserver&) = delete;
@@ -61,6 +63,10 @@ class ASH_EXPORT CaptureWindowObserver : public aura::WindowObserver,
   void OnWindowActivated(ActivationReason reason,
                          aura::Window* gained_active,
                          aura::Window* lost_active) override;
+
+  // DesksController::Observer:
+  void OnDeskActivationChanged(const Desk* activated,
+                               const Desk* deactivated) override;
 
  private:
   void StartObserving(aura::Window* window);
