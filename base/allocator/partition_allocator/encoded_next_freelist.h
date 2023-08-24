d3949d6fb49e529c6fb823a327d27a0458259bed
Kalvin Lee <kdlee@chromium.org>
2023-08-24 10:33:48 +0000

    PA: Emphasize _next_ in "encoded" freelist
    Freelist entries are only "encoded" for the purpose of chaining together
    the linked list. "Encoded next" is a bit more precise than just
    "encoded" by itself. This CL trivially renames `encoded_freelist.h` to
    reflect this.
    
    Bug: 1461983
    Change-Id: I17c96ac0c8eeb45c935fe228c7e1dafa717e5f7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4809917
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1187716}

--- base/allocator/partition_allocator/encoded_next_freelist.h
+++ base/allocator/partition_allocator/encoded_next_freelist.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef BASE_ALLOCATOR_PARTITION_ALLOCATOR_ENCODED_FREELIST_H_
-#define BASE_ALLOCATOR_PARTITION_ALLOCATOR_ENCODED_FREELIST_H_
+#ifndef BASE_ALLOCATOR_PARTITION_ALLOCATOR_ENCODED_NEXT_FREELIST_H_
+#define BASE_ALLOCATOR_PARTITION_ALLOCATOR_ENCODED_NEXT_FREELIST_H_
 
 #include <cstddef>
 #include <cstdint>
@@ -319,4 +319,4 @@ PA_ALWAYS_INLINE PartitionFreelistEntry* PartitionFreelistEntry::GetNext(
 
 }  // namespace partition_alloc::internal
 
-#endif  // BASE_ALLOCATOR_PARTITION_ALLOCATOR_ENCODED_FREELIST_H_
+#endif  // BASE_ALLOCATOR_PARTITION_ALLOCATOR_ENCODED_NEXT_FREELIST_H_
