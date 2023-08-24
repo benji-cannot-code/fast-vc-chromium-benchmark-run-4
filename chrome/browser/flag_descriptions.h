d1425ccc3ab87bd1df4f675bab371c33c9a3ddc7
Adem Derinel <derinel@google.com>
2023-08-24 08:23:56 +0000

    [WebAuthn][Android] Add feature flag for CredMan alongside GMSCore
    Existing CredMan flag (kWebAuthnAndroidCredMan) will be kept around
    for some time.
    
    Bug: 1474805
    Change-Id: Ib90fc1f156ce60fa6f0c59f53071c8d14293f9a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798458
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1187700}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2596,6 +2596,9 @@ extern const char kVideoTutorialsDescription[];
 extern const char kWebAuthnAndroidCredManName[];
 extern const char kWebAuthnAndroidCredManDescription[];
 
+extern const char kWebAuthnAndroidCredManAndGmsCoreName[];
+extern const char kWebAuthnAndroidCredManAndGmsCoreDescription[];
+
 extern const char kWebApkInstallFailureNotificationName[];
 extern const char kWebApkInstallFailureNotificationDescription[];
 
