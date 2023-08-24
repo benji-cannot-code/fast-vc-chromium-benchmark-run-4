52920b4c6ec77ec49226684c07da98ec33a3e2d3
rubberyuzu <yuzus@chromium.org>
2023-08-24 08:34:31 +0000

    [bfcache] Avoid eviction when registering CCNS
    crrev.com/c/4790988 introduced a crash bug where `rfh->IsInBackForwardCache()`
    can actually be true. This CL introduces a function to record a blocking
    feature but not call eviction.
    
    Bug: 1475520
    Change-Id: Iaa9f91077ce09da7b5ac94ad12786efb625cf3bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4807529
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187702}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -4820,9 +4820,14 @@ BackForwardCacheDisablingFeatureHandle::BackForwardCacheDisablingFeatureHandle(
   render_frame_host_->OnBackForwardCacheDisablingFeatureUsed(feature_);
 }
 
-void RenderFrameHostImpl::OnBackForwardCacheDisablingFeatureUsed(
+void RenderFrameHostImpl::RecordBackForwardCacheDisablingReason(
     BackForwardCacheDisablingFeature feature) {
   ++browser_reported_bfcache_disabling_features_counts_[feature];
+}
+
+void RenderFrameHostImpl::OnBackForwardCacheDisablingFeatureUsed(
+    BackForwardCacheDisablingFeature feature) {
+  RecordBackForwardCacheDisablingReason(feature);
 
   MaybeEvictFromBackForwardCache();
 }
