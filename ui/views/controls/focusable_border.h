5221364e877073960a022e5b43c0adea3c58dff3
Joseph Park <josephjoopark@chromium.org>
2023-08-24 14:12:09 +0000

    [CR2023] Fix Textfield border width to account for scale factor
    This change adds the option to paint FocusableBorder using dp in order
    to fix the above issue
    
    Bug: 1466607
    Change-Id: I817d7a1c9e9aa2a090edd1299d182cea3d4320ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4808278
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Joseph Park <josephjoopark@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187782}

--- ui/views/controls/focusable_border.h
+++ ui/views/controls/focusable_border.h
@@ -21,7 +21,7 @@ namespace views {
 // A Border class to draw a focused border around a field (e.g textfield).
 class VIEWS_EXPORT FocusableBorder : public Border {
  public:
-  FocusableBorder();
+  explicit FocusableBorder(bool should_scale = false);
 
   FocusableBorder(const FocusableBorder&) = delete;
   FocusableBorder& operator=(const FocusableBorder&) = delete;
@@ -50,6 +50,7 @@ class VIEWS_EXPORT FocusableBorder : public Border {
   gfx::Insets insets_;
   float corner_radius_;
   absl::optional<ui::ColorId> override_color_id_;
+  bool should_scale_;
 };
 
 }  // namespace views
