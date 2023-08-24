df4b85c7f7fe2835a382d0b30e3f6d3bf0e86454
Juan Garza Sanchez <juangarza@chromium.org>
2023-08-24 22:45:51 +0000

    usb: Support device events when service worker is inactive
    This change list completes ServiceWorkerUsbDelegateObserver and makes it
    responsible to keep track what ServiceWorkerRegistration should respond
    to USB events and wake up the service worker if it is not in running
    state.
    
    Bug: 1446487
    Change-Id: Ibadd754f302d6ab5eb42f7df0ecb46ba46226a7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4804134
    Auto-Submit: Juan Garza Sanchez <juangarza@chromium.org>
    Commit-Queue: Juan Garza Sanchez <juangarza@chromium.org>
    Reviewed-by: Jack Hsieh <chengweih@chromium.org>
    Commit-Queue: Jack Hsieh <chengweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188062}

--- content/browser/usb/usb_test_utils.cc
+++ content/browser/usb/usb_test_utils.cc
@@ -29,11 +29,17 @@ std::unique_ptr<UsbChooser> MockUsbDelegate::RunChooser(
 
 void MockUsbDelegate::AddObserver(BrowserContext* browser_context,
                                   Observer* observer) {
+  if (assert_browser_context_) {
+    ASSERT_TRUE(browser_context);
+  }
   observer_list_.AddObserver(observer);
 }
 
 void MockUsbDelegate::RemoveObserver(BrowserContext* browser_context,
                                      Observer* observer) {
+  if (assert_browser_context_) {
+    ASSERT_TRUE(browser_context);
+  }
   observer_list_.RemoveObserver(observer);
 }
 
@@ -58,6 +64,10 @@ void MockUsbDelegate::OnDeviceManagerConnectionError() {
   for (auto& observer : observer_list_) {
     observer.OnDeviceManagerConnectionError();
   }
+}
+
+void MockUsbDelegate::SetAssertBrowserContext(bool assert_browser_context) {
+  assert_browser_context_ = assert_browser_context;
 }
 
 }  // namespace content
