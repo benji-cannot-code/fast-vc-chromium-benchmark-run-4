43acee7997f85acf43551433949d43fbc9e3c09a
Quentin Pubert <qpubert@google.com>
2023-08-24 15:50:55 +0000

    [iOS] Add account picker sheet, configuration, layout delegate
    This CL is the first step to add the account picker UI for the Save to
    Photos feature. This code is essentially copy-pasted from
    `i/c/b/ui/**/consistency_promo_signin/consistency_sheet/`.
    
    Fixed: 1474414
    Change-Id: I0418d964c647e300506688d9048a9129beec339b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798155
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187854}

---
+++ ios/chrome/browser/ui/account_picker/account_picker_screen/account_picker_screen_constants.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_ACCOUNT_PICKER_ACCOUNT_PICKER_SCREEN_ACCOUNT_PICKER_SCREEN_CONSTANTS_H_
+#define IOS_CHROME_BROWSER_UI_ACCOUNT_PICKER_ACCOUNT_PICKER_SCREEN_ACCOUNT_PICKER_SCREEN_CONSTANTS_H_
+
+#import <UIKit/UIKit.h>
+
+// Maximum height ratio for the bottom sheet container view.
+extern const CGFloat kMaxPickAccountHeightRatioWithWindow;
+
+// AccountPickerScreenNavigationController accessibility identifier.
+extern NSString* const
+    kAccountPickerScreenNavigationControllerAccessibilityIdentifier;
+
+#endif  // IOS_CHROME_BROWSER_UI_ACCOUNT_PICKER_ACCOUNT_PICKER_SCREEN_ACCOUNT_PICKER_SCREEN_CONSTANTS_H_
