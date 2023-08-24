f667bbcfea322e07eced19e25f50bcef91e7af41
François Degros <fdegros@chromium.org>
2023-08-24 00:49:28 +0000

    BulkPinning: Redact some error logs
    BUG=b:297134745
    
    Change-Id: I9ceb503b7a233abc835de03e351b1261593b3ded
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4804946
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Auto-Submit: François Degros <fdegros@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187601}

--- chromeos/ash/components/drivefs/drivefs_pin_manager.cc
+++ chromeos/ash/components/drivefs/drivefs_pin_manager.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <iomanip>
 #include <locale>
 #include <sstream>
+#include <string_view>
 #include <type_traits>
 
 #include "base/files/file_path.h"
@@ -30,7 +31,6 @@ namespace {
 using ash::SpacedClient;
 using base::Seconds;
 using base::SequencedTaskRunner;
-using base::StringPiece;
 using base::TimeDelta;
 using base::UmaHistogramBoolean;
 using mojom::FileMetadata;
@@ -93,7 +93,7 @@ ostream& operator<<(ostream& out, Quoter<T> q) {
   // Does the string start with 'k'?
   if (!s.empty() && s.front() == 'k') {
     // Skip the 'k' prefix.
-    return out << StringPiece(s).substr(1);
+    return out << std::string_view(s).substr(1);
   }
 
   // No 'k' prefix. Print between parentheses.
@@ -125,7 +125,25 @@ ostream& operator<<(ostream& out, Quoter<TimeDelta> q) {
 }
 
 ostream& operator<<(ostream& out, Quoter<Path> q) {
-  return out << "'" << (*q.value) << "'";
+  const std::string& s = q.value->value();
+  if (VLOG_IS_ON(1)) {
+    return out << "'" << s << "'";
+  }
+
+  for (const std::string_view prefix :
+       {"/root", "/.files-by-id", "/.shortcuts-by-id"}) {
+    if (s.starts_with(prefix)) {
+      if (s.size() == prefix.size()) {
+        return out << "'" << prefix << "'";
+      }
+      DCHECK_GT(s.size(), prefix.size());
+      if (s[prefix.size()] == '/') {
+        return out << "'" << prefix << "/***'";
+      }
+    }
+  }
+
+  return out << "'***'";
 }
 
 ostream& operator<<(ostream& out, Quoter<std::string> q) {
