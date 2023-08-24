c1b6faa049dd82a35a1355f895ef1bd7cd6590fa
James Cook <jamescook@chromium.org>
2023-08-24 16:59:07 +0000

    crosier: Fix browser cleanup in DemoIntegrationTest
    It's not safe to mutate the BrowserList while iterating over the list.
    Copy the list into a vector before closing the browsers.
    
    Bug: none
    Test: chromeos_integration_tests --gtest_filter=DemoIntegrationTest.*
    Change-Id: I24e3b1e808237636a24cbbf82f6354b68e8778b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4809448
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187895}

--- chrome/test/base/chromeos/crosier/demo_integration_test.cc
+++ chrome/test/base/chromeos/crosier/demo_integration_test.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <vector>
+
 #include "base/run_loop.h"
 #include "base/test/gtest_tags.h"
 #include "base/test/test_switches.h"
@@ -95,7 +97,10 @@ class DemoIntegrationTest : public MixinBasedInProcessBrowserTest {
     // manager, but AttemptUserExit() uses session manager to kill the chrome
     // binary.
     // TODO(b/292067979): Find a better way to work around this issue.
-    for (Browser* browser : *BrowserList::GetInstance()) {
+    auto* browser_list = BrowserList::GetInstance();
+    // Copy the browser list to avoid mutating it during iteration.
+    std::vector<Browser*> browsers(browser_list->begin(), browser_list->end());
+    for (Browser* browser : browsers) {
       CloseBrowserSynchronously(browser);
     }
 
