be73dcd15fcab36bf32b0b78948de0d3ef181997
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-24 00:47:11 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: I38e831a1b4706d1ef1354244f1d6b7649aeb691c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4801352
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187600}

--- chrome/browser/sync/wifi_configuration_sync_service_factory.h
+++ chrome/browser/sync/wifi_configuration_sync_service_factory.h
@@ -40,7 +40,7 @@ class WifiConfigurationSyncServiceFactory : public ProfileKeyedServiceFactory {
   ~WifiConfigurationSyncServiceFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
   void RegisterProfilePrefs(
       user_prefs::PrefRegistrySyncable* registry) override;
