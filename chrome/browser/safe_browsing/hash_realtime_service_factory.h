a73fe52c9b7848b0fe90f62f6b72a4cf30c67319
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-24 00:42:07 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: I64b1c55f8d33110478fae7d15acc132ca169aa51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4801076
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Commit-Queue: Nathan Parker <nparker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187595}

--- chrome/browser/safe_browsing/hash_realtime_service_factory.h
+++ chrome/browser/safe_browsing/hash_realtime_service_factory.h
@@ -44,7 +44,7 @@ class HashRealTimeServiceFactory : public ProfileKeyedServiceFactory {
   ~HashRealTimeServiceFactory() override = default;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 
   static bool IsEnhancedProtectionEnabled(Profile* profile);
