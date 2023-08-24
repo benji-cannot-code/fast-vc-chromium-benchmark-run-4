6d0c227b526e38a5f073d0d4d24d6d73203821c8
Zhiyuan Cai <zhiyuancai@chromium.org>
2023-08-24 17:54:16 +0000

    [CodeHealth] Batch omaha tests
    Bug: 1474102
    Change-Id: I334e9288ef9a66257533d8c7c524110b62736af2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4799936
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Zhiyuan Cai <zhiyuancai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187932}

--- chrome/android/junit/src/org/chromium/chrome/browser/omaha/OmahaBaseTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/omaha/OmahaBaseTest.java
@@ -22,6 +22,7 @@ import org.chromium.base.FakeTimeTestRule;
 import org.chromium.base.FeatureList;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.omaha.MockRequestGenerator.DeviceType;
 
@@ -47,6 +48,7 @@ import java.util.List;
  * system, such as whether Chrome was installed through the system image.
  */
 @RunWith(BaseRobolectricTestRunner.class)
+@Batch(Batch.UNIT_TESTS)
 @Config(manifest = Config.NONE)
 public class OmahaBaseTest {
     private static class TimestampPair {
