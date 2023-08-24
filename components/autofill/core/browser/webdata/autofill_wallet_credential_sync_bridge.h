5eb538ba5a9c325fd20d3e7b1bfa2facf778d809
Vipul Vinod Koul <koulvipul@google.com>
2023-08-24 18:55:42 +0000

    [CVV Storage] Implement MergeFullSyncData and ApplyIncrementalSyncChanges for AutofillWalletCredentialSyncBridge.
    There are 3 major operations w.r.t. data sync added here-
    1. Add a Server CVC
    2. Delete a Server CVC
    3. Update an existing Server CVC
    
    DD- go/pay-autofill-cvv-storage-chrome
    
    Bug: 1458044
    Change-Id: I4f071904c2d072f6393d9396925211eca1019836
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4763964
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Alexander Tekle <alexandertekle@google.com>
    Commit-Queue: Vipul Koul <koulvipul@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187954}

--- components/autofill/core/browser/webdata/autofill_wallet_credential_sync_bridge.h
+++ components/autofill/core/browser/webdata/autofill_wallet_credential_sync_bridge.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/sequence_checker.h"
 #include "base/supports_user_data.h"
+#include "components/autofill/core/browser/webdata/autofill_table.h"
 #include "components/sync/model/metadata_change_list.h"
 #include "components/sync/model/model_error.h"
 #include "components/sync/model/model_type_change_processor.h"
@@ -72,7 +73,7 @@ class AutofillWalletCredentialSyncBridge : public base::SupportsUserData::Data,
   const raw_ptr<AutofillWebDataBackend> web_data_backend_;
 
   // Returns the table associated with the `web_data_backend_`.
-  AutofillTable* GetAutofillTable();
+  AutofillTable* GetAutofillTable() const;
 
   // Synchronously load sync metadata from the autofill table and pass it to the
   // processor so that it can start tracking changes.
