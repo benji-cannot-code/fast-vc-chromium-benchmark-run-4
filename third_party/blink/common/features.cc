9942267abafd3b8fd933a7068c203fc4f9f9fcf6
Chris Harrelson <chrishtr@chromium.org>
2023-08-24 23:40:11 +0000

    Enable kSerializeAccessibilityPostLifecycle by default
    Bug: 1342801
    Change-Id: If1077a251a16d9d1553cac861fe6be65da7cc67e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4811738
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188086}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1539,7 +1539,7 @@ BASE_FEATURE(kSendCnameAliasesToSubresourceFilterFromRenderer,
 
 BASE_FEATURE(kSerializeAccessibilityPostLifecycle,
              "SerializeAccessibilityPostLifecycle",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Experiment of the delay from navigation to starting an update of a service
 // worker's script.
