a1541edbe3a9114f862f13bd0a96f184fcb30052
Piotr Pawliczek <pawliczek@google.com>
2023-08-24 15:25:01 +0000

    Fix uninstallation of USB printers
    Currently, the method CupsPrintersManager::UninstallPrinter(...) is
    called only when a user removes saved printer or detaches USB printer.
    Unfortunately, it does not work correctly for USB printers, because the
    method GetPrinter(...) called inside always returns nullptr (for USB
    printers). This patch fixes this issue by introducting two changes:
    1. Move a call to the histogram Printing.CUPS.PrinterRemoved from
       OnRemovedPrinter(...) to RemoveSavedPrinter(...). Currently, the
       histogram is called only when the method UninstallPrinter(...) is
       called inside RemoveSavedPrinter(...), so this change allows to keep
       the histogram working the same way as before.
    2. Remove the condition from UninstallPrinter(...) blocking the call to
       CupsRemovePrinter(...). The call to GetPrinter(...) is not needed
       any more, since it was added there only to provide data for the
       histogram.
    I have tested this patch on atlas and confirmed that USB printers are
    removed from CUPS when disconnected from chromebook. In the previous
    versions, configured USB printers were left in CUPS and never removed.
    I have also confirmed that this bug occurred in the version 115.
    
    BUG=none
    TEST=tested on atlas
    
    Change-Id: Ia37779dcefdeb704f3d45894b560746445f9af7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4804421
    Auto-Submit: Piotr Pawliczek <pawliczek@chromium.org>
    Commit-Queue: Benjamin Gordon <bmgordon@chromium.org>
    Reviewed-by: Benjamin Gordon <bmgordon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187836}

--- chrome/browser/ash/printing/cups_printers_manager.cc
+++ chrome/browser/ash/printing/cups_printers_manager.cc
@@ -83,15 +83,12 @@ using ::chromeos::Printer;
 using ::chromeos::PrinterClass;
 using ::printing::PrinterQueryResult;
 
-void OnRemovedPrinter(const Printer::PrinterProtocol& protocol, bool success) {
+void OnRemovedPrinter(bool success) {
   if (success) {
     PRINTER_LOG(DEBUG) << "Printer removal succeeded.";
   } else {
     PRINTER_LOG(DEBUG) << "Printer removal failed.";
   }
-
-  base::UmaHistogramEnumeration("Printing.CUPS.PrinterRemoved", protocol,
-                                Printer::PrinterProtocol::kProtocolMax);
 }
 
 class CupsPrintersManagerImpl
@@ -209,6 +206,9 @@ class CupsPrintersManagerImpl
     auto existing = synced_printers_manager_->GetPrinter(printer_id);
     if (existing) {
       event_tracker_->RecordPrinterRemoved(*existing);
+      const Printer::PrinterProtocol protocol = existing->GetProtocol();
+      base::UmaHistogramEnumeration("Printing.CUPS.PrinterRemoved", protocol,
+                                    Printer::PrinterProtocol::kProtocolMax);
     }
     synced_printers_manager_->RemoveSavedPrinter(printer_id);
     // Note that we will rebuild our lists when we get the observer
@@ -368,13 +368,8 @@ class CupsPrintersManagerImpl
     // Uninstall printer if installed completely.
     if (installed_printer_fingerprints_.erase(printer_id)) {
       // The printer was present in `installed_printer_fingerprints_`.
-      absl::optional<chromeos::Printer> printer = GetPrinter(printer_id);
-      if (printer) {
-        const Printer::PrinterProtocol protocol = printer->GetProtocol();
-        DebugDaemonClient::Get()->CupsRemovePrinter(
-            printer_id, base::BindOnce(&OnRemovedPrinter, protocol),
-            base::DoNothing());
-      }
+      DebugDaemonClient::Get()->CupsRemovePrinter(
+          printer_id, base::BindOnce(&OnRemovedPrinter), base::DoNothing());
       return;
     }
 
