ad98899296eca43981252079855ba3d2ad2621e6
Tommy Steimel <steimel@chromium.org>
2023-08-24 14:50:12 +0000

    [ChromeRefresh2023] Update document pip windows for Chrome Refresh
    This CL makes a few different UI updates to document picture-in-picture
    windows for Chrome Refresh:
    
    1) Updates the color of the new pageinfo icon. The new icon has a white
    background that doesn't make sense with PiP always being in dark mode,
    so this adds some code to override into dark colors instead.
    
    2) Updates the back-to-tab and close button icons.
    
    3) Updates the margins on the pageinfo button to match new specs.
    
    4) Updates the pip foreground and background colors to match new specs.
    
    Bug: 1474072
    Change-Id: Ie627ff46b188831d94660290385a74c22334029a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4795250
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187811}

--- chrome/browser/ui/color/material_chrome_color_mixer.cc
+++ chrome/browser/ui/color/material_chrome_color_mixer.cc
@@ -210,6 +210,11 @@ void AddMaterialChromeColorMixer(ui::ColorProvider* provider,
   mixer[kColorOmniboxChipInkDropRipple] = {
       ui::kColorSysStateRippleNeutralOnSubtle};
 
+  // Picture-in-Picture window colors.
+  mixer[kColorPipWindowForeground] = {ui::kColorSysOnSurface};
+  mixer[kColorPipWindowForegroundInactive] = {ui::kColorSysOnSurfaceSubtle};
+  mixer[kColorPipWindowTopBarBackground] = {ui::kColorSysSurface};
+
   // Tabstrip tab alert colors.
   mixer[kColorTabAlertAudioPlayingActiveFrameActive] = {
       ui::kColorSysOnSurfaceSubtle};
