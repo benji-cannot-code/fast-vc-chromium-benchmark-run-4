c409bf07b4ed89a422c8bbd84c85e48218abcc2c
Alberto Juarez <albertojuarez@google.com>
2023-08-24 20:31:41 +0000

    Add support for config file callback in chrome
    Adding support for the callback to pass information to missive, not
    using it yet, will be implementing in a following CL, breaking
    them up to make it easier to review.
    
    BUG=b:289117140
    
    Change-Id: I934e3e9781f9f09140d7aae9d841b13e65d858e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4795425
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Alberto Juarez <albertojuarez@google.com>
    Reviewed-by: Hong Xu <xuhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1188005}

--- chrome/browser/ash/dbus/encrypted_reporting_service_provider.h
+++ chrome/browser/ash/dbus/encrypted_reporting_service_provider.h
@@ -67,6 +67,8 @@ class EncryptedReportingServiceProvider
   GetReportSuccessUploadCallback();
   static ::reporting::UploadClient::EncryptionKeyAttachedCallback
   GetEncryptionKeyAttachedCallback();
+  static ::reporting::UploadClient::UpdateConfigInMissiveCallback
+  GetUpdateConfigInMissiveCallback();
 
   // Returns true if called on the origin thread.
   bool OnOriginThread() const;
