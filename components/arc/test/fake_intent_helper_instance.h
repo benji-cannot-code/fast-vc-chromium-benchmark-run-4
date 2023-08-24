c76e18689ad330b02e34a1dd3b9cb5c929ad6daa
Nergi Rahardi <nergi@google.com>
2023-08-24 01:49:57 +0000

    Unify IPC to enable accessibility options into 1 call.
    Currently, even at startup, each accessibility feature is enabled
    separately, meanwhile this triggers the start/shutdown of
    AccessibilityHelperService in ARC. Due to the async nature of services,
    there can be race condition between start and shutdown, causing
    AccessibilityHelperService to not start even though a feature was
    enabled
    
    BUG=b:296330419
    TEST=ArcIntentHelperServiceTest
    TEST=tast run arc.SettingsBridge.vm
    
    Change-Id: I69eaa583169e4bf1a41b228916f89cf678135221
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4804987
    Reviewed-by: Hiroki Sato <hirokisato@chromium.org>
    Commit-Queue: Nergi Rahardi <nergi@google.com>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187615}

--- components/arc/test/fake_intent_helper_instance.h
+++ components/arc/test/fake_intent_helper_instance.h
@@ -66,6 +66,9 @@ class FakeIntentHelperInstance : public mojom::IntentHelperInstance {
   arc::mojom::CaptionStylePtr GetCaptionStyle() const {
     return caption_style_->Clone();
   }
+  arc::mojom::AccessibilityFeaturesPtr GetAccessibilityFeatures() const {
+    return accessibility_features_->Clone();
+  }
 
   // Sets a list of intent handlers to be returned in response to
   // RequestIntentHandlerList() calls with intents containing |action|.
@@ -130,6 +133,9 @@ class FakeIntentHelperInstance : public mojom::IntentHelperInstance {
 
   void SetCaptionStyle(arc::mojom::CaptionStylePtr caption_style) override;
 
+  void EnableAccessibilityFeatures(
+      arc::mojom::AccessibilityFeaturesPtr accessibility_features) override;
+
  private:
   std::vector<Broadcast> broadcasts_;
 
@@ -148,6 +154,8 @@ class FakeIntentHelperInstance : public mojom::IntentHelperInstance {
   mojo::Remote<mojom::IntentHelperHost> host_remote_;
 
   arc::mojom::CaptionStylePtr caption_style_;
+
+  arc::mojom::AccessibilityFeaturesPtr accessibility_features_;
 };
 
 }  // namespace arc
