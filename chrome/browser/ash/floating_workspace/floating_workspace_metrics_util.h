a8c6823b1601b751db4be848840177c53c69655c
zhumatthew <zhumatthew@google.com>
2023-08-24 00:06:43 +0000

    fws: Add additional metrics for success/failure
    Add additional metrics to determine rate of success. This is done
    by checking how many times we initialize fws v2 and how many times
    we record a fws launch + no template found. At the moment there is
    no way to determine if LaunchAppsFromTemplate has any failures
    that can lead to a blocking launch, so this is the best way to
    determine it at the moment.
    
    BUG=b:297251580
    TEST=FloatingWorkspaceServiceTest*
    
    Change-Id: I3dc0168124ec31b7b069168fb4b7ed5fac97f0a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4808514
    Reviewed-by: Yanzhu Du <yzd@google.com>
    Commit-Queue: Matthew Zhu <zhumatthew@google.com>
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187582}

--- chrome/browser/ash/floating_workspace/floating_workspace_metrics_util.h
+++ chrome/browser/ash/floating_workspace/floating_workspace_metrics_util.h
@@ -56,6 +56,10 @@ constexpr char kFloatingWorkspaceV2TemplateSize[] =
     "Ash.FloatingWorkspace.TemplateSize";
 constexpr char kFloatingWorkspaceV2TemplateUploadStatus[] =
     "Ash.FloatingWorkspace.TemplateUploadStatus";
+constexpr char kFloatingWorkspaceV2Initialized[] =
+    "Ash.FloatingWorkspace.FloatingWorkspaceV2Initialized";
+constexpr char kFloatingWorkspaceV2TemplateNotFound[] =
+    "Ash.FloatingWorkspace.TemplateNotFound";
 
 void RecordFloatingWorkspaceV1InitializedHistogram();
 void RecordFloatingWorkspaceV1RestoredSessionType(
@@ -68,6 +72,8 @@ void RecordFloatingWorkspaceV2TemplateLoadTime(base::TimeDelta duration);
 void RecordFloatingWorkspaceV2TemplateSize(size_t file_size);
 void RecordFloatingWorkspaceV2TemplateUploadStatusHistogram(
     desks_storage::DeskModel::AddOrUpdateEntryStatus status);
+void RecordFloatingWorkspaceV2InitializedHistogram();
+void RecordFloatingWorkspaceV2TemplateNotFound();
 
 }  // namespace ash::floating_workspace_metrics_util
 
