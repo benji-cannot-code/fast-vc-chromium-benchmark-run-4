da12d0c82d8b8cd3289a9bbb24770942563faf49
Guido Urdaneta <guidou@chromium.org>
2023-08-25 20:13:53 +0000

    [MediaStream] Enable EnumerateDevicesHideDeviceIDs on Android
    Bug: 1307642
    Change-Id: I5fd0a07d2e9c3dc95a85a175372692cd1d519deb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4813808
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188486}

--- content/browser/renderer_host/media/media_devices_dispatcher_host_unittest.cc
+++ content/browser/renderer_host/media/media_devices_dispatcher_host_unittest.cc
@@ -352,12 +352,8 @@ class MediaDevicesDispatcherHostTest
                        .empty());
 
     EXPECT_FALSE(DoesContainRawIds(enumerated_devices_));
-#if BUILDFLAG(IS_ANDROID)
-    EXPECT_TRUE(DoesEveryDeviceMapToRawId(enumerated_devices_));
-#else
     EXPECT_EQ(DoesEveryDeviceMapToRawId(enumerated_devices_),
               permission_override_value);
-#endif
   }
 
   bool DoesContainRawIds(
