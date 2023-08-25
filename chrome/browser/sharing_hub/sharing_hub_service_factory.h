af7d0c8fc4d584b4be93c00d4e9b9a8c44e0ea4d
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-25 08:17:39 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: I367aa1527ec2bc992fa2c884c84f222609ca8be9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800725
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1188223}

--- chrome/browser/sharing_hub/sharing_hub_service_factory.h
+++ chrome/browser/sharing_hub/sharing_hub_service_factory.h
@@ -30,7 +30,7 @@ class SharingHubServiceFactory : public ProfileKeyedServiceFactory {
   ~SharingHubServiceFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
