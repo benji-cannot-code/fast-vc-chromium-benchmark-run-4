1ce45ac3cd7c3c218e3ed68f3c87a5932050cd78
Joey Arhar <jarhar@chromium.org>
2023-08-25 01:54:28 +0000

    Revert "Make HasPopoverAttribute check attribute value"
    This reverts commit 9e0d46b6aa95ffb4d86f46c06ff0c4d6266f252a.
    
    Reason for revert: https://bugs.chromium.org/p/chromium/issues/detail?id=1473539
    
    Fixed: 1473539
    
    Original change's description:
    > Make HasPopoverAttribute check attribute value
    >
    > I've been getting feedback about how we don't perfectly match the HTML
    > spec on some popover things, and one of them was how instead of looking
    > at the presence of the popover attribute we look at whether our lazily
    > initialized PopoverData is present or not.
    >
    > This patch adds a CHECK to HasPopoverAttribute to make sure that the
    > lazily initialized value matches the presence of PopoverData in order to
    > make sure that we are actually matching the spec.
    >
    > Change-Id: I9bc85b11d0981dda5df84cb12149813c4a367c03
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4691243
    > Commit-Queue: Joey Arhar <jarhar@chromium.org>
    > Reviewed-by: Mason Freed <masonf@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1172471}
    
    Change-Id: I8783cd17ecd398e54ad292954bcd539fa8817a27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4811149
    Auto-Submit: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188135}

--- third_party/blink/renderer/core/html/html_element.cc
+++ third_party/blink/renderer/core/html/html_element.cc
@@ -1223,9 +1223,7 @@ void HTMLElement::UpdatePopoverAttribute(const AtomicString& value) {
         "Found a 'popover' attribute with an invalid value."));
     UseCounter::Count(GetDocument(), WebFeature::kPopoverTypeInvalid);
   }
-  // The attribute might not be set here, so check GetPopoverData instead of
-  // HasPopoverAttribute.
-  if (GetPopoverData()) {
+  if (HasPopoverAttribute()) {
     if (PopoverType() == type)
       return;
     String original_type = FastGetAttribute(html_names::kPopoverAttr);
@@ -1244,9 +1242,7 @@ void HTMLElement::UpdatePopoverAttribute(const AtomicString& value) {
     }
   }
   if (type == PopoverValueType::kNone) {
-    // The attribute might not be set here, so check GetPopoverData instead of
-    // HasPopoverAttribute.
-    if (GetPopoverData()) {
+    if (HasPopoverAttribute()) {
       // If the popover attribute is being removed, remove the PopoverData.
       RemovePopoverData();
     }
@@ -1272,7 +1268,6 @@ void HTMLElement::UpdatePopoverAttribute(const AtomicString& value) {
 }
 
 bool HTMLElement::HasPopoverAttribute() const {
-  CHECK_EQ(FastHasAttribute(html_names::kPopoverAttr), !!GetPopoverData());
   return GetPopoverData();
 }
 
@@ -1327,14 +1322,12 @@ bool HTMLElement::IsPopoverReady(PopoverTriggerAction action,
     }
   };
 
-  if (!FastHasAttribute(html_names::kPopoverAttr)) {
+  if (!HasPopoverAttribute()) {
     maybe_throw_exception(DOMExceptionCode::kNotSupportedError,
                           "Not supported on elements that do not have a valid "
                           "value for the 'popover' attribute.");
     return false;
   }
-  CHECK(GetPopoverData());
-
   if (action == PopoverTriggerAction::kShow &&
       GetPopoverData()->visibilityState() != PopoverVisibilityState::kHidden) {
     if (!RuntimeEnabledFeatures::PopoverDialogDontThrowEnabled()) {
