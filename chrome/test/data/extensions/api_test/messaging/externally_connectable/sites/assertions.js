b69ab0302015a9dd4b8631fe25c45837d2181ca3
Tim Judkins <tjudkins@chromium.org>
2023-08-25 23:42:44 +0000

    [Extensions] Fix and refactor externally connectable messaging apitest
    The checking of lastError was being done outside of the direct callback
    and was also being done inside a callback to postMessage, which doesn't
    actually take a callback at all.
    
    This CL refactors and corrects these parts of the test, in preparation
    for adding a new case in here.
    
    Bug: 1410022
    Change-Id: I9a0d2e55485721645f151d125eaac18c5f405473
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4811806
    Commit-Queue: Tim <tjudkins@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188596}

--- chrome/test/data/extensions/api_test/messaging/externally_connectable/sites/assertions.js
+++ chrome/test/data/extensions/api_test/messaging/externally_connectable/sites/assertions.js
@@ -121,11 +121,10 @@ if (parent == window) {
   });
 }
 
-function checkLastError() {
-  if (!chrome.runtime.lastError)
-    return;
-  if (chrome.runtime.lastError.message == kCouldNotEstablishConnection)
+function throwResultError(errorMessage) {
+  if (errorMessage == kCouldNotEstablishConnection) {
     throw new ResultError(results.COULD_NOT_ESTABLISH_CONNECTION_ERROR);
+  }
   throw new ResultError(results.OTHER_ERROR);
 }
 
@@ -221,47 +220,35 @@ window.assertions = {
     try {
       checkRuntime();
 
-      if (!message)
+      if (!message) {
         message = kMessage;
+      }
 
       async function canSendMessage() {
-        const response = await new Promise((resolve) => {
-          chrome.runtime.sendMessage(extensionId, message, function(response) {
-            resolve(response);
-          });
-        });
-        checkLastError();
+        const response = await new Promise((resolve, reject) => {
+          chrome.runtime.sendMessage(
+              extensionId, message, function(response) {
+                if (chrome.runtime.lastError) {
+                  reject(chrome.runtime.lastError.message);
+                }
+                resolve(response);
+              });
+        }).catch(throwResultError);
         checkResponse(response, message, isApp);
       }
 
       async function canConnectAndSendMessages() {
         var port = chrome.runtime.connect(extensionId);
-        return new Promise((resolve, reject) => {
-          port.postMessage(message, function() {
-            try {
-              checkLastError();
-            } catch(err) {
-              reject(err);
-            }
-          });
-          port.postMessage(message, function() {
-            try {
-              checkLastError();
-            } catch(err) {
-              reject(err);
-            }
-          });
+        return new Promise((resolve) => {
+          port.postMessage(message);
+          port.postMessage(message);
           var pendingResponses = 2;
-          port.onMessage.addListener(async function(response) {
+          port.onMessage.addListener(function(response) {
             pendingResponses--;
-            try {
-              checkLastError();
-              checkResponse(response, message, isApp);
-            } catch (err) {
-              return reject(err);
-            }
-            if (pendingResponses == 0)
+            checkResponse(response, message, isApp);
+            if (pendingResponses == 0) {
               return resolve(results.OK);
+            }
           });
         });
       }
@@ -288,7 +275,7 @@ window.assertions = {
     function runIllegalFunction(fun) {
       try {
         fun();
-      } catch(e) {
+      } catch (e) {
         return true;
       }
       console.error('Function did not throw exception: ' + fun);
