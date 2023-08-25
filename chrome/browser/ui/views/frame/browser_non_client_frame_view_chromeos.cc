ca0a80ad19e253a59e69635320127a15813c9f13
Erik Chen <erikchen@chromium.org>
2023-08-25 00:55:33 +0000

    Fix alignment for IDR_THEME_TOOLBAR and IDR_THEME_FRAME.
    This CL makes two changes:
    
    (1) It standardizes the logic from different views for painting the
    toolbar theme, tab theme, and frame theme. This ensures that the themes
    are painted consistently. The new logic is x-platform. Although it must
    be called in several platform-specific classes, the base calculations
    can now be shared.
    
    (2) It aligns the origin point for the toolbar theme, tab theme, and
    frame theme. This fixes some themes (which relies on this behavior) and
    breaks some themes (which rely on the behavior that the toolbar theme is
    aligned with the top painted pixel of the active tab). I made this
    choice for two reasons:
    
    (2a) This matches the pre-existing code level documentation. (2b) The
    themes that this breaks are generally relying on implementation details
    of chrome. For example, some themes rely on the shape of the tabs: flat
    upper side. Other themes rely on the exact DIP height of the current tab
    strip.
    
    Note that most themes are not affected by (2) since most themes use a
    repeating pixel for IDR_THEME_TOOLBAR.
    
    Theme with a gradient. Before CL on left, after CL on right:
    https://screenshot.googleplex.com/9yb84TXEK8YsdJH
    
    Theme with an image. Before CL on left, after CL on right:
    https://screenshot.googleplex.com/LViTJPTYLSfqhZe
    
    Change-Id: I9754769c078cbb606f15a1adfe844a634ce67a12
    Bug: 1473538
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4809121
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188116}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_chromeos.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_chromeos.cc
@@ -601,7 +601,7 @@ gfx::ImageSkia BrowserNonClientFrameViewChromeOS::GetFrameHeaderImage(
 }
 
 int BrowserNonClientFrameViewChromeOS::GetFrameHeaderImageYInset() {
-  return ThemeProperties::kFrameHeightAboveTabs - GetTopInset(false);
+  return browser_view()->GetThemeOffsetFromBrowserView().y();
 }
 
 gfx::ImageSkia BrowserNonClientFrameViewChromeOS::GetFrameHeaderOverlayImage(
--- ui/views/window/frame_background.h
+++ ui/views/window/frame_background.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "third_party/skia/include/core/SkColor.h"
+#include "ui/gfx/geometry/point.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/views/views_export.h"
 
@@ -50,8 +51,11 @@ class VIEWS_EXPORT FrameBackground {
   // Memory is owned by the caller.
   void set_theme_image(const gfx::ImageSkia& image) { theme_image_ = image; }
 
-  // Sets an inset into the theme image to begin painting at.
-  void set_theme_image_y_inset(int y_inset) { theme_image_y_inset_ = y_inset; }
+  // Sets an inset into the theme image to begin painting at. This must be
+  // further modified by the origin of the frame.
+  void set_theme_image_inset(gfx::Point theme_image_inset) {
+    theme_image_inset_ = theme_image_inset;
+  }
 
   // Sets an image that overlays the top window image.  Usually used to add
   // edge highlighting to provide the illusion of depth.  May be null (empty).
@@ -109,7 +113,7 @@ class VIEWS_EXPORT FrameBackground {
   bool use_custom_frame_ = true;
   bool is_active_ = true;
   gfx::ImageSkia theme_image_;
-  int theme_image_y_inset_ = 0;
+  gfx::Point theme_image_inset_;
   gfx::ImageSkia theme_overlay_image_;
   int top_area_height_ = 0;
 
