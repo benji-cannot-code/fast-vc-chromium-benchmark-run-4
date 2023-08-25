d98d5b42beac58ae4b061b168429ed95c7f7653a
thefrog <thefrog@chromium.org>
2023-08-25 13:32:31 +0000

    Adjust Desktop privacy guide for hash-prefix real-time lookups
    Changes:
     - When the friendlier settings feature is enabled, modifies the
       sublabel of Safe Browsing standard protection.
     - When the friendlier settings features is disabled, modifies the
       some additional details in the Safe Browsing standard protection
       description (FeatureDescription2 + PrivacyDescription1).
    
    Note that the "off" branch for hash-prefix real-time lookups will be
    long-lived, so this does not follow the general strategy of adding the
    disabled case only in the "FlagsDisabled" suite.
    
    Test changes:
     - `label` is added to PrivacyGuideDescriptionItemElement so that tests
       can read it.
     - New child test suites added to the SafeBrowsingFragment suite:
         1) HashPrefixRealTimeDisabled
         2) HashPrefixRealTimeEnabled
         3) HashPrefixRealTimeEnabled_FriendlierSettingsDisabled
     - The test suite SafeBrowsingFragment_FlagsDisabled is renamed to
       FlagsDisabled since it is already a child suite of the suite
       SafeBrowsingFragment.
     - The test UpdatedStandardProtectionPrivacyGuide is renamed to
       StandardProtectionDescription and is moved within the child suite
       HashPrefixRealTimeDisabled.
     - The HashPrefixRealTimeDisabled and HashPrefixRealTimeEnabled suites
       are expected to stick around long-term, whereas the FlagsDisabled and
       HashPrefixRealTimeEnabled_FriendlierSettingsDisabled suites contain
       only temporary test cases.
    
    Bug: 1469956
    Change-Id: I1a754710fb27b1defc4a68a8ad25248a1dcd0d85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4802965
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188308}

--- chrome/browser/resources/settings/privacy_page/privacy_guide/privacy_guide_description_item.ts
+++ chrome/browser/resources/settings/privacy_page/privacy_guide/privacy_guide_description_item.ts
@@ -45,6 +45,8 @@ export class PrivacyGuideDescriptionItemElement extends PolymerElement {
     };
   }
 
+  label: string;
+
   private sanitizeInnerHtml_(rawString: string): TrustedHTML {
     return sanitizeInnerHtml(rawString);
   }
--- chrome/browser/resources/settings/privacy_page/privacy_guide/privacy_guide_safe_browsing_fragment.ts
+++ chrome/browser/resources/settings/privacy_page/privacy_guide/privacy_guide_safe_browsing_fragment.ts
@@ -63,6 +63,13 @@ export class PrivacyGuideSafeBrowsingFragmentElement extends
               'enableFriendlierSafeBrowsingSettings');
         },
       },
+
+      enableHashPrefixRealTimeLookups_: {
+        type: Boolean,
+        value() {
+          return loadTimeData.getBoolean('enableHashPrefixRealTimeLookups');
+        },
+      },
     };
   }
 
@@ -70,6 +77,7 @@ export class PrivacyGuideSafeBrowsingFragmentElement extends
       MetricsBrowserProxyImpl.getInstance();
   private startStateEnhanced_: boolean;
   private enableFriendlierSafeBrowsingSettings_: boolean;
+  private enableHashPrefixRealTimeLookups_: boolean;
 
   override ready() {
     super.ready();
@@ -143,8 +151,24 @@ export class PrivacyGuideSafeBrowsingFragmentElement extends
   private getSafeBrowsingStandardSubLabel_(): string {
     return this.i18n(
         this.enableFriendlierSafeBrowsingSettings_ ?
+            this.enableHashPrefixRealTimeLookups_ ?
+            'safeBrowsingStandardDescUpdatedProxy' :
             'safeBrowsingStandardDescUpdated' :
             'safeBrowsingStandardDesc');
+  }
+
+  private getStandardProtectionFeatureDescription2_(): string {
+    return this.i18n(
+        this.enableHashPrefixRealTimeLookups_ ?
+            'privacyGuideSafeBrowsingCardStandardProtectionFeatureDescription2Proxy' :
+            'privacyGuideSafeBrowsingCardStandardProtectionFeatureDescription2');
+  }
+
+  private getStandardProtectionPrivacyDescription1_(): string {
+    return this.i18n(
+        this.enableHashPrefixRealTimeLookups_ ?
+            'privacyGuideSafeBrowsingCardStandardProtectionPrivacyDescription1Proxy' :
+            'privacyGuideSafeBrowsingCardStandardProtectionPrivacyDescription1');
   }
 }
 
