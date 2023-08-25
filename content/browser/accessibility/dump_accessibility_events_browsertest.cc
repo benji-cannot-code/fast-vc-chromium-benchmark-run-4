d1ae926bc7e9b0473c4ef251d03d074b3daa4ef6
Luis Juan Sanchez Padilla <lusanpad@microsoft.com>
2023-08-25 16:20:46 +0000

    Add AX Selection Event when an aria selected node is added to the tree.
    Currently, when a node whose aria-selected attribute is set to true is
    added to the tree, no selection event is fired as AXEventGenerator did
    not have a previous state to compare it to. This causes problems with
    ATs as they are not notified of this already existing selection. This
    change fixes this issue by firing a selection event if the aria-selected
    property is set to true when a new node is added to the tree.
    
    Change-Id: I5ad584c07803c6c219c31f0c015deed4326453af
    AX-Relnotes: n/a.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4791732
    Commit-Queue: Luis Sanchez Padilla <lusanpad@microsoft.com>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188375}

--- content/browser/accessibility/dump_accessibility_events_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_events_browsertest.cc
@@ -1170,6 +1170,11 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityEventsTest,
 }
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityEventsTest,
+                       AccessibilityEventsAriaSelectedChangedNewSubtree) {
+  RunEventTest(FILE_PATH_LITERAL("aria-selected-changed-new-subtree.html"));
+}
+
+IN_PROC_BROWSER_TEST_P(DumpAccessibilityEventsTest,
                        AccessibilityEventsButtonClick) {
   RunEventTest(FILE_PATH_LITERAL("button-click.html"));
 }
