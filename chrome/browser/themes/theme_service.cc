f80c4c4125721b9a75376163f6122f3291b530bf
Thomas Lukaszewicz <tluk@chromium.org>
2023-08-25 23:44:45 +0000

    [views] Ensure browser theme change notifications propagate to children
    Currently browser theme changes are only propagated to the BrowserFrame
    root Widget. However currently other owned Widgets in the frame's
    hierarchy inherit theme information but will not receive a notification
    that this theme information has changed.
    
    This CL ensures that owned Widgets receive ThemeChange() notifications
    when the browser theme has changed.
    
    Note: this is not an issue for NativeTheme changes as owned Widgets
    maintain their own NativeTheme observations.
    
    This CL also eliminates redundant code in status_bubble_views that
    existed to propagate these browser theme change notifications to
    the status bubble (which is now covered by the more general change
    in this CL).
    
    Bug: 1472289
    Change-Id: I6bfb30cafc4c24510b2c382eef7cafe2fb615677
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4775743
    Auto-Submit: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188597}

--- chrome/browser/themes/theme_service.cc
+++ chrome/browser/themes/theme_service.cc
@@ -553,8 +553,11 @@ SkColor ThemeService::GetPolicyThemeColor() const {
 
 void ThemeService::SetBrowserColorScheme(
     ThemeService::BrowserColorScheme color_scheme) {
-  profile_->GetPrefs()->SetInteger(prefs::kBrowserColorScheme,
-                                   static_cast<int>(color_scheme));
+  {
+    base::AutoReset<bool> resetter(&should_suppress_theme_updates_, true);
+    profile_->GetPrefs()->SetInteger(prefs::kBrowserColorScheme,
+                                     static_cast<int>(color_scheme));
+  }
   NotifyThemeChanged();
 }
 
@@ -586,8 +589,11 @@ absl::optional<SkColor> ThemeService::GetUserColor() const {
 
 void ThemeService::SetBrowserColorVariant(
     ui::mojom::BrowserColorVariant color_variant) {
-  profile_->GetPrefs()->SetInteger(prefs::kBrowserColorVariant,
-                                   static_cast<int>(color_variant));
+  {
+    base::AutoReset<bool> resetter(&should_suppress_theme_updates_, true);
+    profile_->GetPrefs()->SetInteger(prefs::kBrowserColorVariant,
+                                     static_cast<int>(color_variant));
+  }
   NotifyThemeChanged();
 }
 
