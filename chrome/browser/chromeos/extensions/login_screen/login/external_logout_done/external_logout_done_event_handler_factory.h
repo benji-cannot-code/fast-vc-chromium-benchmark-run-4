b48bbfaf77a706aa3b656a18c5ee7bef4906e5e9
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-25 13:38:17 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: Iabd8cdd81280c7ce0e45cbb97ef667094f9e0e8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800114
    Reviewed-by: Maria Petrisor <mpetrisor@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Maria Petrisor <mpetrisor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188310}

--- chrome/browser/chromeos/extensions/login_screen/login/external_logout_done/external_logout_done_event_handler_factory.h
+++ chrome/browser/chromeos/extensions/login_screen/login/external_logout_done/external_logout_done_event_handler_factory.h
@@ -34,7 +34,7 @@ class ExternalLogoutDoneEventHandlerFactory
   ~ExternalLogoutDoneEventHandlerFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* browser_context) const override;
   bool ServiceIsNULLWhileTesting() const override;
   bool ServiceIsCreatedWithBrowserContext() const override;
