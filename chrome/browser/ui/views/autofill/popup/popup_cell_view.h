b7caed1f6847a46be3333956906cf1a97140215f
Jan Keitel <jkeitel@google.com>
2023-08-25 09:00:53 +0000

    Move accept handling from PopupRow to PopupCell.
    Ideally, each element (the view, the row, the cell) should handle as
    many of its own events as it can. This CL takes care of a TODO to
    move the return key handling to the cell itself.
    
    Bug: 1411172
    Change-Id: I8809810babc7c7cee20a2d0b496af79c8309edf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4813076
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1188239}

--- chrome/browser/ui/views/autofill/popup/popup_cell_view.h
+++ chrome/browser/ui/views/autofill/popup/popup_cell_view.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "components/autofill/core/common/aliases.h"
+#include "content/public/common/input/native_web_keyboard_event.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/accessibility/ax_action_data.h"
 #include "ui/base/metadata/metadata_header_macros.h"
