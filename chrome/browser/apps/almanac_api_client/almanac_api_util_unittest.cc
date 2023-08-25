f706fb6c9b6a9d8b9b481cfd2c2ca31048af6d02
Elitsa Bankova <elitsa@google.com>
2023-08-25 01:40:17 +0000

    Almanac: reland the common utils correctly
    The relanded version was somehow mixed up. I've reverted to the correct
    version and kept the test fix
    
    Bug: b/296157719
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_chromeos_asan_rel_ng
    Change-Id: I833b81cafc9c1edd5c6e6e97799ab334cd281764
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4807453
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Elitsa Bankova <elitsa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1188130}

--- chrome/browser/apps/almanac_api_client/almanac_api_util_unittest.cc
+++ chrome/browser/apps/almanac_api_client/almanac_api_util_unittest.cc
@@ -32,10 +32,11 @@ TEST_F(AlmanacApiUtilTest, NoDownloadError) {
   response_info->headers =
       base::MakeRefCounted<net::HttpResponseHeaders>("HTTP/1.1 200 OK");
   std::string response_body;
-  EXPECT_FALSE(HasDownloadError(net::OK, response_info.get(), &response_body,
-                                "endpoint", "histogram"));
-  EXPECT_FALSE(HasDownloadError(net::OK, nullptr, &response_body, "endpoint",
-                                absl::nullopt));
+  EXPECT_TRUE(GetDownloadError(net::OK, response_info.get(), &response_body,
+                               "histogram")
+                  .ok());
+  EXPECT_TRUE(
+      GetDownloadError(net::OK, nullptr, &response_body, absl::nullopt).ok());
 }
 
 TEST_F(AlmanacApiUtilTest, NetDownloadError) {
@@ -43,11 +44,13 @@ TEST_F(AlmanacApiUtilTest, NetDownloadError) {
   response_info->headers =
       base::MakeRefCounted<net::HttpResponseHeaders>("HTTP/1.1 200 OK");
   std::string response_body;
-  EXPECT_TRUE(HasDownloadError(net::ERR_INSUFFICIENT_RESOURCES,
-                               response_info.get(), &response_body, "endpoint",
-                               "histogram"));
-  EXPECT_TRUE(HasDownloadError(net::ERR_CONNECTION_FAILED, nullptr,
-                               &response_body, "endpoint", absl::nullopt));
+  EXPECT_FALSE(GetDownloadError(net::ERR_INSUFFICIENT_RESOURCES,
+                                response_info.get(), &response_body,
+                                "histogram")
+                   .ok());
+  EXPECT_FALSE(GetDownloadError(net::ERR_CONNECTION_FAILED, nullptr,
+                                &response_body, absl::nullopt)
+                   .ok());
 }
 
 TEST_F(AlmanacApiUtilTest, ServerDownloadError) {
@@ -55,12 +58,14 @@ TEST_F(AlmanacApiUtilTest, ServerDownloadError) {
   response_info->headers =
       base::MakeRefCounted<net::HttpResponseHeaders>("HTTP/1.1 404");
   std::string response_body;
-  EXPECT_TRUE(HasDownloadError(net::OK, response_info.get(), &response_body,
-                               "endpoint", "histogram"));
+  EXPECT_FALSE(GetDownloadError(net::OK, response_info.get(), &response_body,
+                                "histogram")
+                   .ok());
   response_info->headers =
       base::MakeRefCounted<net::HttpResponseHeaders>("HTTP/1.1 502");
-  EXPECT_TRUE(HasDownloadError(net::OK, response_info.get(), &response_body,
-                               "endpoint", absl::nullopt));
+  EXPECT_FALSE(GetDownloadError(net::OK, response_info.get(), &response_body,
+                                absl::nullopt)
+                   .ok());
 }
 
 TEST_F(AlmanacApiUtilTest, NoRequestBodyDownloadError) {
@@ -68,10 +73,11 @@ TEST_F(AlmanacApiUtilTest, NoRequestBodyDownloadError) {
   response_info->headers =
       base::MakeRefCounted<net::HttpResponseHeaders>("HTTP/1.1 200 OK");
   std::string* response_body = nullptr;
-  EXPECT_TRUE(HasDownloadError(net::OK, response_info.get(), response_body,
-                               "endpoint", "histogram"));
-  EXPECT_TRUE(HasDownloadError(net::OK, nullptr, response_body, "endpoint",
-                               absl::nullopt));
+  EXPECT_FALSE(
+      GetDownloadError(net::OK, response_info.get(), response_body, "histogram")
+          .ok());
+  EXPECT_FALSE(
+      GetDownloadError(net::OK, nullptr, response_body, absl::nullopt).ok());
 }
 }  // namespace
 }  // namespace apps
