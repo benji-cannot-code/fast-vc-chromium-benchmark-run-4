cc69c7a2f3aa367f41e85a99e716d66a938f043d
Simon Hangl <simonha@google.com>
2023-08-25 14:59:47 +0000

    Add policy definition for OneDrive integration with enterprise clippy
    This CL adds a policy for one drive integration in managed user
    sessions. Admins can configure to
    * disallow integration
    * allow the user to set up integration
    * automatically set up integration.
    
    Bug: b/294983416
    Change-Id: Ibfb2ba430a9bbd38fa6641b344ed8f35c7588f96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4803894
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Simon Hangl <simonha@google.com>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188343}

---
+++ chrome/browser/chromeos/enterprise/cloud_storage/policy_utils.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/enterprise/cloud_storage/policy_utils.h"
+
+#include "chrome/common/pref_names.h"
+#include "components/prefs/pref_registry_simple.h"
+
+namespace {
+constexpr char MicrosoftOneDriveMountAllowed[] = "allowed";
+}  // namespace
+
+namespace chromeos::cloud_storage {
+
+void RegisterProfilePrefs(PrefRegistrySimple* registry) {
+  registry->RegisterStringPref(prefs::kMicrosoftOneDriveMount,
+                               MicrosoftOneDriveMountAllowed);
+}
+
+}  // namespace chromeos::cloud_storage
