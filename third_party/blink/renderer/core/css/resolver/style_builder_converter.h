cc6f340bf8f8c43fd415bc8f77f9deeeeafcd6f6
Xiaocheng Hu <xiaochengh@chromium.org>
2023-08-25 08:31:21 +0000

    [anchor-position] Allow multiple anchor names on the same element
    This patch implements the recent CSSWG resolution to change
    `anchor-name` property into a comma-separated list of names:
    
    https://github.com/w3c/csswg-drafts/issues/8837#issuecomment-1690145010
    
    Fixed: 1475812
    Change-Id: I5d8abb58fa8bfc9ee5dd72a833098ae8685496c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4811709
    Auto-Submit: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188229}

--- third_party/blink/renderer/core/css/resolver/style_builder_converter.h
+++ third_party/blink/renderer/core/css/resolver/style_builder_converter.h
@@ -222,6 +222,8 @@ class StyleBuilderConverter {
                                            const CSSValue&);
   static ScopedCSSName* ConvertAnchorDefault(StyleResolverState&,
                                              const CSSValue&);
+  static ScopedCSSNameList* ConvertAnchorName(StyleResolverState&,
+                                              const CSSValue&);
   static StyleInitialLetter ConvertInitialLetter(StyleResolverState&,
                                                  const CSSValue&);
   static StyleOffsetRotation ConvertOffsetRotate(StyleResolverState&,
