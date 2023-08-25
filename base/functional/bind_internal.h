a8ca7650e1c06e1d6b1f848cb9bbe970ae072911
Mark Brand <markbrand@google.com>
2023-08-25 10:11:42 +0000

    Remove unneeded include in bind_internal.h
    Change-Id: Id0938be51a5b1e33e263e4cfd5d8a6690ad529c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4778422
    Commit-Queue: Mark Brand <markbrand@google.com>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188265}

--- base/functional/bind_internal.h
+++ base/functional/bind_internal.h
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/unretained_traits.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/raw_ptr_asan_bound_arg_tracker.h"
-#include "base/memory/raw_ptr_asan_service.h"
 #include "base/memory/raw_ref.h"
 #include "base/memory/raw_scoped_refptr_mismatch_checker.h"
 #include "base/memory/weak_ptr.h"
