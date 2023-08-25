2bd36c741981f602b554710d83e68fd0abecb159
Luis Juan Sanchez Padilla <lusanpad@microsoft.com>
2023-08-25 16:07:41 +0000

    Enable WGC Capture for Eye Dropper
    Currently the Eye Dropper view is affected by the Accessibility Color
    Filters, causing the pixels inside the view to not match the ones behind
    it. This change adds the capability of using WGC instead of directx for
    screen capturing, which fixes this issue.
    
    Adding this feature behind a feature flag in order to be able to experiment with this change as WGC is not yet used by default for screen capture.
    
    Bug: 1475728
    Change-Id: I8314d192f222094138edf12ce0d687ffc894c305
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4811255
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Sanket Joshi <sajos@microsoft.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Luis Sanchez Padilla <lusanpad@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1188368}

--- content/public/browser/desktop_capture.cc
+++ content/public/browser/desktop_capture.cc
@@ -24,6 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/nix/xdg_util.h"
 #endif
 
+#if BUILDFLAG(IS_WIN)
+#include "base/win/windows_version.h"
+#endif
+
 namespace content::desktop_capture {
 
 webrtc::DesktopCaptureOptions CreateDesktopCaptureOptions() {
@@ -55,14 +59,20 @@ webrtc::DesktopCaptureOptions CreateDesktopCaptureOptions() {
   return options;
 }
 
-std::unique_ptr<webrtc::DesktopCapturer> CreateScreenCapturer() {
+std::unique_ptr<webrtc::DesktopCapturer> CreateScreenCapturer(
+    bool allow_wgc_screen_capturer) {
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
   return std::make_unique<DesktopCapturerLacros>(
       DesktopCapturerLacros::CaptureType::kScreen,
       webrtc::DesktopCaptureOptions());
 #else
-  return webrtc::DesktopCapturer::CreateScreenCapturer(
-      CreateDesktopCaptureOptions());
+  auto options = desktop_capture::CreateDesktopCaptureOptions();
+#if defined(RTC_ENABLE_WIN_WGC)
+  if (allow_wgc_screen_capturer) {
+    options.set_allow_wgc_screen_capturer(true);
+  }
+#endif  // defined(RTC_ENABLE_WIN_WGC)
+  return webrtc::DesktopCapturer::CreateScreenCapturer(options);
 #endif
 }
 
