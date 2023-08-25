40f584c8857af9ef40ad93f2c0a3e9cd328df655
Marc Treib <treib@chromium.org>
2023-08-25 07:07:37 +0000

    Disable kReplaceSyncPromosWithSignInPromos in some policy EG tests
    These recently-introduced tests don't support
    kReplaceSyncPromosWithSignInPromos yet, due to a signin confirmation
    dialog that shows up with that feature enabled which isn't handled by
    the signin helpers yet.
    
    Bug: 1462156
    Change-Id: I8549b9419d5bfd2bb9e16ef631141c5bb277d1d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4807632
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188205}

--- ios/chrome/browser/policy/policy_egtest.mm
+++ ios/chrome/browser/policy/policy_egtest.mm
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/policy/test_support/signature_provider.h"
 #import "components/safe_browsing/core/common/features.h"
 #import "components/strings/grit/components_strings.h"
+#import "components/sync/base/features.h"
 #import "ios/chrome/browser/ntp/features.h"
 #import "ios/chrome/browser/policy/cloud/user_policy_constants.h"
 #import "ios/chrome/browser/policy/policy_app_interface.h"
@@ -167,6 +168,16 @@ NSString* const kDomain2 = @"domain2.com";
       [self isRunningTest:@selector(testManagementPageManagedWithUserPolicy)]) {
     config.features_enabled.push_back(
         policy::kUserPolicyForSigninOrSyncConsentLevel);
+  }
+
+  if ([self isRunningTest:@selector
+            (testManagementPageManagedWithCBCMAndUserPolicyDifferentDomains)] ||
+      [self isRunningTest:@selector
+            (testManagementPageManagedWithCBCMAndUserPolicySameDomains)] ||
+      [self isRunningTest:@selector(testManagementPageManagedWithUserPolicy)] ||
+      [self isRunningTest:@selector(testPopupMenuItemWithUserPolicy)]) {
+    config.features_disabled.push_back(
+        syncer::kReplaceSyncPromosWithSignInPromos);
   }
 
   return config;
@@ -625,6 +636,8 @@ NSString* const kDomain2 = @"domain2.com";
                     _server->GetServiceURL().spec()}));
   config.features_enabled.push_back(
       policy::kUserPolicyForSigninOrSyncConsentLevel);
+  config.features_disabled.push_back(
+      syncer::kReplaceSyncPromosWithSignInPromos);
   [[AppLaunchManager sharedManager] ensureAppLaunchedWithConfiguration:config];
 
   // Set CBCM policies.
@@ -671,6 +684,8 @@ NSString* const kDomain2 = @"domain2.com";
                     _server->GetServiceURL().spec()}));
   config.features_enabled.push_back(
       policy::kUserPolicyForSigninOrSyncConsentLevel);
+  config.features_disabled.push_back(
+      syncer::kReplaceSyncPromosWithSignInPromos);
   [[AppLaunchManager sharedManager] ensureAppLaunchedWithConfiguration:config];
 
   // Set CBCM policies.
