d0e2396fcc10842589ddf8f874ae5999a252c9e8
Chuong Ho <hdchuong@google.com>
2023-08-25 12:49:29 +0000

    Expose CanBeTriggered and IsAllowedForUse in mediator
    This CL creates two public methods, `CanBeTriggered` and
    `IsAllowedForUse` for mediator. These two APIs will be
    queried from the other components later.
    
    `IsAllowedForUse` will be needed to decide if the feature toggle in the settings page should be shown.
    
    `CanBeTriggered`, along with `GetConsentStatus` will be queried from the other components.
    
    Bug: b/295440693
    Change-Id: Ib5668804a08e9b829a849b4f27e6d4b783f5d50d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4804945
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: Chuong Ho <hdchuong@google.com>
    Reviewed-by: Zac Partridge <zacpartridge@google.com>
    Cr-Commit-Position: refs/heads/main@{#1188301}

--- chrome/browser/ash/input_method/editor_mediator.h
+++ chrome/browser/ash/input_method/editor_mediator.h
@@ -54,6 +54,12 @@ class EditorMediator : public EditorInstanceImpl::Delegate,
   // EditorInstanceImpl::Delegate overrides
   void CommitEditorResult(std::string_view text) override;
 
+  // Checks if the feature should be visible.
+  bool IsAllowedForUse();
+
+  // Checks if the feature can be triggered.
+  bool CanBeTriggered();
+
   ConsentStatus GetConsentStatus();
 
   // ProfileObserver:
