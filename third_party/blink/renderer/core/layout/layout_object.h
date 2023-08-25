68a1a97013eecf2c505b41d138d5877a6ba180cb
Morten Stenshorne <mstensho@chromium.org>
2023-08-25 17:54:47 +0000

    [RemoveLegacy] Special legacy float and column-span paint code.
    Remove LayoutObject::IsFloatingWithNonContainingBlockParent(). This was
    special-code only needed by the legacy engine.
    
    Also remove IsLayoutNGContainingBlock(). Poor thing always returned
    true, and as of now all call sites were gone as well. :)
    
    Bug: 1229581
    Change-Id: Ie1bf9eec8a94f74361441a1cabe6508aeb46592d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4813077
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188418}

--- third_party/blink/renderer/core/layout/layout_object.h
+++ third_party/blink/renderer/core/layout/layout_object.h
@@ -1404,11 +1404,6 @@ class CORE_EXPORT LayoutObject : public GarbageCollected<LayoutObject>,
     return bitfields_.Floating();
   }
 
-  bool IsFloatingWithNonContainingBlockParent() const {
-    NOT_DESTROYED();
-    return IsFloating() && Parent() && !Parent()->IsLayoutBlockFlow();
-  }
-
   virtual bool IsInitialLetterBox() const {
     NOT_DESTROYED();
     return false;
