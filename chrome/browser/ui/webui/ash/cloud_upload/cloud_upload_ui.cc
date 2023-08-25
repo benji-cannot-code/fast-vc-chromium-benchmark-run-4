27e25a7b01fa818c1e0d4eb04b7489f950bc6072
Jérémie Boulic <jboulic@chromium.org>
2023-08-25 04:42:52 +0000

    [Cloud upload] Use shared method to override TrustedTypes CSP
    Replaces CSP overrides added in crrev.com/c/4728780.
    
    Bug: None
    Change-Id: I16db292a6abc3b5a14201fe046ea697af134e4f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4812316
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188168}

--- chrome/browser/ui/webui/ash/cloud_upload/cloud_upload_ui.cc
+++ chrome/browser/ui/webui/ash/cloud_upload/cloud_upload_ui.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/ash/cloud_upload/cloud_upload_ui.h"
 
+#include "ash/webui/common/trusted_types_util.h"
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
 #include "chrome/browser/chromeos/upload_office_to_cloud/upload_office_to_cloud.h"
@@ -110,19 +111,10 @@ CloudUploadUI::CloudUploadUI(content::WebUI* web_ui)
   webui::SetupWebUIDataSource(
       source, base::make_span(kCloudUploadResources, kCloudUploadResourcesSize),
       IDR_CLOUD_UPLOAD_MAIN_HTML);
-  // Required for lottie animations.
   source->OverrideContentSecurityPolicy(
       network::mojom::CSPDirectiveName::WorkerSrc,
       "worker-src blob: chrome://resources 'self';");
-  source->OverrideContentSecurityPolicy(
-      network::mojom::CSPDirectiveName::TrustedTypes,
-      "trusted-types static-types "
-      // Required by lottie.
-      "lottie-worker-script-loader webui-test-script "
-      // Required by lit-html.
-      "lit-html "
-      // Required by polymer.
-      "polymer-html-literal polymer-template-event-attribute-policy;");
+  ash::EnableTrustedTypesCSP(source);
 }
 
 CloudUploadUI::~CloudUploadUI() = default;
