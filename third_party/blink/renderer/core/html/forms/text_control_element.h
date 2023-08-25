a3a39d2df64507122a25ab034adff54ae92c3570
Jihad Hanna <jihadghanna@google.com>
2023-08-25 17:44:55 +0000

    Explicitly show/hide suggestions for text elements
    Currently, TextControlElements show and hide suggestions based on the
    value of the suggestion itself. If the suggested value is empty, it
    means that there's no suggested value to show and we should hence show
    the actual value of the field, otherwise, we should hide the value and
    show the suggested value.
    
    While this method worked pretty well until now, it stops working when
    the Autofill Undo project comes into perspective. Consider a field that
    was initially empty and then autofilled. Undoing the autofill would
    revert the field to being empty. When previewing this undo operation,
    the field's suggested value should be the empty string.
    
    This CL bases the show/hide behavior of inner editor values of
    TextControlElement on the nullness of the string instead of its
    emptiness, so that empty suggested values can be shown.
    
    Bug: 1441410
    Change-Id: Icda27903077378537561edd3e20d185a7d535e4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4769360
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1188414}

--- third_party/blink/renderer/core/html/forms/text_control_element.h
+++ third_party/blink/renderer/core/html/forms/text_control_element.h
@@ -156,6 +156,7 @@ class CORE_EXPORT TextControlElement : public HTMLFormControlElementWithState {
   void SetAutofillValue(const String& value,
                         WebAutofillState = WebAutofillState::kAutofilled);
 
+  // A null value indicates that the suggested value should be hidden.
   virtual void SetSuggestedValue(const String& value);
   const String& SuggestedValue() const;
 
