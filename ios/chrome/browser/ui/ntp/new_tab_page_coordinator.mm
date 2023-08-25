656e868226e6937633c7c8a9cf130e887b96b44d
Chris Lu <thegreenfrog@chromium.org>
2023-08-25 21:36:54 +0000

    [ios] Do not Return Snapshot of the NTPHeader for Overscroll
    The snapshot view of the NTPHeader's fakebox, which includes the visible
    identity icon, is slightly misaligned with the real icon on the
    initial scroll now that NTP views do not extend up into the safe area.
    This snapshot is meant to animate a fade out into the overscroll view.
    However, this snapshot is really not needed as the real profile icon
    does not clash with the overscroll views.
    
    This change also does a drive-by clean up of unused
    kOverscrollActionsWillStart and kOverscrollActionsDidEnd.
    
    Fixed: 1474671
    Change-Id: I21e3b9e6d276721a52d8d2f506ed9980975c5343
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4811657
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188539}

--- ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
@@ -1290,8 +1290,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (UIView*)toolbarSnapshotViewForOverscrollActionsController:
     (OverscrollActionsController*)controller {
-  return [[self.headerViewController toolBarView]
-      snapshotViewAfterScreenUpdates:NO];
+  return nil;
 }
 
 - (UIView*)headerViewForOverscrollActionsController:
--- ios/chrome/browser/ui/overscroll_actions/overscroll_actions_controller.mm
+++ ios/chrome/browser/ui/overscroll_actions/overscroll_actions_controller.mm
@@ -119,9 +119,6 @@ UIEdgeInsets TopContentInset(UIScrollView* scrollView, CGFloat topInset) {
 
 }  // namespace
 
-NSString* const kOverscrollActionsWillStart = @"OverscrollActionsWillStart";
-NSString* const kOverscrollActionsDidEnd = @"OverscrollActionsDidStop";
-
 // This protocol describes the subset of methods used between the
 // CRWWebViewScrollViewProxy and the UIWebView.
 @protocol OverscrollActionsScrollView<NSObject>
@@ -813,9 +810,6 @@ NSString* const kOverscrollActionsDidEnd = @"OverscrollActionsDidStop";
                          self.initialContentInset + statusBarFrame.size.height,
                          0);
       self.panPointScreenOrigin = CGPointZero;
-      [[NSNotificationCenter defaultCenter]
-          postNotificationName:kOverscrollActionsDidEnd
-                        object:self];
       [self resetScrollViewTopContentInset];
       self.disablingFullscreen = NO;
       if (_shouldInvalidate) {
@@ -830,10 +824,10 @@ NSString* const kOverscrollActionsDidEnd = @"OverscrollActionsDidStop";
         if (previousOverscrollState == OverscrollState::NO_PULL_STARTED) {
           UIView* view = [self.delegate
               toolbarSnapshotViewForOverscrollActionsController:self];
-          [self.overscrollActionView addSnapshotView:view];
-          [[NSNotificationCenter defaultCenter]
-              postNotificationName:kOverscrollActionsWillStart
-                            object:self];
+          if (view) {
+            // The NTP does not grab a snapshot
+            [self.overscrollActionView addSnapshotView:view];
+          }
           self.disablingFullscreen = YES;
         }
         [CATransaction begin];
