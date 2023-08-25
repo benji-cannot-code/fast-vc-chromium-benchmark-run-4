2ac4ae80069b40f5af55adcef41f5f3982f0b9e6
Chuong Ho <hdchuong@google.com>
2023-08-25 13:38:41 +0000

    only show toggle setting if allowed for use
    This CL adds a conditional check which decides if the toggle in the
    setting page should be shown.
    
    Bug: b:294962917
    Change-Id: I193044d9a6ddda5f738ef901eb0e145212067a59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4805723
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Chuong Ho <hdchuong@google.com>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188311}

--- chrome/browser/ui/webui/settings/ash/languages_section.cc
+++ chrome/browser/ui/webui/settings/ash/languages_section.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/no_destructor.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chrome/browser/ash/input_method/editor_mediator.h"
 #include "chrome/browser/ash/input_method/input_method_settings.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/ash/keyboard/chrome_keyboard_controller_client.h"
@@ -169,9 +170,9 @@ void AddSmartInputsStrings(content::WebUIDataSource* html_source,
        IDS_SETTINGS_SUGGESTIONS_EMOJI_SUGGESTION_DESCRIPTION}};
   html_source->AddLocalizedStrings(kLocalizedStrings);
 
-  // TODO: b/294962917 - Hook up with the EditorSwitch component instead of
-  // hard-coding "false".
-  html_source->AddBoolean("allowOrca", false);
+  html_source->AddBoolean(
+      "allowOrca", input_method::EditorMediator::HasInstance() &&
+                       input_method::EditorMediator::Get()->IsAllowedForUse());
   html_source->AddBoolean("allowEmojiSuggestion", is_emoji_suggestion_allowed);
 }
 
