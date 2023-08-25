cc7926d99cc3d4ba5489183979725e58fb1197d9
Calder Kitagawa <ckitagawa@chromium.org>
2023-08-25 18:14:35 +0000

    [Tab Content Manager] Re-enable tests
    testJpegRefetch was failing because tab loads were flaky. This CL
    attempts to wait longer for tab load.
    
    testLiveLayerDraws had a bad golden, but was otherwise fixed by an
    emulator update.
    
    Bug: 1455878, 1454653
    Change-Id: I3141c4ac4c3bdba234c3b27e2ad73a6d81133755
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4806970
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188431}

--- chrome/android/javatests/src/org/chromium/chrome/browser/compositor/layouts/content/TabContentManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/compositor/layouts/content/TabContentManagerTest.java
@@ -23,7 +23,7 @@ import org.chromium.base.Callback;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
+import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.compositor.CompositorView;
 import org.chromium.chrome.browser.compositor.CompositorViewHolder;
@@ -35,7 +35,6 @@ import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.R;
 import org.chromium.chrome.test.batch.BlankCTATabInitialStateRule;
-import org.chromium.chrome.test.util.browser.Features.DisableFeatures;
 import org.chromium.chrome.test.util.browser.Features.EnableFeatures;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.ui.test.util.RenderTestRule;
@@ -75,7 +74,6 @@ public class TabContentManagerTest {
     @Test
     @MediumTest
     @Feature({"RenderTest"})
-    @DisabledTest(message = "https://crbug.com/1455878")
     public void testLiveLayerDraws() throws Exception {
         final String testHttpsUrl1 =
                 sActivityTestRule.getTestServer().getURL("/chrome/test/data/android/test.html");
@@ -90,13 +88,14 @@ public class TabContentManagerTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "https://crbug.com/1454653")
-    // Disable "AImageReader" as a workaround for https://crbug.com/1454914
-    @DisableFeatures("AImageReader")
     public void testJpegRefetch() throws Exception {
         final String testHttpsUrl1 =
-                sActivityTestRule.getTestServer().getURL("/chrome/test/data/android/test.html");
+                sActivityTestRule.getTestServer().getURL("/chrome/test/data/android/google.html");
         sActivityTestRule.loadUrlInNewTab(testHttpsUrl1);
+
+        // Sometimes loadUrlInNewTab returns before the tab is actually loaded. Confirm again.
+        final Tab currentTab = sActivityTestRule.getActivity().getActivityTab();
+        CriteriaHelper.pollUiThread(() -> !currentTab.isLoading());
 
         final CallbackHelper helper = new CallbackHelper();
         final Bitmap[] bitmapHolder = new Bitmap[1];
@@ -106,7 +105,6 @@ public class TabContentManagerTest {
         };
 
         TestThreadUtils.runOnUiThreadBlocking(() -> {
-            final Tab currentTab = sActivityTestRule.getActivity().getActivityTab();
             final TabContentManager tabContentManager =
                     sActivityTestRule.getActivity().getTabContentManagerSupplier().get();
             final int height = 100;
