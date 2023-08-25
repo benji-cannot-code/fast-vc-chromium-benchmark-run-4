b2a8b3ceb63c9386e04dc148c2b3041df03fe40b
Greg Thompson <grt@chromium.org>
2023-08-25 21:06:34 +0000

    Reland "[windows] Remove RegKey::DeleteEmptyKey"
    This is a reland of commit 2887a0a67cb41519077275da42efc993126f1281 with
    a fix to perform non-recursive deletes when desired.
    
    Original change's description:
    > [windows] Remove RegKey::DeleteEmptyKey
    >
    > It's more straightforward and efficient for callers to check for
    > emptiness if they care.
    >
    > In support of this, GetValueCount now uses base::expected to distinguish
    > success from failure.
    >
    > Fixed: 1473203
    > Change-Id: Ia3513c7c23fdbd28ef0b762a13575780d48ef20a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4807912
    > Auto-Submit: Greg Thompson <grt@chromium.org>
    > Reviewed-by: S Ganesh <ganesh@chromium.org>
    > Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    > Commit-Queue: Greg Thompson <grt@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1188020}
    
    Change-Id: I160dc90e921e4606c595a2675c28c828fc53d126
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4813255
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188521}

--- base/win/registry.h
+++ base/win/registry.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_export.h"
 #include "base/functional/callback_forward.h"
 #include "base/types/expected.h"
+#include "base/types/strong_alias.h"
 #include "base/win/windows_types.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
@@ -81,9 +82,9 @@ class BASE_EXPORT RegKey {
   // occurrs while attempting to access it.
   bool HasValue(const wchar_t* value_name) const;
 
-  // Returns the number of values for this key, or 0 if the number cannot be
-  // determined.
-  DWORD GetValueCount() const;
+  // Returns the number of values for this key, or an error code if the number
+  // cannot be determined.
+  base::expected<DWORD, LONG> GetValueCount() const;
 
   // Returns the last write time or 0 on failure.
   FILETIME GetLastWriteTime() const;
@@ -94,13 +95,12 @@ class BASE_EXPORT RegKey {
   // True while the key is valid.
   bool Valid() const { return key_ != nullptr; }
 
-  // Kills a key and everything that lives below it; please be careful when
-  // using it.
-  LONG DeleteKey(const wchar_t* name);
-
-  // Deletes an empty subkey.  If the subkey has subkeys or values then this
-  // will fail.
-  LONG DeleteEmptyKey(const wchar_t* name);
+  // Kills a key and, by default, everything that lives below it; please be
+  // careful when using it. `recursive` = false may be used to prevent
+  // recursion, in which case the key is only deleted if it has no subkeys.
+  using RecursiveDelete = base::StrongAlias<class RecursiveDeleteTag, bool>;
+  LONG DeleteKey(const wchar_t* name,
+                 RecursiveDelete recursive = RecursiveDelete(true));
 
   // Deletes a single value within the key.
   LONG DeleteValue(const wchar_t* name);
