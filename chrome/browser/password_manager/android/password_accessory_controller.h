495680b86ebf37b0fa34cd2612d1f6feff79ed0d
Adem Derinel <derinel@google.com>
2023-08-25 07:51:08 +0000

    [Passwords][CodeHealth] Remove unused includes from accessory controller
    Bug: 1464469
    Change-Id: Iab508a8a911423ee5e3174801bc27563eb961c26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4810219
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188216}

--- chrome/browser/password_manager/android/password_accessory_controller.h
+++ chrome/browser/password_manager/android/password_accessory_controller.h
@@ -11,9 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/mojom/autofill_types.mojom-forward.h"
 #include "components/autofill/core/common/password_generation_util.h"
 #include "components/password_manager/core/browser/credential_cache.h"
-#include "content/public/browser/web_contents_user_data.h"
-#include "ui/gfx/image/image.h"
-#include "url/gurl.h"
+#include "content/public/browser/web_contents.h"
 
 // Interface for password-specific keyboard accessory controller between the
 // ManualFillingController and PasswordManagerClient.
