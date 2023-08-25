7e1fb8a97f4bab16df4f892777cce2f0329eec19
Tao Wu <wutao@chromium.org>
2023-08-25 21:21:26 +0000

    Handle escape key press on Editor Menu
    Press escape key will dismiss the widget.
    
    Bug: b/297295791
    Test: Manually
    Change-Id: Ib9962295caf5abd7078629d1752fcebe0f0cad2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4813971
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188528}

--- chrome/browser/ui/views/editor_menu/editor_menu_view.cc
+++ chrome/browser/ui/views/editor_menu/editor_menu_view.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/editor_menu/utils/pre_target_handler.h"
 #include "chrome/browser/ui/views/editor_menu/utils/utils.h"
 #include "components/vector_icons/vector_icons.h"
+#include "ui/base/accelerators/accelerator.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/chromeos/styles/cros_tokens_color_mappings.h"
 #include "ui/color/color_id.h"
@@ -102,6 +103,7 @@ views::UniqueWidgetPtr EditorMenuView::CreateWidget(
 
 void EditorMenuView::AddedToWidget() {
   widget_observation_.Observe(GetWidget());
+  AddAccelerator(ui::Accelerator(ui::VKEY_ESCAPE, ui::EF_NONE));
 }
 
 void EditorMenuView::RequestFocus() {
@@ -112,6 +114,12 @@ void EditorMenuView::RequestFocus() {
 void EditorMenuView::GetAccessibleNodeData(ui::AXNodeData* node_data) {
   node_data->role = ax::mojom::Role::kDialog;
   node_data->SetName(kContainerTitle);
+}
+
+bool EditorMenuView::AcceleratorPressed(const ui::Accelerator& accelerator) {
+  CHECK_EQ(accelerator.key_code(), ui::VKEY_ESCAPE);
+  GetWidget()->Close();
+  return true;
 }
 
 void EditorMenuView::OnWidgetDestroying(views::Widget* widget) {
