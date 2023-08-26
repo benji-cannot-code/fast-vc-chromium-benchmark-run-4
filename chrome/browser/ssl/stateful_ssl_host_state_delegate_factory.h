1922e43c38856835e6fe9ca225efe18591904980
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-26 00:02:16 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: I3beae1244388ce1cccfd0370a84950c1e648632f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4801087
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188605}

--- chrome/browser/ssl/stateful_ssl_host_state_delegate_factory.h
+++ chrome/browser/ssl/stateful_ssl_host_state_delegate_factory.h
@@ -36,7 +36,7 @@ class StatefulSSLHostStateDelegateFactory : public ProfileKeyedServiceFactory {
   ~StatefulSSLHostStateDelegateFactory() override;
 
   // BrowserContextKeyedServiceFactory methods:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
   bool ServiceIsNULLWhileTesting() const override;
 };
