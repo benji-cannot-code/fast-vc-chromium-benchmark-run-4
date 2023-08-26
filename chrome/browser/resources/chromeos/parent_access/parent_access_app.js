bc5262f3c5246eb7f4b92189f076dbd6f030bd94
Amber Haynes <amberhaynes@chromium.org>
2023-08-26 00:38:56 +0000

    Parent Access: Migrate cr-button to cros-button
    + Added new cros-button element to relevant PAW screens along
    with logic to switch between button types based on 'jelly-enabled',
    which, in this case, exists if and only if ParentAccessJelly is enabled.
    Demos (w/ Flag Enabled):
    LEA flow:
    https://screencast.googleplex.com/cast/NjA2MDkyNjM4NDk5NjM1Mnw4NzVmY2NiMC1kOQ
    LWA flow:
    https://screencast.googleplex.com/cast/NDkxOTI2MjEzNjIzODA4MHwzYTc4NDFjOS00YQ
    LEA Disabled screen:
    https://screencast.googleplex.com/cast/NTY0ODE5OTAxMzM2NzgwOHw5OWVmNTY3OC01MA
    
    Bug: b:293648390
    Change-Id: I4fc1d3071dac113bcb2deb7185dabeffc25c8451
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4808203
    Reviewed-by: Courtney Wong <courtneywong@chromium.org>
    Commit-Queue: Amber Haynes <amberhaynes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188619}

--- chrome/browser/resources/chromeos/parent_access/parent_access_app.js
+++ chrome/browser/resources/chromeos/parent_access/parent_access_app.js
@@ -43,6 +43,15 @@ export const ParentAccessEvent = {
   ON_SCREEN_SWITCHED: 'on-screen-switched',
 };
 
+/**
+ * Returns true if the Parent Access Jelly feature flag is enabled.
+ * @return {boolean}
+ */
+export function isParentAccessJellyEnabled() {
+  return loadTimeData.valueExists('isParentAccessJellyEnabled') &&
+      loadTimeData.getBoolean('isParentAccessJellyEnabled');
+}
+
 class ParentAccessApp extends PolymerElement {
   static get is() {
     return 'parent-access-app';
@@ -61,18 +70,6 @@ class ParentAccessApp extends PolymerElement {
       currentScreen_: {
         type: Screens,
       },
-      /**
-       * Returns true if the Parent Access Jelly feature flag is enabled.
-       * @private
-       */
-      isParentAccessJellyEnabled: {
-        type: Boolean,
-        readOnly: true,
-        value() {
-          return loadTimeData.valueExists('isParentAccessJellyEnabled') &&
-              loadTimeData.getBoolean('isParentAccessJellyEnabled');
-        },
-      },
     };
   }
 
@@ -80,7 +77,8 @@ class ParentAccessApp extends PolymerElement {
   ready() {
     super.ready();
 
-    if (this.isParentAccessJellyEnabled) {
+    // TODO (b/297564545): Clean up Jelly flag logic after Jelly is enabled.
+    if (isParentAccessJellyEnabled()) {
       const link = document.createElement('link');
       link.rel = 'stylesheet';
       link.href = 'chrome://theme/colors.css?sets=legacy,sys';
