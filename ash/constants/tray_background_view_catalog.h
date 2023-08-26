520655abd7410d09d599ee2cff14061b91683b4f
Andrew Pantera <andp@chromium.org>
2023-08-26 00:58:56 +0000

    focus-mode: Add lamp button in shelf
    Add a focus mode tray to the shelf. The tray should be shown on all
    displays when focus mode is active, and will eventually open the focus
    pod (b/286932322) when pressed.
    
    Screenshot of new tray:
    https://screenshot.googleplex.com/ti9ouK9RMznphML.png
    
    Bug: b/286932320
    Change-Id: I2adb07e033a5905a00187fa8cdc245ae7b2b918d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4785730
    Reviewed-by: Richard Chui <richui@chromium.org>
    Commit-Queue: Andrew Pantera <andp@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188624}

--- ash/constants/tray_background_view_catalog.h
+++ ash/constants/tray_background_view_catalog.h
@@ -37,7 +37,8 @@ enum class TrayBackgroundViewCatalogName {
   kVirtualKeyboardAccessibilityWindow = 20,
   kWmMode = 21,
   kVideoConferenceTray = 22,
-  kMaxValue = kVideoConferenceTray,
+  kFocusMode = 23,
+  kMaxValue = kFocusMode,
 };
 
 }  // namespace ash
--- ash/system/status_area_widget.cc
+++ ash/system/status_area_widget.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/accessibility/dictation_button_tray.h"
 #include "ash/system/accessibility/select_to_speak/select_to_speak_tray.h"
 #include "ash/system/eche/eche_tray.h"
+#include "ash/system/focus_mode/focus_mode_tray.h"
 #include "ash/system/holding_space/holding_space_tray.h"
 #include "ash/system/ime_menu/ime_menu_tray.h"
 #include "ash/system/media/media_tray.h"
@@ -87,6 +88,10 @@ void StatusAreaWidget::Initialize() {
   // Create the child views, left to right.
   overflow_button_tray_ =
       AddTrayButton(std::make_unique<StatusAreaOverflowButtonTray>(shelf_));
+  if (features::IsFocusModeEnabled()) {
+    // TODO(b/297445628): Get approval for order of focus mode tray in shelf.
+    focus_mode_tray_ = AddTrayButton(std::make_unique<FocusModeTray>(shelf_));
+  }
   if (features::IsVideoConferenceEnabled()) {
     video_conference_tray_ =
         AddTrayButton(std::make_unique<VideoConferenceTray>(shelf_));
@@ -299,6 +304,7 @@ void StatusAreaWidget::LogVisiblePodCountMetric() {
       case TrayBackgroundViewCatalogName::kVirtualKeyboardStatusArea:
       case TrayBackgroundViewCatalogName::kWmMode:
       case TrayBackgroundViewCatalogName::kVideoConferenceTray:
+      case TrayBackgroundViewCatalogName::kFocusMode:
         if (!tray_button->GetVisible()) {
           continue;
         }
