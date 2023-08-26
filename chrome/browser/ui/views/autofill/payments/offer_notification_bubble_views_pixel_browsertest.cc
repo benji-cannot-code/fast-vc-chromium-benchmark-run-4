014cbbd34d0b2c6ebdd778b616b36013c6209562
Mei Liang <meiliang@chromium.org>
2023-08-26 00:43:41 +0000

    [Discounts] Make notification bubble view ChromeRefresh2023 compatible
    The spacing for the following elements doesn't match the mock, pending
    on framework support:
      * The space between the header image view and the dialog frame
      * The space between the header image view and the title view
    
    Mock: https://screenshot.googleplex.com/75KeLzS4VcNiZKf
    Screenshot: https://screenshot.googleplex.com/BQd8SViTc2v7VPP
    
    Bug: b:289240164
    Change-Id: I2751ad4c4a7a7783b1548a686f11b21fe201d708
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4786950
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1188621}

--- chrome/browser/ui/views/autofill/payments/offer_notification_bubble_views_pixel_browsertest.cc
+++ chrome/browser/ui/views/autofill/payments/offer_notification_bubble_views_pixel_browsertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/data_model/autofill_offer_data.h"
 #include "components/commerce/core/commerce_feature_list.h"
 #include "content/public/test/browser_test.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/compositor/scoped_animation_duration_scale_mode.h"
 #include "ui/views/widget/any_widget_observer.h"
 
@@ -120,7 +121,12 @@ INSTANTIATE_TEST_SUITE_P(
         OfferNotificationBubbleViewPixelTestConfig{
             "FreeListingOffer_on_navigation",
             absl::make_optional<std::vector<base::test::FeatureRefAndParams>>(
-                {{commerce::kShowDiscountOnNavigation, {}}})}),
+                {{commerce::kShowDiscountOnNavigation, {}}})},
+        OfferNotificationBubbleViewPixelTestConfig{
+            "FreeListingOffer_on_navigation_chrome_refresh_style",
+            absl::make_optional<std::vector<base::test::FeatureRefAndParams>>(
+                {{commerce::kShowDiscountOnNavigation, {}},
+                 {::features::kChromeRefresh2023, {}}})}),
     GetTestName);
 
 // TODO(crbug.com/1473417): Disabled because this is flaky on the bots, but not
