2549268256e3705d9a53081bca9ef74ef9f11289
HuanPo Lin <robertlin@chromium.org>
2023-08-27 02:57:33 +0000

    preload: Add LINT.Change to check new prerender trigger types
    This CL adds LINT.Change and a document link to prerender_utils.cc
    about how to include the newly added prerender trigger types in metrics.
    
    Bug: 1462832
    Change-Id: I870bf8a4bddf8091410387b97ff42465b7b97343
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4795933
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188740}

--- content/public/browser/prerender_trigger_type.h
+++ content/public/browser/prerender_trigger_type.h
@@ -8,6 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+// If you add a new type of prerender trigger, please refer to the internal
+// document go/update-prerender-new-trigger-metrics to make sure that metrics
+// include the newly added trigger type. The string for the trigger type is
+// generated in PrerenderPageLoadMetricsObserver::AppendSuffix.
+// LINT.IfChange
 enum class PrerenderTriggerType {
   // https://wicg.github.io/nav-speculation/prerendering.html#speculation-rules
   kSpeculationRule,
@@ -16,6 +21,7 @@ enum class PrerenderTriggerType {
   // Trigger used by content embedders.
   kEmbedder,
 };
+// LINT.ThenChange(//components/page_load_metrics/browser/observers/prerender_page_load_metrics_observer.cc)
 
 }  // namespace content
 
