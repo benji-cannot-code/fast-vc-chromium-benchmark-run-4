4b3b242a1a95c17562bf7730639e44d7fd6c7025
Tommy Steimel <steimel@chromium.org>
2023-08-28 22:30:17 +0000

    Reland "[ChromeRefresh2023] Update document pip windows for Chrome Refresh"
    This is a reland of commit 6ffb7225aa3a3d420b7799e9cd5d5f69952e4a4d
    
    This reland removes the updated color ids since they had multiple issues including making the text/icons on video picture-in-picture windows unreadable. These will be dealt with in a separate CL.
    
    Original change's description:
    > [ChromeRefresh2023] Update document pip windows for Chrome Refresh
    >
    > This CL makes a few different UI updates to document picture-in-picture
    > windows for Chrome Refresh:
    >
    > 1) Updates the color of the new pageinfo icon. The new icon has a white
    > background that doesn't make sense with PiP always being in dark mode,
    > so this adds some code to override into dark colors instead.
    >
    > 2) Updates the back-to-tab and close button icons.
    >
    > 3) Updates the margins on the pageinfo button to match new specs.
    >
    > 4) Updates the pip foreground and background colors to match new specs.
    >
    > Bug: 1474072
    > Change-Id: Ie627ff46b188831d94660290385a74c22334029a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4795250
    > Reviewed-by: Evan Stade <estade@chromium.org>
    > Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Commit-Queue: Tommy Steimel <steimel@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1187811}
    
    Bug: 1474072
    Change-Id: I4829d8fb4926a1dba918a865978f5c3514c00bc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4814275
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189216}

--- chrome/browser/ui/views/overlay/close_image_button.cc
+++ chrome/browser/ui/views/overlay/close_image_button.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/ui/color/chrome_color_id.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/omnibox/browser/omnibox_field_trial.h"
+#include "components/vector_icons/vector_icons.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/models/image_model.h"
@@ -25,9 +27,13 @@ constexpr int kCloseButtonIconSize = 16;
 CloseImageButton::CloseImageButton(PressedCallback callback)
     : OverlayWindowImageButton(std::move(callback)) {
   SetSize(gfx::Size(kCloseButtonSize, kCloseButtonSize));
+
+  auto* icon = &views::kIcCloseIcon;
+  if (OmniboxFieldTrial::IsChromeRefreshIconsEnabled()) {
+    icon = &vector_icons::kCloseChromeRefreshIcon;
+  }
   SetImageModel(views::Button::STATE_NORMAL,
-                ui::ImageModel::FromVectorIcon(views::kIcCloseIcon,
-                                               kColorPipWindowForeground,
+                ui::ImageModel::FromVectorIcon(*icon, kColorPipWindowForeground,
                                                kCloseButtonIconSize));
 
   // Accessibility.
