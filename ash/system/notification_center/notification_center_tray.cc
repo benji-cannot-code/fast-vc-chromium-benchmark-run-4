e685300365dee7bf62aa84124d57975f05cd7540
Elliot Tuck <etuck@chromium.org>
2023-08-28 16:59:31 +0000

    Always update tray when notification added/removed
    The notification tray currently relies on its child tray items to inform
    it of when it needs to be updated. For certain sequences of notification
    updates, this can result in the tray not being up-to-date. This CL
    ensures that the tray is always up-to-date by explicitly updating it
    whenever a notification is added/removed.
    
    Bug: b:296918234,b:297375423,b:297579552
    Change-Id: I728dd4e1f73e770397c5c9c6f9feb5239a2e329d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4811257
    Commit-Queue: Elliot Tuck <etuck@chromium.org>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189019}

--- ash/system/notification_center/notification_center_tray.cc
+++ ash/system/notification_center/notification_center_tray.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/tray_container.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 
 namespace ash {
