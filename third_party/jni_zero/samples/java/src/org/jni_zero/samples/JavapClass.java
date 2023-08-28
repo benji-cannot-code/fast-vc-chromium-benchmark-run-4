e825deec4057f9e9759d266cace86f5e09b5d56c
Sam Maier <smaier@chromium.org>
2023-08-28 14:18:54 +0000

    jni_zero: repackaging java classes to use org.jni_zero
    Also, fixed bug where we used ParserError instead of the correct
    ParseError.
    
    Bug: 1456762
    Change-Id: I10b60ae5ba544af99172e8225f154cb418617949
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4808461
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188950}

--- third_party/jni_zero/samples/java/src/org/jni_zero/samples/JavapClass.java
+++ third_party/jni_zero/samples/java/src/org/jni_zero/samples/JavapClass.java
@@ -3,10 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.example.jni_generator;
+package org.jni_zero.samples;
 
 // To regenerate .class file:
-//   javac java/src/org/chromium/example/jni_generator/JavapClass.java
+//   javac java/src/org/jni_zero/samples/JavapClass.java
 public class JavapClass<T> {
     public static final int CONST_INT = 3;
     public static boolean sBoolValue = true;
