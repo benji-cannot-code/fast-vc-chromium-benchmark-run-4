6ec985dd29506b40d1e01b5343cb6da999ccb2d1
Nina Satragno <nsatragno@chromium.org>
2023-08-28 21:37:31 +0000

    [webauthn] Hide "Other devices" title if no creds
    Hide the "Other devices" title if there are no a priori discovered
    passkeys for a modal request. This resolves some ambiguity on Windows
    where "Windows Hello or external security key" is both "This device" and
    "Other devices".
    
    Bug: 1459273
    Change-Id: Ia68365f0066fbc4589b2d371b9c8d702e4fbddad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4811480
    Reviewed-by: Adam Langley <agl@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189162}

--- chrome/browser/ui/webauthn/authenticator_dialog_browsertest.cc
+++ chrome/browser/ui/webauthn/authenticator_dialog_browsertest.cc
@@ -615,7 +615,9 @@ class GPMPasskeysAuthenticatorDialogTest : public AuthenticatorDialogTest {
         /*extension_is_v2=*/absl::nullopt, std::move(phones),
         /*contact_phone_callback=*/base::DoNothing(), "fido://qrcode");
 
-    if (name == "local_and_phone") {
+    if (name == "no_passkeys_discovered") {
+      transport_availability.recognized_credentials = {};
+    } else if (name == "local_and_phone") {
       transport_availability.recognized_credentials = {
           std::move(local_cred1),
           std::move(local_cred2),
@@ -682,6 +684,8 @@ class GPMPasskeysAuthenticatorDialogTest : public AuthenticatorDialogTest {
       transport_availability.available_transports = {
           AuthenticatorTransport::kHybrid,
       };
+    } else {
+      NOTREACHED();
     }
     model_->StartFlow(std::move(transport_availability),
                       /*is_conditional_mediation=*/false);
@@ -691,6 +695,11 @@ class GPMPasskeysAuthenticatorDialogTest : public AuthenticatorDialogTest {
   std::unique_ptr<AuthenticatorRequestDialogModel> model_;
   base::test::ScopedFeatureList scoped_feature_list_;
 };
+
+IN_PROC_BROWSER_TEST_F(GPMPasskeysAuthenticatorDialogTest,
+                       InvokeUi_no_passkeys_discovered) {
+  ShowAndVerifyUi();
+}
 
 IN_PROC_BROWSER_TEST_F(GPMPasskeysAuthenticatorDialogTest,
                        InvokeUi_local_and_phone) {
