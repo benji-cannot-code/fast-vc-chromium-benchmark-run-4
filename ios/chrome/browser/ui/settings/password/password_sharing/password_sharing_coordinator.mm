62db97505dc4b652f3bf93cf2451b196555e3aa6
Rafał Godlewski <rgod@google.com>
2023-08-28 19:41:53 +0000

    [iOS] Add password sharing error view
    Adds modal alert being displayed on error of recipients fetching
    
    Bug: 1463882
    Change-Id: I98255c654c87886b4bfc1d52fbf84d02bb343b2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798391
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1189114}

--- ios/chrome/browser/ui/settings/password/password_sharing/password_sharing_coordinator.mm
+++ ios/chrome/browser/ui/settings/password/password_sharing/password_sharing_coordinator.mm
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/password/password_sharing/password_sharing_coordinator.h"
 
 #import "components/password_manager/core/browser/sharing/recipients_fetcher.h"
+#import "components/strings/grit/components_strings.h"
+#import "ios/chrome/browser/shared/coordinator/alert/alert_coordinator.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/shared/ui/table_view/table_view_navigation_controller.h"
@@ -20,7 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/password/password_sharing/password_sharing_mediator_delegate.h"
 #import "ios/chrome/browser/ui/settings/password/password_sharing/password_sharing_view_controller.h"
 #import "ios/chrome/browser/ui/settings/password/password_sharing/recipient_info.h"
+#import "ios/chrome/grit/ios_strings.h"
 #import "services/network/public/cpp/shared_url_loader_factory.h"
+#import "ui/base/l10n/l10n_util.h"
 
 using password_manager::FetchFamilyMembersRequestStatus;
 
@@ -43,6 +47,9 @@ using password_manager::FetchFamilyMembersRequestStatus;
 
 // Coordinator for family promo view.
 @property(nonatomic, strong) FamilyPromoCoordinator* familyPromoCoordinator;
+
+// Coordinator to display modal alerts to the user.
+@property(nonatomic, strong) AlertCoordinator* alertCoordinator;
 
 @end
 
@@ -94,6 +101,7 @@ using password_manager::FetchFamilyMembersRequestStatus;
 
   [self stopFamilyPickerCoordinator];
   [self stopFamilyPromoCoordinator];
+  [self stopAlertCoordinator];
 }
 
 #pragma mark - FamilyPickerCoordinatorDelegate
@@ -146,12 +154,40 @@ using password_manager::FetchFamilyMembersRequestStatus;
     case FetchFamilyMembersRequestStatus::kUnknown:
     case FetchFamilyMembersRequestStatus::kNetworkError:
     case FetchFamilyMembersRequestStatus::kPendingRequest:
-      // TODO(crbug.com/1463882): Implement error view.
+      __weak __typeof(self) weakSelf = self;
+      [self.viewController.presentingViewController
+          dismissViewControllerAnimated:YES
+                             completion:^() {
+                               [weakSelf startAlertCoordinator];
+                             }];
       break;
   }
 }
 
 #pragma mark - Private
+
+- (void)startAlertCoordinator {
+  NSString* title = l10n_util::GetNSString(
+      IDS_IOS_PASSWORD_SHARING_FETCHING_RECIPIENTS_ERROR_TITLE);
+  NSString* message = l10n_util::GetNSString(
+      IDS_IOS_PASSWORD_SHARING_FETCHING_RECIPIENTS_ERROR);
+  [self.alertCoordinator stop];
+  self.alertCoordinator = [[AlertCoordinator alloc]
+      initWithBaseViewController:self.baseViewController
+                         browser:self.browser
+                           title:title
+                         message:message];
+  __weak __typeof(self) weakSelf = self;
+  [self.alertCoordinator
+      addItemWithTitle:l10n_util::GetNSString(IDS_CANCEL)
+                action:^() {
+                  [weakSelf stopAlertCoordinator];
+                  [weakSelf.delegate
+                      passwordSharingCoordinatorDidRemove:weakSelf];
+                }
+                 style:UIAlertActionStyleCancel];
+  [self.alertCoordinator start];
+}
 
 - (void)stopFamilyPickerCoordinator {
   [self.familyPickerCoordinator stop];
@@ -163,6 +199,11 @@ using password_manager::FetchFamilyMembersRequestStatus;
   [self.familyPromoCoordinator stop];
   self.familyPromoCoordinator.delegate = nil;
   self.familyPromoCoordinator = nil;
+}
+
+- (void)stopAlertCoordinator {
+  [self.alertCoordinator stop];
+  self.alertCoordinator = nil;
 }
 
 @end
