633754d1f9f1d84bdff65058499d36dc4a5d6d2d
Evan Stade <estade@chromium.org>
2023-08-28 21:34:08 +0000

    IndexedDB: fix hang in IndexedDBConnectionCoordinator
    Whenever `state_` changes*, aside from inside the Perform() method,
    `tasks_available_callback_` must be run. After
    crrev.com/01b94c0abfb5d620a5b2122fe0dd122bfbb90753 some codepaths
    within Perform() became asynchronous which were previously always
    synchronous. This change adds some more invocations of this callback
    which have therefore become necessary.
    
    *except if it changes from one type of "pending async work" to another,
    which is why you might notice that occasionally the callback is not
    run after updating `state_`.
    
    Aside from fixing a real problem, this should also fix flakiness in
    transaction-abort-generator-revert.html
    
    credit to jsbell@chromium.org for the reduction.
    
    Bug: 1472028,1466727
    Change-Id: I6d6ae34d0c32f1c0c5a4c35c8955bfdcdfd9c2ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4814947
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189156}

--- content/browser/indexed_db/indexed_db_connection_coordinator.cc
+++ content/browser/indexed_db/indexed_db_connection_coordinator.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/auto_reset.h"
+#include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
@@ -161,6 +163,11 @@ class IndexedDBConnectionCoordinator::OpenRequest
   OpenRequest& operator=(const OpenRequest&) = delete;
 
   void Perform(bool has_connections) override {
+    // Since `state_` is checked after the call to `Perform()`, temporarily make
+    // `tasks_available_callback_` a no-op.
+    base::AutoReset suspend_callback(&tasks_available_callback_,
+                                     base::DoNothing());
+
     // If the metadata is in an uninitialized state, that means one of two
     // things:
     //
@@ -206,6 +213,7 @@ class IndexedDBConnectionCoordinator::OpenRequest
   }
 
   void ContinueOpening(bool has_connections) {
+    base::ScopedClosureRunner scoped_tasks_available(tasks_available_callback_);
     const int64_t old_version = db_->metadata().version;
     int64_t& new_version = pending_->version;
 
@@ -301,8 +309,8 @@ class IndexedDBConnectionCoordinator::OpenRequest
   }
 
   // Initiate the upgrade. The bulk of the work actually happens in
-  // IndexedDBConnectionCoordinator::VersionChangeOperation in order to kick the
-  // transaction into the correct state.
+  // VersionChangeOperation in order to kick the transaction into the correct
+  // state.
   void StartUpgrade() {
     DCHECK(state_ == RequestState::kPendingLocks);
 
@@ -434,6 +442,11 @@ class IndexedDBConnectionCoordinator::DeleteRequest
   DeleteRequest& operator=(const DeleteRequest&) = delete;
 
   void Perform(bool has_connections) override {
+    // Since `state_` is checked after the call to `Perform()`, temporarily make
+    // `tasks_available_callback_` a no-op.
+    base::AutoReset suspend_callback(&tasks_available_callback_,
+                                     base::DoNothing());
+
     if (db_->metadata().id == kInvalidDatabaseId) {
       ContinueAfterAcquiringLocks(base::BindOnce(
           &IndexedDBConnectionCoordinator::DeleteRequest::InitDatabase,
@@ -444,6 +457,7 @@ class IndexedDBConnectionCoordinator::DeleteRequest
   }
 
   void InitDatabase(bool has_connections) {
+    base::ScopedClosureRunner scoped_tasks_available(tasks_available_callback_);
     saved_leveldb_status_ = db_->OpenInternal();
     if (!saved_leveldb_status_.ok()) {
       IndexedDBDatabaseError error(blink::mojom::IDBException::kUnknownError,
@@ -513,13 +527,13 @@ class IndexedDBConnectionCoordinator::DeleteRequest
     if (!weak_ptr)
       return;
 
+    base::ScopedClosureRunner scoped_tasks_available(tasks_available_callback_);
     if (!saved_leveldb_status_.ok()) {
       // TODO(jsbell): Consider including sanitized leveldb status message.
       IndexedDBDatabaseError error(blink::mojom::IDBException::kUnknownError,
                                    "Internal error deleting database.");
       factory_client_->OnError(error);
       state_ = RequestState::kError;
-      tasks_available_callback_.Run();
       return;
     }
 
@@ -535,7 +549,6 @@ class IndexedDBConnectionCoordinator::DeleteRequest
     factory_client_->OnDeleteSuccess(old_version);
 
     state_ = RequestState::kDone;
-    tasks_available_callback_.Run();
   }
 
   void CreateAndBindTransaction() override { NOTREACHED(); }
