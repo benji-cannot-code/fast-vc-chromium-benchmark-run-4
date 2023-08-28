35866a9d805cac42f1c2b702244dce1720ec8621
Marijn Kruisselbrink <mek@chromium.org>
2023-08-28 17:00:47 +0000

    Fix DCHECK in CRWWebControllerContainerView dealloc.
    Assigning nil to contentViewProxy.contentView is explicitly not
    supported by its setter, and reliably triggers a DCHECK. Instead call
    the method that exists to null out this field.
    
    This bug is blocking updating OCMock, as newer OCMock versions have
    fixed some memory leaks which now cause this dealloc method to
    actually be called in tests.
    
    Bug: 1476506
    Change-Id: I160694081f845d9efa00e817cc4e810555f7d2e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4817967
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Auto-Submit: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189021}

--- ios/web/web_state/ui/crw_web_controller_container_view.mm
+++ ios/web/web_state/ui/crw_web_controller_container_view.mm
@@ -55,7 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)dealloc {
-  self.contentViewProxy.contentView = nil;
+  [self.contentViewProxy clearContentViewAndAddPlaceholder:NO];
 }
 
 #pragma mark Accessors
