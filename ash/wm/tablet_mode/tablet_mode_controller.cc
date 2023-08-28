b5d6a3f7a3c9058e9b40915e0e2a77e5dca3d4bd
lschyi <lschyi@google.com>
2023-08-28 02:16:18 +0000

    Fix tablet mode controller requesting machine state too early
    Tablet mode controller sends DBus request to powerd daemon for getting
    the lid and detachable base status, then update the UI accordingly.
    However, powerd may not be ready for the first time tablet controller
    asking the status, thus the machine always stays in clamshell mode.
    
    Update the tablet mode controller via overriding the
    `PowerManagerBecameAvailable` member function, and perform requesting
    switch status in it. With overriding this function, the first time
    switch request is ensured to be sent after the powerd daemon is ready.
    
    BUG=b:283533859
    
    Change-Id: I51d713b4618c4442f9d2db902d17552762feb2ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4813120
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Sung-Chi Li <lschyi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188821}

--- ash/wm/tablet_mode/tablet_mode_controller.cc
+++ ash/wm/tablet_mode/tablet_mode_controller.cc
@@ -420,8 +420,6 @@ TabletModeController::TabletModeController()
   chromeos::PowerManagerClient* power_manager_client =
       chromeos::PowerManagerClient::Get();
   power_manager_client->AddObserver(this);
-  power_manager_client->GetSwitchStates(base::BindOnce(
-      &TabletModeController::OnGetSwitchStates, weak_factory_.GetWeakPtr()));
 }
 
 TabletModeController::~TabletModeController() {
@@ -685,6 +683,14 @@ void TabletModeController::OnAccelerometerUpdated(
   }
 
   StartTrackingTabletUsageMetricsIfApplicable();
+}
+
+void TabletModeController::PowerManagerBecameAvailable(bool available) {
+  if (!available) {
+    return;
+  }
+  chromeos::PowerManagerClient::Get()->GetSwitchStates(base::BindOnce(
+      &TabletModeController::OnGetSwitchStates, weak_factory_.GetWeakPtr()));
 }
 
 void TabletModeController::LidEventReceived(
