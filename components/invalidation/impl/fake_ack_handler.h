ea3ef31f5cc2452f415adbff4959daebf3274594
Roland Bock <rbock@google.com>
2023-08-28 13:06:33 +0000

    [Invalidation] Remove unused functions
    Invalidation::Drop is currently unused. It is the only user of
    AckHandler::Drop, which is the only user of
    UnackedInvalidationSet::Drop, which is the only user of
    Invalidation::InitFromDroppedInvalidation.
    
    This CL removes these functions.
    
    Bug: None
    Tests: None (non-functional change)
    Change-Id: Id838592fc03fb3c6a9400be435a57420900ed5d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4813872
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1188935}

--- components/invalidation/impl/fake_ack_handler.h
+++ components/invalidation/impl/fake_ack_handler.h
@@ -44,10 +44,6 @@ class INVALIDATION_EXPORT FakeAckHandler
   // acknowledged.
   bool IsAcknowledged(const Invalidation& invalidation) const;
 
-  // Returns true if the specified invalidation has been delivered and
-  // dropped.
-  bool IsDropped(const Invalidation& invalidation) const;
-
   // Returns true if the specified invalidation was never delivered.
   bool IsUnsent(const Invalidation& invalidation) const;
 
@@ -56,7 +52,6 @@ class INVALIDATION_EXPORT FakeAckHandler
 
   // Implementation of AckHandler.
   void Acknowledge(const Topic& topic, const AckHandle& handle) override;
-  void Drop(const Topic& topic, const AckHandle& handle) override;
 
  private:
   typedef std::vector<Invalidation> InvalidationVector;
@@ -64,7 +59,6 @@ class INVALIDATION_EXPORT FakeAckHandler
   InvalidationVector unsent_invalidations_;
   InvalidationVector unacked_invalidations_;
   InvalidationVector acked_invalidations_;
-  InvalidationVector dropped_invalidations_;
 
   std::map<Topic, AckHandle> unrecovered_drop_events_;
 };
