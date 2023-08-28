c83266ab47685c758fcf2a20f1d9cceb37ce8850
Juan Mojica <juanmojica@google.com>
2023-08-28 23:56:20 +0000

    [CSC] Add method type for hard refreshing the companion page.
    Bug: b:295236622
    Change-Id: I5c6f84c0ac91eb8f600bbb26d871a35750094a7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4814080
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Anudeep Palanki <apalanki@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1189262}

--- chrome/browser/companion/core/companion_url_builder_unittest.cc
+++ chrome/browser/companion/core/companion_url_builder_unittest.cc
@@ -187,6 +187,7 @@ TEST_F(CompanionUrlBuilderTest, MsbbOff) {
   EXPECT_TRUE(proto.is_sign_in_allowed());
   EXPECT_FALSE(proto.has_msbb_enabled());
   EXPECT_TRUE(proto.is_upload_dialog_supported());
+  EXPECT_TRUE(proto.is_hard_refresh_supported());
 }
 
 TEST_F(CompanionUrlBuilderTest, MsbbOn) {
@@ -225,6 +226,7 @@ TEST_F(CompanionUrlBuilderTest, MsbbOn) {
   EXPECT_TRUE(proto.links_open_in_new_tab());
   EXPECT_FALSE(proto.is_vqs_enabled_on_chrome());
   EXPECT_TRUE(proto.is_upload_dialog_supported());
+  EXPECT_TRUE(proto.is_hard_refresh_supported());
 
   // Verify promo state.
   EXPECT_TRUE(proto.has_promo_state());
