8c619ff3cd134ee11882e8bacaa905997f825d49
Victor Tan <victortan@chromium.org>
2023-08-28 15:41:47 +0000

    [WebView] Implement support library glue for user-agent metadata
    Add boundary interface and support lib glue for set/get user-agent
    metadata.
    
    Change-Id: Ib9be1db7cc4555efecb933892cf273c4bf4955e6
    Bug: b/294183509
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4783052
    Reviewed-by: Peter Pakkenberg <pbirk@chromium.org>
    Commit-Queue: Victor Tan <victortan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188979}

--- android_webview/support_library/boundary_interfaces/src/org/chromium/support_lib_boundary/WebSettingsBoundaryInterface.java
+++ android_webview/support_library/boundary_interfaces/src/org/chromium/support_lib_boundary/WebSettingsBoundaryInterface.java
@@ -12,6 +12,7 @@ package org.chromium.support_lib_boundary;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -63,4 +64,7 @@ public interface WebSettingsBoundaryInterface {
 
     void setEnterpriseAuthenticationAppLinkPolicyEnabled(boolean enabled);
     boolean getEnterpriseAuthenticationAppLinkPolicyEnabled();
+
+    void setUserAgentMetadataFromMap(Map<String, Object> uaMetadata);
+    Map<String, Object> getUserAgentMetadataMap();
 }
