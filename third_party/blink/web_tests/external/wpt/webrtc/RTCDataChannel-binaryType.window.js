628420712ab43745d89fa0ca6757a7debda592f6
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2023-08-28 21:40:18 +0000

    Import wpt@04227f01f6ef154ff9fb2476e713b71250ae4b2b
    Using wpt-import in Chromium 2a3e08231dd85fad875210300ab0fa045be3e207.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    hbos@chromium.org, hta@chromium.org, guidou@chromium.org, phancke@microsoft.com:
      external/wpt/webrtc
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I862d07e4a9d7e8182277e19a7f52ecd4d5e61ad9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4817649
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1189167}

--- third_party/blink/web_tests/external/wpt/webrtc/RTCDataChannel-binaryType.window.js
+++ third_party/blink/web_tests/external/wpt/webrtc/RTCDataChannel-binaryType.window.js
@@ -2,7 +2,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict';
 
 const validBinaryTypes = ['blob', 'arraybuffer'];
-const invalidBinaryTypes = ['jellyfish', 'arraybuffer ', '', null, undefined];
+const invalidBinaryTypes = ['jellyfish', 'arraybuffer ', '', null, undefined, 234, 54n];
+
+test((t) => {
+  const pc = new RTCPeerConnection();
+  t.add_cleanup(() => pc.close());
+  const dc = pc.createDataChannel('test-binary-type');
+
+  assert_equals(dc.binaryType, "blob", `dc.binaryType should be 'blob'`);
+}, `Default binaryType value`);
 
 for (const binaryType of validBinaryTypes) {
   test((t) => {
@@ -21,8 +29,8 @@ for (const binaryType of invalidBinaryTypes) {
     t.add_cleanup(() => pc.close());
     const dc = pc.createDataChannel('test-binary-type');
 
-    assert_throws_dom('SyntaxError', () => {
-      dc.binaryType = binaryType;
-    });
-  }, `Setting invalid binaryType '${binaryType}' should throw SyntaxError`);
+    dc.binaryType = "arraybuffer";
+    dc.binaryType = binaryType;
+    assert_equals(dc.binaryType, "arraybuffer");
+  }, `Setting binaryType to '${binaryType}' should be ignored`);
 }
