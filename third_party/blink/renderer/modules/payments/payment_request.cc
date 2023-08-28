63cc00dafb98cffc501f5654ac4acae888a8a92f
Nick Burris <nburris@chromium.org>
2023-08-28 16:50:15 +0000

    [PaymentRequest] Return SecurityError for activationless show
    This fixes the browser-side error logic for activationless show() to
    correctly return a SecurityError instead of NotAllowedError. This is
    behind a disabled flag which will be enabled in a following patch
    https://crrev.com/c/4752600.
    
    Bug: 1454204
    Change-Id: Icc4e79b6838c7e4131c03ab8beedf03cd6b9036c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4807410
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189016}

--- third_party/blink/renderer/modules/payments/payment_request.cc
+++ third_party/blink/renderer/modules/payments/payment_request.cc
@@ -1642,6 +1642,10 @@ void PaymentRequest::OnError(PaymentErrorReason error,
       exception_code = DOMExceptionCode::kOptOutError;
       break;
 
+    case PaymentErrorReason::USER_ACTIVATION_REQUIRED:
+      exception_code = DOMExceptionCode::kSecurityError;
+      break;
+
     case PaymentErrorReason::UNKNOWN:
       break;
   }
