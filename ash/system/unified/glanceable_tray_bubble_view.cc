0651221c4402f7a9dd41f4666d5f7050097bc940
Toni Barzic <tbarzic@chromium.org>
2023-08-28 14:55:16 +0000

    Show whole glanceable when it receives focus
    When the focus moves into a glanceable container, attempt scrolling the
    whole glanceable container to visible before changing focus (which will
    additionally ensure that the newly focused view is actually visible,
    which can potentially happen if there is no space to show the whole
    glanceable within the display work area bounds).
    
    BUG=b:296472561
    
    Change-Id: If2df293a17dbe569d5d3a8b92bdc5d45d14a4cfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4815946
    Reviewed-by: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188965}

--- ash/system/unified/glanceable_tray_bubble_view.cc
+++ ash/system/unified/glanceable_tray_bubble_view.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/layer.h"
 #include "ui/gfx/geometry/rounded_corners_f.h"
 #include "ui/views/controls/scroll_view.h"
+#include "ui/views/focus/focus_manager.h"
 
 namespace ash {
 
@@ -38,7 +39,8 @@ namespace {
 // The view that parents glanceable bubbles. It's a flex layout view that
 // propagates child preferred size changes to the tray bubble view and the
 // container bounds changes to the bubble view.
-class ContainerView : public views::FlexLayoutView {
+class ContainerView : public views::FlexLayoutView,
+                      public views::FocusChangeListener {
  public:
   using HeightChangeCallback = base::RepeatingCallback<void(int height_delta)>;
   ContainerView(const base::RepeatingClosure& preferred_size_change_callback,
@@ -87,7 +89,38 @@ class ContainerView : public views::FlexLayoutView {
     }
   }
 
+  void AddedToWidget() override {
+    GetFocusManager()->AddFocusChangeListener(this);
+  }
+
+  void RemovedFromWidget() override {
+    GetFocusManager()->RemoveFocusChangeListener(this);
+  }
+
+  // views::FocusChangeListener:
+  void OnWillChangeFocus(views::View* focused_before,
+                         views::View* focused_now) override {
+    views::View* container_for_new_focus = GetChildThatContains(focused_now);
+    // It the focus is moving into a glanceable container, try scrolling the
+    // whole container into the viewport.
+    if (container_for_new_focus &&
+        container_for_new_focus != GetChildThatContains(focused_before)) {
+      container_for_new_focus->ScrollViewToVisible();
+    }
+  }
+  void OnDidChangeFocus(views::View* focused_before,
+                        views::View* focused_now) override {}
+
  private:
+  views::View* GetChildThatContains(views::View* view) {
+    for (auto* child : children()) {
+      if (child->Contains(view)) {
+        return child;
+      }
+    }
+    return nullptr;
+  }
+
   base::RepeatingClosure preferred_size_change_callback_;
   HeightChangeCallback height_change_callback_;
 };
