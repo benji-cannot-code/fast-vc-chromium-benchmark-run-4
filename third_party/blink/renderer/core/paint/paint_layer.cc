fbcab40a647bcfba260107ba3f5b380f16e1d223
Xianzhu Wang <wangxianzhu@chromium.org>
2023-08-28 22:49:40 +0000

    [RemoveLegacy] PaintPropertyTreeBuilderContext always has one fragment
    Now PaintPropertyTreeBuilder is called from PrePaintTreeWalk either
    for a non-fragmented object or for each fragment of a ng-block-
    fragmented object, so it always sees one fragment.
    
    PaintPropertyTreeBuilderFragmentContext is still not folded into
    PaintPropertyTreeBuilderContext because hidden svg containers need
    the default constructor of the former to initialize an independent
    paint property tree builder context. Will consider refactor.
    
    Bug: 1229581
    Change-Id: Ic86e0e068faf1fa480a9d64b99dba0aa1cdf4f0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4818067
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189229}

--- third_party/blink/renderer/core/paint/paint_layer.cc
+++ third_party/blink/renderer/core/paint/paint_layer.cc
@@ -103,6 +103,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/compositor_filter_operations.h"
 #include "third_party/blink/renderer/platform/graphics/filters/filter.h"
 #include "third_party/blink/renderer/platform/graphics/paint/geometry_mapper.h"
+#include "third_party/blink/renderer/platform/heap/collection_support/clear_collection_scope.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/instrumentation/histogram.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
