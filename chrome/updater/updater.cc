a8e7f9e2a81d8fe6b2182804c215fe14d8c436e3
Sorin Jianu <sorin@chromium.org>
2023-08-28 12:59:02 +0000

    updater: use an UI message type on main sequence as needed.
    Bug: 1476296
    Change-Id: I44cb8094c23875cb85e81b9ec8a0214cd97fe090
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4815887
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188933}

--- chrome/updater/updater.cc
+++ chrome/updater/updater.cc
@@ -132,7 +132,6 @@ int HandleUpdaterCommands(UpdaterScope updater_scope,
   InitializeThreadPool("updater");
   const base::ScopedClosureRunner shutdown_thread_pool(
       base::BindOnce([] { base::ThreadPoolInstance::Get()->Shutdown(); }));
-  base::SingleThreadTaskExecutor main_task_executor(base::MessagePumpType::UI);
 
   // Records a backtrace in the log, crashes the program, saves a crash dump,
   // and reports the crash.
@@ -144,6 +143,18 @@ int HandleUpdaterCommands(UpdaterScope updater_scope,
   // continue to function.
   ScopedIPCSupportWrapper ipc_support;
 #endif
+  // TODO(crbug.com/1476296) - eliminate the need to have a UI message type
+  // on the main sequence by refactoring the splash screen and the rest of UI.
+  const bool is_app_install_mode = command_line->HasSwitch(kInstallSwitch) ||
+                                   command_line->HasSwitch(kTagSwitch) ||
+                                   command_line->HasSwitch(kRuntimeSwitch) ||
+                                   command_line->HasSwitch(kHandoffSwitch);
+  base::SingleThreadTaskExecutor main_task_executor(
+      is_app_install_mode ? base::MessagePumpType::UI
+                          : base::MessagePumpType::DEFAULT);
+  if (is_app_install_mode) {
+    return MakeAppInstall(command_line->HasSwitch(kSilentSwitch))->Run();
+  }
 
   if (command_line->HasSwitch(kServerSwitch)) {
     return MakeAppServer()->Run();
@@ -160,13 +171,6 @@ int HandleUpdaterCommands(UpdaterScope updater_scope,
     return kErrorOk;
   }
 #endif  // BUILDFLAG(IS_WIN)
-
-  if (command_line->HasSwitch(kInstallSwitch) ||
-      command_line->HasSwitch(kTagSwitch) ||
-      command_line->HasSwitch(kRuntimeSwitch) ||
-      command_line->HasSwitch(kHandoffSwitch)) {
-    return MakeAppInstall(command_line->HasSwitch(kSilentSwitch))->Run();
-  }
 
   if (command_line->HasSwitch(kUninstallSwitch) ||
       command_line->HasSwitch(kUninstallIfUnusedSwitch)) {
