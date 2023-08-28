b0576abf1281983b9ed733a831bca26a013fc60a
Daniel Murphy <dmurph@chromium.org>
2023-08-28 19:04:03 +0000

    [dPWA] Remove browser filter on CrOS 'disabled' app throttle.
    After a refactor to pull this throttle out, it was noticed that the
    original code filtered out valid use-cases for this disabled page to
    be displayed.
    
    This change removes that filtering, so now the following cases can now
    be correctly disabled:
    - App browsers
    - Prerendering of disabled content
    
    Bug: 1474680
    Change-Id: Ic56dbb9545c2d7a93e958d277046a023e6b9889c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4799832
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189093}

--- chrome/browser/apps/intent_helper/chromeos_disabled_apps_throttle.cc
+++ chrome/browser/apps/intent_helper/chromeos_disabled_apps_throttle.cc
@@ -11,14 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
-#include "chrome/browser/apps/intent_helper/intent_picker_internal.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/policy/system_features_disable_list_policy_handler.h"
-#include "chrome/browser/preloading/prefetch/no_state_prefetch/chrome_no_state_prefetch_contents_delegate.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_finder.h"
-#include "chrome/browser/web_applications/web_app_utils.h"
 #include "chrome/grit/browser_resources.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/navigation_handle.h"
@@ -49,30 +44,6 @@ std::string GetAppDisabledErrorPage() {
   webui::SetLoadTimeDataDefaults(app_locale, &strings);
   return webui::GetI18nTemplateHtml(html, strings);
 }
-
-bool ShouldCheckAppsForUrl(content::WebContents* web_contents) {
-  // Do not check apps for url if no apps can be installed, e.g. in incognito.
-  // Do not check apps for a no-state prefetcher navigation.
-  if (!web_app::AreWebAppsUserInstallable(
-          Profile::FromBrowserContext(web_contents->GetBrowserContext())) ||
-      prerender::ChromeNoStatePrefetchContentsDelegate::FromWebContents(
-          web_contents) != nullptr) {
-    return false;
-  }
-
-  // Do not check apps for url if we are already in an app browser.
-  // It is possible that the web contents is not inserted to tab strip
-  // model at this stage (e.g. open url in new tab). So if we cannot
-  // find a browser at this moment, skip the check and this will be handled
-  // in later stage.
-  Browser* browser = chrome::FindBrowserWithWebContents(web_contents);
-  if (browser && (browser->is_type_app() || browser->is_type_app_popup())) {
-    return false;
-  }
-
-  return true;
-}
-
 }  // namespace
 
 // static
@@ -88,12 +59,6 @@ ChromeOsDisabledAppsThrottle::MaybeCreate(content::NavigationHandle* handle) {
   Profile* profile =
       Profile::FromBrowserContext(web_contents->GetBrowserContext());
   if (!AppServiceProxyFactory::IsAppServiceAvailableForProfile(profile)) {
-    return nullptr;
-  }
-
-  // TODO(dmurph): Remove this, as prerenders and app browsers should be able to
-  // escape the disabled throttle. https://crbug.com/1474680
-  if (!ShouldCheckAppsForUrl(web_contents)) {
     return nullptr;
   }
 
