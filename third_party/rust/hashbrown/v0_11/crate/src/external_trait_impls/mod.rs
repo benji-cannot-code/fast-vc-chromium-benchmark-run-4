44699a66545d18e53fdc667ae7c9fb58f5f0e59e
Collin Baker <collinbaker@chromium.org>
2023-08-28 17:09:40 +0000

    Remove third_party/rust gnrt dependencies
    gnrt was moved to a Cargo-managed build and no longer uses vendored
    crates in third_party/rust. Remove the crates not used elsewhere.
    
    Bug: 1475069
    Change-Id: I102ae8deeced53656fe08ea9a65732df31399d0b
    
    Cq-Include-Trybots: luci.chromium.try:android-rust-arm32-rel,android-rust-arm64-dbg,android-rust-arm64-rel,linux-rust-x64-dbg,linux-rust-x64-rel,mac-rust-x64-dbg,win-rust-x64-dbg,win-rust-x64-rel
    Change-Id: I102ae8deeced53656fe08ea9a65732df31399d0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4815127
    Auto-Submit: Collin Baker <collinbaker@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189025}

--- third_party/rust/hashbrown/v0_11/crate/src/external_trait_impls/mod.rs
+++
@@ -1,5 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#[cfg(feature = "rayon")]
-pub(crate) mod rayon;
-#[cfg(feature = "serde")]
-mod serde;
--- third_party/rust/hashbrown/v0_11/crate/src/external_trait_impls/rayon/mod.rs
+++
@@ -1,5 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-mod helpers;
-pub(crate) mod map;
-pub(crate) mod raw;
-pub(crate) mod set;
