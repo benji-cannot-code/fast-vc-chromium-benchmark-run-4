1f92292ab96bc901415387a9315d8f465efc6622
Bastian Kersting <bkersting@google.com>
2023-08-28 16:19:26 +0000

    [Telemetry] Add error handling to DiagnosticRoutine
    This change adds the os.diagnostics.onRoutineException callback
    and its invocation from DiagnosticRoutine.
    
    Bug: b/286031590, b/294819343
    Test: browser_tests
    --gtest_filter="*TelemetryExtensionDiagnosticRoutineObserverBrowserTest*"
    
    NO_IFTTT=Documentation for this change will be done in one batch
    Low-Coverage-Reason: Tests for the error callback will be added in
    subsequent patches.
    
    Change-Id: Idf2bf4fb0a2962f37c8662204bf93941cc27f75c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4783812
    Reviewed-by: Ethan Cheng <yycheng@google.com>
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188998}

--- chrome/browser/chromeos/extensions/telemetry/api/routines/diagnostic_routine_converters.h
+++ chrome/browser/chromeos/extensions/telemetry/api/routines/diagnostic_routine_converters.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/uuid.h"
 #include "chrome/common/chromeos/extensions/api/diagnostics.h"
 #include "chromeos/crosapi/mojom/telemetry_diagnostic_routine_service.mojom.h"
+#include "chromeos/crosapi/mojom/telemetry_extension_exception.mojom.h"
 
 // This file contains helper functions used by the routine API to convert its
 // types to/from telemetry service types.
@@ -46,6 +47,9 @@ api::os_diagnostics::MemoryRoutineFinishedInfo UncheckedConvertPtr(
     bool has_passed);
 
 }  // namespace unchecked
+
+api::os_diagnostics::ExceptionReason Convert(
+    crosapi::mojom::TelemetryExtensionException::Reason input);
 
 api::os_diagnostics::RoutineWaitingReason Convert(
     crosapi::mojom::TelemetryDiagnosticRoutineStateWaiting::Reason input);
