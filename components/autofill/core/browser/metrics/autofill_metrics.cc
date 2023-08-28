caaacc227072b71170e66286768ae20f4b71d9b6
Jihad Hanna <jihadghanna@google.com>
2023-08-28 15:59:36 +0000

    Added a user action for Undo
    A user action exist to track the usage of Clear Form.
    
    This CL adds a similar action for Undo so that we can compare their
    usages.
    
    Bug: 1441410
    Change-Id: I40a57646821d76a36f96f639256e11dd39c53f74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4803863
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1188988}

--- components/autofill/core/browser/metrics/autofill_metrics.cc
+++ components/autofill/core/browser/metrics/autofill_metrics.cc
@@ -1706,6 +1706,11 @@ void AutofillMetrics::LogAutofillFormCleared() {
 }
 
 // static
+void AutofillMetrics::LogAutofillUndo() {
+  base::RecordAction(base::UserMetricsAction("Autofill_UndoFilling"));
+}
+
+// static
 void AutofillMetrics::LogNumberOfEditedAutofilledFields(
     size_t num_edited_autofilled_fields,
     bool observed_submission) {
