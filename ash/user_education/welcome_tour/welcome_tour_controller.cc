400acc6875652a529ac5c43a83b8a9ea31ff9a5a
David Black <dmblack@google.com>
2023-08-28 18:23:24 +0000

    Suppress toasts while the Welcome Tour is in progress.
    Suppress toasts while the Welcome Tour is in progress. Toasts that are
    suppressed will not be restored when the Welcome Tour ends as toasts are
    tied to a specific context that has likely changed.
    
    Note that this CL also ensures that the Welcome Tour is aborted when Shell is destroying so that the tour does not outlive its dependencies.
    
    Bug: b:277091624
    Change-Id: Ibaa318a55546aefac929e1456f615a04bc278cc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4807375
    Reviewed-by: Angus McLean <angusmclean@google.com>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1189065}

--- ash/user_education/welcome_tour/welcome_tour_controller.cc
+++ ash/user_education/welcome_tour/welcome_tour_controller.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_element_identifiers.h"
 #include "ash/constants/ash_features.h"
 #include "ash/public/cpp/app_list/app_list_metrics.h"
+#include "ash/public/cpp/system/scoped_toast_pause.h"
+#include "ash/public/cpp/system/toast_manager.h"
 #include "ash/public/cpp/tablet_mode.h"
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
@@ -289,7 +291,7 @@ WelcomeTourController::GetTutorialDescriptions() {
 }
 
 void WelcomeTourController::OnAccessibilityControllerShutdown() {
-  accessibility_observation_.Reset();
+  MaybeAbortWelcomeTour(welcome_tour_metrics::AbortedReason::kShutdown);
 }
 
 void WelcomeTourController::OnAccessibilityStatusChanged() {
@@ -313,8 +315,12 @@ void WelcomeTourController::OnSessionStateChanged(
   MaybeStartWelcomeTour();
 }
 
+void WelcomeTourController::OnShellDestroying() {
+  MaybeAbortWelcomeTour(welcome_tour_metrics::AbortedReason::kShutdown);
+}
+
 void WelcomeTourController::OnTabletControllerDestroyed() {
-  tablet_mode_observation_.Reset();
+  MaybeAbortWelcomeTour(welcome_tour_metrics::AbortedReason::kShutdown);
 }
 
 void WelcomeTourController::OnTabletModeStarting() {
@@ -442,14 +448,13 @@ void WelcomeTourController::OnWelcomeTourStarted() {
       base::BindRepeating(&WelcomeTourController::MaybeAbortWelcomeTour,
                           weak_ptr_factory_.GetWeakPtr(),
                           welcome_tour_metrics::AbortedReason::kAccelerator));
-
   accessibility_observation_.Observe(Shell::Get()->accessibility_controller());
-
   notification_blocker_ = std::make_unique<WelcomeTourNotificationBlocker>();
   notification_blocker_->Init();
-
   scrim_ = std::make_unique<WelcomeTourScrim>();
+  shell_observation_.Observe(Shell::Get());
   tablet_mode_observation_.Observe(TabletMode::Get());
+  toast_pause_ = ToastManager::Get()->CreateScopedPause();
   window_minimizer_ = std::make_unique<WelcomeTourWindowMinimizer>();
 
   // NOTE: The accept button doesn't need to be explicitly handled because the
@@ -489,7 +494,9 @@ void WelcomeTourController::OnWelcomeTourEnded(
   accessibility_observation_.Reset();
   notification_blocker_.reset();
   scrim_.reset();
+  shell_observation_.Reset();
   tablet_mode_observation_.Reset();
+  toast_pause_.reset();
   window_minimizer_.reset();
 
   if (completed) {
--- ash/user_education/welcome_tour/welcome_tour_controller.h
+++ ash/user_education/welcome_tour/welcome_tour_controller.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/session/session_observer.h"
 #include "ash/public/cpp/tablet_mode.h"
 #include "ash/public/cpp/tablet_mode_observer.h"
+#include "ash/shell_observer.h"
 #include "ash/user_education/user_education_feature_controller.h"
 #include "ash/user_education/welcome_tour/welcome_tour_metrics.h"
 #include "base/memory/weak_ptr.h"
@@ -25,7 +26,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 class AccessibilityControllerImpl;
+class ScopedToastPause;
 class SessionController;
+class Shell;
 class WelcomeTourAcceleratorHandler;
 class WelcomeTourControllerObserver;
 class WelcomeTourNotificationBlocker;
@@ -38,6 +41,7 @@ class WelcomeTourWindowMinimizer;
 class ASH_EXPORT WelcomeTourController : public UserEducationFeatureController,
                                          public AccessibilityObserver,
                                          public SessionObserver,
+                                         public ShellObserver,
                                          public TabletModeObserver {
  public:
   WelcomeTourController();
@@ -70,6 +74,9 @@ class ASH_EXPORT WelcomeTourController : public UserEducationFeatureController,
   void OnChromeTerminating() override;
   void OnSessionStateChanged(session_manager::SessionState) override;
 
+  // ShellObserver:
+  void OnShellDestroying() override;
+
   // TabletModeObserver:
   void OnTabletControllerDestroyed() override;
   void OnTabletModeStarting() override;
@@ -100,6 +107,10 @@ class ASH_EXPORT WelcomeTourController : public UserEducationFeatureController,
   // The elapsed time since the beginning of the `current_step_`.
   base::ElapsedTimer current_step_timer_;
 
+  // Handles accelerator actions during the Welcome Tour. Exists only while the
+  // Welcome Tour is in progress.
+  std::unique_ptr<WelcomeTourAcceleratorHandler> accelerator_handler_;
+
   // Blocks all notifications while the Welcome Tour is in progress. Any
   // notifications received during the tour will appear in the Notification
   // Center after the tour is over.
@@ -110,9 +121,9 @@ class ASH_EXPORT WelcomeTourController : public UserEducationFeatureController,
   // is in progress.
   std::unique_ptr<WelcomeTourScrim> scrim_;
 
-  // Handles accelerator actions during the Welcome Tour. Created/destroyed when
-  // the Welcome Tour starts/ends.
-  std::unique_ptr<WelcomeTourAcceleratorHandler> accelerator_handler_;
+  // Suppresses all toasts during the Welcome Tour. Exists only while the
+  // Welcome Tour is in progress.
+  std::unique_ptr<ScopedToastPause> toast_pause_;
 
   // Minimizes any app windows that are visible at the start of the Welcome
   // Tour, and any that attempt to become visible during the tour. Exists only
@@ -131,6 +142,11 @@ class ASH_EXPORT WelcomeTourController : public UserEducationFeatureController,
   // the first time at which point the Welcome Tour is started.
   base::ScopedObservation<SessionController, SessionObserver>
       session_observation_{this};
+
+  // Shell is observed only while the Welcome Tour is in progress. The Welcome
+  // Tour is aborted when Shell is destroying to ensure the Welcome Tour does
+  // not outlive its dependencies.
+  base::ScopedObservation<Shell, ShellObserver> shell_observation_{this};
 
   // Tablet mode is observed only while the Welcome Tour is in progress, and
   // will trigger an abort of the tour if the device switches to tablet mode.
