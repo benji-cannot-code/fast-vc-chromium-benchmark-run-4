625835f8c5e31c9ddcdafc7ac67294c65ec16658
Ted Meyer <tmathmeyer@chromium.org>
2023-08-28 21:30:41 +0000

    Add builtin-hls chrome://flags entry
    R=dalecurtis
    
    Bug: 1266991
    Change-Id: I50a6c38b72fce12c68d27f16712a4302e2842022
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4818115
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189150}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2288,6 +2288,9 @@ extern const char kDrawEdgeToEdgeDescription[];
 extern const char kEnableAndroidGamepadVibrationName[];
 extern const char kEnableAndroidGamepadVibrationDescription[];
 
+extern const char kEnableBuiltinHlsName[];
+extern const char kEnableBuiltinHlsDescription[];
+
 extern const char kEnableCommandLineOnNonRootedName[];
 extern const char kEnableCommandLineOnNoRootedDescription[];
 
