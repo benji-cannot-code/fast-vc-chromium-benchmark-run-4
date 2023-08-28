c54efc8bb84c073b1a8e0f859519b876f05b6022
Erik Chen <erikchen@chromium.org>
2023-08-28 21:47:40 +0000

    Delete GetThemeBackgroundXInset.
    This CL simplified theme drawing on all platforms:
    https://chromium-review.googlesource.com/c/chromium/src/+/4809121.
    
    Once that CL landed, it became clear that GetThemeBackgroundXInset was
    no longer necessary.
    
    Change-Id: I2bf8eca017bf2d15a6cdae0b79d8ab94df12494a
    Bug: 1473538
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4815890
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189180}

--- chrome/browser/ui/views/frame/picture_in_picture_browser_frame_view.h
+++ chrome/browser/ui/views/frame/picture_in_picture_browser_frame_view.h
@@ -64,7 +64,6 @@ class PictureInPictureBrowserFrameView
   void LayoutWebAppWindowTitle(const gfx::Rect& available_space,
                                views::Label& window_title_label) const override;
   int GetTopInset(bool restored) const override;
-  int GetThemeBackgroundXInset() const override;
   void OnBrowserViewInitViewsComplete() override;
   void UpdateThrobber(bool running) override {}
   gfx::Rect GetBoundsForClientView() const override;
