c677a019f11247a82a601e721afadf27b4d1054d
Austin Sullivan <asully@chromium.org>
2023-08-28 16:37:35 +0000

    FSA: Add FileSystemObserver flag to chrome://flags
    Bug: 1019297
    Change-Id: I6a29657d14583a8510e418a2c5e3b6759ebb1e06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4652757
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189011}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -958,6 +958,9 @@ extern const char kWebIdentityMDocsDescription[];
 extern const char kFileHandlingIconsName[];
 extern const char kFileHandlingIconsDescription[];
 
+extern const char kFileSystemObserverName[];
+extern const char kFileSystemObserverDescription[];
+
 #if BUILDFLAG(IS_ANDROID)
 extern const char kFillingAcrossAffiliatedWebsitesName[];
 extern const char kFillingAcrossAffiliatedWebsitesDescription[];
