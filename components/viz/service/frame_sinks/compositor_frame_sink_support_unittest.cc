6b9ea458e753e9f88d080bdf95f39f7347e7fbb5
William Liu <liuwilliam@chromium.org>
2023-08-28 21:34:40 +0000

    [Default Nav Transition] Let surface pick up the exact CopyOutRequest.
    Before this change the arrived exact request will be queued up the
    CompositorFrameSinkSupport before it is picked up by the targeted
    surface. This CL makes sure that the exact request is picked up upon
    arrival.
    
    In a cross-renderer navigation, the old document will be unloaded in the
    old renderer, thus tearing down the old CompositorFrameSink. If we then
    submit a copy request, that request will have no
    CompositorFrameSinkSupport to go, even though the targeted surface is
    still alive.
    
    Bug: 1414172, 1465547
    Low-Coverage-Reason: the feature CL on top of this will have full tests.
    Change-Id: I56578c7bf82277661ed35c81b168606663827f6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4790841
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189157}

--- components/viz/service/frame_sinks/compositor_frame_sink_support_unittest.cc
+++ components/viz/service/frame_sinks/compositor_frame_sink_support_unittest.cc
@@ -1474,6 +1474,7 @@ TEST_F(CompositorFrameSinkSupportTest,
            CopyOutputRequest::ResultFormat::RGBA,
            CopyOutputRequest::ResultDestination::kSystemMemory,
            base::BindOnce(StubResultCallback))});
+  EXPECT_TRUE(surface_observer_.IsSurfaceDamaged(id1));
 
   // Send an exact CopyOutputRequest for Surface1. It can only be picked up by
   // Surface1.
@@ -1484,6 +1485,7 @@ TEST_F(CompositorFrameSinkSupportTest,
            CopyOutputRequest::ResultDestination::kSystemMemory,
            base::BindOnce(StubResultCallback)),
        /*capture_exact_id=*/true});
+  EXPECT_TRUE(surface_observer_.IsSurfaceDamaged(id2));
 
   // Surface2 picks up the non-exact CopyOutputRequest.
   GetSurfaceForId(id2)->TakeCopyOutputRequestsFromClient();
--- content/browser/renderer_host/render_widget_host_view_browsertest.cc
+++ content/browser/renderer_host/render_widget_host_view_browsertest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/task/current_thread.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_timeouts.h"
@@ -53,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/test_utils.h"
 #include "content/shell/browser/shell.h"
 #include "content/test/did_commit_navigation_interceptor.h"
+#include "content/test/render_document_feature.h"
 #include "net/base/filename_util.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/default_handlers.h"
@@ -1745,6 +1747,11 @@ class RenderWidgetHostViewCopyFromSurfaceBrowserTest
     } else {
       scoped_feature_list_.InitAndDisableFeature(features::kSlimCompositor);
     }
+
+    // Enable `RenderDocument` to guarantee renderer/RFH swap for cross-site
+    // navigations.
+    InitAndEnableRenderDocumentFeature(&scoped_feature_list_render_document_,
+                                       RenderDocumentFeatureFullyEnabled()[0]);
   }
 
   void SetUpOnMainThread() override {
@@ -1762,6 +1769,7 @@ class RenderWidgetHostViewCopyFromSurfaceBrowserTest
 
  private:
   base::test::ScopedFeatureList scoped_feature_list_;
+  base::test::ScopedFeatureList scoped_feature_list_render_document_;
 };
 
 IN_PROC_BROWSER_TEST_P(RenderWidgetHostViewCopyFromSurfaceBrowserTest,
@@ -1842,9 +1850,9 @@ class ScopedSnapshotWaiter : public WebContentsObserver {
     auto* request = NavigationRequest::From(handle);
     request->set_ready_to_commit_callback_for_testing(base::BindOnce(
         [](RenderWidgetHostView* old_view,
-           base::OnceCallback<bool()> is_same_proc_nav,
+           base::OnceCallback<bool()> renderer_swapped,
            base::RepeatingClosure resume) {
-          ASSERT_FALSE(std::move(is_same_proc_nav).Run());
+          ASSERT_TRUE(std::move(renderer_swapped).Run());
           ASSERT_TRUE(old_view);
           static_cast<RenderWidgetHostViewBase*>(old_view)
               ->CopyFromExactSurface(gfx::Rect(), gfx::Size(),
@@ -1853,8 +1861,14 @@ class ScopedSnapshotWaiter : public WebContentsObserver {
         },
         request->frame_tree_node()->current_frame_host()->GetView(),
         // The request must outlive its own callback.
-        base::BindOnce(&NavigationRequest::IsSameProcess,
-                       base::Unretained(request)),
+        base::BindOnce(
+            base::BindLambdaForTesting([](NavigationRequest* request) {
+              return request->GetRenderFrameHost() !=
+                     request->frame_tree_node()
+                         ->render_manager()
+                         ->current_frame_host();
+            }),
+            base::Unretained(request)),
         run_loop_.QuitClosure()));
   }
 
