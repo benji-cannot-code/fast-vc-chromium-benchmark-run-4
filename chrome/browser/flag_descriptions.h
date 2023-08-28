969e5423070dc4122a4fbe6f69926a6e75a3f078
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2023-08-28 23:10:42 +0000

    Revert "Add builtin-hls chrome://flags entry"
    This reverts commit 18f7cde5b756507960793ec6a2bbd80f3f4ab914.
    
    Reason for revert:
    LUCI Bisection identified this CL as the culprit of a build failure. See the analysis: https://luci-bisection.appspot.com/analysis/b/8771481792899525473
    
    Sample failed build: https://ci.chromium.org/b/8771481792899525473
    
    If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fluci-bisection.appspot.com%2Fanalysis%2Fb%2F8771481792899525473&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F4818115
    
    Original change's description:
    > Add builtin-hls chrome://flags entry
    >
    > R=dalecurtis
    >
    > Bug: 1266991
    > Change-Id: I50a6c38b72fce12c68d27f16712a4302e2842022
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4818115
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1189150}
    >
    
    Bug: 1266991
    Change-Id: Ica28b3b6d18fe28436c2a9e509ae1a9a3a044c2b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4819754
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1189236}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2288,9 +2288,6 @@ extern const char kDrawEdgeToEdgeDescription[];
 extern const char kEnableAndroidGamepadVibrationName[];
 extern const char kEnableAndroidGamepadVibrationDescription[];
 
-extern const char kEnableBuiltinHlsName[];
-extern const char kEnableBuiltinHlsDescription[];
-
 extern const char kEnableCommandLineOnNonRootedName[];
 extern const char kEnableCommandLineOnNoRootedDescription[];
 
