c8223a661defb6d10dc686b02da38bc641c769c0
Charles Hager <clhager@google.com>
2023-08-29 18:34:02 +0000

    Added MVC flow for Missing Device Lock
    Light mode: https://screenshot.googleplex.com/9AToEH7esH3LbrE.png
    Night mode: https://screenshot.googleplex.com/7saXPzbhEyEv8zq.png
    
    Bug: 1470704
    Change-Id: I67368c880cae517d65cf7ca450ef6bff62af63f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4819756
    Commit-Queue: Charles Hager <clhager@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1189643}

---
+++ chrome/browser/ui/android/device_lock/java/src/org/chromium/chrome/browser/ui/device_lock/MissingDeviceLockViewBinder.java
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ui.device_lock;
+
+import static org.chromium.chrome.browser.ui.device_lock.MissingDeviceLockProperties.ON_CHECKBOX_TOGGLED;
+import static org.chromium.chrome.browser.ui.device_lock.MissingDeviceLockProperties.ON_CONTINUE_CLICKED;
+import static org.chromium.chrome.browser.ui.device_lock.MissingDeviceLockProperties.ON_CREATE_DEVICE_LOCK_CLICKED;
+import static org.chromium.chrome.browser.ui.device_lock.MissingDeviceLockProperties.REMOVE_ALL_LOCAL_DATA_CHECKED;
+
+import org.chromium.base.ThreadUtils;
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel;
+
+/**
+ * Stateless Missing Device Lock page view binder.
+ */
+public class MissingDeviceLockViewBinder {
+    public static void bind(
+            PropertyModel model, MissingDeviceLockView view, PropertyKey propertyKey) {
+        if (propertyKey == ON_CREATE_DEVICE_LOCK_CLICKED) {
+            view.getCreateDeviceLockButton().setOnClickListener(
+                    model.get(ON_CREATE_DEVICE_LOCK_CLICKED));
+        } else if (propertyKey == ON_CONTINUE_CLICKED) {
+            view.getContinueButton().setOnClickListener(model.get(ON_CONTINUE_CLICKED));
+        } else if (propertyKey == ON_CHECKBOX_TOGGLED) {
+            view.getCheckbox().setOnCheckedChangeListener(model.get(ON_CHECKBOX_TOGGLED));
+        } else if (propertyKey == REMOVE_ALL_LOCAL_DATA_CHECKED) {
+            ThreadUtils.runOnUiThread(() -> {
+                view.getCheckbox().setChecked(model.get(REMOVE_ALL_LOCAL_DATA_CHECKED));
+            });
+        }
+    }
+}
