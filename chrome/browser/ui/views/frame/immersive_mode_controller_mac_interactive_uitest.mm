a06b8370f84c2e74057c1af5f20b3e9136073011
Leonard Grey <lgrey@chromium.org>
2023-08-29 19:01:16 +0000

    Mac immersive: don't adjust offset for find bar if always showing toolbar
    Bug: 1465631
    Change-Id: I5f7cc63bd74ad93a544fbd525b2a74dcc7bbffda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4818191
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189653}

--- chrome/browser/ui/views/frame/immersive_mode_controller_mac_interactive_uitest.mm
+++ chrome/browser/ui/views/frame/immersive_mode_controller_mac_interactive_uitest.mm
@@ -11,12 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/apple/foundation_util.h"
 #import "base/mac/mac_util.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/exclusive_access/exclusive_access_manager.h"
 #include "chrome/browser/ui/exclusive_access/exclusive_access_test.h"
 #include "chrome/browser/ui/find_bar/find_bar_host_unittest_util.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/common/chrome_features.h"
+#include "chrome/common/pref_names.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #import "components/remote_cocoa/app_shim/browser_native_widget_window_mac.h"
 #include "content/public/test/browser_test.h"
@@ -209,10 +211,16 @@ IN_PROC_BROWSER_TEST_F(ImmersiveModeControllerMacInteractiveTest,
   EXPECT_EQ([overlay_widget_window contentView], overlay_widget_content_view);
 }
 
-// Tests that minimum content offset is nonzero iff the find bar is shown.
+// Tests that minimum content offset is nonzero iff the find bar is shown and
+// "Always Show Toolbar in Full Screen" is off.
 IN_PROC_BROWSER_TEST_F(ImmersiveModeControllerMacInteractiveTest,
                        MinimumContentOffset) {
   chrome::DisableFindBarAnimationsDuringTesting(true);
+
+  PrefService* prefs = browser()->profile()->GetPrefs();
+  bool original_always_show = prefs->GetBoolean(prefs::kShowFullscreenToolbar);
+
+  prefs->SetBoolean(prefs::kShowFullscreenToolbar, false);
 
   BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser());
   ImmersiveModeController* controller =
@@ -220,14 +228,31 @@ IN_PROC_BROWSER_TEST_F(ImmersiveModeControllerMacInteractiveTest,
   controller->SetEnabled(true);
   EXPECT_EQ(controller->GetMinimumContentOffset(), 0);
 
-  views::NamedWidgetShownWaiter shown_waiter(
-      views::test::AnyWidgetTestPasskey{}, "DropdownBarHost");
-  chrome::Find(browser());
-  std::ignore = shown_waiter.WaitIfNeededAndGet();
-  EXPECT_GT(controller->GetMinimumContentOffset(), 0);
+  {
+    views::NamedWidgetShownWaiter shown_waiter(
+        views::test::AnyWidgetTestPasskey{}, "DropdownBarHost");
+    chrome::Find(browser());
+    std::ignore = shown_waiter.WaitIfNeededAndGet();
+    EXPECT_GT(controller->GetMinimumContentOffset(), 0);
+  }
 
   chrome::CloseFind(browser());
   EXPECT_EQ(controller->GetMinimumContentOffset(), 0);
+
+  // Now, with "Always Show..." on
+  prefs->SetBoolean(prefs::kShowFullscreenToolbar, true);
+  {
+    views::NamedWidgetShownWaiter shown_waiter(
+        views::test::AnyWidgetTestPasskey{}, "DropdownBarHost");
+    chrome::Find(browser());
+    std::ignore = shown_waiter.WaitIfNeededAndGet();
+    EXPECT_EQ(controller->GetMinimumContentOffset(), 0);
+  }
+  chrome::CloseFind(browser());
+  EXPECT_EQ(controller->GetMinimumContentOffset(), 0);
+
+  // Reset the pref.
+  prefs->SetBoolean(prefs::kShowFullscreenToolbar, original_always_show);
 
   chrome::DisableFindBarAnimationsDuringTesting(false);
 }
