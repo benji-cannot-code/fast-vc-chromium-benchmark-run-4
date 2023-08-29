6f9d5dae7a98e8bc873ac702b4d1d99b4a1a6434
Thiabaud Engelbrecht <thiabaud@google.com>
2023-08-29 19:37:15 +0000

    [ssm] Clean up usage of shared_ptr in NativeUnwinderAndroidMemoryRegionsMap
    This CL moves the shared_ptr<> representation of unwindstack::Memory
    into the NativeUnwinderAndroidMemoryRegionsMap subclass so that it can
    be used by both NativeUnwinderAndroid and LibunwindstackUnwinderAndroid.
    Use of shared_ptr<> is required by the libunwindstack API.
    
    Bug: 1475718
    Change-Id: Iab41e640467c9c8180adf53a7b389c13be161dd9
    No-Presubmit: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4809278
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1189669}

--- base/profiler/native_unwinder_android_memory_regions_map.h
+++ base/profiler/native_unwinder_android_memory_regions_map.h
@@ -6,13 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_PROFILER_NATIVE_UNWINDER_ANDROID_MEMORY_REGIONS_MAP_H_
 #define BASE_PROFILER_NATIVE_UNWINDER_ANDROID_MEMORY_REGIONS_MAP_H_
 
-#include <memory>
-
-namespace unwindstack {
-class Maps;
-class Memory;
-}  // namespace unwindstack
-
 namespace base {
 
 // NativeUnwinderAndroidMemoryRegionsMap is an opaque interface that hides
@@ -21,6 +14,9 @@ namespace base {
 // pass the underlying instances around without referencing libunwindstack.
 // NativeUnwinderAndroidMemoryRegionsMap's implementation must live in the
 // stack_unwinder dynamic feature module.
+//
+// Code within the dynamic feature module is expected to downcast to the
+// derived type to access the unwindstack types.
 class NativeUnwinderAndroidMemoryRegionsMap {
  public:
   NativeUnwinderAndroidMemoryRegionsMap() = default;
@@ -30,12 +26,6 @@ class NativeUnwinderAndroidMemoryRegionsMap {
       const NativeUnwinderAndroidMemoryRegionsMap&) = delete;
   NativeUnwinderAndroidMemoryRegionsMap& operator=(
       const NativeUnwinderAndroidMemoryRegionsMap&) = delete;
-
-  virtual unwindstack::Maps* GetMaps() = 0;
-  virtual unwindstack::Memory* GetMemory() = 0;
-  // This function exists to provide a method for
-  // `LibunwindstackUnwinderAndroid` to take the ownership of `Memory`.
-  virtual std::unique_ptr<unwindstack::Memory> TakeMemory() = 0;
 };
 
 }  // namespace base
