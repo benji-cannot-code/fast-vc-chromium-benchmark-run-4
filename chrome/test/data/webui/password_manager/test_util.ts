53e988b503b17fcabd1600aa3dcec866bffcc4e9
Andrii Natiahlyi <natiahlyi@google.com>
2023-08-29 13:16:07 +0000

    [Passwords] Add sharing confirmation description & footer
    This CL adds strings for the following scenarios:
    - Credential was shared with single or multiple recipients.
    - Shared credential has a change password url.
    - Shared credential has no change password url (only possible for
    android apps with no affiliated website).
    
    Impl:
    - https://screenshot.googleplex.com/7ZKQzyQMuLNgPQk.png
    - https://screenshot.googleplex.com/8tLpgSGo7eVVzuy.png
    Mocks: https://screenshot.googleplex.com/6VvAyxvybaPQm2c.png
    
    Bug: 1445526
    Change-Id: Ied8a8433554adc2c61873b55674b7774aadda71b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4816267
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Andrii Natiahlyi <natiahlyi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1189477}

--- chrome/test/data/webui/password_manager/test_util.ts
+++ chrome/test/data/webui/password_manager/test_util.ts
@@ -54,6 +54,7 @@ export interface PasswordEntryParams {
   inAccountStore?: boolean;
   inProfileStore?: boolean;
   note?: string;
+  changePasswordUrl?: string;
   affiliatedDomains?: chrome.passwordsPrivate.DomainInfo[];
 }
 
@@ -97,6 +98,7 @@ export function createPasswordEntry(params?: PasswordEntryParams):
     id: id,
     storedIn: storeType,
     note: note,
+    changePasswordUrl: params.changePasswordUrl,
     password: params.password || '',
     affiliatedDomains: params.affiliatedDomains || [domain],
   };
