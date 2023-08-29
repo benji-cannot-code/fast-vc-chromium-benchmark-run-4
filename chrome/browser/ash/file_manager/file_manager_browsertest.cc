ab6dcfc71114b576ac782de981818fd57dca4e07
Oleg Davydov <burunduk@chromium.org>
2023-08-29 09:10:18 +0000

    [file manager] Remove share tasks for encrypted files
    Typical share options like “Copy” or share with some apps need file
    contents to work with, which we don't have for CSE files. Therefore
    these sharing options should be filtered out.
    
    To test it the following approach was implemented:
     * The entry point is a typical file manager integration test, which is
       listed in `file_manager_browsertest.cc` and implemented in
       `integration_tests/file_manager/share.js`.
     * The test cannot poke into sharesheet widget by itself, so it makes a
       test call, named `getSharesheetInfo`.
     * The `getSharesheetInfo` (implemented in
       `file_manager_browsertest_base.cc`) finds the sharesheet widget
       (similarly how it's done in `sharesheet_bubble_view_browsertest.cc`),
       scans the targets and sends the list of label back to the test.
     * To find targets' labels thers views are marked with the special ID.
    
    Bug: b:295146383
    Change-Id: I1154f9994adff8c7617205dd6206c03f2cb2ebe0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4801357
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Oleg Davydov <burunduk@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Austin Tankiang <austinct@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189410}

--- chrome/browser/ash/file_manager/file_manager_browsertest.cc
+++ chrome/browser/ash/file_manager/file_manager_browsertest.cc
@@ -1639,6 +1639,11 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("checkEncryptedMoveEnabled")));
 
 WRAPPED_INSTANTIATE_TEST_SUITE_P(
+    Share, /* share.js */
+    FilesAppBrowserTest,
+    ::testing::Values(TestCase("checkEncryptedSharesheetOptions")));
+
+WRAPPED_INSTANTIATE_TEST_SUITE_P(
     Toolbar, /* toolbar.js */
     FilesAppBrowserTest,
     ::testing::Values(
--- chrome/browser/sharesheet/share_action/share_action.cc
+++ chrome/browser/sharesheet/share_action/share_action.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "components/drive/drive_api_util.h"
 #include "components/services/app_service/public/cpp/intent_util.h"
 #endif
 
@@ -20,6 +21,12 @@ bool ShareAction::HasActionView() {
 bool ShareAction::ShouldShowAction(const apps::IntentPtr& intent,
                                    bool contains_hosted_document) {
 #if BUILDFLAG(IS_CHROMEOS_ASH)
+  for (const auto& file : intent->files) {
+    if (file->mime_type &&
+        drive::util::IsEncryptedMimeType(file->mime_type.value())) {
+      return false;
+    }
+  }
   return !contains_hosted_document && intent && !intent->OnlyShareToDrive() &&
          intent->IsIntentValid();
 #else
