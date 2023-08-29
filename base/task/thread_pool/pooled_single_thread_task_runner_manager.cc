8d9d9d707aa0b4f8053d592907080003b0eaf039
S. Ganesh <ganesh@chromium.org>
2023-08-29 18:28:13 +0000

    Check the result of calling CoInitialize in `WorkerThreadCOMDelegate`
    We `CHECK` to make sure the worker COM thread is initialized correctly
    in an STA.
    
    Bug: 1475144, 1473487
    Change-Id: If599e542ebe59fbdae0443541dcca32f5df420df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4820346
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1189638}

--- base/task/thread_pool/pooled_single_thread_task_runner_manager.cc
+++ base/task/thread_pool/pooled_single_thread_task_runner_manager.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
+#include "base/check.h"
 #include "base/debug/leak_annotations.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
@@ -282,6 +283,9 @@ class WorkerThreadCOMDelegate : public WorkerThreadDelegate {
     WorkerThreadDelegate::OnMainEntry(worker);
 
     scoped_com_initializer_ = std::make_unique<win::ScopedCOMInitializer>();
+
+    // CHECK to make sure this COM thread is initialized correctly in an STA.
+    CHECK(scoped_com_initializer_->Succeeded());
   }
 
   RegisteredTaskSource GetWork(WorkerThread* worker) override {
