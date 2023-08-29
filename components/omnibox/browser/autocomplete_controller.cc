1e7984c1492e0f3b645ce261c973221b41f244a0
Orin Jaworski <orinj@google.com>
2023-08-29 16:59:12 +0000

    [omnibox][actions] Implement action to suggestion splitting for pedals.
    This CL converts pedal actions to dedicated matches in the autocomplete
    result set. The new match uses information from the pedal to update
    match text and vector icon. The previously deprecated pedal match type
    is restored.
    
    Screenshots:
    
    --disable-features=OmniboxActionsUISimplification
    http://screen/3PPFmhB5n9j3AyA
    
    --enable-features=OmniboxActionsUISimplification
    http://screen/79p8zRStm3NhwvZ
    
    Bug: 1475065
    Change-Id: I9efec73e49c70e27a8ab8da69a890a471e3d49db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4808940
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Khalid Peer <khalidpeer@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1189582}

--- components/omnibox/browser/autocomplete_controller.cc
+++ components/omnibox/browser/autocomplete_controller.cc
@@ -1212,6 +1212,12 @@ void AutocompleteController::AttachActions() {
 #endif
   }
   result_.TrimOmniboxActions(input_.IsZeroSuggest());
+
+  if (OmniboxFieldTrial::IsActionsUISimplificationEnabled()) {
+    result_.SplitActionsToSuggestions();
+    // TODO(crbug.com/1473162): Trim result length again;
+    //  may require refactoring SortAndCull.
+  }
 }
 
 void AutocompleteController::UpdateAssociatedKeywords(
