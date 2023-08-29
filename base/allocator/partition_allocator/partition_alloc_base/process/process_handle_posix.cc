f9b2285d9d3fff96e0b6226b670431676cb48a30
Takashi Sakamoto <tasak@google.com>
2023-08-29 00:48:46 +0000

    [PA] Make StackTrace available inside PartitionAlloc.
    PS4 = copy //base/debug/stack_trace* (except fuchsia and nacl) and
     //base/process/proces_handle{.h,_posix.cc,_win.cc}.
    
    Bug: 1293552
    
    Change-Id: I4709b4e8b74fad7b9ad835d172f8db2896f0482a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4807184
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189283}

---
+++ base/allocator/partition_allocator/partition_alloc_base/process/process_handle_posix.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/allocator/partition_allocator/partition_alloc_base/process/process_handle.h"
+
+#include <unistd.h>
+
+namespace partition_alloc::internal::base {
+
+ProcessId GetCurrentProcId() {
+  return getpid();
+}
+
+}  // namespace partition_alloc::internal::base
