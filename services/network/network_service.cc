7a63e4066dc6d01b343d799ebccdbc72dfed22e0
Liza Burakova <liza@chromium.org>
2023-08-29 20:04:43 +0000

    Adding a SandboxedVFS Delegate to the network service.
    This change adds a SandboxedVFSDelegate class that is necessary for
    brokering opening files when the network service is sandboxed on Android. The delegate will own a mojo remote to a new
    NetworkFileBroker that will be created in a follow up CL.
    
    This CL simply introduces the SandboxedVFSDelegate, and also introduces how the delegate will invalidate filepaths once a network context is
    destroyed. The invalidation will be necessary as the browser will
    be tracking what filepaths the network service can open, and therefore
    will also need to track when those filepaths are no longer associated
    with an existing network context.
    
    Bug: 1333558
    Change-Id: I60237a8828de280cc72824887aeadedc2bfcda64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4646410
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Liza Burakova <liza@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189698}

--- services/network/network_service.cc
+++ services/network/network_service.cc
@@ -110,6 +110,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_ANDROID)
 #include "base/android/application_status_listener.h"
 #include "net/android/http_auth_negotiate_android.h"
+#include "services/network/sandboxed_vfs_delegate.h"
+#include "sql/database.h"
+#include "sql/sandboxed_vfs.h"
 #endif
 
 #if BUILDFLAG(IS_CT_SUPPORTED)
@@ -127,6 +130,10 @@ namespace network {
 namespace {
 
 NetworkService* g_network_service = nullptr;
+
+#if BUILDFLAG(IS_ANDROID)
+constexpr char kSandboxedVfsName[] = "network_service";
+#endif
 
 std::unique_ptr<net::NetworkChangeNotifier> CreateNetworkChangeNotifierIfNeeded(
     net::NetworkChangeNotifier::ConnectionType initial_connection_type,
@@ -596,6 +603,14 @@ void NetworkService::DeregisterNetworkContext(NetworkContext* network_context) {
   network_contexts_.erase(network_context);
 }
 
+#if BUILDFLAG(IS_ANDROID)
+void NetworkService::InvalidateNetworkContextPath(const base::FilePath& path) {
+  if (sandboxed_vfs_delegate_ptr_) {
+    sandboxed_vfs_delegate_ptr_->InvalidateFileBrokerPath(path);
+  }
+}
+#endif  // BUILDFLAG(IS_ANDROID)
+
 void NetworkService::CreateNetLogEntriesForActiveObjects(
     net::NetLog::ThreadSafeObserver* observer) {
   std::set<net::URLRequestContext*> contexts;
@@ -823,6 +838,16 @@ void NetworkService::OnApplicationStateChange(
       listener->Notify(state);
     }
   }
+}
+
+void NetworkService::SetSandboxedVFS() {
+  // TODO(crbug.com/1117049): stop disabling mmaping by default once
+  // sql::Database is more configurable.
+  sql::Database::DisableMmapByDefault();
+  auto delegate = std::make_unique<SandboxedVfsDelegate>();
+  sandboxed_vfs_delegate_ptr_ = delegate.get();
+  sql::SandboxedVfs::Register(kSandboxedVfsName, std::move(delegate),
+                              /*make_default=*/true);
 }
 #endif
 
