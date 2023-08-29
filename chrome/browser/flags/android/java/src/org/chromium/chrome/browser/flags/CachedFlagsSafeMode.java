2f0db345f0d130191bad867c80b3c178a7114ae2
Henrique Nakashima <hnakashima@chromium.org>
2023-08-29 21:11:55 +0000

    [Android] Do not enter Safe Mode for Cached Flags in tests
    Unless the test specifically tests Safe Mode, prevent Safe Mode from
    engaging in test environments.
    
    In batched tests, Safe Mode might engage after the first
    failures/crashes resulting in unexpected flag behavior.
    
    Change-Id: Ice6272069a993912905d1d81db0903ed8de9770e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4822691
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189748}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/CachedFlagsSafeMode.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/CachedFlagsSafeMode.java
@@ -19,6 +19,7 @@ import org.chromium.base.ResettersForTesting;
 import org.chromium.base.TraceEvent;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.task.AsyncTask;
+import org.chromium.build.BuildConfig;
 import org.chromium.chrome.browser.preferences.ChromePreferenceKeys;
 import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
 import org.chromium.components.version_info.VersionInfo;
@@ -205,6 +206,12 @@ public class CachedFlagsSafeMode {
     }
 
     private boolean shouldEnterSafeMode() {
+        if (BuildConfig.IS_FOR_TEST
+                && (mSafeModeExperimentForcedForTesting == null
+                        || !mSafeModeExperimentForcedForTesting)) {
+            return false;
+        }
+
         int safeModeRunsLeft = SharedPreferencesManager.getInstance().readInt(
                 ChromePreferenceKeys.FLAGS_SAFE_MODE_RUNS_LEFT, 0);
         assert safeModeRunsLeft <= 2;
