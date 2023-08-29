1690525cef14f31fbe1116ac2612745209574733
David Jean <djean@chromium.org>
2023-08-29 08:44:20 +0000

    [ios] Add metadata to extracted text
    Part of second attempt at sharing extracted text between observers
    (like intent detection and language detection)
    
    Bug: 1350973
    Change-Id: I0c73ba0c43e5a11e5caf081a4fdab5fdc1348570
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4804049
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189397}

--- ios/web/annotations/resources/annotations.ts
+++ ios/web/annotations/resources/annotations.ts
@@ -111,7 +111,11 @@ class MutationsDuringClickTracker {
   }
 }
 
-function areIntentsDisabled() {
+/**
+ * Searches page elements for "nointentdetection" meta tag. Returns true if
+ * "nointentdetection" meta tag is defined.
+ */
+function hasNoIntentDetection() {
   const metas = document.getElementsByTagName('meta');
   for (let i = 0; i < metas.length; i++) {
     if (metas[i]!.getAttribute('name') === 'chrome' &&
@@ -121,6 +125,35 @@ function areIntentsDisabled() {
   }
 
   return false;
+}
+
+/**
+ * Searches page elements for "notranslate" meta tag. Returns true if
+ * "notranslate" meta tag is defined.
+ */
+function hasNoTranslate(): boolean {
+  const metas = document.getElementsByTagName('meta');
+  for (let i = 0; i < metas.length; i++) {
+    if (metas[i]!.getAttribute('name') === 'google' &&
+        metas[i]!.getAttribute('content') === 'notranslate') {
+      return true;
+    }
+  }
+  return false;
+}
+
+/**
+ * Gets the content of a meta tag by httpEquiv for `httpEquiv`. The function is
+ * case insensitive.
+ */
+function getMetaContentByHttpEquiv(httpEquiv: string) {
+  const metaTags = document.getElementsByTagName('meta');
+  for (let metaTag of metaTags) {
+    if (metaTag.httpEquiv.toLowerCase() === httpEquiv) {
+      return metaTag.content;
+    }
+  }
+  return '';
 }
 
 const highlightTextColor = "#000";
@@ -158,6 +191,12 @@ function extractText(maxChars: number, seqId: number): void {
     command: 'annotations.extractedText',
     text: getPageText(maxChars),
     seqId: seqId,
+    metadata: {
+      hasNoIntentDetection: hasNoIntentDetection(),
+      hasNoTranslate: hasNoTranslate(),
+      htmlLang: document.documentElement.lang,
+      httpContentLanguage: getMetaContentByHttpEquiv('content-language'),
+    },
   });
 }
 
@@ -462,9 +501,6 @@ function enumerateSectionsNodes(process: EnumNodesFunction): void {
  * @param maxChars - maximum number of characters to parse out.
  */
 function getPageText(maxChars: number): string {
-  if (areIntentsDisabled()) {
-    return '';
-  }
   const parts: string[] = [];
   sections = [];
   enumerateTextNodes(document.body, function(node, index, text) {
