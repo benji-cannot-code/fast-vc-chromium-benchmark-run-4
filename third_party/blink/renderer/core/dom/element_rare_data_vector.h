f4c75e4486b1c97054699e49fd2aa46a05b34dab
Elad Alon <eladalon@chromium.org>
2023-08-29 19:03:25 +0000

    [Element Capture][RestrictionTarget] Extend ElementRareDataVector
    ElementRareDataVector currently holds kRegionCaptureCropId,
    which is a the UUID backing the CropTarget which might be
    associated with an Element.
    
    We add kRestrictionTargetId, a separate UUID backing
    RestrictionTargets associated with the element.
    
    Assume document D minted one CropTarget and one RestrictionTarget for
    a given target-element. Assume that D then posted those tokens to two
    separate documents, D_REGION_CAPTURE and D_ELEMENT_CAPTURE.
    Each token represents the permission to mutate tab-capture of
    the current tab into a capture of a portion of the tab,
    but each token allows a different mutation.
    
    Our use of two separate UUIDs for two separate token types,
    even when they back the same element, ensures that
    a compromised renderer cannot convert between these tokens,
    and gain access to capabilities that the minting document
    did not intend to allow.
    
    Bug: 1418194
    Change-Id: Ib6e00616cd45d14a75ec0789574249bce3dd57b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4817950
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189654}

--- third_party/blink/renderer/core/dom/element_rare_data_vector.h
+++ third_party/blink/renderer/core/dom/element_rare_data_vector.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/pseudo_element_data.h"
 #include "third_party/blink/renderer/platform/heap/trace_traits.h"
 #include "third_party/blink/renderer/platform/region_capture_crop_id.h"
+#include "third_party/blink/renderer/platform/restriction_target_id.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 
 namespace blink {
@@ -95,8 +96,9 @@ class CORE_EXPORT ElementRareDataVector final : public NodeRareData {
     kImplicitlyAnchoredElementCount = 27,
     kLastRememberedBlockSize = 28,
     kLastRememberedInlineSize = 29,
+    kRestrictionTargetId = 30,
 
-    kNumFields = 30,
+    kNumFields = 31,
   };
 
   ElementRareDataField* GetField(FieldId field_id) const;
@@ -256,6 +258,15 @@ class CORE_EXPORT ElementRareDataVector final : public NodeRareData {
   // Sets a crop-ID on the item. Must be called at most once. Cannot be used
   // to unset a previously set crop-ID.
   void SetRegionCaptureCropId(std::unique_ptr<RegionCaptureCropId> crop_id);
+
+  // Returns the ID backing a RestrictionTarget if one was set on the Element,
+  // or nullptr otherwise.
+  const RestrictionTargetId* GetRestrictionTargetId() const;
+  // Returns the ID backing a RestrictionTarget if one was set on the Element,
+  // or nullptr otherwise.
+  // Sets an ID backing a RestrictionTarget associated with the Element.
+  // Must be called at most once. Cannot be used to unset a previously set IDs.
+  void SetRestrictionTargetId(std::unique_ptr<RestrictionTargetId> id);
 
   using ResizeObserverDataMap =
       HeapHashMap<Member<ResizeObserver>, Member<ResizeObservation>>;
