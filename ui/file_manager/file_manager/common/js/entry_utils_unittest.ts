caf9b09fd9335333e87a69c032ca65c5e3a36d58
Marcello Salomao <msalomao@google.com>
2023-08-29 07:52:23 +0000

    Reland "[Files app] Ducks: reducer migration: all entries"
    This is a reland of commit 09289cb51d8233d5b7f43d7614b89fb3f6a28b22
    
    The underlying issue was fixed in https://crrev.com/c/4820730
    
    No additional changes were added to this CL. It's a clean reland.
    
    Original change's description:
    > [Files app] Ducks: reducer migration: all entries
    >
    > Bug: b:295777015
    > Test: CQ passes
    > Change-Id: I44e4f2654a0677d0605e0e8e50c938917c3ca8fd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4812677
    > Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    > Auto-Submit: Marcello Salomao <msalomao@google.com>
    > Commit-Queue: Marcello Salomao <msalomao@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1188878}
    
    Bug: b:295777015
    Change-Id: Idf164f2b5f48929f689b70d11772013ceb5cdd78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4820988
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Marcello Salomao <msalomao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1189383}

--- ui/file_manager/file_manager/common/js/entry_utils_unittest.ts
+++ ui/file_manager/file_manager/common/js/entry_utils_unittest.ts
@@ -3,10 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// TODO(b/296792757)
+import '../../state/store.js';
+
 import {assertFalse, assertTrue} from 'chrome://webui-test/chromeos/chai_assert.js';
 
+import {convertEntryToFileData} from '../../state/ducks/all_entries.js';
 import {setUpFileManagerOnWindow} from '../../state/for_tests.js';
-import {convertEntryToFileData} from '../../state/reducers/all_entries.js';
 
 import {isEntryInsideDrive, isEntryInsideMyDrive} from './entry_utils.js';
 import {EntryList} from './files_app_entry_types.js';
