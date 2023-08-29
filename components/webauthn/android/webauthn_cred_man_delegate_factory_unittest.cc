99a1900169f6d5e7fc298c91dbf82d944a297992
Adem Derinel <derinel@google.com>
2023-08-29 06:08:47 +0000

    [WebAuthn][Android][CodeHealth] Remove unused includes
    Bug: 1464469
    Change-Id: I1f03889c8bf00159623e3dbdf2e18fe1feff5f2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4813792
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1189367}

--- components/webauthn/android/webauthn_cred_man_delegate_factory_unittest.cc
+++ components/webauthn/android/webauthn_cred_man_delegate_factory_unittest.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/webauthn/android/webauthn_cred_man_delegate_factory.h"
 
-#include <memory>
-
 #include "base/memory/raw_ptr.h"
 #include "components/webauthn/android/webauthn_cred_man_delegate.h"
 #include "content/public/browser/render_frame_host.h"
