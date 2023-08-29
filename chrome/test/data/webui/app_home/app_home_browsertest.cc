c7bdd9a6f26c7a9653b433b162cb52970d6f77e7
Dibyajyoti Pal <dibyapal@google.com>
2023-08-29 17:12:17 +0000

    [dPWA/App Home] Remove App Home flags
    This CL removes the App Home feature flags and cleans up any usage of
    that flag anywhere.
    A follow-up CL will clean up the AppLauncherHandler logic since
    that is now dead code, and was messy to clean up without knowing how
    much of that relies on new tab page logic.
    
    Bug: 1350406
    Change-Id: I7ebe3c3d93287dc938e4dbebd968859b5fb1873a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4814001
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189589}

--- chrome/test/data/webui/app_home/app_home_browsertest.cc
+++ chrome/test/data/webui/app_home/app_home_browsertest.cc
@@ -12,10 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class AppHomeTest : public WebUIMochaBrowserTest {
  protected:
   AppHomeTest() { set_test_loader_host(chrome::kChromeUIAppLauncherPageHost); }
-
- private:
-  base::test::ScopedFeatureList scoped_feature_list_{
-      features::kDesktopPWAsAppHomePage};
 };
 
 IN_PROC_BROWSER_TEST_F(AppHomeTest, AppList) {
