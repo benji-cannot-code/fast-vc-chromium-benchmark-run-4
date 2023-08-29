3eae3cef314873511fd376d18df2fda82c7881ea
junwei <junwei.fu@intel.com>
2023-08-29 12:29:42 +0000

    WebNN: Define conv2d operator in mojo
    The conv2d [1] is to compute a 2-D convolution given 4-D input and filter tensors.
    
    Validate the conv2d operator with converting the attributes from mojo
    to component library type and add unit tests to verify to the graph
    from renderer process.
    
    [1] https://www.w3.org/TR/webnn/#api-mlgraphbuilder-conv2d
    
    Bug: 1273291
    Change-Id: Ibf0ef309a1b4e1de575669093fd480bef5c57ba3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4653303
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Commit-Queue: Junwei Fu <junwei.fu@intel.com>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189467}

--- third_party/blink/renderer/modules/ml/webnn/ml_graph_type_converter.h
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_type_converter.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_ML_WEBNN_ML_GRAPH_TYPE_CONVERTER_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_ML_WEBNN_ML_GRAPH_TYPE_CONVERTER_H_
 
+#include "base/types/expected.h"
 #include "mojo/public/cpp/bindings/type_converter.h"
 #include "services/webnn/public/mojom/webnn_graph.mojom-blink.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_map.h"
@@ -14,7 +15,7 @@ namespace blink {
 
 class MLOperand;
 class MLOperator;
-webnn::mojom::blink::OperatorPtr ConvertToMojoOperator(
+base::expected<webnn::mojom::blink::OperatorPtr, String> ConvertToMojoOperator(
     const HeapHashMap<Member<const MLOperand>, uint64_t>& operand_to_id_map,
     const MLOperator* op);
 
