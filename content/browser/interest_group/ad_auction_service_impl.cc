ccd0b1c5e132a7eaefb0bc7adce97a0898e89a26
Xiaochen Zhou <xiaochenzh@chromium.org>
2023-08-29 13:38:38 +0000

    FLEDGE: Fix dangling unretained pointers in OnAuctionComplete.
    Changed to
    1. use global ID for render frame host.
    2. use weak pointers for PageImpl.
    
    Bug: 1474654
    Change-Id: Ifedb92a56600f3143fbf570fd6224374688bad38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4802963
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Xiaochen Zhou <xiaochenzh@chromium.org>
    Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189484}

--- content/browser/interest_group/ad_auction_service_impl.cc
+++ content/browser/interest_group/ad_auction_service_impl.cc
@@ -345,8 +345,8 @@ void AdAuctionServiceImpl::RunAdAuction(
       base::BindOnce(
           &AdAuctionServiceImpl::OnAuctionComplete, base::Unretained(this),
           std::move(callback), std::move(urn_uuid.value()),
-          fenced_frame_urls_map.unique_id(), base::Unretained(GetFrame()),
-          base::Unretained(&(GetFrame()->GetPage()))));
+          fenced_frame_urls_map.unique_id(), GetFrame()->GetGlobalId(),
+          GetFrame()->GetPage().GetWeakPtrImpl()));
   AuctionRunner* raw_auction = auction.get();
   auctions_.emplace(raw_auction, std::move(auction));
 }
@@ -646,8 +646,8 @@ void AdAuctionServiceImpl::OnAuctionComplete(
     RunAdAuctionCallback callback,
     GURL urn_uuid,
     FencedFrameURLMapping::Id fenced_frame_urls_map_id,
-    const RenderFrameHostImpl* render_frame_host_impl,
-    const PageImpl* page_impl,
+    GlobalRenderFrameHostId render_frame_host_id,
+    const base::WeakPtr<PageImpl> page_impl,
     AuctionRunner* auction,
     bool manually_aborted,
     absl::optional<blink::InterestGroupKey> winning_group_key,
@@ -723,10 +723,10 @@ void AdAuctionServiceImpl::OnAuctionComplete(
   // Each possible scenario is checked below. They are put in separate if branch
   // in order to identify from the dump.
   bool mismatch_with_auction_start = false;
-  if (render_frame_host_impl != GetFrame()) {
+  if (render_frame_host_id != GetFrame()->GetGlobalId()) {
     base::debug::DumpWithoutCrashing();
     mismatch_with_auction_start = true;
-  } else if (page_impl != &(GetFrame()->GetPage())) {
+  } else if (page_impl.get() != &(GetFrame()->GetPage())) {
     base::debug::DumpWithoutCrashing();
     mismatch_with_auction_start = true;
   } else if (fenced_frame_urls_map_id !=
