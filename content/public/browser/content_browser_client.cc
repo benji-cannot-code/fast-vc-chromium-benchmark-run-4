e9e63e44a619d6dbaeb78cf76f1d52d0fe7fc2e5
Jonathan Hao <phao@chromium.org>
2023-08-29 10:51:52 +0000

    [Private Network Access] Add ability to force enable
    Renaming ShouldAllowInsecurePrivateNetworkRequests to
    ShouldOverridePrivateNetworkRequestPolicy and have it return an enum
    value so we can override it to either kAllow, kPreflightBlock, or not
    override (kDefault)
    
    Bug: 1469923
    Change-Id: I9366c06f864035055b8f903ba405be52d9779396
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4797774
    Commit-Queue: Jonathan Hao <phao@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Auto-Submit: Jonathan Hao <phao@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Vigen Issahhanjan <vigeni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1189443}

--- content/public/browser/content_browser_client.cc
+++ content/public/browser/content_browser_client.cc
@@ -1392,10 +1392,11 @@ bool ContentBrowserClient::ShouldServiceWorkerInheritPolicyContainerFromCreator(
   return url.SchemeIsLocal();
 }
 
-bool ContentBrowserClient::ShouldAllowInsecurePrivateNetworkRequests(
+ContentBrowserClient::PrivateNetworkRequestPolicyOverride
+ContentBrowserClient::ShouldOverridePrivateNetworkRequestPolicy(
     BrowserContext* browser_context,
     const url::Origin& origin) {
-  return false;
+  return PrivateNetworkRequestPolicyOverride::kDefault;
 }
 
 bool ContentBrowserClient::IsJitDisabledForSite(BrowserContext* browser_context,
