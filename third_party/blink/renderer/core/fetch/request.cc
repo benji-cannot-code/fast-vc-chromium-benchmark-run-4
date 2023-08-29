9fccc3206f743697526d323fa8aa2ba3c982d545
Camillia Smith Barnes <cammie@chromium.org>
2023-08-29 18:29:09 +0000

    Shared Storage: Add UseCounter for response headers
    We add three `UseCounter` values for the three currently implemented
    code paths for writing to Shared Storage via response headers:
    fetch, image elements, and iframe elements.
    
    Bug: 1434529,1218540
    Change-Id: Ie12e0fff9ce69d087a8a294d2cf6553212270137
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4818051
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189639}

--- third_party/blink/renderer/core/fetch/request.cc
+++ third_party/blink/renderer/core/fetch/request.cc
@@ -573,6 +573,11 @@ Request* Request::CreateRequestWithRequestOrString(
       return nullptr;
     }
     request->SetSharedStorageWritable(init->sharedStorageWritable());
+    if (init->sharedStorageWritable()) {
+      UseCounter::Count(
+          execution_context,
+          mojom::blink::WebFeature::kSharedStorageAPI_Fetch_Attribute);
+    }
   }
 
   // "If |init|'s method member is present, let |method| be it and run these
