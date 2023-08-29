84d416ce36483932c9f3cbac79928eb58958918b
Adam Langley <agl@chromium.org>
2023-08-29 17:40:59 +0000

    webauthn: add enterprise policy for iCloud Keychain
    See go/rzghs for background.
    
    Bug: 1439987
    Change-Id: I02c5a02ad6814ab23e57d974b3ddd2af002fe3da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4811711
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Victor Gabriel Savu <vsavu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1189605}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -1925,6 +1925,9 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
   { key::kScreenTimeEnabled,
     policy_prefs::kScreenTimeEnabled,
     base::Value::Type::BOOLEAN},
+  { key::kCreatePasskeysInICloudKeychain,
+    prefs::kCreatePasskeysInICloudKeychain,
+    base::Value::Type::BOOLEAN },
 #endif
   { key::kAccessControlAllowMethodsInCORSPreflightSpecConformant,
     prefs::kAccessControlAllowMethodsInCORSPreflightSpecConformant,
