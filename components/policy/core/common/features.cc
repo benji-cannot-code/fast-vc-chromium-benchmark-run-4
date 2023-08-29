c8724fe8b27ff076a09908da3aa8e89c8e0559e3
Igor Ruvinov <igorruvinov@chromium.org>
2023-08-29 15:59:52 +0000

    Enable SafeSitesFilterBehaviorPolicyAndroid by default.
    The policy is ramped to 1% on Stable and there are no major changes to
    the guiding metrics:
    http://uma/p/chrome/variations?sid=c005c656b3b785c765c106178ff3a42c
    
    Bug: b/251892586, b/251901337
    Change-Id: I4652321a94e4fd0c7e7a2e6c96a8d4eb2050bc8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4822688
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Commit-Queue: Anqing Zhao <anqing@chromium.org>
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Auto-Submit: Igor Ruvinov <igorruvinov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189547}

--- components/policy/core/common/features.cc
+++ components/policy/core/common/features.cc
@@ -30,7 +30,7 @@ BASE_FEATURE(kPolicyLogsPageAndroid,
 
 BASE_FEATURE(kSafeSitesFilterBehaviorPolicyAndroid,
              "SafeSitesFilterBehaviorPolicyAndroid",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_ANDROID)
 
 #if BUILDFLAG(IS_IOS)
