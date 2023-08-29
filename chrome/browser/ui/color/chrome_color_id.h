ff13453c2484b5c3b45f99c3a18dcc5a47a903ce
Keren Zhu <kerenzhu@chromium.org>
2023-08-29 15:01:24 +0000

    Align inactive bookmark text color with the toolbar buttons
    Bookmark text was using the default LabelButton text color which is
    unaware of the theme color and might cause low contrast issue when the
    browser is inactive on mac. This is seen on both CR2023 and non-CR2023.
    
    Fix it by aligning the disabled (used for inactive) bookmark text color
    with the toolbar buttons.
    
    Bug: 1474997
    Change-Id: I08fe260ef57e726dae69d5dfecf8ce26999b24c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4818190
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189513}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   /* Bookmark bar colors. */ \
   E_CPONLY(kColorBookmarkBarBackground) \
   E_CPONLY(kColorBookmarkBarForeground) \
+  E_CPONLY(kColorBookmarkBarForegroundDisabled) \
   E_CPONLY(kColorBookmarkBarSeparator) \
   E_CPONLY(kColorBookmarkBarSeparatorChromeRefresh) \
   E_CPONLY(kColorBookmarkButtonIcon) \
