a04a2dae18698fb185ffdce694613f56f76917c4
Alexander Cooper <alcooper@chromium.org>
2023-08-29 05:14:35 +0000

    Fix Cardboard JNI Registration
    When building with splits, the QrCode scanner throws an error when
    trying to talk back to the native code, since the native code exists in
    a different split than the java code. There were a few build errors that
    were encountered when trying to generate this registration as part of
    the build, and as such a registration header was manually generated.
    
    Bug: 1476661
    Change-Id: Icb57b07a6e45a029eb7f2ceda53e72d4f765d6b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4818850
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189352}

--- device/vr/android/cardboard/cardboard_sdk_impl.cc
+++ device/vr/android/cardboard/cardboard_sdk_impl.cc
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/jni_android.h"
 #include "third_party/cardboard/src/sdk/include/cardboard.h"
+#include "third_party/cardboard/src_overrides/sdk/jni_utils/android/jni_registration.h"
+
+using base::android::AttachCurrentThread;
 
 namespace device {
 
@@ -14,10 +17,24 @@ CardboardSdkImpl::CardboardSdkImpl() = default;
 CardboardSdkImpl::~CardboardSdkImpl() = default;
 
 void CardboardSdkImpl::Initialize(jobject context) {
+  if (initialized_) {
+    return;
+  }
+
   Cardboard_initializeAndroid(base::android::GetVM(), context);
+
+  // TODO(https://crbug.com/1476661): Cardboard's jni_utils initializeAndroid
+  // does not seem to get triggered, and at present this isn't worth adding an
+  // override to the QrCode initializeAndroid just for this, but consider
+  // finding it a better home.
+  cardboard::RegisterNatives(AttachCurrentThread());
+
+  initialized_ = true;
 }
 
 void CardboardSdkImpl::SwitchViewer() {
+  CHECK(initialized_);
+
   // Launches a new QR code scanner activity in order to scan a QR code with
   // the parameters of a new Cardboard viewer. Whether the QR code with
   // Cardboard viewer paramerter is scanned or the scanning is skipped, the
