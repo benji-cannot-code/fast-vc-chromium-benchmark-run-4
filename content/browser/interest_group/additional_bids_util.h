2c9f437e59e3afb49593e7cd29f246e48e98111e
Maks Orlovich <morlovich@chromium.org>
2023-08-29 00:05:56 +0000

    FLEDGE: Signed additional bid decoder
    Bug: 1464874
    Change-Id: I0bc7c85627f84b8ef28bc79a0f95e637d6f28cb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4812105
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189268}

--- content/browser/interest_group/additional_bids_util.h
+++ content/browser/interest_group/additional_bids_util.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_INTEREST_GROUP_ADDITIONAL_BIDS_UTIL_H_
 #define CONTENT_BROWSER_INTEREST_GROUP_ADDITIONAL_BIDS_UTIL_H_
 
+#include <stdint.h>
+
+#include <array>
 #include <string>
 #include <vector>
 
@@ -63,6 +66,29 @@ DecodeAdditionalBid(InterestGroupAuction* auction,
                     const base::Uuid& auction_nonce,
                     const url::Origin& seller,
                     base::optional_ref<const url::Origin> top_level_seller);
+
+struct CONTENT_EXPORT SignedAdditionalBid {
+  struct Signature {
+    std::array<uint8_t, 32> key;
+    std::array<uint8_t, 64> signature;
+  };
+
+  SignedAdditionalBid();
+  SignedAdditionalBid(const SignedAdditionalBid& other) = delete;
+  SignedAdditionalBid(SignedAdditionalBid&& other);
+  ~SignedAdditionalBid();
+
+  SignedAdditionalBid& operator=(const SignedAdditionalBid&) = delete;
+  SignedAdditionalBid& operator=(SignedAdditionalBid&&);
+
+  std::string additional_bid_json;
+  std::vector<Signature> signatures;
+};
+
+// Tries to decode a signed additional bid JSON represented as
+// `signed_additional_bid_in`.
+CONTENT_EXPORT base::expected<SignedAdditionalBid, std::string>
+DecodeSignedAdditionalBid(base::Value signed_additional_bid_in);
 
 }  // namespace content
 
