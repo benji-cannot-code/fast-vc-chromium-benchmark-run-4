14cd5944609a55b9e19dd37ad94032a5477dddf7
dljames <dljames@google.com>
2023-08-29 20:01:54 +0000

    [ Extensions ] Change OnEntryWillBeRemoved -> OnEntryRemoved
    Changes the reading list remove event from `OnEntryWillBeRemoved` to
    `OnEntryRemoved`.
    
    Bug: 1421058
    Change-Id: I09b66770fab1f14728b269f1d9d0631d976730a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4811621
    Commit-Queue: Darryl James <dljames@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1189694}

--- chrome/browser/extensions/api/reading_list/reading_list_api_unittest.cc
+++ chrome/browser/extensions/api/reading_list/reading_list_api_unittest.cc
@@ -489,7 +489,7 @@ TEST_F(ReadingListApiUnitTest, ReadingListOnEntryAdded) {
 }
 
 // Test that removing an entry generates an event.
-TEST_F(ReadingListApiUnitTest, ReadingListOnEntryWillBeRemoved) {
+TEST_F(ReadingListApiUnitTest, ReadingListOnEntryRemoved) {
   ReadingListModel* const reading_list_model =
       ReadingListModelFactory::GetForBrowserContext(profile());
 
@@ -507,9 +507,8 @@ TEST_F(ReadingListApiUnitTest, ReadingListOnEntryWillBeRemoved) {
   EXPECT_EQ(reading_list_model->size(), 0u);
 
   EXPECT_EQ(event_observer.events().size(), 1u);
-  EXPECT_TRUE(
-      base::Contains(event_observer.events(),
-                     api::reading_list::OnEntryWillBeRemoved::kEventName));
+  EXPECT_TRUE(base::Contains(event_observer.events(),
+                             api::reading_list::OnEntryRemoved::kEventName));
 }
 
 // Test that updating an entry generates an event.
