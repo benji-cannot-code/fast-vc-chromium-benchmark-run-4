8ea255018d4250e2ebe65337c451feefa2abf5af
Xiaoling Bao <xiaolingbao@chromium.org>
2023-08-30 05:49:10 +0000

    Create function to set platform policies (to enable extending policy settings to macOS)
    Bug: 1464354
    Change-Id: I0e1df603f4abc266810265dc3427dcc622771767
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4824325
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189944}

--- chrome/updater/test/integration_test_commands_user.cc
+++ chrome/updater/test/integration_test_commands_user.cc
@@ -86,6 +86,10 @@ class IntegrationTestCommandsUser : public IntegrationTestCommands {
     updater::test::SetGroupPolicies(values);
   }
 
+  void SetPlatformPolicies(const base::Value::Dict& values) const override {
+    updater::test::SetPlatformPolicies(values);
+  }
+
   void SetMachineManaged(bool is_managed_device) const override {
     updater::test::SetMachineManaged(is_managed_device);
   }
