ab1f424832c41d516fc5c96b89defad0119d41b2
Curt Clemens <cclem@google.com>
2023-08-30 21:07:23 +0000

    [QuickStart] Allow QuickStartMessage to decode unspecified payloads
    This is the first in a chain of CLs that will allow QuickStartDecoder
    to parse any packet from the source device and figure out its type.
    Currently we have separate methods for decoding each packet type, which
    makes it more difficult to handle packets that could be one of several
    types.
    
    This CL adds an overload of QuickStartMessage::ReadMessage() that does
    not require an enum value specifying the packet type. Instead, it
    determines the packet type by examining the packet. The original
    ReadMessage() that does take a packet type now calls the new method
    and simply checks that the decoded packet matches the expected type.
    
    Bug: b/294253611
    Test: Existing tests provide coverage, add test for unexpected msg.
    Change-Id: Id6efd792057cbb78f5e103f933ef857f3590882e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4794841
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Commit-Queue: Curt Clemens <cclem@google.com>
    Reviewed-by: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1190306}

--- chromeos/ash/components/quick_start/quick_start_message_type.h
+++ chromeos/ash/components/quick_start/quick_start_message_type.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMEOS_ASH_COMPONENTS_QUICK_START_QUICK_START_MESSAGE_TYPE_H_
 #define CHROMEOS_ASH_COMPONENTS_QUICK_START_QUICK_START_MESSAGE_TYPE_H_
 
+#include <ostream>
+
 namespace ash::quick_start {
 
 // Lists various types of QuickStartMessages possible.
@@ -13,9 +15,11 @@ enum class QuickStartMessageType {
   kSecondDeviceAuthPayload,  // secondDeviceAuthPayload in message
   kBootstrapOptions,         // bootstrapOptions in message
   kBootstrapConfigurations,  // bootstrapConfiguration in message
-  kFidoMessage,              // fidoMessage in message
   kQuickStartPayload,        // quickStartPayload in message
 };
+
+std::ostream& operator<<(std::ostream& stream,
+                         const QuickStartMessageType& message_type);
 
 }  // namespace ash::quick_start
 
