b2805165a505f5ffd6802a12e4c664a41a2c36e0
Nicolás Peña <npm@chromium.org>
2023-08-30 07:30:05 +0000

    [FedCM] Fix 3P OT token query
    Bug: 1475425
    Change-Id: I9ac0933cd30b5ae3da3a24c36b168610f66434ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4823290
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189969}

--- content/browser/webid/federated_auth_user_info_request.cc
+++ content/browser/webid/federated_auth_user_info_request.cc
@@ -131,8 +131,8 @@ void FederatedAuthUserInfoRequest::SetCallbackAndStart(
     return;
   }
 
-  if (!network::IsOriginPotentiallyTrustworthy(
-          url::Origin::Create(idp_config_url_))) {
+  url::Origin idp_origin = url::Origin::Create(idp_config_url_);
+  if (!network::IsOriginPotentiallyTrustworthy(idp_origin)) {
     CompleteWithError(
         FederatedAuthUserInfoRequestResult::kNotPotentiallyTrustworthy);
     return;
@@ -147,7 +147,7 @@ void FederatedAuthUserInfoRequest::SetCallbackAndStart(
 
   if (webid::ShouldFailAccountsEndpointRequestBecauseNotSignedInWithIdp(
           *render_frame_host_, idp_config_url_, permission_delegate_) &&
-      webid::GetIdpSigninStatusMode(*render_frame_host_) ==
+      webid::GetIdpSigninStatusMode(*render_frame_host_, idp_origin) ==
           FedCmIdpSigninStatusMode::ENABLED) {
     CompleteWithError(FederatedAuthUserInfoRequestResult::kNotSignedInWithIdp);
     return;
@@ -199,7 +199,8 @@ void FederatedAuthUserInfoRequest::OnAllConfigAndWellKnownFetched(
       webid::ShouldFailAccountsEndpointRequestBecauseNotSignedInWithIdp(
           *render_frame_host_, idp_config_url_, permission_delegate_);
   if (does_idp_have_failing_signin_status_ &&
-      webid::GetIdpSigninStatusMode(*render_frame_host_) ==
+      webid::GetIdpSigninStatusMode(*render_frame_host_,
+                                    url::Origin::Create(idp_config_url_)) ==
           FedCmIdpSigninStatusMode::ENABLED) {
     CompleteWithError(FederatedAuthUserInfoRequestResult::kNotSignedInWithIdp);
     return;
