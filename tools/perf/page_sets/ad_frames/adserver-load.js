5a55ac47a7cdc6f517089a462951f735c526879b
Liam Brady <lbrady@google.com>
2023-08-30 22:27:58 +0000

    Fenced frame: Use config to load in performance benchmark tests.
    Fenced frames no longer support loading through the "src" attribute.
    This CL updates the performance benchmarking tests to create fenced
    frames using by setting the "config" attribute to a FencedFrameConfig.
    This matches how other fenced frames tests handle loading.
    
    Change-Id: I1802e8aad66b2cca4394a278546252b4bc72c837
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4828364
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Liam Brady <lbrady@google.com>
    Cr-Commit-Position: refs/heads/main@{#1190367}

--- tools/perf/page_sets/ad_frames/adserver-load.js
+++ tools/perf/page_sets/ad_frames/adserver-load.js
@@ -9,7 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 function LoadFrame(type, url) {
   const frame = document.createElement(type);
-  frame.src = url;
+  if (type == "fencedframe") {
+    frame.config = new FencedFrameConfig(url);
+  } else {
+    frame.src = url;
+  }
   const dimensions = url.split(";sz=")[1].split("x");
   frame.width = dimensions[0];
   frame.height = dimensions[1];
