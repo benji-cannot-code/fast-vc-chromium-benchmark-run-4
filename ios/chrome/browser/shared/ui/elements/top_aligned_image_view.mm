0894aad27c40dafaf7a2d7de845ef88cfe39a38e
Louis Romero <lpromero@google.com>
2023-08-30 13:45:00 +0000

    Tentative fix for crash in TopAlignedImageView
    crbug/1468662 shows crashes when setting the frame of the image view:
    its position is NaN. This likely means that we divided by 0 in our
    calculations.
    
    This CL checks for the divisors against 0, to alleviate this crash.
    
    Bug: 1468662
    Change-Id: I9ce986ae9b68b3b28d7f0f6bd10b7a140bb75455
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4822889
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1190091}

--- ios/chrome/browser/shared/ui/elements/top_aligned_image_view.mm
+++ ios/chrome/browser/shared/ui/elements/top_aligned_image_view.mm
@@ -27,20 +27,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)layoutSubviews {
   [super layoutSubviews];
-  if (!self.image) {
+  const CGSize imageSize = self.image.size;
+  if (imageSize.width == 0 || imageSize.height == 0) {
     return;
   }
-  CGFloat widthScaleFactor = CGRectGetWidth(self.frame) / self.image.size.width;
-  CGFloat heightScaleFactor =
-      CGRectGetHeight(self.frame) / self.image.size.height;
+  CGFloat widthScaleFactor = CGRectGetWidth(self.frame) / imageSize.width;
+  CGFloat heightScaleFactor = CGRectGetHeight(self.frame) / imageSize.height;
   CGFloat imageViewWidth;
   CGFloat imageViewHeight;
-  if (self.image.size.width > self.image.size.height) {
-    imageViewWidth = self.image.size.width * heightScaleFactor;
+  if (imageSize.width > imageSize.height) {
+    imageViewWidth = imageSize.width * heightScaleFactor;
     imageViewHeight = CGRectGetHeight(self.frame);
   } else {
     imageViewWidth = CGRectGetWidth(self.frame);
-    imageViewHeight = self.image.size.height * widthScaleFactor;
+    imageViewHeight = imageSize.height * widthScaleFactor;
   }
   self.innerImageView.frame =
       CGRectMake((self.frame.size.width - imageViewWidth) / 2.0f, 0,
