46fb60c424fed72eed5c5d6b7999e389550c0583
mlcui <mlcui@google.com>
2023-08-30 02:17:40 +0000

    [LanguagePackHandwriting] Add EngineIdToHandwritingLocale
    This is a convenience function which wraps various IMF functions
    together for use in `EngineIdsToHandwritingLocales`.
    
    This implementation also fixes an issue with the given example
    `engine_id_to_handwriting_locale` argument - that the engine ID is not
    converted into an input method ID before being passed into
    `GetInputMethodDescriptorFromId`.
    
    Change-Id: I61ff74a34dc04ad66233766463dc744cff908d1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4797390
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Claudio M <claudiomagni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189884}

--- chromeos/ash/components/language_packs/handwriting.cc
+++ chromeos/ash/components/language_packs/handwriting.cc
@@ -16,6 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "chromeos/ash/components/language_packs/language_pack_manager.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "ui/base/ime/ash/extension_ime_util.h"
+#include "ui/base/ime/ash/input_method_manager.h"
+#include "ui/base/ime/ash/input_method_util.h"
 
 namespace ash::language_packs {
 
@@ -34,6 +37,19 @@ base::flat_set<std::string> EngineIdsToHandwritingLocales(
   }
 
   return handwriting_locales;
+}
+
+absl::optional<std::string> EngineIdToHandwritingLocale(
+    input_method::InputMethodUtil* const util,
+    const std::string& engine_id) {
+  const std::string input_method_id =
+      extension_ime_util::GetInputMethodIDByEngineID(engine_id);
+  const input_method::InputMethodDescriptor* descriptor =
+      util->GetInputMethodDescriptorFromId(input_method_id);
+  if (descriptor == nullptr) {
+    return absl::nullopt;
+  }
+  return descriptor->handwriting_language();
 }
 
 absl::optional<std::string> HandwritingLocaleToDlc(std::string_view locale) {
