c9f9266e3bc9ddbe303c147e29615dfd36e6b518
François Degros <fdegros@chromium.org>
2023-08-30 02:40:28 +0000

    BulkPinning: Don't sync when connected to a metered network
    Removed code duplication.
    Simplified DriveIntegrationService::PreferenceWatcher.
    Implemented DriveIntegrationService::PreferenceWatcher::DefaultNetworkChanged().
    Refactored GetDriveConnectionStatus().
    Simplified DriveIntegrationService::UpdateNetworkState().
    
    BUG=b:296963410, b:296469096, b:296963632, b:298123572
    
    Change-Id: I77ab41144974c7d995938e9f6a86027fcdee9787
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4821367
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189889}

--- chrome/browser/ash/drive/file_system_util.h
+++ chrome/browser/ash/drive/file_system_util.h
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_ASH_DRIVE_FILE_SYSTEM_UTIL_H_
 
 #include "base/files/file_path.h"
-#include "components/drive/file_errors.h"
-#include "url/gurl.h"
 
 class Profile;
 
