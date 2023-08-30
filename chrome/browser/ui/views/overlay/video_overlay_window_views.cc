797758903ae5d31415786d70012a73d7abaf2d11
Frank Liberato <liberato@chromium.org>
2023-08-30 14:28:29 +0000

    [autopip] Plumb allow / block UI to video overlay window.
    This CL shows the allow / block UI (overlay view) for pip 1.x
    windows.  Window dragging / resizing behavior is preserved.
    
    Change-Id: Ia4169eaa83eec94f5f6999226e5d07556adb63ba
    Bug: 1472171
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4760942
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1190111}

--- chrome/browser/ui/views/overlay/video_overlay_window_views.cc
+++ chrome/browser/ui/views/overlay/video_overlay_window_views.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "build/build_config.h"
+#include "chrome/browser/picture_in_picture/picture_in_picture_window_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
@@ -326,7 +327,10 @@ VideoOverlayWindowViews::VideoOverlayWindowViews(
           VideoOverlayWindowViews::kControlHideDelayAfterMove,
           base::BindRepeating(
               &VideoOverlayWindowViews::ReEnableControlsAfterMove,
-              base::Unretained(this))) {
+              base::Unretained(this))),
+      get_overlay_view_cb_(base::BindRepeating(
+          &PictureInPictureWindowManager::GetOverlayView,
+          base::Unretained(PictureInPictureWindowManager::GetInstance()))) {
   display::Screen::GetScreen()->AddObserver(this);
 }
 
@@ -598,8 +602,9 @@ void VideoOverlayWindowViews::UpdateControlsVisibility(bool is_visible) {
     return;
   }
 
+  // If the overlay view is shown, then the other controls are always hidden.
   GetControlsContainerView()->SetVisible(
-      force_controls_visible_.value_or(is_visible));
+      !IsOverlayViewShown() && force_controls_visible_.value_or(is_visible));
 }
 
 void VideoOverlayWindowViews::UpdateControlsBounds() {
@@ -685,6 +690,13 @@ void VideoOverlayWindowViews::UpdateMaxSize(const gfx::Rect& work_area) {
 
 bool VideoOverlayWindowViews::ControlsHitTestContainsPoint(
     const gfx::Point& point) {
+  if (IsOverlayViewShown()) {
+    // Let the overlay view consume this event if it wants to.  If not, then
+    // ignore any of our controls as well.  This will still permit dragging the
+    // window by any parts that aren't consumed by the overlay view.
+    return overlay_view_->GetEventHandlerForPoint(point);
+  }
+
   if (!AreControlsVisible())
     return false;
   if (GetBackToTabControlsBounds().Contains(point) ||
@@ -1006,6 +1018,10 @@ void VideoOverlayWindowViews::UpdateLayerBoundsWithLetterboxing(
   if (video_view_->layer()->has_external_content())
     video_view_->layer()->SetSurfaceSize(video_bounds.size());
 
+  if (IsOverlayViewShown()) {
+    overlay_view_->SetBoundsRect(gfx::Rect(GetBounds().size()));
+  }
+
   // Notify the controller that the bounds have changed.
   controller_->UpdateLayerBounds();
 }
@@ -1210,6 +1226,20 @@ void VideoOverlayWindowViews::ShowInactive() {
   ash::SetCornerRadius(GetNativeWindow(), GetRootView()->layer(),
                        chromeos::kPipRoundedCornerRadius);
 #endif
+
+  // If there is an existing overlay view, remove it now.
+  if (overlay_view_) {
+    GetContentsView()->RemoveChildView(overlay_view_);
+    overlay_view_ = nullptr;
+  }
+
+  // Re-add it if needed.
+  if (auto overlay_view = get_overlay_view_cb_.Run()) {
+    overlay_view_ = GetContentsView()->AddChildView(std::move(overlay_view));
+    // Also update the bounds, since that's already happened for everything
+    // else, potentially, during widget resize.
+    overlay_view_->SetBoundsRect(gfx::Rect(GetBounds().size()));
+  }
 
   // If this is not the first time the window is shown, this will be a no-op.
   has_been_shown_ = true;
@@ -1556,4 +1586,8 @@ void VideoOverlayWindowViews::MaybeUnregisterFrameSinkHierarchy() {
     GetCompositor()->RemoveChildFrameSink(*GetCurrentFrameSinkId());
     has_registered_frame_sink_hierarchy_ = false;
   }
+}
+
+bool VideoOverlayWindowViews::IsOverlayViewShown() const {
+  return overlay_view_ && overlay_view_->GetVisible();
 }
