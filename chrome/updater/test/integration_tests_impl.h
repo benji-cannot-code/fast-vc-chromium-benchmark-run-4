36ac981c10b9db0af20e01cb766b51829a54e733
S. Ganesh <ganesh@chromium.org>
2023-08-30 16:47:26 +0000

    Changes to give the MSI integration tests a separate test fixture
    This is to facilitate further additions to the MSI tests.
    
    Bug: 1476724
    Change-Id: Ifd4b14212e112a14a0fd21ce79efc729a3a2380c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4825306
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190174}

--- chrome/updater/test/integration_tests_impl.h
+++ chrome/updater/test/integration_tests_impl.h
@@ -43,7 +43,8 @@ enum class AppBundleWebCreateMode {
 };
 
 struct AppUpdateExpectation {
-  AppUpdateExpectation(const std::string& app_id,
+  AppUpdateExpectation(const std::string& args,
+                       const std::string& app_id,
                        const base::Version& from_version,
                        const base::Version& to_version,
                        bool is_install,
@@ -54,6 +55,7 @@ struct AppUpdateExpectation {
   AppUpdateExpectation(const AppUpdateExpectation&);
   ~AppUpdateExpectation();
 
+  const std::string args;
   const std::string app_id;
   const base::Version from_version;
   const base::Version to_version;
