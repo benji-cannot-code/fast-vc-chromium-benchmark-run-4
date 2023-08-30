345e7204c81b951cc68b0f6d60e6cd310c0db58d
Ali Juma <ajuma@chromium.org>
2023-08-30 23:50:16 +0000

    [iOS] Update settings strings for hash-prefix real-time lookups
    This updates the strings displayed for Standard Protection in
    Safe Browsing settings, when hash-prefix real-time lookups are
    enabled.
    
    Bug: 1468377
    Change-Id: I636dbc56236b6d8e8783a105de2c5cb009b789c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4827286
    Reviewed-by: Joemer Ramos <joemerramos@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190416}

--- ios/chrome/browser/ui/settings/privacy/safe_browsing/safe_browsing_standard_protection_mediator.mm
+++ ios/chrome/browser/ui/settings/privacy/safe_browsing/safe_browsing_standard_protection_mediator.mm
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/apple/foundation_util.h"
 #import "base/notreached.h"
+#import "build/branding_buildflags.h"
 #import "components/password_manager/core/common/password_manager_pref_names.h"
 #import "components/prefs/pref_service.h"
+#import "components/safe_browsing/core/browser/hashprefix_realtime/hash_realtime_utils.h"
 #import "components/safe_browsing/core/common/features.h"
 #import "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #import "components/signin/public/identity_manager/objc/identity_manager_observer_bridge.h"
@@ -199,10 +201,18 @@ const CGFloat kSymbolSize = 20;
   if (!_metricIconHeader) {
     UIImage* metricIcon =
         DefaultSymbolWithPointSize(kCheckmarkCircleSymbol, kSymbolSize);
+    NSInteger detailText = IDS_IOS_SAFE_BROWSING_STANDARD_PROTECTION_BULLET_TWO;
+
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+    if (safe_browsing::hash_realtime_utils::
+            IsHashRealTimeLookupEligibleInSession()) {
+      detailText = IDS_IOS_SAFE_BROWSING_STANDARD_PROTECTION_BULLET_TWO_PROXY;
+    }
+#endif
+
     SafeBrowsingHeaderItem* metricIconItem = [self
              detailItemWithType:ItemTypeMetricIcon
-                     detailText:
-                         IDS_IOS_SAFE_BROWSING_STANDARD_PROTECTION_BULLET_TWO
+                     detailText:detailText
                           image:metricIcon
         accessibilityIdentifier:kSafeBrowsingStandardProtectionMetricCellId];
     _metricIconHeader = metricIconItem;
