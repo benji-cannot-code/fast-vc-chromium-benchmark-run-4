3c667409d17809bb54f246e28f5cddddc56ec29b
Robert Flack <flackr@chromium.org>
2023-08-30 17:26:46 +0000

    Add use counter for nested snap areas.
    It would be very useful to know how often sites have nested snap
    areas as we make improvements to the snap logic for these cases.
    
    Bug: 1475994
    Change-Id: I59930cfdfdfa3eea161bdcdb7dfe45fa240724f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4822695
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190206}

--- third_party/blink/renderer/core/layout/ng/ng_fragment_builder.cc
+++ third_party/blink/renderer/core/layout/ng/ng_fragment_builder.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/ng_fragment_builder.h"
 
 #include "base/containers/contains.h"
+#include "third_party/blink/public/mojom/use_counter/metrics/web_feature.mojom-shared.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_physical_fragment.h"
 #include "third_party/blink/renderer/core/style/computed_style_base_constants.h"
@@ -138,6 +139,10 @@ void NGFragmentBuilder::PropagateSnapAreas(const NGPhysicalFragment& child) {
     for (auto& child_snap_area : *child_snap_areas) {
       snap_areas.insert(child_snap_area);
     }
+  }
+
+  if (child.IsSnapArea() && child.PropagatedSnapAreas()) {
+    child.GetDocument().CountUse(WebFeature::kScrollSnapNestedSnapAreas);
   }
 }
 
