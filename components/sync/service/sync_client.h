344774926515c19a45adae9eb97efab432be675e
Ankush Singh <ankushkush@google.com>
2023-08-30 13:15:12 +0000

    [sync] Add methods in SyncClient to query local data and trigger upload
    This method will be called by SyncService. The methods are implemented
    by the client because client has direct access to the local and account
    stores/models of the data types.
    
    Bug: 1451508
    Change-Id: I77db6fe08078d05f0e78d93b504587c66e318766
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4822887
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1190080}

--- components/sync/service/sync_client.h
+++ components/sync/service/sync_client.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SYNC_SERVICE_SYNC_CLIENT_H_
 #define COMPONENTS_SYNC_SERVICE_SYNC_CLIENT_H_
 
+#include <map>
+
 #include "base/files/file_path.h"
 #include "base/memory/ref_counted.h"
 #include "components/sync/base/extensions_activity.h"
@@ -24,6 +26,7 @@ class TrustedVaultClient;
 
 namespace syncer {
 
+struct LocalDataDescription;
 class SyncApiComponentFactory;
 class SyncInvalidationsService;
 class SyncService;
@@ -70,6 +73,27 @@ class SyncClient {
   // TODO(crbug.com/1137346): Replace this mechanism with a more universal one,
   // e.g. using SyncServiceObserver.
   virtual void OnLocalSyncTransportDataCleared() = 0;
+
+  // Queries the count and description/preview of existing local data for
+  // `types` data types. This is an asynchronous method which returns the result
+  // via the callback `callback` once the information for all the data types in
+  // `types` is available.
+  // Note: Only data types that are enabled and support this functionality are
+  // part of the response.
+  // TODO(crbug.com/1451508): Mark as pure virtual once all implementations have
+  // overridden this.
+  virtual void GetLocalDataDescriptions(
+      ModelTypeSet types,
+      base::OnceCallback<void(std::map<ModelType, LocalDataDescription>)>
+          callback);
+
+  // Requests the client to move all local data to account for `types` data
+  // types. This is an asynchronous method which moves the local data for all
+  // `types` to the account store locally. Upload to the server will happen as
+  // part of the regular commit process, and is NOT part of this method.
+  // TODO(crbug.com/1451508): Mark as pure virtual once all implementations have
+  // overridden this.
+  virtual void TriggerLocalDataMigration(ModelTypeSet types);
 };
 
 }  // namespace syncer
