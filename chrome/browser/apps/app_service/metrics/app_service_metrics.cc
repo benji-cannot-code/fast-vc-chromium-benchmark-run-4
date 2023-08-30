3e4554dea4a8ab79dbe5fec976713cbc3bc04c70
Angus L. M. McLean IV <angusmclean@google.com>
2023-08-30 20:03:55 +0000

    welcome tour: Log relevant default app launches
    Adds calls to record metrics for the Files and Settings apps launches.
    These metrics help measure the impact that the ChromeOS Welcome Tour
    is having on the way users interact with ChromeOS.
    
    More context here: http://shortn/_EpmOJgLlQw
    
    Bug: b:296415984
    Change-Id: Id6e80e885d43af982d7e8ac6486b65b30c1045b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4803274
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Angus McLean <angusmclean@google.com>
    Cr-Commit-Position: refs/heads/main@{#1190280}

--- chrome/browser/apps/app_service/metrics/app_service_metrics.cc
+++ chrome/browser/apps/app_service/metrics/app_service_metrics.cc
@@ -16,7 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/constants.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "ash/constants/ash_features.h"
 #include "ash/public/cpp/app_list/internal_app_id_constants.h"
+#include "ash/user_education/welcome_tour/welcome_tour_metrics.h"
 #include "ash/webui/projector_app/public/cpp/projector_app_constants.h"
 #include "chrome/browser/ash/app_list/arc/arc_app_utils.h"
 #include "chrome/browser/ash/file_manager/app_id.h"
@@ -161,6 +163,23 @@ void RecordDefaultAppLaunch(apps::DefaultAppName default_app_name,
   }
 }
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+void RecordWelcomeTourInteraction(apps::DefaultAppName default_app_name) {
+  switch (default_app_name) {
+    case apps::DefaultAppName::kSettings:
+      ash::welcome_tour_metrics::RecordInteraction(
+          ash::welcome_tour_metrics::Interaction::kSettingsApp);
+      break;
+    case apps::DefaultAppName::kFiles:
+      ash::welcome_tour_metrics::RecordInteraction(
+          ash::welcome_tour_metrics::Interaction::kFilesApp);
+      break;
+    default:
+      break;
+  }
+}
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+
 }  // namespace
 
 namespace apps {
@@ -177,6 +196,11 @@ void RecordAppLaunch(const std::string& app_id,
   if (const absl::optional<apps::DefaultAppName> app_name =
           SystemWebAppIdToName(app_id)) {
     RecordDefaultAppLaunch(app_name.value(), launch_source);
+
+    if (ash::features::IsWelcomeTourEnabled()) {
+      RecordWelcomeTourInteraction(app_name.value());
+    }
+
     return;
   }
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
