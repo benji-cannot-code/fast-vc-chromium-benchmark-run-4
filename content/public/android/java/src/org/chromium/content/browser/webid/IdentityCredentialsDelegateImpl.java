22ccd4cb44ca8a7868b6a20437bad2585a331fe6
Sam Maier <smaier@chromium.org>
2023-08-30 17:21:16 +0000

    Adding delegate for identity credentials
    This doesn't do anything yet - this is just setting up the
    infrastructure for a downstream usage of identity credentials, until we
    have made a publicly available version of the identity credentials
    GMSCore library.
    
    Change-Id: Ia412d54324d9486ed7168128cd75bd2554ab1e85
    Bug: 1475970
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4812426
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Sam Goto <goto@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190202}

---
+++ content/public/android/java/src/org/chromium/content/browser/webid/IdentityCredentialsDelegateImpl.java
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.content.browser.webid;
+
+import org.chromium.base.Promise;
+
+/**
+ * Public no-op implementation of IdentityCredentialsDelegate.
+ *
+ * TODO(crbug.com/1475970) upstream the private version of this.
+ */
+public class IdentityCredentialsDelegateImpl implements IdentityCredentialsDelegate {
+    @Override
+    public Promise<String> get(String origin, String request) {
+        return null;
+    }
+}
