b906231e39d09e638af81df4edf0bae45c23b7c3
Riley Tatum <rtatum@google.com>
2023-08-30 19:42:04 +0000

    Create cr-chip
    After talking to UX, it seems that the chip element should be basically
    the same as what was built in sp-filter-chip, except it should have
    paper ripple. Therefore, the CSS is largely borrowed from
    sp-filter-chip.
    
    This does not currently support suggestion chips, but that could easily
    be added as a class for it, just like we do with button types.
    
    Some things to note:
    * I used an attribute "chip-role" for passing the role through since
      role seems to be used by polymer and I wasn't sure how screenreaders
      would take a nested role attribute since it would show in the DOM.
    * Unlike sp-filter-chip, this chip component is not keeping track of
      filtered and disabled within the ts since it doesn't need to pass it
      on to the inner button.
    * This chip component expects the parent to handle basically all of the
      action logic and is mostly styling.
    * The padding and hover color were updated vs sp-filter-chip to match
      designs.
    
    Screenshots:
    GM3: http://screenshot/4RY8TnZTaMMFHza
    GM2: http://screenshot/3AXuPDpkk7UAPER
    
    NOTE for GM2: I don't have designs for this so it is just matching
    sp-filter-chip.
    
    Bug: 1465897
    Change-Id: I7adfc3a37e25f86fae1b497836c4619b2cf27831
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4812419
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1190273}

--- ui/color/color_id.h
+++ ui/color/color_id.h
@@ -275,6 +275,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorCheckboxOutlineDisabled) \
   E_CPONLY(kColorCheckboxForegroundChecked) \
   E_CPONLY(kColorCheckboxForegroundUnchecked) \
+  E_CPONLY(kColorChipBackgroundHover) \
+  E_CPONLY(kColorChipBackgroundSelected) \
+  E_CPONLY(kColorChipBorder) \
+  E_CPONLY(kColorChipForeground) \
+  E_CPONLY(kColorChipForegroundSelected) \
+  E_CPONLY(kColorChipIcon) \
+  E_CPONLY(kColorChipIconSelected) \
   E_CPONLY(kColorComboboxBackground) \
   E_CPONLY(kColorComboboxBackgroundDisabled) \
   E_CPONLY(kColorComboboxContainerOutline) \
