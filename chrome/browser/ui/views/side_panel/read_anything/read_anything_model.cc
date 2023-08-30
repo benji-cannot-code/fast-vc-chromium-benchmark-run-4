b7651e65ceca39705314def3e842bee901a1d681
Kristi Saney <kristislee@google.com>
2023-08-30 22:12:02 +0000

    [read-anything] Move logging text style to the page handler.
    This CL does a couple things:
    1. Ensures the current text styles are logged when the side panel closes for both the views (existing) and webui (new) toolbars.
    2. Retrieves the current values from prefs, rather than the ReadAnythingModel.
    3. Uses the mojom enum for logging color, which is what should've been happening before, but when I made a new enum for it a few months ago, I didn't relize we had a mojom enum for it already.
    
    All these are in service of migrating to a web-ui toolbar instead of the views toolbar.
    
    Bug: 1465029
    Change-Id: I9ff901aa46380df14db2071ff3123f0394bea3cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4819826
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1190354}

--- chrome/browser/ui/views/side_panel/read_anything/read_anything_model.cc
+++ chrome/browser/ui/views/side_panel/read_anything/read_anything_model.cc
@@ -220,30 +220,24 @@ void ReadAnythingModel::NotifyThemeChanged() {
 
 ReadAnythingFontModel::ReadAnythingFontModel() {}
 
+// If you change these fonts, please also update read_anything_constants.h
 void ReadAnythingFontModel::SetDefaultLanguage(const std::string& lang) {
   if (base::Contains(kLanguagesSupportedByPoppins, lang)) {
-    FontInfo kPoppins = {u"Poppins", ReadAnythingFont::kPoppins};
-    font_choices_.emplace_back(kPoppins);
+    font_choices_.emplace_back(u"Poppins");
   }
-  FontInfo kSansSerif = {u"Sans-serif", ReadAnythingFont::kSansSerif};
-  FontInfo kSerif = {u"Serif", ReadAnythingFont::kSerif};
-  font_choices_.emplace_back(kSansSerif);
-  font_choices_.emplace_back(kSerif);
+  font_choices_.emplace_back(u"Sans-serif");
+  font_choices_.emplace_back(u"Serif");
   if (base::Contains(kLanguagesSupportedByComicNeue, lang)) {
-    FontInfo kComicNeue = {u"Comic Neue", ReadAnythingFont::kComicNeue};
-    font_choices_.emplace_back(kComicNeue);
+    font_choices_.emplace_back(u"Comic Neue");
   }
   if (base::Contains(kLanguagesSupportedByLexendDeca, lang)) {
-    FontInfo kLexendDeca = {u"Lexend Deca", ReadAnythingFont::kLexendDeca};
-    font_choices_.emplace_back(kLexendDeca);
+    font_choices_.emplace_back(u"Lexend Deca");
   }
   if (base::Contains(kLanguagesSupportedByEbGaramond, lang)) {
-    FontInfo kEbGaramond = {u"EB Garamond", ReadAnythingFont::kEbGaramond};
-    font_choices_.emplace_back(kEbGaramond);
+    font_choices_.emplace_back(u"EB Garamond");
   }
   if (base::Contains(kLanguagesSupportedByStixTwoText, lang)) {
-    FontInfo kStixTwoText = {u"STIX Two Text", ReadAnythingFont::kStixTwoText};
-    font_choices_.emplace_back(kStixTwoText);
+    font_choices_.emplace_back(u"STIX Two Text");
   }
   font_choices_.shrink_to_fit();
 }
@@ -253,10 +247,7 @@ bool ReadAnythingFontModel::IsValidFontIndex(size_t index) {
 }
 
 size_t ReadAnythingFontModel::GetFontNameIndex(std::string font_name) {
-  auto it = base::ranges::find_if(
-      font_choices_, [font_name](const FontInfo& font_info) {
-        return base::UTF8ToUTF16(font_name) == font_info.name;
-      });
+  auto it = base::ranges::find(font_choices_, base::UTF8ToUTF16(font_name));
   return static_cast<size_t>(it - font_choices_.begin());
 }
 
@@ -284,12 +275,12 @@ std::u16string ReadAnythingFontModel::GetItemAt(size_t index) const {
 
 std::u16string ReadAnythingFontModel::GetDropDownTextAt(size_t index) const {
   DCHECK_LT(index, GetItemCount());
-  return font_choices_[index].name;
+  return font_choices_[index];
 }
 
 std::string ReadAnythingFontModel::GetFontNameAt(size_t index) {
   DCHECK_LT(index, GetItemCount());
-  return base::UTF16ToUTF8(font_choices_[index].name);
+  return base::UTF16ToUTF8(font_choices_[index]);
 }
 
 absl::optional<ui::ColorId>
@@ -321,8 +312,7 @@ ReadAnythingColorsModel::ColorInfo::ColorInfo(
     ui::ColorId separator_color_id,
     ui::ColorId dropdown_color_id,
     ui::ColorId selected_dropdown_color_id,
-    ui::ColorId focus_ring_color_id,
-    ColorInfo::ReadAnythingColor logging_value)
+    ui::ColorId focus_ring_color_id)
     : name(name),
       icon_asset(icon_asset),
       foreground_color_id(foreground_color_id),
@@ -330,8 +320,7 @@ ReadAnythingColorsModel::ColorInfo::ColorInfo(
       separator_color_id(separator_color_id),
       dropdown_color_id(dropdown_color_id),
       selected_dropdown_color_id(selected_dropdown_color_id),
-      focus_ring_color_id(focus_ring_color_id),
-      logging_value(logging_value) {}
+      focus_ring_color_id(focus_ring_color_id) {}
 ReadAnythingColorsModel::ColorInfo::ColorInfo(const ColorInfo& other) = default;
 ReadAnythingColorsModel::ColorInfo::ColorInfo(ColorInfo&&) = default;
 ReadAnythingColorsModel::ColorInfo&
@@ -349,8 +338,7 @@ ReadAnythingColorsModel::ReadAnythingColorsModel() {
       kColorReadAnythingSeparator,
       kColorReadAnythingDropdownBackground,
       kColorReadAnythingDropdownSelected,
-      kColorReadAnythingFocusRingBackground,
-      ColorInfo::ReadAnythingColor::kDefault};
+      kColorReadAnythingFocusRingBackground};
 
   ColorInfo kLightColors = {
       l10n_util::GetStringUTF16(IDS_READING_MODE_LIGHT_COLOR_LABEL),
@@ -360,8 +348,7 @@ ReadAnythingColorsModel::ReadAnythingColorsModel() {
       kColorReadAnythingSeparatorLight,
       kColorReadAnythingDropdownBackgroundLight,
       kColorReadAnythingDropdownSelectedLight,
-      kColorReadAnythingFocusRingBackgroundLight,
-      ColorInfo::ReadAnythingColor::kLight};
+      kColorReadAnythingFocusRingBackgroundLight};
 
   ColorInfo kDarkColors = {
       l10n_util::GetStringUTF16(IDS_READING_MODE_DARK_COLOR_LABEL),
@@ -371,8 +358,7 @@ ReadAnythingColorsModel::ReadAnythingColorsModel() {
       kColorReadAnythingSeparatorDark,
       kColorReadAnythingDropdownBackgroundDark,
       kColorReadAnythingDropdownSelectedDark,
-      kColorReadAnythingFocusRingBackgroundDark,
-      ColorInfo::ReadAnythingColor::kDark};
+      kColorReadAnythingFocusRingBackgroundDark};
 
   ColorInfo kYellowColors = {
       l10n_util::GetStringUTF16(IDS_READING_MODE_YELLOW_COLOR_LABEL),
@@ -382,8 +368,7 @@ ReadAnythingColorsModel::ReadAnythingColorsModel() {
       kColorReadAnythingSeparatorYellow,
       kColorReadAnythingDropdownBackgroundYellow,
       kColorReadAnythingDropdownSelectedYellow,
-      kColorReadAnythingFocusRingBackgroundYellow,
-      ColorInfo::ReadAnythingColor::kYellow};
+      kColorReadAnythingFocusRingBackgroundYellow};
 
   ColorInfo kBlueColors = {
       l10n_util::GetStringUTF16(IDS_READING_MODE_BLUE_COLOR_LABEL),
@@ -393,8 +378,7 @@ ReadAnythingColorsModel::ReadAnythingColorsModel() {
       kColorReadAnythingSeparatorBlue,
       kColorReadAnythingDropdownBackgroundBlue,
       kColorReadAnythingDropdownSelectedBlue,
-      kColorReadAnythingFocusRingBackgroundBlue,
-      ColorInfo::ReadAnythingColor::kBlue};
+      kColorReadAnythingFocusRingBackgroundBlue};
 
   colors_choices_.emplace_back(kDefaultColors);
   colors_choices_.emplace_back(kLightColors);
