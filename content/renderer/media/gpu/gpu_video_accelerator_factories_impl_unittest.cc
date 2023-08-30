580ab975f6c5aaa05befe47745d47275f9156128
Shubham Gupta <shubh.gupta@samsung.com>
2023-08-30 16:06:33 +0000

    Remove GpuMemoryBufferManager from ContextProviderCommandBuffer.
    ContextProviderCommandBuffer doesn't use GpuMemoryBufferManager anymore
    
    Bug: 1476945
    Change-Id: I022228cfa829a0618e11e9737c0dcdb14e8f4ecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4823223
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Shubham Gupta <shubh.gupta@samsung.com>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190153}

--- content/renderer/media/gpu/gpu_video_accelerator_factories_impl_unittest.cc
+++ content/renderer/media/gpu/gpu_video_accelerator_factories_impl_unittest.cc
@@ -132,12 +132,10 @@ class MockOverlayInfoCbHandler {
 class MockContextProviderCommandBuffer
     : public viz::ContextProviderCommandBuffer {
  public:
-  MockContextProviderCommandBuffer(
-      scoped_refptr<gpu::GpuChannelHost> channel,
-      gpu::GpuMemoryBufferManager* gpu_memory_buffer_manager)
+  explicit MockContextProviderCommandBuffer(
+      scoped_refptr<gpu::GpuChannelHost> channel)
       : viz::ContextProviderCommandBuffer(
             std::move(channel),
-            gpu_memory_buffer_manager,
             content::kGpuStreamIdDefault,
             content::kGpuStreamPriorityDefault,
             gpu::kNullSurfaceHandle,
@@ -372,8 +370,7 @@ class GpuVideoAcceleratorFactoriesImplTest : public testing::Test {
             base::MakeRefCounted<TestGpuChannelHost>(mock_gpu_channel_)),
         mock_context_provider_(
             base::MakeRefCounted<NiceMock<MockContextProviderCommandBuffer>>(
-                gpu_channel_host_,
-                &gpu_memory_buffer_manager_)) {}
+                gpu_channel_host_)) {}
   ~GpuVideoAcceleratorFactoriesImplTest() override = default;
 
   void SetUp() override {
