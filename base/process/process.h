ba09011840d70d21dcadf762b1ca90bab460ad7f
Patrick Monette <pmonette@chromium.org>
2023-08-30 23:20:44 +0000

    Remove unused feature MacAllowBackgroundingProcesses
    This feature was renamed to MacAllowBackgroundingRenderProcesses but
    the declaration was left in.
    
    Change-Id: I92e003be2af14bd590ecad8ce5f933a1ce933f01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4828542
    Auto-Submit: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190395}

--- base/process/process.h
+++ base/process/process.h
@@ -32,10 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-#if BUILDFLAG(IS_APPLE)
-BASE_DECLARE_FEATURE(kMacAllowBackgroundingProcesses);
-#endif
-
 #if BUILDFLAG(IS_CHROMEOS)
 // OneGroupPerRenderer feature places each foreground renderer process into
 // its own cgroup. This will cause the scheduler to use the aggregate runtime
