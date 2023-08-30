97a6d52b6bb00129ddab306c1ad1c86587d49437
Marc Treib <treib@chromium.org>
2023-08-30 08:40:01 +0000

    Fix TabPickupSettingsTestCase with kReplaceSyncPromosWithSignInPromos
    With kReplaceSyncPromosWithSignInPromos, the "syncing" state can't be
    entered anymore, so some parts of these tests don't apply anymore.
    
    Bug: 1462156
    Change-Id: I55b602afa3916301820f0a4933d76d9a441b9fde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4822006
    Auto-Submit: Marc Treib <treib@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189989}

--- ios/chrome/browser/ui/settings/tabs/tab_pickup/tab_pickup_settings_egtest.mm
+++ ios/chrome/browser/ui/settings/tabs/tab_pickup/tab_pickup_settings_egtest.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/strings/sys_string_conversions.h"
 #import "base/test/ios/wait_util.h"
+#import "components/sync/base/features.h"
 #import "ios/chrome/browser/signin/fake_system_identity.h"
 #import "ios/chrome/browser/tabs/tab_pickup/features.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey.h"
@@ -105,6 +106,14 @@ id<GREYMatcher> TabPickupPrivacyFooterGoogleServicesLink() {
 - (AppLaunchConfiguration)appConfigurationForTestCase {
   AppLaunchConfiguration config;
   config.features_enabled.push_back(kTabPickupThreshold);
+
+  if ([self isRunningTest:@selector(testTabPickupSettingsSynced)]) {
+    // With kReplaceSyncPromosWithSignInPromos enabled, Sync can't be enabled
+    // anymore.
+    config.features_disabled.push_back(
+        syncer::kReplaceSyncPromosWithSignInPromos);
+  }
+
   return config;
 }
 
@@ -159,16 +168,20 @@ id<GREYMatcher> TabPickupPrivacyFooterGoogleServicesLink() {
   OpenTabsSettings();
   OpenTabPickupFromTabsSettings();
 
-  // Check the "Sync" link.
-  [[EarlGrey selectElementWithMatcher:TabPickupPrivacyFooterSyncLink()]
-      performAction:grey_tap()];
-  [[EarlGrey
-      selectElementWithMatcher:grey_accessibilityID(
-                                   kManageSyncTableViewAccessibilityIdentifier)]
-      assertWithMatcher:grey_sufficientlyVisible()];
+  // The "Sync" links only exists without isReplaceSyncWithSigninEnabled.
+  if (![ChromeEarlGrey isReplaceSyncWithSigninEnabled]) {
+    // Check the "Sync" link.
+    [[EarlGrey selectElementWithMatcher:TabPickupPrivacyFooterSyncLink()]
+        performAction:grey_tap()];
+    [[EarlGrey
+        selectElementWithMatcher:
+            grey_accessibilityID(kManageSyncTableViewAccessibilityIdentifier)]
+        assertWithMatcher:grey_sufficientlyVisible()];
 
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::SettingsMenuBackButton(
-                                          0)] performAction:grey_tap()];
+    [[EarlGrey
+        selectElementWithMatcher:chrome_test_util::SettingsMenuBackButton(0)]
+        performAction:grey_tap()];
+  }
 
   // Check the "Google Services" link.
   [[EarlGrey
