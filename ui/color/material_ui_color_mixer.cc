7752101172586b8af7e525ad2ceaf010d975cce4
John Lee <johntlee@chromium.org>
2023-08-30 23:40:39 +0000

    Customize Chrome: Update segmented button colors
    Fixed: 1468643
    Change-Id: I79140f030028923331a76717abdbfd024cc2b4c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4827199
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190407}

--- ui/color/material_ui_color_mixer.cc
+++ ui/color/material_ui_color_mixer.cc
@@ -118,11 +118,11 @@ void AddMaterialUiColorMixer(ColorProvider* provider,
   mixer[kColorRadioButtonForegroundUnchecked] = {kColorSysOutline};
   mixer[kColorSecondaryForeground] = {ui::kColorSysOnSurfaceSubtle};
   mixer[kColorSegmentedButtonBorder] = {kColorSysTonalOutline};
-  mixer[kColorSegmentedButtonForegroundChecked] = {kColorSysOnTonalContainer};
+  mixer[kColorSegmentedButtonForegroundChecked] = {kColorSysOnPrimary};
   mixer[kColorSegmentedButtonForegroundUnchecked] = {kColorSysOnSurfaceSubtle};
   mixer[kColorSegmentedButtonHover] = {kColorSysStateHoverOnSubtle};
   mixer[kColorSegmentedButtonRipple] = {kColorSysStateRippleNeutralOnSubtle};
-  mixer[kColorSegmentedButtonChecked] = {kColorSysTonalContainer};
+  mixer[kColorSegmentedButtonChecked] = {kColorSysPrimary};
   mixer[kColorSeparator] = {kColorSysDivider};
   mixer[kColorSidePanelComboboxBackground] = {kColorSysBaseContainer};
   mixer[kColorSliderThumb] = {kColorSysPrimary};
