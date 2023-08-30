d71d84a77b1d7f208f5b85a3dd9ad7fcc5273b7b
Hong Xu <xuhong@google.com>
2023-08-30 23:35:59 +0000

    Reland "When fetching PSR info has failed, try again"
    This reverts commit 3762ce6a48cdf0483611333996b28471113c86ab.
    
    Reason for revert: Wait time causes timeouts for unit tests. Set wait
    time to zero for unit tests.
    
    Original change's description:
    > Revert "When fetching PSR info has failed, try again"
    >
    > This reverts commit a0967d1bfa907b39ddf0c2a52517e39f4cfc35b5.
    >
    > Reason for revert: b/298172505 - CrosHealthdMetricSamplerTest.* tests
    > failing
    >
    > Original change's description:
    > > When fetching PSR info has failed, try again
    > >
    > > Fetching PSR info may be racey, e.g., b/297413010. Try again if it
    > > fails.
    > >
    > > This CL is expected to work after crrev.com/c/4813187
    > >
    > > Bug: b:280068539
    > > Change-Id: Id5ac977fa9627c969007da06cccba32ca16a24f2
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4820433
    > > Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    > > Commit-Queue: Hong Xu <xuhong@google.com>
    > > Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1189767}
    >
    > Bug: b:298172505
    > Bug: b:280068539
    > Change-Id: I0e5ca5afa194771d0b4ea42f3e9b009c27c82437
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4826482
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: David Munro <davidmunro@google.com>
    > Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    > Commit-Queue: Andre Le <leandre@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1190176}
    
    Bug: b:298172505
    Bug: b:280068539
    Fixed: b:298172505
    Change-Id: I4133704afac8578498f5785c6d486b220012f334
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4827887
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Hong Xu <xuhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1190405}

--- chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_metric_sampler_unittest.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_metric_sampler_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/bind.h"
 #include "base/test/task_environment.h"
+#include "base/time/time.h"
 #include "chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_info_metric_sampler_test_utils.h"
 #include "chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_sampler_handlers/cros_healthd_audio_sampler_handler.h"
 #include "chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_sampler_handlers/cros_healthd_boot_performance_sampler_handler.h"
@@ -31,6 +32,22 @@ namespace cros_healthd = ::ash::cros_healthd::mojom;
 using ::testing::Eq;
 using ::testing::StrEq;
 
+namespace {
+// Child of `CrosHealthdPsrSamplerHandler` that sets wait time between retries
+// to zero to prevent time out in unit tests. This is less intrusive
+// to production code than adding a method `SetWaitTimeForTest` to
+// `CrosHealthdPsrSamplerHandler`.
+class CrosHealthdPsrSamplerHandlerForTest
+    : public CrosHealthdPsrSamplerHandler {
+ public:
+  CrosHealthdPsrSamplerHandlerForTest() { wait_time_ = base::TimeDelta(); }
+  CrosHealthdPsrSamplerHandlerForTest(
+      const CrosHealthdPsrSamplerHandlerForTest&) = delete;
+  CrosHealthdPsrSamplerHandlerForTest& operator=(
+      const CrosHealthdPsrSamplerHandlerForTest&) = delete;
+  ~CrosHealthdPsrSamplerHandlerForTest() override = default;
+};
+
 struct TbtTestCase {
   std::string test_name;
   std::vector<cros_healthd::ThunderboltSecurityLevel> healthd_security_levels;
@@ -38,8 +55,8 @@ struct TbtTestCase {
 };
 
 // Memory constants.
-static constexpr int64_t kTmeMaxKeys = 2;
-static constexpr int64_t kTmeKeysLength = 4;
+constexpr int64_t kTmeMaxKeys = 2;
+constexpr int64_t kTmeKeysLength = 4;
 
 // Boot Performance constants.
 constexpr int64_t kBootUpSeconds = 5054;
@@ -48,6 +65,7 @@ constexpr int64_t kShutdownSeconds = 44003;
 constexpr int64_t kShutdownTimestampSeconds = 49;
 constexpr char kShutdownReason[] = "user-request";
 constexpr char kShutdownReasonNotApplicable[] = "N/A";
+}  // namespace
 
 cros_healthd::AudioInfoPtr CreateAudioInfo(
     bool output_mute,
@@ -278,7 +296,7 @@ TEST_F(CrosHealthdMetricSamplerTest, TestUsbTelemetry) {
 
 TEST_F(CrosHealthdMetricSamplerTest, TestRuntimeCountersTelemetryNoPsrInfo) {
   const absl::optional<MetricData> optional_result =
-      CollectData(std::make_unique<CrosHealthdPsrSamplerHandler>(),
+      CollectData(std::make_unique<CrosHealthdPsrSamplerHandlerForTest>(),
                   CreateSystemResult(CreateSystemInfoWithPsr(nullptr)),
                   cros_healthd::ProbeCategoryEnum::kSystem,
                   CrosHealthdSamplerHandler::MetricType::kTelemetry);
@@ -289,7 +307,7 @@ TEST_F(CrosHealthdMetricSamplerTest, TestRuntimeCountersTelemetryNoPsrInfo) {
 TEST_F(CrosHealthdMetricSamplerTest,
        TestRuntimeCountersTelemetryErrorGettingPsrInfo) {
   const absl::optional<MetricData> optional_result = CollectData(
-      std::make_unique<CrosHealthdPsrSamplerHandler>(),
+      std::make_unique<CrosHealthdPsrSamplerHandlerForTest>(),
       CreateSystemResultWithError(), cros_healthd::ProbeCategoryEnum::kSystem,
       CrosHealthdSamplerHandler::MetricType::kTelemetry);
 
@@ -299,7 +317,7 @@ TEST_F(CrosHealthdMetricSamplerTest,
 TEST_F(CrosHealthdMetricSamplerTest,
        TestRuntimeCountersTelemetryPsrUnsupported) {
   const absl::optional<MetricData> optional_result =
-      CollectData(std::make_unique<CrosHealthdPsrSamplerHandler>(),
+      CollectData(std::make_unique<CrosHealthdPsrSamplerHandlerForTest>(),
                   CreateSystemResult(CreateSystemInfoWithPsrUnsupported()),
                   cros_healthd::ProbeCategoryEnum::kSystem,
                   CrosHealthdSamplerHandler::MetricType::kTelemetry);
@@ -310,7 +328,7 @@ TEST_F(CrosHealthdMetricSamplerTest,
 TEST_F(CrosHealthdMetricSamplerTest,
        TestRuntimeCountersTelemetryPsrNotStarted) {
   const absl::optional<MetricData> optional_result =
-      CollectData(std::make_unique<CrosHealthdPsrSamplerHandler>(),
+      CollectData(std::make_unique<CrosHealthdPsrSamplerHandlerForTest>(),
                   CreateSystemResult(CreateSystemInfoWithPsrLogState(
                       cros_healthd::PsrInfo::LogState::kNotStarted)),
                   cros_healthd::ProbeCategoryEnum::kSystem,
@@ -321,7 +339,7 @@ TEST_F(CrosHealthdMetricSamplerTest,
 
 TEST_F(CrosHealthdMetricSamplerTest, TestRuntimeCountersTelemetryPsrStopped) {
   const absl::optional<MetricData> optional_result =
-      CollectData(std::make_unique<CrosHealthdPsrSamplerHandler>(),
+      CollectData(std::make_unique<CrosHealthdPsrSamplerHandlerForTest>(),
                   CreateSystemResult(CreateSystemInfoWithPsrLogState(
                       cros_healthd::PsrInfo::LogState::kStopped)),
                   cros_healthd::ProbeCategoryEnum::kSystem,
@@ -337,6 +355,9 @@ TEST_F(CrosHealthdMetricSamplerTest,
   constexpr uint32_t kS4Counter = 3u;
   constexpr uint32_t kS3Counter = 4u;
 
+  // Use `CrosHealthdPsrSamplerHandler` instead of
+  // `CrosHealthdPsrSamplerHandler` here because the waiting should not be
+  // triggered here.
   const absl::optional<MetricData> optional_result =
       CollectData(std::make_unique<CrosHealthdPsrSamplerHandler>(),
                   CreateSystemResult(CreateSystemInfoWithPsrSupportedRunning(
@@ -361,7 +382,7 @@ TEST_F(CrosHealthdMetricSamplerTest,
             static_cast<int64_t>(kS5Counter));
 }
 
-TEST_P(CrosHealthdMetricSamplerMemoryInfoTest, TestMemoryInfoeporting) {
+TEST_P(CrosHealthdMetricSamplerMemoryInfoTest, TestMemoryInfoReporting) {
   const auto& test_case = GetParam();
   const absl::optional<MetricData> optional_result = CollectData(
       std::make_unique<CrosHealthdMemorySamplerHandler>(),
