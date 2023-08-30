7e22dc3002d23c7499fd6f61bca295ed449061a1
aliaaborhamy <aliaaborhamy@google.com>
2023-08-30 14:56:36 +0000

    [iOS] Have the most visited sites expires after 4 weeks from the last modified date
    -Added a Test Flag to do the refresh after 5 minutes in place of 4 weeks.
    
    Bug: 1454444
    Change-Id: I39bfd47e554672daafe39fa1088e56fe73dc6313
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4808010
    Commit-Queue: Aliaa Alborhamy <aliaaborhamy@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1190126}

--- ios/chrome/common/app_group/app_group_constants.mm
+++ ios/chrome/common/app_group/app_group_constants.mm
@@ -72,6 +72,9 @@ NSString* const kOpenCommandSourceCredentialsExtension =
 
 NSString* const kSuggestedItems = @"SuggestedItems";
 
+NSString* const kSuggestedItemsLastModificationDate =
+    @"SuggestedItemsLastModificationDate";
+
 NSString* ApplicationGroup() {
   return [AppGroupHelper applicationGroup];
 }
