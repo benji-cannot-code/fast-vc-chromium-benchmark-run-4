b157bee7d9bc179324cdb1476812340f92543287
Solomon Kinard <solomonkinard@chromium.org>
2023-08-30 00:35:16 +0000

    Extensions: WFH: Add tests for "launch_type: multiple-clients"
    * Introduce an enum for launch_type without changing idl definition.
    * Test multiple-clients. As a byproduct, single-client is now tested.
    * All calls are now simplified to use the new LaunchWithIntent().
    * Test that the file is available to setConsumer().
    * Removed some unused includes files.
    
    Doc:
    https://docs.google.com/document/d/1_AAZw4n1rpBGncUTKD4T6VNpBi1SSKywV8soLElMcSg
    
    Bug: chromium:1448893
    Change-Id: I3d5c65c838e80a716e262b2fd112f4f397c60e0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4819574
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189844}

--- chrome/browser/apps/app_service/publishers/extension_apps_chromeos_browsertest.cc
+++ chrome/browser/apps/app_service/publishers/extension_apps_chromeos_browsertest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <utility>
-#include <vector>
 
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
@@ -38,8 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/common/file_system/file_system_types.h"
 #include "ui/base/window_open_disposition.h"
 #include "ui/display/types/display_constants.h"
-#include "ui/views/widget/any_widget_observer.h"
-#include "ui/views/widget/widget.h"
 
 namespace apps {
 
