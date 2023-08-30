d1bb2ff2ddf0b6808b728ca9c2099e1aae805997
mikt <mikt@google.com>
2023-08-30 03:55:30 +0000

    [BRP/clang-plugin] No raw_ptr enforcement to ptrs in STACK_ALLOCATED()
    - raw_ptr<T> usage on-stack context (e.g. function parameter) is
      optional and not enforced.
      Member fields in STACK_ALLOCATED() objects are known to be always
      on-stack, so we should not enforce raw_ptr<T> usage there.
      This CL will exclude there from enforcement.
    - This CL is effective only if `check-raw-ptr-to-stack-allocated` is
      activated. It is not enabled yet.
    
    Note:
      raw_ptr<T> pointing to STACK_ALLOCATED() is already excluded from
      enforcement. https://crrev.com/c/4542127
      This CL adds exclusion for raw_ptr<T> as a member of
      STACK_ALLOCATED() struct/class.
    
      struct Foo {
        STACK_ALLOCATED();
        raw_ptr<int> ptr1; // OK, but not recommended
        int*         ptr2; // Allowed by crrev.com/c/4800771 (this CL)
      }
      struct Bar {
        raw_ptr<Foo> ptr1; // Disallowed by crrev.com/c/4542127
        Foo*         ptr2; // Allowed by crrev.com/c/4542127
      }
    
    Change-Id: I41bfe1d0b1289420dbaa479180b0d8e8023b35a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800771
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189912}

--- tools/clang/plugins/RawPtrHelpers.cpp
+++ tools/clang/plugins/RawPtrHelpers.cpp
@@ -111,6 +111,17 @@ clang::ast_matchers::internal::Matcher<clang::QualType> StackAllocatedQualType(
 // RAW_PTR_EXCLUSION
 // - located under third_party/ except under third_party/blink as Blink
 // is part of chromium git repo.
+//
+// Additionally, if |options.should_exclude_stack_allocated_records|,
+// - Pointer pointing to a STACK_ALLOCATED() object.
+// - Pointer that are a member of STACK_ALLOCATED() object.
+//    struct Foo {
+//      STACK_ALLOCATED();
+//      int*         ptr2; // isDeclaredInStackAllocated(...)
+//    }
+//    struct Bar {
+//      Foo*         ptr2; // hasDescendant(StackAllocatedQualType(...))
+//    }
 clang::ast_matchers::internal::Matcher<clang::NamedDecl> PtrAndRefExclusions(
     const RawPtrAndRefExclusionsOptions& options) {
   if (!options.fix_crbug_1449812) {
@@ -135,7 +146,8 @@ clang::ast_matchers::internal::Matcher<clang::NamedDecl> PtrAndRefExclusions(
           isFieldDeclListedInFilterFile(options.fields_to_exclude),
           ImplicitFieldDeclaration(), isObjCSynthesize(),
           hasDescendant(
-              StackAllocatedQualType(options.stack_allocated_predicate)));
+              StackAllocatedQualType(options.stack_allocated_predicate)),
+          isDeclaredInStackAllocated(*options.stack_allocated_predicate));
     }
   } else {
     // After fix for crbug.com/1449812
@@ -150,14 +162,16 @@ clang::ast_matchers::internal::Matcher<clang::NamedDecl> PtrAndRefExclusions(
                    isFieldDeclListedInFilterFile(options.fields_to_exclude),
                    ImplicitFieldDeclaration(), isObjCSynthesize());
     } else {
-      return anyOf(isSpellingInSystemHeader(), isInExternCContext(),
-                   isRawPtrExclusionAnnotated(), isInThirdPartyLocation(),
-                   isInGeneratedLocation(), isNotSpelledInSource(),
-                   isInLocationListedInFilterFile(options.paths_to_exclude),
-                   isFieldDeclListedInFilterFile(options.fields_to_exclude),
-                   ImplicitFieldDeclaration(), isObjCSynthesize(),
-                   hasDescendant(StackAllocatedQualType(
-                       options.stack_allocated_predicate)));
+      return anyOf(
+          isSpellingInSystemHeader(), isInExternCContext(),
+          isRawPtrExclusionAnnotated(), isInThirdPartyLocation(),
+          isInGeneratedLocation(), isNotSpelledInSource(),
+          isInLocationListedInFilterFile(options.paths_to_exclude),
+          isFieldDeclListedInFilterFile(options.fields_to_exclude),
+          ImplicitFieldDeclaration(), isObjCSynthesize(),
+          hasDescendant(
+              StackAllocatedQualType(options.stack_allocated_predicate)),
+          isDeclaredInStackAllocated(*options.stack_allocated_predicate));
     }
   }
 }
