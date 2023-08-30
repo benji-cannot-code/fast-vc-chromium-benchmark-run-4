f1c7fdbd5d7351cb4dc2351ab95b963432ed0be2
Tim <tjudkins@chromium.org>
2023-08-30 17:17:36 +0000

    Revert "Refactor enum ConnectionStatusType -> enum class ConnectionStatus"
    This reverts commit a5897f9ad850f04bef739a2125338bea25cc4c32.
    
    Reason for revert: Parent change started causing consistent failures on builders, this needs to be reverted so the parent change can also be reverted in crrev.com/c/4827185
    
    Original change's description:
    > Refactor enum ConnectionStatusType -> enum class ConnectionStatus
    >
    > BUG=b:296963632
    >
    > Change-Id: Ia3be2fc7e88db97f7acfde96ab514d9553c5b2fe
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4821468
    > Reviewed-by: Ben Reich <benreich@chromium.org>
    > Commit-Queue: François Degros <fdegros@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1189941}
    
    BUG=b:296963632
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    
    Change-Id: I71a6adf8a7782ecfccbed249d4708dae7c5372b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4827346
    Owners-Override: Tim Judkins <tjudkins@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190198}

--- chrome/browser/ash/extensions/file_manager/event_router.cc
+++ chrome/browser/ash/extensions/file_manager/event_router.cc
@@ -1103,7 +1103,7 @@ void EventRouter::OnFileSystemMountFailed() {
 }
 
 void EventRouter::OnDriveConnectionStatusChanged(
-    drive::util::ConnectionStatus status) {
+    drive::util::ConnectionStatusType status) {
   NotifyDriveConnectionStatusChanged();
 }
 
--- chrome/browser/ash/extensions/file_manager/private_api_sharesheet.cc
+++ chrome/browser/ash/extensions/file_manager/private_api_sharesheet.cc
@@ -111,12 +111,11 @@ void FileManagerPrivateInternalSharesheetHasTargetsFunction::
 
   if (file_system_urls_.size() == 1 &&
       file_system_urls_[0].type() == storage::kFileSystemTypeDriveFs) {
-    using drive::util::ConnectionStatus;
-    const ConnectionStatus status = drive::util::GetDriveConnectionStatus(
+    auto connection_status = drive::util::GetDriveConnectionStatus(
         Profile::FromBrowserContext(browser_context()));
 
-    using enum ConnectionStatus;
-    if (status == kMetered || status == kConnected) {
+    if (connection_status == drive::util::DRIVE_CONNECTED_METERED ||
+        connection_status == drive::util::DRIVE_CONNECTED) {
       file_manager::util::SingleEntryPropertiesGetterForDriveFs::Start(
           file_system_urls_[0], profile_,
           base::BindOnce(
@@ -247,12 +246,11 @@ void FileManagerPrivateInternalInvokeSharesheetFunction::OnMimeTypesCollected(
 
   if (file_system_urls_.size() == 1 &&
       file_system_urls_[0].type() == storage::kFileSystemTypeDriveFs) {
-    using drive::util::ConnectionStatus;
-    const ConnectionStatus status = drive::util::GetDriveConnectionStatus(
+    auto connection_status = drive::util::GetDriveConnectionStatus(
         Profile::FromBrowserContext(browser_context()));
 
-    using enum ConnectionStatus;
-    if (status == kMetered || status == kConnected) {
+    if (connection_status == drive::util::DRIVE_CONNECTED_METERED ||
+        connection_status == drive::util::DRIVE_CONNECTED) {
       file_manager::util::SingleEntryPropertiesGetterForDriveFs::Start(
           file_system_urls_[0], profile_,
           base::BindOnce(&FileManagerPrivateInternalInvokeSharesheetFunction::
