ef1301d6017518191602b9c2b2d894bcfc440ed4
Kush Sinha <sinhak@chromium.org>
2023-08-30 09:09:37 +0000

    AccountManager: Add an API to return token hash
    Add a temporary diagnostics API to return the hash of tokens stored
    against accounts.
    
    This will be used in follow-up CLs to figure out if token handles are
    being checked against the latest token / LST. We have received reports
    from Gaia that ChromeOS checks tokens for older LSTs.
    
    Bug: b/297349237
    Test: components_unittests --gtest_filter="*AccountManagerTest.GetTokenHash*"
    Change-Id: I5f7719eb37347550192640433a78c6def58fa3ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4813248
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190003}

--- components/account_manager_core/chromeos/account_manager.cc
+++ components/account_manager_core/chromeos/account_manager.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/important_file_writer.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
+#include "base/hash/sha1.h"
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/memory/raw_ptr.h"
@@ -21,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
 #include "base/ranges/algorithm.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/thread_pool.h"
 #include "build/chromeos_buildflags.h"
@@ -112,6 +114,13 @@ internal::AccountType ToProtoAccountType(
     case ::account_manager::AccountType::kActiveDirectory:
       return internal::AccountType::ACCOUNT_TYPE_ACTIVE_DIRECTORY;
   }
+}
+
+// Returns a Base16 encoded SHA1 digest of `data`.
+std::string Sha1Digest(const std::string& data) {
+  const base::SHA1Digest hash =
+      base::SHA1HashSpan(base::as_bytes(base::make_span(data)));
+  return base::HexEncode(hash);
 }
 
 }  // namespace
@@ -772,6 +781,30 @@ void AccountManager::CheckDummyGaiaTokenForAllAccounts(
   }
 
   std::move(callback).Run(accounts_list);
+}
+
+void AccountManager::GetTokenHash(
+    const ::account_manager::AccountKey& account_key,
+    base::OnceCallback<void(const std::string&)> callback) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK_NE(init_state_, InitializationState::kNotStarted);
+
+  if (init_state_ != InitializationState::kInitialized) {
+    base::OnceClosure closure = base::BindOnce(
+        &AccountManager::GetTokenHash, weak_factory_.GetWeakPtr(), account_key,
+        std::move(callback));
+    RunOnInitialization(std::move(closure));
+    return;
+  }
+
+  DCHECK_EQ(init_state_, InitializationState::kInitialized);
+  auto it = accounts_.find(account_key);
+  if (it == accounts_.end()) {
+    std::move(callback).Run(std::string());
+    return;
+  }
+
+  std::move(callback).Run(Sha1Digest(it->second.token));
 }
 
 void AccountManager::MaybeRevokeTokenOnServer(
--- components/account_manager_core/chromeos/account_manager_unittest.cc
+++ components/account_manager_core/chromeos/account_manager_unittest.cc
@@ -14,7 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
+#include "base/hash/sha1.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/test/bind.h"
 #include "base/test/task_environment.h"
@@ -624,6 +626,25 @@ TEST_F(AccountManagerTest, HasDummyGaiaTokenReturnsFalseForValidTokens) {
   account_manager()->UpsertAccount(kGaiaAccountKey, kRawUserEmail, kGaiaToken);
   RunAllPendingTasks();
   EXPECT_FALSE(HasDummyGaiaTokenBlocking(kGaiaAccountKey));
+}
+
+TEST_F(AccountManagerTest, GetTokenHashReturnsAnEmptyStringForUnknownAccounts) {
+  base::test::TestFuture<const std::string&> future;
+  account_manager()->GetTokenHash(kGaiaAccountKey, future.GetCallback());
+  EXPECT_EQ(std::string(), future.Get());
+}
+
+TEST_F(AccountManagerTest, GetTokenHashReturnsSha1Hash) {
+  account_manager()->UpsertAccount(kGaiaAccountKey, kRawUserEmail, kGaiaToken);
+  RunAllPendingTasks();
+
+  base::test::TestFuture<const std::string&> future;
+  account_manager()->GetTokenHash(kGaiaAccountKey, future.GetCallback());
+
+  const base::SHA1Digest token_hash = base::SHA1HashSpan(
+      base::as_bytes(base::make_span(std::string(kGaiaToken))));
+  const std::string token_hash_digest = base::HexEncode(token_hash);
+  EXPECT_EQ(token_hash_digest, future.Get());
 }
 
 TEST_F(AccountManagerTest,
