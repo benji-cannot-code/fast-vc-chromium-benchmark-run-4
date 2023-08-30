2670ccab3bb0933457d20f7df28b232fa31138f5
Alexander Bolodurin <alexbn@chromium.org>
2023-08-30 09:49:28 +0000

    [office] dedupe Google Drive open errors
    Centralise Google Drive open result UMA logging. This moves
    OpenNewTabForHostedOfficeFile into cloud_upload_dialog.cc close to the
    rest of UMA logging logic (it's only used there anyway).
    
    Bug: b/296950967
    Change-Id: I474fe408f64f2f359073a31e65c39d30f4cf358a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4821247
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190020}

--- chrome/browser/ash/file_manager/open_with_browser.h
+++ chrome/browser/ash/file_manager/open_with_browser.h
@@ -9,9 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_ASH_FILE_MANAGER_OPEN_WITH_BROWSER_H_
 
 #include "base/files/file_path.h"
-#include "url/gurl.h"
 
-class GURL;
 class Profile;
 
 namespace storage {
@@ -34,11 +32,6 @@ namespace util {
 bool OpenFileWithBrowser(Profile* profile,
                          const storage::FileSystemURL& file_system_url,
                          const std::string& action_id);
-
-// Opens the file specified by |url| in a new tab. |url| must be a
-// docs.google.com URL for an office file. Returns true if there were no errors
-// opening the URL, false otherwise.
-bool OpenNewTabForHostedOfficeFile(const GURL& url);
 
 }  // namespace util
 }  // namespace file_manager
