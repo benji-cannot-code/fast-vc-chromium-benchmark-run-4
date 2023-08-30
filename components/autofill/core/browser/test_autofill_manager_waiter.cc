12181e1c8139451fe08f8256436e22f1eae348dc
Christoph Schwering <schwering@google.com>
2023-08-30 12:33:02 +0000

    [Autofill] Add single-event waiter for AutofillManager
    This CL adds a utility function for tests to block until an
    AutofillManager::Observer event happens:
    
      base::OnceCallback wait = WaitForEvent(
          autofill_manager, &AutofillManager::OnFoo, FieldsAre(args...));
      ...  // trigger OnFoo()
      EXPECT_TRUE(std::move(wait).Run());
    
    Bug: 1119526, 1476270
    Change-Id: I769d3e0df64b6b2c306c4ada9efaeb00b03c2ec7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4814868
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1190061}

--- components/autofill/core/browser/test_autofill_manager_waiter.cc
+++ components/autofill/core/browser/test_autofill_manager_waiter.cc
@@ -5,8 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/core/browser/test_autofill_manager_waiter.h"
 
+#include <vector>
+
 #include "base/check_op.h"
-#include "base/containers/contains.h"
 #include "base/logging.h"
 #include "base/memory/raw_ptr.h"
 #include "base/strings/string_util.h"
