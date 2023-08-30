e3513240020c4a1cd2fc9ef4b02dc6c0606467ba
Benjamin Williams <bwwilliams@google.com>
2023-08-30 16:17:28 +0000

    [iOS] Fix Windows epoch bug in new Safety Check module.
    If the Safety Check has never been run before, the new Safety Check
    module will use the default, unset Last Run Time, which is the Windows
    epoch (January 1, 1601). For new Safety Check users, the Last Run Time
    of the Safety Check will, incorrectly & confusingly, display "422 years
    ago".
    
    This change introduces a bug fix via:
    
    1. Updating the SafetyCheckState `lastRunTime` to
    absl::optional<base::Time>.
    
    2. Displaying the Last Run Time only if the run happened within the last
    24 hours.
    
    Change-Id: I46a30df4b21ad60d526d36e7b9bbde7767d5fbc5
    Fixed: 1476513
    Low-Coverage-Reason: Tests coming soon.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4818526
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190159}

--- ios/chrome/browser/ui/content_suggestions/safety_check/safety_check_state.h
+++ ios/chrome/browser/ui/content_suggestions/safety_check/safety_check_state.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_UI_CONTENT_SUGGESTIONS_SAFETY_CHECK_SAFETY_CHECK_STATE_H_
 
 #import "base/time/time.h"
+#import "third_party/abseil-cpp/absl/types/optional.h"
 
 #import <UIKit/UIKit.h>
 
@@ -48,7 +49,7 @@ enum class RunningSafetyCheckState;
 @property(nonatomic, assign) NSInteger compromisedPasswordsCount;
 
 // The last run time of the Safety Check.
-@property(nonatomic, assign) base::Time lastRunTime;
+@property(nonatomic, assign) absl::optional<base::Time> lastRunTime;
 
 @end
 
