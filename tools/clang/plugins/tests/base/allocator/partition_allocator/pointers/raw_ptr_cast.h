ed1cd8880d13efeb5e3c305751909381c891c42d
mikt <mikt@google.com>
2023-08-30 04:34:05 +0000

    [BRP] Use universal references in raw_ptr_cast.h
    We need to use it to support cast of references.
    This CL fixes following casts:
    
    struct MyInt {
        int val;
    };
    struct DerivedInt : MyInt {};
    void f(DerivedInt& ref) {
        (void) unsafe_raw_ptr_static_cast<MyInt&>(ref);    // Fixed UB
        (void) unsafe_raw_ptr_reinterpret_cast<int&>(ref); // Fixed UB
    }
    
    Prior to this CL, |Source| template type argument was deduced as
    |DerivedInt|, not |DerivedInt&|. That makes |unsafe_raw_ptr_***_cast| to
    return a reference to stack-allocated function argument and its usage is
    UB.
    
    Bug: 1475895
    Change-Id: Iec0e607bac8e37c08df9b92eb78d31e7f4d3fc4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4812134
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189925}

--- tools/clang/plugins/tests/base/allocator/partition_allocator/pointers/raw_ptr_cast.h
+++ tools/clang/plugins/tests/base/allocator/partition_allocator/pointers/raw_ptr_cast.h
@@ -9,12 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 
 template <typename Dest, typename Source>
-inline constexpr Dest unsafe_raw_ptr_static_cast(Source source) {
+inline constexpr Dest unsafe_raw_ptr_static_cast(Source&& source) {
   return static_cast<Dest>(source);
 }
 
 template <typename Dest, typename Source>
-inline constexpr Dest unsafe_raw_ptr_reinterpret_cast(Source source) {
+inline constexpr Dest unsafe_raw_ptr_reinterpret_cast(Source&& source) {
   return reinterpret_cast<Dest>(source);
 }
 
