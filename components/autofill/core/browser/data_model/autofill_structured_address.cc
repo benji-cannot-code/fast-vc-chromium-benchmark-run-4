ffb7e1ab891396b3284df5a8e42043819493a113
Peter Kotwicz <pkotwicz@chromium.org>
2023-08-30 20:41:09 +0000

    [Autofill][Refactor] Remove redundant autofill::RewriterCache
    autofill::RewriterCache is redundant with the cache in
    address_rewriter.cc
    
    BUG=None
    
    Change-Id: I75fd7238307e127004eab99c3c92c1a1ab446b5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4729209
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190300}

--- components/autofill/core/browser/data_model/autofill_structured_address.cc
+++ components/autofill/core/browser/data_model/autofill_structured_address.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "components/autofill/core/browser/address_rewriter.h"
 #include "components/autofill/core/browser/autofill_type.h"
 #include "components/autofill/core/browser/data_model/autofill_structured_address_component.h"
 #include "components/autofill/core/browser/data_model/autofill_structured_address_regex_provider.h"
@@ -26,20 +27,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
-namespace {
-// Applies the |country_code| specific rewriter to the normalized value. If
-// |country_code| is empty, it defaults to US.
-std::u16string RewriteValue(const std::u16string& value,
-                            const std::u16string& country_code) {
-  return RewriterCache::Rewrite(country_code.empty() ? u"US" : country_code,
-                                value);
-}
-}  // namespace
-
 std::u16string AddressComponentWithRewriter::GetValueForComparison(
     const std::u16string& value,
     const AddressComponent& other) const {
-  return RewriteValue(NormalizeValue(value), GetCommonCountry(other));
+  return NormalizeAndRewrite(GetCommonCountry(other), value,
+                             /*keep_white_space=*/true);
 }
 
 StreetNameNode::StreetNameNode(AddressComponent* parent)
@@ -341,8 +333,8 @@ std::u16string PostalCodeNode::GetNormalizedValue() const {
 std::u16string PostalCodeNode::GetValueForComparison(
     const std::u16string& value,
     const AddressComponent& other) const {
-  return RewriteValue(NormalizeValue(value, /*keep_white_space=*/false),
-                      GetCommonCountry(other));
+  return NormalizeAndRewrite(GetCommonCountry(other), value,
+                             /*keep_white_space=*/false);
 }
 
 SortingCodeNode::SortingCodeNode(AddressComponent* parent)
