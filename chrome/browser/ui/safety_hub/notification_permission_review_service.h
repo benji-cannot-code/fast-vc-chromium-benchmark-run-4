5cc5a881faf915cde68e36a09291c6b429e64053
Side Yilmaz <sideyilmaz@chromium.org>
2023-08-30 10:05:05 +0000

    Bring notification info string back
    Notification info string disappeared in crrev.com/c/4665964. This
    CL brings it back and adds test for the detail string.
    
    Bug: 1443466
    Change-Id: I6d28eb36f7c6a1e5798972647915dffea64c239f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4819966
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Side YILMAZ <sideyilmaz@chromium.org>
    Reviewed-by: Tom Van Goethem <tov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190024}

--- chrome/browser/ui/safety_hub/notification_permission_review_service.h
+++ chrome/browser/ui/safety_hub/notification_permission_review_service.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/content_settings_pattern.h"
 #include "components/keyed_service/core/keyed_service.h"
 
+constexpr char kSafetyHubNotificationInfoString[] = "notificationInfoString";
+
 struct NotificationPermissions {
   ContentSettingsPattern primary_pattern;
   ContentSettingsPattern secondary_pattern;
