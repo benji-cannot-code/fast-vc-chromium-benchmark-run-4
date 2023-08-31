39a5b58a856e2dfde0fc2386edc928245ede32e3
John Palmer <jopalmer@chromium.org>
2023-08-31 01:53:22 +0000

    Diacritics: Restrict rollout to Managed users in NZ
    Rollout was always meant to be managed in NZ only, but accidentally made
    it everyone in NZ. Fixing that now.
    
    Fixed: b:298145548
    Change-Id: I64900c8564c8fb1a882f3be4850b4f27b9602d70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4829262
    Auto-Submit: John Palmer <jopalmer@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190478}

--- ash/keyboard/keyboard_controller_impl.cc
+++ ash/keyboard/keyboard_controller_impl.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/keyboard/ui/keyboard_ui_factory.h"
 #include "ash/keyboard/virtual_keyboard_controller.h"
 #include "ash/public/cpp/keyboard/keyboard_switches.h"
+#include "ash/public/cpp/login_types.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/root_window_controller.h"
 #include "ash/session/session_controller_impl.h"
@@ -21,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/shell_delegate.h"
 #include "ash/system/input_device_settings/input_device_settings_controller_impl.h"
+#include "ash/system/model/enterprise_domain_model.h"
+#include "ash/system/model/system_tray_model.h"
 #include "ash/wm/window_util.h"
 #include "base/command_line.h"
 #include "base/feature_list.h"
@@ -99,10 +102,15 @@ KeyboardControllerImpl::~KeyboardControllerImpl() {
 // static
 void KeyboardControllerImpl::RegisterProfilePrefs(PrefRegistrySimple* registry,
                                                   std::string_view country) {
-  // Longpress diacritics pref is default on for NZ only, default off otherwise
+  // Longpress diacritics pref is default on for NZ managed users only, default
+  // off otherwise.
   registry->RegisterBooleanPref(
       ash::prefs::kLongPressDiacriticsEnabled,
-      country == "NZ" ||
+      (country == "NZ" &&
+       Shell::Get()
+               ->system_tray_model()
+               ->enterprise_domain()
+               ->management_device_mode() == ManagementDeviceMode::kNone) ||
           base::FeatureList::IsEnabled(
               ash::features::kDiacriticsOnPhysicalKeyboardLongpressDefaultOn),
       user_prefs::PrefRegistrySyncable::SYNCABLE_OS_PREF);
