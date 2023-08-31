1f8dd83ae5978f58fd52a5275e343c3c3e0e3ba3
Nick Diego Yamane <nickdiego@igalia.com>
2023-08-31 15:10:07 +0000

    wayland: standardize event listeners in xdg object wrappers
    - Use 'On' prefix + event-name-in-camel-case pattern for event handler
      callbacks
    - Use constexpr + kPrefixedConstName for listener vars
    - Use designated initializers for listener vars
    - Remove 'struct' keyword in function/var decls unless strictly needed.
    
    R=fangzhoug, tonikitoo@igalia.com
    
    Test: No functional changes expected.
    Bug: 1472873
    Change-Id: I59c7058b4230dd3677d958a1ba026ed60efdcf20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4824482
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Auto-Submit: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1190759}

--- ui/ozone/platform/wayland/host/xdg_popup_wrapper_impl.h
+++ ui/ozone/platform/wayland/host/xdg_popup_wrapper_impl.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_OZONE_PLATFORM_WAYLAND_HOST_XDG_POPUP_WRAPPER_IMPL_H_
 #define UI_OZONE_PLATFORM_WAYLAND_HOST_XDG_POPUP_WRAPPER_IMPL_H_
 
+#include <cstdint>
 #include <memory>
 
 #include "base/memory/raw_ptr.h"
@@ -44,17 +45,15 @@ class XDGPopupWrapperImpl : public ShellPopupWrapper {
  private:
   wl::Object<xdg_positioner> CreatePositioner();
 
-  // xdg_popup_listener
-  static void Configure(void* data,
-                              struct xdg_popup* xdg_popup,
-                              int32_t x,
-                              int32_t y,
-                              int32_t width,
-                              int32_t height);
-  static void PopupDone(void* data, struct xdg_popup* xdg_popup);
-  static void Repositioned(void* data,
-                           struct xdg_popup* xdg_popup,
-                           uint32_t token);
+  // xdg_popup_listener callbacks:
+  static void OnConfigure(void* data,
+                          xdg_popup* popup,
+                          int32_t x,
+                          int32_t y,
+                          int32_t width,
+                          int32_t height);
+  static void OnPopupDone(void* data, xdg_popup* popup);
+  static void OnRepositioned(void* data, xdg_popup* popup, uint32_t token);
 
   XDGSurfaceWrapperImpl* xdg_surface_wrapper() const;
 
