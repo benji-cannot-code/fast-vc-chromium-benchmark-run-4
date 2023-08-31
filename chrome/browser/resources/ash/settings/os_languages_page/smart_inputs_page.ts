1f0f54921a5b84676b45fbfb1169eb656c7bfb83
mlcui <mlcui@google.com>
2023-08-31 04:02:08 +0000

    os_languages_page: Remove redundant mixin props from properties()
    By doing so, we also moved some default properties specified in the
    `value()` of some properties to be field initializers.
    
    Bug: b:265556004
    Fixed: b:265554350
    Change-Id: If410faca89f17cd7e78e293567a6128239e5b949
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4823511
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1190522}

--- chrome/browser/resources/ash/settings/os_languages_page/smart_inputs_page.ts
+++ chrome/browser/resources/ash/settings/os_languages_page/smart_inputs_page.ts
@@ -39,14 +39,6 @@ export class OsSettingsSmartInputsPageElement extends
 
   static get properties() {
     return {
-      // TODO(b/265554350): Remove this property from properties() as it is
-      // already specified in PrefsMixin.
-      /** Preferences state. */
-      prefs: {
-        type: Object,
-        notify: true,
-      },
-
       allowEmojiSuggestion_: {
         type: Boolean,
         value() {
@@ -60,16 +52,6 @@ export class OsSettingsSmartInputsPageElement extends
           return loadTimeData.getBoolean('allowOrca');
         },
       },
-
-      /**
-       * Used by DeepLinkingMixin to focus this page's deep links.
-       */
-      supportedSettingIds: {
-        type: Object,
-        value: () => new Set<Setting>([
-          Setting.kShowEmojiSuggestions,
-        ]),
-      },
     };
   }
 
@@ -78,9 +60,9 @@ export class OsSettingsSmartInputsPageElement extends
 
   // Internal properties for mixins.
   // From DeepLinkingMixin.
-  // override supportedSettingIds = new Set<Setting>([
-  //   Setting.kShowEmojiSuggestions,
-  // ]);
+  override supportedSettingIds = new Set([
+    Setting.kShowEmojiSuggestions,
+  ]);
 
   // loadTimeData flags.
   private allowEmojiSuggestion_: boolean;
