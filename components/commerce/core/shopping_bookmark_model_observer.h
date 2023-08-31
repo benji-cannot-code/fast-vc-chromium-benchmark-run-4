7bd797c4befd7b7609fa41facc264734d90ab8fb
Boris Sazonov <bsazonov@chromium.org>
2023-08-31 09:06:54 +0000

    [UNO] Use Uuid in ShoppingBookmarkModelObserver
    Similar to https://crrev.com/c/4720871, but for
    ShoppingBookmarkModelObserver.
    
    Bug: 1425818
    Change-Id: I2a6722f55c78d8c67b27fa29d77d06eaa1f96b52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4828023
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190609}

--- components/commerce/core/shopping_bookmark_model_observer.h
+++ components/commerce/core/shopping_bookmark_model_observer.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/scoped_observation.h"
+#include "base/uuid.h"
 #include "components/bookmarks/browser/base_bookmark_model_observer.h"
 #include "components/bookmarks/browser/bookmark_model.h"
 
@@ -77,9 +78,9 @@ class ShoppingBookmarkModelObserver
 
   raw_ptr<SubscriptionsManager> subscriptions_manager_;
 
-  // A map of bookmark ID to its current URL. This is used to detect incoming
+  // A map of bookmark Uuid to its current URL. This is used to detect incoming
   // changes to the URL since there isn't an explicit event for it.
-  std::map<int64_t, GURL> node_to_url_map_;
+  std::map<base::Uuid, GURL> node_to_url_map_;
 
   // Track the title of the shopping collection if we get a signal that the node
   // will change.
