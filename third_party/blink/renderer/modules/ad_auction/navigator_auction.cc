a49a5da8596d0ebf96ad74144768deba6fedd744
Maks Orlovich <morlovich@chromium.org>
2023-08-31 14:58:51 +0000

    FLEDGE: Change additional bids to header-based.
    The bids are now not provided via additional bids promise, but rather by special headers on a fetch from the seller (which proves that the seller provided by them), with promise now just signaling that the headers should be available now, and hence losing the payload, in both JS and Mojo.
    
    As part of this, enforce that expects_additional_bids requires auction_nonce (which is what headers are keyed by), with a blink error and mojo traits check.
    
    Bug: 1464874, 1475652
    Change-Id: I8b730f2d6d3f276a146bea2236404784ba1ca3ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4815507
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190753}

--- third_party/blink/renderer/modules/ad_auction/navigator_auction.cc
+++ third_party/blink/renderer/modules/ad_auction/navigator_auction.cc
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/v8_auction_ad_interest_group.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_auction_ad_interest_group_key.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_auction_ad_interest_group_size.h"
-#include "third_party/blink/renderer/bindings/modules/v8/v8_auction_additional_bid_config.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_auction_additional_bid_signature.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_auction_report_buyers_config.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_union_adproperties_adpropertiessequence.h"
@@ -1465,15 +1464,24 @@ void CopyDirectFromSellerSignalsHeaderAdSlotFromIdlToMojo(
   output.expects_direct_from_seller_signals_header_ad_slot = true;
 }
 
-void CopyAdditionalBidsFromIdlToMojo(
+bool CopyAdditionalBidsFromIdlToMojo(
     NavigatorAuction::AuctionHandle* auction_handle,
     const mojom::blink::AuctionAdConfigAuctionId* auction_id,
     ScriptState& script_state,
+    ExceptionState& exception_state,
     const AuctionAdConfig& input,
     mojom::blink::AuctionAdConfig& output) {
   if (!input.hasAdditionalBids()) {
     output.expects_additional_bids = false;
-    return;
+    return true;
+  }
+
+  if (!input.hasAuctionNonce()) {
+    exception_state.ThrowTypeError(
+        String::Format("additionalBids specified for AuctionAdConfig with "
+                       "seller '%s' which does not have an auctionNonce.",
+                       input.seller().Utf8().c_str()));
+    return false;
   }
 
   auction_handle->AttachPromiseHandler(
@@ -1482,6 +1490,7 @@ void CopyAdditionalBidsFromIdlToMojo(
           NavigatorAuction::AuctionHandle::AdditionalBidsResolved>(
           auction_handle, auction_id->Clone(), input.seller()));
   output.expects_additional_bids = true;
+  return true;
 }
 
 // Returns nullopt + sets exception on failure, or returns a concrete value.
@@ -2037,7 +2046,10 @@ mojom::blink::AuctionAdConfigPtr IdlAuctionConfigToMojo(
       !CopyRequestedSizeFromIdlToMojo(context, exception_state, config,
                                       *mojo_config) ||
       !CopyAuctionNonceFromIdlToMojo(context, exception_state, config,
-                                     *mojo_config)) {
+                                     *mojo_config) ||
+      !CopyAdditionalBidsFromIdlToMojo(auction_handle, auction_id.get(),
+                                       script_state, exception_state, config,
+                                       *mojo_config)) {
     return mojom::blink::AuctionAdConfigPtr();
   }
 
@@ -2067,8 +2079,6 @@ mojom::blink::AuctionAdConfigPtr IdlAuctionConfigToMojo(
       auction_handle, auction_id.get(), script_state, config, *mojo_config);
   CopyPerBuyerCurrenciesFromIdlToMojo(auction_handle, auction_id.get(),
                                       script_state, config, *mojo_config);
-  CopyAdditionalBidsFromIdlToMojo(auction_handle, auction_id.get(),
-                                  script_state, config, *mojo_config);
 
   if (mojo_config->server_response && !is_top_level) {
     // TODO(1457241): Add support for multi-level auctions including server-side
@@ -2723,70 +2733,7 @@ ScriptValue NavigatorAuction::AuctionHandle::AdditionalBidsResolved::Call(
     return ScriptValue();
   }
 
-  ExceptionState exception_state(script_state->GetIsolate(),
-                                 ExceptionState::kExecutionContext,
-                                 "NavigatorAuction", "runAdAuction");
-  HeapVector<Member<AuctionAdditionalBidConfig>> additional_bids;
-  bool ok = false;
-
-  if (!value.IsEmpty()) {
-    additional_bids =
-        NativeValueTraits<IDLSequence<AuctionAdditionalBidConfig>>::NativeValue(
-            script_state->GetIsolate(), value.V8Value(), exception_state);
-
-    ok = true;
-    // Check the array dimensions.
-    for (const auto& entry : additional_bids) {
-      for (wtf_size_t s = 0; s < entry->signatures().size(); ++s) {
-        const auto& sig = entry->signatures()[s];
-        if (sig->key()->length() != 32) {
-          exception_state.ThrowTypeError(String::Format(
-              "'additionalBids.signatures[%d].key' for AuctionAdConfig with "
-              "seller '%s' must be 32 bytes long.",
-              static_cast<int>(s), seller_name_.Utf8().c_str()));
-          ok = false;
-          break;
-        }
-
-        if (sig->signature()->length() != 64) {
-          exception_state.ThrowTypeError(String::Format(
-              "'additionalBids.signatures[%d].signature' for AuctionAdConfig "
-              "with seller '%s' must be 64 bytes long.",
-              static_cast<int>(s), seller_name_.Utf8().c_str()));
-          ok = false;
-          break;
-        }
-      }
-      if (!ok) {
-        break;
-      }
-    }
-  }
-
-  if (ok && !exception_state.HadException()) {
-    WTF::Vector<mojom::blink::AuctionAdConfigAdditionalBidPtr>
-        converted_additional_bids;
-    for (const auto& entry : additional_bids) {
-      auto additional_bid = mojom::blink::AuctionAdConfigAdditionalBid::New();
-      additional_bid->bid = entry->bid();
-      for (const auto& sig : entry->signatures()) {
-        additional_bid->signatures.push_back(
-            mojom::blink::AuctionAdConfigAdditionalBidSignature::New());
-        additional_bid->signatures.back()->key.Append(
-            sig->key()->Data(), static_cast<wtf_size_t>(sig->key()->length()));
-        additional_bid->signatures.back()->signature.Append(
-            sig->signature()->Data(),
-            static_cast<wtf_size_t>(sig->signature()->length()));
-      }
-
-      converted_additional_bids.push_back(std::move(additional_bid));
-    }
-
-    auction_handle()->mojo_pipe()->ResolvedAdditionalBids(
-        auction_id_->Clone(), std::move(converted_additional_bids));
-  } else {
-    auction_handle()->Abort();
-  }
+  auction_handle()->mojo_pipe()->ResolvedAdditionalBids(auction_id_->Clone());
 
   return ScriptValue();
 }
