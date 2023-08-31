6856a1cece114ebf578badafc844d36383af94e6
Finnur Thorarinsson <finnur@chromium.org>
2023-08-31 17:47:35 +0000

    [MediaPicker]: Launch the new picker on Android U.
    The study flavor we settled on was via ACTION_GET_CONTENT,
    also known as the Indirect method.
    
    This CL also changes the fallback when launching
    Indirect fails to DirectPlus, which is a better
    alternative than Direct (Direct is more permissive in
    allowing MIME-types the website doesn't necessarily handle,
    which leads to over-counting).
    
    logic, the changes are trivial and the file is scheduled
    to be trimmed quite a bit in the aftermath of the launch
    (and deleted after that).
    
    Bug: launch/4242276
    Change-Id: If543ee0ce71b472342d685a79337809bef9a4c5e
    Low-Coverage-Reason: The file in question has very little
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4822584
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190865}

--- components/browser_ui/photo_picker/android/java/src/org/chromium/components/browser_ui/photo_picker/PhotoPickerFeatures.java
+++ components/browser_ui/photo_picker/android/java/src/org/chromium/components/browser_ui/photo_picker/PhotoPickerFeatures.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.browser_ui.photo_picker;
 
+import android.os.Build;
+
 import org.chromium.base.Features;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.NativeMethods;
@@ -30,21 +32,38 @@ public class PhotoPickerFeatures extends Features {
     private final int mOrdinal;
 
     public static boolean launchViaActionGetContent() {
+        // ActionGetContent is the study flavor we want to launch with on Android U.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            return true;
+        }
+
         return ANDROID_MEDIA_PICKER_ADOPTION.getFieldTrialParamByFeatureAsBoolean(
                 PARAM_ACTION_GET_CONTENT, false);
     }
 
     public static boolean launchViaActionPickImages() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            return false;
+        }
+
         return ANDROID_MEDIA_PICKER_ADOPTION.getFieldTrialParamByFeatureAsBoolean(
                 PARAM_ACTION_PICK_IMAGES, false);
     }
 
     public static boolean launchViaActionPickImagesPlus() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            return false;
+        }
+
         return ANDROID_MEDIA_PICKER_ADOPTION.getFieldTrialParamByFeatureAsBoolean(
                 PARAM_ACTION_PICK_IMAGES_PLUS, false);
     }
 
     public static boolean launchRegularWithoutBrowse() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            return false;
+        }
+
         return ANDROID_MEDIA_PICKER_ADOPTION.getFieldTrialParamByFeatureAsBoolean(
                 PARAM_CHROME_PICKER_SUPPRESS_BROWSE, false);
     }
