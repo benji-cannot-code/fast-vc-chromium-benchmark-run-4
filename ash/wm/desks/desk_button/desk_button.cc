960c7d79650b26944de635847f9383977e2b695d
Hongyu Long <hongyulong@chromium.org>
2023-08-31 21:00:07 +0000

    bento_button: Update abbreviated desk name for desk button
    This CL will change the abbreviated desk name for customized desk names
    with the rule that the first/left unicode will be selected. Also, we don't converse the character selected to the uppercase type.
    
    Bug: b/296111590
    Test: Manul & unittest
    Change-Id: I52bb2af123e07ba553b0bcfeb0f30977377ec6e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4828866
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Reviewed-by: Yongshun Liu <yongshun@chromium.org>
    Commit-Queue: Hongyu Long <hongyulong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190976}

--- ash/wm/desks/desk_button/desk_button.cc
+++ ash/wm/desks/desk_button/desk_button.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/desks/desk_bar_controller.h"
 #include "ash/wm/desks/desks_constants.h"
 #include "ash/wm/desks/desks_controller.h"
-#include "base/i18n/case_conversion.h"
+#include "base/i18n/break_iterator.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/string_number_conversions.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -435,11 +435,20 @@ void DeskButton::CalculateDisplayNames(const Desk* desk) {
   }
 
   desk_name_ = desk->name();
+  base::i18n::BreakIterator iter(desk_name_,
+                                 base::i18n::BreakIterator::BREAK_CHARACTER);
 
-  if (desk->is_name_set_by_user() &&
-      ((desk_name_[0] >= u'a' && desk_name_[0] <= u'z') ||
-       (desk_name_[0] >= u'A' && desk_name_[0] <= u'Z'))) {
-    abbreviated_desk_name_ = base::i18n::ToUpper(desk_name_.substr(0, 1));
+  if (!iter.Init()) {
+    return;
+  }
+
+  if (desk->is_name_set_by_user()) {
+    // For the customized desk name, it shows the first unicode for the
+    // `abbreviated_desk_name_`.
+    if (!iter.Advance()) {
+      return;
+    }
+    abbreviated_desk_name_ = iter.GetString();
   } else {
     abbreviated_desk_name_ =
         u"#" + base::NumberToString16(
