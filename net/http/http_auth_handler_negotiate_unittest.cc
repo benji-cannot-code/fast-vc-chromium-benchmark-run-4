2597d0218f052173b83819f89320b9ba0e0ee7df
Matthew Denton <mpdenton@chromium.org>
2023-08-31 04:29:02 +0000

    Extend net::HttpAuthPreferences::AllowGssapiLibraryLoad() to Linux
    GSSAPI library loading can't be available in a sandboxed network
    service, so this CL extends AllowGssapiLibraryLoad() to Linux desktop.
    AllowGssapiLibraryLoad() is also a useful signal that the user might
    want kerberos in which case we will want to disable the Linux network
    service sandbox (this is left to a future CL).
    
    Change-Id: Ie44e855efa8c579345ed7bb8f7713a70088ada96
    Bug: 1474362, 1079808
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798250
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190532}

--- net/http/http_auth_handler_negotiate_unittest.cc
+++ net/http/http_auth_handler_negotiate_unittest.cc
@@ -473,8 +473,8 @@ TEST_F(HttpAuthHandlerNegotiateTest, MissingGSSAPI) {
 }
 #endif  // BUILDFLAG(USE_EXTERNAL_GSSAPI)
 
-// AllowGssapiLibraryLoad() is only supported on ChromeOS.
-#if BUILDFLAG(IS_CHROMEOS)
+// AllowGssapiLibraryLoad() is only supported on ChromeOS and Linux.
+#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
 TEST_F(HttpAuthHandlerNegotiateTest, AllowGssapiLibraryLoad) {
   // Disabling allow_gssapi_library_load should prevent handler creation.
   SetupMocks(AuthLibrary());
@@ -490,7 +490,7 @@ TEST_F(HttpAuthHandlerNegotiateTest, AllowGssapiLibraryLoad) {
   EXPECT_EQ(OK, rv);
   EXPECT_TRUE(auth_handler);
 }
-#endif  // BUILDFLAG(IS_CHROMEOS)
+#endif  // BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
 
 #endif  // BUILDFLAG(IS_POSIX)
 
