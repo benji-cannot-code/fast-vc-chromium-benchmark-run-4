90d7cdef9299732a8558222ed745ae016c582cb3
Thomas Quintanilla <tquintanilla@google.com>
2023-08-31 13:44:45 +0000

    Updates protocol to take new event report windows field
    Change-Id: Ifb49991908ab5a60d1398432a6b0178c4884d1a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4826052
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Thomas Quintanilla <tquintanilla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190710}

--- third_party/blink/web_tests/http/tests/inspector-protocol/attribution-reporting/resources/register-source-filter-data-and-agg-keys.php
+++ third_party/blink/web_tests/http/tests/inspector-protocol/attribution-reporting/resources/register-source-filter-data-and-agg-keys.php
@@ -1,4 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
-header('Attribution-Reporting-Register-Source: {"source_event_id":"123","destination":"https://a.example","filter_data":{"a":["b","c"]},"aggregation_keys":{"d":"0xef"}}');
+header('Attribution-Reporting-Register-Source: {"source_event_id":"123","destination":"https://a.example","filter_data":{"a":["b","c"]},"aggregation_keys":{"d":"0xef"},"event_report_windows":{"start_time":0,"end_times":[86400]}}');
 ?>
