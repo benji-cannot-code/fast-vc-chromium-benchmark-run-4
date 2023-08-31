68c02a528452770003bd4c74b607c64cd34c8fad
Yoav Weiss <yoavweiss@chromium.org>
2023-08-31 10:11:57 +0000

    Avoid allocating RecordId objects in ElementTiming and LCP
    RecordId objects in current code keep around references to LayoutObject
    and ImageResourceContent, both GCed objects.
    Turn out that most of these references are not needed and are only used
    as hashmap keys that would be better served with an actual hash. The
    ones that are needed don't need extensive lifetimes and can be stack
    allocated.
    
    Bug=1472365,1472366
    
    Change-Id: I3fd77bed9899932d5bfadc2a8e6403a8e434235f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4821128
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190644}

--- third_party/blink/renderer/core/paint/timing/image_element_timing_test.cc
+++ third_party/blink/renderer/core/paint/timing/image_element_timing_test.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_image.h"
 #include "third_party/blink/renderer/core/layout/svg/layout_svg_image.h"
 #include "third_party/blink/renderer/core/loader/resource/image_resource_content.h"
+#include "third_party/blink/renderer/core/paint/timing/media_record_id.h"
 #include "third_party/blink/renderer/platform/graphics/unaccelerated_static_bitmap_image.h"
 #include "third_party/blink/renderer/platform/testing/paint_test_configurations.h"
 #include "third_party/blink/renderer/platform/testing/url_test_helpers.h"
@@ -61,11 +62,9 @@ class ImageElementTimingTest : public testing::Test,
     return nullptr;
   }
 
-  bool ImagesNotifiedContains(
-      const std::pair<const LayoutObject*, const ImageResourceContent*>&
-          record_id) {
+  bool ImagesNotifiedContains(MediaRecordIdHash record_id_hash) {
     return ImageElementTiming::From(*GetDoc()->domWindow())
-        .images_notified_.Contains(record_id);
+        .images_notified_.Contains(record_id_hash);
   }
 
   unsigned ImagesNotifiedSize() {
@@ -160,7 +159,7 @@ TEST_P(ImageElementTimingTest, IgnoresUnmarkedElement) {
   ASSERT_TRUE(layout_image);
   UpdateAllLifecyclePhases();
   EXPECT_FALSE(ImagesNotifiedContains(
-      std::make_pair(layout_image, layout_image->CachedImage())));
+      MediaRecordId::GenerateHash(layout_image, layout_image->CachedImage())));
 }
 
 TEST_P(ImageElementTimingTest, ImageInsideSVG) {
@@ -180,7 +179,7 @@ TEST_P(ImageElementTimingTest, ImageInsideSVG) {
 
   // |layout_image| should have had its paint notified to ImageElementTiming.
   EXPECT_TRUE(ImagesNotifiedContains(
-      std::make_pair(layout_image, layout_image->CachedImage())));
+      MediaRecordId::GenerateHash(layout_image, layout_image->CachedImage())));
 }
 
 TEST_P(ImageElementTimingTest, ImageInsideNonRenderedSVG) {
@@ -215,7 +214,7 @@ TEST_P(ImageElementTimingTest, ImageRemoved) {
   ASSERT_TRUE(layout_image);
   UpdateAllLifecyclePhases();
   EXPECT_TRUE(ImagesNotifiedContains(
-      std::make_pair(layout_image, layout_image->CachedImage())));
+      MediaRecordId::GenerateHash(layout_image, layout_image->CachedImage())));
 
   GetDoc()->getElementById(AtomicString("target"))->remove();
   // |layout_image| should no longer be part of |images_notified| since it will
@@ -235,7 +234,7 @@ TEST_P(ImageElementTimingTest, SVGImageRemoved) {
   LayoutSVGImage* layout_image = SetSVGImageResource("target", 5, 5);
   ASSERT_TRUE(layout_image);
   UpdateAllLifecyclePhases();
-  EXPECT_TRUE(ImagesNotifiedContains(std::make_pair(
+  EXPECT_TRUE(ImagesNotifiedContains(MediaRecordId::GenerateHash(
       layout_image, layout_image->ImageResource()->CachedImage())));
 
   GetDoc()->getElementById(AtomicString("target"))->remove();
@@ -262,7 +261,8 @@ TEST_P(ImageElementTimingTest, BackgroundImageRemoved) {
       object->Style()->BackgroundLayers().GetImage()->CachedImage();
   UpdateAllLifecyclePhases();
   EXPECT_EQ(ImagesNotifiedSize(), 1u);
-  EXPECT_TRUE(ImagesNotifiedContains(std::make_pair(object, content)));
+  EXPECT_TRUE(
+      ImagesNotifiedContains(MediaRecordId::GenerateHash(object, content)));
 
   GetDoc()->getElementById(AtomicString("target"))->remove();
   EXPECT_EQ(ImagesNotifiedSize(), 0u);
--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -73,6 +73,7 @@ _CONFIG = [
             'base::DefaultTickClock',
             'base::ElapsedTimer',
             'base::EnumSet',
+            'base::HashInts',
             'base::JobDelegate',
             'base::JobHandle',
             'base::PostJob',
