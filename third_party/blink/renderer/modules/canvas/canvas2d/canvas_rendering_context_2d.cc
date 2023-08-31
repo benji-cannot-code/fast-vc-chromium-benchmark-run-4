a352f69caa3b1d516d8604d0dfaa9a9fdb8a298e
Justin Novosad <junov@chromium.org>
2023-08-31 21:18:52 +0000

    Stop using Canvas2dLayerBridge to access CanvasResourceProvider
    Code cleanup.  Going through canvas 2d layer bridge to access
    the canvas resource provider is no longer necessary and add unnecessary
    indirections and complexity.
    
    Bug: 1476964
    Change-Id: I9f6ea6c51e8ed431c4bfc49c9b08fb6848441c59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4823264
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Auto-Submit: Justin Novosad <junov@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190981}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
@@ -379,13 +379,9 @@ cc::PaintCanvas* CanvasRenderingContext2D::GetOrCreatePaintCanvas() {
   if (UNLIKELY(isContextLost()))
     return nullptr;
   if (LIKELY(canvas()->GetOrCreateCanvas2DLayerBridge())) {
-    if (LIKELY(!layer_count_) &&
-        LIKELY(canvas()->GetCanvas2DLayerBridge()->ResourceProvider())) {
+    if (LIKELY(!layer_count_) && LIKELY(Host()->ResourceProvider())) {
       // TODO(crbug.com/1246486): Make auto-flushing layer friendly.
-      canvas()
-          ->GetCanvas2DLayerBridge()
-          ->ResourceProvider()
-          ->FlushIfRecordingLimitExceeded();
+      Host()->ResourceProvider()->FlushIfRecordingLimitExceeded();
     }
     return canvas()->GetCanvas2DLayerBridge()->GetPaintCanvas();
   }
@@ -393,10 +389,9 @@ cc::PaintCanvas* CanvasRenderingContext2D::GetOrCreatePaintCanvas() {
 }
 
 cc::PaintCanvas* CanvasRenderingContext2D::GetPaintCanvas() {
-  if (UNLIKELY(isContextLost() || !canvas() ||
-               !canvas()->GetCanvas2DLayerBridge() ||
-               !canvas()->GetCanvas2DLayerBridge()->ResourceProvider()))
+  if (UNLIKELY(isContextLost() || !Host() || !Host()->ResourceProvider())) {
     return nullptr;
+  }
   return canvas()->GetCanvas2DLayerBridge()->GetPaintCanvas();
 }
 
@@ -407,18 +402,14 @@ void CanvasRenderingContext2D::WillDraw(
   // Always draw everything during printing.
   if (!layer_count_) {
     // TODO(crbug.com/1246486): Make auto-flushing layer friendly.
-    canvas()
-        ->GetCanvas2DLayerBridge()
-        ->ResourceProvider()
-        ->FlushIfRecordingLimitExceeded();
+    Host()->ResourceProvider()->FlushIfRecordingLimitExceeded();
   }
 }
 
 void CanvasRenderingContext2D::FlushCanvas(
     CanvasResourceProvider::FlushReason reason) {
-  if (canvas() && canvas()->GetCanvas2DLayerBridge() &&
-      canvas()->GetCanvas2DLayerBridge()->ResourceProvider()) {
-    canvas()->GetCanvas2DLayerBridge()->ResourceProvider()->FlushCanvas(reason);
+  if (Host() && Host()->ResourceProvider()) {
+    Host()->ResourceProvider()->FlushCanvas(reason);
   }
 }
 
--- third_party/blink/renderer/platform/graphics/canvas_2d_layer_bridge.h
+++ third_party/blink/renderer/platform/graphics/canvas_2d_layer_bridge.h
@@ -269,7 +269,6 @@ class PLATFORM_EXPORT Canvas2DLayerBridge : public cc::TextureLayerClient {
     logger_ = std::move(logger);
   }
   CanvasResourceProvider* GetOrCreateResourceProvider();
-  CanvasResourceProvider* ResourceProvider() const;
   void FlushRecording(CanvasResourceProvider::FlushReason);
 
   cc::PaintRecord* getLastRecord() {
@@ -293,6 +292,7 @@ class PLATFORM_EXPORT Canvas2DLayerBridge : public cc::TextureLayerClient {
 
   bool IsHidden() { return is_hidden_; }
   bool CheckResourceProviderValid();
+  CanvasResourceProvider* ResourceProvider() const;
   void ResetResourceProvider();
 
   void SkipQueuedDrawCommands();
