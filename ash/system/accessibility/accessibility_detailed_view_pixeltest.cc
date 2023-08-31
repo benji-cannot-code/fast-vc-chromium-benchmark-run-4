421762e64f4abd08ea3ebed6d7c7ebbe86b036b7
Michael Ludwig <michaelludwig@google.com>
2023-08-31 22:40:34 +0000

    Remove SK_USE_LEGACY_BLUR_IMAGEFILTER staging flag
    With the removal of this flag, CPU-based blurs will now respect the
    SkTileMode passed to the blur image filter. Both GPU and CPU blurs
    will tend to have more optimal image sizes (smaller) when using
    kDecal. kDecal should also use fewer renderpasses. There is a
    temporary regression in image size and renderpasses for other tile
    modes but this is a required step in the overall blur refactoring.
    
    It also fixes a bug where decal would be applied to the intersection
    of both the filter region and primitive subregion when performing
    blurs in SVGs. This is why there are larger changes to the
    `effect-reference-subregion`: the blur is able to read the hidden color cells of the input image, some of which are red so the overall blur color shifts from green to brownish.
    
    Most viz pixel tests have benign updates. However, the blur_filter_with_clip test is meant to have clamped content. The SW
    expected image had baked in the previous limitation of decal-only
    blurs which is no longer the case. However, the software_renderer
    compositor seems to be sizing images a little off so the clamp is only
    applying to the top and left edges. Now the new expected image is
    just half wrong :)
    
    Chromium is still relying on deprecated behavior for clamped blurs
    where it doesn't specify the clamping geometry with the image filter.
    This will require more changes but would likely get around the issue
    with how the software_renderer prepares the images to be blurred.
    
    Numerous ash pixel tests had to be updated, but I couldn't spot any
    meaningful differences in their content. My guess is that the widgets
    and windows all had some amount of blur use, so this change touched
    everywhere.
    
    Bug: b/294100597
    Bug: b/40040586
    Change-Id: I67d5840113e87cfbed1d3a2c4d624c8216991775
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4818347
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>
    Cr-Commit-Position: refs/heads/main@{#1191026}

--- ash/system/accessibility/accessibility_detailed_view_pixeltest.cc
+++ ash/system/accessibility/accessibility_detailed_view_pixeltest.cc
@@ -62,7 +62,7 @@ TEST_P(AccessibilityDetailedViewPixelTest, Basics) {
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "check_view",
-      /*revision_number=*/4, detailed_view_container));
+      /*revision_number=*/5, detailed_view_container));
 }
 
 }  // namespace ash
