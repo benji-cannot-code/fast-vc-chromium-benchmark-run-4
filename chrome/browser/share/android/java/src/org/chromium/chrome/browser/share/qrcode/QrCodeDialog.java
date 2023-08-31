5ba92b9890a95e480ea4902241c344abd0f1e391
Wenyu Fu <wenyufu@google.com>
2023-08-31 23:18:34 +0000

    [Share] Remove QR code scan tab
    Context see bugs.
    
    Bug: 1374731, 1477441
    Change-Id: I41d40b4abda2b26f91a470e02d956bd84a7d67a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4828661
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sophey Dong <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191053}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/qrcode/QrCodeDialog.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/qrcode/QrCodeDialog.java
@@ -19,7 +19,6 @@ import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.tabs.TabLayout;
 
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.share.qrcode.scan_tab.QrCodeScanCoordinator;
 import org.chromium.chrome.browser.share.qrcode.share_tab.QrCodeShareCoordinator;
 import org.chromium.components.browser_ui.widget.FullscreenAlertDialog;
 import org.chromium.ui.base.WindowAndroid;
@@ -35,6 +34,7 @@ public class QrCodeDialog extends DialogFragment {
     public static String URL_KEY = "url_key";
 
     private WindowAndroid mWindowAndroid;
+    // TODO(crbug/1477441): Remove list of Tabs.
     protected ArrayList<QrCodeDialogTab> mTabs;
     private TabLayoutPageListener mTabLayoutPageListener;
 
@@ -109,6 +109,7 @@ public class QrCodeDialog extends DialogFragment {
         ChromeImageButton closeButton = dialogView.findViewById(R.id.close_button);
         closeButton.setOnClickListener(v -> dismiss());
         TabLayout tabLayout = dialogView.findViewById(R.id.tab_layout);
+        assert tabLayout.getTabCount() == mTabs.size();
 
         // Setup page adapter and tab layout.
         ArrayList<View> pages = new ArrayList<View>();
@@ -137,11 +138,8 @@ public class QrCodeDialog extends DialogFragment {
 
         QrCodeShareCoordinator shareCoordinator = new QrCodeShareCoordinator(
                 context, this::dismiss, getArguments().getString(URL_KEY), mWindowAndroid);
-        QrCodeScanCoordinator scanCoordinator =
-                new QrCodeScanCoordinator(context, this::dismiss, mWindowAndroid);
 
         mTabs = new ArrayList<>();
         mTabs.add(shareCoordinator);
-        mTabs.add(scanCoordinator);
     }
 }
