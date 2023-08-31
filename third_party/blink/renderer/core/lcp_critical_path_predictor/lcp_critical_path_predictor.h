b23e8651662b9f7ff1a66bfdd1f8860fbc6e9ca2
Alex N. Jose <alexnj@chromium.org>
2023-08-31 08:36:20 +0000

    [LCPP] LCPP Async script read path: Boost priority of LCP async scripts
    This CL implements the priority adjustment portion of LCPP Async Script
    read path and corresponding web_test. A follow up CL would plumb
    URL hints from browser process to renderer.
    
    This reuses the existing plumbing of LCPP as much as it's possible,
    and introduces `lcp_influencer_script_urls` adjacent to the element
    locators. For the initial tests, we assume influencers = creators of
    the LCP Image.
    
    Bug: 1419756
    Change-Id: I4054a0706f8b4ffddcf7fccb31761c6c4b28f4d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4803220
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190595}

--- third_party/blink/renderer/core/lcp_critical_path_predictor/lcp_critical_path_predictor.h
+++ third_party/blink/renderer/core/lcp_critical_path_predictor/lcp_critical_path_predictor.h
@@ -46,6 +46,17 @@ class CORE_EXPORT LCPCriticalPathPredictor final
     return lcp_element_locators_;
   }
 
+  void set_lcp_influencer_scripts(HashSet<KURL> scripts);
+
+  const HashSet<KURL>& lcp_influencer_scripts() {
+    return lcp_influencer_scripts_;
+  }
+
+  void Reset() {
+    lcp_element_locators_.clear();
+    lcp_influencer_scripts_.clear();
+  }
+
   // Member functions invoked in LCPP hint production path (write path):
 
   void OnLargestContentfulPaintUpdated(Element* lcp_element);
@@ -64,6 +75,7 @@ class CORE_EXPORT LCPCriticalPathPredictor final
   // LCPP hints for consumption (read path):
 
   Vector<ElementLocator> lcp_element_locators_;
+  HashSet<KURL> lcp_influencer_scripts_;
 };
 
 }  // namespace blink
