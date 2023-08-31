6d50adab41e6241b37cdab53c6b6b0cd0b9e73f2
Justin Novosad <junov@chromium.org>
2023-08-31 15:40:52 +0000

    Move text attribute setters into BaseRenderingContext2D
    This code cleanup CL de-duplicates some attribute setter code in
    preparation for implementing the display list feature.
    
    Bug: 1467562
    Change-Id: Icebf9fc30f3775641addeab55343124e7c15ee8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4791824
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190780}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.h
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.h
@@ -117,14 +117,6 @@ class MODULES_EXPORT CanvasRenderingContext2D final
 
   void setFontForTesting(const String& new_font) override;
 
-  void setLetterSpacing(const String&);
-  void setWordSpacing(const String&);
-  void setTextRendering(const String&);
-
-  void setFontKerning(const String&);
-  void setFontStretch(const String&);
-  void setFontVariantCaps(const String&);
-
   void drawFormattedText(FormattedText* formatted_text,
                          double x,
                          double y,
@@ -222,6 +214,7 @@ class MODULES_EXPORT CanvasRenderingContext2D final
 
  protected:
   HTMLCanvasElement* HostAsHTMLCanvasElement() const final;
+  FontSelector* GetFontSelector() const final;
 
   PredefinedColorSpace GetDefaultImageDataColorSpace() const final {
     return color_params_.ColorSpace();
