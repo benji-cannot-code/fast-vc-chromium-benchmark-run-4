e74a6b9f4f4d8e1a1572134491544fece46391ca
Bastian Kersting <bkersting@google.com>
2023-08-31 12:15:58 +0000

    [Telemetry] Add CreateRoutine to DiagnosticRoutineManager
    This change adds an implementation of CreateRoutine to
    the DiagnosticRoutineManager. This change also adds the
    capabilities to track currently open PWAs and cut connections
    accordingly.
    
    This is part of a bigger effort to add a new diagnostics API, please
    visit go/dd-telemetry-extension-diag-revisit for more context.
    
    Bug: b/286031590, b/268125594
    Test: unit_tests
    --gtest_filter="*TelemetryExtensionDiagnosticRoutinesManagerTest*"
    
    Change-Id: I871fac0a4dbcfca6993c31198df7c50346bf9a9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4793611
    Reviewed-by: Chung-sheng Wu <chungsheng@google.com>
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Cr-Commit-Position: refs/heads/main@{#1190671}

--- chrome/browser/chromeos/extensions/telemetry/api/routines/diagnostic_routine.h
+++ chrome/browser/chromeos/extensions/telemetry/api/routines/diagnostic_routine.h
@@ -57,6 +57,8 @@ class DiagnosticRoutine {
                                   const std::string& message);
 
  private:
+  friend class TelemetryExtensionDiagnosticRoutinesManagerTest;
+
   mojo::Remote<crosapi::mojom::TelemetryDiagnosticRoutineControl>
       routine_control_;
   DiagnosticRoutineObservation observation_;
--- chrome/browser/chromeos/extensions/telemetry/api/routines/diagnostic_routine_observation.cc
+++ chrome/browser/chromeos/extensions/telemetry/api/routines/diagnostic_routine_observation.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/check_is_test.h"
 #include "base/memory/raw_ptr.h"
 #include "base/notreached.h"
 #include "base/uuid.h"
@@ -106,6 +107,12 @@ void DiagnosticRoutineObservation::OnRoutineStateChange(
         return;
       }
     }
+  }
+
+  // The `EventRouter` might be unavailable in unittests.
+  if (!extensions::EventRouter::Get(browser_context_)) {
+    CHECK_IS_TEST();
+    return;
   }
 
   extensions::EventRouter::Get(browser_context_)
