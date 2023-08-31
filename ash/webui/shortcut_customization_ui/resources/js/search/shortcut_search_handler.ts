bd66782ff3038b5c61c539d0db0463a877d32dab
wenyu zhang <zhangwenyu@google.com>
2023-08-31 18:06:53 +0000

    flag: Remove enable-search-in-shortcuts-app flag - frontend part
    Bug: b/298392412
    Test: Browsertest --gtest_filter=ShortcutCustomizationApp*
    Change-Id: Ied966df742f3010437f34a3230b2cde8f2c195dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4832733
    Commit-Queue: Wenyu Zhang <zhangwenyu@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190880}

--- ash/webui/shortcut_customization_ui/resources/js/search/shortcut_search_handler.ts
+++ ash/webui/shortcut_customization_ui/resources/js/search/shortcut_search_handler.ts
@@ -5,11 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {assert} from 'chrome://resources/js/assert_ts.js';
 
-import {fakeSearchResults} from '../fake_data.js';
 import {ShortcutSearchHandler, ShortcutSearchHandlerInterface} from '../shortcut_types.js';
-import {isSearchEnabled} from '../shortcut_utils.js';
-
-import {FakeShortcutSearchHandler} from './fake_shortcut_search_handler.js';
 
 /**
  * @fileoverview
@@ -23,27 +19,9 @@ export function setShortcutSearchHandlerForTesting(
   shortcutSearchHandler = testHandler;
 }
 
-/**
- * Create a Fake ShortcutSearchHandler with reasonable fake data.
- */
-export function setupFakeShortcutSearchHandler(): void {
-  // Create handler.
-  const handler = new FakeShortcutSearchHandler();
-
-  // Setup search response.
-  handler.setFakeSearchResult(fakeSearchResults);
-
-  // Set the fake handler.
-  setShortcutSearchHandlerForTesting(handler);
-}
-
 export function getShortcutSearchHandler(): ShortcutSearchHandlerInterface {
   if (!shortcutSearchHandler) {
-    if (isSearchEnabled()) {
-      shortcutSearchHandler = ShortcutSearchHandler.getRemote();
-    } else {
-      setupFakeShortcutSearchHandler();
-    }
+    shortcutSearchHandler = ShortcutSearchHandler.getRemote();
   }
   assert(!!shortcutSearchHandler);
   return shortcutSearchHandler;
