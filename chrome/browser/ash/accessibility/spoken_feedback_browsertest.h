3794b28743ae81a7dbff3231bb6b9a6509a4088d
David Tseng <dtseng@google.com>
2023-08-31 23:56:01 +0000

    [lacros-a11y]: explicitly expect ChromeVox intro message
    In order to try and resolve edge case flakes in
    https://chromium-review.googlesource.com/c/chromium/src/+/4707196
    
    require ChromeVox tests to expect the specific intro text.
    
    It was omitted at one point because not all callers needed it to pass
    and worked fine.
    
    R=akihiroota@chromium.org
    
    Bug: none
    Test: cq
    Change-Id: I4cc3e58fe592ec695d0cf92b09249982ade853b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4833599
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191071}

--- chrome/browser/ash/accessibility/spoken_feedback_browsertest.h
+++ chrome/browser/ash/accessibility/spoken_feedback_browsertest.h
@@ -56,7 +56,7 @@ class LoggedInSpokenFeedbackTest : public InProcessBrowserTest {
 
   void DisableEarcons();
 
-  void EnableChromeVox();
+  void EnableChromeVox(bool check_for_intro = true);
 
   void StablizeChromeVoxState();
 
