bf9e09e277a4f2d97b4c8c305a6db379d42b0044
François Degros <fdegros@chromium.org>
2023-08-31 02:12:01 +0000

    Refactor enum ConnectionStatusType -> enum class ConnectionStatus
    BUG=b:298283614
    
    Change-Id: I0230f87f731e007eda4496184c61a8cd55206021
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4829302
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190486}

--- chrome/browser/ash/extensions/file_manager/private_api_sharesheet.cc
+++ chrome/browser/ash/extensions/file_manager/private_api_sharesheet.cc
@@ -111,11 +111,12 @@ void FileManagerPrivateInternalSharesheetHasTargetsFunction::
 
   if (file_system_urls_.size() == 1 &&
       file_system_urls_[0].type() == storage::kFileSystemTypeDriveFs) {
-    auto connection_status = drive::util::GetDriveConnectionStatus(
+    using drive::util::ConnectionStatus;
+    const ConnectionStatus status = drive::util::GetDriveConnectionStatus(
         Profile::FromBrowserContext(browser_context()));
 
-    if (connection_status == drive::util::DRIVE_CONNECTED_METERED ||
-        connection_status == drive::util::DRIVE_CONNECTED) {
+    using enum ConnectionStatus;
+    if (status == kMetered || status == kConnected) {
       file_manager::util::SingleEntryPropertiesGetterForDriveFs::Start(
           file_system_urls_[0], profile_,
           base::BindOnce(
@@ -246,11 +247,12 @@ void FileManagerPrivateInternalInvokeSharesheetFunction::OnMimeTypesCollected(
 
   if (file_system_urls_.size() == 1 &&
       file_system_urls_[0].type() == storage::kFileSystemTypeDriveFs) {
-    auto connection_status = drive::util::GetDriveConnectionStatus(
+    using drive::util::ConnectionStatus;
+    const ConnectionStatus status = drive::util::GetDriveConnectionStatus(
         Profile::FromBrowserContext(browser_context()));
 
-    if (connection_status == drive::util::DRIVE_CONNECTED_METERED ||
-        connection_status == drive::util::DRIVE_CONNECTED) {
+    using enum ConnectionStatus;
+    if (status == kMetered || status == kConnected) {
       file_manager::util::SingleEntryPropertiesGetterForDriveFs::Start(
           file_system_urls_[0], profile_,
           base::BindOnce(&FileManagerPrivateInternalInvokeSharesheetFunction::
