2ad1587eb43bbee53c89bdcd9fa323a0a8f2428d
Simon Hangl <simonha@google.com>
2023-08-31 12:21:10 +0000

    Fix raw_ptr init in the web_applications/commands folder
    raw_ptr is not necessarily null-initialized [1]. This CL adds
    initializations in the web_applications/commands folder. Further,
    unused raw_ptr.h header includes are removed.
    
    Bug: b/278246367
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:base/memory/raw_ptr.md;drc=bc4df603d4fc38f23fd7b26b74a4ad1a49b935b4
    
    Change-Id: Idc2c05b5d6298ab366473d4f19105e993cb9c442
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4822610
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1190673}

--- chrome/browser/web_applications/commands/manifest_update_finalize_command.cc
+++ chrome/browser/web_applications/commands/manifest_update_finalize_command.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/strings/to_string.h"
 #include "base/strings/utf_string_conversions.h"
--- chrome/browser/web_applications/commands/manifest_update_finalize_command.h
+++ chrome/browser/web_applications/commands/manifest_update_finalize_command.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/values.h"
 #include "chrome/browser/profiles/keep_alive/scoped_profile_keep_alive.h"
