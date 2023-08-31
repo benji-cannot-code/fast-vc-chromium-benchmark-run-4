5c5866571a2b42416970b8a0ad7ca218263f0bd8
Rob Buis <rbuis@igalia.com>
2023-08-31 20:06:56 +0000

    Make EnsurePaintLocationDataValidForNode call UpdateStyleAndLayoutForNode
    Make EnsurePaintLocationDataValidForNode call UpdateStyleAndLayoutForNode since
    the implementations are identical.
    
    Change-Id: I53784f0d48cc047810ab2a0782b97148c89bc91c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4832014
    Commit-Queue: Rob Buis <rbuis@igalia.com>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190939}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -2449,6 +2449,9 @@ void Document::UpdateStyleAndLayoutForNode(const Node* node,
 
   DisplayLockUtilities::ScopedForcedUpdate scoped_update_forced(
       node, DisplayLockContext::ForcedPhase::kLayout);
+
+  // For all nodes we must have up-to-date style and have performed layout to do
+  // any location-based calculation.
   UpdateStyleAndLayout(reason);
 }
 
@@ -2743,16 +2746,7 @@ const ComputedStyle* Document::StyleForPage(uint32_t page_index,
 void Document::EnsurePaintLocationDataValidForNode(
     const Node* node,
     DocumentUpdateReason reason) {
-  DCHECK(node);
-  if (!node->InActiveDocument())
-    return;
-
-  DisplayLockUtilities::ScopedForcedUpdate scoped_update_forced(
-      node, DisplayLockContext::ForcedPhase::kLayout);
-
-  // For all nodes we must have up-to-date style and have performed layout to do
-  // any location-based calculation.
-  UpdateStyleAndLayout(reason);
+  UpdateStyleAndLayoutForNode(node, reason);
 }
 
 void Document::GetPageDescription(uint32_t page_index,
