021b18ee78194b4671bca6d9dbb2d50597cffbc4
Joemer Ramos <joemerramos@chromium.org>
2023-08-31 18:42:03 +0000

    [iOS] Enable Fullscreen API by default
    Bug: 1295480
    Change-Id: I6beb88c3b6eab36704b04db8ed2b44e7dd99fc55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4832492
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190894}

--- ios/web/common/features.mm
+++ ios/web/common/features.mm
@@ -49,7 +49,7 @@ BASE_FEATURE(kSynthesizedRestoreSession,
 
 BASE_FEATURE(kEnableFullscreenAPI,
              "EnableFullscreenAPI",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kUseLoadSimulatedRequestForOfflinePage,
              "UseLoadSimulatedRequestForErrorPageNavigation",
