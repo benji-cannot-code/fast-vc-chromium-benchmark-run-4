b42d3095286f1594c0f8aa7f76b863c737b2bd33
Christine Smith <christinesm@chromium.org>
2023-08-31 17:19:57 +0000

    Remove enable-storage-pressure-event flag
    Removes the expired enable-storage-pressure-event flag from the
    codebase. This feature is no longer planned to be implemented.
    
    Bug: 1456943
    Change-Id: Ic09023a37fc7a09933a0a1de6a9b4f98e389f356
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4827383
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Christine Smith <christinesm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190846}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1817,9 +1817,6 @@ extern const char kSmoothScrollingDescription[];
 extern const char kSplitCacheByNetworkIsolationKeyName[];
 extern const char kSplitCacheByNetworkIsolationKeyDescription[];
 
-extern const char kStoragePressureEventName[];
-extern const char kStoragePressureEventDescription[];
-
 extern const char kStrictOriginIsolationName[];
 extern const char kStrictOriginIsolationDescription[];
 
