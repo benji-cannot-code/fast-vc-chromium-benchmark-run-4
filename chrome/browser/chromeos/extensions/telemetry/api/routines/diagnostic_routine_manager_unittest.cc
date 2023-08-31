05540daaa7a47c80eac253d2a20510a5c63de0ea
Bastian Kersting <bkersting@google.com>
2023-08-31 17:20:28 +0000

    [Telemetry] Fix raw_ptrs in tests
    This change removes the base::RawPtrTraits::kMayDangle
    tags from raw_ptrs in test.
    
    Bug: None
    Test: unit_tests
    --gtest_filter="*TelemetryExtensionDiagnosticRoutinesManagerTest*"
    Test: browser_tests --gtest_filter=*TelemetryExtensionEventsApiBrowserTest*
    
    Change-Id: Ieb5d51d6b5a1f96d8d35a42b68c09e8cbcdee18d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4832317
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190847}

--- chrome/browser/chromeos/extensions/telemetry/api/routines/diagnostic_routine_manager_unittest.cc
+++ chrome/browser/chromeos/extensions/telemetry/api/routines/diagnostic_routine_manager_unittest.cc
@@ -11,10 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-#include "base/allocator/partition_allocator/pointers/raw_ptr.h"
 #include "base/check_deref.h"
 #include "base/containers/flat_map.h"
 #include "base/files/file_path.h"
+#include "base/memory/raw_ptr.h"
 #include "base/test/bind.h"
 #include "base/test/test_future.h"
 #include "base/types/expected.h"
@@ -89,6 +89,13 @@ class TelemetryExtensionDiagnosticRoutinesManagerTest
     chromeos::LacrosService::Get()->InjectRemoteForTesting(
         fake_routines_service_impl_->receiver().BindNewPipeAndPassRemote());
 #endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
+  }
+
+  void TearDown() override {
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+    fake_routines_service_impl_ = nullptr;
+#endif
+    BrowserWithTestWindowTest::TearDown();
   }
 
  protected:
@@ -170,8 +177,7 @@ class TelemetryExtensionDiagnosticRoutinesManagerTest
 
  private:
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-  raw_ptr<FakeDiagnosticRoutinesService, base::RawPtrTraits::kMayDangle>
-      fake_routines_service_impl_;
+  raw_ptr<FakeDiagnosticRoutinesService> fake_routines_service_impl_;
   FakeDiagnosticRoutinesServiceFactory fake_routines_service_factory_;
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
