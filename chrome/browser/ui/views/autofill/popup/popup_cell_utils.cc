071242552b26a5d9abdf5b5f2a66888020db5493
Bruno Braga <brunobraga@google.com>
2023-08-31 11:34:34 +0000

    [mullet m2] Update main suggestion icon and popup ids.
    1. Handles the new icon introduced by mullet on the main popup.
    2. Handles updating the main popup item id depending on the last
    filling granularity.
    
    This CL is a broken up cl from:
    https://chromium-review.googlesource.com/c/chromium/src/+/4823023
    because adding the labels will be a bit more complicated than expected,
    I decided to break it up to make reviewing easier
    
    Bug: 1459990
    Change-Id: Iad3530160492f5b9dda804dab5e8eeb3cfac3431
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4827088
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1190661}

--- chrome/browser/ui/views/autofill/popup/popup_cell_utils.cc
+++ chrome/browser/ui/views/autofill/popup/popup_cell_utils.cc
@@ -148,6 +148,10 @@ std::unique_ptr<views::ImageView> GetIconImageViewByName(
     return ImageViewFromVectorIcon(vector_icons::kEditIcon, kIconSize);
   }
 
+  if (icon_str == "locationIcon") {
+    return ImageViewFromVectorIcon(vector_icons::kLocationOnIcon, kIconSize);
+  }
+
   if (icon_str == "deleteIcon") {
     return ImageViewFromVectorIcon(kTrashCanLightIcon, kIconSize);
   }
