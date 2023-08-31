9f784b962b9fc823362ef6ba9abb888550f0f6d5
Frank Liberato <liberato@chromium.org>
2023-08-31 04:08:05 +0000

    Don't show the auto-pip allow / block UI all the time
    Rather than showing the allow / block UI for any picture in picture
    window that's opened while the command-line flag for the feature is
    enabled, instead show it only if the pip instance really was caused
    by auto-pip.
    
    Bug: 1464066
    Change-Id: I1a5242b3e2292707b7a3cf6986a4cc3dc818cc0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4824963
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190525}

--- chrome/browser/picture_in_picture/picture_in_picture_window_manager_unittest.cc
+++ chrome/browser/picture_in_picture/picture_in_picture_window_manager_unittest.cc
@@ -14,6 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/display/display.h"
 
+#if !BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/picture_in_picture/auto_picture_in_picture_tab_helper.h"
+#include "ui/views/view.h"
+#endif  // !BUILDFLAG(IS_ANDROID)
+
 namespace {
 
 typedef base::ScopedObservation<PictureInPictureWindowManager,
@@ -156,5 +161,22 @@ TEST_F(PictureInPictureWindowManagerTest, OnEnterDocumentPictureInPicture) {
 
   picture_in_picture_window_manager->EnterDocumentPictureInPicture(
       web_contents(), child_web_contents());
+}
+
+TEST_F(PictureInPictureWindowManagerTest, DontShowAutoPipSettingUiWithoutPip) {
+  PictureInPictureWindowManager* picture_in_picture_window_manager =
+      PictureInPictureWindowManager::GetInstance();
+  // There's no pip open, so expect no setting UI.
+  EXPECT_FALSE(picture_in_picture_window_manager->GetOverlayView());
+}
+
+TEST_F(PictureInPictureWindowManagerTest,
+       DontShowAutoPipSettingUiForNonAutoPip) {
+  PictureInPictureWindowManager* picture_in_picture_window_manager =
+      PictureInPictureWindowManager::GetInstance();
+  picture_in_picture_window_manager->EnterDocumentPictureInPicture(
+      web_contents(), child_web_contents());
+  // This isn't auto-pip, so expect no overlay view.
+  EXPECT_FALSE(picture_in_picture_window_manager->GetOverlayView());
 }
 #endif
