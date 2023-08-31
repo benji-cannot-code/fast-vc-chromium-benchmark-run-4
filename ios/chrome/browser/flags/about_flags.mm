ae507f9c1189a362def97982ac75797a490542dc
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-08-31 10:28:24 +0000

    Update chrome://flags#replace-sync-promos-with-sign-in-promos
    - Make sure ConsistencyNewAccountInterface is always enabled.
    - Remove launched flags.
    
    Bug: 1447010
    Change-Id: If1ed134d4b982bf418cba4c255bf4d3927de858f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4827231
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190648}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -718,21 +718,19 @@ const FeatureEntry::Choice kReplaceSyncPromosWithSignInPromosChoices[] = {
     {"Default", "", ""},
     {"Disabled", "disable-features",
      "ReplaceSyncPromosWithSignInPromos,"
+     "ConsistencyNewAccountInterface,"
      "FeedBottomSyncStringRemoval"},
     {"Base only", "enable-features",
      "ReplaceSyncPromosWithSignInPromos,"
+     "ConsistencyNewAccountInterface,"
      "FeedBottomSyncStringRemoval"},
     {"Everything (bookmarks, reading list, etc)", "enable-features",
      "ReplaceSyncPromosWithSignInPromos,"
+     "ConsistencyNewAccountInterface,"
      "FeedBottomSyncStringRemoval,"
      "SyncEnableContactInfoDataTypeInTransportMode,"
      "SyncEnableContactInfoDataTypeForCustomPassphraseUsers,"
-     "EnableBookmarksAccountStorage,"
-     "EnablePreferencesAccountStorage,"
-     "ReadingListEnableDualReadingListModel,"
-     "ReadingListEnableSyncTransportModeUponSignIn,"
-     "ConsistencyNewAccountInterface,"
-     "AutofillAccountProfileStorage"},
+     "EnablePreferencesAccountStorage"},
 };
 
 const FeatureEntry::FeatureParam kOneTapForMapsConsentModeDefault[] = {
@@ -1428,10 +1426,6 @@ const flags_ui::FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kSpotlightReadingListSourceName,
      flag_descriptions::kSpotlightReadingListSourceDescription,
      flags_ui::kOsIos, FEATURE_VALUE_TYPE(kSpotlightReadingListSource)},
-    {"consistency-new-account-interface",
-     flag_descriptions::kConsistencyNewAccountInterfaceName,
-     flag_descriptions::kConsistencyNewAccountInterfaceDescription,
-     flags_ui::kOsIos, FEATURE_VALUE_TYPE(kConsistencyNewAccountInterface)},
     {"policy-logs-page-ios", flag_descriptions::kPolicyLogsPageIOSName,
      flag_descriptions::kPolicyLogsPageIOSDescription, flags_ui::kOsIos,
      FEATURE_VALUE_TYPE(policy::features::kPolicyLogsPageIOS)},
