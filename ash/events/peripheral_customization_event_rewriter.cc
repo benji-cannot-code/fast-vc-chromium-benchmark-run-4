55d210a4bc7f58de88d9b691d0354d32493efdf1
David Padlipsky <dpad@google.com>
2023-09-01 19:38:27 +0000

    customization: Allow buttons and keys to be remapped to actions
    Hooks up the peripheral customization event rewriter to the accelerator
    controller to execute the requested actions by users.
    
    Bug: b/241965717
    Change-Id: I92c3fe5823b309bf58fc2ae863ea7dd8db557f71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4808881
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1191527}

--- ash/events/peripheral_customization_event_rewriter.cc
+++ ash/events/peripheral_customization_event_rewriter.cc
@@ -7,9 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "ash/accelerators/accelerator_controller_impl.h"
 #include "ash/constants/ash_features.h"
 #include "ash/public/mojom/input_device_settings.mojom-shared.h"
 #include "ash/public/mojom/input_device_settings.mojom.h"
+#include "ash/shell.h"
 #include "base/check.h"
 #include "base/notreached.h"
 #include "base/ranges/algorithm.h"
@@ -227,7 +229,15 @@ bool PeripheralCustomizationEventRewriter::RewriteEventFromButton(
   }
 
   if (remapping_action->is_action()) {
-    // TODO(dpad): Implement action rewriting from button presses.
+    if (event.type() == ui::ET_KEY_PRESSED ||
+        event.type() == ui::ET_MOUSE_PRESSED) {
+      // Every accelerator supported by peripheral customization is not impacted
+      // by the accelerator passed. Therefore, passing an empty accelerator will
+      // cause no issues.
+      Shell::Get()->accelerator_controller()->PerformActionIfEnabled(
+          remapping_action->get_action(), /*accelerator=*/{});
+    }
+
     return true;
   }
 
