1e0b4d896bb24d2a5546eb8acb67e3b04b7125e6
Toni Barzic <tbarzic@chromium.org>
2023-09-01 21:36:17 +0000

    Reland "Ensure margin between calendar and shelf"
    This is a reland of commit 4c2a564e609195d33a5878bbd19828e32fd80f9b
    Fix: uprev pixel tests - this causes the UI in the pixel tests to
    be translated vertically, but that's expected.
    
    Original change's description:
    > Ensure margin between calendar and shelf
    >
    > Update the glanceables bubble layout so there is margin between calendar
    > view and the shelf, even when the calendar view is scrolled
    >
    > Change-Id: I28851ec76a28b40a165dd0f9603009e8851aeaf0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4834775
    > Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    > Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1191473}
    
    Change-Id: I1795c50b690eb0bd71d4c40c3931a39c46f85067
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4835866
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191601}

--- ash/glanceables/glanceables_pixeltest.cc
+++ ash/glanceables/glanceables_pixeltest.cc
@@ -116,7 +116,7 @@ TEST_F(GlanceablesPixelTest, GlanceablesZeroState) {
   GetGlanceableTrayBubble()->GetTasksView()->ScrollViewToVisible();
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
-      "glanceables_zero_state", /*revision_number=*/1,
+      "glanceables_zero_state", /*revision_number=*/2,
       GetGlanceableTrayBubble()->GetBubbleView()));
 }
 
@@ -154,7 +154,7 @@ TEST_F(GlanceablesPixelTest, GlanceablesTasksMarkAsCompleted) {
             fake_glanceables_tasks_client()->pending_completed_tasks().size());
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
-      "glanceables_task_view_no_completed_tasks", /*revision_number=*/2,
+      "glanceables_task_view_no_completed_tasks", /*revision_number=*/3,
       GetGlanceableTrayBubble()->GetTasksView()));
 
   GestureTapOn(task_view->GetButtonForTest());
@@ -163,7 +163,7 @@ TEST_F(GlanceablesPixelTest, GlanceablesTasksMarkAsCompleted) {
             fake_glanceables_tasks_client()->pending_completed_tasks().size());
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
-      "glanceables_task_view_one_completed_task", /*revision_number=*/2,
+      "glanceables_task_view_one_completed_task", /*revision_number=*/3,
       GetGlanceableTrayBubble()->GetTasksView()));
 }
 
