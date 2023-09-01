3153112402f930d4dcbf5c1541be7fe192d9cae3
Dana Fried <dfried@chromium.org>
2023-09-01 20:33:15 +0000

    [User Education] Regression tests for WebUI help bubble behavior.
    Verifying existing behavior is correct before attempting to address the
    attached bug. Without these regression tests, it is not clear what is
    wrong or that any potential fix might not break other use cases.
    
    Bug: 1474307
    Change-Id: Ibdf8b40dfaaeb331e0ccf36a95282c2a35acbb0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4810928
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191564}

--- components/user_education/webui/help_bubble_handler_unittest.cc
+++ components/user_education/webui/help_bubble_handler_unittest.cc
@@ -462,6 +462,8 @@ TEST_F(HelpBubbleHandlerTest, HelpBubbleClosedWhenVisibilityChanges) {
   EXPECT_CALL(test_handler_->mock(), ShowHelpBubble(testing::_));
   auto help_bubble = help_bubble_factory_registry_.CreateHelpBubble(
       element, std::move(params));
+  EXPECT_TRUE(
+      test_handler_->IsHelpBubbleShowingForTesting(element->identifier()));
 
   // This should have no effect since it's the wrong element.
   handler()->HelpBubbleAnchorVisibilityChanged(
@@ -471,6 +473,8 @@ TEST_F(HelpBubbleHandlerTest, HelpBubbleClosedWhenVisibilityChanges) {
   handler()->HelpBubbleAnchorVisibilityChanged(
       kHelpBubbleHandlerTestElementIdentifier.GetName(), false, gfx::RectF());
   EXPECT_FALSE(help_bubble->is_open());
+  EXPECT_FALSE(
+      test_handler_->IsHelpBubbleShowingForTesting(element->identifier()));
 }
 
 TEST_F(HelpBubbleHandlerTest, HelpBubbleClosedWhenClosedRemotely) {
@@ -490,6 +494,8 @@ TEST_F(HelpBubbleHandlerTest, HelpBubbleClosedWhenClosedRemotely) {
   auto help_bubble = help_bubble_factory_registry_.CreateHelpBubble(
       element, std::move(params));
   auto subscription = help_bubble->AddOnCloseCallback(closed.Get());
+  EXPECT_TRUE(
+      test_handler_->IsHelpBubbleShowingForTesting(element->identifier()));
 
   EXPECT_CALL_IN_SCOPE(
       closed, Run,
@@ -497,6 +503,8 @@ TEST_F(HelpBubbleHandlerTest, HelpBubbleClosedWhenClosedRemotely) {
           kHelpBubbleHandlerTestElementIdentifier.GetName(),
           help_bubble::mojom::HelpBubbleClosedReason::kPageChanged));
   EXPECT_FALSE(help_bubble->is_open());
+  EXPECT_FALSE(
+      test_handler_->IsHelpBubbleShowingForTesting(element->identifier()));
 }
 
 TEST_F(HelpBubbleHandlerTest, DestroyHandlerCleansUpElement) {
@@ -552,6 +560,8 @@ TEST_F(HelpBubbleHandlerTest, HelpBubbleClosedWhenClosedByUserCallsDismiss) {
   EXPECT_CALL(test_handler_->mock(), ShowHelpBubble(testing::_));
   auto help_bubble = help_bubble_factory_registry_.CreateHelpBubble(
       element, std::move(params));
+  EXPECT_TRUE(
+      test_handler_->IsHelpBubbleShowingForTesting(element->identifier()));
 
   EXPECT_CALL_IN_SCOPE(
       dismissed, Run,
@@ -559,6 +569,8 @@ TEST_F(HelpBubbleHandlerTest, HelpBubbleClosedWhenClosedByUserCallsDismiss) {
           kHelpBubbleHandlerTestElementIdentifier.GetName(),
           help_bubble::mojom::HelpBubbleClosedReason::kDismissedByUser));
   EXPECT_FALSE(help_bubble->is_open());
+  EXPECT_FALSE(
+      test_handler_->IsHelpBubbleShowingForTesting(element->identifier()));
 }
 
 TEST_F(HelpBubbleHandlerTest, ButtonPressedCallsCallback) {
@@ -591,12 +603,16 @@ TEST_F(HelpBubbleHandlerTest, ButtonPressedCallsCallback) {
   EXPECT_CALL(test_handler_->mock(), ShowHelpBubble(testing::_));
   auto help_bubble = help_bubble_factory_registry_.CreateHelpBubble(
       element, std::move(params));
+  EXPECT_TRUE(
+      test_handler_->IsHelpBubbleShowingForTesting(element->identifier()));
 
   EXPECT_CALL_IN_SCOPE(
       button2_pressed, Run,
       handler()->HelpBubbleButtonPressed(
           kHelpBubbleHandlerTestElementIdentifier.GetName(), 1));
   EXPECT_FALSE(help_bubble->is_open());
+  EXPECT_FALSE(
+      test_handler_->IsHelpBubbleShowingForTesting(element->identifier()));
 }
 
 TEST_F(HelpBubbleHandlerTest, ShowMultipleBubblesAndCloseOneViaVisibility) {
@@ -620,6 +636,8 @@ TEST_F(HelpBubbleHandlerTest, ShowMultipleBubblesAndCloseOneViaVisibility) {
   EXPECT_CALL(test_handler_->mock(), ShowHelpBubble(testing::_));
   auto help_bubble = help_bubble_factory_registry_.CreateHelpBubble(
       element, std::move(params));
+  EXPECT_TRUE(
+      test_handler_->IsHelpBubbleShowingForTesting(element->identifier()));
 
   HelpBubbleParams params2;
   params2.body_text = u"Help bubble body 2.";
@@ -627,6 +645,8 @@ TEST_F(HelpBubbleHandlerTest, ShowMultipleBubblesAndCloseOneViaVisibility) {
   EXPECT_CALL(test_handler_->mock(), ShowHelpBubble(testing::_));
   auto help_bubble2 = help_bubble_factory_registry_.CreateHelpBubble(
       element2, std::move(params2));
+  EXPECT_TRUE(
+      test_handler_->IsHelpBubbleShowingForTesting(element2->identifier()));
 
   EXPECT_TRUE(help_bubble->is_open());
   EXPECT_TRUE(help_bubble2->is_open());
@@ -636,6 +656,10 @@ TEST_F(HelpBubbleHandlerTest, ShowMultipleBubblesAndCloseOneViaVisibility) {
       kHelpBubbleHandlerTestElementIdentifier.GetName(), false, gfx::RectF());
   EXPECT_FALSE(help_bubble->is_open());
   EXPECT_TRUE(help_bubble2->is_open());
+  EXPECT_FALSE(
+      test_handler_->IsHelpBubbleShowingForTesting(element->identifier()));
+  EXPECT_TRUE(
+      test_handler_->IsHelpBubbleShowingForTesting(element2->identifier()));
 
   // When the second bubble goes away, it will attempt to close the bubble on
   // the remote.
@@ -665,6 +689,8 @@ TEST_F(HelpBubbleHandlerTest, ShowMultipleBubblesAndCloseOneViaCallback) {
   EXPECT_CALL(test_handler_->mock(), ShowHelpBubble(testing::_));
   auto help_bubble = help_bubble_factory_registry_.CreateHelpBubble(
       element, std::move(params));
+  EXPECT_TRUE(
+      test_handler_->IsHelpBubbleShowingForTesting(element->identifier()));
 
   HelpBubbleParams params2;
   params2.body_text = u"Help bubble body 2.";
@@ -672,6 +698,8 @@ TEST_F(HelpBubbleHandlerTest, ShowMultipleBubblesAndCloseOneViaCallback) {
   EXPECT_CALL(test_handler_->mock(), ShowHelpBubble(testing::_));
   auto help_bubble2 = help_bubble_factory_registry_.CreateHelpBubble(
       element2, std::move(params2));
+  EXPECT_TRUE(
+      test_handler_->IsHelpBubbleShowingForTesting(element2->identifier()));
 
   EXPECT_TRUE(help_bubble->is_open());
   EXPECT_TRUE(help_bubble2->is_open());
@@ -682,6 +710,10 @@ TEST_F(HelpBubbleHandlerTest, ShowMultipleBubblesAndCloseOneViaCallback) {
       help_bubble::mojom::HelpBubbleClosedReason::kPageChanged);
   EXPECT_FALSE(help_bubble->is_open());
   EXPECT_TRUE(help_bubble2->is_open());
+  EXPECT_FALSE(
+      test_handler_->IsHelpBubbleShowingForTesting(element->identifier()));
+  EXPECT_TRUE(
+      test_handler_->IsHelpBubbleShowingForTesting(element2->identifier()));
 
   // When the second bubble goes away, it will attempt to close the bubble on
   // the remote.
