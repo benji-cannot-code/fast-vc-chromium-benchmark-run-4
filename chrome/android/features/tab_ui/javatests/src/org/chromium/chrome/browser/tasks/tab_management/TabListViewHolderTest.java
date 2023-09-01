1e3183325fbeca4c841251dee9bdf07c0ceff623
Calder Kitagawa <ckitagawa@chromium.org>
2023-09-01 15:11:32 +0000

    [Tab Switcher] Re-enable testHiddenGC
    This test flaked once due to a race related to post-test
    flag changes and was disabled. Test-reviver shows a clean
    bill of health since. hnakashima@ is working on improvements
    to the flags API to avoid this type of flake in the future.
    
    Bug: 1469464
    Fixed: 1477382
    Change-Id: I4608397d964f5ba766d613f1273040dff1f03d7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4836292
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191370}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabListViewHolderTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabListViewHolderTest.java
@@ -58,7 +58,6 @@ import org.chromium.base.test.UiThreadTest;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.chrome.browser.flags.CachedFeatureFlags;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -550,7 +549,6 @@ public class TabListViewHolderTest extends BlankUiTestActivityTestCase {
     @Test
     @MediumTest
     @UiThreadTest
-    @DisabledTest(message = "crbug.com/1469464")
     public void testHiddenGC() {
         ImageView thumbnail = mTabGridView.findViewById(R.id.tab_thumbnail);
         mShouldReturnBitmap = true;
