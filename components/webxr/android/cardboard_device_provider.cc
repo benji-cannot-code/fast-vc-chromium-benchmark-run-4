ce55db925762c9dab35bc85466ad7aa5cfb26646
Alexander Cooper <alcooper@chromium.org>
2023-09-01 23:25:02 +0000

    Resume RequestSession flow after scanning cardboard QR Code
    Adds infrastructure to resume the RequestSession flow after scanning the
    Cardboard QR Code. When the QR Code activity is finished, our activity
    gets resumed, which we can use to resume the request session flow. Since
    we are only notified of the activity resume in Java, we have to make a
    helper object to begin listening to these events, which can then forward
    the status along to native code and then to a subscribed handler. This
    infrastructure can be used further in the future to attempt to pause
    and resume a session during the "switch viewer" flow, which does the
    same thing.
    
    Bug: 1429091
    Change-Id: I91deae12a978c3baf0b3820f890f924896e5fb81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4836426
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191651}

--- components/webxr/android/cardboard_device_provider.cc
+++ components/webxr/android/cardboard_device_provider.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/webxr/android/cardboard_device_provider.h"
 
+#include "components/webxr/android/xr_activity_listener.h"
 #include "components/webxr/android/xr_session_coordinator.h"
 #include "components/webxr/mailbox_to_surface_bridge_impl.h"
 #include "device/vr/android/cardboard/cardboard_device.h"
@@ -28,7 +29,8 @@ void CardboardDeviceProvider::Initialize(
       std::make_unique<device::CardboardSdkImpl>(),
       std::make_unique<webxr::MailboxToSurfaceBridgeFactoryImpl>(),
       std::make_unique<webxr::XrSessionCoordinator>(),
-      std::move(compositor_delegate_provider_));
+      std::move(compositor_delegate_provider_),
+      std::make_unique<webxr::XrActivityListenerFactory>());
 
   client->AddRuntime(cardboard_device_->GetId(),
                      cardboard_device_->GetDeviceData(),
---
+++ device/vr/android/xr_activity_state_handler.h
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DEVICE_VR_ANDROID_XR_ACTIVITY_STATE_HANDLER_H_
+#define DEVICE_VR_ANDROID_XR_ACTIVITY_STATE_HANDLER_H_
+
+#include <memory>
+
+#include "base/functional/callback_forward.h"
+
+namespace device {
+
+// The purpose of this interface is to allow for dependency injection to allow
+// //device code to listen to calls to the ActivityLifecycleCallbacks interface,
+// which has to be implemented in //components, because it needs to access the
+// WebContents and Activity objects, which cannot be used in //device.
+//  Unfortunately, the implementation in //components must have the same name as
+// the java class for the jni headers to build properly; so we're left to come
+// up with a slightly different name here.
+class XrActivityStateHandler {
+ public:
+  virtual ~XrActivityStateHandler() = default;
+  virtual void SetResumedHandler(base::RepeatingClosure resumed_handler) = 0;
+};
+
+class XrActivityStateHandlerFactory {
+ public:
+  virtual ~XrActivityStateHandlerFactory() = default;
+  virtual std::unique_ptr<XrActivityStateHandler> Create(
+      int render_process_id,
+      int render_frame_id) = 0;
+};
+
+}  // namespace device
+
+#endif  // DEVICE_VR_ANDROID_XR_ACTIVITY_STATE_HANDLER_H_
