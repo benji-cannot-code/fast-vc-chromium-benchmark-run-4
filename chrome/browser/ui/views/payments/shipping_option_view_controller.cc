7cb13361df785fe50e62fcf157aa14c7fa4f5624
Peter Kotwicz <pkotwicz@chromium.org>
2023-09-01 13:56:31 +0000

    Fix dangling pointer in ShippingOptionItem
    This CL fixes the ShippingOptionItem::shipping_option_ dangling
    raw_ptr by converting the raw_ptr to a StructPtr.
    
    BUG=1477076
    
    Change-Id: I5eb0f46cb8a6db7785662cc2593af096f164f39b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4824093
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191328}

--- chrome/browser/ui/views/payments/shipping_option_view_controller.cc
+++ chrome/browser/ui/views/payments/shipping_option_view_controller.cc
@@ -24,7 +24,7 @@ namespace {
 
 class ShippingOptionItem : public PaymentRequestItemList::Item {
  public:
-  ShippingOptionItem(mojom::PaymentShippingOption* shipping_option,
+  ShippingOptionItem(mojom::PaymentShippingOptionPtr shipping_option,
                      base::WeakPtr<PaymentRequestSpec> spec,
                      base::WeakPtr<PaymentRequestState> state,
                      PaymentRequestItemList* parent_list,
@@ -36,7 +36,7 @@ class ShippingOptionItem : public PaymentRequestItemList::Item {
                                      selected,
                                      /*clickable=*/true,
                                      /*show_edit_button=*/false),
-        shipping_option_(shipping_option) {
+        shipping_option_(std::move(shipping_option)) {
     Init();
   }
 
@@ -50,7 +50,7 @@ class ShippingOptionItem : public PaymentRequestItemList::Item {
   std::unique_ptr<views::View> CreateContentView(
       std::u16string* accessible_content) override {
     return CreateShippingOptionLabel(
-        shipping_option_,
+        shipping_option_.get(),
         /*formatted_amount=*/
         spec() ? spec()->GetFormattedCurrencyAmount(shipping_option_->amount)
                : std::u16string(),
@@ -82,7 +82,7 @@ class ShippingOptionItem : public PaymentRequestItemList::Item {
     NOTREACHED_NORETURN();
   }
 
-  raw_ptr<mojom::PaymentShippingOption> shipping_option_;
+  mojom::PaymentShippingOptionPtr shipping_option_;
 };
 
 }  // namespace
@@ -96,7 +96,7 @@ ShippingOptionViewController::ShippingOptionViewController(
   spec->AddObserver(this);
   for (const auto& option : spec->GetShippingOptions()) {
     shipping_option_list_.AddItem(std::make_unique<ShippingOptionItem>(
-        option.get(), spec, state, &shipping_option_list_, dialog,
+        option->Clone(), spec, state, &shipping_option_list_, dialog,
         option.get() == spec->selected_shipping_option()));
   }
 }
