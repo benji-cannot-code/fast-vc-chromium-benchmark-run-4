6e7dc0d58ff1f8aef9d332213f123ed5cc38b6ea
Jordan Bayles <jophba@chromium.org>
2023-09-01 04:50:25 +0000

    Roll Open Screen from 8e93b9658d78 to 1fb2df5c8f23 (1 revision)
    https://chromium.googlesource.com/openscreen.git/+log/8e93b9658d78..1fb2df5c8f23
    
    2023-08-31 jophba@chromium.org [Cleanup] Remove absl::Span in favor of openscreen::Span
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/openscreen-chromium
    Please CC jophba@chromium.org on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Chromium: https://bugs.chromium.org/p/chromium/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://bugs.chromium.org/p/skia/issues/entry?template=Autoroller+Bug
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Bug: chromium:239435405
    Tbr: jophba@chromium.org
    Change-Id: I06fc0fe1331b0a3f0c32ab46b686a1e99e7202ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4833909
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Bot-Commit: chromium-autoroll <chromium-autoroll@skia-public.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1191163}

--- components/media_router/common/providers/cast/channel/openscreen_message_framer_deserialize_fuzzer.cc
+++ components/media_router/common/providers/cast/channel/openscreen_message_framer_deserialize_fuzzer.cc
@@ -7,13 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstdint>
 
 #include "third_party/openscreen/src/cast/common/channel/message_framer.h"
+#include "third_party/openscreen/src/platform/base/span.h"
 #include "third_party/protobuf/src/google/protobuf/stubs/logging.h"
 
 // Silence logging from the protobuf library.
 google::protobuf::LogSilencer log_silencer;
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  auto buffer = absl::Span<const uint8_t>(data, size);
+  openscreen::ByteView buffer(data, size);
 
   size_t bytes_ingested = 0u;
   while (bytes_ingested < size) {
@@ -25,7 +26,8 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
       break;
     }
     bytes_ingested += result.value().length;
-    buffer = buffer.subspan(result.value().length);
+    buffer = buffer.subspan(result.value().length,
+                            buffer.size() - result.value().length);
   }
   return 0;
 }
