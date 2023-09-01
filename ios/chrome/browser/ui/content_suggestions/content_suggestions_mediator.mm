dee89395660d8d81658770daad041263edb81c0d
Ewann Pelle <ewannpv@chromium.org>
2023-09-01 13:08:48 +0000

    [iOS][TR] Hide the tile on tap
    Hides the tab resumption tile after opening the tab.
    Also checks if the local tab should be displayed before presenting it.
    
    Bug: 1464185
    Change-Id: If7aea2cb0bd05c9ff8695aaca9ffc4d34baa1c5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4822484
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191312}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_mediator.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_mediator.mm
@@ -566,6 +566,7 @@ bool CredentialProviderPromoDismissed(PrefService* local_state) {
   switch (_tabResumptionItem.itemType) {
     case TabResumptionItemType::kLastSyncedTab:
       // TODO(crbug.com/1464185): Add metrics.
+      // TODO(crbug.com/1464185): Derank or hide the tile.
       break;
     case TabResumptionItemType::kMostRecentTab: {
       [self.NTPMetricsDelegate recentTabTileOpened];
@@ -573,13 +574,15 @@ bool CredentialProviderPromoDismissed(PrefService* local_state) {
       break;
     }
   }
-  // TODO(crbug.com/1464185): Hide the tile.
 
   web::NavigationManager::WebLoadParams webLoadParams =
       web::NavigationManager::WebLoadParams(_tabResumptionItem.tabURL);
   UrlLoadParams params = UrlLoadParams::SwitchToTab(webLoadParams);
   params.web_params.transition_type = ui::PAGE_TRANSITION_AUTO_BOOKMARK;
   UrlLoadingBrowserAgent::FromBrowser(self.browser)->Load(params);
+
+  [self.consumer hideTabResumption];
+  _tabResumptionItem = nil;
 }
 
 #pragma mark - ContentSuggestionsGestureCommands
@@ -627,8 +630,9 @@ bool CredentialProviderPromoDismissed(PrefService* local_state) {
 #pragma mark - StartSurfaceRecentTabObserving
 
 - (void)mostRecentTabWasRemoved:(web::WebState*)web_state {
-  if (IsTabResumptionEnabled()) {
-    // TODO(crbug.com/1464185): Implement this.
+  if (IsTabResumptionEnabled() && _tabResumptionItem) {
+    _tabResumptionItem = nil;
+    [self.consumer hideTabResumption];
   } else {
     [self hideRecentTabTile];
   }
@@ -1223,6 +1227,10 @@ bool CredentialProviderPromoDismissed(PrefService* local_state) {
     [self.consumer showTabResumptionWithItem:_tabResumptionItem];
     return;
   }
+
+  _tabResumptionHelper->SetCanSHowMostRecentItem(
+      NewTabPageTabHelper::FromWebState(self.webState)
+          ->ShouldShowStartSurface());
 
   __weak __typeof(self) weakSelf = self;
   _tabResumptionHelper->LastTabResumptionItem(^(TabResumptionItem* item) {
