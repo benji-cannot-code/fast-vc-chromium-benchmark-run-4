c38626e6d834f68a40851f024939edf4eff22fb6
Jan Keitel <jkeitel@google.com>
2023-09-01 12:05:46 +0000

    Add Objective-C bridge for AutofillManager::Observer.
    This CL is preparation for having the SharedPasswordController observe
    the AutofillManagers of a WebState. This will enable moving the
    propagation logic for Autofill server predictions to PWM to a central
    location for both Chrome and WebView on iOS.
    
    Low-coverage-reason: Tests would not be useful, it's purely bridge code.
    Bug: 1466435
    Change-Id: Idc7f96ab406e3c83ffa04e4b186fd959b223898e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4832432
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191294}

---
+++ components/autofill/ios/browser/autofill_manager_observer_bridge.mm
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "components/autofill/ios/browser/autofill_manager_observer_bridge.h"
+
+namespace autofill {
+
+AutofillManagerObserverBridge::AutofillManagerObserverBridge(
+    id<AutofillManagerObserver> observer)
+    : observer_(observer) {}
+
+AutofillManagerObserverBridge::~AutofillManagerObserverBridge() = default;
+
+void AutofillManagerObserverBridge::OnAutofillManagerDestroyed(
+    AutofillManager& manager) {
+  const SEL selector = @selector(onAutofillManagerDestroyed:);
+  if (![observer_ respondsToSelector:selector]) {
+    return;
+  }
+  [observer_ onAutofillManagerDestroyed:manager];
+}
+
+void AutofillManagerObserverBridge::OnFieldTypesDetermined(
+    AutofillManager& manager,
+    FormGlobalId form,
+    FieldTypeSource source) {
+  const SEL selector = @selector(onFieldTypesDetermined:forForm:fromSource:);
+  if (![observer_ respondsToSelector:selector]) {
+    return;
+  }
+  [observer_ onFieldTypesDetermined:manager forForm:form fromSource:source];
+}
+
+}  // namespace autofill
