718226b7444bd676542c0372c75f0bffd9c06d62
Roman Arora <romanarora@chromium.org>
2023-09-01 17:54:01 +0000

    [ntp-history] Actionable header tile
    A follow up will address the entire header tile being focusable when
    the suggestion chip feature flag is false.
    
    Demo:
    Narrow modules:
    https://drive.google.com/file/d/1AclQjcAOZH1_dZRrrHIccQIJLdqvUFZw/view
    
    Screenshots:
    https://drive.google.com/corp/drive/u/0/folders/1i3eicOrVdcm6IEB3dq4IWICUWuOuameK
    https://drive.google.com/file/d/1CBcgT1GpANPWRkqXajyzSfmiJjGd8-yz/view
    
    Fixed: 1474050
    Change-Id: Ib7b2b84d6ba35d07a75a1b617fef862c3d9d7a76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4827348
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191458}

--- chrome/browser/resources/new_tab_page/modules/v2/history_clusters/module.ts
+++ chrome/browser/resources/new_tab_page/modules/v2/history_clusters/module.ts
@@ -72,7 +72,7 @@ export class HistoryClustersModuleElement extends I18nMixin
 
       showRelatedSearches: {
         type: Boolean,
-        computed: `computeShowRelatedSearches()`,
+        computed: `computeShowRelatedSearches(cluster)`,
         reflectToAttribute: true,
       },
     };
@@ -177,10 +177,16 @@ export class HistoryClustersModuleElement extends I18nMixin
     this.$.infoDialogRender.get().showModal();
   }
 
+  private onSuggestClick_() {
+    HistoryClustersProxyImpl.getInstance().handler.recordClick(this.cluster.id);
+    this.dispatchEvent(new Event('usage', {bubbles: true, composed: true}));
+  }
+
   private onShowAllButtonClick_() {
-    assert(this.cluster.label.length >= 2);
+    assert(this.cluster.label.length >= 2, 'Unexpected cluster label length');
     HistoryClustersProxyImpl.getInstance().handler.showJourneysSidePanel(
         this.cluster.label.substring(1, this.cluster.label.length - 1));
+    this.dispatchEvent(new Event('usage', {bubbles: true, composed: true}));
   }
 
   private shouldShowCartTile_(cart: Object): boolean {
