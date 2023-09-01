a38ce90d4deebad1bd358716844bb5eb8ee8b09f
Jun Ishiguro <junis@google.com>
2023-09-01 08:22:10 +0000

    Use CHECK_EQ rather than DCHECK_EQ for cheap validations
    According to the style guide [1], we should use CHECK() family for cheap
    assertions. This CL changes the sync-internals page's message handlers
    to use CHECK_EQ for validating the number of arguments.
    
    [1]: https://chromium.googlesource.com/chromium/src/+/main/styleguide/c++/c++.md#check_dcheck_notreached_noreturn_and-notreached
    
    Change-Id: Ic4ed33f411712f2be96cf4f553608d02ccea2ae0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4834552
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1191218}

--- chrome/browser/ui/webui/sync_internals/sync_internals_message_handler.cc
+++ chrome/browser/ui/webui/sync_internals/sync_internals_message_handler.cc
@@ -166,7 +166,7 @@ void SyncInternalsMessageHandler::RegisterMessages() {
 
 void SyncInternalsMessageHandler::HandleRequestDataAndRegisterForUpdates(
     const base::Value::List& args) {
-  DCHECK(args.empty());
+  CHECK(args.empty());
   AllowJavascript();
 
   // is_registered_ flag protects us from double-registering.  This could
@@ -187,7 +187,7 @@ void SyncInternalsMessageHandler::HandleRequestDataAndRegisterForUpdates(
 
 void SyncInternalsMessageHandler::HandleRequestListOfTypes(
     const base::Value::List& args) {
-  DCHECK(args.empty());
+  CHECK(args.empty());
   AllowJavascript();
 
   base::Value::Dict event_details;
@@ -203,7 +203,7 @@ void SyncInternalsMessageHandler::HandleRequestListOfTypes(
 
 void SyncInternalsMessageHandler::HandleRequestIncludeSpecificsInitialState(
     const base::Value::List& args) {
-  DCHECK(args.empty());
+  CHECK(args.empty());
   AllowJavascript();
 
   base::Value::Dict value;
@@ -216,7 +216,7 @@ void SyncInternalsMessageHandler::HandleRequestIncludeSpecificsInitialState(
 
 void SyncInternalsMessageHandler::HandleGetAllNodes(
     const base::Value::List& args) {
-  DCHECK_EQ(1U, args.size());
+  CHECK_EQ(1U, args.size());
   AllowJavascript();
 
   const std::string& callback_id = args[0].GetString();
@@ -235,14 +235,14 @@ void SyncInternalsMessageHandler::HandleGetAllNodes(
 
 void SyncInternalsMessageHandler::HandleSetIncludeSpecifics(
     const base::Value::List& args) {
-  DCHECK_EQ(1U, args.size());
+  CHECK_EQ(1U, args.size());
   AllowJavascript();
   include_specifics_ = args[0].GetBool();
 }
 
 void SyncInternalsMessageHandler::HandleWriteUserEvent(
     const base::Value::List& args) {
-  DCHECK_EQ(2U, args.size());
+  CHECK_EQ(2U, args.size());
   AllowJavascript();
 
   Profile* profile = Profile::FromWebUI(web_ui());
@@ -267,7 +267,7 @@ void SyncInternalsMessageHandler::HandleWriteUserEvent(
 
 void SyncInternalsMessageHandler::HandleRequestStart(
     const base::Value::List& args) {
-  DCHECK_EQ(0U, args.size());
+  CHECK_EQ(0U, args.size());
 
   SyncService* service = GetSyncService();
   if (!service) {
@@ -284,7 +284,7 @@ void SyncInternalsMessageHandler::HandleRequestStart(
 
 void SyncInternalsMessageHandler::HandleRequestStopClearData(
     const base::Value::List& args) {
-  DCHECK_EQ(0U, args.size());
+  CHECK_EQ(0U, args.size());
 
   SyncService* service = GetSyncService();
   if (!service) {
