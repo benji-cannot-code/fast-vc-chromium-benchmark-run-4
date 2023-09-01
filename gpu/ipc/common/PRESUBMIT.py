80a5943dbd434e032369ac1d5e23a53f0ac860d6
Gavin Mak <gavinmak@google.com>
2023-09-01 17:53:11 +0000

    Replace input_api.temporary_directory with tempfile.temporary_directory
    input_api exposed a `temporary_directory` method as a replacement for
    py3's tempfile.TemporaryDirectory. Now that py2 is no longer supported,
    replace input_api's temp dir method.
    
    Bug: 1475402
    Change-Id: I9863bf7cc3079fb8774268af5ea89bd8a4cd8554
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4818069
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Gavin Mak <gavinmak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1191457}

--- gpu/ipc/common/PRESUBMIT.py
+++ gpu/ipc/common/PRESUBMIT.py
@@ -10,6 +10,7 @@ for more details on the presubmit API built into depot_tools.
 """
 
 import os.path
+import tempfile
 
 
 def CommonChecks(input_api, output_api):
@@ -34,7 +35,7 @@ def CommonChecks(input_api, output_api):
           'Vulkan types generated files changed but the generator '
           'did not.', long_text=long_text))
 
-  with input_api.temporary_directory() as temp_dir:
+  with tempfile.TemporaryDirectory() as temp_dir:
     commands = []
     if generating_files:
       commands.append(input_api.Command(name='generate_vulkan_types',
--- gpu/vulkan/PRESUBMIT.py
+++ gpu/vulkan/PRESUBMIT.py
@@ -10,6 +10,7 @@ for more details on the presubmit API built into depot_tools.
 """
 
 import os.path
+import tempfile
 
 def CommonChecks(input_api, output_api):
   generating_files = input_api.AffectedFiles(
@@ -31,7 +32,7 @@ def CommonChecks(input_api, output_api):
           'Vulkan function pointer generated files changed but the generator '
           'did not.', long_text=long_text))
 
-  with input_api.temporary_directory() as temp_dir:
+  with tempfile.TemporaryDirectory() as temp_dir:
     commands = []
     if generating_files:
       python_executable = input_api.python3_executable
