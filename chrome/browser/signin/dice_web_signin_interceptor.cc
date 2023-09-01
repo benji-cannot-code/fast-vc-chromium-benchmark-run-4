c99ded009a201282ee5b7ad8ea5555b4ffe93295
Yann Dago <ydago@chromium.org>
2023-09-01 15:07:25 +0000

    Create new profile separation policies
    Add ProfileSeparationSettings, ProfileSeparationDataMigrationSettings
    and ProfileSeparationSecondaryDomainAllowlist policies and put them
    under the atomic policy group PolicySeparation.
    
    Theses policies are not functional at the moment and their
    implementation will be done in future CLs.
    
    Bug: 1475380
    Change-Id: I94f6e94d6e18057e82062fd3e5efc1f35df1d084
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4770014
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1191364}

--- chrome/browser/signin/dice_web_signin_interceptor.cc
+++ chrome/browser/signin/dice_web_signin_interceptor.cc
@@ -122,6 +122,10 @@ void DiceWebSigninInterceptor::RegisterProfilePrefs(
                                std::string());
   registry->RegisterBooleanPref(
       prefs::kManagedAccountsSigninRestrictionScopeMachine, false);
+  registry->RegisterIntegerPref(prefs::kProfileSeparationSettings, 0);
+  registry->RegisterIntegerPref(prefs::kProfileSeparationDataMigrationSettings,
+                                1);
+  registry->RegisterListPref(prefs::kProfileSeparationDomainExceptionList);
 }
 
 absl::optional<SigninInterceptionHeuristicOutcome>
