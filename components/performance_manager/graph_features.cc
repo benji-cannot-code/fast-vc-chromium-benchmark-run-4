7d1f46adfcf8779493970e14740411b9edfb841c
Joe Mason <joenotcharles@google.com>
2023-09-01 18:54:18 +0000

    Add ProcessContextRegistry
    Adds a registry of ProcessContext objects, plus shared storage and
    testing infrastructure that will be used by registries of other
    ResourceContext types.
    
    R=anthonyvd
    
    Bug: 1471683
    Change-Id: I6de12fc74c515ee26bd30a59398bdc927584f0e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4806951
    Reviewed-by: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1191505}

--- components/performance_manager/graph_features.cc
+++ components/performance_manager/graph_features.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/performance_manager/graph/worker_node_impl_describer.h"
 #include "components/performance_manager/public/graph/graph.h"
 #include "components/performance_manager/public/metrics/metrics_collector.h"
+#include "components/performance_manager/resource_attribution/resource_context_registry_storage.h"
 #include "components/performance_manager/v8_memory/v8_context_tracker.h"
 
 #if !BUILDFLAG(IS_ANDROID)
@@ -56,6 +57,11 @@ void GraphFeatures::ConfigureGraph(Graph* graph) const {
     Install<PageLoadTrackerDecorator>(graph);
   if (flags_.process_hosted_content_types_aggregator)
     Install<ProcessHostedContentTypesAggregator>(graph);
+  if (flags_.resource_attribution_registries) {
+    // ResourceContextRegistryStorage owns the facades for each ResourceContext
+    // type.
+    Install<resource_attribution::ResourceContextRegistryStorage>(graph);
+  }
 
 #if !BUILDFLAG(IS_ANDROID)
   if (flags_.site_data_recorder)
