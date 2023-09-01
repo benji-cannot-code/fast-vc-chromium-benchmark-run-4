33e5808a00e66037e511a82a83b43c550d0ad2eb
Hsin-chen Chuang <chharry@chromium.org>
2023-09-01 09:32:42 +0000

    [floss] BluetoothSocketFloss: Run listen/close callback on state changed
    Accept()/Disconnect() call fails if we haven't yet received the socket
    info, and this may cause leaking. Run the listen success callback when
    the info arrived to avoid the failure.
    
    Floss rejects listening using RFCOMM with the same UUID. Invoke the
    disconnect callback on state changed to prevent UI from listening the
    same UUID while the previous one is not yet cleaned up.
    
    BUG=b:296819230
    TEST=Verify no duplicate NearybyShare UUIDs on Cherry/Tomato
    TEST=./out/Default/device_unittests --gtest_filter='*Floss*' \
                                        --gtest_repeat=500
    
    Change-Id: I650963caae110edd17f7502ffc0589ab30737be0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4821466
    Commit-Queue: Hsin-chen Chuang <chharry@google.com>
    Reviewed-by: Abhishek Pandit-Subedi <abhishekpandit@google.com>
    Cr-Commit-Position: refs/heads/main@{#1191246}

--- device/bluetooth/floss/bluetooth_socket_floss.cc
+++ device/bluetooth/floss/bluetooth_socket_floss.cc
@@ -151,6 +151,14 @@ void BluetoothSocketFloss::Disconnect(base::OnceClosure callback) {
                        weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
     listening_socket_info_ = absl::nullopt;
     pending_accept_socket_.reset();
+  } else {
+    if (pending_listen_ready_callback_) {
+      LOG(WARNING) << "Disconnecting listening socket before it is ready, "
+                   << "which may cause leaking!";
+      pending_listen_ready_callback_.Reset();
+    } else {
+      LOG(WARNING) << "Disconnecting socket (" << this << ") with no info";
+    }
   }
 
   // If there is a pending accept, clear it.
@@ -209,6 +217,12 @@ void BluetoothSocketFloss::DoConnectionStateChanged(
     listening_socket_info_ = socket;
   }
 
+  // Since we've received the socket info, we are ready for |Accept| and
+  // |Disconnect| now.
+  if (pending_listen_ready_callback_) {
+    std::move(pending_listen_ready_callback_).Run();
+  }
+
   // Every time we get a socket state update, the socket gets reset to a not
   // accepting state. Mark our internal state to match that.
   is_accepting_ = false;
@@ -221,6 +235,17 @@ void BluetoothSocketFloss::DoConnectionStateChanged(
         listening_socket_info_->id, absl::nullopt,
         base::BindOnce(&BluetoothSocketFloss::CompleteAccept,
                        weak_ptr_factory_.GetWeakPtr()));
+    return;
+  }
+
+  if (state == FlossSocketManager::ServerSocketState::kClosed) {
+    if (pending_listen_close_callback_) {
+      std::move(pending_listen_close_callback_).Run();
+    } else {
+      LOG(WARNING) << "Server socket with uuid "
+                   << listening_socket_info_->uuid.value()
+                   << " closed unexpectedly";
+    }
     return;
   }
 
@@ -257,7 +282,8 @@ void BluetoothSocketFloss::CompleteListen(
     return;
   }
 
-  std::move(success_callback).Run();
+  // On success, the callbacks will be invoked in DoConnectionStateChanged.
+  pending_listen_ready_callback_ = std::move(success_callback);
 }
 
 void BluetoothSocketFloss::CompleteConnect(
@@ -352,6 +378,11 @@ void BluetoothSocketFloss::CompleteClose(
     DBusResult<FlossDBusClient::BtifStatus> result) {
   if (result.has_value()) {
     DVLOG(1) << "Result of closing socket = " << static_cast<uint32_t>(*result);
+    if (*result == FlossDBusClient::BtifStatus::kSuccess) {
+      // If |Close| succeeded, run the callback in DoConnectionStateChanged.
+      pending_listen_close_callback_ = std::move(callback);
+      return;
+    }
   }
 
   is_accepting_ = false;
