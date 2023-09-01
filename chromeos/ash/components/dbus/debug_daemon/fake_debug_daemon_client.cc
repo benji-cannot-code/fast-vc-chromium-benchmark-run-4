f9a56ecadb85b3b9d371732104ecf27286b6ddc2
Bryan Cain <bryancain@chromium.org>
2023-09-01 16:57:24 +0000

    Send the user's language to cupsd via debugd at printer setup time
    This is needed so that CUPS knows what language to use for the names of
    non-standard media types. (The registered/standard media type names are
    already localized within Chromium.)
    
          language in system settings, verify that non-standard media types
          are in the correct language
    
    Bug: b:269772562
    Test: enable the borderless printing feature flag, set a non-English
    Change-Id: Ica38e3ca1021b938bc5c0ba90a293f7d93fe21a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4781263
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Bryan Cain <bryancain@chromium.org>
    Reviewed-by: Benjamin Gordon <bmgordon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191419}

--- chromeos/ash/components/dbus/debug_daemon/fake_debug_daemon_client.cc
+++ chromeos/ash/components/dbus/debug_daemon/fake_debug_daemon_client.cc
@@ -248,6 +248,7 @@ void FakeDebugDaemonClient::SetServiceIsAvailable(bool is_available) {
 void FakeDebugDaemonClient::CupsAddManuallyConfiguredPrinter(
     const std::string& name,
     const std::string& uri,
+    const std::string& language,
     const std::string& ppd_contents,
     CupsAddPrinterCallback callback) {
   printers_.insert(name);
@@ -258,6 +259,7 @@ void FakeDebugDaemonClient::CupsAddManuallyConfiguredPrinter(
 void FakeDebugDaemonClient::CupsAddAutoConfiguredPrinter(
     const std::string& name,
     const std::string& uri,
+    const std::string& language,
     CupsAddPrinterCallback callback) {
   printers_.insert(name);
   base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
