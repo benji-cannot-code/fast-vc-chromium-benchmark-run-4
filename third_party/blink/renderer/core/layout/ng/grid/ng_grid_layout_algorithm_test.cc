bee9973e7a2e82c89272d4cecc8cb493849e0a0e
Ian Kilpatrick <ikilpatrick@chromium.org>
2023-09-01 01:32:17 +0000

    [grid] Revert LayoutNewGridStaticPosition behaviour.
    Too many failures on Canary for this change to be web-compatible
    unfortunately.
    
    Bug: 1454831, 1477880
    Change-Id: I215cb9d4e34ed55da96f6e32781044b7a2794552
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4833670
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191107}

--- third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm_test.cc
+++ third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm_test.cc
@@ -1392,10 +1392,6 @@ TEST_F(NGGridLayoutAlgorithmTest, ItemsSizeWithGap) {
 }
 
 TEST_F(NGGridLayoutAlgorithmTest, PositionedOutOfFlowItems) {
-  if (RuntimeEnabledFeatures::LayoutNewGridStaticPositionEnabled()) {
-    return;
-  }
-
   SetBodyInnerHTML(R"HTML(
     <style>
       #grid {
