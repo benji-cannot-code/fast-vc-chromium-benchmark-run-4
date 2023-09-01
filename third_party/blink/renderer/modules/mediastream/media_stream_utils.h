49e7e88799816ac7912da8eb79da494baacf2ed0
Tony <legendmastertony@gmail.com>
2023-09-01 17:27:26 +0000

    Add a feature to control calculating scale factor
    On calling getDisplayMedia without size constraints,
    the size blink sends to screen capture component
    is smaller than the screen size
    when device scale factor is larger than 1
    
    The feature is enabled by default on all platforms except ChromeOS due to timeouts observed on some devices.
    
    Bug: 1429161
    Change-Id: I939fd0913f0878a1626749db7864411ca27db85b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4826746
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191439}

--- third_party/blink/renderer/modules/mediastream/media_stream_utils.h
+++ third_party/blink/renderer/modules/mediastream/media_stream_utils.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASTREAM_MEDIA_STREAM_UTILS_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASTREAM_MEDIA_STREAM_UTILS_H_
 
+#include "base/feature_list.h"
 #include "base/memory/scoped_refptr.h"
 #include "third_party/blink/public/mojom/mediastream/media_stream.mojom-blink.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
@@ -18,6 +19,8 @@ class ExecutionContext;
 class LocalFrame;
 class MediaStreamSource;
 class MediaStreamTrack;
+
+MODULES_EXPORT BASE_DECLARE_FEATURE(kGetDisplayMediaScreenScaleFactor);
 
 class MODULES_EXPORT MediaStreamUtils {
   STATIC_ONLY(MediaStreamUtils);
--- third_party/blink/renderer/modules/mediastream/media_stream_utils_test.cc
+++ third_party/blink/renderer/modules/mediastream/media_stream_utils_test.cc
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/mediastream/media_stream_utils.h"
 
+#include "base/test/scoped_feature_list.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/core/testing/core_unit_test_helper.h"
 #include "ui/display/screen_info.h"
@@ -24,11 +25,18 @@ class FakeChromeClient : public RenderingTestChromeClient {
 
 class ScreenSizeTest : public RenderingTest {
  public:
+  ScreenSizeTest() {
+    scoped_feature_list_.InitAndEnableFeature(
+        blink::kGetDisplayMediaScreenScaleFactor);
+  }
   FakeChromeClient& GetChromeClient() const override { return *client_; }
 
  protected:
   Persistent<FakeChromeClient> client_ =
       MakeGarbageCollected<FakeChromeClient>();
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 TEST_F(ScreenSizeTest, Basic) {
@@ -37,6 +45,15 @@ TEST_F(ScreenSizeTest, Basic) {
   client_->AddScreenInfo(screen);
   EXPECT_EQ(MediaStreamUtils::GetScreenSize(&GetFrame()),
             gfx::Size(1920, 1200));
+}
+
+TEST_F(ScreenSizeTest, ScaleFactor) {
+  display::ScreenInfo screen;
+  screen.rect = gfx::Rect(1536, 864);
+  screen.device_scale_factor = 1.25;
+  client_->AddScreenInfo(screen);
+  EXPECT_EQ(MediaStreamUtils::GetScreenSize(&GetFrame()),
+            gfx::Size(1920, 1080));
 }
 
 TEST_F(ScreenSizeTest, MultiScreen) {
@@ -48,6 +65,27 @@ TEST_F(ScreenSizeTest, MultiScreen) {
   client_->AddScreenInfo(screen_2);
   EXPECT_EQ(MediaStreamUtils::GetScreenSize(&GetFrame()),
             gfx::Size(1920, 2560));
+}
+
+TEST_F(ScreenSizeTest, MultiScreenScaleFactor) {
+  display::ScreenInfo screen_1;
+  display::ScreenInfo screen_2;
+  screen_1.rect = gfx::Rect(1920, 1080);
+  screen_1.device_scale_factor = 2;
+  client_->AddScreenInfo(screen_1);
+  screen_2.rect = gfx::Rect(1440, 2560);
+  client_->AddScreenInfo(screen_2);
+  EXPECT_EQ(MediaStreamUtils::GetScreenSize(&GetFrame()),
+            gfx::Size(3840, 2560));
+}
+
+TEST_F(ScreenSizeTest, RoundUpSizeScaleFactor) {
+  display::ScreenInfo screen;
+  screen.rect = gfx::Rect(1097, 617);
+  screen.device_scale_factor = 1.75;
+  client_->AddScreenInfo(screen);
+  EXPECT_EQ(MediaStreamUtils::GetScreenSize(&GetFrame()),
+            gfx::Size(1920, 1080));
 }
 
 }  // namespace
