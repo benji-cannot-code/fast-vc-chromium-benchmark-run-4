86808fdebcb59cf62bf6542b3c119f83bbc71d23
Marcello Salomao <msalomao@google.com>
2023-09-01 02:19:23 +0000

    [Files app] Ducks: add addAction method to Slice
    The addAction method allow actions to be created in
    a way that lets multiple reducer across different slices
    handle the same action type.
    
    As a demonstration of this new method, the reducer function
    `updateDeviceConnectionStateReducer` has been split into
    2 slices: volumes and device.
    
    Notice that, as expected, call sites remain unchanged as a
    result of this CL.
    
    Bug: b:295777015
    Test: browser_tests FileManagerJsTest.ReducerDevice
    Change-Id: I3f51c73e43cf1bf8f6f5e56782c893af4b3c32fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4829403
    Commit-Queue: Marcello Salomao <msalomao@google.com>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191125}

--- ui/file_manager/file_manager/state/ducks/device.ts
+++ ui/file_manager/file_manager/state/ducks/device.ts
@@ -3,14 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {VolumeEntry} from '../../common/js/files_app_entry_types.js';
-import {util} from '../../common/js/util.js';
-import {State, Volume} from '../../externs/ts/state.js';
+import {State} from '../../externs/ts/state.js';
 import {Slice} from '../../lib/base_store.js';
-import {updateFileData} from '../ducks/all_entries.js';
-import {getEntry} from '../store.js';
-
-import {updateVolume} from './volumes.js';
 
 /**
  * @fileoverview Device slice of the store.
@@ -27,7 +21,6 @@ function updateDeviceConnectionStateReducer(currentState: State, payload: {
   connection: chrome.fileManagerPrivate.DeviceConnectionState,
 }): State {
   let device: State['device']|undefined;
-  let volumes: State['volumes']|undefined;
 
   // Device connection.
   if (payload.connection !== currentState.device.connection) {
@@ -37,51 +30,5 @@ function updateDeviceConnectionStateReducer(currentState: State, payload: {
     };
   }
 
-  // Find ODFS volume(s) and disable it (or them) if offline.
-  const disableODFS = payload.connection ===
-      chrome.fileManagerPrivate.DeviceConnectionState.OFFLINE;
-  for (const volume of Object.values<Volume>(currentState.volumes)) {
-    if (!util.isOneDriveId(volume.providerId) ||
-        volume.isDisabled === disableODFS) {
-      continue;
-    }
-    const updatedVolume =
-        updateVolume(currentState, volume.volumeId, {isDisabled: disableODFS});
-    if (updatedVolume) {
-      if (!volumes) {
-        volumes = {
-          ...currentState.volumes,
-          [volume.volumeId]: updatedVolume,
-        };
-      } else {
-        volumes[volume.volumeId] = updatedVolume;
-      }
-    }
-    // Make the ODFS FileData/VolumeEntry consistent with its volume in the
-    // store.
-    updateFileData(currentState, volume.rootKey!, {disabled: disableODFS});
-    const odfsVolumeEntry =
-        getEntry(currentState, volume.rootKey!) as VolumeEntry;
-    if (odfsVolumeEntry) {
-      odfsVolumeEntry.disabled = disableODFS;
-    }
-  }
-
-  if (!device && !volumes) {
-    return currentState;
-  }
-
-  const newState = {
-    ...currentState,
-  };
-
-  if (device) {
-    newState.device = device;
-  }
-
-  if (volumes) {
-    newState.volumes = volumes;
-  }
-
-  return newState;
+  return device ? {...currentState, device} : currentState;
 }
