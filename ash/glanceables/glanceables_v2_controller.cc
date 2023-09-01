e9a2dc4cd4a79b2a41540de0f26220946f0417fe
Artsiom Mitrokhin <amitrokhin@google.com>
2023-09-01 00:38:59 +0000

    Update logic of selecting initial value for task lists combobox
    Bug: b/292002951
    Change-Id: I7ee276390ceffe57d2235083566844feca37ee68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4831916
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191090}

--- ash/glanceables/glanceables_v2_controller.cc
+++ ash/glanceables/glanceables_v2_controller.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/glanceables/tasks/glanceables_tasks_client.h"
 #include "ash/public/cpp/session/session_controller.h"
 #include "ash/system/unified/classroom_bubble_student_view.h"
+#include "ash/system/unified/tasks_combobox_model.h"
 #include "base/check.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
@@ -34,6 +35,7 @@ void GlanceablesV2Controller::RegisterUserProfilePrefs(
     PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(prefs::kGlanceablesEnabled, true);
   ClassroomBubbleStudentView::RegisterUserProfilePrefs(registry);
+  TasksComboboxModel::RegisterUserProfilePrefs(registry);
 }
 
 void GlanceablesV2Controller::OnActiveUserSessionChanged(
