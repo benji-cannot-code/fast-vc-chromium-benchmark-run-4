b9c9d20d661ef8747989ec95f5c6135fda482bc9
Ye Tian <tianye@microsoft.com>
2023-09-01 17:21:20 +0000

    [iOS] CBDCoordinator requests its owner to stop it and destruct itself
    Following this cl: crrev.com/c/4729590
    
    Note that:
    * clearBrowsingDataTableViewControllerWantsDismissal is called when
    user press "Done" button on the right of navigation bar
    * clearBrowsingDataTableViewControllerWasRemoved is called when user
    swipe down the HistoryCBD page.
    * Both of the above actions only want to dismiss HistoryCBD page and
    do not want to dismiss History, so should not call
    `closeHistoryWithCompletion` which results close history page.
    * But now, if user pull down to close the HistoryCBD page, it will
    cause History page close too.
    
    Finally, HistoryCoordinator need to destruct its owned
    HistoryCBDCoordinator when HistoryCBDCoordinator stop.
    
    Bug: 1475103
    Change-Id: I51ea698fd2f078355b105b076eacfbc5a9ae3b71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800833
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Tian Ye <tianye@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1191433}

--- ios/chrome/browser/ui/history/history_clear_browsing_data_coordinator.mm
+++ ios/chrome/browser/ui/history/history_clear_browsing_data_coordinator.mm
@@ -112,11 +112,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)clearBrowsingDataTableViewControllerWantsDismissal:
     (ClearBrowsingDataTableViewController*)controller {
   CHECK_EQ(controller, self.clearBrowsingDataTableViewController);
-  [self stopWithCompletion:nil];
-}
-
-- (void)dismissClearBrowsingData {
-  DCHECK(self.historyClearBrowsingDataNavigationController);
   [self.delegate dismissHistoryClearBrowsingData:self withCompletion:nil];
 }
 
