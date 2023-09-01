f9d1f497bf6c2b3f339f2e8ca02b8da3b9b6b2cb
David Bertoni <dbertoni@chromium.org>
2023-09-01 01:33:24 +0000

    [Extensions] Add a histogram for the number of context menu items.
    The CL implements a histogram that counts the number of context menu
    items read from storage for an extension. We'll use the results to
    determine a reasonable limit for the number of context menu items an
    extension can create.
    
    Bug: 1475294
    Change-Id: I70f65351340429c89bf082d04435606bf69705a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4819572
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191109}

---
+++ chrome/test/data/extensions/api_test/context_menus/count/background.js
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function createMenuItems() {
+  // Create two parent items with two children each.
+  let parents = [];
+  parents.push(chrome.contextMenus.create({
+    title: 'Test parent item 1',
+    id: 'parent1'
+  }));
+  parents.push(chrome.contextMenus.create({
+    title: 'Test parent item 2',
+    id: 'parent2'
+  }));
+
+  let currentId = 1;
+  parents.forEach((parent) => {
+    let idString = 'child' + currentId;
+    ++currentId;
+    chrome.contextMenus.create({
+      title: 'Child 1',
+      parentId: parent,
+      id: idString
+    });
+
+    idString = 'child' + currentId;
+    ++currentId;
+    chrome.contextMenus.create({
+      title: 'Child 2',
+      parentId: parent,
+      id: idString
+    });
+  });
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+  // Create the menu items and signal success.
+  createMenuItems();
+  chrome.test.assertNoLastError();
+  chrome.test.notifyPass();
+});
