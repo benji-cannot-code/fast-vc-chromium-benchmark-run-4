54dd85c8f3c954370c61ef86cd992caacefead52
Xinghui Lu <xinghuilu@chromium.org>
2023-09-01 22:12:21 +0000

    [SBv5] Add chrome flag: safe-browsing-hash-prefix on Android
    Only add the flag on Android because it is not useful on desktop
    without a valid relay URL.
    
    Bug: 1444511
    Change-Id: If2a808a478a8bec628c6dfe7951fd250c6df5dc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4834820
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191619}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -4264,6 +4264,12 @@ const char kRevokeNotificationsPermissionIfDisabledOnAppLevelDescription[] =
     "Allow revoking site-level notification permission if Chrome has no "
     "app-level notification permission on Android.";
 
+const char kSafeBrowsingHashPrefixRealTimeLookupsName[] =
+    "Safe Browsing Hash Prefix Real Time Lookups";
+const char kSafeBrowsingHashPrefixRealTimeLookupsDescription[] =
+    "Enable checking URLs through Safe Browsing hash-prefix real time "
+    "protocol.";
+
 const char kSafeModeForCachedFlagsName[] = "Safe Mode for Cached Flags";
 const char kSafeModeForCachedFlagsDescription[] =
     "Attempts recovery from startup crash loops caused by a bad field trial "
