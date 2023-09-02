2497fb03e644bfd09b58678a00221550cecc3cf7
Matt Reynolds <mattreynolds@google.com>
2023-09-02 01:01:59 +0000

    serial: Check for new RFCOMM serial ports on DeviceChanged
    A Bluetooth Classic device may support more services than contained in
    the initial advertisement, with additional service UUIDs included in a
    later advertisement. Modify BluetoothSerialDeviceEnumerator's
    BluetoothAdapter observer to observe DeviceChanged events and create
    serial ports for any newly advertised services.
    
    Bug: b/293246910
    Change-Id: I342e0f7ffc0bda9a9b092eaa54501332a2c701ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4795124
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191680}

--- services/device/serial/bluetooth_serial_device_enumerator.cc
+++ services/device/serial/bluetooth_serial_device_enumerator.cc
@@ -52,6 +52,8 @@ class BluetoothSerialDeviceEnumerator::AdapterHelper
   void DeviceAdded(BluetoothAdapter* adapter, BluetoothDevice* device) override;
   void DeviceRemoved(BluetoothAdapter* adapter,
                      BluetoothDevice* device) override;
+  void DeviceChanged(BluetoothAdapter* adapter,
+                     BluetoothDevice* device) override;
 
   void OpenPort(const std::string& address,
                 const BluetoothUUID& service_class_id,
@@ -134,6 +136,17 @@ void BluetoothSerialDeviceEnumerator::AdapterHelper::DeviceRemoved(
   enumerator_runner_->PostTask(
       FROM_HERE, base::BindOnce(&BluetoothSerialDeviceEnumerator::DeviceRemoved,
                                 enumerator_, device->GetAddress()));
+}
+
+void BluetoothSerialDeviceEnumerator::AdapterHelper::DeviceChanged(
+    BluetoothAdapter* adapter,
+    BluetoothDevice* device) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  enumerator_runner_->PostTask(
+      FROM_HERE,
+      base::BindOnce(&BluetoothSerialDeviceEnumerator::DeviceAdded, enumerator_,
+                     device->GetAddress(), device->GetNameForDisplay(),
+                     device->GetUUIDs()));
 }
 
 void BluetoothSerialDeviceEnumerator::AdapterHelper::OpenPort(
@@ -241,6 +254,14 @@ void BluetoothSerialDeviceEnumerator::DeviceAddedForTesting(
     BluetoothDevice* device) {
   // Pass the device to our helper, which will in turn pass it back to me.
   helper_.AsyncCall(&AdapterHelper::DeviceAdded)
+      .WithArgs(base::Unretained(adapter), device);
+}
+
+void BluetoothSerialDeviceEnumerator::DeviceChangedForTesting(
+    BluetoothAdapter* adapter,
+    BluetoothDevice* device) {
+  // Pass the device to our helper, which will in turn pass it back to me.
+  helper_.AsyncCall(&AdapterHelper::DeviceChanged)
       .WithArgs(base::Unretained(adapter), device);
 }
 
