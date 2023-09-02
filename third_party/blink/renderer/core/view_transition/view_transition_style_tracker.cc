9ca1c707c2cc6c6fdc455c0bccec09e4eea17cd0
Khushal Sagar <khushalsagar@chromium.org>
2023-09-02 17:15:32 +0000

    view-transition: Fix overflow rect calculation
    We currently skip LayoutText when traversing the layout tree for VT
    elements, resulting in incorrect bounds for the generated image.
    
    R=chrishtr@chromium.org
    
    Fixed: 1477703
    Change-Id: Ida988513902392d46e8c3078afaace2c429bcf88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4834245
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191726}

--- third_party/blink/renderer/core/view_transition/view_transition_style_tracker.cc
+++ third_party/blink/renderer/core/view_transition/view_transition_style_tracker.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/browser_controls.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
+#include "third_party/blink/renderer/core/layout/layout_text.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
 #include "third_party/blink/renderer/core/layout/layout_view_transition_root.h"
 #include "third_party/blink/renderer/core/page/page.h"
@@ -1723,6 +1724,18 @@ PhysicalRect ViewTransitionStyleTracker::ComputeVisualOverflowRect(
         PhysicalRect mapped_overflow_rect =
             ComputeVisualOverflowRect(*child_box, ancestor_for_recursion);
         result.Unite(mapped_overflow_rect);
+      } else if (auto* child_text = DynamicTo<LayoutText>(child)) {
+        const bool child_visible =
+            child_text->StyleRef().Visibility() == EVisibility::kVisible ||
+            !child_text->VisualRectRespectsVisibility();
+        if (!child_visible) {
+          continue;
+        }
+
+        auto overflow_rect = child_text->PhysicalVisualOverflowRect();
+        child_text->MapToVisualRectInAncestorSpace(
+            ancestor_for_recursion, overflow_rect, kUseGeometryMapper);
+        result.Unite(overflow_rect);
       }
     }
   }
