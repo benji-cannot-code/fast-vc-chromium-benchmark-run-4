4c0ec5fa5989ebe3f6b581d50030bb823ced9bde
Ewann Pelle <ewannpv@chromium.org>
2023-09-04 14:24:19 +0000

    [iOS] Launch Pinned Tabs
    Change-Id: If8e8882d1af1b088d3a93c798556be3268b8c470
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4838337
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192109}

--- ios/chrome/browser/tabs/features.mm
+++ ios/chrome/browser/tabs/features.mm
@@ -11,8 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 BASE_FEATURE(kEnablePinnedTabs,
              "EnablePinnedTabs",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsPinnedTabsEnabled() {
   if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_TABLET) {
