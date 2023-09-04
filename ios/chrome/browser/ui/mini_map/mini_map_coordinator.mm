e0013fffd04092ee4ef191255baed6706fc8cc1c
Olivier ROBIN <olivierrobin@google.com>
2023-09-04 10:55:57 +0000

    Remove title text attribute for Mini Map IPH
    Formatting is handled at the view level.
    
    Bug: 1476494
    Change-Id: I67b359280655703e70e784a1ea3742555c1415b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4834367
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1192034}

--- ios/chrome/browser/ui/mini_map/mini_map_coordinator.mm
+++ ios/chrome/browser/ui/mini_map/mini_map_coordinator.mm
@@ -179,20 +179,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     NSString* iphTitle = l10n_util::GetNSString(IDS_IOS_MINI_MAP_IPH_TITLE);
     NSString* iphSubtitle =
         l10n_util::GetNSString(IDS_IOS_MINI_MAP_IPH_SUBTITLE);
-    NSDictionary* textAttributes = @{
-      NSForegroundColorAttributeName : [UIColor colorNamed:kTextSecondaryColor],
-      NSFontAttributeName :
-          [UIFont preferredFontForTextStyle:UIFontTextStyleSubheadline]
-    };
 
     NSDictionary* linkAttributes = @{
       NSForegroundColorAttributeName : [UIColor colorNamed:kBlueColor],
-      NSFontAttributeName :
-          [UIFont preferredFontForTextStyle:UIFontTextStyleSubheadline],
       NSLinkAttributeName : net::NSURLWithGURL(GURL("chrome://settings")),
     };
-    NSAttributedString* attrSubtitle = AttributedStringFromStringWithLink(
-        iphSubtitle, textAttributes, linkAttributes);
+    NSAttributedString* attrSubtitle =
+        AttributedStringFromStringWithLink(iphSubtitle, nil, linkAttributes);
 
     [self.miniMapController configureIPHWithTitle:[[NSAttributedString alloc]
                                                       initWithString:iphTitle]
