6f1b1e556047d0a5fcfa212ae9f7751f54867006
Chung-Sheng Wu <chungsheng@google.com>
2023-09-04 03:36:14 +0000

    shimless_rma: Add 3p diag app profile unit test
    Add unit test to verify the diagnostics app profile helper. This test
    the installation of a test extension.
    
    Bug: b:297155775
    
    Change-Id: Iad69359f7263a68792a35dbfd264795846e35b79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4813301
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Nasser Al-shawwa <alshawwa@chromium.org>
    Commit-Queue: Chung-sheng Wu <chungsheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1191927}

--- chrome/browser/ash/shimless_rma/chrome_shimless_rma_delegate.h
+++ chrome/browser/ash/shimless_rma/chrome_shimless_rma_delegate.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/ash/shimless_rma/diagnostics_app_profile_helper.h"
 #include "chrome/services/qrcode_generator/public/cpp/qrcode_generator_service.h"
 #include "chrome/services/qrcode_generator/public/mojom/qrcode_generator.mojom.h"
 
@@ -49,6 +50,9 @@ class ChromeShimlessRmaDelegate : public ShimlessRmaDelegate {
                qrcode_generator::QRImageGenerator::ResponseCallback callback)>
           qrcode_service_override);
 
+  void SetDiagnosticsAppProfileHelperDelegateForTesting(
+      DiagnosticsAppProfileHelperDelegate* delegate);
+
  private:
   void OnQrCodeGenerated(
       base::OnceCallback<void(const std::string& qr_code_image)> callback,
@@ -72,6 +76,11 @@ class ChromeShimlessRmaDelegate : public ShimlessRmaDelegate {
       qrcode_generator::mojom::GenerateQRCodeRequestPtr request,
       qrcode_generator::QRImageGenerator::ResponseCallback callback)>
       qrcode_service_override_;
+
+  DiagnosticsAppProfileHelperDelegate diagnostics_app_profile_helper_delegete_;
+  raw_ptr<DiagnosticsAppProfileHelperDelegate>
+      diagnostics_app_profile_helper_delegete_ptr_{
+          &diagnostics_app_profile_helper_delegete_};
 
   base::WeakPtrFactory<ChromeShimlessRmaDelegate> weak_ptr_factory_{this};
 };
