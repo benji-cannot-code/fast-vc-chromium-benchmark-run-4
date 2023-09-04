216adb692c0869181e22ad41ea0c42939ea41ce5
Stefano Duo <stefanoduo@google.com>
2023-09-04 14:29:27 +0000

    Group all of NCN's NetLog events into type NETWORK_CHANGE_NOTIFIER
    Currently these events are added as global entries, making it
    difficult to see all of them together. With this change they will all
    appear under the same NETWORK_CHANGE_NOTIFIER source type.
    
    Before:
    Source type:NONE, Description: SPECIFIC_NETWORK_MADE_DEFAULT, Entry:
    t=97 [st=0]  SPECIFIC_NETWORK_MADE_DEFAULT
                 --> changed_network_handle = 101
                 --> changed_network_type = "CONNECTION_4G"
                 --> current_active_networks.101 = "CONNECTION_4G"
                 --> current_active_networks.104 = "CONNECTION_WIFI"
                 --> default_active_network_handle = 101
    Source type:NONE, Description: NETWORK_IP_ADDRESSES_CHANGED, Entry:
    t=97 [st=0]  NETWORK_IP_ADDRESSES_CHANGED
    Source type:NONE, Description: NETWORK_CONNECTIVITY_CHANGED, Entry:
    t=97 [st=0]  NETWORK_CONNECTIVITY_CHANGED
                 --> new_connection_type = "CONNECTION_4G"
    [...]
    
    After:
    Source type: NETWORK_CHANGE_NOTIFIER, Description: None, Entry:
    t= 88 [st=114]  SPECIFIC_NETWORK_MADE_DEFAULT
                    --> changed_network_handle = 101
                    --> changed_network_type = "CONNECTION_4G"
                    --> current_active_networks.101 = "CONNECTION_4G"
                    --> current_active_networks.104 = "CONNECTION_WIFI"
                    --> default_active_network_handle = 101
    t= 88 [st=114]  NETWORK_IP_ADDRESSES_CHANGED
    t= 89 [st=115]  NETWORK_CONNECTIVITY_CHANGED
                    --> new_connection_type = "CONNECTION_4G"
    [...]
    
    Bug: b:290583020
    Change-Id: Ib20c7931c124e5f1cf26b817c060cb05c523d737
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4812744
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1192114}

--- net/base/logging_network_change_observer.h
+++ net/base/logging_network_change_observer.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_export.h"
 #include "net/base/network_change_notifier.h"
 #include "net/base/network_handle.h"
+#include "net/log/net_log_with_source.h"
 
 namespace net {
 
@@ -48,7 +49,7 @@ class NET_EXPORT LoggingNetworkChangeObserver
   void OnNetworkSoonToDisconnect(handles::NetworkHandle network) override;
   void OnNetworkMadeDefault(handles::NetworkHandle network) override;
 
-  raw_ptr<NetLog> net_log_;
+  NetLogWithSource net_log_;
 };
 
 }  // namespace net
