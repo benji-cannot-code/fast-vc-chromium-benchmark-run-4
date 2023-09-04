1c051886ca25224cadcb15250f9dec7f77a69a0a
Alex Ilin <alexilin@chromium.org>
2023-09-04 12:14:38 +0000

    [DBSC] Make key service non-optional for registration fetcher
    UnexportableKeyService was made optional to facilitate manual testing
    but we now have a feature flag for software emulation, so this should be
    no longer needed.
    
    Bug: b/273920956
    Change-Id: I2a212823e58afcc33586627f076f5a0e031caaff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4835882
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192062}

--- chrome/browser/signin/bound_session_credentials/bound_session_registration_fetcher_impl.h
+++ chrome/browser/signin/bound_session_credentials/bound_session_registration_fetcher_impl.h
@@ -9,9 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/bound_session_credentials/bound_session_registration_fetcher.h"
 
 #include <memory>
+#include <string>
 
 #include "base/functional/callback.h"
-#include "base/memory/raw_ptr.h"
+#include "base/memory/raw_ref.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/signin/bound_session_credentials/bound_session_cookie_refresh_service.h"
 #include "chrome/browser/signin/bound_session_credentials/bound_session_registration_fetcher_param.h"
@@ -36,7 +37,7 @@ class BoundSessionRegistrationFetcherImpl
   explicit BoundSessionRegistrationFetcherImpl(
       BoundSessionRegistrationFetcherParam registration_params,
       scoped_refptr<network::SharedURLLoaderFactory> loader_factory,
-      unexportable_keys::UnexportableKeyService* key_service);
+      unexportable_keys::UnexportableKeyService& key_service);
 
   BoundSessionRegistrationFetcherImpl(
       BoundSessionRegistrationFetcherImpl&& other) = delete;
@@ -60,7 +61,7 @@ class BoundSessionRegistrationFetcherImpl
   void StartFetchingRegistration(const std::string& registration_token);
 
   BoundSessionRegistrationFetcherParam registration_params_;
-  const raw_ptr<unexportable_keys::UnexportableKeyService> key_service_;
+  const raw_ref<unexportable_keys::UnexportableKeyService> key_service_;
   std::string wrapped_key_str_;
 
   // Non-null after a fetch has started.
