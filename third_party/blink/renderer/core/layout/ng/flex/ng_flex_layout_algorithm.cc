fcfed73886ca566b93c15b0f5dc37e4d9ff9c9e6
Ian Kilpatrick <ikilpatrick@chromium.org>
2023-09-04 07:26:45 +0000

    [flex] Remove LayoutFlexNewColumnAlgorithm flag.
    Launched in M117.
    
    Bug: 507397
    Change-Id: I8cd2e2a4fc0b0484a9c54b0394cc21f99119d354
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4836606
    Auto-Submit: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191975}

--- third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.cc
@@ -2303,8 +2303,7 @@ MinMaxSizesResult NGFlexLayoutAlgorithm::ComputeMinMaxSizes(
           Node(), BorderScrollbarPadding()))
     return *result;
 
-  if (RuntimeEnabledFeatures::LayoutFlexNewColumnAlgorithmEnabled() &&
-      is_column_ && algorithm_.IsMultiline()) {
+  if (is_column_ && algorithm_.IsMultiline()) {
     return ComputeMinMaxSizeOfMultilineColumnContainer();
   }
 
