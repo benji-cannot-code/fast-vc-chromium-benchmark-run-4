470d8f3eb15c7fff35193aa3669d115f298ddf97
Mikihito Matsuura <mikt@google.com>
2023-09-04 04:38:10 +0000

    Revert "add tablet mode observer into the mediator"
    This reverts commit 2e4688f38461ddc34648764a1fb1505503740843.
    
    Reason for revert: "EditorSwitchTest.FeatureCanNotBeTriggeredOnTabletMode" failing on
    "linux-cfm-rel"
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-cfm-rel/46985/overview
    
    Original change's description:
    > add tablet mode observer into the mediator
    >
    > This CL adds a listener for tablet mode change, and updates the switch value accordingly.
    >
    > Bug: b/295440693
    > Change-Id: I4d18e643f0973ca1dbc1ee1a1964dccbe6fbcd2e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798317
    > Commit-Queue: Chuong Ho <hdchuong@google.com>
    > Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1191824}
    
    Bug: b/295440693
    Change-Id: I7f600825d5b14924abd8459db69b1c5580bb4180
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4835499
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Mikihito Matsuura <mikt@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Mikihito Matsuura <mikt@google.com>
    Cr-Commit-Position: refs/heads/main@{#1191939}

--- chrome/browser/ash/input_method/editor_mediator.cc
+++ chrome/browser/ash/input_method/editor_mediator.cc
@@ -6,9 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/input_method/editor_mediator.h"
 
 #include "ash/constants/ash_pref_names.h"
-#include "ash/public/cpp/tablet_mode.h"
-#include "ash/shell.h"
-#include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "base/check_op.h"
 #include "chrome/browser/ui/webui/ash/mako/mako_ui.h"
 #include "ui/base/ime/ash/ime_bridge.h"
@@ -31,9 +28,6 @@ EditorMediator::EditorMediator(Profile* profile, std::string_view country_code)
   g_instance_ = this;
 
   profile_observation_.Observe(profile_);
-  tablet_mode_observation_.Observe(TabletMode::Get());
-
-  editor_switch_->OnTabletModeUpdated(ash::TabletMode::IsInTabletMode());
 }
 
 EditorMediator::~EditorMediator() {
@@ -72,18 +66,6 @@ void EditorMediator::OnBlur() {
 
 void EditorMediator::OnActivateIme(std::string_view engine_id) {
   editor_switch_->OnActivateIme(engine_id);
-}
-
-void EditorMediator::OnTabletModeStarting() {
-  editor_switch_->OnTabletModeUpdated(/*tablet_mode_enabled=*/true);
-}
-
-void EditorMediator::OnTabletModeEnded() {
-  editor_switch_->OnTabletModeUpdated(/*tablet_mode_enabled=*/false);
-}
-
-void EditorMediator::OnTabletControllerDestroyed() {
-  tablet_mode_observation_.Reset();
 }
 
 void EditorMediator::OnConsentActionReceived(ConsentAction consent_action) {
--- chrome/browser/ash/input_method/editor_switch.h
+++ chrome/browser/ash/input_method/editor_switch.h
@@ -39,8 +39,6 @@ class EditorSwitch {
 
   void OnActivateIme(std::string_view engine_id);
 
-  void OnTabletModeUpdated(bool tablet_mode_enabled);
-
  private:
   raw_ptr<Profile> profile_;
 
@@ -48,7 +46,6 @@ class EditorSwitch {
   std::string active_engine_id_;
   ui::TextInputType input_type_ = ui::TEXT_INPUT_TYPE_NONE;
   ash::AppType app_type_ = ash::AppType::NON_APP;
-  bool tablet_mode_enabled_ = false;
 };
 
 }  // namespace ash::input_method
