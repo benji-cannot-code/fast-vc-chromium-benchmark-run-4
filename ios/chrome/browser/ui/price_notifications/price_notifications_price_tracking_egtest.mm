d2af17a0d4a87d8891685b0692379978a01738e3
Marc Treib <treib@chromium.org>
2023-09-04 15:11:15 +0000

    [iOS] Disable testPriceTrackingDismissButton with SyncToSignin
    The test
    PriceNotificationsPriceTrackingTestCase/testPriceTrackingDismissButton
    currently fails if the feature kReplaceSyncPromosWithSignInPromos is
    enabled. This CL temporarily skips it in that case, pending
    investigation.
    
    Bug: 1462156, 1478755
    Change-Id: I0a549f7ea4567a158ad129531e9287cccce51ce0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4839217
    Commit-Queue: David Roger <droger@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Auto-Submit: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192130}

--- ios/chrome/browser/ui/price_notifications/price_notifications_price_tracking_egtest.mm
+++ ios/chrome/browser/ui/price_notifications/price_notifications_price_tracking_egtest.mm
@@ -39,6 +39,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)testPriceTrackingDismissButton {
+  // TODO(crbug.com/1478755): Investigate why this test fails with
+  // ReplaceSyncWithSignin.
+  if ([ChromeEarlGrey isReplaceSyncWithSigninEnabled]) {
+    EARL_GREY_TEST_SKIPPED(@"crbug.com/1478755: Temporarily disabled.");
+  }
+
   [self signinPriceTrackingUser];
   [self openTrackingPriceUI];
 
