f6b3163ea461b16e5581b1e47dc33b764b612ff7
sisidovski <sisidovski@chromium.org>
2023-09-04 00:55:26 +0000

    Fix CHECK in State:kCompleted
    Fix the crash error caused by unexpected state transitions.
    kCompleted can be transferred from kResponseReceived.
    
    Bug: 1477939, 1478382
    Change-Id: If64ee1cab00cd67d132e02747ace1e399ad9173f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4835496
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191888}

--- content/common/service_worker/race_network_request_url_loader_client.cc
+++ content/common/service_worker/race_network_request_url_loader_client.cc
@@ -578,6 +578,7 @@ void ServiceWorkerRaceNetworkRequestURLLoaderClient::TransitionState(
       break;
     case State::kCompleted:
       CHECK(state_ == State::kWaitForBody ||
+            state_ == State::kResponseReceived ||
             state_ == State::kDataTransferStarted ||
             state_ == State::kResponseCommitted ||
             state_ == State::kDataTransferFinished)
