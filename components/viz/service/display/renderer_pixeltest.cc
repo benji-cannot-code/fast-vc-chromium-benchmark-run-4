305743a976becabe59a039d1ce6a2addd933daf3
Saifuddin Hitawala <hitawala@chromium.org>
2023-09-05 19:55:30 +0000

    Graphite: Rebaseline viz tests with quad size rounding differences
    Add new images for viz pixel tests with quad size mismatch which may
    be due to some size rounding up/down changes with graphite.
    
    Bug: 1442381
    Change-Id: I74d1f0cd71881218c63195b5ed2022a333d97edd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4843969
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192638}

--- components/viz/service/display/renderer_pixeltest.cc
+++ components/viz/service/display/renderer_pixeltest.cc
@@ -78,6 +78,7 @@ const gfx::DisplayColorSpaces kRec601DisplayColorSpaces(
 #if !BUILDFLAG(IS_ANDROID)
 
 constexpr char kANGLEMetalStr[] = "_angle_metal";
+constexpr char kGraphiteStr[] = "_graphite";
 
 bool IsANGLEMetal() {
   return gl::GetGLImplementationParts() ==
@@ -1299,13 +1300,17 @@ TEST_P(RendererPixelTest, BypassableTextureQuad_Rotation_ClipRect) {
     pass_list.push_back(std::move(root_pass));
   }
 
-  EXPECT_TRUE(this->RunPixelTest(
-      &pass_list,
-      base::FilePath(FILE_PATH_LITERAL("bypass_texture_rotated.png")),
-      cc::FuzzyPixelComparator()
-          .SetErrorPixelsPercentageLimit(3.5f)
-          .SetAbsErrorLimit(127)
-          .SetAvgAbsErrorLimit(40)));
+  base::FilePath expected_result =
+      base::FilePath(FILE_PATH_LITERAL("bypass_texture_rotated.png"));
+  if (is_skia_graphite()) {
+    expected_result = expected_result.InsertBeforeExtensionASCII(kGraphiteStr);
+  }
+
+  EXPECT_TRUE(this->RunPixelTest(&pass_list, expected_result,
+                                 cc::FuzzyPixelComparator()
+                                     .SetErrorPixelsPercentageLimit(3.5f)
+                                     .SetAbsErrorLimit(127)
+                                     .SetAvgAbsErrorLimit(40)));
 }
 
 // Tests that exercise render pass bypass code. When the feature is enabled by
@@ -2180,6 +2185,9 @@ TEST_P(IntersectingVideoQuadPixelTest, YUVVideoQuads) {
   base::FilePath baseline = base::FilePath(
       FILE_PATH_LITERAL("intersecting_blue_green_squares_video.png"));
 
+  if (is_skia_graphite()) {
+    baseline = baseline.InsertBeforeExtensionASCII(kGraphiteStr);
+  }
   if (renderer_type() == RendererType::kSkiaGL && IsANGLEMetal()) {
     baseline = baseline.InsertBeforeExtensionASCII(kANGLEMetalStr);
   }
@@ -2217,6 +2225,9 @@ TEST_P(IntersectingVideoQuadPixelTest, Y16VideoQuads) {
   base::FilePath baseline = base::FilePath(
       FILE_PATH_LITERAL("intersecting_light_dark_squares_video.png"));
 
+  if (is_skia_graphite()) {
+    baseline = baseline.InsertBeforeExtensionASCII(kGraphiteStr);
+  }
   if (renderer_type() == RendererType::kSkiaGL && IsANGLEMetal()) {
     baseline = baseline.InsertBeforeExtensionASCII(kANGLEMetalStr);
   }
@@ -3888,10 +3899,13 @@ TEST_P(GPURendererPixelTest, SolidColorDrawQuadForceAntiAliasingOff) {
   AggregatedRenderPassList pass_list;
   pass_list.push_back(std::move(pass));
 
-  EXPECT_TRUE(this->RunPixelTest(
-      &pass_list,
-      base::FilePath(FILE_PATH_LITERAL("force_anti_aliasing_off.png")),
-      cc::AlphaDiscardingExactPixelComparator()));
+  base::FilePath expected_result =
+      base::FilePath(FILE_PATH_LITERAL("force_anti_aliasing_off.png"));
+  if (is_skia_graphite()) {
+    expected_result = expected_result.InsertBeforeExtensionASCII(kGraphiteStr);
+  }
+  EXPECT_TRUE(this->RunPixelTest(&pass_list, expected_result,
+                                 cc::AlphaDiscardingExactPixelComparator()));
 }
 
 // This test tests that forcing anti-aliasing off works as expected for
@@ -3947,10 +3961,13 @@ TEST_P(GPURendererPixelTest, RenderPassDrawQuadForceAntiAliasingOff) {
   pass_list.push_back(std::move(child_pass));
   pass_list.push_back(std::move(root_pass));
 
-  EXPECT_TRUE(this->RunPixelTest(
-      &pass_list,
-      base::FilePath(FILE_PATH_LITERAL("force_anti_aliasing_off.png")),
-      cc::AlphaDiscardingExactPixelComparator()));
+  base::FilePath expected_result =
+      base::FilePath(FILE_PATH_LITERAL("force_anti_aliasing_off.png"));
+  if (is_skia_graphite()) {
+    expected_result = expected_result.InsertBeforeExtensionASCII(kGraphiteStr);
+  }
+  EXPECT_TRUE(this->RunPixelTest(&pass_list, expected_result,
+                                 cc::AlphaDiscardingExactPixelComparator()));
 }
 
 // This test tests that forcing anti-aliasing off works as expected for
@@ -4016,10 +4033,13 @@ TEST_P(GPURendererPixelTest, TileDrawQuadForceAntiAliasingOff) {
   AggregatedRenderPassList pass_list;
   pass_list.push_back(std::move(pass));
 
-  EXPECT_TRUE(this->RunPixelTest(
-      &pass_list,
-      base::FilePath(FILE_PATH_LITERAL("force_anti_aliasing_off.png")),
-      cc::AlphaDiscardingExactPixelComparator()));
+  base::FilePath expected_result =
+      base::FilePath(FILE_PATH_LITERAL("force_anti_aliasing_off.png"));
+  if (is_skia_graphite()) {
+    expected_result = expected_result.InsertBeforeExtensionASCII(kGraphiteStr);
+  }
+  EXPECT_TRUE(this->RunPixelTest(&pass_list, expected_result,
+                                 cc::AlphaDiscardingExactPixelComparator()));
 }
 
 // This test tests that forcing anti-aliasing off works as expected while
@@ -4046,10 +4066,13 @@ TEST_P(GPURendererPixelTest, BlendingWithoutAntiAliasing) {
   AggregatedRenderPassList pass_list;
   pass_list.push_back(std::move(pass));
 
-  EXPECT_TRUE(this->RunPixelTest(
-      &pass_list,
-      base::FilePath(FILE_PATH_LITERAL("translucent_quads_no_aa.png")),
-      cc::AlphaDiscardingExactPixelComparator()));
+  base::FilePath expected_result =
+      base::FilePath(FILE_PATH_LITERAL("translucent_quads_no_aa.png"));
+  if (is_skia_graphite()) {
+    expected_result = expected_result.InsertBeforeExtensionASCII(kGraphiteStr);
+  }
+  EXPECT_TRUE(this->RunPixelTest(&pass_list, expected_result,
+                                 cc::AlphaDiscardingExactPixelComparator()));
 }
 
 TEST_P(GPURendererPixelTest, TrilinearFiltering) {
@@ -5904,7 +5927,7 @@ TEST_P(DelegatedInkTest, DrawTrailWithPredictionDisabled) {
   base::FilePath expected_result = base::FilePath(
       FILE_PATH_LITERAL("delegated_ink_trail_no_prediction.png"));
   if (is_skia_graphite()) {
-    expected_result = expected_result.InsertBeforeExtensionASCII("_graphite");
+    expected_result = expected_result.InsertBeforeExtensionASCII(kGraphiteStr);
   }
 
   // Confirm that the trail was drawn without prediction.
@@ -5960,7 +5983,7 @@ TEST_P(DelegatedInkWithPredictionTest, DrawOneTrailAndErase) {
   base::FilePath expected_result =
       base::FilePath(FILE_PATH_LITERAL("delegated_ink_one_trail.png"));
   if (is_skia_graphite()) {
-    expected_result = expected_result.InsertBeforeExtensionASCII("_graphite");
+    expected_result = expected_result.InsertBeforeExtensionASCII(kGraphiteStr);
   }
   EXPECT_TRUE(DrawAndTestTrail(expected_result));
 
@@ -5987,7 +6010,7 @@ TEST_P(DelegatedInkWithPredictionTest, DrawTwoTrailsAndErase) {
   base::FilePath expected_result =
       base::FilePath(FILE_PATH_LITERAL("delegated_ink_two_trails_first.png"));
   if (is_skia_graphite()) {
-    expected_result = expected_result.InsertBeforeExtensionASCII("_graphite");
+    expected_result = expected_result.InsertBeforeExtensionASCII(kGraphiteStr);
   }
   EXPECT_TRUE(DrawAndTestTrail(expected_result));
 
@@ -6003,7 +6026,7 @@ TEST_P(DelegatedInkWithPredictionTest, DrawTwoTrailsAndErase) {
       base::FilePath(FILE_PATH_LITERAL("delegated_ink_two_trails_second.png"));
   if (is_skia_graphite()) {
     expected_result_second =
-        expected_result_second.InsertBeforeExtensionASCII("_graphite");
+        expected_result_second.InsertBeforeExtensionASCII(kGraphiteStr);
   }
   EXPECT_TRUE(DrawAndTestTrail(expected_result_second));
 
@@ -6035,7 +6058,7 @@ TEST_P(DelegatedInkWithPredictionTest, TrailExtendsBeyondPresentationArea) {
   base::FilePath expected_result = base::FilePath(FILE_PATH_LITERAL(
       "delegated_ink_trail_clipped_by_presentation_area.png"));
   if (is_skia_graphite()) {
-    expected_result = expected_result.InsertBeforeExtensionASCII("_graphite");
+    expected_result = expected_result.InsertBeforeExtensionASCII(kGraphiteStr);
   }
   EXPECT_TRUE(DrawAndTestTrail(expected_result));
 }
@@ -6079,7 +6102,7 @@ TEST_P(DelegatedInkWithPredictionTest, DelegatedInkTrailAfterBatchedQuads) {
   base::FilePath expected_result = base::FilePath(
       FILE_PATH_LITERAL("delegated_ink_trail_on_batched_quads.png"));
   if (is_skia_graphite()) {
-    expected_result = expected_result.InsertBeforeExtensionASCII("_graphite");
+    expected_result = expected_result.InsertBeforeExtensionASCII(kGraphiteStr);
   }
 
   EXPECT_TRUE(
@@ -6174,7 +6197,7 @@ TEST_P(DelegatedInkWithPredictionTest, DrawTrailsWithDifferentPointerIds) {
   base::FilePath expected_result =
       base::FilePath(FILE_PATH_LITERAL("delegated_ink_pointer_id_1.png"));
   if (is_skia_graphite()) {
-    expected_result = expected_result.InsertBeforeExtensionASCII("_graphite");
+    expected_result = expected_result.InsertBeforeExtensionASCII(kGraphiteStr);
   }
   EXPECT_TRUE(DrawAndTestTrail(expected_result));
 
@@ -6186,7 +6209,7 @@ TEST_P(DelegatedInkWithPredictionTest, DrawTrailsWithDifferentPointerIds) {
       base::FilePath(FILE_PATH_LITERAL("delegated_ink_pointer_id_2.png"));
   if (is_skia_graphite()) {
     expected_result_second =
-        expected_result_second.InsertBeforeExtensionASCII("_graphite");
+        expected_result_second.InsertBeforeExtensionASCII(kGraphiteStr);
   }
   EXPECT_TRUE(DrawAndTestTrail(expected_result_second));
 
@@ -6216,7 +6239,7 @@ TEST_P(DelegatedInkWithPredictionTest,
   base::FilePath expected_result =
       base::FilePath(FILE_PATH_LITERAL("delegated_ink_one_trail.png"));
   if (is_skia_graphite()) {
-    expected_result = expected_result.InsertBeforeExtensionASCII("_graphite");
+    expected_result = expected_result.InsertBeforeExtensionASCII(kGraphiteStr);
   }
   EXPECT_TRUE(DrawAndTestTrail(expected_result));
 
