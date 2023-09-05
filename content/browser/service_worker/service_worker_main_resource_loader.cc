d8dcb6e0778f0f3c093447380c68a82384cb9aed
sisidovski <sisidovski@chromium.org>
2023-09-05 08:35:27 +0000

    Introduce blocked_hosts param to ServiceWorkerAutoPreload
    This CL add the new feature param to ServiceWorkerAutoPreload.
    "blocked_hosts" could have a comma separated string containing host
    names, in which the AutoPreload feature is disabled.
    
    This mechanism is needed when we'd like to disable the feature on some
    specific websites while running the experiment.
    
    Bug: 1472634
    Change-Id: I2dab085685ca537d3c09045baca255ab43a12e2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4841140
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192325}

--- content/browser/service_worker/service_worker_main_resource_loader.cc
+++ content/browser/service_worker/service_worker_main_resource_loader.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
+#include "base/containers/flat_set.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
 #include "base/strings/strcat.h"
+#include "base/strings/string_split.h"
 #include "base/time/time.h"
 #include "base/trace_event/common/trace_event_common.h"
 #include "base/trace_event/trace_event.h"
@@ -404,6 +406,17 @@ bool ServiceWorkerMainResourceLoader::MaybeStartAutoPreload(
       kServiceWorkerAutoPreload, "use_allowlist",
       /*default_value=*/false);
   if (use_allowlist && !HasRaceNetworkRequestEligibleScript(version)) {
+    return false;
+  }
+
+  // Hosts to disable AutoPreload feature. This mechanism is needed to address
+  // the case when the AutoPreload behavior is problematic for some websites and
+  // those should be opted out from the feature.
+  const static base::NoDestructor<base::flat_set<std::string>> blocked_hosts(
+      base::SplitString(base::GetFieldTrialParamValueByFeature(
+                            kServiceWorkerAutoPreload, "blocked_hosts"),
+                        ",", base::TRIM_WHITESPACE, base::SPLIT_WANT_NONEMPTY));
+  if (blocked_hosts->contains(resource_request_.url.host())) {
     return false;
   }
 
