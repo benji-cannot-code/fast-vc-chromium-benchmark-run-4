6af9c400b571d7eea3c33f1b7150be4f594983a2
Rushan Suleymanov <rushans@google.com>
2023-09-05 13:48:53 +0000

    Issue a tombstone after incoming password invitation is processed
    Bug: 1445868
    Change-Id: Ibd79cf549f82a54d6e377513a152f1a898b079cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4841640
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192422}

--- components/password_manager/core/browser/sharing/incoming_password_sharing_invitation_sync_bridge_unittest.cc
+++ components/password_manager/core/browser/sharing/incoming_password_sharing_invitation_sync_bridge_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using syncer::HasInitialSyncDone;
 using syncer::IsEmptyMetadataBatch;
 using syncer::MetadataBatchContains;
+using testing::_;
 using testing::InSequence;
 using testing::Invoke;
 using testing::IsEmpty;
@@ -180,6 +181,9 @@ TEST_F(IncomingPasswordSharingInvitationSyncBridgeTest,
       bridge()->CreateMetadataChangeList();
   syncer::EntityChangeList entity_changes;
   entity_changes.push_back(EntityChangeFromSpecifics(MakeSpecifics()));
+
+  EXPECT_CALL(*mock_processor(),
+              Delete(entity_changes.front()->storage_key(), _));
   bridge()->ApplyIncrementalSyncChanges(std::move(metadata_changes),
                                         std::move(entity_changes));
 
@@ -216,6 +220,9 @@ TEST_F(IncomingPasswordSharingInvitationSyncBridgeTest,
       bridge()->CreateMetadataChangeList();
   syncer::EntityChangeList entity_changes;
   entity_changes.push_back(EntityChangeFromSpecifics(MakeSpecifics()));
+
+  EXPECT_CALL(*mock_processor(),
+              Delete(entity_changes.front()->storage_key(), _));
   bridge()->MergeFullSyncData(std::move(metadata_changes),
                               std::move(entity_changes));
 
