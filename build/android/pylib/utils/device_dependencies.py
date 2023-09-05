62be9c78089c51549b16968346129b4eafe5bd24
Andrew Grieve <agrieve@chromium.org>
2023-09-05 18:12:29 +0000

    Android: Do not push devtools-frontend to device for tests
    There was already an exclude for this, but it was no longer matching due
    to paths changing.
    
    Bug: 1478271
    Change-Id: Id58239214d8cc7b654babd7d2579e38dbe0d9e23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4843608
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192574}

--- build/android/pylib/utils/device_dependencies.py
+++ build/android/pylib/utils/device_dependencies.py
@@ -49,7 +49,7 @@ _EXCLUSIONS = [
     re.compile(r'.*third_party/proguard/.*'),
 
     # Our tests don't need these.
-    re.compile(r'.*/devtools-frontend/src/front_end/.*'),
+    re.compile(r'.*/devtools-frontend/.*front_end/.*'),
 
     # Build artifacts:
     re.compile(r'.*\.stamp'),
