f1340030ad8093e494fbdaf4424c307995cee237
Morten Stenshorne <mstensho@chromium.org>
2023-09-05 17:40:49 +0000

    [RemoveLegacy] Remove legacy float pre-paint code.
    Bug: 1229581
    Change-Id: I73b4d2c987a69595814b3077b7eae9cbc20cee0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4839203
    Auto-Submit: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192556}

--- third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
+++ third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
@@ -2707,9 +2707,6 @@ void FragmentPaintPropertyTreeBuilder::UpdateOutOfFlowContext() {
   if (!object_.IsBoxModelObject() && !properties_)
     return;
 
-  if (object_.IsLayoutBlock())
-    context_.paint_offset_for_float = context_.current.paint_offset;
-
   if (object_.CanContainAbsolutePositionObjects())
     context_.absolute_position = context_.current;
 
@@ -2806,11 +2803,6 @@ void FragmentPaintPropertyTreeBuilder::UpdateClipIsolationNode() {
 }
 
 void FragmentPaintPropertyTreeBuilder::UpdatePaintOffset() {
-  if (!pre_paint_info_) {
-    if (object_.IsFloating() && !object_.IsInLayoutNGInlineFormattingContext())
-      context_.current.paint_offset = context_.paint_offset_for_float;
-  }
-
   if (object_.IsBoxModelObject()) {
     const auto& box_model_object = To<LayoutBoxModelObject>(object_);
     switch (box_model_object.StyleRef().GetPosition()) {
