4e47f09a7c0c5844b327277be1255c2730133bc1
Dominic Farolino <dom@chromium.org>
2023-09-05 14:59:39 +0000

    Blink: Fix lazyload image memory leak
    This CL implements a simple fix for the lazy load memory leak described
    in https://crbug.com/1213045.
    
    This CL is intended to supersede prior attempts at fixing this memory
    leak that are overly complicated. The history of attempts at this issue
    are described in: https://docs.google.com/document/d/1B7EFhH846bvYWCW4Q2TLzkC2RpZIGozYRQOP6T3MT-w/edit, and the prior drafted CL attempts superseded by this CL
    are:
    
     - https://crrev.com/c/3349145
     - https://crrev.com/c/3350983
    
    Bug: 1213045
    Change-Id: I1377adbb79d8c9d8bb1fd8b8278d32cb13c591fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4807772
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192459}

--- third_party/blink/renderer/core/html/lazy_load_image_observer_test.cc
+++ third_party/blink/renderer/core/html/lazy_load_image_observer_test.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/testing/sim/sim_compositor.h"
 #include "third_party/blink/renderer/core/testing/sim/sim_request.h"
 #include "third_party/blink/renderer/core/testing/sim/sim_test.h"
+#include "third_party/blink/renderer/platform/heap/thread_state.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_fetcher.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_request.h"
@@ -1022,6 +1023,38 @@ TEST_F(LazyLoadImagesTest, LazyLoadFileUrls) {
   test::RunPendingTasks();
 
   EXPECT_TRUE(lazy->CachedImage()->IsLoading());
+}
+
+// This is a regression test added for https://crbug.com/1213045, which was
+// filed for a memory leak whereby lazy loaded images currently being deferred
+// but that were removed from the DOM were never actually garbage collected.
+TEST_F(LazyLoadImagesTest, GarbageCollectDeferredLazyLoadImages) {
+  SimRequest main_resource("https://example.com/", "text/html");
+  LoadURL("https://example.com/");
+  main_resource.Complete(String::Format(
+      R"HTML(
+        <body>
+        <div style='height: %dpx;'></div>
+        <img src='https://example.com/image.png' loading='lazy'>
+        </body>)HTML",
+      kViewportHeight + kLoadingDistanceThreshold + 100));
+
+  Compositor().BeginFrame();
+  test::RunPendingTasks();
+
+  WeakPersistent<HTMLImageElement> image =
+      To<HTMLImageElement>(GetDocument().QuerySelector(AtomicString("img")));
+  EXPECT_FALSE(image->complete());
+  image->remove();
+  EXPECT_FALSE(image->isConnected());
+  EXPECT_FALSE(image->complete());
+  EXPECT_NE(image, nullptr);
+
+  GetDocument().View()->UpdateAllLifecyclePhasesForTest();
+  test::RunPendingTasks();
+  ThreadState::Current()->CollectAllGarbageForTesting();
+
+  EXPECT_EQ(nullptr, image);
 }
 
 class DelayOutOfViewportLazyImagesTest : public SimTest {
