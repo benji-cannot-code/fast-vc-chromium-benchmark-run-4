14d7a32f6f8c4fa68a5054c35c3143a046bb865e
Dominik Röttsches <drott@chromium.org>
2023-09-05 14:04:45 +0000

    [Fontations-backend] Roll Fontations libraries
    Roll skrifa to 0.10, read_fonts to 0.11 and font_types to 0.4.0.
    
    Simplify Skia BUILD.gn.
    
    Bug: 1446251
    Change-Id: I58e1535251ef73e8c383f32536fd9dcd341ee0e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4836126
    Reviewed-by: Kevin Lubick <kjlubick@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192426}

---
+++ third_party/rust/skrifa/v0_10/crate/src/scale/cff/mod.rs
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+//! Support for scaling CFF outlines.
+
+mod hint;
+mod scaler;
+
+pub(crate) use scaler::{Scaler, Subfont};
