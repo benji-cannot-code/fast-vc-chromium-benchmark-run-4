5acbf861209239ececb3d1d92e3dfd663f7d5c6d
Alexis Hetu <sugoi@chromium.org>
2023-09-05 22:40:49 +0000

    [iOS] Make sure the suggestions provider is set before using it
    The Payments Bottom Sheet does not require suggestions from the
    suggestions provider, as it gets its list of suggested credit
    cards from the personal data manager instead. Unfortunately, it
    still requires the suggestions provider to be properly set before
    using it to perform the suggestion selection.
    
    This CL adds code to the Payments Bottom Sheet's Mediator's
    initializer to make sure the provider is properly set, which
    happens within "onSuggestionsReady:provider:", which happens
    as a result of querying suggestions. Apparently, this doesn't
    always automatically happen on form load.
    
    Bug: 1475358, 1476212
    Change-Id: Ib7d3aec31cb522bd6305e0ff5f3d958e6ec5de0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4817613
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192735}

--- ios/chrome/browser/ui/autofill/bottom_sheet/payments_suggestion_bottom_sheet_mediator.mm
+++ ios/chrome/browser/ui/autofill/bottom_sheet/payments_suggestion_bottom_sheet_mediator.mm
@@ -212,6 +212,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         base::ScopedObservation<WebStateList, WebStateListObserverBridge>>(
         _webStateListObserver.get());
     _webStateListObservation->Observe(_webStateList);
+
+    [self setupSuggestionsProvider];
   }
   return self;
 }
@@ -370,6 +372,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)onWebStateChange {
   [self.consumer dismiss];
+}
+
+// Make sure the suggestions provider is properly set up. We need to make sure
+// that FormSuggestionController's "_provider" member is set, which happens
+// within [FormSuggestionController onSuggestionsReady:provider:], before the
+// credit card suggestion is selected.
+// TODO(crbug.com/1479175): Remove this dependency on suggestions.
+- (void)setupSuggestionsProvider {
+  web::WebState* activeWebState = _webStateList->GetActiveWebState();
+  if (!activeWebState) {
+    return;
+  }
+
+  FormSuggestionTabHelper* tabHelper =
+      FormSuggestionTabHelper::FromWebState(activeWebState);
+  if (!tabHelper) {
+    return;
+  }
+
+  id<FormInputSuggestionsProvider> provider =
+      tabHelper->GetAccessoryViewProvider();
+  [provider retrieveSuggestionsForForm:_params
+                              webState:activeWebState
+              accessoryViewUpdateBlock:nil];
 }
 
 // Returns the icon associated with the provided credit card.
