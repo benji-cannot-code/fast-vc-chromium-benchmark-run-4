5f22ccbef0203be6f214e668bc955e7ea256b15e
ssid <ssid@chromium.org>
2023-09-05 21:46:57 +0000

    [segmentation] Deprecate GetCachedResultForClient
    Cache need not know about post processing. The client can decide how to
    post process the result. Remove this method and one use case.
    
    Bug: 1443061
    Change-Id: I22c240446ddd277bb318d6be9d99e77dd5b27aa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4790931
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Auto-Submit: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192707}

--- components/segmentation_platform/internal/post_processor/post_processing_test_utils.h
+++ components/segmentation_platform/internal/post_processor/post_processing_test_utils.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/segmentation_platform/internal/proto/client_results.pb.h"
 #include "components/segmentation_platform/public/config.h"
+#include "components/segmentation_platform/public/proto/output_config.pb.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace segmentation_platform::test_utils {
@@ -18,6 +19,8 @@ proto::OutputConfig GetTestOutputConfigForBinnedClassifier();
 proto::OutputConfig GetTestOutputConfigForMultiClassClassifier(
     int top_k_outputs,
     absl::optional<float> threshold);
+proto::OutputConfig GetTestOutputConfigForGenericPredictor(
+    const std::vector<std::string>& labels);
 
 std::unique_ptr<Config> CreateTestConfig();
 std::unique_ptr<Config> CreateTestConfig(const std::string& client_key,
