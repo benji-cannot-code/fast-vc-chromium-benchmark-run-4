4075696141985a36d98fabff84b8e72ad1bba8b5
Kent Tamura <tkent@chromium.org>
2023-09-05 06:37:02 +0000

    Layout: Rename layout_ng_ruby_run*.* to layout_ruby_column*.*
    This CL has no behavior changes.
    
    Bug: 1477130
    Change-Id: I732c0564b4a3e7c1c9e736c9edbef728daa7f5d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4841117
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192289}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_items_builder_test.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_items_builder_test.cc
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/core/css/resolver/style_resolver.h"
 #include "third_party/blink/renderer/core/layout/layout_inline.h"
+#include "third_party/blink/renderer/core/layout/layout_ruby_column.h"
 #include "third_party/blink/renderer/core/layout/layout_text.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_node_data.h"
-#include "third_party/blink/renderer/core/layout/ng/layout_ng_ruby_run.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/core/testing/core_unit_test_helper.h"
 
--- third_party/blink/renderer/core/layout/ng/layout_ng_ruby_as_block.cc
+++ third_party/blink/renderer/core/layout/ng/layout_ng_ruby_as_block.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/core/layout/layout_ruby.h"
-#include "third_party/blink/renderer/core/layout/ng/layout_ng_ruby_run.h"
+#include "third_party/blink/renderer/core/layout/layout_ruby_column.h"
 
 namespace blink {
 
