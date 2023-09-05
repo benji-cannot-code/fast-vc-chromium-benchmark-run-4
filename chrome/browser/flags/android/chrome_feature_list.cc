2e8310c311496a01abf8b179d0e9792da38157be
Noureldien Refae <noureldien@google.com>
2023-09-05 10:52:25 +0000

    Privacy Guide: Add feature for Privacy Guide Preload Clank
    Add a feature for clank Privacy Guide Preload
    
    Bug: 1215630
    Change-Id: Ie2232abc6dd8d9fc51da9b7d45dea996252f0e95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4839204
    Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    Commit-Queue: Nour Eldien Ayman <noureldien@google.com>
    Cr-Commit-Position: refs/heads/main@{#1192366}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -119,6 +119,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kElasticOverscroll,
     &features::kPrivacyGuideAndroid,
     &features::kPrivacyGuideAndroid3,
+    &features::kPrivacyGuidePreloadAndroid,
     &features::kPrivacyGuideAndroidPostMVP,
     &features::kPushMessagingDisallowSenderIDs,
     &features::kPwaUpdateDialogForIcon,
