6f2057e9fe48732c1655ea39ffbf748c53b7ab8f
Ana Salazar <anasalazar@chromium.org>
2023-09-05 20:10:40 +0000

    Revert "Show an error message on fetch error for classroom glanceables"
    This reverts commit a29a268694ee192bf45d4e030cb0fc4f631e21a0.
    
    Reason for revert: Wait for after branch to land
    
    Original change's description:
    > Show an error message on fetch error for classroom glanceables
    >
    > If an error occurs while the glanceables classroom client attempts to
    > update the data on the bubble, show an error message 'Failed to reload
    > the data'
    >
    > Bug: b:293640451
    > Change-Id: I201cd38a34b8ad3804dba609a77b207da70ad976
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4750560
    > Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    > Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1192648}
    
    Bug: b:293640451
    Change-Id: Ic4190b49b96081a2f8739ec3b376f2525539af6d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4844417
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192650}

--- ash/glanceables/common/glanceables_view_id.h
+++ ash/glanceables/common/glanceables_view_id.h
@@ -49,7 +49,6 @@ enum class GlanceablesViewId {
   // `GlanceablesErrorMessageView`
   kGlanceablesErrorMessageLabel,
   kGlanceablesErrorMessageButton,
-  kGlanceablesErrorMessageView,
 };
 
 }  // namespace ash
