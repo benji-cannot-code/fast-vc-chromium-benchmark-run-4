e13afa88e0747b6482502c6c8b6edd582b082f10
Renato Silva <rrsilva@google.com>
2023-09-05 15:56:46 +0000

    [QuickStart] Observe screen changes
    Make QuickStartController an observer of the OobeUI. This will be
    used for reacting to screen transitions.
    
    Bug: b:298609218
    Change-Id: I3de33e4e3ecd865d19d2a803443c52431adfc0d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4835919
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1192494}

--- chrome/browser/ash/login/quickstart_controller.h
+++ chrome/browser/ash/login/quickstart_controller.h
@@ -7,13 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_ASH_LOGIN_QUICKSTART_CONTROLLER_H_
 
 #include "base/memory/weak_ptr.h"
+#include "base/scoped_observation.h"
 #include "chrome/browser/ash/login/oobe_quick_start/connectivity/target_device_connection_broker.h"
 #include "chrome/browser/ash/login/oobe_quick_start/target_device_bootstrap_controller.h"
+#include "chrome/browser/ash/login/oobe_screen.h"
+#include "chrome/browser/ui/webui/ash/login/oobe_ui.h"
 
 namespace ash {
 
 // Main orchestrator of the QuickStart flow in OOBE
-class QuickStartController {
+class QuickStartController : public OobeUI::Observer {
  public:
   using EntryPointButtonVisibilityCallback = base::OnceCallback<void(bool)>;
 
@@ -22,7 +25,7 @@ class QuickStartController {
   QuickStartController(const QuickStartController&) = delete;
   QuickStartController& operator=(const QuickStartController&) = delete;
 
-  ~QuickStartController();
+  ~QuickStartController() override;
 
   // Enable QuickStart even when the feature isn't enabled. This is only called
   // when enabling via the keyboard shortcut Ctrl+Alt+Q on the Welcome screen.
@@ -33,6 +36,8 @@ class QuickStartController {
   void IsSupported(EntryPointButtonVisibilityCallback callback);
 
  private:
+  void InitTargetDeviceBootstrapController();
+
   // TODO(b:298609218) Improve the way the QuickStartScreen accesses this.
   friend class QuickStartScreen;
   quick_start::TargetDeviceBootstrapController* bootstrap_controller() {
@@ -42,6 +47,14 @@ class QuickStartController {
   void OnGetQuickStartFeatureSupportStatus(
       EntryPointButtonVisibilityCallback callback,
       quick_start::TargetDeviceConnectionBroker::FeatureSupportStatus status);
+
+  // OobeUI::Observer:
+  void OnCurrentScreenChanged(OobeScreenId previous_screen,
+                              OobeScreenId current_screen) override;
+  void OnDestroyingOobeUI() override;
+
+  // Activates the OobeUI::Observer
+  void StartObservingScreenTransitions();
 
   // Whether QuickStart is supported in this system. Can only be determined
   // by explicitly probing TargetDeviceBootstrapController.
@@ -53,6 +66,10 @@ class QuickStartController {
   base::WeakPtr<quick_start::TargetDeviceBootstrapController>
       bootstrap_controller_;
 
+  // Source of truth of OOBE's current state via OobeUI::Observer
+  absl::optional<OobeScreenId> current_screen_, previous_screen_;
+
+  base::ScopedObservation<OobeUI, OobeUI::Observer> observation_{this};
   base::WeakPtrFactory<QuickStartController> weak_ptr_factory_{this};
 };
 
