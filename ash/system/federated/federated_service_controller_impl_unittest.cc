f9fe9d19a91a93b18289e90d34c27df01f2f5219
Amanda Deacon <amandadeacon@chromium.org>
2023-09-05 05:07:31 +0000

    [Federated strings] Add skeleton of FederatedClientManager.
    In this CL:
    
    - Add a skeleton (.h and .cc) of new class FederatedClientManager.
    - Add a skeleton unit test file.
    - Instantiate the client manager at least once in a test.
    
    The client manager abstracts away the details of communicating with
    the FederatedServiceController and setting up a Mojo connection to the
    FederatedService.
    
    The manager class has a public method specific to clients of the
    federated strings service. However, the manager class can easily be
    extended in the future to serve generic clients or other types of
    service.
    
    For follow-up:
    
    - (short-term) Fill in the implementation.
    - (long-term) (clients) Instantiate in ash-chrome.
    
    Test: unit test outline added, `autoninja -C out/Default ash_unittests && ./out/Default/ash_unittests --gtest_filter="*FederatedClientManager*"
    Bug: b/289140140
    Change-Id: Ifa84628ad6573f38e65dfc64998a4308cf48929d
    Low-Coverage-Reason: Not implemented yet.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4686890
    Commit-Queue: Amanda Deacon <amandadeacon@chromium.org>
    Reviewed-by: Xinglong Luan <alanlxl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192267}

--- ash/system/federated/federated_service_controller_impl_unittest.cc
+++ ash/system/federated/federated_service_controller_impl_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test/ash_test_base.h"
 #include "base/memory/raw_ptr.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/test/task_environment.h"
 #include "chromeos/ash/services/federated/public/cpp/fake_service_connection.h"
 #include "chromeos/ash/services/federated/public/cpp/service_connection.h"
 #include "testing/gtest/include/gtest/gtest.h"
