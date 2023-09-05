2b98b19734d58f0dc212e538b21b5ae9888dbc1e
Hao Liu <haoliuk@chromium.org>
2023-09-05 14:58:37 +0000

    Fix UKM recording for last soft nav metric
    At the end of page load when we record PageLoad event metrics, we also
    record the last soft navigation event metrics. But we didn't check if
    any soft navigations ever happens.
    
    This CL is to fix this overlooked bug by adding a check. The soft
    navigation count should be larger than 0 and the navigation id should
    be non empty if any soft navigation happens.
    
    Bug: 1472878
    Change-Id: Iccdd4faed6f5dbceb59b5144cae38787b74603ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4833444
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Hao Liu <haoliuk@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192458}

--- chrome/browser/page_load_metrics/integration_tests/metric_integration_test.cc
+++ chrome/browser/page_load_metrics/integration_tests/metric_integration_test.cc
@@ -179,6 +179,12 @@ void MetricIntegrationTest::
   }
 }
 
+void MetricIntegrationTest::ExpectUkmEventNotRecorded(
+    base::StringPiece event_name) {
+  auto merged_entries = ukm_recorder().GetMergedEntriesByName(event_name);
+  EXPECT_EQ(merged_entries.size(), 0u);
+}
+
 void MetricIntegrationTest::ExpectUKMPageLoadMetricGreaterThan(
     base::StringPiece metric_name,
     int64_t expected_value) {
