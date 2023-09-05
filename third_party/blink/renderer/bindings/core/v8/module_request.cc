5a595d75229d0ca6122e62dbfd82244754709a9f
Nicolò Ribaudo <nribaudo@igalia.com>
2023-09-05 14:06:32 +0000

    [import-attributes] Throw on invalid attributes keys
    In the past six months, the old import assertions proposal has been
    renamed to "import attributes" with the follwing major changes:
    1. the keyword is now `with` instead of `assert`
    2. unknown assertions cause an error rather than being ignored
    
    (1) has been implemented in V8 at [v8-patch], under the
    `--harmony-import-attributes` flag. This patch implements (2), that due
    to how module loading is layered must be implemented in Chromium.
    
    Implementing this change uncovered a bogous assertion in the HTML spec:
    this patch implements the fix proposed at [html-assertion-fix], that
    adds a check for invalid attributes in the "create a JavaScript module
    script" algorithm.
    
    WPT have been merged at [wpt], and tests for the additional HTML PR are
    being reviewed at [wpt2]. This patch passes all the tests in the
    import-assertions and import-attributes folders.
    
    [v8-patch]: https://chromium.googlesource.com/v8/v8/+/a0fd3209dda8527d7da810abe231df27fffe789e
    [html-assertion-fix]: https://github.com/whatwg/html/pull/9599
    [wpt]: https://github.com/web-platform-tests/wpt/pull/41156
    [wpt2]: https://github.com/web-platform-tests/wpt/pull/41581
    
    Bug: v8:13856
    Change-Id: I702985feb17af1c2fefee231be0dac66b3e6cc98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4738316
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192427}

--- third_party/blink/renderer/bindings/core/v8/module_request.cc
+++ third_party/blink/renderer/bindings/core/v8/module_request.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "third_party/blink/renderer/bindings/core/v8/module_request.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 
@@ -15,6 +16,20 @@ String ModuleRequest::GetModuleTypeString() const {
     }
   }
   return String();
+}
+
+bool ModuleRequest::HasInvalidImportAttributeKey(String* invalid_key) const {
+  if (!RuntimeEnabledFeatures::ImportAttributesDisallowUnknownKeysEnabled()) {
+    return false;
+  }
+
+  for (const ImportAssertion& attr : import_assertions) {
+    if (attr.key != "type") {
+      *invalid_key = attr.key;
+      return true;
+    }
+  }
+  return false;
 }
 
 }  // namespace blink
