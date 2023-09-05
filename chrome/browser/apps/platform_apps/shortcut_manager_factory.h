428ac05d3b326bc3cbba87bd3868e1e21a237902
Arthur Milchior <arthurmilchior@chromium.org>
2023-09-05 18:43:37 +0000

    Implements the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one being deprecated,
    this CL replaces it by the first one.
    
    This also ensures that, instead of applying `WrapUnique` to a `new` raw
    ptr, it directly uses `make_unique`.
    
    Change-Id: I341e69875dc2c15b216a2ec8f6d7ff4db810dbc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4799790
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192593}

--- chrome/browser/apps/platform_apps/shortcut_manager_factory.h
+++ chrome/browser/apps/platform_apps/shortcut_manager_factory.h
@@ -34,7 +34,7 @@ class AppShortcutManagerFactory : public ProfileKeyedServiceFactory {
   ~AppShortcutManagerFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
   bool ServiceIsCreatedWithBrowserContext() const override;
   bool ServiceIsNULLWhileTesting() const override;
