cca09fd3b61f4d6d5f4bd209bc3c13fae56cd33c
Aashna Sheth <aashnas@google.com>
2023-09-05 15:42:16 +0000

    [PageZoom] Text Size Contrast Seekbar Update
    This CL applies the new text-size-contrast-factor web preference to
    the text contrast seekbar view.
    
    Bug: 1459631, b/265491863
    
    Low-Coverage-Reason: In beginning phase of new seekbar. Adding small
    update to how seekbar status is persisted. New tests (similar to those
    in PageZoomMediatorUnitTest.java) for this seekbar will be added when
    seekbar functionality is finalized.
    
    Change-Id: I5baa886a70df5f5a928d1d8e1ede9bfbd28b5f27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4822136
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Aashna Sheth <aashnas@google.com>
    Reviewed-by: Jocelyn Tran <jocelyntran@google.com>
    Cr-Commit-Position: refs/heads/main@{#1192484}

--- components/browser_ui/accessibility/android/java/src/org/chromium/components/browser_ui/accessibility/PageZoomPreference.java
+++ components/browser_ui/accessibility/android/java/src/org/chromium/components/browser_ui/accessibility/PageZoomPreference.java
@@ -21,6 +21,8 @@ import androidx.annotation.Nullable;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceViewHolder;
 
+import org.chromium.components.user_prefs.UserPrefs;
+import org.chromium.content_public.browser.BrowserContextHandle;
 import org.chromium.content_public.browser.ContentFeatureList;
 import org.chromium.content_public.browser.ContentFeatureMap;
 import org.chromium.ui.widget.ChromeImageButton;
@@ -30,6 +32,7 @@ import org.chromium.ui.widget.ChromeImageButton;
  */
 public class PageZoomPreference extends Preference implements SeekBar.OnSeekBarChangeListener {
     private int mInitialValue;
+    private BrowserContextHandle mBrowserContextHandle;
     private SeekBar mSeekBar;
     private ChromeImageButton mDecreaseButton;
     private ChromeImageButton mIncreaseButton;
@@ -61,6 +64,10 @@ public class PageZoomPreference extends Preference implements SeekBar.OnSeekBarC
         super(context, attrs);
 
         setLayoutResource(R.layout.page_zoom_preference);
+    }
+
+    public void setmBrowserContextHandle(BrowserContextHandle browserContextHandle) {
+        mBrowserContextHandle = browserContextHandle;
     }
 
     @Override
@@ -236,6 +243,11 @@ public class PageZoomPreference extends Preference implements SeekBar.OnSeekBarC
         } else if (seekBar.getId() == R.id.text_size_contrast_slider) {
             // TODO(crbug.com/1459631): add functionality to save the new progress variable to a
             // profile setting, which can access on c++ side
+
+            UserPrefs.get(mBrowserContextHandle)
+                    .setInteger("settings.a11y.text_size_contrast_factor",
+                            PageZoomUtils.TEXT_SIZE_CONTRAST_MAX_LEVEL - seekBar.getProgress());
+            // TODO(crbug.com/1459631): Add page refresh functionality
         }
     }
 
