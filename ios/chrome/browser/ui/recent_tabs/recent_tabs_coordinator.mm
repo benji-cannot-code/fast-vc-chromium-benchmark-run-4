aaade67dc491a639f3147322b90c20d97194121a
ginnnnnnny <ginnyhuang@chromium.org>
2023-09-05 16:19:06 +0000

    [iOS][byot] Support lazy loading tabs and use it in BringAndroidTabs
    This CL supports lazy loading options in TabInsertionBrowserAgent and
    UrlLoadingBrowserAgent, and changes the behavior when users "open"
    tabs from BYOT. Instead of loading all the tabs at once, it only loads
    the first 6 (enough to fill the tab grid) and adds the rest to the tab
    grid without loading them. In the tab grid, the user would see the
    title of the page in the grid, and the page only loads if the user taps
    on the grid to open it.
    
    This is done by turning each remote tab into a CRWSessionStorage,
    use the storage object to create an unrealized web state, and add it
    to the web state list. Also the option to lazy load tabs and place-
    holder title is added to UrlLoadingParams.
    
    Fixed: 1468596
    Change-Id: I11f9712d1c8edbbd61be1d5b76930dd15e3dd43d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4833514
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192506}

--- ios/chrome/browser/ui/recent_tabs/recent_tabs_coordinator.mm
+++ ios/chrome/browser/ui/recent_tabs/recent_tabs_coordinator.mm
@@ -183,8 +183,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   BOOL inIncognito = self.browser->GetBrowserState()->IsOffTheRecord();
   UrlLoadingBrowserAgent* URLLoader =
       UrlLoadingBrowserAgent::FromBrowser(self.browser);
-  OpenDistantSessionInBackground(session, inIncognito, URLLoader,
-                                 self.loadStrategy);
+  OpenDistantSessionInBackground(session, inIncognito,
+                                 GetDefaultNumberOfTabsToLoadSimultaneously(),
+                                 URLLoader, self.loadStrategy);
 
   [self showActiveRegularTabFromRecentTabs];
 }
