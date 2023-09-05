9d961817a4f7d4de91726a9941636a2f1df656fd
Veronique Nguyen <veronguyen@google.com>
2023-09-05 05:34:57 +0000

    [iOS] Use BrowserList to get browsers to update the form managers
    We are getting the BrowserList and going through each browser's active
    web state to get the passwordManagerClient. This will allow us to
    update the form managers of password changes. By doing so, this will
    work for multi window changes.
    
    CL to skip updating form managers:
    https://chromium-review.googlesource.com/c/chromium/src/+/4727598
    
    We have an EG test for this:
    PasswordManagerTestCase/testEditPasswordWithOnlyIncognitoTabOpen
    
    Bug: 1468506
    Change-Id: I3e4231398be26e437c77b183d07aa2082a2b4110
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4811722
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Veronique Nguyen <veronguyen@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1192274}

--- ios/chrome/browser/ui/settings/password/password_details/password_details_coordinator.mm
+++ ios/chrome/browser/ui/settings/password/password_details/password_details_coordinator.mm
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/coordinator/alert/action_sheet_coordinator.h"
 #import "ios/chrome/browser/shared/coordinator/alert/alert_coordinator.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
+#import "ios/chrome/browser/shared/model/browser/browser_list.h"
+#import "ios/chrome/browser/shared/model/browser/browser_list_factory.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/shared/public/commands/application_commands.h"
@@ -435,19 +437,17 @@ using password_manager::features::IsAuthOnEntryV2Enabled;
 }
 
 - (void)updateFormManagers {
-  web::WebState* activeWebState =
-      self.browser->GetWebStateList()->GetActiveWebState();
-  if (!activeWebState) {
-    // PasswordDetailsCoordinator and other settings coordinators always receive
-    // a normal Browser, even if they are started from incognito. So if only
-    // incognito tabs are open, `activeWebState` is null, causing a crash
-    // (crbug.com/1468506).
-    return;
+  ChromeBrowserState* browserState = self.browser->GetBrowserState();
+  BrowserList* browserList =
+      BrowserListFactory::GetForBrowserState(browserState);
+
+  for (Browser* browser : browserList->AllRegularBrowsers()) {
+    [self updateFormManagersForBrowser:browser];
   }
-  password_manager::PasswordManagerClient* passwordManagerClient =
-      PasswordTabHelper::FromWebState(activeWebState)
-          ->GetPasswordManagerClient();
-  passwordManagerClient->UpdateFormManagers();
+
+  for (Browser* browser : browserList->AllIncognitoBrowsers()) {
+    [self updateFormManagersForBrowser:browser];
+  }
 }
 
 #pragma mark - ReauthenticationCoordinatorDelegate
@@ -527,6 +527,17 @@ using password_manager::features::IsAuthOnEntryV2Enabled;
     case DetailsContext::kOutsideSettings:
       return YES;
   }
+}
+
+// Refreshes the password suggestions list for a specific `browser`.
+- (void)updateFormManagersForBrowser:(Browser*)browser {
+  web::WebState* webState = browser->GetWebStateList()->GetActiveWebState();
+  if (!webState) {
+    return;
+  }
+  password_manager::PasswordManagerClient* passwordManagerClient =
+      PasswordTabHelper::FromWebState(webState)->GetPasswordManagerClient();
+  passwordManagerClient->UpdateFormManagers();
 }
 
 @end
