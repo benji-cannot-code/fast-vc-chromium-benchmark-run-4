aa2cfef0c55de6693bb6060b56bd3527201a68c2
Satoshi Niwa <niwa@google.com>
2023-09-05 08:13:28 +0000

    arc: Add new mojo function TimerHost::SetTime
    We need this for ARC-R Container: See b/277537654
    CL for adding arc-set-time upstart job: crrev.com/c/4768319
    
    Also disable ArcTimerBridge for ARCVM,
    as the service is only used by ARC Container.
    
    BUG=b:277537654
    TEST=ash_components_unittests
    
    Change-Id: I4e71bdbbe171fbcb6a348e4efabc6910569cec00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4778365
    Commit-Queue: Satoshi Niwa <niwa@chromium.org>
    Reviewed-by: Josh Horwich <jhorwich@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192316}

--- chrome/browser/ash/arc/session/arc_service_launcher.cc
+++ chrome/browser/ash/arc/session/arc_service_launcher.cc
@@ -315,7 +315,6 @@ void ArcServiceLauncher::OnPrimaryUserProfilePrepared(Profile* profile) {
   ArcSharesheetBridge::GetForBrowserContext(profile);
   ArcSurveyService::GetForBrowserContext(profile);
   ArcSystemUIBridge::GetForBrowserContext(profile);
-  ArcTimerBridge::GetForBrowserContext(profile);
   ArcTracingBridge::GetForBrowserContext(profile);
   ArcTtsService::GetForBrowserContext(profile);
   ArcUsbHostBridge::GetForBrowserContext(profile);
@@ -347,6 +346,7 @@ void ArcServiceLauncher::OnPrimaryUserProfilePrepared(Profile* profile) {
       ArcIdleManager::GetForBrowserContext(profile);
   } else {
     // ARC Container-only services.
+    ArcTimerBridge::GetForBrowserContext(profile);
     ArcAppfuseBridge::GetForBrowserContext(profile);
     ArcObbMounterBridge::GetForBrowserContext(profile);
   }
