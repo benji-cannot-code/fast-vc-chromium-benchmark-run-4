4d85d86058fcd6a795d975a66794b8a18ae97c99
Sammie Quon <sammiequon@chromium.org>
2023-09-06 01:02:31 +0000

    Revert "Scrolling on clamshell overview mode."
    This reverts commit cae7ed8bf6d7b712c538bb3d8fd082178c706fb2.
    
    Reason for revert: b/298718647
    
    Original change's description:
    > Scrolling on clamshell overview mode.
    >
    > Prevents overview mode in clamshell from being cluttered by adding a
    > scroll layout when all the windows cannot fit on the screen.
    >
    > Adds a cros-labs feature flag and basic functionality.
    >
    > Adds basic tests. Will add more tests in future CLs and as edge cases
    > are found.
    >
    > Test: Manual, ash_unittests
    > Change-Id: Ifa5aef315f5a97a5d2b0c0a54c6a4957a84bee72
    > Bug: b/284165330
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4574788
    > Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    > Commit-Queue: Phillip Vanderlaat <pvanderlaat@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1157242}
    
    Bug: b/284165330
    Change-Id: If7b8205afa92c4a1844773c9d76127bebcd63765
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4836977
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192807}

--- ash/wm/overview/overview_item.cc
+++ ash/wm/overview/overview_item.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/rounded_label_widget.h"
 #include "ash/style/system_shadow.h"
-#include "ash/wm/desks/desk_bar_controller.h"
 #include "ash/wm/desks/desks_util.h"
 #include "ash/wm/desks/templates/saved_desk_animations.h"
 #include "ash/wm/drag_window_controller.h"
--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "components/supervised_user/core/common/buildflags.h"
-#include "flag_descriptions.h"
 #include "pdf/buildflags.h"
 
 // Keep in identical order as the header file, see the comment at the top
@@ -6853,13 +6852,6 @@ const char kShowTouchHudDescription[] =
     "Shows a trail of colored dots for the last few touch points. Pressing "
     "Ctrl-Alt-I shows a heads-up display view in the top-left corner. Helps "
     "debug hardware issues that generate spurious touch events.";
-
-const char kOverviewScrollLayoutForClamshellName[] =
-    "Scrollable overview layout for clamshell";
-const char kOverviewScrollLayoutForClamshellDescription[] =
-    "Shows a new overview mode layout on clamshell when a large number of "
-    "windows are on the display. Improves the performance of overview mode and "
-    "prevents the screen from getting too cluttered";
 
 const char kContinuousOverviewScrollAnimationName[] =
     "Makes the gesture for Overview continuous";
