d37be7457ddd4d1823fe638ab902e09f0551367c
Matt Reichhoff <mreichhoff@chromium.org>
2023-09-06 12:37:44 +0000

    Remove unneeded plus_address feature dependency
    Mocking means there's no need for this dependency.
    
    Change-Id: I44d935c93b219b071a38a8f3ba3350557c79019b
    Bug: 1467623
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4843229
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192974}

--- components/autofill/core/browser/browser_autofill_manager_unittest.cc
+++ components/autofill/core/browser/browser_autofill_manager_unittest.cc
@@ -81,7 +81,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/form_field_data.h"
 #include "components/autofill/core/common/signatures.h"
 #include "components/feature_engagement/public/feature_constants.h"
-#include "components/plus_addresses/features.h"
 #include "components/plus_addresses/plus_address_service.h"
 #include "components/prefs/pref_service.h"
 #include "components/security_interstitials/core/pref_names.h"
@@ -10748,7 +10747,6 @@ class BrowserAutofillManagerPlusAddressTest
         .WillByDefault(Return(mock_plus_address_service_.get()));
   }
   std::unique_ptr<NiceMock<MockPlusAddressService>> mock_plus_address_service_;
-  base::test::ScopedFeatureList features_{plus_addresses::kFeature};
 };
 
 // Ensure that plus address options aren't shown unexpectedly.
