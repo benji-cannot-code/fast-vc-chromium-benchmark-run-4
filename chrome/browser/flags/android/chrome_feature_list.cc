c3dad68764c619406778d13cbaf3c3b5bbcb3d67
Aishwarya Rajesh <aishwaryarj@google.com>
2023-09-06 21:07:49 +0000

    [APS] Default-enable flag AdvancedPeripheralsSupport
    Bug: 1445822
    Change-Id: Id67f63f8d34907daa6c9ea7621724ee020dc5871
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4847271
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1193250}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -449,7 +449,7 @@ BASE_FEATURE(kAddToHomescreenIPH,
 
 BASE_FEATURE(kAdvancedPeripheralsSupport,
              "AdvancedPeripheralsSupport",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAdvancedPeripheralsSupportTabStrip,
              "AdvancedPeripheralsSupportTabStrip",
