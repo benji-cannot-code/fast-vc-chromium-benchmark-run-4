0687554d65ba2e6584949a6e54bf311ff8b5fbba
Sam Davis Omekara <samomekarajr@microsoft.com>
2023-09-06 19:16:54 +0000

    Port remaining control color ids to the color pipeline
    This CL continues and concludes porting NativeThemeBase::ControlColorId
    to the color pipeline. This change should not introduce any functional
    change in production but introduces slight changes in web tests for
    fluent scrollbars. Initial porting of controls was done for scrollbars
    in CL:3966310. This change is also part of the ongoing effort to move
    the default forced colors definition to the color pipeline.
    
    List of changes:
    1. Added ColorIds and RendererColorIds for the
    NativeThemeBase::ControlColorId.
    2. Added dark and light color definitions for these controls.
    3. Added forced colors color definitions for these controls.
    4. Moved the ColorSchemeForAccentColor() function to the
    WebThemeEngineDefault to ensure the right color scheme is obtained
    before painting.
    5. Introduce a color provider to support web tests that rely on
    hardcoded High Contrast colors to paint CL:2295695. As a result,
    we update web tests for fluent scroll bars to reflect these colors
    while in forced colors mode.
    
    Bug: 1430181
    Change-Id: I82f31ad2dae80d4bb9973c9037720077864e8d8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4781766
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Commit-Queue: Sam Davis Omekara <samomekarajr@microsoft.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193182}

--- ui/native_theme/native_theme_fluent.cc
+++ ui/native_theme/native_theme_fluent.cc
@@ -75,7 +75,7 @@ void NativeThemeFluent::PaintScrollbarTrack(
     outline_rect.Inset(outline_insets + gfx::InsetsF(edge_insets));
 
     const SkColor track_outline_color =
-        color_provider->GetColor(kColorScrollbarThumb);
+        color_provider->GetColor(kColorWebNativeControlScrollbarThumb);
 
     cc::PaintFlags outline_flags;
     outline_flags.setColor(track_outline_color);
@@ -90,7 +90,7 @@ void NativeThemeFluent::PaintScrollbarTrack(
   const SkColor track_color =
       extra_params.track_color.has_value()
           ? extra_params.track_color.value()
-          : color_provider->GetColor(kColorScrollbarTrack);
+          : color_provider->GetColor(kColorWebNativeControlScrollbarTrack);
   cc::PaintFlags flags;
   flags.setColor(track_color);
   canvas->drawIRect(gfx::RectToSkIRect(track_fill_rect), flags);
@@ -116,11 +116,11 @@ void NativeThemeFluent::PaintScrollbarThumb(
   path.addRRect(rrect);
   canvas->clipPath(path, true);
 
-  ColorId thumb_color_id = kColorScrollbarThumb;
+  ColorId thumb_color_id = kColorWebNativeControlScrollbarThumb;
   if (state == NativeTheme::kPressed) {
-    thumb_color_id = kColorScrollbarThumbPressed;
+    thumb_color_id = kColorWebNativeControlScrollbarThumbPressed;
   } else if (state == NativeTheme::kHovered) {
-    thumb_color_id = kColorScrollbarThumbHovered;
+    thumb_color_id = kColorWebNativeControlScrollbarThumbHovered;
   }
   const SkColor thumb_color = color_provider->GetColor(thumb_color_id);
   cc::PaintFlags flags;
@@ -140,7 +140,7 @@ void NativeThemeFluent::PaintScrollbarCorner(
   const SkColor corner_color =
       extra_params.track_color.has_value()
           ? extra_params.track_color.value()
-          : color_provider->GetColor(kColorScrollbarCorner);
+          : color_provider->GetColor(kColorWebNativeControlScrollbarCorner);
   flags.setColor(corner_color);
   canvas->drawIRect(RectToSkIRect(rect), flags);
 }
@@ -183,7 +183,7 @@ void NativeThemeFluent::PaintButton(
   const SkColor button_color =
       arrow.track_color.has_value()
           ? arrow.track_color.value()
-          : color_provider->GetColor(kColorScrollbarTrack);
+          : color_provider->GetColor(kColorWebNativeControlScrollbarTrack);
   flags.setColor(button_color);
   gfx::Rect button_fill_rect = rect;
   if (InForcedColorsMode()) {
@@ -202,7 +202,7 @@ void NativeThemeFluent::PaintButton(
     gfx::RectF outline_rect(rect);
     outline_rect.Inset(outline_insets + gfx::InsetsF(edge_insets));
     const SkColor arrow_outline_color =
-        color_provider->GetColor(kColorScrollbarThumb);
+        color_provider->GetColor(kColorWebNativeControlScrollbarThumb);
 
     cc::PaintFlags outline_flags;
     outline_flags.setColor(arrow_outline_color);
@@ -229,8 +229,8 @@ void NativeThemeFluent::PaintArrow(
     const ScrollbarArrowExtraParams& arrow) const {
   const ColorId arrow_color_id =
       state == NativeTheme::kPressed || state == NativeTheme::kHovered
-          ? kColorScrollbarArrowForegroundPressed
-          : kColorScrollbarArrowForeground;
+          ? kColorWebNativeControlScrollbarArrowForegroundPressed
+          : kColorWebNativeControlScrollbarArrowForeground;
   // TODO(crbug.com/891944): Adjust thumb_color based on `state`.
   const SkColor arrow_color = arrow.thumb_color.has_value()
                                   ? arrow.thumb_color.value()
