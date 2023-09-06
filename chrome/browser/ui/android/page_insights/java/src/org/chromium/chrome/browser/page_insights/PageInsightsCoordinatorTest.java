d4627f84be3ea016e495aee2c37c97c4ab70e6a1
Edmund Wright <edmundw@google.com>
2023-09-06 16:46:13 +0000

    [gbb-cct] Implement actions that can be invoked by server-provided UI.
    Bug: b/286003870
    Bug: b/290846074
    Change-Id: Ibc1ff3c1c49a847062df5539fb253658f20ba6f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4839497
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Auto-Submit: Edmund Wright <edmundw@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193095}

--- chrome/browser/ui/android/page_insights/java/src/org/chromium/chrome/browser/page_insights/PageInsightsCoordinatorTest.java
+++ chrome/browser/ui/android/page_insights/java/src/org/chromium/chrome/browser/page_insights/PageInsightsCoordinatorTest.java
@@ -39,6 +39,8 @@ import org.chromium.base.test.BaseActivityTestRule;
 import org.chromium.base.test.util.Batch;
 import org.chromium.chrome.browser.browser_controls.BrowserControlsSizer;
 import org.chromium.chrome.browser.browser_controls.BrowserControlsStateProvider;
+import org.chromium.chrome.browser.page_insights.proto.PageInsights;
+import org.chromium.chrome.browser.share.ShareDelegate;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabObserver;
 import org.chromium.chrome.browser.xsurface.ProcessScope;
@@ -105,6 +107,8 @@ public class PageInsightsCoordinatorTest {
     private PageInsightsSurfaceScope mSurfaceScope;
     @Mock
     private PageInsightsSurfaceRenderer mSurfaceRenderer;
+    @Mock
+    private Supplier<ShareDelegate> mShareDelegateSupplier;
 
     private PageInsightsCoordinator mPageInsightsCoordinator;
     private ManagedBottomSheetController mPageInsightsController;
@@ -162,8 +166,9 @@ public class PageInsightsCoordinatorTest {
         });
         doReturn(true).when(mIsPageInsightsHubEnabled).getAsBoolean();
         mPageInsightsCoordinator = new PageInsightsCoordinator(activity, mTabProvider,
-                mPageInsightsController, mBottomUiController, mExpandedSheetHelper,
-                mBrowserControlsStateProvider, mBrowserControlsSizer, mIsPageInsightsHubEnabled);
+                mShareDelegateSupplier, mPageInsightsController, mBottomUiController,
+                mExpandedSheetHelper, mBrowserControlsStateProvider, mBrowserControlsSizer,
+                mIsPageInsightsHubEnabled);
         mTestSupport = new BottomSheetTestSupport(mPageInsightsController);
         waitForAnimationToFinish();
     }
