4cc6420398c25ea8d0db7bde520b15b95490b88d
Colin Blundell <blundell@chromium.org>
2023-09-06 09:01:40 +0000

    [ScopedRestoreTexture] Restore prev binding even if invalid w/killswitch
    The `restore_prev_even_if_invalid` param was introduced to support
    legacy behavior while porting clients of duplicate solutions. Our
    suspicion is that it's not necessary to guard restoring the previous
    binding with a check that it's valid; that behavior was introduced as a
    speculative fix for a crash in [1] but didn't seem to help ([2]). This
    CL changes ScopedRestoreTexture to unconditionally restore the previous
    binding with a killswitch.  Once this change definitively lands, we
    will remove the `restore_prev_even_if_invalid` parameter altogether.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/3669775
    [2] https://bugs.chromium.org/p/chromium/issues/detail?id=1327231#c13)
    
    Bug: 1367187
    Change-Id: If1f781f6041a791e78dc8700ebd006ffa1f5f68f
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4748190
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192915}

--- ui/gl/scoped_restore_texture.cc
+++ ui/gl/scoped_restore_texture.cc
@@ -5,9 +5,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gl/scoped_restore_texture.h"
 
+#include "base/feature_list.h"
 #include "base/notreached.h"
 
 namespace gl {
+
+namespace {
+// The `restore_prev_even_if_invalid` param was introduced to support legacy
+// behavior while porting clients of duplicate solutions. Our suspicion is that
+// it's not necessary to guard restoring the previous binding with a check that
+// it's valid; that behavior was introduced as a speculative fix for a crash
+// in https://chromium-review.googlesource.com/c/chromium/src/+/3669775 but
+// didn't seem to help
+// (https://bugs.chromium.org/p/chromium/issues/detail?id=1327231#c13). This
+// killswitch guards switching to unconditionally restore the previous binding.
+// Once this change definitively lands, we will unconditionally restore the
+// previous binding and remove the `restore_prev_even_if_invalid` parameter
+// altogether.
+BASE_FEATURE(kAlwaysRestorePrevEvenIfInvalid,
+             "AlwaysRestorePrevEvenIfInvalid",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+}  // namespace
 
 ScopedRestoreTexture::ScopedRestoreTexture(
     gl::GLApi* api,
@@ -35,7 +53,9 @@ ScopedRestoreTexture::ScopedRestoreTexture(
   api->glGetIntegervFn(get_target, &binding);
   // The bound texture could be already deleted by another context, and the
   // texture ID |binding| could be reused and points to a different texture.
-  if (api->glIsTextureFn(binding) || restore_prev_even_if_invalid) {
+  // NOTE: See comment on killswitch above.
+  if (api->glIsTextureFn(binding) || restore_prev_even_if_invalid ||
+      base::FeatureList::IsEnabled(kAlwaysRestorePrevEvenIfInvalid)) {
     prev_binding_ = binding;
   }
   if (new_binding) {
