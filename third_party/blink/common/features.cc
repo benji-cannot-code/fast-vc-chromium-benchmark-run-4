b4eff6bb32e46086fd2fb00c39001693243983d3
Luke Warlow <luke@warlow.dev>
2023-09-06 18:18:46 +0000

    Enable PrefersReducedTransparency Client Hint
    Bug: 1466423
    Change-Id: Ifc3d228025ef7444ad1ef3bfa24943357cb050db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4846931
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Luke <lukewarlow156@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1193146}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -478,7 +478,7 @@ BASE_FEATURE(kClientHintsFormFactor,
 // Enable `sec-ch-prefers-reduced-transparency` client hint.
 BASE_FEATURE(kClientHintsPrefersReducedTransparency,
              "ClientHintsPrefersReducedTransparency",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Enable `sec-ch-width` client hint.
 BASE_FEATURE(kClientHintsResourceWidth,
