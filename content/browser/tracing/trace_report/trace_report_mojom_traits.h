d5e305b5bb8b0692fa609401786ede59c17a5700
Alex Attar <aattar@google.com>
2023-09-06 17:12:23 +0000

    Adds the structure necessary to display the trace report database from from previous CL.
    Adding a trace_report handler that will communicate through the trace_report_manager with the local trace_report_database. The UI resources are not present in this CL.
    
    Next CL will include:
    - The resources for UI (HTML, CSS)
    - Binding of the resources to the trace_report_handler
    
    Change-Id: I46b97286cec3815287ccb5a010e09b1964b36b10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4630049
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Commit-Queue: Alex Attar <aattar@google.com>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193106}

---
+++ content/browser/tracing/trace_report/trace_report_mojom_traits.h
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_TRACING_TRACE_REPORT_TRACE_REPORT_MOJOM_TRAITS_H_
+#define CONTENT_BROWSER_TRACING_TRACE_REPORT_TRACE_REPORT_MOJOM_TRAITS_H_
+
+#include "content/browser/tracing/trace_report/trace_report.mojom.h"
+#include "content/browser/tracing/trace_report/trace_report_database.h"
+
+namespace mojo {
+
+namespace {
+
+using ReportUploadState = trace_report::mojom::ReportUploadState;
+using SkipUploadReason = trace_report::mojom::SkipUploadReason;
+
+}  // namespace
+
+template <>
+struct EnumTraits<ReportUploadState, content::ReportUploadState> {
+  static ReportUploadState ToMojom(content::ReportUploadState input);
+  static bool FromMojom(ReportUploadState input,
+                        content::ReportUploadState* output);
+};
+
+template <>
+struct EnumTraits<SkipUploadReason, content::SkipUploadReason> {
+  static SkipUploadReason ToMojom(content::SkipUploadReason input);
+  static bool FromMojom(SkipUploadReason input,
+                        content::SkipUploadReason* output);
+};
+
+}  // namespace mojo
+
+#endif  // CONTENT_BROWSER_TRACING_TRACE_REPORT_TRACE_REPORT_MOJOM_TRAITS_H_
