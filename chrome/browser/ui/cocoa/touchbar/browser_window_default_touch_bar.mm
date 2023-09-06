8bc3c37e71c5a08917db625e69104b41335a07fc
Leonard Grey <lgrey@chromium.org>
2023-09-06 15:07:48 +0000

    Mac: extract some fullscreen utilities
    The content fullscreen part of this is yak-shaving in advance of
    https://crbug.com/1477825. I'm preserving current behavior here, but
    am honestly a little confused on how the extension fullscreen thing
    ties in.
    
    Bug: 1477825, 1476662
    Change-Id: Iea8344968dec630fbab4bd83ae8b656949ebeb63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4833443
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193042}

--- chrome/browser/ui/cocoa/touchbar/browser_window_default_touch_bar.mm
+++ chrome/browser/ui/cocoa/touchbar/browser_window_default_touch_bar.mm
@@ -26,8 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_list_observer.h"
 #import "chrome/browser/ui/cocoa/touchbar/browser_window_touch_bar_controller.h"
-#include "chrome/browser/ui/exclusive_access/exclusive_access_manager.h"
-#include "chrome/browser/ui/exclusive_access/fullscreen_controller.h"
+#include "chrome/browser/ui/fullscreen_util_mac.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/omnibox/browser/vector_icons.h"
@@ -299,10 +298,7 @@ class TouchBarNotificationBridge : public CommandObserver,
   // When in tab or extension fullscreen, we should show a touch bar containing
   // only items associated with that mode. Since the toolbar is hidden, only
   // the option to exit fullscreen should show up.
-  FullscreenController* controller =
-      _browser->exclusive_access_manager()->fullscreen_controller();
-  if (controller->IsWindowFullscreenForTabOrPending() ||
-      controller->IsExtensionFullscreenOrPending()) {
+  if (fullscreen_utils::IsInContentFullscreen(_browser)) {
     return [self createTabFullscreenTouchBar];
   }
 
---
+++ chrome/browser/ui/fullscreen_util_mac.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/fullscreen_util_mac.h"
+
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/exclusive_access/exclusive_access_context.h"
+#include "chrome/browser/ui/exclusive_access/exclusive_access_manager.h"
+#include "chrome/browser/ui/exclusive_access/fullscreen_controller.h"
+#include "chrome/browser/ui/web_applications/app_browser_controller.h"
+#include "chrome/common/pref_names.h"
+#include "components/prefs/pref_service.h"
+
+namespace fullscreen_utils {
+
+bool IsInContentFullscreen(Browser* browser) {
+  FullscreenController* controller =
+      browser->exclusive_access_manager()->fullscreen_controller();
+  return controller->IsWindowFullscreenForTabOrPending() ||
+         controller->IsExtensionFullscreenOrPending();
+}
+
+bool IsAlwaysShowToolbarEnabled(const Browser* browser) {
+  if (web_app::AppBrowserController::IsWebApp(browser)) {
+    const web_app::AppBrowserController* controller = browser->app_controller();
+    return controller->AlwaysShowToolbarInFullscreen();
+  }
+  return browser->profile()->GetPrefs()->GetBoolean(
+      prefs::kShowFullscreenToolbar);
+}
+
+}  // namespace fullscreen_utils
