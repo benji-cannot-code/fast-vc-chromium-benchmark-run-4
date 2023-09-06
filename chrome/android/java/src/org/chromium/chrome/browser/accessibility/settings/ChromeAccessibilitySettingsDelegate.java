6c7580e142b639be761dc932c3e8b142ef71810c
Aashna Sheth <aashnas@google.com>
2023-09-06 20:15:59 +0000

    [PageZoom] Jump to Zoom Info From Accessibility Settings
    This CL adds a new row to the Chrome Accessibility settings that leads
    to a site settings page displaying the zoom level for all
    non-default-zoom websites.
    
    Screenshot (new row): https://hsv.googleplex.com/6232826774028288
    
    Bug: 1459631, b/265491863
    
    Change-Id: I50c24f375ba4416a0755a039157e477fcd7e60ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4833284
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Aashna Sheth <aashnas@google.com>
    Cr-Commit-Position: refs/heads/main@{#1193224}

--- chrome/android/java/src/org/chromium/chrome/browser/accessibility/settings/ChromeAccessibilitySettingsDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/accessibility/settings/ChromeAccessibilitySettingsDelegate.java
@@ -5,14 +5,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.accessibility.settings;
 
+import android.content.Context;
+import android.os.Bundle;
+
 import androidx.preference.PreferenceFragmentCompat;
 
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.image_descriptions.ImageDescriptionsController;
 import org.chromium.chrome.browser.preferences.Pref;
 import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.settings.SettingsLauncherImpl;
 import org.chromium.components.browser_ui.accessibility.AccessibilitySettingsDelegate;
 import org.chromium.components.browser_ui.accessibility.PageZoomUtils;
+import org.chromium.components.browser_ui.settings.SettingsLauncher;
+import org.chromium.components.browser_ui.site_settings.AllSiteSettings;
+import org.chromium.components.browser_ui.site_settings.SingleCategorySettings;
+import org.chromium.components.browser_ui.site_settings.SiteSettingsCategory;
 import org.chromium.components.user_prefs.UserPrefs;
 import org.chromium.content_public.browser.BrowserContextHandle;
 
@@ -66,5 +74,14 @@ public class ChromeAccessibilitySettingsDelegate implements AccessibilitySetting
     @Override
     public boolean showPageZoomSettingsUI() {
         return PageZoomUtils.shouldShowSettingsUI();
+    }
+
+    @Override
+    public void launchSiteSettingsZoomActivity(Context context) {
+        Bundle initialArguments = new Bundle();
+        initialArguments.putString(SingleCategorySettings.EXTRA_CATEGORY,
+                SiteSettingsCategory.preferenceKey(SiteSettingsCategory.Type.ZOOM));
+        SettingsLauncher settingsLauncher = new SettingsLauncherImpl();
+        settingsLauncher.launchSettingsActivity(context, AllSiteSettings.class, initialArguments);
     }
 }
--- components/browser_ui/accessibility/android/java/src/org/chromium/components/browser_ui/accessibility/AccessibilitySettingsDelegate.java
+++ components/browser_ui/accessibility/android/java/src/org/chromium/components/browser_ui/accessibility/AccessibilitySettingsDelegate.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.browser_ui.accessibility;
 
+import android.content.Context;
+
 import androidx.annotation.NonNull;
 import androidx.preference.PreferenceFragmentCompat;
 
@@ -50,4 +52,11 @@ public interface AccessibilitySettingsDelegate {
      * Returns whether or not the 'Zoom' feature specific UI should be shown in Settings.
      */
     boolean showPageZoomSettingsUI();
+
+    /**
+     * Launches a site settings category that displays zoom levels for each website.
+     *
+     * @param context the context from which to launch the activity from.
+     */
+    void launchSiteSettingsZoomActivity(Context context);
 }
