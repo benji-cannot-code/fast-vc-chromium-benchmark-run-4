24e0be17a5b40fa6e48406fb375adb2ddb09fd51
Yuheng Huang <yuhengh@chromium.org>
2023-09-06 14:56:35 +0000

    User Notes: Fix browser crash with user notes URL
    The crash in the bug is caused by using a pref service[1] not
    registered when User Notes feature is not enabled. The fix will return
    early in the call before hitting that.
    
    [1]https://source.chromium.org/chromium/chromium/src/+/cc8d0cca3c41e7e4bfc9a95caa0b2007f2bbf5d5:chrome/browser/ui/webui/side_panel/user_notes/user_notes_side_panel_ui.cc;l=62
    
    Change-Id: I822cef38098358c5471cae37b3bc22461e91ae8d
    Bug: 1477179
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4841687
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193038}

--- chrome/browser/ui/webui/side_panel/user_notes/user_notes_side_panel_ui.cc
+++ chrome/browser/ui/webui/side_panel/user_notes/user_notes_side_panel_ui.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/side_panel_user_notes_resources_map.h"
 #include "components/prefs/pref_service.h"
 #include "components/strings/grit/components_strings.h"
+#include "components/user_notes/user_notes_features.h"
 #include "components/user_notes/user_notes_prefs.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui_data_source.h"
@@ -27,6 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 UserNotesSidePanelUI::UserNotesSidePanelUI(content::WebUI* web_ui)
     : ui::MojoBubbleWebUIController(web_ui, true) {
+  if (!user_notes::IsUserNotesEnabled()) {
+    return;
+  }
   content::WebUIDataSource* source = content::WebUIDataSource::CreateAndAdd(
       web_ui->GetWebContents()->GetBrowserContext(),
       chrome::kChromeUIUserNotesSidePanelHost);
