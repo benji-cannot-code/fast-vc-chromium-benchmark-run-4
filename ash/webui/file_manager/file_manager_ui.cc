55cca0e5230ef58461066a5abe39bc4681a74878
Luciano Pacheco <lucmult@chromium.org>
2023-09-06 03:17:35 +0000

    Files app: Stop auto generating the main.html
    The script gen_main_html.py was used to mutate the main.html to be
    able to share it between the SWA and the legacy extension. SWA is
    fully launched so we can make the main.html support the SWA only
    directly.
    
    The changes that the script was performing that was merged in the
    main.html.
    
    1. Including the init_globals.js as early as possible.
    2. Add charset="utf-8" to the <meta>
    3. Replace the foreground/js/main.js with the /main.js which later
       dynamically loads the foreground/js/main.js.
    
    NOTE: The follow up CL:4831211 will rework these 2 main.js to have only
    1 main.js.
    
    Bug: b:244393138, b:262833874
    Change-Id: I99c5b12ff68c6b2fb9e3ed14e9b5ce5a8bb60c32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4829263
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192842}

--- ash/webui/file_manager/file_manager_ui.cc
+++ ash/webui/file_manager/file_manager_ui.cc
@@ -23,8 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui_data_source.h"
 #include "content/public/common/url_constants.h"
 #include "services/network/public/mojom/content_security_policy.mojom.h"
+#include "ui/file_manager/grit/file_manager_gen_resources.h"
 #include "ui/file_manager/grit/file_manager_gen_resources_map.h"
-#include "ui/file_manager/grit/file_manager_resources.h"
 #include "ui/file_manager/grit/file_manager_resources_map.h"
 #include "ui/webui/color_change_listener/color_change_handler.h"
 
@@ -94,8 +94,7 @@ void FileManagerUI::CreateAndAddTrustedAppDataSource(content::WebUI* web_ui,
       web_ui->GetWebContents()->GetBrowserContext(), kChromeUIFileManagerHost);
 
   // Setup chrome://file-manager main and default page.
-  source->AddResourcePath("", IDR_FILE_MANAGER_SWA_MAIN_HTML);
-
+  source->AddResourcePath("", IDR_FILE_MANAGER_MAIN_HTML);
   // Add chrome://file-manager content.
   source->AddResourcePaths(
       base::make_span(kFileManagerSwaResources, kFileManagerSwaResourcesSize));
