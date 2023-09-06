30d4ad087b1eec2558c36efcba976efee06753d7
mikt <mikt@google.com>
2023-09-06 07:06:54 +0000

    [BRP/rewriter] Fix path-based exclusions
    crrev.com/c/4816176 broke the rewriter by removing some entries in
    `RawPtrManualPathsToIgnore.h`.
    This CL will add entries from `SeparateRepositoryPaths.h` to exclusions.
    
    Change-Id: I3bceec9252ecf06568e72246bd0e028da33d2e0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4841794
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192884}

--- tools/clang/rewrite_raw_ptr_fields/RewriteRawPtrFields.cpp
+++ tools/clang/rewrite_raw_ptr_fields/RewriteRawPtrFields.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "RawPtrHelpers.h"
 #include "RawPtrManualPathsToIgnore.h"
+#include "SeparateRepositoryPaths.h"
 #include "clang/AST/ASTContext.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 #include "clang/ASTMatchers/ASTMatchers.h"
@@ -1237,6 +1238,9 @@ int main(int argc, const char* argv[]) {
   if (override_exclude_paths_param == "") {
     std::vector<std::string> paths_to_exclude_lines;
     for (auto* const line : kRawPtrManualPathsToIgnore) {
+      paths_to_exclude_lines.push_back(line);
+    }
+    for (auto* const line : kSeparateRepositoryPaths) {
       paths_to_exclude_lines.push_back(line);
     }
     paths_to_exclude = std::make_unique<FilterFile>(paths_to_exclude_lines);
