6a017b3cb0edbc41f0e1ce316671f32f34f1f1a3
Lingyu Feng <lingyufeng@chromium.org>
2023-09-06 03:17:47 +0000

    Add UMA for counting frame color changes
    The frame color sometimes flickers when an ARC app is launched
    (b/196110656). To get a better idea about this problem, this CL adds
    metrics for such undesired window behavior. UMA records the number of
    frame color changes within three seconds after the app is launched.
    
    BUG=b:294145104
    TEST=manually checked chrome://histograms
    TEST=DefaultFrameHeaderTest.*
    
    Change-Id: Ibaf64e775cacfb2b614449c68ae9bd770120ede8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4812388
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Auto-Submit: Lingyu Feng <lingyufeng@chromium.org>
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Commit-Queue: Lingyu Feng <lingyufeng@chromium.org>
    Reviewed-by: Shengsong Tan <sstan@chromium.org>
    Reviewed-by: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192843}

--- chromeos/ui/frame/default_frame_header.cc
+++ chromeos/ui/frame/default_frame_header.cc
@@ -80,6 +80,7 @@ DefaultFrameHeader::DefaultFrameHeader(
     : FrameHeader(target_widget, header_view) {
   DCHECK(caption_button_container);
   SetCaptionButtonContainer(caption_button_container);
+  InitializeFrameColorMetricsHelper();
 }
 
 DefaultFrameHeader::~DefaultFrameHeader() = default;
@@ -111,6 +112,9 @@ void DefaultFrameHeader::UpdateFrameColors() {
 
   if (updated) {
     StartTransitionAnimation(kDefaultFrameColorChangeAnimationDuration);
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+    frame_color_metrics_helper_->UpdateFrameColorChangesCount();
+#endif
   }
 
   if (::features::IsChromeRefresh2023() &&
@@ -187,6 +191,15 @@ SkColor DefaultFrameHeader::GetCurrentFrameColor() const {
 
 SkColor DefaultFrameHeader::GetActiveFrameColorForPaintForTest() {
   return active_frame_color_;
+}
+
+void DefaultFrameHeader::InitializeFrameColorMetricsHelper() {
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  aura::Window* window = GetTargetWindow();
+  CHECK(window);
+  frame_color_metrics_helper_ = std::make_unique<FrameColorMetricsHelper>(
+      static_cast<ash::AppType>(window->GetProperty(aura::client::kAppType)));
+#endif
 }
 
 }  // namespace chromeos
--- chromeos/ui/frame/default_frame_header.h
+++ chromeos/ui/frame/default_frame_header.h
@@ -6,10 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMEOS_UI_FRAME_DEFAULT_FRAME_HEADER_H_
 #define CHROMEOS_UI_FRAME_DEFAULT_FRAME_HEADER_H_
 
+#include <memory>
+
 #include "base/compiler_specific.h"  // override
 #include "base/component_export.h"
 #include "base/gtest_prod_util.h"
 #include "chromeos/ui/base/chromeos_ui_constants.h"
+#include "chromeos/ui/frame/frame_color_metrics_helper.h"
 #include "chromeos/ui/frame/frame_header.h"
 
 namespace ash {
@@ -57,10 +60,19 @@ class COMPONENT_EXPORT(CHROMEOS_UI_FRAME) DefaultFrameHeader
 
   SkColor GetActiveFrameColorForPaintForTest();
 
+  // This function should be only called once, i.e., one instance of
+  // default_frame_header should own only one `frame_color_metrics_helper_`
+  // throughout its lifetime.
+  void InitializeFrameColorMetricsHelper();
+
   SkColor active_frame_color_ = chromeos::kDefaultFrameColor;
   SkColor inactive_frame_color_ = chromeos::kDefaultFrameColor;
 
   int width_in_pixels_ = -1;
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  std::unique_ptr<FrameColorMetricsHelper> frame_color_metrics_helper_;
+#endif
 };
 
 }  // namespace chromeos
