e5c087936424372e5f36dfd76b904bc48f4c3e26
Mirko Bonadei <mbonadei@chromium.org>
2023-09-06 09:16:25 +0000

    Roll abseil_revision b06ab1f355..415a1d1cb9
    Change Log:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+log/b06ab1f355..415a1d1cb9
    Full diff:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+/b06ab1f355..415a1d1cb9
    
    Bug: None
    Change-Id: I33da746c2e9c3cf3d0c917c8bb101286c17bd94b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4843630
    Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>
    Auto-Submit: Mirko Bonadei <mbonadei@chromium.org>
    Reviewed-by: Danil Chapovalov <danilchap@chromium.org>
    Commit-Queue: Danil Chapovalov <danilchap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192919}

--- third_party/abseil-cpp/absl/status/status.cc
+++ third_party/abseil-cpp/absl/status/status.cc
@@ -47,6 +47,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace absl {
 ABSL_NAMESPACE_BEGIN
 
+static_assert(
+    alignof(status_internal::StatusRep) >= 4,
+    "absl::Status assumes it can use the bottom 2 bits of a StatusRep*.");
+
 std::string StatusCodeToString(StatusCode code) {
   switch (code) {
     case StatusCode::kOk:
