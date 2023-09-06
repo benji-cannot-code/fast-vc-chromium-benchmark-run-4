2194ff4e05b4879ae2ad54a66737d63360d532c9
Sergii Bykov <sbykov@google.com>
2023-09-06 21:05:50 +0000

    Remove expired kEnableRestrictedWebApis feature
    Remove enable-restricted-web-apis flag and kEnableRestrictedWebApis
    feature. Flag expired in 115 and feature is long enabled by default.
    API is currently protected by a policy, so there is no more need for a
    flag.
    
    Bug: b:299083854
    Change-Id: I33dd7cf02f6b234407f0fe2993b9aac0015e1c1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4843069
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Commit-Queue: Sergii Bykov <sbykov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1193249}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -8799,11 +8799,6 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kEnableManagedConfigurationWebApiDescription, kOsCrOS,
      FEATURE_VALUE_TYPE(blink::features::kManagedConfiguration)},
 
-    {"enable-restricted-web-apis",
-     flag_descriptions::kEnableRestrictedWebApisName,
-     flag_descriptions::kEnableRestrictedWebApisDescription, kOsCrOS,
-     FEATURE_VALUE_TYPE(features::kEnableRestrictedWebApis)},
-
     {"clear-cross-site-cross-browsing-context-group-window-name",
      flag_descriptions::kClearCrossSiteCrossBrowsingContextGroupWindowNameName,
      flag_descriptions::
--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -1499,12 +1499,6 @@ const char kEnableResamplingScrollEventsExperimentalPredictionDescription[] =
     "Predicts the scroll amount after the vsync time to more closely match "
     "when the frame is visible.";
 
-const char kEnableRestrictedWebApisName[] =
-    "Enable the restricted web APIs for high-trusted apps.";
-const char kEnableRestrictedWebApisDescription[] =
-    "Enable the restricted web APIs for dev trial. This will be replaced with "
-    "permission policies to control the capabilities afterwards.";
-
 const char kEnableWebAuthenticationChromeOSAuthenticatorName[] =
     "ChromeOS platform Web Authentication support";
 const char kEnableWebAuthenticationChromeOSAuthenticatorDescription[] =
