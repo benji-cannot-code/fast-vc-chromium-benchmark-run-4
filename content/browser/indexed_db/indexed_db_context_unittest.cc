ce6a8d7b70ea2de5ea030086097987da401ce83c
Evan Stade <estade@chromium.org>
2023-09-06 23:17:43 +0000

    IndexedDB: skip over unrecognized leveldb databases.
    When the leveldb directory name doesn't match up with a serialized
    storage key, ignore it rather than trying to load bucket for it.
    
    Bug: 1472826
    Change-Id: I5855f4ed12b56698e3d95f8e5a76b906e014078d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4846115
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193308}

--- content/browser/indexed_db/indexed_db_context_unittest.cc
+++ content/browser/indexed_db/indexed_db_context_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/barrier_closure.h"
+#include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/run_loop.h"
 #include "base/task/sequenced_task_runner.h"
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/storage/public/cpp/quota_error_or.h"
 #include "content/browser/indexed_db/indexed_db_context_impl.h"
 #include "content/browser/indexed_db/indexed_db_factory.h"
+#include "content/browser/indexed_db/indexed_db_leveldb_operations.h"
 #include "content/browser/indexed_db/mock_mojo_indexed_db_database_callbacks.h"
 #include "content/browser/indexed_db/mock_mojo_indexed_db_factory_client.h"
 #include "mojo/public/cpp/bindings/associated_receiver.h"
@@ -217,6 +219,19 @@ TEST_F(IndexedDBContextTest, GetDefaultBucketError) {
       mock_factory_client->CreateInterfacePtrAndBind(), u"database_name",
       /*force_close=*/true);
   loop_3.Run();
+}
+
+// Regression test for crbug.com/1472826
+TEST_F(IndexedDBContextTest, DontChokeOnBadLegacyFiles) {
+  base::CreateDirectory(indexed_db_context_->GetFirstPartyDataPathForTesting()
+                            .AppendASCII("invalid_storage_key")
+                            .AddExtension(indexed_db::kIndexedDBExtension)
+                            .AddExtension(indexed_db::kLevelDBExtension));
+
+  base::RunLoop run_loop;
+  indexed_db_context_->ForceInitializeFromFilesForTesting(
+      run_loop.QuitClosure());
+  run_loop.Run();
 }
 
 }  // namespace content
