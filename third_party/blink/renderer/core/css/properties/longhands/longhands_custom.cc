cd2a366c36f889a5c3a7ee89151bd67a5c47ff25
Fredrik Söderquist <fs@opera.com>
2023-09-06 09:23:37 +0000

    [RemoveLegacy] Convert trivial LayoutBox::BorderBoxRect() uses to Physical*()
    Bug: 1229581
    Change-Id: Ie6e5cb7555fc836051994f856e66e332112c0fad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4843035
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1192922}

--- third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
+++ third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
@@ -6393,9 +6393,9 @@ const CSSValue* PerspectiveOrigin::CSSValueFromComputedStyleInternal(
     const LayoutObject* layout_object,
     bool allow_visited_style) const {
   if (layout_object) {
-    LayoutRect box;
+    PhysicalRect box;
     if (layout_object->IsBox()) {
-      box = To<LayoutBox>(layout_object)->BorderBoxRect();
+      box = To<LayoutBox>(layout_object)->PhysicalBorderBoxRect();
     }
 
     return MakeGarbageCollected<CSSValuePair>(
