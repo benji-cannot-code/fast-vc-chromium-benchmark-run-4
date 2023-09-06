31d38c984369ed9d0ed02c0cdb9c403b7f49c38b
Thomas Lukaszewicz <tluk@chromium.org>
2023-09-06 17:46:26 +0000

    [color] Add UserColorSource param to the ColorProviderKey
    This CL introduces the UserColorSource enum to the key, replacing
    the previous is_grayscale param in the process.
    
    The enum defines baseline, grayscale and accent color sources. These
    options define how the color pipeline selects the input color for the
    generation of sys color tokens. These options are intended to be
    mutually exclusive.
    
    This CL also sets the default source to baseline on windows platforms.
    Follow up CLs will populate the user_color on Windows' NativeTheme
    instance from the AccentColorObserver. However on Windows we do not
    want to express this accent color unless the user has explicitly
    opted to do so for their browser. This is in contrast to ChromeOS
    where we expect the accent color to be expressed in the absence of
    any overrides by the browser.
    
    Bug: 1469713
    Change-Id: I3c428d9d2d0f510c44decedef62c16b847ce1d1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4842769
    Reviewed-by: Sean Kau <skau@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193125}

--- ui/color/color_provider_key.cc
+++ ui/color/color_provider_key.cc
@@ -22,9 +22,9 @@ ColorProviderKey::ColorProviderKey()
                        ContrastMode::kNormal,
                        SystemTheme::kDefault,
                        FrameType::kChromium,
+                       UserColorSource::kAccent,
                        absl::nullopt,
                        absl::nullopt,
-                       false,
                        nullptr) {}
 
 ColorProviderKey::ColorProviderKey(
@@ -32,18 +32,18 @@ ColorProviderKey::ColorProviderKey(
     ContrastMode contrast_mode,
     SystemTheme system_theme,
     FrameType frame_type,
+    UserColorSource user_color_source,
     absl::optional<SkColor> user_color,
     absl::optional<SchemeVariant> scheme_variant,
-    bool is_grayscale,
     scoped_refptr<ThemeInitializerSupplier> custom_theme)
     : color_mode(color_mode),
       contrast_mode(contrast_mode),
       elevation_mode(ElevationMode::kLow),
       system_theme(system_theme),
       frame_type(frame_type),
+      user_color_source(user_color_source),
       user_color(user_color),
       scheme_variant(scheme_variant),
-      is_grayscale(is_grayscale),
       custom_theme(std::move(custom_theme)) {}
 
 ColorProviderKey::ColorProviderKey(const ColorProviderKey&) = default;
