9a4d21570e5f3db3daf2f2320284f373772721c3
Olivia Saul <jsaul@google.com>
2023-09-07 20:40:34 +0000

    [AF] UPI cleanup CL 4/5
    This CL essentially reverts the 7 AutofillTable-related files of
    https://crrev.com/c/1761606 from 2019. It removes the table that was
    created in order to store UPI IDs locally to Chrome.
    
    Bug: 1465990
    Change-Id: I151cbd21727542efe81b8c8538f64d704c6c9865
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4848888
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Olivia Saul <jsaul@google.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193765}

--- components/autofill/core/browser/webdata/autofill_table.cc
+++ components/autofill/core/browser/webdata/autofill_table.cc
@@ -267,7 +267,6 @@ constexpr std::string_view kPaymentsCustomerDataTable =
 constexpr std::string_view kCustomerId = "customer_id";
 
 constexpr std::string_view kPaymentsUpiVpaTable = "payments_upi_vpa";
-constexpr std::string_view kVpa = "vpa";
 
 constexpr std::string_view kOfferDataTable = "offer_data";
 constexpr std::string_view kOfferId = "offer_id";
@@ -1168,7 +1167,6 @@ bool AutofillTable::CreateTablesIfNecessary() {
          InitServerCardMetadataTable() && InitServerAddressesTable() &&
          InitServerAddressMetadataTable() && InitAutofillSyncMetadataTable() &&
          InitModelTypeStateTable() && InitPaymentsCustomerDataTable() &&
-         InitPaymentsUPIVPATable() &&
          InitServerCreditCardCloudTokenDataTable() && InitOfferDataTable() &&
          InitOfferEligibleInstrumentTable() && InitOfferMerchantDomainTable() &&
          InitProfileMetadataTable(AutofillProfile::Source::kAccount) &&
@@ -1287,6 +1285,9 @@ bool AutofillTable::MigrateToVersion(int version,
     case 117:
       *update_compatible_version = false;
       return MigrateToVersion117AddProfileObservationColumn();
+    case 118:
+      *update_compatible_version = true;
+      return MigrateToVersion118RemovePaymentsUpiVpaTable();
   }
   return true;
 }
@@ -2881,21 +2882,6 @@ bool AutofillTable::RemoveAllVirtualCardUsageData() {
   return Delete(db_, kVirtualCardUsageDataTable);
 }
 
-bool AutofillTable::InsertUpiId(const std::string& upi_id) {
-  sql::Transaction transaction(db_);
-  if (!transaction.Begin())
-    return false;
-
-  sql::Statement insert_upi_id_statement;
-  InsertBuilder(db_, insert_upi_id_statement, kPaymentsUpiVpaTable, {kVpa});
-  insert_upi_id_statement.BindString(0, upi_id);
-  insert_upi_id_statement.Run();
-
-  transaction.Commit();
-
-  return db_->GetLastChangeCount() > 0;
-}
-
 bool AutofillTable::ClearAllServerData() {
   sql::Transaction transaction(db_);
   if (!transaction.Begin())
@@ -3552,6 +3538,14 @@ bool AutofillTable::MigrateToVersion117AddProfileObservationColumn() {
          transaction.Commit();
 }
 
+bool AutofillTable::MigrateToVersion118RemovePaymentsUpiVpaTable() {
+  sql::Transaction transaction(db_);
+  return transaction.Begin() &&
+         (!db_->DoesTableExist(kPaymentsUpiVpaTable) ||
+          DropTable(db_, kPaymentsUpiVpaTable)) &&
+         transaction.Commit();
+}
+
 bool AutofillTable::AddFormFieldValuesTime(
     const std::vector<FormFieldData>& elements,
     std::vector<AutofillChange>* changes,
@@ -4011,10 +4005,6 @@ bool AutofillTable::InitModelTypeStateTable() {
 bool AutofillTable::InitPaymentsCustomerDataTable() {
   return CreateTableIfNotExists(db_, kPaymentsCustomerDataTable,
                                 {{kCustomerId, "VARCHAR"}});
-}
-
-bool AutofillTable::InitPaymentsUPIVPATable() {
-  return CreateTableIfNotExists(db_, kPaymentsUpiVpaTable, {{kVpa, "VARCHAR"}});
 }
 
 bool AutofillTable::InitServerCreditCardCloudTokenDataTable() {
