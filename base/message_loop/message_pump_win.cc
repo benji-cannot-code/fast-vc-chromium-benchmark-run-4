f2fc0bab60073dadef7b01089597324e3d853bfd
Sorin Jianu <sorin@chromium.org>
2023-09-07 00:25:48 +0000

    Check the result of `message_window_.Create` in MessagePumpForUI ctor.
    If the window can't be created for any reason, the message pump for UI
    won't work correctly. For example, a task executor instantiated
    with this message loop type won't schedule tasks as expected.
    
    Bug: 1476285
    Change-Id: Ia6bb9c10e44e7e7e1de20ddab3f56caced3c0004
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4846652
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193345}

--- base/message_loop/message_pump_win.cc
+++ base/message_loop/message_pump_win.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <type_traits>
 
 #include "base/auto_reset.h"
+#include "base/check.h"
 #include "base/debug/alias.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
@@ -91,7 +92,7 @@ void MessagePumpWin::Quit() {
 MessagePumpForUI::MessagePumpForUI() {
   bool succeeded = message_window_.Create(
       BindRepeating(&MessagePumpForUI::MessageCallback, Unretained(this)));
-  DCHECK(succeeded);
+  CHECK(succeeded);
 }
 
 MessagePumpForUI::~MessagePumpForUI() = default;
