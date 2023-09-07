f6335ab24ae06e001a19a684b68cdb69d8b0a14f
Erica Lee <ericamlee@google.com>
2023-09-07 19:46:53 +0000

    p13n: add SeaPen tile
    Added the tile, which can be clicked to reach a new page. We'll set up
    the real conditions for loading the tile in a later CL.
    
    BUG=b:299107198
    TEST=https://screencast.googleplex.com/cast/NjI5MTk4NjAyODk1MzYwMHw1N2Y0NzNjMy0wMg
    TEST=browser_tests --gtest_filter=*PersonalizationAppComponent*
    
    Change-Id: I4e6d56ce498e9909a760356cf857cba40c6cfb74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4845049
    Commit-Queue: Erica Lee <ericamlee@google.com>
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1193732}

--- ash/webui/personalization_app/resources/js/personalization_router_element.ts
+++ ash/webui/personalization_app/resources/js/personalization_router_element.ts
@@ -28,6 +28,7 @@ export enum Paths {
   GOOGLE_PHOTOS_COLLECTION = '/wallpaper/google-photos',
   LOCAL_COLLECTION = '/wallpaper/local',
   ROOT = '/',
+  SEA_PEN_COLLECTION = '/wallpaper/sea-pen',
   USER = '/user',
 }
 
