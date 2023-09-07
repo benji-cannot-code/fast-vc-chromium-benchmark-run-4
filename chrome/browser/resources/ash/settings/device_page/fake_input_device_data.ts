195eb1b8fbe4770ad83dbbf4fe2d7cc0c14e5c85
Michael Checo <michaelcheco@google.com>
2023-09-07 18:42:22 +0000

    [F-Keys]: Display shortcut options in f11/f12 dropdowns
    - A follow-up CL will check the state of `topRowAreFkeys` to
      conditionally display Search/Launcher in the shortcut options.
    
    Screenshots:
     - F11: http://screen/6UzSMAmb587zX76
     - F12: http://screen/54k8DRJbwwximdA
    
    Bug: b/297262494
    Test: browser_tests --gtest_filter=*OSSettingsDevicePageFK*
    Change-Id: Ib5cb369e1dcd1266a4e27cf05fb2a5b84775ff9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4814810
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1193705}

--- chrome/browser/resources/ash/settings/device_page/fake_input_device_data.ts
+++ chrome/browser/resources/ash/settings/device_page/fake_input_device_data.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {AcceleratorAction, ActionChoice, CustomizableButton, ExtendedFkeysModifier, GraphicsTablet, Keyboard, MetaKey, ModifierKey, Mouse, PointingStick, SimulateRightClickModifier, SixPackKeyInfo, SixPackShortcutModifier, Stylus, Touchpad, Vkey} from './input_device_settings_types.js';
+import {AcceleratorAction, ActionChoice, CustomizableButton, ExtendedFkeysModifier, GraphicsTablet, Keyboard, MetaKey, ModifierKey, Mouse, PointingStick, SimulateRightClickModifier, SixPackKeyInfo, SixPackShortcutModifier, Stylus, TopRowActionKey, Touchpad, Vkey} from './input_device_settings_types.js';
 
 const defaultSixPackKeyRemappings: SixPackKeyInfo = {
   pageDown: SixPackShortcutModifier.kSearch,
@@ -61,6 +61,18 @@ export const fakeKeyboards: Keyboard[] = [
       f11: ExtendedFkeysModifier.ALT,
       f12: ExtendedFkeysModifier.SHIFT,
     },
+    topRowActionKeys: [
+      TopRowActionKey.BACK,
+      TopRowActionKey.FORWARD,
+      TopRowActionKey.REFRESH,
+      TopRowActionKey.FULLSCREEN,
+      TopRowActionKey.OVERVIEW,
+      TopRowActionKey.SCREEN_BRIGHTNESS_DOWN,
+      TopRowActionKey.SCREEN_BRIGHTNESS_UP,
+      TopRowActionKey.VOLUME_MUTE,
+      TopRowActionKey.VOLUME_DOWN,
+      TopRowActionKey.VOLUME_UP,
+    ],
   },
   {
     id: 8,
