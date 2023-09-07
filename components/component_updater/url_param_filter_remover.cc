50ef24626274866a6bb266ccf9142ad0a285b2d2
Joshua Pawlicki <waffles@chromium.org>
2023-09-07 17:39:37 +0000

    Component Updater: Fix component cleanups.
    The helper function used by this component cleanup code cleans version-
    named siblings and empty parents of the path it is passed, however all
    callers are expecting it to clean the path passed. They should use a
    recursive delete instead, and the function is removed.
    
    This appears to have been buggy since it was added to the codebase; I
    don't see any example of it having been used properly, and locally, I
    can confirm that it is failing to clean deprecated components.
    
    Fixed: 1480025
    Change-Id: Icc11380941e0a673ca4db08a68be0a622dcf3074
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4849988
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193670}

--- components/component_updater/url_param_filter_remover.cc
+++ components/component_updater/url_param_filter_remover.cc
@@ -5,11 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/component_updater/url_param_filter_remover.h"
 
+#include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
 #include "base/task/thread_pool.h"
-
-#include "components/component_updater/component_updater_utils.h"
 
 namespace component_updater {
 
@@ -17,7 +16,7 @@ void DeleteUrlParamFilter(const base::FilePath& user_data_dir) {
   base::ThreadPool::PostTask(
       FROM_HERE, {base::TaskPriority::BEST_EFFORT, base::MayBlock()},
       base::BindOnce(
-          &DeleteFilesAndParentDirectory,
+          base::IgnoreResult(&base::DeletePathRecursively),
           user_data_dir.Append(FILE_PATH_LITERAL("UrlParamClassifications"))));
 }
 
