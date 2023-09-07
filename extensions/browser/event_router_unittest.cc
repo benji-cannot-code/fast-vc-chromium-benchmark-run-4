dcf720d7e87671c8b5ea8209c8a3bc6704ce723f
Yi Gu <yigu@chromium.org>
2023-09-07 14:35:09 +0000

    [Gardener] Dropping https from linked bugs
    This is a follow up patch to align with chromium coding style:
    https://chromium.googlesource.com/chromium/src/+/HEAD/docs/ios/style.md#todo-comments
    
    Change-Id: Ibd318bf0647ac7b051ed998020092ee0f2907dca
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4850067
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Owners-Override: Yi Gu <yigu@chromium.org>
    Auto-Submit: Yi Gu <yigu@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193568}

--- extensions/browser/event_router_unittest.cc
+++ extensions/browser/event_router_unittest.cc
@@ -581,7 +581,7 @@ TEST_F(EventRouterTest, TestReportEvent) {
 }
 
 // Tests adding and removing events with filters.
-// TODO(https://crbug.com/1479954): test is flaky across platforms.
+// TODO(crbug.com/1479954): test is flaky across platforms.
 TEST_P(EventRouterFilterTest, DISABLED_Basic) {
   // For the purpose of this test, "." is important in |event_name| as it
   // exercises the code path that uses |event_name| as a key in
@@ -646,7 +646,7 @@ TEST_P(EventRouterFilterTest, DISABLED_Basic) {
   ASSERT_FALSE(ContainsFilter(kExtensionId, kEventName, filters[2]));
 }
 
-// TODO(https://crbug.com/1479954): test is flaky across platforms.
+// TODO(crbug.com/1479954): test is flaky across platforms.
 TEST_P(EventRouterFilterTest, DISABLED_URLBasedFilteredEventListener) {
   const std::string kEventName = "windows.onRemoved";
   const GURL kUrl("chrome-untrusted://terminal");
@@ -754,7 +754,7 @@ TEST_F(EventRouterDispatchTest, TestDispatch) {
   EXPECT_EQ(0u, observer.dispatched_events().size());
 }
 
-// TODO(https://crbug.com/1479954): test is flaky across platforms.
+// TODO(crbug.com/1479954): test is flaky across platforms.
 TEST_F(EventRouterDispatchTest, DISABLED_TestDispatchCallback) {
   std::string ext1 = "ext1";
   std::string ext2 = "ext2";
