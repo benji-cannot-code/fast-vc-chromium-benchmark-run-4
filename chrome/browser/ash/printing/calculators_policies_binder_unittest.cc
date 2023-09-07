99c2220476f0606e6d40f9a02b1033f3b7f5446a
Sparik Hayrapetyan <ust@google.com>
2023-09-07 17:35:42 +0000

    Deduplicate enterprise printers in EnterprisePrintersProvider
    The same printer can be pushed both with user and device policy. This
    scenario currently triggers a DCHECK in PrintersMap::Insert(). This CL
    makes sure EnterprisePrintersProvider removes duplicate printers before
    passing them down to observers.
    
    Bug: b/237832781
    Change-Id: I46b4e2a824483f6a06783fa1b45fba6bed810430
    Fixed: b/237832781
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4843171
    Commit-Queue: Sparik Hayrapetyan <ust@google.com>
    Reviewed-by: Piotr Pawliczek <pawliczek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193666}

--- chrome/browser/ash/printing/calculators_policies_binder_unittest.cc
+++ chrome/browser/ash/printing/calculators_policies_binder_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/settings/scoped_testing_cros_settings.h"
 #include "chrome/browser/ash/settings/stub_cros_settings_provider.h"
 #include "chrome/common/pref_names.h"
+#include "chromeos/printing/printer_configuration.h"
 #include "components/prefs/testing_pref_service.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
