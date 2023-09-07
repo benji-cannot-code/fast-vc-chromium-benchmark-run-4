1e13a976c91aa086593dccde93fe994abc31121a
Yi Gu <yigu@chromium.org>
2023-09-07 17:40:48 +0000

    [Gardener] Disable All/SocketBIOAdapterTest.ReadEOFAsync/0 on Android
    See linked bug for details.
    
    Change-Id: Icf5c2ee8d0450753eba5d8bbac2f7775db834b51
    Bug: 1480024
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4848629
    Owners-Override: Yi Gu <yigu@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Auto-Submit: Yi Gu <yigu@chromium.org>
    Reviewed-by: David Bienvenu <davidbienvenu@google.com>
    Commit-Queue: David Bienvenu <davidbienvenu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1193672}

--- net/socket/socket_bio_adapter_unittest.cc
+++ net/socket/socket_bio_adapter_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/memory/raw_ptr.h"
 #include "base/run_loop.h"
+#include "build/build_config.h"
 #include "crypto/openssl_util.h"
 #include "net/base/address_list.h"
 #include "net/base/completion_once_callback.h"
@@ -284,8 +285,14 @@ TEST_P(SocketBIOAdapterTest, ReadEOFSync) {
   ExpectReadError(adapter->bio(), ERR_CONNECTION_CLOSED, tracer);
 }
 
+#if BUILDFLAG(IS_ANDROID)
 // Test that asynchronous EOF is mapped to ERR_CONNECTION_CLOSED.
-TEST_P(SocketBIOAdapterTest, ReadEOFAsync) {
+// TODO(crbug.com/1480024): Test is flaky on Android.
+#define MAYBE_ReadEOFAsync DISABLED_ReadEOFAsync
+#else
+#define MAYBE_ReadEOFAsync ReadEOFAsync
+#endif
+TEST_P(SocketBIOAdapterTest, MAYBE_ReadEOFAsync) {
   crypto::OpenSSLErrStackTracer tracer(FROM_HERE);
 
   MockRead reads[] = {
