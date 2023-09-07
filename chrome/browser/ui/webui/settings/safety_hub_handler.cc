dd5037e2d6c9364b7523db7084f489cc9d1aec53
Tom Van Goethem <tov@chromium.org>
2023-09-07 23:21:33 +0000

    [SafetyCheck] Disable SafetyCheck services and review in guest mode
    This CL adds additional checks to ensure that the permission review UI
    is not shown when it shouldn't, e.g. when the user is in guest mode.
    Additionally, the notification review service and unused site
    permissions service are disabled in guest mode.
    
    Bug: 1477150, 1418376, 1345920
    Low-Coverage-Reason: Added checks for nullptr
    Change-Id: I58cd552f58d9fe3528915f9f420d248f037ec8b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4839796
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Commit-Queue: Tom Van Goethem <tov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193835}

--- chrome/browser/ui/webui/settings/safety_hub_handler.cc
+++ chrome/browser/ui/webui/settings/safety_hub_handler.cc
@@ -143,6 +143,7 @@ void SafetyHubHandler::HandleAllowPermissionsAgainForUnusedSite(
 
   UnusedSitePermissionsService* service =
       UnusedSitePermissionsServiceFactory::GetForProfile(profile_);
+  CHECK(service);
 
   url::Origin origin = url::Origin::Create(GURL(origin_str));
 
@@ -159,6 +160,7 @@ void SafetyHubHandler::HandleUndoAllowPermissionsAgainForUnusedSite(
       GetUnusedSitePermissionsFromDict(args[0].GetDict());
   UnusedSitePermissionsService* service =
       UnusedSitePermissionsServiceFactory::GetForProfile(profile_);
+  CHECK(service);
 
   service->UndoRegrantPermissionsForOrigin(permissions, constraints, origin);
 
@@ -169,6 +171,7 @@ void SafetyHubHandler::HandleAcknowledgeRevokedUnusedSitePermissionsList(
     const base::Value::List& args) {
   UnusedSitePermissionsService* service =
       UnusedSitePermissionsServiceFactory::GetForProfile(profile_);
+  CHECK(service);
   service->ClearRevokedPermissionsList();
 
   SendUnusedSitePermissionsReviewList();
@@ -182,6 +185,7 @@ void SafetyHubHandler::HandleUndoAcknowledgeRevokedUnusedSitePermissionsList(
   const base::Value::List& unused_site_permissions_list = args[0].GetList();
   UnusedSitePermissionsService* service =
       UnusedSitePermissionsServiceFactory::GetForProfile(profile_);
+  CHECK(service);
 
   for (const auto& unused_site_permissions_js : unused_site_permissions_list) {
     CHECK(unused_site_permissions_js.is_dict());
@@ -264,6 +268,10 @@ void SafetyHubHandler::HandleGetNotificationPermissionReviewList(
       NotificationPermissionsReviewServiceFactory::GetForProfile(profile_);
   DCHECK(service);
 
+  if (!service) {
+    RejectJavascriptCallback(callback_id, base::Value());
+  }
+
   base::Value::List result =
       service->PopulateNotificationPermissionReviewData(profile_);
 
@@ -277,7 +285,7 @@ void SafetyHubHandler::HandleIgnoreOriginsForNotificationPermissionReview(
 
   NotificationPermissionsReviewService* service =
       NotificationPermissionsReviewServiceFactory::GetForProfile(profile_);
-  DCHECK(service);
+  CHECK(service);
 
   for (const auto& origin : origins) {
     const ContentSettingsPattern primary_pattern =
@@ -349,7 +357,7 @@ void SafetyHubHandler::HandleUndoIgnoreOriginsForNotificationPermissionReview(
   const base::Value::List& origins = args[0].GetList();
   NotificationPermissionsReviewService* service =
       NotificationPermissionsReviewServiceFactory::GetForProfile(profile_);
-  DCHECK(service);
+  CHECK(service);
 
   for (const auto& origin : origins) {
     const ContentSettingsPattern& primary_pattern =
@@ -478,7 +486,9 @@ void SafetyHubHandler::SendUnusedSitePermissionsReviewList() {
 void SafetyHubHandler::SendNotificationPermissionReviewList() {
   NotificationPermissionsReviewService* service =
       NotificationPermissionsReviewServiceFactory::GetForProfile(profile_);
-  CHECK(service);
+  if (!service) {
+    return;
+  }
 
   base::Value::List result =
       service->PopulateNotificationPermissionReviewData(profile_);
