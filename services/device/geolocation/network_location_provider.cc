a46763e8ac4be8dbea27836a5b6cdee144e8e819
Matt Reynolds <mattreynolds@google.com>
2023-09-07 17:21:18 +0000

    geolocation: Add network request and response data to internals
    Adds new tables to the chrome://location-internals WebUI to show the
    most recent request made to the location service and the received
    position estimate.
    
    Bug: 1455586
    Change-Id: I1f409798c0640c23dd23c0acd7f9b74a2d1a5ffd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4764267
    Reviewed-by: Alvin Ji <alvinji@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193655}

--- services/device/geolocation/network_location_provider.cc
+++ services/device/geolocation/network_location_provider.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <iterator>
 #include <utility>
 
+#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/location.h"
 #include "base/memory/scoped_refptr.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/device_event_log/device_event_log.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "services/device/geolocation/position_cache.h"
+#include "services/device/public/cpp/device_features.h"
 #include "services/device/public/cpp/geolocation/geoposition.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
@@ -47,7 +49,9 @@ NetworkLocationProvider::NetworkLocationProvider(
     const scoped_refptr<base::SingleThreadTaskRunner> main_task_runner,
     const std::string& api_key,
     PositionCache* position_cache,
-    base::RepeatingClosure internals_updated_closure)
+    base::RepeatingClosure internals_updated_closure,
+    NetworkRequestCallback network_request_callback,
+    NetworkResponseCallback network_response_callback)
     : wifi_data_update_callback_(
           base::BindRepeating(&NetworkLocationProvider::OnWifiDataUpdate,
                               base::Unretained(this))),
@@ -60,9 +64,13 @@ NetworkLocationProvider::NetworkLocationProvider(
           api_key,
           base::BindRepeating(&NetworkLocationProvider::OnLocationResponse,
                               base::Unretained(this)))),
-      internals_updated_closure_(std::move(internals_updated_closure)) {
+      internals_updated_closure_(std::move(internals_updated_closure)),
+      network_request_callback_(std::move(network_request_callback)),
+      network_response_callback_(std::move(network_response_callback)) {
   DCHECK(position_cache_);
   CHECK(internals_updated_closure_);
+  CHECK(network_request_callback_);
+  CHECK(network_response_callback_);
 #if BUILDFLAG(IS_APPLE)
   DCHECK(geolocation_manager);
   geolocation_manager_ = geolocation_manager;
@@ -201,7 +209,8 @@ void NetworkLocationProvider::OnWifiDataUpdate() {
 void NetworkLocationProvider::OnLocationResponse(
     mojom::GeopositionResultPtr result,
     bool server_error,
-    const WifiData& wifi_data) {
+    const WifiData& wifi_data,
+    mojom::NetworkLocationResponsePtr response_data) {
   DCHECK(thread_checker_.CalledOnValidThread());
   GEOLOCATION_LOG(DEBUG) << "Got new position";
   // Record the position and update our cache.
@@ -215,6 +224,10 @@ void NetworkLocationProvider::OnLocationResponse(
     location_provider_update_callback_.Run(this, std::move(result));
   }
   internals_updated_closure_.Run();
+
+  if (base::FeatureList::IsEnabled(features::kGeolocationDiagnosticsObserver)) {
+    network_response_callback_.Run(std::move(response_data));
+  }
 }
 
 void NetworkLocationProvider::StartProvider(bool high_accuracy) {
@@ -352,6 +365,10 @@ void NetworkLocationProvider::RequestPosition() {
       })");
   request_->MakeRequest(wifi_data_, wifi_timestamp_,
                         partial_traffic_annotation);
+
+  if (base::FeatureList::IsEnabled(features::kGeolocationDiagnosticsObserver)) {
+    network_request_callback_.Run(request_->GetRequestDataForDiagnostics());
+  }
 }
 
 bool NetworkLocationProvider::IsStarted() const {
