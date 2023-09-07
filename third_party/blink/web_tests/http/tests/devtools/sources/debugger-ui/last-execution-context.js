d30365aab25db5362f0fa08a96dd82d01adb7306
Simon Zünd <szuend@chromium.org>
2023-09-07 12:27:13 +0000

    [devtools] Replace Main.* global with import
    R=wolfi@chromium.org
    
    Bug: 1442410
    Change-Id: Icedc0eac3e3c90af2b58e310b83b7c3740635301
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4846967
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193524}

--- third_party/blink/web_tests/http/tests/devtools/sources/debugger-ui/last-execution-context.js
+++ third_party/blink/web_tests/http/tests/devtools/sources/debugger-ui/last-execution-context.js
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {TestRunner} from 'test_runner';
 import {SDKTestRunner} from 'sdk_test_runner';
 
+import * as Main from 'devtools/entrypoints/main/main.js';
 import * as SDK from 'devtools/core/sdk/sdk.js';
 
 (async function() {
@@ -16,7 +17,7 @@ import * as SDK from 'devtools/core/sdk/sdk.js';
   var context = new UI.Context();
   context.addFlavorChangeListener(SDK.RuntimeModel.ExecutionContext, executionContextChanged, this);
   context.addFlavorChangeListener(SDK.Target.Target, targetChanged, this);
-  new Main.ExecutionContextSelector(SDK.TargetManager.TargetManager.instance(), context);
+  new Main.ExecutionContextSelector.ExecutionContextSelector(SDK.TargetManager.TargetManager.instance(), context);
 
   function executionContextChanged(event) {
     var executionContext = event.data;
