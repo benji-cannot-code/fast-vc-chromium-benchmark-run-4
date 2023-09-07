dfe45e613c84904715fe835cd2d030e7d3aebf50
Daniel Rubery <drubery@chromium.org>
2023-09-07 23:31:36 +0000

    Remove references to chrome_cleaner and software_reset_prompt
    These features are no longer actively maintained. Since M112, the
    feature has been disabled in Chrome. This CL removes all references to
    the unused code, while the dependent CL is a set of pure file deletions.
    
    Bug: 1439998
    Change-Id: Iec01be4090c6a9118e9f2b65cffdddf2c0b10f20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4836600
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193839}

--- chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
@@ -141,7 +141,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(IS_WIN)
-#include "chrome/browser/safe_browsing/chrome_cleaner/srt_field_trial_win.h"
 #include "device/fido/win/webauthn_api.h"
 
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
