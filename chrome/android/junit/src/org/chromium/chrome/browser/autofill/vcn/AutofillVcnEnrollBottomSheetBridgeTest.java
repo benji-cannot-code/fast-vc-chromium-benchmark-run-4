37d21be3fad26545690bc92131dedcefa2d1502e
Rouslan Solomakhin <rouslan@chromium.org>
2023-09-07 18:41:12 +0000

    [Payments] Set drag handlebar color for virtual card enroll sheet
    Bug: 1479982
    Change-Id: Ib9a71bc574d8a1cc7ee8006b3d14e8ee11c73e7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4847688
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193703}

--- chrome/android/junit/src/org/chromium/chrome/browser/autofill/vcn/AutofillVcnEnrollBottomSheetBridgeTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/autofill/vcn/AutofillVcnEnrollBottomSheetBridgeTest.java
@@ -31,6 +31,7 @@ import org.chromium.base.FeatureList;
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.JniMocker;
+import org.chromium.chrome.R;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.layouts.LayoutStateProvider;
 import org.chromium.chrome.browser.layouts.LayoutType;
@@ -79,6 +80,7 @@ public final class AutofillVcnEnrollBottomSheetBridgeTest {
         MockitoAnnotations.initMocks(this);
         mJniMocker.mock(AutofillVcnEnrollBottomSheetBridgeJni.TEST_HOOKS, mBridgeNatives);
         Activity activity = Robolectric.buildActivity(Activity.class).create().get();
+        activity.setTheme(R.style.Theme_BrowserUI_DayNight);
         mWindow = new WindowAndroid(activity);
         BottomSheetControllerFactory.attach(mWindow, mBottomSheetController);
         mBridge = new AutofillVcnEnrollBottomSheetBridge();
