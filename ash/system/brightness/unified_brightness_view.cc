b791ef364954607fbf14661ce81156ec06a06b68
Sylvie Liu <sylvieliu@chromium.org>
2023-09-07 17:48:51 +0000

    Enabled night light button and feature tile in the locked screen
    Screenshot:
    https://screenshot.googleplex.com/xnQuPwZLvMtVR8r
    
    Bug: b:294868714
    Change-Id: If6fbf0c4a2e0b2da2978d850c7927bad75f31c19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4812180
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Commit-Queue: Sylvie Liu <sylvieliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193677}

--- ash/system/brightness/unified_brightness_view.cc
+++ ash/system/brightness/unified_brightness_view.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/ash_color_id.h"
 #include "ash/system/brightness/unified_brightness_slider_controller.h"
 #include "ash/system/night_light/night_light_controller_impl.h"
+#include "ash/system/tray/tray_popup_utils.h"
 #include "ash/wm/screen_pinning_controller.h"
 #include "ash/wm/window_state.h"
 #include "base/functional/bind.h"
@@ -82,7 +83,7 @@ UnifiedBrightnessView::UnifiedBrightnessView(
     // enabled. In the lock screen and sign-in screen, the `night_light_button_`
     // should be disabled.
     night_light_button_->SetEnabled(
-        Shell::Get()->session_controller()->ShouldEnableSettings());
+        TrayPopupUtils::CanShowNightLightFeatureTile());
     night_light_button_->SetToggled(toggled);
 
     more_button_ = AddChildView(std::make_unique<IconButton>(
@@ -140,7 +141,7 @@ void UnifiedBrightnessView::OnNightLightButtonPressed() {
 
 void UnifiedBrightnessView::UpdateNightLightButton() {
   night_light_button_->SetEnabled(
-      Shell::Get()->session_controller()->ShouldEnableSettings());
+      TrayPopupUtils::CanShowNightLightFeatureTile());
   const bool toggled = night_light_controller_->GetEnabled();
 
   // Sets `night_light_button_` toggle state to update its icon, icon color,
