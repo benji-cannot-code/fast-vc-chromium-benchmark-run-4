2df8a81736d9bbd92a67e742b40b33b7959889df
“Kavita <kavitasoni@chromium.org>
2023-09-07 19:42:43 +0000

    [Clank][UX Enhancements] Upstream Save Card Expiration Date Fix UX
    This CL adds a new xml layout ‘icon_after_title_view.xml’ for a custom
    title view. The UX enhancement is supported by a feature flag and
    affects Upstream Save card expiration date fix dialog’s title and
    title icon view.
    
    AutofillSaveCardPromptBase uses autofill_save_card_base_layout.xml for
    a custom view. To introduce a custom title view, this CL adds a ViewStub
     in a parent LinearLayout and puts the LinearLayout with other parts of
     dialog below the new ViewStub to not affect the custom title
    view with different padding and margin settings.
    
    If the feature flag is enabled AutofillSaveCardPromptBase inflates
    that ViewStub with icon_after_title_view.xml else uses the default
     title view. This dialog's default title view is set using a
    PropertyModel.Builder with ModalDialogProperties.TITLE and
     ModalDialogProperties.TITLE_ICON. ModalDialogManager uses
    modal_dialog_title.xml for the default title view.
    
    Before: https://screenshot.googleplex.com/Asgi7HBm3hdH5QX
    
    After: https://storage.mtls.cloud.google.com/chromium-translation-screenshots/Gpay_expiration_date_fix_ux_after.png?authuser=0
    
    Low-Coverage-Reason: WIP CL to add UTs (https://crrev.com/c/4822348).
    Bug: 1467523
    Change-Id: Ia46b27b3e1f2775c42341baedb0905910f178806
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4811477
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Commit-Queue: Kavita Soni <kavitasoni@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193728}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/AutofillNameFixFlowPrompt.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/AutofillNameFixFlowPrompt.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.autofill;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.view.View;
@@ -75,8 +76,9 @@ public class AutofillNameFixFlowPrompt
     private AutofillNameFixFlowPrompt(Context context, AutofillNameFixFlowPromptDelegate delegate,
             String inferredName, String title, int drawableId, String confirmButtonLabel,
             boolean filledConfirmButton) {
-        super(context, delegate, R.layout.autofill_name_fixflow, title, drawableId,
-                confirmButtonLabel, filledConfirmButton);
+        super(context, delegate, R.layout.autofill_name_fixflow,
+                /*customTitleLayoutId=*/Resources.ID_NULL, title, drawableId, confirmButtonLabel,
+                filledConfirmButton);
         mDelegate = delegate;
         // Dialog of infobar doesn't show any details of the cc.
         mDialogView.findViewById(R.id.cc_details).setVisibility(View.GONE);
