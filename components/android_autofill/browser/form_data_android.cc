0e531cd626949655727f9217d7dab01df2aaef42
Jan Keitel <jkeitel@google.com>
2023-09-07 06:23:09 +0000

    Introduce interface for FormFieldDataAndroid's java bridge.
    This CL is a no-op with regards to functionality. Its purpose is to
    separate C++ logic from C++ <-> Java bridge code for FormFieldData.
    The aim is to write unit tests for FormFieldData with a mocked bridge,
    as FormFieldData might grow in complexity.
    
    Bug: 1478934
    Change-Id: Ifbb92fe49a77234f8e72f7f38d1b6c3ae93e68ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4841636
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193439}

--- components/android_autofill/browser/form_data_android.cc
+++ components/android_autofill/browser/form_data_android.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_array.h"
 #include "base/android/jni_string.h"
 #include "components/android_autofill/browser/form_field_data_android.h"
+#include "components/android_autofill/browser/form_field_data_android_bridge_impl.h"
 #include "components/android_autofill/browser/jni_headers/FormData_jni.h"
 #include "components/autofill/core/browser/form_structure.h"
 #include "components/autofill/core/common/form_field_data.h"
@@ -43,7 +44,9 @@ ScopedJavaLocalRef<jobject> FormDataAndroid::GetJavaPeer(
     fields_.clear();
     fields_.reserve(form_.fields.size());
     for (FormFieldData& field : form_.fields) {
-      fields_.push_back(std::make_unique<FormFieldDataAndroid>(&field));
+      // TODO(crbug.com/1478934): Use a factory for the bridge.
+      fields_.push_back(std::make_unique<FormFieldDataAndroid>(
+          std::make_unique<FormFieldDataAndroidBridgeImpl>(), &field));
     }
 
     if (form_structure)
@@ -141,9 +144,10 @@ void FormDataAndroid::UpdateFieldTypes(const FormStructure& form_structure) {
           static_cast<ServerFieldType>(prediction.type()));
     }
     form_field_data_android->get()->UpdateAutofillTypes(
-        AutofillType(autofill_field->heuristic_type()),
-        AutofillType(autofill_field->server_type()),
-        autofill_field->ComputedType(), server_predictions);
+        FormFieldDataAndroid::FieldTypes(
+            AutofillType(autofill_field->heuristic_type()),
+            AutofillType(autofill_field->server_type()),
+            autofill_field->ComputedType(), std::move(server_predictions)));
     if (++form_field_data_android == fields_.end())
       break;
   }
