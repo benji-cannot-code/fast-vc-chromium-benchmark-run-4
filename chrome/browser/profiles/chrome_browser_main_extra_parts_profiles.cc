c010934789d3ebf5b677e62e3c4fc788a0002118
Fiona Macintosh <fmacintosh@google.com>
2023-09-07 16:05:20 +0000

    Create base tracking protection settings service with function for DNT
    Bug: b:298117702
    Change-Id: Ie746a7a9091b191b4ff8768edebc27d186bec6ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4845665
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1193615}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -132,6 +132,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_service_factory.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_settings_factory.h"
 #include "chrome/browser/privacy_sandbox/tracking_protection_onboarding_factory.h"
+#include "chrome/browser/privacy_sandbox/tracking_protection_settings_factory.h"
 #include "chrome/browser/profile_resetter/triggered_profile_resetter_factory.h"
 #include "chrome/browser/profiles/renderer_updater_factory.h"
 #include "chrome/browser/push_messaging/push_messaging_service_factory.h"
@@ -944,7 +945,6 @@ void ChromeBrowserMainExtraPartsProfiles::
   PrivacyMetricsServiceFactory::GetInstance();
   PrivacySandboxServiceFactory::GetInstance();
   PrivacySandboxSettingsFactory::GetInstance();
-  TrackingProtectionOnboardingFactory::GetInstance();
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
   ProfileAccountManagerFactory::GetInstance();
 #endif
@@ -1099,6 +1099,8 @@ void ChromeBrowserMainExtraPartsProfiles::
   ToolbarActionsModelFactory::GetInstance();
 #endif
   TopSitesFactory::GetInstance();
+  TrackingProtectionOnboardingFactory::GetInstance();
+  TrackingProtectionSettingsFactory::GetInstance();
   translate::TranslateRankerFactory::GetInstance();
   TranslateModelServiceFactory::GetInstance();
 #if !BUILDFLAG(IS_ANDROID)
