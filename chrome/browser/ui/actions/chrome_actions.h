0d83ffb284fcbbae1567c3f257e9dae2b408557b
Allen Bauer <kylixrd@chromium.org>
2023-09-07 20:49:47 +0000

    Integrate action creation into browser-startup.
    Change-Id: Ie7a18963576f184c5a664b7c4ffe77d0683add23
    Bug: 1472023
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4832634
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Joseph Park <josephjoopark@chromium.org>
    Reviewed-by: Elaine Chien <elainechien@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193771}

---
+++ chrome/browser/ui/actions/chrome_actions.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_ACTIONS_CHROME_ACTIONS_H_
+#define CHROME_BROWSER_UI_ACTIONS_CHROME_ACTIONS_H_
+
+namespace actions {
+class ActionManager;
+}  // namespace actions
+
+// Called during browser startup. Default command actions are created/
+// initialized here.
+void InitializeActions(actions::ActionManager* manager);
+
+#endif  // CHROME_BROWSER_UI_ACTIONS_CHROME_ACTIONS_H_
