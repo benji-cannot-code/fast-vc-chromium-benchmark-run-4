dda77802365f8d21e5fd4ce78283f72b45f7f860
François Beaufort <beaufort.francois@gmail.com>
2023-09-07 10:53:16 +0000

    Implement the DisplayMediaStreamOptions monitorTypeSurfaces
    This adds the ability for web applications to provide a hint, indicating
    to the user agent whether the application, upon calling getDisplayMedia,
    wishes the user has the option to choose the display surfaces whose type
    is monitor (screen).
    
    Spec PR: https://github.com/w3c/mediacapture-screen-share/pull/274
    Intent to ship: https://groups.google.com/a/chromium.org/g/blink-dev/c/t3kqlI58U8Q/m/EDnovcDgAAAJ
    
    Spec: https://w3c.github.io/mediacapture-screen-share/#dom-displaymediastreamoptions-monitortypesurfaces
    Demo: https://screen-sharing-controls.glitch.me/?monitorTypeSurfaces=exclude
    Bug: 1477706
    Change-Id: I25ffabe546ddc778f660834e44a9dab15494e92a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4831171
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1193498}

--- third_party/blink/renderer/modules/mediastream/media_devices.cc
+++ third_party/blink/renderer/modules/mediastream/media_devices.cc
@@ -331,6 +331,9 @@ MediaStreamConstraints* ToMediaStreamConstraints(
   if (source->hasSurfaceSwitching()) {
     constraints->setSurfaceSwitching(source->surfaceSwitching());
   }
+  if (source->hasMonitorTypeSurfaces()) {
+    constraints->setMonitorTypeSurfaces(source->monitorTypeSurfaces());
+  }
   return constraints;
 }
 
