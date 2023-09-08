a16353c24e55253f300372b38fab3f767453cf59
Timofey Chudakov <tchudakov@google.com>
2023-09-08 16:21:19 +0000

    [Desktop][I18n] Forwards all supported AutofillProfile fields.
    Address internationalization means every AutofillProfile field
    can potentially be displayed to the user in the editor. Thus, every
    supported field is forwarded to the JS side.
    
    Bug: 1441904
    Change-Id: Id273124c99e6d19a12fd7369c5f51191fe02f477
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4773359
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1194154}

--- chrome/browser/resources/settings/autofill_page/address_edit_dialog_components.ts
+++ chrome/browser/resources/settings/autofill_page/address_edit_dialog_components.ts
@@ -97,6 +97,10 @@ export class AddressComponentUi {
     return !isValueEmpty(this.value);
   }
 
+  get fieldType(): chrome.autofillPrivate.ServerFieldType {
+    return this.fieldType_;
+  }
+
   makeValidatable(): void {
     this.isValidatable_ = true;
   }
