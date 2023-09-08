5ad0ad65d17c25824816c169bb41a49b202daa68
Christian Flach <cmfcmf@chromium.org>
2023-09-08 13:59:33 +0000

    iwa: Wait for ongoing updates to complete before serving requests
    If an update is in the process of being applied, pause all requests to
    the IWA until the update is applied. There are two cases where this is
    relevant:
    - Normally, we wait for all windows of an IWA to close, and only then
      kick-off the update apply process. However, if the user were to
      re-open an IWA window during the update apply process, we need to
      delay loading of the window contents until after the update has
      finished applying.
    - On startup, if updates from the previous browser session are still
      pending (possibly because the user never closed the windows of the
      IWA), then we queue the pending updates to be applied. However, if
      either the user or the browser opens an IWA window while we apply
      updates, we need to wait for that to finish, similar to the case
      above.
    
                         currently disabled due to crbug.com/1479463.
    
    Low-Coverage-Reason: There is a test for Ash, but the Lacros test is
    Bug: 1444407
    Change-Id: I0fdf9579e72735b42dd3923ed6bfd03888b55488
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4831295
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Commit-Queue: Christian Flach <cmfcmf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194079}

--- chrome/browser/ui/web_applications/test/isolated_web_app_test_utils.cc
+++ chrome/browser/ui/web_applications/test/isolated_web_app_test_utils.cc
@@ -68,8 +68,9 @@ Browser* IsolatedWebAppBrowserTestHarness::GetBrowserFromFrame(
 }
 
 content::RenderFrameHost* IsolatedWebAppBrowserTestHarness::OpenApp(
-    const AppId& app_id) {
-  return OpenIsolatedWebApp(profile(), app_id);
+    const AppId& app_id,
+    base::StringPiece path) {
+  return OpenIsolatedWebApp(profile(), app_id, path);
 }
 
 content::RenderFrameHost*
@@ -117,13 +118,14 @@ IsolatedWebAppUrlInfo InstallDevModeProxyIsolatedWebApp(
 }
 
 content::RenderFrameHost* OpenIsolatedWebApp(Profile* profile,
-                                             const AppId& app_id) {
+                                             const AppId& app_id,
+                                             base::StringPiece path) {
   WebAppRegistrar& registrar =
       WebAppProvider::GetForWebApps(profile)->registrar_unsafe();
   const WebApp* app = registrar.GetAppById(app_id);
   EXPECT_TRUE(app);
 
-  NavigateParams params(profile, app->start_url(),
+  NavigateParams params(profile, app->start_url().Resolve(path),
                         ui::PAGE_TRANSITION_GENERATED);
   params.app_id = app->app_id();
   params.window_action = NavigateParams::SHOW_WINDOW;
