1bdf228e34732072fa8cb06ceb53aab13a03eaa5
Filipa Senra <fsenra@google.com>
2023-09-08 09:04:14 +0000

    [PrivacySandbox] Remove deprecated PSExtensionsAPI for extensions
    Will not remove the DepecrationIssue for PrivacySandboxExtensionsAPI
    so there is an example of a deprecation issue in the Devtools Tab on
    the browser side.
    
    Fixed: b:263568309, 1470295
    Change-Id: Iafc160b9fc9116b656fa1d835a8d696a1fdd12ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4847681
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1193985}

--- chrome/test/data/extensions/api_test/preference/standard/test.js
+++ chrome/test/data/extensions/api_test/preference/standard/test.js
@@ -26,7 +26,6 @@ var preferences_to_test = [
     root: chrome.privacy.websites,
     preferences: {
       thirdPartyCookiesAllowed: false,
-      privacySandboxEnabled: true,
       hyperlinkAuditingEnabled: false,
       referrersEnabled: false,
       doNotTrackEnabled: false,
@@ -159,6 +158,14 @@ chrome.test.sendMessage('ready', function(message) {
           }
         }
       },
+      function setGlobals() {
+        for (let preferenceSet of preferences_to_test) {
+          for (let key in preferenceSet.preferences) {
+            prefSetterOppositeOfDefault.call(
+                preferenceSet.root, key, preferenceSet.preferences[key]);
+          }
+        }
+      },
       // For Privacy Sandbox APIs unable to enable a pref.
       function setToEnableExpectErrorDefault() {
         for (let preferenceSet of
@@ -186,19 +193,6 @@ chrome.test.sendMessage('ready', function(message) {
           for (let key in preferenceSet.preferences) {
             privacySandboxPrefSetterToTrueExpectErrorAndControlled.call(
                 preferenceSet.root, key);
-          }
-        }
-      },
-      // setGlobals() after setToEnableExpectErrorAndControlled(), so disabling
-      // privacySandboxEnabled doesn't trigger topicsEnabled, fledgeEnabled and
-      // adsMeasurementEnabled to false
-      // TODO(b/263568309): Move this method  after getPreferences() after the
-      // deprecated API privacySandboxEnabled is retired.
-      function setGlobals() {
-        for (let preferenceSet of preferences_to_test) {
-          for (let key in preferenceSet.preferences) {
-            prefSetterOppositeOfDefault.call(
-                preferenceSet.root, key, preferenceSet.preferences[key]);
           }
         }
       },
