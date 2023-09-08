e3ce70e6d1617a5331f5b89577465bc636298c10
Vishwas Uppoor <vishwasuppoor@google.com>
2023-09-08 12:22:52 +0000

    [ImageFetcher][Metadata] Pass Java AutofillImageFetcher instance when Java PDM is initialized
    Motivation: go/pay-autofill-clank-why-no-card-art
    Implementation: https://docs.google.com/document/d/1ebNb-mDAAitUpNeN0J373EetMenwWn_pNfA2nUqKPwk/edit?resourcekey=0-wGgiulOm_u7Ikd2Cvr6FIQ
    
    When Android surfaces need credit card art images, they initialize the
    Java PDM and request for it.
    
    Pass the Java AutofillImageFetcher that was initialized at browser
    startup which has the prefetched images.
    
    Note: This only passes the Java AutofillImageFetcher to Java PDM. In the
    next CL, the prefetched images will be used.
    
    Low-Coverage-Reason: Android AutofillImageFetcherImpl only makes JNI calls, and can't be tested.
    Bug: chromium:1467754, chromium:1479448, b:277916170
    Change-Id: Ia20cc61b21a999b2c683179a7cfffb91c742c5f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4844186
    Reviewed-by: Siddharth Shah <siashah@chromium.org>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194032}

--- chrome/browser/autofill/android/personal_data_manager_android.cc
+++ chrome/browser/autofill/android/personal_data_manager_android.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
 #include "chrome/browser/android/resource_mapper.h"
+#include "chrome/browser/autofill/android/autofill_image_fetcher_impl.h"
 #include "chrome/browser/autofill/android/jni_headers/PersonalDataManager_jni.h"
 #include "chrome/browser/autofill/personal_data_manager_factory.h"
 #include "chrome/browser/browser_process.h"
@@ -588,6 +589,13 @@ jboolean PersonalDataManagerAndroid::IsFidoAuthenticationAvailable(
 
 void PersonalDataManagerAndroid::SetSyncServiceForTesting(JNIEnv* env) {
   personal_data_manager_->SetSyncingForTest(true);
+}
+
+base::android::ScopedJavaLocalRef<jobject>
+PersonalDataManagerAndroid::GetOrCreateJavaImageFetcher(JNIEnv* env) {
+  return static_cast<AutofillImageFetcherImpl*>(
+             personal_data_manager_->GetImageFetcher())
+      ->GetOrCreateJavaImageFetcher();
 }
 
 ScopedJavaLocalRef<jobjectArray> PersonalDataManagerAndroid::GetProfileGUIDs(
--- components/autofill/core/browser/personal_data_manager.cc
+++ components/autofill/core/browser/personal_data_manager.cc
@@ -2104,6 +2104,10 @@ bool PersonalDataManager::IsPaymentCvcStorageEnabled() {
          prefs::IsPaymentCvcStorageEnabled(pref_service_);
 }
 
+AutofillImageFetcherBase* PersonalDataManager::GetImageFetcher() const {
+  return image_fetcher_;
+}
+
 AutofillProfileMigrationStrikeDatabase*
 PersonalDataManager::GetProfileMigrationStrikeDatabase() {
   return const_cast<AutofillProfileMigrationStrikeDatabase*>(
