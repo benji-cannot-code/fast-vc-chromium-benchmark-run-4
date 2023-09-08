61b33851cf3c8e802069b11896a12b7d6d572a18
Andy Phan <andyphan@chromium.org>
2023-09-08 23:08:58 +0000

    [CodeHealth] Fix dangling pointer in FtlMessageClientTest
    Set the pointer to MockMessageReceptionChannel to nullptr on TearDown().
    
    FtlMessagingClientTest has raw pointer to MockMessageReceptionChannel,
    which FtlMessagingClient owns. FtlMessagingClientTest owns
    FtlMessagingClient, so on TearDown(), the pointer to
    MockMessageReceptionChannel is dangling when FtlMessagingClient is
    destroyed. Avoid this by setting the pointer to nullptr before
    destroying FtlMessagingClient.
    
    Change-Id: I95b3237e58fd874ffcca0f13e4b3fc8de9f0ce8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4850603
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194363}

--- remoting/signaling/ftl_messaging_client_unittest.cc
+++ remoting/signaling/ftl_messaging_client_unittest.cc
@@ -157,8 +157,7 @@ class FtlMessagingClientTest : public testing::Test {
   ProtobufHttpTestResponder test_responder_;
   FakeOAuthTokenGetter token_getter_{OAuthTokenGetter::Status::SUCCESS, "", ""};
   std::unique_ptr<FtlMessagingClient> messaging_client_;
-  raw_ptr<MockMessageReceptionChannel, DanglingUntriaged>
-      mock_message_reception_channel_;
+  raw_ptr<MockMessageReceptionChannel> mock_message_reception_channel_;
 
  private:
   base::test::TaskEnvironment task_environment_;
@@ -178,6 +177,7 @@ void FtlMessagingClientTest::SetUp() {
 }
 
 void FtlMessagingClientTest::TearDown() {
+  mock_message_reception_channel_ = nullptr;
   messaging_client_.reset();
 }
 
