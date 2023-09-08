c950a23ad17fa443f9d530489b325e2cce35d380
Piotr Pawliczek <pawliczek@google.com>
2023-09-08 16:18:22 +0000

    IppUsb: Refactor AutomaticUsbPrinterConfigurer
    Refactor the implementation of automatic setup of USB printers. The
    logic responsible for USB printers autoconfiguration was moved from
    CupsPrintersManager to AutomaticUsbPrinterConfigurer. The class
    AutomaticUsbPrinterConfigurer was rewritten.
    
    Now, CupsPrintersManager provides to AutomaticUsbPrinterConfigurer a
    list of all currently connected USB printers by calling a method
    UpdateListOfConnectedPrinters(). This method calculates the difference
    between the given list of printers and the list passed in the previous
    call. Printers that disappeared from the list are uninstalled. For
    each new printer on the list, an installation procedure is started.
    When the procedure for a given printer finishes, the printer is
    assigned to the list of configured printers (when the setup succeeded)
    or to the list of unconfigured printers (when the automatic setup
    failed). CupsPrintersManager uses these two list to update the lists
    of Automatic and Discovered printers.
    
    BUG=b:184293121
    TEST=unit_tests and tested manually on atlas
    
    Change-Id: I91a84bcde2f165aa8d2dc0b35e7a45da1304d164
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4814849
    Commit-Queue: Piotr Pawliczek <pawliczek@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194153}

--- chrome/browser/ash/printing/ppd_resolution_state.cc
+++ chrome/browser/ash/printing/ppd_resolution_state.cc
@@ -10,9 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 PpdResolutionState::PpdResolutionState()
-    : is_inflight_(true),
-      is_ppd_resolution_successful_(false),
-      is_not_autoconfigurable_(false) {}
+    : is_inflight_(true), is_ppd_resolution_successful_(false) {}
 PpdResolutionState::PpdResolutionState(PpdResolutionState&& other) = default;
 PpdResolutionState& PpdResolutionState::operator=(PpdResolutionState&& rhs) =
     default;
@@ -60,14 +58,6 @@ bool PpdResolutionState::IsInflight() const {
 
 bool PpdResolutionState::WasResolutionSuccessful() const {
   return is_ppd_resolution_successful_;
-}
-
-void PpdResolutionState::MarkPrinterAsNotAutoconfigurable() {
-  is_not_autoconfigurable_ = true;
-}
-
-bool PpdResolutionState::IsMarkedAsNotAutoconfigurable() const {
-  return is_not_autoconfigurable_;
 }
 
 }  // namespace ash
