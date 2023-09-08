d902184ce0f420f94f83f69d84ebc08158c887b9
nihardamar <nihardamar@google.com>
2023-09-08 22:59:40 +0000

    upgrade http tests to php8
    Remove some more baselines, upgrade from
    php7->php8.
    Make sure tests are backwards compatible.
    
    Change-Id: Id6349c2dfca3b143a8529f77c5ebc11599dca530
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4833522
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Nihar Damarasingu <nihardamar@google.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1194358}

--- third_party/blink/web_tests/http/tests/eventsource/resources/echo-url.php
+++ third_party/blink/web_tests/http/tests/eventsource/resources/echo-url.php
@@ -1,8 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
 header("Content-Type: text/event-stream");
-$url = $_SERVER["REQUEST_URI"];
-$id = $_SERVER["HTTP_LAST_EVENT_ID"];
+$url = $_SERVER["REQUEST_URI"] ?? null;
+$id = $_SERVER["HTTP_LAST_EVENT_ID"] ?? null;
 
 echo "id: 77\n";
 echo "retry: 300\n";
--- third_party/blink/web_tests/http/tests/eventsource/resources/status-codes.php
+++ third_party/blink/web_tests/http/tests/eventsource/resources/status-codes.php
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
-$status_code = $_GET['status-code'];
+$status_code = $_GET['status-code'] ?? null;
 header("HTTP/1.1 $status_code OK");
 
 switch ($status_code) {
@@ -12,8 +12,8 @@ case 301:
 case 302:
 case 303:
 case 307:
-    $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\') . "/simple-event-stream.asis";
-    header("Location: http://" . $_SERVER['HTTP_HOST'] . $uri);
+    $uri = rtrim(dirname(($_SERVER['PHP_SELF'] ?? null)), '/\\') . "/simple-event-stream.asis";
+    header("Location: http://" . ($_SERVER['HTTP_HOST'] ?? null) . $uri);
     break;
 }
 ?>
