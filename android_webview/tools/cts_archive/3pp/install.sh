5af0c3e3b9f70378c73a36ad83b65d12927c463b
Mateusz Zbikowski <zbikowski@google.com>
2023-09-08 17:34:18 +0000

    Update Webview cts_archive
    BAU update (follows go/update-cts-proposal)
    
    Bug: None
    Change-Id: I6ca8427f47dc5d56c4b194e5ef5e08cb58947d5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4852905
    Commit-Queue: Mateusz Zbikowski <zbikowski@google.com>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Auto-Submit: Mateusz Zbikowski <zbikowski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1194201}

--- android_webview/tools/cts_archive/3pp/install.sh
+++ android_webview/tools/cts_archive/3pp/install.sh
@@ -147,8 +147,8 @@ zip -rm "$TARGET_DIR/$OUT_ZIPFILE" android-cts
 # platform separator for readability
 
 TARGET_DIR="$PREFIX/arm64/Q"
-IN_ZIPFILE=android-cts-10_r15-linux_x86-arm.zip
-OUT_ZIPFILE=android-cts-arm64-10_r15.zip
+IN_ZIPFILE=android-cts-10_r16-linux_x86-arm.zip
+OUT_ZIPFILE=android-cts-arm64-10_r16.zip
 
 mkdir -p "$TARGET_DIR"
 unzip "$IN_ZIPFILE" "android-cts/testcases/CtsWebkitTestCases.apk"
@@ -164,8 +164,8 @@ zip -rm "$TARGET_DIR/$OUT_ZIPFILE" android-cts
 # platform separator for readability
 
 TARGET_DIR="$PREFIX/x86/Q"
-IN_ZIPFILE=android-cts-10_r15-linux_x86-x86.zip
-OUT_ZIPFILE=android-cts-x86-10_r15.zip
+IN_ZIPFILE=android-cts-10_r16-linux_x86-x86.zip
+OUT_ZIPFILE=android-cts-x86-10_r16.zip
 
 mkdir -p "$TARGET_DIR"
 unzip "$IN_ZIPFILE" "android-cts/testcases/CtsWebkitTestCases.apk"
@@ -181,8 +181,8 @@ zip -rm "$TARGET_DIR/$OUT_ZIPFILE" android-cts
 # platform separator for readability
 
 TARGET_DIR="$PREFIX/arm64/R"
-IN_ZIPFILE=android-cts-11_r11-linux_x86-arm.zip
-OUT_ZIPFILE=android-cts-arm64-11_r11.zip
+IN_ZIPFILE=android-cts-11_r13-linux_x86-arm.zip
+OUT_ZIPFILE=android-cts-arm64-11_r13.zip
 
 mkdir -p "$TARGET_DIR"
 unzip "$IN_ZIPFILE" "android-cts/testcases/CtsWebkitTestCases.apk"
@@ -201,8 +201,8 @@ zip -rm "$TARGET_DIR/$OUT_ZIPFILE" android-cts
 # platform separator for readability
 
 TARGET_DIR="$PREFIX/x86/R"
-IN_ZIPFILE=android-cts-11_r11-linux_x86-x86.zip
-OUT_ZIPFILE=android-cts-x86-11_r11.zip
+IN_ZIPFILE=android-cts-11_r13-linux_x86-x86.zip
+OUT_ZIPFILE=android-cts-x86-11_r13.zip
 
 mkdir -p "$TARGET_DIR"
 unzip "$IN_ZIPFILE" "android-cts/testcases/CtsWebkitTestCases.apk"
@@ -221,8 +221,8 @@ zip -rm "$TARGET_DIR/$OUT_ZIPFILE" android-cts
 # platform separator for readability
 
 TARGET_DIR="$PREFIX/arm64/S"
-IN_ZIPFILE=android-cts-12_r7-linux_x86-arm.zip
-OUT_ZIPFILE=android-cts-arm64-12_r7.zip
+IN_ZIPFILE=android-cts-12_r9-linux_x86-arm.zip
+OUT_ZIPFILE=android-cts-arm64-12_r9.zip
 
 mkdir -p "$TARGET_DIR"
 unzip "$IN_ZIPFILE" "android-cts/testcases/CtsWebkitTestCases.apk"
@@ -241,8 +241,8 @@ zip -rm "$TARGET_DIR/$OUT_ZIPFILE" android-cts
 # platform separator for readability
 
 TARGET_DIR="$PREFIX/x86/S"
-IN_ZIPFILE=android-cts-12_r7-linux_x86-x86.zip
-OUT_ZIPFILE=android-cts-x86-12_r7.zip
+IN_ZIPFILE=android-cts-12_r9-linux_x86-x86.zip
+OUT_ZIPFILE=android-cts-x86-12_r9.zip
 
 mkdir -p "$TARGET_DIR"
 unzip "$IN_ZIPFILE" "android-cts/testcases/CtsWebkitTestCases.apk"
@@ -261,12 +261,12 @@ zip -rm "$TARGET_DIR/$OUT_ZIPFILE" android-cts
 # platform separator for readability
 
 # Below this line, we need to use unzip's -j (junk paths) and -d "$UNZIP_DEST"
-# flags. This is to rewrite paths, in order to stanardize them, as run_cts.py
+# flags. This is to rewrite paths, in order to standardize them, as run_cts.py
 # assumes they will be the same across archs for a given release.
 
 TARGET_DIR="$PREFIX/arm64/T"
-IN_ZIPFILE=android-cts-13_r3-linux_x86-arm.zip
-OUT_ZIPFILE=android-cts-arm64-13_r3.zip
+IN_ZIPFILE=android-cts-13_r5-linux_x86-arm.zip
+OUT_ZIPFILE=android-cts-arm64-13_r5.zip
 UNZIP_DEST=android-cts/testcases
 
 mkdir -p "$TARGET_DIR"
@@ -286,8 +286,8 @@ zip -rm "$TARGET_DIR/$OUT_ZIPFILE" android-cts
 # platform separator for readability
 
 TARGET_DIR="$PREFIX/x86/T"
-IN_ZIPFILE=android-cts-13_r3-linux_x86-x86.zip
-OUT_ZIPFILE=android-cts-x86-13_r3.zip
+IN_ZIPFILE=android-cts-13_r5-linux_x86-x86.zip
+OUT_ZIPFILE=android-cts-x86-13_r5.zip
 
 mkdir -p "$TARGET_DIR"
 mkdir -p "$UNZIP_DEST"
