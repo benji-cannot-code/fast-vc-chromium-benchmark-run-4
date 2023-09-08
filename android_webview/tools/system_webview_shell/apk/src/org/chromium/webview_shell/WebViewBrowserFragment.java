76e8378c6ab2a14ee75e8d406ca87e873c1f0dae
Sayed <elabadysayed@chromium.org>
2023-09-08 08:29:13 +0000

    [WebView-shell] Move ActivityResultLancher initalisation before saveState check
    WebViewShell crashes after switching WebView implementation due to missing
    ActivityResultLauncher initialisation, this CL moves the initialisation to
    the correct place with adding empty constructor for Fragment as recommended
    in Android.
    
    Bug: 1478962
    Change-Id: Id2b17a554dea796ebfc69ce20a6f440878a0d0b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4849967
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Sayed Elabady <elabadysayed@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193972}

--- android_webview/tools/system_webview_shell/apk/src/org/chromium/webview_shell/WebViewBrowserFragment.java
+++ android_webview/tools/system_webview_shell/apk/src/org/chromium/webview_shell/WebViewBrowserFragment.java
@@ -111,7 +111,7 @@ public class WebViewBrowserFragment extends Fragment {
     private EditText mUrlBar;
     private WebView mWebView;
     private View mFullscreenView;
-    private final ActivityResultRegistry mRegistry;
+    private ActivityResultRegistry mActivityResultRegistry;
     private final OnBackInvokedCallback mOnBackInvokedCallback =
             Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU ? ()
             -> mWebView.goBack()
@@ -133,11 +133,11 @@ public class WebViewBrowserFragment extends Fragment {
     public void setFilePathCallback(ValueCallback<Uri[]> inCallback) {
         mFilePathCallback = inCallback;
     }
-
-    public WebViewBrowserFragment(ActivityResultRegistry registry) {
-        super();
-        mRegistry = registry;
+    public void setActivityResultRegistry(ActivityResultRegistry activityResultRegistry) {
+        mActivityResultRegistry = activityResultRegistry;
     }
+
+    public WebViewBrowserFragment() {}
     // Work around our wonky API by wrapping a geo permission prompt inside a regular
     // PermissionRequest.
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP) // GeoPermissionRequest class requires API level 21.
@@ -271,6 +271,9 @@ public class WebViewBrowserFragment extends Fragment {
         enableStrictMode();
 
         createAndInitializeWebView();
+        mFileContents = registerForActivityResult(mMultiFileSelector, mActivityResultRegistry,
+                result -> mFilePathCallback.onReceiveValue(result));
+
         String url = getUrlFromIntent(requireActivity().getIntent());
         if (url == null) {
             mWebView.restoreState(savedInstanceState);
@@ -297,8 +300,6 @@ public class WebViewBrowserFragment extends Fragment {
         setUrlBarText(url);
         setUrlFail(false);
         loadUrlFromUrlBar(mUrlBar);
-        mFileContents = registerForActivityResult(
-                mMultiFileSelector, mRegistry, result -> mFilePathCallback.onReceiveValue(result));
     }
 
     @Override
