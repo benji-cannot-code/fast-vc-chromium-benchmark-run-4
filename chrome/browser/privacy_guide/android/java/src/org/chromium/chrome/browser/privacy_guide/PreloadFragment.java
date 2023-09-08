7e8f45ba1baa2360fe764a2222e0111c10b2d41c
Noureldien Refae <noureldien@google.com>
2023-09-08 08:40:10 +0000

    Privacy Guide: Add preload card to clank
    Add preload card base to clank cards deck.
    
    Low-Coverage-Reason: Tests are on the last relation chain CL.
    
    Bug: 1215630
    Change-Id: I6d348d78c3611bd1756ba962087e3542bb684ea5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4843030
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Nour Eldien Ayman <noureldien@google.com>
    Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1193975}

---
+++ chrome/browser/privacy_guide/android/java/src/org/chromium/chrome/browser/privacy_guide/PreloadFragment.java
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.privacy_guide;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * Controls the behaviour of the Preload privacy guide page.
+ */
+public class PreloadFragment extends Fragment {
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.privacy_guide_preload_step, container, false);
+    }
+}
