a321638e8c238fe1ecb81d2effbaae7cae051634
Theo Cristea <theocristea@google.com>
2023-09-08 16:15:21 +0000

    Added CHECK's for local state inside DeviceAuthenticatorWin
    Change-Id: Iec80167026dd559f0dacf4b2271e74b57769c0dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4852900
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Cr-Commit-Position: refs/heads/main@{#1194149}

--- chrome/browser/device_reauth/win/device_authenticator_win.cc
+++ chrome/browser/device_reauth/win/device_authenticator_win.cc
@@ -18,6 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 void SaveAvailability(BiometricAuthenticationStatusWin availability) {
+  CHECK(g_browser_process);
+  CHECK(g_browser_process->local_state());
+
   bool is_available =
       availability == BiometricAuthenticationStatusWin::kAvailable;
   g_browser_process->local_state()->SetBoolean(
