c41834e1fdc9cc49bd9e0b5ed48b2e6b0b54abd5
Jérôme Lebel <jlebel@chromium.org>
2023-09-08 07:34:04 +0000

    [iOS] Make interrupt of UserSigninCoordinator more robust
    This patch changes a little bit the logic to interrupt
    UserSigninCoordinator when called with `UIShutdownNoDismiss`.
    
    Now, the coordinator still interrupts the mediator, but the
    coordinator doesn't expect a callback from the mediator anymore.
    As soon as the mediator is interrupted, the coordinator continues
    the interruption workflow by:
    + Dropping the UI (as we are in UIShutdownNoDismiss case)
    + Call the signinCompletion block (to make the sign-in done)
    + Call the interruption completion block.
    
    This patch will be cherry-picked in M118.
    
    Fixed: 1474122
    Change-Id: Iaff75c4a09abf0e3cbf9fc3f88b05e1deee20491
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4846515
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Menghan Yang <myuu@google.com>
    Commit-Queue: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1193962}

--- ios/chrome/browser/ui/authentication/signin/user_signin/user_signin_mediator.mm
+++ ios/chrome/browser/ui/authentication/signin/user_signin/user_signin_mediator.mm
@@ -132,7 +132,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)interruptWithAction:(SigninCoordinatorInterrupt)action
                  completion:(ProceduralBlock)completion {
   [self.authenticationFlow interruptWithAction:action];
-  DCHECK(self.delegate);
   switch (self.delegate.signinStateOnStart) {
     case IdentitySigninStateSignedOut:
       switch (action) {
