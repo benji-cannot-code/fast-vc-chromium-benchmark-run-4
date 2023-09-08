1aed28cfb90218bb6af6ac6056a218c97b2680b8
Ruma Kesh <rkesh@google.com>
2023-09-08 21:21:46 +0000

    CrOS Settings: Convert os bluetooth devices subpage browser test to TS
    gtest_filter="*OsBluetoothPageOsBluetoothDevicesSubpage*"
    
    Bug: b/270728282
    Test: browser_tests --
    Change-Id: Ifc270344ecb16a8745b08ea51e867bbe8e2d8aa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4850448
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Ruma Kesh <rkesh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1194318}

--- chrome/test/data/webui/settings/chromeos/os_settings_browsertest.js
+++ chrome/test/data/webui/settings/chromeos/os_settings_browsertest.js
@@ -70,11 +70,11 @@ TEST_F_WITH_PREAMBLE(
 
 // TODO(crbug.com/1234871) Move this test back into the list of tests below once
 // Fast pair is launched.
-var OSSettingsOsBluetoothDevicesSubpageTest =
+var OSSettingsOsBluetoothPageOsBluetoothDevicesSubpageTest =
     class extends OSSettingsBrowserTest {
   /** @override */
   get browsePreload() {
-    return 'chrome://os-settings/test_loader.html?module=settings/chromeos/os_bluetooth_page/os_bluetooth_devices_subpage_tests.js';
+    return 'chrome://os-settings/test_loader.html?module=settings/chromeos/os_bluetooth_page/os_bluetooth_devices_subpage_test.js';
   }
 
   /** @override */
@@ -89,9 +89,11 @@ var OSSettingsOsBluetoothDevicesSubpageTest =
   }
 };
 
-TEST_F('OSSettingsOsBluetoothDevicesSubpageTest', 'AllJsTests', () => {
-  mocha.run();
-});
+TEST_F(
+    'OSSettingsOsBluetoothPageOsBluetoothDevicesSubpageTest', 'AllJsTests',
+    () => {
+      mocha.run();
+    });
 
 // TODO (b/238647706) Move this test back into the list of tests below once
 // Fast pair is launched.
