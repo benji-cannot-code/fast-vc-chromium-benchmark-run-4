f9337c09a189adc6fb455df6862224b86cf3a135
Ayu Ishii <ayui@chromium.org>
2023-09-08 16:50:11 +0000

    WebSQL: enable WebSQL in browsertests
    WebSQL API is to be removed in M119 and will only be accessible
    via flag. This change updates tests to enable WebSQL API
    while the feature is accessible via flag.
    
    Disable by default done here: https://crrev.com/c/4673258
    ChromeStatus: https://chromestatus.com/feature/5134293578285056
    
    Bug: 695592
    Change-Id: I38c9633a19876be9439a55c043c365d77f457e6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4850763
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194179}

--- content/browser/database_browsertest.cc
+++ content/browser/database_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_feature_list.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/download_manager.h"
 #include "content/public/browser/web_contents.h"
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/test_utils.h"
 #include "content/shell/browser/shell.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/common/features.h"
 
 namespace content {
 
@@ -27,6 +29,13 @@ namespace content {
 class MAYBE_DatabaseTest : public ContentBrowserTest {
  public:
   MAYBE_DatabaseTest() {}
+
+  void SetUp() override {
+    // WebSQL is disabled by default as of M119 (crbug/695592). Enable feature
+    // in tests during deprecation trial and enterprise policy support.
+    base::test::ScopedFeatureList feature_list{blink::features::kWebSQLAccess};
+    ContentBrowserTest::SetUp();
+  }
 
   void RunScriptAndCheckResult(Shell* shell,
                                const std::string& script,
--- content/browser/quota/quota_browsertest.cc
+++ content/browser/quota/quota_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
+#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "components/services/storage/public/cpp/constants.h"
 #include "content/public/browser/browser_context.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/shell/browser/shell.h"
 #include "storage/browser/quota/quota_manager_impl.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/common/features.h"
 #include "url/gurl.h"
 
 namespace content {
@@ -23,6 +25,13 @@ namespace content {
 class QuotaBrowserTest : public ContentBrowserTest {
  public:
   QuotaBrowserTest() = default;
+
+  void SetUp() override {
+    // WebSQL is disabled by default as of M119 (crbug/695592). Enable feature
+    // in tests during deprecation trial and enterprise policy support.
+    base::test::ScopedFeatureList feature_list{blink::features::kWebSQLAccess};
+    ContentBrowserTest::SetUp();
+  }
 
   base::FilePath profile_path() {
     return shell()
