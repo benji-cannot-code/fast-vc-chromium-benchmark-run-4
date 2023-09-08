c50005addf06c5cf7ec9a2ea43810bf65c05a251
Sorin Jianu <sorin@chromium.org>
2023-09-08 19:34:50 +0000

    updater: don't use a COM STA TaskRunner for policy fetches.
    Such a task runner is prone to failures when the code is run
    in session 0.
    
    Fixed: 1480502
    Change-Id: I579277fe1a96ec3e8802d345feeb47fbf6654b90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4852772
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194271}

--- chrome/updater/policy/policy_fetcher.cc
+++ chrome/updater/policy/policy_fetcher.cc
@@ -29,18 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 namespace updater {
-namespace {
-
-scoped_refptr<base::SequencedTaskRunner> GetBlockingTaskRunner() {
-  constexpr base::TaskTraits KMayBlockTraits = {base::MayBlock()};
-#if BUILDFLAG(IS_WIN)
-  return base::ThreadPool::CreateCOMSTATaskRunner(KMayBlockTraits);
-#else
-  return base::ThreadPool::CreateSequencedTaskRunner(KMayBlockTraits);
-#endif
-}
-
-}  // namespace
 
 PolicyFetcher::PolicyFetcher(
     const GURL& server_url,
@@ -49,7 +37,8 @@ PolicyFetcher::PolicyFetcher(
     : server_url_(server_url),
       policy_service_proxy_configuration_(proxy_configuration),
       override_is_managed_device_(override_is_managed_device),
-      sequenced_task_runner_(GetBlockingTaskRunner()) {
+      sequenced_task_runner_(
+          base::ThreadPool::CreateSequencedTaskRunner({base::MayBlock()})) {
   VLOG(0) << "Policy server: " << server_url_.possibly_invalid_spec();
 }
 
