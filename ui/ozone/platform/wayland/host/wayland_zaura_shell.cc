0bcb769aad1816bcbbb57fb08f47eeea03cd3d18
Sammie Quon <sammiequon@chromium.org>
2023-09-08 18:47:10 +0000

    lacros/client: Allows float to be set to different locations
    Server side CL: CL:4814853
    
    Pipes the float start location to the new set_float_to_location
    added in the server side cl.
    
    Test: manual
    Test: ozone_unittests *WaylandWindowTest.SetUnsetFloat*
    Change-Id: I32a856c8b72280eaa6c57a0d789a252a11e34d9c
    Fixed: b/289082657
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4844563
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194249}

--- ui/ozone/platform/wayland/host/wayland_zaura_shell.cc
+++ ui/ozone/platform/wayland/host/wayland_zaura_shell.cc
@@ -24,7 +24,7 @@ namespace ui {
 
 namespace {
 constexpr uint32_t kMinVersion = 1;
-constexpr uint32_t kMaxVersion = 58;
+constexpr uint32_t kMaxVersion = 59;
 }
 
 // static
--- ui/platform_window/extensions/wayland_extension.h
+++ ui/platform_window/extensions/wayland_extension.h
@@ -20,6 +20,11 @@ enum class WaylandWindowSnapDirection {
   kSecondary,
 };
 
+enum class WaylandFloatStartLocation {
+  kBottomRight,
+  kBottomLeft,
+};
+
 enum class WaylandOrientationLockType {
   kAny,
   kNatural,
@@ -90,7 +95,9 @@ class COMPONENT_EXPORT(PLATFORM_WINDOW) WaylandExtension {
 
   // Signals the underneath platform to float the browser window on top other
   // windows.
-  virtual void SetFloat(bool value) = 0;
+  virtual void SetFloatToLocation(
+      WaylandFloatStartLocation float_start_location) = 0;
+  virtual void UnSetFloat() = 0;
 
  protected:
   virtual ~WaylandExtension();
