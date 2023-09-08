260eb6cbd1e52d5216b51cf8343a261659a52e45
Prameet Shah <phshah@google.com>
2023-09-08 21:04:19 +0000

    gd: Rename "main menu button" to "Game Dashboard button"
    Bug: b:299526295
    Test: CQ & ran ASH unit tests
    Change-Id: I60d51a4a37d1ec560c3217659073cd4f30a4409e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4852166
    Commit-Queue: Prameet Shah <phshah@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194304}

--- ash/game_dashboard/game_dashboard_main_menu_view.h
+++ ash/game_dashboard/game_dashboard_main_menu_view.h
@@ -27,8 +27,6 @@ class ASH_EXPORT GameDashboardMainMenuView
 
   explicit GameDashboardMainMenuView(GameDashboardContext* context);
 
-  GameDashboardMainMenuView(views::Widget* main_menu_button_widget,
-                            aura::Window* game_window);
   GameDashboardMainMenuView(const GameDashboardMainMenuView&) = delete;
   GameDashboardMainMenuView& operator=(const GameDashboardMainMenuView) =
       delete;
