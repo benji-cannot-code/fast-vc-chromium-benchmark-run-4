53cb15f0526aac543263a044c99e54cedc31c4c8
Kevin Lubick <kjlubick@google.com>
2023-09-08 13:12:35 +0000

    Set SkSerialProcs for explicit image encoding
    Skia plans to stop the default behavior of encoding SkImages in
    SkPictures using a PNG encoder. To preserve that behavior,
    clients need to explicitly set an SkSerialProc to do so.
    
    I assume that picture_debug_util.cc and gpu_benchmarking_extension.cc
    both have SkPictures which could contain SkImages. I can tell by
    inspection serial_utils_unittest.cc does not, but figured we could
    make the use of the default behavior explicit.
    
    Bug: b/40045064
    Change-Id: I54b0fb960eec688359e11cb27948f00ce9463f56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4845659
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Kevin Lubick <kjlubick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194054}

--- content/renderer/gpu_benchmarking_extension.cc
+++ content/renderer/gpu_benchmarking_extension.cc
@@ -63,8 +63,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkPicture.h"
 #include "third_party/skia/include/core/SkPictureRecorder.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
+#include "third_party/skia/include/core/SkSerialProcs.h"
 #include "third_party/skia/include/core/SkStream.h"
 #include "third_party/skia/include/docs/SkXPSDocument.h"
+#include "third_party/skia/include/encode/SkPngEncoder.h"
 // Note that headers in third_party/skia/src are fragile.  This is
 // an experimental, fragile, and diagnostic-only document type.
 #include "third_party/skia/src/utils/SkMultiPictureDocument.h"
@@ -183,7 +185,14 @@ class SkPictureSerializer {
       SkFILEWStream file(filepath.c_str());
       DCHECK(file.isValid());
 
-      auto data = picture->serialize();
+      SkSerialProcs procs{
+          .fImageProc = [](SkImage* img, void*) -> sk_sp<SkData> {
+            // Note: if the picture contains texture-backed (gpu) images, they
+            // will fail to be read-back and therefore fail to be encoded unless
+            // we can thread the correct GrDirectContext through to here.
+            return SkPngEncoder::Encode(nullptr, img, SkPngEncoder::Options{});
+          }};
+      auto data = picture->serialize(&procs);
       file.write(data->data(), data->size());
       file.fsync();
     }
