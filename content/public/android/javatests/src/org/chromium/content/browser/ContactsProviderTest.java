05d6047ff7aaa9eca15177688bd91dc552ddb3e0
Finnur Thorarinsson <finnur@chromium.org>
2023-09-08 19:29:13 +0000

    [ContactsPicker]: Batch tests for the ContactsProviderTest.
    Bug: None
    Change-Id: I8ba938ea68689bf363f25e2e11e34c89ac4e63e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4850698
    Auto-Submit: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194269}

--- content/public/android/javatests/src/org/chromium/content/browser/ContactsProviderTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ContactsProviderTest.java
@@ -16,6 +16,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
@@ -41,9 +42,10 @@ import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
- * Tests contacts Web API functionality.
+ * Tests Contacts Web API functionality.
  */
 @RunWith(ContentJUnit4ClassRunner.class)
+@Batch(Batch.PER_CLASS)
 public class ContactsProviderTest {
     private static final String TEST_URL = "/content/test/data/android/title1.html";
     private static final String FENCED_FRAME_URL =
