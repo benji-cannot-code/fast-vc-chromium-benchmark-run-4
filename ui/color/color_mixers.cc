1fb5782cdd51c700c2e2985fcc39b2ebae93ce1f
Sam Davis Omekara <samomekarajr@microsoft.com>
2023-09-08 23:28:48 +0000

    Adding a forced colors mixer to the color pipeline.
    This CL introduces a mixer for the Forced Colors key, which is used to
    apply color recipes when forced colors mode is enabled. Currently,
    Forced colors mode can be activated by the operating system settings
    on Windows or by emulation via the DevTools feature. This CL is part of
    our effort to move the default forced colors definition to the color
    pipeline, which will allow us to handle forced colors in a
    platform-agnostic way. This CL does not introduce any functional
    changes.
    
    In future CLs, we will add a forced colors provider to the
    WebThemeEngine and use it for painting in LayoutTheme.
    
    Bug: 1430181
    Change-Id: I316acb4c6ceb186f9b41d321bbb0b3ebf819d58a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4837761
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Commit-Queue: Sam Davis Omekara <samomekarajr@microsoft.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194374}

--- ui/color/color_mixers.cc
+++ ui/color/color_mixers.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/base/ui_base_features.h"
 #include "ui/color/core_default_color_mixer.h"
+#include "ui/color/forced_colors_mixer.h"
 #include "ui/color/material_ui_color_mixer.h"
 #include "ui/color/native_color_mixers.h"
 #include "ui/color/ref_color_mixer.h"
@@ -28,6 +29,7 @@ void AddColorMixers(ColorProvider* provider, const ColorProviderKey& key) {
     AddMaterialUiColorMixer(provider, key);
   }
   AddNativeUiColorMixer(provider, key);
+  AddForcedColorsColorMixer(provider, key);
   AddNativePostprocessingMixer(provider, key);
 }
 
--- ui/color/color_provider_utils.cc
+++ ui/color/color_provider_utils.cc
@@ -394,7 +394,6 @@ ColorProvider CreateEmulatedForcedColorsColorProvider(bool dark_mode) {
       kColorForcedHighlight};
   mixer[kColorWebNativeControlScrollbarCorner] = {kColorForcedBtnFace};
   CompleteControlsForcedColorsDefinition(mixer);
-  CompleteFluentScrollbarColorsDefinition(mixer);
   color_provider.GenerateColorMap();
   return color_provider;
 }
@@ -452,7 +451,7 @@ ColorProvider CreateEmulatedForcedColorsColorProviderForWebTests() {
   return color_provider;
 }
 
-void CompleteFluentScrollbarColorsDefinition(ui::ColorMixer& mixer) {
+void CompleteScrollbarColorsDefinition(ui::ColorMixer& mixer) {
   mixer[kColorWebNativeControlScrollbarArrowBackgroundHovered] = {
       kColorWebNativeControlScrollbarCorner};
   mixer[kColorWebNativeControlScrollbarArrowBackgroundPressed] = {
@@ -495,10 +494,15 @@ void CompleteControlsForcedColorsDefinition(ui::ColorMixer& mixer) {
   mixer[kColorWebNativeControlFillPressed] = {kColorForcedWindow};
   mixer[kColorWebNativeControlLightenLayer] = {kColorForcedWindow};
   mixer[kColorWebNativeControlProgressValue] = {kColorForcedHighlight};
+  mixer[kColorWebNativeControlScrollbarArrowForeground] = {kColorForcedBtnText};
+  mixer[kColorWebNativeControlScrollbarArrowForegroundPressed] = {
+      kColorForcedHighlight};
+  mixer[kColorWebNativeControlScrollbarCorner] = {kColorForcedBtnFace};
   mixer[kColorWebNativeControlSlider] = {kColorForcedHighlight};
   mixer[kColorWebNativeControlSliderDisabled] = {kColorForcedGrayText};
   mixer[kColorWebNativeControlSliderHovered] = {kColorForcedHighlight};
   mixer[kColorWebNativeControlSliderPressed] = {kColorForcedHighlight};
+  CompleteScrollbarColorsDefinition(mixer);
 }
 
 bool IsRendererColorMappingEquivalent(
