26566a0d5f6a2d6fb1197165991f13186786a51a
Thomas Nguyen <tungnh@chromium.org>
2023-09-08 11:32:05 +0000

    Revert "Allow anchor interaction tracking on localhost"
    This reverts commit bf798ff87519d500f82b4238d4cb69dffaf67074.
    
    Reason for revert: NavigationPredictorBrowserTest.PipelineHttp failed in several platforms, e.g https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests%20%28dbg%29%281%29
    
    Original change's description:
    > Allow anchor interaction tracking on localhost
    >
    > There's an explicit check that a document's URL is HTTPS before
    > computing anchor element metrics. This notably excludes
    > http://localhost. We now check for HTTP/HTTPS and potential
    > trustworthiness of the HTTP case.
    >
    > Bug: None
    > Change-Id: I6307808792d8ff60059f1de2c54287ad83e33d2e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4846664
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Auto-Submit: Kevin McNee <mcnee@chromium.org>
    > Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    > Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1193952}
    
    Bug: None
    Change-Id: Ifefaf93eb2a1720610eab720f3dc0a775df98483
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4851692
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Thomas Nguyen <tungnh@chromium.org>
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194020}

--- third_party/blink/renderer/core/html/anchor_element_metrics_sender.cc
+++ third_party/blink/renderer/core/html/anchor_element_metrics_sender.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/location.h"
 #include "base/rand_util.h"
-#include "services/network/public/cpp/is_potentially_trustworthy.h"
 #include "third_party/blink/public/common/browser_interface_broker_proxy.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/input/web_pointer_properties.h"
@@ -67,8 +66,7 @@ bool AnchorElementMetricsSender::HasAnchorElementMetricsSender(
       base::FeatureList::IsEnabled(features::kNavigationPredictor);
   const KURL& url = document.BaseURL();
   return is_feature_enabled && document.IsInOutermostMainFrame() &&
-         url.IsValid() && url.ProtocolIsInHTTPFamily() &&
-         network::IsUrlPotentiallyTrustworthy(GURL(url));
+         url.IsValid() && url.ProtocolIs("https");
 }
 
 void AnchorElementMetricsSender::
