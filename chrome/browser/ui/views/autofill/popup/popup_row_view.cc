1f570a4eda39cb3ab59c951272b85c1fdf8eb7f3
Dmitry Vykochko <vykochko@google.com>
2023-09-08 13:45:08 +0000

    [Mullet m2] Move LEFT/RIGHT handling from row view to popup view.
    This is a prerequisite work for sub-popup keyboard control functionality
    where the LEFT/RIGHT keys indirectly manage popup closing. Having their
    handlers in the popup view makes the whole keyboard control flow more
    explicit.
    
    Bug: 1466116
    Change-Id: I89c0b59bbb559fd37f9ea9086efa4b3ec33b9806
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4839315
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1194066}

--- chrome/browser/ui/views/autofill/popup/popup_row_view.cc
+++ chrome/browser/ui/views/autofill/popup/popup_row_view.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
-#include "base/i18n/rtl.h"
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/ui/autofill/autofill_popup_controller.h"
 #include "chrome/browser/ui/views/autofill/popup/popup_cell_view.h"
@@ -24,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/autofill_features.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
-#include "ui/events/keycodes/keyboard_codes.h"
 #include "ui/gfx/geometry/insets_outsets_base.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/metadata/type_conversion.h"
@@ -162,42 +160,8 @@ bool PopupRowView::HandleKeyPressEvent(
       content_view_->HandleKeyPressEvent(event)) {
     return true;
   }
-  switch (event.windows_key_code) {
-    case ui::VKEY_LEFT:
-      // `base::i18n::IsRTL` is used here instead of the controller's method
-      // because the controller's `IsRTL` depends on the language of the focused
-      // field and not the overall UI language. However, the layout of the popup
-      // is determined by the overall UI language.
-      if (base::i18n::IsRTL()) {
-        SelectNextCell();
-      } else {
-        SelectPreviousCell();
-      }
-      return true;
-    case ui::VKEY_RIGHT:
-      if (base::i18n::IsRTL()) {
-        SelectPreviousCell();
-      } else {
-        SelectNextCell();
-      }
-      return true;
-    default:
-      return false;
-  }
-}
 
-void PopupRowView::SelectNextCell() {
-  CHECK(GetSelectedCell());
-  if (*GetSelectedCell() == CellType::kContent && GetControlView()) {
-    SetSelectedCell(CellType::kControl);
-  }
-}
-
-void PopupRowView::SelectPreviousCell() {
-  CHECK(GetSelectedCell());
-  if (*GetSelectedCell() == CellType::kControl) {
-    SetSelectedCell(CellType::kContent);
-  }
+  return false;
 }
 
 const PopupCellView* PopupRowView::GetCellView(CellType type) const {
