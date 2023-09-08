410866e26eacbc63dd29581d25f78f338911c54f
Gaston Rodriguez Lopez <gastonr@microsoft.com>
2023-09-08 16:37:47 +0000

    Create Fluent Overlay feature flag.
    Implement feature flag to control overlay fluent scrollbars.
    Fluent Overlay implementation changes that use the flag will come in
    follow up changes.
    
    Bug: 1479162
    Change-Id: Ibe141c5c3fe487fe116304af7daeb3b94ac35132
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4844422
    Commit-Queue: Gaston Rodriguez <gastonr@microsoft.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Olga Gerchikov <gerchiko@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1194168}

--- ui/native_theme/native_theme_features.h
+++ ui/native_theme/native_theme_features.h
@@ -15,6 +15,7 @@ namespace features {
 
 NATIVE_THEME_EXPORT BASE_DECLARE_FEATURE(kOverlayScrollbar);
 NATIVE_THEME_EXPORT BASE_DECLARE_FEATURE(kFluentScrollbar);
+NATIVE_THEME_EXPORT BASE_DECLARE_FEATURE(kFluentOverlayScrollbar);
 
 }  // namespace features
 
@@ -22,6 +23,7 @@ namespace ui {
 
 NATIVE_THEME_EXPORT bool IsOverlayScrollbarEnabled();
 NATIVE_THEME_EXPORT bool IsFluentScrollbarEnabled();
+NATIVE_THEME_EXPORT bool IsFluentOverlayScrollbarEnabled();
 
 }  // namespace ui
 
