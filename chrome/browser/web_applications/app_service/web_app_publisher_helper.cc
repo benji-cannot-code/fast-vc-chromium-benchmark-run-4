bd356d3b916e3bcf75d47461b75c2944d5e684d2
Mustapha Jaber <mustjab@microsoft.com>
2023-09-08 21:06:26 +0000

    Added ScopedProfileKeepAlive while we uninstall web app using WebAppPublisherHelper to avoid dangling pointer crash.
    Bug: 1477767
    Change-Id: I0586920a9fb38fc99fd7b642fc45f8bbc3c2764a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4843676
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Mustapha Jaber <mustjab@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1194307}

--- chrome/browser/web_applications/app_service/web_app_publisher_helper.cc
+++ chrome/browser/web_applications/app_service/web_app_publisher_helper.cc
@@ -51,6 +51,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/policy_util.h"
 #include "chrome/browser/apps/app_service/publishers/app_publisher.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
+#include "chrome/browser/profiles/keep_alive/profile_keep_alive_types.h"
+#include "chrome/browser/profiles/keep_alive/scoped_profile_keep_alive.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/web_applications/app_service/publisher_helper.h"
 #include "chrome/browser/web_applications/locks/app_lock.h"
@@ -816,6 +818,16 @@ void WebAppPublisherHelper::UninstallWebApp(
     return;
   }
 
+  // Off the record profiles cannot be 'kept alive'.
+  std::unique_ptr<ScopedProfileKeepAlive> profile_keep_alive =
+      profile_->IsOffTheRecord()
+          ? nullptr
+          : std::make_unique<ScopedProfileKeepAlive>(
+                profile_, ProfileKeepAliveOrigin::kWebAppUninstall);
+  // Ensure profile is kept alive until ClearSiteData is done.
+  auto callback = base::BindOnce(
+      [](std::unique_ptr<ScopedProfileKeepAlive> profile_keep_alive) {},
+      std::move(profile_keep_alive));
   content::ClearSiteData(
       base::BindRepeating(
           [](content::BrowserContext* browser_context) {
@@ -827,7 +839,7 @@ void WebAppPublisherHelper::UninstallWebApp(
       /*storage_buckets_to_remove=*/{}, /*avoid_closing_connections=*/false,
       /*cookie_partition_key=*/absl::nullopt,
       /*storage_key=*/absl::nullopt,
-      /*partitioned_state_allowed_only=*/false, base::DoNothing());
+      /*partitioned_state_allowed_only=*/false, std::move(callback));
 }
 
 void WebAppPublisherHelper::SetIconEffect(const std::string& app_id) {
