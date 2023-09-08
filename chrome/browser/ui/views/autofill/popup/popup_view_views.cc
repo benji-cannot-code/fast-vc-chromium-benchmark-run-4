42715c492ec31b11cbe440f74425dcd2b7a9908d
Dmitry Vykochko <vykochko@google.com>
2023-09-08 13:45:08 +0000

    [Mullet m2] Autofill popup views, close by no selection in children.
    According the UX requirements all sub-popups should be closed (with
    some delay) when the user moves mouse out of any (sub-)popup, this
    CL supports this logic. It also improves the child-parent mouse move events retranslation to fix the case when the mouse moves too fast,
    producing not enough move events and leaving the sub-popup open.
    
    Bug: 1466116
    Change-Id: I42811d881ce6809310c26ec00e40524dfdbb2b34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4737992
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1194064}

--- chrome/browser/ui/views/autofill/popup/popup_view_views.cc
+++ chrome/browser/ui/views/autofill/popup/popup_view_views.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/contains.h"
 #include "base/feature_list.h"
+#include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
@@ -166,6 +167,14 @@ void PopupViewViews::GetAccessibleNodeData(ui::AXNodeData* node_data) {
   }
   node_data->SetNameChecked(
       l10n_util::GetStringUTF16(IDS_AUTOFILL_POPUP_ACCESSIBLE_NODE_DATA));
+}
+
+void PopupViewViews::OnMouseEntered(const ui::MouseEvent& event) {
+  OnMouseEnteredInChildren();
+}
+
+void PopupViewViews::OnMouseExited(const ui::MouseEvent& event) {
+  OnMouseExitedInChildren();
 }
 
 bool PopupViewViews::Show(
@@ -443,6 +452,8 @@ bool PopupViewViews::HasPopupRowViewAt(size_t index) const {
 }
 
 void PopupViewViews::InitViews() {
+  SetNotifyEnterExitOnChild(true);
+
   views::BoxLayout* layout =
       SetLayoutManager(std::make_unique<views::BoxLayout>(
           views::BoxLayout::Orientation::kVertical));
@@ -703,6 +714,31 @@ bool PopupViewViews::DoUpdateBoundsAndRedrawPopup() {
 
   SchedulePaint();
   return true;
+}
+
+void PopupViewViews::OnMouseEnteredInChildren() {
+  if (parent_ && parent_->get()) {
+    parent_->get()->OnMouseEnteredInChildren();
+  }
+
+  // Cancel scheluled sub-popup closing.
+  no_selection_sub_popup_close_timer_.Stop();
+}
+
+void PopupViewViews::OnMouseExitedInChildren() {
+  if (GetSelectedCell()) {
+    return;
+  }
+
+  if (parent_ && parent_->get()) {
+    parent_->get()->OnMouseExitedInChildren();
+  }
+
+  // Schedule sub-popup closing.
+  no_selection_sub_popup_close_timer_.Start(
+      FROM_HERE, kNoSelectionHideSubPopupDelay,
+      base::BindRepeating(&PopupViewViews::SetCellWithOpenSubPopup,
+                          weak_ptr_factory_.GetWeakPtr(), absl::nullopt));
 }
 
 bool PopupViewViews::CanShowDropdownInBounds(const gfx::Rect& bounds) const {
