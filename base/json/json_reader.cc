6b63f716a8ce4c2c9f781ee68091d4488e8c0b2a
Dustin J. Mitchell <djmitche@chromium.org>
2023-09-08 17:29:36 +0000

    Choose JSON parser with feature UseRustJsonParser
    JSON is sometimes parsed before features are loaded, in which case this defaults to the C++ parser.
    
    Bug: 1476100
    Change-Id: Icaaf7756b19117315246a43e27c3e699164ca122
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4833279
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194198}

--- base/json/json_reader.cc
+++ base/json/json_reader.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/features.h"
 #include "base/logging.h"
 #include "base/rust_buildflags.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -15,9 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_piece_rust.h"
 #include "third_party/rust/serde_json_lenient/v0_1/wrapper/functions.h"
 #include "third_party/rust/serde_json_lenient/v0_1/wrapper/lib.rs.h"
-#else
+#endif  // BUILDFLAG(BUILD_RUST_JSON_READER)
 #include "base/json/json_parser.h"
-#endif
 
 namespace base {
 
@@ -136,11 +136,16 @@ absl::optional<Value> JSONReader::Read(StringPiece json,
                                        int options,
                                        size_t max_depth) {
 #if BUILDFLAG(BUILD_RUST_JSON_READER)
-  JSONReader::Result result = DecodeJSONInRust(json, options, max_depth);
-  if (!result.has_value()) {
-    return absl::nullopt;
+  if (UsingRust()) {
+    JSONReader::Result result = DecodeJSONInRust(json, options, max_depth);
+    if (!result.has_value()) {
+      return absl::nullopt;
+    }
+    return std::move(*result);
+  } else {
+    internal::JSONParser parser(options, max_depth);
+    return parser.Parse(json);
   }
-  return std::move(*result);
 #else   // BUILDFLAG(BUILD_RUST_JSON_READER)
   internal::JSONParser parser(options, max_depth);
   return parser.Parse(json);
@@ -162,7 +167,21 @@ absl::optional<Value::Dict> JSONReader::ReadDict(StringPiece json,
 JSONReader::Result JSONReader::ReadAndReturnValueWithError(StringPiece json,
                                                            int options) {
 #if BUILDFLAG(BUILD_RUST_JSON_READER)
-  return DecodeJSONInRust(json, options, internal::kAbsoluteMaxDepth);
+  if (UsingRust()) {
+    return DecodeJSONInRust(json, options, internal::kAbsoluteMaxDepth);
+  } else {
+    internal::JSONParser parser(options);
+    auto value = parser.Parse(json);
+    if (!value) {
+      Error error;
+      error.message = parser.GetErrorMessage();
+      error.line = parser.error_line();
+      error.column = parser.error_column();
+      return base::unexpected(std::move(error));
+    }
+
+    return std::move(*value);
+  }
 #else   // BUILDFLAG(BUILD_RUST_JSON_READER)
   internal::JSONParser parser(options);
   auto value = parser.Parse(json);
@@ -175,6 +194,21 @@ JSONReader::Result JSONReader::ReadAndReturnValueWithError(StringPiece json,
   }
 
   return std::move(*value);
+#endif  // BUILDFLAG(BUILD_RUST_JSON_READER)
+}
+
+// static
+bool JSONReader::UsingRust() {
+  // If features have not yet been enabled, we cannot check the feature, so fall
+  // back to the C++ parser. In practice, this seems to apply to
+  // `ReadPrefsFromDisk()`, which is parsing trusted JSON.
+  if (!base::FeatureList::GetInstance()) {
+    return false;
+  }
+#if BUILDFLAG(BUILD_RUST_JSON_READER)
+  return base::FeatureList::IsEnabled(base::features::kUseRustJsonParser);
+#else   // BUILDFLAG(BUILD_RUST_JSON_READER)
+  return false;
 #endif  // BUILDFLAG(BUILD_RUST_JSON_READER)
 }
 
