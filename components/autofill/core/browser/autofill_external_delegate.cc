d7301f54029f9f077416487490c3d09352479398
Bruno Braga <brunobraga@google.com>
2023-09-08 16:10:09 +0000

    [mullet m2] Expand what we consider an autofill suggestion.
    When the user chooses group or field by field filling the next
    suggestion will stay at that level. This means it wil not be kAddress.
    Instead it will be one of the new filling granularities type.
    
    Bug: 1459990
    Change-Id: Ia3aeec0e66a3cba5858e437d41d7f0aaf34bdfa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4846579
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Auto-Submit: Bruno Braga <brunobraga@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1194147}

--- components/autofill/core/browser/autofill_external_delegate.cc
+++ components/autofill/core/browser/autofill_external_delegate.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/containers/contains.h"
 #include "base/containers/cxx20_erase.h"
+#include "base/containers/fixed_flat_set.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/i18n/case_conversion.h"
@@ -52,6 +53,22 @@ namespace {
 bool IsAutofillWarningEntry(PopupItemId popup_item_id) {
   return popup_item_id == PopupItemId::kInsecureContextPaymentDisabledMessage ||
          popup_item_id == PopupItemId::kMixedFormMessage;
+}
+
+// Returns true if any of the `suggestions` is an Autofill suggestion.
+bool HasAutofillSuggestions(const std::vector<Suggestion>& suggestions) {
+  // Virtual cards can appear on their own when filling the CVC for a card
+  // that a merchant has saved. This indicates there could be Autofill
+  // suggestions related to standalone CVC fields.
+  static constexpr auto kAutofillSuggestions =
+      base::MakeFixedFlatSet<PopupItemId>(
+          {PopupItemId::kCreditCardEntry, PopupItemId::kVirtualCreditCardEntry,
+           PopupItemId::kAddressEntry, PopupItemId::kFieldByFieldFilling,
+           PopupItemId::kFillFullAddress, PopupItemId::kFillFullPhoneNumber,
+           PopupItemId::kFillFullName});
+  return base::ranges::any_of(suggestions, [&](const Suggestion& suggestion) {
+    return kAutofillSuggestions.contains(suggestion.popup_item_id);
+  });
 }
 
 // The `AutofillTriggerSource` indicates what caused an Autofill fill or preview
@@ -145,18 +162,7 @@ void AutofillExternalDelegate::OnSuggestionsReturned(
 
   // Only include "Autofill Options" special menu item if we have Autofill
   // suggestions.
-  has_autofill_suggestions_ = false;
-  for (auto& suggestion : suggestions) {
-    // Virtual cards can appear on their own when filling the CVC for a card
-    // that a merchant has saved. This indicates there could be Autofill
-    // suggestions related to standalone CVC fields.
-    if (suggestion.popup_item_id == PopupItemId::kAddressEntry ||
-        suggestion.popup_item_id == PopupItemId::kCreditCardEntry ||
-        suggestion.popup_item_id == PopupItemId::kVirtualCreditCardEntry) {
-      has_autofill_suggestions_ = true;
-      break;
-    }
-  }
+  has_autofill_suggestions_ = HasAutofillSuggestions(suggestions);
 
   if (should_show_cards_from_account_option_) {
     suggestions.emplace_back(
