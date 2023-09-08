eba501e67af97f674f1c084652e6c1786c961235
Mickey Burks <mickeyburks@chromium.org>
2023-09-08 16:09:03 +0000

    [User Education] Add FeaturePromoCloseReason to battery_saver_button
    Clients will now be required to give a reason for closing promos. This
    allows for better metrics in the User Education system.
    
    Please make sure the semantics of the call to CloseFeaturePromo makes
    sense for your feature. We will now record metrics for why this Promo
    was closed.
    
    Bug: 1473729
    Change-Id: I2de7fdfc57c1a5288d8e7e3839370b1851f880fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4841917
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Auto-Submit: Mickey Burks <mickeyburks@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194146}

--- chrome/browser/ui/views/performance_controls/battery_saver_button.h
+++ chrome/browser/ui/views/performance_controls/battery_saver_button.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/performance_controls/battery_saver_button_controller.h"
 #include "chrome/browser/ui/performance_controls/battery_saver_button_controller_delegate.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_button.h"
+#include "components/user_education/common/feature_promo_controller.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 
 class BrowserView;
@@ -56,7 +57,7 @@ class BatterySaverButton : public ToolbarButton,
   void OnClicked();
 
   void MaybeShowFeaturePromo();
-  void CloseFeaturePromo();
+  void CloseFeaturePromo(user_education::FeaturePromoCloseReason close_reason);
 
  private:
   const raw_ptr<BrowserView> browser_view_;
