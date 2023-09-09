e0f4ee382a6b06e370217d84c2680cea634d085c
Vignesh Shenvi <vshenvi@google.com>
2023-09-09 00:45:43 +0000

    Add helper to validate if a website URL is allowlisted
    This change introduces a new helper that helps validate if a given
    website URL is allowlisted based on the specified policy setting value.
    This is especially useful when tracking metrics like website activity
    and usage.
    
    Bug: b:296644381
    Change-Id: I51f7ccba55598205bdeb389f3517ec686ed9c201
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4854384
    Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Reviewed-by: Hong Xu <xuhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1194396}

--- chrome/browser/chromeos/reporting/metric_reporting_prefs.cc
+++ chrome/browser/chromeos/reporting/metric_reporting_prefs.cc
@@ -4,10 +4,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/chromeos/reporting/metric_reporting_prefs.h"
+#include <algorithm>
 
 #include "base/check.h"
+#include "base/values.h"
 #include "chrome/browser/chromeos/reporting/metric_default_utils.h"
+#include "components/content_settings/core/common/content_settings_pattern.h"
 #include "components/pref_registry/pref_registry_syncable.h"
+#include "components/reporting/metrics/reporting_settings.h"
+#include "url/gurl.h"
 
 namespace reporting {
 
@@ -20,6 +25,35 @@ void RegisterProfilePrefs(::user_prefs::PrefRegistrySyncable* registry) {
       kReportWebsiteTelemetryCollectionRateMs,
       ::reporting::metrics::kDefaultWebsiteTelemetryCollectionRate
           .InMilliseconds());
+}
+
+bool IsWebsiteUrlAllowlisted(const GURL& url,
+                             const ReportingSettings* reporting_settings,
+                             const std::string& policy_setting) {
+  CHECK(reporting_settings);
+  if (!url.SchemeIsHTTPOrHTTPS()) {
+    // Unsupported/invalid URL scheme.
+    return false;
+  }
+
+  const base::Value::List* allowlisted_urls;
+  if (!reporting_settings->GetList(policy_setting, &allowlisted_urls)) {
+    // Policy likely unset. Disallow website metrics reporting regardless.
+    return false;
+  }
+
+  CHECK(allowlisted_urls);
+  auto it =
+      std::find_if(allowlisted_urls->begin(), allowlisted_urls->end(),
+                   [&url](const base::Value& value) {
+                     const ContentSettingsPattern pattern =
+                         ContentSettingsPattern::FromString(value.GetString());
+                     return pattern.Matches(url);
+                   });
+
+  // Return result of the match
+  const auto has_match = (it != allowlisted_urls->end());
+  return has_match;
 }
 
 }  // namespace reporting
