51e01a61ef7e92cf6ec185341100f3dbade5a9f0
Hao Liu <haoliuk@chromium.org>
2023-09-11 23:06:35 +0000

    Remove unused DocumentLoader::TakeNavigationTimingInfo()
    This is remove DocumentLoader::TakeNavigationTimingInfo() that is
    unused. See
    https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/loader/document_loader.h;bpv=1;bpt=1?q=TakeNavigationTimingInfo
    
    Bug: 1476917
    Change-Id: Ib96da4c471d4818b4b4ba933a8bbdbc5a4f23d82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4847561
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Hao Liu <haoliuk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195104}

--- third_party/blink/renderer/core/loader/document_loader.h
+++ third_party/blink/renderer/core/loader/document_loader.h
@@ -154,8 +154,6 @@ class CORE_EXPORT DocumentLoader : public GarbageCollected<DocumentLoader>,
 
   LocalFrame* GetFrame() const { return frame_; }
 
-  mojom::blink::ResourceTimingInfoPtr TakeNavigationTimingInfo();
-
   void DetachFromFrame(bool flush_microtask_queue);
 
   uint64_t MainResourceIdentifier() const;
