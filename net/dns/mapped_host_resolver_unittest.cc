59a1e733297a45687089bd2fefe8c5cbb008f6c6
Hayato Ito <hayato@chromium.org>
2023-09-11 02:38:41 +0000

    Use the standard-compliant forbidden character '^' as an invalid URL
    Currently, in the rewriting rule of the test runners, "~NOTFOUND" is
    used as an invalid URL. However, '~' is NOT a forbidden character
    according to the URL standard.
    
    Chromium is addressing [1] this non-compliant behavior and will permit
    '~' character in URL hosts. Thus, we can no longer use "~NOTFOUND" as
    it will become a valid URL.
    
    As an alternative, any invalid character is acceptable. Let's use '^'
    because '^' is a forbidden character both before and after the CL [1].
    
    The PR for WPT Chrome runner is:
    https://github.com/web-platform-tests/wpt/pull/41840/
    
    [1]: https://crrev.com/c/4823237
    
    Bug: 1416013
    Change-Id: Id8767ab49b54e778da53113aa4995e098bdbee59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4845589
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Chidera Olibie <colibie@google.com>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194658}

--- net/dns/mapped_host_resolver_unittest.cc
+++ net/dns/mapped_host_resolver_unittest.cc
@@ -350,7 +350,7 @@ TEST(MappedHostResolverTest, MapToError) {
       std::make_unique<MappedHostResolver>(std::move(resolver_impl));
 
   // Remap *.google.com to resolving failures.
-  EXPECT_TRUE(resolver->AddRuleFromString("MAP *.google.com ~NOTFOUND"));
+  EXPECT_TRUE(resolver->AddRuleFromString("MAP *.google.com ^NOTFOUND"));
 
   // Try resolving www.google.com --> Should give an error.
   TestCompletionCallback callback1;
@@ -384,7 +384,7 @@ TEST(MappedHostResolverTest, MapHostWithSchemeToError) {
   // Create a remapped resolver that uses `resolver_impl`.
   auto resolver =
       std::make_unique<MappedHostResolver>(std::move(resolver_impl));
-  ASSERT_TRUE(resolver->AddRuleFromString("MAP host.test ~NOTFOUND"));
+  ASSERT_TRUE(resolver->AddRuleFromString("MAP host.test ^NOTFOUND"));
 
   std::unique_ptr<HostResolver::ResolveHostRequest> request =
       resolver->CreateRequest(
