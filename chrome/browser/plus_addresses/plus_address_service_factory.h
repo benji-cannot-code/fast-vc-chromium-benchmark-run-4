a9476a9927256488361cee940f9828b235b77517
Kirubel Aklilu <kaklilu@chromium.org>
2023-09-11 15:07:55 +0000

    Try to init PlusAddressService on profile creation
    It's created lazily right now (deferred until the first Autofill use of
    PlusAddresses). We need this service to be around beforehand to
    prepopulate the PlusAddressService with plus addresses that it fetches
    from the remote PlusAddress server.
    
    Bug: 1467623, b/297366505
    Change-Id: Iee1bf1122a9eacdb3ff8c8480ec0a72f85c6e42d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4854202
    Reviewed-by: Matt Reichhoff <mreichhoff@chromium.org>
    Commit-Queue: Kirubel Aklilu <kaklilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194855}

--- chrome/browser/plus_addresses/plus_address_service_factory.h
+++ chrome/browser/plus_addresses/plus_address_service_factory.h
@@ -39,6 +39,7 @@ class PlusAddressServiceFactory : public ProfileKeyedServiceFactory {
   // BrowserContextKeyedServiceFactory:
   std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
+  bool ServiceIsCreatedWithBrowserContext() const override;
 };
 
 #endif  // CHROME_BROWSER_PLUS_ADDRESSES_PLUS_ADDRESS_SERVICE_FACTORY_H_
