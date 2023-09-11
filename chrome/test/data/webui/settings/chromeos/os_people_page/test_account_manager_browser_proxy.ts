3f510a7d843587e4fc6414cbb162d45e4b5cfbb2
Ruma Kesh <rkesh@google.com>
2023-09-11 20:52:18 +0000

    CrOS Settings: Convert people page account manager subpage browser test to TS
    Additionally, renamed the test and moved the test under the `os_people_page/` folder to mirror the app folder structure.
    
    Bug: b/270728282
    Test: browser_tests --gtest_filter="*OsPeoplePageAccountManagerSubpage*"
    Change-Id: If4968a18e33eaf47050853c4be4e4817f23bfee5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4854724
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Ruma Kesh <rkesh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1195041}

--- chrome/test/data/webui/settings/chromeos/os_people_page/test_account_manager_browser_proxy.ts
+++ chrome/test/data/webui/settings/chromeos/os_people_page/test_account_manager_browser_proxy.ts
@@ -34,6 +34,7 @@ export class TestAccountManagerBrowserProxy extends TestBrowserProxy implements
         pic: 'data:image/png;base64,primaryAccountPicData',
         email: 'primary@gmail.com',
         isAvailableInArc: true,
+        organization: 'Family Link',
       },
       {
         id: '456',
@@ -41,7 +42,7 @@ export class TestAccountManagerBrowserProxy extends TestBrowserProxy implements
         isDeviceAccount: false,
         isSignedIn: true,
         unmigrated: false,
-        isManaged: false,
+        isManaged: true,
         fullName: 'Secondary Account 1',
         email: 'user1@example.com',
         pic: '',
@@ -56,6 +57,18 @@ export class TestAccountManagerBrowserProxy extends TestBrowserProxy implements
         isManaged: false,
         fullName: 'Secondary Account 2',
         email: 'user2@example.com',
+        pic: '',
+        isAvailableInArc: false,
+      },
+      {
+        id: '1010',
+        accountType: 1,
+        isDeviceAccount: false,
+        isSignedIn: false,
+        unmigrated: true,
+        isManaged: false,
+        fullName: 'Secondary Account 3',
+        email: 'user3@example.com',
         pic: '',
         isAvailableInArc: false,
       },
@@ -80,5 +93,33 @@ export class TestAccountManagerBrowserProxy extends TestBrowserProxy implements
 
   changeArcAvailability(account: Account, isAvailableInArc: boolean): void {
     this.methodCalled('changeArcAvailability', [account, isAvailableInArc]);
+  }
+}
+
+export class TestAccountManagerBrowserProxyForUnmanagedAccounts extends
+    TestAccountManagerBrowserProxy {
+  constructor() {
+    super();
+  }
+
+  override getAccounts(): Promise<Account[]> {
+    this.methodCalled('getAccounts');
+
+    return new Promise((resolve) => {
+      resolve([
+        {
+          id: '123',
+          accountType: 1,
+          isDeviceAccount: true,
+          isSignedIn: true,
+          unmigrated: false,
+          isManaged: false,
+          fullName: 'Device Account',
+          email: 'admin@domain.com',
+          pic: 'data:image/png;base64,abc123',
+          isAvailableInArc: false,
+        },
+      ]);
+    });
   }
 }
---
+++ chrome/test/data/webui/settings/chromeos/os_people_page/test_parental_controls_browser_proxy.ts
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {ParentalControlsBrowserProxy} from 'chrome://os-settings/os_settings.js';
+import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
+
+export class TestParentalControlsBrowserProxy extends TestBrowserProxy
+    implements ParentalControlsBrowserProxy {
+  constructor() {
+    super([
+      'showAddSupervisionDialog',
+      'launchFamilyLinkSettings',
+    ]);
+  }
+
+  launchFamilyLinkSettings(): void {
+    this.methodCalled('launchFamilyLinkSettings');
+  }
+
+  showAddSupervisionDialog(): void {
+    this.methodCalled('showAddSupervisionDialog');
+  }
+}
