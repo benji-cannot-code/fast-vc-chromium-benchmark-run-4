66946bdaf2a3efdf84c969bc7c25d8b77e63957d
David Roger <droger@chromium.org>
2023-09-11 11:41:35 +0000

    [profiles] Add assertions to protect against empty profile name
    Users of the ProfileNameResolver expect that the name is never empty.
    However, as seen in bug https://crbug.com/1479101, this is sometimes
    happening.
    
    This CL adds more CHECKs, so that the bug can be caught earlier and
    hopefully will give more information about the scenario at play.
    
    Bug: 1479101
    Change-Id: Iaa449be4fc8ed23af0581a17aaba105b5c2c5f9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4855979
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194778}

--- chrome/browser/ui/profiles/profile_customization_util.cc
+++ chrome/browser/ui/profiles/profile_customization_util.cc
@@ -120,6 +120,7 @@ ProfileNameResolver::ProfileNameResolver(
   std::u16string fallback_profile_name =
       profiles::GetDefaultNameForNewSignedInProfileWithIncompleteInfo(
           extended_account_info);
+  CHECK(!fallback_profile_name.empty());
   extended_account_info_timeout_closure_.Reset(
       base::BindOnce(&ProfileNameResolver::OnProfileNameResolved,
                      weak_ptr_factory_.GetWeakPtr(), fallback_profile_name));
@@ -148,13 +149,15 @@ void ProfileNameResolver::OnExtendedAccountInfoUpdated(
     return;
   }
 
-  OnProfileNameResolved(
-      profiles::GetDefaultNameForNewSignedInProfile(account_info));
+  std::u16string profile_name =
+      profiles::GetDefaultNameForNewSignedInProfile(account_info);
+  CHECK(!profile_name.empty());
+  OnProfileNameResolved(profile_name);
 }
 
 void ProfileNameResolver::OnProfileNameResolved(
     const std::u16string& profile_name) {
-  DCHECK(!profile_name.empty());
+  CHECK(!profile_name.empty());
   DCHECK(resolved_profile_name_.empty());  // Should be resolved only once.
 
   // Cancel timeout and stop listening to further changes.
