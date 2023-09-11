5325a7f7840ef195989c3a5f60703d46485fee26
wd l <liuwd8@gmail.com>
2023-09-11 11:47:09 +0000

    Use unified function to check IsOutermostMainFrame
    Change-Id: I351861c4e2a03a8f03bd7a84904e6473aac384af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4853245
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194779}

--- chrome/renderer/supervised_user/supervised_user_error_page_controller.cc
+++ chrome/renderer/supervised_user/supervised_user_error_page_controller.cc
@@ -18,14 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "v8/include/v8-context.h"
 #include "v8/include/v8-microtask-queue.h"
 
-namespace {
-
-bool IsOutermostMainFrame(content::RenderFrame* render_frame) {
-  return render_frame->IsMainFrame() && !render_frame->IsInFencedFrameTree();
-}
-
-}  // namespace
-
 gin::WrapperInfo SupervisedUserErrorPageController::kWrapperInfo = {
     gin::kEmbedderNativeGin};
 
@@ -90,7 +82,7 @@ void SupervisedUserErrorPageController::RequestUrlAccessLocal() {
 void SupervisedUserErrorPageController::OnRequestUrlAccessRemote(bool success) {
   std::string result = success ? "true" : "false";
   std::string is_outermost_main_frame =
-      IsOutermostMainFrame(render_frame_) ? "true" : "false";
+      render_frame_->GetWebFrame()->IsOutermostMainFrame() ? "true" : "false";
   std::string js =
       base::StringPrintf("setRequestStatus(%s, %s)", result.c_str(),
                          is_outermost_main_frame.c_str());
