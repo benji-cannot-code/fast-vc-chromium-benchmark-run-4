9d232f5d836cb1de787b57833298aafc3b4e2205
Kouhei Ueno <kouhei@chromium.org>
2023-09-11 06:29:26 +0000

    Prerender2: prospective fix for keyboard input hangs when IME is present
    This is a prospective fix for Chrome on macOS not responsive for
    keyboard inputs iff all the below conditions hold:
    - The page was Prerender2-ed and activated successfully,
    - IME is enabled, and
    - RenderWidgetHostViewMac::GetCachedFirstRectForCharacterRange
      fast-path could not be used.
    
    Under this condition, somehow (suggests Mojo capability control bug?)
    `TextInputHost().GotFirstRectForRange()` call [1] doesn't make it to
    the browser UI thread, failing to unblock it waiting until 1500ms
    timeout kicks in. [2]
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/frame/local_frame_mojo_handler.cc;drc=71d3885ad42265ebd0f9b09fa1eb074314f25123;l=980
    [2] https://source.chromium.org/chromium/chromium/src/+/main:content/browser/renderer_host/text_input_client_mac.mm;l=108;drc=2a783dc765bc26cb7387dfff16bdf65fa4ecd9ca
    
    This CL attempts to fix the issue by allowlisting the TextInputHost
    mojom interface in the mojo capabilities control.
    
    Bug: 1480850, 1449566, 115920
    Change-Id: I962af4de897de02ff709a16ec3ebc58f93693731
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4852700
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194699}

--- content/browser/mojo_binder_policy_map_impl.cc
+++ content/browser/mojo_binder_policy_map_impl.cc
@@ -22,6 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/frame/back_forward_cache_controller.mojom.h"
 #include "third_party/blink/public/mojom/frame/frame.mojom.h"
 #include "third_party/blink/public/mojom/indexeddb/indexeddb.mojom.h"
+#if BUILDFLAG(IS_MAC)
+#include "third_party/blink/public/mojom/input/text_input_host.mojom.h"
+#endif
 #include "third_party/blink/public/mojom/loader/code_cache.mojom.h"
 #include "third_party/blink/public/mojom/manifest/manifest_observer.mojom.h"
 #include "third_party/blink/public/mojom/media/renderer_audio_output_stream_factory.mojom.h"
@@ -80,6 +83,15 @@ void RegisterNonAssociatedPoliciesForSameOriginPrerendering(
   // since we wait for a response from code cache when loading resources.
   map.SetNonAssociatedPolicy<blink::mojom::CodeCacheHost>(
       MojoBinderNonAssociatedPolicy::kGrant);
+
+#if BUILDFLAG(IS_MAC)
+  // Set policy to Grant for TextInputHost.
+  // This is used to return macOS IME sync call results to the browser process,
+  // and will hang entire Chrome if paused.
+  // This is a prospective fix added for crbug.com/1480850
+  map.SetNonAssociatedPolicy<blink::mojom::TextInputHost>(
+      MojoBinderNonAssociatedPolicy::kGrant);
+#endif
 }
 
 // Register policies for channel-associated interfaces registered in
