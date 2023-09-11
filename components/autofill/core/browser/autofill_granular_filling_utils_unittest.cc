425c436a3d8693ad43b622c325e51d6d3fdde24f
Bruno Braga <brunobraga@google.com>
2023-09-11 12:41:37 +0000

    [mullet m2] Remove obsolete FillingGranularity and update current usage.
    For group filling we will use a set of fields to specify the target.
    Therefore this enum is obsolete.
    
    Note: I also updates its usages to now handle a ServerFieldTypeSet.
    
    Bug: 1459990
    Change-Id: I7cb92ee4fc47768781fab4af0e1eaf30b6ed62b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4853621
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Auto-Submit: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1194793}

---
+++ components/autofill/core/browser/autofill_granular_filling_utils_unittest.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill/core/browser/autofill_granular_filling_utils.h"
+
+#include "components/autofill/core/browser/autofill_type.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace autofill {
+
+TEST(
+    AutofillGranularFillingUtilsTest,
+    AreFieldsGranularFillingGroup_ReturnsTrueWhenFieldsMatchAGroupFillingGroup) {
+  // Test name fields.
+  EXPECT_TRUE(AreFieldsGranularFillingGroup(
+      GetServerFieldTypesOfGroup(FieldTypeGroup::kName)));
+  // Test phone fields.
+  EXPECT_TRUE(AreFieldsGranularFillingGroup(
+      GetServerFieldTypesOfGroup(FieldTypeGroup::kPhone)));
+  // Tests address fields, which in the context of granular filling
+  // are both `FieldTypeGroup::kName` and `FieldTypeGroup::kCompany` fields.
+  EXPECT_TRUE(AreFieldsGranularFillingGroup(GetAddressFieldsForGroupFilling()));
+}
+
+// Granular filling address group also considers company to be an address field.
+// Therefore, a group containing ONLY address fields, should not be considered
+// a granular filling group.
+TEST(AutofillGranularFillingUtilsTest,
+     AreFieldsGranularFillingGroup_ReturnsFalseForAutofillAddressFieldsOnly) {
+  EXPECT_FALSE(AreFieldsGranularFillingGroup(
+      GetServerFieldTypesOfGroup(FieldTypeGroup::kAddress)));
+}
+
+}  // namespace autofill
