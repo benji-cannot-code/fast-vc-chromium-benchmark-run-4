2dbea55dec931cd0013b39220c0fd0bb2b019edc
Neil Coronado <nemco@google.com>
2023-09-11 20:08:19 +0000

    [TabStripPerformance] Use correct method to emit metrics
    Use correct method to emit metrics.
    
    Additionally, add check that placeholder was prepared (so we only emit
    when we have a correct start time for "PlaceholderStripVisibleDuration."
    
    Bug: 1479509
    Change-Id: Id29aeacb8bce6aa48fa827c82bc6789bc669d41c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4850754
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Neil Coronado <nemco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1195015}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
@@ -675,7 +675,8 @@ public class StripLayoutHelper implements StripLayoutTab.StripLayoutTabDelegate 
             mSelectedOnStartup = mModel.isActiveModel();
             if (mPlaceholderStripReady) replacePlaceholdersForRestoredTabs();
         } else {
-            RecordHistogram.recordTimesHistogram(PLACEHOLDER_VISIBLE_DURATION_HISTOGRAM_NAME, 0L);
+            RecordHistogram.recordMediumTimesHistogram(
+                    PLACEHOLDER_VISIBLE_DURATION_HISTOGRAM_NAME, 0L);
 
             computeAndUpdateTabOrders(false, false);
         }
@@ -687,7 +688,7 @@ public class StripLayoutHelper implements StripLayoutTab.StripLayoutTabDelegate 
     protected void onTabStateInitialized() {
         mTabStateInitialized = true;
 
-        if (ChromeFeatureList.sTabStripStartupRefactoring.isEnabled()) {
+        if (ChromeFeatureList.sTabStripStartupRefactoring.isEnabled() && mPlaceholderStripReady) {
             int numLeftoverPlaceholders = 0;
             for (int i = 0; i < mStripTabs.length; i++) {
                 if (mStripTabs[i].getIsPlaceholder()) numLeftoverPlaceholders++;
