562458055e3ce5e9c37ff11c084d5f32c952e2cf
Dave Tapuska <dtapuska@google.com>
2023-09-11 14:41:39 +0000

    Rewrite blink::MainThreadIsolate in dom_distiller
    Use the AgentSchedulingGroup's isolate instead of the sky hook.
    
    Bug: 263412
    Change-Id: Ib1a22fe0e30d0a048f74d2fb31d5360df63a7114
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4853941
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194841}

--- components/dom_distiller/content/renderer/distiller_native_javascript.cc
+++ components/dom_distiller/content/renderer/distiller_native_javascript.cc
@@ -16,7 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gin/arguments.h"
 #include "gin/function_template.h"
 #include "third_party/blink/public/common/browser_interface_broker_proxy.h"
-#include "third_party/blink/public/web/blink.h"
+#include "third_party/blink/public/platform/scheduler/web_agent_group_scheduler.h"
+#include "third_party/blink/public/web/web_local_frame.h"
 #include "v8/include/v8.h"
 
 namespace {
@@ -57,7 +58,8 @@ void DistillerNativeJavaScript::StoreFloatFontScaling(float float_font_scale) {
 
 void DistillerNativeJavaScript::AddJavaScriptObjectToFrame(
     v8::Local<v8::Context> context) {
-  v8::Isolate* isolate = blink::MainThreadIsolate();
+  v8::Isolate* isolate =
+      render_frame_->GetWebFrame()->GetAgentGroupScheduler()->Isolate();
   v8::HandleScope handle_scope(isolate);
   if (context.IsEmpty())
     return;
