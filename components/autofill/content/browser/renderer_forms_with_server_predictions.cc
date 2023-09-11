1898e3e664f60fb48da8eba6593bce1bce848783
Jan Keitel <jkeitel@google.com>
2023-09-11 12:20:59 +0000

    Simplify the signature of GetFormDataAndServerPredictions.
    With the move to observing prediction events, predictions are now passed
    form by form - there's no need to support multiple forms in this
    helper function anymore.
    
    Bug: 1466435
    Change-Id: I3cfc2cfde75391c1f4bf9658e52fc4c877896be1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4852187
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1194787}

--- components/autofill/content/browser/renderer_forms_with_server_predictions.cc
+++ components/autofill/content/browser/renderer_forms_with_server_predictions.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/flat_map.h"
-#include "base/containers/span.h"
 #include "base/feature_list.h"
 #include "components/autofill/content/browser/content_autofill_client.h"
 #include "components/autofill/content/browser/content_autofill_driver.h"
@@ -34,7 +33,7 @@ RendererFormsWithServerPredictions::FromBrowserForm(AutofillManager& manager,
   }
 
   FormDataAndServerPredictions form_and_predictions =
-      GetFormDataAndServerPredictions(base::make_span(&browser_form, 1u));
+      GetFormDataAndServerPredictions(*browser_form);
   RendererFormsWithServerPredictions result;
   result.predictions = std::move(form_and_predictions.predictions);
   if (base::FeatureList::IsEnabled(
@@ -46,7 +45,7 @@ RendererFormsWithServerPredictions::FromBrowserForm(AutofillManager& manager,
     const AutofillDriverRouter& router =
         client.GetAutofillDriverFactory()->router();
     std::vector<FormData> renderer_forms =
-        router.GetRendererForms(form_and_predictions.form_datas.front());
+        router.GetRendererForms(form_and_predictions.form_data);
 
     result.renderer_forms.reserve(renderer_forms.size());
     base::flat_map<LocalFrameToken, content::GlobalRenderFrameHostId>
@@ -79,7 +78,7 @@ RendererFormsWithServerPredictions::FromBrowserForm(AutofillManager& manager,
 
   ContentAutofillDriver& driver =
       static_cast<ContentAutofillDriver&>(manager.driver());
-  result.renderer_forms = {{std::move(form_and_predictions.form_datas[0]),
+  result.renderer_forms = {{std::move(form_and_predictions.form_data),
                             driver.render_frame_host()->GetGlobalId()}};
   return result;
 }
