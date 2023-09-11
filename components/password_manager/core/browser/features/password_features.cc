cf2796aaa7493a48889f5bb17a904c4e85a997b4
Viktor Semeniuk <vsemeniuk@google.com>
2023-09-11 13:24:56 +0000

    Clean-up launched password manager feature flags
    This CL removes kBiometricAuthenticationInSettings and
    kBiometricAuthenticationForFilling.
    
    Change-Id: I73c34782a1554807837c3ec966a0f55a6ffff85c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4843172
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1194811}

--- components/password_manager/core/browser/features/password_features.cc
+++ components/password_manager/core/browser/features/password_features.cc
@@ -6,19 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/features/password_features.h"
 
 namespace password_manager::features {
-#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
-// Enables biometric authentication before form filling.
-BASE_FEATURE(kBiometricAuthenticationForFilling,
-             "BiometricAuthenticationForFilling",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-#endif
-
-#if BUILDFLAG(IS_MAC)
-// Enables biometric authentication in settings.
-BASE_FEATURE(kBiometricAuthenticationInSettings,
-             "BiometricAuthenticationInSettings",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-#endif
 
 // Enables Biometrics for the Touch To Fill feature. This only effects Android.
 BASE_FEATURE(kBiometricTouchToFill,
