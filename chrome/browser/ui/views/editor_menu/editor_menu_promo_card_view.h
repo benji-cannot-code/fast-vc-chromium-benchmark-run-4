03d3804fa75b1af0328cc187df4296b2d443213f
Michelle <michellegc@google.com>
2023-09-11 20:13:31 +0000

    Adjust editor menu promo card bounds.
    Make some adjustments to EditorMenuPromoCardView's bounds to better
    reflect the specs:
    
    1. Set the min width and max width according to the specs.
    
    2. Add some logic to adjust the promo card position if is horizontally
       offscreen.
    
    3. Use longer placeholder text for the description so that we can check
       multiline text is sized correctly. (E.g. this surfaces an issue with
       resizing the multiline label similar to crbug.com/c/678337. Issue
       seems hard to fix so we add a workaround by explicitly setting the
       labels' maximum width.)
    
    Logic for adjusting the promo card position is not perfect yet, e.g. if
    anchor view is very tall, then placing the promo card above or below
    will both place the promo card partially offscreen. But, it should be
    good enough for now. In a followup, consider improving the logic and
    also refactoring so that some UpdateBounds logic can be shared with
    EditorMenuView (b:297296089).
    
    Bug: b:295061567
    Change-Id: I1e046cac7ea49a2f901529751ff6b45d66445486
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4852421
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1195019}

--- chrome/browser/ui/views/editor_menu/editor_menu_promo_card_view.h
+++ chrome/browser/ui/views/editor_menu/editor_menu_promo_card_view.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget_observer.h"
 
 namespace views {
+class Label;
 class MdTextButton;
 }
 
@@ -67,6 +68,8 @@ class EditorMenuPromoCardView : public views::View,
   // `delegate_` outlives `this`.
   raw_ptr<EditorMenuViewDelegate> delegate_ = nullptr;
 
+  raw_ptr<views::Label> title_ = nullptr;
+  raw_ptr<views::Label> description_ = nullptr;
   raw_ptr<views::MdTextButton> dismiss_button_ = nullptr;
   raw_ptr<views::MdTextButton> tell_me_more_button_ = nullptr;
 
