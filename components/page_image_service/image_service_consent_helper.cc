7acff445fc8db8c35d0461adc3d937e0c79ce5f0
Sophie Chang <sophiechang@chromium.org>
2023-09-11 18:42:21 +0000

    Add new time out state to image service consent histogram
    OBSOLETE_HISTOGRAMS=Patterned histogram PageImageService.ConsentSuccess{ClientId} is replaced by PageImageService.ConsentStatus{ClientId}.
    
    Bug: 1425071
    Change-Id: I84d436e6d5cc44e6b953e942e12c0a5f2286c0e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4854269
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194962}

--- components/page_image_service/image_service_consent_helper.cc
+++ components/page_image_service/image_service_consent_helper.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "components/page_image_service/features.h"
+#include "components/page_image_service/metrics_util.h"
 #include "components/sync/service/sync_service.h"
 #include "components/unified_consent/consent_throttle.h"
 #include "components/unified_consent/url_keyed_data_collection_consent_helper.h"
@@ -16,6 +17,13 @@ namespace page_image_service {
 namespace {
 
 constexpr base::TimeDelta kTimeout = base::Seconds(10);
+
+void RunConsentThrottleCallback(
+    base::OnceCallback<void(PageImageServiceConsentStatus)> callback,
+    bool success) {
+  std::move(callback).Run(success ? PageImageServiceConsentStatus::kSuccess
+                                  : PageImageServiceConsentStatus::kFailure);
+}
 
 }  // namespace
 
@@ -43,15 +51,18 @@ ImageServiceConsentHelper::ImageServiceConsentHelper(
 ImageServiceConsentHelper::~ImageServiceConsentHelper() = default;
 
 void ImageServiceConsentHelper::EnqueueRequest(
-    base::OnceCallback<void(bool)> callback) {
+    base::OnceCallback<void(PageImageServiceConsentStatus)> callback) {
   if (consent_throttle_) {
-    consent_throttle_->EnqueueRequest(std::move(callback));
+    consent_throttle_->EnqueueRequest(
+        base::BindOnce(&RunConsentThrottleCallback, std::move(callback)));
     return;
   }
 
   absl::optional<bool> consent_status = GetConsentStatus();
   if (consent_status.has_value()) {
-    std::move(callback).Run(*consent_status);
+    std::move(callback).Run(*consent_status
+                                ? PageImageServiceConsentStatus::kSuccess
+                                : PageImageServiceConsentStatus::kFailure);
     return;
   }
 
@@ -75,7 +86,9 @@ void ImageServiceConsentHelper::OnStateChanged(
   }
 
   for (auto& request_callback : enqueued_request_callbacks_) {
-    std::move(request_callback).Run(*consent_status);
+    std::move(request_callback)
+        .Run(*consent_status ? PageImageServiceConsentStatus::kSuccess
+                             : PageImageServiceConsentStatus::kFailure);
   }
 
   enqueued_request_callbacks_.clear();
@@ -107,7 +120,7 @@ absl::optional<bool> ImageServiceConsentHelper::GetConsentStatus() {
 
 void ImageServiceConsentHelper::OnTimeoutExpired() {
   for (auto& request_callback : enqueued_request_callbacks_) {
-    std::move(request_callback).Run(false);
+    std::move(request_callback).Run(PageImageServiceConsentStatus::kTimedOut);
   }
   enqueued_request_callbacks_.clear();
 }
