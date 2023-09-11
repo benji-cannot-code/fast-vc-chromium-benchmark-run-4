5c47c70da836e2a00c3aeafe614bfffbf42bcaba
wenyu zhang <zhangwenyu@google.com>
2023-09-11 22:45:26 +0000

    flag: Remove shimless-rma-diagnostic-page feature flag
    Original CL adding the flag: https://crrev.com/c/4356439.
    
    Bug: b/298392412
    Change-Id: Ie87269137008acaf37a5581689ab2f91492c6f3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4833571
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Wenyu Zhang <zhangwenyu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1195087}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3916,9 +3916,6 @@ extern const char kShimlessRMAComplianceCheckDescription[];
 extern const char kShimlessRMAOsUpdateName[];
 extern const char kShimlessRMAOsUpdateDescription[];
 
-extern const char kShimlessRMADiagnosticPageName[];
-extern const char kShimlessRMADiagnosticPageDescription[];
-
 extern const char kShortcutCustomizationJellyName[];
 extern const char kShortcutCustomizationJellyDescription[];
 
