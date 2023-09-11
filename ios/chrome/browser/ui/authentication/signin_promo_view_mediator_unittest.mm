3d3b7173399a793ca088b58a076c25ce0a5d0975
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-09-11 16:20:19 +0000

    [ios] Rename `identity` in SigninPromoViewMediator
    No behavior change.
    I almost made a mistake because I thought this was always the identity
    selected to sign-in. If the user taps the secondary button, that's not
    necessarily true. Let's rename it to be more specific.
    
    Bug: 1479558
    Change-Id: I34b1d9c37595edfcb3d2a0444bd23152ac0f5f12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4853739
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1194888}

--- ios/chrome/browser/ui/authentication/signin_promo_view_mediator_unittest.mm
+++ ios/chrome/browser/ui/authentication/signin_promo_view_mediator_unittest.mm
@@ -155,7 +155,7 @@ class SigninPromoViewMediatorTest : public PlatformTest {
   // Tests the mediator with a new created configurator when no accounts are on
   // the device.
   void TestSigninPromoWithNoAccounts(SigninPromoViewStyle style) {
-    EXPECT_EQ(nil, mediator_.identity);
+    EXPECT_EQ(nil, mediator_.displayedIdentity);
     CheckNoAccountsConfigurator([mediator_ createConfigurator], style);
   }
 
@@ -232,7 +232,7 @@ class SigninPromoViewMediatorTest : public PlatformTest {
   // Expects the signin promo view to be configured when accounts are on the
   // device.
   void ExpectSigninWithAccountConfiguration(SigninPromoViewStyle style) {
-    EXPECT_EQ(identity_, mediator_.identity);
+    EXPECT_EQ(identity_, mediator_.displayedIdentity);
     OCMExpect(
         [signin_promo_view_ setMode:SigninPromoViewModeSigninWithAccount]);
     switch (style) {
@@ -579,7 +579,7 @@ TEST_F(SigninPromoViewMediatorTest, SigninPromoWhileSignedIn) {
   CreateMediator(signin_metrics::AccessPoint::ACCESS_POINT_RECENT_TABS);
   ExpectConfiguratorNotification(NO /* identity changed */);
   [mediator_ signinPromoViewIsVisible];
-  EXPECT_EQ(identity_, mediator_.identity);
+  EXPECT_EQ(identity_, mediator_.displayedIdentity);
   fake_system_identity_manager()->WaitForServiceCallbacksToComplete();
   CheckSyncPromoWithAccountConfigurator(configurator_,
                                         SigninPromoViewStyleStandard);
