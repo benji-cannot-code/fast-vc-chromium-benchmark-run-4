01d9cfcaaf0aa1503dff0ba5a2991b92af43eaee
Paolo Severini <paolosev@microsoft.com>
2023-09-11 14:49:47 +0000

    [v8][etw] Enables filtering of ETW tracing by URL
    Implements a callback function called by v8 to selectively enable ETW
    tracing only for a set of URLs specified in a Windows Performance
    Recorder profile.
    The V8 counterpart is:
    https://chromium-review.googlesource.com/c/v8/v8/+/4775987.
    
    Bug: v8:11043
    Change-Id: Ied5a011b25c9df8846843fcb4cb3cf5748cb7b59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4776268
    Commit-Queue: Paolo Severini <paolosev@microsoft.com>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194850}

--- third_party/blink/renderer/bindings/core/v8/v8_initializer.cc
+++ third_party/blink/renderer/bindings/core/v8/v8_initializer.cc
@@ -109,6 +109,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+#if BUILDFLAG(IS_WIN)
+// Defined in v8_initializer_win.cc.
+bool FilterETWSessionByURLCallback(v8::Local<v8::Context> context,
+                                   const std::string& json_payload);
+#endif  // BUILDFLAG(IS_WIN)
+
 static String ExtractMessageForConsole(v8::Isolate* isolate,
                                        v8::Local<v8::Value> data) {
   DOMException* exception = V8DOMException::ToWrappable(isolate, data);
@@ -708,6 +714,10 @@ void InitializeV8Common(v8::Isolate* isolate) {
   isolate->SetHostInitializeImportMetaObjectCallback(
       HostGetImportMetaProperties);
   isolate->SetMetricsRecorder(std::make_shared<V8MetricsRecorder>(isolate));
+
+#if BUILDFLAG(IS_WIN)
+  isolate->SetFilterETWSessionByURLCallback(FilterETWSessionByURLCallback);
+#endif  // BUILDFLAG(IS_WIN)
 
   V8ContextSnapshot::EnsureInterfaceTemplates(isolate);
 
