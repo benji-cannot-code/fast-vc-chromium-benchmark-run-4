f9e02e5e080ff809d5f43026a9c93a3289dad551
Claudio M <claudiomagni@google.com>
2023-09-12 11:07:12 +0000

    language_packs: Add function that filters strings based on mapping
    This utility function is called in a variety of scenarios for Language
    Packs, like for example getting the list of DLC IDs to install given
    the locales corresponding to the currently enabled input methods.
    
    There is already a function in handwriting.h that does the same thing:
    MapIdsToHandwritingLocales(). This version is more generic and it's
    meant to replace the previous one. That will be done in another CL that
    will re-evaluate whether to move more logic to the Input Methods
    codebase.
    
    Bug: b:294302472
    Change-Id: I715e6f43b55387765cd8d6cc5ccec81c5b15c59b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4855871
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Claudio M <claudiomagni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195309}

--- chromeos/ash/components/language_packs/language_packs_util.cc
+++ chromeos/ash/components/language_packs/language_packs_util.cc
@@ -5,12 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/ash/components/language_packs/language_packs_util.h"
 
+#include "base/containers/flat_set.h"
+#include "base/containers/span.h"
+#include "base/functional/callback.h"
 #include "base/logging.h"
 #include "base/strings/string_util.h"
 #include "chromeos/ash/components/dbus/dlcservice/dlcservice_client.h"
 #include "components/language/core/common/locale_util.h"
 #include "components/session_manager/core/session_manager.h"
 #include "components/session_manager/session_manager_types.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/cros_system_api/dbus/dlcservice/dbus-constants.h"
 
 namespace ash::language_packs {
@@ -178,6 +182,21 @@ const std::string ResolveLocale(const std::string& feature_id,
 bool IsOobe() {
   return session_manager::SessionManager::Get()->session_state() ==
          session_manager::SessionState::OOBE;
+}
+
+base::flat_set<std::string> MapThenFilterStrings(
+    base::span<const std::string> inputs,
+    base::RepeatingCallback<absl::optional<std::string>(const std::string&)>
+        input_mapping) {
+  std::vector<std::string> output;
+  for (const auto& input : inputs) {
+    const absl::optional<std::string> result = input_mapping.Run(input);
+    if (result.has_value()) {
+      output.push_back(std::move(*result));
+    }
+  }
+
+  return output;
 }
 
 }  // namespace ash::language_packs
