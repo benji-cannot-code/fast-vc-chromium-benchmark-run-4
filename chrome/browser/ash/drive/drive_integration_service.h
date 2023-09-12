48eb7b9bf047162fe1cc0fb97783e5170a1a9130
François Degros <fdegros@chromium.org>
2023-09-12 04:11:12 +0000

    BulkPinning: Use Observer nested types
    BUG=b:299393828
    
    Change-Id: I8bfceb5ee466321358773dbdde009fa3946fb6f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4852704
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Auto-Submit: François Degros <fdegros@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195205}

--- chrome/browser/ash/drive/drive_integration_service.h
+++ chrome/browser/ash/drive/drive_integration_service.h
@@ -120,7 +120,7 @@ class DriveIntegrationServiceObserver : public base::CheckedObserver {
 class DriveIntegrationService : public KeyedService,
                                 public drivefs::DriveFsHost::MountObserver,
                                 drivefs::pinning::PinManager::Observer,
-                                ash::NetworkStateHandlerObserver {
+                                ash::NetworkStateHandler::Observer {
  public:
   using DriveFsMojoListenerFactory = base::RepeatingCallback<
       std::unique_ptr<drivefs::DriveFsBootstrapListener>()>;
@@ -468,7 +468,7 @@ class DriveIntegrationService : public KeyedService,
   void OnDrivePrefChanged();
   void OnMirroringPrefChanged();
 
-  // NetworkStateHandlerObserver implementation.
+  // NetworkStateHandler::Observer implementation.
   void PortalStateChanged(const ash::NetworkState*,
                           ash::NetworkState::PortalState portal_state) override;
   void DefaultNetworkChanged(const ash::NetworkState*) override;
@@ -517,7 +517,7 @@ class DriveIntegrationService : public KeyedService,
   PrefChangeRegistrar registrar_;
 
   base::ScopedObservation<ash::NetworkStateHandler,
-                          ash::NetworkStateHandlerObserver>
+                          ash::NetworkStateHandler::Observer>
       network_state_handler_{this};
 
   // Note: This should remain the last member so it'll be destroyed and
