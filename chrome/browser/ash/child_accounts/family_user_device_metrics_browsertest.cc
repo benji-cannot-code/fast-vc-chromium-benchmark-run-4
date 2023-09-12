3b7df513c6f91d94a915643dc04607f19df68640
Frank Liberato <liberato@chromium.org>
2023-09-12 23:55:45 +0000

    Revert "Add phone hub onboarding eligible condition"
    This reverts commit e2c22655beda26aaa902ef1aaa9560a7857d7d4e.
    
    Reason for revert: failing on:
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-dbg/34119/test-results?sortby=&groupby=
    
    2023-09-12T23:08:42.747041Z FATAL browser_tests[325873:325873]: [scalable_iph_browser_test_base.cc(114)] Check failed: !mock_delegate_. Mocks have already been set up.
    [...]
    #6 0x55e25ba19703 ash::ScalableIphBrowserTestBase::SetUpMocks()
    #7 0x55e25ba19661 ash::ScalableIphBrowserTestBase::SetUpOnMainThread()
    #8 0x55e2699e883b content::BrowserTestBase::ProxyRunTestOnMainThreadLoop()
    [...]
    
    Original change's description:
    > Add phone hub onboarding eligible condition
    >
    > - This CL adds x_CustomConditionPhoneHubOnboardingEligible. The
    >   condition is true if feature status of Phone Hub is either
    >   kEligiblePhoneButNotSetUp or kDisabled.
    > - This adds a dependency from ScalableIph to PhoneHubManager.
    >   PhoneHubManager is expected to be initialized at a specific timing.
    >   This CL makes sure that ScalableIph is initialized after
    >   PhoneHubManager.
    > - This CL fixes an issue with our OOBE flow test in our browser tests.
    >   - To fix the browser test, we had to change behavior of
    >     LoggedInUserMixin. Previously LoggedInUserMixin has configured
    >     LoginManagerMixin to skip post login screens by default. This CL
    >     makes it optional.
    >   - familiy_user_device_metrics_browsertest.cc was depending on the
    >     default behavior. This CL updates the test.
    >
    > Bug: b:295369951
    > Test: browser_tests --gtest_filter=*ScalableIph*
    > Change-Id: I361169d5ca7d531ed26f16cd94b3247be6edb94d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4845174
    > Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    > Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    > Reviewed-by: Courtney Wong <courtneywong@chromium.org>
    > Reviewed-by: Tao Wu <wutao@chromium.org>
    > Commit-Queue: Yuki Awano <yawano@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1195612}
    
    Bug: b:295369951
    Change-Id: I7bde487bc9268817bb1468c7db30d470e42de941
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4860192
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Owners-Override: Frank Liberato <liberato@google.com>
    Cr-Commit-Position: refs/heads/main@{#1195718}

--- chrome/browser/ash/child_accounts/family_user_device_metrics_browsertest.cc
+++ chrome/browser/ash/child_accounts/family_user_device_metrics_browsertest.cc
@@ -149,7 +149,6 @@ IN_PROC_BROWSER_TEST_P(FamilyUserDeviceMetricsTest, SingleUserCount) {
 IN_PROC_BROWSER_TEST_P(FamilyUserDeviceMetricsTest, LoginAsNewChildUser) {
   base::HistogramTester histogram_tester;
 
-  logged_in_user_mixin_.GetLoginManagerMixin()->SkipPostLoginScreens();
   logged_in_user_mixin_.GetLoginManagerMixin()->LoginAsNewChildUser();
   logged_in_user_mixin_.GetLoginManagerMixin()->WaitForActiveSession();
 
@@ -175,7 +174,6 @@ IN_PROC_BROWSER_TEST_P(FamilyUserDeviceMetricsTest, LoginAsNewChildUser) {
 IN_PROC_BROWSER_TEST_P(FamilyUserDeviceMetricsTest, LoginAsNewRegularUser) {
   base::HistogramTester histogram_tester;
 
-  logged_in_user_mixin_.GetLoginManagerMixin()->SkipPostLoginScreens();
   logged_in_user_mixin_.GetLoginManagerMixin()->LoginAsNewRegularUser();
   logged_in_user_mixin_.GetLoginManagerMixin()->WaitForActiveSession();
 
@@ -203,7 +201,6 @@ IN_PROC_BROWSER_TEST_P(FamilyUserDeviceMetricsTest, GuestUser) {
 
   user_manager_->AddGuestUser();
 
-  logged_in_user_mixin_.GetLoginManagerMixin()->SkipPostLoginScreens();
   logged_in_user_mixin_.GetLoginManagerMixin()->LoginAsNewRegularUser();
   logged_in_user_mixin_.GetLoginManagerMixin()->WaitForActiveSession();
 
@@ -230,7 +227,6 @@ class FamilyUserDeviceMetricsManagedDeviceTest
     : public FamilyUserDeviceMetricsTest {
  protected:
   void LoginAsNewRegularUser() {
-    logged_in_user_mixin_.GetLoginManagerMixin()->SkipPostLoginScreens();
     logged_in_user_mixin_.GetLoginManagerMixin()->LoginAsNewRegularUser();
     logged_in_user_mixin_.GetLoginManagerMixin()->WaitForActiveSession();
   }
--- chrome/browser/scalable_iph/scalable_iph_factory_impl.cc
+++ chrome/browser/scalable_iph/scalable_iph_factory_impl.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_features.h"
 #include "base/logging.h"
 #include "base/no_destructor.h"
-#include "chrome/browser/ash/phonehub/phone_hub_manager_factory.h"
 #include "chrome/browser/ash/policy/core/browser_policy_connector_ash.h"
 #include "chrome/browser/ash/printing/synced_printers_manager_factory.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
@@ -22,8 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/browser_context_helper/browser_context_helper.h"
 #include "chromeos/ash/components/scalable_iph/scalable_iph.h"
 #include "chromeos/ash/components/scalable_iph/scalable_iph_delegate.h"
-#include "chromeos/ash/services/multidevice_setup/public/cpp/prefs.h"
-#include "chromeos/ash/services/multidevice_setup/public/mojom/multidevice_setup.mojom.h"
 #include "components/feature_engagement/public/tracker.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/user_manager/user.h"
@@ -57,7 +54,6 @@ ScalableIphFactoryImpl::ScalableIphFactoryImpl() {
   // to avoid circular dependencies from //chrome/browser.
   DependsOn(feature_engagement::TrackerFactory::GetInstance());
   DependsOn(ash::SyncedPrintersManagerFactory::GetInstance());
-  DependsOn(ash::phonehub::PhoneHubManagerFactory::GetInstance());
 }
 
 ScalableIphFactoryImpl::~ScalableIphFactoryImpl() = default;
@@ -93,15 +89,6 @@ content::BrowserContext* ScalableIphFactoryImpl::GetBrowserContextToUse(
   }
 
   if (profile->IsChild()) {
-    return nullptr;
-  }
-
-  if (!ash::multidevice_setup::IsFeatureAllowed(
-          ash::multidevice_setup::mojom::Feature::kPhoneHub,
-          profile->GetPrefs())) {
-    DLOG(WARNING) << "Phone hub feature is disabled by a policy. This is "
-                     "expected only for test code as we are returning early "
-                     "above if a profile is managed.";
     return nullptr;
   }
 
