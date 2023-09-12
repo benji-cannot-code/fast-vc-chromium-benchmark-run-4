c191e46c7f23ade9747dfc27a7aa22ccd07367f2
Harvey Yang <chenghaoyang@google.com>
2023-09-12 04:04:03 +0000

    accelerometer: Remove unused file implementations
    As all ChromeOS devices are using iioservice, we no longer need the
    file implementations for accelerometers.
    
    BUG=b:274875997
    TEST=run on strongbad
    
    Change-Id: I0b35e8c038da295a2c08d9dd062141cf867dcca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4829268
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Harvey Yang <chenghaoyang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195204}

--- ash/accelerometer/accelerometer_reader.cc
+++ ash/accelerometer/accelerometer_reader.cc
@@ -11,11 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/current_thread.h"
 #include "base/task/sequenced_task_runner.h"
 #include "chromeos/components/sensors/buildflags.h"
-#if BUILDFLAG(USE_IIOSERVICE)
 #include "ash/accelerometer/accelerometer_provider_mojo.h"
-#else  // !BUILDFLAG(USE_IIOSERVICE)
-#include "ash/accelerometer/accelerometer_file_reader.h"
-#endif  // BUILDFLAG(USE_IIOSERVICE)
 
 namespace ash {
 
@@ -56,11 +52,7 @@ void AccelerometerReader::SetECLidAngleDriverStatusForTesting(
 }
 
 AccelerometerReader::AccelerometerReader() {
-#if BUILDFLAG(USE_IIOSERVICE)
   accelerometer_provider_ = new AccelerometerProviderMojo();
-#else   // !BUILDFLAG(USE_IIOSERVICE)
-  accelerometer_provider_ = new AccelerometerFileReader();
-#endif  // BUILDFLAG(USE_IIOSERVICE)
 }
 
 AccelerometerReader::~AccelerometerReader() = default;
