6a620c019832f8281eb6744da201fd9fe99c5cf1
Hira Mahmood <hiramahmood@google.com>
2023-09-12 15:53:20 +0000

    [iOS] Create parcel tracking opt-in coordinator
    This CL creates the ParcelTrackingOptInCoordinator and triggers it
    through a new UI command after a parcel tracking number is detected. A
    follow-up CL will implement the coordinator's start and stop methods to
    initialize a new mediator and view controller for the opt-in UI.
    
    Design doc linked in the bug.
    
    tested by EGTest once feature UI is complete
    
    Low-Coverage-Reason: new annotations_tab_helper functionality will be
    Bug: 1473449
    Change-Id: I8490b81bd4e2a39837bc6175804b57f12b5487bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4823425
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: David Jean <djean@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1195426}

---
+++ ios/chrome/browser/shared/public/commands/parcel_tracking_opt_in_commands.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SHARED_PUBLIC_COMMANDS_PARCEL_TRACKING_OPT_IN_COMMANDS_H_
+#define IOS_CHROME_BROWSER_SHARED_PUBLIC_COMMANDS_PARCEL_TRACKING_OPT_IN_COMMANDS_H_
+
+#import "ios/web/public/annotations/custom_text_checking_result.h"
+
+// Commands related to the parcel tracking opt-in prompt.
+@protocol ParcelTrackingOptInCommands <NSObject>
+
+// Shows the parcel tracking opt-in prompt.
+- (void)showParcelTrackingOptInPromptWithParcels:
+            (NSArray<CustomTextCheckingResult*>*)parcels
+                                     forWebState:(web::WebState*)webState;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_SHARED_PUBLIC_COMMANDS_PARCEL_TRACKING_OPT_IN_COMMANDS_H_
--- ios/chrome/browser/ui/browser_view/browser_coordinator.mm
+++ ios/chrome/browser/ui/browser_view/browser_coordinator.mm
@@ -72,6 +72,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/public/commands/mini_map_commands.h"
 #import "ios/chrome/browser/shared/public/commands/new_tab_page_commands.h"
 #import "ios/chrome/browser/shared/public/commands/page_info_commands.h"
+#import "ios/chrome/browser/shared/public/commands/parcel_tracking_opt_in_commands.h"
 #import "ios/chrome/browser/shared/public/commands/password_breach_commands.h"
 #import "ios/chrome/browser/shared/public/commands/password_protection_commands.h"
 #import "ios/chrome/browser/shared/public/commands/password_suggestion_commands.h"
@@ -152,6 +153,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/overscroll_actions/overscroll_actions_controller.h"
 #import "ios/chrome/browser/ui/page_info/page_info_coordinator.h"
 #import "ios/chrome/browser/ui/page_info/requirements/page_info_presentation.h"
+#import "ios/chrome/browser/ui/parcel_tracking/parcel_tracking_opt_in_coordinator.h"
 #import "ios/chrome/browser/ui/passwords/account_storage_notice/passwords_account_storage_notice_coordinator.h"
 #import "ios/chrome/browser/ui/passwords/bottom_sheet/password_suggestion_bottom_sheet_coordinator.h"
 #import "ios/chrome/browser/ui/passwords/bottom_sheet/password_suggestion_bottom_sheet_coordinator_delegate.h"
@@ -276,7 +278,8 @@ enum class ToolbarKind {
     ToolbarAccessoryCoordinatorDelegate,
     URLLoadingDelegate,
     WebContentCommands,
-    WebNavigationNTPDelegate>
+    WebNavigationNTPDelegate,
+    ParcelTrackingOptInCommands>
 
 // Whether the coordinator is started.
 @property(nonatomic, assign, getter=isStarted) BOOL started;
@@ -375,6 +378,10 @@ enum class ToolbarKind {
 
 // Coordinator for Page Info UI.
 @property(nonatomic, strong) ChromeCoordinator* pageInfoCoordinator;
+
+// Coordinator for parcel tracking opt-in UI presentation.
+@property(nonatomic, strong)
+    ParcelTrackingOptInCoordinator* parcelTrackingOptInCoordinator;
 
 // Coordinator for the PassKit UI presentation.
 @property(nonatomic, strong) PassKitCoordinator* passKitCoordinator;
@@ -809,6 +816,7 @@ enum class ToolbarKind {
     @protocol(WebContentCommands),
     @protocol(DefaultBrowserPromoCommands),
     @protocol(MiniMapCommands),
+    @protocol(ParcelTrackingOptInCommands),
   ];
 
   for (Protocol* protocol in protocols) {
@@ -1367,6 +1375,9 @@ enum class ToolbarKind {
 
   [self.saveToPhotosCoordinator stop];
   self.saveToPhotosCoordinator = nil;
+
+  [self.parcelTrackingOptInCoordinator stop];
+  self.parcelTrackingOptInCoordinator = nil;
 }
 
 // Starts independent mediators owned by this coordinator.
@@ -2288,6 +2299,27 @@ enum class ToolbarKind {
 
   if (FollowBrowserAgent::FromBrowser(self.browser)) {
     FollowBrowserAgent::FromBrowser(self.browser)->ClearUIProviders();
+  }
+}
+
+#pragma mark - ParcelTrackingOptInCommands
+
+- (void)showParcelTrackingOptInPromptWithParcels:
+            (NSArray<CustomTextCheckingResult*>*)parcels
+                                     forWebState:(web::WebState*)webState {
+  [self dismissParcelTrackingOptInPrompt];
+  self.parcelTrackingOptInCoordinator = [[ParcelTrackingOptInCoordinator alloc]
+      initWithBaseViewController:self.viewController
+                         browser:self.browser
+                        webState:webState
+                         parcels:parcels];
+  [self.parcelTrackingOptInCoordinator start];
+}
+
+- (void)dismissParcelTrackingOptInPrompt {
+  if (self.parcelTrackingOptInCoordinator) {
+    [self.parcelTrackingOptInCoordinator stop];
+    self.parcelTrackingOptInCoordinator = nil;
   }
 }
 
