d4f008e597617239ec55401ccf996b1ebc2269f2
Fabian Sommer <fabiansommer@chromium.org>
2023-09-12 12:15:05 +0000

    [Lacros] Use ash binaries for PNaCl translator
    The PNaCl translator binaries are shipped with ash, but not with Lacros.
    Instead, pnacl was downloaded by the component updater on Lacros. This
    caused it to be unavailable for a few minutes in a new profile.
    Since the PNaCl binaries are not changing anymore, we can just reuse the
    ones shipped with ash.
    With this CL, Lacros creates a symlink to the ash pnacl binaries on
    startup.
    No longer register PNaCl in the component updater in Lacros.
    This also saves a few MB of disk space.
    
    Fixed: 1450745, b/260660168
    Change-Id: I911f1a79e63b60e317c04a4382f4b0d468c7171e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4811895
    Commit-Queue: Fabian Sommer <fabiansommer@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195326}

--- chrome/browser/component_updater/pnacl_component_installer.cc
+++ chrome/browser/component_updater/pnacl_component_installer.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "components/component_updater/component_installer.h"
 #include "components/component_updater/component_updater_service.h"
+#include "components/component_updater/component_updater_utils.h"
 #include "components/nacl/common/nacl_switches.h"
 #include "components/update_client/update_query_params.h"
 #include "components/update_client/utils.h"
@@ -260,6 +261,13 @@ void RegisterPnaclComponent(ComponentUpdateService* cus) {
   auto installer = base::MakeRefCounted<ComponentInstaller>(
       std::make_unique<PnaclComponentInstallerPolicy>());
   installer->Register(cus, base::OnceClosure());
+}
+
+void DeletePnaclComponent(const base::FilePath& user_data_dir) {
+  base::ThreadPool::PostTask(
+      FROM_HERE, {base::TaskPriority::BEST_EFFORT, base::MayBlock()},
+      base::BindOnce(base::IgnoreResult(&base::DeletePathRecursively),
+                     user_data_dir.Append(FILE_PATH_LITERAL("pnacl"))));
 }
 
 }  // namespace component_updater
--- chrome/browser/component_updater/pnacl_component_installer.h
+++ chrome/browser/component_updater/pnacl_component_installer.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_COMPONENT_UPDATER_PNACL_COMPONENT_INSTALLER_H_
 #define CHROME_BROWSER_COMPONENT_UPDATER_PNACL_COMPONENT_INSTALLER_H_
 
+namespace base {
+class FilePath;
+}  // namespace base
+
 namespace pnacl {
 // Returns true if PNaCl actually needs an on-demand component update.
 // E.g., if PNaCl is not yet installed and the user is loading a PNaCl app,
@@ -21,6 +25,8 @@ namespace component_updater {
 class ComponentUpdateService;
 
 void RegisterPnaclComponent(ComponentUpdateService* cus);
+
+void DeletePnaclComponent(const base::FilePath& user_data_dir);
 
 }  // namespace component_updater
 
