f340ffa964116e9b97080d2e58333d59ab29b278
Zoraiz Naeem <zoraiznaeem@chromium.org>
2023-09-12 22:40:24 +0000

    [Cleanup] Remove unnecessary usage of `ShadowController::UpdateShadowForWindow()`
    As part of crrev.com/c/4846299, ShouldUpdateShadowOnWindowPropertyChange
    was added onto the ShadowControllerDelegate. This method allows us to
    update shadow based on ChromeOS related property changes.
    
    This allows to get rid of calls to ShadowController::UpdateShadowForWindow().
    
    This CL removes the call to UpdateShadowForWindow() when
    kWindowStateKey property changes. We need to update shadow since
    shadow radius can change as window state changes.
    
    Bug: b:299168506
    Change-Id: Iac6003d35de2bdf7d25383270007ed429c3b5023
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4853953
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195678}

--- ash/wm/window_state.h
+++ ash/wm/window_state.h
@@ -491,8 +491,6 @@ class ASH_EXPORT WindowState : public aura::WindowObserver {
 
   explicit WindowState(aura::Window* window);
 
-  void Init();
-
   WindowStateDelegate* delegate() { return delegate_.get(); }
   BoundsChangeAnimationType bounds_animation_type() {
     return bounds_animation_type_;
