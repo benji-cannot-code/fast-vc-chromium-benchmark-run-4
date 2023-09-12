03e9004845e5734191b5672ad27f2161a4afc23d
Jérôme Lebel <jlebel@chromium.org>
2023-09-12 13:14:50 +0000

    [iOS] create the batch upload view
    This CL only add the UI. The mediator is waiting for the proper API to
    be implemented.
    
    This view can not be opened yet. It is waiting for change in the manage
    sync settings.
    
    To test, you must:
    * enable kSyncEnableBatchUploadLocalData to get the bulk upload button
    to display in the sync manager,
    * enable
    kSyncEnableBatchUploadLocalDataWithDummyDataForTesting to get dummy
    data, (and set
    SyncResponseDelayForBatchUploadLocalDataWithDummyDataForTesting to
    change waiting time). This can only be done in
    components/sync/base/features.cc as it corresponds to no
    chrome://flag entries
    
    Result can be seen on
    https://drive.google.com/file/d/18PpoQRdO0Y6NqPj9_eLShLPd6MLseSX3/view?usp=drive_link
    
    Fixed: 1468246
    Change-Id: I26a602591aa958d4d5a80c8e0f4020a08a3c5999
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4822868
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195347}

--- ios/chrome/browser/ui/settings/google_services/manage_sync_settings_command_handler.h
+++ ios/chrome/browser/ui/settings/google_services/manage_sync_settings_command_handler.h
@@ -34,6 +34,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // it doesn't close the current view.
 - (void)showManageYourGoogleAccount;
 
+// Open the view to batch upload data.
+- (void)openBulkUpload;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_SETTINGS_GOOGLE_SERVICES_MANAGE_SYNC_SETTINGS_COMMAND_HANDLER_H_
--- ios/chrome/browser/ui/settings/google_services/manage_sync_settings_coordinator.mm
+++ ios/chrome/browser/ui/settings/google_services/manage_sync_settings_coordinator.mm
@@ -40,6 +40,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/sync/sync_setup_service_factory.h"
 #import "ios/chrome/browser/ui/authentication/signout_action_sheet_coordinator.h"
 #import "ios/chrome/browser/ui/settings/google_services/accounts_table_view_controller.h"
+#import "ios/chrome/browser/ui/settings/google_services/bulk_upload/bulk_upload_coordinator.h"
+#import "ios/chrome/browser/ui/settings/google_services/bulk_upload/bulk_upload_coordinator_delegate.h"
 #import "ios/chrome/browser/ui/settings/google_services/manage_sync_settings_command_handler.h"
 #import "ios/chrome/browser/ui/settings/google_services/manage_sync_settings_constants.h"
 #import "ios/chrome/browser/ui/settings/google_services/manage_sync_settings_mediator.h"
@@ -56,6 +58,7 @@ using signin_metrics::PromoAction;
 using DismissViewCallback = SystemIdentityManager::DismissViewCallback;
 
 @interface ManageSyncSettingsCoordinator () <
+    BulkUploadCoordinatorDelegate,
     ManageSyncSettingsCommandHandler,
     ManageSyncSettingsTableViewControllerPresentationDelegate,
     SignoutActionSheetCoordinatorDelegate,
@@ -65,6 +68,8 @@ using DismissViewCallback = SystemIdentityManager::DismissViewCallback;
   std::unique_ptr<SyncObserverBridge> _syncObserver;
   // Whether Settings have been dismissed.
   BOOL _settingsAreDismissed;
+  // The coordinator for the view Save in Account.
+  BulkUploadCoordinator* _bulkUploadCoordinator;
 }
 
 // View controller.
@@ -174,6 +179,7 @@ using DismissViewCallback = SystemIdentityManager::DismissViewCallback;
 - (void)stop {
   [super stop];
   [self.mediator disconnect];
+  [self stopBulkUpload];
   self.mediator = nil;
   self.viewController = nil;
   // This coordinator displays the main view and it is in charge to enable sync
@@ -202,6 +208,12 @@ using DismissViewCallback = SystemIdentityManager::DismissViewCallback;
 }
 
 #pragma mark - Private
+
+- (void)stopBulkUpload {
+  [_bulkUploadCoordinator stop];
+  _bulkUploadCoordinator.delegate = nil;
+  _bulkUploadCoordinator = nil;
+}
 
 // Closes the Manage sync settings view controller.
 - (void)closeManageSyncSettings {
@@ -244,6 +256,16 @@ using DismissViewCallback = SystemIdentityManager::DismissViewCallback;
 }
 
 #pragma mark - ManageSyncSettingsCommandHandler
+
+- (void)openBulkUpload {
+  [self stopBulkUpload];
+  base::RecordAction(base::UserMetricsAction("BulkUploadSettingsOpen"));
+  _bulkUploadCoordinator = [[BulkUploadCoordinator alloc]
+      initWithBaseNavigationController:self.baseNavigationController
+                               browser:self.browser];
+  _bulkUploadCoordinator.delegate = self;
+  [_bulkUploadCoordinator start];
+}
 
 - (void)openWebAppActivityDialog {
   base::RecordAction(base::UserMetricsAction(
@@ -420,6 +442,13 @@ using DismissViewCallback = SystemIdentityManager::DismissViewCallback;
             accessPoint:AccessPoint::ACCESS_POINT_SETTINGS];
   [applicationCommands showSignin:signinCommand
                baseViewController:self.viewController];
+}
+
+#pragma mark - BulkUploadCoordinatorDelegate
+
+- (void)bulkUploadCoordinatorShouldStop:(BulkUploadCoordinator*)coordinator {
+  DCHECK_EQ(coordinator, _bulkUploadCoordinator);
+  [self stopBulkUpload];
 }
 
 #pragma mark - SyncObserverModelBridge
