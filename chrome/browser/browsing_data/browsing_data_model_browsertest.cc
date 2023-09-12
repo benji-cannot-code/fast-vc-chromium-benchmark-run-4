92684d3dfd6cc0e20e59131feeb3e3c058869d54
David Bienvenu <davidbienvenu@chromium.org>
2023-09-12 15:06:29 +0000

    win: Stop BrowsingDataModel tests creating shortcuts in startmenu
    Use ScopedPathOverride to prevent BrowsingDatamModelTests from creating
    shortcuts in the user's desktop start menu.
    
    Bug: 1457508
    Change-Id: Ic381b1201a23283574e50a7fc454ca1d46d2fc30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4858884
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195397}

--- chrome/browser/browsing_data/browsing_data_model_browsertest.cc
+++ chrome/browser/browsing_data/browsing_data_model_browsertest.cc
@@ -64,6 +64,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 #include "url/origin.h"
 
+#if BUILDFLAG(IS_WIN)
+#include "base/base_paths_win.h"
+#include "base/test/scoped_path_override.h"
+#endif  // BUILDFLAG(IS_WIN)
+
 using base::test::FeatureRef;
 using base::test::FeatureRefAndParams;
 
@@ -355,6 +360,11 @@ class BrowsingDataModelBrowserTest
   network::test::TrustTokenRequestHandler request_handler_;
 
  private:
+#if BUILDFLAG(IS_WIN)
+  // This is used to prevent creating shortcuts in the start menu dir.
+  base::ScopedPathOverride override_start_dir_{base::DIR_START_MENU};
+#endif  // BUILDFLAG(IS_WIN)
+
   base::test::ScopedFeatureList feature_list_;
   std::unique_ptr<net::EmbeddedTestServer> https_server_;
 };
