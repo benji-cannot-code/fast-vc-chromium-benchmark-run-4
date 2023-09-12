295fa65af33248487fff4f6a0619168db762449a
Dave Tapuska <dtapuska@chromium.org>
2023-09-12 13:47:23 +0000

    Rewrite more MainThreadIsolate in //components
    Use the AgentGroupScheduler's Isolate instead of the static sky
    hook.
    
    Bug: 263412
    Change-Id: Id61ee96f80106572970fc42858643fefae0c7020
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4854003
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195357}

--- components/plugins/renderer/plugin_placeholder.cc
+++ components/plugins/renderer/plugin_placeholder.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/renderer/v8_value_converter.h"
 #include "gin/object_template_builder.h"
 #include "third_party/blink/public/common/web_preferences/web_preferences.h"
-#include "third_party/blink/public/web/blink.h"
+#include "third_party/blink/public/platform/scheduler/web_agent_group_scheduler.h"
 #include "third_party/blink/public/web/web_dom_message_event.h"
 #include "third_party/blink/public/web/web_element.h"
 #include "third_party/blink/public/web/web_local_frame.h"
@@ -128,13 +128,13 @@ void PluginPlaceholderBase::NotifyPlaceholderReadyForTestingCallback() {
   blink::WebElement element = plugin()->Container()->GetElement();
   element.SetAttribute("placeholderReady", "true");
 
+  blink::WebLocalFrame* frame = element.GetDocument().GetFrame();
   base::Value value("placeholderReady");
   blink::WebSerializedScriptValue message_data =
       blink::WebSerializedScriptValue::Serialize(
-          blink::MainThreadIsolate(),
+          frame->GetAgentGroupScheduler()->Isolate(),
           content::V8ValueConverter::Create()->ToV8Value(
-              value,
-              element.GetDocument().GetFrame()->MainWorldScriptContext()));
+              value, frame->MainWorldScriptContext()));
   blink::WebDOMMessageEvent msg_event(message_data);
 
   plugin()->Container()->EnqueueMessageEvent(msg_event);
