4002142ff73523e8af0c4adad48984126f862f89
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2023-09-12 06:35:05 +0000

    Import wpt@61f49db98626648c2b218835dd1df2152c6d8dfd
    Using wpt-import in Chromium 3f80cdb13961f340aee5d2f2d15a8980ebb063d6.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    andruud@chromium.org:
      external/wpt/css/css-properties-values-api
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I18a9442ef9ff0e33ad40a5a2cdf4eb8494b5582f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4857858
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1195231}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/browsing_context/reload/__init__.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/browsing_context/reload/__init__.py
@@ -1,1 +1,41 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import pytest
+
+from webdriver.bidi.error import UnknownErrorException
+
+from ... import any_string
+
+
+async def reload_and_assert(
+    bidi_session,
+    context,
+    expected_error=False,
+    last_navigation=None,
+    url=None,
+    wait="complete",
+):
+    if expected_error:
+        with pytest.raises(UnknownErrorException):
+            await bidi_session.browsing_context.reload(
+                context=context['context'], wait=wait
+            )
+
+    else:
+        result = await bidi_session.browsing_context.reload(
+            context=context['context'], wait=wait
+        )
+
+        any_string(result["navigation"])
+        any_string(result["url"])
+
+        if last_navigation is not None:
+            assert result["navigation"] != last_navigation
+
+        if url is not None:
+            assert result["url"] == url
+
+            contexts = await bidi_session.browsing_context.get_tree(root=context['context'])
+            assert len(contexts) == 1
+            assert contexts[0]["url"] == url
+
+        return result
