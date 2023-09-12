e30c86cd73f3a05f989d7189969afa9ffc8e1415
Nicolas Dossou-Gbete <dgn@chromium.org>
2023-09-12 09:01:32 +0000

    🧇 Suppress the dialog for web app browser windows
    These windows are not dedicated to general purpose browsing and this
    would just make for a bad and confusing user experience.
    
    Fixed: b:299584825
    Change-Id: Ib373834efa2aab80ce6ce7a75f240217415d8717
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4853535
    Auto-Submit: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Jack Yammine <jyammine@google.com>
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195278}

--- chrome/browser/search_engine_choice/search_engine_choice_service.cc
+++ chrome/browser/search_engine_choice/search_engine_choice_service.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/web_applications/app_browser_controller.h"
 #include "components/prefs/pref_service.h"
 #include "components/search_engines/search_engine_choice_utils.h"
 #include "components/search_engines/search_engines_pref_names.h"
@@ -122,6 +123,13 @@ SearchEngineChoiceService::GetSearchEngines() {
 }
 
 bool SearchEngineChoiceService::CanShowDialog(Browser& browser) {
+  if (web_app::AppBrowserController::IsWebApp(&browser)) {
+    // Showing a Chrome-specific search engine dialog on top of a window
+    // dedicated to a specific web app is a horrible UX, we suppress it for this
+    // window. When the user proceeds to a non-web app window they will get it.
+    return false;
+  }
+
   // To avoid conflict, the dialog should not be shown if a sign-in dialog is
   // being currently displayed.
   if (browser.signin_view_controller()->ShowsModalDialog()) {
