13b1745dae546c147ff3c258102465100b5930a3
Abe Boujane <boujane@google.com>
2023-09-12 13:36:05 +0000

    Added functionality to the TrackingProtectionOnboarding service
    This includes the ability to mark profiles as eligible/onboarded, as
    well as recording the acknowledgment action.
    
    Bug: b:295926275
    Change-Id: I112b99c27a14b8943f75e5719b6c1372d2b51832
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4850941
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Abe Boujane <boujane@google.com>
    Auto-Submit: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1195354}

--- components/privacy_sandbox/tracking_protection_onboarding.cc
+++ components/privacy_sandbox/tracking_protection_onboarding.cc
@@ -11,6 +11,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace privacy_sandbox {
 
+namespace {
+
+using ::privacy_sandbox::tracking_protection::
+    TrackingProtectionOnboardingStatus;
+
+TrackingProtectionOnboardingStatus GetInternalOnboardingStatus(
+    PrefService* pref_service) {
+  return static_cast<TrackingProtectionOnboardingStatus>(
+      pref_service->GetInteger(prefs::kTrackingProtectionOnboardingStatus));
+}
+
+}  // namespace
+
 TrackingProtectionOnboarding::TrackingProtectionOnboarding(
     PrefService* pref_service)
     : pref_service_(pref_service) {
@@ -27,7 +40,8 @@ TrackingProtectionOnboarding::TrackingProtectionOnboarding(
 TrackingProtectionOnboarding::~TrackingProtectionOnboarding() = default;
 
 void TrackingProtectionOnboarding::OnOnboardingPrefChanged() const {
-  if (GetOnboardingStatus() != OnboardingStatus::kOnboarded) {
+  if (GetInternalOnboardingStatus(pref_service_) !=
+      TrackingProtectionOnboardingStatus::kOnboarded) {
     return;
   }
 
@@ -36,19 +50,69 @@ void TrackingProtectionOnboarding::OnOnboardingPrefChanged() const {
   }
 }
 
+void TrackingProtectionOnboarding::MaybeMarkEligible() {
+  auto status = GetInternalOnboardingStatus(pref_service_);
+  if (status != TrackingProtectionOnboardingStatus::kIneligible) {
+    return;
+  }
+  pref_service_->SetTime(prefs::kTrackingProtectionEligibleSince,
+                         base::Time::Now());
+  pref_service_->SetInteger(
+      prefs::kTrackingProtectionOnboardingStatus,
+      static_cast<int>(
+          TrackingProtectionOnboarding::OnboardingStatus::kEligible));
+}
+
+void TrackingProtectionOnboarding::NoticeShown() {
+  auto status = GetInternalOnboardingStatus(pref_service_);
+  if (status != TrackingProtectionOnboardingStatus::kEligible) {
+    return;
+  }
+  pref_service_->SetTime(prefs::kTrackingProtectionOnboardedSince,
+                         base::Time::Now());
+  pref_service_->SetInteger(
+      prefs::kTrackingProtectionOnboardingStatus,
+      static_cast<int>(
+          TrackingProtectionOnboarding::OnboardingStatus::kOnboarded));
+}
+
+void TrackingProtectionOnboarding::NoticeActionTaken(
+    TrackingProtectionOnboarding::NoticeAction action) {
+  switch (action) {
+    case NoticeAction::kNone:
+      return;
+    case NoticeAction::kGotIt:
+    case NoticeAction::kSettings:
+    case NoticeAction::kLearnMore:
+    case NoticeAction::kClosed:
+      pref_service_->SetBoolean(prefs::kTrackingProtectionOnboardingAcked,
+                                true);
+      return;
+  }
+}
+
+bool TrackingProtectionOnboarding::ShouldShowOnboardingNotice() {
+  auto onboarding_status = GetInternalOnboardingStatus(pref_service_);
+  switch (onboarding_status) {
+    case TrackingProtectionOnboardingStatus::kIneligible:
+      return false;
+    case TrackingProtectionOnboardingStatus::kEligible:
+      return true;
+    case TrackingProtectionOnboardingStatus::kOnboarded:
+      return !pref_service_->GetBoolean(
+          prefs::kTrackingProtectionOnboardingAcked);
+  }
+}
+
 TrackingProtectionOnboarding::OnboardingStatus
 TrackingProtectionOnboarding::GetOnboardingStatus() const {
-  auto onboarding_status =
-      static_cast<tracking_protection::TrackingProtectionOnboardingStatus>(
-          pref_service_->GetInteger(
-              prefs::kTrackingProtectionOnboardingStatus));
-
+  auto onboarding_status = GetInternalOnboardingStatus(pref_service_);
   switch (onboarding_status) {
-    case tracking_protection::TrackingProtectionOnboardingStatus::kIneligible:
+    case TrackingProtectionOnboardingStatus::kIneligible:
       return OnboardingStatus::kIneligible;
-    case tracking_protection::TrackingProtectionOnboardingStatus::kEligible:
+    case TrackingProtectionOnboardingStatus::kEligible:
       return OnboardingStatus::kEligible;
-    case tracking_protection::TrackingProtectionOnboardingStatus::kOnboarded:
+    case TrackingProtectionOnboardingStatus::kOnboarded:
       return OnboardingStatus::kOnboarded;
   }
 }
