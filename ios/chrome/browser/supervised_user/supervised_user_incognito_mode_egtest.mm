b6326845a8a0f9ab26356e6c3a71e7c6293cd0be
Nohemi Fernandez <fernandex@chromium.org>
2023-09-12 12:59:47 +0000

    Revert "[iOS] Use camel case for "Learn More" link in incognito tab grid."
    This reverts commit 421e4f80a4313849156863b055d259987cb67b88.
    
    Reason for revert: On further analysis by the UX team, we will not need to change the capitalization in this case. 
    
    Original change's description:
    > [iOS] Use camel case for "Learn More" link in incognito tab grid.
    >
    > Fixed: b/295143684
    > Change-Id: I73e85c7b5d635f09f6f2e07db412fa3d6543cf4f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4808110
    > Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    > Auto-Submit: Nohemi Fernandez <fernandex@chromium.org>
    > Reviewed-by: Duong Dac <ddac@chromium.org>
    > Commit-Queue: Duong Dac <ddac@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1187890}
    
    Change-Id: I1cb8515b0af49ad430a872f436011e665b9feafe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4858739
    Reviewed-by: Duong Dac <ddac@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195342}

--- ios/chrome/browser/supervised_user/supervised_user_incognito_mode_egtest.mm
+++ ios/chrome/browser/supervised_user/supervised_user_incognito_mode_egtest.mm
@@ -30,8 +30,8 @@ namespace {
 NSString* const kTestSupervisedIncognitoMessage =
     @"Your account is managed by your parent.";
 
-// Label used to find the 'Learn More' link.
-NSString* const kTestLearnMoreLabel = @"Learn More";
+// Label used to find the 'Learn more' link.
+NSString* const kTestLearnMoreLabel = @"Learn more";
 
 }  // namespace
 
@@ -132,7 +132,7 @@ NSString* const kTestLearnMoreLabel = @"Learn More";
                                           kTestSupervisedIncognitoMessage)]
       assertWithMatcher:grey_notNil()];
 
-  // Check that the "Learn More" link works.
+  // Check that the "Learn more" link works.
   [[EarlGrey
       selectElementWithMatcher:grey_allOf(
                                    grey_accessibilityLabel(kTestLearnMoreLabel),
