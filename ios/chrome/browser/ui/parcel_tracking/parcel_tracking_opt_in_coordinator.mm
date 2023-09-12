55fcce72825f44283892b2b073759a305777dba3
Hira Mahmood <hiramahmood@google.com>
2023-09-12 22:32:10 +0000

    [iOS] Create parcel tracking opt-in prompt
    This CL creates the parcel tracking opt-in prompt that is
    displayed after the first time a tracking number is
    detected on a webpage.
    
    Currently, interaction with the prompt only changes some pref
    values. Actual package tracking and infobar display will
    come in a later CL.
    
    Screen recording of prompt:
    https://drive.google.com/file/d/1DjkgEmLuEmOGleYO3Uc8DNwQnz9A2DxW/view?usp=sharing&resourcekey=0-m3STOlcmFRlXXrlULmiwig
    
    Low-Coverage-Reason: will add eg-test in following CL
    Bug: 1473449
    Change-Id: Icd5947e3b6f546ed88a3620914bd93fde30de68b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4847589
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195671}

--- ios/chrome/browser/ui/parcel_tracking/parcel_tracking_opt_in_coordinator.mm
+++ ios/chrome/browser/ui/parcel_tracking/parcel_tracking_opt_in_coordinator.mm
@@ -5,9 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/parcel_tracking/parcel_tracking_opt_in_coordinator.h"
 
+#import "components/prefs/pref_service.h"
+#import "ios/chrome/browser/parcel_tracking/parcel_tracking_util.h"
+#import "ios/chrome/browser/shared/model/browser/browser.h"
+#import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
+#import "ios/chrome/browser/shared/model/prefs/pref_names.h"
+#import "ios/chrome/browser/ui/parcel_tracking/parcel_tracking_opt_in_mediator.h"
+#import "ios/chrome/browser/ui/parcel_tracking/parcel_tracking_opt_in_view_controller.h"
+
 @implementation ParcelTrackingOptInCoordinator {
   web::WebState* _webState;
   NSArray<CustomTextCheckingResult*>* _parcels;
+  ParcelTrackingOptInMediator* _mediator;
+  ParcelTrackingOptInViewController* _viewController;
 }
 
 - (instancetype)initWithBaseViewController:(UIViewController*)viewController
@@ -23,6 +33,59 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return self;
 }
 
-// TODO(crbug.com/1473449): override start and stop methods.
+- (void)start {
+  [super start];
+  _mediator = [[ParcelTrackingOptInMediator alloc] initWithWebState:_webState];
+  _viewController = [[ParcelTrackingOptInViewController alloc] init];
+  _viewController.actionHandler = self;
+  [self.baseViewController presentViewController:_viewController
+                                        animated:YES
+                                      completion:nil];
+  self.browser->GetBrowserState()->GetPrefs()->SetBoolean(
+      prefs::kIosParcelTrackingOptInPromptDisplayed, true);
+}
+
+- (void)stop {
+  [self dismissPrompt];
+  _mediator = nil;
+  _viewController = nil;
+  [super stop];
+}
+
+#pragma mark - ConfirmationAlertActionHandler
+
+- (void)confirmationAlertPrimaryAction {
+  [self dismissPrompt];
+  self.browser->GetBrowserState()->GetPrefs()->SetInteger(
+      prefs::kIosParcelTrackingOptInStatus,
+      static_cast<int>(IOSParcelTrackingOptInStatus::kAlwaysTrack));
+  [_mediator didTapPrimaryActionButton:_parcels];
+  // TODO(crbug.com/1473449): record metric.
+}
+
+- (void)confirmationAlertSecondaryAction {
+  [self dismissPrompt];
+  self.browser->GetBrowserState()->GetPrefs()->SetInteger(
+      prefs::kIosParcelTrackingOptInStatus,
+      static_cast<int>(IOSParcelTrackingOptInStatus::kNeverTrack));
+  // TODO(crbug.com/1473449): record metric.
+}
+
+- (void)confirmationAlertTertiaryAction {
+  [self dismissPrompt];
+  self.browser->GetBrowserState()->GetPrefs()->SetInteger(
+      prefs::kIosParcelTrackingOptInStatus,
+      static_cast<int>(IOSParcelTrackingOptInStatus::kAskToTrack));
+  [_mediator didTapTertiaryActionButton:_parcels];
+  // TODO(crbug.com/1473449): record metric.
+}
+
+#pragma mark - Private
+
+// Dismisses the view controller.
+- (void)dismissPrompt {
+  [_viewController.presentingViewController dismissViewControllerAnimated:YES
+                                                               completion:nil];
+}
 
 @end
---
+++ ios/chrome/browser/ui/parcel_tracking/parcel_tracking_opt_in_mediator.mm
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/parcel_tracking/parcel_tracking_opt_in_mediator.h"
+
+@implementation ParcelTrackingOptInMediator {
+  web::WebState* _webState;
+}
+
+- (instancetype)initWithWebState:(web::WebState*)webState {
+  self = [super init];
+  if (self) {
+    _webState = webState;
+  }
+  return self;
+}
+
+- (void)didTapPrimaryActionButton:
+    (NSArray<CustomTextCheckingResult*>*)parcelList {
+  // TODO(crbug.com/1473449): Call on AnnotationsTabHelper to track parcels once
+  // Shopping Service API is ready.
+  // TODO(crbug.com/1473449): trigger infobar.
+}
+
+- (void)didTapTertiaryActionButton:
+    (NSArray<CustomTextCheckingResult*>*)parcelList {
+  // TODO(crbug.com/1473449): trigger infobar.
+}
+
+@end
