3778f76b2183ccdde5ab7be28eb4d5546791e08d
Jack Yammine <jyammine@google.com>
2023-09-12 17:03:35 +0000

    [waffle]Handle small screen variants
    Handle the case where the user restores a small size browser.
    We have different variants based on width and height of the dialog.
    
    Fixed: b/298029076
    Change-Id: If3d88b06a165147f295cf50946b4a203e7276fd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4841637
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Jens Mueller <muellerj@google.com>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Cr-Commit-Position: refs/heads/main@{#1195482}

--- chrome/browser/ui/webui/search_engine_choice/search_engine_choice_handler.cc
+++ chrome/browser/ui/webui/search_engine_choice/search_engine_choice_handler.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 SearchEngineChoiceHandler::SearchEngineChoiceHandler(
     mojo::PendingReceiver<search_engine_choice::mojom::PageHandler> receiver,
-    base::OnceCallback<void(int)> display_dialog_callback,
+    base::OnceCallback<void()> display_dialog_callback,
     base::OnceCallback<void(int)> handle_choice_selected_callback)
     : receiver_(this, std::move(receiver)),
       display_dialog_callback_(std::move(display_dialog_callback)),
@@ -21,9 +21,9 @@ SearchEngineChoiceHandler::SearchEngineChoiceHandler(
 
 SearchEngineChoiceHandler::~SearchEngineChoiceHandler() = default;
 
-void SearchEngineChoiceHandler::DisplayDialog(uint32_t content_height) {
+void SearchEngineChoiceHandler::DisplayDialog() {
   if (display_dialog_callback_) {
-    std::move(display_dialog_callback_).Run(content_height);
+    std::move(display_dialog_callback_).Run();
   }
 }
 
--- chrome/browser/ui/webui/search_engine_choice/search_engine_choice_ui.cc
+++ chrome/browser/ui/webui/search_engine_choice/search_engine_choice_ui.cc
@@ -117,7 +117,7 @@ void SearchEngineChoiceUI::BindInterface(
 }
 
 void SearchEngineChoiceUI::Initialize(
-    base::OnceCallback<void(int)> display_dialog_callback) {
+    base::OnceCallback<void()> display_dialog_callback) {
   CHECK(display_dialog_callback);
   display_dialog_callback_ = std::move(display_dialog_callback);
 }
