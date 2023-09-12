79279fec97d661f185f418abe4c752111bdcc015
Jun Ishiguro <junis@google.com>
2023-09-12 01:56:24 +0000

    Lacros: Enable os link click handler for chrome://version only in Ash
    There is a problem in chrome://version page in Lacros where shift-clicks
    on the os://version link open an extra Lacros window along with a new
    window showing the os version.
    
    This CL fixes this issue by disabling the click handler in Lacros and
    using href="os://version" directly.
    
    Bug: 1477039
    Change-Id: Iefc780bdfb6c7c756c86d17700a3198c553b3067
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4852859
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1195163}

--- chrome/browser/ui/webui/version/version_handler_chromeos.cc
+++ chrome/browser/ui/webui/version/version_handler_chromeos.cc
@@ -29,7 +29,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
 const char kCrosUrlVersionRedirect[] = "crosUrlVersionRedirect";
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
 std::string GetOsVersion() {
@@ -79,35 +81,32 @@ void VersionHandlerChromeOS::HandleRequestVersionInfo(
                      weak_factory_.GetWeakPtr()));
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-  const bool showSystemFlagsLink = crosapi::browser_util::IsLacrosEnabled();
-#else
-  const bool showSystemFlagsLink = true;
-#endif
-
-  FireWebUIListener("return-lacros-enabled", base::Value(showSystemFlagsLink));
+  const bool is_lacros_enabled = crosapi::browser_util::IsLacrosEnabled();
+  FireWebUIListener("return-lacros-enabled", base::Value(is_lacros_enabled));
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 }
 
 void VersionHandlerChromeOS::RegisterMessages() {
   VersionHandler::RegisterMessages();
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
   web_ui()->RegisterMessageCallback(
       kCrosUrlVersionRedirect,
       base::BindRepeating(&VersionHandlerChromeOS::HandleCrosUrlVersionRedirect,
                           base::Unretained(this)));
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 }
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
 void VersionHandlerChromeOS::HandleCrosUrlVersionRedirect(
     const base::Value::List& args) {
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-  lacros_url_handling::NavigateInAsh(GURL(chrome::kOsUIVersionURL));
-#else
   // Note: This will only be called by the UI when Lacros is available.
   DCHECK(crosapi::BrowserManager::Get());
   crosapi::BrowserManager::Get()->SwitchToTab(
       GURL(chrome::kChromeUIVersionURL),
       /*path_behavior=*/NavigateParams::RESPECT);
-#endif
 }
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
 void VersionHandlerChromeOS::OnOsVersion(const std::string& version) {
