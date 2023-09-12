be0c813fa5a210eab5a1a816a68345d4fd7d0f65
Garrett Tanzer <gtanzer@chromium.org>
2023-09-12 12:54:21 +0000

    Fenced frames: Add feature flag for M119 additions
    Add kFencedFramesM119Features blink feature flag.
    Gate the new ad size macro format (${AD_WIDTH} and ${AD_HEIGHT}).
    
    Change-Id: I172a5b59575b2fcc3c71952fe152de0446d532ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4847406
    Reviewed-by: Qingxin Wu <qingxinwu@google.com>
    Commit-Queue: Garrett Tanzer <gtanzer@chromium.org>
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195340}

--- content/browser/interest_group/interest_group_browsertest.cc
+++ content/browser/interest_group/interest_group_browsertest.cc
@@ -686,6 +686,7 @@ class InterestGroupBrowserTest : public ContentBrowserTest {
          blink::features::kFledgeNegativeTargeting,
          blink::features::kBiddingAndScoringDebugReportingAPI,
          blink::features::kFledgeDirectFromSellerSignalsHeaderAdSlot,
+         blink::features::kFencedFramesM119Features,
          features::kBackForwardCache},
         /*disabled_features=*/
         {blink::features::kFencedFrames});
