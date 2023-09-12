e5a85df0bee97587fef723c9996570247f464e08
Emily Shack <emshack@chromium.org>
2023-09-12 20:37:34 +0000

    Tab Organization: Add basic TabOrganizationButton
    Adds TabOrganizationButton and TabSearchContainer classes. No custom
    behavior/styling yet, this just sets up the overall class structure.
    
    Bug: 1469126
    Change-Id: I2508e54842515b1e6c65d70f5847b82eded27331
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4836652
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195606}

---
+++ chrome/browser/ui/views/tabs/tab_organization_button.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_TABS_TAB_ORGANIZATION_BUTTON_H_
+#define CHROME_BROWSER_UI_VIEWS_TABS_TAB_ORGANIZATION_BUTTON_H_
+
+#include "chrome/browser/ui/views/tabs/tab_strip_control_button.h"
+#include "ui/base/metadata/metadata_header_macros.h"
+
+class TabStrip;
+
+class TabOrganizationButton : public TabStripControlButton {
+ public:
+  METADATA_HEADER(TabOrganizationButton);
+  explicit TabOrganizationButton(TabStrip* tab_strip);
+  TabOrganizationButton(const TabOrganizationButton&) = delete;
+  TabOrganizationButton& operator=(const TabOrganizationButton&) = delete;
+  ~TabOrganizationButton() override;
+
+ protected:
+  int GetCornerRadius() const override;
+};
+
+#endif  // CHROME_BROWSER_UI_VIEWS_TABS_TAB_ORGANIZATION_BUTTON_H_
