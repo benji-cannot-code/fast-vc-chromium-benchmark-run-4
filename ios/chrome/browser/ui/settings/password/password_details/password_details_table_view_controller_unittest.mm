41605ece40a8a0816887570e3ab72e3e88d4611e
Ernesto Izquierdo Clua <eic@google.com>
2023-09-12 15:11:10 +0000

    [ios] Remove expired histogram from Password Details
    Removed expired histogram and added TODO for replacing deprecated API
    in Password Details.
    
    OBSOLETE_HISTOGRAM[PasswordManager.iOS.PasswordDetails.CopyDetailsFailed]=Monitoring histogram no longer needed.
    
    Bug: 1472794
    Change-Id: I825cc416362219ea3a314fe74339dabd502fbe97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4856460
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1195400}

--- ios/chrome/browser/ui/settings/password/password_details/password_details_table_view_controller_unittest.mm
+++ ios/chrome/browser/ui/settings/password/password_details/password_details_table_view_controller_unittest.mm
@@ -379,9 +379,6 @@ class PasswordDetailsTableViewControllerTest
     UIPasteboard* general_pasteboard = [UIPasteboard generalPasteboard];
     EXPECT_NSEQ(expected_pasteboard, general_pasteboard.string);
     EXPECT_NSEQ(expected_snackbar_message, snack_bar().snackbarMessage);
-    // Verify that the error histogram was emitted to the success bucket.
-    histogram_tester.ExpectUniqueSample(
-        "PasswordManager.iOS.PasswordDetails.CopyDetailsFailed", false, 1);
   }
 
   void SetCredentialType(CredentialType credentialType) {
@@ -891,10 +888,6 @@ TEST_F(PasswordDetailsTableViewControllerTest, CopyUsername) {
       l10n_util::GetNSString(IDS_IOS_SETTINGS_USERNAME_WAS_COPIED_MESSAGE),
       snack_bar().snackbarMessage);
 
-  // Verify that the error histogram was emitted to the success bucket.
-  histogram_tester.ExpectUniqueSample(
-      "PasswordManager.iOS.PasswordDetails.CopyDetailsFailed", false, 1);
-
   EXPECT_FALSE(handler().passwordCopiedByUserCalled);
 }
 
@@ -921,9 +914,6 @@ TEST_F(PasswordDetailsTableViewControllerTest, CopyPasswordSuccess) {
   EXPECT_NSEQ(
       l10n_util::GetNSString(IDS_IOS_SETTINGS_PASSWORD_WAS_COPIED_MESSAGE),
       snack_bar().snackbarMessage);
-  // Verify that the error histogram was emitted to the success bucket.
-  histogram_tester.ExpectUniqueSample(
-      "PasswordManager.iOS.PasswordDetails.CopyDetailsFailed", false, 1);
 }
 
 // Tests error histogram is emitted when we fail copying a field.
@@ -937,10 +927,6 @@ TEST_F(PasswordDetailsTableViewControllerTest, CopyDetailsFailedEmitted) {
   // When no menu controller is passed, there's no way of knowing which field
   // should be copied to the pasteboard and thus copying should fail.
   [password_details copyPasswordDetails:nil];
-
-  // Verify that the error histogram was emitted to the failure bucket.
-  histogram_tester.ExpectUniqueSample(
-      "PasswordManager.iOS.PasswordDetails.CopyDetailsFailed", true, 1);
 
   EXPECT_FALSE(handler().passwordCopiedByUserCalled);
 }
