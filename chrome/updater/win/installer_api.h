4328ecad1c93e9187bdbc0fd2a25b7adbf1ca218
S. Ganesh <ganesh@chromium.org>
2023-09-12 17:50:53 +0000

    updater: Installer results API: Handle the reboot case correctly
    When an installer returns `ERROR_SUCCESS_REBOOT_REQUIRED`, the state of
    the update is now set to a success and the UI gives a notice to the user
    to reboot the system.
    
    Bug: 1481314
    Change-Id: I5d720e7006d66676c31fa445ece3a5883b5c1259
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4857290
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195520}

--- chrome/updater/win/installer_api.h
+++ chrome/updater/win/installer_api.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_UPDATER_WIN_INSTALLER_API_H_
 
 #include <string>
+#include <utility>
 
 #include "base/win/registry.h"
 #include "chrome/updater/enum_traits.h"
@@ -130,9 +131,17 @@ bool SetInstallerOutcomeForTesting(UpdaterScope updater_scope,
                                    const std::string& app_id,
                                    const InstallerOutcome& installer_outcome);
 
-// Translates the Installer API outcome into an `Installer::Result` value.
-// `exit_code` is the exit code of the installer process, which may be used
-// in some cases, depending on the installer outcome.
+// Translates the Installer API outcome into an `Installer::Result` value and
+// the original error code set by the installer if relevant in some cases such
+// as reboot.
+// TODO(crbug.com/1481362): Remove the need for
+// `MakeInstallerResultAndOriginalError` if this can be implemented in
+// `update_client` instead.
+std::pair<Installer::Result, absl::optional<int>>
+MakeInstallerResultAndOriginalError(
+    absl::optional<InstallerOutcome> installer_outcome,
+    int exit_code);
+
 Installer::Result MakeInstallerResult(
     absl::optional<InstallerOutcome> installer_outcome,
     int exit_code);
