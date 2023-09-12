9bef61021d932c7900d2b8ff5d360e6bec088f02
Calder Kitagawa <ckitagawa@chromium.org>
2023-09-12 17:44:56 +0000

    [Tab Group UI] Make TabGridDialogController init lazy
    Restore the lazy nature of the TabGridDialogController initialization.
    This regressed in: crrev.com/1127368
    
    Originally this code used a Supplier with
    1. `get()` overridden to lazily perform one-time init on first call.
    2. `hasValue()` overridden to check if the object initialized in `get()`
       was ready yet.
    
    `hasValue()` was then used to guard `get()` calls where initialization
    of the object via `get()` was not immediately required creating a lazy
    supplier.
    
    The regression was caused by replacing `Supplier` with
    `OneshotSupplierImpl` which was not aware of `hasValue()` being a guard
    for `get()`. As a result, when `onAvailable()` was invoked `get()` was
    called immediately turning the lazy initialization into eager
    initialization.
    
    This CL introduces LazyOneshotSupplier which requires clients to
    override `doSet()`. `doSet()` should call `set(T)` and will be invoked
    in `get()` if `hasValue()` is false. Other changes include:
    * `hasValue()` is a proxy for whether the promise is fulfilled.
    * `onAvailable()` only invokes `get()` if `hasValue()` is true.
    
    Bug: 1480970
    Change-Id: Ia4278393b8500e287125c3adda8c9f393e1a20ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4851426
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195514}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiCoordinator.java
@@ -16,9 +16,9 @@ import androidx.annotation.NonNull;
 import org.chromium.base.Callback;
 import org.chromium.base.TraceEvent;
 import org.chromium.base.metrics.RecordHistogram;
+import org.chromium.base.supplier.LazyOneshotSupplier;
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.supplier.OneshotSupplier;
-import org.chromium.base.supplier.OneshotSupplierImpl;
 import org.chromium.base.supplier.Supplier;
 import org.chromium.chrome.browser.browser_controls.BrowserControlsStateProvider;
 import org.chromium.chrome.browser.compositor.layouts.content.TabContentManager;
@@ -75,7 +75,7 @@ public class TabGroupUiCoordinator implements TabGroupUiMediator.ResetHandler, T
     private final TabContentManager mTabContentManager;
     private PropertyModelChangeProcessor mModelChangeProcessor;
     private TabGridDialogCoordinator mTabGridDialogCoordinator;
-    private OneshotSupplierImpl<TabGridDialogMediator.DialogController>
+    private LazyOneshotSupplier<TabGridDialogMediator.DialogController>
             mTabGridDialogControllerSupplier;
     private TabListCoordinator mTabStripCoordinator;
     private TabGroupUiMediator mMediator;
@@ -156,19 +156,12 @@ public class TabGroupUiCoordinator implements TabGroupUiMediator.ResetHandler, T
             // TODO(crbug.com/972217): find a way to enable interactions between grid tab switcher
             //  and the dialog here.
             if (mScrimCoordinator != null) {
-                mTabGridDialogControllerSupplier =
-                        new OneshotSupplierImpl<>() {
-                            @Override
-                            public TabGridDialogMediator.DialogController get() {
-                                initTabGridDialogCoordinator();
-                                return mTabGridDialogCoordinator.getDialogController();
-                            }
-
-                            @Override
-                            public boolean hasValue() {
-                                return mTabGridDialogCoordinator != null;
-                            }
-                        };
+                mTabGridDialogControllerSupplier = new LazyOneshotSupplier<>() {
+                    @Override
+                    public void doSet() {
+                        initTabGridDialogCoordinator();
+                    }
+                };
             } else {
                 mTabGridDialogControllerSupplier = null;
             }
--- chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiMediatorUnitTest.java
+++ chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiMediatorUnitTest.java
@@ -46,6 +46,7 @@ import org.robolectric.annotation.Config;
 import org.robolectric.annotation.LooperMode;
 
 import org.chromium.base.Callback;
+import org.chromium.base.supplier.LazyOneshotSupplier;
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.supplier.OneshotSupplierImpl;
@@ -164,6 +165,7 @@ public class TabGroupUiMediatorUnitTest {
     private InOrder mVisibilityControllerInOrder;
     private OneshotSupplierImpl<LayoutStateProvider> mLayoutStateProviderSupplier =
             new OneshotSupplierImpl<>();
+    private LazyOneshotSupplier<TabGridDialogMediator.DialogController> mDialogControllerSupplier;
     private final ObservableSupplierImpl<Boolean> mTabGridDialogBackPressSupplier =
             new ObservableSupplierImpl<>();
 
@@ -208,16 +210,19 @@ public class TabGroupUiMediatorUnitTest {
             doReturn(currentTab).when(mTabModelSelector).getCurrentTab();
         }
 
-        TabGridDialogMediator.DialogController controller = mTabGridDialogController;
-        OneshotSupplierImpl<TabGridDialogMediator.DialogController> controllerSupplier =
-                new OneshotSupplierImpl<>();
+        // Fake a similar behavior to the supplier in TabGroupUiCoordinator.
+        mDialogControllerSupplier = new LazyOneshotSupplier<>() {
+            @Override
+            public void doSet() {
+                set(mTabGridDialogController);
+            }
+        };
         doReturn(mTabGridDialogBackPressSupplier)
                 .when(mTabGridDialogController)
                 .getHandleBackPressChangedSupplier();
-        controllerSupplier.set(controller);
         mTabGroupUiMediator = new TabGroupUiMediator(mContext, mVisibilityController, mResetHandler,
                 mModel, mTabModelSelector, mTabCreatorManager, mLayoutStateProviderSupplier,
-                mIncognitoStateProvider, controllerSupplier, mOmniboxFocusStateSupplier);
+                mIncognitoStateProvider, mDialogControllerSupplier, mOmniboxFocusStateSupplier);
 
         if (currentTab == null) {
             verifyNeverReset();
@@ -845,6 +850,7 @@ public class TabGroupUiMediatorUnitTest {
     @Test
     public void backButtonPress_ShouldHandle() {
         initAndAssertProperties(mTab1);
+        mDialogControllerSupplier.get();
         doReturn(true).when(mTabGridDialogController).handleBackPressed();
         mTabGridDialogBackPressSupplier.set(true);
 
@@ -858,6 +864,7 @@ public class TabGroupUiMediatorUnitTest {
     @Test
     public void backButtonPress_ShouldNotHandle() {
         initAndAssertProperties(mTab1);
+        mDialogControllerSupplier.get();
         doReturn(false).when(mTabGridDialogController).handleBackPressed();
         mTabGridDialogBackPressSupplier.set(false);
         var groupUiBackPressSupplier = mTabGroupUiMediator.getHandleBackPressChangedSupplier();
@@ -870,16 +877,6 @@ public class TabGroupUiMediatorUnitTest {
     @Test
     public void backButtonPress_LateInitController() {
         initAndAssertProperties(mTab1);
-        TabGridDialogMediator.DialogController controller = mTabGridDialogController;
-        var backPressSupplier = new ObservableSupplierImpl<Boolean>();
-        doReturn(backPressSupplier)
-                .when(mTabGridDialogController)
-                .getHandleBackPressChangedSupplier();
-        OneshotSupplierImpl<TabGridDialogMediator.DialogController> controllerSupplier =
-                new OneshotSupplierImpl<>();
-        mTabGroupUiMediator = new TabGroupUiMediator(mContext, mVisibilityController, mResetHandler,
-                mModel, mTabModelSelector, mTabCreatorManager, mLayoutStateProviderSupplier,
-                mIncognitoStateProvider, controllerSupplier, mOmniboxFocusStateSupplier);
 
         var groupUiBackPressSupplier = mTabGroupUiMediator.getHandleBackPressChangedSupplier();
 
@@ -887,13 +884,13 @@ public class TabGroupUiMediatorUnitTest {
         Assert.assertNotEquals(Boolean.TRUE, groupUiBackPressSupplier.get());
 
         // Late init.
-        controllerSupplier.set(controller);
+        mDialogControllerSupplier.get();
         doReturn(false).when(mTabGridDialogController).handleBackPressed();
-        backPressSupplier.set(false);
+        mTabGridDialogBackPressSupplier.set(false);
 
         Assert.assertFalse(groupUiBackPressSupplier.get());
 
-        backPressSupplier.set(true);
+        mTabGridDialogBackPressSupplier.set(true);
         doReturn(true).when(mTabGridDialogController).handleBackPressed();
         Assert.assertTrue(groupUiBackPressSupplier.get());
     }
