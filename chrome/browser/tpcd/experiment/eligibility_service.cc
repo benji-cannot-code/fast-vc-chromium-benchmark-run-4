71d11aaf89650973fffc39b3c95baa85bd2aca8d
Patricia Alfonso <trishalfonso@google.com>
2023-09-12 18:58:51 +0000

    [3PCD][Mode B] Provide skeleton for Mode B Experiment Service
    3PCD Mode B Experiment contains requirements that make certain clients
    ineligible for the experiment. Some of these requirements depend on
    profile attributes (like account holder age, enterprise users, etc.) and
    others depend on the Chrome browser session (First Run Experience).
    
    The tpcd::experiment::EligibilityService is a KeyedService (i.e. one per
    profile) that looks at clients enrolled into the experiment through the
    Finch trial configs and filters out the ineligible users based on the
    requirements of the experiment. For each eligible profile, request the
    UX Onboarding service to onboard the client into seeing the new UX. Once
    onboarded, this service tells a client-wide manager service to register
    this client for the synthetic trial, which will be used for experiment
    analysis.
    
    Change-Id: I4f059169757ea9db9679926c908d70215728f9d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4823566
    Reviewed-by: Ryan Tarpine <rtarpine@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Patricia Alfonso <trishalfonso@google.com>
    Cr-Commit-Position: refs/heads/main@{#1195537}

---
+++ chrome/browser/tpcd/experiment/eligibility_service.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/tpcd/experiment/eligibility_service.h"
+
+#include "base/feature_list.h"
+#include "chrome/browser/tpcd/experiment/eligibility_service_factory.h"
+#include "chrome/browser/tpcd/experiment/tpcd_experiment_features.h"
+
+namespace tpcd::experiment {
+
+EligibilityService::EligibilityService(Profile* profile)
+    : profile_(profile), pref_service_(profile->GetPrefs()) {
+  DCHECK(base::FeatureList::IsEnabled(k3PCDModeBExperiment));
+  DCHECK(pref_service_);
+}
+
+EligibilityService::~EligibilityService() = default;
+
+// static
+EligibilityService* EligibilityService::Get(Profile* profile) {
+  return EligibilityServiceFactory::GetForProfile(profile);
+}
+
+}  // namespace tpcd::experiment
