3d6b165e3e19c471e83e5b129e6ac8c1ab4bbd0f
Yulun Wu <yulunwu@chromium.org>
2023-09-12 20:57:05 +0000

    Add alternate styling for iconified text and inline icons.
    The Keyboard Shortcuts app is going through a visual overhaul. We
    should have consistent styling between the shortcut results UI in the
    app and the results in app list search.
    
    See bug for mocks.
    
    Bug: b:299312014
    Change-Id: Ia6f6679986c6300526279a76cb09ef9b73557abe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4857099
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195615}

--- ash/app_list/views/search_result_inline_icon_view.h
+++ ash/app_list/views/search_result_inline_icon_view.h
@@ -23,7 +23,7 @@ namespace ash {
 // Displays a rounded rect bubble containing styled text xor a vector icon.
 class ASH_EXPORT SearchResultInlineIconView : public views::View {
  public:
-  SearchResultInlineIconView();
+  explicit SearchResultInlineIconView(bool use_modified_styling);
   SearchResultInlineIconView(const SearchResultInlineIconView&) = delete;
   SearchResultInlineIconView& operator=(const SearchResultInlineIconView&) =
       delete;
@@ -37,9 +37,11 @@ class ASH_EXPORT SearchResultInlineIconView : public views::View {
  private:
   class SizedLabel;
 
+  // Style iconified text for modifier keys like 'ctrl' and 'alt' differently.
+  const bool use_modified_styling_;
+
   // views::View:
   void OnPaint(gfx::Canvas* canvas) override;
-  void OnThemeChanged() override;
 
   // Cached icon used to recolor icon_image_ when OnThemeChanged() is called.
   raw_ptr<const gfx::VectorIcon, ExperimentalAsh> icon_ = nullptr;
