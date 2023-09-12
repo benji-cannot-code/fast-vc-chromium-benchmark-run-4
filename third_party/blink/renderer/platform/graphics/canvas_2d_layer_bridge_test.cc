d7a236a03213e1468bb3c249525e31737b5d156c
Justin Novosad <junov@chromium.org>
2023-09-12 16:47:52 +0000

    Move class HibernationHandler to a separate file.
    Bug: 1476964
    Change-Id: I72613c113e7d7e0c49a607dce33fc0733089c522
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4835713
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195465}

--- third_party/blink/renderer/platform/graphics/canvas_2d_layer_bridge_test.cc
+++ third_party/blink/renderer/platform/graphics/canvas_2d_layer_bridge_test.cc
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/platform/platform.h"
+#include "third_party/blink/renderer/platform/graphics/canvas_hibernation_handler.h"
 #include "third_party/blink/renderer/platform/graphics/canvas_resource_host.h"
 #include "third_party/blink/renderer/platform/graphics/canvas_resource_provider.h"
 #include "third_party/blink/renderer/platform/graphics/gpu/shared_gpu_context.h"
--- third_party/blink/renderer/platform/scheduler/public/main_thread.h
+++ third_party/blink/renderer/platform/scheduler/public/main_thread.h
@@ -34,7 +34,7 @@ class MainThreadTaskRunnerRestricted {
   friend class WebGLWebCodecsVideoFrame;
   friend class WebRtcVideoFrameAdapter;
   friend class WorkerGlobalScope;
-  friend class HibernationHandler;
+  friend class CanvasHibernationHandler;
   friend class HibernatedCanvasMemoryDumpProvider;
   friend MainThreadTaskRunnerRestricted AccessMainThreadForGpuFactories();
   friend MainThreadTaskRunnerRestricted
