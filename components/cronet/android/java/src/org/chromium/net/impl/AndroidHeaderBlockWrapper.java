6a36cb3131bc6c9392e34f7d8f34f3cb15ae67b8
Stefano Duo <stefanoduo@google.com>
2023-09-13 10:07:51 +0000

    Add baseline implementation of PlatformCronetProvider.
    This implementation closely resembles the one in google3, although some
    minor adjustments were made to it per discussion with stefanoduo@. The
    implementation is a bit rough on the edges at places, and will be
    polished as more tests are enabled.
    
    Bug: 1482137
    Cq-Include-Trybots: luci.chromium.try:android-cronet-arm-dbg,android-cronet-arm64-dbg,android-cronet-arm64-rel,android-cronet-x86-dbg,android-cronet-x86-dbg-10-tests,android-cronet-x86-dbg-11-tests,android-cronet-x86-rel,android-cronet-x86-dbg-pie-tests,android_cronet
    Change-Id: I0e9067d311a694965bbe43e97111973fef859336
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583150
    Reviewed-by: Chidera Olibie <colibie@google.com>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1195907}

---
+++ components/cronet/android/java/src/org/chromium/net/impl/AndroidHeaderBlockWrapper.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.net.impl;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+@RequiresApi(api = 34)
+class AndroidHeaderBlockWrapper extends org.chromium.net.UrlResponseInfo.HeaderBlock {
+    private final android.net.http.HeaderBlock mBackend;
+
+    AndroidHeaderBlockWrapper(android.net.http.HeaderBlock backend) {
+        this.mBackend = backend;
+    }
+
+    @Override
+    public List<Entry<String, String>> getAsList() {
+        return mBackend.getAsList();
+    }
+
+    @Override
+    public Map<String, List<String>> getAsMap() {
+        return mBackend.getAsMap();
+    }
+}
---
+++ components/cronet/android/java/src/org/chromium/net/impl/UrlRequestStatusListenerWrapper.java
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.net.impl;
+
+import androidx.annotation.RequiresApi;
+
+@RequiresApi(api = 34)
+@SuppressWarnings("Override")
+class UrlRequestStatusListenerWrapper implements android.net.http.UrlRequest.StatusListener {
+    private final org.chromium.net.UrlRequest.StatusListener mBackend;
+
+    public UrlRequestStatusListenerWrapper(org.chromium.net.UrlRequest.StatusListener backend) {
+        this.mBackend = backend;
+    }
+
+    @Override
+    public void onStatus(int i) {
+        mBackend.onStatus(i);
+    }
+}
