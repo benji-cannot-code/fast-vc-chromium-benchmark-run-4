0b55441c1293d0a35ba1b231dca80e171da2d327
Justin Novosad <junov@chromium.org>
2023-09-13 20:59:59 +0000

    Move CanvasHibernationHandler tests into dedicated file
    Bug: 1476964
    Change-Id: I212bde11618838b792ed957cd7e94569ab3bf1e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4833446
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196234}

--- third_party/blink/renderer/platform/graphics/test/fake_canvas_resource_host.h
+++ third_party/blink/renderer/platform/graphics/test/fake_canvas_resource_host.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/graphics/canvas_resource_host.h"
 #include "third_party/blink/renderer/platform/graphics/canvas_resource_provider.h"
+#include "third_party/blink/renderer/platform/graphics/gpu/shared_gpu_context.h"
 #include "third_party/blink/renderer/platform/graphics/paint/paint_canvas.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
