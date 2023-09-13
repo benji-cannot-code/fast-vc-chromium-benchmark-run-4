3d6d706e802f678fbee68c166382c98d5aba15c3
Ahmed Fakhry <afakhry@chromium.org>
2023-09-13 17:15:01 +0000

    capture_audio_mixing: (A11y) Announce the capture label contents
    We use the capture label to give users some visual instructions such
    as "Click anywhere to record fullscreen". These were not being
    announced by ChromeVox.
    
    This CL makes sure we always announce whenever there is a change in
    the text of the label.
    
    Fixed: b/299678417
    Test: Manually
    Change-Id: I4a1c38bfc9e5f5446a2a9d16f09fa35dcbfdfaf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4863178
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196085}

--- ash/capture_mode/capture_label_view.cc
+++ ash/capture_mode/capture_label_view.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/i18n/number_formatting.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/task/task_runner.h"
 #include "base/time/time.h"
@@ -266,8 +267,10 @@ void CaptureLabelView::UpdateIconAndText() {
 
   const bool label_visibility = !text.empty();
   label_->SetVisible(label_visibility);
-  if (label_visibility)
+  if (label_visibility && (label_->GetText() != text)) {
     label_->SetText(text);
+    capture_mode_util::TriggerAccessibilityAlertSoon(base::UTF16ToUTF8(text));
+  }
 }
 
 bool CaptureLabelView::ShouldHandleEvent() {
