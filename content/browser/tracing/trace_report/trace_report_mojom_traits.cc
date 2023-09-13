1ef17b3dd59d8867a3e96f9c5f0f24103d27d398
Etienne Pierre-doray <etiennep@chromium.org>
2023-09-13 18:16:45 +0000

    [tracing] Prevent uploads for traces not anonymized.
    This CL marks (with skip_reason) traces without
    |requires_anonymized_data_| as not anonymized, which prevents upload.
    This is a no-op because traces without |requires_anonymized_data_|
    are never saved in the database.
    This will change in a follow-up
    https://chromium-review.googlesource.com/c/chromium/src/+/4850964
    where it becomes relevant to check |requires_anonymized_data_|.
    
    Bug: 1418116
    Change-Id: I2f52e7b3b9846cf85af7fe57471e1945ba849459
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4850696
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196138}

--- content/browser/tracing/trace_report/trace_report_mojom_traits.cc
+++ content/browser/tracing/trace_report/trace_report_mojom_traits.cc
@@ -49,6 +49,8 @@ EnumTraits<SkipUploadReason, content::SkipUploadReason>::ToMojom(
       return SkipUploadReason::kNoSkip;
     case content::SkipUploadReason::kSizeLimitExceeded:
       return SkipUploadReason::kSizeLimitExceeded;
+    case content::SkipUploadReason::kNotAnonymized:
+      return SkipUploadReason::kNotAnonymized;
   }
 }
 
@@ -61,6 +63,9 @@ bool EnumTraits<SkipUploadReason, content::SkipUploadReason>::FromMojom(
       return true;
     case SkipUploadReason::kSizeLimitExceeded:
       *output = content::SkipUploadReason::kSizeLimitExceeded;
+      return true;
+    case SkipUploadReason::kNotAnonymized:
+      *output = content::SkipUploadReason::kNotAnonymized;
       return true;
   }
 }
