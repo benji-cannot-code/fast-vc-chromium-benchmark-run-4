701d9685a9f3add0fd66c1ef1422a156c3136c83
rudranshd <rudranshd@google.com>
2023-09-13 21:01:11 +0000

    [Instant Hotspot] Create Header for Your Devices section
    Create files and class for listing 'Your Devices' in quick settings
    menu.
    
    View controller changes and unit-test to be added in following CL.
    
    Tested: deployed to DUT with connected Pixel 7 phone.
    
    Screenshot: https://screenshot.googleplex.com/4vZhHxmiFMyjSsh
    
    Bug: b/290078054, b/290078735
    Change-Id: I7cddb78a98a5315d1e2f2808b7bedb74c09df8cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4734073
    Commit-Queue: Rudransh Dikshit <rudranshd@google.com>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196236}

--- ash/system/network/network_detailed_network_view_impl.cc
+++ ash/system/network/network_detailed_network_view_impl.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/network/network_detailed_view.h"
 #include "ash/system/network/network_list_mobile_header_view_impl.h"
 #include "ash/system/network/network_list_network_item_view.h"
+#include "ash/system/network/network_list_tether_hosts_header_view.h"
 #include "ash/system/network/network_list_wifi_header_view_impl.h"
 #include "ash/system/network/network_utils.h"
 #include "ash/system/network/tray_network_state_model.h"
@@ -247,6 +248,21 @@ void NetworkDetailedNetworkViewImpl::ReorderMobileListView(size_t index) {
   }
 }
 
+void NetworkDetailedNetworkViewImpl::ReorderTetherHostsTopContainer(
+    size_t index) {
+  DCHECK(base::FeatureList::IsEnabled(features::kInstantHotspotRebrand));
+  if (tether_hosts_top_container_) {
+    scroll_content()->ReorderChildView(tether_hosts_top_container_, index);
+  }
+}
+
+void NetworkDetailedNetworkViewImpl::ReorderTetherHostsListView(size_t index) {
+  DCHECK(base::FeatureList::IsEnabled(features::kInstantHotspotRebrand));
+  if (tether_hosts_network_list_view_) {
+    scroll_content()->ReorderChildView(tether_hosts_network_list_view_, index);
+  }
+}
+
 void NetworkDetailedNetworkViewImpl::MaybeRemoveFirstListView() {
   if (first_list_view_ && first_list_view_->children().empty()) {
     scroll_content()->RemoveChildViewT(first_list_view_.get());
@@ -281,6 +297,21 @@ void NetworkDetailedNetworkViewImpl::UpdateMobileStatus(bool enabled) {
   }
   if (mobile_network_list_view_) {
     mobile_network_list_view_->SetVisible(enabled);
+  }
+}
+
+void NetworkDetailedNetworkViewImpl::UpdateTetherHostsStatus(bool enabled) {
+  if (!features::IsQsRevampEnabled()) {
+    return;
+  }
+
+  if (tether_hosts_top_container_) {
+    tether_hosts_top_container_->SetBehavior(
+        enabled ? RoundedContainer::Behavior::kTopRounded
+                : RoundedContainer::Behavior::kAllRounded);
+  }
+  if (tether_hosts_network_list_view_) {
+    tether_hosts_network_list_view_->SetVisible(enabled);
   }
 }
 
--- ash/system/network/network_detailed_network_view_impl.h
+++ ash/system/network/network_detailed_network_view_impl.h
@@ -7,13 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_SYSTEM_NETWORK_NETWORK_DETAILED_NETWORK_VIEW_IMPL_H_
 
 #include "ash/ash_export.h"
-#include "base/memory/raw_ptr.h"
-
 #include "ash/style/rounded_container.h"
 #include "ash/system/network/network_detailed_network_view.h"
 #include "ash/system/network/network_list_mobile_header_view_impl.h"
 #include "ash/system/network/network_list_network_item_view.h"
 #include "ash/system/network/network_list_wifi_header_view_impl.h"
+#include "base/memory/raw_ptr.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/views/view.h"
 
@@ -55,9 +54,12 @@ class ASH_EXPORT NetworkDetailedNetworkViewImpl
   void ReorderNetworkListView(size_t index) override;
   void ReorderMobileTopContainer(size_t index) override;
   void ReorderMobileListView(size_t index) override;
+  void ReorderTetherHostsTopContainer(size_t index) override;
+  void ReorderTetherHostsListView(size_t index) override;
   void MaybeRemoveFirstListView() override;
   void UpdateWifiStatus(bool enabled) override;
   void UpdateMobileStatus(bool enabled) override;
+  void UpdateTetherHostsStatus(bool enabled) override;
   void ScrollToPosition(int position) override;
   int GetScrollPosition() override;
 
@@ -72,11 +74,16 @@ class ASH_EXPORT NetworkDetailedNetworkViewImpl
   // true.
   raw_ptr<RoundedContainer, DanglingUntriaged | ExperimentalAsh>
       first_list_view_ = nullptr;
+
   raw_ptr<RoundedContainer, ExperimentalAsh> mobile_top_container_ = nullptr;
   raw_ptr<RoundedContainer, ExperimentalAsh> mobile_network_list_view_ =
       nullptr;
   raw_ptr<RoundedContainer, ExperimentalAsh> wifi_top_container_ = nullptr;
   raw_ptr<RoundedContainer, ExperimentalAsh> wifi_network_list_view_ = nullptr;
+  raw_ptr<RoundedContainer, ExperimentalAsh> tether_hosts_top_container_ =
+      nullptr;
+  raw_ptr<RoundedContainer, ExperimentalAsh> tether_hosts_network_list_view_ =
+      nullptr;
 };
 
 }  // namespace ash
