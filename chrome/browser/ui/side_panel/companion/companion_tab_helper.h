ada53498afd7956c36a2c452ce7724ba8d59ec5d
Duncan Mercer <mercerd@google.com>
2023-09-13 17:34:22 +0000

    [CSC] Open CSC in new tabs spawned from tab with CSC opened
    Bug: b:286416404
    Change-Id: I90da605f16c168148cf9e140549fce32b9a608f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4856431
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1196101}

--- chrome/browser/ui/side_panel/companion/companion_tab_helper.h
+++ chrome/browser/ui/side_panel/companion/companion_tab_helper.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/side_panel/side_panel_enums.h"
 #include "components/lens/buildflags.h"
 #include "components/lens/lens_metrics.h"
+#include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 
 #if BUILDFLAG(ENABLE_LENS_DESKTOP_GOOGLE_BRANDED_FEATURES)
@@ -28,7 +29,8 @@ class CompanionPageHandler;
 // values such as a text query. This class also owns the
 // CompanionSidePanelController.
 class CompanionTabHelper
-    : public content::WebContentsUserData<CompanionTabHelper> {
+    : public content::WebContentsUserData<CompanionTabHelper>,
+      public content::WebContentsObserver {
  public:
   class Delegate {
    public:
@@ -48,6 +50,10 @@ class CompanionTabHelper
     virtual void OnCompanionSidePanelClosed() = 0;
     // Retrieves the web contents for testing purposes.
     virtual content::WebContents* GetCompanionWebContentsForTesting() = 0;
+    // Returns true if Companion is currently open and showing to the user.
+    virtual bool IsCompanionShowing() = 0;
+    // Set Companion as the active side panel entry in the given web contents.
+    virtual void SetCompanionAsActiveEntry(content::WebContents* contents) = 0;
     // Add a callback to be called when Companion is fully loaded in the side
     // panel, i.e. the spinner of the tab would stop spinning, Javascript is
     // loaded and the onload event was dispatched.
@@ -60,6 +66,16 @@ class CompanionTabHelper
   CompanionTabHelper(const CompanionTabHelper&) = delete;
   CompanionTabHelper& operator=(const CompanionTabHelper&) = delete;
   ~CompanionTabHelper() override;
+
+  // content::WebContentsObserver overrides.
+  void DidOpenRequestedURL(content::WebContents* new_contents,
+                           content::RenderFrameHost* source_render_frame_host,
+                           const GURL& url,
+                           const content::Referrer& referrer,
+                           WindowOpenDisposition disposition,
+                           ui::PageTransition transition,
+                           bool started_from_context_menu,
+                           bool renderer_initiated) override;
 
   // Add a callback to be called when Companion is fully loaded in the side
   // panel, i.e. the spinner of the tab would stop spinning, Javascript is
