3e53751a7bc990d73cd205df4d2646fc81080ac8
Chung-Sheng Wu <chungsheng@google.com>
2023-09-13 11:22:53 +0000

    help_app: Disable for Shimless RMA App profile
    Bug: b:289480939
    Change-Id: I69cc9988905335ff17b5c20667bdecd0e68f3f08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4861703
    Reviewed-by: Callistus Tan <callistus@google.com>
    Commit-Queue: Chung-sheng Wu <chungsheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1195925}

--- chrome/browser/ash/system_web_apps/apps/help_app/help_app_untrusted_ui_config.cc
+++ chrome/browser/ash/system_web_apps/apps/help_app/help_app_untrusted_ui_config.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/webui_url_constants.h"
+#include "chromeos/ash/components/browser_context_helper/browser_context_types.h"
 #include "chromeos/ash/components/system/statistics_provider.h"
 #include "chromeos/ash/services/multidevice_setup/public/cpp/prefs.h"
 #include "chromeos/constants/chromeos_features.h"
@@ -180,6 +181,13 @@ HelpAppUntrustedUIConfig::HelpAppUntrustedUIConfig()
     : WebUIConfig(content::kChromeUIUntrustedScheme, kChromeUIHelpAppHost) {}
 
 HelpAppUntrustedUIConfig::~HelpAppUntrustedUIConfig() = default;
+
+bool HelpAppUntrustedUIConfig::IsWebUIEnabled(
+    content::BrowserContext* browser_context) {
+  // TODO(b/300226633): Maybe use `IsUserBrowserContext` to filter all ash
+  // profiles.
+  return !IsShimlessRmaAppBrowserContext(browser_context);
+}
 
 std::unique_ptr<content::WebUIController>
 HelpAppUntrustedUIConfig::CreateWebUIController(content::WebUI* web_ui,
