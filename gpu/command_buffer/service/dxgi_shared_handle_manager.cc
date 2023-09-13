f80095d37f17aa06dc6290b08e91bcebc2fda2ed
Sunny Sachanandani <sunnyps@chromium.org>
2023-09-13 19:32:28 +0000

    gpu: Use AddRefWithCheck for DXGISharedHandleState
    Since DXGISharedHandleState uses start-from-one ref-count (adopt ref)
    semantics, it can use AddRefWithCheck instead of just AddRef for safety.
    This should always have been the case, but I noticed it wasn't when
    working on adding custom ref-counting to ContextProviderCommandBuffer in
    https://crrev.com/c/4860505.
    
    Bug: 1481757
    Change-Id: I2a0dbe7d8ca4d63ad21fa0b932625d7199f5ad3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4864039
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196189}

--- gpu/command_buffer/service/dxgi_shared_handle_manager.cc
+++ gpu/command_buffer/service/dxgi_shared_handle_manager.cc
@@ -91,7 +91,7 @@ DXGISharedHandleState::~DXGISharedHandleState() {
 }
 
 void DXGISharedHandleState::AddRef() const {
-  base::subtle::RefCountedThreadSafeBase::AddRef();
+  base::subtle::RefCountedThreadSafeBase::AddRefWithCheck();
 }
 
 void DXGISharedHandleState::Release() const {
