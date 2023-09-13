34974469956cb0e670210f6eff815899fb800b09
Antonio Gomes <tonikitoo@igalia.com>
2023-09-13 22:22:17 +0000

    [lacros] Include <string_view> from net/cert/pki/general_names.h
    ... otherwise lacros amd64 builds might fail with:
    
      In file included from ../../net/cert/pki/parse_certificate.cc:5:
      In file included from ../../net/cert/pki/parse_certificate.h:15:
      ../../net/cert/pki/general_names.h:78:20:
        error: no member named 'string_view' in namespace 'std'
          std::vector<std::string_view> rfc822_names;
                      ~~~~~^
      ../../net/cert/pki/general_names.h:81:20:
        error: no member named 'string_view' in namespace 'std'
          std::vector<std::string_view> dns_names;
                      ~~~~~^
      (...)
      3 errors generated.
    
    BUG=NONE
    R=mmenke@chromium.org
    
    Change-Id: I072f845075a0338a544b355d1fad705b41c62060
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4862165
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1196287}

--- net/cert/pki/general_names.h
+++ net/cert/pki/general_names.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define NET_CERT_PKI_GENERAL_NAMES_H_
 
 #include <memory>
+#include <string_view>
 #include <vector>
 
 #include "net/base/net_export.h"
