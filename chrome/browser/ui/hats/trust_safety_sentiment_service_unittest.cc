4d7817b3b1f8ea69cd66cda69cf9782de2d38f07
Sarah Criel <skrakowi@chromium.org>
2023-09-13 15:44:57 +0000

    Add histogram that checks infrastructure implementation
    In this CL, we add a histogram that verifies whether TriggerOccurred
    is being called when it should be. This is to support the
    implementation of newly added infrastructure between Chrome counter
    abuse features and the Trust and Safety sentiment service. This CL
    also includes minor code quality improvements with the safe browsing
    blocking page integration. We included these changes in this CL because
    they are minor and we want to cherry pick these into M118 so we have the
    verification of the implementation for this milestone.
    
    Bug: 1475956
    Change-Id: I875e0c91a40dfc7568b88673a6a75d9c20b2090b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4859261
    Reviewed-by: Ehimare Okoyomon <eokoyomon@chromium.org>
    Commit-Queue: Sarah Krakowiak <skrakowi@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196026}

--- chrome/browser/ui/hats/trust_safety_sentiment_service_unittest.cc
+++ chrome/browser/ui/hats/trust_safety_sentiment_service_unittest.cc
@@ -176,7 +176,7 @@ class TrustSafetySentimentServiceTest : public testing::Test {
     std::string privacy_sandbox_4_consent_decline_probability = "0.1";
     std::string privacy_sandbox_4_notice_ok_probability = "0.1";
     std::string privacy_sandbox_4_notice_settings_probability = "0.1";
-    std::string safe_browsing_interstitial_probability = "0.0";
+    std::string safe_browsing_interstitial_probability = "0.4";
     std::string browsing_data_trigger_id = "browsing-data-test";
     std::string control_group_trigger_id = "control-group-test";
     std::string password_check_trigger_id = "password-check-test";
@@ -191,6 +191,8 @@ class TrustSafetySentimentServiceTest : public testing::Test {
         "privacy-sandbox-4-notice-ok";
     std::string privacy_sandbox_4_notice_settings_trigger_id =
         "privacy-sandbox-4-notice-settings";
+    std::string safe_browsing_interstitial_trigger_id =
+        "safe-browsing-interstitial";
   };
 
   void SetupFeatureParametersV2(FeatureParamsV2 params) {
@@ -233,6 +235,8 @@ class TrustSafetySentimentServiceTest : public testing::Test {
              params.privacy_sandbox_4_notice_ok_trigger_id},
             {"privacy-sandbox-4-notice-settings-trigger-id",
              params.privacy_sandbox_4_notice_settings_trigger_id},
+            {"safe-browsing-interstitial-trigger-id",
+             params.safe_browsing_interstitial_trigger_id},
         });
   }
 
@@ -253,6 +257,22 @@ class TrustSafetySentimentServiceTest : public testing::Test {
         histogram_tester()->ExpectBucketCount(histogram_name, area, 1);
       }
     }
+  }
+
+  void CheckCallTriggerOccurredHistogram(
+      const std::map<TrustSafetySentimentService::FeatureArea, int>&
+          triggered_area_counts) {
+    int total_count = 0;
+    const std::string histogram_name =
+        "Feedback.TrustSafetySentiment.CallTriggerOccurred";
+    for (const auto& histogram_expected : triggered_area_counts) {
+      const auto& feature_area = histogram_expected.first;
+      int expected_count = histogram_expected.second;
+      histogram_tester()->ExpectBucketCount(histogram_name, feature_area,
+                                            expected_count);
+      total_count += expected_count;
+    }
+    histogram_tester()->ExpectTotalCount(histogram_name, total_count);
   }
 
   TrustSafetySentimentService* service() {
@@ -303,6 +323,9 @@ TEST_F(TrustSafetySentimentServiceTest, Eligibility_NtpOpens) {
   CheckHistograms({TrustSafetySentimentService::FeatureArea::kPrivacySettings,
                    TrustSafetySentimentService::FeatureArea::kTrustedSurface},
                   {});
+  CheckCallTriggerOccurredHistogram(
+      {{TrustSafetySentimentService::FeatureArea::kPrivacySettings, 1},
+       {TrustSafetySentimentService::FeatureArea::kTrustedSurface, 1}});
 
   // The next NTP should be eligible for a survey.
   EXPECT_CALL(*mock_hats_service(), LaunchSurvey(_, _, _, _, _));
@@ -343,6 +366,9 @@ TEST_F(TrustSafetySentimentServiceTest, Eligibility_Time) {
   CheckHistograms({TrustSafetySentimentService::FeatureArea::kPrivacySettings,
                    TrustSafetySentimentService::FeatureArea::kTrustedSurface},
                   {TrustSafetySentimentService::FeatureArea::kTrustedSurface});
+  CheckCallTriggerOccurredHistogram(
+      {{TrustSafetySentimentService::FeatureArea::kPrivacySettings, 1},
+       {TrustSafetySentimentService::FeatureArea::kTrustedSurface, 1}});
 }
 
 TEST_F(TrustSafetySentimentServiceTest, TriggerProbability) {
@@ -358,6 +384,8 @@ TEST_F(TrustSafetySentimentServiceTest, TriggerProbability) {
       TrustSafetySentimentService::FeatureArea::kTrustedSurface, {});
   service()->OpenedNewTabPage();
   CheckHistograms({}, {});
+  CheckCallTriggerOccurredHistogram(
+      {{TrustSafetySentimentService::FeatureArea::kTrustedSurface, 1}});
 }
 
 TEST_F(TrustSafetySentimentServiceTest, TriggersClearOnLaunch) {
@@ -410,6 +438,10 @@ TEST_F(TrustSafetySentimentServiceTest, TriggersClearOnLaunch) {
                    TrustSafetySentimentService::FeatureArea::kTransactions},
                   {surveyed_feature_area,
                    TrustSafetySentimentService::FeatureArea::kTransactions});
+  CheckCallTriggerOccurredHistogram(
+      {{TrustSafetySentimentService::FeatureArea::kPrivacySettings, 1},
+       {TrustSafetySentimentService::FeatureArea::kTrustedSurface, 1},
+       {TrustSafetySentimentService::FeatureArea::kTransactions, 1}});
 }
 
 TEST_F(TrustSafetySentimentServiceTest, SettingsWatcher_PrivacySettings) {
@@ -908,6 +940,9 @@ TEST_F(TrustSafetySentimentServiceTest, ActiveIncognitoPreventsSurvey) {
   CheckHistograms({TrustSafetySentimentService::FeatureArea::kPrivacySettings,
                    TrustSafetySentimentService::FeatureArea::kIneligible},
                   {TrustSafetySentimentService::FeatureArea::kPrivacySettings});
+  CheckCallTriggerOccurredHistogram(
+      {{TrustSafetySentimentService::FeatureArea::kSafetyCheck, 1},
+       {TrustSafetySentimentService::FeatureArea::kPrivacySettings, 1}});
 }
 
 TEST_F(TrustSafetySentimentServiceTest, ClosingIncognitoDelaysSurvey) {
@@ -961,6 +996,9 @@ TEST_F(TrustSafetySentimentServiceTest, ClosingIncognitoDelaysSurvey) {
   CheckHistograms({TrustSafetySentimentService::FeatureArea::kPrivacySettings,
                    TrustSafetySentimentService::FeatureArea::kIneligible},
                   {TrustSafetySentimentService::FeatureArea::kPrivacySettings});
+  CheckCallTriggerOccurredHistogram(
+      {{TrustSafetySentimentService::FeatureArea::kSafetyCheck, 1},
+       {TrustSafetySentimentService::FeatureArea::kPrivacySettings, 1}});
 }
 
 TEST_F(TrustSafetySentimentServiceTest, AllFeatureAreasHaveTriggers) {
@@ -1103,6 +1141,8 @@ TEST_F(TrustSafetySentimentServiceTest, Eligibility_V1FeatureWhileV2Enabled) {
   service()->OpenedNewTabPage();
   CheckHistograms({TrustSafetySentimentService::FeatureArea::kPrivacySettings},
                   {TrustSafetySentimentService::FeatureArea::kPrivacySettings});
+  CheckCallTriggerOccurredHistogram(
+      {{TrustSafetySentimentService::FeatureArea::kPrivacySettings, 2}});
 }
 
 TEST_F(TrustSafetySentimentServiceTest, V2_TrustedSurface) {
@@ -1142,6 +1182,8 @@ TEST_F(TrustSafetySentimentServiceTest, V2_TrustedSurface) {
   service()->OpenedNewTabPage();
   CheckHistograms({TrustSafetySentimentService::FeatureArea::kTrustedSurface},
                   {TrustSafetySentimentService::FeatureArea::kTrustedSurface});
+  CheckCallTriggerOccurredHistogram(
+      {{TrustSafetySentimentService::FeatureArea::kTrustedSurface, 1}});
 }
 
 TEST_F(TrustSafetySentimentServiceTest, V2_SafetyCheck) {
@@ -1167,6 +1209,9 @@ TEST_F(TrustSafetySentimentServiceTest, V2_SafetyCheck) {
   service()->OpenedNewTabPage();
   CheckHistograms({TrustSafetySentimentService::FeatureArea::kSafetyCheck},
                   {TrustSafetySentimentService::FeatureArea::kSafetyCheck});
+  CheckCallTriggerOccurredHistogram(
+      {{TrustSafetySentimentService::FeatureArea::kSafetyCheck, 1},
+       {TrustSafetySentimentService::FeatureArea::kPrivacySettings, 1}});
 }
 
 TEST_F(TrustSafetySentimentServiceTest, V2_PasswordCheck) {
@@ -1186,6 +1231,8 @@ TEST_F(TrustSafetySentimentServiceTest, V2_PasswordCheck) {
   service()->OpenedNewTabPage();
   CheckHistograms({TrustSafetySentimentService::FeatureArea::kPasswordCheck},
                   {TrustSafetySentimentService::FeatureArea::kPasswordCheck});
+  CheckCallTriggerOccurredHistogram(
+      {{TrustSafetySentimentService::FeatureArea::kPasswordCheck, 1}});
 }
 
 TEST_F(TrustSafetySentimentServiceTest, V2_BrowsingData) {
@@ -1240,6 +1287,7 @@ TEST_F(TrustSafetySentimentServiceTest, V2_BrowsingData_NotInterested) {
   service()->ClearedBrowsingData(browsing_data::BrowsingDataType::COOKIES);
   service()->OpenedNewTabPage();
   CheckHistograms({}, {});
+  CheckCallTriggerOccurredHistogram({});
 }
 
 TEST_F(TrustSafetySentimentServiceTest, V2_PrivacyGuide) {
@@ -1260,6 +1308,8 @@ TEST_F(TrustSafetySentimentServiceTest, V2_PrivacyGuide) {
   service()->OpenedNewTabPage();
   CheckHistograms({TrustSafetySentimentService::FeatureArea::kPrivacyGuide},
                   {TrustSafetySentimentService::FeatureArea::kPrivacyGuide});
+  CheckCallTriggerOccurredHistogram(
+      {{TrustSafetySentimentService::FeatureArea::kPrivacyGuide, 1}});
 }
 
 TEST_F(TrustSafetySentimentServiceTest, V2_ControlGroup) {
@@ -1297,6 +1347,8 @@ TEST_F(TrustSafetySentimentServiceTest, V2_ControlGroup) {
   service()->OpenedNewTabPage();
   CheckHistograms({TrustSafetySentimentService::FeatureArea::kControlGroup},
                   {TrustSafetySentimentService::FeatureArea::kControlGroup});
+  CheckCallTriggerOccurredHistogram(
+      {{TrustSafetySentimentService::FeatureArea::kControlGroup, 1}});
   testing::Mock::VerifyAndClearExpectations(mock_hats_service());
 
   // A second valid trigger should not launch a survey because we have already
@@ -1407,4 +1459,7 @@ TEST_F(TrustSafetySentimentServiceTest, V2_SafeBrowsingInterstitial) {
   CheckHistograms(
       {TrustSafetySentimentService::FeatureArea::kSafeBrowsingInterstitial},
       {TrustSafetySentimentService::FeatureArea::kSafeBrowsingInterstitial});
+  CheckCallTriggerOccurredHistogram(
+      {{TrustSafetySentimentService::FeatureArea::kSafeBrowsingInterstitial,
+        1}});
 }
