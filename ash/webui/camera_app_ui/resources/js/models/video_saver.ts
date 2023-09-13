89f8c110b1b10da036d88aab6b6d1f3a68ffdfbd
Pi-Hsun Shih <pihsun@chromium.org>
2023-09-13 07:16:53 +0000

    CCA: ESLint: Fix floating promise warning in video_saver.ts
    `cca.py lint --eslintrc .eslintrc_floating_promise.js` error count goes
    down from 82 to 81.
    
    Bug: b:172336355
    Test: cca.py lint --eslintrc .eslintrc_floating_promise.js
    Change-Id: Iccc64eec66d60459a3e4307d9e849c7be9335ce4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4858199
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195841}

--- ash/webui/camera_app_ui/resources/js/models/video_saver.ts
+++ ash/webui/camera_app_ui/resources/js/models/video_saver.ts
@@ -193,7 +193,8 @@ class TimeLapseFixedSpeedSaver {
       private readonly processor: Comlink.Remote<VideoProcessor>) {}
 
   write(blob: Blob, frameNo: number): void {
-    this.processor.write(blob);
+    // processor.write does queuing internally.
+    void this.processor.write(blob);
     this.maxWrittenFrame = frameNo;
   }
 
