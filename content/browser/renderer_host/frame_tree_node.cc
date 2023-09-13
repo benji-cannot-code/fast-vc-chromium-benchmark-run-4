75d57c2b825c21cd66229d85c93deb226d32ca42
Mingyu Lei <leimy@chromium.org>
2023-09-13 09:44:10 +0000

    Disable DumpWithoutCrashing from TakeNavigationRequest
    Bug: 1478592
    Change-Id: Id919bd34dda0d8bba737001ea9bcde599f56edc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4861045
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195901}

--- content/browser/renderer_host/frame_tree_node.cc
+++ content/browser/renderer_host/frame_tree_node.cc
@@ -47,7 +47,7 @@ namespace features {
 
 BASE_FEATURE(kDumpWhenFrameTreeNodeTakesNavigationRequestWithEvictedBFCacheRFH,
              "DumpWhenFrameTreeNodeTakesNavigationRequestWithEvictedBFCacheRFH",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 }  // namespace features
 
