a57dbd99906b13eda2a78d36ea91a59e70bd98a9
Taiyo Mizuhashi <taiyo@chromium.org>
2023-09-13 07:38:40 +0000

    Move kPrerender2BypassMemoryLimitCheck to prerender/prerender_features
    Along with introducing prerender/prerender_features, this CL moves
    kPrerender2NewLimitAndScheduler to there.
    
    Bug: n/a
    Change-Id: I838f45c4edeebd9abaed5c7677dc54608eb0eb0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4853163
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195849}

--- content/browser/preloading/prerender/prerender_features.h
+++ content/browser/preloading/prerender/prerender_features.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content::features {
 
+CONTENT_EXPORT BASE_DECLARE_FEATURE(kPrerender2BypassMemoryLimitCheck);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kPrerender2NewLimitAndScheduler);
 
 }  // namespace content::features
