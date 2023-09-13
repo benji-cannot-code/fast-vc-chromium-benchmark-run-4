7d29d0c3ac95881adcfde36f736d48d22e0083d3
Jonathan Lee <jonathanjlee@google.com>
2023-09-13 18:44:25 +0000

    [wpt-metadata] Allow specifying a custom `--min-samples`
    `update-metadata` has a global minimum sample requirement for updates to
    keep flakiness from destabilizing expectations. Make this threshold
    configurable so that `run_wpt_tests.py --reset-results` can specify a
    lower one.
    
    Bug: 1480061
    Change-Id: I0031a16b1299e759480c914d9bb1dc77ac6a01e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4860007
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1196158}

--- third_party/blink/tools/blinkpy/tool/commands/update_metadata_unittest.py
+++ third_party/blink/tools/blinkpy/tool/commands/update_metadata_unittest.py
@@ -37,9 +37,6 @@ class BaseUpdateMetadataTest(LoggingTestCase):
     def setUp(self):
         super().setUp()
         self.maxDiff = None
-        # Lower this threshold so that test results do not need to be repeated.
-        MetadataUpdater.min_results_for_update = 1
-
         self.tool = MockBlinkTool()
         self.finder = path_finder.PathFinder(self.tool.filesystem)
         self.tool.filesystem.write_text_file(
@@ -218,7 +215,7 @@ class UpdateMetadataExecuteTest(BaseUpdateMetadataTest):
                 'OK',
                 'status':
                 'CRASH',
-            }],
+            }] * 4,
         }).encode()
 
     def _unstaged_changes(self):
@@ -290,7 +287,7 @@ class UpdateMetadataExecuteTest(BaseUpdateMetadataTest):
                 'subtests': [],
                 'expected': 'PASS',
                 'status': 'FAIL',
-            }],
+            }] * 4,
         }).encode()
         with self._patch_builtins() as stack:
             stack.enter_context(self._unstaged_changes())
@@ -491,7 +488,7 @@ class UpdateMetadataExecuteTest(BaseUpdateMetadataTest):
                 'test': '/new-test-on-tot.html',
                 'subtests': [],
                 'status': 'PASS',
-            }],
+            }] * 4,
         }).encode()
         with self._patch_builtins():
             exit_code = self.command.main(['fail.html'])
@@ -694,6 +691,9 @@ class UpdateMetadataASTSerializationTest(BaseUpdateMetadataTest):
                     configs[run_info] = test_port
 
             configs = TestConfigurations(self.tool.filesystem, configs)
+            # Lower this threshold so that test results do not need to be
+            # repeated.
+            options.setdefault('min_samples', 1)
             updater = MetadataUpdater.from_manifests(
                 manifests, configs, self.tool.port_factory.get(), **options)
             updater.collect_results(
@@ -1181,19 +1181,20 @@ class UpdateMetadataASTSerializationTest(BaseUpdateMetadataTest):
         unexpectedly passes. Unexpected passes should be removed separately
         using long-term test history.
         """
-        MetadataUpdater.min_results_for_update = 2
         self.write_contents(
             'external/wpt/fail.html.ini', """\
             [fail.html]
               expected: FAIL
             """)
-        self.update({
-            'results': [{
-                'test': '/fail.html',
-                'status': 'PASS',
-                'expected': 'FAIL',
-            }],
-        })
+        self.update(
+            {
+                'results': [{
+                    'test': '/fail.html',
+                    'status': 'PASS',
+                    'expected': 'FAIL',
+                }],
+            },
+            min_samples=2)
         self.assert_contents(
             'external/wpt/fail.html.ini', """\
             [fail.html]
