b05e2436a0243139964b43a339aaa638a6874eec
Saifuddin Hitawala <hitawala@chromium.org>
2023-09-13 17:52:52 +0000

    Graphite: Rebaseline cc/viz pixeltest images with dawn-swiftshader
    Rebaseline cc/viz pixel tests images using dawn-swiftshader backend
    instead of metal backend which was being used by bots earlier.
    
    This should allow us to enabled cc/viz pixel tests on arm macs (at
    least).
    
    Bug: 1442381
    Change-Id: I4e02d23ad999d02482f8c6da65c79f0f7a45c882
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4859822
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Michael Ludwig <michaelludwig@google.com>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196122}

--- cc/trees/layer_tree_host_pixeltest_filters.cc
+++ cc/trees/layer_tree_host_pixeltest_filters.cc
@@ -1081,9 +1081,12 @@ TEST_P(LayerTreeHostFiltersPixelTest, EnlargedTextureWithAlphaThresholdFilter) {
   // Force the allocation a larger textures.
   set_enlarge_texture_amount(gfx::Size(50, 50));
 
-  RunPixelTest(
-      background,
-      base::FilePath(FILE_PATH_LITERAL("enlarged_texture_on_threshold.png")));
+  base::FilePath expected_result =
+      base::FilePath(FILE_PATH_LITERAL("enlarged_texture_on_threshold.png"));
+  if (use_skia_graphite()) {
+    expected_result = expected_result.InsertBeforeExtensionASCII("_graphite");
+  }
+  RunPixelTest(background, expected_result);
 }
 
 TEST_P(LayerTreeHostFiltersPixelTest, EnlargedTextureWithCropOffsetFilter) {
@@ -1119,9 +1122,12 @@ TEST_P(LayerTreeHostFiltersPixelTest, EnlargedTextureWithCropOffsetFilter) {
   // Force the allocation a larger textures.
   set_enlarge_texture_amount(gfx::Size(50, 50));
 
-  RunPixelTest(
-      background,
-      base::FilePath(FILE_PATH_LITERAL("enlarged_texture_on_crop_offset.png")));
+  base::FilePath expected_result =
+      base::FilePath(FILE_PATH_LITERAL("enlarged_texture_on_crop_offset.png"));
+  if (use_skia_graphite()) {
+    expected_result = expected_result.InsertBeforeExtensionASCII("_graphite");
+  }
+  RunPixelTest(background, expected_result);
 }
 
 TEST_P(LayerTreeHostFiltersPixelTest, BlurFilterWithClip) {
