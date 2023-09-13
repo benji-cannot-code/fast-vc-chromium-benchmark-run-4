06fc4e145d11876cf436acd16361ec810de5e501
Michelle <michellegc@google.com>
2023-09-13 04:12:53 +0000

    Remove unneeded editor panel types.
    ash::input_method::EditorPanelContext and related types were added as
    placeholders to experiment with a skeleton EditorPanelManager interface
    before adding a crosapi. Now that the editor panel crosapi has been
    added (CL:4835565), we will probably just use the mojo generated types.
    Remove the placeholders since they don't seem to be needed anymore.
    
    Bug: b:295059934
    Change-Id: Id895b06938ef795b43a96ec64dc2f4399412bc8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4861819
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195796}

--- chrome/browser/ash/input_method/editor_panel_manager.cc
+++ chrome/browser/ash/input_method/editor_panel_manager.cc
@@ -14,14 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash::input_method {
 
-EditorPanelPresetTextQuery::EditorPanelPresetTextQuery() = default;
-
-EditorPanelPresetTextQuery::~EditorPanelPresetTextQuery() = default;
-
-EditorPanelContext::EditorPanelContext() = default;
-
-EditorPanelContext::~EditorPanelContext() = default;
-
 EditorPanelManager::EditorPanelManager(Delegate* delegate)
     : delegate_(delegate) {}
 
