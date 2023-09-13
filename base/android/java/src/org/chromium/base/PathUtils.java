6cbf56e65e850a88b98ecb0189dff97e0b3a263f
Andrew Grieve <agrieve@chromium.org>
2023-09-13 19:47:34 +0000

    Android: Mark kitkat as unsupported in our build system
    Cronet no longer requires kitkat support, so we should be able to remove
    kitkat-specific things (e.g. legacy multidex).
    
    Bug: 1471145
    Change-Id: Ib1caf8a22870db4dd775999fa9bee38f3d9a7da9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4822585
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Peter Pakkenberg <pbirk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196200}

--- base/android/java/src/org/chromium/base/PathUtils.java
+++ base/android/java/src/org/chromium/base/PathUtils.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.base;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.os.Build;
@@ -89,11 +88,7 @@ public abstract class PathUtils {
         }
     }
 
-    @SuppressLint("NewApi")
     private static void chmod(String path, int mode) {
-        // Both Os.chmod and ErrnoException require SDK >= 21. But while Dalvik on < 21 tolerates
-        // Os.chmod, it throws VerifyError for ErrnoException, so catch Exception instead.
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) return;
         try {
             Os.chmod(path, mode);
         } catch (Exception e) {
