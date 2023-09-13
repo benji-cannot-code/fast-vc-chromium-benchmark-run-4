d21be593d1ce18f657f59f8251f680ccfab46219
Aman Verma <amanvr@google.com>
2023-09-13 14:57:59 +0000

    [iOS] Move ios/c/b/browser_state_metrics to model subfolder
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/<foo>/ will be moved to
    ios/c/b/<foo>/model/ to showcase that they are model files.
    
    Bug: 1319852, 1480282
    Change-Id: Id3b8aaaba755b634f2c484fc44e17aae3eb5296e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4859724
    Commit-Queue: Aman Verma <amanvr@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195999}

--- ios/chrome/browser/browser_state_metrics/model/browser_state_metrics.cc
+++ ios/chrome/browser/browser_state_metrics/model/browser_state_metrics.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ios/chrome/browser/browser_state_metrics/browser_state_metrics.h"
+#include "ios/chrome/browser/browser_state_metrics/model/browser_state_metrics.h"
 
 #include <stddef.h>
 
@@ -20,12 +20,14 @@ bool CountBrowserStateInformation(ios::ChromeBrowserStateManager* manager,
   counts->total = number_of_browser_states;
 
   // Ignore other metrics if we have no browser states.
-  if (!number_of_browser_states)
+  if (!number_of_browser_states) {
     return false;
+  }
 
   for (size_t i = 0; i < number_of_browser_states; ++i) {
-    if (info_cache->BrowserStateIsAuthenticatedAtIndex(i))
+    if (info_cache->BrowserStateIsAuthenticatedAtIndex(i)) {
       counts->signedin++;
+    }
   }
   return true;
 }
