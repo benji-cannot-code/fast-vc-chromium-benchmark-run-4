e1c8378dc20a1a2dcc32049eae796fea22c85da8
Sky Malice <skym@chromium.org>
2023-09-13 17:45:31 +0000

    Cache bookmark ids in BookmarkBridge.java
    Bug: 1449020
    Change-Id: I3503328f2e97aac0a7bbf749e23a15f721c8a4bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4857327
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196111}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkBridge.java
@@ -30,7 +30,6 @@ import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.components.bookmarks.BookmarkId;
 import org.chromium.components.bookmarks.BookmarkItem;
 import org.chromium.components.bookmarks.BookmarkType;
-import org.chromium.components.commerce.core.ShoppingService;
 import org.chromium.components.power_bookmarks.PowerBookmarkMeta;
 import org.chromium.components.power_bookmarks.PowerBookmarkType;
 import org.chromium.content_public.browser.WebContents;
@@ -44,12 +43,20 @@ import java.util.List;
  * bookmark model stored in native.
  */
 class BookmarkBridge {
+    private final ObserverList<BookmarkModelObserver> mObservers = new ObserverList<>();
+
     private long mNativeBookmarkBridge;
     private boolean mIsDestroyed;
     private boolean mIsDoingExtensiveChanges;
     private boolean mIsNativeBookmarkModelLoaded;
-    private final ObserverList<BookmarkModelObserver> mObservers = new ObserverList<>();
-    private ShoppingService mShoppingService;
+
+    // Lazily set pseudo-constants. These should never change at runtime. Used to avoid crossing
+    // JNI to fetch information.
+    private @Nullable BookmarkId mRootFolderId;
+    private @Nullable BookmarkId mMobileFolderId;
+    private @Nullable BookmarkId mOtherFolderId;
+    private @Nullable BookmarkId mDesktopFolderId;
+    private @Nullable BookmarkId mReadingListFolderId;
 
     /**
      * Handler to fetch the bookmarks, titles, urls and folder hierarchy.
@@ -249,7 +256,11 @@ class BookmarkBridge {
         ThreadUtils.assertOnUiThread();
         if (mNativeBookmarkBridge == 0) return null;
         assert mIsNativeBookmarkModelLoaded;
-        return BookmarkBridgeJni.get().getReadingListFolder(mNativeBookmarkBridge);
+        if (mReadingListFolderId == null) {
+            mReadingListFolderId =
+                    BookmarkBridgeJni.get().getReadingListFolder(mNativeBookmarkBridge);
+        }
+        return mReadingListFolderId;
     }
 
     /**
@@ -322,7 +333,10 @@ class BookmarkBridge {
         ThreadUtils.assertOnUiThread();
         if (mNativeBookmarkBridge == 0) return null;
         assert mIsNativeBookmarkModelLoaded;
-        return BookmarkBridgeJni.get().getRootFolderId(mNativeBookmarkBridge);
+        if (mRootFolderId == null) {
+            mRootFolderId = BookmarkBridgeJni.get().getRootFolderId(mNativeBookmarkBridge);
+        }
+        return mRootFolderId;
     }
 
     /**
@@ -332,7 +346,10 @@ class BookmarkBridge {
         ThreadUtils.assertOnUiThread();
         if (mNativeBookmarkBridge == 0) return null;
         assert mIsNativeBookmarkModelLoaded;
-        return BookmarkBridgeJni.get().getMobileFolderId(mNativeBookmarkBridge);
+        if (mMobileFolderId == null) {
+            mMobileFolderId = BookmarkBridgeJni.get().getMobileFolderId(mNativeBookmarkBridge);
+        }
+        return mMobileFolderId;
     }
 
     /**
@@ -342,7 +359,10 @@ class BookmarkBridge {
         ThreadUtils.assertOnUiThread();
         if (mNativeBookmarkBridge == 0) return null;
         assert mIsNativeBookmarkModelLoaded;
-        return BookmarkBridgeJni.get().getOtherFolderId(mNativeBookmarkBridge);
+        if (mOtherFolderId == null) {
+            mOtherFolderId = BookmarkBridgeJni.get().getOtherFolderId(mNativeBookmarkBridge);
+        }
+        return mOtherFolderId;
     }
 
     /**
@@ -352,7 +372,10 @@ class BookmarkBridge {
         ThreadUtils.assertOnUiThread();
         if (mNativeBookmarkBridge == 0) return null;
         assert mIsNativeBookmarkModelLoaded;
-        return BookmarkBridgeJni.get().getDesktopFolderId(mNativeBookmarkBridge);
+        if (mDesktopFolderId == null) {
+            mDesktopFolderId = BookmarkBridgeJni.get().getDesktopFolderId(mNativeBookmarkBridge);
+        }
+        return mDesktopFolderId;
     }
 
     /**
--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkUtils.java
@@ -612,7 +612,6 @@ public class BookmarkUtils {
      * @return The list of top level bookmark folder ids.
      */
     public static List<BookmarkId> populateTopLevelFolders(BookmarkModel bookmarkModel) {
-        // TODO(crbug.com/1449020): Refactor this to not go through JNI so much.
         List<BookmarkId> topLevelFolders = new ArrayList<>();
         BookmarkId desktopNodeId = bookmarkModel.getDesktopFolderId();
         BookmarkId mobileNodeId = bookmarkModel.getMobileFolderId();
@@ -821,7 +820,6 @@ public class BookmarkUtils {
     /** Returns whether the given folder should display images. */
     public static boolean shouldShowImagesForFolder(
             BookmarkModel bookmarkModel, BookmarkId folder) {
-        // TODO(crbug.com/1449020): Refactor this to not go through JNI so much.
         BookmarkId rootNodeId = bookmarkModel.getRootFolderId();
         BookmarkId desktopNodeId = bookmarkModel.getDesktopFolderId();
         BookmarkId mobileNodeId = bookmarkModel.getMobileFolderId();
