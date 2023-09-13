042b45e07b3e7870a8a1f9333be225a472434802
Nasser Al-shawwa <alshawwa@chromium.org>
2023-09-13 22:19:38 +0000

    Cleanup CrowdstrikeSignalReporting feature flag
    Bug: b/280612757
    Change-Id: I899045e13d3e2e84732db07650ef0e5815e90772
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4860766
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196285}

--- components/policy/core/common/features.cc
+++ components/policy/core/common/features.cc
@@ -11,10 +11,6 @@ namespace policy {
 
 namespace features {
 
-BASE_FEATURE(kCrowdstrikeSignalReporting,
-             "CrowdstrikeSignalReporting",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 BASE_FEATURE(kEnablePolicyTestPage,
              "EnablePolicyTestPage",
              base::FEATURE_DISABLED_BY_DEFAULT);
--- components/policy/core/common/features.h
+++ components/policy/core/common/features.h
@@ -15,10 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace policy {
 namespace features {
 
-// Enable detection/upload Crowdstrike Agent signals with security
-// events.
-POLICY_EXPORT BASE_DECLARE_FEATURE(kCrowdstrikeSignalReporting);
-
 // Enable the policy test page at chrome://policy/test.
 POLICY_EXPORT BASE_DECLARE_FEATURE(kEnablePolicyTestPage);
 
