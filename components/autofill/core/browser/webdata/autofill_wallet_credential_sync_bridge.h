954fb037f35d8ef9eaacde15f1893dd5433b9e32
Vipul Vinod Koul <koulvipul@google.com>
2023-09-13 17:38:55 +0000

    [CVV Storage] Implement reconciliation of the server cvc with card data
    If a credit card was removed from pay.google.com, the card will be
    synced with the REMOVE tag. If there is a cvc associated with this card,
    the same needs to be deleted from the local storage and the Chrome sync
    server.
    
    Design doc- go/pay-autofill-cvv-storage-chrome
    
    Bug: 1458044
    Change-Id: If9107c57ba42937111cea3df49dd97f0ff420009
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4855024
    Reviewed-by: Siyu An <siyua@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Alexander Tekle <alexandertekle@google.com>
    Commit-Queue: Vipul Koul <koulvipul@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196105}

--- components/autofill/core/browser/webdata/autofill_wallet_credential_sync_bridge.h
+++ components/autofill/core/browser/webdata/autofill_wallet_credential_sync_bridge.h
@@ -75,6 +75,7 @@ class AutofillWalletCredentialSyncBridge
   bool IsEntityDataValid(const syncer::EntityData& entity_data) const override;
 
   // AutofillWebDataServiceObserverOnDBSequence.
+  void CreditCardChanged(const CreditCardChange& change) override;
   void ServerCvcChanged(const ServerCvcChange& change) override;
 
  private:
