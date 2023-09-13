4b569fc3ac3c334bed9f8d457738d0765016b549
Alex Turner <alexmt@chromium.org>
2023-09-13 19:28:42 +0000

    Add a use counter for privateAggregation.enableDebugMode()
    We intend to eventually deprecate this call once it is a no-op for all
    users (see also crrev.com/c/4822930). This use counter will help with
    that future deprecation while allowing for better understanding of usage
    today.
    
    Bug: 1476980
    Change-Id: I2821c8df8a9305b03268dc44bdbabd62d2ca76bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4843372
    Reviewed-by: Qingxin Wu <qingxinwu@google.com>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196185}

--- third_party/blink/renderer/modules/shared_storage/private_aggregation.cc
+++ third_party/blink/renderer/modules/shared_storage/private_aggregation.cc
@@ -71,7 +71,7 @@ void PrivateAggregation::contributeToHistogram(
   ExecutionContext* execution_context = ExecutionContext::From(script_state);
   CHECK(execution_context->IsSharedStorageWorkletGlobalScope());
 
-  EnsureUseCountersAreRecorded();
+  EnsureGeneralUseCountersAreRecorded();
 
   if (!global_scope_->private_aggregation_permissions_policy_allowed()) {
     exception_state.ThrowDOMException(DOMExceptionCode::kInvalidAccessError,
@@ -124,7 +124,8 @@ void PrivateAggregation::enableDebugMode(
   ExecutionContext* execution_context = ExecutionContext::From(script_state);
   CHECK(execution_context->IsSharedStorageWorkletGlobalScope());
 
-  EnsureUseCountersAreRecorded();
+  EnsureGeneralUseCountersAreRecorded();
+  EnsureEnableDebugModeUseCounterIsRecorded();
 
   if (!global_scope_->private_aggregation_permissions_policy_allowed()) {
     exception_state.ThrowDOMException(DOMExceptionCode::kInvalidAccessError,
@@ -199,12 +200,20 @@ void PrivateAggregation::OnWorkletDestroyed() {
   CHECK(operation_states_.empty());
 }
 
-void PrivateAggregation::EnsureUseCountersAreRecorded() {
-  if (!has_recorded_use_counters_) {
-    has_recorded_use_counters_ = true;
+void PrivateAggregation::EnsureGeneralUseCountersAreRecorded() {
+  if (!has_recorded_general_use_counters_) {
+    has_recorded_general_use_counters_ = true;
     global_scope_->GetSharedStorageWorkletServiceClient()->RecordUseCounters(
         {mojom::blink::WebFeature::kPrivateAggregationApiAll,
          mojom::blink::WebFeature::kPrivateAggregationApiSharedStorage});
+  }
+}
+
+void PrivateAggregation::EnsureEnableDebugModeUseCounterIsRecorded() {
+  if (!has_recorded_enable_debug_mode_use_counter_) {
+    has_recorded_enable_debug_mode_use_counter_ = true;
+    global_scope_->GetSharedStorageWorkletServiceClient()->RecordUseCounters(
+        {mojom::blink::WebFeature::kPrivateAggregationApiEnableDebugMode});
   }
 }
 
