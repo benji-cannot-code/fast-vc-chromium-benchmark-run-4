0173582bd65211c3cd619d383a58b34d2ab6e636
Vikram Pasupathy <vpasupathy@chromium.org>
2023-09-13 23:54:38 +0000

    media: Use recommended CreateUniqueTempDirectoryScopedToTest()
    The function above is recommended, as it handles and generates a temp
    dir unique to the test.
    
    Bug: <none>
    Change-Id: I7ba1e9c647b0a44a749afc6d67325cc3d5056e89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4851016
    Commit-Queue: Vikram Pasupathy <vpasupathy@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196321}

--- media/filters/android/video_frame_extractor_unittest.cc
+++ media/filters/android/video_frame_extractor_unittest.cc
@@ -8,10 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/files/file_util.h"
-#include "base/files/scoped_temp_dir.h"
 #include "base/functional/bind.h"
 #include "base/run_loop.h"
 #include "base/test/task_environment.h"
+#include "base/test/test_file_util.h"
 #include "base/test/test_mock_time_task_runner.h"
 #include "media/base/test_data_util.h"
 #include "media/filters/file_data_source.h"
@@ -49,7 +49,6 @@ class VideoFrameExtractorTest : public testing::Test {
   ~VideoFrameExtractorTest() override {}
 
  protected:
-  void SetUp() override { ASSERT_TRUE(temp_dir_.CreateUniqueTempDir()); }
 
   ExtractVideoFrameResult ExtractFrame(const base::FilePath& file_path) {
     base::File file(
@@ -67,11 +66,8 @@ class VideoFrameExtractorTest : public testing::Test {
     return result;
   }
 
-  const base::FilePath& temp_dir() const { return temp_dir_.GetPath(); }
-
  private:
   base::test::TaskEnvironment task_environment_;
-  base::ScopedTempDir temp_dir_;
   std::unique_ptr<FileDataSource> data_source_;
   std::unique_ptr<VideoFrameExtractor> extractor_;
 };
@@ -88,7 +84,8 @@ TEST_F(VideoFrameExtractorTest, ExtractVideoFrame) {
 // file.
 TEST_F(VideoFrameExtractorTest, ExtractInvalidVideoFile) {
   // Creates a dummy video file, frame extraction should fail.
-  base::FilePath file = temp_dir().AppendASCII("test.txt");
+  base::FilePath file =
+      base::CreateUniqueTempDirectoryScopedToTest().AppendASCII("test.txt");
   EXPECT_TRUE(base::WriteFile(file, "123"));
 
   auto result = ExtractFrame(file);
