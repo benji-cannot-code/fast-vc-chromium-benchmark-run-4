c5342d6e87114ff266d816280910bf52d17f554f
Ergün Erdoğmuş <ergunsh@chromium.org>
2023-09-13 11:54:02 +0000

    Add initial JS error reporting for DevTools
    Change-Id: Ia2d596db038117e5574f5f06abdf1ba48fcb62f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4706120
    Reviewed-by: Ian Barkley-Yeung <iby@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Auto-Submit: Ergün Erdoğmuş <ergunsh@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195936}

--- content/browser/devtools/devtools_frontend_host_impl.h
+++ content/browser/devtools/devtools_frontend_host_impl.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_DEVTOOLS_DEVTOOLS_FRONTEND_HOST_IMPL_H_
 
 #include "content/public/browser/devtools_frontend_host.h"
+#include "content/public/browser/web_contents_observer.h"
 #include "mojo/public/cpp/bindings/associated_receiver.h"
 #include "third_party/blink/public/mojom/devtools/devtools_frontend.mojom.h"
 
@@ -15,7 +16,8 @@ namespace content {
 class WebContents;
 
 class DevToolsFrontendHostImpl : public DevToolsFrontendHost,
-                                 public blink::mojom::DevToolsFrontendHost {
+                                 public blink::mojom::DevToolsFrontendHost,
+                                 public WebContentsObserver {
  public:
   DevToolsFrontendHostImpl(
       RenderFrameHost* frame_host,
@@ -26,7 +28,21 @@ class DevToolsFrontendHostImpl : public DevToolsFrontendHost,
 
   ~DevToolsFrontendHostImpl() override;
 
+  static CONTENT_EXPORT std::unique_ptr<DevToolsFrontendHostImpl>
+  CreateForTesting(RenderFrameHost* frame_host,
+                   const HandleMessageCallback& handle_message_callback);
+
   void BadMessageReceived() override;
+
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+  void OnDidAddMessageToConsole(
+      RenderFrameHost* source_frame,
+      blink::mojom::ConsoleMessageLevel log_level,
+      const std::u16string& message,
+      int32_t line_no,
+      const std::u16string& source_id,
+      const absl::optional<std::u16string>& untrusted_stack_trace) override;
+#endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 
  private:
   // blink::mojom::DevToolsFrontendHost implementation.
