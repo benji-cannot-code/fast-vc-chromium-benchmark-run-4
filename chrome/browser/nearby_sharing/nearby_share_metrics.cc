db69b59ab0674806bd29bb46f86bd7e9cacfb7b0
Brando <brandosocarras@google.com>
2023-09-13 00:11:45 +0000

    [Nearby][Self Share] Implement Self Share Success Metrics
    Create Nearby.Share.IsSelfShare boolean histogram.
    
    Add 'SelfShare' as a variant to the ContactStatus token in
    Nearby.Share.Transfer.Success.{Direction}.{ShareTargetType}.{ContactStatus}
    and similarly '.SelfShare' as a variant to the Variation token in
    Nearby.Share.Transfer.FinalStatus{Variation}.
    
    These metrics will allow for calculation of Self Share success rate,
    Self Share attempts / Nearby Share attempts, and total Nearby Share
    attempts.
    
    Tested: manually on DUT with Self Share enabled/disabled.
    Fixed: b/279502099
    Change-Id: I05448f70848b7a60da5eb883e1eeb0b998d44f84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4852776
    Reviewed-by: Crisrael Lucero <crisrael@google.com>
    Reviewed-by: Daniel Classon <dclasson@google.com>
    Commit-Queue: Brando Socarras <brandosocarras@google.com>
    Cr-Commit-Position: refs/heads/main@{#1195720}

--- chrome/browser/nearby_sharing/nearby_share_metrics.cc
+++ chrome/browser/nearby_sharing/nearby_share_metrics.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/safe_conversions.h"
 #include "base/strings/string_util.h"
 #include "chrome/browser/ash/file_manager/path_util.h"
+#include "chrome/browser/nearby_sharing/common/nearby_share_features.h"
 #include "chrome/browser/nearby_sharing/nearby_share_feature_status.h"
 #include "chromeos/ash/services/nearby/public/mojom/nearby_connections_types.mojom.h"
 #include "chromeos/ash/services/nearby/public/mojom/nearby_decoder_types.mojom.h"
@@ -391,6 +392,14 @@ UpgradedMedium GetUpgradedMediumForMetrics(
   }
 }
 
+std::string GetContactStatus(bool is_contact, bool for_self_share) {
+  if (for_self_share) {
+    return ".SelfShare";
+  }
+
+  return is_contact ? ".Contact" : ".NonContact";
+}
+
 void RecordNearbySharePayloadAttachmentTypeMetric(
     AttachmentType type,
     bool is_incoming,
@@ -643,7 +652,8 @@ void RecordNearbyShareTransferFinalStatusMetric(
     bool is_incoming,
     nearby_share::mojom::ShareTargetType type,
     TransferMetadata::Status status,
-    bool is_known) {
+    bool is_known,
+    bool for_self_share) {
   DCHECK(TransferMetadata::IsFinalStatus(status));
 
   // Emit success/failure to Standard Feature Usage Logging if there was a
@@ -661,9 +671,13 @@ void RecordNearbyShareTransferFinalStatusMetric(
 
   base::UmaHistogramBoolean("Nearby.Share.IsKnownContact", is_known);
 
+  // Log whether the transfer was a Self Share if Self Share is enabled.
+  if (features::IsSelfShareEnabled()) {
+    base::UmaHistogramBoolean("Nearby.Share.IsSelfShare", for_self_share);
+  }
+
   std::string send_or_receive = GetDirectionSubcategoryName(is_incoming);
   std::string share_target_type = GetShareTargetTypeSubcategoryName(type);
-  std::string contact_or_not = GetIsKnownSubcategoryName(is_known);
 
   base::UmaHistogramEnumeration("Nearby.Share.DeviceType", type);
   base::UmaHistogramEnumeration("Nearby.Share.DeviceType" + send_or_receive,
@@ -677,7 +691,8 @@ void RecordNearbyShareTransferFinalStatusMetric(
     base::UmaHistogramEnumeration(prefix, final_status);
     base::UmaHistogramEnumeration(prefix + send_or_receive, final_status);
     base::UmaHistogramEnumeration(prefix + share_target_type, final_status);
-    base::UmaHistogramEnumeration(prefix + contact_or_not, final_status);
+    base::UmaHistogramEnumeration(
+        prefix + GetContactStatus(is_known, for_self_share), final_status);
   }
 
   // Log the transfer success/failure for high-level success and Critical User
@@ -697,9 +712,11 @@ void RecordNearbyShareTransferFinalStatusMetric(
     }
     if (success.has_value()) {
       const std::string prefix = "Nearby.Share.Transfer.Success";
+      const std::string contact_status =
+          GetContactStatus(is_known, for_self_share);
       base::UmaHistogramBoolean(prefix, *success);
       base::UmaHistogramBoolean(
-          prefix + send_or_receive + share_target_type + contact_or_not,
+          prefix + send_or_receive + share_target_type + contact_status,
           *success);
     }
   }
