0a7286a547560369241f6dd7141ea8bea0092f4c
Andrew Rayskiy <greengrape@google.com>
2023-09-13 12:27:55 +0000

    [Sheriff] Fix broken HeapMojoReceiverSet...ClearWithReason test
    Introduced in crrev.com/c/4861463.
    
    disconnected_reason_code() is not initialized by the time of the call
    which makes Linux MSan unhappy.
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Tests/43004/overview
    
    Bug: 1465781
    Change-Id: I6b9607ffd397d57d064e42c2c6d4bc4baccbedaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4859468
    Reviewed-by: Sonja Laurila <laurila@google.com>
    Owners-Override: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1195946}

--- third_party/blink/renderer/platform/mojo/heap_mojo_receiver_set_test.cc
+++ third_party/blink/renderer/platform/mojo/heap_mojo_receiver_set_test.cc
@@ -121,8 +121,12 @@ template <HeapMojoWrapperMode Mode, typename ContextType>
 class HeapMojoReceiverSetDisconnectWithReasonHandlerBaseTest
     : public HeapMojoReceiverSetDisconnectHandlerBaseTest<Mode, ContextType> {
  public:
-  uint32_t& disconnected_reason_code() { return disconnected_reason_code_; }
-  std::string& disconnected_description() { return disconnected_description_; }
+  absl::optional<uint32_t>& disconnected_reason_code() {
+    return disconnected_reason_code_;
+  }
+  absl::optional<std::string>& disconnected_description() {
+    return disconnected_description_;
+  }
 
  protected:
   void SetUp() override {
@@ -145,8 +149,8 @@ class HeapMojoReceiverSetDisconnectWithReasonHandlerBaseTest
         WTF::Unretained(this)));
   }
 
-  uint32_t disconnected_reason_code_;
-  std::string disconnected_description_;
+  absl::optional<uint32_t> disconnected_reason_code_;
+  absl::optional<std::string> disconnected_description_;
 };
 
 }  // namespace
@@ -335,8 +339,9 @@ TEST_F(HeapMojoReceiverSetDisconnectWithReasonHandlerWithoutContextObserverTest,
        ClearWithReason) {
   const std::string message = "test message";
   const uint32_t reason = 15;
-  ASSERT_NE(disconnected_reason_code(), reason);
-  ASSERT_TRUE(disconnected_description().empty());
+
+  ASSERT_FALSE(disconnected_reason_code().has_value());
+  ASSERT_FALSE(disconnected_description().has_value());
 
   owner()->receiver_set().ClearWithReason(reason, message);
   run_loop().Run();
