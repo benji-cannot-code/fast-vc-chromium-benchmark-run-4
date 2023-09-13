1b9e547255585ea4b089f982e096092013eea02e
Sammie Quon <sammiequon@chromium.org>
2023-09-13 19:12:38 +0000

    overview: Use transform instead of rect for continuous animation
    Computes and stores the target transform instead of the target bounds.
    This allows us to not use `OverviewItem::SetBounds`, which will be
    a bit more performant. Also, that function hides the header, which is
    why we see the "hitch" mentioned in the bug. That function also updates
    shadows and rounded corners; as we don't call it each tick anymore, we
    have to manually update those.
    
    Test: manual
    Change-Id: I12ab81ab25d4b01e4f58ee34dff3fb4cb4b1f7d0
    Fixed: b/295063288
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4854610
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196173}

--- ash/wm/overview/overview_group_item.cc
+++ ash/wm/overview/overview_group_item.cc
@@ -113,6 +113,11 @@ void OverviewGroupItem::SetBounds(const gfx::RectF& target_bounds,
   overview_items_[1]->SetBounds(sub_bounds2, animation_type);
 }
 
+gfx::Transform OverviewGroupItem::ComputeTargetTransform(
+    const gfx::RectF& target_bounds) {
+  return gfx::Transform();
+}
+
 gfx::RectF OverviewGroupItem::GetTargetBoundsInScreen() const {
   gfx::RectF target_bounds;
   for (auto& item : overview_items_) {
@@ -201,8 +206,7 @@ void OverviewGroupItem::OnOverviewItemDragStarted(OverviewItemBase* item) {}
 void OverviewGroupItem::OnOverviewItemDragEnded(bool snap) {}
 
 void OverviewGroupItem::OnOverviewItemContinuousScroll(
-    const gfx::RectF& target_bouns,
-    bool first_scroll,
+    const gfx::Transform& target_transform,
     float scroll_ratio) {}
 
 void OverviewGroupItem::SetVisibleDuringItemDragging(bool visible,
