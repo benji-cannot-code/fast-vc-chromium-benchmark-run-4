7b8c307dff60c6ab06636acf8098b01fb25b3b18
Jens Mueller <muellerj@google.com>
2023-09-13 18:00:04 +0000

    Add remaining search engine choice screen strings.
    Bug: b/298029684
    Change-Id: I394fd92cb35533f9ed77156dbaf38c8e47c557e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4859188
    Commit-Queue: Jens Mueller <muellerj@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Jack Yammine <jyammine@google.com>
    Cr-Commit-Position: refs/heads/main@{#1196126}

--- chrome/browser/ui/webui/search_engine_choice/search_engine_choice_ui.cc
+++ chrome/browser/ui/webui/search_engine_choice/search_engine_choice_ui.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/webui_util.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/chrome_unscaled_resources.h"
-#include "chrome/grit/generated_resources.h"
 #include "chrome/grit/search_engine_choice_resources.h"
 #include "chrome/grit/search_engine_choice_resources_map.h"
 #include "chrome/grit/signin_resources.h"
@@ -59,7 +58,12 @@ SearchEngineChoiceUI::SearchEngineChoiceUI(content::WebUI* web_ui)
       web_ui->GetWebContents()->GetBrowserContext(),
       chrome::kChromeUISearchEngineChoiceHost);
 
-  source->AddLocalizedString("title", IDS_SEARCH_ENGINE_CHOICE_PAGE_TITLE);
+  // TODO(b/280753567): Differentiate new from existing users. For new users use
+  // IDS_SEARCH_ENGINE_CHOICE_PAGE_TITLE if
+  // FirstRunServiceFactory::GetForBrowserContext(profile_.get()) is present
+  // indicating it's part of FRE.
+  source->AddLocalizedString("title",
+                             IDS_SEARCH_ENGINE_CHOICE_PAGE_EXISTING_USER_TITLE);
   source->AddLocalizedString("subtitle",
                              IDS_SEARCH_ENGINE_CHOICE_PAGE_SUBTITLE);
   source->AddLocalizedString("subtitleInfoLink",
@@ -77,8 +81,7 @@ SearchEngineChoiceUI::SearchEngineChoiceUI(content::WebUI* web_ui)
   source->AddLocalizedString(
       "infoDialogThirdParagraph",
       IDS_SEARCH_ENGINE_CHOICE_INFO_DIALOG_BODY_THIRD_PARAGRAPH);
-  source->AddLocalizedString("infoDialogButtonText",
-                             IDS_SEARCH_ENGINE_CHOICE_INFO_DIALOG_BUTTON_TITLE);
+  source->AddLocalizedString("infoDialogButtonText", IDS_CLOSE);
   source->AddLocalizedString("productLogoAltText",
                              IDS_SHORT_PRODUCT_LOGO_ALT_TEXT);
 
