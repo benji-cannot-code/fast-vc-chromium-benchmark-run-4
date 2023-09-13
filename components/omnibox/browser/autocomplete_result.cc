7b9d8bf61862bf958e94b2b8eaea0fc0765bd045
Tomasz Wiszkowski <ender@google.com>
2023-09-13 16:17:17 +0000

    Notify Java AutocompleteResult about broken relationship.
    This change ensures we don't initiate calls to VerifyCoherency when
    C++ breaks relationship.
    
    Technically Native object continues to be valid, but its content may
    not reflect what Java holds, leading to an unnecessary VerifyCoherency
    call.
    
    Added destructor call for completeness.
    
    Change-Id: I114a9cc53a274c380fb5df9934d8ef1c030e6e0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4859268
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196043}

--- components/omnibox/browser/autocomplete_result.cc
+++ components/omnibox/browser/autocomplete_result.cc
@@ -165,7 +165,11 @@ AutocompleteResult::AutocompleteResult() {
   MergeSuggestionGroupsMap(omnibox::BuildDefaultGroups());
 }
 
-AutocompleteResult::~AutocompleteResult() = default;
+AutocompleteResult::~AutocompleteResult() {
+#if BUILDFLAG(IS_ANDROID)
+  DestroyJavaObject();
+#endif
+}
 
 void AutocompleteResult::TransferOldMatches(const AutocompleteInput& input,
                                             AutocompleteResult* old_matches) {
@@ -1032,7 +1036,7 @@ void AutocompleteResult::Reset() {
   suggestion_groups_map_.clear();
   MergeSuggestionGroupsMap(omnibox::BuildDefaultGroups());
 #if BUILDFLAG(IS_ANDROID)
-  java_result_.Reset();
+  DestroyJavaObject();
 #endif
 }
 
@@ -1040,8 +1044,8 @@ void AutocompleteResult::Swap(AutocompleteResult* other) {
   matches_.swap(other->matches_);
   suggestion_groups_map_.swap(other->suggestion_groups_map_);
 #if BUILDFLAG(IS_ANDROID)
-  java_result_.Reset();
-  other->java_result_.Reset();
+  DestroyJavaObject();
+  other->DestroyJavaObject();
 #endif
 }
 
@@ -1052,7 +1056,7 @@ void AutocompleteResult::CopyFrom(const AutocompleteResult& other) {
   matches_ = other.matches_;
   suggestion_groups_map_ = other.suggestion_groups_map_;
 #if BUILDFLAG(IS_ANDROID)
-  java_result_.Reset();
+  DestroyJavaObject();
 #endif
 }
 
