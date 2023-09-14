ad278c42170e14f25de5457a9ec900af9b8c1d20
Joel Hockey <joelhockey@chromium.org>
2023-09-14 22:12:12 +0000

    Fix devtools to work with WebView
    Mark all WebView WebContentses and set devtools type to
    "popup_page" rather than "other".
    
    This is an alternative to listing the URLs of every dialog in
    https://source.chromium.org/chromium/chromium/src/+/main:third_party/devtools-frontend/src/front_end/core/sdk/ChildTargetManager.ts;l=132-139;drc=7d299a1755d7d6c0e841241c35db7c83a40210f5
    
    Change-Id: Ib61c34b4f44179f107e00e2c4f08dc08f8bd007d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4842093
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Vladimir Nechaev <nechaev@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196847}

--- chrome/browser/devtools/chrome_devtools_manager_delegate.cc
+++ chrome/browser/devtools/chrome_devtools_manager_delegate.cc
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/manifest.h"
 #include "extensions/common/mojom/view_type.mojom.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "ui/views/controls/webview/webview.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ash/constants/ash_switches.h"
@@ -170,9 +171,15 @@ std::string ChromeDevToolsManagerDelegate::GetTargetType(
 
   std::string extension_name;
   std::string extension_type;
-  if (!GetExtensionInfo(web_contents, &extension_name, &extension_type))
-    return DevToolsAgentHost::kTypeOther;
-  return extension_type;
+  if (GetExtensionInfo(web_contents, &extension_name, &extension_type)) {
+    return extension_type;
+  }
+
+  if (views::WebView::IsWebViewContents(web_contents)) {
+    return DevToolsAgentHost::kTypePage;
+  }
+
+  return DevToolsAgentHost::kTypeOther;
 }
 
 std::string ChromeDevToolsManagerDelegate::GetTargetTitle(
