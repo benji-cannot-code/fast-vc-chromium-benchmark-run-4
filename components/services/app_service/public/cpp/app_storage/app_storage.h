d3cc4b639526d410b7318e59f2d0f1571aa85542
Nancy Wang <nancylingwang@chromium.org>
2023-09-14 07:47:37 +0000

    Reading the app info data from the AppStorage file.
    This CL adds the ReadFromFile function to read the app info from the
    AppStorage file.
    
    This CL should not affect any current functions/implementations as
    the flag is disabled.
    
    TODO with the follow up CLs:
    1. Add tests.
    2. Get the app types to set initialized for app types.
    3. Write the app info to the AppStorage file.
    4. Add other fields in the App structure to AppStorage.
    5. Handle the conflict with OnAppUpdate by app platforms.
    
    BUG=1385932
    
    Change-Id: Ie01a287c31c8431c42d2ef2dc22c9b46a42f83b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4857572
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196443}

--- components/services/app_service/public/cpp/app_storage/app_storage.h
+++ components/services/app_service/public/cpp/app_storage/app_storage.h
@@ -6,11 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SERVICES_APP_SERVICE_PUBLIC_CPP_APP_STORAGE_APP_STORAGE_H_
 #define COMPONENTS_SERVICES_APP_SERVICE_PUBLIC_CPP_APP_STORAGE_APP_STORAGE_H_
 
+#include <vector>
+
 #include "base/compiler_specific.h"
 #include "base/component_export.h"
+#include "base/memory/raw_ref.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "components/services/app_service/public/cpp/app_registry_cache.h"
+#include "components/services/app_service/public/cpp/app_types.h"
 
 namespace base {
 class FilePath;
@@ -39,11 +44,19 @@ class COMPONENT_EXPORT(APP_UPDATE) AppStorage
   void OnAppRegistryCacheWillBeDestroyed(
       apps::AppRegistryCache* cache) override;
 
+  // Invoked when reading the app info data from the AppStorage file is
+  // finished.
+  void OnGetAppInfoData(std::vector<AppPtr> apps);
+
+  raw_ref<apps::AppRegistryCache> app_registry_cache_;
+
   scoped_refptr<AppStorageFileHandler> file_handler_;
 
   base::ScopedObservation<apps::AppRegistryCache,
                           apps::AppRegistryCache::Observer>
       app_registry_cache_observer_{this};
+
+  base::WeakPtrFactory<AppStorage> weak_factory_{this};
 };
 
 }  // namespace apps
