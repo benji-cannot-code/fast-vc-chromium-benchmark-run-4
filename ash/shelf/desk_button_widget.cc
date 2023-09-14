ca8d98c5779438ccc4d5e22a00b68ffe90ac66a4
Yongshun Liu <yongshun@chromium.org>
2023-09-14 15:15:36 +0000

    bento_button: Fix desk button animation on shelf
    This fixes animation for two cases:
      - app pin or unpin
      - device scale factor change
    
    With this, it only animates x axis movement for bottom shelf and y axis
    movement for side shelf when the widget size remains the same and non
    empty.
    
    Demo:
      b/296109302#comment2
      b/299353741#comment3
    
    Bug: b/299353741, b/296109302, b/300139548
    Test: manual
    Change-Id: I7a610e6cdec3fb771f9a91487af4fa8253a2378a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4865026
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196604}

--- ash/shelf/desk_button_widget.cc
+++ ash/shelf/desk_button_widget.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/rtl.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/gfx/geometry/point.h"
+#include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/transform_util.h"
 #include "ui/views/animation/animation_builder.h"
 #include "ui/views/background.h"
@@ -197,13 +198,13 @@ bool DeskButtonWidget::ShouldBeVisible() const {
 }
 
 void DeskButtonWidget::SetExpanded(bool expanded) {
-  if (is_expanded_ == expanded) {
+  if (is_expanded_ == expanded || !ShouldBeVisible()) {
     return;
   }
 
   is_expanded_ = expanded;
 
-  if (is_horizontal_shelf_ && ShouldBeVisible()) {
+  if (is_horizontal_shelf_) {
     // If we are in horizontal alignment, then we need to recalculate and update
     // the hotseat bounds with the new button state before recalculating and
     // updating the desk button bounds so that the hotseat provides the correct
@@ -233,8 +234,12 @@ void DeskButtonWidget::PrepareForAlignmentChange(ShelfAlignment new_alignment) {
 }
 
 void DeskButtonWidget::CalculateTargetBounds() {
-  target_bounds_ =
-      is_expanded_ ? GetTargetExpandedBounds() : GetTargetShrunkBounds();
+  if (ShouldBeVisible()) {
+    target_bounds_ =
+        is_expanded_ ? GetTargetExpandedBounds() : GetTargetShrunkBounds();
+  } else {
+    target_bounds_ = gfx::Rect();
+  }
 }
 
 gfx::Rect DeskButtonWidget::GetTargetBounds() const {
@@ -260,7 +265,13 @@ void DeskButtonWidget::UpdateLayout(bool animate) {
     return;
   }
 
-  const bool animate_transform = initial_bounds.size() == target_bounds_.size();
+  // We only animate x axis movement for bottom shelf and y axis movement for
+  // side shelf when the widget size remains the same and non empty.
+  const bool animate_transform =
+      initial_bounds.size() == target_bounds_.size() &&
+      !target_bounds_.IsEmpty() &&
+      ((is_horizontal_shelf_ && initial_bounds.y() == target_bounds_.y()) ||
+       (!is_horizontal_shelf_ && initial_bounds.x() == target_bounds_.x()));
 
   if (animate_transform) {
     const gfx::Transform initial_transform = gfx::TransformBetweenRects(
