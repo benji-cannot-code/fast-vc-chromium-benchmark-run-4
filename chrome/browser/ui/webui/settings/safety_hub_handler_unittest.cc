04aef52fba3eb652e9f9d59ff26e786509db0ebe
Eduard Hez <ehez@chromium.org>
2023-09-14 05:42:59 +0000

    [SHv2] Move SafetyHub constants to a separate file
    This CL moves values that are required in several places (e.g. handler
    and services) into a constants file.
    
    Bug: 1443466
    Change-Id: Ia3db18e08e59ceff227722024761663a8bafd44f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4863618
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Commit-Queue: Eduard Hez <ehez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196425}

--- chrome/browser/ui/webui/settings/safety_hub_handler_unittest.cc
+++ chrome/browser/ui/webui/settings/safety_hub_handler_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/ui/safety_hub/notification_permission_review_service_factory.h"
+#include "chrome/browser/ui/safety_hub/safety_hub_constants.h"
 #include "chrome/browser/ui/safety_hub/unused_site_permissions_service.h"
 #include "chrome/browser/ui/webui/settings/safety_hub_handler.h"
 #include "chrome/browser/ui/webui/settings/site_settings_helper.h"
@@ -36,6 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "url/gurl.h"
+
+using safety_hub::SafetyHubCardState;
 
 enum SettingManager { USER, ADMIN, EXTENSION };
 constexpr char kUnusedTestSite[] = "https://example1.com";
