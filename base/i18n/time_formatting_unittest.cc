aeb5f2834d30a846e70041f3beed7bd4f29c5996
Peter Kasting <pkasting@chromium.org>
2023-09-14 16:24:09 +0000

    Use aggregate init/designed initializers more: /base
    This allows more variables to be const/constexpr, provides field names,
    and allows us to avoid explicitly zeroing.
    
    This CL was uploaded by git cl split.
    
    Bug: none
    Change-Id: I1df67092717dd498db81ba9a2343607da8882e73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4865159
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196643}

--- base/i18n/time_formatting_unittest.cc
+++ base/i18n/time_formatting_unittest.cc
@@ -23,10 +23,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 namespace {
 
-const Time::Exploded kTestDateTimeExploded = {
-    2011, 4,  6, 30,  // Sat, Apr 30, 2011
-    22,   42, 7, 0    // 22:42:07.000 in UTC = 15:42:07 in US PDT.
-};
+constexpr Time::Exploded kTestDateTimeExploded = {.year = 2011,
+                                                  .month = 4,
+                                                  .day_of_week = 6,
+                                                  .day_of_month = 30,
+                                                  .hour = 22,
+                                                  .minute = 42,
+                                                  .second = 7};
 
 // Returns difference between the local time and GMT formatted as string.
 // This function gets |time| because the difference depends on time,
