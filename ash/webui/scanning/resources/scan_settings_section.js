2bc2630ea3cd8f5e1e0ee3e1b9563fa053da5380
Ashley Prasad <ashleydp@google.com>
2023-09-14 17:08:36 +0000

    scanning: Replace html_to_js with html_to_wrapper
    In preparation of TypeScript migration, replace html_to_js with
    html_to_wrapper to enable using build_webui.
    
    Bug: b:299520240
    Test: browser_tests --gtest_filter=*ScanningApp*
    Test: tast run -build=true ${DUT} scanapp.* (excluding hardware tests)
    Change-Id: I832742cd2d5c05f18665f3263b8a1c4d647abf83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4859852
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1196668}

--- ash/webui/scanning/resources/scan_settings_section.js
+++ ash/webui/scanning/resources/scan_settings_section.js
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import './scanning_fonts_css.js';
 import './scanning_shared_css.js';
 
-import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {getTemplate} from './scan_settings_section.html.js';
 
 /** @polymer */
 class ScanSettingsSectionElement extends PolymerElement {
@@ -15,7 +17,7 @@ class ScanSettingsSectionElement extends PolymerElement {
   }
 
   static get template() {
-    return html`{__html_template__}`;
+    return getTemplate();
   }
 }
 
--- ash/webui/scanning/resources/scanning_fonts_css.js
+++ ash/webui/scanning/resources/scanning_fonts_css.js
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {getTemplate} from './scanning_fonts_css.html.js';
 
 const styleMod = document.createElement('dom-module');
-styleMod.appendChild(html`{__html_template__}`.content);
+styleMod.appendChild(getTemplate().content);
 styleMod.register('scanning-fonts');
