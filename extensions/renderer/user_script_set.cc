b1d7ddb67538110724e936a648666d1a5f4f1745
Emilia Paz <emiliapaz@chromium.org>
2023-09-14 20:19:51 +0000

    [Extensions UserScripts] Change UserScript::File to UserScript::Content
    UserScript::File used to represent the script information containing
    the file path. Later, it was also used to store content without a
    file. Thus, we can update the name to better reflect it.
    
    No functionality changed.
    
    Bug: 1482402
    Change-Id: I4e9943ad17f876132ca698e7f4801b2880020d1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4861423
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196772}

--- extensions/renderer/user_script_set.cc
+++ extensions/renderer/user_script_set.cc
@@ -239,7 +239,7 @@ std::unique_ptr<ScriptInjection> UserScriptSet::GetInjectionForScript(
   return injection;
 }
 
-blink::WebString UserScriptSet::GetJsSource(const UserScript::File& file,
+blink::WebString UserScriptSet::GetJsSource(const UserScript::Content& file,
                                             bool emulate_greasemonkey) {
   const GURL& url = file.url();
   auto iter = script_sources_.find(url);
@@ -263,7 +263,7 @@ blink::WebString UserScriptSet::GetJsSource(const UserScript::File& file,
   return source;
 }
 
-blink::WebString UserScriptSet::GetCssSource(const UserScript::File& file) {
+blink::WebString UserScriptSet::GetCssSource(const UserScript::Content& file) {
   const GURL& url = file.url();
   auto iter = script_sources_.find(url);
   if (iter != script_sources_.end())
