eb9b614901da64c7662240c653e76782d0a1534b
Patricia Alfonso <trishalfonso@google.com>
2023-09-14 21:02:43 +0000

    [3PCD] [Mode B] Introduce experiment pref
    Define the 3PCD Experiment client-wide (local store) and profile prefs.
    The client-wide prefs will store a client's membership status in the
    Mode B experiment and its registration status in the synthetic trial.
    Use an integer pref for cohort so these values correspond to the enum
    value for the feature param tpcd::experimentkCohort and a boolean for
    isRegistered. The profile pref will store if the client is eligible and
    that will be used in combination with the onboarding status from the UX
    onboarding service to tell the client-wide manager whether to register
    the client for the synthetic trial.
    
    Later CLs will introduce the filters to determine a client's membership
    status in the experiment. Once determined eligible and registered in the
    synthetic trial, set this pref to the correct experiment arm. If
    determined ineligible at this point, immediately set that in the
    experiment pref. Once a client is marked "ineligible" this pref should
    not change. It is possible for a previously eligible client to become
    ineligible (by signing into a profile that is ineligible), but mostly
    this should just be set once.
    
    Change-Id: I554a082bdc8803065353367dbfa8363d6694ac45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4827721
    Reviewed-by: Ryan Tarpine <rtarpine@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Patricia Alfonso <trishalfonso@google.com>
    Cr-Commit-Position: refs/heads/main@{#1196801}

---
+++ chrome/browser/tpcd/experiment/tpcd_pref_names.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/tpcd/experiment/tpcd_pref_names.h"
+
+#include "chrome/browser/tpcd/experiment/tpcd_utils.h"
+#include "components/prefs/pref_registry_simple.h"
+
+namespace tpcd::experiment {
+namespace prefs {
+
+// Local State Prefs
+const char kTPCDExperimentClientState[] = "tpcd_experiment.client_state";
+
+// Profile Prefs
+const char kTPCDExperimentProfileState[] = "tpcd_experiment.profile_state";
+
+}  // namespace prefs
+
+void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
+  registry->RegisterIntegerPref(
+      prefs::kTPCDExperimentClientState,
+      static_cast<int>(utils::ExperimentState::kUnknownEligiblity));
+}
+
+void RegisterProfilePrefs(PrefRegistrySimple* registry) {
+  registry->RegisterIntegerPref(
+      prefs::kTPCDExperimentProfileState,
+      static_cast<int>(utils::ExperimentState::kUnknownEligiblity));
+}
+
+}  // namespace tpcd::experiment
