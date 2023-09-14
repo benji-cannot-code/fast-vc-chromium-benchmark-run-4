83af0558784f573b20055cfff71e61fd1919c58c
Emmanuel Arias Soto <eariassoto@google.com>
2023-09-14 09:43:35 +0000

    [Accessibility] Add Settings UI menu entry for face tracking feature
    Adds a new menu entry under "Accessibility -> Cursor and touchpad" to
    enable the feature to control the mouse cursor and keyboard with
    face movements. This feature is a prototype and guarded by a feature
    flag.
    
    Screenshots: screen/9EHxuQz3gR5vQDx, screen/3gGexxeipBDEVdy
    Flag: AccessibilityGameFaceIntegration
    Bug: b:299743067
    Test: browser_tests --gtest_filter="OSSettingsOsA11yPageCursorAndTouchpadPage*"
    Change-Id: Ifdfad390a271e6a7317b96757024a7c0234ad693
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4854400
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Emmanuel Arias Soto <eariassoto@google.com>
    Cr-Commit-Position: refs/heads/main@{#1196490}

--- ash/accessibility/accessibility_controller_impl.cc
+++ ash/accessibility/accessibility_controller_impl.cc
@@ -1049,6 +1049,8 @@ void AccessibilityControllerImpl::RegisterProfilePrefs(
                                 false);
   registry->RegisterBooleanPref(
       prefs::kAccessibilityTabletModeShelfNavigationButtonsEnabled, false);
+  registry->RegisterBooleanPref(prefs::kAccessibilityFaceTrackingEnabled,
+                                false);
 
   // Not syncable because it might change depending on application locale,
   // user settings, and because different languages can cause speech recognition
--- chrome/browser/resources/ash/settings/os_a11y_page/cursor_and_touchpad_page.ts
+++ chrome/browser/resources/ash/settings/os_a11y_page/cursor_and_touchpad_page.ts
@@ -219,6 +219,18 @@ export class SettingsCursorAndTouchpadPageElement extends
       },
 
       /**
+       * Whether the face movements mouse cursor and keyboard control feature is
+       * enabled.
+       */
+      isAccessibilityFaceTrackingEnabled_: {
+        type: Boolean,
+        value() {
+          return loadTimeData.getBoolean(
+              'isAccessibilityGameFaceIntegrationEnabled');
+        },
+      },
+
+      /**
        * Used by DeepLinkingMixin to focus this page's deep links.
        */
       supportedSettingIds: {
@@ -251,6 +263,7 @@ export class SettingsCursorAndTouchpadPageElement extends
   private shelfNavigationButtonsPref_:
       chrome.settingsPrivate.PrefObject<boolean>;
   private showShelfNavigationButtonsSettings_: boolean;
+  private isAccessibilityFaceTrackingEnabled_: boolean;
 
   constructor() {
     super();
