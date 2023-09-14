11f22d0b03ed6782a8505e5ada312eac086b0160
Hira Mahmood <hiramahmood@google.com>
2023-09-14 18:50:14 +0000

    [iOS] Create ParcelTrackingInfobarDelegate
    This CL creates the ParcelTrackingInfobarDelegate, which will retrieve
    data for the parcel tracking banner and modal infobars.
    
    Bug: 1473449
    Change-Id: Ib7cadc8598dbe871537e232b7c49181c5a29d16c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4856667
    Reviewed-by: David Jean <djean@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196738}

--- ios/chrome/browser/shared/public/commands/parcel_tracking_opt_in_commands.h
+++ ios/chrome/browser/shared/public/commands/parcel_tracking_opt_in_commands.h
@@ -11,10 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Commands related to the parcel tracking opt-in prompt.
 @protocol ParcelTrackingOptInCommands <NSObject>
 
-// Shows the parcel tracking opt-in prompt.
-- (void)showParcelTrackingOptInPromptWithParcels:
-            (NSArray<CustomTextCheckingResult*>*)parcels
-                                     forWebState:(web::WebState*)webState;
+// Shows the parcel tracking opt-in UI if the user is eligible.
+- (void)showParcelTrackingUIWithParcels:
+    (NSArray<CustomTextCheckingResult*>*)parcels;
 
 @end
 
