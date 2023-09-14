f89e4543d6445b9fbcc37eefa6fbc7a0d751f891
Takumi Fujimoto <takumif@chromium.org>
2023-09-14 22:35:29 +0000

    Update MdTextButtonWithDownArrow icon on state change
    This CL makes the button update its arrow color when its state changes
    e.g. from disabled to enabled.
    
    Bug: 1467969
    Change-Id: I5b7e034c180569b38ec3ff7ec268a6e23bd70b70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4865198
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196860}

--- chrome/browser/ui/views/controls/md_text_button_with_down_arrow.h
+++ chrome/browser/ui/views/controls/md_text_button_with_down_arrow.h
@@ -29,6 +29,7 @@ class MdTextButtonWithDownArrow : public MdTextButton {
  protected:
   // views::MdTextButton:
   void OnThemeChanged() override;
+  void StateChanged(ButtonState old_state) override;
 
  private:
   void SetDropArrowImage();
