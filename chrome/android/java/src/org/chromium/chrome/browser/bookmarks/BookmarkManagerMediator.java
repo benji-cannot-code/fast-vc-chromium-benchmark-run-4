e677e28bf8bb4381bb74907a5399b8830bfe01ad
Brandon Wylie <wylieb@chromium.org>
2023-09-14 21:21:25 +0000

    [bookmarks] Prevent reordering while a filter is active
    Since the complete hierarchy isn't visible, reordering doesn't have any
    meaning and should be disabled.
    
    Bug: 1433329
    Change-Id: I96d3764ab70ddf60ff8578dc3e812c60bfb0deea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4859278
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196820}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerMediator.java
@@ -101,8 +101,8 @@ class BookmarkManagerMediator
                     && mBookmarkDelegate.getCurrentUiMode() == BookmarkUiMode.FOLDER;
             if (BookmarkFeatures.isAndroidImprovedBookmarksEnabled()) {
                 return enabled
-                        && mBookmarkUiPrefs.getBookmarkRowSortOrder()
-                        == BookmarkRowSortOrder.MANUAL;
+                        && mBookmarkUiPrefs.getBookmarkRowSortOrder() == BookmarkRowSortOrder.MANUAL
+                        && mCurrentPowerFilter.isEmpty();
             }
             return enabled;
         }
