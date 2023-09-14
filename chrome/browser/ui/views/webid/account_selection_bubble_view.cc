5838d5e376379d5244e541ea33788a8c7e294a34
Jose Dapena Paz <jdapena@igalia.com>
2023-09-14 19:21:45 +0000

    libstdc++: cannot map to a pair with std::tie
    Apparently libstdc++ cannot assign with tuple to a pair, as it was
    done in AccountSelectionBubbleView. Return an initializer list.
    
    Bug: 957519
    Change-Id: I9badf2ffb50291381f8d6d2b0f88948cfc758354
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4864368
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196755}

--- chrome/browser/ui/views/webid/account_selection_bubble_view.cc
+++ chrome/browser/ui/views/webid/account_selection_bubble_view.cc
@@ -487,7 +487,7 @@ std::pair<std::u16string, std::u16string> GetErrorDialogText(
       kInvalidRequest, kUnauthorizedClient, kAccessDenied,
       kTemporarilyUnavailable};
   if (!codes_need_extra_description.contains(code)) {
-    return std::tie(summary, description);
+    return {summary, description};
   }
 
   if (url.is_empty()) {
@@ -500,7 +500,7 @@ std::pair<std::u16string, std::u16string> GetErrorDialogText(
                               idp_for_display);
   }
 
-  return std::tie(summary, description);
+  return {summary, description};
 }
 
 }  // namespace
