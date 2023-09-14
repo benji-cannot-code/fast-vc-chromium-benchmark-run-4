8f15de2772eb4a64d8f85a5dcb7460ea6d9da9c9
Daniel White <danieltwhite@google.com>
2023-09-14 15:11:49 +0000

    [iOS] Export MAX_* fill_constants
    This CL exports the MAX*_ constants instead of exposing them through
    __gCrWeb.
    
    Bug: 1380545
    Change-Id: I0a302d8832cd581e5224bfa00408b44a1a18f2ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4828083
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1196601}

--- components/autofill/ios/form_util/resources/fill_constants.js
+++ components/autofill/ios/form_util/resources/fill_constants.js
@@ -8,20 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  * This variable is from AutofillTable::kMaxDataLength in
  * chromium/src/components/autofill/core/browser/webdata/autofill_table.h
- *
- * @const {number}
  */
-__gCrWeb.fill.MAX_DATA_LENGTH = 1024;
+const MAX_DATA_LENGTH = 1024;
 
 /**
  * The maximum string length supported by Autofill.
  *
  * This variable is from kMaxStringLength in
  * chromium/src/components/autofill/core/common/autofill_constant.h
- *
- * @const {number}
  */
-__gCrWeb.fill.MAX_STRING_LENGTH = 1024;
+const MAX_STRING_LENGTH = 1024;
 
 /**
  * The maximum number of form fields we are willing to parse, due to
@@ -30,12 +26,10 @@ __gCrWeb.fill.MAX_STRING_LENGTH = 1024;
  * (3) router configuration pages; and (4) other configuration pages, e.g. for
  * Google code project settings.
  *
- * This variable is |kMaxExtractableFields| from
+ * This variable is `kMaxExtractableFields` from
  * chromium/src/components/autofill/core/common/autofill_constants.h
- *
- * @const {number}
  */
-__gCrWeb.fill.MAX_EXTRACTABLE_FIELDS = 200;
+const MAX_EXTRACTABLE_FIELDS = 200;
 
 /**
  * A bit field mask to extract data from WebFormControlElement for
@@ -106,3 +100,9 @@ __gCrWeb.fill.RENDERER_ID_NOT_SET = '0';
  * @const {symbol}
  */
 __gCrWeb.fill.ID_SYMBOL = window.Symbol.for('__gChrome~uniqueID');
+
+export {
+  MAX_DATA_LENGTH,
+  MAX_STRING_LENGTH,
+  MAX_EXTRACTABLE_FIELDS,
+};
