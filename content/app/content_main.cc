71b2c1a8a263aa121c07df93fcf5379990d24d1b
Peter Kvitek <kvitekp@chromium.org>
2023-09-14 00:36:40 +0000

    [headless] Route stdio to parent console on Windows
    This is similar to https://crrev.com/c/4153110, however, it will
    not create a console if one does not exist which is the reason
    https://crrev.com/c/4153110 was reverted.
    
    Bug: 1482491
    Change-Id: I26365c61b8a982f1c97d3fa8be967c490c386814
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4864838
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196340}

--- content/app/content_main.cc
+++ content/app/content_main.cc
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/win_util.h"
 #include "base/win/windows_version.h"
 #include "ui/base/win/atl_module.h"
+#include "ui/gfx/switches.h"
 #endif
 
 #if BUILDFLAG(IS_POSIX) && !BUILDFLAG(IS_ANDROID)
@@ -309,7 +310,12 @@ RunContentProcess(ContentMainParams params,
     // Route stdio to parent console (if any) or create one.
     if (base::CommandLine::ForCurrentProcess()->HasSwitch(
             switches::kEnableLogging)) {
-      base::RouteStdioToConsole(true);
+      base::RouteStdioToConsole(/*create_console_if_not_found*/ true);
+    } else if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+                   switches::kHeadless)) {
+      // When running in headless mode we want stdio routed however if
+      // console does not exist we should not create one.
+      base::RouteStdioToConsole(/*create_console_if_not_found*/ false);
     }
 #endif
 
