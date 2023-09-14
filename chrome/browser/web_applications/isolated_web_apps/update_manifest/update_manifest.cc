898dafe2830bb571b07ee8e037e249ea8ca6143a
Christian Flach <cmfcmf@chromium.org>
2023-09-14 11:20:33 +0000

    iwa: Allow "potentially trustworthy" IWA bundle URLs in Update Manifests
    This allows the browser to download IWA Web Bundles even over http, as
    long as the http origin is configured to be treated securely, either via
    `OverrideSecurityRestrictionsOnInsecureOrigin` policy or
    `--unsafely-treat-insecure-origin-as-secure` flag / command line switch.
    
    Fixed: b:282633777
    Bug: b:282633777, 1444407
    Change-Id: Ia5b7720d5cf7c808cae5ba96f904fa6310c7d059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4866318
    Commit-Queue: Christian Flach <cmfcmf@chromium.org>
    Auto-Submit: Christian Flach <cmfcmf@chromium.org>
    Reviewed-by: Oleksandr Peletskyi <peletskyi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1196517}

--- chrome/browser/web_applications/isolated_web_apps/update_manifest/update_manifest.cc
+++ chrome/browser/web_applications/isolated_web_apps/update_manifest/update_manifest.cc
@@ -13,8 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/expected.h"
 #include "base/version.h"
 #include "chrome/browser/web_applications/isolated_web_apps/isolated_web_app_version.h"
-#include "net/base/url_util.h"
-#include "url/url_constants.h"
+#include "services/network/public/cpp/is_potentially_trustworthy.h"
 
 namespace web_app {
 
@@ -58,13 +57,16 @@ UpdateManifest::CreateFromJson(const base::Value& json,
     CHECK(version.IsValid());
 
     GURL src = update_manifest_url.Resolve(*src_string);
-    if (!src.is_valid()) {
+    if (!src.is_valid() || src == update_manifest_url) {
       continue;
     }
-    if (!src.SchemeIs(url::kHttpsScheme) && !net::IsLocalhost(src)) {
-      // Only https: and localhost URLs are supported as the src URL.
-      // TODO(b/282633777): Re-consider this limitation for managed
-      // environments.
+    if (!src.SchemeIsHTTPOrHTTPS() ||
+        !network::IsUrlPotentiallyTrustworthy(src)) {
+      // Only https: and http: URLs are supported as the src URL. Also, they
+      // need to be "potentially trustworthy", which includes https:, localhost,
+      // and origins configured as trustworthy via enterprise policy. The
+      // separate check for the scheme is crucial, as file:// and some other
+      // URLs are "potentially trustworthy".
       continue;
     }
 
