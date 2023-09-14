1bcdb1f6724c66f251a169b195caf0d81ff4d03f
Nohemi Fernandez <fernandex@chromium.org>
2023-09-14 10:03:52 +0000

    Add feature flag to control kid-friendly content experiment.
    Bug: b/300260872
    Change-Id: I46fed59cddc59dda5d1546191201b6c532aa2994
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4859935
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196497}

--- components/supervised_user/core/common/features.cc
+++ components/supervised_user/core/common/features.cc
@@ -14,6 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace supervised_user {
 
+BASE_FEATURE(kKidFriendlyContentFeed,
+             "KidFriendlyContentFeed",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Enables local parent approvals for the blocked website on the Family Link
 // user's device.
 // The feature includes one experiment parameter: "preferred_button", which
@@ -153,6 +157,10 @@ bool IsChildAccountSupervisionEnabled() {
          base::FeatureList::IsEnabled(
              supervised_user::kClearingCookiesKeepsSupervisedUsersSignedIn);
 #endif
+}
+
+bool IsKidFriendlyContentFeedAvailable() {
+  return base::FeatureList::IsEnabled(kKidFriendlyContentFeed);
 }
 
 }  // namespace supervised_user
