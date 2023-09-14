b5419ca88900145a2d2614fcf6fc2b4e0108b922
Kent Tamura <tkent@chromium.org>
2023-09-14 07:49:08 +0000

    [RemoveLegacy] Merge TextDecorationOffsetBase into NGTextDecorationOffset
    This CL has no behavior changes.
    
    Bug: 1229581
    Change-Id: I9e5b34c2413ddc0295326bdc92b3dc3b7a045b05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4861366
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196445}

--- third_party/blink/renderer/core/paint/text_painter_base.cc
+++ third_party/blink/renderer/core/paint/text_painter_base.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/properties/longhands.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/highlight/highlight_style_utils.h"
-#include "third_party/blink/renderer/core/layout/text_decoration_offset_base.h"
 #include "third_party/blink/renderer/core/paint/applied_decoration_painter.h"
 #include "third_party/blink/renderer/core/paint/box_painter_base.h"
 #include "third_party/blink/renderer/core/paint/paint_info.h"
