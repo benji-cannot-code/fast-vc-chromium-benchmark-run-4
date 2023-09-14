9600dc80a159489bc85d170043f81d57cc97f563
Rebekah Potter <rbpotter@chromium.org>
2023-09-14 21:22:33 +0000

    Print Preview: Migrate tests to WebUIMochaBrowserTest, part 2
    Handling app, sidebar, pages and pdf tests. In order to migrate pages
    tests, adding PrintPreviewFocusTest.
    
    Bug: 1457360
    Change-Id: I8eb19f71eae6501e8fa95b974e29db4ecfc4f527
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4861280
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1196822}

--- chrome/test/data/webui/print_preview/print_preview_sidebar_test.ts
+++ chrome/test/data/webui/print_preview/print_preview_sidebar_test.ts
@@ -15,18 +15,7 @@ import {NativeLayerStub} from './native_layer_stub.js';
 import {getCddTemplate} from './print_preview_test_utils.js';
 
 
-const print_preview_sidebar_test = {
-  suiteName: 'PrintPreviewSidebarTest',
-  TestNames: {
-    SettingsSectionsVisibilityChange: 'settings sections visibility change',
-    SheetCountWithDuplex: 'sheet count with duplex',
-    SheetCountWithCopies: 'sheet count with copies',
-  },
-};
-
-Object.assign(window, {print_preview_sidebar_test: print_preview_sidebar_test});
-
-suite(print_preview_sidebar_test.suiteName, function() {
+suite('PrintPreviewSidebarTest', function() {
   let sidebar: PrintPreviewSidebarElement;
 
   let model: PrintPreviewModelElement;
@@ -58,8 +47,7 @@ suite(print_preview_sidebar_test.suiteName, function() {
   });
 
   test(
-      print_preview_sidebar_test.TestNames.SettingsSectionsVisibilityChange,
-      function() {
+      'SettingsSectionsVisibilityChange', function() {
         const moreSettingsElement =
             sidebar.shadowRoot!.querySelector('print-preview-more-settings')!;
         moreSettingsElement.$.label.click();
@@ -83,7 +71,7 @@ suite(print_preview_sidebar_test.suiteName, function() {
 
   // Tests that number of sheets is correctly calculated if duplex setting is
   // enabled.
-  test(print_preview_sidebar_test.TestNames.SheetCountWithDuplex, function() {
+  test('SheetCountWithDuplex', function() {
     const header = sidebar.shadowRoot!.querySelector('print-preview-header')!;
     assertEquals(1, header.sheetCount);
     sidebar.setSetting('pages', [1, 2, 3]);
@@ -96,7 +84,7 @@ suite(print_preview_sidebar_test.suiteName, function() {
 
   // Tests that number of sheets is correctly calculated if multiple copies
   // setting is enabled.
-  test(print_preview_sidebar_test.TestNames.SheetCountWithCopies, function() {
+  test('SheetCountWithCopies', function() {
     const header = sidebar.shadowRoot!.querySelector('print-preview-header')!;
     assertEquals(1, header.sheetCount);
     sidebar.setSetting('copies', 4);
