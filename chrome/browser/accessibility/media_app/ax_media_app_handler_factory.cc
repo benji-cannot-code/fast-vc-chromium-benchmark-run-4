d83e98699316043a158a9d0c4c6ef8ff6c828fe9
Nektarios Paisios <nektar@chromium.org>
2023-09-14 18:39:15 +0000

    Creates a skeleton implementation of the APIs used to talk to the Media App
    In an effort to make PDFs in the Media App on ChromeOS (AKA Backlight,)
    accessible, we aim to acquire images from every page of the PDF,
    send them to an OCR Service and build an accessibility tree from
    the results.
    
    Design doc:
    https://docs.google.com/document/d/1k8OH4XGjZrLVA705Xfy6vzSwGL9DkfRt9DG5s66lRjQ/edit?usp=sharing&resourcekey=0-K_9Y1EMhNoo8Ez9RPbQNMA
    
    AX_Relnotes: n/a.
    R=aleventhal@chromium.org
    b/289012145
    
    Change-Id: I20c8a930d0915ae29a9ec4c3ab0d156d9d4d7463
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4853328
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196727}

---
+++ chrome/browser/accessibility/media_app/ax_media_app_handler_factory.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/accessibility/media_app/ax_media_app_handler_factory.h"
+
+namespace ash {
+
+// static
+AXMediaAppHandlerFactory* AXMediaAppHandlerFactory::GetInstance() {
+  static base::NoDestructor<AXMediaAppHandlerFactory> instance;
+  return instance.get();
+}
+
+AXMediaAppHandlerFactory::AXMediaAppHandlerFactory() = default;
+AXMediaAppHandlerFactory::~AXMediaAppHandlerFactory() = default;
+
+AXMediaAppHandler& AXMediaAppHandlerFactory::CreateAXMediaAppHandler() {
+  auto handler = std::make_unique<AXMediaAppHandler>();
+  handlers_.push_back(std::move(handler));
+  return *(handlers_[handlers_.size() - 1u].get());
+}
+
+}  // namespace ash
