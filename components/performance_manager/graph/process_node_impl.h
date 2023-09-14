8fc953e62c08dd8f299e189d3fbf3f2de3e96a68
Joe Mason <joenotcharles@google.com>
2023-09-14 18:34:24 +0000

    Update CPU measurements when frames and workers are added or removed
    Also adds missing visitor methods to ProcessNode and WorkerNode, and fixes a crash when ResourceContextRegistryStorage observes a mock ProcessNode with an invalid id.
    
    Bug: 1471683
    Change-Id: Idcaf230b1d0f5253619c53d097a02d0a74cab613
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4850703
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196723}

--- components/performance_manager/graph/process_node_impl.h
+++ components/performance_manager/graph/process_node_impl.h
@@ -231,6 +231,7 @@ class ProcessNodeImpl
   absl::optional<int32_t> GetExitStatus() const override;
   const std::string& GetMetricsName() const override;
   bool VisitFrameNodes(const FrameNodeVisitor& visitor) const override;
+  bool VisitWorkerNodes(const WorkerNodeVisitor& visitor) const override;
   base::flat_set<const FrameNode*> GetFrameNodes() const override;
   base::flat_set<const WorkerNode*> GetWorkerNodes() const override;
   bool GetMainThreadTaskLoadIsLow() const override;
