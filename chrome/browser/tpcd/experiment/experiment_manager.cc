86a96dffb55910a441fff5e43cd3144532dc7b49
Patricia Alfonso <trishalfonso@google.com>
2023-09-14 15:18:14 +0000

    [3PCD][Experiment] Define 3PCD Experiment Manager skeletion
    For both Mode A and Mode B experiments, the eligibility criteria contain
    profile-level filter requirements, but the experiment and metrics are
    client-level. Define a manager for this client-level work.
    
    Change-Id: I912a5ce734ea9c76b3f270fbd7da42c5a851bbe0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4858983
    Reviewed-by: Anton Maliev <amaliev@chromium.org>
    Commit-Queue: Patricia Alfonso <trishalfonso@google.com>
    Cr-Commit-Position: refs/heads/main@{#1196605}

---
+++ chrome/browser/tpcd/experiment/experiment_manager.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/tpcd/experiment/experiment_manager.h"
+
+namespace tpcd::experiment {
+
+void ExperimentManager::SetClientEligibility(bool is_eligible,
+                                             bool is_onboarded) {
+  // TODO(trishalfonso@google.com): set the local state pref with appropriate
+  // experiment state enum value.
+}
+
+void ExperimentManager::RegisterSyntheticTrial() {
+  // TODO(trishalfonso@google.com): Register the client for the appropriate arm
+  // of the Mode B synthetic trial based on feature flags and params.
+}
+
+void ExperimentManager::UnregisterSyntheticTrial() {
+  // TODO(trishalfonos@google.com): Once synthetic trial is set up, register the
+  // current client for "invalidated" trial arm here.
+}
+
+bool ExperimentManager::isClientEligible() {
+  // TODO(trishalfonso@google.com): Add checks for if the client was installed <
+  // 30 days ago and if the client is on an Android device with PWAs installed.
+  return false;
+}
+
+}  // namespace tpcd::experiment
