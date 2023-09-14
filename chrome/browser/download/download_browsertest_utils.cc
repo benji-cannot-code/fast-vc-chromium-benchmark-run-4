6f8fad3c283298942ccafb26c4f222d99ec60663
Lily Chen <chlily@chromium.org>
2023-09-14 21:40:23 +0000

    [DownloadBubble] Add input event activation protector
    To mitigate the risk of clickjacking, introduce a delay during which
    no clicks will be processed.
    
    Bug: 1473957
    Change-Id: I2137e5602bad3ae324b462ca8ba9bffd6ba2056f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4864122
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196830}

--- chrome/browser/download/download_browsertest_utils.cc
+++ chrome/browser/download/download_browsertest_utils.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/mock_host_resolver.h"
 #include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
 #include "third_party/blink/public/common/switches.h"
+#include "ui/views/views_switches.h"
 
 using content::DownloadManager;
 using content::WebContents;
@@ -105,6 +106,11 @@ void DownloadTestBase::SetUpCommandLine(base::CommandLine* command_line) {
   // Slower builders (linux-chromeos-rel, debug, and maybe others) are flaky
   // due to slower loading interacting with deferred commits.
   command_line->AppendSwitch(blink::switches::kAllowPreCommitInput);
+
+  // Clicks from tests should always be allowed, even on dialogs that have
+  // protection against accidental double-clicking/etc.
+  command_line->AppendSwitch(
+      views::switches::kDisableInputEventActivationProtectionForTesting);
 }
 
 void DownloadTestBase::TearDownOnMainThread() {
