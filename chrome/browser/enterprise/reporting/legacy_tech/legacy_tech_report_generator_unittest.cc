6abd0c5755e809a940f7cddec8bd21a40f06aeee
Peter Kasting <pkasting@chromium.org>
2023-09-14 01:22:41 +0000

    Use aggregate init/designed initializers more: /chrome
    This allows more variables to be const/constexpr, provides field names,
    and allows us to avoid explicitly zeroing.
    
    This CL was uploaded by git cl split.
    
    R=benreich@chromium.org, dpapad@chromium.org, estark@chromium.org, finnur@chromium.org, jamescook@chromium.org, mfoltz@chromium.org, nsatragno@chromium.org, pmonette@chromium.org, romanarora@chromium.org, tommycli@chromium.org, waffles@chromium.org, zmin@chromium.org
    
    Bug: none
    Change-Id: Iccead2706821a86161c7630fbd10453a2cec396f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4865218
    Reviewed-by: James Cook <jamescook@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196362}

--- chrome/browser/enterprise/reporting/legacy_tech/legacy_tech_report_generator_unittest.cc
+++ chrome/browser/enterprise/reporting/legacy_tech/legacy_tech_report_generator_unittest.cc
@@ -12,15 +12,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace enterprise_reporting {
 
 namespace {
-base::Time::Exploded kTestDate = {
-    2023, 5,  4,  4,  // May. 4, 2023, Thu
-    22,   10, 15, 0   // 22:10:15.000
-};
 
-base::Time::Exploded kTestDateInMidnight = {
-    2023, 5, 4, 4,  // May. 4, 2023, Thu
-    0,    0, 0, 0   // 00:00:00.000
-};
+constexpr base::Time::Exploded kTestDate = {.year = 2023,
+                                            .month = 5,
+                                            .day_of_week = 4,
+                                            .day_of_month = 4,
+                                            .hour = 22,
+                                            .minute = 10,
+                                            .second = 15};
+
+constexpr base::Time::Exploded kTestDateInMidnight = {.year = 2023,
+                                                      .month = 5,
+                                                      .day_of_week = 4,
+                                                      .day_of_month = 4};
 
 }  // namespace
 
