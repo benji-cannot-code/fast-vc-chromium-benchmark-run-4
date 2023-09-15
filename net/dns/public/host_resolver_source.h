f9d2054f239ca03614e7e5da4f194219152a65f1
Eric Orth <ericorth@chromium.org>
2023-09-15 15:26:38 +0000

    Add (de)serialization support to HostResolverCache
    Used for persistence in Cronet code and for logging in all code.
    
    Bug: 1448654
    Change-Id: I4ff11f17a84bc4dc1e22266fa74123fc53ffd56d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4851682
    Commit-Queue: Eric Orth <ericorth@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197163}

--- net/dns/public/host_resolver_source.h
+++ net/dns/public/host_resolver_source.h
@@ -8,24 +8,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <iterator>
 
+#include "base/values.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
 namespace net {
 
 // Enumeration to specify the allowed results source for HostResolver
 // requests.
+//
+// Integer values used for (de)serialization. Do not renumber.
 enum class HostResolverSource {
   // Resolver will pick an appropriate source. Results could come from DNS,
   // MulticastDNS, HOSTS file, etc.
-  ANY,
+  ANY = 0,
 
   // Results will only be retrieved from the system or OS, eg via the
   // getaddrinfo() system call.
-  SYSTEM,
+  SYSTEM = 1,
 
   // Results will only come from DNS queries.
-  DNS,
+  DNS = 2,
 
   // Results will only come from Multicast DNS queries.
-  MULTICAST_DNS,
+  MULTICAST_DNS = 3,
 
   // No external sources will be used. Results will only come from fast local
   // sources that are available no matter the source setting, e.g. cache, hosts
@@ -33,10 +38,16 @@ enum class HostResolverSource {
   // to finish synchronously. Resolves with this settings will return
   // ERR_NAME_NOT_RESOLVED if an asynchronous IPv6 reachability probe needs to
   // be done.
-  LOCAL_ONLY,
+  LOCAL_ONLY = 4,
 
   MAX = LOCAL_ONLY
 };
+
+base::Value ToValue(HostResolverSource source);
+
+// absl::nullopt if `value` is malformed for deserialization.
+absl::optional<HostResolverSource> HostResolverSourceFromValue(
+    const base::Value& value);
 
 const HostResolverSource kHostResolverSources[] = {
     HostResolverSource::ANY, HostResolverSource::SYSTEM,
