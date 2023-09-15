dec19ce2795634af1175c080b3d129b02ffd6a5c
Dana Fried <dfried@chromium.org>
2023-09-15 23:45:38 +0000

    [User Education] Don't statically cache flag value
    There was an order-of-operations issue and a static caching issue that
    was causing test flakiness.
    
    Bug: 1479993
    Change-Id: I3c46c39f4f3264aabde2d77655aae3186c64d0c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4869536
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197435}

--- chrome/browser/user_education/user_education_configuration_provider.h
+++ chrome/browser/user_education/user_education_configuration_provider.h
@@ -39,6 +39,7 @@ class UserEducationConfigurationProvider
 
  private:
   user_education::FeaturePromoRegistry registry_;
+  const bool overwrite_valid_configurations_;
 };
 
 #endif  // CHROME_BROWSER_USER_EDUCATION_USER_EDUCATION_CONFIGURATION_PROVIDER_H_
