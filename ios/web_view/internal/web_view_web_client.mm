52b6f6ff9133a30c2ff06492673e5f8d2721ba44
Mike Dougherty <michaeldo@chromium.org>
2023-09-15 00:28:28 +0000

    Migrate web_view scripts to JavaScriptFeature
    Bug: 1481169
    Change-Id: I27fb41ba60e7baea3a93d1b935512ed889e48e0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4865197
    Reviewed-by: John Wu <jzw@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1196917}

--- ios/web_view/internal/web_view_web_client.mm
+++ ios/web_view/internal/web_view_web_client.mm
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web_view/internal/cwv_ssl_status_internal.h"
 #import "ios/web_view/internal/cwv_ssl_util.h"
 #import "ios/web_view/internal/cwv_web_view_internal.h"
+#import "ios/web_view/internal/js_messaging/web_view_scripts_java_script_feature.h"
 #import "ios/web_view/internal/safe_browsing/cwv_unsafe_url_handler_internal.h"
 #import "ios/web_view/internal/web_view_browser_state.h"
 #import "ios/web_view/internal/web_view_early_page_script_provider.h"
@@ -102,7 +103,8 @@ std::vector<web::JavaScriptFeature*> WebViewWebClient::GetJavaScriptFeatures(
       security_interstitials::IOSSecurityInterstitialJavaScriptFeature::
           GetInstance(),
       translate::TranslateJavaScriptFeature::GetInstance(),
-      WebViewMessageHandlerJavaScriptFeature::FromBrowserState(browser_state)};
+      WebViewMessageHandlerJavaScriptFeature::FromBrowserState(browser_state),
+      WebViewScriptsJavaScriptFeature::FromBrowserState(browser_state)};
 }
 
 NSString* WebViewWebClient::GetDocumentStartScriptForAllFrames(
