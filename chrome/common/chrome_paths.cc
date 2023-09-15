89bb0c117fcad596560063e6339beef7ade382cb
Andrea Orru <andreaorru@chromium.org>
2023-09-15 01:11:42 +0000

    Move Lacros components to /home/chronos/lacros_shared (under flag)
    This change introduces a kLacrosSharedComponentsDir feature flag
    for Ash that, when enabled, changes the directory for updated
    browser components from /home/chronos/user/lacros to
    /home/chronos/lacros_shared (outside of the cryptohome).
    
    This causes the Lacros browser components to be shared across users.
    Design doc: http://go/lacros-shared-components
    
    TEST=Enable the flag in Ash and verify components go into the new dir
    BUG=None
    
    Change-Id: I5defdf742459935b3b19f0d5500681f0c5195852
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4835273
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196928}

--- chrome/common/chrome_paths.cc
+++ chrome/common/chrome_paths.cc
@@ -44,6 +44,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "base/command_line.h"
+#include "chrome/common/chrome_switches.h"
 #include "chromeos/crosapi/cpp/crosapi_constants.h"  // nogncheck
 #include "chromeos/lacros/lacros_paths.h"
 #endif
@@ -387,12 +389,22 @@ bool PathProvider(int key, base::FilePath* result) {
       cur = cur.AppendASCII(kWidevineCdmBaseDirectory);
       break;
 
-    case chrome::DIR_COMPONENT_UPDATED_WIDEVINE_CDM:
-      if (!base::PathService::Get(chrome::DIR_USER_DATA, &cur)) {
+    case chrome::DIR_COMPONENT_UPDATED_WIDEVINE_CDM: {
+      int components_dir =
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+          base::CommandLine::ForCurrentProcess()->HasSwitch(
+              switches::kEnableLacrosSharedComponentsDir)
+              ? static_cast<int>(chromeos::lacros_paths::LACROS_SHARED_DIR)
+              : static_cast<int>(chrome::DIR_USER_DATA);
+#else
+          chrome::DIR_USER_DATA;
+#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
+      if (!base::PathService::Get(components_dir, &cur)) {
         return false;
       }
       cur = cur.AppendASCII(kWidevineCdmBaseDirectory);
       break;
+    }
     case chrome::FILE_COMPONENT_WIDEVINE_CDM_HINT:
       if (!base::PathService::Get(chrome::DIR_COMPONENT_UPDATED_WIDEVINE_CDM,
                                   &cur)) {
