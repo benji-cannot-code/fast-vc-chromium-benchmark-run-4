db4dac22d390d888e2b361fad09200feaec29b9d
Henrique Nakashima <hnakashima@chromium.org>
2023-09-15 21:36:46 +0000

    [Android] Use ResettersForTesting for BasePreferenceKeyChecker
    This always resets mKeyChecker after tests. Previously, the first test
    that disabled it would cause this side-effect to carry on to the rest of
    the shard (for Robolectric) or batch (for Instrumentation, although
    AFAICT this was not used in Instrumentation tests).
    
    Bug: 1483409
    Change-Id: Id7d66b87acffc8de417e02e8dcab37860cc5ebaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4865505
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197382}

--- chrome/browser/preferences/android/java/src/org/chromium/chrome/browser/preferences/SharedPreferencesManager.java
+++ chrome/browser/preferences/android/java/src/org/chromium/chrome/browser/preferences/SharedPreferencesManager.java
@@ -12,6 +12,7 @@ import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.ContextUtils;
+import org.chromium.base.ResettersForTesting;
 import org.chromium.base.StrictModeContext;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.build.BuildConfig;
@@ -61,15 +62,10 @@ public class SharedPreferencesManager {
         }
     }
 
-    BaseChromePreferenceKeyChecker swapKeyCheckerForTesting(
-            BaseChromePreferenceKeyChecker newChecker) {
-        BaseChromePreferenceKeyChecker swappedOut = mKeyChecker;
-        mKeyChecker = newChecker;
-        return swappedOut;
-    }
-
     public void disableKeyCheckerForTesting() {
+        BaseChromePreferenceKeyChecker swappedOut = mKeyChecker;
         mKeyChecker = new BaseChromePreferenceKeyChecker();
+        ResettersForTesting.register(() -> mKeyChecker = swappedOut);
     }
 
     /**
