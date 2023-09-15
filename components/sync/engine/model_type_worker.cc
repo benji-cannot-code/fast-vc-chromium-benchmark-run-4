242969ac1c5f33e08ea79acb8d8597222bcd5c9f
Rushan Suleymanov <rushans@google.com>
2023-09-15 12:08:00 +0000

    [Sync] Add histogram for encryption result
    This CL adds a new histogram to record whether encryption during Commit
    succeeded.
    
    Bug: 1468523
    Change-Id: I068f4963e8783633f59293bc3d817c93dc682897
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4836217
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1197091}

--- components/sync/engine/model_type_worker.cc
+++ components/sync/engine/model_type_worker.cc
@@ -60,6 +60,8 @@ const char kBlockedByUndecryptableUpdateHistogramName[] =
     "Sync.ModelTypeBlockedDueToUndecryptableUpdate";
 const char kPasswordNotesStateHistogramName[] =
     "Sync.PasswordNotesStateInUpdate";
+constexpr char kEntityEncryptionResultHistogramName[] =
+    "Sync.EntityEncryptionSucceeded";
 
 BASE_FEATURE(kSyncKeepGcDirectiveDuringSyncCycle,
              "SyncKeepGcDirectiveDuringSyncCycle",
@@ -67,6 +69,14 @@ BASE_FEATURE(kSyncKeepGcDirectiveDuringSyncCycle,
 
 void LogPasswordNotesState(PasswordNotesStateForUMA state) {
   base::UmaHistogramEnumeration(kPasswordNotesStateHistogramName, state);
+}
+
+void LogEncryptionResult(ModelType type, bool success) {
+  base::UmaHistogramBoolean(kEntityEncryptionResultHistogramName, success);
+  base::UmaHistogramBoolean(
+      base::StrCat({kEntityEncryptionResultHistogramName, ".",
+                    ModelTypeToHistogramSuffix(type)}),
+      success);
 }
 
 // A proxy which can be called from any sequence and delegates the work to the
@@ -1323,6 +1333,7 @@ void ModelTypeWorker::EncryptPasswordSpecificsData(
     CommitRequestDataList* request_data_list) {
   CHECK(cryptographer_);
   CHECK(encryption_enabled_);
+  CHECK_EQ(type_, PASSWORDS);
 
   for (std::unique_ptr<CommitRequestData>& request_data : *request_data_list) {
     EntityData* entity_data = request_data->entity.get();
@@ -1345,7 +1356,7 @@ void ModelTypeWorker::EncryptPasswordSpecificsData(
     bool result = cryptographer_->Encrypt(
         password_data,
         encrypted_password.mutable_password()->mutable_encrypted());
-    DCHECK(result);
+    LogEncryptionResult(type_, result);
     if (base::FeatureList::IsEnabled(syncer::kPasswordNotesWithBackup)) {
       // `encrypted_notes_backup` field needs to be populated regardless of
       // whether or not there are any notes.
@@ -1369,6 +1380,7 @@ void ModelTypeWorker::EncryptPasswordSpecificsData(
 void ModelTypeWorker::EncryptOutgoingPasswordSharingInvitations(
     CommitRequestDataList* request_data_list) {
   CHECK(cryptographer_);
+  CHECK_EQ(type_, OUTGOING_PASSWORD_SHARING_INVITATION);
 
   for (std::unique_ptr<CommitRequestData>& request_data : *request_data_list) {
     EntityData* entity_data = request_data->entity.get();
@@ -1390,7 +1402,7 @@ void ModelTypeWorker::EncryptOutgoingPasswordSharingInvitations(
     // There should not be encryption failure but DCHECK is not used because
     // it's not guaranteed. In the worst case, the entity will be committed with
     // empty specifics (no unencrypted data will be committed to the server).
-    // TODO(crbug.com/1468523): add a metric to record encryption failures.
+    LogEncryptionResult(type_, encrypted_data.has_value());
     if (encrypted_data) {
       specifics->set_encrypted_password_sharing_invitation_data(
           encrypted_data->data(), encrypted_data->size());
@@ -1418,9 +1430,9 @@ void ModelTypeWorker::EncryptSpecifics(
       continue;
     }
     sync_pb::EntitySpecifics encrypted_specifics;
-    // TODO(crbug.com/1468523): Log encryption result.
-    cryptographer_->Encrypt(entity_data->specifics,
-                            encrypted_specifics.mutable_encrypted());
+    bool success = cryptographer_->Encrypt(
+        entity_data->specifics, encrypted_specifics.mutable_encrypted());
+    LogEncryptionResult(type_, success);
     entity_data->specifics.CopyFrom(encrypted_specifics);
   }
 }
