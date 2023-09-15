c8c4cc7fbe5352276b04e8a9dd66be1068fb0031
minch <minch@google.com>
2023-09-15 17:50:47 +0000

    Remove WorkspaceLayoutManager::OnWindowBoundsChanged
    This function is not needed now with a default implementation.
    
    Bug: b/299947829
    Change-Id: Iff85e39182b33b61aa735ccf4bd3e9bef18d1964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4868319
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197247}

--- ash/wm/workspace/workspace_layout_manager.cc
+++ ash/wm/workspace/workspace_layout_manager.cc
@@ -348,12 +348,6 @@ void WorkspaceLayoutManager::OnWindowDestroying(aura::Window* window) {
   Shell::Get()->desks_controller()->MaybeRemoveVisibleOnAllDesksWindow(window);
 }
 
-void WorkspaceLayoutManager::OnWindowBoundsChanged(
-    aura::Window* window,
-    const gfx::Rect& old_bounds,
-    const gfx::Rect& new_bounds,
-    ui::PropertyChangeReason reason) {}
-
 //////////////////////////////////////////////////////////////////////////////
 // WorkspaceLayoutManager, wm::ActivationChangeObserver implementation:
 
