f70f38878c6cc122aca86f9491391c24039a9d4e
Sandor Major <sandormajor@google.com>
2023-09-15 21:12:43 +0000

    Delete PrefState enum
    Referencing code was deleted in https://crrev.com/c/3991275
    
    Change-Id: I359ff45a9a037d94d1f0e808ee8542cabf3a9372
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4864147
    Commit-Queue: Shuran Huang <shuuran@chromium.org>
    Reviewed-by: Shuran Huang <shuuran@chromium.org>
    Auto-Submit: Sandor «Alex» Major <sandormajor@google.com>
    Commit-Queue: Sandor «Alex» Major <sandormajor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1197349}

--- chrome/browser/first_party_sets/first_party_sets_policy_service_unittest.cc
+++ chrome/browser/first_party_sets/first_party_sets_policy_service_unittest.cc
@@ -710,12 +710,6 @@ TEST_F(ThirdPartyCookieBlockingFirstPartySetsPolicyServiceTest, AlwaysEnabled) {
   env().RunUntilIdle();
 }
 
-namespace {
-
-enum PrefState { kDefault, kDisabled, kEnabled };
-
-}  // namespace
-
 class FirstPartySetsPolicyServiceResumeThrottleTest
     : public FirstPartySetsPolicyServiceTest,
       public ::testing::WithParamInterface<bool> {
