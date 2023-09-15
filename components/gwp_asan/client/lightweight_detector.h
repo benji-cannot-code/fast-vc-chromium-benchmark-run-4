f51eb77f2e0b96111ed41fc57cf31a3c6c697a0a
Jose Dapena Paz <jdapena@igalia.com>
2023-09-15 06:46:15 +0000

    IWYU: usage of std::atomic requires include in lightweight_detector.h
    Bug: 957519
    Change-Id: Ib6e3b67b9e741af58c78c924c5756b80d37c3e67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4866063
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Reviewed-by: Jonathan Metzman <metzman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196995}

--- components/gwp_asan/client/lightweight_detector.h
+++ components/gwp_asan/client/lightweight_detector.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_GWP_ASAN_CLIENT_LIGHTWEIGHT_DETECTOR_H_
 #define COMPONENTS_GWP_ASAN_CLIENT_LIGHTWEIGHT_DETECTOR_H_
 
+#include <atomic>
+
 #include "base/gtest_prod_util.h"
 #include "components/gwp_asan/client/export.h"
 #include "components/gwp_asan/common/lightweight_detector_state.h"
