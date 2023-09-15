2cd1029da64dc6f6a6a97deac19f9fd9f5d26940
Georg Neis <neis@chromium.org>
2023-09-15 14:09:11 +0000

    Improve #lacros-availability-ignore documentation
    Clarify that the flag is only meaningful for Google-internal accounts.
    
    Bug: chromium:1481732
    Change-Id: I91cb24b6ead5c00f3a863b480b919cb3926160d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4868151
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Auto-Submit: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197132}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -6527,8 +6527,9 @@ const char kCrosComponentsDescription[] =
 const char kLacrosAvailabilityIgnoreName[] =
     "Ignore lacros-availability policy";
 const char kLacrosAvailabilityIgnoreDescription[] =
-    "Makes the lacros-availability policy have no effect. Instead Lacros "
-    "availability will be controlled by experiment and/or user flags.";
+    "Makes the lacros-availability policy have no effect on Google-internal "
+    "accounts. Instead, Lacros availability will be controlled by experiment "
+    "and/or user flags for such accounts.";
 
 const char kLacrosOnlyName[] = "Lacros is the only browser";
 const char kLacrosOnlyDescription[] =
