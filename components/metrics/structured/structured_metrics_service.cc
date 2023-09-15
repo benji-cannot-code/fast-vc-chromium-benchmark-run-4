228382702082be1795fb308be2bb955b01c921ac
Andrew Bregger <andrewbregger@google.com>
2023-09-15 21:04:46 +0000

    Populate SystemProfile in Structured Metrics Service
    Bug: b/299957735
    Change-Id: I2218ceb03ffa08d4d37a1204772940defffa4fd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4867260
    Reviewed-by: Jong Ahn <jongahn@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Andrew Bregger <andrewbregger@google.com>
    Cr-Commit-Position: refs/heads/main@{#1197336}

--- components/metrics/structured/structured_metrics_service.cc
+++ components/metrics/structured/structured_metrics_service.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/metrics/structured/structured_metrics_service.h"
 
+#include "components/metrics/metrics_log.h"
 #include "components/metrics/metrics_service_client.h"
 #include "components/metrics/structured/reporting/structured_metrics_reporting_service.h"
 #include "components/metrics/structured/structured_metrics_features.h"
+#include "third_party/metrics_proto/system_profile.pb.h"
 
 namespace metrics::structured {
 
@@ -172,6 +174,9 @@ void StructuredMetricsService::InitializeUmaProto(
   if (product != uma_proto.product()) {
     uma_proto.set_product(product);
   }
+
+  SystemProfileProto* system_profile = uma_proto.mutable_system_profile();
+  metrics::MetricsLog::RecordCoreSystemProfile(client_, system_profile);
 }
 
 // static:
@@ -199,6 +204,11 @@ StructuredMetricsService::GetLogStoreLimits() {
 void StructuredMetricsService::SetRecorderForTest(
     std::unique_ptr<StructuredMetricsRecorder> recorder) {
   recorder_ = std::move(recorder);
+}
+
+MetricsServiceClient* StructuredMetricsService::GetMetricsServiceClient()
+    const {
+  return client_;
 }
 
 }  // namespace metrics::structured
