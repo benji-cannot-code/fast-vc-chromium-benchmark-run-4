fbc2e4eaf8b439c3f1ad5731b9734f866522dff6
Bruno Braga <brunobraga@google.com>
2023-09-15 15:00:00 +0000

    [mullet m2] Filters interesting fields based on the last targeted ones.
    This CL allows to filter the fields to be filled, after a form
    interaction depending the last granularity.
    
    If for instance, an user chooses to a specific field to be
    filled, GetLastServerFieldTypesToFillForSection will return a set of
    fields with one value only. Which choose, to eventually tell
    the manager (and in a follow up CL the suggestions generator)
    that we want to filter out every field in the form (via SkipStatus)
    that does not match the triggering field. Similarly if the user chooses
    group filling, we make possible that in the next interaction,
    only fields that match the triggering field group is
    taken into account.
    
    Demo: https://screencast.googleplex.com/cast/NTM4MzczNzYxNjM2NzYxNnwyYzdkZDIwNC0zMg
    
    Bug: 1459990
    Change-Id: I626a5826fdcb12974e47196c698f7233dedbd3a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4863565
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1197149}

--- components/autofill/core/browser/autofill_granular_filling_utils.h
+++ components/autofill/core/browser/autofill_granular_filling_utils.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_AUTOFILL_GRANULAR_FILLING_UTILS_H_
 #define COMPONENTS_AUTOFILL_CORE_BROWSER_AUTOFILL_GRANULAR_FILLING_UTILS_H_
 
+#include "components/autofill/core/browser/autofill_type.h"
 #include "components/autofill/core/browser/field_types.h"
 
 // Helper methods specific for granular filling related behaviour.
@@ -22,6 +23,15 @@ ServerFieldTypeSet GetAddressFieldsForGroupFilling();
 // filling groups, i.e., FieldTypeGroup::kName fields, FieldTypeGroup::kPhone
 // fields or `GetAddressFieldsForGroupFilling` (from the method above).
 bool AreFieldsGranularFillingGroup(const ServerFieldTypeSet& fields);
+
+// Returns a set of fields to be filled, given the last targeted fields and
+// the current field type. For example, if the last targeted fields matches
+// one of the group filling sets, we will return the set of fields that matches
+// the trigering field group. This is done so that the user stays at the same
+// granularity as the one previously chosen.
+ServerFieldTypeSet GetTargetServerFieldsForTypeAndLastTargetedFields(
+    const ServerFieldTypeSet& last_targeted_fields,
+    const AutofillType& type);
 
 }  // namespace autofill
 
