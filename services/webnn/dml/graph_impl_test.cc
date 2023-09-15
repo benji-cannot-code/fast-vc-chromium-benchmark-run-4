a87b0bf0a5ecd77321024eb184c4fe2dd3fec927
Ningxin Hu <ningxin.hu@intel.com>
2023-09-15 01:56:14 +0000

    WebNN: Fix missing creating graph edge for operand c of DML gemm op
    This CL fixes the issue that the graph edge for optional operand c of
    DirectML gemm operator is missed to create when it presents.
    
    With this fix, it causes the `BuildOneInputAndOneConstantOperand` test
    to fail. The issue is if the optional operand of a DirectML operator
    doesn't present, for example the operand c of gemm, the compiled
    operator produced by `IDMLDevice::CompileOperator()` and
    `IDMLDevice1::CompileGraph()` would require different input binding
    count.
    
    To simplify the input binding handling, this CL removes the code path
    that supports single-operator-graph compilation by
    `IDMLDevice::CompileOperator()`. That causes all graph building unit
    tests to require `IDMLDevice1::CompileGraph()` to be available.
    
    Bug: 1482682,1273291
    Change-Id: I6ee5930aa6ed5b2ba8497db58b18a7d722eaec9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4865015
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Commit-Queue: ningxin hu <ningxin.hu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1196940}

--- services/webnn/dml/graph_impl_test.cc
+++ services/webnn/dml/graph_impl_test.cc
@@ -28,7 +28,6 @@ class WebNNGraphDMLImplTest : public TestBase {
   bool CreateAndBuildGraph(const mojom::GraphInfoPtr& graph_info);
 
  protected:
-  bool is_compile_graph_supported_ = true;
   base::test::TaskEnvironment task_environment_;
   scoped_refptr<Adapter> adapter_;
 };
@@ -39,8 +38,10 @@ void WebNNGraphDMLImplTest::SetUp() {
   Adapter::EnableDebugLayerForTesting();
   adapter_ = Adapter::GetInstanceForTesting();
   ASSERT_NE(adapter_.get(), nullptr);
-  is_compile_graph_supported_ =
-      adapter_->IsDMLDeviceCompileGraphSupportedForTesting();
+  // Graph compilation relies on IDMLDevice1::CompileGraph introduced in
+  // DirectML version 1.2 or DML_FEATURE_LEVEL_2_1, so skip the tests if the
+  // DirectML version doesn't support this feature.
+  SKIP_TEST_IF(!adapter_->IsDMLDeviceCompileGraphSupportedForTesting());
 }
 
 bool WebNNGraphDMLImplTest::CreateAndBuildGraph(
@@ -425,7 +426,6 @@ TEST_F(WebNNGraphDMLImplTest, BuildSingleOperatorSoftmax) {
 //       |
 //      relu2
 TEST_F(WebNNGraphDMLImplTest, BuildGraphWithTwoRelu) {
-  SKIP_TEST_IF(!is_compile_graph_supported_);
   // Build the mojom graph info.
   GraphInfoBuilder builder;
   uint64_t input_operand_id = builder.BuildInput(
@@ -461,7 +461,6 @@ TEST_F(WebNNGraphDMLImplTest, BuildSingleOperatorReshape) {
 //       |
 //     reshape
 TEST_F(WebNNGraphDMLImplTest, BuildGraphWithReshapeAsLastNode) {
-  SKIP_TEST_IF(!is_compile_graph_supported_);
   // Build the mojom graph info.
   GraphInfoBuilder builder;
   uint64_t input_operand_id = builder.BuildInput(
@@ -485,7 +484,6 @@ TEST_F(WebNNGraphDMLImplTest, BuildGraphWithReshapeAsLastNode) {
 //       |
 //      relu
 TEST_F(WebNNGraphDMLImplTest, BuildGraphWithReshapeAsIntermediateNode) {
-  SKIP_TEST_IF(!is_compile_graph_supported_);
   // Build the mojom graph info.
   GraphInfoBuilder builder;
   uint64_t input_operand_id = builder.BuildInput(
@@ -508,7 +506,6 @@ TEST_F(WebNNGraphDMLImplTest, BuildGraphWithReshapeAsIntermediateNode) {
 //       |
 //    reshape2
 TEST_F(WebNNGraphDMLImplTest, BuildGraphWithTwoReshape) {
-  SKIP_TEST_IF(!is_compile_graph_supported_);
   // Build the mojom graph info.
   GraphInfoBuilder builder;
   uint64_t input_operand_id = builder.BuildInput(
@@ -531,7 +528,6 @@ TEST_F(WebNNGraphDMLImplTest, BuildGraphWithTwoReshape) {
 //     |        |
 // [output1] [output2]
 TEST_F(WebNNGraphDMLImplTest, BuildGraphWithTwoOutputs) {
-  SKIP_TEST_IF(!is_compile_graph_supported_);
   // Build the mojom graph info.
   GraphInfoBuilder builder;
   uint64_t input_operand_id = builder.BuildInput(
@@ -670,7 +666,6 @@ TEST_F(WebNNGraphDMLImplTest, BuildMultipleInputsAppendingConstants) {
   // DML_FEATURE_LEVEL_4_0.
   SKIP_TEST_IF(GetMaxSupportedDMLFeatureLevel(adapter_->dml_device()) <
                DML_FEATURE_LEVEL_4_0);
-  SKIP_TEST_IF(!is_compile_graph_supported_);
   // Build the mojom graph info.
   GraphInfoBuilder builder;
   uint64_t input_a_operand_id =
@@ -722,7 +717,6 @@ TEST_F(WebNNGraphDMLImplTest, BuildMultipleConstantsAppendingInputs) {
   // DML_FEATURE_LEVEL_4_0.
   SKIP_TEST_IF(GetMaxSupportedDMLFeatureLevel(adapter_->dml_device()) <
                DML_FEATURE_LEVEL_4_0);
-  SKIP_TEST_IF(!is_compile_graph_supported_);
   // Build the mojom graph info.
   GraphInfoBuilder builder;
   uint64_t input_a_operand_id =
