28d5856096028178ed63fa375025533c181ef9e3
David Pennington <dpenning@chromium.org>
2023-09-15 23:30:25 +0000

    [TabOrganization] Create a basic session object.
    The session object owns the request and the organizations, and will be accessible via the service object.
    
    Change-Id: I748904a20a55a4501793d6c21b73dac978188233
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4850787
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197425}

---
+++ chrome/browser/ui/tabs/organization/tab_organization_session.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/tabs/organization/tab_organization_session.h"
+
+#include <vector>
+
+#include "chrome/browser/ui/tabs/organization/tab_organization.h"
+#include "chrome/browser/ui/tabs/organization/tab_organization_request.h"
+#include "tab_organization_session.h"
+
+TabOrganizationSession::TabOrganizationSession()
+    : TabOrganizationSession(std::make_unique<TabOrganizationRequest>()) {}
+
+TabOrganizationSession::TabOrganizationSession(
+    std::unique_ptr<TabOrganizationRequest> request)
+    : request_(std::move(request)), tab_organizations_({}) {}
+
+TabOrganizationSession::~TabOrganizationSession() = default;
+
+void TabOrganizationSession::StartRequest() {
+  CHECK(request_);
+  request_->SetResponseCallback(base::BindOnce(
+      &TabOrganizationSession::PopulateOrganizations, base::Unretained(this)));
+  request_->StartRequest();
+}
+
+void TabOrganizationSession::PopulateOrganizations(
+    const TabOrganizationResponse* response) {}
