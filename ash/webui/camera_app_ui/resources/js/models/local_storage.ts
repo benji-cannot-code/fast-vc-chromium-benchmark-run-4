8b4491f058fe4aff9e495f3ae5e3a04c6220d8b5
Dorah <dorahkim@google.com>
2023-09-15 10:06:52 +0000

    CCA: Change GA ID refreshing system
    This CL updates Google Analytics User ID refreshing system. It
    automatically refreshes User ID every 90 days whenever a user opens
    ChromeOS Camera App.
    
    Bug: b:296349884
    Test: From a console log.
    Change-Id: If02da5f5b5ac58fce13439e12e89774ad94c4e35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4853021
    Reviewed-by: Chu-Hsuan Yang <chuhsuan@chromium.org>
    Commit-Queue: Dorah Kim <dorahkim@google.com>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197056}

--- ash/webui/camera_app_ui/resources/js/models/local_storage.ts
+++ ash/webui/camera_app_ui/resources/js/models/local_storage.ts
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {
   assertBoolean,
   assertInstanceof,
+  assertNumber,
   assertString,
   checkEnumVariant,
 } from '../assert.js';
@@ -49,6 +50,13 @@ export function getString(key: LocalStorageKey, defaultValue = ''): string {
  */
 export function getBool(key: LocalStorageKey, defaultValue = false): boolean {
   return assertBoolean(getHelper(key, defaultValue));
+}
+
+/**
+ * @return The number in storage or |defaultValue| if not found.
+ */
+export function getNumber(key: LocalStorageKey, defaultValue = 0): number {
+  return assertNumber(getHelper(key, defaultValue));
 }
 
 /**
