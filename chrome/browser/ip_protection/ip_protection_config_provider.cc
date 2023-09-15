1603ea9a9b9c25a766277f06f750ed212315c348
Andrew Williams <awillia@chromium.org>
2023-09-15 15:39:06 +0000

    [IP Protection] Test code refactors and improvements
    This CL makes some test refactors and improvements which are helpful for
    follow-on work to reset cooldowns in the network service from the
    browser process. Specifically:
    
     - Ensure that Shutdown is always called on the
       IpProtectionConfigProvider in the unit tests. This becomes important
       later when Shutdown will need to unregister IdentityManager observers
       to prevent UaFs.
    
     - Disable IpProtectionConfigCache automatic cache management when using
       the VerifyIpProtectionConfigGetterForTesting method and cleanup state
       from any initial attempts to request tokens. This addresses an
       unintended interaction between the cache management logic and the
       TryGetAuthToken interceptor in the browser tests where the former would
       only send one token back, triggering repeated TryGetAuthTokens calls
       until the IpProtectionConfigCache had more than the low watermark
       number of tokens. Also, the IpProtectionConfigCache will try to get
       tokens during test setup and will likely get a cooldown time, which we
       want to clear when we are ready to start testing so that it doesn't
       interfere with the new VerifyIpProtectionConfigGetterForTesting
       behavior (see below)
    
     - Update VerifyIpProtectionConfigGetterForTesting so that it can also
       be used to test that the network service is in a cooldown period.
       This property will be used in a follow-up CL.
    
     - Replaces the "cache fill" naming in the IpProtectionConfigCache code
       to indicate that really it's just a call to TryGetAuthTokens, which
       might make tokens available but might also return a cooldown time.
    
     - Ensure that the BSA implementation is destroyed before the
       IpProtectionConfigProvider in the unit tests to prevent UaFs
    
    Bug: 1480010
    Change-Id: Ib9b2252386c85426bb3a3f64320544a35c977153
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4846958
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Andrew Williams <awillia@chromium.org>
    Reviewed-by: Dustin Mitchell <djmitche@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1197171}

--- chrome/browser/ip_protection/ip_protection_config_provider.cc
+++ chrome/browser/ip_protection/ip_protection_config_provider.cc
@@ -144,10 +144,13 @@ void IpProtectionConfigProvider::FetchBlindSignedToken(
   auto bsa_get_tokens_start_time = base::TimeTicks::Now();
   bsa_->GetTokens(
       access_token_info.token, batch_size,
-      [this, bsa_get_tokens_start_time, callback = std::move(callback)](
+      [weak_ptr = weak_ptr_factory_.GetWeakPtr(), bsa_get_tokens_start_time,
+       callback = std::move(callback)](
           absl::StatusOr<absl::Span<quiche::BlindSignToken>> tokens) mutable {
-        OnFetchBlindSignedTokenCompleted(bsa_get_tokens_start_time,
-                                         std::move(callback), tokens);
+        if (weak_ptr) {
+          weak_ptr->OnFetchBlindSignedTokenCompleted(
+              bsa_get_tokens_start_time, std::move(callback), tokens);
+        }
       });
 }
 
@@ -298,6 +301,7 @@ void IpProtectionConfigProvider::Shutdown() {
   // receiver set.
   receivers_.Clear();
   identity_manager_ = nullptr;
+  bsa_ = nullptr;
 }
 
 /*static*/
