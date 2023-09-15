2efa9ae82d4b9b1251a8c08ce57e561f8a5655d4
Norge Vizcay <vizcay@google.com>
2023-09-15 17:21:41 +0000

    Remove ScopedNewBadgeTracker from AutofillContextMenuManager.
    The ScopedNewBadgeTracker won't be used during the autofill feedback
    feature rollout. Therefore it can be removed.
    
    Change-Id: I9fddb48d04f88607a695805998f2069c0f044e82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4865110
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1197233}

--- chrome/browser/ui/autofill/autofill_context_menu_manager.h
+++ chrome/browser/ui/autofill/autofill_context_menu_manager.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/types/strong_alias.h"
-#include "chrome/browser/ui/user_education/scoped_new_badge_tracker.h"
 #include "components/autofill/content/browser/content_autofill_driver.h"
 #include "components/autofill/core/common/unique_ids.h"
 #include "components/renderer_context_menu/render_view_context_menu_base.h"
@@ -39,12 +38,10 @@ class AutofillContextMenuManager : public RenderViewContextMenuObserver {
   // Returns true if the given id is one generated for autofill context menu.
   static bool IsAutofillCustomCommandId(CommandId command_id);
 
-  AutofillContextMenuManager(
-      PersonalDataManager* personal_data_manager,
-      RenderViewContextMenuBase* delegate,
-      ui::SimpleMenuModel* menu_model,
-      Browser* browser,
-      std::unique_ptr<ScopedNewBadgeTracker> new_badge_tracker);
+  AutofillContextMenuManager(PersonalDataManager* personal_data_manager,
+                             RenderViewContextMenuBase* delegate,
+                             ui::SimpleMenuModel* menu_model,
+                             Browser* browser);
   ~AutofillContextMenuManager() override;
   AutofillContextMenuManager(const AutofillContextMenuManager&) = delete;
   AutofillContextMenuManager& operator=(const AutofillContextMenuManager&) =
@@ -92,8 +89,6 @@ class AutofillContextMenuManager : public RenderViewContextMenuObserver {
   const raw_ptr<RenderViewContextMenuBase> delegate_;
   const raw_ptr<Browser> browser_;
   content::ContextMenuParams params_;
-
-  std::unique_ptr<ScopedNewBadgeTracker> new_badge_tracker_;
 };
 
 }  // namespace autofill
