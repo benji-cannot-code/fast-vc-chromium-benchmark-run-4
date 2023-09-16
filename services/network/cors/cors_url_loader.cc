3453b118b2ccf4c8406c11bb1fb1e55eeda07b27
Kenichi Ishibashi <bashi@chromium.org>
2023-09-16 01:34:41 +0000

    Use NetLogStringValue() in NetLogCorsURLLoaderStartParams()
    Request headers may contain invalid UTF-8 sequences. Use
    NetLogStringValue() to escape them if exist.
    
    Bug: 1480616
    Change-Id: I107fa211555ce42df2441b9cc4586db0038fa6a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4867670
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197465}

--- services/network/cors/cors_url_loader.cc
+++ services/network/cors/cors_url_loader.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/load_flags.h"
 #include "net/cookies/cookie_partition_key.h"
 #include "net/http/http_status_code.h"
+#include "net/log/net_log_values.h"
 #include "services/network/cors/cors_url_loader_factory.h"
 #include "services/network/cors/cors_util.h"
 #include "services/network/masked_domain_list/network_service_resource_block_list.h"
@@ -110,7 +111,7 @@ base::Value::Dict NetLogCorsURLLoaderStartParams(
   return base::Value::Dict()
       .Set("url", request.url.possibly_invalid_spec())
       .Set("method", request.method)
-      .Set("headers", request.headers.ToString())
+      .Set("headers", net::NetLogStringValue(request.headers.ToString()))
       .Set("is_revalidating", request.is_revalidating)
       .Set("cors_preflight_policy", cors_preflight_policy);
 }
