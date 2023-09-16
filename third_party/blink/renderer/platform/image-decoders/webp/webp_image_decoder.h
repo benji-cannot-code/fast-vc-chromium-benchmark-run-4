5690d1940f26f96fa17597d39a8b97af0fffa193
Wan-Teh Chang <wtc@google.com>
2023-09-16 01:39:22 +0000

    Add WebP bpp (bits per pixel) histograms
    Add a family of 23 Blink.DecodedImage.WebPDensity.Count.* histograms to
    record the bits-per-pixel values of WebP images for various image sizes
    (where size is the area = width * height).
    
    - 9 histograms for image sizes 0.1 MP ~ 0.9 MP, in increments of 0.1 MP
    - 13 histograms for image sizes 1 MP - 13 MP, in increments of 1 MP
    - 1 histogram for image sizes > 13 MP
    
    To avoid recording outliers in these histograms, only lossy, still (not
    animated) images without an alpha plane are recorded in these
    histograms.
    
    The histograms are updated when the images are successfully decoded.
    
    Change the comparison
        demux_state_ <= WEBP_DEMUX_PARSING_HEADER
    to the equivalent comparison
        demux_state_ < WEBP_DEMUX_PARSED_HEADER
    and change
        DCHECK_GT(demux_state_, WEBP_DEMUX_PARSING_HEADER)
    to
        DCHECK_GE(demux_state_, WEBP_DEMUX_PARSED_HEADER)
    so that demux_state_ is compared with WEBP_DEMUX_PARSED_HEADER
    consistently.
    
    Bug: 1456182
    Change-Id: Ia876ad9845f7aee112d7a9ec319edaace96fc965
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4828711
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1197466}

--- third_party/blink/renderer/platform/image-decoders/webp/webp_image_decoder.h
+++ third_party/blink/renderer/platform/image-decoders/webp/webp_image_decoder.h
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/image-decoders/image_decoder.h"
 
+#include <stddef.h>
+
+#include "base/functional/callback.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 #include "webp/decode.h"
 #include "webp/demux.h"
@@ -46,7 +49,7 @@ class PLATFORM_EXPORT WEBPImageDecoder final : public ImageDecoder {
   // Returns true if the image data received so far (as stored in
   // |consolidated_data_|) can potentially be decoded and rendered from YUV
   // planes.
-  bool CanAllowYUVDecodingForWebP();
+  bool CanAllowYUVDecodingForWebP() const;
   bool HasImagePlanes() const { return image_planes_.get(); }
   bool DecodeSingleFrameToYUV(const uint8_t* data_bytes, wtf_size_t data_size);
   bool DecodeSingleFrame(const uint8_t* data_bytes,
@@ -71,7 +74,11 @@ class PLATFORM_EXPORT WEBPImageDecoder final : public ImageDecoder {
 
   raw_ptr<WebPIDecoder, DanglingUntriaged> decoder_;
   WebPDecBuffer decoder_buffer_;
+  // format_flags_ and is_lossy_not_animated_no_alpha_ are set when
+  // demux_state_ is greater than or equal to WEBP_DEMUX_PARSED_HEADER and
+  // WebPDemuxGetI(demux_, WEBP_FF_FRAME_COUNT) returns a nonzero value.
   int format_flags_;
+  bool is_lossy_not_animated_no_alpha_;
   bool frame_background_has_alpha_;
 
   // Provides the size of each component.
@@ -101,6 +108,11 @@ class PLATFORM_EXPORT WEBPImageDecoder final : public ImageDecoder {
   bool have_parsed_current_data_;
   int repetition_count_;
   int decoded_height_;
+  // Used to call UpdateBppHistogram<"WebP">() at most once to record the
+  // bits-per-pixel value of the image when the image is successfully decoded.
+  // Note that void(gfx::Size, size_t) is the function call signature of
+  // UpdateBppHistogram<"WebP">().
+  base::OnceCallback<void(gfx::Size, size_t)> update_bpp_histogram_callback_;
 
   typedef void (*AlphaBlendFunction)(ImageFrame&, ImageFrame&, int, int, int);
   AlphaBlendFunction blend_function_;
