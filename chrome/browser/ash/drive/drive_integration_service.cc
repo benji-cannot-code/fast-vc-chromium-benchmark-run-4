0a8b7b206e83cd6e2638dc02f03067cd885176cd
Ben Reich <benreich@chromium.org>
2023-09-16 07:45:04 +0000

    [FilesBulkPinning] Extract drive related prefs to static method
    Bug: b/300534960
    Change-Id: I3747003a447599f4abcd1a9ab8fef6603da925e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4862927
    Reviewed-by: François Degros <fdegros@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197511}

--- chrome/browser/ash/drive/drive_integration_service.cc
+++ chrome/browser/ash/drive/drive_integration_service.cc
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/drive/file_system_core_util.h"
 #include "components/drive/resource_metadata_storage.h"
 #include "components/metrics/metrics_pref_names.h"
+#include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
@@ -1680,6 +1681,31 @@ void DriveIntegrationService::OnNetworkChanged() {
   if (pin_manager_) {
     pin_manager_->SetOnline(is_online_);
   }
+}
+
+// static
+void DriveIntegrationService::RegisterProfilePrefs(
+    user_prefs::PrefRegistrySyncable* registry) {
+  registry->RegisterBooleanPref(
+      prefs::kDisableDrive, false,
+      user_prefs::PrefRegistrySyncable::SYNCABLE_OS_PREF);
+  registry->RegisterBooleanPref(
+      prefs::kDisableDriveOverCellular, true,
+      user_prefs::PrefRegistrySyncable::SYNCABLE_OS_PREF);
+  registry->RegisterBooleanPref(prefs::kDriveFsWasLaunchedAtLeastOnce, false);
+  registry->RegisterStringPref(prefs::kDriveFsProfileSalt, "");
+  registry->RegisterBooleanPref(prefs::kDriveFsPinnedMigrated, false);
+  registry->RegisterBooleanPref(prefs::kDriveFsEnableVerboseLogging, false);
+  // Do not sync prefs::kDriveFsEnableMirrorSync and
+  // prefs::kDriveFsMirrorSyncMachineId because we're syncing local files
+  // and users may wish to turn this off on a per device basis.
+  registry->RegisterBooleanPref(prefs::kDriveFsEnableMirrorSync, false);
+  registry->RegisterStringPref(prefs::kDriveFsMirrorSyncMachineRootId, "");
+  // Do not sync kDriveFsBulkPinningEnabled as this maintains files that are
+  // locally pinned to this device and should not sync the state across multiple
+  // devices.
+  registry->RegisterBooleanPref(prefs::kDriveFsBulkPinningVisible, true);
+  registry->RegisterBooleanPref(prefs::kDriveFsBulkPinningEnabled, false);
 }
 
 void DriveIntegrationService::OnDrivePrefChanged() {
