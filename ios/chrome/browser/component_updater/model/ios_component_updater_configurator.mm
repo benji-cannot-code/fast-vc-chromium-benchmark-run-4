aa9ce18ea1815243304d5766a6b857cf4105e960
Aman Verma <amanvr@google.com>
2023-09-17 15:42:02 +0000

    [Code Health] [iOS] Move ios/c/b/component_updater to model subfolder
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/<foo>/ will be moved to
    ios/c/b/<foo>/model/ to showcase that they are model files.
    
    Bug: 1319852, 1480286
    Change-Id: I7007b39a4e04b08dbbeb1ed18e58c2c18795fe4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4863426
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Aman Verma <amanvr@google.com>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Aman Verma <amanvr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1197634}

--- ios/chrome/browser/component_updater/model/ios_component_updater_configurator.mm
+++ ios/chrome/browser/component_updater/model/ios_component_updater_configurator.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/component_updater/ios_component_updater_configurator.h"
+#import "ios/chrome/browser/component_updater/model/ios_component_updater_configurator.h"
 
 #import <stdint.h>
 
