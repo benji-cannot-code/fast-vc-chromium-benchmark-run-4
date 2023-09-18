b9a1f2ad053ff12dec83dcaceb451a4c6f5bb433
Ana Salazar <anasalazar@chromium.org>
2023-09-18 19:37:58 +0000

    Remove app list drag and drop refactor flag
    Bug: 1479608
    Change-Id: I253fa5c7b7203b07466f5f3f7800b60e7639cf91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4874816
    Auto-Submit: Ana Salazar <anasalazar@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197958}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1693,9 +1693,6 @@ extern const char kQuickIntensiveWakeUpThrottlingAfterLoadingDescription[];
 extern const char kQuickDeleteForAndroidName[];
 extern const char kQuickDeleteForAndroidDescription[];
 
-extern const char kAppListDragAndDropRefactorName[];
-extern const char kAppListDragAndDropRefactorDescription[];
-
 extern const char kSettingsAppNotificationSettingsName[];
 extern const char kSettingsAppNotificationSettingsDescription[];
 
