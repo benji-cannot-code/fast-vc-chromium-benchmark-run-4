f1262436da2717f0b6d2bbaae80db3edb665bce7
Chuong Ho <hdchuong@google.com>
2023-09-18 00:54:34 +0000

    Bind mojo pipes after the editor service is setup
    After the creation of the required mojo classes (EditorClientConnector, EditorEventSinkProxy, TextActuator and TextQueryProvider), these mojo pipes need to be bound from both browser and shared library end.
    
    This CL adds a new class called `EditorServiceConnector` that manages the connection bootstrap between editor mediator and the orca service. After the connection is bootstrapped, it then binds 4 mojo pipes with the shared library.
    
    We only need to bootstrap the connection to the service when the feature is allowed for use and on the very first focus event.
    
    Bug: b:298285960
    Change-Id: I9cb31d2c27242f5f2f477c3c683c92220fd35c30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4835233
    Commit-Queue: Chuong Ho <hdchuong@google.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197671}

--- chrome/browser/ash/input_method/editor_mediator.h
+++ chrome/browser/ash/input_method/editor_mediator.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/input_method/editor_event_sink.h"
 #include "chrome/browser/ash/input_method/editor_instance_impl.h"
 #include "chrome/browser/ash/input_method/editor_panel_manager.h"
+#include "chrome/browser/ash/input_method/editor_service_connector.h"
 #include "chrome/browser/ash/input_method/editor_switch.h"
 #include "chrome/browser/ash/input_method/editor_text_actuator.h"
 #include "chrome/browser/ash/input_method/editor_text_query_provider.h"
@@ -96,6 +97,10 @@ class EditorMediator : public EditorInstanceImpl::Delegate,
  private:
   void OnTextFieldContextualInfoChanged(const TextFieldContextualInfo& info);
 
+  void SetUpNewEditorService();
+  void BindEditor();
+  void OnEditorServiceConnected(bool is_connection_bound);
+
   bool GetUserPref();
   void SetUserPref(bool value);
 
@@ -108,6 +113,7 @@ class EditorMediator : public EditorInstanceImpl::Delegate,
 
   std::unique_ptr<EditorSwitch> editor_switch_;
   std::unique_ptr<EditorConsentStore> consent_store_;
+  EditorServiceConnector editor_service_connector_;
 
   // TODO: b:298285960 - add the instantiation of this instance.
   std::unique_ptr<EditorEventProxy> editor_event_proxy_;
