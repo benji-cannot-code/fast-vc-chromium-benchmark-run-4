fcd08bbe9fdf2baceac7746b3deec4b9355ab7c3
Chris Lu <thegreenfrog@chromium.org>
2023-09-18 17:11:59 +0000

    [ios] Add placeholders to Magic Stack
    This change creates the Magic Stack and adds two placeholders if the
    Home surface is being constructed and the the Magic Stack order
    from Segmentation has not been received yet. When it is received from
    Segmentation, the placeholders will be removed and the modules will
    be added into the Magic Stack.
    
    Bug: 1483523
    Change-Id: Ic49f0b00535083a3618a14c947a5bf11856e1423
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4869622
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1197886}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_mediator.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_mediator.mm
@@ -1228,7 +1228,6 @@ bool CredentialProviderPromoDismissed(PrefService* local_state) {
   _magicStackOrderFromSegmentation = magicStackOrder;
   _latestMagicStackOrder = [self segmentationMagicStackOrder];
   [self.consumer setMagicStackOrder:_latestMagicStackOrder];
-  [self.feedDelegate contentSuggestionsWasUpdated];
 }
 
 - (void)addSetUpListToMagicStackOrder:(NSMutableArray*)order {
