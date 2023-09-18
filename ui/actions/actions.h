e2b420ff3d38353a035e9ffffcae1437a631fe19
Keren Zhu <kerenzhu@chromium.org>
2023-09-18 23:12:45 +0000

    Add invoke count and time to ActionItem
    Bug: 1472023
    Change-Id: I52a41f626e7c2de6e9d73a0707d9f8b58dad6af7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4873658
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198075}

--- ui/actions/actions.h
+++ ui/actions/actions.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/component_export.h"
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
+#include "base/time/time.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/actions/action_id.h"
 #include "ui/base/accelerators/accelerator.h"
@@ -170,6 +171,11 @@ class COMPONENT_EXPORT(ACTIONS) ActionItem : public BaseAction {
   ActionItem& operator=(const ActionItem&) = delete;
   ~ActionItem() override;
 
+  // Build an action.
+  static ActionItemBuilder Builder(InvokeActionCallback callback);
+  static ActionItemBuilder Builder();
+
+  // Configure action states and attributes.
   absl::optional<ActionId> GetActionId() const;
   void SetActionId(absl::optional<ActionId> action_id);
   ui::Accelerator GetAccelerator() const;
@@ -193,15 +199,19 @@ class COMPONENT_EXPORT(ACTIONS) ActionItem : public BaseAction {
   [[nodiscard]] base::CallbackListSubscription AddActionChangedCallback(
       ActionChangedCallback callback);
 
-  // Alternative terms used to identify this action. Used for search indexing
+  // Alternative terms used to identify this action. Used for search indexing.
   void AddSynonyms(std::initializer_list<std::u16string> synonyms);
 
+  // Do a "batch" update of the ActionItem state without triggering
+  // ActionChanged callbacks for each state change.
+  [[nodiscard]] ScopedActionUpdate BeginUpdate();
+
+  // Invoke an action.
   void InvokeAction();
 
-  static ActionItemBuilder Builder(InvokeActionCallback callback);
-  static ActionItemBuilder Builder();
-
-  [[nodiscard]] ScopedActionUpdate BeginUpdate();
+  // Get action metrics.
+  int GetInvokeCount() const;
+  absl::optional<base::TimeTicks> GetLastInvokeTime() const;
 
  protected:
   // ActionList::Delegate override.
@@ -229,6 +239,8 @@ class COMPONENT_EXPORT(ACTIONS) ActionItem : public BaseAction {
   ui::ImageModel image_;
   Synonyms synonyms_;
   InvokeActionCallback callback_;
+  int invoke_count_ = 0;
+  absl::optional<base::TimeTicks> last_invoke_time_;
 };
 
 class COMPONENT_EXPORT(ACTIONS) ActionManager
