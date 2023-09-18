ed4c774486a95a3e273d33f70bf0300efca72b17
Rushan Suleymanov <rushans@google.com>
2023-09-18 17:33:49 +0000

    [Sync] Add histogram for outgoing password invitation commit errors
    This CL adds a histogram to record datatype-specific errors for outgoing
    password sharing invtiation commits.
    
    Bug: 1468524
    Change-Id: I349033f25adbf288e017a2b14d6087ecbd04c0d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4873996
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1197896}

--- components/password_manager/core/browser/sharing/outgoing_password_sharing_invitation_sync_bridge.cc
+++ components/password_manager/core/browser/sharing/outgoing_password_sharing_invitation_sync_bridge.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/password_manager/core/browser/sharing/outgoing_password_sharing_invitation_sync_bridge.h"
 
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/uuid.h"
 #include "components/password_manager/core/browser/password_form.h"
@@ -206,8 +207,16 @@ void OutgoingPasswordSharingInvitationSyncBridge::OnCommitAttemptErrors(
        error_response_list) {
     if (error_response.response_type ==
         sync_pb::CommitResponse::INVALID_MESSAGE) {
-      // TODO(crbug.com/1468524): record UMA metric for datatype-siecific
-      // errors.
+      if (error_response.datatype_specific_error
+              .has_outgoing_password_sharing_invitation_error()) {
+        base::UmaHistogramExactLinear(
+            "Sync.OutgoingPassordSharingInvitation.CommitError",
+            error_response.datatype_specific_error
+                .outgoing_password_sharing_invitation_error()
+                .error_code(),
+            sync_pb::OutgoingPasswordSharingInvitationCommitError::
+                ErrorCode_ARRAYSIZE);
+      }
       change_processor()->UntrackEntityForClientTagHash(
           error_response.client_tag_hash);
       outgoing_invitations_in_flight_.erase(error_response.client_tag_hash);
