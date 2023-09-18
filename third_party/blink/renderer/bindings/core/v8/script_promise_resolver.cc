4bf9032357fbff4171023e7c433cff9e1a38924e
Noam Rosenthal <nrosenthal@chromium.org>
2023-09-18 11:38:32 +0000

    LoAF: Expose script URL for promise resolvers
    Capture the current script URL when constructing a
    promise resolver (e.g. calling fetch()), and read
    it when the promise resolver is invoked (e.g.
    the response is being processed).
    
    Confirmed with the V8 team that this should (in theory)
    have acceptable overhead, as GetCurrentScriptUrl() is
    called only in places that are "heavy" to begin with,
    like invoking a new fetch or calling a platform API.
    
    Change-Id: I2c81a3694849bd73f0986f19f3de752093f6d7b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4802170
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197775}

--- third_party/blink/renderer/bindings/core/v8/script_promise_resolver.cc
+++ third_party/blink/renderer/bindings/core/v8/script_promise_resolver.cc
@@ -58,6 +58,7 @@ ScriptPromiseResolver::ScriptPromiseResolver(
     state_ = kDetached;
     resolver_.Clear();
   }
+  script_url_ = GetCurrentScriptUrl(script_state->GetIsolate());
 }
 
 ScriptPromiseResolver::~ScriptPromiseResolver() = default;
@@ -147,7 +148,7 @@ void ScriptPromiseResolver::ResolveOrRejectImmediately() {
 
   probe::WillHandlePromise(GetExecutionContext(), script_state_,
                            state_ == kResolving, class_like_name_,
-                           property_like_name_);
+                           property_like_name_, script_url_);
   {
     if (state_ == kResolving) {
       resolver_.Resolve(value_.Get(script_state_->GetIsolate()));
