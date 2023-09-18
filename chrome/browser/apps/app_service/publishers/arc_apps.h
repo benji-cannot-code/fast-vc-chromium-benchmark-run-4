6fa15b8725134dec71655ac2a658839d374a1a0d
Vivian Pao <vpao@google.com>
2023-09-18 03:25:46 +0000

    [Promise Icons] Remove ARC promise apps that failed to install
    When an ARC installation fails, remove the corresponding promise app
    tied to that installation from the PromiseAppRegistryCache.
    
    Bug: b/261907409
    Change-Id: I7eb623c7c912d23df1ec221e27ff6f710518ef5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4865413
    Commit-Queue: Vivian Pao <vpao@google.com>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197694}

--- chrome/browser/apps/app_service/publishers/arc_apps.h
+++ chrome/browser/apps/app_service/publishers/arc_apps.h
@@ -158,6 +158,8 @@ class ArcApps : public KeyedService,
                                      float progress) override;
   void OnInstallationActiveChanged(const std::string& package_name,
                                    bool active) override;
+  void OnInstallationFinished(const std::string& package_name,
+                              bool success) override;
 
   // arc::ArcIntentHelperObserver overrides.
   void OnIntentFiltersUpdated(
