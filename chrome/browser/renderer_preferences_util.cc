32f97dd6b13a59f253abeab0a652d6a9efd78bbe
Fiona Macintosh <fmacintosh@google.com>
2023-09-18 12:55:49 +0000

    Replace DNT pref readers/observers with TrackingProtectionSettings API
    Bug: b:300494177
    Change-Id: I2c6a3293a691ce94ec5c6306a7475ed6c40f7b8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4864331
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197785}

--- chrome/browser/renderer_preferences_util.cc
+++ chrome/browser/renderer_preferences_util.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/net/convert_explicitly_allowed_network_ports_pref.h"
+#include "chrome/browser/privacy_sandbox/tracking_protection_settings_factory.h"
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "chrome/browser/ash/login/demo_mode/demo_session.h"
 #endif
@@ -24,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/language/core/browser/language_prefs.h"
 #include "components/language/core/browser/pref_names.h"
 #include "components/prefs/pref_service.h"
-#include "components/privacy_sandbox/tracking_protection_prefs.h"
+#include "components/privacy_sandbox/tracking_protection_settings.h"
 #include "content/public/browser/browser_accessibility_state.h"
 #include "content/public/browser/renderer_preferences_util.h"
 #include "media/media_buildflags.h"
@@ -111,7 +112,8 @@ void UpdateFromSystemSettings(blink::RendererPreferences* prefs,
       profile, pref_service->GetString(language::prefs::kAcceptLanguages));
   prefs->enable_referrers = pref_service->GetBoolean(prefs::kEnableReferrers);
   prefs->enable_do_not_track =
-      pref_service->GetBoolean(prefs::kEnableDoNotTrack);
+      TrackingProtectionSettingsFactory::GetForProfile(profile)
+          ->IsDoNotTrackEnabled();
   prefs->enable_encrypted_media =
       pref_service->GetBoolean(prefs::kEnableEncryptedMedia);
   prefs->webrtc_ip_handling_policy = std::string();
--- chrome/browser/ui/prefs/pref_watcher.h
+++ chrome/browser/ui/prefs/pref_watcher.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_keyed_service_factory.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/prefs/pref_change_registrar.h"
+#include "components/privacy_sandbox/tracking_protection_settings.h"
+#include "components/privacy_sandbox/tracking_protection_settings_observer.h"
 #include "mojo/public/cpp/bindings/remote_set.h"
 #include "third_party/blink/public/mojom/renderer_preference_watcher.mojom.h"
 
@@ -19,7 +21,8 @@ class PrefsTabHelper;
 
 // Watches updates in WebKitPreferences and blink::RendererPreferences, and
 // notifies tab helpers and registered watchers of those updates.
-class PrefWatcher : public KeyedService {
+class PrefWatcher : public KeyedService,
+                    public privacy_sandbox::TrackingProtectionSettingsObserver {
  public:
   explicit PrefWatcher(Profile* profile);
   ~PrefWatcher() override;
@@ -35,6 +38,8 @@ class PrefWatcher : public KeyedService {
   // KeyedService overrides:
   void Shutdown() override;
 
+  void OnDoNotTrackEnabledChanged() override;
+
   void UpdateRendererPreferences();
   void OnWebPrefChanged(const std::string& pref_name);
   void OnLiveCaptionEnabledPrefChanged(const std::string& pref_name);
@@ -42,6 +47,12 @@ class PrefWatcher : public KeyedService {
   raw_ptr<Profile> profile_;
   PrefChangeRegistrar profile_pref_change_registrar_;
   PrefChangeRegistrar local_state_pref_change_registrar_;
+  raw_ptr<privacy_sandbox::TrackingProtectionSettings>
+      tracking_protection_settings_;
+
+  base::ScopedObservation<privacy_sandbox::TrackingProtectionSettings,
+                          privacy_sandbox::TrackingProtectionSettingsObserver>
+      tracking_protection_settings_observation_{this};
 
   // |tab_helpers_| observe changes in WebKitPreferences and
   // blink::RendererPreferences.
