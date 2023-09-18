fa375b994db7fa21c9dba898f6795dbffd2133bb
Jan Keitel <jkeitel@google.com>
2023-09-18 11:48:37 +0000

    Support visibility changes in FormDataAndroid.
    This CL adds support for propagating visibility changes from C++ to
    Java.
    
    Bug: 1482048
    Change-Id: I81fde29bab0536615d5bf7cc0c82d574cc9fcd2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4863429
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197777}

--- components/android_autofill/browser/mock_form_field_data_android_bridge.h
+++ components/android_autofill/browser/mock_form_field_data_android_bridge.h
@@ -30,6 +30,7 @@ class MockFormFieldDataAndroidBridge : public FormFieldDataAndroidBridge {
               (const FormFieldDataAndroid::FieldTypes&),
               (override));
   MOCK_METHOD(void, UpdateValue, (std::u16string_view), (override));
+  MOCK_METHOD(void, UpdateVisible, (bool), (override));
 };
 
 }  // namespace autofill
