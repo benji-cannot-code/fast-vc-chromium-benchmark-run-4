79f35273885046b50b4843f16d4133b055fc2668
Maggie Cai <mxcai@google.com>
2023-09-18 13:25:47 +0000

    [Shortcut] Create a shortcut removal dialog.
    BUG=1412708
    
    Change-Id: I18a5f43a86fbea428790dc421419bab83ac6d628
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4867813
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197794}

--- chrome/browser/ui/ash/shelf/app_service/app_service_shortcut_shelf_item_controller_browsertest.cc
+++ chrome/browser/ui/ash/shelf/app_service/app_service_shortcut_shelf_item_controller_browsertest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/shelf/chrome_shelf_controller.h"
 #include "chrome/browser/ui/ash/shelf/chrome_shelf_controller_util.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/views/apps/app_dialog/shortcut_removal_dialog_view.h"
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/grit/generated_resources.h"
@@ -41,11 +42,11 @@ class AppServiceShortcutShelfItemControllerBrowserTest
   void SetUpOnMainThread() override { ASSERT_TRUE(controller()); }
 
   apps::ShortcutId CreateWebAppBasedShortcut(
-      const GURL& app_url,
+      const GURL& shortcut_url,
       const std::u16string& shortcut_name) {
     // Create web app based shortcut.
     auto web_app_info = std::make_unique<web_app::WebAppInstallInfo>();
-    web_app_info->start_url = app_url;
+    web_app_info->start_url = shortcut_url;
     web_app_info->title = shortcut_name;
     auto local_shortcut_id = web_app::test::InstallWebApp(
         browser()->profile(), std::move(web_app_info));
@@ -201,6 +202,10 @@ IN_PROC_BROWSER_TEST_F(AppServiceShortcutShelfItemControllerBrowserTest,
                 .vector_icon());
 
   menu_model->ActivatedAt(uninstall_command_index.value());
+  ShortcutRemovalDialogView* last_created_dialog =
+      ShortcutRemovalDialogView::GetLastCreatedViewForTesting();
+  last_created_dialog->AcceptDialog();
+
   base::RunLoop().RunUntilIdle();
   content::RunAllTasksUntilIdle();
 
