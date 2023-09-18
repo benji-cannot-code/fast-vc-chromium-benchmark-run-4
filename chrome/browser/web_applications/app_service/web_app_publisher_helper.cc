f6488678a5a6b1debf370953df325c0cb1834db5
Tim Sergeant <tsergeant@chromium.org>
2023-09-18 07:39:39 +0000

    [Default Apps] Add workaround to show an OEM app outside the OEM folder
    We are planning on preloading a partner app through the App Preload
    System, which can currently only preload apps as "OEM installs", which
    appear in the OEM folder.
    
    This CL adds a workaround to make this one particular app appear outside
    of the OEM folder, by changing how it is published to App Service.
    
    As a follow-up, over the next milestone or two, we will add the
    capability for APS to preload non-OEM apps, and migrate this app away
    from this workaround.
    
    Bug: b/300529104
    Change-Id: If08ee9f347c49bfa31fe4bf4cf12c0f71f159f0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4868113
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197715}

--- chrome/browser/web_applications/app_service/web_app_publisher_helper.cc
+++ chrome/browser/web_applications/app_service/web_app_publisher_helper.cc
@@ -66,6 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_command_scheduler.h"
 #include "chrome/browser/web_applications/web_app_constants.h"
 #include "chrome/browser/web_applications/web_app_helpers.h"
+#include "chrome/browser/web_applications/web_app_id_constants.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_registrar.h"
 #include "chrome/browser/web_applications/web_app_sync_bridge.h"
@@ -213,6 +214,14 @@ apps::InstallReason GetHighestPriorityInstallReason(const WebApp* web_app) {
       DCHECK(!web_app->IsSystemApp());
       return apps::InstallReason::kOem;
     }
+  }
+
+  // On some devices, this partner app is installed through App Preload Service
+  // as an OEM app, but should not appear in the OEM folder.
+  // TODO(b/300857328): Remove this workaround.
+  if (web_app->GetHighestPrioritySource() == WebAppManagement::kOem &&
+      web_app->app_id() == web_app::kDefaultInstalledPartnerAppId) {
+    return apps::InstallReason::kDefault;
   }
 
   switch (web_app->GetHighestPrioritySource()) {
