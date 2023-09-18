a247a923309e8de76599a4a8f6d959fd187c2bb2
Bruno Pitrus <brunopitrus@hotmail.com>
2023-09-18 23:41:48 +0000

    IWYU: add missing header for absl::WrapUnique
    Change-Id: I6006f25937b1257e70c539d5b229b7d9622cc480
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4794170
    Reviewed-by: Ken Rockot <rockot@google.com>
    Auto-Submit: Bruno Pitrus <brunopitrus@hotmail.com>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1198087}

--- services/metrics/public/cpp/mojo_ukm_recorder.cc
+++ services/metrics/public/cpp/mojo_ukm_recorder.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "services/metrics/public/mojom/ukm_interface.mojom-forward.h"
 #include "services/metrics/public/mojom/ukm_interface.mojom.h"
+#include "third_party/abseil-cpp/absl/memory/memory.h"
 
 namespace ukm {
 
