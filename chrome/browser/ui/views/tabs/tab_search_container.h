bd47821fe26258bf90d73eccd26a98dc431d6a12
Emily Shack <emshack@chromium.org>
2023-09-18 23:13:13 +0000

    Tab Organization: Show/Hide TabOrganizationButton
    Adds logic to show and hide the button, including animation of the
    button width and tab search button corner radius.
    
    Bug: 1469126
    Change-Id: I7f5fa3213725d3def9d3031527a0f39c36172045
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4868683
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198076}

--- chrome/browser/ui/views/tabs/tab_search_container.h
+++ chrome/browser/ui/views/tabs/tab_search_container.h
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/gfx/animation/animation.h"
+#include "ui/gfx/animation/slide_animation.h"
+#include "ui/views/animation/animation_delegate_views.h"
 #include "ui/views/view.h"
 
 enum class Edge;
@@ -15,7 +18,8 @@ class TabOrganizationButton;
 class TabSearchButton;
 class TabStrip;
 
-class TabSearchContainer : public views::View {
+class TabSearchContainer : public views::View,
+                           public views::AnimationDelegateViews {
  public:
   METADATA_HEADER(TabSearchContainer);
   TabSearchContainer(TabStrip* tab_strip, bool before_tab_strip);
@@ -28,10 +32,26 @@ class TabSearchContainer : public views::View {
   }
   TabSearchButton* tab_search_button() { return tab_search_button_; }
 
+  gfx::SlideAnimation* expansion_animation_for_testing() {
+    return &expansion_animation_;
+  }
+
+  void ShowTabOrganization();
+  void HideTabOrganization();
+
+  // views::AnimationDelegateViews
+  void AnimationCanceled(const gfx::Animation* animation) override;
+  void AnimationEnded(const gfx::Animation* animation) override;
+  void AnimationProgressed(const gfx::Animation* animation) override;
+
  private:
+  void ApplyAnimationValue(float value);
+
   raw_ptr<TabOrganizationButton, DanglingUntriaged> tab_organization_button_ =
       nullptr;
   raw_ptr<TabSearchButton, DanglingUntriaged> tab_search_button_ = nullptr;
+  // Animation controlling expansion and collapse of tab_organization_button_.
+  gfx::SlideAnimation expansion_animation_{this};
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_TABS_TAB_SEARCH_CONTAINER_H_
