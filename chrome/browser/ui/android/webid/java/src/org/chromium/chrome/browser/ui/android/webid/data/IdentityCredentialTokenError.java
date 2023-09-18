c3aee3b31aa5e5b498708dff3153078060177e89
Zachary Tan <tanzachary@chromium.org>
2023-09-18 19:25:11 +0000

    [FedCM] Implement generic error dialog on android
    In additional patches, we will add a "got it" button to acknowledge the
    dialog and a "more details" button which opens a CCT to the given error
    URL.
    
    Screenshot
    https://bugs.chromium.org/p/chromium/issues/detail?id=1420642#c20
    
    Low-Coverage-Reason: Tests would not be useful for the offending file
    Bug: 1420642
    Change-Id: Ied867b7c219eb3b283b8f1d5469cef1cc07f271a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4850869
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Zachary Tan <tanzachary@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197949}

---
+++ chrome/browser/ui/android/webid/java/src/org/chromium/chrome/browser/ui/android/webid/data/IdentityCredentialTokenError.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ui.android.webid.data;
+
+import org.chromium.base.annotations.CalledByNative;
+import org.chromium.url.GURL;
+
+/**
+ * Holds data used to represent an error for display in the "error" dialog.
+ */
+public class IdentityCredentialTokenError {
+    private final String mCode;
+    private final GURL mUrl;
+
+    @CalledByNative
+    public IdentityCredentialTokenError(String code, GURL url) {
+        mCode = code;
+        mUrl = url;
+    }
+
+    public String getCode() {
+        return mCode;
+    }
+
+    public GURL getUrl() {
+        return mUrl;
+    }
+}
