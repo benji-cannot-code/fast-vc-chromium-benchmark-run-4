bf7502f37d0cef9e5daef94a1c55ece3a04604b8
Ernesto Izquierdo Clua <eic@google.com>
2023-09-18 14:12:18 +0000

    [ios] Request expiration date as for expired cards in CVC prompt
    This patch fixes a bug in the Card Verification Code prompt for
    autofill. When the user wants to autofill a credit card already saved
    in their profile, the CVC prompt is displayed. If the card is already
    expired, they need to enter the new CVC and expiration date through
    the prompt. The bug was about not requesting the expiration date right
    away, so users would enter a CVC, the verification would fail and then
    the expiration date would be required. The patch checks for expired
    cards when the CVC prompt is presented and requires expiration date
    accordingly. A unit test verifies the state of the prompt for expired
    cards.
    
    A minor refactoring was also needed to focus on the right field( expiration date or cvc ) after each UI transition.
    
    Bug: 1472722
    Change-Id: I2f820ac36c503b503dc965d478e4ac253463dc42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4864459
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1197814}

--- ios/chrome/browser/ui/autofill/card_unmask_prompt_view_controller.mm
+++ ios/chrome/browser/ui/autofill/card_unmask_prompt_view_controller.mm
@@ -41,6 +41,13 @@ typedef NS_ENUM(NSInteger, ItemType) {
   ItemTypeExpirationDateInput,
 };
 
+// Represents the next first responder after a UI transition.
+enum class ItemToFocus {
+  kNone,
+  kCVC,
+  kExpirationDate,
+};
+
 // Empty space on top of the input section. This value added up to the gPay
 // badge bottom padding achieves the mock's vertical spacing between the gPay
 // badge in the header.
@@ -74,12 +81,9 @@ const char kFooterDummyLinkTarget[] = "about:blank";
   CVCHeaderItem* _headerItem;
   // Model of the expiration date input cell.
   ExpirationDateEditItem* _expirationDateInputItem;
-  // Flag indicating that we should set the focus in either the CVC or
-  // expiration date fields once the tableView is reloaded. After the view shows
-  // the CVC form or the expiration form, we want to focus the CVC and
-  // expiration date fields respectively. When transitioning from one form to
-  // the other, we activate this flag so the focus is updated.
-  BOOL _shouldUpdateFocus;
+  // Whether we should set the focus on the CVC or
+  // expiration date fields once the tableView is reloaded.
+  ItemToFocus _itemToFocus;
 }
 
 @end
@@ -92,7 +96,7 @@ const char kFooterDummyLinkTarget[] = "about:blank";
   if (self) {
     _bridge = bridge;
     // Focus CVC field after initial load.
-    _shouldUpdateFocus = YES;
+    _itemToFocus = ItemToFocus::kCVC;
   }
 
   return self;
@@ -162,6 +166,10 @@ const char kFooterDummyLinkTarget[] = "about:blank";
   _CVCInputItem = [self createCVCInputItem];
   [self.tableViewModel addItem:_CVCInputItem
        toSectionWithIdentifier:SectionIdentifierInputs];
+
+  if (_bridge->GetController()->ShouldRequestExpirationDate()) {
+    [self addExpirationDateInputItem];
+  }
 }
 
 #pragma mark - Public
@@ -280,7 +288,7 @@ const char kFooterDummyLinkTarget[] = "about:blank";
   [self removeFooterItem];
 
   // Change focus to expiration date field once the cells are loaded.
-  _shouldUpdateFocus = YES;
+  _itemToFocus = ItemToFocus::kExpirationDate;
 
   [self reloadAllSections];
 
@@ -310,7 +318,7 @@ const char kFooterDummyLinkTarget[] = "about:blank";
         forSectionWithIdentifier:SectionIdentifierInputs];
 
   // Restore focus to CVC input field after the section is reloaded.
-  _shouldUpdateFocus = YES;
+  _itemToFocus = ItemToFocus::kCVC;
 
   // Reload inputs section to display footer.
   NSIndexSet* indexSet = [[NSIndexSet alloc]
@@ -555,7 +563,8 @@ const char kFooterDummyLinkTarget[] = "about:blank";
   // needed.
   // Don't update focus when Voice Over is running. Instead, the instructions
   // will be read, providing more context for users with Voice Over enabled.
-  if (UIAccessibilityIsVoiceOverRunning() || !_shouldUpdateFocus ||
+  if (UIAccessibilityIsVoiceOverRunning() ||
+      _itemToFocus == ItemToFocus::kNone ||
       ![cell isKindOfClass:TableViewTextEditCell.class]) {
     return;
   }
@@ -566,13 +575,12 @@ const char kFooterDummyLinkTarget[] = "about:blank";
 
   ItemType rowItemType = static_cast<ItemType>(
       [self.tableViewModel itemTypeForIndexPath:indexPath]);
-  // If the expiration date cell is displayed then we're transitioning to the
-  // expiration date form. Only focus the CVC cell when we're not transitioning
-  // to the expiration date form.
-  if (rowItemType == ItemTypeExpirationDateInput ||
-      (rowItemType == ItemTypeCVCInput && !_expirationDateInputItem)) {
+
+  if ((rowItemType == ItemTypeExpirationDateInput &&
+       _itemToFocus == ItemToFocus::kExpirationDate) ||
+      (rowItemType == ItemTypeCVCInput && _itemToFocus == ItemToFocus::kCVC)) {
     [rowCell.textField becomeFirstResponder];
-    _shouldUpdateFocus = NO;
+    _itemToFocus = ItemToFocus::kNone;
   }
 }
 
