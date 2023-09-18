eaf5f7ce276c375c05c6ce1d6ab16a864248a749
Courtney Wong <courtneywong@chromium.org>
2023-09-18 22:54:58 +0000

    [Families TS Migration] Migrate ParentAccess JS tests to TS
    Also increase the resource size for webui tests. Adding these tests
    caused the previous limit of 1400 to be exceeded.
    
    Tested: Ran the browser tests
    
    Bug: b:293655835
    Change-Id: I5a051766f05a331b60acb13bb66c1538b6335972
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4850609
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198059}

--- ash/webui/common/resources/post_message_api/post_message_api_client.js
+++ ash/webui/common/resources/post_message_api/post_message_api_client.js
@@ -43,7 +43,7 @@ export class PostMessageApiClient {
   /**
    * @param {!string} serverOriginURLFilter  Only messages from this origin
    *     will be accepted.
-   * @param {Window} targetWindow, If the connection is already established,
+   * @param {?Window} targetWindow, If the connection is already established,
    *     then provide the target window.
    */
   constructor(serverOriginURLFilter, targetWindow) {
@@ -66,7 +66,7 @@ export class PostMessageApiClient {
 
     /**
      * The parent window.
-     * @private {Window}
+     * @private {?Window}
      */
     this.targetWindow_ = targetWindow;
 
