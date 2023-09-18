1cb8d3f2fd2ea048b95ac63d60c001eb5789f82d
Angela Xiao <angelaxiao@chromium.org>
2023-09-18 20:18:01 +0000

    Scalable Notifiers: remove flags from chrome://flags
    Remove all the Scalable IPH feature flags used in the teamfood config
    from chrome://flags. Since dogfooding will be enabled through Finch,
    these flags shouldn't be manually changed.
    
    Bug: b/300161322
    Change-Id: I0778191450da6a620ec0cdff2f21b75d2a4722a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4869247
    Reviewed-by: Yuki Awano <yawano@google.com>
    Commit-Queue: Angela Xiao <angelaxiao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197981}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3694,9 +3694,6 @@ extern const char kHelpAppAutoTriggerInstallDialogDescription[];
 extern const char kHelpAppLauncherSearchName[];
 extern const char kHelpAppLauncherSearchDescription[];
 
-extern const char kHelpAppWelcomeTipsName[];
-extern const char kHelpAppWelcomeTipsDescription[];
-
 extern const char kDiacriticsOnPhysicalKeyboardLongpressName[];
 extern const char kDiacriticsOnPhysicalKeyboardLongpressDescription[];
 
@@ -3902,9 +3899,6 @@ extern const char kArcInputOverlayDescriptionBeta[];
 
 extern const char kArcInputOverlayNameAlphaV2[];
 extern const char kArcInputOverlayDescriptionAlphaV2[];
-
-extern const char kScalableIphName[];
-extern const char kScalableIphDescription[];
 
 extern const char kScalableIphDebugName[];
 extern const char kScalableIphDebugDescription[];
