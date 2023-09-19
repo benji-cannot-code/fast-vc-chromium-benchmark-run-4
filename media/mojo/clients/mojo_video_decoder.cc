597a222f3b1a7a17dd81a1fb500172204c01615c
Dale Curtis <dalecurtis@chromium.org>
2023-09-19 02:19:28 +0000

    Always attempt video decoder init if we don't have fallback.
    For codecs such as H264, H265 we may not have software fallback
    depending on the platform. As such even if OS metadata suggests
    the playback will fail, try it anyways because it certainly will
    otherwise.
    
    This is useful since the OS metadata doesn't always appear accurate
    on Android and may be too expensive to compute accurately on other
    platforms (e.g., requires testing resolutions one at a time).
    
    Fixed: 1473863
    Change-Id: I7153419a85805433ac541bb6fa1037ad93d1789e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4852628
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198159}

--- media/mojo/clients/mojo_video_decoder.cc
+++ media/mojo/clients/mojo_video_decoder.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/demuxer_stream.h"
 #include "media/base/media_switches.h"
 #include "media/base/overlay_info.h"
+#include "media/base/supported_types.h"
 #include "media/base/video_frame.h"
 #include "media/media_buildflags.h"
 #include "media/mojo/clients/mojo_media_log_service.h"
@@ -155,9 +156,12 @@ void MojoVideoDecoder::Initialize(const VideoDecoderConfig& config,
   if (gpu_factories_)
     decoder_type_ = gpu_factories_->GetDecoderType();
 
-  // Fail immediately if we know that the remote side cannot support |config|.
-  if (gpu_factories_ && gpu_factories_->IsDecoderConfigSupported(config) ==
-                            GpuVideoAcceleratorFactories::Supported::kFalse) {
+  // If the codec has software fallback, fail immediately if we know that the
+  // remote side cannot support |config|.
+  if (gpu_factories_ &&
+      gpu_factories_->IsDecoderConfigSupported(config) ==
+          GpuVideoAcceleratorFactories::Supported::kFalse &&
+      IsBuiltInVideoCodec(config.codec())) {
     FailInit(std::move(init_cb), DecoderStatus::Codes::kUnsupportedConfig);
     return;
   }
