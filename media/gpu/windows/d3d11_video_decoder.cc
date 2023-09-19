b34762d648d25ade87b99687b4fd2888df04c3a8
Titouan Rigoudy <titouan@chromium.org>
2023-09-19 11:54:03 +0000

    Revert "Always attempt video decoder init if we don't have fallback."
    This reverts commit f5be305cce08118a4983ac2fa7441ff30eca8aad.
    
    Reason for revert: breaks tests on win-asan bot.
    
    Bug: chromium:1484552
    
    Original change's description:
    > Always attempt video decoder init if we don't have fallback.
    >
    > For codecs such as H264, H265 we may not have software fallback
    > depending on the platform. As such even if OS metadata suggests
    > the playback will fail, try it anyways because it certainly will
    > otherwise.
    >
    > This is useful since the OS metadata doesn't always appear accurate
    > on Android and may be too expensive to compute accurately on other
    > platforms (e.g., requires testing resolutions one at a time).
    >
    > Fixed: 1473863
    > Change-Id: I7153419a85805433ac541bb6fa1037ad93d1789e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4852628
    > Reviewed-by: Dan Sanders <sandersd@chromium.org>
    > Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1198159}
    
    Change-Id: I2686bc00096369855b04144212d4c29dd2cc947f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4875541
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Titouan Rigoudy <titouan@chromium.org>
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Owners-Override: Titouan Rigoudy <titouan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198307}

--- media/gpu/windows/d3d11_video_decoder.cc
+++ media/gpu/windows/d3d11_video_decoder.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/decoder_buffer.h"
 #include "media/base/media_log.h"
 #include "media/base/media_switches.h"
-#include "media/base/supported_types.h"
 #include "media/base/video_aspect_ratio.h"
 #include "media/base/video_codecs.h"
 #include "media/base/video_decoder_config.h"
@@ -362,9 +361,7 @@ void D3D11VideoDecoder::Initialize(const VideoDecoderConfig& config,
     }
   }
 
-  // If we don't have support support for a given codec, try to initialize
-  // anyways -- otherwise we're certain to fail playback.
-  if (!is_supported && IsBuiltInVideoCodec(config.codec())) {
+  if (!is_supported) {
     return PostDecoderStatus(
         DecoderStatus(DecoderStatus::Codes::kUnsupportedConfig)
             .WithData("config", config));
