b8f96ab441e33f58b36eb2a8b0bafb5c53958771
Leonid Baraz <lbaraz@chromium.org>
2023-09-19 18:10:58 +0000

    enterprise_reporting switch to Mojo
    Redefine browser<->page communications to use mojo in
    chrome://enterprise-reporting.
    
    Bug: b/214041435
    Change-Id: I49587f13aa306021f1490b622e98d1a74dc31564
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4862634
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Reviewed-by: Josh Hilke <jrhilke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1198485}

---
+++ chrome/browser/ui/webui/ash/enterprise_reporting/history_converter.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_WEBUI_ASH_ENTERPRISE_REPORTING_HISTORY_CONVERTER_H_
+#define CHROME_BROWSER_UI_WEBUI_ASH_ENTERPRISE_REPORTING_HISTORY_CONVERTER_H_
+
+#include "chrome/browser/ui/webui/ash/enterprise_reporting/enterprise_reporting.mojom.h"
+#include "chromeos/dbus/missive/history_tracker.h"
+#include "components/reporting/proto/synced/health.pb.h"
+#include "components/reporting/proto/synced/status.pb.h"
+
+namespace ash::reporting {
+
+mojo::StructPtr<enterprise_reporting::mojom::ErpHistoryData> ConvertHistory(
+    const ::reporting::ERPHealthData& data);
+
+}  // namespace ash::reporting
+
+#endif  // CHROME_BROWSER_UI_WEBUI_ASH_ENTERPRISE_REPORTING_HISTORY_CONVERTER_H_
