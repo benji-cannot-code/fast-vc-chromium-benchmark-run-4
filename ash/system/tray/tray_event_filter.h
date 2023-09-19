c37a6776fe56edb3f58da5456708b44324bc248a
Andre Le <leandre@chromium.org>
2023-09-19 17:57:34 +0000

    Bubble Refactor: Use a base class for bubble event filter
    Create BubbleEventFilter that is used in TrayEventFilter and
    AppListBubbleEventFilter. This class should take care of all the basic
    functionalities of event filter, and the subclasses just do the
    customization for each type of bubble. This design rationale is
    discussed in go/cros-tray-bubble-refactor.
    
    Also move the tests from AppListBubbleEventFilterTest to
    BubbleEventFilterTest, and add a test for the edge case in the app list
    test suite. Clean up to TrayEventFilterTest will happen in the next CL.
    
    Bug: b:300142793
    Change-Id: Ibfbc10b97f532fbeef93ae434379740c9e4495f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4866733
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198478}

--- ash/system/tray/tray_event_filter.h
+++ ash/system/tray/tray_event_filter.h
@@ -7,8 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_SYSTEM_TRAY_TRAY_EVENT_FILTER_H_
 
 #include "ash/ash_export.h"
+#include "ash/bubble/bubble_event_filter.h"
 #include "base/memory/raw_ptr.h"
-#include "ui/events/event_handler.h"
+#include "base/memory/weak_ptr.h"
 
 namespace ui {
 class LocatedEvent;
@@ -23,9 +24,9 @@ namespace ash {
 class TrayBackgroundView;
 class TrayBubbleView;
 
-// Handles events for a tray bubble, e.g. to close the system tray bubble when
-// the user clicks outside it.
-class ASH_EXPORT TrayEventFilter : public ui::EventHandler {
+// Handles events for tray bubbles, closing the system tray bubble when the user
+// clicks outside of it.
+class ASH_EXPORT TrayEventFilter : public BubbleEventFilter {
  public:
   TrayEventFilter(views::Widget* bubble_widget,
                   TrayBubbleView* bubble_view,
@@ -36,21 +37,16 @@ class ASH_EXPORT TrayEventFilter : public ui::EventHandler {
 
   ~TrayEventFilter() override;
 
-  // ui::EventHandler:
-  void OnMouseEvent(ui::MouseEvent* event) override;
-  void OnTouchEvent(ui::TouchEvent* event) override;
+  // BubbleEventFilter:
   void OnGestureEvent(ui::GestureEvent* event) override;
+  bool ShouldRunOnClickOutsideCallback(const ui::LocatedEvent& event) override;
 
  private:
-  // Evaluates whether the given `event` should cause the tray button to trigger
-  // `ClickedOutsideBubble()`.
-  bool ShouldTriggerClickedOutsideBubble(const ui::LocatedEvent& event);
-
-  void ProcessPressedEvent(const ui::LocatedEvent& event);
-
   const raw_ptr<views::Widget> bubble_widget_;
   const raw_ptr<TrayBubbleView> bubble_view_;
   const raw_ptr<TrayBackgroundView> tray_button_;
+
+  base::WeakPtrFactory<TrayEventFilter> weak_factory_{this};
 };
 
 }  // namespace ash
