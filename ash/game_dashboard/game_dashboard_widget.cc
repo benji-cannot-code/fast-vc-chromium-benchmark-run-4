43d65366b6fe5d773b6665b9e3aa20c972e52b0c
Prameet Shah <phshah@google.com>
2023-09-19 01:09:57 +0000

    gd: Enable dynamic color support for GameDashboardWidgets
    Bug: b:300999567
    Test: Manual and ash unit tests
    Change-Id: Ie42d9a71cdabdc9e24baac32954c757f538d8512
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4873843
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Prameet Shah <phshah@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198127}

--- ash/game_dashboard/game_dashboard_widget.cc
+++ ash/game_dashboard/game_dashboard_widget.cc
@@ -5,15 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/game_dashboard/game_dashboard_widget.h"
 
-#include "third_party/skia/include/core/SkColor.h"
 #include "ui/color/color_provider_key.h"
-
-namespace {
-
-// The base color for all `GameDashboardWidget`s.
-constexpr SkColor kWindlightSeedColor = SkColorSetRGB(0x3F, 0x5A, 0xA9);
-
-}  // namespace
 
 namespace ash {
 
@@ -24,8 +16,6 @@ GameDashboardWidget::~GameDashboardWidget() = default;
 ui::ColorProviderKey GameDashboardWidget::GetColorProviderKey() const {
   auto key = Widget::GetColorProviderKey();
   key.color_mode = ui::ColorProviderKey::ColorMode::kDark;
-  key.scheme_variant = ui::ColorProviderKey::SchemeVariant::kTonalSpot;
-  key.user_color = kWindlightSeedColor;
   return key;
 }
 
