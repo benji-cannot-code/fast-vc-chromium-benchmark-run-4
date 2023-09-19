36d14e95c19cee7a20b8a3b1f3eb4880cfe5f72f
Ernesto Izquierdo Clua <eic@google.com>
2023-09-19 17:14:32 +0000

    [ios][auth-on-entry2] Animate Password Issues when auth on entry disabled
    This patch fixes a bug where Password Issues was not animated when
    presented even when the Auth On Entry feature was disabled.
    
    Change-Id: I5227303c3c0657dc5e973e229374206db701f4cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4874959
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1198451}

--- ios/chrome/browser/ui/settings/password/password_issues/password_issues_coordinator.mm
+++ ios/chrome/browser/ui/settings/password/password_issues/password_issues_coordinator.mm
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ui/base/l10n/l10n_util.h"
 
 using password_manager::WarningType;
+using password_manager::features::IsAuthOnEntryV2Enabled;
 
 namespace {
 
@@ -145,8 +146,10 @@ DetailsContext ComputeDetailsContextFromWarningType(WarningType warning_type) {
 
   // Disable animation when content will be blocked for reauth to prevent
   // flickering in navigation bar.
-  [self.baseNavigationController pushViewController:self.viewController
-                                           animated:_skipAuthenticationOnStart];
+  [self.baseNavigationController
+      pushViewController:self.viewController
+                animated:_skipAuthenticationOnStart ||
+                         !IsAuthOnEntryV2Enabled()];
 
   [self startReauthCoordinatorWithAuthOnStart:!_skipAuthenticationOnStart];
 }
@@ -295,7 +298,7 @@ DetailsContext ComputeDetailsContextFromWarningType(WarningType warning_type) {
 // scene is backgrounded and foregrounded until reauthCoordinator is stopped.
 - (void)startReauthCoordinatorWithAuthOnStart:(BOOL)authOnStart {
   // No-op if Auth on Entry is not enabled for the password manager.
-  if (!password_manager::features::IsAuthOnEntryV2Enabled()) {
+  if (!IsAuthOnEntryV2Enabled()) {
     return;
   }
 
@@ -330,7 +333,7 @@ DetailsContext ComputeDetailsContextFromWarningType(WarningType warning_type) {
 - (void)restartReauthCoordinator {
   // Restart reauth coordinator so it monitors scene state changes and requests
   // local authentication after the scene goes to the background.
-  if (password_manager::features::IsAuthOnEntryV2Enabled()) {
+  if (IsAuthOnEntryV2Enabled()) {
     [self startReauthCoordinatorWithAuthOnStart:NO];
   }
 }
