1cbfd4926e2ce02384826460e2af3aad126c2635
Maks Orlovich <morlovich@chromium.org>
2023-09-19 13:26:08 +0000

    FLEDGE: Share JSON DataDecoder for per-seller data.
    ... Rather than starting a new process for every single thing.
    
    Change-Id: I2f18f740ad5ca95bbea41bf62009a22c7043cdc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4846949
    Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198339}

--- content/browser/interest_group/interest_group_auction.cc
+++ content/browser/interest_group/interest_group_auction.cc
@@ -2576,6 +2576,8 @@ void InterestGroupAuction::NotifyDirectFromSellerSignalsHeaderAdSlotConfig(
   }
   direct_from_seller_signals_header_ad_slot_pending_ = true;
   HeaderDirectFromSellerSignals::ParseAndFind(
+      base::BindRepeating(&InterestGroupAuction::GetDataDecoder,
+                          weak_ptr_factory_.GetWeakPtr()),
       auction_page_data->GetAuctionSignalsForOrigin(config_->seller),
       *direct_from_seller_signals_header_ad_slot,
       base::BindOnce(
@@ -3562,7 +3564,7 @@ void InterestGroupAuction::DecodeAdditionalBidsIfReady() {
       continue;
     }
     ++num_scoring_dependencies_;
-    data_decoder::DataDecoder::ParseJsonIsolated(
+    data_decoder_.ParseJson(
         signed_additional_bid_data,
         base::BindOnce(&InterestGroupAuction::HandleDecodedSignedAdditionalBid,
                        weak_ptr_factory_.GetWeakPtr()));
@@ -3590,7 +3592,7 @@ void InterestGroupAuction::HandleDecodedSignedAdditionalBid(
 
   auto valid_signatures = maybe_signed_additional_bid->VerifySignatures();
 
-  data_decoder::DataDecoder::ParseJsonIsolated(
+  data_decoder_.ParseJson(
       maybe_signed_additional_bid->additional_bid_json,
       base::BindOnce(&InterestGroupAuction::HandleDecodedAdditionalBid,
                      weak_ptr_factory_.GetWeakPtr(),
@@ -4407,6 +4409,15 @@ void InterestGroupAuction::OnDirectFromSellerSignalHeaderAdSlotResolved(
     OnScoringDependencyDone();
     ScoreQueuedBidsIfReady();
   }
+}
+
+// static
+data_decoder::DataDecoder* InterestGroupAuction::GetDataDecoder(
+    base::WeakPtr<InterestGroupAuction> instance) {
+  if (!instance) {
+    return nullptr;
+  }
+  return &instance->data_decoder_;
 }
 
 }  // namespace content
