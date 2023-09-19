0c3ea90e064f8089653ed407406f7eaa9580a723
Kristi Saney <kristislee@google.com>
2023-09-19 16:30:02 +0000

    Update styling of next/previous to match mocks.
    https://screenshot.googleplex.com/9BAyXNRkQj9c8iz
    
    Bug: 1474951
    Change-Id: I1e10883e643d727aad02128e6cdc20179612b98f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4875504
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1198422}

--- chrome/browser/resources/side_panel/read_anything/read_anything_toolbar.ts
+++ chrome/browser/resources/side_panel/read_anything/read_anything_toolbar.ts
@@ -240,6 +240,10 @@ export class ReadAnythingToolbar extends ReadAnythingToolbarBase {
     assert(button);
     button.setAttribute('iron-icon', 'read-anything-20:pause');
     this.isPaused_ = false;
+
+    this.updateStyles({
+      '--audio-controls-background': 'var(--color-sys-tonal-container)',
+    });
   }
 
   updateUiForPausing() {
@@ -249,6 +253,10 @@ export class ReadAnythingToolbar extends ReadAnythingToolbarBase {
     assert(button);
     button.setAttribute('iron-icon', 'read-anything-20:play');
     this.isPaused_ = true;
+
+    this.updateStyles({
+      '--audio-controls-background': 'transparent',
+    });
   }
 
   private closeMenus_() {
