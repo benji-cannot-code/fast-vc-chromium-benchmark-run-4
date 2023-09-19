978c009377564107a80139ef3c2d865c154127c0
Quentin Pubert <qpubert@google.com>
2023-09-19 08:42:33 +0000

    [iOS] Add IdentityButton cell and item
    This CL adds a new item/cell class pair so an IdentityButtonControl can
    be embedded in a table view. This will be used in the Downloads settings
    to present an IdentityButtonControl just like it is done in the account
    picker view.
    
    Bug: 1478752
    Change-Id: Iaf98a44956875fa1efc6e851b84cc64ff8e6658c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4853505
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1198251}

---
+++ ios/chrome/browser/ui/settings/downloads/identity_button_cell.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_SETTINGS_DOWNLOADS_IDENTITY_BUTTON_CELL_H_
+#define IOS_CHROME_BROWSER_UI_SETTINGS_DOWNLOADS_IDENTITY_BUTTON_CELL_H_
+
+#import <UIKit/UIKit.h>
+
+#import "ios/chrome/browser/shared/ui/table_view/cells/table_view_cell.h"
+
+@class IdentityButtonControl;
+
+// A table view cell containing an IdentityButtonControl.
+@interface IdentityButtonCell : TableViewCell
+
+@property(nonatomic, strong, readonly)
+    IdentityButtonControl* identityButtonControl;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_SETTINGS_DOWNLOADS_IDENTITY_BUTTON_CELL_H_
---
+++ ios/chrome/browser/ui/settings/downloads/identity_button_cell.mm
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/settings/downloads/identity_button_cell.h"
+
+#import "ios/chrome/browser/ui/authentication/views/identity_button_control.h"
+#import "ios/chrome/common/ui/util/constraints_ui_util.h"
+
+@implementation IdentityButtonCell
+
+@synthesize identityButtonControl = _identityButtonControl;
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style
+              reuseIdentifier:(NSString*)reuseIdentifier {
+  self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+  if (self) {
+    _identityButtonControl =
+        [[IdentityButtonControl alloc] initWithFrame:self.contentView.frame];
+    // Remove rounded corners.
+    _identityButtonControl.layer.cornerRadius = 0;
+    _identityButtonControl.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.contentView addSubview:_identityButtonControl];
+    AddSameConstraints(self.contentView, _identityButtonControl);
+  }
+  return self;
+}
+
+@end
