315859dddda7d0eb327deb3b53749e1e9693a97b
Michael Wojcicka <mwoj@google.com>
2023-09-19 23:40:45 +0000

    Remove Battery Saver Notification from Message Center on Action
    - When the enable/disable button in the battery saver notification is
      pressed, the notification should be dismissed from the message center.
    
    - Also cleaned up an unused parameter in TestBatterySaverNotification().
    
    Change-Id: I153a41331faea9f99e7acbfdd50f2d6858218757
    Bug: b:301148779
    Test: ash_unittests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4877793
    Commit-Queue: Michael Wojcicka <mwoj@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198685}

--- ash/system/power/battery_notification_unittest.cc
+++ ash/system/power/battery_notification_unittest.cc
@@ -94,7 +94,6 @@ class BatteryNotificationTest : public AshTestBase {
   }
 
   void TestBatterySaverNotification(
-      const PowerStatus& status,
       const ExpectedNotificationValues& expected_values,
       PowerNotificationController::NotificationState notification_state,
       bool expected_bsm_state_after_click) {
@@ -122,6 +121,9 @@ class BatteryNotificationTest : public AshTestBase {
     // Click the button to turn off/on battery saver mode depending on
     // NotificationState.
     notification->delegate()->Click(0, absl::nullopt);
+
+    // Test that notification is dismissed after button is pressed.
+    EXPECT_EQ(GetBatteryNotification(), nullptr);
 
     // Test Enable Toast on Button Click in Opt-In Branch.
     if (notification_state ==
@@ -282,7 +284,7 @@ TEST_F(BatteryNotificationTest, ThresholdBatterySaverOptOutNotification) {
 
   // Battery Saver should turn off when the button is clicked.
   TestBatterySaverNotification(
-      *PowerStatus::Get(), expected_values,
+      expected_values,
       PowerNotificationController::NOTIFICATION_BSM_ENABLING_AT_THRESHOLD,
       /*expected_bsm_state_after_click=*/false);
 }
@@ -304,7 +306,7 @@ TEST_F(BatteryNotificationTest,
 
   // Battery Saver should turn off when the button is clicked.
   TestBatterySaverNotification(
-      *PowerStatus::Get(), expected_values,
+      expected_values,
       PowerNotificationController::NOTIFICATION_BSM_ENABLING_AT_THRESHOLD,
       /*expected_bsm_state_after_click=*/false);
 }
@@ -325,7 +327,7 @@ TEST_F(BatteryNotificationTest, ThresholdBatterySaverOptInNotification) {
 
   // Battery Saver should turn on when the button is clicked.
   TestBatterySaverNotification(
-      *PowerStatus::Get(), expected_values,
+      expected_values,
       PowerNotificationController::NOTIFICATION_BSM_THRESHOLD_OPT_IN,
       /*expected_bsm_state_after_click=*/true);
 }
@@ -348,7 +350,7 @@ TEST_F(BatteryNotificationTest,
 
   // Battery Saver should turn on when the button is clicked.
   TestBatterySaverNotification(
-      *PowerStatus::Get(), expected_values,
+      expected_values,
       PowerNotificationController::NOTIFICATION_BSM_THRESHOLD_OPT_IN,
       /*expected_bsm_state_after_click=*/true);
 }
