52d6da95d8d5192bf90c64e9ca7de08aeab5c0ed
Russ Hamilton <behamilton@google.com>
2023-09-19 18:20:02 +0000

    Add (optional) coordinator option to getInterestGroupAdAuctionData
    If the coordinator is not specified, we currently default to GCP.
    This CL doesn't actually add support for coordinators other than
    GCP. That will be done in a follow-up CL.
    
    Bug: 1473331
    Change-Id: Iab75a77ff02f9d1f3c05767d2ff7f8991c8da169
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4851113
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198493}

--- content/browser/interest_group/interest_group_manager_impl.cc
+++ content/browser/interest_group/interest_group_manager_impl.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/mojom/client_security_state.mojom.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/interest_group/interest_group.h"
 
 #include "url/gurl.h"
@@ -455,9 +456,10 @@ void InterestGroupManagerImpl::OnAdAuctionDataLoadComplete(
 
 void InterestGroupManagerImpl::GetBiddingAndAuctionServerKey(
     network::mojom::URLLoaderFactory* loader,
+    blink::mojom::AdAuctionCoordinator coordinator,
     base::OnceCallback<void(absl::optional<BiddingAndAuctionServerKey>)>
         callback) {
-  ba_key_fetcher_.GetOrFetchKey(loader, std::move(callback));
+  ba_key_fetcher_.GetOrFetchKey(loader, coordinator, std::move(callback));
 }
 
 void InterestGroupManagerImpl::OnJoinInterestGroupPermissionsChecked(
--- content/browser/interest_group/interest_group_manager_impl.h
+++ content/browser/interest_group/interest_group_manager_impl.h
@@ -340,6 +340,7 @@ class CONTENT_EXPORT InterestGroupManagerImpl : public InterestGroupManager {
 
   void GetBiddingAndAuctionServerKey(
       network::mojom::URLLoaderFactory* loader,
+      blink::mojom::AdAuctionCoordinator coordinator,
       base::OnceCallback<void(absl::optional<BiddingAndAuctionServerKey>)>
           callback);
 
