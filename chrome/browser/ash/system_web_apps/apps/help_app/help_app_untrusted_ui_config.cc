a95a1244bba66329ae03c5b044004837622fa0cc
Angela Xiao <angelaxiao@chromium.org>
2023-09-19 00:26:08 +0000

    Scalable Notifiers: add IsBrowserContextEligible
    Since the initialization of the scalable IPH service is now dependent
    on the initialization of some other services, add
    `IsBrowserContextEligible` as a substitute check for scalable IPH
    availability. If the browser context is eligible, we will assume that
    the service will also become available and proceed (i.e. show the
    Welcome Tips page in the Explore app after OOBE).
    
    Test: tested manually, existing tests
    Bug: b/300708368
    Change-Id: I0c33c16e277fc3e5a160e10d1fe505d9b328800c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4875454
    Reviewed-by: Callistus Tan <callistus@google.com>
    Commit-Queue: Angela Xiao <angelaxiao@chromium.org>
    Reviewed-by: Yuki Awano <yawano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1198108}

--- chrome/browser/ash/system_web_apps/apps/help_app/help_app_untrusted_ui_config.cc
+++ chrome/browser/ash/system_web_apps/apps/help_app/help_app_untrusted_ui_config.cc
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/upload_office_to_cloud/upload_office_to_cloud.h"
 #include "chrome/browser/policy/profile_policy_connector.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/scalable_iph/scalable_iph_factory.h"
+#include "chrome/browser/scalable_iph/scalable_iph_factory_impl.h"
 #include "chrome/common/channel_info.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/pref_names.h"
@@ -102,9 +102,9 @@ void PopulateLoadTimeData(content::WebUI* web_ui,
   Profile* profile = Profile::FromWebUI(web_ui);
   PrefService* pref_service = profile->GetPrefs();
 
-  scalable_iph::ScalableIph* scalable_iph =
-      ScalableIphFactory::GetForBrowserContext(profile);
-  if (scalable_iph) {
+  bool is_scalable_iph_available =
+      ScalableIphFactoryImpl::IsBrowserContextEligible(profile);
+  if (is_scalable_iph_available) {
     source->AddBoolean("HelpAppWelcomeTips",
                        ash::features::AreHelpAppWelcomeTipsEnabled());
     bool first_week_of_profile =
