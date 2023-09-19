994842ac07fa3f26c40ca8ac8ba1507e54668ca7
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2023-09-19 08:43:50 +0000

    Import wpt@2f5741f704e062180e3424c1126ba5b30cf6dd07
    Using wpt-import in Chromium f549b4b46294177a6c2df18079df0529971aae22.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    domenic@chromium.org, ricea@chromium.org:
      external/wpt/streams
    mstensho@chromium.org:
      external/wpt/css/css-break
    pcupp@microsoft.com:
      external/wpt/editing
    rego@igalia.com:
      external/wpt/css/selectors
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: Ic31fec3990f7a531b00dbcb2042410d562a84a7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4873719
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1198253}

--- third_party/blink/web_tests/external/wpt/streams/transferable/transform-stream-members.any.js
+++ third_party/blink/web_tests/external/wpt/streams/transferable/transform-stream-members.any.js
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: global=window,dedicatedworker,shadowrealm
+
 const combinations = [
   (t => [t, t.readable])(new TransformStream()),
   (t => [t.readable, t])(new TransformStream()),
