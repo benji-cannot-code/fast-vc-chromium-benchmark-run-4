895298a73a2dcf9494b4ca4ce8d001e80103e121
Ian Kilpatrick <ikilpatrick@chromium.org>
2023-09-19 20:03:01 +0000

    [cleanup] Change HasScrollableOverflow{X,Y} not to rely on pixel-snap.
    This patch changes HasScrollableOverflowX,HasScrollableOverflowY to
    not rely on:
     - PixelSnappedScrollWidth
     - PixelSnappedScrollHeight
     - PixelSnappedClientWidth
     - PixelSnappedClientHeight
    
    Bug: 962299
    Change-Id: Ic37161baaa7763e6243f430f1c0aa07b9c5a68ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4853259
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198569}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -1062,6 +1062,7 @@ LayoutUnit LayoutBox::ClientHeightFrom(LayoutUnit height) const {
 
 int LayoutBox::PixelSnappedClientWidth() const {
   NOT_DESTROYED();
+  DCHECK(!RuntimeEnabledFeatures::LayoutNewOverflowLogicEnabled());
   LayoutUnit left = RuntimeEnabledFeatures::LayoutNGNoLocationEnabled()
                         ? PhysicalLocation().left
                         : Location().X();
@@ -1071,6 +1072,7 @@ int LayoutBox::PixelSnappedClientWidth() const {
 DISABLE_CFI_PERF
 int LayoutBox::PixelSnappedClientHeight() const {
   NOT_DESTROYED();
+  DCHECK(!RuntimeEnabledFeatures::LayoutNewOverflowLogicEnabled());
   LayoutUnit top = RuntimeEnabledFeatures::LayoutNGNoLocationEnabled()
                        ? PhysicalLocation().top
                        : Location().Y();
@@ -1157,6 +1159,7 @@ LayoutUnit LayoutBox::ScrollHeight() const {
 
 int LayoutBox::PixelSnappedScrollWidth() const {
   NOT_DESTROYED();
+  DCHECK(!RuntimeEnabledFeatures::LayoutNewOverflowLogicEnabled());
   LayoutUnit left = RuntimeEnabledFeatures::LayoutNGNoLocationEnabled()
                         ? PhysicalLocation().left
                         : Location().X();
@@ -1165,6 +1168,7 @@ int LayoutBox::PixelSnappedScrollWidth() const {
 
 int LayoutBox::PixelSnappedScrollHeight() const {
   NOT_DESTROYED();
+  DCHECK(!RuntimeEnabledFeatures::LayoutNewOverflowLogicEnabled());
   LayoutUnit top = RuntimeEnabledFeatures::LayoutNGNoLocationEnabled()
                        ? PhysicalLocation().top
                        : Location().Y();
