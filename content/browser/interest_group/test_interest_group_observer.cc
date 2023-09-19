59813b5c6cf37cc29d5415ec2b0926a40994de3f
Maks Orlovich <morlovich@chromium.org>
2023-09-19 17:12:43 +0000

    FLEDGE: Report additional bids to devtools, too.
    This mostly involves splitting out the reporting from
    InterestGroupManagerImpl::RecordInterestGroupBids and RecordInterestGroupWin and just doing it directly (and, for bids, also at earlier time --- waiting for the reporter to notify devtools of those doesn't seem necessary).
    
    Two new enum value is also added for additional bids + them winning, to distinguish them from regular DB bids, which have separate namespace.
    
    Bug: 1475640
    Change-Id: I831d13584edd764d6f4530c42a3e4620f09b3262
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4873598
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198450}

--- content/browser/interest_group/test_interest_group_observer.cc
+++ content/browser/interest_group/test_interest_group_observer.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "content/browser/interest_group/interest_group_manager_impl.h"
 #include "content/common/content_export.h"
+#include "testing/gmock/include/gmock/gmock-matchers.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/origin.h"
 
@@ -44,7 +45,7 @@ void TestInterestGroupObserver::WaitForAccesses(
     run_loop_->Run();
     run_loop_.reset();
   }
-  EXPECT_EQ(expected, accesses_);
+  EXPECT_THAT(accesses_, ::testing::UnorderedElementsAreArray(expected));
 
   // Clear accesses so can be reused.
   accesses_.clear();
