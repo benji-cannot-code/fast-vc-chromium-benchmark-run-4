658d9911cd89ed1283a875cc01faf957776e2a1e
Nina Satragno <nsatragno@chromium.org>
2023-09-19 22:56:13 +0000

    [webauthn] Jump to phone when syncing passkeys
    Jump to the phone confirmation screen for allow-list requests when a GPM
    passkey from sync matches the request, and there are no local matches.
    
    Fixed: 1478310
    Change-Id: I6365e8206db00f009c2033a58b4fde6c5aa981e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4844448
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198667}

--- chrome/browser/ui/webauthn/sheet_models_unittest.cc
+++ chrome/browser/ui/webauthn/sheet_models_unittest.cc
@@ -29,9 +29,9 @@ class MockDialogModel : public AuthenticatorRequestDialogModel {
 
   MOCK_METHOD(base::span<const Mechanism>, mechanisms, (), (const));
   MOCK_METHOD(absl::optional<std::u16string>,
-              GetPrioritySyncedPhoneName,
+              GetPriorityPhoneName,
               (),
-              (const));
+              (const override));
 };
 
 class TestAuthenticatorSheetModel : public AuthenticatorSheetModelBase {
@@ -115,7 +115,7 @@ constexpr char16_t kPhoneName[] = u"pixel 7";
 TEST_F(AuthenticatorMultiSourcePickerSheetModelTest, GPMPasskeysOnly) {
   MockDialogModel dialog_model;
   std::vector<Mechanism> mechanisms;
-  EXPECT_CALL(dialog_model, GetPrioritySyncedPhoneName)
+  EXPECT_CALL(dialog_model, GetPriorityPhoneName)
       .WillRepeatedly(testing::Return(kPhoneName));
   mechanisms.emplace_back(
       Mechanism::Credential({device::AuthenticatorType::kPhone, {0}}),
@@ -140,7 +140,7 @@ TEST_F(AuthenticatorMultiSourcePickerSheetModelTest, GPMPasskeysOnly) {
 TEST_F(AuthenticatorMultiSourcePickerSheetModelTest, GPMAndLocalPasskeys) {
   MockDialogModel dialog_model;
   std::vector<Mechanism> mechanisms;
-  EXPECT_CALL(dialog_model, GetPrioritySyncedPhoneName)
+  EXPECT_CALL(dialog_model, GetPriorityPhoneName)
       .WillRepeatedly(testing::Return(kPhoneName));
   mechanisms.emplace_back(
       Mechanism::Credential({device::AuthenticatorType::kPhone, {0}}),
