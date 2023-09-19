0c523608c9f9e061beeec5404d72266f74b64cca
Chuong Ho <hdchuong@google.com>
2023-09-19 13:56:06 +0000

    Update the profile when the active user has changed
    There is a known issue when the user with OWNER ACCOUNT (the very first ACCOUNT that is logged in) logs in to the Chromebook, the profile is not re-created and they will continue to use the incognito sign-in profile. This causes crashes/misbehaviors for pref service or identity manager.
    
    This issue is not witnessed for users who are NOT the OWNER ACCOUNT, e.g. the second and third account. For those users, their Profile instance is always created after their successful login, and that Profile instance correctly reflects their user data.
    
    This CL fixes that issue by adding an Observer to the Mediator that listens to the "ActiveUserChanged" event. When the OWNER ACCOUNT user logs in, "ActiveUserChanged" is fired because the active user has switched from a null user (incognito profile) into a real OWNER user (real profile).
    
    The method used in this CL is inspired by those applied in:
    * Magnifier Manager: https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ash/accessibility/magnification_manager.cc;l=181-188
    
    * System Clock:
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ash/system/system_clock.cc;l=83-90;drc=ea0d7cb33d730a591f481c84881dd8d009a6b948
    
    Bug: b:298865729
    
    Change-Id: Iad2dfa62175e2039d54a28407714c70afd67ff6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4835286
    Commit-Queue: Chuong Ho <hdchuong@google.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198355}

--- chrome/browser/ash/input_method/editor_mediator.cc
+++ chrome/browser/ash/input_method/editor_mediator.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "base/check_op.h"
+#include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/ui/webui/ash/mako/mako_ui.h"
 #include "ui/base/ime/ash/ime_bridge.h"
 
@@ -30,6 +31,7 @@ EditorMediator::EditorMediator(Profile* profile, std::string_view country_code)
   DCHECK(!g_instance_);
   g_instance_ = this;
 
+  user_manager::UserManager::Get()->AddSessionStateObserver(this);
   profile_observation_.Observe(profile_);
   tablet_mode_observation_.Observe(TabletMode::Get());
 
@@ -39,6 +41,10 @@ EditorMediator::EditorMediator(Profile* profile, std::string_view country_code)
 EditorMediator::~EditorMediator() {
   DCHECK_EQ(g_instance_, this);
   g_instance_ = nullptr;
+
+  if (user_manager::UserManager::IsInitialized()) {
+    user_manager::UserManager::Get()->RemoveSessionStateObserver(this);
+  }
 }
 
 EditorMediator* EditorMediator::Get() {
@@ -171,6 +177,23 @@ bool EditorMediator::CanBeTriggered() {
 
 ConsentStatus EditorMediator::GetConsentStatus() {
   return consent_store_->GetConsentStatus();
+}
+
+void EditorMediator::ActiveUserChanged(user_manager::User* user) {
+  if (user) {
+    user->AddProfileCreatedObserver(
+        base::BindOnce(&EditorMediator::SetProfileByUser,
+                       weak_ptr_factory_.GetWeakPtr(), user));
+  }
+}
+
+void EditorMediator::SetProfileByUser(user_manager::User* user) {
+  profile_ = ProfileHelper::Get()->GetProfileByUser(user);
+  profile_observation_.Reset();
+  profile_observation_.Observe(profile_);
+  editor_switch_->SetProfile(profile_);
+  consent_store_->SetPrefService(profile_->GetPrefs());
+  text_query_provider_->OnProfileChanged(profile_);
 }
 
 void EditorMediator::OnProfileWillBeDestroyed(Profile* profile) {
