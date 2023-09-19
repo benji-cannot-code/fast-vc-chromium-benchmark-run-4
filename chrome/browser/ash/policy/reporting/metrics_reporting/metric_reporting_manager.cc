0b77b0318a4824b88f69f9612c06cb408d738da6
Hong Xu <xuhong@chromium.org>
2023-09-19 03:53:30 +0000

    Enable runtime counters by default
    launch/4263280 has been approved, enable the run time counters feature
    by default thus the policy would take full control.
    
    Bug: b:280068539
    Fixed: b:280068539
    Change-Id: If10d7f965fc36a0bec57c30bfc6dbdb6c9f0fe5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4874867
    Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    Auto-Submit: Hong Xu <xuhong@google.com>
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1198178}

--- chrome/browser/ash/policy/reporting/metrics_reporting/metric_reporting_manager.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/metric_reporting_manager.cc
@@ -100,7 +100,7 @@ BASE_FEATURE(kEnableFatalCrashEventsObserver,
              base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kEnableRuntimeCountersTelemetry,
              "EnableRuntimeCountersTelemetry",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool MetricReportingManager::Delegate::IsUserAffiliated(
     Profile& profile) const {
