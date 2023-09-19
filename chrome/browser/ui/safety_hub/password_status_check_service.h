eb8931584f4c5dccb46b2be22661f90cbf4b48e5
Eduard Hez <ehez@chromium.org>
2023-09-19 09:06:01 +0000

    [SHv2] Small fixes for password check module
    This CL fixes two issues:
    (1) When a password had multiple issues, e.g. both weak and reused, only one was counted because the counting logic stopped early. Now all issues
    are tracked.
    (2) To determine the next password check time, a fixed constant was added to the last time. However when the time for the last password
    check was a long time ago, the next run was also in the past. Now the
    interval is added until the next check is in the future.
    
    Bug: 1443466
    Change-Id: I0df0237eaab07dbc0c885c37acf0b49c530b1b2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4874259
    Commit-Queue: Eduard Hez <ehez@chromium.org>
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198259}

--- chrome/browser/ui/safety_hub/password_status_check_service.h
+++ chrome/browser/ui/safety_hub/password_status_check_service.h
@@ -84,7 +84,8 @@ class PasswordStatusCheckService
  private:
   FRIEND_TEST_ALL_PREFIXES(PasswordStatusCheckServiceBaseTest,
                            CheckTimeUpdatedAfterRunScheduledInThePast);
-
+  FRIEND_TEST_ALL_PREFIXES(PasswordStatusCheckServiceBaseTest,
+                           CheckTimeUpdatedAfterRunScheduledLongTimeInThePast);
   // Triggers Password Manager's password check to discover new credential
   // issues.
   void RunPasswordCheckAsync();
