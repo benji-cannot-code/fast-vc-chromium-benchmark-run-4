0d555c4b0c5e33a9dfd714d4dacfcaad57470d81
Monica Basta <msalama@chromium.org>
2023-09-19 08:38:41 +0000

    [DBSC] Limit DBSC session registration/termination to a.g.c
    Bug: b/273920956
    Change-Id: I3386d38d481da2b2d7d03aeafc23f78ed584d8e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4850307
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198249}

--- chrome/browser/signin/header_modification_delegate_impl.cc
+++ chrome/browser/signin/header_modification_delegate_impl.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/storage_partition.h"
+#include "google_apis/gaia/gaia_auth_util.h"
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 #include "extensions/browser/guest_view/web_view/web_view_renderer_state.h"
@@ -150,7 +151,8 @@ void HeaderModificationDelegateImpl::ProcessResponse(
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
 #if BUILDFLAG(ENABLE_BOUND_SESSION_CREDENTIALS)
-  if (switches::IsBoundSessionCredentialsEnabled()) {
+  if (gaia::HasGaiaSchemeHostPort(response_adapter->GetUrl()) &&
+      switches::IsBoundSessionCredentialsEnabled()) {
     BoundSessionCookieRefreshService* bound_session_cookie_refresh_service =
         BoundSessionCookieRefreshServiceFactory::GetForProfile(profile_);
     if (bound_session_cookie_refresh_service) {
