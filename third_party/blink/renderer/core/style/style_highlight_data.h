493eb094644662a94c93685e1fe835465d8b1545
Rune Lillesveen <futhark@chromium.org>
2023-09-19 14:06:50 +0000

    [@container] Fix ::highlight() style recalc for size containers
    Highlight pseudo styles are updated along with the originating element's
    style in RecalcOwnStyle(). If the originating element is a size
    container and the highlight styles depend on size container queries, we
    need to revisit the highlight style computation when the layout size is
    known.
    
    Bug: 1481068
    Change-Id: Icc9b26565e2ffc5e0b06b51d9175ccb8648104de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4868848
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198359}

--- third_party/blink/renderer/core/style/style_highlight_data.h
+++ third_party/blink/renderer/core/style/style_highlight_data.h
@@ -46,6 +46,8 @@ class CORE_EXPORT StyleHighlightData final {
   void SetGrammarError(const ComputedStyle*);
   void SetCustomHighlight(const AtomicString&, const ComputedStyle*);
 
+  bool DependsOnSizeContainerQueries() const;
+
   void Trace(Visitor*) const;
 
  private:
