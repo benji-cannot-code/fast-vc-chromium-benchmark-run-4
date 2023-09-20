e5a590a2f98f004a2ddb314e450626526bfa8957
Stephan Hartmann <stha09@googlemail.com>
2023-09-20 18:03:47 +0000

    gtk4: fix building with >=gtk-4.1.0
    gtk-4.1.1 changed the signature of some gsk function by making the parameters const.
    
    Bug: 1484996
    Change-Id: Ide526e35b10d614a404acbfb3d946993f6267571
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4876645
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199115}

--- ui/gtk/gtk_compat.h
+++ ui/gtk/gtk_compat.h
@@ -19,6 +19,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gtk/gtk_types.h"
 
+#if GTK_CHECK_VERSION(4, 1, 1)
+#define UI_GTK_CONST const
+#else
+#define UI_GTK_CONST
+#endif
+
 extern "C" {
 #include "ui/gtk/gdk.sigs"
 #include "ui/gtk/gdk_pixbuf.sigs"
