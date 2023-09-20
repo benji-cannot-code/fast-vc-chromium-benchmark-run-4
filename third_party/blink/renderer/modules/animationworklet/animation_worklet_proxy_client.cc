428d6ef16a518f64283e3eb89f2a4b31a31c974f
Jonathan Ross <jonross@chromium.org>
2023-09-20 19:45:13 +0000

    Remove expired histograms Animation.AnimationWorklet.MutateDuration
    Metric long expired, looking to remove it.
    
    OBSOLETE_HISTOGRAM=Animation.AnimationWorklet.MutateDuration long
    expired
    
    Bug: 1385008
    Change-Id: I2c19f6674d41b8d1739d3e2c8cf4c0a1cbf6908b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4873649
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199179}

--- third_party/blink/renderer/modules/animationworklet/animation_worklet_proxy_client.cc
+++ third_party/blink/renderer/modules/animationworklet/animation_worklet_proxy_client.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/histogram_macros.h"
 #include "base/task/single_thread_task_runner.h"
-#include "base/timer/elapsed_timer.h"
 #include "third_party/blink/public/platform/scheduler/web_agent_group_scheduler.h"
 #include "third_party/blink/renderer/core/animation/worklet_animation_controller.h"
 #include "third_party/blink/renderer/core/dom/document.h"
@@ -163,7 +162,6 @@ std::unique_ptr<AnimationWorkletOutput> AnimationWorkletProxyClient::Mutate(
   if (state_ == RunState::kDisposed)
     return output;
 
-  base::ElapsedTimer timer;
   DCHECK(input);
 #if DCHECK_IS_ON()
   DCHECK(input->ValidateId(worklet_id_))
@@ -179,11 +177,6 @@ std::unique_ptr<AnimationWorkletOutput> AnimationWorkletProxyClient::Mutate(
 
   global_scope->UpdateAnimators(*input, output.get(),
                                 [](Animator* animator) { return true; });
-
-  UMA_HISTOGRAM_CUSTOM_MICROSECONDS_TIMES(
-      "Animation.AnimationWorklet.MutateDuration", timer.Elapsed(),
-      base::Microseconds(1), base::Seconds(10), 50);
-
   return output;
 }
 
