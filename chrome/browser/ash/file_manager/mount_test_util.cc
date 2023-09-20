8af65cfc6162f6ecfe02202985cc600b1b63a774
François Degros <fdegros@chromium.org>
2023-09-20 08:40:37 +0000

    Simplify DriveMountPointWaiter
    BUG=b:301024841
    
    Change-Id: I85dcfa13d53b5c05fcba5fe8df912658350da172
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4876047
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Auto-Submit: François Degros <fdegros@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198867}

--- chrome/browser/ash/file_manager/mount_test_util.cc
+++ chrome/browser/ash/file_manager/mount_test_util.cc
@@ -16,19 +16,16 @@ namespace {
 
 // Helper class used to wait for |OnFileSystemMounted| event from a drive file
 // system.
-class DriveMountPointWaiter : public drive::DriveIntegrationServiceObserver {
+class DriveMountPointWaiter : public drive::DriveIntegrationService::Observer {
  public:
-  explicit DriveMountPointWaiter(
-      drive::DriveIntegrationService* integration_service)
-      : integration_service_(integration_service) {
-    integration_service_->AddObserver(this);
+  explicit DriveMountPointWaiter(drive::DriveIntegrationService* service)
+      : service_(service) {
+    service_->AddObserver(this);
   }
 
-  ~DriveMountPointWaiter() override {
-    integration_service_->RemoveObserver(this);
-  }
+  ~DriveMountPointWaiter() override { service_->RemoveObserver(this); }
 
-  // DriveIntegrationServiceObserver override.
+  // DriveIntegrationService::Observer implementation.
   void OnFileSystemMounted() override {
     // Note that it is OK for |run_loop_.Quit| to be called before
     // |run_loop_.Run|. In this case |Run| will return immediately.
@@ -39,7 +36,7 @@ class DriveMountPointWaiter : public drive::DriveIntegrationServiceObserver {
   void Wait() { run_loop_.Run(); }
 
  private:
-  raw_ptr<drive::DriveIntegrationService, ExperimentalAsh> integration_service_;
+  const raw_ptr<drive::DriveIntegrationService, ExperimentalAsh> service_;
   base::RunLoop run_loop_;
 };
 
