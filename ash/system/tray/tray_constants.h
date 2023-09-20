7ba9985643dfa89411b7e3f43d5b075c41db6d88
Kevin Radtke <kradtke@chromium.org>
2023-09-20 00:49:50 +0000

    Update secondary bubble duration to 3s
    Update constant to use `base::TimeDelta` instead of an int, and modify
    its value from 2 to 3 seconds.
    
    Based on go/notifier-framework specifications.
    
    Bug: b:296302181
    Change-Id: I843751703e782078bda79a0f786e7d41d0f56661
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4874502
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198717}

--- ash/system/tray/tray_constants.h
+++ ash/system/tray/tray_constants.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_SYSTEM_TRAY_TRAY_CONSTANTS_H_
 
 #include "ash/ash_export.h"
+#include "base/time/time.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/geometry/size.h"
@@ -35,8 +36,10 @@ constexpr int kRevampedTrayMenuWidth = 400;
 constexpr int kGlanceablesLeftRightMargin = 16;
 constexpr int kGlanceablesVerticalMargin = 16;
 
-constexpr int kTrayPopupAutoCloseDelayInSeconds = 2;
-constexpr int kTrayPopupAutoCloseDelayInSecondsWithSpokenFeedback = 5;
+constexpr base::TimeDelta kSecondaryBubbleDuration = base::Seconds(3);
+constexpr base::TimeDelta kSecondaryBubbleWithSpokenFeedbackDuration =
+    base::Seconds(5);
+
 constexpr int kTrayPopupPaddingHorizontal = 18;
 constexpr int kTrayPopupButtonEndMargin = 10;
 
