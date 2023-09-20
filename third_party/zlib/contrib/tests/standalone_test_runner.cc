70ee4d8facbf2dd8029f96a1bb1e926f39b2d3de
Gustavo A Espinoza <gustavo.adolfo.espinoza.quintero@intel.com>
2023-09-20 22:41:05 +0000

    [zlib][build] Standalone build of unit tests
    Added a standalone build for unit tests using cmake. This adds a new .cc
    file that acts as an entry point for starting googletest.
    
    Bug: 1381000
    Change-Id: Ica723b40d7ad8fbf5eb633f52c4a526feac03964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4879546
    Reviewed-by: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Commit-Queue: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199281}

---
+++ third_party/zlib/contrib/tests/standalone_test_runner.cc
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the Chromium source repository LICENSE file.
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
--- third_party/zlib/contrib/tests/utils_unittest.cc
+++ third_party/zlib/contrib/tests/utils_unittest.cc
@@ -8,12 +8,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstddef>
 #include <vector>
 
-#include "base/files/file_path.h"
-#include "base/files/scoped_temp_dir.h"
 #include "compression_utils_portable.h"
 #include "gtest.h"
+
+#if !defined(CMAKE_STANDALONE_UNITTESTS)
+#include "base/files/file_path.h"
+#include "base/files/scoped_temp_dir.h"
+
 #include "third_party/zlib/contrib/minizip/unzip.h"
 #include "third_party/zlib/contrib/minizip/zip.h"
+#endif
+
 #include "zlib.h"
 
 void TestPayloads(size_t input_size, zlib_internal::WrapperType type) {
@@ -1021,6 +1026,9 @@ TEST(ZlibTest, DeflateZFixedCorruption) {
       0);
 }
 
+// TODO(gustavoa): make these tests run standalone.
+#ifndef CMAKE_STANDALONE_UNITTESTS
+
 TEST(ZlibTest, ZipFilenameCommentSize) {
   // Check that minizip rejects zip member filenames or comments longer than
   // the zip format can represent.
@@ -1139,3 +1147,5 @@ TEST(ZlibTest, ZipExtraFieldSize) {
   EXPECT_EQ(unzGoToNextFile(uzf), UNZ_END_OF_LIST_OF_FILE);
   EXPECT_EQ(unzClose(uzf), UNZ_OK);
 }
+
+#endif
