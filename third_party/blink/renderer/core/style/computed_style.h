ff6deee39d7077635731c47d44cd69e4d89cfcb6
Anders Hartvoll Ruud <andruud@chromium.org>
2023-09-20 22:31:41 +0000

    [StyleBuilder] Remove [Raw]DataRef
    These wrappers no longer serve any important purpose, and is mostly
    just in the way of future development. This CL removes DataRef and
    RawDataRef, and instead uses UncompressedMember and raw pointers
    (respectively).
    
    Accessing (i.e. mutating) fields now happen via nested Access calls.
    E.g. to access X->Y->Z, we previously did:
    
      X.Access(x_bit)->Y.Access(y_bit)->Z = ...
    
    Whereas now we do:
    
      Access(Access(X, x_bit)->Y, y_bit)->Z = ...
    
    This is arguably less readable, but also not very important, since
    clients of ComputedStyle do not interact directly with this code.
    
    Some minor cleanup to ComputedStyleAccessFlags: we don't need a name
    for this struct anymore, since all the flags are once again only
    relevant within the builder.
    
    The style perf test appears neutral (Alder Lake).
    
    Change-Id: Ia8e3b058caef80f9db60a38e5e3eed2e2a1514d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4876647
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199265}

--- third_party/blink/renderer/core/style/computed_style.h
+++ third_party/blink/renderer/core/style/computed_style.h
@@ -54,7 +54,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/style/computed_style_constants.h"
 #include "third_party/blink/renderer/core/style/computed_style_initial_values.h"
 #include "third_party/blink/renderer/core/style/cursor_list.h"
-#include "third_party/blink/renderer/core/style/data_ref.h"
 #include "third_party/blink/renderer/core/style/display_style.h"
 #include "third_party/blink/renderer/core/style/filter_operations.h"
 #include "third_party/blink/renderer/core/style/font_size_style.h"
