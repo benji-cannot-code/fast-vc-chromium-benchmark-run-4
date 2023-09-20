0a5e8275935474c1b92235f198f3fef68f99300c
John Lee <johntlee@chromium.org>
2023-09-20 20:04:55 +0000

    Downloads WebUI: Replace old paper color for download item icon
    In an effort to remove usages of --paper-*-* colors, this CL replaces
    --paper-grey-400 with --google-grey-400. The difference is subtle
    (see https://jsfiddle.net/caux60tq/).
    
    Bug: 1062154
    Change-Id: If870ad06db0f69c977d0c0de00beb40f3a582c14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4874828
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199199}

--- chrome/browser/resources/downloads/item.ts
+++ chrome/browser/resources/downloads/item.ts
@@ -574,7 +574,7 @@ export class DownloadsItemElement extends DownloadsItemElementBase {
       return 'red';
     }
     if (!this.useFileIcon_) {
-      return 'paper-grey';
+      return 'light-grey';
     }
     return '';
   }
