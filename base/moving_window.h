51b65c77bc8df200b65f9f2c8e1d529b01bc3f7a
Stephan Hartmann <stha09@googlemail.com>
2023-09-20 16:04:08 +0000

    IWYU: add math.h for sqrt in base::MovingWindow
    Bug: 957519
    Change-Id: I6a6f4821fea5604ea7b8ff278fa1e641b4863951
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4877015
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#1199039}

--- base/moving_window.h
+++ base/moving_window.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_MOVING_WINDOW_H_
 #define BASE_MOVING_WINDOW_H_
 
+#include <math.h>
 #include <stddef.h>
 
 #include <functional>
