8c259f56c1ef99f962b60a85b6e1063466f12525
Nicolas MacBeth <nicolasmacbeth@google.com>
2023-09-20 15:38:58 +0000

    [a11y] Fix "User passwords on all your devices..." is read as a link
    Removed the link accessibility trait from the module descriptions to not
    have them read as links by VoiceOver.
    
    Bug: 1484763, b/301247343
    Change-Id: I92d1db2cf6ce678876d95483a34ae831aba63cc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4878214
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1199019}

--- ios/chrome/browser/ui/settings/password/password_settings/password_settings_view_controller.mm
+++ ios/chrome/browser/ui/settings/password/password_settings/password_settings_view_controller.mm
@@ -427,8 +427,6 @@ typedef NS_ENUM(NSInteger, ModelLoadStatus) {
   _bulkMovePasswordsToAccountDescriptionItem.enabled = NO;
   _bulkMovePasswordsToAccountDescriptionItem.accessibilityIdentifier =
       kPasswordSettingsBulkMovePasswordsToAccountDescriptionTableViewId;
-  _bulkMovePasswordsToAccountDescriptionItem.accessibilityTraits |=
-      UIAccessibilityTraitLink;
 
   std::u16string pattern = l10n_util::GetStringUTF16(
       IDS_IOS_PASSWORD_SETTINGS_BULK_UPLOAD_PASSWORDS_SECTION_DESCRIPTION);
@@ -515,8 +513,6 @@ typedef NS_ENUM(NSInteger, ModelLoadStatus) {
   _onDeviceEncryptionOptInDescriptionItem.enabled = NO;
   _onDeviceEncryptionOptInDescriptionItem.accessibilityIdentifier =
       kPasswordSettingsOnDeviceEncryptionOptInId;
-  _onDeviceEncryptionOptInDescriptionItem.accessibilityTraits |=
-      UIAccessibilityTraitLink;
   return _onDeviceEncryptionOptInDescriptionItem;
 }
 
