3fb01be2a196f765099bb9ef468af6343d6d45da
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2023-09-20 02:10:43 +0000

    Import wpt@ec15f835348a29101b0acd93cae2ab1c4015326b
    Using wpt-import in Chromium e8a2766a24d84d172e82f4d721fd548d82f53655.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    ericwilligers@chromium.org:
      external/wpt/css/motion
    japhet@chromium.org:
      external/wpt/fetch
    rego@igalia.com:
      external/wpt/css/css-contain
      external/wpt/css/css-grid
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: Iedca67dc9bbb248e8de2db7cfbee545954c4ab3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4873764
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1198753}

--- third_party/blink/web_tests/external/wpt/fetch/orb/tentative/status.sub.any.js
+++ third_party/blink/web_tests/external/wpt/fetch/orb/tentative/status.sub.any.js
@@ -3,32 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 const path = "http://{{domains[www1]}}:{{ports[http][0]}}/fetch/orb/resources";
 
-promise_test(
-  t =>
-    promise_rejects_js(
-      t,
-      TypeError,
-      fetchORB(
-        `${path}/data.json`,
-        null,
-        contentType("application/json"),
-        "status(206)"
-      )
-    ),
-  "ORB should block opaque-response-blocklisted MIME type with status 206"
-);
+expected_block(
+  `${path}/data.json`,
+  (orb_test, message) => promise_test(
+    t => orb_test(t, contentType("application/json"), "status(206)"),
+    message("ORB should block opaque-response-blocklisted MIME type with status 206")));
 
-promise_test(
-  t =>
-    promise_rejects_js(
-      t,
-      TypeError,
-      fetchORB(
-        `${path}/data.json`,
-        null,
-        contentType("application/json"),
-        "status(302)"
-      )
-    ),
-  "ORB should block opaque response with non-ok status"
-);
+expected_block(
+  `${path}/data.json`,
+  (orb_test, message) =>
+    promise_test(
+      t => orb_test(t, contentType("application/json"), "status(302)"),
+      message("ORB should block opaque response with non-ok status")));
