98d33ca163d792dceb4154afe0cae7ed15e65b7e
Ankush Singh <ankushkush@google.com>
2023-09-20 13:37:54 +0000

    [sync] Comment out deprecated enum value from syncable prefs database
    Bug: 1412699
    Change-Id: I36052f2f848d25a71ad59b868e09390a54d72e5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4876885
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1198946}

--- components/sync_preferences/common_syncable_prefs_database.cc
+++ components/sync_preferences/common_syncable_prefs_database.cc
@@ -47,7 +47,7 @@ enum {
   kAutofillCreditCardEnabled = 1,
   kAutofillEnabledDeprecated = 2,
   kAutofillHasSeenIban = 3,
-  kAutofillIbanEnabled = 4,  // Obsolete.
+  // kAutofillIbanEnabled = 4,  (obsolete).
   kAutofillLastVersionDeduped = 5,
   kAutofillLastVersionDisusedAddressesDeleted = 6,
   kAutofillProfileEnabled = 7,
