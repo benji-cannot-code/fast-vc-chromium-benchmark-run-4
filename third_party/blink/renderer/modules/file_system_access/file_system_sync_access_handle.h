8c797fc5064de16e2cfc5c8855e9899e9fb1dbcd
Austin Sullivan <asully@chromium.org>
2023-09-20 15:42:57 +0000

    FSA: Allow use of ArrayBuffer for SyncAccessHandle.read and .write
    These methods are specified as taking a AllowSharedBufferSource, but
    when the implementation was ported over from the Storage Foundation API
    (back in https://crrev.com/c/3014235!) the IDL wasn't properly updated
    
    Spec: https://fs.spec.whatwg.org/#filesystemsyncaccesshandle
    
    Fixed: 1418593
    Change-Id: Id16bf59d1125163fa4a71288eeadc62507650683
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4875125
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Daseul Lee <dslee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199023}

--- third_party/blink/renderer/modules/file_system_access/file_system_sync_access_handle.h
+++ third_party/blink/renderer/modules/file_system_access/file_system_sync_access_handle.h
@@ -11,8 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/v8_file_system_create_sync_access_handle_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_file_system_read_write_options.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
-#include "third_party/blink/renderer/core/typed_arrays/array_buffer_view_helpers.h"
-#include "third_party/blink/renderer/core/typed_arrays/dom_array_buffer_view.h"
+#include "third_party/blink/renderer/modules/file_system_access/allow_shared_buffer_source_util.h"
 #include "third_party/blink/renderer/modules/file_system_access/file_system_access_file_delegate.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
@@ -45,11 +44,11 @@ class FileSystemSyncAccessHandle final : public ScriptWrappable {
 
   void truncate(uint64_t size, ExceptionState&);
 
-  uint64_t read(MaybeShared<DOMArrayBufferView> buffer,
+  uint64_t read(const AllowSharedBufferSource* buffer,
                 FileSystemReadWriteOptions* options,
                 ExceptionState&);
 
-  uint64_t write(MaybeShared<DOMArrayBufferView> buffer,
+  uint64_t write(const AllowSharedBufferSource* buffer,
                  FileSystemReadWriteOptions* options,
                  ExceptionState&);
 
