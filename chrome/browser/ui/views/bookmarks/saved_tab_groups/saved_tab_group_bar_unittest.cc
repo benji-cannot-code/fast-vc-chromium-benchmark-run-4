b96e84a03ddad011a0f7fbf08419905d6a75ae07
jongmok.kim <jongmok.kim@navercorp.com>
2023-09-20 03:21:25 +0000

    [SavedTabGroup] Add DCHECK in SavedTabGroupBar constructor.
    - When the #tab-groups-saved feature flag is turned on and profile is regular, `SavedTabGroupBar` is instantiated.
    - If the #tab-groups-saved feature flag is turned off, there is no SavedTabGroupModel.
    
    Bug: 1372008
    Change-Id: I3dec1fa2ed8039fa38b1518385d164cb6c73988b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4875753
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: jongmok kim <johny.kimc@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1198781}

--- chrome/browser/ui/views/bookmarks/saved_tab_groups/saved_tab_group_bar_unittest.cc
+++ chrome/browser/ui/views/bookmarks/saved_tab_groups/saved_tab_group_bar_unittest.cc
@@ -7,9 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/test/scoped_feature_list.h"
 #include "base/uuid.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/layout_constants.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/bookmarks/saved_tab_groups/saved_tab_group_button.h"
 #include "chrome/browser/ui/views/bookmarks/saved_tab_groups/saved_tab_group_overflow_button.h"
 #include "chrome/test/base/test_browser_window.h"
@@ -54,7 +56,9 @@ const tab_groups::TabGroupColorId kNewColor = tab_groups::TabGroupColorId::kRed;
 class SavedTabGroupBarUnitTest : public ChromeViewsTestBase {
  public:
   SavedTabGroupBarUnitTest()
-      : saved_tab_group_model_(std::make_unique<SavedTabGroupModel>()) {}
+      : saved_tab_group_model_(std::make_unique<SavedTabGroupModel>()) {
+    feature_list_.InitAndEnableFeature(features::kTabGroupsSave);
+  }
 
   SavedTabGroupBar* saved_tab_group_bar() { return saved_tab_group_bar_.get(); }
   SavedTabGroupModel* saved_tab_group_model() {
@@ -121,6 +125,7 @@ class SavedTabGroupBarUnitTest : public ChromeViewsTestBase {
   std::unique_ptr<SavedTabGroupBar> saved_tab_group_bar_;
   std::unique_ptr<SavedTabGroupModel> saved_tab_group_model_;
 
+  base::test::ScopedFeatureList feature_list_;
   std::unique_ptr<TestingProfile> profile_;
   std::unique_ptr<TestBrowserWindow> browser_window_;
   std::unique_ptr<Browser> browser_;
