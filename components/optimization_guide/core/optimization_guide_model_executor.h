4775de640cfb1a57a9d13999255805f772162481
rajendrant <rajendrant@chromium.org>
2023-09-20 23:54:23 +0000

    Add model execution fetcher
    Adds the fetcher to get model execution results from the remote optimization guide service.
    
    Change-Id: Ibc5f85ab7de45ceea1912a4e95188f23426491cb
    Bug: b/298715226
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4863551
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199317}

---
+++ components/optimization_guide/core/optimization_guide_model_executor.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_OPTIMIZATION_GUIDE_CORE_OPTIMIZATION_GUIDE_MODEL_EXECUTOR_H_
+#define COMPONENTS_OPTIMIZATION_GUIDE_CORE_OPTIMIZATION_GUIDE_MODEL_EXECUTOR_H_
+
+#include "base/functional/callback_forward.h"
+#include "base/types/optional_ref.h"
+#include "components/optimization_guide/proto/model_execution.pb.h"
+
+namespace optimization_guide {
+
+// The result type of model execution.
+using OptimizationGuideModelExecutionResult =
+    base::optional_ref<const proto::Any /*response_metadata*/>;
+
+// The callback for receiving the model execution result.
+using OptimizationGuideModelExecutionResultCallback =
+    base::OnceCallback<void(OptimizationGuideModelExecutionResult)>;
+
+// Interface for model execution.
+class OptimizationGuideModelExecutor {
+ public:
+  // Executes the model for `feature` with `request_metadata` and invokes the
+  // `callback` with the result.
+  void ExecuteModel(proto::ModelExecutionFeature feature,
+                    const google::protobuf::MessageLite& request_metadata,
+                    OptimizationGuideModelExecutionResultCallback callback);
+};
+
+}  // namespace optimization_guide
+
+#endif  // COMPONENTS_OPTIMIZATION_GUIDE_CORE_OPTIMIZATION_GUIDE_MODEL_EXECUTOR_H_
