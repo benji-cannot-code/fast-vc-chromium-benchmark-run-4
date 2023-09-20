4ed7e82c808576106bf6ff2562482d25e7a3a57e
Gauthier Ambard <gambard@chromium.org>
2023-09-20 17:58:15 +0000

    [iOS] Don't crash when snapshot has 0-size
    It seems that in some cases, the snapshot has a 0-size, which leads to
    a crash.
    This CL adds dump_will_be_check to understand where it is coming from.
    
    Bug: 1483997, 1468662
    Change-Id: Ie5f17059bba20568d45d8d74be38f501df60615b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4874398
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199112}

--- ios/chrome/browser/snapshots/snapshot_generator.mm
+++ ios/chrome/browser/snapshots/snapshot_generator.mm
@@ -291,6 +291,11 @@ BOOL ViewHierarchyContainsWebView(UIView* view) {
   // Set the mode to UIViewTintAdjustmentModeAutomatic.
   baseView.tintAdjustmentMode = UIViewTintAdjustmentModeAutomatic;
 
+  // TODO(crbug.com/1483997): Remove this once we know where the issue is coming
+  // from.
+  DUMP_WILL_BE_CHECK(!image ||
+                     (image.size.width != 0 && image.size.height != 0));
+
   return image;
 }
 
@@ -319,7 +324,7 @@ BOOL ViewHierarchyContainsWebView(UIView* view) {
       [[UIGraphicsImageRenderer alloc] initWithSize:frameInWindow.size
                                              format:format];
 
-  return
+  UIImage* image =
       [renderer imageWithActions:^(UIGraphicsImageRendererContext* UIContext) {
         CGContextRef context = UIContext.CGContext;
 
@@ -336,6 +341,13 @@ BOOL ViewHierarchyContainsWebView(UIView* view) {
                               -frameInWindow.origin.y);
         [self drawOverlays:overlays context:context];
       }];
+
+  // TODO(crbug.com/1483997): Remove this once we know where the issue is coming
+  // from.
+  DUMP_WILL_BE_CHECK(!image ||
+                     (image.size.width != 0 && image.size.height != 0));
+
+  return image;
 }
 
 // Updates the snapshot cache with `snapshot`.
--- ios/chrome/common/ui/util/image_util.mm
+++ ios/chrome/common/ui/util/image_util.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/common/ui/util/image_util.h"
 
+#import "base/check.h"
 #import "ui/gfx/image/resize_image_dimensions.h"
 
 UIImage* ResizeImage(UIImage* image,
@@ -38,9 +39,17 @@ UIImage* ResizeImage(UIImage* image,
       [[UIGraphicsImageRenderer alloc] initWithSize:revisedTargetSize
                                              format:format];
 
-  return [renderer imageWithActions:^(UIGraphicsImageRendererContext* context) {
-    [image drawInRect:projectTo];
-  }];
+  UIImage* result_image =
+      [renderer imageWithActions:^(UIGraphicsImageRendererContext* context) {
+        [image drawInRect:projectTo];
+      }];
+
+  // TODO(crbug.com/1483997): Remove this once we know where the issue is coming
+  // from.
+  DUMP_WILL_BE_CHECK(!result_image || (result_image.size.width != 0 &&
+                                       result_image.size.height != 0));
+
+  return result_image;
 }
 
 UIImage* ResizeImageForSearchByImage(UIImage* image) {
