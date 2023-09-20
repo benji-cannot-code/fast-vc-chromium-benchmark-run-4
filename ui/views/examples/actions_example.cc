4cb114d60dd14fffb8f062c640271d82d462feb5
Allen Bauer <kylixrd@chromium.org>
2023-09-20 16:09:17 +0000

    Add observer to ViewsComboboxModel to fix dangling pointer.
    Change-Id: I63678afb4fdd2726dad14528fa7fa627fdbf731c
    Bug: 1480488
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4878607
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Auto-Submit: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1199045}

--- ui/views/examples/actions_example.cc
+++ ui/views/examples/actions_example.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/fixed_flat_map.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
+#include "base/scoped_observation.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -45,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/style/typography.h"
 #include "ui/views/view.h"
 #include "ui/views/view_class_properties.h"
+#include "ui/views/view_observer.h"
 #include "ui/views/view_utils.h"
 
 namespace views::examples {
@@ -248,7 +250,7 @@ constexpr auto kActionIdStrings =
 #include "ui/actions/action_id_macros.inc"  // NOLINT(build/include)
 #undef STRINGIZE_ACTION_IDS
 
-class ViewsComboboxModel : public ui::ComboboxModel {
+class ViewsComboboxModel : public ui::ComboboxModel, public ViewObserver {
  public:
   explicit ViewsComboboxModel(View* container);
   ViewsComboboxModel(const ViewsComboboxModel&) = delete;
@@ -262,12 +264,19 @@ class ViewsComboboxModel : public ui::ComboboxModel {
 
   View* GetViewItemAt(size_t index) const;
 
+ protected:
+  // ViewObserver overrides
+  void OnViewIsDeleting(View* observed_view) override;
+
  private:
   raw_ptr<View> container_;
+  base::ScopedObservation<View, ViewObserver> container_observation_{this};
 };
 
 ViewsComboboxModel::ViewsComboboxModel(View* container)
-    : container_(container) {}
+    : container_(container) {
+  container_observation_.Observe(container_);
+}
 
 size_t ViewsComboboxModel::GetItemCount() const {
   size_t size = container_->children().size();
@@ -296,6 +305,13 @@ View* ViewsComboboxModel::GetViewItemAt(size_t index) const {
     return container_->children()[index];
   }
   return nullptr;
+}
+
+void ViewsComboboxModel::OnViewIsDeleting(View* observed_view) {
+  if (observed_view == container_.get()) {
+    container_observation_.Reset();
+    container_ = nullptr;
+  }
 }
 
 class ActionItemComboboxModel : public ui::ComboboxModel {
