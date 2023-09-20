3147bdaf6d3f4f902b8a3edc695328bfc6461d90
Hong Xu <xuhong@chromium.org>
2023-09-20 22:54:21 +0000

    Do not destruct kTopLevelKeyAllowList
    Destructing a static object risks accessing destroyed objects.
    
    Also some minor efficiency improvement:
    
    - Use base::flat_set instead of std::set, because this is a small
      constant set.
    - Returns a reference instead of a copy for more efficient access.
    
    Bug=None
    
    Change-Id: Idcf2914f9eb1d14465ded6d4af750da3b9ab717f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4879983
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Hong Xu <xuhong@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199291}

--- components/policy/core/common/cloud/encrypted_reporting_job_configuration.cc
+++ components/policy/core/common/cloud/encrypted_reporting_job_configuration.cc
@@ -5,9 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/policy/core/common/cloud/encrypted_reporting_job_configuration.h"
 
+#include <initializer_list>
+#include <string>
 #include <utility>
 
 #include "base/containers/contains.h"
+#include "base/containers/flat_set.h"
 #include "base/no_destructor.h"
 #include "base/strings/string_number_conversions.h"
 #include "components/reporting/proto/synced/record_constants.pb.h"
@@ -321,17 +324,15 @@ std::string EncryptedReportingJobConfiguration::GetUmaString() const {
   return "Browser.ERP.Unmanaged";
 }
 
-std::set<std::string>
+// static
+const base::flat_set<std::string>&
 EncryptedReportingJobConfiguration::GetTopLevelKeyAllowList() {
-  static std::set<std::string> kTopLevelKeyAllowList{
-      kAttachConfigurationFileKey,
-      kAttachEncryptionSettingsKey,
-      kBrowserKey,
-      kDeviceKey,
-      kEncryptedRecordListKey,
-      kRequestId,
-      kSourceKey};
-  return kTopLevelKeyAllowList;
+  static const base::NoDestructor<base::flat_set<std::string>>
+      kTopLevelKeyAllowList{std::initializer_list<std::string>{
+          kAttachConfigurationFileKey, kAttachEncryptionSettingsKey,
+          kBrowserKey, kDeviceKey, kEncryptedRecordListKey, kRequestId,
+          kSourceKey}};
+  return *kTopLevelKeyAllowList;
 }
 
 // static
