8666c03bf81525dcbd9291f0d70cf30db6974c68
Robert Liao <robliao@chromium.org>
2023-09-20 22:02:21 +0000

    Set the NSApp Application Policy to Regular
    This allows the examples app to appear on the dock and allows it
    to receive keyboard events.
    
    Change-Id: Ia3a5bc63fa23a54506a4101f892ffc151a31ba96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4874165
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199255}

--- ui/webui/examples/app/mac_init.mm
+++ ui/webui/examples/app/mac_init.mm
@@ -54,6 +54,7 @@ namespace webui_examples {
 void MacPreBrowserMain() {
   CHECK_EQ(NSApp, nil);
   [ExamplesApplication sharedApplication];
+  [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
 }
 
 }  // namespace webui_examples
