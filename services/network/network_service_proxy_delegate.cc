4bda589f31043a6fab7ce5b42f7d6df7b89fb690
Dustin J. Mitchell <djmitche@chromium.org>
2023-09-20 13:03:24 +0000

    Use base::Contains instead of for loop
    As suggested by ricea@chromium.org in
    https://chromium-review.googlesource.com/c/chromium/src/+/4852391/comment/16482248_a44d9204/
    
    R=ricea@chromium.org
    
    Change-Id: I63f96a8d6245fde8e1f744dda78ec6852471d438
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4873668
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198935}

--- services/network/network_service_proxy_delegate.cc
+++ services/network/network_service_proxy_delegate.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/network/network_service_proxy_delegate.h"
 #include "base/base64.h"
+#include "base/containers/contains.h"
 #include "base/functional/bind.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/strings/strcat.h"
@@ -327,13 +328,7 @@ bool NetworkServiceProxyDelegate::IsProxyForIpProtection(
   // This list will typically be quite short (2-3), so linear search is
   // adequate.
   std::string proxy_server_host = proxy_server.GetHost();
-  for (auto& list_host : ipp_config_cache_->ProxyList()) {
-    if (list_host == proxy_server_host) {
-      return true;
-    }
-  }
-
-  return false;
+  return base::Contains(ipp_config_cache_->ProxyList(), proxy_server_host);
 }
 
 bool NetworkServiceProxyDelegate::EligibleForProxy(
