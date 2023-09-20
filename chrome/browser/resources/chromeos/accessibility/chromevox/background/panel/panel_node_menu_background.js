02d0b0f2fd8d8cb9408e58353662e63c45935027
Anastasia Helfinstein <anastasi@google.com>
2023-09-20 15:11:50 +0000

    [ChromeVox] Add method PanelBackground.waitForMenusLoaded
    Will be used to reduce flakiness of some tests in a follow-up change.
    
    Skipping test validation because there seems to be an unrelated flake in  first_party_sets_database.
    
    AX-Relnotes: n/a.
    Bug: b/265481751
    Test: New test ChromeVoxPanelBackgroundTest.WaitForMenusLoaded
    Validate-Test-Flakiness: skip
    Change-Id: I7270f8193fea1ff31c7dc146b54d76d35bf67e95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4834695
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1199004}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/panel/panel_node_menu_background.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/panel/panel_node_menu_background.js
@@ -46,6 +46,15 @@ export class PanelNodeMenuBackground {
     this.nodeCount_ = 0;
     /** @private {boolean} */
     this.isEmpty_ = true;
+    /** @private {function()} */
+    this.onFinish_;
+    /** @private {!Promise} */
+    this.finishPromise_ = new Promise(resolve => this.onFinish_ = resolve);
+  }
+
+  /** @return {!Promise} */
+  waitForFinish() {
+    return this.finishPromise_;
   }
 
   /**
@@ -129,6 +138,7 @@ export class PanelNodeMenuBackground {
         menuId: this.menuId_,
       });
     }
+    this.onFinish_();
   }
 
   /**
