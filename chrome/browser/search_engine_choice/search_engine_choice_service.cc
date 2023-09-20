22455954a2ce41e21e8febbfd4cc000005f33839
Jack Yammine <jyammine@google.com>
2023-09-20 09:36:27 +0000

    [waffle]Move the privacy sandbox dialog to the next run
    Move the Privacy Sandbox dialog to the next run of Chrome if the
    Search Engine Choice dialog will be displayed in the current run.
    
    Fixed: b/298030307
    Change-Id: Ie98339d7c242429c5f2bb65daf35ab2a2a98d65c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4874261
    Reviewed-by: Jens Mueller <muellerj@google.com>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Cr-Commit-Position: refs/heads/main@{#1198890}

--- chrome/browser/search_engine_choice/search_engine_choice_service.cc
+++ chrome/browser/search_engine_choice/search_engine_choice_service.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/startup/first_run_service.h"
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
 #include "components/prefs/pref_service.h"
 #include "components/search_engines/search_engine_choice_utils.h"
@@ -72,6 +73,15 @@ void SearchEngineChoiceService::NotifyChoiceMade(int prepopulate_id) {
 
     CHECK_EQ(default_search_provider->prepopulate_id(), 0);
   }
+
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
+  // Log that the choice was made during the FRE.
+  if (FirstRunService* first_run_service =
+          FirstRunServiceFactory::GetForBrowserContextIfExists(
+              &profile_.get())) {
+    choice_made_in_fre_ = true;
+  }
+#endif
 
   // Closes the dialogs that are open on other browser windows that
   // have the same profile as the one on which the choice was made.
@@ -151,6 +161,10 @@ bool SearchEngineChoiceService::HasUserMadeChoice() const {
   PrefService* pref_service = profile_->GetPrefs();
   return pref_service->GetInt64(
       prefs::kDefaultSearchProviderChoiceScreenCompletionTimestamp);
+}
+
+bool SearchEngineChoiceService::WasChoiceMadeInFRE() const {
+  return choice_made_in_fre_;
 }
 
 bool SearchEngineChoiceService::HasPendingDialog(Browser& browser) {
