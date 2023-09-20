24d5721e6c40b3bfbf9e4e8cd3ad916a2d527c2e
Dan Sanders <sandersd@chromium.org>
2023-09-20 20:01:08 +0000

    [media] Support alpha decoding in VTVD HEVC
    This change includes:
      - Parsing VPS extensions to determine whether there is an alpha layer.
      - Adding a mechanism to pass non-base layers through from H265Decoder.
      - Actual support for alpha decoding in VTVD HEVC.
      - A replacement of VTVD HEVC parameter set logic to account for active
        parameter sets on each layer.
    
    Bug: 1331597
    Change-Id: Ic0755d63d65115d8fbf4c5acec93075e642b0dd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4863237
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199195}

--- media/gpu/mac/video_toolbox_decode_metadata.h
+++ media/gpu/mac/video_toolbox_decode_metadata.h
@@ -22,6 +22,7 @@ namespace media {
 struct MEDIA_GPU_EXPORT VideoToolboxSessionMetadata {
   bool allow_software_decoding = false;
   bool is_hbd = false;
+  bool has_alpha = false;
 };
 
 struct MEDIA_GPU_EXPORT VideoToolboxDecodeMetadata {
