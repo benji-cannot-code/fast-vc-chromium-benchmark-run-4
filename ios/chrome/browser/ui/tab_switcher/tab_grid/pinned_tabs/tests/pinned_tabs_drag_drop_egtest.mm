149f35224bfdbe09030616152bd93a8d713dfdd2
Louis Romero <lpromero@google.com>
2023-09-20 14:32:05 +0000

    Remove unnecessary `using` in test file
    This `using` is not used.
    
    Change-Id: I36d6548a82125ac8f85eecf34c24eda950be2bbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4874413
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1198980}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/pinned_tabs/tests/pinned_tabs_drag_drop_egtest.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/pinned_tabs/tests/pinned_tabs_drag_drop_egtest.mm
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using ::base::test::ios::kWaitForUIElementTimeout;
 using ::base::test::ios::WaitUntilConditionOrTimeout;
-using chrome_test_util::LongPressCellAndDragToOffsetOf;
 
 namespace {
 
