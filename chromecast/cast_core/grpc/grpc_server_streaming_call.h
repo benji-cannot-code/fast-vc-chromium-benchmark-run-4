b6c77401d2718e946c0407228800254186a821dd
Yuki Shiino <yukishiino@chromium.org>
2023-09-20 16:09:09 +0000

    raw_ptr: Remove unnecessary call to .get() for PMF
    As a follow-up for https://crrev.com/c/4866059 , remove unnecessary
    calls to .get() when invoking a member function.
    
    Change-Id: Ia0c3ee1d9ebb6d2ea8dad2d37c215bc7420d76c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4876158
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Vigen Issahhanjan <vigeni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1199044}

--- chromecast/cast_core/grpc/grpc_server_streaming_call.h
+++ chromecast/cast_core/grpc/grpc_server_streaming_call.h
@@ -89,7 +89,7 @@ class GrpcServerStreamingCall : public GrpcCall<TGrpcStub, TRequest> {
 
     void Start() override {
       ReactorBase::Start();
-      (async_interface_.get()->*AsyncMethodPtr)(context(), request(), this);
+      (async_interface_->*AsyncMethodPtr)(context(), request(), this);
       grpc::ClientReadReactor<Response>::StartRead(&response_);
       grpc::ClientReadReactor<Response>::StartCall();
     }
