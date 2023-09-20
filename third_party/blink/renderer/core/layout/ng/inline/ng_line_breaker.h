ed8a8a024d7151a5ff3ceacbd83541237acc912a
Morten Stenshorne <mstensho@chromium.org>
2023-09-20 06:13:30 +0000

    Split up NGLineBreaker::HandleFloat().
    Move out the code that determines whether the float should go below the
    current line. This is in preparation for improved block fragmentation of
    floats.
    
    Change-Id: Ia93cf708d98c049b8ef30445ff6e00873eaefbad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4876681
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198816}

--- third_party/blink/renderer/core/layout/ng/inline/ng_line_breaker.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_line_breaker.h
@@ -218,8 +218,10 @@ class CORE_EXPORT NGLineBreaker {
   const NGInlineItem* TryGetAtomicInlineItemAfter(
       const NGInlineItem& item) const;
 
+  bool ShouldPushFloatAfterLine(NGUnpositionedFloat*, NGLineInfo*);
   void HandleFloat(const NGInlineItem&,
                    NGLineInfo*);
+
   void HandleInitialLetter(const NGInlineItem&, NGLineInfo*);
   void HandleOutOfFlowPositioned(const NGInlineItem&, NGLineInfo*);
 
