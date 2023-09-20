89c223b8968f82b2cdcb518ceadd89a6c85bc536
Jose Dapena Paz <jdapena@igalia.com>
2023-09-20 08:11:55 +0000

    IWYU: missing include for size_t usage in cxx20_erase_internal.h
    Bug: 957519
    Change-Id: I8776ed557515b3abf19f39c6a0bc8ec74d53cbc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4871497
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1198855}

--- base/containers/cxx20_erase_internal.h
+++ base/containers/cxx20_erase_internal.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_CONTAINERS_CXX20_ERASE_INTERNAL_H_
 #define BASE_CONTAINERS_CXX20_ERASE_INTERNAL_H_
 
+#include <cstddef>
+
 // Internal portion of base/containers/cxx20_erase_*.h. Please include those
 // headers instead of including this directly.
 
