49b8a8a04d6f4399a5a4f6456ca362c191efaacc
Fredrik Söderquist <fs@opera.com>
2023-09-20 19:29:43 +0000

    [RemoveLegacy] Fold TextPainterBase::PaintEmphasisMarkForCombinedText
    Into the only caller (NGTextCombinePainter::PaintEmphasisMark). Drop the
    unused Document argument.
    
    Bug: 1229581
    Change-Id: Ie1e0a7a5d83cd616cf5e3ba21515373c3351a619
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4876659
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1199169}

--- third_party/blink/renderer/core/paint/ng/ng_text_combine_painter.h
+++ third_party/blink/renderer/core/paint/ng/ng_text_combine_painter.h
@@ -7,12 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_PAINT_NG_NG_TEXT_COMBINE_PAINTER_H_
 
 #include "third_party/blink/renderer/core/paint/ng/ng_text_painter_base.h"
-#include "third_party/blink/renderer/platform/graphics/dom_node_id.h"
 
 namespace blink {
 
 class ComputedStyle;
-class Document;
 class LayoutNGTextCombine;
 
 // The painter for painting text decorations and emphasis marks for
@@ -39,9 +37,13 @@ class NGTextCombinePainter final : public NGTextPainterBase {
  private:
   void PaintDecorations(const PaintInfo& paint_info,
                         const TextPaintStyle& text_style);
+  // Paints emphasis mark as for ideographic full stop character. Callers of
+  // this function should rotate canvas to paint emphasis mark at left/right
+  // side instead of top/bottom side.
+  // `emphasis_mark_font` is used for painting emphasis mark because `font_`
+  // may be compressed font (width variants).
   void PaintEmphasisMark(const TextPaintStyle& text_style,
-                         const Font& emphasis_mark_font,
-                         const Document& document);
+                         const Font& emphasis_mark_font);
 
   const ComputedStyle& style_;
 };
