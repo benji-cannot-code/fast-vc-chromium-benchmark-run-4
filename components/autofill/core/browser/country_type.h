0b0129bc8e300391725670e71fdfa7e834af0997
Norge Vizcay <vizcay@google.com>
2023-09-20 09:40:15 +0000

    [AutofillDataModel] Introduce AutofillAddressCountryCode StrongAlias.
    The CL introduces a StrongAlias (AutofillAddressCountryCode) to
    distinguish country code specified in address profiles from others.
    
    Bug: 1464568
    Change-Id: I30ba786b3b377cd1bbe65800b245cdd04698e228
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4875085
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1198891}

--- components/autofill/core/browser/country_type.h
+++ components/autofill/core/browser/country_type.h
@@ -17,4 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // estimated by the variations::VariationsService.
 using GeoIpCountryCode = base::StrongAlias<class GeoIpCountryTag, std::string>;
 
+// Country code in the format of uppercase ISO 3166-1 alpha-2. Example: US, BR,
+// IN. Empty if unknown.
+// StrongAlias to distinguish countries specified in address profiles.
+using AddressCountryCode =
+    base::StrongAlias<class AutofillAddressCountryTag, std::string>;
+
 #endif  // COMPONENTS_AUTOFILL_CORE_BROWSER_COUNTRY_TYPE_H_
