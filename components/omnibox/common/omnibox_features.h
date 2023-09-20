ee808648cc2e8834b839bdac984c981707c651d6
manukh <manukh@chromium.org>
2023-09-20 18:41:00 +0000

    [omnibox][perf] Cleanup.
    Launched in m117 with crrev.com/c/4674135
    
    Cleans up feature code for
    - `kRedoCurrentMatch`
    - `kRevertModelBeforeClosingPopup`
    - `kUseExistingAutocompleteClient`
    
    Bug: 806262, 1418490
    Change-Id: Ic67f917ec2b0e4394aa2299203f8e0059dd216f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4866194
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199129}

--- components/omnibox/common/omnibox_features.h
+++ components/omnibox/common/omnibox_features.h
@@ -115,10 +115,6 @@ BASE_DECLARE_FEATURE(kOmniboxSteadyStateTextStyle);
 
 BASE_DECLARE_FEATURE(kOmniboxSteadyStateTextColor);
 
-BASE_DECLARE_FEATURE(kRedoCurrentMatch);
-BASE_DECLARE_FEATURE(kRevertModelBeforeClosingPopup);
-BASE_DECLARE_FEATURE(kUseExistingAutocompleteClient);
-
 // Omnibox & Suggestions UI - these affect both the omnibox and the suggestions
 // popup.
 BASE_DECLARE_FEATURE(kOmniboxModernizeVisualUpdate);
