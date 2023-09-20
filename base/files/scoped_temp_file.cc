ccbb20723019f03738c1b38433a55e3c32fd3e33
Tom Anderson <thomasanderson@chromium.org>
2023-09-20 19:49:22 +0000

    Add base::ScopedTempFile
    ScopedTempFile aims to replace ad-hoc classes that duplicate this
    functionality such as system_media_controls::internal::ScopedFile [1]
    and ResourceFile [2].
    
    Some alternatives were considered:
    * Reuse ScopedTempDir
      - Have ScopedTempDir handle files too.  Surprisingly, the only code
        that's reusable would be Take() and GetPath().  Everything else
        needs conditional logic depending on if a file or directory is
        used.  See [3] for my attempt at this.  Additionally,
        ScopedTempDir would need to be renamed to something like
        ScopedTempPath, and ScopedTempDir is currently used in 1,524 files.
    * Don't add ScopedTempFile
      - Use ScopedTempDir instead.  This isn't ideal due to the additional
        system call overhead.
      - Live with the status quo.  Not ideal due to duplication.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:components/system_media_controls/linux/system_media_controls_linux.h;drc=aaa45ce5f47f93e9cfc70a68d32cbab17cc3cf40;l=50
    [2] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/notifications/notification_platform_bridge_linux.cc;drc=d78ab264fc6d9d013047f9d7bf1beafb00c02ebd;l=241
    [3] https://chromium-review.googlesource.com/c/chromium/src/+/4877621
    
    R=thestig
    
    Change-Id: I7831a41ee1c2eb65485db31ee4f4ee004f267f41
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4877813
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199182}

---
+++ base/files/scoped_temp_file.cc
@@ -0,0 +1,54 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/files/scoped_temp_file.h"
+
+#include <utility>
+
+#include "base/check.h"
+#include "base/files/file_util.h"
+#include "base/logging.h"
+
+namespace base {
+
+ScopedTempFile::ScopedTempFile() = default;
+
+ScopedTempFile::ScopedTempFile(ScopedTempFile&& other) noexcept
+    : path_(std::move(other.path_)) {}
+
+ScopedTempFile& ScopedTempFile::operator=(ScopedTempFile&& other) noexcept {
+  if (!path_.empty()) {
+    CHECK_NE(path_, other.path_);
+  }
+  if (!Delete()) {
+    DLOG(WARNING) << "Could not delete temp dir in operator=().";
+  }
+  path_ = std::move(other.path_);
+  return *this;
+}
+
+ScopedTempFile::~ScopedTempFile() {
+  if (!Delete()) {
+    DLOG(WARNING) << "Could not delete temp dir in destructor.";
+  }
+}
+
+bool ScopedTempFile::Create() {
+  CHECK(path_.empty());
+  return base::CreateTemporaryFile(&path_);
+}
+
+bool ScopedTempFile::Delete() {
+  if (path_.empty()) {
+    return true;
+  }
+  if (DeleteFile(path_)) {
+    path_.clear();
+    return true;
+  }
+  return false;
+}
+
+}  // namespace base
