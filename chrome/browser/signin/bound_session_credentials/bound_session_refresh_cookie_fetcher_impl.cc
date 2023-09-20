a6d0299808410d15d7543f152b6a0e1d2b5ed745
Alex Ilin <alexilin@chromium.org>
2023-09-20 08:01:58 +0000

    [DBSC] Add trace events to c/b/signin/bound_session_credentials/
    This CL adds events for tracing individual registration fetches and
    cookie refresh fetches.
    
    Bug: b/296999000
    Change-Id: I6c5b3b41bbe074e440e9b0e8067d5c460d16e6fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4872723
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198852}

--- chrome/browser/signin/bound_session_credentials/bound_session_refresh_cookie_fetcher_impl.cc
+++ chrome/browser/signin/bound_session_credentials/bound_session_refresh_cookie_fetcher_impl.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/time/time.h"
 #include "base/timer/elapsed_timer.h"
+#include "base/trace_event/typed_macros.h"
 #include "chrome/browser/signin/bound_session_credentials/session_binding_helper.h"
 #include "components/signin/public/base/wait_for_network_callback_helper.h"
 #include "google_apis/gaia/gaia_urls.h"
@@ -66,6 +67,9 @@ BoundSessionRefreshCookieFetcherImpl::~BoundSessionRefreshCookieFetcherImpl() =
 
 void BoundSessionRefreshCookieFetcherImpl::Start(
     RefreshCookieCompleteCallback callback) {
+  TRACE_EVENT("browser", "BoundSessionRefreshCookieFetcherImpl::Start",
+              perfetto::Flow::FromPointer(this), "url",
+              expected_cookie_domain_);
   CHECK(!callback_);
   CHECK(callback);
   callback_ = std::move(callback);
@@ -76,6 +80,10 @@ void BoundSessionRefreshCookieFetcherImpl::Start(
 
 void BoundSessionRefreshCookieFetcherImpl::StartRefreshRequest(
     absl::optional<std::string> sec_session_challenge_response) {
+  TRACE_EVENT("browser",
+              "BoundSessionRefreshCookieFetcherImpl::StartRefreshRequest",
+              perfetto::Flow::FromPointer(this), "has_challenge",
+              sec_session_challenge_response.has_value());
   if (!cookie_refresh_duration_.has_value()) {
     cookie_refresh_duration_ = base::TimeTicks::Now();
   }
@@ -157,6 +165,9 @@ void BoundSessionRefreshCookieFetcherImpl::StartRefreshRequest(
 void BoundSessionRefreshCookieFetcherImpl::OnURLLoaderComplete(
     scoped_refptr<net::HttpResponseHeaders> headers) {
   net::Error net_error = static_cast<net::Error>(url_loader_->NetError());
+  TRACE_EVENT("browser",
+              "BoundSessionRefreshCookieFetcherImpl::OnURLLoaderComplete",
+              perfetto::Flow::FromPointer(this), "net_error", net_error);
 
   absl::optional<std::string> challenge_header_value =
       GetChallengeIfBindingKeyAssertionRequired(headers);
@@ -222,6 +233,9 @@ void BoundSessionRefreshCookieFetcherImpl::ReportRefreshResult() {
   if (result_ == Result::kSuccess && !expected_cookies_set_) {
     result_ = Result::kServerUnexepectedResponse;
   }
+  TRACE_EVENT("browser",
+              "BoundSessionRefreshCookieFetcherImpl::ReportRefreshResult",
+              perfetto::TerminatingFlow::FromPointer(this), "result", result_);
   base::UmaHistogramEnumeration(
       "Signin.BoundSessionCredentials.CookieRotationResult", result_);
 
@@ -295,6 +309,9 @@ void BoundSessionRefreshCookieFetcherImpl::
 
 void BoundSessionRefreshCookieFetcherImpl::RefreshWithChallenge(
     const std::string& challenge) {
+  TRACE_EVENT("browser",
+              "BoundSessionRefreshCookieFetcherImpl::RefreshWithChallenge",
+              perfetto::Flow::FromPointer(this));
   session_binding_helper_->GenerateBindingKeyAssertion(
       challenge, GaiaUrls::GetInstance()->rotate_bound_cookies_url(),
       base::BindOnce(
@@ -308,6 +325,10 @@ void BoundSessionRefreshCookieFetcherImpl::OnGenerateBindingKeyAssertion(
   base::UmaHistogramMediumTimes(
       "Signin.BoundSessionCredentials.CookieRotationGenerateAssertionDuration",
       generate_assertion_timer.Elapsed());
+  TRACE_EVENT(
+      "browser",
+      "BoundSessionRefreshCookieFetcherImpl::OnGenerateBindingKeyAssertion",
+      perfetto::Flow::FromPointer(this), "success", !assertion.empty());
 
   if (assertion.empty()) {
     CompleteRequestAndReportRefreshResult(Result::kSignChallengeFailed);
@@ -321,6 +342,7 @@ void BoundSessionRefreshCookieFetcherImpl::OnGenerateBindingKeyAssertion(
 
 void BoundSessionRefreshCookieFetcherImpl::OnCookiesAccessed(
     std::vector<network::mojom::CookieAccessDetailsPtr> details_vector) {
+  // TODO(b/296999000): record a trace event.
   for (const auto& cookie_details : details_vector) {
     if (cookie_details->type !=
         network::mojom::CookieAccessDetails::Type::kChange) {
--- chrome/browser/signin/bound_session_credentials/bound_session_registration_fetcher_impl.cc
+++ chrome/browser/signin/bound_session_credentials/bound_session_registration_fetcher_impl.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/trace_event/typed_macros.h"
 #include "base/values.h"
 #include "chrome/browser/signin/bound_session_credentials/bound_session_params.pb.h"
 #include "chrome/browser/signin/bound_session_credentials/bound_session_params_storage.h"
@@ -58,6 +59,9 @@ BoundSessionRegistrationFetcherImpl::~BoundSessionRegistrationFetcherImpl() =
 
 void BoundSessionRegistrationFetcherImpl::Start(
     RegistrationCompleteCallback callback) {
+  TRACE_EVENT("browser", "BoundSessionRegistrationFetcherImpl::Start",
+              perfetto::Flow::FromPointer(this), "endpoint",
+              registration_params_.RegistrationEndpoint());
   callback_ = std::move(callback);
   // base::Unretained() is safe since `this` owns
   // `registration_token_helper_`.
@@ -74,6 +78,9 @@ void BoundSessionRegistrationFetcherImpl::OnURLLoaderComplete(
     std::unique_ptr<std::string> response_body) {
   const network::mojom::URLResponseHead* head = url_loader_->ResponseInfo();
   net::Error net_error = static_cast<net::Error>(url_loader_->NetError());
+  TRACE_EVENT("browser",
+              "BoundSessionRegistrationFetcherImpl::OnURLLoaderComplete",
+              perfetto::Flow::FromPointer(this), "net_error", net_error);
 
   absl::optional<int> http_response_code;
   if (head && head->headers) {
@@ -144,6 +151,9 @@ void BoundSessionRegistrationFetcherImpl::OnURLLoaderComplete(
 
 void BoundSessionRegistrationFetcherImpl::OnRegistrationTokenCreated(
     absl::optional<RegistrationTokenHelper::Result> result) {
+  TRACE_EVENT("browser",
+              "BoundSessionRegistrationFetcherImpl::OnRegistrationTokenCreated",
+              perfetto::Flow::FromPointer(this), "success", result.has_value());
   if (!result.has_value()) {
     RunCallbackAndRecordMetrics(
         base::unexpected(RegistrationError::kGenerateRegistrationTokenFailed));
@@ -229,6 +239,10 @@ void BoundSessionRegistrationFetcherImpl::RunCallbackAndRecordMetrics(
 
   RegistrationError error_for_metrics =
       params_or_error.error_or(RegistrationError::kNone);
+  TRACE_EVENT(
+      "browser",
+      "BoundSessionRegistrationFetcherImpl::RunCallbackAndRecordMetrics",
+      perfetto::TerminatingFlow::FromPointer(this), "error", error_for_metrics);
   base::UmaHistogramEnumeration(
       "Signin.BoundSessionCredentials.SessionRegistrationResult",
       error_for_metrics);
