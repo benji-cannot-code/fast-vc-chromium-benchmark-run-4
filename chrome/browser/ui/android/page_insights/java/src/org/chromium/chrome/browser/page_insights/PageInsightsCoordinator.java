2be812af3f585794488f65eba3e4da4d0725a999
Emilia Palaghita <pemilia@google.com>
2023-09-21 18:32:38 +0000

    [gbb-cct] Add UMA histogram logging for Page Insights Hub.
    Bug: b/293448253
    Change-Id: I5f8190e5740692d6e5e42f9a22b3642751b0e3ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4863571
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Emilia Palaghita <pemilia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1199743}

--- chrome/browser/ui/android/page_insights/java/src/org/chromium/chrome/browser/page_insights/PageInsightsCoordinator.java
+++ chrome/browser/ui/android/page_insights/java/src/org/chromium/chrome/browser/page_insights/PageInsightsCoordinator.java
@@ -80,7 +80,7 @@ public class PageInsightsCoordinator {
      * Launch PageInsights hub in bottom sheet container and fetch the data to show.
      */
     public void launch() {
-        mMediator.openInExpandedState();
+        mMediator.launch();
     }
 
     /**
