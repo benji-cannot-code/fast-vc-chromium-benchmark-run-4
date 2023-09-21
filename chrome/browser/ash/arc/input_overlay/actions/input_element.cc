b3006ce1082e7ee8f65a6142f3ab664290ed13c1
Cici Ruan <cuicuiruan@google.com>
2023-09-21 01:09:37 +0000

    arc/gio: Set mapping hint invisible if not bound
    This CL updates the logic of checking whether the InputElement is
    unbound. If it binds at least one key, it is considered as bound.
    
    Bug: b/301027630
    Test: manual test
    Test: unit_tests --gtest_filter=*InputElement*
    Change-Id: Ia6799afdaf67574c7d1786bfbd016fb0a3d40d4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4876433
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Auto-Submit: Cici Ruan <cuicuiruan@google.com>
    Commit-Queue: David Jacobo <djacobo@chromium.org>
    Reviewed-by: Joseph Pang <pjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1199346}

--- chrome/browser/ash/arc/input_overlay/actions/input_element.cc
+++ chrome/browser/ash/arc/input_overlay/actions/input_element.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ranges/algorithm.h"
 #include "chrome/browser/ash/arc/input_overlay/util.h"
 #include "ui/events/event_constants.h"
+#include "ui/events/keycodes/dom/dom_code.h"
 #include "ui/events/keycodes/dom/keycode_converter.h"
 
 namespace arc::input_overlay {
@@ -132,6 +133,27 @@ bool InputElement::IsOverlapped(const InputElement& input_element) const {
   return mouse_action_ == input_element.mouse_action();
 }
 
+bool InputElement::IsUnbound() const {
+  if (input_sources_ == InputSource::IS_NONE) {
+    return true;
+  }
+
+  if (IsInputSourceSet(InputSource::IS_KEYBOARD)) {
+    for (const auto key : keys()) {
+      if (key != ui::DomCode::NONE) {
+        // Consider it as bound if there is at lease one key is bound.
+        return false;
+      }
+    }
+  }
+
+  if (IsInputSourceSet(InputSource::IS_MOUSE)) {
+    return mouse_action_ == MouseAction::NONE;
+  }
+
+  return true;
+}
+
 void InputElement::SetKey(size_t index, ui::DomCode code) {
   DCHECK(index < keys_.size());
   if (index >= keys_.size()) {
@@ -175,6 +197,10 @@ bool InputElement::operator==(const InputElement& other) const {
 
 bool InputElement::operator!=(const InputElement& other) const {
   return !(*this == other);
+}
+
+bool InputElement::IsInputSourceSet(InputSource input_source) const {
+  return (input_sources_ & input_source) == input_source;
 }
 
 }  // namespace arc::input_overlay
