6c1b286118041e8ad0191f1f7449ce66163f9c55
Koji Ishii <kojii@chromium.org>
2023-09-21 16:24:06 +0000

    [text-autospace] Fix line breaking when thre are auto-spacing
    crrev.com/c/4860139 added `has_auto_spacing_after` to
    `ShapeResultCharacterData`. To set this bit,
    `ApplyTextAutoSpacing` calls `EnsurePositionData`.
    
    `EnsurePositionData` also caches the position data, which
    `ApplyTextAutoSpacing` changes, so the cached position data
    was before applying the auto-spacing.
    
    This patch recalculate the position data after applying the
    auto-spacing. To do this, this patch also changes
    `ComputePositionData` to update cached data only, without
    resetting `has_auto_spacing_after`.
    
    This patch fixes the following test when the auto-spacing
    is enabled:
      virtual/text-antialias/international/wrap-CJK-001.html
    
    Bug: 1463890
    Change-Id: I9c213cb8696f1cd51eb845bb4b1974b4f6925084
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4881881
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199657}

--- third_party/blink/renderer/platform/fonts/shaping/shape_result.cc
+++ third_party/blink/renderer/platform/fonts/shaping/shape_result.cc
@@ -1002,6 +1002,7 @@ void ShapeResult::ApplyTextAutoSpacing(
     ApplyTextAutoSpacingCore<TextDirection::kRtl>(offsets_with_spacing.rbegin(),
                                                   offsets_with_spacing.rend());
   }
+  RecalcCharacterPositions();
 }
 
 template <TextDirection direction, class Iterator>
@@ -1884,12 +1885,12 @@ void ShapeResult::ComputePositionData() const {
           float x_position = !rtl ? last_x_position : total_advance;
           for (unsigned i = next_character_index; i < character_index; i++) {
             DCHECK_LT(i, num_characters_);
-            data[i] = {x_position, false, false};
+            data[i].SetCachedData(x_position, false, false);
           }
         }
 
-        data[character_index] = {total_advance, true,
-                                 glyph_data.safe_to_break_before};
+        data[character_index].SetCachedData(total_advance, true,
+                                            glyph_data.safe_to_break_before);
         last_x_position = total_advance;
       }
 
@@ -1904,7 +1905,7 @@ void ShapeResult::ComputePositionData() const {
   if (next_character_index < num_characters_) {
     float x_position = !rtl ? last_x_position : run_advance;
     for (unsigned i = next_character_index; i < num_characters_; i++) {
-      data[i] = {x_position, false, false};
+      data[i].SetCachedData(x_position, false, false);
     }
   }
 
@@ -1917,10 +1918,16 @@ void ShapeResult::EnsurePositionData() const {
 
   character_position_ =
       std::make_unique<CharacterPositionData>(num_characters_, width_);
-  if (Direction() == TextDirection::kLtr)
+  RecalcCharacterPositions();
+}
+
+void ShapeResult::RecalcCharacterPositions() const {
+  DCHECK(character_position_);
+  if (IsLtr()) {
     ComputePositionData<false>();
-  else
+  } else {
     ComputePositionData<true>();
+  }
 }
 
 unsigned ShapeResult::CachedOffsetForPosition(float x) const {
