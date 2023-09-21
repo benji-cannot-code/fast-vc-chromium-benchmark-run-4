2a885a214898cf4eed7cd200af437e955640da87
Dmitry Grebenyuk <dgrebenyuk@google.com>
2023-09-21 05:34:23 +0000

    Update schema for file name search
    For enabling fine name search, we need to update the DB schema.
    
    Test: "passes unittests. Works on DUT."
    Bug: b:301347425
    Change-Id: I7f5cec20859da03cce1a9e7750d4b1163a0aeafc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4881738
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Reviewed-by: CJ Huang <chenjih@google.com>
    Commit-Queue: Dmitry Grebenyuk <dgrebenyuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1199428}

--- chrome/browser/ash/app_list/search/local_image_search/annotation_storage_unittest.cc
+++ chrome/browser/ash/app_list/search/local_image_search/annotation_storage_unittest.cc
@@ -65,7 +65,8 @@ TEST_F(AnnotationStorageTest, Insert) {
   task_environment_.RunUntilIdle();
 
   auto time = base::Time::Now();
-  ImageInfo bar_image({"test"}, test_directory_.AppendASCII("bar.jpg"), time);
+  ImageInfo bar_image({"test"}, test_directory_.AppendASCII("bar.jpg"), time,
+                      /*file_size=*/1);
 
   storage_->Insert(bar_image);
 
@@ -74,7 +75,7 @@ TEST_F(AnnotationStorageTest, Insert) {
   task_environment_.RunUntilIdle();
 
   ImageInfo bar_image1({"test1"}, test_directory_.AppendASCII("bar.jpg"),
-                       std::move(time));
+                       std::move(time), 1);
 
   storage_->Insert(bar_image1);
 
@@ -83,7 +84,7 @@ TEST_F(AnnotationStorageTest, Insert) {
   task_environment_.RunUntilIdle();
 
   ImageInfo foo_image({"test1"}, test_directory_.AppendASCII("foo.png"),
-                      base::Time::Now());
+                      base::Time::Now(), 2);
 
   storage_->Insert(foo_image);
 
@@ -99,10 +100,11 @@ TEST_F(AnnotationStorageTest, Remove) {
 
   auto time = base::Time::Now();
   ImageInfo bar_image({"test"}, test_directory_.AppendASCII("bar.jpg"),
-                      base::Time::Now());
-  ImageInfo foo_image({"test"}, test_directory_.AppendASCII("foo.png"), time);
+                      base::Time::Now(), /*file_size=*/1);
+  ImageInfo foo_image({"test"}, test_directory_.AppendASCII("foo.png"), time,
+                      2);
   ImageInfo foo_image1({"test1"}, test_directory_.AppendASCII("foo.png"),
-                       std::move(time));
+                       std::move(time), 2);
   storage_->Insert(bar_image);
   storage_->Insert(foo_image);
   storage_->Insert(foo_image1);
@@ -131,9 +133,9 @@ TEST_F(AnnotationStorageTest, FindImagePath) {
   task_environment_.RunUntilIdle();
 
   ImageInfo bar_image({"test"}, test_directory_.AppendASCII("bar.jpg"),
-                      base::Time::Now());
+                      base::Time::Now(), /*file_size=*/1);
   ImageInfo foo_image({"test1"}, test_directory_.AppendASCII("foo.png"),
-                      base::Time::Now());
+                      base::Time::Now(), 2);
   storage_->Insert(bar_image);
   storage_->Insert(foo_image);
 
@@ -155,15 +157,15 @@ TEST_F(AnnotationStorageTest, SearchAnnotations) {
 
   ImageInfo document_image1({"test", "bar", "test1"},
                             test_directory_.AppendASCII("document1.jpg"),
-                            base::Time::Now());
+                            base::Time::Now(), /*file_size=*/1);
   ImageInfo document_image2({"testing", "testing_long"},
                             test_directory_.AppendASCII("document2.jpg"),
-                            base::Time::Now());
+                            base::Time::Now(), 2);
   ImageInfo document_image3({"testing_long"},
                             test_directory_.AppendASCII("document3.jpg"),
-                            base::Time::Now());
+                            base::Time::Now(), 3);
   ImageInfo foo_image({"test1"}, test_directory_.AppendASCII("foo.png"),
-                      base::Time::Now());
+                      base::Time::Now(), 4);
   storage_->Insert(document_image1);
   storage_->Insert(document_image2);
   storage_->Insert(document_image3);
@@ -243,15 +245,15 @@ TEST_F(AnnotationStorageTest, MaxResult) {
 
   ImageInfo document_image1({"test", "bar", "test1"},
                             test_directory_.AppendASCII("document1.jpg"),
-                            base::Time::Now());
+                            base::Time::Now(), /*file_size=*/1);
   ImageInfo document_image2({"bar", "test1"},
                             test_directory_.AppendASCII("document2.jpg"),
-                            base::Time::Now());
+                            base::Time::Now(), 2);
   ImageInfo document_image3({"bar", "test1"},
                             test_directory_.AppendASCII("document3.jpg"),
-                            base::Time::Now());
+                            base::Time::Now(), 3);
   ImageInfo foo_image({"test1"}, test_directory_.AppendASCII("foo.png"),
-                      base::Time::Now());
+                      base::Time::Now(), 4);
 
   storage_->Insert(document_image1);
   storage_->Insert(document_image2);
@@ -305,10 +307,10 @@ TEST_F(AnnotationStorageTest, QueryWithStopWords) {
 
   ImageInfo document_image1({"test", "bar", "test1"},
                             test_directory_.AppendASCII("document1.jpg"),
-                            base::Time::Now());
+                            base::Time::Now(), /*file_size=*/1);
   ImageInfo document_image2({"bar", "test1"},
                             test_directory_.AppendASCII("document2.jpg"),
-                            base::Time::Now());
+                            base::Time::Now(), 2);
 
   storage_->Insert(document_image1);
   storage_->Insert(document_image2);
@@ -359,7 +361,7 @@ TEST_F(AnnotationStorageTest, SchemaMigration) {
   task_environment_.RunUntilIdle();
 
   ImageInfo bar_image({"test"}, test_directory_.AppendASCII("bar.jpg"),
-                      base::Time::Now());
+                      base::Time::Now(), /*file_size=*/1);
 
   storage_->Insert(bar_image);
   EXPECT_THAT(storage_->GetAllAnnotations(),
--- chrome/browser/ash/app_list/search/local_image_search/documents_table.cc
+++ chrome/browser/ash/app_list/search/local_image_search/documents_table.cc
@@ -21,9 +21,11 @@ bool DocumentsTable::Create(SqlDatabase* db) {
       // clang-format off
       "CREATE TABLE documents("
           "document_id INTEGER PRIMARY KEY,"
-          "file_path TEXT UNIQUE,"
+          "directory_path TEXT NOT NULL,"
+          "file_name TEXT NOT NULL,"
           "last_modified_time INTEGER NOT NULL,"
-          "document_type INTEGER NOT NULL)";
+          "file_size INTEGER NOT NULL,"
+          "UNIQUE (directory_path, file_name))";
   // clang-format on
 
   std::unique_ptr<sql::Statement> statement =
@@ -34,7 +36,8 @@ bool DocumentsTable::Create(SqlDatabase* db) {
   }
 
   static constexpr char kQuery1[] =
-      "CREATE INDEX idx_documents_filepath ON documents(file_path)";
+      "CREATE INDEX idx_documents_filepath "
+      "ON documents(directory_path, file_name)";
 
   std::unique_ptr<sql::Statement> statement1 =
       db->GetStatementForQuery(SQL_FROM_HERE, kQuery1);
@@ -64,12 +67,12 @@ bool DocumentsTable::Drop(SqlDatabase* db) {
 bool DocumentsTable::InsertOrIgnore(SqlDatabase* db,
                                     const base::FilePath& file_path,
                                     const base::Time& last_modified_time,
-                                    DocumentType document_type) {
+                                    int64_t file_size) {
   static constexpr char kQuery[] =
       // clang-format off
       "INSERT OR IGNORE INTO documents"
-        "(file_path, last_modified_time, document_type) "
-        "VALUES(?,?,?)";
+        "(directory_path, file_name, last_modified_time, file_size) "
+        "VALUES(?,?,?,?)";
   // clang-format on
 
   std::unique_ptr<sql::Statement> statement =
@@ -79,9 +82,11 @@ bool DocumentsTable::InsertOrIgnore(SqlDatabase* db,
     return false;
   }
 
-  statement->BindString(0, file_path.value());
-  statement->BindTime(1, last_modified_time);
-  statement->BindInt(2, static_cast<int>(document_type));
+  // Safe on ChromeOS.
+  statement->BindString(0, file_path.DirName().AsUTF8Unsafe());
+  statement->BindString(1, file_path.BaseName().AsUTF8Unsafe());
+  statement->BindTime(2, last_modified_time);
+  statement->BindInt64(3, file_size);
   if (!statement->Run()) {
     LOG(ERROR) << "Couldn't execute the statement";
     return false;
@@ -96,7 +101,8 @@ bool DocumentsTable::GetDocumentId(SqlDatabase* db,
                                    int64_t& document_id) {
   DVLOG(2) << "GetDocumentId " << file_path;
   static constexpr char kQuery[] =
-      "SELECT document_id FROM documents WHERE file_path=?";
+      "SELECT document_id FROM documents WHERE directory_path=? AND "
+      "file_name=?";
 
   std::unique_ptr<sql::Statement> statement =
       db->GetStatementForQuery(SQL_FROM_HERE, kQuery);
@@ -105,7 +111,8 @@ bool DocumentsTable::GetDocumentId(SqlDatabase* db,
     return false;
   }
 
-  statement->BindString(0, file_path.value());
+  statement->BindString(0, file_path.DirName().AsUTF8Unsafe());
+  statement->BindString(1, file_path.BaseName().AsUTF8Unsafe());
   if (!statement->Step()) {
     LOG(ERROR) << "Couldn't execute the statement";
     return false;
@@ -116,8 +123,9 @@ bool DocumentsTable::GetDocumentId(SqlDatabase* db,
 }
 
 // static
-bool DocumentsTable::Remove(SqlDatabase* db, const base::FilePath& image_path) {
-  static constexpr char kQuery[] = "DELETE FROM documents WHERE file_path=?";
+bool DocumentsTable::Remove(SqlDatabase* db, const base::FilePath& file_path) {
+  static constexpr char kQuery[] =
+      "DELETE FROM documents WHERE directory_path=? AND file_name=?";
 
   std::unique_ptr<sql::Statement> statement =
       db->GetStatementForQuery(SQL_FROM_HERE, kQuery);
@@ -126,7 +134,8 @@ bool DocumentsTable::Remove(SqlDatabase* db, const base::FilePath& image_path) {
     return false;
   }
 
-  statement->BindString(0, image_path.value());
+  statement->BindString(0, file_path.DirName().AsUTF8Unsafe());
+  statement->BindString(1, file_path.BaseName().AsUTF8Unsafe());
   if (!statement->Run()) {
     LOG(ERROR) << "Couldn't execute the statement";
     return false;
