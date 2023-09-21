a9b23dae2d933ccd3a5addd0a0cceccf79fe20b8
Alex Mitra <alexmitra@chromium.org>
2023-09-21 18:36:56 +0000

    Adds unit tests for AndroidStylusWritingHandler
    Bug: 1478738
    Change-Id: I4f79cb4c0dc21b5a42bad900f2de8977f41459bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4861966
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Alex Mitra <alexmitra@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199745}

---
+++ components/stylus_handwriting/android/junit/src/org/chromium/components/stylus_handwriting/test_support/ShadowGlobalSettings.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.stylus_handwriting.test_support;
+
+import android.content.ContentResolver;
+import android.provider.Settings;
+
+import org.robolectric.annotation.Implementation;
+import org.robolectric.annotation.Implements;
+
+/** This shadow class lets tests manipulate the global settings for Android. */
+@Implements(Settings.Global.class)
+public class ShadowGlobalSettings {
+    // Whether handwriting is enabled. 1 = enabled, 0 = disabled, -1 = unset.
+    private static int sHandwritingEnabled = -1;
+
+    @Implementation
+    public static int getInt(ContentResolver contentResolver, String name, int def) {
+        if ("stylus_handwriting_enabled".equals(name)) {
+            return sHandwritingEnabled;
+        }
+        return def;
+    }
+
+    public static void setHandwritingEnabled(boolean value) {
+        sHandwritingEnabled = value ? 1 : 0;
+    }
+}
