680f9807cbfcee64b937f321db7d044aa9eb8ed7
David Pennington <dpenning@chromium.org>
2023-09-21 15:58:11 +0000

    [TabOrganization] Create a basic TabOrganizationService object.
    This CL adds a profile keyed service for TabOrganizations that is created on startup. It provides access to Sessions and send signals to UI's to show or hide based off triggers.
    
    Change-Id: Iba2f020dc50e70c90d72161d4e44fb8eeb80a229
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4851454
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199632}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -450,6 +450,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/performance_controls/performance_controls_hats_service_factory.h"
 #include "chrome/browser/ui/safety_hub/notification_permission_review_service_factory.h"
 #include "chrome/browser/ui/safety_hub/unused_site_permissions_service_factory.h"
+#include "chrome/browser/ui/tabs/organization/tab_organization_service_factory.h"
 #include "chrome/browser/usb/usb_connection_tracker_factory.h"
 #include "chrome/browser/user_notes/user_note_service_factory.h"
 #endif
@@ -1094,6 +1095,11 @@ void ChromeBrowserMainExtraPartsProfiles::
   sync_file_system::SyncFileSystemServiceFactory::GetInstance();
 #endif
   SyncServiceFactory::GetInstance();
+#if !BUILDFLAG(IS_ANDROID)
+  if (base::FeatureList::IsEnabled(features::kTabOrganization)) {
+    TabOrganizationServiceFactory::GetInstance();
+  }
+#endif
   TabRestoreServiceFactory::GetInstance();
   TemplateURLFetcherFactory::GetInstance();
   TemplateURLServiceFactory::GetInstance();
