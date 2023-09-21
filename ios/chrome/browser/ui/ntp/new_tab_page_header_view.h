9d8ca3563bd5f496cfa785c707fed3ac5615f85b
Scott Yoder <scottyoder@google.com>
2023-09-21 22:19:13 +0000

    [ios] Set larger fakebox w/ gradient background for Large Fakebox
    This change includes increasing the height of the Fakebox, and adding
    a gradient background to it. It also updates some spacing / padding.
    There will be more tweaks to spacing and padding as this feature is
    implemented.
    
    Previously the height of the fakebox was the height of the expanded
    toolbar and even though those are separate UI elements a single
    function was used to return that height. Much of the work here was
    breaking that out and figuring out which parts of the code actually
    needed the toolbar height vs which parts needed the fakebox height.
    
    Screenshot:
    https://screenshot.googleplex.com/vQCWXjm58iEi7TY.png
    
    Bug: 1482304
    Change-Id: I489e770c5339d081b0d21c28f42359228761b599
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4881259
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1199878}

--- ios/chrome/browser/ui/ntp/new_tab_page_header_view.h
+++ ios/chrome/browser/ui/ntp/new_tab_page_header_view.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
+@class GradientView;
+
 // Header view for the NTP. The header view contains all views that are
 // displayed above the list of most visited sites, which includes the
 // primary toolbar, doodle, and fake omnibox.
@@ -34,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     NSLayoutConstraint* fakeLocationBarLeadingConstraint;
 @property(nonatomic, strong)
     NSLayoutConstraint* fakeLocationBarTrailingConstraint;
-@property(nonatomic, strong) UIView* fakeLocationBar;
+@property(nonatomic, strong) GradientView* fakeLocationBar;
 @property(nonatomic, strong) UILabel* searchHintLabel;
 
 // `YES` if Google is the default search engine.
