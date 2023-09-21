45ec4eb4f346d222a8fe7e6f01e9b12f1dfd02d6
David Benjamin <davidben@chromium.org>
2023-09-21 23:48:29 +0000

    Switch some strings to StringPiece
    This avoids a copy that definitely does not matter, but I came across it
    and the fix was easy.
    
    Change-Id: Iedf1083c5e560d3c8f1333870c3eb1c9ee39da18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4881924
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199917}

--- net/android/network_library.cc
+++ net/android/network_library.cc
@@ -85,7 +85,7 @@ void ClearTestRootCertificates() {
   Java_AndroidNetworkLibrary_clearTestRootCertificates(env);
 }
 
-bool IsCleartextPermitted(const std::string& host) {
+bool IsCleartextPermitted(base::StringPiece host) {
   JNIEnv* env = AttachCurrentThread();
   ScopedJavaLocalRef<jstring> host_string = ConvertUTF8ToJavaString(env, host);
   return Java_AndroidNetworkLibrary_isCleartextPermitted(env, host_string);
@@ -98,7 +98,7 @@ bool HaveOnlyLoopbackAddresses() {
   return Java_AndroidNetworkLibrary_haveOnlyLoopbackAddresses(env);
 }
 
-bool GetMimeTypeFromExtension(const std::string& extension,
+bool GetMimeTypeFromExtension(base::StringPiece extension,
                               std::string* result) {
   JNIEnv* env = AttachCurrentThread();
 
