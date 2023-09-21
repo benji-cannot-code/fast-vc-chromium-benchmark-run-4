c69916d8a0614d9e2e6c785131199ab50de5ac12
Vivian Pao <vpao@google.com>
2023-09-21 01:06:37 +0000

    [Promise Icons] Pin promise app item in shelf if sync position exists
    When syncing triggers an app installation and causes a promise app to be
    registered in PromiseAppRegistryCache, a corresponding promise app shelf
    item should be created and pinned at the position indicated by the sync
    data. To do this, we expand `UpdatePinnedAppsFromSync` to pin/ create
    promise app items in the shelf and call the method when a new promise
    app is registered.
    
    Bug: b/261907612
    Change-Id: Iead07abf0c5b0eaf22bf1115c0f786d37d10667c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4812814
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Vivian Pao <vpao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1199344}

--- chrome/browser/ui/ash/shelf/shelf_controller_helper.h
+++ chrome/browser/ui/ash/shelf/shelf_controller_helper.h
@@ -92,6 +92,9 @@ class ShelfControllerHelper : public ExtensionEnableFlowDelegate {
   const Profile* profile() const { return profile_; }
   void set_profile(Profile* profile) { profile_ = profile; }
 
+  bool IsValidPromisePackageIdFromAppService(
+      const std::string& promise_package_id) const;
+
  private:
   // ExtensionEnableFlowDelegate:
   void ExtensionEnableFlowFinished() override;
