61d8f509afe9a7401c28978ea52242c10b7cb3aa
Pi-Hsun Shih <pihsun@chromium.org>
2023-09-21 11:42:19 +0000

    CCA: ESLint: Fix floating promise warning in scan_options.ts
    `cca.py lint --eslintrc .eslintrc_floating_promise.js` passes after this
    CL.
    
    Bug: b:172336355
    Test: cca.py lint --eslintrc .eslintrc_floating_promise.js
    Change-Id: I964047c79e929d4e24bcfcd0eaec562055eaaea3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4871838
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199528}

--- ash/webui/camera_app_ui/resources/js/views/camera/scan_options.ts
+++ ash/webui/camera_app_ui/resources/js/views/camera/scan_options.ts
@@ -64,7 +64,8 @@ export class ScanOptions implements CameraUI {
     // ready.
     dom.get('#scan-barcode', HTMLInputElement).checked = true;
 
-    (async () => {
+    // TODO(pihsun): Move this outside of the constructor.
+    void (async () => {
       const {supported} =
           await ChromeHelper.getInstance().getDocumentScannerReadyState();
       dom.get('#scan-document-option', HTMLElement).hidden = !supported;
@@ -76,9 +77,9 @@ export class ScanOptions implements CameraUI {
           evt.preventDefault();
         }
       });
-      option.addEventListener('change', () => {
+      option.addEventListener('change', async () => {
         if (option.checked) {
-          this.switchToScanType(this.getToggledScanOption());
+          await this.switchToScanType(this.getToggledScanOption());
         }
       });
     }
@@ -130,12 +131,14 @@ export class ScanOptions implements CameraUI {
     const {deviceId} = video.getVideoSettings();
     this.documentCornerOverlay.attach(deviceId);
     const scanType = this.getToggledScanOption();
-    (async () => {
+    // Not awaiting here since this is for teardown after preview video
+    // expires.
+    void (async () => {
       await video.onExpired.wait();
       this.detachPreview();
     })();
     await this.switchToScanType(scanType);
-    this.checkDocumentModeReadiness();
+    await this.checkDocumentModeReadiness();
   }
 
   /**
