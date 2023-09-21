337a44f51a14a3dc16c4addb56c3c541630cae60
Philipp Hancke <phancke@microsoft.com>
2023-09-21 07:19:54 +0000

    webrtc-internals: add url parameter for keeping connections
    by adding keepRemovedConnections to the URL parameters, i.e.
      ?keepRemovedConnections
    This is useful to see the history in case of tab crashes.
    
    BUG=chromium:416876
    
    Change-Id: I78230bbe094131484e4d64582aeb6466543f293a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4876665
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Commit-Queue: Philipp Hancke <phancke@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1199452}

--- content/browser/webrtc/resources/webrtc_internals.js
+++ content/browser/webrtc/resources/webrtc_internals.js
@@ -29,6 +29,8 @@ let statsTable = null;
 let userMediaTable = null;
 let dumpCreator = null;
 
+const searchParameters = new URLSearchParams(window.location.search);
+
 // Exporting these on window since they are directly accessed by tests.
 window.setCurrentGetStatsMethod = function(method) {
   currentGetStatsMethod = method;
@@ -161,7 +163,6 @@ function initialize() {
 
   // Requests stats from all peer connections every second unless specified via
   // ?statsInterval=(milliseconds >= 100ms)
-  const searchParameters = new URLSearchParams(window.location.search);
   let statsInterval = 1000;
   if (searchParameters.has('statsInterval')) {
     statsInterval = Math.max(
@@ -306,6 +307,7 @@ function addPeerConnectionUpdate(peerConnectionElement, update) {
 
 /**
  * Removes all information about a peer connection.
+ * Use ?keepRemovedConnections url parameter to prevent the removal.
  *
  * @param {!Object<number>} data The object containing the rid and lid of a peer
  *     connection.
@@ -314,8 +316,9 @@ function removePeerConnection(data) {
   // Disable getElementById restriction here, since |getPeerConnectionId| does
   // not return valid selectors.
   // eslint-disable-next-line no-restricted-properties
+
   const element = document.getElementById(getPeerConnectionId(data));
-  if (element) {
+  if (element && !searchParameters.has('keepRemovedConnections')) {
     delete peerConnectionDataStore[element.id];
     tabView.removeTab(element.id);
   }
