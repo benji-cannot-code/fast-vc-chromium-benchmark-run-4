41f46c47055859cd4f37d162c043439bf2e36f02
Quentin Pubert <qpubert@google.com>
2023-09-21 12:48:24 +0000

    [iOS] Add Save to Photos metrics
    This CL adds three histograms related to Save to Photos:
    
    - `IOS.SaveToPhotos` records failure or success of the Save to Photos
      flow with cause of failure if any, and some additional info about
      actions performed by the user with the Photo once uploaded.
    - `IOS.SaveToPhotos.AccountPicker` records how the account picker was
      used i.e. whether the user cancelled, or selected an account, or if
      the account picker was skipped.
    - `IOS.SaveToPhotos.Settings` records the state of the Save to Photos
      default account preference. This will be recorded in a separate CL by
      the Photos service.
    
    Low-Coverage-Reason: TRIVIAL_CHANGE logging statements
    Bug: 1484607
    Change-Id: Ib6a964e61870cc7cf652c4c1afad72b1a2b967ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4874920
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199542}

---
+++ ios/chrome/browser/photos/photos_metrics.mm
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/photos/photos_metrics.h"
+
+const char kSaveToPhotosActionsHistogram[] = "IOS.SaveToPhotos";
+
+const char kSaveToPhotosAccountPickerActionsHistogram[] =
+    "IOS.SaveToPhotos.AccountPicker";
+
+const char kSaveToPhotosSettingsActionsHistogram[] =
+    "IOS.SaveToPhotos.Settings";
