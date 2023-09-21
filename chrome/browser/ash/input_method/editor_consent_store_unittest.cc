45294ae2519488fa39939cb9c02eb106951c65e4
Chuong Ho <hdchuong@google.com>
2023-09-21 23:36:08 +0000

    inputs: Remove backend response to consent dismissal
    Discussed with UX, for now, consent dismissal is the same as promo card dismissal, and in that case, consent dismissal should not do anything. This CL removes the logic to count the number of consent window dismissal.
    
    
    Change-Id: I034ef534813ac7f95150f7a3b476ce200f2e7ad7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4865381
    Commit-Queue: Chuong Ho <hdchuong@google.com>
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199913}

--- chrome/browser/ash/input_method/editor_consent_store_unittest.cc
+++ chrome/browser/ash/input_method/editor_consent_store_unittest.cc
@@ -33,26 +33,13 @@ TEST_F(EditorConsentStoreTest,
 }
 
 TEST_F(EditorConsentStoreTest,
-       ReceivingApprovalResponseAfterDismissalWillLeadToConsentApproval) {
+       ReceivingApprovalResponseWillLeadToConsentApproval) {
   TestingProfile profile_;
   EditorConsentStore store(profile_.GetPrefs());
 
-  store.ProcessConsentAction(ConsentAction::kDismissed);
   store.ProcessConsentAction(ConsentAction::kApproved);
 
   EXPECT_EQ(store.GetConsentStatus(), ConsentStatus::kApproved);
-}
-
-TEST_F(EditorConsentStoreTest,
-       ManyConsentWindowDismissalsWillLeadToImplicitConsentDecline) {
-  TestingProfile profile_;
-  EditorConsentStore store(profile_.GetPrefs());
-
-  store.ProcessConsentAction(ConsentAction::kDismissed);
-  store.ProcessConsentAction(ConsentAction::kDismissed);
-  store.ProcessConsentAction(ConsentAction::kDismissed);
-
-  EXPECT_EQ(store.GetConsentStatus(), ConsentStatus::kImplicitlyDeclined);
 }
 
 TEST_F(EditorConsentStoreTest,
@@ -61,21 +48,6 @@ TEST_F(EditorConsentStoreTest,
   EditorConsentStore store(profile_.GetPrefs());
 
   store.ProcessConsentAction(ConsentAction::kDeclined);
-  // Simulate a user action to switch on the orca toggle.
-  profile_.GetPrefs()->SetBoolean(prefs::kOrcaEnabled, true);
-
-  EXPECT_EQ(store.GetConsentStatus(), ConsentStatus::kUnset);
-}
-
-TEST_F(
-    EditorConsentStoreTest,
-    SwitchingOnSettingToggleWillResetConsentWhichWasPreviouslyImplicitlyDeclined) {
-  TestingProfile profile_;
-  EditorConsentStore store(profile_.GetPrefs());
-
-  store.ProcessConsentAction(ConsentAction::kDismissed);
-  store.ProcessConsentAction(ConsentAction::kDismissed);
-  store.ProcessConsentAction(ConsentAction::kDismissed);
   // Simulate a user action to switch on the orca toggle.
   profile_.GetPrefs()->SetBoolean(prefs::kOrcaEnabled, true);
 
