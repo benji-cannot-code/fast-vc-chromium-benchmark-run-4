4e08196565dfc9543bd3bd23708cccb1de23fce2
aliaaborhamy <aliaaborhamy@google.com>
2023-09-21 21:34:57 +0000

    [iOS] Add message label to the content extension
    This is a guidance message for the user to install
    Chrome shortcuts widget in place.
    
    Bug: 1484016
    Change-Id: Ief4a4aa31d5cd767d01effa15002f32ea9dcdc73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4881758
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Aliaa Alborhamy <aliaaborhamy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1199853}

--- ios/chrome/content_widget_extension/content_widget_view_controller.h
+++ ios/chrome/content_widget_extension/content_widget_view_controller.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 
 @interface ContentWidgetViewController : UIViewController<NCWidgetProviding>
-
 @end
 
 #endif  // IOS_CHROME_CONTENT_WIDGET_EXTENSION_CONTENT_WIDGET_VIEW_CONTROLLER_H_
