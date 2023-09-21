f4fca87c240dccfb185ea4730d567eb2752cb231
Mariam Mostafa <mariammostafa@google.com>
2023-09-21 17:56:26 +0000

    Add new shadow metrics for Autofill ML model predictions
    These new shadow metrics will be used to compare ML model predictions
    with heuristic and server predictions.
    
    Bug: 1465926
    Change-Id: I9aa95cb767dbde65d12c68c06d95fe62deba55a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4866936
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Commit-Queue: Mariam Mostafa <mariammostafa@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1199721}

--- components/autofill/core/browser/metrics/shadow_prediction_metrics.cc
+++ components/autofill/core/browser/metrics/shadow_prediction_metrics.cc
@@ -5,8 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/core/browser/metrics/shadow_prediction_metrics.h"
 
+#include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
 #include "components/autofill/core/browser/form_parsing/buildflags.h"
+#include "components/autofill/core/browser/heuristic_source.h"
+#include "components/autofill/core/common/autofill_features.h"
+#include "components/optimization_guide/machine_learning_tflite_buildflags.h"
 
 namespace autofill::autofill_metrics {
 
@@ -83,6 +87,32 @@ void LogShadowPredictionComparison(const AutofillField& field) {
       GetShadowPrediction(field.heuristic_type(HeuristicSource::kExperimental),
                           field.heuristic_type(HeuristicSource::kNextGen),
                           submitted_types));
+#endif
+
+#if BUILDFLAG(BUILD_WITH_TFLITE_LIB)
+  if (base::FeatureList::IsEnabled(features::kAutofillModelPredictions)) {
+    base::UmaHistogramSparse(
+        "Autofill.ShadowPredictions.DefaultServerToMLModel",
+        GetShadowPrediction(
+            field.server_type(),
+            field.heuristic_type(HeuristicSource::kMachineLearning),
+            submitted_types));
+#if !BUILDFLAG(USE_INTERNAL_AUTOFILL_PATTERNS)
+    base::UmaHistogramSparse(
+        "Autofill.ShadowPredictions.LegacyPatternSourceToMLModel",
+        GetShadowPrediction(
+            field.heuristic_type(HeuristicSource::kLegacy),
+            field.heuristic_type(HeuristicSource::kMachineLearning),
+            submitted_types));
+#else
+    base::UmaHistogramSparse(
+        "Autofill.ShadowPredictions.DefaultPatternSourceToMLModel",
+        GetShadowPrediction(
+            field.heuristic_type(HeuristicSource::kDefault),
+            field.heuristic_type(HeuristicSource::kMachineLearning),
+            submitted_types));
+#endif
+  }
 #endif
 }
 
