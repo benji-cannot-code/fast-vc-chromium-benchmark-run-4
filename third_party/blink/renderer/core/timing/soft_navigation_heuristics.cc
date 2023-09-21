d5917d9bed371fe50ed6d3762c8aa47ee2dfd4a4
Yoav Weiss <yoavweiss@chromium.org>
2023-09-21 10:37:19 +0000

    [task attribution] Add a build time flag to disable the infrastructure
    Change-Id: I1d50578f8b5e0fb60eb73d026593c85b0d30f9c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4878778
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199514}

--- third_party/blink/renderer/core/timing/soft_navigation_heuristics.cc
+++ third_party/blink/renderer/core/timing/soft_navigation_heuristics.cc
@@ -86,10 +86,12 @@ void SoftNavigationHeuristics::UserInitiatedClick(ScriptState* script_state) {
   // Set task ID to the current one.
   ThreadScheduler* scheduler = ThreadScheduler::Current();
   DCHECK(scheduler);
-  // This should not be called off-main-thread.
-  DCHECK(scheduler->GetTaskAttributionTracker());
+  auto* tracker = scheduler->GetTaskAttributionTracker();
+  if (!tracker) {
+    return;
+  }
   ResetHeuristic();
-  scheduler->GetTaskAttributionTracker()->RegisterObserver(this);
+  tracker->RegisterObserver(this);
   SetIsTrackingSoftNavigationHeuristicsOnDocument(true);
   user_click_timestamp_ = base::TimeTicks::Now();
   TRACE_EVENT_INSTANT("scheduler",
@@ -117,7 +119,11 @@ bool SoftNavigationHeuristics::IsCurrentTaskDescendantOfClickEventHandler(
 void SoftNavigationHeuristics::ClickEventEnded(ScriptState* script_state) {
   ThreadScheduler* scheduler = ThreadScheduler::Current();
   DCHECK(scheduler);
-  scheduler->GetTaskAttributionTracker()->UnregisterObserver(this);
+  auto* tracker = scheduler->GetTaskAttributionTracker();
+  if (!tracker) {
+    return;
+  }
+  tracker->UnregisterObserver(this);
   CheckAndReportSoftNavigation(script_state);
   TRACE_EVENT_INSTANT("scheduler", "SoftNavigationHeuristics::ClickEventEnded");
 }
