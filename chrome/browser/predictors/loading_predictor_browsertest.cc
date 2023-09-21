4504b537bd3ae850434d29272691ece823f5cb53
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2023-09-21 06:24:45 +0000

    LCPP: Factor out Predict* for LCPP from ResourcePrefetchPredictor.
    This is a follow up CL for:
    https://chromium-review.googlesource.com/c/chromium/src/+/4876515
    
    As requested there, Predict* code for LCPP has been factored out from
    ResourceFetchPredictor.
    Since these functions are actually only used by a test, I made them
    hidden inside the .cc file, and a converter from LcppData to
    LCPCriticalPathPredictorNavigationTimeHint got visible.
    
    Change-Id: I258ab19dea8dc2208c206f07c7d693c8e2ecbfda
    Bug: 1419756
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4876344
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199436}

--- chrome/browser/predictors/loading_predictor_browsertest.cc
+++ chrome/browser/predictors/loading_predictor_browsertest.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/navigation_predictor/navigation_predictor_preconnect_client.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
+#include "chrome/browser/predictors/lcp_critical_path_predictor/lcp_critical_path_predictor_util.h"
 #include "chrome/browser/predictors/loading_predictor.h"
 #include "chrome/browser/predictors/loading_predictor_factory.h"
 #include "chrome/browser/predictors/loading_test_util.h"
@@ -907,8 +908,12 @@ class LCPCriticalPathPredictorBrowserTest : public LoadingPredictorBrowserTest {
         loading_predictor()->resource_prefetch_predictor()->GetLcppData(url);
     std::vector<std::string> locators;
     if (lcpp_data) {
-      locators =
-          ResourcePrefetchPredictor::PredictLcpElementLocators(*lcpp_data);
+      absl::optional<blink::mojom::LCPCriticalPathPredictorNavigationTimeHint>
+          hint = ConvertLcppDataToLCPCriticalPathPredictorNavigationTimeHint(
+              *lcpp_data);
+      if (hint) {
+        locators = hint->lcp_element_locators;
+      }
     }
     EXPECT_EQ(expected_locator_count, locators.size()) << from_here.ToString();
     return locators;
