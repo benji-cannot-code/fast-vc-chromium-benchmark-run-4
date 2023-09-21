426e5dfd057e0dc76be2bbd42cb31f39f1c7f59f
Frank Liberato <liberato@chromium.org>
2023-09-21 21:38:31 +0000

    [autopip] Disallow autopip except from https and file.
    Change-Id: I229e444f2f8f28c2c7283963b4679dd51a0c0658
    Bug: 1484667
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4884093
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199855}

--- chrome/browser/picture_in_picture/auto_picture_in_picture_tab_helper.h
+++ chrome/browser/picture_in_picture/auto_picture_in_picture_tab_helper.h
@@ -80,6 +80,8 @@ class AutoPictureInPictureTabHelper
  private:
   explicit AutoPictureInPictureTabHelper(content::WebContents* web_contents);
   friend class content::WebContentsUserData<AutoPictureInPictureTabHelper>;
+  FRIEND_TEST_ALL_PREFIXES(AutoPictureInPictureTabHelperBrowserTest,
+                           CannotAutopipViaHttp);
 
   void MaybeEnterAutoPictureInPicture();
 
