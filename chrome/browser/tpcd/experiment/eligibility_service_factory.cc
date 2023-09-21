f676d7def5a95f85dadaf6f950ee40344629264a
Anton Maliev <amaliev@chromium.org>
2023-09-21 18:13:22 +0000

    Pipe 3PCD experiment eligibility decision from EligibilityService to onboarding UX.
    Bug: b/300490685
    Fixed: b/300490685
    Change-Id: Id731ec50de4a3520263958ed8f24de6f6a8ba298
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4873647
    Commit-Queue: Anton Maliev <amaliev@chromium.org>
    Reviewed-by: Ryan Tarpine <rtarpine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199734}

--- chrome/browser/tpcd/experiment/eligibility_service_factory.cc
+++ chrome/browser/tpcd/experiment/eligibility_service_factory.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tpcd/experiment/eligibility_service_factory.h"
 
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/privacy_sandbox/tracking_protection_onboarding_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/tpcd/experiment/eligibility_service.h"
 
@@ -28,7 +29,9 @@ EligibilityServiceFactory::EligibilityServiceFactory()
           "EligibilityServiceFactory",
           ProfileSelections::Builder()
               .WithRegular(ProfileSelection::kOriginalOnly)
-              .Build()) {}
+              .Build()) {
+  DependsOn(TrackingProtectionOnboardingFactory::GetInstance());
+}
 
 std::unique_ptr<KeyedService>
 EligibilityServiceFactory::BuildServiceInstanceForBrowserContext(
