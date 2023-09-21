074f639fcdddf0eb1149b768ff07728f682cc4fa
Rushan Suleymanov <rushans@google.com>
2023-09-21 15:11:59 +0000

    [Sync] Fix flaky test DepleteQuotaAndRecover
    The test is relying on the expected nudged sync cycles. There is a
    feature toggle which changes this behavior and nudges a poll request
    immediately on browser startup, which might result in an additional sync
    cycle and Bookmarks being committed faster than expected.
    
    Bug: 1447535
    Change-Id: Ie730f303f449412e8b88144383272c2e02878d4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4882665
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199608}

--- chrome/browser/sync/test/integration/single_client_bookmarks_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_bookmarks_sync_test.cc
@@ -204,7 +204,10 @@ class SingleClientBookmarksSyncTestWithEnabledReuploadPreexistingBookmarks
 
 class SingleClientBookmarksThrottlingSyncTest : public SyncTest {
  public:
-  SingleClientBookmarksThrottlingSyncTest() : SyncTest(SINGLE_CLIENT) {}
+  SingleClientBookmarksThrottlingSyncTest() : SyncTest(SINGLE_CLIENT) {
+    features_override_.InitAndDisableFeature(
+        syncer::kSyncPollImmediatelyOnEveryStartup);
+  }
 
   void SetUpInProcessBrowserTestFixture() override {
     SyncTest::SetUpInProcessBrowserTestFixture();
@@ -242,6 +245,7 @@ class SingleClientBookmarksThrottlingSyncTest : public SyncTest {
   }
 
  private:
+  base::test::ScopedFeatureList features_override_;
   base::CallbackListSubscription create_services_subscription_;
 };
 
@@ -1961,9 +1965,8 @@ IN_PROC_BROWSER_TEST_F(SingleClientBookmarksThrottlingSyncTest,
   histogram_tester.ExpectTotalCount("Sync.ModelTypeCommitWithDepletedQuota", 0);
 }
 
-// TODO(crbug.com/1447535): Disabled due to flakiness.
 IN_PROC_BROWSER_TEST_F(SingleClientBookmarksThrottlingSyncTest,
-                       DISABLED_DepleteQuotaAndRecover) {
+                       DepleteQuotaAndRecover) {
   ASSERT_TRUE(SetupClients());
 
   // Setup custom quota params: to effectively never refill, and custom nudge
