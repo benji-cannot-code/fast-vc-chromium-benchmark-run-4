7a5860d2a5a6f14b59568fd31b39a7117bcc822b
wenyu zhang <zhangwenyu@google.com>
2023-09-21 16:08:21 +0000

    display: Fix typo
    Bug: None
    Change-Id: Ifaf9ab32e2bfafbec620fda2849e3a7780fc0fa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4880460
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Wenyu Zhang <zhangwenyu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1199641}

--- ash/display/display_manager_unittest.cc
+++ ash/display/display_manager_unittest.cc
@@ -3753,7 +3753,7 @@ TEST_F(DisplayManagerTest, DisconnectedInternalDisplayShouldUpdateDisplayInfo) {
           .AddMode(MakeDisplayMode())
           .SetOrigin({0, 1000})
           .Build();
-  // "Connectd display" has the current mode.
+  // "Connected display" has the current mode.
   external_snapshot->set_current_mode(external_snapshot->native_mode());
 
   outputs.push_back(external_snapshot.get());
@@ -3804,7 +3804,7 @@ TEST_F(DisplayManagerTest, UpdateInternalDisplayNativeBounds) {
           .SetNativeMode(MakeDisplayMode())
           .AddMode(MakeDisplayMode())
           .Build();
-  // "Connectd display" has the current mode.
+  // "Connected display" has the current mode.
   external_snapshot->set_current_mode(external_snapshot->native_mode());
   outputs.push_back(external_snapshot.get());
 
