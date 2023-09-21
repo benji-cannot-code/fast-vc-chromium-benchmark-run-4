6fbd94e427b6e95f7c7208b9f6b6952e6e6fe6bb
Kuan Huang <kuanhuang@chromium.org>
2023-09-21 21:06:23 +0000

    Disable flaky IsAnimatedLCPTest* and MouseoverLCPTest* tests on lacros
    The tests are timeout during `run_test()` on linux-lacros-tester-rel
    and caussed 47 build failures in last 24 hours.
    
    Also another CL is created to separate the failures from other
    browser_tests timeout (https://crrev.com/c/4873839).
    
    Bug: 1365773
    Change-Id: Id19617289aafbb23f7c0dcc176255f5b800efa42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4880245
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Owners-Override: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Kuan Huang <kuanhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199829}

--- chrome/browser/page_load_metrics/integration_tests/largest_contentful_paint_browsertest.cc
+++ chrome/browser/page_load_metrics/integration_tests/largest_contentful_paint_browsertest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "base/test/trace_event_analyzer.h"
 #include "build/build_config.h"
+#include "build/chromeos_buildflags.h"
 #include "cc/base/switches.h"
 #include "chrome/browser/page_load_metrics/integration_tests/metric_integration_test.h"
 #include "chrome/test/base/ui_test_utils.h"
@@ -319,7 +320,13 @@ IN_PROC_BROWSER_TEST_F(PageViewportInLCPTest, FullSizeImageInIframe) {
       *trace_analyzer, "latest_largest_contentful_paint_ms", lcpTime, 2.0);
 }
 
-class IsAnimatedLCPTest : public MetricIntegrationTest {
+// TODO(crbug.com/1365773): Flaky on lacros
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#define MAYBE_IsAnimatedLCPTest DISABLED_IsAnimatedLCPTest
+#else
+#define MAYBE_IsAnimatedLCPTest IsAnimatedLCPTest
+#endif
+class MAYBE_IsAnimatedLCPTest : public MetricIntegrationTest {
  public:
   void SetUpCommandLine(base::CommandLine* command_line) override {
     command_line->AppendSwitchASCII(switches::kEnableBlinkFeatures,
@@ -352,13 +359,14 @@ class IsAnimatedLCPTest : public MetricIntegrationTest {
   }
 };
 
-IN_PROC_BROWSER_TEST_F(IsAnimatedLCPTest, LargestContentfulPaint_IsAnimated) {
+IN_PROC_BROWSER_TEST_F(MAYBE_IsAnimatedLCPTest,
+                       LargestContentfulPaint_IsAnimated) {
   test_is_animated("/is_animated.html",
                    blink::LargestContentfulPaintType::kAnimatedImage,
                    /*expected=*/true);
 }
 
-IN_PROC_BROWSER_TEST_F(IsAnimatedLCPTest,
+IN_PROC_BROWSER_TEST_F(MAYBE_IsAnimatedLCPTest,
                        LargestContentfulPaint_IsNotAnimated) {
   test_is_animated("/non_animated.html",
                    blink::LargestContentfulPaintType::kAnimatedImage,
@@ -366,7 +374,7 @@ IN_PROC_BROWSER_TEST_F(IsAnimatedLCPTest,
 }
 
 IN_PROC_BROWSER_TEST_F(
-    IsAnimatedLCPTest,
+    MAYBE_IsAnimatedLCPTest,
     LargestContentfulPaint_AnimatedImageWithLargerTextFirst) {
   test_is_animated("/animated_image_with_larger_text_first.html",
                    blink::LargestContentfulPaintType::kAnimatedImage,
@@ -374,14 +382,20 @@ IN_PROC_BROWSER_TEST_F(
 }
 
 // crbug.com/1373885: This test is unreliable on ChromeOS, Linux and Mac
-IN_PROC_BROWSER_TEST_F(IsAnimatedLCPTest,
+IN_PROC_BROWSER_TEST_F(MAYBE_IsAnimatedLCPTest,
                        DISABLED_LargestContentfulPaint_IsVideo) {
   test_is_animated("/is_video.html", blink::LargestContentfulPaintType::kVideo,
                    /*expected=*/true, /*entries=*/0);
 }
 
-class MouseoverLCPTest : public MetricIntegrationTest,
-                         public testing::WithParamInterface<bool> {
+// TODO(crbug.com/1365773): Flaky on lacros
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#define MAYBE_MouseoverLCPTest DISABLED_MouseoverLCPTest
+#else
+#define MAYBE_MouseoverLCPTest MouseoverLCPTest
+#endif
+class MAYBE_MouseoverLCPTest : public MetricIntegrationTest,
+                               public testing::WithParamInterface<bool> {
  public:
   void test_mouseover(const char* html_name,
                       blink::LargestContentfulPaintType flag_set,
@@ -491,9 +505,11 @@ class MouseoverLCPTest : public MetricIntegrationTest,
   }
 };
 
-INSTANTIATE_TEST_SUITE_P(All, MouseoverLCPTest, ::testing::Values(false, true));
+INSTANTIATE_TEST_SUITE_P(All,
+                         MAYBE_MouseoverLCPTest,
+                         ::testing::Values(false, true));
 
-IN_PROC_BROWSER_TEST_P(MouseoverLCPTest,
+IN_PROC_BROWSER_TEST_P(MAYBE_MouseoverLCPTest,
                        LargestContentfulPaint_MouseoverOverLCPImage) {
   test_mouseover("/mouseover.html",
                  blink::LargestContentfulPaintType::kAfterMouseover,
@@ -504,7 +520,7 @@ IN_PROC_BROWSER_TEST_P(MouseoverLCPTest,
                  /*expected=*/true);
 }
 
-IN_PROC_BROWSER_TEST_P(MouseoverLCPTest,
+IN_PROC_BROWSER_TEST_P(MAYBE_MouseoverLCPTest,
                        LargestContentfulPaint_MouseoverOverLCPImageReplace) {
   test_mouseover("/mouseover.html?replace",
                  blink::LargestContentfulPaintType::kAfterMouseover,
@@ -515,7 +531,7 @@ IN_PROC_BROWSER_TEST_P(MouseoverLCPTest,
                  /*expected=*/true);
 }
 
-IN_PROC_BROWSER_TEST_P(MouseoverLCPTest,
+IN_PROC_BROWSER_TEST_P(MAYBE_MouseoverLCPTest,
                        LargestContentfulPaint_MouseoverOverBody) {
   test_mouseover("/mouseover.html",
                  blink::LargestContentfulPaintType::kAfterMouseover,
@@ -526,7 +542,7 @@ IN_PROC_BROWSER_TEST_P(MouseoverLCPTest,
                  /*expected=*/false);
 }
 
-IN_PROC_BROWSER_TEST_P(MouseoverLCPTest,
+IN_PROC_BROWSER_TEST_P(MAYBE_MouseoverLCPTest,
                        LargestContentfulPaint_MouseoverOverLCPImageThenBody) {
   test_mouseover("/mouseover.html?dispatch",
                  blink::LargestContentfulPaintType::kAfterMouseover,
@@ -537,9 +553,9 @@ IN_PROC_BROWSER_TEST_P(MouseoverLCPTest,
                  /*expected=*/false);
 }
 
-class MouseoverLCPTestWithHeuristicFlag : public MouseoverLCPTest {
+class MouseoverLCPTestWithHeuristicFlag : public MAYBE_MouseoverLCPTest {
   void SetUpCommandLine(base::CommandLine* command_line) override {
-    MouseoverLCPTest::SetUpCommandLine(command_line);
+    MAYBE_MouseoverLCPTest::SetUpCommandLine(command_line);
     feature_list_.InitWithFeatures(
         {blink::features::kLCPMouseoverHeuristics} /*enabled*/,
         {} /*disabled*/);
@@ -562,8 +578,17 @@ IN_PROC_BROWSER_TEST_P(MouseoverLCPTestWithHeuristicFlag,
                  /*expected=*/false);
 }
 
-IN_PROC_BROWSER_TEST_P(MouseoverLCPTestWithHeuristicFlag,
-                       LargestContentfulPaint_MouseoverOverLCPImageReplace) {
+// TODO(crbug.com/1365773): Flaky on lacros
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#define MAYBE_LargestContentfulPaint_MouseoverOverLCPImageReplace \
+  DISABLED_LargestContentfulPaint_MouseoverOverLCPImageReplace
+#else
+#define MAYBE_LargestContentfulPaint_MouseoverOverLCPImageReplace \
+  LargestContentfulPaint_MouseoverOverLCPImageReplace
+#endif
+IN_PROC_BROWSER_TEST_P(
+    MouseoverLCPTestWithHeuristicFlag,
+    MAYBE_LargestContentfulPaint_MouseoverOverLCPImageReplace) {
   test_mouseover("/mouseover.html?replace",
                  blink::LargestContentfulPaintType::kAfterMouseover,
                  /*entries=*/"1",
