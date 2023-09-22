28bbedd8be205979e8d2646cd6f728fbc54dd590
Maggie Chen <magchen@chromium.org>
2023-09-22 21:54:42 +0000

    Make blink image_decoder test files a source_set
    In preparation for moving image_decoders to components/,
    source_set("unit_tests") is added to image-decoders/BUILD.gn
    
    Bug: 1462922
    Change-Id: I5159a75e6e28be4248afab4f1a25c10800f0f431
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4883441
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200501}

--- third_party/blink/renderer/core/fetch/bytes_uploader_test.cc
+++ third_party/blink/renderer/core/fetch/bytes_uploader_test.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/platform/scheduler/test/renderer_scheduler_test_support.h"
+#include "third_party/blink/renderer/platform/heap/persistent.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread.h"
 #include "third_party/blink/renderer/platform/testing/unit_test_helpers.h"
 
--- third_party/blink/renderer/platform/testing/unit_test_helpers.h
+++ third_party/blink/renderer/platform/testing/unit_test_helpers.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_TESTING_UNIT_TEST_HELPERS_H_
 
 #include "base/memory/scoped_refptr.h"
-#include "third_party/blink/renderer/platform/timer.h"
+#include "base/time/time.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
