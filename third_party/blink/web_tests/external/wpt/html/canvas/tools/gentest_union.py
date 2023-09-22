7160bccecc54b10e12cde2bd6d47595682e1f5d7
Yi Xu <yiyix@google.com>
2023-09-22 04:21:20 +0000

    Prepare cl to move the-canvas-element to new test generator
    Bug: 1275750
    
    Change-Id: Iebf49e86d42cc6a847d7dbeddaf9bef5332b4824
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800029
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Commit-Queue: Yi Xu <yiyix@google.com>
    Cr-Commit-Position: refs/heads/main@{#1200035}

--- third_party/blink/web_tests/external/wpt/html/canvas/tools/gentest_union.py
+++ third_party/blink/web_tests/external/wpt/html/canvas/tools/gentest_union.py
@@ -1,4 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from gentestutilsunion import genTestUtils_union
 
-genTestUtils_union('name2dir-canvas.yaml')
+genTestUtils_union('name2dir.yaml')
