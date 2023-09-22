78acab0eb0755662d8463acae45d0a4ef8bd767d
Ming-Ying Chung <mych@chromium.org>
2023-09-22 05:31:00 +0000

    [fetch-later] Add UMA metrics in both renderer & browser
    FetchLater is a new API still under development. See the
    attached bug for more details.
    
    Bug: 1465781
    Change-Id: Iba2ddfea1530fd1c2a6f4b2f4e3de43f5e0e7f07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4868152
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Auto-Submit: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200062}

--- content/browser/loader/keep_alive_url_loader.cc
+++ content/browser/loader/keep_alive_url_loader.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_forward.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/trace_event/trace_event.h"
 #include "base/trace_event/typed_macros.h"
 #include "content/browser/renderer_host/policy_container_host.h"
@@ -46,6 +47,23 @@ base::TimeDelta GetDisconnectedKeepAliveURLLoaderTimeout() {
       "disconnected_loader_timeout_seconds",
       base::checked_cast<int32_t>(
           kDefaultDisconnectedKeepAliveURLLoaderTimeout.InSeconds())));
+}
+
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+//
+// Must remain in sync with FetchLaterBrowserMetricType in
+// tools/metrics/histograms/enums.xml.
+enum class FetchLaterBrowserMetricType {
+  kAbortedByInitiator = 0,
+  kStartedAfterInitiatorDisconnected = 1,
+  kStartedByInitiator = 2,
+  kCancelledAfterTimeLimit = 3,
+  kMaxValue = kCancelledAfterTimeLimit,
+};
+
+void LogFetchLaterMetric(const FetchLaterBrowserMetricType& type) {
+  base::UmaHistogramEnumeration("FetchLater.Browser.Metrics", type);
 }
 
 // A convenient holder to aggregate modified header fields for redirect.
@@ -322,6 +340,7 @@ KeepAliveURLLoader::KeepAliveURLLoader(
               request_id_, "url", last_url_);
   TRACE_EVENT_NESTABLE_ASYNC_BEGIN1("loading", "KeepAliveURLLoader",
                                     request_id_, "url", last_url_);
+  base::UmaHistogramBoolean("FetchLater.Browser.Total", true);
 
   // TODO(crbug.com/1356128): Replace custom throttle logic here with blink's.
   for (auto& content_throttle : throttles) {
@@ -335,6 +354,10 @@ void KeepAliveURLLoader::Start() {
   TRACE_EVENT("loading", "KeepAliveURLLoader::Start", "request_id",
               request_id_);
   is_started_ = true;
+
+  if (IsFetchLater()) {
+    base::UmaHistogramBoolean("FetchLater.Browser.Total.Started", true);
+  }
 
   // Asks the network service to create a URL loader with passed in params.
   network_loader_factory_->CreateLoaderAndStart(
@@ -859,6 +882,8 @@ void KeepAliveURLLoader::OnURLLoaderDisconnected() {
 
   if (!IsStarted()) {
     // May be the last chance to start a deferred loader.
+    LogFetchLaterMetric(
+        FetchLaterBrowserMetricType::kStartedAfterInitiatorDisconnected);
     Start();
   }
   // For other types of keepalive requests, this loader does not care about
@@ -873,8 +898,13 @@ void KeepAliveURLLoader::OnURLLoaderDisconnected() {
 }
 
 void KeepAliveURLLoader::OnDisconnectedLoaderTimerFired() {
-  // TODO(crbug.com/1465781): Add UMA histogram logging.
+  LogFetchLaterMetric(FetchLaterBrowserMetricType::kCancelledAfterTimeLimit);
   DeleteSelf();
+}
+
+bool KeepAliveURLLoader::IsFetchLater() const {
+  return base::FeatureList::IsEnabled(blink::features::kFetchLaterAPI) &&
+         resource_request_.is_fetch_later_api;
 }
 
 void KeepAliveURLLoader::DeleteSelf() {
