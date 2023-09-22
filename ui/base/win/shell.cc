e5189c5461e47c210bf2091e61417fcb77ec031d
Peter Kasting <pkasting@chromium.org>
2023-09-22 16:35:04 +0000

    Remove usage of wstring StringPrintf(): ui/
    This function is going away.
    
    Bug: 1371963
    Change-Id: Ie28d56db3a58bf7acbfb83bc645d1437cc723256
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4885240
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1200278}

--- ui/base/win/shell.cc
+++ ui/base/win/shell.cc
@@ -16,8 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/native_library.h"
+#include "base/strings/strcat.h"
+#include "base/strings/string_number_conversions_win.h"
 #include "base/strings/string_util.h"
-#include "base/strings/stringprintf.h"
 #include "base/threading/scoped_blocking_call.h"
 #include "base/threading/scoped_thread_priority.h"
 #include "base/win/win_util.h"
@@ -116,8 +117,8 @@ void SetAppDetailsForWindow(const std::wstring& app_id,
     // index notation when file path has commas.
     base::win::SetStringValueForPropertyStore(
         pps.Get(), PKEY_AppUserModel_RelaunchIconResource,
-        base::StringPrintf(L"%ls,%d", app_icon_path.value().c_str(),
-                           app_icon_index)
+        base::StrCat({app_icon_path.value(), L",",
+                      base::NumberToWString(app_icon_index)})
             .c_str());
   }
   if (!relaunch_command.empty()) {
