3d549dbc64412239e072700e9daee9415ef04afe
Matthias Körber <koerber@google.com>
2023-09-22 12:00:59 +0000

    Move address rewriter to geo folder
    The change in this CL is generated by the following command
    
    $ git mv c/a/c/b/address_rewrite* c/a/c/b/geo
    $ tools/git/mass-rename.sh
    
    Additionally, the paths to the rule files have been extended in the
    .grd file.
    
    Follow up work: move the .grd file
    
    Bug: 1485455
    Change-Id: Ie57cd8a827689b97205417923e63de7aec13f657
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4886210
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1200151}

--- components/autofill/core/browser/data_model/autofill_structured_address_utils.h
+++ components/autofill/core/browser/data_model/autofill_structured_address_utils.h
@@ -14,10 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_map.h"
 #include "base/no_destructor.h"
 #include "base/synchronization/lock.h"
-#include "components/autofill/core/browser/address_rewriter.h"
 #include "components/autofill/core/browser/autofill_type.h"
 #include "components/autofill/core/browser/data_model/autofill_structured_address_constants.h"
 #include "components/autofill/core/browser/field_types.h"
+#include "components/autofill/core/browser/geo/address_rewriter.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/re2/src/re2/re2.h"
 
--- components/autofill/core/browser/geo/address_rewriter.h
+++ components/autofill/core/browser/geo/address_rewriter.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_ADDRESS_REWRITER_H_
-#define COMPONENTS_AUTOFILL_CORE_BROWSER_ADDRESS_REWRITER_H_
+#ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_GEO_ADDRESS_REWRITER_H_
+#define COMPONENTS_AUTOFILL_CORE_BROWSER_GEO_ADDRESS_REWRITER_H_
 
 #include <string>
 
@@ -40,4 +40,4 @@ class AddressRewriter {
 
 }  // namespace autofill
 
-#endif  // COMPONENTS_AUTOFILL_CORE_BROWSER_ADDRESS_REWRITER_H_
+#endif  // COMPONENTS_AUTOFILL_CORE_BROWSER_GEO_ADDRESS_REWRITER_H_
