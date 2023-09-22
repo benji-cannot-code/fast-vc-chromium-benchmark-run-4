c382381ff93c7b204313195e5ffa07d9fa988c72
Gavin Williams <gavinwill@chromium.org>
2023-09-22 16:38:06 +0000

    Printing: Add feature flag for local printer observing
    This flag will be used to develop local printer observing for Print
    Preview and Printer settings.
    
    Bug: b:300136694
    Test: unit_tests --gtest_filter=AboutFlagsHistogramTest.CheckHistograms:AboutFlagsTest.EveryFlagHasMetadata
    Change-Id: I8e3f6aa7ae2f894cc756a1ddfe5474ee4effcbd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4884042
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200283}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2125,6 +2125,12 @@ BASE_FEATURE(kPreferConstantFrameRate,
              "PreferConstantFrameRate",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Enables the observing of local printers to provide UI surfaces with live
+// updates.
+BASE_FEATURE(kLocalPrinterObserving,
+             "LocalPrinterObserving",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // If enabled, the jelly colors will be used in the print management app.
 // Requires jelly-colors flag to also be enabled.
 BASE_FEATURE(kPrintManagementJelly,
@@ -3863,6 +3869,10 @@ bool IsPipDoubleTapToResizeEnabled() {
 
 bool IsPipPinchToResizeEnabled() {
   return base::FeatureList::IsEnabled(kPipPinchToResize);
+}
+
+bool IsLocalPrinterObservingEnabled() {
+  return base::FeatureList::IsEnabled(kLocalPrinterObserving);
 }
 
 bool IsPrinterSettingsPrinterStatusEnabled() {
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3850,6 +3850,9 @@ extern const char kPolicyProvidedTrustAnchorsAllowedAtLockScreenDescription[];
 extern const char kPreferDcheckName[];
 extern const char kPreferDcheckDescription[];
 
+extern const char kLocalPrinterObservingName[];
+extern const char kLocalPrinterObservingDescription[];
+
 extern const char kPrinterSettingsPrinterStatusName[];
 extern const char kPrinterSettingsPrinterStatusDescription[];
 
