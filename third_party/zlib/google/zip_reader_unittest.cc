e95c5add20b739a19919580983bdc1440f7210d6
Peter Kasting <pkasting@chromium.org>
2023-09-22 23:53:58 +0000

    Use UnlocalizedTimeFormatWithPattern() more: third_party/zlib/
    Bug: 1476086
    Change-Id: I0d9be8cb5a1e2f115dfb7cc8563e10c0682f202e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4888721
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200545}

--- third_party/zlib/google/zip_reader_unittest.cc
+++ third_party/zlib/google/zip_reader_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "base/functional/bind.h"
 #include "base/hash/md5.h"
+#include "base/i18n/time_formatting.h"
 #include "base/path_service.h"
 #include "base/run_loop.h"
 #include "base/strings/string_piece.h"
@@ -32,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
+#include "third_party/icu/source/i18n/unicode/timezone.h"
 #include "third_party/zlib/google/zip_internal.h"
 
 using ::testing::_;
@@ -289,18 +291,10 @@ TEST_F(ZipReaderTest, RegularFile) {
 
   EXPECT_EQ(target_path, entry->path);
   EXPECT_EQ(13527, entry->original_size);
-
-  // The expected time stamp: 2009-05-29 06:22:20
-  base::Time::Exploded exploded = {};  // Zero-clear.
-  entry->last_modified.UTCExplode(&exploded);
-  EXPECT_EQ(2009, exploded.year);
-  EXPECT_EQ(5, exploded.month);
-  EXPECT_EQ(29, exploded.day_of_month);
-  EXPECT_EQ(6, exploded.hour);
-  EXPECT_EQ(22, exploded.minute);
-  EXPECT_EQ(20, exploded.second);
-  EXPECT_EQ(0, exploded.millisecond);
-
+  EXPECT_EQ("2009-05-29 06:22:20.000",
+            base::UnlocalizedTimeFormatWithPattern(entry->last_modified,
+                                                   "y-MM-dd HH:mm:ss.SSS",
+                                                   icu::TimeZone::getGMT()));
   EXPECT_FALSE(entry->is_unsafe);
   EXPECT_FALSE(entry->is_directory);
 }
@@ -397,18 +391,10 @@ TEST_F(ZipReaderTest, Directory) {
   EXPECT_EQ(target_path, entry->path);
   // The directory size should be zero.
   EXPECT_EQ(0, entry->original_size);
-
-  // The expected time stamp: 2009-05-31 15:49:52
-  base::Time::Exploded exploded = {};  // Zero-clear.
-  entry->last_modified.UTCExplode(&exploded);
-  EXPECT_EQ(2009, exploded.year);
-  EXPECT_EQ(5, exploded.month);
-  EXPECT_EQ(31, exploded.day_of_month);
-  EXPECT_EQ(15, exploded.hour);
-  EXPECT_EQ(49, exploded.minute);
-  EXPECT_EQ(52, exploded.second);
-  EXPECT_EQ(0, exploded.millisecond);
-
+  EXPECT_EQ("2009-05-31 15:49:52.000",
+            base::UnlocalizedTimeFormatWithPattern(entry->last_modified,
+                                                   "y-MM-dd HH:mm:ss.SSS",
+                                                   icu::TimeZone::getGMT()));
   EXPECT_FALSE(entry->is_unsafe);
   EXPECT_TRUE(entry->is_directory);
 }
