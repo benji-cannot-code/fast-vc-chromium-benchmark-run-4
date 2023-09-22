d71f38b0228011f9cc528a11531178c24ea7d8e4
Lei Zhang <thestig@chromium.org>
2023-09-22 22:46:31 +0000

    Enable -Wexit-time-destructors for components/omnibox code
    Make sure production code in components/omnibox do not contain any exit
    time destructors. Remove existing ones. Along the way, tidy
    components/omnibox/browser/BUILD.gn.
    
    Bug: 101600
    Change-Id: I3725c7840199939ba28e03f3eecc76584a93b514
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4885260
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200520}

--- components/omnibox/browser/omnibox_field_trial.cc
+++ components/omnibox/browser/omnibox_field_trial.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/metrics/field_trial.h"
 #include "base/metrics/field_trial_params.h"
+#include "base/no_destructor.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
@@ -163,8 +164,8 @@ std::string GetValueForRuleInContextFromVariationParams(
 }
 
 OmniboxFieldTrial::MLConfig& GetMLConfigInternal() {
-  static OmniboxFieldTrial::MLConfig s_config;
-  return s_config;
+  static base::NoDestructor<OmniboxFieldTrial::MLConfig> s_config;
+  return *s_config;
 }
 
 bool IsKoreanLocale(const std::string& locale) {
