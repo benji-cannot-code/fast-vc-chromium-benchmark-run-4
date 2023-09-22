60234c683f2d1b2fdaf47ebbd936f3a98c9c36c4
Marc Treib <treib@chromium.org>
2023-09-22 14:34:30 +0000

    Sync E2E tests: Allow signing in without enabling Sync
    This CL adds the infrastructure for Sync E2E tests to sign in without
    enabling Sync, i.e. to cover Sync-the-transport.
    This functionality isn't used yet - a follow-up CL will introduce a
    first E2E test using Sync-the-transport.
    
    Bug: 1455032
    Change-Id: Idcbe419ac306d15052308e23707a0d4b22def71c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4887173
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200222}

--- chrome/browser/ui/webui/signin/login_ui_test_utils.h
+++ chrome/browser/ui/webui/signin/login_ui_test_utils.h
@@ -7,11 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_WEBUI_SIGNIN_LOGIN_UI_TEST_UTILS_H_
 
 #include <string>
-#include <vector>
 
 #include "base/time/time.h"
 #include "chrome/browser/ui/webui/signin/signin_email_confirmation_dialog.h"
-#include "components/signin/public/base/signin_metrics.h"
+#include "components/signin/public/base/consent_level.h"
 
 class Browser;
 
@@ -43,7 +42,8 @@ void SigninInOldGaiaFlow(content::WebContents* web_contents,
 // This will block until a signin succeeded or failed notification is observed.
 bool SignInWithUI(Browser* browser,
                   const std::string& email,
-                  const std::string& password);
+                  const std::string& password,
+                  signin::ConsentLevel consent_level);
 
 // Waits for sync confirmation dialog to get displayed, then executes javascript
 // to click on confirm button. Returns false if dialog wasn't dismissed before
