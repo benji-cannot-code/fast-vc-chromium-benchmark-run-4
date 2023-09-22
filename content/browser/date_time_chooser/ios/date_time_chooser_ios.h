2430ba1ea3dd18ca8ac08b851d6e70afc2a63958
Min Chen <minch@chromium.org>
2023-09-22 20:20:39 +0000

    Revert "[ios] Introduce DateTimeChooserCoordinator"
    This reverts commit 761286a2b3e8be93622be14b38ddd0be799332c3.
    
    Reason for revert: Might cause a test consistency failure on https://ci.chromium.org/ui/p/chromium/builders/luci.chromium.ci/chromeos-amd64-generic-rel
    
    Please feel free to reland if it is not related, thanks.
    
    Original change's description:
    > [ios] Introduce DateTimeChooserCoordinator
    >
    > This CL creates DateTimeChooser dialog using UI components on iOS and
    > handles the selected result. It introduces DateTimeChooserCoordinator,
    > DateTimeChooserMediator, DateTimeChooserController, and
    > DateTimeChooserDelegate to manage UI components and communicate with
    > them.
    >
    > These are created when mojom::DateTimeChooser is bound
    > and OpenDateTimeDialog() is called.
    >
    > Bug: 1461947
    > Change-Id: Iadf712e9f8fe7a879bd0362a02ed47263bdec499
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4699373
    > Reviewed-by: Mark Cogan <marq@chromium.org>
    > Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1200094}
    
    Bug: 1461947, b/301608467
    Change-Id: Ie11fffc341d610294b348f826a827942ba72c9da
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4886786
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Min Chen <minch@chromium.org>
    Auto-Submit: Min Chen <minch@chromium.org>
    Owners-Override: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200426}

--- content/browser/date_time_chooser/ios/date_time_chooser_ios.h
+++ content/browser/date_time_chooser/ios/date_time_chooser_ios.h
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_export.h"
 #include "third_party/blink/public/mojom/choosers/date_time_chooser.mojom-forward.h"
 
-@class DateTimeChooserCoordinator;
-
 namespace content {
 
 // IOS implementation for DateTimeChooser dialogs.
@@ -23,17 +21,11 @@ class CONTENT_EXPORT DateTimeChooserIOS : public DateTimeChooser {
   DateTimeChooserIOS(const DateTimeChooserIOS&) = delete;
   DateTimeChooserIOS& operator=(const DateTimeChooserIOS&) = delete;
 
-  // Called when a dialog is closed by a user action.
-  void OnDialogClosed(bool success, double value);
-
  protected:
   // DateTimeChooser:
   void OpenPlatformDialog(blink::mojom::DateTimeDialogValuePtr value,
                           OpenDateTimeDialogCallback callback) override;
   void ClosePlatformDialog() override;
-
- private:
-  DateTimeChooserCoordinator* __strong coordinator_;
 };
 
 }  // namespace content
