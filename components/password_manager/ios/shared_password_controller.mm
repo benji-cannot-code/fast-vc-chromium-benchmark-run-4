e9c7e0310e5ca8eb3389e3a22d7e6adc7f301ccb
Vincent Boisselle <vincb@google.com>
2023-09-22 17:00:39 +0000

    [ios][pwm] Use fill data even when no pwd forms detected in frame
    Related to http://go/bling-signin-uff
    
    --
    Problem:
    
    Currently, if there is no form with a password field in the frame,
    the -checkIfSuggestionsAvailableForForm query will be negative even if
    there was fill data processed for forms in the frame (e.g. coming
    from server suggestions).
    
    It isn't really a problem for 2-fields forms (username &
    password) but for single field forms it can be a problem, especially
    for username-only forms, which is a problem for sign-in UFF.
    
    It is frequent that a frame will only contain one username field in
    the first page of the sign-in UFF flow, so these fields will go under
    the radar of the current password form detection approach (because
    it needs at least one form with a password field in the
    frame), resulting in no suggestions at all for the username field.
    --
    
    --
    Solutions
    
    Here we propose a fix where -checkIfSuggestionsAvailableForForm
    look for fill data instead of detected forms in the frame.
    -checkIfSuggestionsAvailableForForm will be positive as soon as there
    is fill data (not empty) for the queried frame.
    
    Note that this CL won't yet enable the processing of fill data for
    password forms, but it is one important step towards supporting
    username-only forms. This is because username-only forms are still
    blocked here: http://shortn/_GDcvWaKNdO
    --
    
    --
    Side effects
    
    > We expect a drop in form extraction calls because having fill data
    for the frame will be considered as enough to try offering
    suggestions, which means that calling
    -suggestionHelperShouldTriggerFormExtraction is less likely to be
    needed.
    
    > The _fillDataMap may be a bit bigger on average because the fill
    data won't be cleaned up when there are no forms detected in the
    renderer. Before this change, a call to
    -processWithNoSavedCredentialsWithFrameId would reset the fill data
    cache, but it isn't the case anymore. The _fillDataMap is still
    cleaned up on page reload.
      - Note that the fill data only concerns the forms that could be
        parsed as password forms, so we should only expect a few different
        forms to cache per webstate/frame.
    
    --
    
    Bug: b/299279862
    Change-Id: I0074dea4cdc5c46ac5580d099310c182c170de35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4850699
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1200303}

--- components/password_manager/ios/shared_password_controller.mm
+++ components/password_manager/ios/shared_password_controller.mm
@@ -735,7 +735,6 @@ NSString* const kPasswordFormSuggestionSuffix = @" ••••••••";
   IOSPasswordManagerDriver* driver =
       [_driverHelper PasswordManagerDriver:frame];
   if (!forms.empty()) {
-    [self.suggestionHelper updateStateOnPasswordFormExtracted];
     UniqueIDDataTabHelper* uniqueIDDataTabHelper =
         UniqueIDDataTabHelper::FromWebState(_webState);
     // Update NextAvailableRendererId if a bigger value was extracted.
