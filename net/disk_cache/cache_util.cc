64125443e8a21c51b7d1fb13adf8c2f64de5dbcc
Peter Kasting <pkasting@chromium.org>
2023-09-22 18:17:48 +0000

    Remove usage of wstring StringPrintf(): net/
    This function is going away.
    
    Bug: 1371963
    Change-Id: Ifb618d5ed17f912fb64de74296af4f5d3d4b01ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4885335
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200356}

--- net/disk_cache/cache_util.cc
+++ net/disk_cache/cache_util.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/field_trial_params.h"
 #include "base/numerics/clamped_math.h"
 #include "base/numerics/ostream_operators.h"
+#include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
@@ -34,9 +35,15 @@ const int kMaxOldFolders = 100;
 base::FilePath GetPrefixedName(const base::FilePath& path,
                                const base::SafeBaseName& basename,
                                int index) {
-  const base::FilePath::StringType filename =
-      base::StringPrintf(FILE_PATH_LITERAL("old_%" PRFilePath "_%03d"),
-                         basename.path().value().c_str(), index);
+  const std::string index_str = base::StringPrintf("_%03d", index);
+  const base::FilePath::StringType filename = base::StrCat({
+    FILE_PATH_LITERAL("old_"), basename.path().value(),
+#if BUILDFLAG(IS_WIN)
+        base::ASCIIToWide(index_str)
+#else
+        index_str
+#endif
+  });
   return path.Append(filename);
 }
 
