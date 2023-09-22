2dc26727e1af245c320c50a280c168ddf3b6f71c
Riley Tatum <rtatum@google.com>
2023-09-22 17:20:46 +0000

    Wallpaper search simple form with styling
    * Copied over the input and button from wallpaper-search-simple for now
      while there are more design decisions being discussed. That way result
      functionality can be started.
    * Followed the designs for the submit/search button, unlike
      wallpaper-search-simple.
    
    Screenshot: http://screenshot/7BJUjLSGtF7UAh7
    
    Bug: b/300970975
    Change-Id: I3912ddbfaa186f999a0d041480bad3943bbba7a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4885232
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1200315}

--- chrome/browser/resources/side_panel/customize_chrome/wallpaper_search.ts
+++ chrome/browser/resources/side_panel/customize_chrome/wallpaper_search.ts
@@ -5,15 +5,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://customize-chrome-side-panel.top-chrome/shared/sp_heading.js';
 import 'chrome://customize-chrome-side-panel.top-chrome/shared/sp_shared_style.css.js';
+import 'chrome://resources/cr_elements/cr_button/cr_button.js';
+import 'chrome://resources/cr_elements/cr_input/cr_input.js';
 
 import {SpHeading} from 'chrome://customize-chrome-side-panel.top-chrome/shared/sp_heading.js';
+import {CrButtonElement} from 'chrome://resources/cr_elements/cr_button/cr_button.js';
+import {CrInputElement} from 'chrome://resources/cr_elements/cr_input/cr_input.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
+import {CustomizeChromeApiProxy} from './customize_chrome_api_proxy.js';
 import {getTemplate} from './wallpaper_search.html.js';
 
 export interface WallpaperSearchElement {
   $: {
     heading: SpHeading,
+    queryInput: CrInputElement,
+    submitButton: CrButtonElement,
   };
 }
 
@@ -27,8 +34,12 @@ export class WallpaperSearchElement extends PolymerElement {
   }
 
   static get properties() {
-    return {};
+    return {
+      query_: String,
+    };
   }
+
+  private query_: string;
 
   focusOnBackButton() {
     this.$.heading.getBackButton().focus();
@@ -36,6 +47,14 @@ export class WallpaperSearchElement extends PolymerElement {
 
   private async onBackClick_() {
     this.dispatchEvent(new Event('back-click'));
+  }
+
+  private async onSearchClick_() {
+    const {success} =
+        await CustomizeChromeApiProxy.getInstance().handler.searchWallpaper(
+            this.query_);
+    this.$.queryInput.invalid = !success;
+    this.$.queryInput.errorMessage = 'Error';
   }
 }
 
