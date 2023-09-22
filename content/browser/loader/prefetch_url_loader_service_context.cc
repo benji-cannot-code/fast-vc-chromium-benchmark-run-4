d702a540b280cff0ee72d1921e0b5cd4238142c0
Kenichi Ishibashi <bashi@chromium.org>
2023-09-22 04:24:05 +0000

    Remove unused includes from prefetch_url_loader_service_context.cc
    Bug: N/A
    Change-Id: I3bdf61d5b1eee290f7b512fb05245c0be8b1c292
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4885732
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200039}

--- content/browser/loader/prefetch_url_loader_service_context.cc
+++ content/browser/loader/prefetch_url_loader_service_context.cc
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/url_loader_throttles.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_client.h"
-#include "net/base/network_anonymization_key.h"
-#include "services/network/public/cpp/features.h"
 #include "third_party/blink/public/common/loader/url_loader_throttle.h"
 #include "third_party/blink/public/common/renderer_preferences/renderer_preferences.h"
 
