1ef1406d9ad2436980218b47322c80b5d4266bf7
Jose Dapena Paz <jdapena@igalia.com>
2023-09-22 08:53:09 +0000

    IWYU: constructing unique_ptr from nullptr requires type declaration
    Bug: 957519
    Change-Id: I98f2418ad4cda2ab2b2efcf6f951efbc1fb07691
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4878122
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1200123}

--- content/browser/webui/webui_config_map_unittest.cc
+++ content/browser/webui/webui_config_map_unittest.cc
@@ -5,13 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/browser/webui_config_map.h"
 
+#include "content/public/browser/web_ui_controller.h"
 #include "content/public/browser/webui_config.h"
 #include "content/public/test/scoped_web_ui_controller_factory_registration.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace content {
-
-class WebUIController;
 
 namespace {
 
