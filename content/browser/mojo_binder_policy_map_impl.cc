3f862620b3c60be64fbb1e3198de3db583f2e7d5
Kouhei Ueno <kouhei@chromium.org>
2023-09-22 20:33:20 +0000

    Put crbug.com/115920 fix under default enabled flag
    It appears the bug fix considerably improved INP and other responsive
    metrics on macOS. Put the fix under flag so we can measure its impact.
    
    Bug: 115920
    Change-Id: I7e6d3da47f74c5b15c202f254a717ddd21247d48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4887428
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200437}

--- content/browser/mojo_binder_policy_map_impl.cc
+++ content/browser/mojo_binder_policy_map_impl.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/mojo_binder_policy_map_impl.h"
 
+#include "base/feature_list.h"
 #include "base/no_destructor.h"
 #include "content/common/dom_automation_controller.mojom.h"
 #include "content/common/frame.mojom.h"
@@ -32,6 +33,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/page/display_cutout.mojom.h"
 
 namespace content {
+
+#if BUILDFLAG(IS_MAC)
+// Put crbug.com/115920 fix under flag, so we can measure its CWV impact.
+BASE_FEATURE(kTextInputHostMojoCapabilityControlWorkaround,
+             "TextInputHostMojoCapabilityControlWorkaround",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+#endif
 
 namespace {
 
@@ -89,8 +97,11 @@ void RegisterNonAssociatedPoliciesForSameOriginPrerendering(
   // This is used to return macOS IME sync call results to the browser process,
   // and will hang entire Chrome if paused.
   // This is a prospective fix added for crbug.com/1480850
-  map.SetNonAssociatedPolicy<blink::mojom::TextInputHost>(
-      MojoBinderNonAssociatedPolicy::kGrant);
+  if (base::FeatureList::IsEnabled(
+          kTextInputHostMojoCapabilityControlWorkaround)) {
+    map.SetNonAssociatedPolicy<blink::mojom::TextInputHost>(
+        MojoBinderNonAssociatedPolicy::kGrant);
+  }
 #endif
 }
 
