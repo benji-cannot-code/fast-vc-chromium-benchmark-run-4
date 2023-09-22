a9bc72e0cc1ed7acfd2bc470ca40385ac1a49c2c
Xinglong Luan <alanlxl@google.com>
2023-09-22 01:46:06 +0000

    Add freeform enums
    Bug: b/288019728
    Test: unit_tests --gtest_filter="OrcaProviderTest.*"
    Test: verified on DUT
    Change-Id: I07c74f8ac88a36730684d44d046505e944acdfb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4885729
    Commit-Queue: Xinglong Luan <alanlxl@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199986}

--- chrome/browser/manta/orca_provider.cc
+++ chrome/browser/manta/orca_provider.cc
@@ -49,6 +49,8 @@ GetToneMap() {
           {"REPHRASE", proto::RequestConfig::REPHRASE},
           {"FORMALIZE", proto::RequestConfig::FORMALIZE},
           {"EMOJIFY", proto::RequestConfig::EMOJIFY},
+          {"FREEFORM_REWRITE", proto::RequestConfig::FREEFORM_REWRITE},
+          {"FREEFORM_WRITE", proto::RequestConfig::FREEFORM_WRITE},
       });
 
   return *tone_map;
