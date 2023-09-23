6d443fec38547e8ec71ceb650ed11ed92592a505
Wes Okuhara <wesokuhara@google.com>
2023-09-23 01:45:48 +0000

    CrOS Settings: Add missing return types (4/N)
    Adds missing returns types to the search and assistant page files.
    This is done in preparation for enabling an ESLint rule enforcing
    return types on functions.
    
    Bug: b/301145435
    Change-Id: Ic22ae4a597a8365663d118f722f705b972afd52c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4888745
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Commit-Queue: Connie Xu <conniekxu@chromium.org>
    Auto-Submit: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1200579}

--- chrome/browser/resources/ash/settings/os_search_page/google_assistant_browser_proxy.ts
+++ chrome/browser/resources/ash/settings/os_search_page/google_assistant_browser_proxy.ts
@@ -31,15 +31,15 @@ export class GoogleAssistantBrowserProxyImpl implements
     instance = obj;
   }
 
-  showGoogleAssistantSettings() {
+  showGoogleAssistantSettings(): void {
     chrome.send('showGoogleAssistantSettings');
   }
 
-  retrainAssistantVoiceModel() {
+  retrainAssistantVoiceModel(): void {
     chrome.send('retrainAssistantVoiceModel');
   }
 
-  syncVoiceModelStatus() {
+  syncVoiceModelStatus(): void {
     chrome.send('syncVoiceModelStatus');
   }
 }
