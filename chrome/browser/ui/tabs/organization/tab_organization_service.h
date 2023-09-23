5994de3cea08a1efe9e2a8e22fb9acd2fe8b0f2b
David Pennington <dpenning@chromium.org>
2023-09-23 01:08:52 +0000

    [TabOrganization] Create IsValidForOrganizing function for TabData.
    Change-Id: I9516d0e9a2750a31199e21cb99e047a150e0ff0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4863186
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200570}

--- chrome/browser/ui/tabs/organization/tab_organization_service.h
+++ chrome/browser/ui/tabs/organization/tab_organization_service.h
@@ -47,7 +47,7 @@ class TabOrganizationService : public KeyedService {
   }
 
  private:
-  // mapping of browser to
+  // mapping of browser to session.
   BrowserSessionMap browser_session_map_;
 
   // A list of the observers of a tab organization Service.
