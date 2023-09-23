4e2741b9f6af525c6c37f2b953e11f50941c00d2
David Padlipsky <dpad@google.com>
2023-09-23 01:56:47 +0000

    accelerators: Add state machines for capslock handling
    - Adds capslock state machine to determine when Alt + Search is a valid
      accelerator to toggle capslock.
    - Adds capslock disable state machine to determine when Shift is a valid
      accelerator to disable capslock.
    
    Bug: b/285970320
    Change-Id: I051f0006706f36603a1503ac12f963407bf5e987
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4730457
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1200581}

--- ash/accelerators/accelerator_controller_impl.h
+++ ash/accelerators/accelerator_controller_impl.h
@@ -13,9 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <vector>
 
+#include "ash/accelerators/accelerator_capslock_state_machine.h"
 #include "ash/accelerators/accelerator_history_impl.h"
 #include "ash/accelerators/accelerator_launcher_state_machine.h"
 #include "ash/accelerators/accelerator_prefs.h"
+#include "ash/accelerators/accelerator_shift_disable_capslock_state_machine.h"
 #include "ash/accelerators/accelerator_table.h"
 #include "ash/accelerators/ash_accelerator_configuration.h"
 #include "ash/accelerators/exit_warning_handler.h"
@@ -243,6 +245,9 @@ class ASH_EXPORT AcceleratorControllerImpl
   // A tracker for the current and previous accelerators.
   std::unique_ptr<AcceleratorHistoryImpl> accelerator_history_;
   std::unique_ptr<AcceleratorLauncherStateMachine> launcher_state_machine_;
+  std::unique_ptr<AcceleratorCapslockStateMachine> capslock_state_machine_;
+  std::unique_ptr<AcceleratorShiftDisableCapslockStateMachine>
+      shift_disable_state_machine_;
 
   // Manages all accelerator mappings.
   raw_ptr<AshAcceleratorConfiguration, ExperimentalAsh>
