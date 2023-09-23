5e9829e2df0f232d41b7b29e1a7aa3af9e55a067
Wenyu Fu <wenyufu@google.com>
2023-09-23 01:37:40 +0000

    [Survey] Add SurveyUiDelegateBridge
    Add SurveyUiDelegateBridge that can be created from C++.
    
    Bug: 1466724, 1400731
    Change-Id: Ibd6861eac585c2a516e93e6fb49464254e256dec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4884829
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1200577}

---
+++ chrome/browser/ui/android/hats/survey_ui_delegate_android.cc
@@ -0,0 +1,51 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/android/hats/survey_ui_delegate_android.h"
+
+#include "chrome/browser/ui/android/hats/internal/jni_headers/SurveyUiDelegateBridge_jni.h"
+#include "components/messages/android/message_wrapper.h"
+#include "ui/android/window_android.h"
+
+using base::android::JavaRef;
+using base::android::ScopedJavaLocalRef;
+
+namespace hats {
+
+SurveyUiDelegateAndroid::SurveyUiDelegateAndroid() {
+  JNIEnv* env = base::android::AttachCurrentThread();
+  jobj_ =
+      Java_SurveyUiDelegateBridge_create(env, reinterpret_cast<int64_t>(this));
+}
+
+SurveyUiDelegateAndroid::SurveyUiDelegateAndroid(
+    messages::MessageWrapper* messages,
+    ui::WindowAndroid* window) {
+  JNIEnv* env = base::android::AttachCurrentThread();
+  jobj_ = Java_SurveyUiDelegateBridge_createFromMessage(
+      env, reinterpret_cast<int64_t>(this), messages->java_window_android(),
+      window->GetJavaObject());
+}
+
+SurveyUiDelegateAndroid::~SurveyUiDelegateAndroid() = default;
+
+void SurveyUiDelegateAndroid::ShowSurveyInvitation(
+    JNIEnv* env,
+    const JavaParamRef<jobject>& on_accepted_callback,
+    const JavaParamRef<jobject>& on_declined_callback,
+    const JavaParamRef<jobject>& on_presentation_failed_callback) {
+  LOG(WARNING) << "Unimplemented SurveyUiDelegateAndroid::ShowSurveyInvitation";
+}
+
+void SurveyUiDelegateAndroid::Dismiss(JNIEnv* env) {
+  LOG(WARNING) << "Unimplemented SurveyUiDelegateAndroid::Dismiss";
+}
+
+const JavaRef<jobject>& SurveyUiDelegateAndroid::GetJavaObject(
+    JNIEnv* env) const {
+  return jobj_;
+}
+
+}  // namespace hats
