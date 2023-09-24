974db06e4c36822fdea79463de6d49fc06212ee8
Finnur Thorarinsson <finnur@chromium.org>
2023-09-24 11:12:16 +0000

    [PwaRestore]: Activate the Back button (arrow).
    The back button makes the bottom sheet go back to
    the Preview (peeking) state.
    
    Bug: 1481570
    Change-Id: I14fc47e2679a5ed0472772c828fbd66e59d4c663
    
    I'll spend some time on Monday adding test coverage,
    but the percentage drops measured by code coverage
    don't mean much because the file is very small to
    begin with. The lines (missing coverage) are hard
    to test except with an on-device test, which
    is planned for later.
    
    Low-Coverage-Reason: It is almost 9pm on a Friday,
    Change-Id: I14fc47e2679a5ed0472772c828fbd66e59d4c663
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4886372
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200721}

--- components/webapps/browser/android/java/src/org/chromium/components/webapps/pwa_restore_ui/PwaRestoreBottomSheetCoordinator.java
+++ components/webapps/browser/android/java/src/org/chromium/components/webapps/pwa_restore_ui/PwaRestoreBottomSheetCoordinator.java
@@ -34,7 +34,8 @@ public class PwaRestoreBottomSheetCoordinator {
         mView = new PwaRestoreBottomSheetView(activity);
         mView.initialize(backArrowId);
         mContent = new PwaRestoreBottomSheetContent(mView);
-        mMediator = new PwaRestoreBottomSheetMediator(activity, this::onReviewButtonClicked);
+        mMediator = new PwaRestoreBottomSheetMediator(
+                activity, this::onReviewButtonClicked, this::onBackButtonClicked);
 
         PropertyModelChangeProcessor.create(
                 mMediator.getModel(), mView, PwaRestoreBottomSheetViewBinder::bind);
@@ -51,6 +52,11 @@ public class PwaRestoreBottomSheetCoordinator {
     protected void onReviewButtonClicked() {
         mMediator.setPreviewState();
         mController.expandSheet();
+    }
+
+    protected void onBackButtonClicked() {
+        mMediator.setPeekingState();
+        mController.collapseSheet(/* animate= */ true);
     }
 
     protected PropertyModel getModelForTesting() {
