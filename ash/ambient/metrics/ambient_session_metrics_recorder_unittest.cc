6a33bd4ae6eb4023b940cf38bbb1214470567ae1
Eric Sum <esum@google.com>
2023-09-25 19:25:42 +0000

    Add metrics to ambient mode initialization.
    New metrics to track how often each of the ambient modes successfully
    initialize the data necessary for the UI (downloading photos, videos,
    etc). This will be important for the DLC rollout to track how often
    installing the video DLC succeeds/fails.
    
    Bug: b:289085706
    Change-Id: I41daf40546417183bf2fa221e2a7581a5bfa52b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4878135
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Eric Sum <esum@google.com>
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201113}

--- ash/ambient/metrics/ambient_session_metrics_recorder_unittest.cc
+++ ash/ambient/metrics/ambient_session_metrics_recorder_unittest.cc
@@ -68,6 +68,7 @@ TEST_P(AmbientSessionMetricsRecorderTest, MetricsEngagementTime) {
   Shell::Get()->tablet_mode_controller()->SetEnabledForTest(false);
   {
     AmbientSessionMetricsRecorder recorder(GetParam());
+    recorder.SetInitStatus(true);
     recorder.RegisterScreen();
     task_environment()->FastForwardBy(kExpectedEngagementTime);
   }
@@ -83,6 +84,7 @@ TEST_P(AmbientSessionMetricsRecorderTest, MetricsEngagementTime) {
   Shell::Get()->tablet_mode_controller()->SetEnabledForTest(true);
   {
     AmbientSessionMetricsRecorder recorder(GetParam());
+    recorder.SetInitStatus(true);
     recorder.RegisterScreen();
     task_environment()->FastForwardBy(kExpectedEngagementTime);
   }
@@ -100,6 +102,7 @@ TEST_P(AmbientSessionMetricsRecorderTest, MetricsStartupTime) {
   base::HistogramTester histogram_tester;
   AmbientSessionMetricsRecorder recorder(GetParam());
   task_environment()->FastForwardBy(kExpectedStartupTime);
+  recorder.SetInitStatus(true);
   recorder.RegisterScreen();
   // Should be ignored. The time that the first screen starts rendering should
   // be when the startup time is recorded.
@@ -120,12 +123,36 @@ TEST_P(AmbientSessionMetricsRecorderTest, MetricsStartupTimeFailedToStart) {
   histogram_tester.ExpectUniqueTimeSample(
       base::StrCat({"Ash.AmbientMode.StartupTime.", GetParam().ToString()}),
       kFailedStartupTime, 1);
+  histogram_tester.ExpectUniqueSample(
+      base::StrCat({"Ash.AmbientMode.Init.", GetParam().ToString()}),
+      /*sample=*/0, /*expected_count=*/1);
+}
+
+TEST_P(AmbientSessionMetricsRecorderTest, InitStatusSuccess) {
+  base::HistogramTester histogram_tester;
+  AmbientSessionMetricsRecorder recorder(GetParam());
+  recorder.SetInitStatus(true);
+  recorder.RegisterScreen();
+  recorder.RegisterScreen();
+  histogram_tester.ExpectUniqueSample(
+      base::StrCat({"Ash.AmbientMode.Init.", GetParam().ToString()}),
+      /*sample=*/1, /*expected_count=*/1);
+}
+
+TEST_P(AmbientSessionMetricsRecorderTest, InitStatusFailed) {
+  base::HistogramTester histogram_tester;
+  AmbientSessionMetricsRecorder recorder(GetParam());
+  recorder.SetInitStatus(false);
+  histogram_tester.ExpectUniqueSample(
+      base::StrCat({"Ash.AmbientMode.Init.", GetParam().ToString()}),
+      /*sample=*/0, /*expected_count=*/1);
 }
 
 TEST_P(AmbientSessionMetricsRecorderTest, RecordsScreenCount) {
   base::HistogramTester histogram_tester;
   {
     AmbientSessionMetricsRecorder recorder(GetParam());
+    recorder.SetInitStatus(true);
     recorder.RegisterScreen();
   }
   histogram_tester.ExpectUniqueSample(
@@ -133,6 +160,7 @@ TEST_P(AmbientSessionMetricsRecorderTest, RecordsScreenCount) {
       /*expected_bucket_count=*/1);
   {
     AmbientSessionMetricsRecorder recorder(GetParam());
+    recorder.SetInitStatus(true);
     recorder.RegisterScreen();
     recorder.RegisterScreen();
   }
