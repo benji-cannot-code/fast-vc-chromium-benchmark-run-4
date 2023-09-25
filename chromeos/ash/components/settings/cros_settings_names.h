756e62e827dc2cbce2ee8bad4ffb1e5b31310b8d
Andrei Nedoluzhko <nedol@google.com>
2023-09-25 10:12:36 +0000

    Add DeviceDlcPredownloadList policy
    Add the DeviceDlcPredownloadList device policy that will be
    used to pre download certain DLCs to the device.
    
    DD: https://goto.google.com/dlc-predownload-policy
    Bug: b/297010127
    Change-Id: Icef1fa594c368b0d8da944851f18a02410b9854c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4868219
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Sparik Hayrapetyan <ust@google.com>
    Commit-Queue: Andrei Nedoluzhko <nedol@google.com>
    Cr-Commit-Position: refs/heads/main@{#1200892}

--- chromeos/ash/components/settings/cros_settings_names.h
+++ chromeos/ash/components/settings/cros_settings_names.h
@@ -397,6 +397,9 @@ extern const char kDevicePrintingClientNameTemplate[];
 COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_SETTINGS)
 extern const char kDeviceHindiInscriptLayoutEnabled[];
 
+COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_SETTINGS)
+extern const char kDeviceDlcPredownloadList[];
+
 }  // namespace ash
 
 namespace chromeos {
