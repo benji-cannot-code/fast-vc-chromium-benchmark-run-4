4a81fa7ced97caba3cbb1936d8e373410e283325
Xinyi Ji <xinyiji@google.com>
2023-09-25 18:05:27 +0000

    [SurfacePolish] Polish the scroll-up animation for search box in NTP (phone)
    This CL makes the scrolling-up animation smooth by adding an offset for the transition length to make sure the animation is complete only when the user scrolls up the page to the correct position.
    
    The original animation(we could find the real omnibox still goes up a little after the animation is finished when we scroll up the page):
    https://drive.google.com/file/d/1mcgldDdyMcZFoTGW-7Nj9vlBauQ6lf62/view?usp=sharing&resourcekey=0-UhCYxdHDhatVoPCM69y6WQ
    The animation after this CL:
    https://drive.google.com/file/d/1lp_oTZ00SrSWmtUdfl43Fqa-eCT6nzaG/view?usp=sharing&resourcekey=0-p_EJyeJ1bWLv_cbu8_Mu6w
    
    A summary of the animation between the fake omnibox and the real one is written in the following link:
    https://docs.google.com/document/d/1SFxKRbsNyF3_a_CDBRCUHLTTa5uILE3tEh-01p82teI/edit?usp=sharing
    
    Bug: 1449683
    Change-Id: Ie663121885cbdc40977357c7c4957d89eff9fdb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4879757
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201084}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/SnapScrollHelperImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/SnapScrollHelperImpl.java
@@ -15,6 +15,7 @@ import androidx.annotation.VisibleForTesting;
 
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.feed.SnapScrollHelper;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 
 /**
  * This class handles snap scroll for the search box on a {@link NewTabPage}.
@@ -28,6 +29,7 @@ public class SnapScrollHelperImpl implements SnapScrollHelper {
     private final Runnable mUpdateSearchBoxOnScrollRunnable;
     private final int mToolbarHeight;
     private final int mSearchBoxTransitionLength;
+    private final int mSearchBoxTransitionLengthOffset;
 
     private View mView;
     private boolean mPendingSnapScroll;
@@ -49,6 +51,9 @@ public class SnapScrollHelperImpl implements SnapScrollHelper {
                 + res.getDimensionPixelSize(R.dimen.toolbar_progress_bar_height);
         mSearchBoxTransitionLength =
                 res.getDimensionPixelSize(R.dimen.ntp_search_box_transition_length);
+        mSearchBoxTransitionLengthOffset = ChromeFeatureList.sSurfacePolish.isEnabled()
+                ? res.getDimensionPixelSize(R.dimen.ntp_search_box_transition_length_polish_offset)
+                : 0;
     }
 
     /** @param view The view on which this class needs to handle snap scroll. */
@@ -121,7 +126,8 @@ public class SnapScrollHelperImpl implements SnapScrollHelper {
             View fakeBox = mNewTabPageLayout.getSearchBoxView();
             int fakeBoxUpperBound = fakeBox.getTop() + fakeBox.getPaddingTop();
             scrollPosition = calculateSnapPositionForRegion(scrollPosition,
-                    fakeBoxUpperBound - mSearchBoxTransitionLength, fakeBoxUpperBound);
+                    fakeBoxUpperBound - mSearchBoxTransitionLength,
+                    fakeBoxUpperBound + mSearchBoxTransitionLengthOffset);
         }
 
         return scrollPosition;
