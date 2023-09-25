ac9c32a4ca565a8a3c374d4f1e5f18a5a9663fe2
Jack Yammine <jyammine@google.com>
2023-09-25 10:17:19 +0000

    [waffle]Add search engine icons to the UI on desktop
    Screenshot: http://b/299066235#comment8
    Fixed: b/299066235
    Change-Id: Ie53d49d76d7b47f85a02fe631a946bcc33c56b0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4876658
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Reviewed-by: Jens Mueller <muellerj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1200895}

--- chrome/browser/resources/search_engine_choice/browser_proxy.ts
+++ chrome/browser/resources/search_engine_choice/browser_proxy.ts
@@ -13,6 +13,8 @@ import {PageHandlerFactory, PageHandlerInterface, PageHandlerRemote} from './sea
 export interface SearchEngineChoice {
   prepopulate_id: number;
   name: string;
+  icon_path: string;
+  url: string;
 }
 
 export class SearchEngineChoiceBrowserProxy {
