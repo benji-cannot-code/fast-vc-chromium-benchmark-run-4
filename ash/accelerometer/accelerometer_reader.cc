8e9abc3c0e05e791dbc87c6c12c40e377a9cf8f0
Harvey Yang <chenghaoyang@chromium.org>
2023-09-25 06:49:42 +0000

    Reland "Revert "sensors: Add USE_IIOSERVICE build flag""
    This is a reland of commit d1dddb73a251aee4fde9d16a28c495f709ac5ee0
    
    Amended with the last three usages of sensors' buildflag.
    
    Original change's description:
    > Revert "sensors: Add USE_IIOSERVICE build flag"
    >
    > This reverts commit bfdd738101df79804f8d865ec13bb604eaf0a7cc.
    >
    > Reason for revert: All ChromeOS models are using iioservice now.
    > The flag is not used anymore.
    >
    > Original change's description:
    > > sensors: Add USE_IIOSERVICE build flag
    > >
    > > The flag indicates if IIO Service is running in ChromeOS, which
    > > determines sensor clients to use Mojo implementations or not.
    > >
    > > BUG=b:173485266, b:168434557, b:172414302
    > >
    > > Change-Id: Ib89dea49b03671e9ba36f19720c30e6035521731
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2624327
    > > Commit-Queue: Cheng-Hao Yang <chenghaoyang@chromium.org>
    > > Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#843397}
    >
    > BUG=b:173485266, b:168434557, b:172414302
    >
    > Change-Id: If193ba3584901041879870436167eb7179780ae3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4875907
    > Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    > Reviewed-by: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    > Commit-Queue: Harvey Yang <chenghaoyang@chromium.org>
    > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1200007}
    
    BUG=b:173485266, b:168434557, b:172414302
    
    Change-Id: Ib9f5ae8aed50311eafb15155f5083218bd1a1403
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4885756
    Commit-Queue: Harvey Yang <chenghaoyang@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200839}

--- ash/accelerometer/accelerometer_reader.cc
+++ ash/accelerometer/accelerometer_reader.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "base/task/current_thread.h"
 #include "base/task/sequenced_task_runner.h"
-#include "chromeos/components/sensors/buildflags.h"
 #include "ash/accelerometer/accelerometer_provider_mojo.h"
 
 namespace ash {
--- chrome/browser/ash/power/auto_screen_brightness/als_reader.cc
+++ chrome/browser/ash/power/auto_screen_brightness/als_reader.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/task/thread_pool.h"
-#include "chromeos/components/sensors/buildflags.h"
 #include "chrome/browser/ash/power/auto_screen_brightness/light_provider_mojo.h"
 #include "content/public/browser/browser_thread.h"
 
