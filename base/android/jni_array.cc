1764c5fb8b8cc7a845a1f6aae58651d19132e55f
Torne (Richard Coles) <torne@google.com>
2023-09-25 20:52:00 +0000

    android: correct type for ToTypedJavaArrayOfObjects
    base::android::ToTypedJavaArrayOfObjects only needs a temporary
    reference to the jclass for the array type and so can take a JavaRef& to
    avoid the caller having to construct a ScopedJavaLocalRef.
    
    Change-Id: Ie2e0a4fc562ca99f160aeda4cf9c06585a596526
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4887306
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Auto-Submit: Richard (Torne) Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201133}

--- base/android/jni_array.cc
+++ base/android/jni_array.cc
@@ -175,7 +175,7 @@ BASE_EXPORT ScopedJavaLocalRef<jobjectArray> ToJavaArrayOfObjects(
 BASE_EXPORT ScopedJavaLocalRef<jobjectArray> ToTypedJavaArrayOfObjects(
     JNIEnv* env,
     base::span<const ScopedJavaLocalRef<jobject>> v,
-    ScopedJavaLocalRef<jclass> type) {
+    const JavaRef<jclass>& type) {
   jobjectArray joa =
       env->NewObjectArray(checked_cast<jsize>(v.size()), type.obj(), nullptr);
   CheckException(env);
@@ -189,7 +189,7 @@ BASE_EXPORT ScopedJavaLocalRef<jobjectArray> ToTypedJavaArrayOfObjects(
 BASE_EXPORT ScopedJavaLocalRef<jobjectArray> ToTypedJavaArrayOfObjects(
     JNIEnv* env,
     base::span<const ScopedJavaGlobalRef<jobject>> v,
-    ScopedJavaLocalRef<jclass> type) {
+    const JavaRef<jclass>& type) {
   jobjectArray joa =
       env->NewObjectArray(checked_cast<jsize>(v.size()), type.obj(), nullptr);
   CheckException(env);
