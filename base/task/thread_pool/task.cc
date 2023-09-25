005b23762c81373c7e58e0c5a827c2cfe092cdc2
Etienne Pierre-doray <etiennep@chromium.org>
2023-09-25 19:27:04 +0000

    [task] Move sequence number to task tracker
    This avoids having a global AtomicSequenceNumber and
    makes base::internal::Task trivial (can be removed in follow up).
    
    Change-Id: I12127e4d0952d715aba63fb1b44a2f3434df0a6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4887296
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201116}

--- base/task/thread_pool/task.cc
+++ base/task/thread_pool/task.cc
@@ -7,16 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "base/atomic_sequence_num.h"
-
 namespace base {
 namespace internal {
-
-namespace {
-
-AtomicSequenceNumber g_sequence_nums_for_tracing;
-
-}  // namespace
 
 Task::Task(const Location& posted_from,
            OnceClosure task,
@@ -40,15 +32,7 @@ Task::Task(const Location& posted_from,
                   queue_time,
                   delayed_run_time,
                   leeway,
-                  delay_policy) {
-  // ThreadPoolImpl doesn't use |sequence_num| but tracing (toplevel.flow)
-  // relies on it being unique. While this subtle dependency is a bit
-  // overreaching, ThreadPoolImpl is the only task system that doesn't use
-  // |sequence_num| and the dependent code rarely changes so this isn't worth a
-  // big change and faking it here isn't too bad for now (posting tasks is full
-  // of atomic ops already).
-  this->sequence_num = g_sequence_nums_for_tracing.GetNext();
-}
+                  delay_policy) {}
 
 // This should be "= default but MSVC has trouble with "noexcept = default" in
 // this case.
