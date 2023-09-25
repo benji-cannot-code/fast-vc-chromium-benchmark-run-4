58b3842c48e2170e5b3b1370a061cbbd7c041c76
Chuong Ho <hdchuong@google.com>
2023-09-25 06:15:50 +0000

    settings: make the suggestions subpage visible when at least orca or emoji suggestion is allowed.
    There is a bug that the visibility of the suggestion page only depends
    on whether emoji suggestions are allowed or not. This means that the
    suggestion section will be hidden when orca is allowed and emoji
    suggestion is not.
    
    This CL fixes that issue by adding extra logic:
    `visibility of suggestion = orca_allowed OR emoji_allowed`
    
    Bug: b:294956432
    Change-Id: I86ad64ba94ccbda01b03c3589aa51b9db1e093bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4886183
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Chuong Ho <hdchuong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1200831}

--- chrome/browser/resources/ash/settings/os_languages_page/input_page.ts
+++ chrome/browser/resources/ash/settings/os_languages_page/input_page.ts
@@ -147,6 +147,8 @@ export class OsSettingsInputPageElement extends OsSettingsInputPageElementBase {
       allowEmojiSuggestion_: Boolean,
 
       allowOrca_: Boolean,
+
+      allowSuggestionSection_: Boolean,
     };
   }
 
@@ -184,6 +186,8 @@ export class OsSettingsInputPageElement extends OsSettingsInputPageElementBase {
   private readonly allowEmojiSuggestion_: boolean =
       loadTimeData.getBoolean('allowEmojiSuggestion');
   private readonly allowOrca_: boolean = loadTimeData.getBoolean('allowOrca');
+  private readonly allowSuggestionSection_: boolean =
+      this.allowOrca_ || this.allowEmojiSuggestion_;
 
   // Computed properties.
   private spellCheckLanguages_: SpellCheckLanguageState[]|undefined;
