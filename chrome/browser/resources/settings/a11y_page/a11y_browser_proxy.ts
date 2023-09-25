0352118cb9eef455eccedb4084ab439b271a0266
Charles Meng <charlesmeng@chromium.org>
2023-09-25 21:33:05 +0000

    Add metric for overscroll history navigation
    Adds a boolean metric "Settings.OverscrollHistoryNavigation.Enabled"
    that is logged whenever the user toggles the overscroll history
    navigation setting. Rename mac_system_settings_browser_proxy.ts because
    now it is doing multiple things for the accessibility page.
    
    Bug: 1485640
    Change-Id: Id74fd7b4d8a782b1756cb1c1f40bfd6bdfa53950
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4884150
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Charles Meng <charlesmeng@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201162}

---
+++ chrome/browser/resources/settings/a11y_page/a11y_browser_proxy.ts
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+export interface AccessibilityBrowserProxy {
+  openTrackpadGesturesSettings(): void;
+  recordOverscrollHistoryNavigationChanged(enabled: boolean): void;
+}
+
+export class AccessibilityBrowserProxyImpl implements
+    AccessibilityBrowserProxy {
+  openTrackpadGesturesSettings() {
+    chrome.send('openTrackpadGesturesSettings');
+  }
+
+  recordOverscrollHistoryNavigationChanged(enabled: boolean) {
+    chrome.metricsPrivate.recordBoolean(
+        'Settings.OverscrollHistoryNavigation.Enabled', enabled);
+  }
+
+  static getInstance(): AccessibilityBrowserProxy {
+    return instance || (instance = new AccessibilityBrowserProxyImpl());
+  }
+
+  static setInstance(obj: AccessibilityBrowserProxy) {
+    instance = obj;
+  }
+}
+
+let instance: AccessibilityBrowserProxy|null = null;
