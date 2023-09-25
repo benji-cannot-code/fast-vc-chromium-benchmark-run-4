2d7a306b29129591e3c258f14e3fcf616cab1af6
Olivier Li <olivierli@chromium.org>
2023-09-25 23:16:22 +0000

    Update DTrace file parsing to support changes in input format
    Change-Id: Idc28eefc100d2b33e04af2f6d356cd65eecc9415
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4851482
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201200}

--- tools/mac/power/export_dtrace_test.py
+++ tools/mac/power/export_dtrace_test.py
@@ -48,7 +48,7 @@ class DTraceReadTest(unittest.TestCase):
     self.assertEquals(collapser.GetSamplesListForTesting(), [{
         'frames': [
             ('unsymbolized module', '0x21'),
-            ('unsymbolized module', '0x85'),
+            ('unsymbolized module', '+0x85'),
             ('unsymbolized module', '0x37'),
         ],
         'weight':
