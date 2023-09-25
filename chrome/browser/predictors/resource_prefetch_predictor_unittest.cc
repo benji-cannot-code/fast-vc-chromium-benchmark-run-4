6613715351e6c58482c32fd06e7a42049dabb04e
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2023-09-25 05:22:29 +0000

    LCPP: Factor out core part of LearnLCPP from ResourcePrefetchPredictor.
    This change list factor out the code to update LcppData to LCPP's
    directory for ease of maintenance.
    
    Bug: 1419756
    Change-Id: I7f80a10c1628793d4990436d6e3af64daae43ad8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4881959
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200820}

--- chrome/browser/predictors/resource_prefetch_predictor_unittest.cc
+++ chrome/browser/predictors/resource_prefetch_predictor_unittest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_simple_task_runner.h"
 #include "base/time/time.h"
 #include "chrome/browser/history/history_service_factory.h"
+#include "chrome/browser/predictors/lcp_critical_path_predictor/lcp_critical_path_predictor_util.h"
 #include "chrome/browser/predictors/loading_predictor.h"
 #include "chrome/browser/predictors/loading_test_util.h"
 #include "chrome/browser/predictors/predictors_features.h"
@@ -144,6 +145,15 @@ class ResourcePrefetchPredictorTest : public testing::Test {
   }
 
   void InitializeSampleData();
+
+  void LearnLcpp(const std::string& host,
+                 const std::string& lcp_element_locator,
+                 const std::vector<GURL>& lcp_influencer_scripts) {
+    predictors::LcppDataInputs inputs;
+    inputs.lcp_element_locator = lcp_element_locator;
+    inputs.lcp_influencer_scripts = lcp_influencer_scripts;
+    predictor_->LearnLcpp(host, inputs);
+  }
 
   content::BrowserTaskEnvironment task_environment_;
   std::unique_ptr<TestingProfile> profile_;
@@ -1095,7 +1105,7 @@ TEST_F(ResourcePrefetchPredictorTest, LearnLcpp) {
   };
 
   for (int i = 0; i < 3; ++i) {
-    predictor_->LearnLcpp("a.com", "/#a", {});
+    LearnLcpp("a.com", "/#a", {});
   }
   {
     LcppData data = CreateLcppData("a.com", 10);
@@ -1105,7 +1115,7 @@ TEST_F(ResourcePrefetchPredictorTest, LearnLcpp) {
   }
 
   for (int i = 0; i < 2; ++i) {
-    predictor_->LearnLcpp("a.com", "/#b", {});
+    LearnLcpp("a.com", "/#b", {});
   }
   {
     LcppData data = CreateLcppData("a.com", 10);
@@ -1115,7 +1125,7 @@ TEST_F(ResourcePrefetchPredictorTest, LearnLcpp) {
     EXPECT_DOUBLE_EQ(5, SumOfElementLocatorFrequency(data));
   }
 
-  predictor_->LearnLcpp("a.com", "/#c", {});
+  LearnLcpp("a.com", "/#c", {});
   {
     LcppData data = CreateLcppData("a.com", 10);
     InitializeLcpElementLocatorBucket(data, "/#a", 2.4);
@@ -1125,7 +1135,7 @@ TEST_F(ResourcePrefetchPredictorTest, LearnLcpp) {
     EXPECT_DOUBLE_EQ(5, SumOfElementLocatorFrequency(data));
   }
 
-  predictor_->LearnLcpp("a.com", "/#d", {});
+  LearnLcpp("a.com", "/#d", {});
   {
     LcppData data = CreateLcppData("a.com", 10);
     InitializeLcpElementLocatorBucket(data, "/#a", 1.92);
@@ -1136,8 +1146,8 @@ TEST_F(ResourcePrefetchPredictorTest, LearnLcpp) {
   }
 
   for (int i = 0; i < 2; ++i) {
-    predictor_->LearnLcpp("a.com", "/#c", {});
-    predictor_->LearnLcpp("a.com", "/#d", {});
+    LearnLcpp("a.com", "/#c", {});
+    LearnLcpp("a.com", "/#d", {});
   }
   {
     LcppData data = CreateLcppData("a.com", 10);
@@ -1150,7 +1160,7 @@ TEST_F(ResourcePrefetchPredictorTest, LearnLcpp) {
 
   // Test that element locators and influencer scripts are independently learnt.
   for (int i = 0; i < 2; ++i) {
-    predictor_->LearnLcpp(
+    LearnLcpp(
         "a.com", "",
         {GURL("https://a.com/script1.js"), GURL("https://a.com/script2.js")});
   }
@@ -1170,7 +1180,7 @@ TEST_F(ResourcePrefetchPredictorTest, LearnLcpp) {
   }
 
   for (int i = 0; i < 3; ++i) {
-    predictor_->LearnLcpp(
+    LearnLcpp(
         "a.com", "",
         {GURL("https://a.com/script3.js"), GURL("https://a.com/script4.js")});
   }
@@ -1204,7 +1214,7 @@ TEST_F(ResourcePrefetchPredictorTest, WhenLcppDataIsCorrupted_ResetData) {
   }
 
   // Confirm that new learning process reset the corrupted data.
-  predictor_->LearnLcpp("a.com", "/#a", {});
+  LearnLcpp("a.com", "/#a", {});
   {
     LcppData data = CreateLcppData("a.com", 10);
     InitializeLcpElementLocatorBucket(data, "/#a", 1);
