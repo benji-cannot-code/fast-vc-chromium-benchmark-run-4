35c06622fe7ba342afd17ce004b2c92d4a67de42
Shakti Sahu <shaktisahu@chromium.org>
2023-09-25 17:50:47 +0000

    [Downloads] Added chrome flag to enable manual testing for jobs API
    Bug: 1415186
    Change-Id: I644711cf3fa33623ac1a8917e4718a508cf0a442
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4891168
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201075}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -6915,6 +6915,11 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_VALUE_TYPE(
          download::features::kUseDownloadOfflineContentProvider)},
 
+    {"downloads-migrate-to-jobs-api",
+     flag_descriptions::kDownloadsMigrateToJobsAPIName,
+     flag_descriptions::kDownloadsMigrateToJobsAPIDescription, kOsAndroid,
+     FEATURE_VALUE_TYPE(download::features::kDownloadsMigrateToJobsAPI)},
+
     {"download-range", flag_descriptions::kDownloadRangeName,
      flag_descriptions::kDownloadRangeDescription, kOsAll,
      FEATURE_VALUE_TYPE(download::features::kDownloadRange)},
