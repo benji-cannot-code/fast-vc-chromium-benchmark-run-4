2eb8f7a4cd2414515abec9d260de8ffc881af5ef
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-09-25 08:59:24 +0000

    Revisit UNO pref migration unit tests
    Test-only change.
    - Return a bool from MaybeMigratePrefsForSyncToSigninPart1 / 2 saying
    whether the migration ran. This simplifies some tests.
    - Add a dedicated test to check that the global type prefs are
    untouched by the migration. Remove that check from other tests (less
    clutter).
    - Focus the expectations on the public API GetSelectedTypesForAccount()
    rather than the raw pref values, since we don't care about the
    difference between "default" and "enabled" anyway.
    - For tests that check "type foo got disabled", rely on the type being
    on by default rather than SetBooleanUserPrefValue(..., PREF_TRUE) for
    the global prefs. Motivation: soon the migration logic won't read the
    global prefs, so such calls would need to be removed anyway.
    - Merge SyncToSignin_Part2DoesNotRunOnSignin into
    SyncToSignin_RunsOnlyOnce.
    - Split EXPECT_TRUE(selected_types.HasAll({kFoo, kBar)) into 2
    expectations, for better debuggability. Similarly for EXPECT_FALSE w/
    HasAny().
    - Split mimicked browser restarts into separate blocks, each with one
    instantiation of SyncPrefs, for clarity.
    
    Bug: 1485015
    Change-Id: I48615535d6b80389317c497a949081ffc0a90506
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4886938
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200875}

--- components/sync/service/sync_prefs.cc
+++ components/sync/service/sync_prefs.cc
@@ -657,18 +657,18 @@ void SyncPrefs::ClearPassphrasePromptMutedProductVersion() {
       prefs::internal::kSyncPassphrasePromptMutedProductVersion);
 }
 
-void SyncPrefs::MaybeMigratePrefsForSyncToSigninPart1(
+bool SyncPrefs::MaybeMigratePrefsForSyncToSigninPart1(
     SyncAccountState account_state,
     signin::GaiaIdHash gaia_id_hash) {
   if (!base::FeatureList::IsEnabled(kReplaceSyncPromosWithSignInPromos)) {
     // Ensure that the migration runs again when the feature gets enabled.
     pref_service_->ClearPref(kSyncToSigninMigrationState);
-    return;
+    return false;
   }
 
   // Don't migrate again if this profile was previously migrated.
   if (pref_service_->GetInteger(kSyncToSigninMigrationState) != kNotMigrated) {
-    return;
+    return false;
   }
 
   if (IsLocalSyncEnabled()) {
@@ -677,7 +677,7 @@ void SyncPrefs::MaybeMigratePrefsForSyncToSigninPart1(
     // done.
     pref_service_->SetInteger(kSyncToSigninMigrationState,
                               kMigratedPart2AndFullyDone);
-    return;
+    return false;
   }
 
   switch (account_state) {
@@ -688,7 +688,7 @@ void SyncPrefs::MaybeMigratePrefsForSyncToSigninPart1(
       // later sign in / turn on sync.
       pref_service_->SetInteger(kSyncToSigninMigrationState,
                                 kMigratedPart2AndFullyDone);
-      return;
+      return false;
     }
     case SyncAccountState::kSignedInNotSyncing: {
       pref_service_->SetInteger(kSyncToSigninMigrationState,
@@ -741,12 +741,12 @@ void SyncPrefs::MaybeMigratePrefsForSyncToSigninPart1(
         account_settings->Set(pref_name, enabled);
       }
 
-      return;
+      return true;
     }
   }
 }
 
-void SyncPrefs::MaybeMigratePrefsForSyncToSigninPart2(
+bool SyncPrefs::MaybeMigratePrefsForSyncToSigninPart2(
     signin::GaiaIdHash gaia_id_hash,
     bool is_using_explicit_passphrase) {
   // The migration pref shouldn't be set if the feature is disabled, but if it
@@ -754,14 +754,14 @@ void SyncPrefs::MaybeMigratePrefsForSyncToSigninPart2(
   // the migration will get triggered again once the feature gets enabled again.
   if (!base::FeatureList::IsEnabled(kReplaceSyncPromosWithSignInPromos)) {
     pref_service_->ClearPref(kSyncToSigninMigrationState);
-    return;
+    return false;
   }
 
   // Only run part 2 of the migration if part 1 has run but part 2 hasn't yet.
   // This ensures that it only runs once.
   if (pref_service_->GetInteger(kSyncToSigninMigrationState) !=
       kMigratedPart1ButNot2) {
-    return;
+    return false;
   }
   pref_service_->SetInteger(kSyncToSigninMigrationState,
                             kMigratedPart2AndFullyDone);
@@ -782,7 +782,9 @@ void SyncPrefs::MaybeMigratePrefsForSyncToSigninPart2(
     // from kAutofill.
     account_settings->Set(GetPrefNameForType(UserSelectableType::kPayments),
                           false);
+    return true;
   }
+  return false;
 }
 
 // static
