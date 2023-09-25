b0bed05416b16ae9111ab683d1c29e5082a56b25
Ian Vollick <vollick@chromium.org>
2023-09-25 13:23:01 +0000

    [ios] Use HW GL for content_browsertests and Angle+Metal
    With this change, we follow mac and don't force software GL. We also
    change the allowed GL implementations to include only kMetal.
    
    credit: thanks to vasilyt@ for walking me through the graphics init
    paths, noticing the issue with allowed GL implementations and giving
    guidance on approach after looking into Android's behavior.
    
    Bug: 1411704
    Change-Id: I59ee4a629bb83f25982a50d2f78fb3fbae76db7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4885954
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200941}

--- ui/gl/init/gl_factory_ios.mm
+++ ui/gl/init/gl_factory_ios.mm
@@ -17,7 +17,7 @@ namespace gl::init {
 
 std::vector<GLImplementationParts> GetAllowedGLImplementations() {
   std::vector<GLImplementationParts> impls;
-  impls.emplace_back(kGLImplementationEGLANGLE);
+  impls.emplace_back(gl::ANGLEImplementation::kMetal);
   return impls;
 }
 
