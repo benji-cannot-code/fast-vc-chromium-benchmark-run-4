ff343a1b80df96cedb1e06a9d824da394b38cadc
Orin Jaworski <orinj@google.com>
2023-09-25 23:43:03 +0000

    [omnibox][actions] Implement realbox actions inlining for simplified UI
    This CL plumbs feature state into the webui and uses it to change HTML
    structure and CSS styling to implement the simplified actions UI for
    the realbox.
    
    Screenshots:
    [Before CL - SnipIt](http://screen/3PXcY6gjxekWF3T)
    [Simplified Disabled - SnipIt](http://screen/44PZTUGG2ZjsQ2G)
    [Simplified Enabled - SnipIt](http://screen/9VkVsWRTw5PAX3i)
    
    Low-Coverage-Reason: EXPERIMENTAL_CODE These functions will not be
    needed once a launch decision is made.
    
    Bug: 1477861
    Change-Id: If77900af213310257bb4fd846584dffc23bf72d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4885241
    Reviewed-by: Khalid Peer <khalidpeer@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1201226}

--- chrome/browser/ui/webui/realbox/realbox_handler.cc
+++ chrome/browser/ui/webui/realbox/realbox_handler.cc
@@ -588,6 +588,10 @@ void RealboxHandler::SetupWebUIDataSource(content::WebUIDataSource* source,
   source->AddBoolean(
       "realboxLensDirectUpload",
       base::FeatureList::IsEnabled(ntp_features::kNtpLensDirectUpload));
+
+  source->AddBoolean(
+      "omniboxActionsUISimplification",
+      base::FeatureList::IsEnabled(omnibox::kOmniboxActionsUISimplification));
 }
 
 // static
