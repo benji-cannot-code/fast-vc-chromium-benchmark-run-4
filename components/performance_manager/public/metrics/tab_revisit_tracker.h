a34b6d83abc1415dab18487dbb331012f3f98318
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2023-09-25 14:20:22 +0000

    Wait for ukm source id for discarded tabs in TabRevisitTracker
    Bug: 1483776
    Change-Id: I0ea34062bf00924634f85114faf26af24f211d80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4883773
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200968}

--- components/performance_manager/public/metrics/tab_revisit_tracker.h
+++ components/performance_manager/public/metrics/tab_revisit_tracker.h
@@ -23,7 +23,8 @@ namespace performance_manager {
 class TabRevisitTracker : public GraphOwned,
                           public TabPageObserver,
                           public PageLiveStateObserverDefaultImpl,
-                          public TabConnectednessDecorator::Observer {
+                          public TabConnectednessDecorator::Observer,
+                          public PageNode::ObserverDefaultImpl {
  public:
   static constexpr char kTimeToRevisitHistogramName[] =
       "PerformanceManager.TabRevisitTracker.TimeToRevisit2";
@@ -33,8 +34,7 @@ class TabRevisitTracker : public GraphOwned,
   TabRevisitTracker();
   ~TabRevisitTracker() override;
 
- private:
-  friend class TabRevisitTrackerTest;
+  static int64_t ExponentiallyBucketedSeconds(base::TimeDelta time);
 
   enum class State {
     // The order of the leading elements must match the one in enums.xml
@@ -61,6 +61,11 @@ class TabRevisitTracker : public GraphOwned,
     absl::optional<int64_t> connectedness_to_last_switch_active_tab;
   };
 
+ private:
+  friend class TabRevisitTrackerTest;
+
+  class UkmSourceIdReadyRecorder;
+
   void RecordRevisitHistograms(const TabPageDecorator::TabHandle* tab_handle);
   void RecordCloseHistograms(const TabPageDecorator::TabHandle* tab_handle);
   void RecordStateChangeUkmAndUpdateStateBundle(
@@ -70,9 +75,6 @@ class TabRevisitTracker : public GraphOwned,
   StateBundle CreateUpdatedStateBundle(
       const TabPageDecorator::TabHandle* tab_handle,
       State new_state) const;
-
-  int64_t StateToSample(TabRevisitTracker::State state);
-  static int64_t ExponentiallyBucketedSeconds(base::TimeDelta time);
 
   // GraphOwned:
   void OnPassedToGraph(Graph* graph) override;
@@ -92,7 +94,13 @@ class TabRevisitTracker : public GraphOwned,
   void OnBeforeTabSwitch(TabPageDecorator::TabHandle* source,
                          TabPageDecorator::TabHandle* destination) override;
 
+  // PageNode::ObserverDefaultImpl:
+  void OnUkmSourceIdChanged(const PageNode* page_node) override;
+
   std::map<const TabPageDecorator::TabHandle*, StateBundle> tab_states_;
+  std::map<const TabPageDecorator::TabHandle*,
+           std::unique_ptr<UkmSourceIdReadyRecorder>>
+      pending_ukm_records_;
 };
 
 }  // namespace performance_manager
