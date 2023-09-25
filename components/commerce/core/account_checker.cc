fbeaae2d6bc69f0be83e27a24b5ce3817c0ed3f7
Marc Treib <treib@chromium.org>
2023-09-25 16:56:50 +0000

    Annotate kSync/IsSyncFeatureActive usages in commerce::AccountChecker
    ConsentLevel::kSync and similar APIs like IsSyncFeatureActive() are
    deprecated and will eventually go away. This CL adds TODOs for a few
    instances in commerce::AccountChecker.
    This is a (trivial) followup to crrev.com/c/4843290.
    
    Bug: 1462286
    Change-Id: Ifac5c73f197a35b8a66a3b0ccd9f79fac85848f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4886910
    Auto-Submit: Marc Treib <treib@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201042}

--- components/commerce/core/account_checker.cc
+++ components/commerce/core/account_checker.cc
@@ -71,6 +71,9 @@ bool AccountChecker::IsSignedIn() {
            identity_manager_->HasPrimaryAccount(signin::ConsentLevel::kSignin);
   }
   // The feature is not enabled, fallback to old behavior.
+  // TODO(crbug.com/1462978): Delete ConsentLevel::kSync usage once
+  // kReplaceSyncPromosWithSignInPromos is launched on all platforms. See
+  // ConsentLevel::kSync documentation for details.
   return identity_manager_ &&
          identity_manager_->HasPrimaryAccount(signin::ConsentLevel::kSync);
 }
@@ -83,6 +86,9 @@ bool AccountChecker::IsSyncingBookmarks() {
                                 syncer::UploadState::ACTIVE;
   }
   // The feature is not enabled, fallback to old behavior.
+  // TODO(crbug.com/1462978): Delete IsSyncFeatureActive() usage once
+  // kReplaceSyncPromosWithSignInPromos is launched on all platforms. See
+  // ConsentLevel::kSync documentation for details.
   return sync_service_ && sync_service_->IsSyncFeatureActive() &&
          syncer::GetUploadToGoogleState(sync_service_,
                                         syncer::ModelType::BOOKMARKS) !=
@@ -369,8 +375,9 @@ std::unique_ptr<EndpointFetcher> AccountChecker::CreateEndpointFetcher(
     int64_t timeout_ms,
     const std::string& post_data,
     const net::NetworkTrafficAnnotationTag& annotation_tag) {
-  // TODO(crbug.com/1463438): ConsentLevel::kSync is deprecated and should be
-  //     removed. See ConsentLevel::kSync documentation for details.
+  // TODO(crbug.com/1462978): Delete ConsentLevel::kSync usage once
+  // kReplaceSyncPromosWithSignInPromos is launched on all platforms. See
+  // ConsentLevel::kSync documentation for details.
   signin::ConsentLevel consent_level =
       base::FeatureList::IsEnabled(syncer::kReplaceSyncPromosWithSignInPromos)
           ? signin::ConsentLevel::kSignin
