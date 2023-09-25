6556afb47d4c390f6d07c223923f61f724810339
Nan Lin <linnan@chromium.org>
2023-09-25 16:58:06 +0000

    [3PCD] Create eligiblity service with the profile
    Change-Id: I334b1b7c9935f4ab16a51304ae54a1d93a25136a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4880165
    Reviewed-by: Anton Maliev <amaliev@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201045}

--- chrome/browser/privacy_sandbox/privacy_sandbox_service_factory.cc
+++ chrome/browser/privacy_sandbox/privacy_sandbox_service_factory.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_service.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_settings_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/tpcd/experiment/eligibility_service_factory.h"
 #include "components/content_settings/core/browser/cookie_settings.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/keyed_service/core/keyed_service.h"
@@ -77,6 +78,11 @@ PrivacySandboxServiceFactory::PrivacySandboxServiceFactory()
 #endif
   DependsOn(
       first_party_sets::FirstPartySetsPolicyServiceFactory::GetInstance());
+
+  // The Eligibility service should be created before the Privacy Sandbox
+  // service is created to determine the cookie deprecation experiment
+  // eligibility.
+  DependsOn(tpcd::experiment::EligibilityServiceFactory::GetInstance());
 }
 
 std::unique_ptr<KeyedService>
--- chrome/browser/tpcd/experiment/eligibility_service_factory.h
+++ chrome/browser/tpcd/experiment/eligibility_service_factory.h
@@ -29,6 +29,8 @@ class EligibilityServiceFactory : public ProfileKeyedServiceFactory {
   EligibilityServiceFactory();
 
   // BrowserContextKeyedServiceFactory:
+  bool ServiceIsCreatedWithBrowserContext() const override;
+
   std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
