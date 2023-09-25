f2d6e4faad98d429312f8a0e3f073618ccd59e03
Andrew Grieve <agrieve@chromium.org>
2023-09-25 19:44:15 +0000

    Add a no-op version of androidx.window
    Bug: b/301417565
    Change-Id: I072bb980c472b2ff8ab3fe967dee85aca473259a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4885112
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201125}

---
+++ third_party/androidx/local_modifications/window/java/androidx/window/layout/DisplayFeature.java
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+package androidx.window.layout;
+
+import android.graphics.Rect;
+
+/**
+ * Placeholder so that things that use it compile.
+ */
+public interface DisplayFeature {
+    Rect getBounds();
+}
