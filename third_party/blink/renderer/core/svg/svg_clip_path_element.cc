8f824569f1ba0750bf806c2225bcf35868cc7b7a
Fredrik Söderquist <fs@opera.com>
2023-09-25 19:28:04 +0000

    Change SVGClipPathElement to inherit from SVGElement
    This indirectly also end up removing a bunch of methods that are on
    SVGGraphicsElement (such as getCTM, getBBox et.c.) It also removes the
    SVGTests implementation.
    SVGClipPathElement also has to grow it's own |transform_| and associated
    handling.
    
    I2S thread https://groups.google.com/a/chromium.org/g/blink-dev/c/Hq3r7dB6yoc/m/H4eQ7TyeAgAJ
    
    Fixed: 851080
    Change-Id: I395f00186cfaeabc2e006ae3d9671bc9ad8469af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4789072
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201117}

--- third_party/blink/renderer/core/svg/svg_clip_path_element.cc
+++ third_party/blink/renderer/core/svg/svg_clip_path_element.cc
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 SVGClipPathElement::SVGClipPathElement(Document& document)
-    : SVGGraphicsElement(svg_names::kClipPathTag, document),
+    : SVGTransformableElement(svg_names::kClipPathTag, document),
       clip_path_units_(MakeGarbageCollected<
                        SVGAnimatedEnumeration<SVGUnitTypes::SVGUnitType>>(
           this,
@@ -37,26 +37,24 @@ SVGClipPathElement::SVGClipPathElement(Document& document)
 
 void SVGClipPathElement::Trace(Visitor* visitor) const {
   visitor->Trace(clip_path_units_);
-  SVGGraphicsElement::Trace(visitor);
+  SVGTransformableElement::Trace(visitor);
 }
 
 void SVGClipPathElement::SvgAttributeChanged(
     const SvgAttributeChangedParams& params) {
   if (params.name == svg_names::kClipPathUnitsAttr) {
     SVGElement::InvalidationGuard invalidation_guard(this);
-
     auto* layout_object = To<LayoutSVGResourceContainer>(GetLayoutObject());
     if (layout_object) {
       layout_object->InvalidateCache();
     }
     return;
   }
-
-  SVGGraphicsElement::SvgAttributeChanged(params);
+  SVGTransformableElement::SvgAttributeChanged(params);
 }
 
 void SVGClipPathElement::ChildrenChanged(const ChildrenChange& change) {
-  SVGGraphicsElement::ChildrenChanged(change);
+  SVGTransformableElement::ChildrenChanged(change);
 
   if (change.ByParser())
     return;
@@ -75,15 +73,14 @@ SVGAnimatedPropertyBase* SVGClipPathElement::PropertyFromAttribute(
     const QualifiedName& attribute_name) const {
   if (attribute_name == svg_names::kClipPathUnitsAttr) {
     return clip_path_units_.Get();
-  } else {
-    return SVGGraphicsElement::PropertyFromAttribute(attribute_name);
   }
+  return SVGTransformableElement::PropertyFromAttribute(attribute_name);
 }
 
 void SVGClipPathElement::SynchronizeAllSVGAttributes() const {
   SVGAnimatedPropertyBase* attrs[]{clip_path_units_.Get()};
   SynchronizeListOfSVGAttributes(attrs);
-  SVGGraphicsElement::SynchronizeAllSVGAttributes();
+  SVGTransformableElement::SynchronizeAllSVGAttributes();
 }
 
 }  // namespace blink
--- third_party/blink/renderer/core/svg/svg_clip_path_element.h
+++ third_party/blink/renderer/core/svg/svg_clip_path_element.h
@@ -23,15 +23,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_SVG_SVG_CLIP_PATH_ELEMENT_H_
 
 #include "third_party/blink/renderer/core/svg/svg_animated_enumeration.h"
-#include "third_party/blink/renderer/core/svg/svg_graphics_element.h"
+#include "third_party/blink/renderer/core/svg/svg_transformable_element.h"
 #include "third_party/blink/renderer/core/svg/svg_unit_types.h"
-#include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 
 namespace blink {
 
-class LayoutObject;
-
-class SVGClipPathElement final : public SVGGraphicsElement {
+class SVGClipPathElement final : public SVGTransformableElement {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
