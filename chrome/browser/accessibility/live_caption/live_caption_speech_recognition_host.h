0650dc2a2b5cece28bec2f7eae0871de86c38619
Evan Liu <evliu@google.com>
2023-09-25 23:37:04 +0000

    Implement strategies for reducing caption flickering
    This CL implements a Wait-K approach[1] and a Greedy Text Stabilization algorithm[2] to reduce the amount of flickering when translating partial speech recognition results. These approaches are placed behind two separate feature flags and only apply to translations. The following table illustrates the flicker rate for a 30 second audio clip translating English to French:
            Trial 1.   Trial 2.  Trial 3.
    None    24         25        26
    Wait-K  16         20        20
    GTS     4          4         4
    
    [1] arxiv.org/abs/2005.08595
    [2] go/w2t
    
    Bug: 1471300,1369737
    Change-Id: I288a43e411ef3bebf09aaa05346c778c7621d321
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4852478
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1201219}

--- chrome/browser/accessibility/live_caption/live_caption_speech_recognition_host.h
+++ chrome/browser/accessibility/live_caption/live_caption_speech_recognition_host.h
@@ -27,6 +27,7 @@ class RenderFrameHost;
 namespace captions {
 
 class CaptionBubbleContextBrowser;
+class GreedyTextStabilizer;
 class LiveCaptionController;
 class LiveTranslateController;
 
@@ -95,6 +96,9 @@ class LiveCaptionSpeechRecognitionHost
   // does not exist. Lifetime is tied to the BrowserContext.
   LiveTranslateController* GetLiveTranslateController();
 
+  // Processes and returns the text to be dispatched.
+  std::string GetTextForDispatch(const std::string& text, bool is_final);
+
   std::unique_ptr<CaptionBubbleContextBrowser> context_;
 
   // A flag used by the Live Translate feature indicating whether transcriptions
@@ -117,6 +121,8 @@ class LiveCaptionSpeechRecognitionHost
 
   // The number of characters sent to the translation service.
   int characters_translated_ = 0;
+
+  std::unique_ptr<captions::GreedyTextStabilizer> greedy_text_stabilizer_;
 
   base::WeakPtrFactory<LiveCaptionSpeechRecognitionHost> weak_factory_{this};
 };
