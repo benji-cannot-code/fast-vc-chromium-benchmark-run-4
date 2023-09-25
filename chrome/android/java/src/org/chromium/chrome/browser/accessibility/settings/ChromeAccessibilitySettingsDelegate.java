bc88e8415d0cdcb14bef63b980e43ad817d5293d
Mark Schillaci <mschillaci@google.com>
2023-09-25 23:20:33 +0000

    [Page Zoom] Add delegate to remove dependency on embedder
    This CL adds a delegate for controlling an integer user preference so
    that embedders can provide a concrete instance to the accessibility
    settings page, and the dependency on //chrome can be removed from
    the //components/browser_ui/accessibility package.
    
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL This change is part of a chain of refactoring that is needed before tests can be added.
    AX-Relnotes: N/A
    Bug: 1459631, b/265491863
    Change-Id: I2c82a00b33bad9bbadfa7da94cb3ab367f6093e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4885340
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1201206}

--- chrome/android/java/src/org/chromium/chrome/browser/accessibility/settings/ChromeAccessibilitySettingsDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/accessibility/settings/ChromeAccessibilitySettingsDelegate.java
@@ -44,6 +44,27 @@ public class ChromeAccessibilitySettingsDelegate implements AccessibilitySetting
         }
     }
 
+    private static class TextSizeContrastAccessibilityDelegate
+            implements IntegerPreferenceDelegate {
+        private final BrowserContextHandle mBrowserContextHandle;
+
+        public TextSizeContrastAccessibilityDelegate(BrowserContextHandle mBrowserContextHandle) {
+            this.mBrowserContextHandle = mBrowserContextHandle;
+        }
+
+        @Override
+        public int getValue() {
+            return UserPrefs.get(mBrowserContextHandle)
+                    .getInteger(Pref.ACCESSIBILITY_TEXT_SIZE_CONTRAST_FACTOR);
+        }
+
+        @Override
+        public void setValue(int value) {
+            UserPrefs.get(mBrowserContextHandle)
+                    .setInteger(Pref.ACCESSIBILITY_TEXT_SIZE_CONTRAST_FACTOR, value);
+        }
+    }
+
     private final Profile mProfile;
 
     /**
@@ -62,6 +83,11 @@ public class ChromeAccessibilitySettingsDelegate implements AccessibilitySetting
     @Override
     public BooleanPreferenceDelegate getReaderForAccessibilityDelegate() {
         return new ReaderForAccessibilityDelegate(mProfile);
+    }
+
+    @Override
+    public IntegerPreferenceDelegate getTextSizeContrastAccessibilityDelegate() {
+        return new TextSizeContrastAccessibilityDelegate(getBrowserContextHandle());
     }
 
     @Override
