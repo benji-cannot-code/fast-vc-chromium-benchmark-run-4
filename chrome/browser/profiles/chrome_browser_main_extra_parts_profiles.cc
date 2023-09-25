53b2e29604616e2c73f596399858b4283ab59a46
Abe Boujane <boujane@google.com>
2023-09-25 18:52:32 +0000

    Added TrackingProtectionNoticeFactory
    Bug: b:295926275
    Change-Id: I04885e2fedd7942362a70d629c7a65396b478ce1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4889555
    Commit-Queue: Abe Boujane <boujane@google.com>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201102}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -443,6 +443,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/new_tab_page/one_google_bar/one_google_bar_service_factory.h"
 #include "chrome/browser/new_tab_page/promos/promo_service_factory.h"
 #include "chrome/browser/payments/payment_request_display_manager_factory.h"
+#include "chrome/browser/privacy_sandbox/tracking_protection_notice_factory.h"
 #include "chrome/browser/search/background/ntp_background_service_factory.h"
 #include "chrome/browser/search/background/ntp_custom_background_service_factory.h"
 #include "chrome/browser/serial/serial_chooser_context_factory.h"
@@ -1119,6 +1120,9 @@ void ChromeBrowserMainExtraPartsProfiles::
   TopSitesFactory::GetInstance();
   tpcd::experiment::EligibilityServiceFactory::GetInstance();
   tpcd::metadata::UpdaterServiceFactory::GetInstance();
+#if !BUILDFLAG(IS_ANDROID)
+  TrackingProtectionNoticeFactory::GetInstance();
+#endif
   TrackingProtectionOnboardingFactory::GetInstance();
   TrackingProtectionSettingsFactory::GetInstance();
   translate::TranslateRankerFactory::GetInstance();
