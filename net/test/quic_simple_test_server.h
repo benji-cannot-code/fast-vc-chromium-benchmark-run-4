5161c8d50c8466936ce129d3f6a35f1808e1a9d9
Kenichi Ishibashi <bashi@chromium.org>
2023-09-25 08:24:15 +0000

    Add QUIC connection migration browser tests
    This CL adds two tests:
    * A connection is successfully migrated
    * A connection is closed during a connection migration
    
    These tests are skipped when the device is offline or the device
    disabled networks. Since trybots disable networks to reduce test flakiness, trybots skip these tests. https://crbug.com/1486376 is the
    tracking bug to run these tests on trybots.
    
    Bug: 1465889
    Change-Id: I5fd748b5cde87332248301fced80ce5d842887d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4829343
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200868}

--- net/test/quic_simple_test_server.h
+++ net/test/quic_simple_test_server.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/time/time.h"
 #include "net/base/host_port_pair.h"
 #include "net/third_party/quiche/src/quiche/spdy/core/http2_header_block.h"
 #include "url/gurl.h"
@@ -24,12 +25,20 @@ class QuicSimpleTestServer {
   // frames to all connected clients.
   static void ShutdownDispatcherForTesting();
 
+  // Add a response to `path`.
+  static void AddResponse(const std::string& path,
+                          spdy::Http2HeaderBlock response_headers,
+                          const std::string& response_body);
+
   // Add a response to `path` with Early Hints.
   static void AddResponseWithEarlyHints(
       const std::string& path,
       const spdy::Http2HeaderBlock& response_headers,
       const std::string& response_body,
       const std::vector<spdy::Http2HeaderBlock>& early_hints);
+
+  // Set a delay to `path`.
+  static void SetResponseDelay(const std::string& path, base::TimeDelta delay);
 
   // Returns example.com
   static std::string const GetDomain();
