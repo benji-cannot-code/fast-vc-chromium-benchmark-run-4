f14a09b5d41a35c2d143472ff2f57ab5b9f8d35d
Kenichi Ishibashi <bashi@chromium.org>
2023-09-25 07:54:25 +0000

    Remove unused includes from HttpStreamFactory::{Job,JobController}
    These includes seem to be leftovers from past changes.
    
    Bug: N/A
    Change-Id: I7621923cc550c7ff02b677f4900adf5ec7676342
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4891409
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200860}

--- net/http/http_stream_factory_job.h
+++ net/http/http_stream_factory_job.h
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_request_info.h"
 #include "net/http/http_stream_factory.h"
 #include "net/http/http_stream_request.h"
-#include "net/proxy_resolution/proxy_resolution_service.h"
 #include "net/quic/quic_stream_factory.h"
 #include "net/socket/client_socket_handle.h"
 #include "net/socket/client_socket_pool.h"
@@ -33,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/socket/ssl_client_socket.h"
 #include "net/spdy/spdy_session_key.h"
 #include "net/spdy/spdy_session_pool.h"
-#include "net/ssl/ssl_config_service.h"
 #include "url/scheme_host_port.h"
 
 namespace net {
--- net/http/http_stream_factory_job_controller.h
+++ net/http/http_stream_factory_job_controller.h
@@ -14,10 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/net_errors.h"
-#include "net/base/privacy_mode.h"
 #include "net/http/http_stream_factory_job.h"
 #include "net/http/http_stream_request.h"
-#include "net/socket/next_proto.h"
 #include "net/spdy/spdy_session_pool.h"
 
 namespace net {
