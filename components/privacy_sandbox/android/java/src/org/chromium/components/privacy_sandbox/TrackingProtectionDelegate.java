2b8e36658997fe02f683a9b905a53624d30e6464
Andrey Zaytsev <andzaytsev@google.com>
2023-09-25 20:54:26 +0000

    Tracking Protection: Connect prefs to switches in UI
    Tested manually - render tests will be added once the UI is finished.
    Low-Coverage-Reason: render tests not implemented yet.
    
    Bug: b:295926938
    Change-Id: Ie8a722803a51170632d58df2ecbdfffe0a32200d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4889645
    Commit-Queue: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201138}

---
+++ components/privacy_sandbox/android/java/src/org/chromium/components/privacy_sandbox/TrackingProtectionDelegate.java
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.privacy_sandbox;
+
+/** Interface implemented by the embedder to access embedder-specific logic. */
+public interface TrackingProtectionDelegate {
+    /** @return whether block all 3PCD pref is enabled. */
+    boolean isBlockAll3PCDEnabled();
+
+    /** Set the value of the block all 3PCD pref. */
+    void setBlockAll3PCD(boolean enabled);
+
+    /** @return whether the Do Not Track pref is enabled. */
+    boolean isDoNotTrackEnabled();
+
+    /** Set the value of the Do Not Track Pref. */
+    void setDoNotTrack(boolean enabled);
+}
