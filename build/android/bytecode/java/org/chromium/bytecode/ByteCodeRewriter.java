chromium/chromium

commit b16c8c4f521345399b30c6b7911ad16b462e4197

Removing unnecessary classpath arg from trace event bytecode rewriter
With some possible changes coming in doubledown related to
crbug.com/1484366, we are creating many more jars than previously, and
thus our classpath is getting huge. We are running over the max
command line size, even with the file @filearg, since we were passing
--classpath separately.

However, it appears that the classpath doesn't do anything, so we can
just delete it.

Bug: 1484366
Change-Id: I01c038b3e7bd57252078e626ad6dbff82bdba0b7
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4877322
Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
Commit-Queue: Sam Maier <smaier@chromium.org>
Auto-Submit: Sam Maier <smaier@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1200193}

build/android/gyp/trace_event_bytecode_rewriter.py
@@ -40,18 +40,16 @@ def main(argv):
     if not os.path.exists(jar_dir):
       os.makedirs(jar_dir)
 
-  all_input_jars = set(args.classpath + args.input_jars)
-  cmd = [
-      args.script, '--classpath', ':'.join(sorted(all_input_jars)),
-      ':'.join(args.input_jars), ':'.join(args.output_jars)
-  ]
+  extra_inputs = set(args.classpath) - set(args.input_jars)
+  all_input_jars = args.input_jars + list(extra_inputs)
+  cmd = [args.script, ':'.join(all_input_jars), ':'.join(args.output_jars)]
   if sum(len(x) for x in cmd) > _MAX_CMDLINE:
     # Cannot put --classpath in the args file because that is consumed by the
     # wrapper script.
     args_file = tempfile.NamedTemporaryFile(mode='w')
-    args_file.write('\n'.join(cmd[3:]))
+    args_file.write('\n'.join(cmd[1:]))
     args_file.flush()
-    cmd[3:] = ['@' + args_file.name]
+    cmd[1:] = ['@' + args_file.name]
 
   build_utils.CheckOutput(cmd, print_stdout=True)
 

build/android/bytecode/java/org/chromium/bytecode/TraceEventAdder.java
@@ -47,20 +47,21 @@ public class TraceEventAdder extends ByteCodeRewriter {
         args = ByteCodeRewriter.expandArgs(args);
 
         if (args.length < 2) {
-            System.err.println("Expected arguments: <':' separated list with N input jar paths> "
-                    + "<':' separated list with N output jar paths>");
+            System.err.println("Expected arguments: <':' separated list of input jar paths> "
+                    + "<':' separated list of output jar paths>");
             System.exit(1);
         }
 
         String[] inputJars = args[0].split(":");
         String[] outputJars = args[1].split(":");
 
-        assert inputJars.length
-                == outputJars.length : "Input and output lists are not the same length. Inputs: "
+        assert inputJars.length >= outputJars.length
+            : "Input list must be a superset of the output list, where the "
+                        + "first N entries match, and N is the length of the output list."
                         + inputJars.length + " Outputs: " + outputJars.length;
 
         // outputJars[n] must be the same as inputJars[n] but with a suffix, validate this.
-        for (int i = 0; i < inputJars.length; i++) {
+        for (int i = 0; i < outputJars.length; i++) {
             File inputJarPath = new File(inputJars[i]);
             String inputJarFilename = inputJarPath.getName();
             File outputJarPath = new File(outputJars[i]);
@@ -68,6 +69,7 @@ public class TraceEventAdder extends ByteCodeRewriter {
             String inputFilenameNoExtension =
                     inputJarFilename.substring(0, inputJarFilename.lastIndexOf(".jar"));
 
+            // Ensuring that for every output, we have the matching input.
             assert outputJarPath.getName().startsWith(inputFilenameNoExtension);
         }
 
@@ -76,7 +78,7 @@ public class TraceEventAdder extends ByteCodeRewriter {
         ClassLoader classPathJarsClassLoader = loadJars(classPathJarsPaths);
 
         TraceEventAdder adder = new TraceEventAdder(classPathJarsClassLoader);
-        for (int i = 0; i < inputJars.length; i++) {
+        for (int i = 0; i < outputJars.length; i++) {
             adder.rewrite(new File(inputJars[i]), new File(outputJars[i]));
         }
     }

benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 301c9c7b919875f1ab317e0d0b1e96ea4f01a0d3

Fix androidx roll breaking on trace_event_bytecode_rewriter.py
I think this was broken by recent commit:
b16c8c4f521345399b30c6b7911ad16b462e4197

which removed --classpath from the wrapper script. This re-adds the
flag, but only for sdk jar.

Bug: 1484366, 1485990
Change-Id: I152e67b48bcfe5c919f580c9650f38ad9d0f09a4
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4884859
Reviewed-by: Sam Maier <smaier@chromium.org>
Commit-Queue: Andrew Grieve <agrieve@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1200962}

build/android/bytecode/java/org/chromium/bytecode/ByteCodeRewriter.java
@@ -121,8 +121,8 @@ public abstract class ByteCodeRewriter {
             byte[] classData = writer.toByteArray();
             outputStream.write(classData, 0, classData.length);
             return true;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } catch (Throwable e) {
+            throw new RuntimeException("Failed when processing " + entry.getName(), e);
         }
     }
 }

build/android/gyp/trace_event_bytecode_rewriter.py
@@ -41,21 +41,23 @@ def main(argv):
     if not os.path.exists(jar_dir):
       os.makedirs(jar_dir)
 
-  extra_inputs = set(args.classpath) - set(args.input_jars)
-  all_input_jars = args.input_jars + list(extra_inputs)
-  cmd = [args.script, ':'.join(all_input_jars), ':'.join(args.output_jars)]
+  cmd = [
+      args.script, '--classpath', ':'.join(args.classpath),
+      ':'.join(args.input_jars), ':'.join(args.output_jars)
+  ]
   if sum(len(x) for x in cmd) > _MAX_CMDLINE:
     # Cannot put --classpath in the args file because that is consumed by the
     # wrapper script.
     args_file = tempfile.NamedTemporaryFile(mode='w')
-    args_file.write('\n'.join(cmd[1:]))
+    args_file.write('\n'.join(cmd[3:]))
     args_file.flush()
-    cmd[1:] = ['@' + args_file.name]
+    cmd[3:] = ['@' + args_file.name]
 
   build_utils.CheckOutput(cmd, print_stdout=True)
 
   build_utils.Touch(args.stamp)
 
+  all_input_jars = args.input_jars + args.classpath
   action_helpers.write_depfile(args.depfile, args.stamp, inputs=all_input_jars)
 
 

build/config/android/rules.gni
@@ -4712,8 +4712,6 @@ if (enable_java_templates && is_android) {
             "--script",
             rebase_path(_rewriter_path, root_build_dir),
             "--classpath",
-            "@FileArg($_rebased_build_config:deps_info:javac_full_classpath)",
-            "--classpath",
             "@FileArg($_rebased_build_config:android:sdk_jars)",
             "--input-jars",
             "@FileArg($_rebased_build_config:deps_info:device_classpath)",
301c9c7b919875f1ab317e0d0b1e96ea4f01a0d3
Andrew Grieve <agrieve@chromium.org>
2023-09-25 14:12:26 +0000

    Fix androidx roll breaking on trace_event_bytecode_rewriter.py
    I think this was broken by recent commit:
    b16c8c4f521345399b30c6b7911ad16b462e4197
    
    which removed --classpath from the wrapper script. This re-adds the
    flag, but only for sdk jar.
    
    Bug: 1484366, 1485990
    Change-Id: I152e67b48bcfe5c919f580c9650f38ad9d0f09a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4884859
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200962}

--- build/android/bytecode/java/org/chromium/bytecode/ByteCodeRewriter.java
+++ build/android/bytecode/java/org/chromium/bytecode/ByteCodeRewriter.java
@@ -121,8 +121,8 @@ public abstract class ByteCodeRewriter {
             byte[] classData = writer.toByteArray();
             outputStream.write(classData, 0, classData.length);
             return true;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } catch (Throwable e) {
+            throw new RuntimeException("Failed when processing " + entry.getName(), e);
         }
     }
 }
