f64e96efa0a5602f4585702bdcbc39bf9ea451c9
Michal Mazur <michamazur@google.com>
2023-09-26 12:05:22 +0000

    carrier_lock: Implement class to check PSM claim
    This class will handle communication with Private Set Membership
    server in order to verify if device needs Carrier Lock configuration.
    
    Bug: b:262288980
    Test: CarrierLockCheckPsmClaim
    
    Change-Id: Id8a2649ca4681d4ac34ef3e90d24fa2823839763
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4459283
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Sergiy Belozorov <sergiyb@chromium.org>
    Commit-Queue: Michał Mazur <michamazur@google.com>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/main@{#1201482}

---
+++ chromeos/ash/components/carrier_lock/fake_psm_claim_verifier.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/ash/components/carrier_lock/fake_psm_claim_verifier.h"
+
+namespace ash::carrier_lock {
+
+void FakePsmClaimVerifier::CheckPsmClaim(std::string serial,
+                                         std::string manufacturer,
+                                         std::string model,
+                                         Callback callback) {
+  std::move(callback).Run(result_);
+}
+
+bool FakePsmClaimVerifier::GetMembership() {
+  return member_;
+}
+
+void FakePsmClaimVerifier::SetMemberAndResult(bool member, Result result) {
+  member_ = member;
+  result_ = result;
+}
+
+}  // namespace ash::carrier_lock
