df420eb2a1633079107621bc15e9443645d729a0
Jayson Adams <shrike@chromium.org>
2023-09-26 15:44:30 +0000

    [Mac Sonoma Tests] Fix ClipboardMacTest to generate valid image on macOS 14
    It appears that macOS 14 has stricter requirements when creating a
    CGImage. This cl changes the bitmapInfo parameter of CGImageCreate to
    avoid returning nil.
    
    Test: out/Debug/ui_base_unittests --gtest_filter="All/ClipboardMacTest*"
    
    Bug: 1486154
    Change-Id: I90a586e6976e49f982453d43be89a39a98dac4b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4889025
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Jayson Adams <shrike@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201610}

--- ui/base/clipboard/clipboard_mac_unittest.mm
+++ ui/base/clipboard/clipboard_mac_unittest.mm
@@ -69,8 +69,8 @@ class ClipboardMacTest : public PlatformTest,
         CGColorSpaceCreateWithName(kCGColorSpaceSRGB));
     base::apple::ScopedCFTypeRef<CGImageRef> image_ref(
         CGImageCreate(pixel_width, pixel_height, 8, 32, 4 * pixel_width,
-                      color_space.get(), kCGBitmapByteOrderDefault,
-                      provider.get(), nullptr, NO, kCGRenderingIntentDefault));
+                      color_space.get(), kCGImageAlphaLast, provider.get(),
+                      nullptr, NO, kCGRenderingIntentDefault));
     return [[NSImage alloc] initWithCGImage:image_ref.get()
                                        size:NSMakeSize(width, height)];
   }
