3ebf76f2aee5f14bb91fbb7ae718d173dd583825
Christopher Cameron <ccameron@chromium.org>
2023-09-26 15:44:42 +0000

    Android/HDR: Update dynamic-range media query
    Set the gfx::DisplayColorSpaces HDR-to-SDR ratio to at least
    display::kMinHDRCapableMaxLuminanceRelative if the display is HDR
    capable. This is the mechanism that is used on macOS to indicate
    HDR capable displays. It may be that a separate boolean may be
    preferable.
    
    Update gfx::DisplayColorSpaces::SupportsHDR to return true if
    the HDR-to-SDR ratio is greater than 1.
    
    Low-Coverage-Reason: HARD_TO_TEST
    Bug: 1486633
    Change-Id: Ie2eef52b4147c44992d1ecf5664c80067c609c63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4892786
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201611}

--- ui/android/java/src/org/chromium/ui/display/PhysicalDisplayAndroid.java
+++ ui/android/java/src/org/chromium/ui/display/PhysicalDisplayAndroid.java
@@ -49,6 +49,11 @@ import java.util.function.Consumer;
         return display.getHdrSdrRatio();
     }
 
+    private static boolean isHdr(Display display) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.UPSIDE_DOWN_CAKE) return false;
+        return display.isHdr() && display.isHdrSdrRatioAvailable();
+    }
+
     private static boolean hasForcedDIPScale() {
         if (sForcedDIPScale == null) {
             String forcedScaleAsString = CommandLine.getInstance().getSwitchValue(
@@ -249,7 +254,7 @@ import java.util.function.Consumer;
     private void hdrSdrRatioChanged(Display display) {
         assert display.getDisplayId() == mDisplay.getDisplayId();
         super.update(null, null, null, null, null, null, null, null, null, null, null, null,
-                getHdrSdrRatio(mDisplay));
+                isHdr(mDisplay), getHdrSdrRatio(mDisplay));
     }
 
     private void updateCommon(Point size, float density, float xdpi, float ydpi, Display display) {
@@ -274,6 +279,7 @@ import java.util.function.Consumer;
 
         super.update(size, density, xdpi, ydpi, bitsPerPixel(pixelFormatId),
                 bitsPerComponent(pixelFormatId), display.getRotation(), isWideColorGamut, null,
-                display.getRefreshRate(), currentMode, supportedModes, getHdrSdrRatio(display));
+                display.getRefreshRate(), currentMode, supportedModes, isHdr(display),
+                getHdrSdrRatio(display));
     }
 }
