2591ceb3d07c671fcd948413f156c24144684e96
Christian Xu <christianxu@google.com>
2023-09-26 14:09:11 +0000

    [iOS][Toolbar] Add SecondaryToolbarMediator
    Add SecondaryToolbarMediator and move
    SecondaryToolbarKeyboardStateProvider to it.
    
    Bug: 1462578
    Change-Id: I7c8b98cdde5cf595efcd9e46f52812b88db1dba4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4889653
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201538}

---
+++ ios/chrome/browser/ui/toolbar/secondary_toolbar_mediator.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_TOOLBAR_SECONDARY_TOOLBAR_MEDIATOR_H_
+#define IOS_CHROME_BROWSER_UI_TOOLBAR_SECONDARY_TOOLBAR_MEDIATOR_H_
+
+#import <Foundation/Foundation.h>
+
+#import "ios/chrome/browser/ui/toolbar/secondary_toolbar_keyboard_state_provider.h"
+
+class WebStateList;
+
+/// Mediator providing web state information to SecondaryToolbarViewController.
+@interface SecondaryToolbarMediator
+    : NSObject <SecondaryToolbarKeyboardStateProvider>
+
+/// Creates an instance of the mediator.
+- (instancetype)initWithWebStateList:(WebStateList*)webStateList;
+
+/// Disconnects web state list reference.
+- (void)disconnect;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_TOOLBAR_SECONDARY_TOOLBAR_MEDIATOR_H_
---
+++ ios/chrome/browser/ui/toolbar/secondary_toolbar_mediator.mm
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/toolbar/secondary_toolbar_mediator.h"
+
+#import "base/memory/weak_ptr.h"
+#import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
+#import "ios/web/public/ui/crw_web_view_proxy.h"
+#import "ios/web/public/web_state.h"
+
+@implementation SecondaryToolbarMediator {
+  // The Browser's WebStateList.
+  base::WeakPtr<WebStateList> _webStateList;
+}
+
+- (instancetype)initWithWebStateList:(WebStateList*)webStateList {
+  if (self = [super init]) {
+    DCHECK(webStateList);
+    _webStateList = webStateList->AsWeakPtr();
+  }
+  return self;
+}
+
+- (void)disconnect {
+  if (_webStateList) {
+    _webStateList.reset();
+  }
+}
+
+#pragma mark - SecondaryToolbarKeyboardStateProvider
+
+- (BOOL)keyboardIsActiveForWebContent {
+  if (_webStateList && _webStateList->GetActiveWebState() &&
+      _webStateList->GetActiveWebState()->GetWebViewProxy()) {
+    return _webStateList->GetActiveWebState()
+        ->GetWebViewProxy()
+        .keyboardVisible;
+  }
+  return NO;
+}
+
+@end
