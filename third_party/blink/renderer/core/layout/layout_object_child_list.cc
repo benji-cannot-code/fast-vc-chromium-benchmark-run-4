878cf909376a1ed07d8b908179443b5044a1d1b7
Daniil Sakhapov <sakhapov@chromium.org>
2023-09-26 21:29:56 +0000

    Connect css counters to style containment scope tree and remove old code
    This is stage 2 of CSS Counters projects. The idea is to remove old
    style containment code from LayoutCounter and CounterNode and instead
    connect them to StyleContainmentScopeTree.
    
    Design doc:
    https://docs.google.com/document/d/1lc7OouWnelduHuRBxsQ9rmSS85q7E-h42NrOpKVkkoU
    
    Bug: 990657
    Change-Id: I5d281170937b89e5999f1104c151750e4dff7253
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4711729
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201814}

--- third_party/blink/renderer/core/layout/layout_object_child_list.cc
+++ third_party/blink/renderer/core/layout/layout_object_child_list.cc
@@ -111,7 +111,6 @@ LayoutObject* LayoutObjectChildList::RemoveChildNode(
     InvalidatePaintOnRemoval(*old_child);
 
     if (notify_layout_object) {
-      LayoutCounter::LayoutObjectSubtreeWillBeDetached(old_child);
       old_child->WillBeRemovedFromTree();
     }
 
@@ -207,7 +206,6 @@ void LayoutObjectChildList::InsertChildNode(LayoutObject* owner,
 
     if (notify_layout_object) {
       new_child->InsertedIntoTree();
-      LayoutCounter::LayoutObjectSubtreeAttached(new_child);
     }
 
     if (owner->IsInLayoutNGInlineFormattingContext() ||
--- third_party/blink/renderer/core/layout/layout_view.cc
+++ third_party/blink/renderer/core/layout/layout_view.cc
@@ -104,7 +104,6 @@ class HitTestLatencyRecorder {
 LayoutView::LayoutView(ContainerNode* document)
     : LayoutNGBlockFlow(document),
       frame_view_(To<Document>(document)->View()),
-      layout_counter_count_(0),
       hit_test_count_(0),
       hit_test_cache_hits_(0),
       hit_test_cache_(MakeGarbageCollected<HitTestCache>()),
@@ -905,8 +904,9 @@ void LayoutView::UpdateMarkersAndCountersAfterStyleChange(
          "container query style recalcs";
 
   needs_marker_counter_update_ = false;
-  if (!HasLayoutCounters() && !HasLayoutListItems())
+  if (!HasLayoutListItems()) {
     return;
+  }
 
   // For container queries style recalc, we know the counter styles didn't
   // change outside the container. Hence, we can start the update traversal from
@@ -926,8 +926,6 @@ void LayoutView::UpdateMarkersAndCountersAfterStyleChange(
     } else if (auto* inline_list_item =
                    DynamicTo<LayoutNGInlineListItem>(layout_object)) {
       inline_list_item->UpdateCounterStyle();
-    } else if (auto* counter = DynamicTo<LayoutCounter>(layout_object)) {
-      counter->UpdateCounter();
     }
   }
 }
