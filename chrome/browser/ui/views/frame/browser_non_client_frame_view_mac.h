b3017151d30c68dfdf0ecf04775339c95692128e
Glenn Hartmann <hartmanng@chromium.org>
2023-09-26 00:22:05 +0000

    Move web_app::AppId and web_app::ManifestId to a more public place
    So that Android and Desktop implementations can share the types.
    
    Note that this is part of a multi-CL change. This CL only touches
    references in /chrome/browser/ui/views/frame.
    
    BUG=1486624
    
    This CL was uploaded by git cl split.
    
    R=pkasting@chromium.org
    
    Change-Id: I175ce495e30f0901cfa6802626955cb48dcf2777
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4884745
    Auto-Submit: Glenn Hartmann <hartmanng@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201250}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_mac.h
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_mac.h
@@ -70,7 +70,7 @@ class BrowserNonClientFrameViewMac : public BrowserNonClientFrameView,
   void PaintChildren(const views::PaintInfo& info) override;
 
   // web_app::WebAppRegistrarObserver
-  void OnAlwaysShowToolbarInFullscreenChanged(const web_app::AppId& app_id,
+  void OnAlwaysShowToolbarInFullscreenChanged(const webapps::AppId& app_id,
                                               bool show) override;
   void OnAppRegistrarDestroyed() override;
 
