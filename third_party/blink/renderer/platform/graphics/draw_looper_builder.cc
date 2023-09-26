545c9a0b9877d8d0997767e7b651e5b774d5bbca
Aaron Krajeski <aaronhk@chromium.org>
2023-09-26 19:01:49 +0000

    Don't squash shadows to integer color
    DrawLooperBuilder::AddShadow was using an SkColor, thus we were losing
    out-of-gamut rgb color for shadows.
    
    crbug.com/1439859 also contains a weird corner case with gradient text
    on mac only that requires non-integer font and explicit line height. I
    haven't been able to track the cause of it down, but it is worth
    separating into a different issue. Here is a test for that:
    https://chromium-review.googlesource.com/c/chromium/src/+/4892666
    
    Bug: 1439859
    Change-Id: Ifc77c9eff08fd6072e59cdebf3182455e0c0543a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4879833
    Reviewed-by: Yi Xu <yiyix@chromium.org>
    Commit-Queue: Aaron Krajeski <aaronhk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201718}

--- third_party/blink/renderer/platform/graphics/draw_looper_builder.cc
+++ third_party/blink/renderer/platform/graphics/draw_looper_builder.cc
@@ -69,8 +69,6 @@ void DrawLooperBuilder::AddShadow(const gfx::Vector2dF& offset,
     return;
   }
 
-  SkColor sk_color = color.Rgb();
-
   SkLayerDrawLooper::LayerInfo info;
 
   switch (shadow_alpha_mode) {
@@ -99,7 +97,8 @@ void DrawLooperBuilder::AddShadow(const gfx::Vector2dF& offset,
         SkMaskFilter::MakeBlur(kNormal_SkBlurStyle, sigma, respectCTM));
   }
 
-  paint->setColorFilter(SkColorFilters::Blend(sk_color, SkBlendMode::kSrcIn));
+  paint->setColorFilter(SkColorFilters::Blend(
+      color.toSkColor4f(), SkColorSpace::MakeSRGB(), SkBlendMode::kSrcIn));
 }
 
 }  // namespace blink
