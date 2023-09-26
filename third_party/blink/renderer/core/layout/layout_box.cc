13ddbd8457367b1aa9e363eea1c7d18051897179
Kent Tamura <tkent@chromium.org>
2023-09-26 04:51:57 +0000

    Implement form-sizing behavior for <select>
    * Drop down:
      The width depends on the selected OPTION label, instead of the maximum
      width of all OPTION labels.
    
    * List box:
      The height depends on the total height of OPTION labels, instead of
      the 'size' attribute value.
    
    They have zero content height if there are no OPTIONs.
    
    Bug: 1447058
    Change-Id: I16cfb423a8caa776e2c6df1b0bd1bf82413c1447
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4893426
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201354}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -1371,7 +1371,8 @@ LayoutUnit LayoutBox::DefaultIntrinsicContentInlineSize() const {
   const bool apply_fixed_size = StyleRef().ApplyControlFixedSize();
   const auto* select = DynamicTo<HTMLSelectElement>(element);
   if (UNLIKELY(select && select->UsesMenuList())) {
-    return MenuListIntrinsicInlineSize(*select, *this);
+    return apply_fixed_size ? MenuListIntrinsicInlineSize(*select, *this)
+                            : kIndefiniteSize;
   }
   const auto* input = DynamicTo<HTMLInputElement>(element);
   if (UNLIKELY(input)) {
@@ -1410,21 +1411,6 @@ LayoutUnit LayoutBox::DefaultIntrinsicContentBlockSize() const {
   // get here.
   DCHECK(!HasOverrideIntrinsicContentLogicalHeight());
 
-  const bool apply_fixed_size = StyleRef().ApplyControlFixedSize();
-  if (const auto* select = DynamicTo<HTMLSelectElement>(GetNode())) {
-    if (select->UsesMenuList())
-      return MenuListIntrinsicBlockSize(*select, *this);
-    return ListBoxItemBlockSize(*select, *this) * select->ListBoxSize() -
-           ComputeLogicalScrollbars().BlockSum();
-  }
-  if (IsTextField() && apply_fixed_size) {
-    return TextFieldIntrinsicBlockSize(*To<HTMLInputElement>(GetNode()), *this);
-  }
-  if (IsTextArea() && apply_fixed_size) {
-    return TextAreaIntrinsicBlockSize(*To<HTMLTextAreaElement>(GetNode()),
-                                      *this);
-  }
-
   auto effective_appearance = StyleRef().EffectiveAppearance();
   if (effective_appearance == kCheckboxPart) {
     return ThemePartIntrinsicSize(*this, WebThemeEngine::kPartCheckbox)
@@ -1432,6 +1418,23 @@ LayoutUnit LayoutBox::DefaultIntrinsicContentBlockSize() const {
   }
   if (effective_appearance == kRadioPart) {
     return ThemePartIntrinsicSize(*this, WebThemeEngine::kPartRadio).block_size;
+  }
+
+  if (!StyleRef().ApplyControlFixedSize()) {
+    return kIndefiniteSize;
+  }
+  if (const auto* select = DynamicTo<HTMLSelectElement>(GetNode())) {
+    if (select->UsesMenuList())
+      return MenuListIntrinsicBlockSize(*select, *this);
+    return ListBoxItemBlockSize(*select, *this) * select->ListBoxSize() -
+           ComputeLogicalScrollbars().BlockSum();
+  }
+  if (IsTextField()) {
+    return TextFieldIntrinsicBlockSize(*To<HTMLInputElement>(GetNode()), *this);
+  }
+  if (IsTextArea()) {
+    return TextAreaIntrinsicBlockSize(*To<HTMLTextAreaElement>(GetNode()),
+                                      *this);
   }
 
   return kIndefiniteSize;
