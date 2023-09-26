8385a348a0bd200e98365d988046b37dfadb6c4c
Danny Wang <wangdanny@google.com>
2023-09-26 18:52:55 +0000

    customization: Utilize new hardcoded action in UI
    Bug: b/241965717
    Test: browser_tests
    Change-Id: I679582e68cd95962bdbfd844d49a404501736be5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4869246
    Commit-Queue: Danny Wang <wangdanny@google.com>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1201707}

--- chrome/browser/ui/webui/settings/ash/input_device_settings/input_device_settings_provider.cc
+++ chrome/browser/ui/webui/settings/ash/input_device_settings/input_device_settings_provider.cc
@@ -21,10 +21,13 @@ namespace ash::settings {
 
 namespace {
 
+using ActionTypeVariant =
+    absl::variant<AcceleratorAction, ::ash::mojom::HardCodedAction>;
+
 // Used to represent a constant version of the mojom::ActionChoice struct.
 struct ActionChoice {
   const char* name;
-  AcceleratorAction action_id;
+  ActionTypeVariant action_variant;
 };
 
 // TODO(dpad): Update list to official list of actions.
@@ -39,6 +42,8 @@ constexpr ActionChoice kMouseButtonOptions[] = {
     {"Turn on high contrast", AcceleratorAction::kToggleHighContrast},
     {"Turn on magnifier", AcceleratorAction::kToggleFullscreenMagnifier},
     {"Turn on dictation", AcceleratorAction::kEnableOrToggleDictation},
+    {"Copy", ::ash::mojom::HardCodedAction::kCopy},
+    {"Paste", ::ash::mojom::HardCodedAction::kPaste},
 };
 
 // TODO(dpad): Update list to official list of actions.
@@ -53,7 +58,23 @@ constexpr ActionChoice kGraphicsTabletOptions[] = {
     {"Turn on high contrast", AcceleratorAction::kToggleHighContrast},
     {"Turn on magnifier", AcceleratorAction::kToggleFullscreenMagnifier},
     {"Turn on dictation", AcceleratorAction::kEnableOrToggleDictation},
+    {"Copy", ::ash::mojom::HardCodedAction::kCopy},
+    {"Paste", ::ash::mojom::HardCodedAction::kPaste},
 };
+
+mojom::ActionTypePtr GetActionType(AcceleratorAction action) {
+  return mojom::ActionType::NewAcceleratorAction(action);
+}
+
+mojom::ActionTypePtr GetActionType(
+    ::ash::mojom::HardCodedAction hardcoded_action) {
+  return mojom::ActionType::NewHardcodedAction(hardcoded_action);
+}
+
+mojom::ActionTypePtr GetActionTypeFromVariant(ActionTypeVariant variant) {
+  return absl::visit([](auto&& value) { return GetActionType(value); },
+                     variant);
+}
 
 template <typename T>
 struct CustomDeviceKeyComparator {
@@ -502,7 +523,8 @@ void InputDeviceSettingsProvider::
         GetActionsForGraphicsTabletButtonCustomizationCallback callback) {
   std::vector<mojom::ActionChoicePtr> choices;
   for (const auto& choice : kGraphicsTabletOptions) {
-    choices.push_back(mojom::ActionChoice::New(choice.action_id, choice.name));
+    choices.push_back(mojom::ActionChoice::New(
+        GetActionTypeFromVariant(choice.action_variant), choice.name));
   }
   std::move(callback).Run(std::move(choices));
 }
@@ -511,7 +533,8 @@ void InputDeviceSettingsProvider::GetActionsForMouseButtonCustomization(
     GetActionsForMouseButtonCustomizationCallback callback) {
   std::vector<mojom::ActionChoicePtr> choices;
   for (const auto& choice : kMouseButtonOptions) {
-    choices.push_back(mojom::ActionChoice::New(choice.action_id, choice.name));
+    choices.push_back(mojom::ActionChoice::New(
+        GetActionTypeFromVariant(choice.action_variant), choice.name));
   }
   std::move(callback).Run(std::move(choices));
 }
