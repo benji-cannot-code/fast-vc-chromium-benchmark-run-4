79faa613a68600cc732572f8565f6d2e116fff46
Julie Jeongeun Kim <jkim@igalia.com>
2023-09-26 03:40:45 +0000

    Reland "[ios] Introduce DateTimeChooserCoordinator"
    This is a reland of commit 761286a2b3e8be93622be14b38ddd0be799332c3
    
    There are no changes from what originally landed.
    
    Original change's description:
    > [ios] Introduce DateTimeChooserCoordinator
    >
    > This CL creates DateTimeChooser dialog using UI components on iOS and
    > handles the selected result. It introduces DateTimeChooserCoordinator,
    > DateTimeChooserMediator, DateTimeChooserController, and
    > DateTimeChooserDelegate to manage UI components and communicate with
    > them.
    >
    > These are created when mojom::DateTimeChooser is bound
    > and OpenDateTimeDialog() is called.
    >
    > Bug: 1461947
    > Change-Id: Iadf712e9f8fe7a879bd0362a02ed47263bdec499
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4699373
    > Reviewed-by: Mark Cogan <marq@chromium.org>
    > Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1200094}
    
    Bug: 1461947
    Change-Id: Ie236f7f84954b71e507369640cd0f00b2a19fa65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4885998
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201347}

---
+++ content/browser/date_time_chooser/ios/date_time_chooser_mediator.mm
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "content/browser/date_time_chooser/ios/date_time_chooser_mediator.h"
+
+#import "content/browser/date_time_chooser/ios/date_time_chooser_ios.h"
+
+@interface DateTimeChooserMediator ()
+@property(nonatomic, assign) content::DateTimeChooserIOS* dateTimeChooser;
+@end
+
+@implementation DateTimeChooserMediator
+- (instancetype)initWithDateTimeChooser:
+    (content::DateTimeChooserIOS*)dateTimeChooser {
+  if (!(self = [super init])) {
+    return nil;
+  }
+
+  _dateTimeChooser = dateTimeChooser;
+  return self;
+}
+
+#pragma mark - DateTimeChooserDelegate
+
+- (void)dateTimeChooser:(DateTimeChooserViewController*)chooser
+    didCloseSuccessfully:(BOOL)success
+                withDate:(NSDate*)date {
+  self.dateTimeChooser->OnDialogClosed(success,
+                                       [date timeIntervalSince1970] * 1000);
+}
+
+@end
