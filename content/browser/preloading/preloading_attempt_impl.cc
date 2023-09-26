15a7f2363dfc1b4b9ba2a46522dab4ed3c398c6b
Liviu Tinta <liviutinta@chromium.org>
2023-09-26 17:49:11 +0000

    Add UKM for Service Worker Registered check for SR prefetch
    In order to proceed with a prefetch request we are checking if the
    prefetch URL is under the path of a registered service worker. If it
    is, then we fail the prefetch request in order to fallback to the
    caching logic of the Service Worker.
    
    This CL adds UKM metrics for tracking the type of Service Worker check
    we are doing and how long this check takes in microseconds.
    
    The privacy approved UKM Collection review document is here:
    https://docs.google.com/document/d/1fEf8VLsUIU62NrQMKL9Rv3m9gT5NToVLeidv8YW_eqo/edit?resourcekey=0-TD8eP4ttmdpSSZrJsVLaKg
    
    Bug: 1476093
    Change-Id: Ia97e13c3e354b09a6bfcd07a85469b23194657b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4888781
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Liviu Tinta <liviutinta@chromium.org>
    Reviewed-by: Iman Saboori <isaboori@google.com>
    Reviewed-by: Sun Yueru <yrsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201684}

--- content/browser/preloading/preloading_attempt_impl.cc
+++ content/browser/preloading/preloading_attempt_impl.cc
@@ -261,6 +261,17 @@ void PreloadingAttemptImpl::RecordPreloadingAttemptMetrics(
     if (eagerness_) {
       builder.SetSpeculationEagerness(static_cast<int64_t>(eagerness_.value()));
     }
+    if (service_worker_registered_check_) {
+      builder.SetPrefetchServiceWorkerRegisteredCheck(
+          static_cast<int64_t>(service_worker_registered_check_.value()));
+    }
+    if (service_worker_registered_check_duration_) {
+      builder.SetPrefetchServiceWorkerRegisteredForURLCheckDuration(
+          ukm::GetExponentialBucketMin(
+              service_worker_registered_check_duration_.value()
+                  .InMicroseconds(),
+              kServiceWorkerRegisteredCheckDurationBucketSpacing));
+    }
     builder.Record(ukm_recorder);
   }
 
@@ -285,6 +296,17 @@ void PreloadingAttemptImpl::RecordPreloadingAttemptMetrics(
     }
     if (eagerness_) {
       builder.SetSpeculationEagerness(static_cast<int64_t>(eagerness_.value()));
+    }
+    if (service_worker_registered_check_) {
+      builder.SetPrefetchServiceWorkerRegisteredCheck(
+          static_cast<int64_t>(service_worker_registered_check_.value()));
+    }
+    if (service_worker_registered_check_duration_) {
+      builder.SetPrefetchServiceWorkerRegisteredForURLCheckDuration(
+          ukm::GetExponentialBucketMin(
+              service_worker_registered_check_duration_.value()
+                  .InMicroseconds(),
+              kServiceWorkerRegisteredCheckDurationBucketSpacing));
     }
     builder.Record(ukm_recorder);
   }
@@ -326,6 +348,16 @@ void PreloadingAttemptImpl::SetSpeculationEagerness(
       << "predictor_type_: " << predictor_type_.name()
       << " (ukm_value = " << predictor_type_.ukm_value() << ")";
   eagerness_ = eagerness;
+}
+
+void PreloadingAttemptImpl::SetServiceWorkerRegisteredCheck(
+    ServiceWorkerRegisteredCheck check) {
+  service_worker_registered_check_ = check;
+}
+
+void PreloadingAttemptImpl::SetServiceWorkerRegisteredCheckDuration(
+    base::TimeDelta duration) {
+  service_worker_registered_check_duration_ = duration;
 }
 
 // Used for StateTransitions matching.
