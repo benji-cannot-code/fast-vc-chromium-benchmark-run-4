78676b9822283915bf6c5543747bc891771ce5a9
Abe Boujane <boujane@google.com>
2023-09-26 00:21:57 +0000

    Implement Tracking protection Notice Service
    The Tracking Protection notice service is responsible
    for showing/hiding the onboarding notice based on
    the user interaction with the browser.
    
    Bug: b:295926275
    Change-Id: I3a754bfd1a1f1578e776b6bf12030f546d575a9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4889557
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Abe Boujane <boujane@google.com>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Auto-Submit: Abe Boujane <boujane@google.com>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1201249}

--- chrome/browser/ui/tab_helpers.cc
+++ chrome/browser/ui/tab_helpers.cc
@@ -277,6 +277,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/supervised_user/supervised_user_navigation_observer.h"
 #endif
 
+#if !BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/privacy_sandbox/tracking_protection_notice_service.h"
+#endif
+
 using content::WebContents;
 
 namespace {
@@ -553,6 +557,13 @@ void TabHelpers::AttachTabHelpers(WebContents* web_contents) {
   SadTabHelper::CreateForWebContents(web_contents);
   SearchTabHelper::CreateForWebContents(web_contents);
   TabDialogs::CreateForWebContents(web_contents);
+#if !BUILDFLAG(IS_ANDROID)
+  if (privacy_sandbox::TrackingProtectionNoticeService::TabHelper::
+          IsHelperNeeded(profile)) {
+    privacy_sandbox::TrackingProtectionNoticeService::TabHelper::
+        CreateForWebContents(web_contents);
+  }
+#endif
   HighEfficiencyChipTabHelper::CreateForWebContents(web_contents);
   if (base::FeatureList::IsEnabled(
           performance_manager::features::kMemoryUsageInHovercards)) {
