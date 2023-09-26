7019b0a92ce94b74e43e9ba7345abdb42b1c6c48
Angus L. M. McLean IV <angusmclean@google.com>
2023-09-26 21:39:23 +0000

    tote tour: Add counterfactual and drop to pin feature params
    Adds params to the HoldingSpaceTour feature flag in ash. This will
    allow for branching behavior in upcoming CLs for the different arms
    of the experiment and collecting metrics on a counterfactual arm.
    
    When the HoldingSpaceTour:drop-to-pin flag is set to true, dropping
    a file on the desktop will pin it to holding space. Strings will also
    be updated to reflect this behavior.
    
    Bug: b:274659315
    Change-Id: I2183880d4836b38cb4ed17305de89417c72a672a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4894944
    Commit-Queue: Angus McLean <angusmclean@google.com>
    Reviewed-by: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1201815}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1439,6 +1439,12 @@ BASE_FEATURE(kHoldingSpaceTour,
              "HoldingSpaceTour",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+const base::FeatureParam<bool> kHoldingSpaceTourDropToPinEnabled{
+    &kHoldingSpaceTour, "drop-to-pin", false};
+
+const base::FeatureParam<bool> kHoldingSpaceTourEnabledCounterfactually{
+    &kHoldingSpaceTour, "is-counterfactual", false};
+
 BASE_FEATURE(kHomeButtonQuickAppAccess,
              "HomeButtonQuickAppAccess",
              base::FEATURE_ENABLED_BY_DEFAULT);
@@ -3433,8 +3439,17 @@ bool IsHoldingSpaceSuggestionsEnabled() {
   return base::FeatureList::IsEnabled(kHoldingSpaceSuggestions);
 }
 
+bool IsHoldingSpaceTourDropToPinEnabled() {
+  return IsHoldingSpaceTourEnabled() && kHoldingSpaceTourDropToPinEnabled.Get();
+}
+
 bool IsHoldingSpaceTourEnabled() {
   return base::FeatureList::IsEnabled(kHoldingSpaceTour);
+}
+
+bool IsHoldingSpaceTourEnabledCounterfactually() {
+  return IsHoldingSpaceTourEnabled() &&
+         kHoldingSpaceTourEnabledCounterfactually.Get();
 }
 
 bool IsHomeButtonQuickAppAccessEnabled() {
