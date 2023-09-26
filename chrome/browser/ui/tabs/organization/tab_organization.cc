ce839bd0d307ab69ab734a0d9a67c64b8f3e6691
David Pennington <dpenning@chromium.org>
2023-09-26 05:31:49 +0000

    [TabOrganization] Create IsValid function for TabOrganization.
    Change-Id: Ia0d4d649c354401758e274f15540628858b7bbfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4863187
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1201359}

--- chrome/browser/ui/tabs/organization/tab_organization.cc
+++ chrome/browser/ui/tabs/organization/tab_organization.cc
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
 
+namespace {
+constexpr int kMinValidTabsForOrganizing = 2;
+}
+
 TabOrganization::TabOrganization(
     TabDatas tab_datas,
     std::vector<std::u16string> names,
@@ -32,6 +36,20 @@ const std::u16string TabOrganization::GetDisplayName() const {
     return absl::get<std::u16string>(current_name());
   }
   return u"";
+}
+
+bool TabOrganization::IsValidForOrganizing() const {
+  // there must be at least 2 tabs that are valid for organization.
+  int valid_tab_count = 0;
+  for (const std::unique_ptr<TabData>& tab_data : tab_datas_) {
+    if (tab_data->IsValidForOrganizing()) {
+      valid_tab_count++;
+      if (valid_tab_count >= kMinValidTabsForOrganizing) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 // TODO(1469128) Add UKM/UMA Logging on user add.
