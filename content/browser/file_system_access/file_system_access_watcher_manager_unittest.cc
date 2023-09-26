e58bc97ea84916c022da74591eb3102a1990341c
Ian Vollick <vollick@chromium.org>
2023-09-26 22:59:58 +0000

    [ios] Elide some unused functions
    This CL uses a buildflag to elide two functions that aren't used.
    
    Also elides a test to match Android/Fuchsia.
    
    Bug: 1411704
    Change-Id: I7010dda58d54228b3e97f45d4e9a59890d74fe79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4893552
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201854}

--- content/browser/file_system_access/file_system_access_watcher_manager_unittest.cc
+++ content/browser/file_system_access/file_system_access_watcher_manager_unittest.cc
@@ -42,7 +42,7 @@ using Observation = FileSystemAccessWatcherManager::Observation;
 
 namespace {
 
-#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_FUCHSIA)
+#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_FUCHSIA) && !BUILDFLAG(IS_IOS)
 void SpinEventLoopForABit() {
   base::RunLoop loop;
   base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
@@ -58,7 +58,8 @@ bool ReportsModifiedPathForLocalObservations() {
   return false;
 #endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 }
-#endif  // !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_FUCHSIA)
+#endif  // !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_FUCHSIA) &&
+        // !BUILDFLAG(IS_IOS)
 
 // Accumulates changes it receives from the given `observation`.
 class ChangeAccumulator {
@@ -197,7 +198,7 @@ class FileSystemAccessWatcherManagerTest : public testing::Test {
 };
 
 // Watching the local file system is not supported on Android or Fuchsia.
-#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_FUCHSIA)
+#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_FUCHSIA) && !BUILDFLAG(IS_IOS)
 TEST_F(FileSystemAccessWatcherManagerTest, BasicRegistration) {
   base::FilePath dir_path = dir_.GetPath().AppendASCII("dir");
   auto dir_url = manager_->CreateFileSystemURLFromPath(
@@ -229,7 +230,8 @@ TEST_F(FileSystemAccessWatcherManagerTest, BasicRegistration) {
   EXPECT_FALSE(watcher_manager().HasObservationsForTesting());
   EXPECT_FALSE(watcher_manager().HasSourcesForTesting());
 }
-#endif  // !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_FUCHSIA)
+#endif  // !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_FUCHSIA) &&
+        // !BUILDFLAG(IS_IOS)
 
 TEST_F(FileSystemAccessWatcherManagerTest, BasicRegistrationUnownedSource) {
   base::FilePath file_path = dir_.GetPath().AppendASCII("foo");
