dd6aca3fe6b0bcb9aece8a8801a0d5bbea1f856f
Charles Meng <charlesmeng@chromium.org>
2023-09-26 16:08:13 +0000

    Create second version of preloading page with toggle
    Preloading page is shown in performance settings based on a flag. Create
    a second version that replaces the original version when the
    use_v2_preloading_subpage feature param is set to true. Instead of
    having a radio button for disabling preloading, there will be a toggle
    that when disabled will hide the standard/extended preloading options.
    
    original: https://screenshot.googleplex.com/AQo2GztrV3hStEK.png
    version 2: https://screenshot.googleplex.com/AXTcCKXF7YjT3zA.png
    
    Bug: 1484278
    Change-Id: I7beee4887c7177fcaedbb6b6c8e2ceefc3a93baf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4874566
    Reviewed-by: John Lee <johntlee@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Charles Meng <charlesmeng@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201620}

--- chrome/browser/preloading/preloading_features.h
+++ chrome/browser/preloading/preloading_features.h
@@ -7,10 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_PRELOADING_PRELOADING_FEATURES_H_
 
 #include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
 
 namespace features {
 
 BASE_DECLARE_FEATURE(kPerformanceSettingsPreloadingSubpage);
+
+// Whether the v2 UI for the preloading page is shown in performance settings.
+extern const base::FeatureParam<bool> kPerformanceSettingsPreloadingSubpageV2;
 
 }  // namespace features
 
