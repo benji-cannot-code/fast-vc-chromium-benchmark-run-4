a5c2e60482863b9d73a83a37683d8dc26ce041bd
Ali Hijazi <ahijazi@chromium.org>
2023-09-26 16:51:22 +0000

    Rewrite T& into raw_ref<T>
    This change was generated by running
    `tools/clang/rewrite_raw_ptr_fields/rewrite-multiple-platforms.sh`
    for windows.
    
    This is a prerequisite for enforcing raw_ref usage on windows.
    
    Bug: 1446146
    Change-Id: I09b5cad4530ba94834fecc51161a84e24298dba4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4894131
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ali Hijazi <ahijazi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201657}

--- chrome/browser/profiles/profile_shortcut_manager_win.cc
+++ chrome/browser/profiles/profile_shortcut_manager_win.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
 #include "base/logging.h"
+#include "base/memory/raw_ref.h"
 #include "base/path_service.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/strcat.h"
@@ -233,8 +234,8 @@ bool IsChromeShortcut(const base::FilePath& path,
 // that have the specified |command_line|. If |include_empty_command_lines| is
 // true Chrome desktop shortcuts with empty command lines will also be included.
 struct ChromeCommandLineFilter {
-  const base::FilePath& chrome_exe;
-  const std::wstring& command_line;
+  const raw_ref<const base::FilePath> chrome_exe;
+  const raw_ref<const std::wstring> command_line;
   bool include_empty_command_lines;
 
   ChromeCommandLineFilter(const base::FilePath& chrome_exe,
@@ -246,14 +247,15 @@ struct ChromeCommandLineFilter {
 
   bool operator()(const base::FilePath& path) const {
     std::wstring shortcut_command_line;
-    if (!IsChromeShortcut(path, chrome_exe, &shortcut_command_line))
+    if (!IsChromeShortcut(path, *chrome_exe, &shortcut_command_line)) {
       return false;
+    }
 
     // TODO(asvitkine): Change this to build a CommandLine object and ensure all
     // args from |command_line| are present in the shortcut's CommandLine. This
     // will be more robust when |command_line| contains multiple args.
     if ((shortcut_command_line.empty() && include_empty_command_lines) ||
-        (shortcut_command_line.find(command_line) != std::wstring::npos)) {
+        (shortcut_command_line.find(*command_line) != std::wstring::npos)) {
       return true;
     }
     return false;
