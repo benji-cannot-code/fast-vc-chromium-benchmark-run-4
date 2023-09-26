c4eb9b61d4855a406de43e0920702545c8b11dca
Michael Wilson <mjwilson@chromium.org>
2023-09-26 16:46:36 +0000

    Add output buffer bypass to WebAudio
    Currently audio_destination will pull audio data from the previous
    callback before generating new audio data and pushing it to the
    FIFO.  This results in one callback size of of additional latency.
    
    This change does the following, behind a command-line switch:
    - Pull audio data immediately after it is generated
    - Do not prime the output FIFO
    - Do not use the adaptive growth mechanism of the output FIFO
    
    The output FIFO is still necessary because the requested frames
    may not be a multiple of the render quantum size.
    
    With this feature enabled, instead of underruns in the FIFO we will
    get messages like the following from the audio backend if there are
    underruns:
    
      WARNING:sync_reader.cc(255)] SyncReader::Read timed out,
      audio glitch count=10
    
    This patch requires waiting on the AudioWorklet runner thread.
    Ideally, we would run the AudioWorklet on the main audio thread
    instead, but I haven't figured out how to do that yet.
    
    Latency test results (Linux using PulseAudio backend):
    
    Using command:
    out/Default/chrome --audio-buffer-size=128 --disable-features=WebAudioBypassOutputBuffering https://padenot.github.io/roundtrip-latency-tester/
    
    Result:
    - AudioContext sample-rate: : 48000
    - Measured roundtrip: 93.35416666666667ms
    - Computed output latency: 22ms
    
    Using command:
    out/Default/chrome --audio-buffer-size=128 --enable-features=WebAudioBypassOutputBuffering https://padenot.github.io/roundtrip-latency-tester/
    
    Result:
    - AudioContext sample-rate: : 48000
    - Measured roundtrip: 72.02083333333334ms
    - Computed output latency: 22ms
    
    (note that 128 samples at 48000 Hz is about 2.67 ms)
    
    Bug: 1068085
    Change-Id: I66931111c071c10a50eb5d46136f5842b61dbe7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3422428
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201649}

--- base/threading/thread_restrictions.h
+++ base/threading/thread_restrictions.h
@@ -177,6 +177,7 @@ namespace audio {
 class OutputDevice;
 }
 namespace blink {
+class AudioDestination;
 class DiskDataAllocator;
 class IdentifiabilityActiveSampler;
 class RTCVideoDecoderAdapter;
@@ -814,6 +815,7 @@ class BASE_EXPORT
   friend class base::StackSamplingProfiler;
   friend class base::internal::JobTaskSource;
   friend class base::sequence_manager::internal::TaskQueueImpl;
+  friend class blink::AudioDestination;
   friend class blink::RTCVideoDecoderAdapter;
   friend class blink::RTCVideoEncoder;
   friend class blink::WebRtcVideoFrameAdapter;
--- third_party/blink/renderer/platform/audio/audio_destination.h
+++ third_party/blink/renderer/platform/audio/audio_destination.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/scoped_refptr.h"
 #include "base/synchronization/lock.h"
+#include "base/synchronization/waitable_event.h"
 #include "base/task/single_thread_task_runner.h"
 #include "media/base/audio_renderer_sink.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -150,6 +151,10 @@ class PLATFORM_EXPORT AudioDestination final
   // The actual render request to the WebAudio destination node. This method
   // can be invoked on both AudioDeviceThread (single-thread rendering) and
   // AudioWorkletThread (dual-thread rendering).
+  void RequestRenderWait(size_t frames_requested,
+                         size_t frames_to_render,
+                         double delay,
+                         double delay_timestamp);
   void RequestRender(size_t frames_requested,
                      size_t frames_to_render,
                      double delay,
@@ -213,6 +218,12 @@ class PLATFORM_EXPORT AudioDestination final
 
   // Collect the device latency matric only from the initial callback.
   bool is_latency_metric_collected_ = false;
+
+  // This WaitableEvent is only for use with the kWebAudioBypassOutputBuffering
+  // flag enabled. No other WaitableEvents may be used in this class.
+  base::WaitableEvent output_buffer_bypass_wait_event_;
+
+  const bool is_output_buffer_bypassed_ = false;
 };
 
 }  // namespace blink
