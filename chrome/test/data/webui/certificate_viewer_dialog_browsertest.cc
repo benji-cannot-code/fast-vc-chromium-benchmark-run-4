ae344f60a6719732a77e4a40e1b2f3fb7e55ab31
dpapad <dpapad@chromium.org>
2023-09-26 00:12:21 +0000

    WebUI: Use runMochaTest() helper in CertificateViewerUITest.
    Bug: 1457360,783334
    Change-Id: Iad8659cfbca5603ea93bf3d8a5ebc784823065a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4890020
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201244}

--- chrome/test/data/webui/certificate_viewer_dialog_browsertest.cc
+++ chrome/test/data/webui/certificate_viewer_dialog_browsertest.cc
@@ -50,6 +50,13 @@ class CertificateViewerUITest : public WebUIMochaBrowserTest {
     return certs;
   }
 
+  void RunTestCase(const std::string& testCase) {
+    RunTestWithoutTestLoader(
+        "certificate_viewer_dialog_test.js",
+        base::StringPrintf("runMochaTest('CertificateViewer', '%s');",
+                           testCase.c_str()));
+  }
+
  private:
   content::WebContents* ShowCertificateViewer(
       std::vector<bssl::UniquePtr<CRYPTO_BUFFER>> certs) {
@@ -71,18 +78,15 @@ class CertificateViewerUITest : public WebUIMochaBrowserTest {
 };
 
 IN_PROC_BROWSER_TEST_F(CertificateViewerUITest, DialogURL) {
-  RunTestWithoutTestLoader("certificate_viewer_dialog_test.js",
-                           "mocha.grep('DialogURL').run()");
+  RunTestCase("DialogURL");
 }
 
 IN_PROC_BROWSER_TEST_F(CertificateViewerUITest, CommonName) {
-  RunTestWithoutTestLoader("certificate_viewer_dialog_test.js",
-                           "mocha.grep('CommonName').run()");
+  RunTestCase("CommonName");
 }
 
 IN_PROC_BROWSER_TEST_F(CertificateViewerUITest, Details) {
-  RunTestWithoutTestLoader("certificate_viewer_dialog_test.js",
-                           "mocha.grep('Details').run()");
+  RunTestCase("Details");
 }
 
 class CertificateViewerUIInvalidCertTest : public CertificateViewerUITest {
@@ -97,6 +101,5 @@ class CertificateViewerUIInvalidCertTest : public CertificateViewerUITest {
 };
 
 IN_PROC_BROWSER_TEST_F(CertificateViewerUIInvalidCertTest, InvalidCert) {
-  RunTestWithoutTestLoader("certificate_viewer_dialog_test.js",
-                           "mocha.grep('InvalidCert').run()");
+  RunTestCase("InvalidCert");
 }
