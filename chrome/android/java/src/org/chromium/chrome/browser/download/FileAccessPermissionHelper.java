f47bd5d6da863a77aefd16e58f8d6940e50e9b6c
Dushyant Kant Sharma <dush.sharma@samsung.com>
2023-09-26 13:54:27 +0000

    Use inline build condition instead of deprecated function
    Bug: None
    
    Change-Id: I765f0ee2ab59d660aeb455ab1857150aa97baba0
    Signed-off-by: Dushyant Kant Sharma <dush.sharma@samsung.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4891763
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201527}

--- chrome/android/java/src/org/chromium/chrome/browser/download/FileAccessPermissionHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/FileAccessPermissionHelper.java
@@ -8,11 +8,11 @@ package org.chromium.chrome.browser.download;
 import android.Manifest.permission;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.util.Pair;
 
 import androidx.annotation.NonNull;
 
-import org.chromium.base.BuildInfo;
 import org.chromium.base.Callback;
 import org.chromium.components.permissions.AndroidPermissionRequester;
 import org.chromium.ui.base.WindowAndroid;
@@ -54,7 +54,7 @@ public class FileAccessPermissionHelper {
 
     static void requestFileAccessPermissionHelper(
             @NonNull WindowAndroid windowAndroid, final Callback<Pair<Boolean, String>> callback) {
-        if (BuildInfo.isAtLeastT()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
                 || windowAndroid.hasPermission(permission.WRITE_EXTERNAL_STORAGE)) {
             callback.onResult(Pair.create(true, null));
             return;
