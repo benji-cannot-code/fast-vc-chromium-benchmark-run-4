435f8577200a28aa6e11abc116ec867e7e594ec1
Jonathan Lee <jonathanjlee@google.com>
2023-09-26 19:40:56 +0000

    Roll wpt tooling
    This rolls wpt to latest commit at
    https://github.com/web-platform-tests/wpt.
    REMOTE-WPT-HEAD: d64f55daffaf7ad0e8cd041ddcfecffb8c7afaaf
    
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I35d26c4f6b5836a4f300c0cbad68387c1cd286d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4891267
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1201743}

--- third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/executors/executorchrome.py
+++ third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/executors/executorchrome.py
@@ -80,6 +80,8 @@ class ChromeDriverTestharnessProtocolPart(WebDriverTestharnessProtocolPart):
         # exposed to the base WebDriver testharness executor.
         self.test_window = None
         self.reuse_window = self.parent.reuse_window
+        # Company prefix to apply to vendor-specific WebDriver extension commands.
+        self.cdp_company_prefix = "goog"
 
     def close_test_window(self):
         if self.test_window:
@@ -108,7 +110,8 @@ class ChromeDriverTestharnessProtocolPart(WebDriverTestharnessProtocolPart):
                     "cmd": "Page.resetNavigationHistory",
                     "params": {},
                 }
-                self.webdriver.send_session_command("POST", "goog/cdp/execute",
+                self.webdriver.send_session_command("POST",
+                                                    self.cdp_company_prefix + "/cdp/execute",
                                                     body=body)
                 self.webdriver.url = "about:blank"
                 return
@@ -136,6 +139,8 @@ class ChromeDriverPrintProtocolPart(PrintProtocolPart):
     def setup(self):
         self.webdriver = self.parent.webdriver
         self.runner_handle = None
+        # Company prefix to apply to vendor-specific WebDriver extension commands.
+        self.cdp_company_prefix = "goog"
 
     def load_runner(self):
         url = urljoin(self.parent.executor.server_url("http"), "/print_pdf_runner.html")
@@ -167,7 +172,9 @@ class ChromeDriverPrintProtocolPart(PrintProtocolPart):
                 "printBackground": True,
             }
         }
-        return self.webdriver.send_session_command("POST", "goog/cdp/execute", body=body)["data"]
+        return self.webdriver.send_session_command("POST",
+                                                   self.cdp_company_prefix + "/cdp/execute",
+                                                   body=body)["data"]
 
     def pdf_to_png(self, pdf_base64, ranges):
         handle = self.webdriver.window_handle
