5de7900ced0ed60411e138385f06f42dc331ea69
Justin Green <greenjustin@google.com>
2023-09-26 13:51:41 +0000

    media/gpu: Add cyclic refresh support for hardware AV1 encoding
    Add cyclic refresh (AQ mode 3) support for hardware AV1 encoding.
    
    VideoEncoderTest.MeasureProducedBitstreamQuality
    PSNR Average
    Before
    180p: 37.8432dB
    360p: 39.5612dB
    720p: 40.3080dB
    
    After
    180p: 38.0221dB
    360p: 40.0368dB
    720p: 41.0902dB
    
    
    
    VideoEncoderTest.MeasureProducedBitstreamQuality
    Bitrate Deviation
    Before
    180p: 0.55%
    360p: 0.10%
    720p: -0.07%
    
    After
    180p: -0.51%
    360p: -0.01%
    720p: 0.03%
    
    
    
    VideoEncoderTest.MeasureUncappedPerformance
    Bitstream encode time average
    Before
    180p: 1.29832ms
    360p: 1.8383ms
    720p: 2.9665ms
    
    After
    180p: 1.28287ms
    360p: 2.17763ms
    720p: 3.81374ms
    
    Bug: b/274756117
    Test: Tested using video_encode_accelerator_perf_tests on Rex.
    
    Change-Id: I2af5e40b9bae6d59b2e337edd47673603b45b766
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4784030
    Commit-Queue: Justin Green <greenjustin@google.com>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201526}

--- media/gpu/vaapi/av1_vaapi_video_encoder_delegate.h
+++ media/gpu/vaapi/av1_vaapi_video_encoder_delegate.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_GPU_VAAPI_AV1_VAAPI_VIDEO_ENCODER_DELEGATE_H_
 #define MEDIA_GPU_VAAPI_AV1_VAAPI_VIDEO_ENCODER_DELEGATE_H_
 
+#include <stdint.h>
 #include <vector>
 
 #include "media/base/video_bitrate_allocation.h"
@@ -81,8 +82,9 @@ class AV1VaapiVideoEncoderDelegate : public VaapiVideoEncoderDelegate {
   std::vector<uint8_t> PackSequenceHeader() const;
   bool SubmitFrame(EncodeJob& job, PicParamOffsets& offsets);
   bool FillPictureParam(VAEncPictureParameterBufferAV1& pic_param,
+                        VAEncSegMapBufferAV1& segment_map_param,
                         const EncodeJob& job,
-                        const AV1Picture& pic) const;
+                        const AV1Picture& pic);
   bool SubmitFrameOBU(const VAEncPictureParameterBufferAV1& pic_param,
                       PicParamOffsets& offsets);
   std::vector<uint8_t> PackFrameHeader(
@@ -90,6 +92,7 @@ class AV1VaapiVideoEncoderDelegate : public VaapiVideoEncoderDelegate {
       PicParamOffsets& offsets) const;
   bool SubmitPictureParam(VAEncPictureParameterBufferAV1& pic_param,
                           const PicParamOffsets& offsets);
+  bool SubmitSegmentMap(const VAEncSegMapBufferAV1& segment_map_param);
   bool SubmitTileGroup();
   bool SubmitPackedData(const std::vector<uint8_t>& data);
 
@@ -103,6 +106,8 @@ class AV1VaapiVideoEncoderDelegate : public VaapiVideoEncoderDelegate {
   scoped_refptr<AV1Picture> last_frame_ = nullptr;
   VAEncSequenceParameterBufferAV1 seq_param_;
   std::unique_ptr<aom::AV1RateControlRTC> rate_ctrl_;
+  std::vector<uint8_t> segmentation_map_{};
+  uint32_t seg_size_;
 };
 
 }  // namespace media
