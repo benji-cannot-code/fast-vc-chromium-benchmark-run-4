f59d97e6afa219cb468a7e80e29169a2a1e5f29b
aliaaborhamy <aliaaborhamy@google.com>
2023-09-26 15:29:03 +0000

    [iOS] Add Open in Chrome extension's base
    Bug: 1478760
    Change-Id: I20691944d27e67134f3cf7af6de2325954d70fe8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4853545
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Aliaa Alborhamy <aliaaborhamy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1201599}

---
+++ ios/chrome/open_extension/open_view_controller.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_OPEN_EXTENSION_OPEN_VIEW_CONTROLLER_H_
+#define IOS_CHROME_OPEN_EXTENSION_OPEN_VIEW_CONTROLLER_H_
+
+#import <UIKit/UIKit.h>
+
+// This class defines the view controller for the Open In Chrome extension
+@interface OpenViewController : UIViewController
+@end
+
+#endif  // IOS_CHROME_OPEN_EXTENSION_OPEN_VIEW_CONTROLLER_H_
