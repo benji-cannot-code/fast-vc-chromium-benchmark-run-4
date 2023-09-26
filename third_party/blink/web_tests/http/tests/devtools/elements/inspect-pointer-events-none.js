851ff3c611ec4901456977dd813ca7b4007ddca2
Philip Pfaffe <pfaffe@chromium.org>
2023-09-26 11:01:57 +0000

    Remove loadLegacyModule('elements') from web tests
    Bug: 1442410
    Change-Id: Ifda7ad0b8ddc63e4fff368785d333f97252d737e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4886945
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201453}

--- third_party/blink/web_tests/http/tests/devtools/elements/inspect-pointer-events-none.js
+++ third_party/blink/web_tests/http/tests/devtools/elements/inspect-pointer-events-none.js
@@ -10,7 +10,6 @@ import * as ElementsModule from 'devtools/panels/elements/elements.js';
 
 (async function() {
   TestRunner.addResult(`Test that Web Inspector can inspect element with pointer-events:none.\n`);
-  await TestRunner.loadLegacyModule('elements');
   await TestRunner.showPanel('elements');
   await TestRunner.loadHTML(`
       <style>
--- third_party/blink/web_tests/http/tests/devtools/elements/move-node.js
+++ third_party/blink/web_tests/http/tests/devtools/elements/move-node.js
@@ -10,7 +10,6 @@ import * as ElementsModule from 'devtools/panels/elements/elements.js';
 
 (async function() {
   TestRunner.addResult(`Tests elements drag and drop operation internals, verifies post-move selection.\n`);
-  await TestRunner.loadLegacyModule('elements');
   await TestRunner.showPanel('elements');
   await TestRunner.loadHTML(`
       <div id="container">
