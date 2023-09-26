ef68686dfd8b3707c659c2bccd891f15705eade1
Glenn Hartmann <hartmanng@chromium.org>
2023-09-26 01:48:53 +0000

    Move web_app::AppId and web_app::ManifestId to a more public place
    So that Android and Desktop implementations can share the types.
    
    Note that this is part of a multi-CL change. This CL only touches
    references in /chrome/browser/payments/webapps.
    
    BUG=1486624
    
    This CL was uploaded by git cl split.
    
    R=glenrob@chromium.org
    
    Change-Id: I6fa80662526ff9acd8ac7d201f46740cad843733
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4884843
    Auto-Submit: Glenn Hartmann <hartmanng@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201309}

--- chrome/browser/payments/webapps/payment_request_lacros_browsertest.cc
+++ chrome/browser/payments/webapps/payment_request_lacros_browsertest.cc
@@ -18,11 +18,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/web_applications/web_app_controller_browsertest.h"
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
 #include "chrome/browser/web_applications/user_display_mode.h"
-#include "chrome/browser/web_applications/web_app_id.h"
 #include "chrome/browser/web_applications/web_app_install_info.h"
 #include "chromeos/crosapi/mojom/web_app_service.mojom.h"
 #include "chromeos/lacros/lacros_service.h"
 #include "components/payments/core/features.h"
+#include "components/webapps/common/web_app_id.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/content_mock_cert_verifier.h"
@@ -122,7 +122,7 @@ class PaymentRequestLacrosBrowserTest
     return https_server()->GetURL(kTestAppHost, "/simple.html");
   }
 
-  const web_app::AppId& app_id() const { return app_id_; }
+  const webapps::AppId& app_id() const { return app_id_; }
 
  protected:
   const bool has_associated_package_;
@@ -153,7 +153,7 @@ class PaymentRequestLacrosBrowserTest
   testing::NiceMock<MockWebAppService> service_;
   mojo::Receiver<crosapi::mojom::WebAppService> receiver_{&service_};
 
-  web_app::AppId app_id_;
+  webapps::AppId app_id_;
 };
 
 IN_PROC_BROWSER_TEST_P(PaymentRequestLacrosBrowserTest, BrowserTab) {
