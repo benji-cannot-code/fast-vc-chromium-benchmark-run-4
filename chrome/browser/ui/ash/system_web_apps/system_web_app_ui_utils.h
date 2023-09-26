7592e97fbe20326826298ee19c6c30dbcfdccc7c
Glenn Hartmann <hartmanng@chromium.org>
2023-09-26 16:32:04 +0000

    Move web_app::AppId and web_app::ManifestId to a more public place
    So that Android and Desktop implementations can share the types.
    
    Note that this is part of a multi-CL change. This CL only touches
    references in /chrome/browser/ui/ash/system_web_apps.
    
    BUG=1486624
    
    This CL was uploaded by git cl split.
    
    R=hidehiko@chromium.org
    
    Change-Id: I0da73ca98b4b94f80888c06dae147b12d61e90af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4887578
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Auto-Submit: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201641}

--- chrome/browser/ui/ash/system_web_apps/system_web_app_ui_utils.h
+++ chrome/browser/ui/ash/system_web_apps/system_web_app_ui_utils.h
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "chrome/browser/apps/app_service/app_launch_params.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/web_applications/web_app_id.h"
 #include "components/services/app_service/public/cpp/app_launch_util.h"
+#include "components/webapps/common/web_app_id.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "url/gurl.h"
 
@@ -24,10 +24,10 @@ namespace ash {
 // Returns the system app type for the given App ID.
 absl::optional<SystemWebAppType> GetSystemWebAppTypeForAppId(
     Profile* profile,
-    const web_app::AppId& app_id);
+    const webapps::AppId& app_id);
 
 // Returns the PWA system App ID for the given system app type.
-absl::optional<web_app::AppId> GetAppIdForSystemWebApp(
+absl::optional<webapps::AppId> GetAppIdForSystemWebApp(
     Profile* profile,
     SystemWebAppType app_type);
 
