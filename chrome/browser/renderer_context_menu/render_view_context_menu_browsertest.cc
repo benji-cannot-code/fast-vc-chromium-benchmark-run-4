0ebe0d9e6c06238f3eb5ff982b6698e8b1686059
Glenn Hartmann <hartmanng@chromium.org>
2023-09-26 17:45:55 +0000

    Move web_app::AppId and web_app::ManifestId to a more public place
    So that Android and Desktop implementations can share the types.
    
    Note that this is part of a multi-CL change. This CL only touches
    references in /chrome/browser/renderer_context_menu.
    
    BUG=1486624
    
    This CL was uploaded by git cl split.
    
    R=avi@chromium.org
    
    Change-Id: I00a1c053da82e546e8af328cf504096bef939bf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4884744
    Auto-Submit: Glenn Hartmann <hartmanng@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201683}

--- chrome/browser/renderer_context_menu/render_view_context_menu_browsertest.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu_browsertest.cc
@@ -168,8 +168,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using content::WebContents;
 using extensions::MimeHandlerViewGuest;
 using extensions::TestMimeHandlerViewGuest;
-using web_app::AppId;
 using web_app::WebAppProvider;
+using webapps::AppId;
 
 namespace {
 
