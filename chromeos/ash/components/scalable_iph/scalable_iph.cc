8cd6be9d85d4d79041a391a7208d02b1df0fc3a6
Rakina Zata Amni <rakina@chromium.org>
2023-09-26 07:01:08 +0000

    Revert "Scalable Notifiers: record print job creation"
    This reverts commit f8b1bb297502d7317e2c0a5efbd2fcf82e1a912f.
    
    Reason for revert: Might be causing some failing builds:
    https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/36138/overview
    
    Original change's description:
    > Scalable Notifiers: record print job creation
    >
    > When scalable IPH service is enabled for a browser context, record any
    > print job creation events. This will be used for contextual suppression
    > of related IPH notifications, etc.
    >
    > Bug: b/295370257, b/301006258
    > Change-Id: I7e023e394f40dd5aeb0d3ac72fdc592faa7f5610
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4863513
    > Reviewed-by: Piotr Pawliczek <pawliczek@chromium.org>
    > Reviewed-by: Yuki Awano <yawano@google.com>
    > Commit-Queue: Angela Xiao <angelaxiao@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1201192}
    
    Bug: b/295370257, b/301006258
    Change-Id: I8033c0a5322451b5833212435ffaed591dcaa7dc
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4891298
    Auto-Submit: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Rakina Zata Amni <rakina@google.com>
    Cr-Commit-Position: refs/heads/main@{#1201370}

--- chromeos/ash/components/scalable_iph/scalable_iph.cc
+++ chromeos/ash/components/scalable_iph/scalable_iph.cc
@@ -73,7 +73,6 @@ const base::flat_map<ScalableIph::Event, std::string>& GetEventNamesMap() {
            kEventNameShelfItemActivationGooglePhotosAndroid},
           {ScalableIph::Event::kShelfItemActivationGooglePlay,
            kEventNameShelfItemActivationGooglePlay},
-          {ScalableIph::Event::kPrintJobCreated, kEventNamePrintJobCreated},
       });
   return *event_names_map;
 }
@@ -1009,8 +1008,6 @@ std::ostream& operator<<(std::ostream& out, ScalableIph::Event event) {
       return out << "ShelfItemActivationGooglePhotosAndroid";
     case ScalableIph::Event::kShelfItemActivationGooglePlay:
       return out << "ShelfItemActivationGooglePlay";
-    case ScalableIph::Event::kPrintJobCreated:
-      return out << "PrintJobCreated";
   }
 }
 
--- chromeos/ash/components/scalable_iph/scalable_iph.h
+++ chromeos/ash/components/scalable_iph/scalable_iph.h
@@ -94,7 +94,6 @@ class ScalableIph : public KeyedService,
     kShelfItemActivationGooglePhotosWeb,
     kShelfItemActivationGooglePhotosAndroid,
     kShelfItemActivationGooglePlay,
-    kPrintJobCreated,
   };
 
   ScalableIph(feature_engagement::Tracker* tracker,
