2ddeadde7d01a27be7807d842fa893967c25af4b
Luciano Pacheco <lucmult@chromium.org>
2023-09-26 01:54:30 +0000

    Files app: Convert app_window_wrapper to TS
    Bug: b:289003444
    Change-Id: Iac85560a7bb1ddc88dbf8eee1d256615dc3c50bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4891274
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201312}

--- ui/file_manager/file_manager/background/js/launcher.ts
+++ ui/file_manager/file_manager/background/js/launcher.ts
@@ -44,7 +44,8 @@ export async function launchFileManager(appState?: FilesAppState):
 
   await initializationPromise;
 
-  const appWindow = new AppWindowWrapper('main.html');
+  const appWindow = new AppWindowWrapper();
 
-  await appWindow.launch(appState || {});
+  // TODO: Remove `as FileAppsState` this type is an TS interface.
+  await appWindow.launch(appState || {} as FilesAppState);
 }
