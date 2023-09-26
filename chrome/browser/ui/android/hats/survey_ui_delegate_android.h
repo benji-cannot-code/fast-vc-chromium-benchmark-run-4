492f80d8cf1b2436f45fd35169a1a7ee36e1ef0d
Wenyu Fu <wenyufu@google.com>
2023-09-26 23:52:42 +0000

    [Survey] Add SurveyClientBridge
    Add the bridge class to create the SurveyClient in C++
    
    Bug: 1466724, 1400731
    Change-Id: Id0a421de96e8b79db65021a08b59e1f6d911280e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4888911
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1201883}

--- chrome/browser/ui/android/hats/survey_ui_delegate_android.h
+++ chrome/browser/ui/android/hats/survey_ui_delegate_android.h
@@ -23,6 +23,8 @@ class WindowAndroid;
 
 namespace hats {
 
+class SurveyClientAndroid;
+
 // Interface used to display Survey to the Android UI. C++ clients can extend
 // this class, so the C++ instance will receive the corresponding method calls
 // from Java.
@@ -60,7 +62,7 @@ class SurveyUiDelegateAndroid {
   virtual void Dismiss(JNIEnv* env);
 
  private:
-  // friend class SurveyClientAndroid;
+  friend class SurveyClientAndroid;
   const JavaRef<jobject>& GetJavaObject(JNIEnv* env) const;
 
   base::android::ScopedJavaGlobalRef<jobject> jobj_;
