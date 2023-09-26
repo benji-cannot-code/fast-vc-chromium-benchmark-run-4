498abb93465ea3fc167311c4cec02493d327abf1
Matthew DeVore <matvore@chromium.org>
2023-09-26 21:18:40 +0000

    arc/tracing: observe Commit, not TRACE Attach
    Simplify processing of trace results by using a surface observer on
    exo::Surface::Commit rather than a TRACE for exo:surface::Attach. This
    same event can also be used to monitor commit events for implementation
    of the perceived FPS metric.
    
    The purpose of using Attach earlier was to determine the buffer ID
    associated with each frame, which was used to show detailed timelapse
    charts. This is no longer in scope for arc-overview-tracing.
    
    Bug: b/300940761
    Bug: b/296595454
    Test: ArcGraphicsTracingHandlerTest
    Test: ArcTracingGraphicsModelTest
    Test: ArcTracingModelTest
    Test: trace with arc-overview-tracing and verify the 4 App/ChromeOS FPS/Commit Time graphs are still populated
    Test: trace with arc-power-control
    Change-Id: Ibc393c867a39f58360039ee35809f73a51ae2c1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4876018
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Matthew DeVore <matvore@chromium.org>
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Auto-Submit: Matthew DeVore <matvore@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201811}

--- chrome/browser/ash/arc/tracing/arc_tracing_graphics_model.h
+++ chrome/browser/ash/arc/tracing/arc_tracing_graphics_model.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_ARC_TRACING_ARC_TRACING_GRAPHICS_MODEL_H_
 #define CHROME_BROWSER_ASH_ARC_TRACING_ARC_TRACING_GRAPHICS_MODEL_H_
 
+#include <deque>
 #include <map>
 #include <memory>
 #include <set>
@@ -19,6 +20,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace arc {
 
 class ArcTracingModel;
+
+// Timestamps in tick counts accumulated over the course of a trace. Backed by a
+// deque to give O(1) non-amortized insertion time.
+struct TraceTimestamps {
+  TraceTimestamps();
+  ~TraceTimestamps();
+
+  // Prevent accidental copying.
+  TraceTimestamps(const TraceTimestamps&) = delete;
+  TraceTimestamps& operator=(const TraceTimestamps&) = delete;
+
+  void Add(base::TimeTicks timestamp);
+
+  std::deque<int64_t> ticks_ms;
+};
 
 // Graphic buffers events model. It is build from the generic |ArcTracingModel|
 // and contains only events that describe life-cycle of graphics buffers across
@@ -58,13 +74,13 @@ class ArcTracingGraphicsModel {
     kBufferFillJank,                 // 106,
 
     // Wayland exo events
-    kExoSurfaceAttach   = 200,
+    kExoSurfaceAttach   = 200,  // Obsolete
     kExoProduceResource = 201,  // Obsolete
     kExoBound           = 202,  // Obsolete
     kExoPendingQuery    = 203,  // Obsolete
     kExoReleased        = 204,  // Obsolete
     kExoJank            = 205,
-    kExoSurfaceCommit   = 206,  // Obsolete
+    kExoSurfaceCommit   = 206,
 
     // Chrome events
     kChromeBarrierOrder = 300,  // Obsolete
@@ -169,7 +185,8 @@ class ArcTracingGraphicsModel {
       const std::set<ArcTracingGraphicsModel::BufferEventType>& start_types);
 
   // Builds the model from the common tracing model |common_model|.
-  bool Build(const ArcTracingModel& common_model);
+  bool Build(const ArcTracingModel& common_model,
+             const TraceTimestamps& commits);
 
   // Serializes the model to |base::Value::Dict|, this can be passed to
   // javascript for rendering.
