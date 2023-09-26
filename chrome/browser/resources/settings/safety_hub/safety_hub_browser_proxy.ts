45ddafb43bb1106dae23d8acc21dfdbcea2a1adb
Eduard Hez <ehez@chromium.org>
2023-09-26 16:11:21 +0000

    [SHv2] Add frontend logic for entry point
    With this CL, the Safety Hub entry point will distinguish between when
    there are new recommendations and when there are no recommendations.
    This changes the strings, button color, and icons.
    
    As a follow-up the backend needs to provide the required data, i.e.
    which modules have recommendations.
    
    Bug: 1443466
    
    Change-Id: If3d63fbd8f5493ffd915f9947569c525b010ebe0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4886976
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Auto-Submit: Eduard Hez <ehez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201625}

--- chrome/browser/resources/settings/safety_hub/safety_hub_browser_proxy.ts
+++ chrome/browser/resources/settings/safety_hub/safety_hub_browser_proxy.ts
@@ -127,6 +127,12 @@ export interface SafetyHubBrowserProxy {
 
   /** Get the number of extensions that should be reviewed by the user. */
   getNumberOfExtensionsThatNeedReview(): Promise<number>;
+
+  /** Returns true if Safety Hub has recommendations for the user. */
+  getSafetyHubHasRecommendations(): Promise<boolean>;
+
+  /** Get the subheader for Safety Hub entry point in settings. */
+  getSafetyHubEntryPointSubheader(): Promise<string>;
 }
 
 export class SafetyHubBrowserProxyImpl implements SafetyHubBrowserProxy {
@@ -193,6 +199,14 @@ export class SafetyHubBrowserProxyImpl implements SafetyHubBrowserProxy {
 
   getNumberOfExtensionsThatNeedReview() {
     return sendWithPromise('getNumberOfExtensionsThatNeedReview');
+  }
+
+  getSafetyHubHasRecommendations() {
+    return sendWithPromise('getSafetyHubHasRecommendations');
+  }
+
+  getSafetyHubEntryPointSubheader() {
+    return sendWithPromise('getSafetyHubEntryPointSubheader');
   }
 
   static getInstance(): SafetyHubBrowserProxy {
