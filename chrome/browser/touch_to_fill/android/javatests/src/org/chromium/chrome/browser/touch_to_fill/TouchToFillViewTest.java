a0c51ec071a1390268cb2e3beaea2df866ac6053
Adem Derinel <derinel@google.com>
2023-09-27 08:11:56 +0000

    [Android][TTF] Unify credential layouts into one
    Bug: 1474805
    Change-Id: I7a59c93cd6b4ca65320a9976f925921d054f9ceb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4894114
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1201943}

--- chrome/browser/touch_to_fill/android/javatests/src/org/chromium/chrome/browser/touch_to_fill/TouchToFillViewTest.java
+++ chrome/browser/touch_to_fill/android/javatests/src/org/chromium/chrome/browser/touch_to_fill/TouchToFillViewTest.java
@@ -650,7 +650,7 @@ public class TouchToFillViewTest {
     }
 
     private TextView getCredentialPasswordAt(int index) {
-        return getCredentials().getChildAt(index).findViewById(R.id.password);
+        return getCredentials().getChildAt(index).findViewById(R.id.password_or_context);
     }
 
     private TextView getCredentialOriginAt(int index) {
