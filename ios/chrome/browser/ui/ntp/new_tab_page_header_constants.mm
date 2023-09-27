869df8e47d8a85f02b4ee178cf2fee3d40198570
Scott Yoder <scottyoder@google.com>
2023-09-27 12:27:53 +0000

    [ios] Refactor Fakebox animation calculations
    The code that does these calculations is somewhat hard to understand and
    this is an attempt to make it a little easier by introducing an
    interpolate method that calculates a value / dimension when given the
    starting value, the ending value, and a percent. An example usage would
    be to calculate the height of the fakebox given that it is wide at the
    start and narrower at the end, and given a percentage of progress
    through the animation.  Most of the animation is driven by similar
    changes to dimensions.
    
    Along the way, a few miscalculations were identified and fixed:
     * the top margin of the pinned fakebox was off by one
     * the height of the pinned fake toolbar assumed the height of the
       separator was 1 pt, when in reality it was one pixel (no matter the
       pixel density)
    
    Bug: 1482304
    Change-Id: I65352f7d9eebd24991daf8cec2499cf8de481189
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4886484
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1201963}

--- ios/chrome/browser/ui/ntp/new_tab_page_header_constants.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_header_constants.mm
@@ -8,11 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ntp_header {
 
 const CGFloat kAnimationDistance = 42;
-const CGFloat kFakeLocationBarTopConstraint = 4;
+const CGFloat kFakeLocationBarTopConstraint = 5;
 const CGFloat kScrolledToTopOmniboxBottomMargin = 4;
 const CGFloat kCenteredHintLabelSidePadding = 37;
 const CGFloat kHintLabelHeightMargin = 2;
-const CGFloat kMaxTopMarginDiff = 4;
 const CGFloat kFakeOmniboxScrolledToTopMargin = 14;
 
 }  // ntp_header
