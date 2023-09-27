ec8882014a2bc2731794a36e3dc3a65160ec8b3a
Kartar Singh <kartarsingh@google.com>
2023-09-27 13:36:10 +0000

    Enable libunwindstack unwinder by default for tracing.
    This feature was previously disabled by default due to unknown crashes
    occurring in field. This change makes libunwindstack unwinder the
    default unwinder while tracing, after having fixed the crashes and
    verified through the experiments that the crashes are resolved.
    
    Bug: b/269239545
    Change-Id: I89b66bf985f08f06ed225953eeb7d24de80f60f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4890226
    Commit-Queue: Kartar Singh <kartarsingh@google.com>
    Reviewed-by: Tushar Agarwal <agarwaltushar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1201970}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -1163,7 +1163,7 @@ BASE_FEATURE(kUpdateNotificationScheduleServiceImmediateShowOption,
 // only on Android.
 BASE_FEATURE(kUseLibunwindstackNativeUnwinderAndroid,
              "UseLibunwindstackNativeUnwinderAndroid",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kUserMediaScreenCapturing,
              "UserMediaScreenCapturing",
