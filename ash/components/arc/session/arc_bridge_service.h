b648acb5df2b884c9c34d0b8f9e6f1421b3bcafb
Rin Shimokawa <rinrin@google.com>
2023-09-27 18:14:14 +0000

    arc: Introduce ChromeFeatureFlagsHost
    This CL allows the ARC side sends a request and the Chrome side returns
    status or parameter by return value.
    
    DD:go/arc-feature-bridge-dd Option 4
    
    BUG=b:299391995
    
    Change-Id: I1959a7153979a0121bce3053243d2d122395ef8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4853174
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Rin Shimokawa <rinrin@google.com>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Shengsong Tan <sstan@chromium.org>
    Reviewed-by: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202022}

--- ash/components/arc/session/arc_bridge_service.h
+++ ash/components/arc/session/arc_bridge_service.h
@@ -41,6 +41,7 @@ class BootPhaseMonitorHost;
 class BootPhaseMonitorInstance;
 class CameraHost;
 class CameraInstance;
+class ChromeFeatureFlagsHost;
 class ChromeFeatureFlagsInstance;
 class CastReceiverInstance;
 class ClipboardHost;
@@ -200,7 +201,9 @@ class ArcBridgeService {
   ConnectionHolder<mojom::CastReceiverInstance>* cast_receiver() {
     return &cast_receiver_;
   }
-  ConnectionHolder<mojom::ChromeFeatureFlagsInstance>* chrome_feature_flags() {
+  ConnectionHolder<mojom::ChromeFeatureFlagsInstance,
+                   mojom::ChromeFeatureFlagsHost>*
+  chrome_feature_flags() {
     return &chrome_feature_flags_;
   }
   ConnectionHolder<mojom::ClipboardInstance, mojom::ClipboardHost>*
@@ -368,7 +371,9 @@ class ArcBridgeService {
       boot_phase_monitor_;
   ConnectionHolder<mojom::CameraInstance, mojom::CameraHost> camera_;
   ConnectionHolder<mojom::CastReceiverInstance> cast_receiver_;
-  ConnectionHolder<mojom::ChromeFeatureFlagsInstance> chrome_feature_flags_;
+  ConnectionHolder<mojom::ChromeFeatureFlagsInstance,
+                   mojom::ChromeFeatureFlagsHost>
+      chrome_feature_flags_;
   ConnectionHolder<mojom::ClipboardInstance, mojom::ClipboardHost> clipboard_;
   ConnectionHolder<mojom::CompatibilityModeInstance> compatibility_mode_;
   ConnectionHolder<mojom::CrashCollectorInstance, mojom::CrashCollectorHost>
--- ash/components/arc/test/fake_chrome_feature_flags_instance.h
+++ ash/components/arc/test/fake_chrome_feature_flags_instance.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/components/arc/mojom/chrome_feature_flags.mojom.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
+#include "mojo/public/cpp/bindings/remote.h"
 
 namespace arc {
 
@@ -23,6 +24,9 @@ class FakeChromeFeatureFlagsInstance
 
   ~FakeChromeFeatureFlagsInstance() override;
 
+  void Init(mojo::PendingRemote<mojom::ChromeFeatureFlagsHost> host_remote,
+            InitCallback callback) override;
+
   const mojom::FeatureFlagsPtr& flags_called_value() {
     return flags_called_value_.value();
   }
@@ -32,6 +36,7 @@ class FakeChromeFeatureFlagsInstance
 
  private:
   absl::optional<mojom::FeatureFlagsPtr> flags_called_value_;
+  mojo::Remote<mojom::ChromeFeatureFlagsHost> host_remote_;
 };
 
 }  // namespace arc
