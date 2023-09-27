c313b93b04e5b2b9bcd1253a83aaf40cba16945f
Rebekah Potter <rbpotter@chromium.org>
2023-09-27 15:18:34 +0000

    Profile Window: Remove unused test base class and JS
    There are no tests actually using this base class (i.e. no TEST_F uses)
    and it is inheriting from WebUIBrowserTest which is deprecated in
    favor of WebUIMochaBrowserTest. The corresponding JS is therefore not
    ever run.
    
    Deleting this unused class and JS file. If WebUI tests are desired in
    future for this code, they should be implemented using
    WebUIMochaBrowserTest and TypeScript.
    
    Bug: 1457360
    Change-Id: I4a950e5c06cb7c0e2657d5734691aee0bd3574bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4895369
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201987}

--- chrome/browser/profiles/profile_window_browsertest.cc
+++ chrome/browser/profiles/profile_window_browsertest.cc
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/search_test_utils.h"
 #include "chrome/test/base/ui_test_utils.h"
-#include "chrome/test/base/web_ui_browser_test.h"
 #include "components/account_id/account_id.h"
 #include "components/history/core/browser/history_db_task.h"
 #include "components/history/core/browser/history_service.h"
@@ -364,21 +363,3 @@ IN_PROC_BROWSER_TEST_F(ProfileWindowBrowserTest,
   EXPECT_EQ(num_browsers, BrowserList::GetInstance()->size());
   EXPECT_TRUE(ProfilePicker::IsOpen());
 }
-
-class ProfileWindowWebUIBrowserTest : public WebUIBrowserTest {
- public:
-  void OnSystemProfileCreated(std::string* url_to_test,
-                              base::OnceClosure quit_loop,
-                              Profile* profile,
-                              const std::string& url) {
-    *url_to_test = url;
-    std::move(quit_loop).Run();
-  }
-
- private:
-  void SetUpOnMainThread() override {
-    WebUIBrowserTest::SetUpOnMainThread();
-    AddLibrary(
-        base::FilePath(FILE_PATH_LITERAL("profile_window_browsertest.js")));
-  }
-};
