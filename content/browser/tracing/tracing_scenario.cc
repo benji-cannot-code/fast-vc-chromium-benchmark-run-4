d7665dc7230486f237228fc4f15c5a350689e910
Etienne Pierre-doray <etiennep@chromium.org>
2023-09-27 12:40:51 +0000

    [tracing] Implement GenerateMetadataProto for new tracing scenarios
    Change-Id: I6d2002077ddb6cdc8edb572001efae6d6bd0cb89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4873921
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1201965}

--- content/browser/tracing/tracing_scenario.cc
+++ content/browser/tracing/tracing_scenario.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/token.h"
 #include "base/tracing/trace_time.h"
+#include "components/variations/hashing.h"
 #include "content/browser/tracing/background_tracing_manager_impl.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
@@ -129,6 +130,20 @@ void TracingScenario::Abort() {
   }
   SetState(State::kStopping);
   tracing_session_->Stop();
+}
+
+void TracingScenario::GenerateMetadataProto(
+    perfetto::protos::pbzero::ChromeMetadataPacket* metadata) {
+  auto* background_tracing_metadata =
+      metadata->set_background_tracing_metadata();
+
+  uint32_t scenario_name_hash = variations::HashName(scenario_name());
+  background_tracing_metadata->set_scenario_name_hash(scenario_name_hash);
+
+  if (triggered_rule_) {
+    auto* triggered_rule = background_tracing_metadata->set_triggered_rule();
+    triggered_rule_->GenerateMetadataProto(triggered_rule);
+  }
 }
 
 std::unique_ptr<perfetto::TracingSession>
