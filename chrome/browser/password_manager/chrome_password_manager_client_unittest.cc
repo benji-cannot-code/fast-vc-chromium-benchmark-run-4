723889b2163d0873f74eeeedbdb7ed17a4f5e5b7
Ivana Žužić <izuzic@google.com>
2023-09-27 08:44:29 +0000

    [UPMCleanup] Remove kUnifiedPasswordManagerAndroid from unittests
    Removing the flag that is enabled by default from the unittests.
    
    Bug: 1439191
    Change-Id: I2c6779b97fc8abb87b5fc42f5ace8f260ab5159f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4894423
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1201945}

--- chrome/browser/password_manager/chrome_password_manager_client_unittest.cc
+++ chrome/browser/password_manager/chrome_password_manager_client_unittest.cc
@@ -56,8 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_manager.h"
 #include "components/password_manager/core/browser/password_manager_test_utils.h"
 #include "components/password_manager/core/browser/password_store_consumer.h"
-#include "components/password_manager/core/common/password_manager_feature_variations_android.h"
-#include "components/password_manager/core/common/password_manager_features.h"
 #include "components/safe_browsing/buildflags.h"
 #include "components/safe_browsing/core/common/features.h"
 #include "components/sessions/content/content_record_password_state.h"
@@ -1081,17 +1079,6 @@ TEST_F(ChromePasswordManagerClientTest, MissingUIDelegate) {
   client->HideManualFallbackForSaving();
 }
 
-TEST_F(ChromePasswordManagerClientTest,
-       RefreshPasswordManagerSettingsIfNeededUPMDisabled) {
-#if BUILDFLAG(IS_ANDROID)
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndDisableFeature(
-      password_manager::features::kUnifiedPasswordManagerAndroid);
-#endif
-  EXPECT_CALL(settings_service(), RequestSettingsFromBackend).Times(0);
-  GetClient()->RefreshPasswordManagerSettingsIfNeeded();
-}
-
 #if BUILDFLAG(IS_ANDROID)
 class ChromePasswordManagerClientAndroidTest
     : public ChromePasswordManagerClientTest {
@@ -1469,13 +1456,10 @@ TEST_F(ChromePasswordManagerClientAndroidTest,
 
 TEST_F(ChromePasswordManagerClientAndroidTest,
        RefreshPasswordManagerSettingsIfNeededUPMFeatureEnabled) {
-  base::test::ScopedFeatureList feature_list;
   const std::map<std::string, std::string> params = {
       {"stage", base::NumberToString(static_cast<int>(
                     password_manager::features::UpmExperimentVariation::
                         kEnableForSyncingUsers))}};
-  feature_list.InitAndEnableFeatureWithParameters(
-      password_manager::features::kUnifiedPasswordManagerAndroid, params);
 
   EXPECT_CALL(settings_service(), RequestSettingsFromBackend);
   GetClient()->RefreshPasswordManagerSettingsIfNeeded();
