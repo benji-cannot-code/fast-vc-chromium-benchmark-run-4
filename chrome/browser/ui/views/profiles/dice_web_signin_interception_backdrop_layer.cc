a84416584acd5ae4374509910cab1413c7ec9853
Claudio DeSouza <cdesouza@chromium.org>
2023-09-27 12:14:04 +0000

    Fix inclusion path for profile_menu_view.h
    For some reason the path provide is a relative one, which is atypical
    for most of chromium.
    
    Bug: b/299592559
    Change-Id: I31b833837f84cf2a396e274301ebb23d554f94ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4894236
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Claudio DeSouza <cdesouza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201958}

--- chrome/browser/ui/views/profiles/dice_web_signin_interception_backdrop_layer.cc
+++ chrome/browser/ui/views/profiles/dice_web_signin_interception_backdrop_layer.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/browser_frame.h"
 #include "chrome/browser/ui/views/frame/browser_non_client_frame_view.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
-#include "profile_menu_view.h"
+#include "chrome/browser/ui/views/profiles/profile_menu_view.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/ui_base_features.h"
 #include "ui/compositor/layer_type.h"
