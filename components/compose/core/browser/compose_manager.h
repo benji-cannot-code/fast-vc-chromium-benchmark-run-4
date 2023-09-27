3bf1a5dac899e6ad507df1f91d1f7ef382da4ba5
Pete Williamson <petewil@chromium.org>
2023-09-27 23:53:09 +0000

    Add owners file and base structure for compose
    Why components and not chrome?
    1) We are planning to expand from Desktop to iOS, Android, and
    other languages after shipping our MVP early next year.
    2) The project is strongly tied to Autofill, which uses a mixed components
    and chrome architecture, so we are mirroring that.
    
    Bug: b:302001428
    Change-Id: Ie3903c9ebb597e078c74976e243d7374e2b633fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4893224
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Auto-Submit: Peter Williamson <petewil@chromium.org>
    Commit-Queue: Peter Williamson <petewil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202130}

---
+++ components/compose/core/browser/compose_manager.h
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_COMPOSE_CORE_BROWSER_COMPOSE_MANAGER_H_
+#define COMPONENTS_COMPOSE_CORE_BROWSER_COMPOSE_MANAGER_H_
+
+#include <string>
+
+#include "base/functional/callback.h"
+
+namespace compose {
+
+// The interface for embedder-independent, tab-specific compose logic.
+class ComposeManager {
+ public:
+  using ComposeCallback = base::OnceCallback<void(const std::u16string&)>;
+
+  virtual ~ComposeManager() = default;
+
+  virtual bool IsEnabled() const = 0;
+  virtual void OfferCompose(ComposeCallback callback) = 0;
+};
+
+}  // namespace compose
+
+#endif  // COMPONENTS_COMPOSE_CORE_BROWSER_COMPOSE_MANAGER_H_
