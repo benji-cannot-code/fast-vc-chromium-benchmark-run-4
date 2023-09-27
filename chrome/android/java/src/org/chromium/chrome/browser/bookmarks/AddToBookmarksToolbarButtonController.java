980035702d57aa41c596e10800d762ea3cc9df2f
Zhe Li <zheliooo@google.com>
2023-09-27 23:07:27 +0000

    [APS] Apply circular hover highlight to more optional buttons
     - Read aloud
     - Bookmark screenshot: https://screenshot.googleplex.com/AapmLpK8nePJjQY
     - Translate screenshot: https://screenshot.googleplex.com/7UYF4qjcezeBA7L
    
    Bug: 1485299
    Change-Id: Id3d951f55296aac307ef989f7058c3a80cd8cc5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4880179
    Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1202116}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/AddToBookmarksToolbarButtonController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/AddToBookmarksToolbarButtonController.java
@@ -91,7 +91,7 @@ public class AddToBookmarksToolbarButtonController
                 /* actionChipLabelResId = */ Resources.ID_NULL, /* supportsTinting = */ true,
                 /* iphCommandBuilder = */ null, AdaptiveToolbarButtonVariant.ADD_TO_BOOKMARKS,
                 /* tooltipTextResId = */ Resources.ID_NULL,
-                /* showHoverHighlight = */ false);
+                /* showHoverHighlight = */ true);
         mActivityLifecycleDispatcher = activityLifecycleDispatcher;
         mTabBookmarkerSupplier = tabBookmarkerSupplier;
         mTrackerSupplier = trackerSupplier;
@@ -115,7 +115,7 @@ public class AddToBookmarksToolbarButtonController
                 /* iphCommandBuilder= */ null, AdaptiveToolbarButtonVariant.ADD_TO_BOOKMARKS,
                 /* actionChipLabelResId = */ Resources.ID_NULL,
                 /* tooltipTextResId = */ Resources.ID_NULL,
-                /* showHoverHighlight = */ false);
+                /* showHoverHighlight = */ true);
 
         mIsTablet = DeviceFormFactor.isNonMultiDisplayContextOnTablet(mContext);
     }
