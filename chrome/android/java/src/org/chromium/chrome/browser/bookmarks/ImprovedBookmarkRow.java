9badbaa3e4cbaa92c1a552a5ab2022846b2c2f1a
Brandon Wylie <wylieb@chromium.org>
2023-09-27 00:28:24 +0000

    [bookmarks] Add fade-in effect for start image drawable changes
    Adding animation for when a drawable is set. Cross-fading will never
    happen because we intentionally null out the image when the row
    is re-bound to prevent showing a stale image while the new image is
    fetched.
    
    Bug: 1482883
    Change-Id: If99bb2943b266d584d7a2bf43f713eb863e1f5bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4888985
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1201905}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/ImprovedBookmarkRow.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/ImprovedBookmarkRow.java
@@ -12,12 +12,14 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewPropertyAnimator;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.ColorInt;
 import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 
 import org.chromium.chrome.R;
 import org.chromium.components.bookmarks.BookmarkId;
@@ -34,9 +36,17 @@ import org.chromium.components.browser_ui.widget.selectable_list.SelectableListU
 // TODO(crbug.com/): Make selection delegate optional for this class and remove
 // SelectableItemViewBase inheritance. It's no longer needed.
 public class ImprovedBookmarkRow extends SelectableItemViewBase<BookmarkId> {
+    /**
+     * The base duration of the settling animation of the sheet. 218 ms is a spec for material
+     * design (this is the minimum time a user is guaranteed to pay attention to something).
+     */
+    @VisibleForTesting
+    static final int BASE_ANIMATION_DURATION_MS = 218;
+
     private ViewGroup mContainer;
     // The start image view which is shows the favicon.
     private ImageView mStartImageView;
+    private View mStartImageContainer;
     private ImprovedBookmarkFolderView mFolderIconView;
     // Displays the title of the bookmark.
     private TextView mTitleView;
@@ -97,6 +107,7 @@ public class ImprovedBookmarkRow extends SelectableItemViewBase<BookmarkId> {
         mContainer = findViewById(R.id.container);
 
         mStartImageView = findViewById(R.id.start_image);
+        mStartImageContainer = findViewById(R.id.start_image_container);
         mFolderIconView = findViewById(R.id.folder_view);
 
         mTitleView = findViewById(R.id.title);
@@ -124,15 +135,25 @@ public class ImprovedBookmarkRow extends SelectableItemViewBase<BookmarkId> {
     }
 
     void setStartImageVisible(boolean visible) {
-        mStartImageView.setVisibility(visible ? View.VISIBLE : View.GONE);
+        mStartImageContainer.setVisibility(visible ? View.VISIBLE : View.GONE);
     }
 
     void setFolderViewVisible(boolean visible) {
         mFolderIconView.setVisibility(visible ? View.VISIBLE : View.GONE);
     }
 
-    void setStartIconDrawable(Drawable drawable) {
+    void setStartIconDrawable(@Nullable Drawable drawable) {
         mStartImageView.setImageDrawable(drawable);
+        // No need to fade-in a null drawable.
+        if (drawable == null) return;
+
+        mStartImageView.setAlpha(0f);
+
+        // Using a local variable to facilitate testing.
+        ViewPropertyAnimator anim = mStartImageView.animate();
+        anim.setDuration(BASE_ANIMATION_DURATION_MS);
+        anim.alpha(1f);
+        anim.start();
     }
 
     void setStartIconTint(ColorStateList tint) {
@@ -231,5 +252,9 @@ public class ImprovedBookmarkRow extends SelectableItemViewBase<BookmarkId> {
 
     ImprovedBookmarkFolderView getFolderView() {
         return mFolderIconView;
+    }
+
+    void setStartImageViewForTesting(ImageView startImageView) {
+        mStartImageView = startImageView;
     }
 }
