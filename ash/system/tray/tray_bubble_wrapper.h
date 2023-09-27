5ad3f94d4f317509afcbf2902b464ea779490d16
Andre Le <leandre@chromium.org>
2023-09-27 22:20:05 +0000

    Bubble Refactor: Move event handling to TrayEventFilter
    In the effort to remove TrayBubbleWrapper, we will move the event
    handling code from TrayBubbleWrapper to TrayEventFilter. This makes
    sense since TrayEventFilter is already doing a related thing. Also added
    more test coverage around this area to make sure that the refactor is
    clean.
    
    More details about this effort can be found in
    go/cros-tray-bubble-refactor.
    
    Bug: b:259734217
    Change-Id: Ie8f14e5a8e9122f2cd4b23b1dde96a4ca8c8185f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4771136
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202109}

--- ash/system/tray/tray_bubble_wrapper.h
+++ ash/system/tray/tray_bubble_wrapper.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/tray_bubble_base.h"
 #include "base/memory/raw_ptr.h"
 #include "ui/views/widget/widget_observer.h"
-#include "ui/wm/public/activation_change_observer.h"
 
 namespace ash {
 
@@ -18,9 +17,8 @@ class TrayBackgroundView;
 class TrayBubbleView;
 class TrayEventFilter;
 
-// Creates and manages the Widget and EventFilter components of a bubble.
-class ASH_EXPORT TrayBubbleWrapper : public TrayBubbleBase,
-                                     public ::wm::ActivationChangeObserver {
+// Creates and manages the Widget components of a bubble.
+class ASH_EXPORT TrayBubbleWrapper : public TrayBubbleBase {
  public:
   // `event_handling` When set to false disables the tray's event filtering
   // and also ignores the activation events. Eche window is an example of a use
@@ -44,11 +42,6 @@ class ASH_EXPORT TrayBubbleWrapper : public TrayBubbleBase,
 
   // views::WidgetObserver overrides:
   void OnWidgetDestroying(views::Widget* widget) override;
-
-  // ::wm::ActivationChangeObserver overrides:
-  void OnWindowActivated(ActivationReason reason,
-                         aura::Window* gained_active,
-                         aura::Window* lost_active) override;
 
   TrayBackgroundView* tray() { return tray_; }
   TrayBubbleView* bubble_view() { return bubble_view_; }
