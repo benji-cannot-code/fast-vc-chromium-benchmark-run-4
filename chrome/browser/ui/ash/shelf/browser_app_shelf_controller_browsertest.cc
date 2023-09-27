a6239c20a3e339547ce47e3f196609e95318095a
Jenny Zhang <jennyz@chromium.org>
2023-09-27 16:22:07 +0000

    Lacros: Improve browser_tests_require_lacros tests.
    This cl optimizes the browser_tests_require_lacros by disabling
    gpu sandbox in Lacros (gpu sandbox can't be started successfully
    in linux emulator), and not opening ash web browser(ash web browser
    is disbled in Lacros only mode), to speed up the tests.
    Two of BrowserAppShelfControllerBrowserTest are disabled
    due to an issue exposed by this cl.
    
    Bug: 1483530,1482949,1484723
    Change-Id: I2e2b4ffc37cb06ab3558fcc18a52b02d60c461bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4893997
    Reviewed-by: Sven Zheng <svenzheng@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201995}

--- chrome/browser/ui/ash/shelf/browser_app_shelf_controller_browsertest.cc
+++ chrome/browser/ui/ash/shelf/browser_app_shelf_controller_browsertest.cc
@@ -350,10 +350,8 @@ IN_PROC_BROWSER_TEST_F(BrowserAppShelfControllerBrowserTest, TabbedApps) {
     SCOPED_TRACE("initial state");
 
     // StartLacros opens one Ash and one Lacros window.
-    WAIT_FOR(registry_->IsAshBrowserRunning() &&
-             registry_->IsLacrosBrowserRunning());
+    WAIT_FOR(registry_->IsLacrosBrowserRunning());
     EXPECT_EQ(ShelfStatus(kLacrosAppId), ash::STATUS_RUNNING);
-    EXPECT_EQ(ShelfStatus(kChromeAppId), ash::STATUS_RUNNING);
   }
 
   const apps::BrowserWindowInstance* lacros =
@@ -464,10 +462,8 @@ IN_PROC_BROWSER_TEST_F(BrowserAppShelfControllerBrowserTest, WindowedApps) {
     SCOPED_TRACE("initial state");
 
     // StartLacros opens one Ash and one Lacros window.
-    WAIT_FOR(registry_->IsAshBrowserRunning() &&
-             registry_->IsLacrosBrowserRunning());
+    WAIT_FOR(registry_->IsLacrosBrowserRunning());
     EXPECT_EQ(ShelfStatus(kLacrosAppId), ash::STATUS_RUNNING);
-    EXPECT_EQ(ShelfStatus(kChromeAppId), ash::STATUS_RUNNING);
   }
 
   {
