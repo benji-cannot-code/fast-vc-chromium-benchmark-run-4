083159f9c9a778ca2df9e5710282e13402ff31a5
Anastasia Helfinstein <anastasi@google.com>
2023-09-28 22:19:04 +0000

    [automation] Add service worker case to NotifyTreeEventListenersChanged
    AX-Relnotes: n/a.
    Bug: b/260590502,1487002
    Change-Id: I4705fa49f3490258db46027cdb7f7793391b0f97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4857266
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202883}

--- extensions/renderer/api/automation/automation_internal_custom_bindings.h
+++ extensions/renderer/api/automation/automation_internal_custom_bindings.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/memory/weak_ptr.h"
+#include "base/task/single_thread_task_runner.h"
 #include "extensions/common/api/automation.h"
 #include "extensions/renderer/object_backed_native_handler.h"
 #include "ui/accessibility/ax_enums.mojom-shared.h"
@@ -38,6 +39,7 @@ class AutomationInternalCustomBindings : public ObjectBackedNativeHandler,
   AutomationInternalCustomBindings(
       ScriptContext* context,
       NativeExtensionBindingsSystem* bindings_system,
+      scoped_refptr<base::SingleThreadTaskRunner> io_task_runner,
       int worker_thread_id);
 
   AutomationInternalCustomBindings(const AutomationInternalCustomBindings&) =
@@ -96,6 +98,7 @@ class AutomationInternalCustomBindings : public ObjectBackedNativeHandler,
 
   std::unique_ptr<ui::AutomationV8Bindings> automation_v8_bindings_;
 
+  scoped_refptr<base::SingleThreadTaskRunner> io_task_runner_;
   const int worker_thread_id_;
 
   base::WeakPtrFactory<AutomationInternalCustomBindings> weak_ptr_factory_{
