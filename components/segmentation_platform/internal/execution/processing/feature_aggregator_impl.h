10e98328dc62f7cd46173b5ae6ee9a369782a031
Siddhartha S <ssid@chromium.org>
2023-09-28 06:07:58 +0000

    [segmentation] Batch process uma features
    Process all UMA features synchronously. Reduces a lot of thread hops for
    models with many features.
    
    Bug: b/299529800
    Change-Id: Ie1d1d78a6e0f2d0ebf60f9cfc2491f360283fcea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4875307
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Cr-Commit-Position: refs/heads/main@{#1202352}

--- components/segmentation_platform/internal/execution/processing/feature_aggregator_impl.h
+++ components/segmentation_platform/internal/execution/processing/feature_aggregator_impl.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/segmentation_platform/internal/database/signal_database.h"
 #include "components/segmentation_platform/internal/execution/processing/feature_aggregator.h"
 #include "components/segmentation_platform/public/proto/aggregation.pb.h"
-#include "components/segmentation_platform/public/proto/model_metadata.pb.h"
 #include "components/segmentation_platform/public/proto/types.pb.h"
 
 namespace segmentation_platform::processing {
@@ -31,14 +30,14 @@ class FeatureAggregatorImpl : public FeatureAggregator {
   // FeatureAggregator overrides.
   absl::optional<std::vector<float>> Process(
       proto::SignalType signal_type,
+      uint64_t name_hash,
       proto::Aggregation aggregation,
       uint64_t bucket_count,
+      const base::Time& start_time,
       const base::Time& end_time,
       const base::TimeDelta& bucket_duration,
-      const std::vector<SignalDatabase::Sample>& samples) const override;
-  void FilterEnumSamples(
       const std::vector<int32_t>& accepted_enum_ids,
-      std::vector<SignalDatabase::Sample>& samples) const override;
+      const std::vector<SignalDatabase::DbEntry>& all_samples) const override;
 };
 
 }  // namespace segmentation_platform::processing
