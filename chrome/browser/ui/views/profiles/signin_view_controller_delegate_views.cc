46cec1ffcc88d5ff29263f9be734f2b3a3dcb7e5
Gabriel Oliveira <gabolvr@google.com>
2023-09-28 12:00:04 +0000

    [Profiles] Delete ephemeral profile when escaping local profile creation
    Prior to this CL, when cancelling the profile customization dialog, for
    instance through the escape key, it would close the dialog.
    
    For the local profile creation case, the profile is ephemeral and not
    named yet. It shouldn't be possible to escape the profile customization
    in this step of the local profile creation.
    
    Cancelling the local profile creation shouldn then close all browser
    windows for the profile and thus delete it, since it's an ephemeral
    profile, similar to the "Delete profile" button on the dialog.
    
    Preview:
    https://screencast.googleplex.com/cast/NTA5NjIxNzc0MTYyMzI5NnxiZmY0MDBjZS01Mg
    
    Bug: 1485787
    Change-Id: I98e73cb09729db53e4394bf0c954f1562a1928c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4896636
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1202523}

--- chrome/browser/ui/views/profiles/signin_view_controller_delegate_views.cc
+++ chrome/browser/ui/views/profiles/signin_view_controller_delegate_views.cc
@@ -10,8 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/memory/weak_ptr.h"
 #include "build/build_config.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_avatar_icon_util.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/search_engine_choice/search_engine_choice_service.h"
 #include "chrome/browser/search_engine_choice/search_engine_choice_service_factory.h"
 #include "chrome/browser/signin/reauth_result.h"
@@ -21,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/color/chrome_color_id.h"
+#include "chrome/browser/ui/profiles/profile_picker.h"
 #include "chrome/browser/ui/search_engine_choice/search_engine_choice_tab_helper.h"
 #include "chrome/browser/ui/signin/signin_view_controller.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -290,7 +293,8 @@ SigninViewControllerDelegateViews::SigninViewControllerDelegateViews(
     Browser* browser,
     ui::ModalType dialog_modal_type,
     bool wait_for_size,
-    bool should_show_close_button)
+    bool should_show_close_button,
+    bool delete_profile_on_cancel)
     : content_view_(content_view.get()),
       web_contents_(content_view->GetWebContents()),
       browser_(browser),
@@ -322,6 +326,17 @@ SigninViewControllerDelegateViews::SigninViewControllerDelegateViews(
   animated_view->AddChildView(std::move(content_view));
 
   SetButtons(ui::DIALOG_BUTTON_NONE);
+
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
+    BUILDFLAG(IS_CHROMEOS_LACROS)
+  // On the local profile creation dialog, cancelling the dialog (for instance
+  // through the VKEY_ESCAPE accelerator) should delete the profile.
+  if (delete_profile_on_cancel) {
+    SetCancelCallback(base::BindOnce(
+        &SigninViewControllerDelegateViews::DeleteProfileOnCancel,
+        base::Unretained(this)));
+  }
+#endif
 
   web_contents_->SetDelegate(this);
 
@@ -402,6 +417,27 @@ void SigninViewControllerDelegateViews::DisplayModal() {
   content_view_->RequestFocus();
 }
 
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
+    BUILDFLAG(IS_CHROMEOS_LACROS)
+void SigninViewControllerDelegateViews::DeleteProfileOnCancel() {
+  ProfileAttributesEntry* entry =
+      g_browser_process->profile_manager()
+          ->GetProfileAttributesStorage()
+          .GetProfileAttributesWithPath(browser_->profile()->GetPath());
+  DCHECK(entry);
+  DCHECK(entry->IsEphemeral());
+  // Open the profile picker in the profile creation step again.
+  ProfilePicker::Show(ProfilePicker::Params::FromEntryPoint(
+      ProfilePicker::EntryPoint::kOpenNewWindowAfterProfileDeletion));
+  // Since the profile is ephemeral, closing all browser windows triggers the
+  // deletion.
+  BrowserList::CloseAllBrowsersWithProfile(browser_->profile(),
+                                           BrowserList::CloseCallback(),
+                                           BrowserList::CloseCallback(),
+                                           /*skip_beforeunload=*/true);
+}
+#endif
+
 BEGIN_METADATA(SigninViewControllerDelegateViews, views::DialogDelegateView)
 END_METADATA
 
@@ -450,7 +486,7 @@ SigninViewControllerDelegate::CreateProfileCustomizationDelegate(
   return new SigninViewControllerDelegateViews(
       SigninViewControllerDelegateViews::CreateProfileCustomizationWebView(
           browser, is_local_profile_creation, show_profile_switch_iph),
-      browser, ui::MODAL_TYPE_WINDOW, false, false);
+      browser, ui::MODAL_TYPE_WINDOW, false, false, is_local_profile_creation);
 }
 #endif  // BUILDFLAG(ENABLE_DICE_SUPPORT) || BUILDFLAG(IS_CHROMEOS_LACROS)
 
