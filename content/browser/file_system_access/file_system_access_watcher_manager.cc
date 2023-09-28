750f58b754bf84cf3cdb57cf0e7e28c19dc3854b
Austin Sullivan <asully@chromium.org>
2023-09-28 01:38:30 +0000

    FSA: Do not build FileSystemAccessLocalPathWatcher on iOS
    These changes should have been included in https://crrev.com/c/4893610
    
    Bug: 1019297
    Change-Id: I876631952d3b4a326e42b061a27b85fd2a9158f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4896246
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Christine Smith <christinesm@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202199}

--- content/browser/file_system_access/file_system_access_watcher_manager.cc
+++ content/browser/file_system_access/file_system_access_watcher_manager.cc
@@ -28,9 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/common/file_system/file_system_types.h"
 #include "third_party/blink/public/mojom/file_system_access/file_system_access_error.mojom-shared.h"
 
-#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_FUCHSIA)
+#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS) && !BUILDFLAG(IS_FUCHSIA)
 #include "content/browser/file_system_access/file_system_access_local_path_watcher.h"
-#endif  // !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_FUCHSIA)
+#endif  // !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS) &&
+        // !BUILDFLAG(IS_FUCHSIA)
 
 namespace content {
 
