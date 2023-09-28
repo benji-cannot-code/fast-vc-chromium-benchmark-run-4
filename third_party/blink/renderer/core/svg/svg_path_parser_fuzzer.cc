695d988dbf8a5aa1f3745b152522f7884e8e6fe8
Fredrik Söderquist <fs@opera.com>
2023-09-28 09:49:31 +0000

    Fuzzer for SVG path parsing
    Bug: 1485123
    Change-Id: I1e35fa5c24e9b60d0d4800dacf53cc9b33bd6e53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4882680
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1202459}

---
+++ third_party/blink/renderer/core/svg/svg_path_parser_fuzzer.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/svg/svg_path_parser.h"
+
+#include "third_party/blink/renderer/core/svg/svg_path_string_source.h"
+#include "third_party/blink/renderer/platform/testing/blink_fuzzer_test_support.h"
+#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  static blink::BlinkFuzzerTestSupport test_support;
+  String input_string = String::FromUTF8WithLatin1Fallback(data, size);
+  blink::SVGPathStringSource source(input_string);
+  class NullConsumer {
+   public:
+    void EmitSegment(const blink::PathSegmentData&) {}
+  } null_consumer;
+  blink::svg_path_parser::ParsePath(source, null_consumer);
+  return 0;
+}
