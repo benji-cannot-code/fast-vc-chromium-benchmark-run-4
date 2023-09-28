1886203f73493752ab8f2aba843be402461e3ddf
Michael Wojcicka <mwoj@google.com>
2023-09-28 23:07:31 +0000

    Remove alert color on badge while Battery Saver is active
    Change-Id: I236d58dab2e7c20b2241b85f9ff9beaf48cba957
    Bug: b:300089206
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4902984
    Commit-Queue: Michael Wojcicka <mwoj@google.com>
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202926}

--- ash/system/power/power_status.cc
+++ ash/system/power/power_status.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <cmath>
 
+#include "ash/constants/ash_features.h"
 #include "ash/public/cpp/power_utils.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/strings/grit/ash_strings.h"
@@ -293,7 +294,12 @@ PowerStatus::BatteryImageInfo PowerStatus::GenerateBatteryImageInfo(
 }
 
 void PowerStatus::CalculateBatteryImageInfo(BatteryImageInfo* info) const {
-  info->alert_if_low = !IsLinePowerConnected();
+  // We only alert if we are on battery, and battery saver mode is disabled.
+  if (features::IsBatterySaverAvailable()) {
+    info->alert_if_low = !IsLinePowerConnected() && !IsBatterySaverActive();
+  } else {
+    info->alert_if_low = !IsLinePowerConnected();
+  }
 
   if (!IsUsbChargerConnected() && !IsBatteryPresent()) {
     info->icon_badge = &kUnifiedMenuBatteryXIcon;
