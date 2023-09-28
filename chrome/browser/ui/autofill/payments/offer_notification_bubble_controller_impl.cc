9851ce487f7b931123e9f8b52630dd016a4abcac
Mei Liang <meiliang@chromium.org>
2023-09-28 02:16:03 +0000

    [Discounts] Bug bash 2 - Avoid icon expansion during tab switching
    This CL sets the icon expansion state false once
    controller#ShouldIconExpand is called. The expansion state will be reset
    in the next navigation, so the icon can be expanded again.
    
    Bug: 1480833
    Change-Id: Ic5e737d30ddb974f53f52c2fad6bb707401d440a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4886282
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202240}

--- chrome/browser/ui/autofill/payments/offer_notification_bubble_controller_impl.cc
+++ chrome/browser/ui/autofill/payments/offer_notification_bubble_controller_impl.cc
@@ -123,6 +123,10 @@ bool OfferNotificationBubbleControllerImpl::ShouldIconExpand() const {
   return icon_should_expand_;
 }
 
+void OfferNotificationBubbleControllerImpl::OnIconExpanded() {
+  icon_should_expand_ = false;
+}
+
 void OfferNotificationBubbleControllerImpl::OnBubbleClosed(
     PaymentsBubbleClosedReason closed_reason) {
   set_bubble_view(nullptr);
