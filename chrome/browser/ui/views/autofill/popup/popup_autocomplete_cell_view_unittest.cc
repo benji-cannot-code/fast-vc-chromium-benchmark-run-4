5c725f4098f99135cef9d63b29410ad50a6447b8
Jan Keitel <jkeitel@google.com>
2023-09-28 06:43:29 +0000

    Rename DeleteButton to CellButton in PopupAutocompleteCellView.
    This is preparation for making PopupAutocompleteCellView into a more
    generic PopupCellWithButtonView.
    
    The CL only renames and removes one undefined method - it performs
    no functional changes.
    
    Bug: 1487086
    Change-Id: I414ceaf67530b2b1bd6ed8bb372881c2ea2debeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4890230
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1202371}

--- chrome/browser/ui/views/autofill/popup/popup_autocomplete_cell_view_unittest.cc
+++ chrome/browser/ui/views/autofill/popup/popup_autocomplete_cell_view_unittest.cc
@@ -89,7 +89,7 @@ class PopupAutocompleteCellViewTest : public ChromeViewsTestBase {
                                                     /*line_number=*/0);
     cell->SetAccessibilityDelegate(
         std::make_unique<TestAccessibilityDelegate>());
-    views::ImageButton* button = cell->GetDeleteButton();
+    views::ImageButton* button = cell->GetCellButtonForTest();
     ShowView(std::move(cell));
     return button;
   }
