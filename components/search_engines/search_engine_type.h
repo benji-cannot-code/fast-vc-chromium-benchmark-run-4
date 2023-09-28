58dc27b8bfe79b78da0495f53cd970a59f0fea51
Jens Mueller <muellerj@google.com>
2023-09-28 15:11:49 +0000

    Add new search engine types to histogram values.
    Including a few pre-existing ones that seem to have been forgotten.
    
    Bug: b/282656014
    Change-Id: I641e761c3ae0331e324c234433518d09c03c61d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4887245
    Auto-Submit: Jens Mueller <muellerj@google.com>
    Commit-Queue: Jens Mueller <muellerj@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202635}

--- components/search_engines/search_engine_type.h
+++ components/search_engines/search_engine_type.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // These values are persisted to logs. Entries should not be renumbered and
 // numeric values should never be reused.
 //
+// This should be kept aligned with OmniboxSearchEngineType in enums.xml.
+//
 // A Java counterpart will be generated for this enum.
 // GENERATED_JAVA_ENUM_PACKAGE: org.chromium.chrome.browser.search_engines
 enum SearchEngineType {
