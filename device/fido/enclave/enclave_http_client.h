69fb64b20186102eb022b464b699c65eade3ee02
Ken Buchanan <kenrb@google.com>
2023-09-28 22:54:01 +0000

    [WebAuthn] Enclave client getAssertion implementation
    This updates the enclave client to send properly formed assertion
    requests that can be processed by the enclave service, and parse the
    response.
    
    The client application wraps this code and can interact with a test
    instance of the enclave server.
    
    Bug: 1459620
    Change-Id: I4ec4780d824813b2b1e10494da67bb31e5ded593
    Low-Coverage-Reason: The test service is not available in Chrome yet.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4902562
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Auto-Submit: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202915}

--- device/fido/enclave/enclave_http_client.h
+++ device/fido/enclave/enclave_http_client.h
@@ -33,7 +33,9 @@ class EnclaveHttpClient : public net::URLRequest::Delegate {
     kCommand,
   };
 
-  EnclaveHttpClient(const GURL& service_url, RequestCallback on_request_done);
+  EnclaveHttpClient(const GURL& service_url,
+                    const std::string& username,
+                    RequestCallback on_request_done);
   ~EnclaveHttpClient() override;
 
   EnclaveHttpClient(const EnclaveHttpClient&) = delete;
@@ -68,6 +70,7 @@ class EnclaveHttpClient : public net::URLRequest::Delegate {
   RequestType request_in_progress_ = RequestType::kNone;
 
   GURL service_url_;
+  std::string username_;
   RequestCallback on_request_done_;
 
   // url_request_ has to be before url_request_context_ for destruction
