494d9a74ce60ce101126a54603add7191c3b68b3
Ian Wells <iwells@chromium.org>
2023-09-28 21:29:01 +0000

    Create Player interface and add to playback hooks
    This change prepares for building the player into the isolated split.
    Player will be implemented by PlayerCoordinator. An internal CL will
    implement createPlayer() which creates and returns a new
    PlayerCoordinator. After the internal CL is submitted, another public CL will be needed to switch ReadAloudController over to creating the player from the module rather than directly.
    
    Low-Coverage-Reason: TRIVIAL_CHANGE Player.java only contains empty default implementations of interface methods.
    Bug: b/300935431
    Change-Id: I98a89149718a420b32ff2826de82342e78852654
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4894918
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202836}

--- chrome/android/modules/readaloud/public/java/src/org/chromium/chrome/modules/readaloud/ReadAloudPlaybackHooks.java
+++ chrome/android/modules/readaloud/public/java/src/org/chromium/chrome/modules/readaloud/ReadAloudPlaybackHooks.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.modules.readaloud;
 
+import android.view.ViewStub;
+
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 
 /** Interface for creating ReadAloud playback. */
@@ -46,5 +48,15 @@ public interface ReadAloudPlaybackHooks {
      */
     default ExpandedPlayer createExpandedPlayer(BottomSheetController bottomSheetController) {
         return new ExpandedPlayer() {};
+    }
+
+    /**
+     * Create the player UI.
+     * @param miniPlayerViewStub View stub that can create the mini player UI.
+     * @param delegate Delegate providing the UI with outside dependencies.
+     * @return a Player.
+     */
+    default Player createPlayer(ViewStub miniPlayerViewStub, Player.Delegate delegate) {
+        return new Player() {};
     }
 }
