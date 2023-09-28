bee9d1d65b82d642eb10c8805859b39f272ec7d4
Noam Rosenthal <nrosenthal@chromium.org>
2023-09-28 13:10:50 +0000

    Deflake LoAF test: wrap in parentheses
    Test fails when there is an internal retry,
    which causes the "xhr" global variable to be
    defines more than once.
    
    Bug: 1486016
    Change-Id: I2ae1b7cef06640523ed9d3fc7e359c7b69d14cff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4894513
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202558}

--- third_party/blink/web_tests/external/wpt/long-animation-frame/tentative/resources/event-generates-loaf.js
+++ third_party/blink/web_tests/external/wpt/long-animation-frame/tentative/resources/event-generates-loaf.js
@@ -1,9 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-const xhr = new XMLHttpRequest();
-xhr.open('GET', '/common/dummy.xml');
-xhr.addEventListener('load', () => {
-  const deadline = performance.now() + 360;
-  while (performance.now() < deadline) {
-  }
-});
-xhr.send();
+(() => {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', '/common/dummy.xml');
+  xhr.addEventListener('load', () => {
+    const deadline = performance.now() + 360;
+    while (performance.now() < deadline) {
+    }
+  });
+  xhr.send();
+})();
