e0eea8933a4839eac53318ca95587ae28dda3051
Leena Kaushik <l1.kaushik@samsung.com>
2023-09-28 03:29:40 +0000

    Use base::EraseIf()
    This patch is just a code simplification.
    
    It's much easier to write:
      base::EraseIf(container, ...);
    than:
      container.erase(std::remove_if(container.begin(),
          container.end(), ...), container.end());
    
    Bug: 875665
    
    Change-Id: I4835ca033e4a12ffa4d038b6e5534f15d77229c8
    Signed-off-by: Leena Kaushik <l1.kaushik@samsung.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4896065
    Reviewed-by: Min Chen <minch@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202267}

--- ash/system/tray/tray_detailed_view.cc
+++ ash/system/tray/tray_detailed_view.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
+#include "base/containers/cxx20_erase.h"
 #include "ash/constants/ash_features.h"
 #include "ash/controls/rounded_scroll_bar.h"
 #include "ash/public/cpp/ash_view_ids.h"
@@ -250,11 +251,9 @@ class ScrollContentsView : public views::View {
     }
 
     if (!details.is_add && details.parent == this) {
-      headers_.erase(std::remove_if(headers_.begin(), headers_.end(),
-                                    [details](const Header& header) {
-                                      return header.view.get() == details.child;
-                                    }),
-                     headers_.end());
+      base::EraseIf(headers_, [details](const Header& header) {
+          return header.view.get() == details.child;
+      });
     } else if (details.is_add && details.parent == this &&
                details.child == children().front()) {
       // We always want padding on the bottom of the scroll contents.
