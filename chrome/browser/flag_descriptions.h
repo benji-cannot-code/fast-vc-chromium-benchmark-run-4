c6fb807549d34fb00d81d8b41c7c86038255eb34
Mohamed Amir Yosef <mamir@chromium.org>
2023-09-28 11:10:15 +0000

    Add an about flag to enable sharing feature flags
    Bug: 1298608
    Change-Id: I166f41c7bc2aa4111780c748c91e2129cdabc6b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4822888
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202500}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2180,6 +2180,9 @@ extern const char kUseIDNA2008NonTransitionalDescription[];
 extern const char kEnableVariationsGoogleGroupFilteringName[];
 extern const char kEnableVariationsGoogleGroupFilteringDescription[];
 
+extern const char kEnablePasswordSharingName[];
+extern const char kEnablePasswordSharingDescription[];
+
 // Android --------------------------------------------------------------------
 
 #if BUILDFLAG(IS_ANDROID)
