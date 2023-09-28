703e1ab4122cb0b96ffc634eb1f0f740bcd56489
Rupert Ben Wiser <bewise@chromium.org>
2023-09-28 19:40:40 +0000

    webview: Use 3rd party cookie metrics
    Moving the 3PC read metrics to the components observers
    so that we can share this with WebView.
    
    Bug: 1486864
    Change-Id: Ia406a14be995588233061bf7e35d6c96c09e5e01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4892400
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Rupert Wiser <bewise@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202787}

--- android_webview/browser/page_load_metrics/page_load_metrics_initialize.cc
+++ android_webview/browser/page_load_metrics/page_load_metrics_initialize.cc
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "android_webview/browser/page_load_metrics/aw_page_load_metrics_memory_tracker_factory.h"
 #include "components/page_load_metrics/browser/metrics_web_contents_observer.h"
+#include "components/page_load_metrics/browser/observers/third_party_metrics_observer.h"
 #include "components/page_load_metrics/browser/page_load_metrics_embedder_base.h"
 #include "components/page_load_metrics/browser/page_load_metrics_memory_tracker.h"
+#include "components/page_load_metrics/browser/page_load_tracker.h"
 
 namespace content {
 class BrowserContext;
@@ -54,7 +56,9 @@ PageLoadMetricsEmbedder::PageLoadMetricsEmbedder(
 PageLoadMetricsEmbedder::~PageLoadMetricsEmbedder() = default;
 
 void PageLoadMetricsEmbedder::RegisterEmbedderObservers(
-    page_load_metrics::PageLoadTracker* tracker) {}
+    page_load_metrics::PageLoadTracker* tracker) {
+  tracker->AddObserver(std::make_unique<ThirdPartyMetricsObserver>());
+}
 
 bool PageLoadMetricsEmbedder::IsNewTabPageUrl(const GURL& url) {
   return false;
--- components/page_load_metrics/browser/observers/third_party_metrics_observer_unittest.cc
+++ components/page_load_metrics/browser/observers/third_party_metrics_observer_unittest.cc
@@ -3,10 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/page_load_metrics/observers/third_party_metrics_observer.h"
+#include "components/page_load_metrics/browser/observers/third_party_metrics_observer.h"
 
 #include "base/memory/ptr_util.h"
-#include "chrome/browser/page_load_metrics/observers/page_load_metrics_observer_test_harness.h"
+#include "components/page_load_metrics/browser/observers/page_load_metrics_observer_content_test_harness.h"
 #include "components/page_load_metrics/browser/page_load_tracker.h"
 #include "components/page_load_metrics/common/page_load_metrics.mojom.h"
 #include "components/page_load_metrics/common/test/page_load_metrics_test_util.h"
@@ -27,7 +27,7 @@ using content::RenderFrameHost;
 using content::RenderFrameHostTester;
 
 class ThirdPartyMetricsObserverTestBase
-    : public page_load_metrics::PageLoadMetricsObserverTestHarness {
+    : public page_load_metrics::PageLoadMetricsObserverContentTestHarness {
  public:
   ThirdPartyMetricsObserverTestBase(const ThirdPartyMetricsObserverTestBase&) =
       delete;
@@ -56,8 +56,9 @@ class ThirdPartyMetricsObserverTestBase
   }
 
   RenderFrameHost* AppendChildFrame(content::RenderFrameHost* parent) {
-    if (WithFencedFrames())
+    if (WithFencedFrames()) {
       return content::RenderFrameHostTester::For(parent)->AppendFencedFrame();
+    }
     return content::RenderFrameHostTester::For(parent)->AppendChild("iframe");
   }
 
