263cda29b0687f88fa88e44ade1fee12650f4027
Rakina Zata Amni <rakina@chromium.org>
2023-09-28 02:20:48 +0000

    Update prerender tests to expect RFH change on navigation
    With RenderDocument, cross-document navigations will use new
    RenderFrameHosts (and RenderViewHosts, RenderWidgetHosts, etc for
    main frame navigations). This CL updates the prerender tests that didn't
    expect those changes.
    
    Bug: 936696
    Change-Id: I9e16e27e0cfbe01d227630f9f8bc74d7293bf1e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4875814
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202249}

--- content/browser/preloading/prerender/prerender_browsertest.cc
+++ content/browser/preloading/prerender/prerender_browsertest.cc
@@ -2618,13 +2618,10 @@ class PrerenderMainFrameNavigationBrowserTest
     // URL of the given navigation URLs will separately be handled later as that
     // could cancel prerendering and never finish.
     for (auto it = urls.begin(); it != urls.end() - 1; ++it) {
-      RenderFrameHostImpl* prerender_rfh = GetPrerenderedMainFrameHost(host_id);
       TestNavigationManager navigation_observer(web_contents(), *it);
       NavigatePrerenderedPage(host_id, *it);
       ASSERT_TRUE(navigation_observer.WaitForNavigationFinished());
       EXPECT_TRUE(navigation_observer.was_successful());
-      // Make sure that RenderFrameHost swap didn't happen during navigation.
-      EXPECT_EQ(prerender_rfh, GetPrerenderedMainFrameHost(host_id));
     }
 
     // The last navigation URL. This should cancel prerendering if the
@@ -2634,14 +2631,10 @@ class PrerenderMainFrameNavigationBrowserTest
     switch (expected_status) {
       case PrerenderFinalStatus::kActivated: {
         // Navigation to the last URL should succeed.
-        RenderFrameHostImpl* prerender_rfh =
-            GetPrerenderedMainFrameHost(host_id);
         TestNavigationManager navigation_observer(web_contents(), last_url);
         NavigatePrerenderedPage(host_id, last_url);
         ASSERT_TRUE(navigation_observer.WaitForNavigationFinished());
         EXPECT_TRUE(navigation_observer.was_successful());
-        // Make sure that RenderFrameHost swap didn't happen during navigation.
-        EXPECT_EQ(prerender_rfh, GetPrerenderedMainFrameHost(host_id));
 
         // Activation should succeed.
         switch (trigger_type) {
@@ -2727,7 +2720,6 @@ class PrerenderMainFrameNavigationBrowserTest
     test::PrerenderHostObserver observer(*web_contents_impl(), host_id);
 
     // Run redirections in the main frame of the prerendered page.
-    RenderFrameHostImpl* prerender_rfh = GetPrerenderedMainFrameHost(host_id);
     TestNavigationManager navigation_observer(web_contents(), url);
     NavigatePrerenderedPage(host_id, url);
     ASSERT_TRUE(navigation_observer.WaitForNavigationFinished());
@@ -2736,8 +2728,6 @@ class PrerenderMainFrameNavigationBrowserTest
       case PrerenderFinalStatus::kActivated: {
         // Redirections should succeed.
         EXPECT_TRUE(navigation_observer.was_successful());
-        // Make sure that RenderFrameHost swap didn't happen during navigation.
-        EXPECT_EQ(prerender_rfh, GetPrerenderedMainFrameHost(host_id));
 
         // Activation should succeed.
         switch (trigger_type) {
