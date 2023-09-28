030b9c85953c39b3f5b85421bf2e5ba87d40eca5
Rune Lillesveen <futhark@chromium.org>
2023-09-28 14:12:27 +0000

    Non-inherited properties may depend on inherited line-height
    For the lh unit, non-inherited properties like height might use lh as a
    unit which means properties inherited from the parent can change the
    result in the context of MatchedPropertiesCache. Check whether the
    line-height changed like we do it for font to handle font-relative
    units.
    
    Bug: 1487480
    Change-Id: I6012fdad747f46a964206af8637e10cb130c8cad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4897800
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202593}

--- third_party/blink/renderer/core/style/computed_style.h
+++ third_party/blink/renderer/core/style/computed_style.h
@@ -3126,6 +3126,7 @@ class ComputedStyleBuilder final : public ComputedStyleBuilderBase {
     return LineHeightInternal() ==
            ComputedStyleInitialValues::InitialLineHeight();
   }
+  const Length& LineHeight() const { return LineHeightInternal(); }
 
   // Sizing properties
   const Length& UsedWidth() const {
