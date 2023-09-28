6cc53f5a6c5d001e63bbfa94620e0d051785beb7
Julie Jeongeun Kim <jkim@igalia.com>
2023-09-28 04:08:22 +0000

    [ios] Add FontEnumerationDataSource
    This CL includes FontEnumerationDataSourceMac in iOS to support
    `Local Font Access API`.
    
    Bug: 1487133
    Change-Id: Ib420038ad3ff8cad3cd6a495b55a38d9bb440696
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4896697
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1202279}

--- content/browser/font_access/font_enumeration_data_source.cc
+++ content/browser/font_access/font_enumeration_data_source.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_WIN)
 #include "content/browser/font_access/font_enumeration_data_source_win.h"
-#elif BUILDFLAG(IS_MAC)
+#elif BUILDFLAG(IS_APPLE)
 #include "content/browser/font_access/font_enumeration_data_source_mac.h"
 #elif BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 #include "content/browser/font_access/font_enumeration_data_source_linux.h"
@@ -60,7 +60,7 @@ class FontEnumerationDataSourceNull : public FontEnumerationDataSource {
 std::unique_ptr<FontEnumerationDataSource> FontEnumerationDataSource::Create() {
 #if BUILDFLAG(IS_WIN)
   return std::make_unique<FontEnumerationDataSourceWin>();
-#elif BUILDFLAG(IS_MAC)
+#elif BUILDFLAG(IS_APPLE)
   return std::make_unique<FontEnumerationDataSourceMac>();
 #elif BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
   return std::make_unique<FontEnumerationDataSourceLinux>();
@@ -75,7 +75,7 @@ bool FontEnumerationDataSource::IsOsSupported() {
 
 #if BUILDFLAG(IS_WIN)
   return true;
-#elif BUILDFLAG(IS_MAC)
+#elif BUILDFLAG(IS_APPLE)
   return true;
 #elif BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
   return true;
--- content/browser/font_access/font_enumeration_data_source_unittest.cc
+++ content/browser/font_access/font_enumeration_data_source_unittest.cc
@@ -10,9 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/font_access/font_enumeration_table.pb.h"
 
-#if BUILDFLAG(IS_MAC)
+#if BUILDFLAG(IS_APPLE)
 #include "content/browser/font_access/font_enumeration_data_source_mac.h"
-#endif  // BUILDFLAG(IS_MAC)
+#endif  // BUILDFLAG(IS_APPLE)
 
 namespace content {
 
@@ -49,13 +49,13 @@ TEST_F(FontEnumerationDataSourceTest, GetFonts_DefaultLocale) {
   }
 }
 
-#if BUILDFLAG(IS_MAC)
+#if BUILDFLAG(IS_APPLE)
 TEST_F(FontEnumerationDataSourceTest, CheckInvalidFontMac) {
   const CTFontDescriptorRef fd = nullptr;
   FontEnumerationDataSourceMac data_src_mac;
   EXPECT_FALSE(data_src_mac.IsValidFontMacForTesting(fd));
 }
-#endif  // BUILDFLAG(IS_MAC)
+#endif  // BUILDFLAG(IS_APPLE)
 
 }  // namespace
 
