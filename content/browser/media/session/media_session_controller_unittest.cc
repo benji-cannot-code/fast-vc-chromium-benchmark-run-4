c2f404a1fd31b90084326868870f0af4bf8fbdae
punithnayak <npunith125@gmail.com>
2023-09-28 14:36:42 +0000

    Dangling pointer in content/browser/media/session/media_session_controller_unittest.cc
    Bug: 1485843
    Change-Id: I3567d967e0c9ae794f97e36c7298438164d3ab07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4896092
    Auto-Submit: Punith Nayak <npunith125@gmail.com>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202607}

--- content/browser/media/session/media_session_controller_unittest.cc
+++ content/browser/media/session/media_session_controller_unittest.cc
@@ -215,6 +215,7 @@ class MediaSessionControllerTest : public RenderViewHostImplTestHarness {
   }
 
   void TearDown() override {
+    audio_focus_delegate_ = nullptr;
     // Destruct the controller prior to any other teardown to avoid out of order
     // destruction relative to the MediaSession instance.
     controller_.reset();
@@ -308,8 +309,7 @@ class MediaSessionControllerTest : public RenderViewHostImplTestHarness {
   MediaPlayerId id_ = MediaPlayerId::CreateMediaPlayerIdForTests();
   std::unique_ptr<MediaSessionController> controller_;
   std::unique_ptr<TestMediaPlayer> media_player_;
-  raw_ptr<FakeAudioFocusDelegate, DanglingUntriaged> audio_focus_delegate_ =
-      nullptr;
+  raw_ptr<FakeAudioFocusDelegate> audio_focus_delegate_ = nullptr;
 };
 
 TEST_F(MediaSessionControllerTest, NoAudioNoSession) {
