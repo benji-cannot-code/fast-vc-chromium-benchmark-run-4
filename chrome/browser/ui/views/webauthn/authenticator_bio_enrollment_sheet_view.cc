710c51b3da37904570b92d0813cb5c4f734b8b04
Nina Satragno <nsatragno@chromium.org>
2023-09-28 21:26:15 +0000

    [webauthn] Update dialog focus behaviour
    This CL updates the focus behaviour of webauthn dialogs. Previously, the
    "primary" control of each sheet was focused, which was either a list of
    options or one of the buttons. This behaviour was problematic for screen
    readers:
    
    First, it is not recommended to jump to a button that is most likely not
    what the user wants to do at a given moment [1]. E.g. on the security
    key activation dialog, the user most likely wants to tap their security
    key, not cancel out of the dialog.
    
    Second, the authenticator dialog is special because its contents change
    as the user interacts with the authenticator, but the container itself
    does not. The resulting behaviour is that screen readers will read
    whatever gets focused next but will not announce the new context. This
    is really confusing, e.g. if the request times out, VoiceOver on Mac
    would announce "Cancel - Button" instead of "The request timed out...".
    
    This CL addresses these issues by making the title of the dialog
    accessibility-focusable and focusing it every time the sheet is updated.
    The modal is still keyboard navigable, with the enter key triggering the
    default button if present.
    
    [1] https://www.w3.org/WAI/ARIA/apg/patterns/dialog-modal/
    
    Bug: 1347607
    Change-Id: If59fbd8f2dbfa3eb7a554d6ecd5ad82df29f2879
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4894369
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202832}

--- chrome/browser/ui/views/webauthn/authenticator_bio_enrollment_sheet_view.cc
+++ chrome/browser/ui/views/webauthn/authenticator_bio_enrollment_sheet_view.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/ui/views/accessibility/non_accessible_image_view.h"
 #include "chrome/browser/ui/views/webauthn/ring_progress_bar.h"
-#include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/vector_icons.h"
 
@@ -37,8 +36,7 @@ AuthenticatorBioEnrollmentSheetView::AuthenticatorBioEnrollmentSheetView(
 AuthenticatorBioEnrollmentSheetView::~AuthenticatorBioEnrollmentSheetView() =
     default;
 
-std::pair<std::unique_ptr<views::View>,
-          AuthenticatorRequestSheetView::AutoFocus>
+std::unique_ptr<views::View>
 AuthenticatorBioEnrollmentSheetView::BuildStepSpecificContent() {
   auto* bio_model = static_cast<AuthenticatorBioEnrollmentSheetModel*>(model());
   double target = CalculateProgressFor(bio_model->bio_samples_remaining(),
@@ -67,7 +65,7 @@ AuthenticatorBioEnrollmentSheetView::BuildStepSpecificContent() {
   ring_progress_bar->SetValue(initial, target);
   animation_container->AddChildView(std::move(ring_progress_bar));
 
-  return std::make_pair(std::move(animation_container), AutoFocus::kNo);
+  return animation_container;
 }
 
 bool AuthenticatorBioEnrollmentSheetView::AcceleratorPressed(
