0925f997d1c37d61da9367b477ff1e650924a03b
Alan Screen <awscreen@chromium.org>
2023-09-28 07:12:20 +0000

    Update PrintingApiHandler tests to work with OOPPD
    When out-of-process print drivers is enabled, some extra setup is
    necessary so that a testing instance of the Print Backend service is
    created before the tests run.  This is necessary for the service to use
    the fake classes TestPrintBackend and TestPrintingContext instead of
    the real platform classes, which will directly interface with the
    platform's system printing APIs.  The fake classes provide deterministic
    testing behavior, since there are not consistent print drivers installed
    on platforms nor is printing even enabled on all machines which the
    tests might run.
    
    Bug: b/301590015
    Change-Id: I245778f87aabe14a3e6664e814bacd5db65649a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4891262
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202384}

--- chrome/browser/extensions/api/printing/printing_apitest.cc
+++ chrome/browser/extensions/api/printing/printing_apitest.cc
@@ -28,8 +28,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/test/test_extension_dir.h"
 #include "printing/backend/print_backend.h"
 #include "printing/backend/test_print_backend.h"
+#include "printing/buildflags/buildflags.h"
 #include "printing/mojom/print.mojom.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+#if BUILDFLAG(ENABLE_OOP_PRINTING)
+#include "base/feature_list.h"
+#include "chrome/browser/printing/print_backend_service_test_impl.h"
+#include "chrome/services/printing/public/mojom/print_backend_service.mojom.h"
+#include "mojo/public/cpp/bindings/remote.h"
+#include "printing/printing_features.h"
+#endif
 
 namespace extensions {
 
@@ -82,6 +91,18 @@ class PrintingApiTest : public ExtensionApiTest,
   PrintingApiTest& operator=(const PrintingApiTest&) = delete;
 
  protected:
+  void SetUpOnMainThread() override {
+#if BUILDFLAG(ENABLE_OOP_PRINTING)
+    if (base::FeatureList::IsEnabled(
+            printing::features::kEnableOopPrintDrivers)) {
+      print_backend_service_ =
+          printing::PrintBackendServiceTestImpl::LaunchForTesting(
+              test_remote_, test_print_backend_.get(), /*sandboxed=*/true);
+    }
+#endif
+    ExtensionApiTest::SetUpOnMainThread();
+  }
+
   void SetUpInProcessBrowserTestFixture() override {
     create_services_subscription_ =
         BrowserContextDependencyManager::GetInstance()
@@ -154,6 +175,11 @@ class PrintingApiTest : public ExtensionApiTest,
     ash::CupsPrintersManagerFactory::GetInstance()->SetTestingFactory(
         context, base::BindRepeating(&BuildFakeCupsPrintersManager));
   }
+
+#if BUILDFLAG(ENABLE_OOP_PRINTING)
+  mojo::Remote<printing::mojom::PrintBackendService> test_remote_;
+  std::unique_ptr<printing::PrintBackendServiceTestImpl> print_backend_service_;
+#endif
 
   base::CallbackListSubscription create_services_subscription_;
 
