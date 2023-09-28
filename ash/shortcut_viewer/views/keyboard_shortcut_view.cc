8c9e96e005deafd5788a331c4735634b1383e9cc
Allen Bauer <kylixrd@chromium.org>
2023-09-28 17:32:48 +0000

    Rename TabbedPane TabStrip to TabbedPaneTabStrip to eliminate metadata typename collision.
    Found while working on simplifying metadata use and adding checks.
    
    Bug: N/A
    Change-Id: I4daa5c13b5f4bf3b80d488bd461c898ad0c70c43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4898017
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202743}

--- ash/shortcut_viewer/views/keyboard_shortcut_view.cc
+++ ash/shortcut_viewer/views/keyboard_shortcut_view.cc
@@ -293,8 +293,9 @@ bool KeyboardShortcutView::AcceleratorPressed(
 
 void KeyboardShortcutView::Layout() {
   gfx::Rect content_bounds(GetContentsBounds());
-  if (content_bounds.IsEmpty())
+  if (content_bounds.IsEmpty()) {
     return;
+  }
 
   constexpr int kSearchBoxTopPadding = 8;
   constexpr int kSearchBoxBottomPadding = 16;
@@ -335,8 +336,9 @@ void KeyboardShortcutView::OnPaint(gfx::Canvas* canvas) {
     return;
   }
 
-  if (!needs_init_all_categories_)
+  if (!needs_init_all_categories_) {
     return;
+  }
 
   needs_init_all_categories_ = false;
   // Cannot post a task right after initializing the first category, it will
@@ -368,8 +370,9 @@ void KeyboardShortcutView::QueryChanged(const std::u16string& query) {
 
   debounce_timer_.Stop();
   // If search box is empty, do not show |search_results_container_|.
-  if (query_empty)
+  if (query_empty) {
     return;
+  }
 
   // TODO(wutao): This timeout value is chosen based on subjective search
   // latency tests on Minnie. Objective method or UMA is desired.
@@ -412,8 +415,9 @@ void KeyboardShortcutView::InitViews() {
   // clear the cache.
   KeyboardShortcutItemView::ClearKeycodeToString16Cache();
   for (const auto& item : GetKeyboardShortcutItemList()) {
-    if (ShouldExcludeItem(item))
+    if (ShouldExcludeItem(item)) {
       continue;
+    }
 
     for (auto category : item.categories) {
       shortcut_views_.push_back(
@@ -446,8 +450,9 @@ void KeyboardShortcutView::InitCategoriesTabbedPane(
   active_tab_index_ = categories_tabbed_pane_->GetSelectedTabIndex();
   // If the tab count is 0, GetSelectedTabIndex() will return kNoSelectedTab,
   // which we do not want to cache.
-  if (active_tab_index_ == views::TabStrip::kNoSelectedTab)
+  if (active_tab_index_ == views::TabbedPaneTabStrip::kNoSelectedTab) {
     active_tab_index_ = 0;
+  }
 
   ash::ShortcutCategory current_category = ash::ShortcutCategory::kUnknown;
   KeyboardShortcutItemListView* item_list_view = nullptr;
@@ -494,8 +499,9 @@ void KeyboardShortcutView::InitCategoriesTabbedPane(
     // If |initial_category| has a value, we only initialize the pane with the
     // KeyboardShortcutItemView in the specific category in |initial_category|.
     // Otherwise, we will initialize all the panes.
-    if (initial_category.value_or(category) != category)
+    if (initial_category.value_or(category) != category) {
       continue;
+    }
 
     // Add the item to the category contents container.
     if (!item_list_view->children().empty())
