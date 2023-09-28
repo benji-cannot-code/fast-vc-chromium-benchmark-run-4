d4004000ecd6047b8ca0b1929d73adb6ed338b6f
Cici Ruan <cuicuiruan@google.com>
2023-09-28 16:57:23 +0000

    arc/gio: Fix a regression crash bug when showing GC UIs
    This CL fixes the crash when GC UIs show up. For example, when disabling
    and enabling GC, then input mapping should show up, but it crashes.
    When entering edit mode, EditingList should show up, but it crashes.
    This is because GameDashboardMainMenu has set_close_on_deactivate(true).
    Once GC widgets call Show(), it causes focus issue. So once GC
    UIs/widgets show up, show them as inactive and then the user
    interfaction makes them active.
    
          - hiding and showing input mapping
          - disabling and enabling GC
          - entering into edit mode
    
    Bug: b/302366625
    Test: manual test by
    Change-Id: I0559fe03bcaafea7815a706c86007d8a08461283
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4898435
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1202721}

--- chrome/browser/ash/arc/input_overlay/display_overlay_controller.cc
+++ chrome/browser/ash/arc/input_overlay/display_overlay_controller.cc
@@ -488,7 +488,7 @@ void DisplayOverlayController::SetDisplayMode(DisplayMode mode) {
       } else {
         AddInputMappingWidget();
         if (touch_injector_->input_mapping_visible()) {
-          input_mapping_widget_->Show();
+          input_mapping_widget_->ShowInactive();
         }
 
         auto* input_mapping_view = static_cast<InputMappingView*>(
@@ -512,7 +512,7 @@ void DisplayOverlayController::SetDisplayMode(DisplayMode mode) {
 
       // No matter if the mapping hint is hidden, `input_mapping_widget_` needs
       // to show up in `kEdit` mode.
-      input_mapping_widget_->Show();
+      input_mapping_widget_->ShowInactive();
 
       auto* input_mapping_view = static_cast<InputMappingView*>(
           input_mapping_widget_->GetContentsView());
@@ -697,7 +697,7 @@ void DisplayOverlayController::AddButtonOptionsMenuWidget(Action* action) {
       std::make_unique<ButtonOptionsMenu>(this, action));
   UpdateButtonOptionsMenuWidgetBounds(action);
 
-  button_options_widget_->Show();
+  button_options_widget_->ShowInactive();
 }
 
 void DisplayOverlayController::RemoveButtonOptionsMenuWidget() {
@@ -735,7 +735,7 @@ void DisplayOverlayController::SetButtonOptionsMenuWidgetVisibility(
         static_cast<ButtonOptionsMenu*>(
             button_options_widget_->GetContentsView())
             ->action());
-    button_options_widget_->Show();
+    button_options_widget_->ShowInactive();
   } else {
     button_options_widget_->Hide();
   }
@@ -760,7 +760,7 @@ void DisplayOverlayController::AddButtonLabelListWidget(Action* action) {
                         touch_injector_->window()->GetRootWindow()),
                     touch_injector_->content_bounds().origin(), view),
                 view->GetPreferredSize()));
-  button_label_list_widget_->Show();
+  button_label_list_widget_->ShowInactive();
 }
 
 void DisplayOverlayController::RemoveButtonLabelListWidget() {
@@ -773,7 +773,7 @@ void DisplayOverlayController::RemoveButtonLabelListWidget() {
 
 void DisplayOverlayController::OnButtonLabelListBackButtonPressed() {
   RemoveButtonLabelListWidget();
-  button_options_widget_->Show();
+  button_options_widget_->ShowInactive();
 }
 
 void DisplayOverlayController::AddNudgeWidget(views::View* anchor_view,
@@ -797,7 +797,7 @@ void DisplayOverlayController::AddNudgeWidget(views::View* anchor_view,
       std::make_unique<Nudge>(this, anchor_view, text));
   auto* window = nudge_widget_ptr->GetNativeWindow();
   window->parent()->StackChildAtTop(window);
-  nudge_widget_ptr->Show();
+  nudge_widget_ptr->ShowInactive();
 }
 
 void DisplayOverlayController::RemoveNudgeWidget(views::Widget* widget) {
@@ -952,7 +952,7 @@ void DisplayOverlayController::SetInputMappingVisible(bool visible) {
       return;
     }
     if (visible) {
-      input_mapping_widget_->Show();
+      input_mapping_widget_->ShowInactive();
     } else {
       input_mapping_widget_->Hide();
     }
@@ -1149,7 +1149,7 @@ void DisplayOverlayController::AddEditingListWidget() {
   auto* window = editing_list_widget_->GetNativeWindow();
   window->parent()->StackChildAtTop(window);
 
-  editing_list_widget_->Show();
+  editing_list_widget_->ShowInactive();
   UpdateEditingListWidgetBounds();
 }
 
