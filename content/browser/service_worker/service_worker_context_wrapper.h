12323bb305916ae39fcb8563114aadb86f59587e
Justin Lulejian <jlulejian@chromium.org>
2023-09-28 02:04:09 +0000

    [Extensions] Refactor IsLiveRunningServiceWorker() into two.
    This splits IsLiveRunningServiceWorker() (which currently returns true
    for blink::EmbeddedWorkerStatus::kStarting or ::kRunning) into a method
    for both.
    
    This allows extension service workers to check the running status of the
    worker during event dispatch before attempting to start the worker.
    Currently extension service workers are attempted to be started every
    time an event is received. Which is at the least inefficient (due to
    there being IPC messages involved) and at worst potentially buggy. The
    usage will be in a follow-up CL.
    
    appear to be tested since methods are not implemented.
    
    Change-Id: I1873d37094d63c2202d805001e2ff5f3d4ade03a
    Bug: 1467015
    Low-Coverage-Reason: OTHER service_worker_context_adapter.cc does not
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4708889
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202222}

--- content/browser/service_worker/service_worker_context_wrapper.h
+++ content/browser/service_worker/service_worker_context_wrapper.h
@@ -222,6 +222,7 @@ class CONTENT_EXPORT ServiceWorkerContextWrapper
   void StopAllServiceWorkers(base::OnceClosure callback) override;
   const base::flat_map<int64_t, ServiceWorkerRunningInfo>&
   GetRunningServiceWorkerInfos() override;
+  bool IsLiveStartingServiceWorker(int64_t service_worker_version_id) override;
   bool IsLiveRunningServiceWorker(int64_t service_worker_version_id) override;
   service_manager::InterfaceProvider& GetRemoteInterfaces(
       int64_t service_worker_version_id) override;
@@ -564,6 +565,10 @@ class CONTENT_EXPORT ServiceWorkerContextWrapper
 
   // A loader factory used to register a service worker. Used for tests.
   scoped_refptr<network::SharedURLLoaderFactory> loader_factory_for_test_;
+
+ private:
+  // Returns a version if the worker is live, otherwise nullptr.
+  ServiceWorkerVersion* GetLiveServiceWorker(int64_t service_worker_version_id);
 };
 
 }  // namespace content
