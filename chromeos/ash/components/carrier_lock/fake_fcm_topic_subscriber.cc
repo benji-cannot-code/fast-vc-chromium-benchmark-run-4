1743c07c4ae877861f893f6ff092b4e3f600db62
Michal Mazur <michamazur@google.com>
2023-09-28 10:25:29 +0000

    carrier_lock: Implement class for FCM registration
    This class will handle registration to Firebase Cloud Messaging server
    and subscription to a notification topic for lock configuration updates.
    
    Bug: b:262288980
    Test: CarrierLockSubscribeFcmTopic
    
    Change-Id: I6105d71abe66887bc22d8f01bc248fd72b94d2f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4459285
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Michał Mazur <michamazur@google.com>
    Cr-Commit-Position: refs/heads/main@{#1202480}

---
+++ chromeos/ash/components/carrier_lock/fake_fcm_topic_subscriber.cc
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/ash/components/carrier_lock/fake_fcm_topic_subscriber.h"
+
+namespace ash::carrier_lock {
+
+FakeFcmTopicSubscriber::FakeFcmTopicSubscriber() = default;
+
+FakeFcmTopicSubscriber::~FakeFcmTopicSubscriber() = default;
+
+void FakeFcmTopicSubscriber::RequestToken(NotificationCallback notification,
+                                          Callback callback) {
+  notification_callback_ = std::move(notification);
+  std::move(callback).Run(token_.empty() ? Result::kConnectionError
+                                         : Result::kSuccess);
+}
+
+void FakeFcmTopicSubscriber::SubscribeTopic(const std::string& topic,
+                                            NotificationCallback notification,
+                                            Callback callback) {
+  notification_callback_ = std::move(notification);
+  std::move(callback).Run(result_);
+}
+
+void FakeFcmTopicSubscriber::SetTokenAndResult(std::string token,
+                                               Result result) {
+  token_ = token;
+  result_ = result;
+}
+
+void FakeFcmTopicSubscriber::SendNotification() {
+  notification_callback_.Run(false);
+}
+
+std::string const FakeFcmTopicSubscriber::token() {
+  return token_;
+}
+
+}  // namespace ash::carrier_lock
