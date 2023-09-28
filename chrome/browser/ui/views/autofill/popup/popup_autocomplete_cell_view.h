dda14b107d2cdd83f799016c7106cf8d76dbf460
Jan Keitel <jkeitel@google.com>
2023-09-28 10:47:58 +0000

    Move PopupAutocompleteCellView deletion logic into button callback.
    This CL centralises the deletion logic inside the callback that is a
    button member and simulates a click on the button on RETURN keystrokes.
    The move prepares injecting the button from the outside so that
    there can be different PopupWithCellView objects that all have their
    own action.
    
    Bug: 1487086
    Change-Id: Ibdd7ed1fb0802c7986cccee1c2d8c643722dc369
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4898681
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1202491}

--- chrome/browser/ui/views/autofill/popup/popup_autocomplete_cell_view.h
+++ chrome/browser/ui/views/autofill/popup/popup_autocomplete_cell_view.h
@@ -89,7 +89,6 @@ class PopupAutocompleteCellView : public autofill::PopupCellView,
   void OnMouseExitedCellButton() override;
 
   void CreateDeleteButton();
-  void DeleteAutocompleteEntrySoon();
   void UpdateSelectedAndRunCallback(bool selected);
   void HandleKeyPressEventFocusOnButton();
   void HandleKeyPressEventFocusOnContent();
