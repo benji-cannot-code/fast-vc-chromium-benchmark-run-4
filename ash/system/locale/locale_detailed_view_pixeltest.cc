b0b4caa8fdabc4844e0c296f8903422761fbd441
Michael Ludwig <michaelludwig@google.com>
2023-09-28 19:46:25 +0000

    Add flag to enable safer texture subsetting, re-enable msan SkiaVk tests
    Bug: chromium:1324336
    Change-Id: Ia102f7317684c8df1229581905635596d9a5691f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4864923
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202789}

--- ash/system/locale/locale_detailed_view_pixeltest.cc
+++ ash/system/locale/locale_detailed_view_pixeltest.cc
@@ -63,7 +63,7 @@ TEST_F(LocaleDetailedViewPixelTest, Basics) {
   ASSERT_TRUE(detailed_view);
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "check_view",
-      /*revision_number=*/6, detailed_view));
+      /*revision_number=*/7, detailed_view));
 }
 
 }  // namespace
