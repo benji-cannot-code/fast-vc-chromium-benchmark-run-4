05bcaf85d9619da1ca74ecea1ae7ca96bf9a9537
Aman Verma <amanvr@google.com>
2023-09-28 16:18:46 +0000

    Move //c/b/renderer_host/input/touchpad_tap_suppression_controller.h to //content/common/input
    This refactoring is a part of bigger project to handle input on viz
    thread (go/input-on-viz). We are moving input related interfaces that
    RenderWidgetHostImpl uses to //content/common/input. This would provide
    a single stack in //content/common/input that we quickly enter in either
    the browser or GPU viz which would allow for relatively clean movement
    of code. GPU process can link in the same input stack and forward the
    data to the renderer using the same code.
    
    Bug: b/294826255
    Change-Id: I0d0810ffb09e36ef5ef8ecc5318883e90659c05a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4767621
    Commit-Queue: Aman Verma <amanvr@google.com>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202684}

--- content/common/input/touchpad_tap_suppression_controller.h
+++ content/common/input/touchpad_tap_suppression_controller.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_BROWSER_RENDERER_HOST_INPUT_TOUCHPAD_TAP_SUPPRESSION_CONTROLLER_H_
-#define CONTENT_BROWSER_RENDERER_HOST_INPUT_TOUCHPAD_TAP_SUPPRESSION_CONTROLLER_H_
+#ifndef CONTENT_COMMON_INPUT_TOUCHPAD_TAP_SUPPRESSION_CONTROLLER_H_
+#define CONTENT_COMMON_INPUT_TOUCHPAD_TAP_SUPPRESSION_CONTROLLER_H_
 
 #include "content/common/input/event_with_latency_info.h"
 #include "content/common/input/tap_suppression_controller.h"
@@ -41,4 +41,4 @@ class TouchpadTapSuppressionController : public TapSuppressionController {
 
 }  // namespace content
 
-#endif  // CONTENT_BROWSER_RENDERER_HOST_INPUT_TOUCHPAD_TAP_SUPPRESSION_CONTROLLER_H_
+#endif  // CONTENT_COMMON_INPUT_TOUCHPAD_TAP_SUPPRESSION_CONTROLLER_H_
