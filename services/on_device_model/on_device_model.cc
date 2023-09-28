7d96c7cbd7728175ee1b56a91de9a58629d29575
Clark DuVall <cduvall@chromium.org>
2023-09-28 23:36:20 +0000

    Initial skeleton of OnDeviceModelService
    This creates the basic mojom interface for interacting with on device
    models and a simple implementation of the service.
    
    Bug: b/301660909
    Change-Id: Ica4ecd9c14d5a3edeea2f350c7a5922dc05e9346
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4894193
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202948}

---
+++ services/on_device_model/on_device_model.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/on_device_model/on_device_model.h"
+
+#include "mojo/public/cpp/bindings/remote.h"
+
+namespace on_device_model {
+
+OnDeviceModel::OnDeviceModel(mojom::LoadModelParamsPtr params)
+    : params_(std::move(params)) {}
+
+OnDeviceModel::~OnDeviceModel() = default;
+
+void OnDeviceModel::Execute(
+    const std::string& input,
+    mojo::PendingRemote<mojom::StreamingResponder> response) {
+  mojo::Remote<mojom::StreamingResponder> remote(std::move(response));
+  // TODO(cduvall): Make this work.
+  remote->OnResponse("Model: " + params_->path.MaybeAsASCII());
+  remote->OnResponse("Input: " + input);
+  remote->OnComplete();
+}
+
+}  // namespace on_device_model
---
+++ services/on_device_model/on_device_model.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_ON_DEVICE_MODEL_ON_DEVICE_MODEL_H_
+#define SERVICES_ON_DEVICE_MODEL_ON_DEVICE_MODEL_H_
+
+#include "services/on_device_model/public/mojom/on_device_model.mojom.h"
+
+namespace on_device_model {
+
+class OnDeviceModel : public mojom::OnDeviceModel {
+ public:
+  explicit OnDeviceModel(mojom::LoadModelParamsPtr params);
+  ~OnDeviceModel() override;
+
+  OnDeviceModel(const OnDeviceModel&) = delete;
+  OnDeviceModel& operator=(const OnDeviceModel&) = delete;
+
+  void Execute(
+      const std::string& input,
+      mojo::PendingRemote<mojom::StreamingResponder> response) override;
+
+ private:
+  const mojom::LoadModelParamsPtr params_;
+};
+
+}  // namespace on_device_model
+
+#endif  // SERVICES_ON_DEVICE_MODEL_ON_DEVICE_MODEL_H_
