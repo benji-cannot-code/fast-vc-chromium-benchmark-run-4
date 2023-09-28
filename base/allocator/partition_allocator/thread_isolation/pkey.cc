fcdee1a157ae8672fa715c94ef3175481928309f
Stephen Roettger <sroettger@google.com>
2023-09-28 15:15:56 +0000

    [PKU] enable pkey support on ChromeOS
    Change-Id: Ibe17b9c4fdcc2ff423aed7fab8aab9cbdaac7320
    Bug: v8:13355
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4894718
    Commit-Queue: Stephen Röttger <sroettger@google.com>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202637}

--- base/allocator/partition_allocator/thread_isolation/pkey.cc
+++ base/allocator/partition_allocator/thread_isolation/pkey.cc
@@ -16,8 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/partition_alloc_check.h"
 #include "base/allocator/partition_allocator/thread_isolation/thread_isolation.h"
 
-#if !BUILDFLAG(IS_LINUX)
-#error "This pkey code is currently only supported on Linux"
+#if !BUILDFLAG(IS_LINUX) && !BUILDFLAG(IS_CHROMEOS)
+#error "This pkey code is currently only supported on Linux and ChromeOS"
 #endif
 
 namespace partition_alloc::internal {
