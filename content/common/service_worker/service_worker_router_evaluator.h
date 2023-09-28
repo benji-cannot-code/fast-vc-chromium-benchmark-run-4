cb1c52af83b90cf06eb39f9feffb3ea777867d7c
Takashi Nakayama <tnak@chromium.org>
2023-09-28 06:33:13 +0000

    Refactoring: Split RouterRule into RouterRule and BaseRule in content//service_worker_router_evaluator.cc
    This CL splits a class in a file related to SW Static Routing API. This
    is preliminary change to support recursive structure in conditions of
    SW Static Routing API.
    In addition, this CL conducts migration from `RE2::Set` to `RE2`.
    I plan to add `OrRule`, `AndRule`, and `NotRule` besides `BaseRule`
    class and use them in `RouterRule` class.
    
    Explainer: https://github.com/WICG/service-worker-static-routing-api/blob/main/final-form.md
    Design Doc: https://docs.google.com/document/d/1B2D8w_2M4j9CZC1VccBOAhfCdiRP3yPCSV5UKKDhU70/
    
    Bug: 1485792
    Change-Id: I6bd46b453b8ae491cf3396f5233fc68258d1a682
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4891748
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Takashi Nakayama <tnak@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202367}

--- content/common/service_worker/service_worker_router_evaluator.h
+++ content/common/service_worker/service_worker_router_evaluator.h
@@ -37,6 +37,7 @@ class CONTENT_EXPORT ServiceWorkerRouterEvaluator {
   std::string ToString() const;
 
  private:
+  class BaseCondition;
   class RouterRule;
   void Compile();
   std::vector<blink::ServiceWorkerRouterSource> EvaluateInternal(
