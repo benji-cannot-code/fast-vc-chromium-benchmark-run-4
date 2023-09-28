169d5b608be8d0406e9e4772b21c68a125a52faa
Fredrik Söderquist <fs@opera.com>
2023-09-28 13:08:11 +0000

    Fix LayoutSVGShape::StrokeBoundingBox() for empty shapes
    Return the fill bounding box in this case (it will be empty).
    
    Fixed: 1487253
    Change-Id: Ibd6029641ca06ee1d3e5695a3f00293584428418
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4900520
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202556}

---
+++ third_party/blink/renderer/core/layout/svg/layout_svg_shape_test.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/svg_names.h"
+#include "third_party/blink/renderer/core/testing/core_unit_test_helper.h"
+
+namespace blink {
+
+using LayoutSVGShapeTest = RenderingTest;
+
+TEST_F(LayoutSVGShapeTest, StrokeBoundingBoxOnEmptyShape) {
+  SetBodyInnerHTML(R"HTML(
+    <svg>
+      <circle id="target" stroke="white" stroke-width="100"/>
+    </svg>
+  )HTML");
+
+  auto* circle = GetLayoutObjectByElementId("target");
+  EXPECT_EQ(circle->StrokeBoundingBox(), gfx::RectF(0, 0, 0, 0));
+}
+
+}  // namespace blink
