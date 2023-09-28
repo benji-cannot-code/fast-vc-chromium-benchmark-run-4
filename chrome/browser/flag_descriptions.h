28bb77089870a249ee4b80e937740784f5a9b051
Bill Carr <wicarr@microsoft.com>
2023-09-28 01:58:28 +0000

    Add about://flags entry for Media Foundation Clear Rendering Strategy
    To ease testing this change adds an entry in about://flags to set a
    Media Foundation Rendering Strategy.
    
    (Note: b/c Media Foundation Rendering Strategy is a feature this option
    has the three default options - Default, Enabled, Disabled - which will
    all have the strategy param be the default (currently Dynamic) and then
    has Enabled options for Frame Server, Direct Composition, and Dynamic
    explicitly)
    
    Bug: 1258887
    Change-Id: I58d4d027890ffad7833d6e7e1dacfaf5d057f6cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4895008
    Reviewed-by: Frank Li <frankli@microsoft.com>
    Commit-Queue: William Carr <wicarr@microsoft.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202218}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2960,6 +2960,9 @@ extern const char kHardwareSecureDecryptionFallbackDescription[];
 extern const char kMediaFoundationClearName[];
 extern const char kMediaFoundationClearDescription[];
 
+extern const char kMediaFoundationClearStrategyName[];
+extern const char kMediaFoundationClearStrategyDescription[];
+
 extern const char kRawAudioCaptureName[];
 extern const char kRawAudioCaptureDescription[];
 
