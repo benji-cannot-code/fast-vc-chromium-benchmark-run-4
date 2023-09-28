84803674390ec666ed384373daa2324442b9d82f
Jan Keitel <jkeitel@google.com>
2023-09-28 07:42:49 +0000

    Delay deletion of autocomplete entries.
    Bug: 1417187
    Change-Id: I931471e19218d5a13ebfdc8b5a02b297636363fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4872121
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1202398}

--- chrome/browser/ui/views/autofill/popup/popup_autocomplete_cell_view.h
+++ chrome/browser/ui/views/autofill/popup/popup_autocomplete_cell_view.h
@@ -89,7 +89,7 @@ class PopupAutocompleteCellView : public autofill::PopupCellView,
   void OnMouseExitedCellButton() override;
 
   void CreateDeleteButton();
-  void DeleteAutocompleteEntry();
+  void DeleteAutocompleteEntrySoon();
   void UpdateSelectedAndRunCallback(bool selected);
   void HandleKeyPressEventFocusOnButton();
   void HandleKeyPressEventFocusOnContent();
