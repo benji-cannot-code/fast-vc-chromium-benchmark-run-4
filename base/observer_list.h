4c82e8bc33b6e4cda511d88c22f3dec962254002
Lei Zhang <thestig@chromium.org>
2023-09-28 17:44:31 +0000

    Use the more specific cxx20_erase_$type.h include in observer_list.h
    Per guidance in base/containers/cxx20_erase.h, use cxx20_erase_vector.h
    inside observer_list.h to avoid transitively dragging all the STL
    container headers into all compilation units (~9,000) that include
    observer_list.h.
    
    Change-Id: I869b6bf5b1e0f659096325161711eda863c76b44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4898232
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202753}

--- base/observer_list.h
+++ base/observer_list.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/check_op.h"
-#include "base/containers/cxx20_erase.h"
+#include "base/containers/cxx20_erase_vector.h"
 #include "base/dcheck_is_on.h"
 #include "base/debug/dump_without_crashing.h"
 #include "base/notreached.h"
