7b00d864093fb591af851c0bb36636257e76cfb2
Curt Clemens <cclem@google.com>
2023-09-28 17:24:33 +0000

    [QuickStart] Handle optional UserVerificationMethod packet
    Android introduced a new UserVerificationMethod packet that may be sent,
    but only on newer versions of GMSCore. We need to gracefully handle
    either case where the packet may be present or may not.
    
    The TryDecodeData() method introduced here is a temporary stopgap to
    allow handling optional packets. We need this fix in place now to
    unblock bug bashes for the feature, but a larger refactor of
    QuickStartDecoder to allow doing this in a simpler way is in progress.
    
    Bug: b/294253611
    Test: Added unit tests, manual testing of QS thru Wifi creds
    Change-Id: Ic7c2585223b93f79f83c7dc2a7fe76a5603299f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4791723
    Reviewed-by: Brittany Hartmire <bhartmire@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Curt Clemens <cclem@google.com>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Cr-Commit-Position: refs/heads/main@{#1202737}

--- chrome/browser/nearby_sharing/public/cpp/nearby_connection.h
+++ chrome/browser/nearby_sharing/public/cpp/nearby_connection.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/functional/callback.h"
+#include "base/memory/weak_ptr.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 // A socket-like wrapper around Nearby Connections that allows for asynchronous
@@ -19,7 +20,10 @@ class NearbyConnection {
   using ReadCallback =
       base::OnceCallback<void(absl::optional<std::vector<uint8_t>> bytes)>;
 
-  virtual ~NearbyConnection() = default;
+  NearbyConnection();
+  NearbyConnection(const NearbyConnection&) = delete;
+  NearbyConnection& operator=(const NearbyConnection&) = delete;
+  virtual ~NearbyConnection();
 
   // Reads a stream of bytes from the remote device. Invoke |callback| when
   // there is incoming data or when the socket is closed. Previously set
@@ -39,6 +43,11 @@ class NearbyConnection {
   // closed. This object will be invalidated after |listener| is invoked.
   // Previously set listener will be replaced by |listener|.
   virtual void SetDisconnectionListener(base::OnceClosure listener) = 0;
+
+  base::WeakPtr<NearbyConnection> GetWeakPtr();
+
+ private:
+  base::WeakPtrFactory<NearbyConnection> weak_ptr_factory_{this};
 };
 
 #endif  // CHROME_BROWSER_NEARBY_SHARING_PUBLIC_CPP_NEARBY_CONNECTION_H_
