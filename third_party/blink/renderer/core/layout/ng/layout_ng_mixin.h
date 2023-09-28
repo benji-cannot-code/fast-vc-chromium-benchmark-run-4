574ff155a0378aaa9f865718da1a0a2d49a4cc85
Kent Tamura <tkent@chromium.org>
2023-09-28 07:19:59 +0000

    [RemoveLegacy] Move LayoutNGMixin::Paint to LayoutBlock
    and make sure it's not called for LayoutFlowThread.
    This CL has no behavior changes.
    
    Bug: 1229581
    Change-Id: Ida397da0722a401e392d62f62c58342f41a40470
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4899699
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202388}

--- third_party/blink/renderer/core/layout/ng/layout_ng_mixin.h
+++ third_party/blink/renderer/core/layout/ng/layout_ng_mixin.h
@@ -31,8 +31,6 @@ class LayoutNGMixin : public Base {
   explicit LayoutNGMixin(ContainerNode*);
   ~LayoutNGMixin() override;
 
-  void Paint(const PaintInfo&) const override;
-
   bool IsLayoutNGObject() const final;
 };
 
