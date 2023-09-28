0b504aee6eca025b48e34e5a2a9fce4b47f0bb34
Gordon Seto <gordonseto@google.com>
2023-09-28 23:58:01 +0000

    [CrOS Cellular] Route to APN subpage on invalid-apn notification click
    Update network_state_notifier to route to the APN subpage when the
    notification for `invalid-apn` connection error is clicked.
    
    Fixed: b:301106256
    Test: *NetworkStateNotifier*, *SystemTrayClientImpl*
    Change-Id: I71f7fdaa62f4da516259c074e3fecedc5acae4b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4902888
    Auto-Submit: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202957}

--- chrome/browser/ui/ash/network/network_state_notifier.cc
+++ chrome/browser/ui/ash/network/network_state_notifier.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "ash/constants/ash_features.h"
 #include "ash/constants/notifier_catalogs.h"
 #include "ash/public/cpp/notification_utils.h"
 #include "ash/public/cpp/system_tray_client.h"
@@ -576,6 +577,10 @@ void NetworkStateNotifier::ShowConnectErrorNotification(
   if (IsSimLockConnectionFailure(error_name, network)) {
     on_click = base::BindRepeating(&NetworkStateNotifier::ShowSimUnlockSettings,
                                    weak_ptr_factory_.GetWeakPtr());
+  } else if (features::IsApnRevampEnabled() && network &&
+             network->GetError() == shill::kErrorInvalidAPN) {
+    on_click = base::BindRepeating(&NetworkStateNotifier::ShowApnSettings,
+                                   weak_ptr_factory_.GetWeakPtr(), guid);
   } else {
     on_click = base::BindRepeating(&NetworkStateNotifier::ShowNetworkSettings,
                                    weak_ptr_factory_.GetWeakPtr(), guid);
@@ -628,6 +633,16 @@ void NetworkStateNotifier::ShowSimUnlockSettings() {
 
   NET_LOG(USER) << "Opening SIM unlock settings";
   system_tray_client_->ShowSettingsSimUnlock();
+}
+
+void NetworkStateNotifier::ShowApnSettings(const std::string& network_id) {
+  CHECK(features::IsApnRevampEnabled());
+  if (!system_tray_client_) {
+    return;
+  }
+
+  NET_LOG(USER) << "Opening APN subpage for network: " << network_id;
+  system_tray_client_->ShowApnSubpage(network_id);
 }
 
 void NetworkStateNotifier::ShowCarrierAccountDetail(
