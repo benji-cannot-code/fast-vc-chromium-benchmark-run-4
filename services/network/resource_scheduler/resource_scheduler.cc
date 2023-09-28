727a768fb4efabee503fea6451d14b78d631788b
Minoru Chikamune <chikamune@chromium.org>
2023-09-28 06:50:27 +0000

    MiracleParameter: Introduce MiracleParameter class
    This CL introduces MiracleParameter class, and uses it in
    ResourceScheduler as an example.
    
    Unlike FeatureParam, MiracleParameter determines the parameter value
    based on the amount of physical memory when the following suffix is used
    as a parameter name. If there are no parameter settings that have the
    following suffixes, MiracleParameter behaves the same as the
    FeatureParam.
    
     - "ForLessThan512MB" for less than 512MB memory devices.
     - "For512MBTo1GB" for 512MB to 1GB memory devices.
     - "For1GBTo2GB" for 1GB to 2GB memory devices.
     - "For2GBTo4GB" for 2GB to 4GB memory devices.
     - "For4GBTo8GB" for 4GB to 8GB memory devices.
     - "For8GBTo16GB" for 8GB to 16GB memory devices.
     - "For16GBAndAbove" for 16GB memory and above devices.
    
    The current implementation of MiracleParameter covers "Plan B" only.
    
    <To Reviewers>
    I borrowed many ideas from existing FeatureParam class to write
    MiracleParameter.
    
    <Google internal>
    https://docs.google.com/document/d/1wnAJuh68uyBleTUVadrUtZmkdTyz1eKWC1i6TPrx4f4/
    
    Bug: 1475915
    Change-Id: Icd521e2a534a648352fa3554330ce9c482852ffa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4812383
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202375}

--- services/network/resource_scheduler/resource_scheduler.cc
+++ services/network/resource_scheduler/resource_scheduler.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/tick_clock.h"
 #include "base/trace_event/trace_event.h"
 #include "base/unguessable_token.h"
+#include "components/miracle_parameter/common/public/miracle_parameter.h"
 #include "net/base/isolation_info.h"
 #include "net/base/load_flags.h"
 #include "net/base/request_priority.h"
@@ -107,11 +108,12 @@ BASE_FEATURE(kDelayablePriorityThresholdFeature,
              "DelayablePriorityThresholdFeature",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-const base::FeatureParam<net::RequestPriority>::Option kRequestPriorities[] = {
-    {net::LOWEST, "lowest"},
-    {net::LOW, "low"},
-    {net::MEDIUM, "medium"},
-    {net::HIGHEST, "highest"},
+constexpr base::FeatureParam<net::RequestPriority>::Option
+    kRequestPriorities[] = {
+        {net::LOWEST, "lowest"},
+        {net::LOW, "low"},
+        {net::MEDIUM, "medium"},
+        {net::HIGHEST, "highest"},
 };
 
 }  // namespace
@@ -119,17 +121,18 @@ const base::FeatureParam<net::RequestPriority>::Option kRequestPriorities[] = {
 // The maximum number of requests to allow be in-flight at any point in time per
 // host. This limit does not apply to hosts that support request prioritization
 // when |delay_requests_on_multiplexed_connections| is true.
-constexpr base::FeatureParam<int> kMaxNumDelayableRequestsPerHostPerClient(
-    &kMaxNumDelayableRequestsPerHostPerClientFeature,
-    "MaxNumDelayableRequestsPerHostPerClient",
-    6);
+constexpr miracle_parameter::MiracleParameter<int>
+    kMaxNumDelayableRequestsPerHostPerClient(
+        &kMaxNumDelayableRequestsPerHostPerClientFeature,
+        "MaxNumDelayableRequestsPerHostPerClient",
+        6);
 
 // The priority level below which resources are considered to be delayable.
-constexpr base::FeatureParam<net::RequestPriority> kDelayablePriorityThreshold(
-    &kDelayablePriorityThresholdFeature,
-    "DelayablePriorityThreshold",
-    net::MEDIUM,
-    &kRequestPriorities);
+constexpr miracle_parameter::MiracleParameter<net::RequestPriority>
+    kDelayablePriorityThreshold(&kDelayablePriorityThresholdFeature,
+                                "DelayablePriorityThreshold",
+                                net::MEDIUM,
+                                base::make_span(kRequestPriorities));
 
 // Returns the duration after which the timer to dispatch queued requests should
 // fire.
