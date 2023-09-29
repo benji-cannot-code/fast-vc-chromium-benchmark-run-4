fd3d22d33d13720fd636c1e3ea5238360ebb24cf
Toshiki Kikuchi <toshikikikuchi@google.com>
2023-09-29 03:28:49 +0000

    Respect app-requested orientation only when maximized/fullscreen
    This CL changes ScreenOrientationController to respect an app-requested
    orientation only when the window is maximized or fullscreen.
    When the window doesn’t fill the display (e.g., snapped, floated), we
    should not lock the display orientation per the app-requested
    orientation.
    The primary purpose of this CL is to ignore the app-requested
    orientation of floated windows, but in the first place we had better
    only respect maximized/fullscreen windows in an allowlist manner.
    
    BUG=b:300542469
    BUG=b:299574293
    TEST=ScreenOrientationControllerTest.IgnoreFloatWindowOrientationLock
    TEST=ScreenOrientationControllerTest.SplitViewPreventsLock
    
    Change-Id: I67570de031fb5b370a7f707c248ae25d89b35c69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4865407
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1203051}

--- ash/display/screen_orientation_controller.h
+++ ash/display/screen_orientation_controller.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_DISPLAY_SCREEN_ORIENTATION_CONTROLLER_H_
 #define ASH_DISPLAY_SCREEN_ORIENTATION_CONTROLLER_H_
 
+#include <memory>
 #include <unordered_map>
 
 #include "ash/accelerometer/accelerometer_reader.h"
@@ -45,7 +46,6 @@ class ASH_EXPORT ScreenOrientationController
       public AccelerometerReader::Observer,
       public WindowTreeHostManager::Observer,
       public TabletModeObserver,
-      public SplitViewObserver,
       public display::DisplayObserver {
  public:
   // Observer that reports changes to the state of ScreenOrientationProvider's
@@ -157,16 +157,13 @@ class ASH_EXPORT ScreenOrientationController
   void OnTabletModeEnded() override;
   void OnTabletPhysicalStateChanged() override;
 
-  // SplitViewObserver:
-  void OnSplitViewStateChanged(SplitViewController::State previous_state,
-                               SplitViewController::State state) override;
-
   // display::DisplayObserver:
   void OnWillProcessDisplayChanges() override;
   void OnDidProcessDisplayChanges() override;
 
  private:
   friend class ScreenOrientationControllerTestApi;
+  class WindowStateChangeNotifier;
 
   struct LockInfo {
     LockInfo(chromeos::OrientationType lock, aura::Window* root)
@@ -292,6 +289,8 @@ class ASH_EXPORT ScreenOrientationController
 
   // Register for DisplayObserver callbacks.
   display::ScopedDisplayObserver display_observer_{this};
+
+  std::unique_ptr<WindowStateChangeNotifier> window_state_change_notifier_;
 };
 
 }  // namespace ash
