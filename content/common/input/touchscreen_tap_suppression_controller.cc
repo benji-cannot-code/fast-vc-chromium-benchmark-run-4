1f95c87e7f6ec0452c9612dd95edef006381cb6c
Aman Verma <amanvr@google.com>
2023-09-29 12:00:15 +0000

    Move touchscreen_tap_suppression_controller to //content/common/input
    This refactoring is a part of bigger project to handle input on viz
    thread (go/input-on-viz). We are moving input related interfaces that
    RenderWidgetHostImpl uses to //content/common/input. This would provide
    a single stack in //content/common/input that we quickly enter in either
    the browser or GPU viz which would allow for relatively clean movement
    of code. GPU process can link in the same input stack and forward the
    data to the renderer using the same code.
    
    Bug: b/294828208
    Change-Id: Id079b776ce8fe8796af27fc9620bb7d387db3167
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4774207
    Commit-Queue: Aman Verma <amanvr@google.com>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1203187}

--- content/common/input/touchscreen_tap_suppression_controller.cc
+++ content/common/input/touchscreen_tap_suppression_controller.cc
@@ -3,11 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/browser/renderer_host/input/touchscreen_tap_suppression_controller.h"
+#include "content/common/input/touchscreen_tap_suppression_controller.h"
 
 #include <utility>
-
-#include "content/browser/renderer_host/input/gesture_event_queue.h"
 
 using blink::WebInputEvent;
 
