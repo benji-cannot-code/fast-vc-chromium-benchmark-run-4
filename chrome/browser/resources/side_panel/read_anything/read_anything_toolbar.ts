3a92dc92d9b9349873486a22c095e579d21627e9
Eitan Goldberger <eitang@google.com>
2023-09-29 21:36:21 +0000

    [Read aloud] Add pause button when preview is playing
    https://screenshot.googleplex.com/AbZ8zEf4RpgNof3
    
    Bug: 1474951
    Change-Id: I60525411af41f2822e1bedbc7154c82f0f1cbd6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905704
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Eitan Goldberger <eitang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1203455}

--- chrome/browser/resources/side_panel/read_anything/read_anything_toolbar.ts
+++ chrome/browser/resources/side_panel/read_anything/read_anything_toolbar.ts
@@ -37,6 +37,7 @@ export interface ReadAnythingToolbar {
 interface VoiceDropdown {
   voice: SpeechSynthesisVoice;
   selected: boolean;
+  previewPlaying: boolean;
 }
 
 interface MenuStateItem<T> {
@@ -371,6 +372,33 @@ export class ReadAnythingToolbar extends ReadAnythingToolbarBase {
     ReadAnythingToolbar.maybeUpdateMoreOptions(toolbar);
   }
 
+  showVoicePreviewPlaying(voice: SpeechSynthesisVoice|null) {
+    if (!voice) {
+      return;
+    }
+    this.voiceSelectionOptions_ = this.voiceSelectionOptions_.map(
+        ({data, ...rest}) => ({
+          ...rest,
+          data: {
+            voice: data.voice,
+            selected: data.selected,
+            previewPlaying: this.voicesAreEqual_(data.voice, voice),
+          },
+        }));
+  }
+
+  showVoicePreviewDone() {
+    this.voiceSelectionOptions_ =
+        this.voiceSelectionOptions_.map(({data, ...rest}) => ({
+                                          ...rest,
+                                          data: {
+                                            voice: data.voice,
+                                            selected: data.selected,
+                                            previewPlaying: false,
+                                          },
+                                        }));
+  }
+
   updateUiForPausing() {
     const shadowRoot = this.shadowRoot;
     assert(shadowRoot);
@@ -425,6 +453,7 @@ export class ReadAnythingToolbar extends ReadAnythingToolbarBase {
         voice1.localService === voice2.localService &&
         voice1.name === voice2.name && voice1.voiceURI === voice2.voiceURI;
   }
+
   // TODO(crbug.com/1474951): Add unit tests
   private onVoiceSelectionMenuClick_(event: MouseEvent) {
     if (this.contentPage) {
@@ -444,6 +473,7 @@ export class ReadAnythingToolbar extends ReadAnythingToolbarBase {
                                               selected: this.voicesAreEqual_(
                                                   selectedVoice,
                                                   speechSynthesisVoice),
+                                              previewPlaying: false,
                                             },
                                             callback: () => {},
                                           })),
@@ -534,6 +564,7 @@ export class ReadAnythingToolbar extends ReadAnythingToolbarBase {
             data: {
               voice: data.voice,
               selected: this.voicesAreEqual_(selectedVoice, data.voice),
+              previewPlaying: false,
             },
           }));
     }
