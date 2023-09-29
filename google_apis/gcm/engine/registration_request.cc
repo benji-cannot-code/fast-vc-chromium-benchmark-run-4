6d91956667a05e80f485eedaf2ad6861dc13a8bb
Rushan Suleymanov <rushans@google.com>
2023-09-29 05:56:13 +0000

    Add errors returned during InstanceID registration
    This CL also extends the following related histogram:
    InstanceID.GetToken.RequestStatus
    
    Bug: 1483887
    Change-Id: I41ac2cbbc296bb6c21eee9dcbf1d1f90d21e0e79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4887468
    Reviewed-by: Dmitry Titov <dimich@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1203083}

--- google_apis/gcm/engine/registration_request.cc
+++ google_apis/gcm/engine/registration_request.cc
@@ -49,9 +49,12 @@ const char kDeviceRegistrationError[] = "PHONE_REGISTRATION_ERROR";
 const char kAuthenticationFailed[] = "AUTHENTICATION_FAILED";
 const char kInvalidSender[] = "INVALID_SENDER";
 const char kInvalidParameters[] = "INVALID_PARAMETERS";
-const char kInternalServerError[] = "InternalServerError";
+const char kInternalServerError[] = "INTERNAL_SERVER_ERROR";
 const char kQuotaExceeded[] = "QUOTA_EXCEEDED";
 const char kTooManyRegistrations[] = "TOO_MANY_REGISTRATIONS";
+const char kTooManySubscribers[] = "TOO_MANY_SUBSCRIBERS";
+const char kInvalidTargetVersion[] = "INVALID_TARGET_VERSION";
+const char kFisAuthError[] = "FIS_AUTH_ERROR";
 
 // Gets correct status from the error message.
 RegistrationRequest::Status GetStatusFromError(const std::string& error) {
@@ -76,6 +79,15 @@ RegistrationRequest::Status GetStatusFromError(const std::string& error) {
   if (base::Contains(error, kTooManyRegistrations)) {
     return RegistrationRequest::TOO_MANY_REGISTRATIONS;
   }
+  if (base::Contains(error, kTooManySubscribers)) {
+    return RegistrationRequest::TOO_MANY_SUBSCRIBERS;
+  }
+  if (base::Contains(error, kInvalidTargetVersion)) {
+    return RegistrationRequest::INVALID_TARGET_VERSION;
+  }
+  if (base::Contains(error, kFisAuthError)) {
+    return RegistrationRequest::FIS_AUTH_ERROR;
+  }
   // Should not be reached, unless the server adds new error types.
   return RegistrationRequest::UNKNOWN_ERROR;
 }
@@ -91,6 +103,8 @@ bool ShouldRetryWithStatus(RegistrationRequest::Status status) {
     case RegistrationRequest::NO_RESPONSE_BODY:
     case RegistrationRequest::RESPONSE_PARSING_FAILED:
     case RegistrationRequest::INTERNAL_SERVER_ERROR:
+    case RegistrationRequest::TOO_MANY_SUBSCRIBERS:
+    case RegistrationRequest::FIS_AUTH_ERROR:
       return true;
     case RegistrationRequest::SUCCESS:
     case RegistrationRequest::INVALID_PARAMETERS:
@@ -98,6 +112,7 @@ bool ShouldRetryWithStatus(RegistrationRequest::Status status) {
     case RegistrationRequest::QUOTA_EXCEEDED:
     case RegistrationRequest::TOO_MANY_REGISTRATIONS:
     case RegistrationRequest::REACHED_MAX_RETRIES:
+    case RegistrationRequest::INVALID_TARGET_VERSION:
       return false;
   }
   return false;
