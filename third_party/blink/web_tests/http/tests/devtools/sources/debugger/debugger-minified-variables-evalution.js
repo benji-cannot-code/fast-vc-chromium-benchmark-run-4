baadff6838c1c12db595c4bee24c5e389c61bd12
Philip Pfaffe <pfaffe@chromium.org>
2023-09-29 11:40:53 +0000

    [devtools] fix sources web test
    Bug: chromium:1442410
    Change-Id: I65e64d2630217e45c921bb5e4d4877164fc32f62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4903787
    Auto-Submit: Philip Pfaffe <pfaffe@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1203184}

--- third_party/blink/web_tests/http/tests/devtools/sources/debugger/debugger-minified-variables-evalution.js
+++ third_party/blink/web_tests/http/tests/devtools/sources/debugger/debugger-minified-variables-evalution.js
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {TestRunner} from 'test_runner';
 import {SourcesTestRunner} from 'sources_test_runner';
 
+import * as SourceMapScopesModule from 'devtools/models/source_map_scopes/source_map_scopes.js';
 import * as SourcesModule from 'devtools/panels/sources/sources.js';
 import * as UIModule from 'devtools/ui/legacy/legacy.js';
 
@@ -46,7 +47,7 @@ import * as UIModule from 'devtools/ui/legacy/legacy.js';
   }
 
   function testAtPosition(uiSourceCode, position) {
-    return Sources.SourceMapNamesResolver
+    return SourceMapScopesModule.NamesResolver
         .resolveExpression(
             UIModule.Context.Context.instance().flavor(SDK.DebuggerModel.CallFrame), position.originText, uiSourceCode, position.line,
             position.startColumn, position.endColumn)
