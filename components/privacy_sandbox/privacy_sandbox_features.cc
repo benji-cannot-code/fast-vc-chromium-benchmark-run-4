2bfa23629ed94f77cbb541d878ae328c550a2eaf
Abe Boujane <boujane@google.com>
2023-09-29 12:29:00 +0000

    Don't show prompt if non normal browser. Also emit metrics
    Bug: b:302174476
    Change-Id: I6a0ef8c1f76d1d7aaa766b0cb9673b399b67f06d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4897641
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Auto-Submit: Abe Boujane <boujane@google.com>
    Commit-Queue: Abe Boujane <boujane@google.com>
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1203192}

--- components/privacy_sandbox/privacy_sandbox_features.cc
+++ components/privacy_sandbox/privacy_sandbox_features.cc
@@ -7,6 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace privacy_sandbox {
 
+// Show the Tracking Protection onboarding flow if not already onboarded.
+BASE_FEATURE(kPrivacySandboxSuppressDialogOnNonNormalBrowsers,
+             "PrivacySandboxSuppressDialogOnNonNormalBrowsers",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 BASE_FEATURE(kPrivacySandboxSettings4,
              "PrivacySandboxSettings4",
              base::FEATURE_ENABLED_BY_DEFAULT);
