16489c8c14e55d3eb5705463139c029e252ecfba
Xiaocheng Hu <xiaochengh@chromium.org>
2023-09-29 22:58:44 +0000

    Reland "[anchor-position] Support margin properties in position fallback"
    This reverts commit 426a7d8727ff4a2767fdd3cb22b5f091888aa39c.
    
    Reason for reland: ensured roundtrip for simple values
    
    - Reverted behavior (PS#1): check layout for all boxes
    - Old behavior (base): check layout for boxes with non-fixed style
      values
    - New behavior: check layout for boxes with either non-fixed style
      values or using position fallback style
    
    Original change's description:
    > Revert "[anchor-position] Support margin properties in position fallback"
    >
    > This reverts commit 502912cd04e83f9d1ce1d8df50fc9778b57207e0.
    >
    > Reason for revert: causing https://crbug.com/1485377
    >
    > Original change's description:
    > > [anchor-position] Support margin properties in position fallback
    > >
    > > This patch makes margin properties (except margin-trim) allowed in a
    > > @position-fallback rule, so that different fallback positions can use
    > > different margins.
    > >
    > > There's also some changes to `getComputedStyle()` implementation to
    > > make it return the used margin values from fallback styles. To do so,
    > > we unconditionally obtain the result from a `LayoutBox` if the current
    > > object is a box. Since the result is available only after layout, this
    > > patch also changes all margin properties on boxes to be layout
    > > dependent, regardless of the computed margin values.
    > >
    > > Note: This causes some rounding differences, as the result from
    > > `LayoutBox`, which is the actual layout precision, has a fixed-point
    > > precision of 1/64 px. Therefore, some tests are rebaselined.
    > >
    > > Bug: 1475317
    > > Change-Id: I4a3e242ceb04413db5348770b5cb666a54a1c9e0
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4819910
    > > Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    > > Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    > > Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1190240}
    >
    > Bug: 1475317, 1485377
    > Change-Id: Ieb200c75919ad7ad67745519791fcb9473c24e9b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4881060
    > Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    > Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1199388}
    
    Bug: 1475317, 1485377
    Change-Id: I95789a405a2e1bc71ec3b3819a8bb6c4ddd77ad1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4885150
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Auto-Submit: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1203515}

--- third_party/blink/renderer/core/css/properties/shorthands/shorthands_custom.cc
+++ third_party/blink/renderer/core/css/properties/shorthands/shorthands_custom.cc
@@ -2528,6 +2528,11 @@ bool MarginBlock::ParseShorthand(
       marginBlockShorthand(), important, context, range, properties);
 }
 
+bool MarginBlock::IsLayoutDependent(const ComputedStyle* style,
+                                    LayoutObject* layout_object) const {
+  return layout_object && layout_object->IsBox();
+}
+
 const CSSValue* MarginBlock::CSSValueFromComputedStyleInternal(
     const ComputedStyle& style,
     const LayoutObject* layout_object,
@@ -2551,7 +2556,8 @@ bool Margin::IsLayoutDependent(const ComputedStyle* style,
   return layout_object && layout_object->IsBox() &&
          (!style || !style->MarginBottom().IsFixed() ||
           !style->MarginTop().IsFixed() || !style->MarginLeft().IsFixed() ||
-          !style->MarginRight().IsFixed());
+          !style->MarginRight().IsFixed() ||
+          To<LayoutBox>(layout_object)->UsesPositionFallbackStyle());
 }
 
 const CSSValue* Margin::CSSValueFromComputedStyleInternal(
@@ -2570,6 +2576,11 @@ bool MarginInline::ParseShorthand(
     HeapVector<CSSPropertyValue, 64>& properties) const {
   return css_parsing_utils::ConsumeShorthandVia2Longhands(
       marginInlineShorthand(), important, context, range, properties);
+}
+
+bool MarginInline::IsLayoutDependent(const ComputedStyle* style,
+                                     LayoutObject* layout_object) const {
+  return layout_object && layout_object->IsBox();
 }
 
 const CSSValue* MarginInline::CSSValueFromComputedStyleInternal(
