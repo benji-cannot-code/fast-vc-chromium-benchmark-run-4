7d49057715db810f68fb8761eb41ddf71e4022ac
Tom Sepez <tsepez@chromium.org>
2023-09-29 17:42:29 +0000

    Remove IncrementPointerToMemberOperatorCountForTest() from raw_ptr Impls
    It is no longer called.
    
    Change-Id: Iecfe1936dc704f9bdb0202df83917ee381b62669
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4903304
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1203352}

--- base/allocator/partition_allocator/pointers/raw_ptr_hookable_impl.h
+++ base/allocator/partition_allocator/pointers/raw_ptr_hookable_impl.h
@@ -208,8 +208,6 @@ struct RawPtrHookableImpl {
   // This is for accounting only, used by unit tests.
   PA_ALWAYS_INLINE static constexpr void IncrementSwapCountForTest() {}
   PA_ALWAYS_INLINE static constexpr void IncrementLessCountForTest() {}
-  PA_ALWAYS_INLINE static constexpr void
-  IncrementPointerToMemberOperatorCountForTest() {}
 };
 
 }  // namespace base::internal
