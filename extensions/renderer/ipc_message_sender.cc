a32ba78f257dafcb83f9780a3ead6cb27020eb50
Dave Tapuska <dtapuska@chromium.org>
2023-09-29 15:35:08 +0000

    Bind ServiceWorker associated interfaces on Worker Thread
    The associated interfaces were previously bound on the IO thread
    now we can bind them directly on the WorkerThread.
    
    Bug: 1364183
    Change-Id: I5244c4ba7ab742f50e030c1fac97fcf15c1cfbcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4894951
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1203280}

--- extensions/renderer/ipc_message_sender.cc
+++ extensions/renderer/ipc_message_sender.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/renderer/extensions_renderer_client.h"
 #include "extensions/renderer/native_extension_bindings_system.h"
 #include "extensions/renderer/script_context.h"
+#include "extensions/renderer/service_worker_data.h"
 #include "extensions/renderer/trace_util.h"
 #include "extensions/renderer/worker_thread_dispatcher.h"
 #include "ipc/ipc_sync_channel.h"
@@ -346,7 +347,9 @@ class WorkerThreadIPCMessageSender : public IPCMessageSender {
     params->worker_thread_id = worker_thread_id;
     params->service_worker_version_id = service_worker_version_id_;
 
-    dispatcher_->RequestWorker(std::move(params));
+    WorkerThreadDispatcher::GetServiceWorkerData()
+        ->GetServiceWorkerHost()
+        ->RequestWorker(std::move(params));
   }
 
   void SendResponseAckIPC(ScriptContext* context,
@@ -355,7 +358,9 @@ class WorkerThreadIPCMessageSender : public IPCMessageSender {
     CHECK(context->IsForServiceWorker());
     CHECK_NE(kMainThreadId, content::WorkerThread::GetCurrentId());
 
-    dispatcher_->SendResponseAck(request_uuid);
+    WorkerThreadDispatcher::GetServiceWorkerData()
+        ->GetServiceWorkerHost()
+        ->WorkerResponseAck(request_uuid);
   }
 
   void SendAddUnfilteredEventListenerIPC(
@@ -366,10 +371,14 @@ class WorkerThreadIPCMessageSender : public IPCMessageSender {
     DCHECK_NE(blink::mojom::kInvalidServiceWorkerVersionId,
               context->service_worker_version_id());
 
-    dispatcher_->SendAddEventListener(
-        context->GetExtensionID(), context->service_worker_scope(), event_name,
-        context->service_worker_version_id(),
-        content::WorkerThread::GetCurrentId());
+    WorkerThreadDispatcher::GetServiceWorkerData()
+        ->GetEventRouter()
+        ->AddListenerForServiceWorker(
+            context->GetExtensionID(), event_name,
+            mojom::ServiceWorkerContext::New(
+                context->service_worker_scope(),
+                context->service_worker_version_id(),
+                content::WorkerThread::GetCurrentId()));
   }
 
   void SendRemoveUnfilteredEventListenerIPC(
@@ -380,10 +389,14 @@ class WorkerThreadIPCMessageSender : public IPCMessageSender {
     DCHECK_NE(blink::mojom::kInvalidServiceWorkerVersionId,
               context->service_worker_version_id());
 
-    dispatcher_->SendRemoveEventListener(
-        context->GetExtensionID(), context->service_worker_scope(), event_name,
-        context->service_worker_version_id(),
-        content::WorkerThread::GetCurrentId());
+    WorkerThreadDispatcher::GetServiceWorkerData()
+        ->GetEventRouter()
+        ->RemoveListenerForServiceWorker(
+            context->GetExtensionID(), event_name,
+            mojom::ServiceWorkerContext::New(
+                context->service_worker_scope(),
+                context->service_worker_version_id(),
+                content::WorkerThread::GetCurrentId()));
   }
 
   void SendAddUnfilteredLazyEventListenerIPC(
@@ -392,8 +405,11 @@ class WorkerThreadIPCMessageSender : public IPCMessageSender {
     DCHECK(context->IsForServiceWorker());
     DCHECK_NE(kMainThreadId, content::WorkerThread::GetCurrentId());
 
-    dispatcher_->SendAddEventLazyListener(
-        context->GetExtensionID(), context->service_worker_scope(), event_name);
+    WorkerThreadDispatcher::GetServiceWorkerData()
+        ->GetEventRouter()
+        ->AddLazyListenerForServiceWorker(context->GetExtensionID(),
+                                          context->service_worker_scope(),
+                                          event_name);
   }
 
   void SendRemoveUnfilteredLazyEventListenerIPC(
@@ -402,8 +418,11 @@ class WorkerThreadIPCMessageSender : public IPCMessageSender {
     DCHECK(context->IsForServiceWorker());
     DCHECK_NE(kMainThreadId, content::WorkerThread::GetCurrentId());
 
-    dispatcher_->SendRemoveEventLazyListener(
-        context->GetExtensionID(), context->service_worker_scope(), event_name);
+    WorkerThreadDispatcher::GetServiceWorkerData()
+        ->GetEventRouter()
+        ->RemoveLazyListenerForServiceWorker(context->GetExtensionID(),
+                                             context->service_worker_scope(),
+                                             event_name);
   }
 
   void SendAddFilteredEventListenerIPC(ScriptContext* context,
@@ -415,10 +434,15 @@ class WorkerThreadIPCMessageSender : public IPCMessageSender {
     DCHECK_NE(blink::mojom::kInvalidServiceWorkerVersionId,
               context->service_worker_version_id());
 
-    dispatcher_->SendAddEventFilteredListener(
-        context->GetExtensionID(), context->service_worker_scope(), event_name,
-        context->service_worker_version_id(),
-        content::WorkerThread::GetCurrentId(), filter.Clone(), is_lazy);
+    WorkerThreadDispatcher::GetServiceWorkerData()
+        ->GetEventRouter()
+        ->AddFilteredListenerForServiceWorker(
+            context->GetExtensionID(), event_name,
+            mojom::ServiceWorkerContext::New(
+                context->service_worker_scope(),
+                context->service_worker_version_id(),
+                content::WorkerThread::GetCurrentId()),
+            filter.Clone(), is_lazy);
   }
 
   void SendRemoveFilteredEventListenerIPC(ScriptContext* context,
@@ -430,11 +454,15 @@ class WorkerThreadIPCMessageSender : public IPCMessageSender {
     DCHECK_NE(blink::mojom::kInvalidServiceWorkerVersionId,
               context->service_worker_version_id());
 
-    dispatcher_->SendRemoveEventFilteredListener(
-        context->GetExtensionID(), context->service_worker_scope(), event_name,
-        context->service_worker_version_id(),
-        content::WorkerThread::GetCurrentId(), filter.Clone(),
-        remove_lazy_listener);
+    WorkerThreadDispatcher::GetServiceWorkerData()
+        ->GetEventRouter()
+        ->RemoveFilteredListenerForServiceWorker(
+            context->GetExtensionID(), event_name,
+            mojom::ServiceWorkerContext::New(
+                context->service_worker_scope(),
+                context->service_worker_version_id(),
+                content::WorkerThread::GetCurrentId()),
+            filter.Clone(), remove_lazy_listener);
   }
 
   void SendBindAutomationIPC(
@@ -442,7 +470,9 @@ class WorkerThreadIPCMessageSender : public IPCMessageSender {
       mojo::PendingAssociatedRemote<ax::mojom::Automation> pending_remote)
       override {
     CHECK(context->IsForServiceWorker());
-    dispatcher_->SendBindAutomation(std::move(pending_remote));
+    WorkerThreadDispatcher::GetServiceWorkerData()
+        ->GetAutomationRegistry()
+        ->BindAutomation(std::move(pending_remote));
   }
 
   void SendOpenMessageChannel(ScriptContext* script_context,
--- extensions/renderer/worker_thread_util.cc
+++ extensions/renderer/worker_thread_util.cc
@@ -7,29 +7,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/renderer/worker_thread.h"
 #include "extensions/common/constants.h"
-#include "third_party/abseil-cpp/absl/base/attributes.h"
+#include "extensions/renderer/service_worker_data.h"
+#include "extensions/renderer/worker_thread_dispatcher.h"
 #include "third_party/blink/public/web/modules/service_worker/web_service_worker_context_proxy.h"
 
 namespace extensions {
 namespace worker_thread_util {
 
-namespace {
-ABSL_CONST_INIT thread_local blink::WebServiceWorkerContextProxy*
-    worker_context_proxy = nullptr;
-}
-
 bool IsWorkerThread() {
   return content::WorkerThread::GetCurrentId() != kMainThreadId;
 }
 
-void SetWorkerContextProxy(blink::WebServiceWorkerContextProxy* context_proxy) {
-  worker_context_proxy = context_proxy;
-}
-
 bool HasWorkerContextProxyInteraction() {
   DCHECK(IsWorkerThread());
-  return worker_context_proxy &&
-         worker_context_proxy->IsWindowInteractionAllowed();
+  ServiceWorkerData* data = WorkerThreadDispatcher::GetServiceWorkerData();
+  return data && data->worker_context_proxy()->IsWindowInteractionAllowed();
 }
 
 }  // namespace worker_thread_util
