1caaa274ec418a12f996d734d9c5c0d6344cf2f2
Catherine Zhang <cgzhang@google.com>
2023-09-29 05:41:31 +0000

    Change from Observer to SensorObserver
    Bug: b/265357935
    Change-Id: Ib322d6902222de8762f76a43151913589665dd82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4904563
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Rob Schonberger <robsc@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Catherine Zhang <cgzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1203078}

--- chrome/browser/ui/webui/ash/sensor_info/sensor_page_handler.h
+++ chrome/browser/ui/webui/ash/sensor_info/sensor_page_handler.h
@@ -36,7 +36,8 @@ class SensorProvider;
 // 'OnSensorUpdated' function and in this function forward sensor updates to TS
 // side. SensorPageHandler is also inheriting sensor::mojom::PageHandler, so
 // also responsible of TS side to C++ side communications.
-class SensorPageHandler : public Observer, public sensor::mojom::PageHandler {
+class SensorPageHandler : public SensorObserver,
+                          public sensor::mojom::PageHandler {
  public:
   // Enum class for file open status.
   enum class State {
@@ -81,7 +82,7 @@ class SensorPageHandler : public Observer, public sensor::mojom::PageHandler {
   void StartRecordingUpdate() override;
   void StopRecordingUpdate() override;
 
-  // Observer
+  // SensorObserver:
   void OnSensorUpdated(const SensorUpdate& update) override;
 
  private:
