a838189a95d6743d5a491cb24d298882e6e393b1
Zaina Al-Mashni <zalmashni@google.com>
2023-09-29 14:33:55 +0000

    [iOS] Add flag for Incognito downloads warning
    Bug: 1487921
    Change-Id: Iaa7dea450fbed06f6e19b93da0f28a79e825a724
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905367
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1203225}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -1672,6 +1672,10 @@ const flags_ui::FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kAutofillEnableVirtualCardsDescription,
      flags_ui::kOsIos,
      FEATURE_VALUE_TYPE(autofill::features::kAutofillEnableVirtualCards)},
+    {"ios-incognito-downloads-warning",
+     flag_descriptions::kIOSIncognitoDownloadsWarningName,
+     flag_descriptions::kIOSIncognitoDownloadsWarningDescription,
+     flags_ui::kOsIos, FEATURE_VALUE_TYPE(kIOSIncognitoDownloadsWarning)},
 };
 
 bool SkipConditionalFeatureEntry(const flags_ui::FeatureEntry& entry) {
--- ios/chrome/browser/ui/download/features.mm
+++ ios/chrome/browser/ui/download/features.mm
@@ -16,3 +16,7 @@ BASE_FEATURE(kARKillSwitch,
 BASE_FEATURE(kVCardKillSwitch,
              "DownloadKillSwitchVcard",
              base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kIOSIncognitoDownloadsWarning,
+             "IOSIncognitoDownloadsWarning",
+             base::FEATURE_DISABLED_BY_DEFAULT);
