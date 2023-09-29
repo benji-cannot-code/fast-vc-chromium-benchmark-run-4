da6fd2c81bad935679c224230c514a988b325c6b
Nate Chapin <japhet@chromium.org>
2023-09-29 02:54:23 +0000

    Plumb an ExceptionState through UnderlyingSourceBase::Cancel
    This removes several places where we manually create an
    ExceptionState with a kUnknown ExceptionContextType because we
    hadn't received an ExceptionState.
    
    Change-Id: I27b8d7fb0d2079bf9a0968e296296cdbcf244d7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4892189
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1203026}

--- third_party/blink/renderer/modules/websockets/websocket_stream.cc
+++ third_party/blink/renderer/modules/websockets/websocket_stream.cc
@@ -44,7 +44,9 @@ class WebSocketStream::UnderlyingSource final : public UnderlyingSourceBase {
 
   // UnderlyingSourceBase implementation.
   ScriptPromise pull(ScriptState*) override;
-  ScriptPromise Cancel(ScriptState*, ScriptValue reason) override;
+  ScriptPromise Cancel(ScriptState*,
+                       ScriptValue reason,
+                       ExceptionState&) override;
 
   // API for WebSocketStream.
   void DidReceiveTextMessage(const String&);
@@ -125,10 +127,11 @@ ScriptPromise WebSocketStream::UnderlyingSource::pull(
 
 ScriptPromise WebSocketStream::UnderlyingSource::Cancel(
     ScriptState* script_state,
-    ScriptValue reason) {
+    ScriptValue reason,
+    ExceptionState& exception_state) {
   DVLOG(1) << "WebSocketStream::UnderlyingSource " << this << " Cancel()";
   closed_ = true;
-  creator_->CloseMaybeWithReason(reason);
+  creator_->CloseMaybeWithReason(reason, exception_state);
   return ScriptPromise::CastUndefined(script_state);
 }
 
@@ -215,7 +218,7 @@ ScriptPromise WebSocketStream::UnderlyingSink::close(
     ExceptionState& exception_state) {
   DVLOG(1) << "WebSocketStream::UnderlyingSink " << this << " close()";
   closed_ = true;
-  creator_->CloseWithUnspecifiedCode();
+  creator_->CloseWithUnspecifiedCode(exception_state);
   DCHECK(!close_resolver_);
   close_resolver_ = MakeGarbageCollected<ScriptPromiseResolver>(
       script_state, exception_state.GetContext());
@@ -229,7 +232,7 @@ ScriptPromise WebSocketStream::UnderlyingSink::abort(
   DVLOG(1) << "WebSocketStream::UnderlyingSink " << this << " abort()";
 
   closed_ = true;
-  creator_->CloseMaybeWithReason(reason);
+  creator_->CloseMaybeWithReason(reason, exception_state);
   return ScriptPromise::CastUndefined(script_state);
 }
 
@@ -667,12 +670,9 @@ void WebSocketStream::Connect(ScriptState* script_state,
 
 // If |maybe_reason| contains a valid code and reason, then closes with it,
 // otherwise closes with unspecified code and reason.
-void WebSocketStream::CloseMaybeWithReason(ScriptValue maybe_reason) {
+void WebSocketStream::CloseMaybeWithReason(ScriptValue maybe_reason,
+                                           ExceptionState& exception_state) {
   DVLOG(1) << "WebSocketStream " << this << " CloseMaybeWithReason()";
-
-  // Exceptions thrown here are ignored.
-  ExceptionState exception_state(script_state_->GetIsolate(),
-                                 ExceptionContextType::kUnknown, "", "");
   WebSocketCloseInfo* info = NativeValueTraits<WebSocketCloseInfo>::NativeValue(
       script_state_->GetIsolate(), maybe_reason.V8Value(), exception_state);
   if (!exception_state.HadException() && info->hasCode()) {
@@ -685,14 +685,12 @@ void WebSocketStream::CloseMaybeWithReason(ScriptValue maybe_reason) {
   if (exception_state.HadException()) {
     exception_state.ClearException();
   }
-  CloseWithUnspecifiedCode();
+  CloseWithUnspecifiedCode(exception_state);
 }
 
-void WebSocketStream::CloseWithUnspecifiedCode() {
+void WebSocketStream::CloseWithUnspecifiedCode(
+    ExceptionState& exception_state) {
   DVLOG(1) << "WebSocketStream " << this << " CloseWithUnspecifiedCode()";
-
-  ExceptionState exception_state(script_state_->GetIsolate(),
-                                 ExceptionContextType::kUnknown, "", "");
   CloseInternal(WebSocketChannel::kCloseEventCodeNotSpecified, String(),
                 exception_state);
   DCHECK(!exception_state.HadException());
