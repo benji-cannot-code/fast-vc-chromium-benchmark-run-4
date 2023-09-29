7e4f51342e72aca57b608ea4b6a22a26bd4232fc
Gayane Petrosyan <gayane@google.com>
2023-09-29 17:58:40 +0000

    [iOS] Disable tests failing in official builds #3
    Disable following test
    ForcedSigninTestCase/testSignOutFromAccountsOnThisDeviceSyncDisabled
    FirstRunTestCase/testUMAUncheckedAndCheckItAgain
    FirstRunTestCase/testUMAUncheckedWhenOpenedSecondTime
    FirstRunTestCase/testWithUMAUncheckedAndNoSignin
    FirstRunTestCase/testWithUMAUncheckedAndSignin
    
    Bug: 1487746, 1487756
    Change-Id: Id81e2347ff6264b0666a3ee3f54b2ff2c3bfcb15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4904763
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Owners-Override: Gayane Petrosyan <gayane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1203364}

--- ios/chrome/browser/ui/authentication/signin/forced_signin/forced_signin_egtest.mm
+++ ios/chrome/browser/ui/authentication/signin/forced_signin/forced_signin_egtest.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/strings/sys_string_conversions.h"
 #import "base/test/ios/wait_util.h"
 #import "base/time/time.h"
+#import "build/branding_buildflags.h"
 #import "components/policy/core/common/policy_loader_ios_constants.h"
 #import "components/policy/policy_constants.h"
 #import "components/signin/ios/browser/features.h"
@@ -495,7 +496,15 @@ void CompleteSigninFlow() {
 }
 
 // Tests signing out account from accounts on this device with sync disabled.
-- (void)testSignOutFromAccountsOnThisDeviceSyncDisabled {
+// TODO(crbug.com/1487746): Test fails on official builds.
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#define MAYBE_testSignOutFromAccountsOnThisDeviceSyncDisabled \
+  DISABLED_testSignOutFromAccountsOnThisDeviceSyncDisabled
+#else
+#define MAYBE_testSignOutFromAccountsOnThisDeviceSyncDisabled \
+  testSignOutFromAccountsOnThisDeviceSyncDisabled
+#endif
+- (void)MAYBE_testSignOutFromAccountsOnThisDeviceSyncDisabled {
   // Add account.
   FakeSystemIdentity* fakeIdentity1 = [FakeSystemIdentity fakeIdentity1];
   [SigninEarlGrey addFakeIdentity:fakeIdentity1];
