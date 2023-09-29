0e406381c26d5f31789fd8406f2e5362e8bc3128
Thomas Lukaszewicz <tluk@chromium.org>
2023-09-29 01:21:41 +0000

    [tab strip] Make tab strip controls paint opaque for custom images
    This CL aligns the default behavior of TabStripControlButtons to
    paint opaque by default. This is necessary to cover the case where
    a custom theme background can result in poor NTB visibility.
    
    Bug: 1483984
    Change-Id: Ic77c8cc0eaefc149de8b44388372326420784825
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4894967
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202983}

--- chrome/browser/ui/views/tabs/tab_organization_button.cc
+++ chrome/browser/ui/views/tabs/tab_organization_button.cc
@@ -40,7 +40,7 @@ TabOrganizationButton::TabOrganizationButton(TabStrip* tab_strip,
   UpdateBackgroundFrameInactiveColorId(
       kColorNewTabButtonCRBackgroundFrameInactive);
 
-  SetPaintTransparentForCustomImageTheme(false);
+  set_paint_transparent_for_custom_image_theme(false);
 
   UpdateColors();
 }
