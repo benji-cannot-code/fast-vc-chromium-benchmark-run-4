8f82944d3d427eb108af73c03bfacb00ca612e4d
Eitan Goldberger <eitang@google.com>
2023-09-29 03:33:36 +0000

    [Read aloud] Add generics to MenuStateItem.
    This allows us to add more complex types to the UI with type checking, as demonstrated in the CL following this one.
    
    Bug: 1474951
    Change-Id: I2eb60b8d66d0aa63e1330ef0d8657755a26f8615
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4902266
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Commit-Queue: Eitan Goldberger <eitang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1203053}

--- chrome/browser/resources/side_panel/read_anything/read_anything_toolbar.ts
+++ chrome/browser/resources/side_panel/read_anything/read_anything_toolbar.ts
@@ -34,10 +34,10 @@ export interface ReadAnythingToolbar {
   };
 }
 
-interface MenuStateItem {
+interface MenuStateItem<T> {
   title: string;
   icon: string;
-  data: any;
+  data: T;
   callback: () => void;
 }
 
@@ -150,7 +150,7 @@ export class ReadAnythingToolbar extends ReadAnythingToolbarBase {
   // If you change these fonts, please also update read_anything_constants.h
   private fontOptions_: string[] = [];
 
-  private letterSpacingOptions_: MenuStateItem[] = [
+  private letterSpacingOptions_: Array<MenuStateItem<number>> = [
     {
       title: loadTimeData.getString('letterSpacingStandardTitle'),
       icon: 'read-anything:letter-spacing-standard',
@@ -174,7 +174,7 @@ export class ReadAnythingToolbar extends ReadAnythingToolbarBase {
     },
   ];
 
-  private lineSpacingOptions_: MenuStateItem[] = [
+  private lineSpacingOptions_: Array<MenuStateItem<number>> = [
     {
       title: loadTimeData.getString('lineSpacingStandardTitle'),
       icon: 'read-anything:line-spacing-standard',
@@ -198,7 +198,7 @@ export class ReadAnythingToolbar extends ReadAnythingToolbarBase {
     },
   ];
 
-  private colorOptions_: MenuStateItem[] = [
+  private colorOptions_: Array<MenuStateItem<string>> = [
     {
       title: loadTimeData.getString('defaultColorTitle'),
       icon: 'read-anything:default-theme',
@@ -231,7 +231,8 @@ export class ReadAnythingToolbar extends ReadAnythingToolbarBase {
     },
   ];
 
-  private voiceSelectionOptions_: MenuStateItem[] = [];
+  private voiceSelectionOptions_: Array<MenuStateItem<SpeechSynthesisVoice>> =
+      [];
 
   private rateOptions_: number[] = [0.5, 0.8, 1, 1.2, 1.5, 2, 3, 4];
 
@@ -334,8 +335,8 @@ export class ReadAnythingToolbar extends ReadAnythingToolbarBase {
             parseFloat(chrome.readingMode.letterSpacing.toFixed(2))));
   }
 
-  private getIndexOfSetting_(menuArray: MenuStateItem[], dataToFind: any):
-      number {
+  private getIndexOfSetting_(
+      menuArray: Array<MenuStateItem<any>>, dataToFind: any): number {
     return menuArray.findIndex((item) => (item.data === dataToFind));
   }
 
@@ -416,15 +417,17 @@ export class ReadAnythingToolbar extends ReadAnythingToolbarBase {
     if (this.contentPage) {
       const voices = this.contentPage.getVoices();
       this.voiceSelectionOptions_ = Object.entries(voices).reduce(
-          (aggregateVoiceList: MenuStateItem[], [_, voiceListForLang]) => ([
-            ...aggregateVoiceList,
-            ...(voiceListForLang).map(speechSynthesisVoice => ({
-                                        title: speechSynthesisVoice.name,
-                                        icon: '',
-                                        data: speechSynthesisVoice,
-                                        callback: () => {},
-                                      })),
-          ]),
+          (aggregateVoiceList: Array<MenuStateItem<SpeechSynthesisVoice>>,
+           [_, voiceListForLang]) =>
+              ([
+                ...aggregateVoiceList,
+                ...(voiceListForLang).map(speechSynthesisVoice => ({
+                                            title: speechSynthesisVoice.name,
+                                            icon: '',
+                                            data: speechSynthesisVoice,
+                                            callback: () => {},
+                                          })),
+              ]),
           []);
 
       this.openMenu_(this.$.voiceSelectionMenu, event.target as HTMLElement);
@@ -479,35 +482,36 @@ export class ReadAnythingToolbar extends ReadAnythingToolbarBase {
     }
   }
 
-  private onLetterSpacingClick_(event: DomRepeatEvent<MenuStateItem>) {
+  private onLetterSpacingClick_(event: DomRepeatEvent<MenuStateItem<number>>) {
     this.onTextStyleClick_(
         event, ReadAnythingSettingsChange.LETTER_SPACING_CHANGE,
         this.$.letterSpacingMenu,
         ReadAnythingElement.prototype.updateLetterSpacing);
   }
 
-  private onLineSpacingClick_(event: DomRepeatEvent<MenuStateItem>) {
+  private onLineSpacingClick_(event: DomRepeatEvent<MenuStateItem<number>>) {
     this.onTextStyleClick_(
         event, ReadAnythingSettingsChange.LINE_HEIGHT_CHANGE,
         this.$.lineSpacingMenu,
         ReadAnythingElement.prototype.updateLineSpacing);
   }
 
-  private onColorClick_(event: DomRepeatEvent<MenuStateItem>) {
+  private onColorClick_(event: DomRepeatEvent<MenuStateItem<string>>) {
     this.onTextStyleClick_(
         event, ReadAnythingSettingsChange.THEME_CHANGE, this.$.colorMenu,
         ReadAnythingElement.prototype.updateThemeFromWebUi);
   }
 
-  private onVoiceSelectClick_(event: DomRepeatEvent<MenuStateItem>) {
+  private onVoiceSelectClick_(
+      event: DomRepeatEvent<MenuStateItem<SpeechSynthesisVoice>>) {
     // TODO(crbug.com/1474951): Save voice to prefs.
     if (this.contentPage) {
-      this.contentPage.setSpeechSynthesisVoice(
-          event.model.item.data as SpeechSynthesisVoice);
+      this.contentPage.setSpeechSynthesisVoice(event.model.item.data);
     }
   }
 
-  private onVoicePreviewClick_(event: DomRepeatEvent<MenuStateItem>) {
+  private onVoicePreviewClick_(
+      event: DomRepeatEvent<MenuStateItem<SpeechSynthesisVoice>>) {
     // Because the preview button is layered onto the voice-selection button,
     // the onVoiceSelectClick_() listener is also subscribed to this event. This
     // line is to make sure that the voice-selection callback is not triggered.
@@ -520,8 +524,8 @@ export class ReadAnythingToolbar extends ReadAnythingToolbarBase {
   }
 
   private onTextStyleClick_(
-      event: DomRepeatEvent<MenuStateItem>, logVal: ReadAnythingSettingsChange,
-      menuClicked: CrActionMenuElement,
+      event: DomRepeatEvent<MenuStateItem<any>>,
+      logVal: ReadAnythingSettingsChange, menuClicked: CrActionMenuElement,
       contentPageCallback: ((data: any) => void)) {
     event.model.item.callback();
     chrome.metricsPrivate.recordEnumerationValue(
