a6b33f804623d7a4d704ec3ecc2984705cc2370f
Sky Malice <skym@chromium.org>
2023-09-29 22:28:22 +0000

    Remove unnecessary ShadowPostTask.
    Bug: 1487638
    Change-Id: I547020e43226e92de179e0f15cff6aab5cb2a5f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4901444
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1203503}

--- chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/ScrollTrackerTest.java
+++ chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/ScrollTrackerTest.java
@@ -11,10 +11,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
-import org.robolectric.annotation.Config;
 import org.robolectric.annotation.LooperMode;
 
-import org.chromium.base.task.test.ShadowPostTask;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 
 import java.util.ArrayList;
@@ -23,7 +21,6 @@ import java.util.concurrent.TimeUnit;
 
 /** Tests for {@link ScrollTracker}. */
 @RunWith(BaseRobolectricTestRunner.class)
-@Config(shadows = {ShadowPostTask.class})
 @LooperMode(LooperMode.Mode.LEGACY)
 public final class ScrollTrackerTest {
     // The delay time used when scheduling the report task.
