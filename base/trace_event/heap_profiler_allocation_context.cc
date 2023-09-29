b7397e936e2988e436c8e9a5a7612f8cd7e56c4f
Lei Zhang <thestig@chromium.org>
2023-09-29 19:41:35 +0000

    Convert a base::PersistentHash() caller to use the span version
    Switch a call from the (pointer + size) version of PersistentHash() to
    the span version.
    
    Change-Id: I31a93f8931e027a275f5dbabd50a0225d04e64db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4903051
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1203406}

--- base/trace_event/heap_profiler_allocation_context.cc
+++ base/trace_event/heap_profiler_allocation_context.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <cstring>
 
+#include "base/containers/span.h"
 #include "base/hash/hash.h"
 
 namespace base {
@@ -68,7 +69,8 @@ size_t hash<Backtrace>::operator()(const Backtrace& backtrace) const {
   for (size_t i = 0; i != backtrace.frame_count; ++i) {
     values[i] = backtrace.frames[i].value;
   }
-  return base::PersistentHash(values, backtrace.frame_count * sizeof(*values));
+  auto values_span = base::make_span(values).first(backtrace.frame_count);
+  return base::PersistentHash(base::as_bytes(values_span));
 }
 
 size_t hash<AllocationContext>::operator()(const AllocationContext& ctx) const {
