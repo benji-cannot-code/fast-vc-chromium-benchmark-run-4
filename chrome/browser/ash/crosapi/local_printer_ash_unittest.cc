1e25bd0e5bcf9f58f1003432267da390ca71d4c6
Alan Screen <awscreen@chromium.org>
2023-09-29 19:40:23 +0000

    Correct missing PrintBackendServiceManager client registrations
    Move the check for registered clients in GetService() to be the first
    thing this method does.  This allows tests to expose other paths that
    are otherwise missing registrations with PrintBackendServiceManager.
    Fill those in to correct usage with PrintBackendServiceManager.
    
    Bug: b/301590015
    Change-Id: I7401141bb41c0abd734470564b05a44d2167cbb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4898532
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Benjamin Gordon <bmgordon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1203405}

--- chrome/browser/ash/crosapi/local_printer_ash_unittest.cc
+++ chrome/browser/ash/crosapi/local_printer_ash_unittest.cc
@@ -285,6 +285,11 @@ class LocalPrinterAshTestBase : public testing::Test {
                 unsandboxed_test_remote_, unsandboxed_test_backend_,
                 /*sandboxed=*/false);
       }
+
+      // Client registration is normally covered by `PrintPreviewUI`, so mimic
+      // that here.
+      service_manager_client_id_ =
+          PrintBackendServiceManager::GetInstance().RegisterQueryClient();
 #else
       NOTREACHED();
 #endif  // BUILDFLAG(ENABLE_OOP_PRINTING)
@@ -297,6 +302,11 @@ class LocalPrinterAshTestBase : public testing::Test {
 
   void TearDown() override {
 #if BUILDFLAG(ENABLE_OOP_PRINTING)
+    if (UseService()) {
+      PrintBackendServiceManager::GetInstance().UnregisterClient(
+          service_manager_client_id_);
+    }
+
     PrintBackendServiceManager::ResetForTesting();
 #endif
   }
@@ -386,6 +396,7 @@ class LocalPrinterAshTestBase : public testing::Test {
   std::unique_ptr<PrintBackendServiceTestImpl> sandboxed_print_backend_service_;
   std::unique_ptr<PrintBackendServiceTestImpl>
       unsandboxed_print_backend_service_;
+  PrintBackendServiceManager::ClientId service_manager_client_id_;
 #endif  // BUILDFLAG(ENABLE_OOP_PRINTING)
 };
 
--- chrome/browser/ui/webui/print_preview/local_printer_handler_default_unittest.cc
+++ chrome/browser/ui/webui/print_preview/local_printer_handler_default_unittest.cc
@@ -258,6 +258,11 @@ class LocalPrinterHandlerDefaultTestBase : public testing::Test {
                 /*sandboxed=*/false);
       }
 #endif  // BUILDFLAG(IS_WIN)
+
+      // Client registration is normally covered by `PrintPreviewUI`, so mimic
+      // that here.
+      service_manager_client_id_ =
+          PrintBackendServiceManager::GetInstance().RegisterQueryClient();
 #else
       NOTREACHED();
 #endif  // BUILDFLAG(ENABLE_OOP_PRINTING)
@@ -273,6 +278,8 @@ class LocalPrinterHandlerDefaultTestBase : public testing::Test {
 
   void TearDown() override {
     if (UseService()) {
+      PrintBackendServiceManager::GetInstance().UnregisterClient(
+          service_manager_client_id_);
 #if BUILDFLAG(IS_WIN)
       service_task_runner_->DeleteSoon(
           FROM_HERE, std::move(sandboxed_print_backend_service_));
@@ -419,6 +426,7 @@ class LocalPrinterHandlerDefaultTestBase : public testing::Test {
   std::unique_ptr<PrintBackendServiceTestImpl> sandboxed_print_backend_service_;
   std::unique_ptr<PrintBackendServiceTestImpl>
       unsandboxed_print_backend_service_;
+  PrintBackendServiceManager::ClientId service_manager_client_id_;
 
 #if BUILDFLAG(IS_WIN)
   scoped_refptr<base::SingleThreadTaskRunner> service_task_runner_;
