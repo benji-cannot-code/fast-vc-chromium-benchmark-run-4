9b3a4e5e906ca95c799e902927b864b7af0932e7
Alexis Hetu <sugoi@chromium.org>
2023-09-29 19:51:48 +0000

    [iOS] Move the allocation of the manager to PasswordDetailsMediator
    The IOSChromePasswordCheckManager object can be conditionally
    created based on which type of context is used to create the
    PasswordDetailsMediator object. This CL prepares the code for
    potential optimizations by accomplishing 2 things:
    - The creation of the manager is now done within the
      PasswordDetailsMediator, so that it can now control
      when and if to create it.
    - The "GetInsecureCredentials()" call is now conditional on
      whether the results of this call will actually be used.
    
    Bug: 1422351
    Change-Id: Ib2641011d5591e88f1d01dee14045b5d90c165f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4903703
    Reviewed-by: Veronique Nguyen <veronguyen@google.com>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1203413}

--- ios/chrome/browser/ui/settings/password/password_details/password_details_coordinator.mm
+++ ios/chrome/browser/ui/settings/password/password_details/password_details_coordinator.mm
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/prefs/pref_service.h"
 #import "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/credential_provider_promo/features.h"
-#import "ios/chrome/browser/passwords/ios_chrome_password_check_manager.h"
-#import "ios/chrome/browser/passwords/ios_chrome_password_check_manager_factory.h"
 #import "ios/chrome/browser/passwords/password_tab_helper.h"
 #import "ios/chrome/browser/shared/coordinator/alert/action_sheet_coordinator.h"
 #import "ios/chrome/browser/shared/coordinator/alert/alert_coordinator.h"
@@ -34,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/public/commands/credential_provider_promo_commands.h"
 #import "ios/chrome/browser/shared/public/commands/snackbar_commands.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
-#import "ios/chrome/browser/sync/model/sync_service_factory.h"
 #import "ios/chrome/browser/ui/settings/password/password_details/password_details.h"
 #import "ios/chrome/browser/ui/settings/password/password_details/password_details_consumer.h"
 #import "ios/chrome/browser/ui/settings/password/password_details/password_details_coordinator_delegate.h"
@@ -164,16 +161,12 @@ using password_manager::features::IsAuthOnEntryV2Enabled;
   }
 
   ChromeBrowserState* browserState = self.browser->GetBrowserState();
-  self.mediator = [[PasswordDetailsMediator alloc]
-         initWithPasswords:credentials
-               displayName:displayName
-      passwordCheckManager:IOSChromePasswordCheckManagerFactory::
-                               GetForBrowserState(browserState)
-                                   .get()
-               prefService:browserState->GetPrefs()
-               syncService:SyncServiceFactory::GetForBrowserState(browserState)
-                   context:_context
-                  delegate:self];
+  self.mediator =
+      [[PasswordDetailsMediator alloc] initWithPasswords:credentials
+                                             displayName:displayName
+                                            browserState:browserState
+                                                 context:_context
+                                                delegate:self];
   self.mediator.consumer = self.viewController;
   self.viewController.handler = self;
   self.viewController.delegate = self.mediator;
