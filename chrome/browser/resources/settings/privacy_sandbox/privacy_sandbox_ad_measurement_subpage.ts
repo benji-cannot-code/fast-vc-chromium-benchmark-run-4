8ceaa1a2be02de340aaaf92a59d99fc00aed19e5
Kevin Graney <kmg@google.com>
2023-09-30 00:05:08 +0000

    Move privacy sandbox advertising settings pages
    This refactoring moves more of the privacy sandbox settings pages into a
    single directory structure.
    
    Bug: b:302703189
    Change-Id: I05f761daa54d25859fb14267ff4b785513255843
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4899877
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Kevin Graney <kmg@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1203552}

--- chrome/browser/resources/settings/privacy_sandbox/privacy_sandbox_ad_measurement_subpage.ts
+++ chrome/browser/resources/settings/privacy_sandbox/privacy_sandbox_ad_measurement_subpage.ts
@@ -5,14 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_components/settings_prefs/prefs.js';
 import '/shared/settings/controls/settings_toggle_button.js';
-import '../../settings_shared.css.js';
+import '../settings_shared.css.js';
 
 import {SettingsToggleButtonElement} from '/shared/settings/controls/settings_toggle_button.js';
 import {PrefsMixin} from 'chrome://resources/cr_components/settings_prefs/prefs_mixin.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
-import {HatsBrowserProxyImpl, TrustSafetyInteraction} from '../../hats_browser_proxy.js';
-import {MetricsBrowserProxy, MetricsBrowserProxyImpl} from '../../metrics_browser_proxy.js';
+import {HatsBrowserProxyImpl, TrustSafetyInteraction} from '../hats_browser_proxy.js';
+import {MetricsBrowserProxy, MetricsBrowserProxyImpl} from '../metrics_browser_proxy.js';
 
 import {getTemplate} from './privacy_sandbox_ad_measurement_subpage.html.js';
 
