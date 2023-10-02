8c624ac73bda140e83791c6db59ffe06fe8651f8
Sandor Major <sandormajor@google.com>
2023-10-02 20:50:29 +0000

    Rename First-Party Sets Overrides Pref's const
    kFirstPartySetsOverrides is renamed to kRelatedWebsiteSetsOverrides.
    The Pref's string name stays the same to avoid migration issues of a
    service Pref.
    
    Bug: 1480551
    Change-Id: Ibcedc904760f84bb3f1e87f3a4697fb1e167e6a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4903119
    Commit-Queue: Sandor «Alex» Major <sandormajor@google.com>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204250}

--- chrome/browser/first_party_sets/first_party_sets_pref_names.h
+++ chrome/browser/first_party_sets/first_party_sets_pref_names.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace first_party_sets {
 
 // Add Profile prefs below.
-extern const char kFirstPartySetsOverrides[];
+extern const char kRelatedWebsiteSetsOverrides[];
 
 }  // namespace first_party_sets
 
