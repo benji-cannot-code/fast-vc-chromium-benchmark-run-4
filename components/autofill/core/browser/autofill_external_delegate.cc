568ebc48a096af9977874c415c3e0dcf1390d767
Timofey Chudakov <tchudakov@google.com>
2023-10-02 15:28:43 +0000

    [mullet m2] Implement the delete dialog metric.
    The metric is logged when user closes the delete address
    profile dialog. Design doc: go/autofill-metrics-redesign.
    
    Bug: 1459990
    Change-Id: I9843fd814de5080da6b3a19d03add8cce70d273f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4907015
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1204027}

--- components/autofill/core/browser/autofill_external_delegate.cc
+++ components/autofill/core/browser/autofill_external_delegate.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/browser_autofill_manager.h"
 #include "components/autofill/core/browser/metrics/address_rewriter_in_profile_subset_metrics.h"
 #include "components/autofill/core/browser/metrics/autofill_metrics.h"
+#include "components/autofill/core/browser/metrics/granular_filling_metrics.h"
 #include "components/autofill/core/browser/metrics/suggestions_list_metrics.h"
 #include "components/autofill/core/browser/ui/popup_item_ids.h"
 #include "components/autofill/core/common/autofill_features.h"
@@ -604,6 +605,7 @@ void AutofillExternalDelegate::OnAddressEditorClosed(
 
 void AutofillExternalDelegate::OnDeleteDialogClosed(const std::string& guid,
                                                     bool user_accepted_delete) {
+  autofill_metrics::LogDeleteAddressProfileDialogClosed(user_accepted_delete);
   if (user_accepted_delete) {
     PersonalDataManager* pdm = manager_->client().GetPersonalDataManager();
     if (!pdm_observation_.IsObserving()) {
