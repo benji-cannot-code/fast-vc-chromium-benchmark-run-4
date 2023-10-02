85ad3289fb1d6d0e4976432b72d68507d227a60e
Mohamed Amir Yosef <mamir@chromium.org>
2023-10-02 10:21:45 +0000

    [Passwords] Auto-approve updates to shared passwords from same sender
    This CL add logic behind a feature flag that when enabled, updates
    to shared passwords from the same sender are auto-approved.
    
    Bug: 1448235
    Change-Id: Id43dc1b5b97a75cb3d928d902cab0795ec2ccfc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4898543
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1203917}

--- components/password_manager/core/browser/features/password_features.cc
+++ components/password_manager/core/browser/features/password_features.cc
@@ -7,6 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace password_manager::features {
 
+// When enabled, updates to shared existing passwords from the same sender are
+// auto-approved.
+BASE_FEATURE(kAutoApproveSharedPasswordUpdatesFromSameSender,
+             "AutoApproveSharedPasswordUpdatesFromSameSender",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Enables Biometrics for the Touch To Fill feature. This only effects Android.
 BASE_FEATURE(kBiometricTouchToFill,
              "BiometricTouchToFill",
