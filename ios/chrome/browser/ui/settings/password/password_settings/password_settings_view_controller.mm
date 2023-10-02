ace4e22df8e2a21c694ae0d4022548809d448227
Cheick Cisse <cheickcisse@google.com>
2023-10-02 20:42:29 +0000

    Fix accessibility bug for bulk upload local passwords.
    This CL fixes the accessibility bug that reads ‘Save Password in Account’ button under Passwords settings screen as button and link
    
    Bug: 1487558
    Change-Id: Idc8326bf57a4417037aa04dd03e7c772fe71905a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4908108
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1204241}

--- ios/chrome/browser/ui/settings/password/password_settings/password_settings_view_controller.mm
+++ ios/chrome/browser/ui/settings/password/password_settings/password_settings_view_controller.mm
@@ -457,8 +457,6 @@ typedef NS_ENUM(NSInteger, ModelLoadStatus) {
       UIAccessibilityTraitButton;
   _bulkMovePasswordsToAccountButtonItem.accessibilityIdentifier =
       kPasswordSettingsBulkMovePasswordsToAccountButtonTableViewId;
-  _bulkMovePasswordsToAccountButtonItem.accessibilityTraits |=
-      UIAccessibilityTraitLink;
   return _bulkMovePasswordsToAccountButtonItem;
 }
 
