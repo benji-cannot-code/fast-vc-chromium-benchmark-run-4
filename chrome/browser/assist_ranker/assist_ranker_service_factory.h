520331682ddca65879c11ccd307042d2fc161a74
Arthur Milchior <arthurmilchior@chromium.org>
2023-10-02 12:21:12 +0000

    [Assist ranker] Implements the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one being deprecated,
    this CL replaces it by the first one.
    
    Bug: 1396138
    Change-Id: Ida6d6b8d9232145a25cfb8c72a7bac1969d1cb63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798701
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1203957}

--- chrome/browser/assist_ranker/assist_ranker_service_factory.h
+++ chrome/browser/assist_ranker/assist_ranker_service_factory.h
@@ -36,7 +36,7 @@ class AssistRankerServiceFactory : public ProfileKeyedServiceFactory {
   ~AssistRankerServiceFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
