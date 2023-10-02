d494e047b35d0e8f35f3c95ab84f81c7801ca6bf
Tao Wu <wutao@chromium.org>
2023-10-02 22:41:00 +0000

    assistant: Move launcher searcch iph view
    `LauncherSearchIphView` will be used in launcher and assistant UI.
    Currently the `ash/app_list` can deps on `ash/assistant/ui`, so move
    `LauncherSearchIphView` to `ash/assistant/ui`.
    
    No code and functional changes.
    
    Bug: b/303112470
    Test: passed tests and manually check it still works
    Change-Id: I92ac28b598fbee931c44354ea0c6392f69e77bd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905828
    Reviewed-by: Yuki Awano <yawano@google.com>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204323}

--- ash/assistant/ui/main_stage/launcher_search_iph_view.h
+++ ash/assistant/ui/main_stage/launcher_search_iph_view.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_APP_LIST_VIEWS_LAUNCHER_SEARCH_IPH_VIEW_H_
-#define ASH_APP_LIST_VIEWS_LAUNCHER_SEARCH_IPH_VIEW_H_
+#ifndef ASH_ASSISTANT_UI_MAIN_STAGE_LAUNCHER_SEARCH_IPH_VIEW_H_
+#define ASH_ASSISTANT_UI_MAIN_STAGE_LAUNCHER_SEARCH_IPH_VIEW_H_
 
 #include <memory>
 #include <string>
@@ -71,4 +71,4 @@ class LauncherSearchIphView : public views::View {
 
 }  // namespace ash
 
-#endif  // ASH_APP_LIST_VIEWS_LAUNCHER_SEARCH_IPH_VIEW_H_
+#endif  // ASH_ASSISTANT_UI_MAIN_STAGE_LAUNCHER_SEARCH_IPH_VIEW_H_
