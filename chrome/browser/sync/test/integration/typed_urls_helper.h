ab7830152a110363a662700b5e680c899abe461b
Marc Treib <treib@chromium.org>
2023-10-02 14:29:29 +0000

    Sync tests: Stop using ProfilesHaveSameTypedURLsChecker
    Two tests used this checked, and they can be easily rewritten to a more
    modern style.
    Removing the checker lets us clean up a whole bunch of no-longer-used
    test utils.
    
    Bug: 1365291
    Change-Id: Ibc9304034b8c6a61f5af34dac7499a28e6cdc39a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4902032
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Auto-Submit: Marc Treib <treib@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204002}

--- chrome/browser/sync/test/integration/typed_urls_helper.h
+++ chrome/browser/sync/test/integration/typed_urls_helper.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
-#include "chrome/browser/sync/test/integration/multi_client_status_change_checker.h"
 #include "components/history/core/browser/history_types.h"
 #include "ui/base/page_transition_types.h"
 
@@ -18,9 +17,6 @@ class Time;
 
 // TODO(crbug.com/1365291): Rename this to history_helper.
 namespace typed_urls_helper {
-
-// Gets the typed URLs from a specific sync profile.
-history::URLRows GetTypedUrlsFromClient(int index);
 
 // Gets the URLRow for a specific URL from a specific sync profile. Returns
 // false if the URL was not found in the history DB.
@@ -69,14 +65,5 @@ void AddUrlToHistoryWithTimestamp(int index,
                                   const base::Time& timestamp);
 
 }  // namespace typed_urls_helper
-
-// Checker that blocks until all clients have the same Typed URLs.
-class ProfilesHaveSameTypedURLsChecker : public MultiClientStatusChangeChecker {
- public:
-  ProfilesHaveSameTypedURLsChecker();
-
-  // Implementation of StatusChangeChecker.
-  bool IsExitConditionSatisfied(std::ostream* os) override;
-};
 
 #endif  // CHROME_BROWSER_SYNC_TEST_INTEGRATION_TYPED_URLS_HELPER_H_
