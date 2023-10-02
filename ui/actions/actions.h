760df7dab340a2d6b34d4908ebc0afcc86e22984
Lei Zhang <thestig@chromium.org>
2023-10-02 19:31:12 +0000

    Add some aliases for ui::ActionManager maps
    Instead of writing out the full type of maps, shorten them with aliases.
    Fix nits along the way, like spurious C-style casts and variable names
    that do not follow the style guide.
    
    Change-Id: Idf95341398a2cd69e4be5a43904164b276ff0fd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4906485
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Joseph Park <josephjoopark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204205}

--- ui/actions/actions.h
+++ ui/actions/actions.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_ACTIONS_ACTIONS_H_
 #define UI_ACTIONS_ACTIONS_H_
 
+#include <memory>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include "base/callback_list.h"
@@ -265,6 +267,8 @@ class COMPONENT_EXPORT(ACTIONS) ActionManager
 
   using ActionItemInitializerList =
       base::RepeatingCallbackList<void(ActionManager*)>;
+  using ActionIdToStringMap = base::flat_map<ActionId, std::string_view>;
+  using StringToActionIdMap = base::flat_map<std::string_view, ActionId>;
 
   ActionManager(const ActionManager&) = delete;
   ActionManager& operator=(const ActionManager&) = delete;
@@ -285,10 +289,8 @@ class COMPONENT_EXPORT(ACTIONS) ActionManager
   static std::vector<absl::optional<ActionId>> StringsToActionIds(
       std::vector<std::string> action_id_strings);
 
-  static void AddActionIdToStringMappings(
-      base::flat_map<ActionId, std::string_view> map);
-  static void AddStringToActionIdMappings(
-      base::flat_map<std::string_view, ActionId> map);
+  static void AddActionIdToStringMappings(ActionIdToStringMap map);
+  static void AddStringToActionIdMappings(StringToActionIdMap map);
 
   // The second element in the pair is set to true if a new ActionId is
   // created, or false if an ActionId with the given name already exists.
@@ -338,14 +340,14 @@ class COMPONENT_EXPORT(ACTIONS) ActionManager
   template <typename T, typename U>
   static void MergeMaps(base::flat_map<T, U>& map1, base::flat_map<T, U>& map2);
 
+  static ActionIdToStringMap& GetActionIdToStringMap();
+  static StringToActionIdMap& GetStringToActionIdMap();
+
   // Holds the chain of ActionManager initializer callbacks.
   std::unique_ptr<ActionItemInitializerList> initializer_list_;
 
   // All "root" actions are parented to this action.
   BaseAction root_action_parent_;
-
-  static base::flat_map<ActionId, std::string_view>& GetActionIdToStringMap();
-  static base::flat_map<std::string_view, ActionId>& GetStringToActionIdMap();
 };
 
 COMPONENT_EXPORT(ACTIONS)
