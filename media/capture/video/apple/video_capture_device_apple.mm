824eb088eab91073321963b148dc15d640aca477
Tony Herre <toprice@chromium.org>
2023-10-02 11:59:36 +0000

    Refactor GetDeviceInfos() to not reenumerate cameras repeatedly
    Instead of having GetVideoCaptureDeviceNames() and
    GetDeviceSupportedFormats() do their own enumerations of cameras against
    the AVFoundation enum API, just enumerate first in GetDeviceInfos().
    
    Also, inline GetVideoCaptureDeviceNames() - it's only used here, and
    has a lot of needless code just preparing a dict to be immediately
    looked up.
    
    Bug: 1487516
    Change-Id: I6bbc35c0ac4e454dbb0b677720188448615c7143
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905249
    Reviewed-by: Markus Handell <handellm@google.com>
    Commit-Queue: Tony Herre <toprice@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1203950}

--- media/capture/video/apple/video_capture_device_apple.mm
+++ media/capture/video/apple/video_capture_device_apple.mm
@@ -29,32 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/capture/video/mac/uvc_control_mac.h"
 #endif
 
-@implementation DeviceNameAndTransportType {
-  NSString* __strong _deviceName;
-  // The transport type of the device (USB, PCI, etc), values are defined in
-  // <IOKit/audio/IOAudioTypes.h> as kIOAudioDeviceTransportType*.
-  media::VideoCaptureTransportType _transportType;
-}
-
-- (instancetype)initWithName:(NSString*)deviceName
-               transportType:(media::VideoCaptureTransportType)transportType {
-  if (self = [super init]) {
-    _deviceName = [deviceName copy];
-    _transportType = transportType;
-  }
-  return self;
-}
-
-- (NSString*)deviceName {
-  return _deviceName;
-}
-
-- (media::VideoCaptureTransportType)deviceTransportType {
-  return _transportType;
-}
-
-@end  // @implementation DeviceNameAndTransportType
-
 namespace media {
 
 namespace {
