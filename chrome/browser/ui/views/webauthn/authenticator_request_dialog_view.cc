5f706ede8d94aedbcb3b34c5ccbe5f6e044b07b4
Nina Satragno <nsatragno@chromium.org>
2023-10-02 21:30:21 +0000

    [webauthn] Fix extra button appearing incorrectly
    crrev.com/c/4908008 made it so the buttons get built every time the UI
    updates. However, setting the extra view to nullptr does not actually
    clear the extra button. This CL sets it to an empty view instead.
    
    The easiest way to trigger this is to wait for a timeout, and note that
    "Manage devices" shows up when it shouldn't.
    
    Bug: 1347607
    Change-Id: I6e627228e2dbba53e687c2aa0780ddf1f4c28c6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4902946
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204272}

--- chrome/browser/ui/views/webauthn/authenticator_request_dialog_view.cc
+++ chrome/browser/ui/views/webauthn/authenticator_request_dialog_view.cc
@@ -112,7 +112,7 @@ void AuthenticatorRequestDialogView::UpdateUIForCurrentSheet() {
             base::Unretained(this)),
         l10n_util::GetStringUTF16(IDS_WEBAUTHN_MANAGE_DEVICES)));
   } else {
-    SetExtraView<views::View>(nullptr);
+    SetExtraView(std::make_unique<views::View>());
   }
 
   DialogModelChanged();
