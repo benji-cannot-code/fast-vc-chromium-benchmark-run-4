89fa2c39ca8ae034a3cfa4bd947f624ed743d419
Tomasz Jurkiewicz <tju@google.com>
2023-10-02 19:38:27 +0000

    Add timing of access token and rpc calls to proto_fetcher.
    Introduces stopwatch with lap-time functionality for each stage of
    fetching.
    
    The metrics are implemented so that the possibility to track
    per-every-status latency is preserved.
    
    Bug: b:299088120
    Change-Id: I80c8866e93c4be90006a815421bc0d561761f2b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905035
    Reviewed-by: James Lee <ljjlee@google.com>
    Commit-Queue: James Lee <ljjlee@google.com>
    Auto-Submit: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1204214}

--- components/supervised_user/core/browser/proto_fetcher_unittest.cc
+++ components/supervised_user/core/browser/proto_fetcher_unittest.cc
@@ -409,6 +409,13 @@ TEST_P(ProtoFetcherTest, RecordsMetrics) {
       base::StrCat({GetConfig().histogram_basename, ".Latency"}),
       /*expected_count(grew by)*/ 1);
   histogram_tester.ExpectTotalCount(
+      base::StrCat(
+          {GetConfig().histogram_basename, ".NONE.AccessTokenLatency"}),
+      /*expected_count(grew by)*/ 1);
+  histogram_tester.ExpectTotalCount(
+      base::StrCat({GetConfig().histogram_basename, ".HTTP_OK.ApiLatency"}),
+      /*expected_count(grew by)*/ 1);
+  histogram_tester.ExpectTotalCount(
       base::StrCat({GetConfig().histogram_basename, ".NoError.Latency"}),
       /*expected_count(grew by)*/ 1);
 
@@ -485,6 +492,17 @@ TEST_P(ProtoFetcherTest, RetryingFetcherTerminatesOnOkStatusAndRecordsMetrics) {
   histogram_tester.ExpectTotalCount(
       base::StrCat({GetConfig().histogram_basename, ".NoError.Latency"}),
       /*expected_count(grew by)*/ 1);
+
+  // System made it through access token phase three times.
+  histogram_tester.ExpectTotalCount(
+      base::StrCat(
+          {GetConfig().histogram_basename, ".NONE.AccessTokenLatency"}),
+      /*expected_count(grew by)*/ 3);
+  // Only one successful api call.
+  histogram_tester.ExpectTotalCount(
+      base::StrCat({GetConfig().histogram_basename, ".HTTP_OK.ApiLatency"}),
+      /*expected_count(grew by)*/ 1);
+
   histogram_tester.ExpectTotalCount(
       base::StrCat(
           {GetConfig().histogram_basename, ".HttpStatusOrNetError.Latency"}),
@@ -560,6 +578,17 @@ TEST_P(ProtoFetcherTest,
   histogram_tester.ExpectTotalCount(
       base::StrCat({GetConfig().histogram_basename, ".ParseError.Latency"}),
       /*expected_count(grew by)*/ 1);
+
+  // System made it through access token phase two times.
+  histogram_tester.ExpectTotalCount(
+      base::StrCat(
+          {GetConfig().histogram_basename, ".NONE.AccessTokenLatency"}),
+      /*expected_count(grew by)*/ 2);
+  // Only one successful api call (parse error is a successful api call).
+  histogram_tester.ExpectTotalCount(
+      base::StrCat({GetConfig().histogram_basename, ".HTTP_OK.ApiLatency"}),
+      /*expected_count(grew by)*/ 1);
+
   histogram_tester.ExpectTotalCount(
       base::StrCat(
           {GetConfig().histogram_basename, ".HttpStatusOrNetError.Latency"}),
@@ -624,6 +653,16 @@ TEST_P(ProtoFetcherTest, RetryingFetcherContinuesOnTransientError) {
       base::StrCat(
           {GetConfig().histogram_basename, ".HttpStatusOrNetError.Latency"}),
       /*expected_count(grew by)*/ 2);
+
+  // System made it through access token phase two times.
+  histogram_tester.ExpectTotalCount(
+      base::StrCat(
+          {GetConfig().histogram_basename, ".NONE.AccessTokenLatency"}),
+      /*expected_count(grew by)*/ 3);
+  // Server only responds with error.
+  histogram_tester.ExpectTotalCount(
+      base::StrCat({GetConfig().histogram_basename, ".HTTP_OK.ApiLatency"}),
+      /*expected_count(grew by)*/ 0);
 
   EXPECT_THAT(
       histogram_tester.GetAllSamples(
