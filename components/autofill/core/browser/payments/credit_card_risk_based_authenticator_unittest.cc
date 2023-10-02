9ceef06a7dddfedeab6bafb9bc0794da80318644
Junhui He <junhuihe@google.com>
2023-10-02 19:39:09 +0000

    Replace absl::string_view with std::string_view
    Bug: 1470933
    Change-Id: I7f5b59f59577d52c5c9ee2693243e7bccbbcbc3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4907066
    Commit-Queue: Junhui He <junhuihe@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204215}

--- components/autofill/core/browser/payments/credit_card_risk_based_authenticator_unittest.cc
+++ components/autofill/core/browser/payments/credit_card_risk_based_authenticator_unittest.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace autofill {
 
 namespace {
-constexpr absl::string_view kTestNumber = "4234567890123456";
+constexpr std::string_view kTestNumber = "4234567890123456";
 constexpr int64_t kTestBillingCustomerNumber = 123456;
 }  // namespace
 
