345e14a7e953746f1e03c174aaa9b0bace658268
Adrian Taylor <adetaylor@chromium.org>
2023-10-02 19:04:26 +0000

    Make lookalike fuzzer DCHECK-safe.
    Bug: 1485152, 1487787
    Change-Id: Ib6009b514de97f64419dfdf5f1a9c000ba775bbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905135
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Auto-Submit: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204182}

--- components/lookalikes/core/lookalike_url_util_fuzzer.cc
+++ components/lookalikes/core/lookalike_url_util_fuzzer.cc
@@ -7,13 +7,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/icu_util.h"
 #include "components/lookalikes/core/lookalike_url_util.h"
 
+class ICUInitializer {
+ public:
+  ICUInitializer() { base::i18n::InitializeICU(); }
+};
+
+static ICUInitializer g_icu_initializer;
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  base::i18n::InitializeICU();
   FuzzedDataProvider data_provider(data, size);
   lookalikes::LookalikeUrlMatchType match_type =
       data_provider.ConsumeEnum<lookalikes::LookalikeUrlMatchType>();
   GURL navigated_url = GURL(data_provider.ConsumeRandomLengthString());
   std::string matched_hostname = data_provider.ConsumeRemainingBytesAsString();
+  // Ignore inputs matching the following, to avoid a false positive
+  std::string suggested_domain = lookalikes::GetETLDPlusOne(matched_hostname);
+  if (suggested_domain.empty()) {
+    return 0;
+  }
   lookalikes::GetSuggestedURL(match_type, navigated_url, matched_hostname);
   return 0;
 }
