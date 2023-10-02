cda4c6570b22ec047e3d14ca1fe7d238427c2b67
Alex Mitra <alexmitra@chromium.org>
2023-10-02 15:38:55 +0000

    Annotate SupportLibServiceWorkerControllerAdapter with Lifetime.Profile
    Bug: 1487961
    Change-Id: I02d98a29d77e3d7aad142bc08a34482006dd5ecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905352
    Reviewed-by: Peter Pakkenberg <pbirk@chromium.org>
    Commit-Queue: Alex Mitra <alexmitra@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204032}

--- android_webview/support_library/java/src/org/chromium/support_lib_glue/SupportLibServiceWorkerControllerAdapter.java
+++ android_webview/support_library/java/src/org/chromium/support_lib_glue/SupportLibServiceWorkerControllerAdapter.java
@@ -8,6 +8,7 @@ package org.chromium.support_lib_glue;
 import static org.chromium.support_lib_glue.SupportLibWebViewChromiumFactory.recordApiCall;
 
 import org.chromium.android_webview.AwServiceWorkerController;
+import org.chromium.android_webview.common.Lifetime;
 import org.chromium.base.TraceEvent;
 import org.chromium.support_lib_boundary.ServiceWorkerClientBoundaryInterface;
 import org.chromium.support_lib_boundary.ServiceWorkerControllerBoundaryInterface;
@@ -19,6 +20,7 @@ import java.lang.reflect.InvocationHandler;
 /**
  * Adapter between AwServiceWorkerController and ServiceWorkerControllerBoundaryInterface.
  */
+@Lifetime.Profile
 class SupportLibServiceWorkerControllerAdapter implements ServiceWorkerControllerBoundaryInterface {
     AwServiceWorkerController mAwServiceWorkerController;
 
