f1c190d642517240a8ff7b6ff14f1e71fd0fee85
Qihui Zhao <qihuizhao@google.com>
2023-10-02 20:49:52 +0000

    Rename `pending_ibans_query_` to `pending_local_ibans_query_` in PDM
    This CL renames `pending_ibans_query_` to `pending_local_ibans_query_`
    in PDM because now server-based IBAN has been introduced.
    
    Bug: b:301532395
    Change-Id: I58e51b352068423436d862ecdbcc89f78b2b127f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4891651
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1204249}

--- components/autofill/core/browser/test_personal_data_manager.cc
+++ components/autofill/core/browser/test_personal_data_manager.cc
@@ -211,7 +211,7 @@ void TestPersonalDataManager::LoadCreditCardCloudTokenData() {
 }
 
 void TestPersonalDataManager::LoadIbans() {
-  pending_ibans_query_ = 128;
+  pending_local_ibans_query_ = 128;
   pending_server_ibans_query_ = 129;
   {
     std::vector<std::unique_ptr<Iban>> ibans;
@@ -219,7 +219,7 @@ void TestPersonalDataManager::LoadIbans() {
     std::unique_ptr<WDTypedResult> result =
         std::make_unique<WDResult<std::vector<std::unique_ptr<Iban>>>>(
             AUTOFILL_IBANS_RESULT, std::move(ibans));
-    OnWebDataServiceRequestDone(pending_ibans_query_, std::move(result));
+    OnWebDataServiceRequestDone(pending_local_ibans_query_, std::move(result));
   }
   {
     std::vector<std::unique_ptr<Iban>> server_ibans;
