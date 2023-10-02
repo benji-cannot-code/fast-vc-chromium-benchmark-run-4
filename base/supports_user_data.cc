06d1cff30df629048143baeb205aa8e3b87d2cc8
Daniel Cheng <dcheng@chromium.org>
2023-10-02 22:21:59 +0000

    Remove experiment code for SupportsUserDataFlatHashMap.
    Note that this simply deletes the base::flat_map arm; the experiment was
    considered successful, and the default container guidelines are being
    updated to recommend absl::flat_hash_map as a default. As such, it does
    not seem to make sense to fall back to base::flat_map temporarily, only
    to convert it again later.
    
    Bug: 1121345
    Change-Id: I627cab7a6ef313308811beb6e680e7a037a5eaa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4906604
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204305}

--- base/supports_user_data.cc
+++ base/supports_user_data.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/supports_user_data.h"
 
 #include "base/feature_list.h"
-#include "base/features.h"
 #include "base/sequence_checker.h"
 
 namespace base {
@@ -15,10 +14,7 @@ std::unique_ptr<SupportsUserData::Data> SupportsUserData::Data::Clone() {
   return nullptr;
 }
 
-SupportsUserData::SupportsUserData()
-    : user_data_(FeatureList::IsEnabled(features::kSupportsUserDataFlatHashMap)
-                     ? MapVariants(FlatDataMap())
-                     : MapVariants(DataMap())) {
+SupportsUserData::SupportsUserData() {
   // Harmless to construct on a different execution sequence to subsequent
   // usage.
   DETACH_FROM_SEQUENCE(sequence_checker_);
@@ -31,15 +27,11 @@ SupportsUserData::Data* SupportsUserData::GetUserData(const void* key) const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   // Avoid null keys; they are too vulnerable to collision.
   DCHECK(key);
-  return absl::visit(
-      [key](const auto& map) -> Data* {
-        auto found = map.find(key);
-        if (found != map.end()) {
-          return found->second.get();
-        }
-        return nullptr;
-      },
-      user_data_);
+  auto found = user_data_.find(key);
+  if (found != user_data_.end()) {
+    return found->second.get();
+  }
+  return nullptr;
 }
 
 std::unique_ptr<SupportsUserData::Data> SupportsUserData::TakeUserData(
@@ -47,18 +39,14 @@ std::unique_ptr<SupportsUserData::Data> SupportsUserData::TakeUserData(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   // Null keys are too vulnerable to collision.
   CHECK(key);
-  return absl::visit(
-      [key](auto& map) -> std::unique_ptr<SupportsUserData::Data> {
-        auto found = map.find(key);
-        if (found != map.end()) {
-          std::unique_ptr<SupportsUserData::Data> deowned;
-          deowned.swap(found->second);
-          map.erase(key);
-          return deowned;
-        }
-        return nullptr;
-      },
-      user_data_);
+  auto found = user_data_.find(key);
+  if (found != user_data_.end()) {
+    std::unique_ptr<SupportsUserData::Data> deowned;
+    deowned.swap(found->second);
+    user_data_.erase(key);
+    return deowned;
+  }
+  return nullptr;
 }
 
 void SupportsUserData::SetUserData(const void* key,
@@ -67,8 +55,7 @@ void SupportsUserData::SetUserData(const void* key,
   // Avoid null keys; they are too vulnerable to collision.
   DCHECK(key);
   if (data.get()) {
-    absl::visit([key, &data](auto& map) { map[key] = std::move(data); },
-                user_data_);
+    user_data_[key] = std::move(data);
   } else {
     RemoveUserData(key);
   }
@@ -76,26 +63,22 @@ void SupportsUserData::SetUserData(const void* key,
 
 void SupportsUserData::RemoveUserData(const void* key) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  absl::visit(
-      [key](auto& map) {
-        auto it = map.find(key);
-        if (it != map.end()) {
-          // Remove the entry from the map before deleting `owned_data` to avoid
-          // reentrancy issues when `owned_data` owns `this`. Otherwise:
-          //
-          // 1. `RemoveUserData()` calls `erase()`.
-          // 2. `erase()` deletes `owned_data`.
-          // 3. `owned_data` deletes `this`.
-          //
-          // At this point, `erase()` is still on the stack even though the
-          // backing map (owned by `this`) has already been destroyed, and it
-          // may simply crash, cause a use-after-free, or any other number of
-          // interesting things.
-          auto owned_data = std::move(it->second);
-          map.erase(it);
-        }
-      },
-      user_data_);
+  auto it = user_data_.find(key);
+  if (it != user_data_.end()) {
+    // Remove the entry from the map before deleting `owned_data` to avoid
+    // reentrancy issues when `owned_data` owns `this`. Otherwise:
+    //
+    // 1. `RemoveUserData()` calls `erase()`.
+    // 2. `erase()` deletes `owned_data`.
+    // 3. `owned_data` deletes `this`.
+    //
+    // At this point, `erase()` is still on the stack even though the
+    // backing map (owned by `this`) has already been destroyed, and it
+    // may simply crash, cause a use-after-free, or any other number of
+    // interesting things.
+    auto owned_data = std::move(it->second);
+    user_data_.erase(it);
+  }
 }
 
 void SupportsUserData::DetachFromSequence() {
@@ -103,24 +86,20 @@ void SupportsUserData::DetachFromSequence() {
 }
 
 void SupportsUserData::CloneDataFrom(const SupportsUserData& other) {
-  absl::visit(
-      [this](const auto& other_map) {
-        for (const auto& data_pair : other_map) {
-          auto cloned_data = data_pair.second->Clone();
-          if (cloned_data) {
-            SetUserData(data_pair.first, std::move(cloned_data));
-          }
-        }
-      },
-      other.user_data_);
+  for (const auto& data_pair : other.user_data_) {
+    auto cloned_data = data_pair.second->Clone();
+    if (cloned_data) {
+      SetUserData(data_pair.first, std::move(cloned_data));
+    }
+  }
 }
 
 SupportsUserData::~SupportsUserData() {
-  if (!absl::visit([](const auto& map) { return map.empty(); }, user_data_)) {
+  if (!user_data_.empty()) {
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   }
-  MapVariants local_user_data;
-  user_data_.swap(local_user_data);
+  absl::flat_hash_map<const void*, std::unique_ptr<Data>> user_data;
+  user_data_.swap(user_data);
   // Now this->user_data_ is empty, and any destructors called transitively from
   // the destruction of |local_user_data| will see it that way instead of
   // examining a being-destroyed object.
@@ -128,7 +107,7 @@ SupportsUserData::~SupportsUserData() {
 
 void SupportsUserData::ClearAllUserData() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  absl::visit([](auto& map) { map.clear(); }, user_data_);
+  user_data_.clear();
 }
 
 }  // namespace base
