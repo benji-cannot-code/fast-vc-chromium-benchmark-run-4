4692e64781589710931c2932006487fb06df6d04
Timofey Chudakov <tchudakov@google.com>
2023-10-02 07:54:41 +0000

    [mullet m2] Open delete dialog from the Autofill popup.
    This CL implements the functionality of opening the delete dialog
    after the user has accepted the corresponding suggestion from
    the Autofill subpopup. The dialog offers either to delete the
    autofill profile or to cancel the deletion process.
    
    * If the user accepts the dialog, the corresponding autofill profile
    is deleted by GUID. The implementation will check that the
    PersonalDataManager has finished deleting the autofill profile
    and reopen the Autofill popup afterwards.
    * If the user canceled the dialog, the Autofill popup is reopened
    without deleting any autofill profile.
    
    The AutofillClient::ShowDeleteAddressProfileDialog is extended to
    accept the autofill profile to be deleted and the deletion
    callback to invoke. The callback accepts the boolean flag
    identifying whether the user accepted or declined the dialog.
    
    Bug: 1459990
    Change-Id: I7f24c8d54f01cfd66324f96622ac2e316bfccee2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4890258
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: John Wu <jzw@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1203864}

--- android_webview/browser/aw_autofill_client.cc
+++ android_webview/browser/aw_autofill_client.cc
@@ -213,7 +213,9 @@ void AwAutofillClient::ShowEditAddressProfileDialog(
   NOTREACHED();
 }
 
-void AwAutofillClient::ShowDeleteAddressProfileDialog() {
+void AwAutofillClient::ShowDeleteAddressProfileDialog(
+    const autofill::AutofillProfile& profile,
+    AddressProfileDeleteDialogCallback delete_dialog_callback) {
   NOTREACHED();
 }
 
--- ios/chrome/browser/ui/autofill/chrome_autofill_client_ios.mm
+++ ios/chrome/browser/ui/autofill/chrome_autofill_client_ios.mm
@@ -398,7 +398,9 @@ void ChromeAutofillClientIOS::ShowEditAddressProfileDialog(
   NOTREACHED_NORETURN();
 }
 
-void ChromeAutofillClientIOS::ShowDeleteAddressProfileDialog() {
+void ChromeAutofillClientIOS::ShowDeleteAddressProfileDialog(
+    const AutofillProfile& profile,
+    AddressProfileDeleteDialogCallback delete_dialog_callback) {
   NOTREACHED_NORETURN();
 }
 
