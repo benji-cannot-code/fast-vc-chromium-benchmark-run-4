d8c691fd28ce2e6dc284a512476964c29fa7d128
Colin Blundell <blundell@chromium.org>
2023-10-02 11:49:59 +0000

    [//media] Add path to perform VideoFrame->WebGL upload via SharedImage
    This CL does the following:
    
    * Adds a codepath in PaintCanvasVideoRenderer to perform direct GPU
      VideoFrame->WebGL upload via a temporary SharedImage rather than the
      current paths of (a) using legacy mailboxes and (b) using a
      multiplanarSI- and Ganesh/GL-specific flow. The new path does the
      following:
      * Creates a temporary SharedImage
      * Attaches the SharedImage to the WebGL-provided texture via
        glTexImage2DSharedImageCHROMIUM()
      * Copies the VideoFrame to the WebGL-provided texture via this
        SharedImage
    
    In this codepath, VideoFrame->WebGL direct upload does not use legacy
    mailboxes at all. Once the NaCL use case is eliminated and this
    codepath lands definitively, it will be possible to eliminate
    PaintCanvasVideoRenderer's usage of legacy mailboxes, which will at
    that point be dead code.
    
    Followup will complete support for non-multiplanar SI, which currently
    hits a DCHECK [1].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:media/renderers/video_frame_yuv_converter.cc;l=145?q=video_frame_yuv_converter.cc&ss=chromium
    
    Bug: 1410164
    Change-Id: I84683e4d8e9887f3397db59e93745484e26e51a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4898204
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1203946}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -6246,7 +6246,7 @@ void WebGLRenderingContextBase::TexImageHelperMediaVideoFrame(
             raster_context_provider, ContextGL(), media_video_frame,
             params.target, texture->Object(), adjusted_internalformat,
             params.format, params.type, params.level, unpack_premultiply_alpha_,
-            unpack_flip_y_)) {
+            unpack_flip_y_, /*allow_shared_image_for_direct_upload=*/true)) {
       texture->UpdateLastUploadedFrame(metadata);
       return;
     }
@@ -6263,7 +6263,7 @@ void WebGLRenderingContextBase::TexImageHelperMediaVideoFrame(
             raster_context_provider, ContextGL(), media_video_frame,
             params.target, texture->Object(), adjusted_internalformat,
             params.format, params.type, params.level, unpack_premultiply_alpha_,
-            unpack_flip_y_)) {
+            unpack_flip_y_, /*allow_shared_image_for_direct_upload=*/true)) {
       texture->UpdateLastUploadedFrame(metadata);
       return;
     }
