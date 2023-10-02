d1902d5770f2a2eeab4f5cc56c754ea0835a7841
Min Qin <qinmin@chromium.org>
2023-10-02 17:04:43 +0000

    Make changes to parcel manager to handle string date
    The server will now send string instead of usec, so this CL parses the
    date string back to a base::Time. Doing this will allow it easier for
    UI to format the time object to a localized string, compared to just
    storing the "yyyy-mm-dd" string.
    Also, changed the parcel manager to only send get status request to
    server if the parcels are not done, as there is no need to update
    parcel status if it is already delivered.
    
    Bug: 1488558
    Change-Id: Id8195b442535ddc5f65edb99cea832e430c9940f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4901825
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204081}

--- components/commerce/core/parcel/parcels_server_proxy_unittest.cc
+++ components/commerce/core/parcel/parcels_server_proxy_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/gtest_util.h"
 #include "base/test/task_environment.h"
+#include "base/time/time.h"
 #include "components/commerce/core/commerce_constants.h"
 #include "components/commerce/core/parcel/parcels_server_proxy.h"
 #include "components/commerce/core/proto/parcel.pb.h"
@@ -40,7 +41,7 @@ const std::string kExpectedGetParcelStatusPostData =
 const std::string kResponseSucceeded =
     "{ \"parcelStatus\": [{\"parcelIdentifier\": {\"trackingId\": \"xyz\","
     "\"carrier\": 1}, \"parcelState\": 2, \"trackingUrl\": \"www.foo.com\","
-    "\"estimatedDeliveryTimeUsec\": \"1000\"}]}";
+    "\"estimatedDeliveryDate\": \"2023-10-11\"}]}";
 const std::string kExpectedStartTrackingPostData =
     "{\"parcelIds\":[{\"carrier\":2,\"trackingId\":\"xyz\"}],"
     "\"sourcePageDomain\":\"www.abc.com\"}";
@@ -48,7 +49,6 @@ const std::string kTestTrackingUrl = "www.foo.com";
 const std::string kTestTrackingId = "xyz";
 const std::string kTestSourcePageDomain = "www.abc.com";
 const std::string kDeleteHttpMethod = "DELETE";
-}  // namespace
 
 std::vector<commerce::ParcelIdentifier> GetTestParcelIdentifiers() {
   commerce::ParcelIdentifier identifier;
@@ -56,6 +56,14 @@ std::vector<commerce::ParcelIdentifier> GetTestParcelIdentifiers() {
   identifier.set_carrier(commerce::ParcelIdentifier::UPS);
   return std::vector<commerce::ParcelIdentifier>{identifier};
 }
+
+int64_t GetExpectedDeliveryTimeUsec() {
+  base::Time delivery;
+  CHECK(base::Time::FromString("11-OCT-2023 00:00:00", &delivery));
+  return delivery.ToDeltaSinceWindowsEpoch().InMicroseconds();
+}
+
+}  // namespace
 
 namespace commerce {
 
@@ -122,7 +130,8 @@ TEST_F(ParcelsServerProxyTest, TestGetParcelStatus) {
             auto status = (*parcel_status)[0];
             ASSERT_EQ(ParcelStatus::PICKED_UP, status.parcel_state());
             ASSERT_EQ(kTestTrackingUrl, status.tracking_url());
-            ASSERT_EQ(1000, status.estimated_delivery_time_usec());
+            ASSERT_EQ(GetExpectedDeliveryTimeUsec(),
+                      status.estimated_delivery_time_usec());
             ASSERT_EQ(kTestTrackingId,
                       status.parcel_identifier().tracking_id());
             ASSERT_EQ(commerce::ParcelIdentifier::FEDEX,
@@ -225,7 +234,8 @@ TEST_F(ParcelsServerProxyTest, TestStartTrackingParcels) {
             auto status = (*parcel_status)[0];
             ASSERT_EQ(ParcelStatus::PICKED_UP, status.parcel_state());
             ASSERT_EQ(kTestTrackingUrl, status.tracking_url());
-            ASSERT_EQ(1000, status.estimated_delivery_time_usec());
+            ASSERT_EQ(GetExpectedDeliveryTimeUsec(),
+                      status.estimated_delivery_time_usec());
             ASSERT_EQ(kTestTrackingId,
                       status.parcel_identifier().tracking_id());
             ASSERT_EQ(commerce::ParcelIdentifier::FEDEX,
