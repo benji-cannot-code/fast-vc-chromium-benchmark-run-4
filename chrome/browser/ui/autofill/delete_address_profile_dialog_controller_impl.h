c4e62f0b228b7b6f8f02b34bd8c5d9feb235c43d
Timofey Chudakov <tchudakov@google.com>
2023-10-02 07:18:42 +0000

    [mullet m2] Open DeleteAddressProfileDialogView from the controller.
    This CL adds logic to the DeleteAddressProfileDialogController
    which opens the corresponding view. By default, controller opens the
    view via the dedicated method.
    
    This CL adds an option to set a testing factory the dialog view, which
    overrides the controller's default behavior.
    
    Bug: 1459990
    Change-Id: I1097dd2a128866d5c0b157eb8d4d96ccf4dcc44e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4894132
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1203859}

--- chrome/browser/ui/autofill/delete_address_profile_dialog_controller_impl.h
+++ chrome/browser/ui/autofill/delete_address_profile_dialog_controller_impl.h
@@ -23,6 +23,10 @@ class DeleteAddressProfileDialogControllerImpl
       public content::WebContentsUserData<
           DeleteAddressProfileDialogControllerImpl> {
  public:
+  using DeleteAddressProfileDialogViewFactory = base::RepeatingCallback<void(
+      content::WebContents*,
+      base::WeakPtr<DeleteAddressProfileDialogController>)>;
+
   DeleteAddressProfileDialogControllerImpl(
       const DeleteAddressProfileDialogControllerImpl&) = delete;
   DeleteAddressProfileDialogControllerImpl& operator=(
@@ -43,6 +47,9 @@ class DeleteAddressProfileDialogControllerImpl
   void OnClosed() override;
   void OnDialogDestroying() override;
 
+  void SetViewFactoryForTest(
+      DeleteAddressProfileDialogViewFactory view_factory);
+
  private:
   explicit DeleteAddressProfileDialogControllerImpl(
       content::WebContents* web_contents);
@@ -56,6 +63,7 @@ class DeleteAddressProfileDialogControllerImpl
   bool is_account_address_profile_;
   AutofillClient::AddressProfileDeleteDialogCallback delete_dialog_callback_;
   absl::optional<bool> user_accepted_;
+  DeleteAddressProfileDialogViewFactory view_factory_for_test_;
 
   base::WeakPtrFactory<DeleteAddressProfileDialogController> weak_ptr_factory_{
       this};
--- chrome/browser/ui/autofill/delete_address_profile_dialog_view.h
+++ chrome/browser/ui/autofill/delete_address_profile_dialog_view.h
@@ -3,17 +3,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_VIEWS_AUTOFILL_DELETE_ADDRESS_PROFILE_DIALOG_VIEW_H_
-#define CHROME_BROWSER_UI_VIEWS_AUTOFILL_DELETE_ADDRESS_PROFILE_DIALOG_VIEW_H_
+#ifndef CHROME_BROWSER_UI_AUTOFILL_DELETE_ADDRESS_PROFILE_DIALOG_VIEW_H_
+#define CHROME_BROWSER_UI_AUTOFILL_DELETE_ADDRESS_PROFILE_DIALOG_VIEW_H_
 
 #include "base/memory/weak_ptr.h"
-#include "chrome/browser/ui/autofill/delete_address_profile_dialog_controller.h"
 
 namespace content {
 class WebContents;
 }  // namespace content
 
-namespace autofill::dialogs {
+namespace autofill {
+class DeleteAddressProfileDialogController;
 
 // Shows a modal dialog prompting to user whether they want to delete their
 // profile address.
@@ -21,6 +21,6 @@ void ShowDeleteAddressProfileDialogView(
     content::WebContents* web_contents,
     base::WeakPtr<DeleteAddressProfileDialogController> controller);
 
-}  // namespace autofill::dialogs
+}  // namespace autofill
 
-#endif  // CHROME_BROWSER_UI_VIEWS_AUTOFILL_DELETE_ADDRESS_PROFILE_DIALOG_VIEW_H_
+#endif  // CHROME_BROWSER_UI_AUTOFILL_DELETE_ADDRESS_PROFILE_DIALOG_VIEW_H_
