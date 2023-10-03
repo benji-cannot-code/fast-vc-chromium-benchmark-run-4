26f537fb9b9caf68ac17099e27bffad57c518bec
selya <selya@chromium.org>
2023-10-03 00:08:32 +0000

    Add new named feature parameter for Related Website Sets permission lifetime
    Design document located at the following link:
    https://docs.google.com/document/d/13Pdxn22JkMM_ZYWakIZV83-O8yxa2kLPp3rcnT03vO0/edit?usp=sharing&resourcekey=0-hEDjuvXwtGQWzuTV-tuSxA
    
    Change-Id: I51d47580c8cecb998eeb8e0cd9f5280499c84520
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905869
    Commit-Queue: Aaron Selya <selya@google.com>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204369}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1837,6 +1837,10 @@ const base::FeatureParam<base::TimeDelta>
         &kStorageAccessAPI,
         "storage_access_api_top_level_user_interaction_bound", base::Days(30)};
 const base::FeatureParam<base::TimeDelta>
+    kStorageAccessAPIRelatedWebsiteSetsLifetime{
+        &kStorageAccessAPI, "related_website_sets_permission_lifetime",
+        base::Days(30)};
+const base::FeatureParam<base::TimeDelta>
     kStorageAccessAPIImplicitPermissionLifetime{
         &kStorageAccessAPI, "storage_access_api_implicit_permission_lifetime",
         base::Hours(24)};
