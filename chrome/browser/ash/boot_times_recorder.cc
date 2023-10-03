54630f855ca1bcca0dc49238320fd7a17ac9518d
Alexander Alekseev <alemate@chromium.org>
2023-10-03 01:49:38 +0000

    Chrome OS: Add BootTime.Login3 histogram.
    This change adds a BootTime.Login3 histogram to indicate the end of the login
    animation. This histogram is expected to coincide with the user's perception of
    the "login finished" event.
    
    Bug: b/299307912
    Change-Id: I600dd3e3212a986e94dfe44bba8eeec6f115dc51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4880521
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Auto-Submit: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204407}

--- chrome/browser/ash/boot_times_recorder.cc
+++ chrome/browser/ash/boot_times_recorder.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "ash/shell.h"
 #include "base/check.h"
 #include "base/command_line.h"
 #include "base/files/file_path.h"
@@ -113,6 +114,8 @@ void BootTimesRecorder::LoginDone(bool is_user_new) {
   login_done_ = true;
   login_started_ = false;
   AddLoginTimeMarker("LoginDone", false);
+  ash::Shell::Get()->login_unlock_throughput_recorder()->AddLoginTimeMarker(
+      kUmaLogin);
   RecordCurrentStats(kChromeFirstRender);
   LoginEventRecorder::Get()->ScheduleWriteLoginTimes(
       kLoginTimes, (is_user_new ? kUmaLoginNewUser : kUmaLogin),
