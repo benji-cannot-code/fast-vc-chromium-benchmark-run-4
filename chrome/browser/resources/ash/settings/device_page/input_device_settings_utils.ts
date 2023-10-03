22a0f740e2c45159dbba53049784f5c28577e0a9
YuhanYang <yyhyyh@google.com>
2023-10-03 21:32:59 +0000

    customization: Implement shortcut-input-complete listener
    Implement shortcut-input-complete event listener to store the user's
    input key event. Once clicking save button, the dialog will update
    the button remappings with the latest detected inputKeyEvent.
    
    Bug: b/241965717
    Test: browser_tests, verified logic on VM.
    Change-Id: I21d340dae01995e34e27b21c4b1979fff3d5aea1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4889142
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1204868}

--- chrome/browser/resources/ash/settings/device_page/input_device_settings_utils.ts
+++ chrome/browser/resources/ash/settings/device_page/input_device_settings_utils.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {Button, DeviceSettings, InputDeviceSettingsPolicy, InputDeviceType, PolicyStatus} from './input_device_settings_types.js';
+import {Button, DeviceSettings, InputDeviceSettingsPolicy, InputDeviceType, KeyEvent, PolicyStatus} from './input_device_settings_types.js';
 
 function objectsAreEqual(
     obj1: {[key: string]: any}, obj2: {[key: string]: any}): boolean {
@@ -37,6 +37,11 @@ export function settingsAreEqual(
 
 export function buttonsAreEqual(button1: Button, button2: Button): boolean {
   return objectsAreEqual(button1, button2);
+}
+
+export function keyEventsAreEqual(
+    keyEvent1: KeyEvent, keyEvent2: KeyEvent): boolean {
+  return objectsAreEqual(keyEvent1, keyEvent2);
 }
 
 interface PrefPolicyFields {
