347c48d130a4ecf245c8dcd19a9148cbf4eb5794
Arthur Milchior <arthurmilchior@chromium.org>
2023-10-03 12:32:36 +0000

    Implement the non-deprecated builder
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: I03ad13b4e3ce28af3a176c987460c112157a668f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800108
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204582}

--- chrome/browser/ash/login/saml/in_session_password_sync_manager_factory.cc
+++ chrome/browser/ash/login/saml/in_session_password_sync_manager_factory.cc
@@ -40,7 +40,8 @@ InSessionPasswordSyncManagerFactory::InSessionPasswordSyncManagerFactory()
 InSessionPasswordSyncManagerFactory::~InSessionPasswordSyncManagerFactory() =
     default;
 
-KeyedService* InSessionPasswordSyncManagerFactory::BuildServiceInstanceFor(
+std::unique_ptr<KeyedService>
+InSessionPasswordSyncManagerFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
   Profile* profile = static_cast<Profile*>(context);
 
@@ -48,7 +49,7 @@ KeyedService* InSessionPasswordSyncManagerFactory::BuildServiceInstanceFor(
   if (!ProfileHelper::IsPrimaryProfile(profile)) {
     return nullptr;
   }
-  return new InSessionPasswordSyncManager(profile);
+  return std::make_unique<InSessionPasswordSyncManager>(profile);
 }
 
 }  // namespace ash
