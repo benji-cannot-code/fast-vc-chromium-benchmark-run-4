c73b22a1ffdada0b2b1df48fb6e97b721fbd8a65
David Pennington <dpenning@chromium.org>
2023-10-03 22:45:15 +0000

    [SavedTabGroups] prevent extensions from updating saved tabs
    Bug: 1488007
    Change-Id: Ie39ee753273845d1370ca48941f995af3ed1e2ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4829631
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204910}

--- chrome/browser/extensions/extension_tab_util.h
+++ chrome/browser/extensions/extension_tab_util.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/uuid.h"
 #include "base/values.h"
 #include "chrome/common/extensions/api/tabs.h"
+#include "components/tab_groups/tab_group_id.h"
 #include "extensions/common/features/feature.h"
 #include "ui/base/window_open_disposition.h"
 
@@ -263,6 +264,9 @@ class ExtensionTabUtil {
 
   // Retrieve a TabStripModel only if every browser is editable.
   static TabStripModel* GetEditableTabStripModel(Browser* browser);
+
+  static bool TabIsInSavedTabGroup(content::WebContents* contents,
+                                   TabStripModel* tab_strip_model);
 };
 
 }  // namespace extensions
