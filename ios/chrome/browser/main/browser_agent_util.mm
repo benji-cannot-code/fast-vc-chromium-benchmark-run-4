267f6a41212997961761d34bbc180632c1213d15
Sylvain Defresne <sdefresne@chromium.org>
2023-10-03 12:26:58 +0000

    [ios] Move post restoration setup out of SessionRestorationBrowserAgent
    Some of the tab helper of restored WebStates needs to be informed that
    their WebState has just been created as part of a session restoration.
    Move the logic out of SessionRestorationAgent (to avoid duplicating it
    in SessionRestorationService, and to avoid unnecessary coupling).
    
    Add FaviconBrowserAgent to take care of setting up the FaviconTabHelper
    and move the code taking care of the PlaceholderTabHelper into the
    existing PlaceholderBrowserAgent.
    
    Fix a bug in PlaceholderBrowserAgent where the member field was left
    uninitialized causing the first call of `CancelPagePlaceholder()` to
    be undeterminate if it happened before `ExpectNewForegroundTab()`.
    
    Bug: 1383087
    Change-Id: I3d50bc712dd3ca5a9214775b9dfdd2cb3348ba93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4907987
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1204578}

--- ios/chrome/browser/main/browser_agent_util.mm
+++ ios/chrome/browser/main/browser_agent_util.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/app_launcher/model/app_launcher_browser_agent.h"
 #import "ios/chrome/browser/crash_report/model/breadcrumbs/breadcrumb_manager_browser_agent.h"
 #import "ios/chrome/browser/device_sharing/device_sharing_browser_agent.h"
+#import "ios/chrome/browser/favicon/favicon_browser_agent.h"
 #import "ios/chrome/browser/follow/follow_browser_agent.h"
 #import "ios/chrome/browser/infobars/overlays/browser_agent/infobar_overlay_browser_agent_util.h"
 #import "ios/chrome/browser/lens/lens_browser_agent.h"
@@ -136,6 +137,7 @@ void AttachBrowserAgents(Browser* browser) {
   ReadingListBrowserAgent::CreateForBrowser(browser);
 
   PagePlaceholderBrowserAgent::CreateForBrowser(browser);
+  FaviconBrowserAgent::CreateForBrowser(browser);
 
   // This needs to be called last in case any downstream browser agents need to
   // access upstream agents created earlier in this function.
