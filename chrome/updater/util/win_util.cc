79cde5a29e77650d06c25cafd1b7a3d6a3f71e21
Allan Kerr <akerr@dropbox.com>
2023-10-03 01:48:45 +0000

    Fix incorrect cohort registry key
    When migrating from O3 to O4 the Chromium Updater imports cohort
    information from O3's cohort registry key located at
    `{CLIENT_STATE_KEY}\{app_id}\cohort`. However, O4 currently uses
    `{CLIENT_STATE_KEY}\cohort\{app_id}` meaning cohort information is lost
    on migration.
    
    Fix by updating `GetAppCohortKey` to return
    `{CLIENT_STATE_KEY}\{app_id}\cohort`.
    
    Bug: 1488586
    Change-Id: I6eb530e0ec92c7aaca04b4f6147b46fe943743a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4908399
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204406}

--- chrome/updater/util/win_util.cc
+++ chrome/updater/util/win_util.cc
@@ -297,7 +297,7 @@ std::wstring GetAppCohortKey(const std::string& app_id) {
 }
 
 std::wstring GetAppCohortKey(const std::wstring& app_id) {
-  return base::StrCat({COHORT_KEY, app_id});
+  return base::StrCat({GetAppClientStateKey(app_id), L"\\", kRegKeyCohort});
 }
 
 std::wstring GetAppCommandKey(const std::wstring& app_id,
