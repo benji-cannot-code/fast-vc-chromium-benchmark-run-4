fe740bd6fd4b502767c8bce245d3aa03648f3dc3
Yue Zhang <yuezhanggg@chromium.org>
2023-10-03 17:05:40 +0000

    [PriceInsights] Update folder name when bookmark is moved
    This CL makes ShoppingListHandler observe the bookmark move event, so
    that if the product bookmark that is showing in the price insights
    side panel right now is moved, we'll update the parent bookmark folder
    immediately.
    
    Bug: 1488033, b/298668160
    Change-Id: I2cbccdb800e675515148961d2c956e3dcc1535c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4906024
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204716}

--- chrome/browser/resources/side_panel/commerce/price_tracking_section.ts
+++ chrome/browser/resources/side_panel/commerce/price_tracking_section.ts
@@ -71,6 +71,9 @@ export class PriceTrackingSection extends PolymerElement {
         callbackRouter.operationFailedForBookmark.addListener(
             (product: BookmarkProductInfo, attemptedTrack: boolean) =>
                 this.onBookmarkOperationFailed(product, attemptedTrack)),
+        callbackRouter.onProductBookmarkMoved.addListener(
+            (product: BookmarkProductInfo) =>
+                this.onProductBookmarkMoved(product)),
     );
 
 
@@ -142,6 +145,15 @@ export class PriceTrackingSection extends PolymerElement {
     this.toggleAnnotationText_ = loadTimeData.getString('trackPriceError');
     this.folderName_ = '';
     this.isProductTracked_ = !attemptedTrack;
+  }
+
+  private async onProductBookmarkMoved(product: BookmarkProductInfo) {
+    if (product.info.clusterId === this.productInfo.clusterId &&
+        this.isProductTracked_) {
+      const {name} =
+          await this.shoppingApi_.getParentBookmarkFolderNameForCurrentUrl();
+      this.folderName_ = decodeString16(name);
+    }
   }
 }
 
