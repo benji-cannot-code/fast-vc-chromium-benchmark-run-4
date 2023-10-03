7504f24f61b764d059451ab4c6ca3b68fe312c42
Tina Wang <tinazwang@chromium.org>
2023-10-03 16:57:43 +0000

    Revert "[ios] Fix SetUpList crash when expand button pressed"
    This reverts commit f1b704d288587b50d9634605686bf5242678e1c0.
    
    Reason for revert: Multiple builders have compile failure because of this cl.
    
    https://ci.chromium.org/ui/p/chrome/builders/official/ios/9561/overview
    https://ci.chromium.org/ui/p/chrome/builders/ci/ios-device/815/overview
    
    Original change's description:
    > [ios] Fix SetUpList crash when expand button pressed
    >
    > The crash happens when the expand button is somehow pressed when there
    > are no other items in the list. This causes an exception to be thrown
    > because we are trying to insert at a specific index, right before the
    > expand button.
    >
    > It turns out that when the "All Set" view is shown, the other list items
    > were not being properly removed (including the expand button). This
    > change fixes that.
    >
    > Fixed: 1487698
    > Change-Id: I4e13f5ca36cb40e419dd458fc9279a5b288083c6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4902201
    > Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Scott Yoder <scottyoder@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1204193}
    
    Change-Id: I8ae6ee877b999b47a0b08355153d886e17b7ed79
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4906148
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1204713}

--- ios/chrome/browser/ui/content_suggestions/set_up_list/set_up_list_view.mm
+++ ios/chrome/browser/ui/content_suggestions/set_up_list/set_up_list_view.mm
@@ -150,12 +150,10 @@ constexpr NSString* const kAllSetRight = @"set_up_list_all_set_right";
 
   [_itemsStack layoutIfNeeded];
   _itemsStack.accessibilityElements = @[ allSetView ];
-  _expandButton = nil;
-  NSArray<UIView*>* itemsStackSubviews = _itemsStack.arrangedSubviews;
   __weak __typeof(_itemsStack) weakItemsStack = _itemsStack;
   [UIView animateWithDuration:kAllSetAnimationDuration.InSecondsF()
       animations:^{
-        for (UIView* view in itemsStackSubviews) {
+        for (UIView* view in weakItemsStack.arrangedSubviews) {
           view.alpha = 0;
           view.hidden = YES;
           // Constrain the old item view's position so that it doesn't move
@@ -179,7 +177,7 @@ constexpr NSString* const kAllSetRight = @"set_up_list_all_set_right";
         }
       }
       completion:^(BOOL finished) {
-        for (UIView* view in itemsStackSubviews) {
+        for (UIView* view in weakItemsStack.arrangedSubviews) {
           if (view != allSetView) {
             [view removeFromSuperview];
           }
@@ -440,9 +438,7 @@ constexpr NSString* const kAllSetRight = @"set_up_list_all_set_right";
   _expandButton.accessibilityLabel =
       l10n_util::GetNSString(IDS_IOS_SET_UP_LIST_COLLAPSE);
 
-  // Insert new items just before the expand button.
-  int index = [_itemsStack.arrangedSubviews indexOfObject:_expandButton];
-  CHECK_NE(index, NSNotFound);
+  int index = 2;
   for (SetUpListItemView* item in items) {
     item.alpha = 0;
     item.hidden = YES;
