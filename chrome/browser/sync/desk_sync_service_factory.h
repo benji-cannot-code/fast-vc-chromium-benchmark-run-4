510bda7aa10a814a7060801a81616869f71b9791
Arthur Milchior <arthurmilchior@chromium.org>
2023-10-03 12:36:25 +0000

    DeskSyncServiceFactory: cleanup of the build method
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: Ib921bc507a1498cd2c58ae8fa8d39478c9a2fddd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4801391
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204585}

--- chrome/browser/sync/desk_sync_service_factory.h
+++ chrome/browser/sync/desk_sync_service_factory.h
@@ -34,7 +34,7 @@ class DeskSyncServiceFactory : public ProfileKeyedServiceFactory {
   ~DeskSyncServiceFactory() override = default;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
