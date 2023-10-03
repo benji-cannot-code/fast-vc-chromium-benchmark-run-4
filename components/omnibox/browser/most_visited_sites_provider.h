b28255c6ec48f18b6ccb4bf9405bc50ee4088e8b
Tomasz Wiszkowski <ender@google.com>
2023-10-03 21:31:59 +0000

    Reduce relevance of the off-screen most visited tiles.
    This change drops off-screen Most visited sites and searches relevance
    enabling the de-duplication algorithm to do the following:
    - Search suggestions below the carousel are deduplicated to repeatable
      queries listed in the exposed part of the carousel (first 4 items),
    - Repeatable Queries not exposed in the carousel are deduplicated to
      Search suggestions listed below the carousel.
    
    Note: GetDeviceFormFactor result is cached because of the JNI jump.
    
    Bug: 1474087
    Change-Id: Ifa2fc29f79d6be1ec5709bd43436220731c2535d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4908637
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1204866}

--- components/omnibox/browser/most_visited_sites_provider.h
+++ components/omnibox/browser/most_visited_sites_provider.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/autocomplete_provider.h"
 #include "components/omnibox/browser/autocomplete_provider_client.h"
 #include "components/omnibox/browser/autocomplete_provider_listener.h"
+#include "ui/base/device_form_factor.h"
 
 // Autocomplete provider serving Most Visited Sites in zero-prefix context.
 // Serves most frequently visited URLs in a form of either individual- or
@@ -47,6 +48,7 @@ class MostVisitedSitesProvider : public AutocompleteProvider {
 
   void BlockURL(const GURL& site_url);
 
+  const ui::DeviceFormFactor device_form_factor_;
   const raw_ptr<AutocompleteProviderClient, DanglingUntriaged> client_;
   // Note: used to cancel requests - not a general purpose WeakPtr factory.
   base::WeakPtrFactory<MostVisitedSitesProvider> request_weak_ptr_factory_{
--- components/omnibox/browser/most_visited_sites_provider_unittest.cc
+++ components/omnibox/browser/most_visited_sites_provider_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/metrics_proto/omnibox_event.pb.h"
 #include "third_party/metrics_proto/omnibox_focus_type.pb.h"
+#include "ui/base/device_form_factor.h"
 
 namespace {
 class FakeTopSites : public history::TopSites {
@@ -215,13 +216,23 @@ void MostVisitedSitesProviderTest::CheckMatchesEquivalentTo(
   } else if (ui_type == ExpectedUiType::kIndividualTiles) {
     ASSERT_EQ(urls.size(), NumMostVisitedMatches())
         << "Unexpected number of TILE matches";
+    int expected_relevance = 1600;  // kMostVisitedTilesIndividualHighRelevance
     for (const auto& match : result) {
       EXPECT_EQ(match.type, AutocompleteMatchType::TILE_MOST_VISITED_SITE);
       EXPECT_EQ(urls[match_index].url, match.destination_url)
           << "Invalid Match URL at position " << match_index;
       EXPECT_EQ(urls[match_index].title, match.description)
           << "Invalid Match Title at position " << match_index;
+      EXPECT_EQ(expected_relevance, match.relevance)
+          << "Invalid Match Relevance at position " << match_index;
       ++match_index;
+      // Degrade relevance of partially visible and invisible matches.
+      if (match_index == 4 &&
+          ui::GetDeviceFormFactor() ==
+              ui::DeviceFormFactor::DEVICE_FORM_FACTOR_PHONE) {
+        expected_relevance = 100;  // kMostVisitedTilesIndividualLowRelevance
+      }
+      --expected_relevance;
     }
   }
 }
@@ -476,6 +487,7 @@ TEST_F(MostVisitedSitesProviderTest,
   EXPECT_EQ(0u, NumMostVisitedMatches());
   // Accept only direct TopSites data.
   EXPECT_TRUE(top_sites_->EmitURLs());
+  EXPECT_EQ(5u, top_sites_->urls().size());
   CheckMatchesEquivalentTo(top_sites_->urls(),
                            ExpectedUiType::kIndividualTiles);
 }
