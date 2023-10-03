9a65bf3bbf2417102209c03714437a2da4a8e3bd
Zac Partridge <zacpartridge@google.com>
2023-10-03 05:45:46 +0000

    strip whitespace and symbols from text length
    Just stripping {' ', '\t', '\n', '.', ','} for now
    
    Bug: b:302227048
    Change-Id: I4de13f0fb43310ded7dfc9526e6f8d3e14ce9ba1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4902404
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Zac Partridge <zacpartridge@google.com>
    Cr-Commit-Position: refs/heads/main@{#1204475}

--- chrome/browser/ash/input_method/editor_mediator.cc
+++ chrome/browser/ash/input_method/editor_mediator.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "base/check_op.h"
+#include "base/containers/contains.h"
+#include "base/containers/fixed_flat_set.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/ui/webui/ash/mako/mako_bubble_coordinator.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -21,6 +23,25 @@ namespace ash::input_method {
 namespace {
 
 EditorMediator* g_instance_ = nullptr;
+constexpr auto striped_symbols =
+    base::MakeFixedFlatSet<char>({' ', '\t', '\n', '.', ','});
+
+size_t NonWhitespaceAndSymbolsLength(const std::u16string& text,
+                                     gfx::Range selection_range) {
+  size_t start = selection_range.start();
+  while (start < selection_range.end() &&
+         striped_symbols.contains(text[start])) {
+    start++;
+  }
+
+  size_t end = selection_range.end();
+  while (end > selection_range.start() && end < text.length() &&
+         striped_symbols.contains(text[end])) {
+    end--;
+  }
+
+  return std::max(static_cast<int>(end) - static_cast<int>(start), 0);
+}
 
 }  // namespace
 
@@ -161,7 +182,8 @@ void EditorMediator::OnSurroundingTextChanged(const std::u16string& text,
   if (editor_event_proxy_ != nullptr) {
     editor_event_proxy_->OnSurroundingTextChanged(text, selection_range);
   }
-  editor_switch_->OnTextSelectionLengthChanged(selection_range.length());
+  size_t selected_length = NonWhitespaceAndSymbolsLength(text, selection_range);
+  editor_switch_->OnTextSelectionLengthChanged(selected_length);
 }
 
 void EditorMediator::ProcessConsentAction(ConsentAction consent_action) {
