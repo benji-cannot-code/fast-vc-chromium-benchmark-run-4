749ac6d732dc7d2009c48edc5f5dfcacbe03a7f8
punithnayak <npunith125@gmail.com>
2023-10-03 00:09:41 +0000

    Dangling pointer in content/browser/media/audio_stream_monitor_unittest.cc
    Bug: 1485840
    Change-Id: Id2c9f47c0aa7c94cd4af25c0668bd112bed4ea48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4904446
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204371}

--- content/browser/media/audio_stream_monitor_unittest.cc
+++ content/browser/media/audio_stream_monitor_unittest.cc
@@ -71,6 +71,11 @@ class AudioStreamMonitorTest : public RenderViewHostTestHarness {
     const_cast<const base::TickClock*&>(monitor_->clock_) = &clock_;
   }
 
+  void TearDown() override {
+    monitor_ = nullptr;
+    RenderViewHostTestHarness::TearDown();
+  }
+
   base::TimeTicks GetTestClockTime() { return clock_.NowTicks(); }
 
   void AdvanceClock(const base::TimeDelta& delta) { clock_.Advance(delta); }
@@ -164,7 +169,7 @@ class AudioStreamMonitorTest : public RenderViewHostTestHarness {
   WebContents* web_contents() { return monitor_->web_contents_; }
 
  protected:
-  raw_ptr<AudioStreamMonitor, DanglingUntriaged> monitor_;
+  raw_ptr<AudioStreamMonitor> monitor_;
 
  private:
   void ExpectWasRecentlyAudible() const {
