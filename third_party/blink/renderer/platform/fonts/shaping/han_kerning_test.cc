a4543082847812907141fb655f274df05d497761
Koji Ishii <kojii@chromium.org>
2023-10-03 15:29:43 +0000

    [text-spacing-trim] Avoid redundant entries in features
    This patch fixes two redundant operations:
    1. `ShapeSegment` can run multiple times for the same range
       when there are font cascading or fallback. `HanKerning`
       adds features each time, causing redundant entries.
       This patch fixes this by resetting the `FontFeatures`,
       similar to what `CapsFeatureSettingsScopedOverlay` does.
    2. `ShapeSegment` gives the whole range given to `Shape`, not
       the range of the segment it is currently shaping. This lets
       `HanKerning` to scan the same text multiple times.
    
    These redundancy is not observable, as HarfBuzz ignores
    redundant font features, other than the unnecessarily wasted
    time and memory.
    
    The `FontFeature` argument is changed from a reference to a
    pointer because `HanKerning` will keep the pointer until it
    is destructed.
    
    This patch has no behavior changes.
    
    Bug: 1463891
    Change-Id: I669f4dec230aec3d99535bb76e36eddfe487466e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905013
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204671}

--- third_party/blink/renderer/platform/fonts/shaping/han_kerning_test.cc
+++ third_party/blink/renderer/platform/fonts/shaping/han_kerning_test.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2023 The Chromium Authors
+﻿// Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <testing/gtest/include/gtest/gtest.h>
 
 #include "third_party/blink/renderer/platform/fonts/font.h"
+#include "third_party/blink/renderer/platform/fonts/shaping/font_features.h"
 #include "third_party/blink/renderer/platform/testing/font_test_helpers.h"
+#include "third_party/blink/renderer/platform/testing/runtime_enabled_features_test_helpers.h"
 #include "third_party/blink/renderer/platform/testing/unit_test_helpers.h"
 
 namespace blink {
@@ -20,11 +22,14 @@ Font CreateNotoCjk() {
   return blink::test::CreateTestFont(
       AtomicString("Noto Sans CJK"),
       blink::test::BlinkWebTestsFontsTestDataPath(
-          "noto/cjk/NotoSansCJKjp-Regular-subset-chws.otf"),
+          "noto/cjk/NotoSansCJKjp-Regular-subset-halt.otf"),
       16.0);
 }
 
-class HanKerningTest : public testing::Test {};
+class HanKerningTest : public testing::Test, ScopedCSSTextSpacingTrimForTest {
+ public:
+  explicit HanKerningTest() : ScopedCSSTextSpacingTrimForTest(true) {}
+};
 
 TEST_F(HanKerningTest, FontDataHorizontal) {
   Font noto_cjk = CreateNotoCjk();
@@ -81,6 +86,23 @@ TEST_F(HanKerningTest, FontDataVertical) {
   EXPECT_EQ(zhs_data.type_for_semicolon, HanKerning::CharType::kOther);
   EXPECT_EQ(zht_data.type_for_colon, HanKerning::CharType::kOther);
   EXPECT_EQ(zht_data.type_for_semicolon, HanKerning::CharType::kOther);
+}
+
+TEST_F(HanKerningTest, ResetFeatures) {
+  Font noto_cjk = CreateNotoCjk();
+  const SimpleFontData* noto_cjk_data = noto_cjk.PrimaryFont();
+  EXPECT_TRUE(noto_cjk_data);
+  FontFeatures features;
+  features.Append(
+      {HB_TAG('T', 'E', 'S', 'T'), 1, 0, static_cast<unsigned>(-1)});
+  EXPECT_EQ(features.size(), 1u);
+  const String text(u"国）（国");
+  {
+    HanKerning han_kerning(text, 0, text.length(), *noto_cjk_data,
+                           noto_cjk.GetFontDescription(), true, &features);
+    EXPECT_EQ(features.size(), 2u);
+  }
+  EXPECT_EQ(features.size(), 1u);
 }
 
 }  // namespace
