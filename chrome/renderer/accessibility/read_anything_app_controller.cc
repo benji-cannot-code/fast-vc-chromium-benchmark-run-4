162892d0ccbc052102d9a7b4304396fb7c6e6fee
Abigail Klein <abigailbklein@google.com>
2023-10-04 20:06:27 +0000

    [Read Anything] Check that tree root is not nullptr before serializing.
    Fixed: 1443344
    Change-Id: Ic2a3e0993dc3d78c0e41bbf41356a9d6c0c63d58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4911936
    Reviewed-by: Jocelyn Tran <jocelyntran@google.com>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1205418}

--- chrome/renderer/accessibility/read_anything_app_controller.cc
+++ chrome/renderer/accessibility/read_anything_app_controller.cc
@@ -478,6 +478,9 @@ void ReadAnythingAppController::Distill() {
   ui::AXTreeSerializer<const ui::AXNode*, std::vector<const ui::AXNode*>>
       serializer(tree_source.get());
   ui::AXTreeUpdate snapshot;
+  if (!tree->root()) {
+    return;
+  }
   CHECK(serializer.SerializeChanges(tree->root(), &snapshot));
   model_.SetDistillationInProgress(true);
   distiller_->Distill(*tree, snapshot, model_.active_ukm_source_id());
@@ -701,6 +704,7 @@ gin::ObjectTemplateBuilder ReadAnythingAppController::GetObjectTemplateBuilder(
 ui::AXNodeID ReadAnythingAppController::RootId() const {
   ui::AXSerializableTree* tree =
       model_.GetTreeFromId(model_.active_tree_id()).get();
+  DCHECK(tree->root());
   return tree->root()->id();
 }
 
