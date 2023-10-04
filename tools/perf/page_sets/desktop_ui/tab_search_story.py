a7b6e56b3f0ec88bcacc217fa768fa27b07c128c
Emily Shack <emshack@chromium.org>
2023-10-04 00:01:40 +0000

    Tab Organization: Fix desktop_ui benchmark
    Bug: 1488945
    Change-Id: I772bc7e6f4b46c4b3723a588dea443c939e8f8f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4903975
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204918}

--- tools/perf/page_sets/desktop_ui/tab_search_story.py
+++ tools/perf/page_sets/desktop_ui/tab_search_story.py
@@ -348,5 +348,7 @@ class TabSearchStoryMeasureMemory3TabSearch(TabSearchStoryMeasureMemory):
 
 
 SCROLL_ELEMENT_FUNCTION = '''
-document.querySelector('tab-search-app').shadowRoot.getElementById('tabsList')
+document.querySelector('tab-search-app').shadowRoot
+        .querySelector('tab-search-page').shadowRoot
+        .getElementById('tabsList')
 '''
