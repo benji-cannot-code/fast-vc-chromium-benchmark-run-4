ca2770baf51032654a2500139aa80cb872703a31
Stephan Hartmann <stha09@googlemail.com>
2023-10-04 14:08:07 +0000

    IWYU: add stdint.h for *int64_t in commerce_subscription.h
    Bug: 957519
    Change-Id: I73beaefe15d935f5c0e91feeb8f2de44cfbadbf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905475
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Auto-Submit: Stephan Hartmann <stha09@googlemail.com>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205221}

--- components/commerce/core/subscriptions/commerce_subscription.h
+++ components/commerce/core/subscriptions/commerce_subscription.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_COMMERCE_CORE_SUBSCRIPTIONS_COMMERCE_SUBSCRIPTION_H_
 #define COMPONENTS_COMMERCE_CORE_SUBSCRIPTIONS_COMMERCE_SUBSCRIPTION_H_
 
+#include <stdint.h>
+
 #include <string>
 
 #include "third_party/abseil-cpp/absl/types/optional.h"
