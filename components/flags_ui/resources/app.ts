7d7d5aeb6bb4808efba785cdbaa2488a19dfdd42
dpapad <dpapad@chromium.org>
2023-10-04 00:19:04 +0000

    WebUI: Disallow the use of "public" modifier via ESLint.
    Per the styleguide at [1]
    
    "Never use the public modifier except when declaring non-readonly public
    parameter properties (in constructors)."
    
    Enforcing via the @typescript-eslint/explicit-member-accessibility
    ESLint rule. All violations have been fixed in a precursor CL.
    
    [1] https://google.github.io/styleguide/tsguide.html#visibility
    
    Bug: 720034
    Change-Id: I476152abba0775c1106e42dbed1cbb4d7fcd310a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4908169
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204948}

--- components/flags_ui/resources/app.ts
+++ components/flags_ui/resources/app.ts
@@ -51,7 +51,7 @@ class FlagSearch {
    * Initialises the in page search. Adds searchbox listeners and
    * collates the text elements used for string matching.
    */
-  public init() {
+  init() {
     if (this.initialized) {
       return;
     }
@@ -83,7 +83,7 @@ class FlagSearch {
   /**
    * Clears a search showing all experiments.
    */
-  public clearSearch() {
+  clearSearch() {
     this.searchBox.value = '';
     this.doSearch();
   }
@@ -194,7 +194,7 @@ export class FlagsAppElement extends CustomElement {
       this.getRequiredElement<HTMLButtonElement>('#experiment-restart-button');
   // </if>
 
-  public tabs: Tab[] = [
+  tabs: Tab[] = [
     {
       tabEl: this.getRequiredElement('#tab-available'),
       panelEl: this.getRequiredElement('#tab-content-available'),
@@ -229,7 +229,7 @@ export class FlagsAppElement extends CustomElement {
    * Cause a text string to be announced by screen readers
    * @param text The text that should be announced.
    */
-  public announceStatus(text: string) {
+  announceStatus(text: string) {
     this.getRequiredElement('#screen-reader-status-message').textContent = '';
     setTimeout(() => {
       this.getRequiredElement('#screen-reader-status-message').textContent =
