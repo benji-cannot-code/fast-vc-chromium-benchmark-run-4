53efb008526e0169513f687540c1c6a4d7cfd7c5
cassycc <cassycc@google.com>
2023-10-04 02:58:25 +0000

    [Office upload] Add metric checks to DriveTest tests
    Check that the right metrics were logged in DriveTest tests.
    
    Change-Id: I045acc27eca8a479d91f0b0fe072bec089ae00d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4910631
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205015}

--- chrome/browser/ash/file_manager/file_tasks_browsertest.cc
+++ chrome/browser/ash/file_manager/file_tasks_browsertest.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/file_manager/office_file_tasks.h"
 #include "chrome/browser/ash/file_manager/path_util.h"
 #include "chrome/browser/ash/file_manager/url_util.h"
+#include "chrome/browser/ash/file_manager/volume.h"
 #include "chrome/browser/ash/file_manager/volume_manager.h"
 #include "chrome/browser/ash/file_system_provider/provided_file_system_info.h"
 #include "chrome/browser/ash/file_system_provider/provider_interface.h"
@@ -1198,6 +1199,7 @@ class DriveTest : public TestAccountBrowserTest {
       "https://docs.google.com/document/d/smalldocxid?rtpof=true&usp=drive_fs";
   FileSystemURL drive_test_file_url_;
   std::unique_ptr<ash::cloud_upload::CloudOpenMetrics> cloud_open_metrics_;
+  base::HistogramTester histogram_;
 
  private:
   base::ScopedTempDir temp_dir_;
@@ -1217,8 +1219,8 @@ class DriveTest : public TestAccountBrowserTest {
 };
 
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-// Test to check that the test file fails to open when the system is offline but
-// is successfully opened with a "try-again" dialog choice after the
+// Test to check that a DriveFS test file fails to open when the system is
+// offline but is successfully opened with a "try-again" dialog choice after the
 // systems comes online.
 IN_PROC_BROWSER_TEST_F(DriveTest, OfficeFallbackTryAgain) {
   // Add test file to fake DriveFs.
@@ -1267,6 +1269,17 @@ IN_PROC_BROWSER_TEST_F(DriveTest, OfficeFallbackTryAgain) {
 
   // Wait for file to open in web drive office.
   navigation_observer_office.Wait();
+
+  histogram_.ExpectUniqueSample(ash::cloud_upload::kDriveOpenSourceVolumeMetric,
+                                VolumeType::VOLUME_TYPE_GOOGLE_DRIVE, 1);
+  histogram_.ExpectUniqueSample(ash::cloud_upload::kDriveTransferRequiredMetric,
+                                OfficeFilesTransferRequired::kNotRequired, 1);
+  histogram_.ExpectUniqueSample(
+      ash::cloud_upload::kGoogleDriveTaskResultMetricName,
+      ash::cloud_upload::OfficeTaskResult::kOpened, 1);
+  histogram_.ExpectUniqueSample(
+      ash::cloud_upload::kDriveErrorMetricName,
+      ash::cloud_upload::OfficeDriveOpenErrors::kSuccess, 1);
 }
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 
@@ -1293,6 +1306,17 @@ IN_PROC_BROWSER_TEST_F(DriveTest, OpenFileInDrive) {
 
   // Wait for file to open in web drive office.
   navigation_observer_office.Wait();
+
+  histogram_.ExpectUniqueSample(ash::cloud_upload::kDriveOpenSourceVolumeMetric,
+                                VolumeType::VOLUME_TYPE_GOOGLE_DRIVE, 1);
+  histogram_.ExpectUniqueSample(ash::cloud_upload::kDriveTransferRequiredMetric,
+                                OfficeFilesTransferRequired::kNotRequired, 1);
+  histogram_.ExpectUniqueSample(
+      ash::cloud_upload::kGoogleDriveTaskResultMetricName,
+      ash::cloud_upload::OfficeTaskResult::kOpened, 1);
+  histogram_.ExpectUniqueSample(
+      ash::cloud_upload::kDriveErrorMetricName,
+      ash::cloud_upload::OfficeDriveOpenErrors::kSuccess, 1);
 }
 
 // Test that the setup flow for office files, that has never been run before,
