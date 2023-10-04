5790051b314d98901e673e0816bc35145283b6bd
Lei Zhang <thestig@chromium.org>
2023-10-04 22:48:01 +0000

    Move the SelectFileDialog::Listener out of VmApplicationsServiceProvider
    Make a dedicated class to implement the SelectFileDialog::Listener,
    instead of in VmApplicationsServiceProvider directly. Then the dedicated
    class can call ListenerDestroyed() in its dtor to fulfill the
    requirement from SelectFileDialog. The newly added class also helps
    separate the core VmApplicationsServiceProvider responsibilities from
    the file selection handling.
    
    Fix some nits along the way. In particular, change
    ParseSelectFileDialogFileTypes() to a static method.
    
    Low-Coverage-Reason: OTHER cleaning up existing code with low test coverage
    Change-Id: I84f48503575b95361b7994edad2f50a46d325b18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4900736
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205528}

--- chrome/browser/ash/dbus/vm/vm_applications_service_provider.h
+++ chrome/browser/ash/dbus/vm/vm_applications_service_provider.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_DBUS_VM_VM_APPLICATIONS_SERVICE_PROVIDER_H_
 #define CHROME_BROWSER_ASH_DBUS_VM_VM_APPLICATIONS_SERVICE_PROVIDER_H_
 
+#include <string>
+
 #include "base/gtest_prod_util.h"
 #include "base/memory/weak_ptr.h"
 #include "chromeos/ash/components/dbus/services/cros_dbus_service.h"
@@ -21,8 +23,7 @@ namespace ash {
 // This class exports D-Bus methods for functions that we want to be available
 // to the Crostini container.
 class VmApplicationsServiceProvider
-    : public CrosDBusService::ServiceProviderInterface,
-      public ui::SelectFileDialog::Listener {
+    : public CrosDBusService::ServiceProviderInterface {
  public:
   VmApplicationsServiceProvider();
 
@@ -56,18 +57,11 @@ class VmApplicationsServiceProvider
   void SelectFile(dbus::MethodCall* method_call,
                   dbus::ExportedObject::ResponseSender response_sender);
 
-  void ParseSelectFileDialogFileTypes(
+  // Exposed in the header so unit tests can call it.
+  static void ParseSelectFileDialogFileTypes(
       const std::string& allowed_extensions,
       ui::SelectFileDialog::FileTypeInfo* file_types,
-      int* file_type_index) const;
-
-  // ui::SelectFileDialog::Listener implementation.
-  void FileSelected(const base::FilePath& path,
-                    int index,
-                    void* params) override;
-  void MultiFilesSelected(const std::vector<base::FilePath>& files,
-                          void* params) override;
-  void FileSelectionCanceled(void* params) override;
+      int* file_type_index);
 
   base::WeakPtrFactory<VmApplicationsServiceProvider> weak_ptr_factory_{this};
 };
