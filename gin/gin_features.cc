3e00e70c3216aa97df1ae83eb3abe8618291c71d
Andreas Haas <ahaas@chromium.org>
2023-10-04 08:17:57 +0000

    [v8][wasm] Add blink flag for the generic wasm-to-js wrapper
    Change-Id: Idc3f10fb0ca07b3ee021a38908130bb4f97bfd6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905195
    Reviewed-by: Adam Klein <adamk@chromium.org>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205099}

--- gin/gin_features.cc
+++ gin/gin_features.cc
@@ -257,4 +257,8 @@ BASE_FEATURE(kWebAssemblyInlining,
              "WebAssemblyInlining",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Enable the generic wasm-to-js wrapper.
+BASE_FEATURE(kWebAssemblyGenericWrapper,
+             "WebAssemblyGenericWrapper",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 }  // namespace features
