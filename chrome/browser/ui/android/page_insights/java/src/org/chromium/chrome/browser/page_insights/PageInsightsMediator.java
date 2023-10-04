89a67fed14b9cf25e3f4ccd1be1a7f7cb543324a
Kamal Choudhury <kamalchoudhury@google.com>
2023-10-04 16:05:55 +0000

    [gbb-cct] Implementing Privacy Notice for Page Insights Hub
    Screenshot: https://screenshot.googleplex.com/5VdsZNN3wN5SERU
    
    Bug: 1484017
    Bug: b/299068436
    Change-Id: Iebc866f8a4391bbf5291c1b16bacf1087493c12b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4903463
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Kamal Choudhury <kamalchoudhury@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1205294}

--- chrome/browser/ui/android/page_insights/java/src/org/chromium/chrome/browser/page_insights/PageInsightsMediator.java
+++ chrome/browser/ui/android/page_insights/java/src/org/chromium/chrome/browser/page_insights/PageInsightsMediator.java
@@ -41,6 +41,8 @@ import org.chromium.components.browser_ui.bottomsheet.BottomSheetObserver;
 import org.chromium.components.browser_ui.bottomsheet.EmptyBottomSheetObserver;
 import org.chromium.components.browser_ui.bottomsheet.ExpandedSheetHelper;
 import org.chromium.components.browser_ui.bottomsheet.ManagedBottomSheetController;
+import org.chromium.components.embedder_support.util.UrlConstants;
+import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.ui.util.ColorUtils;
 import org.chromium.url.GURL;
 
@@ -149,7 +151,8 @@ public class PageInsightsMediator extends EmptyTabObserver implements BottomShee
             BrowserControlsSizer browserControlsSizer, BooleanSupplier isPageInsightsHubEnabled,
             long firstLoadTimeMs) {
         mContext = context;
-        mSheetContent = new PageInsightsSheetContent(mContext);
+        mSheetContent =
+                new PageInsightsSheetContent(mContext, view -> loadMyActivityUrl(tabObservable));
         mSheetController = bottomSheetController;
         mBottomUiController = bottomUiController;
         mCurrentTime = System::currentTimeMillis;
@@ -320,6 +323,13 @@ public class PageInsightsMediator extends EmptyTabObserver implements BottomShee
                 mSheetContent.showChildPage(
                         getXSurfaceView(currPage.getElementsOutput()), currPage.getTitle());
             }
+        }
+    }
+
+    private void loadMyActivityUrl(Supplier<Tab> currTabObserver) {
+        Tab currTab = currTabObserver.get();
+        if (currTab != null) {
+            currTab.loadUrl(new LoadUrlParams(UrlConstants.MY_ACTIVITY_HOME_URL));
         }
     }
 
