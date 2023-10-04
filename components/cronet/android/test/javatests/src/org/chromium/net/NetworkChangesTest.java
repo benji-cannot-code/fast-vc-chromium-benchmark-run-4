7d05b8b22ab22c386fcb5839bf1985bd1e92a806
Stefano Duo <stefanoduo@google.com>
2023-10-04 14:34:45 +0000

    Make waitForStatus do what it's supposed to do
    The expectation for waitForStatus is to block until a certain state is
    reached. Confusingly, UrlRequest.StatusListener gets called only once.
    This means that waitForStatus would unblock only if we managed to
    luckily call UrlRequest#getStatus at the right point in time (i.e. when
    it would return the state we're waiting for).
    
    To circumvent that, the listener registered by waitForStatus has to
    re-register itself whenever it doesn't receive the expected state.
    
    Bug: b:290583020
    Change-Id: I15bbe8e76139ff09915c61d3ceda2272a8ba6b3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4900709
    Reviewed-by: Chidera Olibie <colibie@google.com>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1205234}

--- components/cronet/android/test/javatests/src/org/chromium/net/NetworkChangesTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/NetworkChangesTest.java
@@ -290,7 +290,7 @@ public class NetworkChangesTest {
 
         // Without this synchronization it seems that the default network change can happen before
         // the underlying QUIC session is created (read: the test would be flaky).
-        waitForStatus(request, UrlRequest.Status.IDLE);
+        waitForStatus(request, UrlRequest.Status.WAITING_FOR_RESPONSE);
         networks.swapDefaultNetwork();
 
         // Similarly to tests below, 15s should be enough for the NCN notification to propagate.
@@ -333,7 +333,7 @@ public class NetworkChangesTest {
 
         // Without this synchronization it seems that the default network change can happen before
         // the underlying QUIC session is created (read: the test would be flaky).
-        waitForStatus(request, UrlRequest.Status.IDLE);
+        waitForStatus(request, UrlRequest.Status.WAITING_FOR_RESPONSE);
         networks.swapDefaultNetwork();
 
         callback.blockForDone();
@@ -370,7 +370,7 @@ public class NetworkChangesTest {
 
         // Without this synchronization it seems that the default network change can happen before
         // the underlying QUIC session is created (read: the test would be flaky).
-        waitForStatus(request, UrlRequest.Status.IDLE);
+        waitForStatus(request, UrlRequest.Status.WAITING_FOR_RESPONSE);
         networks.swapDefaultNetwork();
 
         callback.blockForDone();
@@ -407,7 +407,7 @@ public class NetworkChangesTest {
 
         // Without this synchronization it seems that the default network change can happen before
         // the underlying QUIC session is created (read: the test would be flaky).
-        waitForStatus(request, UrlRequest.Status.IDLE);
+        waitForStatus(request, UrlRequest.Status.WAITING_FOR_RESPONSE);
         networks.swapDefaultNetwork();
         // Back to previous default network.
         networks.swapDefaultNetwork();
@@ -447,7 +447,7 @@ public class NetworkChangesTest {
 
         // Without this synchronization it seems that the default network change can happen before
         // the underlying QUIC session is created (read: the test would be flaky).
-        waitForStatus(request, UrlRequest.Status.IDLE);
+        waitForStatus(request, UrlRequest.Status.WAITING_FOR_RESPONSE);
         networks.swapDefaultNetwork();
         networks.disconnectNonDefaultNetwork();
 
@@ -490,7 +490,7 @@ public class NetworkChangesTest {
 
         // Without this synchronization it seems that the default network change can happen before
         // the underlying QUIC session is created (read: the test would be flaky).
-        waitForStatus(request, UrlRequest.Status.IDLE);
+        waitForStatus(request, UrlRequest.Status.WAITING_FOR_RESPONSE);
         networks.swapDefaultNetwork();
         networks.disconnectNonDefaultNetwork();
 
@@ -530,7 +530,7 @@ public class NetworkChangesTest {
 
         // Without this synchronization it seems that the default network change can happen before
         // the underlying QUIC session is created (read: the test would be flaky).
-        waitForStatus(request, UrlRequest.Status.IDLE);
+        waitForStatus(request, UrlRequest.Status.WAITING_FOR_RESPONSE);
         networks.disconnectDefaultNetwork();
         networks.connectDefaultNetwork();
 
@@ -732,14 +732,33 @@ public class NetworkChangesTest {
 
     private static void waitForStatus(UrlRequest request, int targetStatus) {
         final ConditionVariable cv = new ConditionVariable(/*open=*/false);
-        request.getStatus(new UrlRequest.StatusListener() {
+        UrlRequest.StatusListener listener = new UrlRequest.StatusListener() {
             @Override
             public void onStatus(int status) {
-                if (status == targetStatus) {
+                // We are not guaranteed to receive every single state update: we might register
+                // the listener too late, missing what we're looking for.
+                // Hence, we should unblock also if we see a status that can only happen after the
+                // one we're waiting for (this works under the assumption that "value ordering" of
+                // states represent the "happens-after ordering" of states, which is true at the
+                // moment).
+                if (status >= targetStatus) {
                     cv.open();
+                } else {
+                    // Very confusingly, UrlRequest.StatusListener#onStatus gets called once.
+                    // We want to keep getting called until we reach the target status, so
+                    // re-register the listener. This effectively means that we're busy-polling the
+                    // state, but this is test code, so it's not too bad. Sleep a bit to avoid
+                    // potential locks contention.
+                    try {
+                        Thread.sleep(/*millis=*/100);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                    request.getStatus(this);
                 }
             }
-        });
+        };
+        request.getStatus(listener);
         cv.block();
     }
 
