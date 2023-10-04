7d68247550031f922ecdef8532183dd29cb2c68d
Glenn Hartmann <hartmanng@chromium.org>
2023-10-04 00:11:46 +0000

    Move web_app::AppId and web_app::ManifestId to a more public place
    So that Android and Desktop implementations can share the types.
    
    Note that this is part of a multi-CL change. This CL only touches
    references in /chrome/browser/apps/platform_apps.
    
    BUG=1486624
    
    This CL was uploaded by git cl split.
    
    R=mgiuca@chromium.org
    
    Change-Id: Ic94fc612546e01012f97239ee14744881d17c239
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4887575
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Auto-Submit: Glenn Hartmann <hartmanng@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204935}

--- chrome/browser/apps/platform_apps/extension_app_shim_manager_delegate_mac.cc
+++ chrome/browser/apps/platform_apps/extension_app_shim_manager_delegate_mac.cc
@@ -107,7 +107,7 @@ ExtensionAppShimManagerDelegate::~ExtensionAppShimManagerDelegate() = default;
 
 bool ExtensionAppShimManagerDelegate::ShowAppWindows(
     Profile* profile,
-    const web_app::AppId& app_id) {
+    const webapps::AppId& app_id) {
   AppWindowList windows =
       AppWindowRegistry::Get(profile)->GetAppWindowsForApp(app_id);
   for (extensions::AppWindow* window : base::Reversed(windows)) {
@@ -119,7 +119,7 @@ bool ExtensionAppShimManagerDelegate::ShowAppWindows(
 
 void ExtensionAppShimManagerDelegate::CloseAppWindows(
     Profile* profile,
-    const web_app::AppId& app_id) {
+    const webapps::AppId& app_id) {
   AppWindowList windows =
       AppWindowRegistry::Get(profile)->GetAppWindowsForApp(app_id);
   for (auto it = windows.begin(); it != windows.end(); ++it) {
@@ -130,14 +130,14 @@ void ExtensionAppShimManagerDelegate::CloseAppWindows(
 
 bool ExtensionAppShimManagerDelegate::AppIsInstalled(
     Profile* profile,
-    const web_app::AppId& app_id) {
+    const webapps::AppId& app_id) {
   const Extension* extension = MaybeGetAppExtension(profile, app_id);
   return profile && extension;
 }
 
 bool ExtensionAppShimManagerDelegate::AppCanCreateHost(
     Profile* profile,
-    const web_app::AppId& app_id) {
+    const webapps::AppId& app_id) {
   const Extension* extension = MaybeGetAppExtension(profile, app_id);
   if (!profile || !extension)
     return false;
@@ -153,13 +153,13 @@ bool ExtensionAppShimManagerDelegate::AppCanCreateHost(
 
 bool ExtensionAppShimManagerDelegate::AppIsMultiProfile(
     Profile* profile,
-    const web_app::AppId& app_id) {
+    const webapps::AppId& app_id) {
   return false;
 }
 
 bool ExtensionAppShimManagerDelegate::AppUsesRemoteCocoa(
     Profile* profile,
-    const web_app::AppId& app_id) {
+    const webapps::AppId& app_id) {
   const Extension* extension = MaybeGetAppExtension(profile, app_id);
   if (!profile || !extension)
     return false;
@@ -174,7 +174,7 @@ bool ExtensionAppShimManagerDelegate::AppUsesRemoteCocoa(
 
 void ExtensionAppShimManagerDelegate::EnableExtension(
     Profile* profile,
-    const web_app::AppId& app_id,
+    const webapps::AppId& app_id,
     base::OnceCallback<void()> callback) {
   const Extension* extension = MaybeGetAppExtension(profile, app_id);
   if (extension)
@@ -185,7 +185,7 @@ void ExtensionAppShimManagerDelegate::EnableExtension(
 
 void ExtensionAppShimManagerDelegate::LaunchApp(
     Profile* profile,
-    const web_app::AppId& app_id,
+    const webapps::AppId& app_id,
     const std::vector<base::FilePath>& files,
     const std::vector<GURL>& urls,
     const GURL& override_url,
@@ -224,7 +224,7 @@ void ExtensionAppShimManagerDelegate::LaunchApp(
 
 void ExtensionAppShimManagerDelegate::LaunchShim(
     Profile* profile,
-    const web_app::AppId& app_id,
+    const webapps::AppId& app_id,
     web_app::LaunchShimUpdateBehavior update_behavior,
     web_app::ShimLaunchMode launch_mode,
     apps::ShimLaunchedCallback launched_callback,
@@ -258,7 +258,7 @@ bool ExtensionAppShimManagerDelegate::HasNonBookmarkAppWindowsOpen() {
 std::vector<chrome::mojom::ApplicationDockMenuItemPtr>
 ExtensionAppShimManagerDelegate::GetAppShortcutsMenuItemInfos(
     Profile* profile,
-    const web_app::AppId& app_id) {
+    const webapps::AppId& app_id) {
   return std::vector<chrome::mojom::ApplicationDockMenuItemPtr>();
 }
 
