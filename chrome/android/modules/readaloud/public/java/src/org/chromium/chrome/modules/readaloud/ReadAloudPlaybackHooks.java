343498bf58742bf8f77c9380eb9d5f54415b0d2d
Basia Zimirska <basiaz@google.com>
2023-10-04 22:38:44 +0000

    Add interface for the highlighter
    Low-Coverage-Reason: Trivial change adding only a new interface
    Bug: b/300941591
    Change-Id: I86d3683efd87ba1e1df30e938fc1841615e72366
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4903059
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Basia Zimirska <basiaz@google.com>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205517}

--- chrome/android/modules/readaloud/public/java/src/org/chromium/chrome/modules/readaloud/ReadAloudPlaybackHooks.java
+++ chrome/android/modules/readaloud/public/java/src/org/chromium/chrome/modules/readaloud/ReadAloudPlaybackHooks.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.modules.readaloud;
 
 import android.view.ViewStub;
 
+import org.chromium.chrome.modules.readaloud.contentjs.Highlighter;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 
 /** Interface for creating ReadAloud playback. */
@@ -58,5 +59,10 @@ public interface ReadAloudPlaybackHooks {
      */
     default Player createPlayer(ViewStub miniPlayerViewStub, Player.Delegate delegate) {
         return new Player() {};
+    }
+
+    /** Creates the Highlighter. */
+    default Highlighter createHighlighter() {
+        return new Highlighter() {};
     }
 }
