19e42062726f94cb690a579ae5ec0457d479d886
Ivana Žužić <izuzic@google.com>
2023-10-04 15:47:33 +0000

    [Gardener] Disable the failing tests in PriceInsightsIconViewInteractiveTest
    Disable the tests that are consistently failing due to uninteresting
    mock function calls.
    
    Bug: 1489243
    Change-Id: I0cce431217c177517451dbdf55c7563c2c45b46e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4911177
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Owners-Override: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Salma Elmahallawy <esalma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1205283}

--- chrome/browser/ui/views/commerce/price_insights_icon_view_interactive_uitest.cc
+++ chrome/browser/ui/views/commerce/price_insights_icon_view_interactive_uitest.cc
@@ -149,8 +149,9 @@ class PriceInsightsIconViewInteractiveTest : public InteractiveBrowserTest {
       this};
 };
 
+// TODO(crbug.com/1489243): Resolve uninteresting mock function calls.
 IN_PROC_BROWSER_TEST_F(PriceInsightsIconViewInteractiveTest,
-                       SidePanelShownOnPress) {
+                       DISABLED_SidePanelShownOnPress) {
   EXPECT_CALL(*mock_shopping_service_, GetProductInfoForUrl);
   EXPECT_CALL(*mock_shopping_service_, GetPriceInsightsInfoForUrl);
 
@@ -176,8 +177,9 @@ IN_PROC_BROWSER_TEST_F(PriceInsightsIconViewInteractiveTest,
       "Commerce.PriceInsights.OmniboxIconClickedAfterLabelShown", 2);
 }
 
+// TODO(crbug.com/1489243): Resolve uninteresting mock function calls.
 IN_PROC_BROWSER_TEST_F(PriceInsightsIconViewInteractiveTest,
-                       IconIsNotHighlightedAfterClicking) {
+                       DISABLED_IconIsNotHighlightedAfterClicking) {
   EXPECT_CALL(*mock_shopping_service_, GetProductInfoForUrl);
   EXPECT_CALL(*mock_shopping_service_, GetPriceInsightsInfoForUrl);
 
