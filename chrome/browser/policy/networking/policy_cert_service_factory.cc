f0393914474a1fe4da99caf39a2e48dba36bc3af
Andreea Costinas <acostinas@google.com>
2023-10-04 13:53:33 +0000

    Lacros: Allow policy provided trust anchors in Incognito
    Bug: b/301059899
    Change-Id: I6ab0de6a3301aef687108688ad2f233c42deaa77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4894229
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Andreea Costinas <acostinas@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1205213}

--- chrome/browser/policy/networking/policy_cert_service_factory.cc
+++ chrome/browser/policy/networking/policy_cert_service_factory.cc
@@ -27,6 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_manager/user_manager.h"
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "chrome/browser/profiles/incognito_helpers.h"
+#endif
+
 namespace policy {
 namespace {
 
@@ -105,9 +109,17 @@ std::unique_ptr<KeyedService> BuildServiceInstanceLacros(
   if (!policy_certificate_provider)
     return nullptr;
 
+  Profile* original_profile = Profile::FromBrowserContext(
+      chrome::GetBrowserContextRedirectedInIncognito(profile));
+  // Only allow trusted policy-provided certificates for non-guest primary
+  // users. Guest users don't have user policy, but set
+  // `may_use_profile_wide_trust_anchors`=false for them out of caution against
+  // future changes.
+  bool may_use_profile_wide_trust_anchors =
+      original_profile->IsMainProfile() && !original_profile->IsGuestSession();
+
   return std::make_unique<PolicyCertService>(
-      profile, policy_certificate_provider,
-      /*may_use_profile_wide_trust_anchors=*/profile->IsMainProfile());
+      profile, policy_certificate_provider, may_use_profile_wide_trust_anchors);
 }
 #endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
 
