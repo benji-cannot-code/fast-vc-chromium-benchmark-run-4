4594ad44c79341a548045cad4b295a06171f6a3b
Ben Reich <benreich@chromium.org>
2023-10-04 03:13:09 +0000

    [Files app] Add FilesGoogleDriveSettingsPage check in ClearOfflineFiles
    The "Clean up storage" button was added in M118, however, it was guarded
    by the BulkPinning flag. This means it is visible but not clickable.
    Updated to add the extra flag for kFilesGoogleDriveSettingsPage to
    ensure it is usable.
    
    Fixed: b/302411448
    Test: browser_tests --gtest_filter=GoogleDriveHandler*
    Change-Id: I7e1a5876f28269bc5acb3d38f305f9c9579ab0ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4903577
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205021}

--- chrome/browser/ash/drive/drive_integration_service.cc
+++ chrome/browser/ash/drive/drive_integration_service.cc
@@ -1265,8 +1265,10 @@ void DriveIntegrationService::OnGetOfflineFilesSpaceUsage(
 
 void DriveIntegrationService::ClearOfflineFiles(
     base::OnceCallback<void(FileError)> callback) {
-  if (!util::IsDriveFsBulkPinningAvailable(profile_) || !IsMounted() ||
-      !GetDriveFsInterface()) {
+  if ((!util::IsDriveFsBulkPinningAvailable(profile_) &&
+       !base::FeatureList::IsEnabled(
+           ash::features::kFilesGoogleDriveSettingsPage)) ||
+      !IsMounted() || !GetDriveFsInterface()) {
     std::move(callback).Run(FILE_ERROR_SERVICE_UNAVAILABLE);
     return;
   }
