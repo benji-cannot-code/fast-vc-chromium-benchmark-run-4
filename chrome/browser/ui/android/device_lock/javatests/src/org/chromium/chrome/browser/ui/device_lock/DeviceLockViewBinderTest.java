9757aa9d3f24395a802bcd02c3355daf8bd19945
Charles Hager <clhager@google.com>
2023-10-04 18:22:19 +0000

    Add spinner ui to device lock page
    Demo (light mode): https://screencast.googleplex.com/cast/NTM1MDE0NTEzMzY0MTcyOHxhYjk5M2EyYi05Mw
    Demo (dark mode): https://screencast.googleplex.com/cast/NjAwMDEyMjYwMDA5NTc0NHw0YzhmZDQwNC1lZQ
    
    Bug: 1488992
    Change-Id: I91cdc64d0e2d3db358f0d03b1c31cbbea2ea2239
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4908965
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1205347}

--- chrome/browser/ui/android/device_lock/javatests/src/org/chromium/chrome/browser/ui/device_lock/DeviceLockViewBinderTest.java
+++ chrome/browser/ui/android/device_lock/javatests/src/org/chromium/chrome/browser/ui/device_lock/DeviceLockViewBinderTest.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.ui.device_lock;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import static org.chromium.chrome.browser.ui.device_lock.DeviceLockProperties.ALL_KEYS;
@@ -16,6 +17,7 @@ import static org.chromium.chrome.browser.ui.device_lock.DeviceLockProperties.ON
 import static org.chromium.chrome.browser.ui.device_lock.DeviceLockProperties.ON_GO_TO_OS_SETTINGS_CLICKED;
 import static org.chromium.chrome.browser.ui.device_lock.DeviceLockProperties.ON_USER_UNDERSTANDS_CLICKED;
 import static org.chromium.chrome.browser.ui.device_lock.DeviceLockProperties.PREEXISTING_DEVICE_LOCK;
+import static org.chromium.chrome.browser.ui.device_lock.DeviceLockProperties.UI_ENABLED;
 
 import android.view.View;
 import android.view.ViewGroup;
@@ -68,6 +70,7 @@ public class DeviceLockViewBinderTest extends BlankUiTestActivityTestCase {
                                  .with(PREEXISTING_DEVICE_LOCK, false)
                                  .with(DEVICE_SUPPORTS_PIN_CREATION_INTENT, true)
                                  .with(IN_SIGN_IN_FLOW, false)
+                                 .with(UI_ENABLED, true)
                                  .with(ON_CREATE_DEVICE_LOCK_CLICKED,
                                          v -> mCreateDeviceLockButtonClicked.set(true))
                                  .with(ON_GO_TO_OS_SETTINGS_CLICKED,
@@ -138,6 +141,31 @@ public class DeviceLockViewBinderTest extends BlankUiTestActivityTestCase {
                 mView.getContinueButton().getVisibility());
         assertEquals("The dismiss button should always be visible", View.VISIBLE,
                 mView.getDismissButton().getVisibility());
+    }
+
+    @Test
+    @UiThreadTest
+    @SmallTest
+    public void testDeviceLockView_uiEnabled_showsEnabledUI() {
+        mViewModel.set(UI_ENABLED, true);
+
+        assertEquals("The progress bar should not be visible.", View.GONE,
+                mView.getProgressBar().getVisibility());
+        assertTrue("The continue button should be enabled.", mView.getContinueButton().isEnabled());
+        assertTrue("The dismiss button should be enabled.", mView.getDismissButton().isEnabled());
+    }
+
+    @Test
+    @UiThreadTest
+    @SmallTest
+    public void testDeviceLockView_uiDisabled_showsDisabledUI() {
+        mViewModel.set(UI_ENABLED, false);
+
+        assertEquals("The progress bar should be visible.", View.VISIBLE,
+                mView.getProgressBar().getVisibility());
+        assertFalse(
+                "The continue button should be disabled.", mView.getContinueButton().isEnabled());
+        assertFalse("The dismiss button should be disabled.", mView.getDismissButton().isEnabled());
     }
 
     @Test
