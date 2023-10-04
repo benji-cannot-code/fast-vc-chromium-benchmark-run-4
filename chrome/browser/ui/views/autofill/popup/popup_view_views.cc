f92480a258a3e2af34bdea8889087cff680903a3
Dmitry Vykochko <vykochko@google.com>
2023-10-04 12:11:45 +0000

    Fix accidental sub-popup closing if the parent is overlapped.
    This gap allowed mouse events to reach the parent popup (which
    caused sub-popup closing when the mouse goes form the parent
    control to the sub-popup):
    https://screenshot.googleplex.com/9BqsySkcBRDyEzr
    
    Fixed by making sub-popup fit tightly on the inner sides.
    
    Bug: 1466116
    Change-Id: I7ec82e0f8f589013e5ed66afbb00f25d0f059964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4900519
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1205179}

--- chrome/browser/ui/views/autofill/popup/popup_view_views.cc
+++ chrome/browser/ui/views/autofill/popup/popup_view_views.cc
@@ -867,7 +867,7 @@ void PopupViewViews::SetCellWithOpenSubPopup(
 
     PopupRowView& row = GetPopupRowViewAt(cell_index->first);
     if (controller_->OpenSubPopup(
-            row.GetCellBounds(cell_index->second), suggestion.children,
+            row.GetControlCellBounds(), suggestion.children,
             AutoselectFirstSuggestion(selection_source ==
                                       PopupCellSelectionSource::kKeyboard))) {
       row.SetCellPermanentlyHighlighted(cell_index->second, true);
