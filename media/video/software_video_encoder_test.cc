fc207333013e58e4fe091eb887460282636653ed
Eugene Zemtsov <eugene@chromium.org>
2023-10-04 06:29:52 +0000

    media: Remove workaround for libvpx threading resize bug
    The bug was fixed in libvpx.
    Also run the same unit test on other encoders.
    
    Bug: 1486441
    Change-Id: I4313c16be2045f0c7844910fddfb34a0fd151018
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4910355
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205074}

--- media/video/software_video_encoder_test.cc
+++ media/video/software_video_encoder_test.cc
@@ -345,7 +345,6 @@ class SoftwareVideoEncoderTest
 };
 
 class H264VideoEncoderTest : public SoftwareVideoEncoderTest {};
-class VpxVideoEncoderTest : public SoftwareVideoEncoderTest {};
 class SVCVideoEncoderTest : public SoftwareVideoEncoderTest {};
 
 TEST_P(SoftwareVideoEncoderTest, StopCallbackWrapping) {
@@ -754,7 +753,7 @@ TEST_P(SVCVideoEncoderTest, ChangeLayers) {
   EXPECT_EQ(chunks.size(), total_frames_count);
 }
 
-TEST_P(VpxVideoEncoderTest, ReconfigureWithResize) {
+TEST_P(SoftwareVideoEncoderTest, ReconfigureWithResizingNumberOfThreads) {
   int outputs_count = 0;
   VideoEncoder::Options options;
   options.frame_size = gfx::Size(1024, 1024);
@@ -1156,11 +1155,6 @@ SwVideoTestParams kVpxParams[] = {
     {VideoCodec::kVP8, VP8PROFILE_ANY, PIXEL_FORMAT_I420},
     {VideoCodec::kVP8, VP8PROFILE_ANY, PIXEL_FORMAT_XRGB}};
 
-INSTANTIATE_TEST_SUITE_P(VpxSpecific,
-                         VpxVideoEncoderTest,
-                         ::testing::ValuesIn(kVpxParams),
-                         PrintTestParams);
-
 INSTANTIATE_TEST_SUITE_P(VpxGeneric,
                          SoftwareVideoEncoderTest,
                          ::testing::ValuesIn(kVpxParams),
@@ -1220,7 +1214,6 @@ INSTANTIATE_TEST_SUITE_P(Av1TemporalSvc,
 #endif  // ENABLE_LIBAOM
 
 GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(H264VideoEncoderTest);
-GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(VpxVideoEncoderTest);
 GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(SVCVideoEncoderTest);
 GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(SoftwareVideoEncoderTest);
 
