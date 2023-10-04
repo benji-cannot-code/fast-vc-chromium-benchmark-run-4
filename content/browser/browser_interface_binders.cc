db3742a1c0333b76b361f2b323c5690ed5453047
Enrico Galli <enrico.galli@intel.com>
2023-10-04 01:41:48 +0000

    WebNN: Implement computeSync method
    This CL implements the MLContext::computeSync()[1] method which is only
    exposed to dedicated workers.
    
    [1] https://www.w3.org/TR/webnn/#dom-mlcontext-computesync
    
    Bug: 1273291
    Change-Id: I341c315ee6cc1e0c46fa8e7c50390305efda532c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4893414
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Commit-Queue: Enrico Galli <enrico.galli@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1204989}

--- content/browser/browser_interface_binders.cc
+++ content/browser/browser_interface_binders.cc
@@ -289,10 +289,18 @@ void BindTextDetection(
 }
 
 #if !BUILDFLAG(IS_CHROMEOS)
-void BindWebNNContextProvider(
+void BindWebNNContextProviderForRenderFrame(
     RenderFrameHost* host,
     mojo::PendingReceiver<webnn::mojom::WebNNContextProvider> receiver) {
   auto* process_host = static_cast<RenderProcessHostImpl*>(host->GetProcess());
+  process_host->GetGpuClient()->BindWebNNContextProvider(std::move(receiver));
+}
+
+void BindWebNNContextProviderForDedicatedWorker(
+    DedicatedWorkerHost* host,
+    mojo::PendingReceiver<webnn::mojom::WebNNContextProvider> receiver) {
+  auto* process_host =
+      static_cast<RenderProcessHostImpl*>(host->GetProcessHost());
   process_host->GetGpuClient()->BindWebNNContextProvider(std::move(receiver));
 }
 #endif
@@ -918,8 +926,8 @@ void PopulateFrameBinders(RenderFrameHostImpl* host, mojo::BinderMap* map) {
 #if !BUILDFLAG(IS_CHROMEOS)
   if (base::FeatureList::IsEnabled(
           webnn::features::kEnableMachineLearningNeuralNetworkService)) {
-    map->Add<webnn::mojom::WebNNContextProvider>(
-        base::BindRepeating(&BindWebNNContextProvider, base::Unretained(host)));
+    map->Add<webnn::mojom::WebNNContextProvider>(base::BindRepeating(
+        &BindWebNNContextProviderForRenderFrame, base::Unretained(host)));
   }
 #endif
 
@@ -1300,6 +1308,16 @@ void PopulateDedicatedWorkerBinders(DedicatedWorkerHost* host,
                           host->GetProcessHost()->GetID()),
       base::ThreadPool::CreateSequencedTaskRunner(
           {base::MayBlock(), base::TaskPriority::USER_VISIBLE}));
+
+#if !BUILDFLAG(IS_CHROMEOS)
+  if (base::FeatureList::IsEnabled(
+          webnn::features::kEnableMachineLearningNeuralNetworkService)) {
+    // base::Unretained(host->GetProcessHost()) is safe because the map is owned
+    // by |DedicatedWorkerHost::broker_|.
+    map->Add<webnn::mojom::WebNNContextProvider>(base::BindRepeating(
+        &BindWebNNContextProviderForDedicatedWorker, base::Unretained(host)));
+  }
+#endif
 
 #if !BUILDFLAG(IS_ANDROID)
   map->Add<blink::mojom::DirectSocketsService>(
