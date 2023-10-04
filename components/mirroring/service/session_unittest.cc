d08f308383233fd9340a4e6a70f95ac4e2a0bfc7
Jordan Bayles <jophba@chromium.org>
2023-10-04 06:09:27 +0000

    [Cast Streaming] Enable OpenscreenCastStreamingSession by default
    This patch enables the OpenscreenCastStreamingSession experiment by
    default now that this experiment has successfully launched. Additional
    patches to remove this feature and related legacy code paths will be
    completed starting in M120 (to allow a killswitch to remain for M119).
    
    Bug: 1316434, b/259135933
    Change-Id: Ia412ecc2ae89f9bba4853d267a90b09f09d42bbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905266
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205066}

--- components/mirroring/service/session_unittest.cc
+++ components/mirroring/service/session_unittest.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/mirroring/service/mirroring_features.h"
 #include "components/mirroring/service/receiver_response.h"
 #include "components/mirroring/service/value_util.h"
+#include "media/base/media_switches.h"
 #include "media/capture/video_capture_types.h"
 #include "media/cast/test/utility/default_config.h"
 #include "media/cast/test/utility/net_utility.h"
@@ -117,7 +118,9 @@ class SessionTest : public mojom::ResourceProvider,
                     public mojom::CastMessageChannel,
                     public ::testing::Test {
  public:
-  SessionTest() = default;
+  SessionTest() {
+    feature_list_.InitAndDisableFeature(media::kOpenscreenCastStreamingSession);
+  }
 
   SessionTest(const SessionTest&) = delete;
   SessionTest& operator=(const SessionTest&) = delete;
@@ -506,6 +509,7 @@ class SessionTest : public mojom::ResourceProvider,
  private:
   base::test::TaskEnvironment task_environment_{
       base::test::TaskEnvironment::TimeSource::MOCK_TIME};
+  base::test::ScopedFeatureList feature_list_;
   const net::IPEndPoint receiver_endpoint_ =
       media::cast::test::GetFreeLocalPort();
   mojo::Receiver<mojom::ResourceProvider> resource_provider_receiver_{this};
