4ce32fa88f494b0a318df2780529fe7b5e1579a4
Seung Jae Lim <andysjlim@chromium.org>
2023-10-04 18:53:40 +0000

    Revert "Add retry mechanism for CSD-Phishing when the classifier is not ready"
    This reverts commit bd4561219bb5ffc85430f1e7873838e124911b0a.
    
    Reason for revert: This change looks to be crashing and the bug to describe it is here: https://bugs.chromium.org/p/chromium/issues/detail?id=1489307
    
    Original change's description:
    > Add retry mechanism for CSD-Phishing when the classifier is not ready
    >
    > CSD-Phishing classification returns a "Classifier Not Ready" when the
    > renderer process attempts to classify the page, but the classifier was
    > not ready due to the model being unavailable. To reduce this number by
    > 80% to 90%, we will wait for the ScorerStorage object to populate with
    > the model for a set amount of time enabled by Chrome variation. If the
    > scorer object has not been updated till the end of the waiting time,
    > then we will mark with "Classifier Not Ready" for
    > PhishingDetectorResult.
    >
    > Bug: 1479042
    > Change-Id: I9a2634911055f9a55a7fbe5040e0416b6b5045e9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4848312
    > Reviewed-by: Daniel Rubery <drubery@chromium.org>
    > Commit-Queue: Seung Jae Lim <andysjlim@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1204700}
    
    Bug: 1479042
    Change-Id: Ibe45569f0fe02c81cf2e542e231cbe5408e39794
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4911856
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Seung Jae Lim <andysjlim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205362}

--- components/safe_browsing/content/renderer/phishing_classifier/features.h
+++ components/safe_browsing/content/renderer/phishing_classifier/features.h
@@ -30,9 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <unordered_map>
 
-#include "base/feature_list.h"
-#include "base/metrics/field_trial_params.h"
-
 namespace safe_browsing {
 
 // Container for a map of features to values, which enforces behavior
@@ -73,10 +70,6 @@ class FeatureMap {
  private:
   std::unordered_map<std::string, double> features_;
 };
-
-BASE_DECLARE_FEATURE(kClientSideDetectionRetryLimit);
-
-extern const base::FeatureParam<int> kClientSideDetectionRetryLimitTime;
 
 namespace features {
 // Constants for the various feature names that we use.
