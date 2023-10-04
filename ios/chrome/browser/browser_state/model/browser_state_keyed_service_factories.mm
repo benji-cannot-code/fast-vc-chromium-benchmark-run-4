ef12fbe7a3bc4532e3375e2feba63a52d84cfd3c
mmrashad <mmrashad@google.com>
2023-10-04 14:31:21 +0000

    [Code Health][iOS] Move ios/c/b/policy_url_blocking to model subfolder
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/<foo>/ will be moved to
    ios/c/b/<foo>/model/ to showcase that they are model files.
    
    Bug: 1319852, 1480333
    Change-Id: I6501817e08912b0e92c850cf690ed9c8f17d98d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4910998
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1205232}

--- ios/chrome/browser/browser_state/model/browser_state_keyed_service_factories.mm
+++ ios/chrome/browser/browser_state/model/browser_state_keyed_service_factories.mm
@@ -51,7 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/passwords/ios_chrome_password_store_factory.h"
 #import "ios/chrome/browser/photos/photos_service_factory.h"
 #import "ios/chrome/browser/policy/cloud/user_policy_signin_service_factory.h"
-#import "ios/chrome/browser/policy_url_blocking/policy_url_blocking_service.h"
+#import "ios/chrome/browser/policy_url_blocking/model/policy_url_blocking_service.h"
 #import "ios/chrome/browser/promos_manager/promos_manager_event_exporter_factory.h"
 #import "ios/chrome/browser/promos_manager/promos_manager_factory.h"
 #import "ios/chrome/browser/push_notification/push_notification_browser_state_service_factory.h"
--- ios/chrome/browser/policy_url_blocking/model/policy_url_blocking_tab_helper.mm
+++ ios/chrome/browser/policy_url_blocking/model/policy_url_blocking_tab_helper.mm
@@ -3,11 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/policy_url_blocking/policy_url_blocking_tab_helper.h"
+#import "ios/chrome/browser/policy_url_blocking/model/policy_url_blocking_tab_helper.h"
 
 #import "components/policy/core/browser/url_blocklist_manager.h"
-#import "ios/chrome/browser/policy_url_blocking/policy_url_blocking_service.h"
-#import "ios/chrome/browser/policy_url_blocking/policy_url_blocking_util.h"
+#import "ios/chrome/browser/policy_url_blocking/model/policy_url_blocking_service.h"
+#import "ios/chrome/browser/policy_url_blocking/model/policy_url_blocking_util.h"
 #import "net/base/mac/url_conversions.h"
 
 PolicyUrlBlockingTabHelper::~PolicyUrlBlockingTabHelper() = default;
