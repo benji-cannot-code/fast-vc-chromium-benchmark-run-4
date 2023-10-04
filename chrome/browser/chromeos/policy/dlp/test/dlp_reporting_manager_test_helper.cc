05ed3fff455d7b61a9f2961531daa73f8f078707
Dominique Fauteux-Chapleau <domfc@chromium.org>
2023-10-04 15:51:38 +0000

    Move dlp_policy_event.proto to components/enterprise/data_controls
    This is done in preparation of future refactors to share reporting code
    between the existing CrOS DLP rules and multi-platform Data Controls.
    This CL also updates relevant `#include` statements and BUILD files.
    
    Bug: b/280449704
    Change-Id: I6f7cf27a17cdd3983dfb4b54071a92be13ad8a02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4908473
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205288}

--- chrome/browser/chromeos/policy/dlp/test/dlp_reporting_manager_test_helper.cc
+++ chrome/browser/chromeos/policy/dlp/test/dlp_reporting_manager_test_helper.cc
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/task/sequenced_task_runner.h"
-#include "chrome/browser/chromeos/policy/dlp/dlp_policy_event.pb.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_reporting_manager.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_rules_manager.h"
+#include "components/enterprise/data_controls/dlp_policy_event.pb.h"
 #include "components/reporting/client/mock_report_queue.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
