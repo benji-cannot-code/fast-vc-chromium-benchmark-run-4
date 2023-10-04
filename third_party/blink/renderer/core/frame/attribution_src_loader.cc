8794c337f0232bc27603c674ad7c64c96b0d7cf4
Thomas Quintanilla <tquintanilla@google.com>
2023-10-04 15:50:43 +0000

    Add metric measuring header response size
    This will allow us to determine if a limit needs to be placed on
    response header sizes.
    
    Bug: 1477806
    Change-Id: Icedbf45a7e3bd9c22c05b18e49385e1569fba15d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4847889
    Commit-Queue: Thomas Quintanilla <tquintanilla@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205287}

--- third_party/blink/renderer/core/frame/attribution_src_loader.cc
+++ third_party/blink/renderer/core/frame/attribution_src_loader.cc
@@ -818,6 +818,10 @@ void AttributionSrcLoader::ResourceClient::HandleSourceRegistration(
   }
 
   if (!headers.web_source.IsNull()) {
+    // Max header size is 256 KB, use 1M count to encapsulate.
+    base::UmaHistogramCounts1M("Conversions.HeadersSize.RegisterSource",
+                               headers.web_source.length());
+
     if (!network::HasAttributionWebSupport(loader_->GetSupport())) {
       headers.LogSourceIgnored(loader_->local_frame_->DomWindow());
       return;
@@ -839,6 +843,10 @@ void AttributionSrcLoader::ResourceClient::HandleSourceRegistration(
   }
 
   DCHECK(!headers.os_source.IsNull());
+  // Max header size is 256 KB, use 1M count to encapsulate.
+  base::UmaHistogramCounts1M("Conversions.HeadersSize.RegisterOsSource",
+                             headers.os_source.length());
+
   if (!network::HasAttributionOsSupport(loader_->GetSupport())) {
     headers.LogOsSourceIgnored(loader_->local_frame_->DomWindow());
     return;
@@ -874,10 +882,15 @@ void AttributionSrcLoader::ResourceClient::HandleTriggerRegistration(
   }
 
   if (!headers.web_trigger.IsNull()) {
+    // Max header size is 256 KB, use 1M count to encapsulate.
+    base::UmaHistogramCounts1M("Conversions.HeadersSize.RegisterTrigger",
+                               headers.web_trigger.length());
+
     if (!network::HasAttributionWebSupport(loader_->GetSupport())) {
       headers.LogTriggerIgnored(loader_->local_frame_->DomWindow());
       return;
     }
+
     auto trigger_data = attribution_reporting::TriggerRegistration::Parse(
         StringUTF8Adaptor(headers.web_trigger).AsStringPiece());
     if (!trigger_data.has_value()) {
@@ -897,6 +910,10 @@ void AttributionSrcLoader::ResourceClient::HandleTriggerRegistration(
   }
 
   DCHECK(!headers.os_trigger.IsNull());
+  // Max header size is 256 KB, use 1M count to encapsulate.
+  base::UmaHistogramCounts1M("Conversions.HeadersSize.RegisterOsTrigger",
+                             headers.os_trigger.length());
+
   if (!network::HasAttributionOsSupport(loader_->GetSupport())) {
     headers.LogOsTriggerIgnored(loader_->local_frame_->DomWindow());
     return;
