0ffd79f38bcedd8528527f4da365692cb5b7e83d
Sophie Wen <sophiewen@chromium.org>
2023-10-04 00:09:46 +0000

    snap-groups: Start overview in SplitViewOverviewSession
    This moves `StartOverview()` to `SplitViewOverviewSession::Init()`.
    This is needed since OverviewGrid and SplitViewController will check
    if `RootWindowController::ForWindow(*)->split_view_overview_session()`
    exists during overview mode starting, updating grid bounds, etc.
    
    Test: existing
    Fixed: b/303146294
    Change-Id: I92882cd18f81f5366c9f97d990bfb5ceb94809cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4908822
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204932}

--- ash/wm/splitview/split_view_overview_session.h
+++ ash/wm/splitview/split_view_overview_session.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/overview_types.h"
 #include "ash/wm/window_state_observer.h"
 #include "base/scoped_observation.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/aura/window_observer.h"
 #include "ui/compositor/presentation_time_recorder.h"
 
@@ -38,6 +39,11 @@ class SplitViewOverviewSession : public aura::WindowObserver,
   SplitViewOverviewSession(const SplitViewOverviewSession&) = delete;
   SplitViewOverviewSession& operator=(const SplitViewOverviewSession&) = delete;
   ~SplitViewOverviewSession() override;
+
+  // Initializes the session by starting overview. This must be called after the
+  // constructor, as consumers may check if `this` exists.
+  void Init(absl::optional<OverviewStartAction> action,
+            absl::optional<OverviewEnterExitType> type);
 
   const aura::Window* window() const { return window_; }
   chromeos::WindowStateType GetWindowStateType() const;
