78a4503f7554006b96a5709919c32c2a7d9970c3
Dominique Fauteux-Chapleau <domfc@chromium.org>
2023-10-04 20:08:35 +0000

    Reland "Move dlp_policy_event.proto to components/enterprise/data_controls"
    This is a reland of commit 7b9e98c35e7401f0c2dd71dbe80cf04583821304
    
    Some CrOS files didn't have their `#include` statement updated, the diff
    can be seen by comparing patchsets 1 and 2.
    
    Original change's description:
    > Move dlp_policy_event.proto to components/enterprise/data_controls
    >
    > This is done in preparation of future refactors to share reporting code
    > between the existing CrOS DLP rules and multi-platform Data Controls.
    > This CL also updates relevant `#include` statements and BUILD files.
    >
    > Bug: b/280449704
    > Change-Id: I6f7cf27a17cdd3983dfb4b54071a92be13ad8a02
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4908473
    > Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    > Reviewed-by: Sergey Poromov <poromov@chromium.org>
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1205288}
    
    Bug: b/280449704
    Change-Id: I892d2e9d130257e1f5a40941f388291e93aa0efb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4912953
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205419}

--- chrome/browser/chromeos/policy/dlp/dlp_reporting_manager.h
+++ chrome/browser/chromeos/policy/dlp/dlp_reporting_manager.h
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/task/sequenced_task_runner.h"
-#include "chrome/browser/chromeos/policy/dlp/dlp_policy_event.pb.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_rules_manager.h"
+#include "components/enterprise/data_controls/dlp_policy_event.pb.h"
 #include "components/reporting/client/report_queue.h"
 #include "components/reporting/util/status.h"
 
