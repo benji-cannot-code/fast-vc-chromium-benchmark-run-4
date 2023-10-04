65f3c023f1c90eac69e775b226f55f9aa66cd076
Ewann Pelle <ewannpv@chromium.org>
2023-10-04 11:45:38 +0000

    [iOS] Fix Symbol names
    Bug: 1489185
    Change-Id: I7df6c86152dad8884db8ce8b9a70ffe636686656
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4910994
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205169}

--- ios/chrome/browser/ui/content_suggestions/magic_stack_half_sheet_table_view_controller.mm
+++ ios/chrome/browser/ui/content_suggestions/magic_stack_half_sheet_table_view_controller.mm
@@ -101,9 +101,9 @@ enum ItemType : NSInteger {
   [self.tableViewModel addSectionWithIdentifier:SectionIdentifierOptions];
 
   if (IsIOSSetUpListEnabled()) {
-    NSString* listSymbolName = kListBulletRectangle;
+    NSString* listSymbolName = kListBulletRectangleSymbol;
     if (@available(iOS 16.0, *)) {
-      listSymbolName = kListBulletClipboard;
+      listSymbolName = kListBulletClipboardSymbol;
     }
     _setUpListToggle = [self
         switchItemWithType:ItemTypeToggleSetUpList
@@ -118,7 +118,7 @@ enum ItemType : NSInteger {
     _safetyCheckToggle = [self
         switchItemWithType:ItemTypeToggleSafetyCheck
                      title:l10n_util::GetNSString(IDS_IOS_SAFETY_CHECK_TITLE)
-                    symbol:DefaultSymbolWithPointSize(kCheckmarkShield,
+                    symbol:DefaultSymbolWithPointSize(kCheckmarkShieldSymbol,
                                                       kIconPointSize)];
     _safetyCheckToggle.on = !_safetyCheckDisabled.value;
     [self.tableViewModel addItem:_safetyCheckToggle
@@ -128,7 +128,7 @@ enum ItemType : NSInteger {
     _tabResumptionToggle = [self
         switchItemWithType:ItemTypeToggleTabResumption
                      title:l10n_util::GetNSString(IDS_IOS_TAB_RESUMPTION_TITLE)
-                    symbol:DefaultSymbolWithPointSize(kMacbookAndIPhone,
+                    symbol:DefaultSymbolWithPointSize(kMacbookAndIPhoneSymbol,
                                                       kIconPointSize)];
     _tabResumptionToggle.on = !_tabResumptionDisabled.value;
     [self.tableViewModel addItem:_tabResumptionToggle
