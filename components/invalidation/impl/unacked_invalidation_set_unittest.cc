9d7cd71bc4ac92bd22853967fffee0be9b9edceb
Roland Bock <rbock@google.com>
2023-10-04 14:13:06 +0000

    Stop creating invalidations with unknown version
    In production code, invalidations with unknown version are only created
    in UnackedInvalidationSet::Truncate. They were used by Sync to indicate
    that invalidations were dropped before being reported to the Sync
    InvalidationHandler.
    
    Now that Sync migrated to a different ivalidation stack, there is no
    need to create or handle invalidations with unknown version. All
    remaining invalidation handlers only care for the latest invalidation
    for any given topic.
    
    Bug: b/303338992
    Change-Id: Ic7fcb36b1208db0596731c22383a6b4a2c441d63
    Tests: Existing, adjusted unit tests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4903737
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1205222}

--- components/invalidation/impl/unacked_invalidation_set_unittest.cc
+++ components/invalidation/impl/unacked_invalidation_set_unittest.cc
@@ -53,7 +53,7 @@ TEST_F(UnackedInvalidationSetTest, OneInvalidation) {
 
   SingleTopicInvalidationSet set = GetStoredInvalidations();
   ASSERT_EQ(1U, set.GetSize());
-  EXPECT_FALSE(set.StartsWithUnknownVersion());
+  EXPECT_EQ(*set.begin(), inv1);
 }
 
 // Test that repeated unknown version invalidations are squashed together.
@@ -67,7 +67,7 @@ TEST_F(UnackedInvalidationSetTest, UnknownVersions) {
 
   SingleTopicInvalidationSet set = GetStoredInvalidations();
   ASSERT_EQ(2U, set.GetSize());
-  EXPECT_TRUE(set.StartsWithUnknownVersion());
+  EXPECT_TRUE(set.begin()->is_unknown_version());
 }
 
 // Tests that no truncation occurs while we're under the limit.
@@ -81,7 +81,6 @@ TEST_F(UnackedInvalidationSetTest, NoTruncation) {
 
   SingleTopicInvalidationSet set = GetStoredInvalidations();
   ASSERT_EQ(kMax, set.GetSize());
-  EXPECT_FALSE(set.StartsWithUnknownVersion());
   EXPECT_EQ(0, set.begin()->version());
   EXPECT_EQ(kMax-1, static_cast<size_t>(set.rbegin()->version()));
 }
@@ -97,8 +96,7 @@ TEST_F(UnackedInvalidationSetTest, Truncation) {
 
   SingleTopicInvalidationSet set = GetStoredInvalidations();
   ASSERT_EQ(kMax, set.GetSize());
-  EXPECT_TRUE(set.StartsWithUnknownVersion());
-  EXPECT_TRUE(set.begin()->is_unknown_version());
+  EXPECT_EQ(1, set.begin()->version());
   EXPECT_EQ(kMax, static_cast<size_t>(set.rbegin()->version()));
 }
 
@@ -113,7 +111,6 @@ TEST_F(UnackedInvalidationSetTest, RegistrationAndTruncation) {
 
   SingleTopicInvalidationSet set = GetStoredInvalidations();
   ASSERT_EQ(kMax, set.GetSize());
-  EXPECT_FALSE(set.StartsWithUnknownVersion());
   EXPECT_EQ(0, set.begin()->version());
   EXPECT_EQ(kMax - 1, static_cast<size_t>(set.rbegin()->version()));
 
@@ -122,8 +119,7 @@ TEST_F(UnackedInvalidationSetTest, RegistrationAndTruncation) {
   unacked_invalidations_.Add(Invalidation::Init(kTopic, kMax, "payload"));
   SingleTopicInvalidationSet set2 = GetStoredInvalidations();
   ASSERT_EQ(kMax, set2.GetSize());
-  EXPECT_TRUE(set2.StartsWithUnknownVersion());
-  EXPECT_TRUE(set2.begin()->is_unknown_version());
+  EXPECT_EQ(1, set2.begin()->version());
   EXPECT_EQ(kMax, static_cast<size_t>(set2.rbegin()->version()));
 }
 
--- components/invalidation/public/invalidation.cc
+++ components/invalidation/public/invalidation.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/invalidation/public/invalidation.h"
 
+#include "base/check_is_test.h"
 #include "base/functional/bind.h"
 #include "base/json/json_string_value_serializer.h"
 #include "base/location.h"
@@ -36,6 +37,7 @@ Invalidation Invalidation::Init(const Topic& topic,
 
 // static
 Invalidation Invalidation::InitUnknownVersion(const Topic& topic) {
+  CHECK_IS_TEST();
   return Invalidation(topic, /*is_unknown_version=*/true, kInvalidVersion,
                       std::string(), AckHandle::CreateUnique());
 }
