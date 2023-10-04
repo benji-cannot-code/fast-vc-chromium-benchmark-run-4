0adcea3e0fe17a09c14911bf8281673f3ddd45fd
Jan Keitel <jkeitel@google.com>
2023-10-04 12:57:24 +0000

    Eliminate AutofillPopupView wrapper.
    The introduction of the wrapper around AutofillPopupView was intended
    as a stop-gap measure because there were too many null pointer
    dereferences. This CL removes the wrapper and makes sure that every
    call site checks whether the pointer to the view (now a weak one)
    is still valid.
    
    Bug: 1277218
    Change-Id: Ia6eab3167ac8cb6b565beff99374b50341a77d87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4891741
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1205192}

--- chrome/browser/ui/autofill/autofill_popup_controller_impl.h
+++ chrome/browser/ui/autofill/autofill_popup_controller_impl.h
@@ -7,12 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_AUTOFILL_AUTOFILL_POPUP_CONTROLLER_IMPL_H_
 
 #include <string>
-#include <type_traits>
 #include <vector>
 
-#include "base/functional/invoke.h"
-#include "base/gtest_prod_util.h"
-#include "base/i18n/rtl.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
@@ -210,45 +206,6 @@ class AutofillPopupControllerImpl
   virtual void HideViewAndDie();
 
  private:
-  // Wraps a raw AutofillPopupView pointer and checks for nullptr before any
-  // dereference. This is useful because AutofillPopupView may be synchronously
-  // deleted and set to nullptr by many calls in AutofillPopupControllerImpl,
-  // which easily leads to segfaults. See crbug.com/1277218 for the lifecycle
-  // management issue in AutofillPopupView.
-  class AutofillPopupViewPtr {
-   public:
-    AutofillPopupViewPtr();
-    AutofillPopupViewPtr(const AutofillPopupViewPtr&) = delete;
-    AutofillPopupViewPtr& operator=(const AutofillPopupViewPtr&) = delete;
-    ~AutofillPopupViewPtr();
-
-    AutofillPopupViewPtr& operator=(base::WeakPtr<AutofillPopupView> ptr) {
-      ptr_ = std::move(ptr);
-      return *this;
-    }
-
-    explicit operator bool() const { return !!ptr_; }
-
-    // If `ptr_ == nullptr`, returns something that converts to false.
-    // If `ptr_ != nullptr`, calls `ptr_->func(args...)` and, if that returns a
-    // value, returns this value wrapped in an `absl::optional`, otherwise
-    // returns true.
-    template <typename Func, typename... Args>
-    [[nodiscard]] auto Call(Func&& func, Args... args) {
-      using ReturnType = decltype(base::invoke(func, *ptr_, args...));
-      if constexpr (!std::is_void_v<ReturnType>) {
-        return ptr_ ? absl::optional<ReturnType>(
-                          base::invoke(func, *ptr_, args...))
-                    : absl::optional<ReturnType>();
-      } else {
-        return ptr_ ? base::invoke(func, *ptr_, args...), true : false;
-      }
-    }
-
-   private:
-    base::WeakPtr<AutofillPopupView> ptr_;
-  };
-
   // content::WebContentsObserver:
   void RenderFrameDeleted(content::RenderFrameHost* render_frame_host) override;
   void DidFinishNavigation(
@@ -278,7 +235,7 @@ class AutofillPopupControllerImpl
   bool IsRootPopup() const;
 
   PopupControllerCommon controller_common_;
-  AutofillPopupViewPtr view_;
+  base::WeakPtr<AutofillPopupView> view_;
   base::WeakPtr<AutofillPopupDelegate> delegate_;
 
   struct {
