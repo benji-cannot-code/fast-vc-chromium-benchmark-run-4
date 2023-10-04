c27b4b003484f893f765394bf55eef69f57b53cc
Andrew Paseltiner <apaseltiner@chromium.org>
2023-10-04 20:21:35 +0000

    Remove unreachable NOTREACHED_NORETURN
    The default clause is redundant as the switch is exhaustive without it.
    
    Change-Id: I9517416190717056ea69b0afa22ea7ace43ed1ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4912113
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Anthony Garant <anthonygarant@chromium.org>
    Reviewed-by: Anthony Garant <anthonygarant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205431}

--- components/attribution_reporting/filters.cc
+++ components/attribution_reporting/filters.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/metrics/histogram_base.h"
 #include "base/metrics/histogram_macros.h"
-#include "base/notreached.h"
 #include "base/ranges/algorithm.h"
 #include "base/time/time.h"
 #include "base/types/expected.h"
@@ -208,8 +207,6 @@ base::expected<FilterData, SourceRegistrationError> FilterData::FromJSON(
         return SourceRegistrationError::kFilterDataValueWrongType;
       case FilterValuesError::kValueTooLong:
         return SourceRegistrationError::kFilterDataValueTooLong;
-      default:
-        NOTREACHED_NORETURN();
     }
   };
   ASSIGN_OR_RETURN(auto filter_values,
