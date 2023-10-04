dd02284a574ad0faa244ae35f32ee62f529c4d37
Kent Tamura <tkent@chromium.org>
2023-10-04 04:53:49 +0000

    Remove FixOffsetInMultiColVerticalRl flag
    The flag has been enabled since M117, and we've seen no issues.
    
    Also, this CL cleans up the code by
     * Changing the LayoutPoint argument of FragmentainerGroupAtVisualPoint
       to LogicalOffset
     * Changing the LayoutPoint return type of VisualPointToFlowThreadPoint
       to LogicalOffset
     * Changing the LayoutPoint argument of VisualPointToFlowThreadPoint to
       PhysicalOffset
    
    This CL should have no behavior changes.
    
    Bug: 1457423
    Change-Id: I1415737ce3d8b94225cfba6689b60d31688ff037
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4902880
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205048}

--- third_party/blink/renderer/core/layout/layout_multi_column_set.h
+++ third_party/blink/renderer/core/layout/layout_multi_column_set.h
@@ -108,7 +108,7 @@ class CORE_EXPORT LayoutMultiColumnSet final : public LayoutBlockFlow {
         flow_thread_offset, rule)];
   }
   const MultiColumnFragmentainerGroup& FragmentainerGroupAtVisualPoint(
-      const LayoutPoint&) const;
+      const LogicalOffset&) const;
   const MultiColumnFragmentainerGroupList& FragmentainerGroups() const {
     NOT_DESTROYED();
     UpdateGeometryIfNeeded();
@@ -186,8 +186,8 @@ class CORE_EXPORT LayoutMultiColumnSet final : public LayoutBlockFlow {
                                                PageBoundaryRule,
                                                CoordinateSpaceConversion) const;
 
-  LayoutPoint VisualPointToFlowThreadPoint(
-      const LayoutPoint& visual_point) const;
+  LogicalOffset VisualPointToFlowThreadPoint(
+      const PhysicalOffset& visual_point) const;
 
   // Reset previously calculated column height. Will mark for layout if needed.
   void ResetColumnHeight();
