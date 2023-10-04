505ec5a53dcd440fecf47dfd0235f38210aa4e9f
dpapad <dpapad@chromium.org>
2023-10-04 23:26:45 +0000

    WebUI: Update TypeScript compiler to version 5.2.2.
     - Remove "w3c-css-typed-object-model-level-1" from tsconfig "types"
       fields, as those types are now included in lib.dom.d.ts.
     - Delete newly unnecessary tsconfig.json files (due to the change
       above).
     - Updated @types/dom-mediacapture-transform, @types/dom-webcodecs,
       @types/w3c-image-capture, @types/webrtc to latest version (necessary
       to fix newly found errors).
     - Fix newly found errors guest_os_shared_usb_devices_test.ts due to
       the more accurate return type of structuredClone() in latest
       version.
     - Fix newly found error in tab_swiper.ts due to stricter relational
       operators type checking, see [1].
    
    CrOS camera_app_ui/ notes:
     - Remove custom definition of FileSystemWriteChunkType, it is now
       included in lib.dom.d.ts.
     - Fix newly found ESLint
       @typescript-eslint/strict-boolean-expressions violation in
       video_saver.ts
    
    [1] https://devblogs.microsoft.com/typescript/announcing-typescript-5-0/#forbidden-implicit-coercions-in-relational-operators
    
    Bug: 1488690
    Change-Id: Ic7e41d99447097d7021f6457167bf1557671a7f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4903376
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205549}

--- ash/webui/camera_app_ui/resources/js/externs/types.d.ts
+++ ash/webui/camera_app_ui/resources/js/externs/types.d.ts
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // still in working draft stage.
 // https://wicg.github.io/file-system-access/
 
-type FileSystemWriteChunkType = Blob|BufferSource|string;
-
 interface FileSystemWritableFileStream extends WritableStream {
   seek(position: number): Promise<void>;
   truncate(size: number): Promise<void>;
