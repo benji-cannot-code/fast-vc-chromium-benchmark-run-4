9fc95faea832f82c4b3686955c0dd2125040434c
Colin Blundell <blundell@chromium.org>
2023-10-04 09:00:49 +0000

    [//cc] Turn MappableSI in OneCopyRasterBufferProvider on by default
    Since the initial landing of the CL that had to be reverted due to
    pinpoint and Tast test regressions, the following has happened:
    
    1. vikassoni@ landed a general solution for a known perf bottleneck
    2. I discovered a correctness problem with the original CL and fixed it
       in the re-re-land
    
    This CL turns the feature back on by default to see whether anything
    pops back up.
    
    Bug: 1431314
    Change-Id: I24abf378ae2a29fbf32d6d3ce5fc24ea4febd216
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905449
    Reviewed-by: vikas soni <vikassoni@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205118}

--- cc/raster/one_copy_raster_buffer_provider.cc
+++ cc/raster/one_copy_raster_buffer_provider.cc
@@ -55,7 +55,7 @@ BASE_FEATURE(kOneCopyRasterBufferPlaybackNormalThreadPriority,
 
 BASE_FEATURE(kAlwaysUseMappableSIForOneCopyRaster,
              "AlwaysUseMappableSIForOneCopyRaster",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace
 
