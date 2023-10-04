b9989ba9f487aba5ae8ed20cd77d5ccc03a65ace
Stephan Hartmann <stha09@googlemail.com>
2023-10-04 14:51:33 +0000

    IWYU: add stdint.h in profile_separation_policies.h
    Bug: 957519
    Change-Id: I6a6c50dbe53eee0250309496ed07de60cfd1ce83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4909875
    Auto-Submit: Stephan Hartmann <stha09@googlemail.com>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205241}

--- components/policy/core/browser/signin/profile_separation_policies.h
+++ components/policy/core/browser/signin/profile_separation_policies.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_POLICY_CORE_BROWSER_SIGNIN_PROFILE_SEPARATION_POLICIES_H_
 #define COMPONENTS_POLICY_CORE_BROWSER_SIGNIN_PROFILE_SEPARATION_POLICIES_H_
 
+#include <stdint.h>
+
 #include <string>
 
 #include "components/policy/policy_export.h"
