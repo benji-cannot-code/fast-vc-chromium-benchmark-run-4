239626bfcfe5751d46af95c21422a643e5fc4e16
Dylan Cutler <dylancutler@google.com>
2023-10-04 17:16:48 +0000

    Add info bar when test third-party-cookie-phaseout is enabled
    Bug: 1441039
    Change-Id: I1a35d9a6c6f68df12c1390a71c9329ebf64f5d15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4907869
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Cr-Commit-Position: refs/heads/main@{#1205321}

--- chrome/browser/ui/startup/infobar_utils.cc
+++ chrome/browser/ui/startup/infobar_utils.cc
@@ -22,12 +22,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/startup/obsolete_system_infobar_delegate.h"
 #include "chrome/browser/ui/startup/startup_browser_creator.h"
 #include "chrome/browser/ui/startup/startup_types.h"
+#include "chrome/browser/ui/startup/test_third_party_cookie_phaseout_infobar_delegate.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "components/infobars/content/content_infobar_manager.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/common/content_switches.h"
 #include "google_apis/google_api_keys.h"
+#include "services/network/public/cpp/network_switches.h"
 
 #if BUILDFLAG(CHROME_FOR_TESTING)
 #include "chrome/browser/ui/startup/chrome_for_testing_infobar_delegate.h"
@@ -102,6 +104,11 @@ void AddInfoBarsIfNecessary(Browser* browser,
     if (base::CommandLine::ForCurrentProcess()->HasSwitch(
             switches::kProtectedAudiencesConsentedDebugToken)) {
       BiddingAndAuctionConsentedDebuggingDelegate::Create(web_contents);
+    }
+
+    if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+            network::switches::kTestThirdPartyCookiePhaseout)) {
+      TestThirdPartyCookiePhaseoutInfoBarDelegate::Create(web_contents);
     }
   }
 
