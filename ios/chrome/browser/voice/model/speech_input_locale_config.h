dbba8ca99ba6e6d2ed0d586dfb9d7ab264053c4e
Robbie Gibson <rkgibson@google.com>
2023-10-04 16:39:02 +0000

    [Code Health][iOS] Prepare to move ios/c/b/voice to model subfolder
    Bug: 1480375
    Change-Id: If9e2011cf1dcf8e34203eaa9d53b800884581351
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4902659
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1205304}

---
+++ ios/chrome/browser/voice/model/speech_input_locale_config.h
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_VOICE_MODEL_SPEECH_INPUT_LOCALE_CONFIG_H_
+#define IOS_CHROME_BROWSER_VOICE_MODEL_SPEECH_INPUT_LOCALE_CONFIG_H_
+
+#include "ios/chrome/browser/voice/speech_input_locale_config.h"
+
+#endif  // IOS_CHROME_BROWSER_VOICE_MODEL_SPEECH_INPUT_LOCALE_CONFIG_H_
