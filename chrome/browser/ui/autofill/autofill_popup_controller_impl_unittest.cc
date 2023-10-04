ffd5ef6cc36a37764e5538ea50b01a4c76094435
Jan Keitel <jkeitel@google.com>
2023-10-04 16:10:41 +0000

    Introduce Compose suggestion and button handlers.
    In addition to handling Compose suggestions in the factory function
    for PopupRows, this CL introduces a handler for "ButtonActions".
    In general, Autofill popup suggestions can have buttons on it. In
    some cases, the action on these buttons is handled by the controller
    itself (e.g. for Autocomplete entries), while in others, the controller
    will simply pass on the action to its delegate (e.g. for the close
    button on Compose suggestions).
    
    Bug: 1487086, 1487965
    Change-Id: Ie89a6b09d93792b84fb3f6180c96335ef8e52425
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905985
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1205295}

--- chrome/browser/ui/autofill/autofill_popup_controller_impl_unittest.cc
+++ chrome/browser/ui/autofill/autofill_popup_controller_impl_unittest.cc
@@ -122,6 +122,10 @@ class MockAutofillExternalDelegate : public AutofillExternalDelegate {
               DidAcceptSuggestion,
               (const Suggestion&, int, AutofillSuggestionTriggerSource),
               (override));
+  MOCK_METHOD(void,
+              DidPerformButtonActionForSuggestion,
+              (const Suggestion&),
+              (override));
 };
 
 class MockAutofillPopupView : public AutofillPopupView {
@@ -187,6 +191,7 @@ class TestAutofillPopupController : public AutofillPopupControllerImpl {
   using AutofillPopupControllerImpl::GetSuggestionLabelsAt;
   using AutofillPopupControllerImpl::GetSuggestionMainTextAt;
   using AutofillPopupControllerImpl::GetWeakPtr;
+  using AutofillPopupControllerImpl::PerformButtonActionForSuggestion;
   using AutofillPopupControllerImpl::RemoveSuggestion;
   using AutofillPopupControllerImpl::SelectSuggestion;
   MOCK_METHOD(void, OnSuggestionsChanged, (), (override));
@@ -937,6 +942,15 @@ TEST_F(AutofillPopupControllerImplTest, EventsAreDelegatedToChildrenAndView) {
       .WillOnce(Return(false));
   EXPECT_CALL(client().popup_view(), HandleKeyPressEvent).Times(1);
   EXPECT_FALSE(client().popup_controller(manager()).HandleKeyPressEvent(event));
+}
+
+// Tests that the controller forwards calls to perform a button action (such as
+// clicking a close button on a suggestion) to its delegate.
+TEST_F(AutofillPopupControllerImplTest, ButtonActionsAreSentToDelegate) {
+  ShowSuggestions(manager(), {PopupItemId::kCompose});
+  EXPECT_CALL(manager().external_delegate(),
+              DidPerformButtonActionForSuggestion);
+  client().popup_controller(manager()).PerformButtonActionForSuggestion(0);
 }
 #endif
 
