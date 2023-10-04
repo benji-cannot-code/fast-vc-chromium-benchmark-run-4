de1e99dde0936dd6b0af0abf57ab5b6ea3eb1165
cassycc <cassycc@google.com>
2023-10-04 02:35:24 +0000

    [Office upload] Share OfficeDriveOpenErrors and Errors metric name
    Move OfficeDriveOpenErrors enum and the Error metric names to
    cloud_upload_util.h for easier sharing when adding the CloudOpenMetrics
    object b/300861997 and
    https://chromium-review.googlesource.com/c/chromium/src/+/4910631
    
    Bug: b/300861997
    Change-Id: Ia73281ceba3daabaf4a131217b718b58a02ecb35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4904620
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1205006}

--- chrome/browser/ash/file_manager/file_tasks_browsertest.cc
+++ chrome/browser/ash/file_manager/file_tasks_browsertest.cc
@@ -1625,7 +1625,7 @@ IN_PROC_BROWSER_TEST_F(OneDriveTest, OfficeFallbackTryAgain) {
       ash::cloud_upload::kOneDriveTaskResultMetricName,
       ash::cloud_upload::OfficeTaskResult::kOpened, 1);
   histogram_.ExpectUniqueSample(
-      kOneDriveErrorMetricName,
+      ash::cloud_upload::kOneDriveErrorMetricName,
       ash::cloud_upload::OfficeOneDriveOpenErrors::kSuccess, 1);
 }
 
@@ -1679,7 +1679,7 @@ IN_PROC_BROWSER_TEST_F(OneDriveTest, OfficeFallbackCancel) {
       ash::cloud_upload::kOneDriveTaskResultMetricName,
       ash::cloud_upload::OfficeTaskResult::kCancelledAtFallback, 1);
   histogram_.ExpectUniqueSample(
-      kOneDriveErrorMetricName,
+      ash::cloud_upload::kOneDriveErrorMetricName,
       ash::cloud_upload::OfficeOneDriveOpenErrors::kOffline, 1);
 }
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
@@ -1717,7 +1717,7 @@ IN_PROC_BROWSER_TEST_F(OneDriveTest, OpenFileFromODFS) {
       ash::cloud_upload::kOneDriveTaskResultMetricName,
       ash::cloud_upload::OfficeTaskResult::kOpened, 1);
   histogram_.ExpectUniqueSample(
-      kOneDriveErrorMetricName,
+      ash::cloud_upload::kOneDriveErrorMetricName,
       ash::cloud_upload::OfficeOneDriveOpenErrors::kSuccess, 1);
 }
 
@@ -1794,7 +1794,7 @@ IN_PROC_BROWSER_TEST_F(OneDriveTest,
       ash::cloud_upload::kOneDriveTaskResultMetricName,
       ash::cloud_upload::OfficeTaskResult::kFailedToOpen, 1);
   histogram_.ExpectUniqueSample(
-      kOneDriveErrorMetricName,
+      ash::cloud_upload::kOneDriveErrorMetricName,
       ash::cloud_upload::OfficeOneDriveOpenErrors::kGetActionsReauthRequired,
       1);
 
@@ -1842,7 +1842,7 @@ IN_PROC_BROWSER_TEST_F(OneDriveTest, FailToOpenFileFromODFSOtherAccessError) {
       ash::cloud_upload::kOneDriveTaskResultMetricName,
       ash::cloud_upload::OfficeTaskResult::kFailedToOpen, 1);
   histogram_.ExpectUniqueSample(
-      kOneDriveErrorMetricName,
+      ash::cloud_upload::kOneDriveErrorMetricName,
       ash::cloud_upload::OfficeOneDriveOpenErrors::kGetActionsAccessDenied, 1);
 
   NotificationDisplayService::GetForProfile(browser()->profile())
@@ -1897,7 +1897,7 @@ IN_PROC_BROWSER_TEST_F(OneDriveTest, OpenFileFromAndroidOneDriveViaODFS) {
       ash::cloud_upload::kOneDriveTaskResultMetricName,
       ash::cloud_upload::OfficeTaskResult::kOpened, 1);
   histogram_.ExpectUniqueSample(
-      kOneDriveErrorMetricName,
+      ash::cloud_upload::kOneDriveErrorMetricName,
       ash::cloud_upload::OfficeOneDriveOpenErrors::kSuccess, 1);
 }
 
@@ -1942,7 +1942,7 @@ IN_PROC_BROWSER_TEST_F(OneDriveTest,
       ash::cloud_upload::kOneDriveTaskResultMetricName,
       ash::cloud_upload::OfficeTaskResult::kFailedToOpen, 1);
   histogram_.ExpectUniqueSample(
-      kOneDriveErrorMetricName,
+      ash::cloud_upload::kOneDriveErrorMetricName,
       ash::cloud_upload::OfficeOneDriveOpenErrors::kEmailsDoNotMatch, 1);
 }
 
@@ -1986,7 +1986,7 @@ IN_PROC_BROWSER_TEST_F(OneDriveTest,
       ash::cloud_upload::OfficeTaskResult::kFailedToOpen, 1);
   // Expect get actions error for a non-existent path.
   histogram_.ExpectUniqueSample(
-      kOneDriveErrorMetricName,
+      ash::cloud_upload::kOneDriveErrorMetricName,
       ash::cloud_upload::OfficeOneDriveOpenErrors::kGetActionsGenericError, 1);
 }
 
@@ -2034,7 +2034,7 @@ IN_PROC_BROWSER_TEST_F(
       ash::cloud_upload::OfficeTaskResult::kFailedToOpen, 1);
   // Expect the conversion to an ODFS equivalent URL to fail.
   histogram_.ExpectUniqueSample(
-      kOneDriveErrorMetricName,
+      ash::cloud_upload::kOneDriveErrorMetricName,
       ash::cloud_upload::OfficeOneDriveOpenErrors::kConversionToODFSUrlError,
       1);
 }
