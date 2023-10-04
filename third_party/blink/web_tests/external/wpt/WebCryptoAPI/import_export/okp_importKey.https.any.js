ca45394764e32859d19bd46784108ddf63e09b86
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2023-10-04 19:36:18 +0000

    Import wpt@d4e14d714c5242e174ba9aec43caf5eb514d0f09
    Using wpt-import in Chromium 52515f58a21bea82e201fd2ef7589d86d2130766.
    With Chromium commits locally applied on WPT:
    dc26878645 "Make navigation-api/ tests resilient to starting with extra NavigationHistoryEntries"
    26e182b564 "[soft-navigations] Improve keyboard SPA handling"
    4175fdad49 "[FLEDGE]:  Add a pair of WPT tests."
    
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I160d76e8c2598444579245c7cab53b1df3318d07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4911615
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1205399}

--- third_party/blink/web_tests/external/wpt/WebCryptoAPI/import_export/okp_importKey.https.any.js
+++ third_party/blink/web_tests/external/wpt/WebCryptoAPI/import_export/okp_importKey.https.any.js
@@ -82,6 +82,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                     }
 
                     testFormat(format, algorithm, data, vector.name, usages, extractable);
+
+                    // Test for https://github.com/WICG/webcrypto-secure-curves/pull/24
+                    if (format === "jwk" && extractable) {
+                        testJwkAlgBehaviours(algorithm, data.jwk, vector.name, usages);
+                    }
                 });
 
             });
@@ -93,6 +98,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                     var data = keyData[vector.name];
 
                     testFormat(format, algorithm, data, vector.name, usages, extractable);
+
+                    // Test for https://github.com/WICG/webcrypto-secure-curves/pull/24
+                    if (format === "jwk" && extractable) {
+                        testJwkAlgBehaviours(algorithm, data.jwk, vector.name, usages);
+                    }
                 });
             });
         });
@@ -125,6 +135,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                 assert_unreached("Threw an unexpected error: " + err.toString());
             });
         }, "Good parameters: " + keySize.toString() + " bits " + parameterString(format, keyData[format], algorithm, extractable, usages));
+    }
+
+    // Test importKey/exportKey "alg" behaviours, alg is ignored upon import and alg is missing for Ed25519 and Ed448 JWK export
+    // https://github.com/WICG/webcrypto-secure-curves/pull/24
+    function testJwkAlgBehaviours(algorithm, keyData, crv, usages) {
+        promise_test(function(test) {
+            return subtle.importKey('jwk', { ...keyData, alg: 'this is ignored' }, algorithm, true, usages).
+            then(function(key) {
+                assert_equals(key.constructor, CryptoKey, "Imported a CryptoKey object");
+
+                return subtle.exportKey('jwk', key).
+                then(function(result) {
+                    assert_equals(Object.keys(result).length, keyData.d ? 6 : 5, "Correct number of JWK members");
+                    assert_equals(result.alg, undefined, 'No JWK "alg" member is present');
+                    assert_true(equalJwk(keyData, result), "Round trip works");
+                }, function(err) {
+                    assert_unreached("Threw an unexpected error: " + err.toString());
+                });
+            }, function(err) {
+                assert_unreached("Threw an unexpected error: " + err.toString());
+            });
+        }, "Good parameters with ignored JWK alg: " + crv.toString() + " " + parameterString('jwk', keyData, algorithm, true, usages));
     }
 
 
