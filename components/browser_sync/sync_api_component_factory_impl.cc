71ce916f9a3991ab50834d863d3c3db0b104c305
Rushan Suleymanov <rushans@google.com>
2023-10-04 09:51:24 +0000

    Add custom model type controller for incoming sharing invitations
    The ModelTypeController is used to control
    IncomingPasswordSharingInvitation sync data type and to couple it with
    Password data type. Since incoming passwords are stored in Passwords
    data type, it's required to be active.
    
    Bug: 1478704
    Change-Id: I3e9826b2aee3a5a4229d1d811118c5a0e9206c29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905365
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Auto-Submit: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1205132}

--- components/browser_sync/sync_api_component_factory_impl.cc
+++ components/browser_sync/sync_api_component_factory_impl.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history/core/browser/sync/history_delete_directives_model_type_controller.h"
 #include "components/history/core/browser/sync/history_model_type_controller.h"
 #include "components/password_manager/core/browser/password_store_interface.h"
+#include "components/password_manager/core/browser/sharing/incoming_password_sharing_invitation_model_type_controller.h"
 #include "components/password_manager/core/browser/sharing/password_receiver_service.h"
 #include "components/password_manager/core/browser/sharing/password_sender_service.h"
 #include "components/password_manager/core/browser/sync/credential_model_type_controller.h"
@@ -406,16 +407,11 @@ SyncApiComponentFactoryImpl::CreateCommonDataTypeControllers(
       // Couple password sharing invitations with password data type.
       if (!disabled_types.Has(syncer::INCOMING_PASSWORD_SHARING_INVITATION) &&
           sync_client_->GetPasswordReceiverService()) {
-        syncer::ModelTypeControllerDelegate* delegate =
-            sync_client_->GetPasswordReceiverService()
-                ->GetControllerDelegate()
-                .get();
-        controllers.push_back(std::make_unique<syncer::ModelTypeController>(
-            syncer::INCOMING_PASSWORD_SHARING_INVITATION,
-            std::make_unique<syncer::ForwardingModelTypeControllerDelegate>(
-                delegate),
-            std::make_unique<syncer::ForwardingModelTypeControllerDelegate>(
-                delegate)));
+        controllers.push_back(
+            std::make_unique<
+                password_manager::
+                    IncomingPasswordSharingInvitationModelTypeController>(
+                sync_service, sync_client_->GetPasswordReceiverService()));
       }
 
       if (!disabled_types.Has(syncer::OUTGOING_PASSWORD_SHARING_INVITATION) &&
---
+++ components/password_manager/core/browser/sharing/incoming_password_sharing_invitation_model_type_controller.h
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_SHARING_INCOMING_PASSWORD_SHARING_INVITATION_MODEL_TYPE_CONTROLLER_H_
+#define COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_SHARING_INCOMING_PASSWORD_SHARING_INVITATION_MODEL_TYPE_CONTROLLER_H_
+
+#include "base/scoped_observation.h"
+#include "components/sync/service/model_type_controller.h"
+#include "components/sync/service/sync_service.h"
+#include "components/sync/service/sync_service_observer.h"
+
+namespace password_manager {
+class PasswordReceiverService;
+
+class IncomingPasswordSharingInvitationModelTypeController
+    : public syncer::ModelTypeController,
+      public syncer::SyncServiceObserver {
+ public:
+  IncomingPasswordSharingInvitationModelTypeController(
+      syncer::SyncService* sync_service,
+      PasswordReceiverService* password_receiver_service);
+
+  IncomingPasswordSharingInvitationModelTypeController(
+      const IncomingPasswordSharingInvitationModelTypeController&) = delete;
+  IncomingPasswordSharingInvitationModelTypeController& operator=(
+      const IncomingPasswordSharingInvitationModelTypeController&) = delete;
+  ~IncomingPasswordSharingInvitationModelTypeController() override;
+
+  // syncer::DataTypeController implementation.
+  PreconditionState GetPreconditionState() const override;
+
+  // syncer::SyncServiceObserver implementation.
+  void OnStateChanged(syncer::SyncService* sync) override;
+
+ private:
+  base::ScopedObservation<syncer::SyncService, syncer::SyncServiceObserver>
+      sync_observation_{this};
+  const raw_ptr<syncer::SyncService> sync_service_;
+};
+
+}  // namespace password_manager
+
+#endif  // COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_SHARING_INCOMING_PASSWORD_SHARING_INVITATION_MODEL_TYPE_CONTROLLER_H_
