2a6f27141dd1a6ee7e040f938b216eeed0f2a2c2
Siyu <siyua@chromium.org>
2023-10-05 23:23:37 +0000

    Clean up code for cloud token virtual cards (2 / n + 4)
    The project is cancelled so remove dead code from the code base.
    The cleaning will take at least 4 CLs to finish as how I see it.
    
    CL1: Clean up autofill suggestion
    CL2: gstatic reader (this one)
    CL3: UI related code, autofill client, virtualcardselectiondialog, test.
    CL4: Cloud token data model & sync (maybe sync will be a standalone CL)
    
    Bug: 1020740, 1419097
    Change-Id: Id3fc962960a056e3b78b67c6ffc297ab10d7f74b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4895745
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Siyu An <siyua@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206124}

--- chrome/browser/ui/autofill/chrome_autofill_client.h
+++ chrome/browser/ui/autofill/chrome_autofill_client.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "build/build_config.h"
-#include "chrome/browser/autofill/autofill_gstatic_reader.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/autofill/payments/autofill_error_dialog_controller_impl.h"
 #include "chrome/browser/ui/autofill/payments/autofill_progress_dialog_controller_impl.h"
@@ -176,8 +175,6 @@ class ChromeAutofillClient : public ContentAutofillClient,
   void HideVirtualCardEnrollBubbleAndIconIfVisible() override;
 #endif
 #if !BUILDFLAG(IS_ANDROID)
-  std::vector<std::string> GetAllowedMerchantsForVirtualCards() override;
-  std::vector<std::string> GetAllowedBinRangesForVirtualCards() override;
   void ShowLocalCardMigrationDialog(
       base::OnceClosure show_migration_dialog_closure) override;
   void ConfirmMigrateLocalCardToCloud(
