68c3c00ff18d737325fb84180842ff515c18ef9d
Li Lin <llin@chromium.org>
2023-10-05 00:32:41 +0000

    Add growth campaigns device targeting.
    Bug: b/300339880
    Change-Id: Ic951bbd3782616e9753ec4b04d5118652e0251a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4906509
    Reviewed-by: Xiqi Ruan <xiqiruan@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Li Lin <llin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205581}

--- chrome/browser/ash/growth/campaigns_manager_client_impl.h
+++ chrome/browser/ash/growth/campaigns_manager_client_impl.h
@@ -30,6 +30,7 @@ class CampaignsManagerClientImpl : public growth::CampaignsManagerClient {
   bool IsDeviceInDemoMode() const override;
   bool IsCloudGamingDevice() const override;
   bool IsFeatureAwareDevice() const override;
+  const std::string& GetApplicationLocale() const override;
 
  private:
   void OnComponentDownloaded(
--- chromeos/ash/components/growth/campaigns_manager_client.h
+++ chromeos/ash/components/growth/campaigns_manager_client.h
@@ -35,6 +35,9 @@ class CampaignsManagerClient {
 
   // True if the device is feature aware device.
   virtual bool IsFeatureAwareDevice() const = 0;
+
+  // Returns application locale.
+  virtual const std::string& GetApplicationLocale() const = 0;
 };
 
 }  // namespace growth
