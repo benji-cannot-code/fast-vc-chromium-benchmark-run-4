ed8acf4fdd77027df11c74e418599ea4098271e7
Ian Vollick <vollick@chromium.org>
2023-10-05 17:27:51 +0000

    [ios] Fix typo in gl_factory_ios.mm
    It looks like an extra 'new' was added in crrev.com/c/4912635
    
    Bug: 1411704
    Change-Id: I428d6e0ac659d0e977b4108cbee7cac0d20bd4b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4915277
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205906}

--- ui/gl/init/gl_factory_ios.mm
+++ ui/gl/init/gl_factory_ios.mm
@@ -90,7 +90,7 @@ scoped_refptr<GLSurface> CreateOffscreenGLSurfaceWithFormat(
     }
     case kGLImplementationMockGL:
     case kGLImplementationStubGL:
-      return new InitializeGLSurface(new GLSurfaceStub());
+      return InitializeGLSurface(new GLSurfaceStub());
     default:
       NOTREACHED();
       return nullptr;
