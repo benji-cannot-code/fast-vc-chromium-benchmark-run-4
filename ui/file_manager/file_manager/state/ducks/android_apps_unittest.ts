0718161403b83af878557ebc0fcbc9b7e1e6aa5f
Wenbo Jie <wenbojie@chromium.org>
2023-10-05 01:15:34 +0000

    [Files F2] Use generic icon if IconSet is not provided
    For android app tree items and volume tree items, if IconSet is
    not provided or returns 'none' background, we need to use generic
    icon instead.
    
    Bug: b:302423177
    Test: browser_tests --gtest_filer="*DirectoryTreeContainer*"
    Change-Id: I8cd7029be5b8e3357b42fdce7a77c08590209a98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4900216
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205598}

--- ui/file_manager/file_manager/state/ducks/android_apps_unittest.ts
+++ ui/file_manager/file_manager/state/ducks/android_apps_unittest.ts
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {State} from '../../externs/ts/state.js';
+import {constants} from '../../foreground/js/constants.js';
 import {setupStore, waitDeepEquals} from '../for_tests.js';
 
 import {addAndroidApps} from './android_apps.js';
@@ -21,7 +22,7 @@ export async function testAddAndroidApps(done: () => void) {
       name: 'App 2',
       packageName: 'com.test.app2',
       activityName: 'Activity2',
-      iconSet: {icon16x16Url: 'url3', icon32x32Url: 'url4'},
+      iconSet: {icon16x16Url: '', icon32x32Url: ''},
     },
   ];
 
@@ -31,8 +32,14 @@ export async function testAddAndroidApps(done: () => void) {
 
   // Expect both android apps are existed in the store.
   const want: State['androidApps'] = {
-    'com.test.app1': androidApps[0],
-    'com.test.app2': androidApps[1],
+    'com.test.app1': {
+      ...androidApps[0],
+      icon: androidApps[0]!.iconSet,
+    },
+    'com.test.app2': {
+      ...androidApps[1],
+      icon: constants.ICON_TYPES.GENERIC,
+    },
   };
   await waitDeepEquals(store, want, (state) => state.androidApps);
 
