b79a7afd36630f7ea2b6b57724e8d8f1dbf49d7c
Rubin Deliallisi <rubindl@chromium.org>
2023-10-05 13:01:57 +0000

    Remove 100% enabled flag from tests
    - PrivacySandbox4 has been turned to default enabled, so removing
      outdated test references.
    
    Bug: 1489402
    Change-Id: I576a1c7d1019b4bf88cf4fcd3bebed146a7bdf72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4912499
    Auto-Submit: Rubin Deliallisi <rubindl@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205788}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/GoogleServicesSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/GoogleServicesSettingsTest.java
@@ -24,7 +24,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
@@ -257,7 +256,6 @@ public class GoogleServicesSettingsTest {
 
     @Test
     @LargeTest
-    @EnableFeatures(ChromeFeatureList.PRIVACY_SANDBOX_SETTINGS_4)
     @MinAndroidSdkLevel(
             value = Build.VERSION_CODES.Q, reason = "Digital Wellbeing is only available from Q.")
     public void
@@ -278,11 +276,10 @@ public class GoogleServicesSettingsTest {
 
     @Test
     @LargeTest
-    @EnableFeatures(ChromeFeatureList.PRIVACY_SANDBOX_SETTINGS_4)
     @MinAndroidSdkLevel(
             value = Build.VERSION_CODES.Q, reason = "Digital Wellbeing is only available from Q.")
     public void
-    testUsageStatsReportingNotShown_FeatureEnabledPrefDisabled() {
+    testUsageStatsReportingNotShown() {
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             PrefService prefService = UserPrefs.get(Profile.getLastUsedRegularProfile());
             prefService.setBoolean(Pref.USAGE_STATS_ENABLED, false);
@@ -292,28 +289,6 @@ public class GoogleServicesSettingsTest {
 
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             Assert.assertNull("Usage stats should not exist when the pref is not set.",
-                    googleServicesSettings.findPreference(
-                            GoogleServicesSettings.PREF_USAGE_STATS_REPORTING));
-        });
-    }
-
-    @Test
-    @LargeTest
-    @DisableFeatures(ChromeFeatureList.PRIVACY_SANDBOX_SETTINGS_4)
-    @MinAndroidSdkLevel(
-            value = Build.VERSION_CODES.Q,
-            reason = "Digital Wellbeing is only available from Q.")
-    @DisabledTest
-    public void testUsageStatsReportingNotShown_FeatureDisabled() {
-        TestThreadUtils.runOnUiThreadBlocking(() -> {
-            PrefService prefService = UserPrefs.get(Profile.getLastUsedRegularProfile());
-            prefService.setBoolean(Pref.USAGE_STATS_ENABLED, true);
-        });
-
-        final GoogleServicesSettings googleServicesSettings = startGoogleServicesSettings();
-
-        TestThreadUtils.runOnUiThreadBlocking(() -> {
-            Assert.assertNull("Usage stats should not exist when the feature is not enabled.",
                     googleServicesSettings.findPreference(
                             GoogleServicesSettings.PREF_USAGE_STATS_REPORTING));
         });
