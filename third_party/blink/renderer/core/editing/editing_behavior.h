b7cb0fc66e063adf05af984186264e34a0690a8e
Kent Tamura <tkent@chromium.org>
2023-10-05 09:45:20 +0000

    Editing: Arrow keys should move the text caret to their physical directions
    This CL adds a WritingMode argument to `EditingBehavior::
    InterpretKeyEvent()`.
    
    * Editor::HandleEditingKeyboardEvent() determines a WritingMode from
      the text selection or the focused element, and pass it to
      InterpretKeyEvent().
    * InterpretKeyEvent() translates a key code for an arrow key depending
      on the specified WritingMode.
    
    Bug: 484651
    Change-Id: I534d30d58ff7d870d42d9e14d185bd784c87f455
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4910691
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205731}

--- third_party/blink/renderer/core/editing/editing_behavior.h
+++ third_party/blink/renderer/core/editing/editing_behavior.h
@@ -28,7 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 
 namespace blink {
+
 class KeyboardEvent;
+enum class WritingMode : uint8_t;
 
 class CORE_EXPORT EditingBehavior {
   STACK_ALLOCATED();
@@ -137,7 +139,8 @@ class CORE_EXPORT EditingBehavior {
 
   // Convert a KeyboardEvent to a command name like "Copy", "Undo" and so on.
   // If nothing, return empty string.
-  const char* InterpretKeyEvent(const KeyboardEvent&) const;
+  const char* InterpretKeyEvent(const KeyboardEvent&,
+                                WritingMode writing_mode) const;
 
   bool ShouldInsertCharacter(const KeyboardEvent&) const;
 
