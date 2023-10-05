5b17b9945fe0eacea420e9ef0a7567cfc8017f3f
Timofey Chudakov <tchudakov@google.com>
2023-10-05 18:00:30 +0000

    [mullet m2] Implement interactive UI tests for the delete dialog.
    Recently added DeleteAddressProfileDialogView requires pixel tests
    to track UI changes over time. Interactive tests added in this CL
    verify that UI has expected behavior for button clicks, etc.
    
    Bug: 1459990
    Change-Id: Ia80d4507b7546d7b9e672550ff846638e938c89b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905025
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205928}

--- chrome/browser/ui/views/autofill/delete_address_profile_dialog_view.cc
+++ chrome/browser/ui/views/autofill/delete_address_profile_dialog_view.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "ui/base/models/dialog_model.h"
 #include "ui/views/widget/widget.h"
+#include "ui/views/window/dialog_client_view.h"
 
 namespace autofill {
 
@@ -28,13 +29,15 @@ void ShowDeleteAddressProfileDialogView(
           .AddOkButton(
               base::BindOnce(&DeleteAddressProfileDialogController::OnAccepted,
                              controller),
-              ui::DialogModelButton::Params().SetLabel(
-                  controller->GetAcceptButtonText()))
+              ui::DialogModelButton::Params()
+                  .SetId(views::DialogClientView::kOkButtonElementId)
+                  .SetLabel(controller->GetAcceptButtonText()))
           .AddCancelButton(
               base::BindOnce(&DeleteAddressProfileDialogController::OnCanceled,
                              controller),
-              ui::DialogModelButton::Params().SetLabel(
-                  controller->GetDeclineButtonText()))
+              ui::DialogModelButton::Params()
+                  .SetId(views::DialogClientView::kCancelButtonElementId)
+                  .SetLabel(controller->GetDeclineButtonText()))
           .AddParagraph(
               ui::DialogModelLabel(controller->GetDeleteConfirmationText())
                   .set_is_secondary())
