774dcd9ed21536d00615353de21d55372c7d03be
Emily Shack <emshack@chromium.org>
2023-10-05 18:19:39 +0000

    Tab Organization: Add failure state
    Also cleans up/standardizes a couple things in the other
    tab_organization_foo files.
    
    Bug: 1469126
    Change-Id: I017ca207cbde3d9f03c53a3b266807f48d6d2eac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4904394
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Charles Meng <charlesmeng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205940}

--- chrome/browser/resources/tab_search/tab_organization_not_started.ts
+++ chrome/browser/resources/tab_search/tab_organization_not_started.ts
@@ -4,13 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
-import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
 import './tab_organization_shared_style.css.js';
 
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './tab_organization_not_started.html.js';
 
+// Not started state for the tab organization UI.
 export class TabOrganizationNotStartedElement extends PolymerElement {
   static get is() {
     return 'tab-organization-not-started';
