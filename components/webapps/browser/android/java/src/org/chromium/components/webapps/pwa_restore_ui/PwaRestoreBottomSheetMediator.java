fa6624cd611351670d8f9d0a3400e13f054bac6a
Alexander Timin <altimin@chromium.org>
2023-10-05 10:37:39 +0000

    Revert "[PwaRestore]: Flesh out the second sheet a bit more."
    This reverts commit fffc81ac0ec4c8d5da4dc6d4f9eaa646a632edbf.
    
    Reason for revert: crbug.com/1489608
    
    This seems to be causing build failures due to errors in pwa_restore_bottom_sheet_content.xml.
    
    Original change's description:
    > [PwaRestore]: Flesh out the second sheet a bit more.
    >
    > This change adds the Deselect All button at the bottom
    > (which calls a function that does nothing), as well as
    > an empty ScrollView and polishes the layout a bit to
    > look more like the mocks.
    >
    > Bug: 1481570
    > Change-Id: I4c320428c0a274e85f510280a94ce74360fb6d21
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4911935
    > Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1205480}
    
    Bug: 1481570
    Bug: 1489608
    Change-Id: I9ebdce706d31292937630fb14e8a5cb4839c7bb3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4912557
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Owners-Override: Alexander Timin <altimin@chromium.org>
    Auto-Submit: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205751}

--- components/webapps/browser/android/java/src/org/chromium/components/webapps/pwa_restore_ui/PwaRestoreBottomSheetMediator.java
+++ components/webapps/browser/android/java/src/org/chromium/components/webapps/pwa_restore_ui/PwaRestoreBottomSheetMediator.java
@@ -24,8 +24,8 @@ class PwaRestoreBottomSheetMediator {
     PwaRestoreBottomSheetMediator(
             Activity activity, Runnable onReviewButtonClicked, Runnable onBackButtonClicked) {
         mActivity = activity;
-        mModel = PwaRestoreProperties.createModel(onReviewButtonClicked, onBackButtonClicked,
-                this::onDeselectButtonClicked, this::onRestoreButtonClicked);
+        mModel = PwaRestoreProperties.createModel(
+                onReviewButtonClicked, onBackButtonClicked, this::onRestoreButtonClicked);
 
         initializeState();
         setPeekingState();
@@ -45,8 +45,6 @@ class PwaRestoreBottomSheetMediator {
                 mActivity.getString(R.string.pwa_restore_description_expanded));
         mModel.set(PwaRestoreProperties.EXPANDED_BUTTON_LABEL,
                 mActivity.getString(R.string.pwa_restore_button_expanded));
-        mModel.set(PwaRestoreProperties.DESELECT_BUTTON_LABEL,
-                mActivity.getString(R.string.pwa_restore_button_deselect));
     }
 
     protected void setPeekingState() {
@@ -55,10 +53,6 @@ class PwaRestoreBottomSheetMediator {
 
     protected void setPreviewState() {
         mModel.set(PwaRestoreProperties.VIEW_STATE, ViewState.VIEW_PWA_LIST);
-    }
-
-    private void onDeselectButtonClicked() {
-        // TODO(finnur): Implement.
     }
 
     private void onRestoreButtonClicked() {
