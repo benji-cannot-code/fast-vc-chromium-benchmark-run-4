d2080fe6cf03e0d9df3d73935b9668daace73cd6
Noureldien Refae <noureldien@google.com>
2023-10-05 09:57:49 +0000

    CBD Timeframe Experiment: Add new pref for the experiment
    Add new 2 prefs for the experiment where each pref belongs to
    different tabs in the CBD dialog (basic/advanced).
    
    Bug: 1487530
    Change-Id: I83042e6cc8913584097980934b9e71f616d5a461
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905815
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Nour Eldien Ayman <noureldien@google.com>
    Cr-Commit-Position: refs/heads/main@{#1205740}

--- components/sync_preferences/common_syncable_prefs_database.cc
+++ components/sync_preferences/common_syncable_prefs_database.cc
@@ -108,6 +108,8 @@ enum {
   kDogfoodGroupsSyncPrefName = 62,
   kSyncableMergeableDictPrefForTesting = 63,  // For tests.
   kAutofillPaymentCvcStorage = 64,
+  kDeleteTimePeriodV2 = 65,
+  kDeleteTimePeriodV2Basic = 66,
   // See components/sync_preferences/README.md about adding new entries here.
   // vvvvv IMPORTANT! vvvvv
   // Note to the reviewer: IT IS YOUR RESPONSIBILITY to ensure that new syncable
@@ -186,6 +188,12 @@ const auto& SyncablePreferences() {
               MergeBehavior::kNone}},
             {browsing_data::prefs::kDeleteTimePeriodBasic,
              {syncable_prefs_ids::kDeleteTimePeriodBasic, syncer::PREFERENCES,
+              false, MergeBehavior::kNone}},
+            {browsing_data::prefs::kDeleteTimePeriodV2,
+             {syncable_prefs_ids::kDeleteTimePeriodV2, syncer::PREFERENCES,
+              false, MergeBehavior::kNone}},
+            {browsing_data::prefs::kDeleteTimePeriodV2Basic,
+             {syncable_prefs_ids::kDeleteTimePeriodV2Basic, syncer::PREFERENCES,
               false, MergeBehavior::kNone}},
             {browsing_data::prefs::kLastClearBrowsingDataTime,
              {syncable_prefs_ids::kLastClearBrowsingDataTime,
