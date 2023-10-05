c8efaa69ce3f6d37ec5818dccc4d21543f2240a7
Anastasia Helfinstein <anastasi@google.com>
2023-10-05 22:05:29 +0000

    [ChromeVox] Move cycleWithAnnouncement to TypingEcho
    AX-Relnotes: n/a.
    Bug: b/265481751
    Test: ChromeVoxEditingTest.*
    Change-Id: If876552a14cd94f8207d7d36419cc7baf875e095
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4877204
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1206075}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/editing/typing_echo.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/editing/typing_echo.js
@@ -4,6 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {LocalStorage} from '../../../common/local_storage.js';
+import {Msgs} from '../../common/msgs.js';
+import {Personality, QueueMode} from '../../common/tts_types.js';
+import {ChromeVox} from '../chromevox.js';
+
 
 /**
  * A list of typing echo options.
@@ -39,6 +43,27 @@ export class TypingEcho {
    */
   static cycle(cur) {
     return ((cur ?? TypingEcho.current) + 1) % STATE_COUNT;
+  }
+
+  static cycleWithAnnouncement() {
+    LocalStorage.set(
+        'typingEcho', TypingEcho.cycle(LocalStorage.getNumber('typingEcho')));
+    let announce = '';
+    switch (LocalStorage.get('typingEcho')) {
+      case TypingEchoState.CHARACTER:
+        announce = Msgs.getMsg('character_echo');
+        break;
+      case TypingEchoState.WORD:
+        announce = Msgs.getMsg('word_echo');
+        break;
+      case TypingEchoState.CHARACTER_AND_WORD:
+        announce = Msgs.getMsg('character_and_word_echo');
+        break;
+      case TypingEchoState.NONE:
+        announce = Msgs.getMsg('none_echo');
+        break;
+    }
+    ChromeVox.tts.speak(announce, QueueMode.FLUSH, Personality.ANNOTATION);
   }
 
   /**
