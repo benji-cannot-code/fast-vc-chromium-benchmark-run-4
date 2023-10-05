f989f605fe3ae0154cf7cbfd23322de9c177d8f5
Veronique Nguyen <veronguyen@google.com>
2023-10-05 15:55:38 +0000

    Add SetConsiderFormAsSecureForTesting to BrowserAutofillManagerTestApi
    Instead of calling SetConsiderFormAsSecureForTesting directly from
    BrowserAutofillManager, we want to call through the
    BrowserAutofillManagerTestApi.
    
    Bug: 1487700
    Change-Id: Ie6a97e724a814adf6409e6cfa03919974e97c9e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4902197
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Veronique Nguyen <veronguyen@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1205863}

--- ios/chrome/browser/ui/autofill/autofill_app_interface.mm
+++ ios/chrome/browser/ui/autofill/autofill_app_interface.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/strings/utf_string_conversions.h"
 #import "base/test/ios/wait_util.h"
 #import "components/autofill/core/browser/autofill_test_utils.h"
+#import "components/autofill/core/browser/browser_autofill_manager_test_api.h"
 #import "components/autofill/core/browser/form_data_importer.h"
 #import "components/autofill/core/browser/payments/credit_card_save_manager.h"
 #import "components/autofill/core/browser/personal_data_manager.h"
@@ -481,8 +482,9 @@ class SaveCardInfobarEGTestHelper
       autofill::AutofillJavaScriptFeature::GetInstance()->GetWebFramesManager(
           web_state);
   web::WebFrame* main_frame = frames_manager->GetMainWebFrame();
-  autofill::AutofillDriverIOS::FromWebStateAndWebFrame(web_state, main_frame)
-      ->GetAutofillManager()
+  test_api(autofill::AutofillDriverIOS::FromWebStateAndWebFrame(web_state,
+                                                                main_frame)
+               ->GetAutofillManager())
       .SetConsiderFormAsSecureForTesting(true);
 }
 
