d7550d64bd5fed02177e00fdf93765b55ea3dbe2
Rupert Ben Wiser <bewise@chromium.org>
2023-10-05 10:08:40 +0000

    webview: Add feature flags and switches to crash for flag overrides
    This should be helpful in knowing if a tester with a crash report had any flag overrides that could have contributed to their issue.
    
    Verified this works:
    http://crash/browse?q=reportid=%270b543683f17b0b76%27#2
    
    The switches have their own section in the UI already:
    https://screenshot.googleplex.com/5ujWQJwhBmgZ46Y
    
    The chrome command line features show up in product data
    (https://screenshot.googleplex.com/C7Qccderkuy2YUS)
    because there is no crash parsing for those fields.
    I've copied the format the ChromeOS folk follow.
    We could always make the case in the future that this is worth while to
    add a new section if needed.
    
    This will only work for the browser process crashes since I am relying on the overrides fields and those aren't set in the render process.
    
    Bug: 1484644
    Change-Id: I4ba6a2da7ca03b6fe0be87f2f8765d5d67127ef2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4890218
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Commit-Queue: Rupert Wiser <bewise@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205745}

--- android_webview/browser/aw_contents_statics.cc
+++ android_webview/browser/aw_contents_statics.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "android_webview/browser/aw_content_browser_client.h"
 #include "android_webview/browser/aw_contents.h"
 #include "android_webview/browser/aw_contents_io_thread_client.h"
+#include "android_webview/browser/aw_crash_keys.h"
 #include "android_webview/browser/safe_browsing/aw_safe_browsing_allowlist_manager.h"
 #include "android_webview/browser_jni_headers/AwContentsStatics_jni.h"
 #include "base/android/jni_array.h"
@@ -145,8 +146,10 @@ void JNI_AwContentsStatics_LogFlagMetrics(
   for (const auto& jfeature : jfeatures.ReadElements<jstring>()) {
     features.insert(ConvertJavaStringToUTF8(jfeature));
   }
+
   flags_ui::ReportAboutFlagsHistogram("Launch.FlagsAtStartup", switches,
                                       features);
+  SetCrashKeysFromFeaturesAndSwitches(switches, features);
 }
 
 // static
