4ef69046aef38b8e383a9cc182d8e1bdd554b99c
Christian Dullweber <dullweber@chromium.org>
2023-10-05 17:27:25 +0000

    SAA: Add category settings page for StorageAccess
    Add StorageAccess permissions as a flat list in site settings.
    
    Screenshot: https://crbug.com/1478113#c10
    Bug: 1478113
    Change-Id: I07eb3dbeed0d7f51b95f0f0d48716f1b29362608
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905467
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205905}

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SiteSettingsCategory.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SiteSettingsCategory.java
@@ -83,10 +83,11 @@ public class SiteSettingsCategory {
         int SITE_DATA = 27;
         int ANTI_ABUSE = 28;
         int ZOOM = 29;
+        int STORAGE_ACCESS = 30;
         /**
          * Number of handled categories used for calculating array sizes.
          */
-        int NUM_ENTRIES = 30;
+        int NUM_ENTRIES = 31;
     }
 
     private final BrowserContextHandle mBrowserContextHandle;
@@ -209,6 +210,8 @@ public class SiteSettingsCategory {
                 return ContentSettingsType.PROTECTED_MEDIA_IDENTIFIER;
             case Type.SENSORS:
                 return ContentSettingsType.SENSORS;
+            case Type.STORAGE_ACCESS:
+                return ContentSettingsType.STORAGE_ACCESS;
             case Type.SOUND:
                 return ContentSettingsType.SOUND;
             case Type.USB:
@@ -291,6 +294,8 @@ public class SiteSettingsCategory {
                 return "protected_content";
             case Type.SENSORS:
                 return "sensors";
+            case Type.STORAGE_ACCESS:
+                return "storage_access";
             case Type.SOUND:
                 return "sound";
             case Type.USB:
