c7fcc1d5b7f344e7cd019f76e3b21de2f74c1ebc
Finnur Thorarinsson <finnur@chromium.org>
2023-10-05 13:38:34 +0000

    Reland "[PwaRestore]: Flesh out the second sheet a bit more."
    (this time with build fix for Components UnitTests, see
    one-liner change in patchset 2).
    
    This change adds the Deselect All button at the bottom
    (which calls a function that does nothing), as well as
    an empty ScrollView and polishes the layout a bit to
    look more like the mocks.
    
    Bug: 1481570, 1489608
    Change-Id: I4bf91551b7b28c6e1aad112cf239cba11718829b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4912482
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Auto-Submit: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205805}

--- components/webapps/browser/android/java/src/org/chromium/components/webapps/pwa_restore_ui/PwaRestoreBottomSheetMediator.java
+++ components/webapps/browser/android/java/src/org/chromium/components/webapps/pwa_restore_ui/PwaRestoreBottomSheetMediator.java
@@ -24,8 +24,8 @@ class PwaRestoreBottomSheetMediator {
     PwaRestoreBottomSheetMediator(
             Activity activity, Runnable onReviewButtonClicked, Runnable onBackButtonClicked) {
         mActivity = activity;
-        mModel = PwaRestoreProperties.createModel(
-                onReviewButtonClicked, onBackButtonClicked, this::onRestoreButtonClicked);
+        mModel = PwaRestoreProperties.createModel(onReviewButtonClicked, onBackButtonClicked,
+                this::onDeselectButtonClicked, this::onRestoreButtonClicked);
 
         initializeState();
         setPeekingState();
@@ -45,6 +45,8 @@ class PwaRestoreBottomSheetMediator {
                 mActivity.getString(R.string.pwa_restore_description_expanded));
         mModel.set(PwaRestoreProperties.EXPANDED_BUTTON_LABEL,
                 mActivity.getString(R.string.pwa_restore_button_expanded));
+        mModel.set(PwaRestoreProperties.DESELECT_BUTTON_LABEL,
+                mActivity.getString(R.string.pwa_restore_button_deselect));
     }
 
     protected void setPeekingState() {
@@ -53,6 +55,10 @@ class PwaRestoreBottomSheetMediator {
 
     protected void setPreviewState() {
         mModel.set(PwaRestoreProperties.VIEW_STATE, ViewState.VIEW_PWA_LIST);
+    }
+
+    private void onDeselectButtonClicked() {
+        // TODO(finnur): Implement.
     }
 
     private void onRestoreButtonClicked() {
