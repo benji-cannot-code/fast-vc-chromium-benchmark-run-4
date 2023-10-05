568678df77d451a8dc0b3c2c223bb3033b19defa
Emilia Paz <emiliapaz@chromium.org>
2023-10-05 17:09:20 +0000

    Revert "Record how often debugger pauses same-origin pages in multiple tabs"
    This reverts commit 7a77454509407666d8a03b13932d44a3df0eb0ab.
    
    Reason for revert:
    Since CL was landed, DevToolsTest.PauseWhenSameOriginDebuggerAlreadyPaused has been failing.
    
    First failure: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests%20(dbg)(1)/116219/test-results?sortby=&groupby=
    
    
    Original change's description:
    > Record how often debugger pauses same-origin pages in multiple tabs
    >
    > This CL adds histograms that record how often DevTools debuggers are
    > already attached or paused for the target origin in another tab when
    > a debugger tries to pause the origin.
    >
    > Bug: 1449114
    > Change-Id: Idd77983297489fb72f6a2a3d7aee19b4c225a6f7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4801011
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    > Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    > Reviewed-by: Danil Somsikov <dsv@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1205778}
    
    Bug: 1449114
    Change-Id: Ib8cb06134cfca93cac94da879ba610a24186e399
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4917292
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Owners-Override: Emilia Paz <emiliapaz@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1205896}

--- content/browser/devtools/devtools_renderer_channel.cc
+++ content/browser/devtools/devtools_renderer_channel.cc
@@ -222,12 +222,4 @@ void DevToolsRendererChannel::ChildTargetDestroyed(
   child_targets_.erase(host);
 }
 
-void DevToolsRendererChannel::MainThreadDebuggerPaused() {
-  owner_->MainThreadDebuggerPaused();
-}
-
-void DevToolsRendererChannel::MainThreadDebuggerResumed() {
-  owner_->MainThreadDebuggerResumed();
-}
-
 }  // namespace content
