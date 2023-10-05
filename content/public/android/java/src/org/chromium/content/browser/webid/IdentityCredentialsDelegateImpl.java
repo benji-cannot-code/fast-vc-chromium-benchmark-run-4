27ef334118da8d301ecb9bac0360efd2205a6b65
Sam Goto <goto@chromium.org>
2023-10-05 18:12:15 +0000

    [wallets] Start removing the unused get() call by setting a default impl
    We no longer use the signature that was introduced, but rather use a
    new one for the get() call. This CL starts cleaning up the unused
    signature by creating a default implementation. The next CL will remove
    the implementation from //clank and then this can be entirely removed
    from upstream.
    
    Process described here:
    
    https://source.chromium.org/chromium/chromium/src/+/main:content/public/android/java/src/org/chromium/content/browser/webid/IdentityCredentialsDelegate.java;l=26?q=identitycredentialsde&ss=chromium
    
    Bug: 1416939
    Change-Id: I31dd2437992f477247aa7ee4507bd2a69c7f3d75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4913919
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Commit-Queue: Sam Goto <goto@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205936}

--- content/public/android/java/src/org/chromium/content/browser/webid/IdentityCredentialsDelegateImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/webid/IdentityCredentialsDelegateImpl.java
@@ -5,16 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.content.browser.webid;
 
-import org.chromium.base.Promise;
-
 /**
  * Public no-op implementation of IdentityCredentialsDelegate.
  *
  * TODO(crbug.com/1475970) upstream the private version of this.
  */
 public class IdentityCredentialsDelegateImpl implements IdentityCredentialsDelegate {
-    @Override
-    public Promise<String> get(String origin, String request) {
-        return null;
-    }
 }
