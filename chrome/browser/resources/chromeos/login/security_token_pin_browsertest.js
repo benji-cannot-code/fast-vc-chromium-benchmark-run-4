86aee9b2b5faef8a1ea4fc8d760554f604d6ff9b
dpapad <dpapad@chromium.org>
2023-10-06 17:38:48 +0000

    WebUI: Use the mocha.js copy in third_party/node/node_modules/mocha.
    This is in preparation of removing the stand-alone third_party/mocha
    folder.
    
    Bug: 1490291
    Change-Id: Ic100f81a606f4f2de3b4177dab5a3f4a526abf9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4914055
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206504}

--- chrome/browser/resources/chromeos/login/security_token_pin_browsertest.js
+++ chrome/browser/resources/chromeos/login/security_token_pin_browsertest.js
@@ -31,7 +31,7 @@ var PolymerSecurityTokenPinTest = class extends PolymerTest {
 
   get extraLibraries() {
     return [
-      '//third_party/mocha/mocha.js',
+      '//third_party/node/node_modules/mocha/mocha.js',
       '//chrome/test/data/webui/mocha_adapter.js',
       'components/oobe_types.js',
     ];
