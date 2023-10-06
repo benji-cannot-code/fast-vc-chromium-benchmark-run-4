a5f85a54cef5844a578b78bbe0649149c3a41cac
L. David Baron <dbaron@chromium.org>
2023-10-06 18:22:31 +0000

    Add use counters for <details name=""> attribute presence and behaviors.
    The counter for the presence of the attribute is unconditional; the
    counters for the behaviors will only run when
    RuntimeEnabledFeatures::AccordionPatternEnabled() is true and the
    behaviors occur.
    
    Bug: 1444057
    Change-Id: I50a51ec20bf980881f8bbe9c854298e2079bb863
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4917156
    Auto-Submit: David Baron <dbaron@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206526}

--- third_party/blink/renderer/core/html/html_details_element.cc
+++ third_party/blink/renderer/core/html/html_details_element.cc
@@ -235,6 +235,9 @@ void HTMLDetailsElement::ParseAttribute(
             OtherElementsInNameGroup());
         for (HTMLDetailsElement* other_details : details_with_name) {
           CHECK_NE(other_details, this);
+          UseCounter::Count(
+              GetDocument(),
+              WebFeature::kHTMLDetailsElementNameAttributeClosesOther);
           other_details->setAttribute(html_names::kOpenAttr, g_null_atom);
         }
       }
@@ -254,6 +257,10 @@ void HTMLDetailsElement::AttributeChanged(
     const AttributeModificationParams& params) {
   const QualifiedName& name = params.name;
   if (name == html_names::kNameAttr) {
+    if (!params.new_value.empty()) {
+      UseCounter::Count(GetDocument(),
+                        WebFeature::kHTMLDetailsElementNameAttribute);
+    }
     MaybeCloseForExclusivity();
   }
 
@@ -289,6 +296,8 @@ void HTMLDetailsElement::MaybeCloseForExclusivity() {
     CHECK_NE(other_details, this);
     if (other_details->is_open_) {
       // close this details element
+      UseCounter::Count(GetDocument(),
+                        WebFeature::kHTMLDetailsElementNameAttributeClosesSelf);
       ToggleOpen();
       break;
     }
