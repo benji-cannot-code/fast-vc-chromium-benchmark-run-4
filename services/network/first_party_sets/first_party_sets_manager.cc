c7b1ceb58a46aa271bfb00ce3223f1685e8e8495
Stephan Hartmann <stha09@googlemail.com>
2023-10-06 13:56:11 +0000

    libstdc++: remove double-brace init in //network
    Compile fails because it would use explicit constructor to
    convert from initializer list.
    
    Bug: 957519
    Change-Id: I9a60b540439eea063dd8261f22cbc31f86d54c76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4911494
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206371}

--- services/network/first_party_sets/first_party_sets_manager.cc
+++ services/network/first_party_sets/first_party_sets_manager.cc
@@ -118,7 +118,7 @@ FirstPartySetsManager::FindEntries(
 
   if (!sets_.has_value()) {
     if (!wait_for_init_) {
-      return {{}};
+      return FirstPartySetsManager::EntriesResult();
     }
     EnqueuePendingQuery(base::BindOnce(
         &FirstPartySetsManager::FindEntriesAndInvoke,
