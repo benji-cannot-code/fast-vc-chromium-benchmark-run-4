778a8d774bcd5c053d2e10f15fab5c7c30e756ec
Joshua Hood <jdh@chromium.org>
2023-10-06 20:18:26 +0000

    [DIPS] Remove Privacy.DIPS.TimeFromStorageToInteraction.* histograms
    This CL removes the histograms.xml entry along with the corresponding
    instrumentation and testing for the
    Privacy.DIPS.TimeFromStorageToInteraction.* histograms.
    
    OBSOLETE_HISTOGRAMS=No longer needed.
    
    Bug: 1484181
    Change-Id: Ib7de5c2fb4146d919a0ae63da43e7414ca254b6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4886366
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Reviewed-by: Ryan Tarpine <rtarpine@chromium.org>
    Commit-Queue: Joshua Hood <jdh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206594}

--- chrome/browser/dips/dips_storage.cc
+++ chrome/browser/dips/dips_storage.cc
@@ -23,16 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-inline void UmaHistogramTimeToInteraction(base::TimeDelta sample,
-                                          DIPSCookieMode mode) {
-  const std::string name = base::StrCat(
-      {"Privacy.DIPS.TimeFromStorageToInteraction", GetHistogramSuffix(mode)});
-
-  base::UmaHistogramCustomTimes(name, sample,
-                                /*min=*/base::TimeDelta(),
-                                /*max=*/base::Days(7), 100);
-}
-
 // The number of sites to process in each call to DIPSStorage::Prepopulate().
 // Intended to be constant; settable only for testing.
 size_t g_prepopulate_chunk_size = 100;
@@ -194,14 +184,6 @@ void DIPSStorage::RecordInteraction(const GURL& url,
   DCHECK(db_);
 
   DIPSState state = Read(url);
-  if (!state.user_interaction_times().has_value() &&
-      state.site_storage_times().has_value()) {
-    // Site previously wrote to storage. Record metric for the time delay
-    // between first storage and interaction.
-    UmaHistogramTimeToInteraction(time - state.site_storage_times()->first,
-                                  mode);
-  }
-
   state.update_user_interaction_time(time);
 }
 
@@ -212,14 +194,6 @@ void DIPSStorage::RecordWebAuthnAssertion(const GURL& url,
   DCHECK(db_);
 
   DIPSState state = Read(url);
-  if (!state.web_authn_assertion_times().has_value() &&
-      state.site_storage_times().has_value()) {
-    // Site previously wrote to storage. Record metric for the time delay
-    // between first storage and interaction.
-    UmaHistogramTimeToInteraction(time - state.site_storage_times()->first,
-                                  mode);
-  }
-
   state.update_web_authn_assertion_time(time);
 }
 
