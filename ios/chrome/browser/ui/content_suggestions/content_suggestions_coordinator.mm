5d6680fc9ff24071e44c7c0d13db27a67afe8691
Chris Lu <thegreenfrog@chromium.org>
2023-10-06 20:50:37 +0000

    [ios] Add Module.Click histogram logging for Tab Resumption/Safety Check
    Fixed: 1490583
    Change-Id: Iedb45a3d9547f0770bc6026463a142e0c1dbf32d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4919141
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206618}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
@@ -537,6 +537,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)didSelectSafetyCheckItem:(SafetyCheckItemType)type {
   CHECK(IsSafetyCheckMagicStackEnabled());
 
+  [self.contentSuggestionsMetricsRecorder
+      recordMagicStackModuleEngagementForType:ContentSuggestionsModuleType::
+                                                  kSafetyCheck];
+
   IOSChromeSafetyCheckManager* safetyCheckManager =
       IOSChromeSafetyCheckManagerFactory::GetForBrowserState(
           self.browser->GetBrowserState());
--- ios/chrome/browser/ui/content_suggestions/content_suggestions_metrics_recorder.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_metrics_recorder.mm
@@ -158,6 +158,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)recordMostRecentTabOpened {
   base::RecordAction(base::UserMetricsAction(kOpenMostRecentTabAction));
+  [self recordMagicStackModuleEngagementForType:ContentSuggestionsModuleType::
+                                                    kTabResumption];
 }
 
 - (void)recordMostVisitedTilesShown {
