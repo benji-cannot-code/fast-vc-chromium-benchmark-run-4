2e2bdbd2ac5d22518c3c50707c49c6d4383fe91a
Chris Lu <thegreenfrog@chromium.org>
2023-10-06 16:09:46 +0000

    [ios] Fix Shortcuts Module Crash
    If for some reason -setShortcutTilesWithConfigs: has not been called
    before -viewDidLoad, self.shortcutsStackView would not be initialized.
    Add that initialization logic into -setShortcutTilesWithConfigs: if
    the Magic Stack was already created, and allow the logic in
    -populateMagicStack to no-op if the shortcutsModuleContainer is not
    ready.
    
    Bug: 1486289
    Change-Id: I893d7603fb025693393c16edaee2c1f1e89148cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4917496
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206448}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.mm
@@ -248,7 +248,6 @@ const base::TimeDelta kSetUpListHideAnimationDuration = base::Milliseconds(250);
     [self showSetUpListWithItems:_savedSetUpListItems];
   }
   if (self.shortcutsViews) {
-    self.shortcutsStackView = [self createShortcutsStackView];
     if (!IsMagicStackEnabled()) {
       [self addUIElement:self.shortcutsStackView
           withCustomBottomSpacing:kMostVisitedBottomMargin];
@@ -440,9 +439,18 @@ const base::TimeDelta kSetUpListHideAnimationDuration = base::Milliseconds(250);
         [[ContentSuggestionsShortcutTileView alloc] initWithConfiguration:item];
     [self.shortcutsViews addObject:view];
   }
-  if (IsMagicStackEnabled() && [self hasMagicStackLoaded]) {
-    [self
-        logTopModuleImpressionForType:ContentSuggestionsModuleType::kShortcuts];
+
+  self.shortcutsStackView = [self createShortcutsStackView];
+  if (IsMagicStackEnabled()) {
+    self.shortcutsModuleContainer = [[MagicStackModuleContainer alloc]
+        initWithContentView:self.shortcutsStackView
+                       type:ContentSuggestionsModuleType::kShortcuts
+                   delegate:self];
+    if ([self hasMagicStackLoaded]) {
+      [self insertModuleIntoMagicStack:self.shortcutsModuleContainer];
+      [self logTopModuleImpressionForType:ContentSuggestionsModuleType::
+                                              kShortcuts];
+    }
   }
 }
 
@@ -1267,10 +1275,6 @@ const base::TimeDelta kSetUpListHideAnimationDuration = base::Milliseconds(250);
         break;
       }
       case ContentSuggestionsModuleType::kShortcuts: {
-        self.shortcutsModuleContainer = [[MagicStackModuleContainer alloc]
-            initWithContentView:self.shortcutsStackView
-                           type:type
-                       delegate:self];
         moduleContainer = self.shortcutsModuleContainer;
         break;
       }
