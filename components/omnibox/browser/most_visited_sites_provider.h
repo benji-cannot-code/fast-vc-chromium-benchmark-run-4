47c9829f8739822acd23019647e240b39c115ecb
Tomasz Wiszkowski <ender@google.com>
2023-10-06 23:15:41 +0000

    Clean up Most Visited Tiles on Search Results Page
    Feature is evaluated and is not launching on any platforms.
    
    Change-Id: I7f467230af565ebb1ebb48e1ef657de71dad3f4f
    Fixed: 1490101
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4919775
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206689}

--- components/omnibox/browser/most_visited_sites_provider.h
+++ components/omnibox/browser/most_visited_sites_provider.h
@@ -32,8 +32,7 @@ class MostVisitedSitesProvider : public AutocompleteProvider {
  private:
   FRIEND_TEST_ALL_PREFIXES(MostVisitedSitesProviderTest,
                            AllowMostVisitedSitesSuggestions);
-  FRIEND_TEST_ALL_PREFIXES(MostVisitedSitesProviderTest,
-                           SrpCoverageIsControlledWithFeatureFlag);
+  FRIEND_TEST_ALL_PREFIXES(MostVisitedSitesProviderTest, NoSRPCoverage);
 
   ~MostVisitedSitesProvider() override;
 
