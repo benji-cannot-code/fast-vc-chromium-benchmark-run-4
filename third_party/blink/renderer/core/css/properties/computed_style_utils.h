8ee1aebf724ef75db10852bee59b2d15a261828c
Kurt Catti-Schmidt <kschmi@microsoft.com>
2023-10-06 23:35:36 +0000

    [GridNG] Report used values for `grid-template` ComputedStyle
    Prior to this change, `grid-template` was using a shared method with
    other grid shorthands for computed style values. This change splits
    this into its own method for specific behavior.
    
    The behavior implemented here is in response to this block from
    the spec:
    
    "Note: In general, resolved values are the computed values, except for
    a small list of legacy 2.1 properties. However, compatibility with early
    implementations of this module requires us to define grid-template-rows
    and grid-template-columns as returning used values."
    https://www.w3.org/TR/css-grid-2/#resolved-track-list-standalone
    
    This specifically calls out `grid-template-rows` and
    `grid-template-columns` as returning used values, which we were
    previously *also* doing for `grid-template` via the shared method.
    Since `grid-template` isn't listed here, we shouldn't actually be
    doing this, and should be returning computed values as the test is
    verifying. Firefox has already implemented this behavior.
    
    The actual change here is pretty straightforward. The basic algorithm
    is shared with `StylePropertySerializer::GetShorthandValueForGrid`,
    and computed values are queried from `ValueForGridTrackList`, which
    I did some slight renames to differentiate between computed and
    used values. All of the shared logic is in CSSOMUtils.
    
    After this, I'll push a small update to the last failing test, which
    is slightly invalid. It should be placing the grid areas in the row
    side, not on columns, as that is invalid and will never round-trip.
    
    Bug: 511177
    Change-Id: I8fbe79f64168a79752c55c8904a2aa9d120cc9b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4910222
    Commit-Queue: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1206693}

--- third_party/blink/renderer/core/css/properties/computed_style_utils.h
+++ third_party/blink/renderer/core/css/properties/computed_style_utils.h
@@ -144,7 +144,8 @@ class CORE_EXPORT ComputedStyleUtils {
                                              const ComputedStyle&);
   static CSSValue* ValueForGridTrackList(GridTrackSizingDirection,
                                          const LayoutObject*,
-                                         const ComputedStyle&);
+                                         const ComputedStyle&,
+                                         bool force_computed_value = false);
   static CSSValue* ValueForGridPosition(const GridPosition&);
   static gfx::SizeF UsedBoxSize(const LayoutObject&);
   static CSSValue* RenderTextDecorationFlagsToCSSValue(TextDecorationLine);
@@ -295,6 +296,11 @@ class CORE_EXPORT ComputedStyleUtils {
                                                   const ComputedStyle&,
                                                   const LayoutObject*,
                                                   bool allow_visited_style);
+  static CSSValueList* ValuesForGridTemplateShorthand(
+      const StylePropertyShorthand&,
+      const ComputedStyle&,
+      const LayoutObject*,
+      bool allow_visited_style);
   static CSSValueList* ValuesForSidesShorthand(const StylePropertyShorthand&,
                                                const ComputedStyle&,
                                                const LayoutObject*,
