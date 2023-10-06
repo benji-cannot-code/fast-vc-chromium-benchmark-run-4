6bada59df8cedf44b0d04ca549367874eb1ed7fc
Moteva <moteva@google.com>
2023-10-06 23:36:32 +0000

    CrOS Settings: Move app notifications into a new subpage
    Move the apps from Settings>Notifications to
    Settings>Notifications>Manage app notifications as part of OS Settings
    Revamp.
    
    Design Doc: go/cros-settings-revamp-string-updates
    Notifications: https://screenshot.googleplex.com/AqmykXMDZcqpE4e
    Manage app notifications: https://screenshot.googleplex.com/5gLkLwH5wHb6B8S
    
    BUG=b/297955380
    TEST=
    out/Default/browser_tests  --gtest_filter="*OsAppsPage*"
    
    Change-Id: I05c07a7d60ce630cd29cae2c37895334879d08c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4912958
    Commit-Queue: Nikki Moteva <moteva@google.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1206694}

--- chrome/browser/resources/ash/settings/os_settings_routes.ts
+++ chrome/browser/resources/ash/settings/os_settings_routes.ts
@@ -122,6 +122,7 @@ export interface OsSettingsRoutes extends MinimumRoutes {
   ADVANCED: Route;
   APN: Route;
   APP_NOTIFICATIONS: Route;
+  APP_NOTIFICATIONS_MANAGER: Route;
   APP_MANAGEMENT: Route;
   APP_MANAGEMENT_DETAIL: Route;
   APP_MANAGEMENT_PLUGIN_VM_SHARED_PATHS: Route;
@@ -392,6 +393,11 @@ export function createRoutes(): OsSettingsRoutes {
   r.APP_NOTIFICATIONS = createSubpage(
       r.APPS, routesMojom.APP_NOTIFICATIONS_SUBPAGE_PATH,
       Subpage.kAppNotifications);
+  if (isRevampWayfindingEnabled()) {
+    r.APP_NOTIFICATIONS_MANAGER = createSubpage(
+        r.APP_NOTIFICATIONS, routesMojom.APP_NOTIFICATIONS_MANAGER_SUBPAGE_PATH,
+        Subpage.kAppNotificationsManager);
+  }
   r.APP_MANAGEMENT = createSubpage(
       r.APPS, routesMojom.APP_MANAGEMENT_SUBPAGE_PATH, Subpage.kAppManagement);
   r.APP_MANAGEMENT_DETAIL = createSubpage(
