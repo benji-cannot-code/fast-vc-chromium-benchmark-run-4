6c9ac6d1a0063ac2691b68425521903e1baa4ec9
Rafał Godlewski <rgod@google.com>
2023-10-06 15:23:01 +0000

    [iOS] Add fetching sender image in sharing status view
    Bug: 1463882
    Change-Id: I3d37d2d674d1f28bfdea2d12321cf10a1854e8f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4915936
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1206426}

---
+++ ios/chrome/browser/ui/settings/password/password_sharing/sharing_status_mediator.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_SETTINGS_PASSWORD_PASSWORD_SHARING_SHARING_STATUS_MEDIATOR_H_
+#define IOS_CHROME_BROWSER_UI_SETTINGS_PASSWORD_PASSWORD_SHARING_SHARING_STATUS_MEDIATOR_H_
+
+#import <Foundation/Foundation.h>
+
+@protocol SharingStatusConsumer;
+
+class AuthenticationService;
+class ChromeAccountManagerService;
+
+// This mediator passes display information about sender and recipients of the
+// user to its consumer.
+@interface SharingStatusMediator : NSObject
+
+- (instancetype)initWithAuthService:(AuthenticationService*)authService
+              accountManagerService:
+                  (ChromeAccountManagerService*)accountManagerService
+    NS_DESIGNATED_INITIALIZER;
+
+- (instancetype)init NS_UNAVAILABLE;
+
+// Consumer of this mediator.
+@property(nonatomic, weak) id<SharingStatusConsumer> consumer;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_SETTINGS_PASSWORD_PASSWORD_SHARING_SHARING_STATUS_MEDIATOR_H_
--- ios/chrome/browser/ui/settings/password/password_sharing/sharing_status_view_controller.h
+++ ios/chrome/browser/ui/settings/password/password_sharing/sharing_status_view_controller.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_UI_SETTINGS_PASSWORD_PASSWORD_SHARING_SHARING_STATUS_VIEW_CONTROLLER_H_
 
 #import "ios/chrome/browser/shared/ui/table_view/chrome_table_view_controller.h"
+#import "ios/chrome/browser/ui/settings/password/password_sharing/sharing_status_consumer.h"
 
 @protocol SharingStatusViewControllerPresentationDelegate;
 
@@ -19,7 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // If the user clicks "cancel" button at any point, the animation stops and
 // cancelled status is presented. Otherwise, success status is displayed when
 // the animation finishes.
-@interface SharingStatusViewController : ChromeTableViewController
+@interface SharingStatusViewController
+    : ChromeTableViewController <SharingStatusConsumer>
 
 // Delegate for handling dismissal of the view.
 @property(nonatomic, weak) id<SharingStatusViewControllerPresentationDelegate>
