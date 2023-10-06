760ffa8811d426fa15539f69a81ecbdd86544a3b
Jason Lin <lxj@google.com>
2023-10-06 19:24:16 +0000

    Revert "Display per-GPUDevice information on about:gpu"
    This reverts commit 57a54836714d9d440b02392fff66e9818ac54290.
    
    Reason for revert: https://crbug.com/1489043#c5
    
    Original change's description:
    > Display per-GPUDevice information on about:gpu
    >
    > The GPUInfo and GPUDevice structs have been modified to contain
    > information for each GPUDevice available. This CL surfaces this
    > information.
    >
    > Bug: chromium:1418417
    > Change-Id: I4eb5f5398a3903112d3c4bf835b799dd4bbe3978
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4898884
    > Commit-Queue: Jonah Ryan-Davis <jonahr@google.com>
    > Reviewed-by: Kenneth Russell <kbr@chromium.org>
    > Reviewed-by: ccameron chromium <ccameron@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1203356}
    
    Bug: chromium:1418417
    Change-Id: I0c8ecd261962fe9b3870b88abddf515581f7518f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4917480
    Owners-Override: Jason Lin <lxj@google.com>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Auto-Submit: Jason Lin <lxj@google.com>
    Reviewed-by: Jason Lin <lxj@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206558}

--- content/browser/resources/gpu/browser_bridge.ts
+++ content/browser/resources/gpu/browser_bridge.ts
@@ -44,7 +44,6 @@ interface GpuInfo {
   compositorInfo: any[];
   dawnInfo?: string[];
   devicePerfInfo: any[];
-  devices: any[];
   diagnostics?: any[];
   displayInfo: any[];
   featureStatus: FeatureStatus;
