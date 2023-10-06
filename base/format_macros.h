efd106c91648269b7d0e4e2eff20dedac0b77a30
Peter Kasting <pkasting@chromium.org>
2023-10-06 18:41:45 +0000

    Simplify format_macros.h.
    We don't use WidePRI* anymore; remove. Also, Microsoft has supported the
    C99 %zu for size_t for the last 8 years or so, so that divergence can be
    removed also.
    
    Bug: 1371963
    Change-Id: I717890b15537df0b79e7b9506000e136c242bfb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4919592
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206537}

--- base/format_macros.h
+++ base/format_macros.h
@@ -40,33 +40,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <inttypes.h>
 
-#if BUILDFLAG(IS_WIN)
-
-#if !defined(PRId64) || !defined(PRIu64) || !defined(PRIx64)
-#error "inttypes.h provided by win toolchain should define these."
-#endif
-
-#define WidePRId64 L"I64d"
-#define WidePRIu64 L"I64u"
-#define WidePRIx64 L"I64x"
-
-#if !defined(PRIuS)
-#define PRIuS "Iu"
-#endif
-
-#elif BUILDFLAG(IS_POSIX) || BUILDFLAG(IS_FUCHSIA)
-
-// GCC will concatenate wide and narrow strings correctly, so nothing needs to
-// be done here.
-#define WidePRId64 PRId64
-#define WidePRIu64 PRIu64
-#define WidePRIx64 PRIx64
-
 #if !defined(PRIuS)
 #define PRIuS "zu"
 #endif
-
-#endif  // BUILDFLAG(IS_WIN)
 
 // The size of NSInteger and NSUInteger varies between 32-bit and 64-bit
 // architectures and Apple does not provides standard format macros and
