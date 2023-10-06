22a110420cef98d7257e192216f902a75bcbb250
Yue She <yueshe@google.com>
2023-10-06 20:21:45 +0000

    roll iOS FYI bots to Xcode15.1 B1; Resolve an edge case when simctl outputs iOS version with patch number
    Bug: 1489496
    Change-Id: Id57658e24d29f7de120f1bf509d31c0260f60811
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4913814
    Commit-Queue: Yue She <yueshe@google.com>
    Reviewed-by: Zhaoyang Li <zhaoyangli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206598}

--- ios/build/bots/scripts/constants.py
+++ ios/build/bots/scripts/constants.py
@@ -6,3 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 # Default Timeout (seconds) to kill a test process when it doesn't have output.
 READLINE_TIMEOUT = 180
+
+# Maximum number of days that we should keep stale iOS simulator runtimes
+MAX_RUNTIME_KEPT_DAYS = '3'
