52e692fa6d75feaf9fa37f7b821ecd2fc46a2563
Stefan Zager <szager@chromium.org>
2023-10-06 22:39:41 +0000

    Block main thread during commit after input
    Bug: chromium:1255972
    Change-Id: I2706412d465e3524512991ea0b4b25fe834114ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4546241
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206681}

--- cc/trees/proxy_main.cc
+++ cc/trees/proxy_main.cc
@@ -245,6 +245,10 @@ void ProxyMain::BeginMainFrame(
     commit_timeout = true;
   }
 
+  bool blocking = !base::FeatureList::IsEnabled(features::kNonBlockingCommit) ||
+                  block_on_next_commit_;
+  block_on_next_commit_ = false;
+
   bool scroll_and_viewport_changes_synced = false;
   if (!IsDeferringCommits()) {
     // Synchronizes scroll offsets and page scale deltas (for pinch zoom) from
@@ -434,7 +438,6 @@ void ProxyMain::BeginMainFrame(
   // point of view, but asynchronously performed on the impl thread,
   // coordinated by the Scheduler.
   CommitTimestamps commit_timestamps;
-  bool blocking = !base::FeatureList::IsEnabled(features::kNonBlockingCommit);
   {
     TRACE_EVENT_WITH_FLOW0("viz,benchmark",
                            "MainFrame.NotifyReadyToCommitOnMain",
@@ -648,6 +651,14 @@ void ProxyMain::SetPauseRendering(bool pause_rendering) {
       FROM_HERE,
       base::BindOnce(&ProxyImpl::SetPauseRendering,
                      base::Unretained(proxy_impl_.get()), pause_rendering_));
+}
+
+void ProxyMain::SetInputResponsePending() {
+  // If the next main frame will contain the visual response to an input event,
+  // we pause execution on the main thread until the compositor thread finishes
+  // processing the commit. This is done to minimize thread contention while
+  // the compositor is doing critical-path work.
+  block_on_next_commit_ = true;
 }
 
 bool ProxyMain::StartDeferringCommits(base::TimeDelta timeout,
