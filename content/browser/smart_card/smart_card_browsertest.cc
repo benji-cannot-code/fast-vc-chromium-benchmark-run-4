975c46cce360496dd088482696176926bcd8a7e5
Daniel d'Andrada <dandrader@google.com>
2023-10-06 09:07:30 +0000

    SmartCard: "reset" as the default disposition when ending transaction
    Makes the implementation match the spec change in:
    https://github.com/WICG/web-smart-card/pull/32
    
    Bug: 1386175
    Change-Id: Ibecbbcf9edb48e972e8d9495d641281814c234fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4898685
    Commit-Queue: Daniel d'Andrada <dandrader@google.com>
    Reviewed-by: Christian Flach <cmfcmf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206299}

--- content/browser/smart_card/smart_card_browsertest.cc
+++ content/browser/smart_card/smart_card_browsertest.cc
@@ -196,7 +196,7 @@ class SmartCardTest : public ContentBrowserTest {
 
       mock_context_factory.ExpectConnectFakeReaderSharedT1(connection_receiver);
       mock_connection.ExpectBeginTransaction(transaction_receiver);
-      mock_transaction.ExpectEndTransaction(SmartCardDisposition::kLeave);
+      mock_transaction.ExpectEndTransaction(SmartCardDisposition::kReset);
     }
 
     std::string js_snippet = std::format(R"(
@@ -1511,7 +1511,7 @@ IN_PROC_BROWSER_TEST_F(SmartCardTest, EndTransactionAfterFailedOperation) {
           std::move(callback).Run(std::move(result));
         });
 
-    mock_transaction.ExpectEndTransaction(SmartCardDisposition::kLeave);
+    mock_transaction.ExpectEndTransaction(SmartCardDisposition::kReset);
   }
 
   EXPECT_EQ(
@@ -1571,7 +1571,7 @@ IN_PROC_BROWSER_TEST_F(SmartCardTest, EndTransactionAfterContextOperation) {
           std::move(callback).Run(std::move(result));
         });
 
-    mock_transaction.ExpectEndTransaction(SmartCardDisposition::kLeave);
+    mock_transaction.ExpectEndTransaction(SmartCardDisposition::kEject);
   }
 
   EXPECT_EQ(
@@ -1587,6 +1587,7 @@ IN_PROC_BROWSER_TEST_F(SmartCardTest, EndTransactionAfterContextOperation) {
 
       let transaction = () => {
         context.listReaders();
+        return "eject";
       };
 
       try {
