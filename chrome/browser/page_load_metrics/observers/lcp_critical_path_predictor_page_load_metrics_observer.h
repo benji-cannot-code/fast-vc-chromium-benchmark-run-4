2df8ab9506233c5051e6e80336608b877527d879
Yoichi Osato <yoichio@chromium.org>
2023-10-06 06:09:28 +0000

    Reland "[LCPP] Record NavigationToLargestContentfulPaint UMA when prerendered"
    This CL relands commit d4e4ecbff0ee64b3b6eb0f6311b4448c087d3596
    reverted by commit f670ee27cebb65a61d7188f9f7d57efda1894f2b.
    This CL fixes the flakiness issue by waiting OnPredictorInitialized.
    (see diff with Patchset 1)
    
    Original change's description:
    > [LCPP] Record NavigationToLargestContentfulPaint UMA when prerendered
    >
    > This CL lets LcpCriticalPathPredictorPageLoadMetricsObserver
    > - not learn LCPP when the page is prerendered to avoid data skew.
    > - report the UMA when the page is prerendered and activated.
    >
    > Bug: 1468188
    > Change-Id: I99b864b941dc7281cde471bd4bf5239a80391c55
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4891302
    > Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    > Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    > Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    > Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1205610}
    
    Test: ./unit_tests --gtest_filter="LcpC*" --gtest_repeat=100
    Bug: 1468188
    Change-Id: I53b9d2eb5dc6717f05dc17947d445dca0759998f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4914756
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206244}

--- chrome/browser/page_load_metrics/observers/lcp_critical_path_predictor_page_load_metrics_observer.h
+++ chrome/browser/page_load_metrics/observers/lcp_critical_path_predictor_page_load_metrics_observer.h
@@ -82,6 +82,9 @@ class LcpCriticalPathPredictorPageLoadMetricsObserver
   void OnFirstContentfulPaintInPage(
       const page_load_metrics::mojom::PageLoadTiming& timing) override;
 
+  // True if the page is prerendered.
+  bool is_prerender_ = false;
+
   // The URL of the last navigation commit.
   absl::optional<GURL> commit_url_;
 
