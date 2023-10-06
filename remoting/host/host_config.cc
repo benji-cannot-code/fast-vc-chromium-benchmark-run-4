3ff6a5c39c9ec8aaaeee9037b36325b96f5355ec
Joe Downing <joedow@google.com>
2023-10-06 22:16:30 +0000

    Removing unused host config keys
    This CL removes a few config key definitions which are no longer used.
    
    Change-Id: Ibec3fb056b6357d14e05fe539cc1166cf70fcd52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4919634
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Auto-Submit: Joe Downing <joedow@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206672}

--- remoting/host/host_config.cc
+++ remoting/host/host_config.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace remoting {
 
-const char kHostEnabledConfigPath[] = "enabled";
 const char kHostOwnerConfigPath[] = "host_owner";
 const char kHostOwnerEmailConfigPath[] = "host_owner_email";
 const char kXmppLoginConfigPath[] = "xmpp_login";
@@ -25,9 +24,6 @@ const char kHostNameConfigPath[] = "host_name";
 const char kHostSecretHashConfigPath[] = "host_secret_hash";
 const char kPrivateKeyConfigPath[] = "private_key";
 const char kUsageStatsConsentConfigPath[] = "usage_stats_consent";
-const char kEnableVp9ConfigPath[] = "enable_vp9";
-const char kEnableH264ConfigPath[] = "enable_h264";
-const char kFrameRecorderBufferKbConfigPath[] = "frame-recorder-buffer-kb";
 
 absl::optional<base::Value::Dict> HostConfigFromJson(const std::string& json) {
   absl::optional<base::Value> value =
