fa018349d19b0657ee135e59639fed6b62f97a74
Mason Freed <masonf@chromium.org>
2023-10-06 23:46:09 +0000

    Clean up PartRoot.replaceChildren() just a bit
    This removes some extra checks and a fast path that wasn't
    very fast. No functional change.
    
    Bug: 1453291
    Change-Id: I20f381caaf8cefeffe3fb828fe0f447f9d345567
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4918453
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206696}

--- third_party/blink/renderer/core/dom/child_node_part.cc
+++ third_party/blink/renderer/core/dom/child_node_part.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document_part_root.h"
 #include "third_party/blink/renderer/core/dom/node_cloning_data.h"
 #include "third_party/blink/renderer/core/dom/node_move_scope.h"
+#include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 
 namespace blink {
@@ -140,20 +141,16 @@ void ChildNodePart::replaceChildren(
     return;
   }
   ContainerNode* parent = parentNode();
-  if (!parent) {
-    return;
-  }
-  if (previous_sibling_ == parent->firstChild() &&
-      next_sibling_ == parent->lastChild()) {
-    parent->replaceChildren(nodes, exception_state);
-    return;
-  }
+  DCHECK(parent) << "Should be guaranteed by IsValid";
   // Remove existing children, leaving endpoints.
   Node* node = previous_sibling_->nextSibling();
   while (node != next_sibling_) {
-    Node* remove = node;
+    Node* to_remove = node;
     node = node->nextSibling();
-    remove->remove();
+    parent->RemoveChild(to_remove, exception_state);
+    if (exception_state.HadException()) {
+      return;
+    }
   }
   // Insert new contents.
   Node* nodes_as_node = Node::ConvertNodeUnionsIntoNode(
