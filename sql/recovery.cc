9b920610571f81ce448fe16db04f8fd06b1c9637
Austin Sullivan <asully@chromium.org>
2023-10-07 00:55:09 +0000

    SQL: Add optional per-DB logging for BuiltInRecovery
    Currently, we only have logging which provides the sum of _all_
    attempts to recovery using BuiltInRecovery
    
    Bug: 1385500
    Change-Id: I1894ef85b5c291f9b80e2573d4d14698c230cae5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4917012
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206716}

--- sql/recovery.cc
+++ sql/recovery.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
+#include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/types/pass_key.h"
@@ -67,13 +68,16 @@ bool BuiltInRecovery::ShouldAttemptRecovery(Database* database,
 }
 
 // static
-SqliteResultCode BuiltInRecovery::RecoverDatabase(Database* database,
-                                                  Strategy strategy) {
+SqliteResultCode BuiltInRecovery::RecoverDatabase(
+    Database* database,
+    Strategy strategy,
+    std::string database_uma_name) {
   if (!BuiltInRecovery::IsSupported()) {
     return SqliteResultCode::kAbort;
   }
 
-  auto recovery = BuiltInRecovery(database, strategy);
+  auto recovery =
+      BuiltInRecovery(database, strategy, std::move(database_uma_name));
   return recovery.RecoverAndReplaceDatabase();
 }
 
@@ -82,7 +86,8 @@ bool BuiltInRecovery::RecoverIfPossible(
     Database* database,
     int extended_error,
     Strategy strategy,
-    const base::Feature* const use_builtin_recovery_if_supported_flag) {
+    const base::Feature* const use_builtin_recovery_if_supported_flag,
+    std::string database_uma_name) {
   // If `BuiltInRecovery` is supported at all, check the flag for this specific
   // database, provided by the feature team.
   bool use_builtin_recovery =
@@ -105,7 +110,8 @@ bool BuiltInRecovery::RecoverIfPossible(
 
   if (use_builtin_recovery) {
     CHECK(BuiltInRecovery::IsSupported());
-    auto result = BuiltInRecovery::RecoverDatabase(database, strategy);
+    auto result =
+        BuiltInRecovery::RecoverDatabase(database, strategy, database_uma_name);
     if (!IsSqliteSuccessCode(result)) {
       DLOG(ERROR) << "Database recovery failed with result code: " << result;
     }
@@ -125,8 +131,11 @@ bool BuiltInRecovery::RecoverIfPossible(
   return true;
 }
 
-BuiltInRecovery::BuiltInRecovery(Database* database, Strategy strategy)
+BuiltInRecovery::BuiltInRecovery(Database* database,
+                                 Strategy strategy,
+                                 std::string database_uma_name)
     : strategy_(strategy),
+      database_uma_name_(std::move(database_uma_name)),
       db_(database),
       recover_db_(sql::DatabaseOptions{
           .exclusive_locking = false,
@@ -161,6 +170,14 @@ BuiltInRecovery::~BuiltInRecovery() {
   base::UmaHistogramEnumeration("Sql.Recovery.Result", result_);
   UmaHistogramSqliteResult("Sql.Recovery.ResultCode",
                            static_cast<int>(sqlite_result_code_));
+
+  if (!database_uma_name_.empty()) {
+    base::UmaHistogramEnumeration(
+        base::StrCat({"Sql.Recovery.Result.", database_uma_name_}), result_);
+    UmaHistogramSqliteResult(
+        base::StrCat({"Sql.Recovery.ResultCode.", database_uma_name_}),
+        static_cast<int>(sqlite_result_code_));
+  }
 
   if (db_) {
     if (result_ == Result::kSuccess) {
