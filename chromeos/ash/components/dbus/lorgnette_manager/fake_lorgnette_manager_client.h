19e71b46f22d782fa3bf3e7969621442e1f29631
Nathan Muggli <nmuggli@google.com>
2023-10-07 14:35:58 +0000

    scanning: Add ReadScanData wrapper
    Add a method to LorgnetteManagerClient to expose the ReadScanData
    lorgnette d-bus function.
    
    Add a method to LorgnetteScannerManager to expose this new
    LorgnetteManagerClient method.
    
    All of these methods use the lorgnette protobuf API.
    
    Bug: b:297435716
    Change-Id: If0888afa425f17ebcef82a5b1159a5f23a0bc921
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4917778
    Reviewed-by: Benjamin Gordon <bmgordon@chromium.org>
    Commit-Queue: Nathan Muggli <nmuggli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1206800}

--- chromeos/ash/components/dbus/lorgnette_manager/fake_lorgnette_manager_client.h
+++ chromeos/ash/components/dbus/lorgnette_manager/fake_lorgnette_manager_client.h
@@ -53,6 +53,10 @@ class COMPONENT_EXPORT(LORGNETTE_MANAGER) FakeLorgnetteManagerClient
       base::RepeatingCallback<void(std::string, uint32_t)> page_callback,
       base::RepeatingCallback<void(uint32_t, uint32_t)> progress_callback)
       override;
+  void ReadScanData(
+      const lorgnette::ReadScanDataRequest& request,
+      chromeos::DBusMethodCallback<lorgnette::ReadScanDataResponse> callback)
+      override;
   void CancelScan(
       chromeos::VoidDBusMethodCallback completion_callback) override;
   void StartScannerDiscovery(
@@ -92,6 +96,10 @@ class COMPONENT_EXPORT(LORGNETTE_MANAGER) FakeLorgnetteManagerClient
   void SetScanResponse(
       const absl::optional<std::vector<std::string>>& scan_response);
 
+  // Sets the response returned by ReadScanData().
+  void SetReadScanDataResponse(
+      const absl::optional<lorgnette::ReadScanDataResponse>& response);
+
  private:
   absl::optional<lorgnette::ListScannersResponse> list_scanners_response_;
   absl::optional<lorgnette::ScannerCapabilities> capabilities_response_;
@@ -99,6 +107,7 @@ class COMPONENT_EXPORT(LORGNETTE_MANAGER) FakeLorgnetteManagerClient
   absl::optional<lorgnette::CloseScannerResponse> close_scanner_response_;
   absl::optional<lorgnette::StartPreparedScanResponse>
       start_prepared_scan_response_;
+  absl::optional<lorgnette::ReadScanDataResponse> read_scan_data_response_;
   absl::optional<std::vector<std::string>> scan_response_;
 };
 
