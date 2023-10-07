1c2cc8add7b0111b7d3936389c980704e4c33c29
Ruma Kesh <rkesh@google.com>
2023-10-07 04:26:56 +0000

    CrOS Settings: Convert internet subpage browser test to TS
    Additionally, moved the test under the `internet_page/` folder to
    mirror the app folder structure.
    
    Bug: b/270728282
    Test: browser_tests --gtest_filter="*InternetPageInternetSubpage*"
    Change-Id: Ic9233ab1f05d0f5d9388dddd367ecf9d2716592c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4913554
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Ruma Kesh <rkesh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1206752}

--- ash/webui/common/resources/network/network_list.d.ts
+++ ash/webui/common/resources/network/network_list.d.ts
@@ -3,4 +3,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-export {};
+import {LegacyElementMixin} from 'chrome://resources/polymer/v3_0/polymer/lib/legacy/legacy-element-mixin.js';
+
+import {OncMojo} from './onc_mojo.js';
+
+interface NetworkListElement extends LegacyElementMixin, HTMLElement {
+  networks: OncMojo.NetworkStateProperties[];
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'network-list': NetworkListElement;
+  }
+}
+
+export {NetworkListElement};
