84eabd94e3d36eb95b4b9dfa9ec70a2b50bff670
Kevin Grosu <kgrosu@google.com>
2023-10-09 16:49:23 +0000

    [Gardener] Disable ExpandablePaymentHandlerTest#testWebContentsInputProtection
    Bug: 1491094
    Change-Id: I5360242e090632e5877aef44f93f8f029bd924c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4922771
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Kevin Grosu <kgrosu@google.com>
    Owners-Override: Kevin Grosu <kgrosu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1207114}

--- chrome/android/javatests/src/org/chromium/chrome/browser/payments/ExpandablePaymentHandlerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/payments/ExpandablePaymentHandlerTest.java
@@ -377,6 +377,7 @@ public class ExpandablePaymentHandlerTest {
     @Test
     @SmallTest
     @Feature({"Payments"})
+    @DisabledTest(message = "https://crbug.com/1491094")
     public void testWebContentsInputProtection() throws Throwable {
         startDefaultServer();
         PaymentHandlerCoordinator paymentHandler = createPaymentHandlerAndShow(mDefaultIsIncognito);
