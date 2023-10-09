8a30accd5c990e80d43ed6880eed3a0cb19edb37
Oleksandr Peletskyi <peletskyi@google.com>
2023-10-09 17:48:19 +0000

    iwa: Make IsolatedWebAppResponseReader closeable
    Added Close() member function that closes the IsolatedWebAppResponseReader.
    In addition let the command helper to close the reader after the file
    verification. This will allow the commands to delete the files without
    any worries that the file is still open.
    
    For testing purpose:
    - Extracted duplication code of the FakeResponseReaderFactory
    - Made IsolatedWebAppResponseReader a pure virtual class with separate
      implementation and mock.
    
    Bug: b/287594073
    Test: added unittests
    Change-Id: Ibb2bbdb0ba69e5a9754d5a80b1b4bf4d5341b9de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4911836
    Reviewed-by: Christian Flach <cmfcmf@chromium.org>
    Commit-Queue: Oleksandr Peletskyi <peletskyi@google.com>
    Auto-Submit: Oleksandr Peletskyi <peletskyi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1207151}

--- chrome/browser/web_applications/isolated_web_apps/isolated_web_app_response_reader_factory.cc
+++ chrome/browser/web_applications/isolated_web_apps/isolated_web_app_response_reader_factory.cc
@@ -158,7 +158,7 @@ void IsolatedWebAppResponseReaderFactory::OnIntegrityBlockAndMetadataRead(
   }
 
   std::move(callback).Run(
-      std::make_unique<IsolatedWebAppResponseReader>(std::move(reader)));
+      std::make_unique<IsolatedWebAppResponseReaderImpl>(std::move(reader)));
 }
 
 }  // namespace web_app
