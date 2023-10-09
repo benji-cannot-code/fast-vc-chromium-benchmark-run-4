50db2ac01b74f01b044c5f57f7a434513f04adae
Fahad Mansoor <fahadmansoor@google.com>
2023-10-09 12:14:12 +0000

    Cellular: Changes to prepare suppress text messages enabled by default
    As we plan to launch the client side in M120, the suppress text
    messages feature is going to be enabled. While testing locally with
    the flag enabled by default we found some test failures. This
    CL fixes those failures.
    
    1. Having a raw_ptr to NetworkMetadataStore causes dangling ptrs in
    some tests, the fix for this is to just make the raw_ptr a weak_ptr
    following dangling ptr check document.
    
    2. Error logs on startup due to GUID resolution, this was fixed
    by moving the GUID computation on message receipt rather than on
    creating the device. This also helps us in cases when the GUID
    actually changes in session (as that can happen after connecting to
    an eSIM for the first time in the session)
    
    Test: chromeos_unittest --gtest_filter=*TextMessageProvider*
    chromeos_unittest --gtest_filter=*NetworkSmsHandler*
    
    Bug: b:298159147
    Change-Id: Ie0c3e2294f61ce72515636c85eb51f095799e4de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4887237
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Fahad Mansoor <fahadmansoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1207036}

--- chromeos/ash/components/network/text_message_provider.cc
+++ chromeos/ash/components/network/text_message_provider.cc
@@ -87,8 +87,9 @@ bool TextMessageProvider::IsAllowTextMessagesPolicySet() {
 }
 
 bool TextMessageProvider::IsMessageSuppressedByUser(const std::string& guid) {
-  return network_metadata_store_->GetUserTextMessageSuppressionState(guid) ==
-         UserTextMessageSuppressionState::kSuppress;
+  return network_metadata_store_ &&
+         network_metadata_store_->GetUserTextMessageSuppressionState(guid) ==
+             UserTextMessageSuppressionState::kSuppress;
 }
 
 bool TextMessageProvider::ShouldAllowTextMessages(const std::string& guid) {
@@ -109,7 +110,6 @@ void TextMessageProvider::RemoveObserver(Observer* observer) {
 
 void TextMessageProvider::SetNetworkMetadataStore(
     NetworkMetadataStore* network_metadata_store) {
-  CHECK(network_metadata_store);
   network_metadata_store_ = network_metadata_store;
 }
 
