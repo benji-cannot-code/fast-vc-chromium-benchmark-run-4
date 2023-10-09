ad8cc5ed0d2b0f40289497c3de5967ca574f78fe
Anders Hartvoll Ruud <andruud@chromium.org>
2023-10-09 20:17:13 +0000

    [css-nesting-ident] Ignore positioned braces for universal syntax
    This was changed by accident in a previous CL.
    
    Also rename ParseVariableReferenceValue to ParseUniversalSyntaxValue,
    which better describes what the function is used for.
    
    Bug: 1427259
    Change-Id: I6736c6555494f326141576710d3236c0a234968c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4919180
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207248}

--- third_party/blink/renderer/core/css/parser/css_variable_parser_test.cc
+++ third_party/blink/renderer/core/css/parser/css_variable_parser_test.cc
@@ -79,7 +79,7 @@ TEST_P(ValidVariableReferenceTest, ContainsValidVariableReferences) {
   EXPECT_TRUE(CSSVariableParser::ContainsValidVariableReferences(range));
 }
 
-TEST_P(ValidVariableReferenceTest, ParseVariableReferenceValue) {
+TEST_P(ValidVariableReferenceTest, ParseUniversalSyntaxValue) {
   SCOPED_TRACE(GetParam());
   Vector<CSSParserToken, 32> tokens = Parse(GetParam());
   CSSParserTokenRange range(tokens);
@@ -87,7 +87,7 @@ TEST_P(ValidVariableReferenceTest, ParseVariableReferenceValue) {
   auto* context = MakeGarbageCollected<CSSParserContext>(
       kHTMLStandardMode, SecureContextMode::kInsecureContext);
   EXPECT_NE(nullptr,
-            CSSVariableParser::ParseVariableReferenceValue(
+            CSSVariableParser::ParseUniversalSyntaxValue(
                 tokenized_value, *context, /* is_animation_tainted */ false));
 }
 
@@ -110,15 +110,15 @@ TEST_P(InvalidVariableReferenceTest, ContainsValidVariableReferences) {
   EXPECT_FALSE(CSSVariableParser::ContainsValidVariableReferences(range));
 }
 
-TEST_P(InvalidVariableReferenceTest, ParseVariableReferenceValue) {
+TEST_P(InvalidVariableReferenceTest, ParseUniversalSyntaxValue) {
   SCOPED_TRACE(GetParam());
   Vector<CSSParserToken, 32> tokens = Parse(GetParam());
   CSSParserTokenRange range(tokens);
   CSSTokenizedValue tokenized_value = {range, /* text */ ""};
   auto* context = MakeGarbageCollected<CSSParserContext>(
       kHTMLStandardMode, SecureContextMode::kInsecureContext);
-  EXPECT_EQ(nullptr,
-            CSSVariableParser::ParseVariableReferenceValue(
+  EXPECT_NE(nullptr,
+            CSSVariableParser::ParseUniversalSyntaxValue(
                 tokenized_value, *context, /* is_animation_tainted */ false));
 }
 
