38ddc267eb3f8d25c35ada2aaa97af9742ee9451
Dylan Cutler <dylancutler@google.com>
2023-10-09 20:05:59 +0000

    Use correct site for cookies in SharedWorker fetch context
    Bug: 723553,1147281,1488557
    Change-Id: I69b6bea96dd509cd6df2abf386c693098754bb62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4915516
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207240}

--- content/browser/worker_host/mock_shared_worker.h
+++ content/browser/worker_host/mock_shared_worker.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/content_security_policy.mojom-forward.h"
 #include "third_party/blink/public/common/messaging/message_port_channel.h"
 #include "third_party/blink/public/common/renderer_preferences/renderer_preferences.h"
+#include "third_party/blink/public/common/storage_key/storage_key.h"
 #include "third_party/blink/public/mojom/service_worker/controller_service_worker.mojom.h"
 #include "third_party/blink/public/mojom/service_worker/service_worker_container.mojom.h"
 #include "third_party/blink/public/mojom/worker/shared_worker_factory.mojom.h"
@@ -88,7 +89,7 @@ class MockSharedWorkerFactory : public blink::mojom::SharedWorkerFactory {
   void CreateSharedWorker(
       blink::mojom::SharedWorkerInfoPtr info,
       const blink::SharedWorkerToken& token,
-      const url::Origin& constructor_origin,
+      const blink::StorageKey& constructor_key,
       bool is_constructor_secure_context,
       const std::string& user_agent,
       const blink::UserAgentMetadata& ua_metadata,
