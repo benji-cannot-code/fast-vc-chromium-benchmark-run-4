81aa8898d35d46b5ce136a5c4cd8a59dd8ee6f03
Andrew Williams <awillia@chromium.org>
2023-10-09 19:40:48 +0000

    [NSP] Set require_<NAK>_ earlier in MakeURLRequestContext
    Sets request_network_anonymization_key_ before it's used in
    NetworkContext::MakeURLRequestContext.
    
    It looks like this was mistakenly changed in a CL unrelated to
    NSP:
    
    https://chromium-review.googlesource.com/c/chromium/src/+/3545978/27/services/network/network_context.cc#2554
    
    AFAICT the only effect this has is to turn on this DCHECK:
    https://source.chromium.org/chromium/chromium/src/+/main:net/url_request/url_request.cc;l=543;drc=f5bdc89c7395ed24f1b8d196a3bdd6232d5bf771
    
    Bug: None
    Change-Id: Idc30b3902184dcf9d7a7ba5b3d159201b8f2c76e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4922812
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207223}

--- services/network/network_context.cc
+++ services/network/network_context.cc
@@ -2736,6 +2736,9 @@ URLRequestContextOwner NetworkContext::MakeURLRequestContext(
   }
 #endif
 
+  require_network_anonymization_key_ =
+      params_->require_network_anonymization_key;
+
   // If `require_network_anonymization_key_` is true, but the features that can
   // trigger another URLRequest are not set to respect NetworkAnonymizationKeys,
   // the URLRequests that they create might not have a NAK, so only set the
@@ -2762,9 +2765,6 @@ URLRequestContextOwner NetworkContext::MakeURLRequestContext(
   }
   auto result =
       URLRequestContextOwner(std::move(pref_service), builder.Build());
-
-  require_network_anonymization_key_ =
-      params_->require_network_anonymization_key;
 
   // Subscribe the CertVerifier to configuration changes that are exposed via
   // the mojom::SSLConfig, but which are not part of the
