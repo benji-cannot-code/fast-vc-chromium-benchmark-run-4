74082a6a48e7a9a546ff42ee516f833a071e8477
Christopher Cameron <ccameron@chromium.org>
2023-10-09 13:53:28 +0000

    gfx::HDRMetadata: Add SDR white level
    ISO 22028-5 includes "nominal diffuse white level", or NDWL, in its
    HDR metadata, which defaults SDR white to 203 nits.
    
    Add a new gfx::HdrMetadataSdrWhiteLevel for this property (using a
    name that more clearly describes how the property will be used).
    
    Bug: 1421266, 1483235
    Change-Id: Iad438e3f2cc3a8750a730ed11203ea69fedcc368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4915700
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207055}

--- ui/gfx/mojom/hdr_metadata_mojom_traits.cc
+++ ui/gfx/mojom/hdr_metadata_mojom_traits.cc
@@ -27,6 +27,13 @@ bool StructTraits<gfx::mojom::HdrMetadataSmpteSt2086DataView,
   return true;
 }
 
+bool StructTraits<gfx::mojom::HdrMetadataNdwlDataView, gfx::HdrMetadataNdwl>::
+    Read(gfx::mojom::HdrMetadataNdwlDataView data,
+         gfx::HdrMetadataNdwl* output) {
+  output->nits = data.nits();
+  return true;
+}
+
 bool StructTraits<gfx::mojom::HdrMetadataExtendedRangeDataView,
                   gfx::HdrMetadataExtendedRange>::
     Read(gfx::mojom::HdrMetadataExtendedRangeDataView data,
@@ -43,6 +50,9 @@ bool StructTraits<gfx::mojom::HDRMetadataDataView, gfx::HDRMetadata>::Read(
     return false;
   }
   if (!data.ReadSmpteSt2086(&output->smpte_st_2086)) {
+    return false;
+  }
+  if (!data.ReadNdwl(&output->ndwl)) {
     return false;
   }
   if (!data.ReadExtendedRange(&output->extended_range)) {
