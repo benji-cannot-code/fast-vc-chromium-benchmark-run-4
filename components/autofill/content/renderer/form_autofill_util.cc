a5ffe361b558bad0e5711b95b417f60c03b6e1e7
Christoph Schwering <schwering@google.com>
2023-10-09 10:35:36 +0000

    [Autofill] Rename form_util::Find*ByUniqueRendererId()
    This CL removes the "Unique" from
    Find{Form,FormControl,ContentEditable}ByUniqueRendererId() as an
    attempt to improve readability through shorter names of the
    form_util functions.
    
    Bug: 1007974
    Change-Id: Icfc58105d41627c325b9e4a2291eef7d7a58aa70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4916006
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1207015}

--- components/autofill/content/renderer/form_autofill_util.cc
+++ components/autofill/content/renderer/form_autofill_util.cc
@@ -2778,8 +2778,8 @@ bool InferLabelForElementForTesting(const WebFormControlElement& element,
   return InferLabelForElement(element, label, label_source);
 }
 
-WebFormElement FindFormByUniqueRendererId(const WebDocument& doc,
-                                          FormRendererId form_renderer_id) {
+WebFormElement FindFormByRendererId(const WebDocument& doc,
+                                    FormRendererId form_renderer_id) {
   if (base::FeatureList::IsEnabled(
           blink::features::kAutofillUseDomNodeIdForRendererId)) {
     if (!form_renderer_id) {
@@ -2800,7 +2800,7 @@ WebFormElement FindFormByUniqueRendererId(const WebDocument& doc,
   return WebFormElement();
 }
 
-WebFormControlElement FindFormControlByUniqueRendererId(
+WebFormControlElement FindFormControlByRendererId(
     const WebDocument& doc,
     FieldRendererId queried_form_control,
     absl::optional<FormRendererId> form_to_be_searched /*= absl::nullopt*/) {
@@ -2850,7 +2850,7 @@ WebFormControlElement FindFormControlByUniqueRendererId(
   return WebFormControlElement();
 }
 
-std::vector<WebFormControlElement> FindFormControlElementsByUniqueRendererId(
+std::vector<WebFormControlElement> FindFormControlElementsByRendererId(
     const WebDocument& doc,
     const std::vector<FieldRendererId>& queried_form_controls) {
   if (base::FeatureList::IsEnabled(
@@ -2859,7 +2859,7 @@ std::vector<WebFormControlElement> FindFormControlElementsByUniqueRendererId(
     control_elements.reserve(queried_form_controls.size());
     for (FieldRendererId queried_form_control : queried_form_controls) {
       control_elements.push_back(
-          FindFormControlByUniqueRendererId(doc, queried_form_control));
+          FindFormControlByRendererId(doc, queried_form_control));
     }
     return control_elements;
   }
@@ -2882,17 +2882,16 @@ std::vector<WebFormControlElement> FindFormControlElementsByUniqueRendererId(
   return result;
 }
 
-std::vector<WebFormControlElement> FindFormControlElementsByUniqueRendererId(
+std::vector<WebFormControlElement> FindFormControlElementsByRendererId(
     const WebDocument& doc,
     FormRendererId form_renderer_id,
     const std::vector<FieldRendererId>& queried_form_controls) {
   if (base::FeatureList::IsEnabled(
           blink::features::kAutofillUseDomNodeIdForRendererId)) {
-    return FindFormControlElementsByUniqueRendererId(doc,
-                                                     queried_form_controls);
+    return FindFormControlElementsByRendererId(doc, queried_form_controls);
   }
   std::vector<WebFormControlElement> result(queried_form_controls.size());
-  WebFormElement form = FindFormByUniqueRendererId(doc, form_renderer_id);
+  WebFormElement form = FindFormByRendererId(doc, form_renderer_id);
   if (form.IsNull())
     return result;
 
@@ -2907,8 +2906,7 @@ std::vector<WebFormControlElement> FindFormControlElementsByUniqueRendererId(
   return result;
 }
 
-WebElement FindContentEditableByUniqueRendererId(
-    FieldRendererId field_renderer_id) {
+WebElement FindContentEditableByRendererId(FieldRendererId field_renderer_id) {
   if (!base::FeatureList::IsEnabled(
           blink::features::kAutofillUseDomNodeIdForRendererId)) {
     return WebElement();
