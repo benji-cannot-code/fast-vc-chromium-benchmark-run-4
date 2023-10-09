d1a7ba2c663a3fa8f42b3cdce099c388b161b3f1
Scott Yoder <scottyoder@google.com>
2023-10-09 12:49:10 +0000

    [ios] Follow up to Large Fakebox animation change
    This change has the ToolbarCoordinator check if the LargeFakebox feature
    is enabled instead of the orchestrator. This allows it to contain all
    the logic of whether the transition is coming from a large fakebox and
    to pass a boolean into the orchestrator which more closely matches its
    name: "animateFromLargeFakebox". It also ensures that this animation
    option is not enabled for incognito.
    
    Bug: 1482304
    Change-Id: Ib5c564ba9012fec9f728e8b5bf37259648e33f2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4920475
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207043}

--- ios/chrome/browser/ui/orchestrator/omnibox_focus_orchestrator.mm
+++ ios/chrome/browser/ui/orchestrator/omnibox_focus_orchestrator.mm
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/orchestrator/omnibox_focus_orchestrator.h"
 
 #import "base/check.h"
-#import "ios/chrome/browser/ntp/features.h"
 #import "ios/chrome/browser/ui/orchestrator/edit_view_animatee.h"
 #import "ios/chrome/browser/ui/orchestrator/location_bar_animatee.h"
 #import "ios/chrome/browser/ui/orchestrator/toolbar_animatee.h"
@@ -255,7 +254,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // Use UIView animateWithDuration instead of UIViewPropertyAnimator to
     // avoid UIKit bug. See https://crbug.com/856155.
     self.inProgressAnimationCount += 1;
-    if (_animateFromLargeFakebox && IsIOSLargeFakeboxEnabled()) {
+    if (_animateFromLargeFakebox) {
       [self.toolbarAnimatee setLocationBarHeightToMatchFakeOmnibox];
     }
     [UIView animateKeyframesWithDuration:kMaterialDuration1
@@ -348,7 +347,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     if (_completion) {
       _completion();
       _completion = nil;
-      if (_animateFromLargeFakebox && IsIOSLargeFakeboxEnabled()) {
+      if (_animateFromLargeFakebox) {
         // Reset the location bar height back to the default.
         [self.toolbarAnimatee setLocationBarHeightExpanded];
       }
@@ -363,7 +362,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)expansion {
   [self.toolbarAnimatee expandLocationBar];
   [self.toolbarAnimatee showCancelButton];
-  if (_animateFromLargeFakebox && IsIOSLargeFakeboxEnabled()) {
+  if (_animateFromLargeFakebox) {
     [self.toolbarAnimatee setLocationBarHeightExpanded];
   }
 }
@@ -371,7 +370,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Visually contracts the location bar for defocus.
 - (void)contraction {
   [self.toolbarAnimatee contractLocationBar];
-  if (_animateFromLargeFakebox && IsIOSLargeFakeboxEnabled()) {
+  if (_animateFromLargeFakebox) {
     [self.toolbarAnimatee setLocationBarHeightToMatchFakeOmnibox];
   }
 }
--- ios/chrome/browser/ui/toolbar/toolbar_coordinator.mm
+++ ios/chrome/browser/ui/toolbar/toolbar_coordinator.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/apple/foundation_util.h"
 #import "components/prefs/pref_service.h"
+#import "ios/chrome/browser/ntp/features.h"
 #import "ios/chrome/browser/ntp/new_tab_page_tab_helper.h"
 #import "ios/chrome/browser/ntp/new_tab_page_util.h"
 #import "ios/chrome/browser/overlays/public/overlay_presentation_context.h"
@@ -292,7 +293,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       transitionToStateOmniboxFocused:focused
                       toolbarExpanded:focused && !IsRegularXRegularSizeClass(
                                                      self.traitEnvironment)
-              animateFromLargeFakebox:[self isNTP]
+              animateFromLargeFakebox:[self animateFromLargeFakebox]
                              animated:animateTransition
                            completion:completion];
   self.locationBarFocused = focused;
@@ -576,19 +577,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                       toolbarExpanded:omniboxFocused &&
                                       !IsRegularXRegularSizeClass(
                                           self.traitEnvironment)
-              animateFromLargeFakebox:[self isNTP]
+              animateFromLargeFakebox:[self animateFromLargeFakebox]
                              animated:NO
                            completion:nil];
 }
 
-/// Returns YES if the current webstate is an NTP.
-- (BOOL)isNTP {
+/// Returns YES if the transition animation is from/to an NTP with a large
+/// fakebox.
+- (BOOL)animateFromLargeFakebox {
+  if (self.browser->GetBrowserState()->IsOffTheRecord()) {
+    return NO;
+  }
   web::WebState* webState =
       self.browser->GetWebStateList()->GetActiveWebState();
   if (!webState) {
     return NO;
   }
   NewTabPageTabHelper* NTPHelper = NewTabPageTabHelper::FromWebState(webState);
-  return NTPHelper && NTPHelper->IsActive();
+  return NTPHelper && NTPHelper->IsActive() && IsIOSLargeFakeboxEnabled();
 }
 @end
