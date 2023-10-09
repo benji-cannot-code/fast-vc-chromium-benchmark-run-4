24165f84040526a87166e249217ca80cc44c9364
Vivian Pao <vpao@google.com>
2023-10-09 09:21:30 +0000

    [Promise Icons] Remove TWA promise apps created by Play Store
    Currently, TWA promise apps created by the Play Store do not get
    deleted at the end of the installation. This is because TWA promise
    apps (which listen to ARC installation events) have a package ID in
    the ARC app format to reflect their installation source. However, when
    the TWA finishes installing, it gets registered in the
    AppRegistryCache as a web app, which would generate a web app package
    ID that does not match the promise app.
    
    To successfully remove a TWA promise app, we need to generate an ARC
    package ID for installed TWAs. This requires us to check whether a web
    app is a TWA and has an associated package name in the
    ApkWebAppService. Since web apps installed from APKs are registered in
    App Service before they are recorded in prefs (which stores the list
    of all installed web app APKs), we need to expand this check to
    include finding a match with any currently installing web app APKs in
    the ApkWebAppService.
    
    Change-Id: I96e1bcae9bfcb2b7c99b61f7b08e3feb47f9add5
    Bug: b/297296711
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4910552
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Vivian Pao <vpao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1207000}

--- chrome/browser/apps/app_service/package_id_util.h
+++ chrome/browser/apps/app_service/package_id_util.h
@@ -6,7 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_APPS_APP_SERVICE_PACKAGE_ID_UTIL_H_
 #define CHROME_BROWSER_APPS_APP_SERVICE_PACKAGE_ID_UTIL_H_
 
+#include "build/chromeos_buildflags.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+
+class Profile;
 
 namespace apps {
 
@@ -17,11 +20,14 @@ class PackageId;
 
 namespace apps_util {
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
 // Generate the package ID for an app using its metadata. When an app has
 // incomplete metadata or a type that package IDs cannot support, we return
 // absl::nullopt since a package ID cannot be generated.
 absl::optional<apps::PackageId> GetPackageIdForApp(
+    Profile* profile,
     const apps::AppUpdate& update);
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 }  // namespace apps_util
 
