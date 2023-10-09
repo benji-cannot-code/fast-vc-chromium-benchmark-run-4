b3e35543d0b0315b58017d5a0cad10edbaf32e45
Andrey Kosyakov <caseq@chromium.org>
2023-10-09 22:42:11 +0000

    Add inspector-protocol/shared-storage/debugger-pause-on-first-script.js
    This demonstrates that shared storage worklet can be paued on first
    script statement.
    
    Change-Id: I1467a1393d1497927d41494602b0ecfd914f76a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4920156
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207339}

--- third_party/blink/web_tests/http/tests/inspector-protocol/shared-storage/resources/module.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/shared-storage/resources/module.js
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+console.log(`loaded module, test token: ${typeof(testToken) === 'undefined' ? '<undefined>' : testToken}`);
 var globalVar = 0;
 
 class EmptyOperation {
