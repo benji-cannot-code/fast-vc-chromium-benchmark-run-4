badc1941de8688ddf1d4729771bc9e5962b18e22
Ian Kilpatrick <ikilpatrick@chromium.org>
2023-10-09 20:36:36 +0000

    [RemoveLegacy] Move NumEffectiveColumns to table (instead of section)
    This method was previously on the section object as legacy only stored
    the appropriate data on that object. Move this up to the table object
    and rename to EffectiveColumnCount.
    
    There should be no behaviour change.
    
    Bug: 1229581
    Change-Id: Ib66d203cfd15a7602ac63b6d1381d6006598d4ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4923369
    Auto-Submit: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207260}

--- third_party/blink/renderer/core/layout/ng/table/layout_ng_table_section.h
+++ third_party/blink/renderer/core/layout/ng/table/layout_ng_table_section.h
@@ -77,8 +77,6 @@ class CORE_EXPORT LayoutNGTableSection : public LayoutBlock {
 
   unsigned NumRows() const;
 
-  unsigned NumEffectiveColumns() const;
-
  protected:
   bool IsOfType(LayoutObjectType type) const override {
     NOT_DESTROYED();
