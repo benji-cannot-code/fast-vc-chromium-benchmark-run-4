03d44d2b06879f2f049bf248262370d8768e9ec9
Xiyuan Xia <xiyuan@chromium.org>
2023-10-09 22:15:51 +0000

    crosier: Get gaia test accounts.
    - Add code to randomly pick a gaia test account from account
      pool for branded build;
    - Add a sanity test;
    
    Bug: b:301445988
    Change-Id: I6c06120e955203d828db44f2d1be6581884b5b01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4919500
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Sven Zheng <svenzheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207330}

---
+++ chrome/test/base/chromeos/crosier/test_accounts.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_TEST_BASE_CHROMEOS_CROSIER_TEST_ACCOUNTS_H_
+#define CHROME_TEST_BASE_CHROMEOS_CROSIER_TEST_ACCOUNTS_H_
+
+#include <string>
+
+namespace crosier {
+
+// Randomly picks a gaia test account from the test accounts pool.
+void GetGaiaTestAccount(std::string& out_email, std::string& out_password);
+
+}  // namespace crosier
+
+#endif  // CHROME_TEST_BASE_CHROMEOS_CROSIER_TEST_ACCOUNTS_H_
---
+++ chrome/test/base/chromeos/crosier/test_accounts_test.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/test/base/chromeos/crosier/test_accounts.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace crosier {
+
+TEST(TestAccountTest, Get) {
+  std::string email;
+  std::string password;
+
+  GetGaiaTestAccount(email, password);
+  EXPECT_TRUE(!email.empty());
+  EXPECT_TRUE(!password.empty());
+}
+
+}  // namespace crosier
