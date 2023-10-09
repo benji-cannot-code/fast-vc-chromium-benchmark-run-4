d2f54712eaf6ac18fa93ae09cb99326d0160daeb
Scott Yoder <scottyoder@google.com>
2023-10-09 21:37:45 +0000

    [ios] Large Fakebox focus animation polish fixes
    This fixes a few focus animation polish issues:
     * it aligns the fakebox text w/ the realbox text so that the focus
       and defocus animations appear more seamless
     * it hides the NTP header while the omnibox is focused so that it
       doesn't peek out behind the primary toolbar since it is taller,
       especially at the start of the defocus animation
     * it ensures the color of the NTP header only transitions to white in
       split toolbar mode
    
    Bug: 1482304
    Change-Id: I22a5c407c66fb33162f95b31ba0d54c1b45be78d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4922810
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1207304}

--- ios/chrome/browser/ui/ntp/new_tab_page_consumer.h
+++ ios/chrome/browser/ui/ntp/new_tab_page_consumer.h
@@ -17,6 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // enough down that the omnibo was already pinned to the top).
 @property(nonatomic, assign, readonly) CGFloat collectionShiftingOffset;
 
+// Indicates that the omnibox has become the first responder to the keyboard.
+- (void)omniboxDidBecomeFirstResponder;
+
 // Indicates that the omnibox will stop being the first responder to the
 // keyboard.
 - (void)omniboxWillResignFirstResponder;
