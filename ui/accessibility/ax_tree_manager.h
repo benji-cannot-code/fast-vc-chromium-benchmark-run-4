2495cfe27e9e033fcacb5a3a54e812f98f9e6d4d
Nektarios Paisios <nektar@chromium.org>
2023-10-09 18:28:11 +0000

    Adds an accessibility action that performs tree stitching: 2 of 5
    Adds the tree managers enabling code to run on ChromeOs.
    
    Given two accessibility trees T0 and T1,
    and a node N in T0, adds via an
    action T1 as a child tree rooted at N.
    This can be used to add extra auto-generated accessible
    information to an inaccessible node.
    
    Design doc:
    https://docs.google.com/document/d/1k8OH4XGjZrLVA705Xfy6vzSwGL9DkfRt9DG5s66lRjQ/edit?usp=sharing&resourcekey=0-K_9Y1EMhNoo8Ez9RPbQNMA
    
    AX-Relnotes: n/a.
    R=aleventhal@chromium.org, rhalavati@chromium.org, benjamin.beaudry@microsoft.com
    b/289012145
    Change-Id: I135957ca4db93fc17c2235b86a9784ccf71a846f
    
    
    Adds an accessibility action that performs tree stitching: 1 of 5
    
    Adds the foundation for the below task.
    
    Given two accessibility trees T0 and T1,
    and a node N in T0, adds via an
    action T1 as a child tree rooted at N.
    This can be used to add extra auto-generated accessible
    information to an inaccessible node.
    
    Design doc:
    https://docs.google.com/document/d/1k8OH4XGjZrLVA705Xfy6vzSwGL9DkfRt9DG5s66lRjQ/edit?usp=sharing&resourcekey=0-K_9Y1EMhNoo8Ez9RPbQNMA
    
    R=aleventhal@chromium.org, rhalavati@chromium.org, benjamin.beaudry@microsoft.com
    b/289012145
    
    AX-Relnotes: n/a.
    Change-Id: Icc4f0c764ded1b90355056e9619394584c99d46f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4916310
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207174}

--- ui/accessibility/ax_tree_manager.h
+++ ui/accessibility/ax_tree_manager.h
@@ -42,6 +42,9 @@ class AX_EXPORT AXTreeManager : public AXTreeObserver {
   // in any AXTreeManager.
   static void SetFocusChangeCallbackForTesting(base::RepeatingClosure callback);
 
+  AXTreeManager();
+  explicit AXTreeManager(std::unique_ptr<AXTree> tree);
+
   AXTreeManager(const AXTreeManager&) = delete;
   AXTreeManager& operator=(const AXTreeManager&) = delete;
 
@@ -145,9 +148,6 @@ class AX_EXPORT AXTreeManager : public AXTreeObserver {
   // It should be made private once that class is removed.
   // TODO(crbug.com/1468416): Make private.
   static AXTreeManagerMap& GetMap();
-
-  AXTreeManager();
-  explicit AXTreeManager(std::unique_ptr<AXTree> tree);
 
   virtual AXTreeManager* GetParentManager() const;
 
--- ui/accessibility/platform/ax_platform_tree_manager.h
+++ ui/accessibility/platform/ax_platform_tree_manager.h
@@ -22,6 +22,13 @@ class AXPlatformNodeDelegate;
 class COMPONENT_EXPORT(AX_PLATFORM) AXPlatformTreeManager
     : public AXTreeManager {
  public:
+  AXPlatformTreeManager() = default;
+  explicit AXPlatformTreeManager(std::unique_ptr<AXTree> tree)
+      : AXTreeManager(std::move(tree)) {}
+  AXPlatformTreeManager(const AXPlatformTreeManager&) = delete;
+  AXPlatformTreeManager& operator=(const AXPlatformTreeManager&) = delete;
+  ~AXPlatformTreeManager() override = default;
+
   // Returns an AXPlatformNode with the specified and |node_id|.
   virtual AXPlatformNode* GetPlatformNodeFromTree(
       const AXNodeID node_id) const = 0;
@@ -34,10 +41,6 @@ class COMPONENT_EXPORT(AX_PLATFORM) AXPlatformTreeManager
   virtual AXPlatformNodeDelegate* RootDelegate() const = 0;
 
   bool IsPlatformTreeManager() const override;
-
- protected:
-  explicit AXPlatformTreeManager(std::unique_ptr<AXTree> tree)
-      : AXTreeManager(std::move(tree)) {}
 };
 
 }  // namespace ui
