c2530e7e4b98d2881d2b1c9bf90d8a677f1b9c42
Jonathan Lee <jonathanjlee@google.com>
2023-10-09 22:01:02 +0000

    Roll wpt tooling
    This rolls wpt to latest commit at
    https://github.com/web-platform-tests/wpt.
    REMOTE-WPT-HEAD: fc2fee11ed7d0f61ac006f7ec3c571abde5d1227
    
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: Iddfcb09109d8510f87bf87787f3abb72d1a4b093
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4923375
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1207315}

--- third_party/wpt_tools/wpt/tools/webdriver/webdriver/bidi/modules/script.py
+++ third_party/wpt_tools/wpt/tools/webdriver/webdriver/bidi/modules/script.py
@@ -125,7 +125,8 @@ class Script(BidiModule):
         arguments: Optional[List[Mapping[str, Any]]] = None,
         this: Optional[Mapping[str, Any]] = None,
         result_ownership: Optional[OwnershipModel] = None,
-        serialization_options: Optional[SerializationOptions] = None
+        serialization_options: Optional[SerializationOptions] = None,
+        user_activation: Optional[bool] = None
     ) -> Mapping[str, Any]:
         params: MutableMapping[str, Any] = {
             "functionDeclaration": function_declaration,
@@ -141,6 +142,8 @@ class Script(BidiModule):
             params["resultOwnership"] = result_ownership
         if serialization_options is not None:
             params["serializationOptions"] = serialization_options
+        if user_activation is not None:
+            params["userActivation"] = user_activation
         return params
 
     @call_function.result
@@ -166,7 +169,8 @@ class Script(BidiModule):
         target: Target,
         await_promise: bool,
         result_ownership: Optional[OwnershipModel] = None,
-        serialization_options: Optional[SerializationOptions] = None
+        serialization_options: Optional[SerializationOptions] = None,
+        user_activation: Optional[bool] = None
     ) -> Mapping[str, Any]:
         params: MutableMapping[str, Any] = {
             "expression": expression,
@@ -178,6 +182,8 @@ class Script(BidiModule):
             params["resultOwnership"] = result_ownership
         if serialization_options is not None:
             params["serializationOptions"] = serialization_options
+        if user_activation is not None:
+            params["userActivation"] = user_activation
         return params
 
     @evaluate.result
--- third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/browsers/firefox.py
+++ third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/browsers/firefox.py
@@ -118,7 +118,7 @@ def browser_kwargs(logger, test_type, run_info_data, config, subsuite, **kwargs)
                       "certutil_binary": kwargs["certutil_binary"],
                       "ca_certificate_path": config.ssl_config["ca_cert_path"],
                       "e10s": kwargs["gecko_e10s"],
-                      "enable_fission": run_info_data["fission"],
+                      "disable_fission": kwargs["disable_fission"],
                       "stackfix_dir": kwargs["stackfix_dir"],
                       "binary_args": kwargs["binary_args"].copy(),
                       "timeout_multiplier": get_timeout_multiplier(test_type,
@@ -175,6 +175,7 @@ def executor_kwargs(logger, test_type, test_environment, run_info_data,
     executor_kwargs["ccov"] = run_info_data.get("ccov", False)
     executor_kwargs["browser_version"] = run_info_data.get("browser_version")
     executor_kwargs["debug_test"] = kwargs["debug_test"]
+    executor_kwargs["disable_fission"] = kwargs["disable_fission"]
     return executor_kwargs
 
 
@@ -204,18 +205,13 @@ def run_info_extras(**kwargs):
         pref_value = get_bool_pref_if_exists(pref)
         return pref_value if pref_value is not None else False
 
-    # Default fission to on, unless we get --[no-]enable-fission or
-    # --set-pref fission.autostart=[true|false]
-    enable_fission = [item for item in [kwargs.get("enable_fission"),
-                                        get_bool_pref_if_exists("fission.autostart"),
-                                        True] if item is not None][0]
-
+    # Default fission to on, unless we get --disable-fission
     rv = {"e10s": kwargs["gecko_e10s"],
           "wasm": kwargs.get("wasm", True),
           "verify": kwargs["verify"],
           "headless": kwargs.get("headless", False) or "MOZ_HEADLESS" in os.environ,
-          "fission": enable_fission,
-          "sessionHistoryInParent": (enable_fission or
+          "fission": not kwargs.get("disable_fission"),
+          "sessionHistoryInParent": (not kwargs.get("disable_fission") or
                                      not get_bool_pref("fission.disableSessionHistoryInParent")),
           "swgl": get_bool_pref("gfx.webrender.software")}
 
@@ -611,7 +607,7 @@ class FirefoxOutputHandler(OutputHandler):
 
 class ProfileCreator:
     def __init__(self, logger, prefs_root, config, test_type, extra_prefs, e10s,
-                 enable_fission, debug_test, browser_channel, binary, certutil_binary,
+                 disable_fission, debug_test, browser_channel, binary, certutil_binary,
                  ca_certificate_path):
         self.logger = logger
         self.prefs_root = prefs_root
@@ -619,7 +615,7 @@ class ProfileCreator:
         self.test_type = test_type
         self.extra_prefs = extra_prefs
         self.e10s = e10s
-        self.enable_fission = enable_fission
+        self.disable_fission = disable_fission
         self.debug_test = debug_test
         self.browser_channel = browser_channel
         self.ca_certificate_path = ca_certificate_path
@@ -694,13 +690,23 @@ class ProfileCreator:
         if self.e10s:
             profile.set_preferences({"browser.tabs.remote.autostart": True})
 
-        if self.enable_fission:
-            profile.set_preferences({"fission.autostart": True})
-        else:
+        profile.set_preferences({"fission.autostart": True})
+        if self.disable_fission:
             profile.set_preferences({"fission.autostart": False})
 
         if self.test_type in ("reftest", "print-reftest"):
             profile.set_preferences({"layout.interruptible-reflow.enabled": False})
+
+        if self.test_type == "testharness":
+            # Allow only touch and wheel events for now to be routed via the
+            # parent process. This is a workaround for tests using testdriver.js
+            # until bug 1773393 is done.
+            #
+            # Note that we don't set it for wdspec tests since it isn't suitable
+            # to ship to WebDriver users.
+            profile.set_preferences({"test.events.async.enabled": True})
+            profile.set_preferences({"test.events.async.key.enabled": False})
+            profile.set_preferences({"test.events.async.mouse.enabled": False})
 
         if self.test_type == "print-reftest":
             profile.set_preferences({"print.always_print_silent": True})
@@ -708,7 +714,7 @@ class ProfileCreator:
         # Bug 1262954: winxp + e10s, disable hwaccel
         if (self.e10s and platform.system() in ("Windows", "Microsoft") and
             "5.1" in platform.version()):
-            self.profile.set_preferences({"layers.acceleration.disabled": True})
+            profile.set_preferences({"layers.acceleration.disabled": True})
 
         if self.debug_test:
             profile.set_preferences({"devtools.console.stdout.content": True})
@@ -770,7 +776,7 @@ class FirefoxBrowser(Browser):
 
     def __init__(self, logger, binary, prefs_root, test_type, extra_prefs=None, debug_info=None,
                  symbols_path=None, stackwalk_binary=None, certutil_binary=None,
-                 ca_certificate_path=None, e10s=False, enable_fission=True,
+                 ca_certificate_path=None, e10s=False, disable_fission=False,
                  stackfix_dir=None, binary_args=None, timeout_multiplier=None, leak_check=False,
                  asan=False, chaos_mode_flags=None, config=None,
                  browser_channel="nightly", headless=None, preload_browser=False,
@@ -800,7 +806,7 @@ class FirefoxBrowser(Browser):
                                          test_type,
                                          extra_prefs,
                                          e10s,
-                                         enable_fission,
+                                         disable_fission,
                                          debug_test,
                                          browser_channel,
                                          binary,
@@ -873,7 +879,7 @@ class FirefoxWdSpecBrowser(WebDriverBrowser):
     def __init__(self, logger, binary, prefs_root, webdriver_binary, webdriver_args,
                  extra_prefs=None, debug_info=None, symbols_path=None, stackwalk_binary=None,
                  certutil_binary=None, ca_certificate_path=None, e10s=False,
-                 enable_fission=False, stackfix_dir=None, leak_check=False,
+                 disable_fission=False, stackfix_dir=None, leak_check=False,
                  asan=False, chaos_mode_flags=None, config=None,
                  browser_channel="nightly", headless=None, debug_test=False, **kwargs):
 
@@ -897,7 +903,7 @@ class FirefoxWdSpecBrowser(WebDriverBrowser):
                                          "wdspec",
                                          extra_prefs,
                                          e10s,
-                                         enable_fission,
+                                         disable_fission,
                                          debug_test,
                                          browser_channel,
                                          binary,
