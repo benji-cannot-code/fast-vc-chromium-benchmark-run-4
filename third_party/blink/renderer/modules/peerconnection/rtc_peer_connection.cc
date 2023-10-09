6b266395a3089a4a08d46f14bce4202525f19de0
Henrik Boström <hbos@chromium.org>
2023-10-09 11:56:40 +0000

    Clean up the WebRtcLegacyGetStatsThrows flag.
    Since throwing is fully launched now and there's no going back, we don't
    need this flag anymore. Delete.
    
    Bug: chromium:822696
    Change-Id: I88c1e50d9f3984d808c8091a2a81cadb52e40e49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4903740
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207031}

--- third_party/blink/renderer/modules/peerconnection/rtc_peer_connection.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_peer_connection.cc
@@ -142,10 +142,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-BASE_FEATURE(kWebRtcLegacyGetStatsThrows,
-             "WebRtcLegacyGetStatsThrows",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 namespace {
 
 const char kSignalingStateClosedMessage[] =
@@ -1692,28 +1688,23 @@ ScriptPromise RTCPeerConnection::LegacyCallbackBasedGetStats(
   auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
   ScriptPromise promise = resolver->Promise();
 
+  // This API is only usable if this Deprecation Trial is enabled:
+  // https://developer.chrome.com/origintrials/#/view_trial/3633278999381147649
+  // TODO(https://crbug.com/822696): In M122, delete this API.
   bool deprecation_trial_enabled =
       RuntimeEnabledFeatures::RTCLegacyCallbackBasedGetStatsEnabled(context);
   if (deprecation_trial_enabled) {
     // The deprecation trial is enabled, allow API usage without the warning.
-    // TODO(https://crbug.com/822696): In M122, delete this API.
     UseCounter::Count(context,
                       WebFeature::kRTCPeerConnectionLegacyGetStatsTrial);
   } else {
-    // The deprecation trial is NOT enabled: show a deprecation warning and
-    // maybe throw an exception.
+    // The deprecation trial is not enabled: count deprecation and throw.
     Deprecation::CountDeprecation(
         context, WebFeature::kRTCPeerConnectionGetStatsLegacyNonCompliant);
-    // The plan from the Intent to Deprecate is:
-    // - M114: Throw an exception in Canary/Beta.
-    // - M117: Throw in Stable.
-    // Which channel to throw on is controlled via the base::Feature.
-    if (base::FeatureList::IsEnabled(kWebRtcLegacyGetStatsThrows)) {
-      exception_state.ThrowDOMException(
-          DOMExceptionCode::kNotSupportedError,
-          "The callback-based getStats() method is no longer supported.");
-      return ScriptPromise();
-    }
+    exception_state.ThrowDOMException(
+        DOMExceptionCode::kNotSupportedError,
+        "The callback-based getStats() method is no longer supported.");
+    return ScriptPromise();
   }
   auto* stats_request = MakeGarbageCollected<RTCStatsRequestImpl>(
       GetExecutionContext(), this, success_callback, selector);
