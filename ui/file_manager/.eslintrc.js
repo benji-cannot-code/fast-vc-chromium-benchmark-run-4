9826b3003f94ca60b1f36b9fe98de51be6e6cc6f
Wenbo Jie <wenbojie@chromium.org>
2023-10-09 06:13:57 +0000

    [FilesTS] Convert file_operation_handler to TS
    Also Remove the fallThrough check from tsconfig, use eslint instead
    because it support commentsPattern to bypass the check
    
    Bug: b:289003444
    Change-Id: Iba1eb163ba334f4b38069244936ec4d4c7212e6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4912078
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206971}

--- ui/file_manager/.eslintrc.js
+++ ui/file_manager/.eslintrc.js
@@ -8,14 +8,16 @@ module.exports = {
     'no-console' : 'off',
   },
 
-  'overrides': [{
-    'files': ['**/*.ts'],
-    'parser': '../../third_party/node/node_modules/@typescript-eslint/parser/dist/index.js',
-    'plugins': [
-      '@typescript-eslint',
-    ],
-    'rules': {
-      // rule override goes here.
-    },
-  }],
+  'overrides' : [{
+                'files' : ['**/*.ts'],
+                'parser' : '../../third_party/node/node_modules/@typescript-eslint/parser/dist/index.js',
+                'plugins' :
+                          [
+                            '@typescript-eslint',
+                          ],
+                'rules' : {
+                  // rule override goes here.
+                  'no-fallthrough' : 'error',
+                },
+              }],
 };
