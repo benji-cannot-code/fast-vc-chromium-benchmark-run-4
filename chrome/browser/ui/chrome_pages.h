cde509c73176c7912d75579d755a83318ca679be
Elaine Chien <elainechien@chromium.org>
2023-10-09 00:58:48 +0000

    Add comment to contact gtech before adding feedback functionality
    Gtech team has requested this comment be added before new feedback
    sources are added in order to ensure that feedback is collected
    correctly.
    
    Bug: None
    Change-Id: Ia67991a687c329ba7e4c99e0603ecadc84d24e00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4918551
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Auto-Submit: Elaine Chien <elainechien@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206939}

--- chrome/browser/ui/chrome_pages.h
+++ chrome/browser/ui/chrome_pages.h
@@ -127,6 +127,10 @@ enum FeedbackSource {
   kFeedbackSourceGameDashboard,
   kFeedbackSourceLogin,
 
+  // ATTENTION: Before making any changes or adding to feedback collection,
+  // please ensure the teams that operationalize feedback are aware and
+  // supportive. Contact: chrome-gtech@
+
   // Must be last.
   kFeedbackSourceCount,
 };
