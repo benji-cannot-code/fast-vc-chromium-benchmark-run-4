582b9fa98015d248aa26c85b5641377a3f38ff19
Kent Tamura <tkent@chromium.org>
2023-10-10 23:45:58 +0000

    [RemoveLegacy] Store layout overflow as PhysicalRect
    ... instead of LayoutRect.
    This CL should have no behavior changes.
    
    Bug: 1229581
    Change-Id: I8d9bc05c7e3c8a157c4a0575872eea231e6dfe6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4918771
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207984}

--- third_party/blink/renderer/core/layout/overflow_model_test.cc
+++ third_party/blink/renderer/core/layout/overflow_model_test.cc
@@ -33,13 +33,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/core/testing/core_unit_test_helper.h"
-#include "third_party/blink/renderer/platform/geometry/layout_rect.h"
 
 namespace blink {
 namespace {
 
-LayoutRect InitialLayoutOverflow() {
-  return LayoutRect(10, 10, 80, 80);
+PhysicalRect InitialLayoutOverflow() {
+  return PhysicalRect(10, 10, 80, 80);
 }
 
 PhysicalRect InitialVisualOverflow() {
