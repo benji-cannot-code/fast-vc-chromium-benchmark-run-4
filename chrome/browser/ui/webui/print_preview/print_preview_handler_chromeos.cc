a9ef283ca850cc43a10b8ffedb43e4b6cd6116a5
Gavin Williams <gavinwill@chromium.org>
2023-10-10 19:11:12 +0000

    Printing: Fix local-printers-updated event format
    The fired event needs to include a PrinterType to match the same format
    as the "printers-added" event.
    
    Bug: b:300136694
    Tests: unit_tests --gtest_filter=*PrintPreviewHandlerChromeOSTest*
    Change-Id: I96784cd358e4dcfc4cac62514dacc0a22473b30f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4927928
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207825}

--- chrome/browser/ui/webui/print_preview/print_preview_handler_chromeos.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_handler_chromeos.cc
@@ -421,6 +421,7 @@ void PrintPreviewHandlerChromeOS::OnHandleObserveLocalPrinters(
 void PrintPreviewHandlerChromeOS::OnLocalPrintersUpdated(
     std::vector<crosapi::mojom::LocalDestinationInfoPtr> printers) {
   FireWebUIListener("local-printers-updated",
+                    base::Value(static_cast<int>(mojom::PrinterType::kLocal)),
                     ConvertPrintersToValues(printers));
 }
 
