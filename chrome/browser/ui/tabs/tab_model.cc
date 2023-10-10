3d8558519eebc88577c6799e6c868238f8af0cdf
Taylor Bergquist <tbergquist@chromium.org>
2023-10-10 20:23:30 +0000

    De-generalize TabBase and rename to TabModel.
    We'll be using TabModel (in some form) as part of the TabStripModel API,
    so this CL simplifies the type a bit by rolling back the incomplete
    refactor to split out the contents type.
    
    Next steps:
    - Add handle support to TabModel
    - Add TabStripModel methods for converting between model indices and handles (to const TabModels)
    
    Bug: 1476012
    Change-Id: I7cff6b9ed937679dd02a90d69d36a81ea711b90e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4822967
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207872}

---
+++ chrome/browser/ui/tabs/tab_model.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/tabs/tab_model.h"
+
+#include "third_party/perfetto/include/perfetto/tracing/traced_value.h"
+
+TabModel::TabModel(std::unique_ptr<content::WebContents> contents)
+    : contents_(std::move(contents)) {}
+
+TabModel::~TabModel() = default;
+
+void TabModel::WriteIntoTrace(perfetto::TracedValue context) const {
+  auto dict = std::move(context).WriteDictionary();
+  dict.Add("web_contents", contents());
+  dict.Add("pinned", pinned());
+  dict.Add("blocked", blocked());
+}
