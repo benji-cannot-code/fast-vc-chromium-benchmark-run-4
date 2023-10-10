d586a9438d655a56f31ac6ab9362bc5dcf82c6d4
Chris Fredrickson <cfredric@chromium.org>
2023-10-10 11:12:45 +0000

    [RWS] Remove flag entry
    The feature has been enabled by default, and it can still be controlled
    via the feature itself.
    
    Fixed: 1489907
    Change-Id: Ia023b42dae5f3e512400e5aa71acf2ca63e31960
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4922250
    Reviewed-by: Dylan Cutler <dylancutler@google.com>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Cr-Commit-Position: refs/heads/main@{#1207565}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -515,9 +515,6 @@ extern const char kUseDnsHttpsSvcbAlpnDescription[];
 extern const char kEditContextName[];
 extern const char kEditContextDescription[];
 
-extern const char kEnableFirstPartySetsName[];
-extern const char kEnableFirstPartySetsDescription[];
-
 extern const char kSHA1ServerSignatureName[];
 extern const char kSHA1ServerSignatureDescription[];
 
