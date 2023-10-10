ec9f1de0594acf4511df06db1d22c9d32ed16b37
Lingyu Feng <lingyufeng@chromium.org>
2023-10-10 02:23:29 +0000

    Add UMA for ARC window closing latency measurement
    This CL adds UMA to measure the time elapsed from window closing
    operation until the window is destroyed. This UMA only measures for ARC
    windows.
    
    BUG=b:299401105
    TEST=manually checked chrome://histograms
    TEST=ArcWmMetricsTest.*
    
    Change-Id: I86fa94ee813406773cebe08c801c3262806e5094
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905149
    Commit-Queue: Lingyu Feng <lingyufeng@chromium.org>
    Reviewed-by: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Reviewed-by: Muhammad Hasan Khan <mhasank@chromium.org>
    Auto-Submit: Lingyu Feng <lingyufeng@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207411}

--- ash/components/arc/metrics/arc_wm_metrics.cc
+++ ash/components/arc/metrics/arc_wm_metrics.cc
@@ -14,7 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/timer/elapsed_timer.h"
 #include "chromeos/ui/base/window_state_type.h"
+#include "components/exo/shell_surface_base.h"
+#include "components/exo/shell_surface_util.h"
 #include "ui/aura/client/aura_constants.h"
+#include "ui/aura/window_observer.h"
 #include "ui/base/ui_base_types.h"
 
 namespace arc {
@@ -27,6 +30,9 @@ constexpr char kWindowMaximizedTimeHistogramPrefix[] =
 // Histogram of the delay for window minimizing operation.
 constexpr char kWindowMinimizedTimeHistogramPrefix[] =
     "Arc.WM.WindowMinimizedDelayTime.";
+// Histogram of the delay for window closing operation.
+constexpr char kWindowClosedTimeHistogramPrefix[] =
+    "Arc.WM.WindowClosedDelayTime.";
 
 constexpr char kArcHistogramName[] = "ArcApp";
 constexpr char kBrowserHistogramName[] = "Browser";
@@ -118,6 +124,43 @@ class ArcWmMetrics::WindowStateChangeObserver
   base::OnceClosure window_operation_completed_callback_;
 };
 
+// A window observer that records the delay of window closing operation for ARC
+// windows.
+class ArcWmMetrics::WindowCloseObserver : public aura::WindowObserver {
+ public:
+  WindowCloseObserver(aura::Window* window, base::OnceClosure callback)
+      : window_close_completed_callback_(std::move(callback)) {
+    window_observation_.Observe(window);
+  }
+
+  WindowCloseObserver(const WindowCloseObserver&) = delete;
+  WindowCloseObserver& operator=(const WindowCloseObserver) = delete;
+  ~WindowCloseObserver() override = default;
+
+  // aura::WindowObserver:
+  void OnWindowDestroyed(aura::Window* window) override {
+    RecordWindowCloseDelay();
+    std::move(window_close_completed_callback_).Run();
+  }
+
+ private:
+  void RecordWindowCloseDelay() {
+    base::UmaHistogramCustomTimes(
+        ArcWmMetrics::GetArcWindowClosedTimeHistogramName(),
+        window_close_elapsed_timer_.Elapsed(),
+        /*minimum=*/base::Milliseconds(1),
+        /*maximum=*/base::Seconds(2), 100);
+  }
+
+  // Tracks the elapsed time from the window closing operation happens until the
+  // the window is destroyed.
+  base::ElapsedTimer window_close_elapsed_timer_;
+  base::ScopedObservation<aura::Window, aura::WindowObserver>
+      window_observation_{this};
+
+  base::OnceClosure window_close_completed_callback_;
+};
+
 ArcWmMetrics::ArcWmMetrics() {
   if (aura::Env::HasInstance()) {
     env_observation_.Observe(aura::Env::GetInstance());
@@ -140,9 +183,16 @@ std::string ArcWmMetrics::GetWindowMinimizedTimeHistogramName(
   return base::StrCat({kWindowMinimizedTimeHistogramPrefix, app_type_str});
 }
 
+// static
+std::string ArcWmMetrics::GetArcWindowClosedTimeHistogramName() {
+  const std::string arc_app_type_str = GetAppTypeName(ash::AppType::ARC_APP);
+  return base::StrCat({kWindowClosedTimeHistogramPrefix, arc_app_type_str});
+}
+
 void ArcWmMetrics::OnWindowInitialized(aura::Window* new_window) {
-  if (static_cast<ash::AppType>(new_window->GetProperty(
-          aura::client::kAppType)) == ash::AppType::NON_APP) {
+  ash::AppType app_type = static_cast<ash::AppType>(
+      new_window->GetProperty(aura::client::kAppType));
+  if (app_type == ash::AppType::NON_APP) {
     return;
   }
 
@@ -151,6 +201,17 @@ void ArcWmMetrics::OnWindowInitialized(aura::Window* new_window) {
   }
 
   window_observations_.AddObservation(new_window);
+
+  if (app_type == ash::AppType::ARC_APP) {
+    auto* shell_surface_base = exo::GetShellSurfaceBaseForWindow(new_window);
+
+    // |shell_surface_base| can be null in unit tests.
+    if (shell_surface_base) {
+      shell_surface_base->set_pre_close_callback(
+          base::BindRepeating(&ArcWmMetrics::OnWindowCloseRequested,
+                              weak_ptr_factory_.GetWeakPtr(), new_window));
+    }
+  }
 }
 
 void ArcWmMetrics::OnWindowPropertyChanged(aura::Window* window,
@@ -202,6 +263,17 @@ void ArcWmMetrics::OnWindowDestroying(aura::Window* window) {
 
 void ArcWmMetrics::OnOperationCompleted(aura::Window* window) {
   state_change_observing_windows_.erase(window);
+}
+
+void ArcWmMetrics::OnWindowCloseRequested(aura::Window* window) {
+  close_observing_windows_.emplace(
+      window, std::make_unique<WindowCloseObserver>(
+                  window, base::BindOnce(&ArcWmMetrics::OnWindowCloseCompleted,
+                                         base::Unretained(this), window)));
+}
+
+void ArcWmMetrics::OnWindowCloseCompleted(aura::Window* window) {
+  close_observing_windows_.erase(window);
 }
 
 }  // namespace arc
