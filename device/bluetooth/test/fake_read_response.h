4591dc59e582f54648cb246f1cc69c1a2e1b7c72
Jose Dapena Paz <jdapena@igalia.com>
2023-10-10 07:29:53 +0000

    IWYU: missing include for uint16_t usage in fake_read_response.h
    Bug: 957519
    Change-Id: I60d8103d7bba5a9507301a9ddf02b8ec14be4176
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4923429
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207491}

--- device/bluetooth/test/fake_read_response.h
+++ device/bluetooth/test/fake_read_response.h
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef DEVICE_BLUETOOTH_TEST_FAKE_READ_RESPONSE_H_
 #define DEVICE_BLUETOOTH_TEST_FAKE_READ_RESPONSE_H_
 
+#include <cstdint>
 #include <vector>
 
 #include "third_party/abseil-cpp/absl/types/optional.h"
