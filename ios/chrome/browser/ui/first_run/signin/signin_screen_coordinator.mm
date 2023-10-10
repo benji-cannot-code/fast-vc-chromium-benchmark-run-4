73f75d9645c8f530b8f4a18a279cc40df861521c
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-10-10 15:12:00 +0000

    [ios/signin] Fix some metrics for upgrade sign-in promo
    At least Signin.SignIn.Completed was logging to ACCESS_POINT_START_PAGE
    instead of ACCESS_POINT_SIGNIN_PROMO. Other histograms probably too.
    
    Fixed: 1491182
    Change-Id: If89034671f8436650607f2d1d9158db647dde980
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4925369
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207663}

--- ios/chrome/browser/ui/first_run/signin/signin_screen_coordinator.mm
+++ ios/chrome/browser/ui/first_run/signin/signin_screen_coordinator.mm
@@ -159,8 +159,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.addAccountSigninCoordinator = [SigninCoordinator
       addAccountCoordinatorWithBaseViewController:self.viewController
                                           browser:self.browser
-                                      accessPoint:signin_metrics::AccessPoint::
-                                                      ACCESS_POINT_START_PAGE];
+                                      accessPoint:_accessPoint];
   __weak __typeof(self) weakSelf = self;
   self.addAccountSigninCoordinator.signinCompletion =
       ^(SigninCoordinatorResult signinResult,
@@ -191,8 +190,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   AuthenticationFlow* authenticationFlow =
       [[AuthenticationFlow alloc] initWithBrowser:self.browser
                                          identity:self.mediator.selectedIdentity
-                                      accessPoint:signin_metrics::AccessPoint::
-                                                      ACCESS_POINT_START_PAGE
+                                      accessPoint:_accessPoint
                                  postSignInAction:PostSignInAction::kNone
                          presentingViewController:self.viewController];
   authenticationFlow.dispatcher = HandlerForProtocol(
