99a423e7b689c6b14113203f22ab57218ee01abe
Fiona Macintosh <fmacintosh@google.com>
2023-10-10 20:34:54 +0000

    [Tracking Protection] Add aria-labels for Learn more links
    Fixed: b:303079876
    Skip-Translation-Screenshots-Check: True
    Change-Id: I5d7b767f28f3006944d4823131c3f05034be52a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4916512
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207880}

--- chrome/browser/resources/settings_shared/controls/settings_toggle_button.ts
+++ chrome/browser/resources/settings_shared/controls/settings_toggle_button.ts
@@ -83,6 +83,11 @@ export class SettingsToggleButtonElement extends
         reflectToAttribute: true,
       },
 
+      learnMoreAriaLabel: {
+        type: String,
+        value: '',
+      },
+
       icon: String,
 
       subLabelIcon: String,
@@ -100,6 +105,7 @@ export class SettingsToggleButtonElement extends
   ariaShowSublabel: boolean;
   elideLabel: boolean;
   icon: string;
+  learnMoreAriaLabel: string;
   learnMoreUrl: string;
   subLabelWithLink: string;
   subLabelIcon: string;
@@ -132,6 +138,11 @@ export class SettingsToggleButtonElement extends
 
   private getAriaLabel_(): string {
     return this.ariaLabel || this.label;
+  }
+
+  private getLearnMoreAriaLabelledBy_(): string {
+    return this.learnMoreAriaLabel ? 'learn-more-aria-label' :
+                                     'sub-label-text learn-more';
   }
 
   private onDisableOrPrefChange_() {
