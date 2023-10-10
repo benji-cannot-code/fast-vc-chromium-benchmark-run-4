4fbcc54b85b5191f6a896a999fb4b62a8462c8e9
Maggie Cai <mxcai@google.com>
2023-10-10 11:12:12 +0000

    [Shortstand] Create a flag for shortstand.
    This CL creates a flag for the shortstand project to differentiate web
    app from browser created shortcut. Which includes:
    1. Web app display in window
    2. shortcuts display in tab
    3. shortcuts should not have notifications
    4. the launcher and shelf location persists
    5. Determine what is shortcut based on go/shortstand
    6. Determine when to show the indicator dialog
    
    BUG=1491299
    
    Change-Id: If47c02be1b6f86b9a7a7ebfe74d851df83eb72b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4924693
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207564}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3271,6 +3271,9 @@ extern const char kCrosBatterySaverAlwaysOnDescription[];
 extern const char kCrosBatterySaverName[];
 extern const char kCrosBatterySaverDescription[];
 
+extern const char kCrosShortstandName[];
+extern const char kCrosShortstandDescription[];
+
 extern const char kCrosSoulName[];
 extern const char kCrosSoulDescription[];
 
