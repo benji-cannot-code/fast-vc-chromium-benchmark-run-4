86fe800a76a0aa772a30dbc4b1328503e5d43206
Etienne Pierre-doray <etiennep@chromium.org>
2023-10-10 15:38:38 +0000

    [tracing] More robust background tracing rules
    Previously, TimerRule with delay_ms=0 would immediately trigger
    and cause reentrency. This CL forces a timer call to avoid reentrency.
    It also validates rule config and fails to create scenarios otherwise.
    
    Bug: 1418116
    Change-Id: I8a9cc81dea5c30c125a3980d054a90bc4e51c85d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4916780
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207680}

--- content/browser/tracing/tracing_scenario_unittest.cc
+++ content/browser/tracing/tracing_scenario_unittest.cc
@@ -222,11 +222,23 @@ class TracingScenarioForTesting : public TracingScenario {
  public:
   TracingScenarioForTesting(const perfetto::protos::gen::ScenarioConfig& config,
                             TestTracingScenarioDelegate* delegate)
-      : TracingScenario(config, delegate) {}
+      : TracingScenario(config, delegate) {
+    EXPECT_TRUE(Initialize(config, false, false));
+  }
 
  protected:
   std::unique_ptr<perfetto::TracingSession> CreateTracingSession() override {
     return std::make_unique<TestTracingSession>();
+  }
+};
+
+class NestedTracingScenarioForTesting : public NestedTracingScenario {
+ public:
+  NestedTracingScenarioForTesting(
+      const perfetto::protos::gen::NestedScenarioConfig& config,
+      NestedTracingScenario::Delegate* delegate)
+      : NestedTracingScenario(config, delegate) {
+    EXPECT_TRUE(Initialize(config));
   }
 };
 
@@ -693,7 +705,7 @@ TEST_F(TracingScenarioTest, NestedUpload) {
 }
 
 TEST_F(NestedTracingScenarioTest, Disabled) {
-  NestedTracingScenario tracing_scenario(
+  NestedTracingScenarioForTesting tracing_scenario(
       ParseNestedScenarioConfigFromText(kDefaultNestedConfig), &delegate);
 
   EXPECT_CALL(delegate, OnNestedScenarioStart(&tracing_scenario)).Times(0);
@@ -715,7 +727,7 @@ TEST_F(NestedTracingScenarioTest, Disabled) {
 }
 
 TEST_F(NestedTracingScenarioTest, StartStop) {
-  NestedTracingScenario tracing_scenario(
+  NestedTracingScenarioForTesting tracing_scenario(
       ParseNestedScenarioConfigFromText(kDefaultNestedConfig), &delegate);
 
   tracing_scenario.Enable();
@@ -736,7 +748,7 @@ TEST_F(NestedTracingScenarioTest, StartStop) {
 }
 
 TEST_F(NestedTracingScenarioTest, Upload) {
-  NestedTracingScenario tracing_scenario(
+  NestedTracingScenarioForTesting tracing_scenario(
       ParseNestedScenarioConfigFromText(kDefaultNestedConfig), &delegate);
 
   tracing_scenario.Enable();
@@ -753,7 +765,7 @@ TEST_F(NestedTracingScenarioTest, Upload) {
 }
 
 TEST_F(NestedTracingScenarioTest, StopUpload) {
-  NestedTracingScenario tracing_scenario(
+  NestedTracingScenarioForTesting tracing_scenario(
       ParseNestedScenarioConfigFromText(kDefaultNestedConfig), &delegate);
 
   tracing_scenario.Enable();
