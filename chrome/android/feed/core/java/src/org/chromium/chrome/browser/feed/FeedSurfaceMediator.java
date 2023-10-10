4bf47e28b47520c777aef7d0f8372a80fce5cf89
Xi Han <hanxi@google.com>
2023-10-10 21:07:47 +0000

    [SurfacePolish] Fix feeds header in narrow window on tablets.
    In this CL, we make the following 2 changes:
    1) FeedSurfaceMediator be an observer of UiConfig, and adjust the
       layout parameter of Feeds header when the window becomes narrow in
       tablets.
    2) Call mUiConfig.updateDisplayStyle() in NTP when mUiConfig is first
       available rather than every time when on NTP is visible. This
       prevents multiple call of updateDisplayStyle() when the display
       style isn't changed.
    Before: https://screenshot.googleplex.com/szBJhaC3pCuJaFL
    After: https://screenshot.googleplex.com/AgSeYPEHVSei6Aq
    Demo: https://drive.google.com/file/d/1oLmMhyFlQzvxqc5np79j4n6KP2afuQsG/view?usp=drive_link
    
    FeedSurfaceMediatorTest.java.
    
    Bug: 1490279
    Low-Coverage-Reason: TRIVIAL_CHANGE Changes in this CL are covered in
    Change-Id: Ica31397be6ef15511806149c0e9faef11281a0bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4916576
    Reviewed-by: Dan H <harringtond@chromium.org>
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207897}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceMediator.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceMediator.java
@@ -51,6 +51,9 @@ import org.chromium.chrome.browser.ui.signin.PersonalizedSigninPromoView;
 import org.chromium.chrome.browser.ui.signin.SyncPromoController;
 import org.chromium.chrome.browser.xsurface.ListLayoutHelper;
 import org.chromium.chrome.browser.xsurface.feed.StreamType;
+import org.chromium.components.browser_ui.widget.displaystyle.DisplayStyleObserver;
+import org.chromium.components.browser_ui.widget.displaystyle.HorizontalDisplayStyle;
+import org.chromium.components.browser_ui.widget.displaystyle.UiConfig;
 import org.chromium.components.browser_ui.widget.listmenu.ListMenu;
 import org.chromium.components.browser_ui.widget.listmenu.ListMenuItemProperties;
 import org.chromium.components.prefs.PrefService;
@@ -208,6 +211,10 @@ public class FeedSurfaceMediator
     private final FeedActionDelegate mActionDelegate;
     private final FeedOptionsCoordinator mOptionsCoordinator;
 
+    // It is non-null for NTP on tablets when SurfacePolish is enabled.
+    private @Nullable final UiConfig mUiConfig;
+    private final DisplayStyleObserver mDisplayStyleObserver = this::onDisplayStyleChanged;
+
     private @Nullable RecyclerView.OnScrollListener mStreamScrollListener;
     private final ObserverList<ScrollListener> mScrollListeners = new ObserverList<>();
     private HasContentListener mHasContentListener;
@@ -249,11 +256,12 @@ public class FeedSurfaceMediator
      * @param headerModel The {@link PropertyModel} that contains this mediator should work with.
      * @param openingTabId The {@link FeedSurfaceCoordinator.StreamTabId} the feed should open to.
      * @param optionsCoordinator The {@link FeedOptionsCoordinator} for the feed.
+     * @param uiConfig The {@link UiConfig} for screen display.
      */
     FeedSurfaceMediator(FeedSurfaceCoordinator coordinator, Context context,
             @Nullable SnapScrollHelper snapScrollHelper, PropertyModel headerModel,
             @FeedSurfaceCoordinator.StreamTabId int openingTabId, FeedActionDelegate actionDelegate,
-            FeedOptionsCoordinator optionsCoordinator) {
+            FeedOptionsCoordinator optionsCoordinator, @Nullable UiConfig uiConfig) {
         mCoordinator = coordinator;
         mHasContentListener = coordinator;
         mContext = context;
@@ -266,6 +274,7 @@ public class FeedSurfaceMediator
         mOptionsCoordinator.setOptionsListener(this);
         mIsNewTabSearchEngineUrlAndroidEnabled =
                 DseNewTabUrlManager.isNewTabSearchEngineUrlAndroidEnabled();
+        mUiConfig = uiConfig;
 
         /**
          * When feature flag isNewTabSearchEngineUrlAndroidEnabled is enabled, the Feeds may be
@@ -305,6 +314,11 @@ public class FeedSurfaceMediator
         mStreamContentChangedListener = contents
                 -> mRecyclerViewAnimationFinishDetector.runWhenAnimationComplete(
                         this::onContentsChanged);
+
+        if (mUiConfig != null) {
+            mUiConfig.addObserver(mDisplayStyleObserver);
+            onDisplayStyleChanged(mUiConfig.getCurrentDisplayStyle());
+        }
 
         initialize();
     }
@@ -372,6 +386,9 @@ public class FeedSurfaceMediator
         destroyPropertiesForStream();
         mPrefChangeRegistrar.destroy();
         mTemplateUrlService.removeObserver(this);
+        if (mUiConfig != null) {
+            mUiConfig.removeObserver(mDisplayStyleObserver);
+        }
     }
 
     public void destroyForTesting() {
@@ -1294,5 +1311,11 @@ public class FeedSurfaceMediator
 
     int getTabToStreamSizeForTesting() {
         return mTabToStreamMap.size();
+    }
+
+    private void onDisplayStyleChanged(UiConfig.DisplayStyle newDisplayStyle) {
+        mSectionHeaderModel.set(
+                SectionHeaderListProperties.IS_NARROW_WINDOW_ON_TABLET_KEY,
+                newDisplayStyle.horizontal < HorizontalDisplayStyle.WIDE);
     }
 }
