b2001d5217813e88aecb5b232ee2f691151aec75
Gavin Williams <gavinwill@chromium.org>
2023-10-10 16:23:41 +0000

    Printing: Listen to local-printers-updated event for local printers
    Adds a listener to the destination store to receive then add local
    printers.
    
    Bug: b:300136694
    Tests: browser_tests --gtest_filter=*DestinationStoreTest*
    Change-Id: I4dac62247a943d0861011465e51082df64bfb801
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4913423
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207704}

--- chrome/browser/resources/print_preview/data/destination_store.ts
+++ chrome/browser/resources/print_preview/data/destination_store.ts
@@ -258,6 +258,16 @@ export class DestinationStore extends EventTarget {
         (type: PrinterType,
          printers: LocalDestinationInfo[]|ExtensionDestinationInfo[]) =>
             this.onPrintersAdded_(type, printers));
+
+    // <if expr="is_chromeos">
+    if (loadTimeData.getBoolean('isLocalPrinterObservingEnabled')) {
+      addListenerCallback(
+          'local-printers-updated',
+          (type: PrinterType,
+           printers: LocalDestinationInfo[]|ExtensionDestinationInfo[]) =>
+              this.onLocalPrintersUpdated_(type, printers));
+    }
+    // </if>
   }
 
   /**
