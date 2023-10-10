1bd832c102d999b5d80b3780dbaa88bc347220b4
Chris Lu <thegreenfrog@chromium.org>
2023-10-10 04:11:43 +0000

    [ios] Add new Set Up List configurations for Magic Stack
    This change moves the Sign in item to the back of the Set Up List hero
    cell list in the Magic Stack. It also adds a finch param in the
    MagicStack feature to configure how many days before the compacted
    single-module SetUpList item is shown.
    
    Bug: 1490935
    Change-Id: I6d794bc33220c74369e5d800df579b8b57934558
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4919605
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1207446}

--- ios/chrome/browser/ui/content_suggestions/set_up_list/utils.mm
+++ ios/chrome/browser/ui/content_suggestions/set_up_list/utils.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/time/time.h"
 #import "ios/chrome/browser/first_run/first_run.h"
 #import "ios/chrome/browser/ntp/set_up_list_prefs.h"
+#import "ios/chrome/browser/ui/content_suggestions/content_suggestions_feature.h"
 #import "ios/chrome/browser/ui/first_run/first_run_util.h"
 
 namespace set_up_list_utils {
@@ -43,7 +44,8 @@ bool IsSetUpListActive(PrefService* local_state) {
 
 bool ShouldShowCompactedSetUpListModule() {
   absl::optional<base::Time> firstRunTime = GetFirstRunTime();
-  base::Time expiry_time = firstRunTime.value() + base::Days(3);
+  base::Time expiry_time =
+      firstRunTime.value() + base::Days(TimeUntilShowingCompactedSetUpList());
   if (base::Time::Now() > expiry_time) {
     return true;
   }
