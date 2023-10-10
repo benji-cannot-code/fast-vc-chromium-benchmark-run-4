6ce8750a34b53ea24ba9750d4e8c4ec78dd2e627
Shibalik Mohapatra <shibalik@chromium.org>
2023-10-10 00:44:46 +0000

    Add a ToString() method to SidePanelEntryKey.
    This method will later be used by SidePanelCoordinator to access the action items dynamically created like in the case of extensions.
    
    Low-Coverage-Reason:TRIVIAL_CHANGE side_panel_entry_id.h (Tests exist for some of the SIDE_PANEL_ENTRY_IDS) and it is trivial that it should not have to exist for every since entry. The functionality is tested.
    
    Change-Id: I700ffc10c68c53b81b772ba924a952f9137005a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4920192
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207382}

--- chrome/browser/ui/side_panel/side_panel_entry_key.h
+++ chrome/browser/ui/side_panel/side_panel_entry_key.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_SIDE_PANEL_SIDE_PANEL_ENTRY_KEY_H_
 #define CHROME_BROWSER_UI_SIDE_PANEL_SIDE_PANEL_ENTRY_KEY_H_
 
+#include <string>
+
 #include "chrome/browser/ui/side_panel/side_panel_entry_id.h"
 #include "extensions/common/extension_id.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -26,6 +28,7 @@ class SidePanelEntryKey {
   absl::optional<extensions::ExtensionId> extension_id() const {
     return extension_id_;
   }
+  std::string ToString() const;
 
  private:
   SidePanelEntryId id_;
