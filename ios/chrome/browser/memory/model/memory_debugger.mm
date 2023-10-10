8e904a5cfd426d891c7e0520c2a0d1b4f38aba7e
mmrashad <mmrashad@google.com>
2023-10-10 15:23:57 +0000

    [Code Health][iOS] Move ios/c/b/memory to model subfolder
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/<foo>/ will be moved to
    ios/c/b/<foo>/model/ to showcase that they are model files.
    
    Bug: 1319852, 1480320
    Change-Id: I74283febd8266d704702bfaf9d79550165a289be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4926529
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207672}

--- ios/chrome/browser/memory/model/memory_debugger.mm
+++ ios/chrome/browser/memory/model/memory_debugger.mm
@@ -3,14 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/memory/memory_debugger.h"
+#import "ios/chrome/browser/memory/model/memory_debugger.h"
 
 #import <stdint.h>
 
 #import <memory>
 
 #import "build/branding_buildflags.h"
-#import "ios/chrome/browser/memory/memory_metrics.h"
+#import "ios/chrome/browser/memory/model/memory_metrics.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/ui/util/device_util.h"
 
--- ios/chrome/browser/memory/model/memory_debugger_manager.mm
+++ ios/chrome/browser/memory/model/memory_debugger_manager.mm
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/memory/memory_debugger_manager.h"
+#import "ios/chrome/browser/memory/model/memory_debugger_manager.h"
 
 #import "base/functional/bind.h"
 #import "components/prefs/pref_member.h"
 #import "components/prefs/pref_registry_simple.h"
 #import "components/prefs/pref_service.h"
-#import "ios/chrome/browser/memory/memory_debugger.h"
+#import "ios/chrome/browser/memory/model/memory_debugger.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 
 @implementation MemoryDebuggerManager {
