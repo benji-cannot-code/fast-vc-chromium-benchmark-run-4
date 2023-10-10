f27163d6713be4520e4c97d3466feb7267c77864
Scott Yoder <scottyoder@google.com>
2023-10-10 13:23:56 +0000

    [ios] Match Fakebox and Omnibox text colors to improve focus animation
    This change updates the color of the text initially shown during the
    omnibox focus animation to match the color of the "Search or Type URL"
    text shown in the fakebox. When these colors match, the focus animation
    appears more seamless / smooth.
    
    In the past these colors were the same, but with the MagicStack and
    LargeFakebox changes, these have diverged a little.
    
    Screencast:
    https://screencast.googleplex.com/cast/NDc1ODI0MjczODU2OTIxNnwzOGEzMWQ5Zi03Ng
    
    Bug: 1482304
    Change-Id: I0cfd27fdf4dc49ec68c314343114d050caf4192b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4922220
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1207607}

--- ios/chrome/browser/ui/location_bar/location_bar_steady_view.mm
+++ ios/chrome/browser/ui/location_bar/location_bar_steady_view.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/shared/ui/util/dynamic_type_util.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
+#import "ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_utils.h"
 #import "ios/chrome/browser/ui/omnibox/omnibox_constants.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
@@ -94,7 +95,7 @@ const CGFloat kLeadingMargin = 20;
       [[LocationBarSteadyViewColorScheme alloc] init];
 
   scheme.fontColor = [UIColor colorNamed:kTextPrimaryColor];
-  scheme.placeholderColor = [UIColor colorNamed:kTextfieldPlaceholderColor];
+  scheme.placeholderColor = content_suggestions::SearchHintLabelColor();
   scheme.trailingButtonColor = [UIColor colorNamed:kGrey600Color];
 
   return scheme;
