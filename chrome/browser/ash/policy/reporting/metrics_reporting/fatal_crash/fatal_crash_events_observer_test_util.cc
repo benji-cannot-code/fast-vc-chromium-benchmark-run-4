949bc1da17934ac89cb198c751b5864d7760b4bd
Hong Xu <xuhong@chromium.org>
2023-10-10 23:23:09 +0000

    Remove a local ID from the save file when the crash is uploaded
    Once a crash is reported again as an uploaded crash, we no longer need
    to save its local ID.
    
    Bug: b:266018440
    Change-Id: Ib5d45c8e34d645140ac08e2ab49c44de63aef7ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4923844
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Hong Xu <xuhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1207973}

--- chrome/browser/ash/policy/reporting/metrics_reporting/fatal_crash/fatal_crash_events_observer_test_util.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/fatal_crash/fatal_crash_events_observer_test_util.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/memory/ptr_util.h"
+#include "base/sequence_checker.h"
 #include "chrome/browser/ash/policy/reporting/metrics_reporting/fatal_crash/fatal_crash_events_observer.h"
 
 namespace reporting {
@@ -42,4 +43,14 @@ void FatalCrashEventsObserver::TestEnvironment::
   observer.SetInterruptedAfterEventObservedForTest(
       interrupted_after_event_observed);
 }
+
+// static
+size_t FatalCrashEventsObserver::TestEnvironment::GetLocalIdEntryQueueSize(
+    FatalCrashEventsObserver& observer) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(observer.sequence_checker_);
+  DCHECK_CALLED_ON_VALID_SEQUENCE(
+      observer.reported_local_id_manager_->sequence_checker_);
+  return observer.reported_local_id_manager_->local_id_entries_.size();
+}
+
 }  // namespace reporting
