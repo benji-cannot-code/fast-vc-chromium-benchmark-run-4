d5fd1d5c5b9d4ea878c1ef2d1094923979e5a9b6
Dana Fried <dfried@chromium.org>
2023-10-10 00:40:31 +0000

    Remove expired histograms
    Bug: 1486567
    Change-Id: I16f301e575764a0abce89fd539beb45d0bf44cb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4923079
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207375}

--- chrome/browser/ui/thumbnails/background_thumbnail_video_capturer.cc
+++ chrome/browser/ui/thumbnails/background_thumbnail_video_capturer.cc
@@ -98,7 +98,6 @@ void BackgroundThumbnailVideoCapturer::OnFrameCaptured(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   CHECK(video_capturer_);
-  const base::TimeTicks time_of_call = base::TimeTicks::Now();
 
   mojo::Remote<::viz::mojom::FrameSinkVideoConsumerFrameCallbacks>
       callbacks_remote(std::move(callbacks));
@@ -126,9 +125,6 @@ void BackgroundThumbnailVideoCapturer::OnFrameCaptured(
     return;
   }
 
-  if (num_received_frames_ == 0)
-    UMA_HISTOGRAM_TIMES("Tab.Preview.TimeToFirstUsableFrameAfterStartCapture",
-                        time_of_call - start_time_);
   TRACE_EVENT_INSTANT1("ui", "Tab.Preview.VideoCaptureFrameReceived",
                        TRACE_EVENT_SCOPE_THREAD, "frame_number",
                        num_received_frames_);
