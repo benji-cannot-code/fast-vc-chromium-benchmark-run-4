04ff513898781a9cac3ede75c52cb23448f5d25a
Andrew Grieve <agrieve@chromium.org>
2023-10-10 19:58:51 +0000

    google-java-format: Target java 11 with local override .class files
    This fixes google-java-foramt when a JAVA_HOME is set to JDK11.
    
    Also fixes recreate_chromium_overrides_jar.sh in a couple minor ways.
    
    Bug: 1462204
    Change-Id: Ie86455ddfc5378e33807864027d94e0c9e36d4df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4917050
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207854}

--- third_party/google-java-format/recreate_chromium_overrides_jar.sh
+++ third_party/google-java-format/recreate_chromium_overrides_jar.sh
@@ -1,10 +1,13 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/bin/bash
 cd $(dirname "$0")
+mkdir tmp
 ../jdk/current/bin/javac \
     -d tmp \
     --add-exports jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED \
     -cp google-java-format.jar \
+    -source 11 \
+    -target 11 \
     local_modifications/java/com/google/googlejavaformat/java/*.java
 rm chromium-overrides.jar
 (cd tmp && zip -rD0 ../chromium-overrides.jar *)
