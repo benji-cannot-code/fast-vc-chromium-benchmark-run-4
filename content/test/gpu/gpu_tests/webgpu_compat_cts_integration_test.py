218e1b07bd098a9489dcf723ab3c4fb3f0cbb113
Gregg Tavares <gman@chromium.org>
2023-10-10 18:37:59 +0000

    Make webgpu_compat_cts test use compat-expectations.txt
    Bug: chromium:1483449
    Change-Id: I371acbab12cf4a8f5c966d3aefacdbc0e7981be9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4912752
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Stephen White <senorblanco@chromium.org>
    Reviewed-by: Gregg Tavares <gman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207801}

--- content/test/gpu/gpu_tests/webgpu_compat_cts_integration_test.py
+++ content/test/gpu/gpu_tests/webgpu_compat_cts_integration_test.py
@@ -13,10 +13,9 @@ from gpu_tests import webgpu_cts_integration_test_base
 
 import gpu_path_util
 
-# TODO(crbug.com/dawn/1996): Change this to the actual compat expectation file
-# once it is available.
 EXPECTATIONS_FILE = os.path.join(gpu_path_util.CHROMIUM_SRC_DIR, 'third_party',
-                                 'dawn', 'webgpu-cts', 'expectations.txt')
+                                 'dawn', 'webgpu-cts',
+                                 'compat-expectations.txt')
 
 
 class WebGpuCompatCtsIntegrationTest(
@@ -37,7 +36,7 @@ class WebGpuCompatCtsIntegrationTest(
 
   @classmethod
   def ExpectationsFiles(cls) -> List[str]:
-    return []
+    return [EXPECTATIONS_FILE]
 
 
 def load_tests(_loader: unittest.TestLoader, _tests: Any,
