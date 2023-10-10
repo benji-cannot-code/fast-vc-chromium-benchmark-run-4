9596ffc5fe108c2454626ec6ded8097b777d0942
Mikel Astiz <mastiz@chromium.org>
2023-10-10 11:31:27 +0000

    Refactor predicate used to count passwords during clear browsing data
    No behavioral changes as the two implementations (before and after)
    achieve the exact same thing but IsSyncFeatureActiveIncludingPasswords()
    allows emphasizing that the dependency to sync-the-feature is desirable.
    
    Change-Id: I43ef34ac1a59a205c0bf0671cc019e3807c7db10
    Bug: 1488793
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4922949
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207574}

--- components/browsing_data/core/counters/passwords_counter.cc
+++ components/browsing_data/core/counters/passwords_counter.cc
@@ -24,19 +24,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace browsing_data {
 namespace {
 
-bool IsPasswordSyncEnabled(const syncer::SyncService* sync_service) {
-  if (!sync_service)
-    return false;
-  switch (password_manager::sync_util::GetPasswordSyncState(sync_service)) {
-    case password_manager::SyncState::kNotSyncing:
-    case password_manager::SyncState::kAccountPasswordsActiveNormalEncryption:
-    case password_manager::SyncState::
-        kAccountPasswordsActiveWithCustomPassphrase:
-      return false;
-    case password_manager::SyncState::kSyncingNormalEncryption:
-    case password_manager::SyncState::kSyncingWithCustomPassphrase:
-      return true;
-  }
+// This predicate is only about profile (non-account) passwords, so it is only
+// concerned about whether sync-the-feature is on or off. Account passwords are
+// counted separately.
+bool IsProfilePasswordSyncEnabled(const syncer::SyncService* sync_service) {
+  // TODO(crbug.com/1464264): Migrate away from `ConsentLevel::kSync` on desktop
+  // platforms, including APIs that depend on sync-the-feature.
+  return password_manager::sync_util::IsSyncFeatureActiveIncludingPasswords(
+      sync_service);
 }
 
 }  // namespace
@@ -224,7 +219,8 @@ const std::vector<std::string>& PasswordsCounter::account_domain_examples() {
 }
 
 void PasswordsCounter::OnInitialized() {
-  sync_tracker_.OnInitialized(base::BindRepeating(&IsPasswordSyncEnabled));
+  sync_tracker_.OnInitialized(
+      base::BindRepeating(&IsProfilePasswordSyncEnabled));
 }
 
 const char* PasswordsCounter::GetPrefName() const {
