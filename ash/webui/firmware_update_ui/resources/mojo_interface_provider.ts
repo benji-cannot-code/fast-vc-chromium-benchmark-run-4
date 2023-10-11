9a0ca34257dc3e760428b9ec647482e8a186183b
Ashley Prasad <ashleydp@google.com>
2023-10-11 16:56:57 +0000

    Firmware UI: Convert mojo_interface_provider to TS
    - Rename file from JS to TS and update BUILD.gn.
    - Remove closure type comments.
    
    Bug: b:302140708
    Test: browser_tests --gtest_filter=*FirmwareUpdate*
    Test: tast run -build=true ${DUT} firmwareupdate.*
    Change-Id: I10958a4d8374c691c9fcebf19b969111c95fbb0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4916200
    Reviewed-by: Longbo Wei <longbowei@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1208331}

--- ash/webui/firmware_update_ui/resources/mojo_interface_provider.ts
+++ ash/webui/firmware_update_ui/resources/mojo_interface_provider.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {assert} from 'chrome://resources/ash/common/assert.js';
+import {assert} from 'chrome://resources/js/assert_ts.js';
 
 import {fakeFirmwareUpdates} from './fake_data.js';
 import {FakeUpdateController} from './fake_update_controller.js';
@@ -18,45 +18,31 @@ import {InstallController, InstallControllerInterface, UpdateProvider, UpdatePro
 
 /**
  * If true this will replace UpdateProvider with a fake.
- * @type {boolean}
  */
 let useFakeProviders = false;
 
-/**
- * @type {?UpdateProviderInterface}
- */
-let updateProvider = null;
+let updateProvider: UpdateProviderInterface|null = null;
 
-/**
- * @type {?InstallControllerInterface}
- */
-let updateController = null;
+let updateController: InstallControllerInterface|null = null;
 
-/**
- * @param {boolean} value
- */
-export function setUseFakeProviders(value) {
+export function setUseFakeProviders(value: boolean): void {
   useFakeProviders = value;
 }
 
-/**
- * @param {!UpdateProviderInterface} testProvider
- */
-export function setUpdateProviderForTesting(testProvider) {
+export function setUpdateProviderForTesting(
+    testProvider: UpdateProviderInterface): void {
   updateProvider = testProvider;
 }
 
-/**
- * @param {!InstallControllerInterface} testUpdateController
- */
-export function setUpdateControllerForTesting(testUpdateController) {
+export function setUpdateControllerForTesting(
+    testUpdateController: InstallControllerInterface): void {
   updateController = testUpdateController;
 }
 
 /**
  * Sets up a FakeUpdateProvider to be used at runtime.
  */
-function setupFakeUpdateProvider() {
+function setupFakeUpdateProvider(): void {
   const provider = new FakeUpdateProvider();
 
   provider.setFakeFirmwareUpdates(fakeFirmwareUpdates);
@@ -66,14 +52,11 @@ function setupFakeUpdateProvider() {
 /**
  * Sets up a FakeUpdateController to be used at runtime.
  */
-function setupFakeUpdateController() {
+function setupFakeUpdateController(): void {
   setUpdateControllerForTesting(new FakeUpdateController());
 }
 
-/**
- * @return {!UpdateProviderInterface}
- */
-export function getUpdateProvider() {
+export function getUpdateProvider(): UpdateProviderInterface {
   if (!updateProvider) {
     if (useFakeProviders) {
       setupFakeUpdateProvider();
@@ -86,10 +69,7 @@ export function getUpdateProvider() {
   return updateProvider;
 }
 
-/**
- * @return {!InstallControllerInterface}
- */
-export function getUpdateController() {
+export function getUpdateController(): InstallControllerInterface {
   if (!updateController) {
     if (useFakeProviders) {
       setupFakeUpdateController();
