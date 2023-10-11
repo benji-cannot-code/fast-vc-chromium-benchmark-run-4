d0e493bef7c0cf43356f6f1a2c6ede7ab37ec137
Nicola Tommasi <tommasin@chromium.org>
2023-10-11 08:26:04 +0000

    Tracking Protection: Implement Clank onboarding Notice
    This CL implements the Clank onboarding notice for Tracking Protection. 
    The notice is a Clank Message and it's shown as part of the 2nd Chrome
    promotion run.
    This message will show indefinitely unless it is dismissed by user.
    
    UI screenshot: https://screenshot.googleplex.com/3uhQMZ763hKC4QK
    
    Bug: b:295927778
    Change-Id: I868a43748e6381e946baabea940ce7c2afbb096c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4916032
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1208133}

---
+++ chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/TrackingProtectionBridge.java
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.privacy_sandbox;
+
+import org.chromium.base.annotations.NativeMethods;
+
+/** Bridge, providing access to the native-side Tracking Protection configuration. */
+public class TrackingProtectionBridge {
+    public static boolean shouldShowOnboardingNotice() {
+        return TrackingProtectionBridgeJni.get().shouldShowOnboardingNotice();
+    }
+
+    @NativeMethods
+    public interface Natives {
+        boolean shouldShowOnboardingNotice();
+    }
+}
---
+++ chrome/browser/privacy_sandbox/android/javatests/src/org/chromium/chrome/browser/privacy_sandbox/FakeTrackingProtectionBridge.java
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.privacy_sandbox;
+
+/** Java implementation of TrackingProtectionBridge for testing. */
+public class FakeTrackingProtectionBridge implements TrackingProtectionBridge.Natives {
+
+    private boolean mShouldShowOnboardingNotice;
+
+    @Override
+    public boolean shouldShowOnboardingNotice() {
+        return mShouldShowOnboardingNotice;
+    }
+
+    public void setShouldShowOnboardingNotice(boolean shouldShowOnboardingNotice) {
+        mShouldShowOnboardingNotice = shouldShowOnboardingNotice;
+    }
+}
