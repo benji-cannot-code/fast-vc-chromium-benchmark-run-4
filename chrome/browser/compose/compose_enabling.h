99609a95b497853d965483ee5600042761fe6c95
Pete Williamson <petewil@chromium.org>
2023-10-11 19:14:05 +0000

    Compose Enabling - Make checks before enabling the compose feature
    Checks feature flag, signin status, MSBB, etc.
    
    Bug: b:300974056
    Change-Id: Ieb4d17d3c6d8884e9685649a707e43cc3cc4395e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4917025
    Commit-Queue: Peter Williamson <petewil@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208394}

---
+++ chrome/browser/compose/compose_enabling.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_COMPOSE_COMPOSE_ENABLING_H_
+#define CHROME_BROWSER_COMPOSE_COMPOSE_ENABLING_H_
+
+#include "chrome/browser/profiles/profile_manager.h"
+#include "components/signin/public/identity_manager/identity_manager.h"
+
+class ComposeEnabling {
+ public:
+  static bool IsEnabledForProfile(Profile* profile);
+
+ private:
+  friend class ComposeEnablingTest;
+  static bool IsEnabled(Profile* profile,
+                        signin::IdentityManager* identity_manager);
+};
+
+#endif  // CHROME_BROWSER_COMPOSE_COMPOSE_ENABLING_H_
