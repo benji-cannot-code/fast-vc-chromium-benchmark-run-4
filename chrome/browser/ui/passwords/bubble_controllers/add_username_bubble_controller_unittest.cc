e95032befe86ef6d626085ff6a20b52bf7361e21
Karol Sygiet <sygiet@google.com>
2023-10-11 08:27:14 +0000

    Add success metric for AddUsername bubble.
    This CL introduces metric measuring if username was filled manually
    into the textfield and then button "Add username" was pressed.
    
    Bug: 1479899
    Change-Id: Id9154deec9f56db4c0761634ac70002d4fbb0e64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4918974
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1208134}

--- chrome/browser/ui/passwords/bubble_controllers/add_username_bubble_controller_unittest.cc
+++ chrome/browser/ui/passwords/bubble_controllers/add_username_bubble_controller_unittest.cc
@@ -18,6 +18,8 @@ using ::testing::ReturnRef;
 namespace {
 constexpr char kUIDismissalReasonGeneralMetric[] =
     "PasswordManager.UIDismissalReason";
+constexpr char kUsernameAddedMetric[] =
+    "PasswordBubble.AddUsernameBubble.UsernameAdded";
 constexpr char16_t kUsername[] = u"Admin";
 constexpr char16_t kPassword[] = u"AdminPass";
 }  // namespace
@@ -72,6 +74,7 @@ TEST_F(AddUsernameBubbleControllerTest, Destroy) {
   histogram_tester.ExpectUniqueSample(
       kUIDismissalReasonGeneralMetric,
       password_manager::metrics_util::NO_DIRECT_INTERACTION, 1);
+  histogram_tester.ExpectUniqueSample(kUsernameAddedMetric, false, 1);
 }
 
 TEST_F(AddUsernameBubbleControllerTest, SavePassword) {
@@ -86,6 +89,7 @@ TEST_F(AddUsernameBubbleControllerTest, SavePassword) {
   histogram_tester.ExpectUniqueSample(
       kUIDismissalReasonGeneralMetric,
       password_manager::metrics_util::CLICKED_ACCEPT, 1);
+  histogram_tester.ExpectUniqueSample(kUsernameAddedMetric, true, 1);
 }
 
 TEST_F(AddUsernameBubbleControllerTest, Title) {
