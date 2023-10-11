22b2b6c7539d1e063fe43ddadd4003723bc28b82
Sky Malice <skym@chromium.org>
2023-10-11 19:03:14 +0000

    Empty HubToolbar component.
    Low-Coverage-Reason: No real logic to cover yet, tests later.
    Bug: 1491190
    Change-Id: I1c0f7468229f19d351a1959440c693ac89f02716
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4931061
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208376}

---
+++ chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubToolbarView.java
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.hub;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+/** Toolbar for the Hub. May contain a single or multiple rows, of which this view is the parent. */
+public class HubToolbarView extends LinearLayout {
+    /** Default {@link LinearLayout} constructor. */
+    public HubToolbarView(Context context) {
+        super(context);
+    }
+}
---
+++ chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubToolbarViewBinder.java
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.hub;
+
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel;
+
+/** Applies properties to the view that holds one pane at a time. */
+public class HubToolbarViewBinder {
+    /** Stateless propagation of properties. */
+    public static void bind(PropertyModel model, HubToolbarView view, PropertyKey propertyKey) {}
+}
