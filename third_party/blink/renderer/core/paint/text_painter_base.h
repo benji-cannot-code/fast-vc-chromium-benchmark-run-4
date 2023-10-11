d7cd4fa56827cd0773225698e673ac84dd166979
Fredrik Söderquist <fs@opera.com>
2023-10-11 17:02:37 +0000

    [RemoveLegacy] Drop TextPainterBase::ellipsis_offset_
    Unused (always 0). Clean up code in NGTextPainter::PaintInternal() that
    was using it.
    
    Bug: 1229581
    Change-Id: I966647e5d8eea4452afef4181cd333c1a26a744b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4931835
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208336}

--- third_party/blink/renderer/core/paint/text_painter_base.h
+++ third_party/blink/renderer/core/paint/text_painter_base.h
@@ -61,7 +61,6 @@ class CORE_EXPORT TextPainterBase {
   const NGInlinePaintContext* InlineContext() const { return inline_context_; }
 
   void SetEmphasisMark(const AtomicString&, TextEmphasisPosition);
-  void SetEllipsisOffset(int offset) { ellipsis_offset_ = offset; }
 
   enum ShadowMode { kBothShadowsAndTextProper, kShadowsOnly, kTextProperOnly };
   static void UpdateGraphicsContext(GraphicsContext&,
@@ -116,7 +115,6 @@ class CORE_EXPORT TextPainterBase {
   const LineRelativeRect text_frame_rect_;
   AtomicString emphasis_mark_;
   int emphasis_mark_offset_ = 0;
-  int ellipsis_offset_ = 0;
   const bool horizontal_;
 };
 
