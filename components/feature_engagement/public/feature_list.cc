47f27140ef70188bc754a077de0260353e937d67
Jan Keitel <jkeitel@google.com>
2023-10-11 21:43:51 +0000

    Add IPH feature for Compose new badge.
    This CL adds an IPH feature to control when a "NEW" badge will be shown
    as part of the Compose entry in the Autofill popup.
    
    Bug: 1487965, b/303418910
    Change-Id: Iefbcb95aac63815bc635562ee4f9da775919fa88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4930908
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1208508}

--- components/feature_engagement/public/feature_list.cc
+++ components/feature_engagement/public/feature_list.cc
@@ -147,6 +147,7 @@ const base::Feature* const kAllFeatures[] = {
     &kIPHBatterySaverModeFeature,
     &kIPHCompanionSidePanelFeature,
     &kIPHCompanionSidePanelRegionSearchFeature,
+    &kIPHComposeNewBadgeFeature,
     &kIPHDesktopTabGroupsNewGroupFeature,
     &kIPHDesktopCustomizeChromeFeature,
     &kIPHDesktopCustomizeChromeRefreshFeature,
