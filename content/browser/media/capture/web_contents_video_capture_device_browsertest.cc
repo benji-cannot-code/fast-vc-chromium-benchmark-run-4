85152c0ebbfe92112c6b51e5fe8e7f870a942539
Colin Blundell <blundell@chromium.org>
2023-10-11 08:53:02 +0000

    [Graphite] Disable browsertest check requiring ZeroCopyTabCapture
    ZeroCopyTabCapture is not yet enabled for Graphite. This CL disables a
    check in the WebContentsVideoCapture browsertests that assume the
    presence of ZeroCopyTabCapture. This will unblock us turning Graphite
    on in fieldtrial_testing_config.json for beta. We are going to enable
    ZeroCopyTabCapture and re-enable this browsertest check before taking
    Graphite to stable.
    
    Bug: 1450879, 1480445
    Change-Id: I320586f8e5d3cabfa997f129495531d917c45f73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4921809
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208141}

--- content/browser/media/capture/web_contents_video_capture_device_browsertest.cc
+++ content/browser/media/capture/web_contents_video_capture_device_browsertest.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 #include "content/shell/browser/shell.h"
+#include "gpu/config/gpu_finch_features.h"
 #include "media/base/media_switches.h"
 #include "media/base/video_frame.h"
 #include "media/base/video_types.h"
@@ -640,7 +641,12 @@ IN_PROC_BROWSER_TEST_P(WebContentsVideoCaptureDeviceBrowserTestP,
 
   capture_stack()->SetFrameReceivedCallback(base::BindRepeating(
       [](media::VideoPixelFormat expected_format, media::VideoFrame* frame) {
-        EXPECT_EQ(frame->format(), expected_format);
+        // TODO(crbug.com/1452092): Remove this when zero-copy tab capture works
+        // with Graphite.
+        if (!features::IsSkiaGraphiteEnabled(
+                base::CommandLine::ForCurrentProcess())) {
+          EXPECT_EQ(frame->format(), expected_format);
+        }
       },
       expected_format));
 
