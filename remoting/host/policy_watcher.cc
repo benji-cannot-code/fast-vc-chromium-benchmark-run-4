82cb823cf485aba6eebca857afb5717287425820
Lei Zhang <thestig@chromium.org>
2023-10-11 19:07:47 +0000

    Use `policy::kPolicyPath` in remoting::PolicyWatcher
    Instead of hard coding /etc/opt/chrome/policies as the policies
    directory, use `policy::kPolicyPath` instead. The policy constant is not
    tied to the Chrome branding.
    
    Bug: 1491608
    Change-Id: Id8c83274df632f2681db2d3f4caa7bd7f8a4c868
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4917162
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1208384}

--- remoting/host/policy_watcher.cc
+++ remoting/host/policy_watcher.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/preferences_mac.h"
 #elif BUILDFLAG(IS_POSIX) && !BUILDFLAG(IS_ANDROID)
 #include "components/policy/core/common/config_dir_policy_loader.h"
+#include "components/policy/core/common/policy_paths.h"  // nogncheck
 #endif
 
 namespace remoting {
@@ -464,8 +465,7 @@ std::unique_ptr<PolicyWatcher> PolicyWatcher::CreateWithTaskRunner(
       new MacPreferences(), bundle_id);
 #elif BUILDFLAG(IS_POSIX) && !BUILDFLAG(IS_ANDROID)
   policy_loader = std::make_unique<policy::ConfigDirPolicyLoader>(
-      file_task_runner,
-      base::FilePath(FILE_PATH_LITERAL("/etc/opt/chrome/policies")),
+      file_task_runner, base::FilePath(policy::kPolicyPath),
       policy::POLICY_SCOPE_MACHINE);
 #elif BUILDFLAG(IS_ANDROID)
   NOTIMPLEMENTED();
