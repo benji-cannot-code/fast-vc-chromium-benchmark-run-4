adccc460b1c0a4abe29d18b751f3c682672bdcc3
Dana Fried <dfried@chromium.org>
2023-10-11 20:40:27 +0000

    [Test Automation] Add a way to unobserve states.
    Probably won't be needed a lot, but useful if you want to install an
    observer that you know will become invalid at some point and it's
    easier to uninstall the observer rather than hardening the observer
    against a UAF.
    
    This also adds documentation, including some missing documentation from an upstream CL.
    
    Change-Id: I69dc8591a5269b38ee9c64187af6ab8a1d034e7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4928454
    Reviewed-by: Kuan Huang <kuanhuang@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208461}

--- ui/base/interaction/interactive_test_internal.h
+++ ui/base/interaction/interactive_test_internal.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/functional/callback_helpers.h"
 #include "base/functional/invoke.h"
+#include "base/gtest_prod_util.h"
 #include "base/logging.h"
 #include "base/no_destructor.h"
 #include "base/strings/strcat.h"
@@ -35,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui::test {
 
 class InteractiveTestApi;
+class InteractiveTestTest;
 
 namespace internal {
 
@@ -108,11 +110,17 @@ class InteractiveTestPrivate {
   TrackedElement* GetPivotElement(ElementContext context) const;
 
   // Adds `state_observer` and associates it with an element with identifier
-  // `id` and context `context`.
+  // `id` and context `context`. Must be unique in its context.
+  // Returns true on success.
   template <typename Observer, typename V = Observer::ValueType>
-  void AddStateObserver(ElementIdentifier id,
+  bool AddStateObserver(ElementIdentifier id,
                         ElementContext context,
                         std::unique_ptr<Observer> state_observer);
+
+  // Removes `StateObserver` with identifier `id` in `context`; if the context
+  // is null, assumes there is exactly one matching observer in some context.
+  // Returns true on success.
+  bool RemoveStateObserver(ElementIdentifier id, ElementContext context);
 
   // Call this method during test SetUp(), or SetUpOnMainThread() for browser
   // tests.
@@ -141,6 +149,7 @@ class InteractiveTestPrivate {
   static MultiStep PostTask(const base::StringPiece& description, T&& task);
 
  private:
+  friend class ui::test::InteractiveTestTest;
   friend class ui::test::InteractiveTestApi;
 
   // Prepare for a sequence to start.
@@ -301,13 +310,22 @@ bool MatchAndExplain(const base::StringPiece& test_name,
 }
 
 template <typename Observer, typename V>
-void InteractiveTestPrivate::AddStateObserver(
+bool InteractiveTestPrivate::AddStateObserver(
     ElementIdentifier id,
     ElementContext context,
     std::unique_ptr<Observer> state_observer) {
+  CHECK(id);
+  CHECK(context);
+  for (const auto& existing : state_observer_elements_) {
+    if (existing->identifier() == id && existing->context() == context) {
+      LOG(ERROR) << "AddStateObserver: Duplicate observer added for " << id;
+      return false;
+    }
+  }
   state_observer_elements_.emplace_back(
       std::make_unique<StateObserverElementT<V>>(id, context,
                                                  std::move(state_observer)));
+  return true;
 }
 
 // static
