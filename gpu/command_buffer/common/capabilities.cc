69f711c9a5641255812933abaa80080c44e8e55a
Saifuddin Hitawala <hitawala@chromium.org>
2023-10-11 13:52:52 +0000

    Multiplanar: Move ShaderPrecision and friends to GLCaps
    As part of moving GL specific capabilities to gpu::GLCapabilities,
    move vertex and fragment shader precisions to GLCapabilities.
    
    Bug: 1465939
    Change-Id: Ia777cabddf3d0cc9b78010b6449a1ef63e732d9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4919286
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208224}

--- gpu/command_buffer/common/capabilities.cc
+++ gpu/command_buffer/common/capabilities.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gpu {
 
-Capabilities::PerStagePrecisions::PerStagePrecisions() = default;
-
 Capabilities::Capabilities() = default;
 
 Capabilities::Capabilities(const Capabilities& other) = default;
@@ -20,5 +18,7 @@ GLCapabilities::GLCapabilities() = default;
 GLCapabilities::GLCapabilities(const GLCapabilities& other) = default;
 
 GLCapabilities::~GLCapabilities() = default;
+
+GLCapabilities::PerStagePrecisions::PerStagePrecisions() = default;
 
 }  // namespace gpu
