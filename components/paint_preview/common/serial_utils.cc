10f3499d2c409bc52358fbac3a44912cdb766d26
Kevin Lubick <kjlubick@google.com>
2023-10-11 18:58:21 +0000

    Register deserialization of Slugs in SkPicture
    In https://skia-review.googlesource.com/c/skia/+/755550 we deprecated
    the default behavior of SkPictures deserializing Slugs (currently
    a gpu-only type) so serialization works without a subtle link to
    a GPU backend.
    
    This means clients who need to deserialize Slugs in SkPictures
    need to register that behavior explicitly, using a provided
    helper function.
    
    The change does *not* impact directly serializing Slugs, only
    in situations where an SkPicture would have a Slug inside it
    (e.g. a call to drawSlug).
    
    Bug: b:40045064
    Change-Id: Icb9e9e520aa70b2b0b03512eb7cd0e0a976990ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4885951
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Kevin Lubick <kjlubick@chromium.org>
    Reviewed-by: Brian Osman <brianosman@google.com>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208367}

--- components/paint_preview/common/serial_utils.cc
+++ components/paint_preview/common/serial_utils.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkPictureRecorder.h"
 #include "third_party/skia/include/core/SkString.h"
 #include "third_party/skia/include/encode/SkPngEncoder.h"
+#include "third_party/skia/include/private/chromium/Slug.h"
 
 namespace paint_preview {
 
@@ -296,6 +297,7 @@ SkDeserialProcs MakeDeserialProcs(DeserializationContext* ctx) {
   procs.fPictureProc = DeserializePictureAsRectData;
   procs.fPictureCtx = ctx;
   procs.fImageProc = DeserializeImage;
+  sktext::gpu::Slug::AddDeserialProcs(&procs, nullptr);
   return procs;
 }
 
