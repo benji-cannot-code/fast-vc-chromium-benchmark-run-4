d8f846a481784b526c1ecc52963d81dfb8e586c3
Sorin Jianu <sorin@chromium.org>
2023-10-11 00:02:56 +0000

    updater: use AppServerWin members from the main sequence only.
    Since AppServerWin::Stop can reset these members on the main
    sequence at any time, accessing them from other sequences is
    a data race.
    
    Bug: 1469358
    Change-Id: I77245e93480ac1d1e4f31a38c5b5c4b23daa43fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4916418
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207996}

--- chrome/updater/app/app_server_win.cc
+++ chrome/updater/app/app_server_win.cc
@@ -253,6 +253,10 @@ AppServerWin::~AppServerWin() {
   NOTREACHED();  // The instance of this class is a leaky singleton.
 }
 
+void AppServerWin::PostRpcTask(base::OnceClosure task) {
+  GetAppServerWinInstance()->PostRpcTaskOnMainSequence(std::move(task));
+}
+
 void AppServerWin::Stop() {
   VLOG(2) << __func__ << ": COM server is shutting down.";
   UnregisterClassObjects();
@@ -263,6 +267,10 @@ void AppServerWin::Stop() {
                                 this_server->update_service_internal_ = nullptr;
                                 this_server->Shutdown(0);
                               }));
+}
+
+void AppServerWin::PostRpcTaskOnMainSequence(base::OnceClosure task) {
+  main_task_runner_->PostTask(FROM_HERE, std::move(task));
 }
 
 bool AppServerWin::RestoreComInterfaces(bool is_internal) {
