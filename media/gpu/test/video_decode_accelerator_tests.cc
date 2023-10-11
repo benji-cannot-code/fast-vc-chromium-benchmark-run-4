b5b706ee9777a1547071c1b1ecb22f4b3d8fe79e
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2023-10-11 19:57:12 +0000

    Revert "[media] Take MediaLog by unique_ptr in Dav1dVideoDecoder"
    This reverts commit cad72bc7a25ad4e05063b574864aedce2d9d58a2.
    
    Reason for revert:
    LUCI Bisection identified this CL as the culprit of a build failure. See the analysis: https://luci-bisection.appspot.com/analysis/b/8767505566136479585
    
    Sample failed build: https://ci.chromium.org/b/8767505566136479585
    
    If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fluci-bisection.appspot.com%2Fanalysis%2Fb%2F8767505566136479585&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F4928568
    
    Original change's description:
    > [media] Take MediaLog by unique_ptr in Dav1dVideoDecoder
    >
    > Dav1dVideoDecoder can temporarily outlive the owner of the MediaLog
    > because its destruction is posted. Switching to an owned MediaLog avoids
    > a dangling pointer.
    >
    > Bug: 1444624
    > Change-Id: Iabd4dfedbb393692a40843d90e85a782083ee7c8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4928568
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Commit-Queue: Dan Sanders <sandersd@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1208402}
    >
    
    Bug: 1444624
    Change-Id: Ib76fdced52294a7e4eeed78fa47009794d7f1fde
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4930541
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1208433}

--- media/gpu/test/video_decode_accelerator_tests.cc
+++ media/gpu/test/video_decode_accelerator_tests.cc
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <limits>
-#include <memory>
 
 #include "base/base_switches.h"
 #include "base/command_line.h"
@@ -259,7 +258,7 @@ class VideoDecoderTest : public ::testing::Test {
     }
 
     Dav1dVideoDecoder decoder(
-        std::make_unique<NullMediaLog>(),
+        /*media_log=*/nullptr,
         OffloadableVideoDecoder::OffloadState::kOffloaded);
     VideoDecoderConfig decoder_config(
         video->Codec(), video->Profile(),
