1ca9720ae685f20e0c6467ed5827d06d92953307
ChangSeok Oh <changseok.oh@bytedance.com>
2023-10-11 16:01:50 +0000

    The font-variant shorthand is incorrectly serialized for none.
    Font-variant: none should return 'none,' but Blink spelled out its
    longhand properties. Besides, it serialized longhands where
    font-variant-ligatures: none along with other non-normal longhands. This
    behavior does not meet the spec [1]. If the font-variant does not
    exactly represent all values of its longhand properties, it should
    return an empty string.
    
    [1] https://drafts.csswg.org/cssom-1/#serializing-css-values
    
    Test:
    external/wpt/css/css-fonts/parsing/font-variant-valid.html
    external/wpt/css/cssom/font-variant-shorthand-serialization.html
    fast/css/font-variant-shorthand-from-longhands.html
    virtual/text-antialias/font-features/font-variant-shorthand.html
    
    Bug: 1412086
    Change-Id: I289a110e3d77ce9bb9bc288e6d3566b953c1e559
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4880723
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: ChangSeok Oh <changseok.oh@bytedance.com>
    Cr-Commit-Position: refs/heads/main@{#1208291}

--- third_party/blink/renderer/core/css/properties/computed_style_utils.cc
+++ third_party/blink/renderer/core/css/properties/computed_style_utils.cc
@@ -3870,7 +3870,8 @@ CSSValue* ComputedStyleUtils::ValuesForFontVariantProperty(
   enum VariantShorthandCases {
     kAllNormal,
     kNoneLigatures,
-    kConcatenateNonNormal
+    kConcatenateNonNormal,
+    kEmptyString
   };
   StylePropertyShorthand shorthand = fontVariantShorthand();
   VariantShorthandCases shorthand_case = kAllNormal;
@@ -3887,7 +3888,8 @@ CSSValue* ComputedStyleUtils::ValuesForFontVariantProperty(
       shorthand_case = kNoneLigatures;
     } else if (!(identifier_value &&
                  identifier_value->GetValueID() == CSSValueID::kNormal)) {
-      shorthand_case = kConcatenateNonNormal;
+      shorthand_case = shorthand_case == kNoneLigatures ? kEmptyString
+                                                        : kConcatenateNonNormal;
       break;
     }
   }
@@ -3915,6 +3917,8 @@ CSSValue* ComputedStyleUtils::ValuesForFontVariantProperty(
       }
       return list;
     }
+    case kEmptyString:
+      return nullptr;
     default:
       NOTREACHED();
       return nullptr;
