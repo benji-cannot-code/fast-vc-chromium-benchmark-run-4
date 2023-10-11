03fe6de9382dfb1679aebb556d7f92303c5c8e47
Ian Vollick <vollick@chromium.org>
2023-10-11 23:19:00 +0000

    [ios] Avoid coercing nullptr into gfx::NativeWindow
    In crrev.com/c/4429239 nullptr was returned for a non-existent window
    which doesn't work with the type's definition on iOS (and uses an
    constructor that has a comment about wanting to be explicit on mac).
    
    Bug: 1049261
    Change-Id: I9b9e4dd7c3506c36c0b6849a39129bb567a74f5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4929929
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208561}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -3022,7 +3022,8 @@ void RenderFrameHostImpl::AccessibilityHitTest(
 
 gfx::NativeWindow RenderFrameHostImpl::GetTopLevelNativeWindow() {
   WebContents* web_contents = WebContents::FromRenderFrameHost(this);
-  return web_contents ? web_contents->GetTopLevelNativeWindow() : nullptr;
+  return web_contents ? web_contents->GetTopLevelNativeWindow()
+                      : gfx::NativeWindow();
 }
 
 bool RenderFrameHostImpl::AccessibilityIsRootFrame() const {
