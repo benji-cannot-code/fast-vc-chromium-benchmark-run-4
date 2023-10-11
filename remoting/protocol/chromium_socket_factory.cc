b5622c115401751ce39311be9d23fb14b520df12
Harald Alvestrand <hta@chromium.org>
2023-10-11 20:11:06 +0000

    Convert remoting remainders to new DNS resolver interface.
    Bug: webrtc:12598
    Change-Id: I01b710b3df58e5d16a4daa2134d6980da55528e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4931488
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Auto-Submit: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208443}

--- remoting/protocol/chromium_socket_factory.cc
+++ remoting/protocol/chromium_socket_factory.cc
@@ -27,8 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/protocol/socket_util.h"
 #include "remoting/protocol/stream_packet_socket.h"
 #include "third_party/webrtc/media/base/rtp_utils.h"
+#include "third_party/webrtc/rtc_base/async_dns_resolver.h"
 #include "third_party/webrtc/rtc_base/async_packet_socket.h"
-#include "third_party/webrtc/rtc_base/async_resolver.h"
 #include "third_party/webrtc/rtc_base/net_helpers.h"
 #include "third_party/webrtc/rtc_base/socket.h"
 
@@ -454,9 +454,9 @@ rtc::AsyncPacketSocket* ChromiumPacketSocketFactory::CreateClientTcpSocket(
   return socket.release();
 }
 
-rtc::AsyncResolverInterface*
-ChromiumPacketSocketFactory::CreateAsyncResolver() {
-  return new rtc::AsyncResolver();
+std::unique_ptr<webrtc::AsyncDnsResolverInterface>
+ChromiumPacketSocketFactory::CreateAsyncDnsResolver() {
+  return std::make_unique<webrtc::AsyncDnsResolver>();
 }
 
 }  // namespace remoting::protocol
--- remoting/test/fake_socket_factory.cc
+++ remoting/test/fake_socket_factory.cc
@@ -267,7 +267,8 @@ rtc::AsyncPacketSocket* FakePacketSocketFactory::CreateClientTcpSocket(
   return nullptr;
 }
 
-rtc::AsyncResolverInterface* FakePacketSocketFactory::CreateAsyncResolver() {
+std::unique_ptr<webrtc::AsyncDnsResolverInterface>
+FakePacketSocketFactory::CreateAsyncDnsResolver() {
   return nullptr;
 }
 
