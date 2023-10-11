2635c5111d42adadc77a3dbd344719759bc1a7da
Jérôme Lebel <jlebel@chromium.org>
2023-10-11 19:02:06 +0000

    [iOS][UNOp2] Record metrics and preference for UpgradePromo (re-FRE)
    ** Issue:
    When UNOp2 is enabled, TwoScreensSigninCoordinator in charge to show
    the UpgradePromo, using SigninScreenCoordinator and
    HistorySyncCoordinator.
    SigninScreenMediator uses UserSigninLogger for the upgrade promo
    instead of UpgradeSigninLogger.
    Also when the user is signed in, HistorySyncCoordinator doesn't use
    records any metrics/prefs related to the UpgradePromo.
    
    ** Fix:
    This patch updates TwoScreensSigninCoordinator to call:
      on start:
    +[UserSigninLogger logSigninStartedWithAccessPoint:
                                 accountManagerService:]
      on finish:
    +[UserSigninLogger logSigninCompletedWithResult:
                                       addedAccount:]
    Those 2 methods are only called when the access point is
    ACCESS_POINT_SIGNIN_PROMO.
    
    EGTests are done in crrev.com/c/4931414.
    
    Fixed: 1491096
    Change-Id: Iad60371ab7c4e25d933da8b25de47912b63c7010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4925234
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208374}

--- ios/chrome/browser/ui/authentication/signin/user_signin/logging/upgrade_signin_logger.mm
+++ ios/chrome/browser/ui/authentication/signin/user_signin/logging/upgrade_signin_logger.mm
@@ -43,17 +43,17 @@ typedef NS_ENUM(NSUInteger, UserSigninPromoAction) {
 
 #pragma mark - Public
 
-- (void)logSigninStarted {
-  [super logSigninStarted];
-  if (!self.accountManagerService) {
++ (void)logSigninStartedWithAccessPoint:(signin_metrics::AccessPoint)accessPoint
+                  accountManagerService:
+                      (ChromeAccountManagerService*)accountManagerService {
+  if (!accountManagerService) {
     return;
   }
-
-  RecordSigninUserActionForAccessPoint(self.accessPoint);
+  RecordSigninUserActionForAccessPoint(accessPoint);
 
   // Records in user defaults that the promo has been shown as well as the
   // number of times it's been displayed.
-  signin::RecordUpgradePromoSigninStarted(self.accountManagerService,
+  signin::RecordUpgradePromoSigninStarted(accountManagerService,
                                           version_info::GetVersion());
   NSUserDefaults* standardDefaults = [NSUserDefaults standardUserDefaults];
   int promoSeenCount =
@@ -62,17 +62,13 @@ typedef NS_ENUM(NSUInteger, UserSigninPromoAction) {
   [standardDefaults setInteger:promoSeenCount
                         forKey:kDisplayedSSORecallPromoCountKey];
 
-  NSArray* identities = self.accountManagerService->GetAllIdentities();
+  NSArray* identities = accountManagerService->GetAllIdentities();
   UMA_HISTOGRAM_COUNTS_100(kUMASSORecallAccountsAvailable, [identities count]);
   UMA_HISTOGRAM_COUNTS_100(kUMASSORecallPromoSeenCount, promoSeenCount);
 }
 
-- (void)logSigninCompletedWithResult:(SigninCoordinatorResult)signinResult
-                        addedAccount:(BOOL)addedAccount
-               advancedSettingsShown:(BOOL)advancedSettingsShown {
-  [super logSigninCompletedWithResult:signinResult
-                         addedAccount:addedAccount
-                advancedSettingsShown:advancedSettingsShown];
++ (void)logSigninCompletedWithResult:(SigninCoordinatorResult)signinResult
+                        addedAccount:(BOOL)addedAccount {
   switch (signinResult) {
     case SigninCoordinatorResultSuccess: {
       UserSigninPromoAction promoAction = addedAccount
@@ -94,6 +90,23 @@ typedef NS_ENUM(NSUInteger, UserSigninPromoAction) {
       break;
     }
   }
+}
+
+- (void)logSigninStarted {
+  [super logSigninStarted];
+  [UpgradeSigninLogger
+      logSigninStartedWithAccessPoint:self.accessPoint
+                accountManagerService:self.accountManagerService];
+}
+
+- (void)logSigninCompletedWithResult:(SigninCoordinatorResult)signinResult
+                        addedAccount:(BOOL)addedAccount
+               advancedSettingsShown:(BOOL)advancedSettingsShown {
+  [super logSigninCompletedWithResult:signinResult
+                         addedAccount:addedAccount
+                advancedSettingsShown:advancedSettingsShown];
+  [UpgradeSigninLogger logSigninCompletedWithResult:signinResult
+                                       addedAccount:addedAccount];
 }
 
 @end
