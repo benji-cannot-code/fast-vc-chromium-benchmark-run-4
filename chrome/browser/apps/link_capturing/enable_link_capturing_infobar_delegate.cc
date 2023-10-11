3c1c7d21c12afec26b66aa84f5443d7bfb6360d7
Daniel Murphy <dmurph@chromium.org>
2023-10-11 00:55:15 +0000

    [dPWA] Adding command for enabling user link capturing
    This CL adds a job and scheduler method for changing the link capturing
    option for an app and disabling that option for any apps with the same
    scope.
    
    Bug: b/302022855
    Change-Id: I442902a9b30ed412a52193713354b4d5757f126a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4908292
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208020}

--- chrome/browser/apps/link_capturing/enable_link_capturing_infobar_delegate.cc
+++ chrome/browser/apps/link_capturing/enable_link_capturing_infobar_delegate.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/functional/callback_helpers.h"
 #include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
 #include "base/numerics/clamped_math.h"
@@ -69,24 +70,6 @@ base::Value IncrementDismissCount(webapps::AppId app_id,
   app->SetSupportedLinksOfferDismissCount(new_count);
 
   debug_result.Set("supported_links_offer_dismiss_count", new_count);
-  return base::Value(std::move(debug_result));
-}
-
-base::Value EnableUserLinkCapturing(webapps::AppId app_id,
-                                    web_app::AppLock& app_lock) {
-  web_app::ScopedRegistryUpdate update = app_lock.sync_bridge().BeginUpdate();
-  web_app::WebApp* app = update->UpdateApp(app_id);
-
-  base::Value::Dict debug_result;
-  debug_result.Set("app_id", app_id);
-  if (!app) {
-    debug_result.Set("error", "AppId does not exist.");
-    debug_result.Set("user_link_capturing_set", false);
-    return base::Value(std::move(debug_result));
-  }
-  app->SetIsUserSelectedAppForSupportedLinks(true);
-
-  debug_result.Set("user_link_capturing_set", true);
   return base::Value(std::move(debug_result));
 }
 }  // namespace
@@ -223,10 +206,8 @@ bool EnableLinkCapturingInfoBarDelegate::Accept() {
   action_taken_ = true;
   base::RecordAction(
       base::UserMetricsAction("LinkCapturingAcceptedFromInfoBar"));
-  provider_->scheduler().ScheduleCallbackWithLock(
-      "EnableUserLinkCapturing",
-      std::make_unique<web_app::AppLockDescription>(app_id_),
-      base::BindOnce(&EnableUserLinkCapturing, app_id_));
+  provider_->scheduler().SetAppCapturesSupportedLinksDisableOverlapping(
+      app_id_, true, base::DoNothing());
   return true;
 }
 
