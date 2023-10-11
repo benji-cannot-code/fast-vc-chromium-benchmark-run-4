54fdf315832454b9c775133b46f3b6c4a4164cfd
Dan Sanders <sandersd@chromium.org>
2023-10-11 19:19:40 +0000

    [media] Take MediaLog by unique_ptr in Dav1dVideoDecoder
    Dav1dVideoDecoder can temporarily outlive the owner of the MediaLog
    because its destruction is posted. Switching to an owned MediaLog avoids
    a dangling pointer.
    
    Bug: 1444624
    Change-Id: Iabd4dfedbb393692a40843d90e85a782083ee7c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4928568
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208402}

--- media/filters/dav1d_video_decoder_unittest.cc
+++ media/filters/dav1d_video_decoder_unittest.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "media/base/decoder_buffer.h"
 #include "media/base/limits.h"
-#include "media/base/mock_media_log.h"
+#include "media/base/media_util.h"
 #include "media/base/test_data_util.h"
 #include "media/base/test_helpers.h"
 #include "media/base/video_frame.h"
@@ -40,7 +40,8 @@ MATCHER(ContainsDecoderErrorLog, "") {
 class Dav1dVideoDecoderTest : public testing::Test {
  public:
   Dav1dVideoDecoderTest()
-      : decoder_(std::make_unique<Dav1dVideoDecoder>(&media_log_)),
+      : decoder_(std::make_unique<Dav1dVideoDecoder>(
+            std::make_unique<NullMediaLog>())),
         i_frame_buffer_(ReadTestDataFile("av1-I-frame-320x240")) {}
 
   Dav1dVideoDecoderTest(const Dav1dVideoDecoderTest&) = delete;
@@ -188,8 +189,6 @@ class Dav1dVideoDecoderTest : public testing::Test {
 
   MOCK_METHOD1(DecodeDone, void(DecoderStatus));
 
-  testing::StrictMock<MockMediaLog> media_log_;
-
   base::test::SingleThreadTaskEnvironment task_environment_;
   std::unique_ptr<Dav1dVideoDecoder> decoder_;
 
@@ -279,8 +278,8 @@ TEST_F(Dav1dVideoDecoderTest, DecodeFrame_LargerWidth) {
 // Decode a VP9 frame which should trigger a decoder error.
 TEST_F(Dav1dVideoDecoderTest, DecodeFrame_Error) {
   Initialize();
-  EXPECT_MEDIA_LOG(ContainsDecoderErrorLog());
-  DecodeSingleFrame(ReadTestDataFile("vp9-I-frame-320x240"));
+  EXPECT_FALSE(
+      DecodeSingleFrame(ReadTestDataFile("vp9-I-frame-320x240")).is_ok());
 }
 
 // Test resetting when decoder has initialized but not decoded.
