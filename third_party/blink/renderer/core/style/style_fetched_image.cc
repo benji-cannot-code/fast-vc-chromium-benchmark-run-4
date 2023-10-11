fd0f08ad8a6f50612e1986b3134eff96d1f84a55
Fredrik Söderquist <fs@opera.com>
2023-10-11 22:21:39 +0000

    Fix resolving of 'auto' for background-size
    BackgroundImageGeometry::CalculateFillTileSize() was not distinguishing
    between "natural ratio" and "natural size" properly, and would thus
    compute incorrect tile sizes in certain cases.
    
    Reimplement the background (tile) sizing step to better follow the
    letter of the spec[1] using the new function GetNaturalSizingInfo()
    on StyleImage.
    
    [1] https://drafts.csswg.org/css-backgrounds/#background-size
    
    Bug: 942975
    Change-Id: Iac5e642ae1ab06fe1db52277aadbe9c98a28f39a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4916298
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208528}

--- third_party/blink/renderer/core/style/style_fetched_image.cc
+++ third_party/blink/renderer/core/style/style_fetched_image.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_client.h"
 #include "third_party/blink/renderer/core/frame/settings.h"
-#include "third_party/blink/renderer/core/layout/intrinsic_sizing_info.h"
 #include "third_party/blink/renderer/core/loader/resource/image_resource_content.h"
 #include "third_party/blink/renderer/core/paint/timing/image_element_timing.h"
 #include "third_party/blink/renderer/core/paint/timing/paint_timing.h"
@@ -150,17 +149,23 @@ bool StyleFetchedImage::IsAccessAllowed(String& failing_url) const {
   return false;
 }
 
-gfx::SizeF StyleFetchedImage::ImageSize(
-    float multiplier,
-    const gfx::SizeF& default_object_size,
-    RespectImageOrientationEnum respect_orientation) const {
+float StyleFetchedImage::ApplyImageResolution(float multiplier) const {
   const Image& image = *image_->GetImage();
   if (image.IsBitmapImage() && override_image_resolution_ > 0.0f) {
     multiplier /= override_image_resolution_;
   } else if (image_->HasDevicePixelRatioHeaderValue()) {
     multiplier /= image_->DevicePixelRatioHeaderValue();
   }
+  return multiplier;
+}
 
+gfx::SizeF StyleFetchedImage::ImageSize(
+    float multiplier,
+    const gfx::SizeF& default_object_size,
+    RespectImageOrientationEnum respect_orientation) const {
+  multiplier = ApplyImageResolution(multiplier);
+
+  const Image& image = *image_->GetImage();
   gfx::SizeF size;
   if (auto* svg_image = DynamicTo<SVGImage>(image)) {
     const gfx::SizeF unzoomed_default_object_size =
@@ -171,6 +176,26 @@ gfx::SizeF StyleFetchedImage::ImageSize(
         image.Size(ForceOrientationIfNecessary(respect_orientation)));
   }
   return ApplyZoom(size, multiplier);
+}
+
+IntrinsicSizingInfo StyleFetchedImage::GetNaturalSizingInfo(
+    float multiplier,
+    RespectImageOrientationEnum respect_orientation) const {
+  const Image& image = *image_->GetImage();
+  IntrinsicSizingInfo intrinsic_sizing_info;
+  if (auto* svg_image = DynamicTo<SVGImage>(image)) {
+    svg_image->GetIntrinsicSizingInfo(intrinsic_sizing_info);
+  } else {
+    gfx::SizeF size(
+        image.Size(ForceOrientationIfNecessary(respect_orientation)));
+    intrinsic_sizing_info.size = size;
+    intrinsic_sizing_info.aspect_ratio = size;
+  }
+
+  multiplier = ApplyImageResolution(multiplier);
+  intrinsic_sizing_info.size =
+      ApplyZoom(intrinsic_sizing_info.size, multiplier);
+  return intrinsic_sizing_info;
 }
 
 bool StyleFetchedImage::HasIntrinsicSize() const {
