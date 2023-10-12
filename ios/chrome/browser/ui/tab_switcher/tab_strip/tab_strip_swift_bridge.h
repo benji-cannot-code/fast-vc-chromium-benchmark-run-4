0d1f0f0b9152d75843ad9d55c9f23ecd1166a122
Gauthier Ambard <gambard@chromium.org>
2023-10-12 17:21:11 +0000

    [iOS] Make the new TabStrip's cells visible
    This CL makes sure that the cells for the new TabStrip can been seen.
    This ensure that we can develop the next steps.
    
    Bug: 1490555
    Change-Id: Ic305473227b43054455306519222bc1d9600ff8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4931559
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1208933}

--- ios/chrome/browser/ui/tab_switcher/tab_strip/tab_strip_swift_bridge.h
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/tab_strip_swift_bridge.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TAB_STRIP_TAB_STRIP_SWIFT_BRIDGE_H_
 #define IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TAB_STRIP_TAB_STRIP_SWIFT_BRIDGE_H_
 
+#import "ios/chrome/browser/ui/tab_switcher/tab_strip/tab_strip_cell.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_strip/tab_strip_mutator.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_switcher_item.h"
 
