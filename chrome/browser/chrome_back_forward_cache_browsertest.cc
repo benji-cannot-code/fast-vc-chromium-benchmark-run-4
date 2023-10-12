36b3453c488b161918cfbc98ab1fb824b0ef4dd9
Adem Derinel <derinel@google.com>
2023-10-12 13:20:11 +0000

    Gardener: Disable tests in ChromeBackForwardCacheBrowserWithEmbedTest
    Bug: 1492026
    Change-Id: Ifab0ba441728de3c337286f2212cf43b3c858d26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4934550
    Owners-Override: Adem Derinel <derinel@google.com>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1208787}

--- chrome/browser/chrome_back_forward_cache_browsertest.cc
+++ chrome/browser/chrome_back_forward_cache_browsertest.cc
@@ -740,8 +740,14 @@ IN_PROC_BROWSER_TEST_P(ChromeBackForwardCacheBrowserWithEmbedTest,
 }
 #endif  // BUILDFLAG(ENABLE_PDF)
 
+// Flaky on Mac: crbug.com/1492026
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_DoesNotCachePageWithEmbeddedPdfAppendedOnPageLoaded DISABLED_DoesNotCachePageWithEmbeddedPdfAppendedOnPageLoaded
+#else
+#define MAYBE_DoesNotCachePageWithEmbeddedPdfAppendedOnPageLoaded DoesNotCachePageWithEmbeddedPdfAppendedOnPageLoaded
+#endif
 IN_PROC_BROWSER_TEST_P(ChromeBackForwardCacheBrowserWithEmbedTest,
-                       DoesNotCachePageWithEmbeddedPdfAppendedOnPageLoaded) {
+                       MAYBE_DoesNotCachePageWithEmbeddedPdfAppendedOnPageLoaded) {
   const auto tag = GetParam();
 
   // Navigate to A.
@@ -797,8 +803,14 @@ IN_PROC_BROWSER_TEST_P(ChromeBackForwardCacheBrowserWithEmbedTest,
             content::RenderFrameHost::LifecycleState::kInBackForwardCache);
 }
 
+// Flaky on Mac: crbug.com/1492026
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_DoesNotCachePageWithEmbeddedHtmlMutatedIntoPdf DISABLED_DoesNotCachePageWithEmbeddedHtmlMutatedIntoPdf
+#else
+#define MAYBE_DoesNotCachePageWithEmbeddedHtmlMutatedIntoPdf DoesNotCachePageWithEmbeddedHtmlMutatedIntoPdf
+#endif
 IN_PROC_BROWSER_TEST_P(ChromeBackForwardCacheBrowserWithEmbedTest,
-                       DoesNotCachePageWithEmbeddedHtmlMutatedIntoPdf) {
+                       MAYBE_DoesNotCachePageWithEmbeddedHtmlMutatedIntoPdf) {
   const auto tag = GetParam();
   const auto page_with_html = base::StringPrintf(
       "/back_forward_cache/page_with_%s_html.html", tag.c_str());
