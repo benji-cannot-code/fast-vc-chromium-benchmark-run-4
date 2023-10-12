87eb103134583a9e74269bd8cedf95119706d142
Nicolas Dossou-Gbete <dgn@chromium.org>
2023-10-12 22:29:23 +0000

    🧇 Ensure the search engines list is rebuilt on feature state change
    As the `SearchEngineChoice` feature changes how the prepopulated
    search engines are surfaced to the rest of the system without changing
    the version number associated with this data, we got into some
    inconsistent states.
    This change records feature state across process runs to allow
    triggering re-merges when it changes.
    
    Bug: b:303641218
    Change-Id: I11e35cfcc06179f4052d9d6dcebc938f66b2bfd0
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4915859
    Reviewed-by: Angela Yoeurng <yoangela@chromium.org>
    Reviewed-by: Jens Mueller <muellerj@google.com>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209129}

--- components/search_engines/template_url_prepopulate_data.cc
+++ components/search_engines/template_url_prepopulate_data.cc
@@ -1737,6 +1737,8 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterIntegerPref(prefs::kSearchProviderOverridesVersion, -1);
   registry->RegisterInt64Pref(
       prefs::kDefaultSearchProviderChoiceScreenRandomShuffleSeed, 0);
+  registry->RegisterBooleanPref(
+      prefs::kDefaultSearchProviderKeywordsUseExtendedList, false);
 }
 
 int GetDataVersion(PrefService* prefs) {
