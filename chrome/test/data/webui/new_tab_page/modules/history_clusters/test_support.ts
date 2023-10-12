68983b5a6a552492270970d1eb5c1379e8ecaa9f
Yue Zhang <yuezhanggg@chromium.org>
2023-10-12 18:50:53 +0000

    [Discounts] Add discount to tile a11y label
    Bug: 1491990
    Change-Id: Ie358088f400e152c2aed935ea31ddbae49f59d0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4933331
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208990}

--- chrome/test/data/webui/new_tab_page/modules/history_clusters/test_support.ts
+++ chrome/test/data/webui/new_tab_page/modules/history_clusters/test_support.ts
@@ -27,7 +27,7 @@ export function createVisit(overrides?: Partial<URLVisit>): URLVisit {
         titleMatchPositions: [],
         urlForDisplayMatchPositions: [],
         duplicates: [],
-        relativeDate: '',
+        relativeDate: '1 min ago',
         annotations: [],
         debugInfo: {},
         rawVisitData: {
