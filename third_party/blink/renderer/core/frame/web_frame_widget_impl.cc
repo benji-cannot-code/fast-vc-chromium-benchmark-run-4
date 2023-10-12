4ccff2b70d3685413399c3fbaa833183a51402b1
David Awogbemila <awogbemila@chromium.org>
2023-10-12 12:32:28 +0000

    Account for device emulation scale in scroll hit tests
    Bug: 1469645
    Change-Id: I42d23a98460531a7eb58da9bed5059b9ecd5c44d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4926190
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: David Awogbemila <awogbemila@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208777}

--- third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
+++ third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
@@ -3724,7 +3724,10 @@ bool WebFrameWidgetImpl::IsProvisional() {
 
 cc::ElementId WebFrameWidgetImpl::GetScrollableContainerIdAt(
     const gfx::PointF& point) {
-  return HitTestResultAt(point).GetScrollableContainerId();
+  gfx::PointF hit_test_point = point;
+  LocalFrameView* view = LocalRootImpl()->GetFrameView();
+  hit_test_point.Scale(1 / view->InputEventsScaleFactor());
+  return HitTestResultForRootFramePos(hit_test_point).GetScrollableContainer();
 }
 
 bool WebFrameWidgetImpl::ShouldHandleImeEvents() {
