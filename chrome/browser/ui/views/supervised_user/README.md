5a634d797c716c83c4ac0ac2a30897d5e0e3cba2
Nohemi Fernandez <fernandex@chromium.org>
2023-10-12 14:50:19 +0000

    Remove launched feature flag `kLocalWebApprovalsV2` from test cases.
    Bug: b/289179073
    Change-Id: I616d5e00cbb01a9bda901a576bfd21cee133b119
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4891623
    Reviewed-by: Courtney Wong <courtneywong@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208841}

---
+++ chrome/browser/ui/views/supervised_user/README.md
@@ -0,0 +1,3 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+This component defines the extensions approval flow for supervised users.
+This flow is only used by Desktop platforms.
--- components/supervised_user/core/common/features.h
+++ components/supervised_user/core/common/features.h
@@ -30,8 +30,6 @@ BASE_DECLARE_FEATURE(kClearingCookiesKeepsSupervisedUsersSignedIn);
 // is enabled
 bool CanDisplayFirstTimeInterstitialBanner();
 
-BASE_DECLARE_FEATURE(kLocalExtensionApprovalsV2);
-
 // Experiments to enable proto fetchers
 BASE_DECLARE_FEATURE(kEnableProtoApiForClassifyUrl);
 
@@ -60,9 +58,6 @@ bool IsProtoApiForClassifyUrlEnabled();
 // Decides whether to use built-in configurable mechanism, instead of manually
 // programmed.
 bool IsRetryMechanismForListFamilyMembersEnabled();
-
-// Returns whether the new local extension approval experience is enabled.
-bool IsLocalExtensionApprovalsV2Enabled();
 
 // Returns true if child account supervision features should be enabled for this
 // client.
