b3d01afdb85e648a5b8bc0d3e79b6f508769554c
Asami Doi <asamidoi@chromium.org>
2023-10-12 15:11:40 +0000

    [ios] Introduce ImageFileManager
    This CL introduces ImageFileManager that handles reading/writing images
    from/to disk. Previously, the methods for handling disk was implemented
    in SnapshotStorage. Now, the methods move to ImageFileManager and
    SnapshotStorage has an instance variable of ImageFileManager.
    
    No behavior changes. The unittests will be updated in another CL.
    
    Bug: 1485474
    Change-Id: I9fb51905fd47571dc7f4827aa8749ff51f1fb79b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4882685
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1208855}

--- ios/chrome/browser/snapshots/model/snapshot_storage_unittest.mm
+++ ios/chrome/browser/snapshots/model/snapshot_storage_unittest.mm
@@ -386,8 +386,8 @@ TEST_F(SnapshotStorageTest, Purge) {
   std::vector<SnapshotID> liveSnapshotIDs = {test_images_.begin()->first};
 
   // Purge the storage.
-  [storage purgeCacheOlderThan:(base::Time::Now() - base::Hours(1))
-                       keeping:liveSnapshotIDs];
+  [storage purgeImagesOlderThan:(base::Time::Now() - base::Hours(1))
+                        keeping:liveSnapshotIDs];
   FlushRunLoops(storage);
 
   // Check that nothing has been deleted.
@@ -398,7 +398,7 @@ TEST_F(SnapshotStorageTest, Purge) {
   }
 
   // Purge the storage.
-  [storage purgeCacheOlderThan:base::Time::Now() keeping:liveSnapshotIDs];
+  [storage purgeImagesOlderThan:base::Time::Now() keeping:liveSnapshotIDs];
   FlushRunLoops(storage);
 
   // Check that the file have been deleted.
