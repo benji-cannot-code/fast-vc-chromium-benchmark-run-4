48145c58ff4283f2888f2f2ae76123124aff46ef
Ciara McMullin <ciaramcmullin@google.com>
2023-10-12 13:02:19 +0000

    Refactor ip_protection_config_cache to use separate class for managing token cache.
    Change-Id: Ib36bb0ac3712fb02fe7cc9169d3ffd2ee9f0dc00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4928388
    Reviewed-by: Andrew Williams <awillia@chromium.org>
    Commit-Queue: Ciara McMullin <ciaramcmullin@google.com>
    Reviewed-by: Dustin Mitchell <djmitche@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208783}

--- services/network/ip_protection_config_cache.h
+++ services/network/ip_protection_config_cache.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/component_export.h"
 #include "services/network/ip_protection_proxy_list_manager.h"
+#include "services/network/ip_protection_token_cache_manager.h"
 #include "services/network/public/mojom/network_context.mojom.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
@@ -24,9 +25,7 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) IpProtectionConfigCache {
  public:
   virtual ~IpProtectionConfigCache() = default;
 
-  // Initializes the proxy list for the cache.
-
-  // TODO(ciaramcmullin): Update comment when token caches are refactored out.
+  // Initializes the proxy list and token managers for the cache.
   virtual void SetUp() = 0;
 
   // Check whether tokens are available.
@@ -45,6 +44,15 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) IpProtectionConfigCache {
   // Invalidate any previous instruction that token requests should not be
   // made until after a specified time.
   virtual void InvalidateTryAgainAfterTime() = 0;
+
+  // Set the token cache manager for the cache.
+  virtual void SetIpProtectionTokenCacheManagerForTesting(
+      std::unique_ptr<IpProtectionTokenCacheManager>
+          ipp_token_cache_manager) = 0;
+
+  // Fetch the token cache manager.
+  virtual IpProtectionTokenCacheManager*
+  GetIpProtectionTokenCacheManagerForTesting() = 0;
 
   // Set the proxy list manager for the cache.
   virtual void SetIpProtectionProxyListManagerForTesting(
--- services/network/network_service_proxy_delegate_unittest.cc
+++ services/network/network_service_proxy_delegate_unittest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_context_builder.h"
 #include "net/url_request/url_request_test_util.h"
 #include "services/network/ip_protection_proxy_list_manager.h"
+#include "services/network/ip_protection_token_cache_manager.h"
 #include "services/network/masked_domain_list/network_service_proxy_allow_list.h"
 #include "services/network/public/cpp/features.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -56,6 +57,17 @@ class MockIpProtectionConfigCache : public IpProtectionConfigCache {
 
   void SetIpProtectionProxyListManagerForTesting(
       std::unique_ptr<IpProtectionProxyListManager> ipp_proxy_list_manager)
+      override {
+    NOTREACHED_NORETURN();
+  }
+
+  IpProtectionTokenCacheManager* GetIpProtectionTokenCacheManagerForTesting()
+      override {
+    NOTREACHED_NORETURN();
+  }
+
+  void SetIpProtectionTokenCacheManagerForTesting(
+      std::unique_ptr<IpProtectionTokenCacheManager> ipp_token_cache_manager)
       override {
     NOTREACHED_NORETURN();
   }
