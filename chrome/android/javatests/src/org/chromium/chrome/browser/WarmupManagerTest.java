12165e37f475198b72d39347a66d48ab64db32de
Charles Hager <clhager@google.com>
2023-10-12 17:49:06 +0000

    Apply automotive UI scale-up to warm up context
    Before:
    https://screencast.googleplex.com/cast/NDY3NDU0MjQxNTkwNDc2OHw2ZWE1OTdjNS02ZA
    
    After:
    https://screencast.googleplex.com/cast/NTI3MDE2MDIyNjEyMzc3NnwxOWZjZDI5My1lYg
    
    Bug: b/291115035
    Change-Id: I701c73b8e1d0754c0ee49a905fc52ea421047324
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4932590
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Kevin Grosu <kgrosu@google.com>
    Commit-Queue: Charles Hager <clhager@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1208950}

--- chrome/android/javatests/src/org/chromium/chrome/browser/WarmupManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/WarmupManagerTest.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import android.content.Context;
 
@@ -33,6 +34,7 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.HistogramWatcher;
+import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.WarmupManager.SpareTabFinalStatus;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -57,6 +59,8 @@ import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.content_public.browser.test.util.WebContentsUtils;
 import org.chromium.net.test.EmbeddedTestServer;
 import org.chromium.net.test.util.TestWebServer;
+import org.chromium.ui.display.DisplayUtil;
+import org.chromium.ui.test.util.DeviceRestriction;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -564,5 +568,37 @@ public class WarmupManagerTest {
 
         // RenderFrame should be created when the CREATE_NEW_TAB_INITIALIZE_RENDERER is enabled.
         Assert.assertTrue(webContents.getMainFrame().isRenderFrameLive());
+    }
+
+    @Test
+    @SmallTest
+    @Restriction({DeviceRestriction.RESTRICTION_TYPE_NON_AUTO})
+    public void testApplyContextOverridesOnNonAutomotive() {
+        Context baseContext = mContext.getApplicationContext();
+        Context updatedContext = WarmupManager.applyContextOverrides(baseContext);
+
+        assertEquals(
+                "The updated context should be the same as the original context.",
+                baseContext,
+                updatedContext);
+    }
+
+    @Test
+    @SmallTest
+    @Restriction({DeviceRestriction.RESTRICTION_TYPE_AUTO})
+    public void testApplyContextOverridesOnAutomotive() {
+        Context baseContext = mContext.getApplicationContext();
+        Context updatedContext = WarmupManager.applyContextOverrides(baseContext);
+
+        assertNotEquals(
+                "The updated context should be different from the original context.",
+                baseContext,
+                updatedContext);
+        assertEquals(
+                "The updated context should have a scaled up densityDpi",
+                (int)
+                        (baseContext.getResources().getDisplayMetrics().densityDpi
+                                * DisplayUtil.getUiScalingFactorForAutomotive()),
+                updatedContext.getResources().getDisplayMetrics().densityDpi);
     }
 }
