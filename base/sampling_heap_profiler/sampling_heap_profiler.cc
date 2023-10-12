7033d60149614110e7877f70ec628bbeaa03a916
André Kempe <andre.kempe@arm.com>
2023-10-12 06:25:11 +0000

    Avoid usage of C++'s thread_local in SamplingHeapProfiler
    Clang's implementation of C++'s thread_local storage uses emutls on
    Android. Emutls again is built upon pthread's thread local storage which
    may allocate if not initialized early in startup process.
    
    This CL changes thread local storage of the SamplingHeapProfiler to the
    same approach used in the PoissonAllocationSampler, ensuring early
    initialization and thus preventing (re)allocations.
    
    Bug: 1475165
    Change-Id: I328076d70264cca1fd0ccde365c1baa23c2ff1da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4930850
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Andre Kempe <andre.kempe@arm.com>
    Cr-Commit-Position: refs/heads/main@{#1208679}

--- base/sampling_heap_profiler/sampling_heap_profiler.cc
+++ base/sampling_heap_profiler/sampling_heap_profiler.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cmath>
 #include <utility>
 
+#include "base/allocator/dispatcher/tls.h"
 #include "base/allocator/partition_allocator/src/partition_alloc/partition_alloc.h"
 #include "base/allocator/partition_allocator/src/partition_alloc/shim/allocator_shim.h"
 #include "base/compiler_specific.h"
@@ -38,6 +39,22 @@ namespace base {
 constexpr uint32_t kMaxStackEntries = 256;
 
 namespace {
+
+struct ThreadLocalData {
+  const char* thread_name = nullptr;
+};
+
+ThreadLocalData* GetThreadLocalData() {
+#if USE_LOCAL_TLS_EMULATION()
+  static base::NoDestructor<
+      base::allocator::dispatcher::ThreadLocalStorage<ThreadLocalData>>
+      thread_local_data("sampling_heap_profiler");
+  return thread_local_data->GetThreadLocalData();
+#else
+  static thread_local ThreadLocalData thread_local_data;
+  return &thread_local_data;
+#endif
+}
 
 #if BUILDFLAG(CAN_UNWIND_WITH_FRAME_POINTERS)
 BASE_FEATURE(kAvoidFramePointers,
@@ -83,12 +100,13 @@ const char* GetAndLeakThreadName() {
 }
 
 const char* UpdateAndGetThreadName(const char* name) {
-  static thread_local const char* thread_name;
+  ThreadLocalData* const thread_local_data = GetThreadLocalData();
   if (name)
-    thread_name = name;
-  if (!thread_name)
-    thread_name = GetAndLeakThreadName();
-  return thread_name;
+    thread_local_data->thread_name = name;
+  if (!thread_local_data->thread_name) {
+    thread_local_data->thread_name = GetAndLeakThreadName();
+  }
+  return thread_local_data->thread_name;
 }
 
 // Checks whether unwinding from this function works.
@@ -305,6 +323,7 @@ std::vector<const char*> SamplingHeapProfiler::GetStrings() {
 
 // static
 void SamplingHeapProfiler::Init() {
+  GetThreadLocalData();
   PoissonAllocationSampler::Init();
 }
 
