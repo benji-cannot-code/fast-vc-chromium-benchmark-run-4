1871086ebd2d7f473f7b7d5fcf32b19e484958f7
Jason Hu <hujasonx@google.com>
2023-10-12 22:24:36 +0000

    [CSC][Lens]Use jpeg for opaque images and webp for others
    CSC = Chrome Search Companion
    
    Allows render_frame_observer to directly pass the context menu bitmap to the core_tab_helper and transfers downscaling/encoding responsibility to
    core_tab_helper.
    
    This change makes it so that core_tab_helper supports choosing the image encoding format based on whether or not transparency needs to be preserved in the original image. Jpeg encoding is faster than webp encoding but doesn't support transparency, so this allows us to choose the faster encoding method for opaque images while preserving transparency for images with an alpha channel.
    
    Bug:1482923, b/295332455
    
    Change-Id: I73471e785d52c993f981ed55f5edcf6492bc8a69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4865058
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Reviewed-by: James Zern <jzern@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1209126}

--- components/lens/lens_features.cc
+++ components/lens/lens_features.cc
@@ -106,20 +106,17 @@ const base::FeatureParam<bool> kEnableLensFullscreenSearch{
 const base::FeatureParam<bool> kLensContextMenuUseAlternateText{
     &kLensSearchOptimizations, "use-lens-context-menu-alternate-text", false};
 
-const base::FeatureParam<bool> kUseWebpInImageSearch{
-    &kLensImageFormatOptimizations, "use-webp-image-search", true};
+const base::FeatureParam<int> kEncodingQualityJpeg{
+    &kLensImageFormatOptimizations, "encoding-quality-jpeg", 40};
 
-const base::FeatureParam<int> kEncodingQualityImageSearch{
-    &kLensImageFormatOptimizations, "encoding-quality-image-search", 45};
+const base::FeatureParam<int> kEncodingQualityWebp{
+    &kLensImageFormatOptimizations, "encoding-quality-webp", 45};
 
 const base::FeatureParam<bool> kUseWebpInRegionSearch{
     &kLensImageFormatOptimizations, "use-webp-region-search", false};
 
 const base::FeatureParam<bool> kUseJpegInRegionSearch{
     &kLensImageFormatOptimizations, "use-jpeg-region-search", true};
-
-const base::FeatureParam<int> kEncodingQualityRegionSearch{
-    &kLensImageFormatOptimizations, "encoding-quality-region-search", 40};
 
 constexpr base::FeatureParam<std::string> kLensPingURL{
     &kEnableLensPing, "lens-ping-url",
@@ -205,13 +202,12 @@ bool UseLensContextMenuItemAlternateText() {
          kLensContextMenuUseAlternateText.Get();
 }
 
-bool IsWebpForImageSearchEnabled() {
-  return base::FeatureList::IsEnabled(kLensImageFormatOptimizations) &&
-         kUseWebpInImageSearch.Get();
+int GetEncodingQualityJpeg() {
+  return kEncodingQualityJpeg.Get();
 }
 
-int GetImageSearchEncodingQuality() {
-  return kEncodingQualityImageSearch.Get();
+int GetEncodingQualityWebp() {
+  return kEncodingQualityWebp.Get();
 }
 
 bool IsWebpForRegionSearchEnabled() {
@@ -222,10 +218,6 @@ bool IsWebpForRegionSearchEnabled() {
 bool IsJpegForRegionSearchEnabled() {
   return base::FeatureList::IsEnabled(kLensImageFormatOptimizations) &&
          kUseJpegInRegionSearch.Get();
-}
-
-int GetRegionSearchEncodingQuality() {
-  return kEncodingQualityRegionSearch.Get();
 }
 
 bool GetEnableContextMenuInLensSidePanel() {
