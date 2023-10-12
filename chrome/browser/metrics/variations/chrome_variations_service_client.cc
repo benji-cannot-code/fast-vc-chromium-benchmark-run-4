cf8e31efcca38d3cb8edcb1e7ca7e743cbca709c
Jenny Zhang <jennyz@chromium.org>
2023-10-12 22:27:46 +0000

    Lacros: Do not access device setting in browser_tests.
    ChromeBrowserPolicyConnector::GetDeviceSettings() access
    the device settings passed from Ash to Lacros via
    crosapi::mojom::BrowserInitParams. This code path is invoked
    when browser test starts Lacros browser for the branded build,
    but crosapi is NOT enabled for Lacros browser_tests, therefore,
    it crashes. This cl fixes this issue.
    
    Bug: 1474764
    Change-Id: I5f4d018a4ff05efac5fd637d9a7a1c0b5b393e41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4928871
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Igor <igorcov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209127}

--- chrome/browser/metrics/variations/chrome_variations_service_client.cc
+++ chrome/browser/metrics/variations/chrome_variations_service_client.cc
@@ -38,8 +38,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "base/check_is_test.h"
 #include "chrome/browser/policy/chrome_browser_policy_connector.h"
 #include "chromeos/crosapi/mojom/device_settings_service.mojom.h"
+#include "chromeos/startup/browser_params_proxy.h"
 #endif
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
@@ -80,6 +82,18 @@ bool ChromeVariationsServiceClient::OverridesRestrictParameter(
                                       parameter);
   return true;
 #elif BUILDFLAG(IS_CHROMEOS_LACROS)
+  // The device settings is passed from Ash to Lacros via
+  // crosapi::mojom::BrowserInitParams. However, crosapi is disabled for Lacros
+  // browser_tests, there is no valid device settings in this situation.
+  // Note: This code path is invoked when browser test starts the browser for
+  // branded Lacros build, see crbug.com/1474764.
+  if (!g_browser_process->browser_policy_connector()->GetDeviceSettings()) {
+    CHECK_IS_TEST();  // IN-TEST
+    CHECK(chromeos::BrowserParamsProxy::Get()
+              ->IsCrosapiDisabledForTesting());  // IN-TEST
+    return false;
+  }
+
   const absl::optional<std::string>& policy_value =
       g_browser_process->browser_policy_connector()
           ->GetDeviceSettings()
