37d37e1dcc89a3437812ed0d781522e47e2afeb1
Sam Maier <smaier@chromium.org>
2023-10-12 14:08:56 +0000

    jni_zero: adding a copy of @CheckDiscard
    Bug: 1377351
    Change-Id: I7446d1db1560de350d485fa4729ca98a05d5790a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4931447
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208810}

---
+++ third_party/jni_zero/java/src/org/jni_zero/CheckDiscard.java
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.jni_zero;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Causes build to assert that annotated classes / methods / fields are optimized away in release
+ * builds (when using checkdiscard_proguard.flags).
+ */
+@Target({ElementType.CONSTRUCTOR, ElementType.FIELD, ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.CLASS)
+public @interface CheckDiscard {
+    /**
+     * Describes why the element should be discarded.
+     *
+     * @return reason for discarding (crbug links are preferred unless reason is trivial).
+     */
+    String value();
+}
