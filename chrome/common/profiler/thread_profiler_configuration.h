51e21b79ea2c79f6cc69145fbe958164203475b0
Thiabaud Engelbrecht <thiabaud@google.com>
2023-10-12 03:05:09 +0000

    Revert "[ssm] Add field trial for java name hashing on android."
    This reverts commit 6a966a60f378fdbd05ca146a17288ffd2eb71158.
    
    Reason for revert: crbug.com/1491890
    
    Original change's description:
    > [ssm] Add field trial for java name hashing on android.
    >
    > Bug: 1475718, 1491890
    > Change-Id: I6e8db8f2967743368488d03bdf983c06ae453484
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4864324
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    > Reviewed-by: Mike Wittman <wittman@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1207443}
    
    Bug: 1475718
    Change-Id: I26e73542fe73ef16a552a1a40f0eef08fe8314af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4929159
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Gabriel Charette <gab@chromium.org>
    Auto-Submit: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1208641}

--- chrome/common/profiler/thread_profiler_configuration.h
+++ chrome/common/profiler/thread_profiler_configuration.h
@@ -61,10 +61,6 @@ class ThreadProfilerConfiguration {
   void AppendCommandLineSwitchForChildProcess(
       base::CommandLine* command_line) const;
 
-#if BUILDFLAG(IS_ANDROID)
-  bool IsJavaNameHashingEnabled() const;
-#endif  // BUILDFLAG(IS_ANDROID)
-
  private:
   friend base::NoDestructor<ThreadProfilerConfiguration>;
 
@@ -81,13 +77,6 @@ class ThreadProfilerConfiguration {
     // Enabled within the experiment (and paired with equal-sized
     // kProfileDisabled group).
     kProfileControl,
-
-#if BUILDFLAG(IS_ANDROID)
-    // Enabled within the experiment (and paired with equal-sized
-    // kProfileDisabled and kProfileControl groups). Java names will be
-    // hashed within this group.
-    kProfileEnabledWithJavaNameHashing,
-#endif  // BUILDFLAG(IS_ANDROID)
 
     // Enabled outside of the experiment.
     kProfileEnabled,
