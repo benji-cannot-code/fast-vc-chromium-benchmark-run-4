65b92450d87512faa0fcf217ab0c85af0d5197bc
Zachary Tan <tanzachary@chromium.org>
2023-10-12 19:29:55 +0000

    [FedCM] Update error UI on android to match new mocks
    1. Got it button now reuses the existing "Continue" button template
    2. More details button is no longer a button but a hyper link in the
    error description, similar to the existing PP/TOS links
    
    Screenshot
    https://bugs.chromium.org/p/chromium/issues/detail?id=1420642#c32
    
    Bug: 1420642
    Change-Id: I28ea041800116f05f101d47d0551cd07e2e27a99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4930016
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Zachary Tan <tanzachary@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209017}

--- chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionProperties.java
+++ chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionProperties.java
@@ -113,14 +113,17 @@ class AccountSelectionProperties {
      * sheet.
      */
     static class ContinueButtonProperties {
-        static final ReadableObjectPropertyKey<Account> ACCOUNT =
-                new ReadableObjectPropertyKey<>("account");
-        static final ReadableObjectPropertyKey<IdentityProviderMetadata> IDP_METADATA =
-                new ReadableObjectPropertyKey<>("idp_metadata");
-        static final ReadableObjectPropertyKey<Callback<Account>> ON_CLICK_LISTENER =
-                new ReadableObjectPropertyKey<>("on_click_listener");
+        static class Properties {
+            public Account mAccount;
+            public IdentityProviderMetadata mIdpMetadata;
+            public Callback<Account> mOnClickListener;
+            public HeaderProperties.HeaderType mHeaderType;
+        }
 
-        static final PropertyKey[] ALL_KEYS = {ACCOUNT, IDP_METADATA, ON_CLICK_LISTENER};
+        static final ReadableObjectPropertyKey<Properties> PROPERTIES =
+                new ReadableObjectPropertyKey<>("properties");
+
+        static final PropertyKey[] ALL_KEYS = {PROPERTIES};
 
         private ContinueButtonProperties() {}
     }
@@ -162,6 +165,7 @@ class AccountSelectionProperties {
             public String mIdpForDisplay;
             public String mTopFrameForDisplay;
             public IdentityCredentialTokenError mError;
+            public Runnable mMoreDetailsClickRunnable;
         }
 
         static final ReadableObjectPropertyKey<Properties> PROPERTIES =
@@ -186,13 +190,10 @@ class AccountSelectionProperties {
                 new WritableObjectPropertyKey<>("idp_signin");
         static final WritableObjectPropertyKey<PropertyModel> ERROR_TEXT =
                 new WritableObjectPropertyKey<>("error_text");
-        static final WritableObjectPropertyKey<PropertyModel> GOT_IT_BUTTON =
-                new WritableObjectPropertyKey<>("got_it_btn");
-        static final WritableObjectPropertyKey<PropertyModel> MORE_DETAILS_BUTTON =
-                new WritableObjectPropertyKey<>("more_details_btn");
 
-        static final PropertyKey[] ALL_KEYS = {CONTINUE_BUTTON, DATA_SHARING_CONSENT, HEADER,
-                IDP_SIGNIN, ERROR_TEXT, GOT_IT_BUTTON, MORE_DETAILS_BUTTON};
+        static final PropertyKey[] ALL_KEYS = {
+            CONTINUE_BUTTON, DATA_SHARING_CONSENT, HEADER, IDP_SIGNIN, ERROR_TEXT
+        };
 
         private ItemProperties() {}
     }
