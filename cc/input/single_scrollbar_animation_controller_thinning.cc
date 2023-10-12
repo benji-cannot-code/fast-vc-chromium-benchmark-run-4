fe7df54de4b0d919991fe9335dd5e8911fed5313
Gaston Rodriguez Lopez <gastonr@microsoft.com>
2023-10-12 18:52:41 +0000

    Fix Fluent Overlay scrollbar thumb hittest bug.
    Thinning animation controller uses ComputeExpandedThumbQuadRect to see
    if the pointer is on top of the thumb and check if the scrollbars
    should consider themselves captured or not.
    This function doesn't take into account that Fluent scrollbars' thumbs
    have a margin between the track and the thumbs, and when clicking on
    this margin the animation controller doesn't consider itself captured.
    
    This CL:
    - Reorganizes the code related to the thumb's rect calculation.
    - ComputeExpandedThumbQuadRect is renamed to a more accurate
    ComputeHitTestableExpandedThumbQuadRect
    - Fixed a broken unittest that was not testing what it was meant to.
    
    Bug: 1489461
    Change-Id: I010726b23d52453632665e181bc96267a8626a67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4913424
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Gaston Rodriguez <gastonr@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1208992}

--- cc/input/single_scrollbar_animation_controller_thinning.cc
+++ cc/input/single_scrollbar_animation_controller_thinning.cc
@@ -23,7 +23,8 @@ float DistanceToScrollbarPart(const gfx::PointF& device_viewport_point,
                               const ScrollbarPart part) {
   gfx::RectF rect;
   if (part == ScrollbarPart::kThumb) {
-    rect = gfx::RectF(gfx::Rect(scrollbar.ComputeExpandedThumbQuadRect()));
+    rect = gfx::RectF(
+        gfx::Rect(scrollbar.ComputeHitTestableExpandedThumbQuadRect()));
   } else {
     rect = gfx::RectF(gfx::Rect(scrollbar.bounds()));
   }
