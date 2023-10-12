b7ab777d8c00eaf2584e417180b0f2757a4ab938
Oleksandr Peletskyi <peletskyi@google.com>
2023-10-12 12:46:39 +0000

    iwa: Move fake response reader factory to the test dir
    The test directory may contain the files that are related to the tests.
    The moved files belong to the web_applications_test_support target now.
    
    Bug: b/287594073
    Test: run unittests
    Change-Id: I9c9f0c354b7b74a449a5f74f8333ef0b7d2eba3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4922319
    Auto-Submit: Oleksandr Peletskyi <peletskyi@google.com>
    Commit-Queue: Oleksandr Peletskyi <peletskyi@google.com>
    Reviewed-by: Christian Flach <cmfcmf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208779}

--- chrome/browser/web_applications/isolated_web_apps/test/isolated_web_app_fake_response_reader_factory.cc
+++ chrome/browser/web_applications/isolated_web_apps/test/isolated_web_app_fake_response_reader_factory.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/web_applications/isolated_web_apps/isolated_web_app_fake_response_reader_factory.h"
+#include "chrome/browser/web_applications/isolated_web_apps/test/isolated_web_app_fake_response_reader_factory.h"
 
 #include <memory>
 
