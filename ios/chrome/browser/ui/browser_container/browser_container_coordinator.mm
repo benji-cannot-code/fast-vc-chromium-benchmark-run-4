9607d4f424ec26998187e8459c46bb12ec1c630a
Olivier ROBIN <olivierrobin@google.com>
2023-10-12 10:45:25 +0000

    Fix EditMenu in iOS16+
    crrev.com/c/4926230 wrongly removed the browserEditMenuHandler
    linkToTextDelegate that is still used on iOS16+
    
    Tested internally on BrowserEditMenuHandlerTestCase
    
    Bug: 1485132, 1492077
    Change-Id: Ie5e839a49eed3779f72d20c77da466ed3855e71d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4931536
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208749}

--- ios/chrome/browser/ui/browser_container/browser_container_coordinator.mm
+++ ios/chrome/browser/ui/browser_container/browser_container_coordinator.mm
@@ -92,6 +92,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   self.browserEditMenuHandler = [[BrowserEditMenuHandler alloc] init];
   self.viewController.browserEditMenuHandler = self.browserEditMenuHandler;
+  self.browserEditMenuHandler.linkToTextDelegate = self.linkToTextMediator;
   self.viewController.linkToTextDelegate = self.linkToTextMediator;
 
   if (IsPartialTranslateEnabled()) {
