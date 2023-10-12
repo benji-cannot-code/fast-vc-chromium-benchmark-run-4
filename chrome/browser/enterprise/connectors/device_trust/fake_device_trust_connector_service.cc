8eee42e6cc362f9347864956686e52851067b4cd
Sebastien Lalancette <seblalancette@chromium.org>
2023-10-12 16:28:56 +0000

    [DTC] Cleanup UserDTCInlineFlowEnabled feature flag
    Bug: b:279840937
    Change-Id: Idea521244304aca86c7c30ba9848babf87a76dd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4915846
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Hamda Mare <hmare@google.com>
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208869}

--- chrome/browser/enterprise/connectors/device_trust/fake_device_trust_connector_service.cc
+++ chrome/browser/enterprise/connectors/device_trust/fake_device_trust_connector_service.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
-#include "chrome/browser/enterprise/connectors/device_trust/device_trust_features.h"
 #include "chrome/browser/enterprise/connectors/device_trust/prefs.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
@@ -37,13 +36,8 @@ FakeDeviceTrustConnectorService::~FakeDeviceTrustConnectorService() = default;
 void FakeDeviceTrustConnectorService::UpdateInlinePolicy(
     base::Value::List new_urls,
     DTCPolicyLevel policy_level) {
-  if (IsUserInlineFlowFeatureEnabled()) {
-    test_prefs_->SetManagedPref(ToPrefName(policy_level),
-                                base::Value(std::move(new_urls)));
-  } else {
-    test_prefs_->SetManagedPref(kContextAwareAccessSignalsAllowlistPref,
-                                base::Value(std::move(new_urls)));
-  }
+  test_prefs_->SetManagedPref(ToPrefName(policy_level),
+                              base::Value(std::move(new_urls)));
 }
 
 }  // namespace enterprise_connectors
