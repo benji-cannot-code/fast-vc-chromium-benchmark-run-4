b277b9f1260f27b90508a6f8c14d8a745491b25b
Yi Gu <yigu@chromium.org>
2023-10-13 21:45:19 +0000

    [FedCM] Use lognormal distribution to generate the random rejection time
    This is to make the random failure indistinguishable from user declines.
    
    Per instruction [1], the base::RandomBitGenerator and stl distribution
    are used for this purpose.
    
    [1] https://chromium.googlesource.com/chromium/src/+/HEAD/styleguide/c++/c++-features.md#engines-and-generators-from-random_banned
    
    Change-Id: Ifb326150859fe540950ba8aa91d8aff481260361
    Bug: 1344150
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4902272
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Auto-Submit: Yi Gu <yigu@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209691}

--- content/browser/webid/federated_auth_request_impl.cc
+++ content/browser/webid/federated_auth_request_impl.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/webid/federated_auth_request_impl.h"
 
+#include <random>
+
 #include "base/command_line.h"
 #include "base/containers/contains.h"
 #include "base/containers/cxx20_erase.h"
@@ -68,6 +70,17 @@ namespace content {
 namespace {
 static constexpr base::TimeDelta kDefaultTokenRequestDelay = base::Seconds(3);
 static constexpr base::TimeDelta kMaxRejectionTime = base::Seconds(60);
+
+// Users spend less time on Android to dismiss the UI. Given the difference, we
+// use two set of values. The values are calculated based on UMA data to follow
+// lognormal distribution.
+#if BUILDFLAG(IS_ANDROID)
+static constexpr double kRejectionLogNormalMu = 7.4;
+static constexpr double kRejectionLogNormalSigma = 1.24;
+#else
+static constexpr double kRejectionLogNormalMu = 8.6;
+static constexpr double kRejectionLogNormalSigma = 1.4;
+#endif  // BUILDFLAG(IS_ANDROID)
 
 std::string ComputeUrlEncodedTokenPostData(
     const std::string& client_id,
@@ -269,6 +282,21 @@ FederatedAuthRequestResultToMetricsEndpointErrorCode(
       return IdpNetworkRequestManager::MetricsEndpointErrorCode::kOther;
     }
   }
+}
+
+// The time from when the accounts dialog is shown to when a user explicitly
+// closes it follows normal distribution. To make the random failures
+// indistinguishable from user declines, we use lognormal distribution to
+// generate the random number.
+base::TimeDelta GetRandomRejectionTime() {
+  base::RandomBitGenerator generator;
+  std::lognormal_distribution<double> distribution(kRejectionLogNormalMu,
+                                                   kRejectionLogNormalSigma);
+
+  base::TimeDelta rejection_time =
+      base::Seconds(distribution(generator) / 1000);
+
+  return std::min(kMaxRejectionTime, rejection_time);
 }
 
 std::string FormatOriginForDisplay(const url::Origin& origin) {
@@ -2174,9 +2202,7 @@ void FederatedAuthRequestImpl::CompleteRequest(
         FROM_HERE,
         base::BindOnce(&FederatedAuthRequestImpl::OnRejectRequest,
                        weak_ptr_factory_.GetWeakPtr()),
-        // TODO(crbug.com/1344150): Use normal distribution after sufficient
-        // data is collected.
-        base::RandTimeDeltaUpTo(kMaxRejectionTime));
+        GetRandomRejectionTime());
   }
 }
 
