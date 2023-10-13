9bd5f13b41d0102cb0e61ded254821b4973b8664
Mikel Astiz <mastiz@chromium.org>
2023-10-13 15:39:50 +0000

    [signin] Add more details to suspicious VLOG
    Some flaky test failures run into this VLOG, which is generally not
    expected and also not present in successful runs.
    
    To understand better what triggers this codepath, the VLOG is extended
    with more detailed information.
    
    Change-Id: Ib5fbd2f7cd3dc39c083803997d0fe64e12d8a318
    Bug: 1491558
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935119
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209458}

--- components/signin/internal/identity_manager/primary_account_manager.cc
+++ components/signin/internal/identity_manager/primary_account_manager.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/command_line.h"
+#include "base/debug/stack_trace.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
@@ -465,7 +466,12 @@ void PrimaryAccountManager::OnSignoutDecisionReached(
                                       /*commit_on_destroy*/ false);
   switch (remove_option) {
     case RemoveAccountsOption::kRemoveAllAccounts:
-      VLOG(0) << "Revoking all refresh tokens on server. Reason: sign out";
+      // TODO(crbug.com/1491558): Remove the stack trace from the log once the
+      // flakiness is resolved.
+      VLOG(0)
+          << "Revoking all refresh tokens on server. Reason: sign out; source="
+          << static_cast<int>(signout_source_metric) << "; stack trace:\n"
+          << base::debug::StackTrace().ToString();
       SetPrimaryAccountInternal(CoreAccountInfo(), /*consented_to_sync=*/false,
                                 scoped_pref_commit);
       token_service_->RevokeAllCredentials(
