17993ce5a82954739248a6bf368a245e4de3315d
Adem Derinel <derinel@google.com>
2023-10-13 08:20:05 +0000

    [Android][CredMan] Add boolean to the bundle for incognito tabs
    The change also bumps the GMSCore version which includes support for
    the options that are added recently.
    
    Bug: 1474805
    Change-Id: I98864ed8fa80274d8fb6c400475ffc9433e99546
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4922572
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1209289}

--- chrome/android/junit/src/org/chromium/chrome/browser/webauth/CredManHelperRobolectricTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/webauth/CredManHelperRobolectricTest.java
@@ -30,6 +30,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.robolectric.annotation.Config;
 
 import org.chromium.base.Callback;
 import org.chromium.base.test.BaseRobolectricTestRunner;
@@ -48,10 +49,14 @@ import org.chromium.components.webauthn.CredManMetricsHelper.CredManPrepareReque
 import org.chromium.components.webauthn.Fido2ApiTestHelper;
 import org.chromium.components.webauthn.WebAuthnBrowserBridge;
 import org.chromium.content_public.browser.RenderFrameHost;
+import org.chromium.content_public.browser.WebContents;
 
 import java.util.List;
 
 @RunWith(BaseRobolectricTestRunner.class)
+@Config(
+        manifest = Config.NONE,
+        shadows = {ShadowWebContentStatics.class})
 public class CredManHelperRobolectricTest {
     private CredManHelper mCredManHelper;
     private Fido2ApiTestHelper.AuthenticatorCallback mCallback;
@@ -61,18 +66,13 @@ public class CredManHelperRobolectricTest {
     private String mOriginString = "https://subdomain.coolwebsitekayserispor.com";
     private byte[] mMaybeClientDataHash = new byte[] {1, 2, 3};
 
-    @Mock
-    private Context mContext;
-    @Mock
-    private RenderFrameHost mFrameHost;
-    @Mock
-    private CredManMetricsHelper mMetricsHelper;
-    @Mock
-    private WebAuthnBrowserBridge mBrowserBridge;
-    @Mock
-    private Callback<Integer> mErrorCallback;
-    @Mock
-    private Barrier mBarrier;
+    @Mock private Context mContext;
+    @Mock private RenderFrameHost mFrameHost;
+    @Mock private WebContents mWebContents;
+    @Mock private CredManMetricsHelper mMetricsHelper;
+    @Mock private WebAuthnBrowserBridge mBrowserBridge;
+    @Mock private Callback<Integer> mErrorCallback;
+    @Mock private Barrier mBarrier;
 
     private CredManHelper.BridgeProvider mBridgeProvider = new CredManHelper.BridgeProvider() {
         @Override
