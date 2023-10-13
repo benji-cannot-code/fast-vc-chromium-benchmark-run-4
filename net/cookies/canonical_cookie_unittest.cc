7ddd708a954c05acacfe7af390c6706678e6b563
selya <selya@chromium.org>
2023-10-13 16:07:09 +0000

    Add metric for tracking number of A->B->A cookies
    A->B->A Cookies occur when site (A) has a site (B)
    embedded in it and site B has site A embedded in it.
    This metric is going to be used to evaluate the
    potential impact of adding an ancestor bit to
    CHIPS.
    
    For more background information see the following link:
    https://github.com/privacycg/CHIPS/issues/40
    
    
    
    Change-Id: I79e72028323e53e3eb4a3f70b48f7ab738b474c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4916592
    Reviewed-by: Dylan Cutler <dylancutler@google.com>
    Commit-Queue: Aaron Selya <selya@google.com>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209471}

--- net/cookies/canonical_cookie_unittest.cc
+++ net/cookies/canonical_cookie_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cookies/cookie_constants.h"
 #include "net/cookies/cookie_inclusion_status.h"
 #include "net/cookies/cookie_options.h"
+#include "net/cookies/cookie_partition_key.h"
 #include "net/cookies/parsed_cookie.h"
 #include "net/http/http_util.h"
 #include "testing/gmock/include/gmock/gmock-matchers.h"
@@ -1502,7 +1503,6 @@ TEST(CanonicalCookieTest, IncludeForRequestURL) {
                                  /*delegate_treats_url_as_trustworthy=*/false})
           .status.HasExactlyExclusionReasonsForTesting(
               {CookieInclusionStatus::EXCLUDE_DOMAIN_MISMATCH}));
-
   // Test that cookie with a cookie path that does not match the url path are
   // not included.
   cookie = CanonicalCookie::Create(url, "A=2; Path=/foo/bar", creation_time,
@@ -1523,7 +1523,6 @@ TEST(CanonicalCookieTest, IncludeForRequestURL) {
               CookieAccessParams{net::CookieAccessSemantics::UNKNOWN,
                                  /*delegate_treats_url_as_trustworthy=*/false})
           .status.IsInclude());
-
   // Test that a secure cookie is not included for a non secure URL.
   GURL secure_url("https://www.example.com");
   cookie = CanonicalCookie::Create(secure_url, "A=2; Secure", creation_time,
@@ -1589,8 +1588,6 @@ TEST(CanonicalCookieTest, IncludeForRequestURL) {
   EXPECT_TRUE(result.status.IsInclude());
   EXPECT_FALSE(result.status.ShouldWarn());
 
-  //
-
   // Test that http only cookies are only included if the include httponly flag
   // is set on the cookie options.
   options.set_include_httponly();
@@ -1629,7 +1626,6 @@ void VerifyIncludeForRequestURLTestCases(
     CookieAccessSemantics access_semantics,
     std::vector<IncludeForRequestURLTestCase> test_cases) {
   GURL url("https://example.test");
-
   for (const auto& test : test_cases) {
     base::Time creation_time = base::Time::Now() - test.creation_time_delta;
     std::unique_ptr<CanonicalCookie> cookie = CanonicalCookie::Create(
@@ -2000,6 +1996,71 @@ TEST(CanonicalCookieTest, IncludeForRequestURLSameSite) {
   }
 }
 
+TEST(CanonicalCookieTest, TestFirstPartyPartitionedAndCrossSiteContext) {
+  std::string histogram_name =
+      "Cookie.FirstPartyPartitioned.HasCrossSiteAncestor";
+  base::Time current_time = base::Time::Now();
+  base::HistogramTester histogram_tester;
+  GURL url("https://www.example.com");
+  GURL url2("https://wwwnottheSame.com");
+  CookieOptions options;
+
+  auto make_cookie = [current_time](const auto& partition_key) {
+    return CanonicalCookie::CreateUnsafeCookieForTesting(
+        "A", "2", "www.example.com", "/test", current_time, base::Time(),
+        base::Time(), base::Time(), true /*secure*/, true /*httponly*/,
+        CookieSameSite::NO_RESTRICTION, COOKIE_PRIORITY_DEFAULT, false,
+        partition_key);
+  };
+
+  auto no_partition_key_cookie = make_cookie(absl::nullopt);
+  auto partitioned_cookie =
+      make_cookie(CookiePartitionKey::FromURLForTesting(GURL(url)));
+  auto nonced_partition_key_cookie =
+      make_cookie(CookiePartitionKey::FromURLForTesting(
+          GURL(url), base::UnguessableToken::Create()));
+  auto different_site_partition_key_cookie =
+      make_cookie(CookiePartitionKey::FromURLForTesting(GURL(url2)));
+
+  histogram_tester.ExpectBucketCount(histogram_name, true, 0);
+  histogram_tester.ExpectBucketCount(histogram_name, false, 0);
+  no_partition_key_cookie->IncludeForRequestURL(
+      url, options,
+      CookieAccessParams{CookieAccessSemantics::NONLEGACY, false});
+
+  histogram_tester.ExpectBucketCount(histogram_name, true, 0);
+  histogram_tester.ExpectBucketCount(histogram_name, false, 0);
+
+  partitioned_cookie->IncludeForRequestURL(
+      url, options,
+      CookieAccessParams{CookieAccessSemantics::NONLEGACY, false});
+  histogram_tester.ExpectBucketCount(histogram_name, true, 1);
+  histogram_tester.ExpectBucketCount(histogram_name, false, 0);
+
+  nonced_partition_key_cookie->IncludeForRequestURL(
+      url, options,
+      CookieAccessParams{CookieAccessSemantics::NONLEGACY, false});
+  histogram_tester.ExpectBucketCount(histogram_name, true, 1);
+  histogram_tester.ExpectBucketCount(histogram_name, false, 0);
+
+  different_site_partition_key_cookie->IncludeForRequestURL(
+      url, options,
+      CookieAccessParams{CookieAccessSemantics::NONLEGACY, false});
+  histogram_tester.ExpectBucketCount(histogram_name, true, 1);
+  histogram_tester.ExpectBucketCount(histogram_name, false, 0);
+
+  // Show that a cookie in a non-CROSS_SITE context registers as false.
+  options.set_same_site_cookie_context(
+      net::CookieOptions::SameSiteCookieContext(
+          net::CookieOptions::SameSiteCookieContext::ContextType::
+              SAME_SITE_LAX));
+
+  partitioned_cookie->IncludeForRequestURL(
+      url, options,
+      CookieAccessParams{CookieAccessSemantics::NONLEGACY, false});
+  histogram_tester.ExpectBucketCount(histogram_name, true, 1);
+  histogram_tester.ExpectBucketCount(histogram_name, false, 1);
+}
 // Test that SameSite=None requires Secure.
 TEST(CanonicalCookieTest, IncludeCookiesWithoutSameSiteMustBeSecure) {
   GURL url("https://www.example.com");
