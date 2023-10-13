40d681d3e280b2c087d854eca44f5dee133769b1
Joey Arhar <jarhar@chromium.org>
2023-10-13 22:19:54 +0000

    selectlist: Replace HTMLSelectListElement::OwnerSelectList
    This patch adds HTMLOptionElement::OwnerSelectList and
    HTMLListboxElement::OwnerSelectList. Now that <listbox>, <option>, and
    <button> all have their own OwnerSelectList implemented after this
    patch, we don't need HTMLSelectListElement::OwnerSelectList which
    accounts for all different types anymore.
    
    Bug: 1121840
    Change-Id: Ic42ad9dce6dc5ec6de97183cb1b275113fe36483
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4928768
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209706}

--- third_party/blink/renderer/core/html/forms/html_listbox_element.h
+++ third_party/blink/renderer/core/html/forms/html_listbox_element.h
@@ -6,18 +6,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_HTML_FORMS_HTML_LISTBOX_ELEMENT_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_HTML_FORMS_HTML_LISTBOX_ELEMENT_H_
 
+#include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
 
 namespace blink {
 
-class HTMLListboxElement : public HTMLElement {
+class CORE_EXPORT HTMLListboxElement : public HTMLElement {
  public:
   explicit HTMLListboxElement(Document&);
 
   InsertionNotificationRequest InsertedInto(ContainerNode&) override;
   void RemovedFrom(ContainerNode& insertion_point) override;
 
-  static bool IsSelectlistAssociated(const Element*);
+  HTMLSelectListElement* OwnerSelectList() const;
 };
 
 }  // namespace blink
