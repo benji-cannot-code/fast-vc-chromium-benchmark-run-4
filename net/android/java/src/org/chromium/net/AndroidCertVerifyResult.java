daf01011a4b590adb8d8193adc5d2401f1144ad1
Sam Maier <smaier@chromium.org>
2023-10-13 19:30:07 +0000

    jni_zero: Migrating all JNI annotations in /net to jni_zero copies
    This CL is the result of a few simple sed commands, switching any import
    statement using any of the 6 JNI annotations, and switching over the gn
    targets from base:jni_java to jni_zero_java.
    
    This CL was uploaded by git cl split.
    
    R=mmenke@chromium.org
    
    Bug: 1456762
    Change-Id: Icb3f2b38bf9207ccb909a9f491e36bbecc40d4c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4939470
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209600}

--- net/android/java/src/org/chromium/net/AndroidCertVerifyResult.java
+++ net/android/java/src/org/chromium/net/AndroidCertVerifyResult.java
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.net;
 
-import org.chromium.base.annotations.CalledByNative;
-import org.chromium.base.annotations.JNINamespace;
+import org.jni_zero.CalledByNative;
+import org.jni_zero.JNINamespace;
 
 import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
--- net/test/android/javatests/src/org/chromium/net/AndroidNetworkLibraryTestUtil.java
+++ net/test/android/javatests/src/org/chromium/net/AndroidNetworkLibraryTestUtil.java
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.net;
 
-import org.chromium.base.annotations.CalledByNativeForTesting;
+import org.jni_zero.CalledByNativeForTesting;
 
 /**
  * Utility functions for testing features implemented in AndroidNetworkLibrary.
