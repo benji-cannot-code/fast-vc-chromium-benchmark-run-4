ec399ce21711fc8e9ba1078415fc04c9f0cfdac4
Federica Germinario <fedegermi@google.com>
2023-10-13 16:20:49 +0000

    [ios] Move TimeSpentInFeedAggregate constant out of NSUserDefaults
    This CL moves TimeSpentInFeedAggregate out of NSUserDefaults.
    
    The key has been moved in the PrefService. Note that there are two
    types of PrefServices, in this case the code has been moved in the
    PrefService coming from BrowserState which is per account and is
    syncable between devices.
    
    Bug: 1486770
    Change-Id: I817880096207f03876126b2f93d7239f7fba4e1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4909873
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209480}

--- ios/chrome/browser/ui/ntp/metrics/feed_metrics_constants.mm
+++ ios/chrome/browser/ui/ntp/metrics/feed_metrics_constants.mm
@@ -26,7 +26,7 @@ NSString* const kLastInteractionTimeForDiscoverGoodVisits =
 NSString* const kLastInteractionTimeForFollowingGoodVisits =
     @"LastInteractionTimeForGoodVisitsFollowing";
 NSString* const kLastDayTimeInFeedReportedKey = @"LastDayTimeInFeedReported";
-NSString* const kTimeSpentInFeedAggregateKey = @"TimeSpentInFeedAggregate";
+const char kTimeSpentInFeedAggregateKey[] = "TimeSpentInFeedAggregate";
 const char kActivityBucketLastReportedDateKey[] =
     "ActivityBucketLastReportedDate";
 NSString* const kActivityBucketLastReportedDateArrayKey =
