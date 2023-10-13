ba3c27f7a1564817a7eec9853811985d0f8325f4
Scott Yoder <scottyoder@google.com>
2023-10-13 19:52:00 +0000

    [ios] Add a divider between Voice and Lens icons in Large Fakebox
    Screenshot:
    https://screenshot.googleplex.com/3FjiZioRN67E4yf
    
    Bug: 1482304
    Change-Id: Iaa819ad3a6dcc54d113e06ca49a2b846b91c027b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935340
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209627}

--- ios/chrome/browser/ui/content_suggestions/ntp_home_egtest.mm
+++ ios/chrome/browser/ui/content_suggestions/ntp_home_egtest.mm
@@ -162,6 +162,10 @@ id<GREYMatcher> notPracticallyVisible() {
   // TODO(crbug.com/1403077): Scrolling issues when promo is enabled.
   config.features_disabled.push_back(kEnableDiscoverFeedTopSyncPromo);
   config.features_disabled.push_back(kIOSSetUpList);
+
+  if ([self isRunningTest:@selector(testLargeFakeboxFocus)]) {
+    config.features_enabled.push_back(kIOSLargeFakebox);
+  }
   return config;
 }
 
@@ -1297,6 +1301,20 @@ id<GREYMatcher> notPracticallyVisible() {
   // Background and foreground the app, then check that the focused omnibox
   // still contains the text.
   [[AppLaunchManager sharedManager] backgroundAndForegroundApp];
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
+      assertWithMatcher:chrome_test_util::OmniboxContainingText(
+                            base::SysNSStringToUTF8(omniboxText))];
+}
+
+// Test that the Large Fakebox can be focused and text can be entered.
+- (void)testLargeFakeboxFocus {
+  // Focus the omnibox and type some text into it.
+  [self focusFakebox];
+  NSString* omniboxText = @"Some text";
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
+      performAction:grey_replaceText(omniboxText)];
+
+  // Check that the omnibox contains the inputted text.
   [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
       assertWithMatcher:chrome_test_util::OmniboxContainingText(
                             base::SysNSStringToUTF8(omniboxText))];
