8306e54840b555a5c346e537f158be399f58d0e8
Peilin Wang <peilinwang@google.com>
2023-10-13 15:32:18 +0000

    [HomepageManager] Force preference keys migration in getters
    Follow up to https://chromium-review.googlesource.com/c/chromium/src/+/4850387
    
    Bug: 1296595
    Change-Id: I3a813e356adf8a554082f83d18196873681360a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935095
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Peilin Wang <peilinwang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1209455}

--- chrome/android/java/src/org/chromium/chrome/browser/homepage/HomepageManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/homepage/HomepageManager.java
@@ -190,6 +190,14 @@ public class HomepageManager implements HomepagePolicyManager.HomepagePolicyStat
         if (!homepagePartnerDefaultUri.equals("")) {
             GURL homepagePartnerDefaultGurl = new GURL(homepagePartnerDefaultUri);
             if (homepagePartnerDefaultGurl.isValid()) {
+                ChromeSharedPreferences.getInstance()
+                        .writeString(
+                                ChromePreferenceKeys.HOMEPAGE_PARTNER_CUSTOMIZED_DEFAULT_GURL,
+                                homepagePartnerDefaultGurl.serialize());
+                ChromeSharedPreferences.getInstance()
+                        .removeKey(
+                                ChromePreferenceKeys
+                                        .DEPRECATED_HOMEPAGE_PARTNER_CUSTOMIZED_DEFAULT_URI);
                 return homepagePartnerDefaultGurl;
             }
         }
@@ -259,6 +267,10 @@ public class HomepageManager implements HomepagePolicyManager.HomepagePolicyStat
         if (!homepageCustomUri.equals("")) {
             GURL homepageCustomGurl = new GURL(homepageCustomUri);
             if (homepageCustomGurl.isValid()) {
+                mSharedPreferencesManager.writeString(
+                        ChromePreferenceKeys.HOMEPAGE_CUSTOM_GURL, homepageCustomGurl.serialize());
+                mSharedPreferencesManager.removeKey(
+                        ChromePreferenceKeys.DEPRECATED_HOMEPAGE_CUSTOM_URI);
                 return homepageCustomGurl;
             }
         }
