3dc74ff0df349ba8fcf26b454ca93360a4bdf825
Yuki Shiino <yukishiino@chromium.org>
2023-10-13 11:00:38 +0000

    bindings: Avoid implicit Member<T>::operator T*()
    Calls member_var.Get() explicitly in order to get rid of
    implicit Member<T>::operator T*().
    
    Bug: 1492410
    Change-Id: I8768a94da7a03c90cff82980c3b05ee43f193b4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4936690
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209336}

--- third_party/blink/renderer/bindings/scripts/bind_gen/dictionary.py
+++ third_party/blink/renderer/bindings/scripts/bind_gen/dictionary.py
@@ -425,7 +425,9 @@ def make_accessor_functions(cg_context):
         func_def.set_base_template_vars(cg_context.template_bindings())
         if not member.is_always_present:
             func_def.body.append(F("DCHECK({}());", member.api_has))
-        func_def.body.append(F("return {};", member.value_var))
+        func_def.body.append(
+            F("return {};",
+              member.type_info.member_var_to_ref_expr(member.value_var)))
         return func_def, None
 
     def make_api_get_or(member):
@@ -440,7 +442,8 @@ def make_accessor_functions(cg_context):
         func_def.body.extend([
             CxxUnlikelyIfNode(cond="!{}()".format(member.api_has),
                               body=T("return fallback_value;")),
-            F("return {};", member.value_var),
+            F("return {};",
+              member.type_info.member_var_to_ref_expr(member.value_var)),
         ])
         return func_def, None
 
@@ -464,7 +467,8 @@ def make_accessor_functions(cg_context):
         copy_func_def.body.extend([
             CxxUnlikelyIfNode(cond="!{}()".format(member.api_has),
                               body=T("return fallback_value;")),
-            F("return {};", member.value_var),
+            F("return {};",
+              member.type_info.member_var_to_ref_expr(member.value_var)),
         ])
 
         move_func_decl = CxxFuncDeclNode(
@@ -482,7 +486,8 @@ def make_accessor_functions(cg_context):
         move_func_def.body.extend([
             CxxUnlikelyIfNode(cond="!{}()".format(member.api_has),
                               body=T("return std::move(fallback_value);")),
-            F("return {};", member.value_var),
+            F("return {};",
+              member.type_info.member_var_to_ref_expr(member.value_var)),
         ])
 
         decls = ListNode([copy_func_decl, move_func_decl])
@@ -503,7 +508,8 @@ def make_accessor_functions(cg_context):
         func_def.body.extend([
             CxxUnlikelyIfNode(cond="!{}()".format(member.api_has),
                               body=T("return fallback_value;")),
-            F("return {};", member.value_var),
+            F("return {};",
+              member.type_info.member_var_to_ref_expr(member.value_var)),
         ])
         return func_def, None
 
@@ -794,7 +800,8 @@ def make_blink_to_v8_function(cg_context):
                         "ToV8(${script_state}, {blink_value})"
                         ".ToLocal(&${v8_value})",
                         native_value_tag=native_value_tag(member.idl_type),
-                        blink_value=member.value_var),
+                        blink_value=member.type_info.member_var_to_ref_expr(
+                            member.value_var)),
                     body=T("return false;")),
                 CxxUnlikelyIfNode(  #
                     cond=_format(
