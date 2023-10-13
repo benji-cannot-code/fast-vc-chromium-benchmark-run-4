0a0acd903577994e4be7bfc9cef0bed026473a18
Dave Tapuska <dtapuska@chromium.org>
2023-10-13 15:23:12 +0000

    [extensions] Move ChannelDelegate and PortId into base class.
    Move some members that will be useful when porting extension messaging
    to mojo into the base class.
    
    Bug: 993189
    Change-Id: Ia3bd8cf22f5c7693d341893334d362250a8cd0ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935296
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209451}

--- extensions/browser/api/messaging/message_port.cc
+++ extensions/browser/api/messaging/message_port.cc
@@ -7,7 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace extensions {
 
-MessagePort::MessagePort() = default;
+MessagePort::MessagePort(base::WeakPtr<ChannelDelegate> channel_delegate,
+                         const PortId& port_id)
+    : weak_channel_delegate_(channel_delegate), port_id_(port_id) {}
+
 MessagePort::~MessagePort() = default;
 
 void MessagePort::RemoveCommonFrames(const MessagePort& port) {}
