3587af03f973913d4e94fe472f981e69dc0811cc
Robbie Gibson <rkgibson@google.com>
2023-10-13 16:35:14 +0000

    [Code Health][iOS] Prepare to refactor ios/c/b/search_engines/ to model
    Bug: 1480348
    Change-Id: Id9495f28c74f83bf3241d86d39ca61d3a36e9f55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4936871
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1209487}

---
+++ ios/chrome/browser/search_engines/model/template_url_service_factory.h
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SEARCH_ENGINES_MODEL_TEMPLATE_URL_SERVICE_FACTORY_H_
+#define IOS_CHROME_BROWSER_SEARCH_ENGINES_MODEL_TEMPLATE_URL_SERVICE_FACTORY_H_
+
+#import "ios/chrome/browser/search_engines/template_url_service_factory.h"
+
+#endif  // IOS_CHROME_BROWSER_SEARCH_ENGINES_MODEL_TEMPLATE_URL_SERVICE_FACTORY_H_
