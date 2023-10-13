a45e17aa12287937340454f4435efa5db94a1f73
Rupert Ben Wiser <bewise@chromium.org>
2023-10-13 19:37:13 +0000

    webview: Add feature flag to enable metrics service only for SDK Runtime
    We have found memory regressions attempting to release this experiment.
    Because of this, we are going to attempt to only release this change
    for the SDK Runtime which is the main motivator for this change.
    
    Bug: 1248039
    Change-Id: I8bb4baf1347468ee29a63c4669d40570f8991f42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4938595
    Auto-Submit: Rupert Wiser <bewise@chromium.org>
    Commit-Queue: Richard (Torne) Coles <torne@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209610}

--- android_webview/browser/aw_feature_map.cc
+++ android_webview/browser/aw_feature_map.cc
@@ -30,6 +30,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kWebViewTestFeature,
     &features::kWebViewJavaJsBridgeMojo,
     &features::kWebViewUseMetricsUploadService,
+    &features::kWebViewUseMetricsUploadServiceOnlySdkRuntime,
     &features::kWebViewXRequestedWithHeaderControl,
     &features::kWebViewXRequestedWithHeaderManifestAllowList,
     &features::kWebViewRestrictSensitiveContent,
