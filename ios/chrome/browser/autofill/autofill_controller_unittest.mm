c4940bdba3179c2998a2762c8439ba6435564ee9
Federica Germinario <fedegermi@google.com>
2023-10-13 14:29:30 +0000

    [ios] Improve initialization of HistogramTester in tests
    Improve initialization of HistogramTester in tests, replacing:
      histogram_tester_.reset(new base::HistogramTester());
    with:
      histogram_tester_ = std::make_unique<base::HistogramTester>();
    
    Bug: none
    Change-Id: I90505400743813e5704d047db0d66d1cb5a491ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935114
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1209420}

--- ios/chrome/browser/autofill/autofill_controller_unittest.mm
+++ ios/chrome/browser/autofill/autofill_controller_unittest.mm
@@ -361,7 +361,7 @@ void AutofillControllerTest::SetUp() {
   [accessory_mediator_ injectWebState:web_state()];
   [accessory_mediator_ injectProvider:suggestion_controller_];
 
-  histogram_tester_.reset(new base::HistogramTester());
+  histogram_tester_ = std::make_unique<base::HistogramTester>();
 }
 
 void AutofillControllerTest::TearDown() {
