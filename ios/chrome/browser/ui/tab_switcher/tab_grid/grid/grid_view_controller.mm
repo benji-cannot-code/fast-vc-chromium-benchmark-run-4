862f763666446cc61b2e8ea420d14d806112c79e
Louis Romero <lpromero@google.com>
2023-10-13 12:07:21 +0000

    Rename GridViewController reloadDataSource
    Bug: 1457146
    Change-Id: I2e9c850e8c6ad13a5d614c8df4bbfa57fc84adbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935173
    Auto-Submit: Louis Romero <lpromero@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209356}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
@@ -248,7 +248,7 @@ typedef NSDiffableDataSourceSnapshot<NSString*, GridItemIdentifier*> Snapshot;
       };
   collectionView.dataSource = self.diffableDataSource;
 
-  [self reloadCollectionViewData];
+  [self reloadDataSource];
 
   // UICollectionViewDropPlaceholder uses a GridCell and needs the class to be
   // registered.
@@ -485,7 +485,7 @@ typedef NSDiffableDataSourceSnapshot<NSString*, GridItemIdentifier*> Snapshot;
 
 - (void)contentWillAppearAnimated:(BOOL)animated {
   self.gridLayout.animatesItemUpdates = YES;
-  [self reloadCollectionViewData];
+  [self reloadDataSource];
   // Selection is invalid if there are no items.
   if ([self shouldShowEmptyState]) {
     [self animateEmptyStateIn];
@@ -574,7 +574,7 @@ typedef NSDiffableDataSourceSnapshot<NSString*, GridItemIdentifier*> Snapshot;
 
 #pragma mark - UICollectionView Diffable Data Source Helpers
 
-- (void)reloadCollectionViewData {
+- (void)reloadDataSource {
   Snapshot* snapshot = [[Snapshot alloc] init];
   [snapshot appendSectionsWithIdentifiers:@[ kOpenTabsSectionIdentifier ]];
   for (TabSwitcherItem* item in self.items) {
@@ -1264,7 +1264,7 @@ typedef NSDiffableDataSourceSnapshot<NSString*, GridItemIdentifier*> Snapshot;
   _selectedEditingItemIDs.clear();
   _selectedSharableEditingItemIDs.clear();
 
-  [self reloadCollectionViewData];
+  [self reloadDataSource];
 
   [self updateSelectedCollectionViewItemRingAndBringIntoView:YES];
 
