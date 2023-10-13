f3d8fe8e4d50f40be634233dc5ae523c52f4b3a0
Benedikt Meurer <bmeurer@chromium.org>
2023-10-13 12:55:14 +0000

    [DevTools] Replace legacy Diff.* globals with imports.
    Bug: chromium:1442410
    Change-Id: I0da4b7520f8d1a85ab9632500ab9447b7b2bcc4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935424
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209372}

--- third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/changes/changes-a11y-test.js
+++ third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/changes/changes-a11y-test.js
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {TestRunner} from 'test_runner';
 import {AxeCoreTestRunner} from 'axe_core_test_runner';
+
+import * as Diff from "devtools/third_party/diff/diff.js";
 import * as UI from 'devtools/ui/legacy/legacy.js';
 
 (async function() {
