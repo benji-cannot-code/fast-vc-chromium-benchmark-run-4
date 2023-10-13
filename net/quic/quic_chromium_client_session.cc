53d918b658f622ab7b48ad2a7e608144dcb1da54
Tsuyoshi Horo <horo@chromium.org>
2023-10-13 02:19:10 +0000

    Roll src/net/third_party/quiche/src/ c780c7040..b867c6940 (10 commits)
    https://quiche.googlesource.com/quiche.git/+log/c780c704064c..b867c6940b93
    
    This CL also updates third_party/anonymous_tokens/src to 47d4233478. It
    is required for https://quiche.googlesource.com/quiche.git/+/c403ad6d.
    
    $ git log c780c7040..b867c6940 --date=short --no-merges --format='%ad %ae %s'
    2023-10-12 bnc Remove unused QuicSpdyClientBase::MaybeAddQuicDataToResend() and class QuicDataToResend.
    2023-10-12 danzh And inline ShouldProcessPendingStreamImmediately() implementation in QuicSession.
    2023-10-12 bnc Remove unused AddPromiseDataToResend() and associated classes and members.
    2023-10-12 bnc Remove spdy_server_push_utils.
    2023-10-12 quiche-dev Add other testonly QUICHE flags defined by Envoy.
    2023-10-12 bnc No public description
    2023-10-11 bnc Move logic that ignores pushed request headers into QuicSpdySession::SpdyFramerVisitor.
    2023-10-11 bnc Remove spdy_session->promised_stream_id() and spdy_session->OnHeadersList() calls from QuicSpdyStream::OnHeadersDecoded().
    2023-10-11 bnc Remove QuicSpdySession::WritePushPromise().
    2023-10-11 elburrito Add Privacy Pass token generation to BlindSignAuth and test clients.
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    Change-Id: If9c0bd1eb23ac561165ffb19dbd604c38aaa7400
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4937192
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209205}

--- net/quic/quic_chromium_client_session.cc
+++ net/quic/quic_chromium_client_session.cc
@@ -55,7 +55,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/spdy/spdy_session.h"
 #include "net/ssl/ssl_connection_status_flags.h"
 #include "net/ssl/ssl_info.h"
-#include "net/third_party/quiche/src/quiche/quic/core/http/spdy_server_push_utils.h"
 #include "net/third_party/quiche/src/quiche/quic/core/quic_stream_priority.h"
 #include "net/third_party/quiche/src/quiche/quic/core/quic_utils.h"
 #include "net/third_party/quiche/src/quiche/quic/platform/api/quic_flags.h"
--- net/quic/quic_http_stream_test.cc
+++ net/quic/quic_http_stream_test.cc
@@ -66,7 +66,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/third_party/quiche/src/quiche/quic/core/crypto/crypto_protocol.h"
 #include "net/third_party/quiche/src/quiche/quic/core/crypto/quic_decrypter.h"
 #include "net/third_party/quiche/src/quiche/quic/core/crypto/quic_encrypter.h"
-#include "net/third_party/quiche/src/quiche/quic/core/http/spdy_server_push_utils.h"
 #include "net/third_party/quiche/src/quiche/quic/core/quic_connection.h"
 #include "net/third_party/quiche/src/quiche/quic/core/quic_utils.h"
 #include "net/third_party/quiche/src/quiche/quic/core/quic_write_blocked_list.h"
