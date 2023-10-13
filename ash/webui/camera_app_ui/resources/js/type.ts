a84cde02e963fca07d38a2c0a396e6178df58f17
Kam Kwankajornkiet <kamchonlathorn@google.com>
2023-10-13 08:41:51 +0000

    CCA: Remove introduction nudge for time-lapse
    Since time-lapse recording is already launched in M117 stable channel,
    we can remove the feature introduction nudge in M120.
    
    Bug: chromium:1489810
    Test: Manually, tast run $DUT camera.CCAUITimeLapse
    Change-Id: If78120dc4212d36ed2c72ea206a8fa602af574e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4937892
    Reviewed-by: Chu-Hsuan Yang <chuhsuan@chromium.org>
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Kam Kwankajornkiet <kamchonlathorn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209295}

--- ash/webui/camera_app_ui/resources/js/type.ts
+++ ash/webui/camera_app_ui/resources/js/type.ts
@@ -495,7 +495,6 @@ export enum LocalStorageKey {
   SAVE_METADATA = 'saveMetadata',
   SHOW_ALL_RESOLUTIONS = 'showAllResolutions',
   SHOW_METADATA = 'showMetadata',
-  TIME_LAPSE_DIALOG_SHOWN = 'timeLapseDialogShown',
   TOGGLE_MIC = 'toggleMic',
 }
 
