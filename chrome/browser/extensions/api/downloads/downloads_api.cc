3224e32922be22b602b9010f75e8abd6280c2bb4
Daniel Rubery <drubery@chromium.org>
2023-10-13 20:30:38 +0000

    Add DownloadCheckResult and DownloadDangerType for local archive inspection
    This CL adds values to each enum for when a Standard Safe Browsing user
    should be prompted to enter the password to a downloaded archive for
    local use.
    
    Bug: 1491184
    Change-Id: I4fc14170d16b37da8b07a6cc2bcfadafb51f6c40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4923696
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209650}

--- chrome/browser/extensions/api/downloads/downloads_api.cc
+++ chrome/browser/extensions/api/downloads/downloads_api.cc
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/common/extensions/api/downloads.h"
+#include "components/download/public/common/download_danger_type.h"
 #include "components/download/public/common/download_interrupt_reasons.h"
 #include "components/download/public/common/download_item.h"
 #include "components/download/public/common/download_url_parameters.h"
@@ -203,13 +204,16 @@ extensions::api::downloads::DangerType ConvertDangerType(
     case download::DOWNLOAD_DANGER_TYPE_DEEP_SCANNED_OPENED_DANGEROUS:
       return extensions::api::downloads::DANGER_TYPE_DEEPSCANNEDOPENEDDANGEROUS;
     case download::DOWNLOAD_DANGER_TYPE_PROMPT_FOR_SCANNING:
-      return extensions::api::downloads::DANGER_TYPE_PROMPTFORSCANING;
+      return extensions::api::downloads::DANGER_TYPE_PROMPTFORSCANNING;
     case download::DOWNLOAD_DANGER_TYPE_BLOCKED_UNSUPPORTED_FILETYPE:
       return extensions::api::downloads::DANGER_TYPE_UNSUPPORTEDFILETYPE;
     case download::DOWNLOAD_DANGER_TYPE_DANGEROUS_ACCOUNT_COMPROMISE:
       return extensions::api::downloads::DANGER_TYPE_ACCOUNTCOMPROMISE;
     case download::DOWNLOAD_DANGER_TYPE_DEEP_SCANNED_FAILED:
       return extensions::api::downloads::DANGER_TYPE_DEEPSCANNEDFAILED;
+    case download::DOWNLOAD_DANGER_TYPE_PROMPT_FOR_LOCAL_PASSWORD_SCANNING:
+      return extensions::api::downloads::
+          DANGER_TYPE_PROMPTFORLOCALPASSWORDSCANNING;
     case download::DOWNLOAD_DANGER_TYPE_MAX:
       NOTREACHED();
       return extensions::api::downloads::DANGER_TYPE_LAST;
