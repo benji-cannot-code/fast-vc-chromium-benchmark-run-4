c65664b55399490398102866e5904663f7cc2d68
Liam Brady <lbrady@google.com>
2023-10-13 18:35:59 +0000

    Clean up imports in fenced_frame_config.cc.
    Change-Id: Iadec0a33a27695cbae03f2aad1a3021f1c210307
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935728
    Reviewed-by: Garrett Tanzer <gtanzer@chromium.org>
    Commit-Queue: Liam Brady <lbrady@google.com>
    Cr-Commit-Position: refs/heads/main@{#1209558}

--- content/browser/fenced_frame/fenced_frame_config.cc
+++ content/browser/fenced_frame/fenced_frame_config.cc
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "content/browser/fenced_frame/fenced_frame_config.h"
-#include "base/feature_list.h"
 #include "base/functional/callback.h"
 #include "base/memory/ref_counted.h"
 #include "base/strings/strcat.h"
@@ -12,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/uuid.h"
 #include "content/browser/fenced_frame/fenced_frame_reporter.h"
 #include "services/network/public/cpp/attribution_reporting_runtime_features.h"
-#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/interest_group/ad_auction_constants.h"
 
 namespace content {
