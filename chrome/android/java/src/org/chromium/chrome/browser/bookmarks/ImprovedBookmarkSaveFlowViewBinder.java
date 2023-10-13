d742076506c6ca1214142860c4df87bb29ddc9e0
Sky Malice <skym@chromium.org>
2023-10-13 15:57:23 +0000

    Strongly type ImprovedBookmarkSaveFlowViewBinder.
    Low-Coverage-Reason: TRIVIAL_CHANGE
    Change-Id: I366b6748c3dfc5ef785a5d84f585833034681fbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4937612
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209466}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/ImprovedBookmarkSaveFlowViewBinder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/ImprovedBookmarkSaveFlowViewBinder.java
@@ -5,15 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.bookmarks;
 
-import android.view.View;
 
 import org.chromium.ui.modelutil.PropertyKey;
 import org.chromium.ui.modelutil.PropertyModel;
 
 /** ViewBinder for the improved bookmarks save flow. */
 class ImprovedBookmarkSaveFlowViewBinder {
-    static void bind(PropertyModel model, View view, PropertyKey propertyKey) {
-        ImprovedBookmarkSaveFlowView improvedSaveFlow = (ImprovedBookmarkSaveFlowView) view;
+    static void bind(
+            PropertyModel model,
+            ImprovedBookmarkSaveFlowView improvedSaveFlow,
+            PropertyKey propertyKey) {
         if (propertyKey == ImprovedBookmarkSaveFlowProperties.BOOKMARK_ROW_CLICK_LISTENER) {
             improvedSaveFlow.setBookmarkRowClickListener(
                     model.get(ImprovedBookmarkSaveFlowProperties.BOOKMARK_ROW_CLICK_LISTENER));
