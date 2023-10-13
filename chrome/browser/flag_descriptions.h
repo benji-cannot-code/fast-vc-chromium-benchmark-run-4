6141e4656ee8e631c0509ee39ca52be62b1c5b51
Maria Kazinova <kazinova@google.com>
2023-10-13 17:39:20 +0000

    [Passwords] Add a feature and a flag for using predictions on saving
    With the flag enabled, password server predictions will be taken into
    account when the form is parsed for saving. With the flag disabled,
    they are only used for parsing on form filling.
    
    Bug: 1489205
    Change-Id: Ic663163fa0ea5498d776122fe5d9976d38ec34fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935180
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209522}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1548,6 +1548,9 @@ extern const char kParallelDownloadingDescription[];
 extern const char kPasswordGenerationExperimentName[];
 extern const char kPasswordGenerationExperimentDescription[];
 
+extern const char kPasswordParsingOnSaveUsesPredictionsName[];
+extern const char kPasswordParsingOnSaveUsesPredictionsDescription[];
+
 extern const char kBacklightOcrName[];
 extern const char kBacklightOcrDescription[];
 
