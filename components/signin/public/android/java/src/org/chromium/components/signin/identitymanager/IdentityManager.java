826f0d682c2031df622c4f37c3f813b689c0c014
Nohemi Fernandez <fernandex@chromium.org>
2023-10-13 12:32:33 +0000

    Use SigninClient::IsClearPrimaryAccountAllowed to prevent signout for supervised users.
    Bug: 1324567
    Change-Id: Id893f427f5a523efc18d8e99f81977df9cc71c2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4926469
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209363}

--- components/signin/public/android/java/src/org/chromium/components/signin/identitymanager/IdentityManager.java
+++ components/signin/public/android/java/src/org/chromium/components/signin/identitymanager/IdentityManager.java
@@ -18,6 +18,7 @@ import org.chromium.components.signin.base.CoreAccountId;
 import org.chromium.components.signin.base.CoreAccountInfo;
 
 import java.util.List;
+
 /**
  * IdentityManager provides access to native IdentityManager's public API to java components.
  */
@@ -184,6 +185,11 @@ public class IdentityManager {
         }
     }
 
+    /** Returns true if the primary account can be cleared/removed from the browser. */
+    public boolean isClearPrimaryAccountAllowed() {
+        return IdentityManagerJni.get().isClearPrimaryAccountAllowed(mNativeIdentityManager);
+    }
+
     /**
      * Called by native to invalidate an OAuth2 token. Please note that the token is invalidated
      * asynchronously.
@@ -208,5 +214,7 @@ public class IdentityManager {
         AccountInfo findExtendedAccountInfoByEmailAddress(long nativeIdentityManager, String email);
         CoreAccountInfo[] getAccountsWithRefreshTokens(long nativeIdentityManager);
         void refreshAccountInfoIfStale(long nativeIdentityManager, CoreAccountId coreAccountId);
+
+        boolean isClearPrimaryAccountAllowed(long nativeIdentityManager);
     }
 }
