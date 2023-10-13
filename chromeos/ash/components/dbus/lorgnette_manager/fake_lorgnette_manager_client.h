2c31a2228dcf89985d96469d465e75a866f40081
Nathan Muggli <nmuggli@google.com>
2023-10-13 16:13:17 +0000

    scanning: Add a wrapper for CancelScan
    The current CancelScan method only works when there is exactly one scan
    active at a time.  This is the use-case for the current scan app.  The
    new documentScan API allows multiple scans at the same time, each one
    having a unique job handle.  Add a wrapper to call the CancelScan method
    using this job handle (via the CancelScanRequest message from the
    lorgnette protobuf API).
    
    Bug: b:297435346
    Change-Id: Ifd73174f085211793ef880e15ac1865a7d51d321
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4926573
    Reviewed-by: Benjamin Gordon <bmgordon@chromium.org>
    Commit-Queue: Nathan Muggli <nmuggli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1209475}

--- chromeos/ash/components/dbus/lorgnette_manager/fake_lorgnette_manager_client.h
+++ chromeos/ash/components/dbus/lorgnette_manager/fake_lorgnette_manager_client.h
@@ -59,6 +59,9 @@ class COMPONENT_EXPORT(LORGNETTE_MANAGER) FakeLorgnetteManagerClient
       override;
   void CancelScan(
       chromeos::VoidDBusMethodCallback completion_callback) override;
+  void CancelScan(const lorgnette::CancelScanRequest& request,
+                  chromeos::DBusMethodCallback<lorgnette::CancelScanResponse>
+                      callback) override;
   void StartScannerDiscovery(
       const lorgnette::StartScannerDiscoveryRequest& request,
       base::RepeatingCallback<void(lorgnette::ScannerListChangedSignal)>
@@ -100,6 +103,10 @@ class COMPONENT_EXPORT(LORGNETTE_MANAGER) FakeLorgnetteManagerClient
   void SetReadScanDataResponse(
       const absl::optional<lorgnette::ReadScanDataResponse>& response);
 
+  // Sets the response returned by CancelScan().
+  void SetCancelScanResponse(
+      const absl::optional<lorgnette::CancelScanResponse>& response);
+
  private:
   absl::optional<lorgnette::ListScannersResponse> list_scanners_response_;
   absl::optional<lorgnette::ScannerCapabilities> capabilities_response_;
@@ -108,6 +115,7 @@ class COMPONENT_EXPORT(LORGNETTE_MANAGER) FakeLorgnetteManagerClient
   absl::optional<lorgnette::StartPreparedScanResponse>
       start_prepared_scan_response_;
   absl::optional<lorgnette::ReadScanDataResponse> read_scan_data_response_;
+  absl::optional<lorgnette::CancelScanResponse> cancel_scan_response_;
   absl::optional<std::vector<std::string>> scan_response_;
 };
 
