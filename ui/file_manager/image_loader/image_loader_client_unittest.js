65cc7a88eb5fe9db09f6af398667e8d03230a992
Luciano Pacheco <lucmult@chromium.org>
2023-10-13 05:23:27 +0000

    Image Loader: Remove deps from test_error_reporting.js
    This was used before async functions and this reduces the dependency
    from ImageLoader on Files app code.
    
    This is a preparation for TS conversion.
    
    Bug: b:289003444
    Change-Id: I1fee37b7ec2c16e9e6035374d971642eabc667f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4938390
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209250}

--- ui/file_manager/image_loader/image_loader_client_unittest.js
+++ ui/file_manager/image_loader/image_loader_client_unittest.js
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {reportPromise} from 'chrome://file-manager/common/js/test_error_reporting.js';
 import {assertFalse, assertTrue} from 'chrome://webui-test/chromeos/chai_assert.js';
 
 import {ImageLoaderClient} from './image_loader_client.js';
@@ -55,46 +54,34 @@ function loadAndCheckCacheUsed(client, url, cache) {
   });
 }
 
-export function testCache(callback) {
+export async function testCache(done) {
   const client = new ImageLoaderClient();
-  reportPromise(
-      loadAndCheckCacheUsed(client, 'http://example.com/image.jpg', true)
-          .then(function(cacheUsed) {
-            assertFalse(cacheUsed);
-            return loadAndCheckCacheUsed(
-                client, 'http://example.com/image.jpg', true);
-          })
-          .then(function(cacheUsed) {
-            assertTrue(cacheUsed);
-          }),
-      callback);
+
+  const cacheUsed =
+      await loadAndCheckCacheUsed(client, 'http://example.com/image.jpg', true);
+  assertFalse(!!cacheUsed);
+  const cacheUsed2 =
+      await loadAndCheckCacheUsed(client, 'http://example.com/image.jpg', true);
+  assertTrue(!!cacheUsed2);
+  done();
 }
 
-export function testNoCache(callback) {
+export async function testNoCache(done) {
   const client = new ImageLoaderClient();
-  reportPromise(
-      loadAndCheckCacheUsed(client, 'http://example.com/image.jpg', false)
-          .then(function(cacheUsed) {
-            assertFalse(cacheUsed);
-            return loadAndCheckCacheUsed(
-                client, 'http://example.com/image.jpg', false);
-          })
-          .then(function(cacheUsed) {
-            assertFalse(cacheUsed);
-          }),
-      callback);
+  const cacheUsed = await loadAndCheckCacheUsed(
+      client, 'http://example.com/image.jpg', false);
+  assertFalse(!!cacheUsed);
+  const cacheUsed2 = await loadAndCheckCacheUsed(
+      client, 'http://example.com/image.jpg', false);
+  assertFalse(!!cacheUsed2);
+  done();
 }
 
-export function testDataURLCache(callback) {
+export async function testDataURLCache(done) {
   const client = new ImageLoaderClient();
-  reportPromise(
-      loadAndCheckCacheUsed(client, 'data:URI', true)
-          .then(function(cacheUsed) {
-            assertFalse(cacheUsed);
-            return loadAndCheckCacheUsed(client, 'data:URI', true);
-          })
-          .then(function(cacheUsed) {
-            assertFalse(cacheUsed);
-          }),
-      callback);
+  const cacheUsed = await loadAndCheckCacheUsed(client, 'data:URI', true);
+  assertFalse(!!cacheUsed);
+  const cacheUsed2 = await loadAndCheckCacheUsed(client, 'data:URI', true);
+  assertFalse(!!cacheUsed2);
+  done();
 }
