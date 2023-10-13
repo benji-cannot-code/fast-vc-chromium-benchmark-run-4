b99ed39c5d54046647d1369cb1a0ffea1180ff20
Kristi Saney <kristislee@google.com>
2023-10-13 19:46:01 +0000

    Add Reading mode webui toolbar flag to chrome://flags
    Bug: 1465029
    Change-Id: Ie223af868bd8eb91d58a289da2af9fb82e629dc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4937310
    Reviewed-by: Jocelyn Tran <jocelyntran@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1209621}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2705,6 +2705,9 @@ extern const char kReadAnythingDescription[];
 extern const char kReadAnythingWithScreen2xName[];
 extern const char kReadAnythingWithScreen2xDescription[];
 
+extern const char kReadAnythingWebUIToolbarName[];
+extern const char kReadAnythingWebUIToolbarDescription[];
+
 extern const char kEnableWebHidOnExtensionServiceWorkerName[];
 extern const char kEnableWebHidOnExtensionServiceWorkerDescription[];
 
