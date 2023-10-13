3fad741d8f710c9d6ccf69f5997c49275aab99bf
Xiaohui Chen <xiaohuic@google.com>
2023-10-13 23:07:31 +0000

    os settings: move cpp files to new location (30/N)
    * Move hats related files
    
    R=assistive-code-review@google.com
    
    Bug: b/284187997
    Change-Id: I00f3c784ca1515164c6421cb1f05f4974ce69132
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4939730
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209728}

--- chrome/browser/ui/webui/ash/settings/services/hats/os_settings_hats_manager.cc
+++ chrome/browser/ui/webui/ash/settings/services/hats/os_settings_hats_manager.cc
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/webui/settings/ash/os_settings_hats_manager.h"
-#include "chrome/browser/ui/webui/settings/ash/os_settings_hats_manager_factory.h"
+#include "chrome/browser/ui/webui/ash/settings/services/hats/os_settings_hats_manager.h"
+#include "chrome/browser/ui/webui/ash/settings/services/hats/os_settings_hats_manager_factory.h"
 
 #include "base/functional/bind.h"
 #include "base/time/time.h"
--- chrome/browser/ui/webui/ash/settings/services/hats/os_settings_hats_manager_factory.cc
+++ chrome/browser/ui/webui/ash/settings/services/hats/os_settings_hats_manager_factory.cc
@@ -4,9 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/no_destructor.h"
-#include "chrome/browser/ui/webui/settings/ash/os_settings_hats_manager_factory.h"
+#include "chrome/browser/ui/webui/ash/settings/services/hats/os_settings_hats_manager_factory.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/webui/settings/ash/os_settings_hats_manager.h"
+#include "chrome/browser/ui/webui/ash/settings/services/hats/os_settings_hats_manager.h"
 
 namespace ash::settings {
 
