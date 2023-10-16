24fa55afb35a7ad9f647dc1fa923a66408706f2b
Vladimir Nechaev <nechaev@chromium.org>
2023-10-16 09:21:19 +0000

    Reland "Add support of session.status PureBiDi command"
    This is a reland of commit 9906eb9d201f0c3a7a1c773bd15e6ca587581401
    
    Original change's description:
    > Add support of session.status PureBiDi command
    >
    > With this commit user is able to establish a WebSocket connection not
    > bound to any session. ChromeDriver is able to handle session.status
    > static BiDi command.
    >
    > Bug: chromedriver:4496
    > Change-Id: Ib70062244f4f2ad8eb18c3d3faca18d8e0ea3341
    > Validate-Test-Flakiness: skip
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905411
    > Reviewed-by: Maksim Sadym <sadym@chromium.org>
    > Commit-Queue: Vladimir Nechaev <nechaev@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1207004}
    
    Bug: chromedriver:4496
    Change-Id: I4ee19cde4cc9675f9eca4646d4e584aef03def7f
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4938596
    Reviewed-by: Thiago Perrotta <tperrotta@chromium.org>
    Commit-Queue: Vladimir Nechaev <nechaev@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210074}

--- chrome/test/chromedriver/client/chromedriver.py
+++ chrome/test/chromedriver/client/chromedriver.py
@@ -3,10 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import sys
-import util
-
 import psutil
+import sys
+import time
+import urllib.parse
+import util
 
 import command_executor
 from command_executor import Command
@@ -359,6 +360,13 @@ class ChromeDriver(object):
 
   def CreateWebSocketConnection(self):
     return WebSocketConnection(self._server_url, self._session_id)
+
+  def CreateWebSocketConnectionIPv6(self):
+    url_components = urllib.parse.urlparse(self._server_url)
+    new_url = urllib.parse.urlunparse(
+        url_components._replace(
+            netloc=('%s:%d' % ('[::1]', url_components.port))))
+    return WebSocketConnection(new_url, self._session_id)
 
   def GetWindowHandles(self):
     return self.ExecuteCommand(Command.GET_WINDOW_HANDLES)
