6afc3a85e89e60b92e460886799dcc4cc0d31e01
Jan Keitel <jkeitel@google.com>
2023-10-16 17:13:16 +0000

    Avoid dangling pointer in ResizeArea test.
    Bug: 1469136
    Change-Id: Ia7e72d22a79b28f976554b3c5214fa682eef157d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4940990
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210259}

--- ui/views/controls/resize_area_unittest.cc
+++ ui/views/controls/resize_area_unittest.cc
@@ -95,7 +95,7 @@ class ResizeAreaTest : public ViewsTestBase {
 
  private:
   std::unique_ptr<TestResizeAreaDelegate> delegate_;
-  raw_ptr<views::Widget, AcrossTasksDanglingUntriaged> widget_ = nullptr;
+  raw_ptr<views::Widget> widget_ = nullptr;
   std::unique_ptr<ui::test::EventGenerator> event_generator_;
 
   // The number of ui::ET_GESTURE_SCROLL_UPDATE events seen by
@@ -146,8 +146,9 @@ void ResizeAreaTest::SetUp() {
 }
 
 void ResizeAreaTest::TearDown() {
-  if (widget_ && !widget_->IsClosed())
-    widget_->Close();
+  if (widget_ && !widget_->IsClosed()) {
+    widget_.ExtractAsDangling()->Close();
+  }
 
   views::ViewsTestBase::TearDown();
 }
