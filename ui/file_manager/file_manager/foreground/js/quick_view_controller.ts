d058401c31e0a813bba346ca1c20315b6c445994
dpapad <dpapad@chromium.org>
2023-10-16 21:13:04 +0000

    WebUI: Rename assert_ts.ts to assert.ts, part 7 (last)
    In this part updating one remaining reference in ui/file_manager/ and
    removing assert_ts.ts.
    
    assert_ts.ts used the "_ts" suffix to differentiate from the previously
    existing assert.js file. The latter no longer exists and the "_ts"
    suffix can be dropped.
    
    Fixed: 1291526
    Change-Id: I7c7f19faa95d9c53bbce7024ebb088f770038506
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935618
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210413}

--- ui/file_manager/file_manager/foreground/js/quick_view_controller.ts
+++ ui/file_manager/file_manager/foreground/js/quick_view_controller.ts
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {ImageLoaderClient} from 'chrome-extension://pmfjbimdmchhbnneeidfognadeopoehp/image_loader_client.js';
 import {LoadImageRequest, LoadImageResponse, LoadImageResponseStatus} from 'chrome-extension://pmfjbimdmchhbnneeidfognadeopoehp/load_image_request.js';
-import {assert} from 'chrome://resources/js/assert_ts.js';
+import {assert} from 'chrome://resources/js/assert.js';
 
 import {DialogType, isModal} from '../../common/js/dialog_type.js';
 import {parseActionId} from '../../common/js/file_tasks.js';
