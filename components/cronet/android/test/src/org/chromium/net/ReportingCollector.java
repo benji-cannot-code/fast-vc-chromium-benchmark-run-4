4d6790ed4336790cb7778839d26d421743a34416
Stefano Duo <stefanoduo@google.com>
2023-10-16 15:36:23 +0000

    ReportingCollector: move assertions within containsReport
    Moving them inside allows us to have better error messages when tests
    will fail.
    
    Bug: 1469900
    Change-Id: Idfe72e3be1f72ae1839258b0cca8318f7b1608ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4748630
    Reviewed-by: Chidera Olibie <colibie@google.com>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1210195}

--- components/cronet/android/test/src/org/chromium/net/ReportingCollector.java
+++ components/cronet/android/test/src/org/chromium/net/ReportingCollector.java
@@ -5,12 +5,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.net;
 
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.truth.Correspondence;
+import com.google.common.truth.Correspondence.BinaryPredicate;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
@@ -19,7 +25,7 @@ import java.util.concurrent.TimeUnit;
  * whether expected reports were actually received.
  */
 class ReportingCollector {
-    private ArrayList<JSONObject> mReceivedReports = new ArrayList<JSONObject>();
+    private List<JSONObject> mReceivedReports = new ArrayList<JSONObject>();
     private Semaphore mReceivedReportsSemaphore = new Semaphore(0);
 
     /**
@@ -47,22 +53,21 @@ class ReportingCollector {
         }
     }
 
-    /**
-     * Checks whether a report with the given payload exists or not.
-     */
-    public boolean containsReport(String expected) {
+    public void assertContainsReport(String expectedString) {
+        JSONObject expectedJson;
+
         try {
-            JSONObject expectedReport = new JSONObject(expected);
-            synchronized (mReceivedReports) {
-                for (JSONObject received : mReceivedReports) {
-                    if (isJSONObjectSubset(expectedReport, received)) {
-                        return true;
-                    }
-                }
-            }
-            return false;
+            expectedJson = new JSONObject(expectedString);
         } catch (JSONException e) {
-            return false;
+            throw new IllegalArgumentException(e);
+        }
+        synchronized (mReceivedReports) {
+            assertThat(mReceivedReports)
+                    .comparingElementsUsing(Correspondence.from(
+                            (BinaryPredicate<JSONObject, JSONObject>) (actual, expected)
+                                    -> isJSONObjectSubset(expected, actual),
+                            "is a subset of"))
+                    .contains(expectedJson);
         }
     }
 
