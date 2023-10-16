a3342aac34a8e2d78d40ce5387a88216f765e4a7
Samuel Huang <huangs@chromium.org>
2023-10-16 04:48:24 +0000

    [SurfacePolish] Fix fake search box (but not sticky box) button colors.
    This CL makes fake search box {mic, lens} button color in Start Surface
    and NTP match the hint text color. Note that sticky box (appears when
    swiping up) icon colors are not yet updated, and needs more work.
    Details:
    * Add |default_icon_color_accent1_container_tint_list| to represent the
      desired context color.
    * SearchBoxMediator: Add |mIsSurfacePolishOmniboxColorEnabled|, and use
      it in onFinishNativeInitialization() to choose color.
    
    Bug: 1489341
    Change-Id: Iaef7b44edd982d5da7a5850db435fea67b57be7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4916534
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Auto-Submit: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210004}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/search/SearchBoxMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/search/SearchBoxMediator.java
@@ -15,6 +15,7 @@ import android.view.ViewGroup;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.graphics.drawable.RoundedBitmapDrawable;
 
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.lens.LensController;
 import org.chromium.chrome.browser.lens.LensEntryPoint;
 import org.chromium.chrome.browser.lens.LensIntentParams;
@@ -25,6 +26,7 @@ import org.chromium.chrome.browser.lifecycle.NativeInitObserver;
 import org.chromium.chrome.browser.omnibox.R;
 import org.chromium.chrome.browser.theme.ThemeUtils;
 import org.chromium.chrome.browser.ui.theme.BrandedColorScheme;
+import org.chromium.chrome.features.start_surface.StartSurfaceConfiguration;
 import org.chromium.ui.base.ViewUtils;
 import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.modelutil.PropertyModel;
@@ -37,6 +39,7 @@ class SearchBoxMediator implements DestroyObserver, NativeInitObserver {
     private final Context mContext;
     private final PropertyModel mModel;
     private final ViewGroup mView;
+    private final boolean mIsSurfacePolishOmniboxColorEnabled;
     private final List<OnClickListener> mVoiceSearchClickListeners = new ArrayList<>();
     private final List<OnClickListener> mLensClickListeners = new ArrayList<>();
     private ActivityLifecycleDispatcher mActivityLifecycleDispatcher;
@@ -46,6 +49,9 @@ class SearchBoxMediator implements DestroyObserver, NativeInitObserver {
         mContext = context;
         mModel = model;
         mView = view;
+        boolean isSurfacePolishEnabled = ChromeFeatureList.sSurfacePolish.isEnabled();
+        mIsSurfacePolishOmniboxColorEnabled = isSurfacePolishEnabled
+                && StartSurfaceConfiguration.SURFACE_POLISH_OMNIBOX_COLOR.getValue();
         PropertyModelChangeProcessor.create(mModel, mView, new SearchBoxViewBinder());
     }
 
@@ -77,8 +83,10 @@ class SearchBoxMediator implements DestroyObserver, NativeInitObserver {
         Drawable drawable = AppCompatResources.getDrawable(mContext, R.drawable.btn_mic);
         mModel.set(SearchBoxProperties.VOICE_SEARCH_DRAWABLE, drawable);
 
-        ColorStateList colorStateList =
-                ThemeUtils.getThemedToolbarIconTint(mContext, BrandedColorScheme.APP_DEFAULT);
+        ColorStateList colorStateList = mIsSurfacePolishOmniboxColorEnabled
+                ? AppCompatResources.getColorStateList(mContext,
+                        R.color.default_icon_color_accent1_container_tint_list)
+                : ThemeUtils.getThemedToolbarIconTint(mContext, BrandedColorScheme.APP_DEFAULT);
         mModel.set(SearchBoxProperties.VOICE_SEARCH_COLOR_STATE_LIST, colorStateList);
     }
 
