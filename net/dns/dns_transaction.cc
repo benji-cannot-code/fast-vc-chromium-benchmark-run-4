1f064257a26720776a5e94a48bdaf676dbf8673d
Kenichi Ishibashi <bashi@chromium.org>
2023-10-16 07:39:03 +0000

    DoH: Restrict the maximum size of the DNS message
    As specified in the RFC [1].
    
    [1] https://datatracker.ietf.org/doc/html/rfc8484#section-6
    
    Bug: 1487704
    Change-Id: I76bcd412068c13712cd1c7281a13726e789b9681
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4941750
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210043}

--- net/dns/dns_transaction.cc
+++ net/dns/dns_transaction.cc
@@ -116,6 +116,10 @@ constexpr net::NetworkTrafficAnnotationTag kTrafficAnnotation =
 
 const char kDnsOverHttpResponseContentType[] = "application/dns-message";
 
+// The maximum size of the DNS message for DoH, per
+// https://datatracker.ietf.org/doc/html/rfc8484#section-6
+const int64_t kDnsOverHttpResponseMaximumSize = 65535;
+
 // Count labels in the fully-qualified name in DNS format.
 int CountLabels(base::span<const uint8_t> name) {
   size_t count = 0;
@@ -540,10 +544,16 @@ class DnsHTTPAttempt : public DnsAttempt, public URLRequest::Delegate {
 
     if (request->response_headers()->HasHeader(
             HttpRequestHeaders::kContentLength)) {
+      if (request_->response_headers()->GetContentLength() >
+          kDnsOverHttpResponseMaximumSize) {
+        ResponseCompleted(ERR_DNS_MALFORMED_RESPONSE);
+        return;
+      }
+
       buffer_->SetCapacity(request_->response_headers()->GetContentLength() +
                            1);
     } else {
-      buffer_->SetCapacity(66560);  // 64kb.
+      buffer_->SetCapacity(kDnsOverHttpResponseMaximumSize + 1);
     }
 
     DCHECK(buffer_->data());
@@ -578,6 +588,11 @@ class DnsHTTPAttempt : public DnsAttempt, public URLRequest::Delegate {
     DCHECK_GE(bytes_read, 0);
 
     if (bytes_read > 0) {
+      if (buffer_->offset() + bytes_read > kDnsOverHttpResponseMaximumSize) {
+        ResponseCompleted(ERR_DNS_MALFORMED_RESPONSE);
+        return;
+      }
+
       buffer_->set_offset(buffer_->offset() + bytes_read);
 
       if (buffer_->RemainingCapacity() == 0) {
