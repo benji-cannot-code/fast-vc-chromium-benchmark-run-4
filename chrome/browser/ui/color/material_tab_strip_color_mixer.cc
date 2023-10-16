c86dc731a163d3b4e549490f352c6f8a33f63325
David Pennington <dpenning@chromium.org>
2023-10-16 23:55:26 +0000

    [ChromeRefresh2023] Update Hover color to be inactive header hover.
    the inactive hover color token didnt exist until recently, adding it to the color mixer for tab strip now.
    
    Bug: 1459699
    Change-Id: I277a5bf31793b67f7abcec092c544577eb569705
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4943755
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Auto-Submit: David Pennington <dpenning@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1210533}

--- chrome/browser/ui/color/material_tab_strip_color_mixer.cc
+++ chrome/browser/ui/color/material_tab_strip_color_mixer.cc
@@ -43,7 +43,7 @@ void AddMaterialTabStripColorMixer(ui::ColorProvider* provider,
 
   // TODO(tbergquist): Use kColorSysStateHeaderHoverInactive, once it exists.
   mixer[kColorTabBackgroundInactiveHoverFrameInactive] = {
-      ui::kColorSysStateHeaderHover};
+      ui::kColorSysStateHeaderHoverInactive};
 
   mixer[kColorTabBackgroundSelectedFrameActive] = {ui::GetResultingPaintColor(
       ui::kColorSysStateHeaderSelect, kColorTabBackgroundInactiveFrameActive)};
