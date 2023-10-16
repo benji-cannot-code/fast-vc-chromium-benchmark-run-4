e13ed25dc28a0a5cd1498c5e8c3e233bb65c7ee5
Joseph Park <josephjoopark@chromium.org>
2023-10-16 23:04:12 +0000

    Refactor action id enum maps for easier testing
    Also contains some code clean up / comments addressed from previous CL
    
    Bug: 1472023
    Change-Id: I4ef4009158380685c90831d8ed6963e7f84ebbcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4937674
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Commit-Queue: Joseph Park <josephjoopark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210495}

--- chrome/browser/ui/toolbar/pinned_toolbar_actions_model_browsertest.cc
+++ chrome/browser/ui/toolbar/pinned_toolbar_actions_model_browsertest.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 const std::vector<absl::optional<std::string>> kTestActionIdStrings =
-    actions::ActionManager::ActionIdsToStrings(
+    actions::ActionIdMap::ActionIdsToStrings(
         {actions::kActionCut, actions::kActionCopy, actions::kActionPaste});
 
 // A simple observer that tracks the number of times certain events occur.
