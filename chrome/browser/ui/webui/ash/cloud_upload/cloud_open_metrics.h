88477776da360159eeabf424977836061e01f4db
cassycc <cassycc@google.com>
2023-10-16 00:32:46 +0000

    [CloudOpenMetrics] Add logged()
    Add Metric::logged() to indicate when a metric has been logged based on
    the Metric::state as there are multiple logged states. This will be used
    in http://crrev.com/c/4935733.
    
    Also update MetricState::Log() to return
    `state == MetricState::kCorrectlyLogged` to reduce lines of code.
    
    Bug: b/300861997
    Change-Id: Ifa1ab65a28f9d4f3253a9468b66381715e258ec1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935732
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1209955}

--- chrome/browser/ui/webui/ash/cloud_upload/cloud_open_metrics.h
+++ chrome/browser/ui/webui/ash/cloud_upload/cloud_open_metrics.h
@@ -93,6 +93,9 @@ class CloudOpenMetrics {
     // state change occurred.
     bool Log(MetricType new_value);
 
+    // Return true if the `state` is a logged state.
+    bool logged();
+
     void set_state(MetricState new_state);
 
     const std::string metric_name;
