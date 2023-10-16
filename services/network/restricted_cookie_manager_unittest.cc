8d29b93425e18a76473a2ddfe348323a6c43e3f2
Dylan Cutler <dylancutler@google.com>
2023-10-16 23:37:05 +0000

    Remove DumpWithoutCrashing from blink::CookieJar::SetCookie
    We have collected around 8 months of data using this, so this
    should provide enough data to debug crbug.com/911299. Given this,
    we can remove the DwC to prevent future bugs being reported in the
    meantime.
    
    Bug: 911299,1414601
    Change-Id: I5f14a7c83677282ed3e2e268eef16a2208541f31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4943472
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210520}

--- services/network/restricted_cookie_manager_unittest.cc
+++ services/network/restricted_cookie_manager_unittest.cc
@@ -239,7 +239,7 @@ class RestrictedCookieManagerSync {
                            const url::Origin& top_frame_origin,
                            bool has_storage_access,
                            const std::string& cookie) {
-    base::test::TestFuture<bool, bool> future;
+    base::test::TestFuture<void> future;
     cookie_service_->SetCookieFromString(url, site_for_cookies,
                                          top_frame_origin, has_storage_access,
                                          cookie, future.GetCallback());
@@ -613,12 +613,9 @@ TEST_P(RestrictedCookieManagerTest, CookieVersion) {
   // Version is still at initial value since nothing modified the cookie.
   EXPECT_EQ(version, mojom::kInitialCookieVersion);
 
-  bool site_for_cookies_ok = false;
-  bool top_frame_origin_ok = false;
   EXPECT_TRUE(backend()->SetCookieFromString(
       kDefaultUrlWithPath, kDefaultSiteForCookies, kDefaultOrigin,
-      /*has_storage_access=*/false, "new-name=new-value;path=/",
-      &site_for_cookies_ok, &top_frame_origin_ok));
+      /*has_storage_access=*/false, "new-name=new-value;path=/"));
 
   EXPECT_TRUE(backend()->GetCookiesString(
       kDefaultUrlWithPath, kDefaultSiteForCookies, kDefaultOrigin,
@@ -1187,14 +1184,9 @@ TEST_P(RestrictedCookieManagerTest, SetCanonicalCookieHttpOnly) {
 }
 
 TEST_P(RestrictedCookieManagerTest, SetCookieFromString) {
-  bool site_for_cookies_ok = false;
-  bool top_frame_origin_ok = false;
   EXPECT_TRUE(backend()->SetCookieFromString(
       kDefaultUrlWithPath, kDefaultSiteForCookies, kDefaultOrigin,
-      /*has_storage_access=*/false, "new-name=new-value;path=/",
-      &site_for_cookies_ok, &top_frame_origin_ok));
-  EXPECT_TRUE(site_for_cookies_ok);
-  EXPECT_TRUE(top_frame_origin_ok);
+      /*has_storage_access=*/false, "new-name=new-value;path=/"));
   auto options = mojom::CookieManagerGetOptions::New();
   options->name = "new-name";
   options->match_type = mojom::CookieMatchType::EQUALS;
@@ -1203,20 +1195,6 @@ TEST_P(RestrictedCookieManagerTest, SetCookieFromString) {
                                   kDefaultOrigin, /*has_storage_access=*/false,
                                   std::move(options)),
       ElementsAre(net::MatchesCookieNameValue("new-name", "new-value")));
-}
-
-TEST_P(RestrictedCookieManagerTest, BadSetCookieFromString) {
-  bool site_for_cookies_ok = true;
-  bool top_frame_origin_ok = true;
-  // Purposely set an invalid cookie to return early before the DCHECK fails.
-  EXPECT_TRUE(backend()->SetCookieFromString(
-      kDefaultUrlWithPath, net::SiteForCookies(),
-      url::Origin::Create(GURL("https://not-example.com")),
-      /*has_storage_access=*/false,
-      "__Host-invalid=host_prefix_with_domain; Domain=example.com",
-      &site_for_cookies_ok, &top_frame_origin_ok));
-  EXPECT_FALSE(site_for_cookies_ok);
-  EXPECT_FALSE(top_frame_origin_ok);
 }
 
 TEST_P(RestrictedCookieManagerTest, SetCanonicalCookieFromWrongOrigin) {
@@ -1264,12 +1242,9 @@ TEST_P(RestrictedCookieManagerTest, SetCanonicalCookieWithMismatchingDomain) {
 
 TEST_P(RestrictedCookieManagerTest, SetCookieFromStringWrongOrigin) {
   ExpectBadMessage();
-  bool site_for_cookies_ok = false;
-  bool top_frame_origin_ok = false;
   EXPECT_TRUE(backend()->SetCookieFromString(
       kOtherUrlWithPath, kDefaultSiteForCookies, kDefaultOrigin,
-      /*has_storage_access=*/false, "new-name=new-value;path=/",
-      &site_for_cookies_ok, &top_frame_origin_ok));
+      /*has_storage_access=*/false, "new-name=new-value;path=/"));
   ASSERT_TRUE(received_bad_message());
 }
 
