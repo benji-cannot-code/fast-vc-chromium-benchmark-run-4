b8913e9861673a393ae51719ff12ac5c57dd393d
Sophie Wen <sophiewen@chromium.org>
2023-10-16 18:28:22 +0000

    snap-groups: Move `SwapWindows()` to SnapGroup
    This moves `SwapWindows()` from SplitViewController to SnapGroup, and
    instead of breaking and recreating the group, simply swaps the windows
    in the group. This is needed since SnapGroupController will either
    start partial overview or add a snap group in `OnWindowSnapped()`, but
    should do neither during a swap.
    
    Note that since the group was previously broken, the divider was also
    broken and recreated, which reset the expanded menu. The expanded menu
    will now persist after the swap button is pressed.
    
    Test: existing
    Fixed: b/305066085
    Change-Id: I7acb79ec4e083983dee3ab1ee6bdaf283a162266
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4939135
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210320}

--- ash/wm/snap_group/snap_group_controller.cc
+++ ash/wm/snap_group/snap_group_controller.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/window_state.h"
 #include "base/check.h"
 #include "base/check_op.h"
+#include "base/containers/adapters.h"
 #include "base/containers/contains.h"
 #include "base/containers/unique_ptr_adapters.h"
 
--- ash/wm/snap_group/snap_group_expanded_menu_view.cc
+++ ash/wm/snap_group/snap_group_expanded_menu_view.cc
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/splitview/split_view_constants.h"
 #include "ash/wm/splitview/split_view_controller.h"
 #include "base/functional/bind.h"
+#include "base/metrics/histogram_functions.h"
+#include "base/metrics/user_metrics.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/compositor/layer.h"
 #include "ui/views/background.h"
@@ -100,7 +102,11 @@ void SnapGroupExpandedMenuView::OnUpdateSecondaryWindowButtonPressed() {
 }
 
 void SnapGroupExpandedMenuView::OnSwapWindowsButtonPressed() {
-  split_view_controller()->SwapWindows(
+  snap_group_->SwapWindows();
+  base::RecordAction(
+      base::UserMetricsAction("SplitView_SwapWindowsButtonSwapWindows"));
+  base::UmaHistogramEnumeration(
+      kSplitViewSwapWindowsSource,
       SplitViewController::SwapWindowsSource::kSnapGroupSwapWindowsButton);
 }
 
