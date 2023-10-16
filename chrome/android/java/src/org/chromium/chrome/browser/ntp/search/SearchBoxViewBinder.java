ddeb1d20e8b2c9374f15021d5efefbd37674f686
Xinyi Ji <xinyiji@google.com>
2023-10-16 22:30:42 +0000

    [SurfacePolish] Updates search text in the fake search box on Start Surface during scrolling-up animation
    This CL updates the size of the search box text to make it animate smoothly during the scrolling-up animation in Start Surface.
    
    The summary of the change is shown in the following link:
    https://drive.google.com/file/d/1c2O6pNS5IknWC5spyzRJrBxIcitD0-Cd/view?usp=sharing&resourcekey=0-4XWQTOizozpZc8_Hshfeug
    
    Bug: 1492252
    Change-Id: I0903ea5b4cc6e1e7681c4a7f924fb69d256d8fc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935339
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210476}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/search/SearchBoxViewBinder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/search/SearchBoxViewBinder.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.ntp.search;
 
 import android.os.Build;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.MarginLayoutParams;
@@ -101,6 +102,9 @@ class SearchBoxViewBinder
         } else if (SearchBoxProperties.SEARCH_TEXT_TRANSLATION_X == propertyKey) {
             searchBoxTextView.setTranslationX(
                     model.get(SearchBoxProperties.SEARCH_TEXT_TRANSLATION_X));
+        } else if (SearchBoxProperties.SEARCH_BOX_TEXT_SIZE == propertyKey) {
+            searchBoxTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP,
+                model.get(SearchBoxProperties.SEARCH_BOX_TEXT_SIZE));
         } else if (SearchBoxProperties.BUTTONS_HEIGHT == propertyKey) {
             int height = model.get(SearchBoxProperties.BUTTONS_HEIGHT);
             ViewGroup.LayoutParams layoutParams = voiceSearchButton.getLayoutParams();
