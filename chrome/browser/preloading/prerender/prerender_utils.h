d6539575142b7d9c94ebb9a75c6cdeedf6135ecb
Hiroki Nakagawa <nhiroki@chromium.org>
2023-10-16 16:48:13 +0000

    Prerender: Remove SearchPrefetchUpgradeToPrerenderIsEnabled()
    After https://crrev.com/c/4643333, when prerendering for default search
    engine is enabled, prefetch/prerender upgrade is also enabled. Therefore
    we no longer need to have a separate check for upgrading.
    
    Bug: 1485775
    Change-Id: Ia9a78093d5387277707a81a3ac079cacce56bb91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4880547
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210239}

--- chrome/browser/preloading/prerender/prerender_utils.h
+++ chrome/browser/preloading/prerender/prerender_utils.h
@@ -32,9 +32,6 @@ bool IsSearchSuggestionPrerenderEnabled();
 
 bool ShouldUpdateCacheEntryManually();
 
-// Whether supporting upgrading a prefetch response to prerender page.
-bool SearchPrefetchUpgradeToPrerenderIsEnabled();
-
 // Whether supporting shareable search prefetch cache. By enabling this feature,
 // prerender won't take the prefetched response away. Instead, it turns to read
 // the response stored in StreamingSearchPrefetchURLLoader.
