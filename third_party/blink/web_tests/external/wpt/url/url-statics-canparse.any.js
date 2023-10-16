69966cc7e0010342b96a37a465ebf8ddeebb4488
Jeremy Roman <jbroman@chromium.org>
2023-10-16 05:16:57 +0000

    url: Implement URL.canParse.
    The WPT is adjusted slightly since it currently exercises behavior
    it was presumably not intended to -- namely, the quirk we have on
    Windows where URLs which begin with a single letter and a colon are
    interpreted as file URLs relative to the given drive letter.
    
    The particulars of URL parsing are adequately tested elsewhere, and
    this test serves to demonstrate that URL.canParse(...) behaves the
    same way that new URL(...) does.
    
    Bug: 1425839
    Change-Id: I13c425c1eaea791da2ccd2a1486112b5c2d6ad0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4936216
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210014}

--- third_party/blink/web_tests/external/wpt/url/url-statics-canparse.any.js
+++ third_party/blink/web_tests/external/wpt/url/url-statics-canparse.any.js
@@ -7,23 +7,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     "expected": false
   },
   {
-    "url": "a:b",
+    "url": "aaa:b",
     "base": undefined,
     "expected": true
   },
   {
     "url": undefined,
-    "base": "a:b",
+    "base": "aaa:b",
     "expected": false
   },
   {
-    "url": "a:/b",
+    "url": "aaa:/b",
     "base": undefined,
     "expected": true
   },
   {
     "url": undefined,
-    "base": "a:/b",
+    "base": "aaa:/b",
     "expected": true
   },
   {
