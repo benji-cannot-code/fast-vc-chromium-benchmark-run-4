5d28f4b3980cc5c913e7d736b6bf0032bb16d74f
Florian Leimgruber <fleimgruber@google.com>
2023-10-16 08:36:37 +0000

    Compute kEditedToSimilarValue observations case-insensitively
    As claimed by the documentation [1], kEditedToSimilarValue profile token
    quality observations do case-insensitive string comparisons. This is
    not how it's implemented right now.
    
    This CL fixes the implementation by calling IsWithinLevenshteinDistance
    on the lower case strings.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:components/autofill/core/browser/profile_token_quality.h;l=69-71;drc=3b2dd26e595950165f25d3bb4722db2c8ae4cbca
    
    Bug: 1453650
    Change-Id: I9ae5cdb0daed5575e775fc0ff23395bc23d7a3d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4937090
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Auto-Submit: Florian Leimgruber <fleimgruber@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1210058}

--- components/autofill/core/browser/profile_token_quality.cc
+++ components/autofill/core/browser/profile_token_quality.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/rand_util.h"
 #include "base/ranges/algorithm.h"
+#include "base/strings/string_util.h"
 #include "base/types/cxx23_to_underlying.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/autofill/core/browser/field_types.h"
@@ -88,7 +89,8 @@ ObservationType GetObservationTypeForEditedField(
   }
 
   if (IsWithinLevenshteinDistance(
-          profile.GetInfo(type, app_locale), edited_value,
+          base::ToLowerASCII(profile.GetInfo(type, app_locale)),
+          base::ToLowerASCII(edited_value),
           ProfileTokenQuality::kMaximumLevenshteinDistance)) {
     return ObservationType::kEditedToSimilarValue;
   }
