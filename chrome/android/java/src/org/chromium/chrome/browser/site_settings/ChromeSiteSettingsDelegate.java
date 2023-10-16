da239aec041c0eb9a7c0fc767b5bfefe61204f64
Nicola Tommasi <tommasin@chromium.org>
2023-10-16 12:45:04 +0000

    [Kartoffel][Clank] Clean up PrivacySandboxSettings4 flag usage - v2
    Continuing the cleanup work on Clank.
    
    Bug: 1449833
    Change-Id: I48492d1b11c2e5af8a853865bea3180217c23c49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935117
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Auto-Submit: Nicola Tommasi <tommasin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210142}

--- chrome/android/java/src/org/chromium/chrome/browser/site_settings/ChromeSiteSettingsDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/site_settings/ChromeSiteSettingsDelegate.java
@@ -156,11 +156,6 @@ public class ChromeSiteSettingsDelegate implements SiteSettingsDelegate {
     }
 
     @Override
-    public boolean isPrivacySandboxSettings4Enabled() {
-        return ChromeFeatureList.isEnabled(ChromeFeatureList.PRIVACY_SANDBOX_SETTINGS_4);
-    }
-
-    @Override
     public boolean isUserBypassUIEnabled() {
         return ChromeFeatureList.isEnabled(ChromeFeatureList.USER_BYPASS_UI);
     }
@@ -228,11 +223,7 @@ public class ChromeSiteSettingsDelegate implements SiteSettingsDelegate {
         if (mPrivacySandboxController == null) return;
 
         // Only show the snackbar when Privacy Sandbox APIs are enabled.
-        if (isPrivacySandboxSettings4Enabled()) {
-            if (!isAnyPrivacySandboxApiEnabledV4()) return;
-        } else {
-            if (!PrivacySandboxBridge.isPrivacySandboxEnabled()) return;
-        }
+        if (!isAnyPrivacySandboxApiEnabledV4()) return;
 
         if (PrivacySandboxBridge.isPrivacySandboxRestricted()) return;
 
