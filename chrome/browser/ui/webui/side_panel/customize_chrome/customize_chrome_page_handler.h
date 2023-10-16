83a8fd97a6ae4d6b3c7c4fcdbffd82dba3bed521
Riley Tatum <rtatum@google.com>
2023-10-16 21:53:18 +0000

    remove unused wallpaper search methods
    This removes the methods used by the wallpaper search PoC that we aren't
    using in the full version.
    
    Bug: b/305306561
    Change-Id: Ie0b1369ceaac99bc7623d5f07d540492da806a85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4939780
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210459}

--- chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_page_handler.h
+++ chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_page_handler.h
@@ -111,8 +111,6 @@ class CustomizeChromePageHandler
   void UpdateModulesSettings() override;
   void UpdateScrollToSection() override;
   void GetDescriptors(GetDescriptorsCallback callback) override;
-  void SearchWallpaper(const std::string& query,
-                       SearchWallpaperCallback callback) override;
   void GetWallpaperSearchResults(
       const std::string& descriptor_a,
       const absl::optional<std::string>& descriptor_b,
@@ -124,9 +122,6 @@ class CustomizeChromePageHandler
  private:
   void OnDescriptorsRetrieved(std::unique_ptr<std::string> response_body);
   void OnDescriptorsJsonParsed(data_decoder::DataDecoder::ValueOrError result);
-  void WallpaperSearchCallback(
-      SearchWallpaperCallback callback,
-      optimization_guide::OptimizationGuideModelExecutionResult result);
   void OnWallpaperSearchResultsRetrieved(
       GetWallpaperSearchResultsCallback callback,
       optimization_guide::OptimizationGuideModelExecutionResult result);
