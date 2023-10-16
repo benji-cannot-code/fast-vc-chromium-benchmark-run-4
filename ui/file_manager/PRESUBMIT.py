00d574dc42066cd1de9c8acced09d2d9b15a0b26
Wenbo Jie <wenbojie@chromium.org>
2023-10-16 04:47:39 +0000

    Reland "[FilesRefresh] Use gm3 CSS files to replace old ones"
    This is a reland of commit e341c1d7bdeed18f5332b9b2a2afa407b2f66512
    The previous change caused a flakiness for a directory tree browser
    test (which was fixed in CL:4929176), this CL now only handles the
    CSS replacement).
    
    Original change's description:
    > [FilesRefresh] Use gm3 CSS files to replace old ones
    >
    > As GM3 is already released, we don't need to maintain 2 CSS
    > files, this CL removes the old CSS files and renames the _gm3
    > back to the original names.
    > The CL also removes the different serving logic related to GM3
    > and the presubmit rules related to GM3.
    >
    > Note: ignore the diff changes for CSS files, what I did is
    > * delete the old CSS file (e.g. file_manager.css)
    > * rename the GM3 CSS file (e.g. rename file_manager_gm3.css to
    >   file_manager.css)
    > so Git treats like we change the file content of
    > file_manager.css directly, which is not the case.
    >
    > Bug: b:301544675
    > Change-Id: Icded47f7dff9f3902e6ec4c7633884958246b413
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4886414
    > Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    > Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1207427}
    
    Bug: b:301544675
    Change-Id: I3e80de91bd14a9f159c012eb1548e599edf90bad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4924972
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210003}

--- ui/file_manager/PRESUBMIT.py
+++ ui/file_manager/PRESUBMIT.py
@@ -17,10 +17,7 @@ def ChecksPatchFormatted(input_api, output_api):
 def ChecksUnitTests(input_api, output_api):
     # Run all unit tests under ui/file_manager/base folder.
     return input_api.canned_checks.RunUnitTestsInDirectory(
-        input_api,
-        output_api,
-        'base',
-        files_to_check=TEST_PATTERNS)
+        input_api, output_api, 'base', files_to_check=TEST_PATTERNS)
 
 
 def ChecksCommon(input_api, output_api):
@@ -42,8 +39,6 @@ def ChecksCommon(input_api, output_api):
 
         sys.path += [input_api.os_path.join(cwd)]
         import base.presubmit_support
-        results += base.presubmit_support._CheckGM3Counterpart(
-            input_api, output_api)
         results += base.presubmit_support._CheckNoDirectLitImport(
             input_api, output_api)
     finally:
