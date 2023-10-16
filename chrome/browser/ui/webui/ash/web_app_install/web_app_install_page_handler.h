199c7370e30b58467c189e909e6b9170a8611234
Louise Brett <loubrett@google.com>
2023-10-16 23:39:00 +0000

    Plumb close dialog method through mojom
    chrome.send() should not be used on ChromeOS so this CL replaces that
    call with a mojo method.
    
    Bug: 1488697, b/302413269
    Change-Id: Ifd86c1aa6f6317f53148fde869870552647aaaef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4938256
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Louise Brett <loubrett@google.com>
    Cr-Commit-Position: refs/heads/main@{#1210521}

--- chrome/browser/ui/webui/ash/web_app_install/web_app_install_page_handler.h
+++ chrome/browser/ui/webui/ash/web_app_install/web_app_install_page_handler.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_ASH_WEB_APP_INSTALL_WEB_APP_INSTALL_PAGE_HANDLER_H_
 #define CHROME_BROWSER_UI_WEBUI_ASH_WEB_APP_INSTALL_WEB_APP_INSTALL_PAGE_HANDLER_H_
 
+#include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/webui/ash/web_app_install/web_app_install.mojom.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
@@ -19,16 +20,22 @@ namespace ash::web_app_install {
 // the browser process exposing various methods for the JS to invoke.
 class WebAppInstallPageHandler : public mojom::PageHandler {
  public:
+  using CloseDialogCallback = base::OnceCallback<void()>;
   explicit WebAppInstallPageHandler(
-      mojo::PendingReceiver<mojom::PageHandler> pending_page_handler);
+      mojo::PendingReceiver<mojom::PageHandler> pending_page_handler,
+      CloseDialogCallback close_dialog_callback);
 
   WebAppInstallPageHandler(const WebAppInstallPageHandler&) = delete;
   WebAppInstallPageHandler& operator=(const WebAppInstallPageHandler&) = delete;
 
   ~WebAppInstallPageHandler() override;
 
+  // mojom::PageHandler:
+  void CloseDialog() override;
+
  private:
   mojo::Receiver<mojom::PageHandler> receiver_;
+  CloseDialogCallback close_dialog_callback_;
 
   base::WeakPtrFactory<WebAppInstallPageHandler> weak_ptr_factory_{this};
 };
