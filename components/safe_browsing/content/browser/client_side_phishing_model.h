2e3fec2a3dc22e72831e8987e23d227ce94180db
Zinovy Nis <zynis@yandex-team.ru>
2023-10-16 17:40:45 +0000

    [CSD] Minor cleanups & fixes
    Change-Id: Idc76b9275488697295c7e549341295cfac8c9d4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4936009
    Auto-Submit: Zinovy Nis <zynis@yandex-team.ru>
    Commit-Queue: Nathan Parker <nparker@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210279}

--- components/safe_browsing/content/browser/client_side_phishing_model.h
+++ components/safe_browsing/content/browser/client_side_phishing_model.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "base/thread_annotations.h"
 #include "components/optimization_guide/core/optimization_target_model_observer.h"
-#include "components/safe_browsing/content/browser/client_side_phishing_model.h"
 #include "components/safe_browsing/core/common/fbs/client_model_generated.h"
 #include "components/safe_browsing/core/common/proto/client_model.pb.h"
 #include "components/safe_browsing/core/common/proto/csd.pb.h"
