f3d26f77388755a043cc04498b2280b344c03183
Xiaohui Chen <xiaohuic@google.com>
2023-10-16 23:28:28 +0000

    os settings: move cpp files to new location (33/N)
    * Move system_preferences related files
    
    R=assistive-code-review@google.com
    
    Bug: b/284187997
    Change-Id: Ia4ab00ed75ac14215aacef0bea8bb47f5abd6f81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4940050
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210513}

--- chrome/browser/ui/webui/ash/settings/pages/system_preferences/system_preferences_section.h
+++ chrome/browser/ui/webui/ash/settings/pages/system_preferences/system_preferences_section.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_WEBUI_SETTINGS_ASH_SYSTEM_PREFERENCES_SECTION_H_
-#define CHROME_BROWSER_UI_WEBUI_SETTINGS_ASH_SYSTEM_PREFERENCES_SECTION_H_
+#ifndef CHROME_BROWSER_UI_WEBUI_ASH_SETTINGS_PAGES_SYSTEM_PREFERENCES_SYSTEM_PREFERENCES_SECTION_H_
+#define CHROME_BROWSER_UI_WEBUI_ASH_SETTINGS_PAGES_SYSTEM_PREFERENCES_SYSTEM_PREFERENCES_SECTION_H_
 
 #include "base/values.h"
 #include "chrome/browser/ui/webui/ash/settings/pages/date_time/date_time_section.h"
@@ -59,4 +59,4 @@ class SystemPreferencesSection : public OsSettingsSection {
 
 }  // namespace ash::settings
 
-#endif  // CHROME_BROWSER_UI_WEBUI_SETTINGS_ASH_SYSTEM_PREFERENCES_SECTION_H_
+#endif  // CHROME_BROWSER_UI_WEBUI_ASH_SETTINGS_PAGES_SYSTEM_PREFERENCES_SYSTEM_PREFERENCES_SECTION_H_
--- chrome/browser/ui/webui/settings/ash/os_settings_sections.cc
+++ chrome/browser/ui/webui/settings/ash/os_settings_sections.cc
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/settings/pages/privacy/privacy_section.h"
 #include "chrome/browser/ui/webui/ash/settings/pages/reset/reset_section.h"
 #include "chrome/browser/ui/webui/ash/settings/pages/search/search_section.h"
-#include "chrome/browser/ui/webui/settings/ash/system_preferences_section.h"
+#include "chrome/browser/ui/webui/ash/settings/pages/system_preferences/system_preferences_section.h"
 #include "chromeos/ash/components/phonehub/phone_hub_manager.h"
 
 namespace ash::settings {
