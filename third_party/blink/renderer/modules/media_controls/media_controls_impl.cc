9ea13ce82ba4597d4e6214c572bbc5913ed62c68
Tommy Steimel <steimel@chromium.org>
2023-10-16 15:39:03 +0000

    Media Controls: Don't open the volume slider when there is no audio
    This CL prevents opening the volume slider on the Blink media controls
    when there is no audio to control.
    
    Bug: 1492585
    Change-Id: I469caf03b68b030419a1106b351339a4b02944dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4940006
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210196}

--- third_party/blink/renderer/modules/media_controls/media_controls_impl.cc
+++ third_party/blink/renderer/modules/media_controls/media_controls_impl.cc
@@ -2154,8 +2154,13 @@ void MediaControlsImpl::CloseVolumeSliderIfNecessary() {
 }
 
 bool MediaControlsImpl::ShouldOpenVolumeSlider() const {
-  if (!volume_slider_)
+  if (!volume_slider_) {
     return false;
+  }
+
+  if (!MediaElement().HasAudio()) {
+    return false;
+  }
 
   return !PreferHiddenVolumeControls(GetDocument());
 }
