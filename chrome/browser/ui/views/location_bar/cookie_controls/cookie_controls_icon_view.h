116325267ac30a2e062e26bc80eaf06c31bea31a
Dirk Pranke <dpranke@google.com>
2023-10-16 21:27:18 +0000

    Enable keyboard-triggered tooltips in Views.
    This adds support for keyboard-triggered tooltips to Views; with
    this, whenever a View is focused (i.e., View::Focus is called), we will now temporarily display a tooltip if the View has text for one, much as if we had hovered over the view instead.
    
    Bug: 1422046, 1068938
    Change-Id: Ic72801b210132f8aac8c731266f783e52065f01e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4814731
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1210435}

--- chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_icon_view.h
+++ chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_icon_view.h
@@ -56,6 +56,7 @@ class CookieControlsIconView : public PageActionIconView,
  protected:
   void OnExecuting(PageActionIconView::ExecuteSource source) override;
   const gfx::VectorIcon& GetVectorIcon() const override;
+  void UpdateTooltipForFocus() override;
 
  private:
   friend class CookieControlsIconViewUnitTest;
