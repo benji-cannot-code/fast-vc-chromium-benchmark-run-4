86097b96651a429fd9485d3bec6a73d3c1d9514c
Christoph Schwering <schwering@google.com>
2023-10-16 10:13:01 +0000

    [Autofill] Add ForTesting suffix to PDM::ClearAllServerData()
    Bug: 1007974
    Change-Id: Ic0ec663bd232f813440a5a66b927fe6bb4a5d238
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935868
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1210096}

--- components/autofill/core/browser/personal_data_manager.h
+++ components/autofill/core/browser/personal_data_manager.h
@@ -299,7 +299,7 @@ class PersonalDataManager : public KeyedService,
   void ResetFullServerCards();
 
   // Deletes all server profiles and cards (both masked and unmasked).
-  void ClearAllServerData();
+  void ClearAllServerDataForTesting();
 
   // Deletes all local profiles and cards.
   virtual void ClearAllLocalData();
