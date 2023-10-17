1cad037796ce0b756ed78bc932fd74c5791b1625
Rebekah Potter <rbpotter@chromium.org>
2023-10-17 17:19:39 +0000

    WebUI: Migrate more chrome://resources mojo files to TypeScript
    Migrating some resources used on ChromeOS that are not required by
    Closure Compiler targets (i.e. only used by TypeScript UIs).
    
    This requires updating a few test files which are still in JavaScript
    to avoid type-only imports, because interfaces used only for type
    checking are removed by the TypeScript compiler for runtime. TS
    compiler elides imports of such types in TS files, but cannot do so for
    JS files, so these imports must be manually removed in JS files.
    
    Bug: 1002798
    Change-Id: I597c43e34b200c14153823de6ef144a1c6bd78b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4932523
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210935}

--- chrome/test/data/webui/nearby_share/nearby_share_app_test.js
+++ chrome/test/data/webui/nearby_share/nearby_share_app_test.js
@@ -5,11 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://nearby/app.js';
 import 'chrome://webui-test/mojo_webui_test_support.js';
+import 'chrome://resources/mojo/chromeos/ash/services/nearby/public/mojom/nearby_share_settings.mojom-webui.js';
 
 import {setContactManagerForTesting} from 'chrome://nearby/shared/nearby_contact_manager.js';
 import {setNearbyShareSettingsForTesting} from 'chrome://nearby/shared/nearby_share_settings.js';
 import {loadTimeData} from 'chrome://resources/ash/common/load_time_data.m.js';
-import {NearbyShareSettingsInterface} from 'chrome://resources/mojo/chromeos/ash/services/nearby/public/mojom/nearby_share_settings.mojom-webui.js';
 import {waitAfterNextRender} from 'chrome://webui-test/polymer_test_util.js';
 
 import {assertEquals, assertFalse, assertTrue} from '../chromeos/chai_assert.js';
@@ -20,7 +20,7 @@ import {FakeNearbyShareSettings} from './shared/fake_nearby_share_settings.js';
 suite('ShareAppTest', function() {
   /** @type {!NearbyShareAppElement} */
   let shareAppElement;
-  /** @type {!NearbyShareSettingsInterface} */
+  /** @type {!FakeNearbyShareSettings} */
   let fakeSettings;
 
   /** @param {!string} page Page to check if it is active. */
--- chrome/test/data/webui/nearby_share/shared/fake_nearby_share_settings.js
+++ chrome/test/data/webui/nearby_share/shared/fake_nearby_share_settings.js
@@ -7,12 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Fake implementation of NearbyShareSettings for testing.
  */
 
-import {DataUsage, DeviceNameValidationResult, FastInitiationNotificationState, NearbyShareSettingsInterface, NearbyShareSettingsObserverInterface, Visibility} from 'chrome://resources/mojo/chromeos/ash/services/nearby/public/mojom/nearby_share_settings.mojom-webui.js';
+import {DataUsage, DeviceNameValidationResult, FastInitiationNotificationState, Visibility} from 'chrome://resources/mojo/chromeos/ash/services/nearby/public/mojom/nearby_share_settings.mojom-webui.js';
 
 /**
  * Fake implementation of NearbyShareSettingsInterface
- *
- * @implements {NearbyShareSettingsInterface}
  */
 export class FakeNearbyShareSettings {
   constructor() {
@@ -31,7 +29,10 @@ export class FakeNearbyShareSettings {
     this.visibility_ = Visibility.kAllContacts;
     /** @private {!Array<!string>} */
     this.allowedContacts_ = [];
-    /** @private {!NearbyShareSettingsObserverInterface} */
+    /**
+     * Restore NearbyShareSettingsObserverInterface type when migrated to TS.
+     * @private {!Object}
+     */
     this.observer_;
     /** @private {!DeviceNameValidationResult} */
     this.nextDeviceNameResult_ = DeviceNameValidationResult.kValid;
@@ -44,8 +45,8 @@ export class FakeNearbyShareSettings {
   }
 
   /**
-   * @param { !NearbyShareSettingsObserverInterface }
-   *     observer
+   * Restore NearbyShareSettingsObserverInterface type when migrated to TS.
+   * @param { !Object } observer
    */
   addSettingsObserver(observer) {
     // Just support a single observer for testing.
