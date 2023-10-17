4d0e70a347ca13ca90fabb6a9322dae27f153997
Ryan Sultanem <rsult@google.com>
2023-10-17 12:06:52 +0000

    [ChromeSigninIntercept] Add the new intercept type and trigger
    Linked to UnoDesktop feature flag.
    New intercept type: ChromeSigninIntercept. Triggered when a web signin
    occurs with no account signed in in chrome. Only triggers on first
    account that signs in.
    Extended histogram and histogram values to the existing interecept
    records.
    
    Currently displays the profile creation bubble content for debugging
    and simplicity. Does not react to the Accept part. Added some TODOs for
    the needed changes.
    
    Follow up work:
    - Create the Chrome Signin specific URL content and handler.
    - React to the Accept part properly.
    
    Bug: b/301431278
    Change-Id: I30e0ccb120e341cc27b3bec4f99a132b64955a4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4926868
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Gabriel Oliveira <gabolvr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1210766}

--- chrome/browser/signin/web_signin_interceptor.cc
+++ chrome/browser/signin/web_signin_interceptor.cc
@@ -5,11 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/signin/web_signin_interceptor.h"
 
-#include <string>
-
-#include "base/metrics/histogram_functions.h"
 #include "components/signin/public/identity_manager/account_info.h"
-#include "components/signin/public/identity_manager/identity_manager.h"
 
 ScopedWebSigninInterceptionBubbleHandle::
     ~ScopedWebSigninInterceptionBubbleHandle() = default;
@@ -23,7 +19,8 @@ bool SigninInterceptionHeuristicOutcomeIsSuccess(
          outcome ==
              SigninInterceptionHeuristicOutcome::kInterceptEnterpriseForced ||
          outcome == SigninInterceptionHeuristicOutcome::
-                        kInterceptEnterpriseForcedProfileSwitch;
+                        kInterceptEnterpriseForcedProfileSwitch ||
+         outcome == SigninInterceptionHeuristicOutcome::kInterceptChromeSignin;
 }
 
 WebSigninInterceptor::Delegate::BubbleParameters::BubbleParameters(
