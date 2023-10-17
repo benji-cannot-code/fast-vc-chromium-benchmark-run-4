7875b46a19e1964ef1028cd01d5bbed81cfdf055
Erik Chen <erikchen@chromium.org>
2023-10-17 23:14:42 +0000

    Expose partial low end mode feature for ChromeOS.
    The feature was first developed for Android. The feature is disabled by
    default on ChromeOS.
    
    Note that the UserLevelMemoryPressureSignal is untouched, since it is
    unneeded on ChromeOS, and also not integrated with partial low end mode.
    
    Change-Id: I9acf397c6e914f64355bd78964502bba18dddd8e
    Bug: b/302044966
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4917876
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211177}

--- components/services/paint_preview_compositor/paint_preview_compositor_collection_impl.cc
+++ components/services/paint_preview_compositor/paint_preview_compositor_collection_impl.cc
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace paint_preview {
 
 namespace {
-#if BUILDFLAG(IS_ANDROID)
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS)
 // A parameter to exclude or not exclude PaintPreviewCompositor from
 // PartialLowModeOnMidRangeDevices. This is used to see how
 // PaintPreviewCompositor affects
@@ -37,7 +37,7 @@ namespace {
 const base::FeatureParam<bool> kPartialLowEndModeExcludePaintPreviewCompositor{
     &base::features::kPartialLowEndModeOnMidRangeDevices,
     "exclude-paint-preview-compositor", false};
-#endif  // BUILDFLAG(IS_ANDROID)
+#endif  // BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS)
 
 // Record whether the compositor is in shutdown. Discardable memory allocations
 // manifest as OOMs during shutdown due to failure to send IPC messages. By
@@ -61,7 +61,7 @@ PaintPreviewCompositorCollectionImpl::PaintPreviewCompositorCollectionImpl(
   // Adapted from content::InitializeSkia().
   // TODO(crbug/1199857): Tune these limits.
   constexpr int kMB = 1024 * 1024;
-#if BUILDFLAG(IS_ANDROID)
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS)
   bool is_low_end_mode =
       base::SysInfo::IsLowEndDeviceOrPartialLowEndModeEnabled(
           kPartialLowEndModeExcludePaintPreviewCompositor);
@@ -71,7 +71,7 @@ PaintPreviewCompositorCollectionImpl::PaintPreviewCompositorCollectionImpl(
   SkGraphics::SetResourceCacheSingleAllocationByteLimit(16 * kMB);
 #else
   SkGraphics::SetResourceCacheSingleAllocationByteLimit(64 * kMB);
-#endif  // BUILDFLAG(IS_ANDROID)
+#endif  // BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS)
 
   if (!initialize_environment_)
     return;
