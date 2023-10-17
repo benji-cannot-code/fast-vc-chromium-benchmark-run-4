942987f2142b1bd779133ec2312e186707b05b26
Tomasz Wiszkowski <ender@google.com>
2023-10-17 21:06:15 +0000

    [Cleanup] Reformat omnibox/U* using google-java-format.
    Manual changes include bullet list formatting. Reformatted using:
    google-java-format --aosp -i {styles,suggestions}/**/*.java
    
    More info: go/chrome-google-java-format-lsc
    
    Bug: 1491626
    Change-Id: I6ba92e1b51c2d7e19d875b70538223ee6271c909
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4949761
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1211092}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlBarEditingTextStateProvider.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlBarEditingTextStateProvider.java
@@ -5,9 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.omnibox;
 
-/**
- * Provider of editing text state from the UrlBar/Omnibox.
- */
+/** Provider of editing text state from the UrlBar/Omnibox. */
 public interface UrlBarEditingTextStateProvider {
     /** Return the starting selection index for the text. */
     public int getSelectionStart();
--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlFocusChangeListener.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlFocusChangeListener.java
@@ -5,19 +5,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.omnibox;
 
-/**
- * Listener to be notified on url focus changes.
- */
+/** Listener to be notified on url focus changes. */
 public interface UrlFocusChangeListener {
     /**
      * Triggered when the URL input field has gained or lost focus.
+     *
      * @param hasFocus Whether the URL field has gained focus.
      */
     void onUrlFocusChange(boolean hasFocus);
 
     /**
      * A notification that animations for focusing or unfocusing the input field has finished.
+     *
      * @param hasFocus Whether the URL field has gained focus.
      */
-    default void onUrlAnimationFinished(boolean hasFocus) {};
+    default void onUrlAnimationFinished(boolean hasFocus) {}
+    ;
 }
