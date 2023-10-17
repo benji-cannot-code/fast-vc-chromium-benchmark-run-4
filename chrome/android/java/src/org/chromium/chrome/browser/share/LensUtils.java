7ed2180d221084609f14e142537335658ee6d627
Anudeep Palanki <apalanki@google.com>
2023-10-17 18:14:39 +0000

    [Lens] Revert context menu string changes Clank
    Reverting the context menu string changes introduced in https://chromium-review.googlesource.com/c/chromium/src/+/4182328
    
    Bug: b/289085606
    Change-Id: I275300e26d791159a5b9f089f82cb60818d68384
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4943140
    Commit-Queue: Anudeep Palanki <apalanki@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1210974}

--- chrome/android/java/src/org/chromium/chrome/browser/share/LensUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/share/LensUtils.java
@@ -10,7 +10,6 @@ import android.os.Build;
 import android.text.TextUtils;
 
 import org.chromium.base.ResettersForTesting;
-import org.chromium.chrome.R;
 import org.chromium.chrome.browser.IntentHandler;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.gsa.GSAState;
@@ -27,10 +26,6 @@ public class LensUtils {
     private static final String DISABLE_ON_INCOGNITO_PARAM_NAME = "disableOnIncognito";
     private static final String ORDER_SHARE_IMAGE_BEFORE_LENS_PARAM_NAME =
             "orderShareImageBeforeLens";
-    private static final String USE_LENS_CONTEXT_MENU_ALTERNATE_TEXT_1_PARAM_NAME =
-            "useLensContextMenuAlternateText1";
-    private static final String USE_LENS_CONTEXT_MENU_ALTERNATE_TEXT_2_PARAM_NAME =
-            "useLensContextMenuAlternateText2";
 
     private static final String MIN_AGSA_VERSION_NAME_FOR_LENS_POSTCAPTURE = "10.65";
 
@@ -172,24 +167,5 @@ public class LensUtils {
                     featureName, LOG_UKM_PARAM_NAME, true);
         }
         return false;
-    }
-
-    /**
-     * Check if experiment to get context menu alternate name text.
-     */
-    public static int getLensContextMenuText() {
-        if (ChromeFeatureList.getFieldTrialParamByFeatureAsBoolean(
-                    ChromeFeatureList.CONTEXT_MENU_GOOGLE_LENS_SEARCH_OPTIMIZATIONS,
-                    USE_LENS_CONTEXT_MENU_ALTERNATE_TEXT_1_PARAM_NAME, false)) {
-            return R.string.contextmenu_search_image_with_google_lens_alt_text_1;
-        }
-
-        if (ChromeFeatureList.getFieldTrialParamByFeatureAsBoolean(
-                    ChromeFeatureList.CONTEXT_MENU_GOOGLE_LENS_SEARCH_OPTIMIZATIONS,
-                    USE_LENS_CONTEXT_MENU_ALTERNATE_TEXT_2_PARAM_NAME, false)) {
-            return R.string.contextmenu_search_image_with_google_lens_alt_text_2;
-        }
-
-        return R.string.contextmenu_search_image_with_google_lens;
     }
 }
