3662c879038daa2a276ddf8bb598e20f2f8e2701
Brandon Wylie <wylieb@chromium.org>
2023-10-17 21:00:26 +0000

    Support intentional nullness for LazyOneshotSupplier
    Bug: 1481191
    Change-Id: I048533b365add4376c756882741f0b41073691c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4937532
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211086}

--- chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiMediatorUnitTest.java
+++ chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiMediatorUnitTest.java
@@ -47,6 +47,7 @@ import org.robolectric.annotation.LooperMode;
 
 import org.chromium.base.Callback;
 import org.chromium.base.supplier.LazyOneshotSupplier;
+import org.chromium.base.supplier.LazyOneshotSupplierImpl;
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.supplier.OneshotSupplierImpl;
@@ -187,7 +188,7 @@ public class TabGroupUiMediatorUnitTest {
 
         // Fake a similar behavior to the supplier in TabGroupUiCoordinator.
         mDialogControllerSupplier =
-                new LazyOneshotSupplier<>() {
+                new LazyOneshotSupplierImpl<>() {
                     @Override
                     public void doSet() {
                         set(mTabGridDialogController);
