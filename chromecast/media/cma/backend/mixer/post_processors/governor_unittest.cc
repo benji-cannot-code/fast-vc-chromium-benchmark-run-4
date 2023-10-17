847dc269cfc595a52da62791f2c5fae22ebd88de
Peter Kasting <pkasting@chromium.org>
2023-10-17 05:14:21 +0000

    Convert various const strings to constexprs: chromecast/
    This makes it possible to supply them to absl::StrFormat() as format
    strings, and may enable better codegen.
    
    Bug: 1371963
    Change-Id: Ia34266e822b44ce11887fc478c54495527fa830f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4943408
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210656}

--- chromecast/media/cma/backend/mixer/post_processors/governor_unittest.cc
+++ chromecast/media/cma/backend/mixer/post_processors/governor_unittest.cc
@@ -23,7 +23,7 @@ namespace post_processor_test {
 
 namespace {
 
-const char* kConfigTemplate =
+constexpr char kConfigTemplate[] =
     R"config({"onset_volume": %f, "clamp_multiplier": %f})config";
 
 const float kDefaultClamp = 0.6f;
