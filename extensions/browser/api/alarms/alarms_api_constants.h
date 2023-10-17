5eef60149c90d43310e72b13d9be6ee54b547362
Devlin Cronin <rdevlin.cronin@chromium.org>
2023-10-17 23:34:04 +0000

    [Extensions] Clean up Alarms API usage of TimeDelta
    The alarms API has limits on the minimum granularity for alarms.
    Currently, these use a weird (and conceptually incorrect, though the
    end result is the proper behavior) mishmash of TimeDelta and double
    multiplication to compare the limits.
    
    Fix this and just use base::TimeDelta, reducing LOC and improving
    readability.
    
    This CL has no behavior change.
    
    Bug: None
    Change-Id: I6d28b9278ff501aafc7f8839d3b6f26b55f3ba1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4949473
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211188}

--- extensions/browser/api/alarms/alarms_api_constants.h
+++ extensions/browser/api/alarms/alarms_api_constants.h
@@ -6,14 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef EXTENSIONS_BROWSER_API_ALARMS_ALARMS_API_CONSTANTS_H_
 #define EXTENSIONS_BROWSER_API_ALARMS_ALARMS_API_CONSTANTS_H_
 
+#include "base/time/time.h"
+
 namespace extensions {
 namespace alarms_api_constants {
 
 // Minimum specifiable alarm period (in minutes) for unpacked extensions.
-extern const double kDevDelayMinimum;
-
+inline constexpr base::TimeDelta kDevDelayMinimum = base::Seconds(1);
 // Minimum specifiable alarm period (in minutes) for packed/crx extensions.
-extern const double kReleaseDelayMinimum;
+inline constexpr base::TimeDelta kReleaseDelayMinimum = base::Minutes(1);
 
 extern const char kWarningMinimumDevDelay[];
 extern const char kWarningMinimumReleaseDelay[];
