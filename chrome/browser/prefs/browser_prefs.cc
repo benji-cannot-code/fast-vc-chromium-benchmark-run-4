a09eb34db58826559c8d858aa8fd0db77a368cd9
Tim Sergeant <tsergeant@chromium.org>
2023-10-17 17:38:21 +0000

    Prefs: Replace constexpr char with const char for deprecated pref names
    Bug: None
    Change-Id: I679ba3a3feb6ae58bf4bf6f03b856bbf2969e2b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4931353
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210948}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -802,20 +802,18 @@ const char kEventRemappedToRightClick[] =
 
 // Deprecated 05/2023.
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-constexpr char kSupervisedUserSyncId[] = "ManagedUserSyncId";
-constexpr char kSupervisedUserManagers[] = "ManagedUserManagers";
-constexpr char kSupervisedUserManagerNames[] = "ManagedUserManagerNames";
-constexpr char kSupervisedUserManagerDisplayEmails[] =
+const char kSupervisedUserSyncId[] = "ManagedUserSyncId";
+const char kSupervisedUserManagers[] = "ManagedUserManagers";
+const char kSupervisedUserManagerNames[] = "ManagedUserManagerNames";
+const char kSupervisedUserManagerDisplayEmails[] =
     "ManagedUserManagerDisplayEmails";
-constexpr char kSupervisedUsersFirstRun[] = "LocallyManagedUsersFirstRun";
-constexpr char kSupervisedUserPasswordSchema[] = "SupervisedUserPasswordSchema";
-constexpr char kSupervisedUserPasswordSalt[] = "SupervisedUserPasswordSalt";
-constexpr char kSupervisedUserPasswordRevision[] =
-    "SupervisedUserPasswordRevision";
-constexpr char kSupervisedUserNeedPasswordUpdate[] =
+const char kSupervisedUsersFirstRun[] = "LocallyManagedUsersFirstRun";
+const char kSupervisedUserPasswordSchema[] = "SupervisedUserPasswordSchema";
+const char kSupervisedUserPasswordSalt[] = "SupervisedUserPasswordSalt";
+const char kSupervisedUserPasswordRevision[] = "SupervisedUserPasswordRevision";
+const char kSupervisedUserNeedPasswordUpdate[] =
     "SupervisedUserNeedPasswordUpdate";
-constexpr char kSupervisedUserIncompleteKey[] =
-    "SupervisedUserHasIncompleteKey";
+const char kSupervisedUserIncompleteKey[] = "SupervisedUserHasIncompleteKey";
 #endif
 
 // Deprecated 06/2023.
@@ -829,7 +827,7 @@ const char kWebAppsExtensionIDs[] = "web_apps.extension_ids";
 
 // Deprecated 06/2023.
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-constexpr char kOsSyncPrefsMigrated[] = "sync.os_sync_prefs_migrated";
+const char kOsSyncPrefsMigrated[] = "sync.os_sync_prefs_migrated";
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 // Deprecated 06/2023
@@ -942,7 +940,7 @@ const char kShouldAttemptReenable[] = "android_sms.should_attempt_reenable";
 
 // Deprecated 10/2023.
 #if BUILDFLAG(IS_CHROMEOS)
-constexpr char kSupportedLinksAppPrefsKey[] = "supported_links_infobar.apps";
+const char kSupportedLinksAppPrefsKey[] = "supported_links_infobar.apps";
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
 // Register local state used only for migration (clearing or moving to a new
