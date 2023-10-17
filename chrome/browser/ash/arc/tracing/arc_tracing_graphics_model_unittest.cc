afc397e4720479147809d5c3639b1140548e0cf7
Matthew DeVore <matvore@chromium.org>
2023-10-17 23:22:20 +0000

    arc/tracing: perceived FPS metric
    Add two perceived event bands and remove the ChromeOS FPS band, as
    proposed in http://doc/1NIpVxCLYO4n3wfUHt5GsNwqRpvLkooW0JE2poVrJRg4.
    
    Change the FPS histogram from ChromeOS to perceived, and the summary
    stat box from ChromeOS FPS to Perceived FPS.
    
    Sample results in web UI: https://screenshot.googleplex.com/6yazrFjJaAq7nDR.png
    
    Test: ArcGraphicsTracingHandlerTest
    Test: trace 32 FPS app on brya DUT
    Test: when tracing one app with intermittent animation, and allowing a video to play in a Chrome browser, verify ChromeOS swaps are regular but perceived swaps are intermittent.
    Bug: b/296595454
    Change-Id: I8d65c69fe67e9b3c91442983fec980d43a2811d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935109
    Commit-Queue: Matthew DeVore <matvore@chromium.org>
    Reviewed-by: Alan Ding <alanding@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211183}

--- chrome/browser/ash/arc/tracing/arc_tracing_graphics_model_unittest.cc
+++ chrome/browser/ash/arc/tracing/arc_tracing_graphics_model_unittest.cc
@@ -5,30 +5,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/arc/tracing/arc_tracing_graphics_model.h"
 
+#include <initializer_list>
+
 #include "chrome/browser/ash/arc/tracing/arc_tracing_model.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace arc {
 
 TEST(ArcTracingGraphicsModelTest, IncludesCommitTimestamps) {
+  const std::initializer_list<int> kCommitsAbsoluteMs = {4200, 8400, 8484};
+  const std::string_view kCommitsRelativeUs = "0 4200000 4284000 ";
+
   ArcTracingGraphicsModel model;
   ArcTracingModel common;
-  TraceTimestamps commits;
-  commits.Add(base::TimeTicks::FromUptimeMillis(4200));
-  commits.Add(base::TimeTicks::FromUptimeMillis(8400));
-  commits.Add(base::TimeTicks::FromUptimeMillis(8484));
-  model.Build(common, std::move(commits));
+  TraceTimestamps timestamps;
+
+  for (int commit_ts : kCommitsAbsoluteMs) {
+    timestamps.AddCommit(base::TimeTicks::FromUptimeMillis(commit_ts));
+  }
+  model.Build(common, std::move(timestamps));
 
   ASSERT_EQ(model.view_buffers().size(), 1ul);
   auto& buffer_events = model.view_buffers().begin()->second.buffer_events();
   ASSERT_EQ(buffer_events.size(), 1ul);
 
-  std::ostringstream timestamps, types;
+  std::ostringstream timestamp_str, types;
   for (const auto& event : buffer_events[0]) {
-    timestamps << event.timestamp << " ";
+    timestamp_str << event.timestamp << " ";
     types << event.type << " ";
   }
-  EXPECT_EQ(timestamps.view(), "0 4200000 4284000 ");
+  EXPECT_EQ(timestamp_str.view(), kCommitsRelativeUs);
   EXPECT_EQ(types.view(), "206 206 206 ");
 }
 
--- chrome/browser/ui/webui/ash/arc_graphics_tracing/arc_graphics_tracing_handler.cc
+++ chrome/browser/ui/webui/ash/arc_graphics_tracing/arc_graphics_tracing_handler.cc
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event_constants.h"
 #include "ui/gfx/codec/png_codec.h"
 #include "ui/gfx/image/image_skia_rep.h"
+#include "ui/gfx/presentation_feedback.h"
 
 namespace ash {
 
@@ -69,7 +70,7 @@ struct ArcGraphicsTracingHandler::ActiveTrace {
   // with absl::optional.
   absl::optional<base::OneShotTimer> stop_timer;
 
-  arc::TraceTimestamps commits;
+  arc::TraceTimestamps stamps;
 };
 
 namespace {
@@ -180,7 +181,7 @@ std::pair<base::Value, std::string> BuildGraphicsModel(
                                      &common_model.system_model());
 
   trace->model.set_skip_structure_validation();
-  if (!trace->model.Build(common_model, trace->commits)) {
+  if (!trace->model.Build(common_model, trace->stamps)) {
     return std::make_pair(base::Value(), "Failed to build tracing model");
   }
 
@@ -372,10 +373,14 @@ void ArcGraphicsTracingHandler::OnSurfaceDestroying(exo::Surface* surface) {
 
 void ArcGraphicsTracingHandler::OnCommit(exo::Surface* surface) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  // TODO(b/296595454): Listen for subsequent presentation of the frame by exo.
-  if (active_trace_) {
-    active_trace_->commits.Add(SystemTicksNow());
+  if (!active_trace_) {
+    return;
   }
+
+  active_trace_->stamps.AddCommit(SystemTicksNow());
+  surface->RequestPresentationCallback(
+      base::BindRepeating(&ArcGraphicsTracingHandler::RecordPresentedFrame,
+                          weak_ptr_factory_.GetWeakPtr()));
 }
 
 void ArcGraphicsTracingHandler::UpdateActiveArcWindowInfo() {
@@ -541,6 +546,17 @@ void ArcGraphicsTracingHandler::OnTracingStopped(
                      std::move(trace), model_path),
       base::BindOnce(&ArcGraphicsTracingHandler::OnGraphicsModelReady,
                      weak_ptr_factory_.GetWeakPtr()));
+}
+
+void ArcGraphicsTracingHandler::RecordPresentedFrame(
+    const gfx::PresentationFeedback& frame) {
+  if (frame.failed()) {
+    VLOG(5) << "Presentation failed";
+  } else if (frame.timestamp == base::TimeTicks()) {
+    VLOG(5) << "Discarded frame";
+  } else if (active_trace_) {
+    active_trace_->stamps.AddPresent(frame.timestamp);
+  }
 }
 
 void ArcGraphicsTracingHandler::OnGraphicsModelReady(
