d31adf1559d92732543e144d15c15eeb754f71bf
Chad Duffin <chadduffin@chromium.org>
2023-10-17 15:44:03 +0000

    cellular: Improve confirmation code error message
    This change updates the confirmation code error message to use "install"
    instead of "connect".
    
    Bug: None
    Test: Manually verified on DUT
    Change-Id: I4331be08a50ac7d592fb78f645a47e27ee84bdda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4939771
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210880}

--- chrome/browser/ui/webui/ash/cellular_setup/cellular_setup_localized_strings_provider.cc
+++ chrome/browser/ui/webui/ash/cellular_setup/cellular_setup_localized_strings_provider.cc
@@ -89,6 +89,8 @@ constexpr webui::LocalizedString kLocalizedStringsWithoutPlaceholders[] = {
      IDS_CELLULAR_SETUP_ESIM_PAGE_CONFIRMATION_CODE_MESSAGE},
     {"confirmationCodeInput",
      IDS_CELLULAR_SETUP_ESIM_PAGE_CONFIRMATION_CODE_INPUT},
+    {"confirmationCodeErrorLegacy",
+     IDS_CELLULAR_SETUP_ESIM_PAGE_CONFIRMATION_CODE_ERROR_LEGACY},
     {"confirmationCodeError",
      IDS_CELLULAR_SETUP_ESIM_PAGE_CONFIRMATION_CODE_ERROR},
     {"confirmationCodeLoading",
