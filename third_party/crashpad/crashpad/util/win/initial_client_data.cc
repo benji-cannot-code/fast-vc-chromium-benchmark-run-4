f55d8bcb9de34eed77d17efd713154f4a98d1920
Peter Kasting <pkasting@chromium.org>
2023-10-17 02:55:25 +0000

    Update Crashpad to ce4e3d6ee0bd0407d755584fdcd33135096ec352
    7c89d500cd77 [ios] Remove ios_use_shared_bundle_id_for_test_apps gn
                 variable
    aef75040fd02 Use format macros for int64_t instead of hardcoding the
                 format
    ce4e3d6ee0bd Roll crashpad/third_party/mini_chromium/mini_chromium/
                 276f2ac53..42f1fddfe (1 commit)
    
    Bug: 1371963
    Change-Id: I9214f65815a7befa10e17f06d42fb40abe31d07b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4944382
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1210612}

--- third_party/crashpad/crashpad/util/win/initial_client_data.cc
+++ third_party/crashpad/crashpad/util/win/initial_client_data.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/format_macros.h"
 #include "base/logging.h"
 #include "base/strings/stringprintf.h"
 #include "util/stdlib/string_number_conversion.h"
@@ -100,7 +101,8 @@ bool InitialClientData::InitializeFromString(const std::string& str) {
 }
 
 std::string InitialClientData::StringRepresentation() const {
-  return base::StringPrintf("0x%x,0x%x,0x%x,0x%x,0x%x,0x%I64x,0x%I64x,0x%I64x",
+  return base::StringPrintf("0x%x,0x%x,0x%x,0x%x,0x%x,0x%" PRIx64 ",0x%" PRIx64
+                            ",0x%" PRIx64,
                             HandleToInt(request_crash_dump_),
                             HandleToInt(request_non_crash_dump_),
                             HandleToInt(non_crash_dump_completed_),
