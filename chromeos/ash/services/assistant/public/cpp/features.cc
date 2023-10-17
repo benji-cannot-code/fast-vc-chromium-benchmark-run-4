b32183513e2c2040427e9cf69b202a8d836e2fe0
Tao Wu <wutao@chromium.org>
2023-10-17 00:53:12 +0000

    assistant: Update strings on related info page
    Bug: b/284214810
    Test: manually check the string updated.
    Change-Id: I1d1b28c8c8da161dab10ae58c9f37c839ebcad4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4944413
    Reviewed-by: Simon Ziegltrum <ziegltrum@google.com>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210561}

--- chromeos/ash/services/assistant/public/cpp/features.cc
+++ chromeos/ash/services/assistant/public/cpp/features.cc
@@ -65,6 +65,10 @@ BASE_FEATURE(kEnableAssistantOnboarding,
              "AssistantOnboarding",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+BASE_FEATURE(kEnableAssistantRelatedInfoStringUpdate,
+             "AssistantRelatedInfoStringUpdate",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 bool IsAppSupportEnabled() {
   return base::FeatureList::IsEnabled(
       assistant::features::kAssistantAppSupport);
@@ -123,6 +127,10 @@ bool IsAssistantLearnMoreEnabled() {
 
 bool IsOnboardingEnabled() {
   return base::FeatureList::IsEnabled(kEnableAssistantOnboarding);
+}
+
+bool IsRelatedInfoStringUpdateEnabled() {
+  return base::FeatureList::IsEnabled(kEnableAssistantRelatedInfoStringUpdate);
 }
 
 }  // namespace ash::assistant::features
