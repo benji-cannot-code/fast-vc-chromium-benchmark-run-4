f3593e5ca61981d09eb106a9a2bee8c58de387b8
Michelle <michellegc@google.com>
2023-10-17 23:00:29 +0000

    Use custom MdTextButton for editor menu chips.
    Make EditorMenuChipView an MdTextButton since that gets most of the
    styling that we want, which simplifies things a bit. Then, customise the
    colour, radius, padding, etc. according to specs.
    
    Bug: b:300857651
    Change-Id: Iceb5e2a7340f6f88830a2f215e28e18d6fbd7bb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4950370
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211170}

--- chrome/browser/ui/views/editor_menu/editor_menu_chip_view.h
+++ chrome/browser/ui/views/editor_menu/editor_menu_chip_view.h
@@ -8,15 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "ui/base/metadata/metadata_header_macros.h"
-#include "ui/views/controls/button/button.h"
-#include "ui/views/controls/button/label_button.h"
+#include "ui/views/controls/button/md_text_button.h"
 
 namespace chromeos::editor_menu {
 
 struct PresetTextQuery;
 
 // A label button with an icon and a rounded rectangle border.
-class EditorMenuChipView : public views::LabelButton {
+class EditorMenuChipView : public views::MdTextButton {
  public:
   METADATA_HEADER(EditorMenuChipView);
 
@@ -25,17 +24,6 @@ class EditorMenuChipView : public views::LabelButton {
   EditorMenuChipView(const EditorMenuChipView&) = delete;
   EditorMenuChipView& operator=(const EditorMenuChipView&) = delete;
   ~EditorMenuChipView() override;
-
-  // views::LabelButton:
-  void AddedToWidget() override;
-  void OnThemeChanged() override;
-
- private:
-  void InitLayout();
-
-  void UpdateBackgroundColor() override;
-
-  const raw_ptr<const gfx::VectorIcon> icon_;
 };
 
 }  // namespace chromeos::editor_menu
