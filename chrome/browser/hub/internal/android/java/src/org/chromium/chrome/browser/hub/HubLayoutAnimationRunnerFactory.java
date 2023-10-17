0d73029e6ce945a7e0e7b1070d5b3a71af547541
Calder Kitagawa <ckitagawa@chromium.org>
2023-10-17 21:16:07 +0000

    [Hub] HubLayoutAnimationRunner
    Create a helper that is responsible for running AnimatorSets provided
    by HubLayoutAnimatorProvider.
    
    Bug: 1492207
    Change-Id: I5fa51417f577d3642bad702e30f57230ee284b60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935434
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1211104}

---
+++ chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubLayoutAnimationRunnerFactory.java
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.hub;
+
+/** Factory for creating {@link HubLayoutAnimationRunner}s. */
+public class HubLayoutAnimationRunnerFactory {
+    /**
+     * Creates a new instance of {@link HubLayoutAnimatorRunner} to run the provided animation.
+     *
+     * @param animatorProvider The {@link HubLayoutAnimatorProvider} that will provide the
+     *     animation.
+     * @return an instance of {@link HubLayoutAnimatorRunnerImpl}.
+     */
+    public static HubLayoutAnimationRunner createHubLayoutAnimationRunner(
+            HubLayoutAnimatorProvider animatorProvider) {
+        return new HubLayoutAnimationRunnerImpl(animatorProvider);
+    }
+}
