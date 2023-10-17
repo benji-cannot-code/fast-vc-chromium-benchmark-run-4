5d22fa97bf3c32fe995c9bb2a01843a3f37b6f44
David Pennington <dpenning@chromium.org>
2023-10-17 17:28:00 +0000

    fix invalid Organization from CHECKing
    Change-Id: I54a0ece91cbb7d81ac04a82d83cf990610831caf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4939141
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1210943}

--- chrome/browser/ui/tabs/organization/tab_organization_session.cc
+++ chrome/browser/ui/tabs/organization/tab_organization_session.cc
@@ -79,7 +79,10 @@ void TabOrganizationSession::StartRequest() {
 void TabOrganizationSession::PopulateAndCreate(
     const TabOrganizationResponse* response) {
   PopulateOrganizations(response);
-  GetNextTabOrganization()->Accept();
+  TabOrganization* organization = GetNextTabOrganization();
+  if (organization->IsValidForOrganizing()) {
+    organization->Accept();
+  }
 }
 
 void TabOrganizationSession::PopulateOrganizations(
