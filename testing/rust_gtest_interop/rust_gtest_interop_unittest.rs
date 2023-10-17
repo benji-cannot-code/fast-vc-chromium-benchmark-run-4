c7b87d14617ec38c75da1b21e92250b96466f8a3
Daniel Cheng <dcheng@chromium.org>
2023-10-17 20:09:38 +0000

    Fix make_canonical_file_path() test's handling of directory separators.
    The test previously hardcoded posix-style separators, which failed on
    Windows.
    
    Bug: 1466674
    Change-Id: Ic9fda61aa8bbc13e90b1eb66adbaa391f7b83194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4945756
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211045}

--- testing/rust_gtest_interop/rust_gtest_interop_unittest.rs
+++ testing/rust_gtest_interop/rust_gtest_interop_unittest.rs
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 use rust_gtest_interop::prelude::*;
+use std::path::PathBuf;
 
 #[gtest(Test, InTopModule)]
 fn test() {
@@ -97,25 +98,64 @@ fn test() -> std::result::Result<(), Box<dyn std::error::Error>> {
 
 #[gtest(Test, Paths)]
 fn test() {
-    expect_eq!(rust_gtest_interop::__private::make_canonical_file_path("foo/bar.rs"), "foo/bar.rs");
+    let expected_result =
+        ["foo", "bar.rs"].iter().collect::<PathBuf>().to_string_lossy().to_string();
+
+    expect_eq!(
+        rust_gtest_interop::__private::make_canonical_file_path("foo/bar.rs"),
+        expected_result
+    );
     expect_eq!(
         rust_gtest_interop::__private::make_canonical_file_path("../foo/bar.rs"),
-        "foo/bar.rs"
+        expected_result
     );
     expect_eq!(
         rust_gtest_interop::__private::make_canonical_file_path("../../foo/bar.rs"),
-        "foo/bar.rs"
+        expected_result
     );
     expect_eq!(
         rust_gtest_interop::__private::make_canonical_file_path("a/../foo/bar.rs"),
-        "foo/bar.rs"
+        expected_result
     );
     expect_eq!(
         rust_gtest_interop::__private::make_canonical_file_path("a/../../../foo/bar.rs"),
-        "foo/bar.rs"
+        expected_result
     );
     expect_eq!(
         rust_gtest_interop::__private::make_canonical_file_path("a/../b/../../foo/bar.rs"),
-        "foo/bar.rs"
+        expected_result
     );
+
+    #[cfg(windows)]
+    {
+        expect_eq!(
+            rust_gtest_interop::__private::make_canonical_file_path(r"foo\bar.rs"),
+            r"foo\bar.rs"
+        );
+
+        expect_eq!(
+            rust_gtest_interop::__private::make_canonical_file_path(r"..\foo\bar.rs"),
+            r"foo\bar.rs"
+        );
+
+        expect_eq!(
+            rust_gtest_interop::__private::make_canonical_file_path(r"..\..\foo\bar.rs"),
+            r"foo\bar.rs"
+        );
+
+        expect_eq!(
+            rust_gtest_interop::__private::make_canonical_file_path(r"a\..\foo\bar.rs"),
+            r"foo\bar.rs"
+        );
+
+        expect_eq!(
+            rust_gtest_interop::__private::make_canonical_file_path(r"a\..\..\..\foo\bar.rs"),
+            r"foo\bar.rs"
+        );
+
+        expect_eq!(
+            rust_gtest_interop::__private::make_canonical_file_path(r"a\..\b\..\..\foo\bar.rs"),
+            expected_result
+        );
+    }
 }
