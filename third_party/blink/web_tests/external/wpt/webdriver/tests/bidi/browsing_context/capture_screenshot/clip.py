28b881af9e8955f25b15b275342e444f637cc3e2
Jonathan Lee <jonathanjlee@google.com>
2023-10-17 18:39:35 +0000

    Import wpt@3d084c0ac239eff15e62958a18b5508203dc2aaa
    Using wpt-import in Chromium 1629f4484d7abe58886760128c6f56f9279a89a8.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    andruud@chromium.org:
      external/wpt/css/css-properties-values-api
    dtapuska@chromium.org:
      external/wpt/uievents
    lanwei@chromium.org:
      external/wpt/infrastructure/testdriver/actions
    nzolghadr@chromium.org, mustaq@chromium.org:
      external/wpt/pointerevents
    pcupp@microsoft.com:
      external/wpt/editing
    rego@igalia.com:
      external/wpt/css/css-contain
      external/wpt/css/css-grid
      external/wpt/css/selectors
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I5d3085e9f6b09587f959ad9b68d62bb227d23bca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4946810
    Reviewed-by: Jie Sheng <jiesheng@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1210991}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/browsing_context/capture_screenshot/clip.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/browsing_context/capture_screenshot/clip.py
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import pytest
 from tests.support.image import png_dimensions
-from webdriver.bidi.modules.browsing_context import ElementOptions, ViewportOptions
+from webdriver.bidi.modules.browsing_context import ElementOptions, BoxOptions
 from webdriver.bidi.modules.script import ContextTarget
 
 from . import (
@@ -130,7 +130,7 @@ async def test_clip_viewport(bidi_session, top_context, inline, compare_png_bidi
     )
     reference_data = await bidi_session.browsing_context.capture_screenshot(
         context=top_context["context"],
-        clip=ViewportOptions(
+        clip=BoxOptions(
             x=element_coordinates[0],
             y=element_coordinates[1],
             width=expected_size[0],
@@ -157,7 +157,7 @@ async def test_clip_viewport(bidi_session, top_context, inline, compare_png_bidi
     )
     data = await bidi_session.browsing_context.capture_screenshot(
         context=top_context["context"],
-        clip=ViewportOptions(
+        clip=BoxOptions(
             x=element_coordinates[0],
             y=element_coordinates[1],
             width=element_dimensions[0],
@@ -185,7 +185,7 @@ async def test_clip_viewport(bidi_session, top_context, inline, compare_png_bidi
     )
     new_data = await bidi_session.browsing_context.capture_screenshot(
         context=top_context["context"],
-        clip=ViewportOptions(
+        clip=BoxOptions(
             x=element_coordinates[0],
             y=element_coordinates[1],
             width=element_dimensions[0],
@@ -241,7 +241,7 @@ async def test_clip_viewport_scroll_to(
     )
     new_data = await bidi_session.browsing_context.capture_screenshot(
         context=top_context["context"],
-        clip=ViewportOptions(
+        clip=BoxOptions(
             x=element_coordinates[0],
             y=element_coordinates[1],
             width=element_dimensions[0],
@@ -290,7 +290,7 @@ async def test_clip_viewport_partially_visible(
     )
     new_data = await bidi_session.browsing_context.capture_screenshot(
         context=top_context["context"],
-        clip=ViewportOptions(
+        clip=BoxOptions(
             x=element_coordinates[0],
             y=element_coordinates[1],
             width=expected_size[0],
--- third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/browsing_context/capture_screenshot/invalid.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/browsing_context/capture_screenshot/invalid.py
@@ -4,7 +4,7 @@ import pytest
 from ... import get_viewport_dimensions
 
 import webdriver.bidi.error as error
-from webdriver.bidi.modules.browsing_context import ElementOptions, ViewportOptions
+from webdriver.bidi.modules.browsing_context import ElementOptions, BoxOptions
 from webdriver.bidi.modules.script import ContextTarget
 
 pytestmark = pytest.mark.asyncio
@@ -99,7 +99,7 @@ async def test_params_clip_viewport_x_invalid_type(bidi_session, top_context, va
     with pytest.raises(error.InvalidArgumentException):
         await bidi_session.browsing_context.capture_screenshot(
             context=top_context["context"],
-            clip=ViewportOptions(x=value, y=0, width=0, height=0),
+            clip=BoxOptions(x=value, y=0, width=0, height=0),
         )
 
 
@@ -108,7 +108,7 @@ async def test_params_clip_viewport_y_invalid_type(bidi_session, top_context, va
     with pytest.raises(error.InvalidArgumentException):
         await bidi_session.browsing_context.capture_screenshot(
             context=top_context["context"],
-            clip=ViewportOptions(x=0, y=value, width=0, height=0),
+            clip=BoxOptions(x=0, y=value, width=0, height=0),
         )
 
 
@@ -119,7 +119,7 @@ async def test_params_clip_viewport_width_invalid_type(
     with pytest.raises(error.InvalidArgumentException):
         await bidi_session.browsing_context.capture_screenshot(
             context=top_context["context"],
-            clip=ViewportOptions(x=0, y=0, width=value, height=0),
+            clip=BoxOptions(x=0, y=0, width=value, height=0),
         )
 
 
@@ -130,7 +130,7 @@ async def test_params_clip_viewport_height_invalid_type(
     with pytest.raises(error.InvalidArgumentException):
         await bidi_session.browsing_context.capture_screenshot(
             context=top_context["context"],
-            clip=ViewportOptions(x=0, y=0, width=0, height=value),
+            clip=BoxOptions(x=0, y=0, width=0, height=value),
         )
 
 
@@ -138,7 +138,7 @@ async def test_params_clip_viewport_dimensions_invalid_value(bidi_session, top_c
     with pytest.raises(error.UnableToCaptureScreenException):
         await bidi_session.browsing_context.capture_screenshot(
             context=top_context["context"],
-            clip=ViewportOptions(x=0, y=0, width=0, height=0),
+            clip=BoxOptions(x=0, y=0, width=0, height=0),
         )
 
 
@@ -150,7 +150,7 @@ async def test_params_clip_viewport_outside_of_window_viewport(
     with pytest.raises(error.UnableToCaptureScreenException):
         await bidi_session.browsing_context.capture_screenshot(
             context=top_context["context"],
-            clip=ViewportOptions(
+            clip=BoxOptions(
                 x=viewport_dimensions["width"],
                 y=viewport_dimensions["height"],
                 width=1,
