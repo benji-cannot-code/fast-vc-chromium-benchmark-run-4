17981f46244c44df52c84104c097074362939c98
Scott Yoder <scottyoder@google.com>
2023-10-17 15:10:35 +0000

    [ios] move "Chrome" branded Set Up List strings to branded strings file
    Bug: 1427439
    Change-Id: I295126732ff5f1240d04ee245ea8718cde63bd73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4943810
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1210851}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_egtest.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_egtest.mm
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/ntp/new_tab_page_feature.h"
 #import "ios/chrome/common/ui/confirmation_alert/constants.h"
 #import "ios/chrome/common/ui/promo_style/constants.h"
+#import "ios/chrome/grit/ios_branded_strings.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/earl_grey/chrome_actions.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
--- ios/chrome/browser/ui/content_suggestions/magic_stack_half_sheet_table_view_controller.mm
+++ ios/chrome/browser/ui/content_suggestions/magic_stack_half_sheet_table_view_controller.mm
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/content_suggestions/safety_check/safety_check_prefs.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
+#import "ios/chrome/grit/ios_branded_strings.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util_mac.h"
 
