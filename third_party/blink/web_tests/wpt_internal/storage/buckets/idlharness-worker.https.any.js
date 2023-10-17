d418fde482edda52e470de210152711b8bbc6ca4
Ayu Ishii <ayui@chromium.org>
2023-10-17 18:02:39 +0000

    StorageBuckets: Updates idlharness test to use spec idl
    This change updates the storage buckets idlharness test
    to now test against the automatically generated idl[1] that
    is scraped from the draft spec by webref[2]. Also removes the
    "tentative" idl created and used while we didn't have a spec yet.
    
    [1]https://crsrc.org/c/third_party/blink/web_tests/external/wpt/interfaces/storage-buckets.idl
    [2]https://github.com/w3c/webref
    
    Bug: None
    Change-Id: I83d8e91f1b06db1e3a18befa6b987fb866146244
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4944371
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210964}

--- third_party/blink/web_tests/wpt_internal/storage/buckets/idlharness-worker.https.any.js
+++ third_party/blink/web_tests/wpt_internal/storage/buckets/idlharness-worker.https.any.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict';
 
 idl_test(
-  ['storage-buckets.tentative'],
+  ['storage-buckets'],
   ['html'],
   async (idl_array, t) => {
     idl_array.add_objects({
