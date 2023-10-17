cd0a7820b57dc71709195559959a053d5399bdc2
Yongshun Liu <yongshun@chromium.org>
2023-10-17 21:31:43 +0000

    bento_button: Fix hotseat widget bounds
    The first shelf icon might become not clickable for overflown shelf
    after entering and exiting overview. This is due to the fact that the
    hotseat widget bounds is not updated in `HotseatWidget::UpdateLayout()`,
    and that's because when hotseat updates its layout, it causes the desk
    button widget to re-layout, then whole shelf may need to re-layout. In
    such case, the cached value of `layout_inputs_` is not up-to-date. The
    fix is to update the cached value before all the layout invocations.
    
    Screen recordings before/after fix can be found at b/305040382#comment2.
    
    Bug: b/305040382
    Test: Manual
    Change-Id: Id6bebe46088961c14ccafdd5b6529cd421f240cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935164
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211115}

--- ash/shelf/hotseat_widget.cc
+++ ash/shelf/hotseat_widget.cc
@@ -1048,6 +1048,10 @@ void HotseatWidget::UpdateLayout(bool animate) {
   if (layout_inputs_ == new_layout_inputs)
     return;
 
+  // The cached `layout_inputs_` should always be up-to-date, thus it is updated
+  // here before all other potential shelf layout invocations.
+  layout_inputs_ = new_layout_inputs;
+
   // Never show this widget outside of an active session.
   if (!new_layout_inputs.is_active_session_state)
     Hide();
@@ -1096,7 +1100,6 @@ void HotseatWidget::UpdateLayout(bool animate) {
     SetBounds(target_bounds);
   }
 
-  layout_inputs_ = new_layout_inputs;
   delegate_view_->UpdateTranslucentBackground();
 
   // Setting visibility during an animation causes the visibility property to
