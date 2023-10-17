fe678f6ed990407dcdaf8dd380860dda3817b48b
Angela Xiao <angelaxiao@chromium.org>
2023-10-17 21:12:51 +0000

    Scalable Notifiers: add help app browsertest for pinned to shelf
    Add a browsertest to confirm that the help app icon is correctly
    pinned to the bottom shelf when the Welcome Tips page experiment is
    enabled.
    
    Bug: b/301487468
    Change-Id: Ib55334a425562fced65f9a1d9e5b37f0e9fa9e3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4944482
    Reviewed-by: Yuki Awano <yawano@google.com>
    Commit-Queue: Angela Xiao <angelaxiao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211103}

--- chrome/browser/ash/scalable_iph/scalable_iph_browsertest.cc
+++ chrome/browser/ash/scalable_iph/scalable_iph_browsertest.cc
@@ -30,11 +30,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/scalable_iph/customizable_test_env_browser_test_base.h"
 #include "chrome/browser/ash/scalable_iph/scalable_iph_browser_test_base.h"
 #include "chrome/browser/ash/scalable_iph/scalable_iph_delegate_impl.h"
+#include "chrome/browser/ash/system_web_apps/system_web_app_manager.h"
 #include "chrome/browser/scalable_iph/scalable_iph_factory.h"
 #include "chrome/browser/ui/ash/system_web_apps/system_web_app_ui_utils.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/web_applications/web_app_id_constants.h"
 #include "chrome/common/chrome_switches.h"
+#include "chrome/common/pref_names.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "chromeos/ash/components/phonehub/fake_feature_status_provider.h"
 #include "chromeos/ash/components/phonehub/feature_status.h"
@@ -215,6 +217,32 @@ class ScalableIphBrowserTestPreinstallApps : public ScalableIphBrowserTest {
     command_line->RemoveSwitch(switches::kDisableDefaultApps);
     command_line->AppendSwitch(
         ash::switches::kAllowDefaultShelfPinLayoutIgnoringSync);
+  }
+};
+
+class ScalableIphBrowserTestHelpApp
+    : public ScalableIphBrowserTestPreinstallApps {
+ public:
+  void InitializeScopedFeatureList() override {
+    base::FieldTrialParams params;
+    AppendVersionNumber(params);
+    base::test::FeatureRefAndParams test_config(TestIphFeature(), params);
+
+    base::test::FeatureRefAndParams scalable_iph_feature(
+        ash::features::kScalableIph, {});
+
+    base::test::FeatureRefAndParams help_app_feature(
+        ash::features::kHelpAppWelcomeTips, {});
+
+    scoped_feature_list_.InitWithFeaturesAndParameters(
+        {scalable_iph_feature, help_app_feature, test_config}, {});
+  }
+
+  void SetUpOnMainThread() override {
+    ScalableIphBrowserTestPreinstallApps::SetUpOnMainThread();
+
+    ash::SystemWebAppManager::GetForTest(browser()->profile())
+        ->InstallSystemAppsForTesting();
   }
 };
 
@@ -937,6 +965,16 @@ IN_PROC_BROWSER_TEST_F(ScalableIphBrowserTestPreinstallApps,
               NotifyEvent(scalable_iph::kEventNameShelfItemActivationYouTube));
   ash::Shelf::ActivateShelfItem(ash::ShelfModel::Get()->ItemIndexByAppID(
       scalable_iph::kWebAppYouTubeAppId));
+}
+
+IN_PROC_BROWSER_TEST_F(ScalableIphBrowserTestHelpApp, HelpAppPinnedToShelf) {
+  if (!IsGoogleChrome()) {
+    GTEST_SKIP()
+        << "Google Chrome is required for preinstall apps used by this test";
+    return;
+  }
+
+  EXPECT_TRUE(ash::ShelfModel::Get()->IsAppPinned(web_app::kHelpAppId));
 }
 
 IN_PROC_BROWSER_TEST_F(ScalableIphBrowserTestOobe, SessionState) {
