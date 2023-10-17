01bd41738eda5762c4b5e7db685e932e7a9370d9
Katie Dektar <katie@chromium.org>
2023-10-17 19:02:40 +0000

    [Gardener] Disable tests flaking on chromeos builders
    Also replaces "dummy" with "simple", a more inclusive term.
    
    Bug: 1492076
    Change-Id: Ib33a1c43a2ee6a81ab295f2ba3e1572f36076f7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4948991
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Auto-Submit: Katie Dektar <katie@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Owners-Override: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211002}

--- chrome/browser/ash/policy/reporting/metrics_reporting/apps/app_usage_telemetry_sampler_browsertest.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/apps/app_usage_telemetry_sampler_browsertest.cc
@@ -276,11 +276,13 @@ class AppUsageTelemetrySamplerBrowserTest
 
 IN_PROC_BROWSER_TEST_F(AppUsageTelemetrySamplerBrowserTest,
                        PRE_ReportUsageData) {
-  // Dummy case that sets up the affiliated user through SetUpOnMainThread
+  // Simple case that sets up the affiliated user through SetUpOnMainThread
   // PRE-condition.
 }
 
-IN_PROC_BROWSER_TEST_F(AppUsageTelemetrySamplerBrowserTest, ReportUsageData) {
+// TODO(crbug.com/1492076): Flaky on Chrome OS.
+IN_PROC_BROWSER_TEST_F(AppUsageTelemetrySamplerBrowserTest,
+                       DISABLED_ReportUsageData) {
   // Install webapp and simulate its usage.
   const auto& app_id = InstallStandaloneWebApp(GURL(kWebAppUrl));
   static constexpr base::TimeDelta kAppUsageDuration = base::Minutes(2);
@@ -323,12 +325,13 @@ IN_PROC_BROWSER_TEST_F(AppUsageTelemetrySamplerBrowserTest, ReportUsageData) {
 
 IN_PROC_BROWSER_TEST_F(AppUsageTelemetrySamplerBrowserTest,
                        PRE_ReportUsageDataWhenSyncDisabled) {
-  // Dummy case that sets up the affiliated user through SetUpOnMainThread
+  // Simple case that sets up the affiliated user through SetUpOnMainThread
   // PRE-condition.
 }
 
+// TODO(crbug.com/1492076): Flaky on Chrome OS.
 IN_PROC_BROWSER_TEST_F(AppUsageTelemetrySamplerBrowserTest,
-                       ReportUsageDataWhenSyncDisabled) {
+                       DISABLED_ReportUsageDataWhenSyncDisabled) {
   sync_service()->SetDisableReasons(
       {::syncer::SyncService::DISABLE_REASON_ENTERPRISE_POLICY});
 
@@ -367,12 +370,13 @@ IN_PROC_BROWSER_TEST_F(AppUsageTelemetrySamplerBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(AppUsageTelemetrySamplerBrowserTest,
                        PRE_ReportUsageDataWhenPolicyDisabled) {
-  // Dummy case that sets up the affiliated user through SetUpOnMainThread
+  // Simple case that sets up the affiliated user through SetUpOnMainThread
   // PRE-condition.
 }
 
+// TODO(crbug.com/1492076): Flaky on Chrome OS.
 IN_PROC_BROWSER_TEST_F(AppUsageTelemetrySamplerBrowserTest,
-                       ReportUsageDataWhenPolicyDisabled) {
+                       DISABLED_ReportUsageDataWhenPolicyDisabled) {
   // Disable policy.
   SetAllowedAppReportingTypes({});
 
@@ -393,7 +397,7 @@ IN_PROC_BROWSER_TEST_F(AppUsageTelemetrySamplerBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(AppUsageTelemetrySamplerBrowserTest,
                        PRE_ReportUsageDataOnSessionTermination) {
-  // Dummy case that sets up the affiliated user through SetUpOnMainThread
+  // Simple case that sets up the affiliated user through SetUpOnMainThread
   // PRE-condition.
 }
 
