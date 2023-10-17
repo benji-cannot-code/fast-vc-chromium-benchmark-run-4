8fefa855e6046de9b25a27965096ffcfac92e6aa
Luke Warlow <luke@warlow.dev>
2023-10-17 22:33:46 +0000

    Implement scrollbar-color animation
    scrollbar-color property is now correctly interpolable.
    
    Bug: 1490796
    Change-Id: I345961efd08a60b65fe7d8037fedf7dfd46e5c07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4752855
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Commit-Queue: Luke <lukewarlow156@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1211154}

--- third_party/blink/renderer/core/animation/css_interpolation_types_map.cc
+++ third_party/blink/renderer/core/animation/css_interpolation_types_map.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/animation/css_resolution_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_rotate_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_scale_interpolation_type.h"
+#include "third_party/blink/renderer/core/animation/css_scrollbar_color_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_shadow_list_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_size_list_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_text_indent_interpolation_type.h"
@@ -448,6 +449,11 @@ const InterpolationTypes& CSSInterpolationTypesMap::Get(
         DCHECK(RuntimeEnabledFeatures::CSSTopLayerForTransitionsEnabled());
         applicable_types->push_back(
             std::make_unique<CSSOverlayInterpolationType>(used_property));
+        break;
+      case CSSPropertyID::kScrollbarColor:
+        applicable_types->push_back(
+            std::make_unique<CSSScrollbarColorInterpolationType>(
+                used_property));
         break;
       default:
         DCHECK(!css_property.IsInterpolable());
