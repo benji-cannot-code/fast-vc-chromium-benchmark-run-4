f818a4820f43eac41d5542e2c0652b8edf70dfd2
John Admanski <jadmanski@google.com>
2023-10-17 16:57:24 +0000

    Use stronger token encryption for oauth2 token
    This switches the oauth2 device token away from using
    WeakEncryptWithSystemSalt, to the not-Weak version. It does this by
    creating a new "v2" pref for storing the new formatted token.
    
    In the store code it will always use the strong encryption and the v2
    pref but to preserve the ability to read the old tokens the load code
    will try to use both prefs (first v2, then falling back to the original
    if that doesn't exist).
    
    Bug: b:298194227
    Change-Id: I474fceea4ecc365ec05acbc2beb07e1685bd05fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4942663
    Commit-Queue: John Admanski <jadmanski@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210920}

--- chrome/browser/device_identity/chromeos/device_oauth2_token_store_chromeos.h
+++ chrome/browser/device_identity/chromeos/device_oauth2_token_store_chromeos.h
@@ -7,9 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_DEVICE_IDENTITY_CHROMEOS_DEVICE_OAUTH2_TOKEN_STORE_CHROMEOS_H_
 
 #include "base/memory/raw_ptr.h"
-#include "chrome/browser/device_identity/device_oauth2_token_store.h"
-
 #include "chrome/browser/ash/settings/cros_settings.h"
+#include "chrome/browser/device_identity/device_oauth2_token_store.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 class PrefRegistrySimple;
 
@@ -54,6 +54,12 @@ class DeviceOAuth2TokenStoreChromeOS : public DeviceOAuth2TokenStore {
   // Encrypts and saves the refresh token. Should only be called when the system
   // salt is available.
   void EncryptAndSaveToken();
+
+  // Attempt to load a refresh token from the local state. This will return null
+  // if an error occurs while loading the token, otherwise the token will be
+  // returned. Note that not having a token at all is not considered an error:
+  // an empty token is returned in this
+  absl::optional<std::string> LoadAndDecryptToken();
 
   // Handles completion of the system salt input. Will invoke |callback| since
   // this function is what happens at the end of the initialization process.
--- chrome/browser/device_identity/chromeos/device_oauth2_token_store_chromeos_unittest.cc
+++ chrome/browser/device_identity/chromeos/device_oauth2_token_store_chromeos_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/policy/core/device_policy_builder.h"
 #include "chrome/browser/ash/settings/cros_settings.h"
 #include "chrome/browser/ash/settings/device_settings_service.h"
+#include "chrome/browser/ash/settings/token_encryptor.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/scoped_testing_local_state.h"
 #include "chrome/test/base/testing_browser_process.h"
@@ -66,8 +67,14 @@ class DeviceOAuth2TokenStoreChromeOSTest : public testing::Test {
     ash::CryptohomeMiscClient::Shutdown();
   }
 
+  std::string GetStubSaltAsString() {
+    std::vector<uint8_t> bytes =
+        ash::FakeCryptohomeMiscClient::GetStubSystemSalt();
+    return std::string(bytes.begin(), bytes.end());
+  }
+
   void SetUpDefaultValues() {
-    SetDeviceRefreshTokenInLocalState("device_refresh_token_4_test");
+    StoreV2TokenInLocalState("device_refresh_token_4_test");
     SetRobotAccountId("service_acct@g.com");
   }
 
@@ -104,10 +111,19 @@ class DeviceOAuth2TokenStoreChromeOSTest : public testing::Test {
     content::RunAllTasksUntilIdle();
   }
 
-  void SetDeviceRefreshTokenInLocalState(const std::string& refresh_token) {
+  void StoreV1TokenInLocalState(const std::string& token) {
+    ash::CryptohomeTokenEncryptor encryptor(GetStubSaltAsString());
     scoped_testing_local_state_.Get()->SetUserPref(
-        prefs::kDeviceRobotAnyApiRefreshToken,
-        std::make_unique<base::Value>(refresh_token));
+        prefs::kDeviceRobotAnyApiRefreshTokenV1,
+        std::make_unique<base::Value>(
+            encryptor.WeakEncryptWithSystemSalt(token)));
+  }
+
+  void StoreV2TokenInLocalState(const std::string& token) {
+    ash::CryptohomeTokenEncryptor encryptor(GetStubSaltAsString());
+    scoped_testing_local_state_.Get()->SetUserPref(
+        prefs::kDeviceRobotAnyApiRefreshTokenV2,
+        std::make_unique<base::Value>(encryptor.EncryptWithSystemSalt(token)));
   }
 
   content::BrowserTaskEnvironment task_environment_;
@@ -144,6 +160,37 @@ TEST_F(DeviceOAuth2TokenStoreChromeOSTest, InitSuccessful) {
   EXPECT_TRUE(init_waiter.HasInitBeenCalled());
   EXPECT_TRUE(init_waiter.GetInitResult());
   EXPECT_TRUE(init_waiter.GetValidationRequired());
+}
+
+TEST_F(DeviceOAuth2TokenStoreChromeOSTest, LoadV1Token) {
+  chromeos::DeviceOAuth2TokenStoreChromeOS store(
+      scoped_testing_local_state_.Get());
+
+  StoreV1TokenInLocalState("test-token");
+  InitStore(&store);
+
+  EXPECT_EQ("test-token", store.GetRefreshToken());
+}
+
+TEST_F(DeviceOAuth2TokenStoreChromeOSTest, LoadV2Token) {
+  chromeos::DeviceOAuth2TokenStoreChromeOS store(
+      scoped_testing_local_state_.Get());
+
+  StoreV2TokenInLocalState("test-token");
+  InitStore(&store);
+
+  EXPECT_EQ("test-token", store.GetRefreshToken());
+}
+
+TEST_F(DeviceOAuth2TokenStoreChromeOSTest, LoadPrefersV2Token) {
+  chromeos::DeviceOAuth2TokenStoreChromeOS store(
+      scoped_testing_local_state_.Get());
+
+  StoreV1TokenInLocalState("test-token-v1");
+  StoreV2TokenInLocalState("test-token-v2");
+  InitStore(&store);
+
+  EXPECT_EQ("test-token-v2", store.GetRefreshToken());
 }
 
 TEST_F(DeviceOAuth2TokenStoreChromeOSTest, SaveToken) {
