9ba9947867f54c787b4d4b50785f49772a440902
Yoav Weiss <yoavweiss@chromium.org>
2023-10-17 07:31:21 +0000

    [task attribution] Reduce TaskAttributionInfo observer memory overhead
    [1] increased the memory consumed by any TaskAttributionInfo instance,
    for something that's only needed on a few of them. This CL removed that
    extra WeakMember on each TaskAttributionInfo in favor of a HashMap that
    stores the relevant observer, for SoftNavigation tasks.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/4927648
    
    Bug: 1492771
    Change-Id: I8a612229ed53c613883a7df8b930acce298e70a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4942198
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210688}

--- third_party/blink/renderer/modules/scheduler/task_attribution_tracker_impl.h
+++ third_party/blink/renderer/modules/scheduler/task_attribution_tracker_impl.h
@@ -91,6 +91,9 @@ class MODULES_EXPORT TaskAttributionTrackerImpl
   virtual ScriptWrappableTaskState* GetCurrentTaskContinuationData(
       ScriptState*) const;
 
+  Observer* GetObserverForTaskDisposal(TaskAttributionId) override;
+  void SetObserverForTaskDisposal(TaskAttributionId, Observer*) override;
+
  private:
   struct TaskAttributionIdPair {
     TaskAttributionIdPair() = default;
@@ -152,6 +155,9 @@ class MODULES_EXPORT TaskAttributionTrackerImpl
   Persistent<TaskAttributionInfo> running_task_ = nullptr;
 
   WTF::HashSet<WeakPersistent<TaskAttributionTracker::Observer>> observers_;
+  WTF::HashMap<TaskAttributionIdType,
+               WeakPersistent<TaskAttributionTracker::Observer>>
+      task_id_observers_;
 
   // A queue of TaskAttributionInfo objects representing tasks that initiated a
   // same-document navigation that was sent to the browser side. They are kept
---
+++ third_party/blink/renderer/platform/scheduler/common/task_attribution_info.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/platform/scheduler/public/task_attribution_info.h"
+#include "third_party/blink/renderer/platform/scheduler/public/task_attribution_tracker.h"
+#include "third_party/blink/renderer/platform/scheduler/public/thread_scheduler.h"
+
+namespace blink::scheduler {
+
+void TaskAttributionInfo::Dispose() {
+  auto* tracker = ThreadScheduler::Current()->GetTaskAttributionTracker();
+  if (!tracker) {
+    return;
+  }
+  if (auto* observer = tracker->GetObserverForTaskDisposal(task_id_)) {
+    observer->OnTaskDisposal(*this);
+  }
+}
+
+}  // namespace blink::scheduler
