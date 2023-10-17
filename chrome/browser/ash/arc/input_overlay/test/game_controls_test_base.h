1b7f73eac5d5d276cb4892230d29d92c180d575d
Cici Ruan <cuicuiruan@google.com>
2023-10-17 15:39:10 +0000

    Revert "arc/gio: Enable Game Controls for different ARC versions"
    This reverts commit c0fb20437e7af41d4df10b318d9a926621f33c96.
    
    Reason for revert: <Revert the change on app.mojom>
    
    Original change's description:
    > arc/gio: Enable Game Controls for different ARC versions
    >
    > Game Controls is available on non-O4C games that are not Game Controls
    > opt-out.
    >
    > Since Optimized for ChromeOS(O4C) feature is not available on ARC-P and
    > it needs to distinguish Game Controls opt-out or O4C app, this CL
    > checks whether the game is Game Controls opt-out, then checks whether
    > the game is O4C game.
    >
    > Android side: ag/24907285
    >
    > Bug: b/275380306
    > Test: manual test
    > Test: unit_tests --gtest_filter=*ArcInputOverlay*
    > Change-Id: Ibf3a89877c893535e9f39b431f7fb48a66f1bbc3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4908849
    > Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > Reviewed-by: David Jacobo <djacobo@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1209204}
    
    Bug: b/275380306
    Change-Id: Ifd6b93ec55c812a2b6fbeb3ef43c2f96044ae0e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4944099
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210878}

--- chrome/browser/ash/arc/input_overlay/test/game_controls_test_base.h
+++ chrome/browser/ash/arc/input_overlay/test/game_controls_test_base.h
@@ -11,10 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test/ash_test_base.h"
 #include "base/memory/raw_ptr.h"
 #include "base/test/scoped_feature_list.h"
-#include "chrome/browser/ash/app_list/arc/arc_app_test.h"
 #include "chrome/browser/ash/arc/input_overlay/constants.h"
-
-class TestingProfile;
 
 namespace arc::input_overlay {
 
@@ -43,10 +40,6 @@ class GameControlsTestBase : public ash::AshTestBase {
   std::unique_ptr<views::Widget> widget_;
   raw_ptr<TouchInjector, DanglingUntriaged> touch_injector_;
   raw_ptr<DisplayOverlayController, DanglingUntriaged> controller_;
-
- private:
-  ArcAppTest arc_app_test_;
-  std::unique_ptr<TestingProfile> profile_;
 };
 
 }  // namespace arc::input_overlay
