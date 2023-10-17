2eb6c2471433ccec3d4b57ba1a7c575c3ddccaad
Igor Kraskevich <kraskevich@google.com>
2023-10-17 15:09:28 +0000

    Check that Renderer tids don't belong to Browser from the Browser process
    This change is relevant only to Chrome on Android because other
    platforms don't have ADPF support.
    
    At the moment the GPU process checks that Renderer tids don't belong to
    itself and the Browser process before including them into the ADPF hint
    session. This check works incorrectly (= always fails) on Beta and
    Stable because the GPU process doesn't have access to the list of
    threads of the Browser process. As a result, Renderer threads are
    missing from the ADPF hint session. It works correctly on Canary and Dev
    because the Chrome app is marked as "profileable".
    
    With this change, the GPU process checks that Renderer tids don't belong
    to itself, and then asks the Browser process to check that these tids
    don't belong to Browser via a mojo message. The GPU and the Browser
    processes have access to the list of the own threads (using
    /proc/self/task) on all channels, so this change fixes Chrome ADPF usage
    on Beta and Stable.
    
    While this is effectively a bug fix, it's hidden behind a disabled by
    default `EnableADPFAsyncThreadsVerification` feature to make sure that
    we can roll it out safely and properly evaluate the impact on field
    metrics for Beta and Stable.
    
    b/302096134#comment41 has a link to the design doc with the discussion
    of other options considered and the security implications.
    
    Local testing with a custom build branded as "Chrome Beta" shows the
    same behavior as the current Chrome app (i.e. Renderer threads missing
    from the ADPF session) when the feature is disabled, and fixed behavior
    (i.e. Renderer threads are in the ADPF session) when the feature is
    enabled.
    
    Bug: b/302096134
    Change-Id: Iaf6fa6cf9ba7394e19f985b5f4681845594b8337
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4921772
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Liza Burakova <liza@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Igor Kraskevich <kraskevich@google.com>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210849}

--- components/viz/common/performance_hint_utils.h
+++ components/viz/common/performance_hint_utils.h
@@ -3,14 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_VIZ_SERVICE_PERFORMANCE_HINT_UTILS_H_
-#define COMPONENTS_VIZ_SERVICE_PERFORMANCE_HINT_UTILS_H_
+#ifndef COMPONENTS_VIZ_COMMON_PERFORMANCE_HINT_UTILS_H_
+#define COMPONENTS_VIZ_COMMON_PERFORMANCE_HINT_UTILS_H_
 
 #include <vector>
 
 #include "base/containers/flat_set.h"
 #include "base/process/process_handle.h"
 #include "base/threading/platform_thread.h"
+#include "components/viz/common/viz_common_export.h"
 
 namespace viz {
 
@@ -19,11 +20,20 @@ namespace viz {
 // used for performance hint belongs to a chromium process. So this check
 // ensures that thread IDs reported by sandboxed processes are indeed in
 // sandboxed processes.
-bool CheckThreadIdsDoNotBelongToProcessIds(
+VIZ_COMMON_EXPORT bool CheckThreadIdsDoNotBelongToProcessIds(
     const std::vector<base::ProcessId>& privileged_process_ids,
+    const base::flat_set<base::PlatformThreadId>&
+        thread_ids_from_sandboxed_process);
+
+// Check that thread IDs in `thread_ids_from_sandboxed_process` are not in
+// current process. This function should be called from the Browser and the GPU
+// processes. Android OS will check that thread IDs used for performance hint
+// belongs to a chromium process. So this check ensures that thread IDs
+// reported by sandboxed processes are indeed in sandboxed processes.
+VIZ_COMMON_EXPORT bool CheckThreadIdsDoNotBelongToCurrentProcess(
     const base::flat_set<base::PlatformThreadId>&
         thread_ids_from_sandboxed_process);
 
 }  // namespace viz
 
-#endif  // COMPONENTS_VIZ_SERVICE_PERFORMANCE_HINT_UTILS_H_
+#endif  // COMPONENTS_VIZ_COMMON_PERFORMANCE_HINT_UTILS_H_
--- components/viz/host/host_frame_sink_manager.cc
+++ components/viz/host/host_frame_sink_manager.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/time.h"
+#include "components/viz/common/performance_hint_utils.h"
 #include "components/viz/common/surfaces/surface_info.h"
 #include "components/viz/host/renderer_settings_creation.h"
 #include "mojo/public/cpp/bindings/sync_call_restrictions.h"
@@ -406,6 +407,16 @@ void HostFrameSinkManager::OnAggregatedHitTestRegionListUpdated(
   for (HitTestRegionObserver& observer : observers_)
     observer.OnAggregatedHitTestRegionListUpdated(frame_sink_id, hit_test_data);
 }
+
+#if BUILDFLAG(IS_ANDROID)
+void HostFrameSinkManager::VerifyThreadIdsDoNotBelongToHost(
+    const std::vector<int32_t>& thread_ids,
+    VerifyThreadIdsDoNotBelongToHostCallback callback) {
+  base::flat_set<base::PlatformThreadId> tids(thread_ids.begin(),
+                                              thread_ids.end());
+  std::move(callback).Run(CheckThreadIdsDoNotBelongToCurrentProcess(tids));
+}
+#endif
 
 uint32_t HostFrameSinkManager::CacheBackBufferForRootSink(
     const FrameSinkId& root_sink_id) {
