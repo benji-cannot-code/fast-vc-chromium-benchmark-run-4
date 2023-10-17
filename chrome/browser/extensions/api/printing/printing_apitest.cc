806f88021d198c4e88b13b5a459997033aca6ac6
Andrew Rayskiy <greengrape@google.com>
2023-10-17 08:28:02 +0000

    [Printing] Reset PrintBackendServiceManager in PrintingApiTest
    PrintBackendServiceTestImpl::LaunchForTesting() has a side effect of
    setting test remotes in PrintBackendServiceManager that keep lingering
    on even after the backend service is gone.
    
    Bug: b/301590015
    Change-Id: Ib73acfab92dad5bdced702a4895f7de4d51c0cb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4941939
    Reviewed-by: Sonja Laurila <laurila@google.com>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Auto-Submit: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1210708}

--- chrome/browser/extensions/api/printing/printing_apitest.cc
+++ chrome/browser/extensions/api/printing/printing_apitest.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(ENABLE_OOP_PRINTING)
 #include "base/feature_list.h"
+#include "chrome/browser/printing/print_backend_service_manager.h"
 #include "chrome/browser/printing/print_backend_service_test_impl.h"
 #include "chrome/services/printing/public/mojom/print_backend_service.mojom.h"
 #include "mojo/public/cpp/bindings/remote.h"
@@ -111,28 +112,42 @@ class PrintingApiTest : public ExtensionApiTest,
   PrintingApiTest& operator=(const PrintingApiTest&) = delete;
 
  protected:
-  void SetUpOnMainThread() override {
+  void SetUp() override {
+    ExtensionApiTest::SetUp();
+    printing::PrintBackend::SetPrintBackendForTesting(
+        test_print_backend_.get());
+  }
+
+  void TearDown() override {
+    printing::PrintBackend::SetPrintBackendForTesting(nullptr);
+    ExtensionApiTest::TearDown();
+  }
+
 #if BUILDFLAG(ENABLE_OOP_PRINTING)
+  void SetUpOnMainThread() override {
+    ExtensionApiTest::SetUpOnMainThread();
     if (base::FeatureList::IsEnabled(
             printing::features::kEnableOopPrintDrivers)) {
       print_backend_service_ =
           printing::PrintBackendServiceTestImpl::LaunchForTesting(
               test_remote_, test_print_backend_.get(), /*sandboxed=*/true);
     }
-#endif
-    ExtensionApiTest::SetUpOnMainThread();
   }
 
+  void TearDownOnMainThread() override {
+    print_backend_service_.reset();
+    printing::PrintBackendServiceManager::GetInstance().ResetForTesting();
+    ExtensionApiTest::TearDownOnMainThread();
+  }
+#endif
+
   void SetUpInProcessBrowserTestFixture() override {
+    ExtensionApiTest::SetUpInProcessBrowserTestFixture();
     create_services_subscription_ =
         BrowserContextDependencyManager::GetInstance()
             ->RegisterCreateServicesCallbackForTesting(base::BindRepeating(
                 &PrintingApiTest::OnWillCreateBrowserContextServices,
                 base::Unretained(this)));
-    test_print_backend_ = base::MakeRefCounted<printing::TestPrintBackend>();
-    printing::PrintBackend::SetPrintBackendForTesting(
-        test_print_backend_.get());
-    ExtensionApiTest::SetUpInProcessBrowserTestFixture();
   }
 
   ash::TestCupsPrintJobManager* GetPrintJobManager() {
@@ -202,7 +217,8 @@ class PrintingApiTest : public ExtensionApiTest,
 
   base::CallbackListSubscription create_services_subscription_;
 
-  scoped_refptr<printing::TestPrintBackend> test_print_backend_;
+  scoped_refptr<printing::TestPrintBackend> test_print_backend_ =
+      base::MakeRefCounted<printing::TestPrintBackend>();
 };
 
 using PrintingPromiseApiTest = PrintingApiTest;
