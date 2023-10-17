9575714157d75608e05e0fa95785918a26ee0d1c
Hongchan Choi <hongchan@chromium.org>
2023-10-17 22:39:45 +0000

    Tests for TypeErrors in AudioWorkletGlobalScope.registerProcessor()
    This CL add a test to verify TypeErrors that are thrown from
    reigsterProcessor() method. It checks two cases:
     - When a given parameter is not a Function
     - When a given parameter is not a constructor
    
    Bug: 1078902
    Change-Id: Iba312bb558622ba68bcf850310c5cd87fa1d8655
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4939970
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Sina Firoozabadi <sinafirooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211158}

---
+++ third_party/blink/web_tests/external/wpt/webaudio/the-audio-api/the-audioworklet-interface/processors/register-processor-typeerrors.js
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// For cross-thread messaging.
+class MessengerProcessor extends AudioWorkletProcessor {
+  constructor() {
+    super();
+    this.port.onmessage = this.startTest.bind(this);
+  }
+
+  process() {}
+
+  startTest(message) {
+    runRegisterProcessorTest(this.port);
+  }
+}
+
+function runRegisterProcessorTest(messagePort) {
+  try {
+    // TypeError when a given parameter is not a Function.
+    const DummyObject = {};
+    registerProcessor('type-error-on-object', DummyObject);
+  } catch (exception) {
+    messagePort.postMessage({
+      name: exception.name,
+      message: exception.message
+    });
+  }
+
+  try {
+    // TypeError When a given parameter is a Function, but not a constructor.
+    const DummyFunction = () => {};
+    registerProcessor('type-error-on-function', DummyFunction);
+  } catch (exception) {
+    messagePort.postMessage({
+      name: exception.name,
+      message: exception.message
+    });
+  }
+}
+
+registerProcessor('messenger-processor', MessengerProcessor);
