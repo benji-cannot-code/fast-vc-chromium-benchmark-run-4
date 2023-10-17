7af06b9ec4339ec475e84665641310dce79db48d
Riley Tatum <rtatum@google.com>
2023-10-17 21:35:57 +0000

    Change submit button text when there are results
    Bug: b/300970975
    Change-Id: Ibaa0a90e29da300991667384f51dd0f5558a97a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4945512
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1211118}

--- chrome/browser/resources/side_panel/customize_chrome/wallpaper_search.ts
+++ chrome/browser/resources/side_panel/customize_chrome/wallpaper_search.ts
@@ -57,6 +57,11 @@ export class WallpaperSearchElement extends PolymerElement {
       },
       emptyContainers_: Object,
       results_: Object,
+      submitBtnText_: {
+        type: String,
+        computed: 'computeSubmitBtnText_(results_)',
+        value: 'Search',
+      },
     };
   }
 
@@ -66,6 +71,7 @@ export class WallpaperSearchElement extends PolymerElement {
   private selectedDescriptorA_: string|null;
   private selectedDescriptorB_: string|null;
   private selectedDescriptorC_: string|null;
+  private submitBtnText_: string;
 
   private pageHandler_: CustomizeChromePageHandlerInterface;
 
@@ -81,6 +87,11 @@ export class WallpaperSearchElement extends PolymerElement {
 
   focusOnBackButton() {
     this.$.heading.getBackButton().focus();
+  }
+
+  private computeSubmitBtnText_() {
+    return this.results_ && this.results_.length > 0 ? 'Search Again' :
+                                                       'Search';
   }
 
   private async onBackClick_() {
