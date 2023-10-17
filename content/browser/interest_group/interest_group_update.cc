245888c549433cc452e922428c91185bcac2cbf2
xtlsheep <xtlsheep@google.com>
2023-10-17 19:34:42 +0000

    Group Network Isolation Key by joining origin
    Right now during the interest group update process, all the interest
    groups of an owner are using the same network isolation key(NIK). To
    improve the privacy from NIK perspective, and also considering network
    performance, we are changing the strategy that different storage
    interest groups with same joining origin from all the owners can use
    the same NIK.
    
    Implementation steps:
    1. For each storage interest group during the update process, it will
    check if it can find isolation information with its joining origin. If
    not it will create a new isolation information and put it into the map
    with joining origin as the key.
    2. After all the owners are updated and the queue is empty, it will
    also clear the whole map.
    
    This feature is controlled by kGroupNIKByJoiningOriginPerOwner and is
    set to disabled by default.
    
    Bug: 1479754
    Change-Id: Id2762475f4f1ebf12f6c0d626c4651458684e120
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4794574
    Commit-Queue: Tianyang Xu <xtlsheep@google.com>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Cr-Commit-Position: refs/heads/main@{#1211023}

--- content/browser/interest_group/interest_group_update.cc
+++ content/browser/interest_group/interest_group_update.cc
@@ -5,11 +5,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/interest_group/interest_group_update.h"
 
+#include <utility>
+
+#include "third_party/blink/public/common/interest_group/interest_group.h"
+
 namespace content {
 
 InterestGroupUpdate::InterestGroupUpdate() = default;
 InterestGroupUpdate::InterestGroupUpdate(const InterestGroupUpdate&) = default;
 InterestGroupUpdate::InterestGroupUpdate(InterestGroupUpdate&&) = default;
 InterestGroupUpdate::~InterestGroupUpdate() = default;
+
+InterestGroupUpdateParameter::InterestGroupUpdateParameter() = default;
+InterestGroupUpdateParameter::InterestGroupUpdateParameter(
+    blink::InterestGroupKey k,
+    GURL u,
+    url::Origin o)
+    : interest_group_key(std::move(k)),
+      update_url(std::move(u)),
+      joining_origin(std::move(o)) {}
+InterestGroupUpdateParameter::~InterestGroupUpdateParameter() = default;
 
 }  // namespace content
--- content/browser/interest_group/storage_interest_group.h
+++ content/browser/interest_group/storage_interest_group.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/time/time.h"
-#include "content/services/auction_worklet/public/mojom/bidder_worklet.mojom-forward.h"
+#include "content/services/auction_worklet/public/mojom/bidder_worklet.mojom.h"
 #include "mojo/public/cpp/bindings/struct_ptr.h"
 #include "third_party/blink/public/common/interest_group/interest_group.h"
 #include "url/gurl.h"
