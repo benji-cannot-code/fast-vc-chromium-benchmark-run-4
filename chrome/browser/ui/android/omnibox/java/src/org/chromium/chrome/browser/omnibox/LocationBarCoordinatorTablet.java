75f721034745fcd69a539f17cca12b82e603aaab
Tomasz Wiszkowski <ender@google.com>
2023-10-17 21:06:15 +0000

    [Cleanup] Reformat omnibox/L* using google-java-format.
    Manual changes include bullet list formatting. Reformatted using:
    google-java-format --aosp -i {styles,suggestions}/**/*.java
    
    More info: go/chrome-google-java-format-lsc
    
    Bug: 1491626
    Change-Id: I72e2e08be6ad8694479f0120848da12e829acfdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4949762
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211093}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarCoordinatorTablet.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarCoordinatorTablet.java
@@ -7,9 +7,7 @@ package org.chromium.chrome.browser.omnibox;
 
 import androidx.annotation.ColorInt;
 
-/**
- * A supplement to {@link LocationBarCoordinator} with methods specific to larger devices.
- */
+/** A supplement to {@link LocationBarCoordinator} with methods specific to larger devices. */
 public class LocationBarCoordinatorTablet implements LocationBarCoordinator.SubCoordinator {
     private LocationBarTablet mLocationBarTablet;
 
@@ -24,6 +22,7 @@ public class LocationBarCoordinatorTablet implements LocationBarCoordinator.SubC
 
     /**
      * Sets the color of the background.
+     *
      * <p>TODO(1133482): Hide this View interaction if possible.
      */
     public void tintBackground(@ColorInt int color) {
