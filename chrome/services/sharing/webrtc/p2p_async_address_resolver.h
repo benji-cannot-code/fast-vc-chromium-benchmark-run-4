c81d371e7d256d32847bc1d2619321de9680461d
Harald Alvestrand <hta@chromium.org>
2023-10-17 03:32:53 +0000

    Remove remaining references to AsyncResolver
    and switch the last users (Nearby) to AsyncDnsResolver
    
    Low-Coverage-Reason: OTHER - No tests using domain names for Nearby
    Bug: webrtc:12598
    Change-Id: I8977d7e39cce0ee9b950afc75d9bfecb5571bfc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4936375
    Reviewed-by: Florent Castelli <orphis@chromium.org>
    Reviewed-by: Curt Clemens <cclem@google.com>
    Commit-Queue: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210626}

--- chrome/services/sharing/webrtc/p2p_async_address_resolver.h
+++ chrome/services/sharing/webrtc/p2p_async_address_resolver.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/ip_address.h"
 #include "services/network/public/mojom/p2p.mojom.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
-#include "third_party/webrtc/rtc_base/async_resolver_interface.h"
 
 namespace sharing {
 
--- third_party/blink/renderer/platform/p2p/host_address_request.h
+++ third_party/blink/renderer/platform/p2p/host_address_request.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/renderer/platform/heap/cross_thread_persistent.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
-#include "third_party/webrtc/rtc_base/async_resolver_interface.h"
+#include "third_party/webrtc/rtc_base/socket_address.h"
 
 namespace blink {
 
