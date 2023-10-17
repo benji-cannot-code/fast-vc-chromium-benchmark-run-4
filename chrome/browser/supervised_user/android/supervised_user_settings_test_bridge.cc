740a3aaf1715ff5ec1eaf78b441aedb4a0b2784f
Nohemi Fernandez <fernandex@chromium.org>
2023-10-17 08:19:36 +0000

    Rename test-only classes for supervised user settings.
    Bug: N/A
    Change-Id: If1eafe0fb91ea0498d2bbc6af8789965d82659e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4942251
    Reviewed-by: Anthi Orfanou <anthie@google.com>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210704}

--- chrome/browser/supervised_user/android/supervised_user_settings_test_bridge.cc
+++ chrome/browser/supervised_user/android/supervised_user_settings_test_bridge.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/supervised_user/supervised_user_service_factory.h"
 #include "chrome/browser/supervised_user/supervised_user_settings_service_factory.h"
 #include "chrome/browser/supervised_user/supervised_user_test_util.h"
-#include "chrome/browser/supervised_user/test_support_jni_headers/SupervisedUserSettingsBridge_jni.h"
+#include "chrome/browser/supervised_user/test_support_jni_headers/SupervisedUserSettingsTestBridge_jni.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/identity_test_environment.h"
 #include "components/signin/public/identity_manager/identity_test_utils.h"
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using base::android::JavaParamRef;
 
-void JNI_SupervisedUserSettingsBridge_SetFilteringBehavior(
+void JNI_SupervisedUserSettingsTestBridge_SetFilteringBehavior(
     JNIEnv* env,
     const JavaParamRef<jobject>& j_profile,
     jint setting) {
@@ -43,7 +43,7 @@ void JNI_SupervisedUserSettingsBridge_SetFilteringBehavior(
       base::Value(setting));
 }
 
-void JNI_SupervisedUserSettingsBridge_SetManualFilterForHost(
+void JNI_SupervisedUserSettingsTestBridge_SetManualFilterForHost(
     JNIEnv* env,
     const JavaParamRef<jobject>& j_profile,
     const JavaParamRef<jstring>& host,
@@ -81,7 +81,7 @@ class TestUrlLoaderFactoryHelper {
   scoped_refptr<network::SharedURLLoaderFactory> shared_url_loader_factory_;
 };
 
-void JNI_SupervisedUserSettingsBridge_SetSafeSearchResponseForTesting(  // IN-TEST
+void JNI_SupervisedUserSettingsTestBridge_SetSafeSearchResponseForTesting(  // IN-TEST
     JNIEnv* env,
     const JavaParamRef<jobject>& j_profile,
     jboolean is_allowed) {
@@ -116,12 +116,12 @@ void JNI_SupervisedUserSettingsBridge_SetSafeSearchResponseForTesting(  // IN-TE
       test_kids_chrome_management_client_.get());
 }
 
-void JNI_SupervisedUserSettingsBridge_SetUpTestUrlLoaderFactoryHelper(
+void JNI_SupervisedUserSettingsTestBridge_SetUpTestUrlLoaderFactoryHelper(
     JNIEnv* env) {
   TestUrlLoaderFactoryHelper::SharedInstance()->SetUp();
 }
 
-void JNI_SupervisedUserSettingsBridge_TearDownTestUrlLoaderFactoryHelper(
+void JNI_SupervisedUserSettingsTestBridge_TearDownTestUrlLoaderFactoryHelper(
     JNIEnv* env) {
   TestUrlLoaderFactoryHelper::SharedInstance()->TearDown();
 }
