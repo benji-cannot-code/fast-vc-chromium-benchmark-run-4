8f21fad4fac5ab613b72bc9d96b764ee72299cdb
Mohannad Farrag <aymanm@google.com>
2023-10-17 15:20:48 +0000

    Remove Deprecated UploadedDataProviders for AOSP Build
    Related to this [patch](https://cs.android.com/android/platform/superproject/main/+/main:external/cronet/patches/0007-Remove-the-deprecated-UploadDataProviders-class.patch)
    
    Change-Id: If287d45554ba23bc1b18cb8af2dc5f9d42f68702
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935381
    Auto-Submit: Mohannad Farrag <aymanm@google.com>
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1210855}

--- components/cronet/android/test/javatests/src/org/chromium/net/CronetUrlRequestTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/CronetUrlRequestTest.java
@@ -39,6 +39,7 @@ import org.chromium.net.CronetTestRule.RequiresMinApi;
 import org.chromium.net.NetworkChangeNotifierAutoDetect.ConnectivityManagerDelegate;
 import org.chromium.net.TestUrlRequestCallback.FailureType;
 import org.chromium.net.TestUrlRequestCallback.ResponseStep;
+import org.chromium.net.apihelpers.UploadDataProviders;
 import org.chromium.net.impl.CronetUrlRequest;
 import org.chromium.net.impl.NetworkExceptionImpl;
 import org.chromium.net.impl.UrlResponseInfoImpl;
@@ -169,7 +170,10 @@ public class CronetUrlRequestTest {
                 "Connection", "close", "Content-Length", "3", "Content-Type", "text/plain");
         mTestRule.assertResponseEquals(urlResponseInfo, callback.getResponseInfoWithChecks());
         checkResponseInfo(
-                callback.getResponseInfoWithChecks(), NativeTestServer.getEchoMethodURL(), 200, "OK");
+                callback.getResponseInfoWithChecks(),
+                NativeTestServer.getEchoMethodURL(),
+                200,
+                "OK");
     }
 
     UrlResponseInfo createUrlResponseInfo(
@@ -269,7 +273,8 @@ public class CronetUrlRequestTest {
         assertThat(callback.mResponseStep).isEqualTo(ResponseStep.ON_RESPONSE_STARTED);
         assertThat(callback.mRedirectResponseInfoList).hasSize(1);
         assertThat(callback.getResponseInfoWithChecks()).hasHttpStatusCodeThat().isEqualTo(200);
-        checkResponseInfo(callback.getResponseInfoWithChecks(), NativeTestServer.getSuccessURL(), 200, "OK");
+        checkResponseInfo(
+                callback.getResponseInfoWithChecks(), NativeTestServer.getSuccessURL(), 200, "OK");
         assertThat(callback.getResponseInfoWithChecks())
                 .hasUrlChainThat()
                 .containsExactly(
@@ -906,7 +911,10 @@ public class CronetUrlRequestTest {
         assertThat(callback.getResponseInfoWithChecks()).hasHttpStatusCodeThat().isEqualTo(200);
         assertThat(callback.mResponseAsString).isEqualTo("GET");
         checkResponseInfo(
-                callback.getResponseInfoWithChecks(), NativeTestServer.getEchoMethodURL(), 200, "OK");
+                callback.getResponseInfoWithChecks(),
+                NativeTestServer.getEchoMethodURL(),
+                200,
+                "OK");
 
         // Check that buffer contents were not modified.
         assertThat(bufferContentsToString(readBuffer, 0, 5)).isEqualTo("FORTE");
