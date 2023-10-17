5b3e46203892b97a394018c2170e783c93c8a340
Sam Maier <smaier@chromium.org>
2023-10-17 14:36:50 +0000

    jni_zero: Migrating all JNI annotations in /testing to jni_zero copies
    This CL is the result of a few simple sed commands, switching any import
    statement using any of the 6 JNI annotations, and switching over the gn
    targets from base:jni_java to jni_zero_java.
    
    This CL was uploaded by git cl split.
    
    R=torne@chromium.org
    
    Bug: 1456762
    Change-Id: I7e8ac52fea27650d50a586e6cd7c6390ee97c3c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4938704
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210832}

--- testing/android/native_test/java/src/org/chromium/native_test/NativeBrowserTest.java
+++ testing/android/native_test/java/src/org/chromium/native_test/NativeBrowserTest.java
@@ -5,10 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.native_test;
 
+import org.jni_zero.JNINamespace;
+import org.jni_zero.NativeMethods;
+
 import org.chromium.base.Log;
 import org.chromium.base.StrictModeContext;
-import org.chromium.base.annotations.JNINamespace;
-import org.chromium.base.annotations.NativeMethods;
 
 import java.io.File;
 
