0262d4ef709d60a4b47843e36b5fb266d00a520c
Regan Hsu <hsuregan@google.com>
2023-10-18 23:12:19 +0000

    [CrOS Cellular] ChromeVox to say "APN types Attach/Default"
    When the attach or default checkbox is navigated to via chromevox, the
    screenreader will say "APN types Attach" and "APN type Default" instead
    of just "Attach" and "Default" as it does currently. As was the case
    before, the screenreader will follow with "Check box", then "Checked"
    or "Not checked".
    
    Bug: b/301308609
    Test: manual
    Change-Id: I2b333b6f5be84fecc92190db908b63ca463c5054
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4944002
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211824}

--- chrome/test/data/webui/settings/chromeos/apn_detail_dialog_test.ts
+++ chrome/test/data/webui/settings/chromeos/apn_detail_dialog_test.ts
@@ -443,10 +443,18 @@ suite('<apn-detail-dialog>', () => {
           assertTrue(!!apnDefaultTypeCheckbox);
           apnDefaultTypeCheckbox.checked = defaultType;
 
+          assertEquals(
+              'apnDetailApnTypesLabel',
+              apnDefaultTypeCheckbox.getAttribute('aria-describedby'));
+
           const apnAttachTypeCheckbox =
               apnDetailDialog.shadowRoot!.querySelector<CrCheckboxElement>(
                   '#apnAttachTypeCheckbox');
           assertTrue(!!apnAttachTypeCheckbox);
+          assertEquals(
+              'apnDetailApnTypesLabel',
+              apnAttachTypeCheckbox.getAttribute('aria-describedby'));
+
           apnAttachTypeCheckbox.checked = attachType;
         };
 
