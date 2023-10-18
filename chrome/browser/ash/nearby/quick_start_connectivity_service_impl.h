ac19ca5914849bdaf17cfd1ecdeb3762f2198225
Brittany Hartmire <bhartmire@google.com>
2023-10-18 21:11:56 +0000

    [QuickStart]: Regression test for NearbyConnection destruction crash
    This cl is a follow-up to crrev.com/c/4936959. This cl addresses nit
    feedback inside NearbyConnectionsManagerImpl::OnDisconnected() and adds
    a regression test to ensure deleting the nearby_connections_manager
    in the nearby_connection disconnect listener callback doesn't result in
    a crash.
    
    share with atlas and pixel4a
    
    Tested: Added new unittests and manually tested quick start and nearby
    Change-Id: I55b06a7ed0f34fbf35cdaae4384a663cd001e067
    Bug: b/303675257
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4948068
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1211745}

--- chrome/browser/ash/nearby/quick_start_connectivity_service_impl.h
+++ chrome/browser/ash/nearby/quick_start_connectivity_service_impl.h
@@ -41,6 +41,8 @@ class QuickStartConnectivityServiceImpl : public QuickStartConnectivityService {
   void Cleanup() override;
 
  private:
+  friend class QuickStartConnectivityServiceImplTest;
+
   void OnNearbyProcessStopped(
       nearby::NearbyProcessManager::NearbyProcessShutdownReason
           shutdown_reason);
