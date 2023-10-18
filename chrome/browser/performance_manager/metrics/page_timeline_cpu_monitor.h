44ebc9f21c471657e1feae0688858a46fc6532d9
Joe Mason <joenotcharles@google.com>
2023-10-18 19:24:05 +0000

    Factor out repeated CPU measurement code
    This moves CPUMeasurementDelegate to its own header file, so that it can
    still be exposed to tests once CPUMeasurementMonitor becomes private,
    and moves SimulatedCPUMeasurementDelegate out of individual tests into a
    shared file in test_support. Code to keep track of simulated delegates
    is moved from the test fixtures into a new
    SimulatedCPUMeasurementDelegateFactory.
    
    Also simplifies some tests to use the RunInGraph() helper instead of
    defining their own RunLoop helpers.
    
    R=agale
    
    Bug: 1471683
    Change-Id: Iec39bc77182784e0e3eecb7dfe731c61bc9431a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4950270
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211676}

--- chrome/browser/performance_manager/metrics/page_timeline_cpu_monitor.h
+++ chrome/browser/performance_manager/metrics/page_timeline_cpu_monitor.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "base/time/time.h"
 #include "components/performance_manager/public/graph/process_node.h"
+#include "components/performance_manager/public/resource_attribution/cpu_measurement_delegate.h"
 #include "components/performance_manager/public/resource_attribution/cpu_measurement_monitor.h"
 #include "components/performance_manager/public/resource_attribution/query_results.h"
 #include "components/performance_manager/public/resource_attribution/resource_contexts.h"
@@ -31,8 +32,7 @@ class PageTimelineCPUMonitor : public ProcessNode::ObserverDefaultImpl {
   // CPUMeasurementDelegate object will be created for each ProcessNode to be
   // measured. Can be overridden for testing by passing a factory callback to
   // SetCPUMeasurementDelegateFactoryForTesting().
-  using CPUMeasurementDelegate =
-      resource_attribution::CPUMeasurementMonitor::CPUMeasurementDelegate;
+  using CPUMeasurementDelegate = resource_attribution::CPUMeasurementDelegate;
 
   // A map from FrameNode's or WorkerNode's to the estimated CPU usage of each.
   // The estimate is a fraction in the range 0% to 100% *
