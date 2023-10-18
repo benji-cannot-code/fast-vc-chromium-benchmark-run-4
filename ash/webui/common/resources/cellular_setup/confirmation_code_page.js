fdf7333d5664b65dfc889f97cb52049b38db1f19
Chad Duffin <chadduffin@chromium.org>
2023-10-18 21:04:44 +0000

    cellular: Remove "busy" variable from new pages
    This change removes the "busy" variable from the pages that were newly
    added to the "add eSIM" dialog since they are no longer used.
    
    Video: https://drive.google.com/file/d/1QQQ8C-dpfoRX__O3iEL72GL3VTTjGM_T/view?usp=sharing
    
    Bug: None
    Test: Manually verified on DUT.
    Change-Id: Ib3e7abf609fd40ea9569f43093c60f027536ea60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4942644
    Reviewed-by: Theo Johnson-kanu <tjohnsonkanu@google.com>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211739}

--- ash/webui/common/resources/cellular_setup/confirmation_code_page.js
+++ ash/webui/common/resources/cellular_setup/confirmation_code_page.js
@@ -41,14 +41,6 @@ Polymer({
     showError: {
       type: Boolean,
     },
-
-    /**
-     * Indicates the UI is busy with an operation and cannot be interacted with.
-     */
-    showBusy: {
-      type: Boolean,
-      value: false,
-    },
   },
 
   /**
