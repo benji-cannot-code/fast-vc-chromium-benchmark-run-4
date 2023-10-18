f08f426f575861f92b3ba5d771aa5adb2678caf9
Michele Fan <michelefan@chromium.org>
2023-10-18 00:07:20 +0000

    snap-group: Move windows contained in `OverviewGroupItem` to another desk
    This cl implements the 1st part of moving `OverviewGroupItem` to another
    desk i.e. moving the windows to the target desk. The 2nd part to restore
    the snap group will be implemented in another cl.
    
    Fixed: b/303093501
    Demo: http://b/303093501#comment2
    Test: Added unit test + run existing
    Change-Id: Iaf14392db1a69849461b084c21aa033fdf11b910
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4938233
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211201}

--- ash/wm/overview/overview_window_drag_controller.cc
+++ ash/wm/overview/overview_window_drag_controller.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/overview_window_drag_controller.h"
 
 #include <algorithm>
-#include <utility>
 
 #include "ash/display/mouse_cursor_event_filter.h"
 #include "ash/screen_util.h"
