efaf9bf7eff23627cff37bf70bf8fd0ba4b4c037
Kristi Saney <kristislee@google.com>
2023-10-18 16:50:50 +0000

    Fix voice selection drop down to match mocks
    https://screenshot.googleplex.com/599FFqcyzmyiGfE
    
    Bug: 1474951
    Change-Id: I40a7a86c4aba55447f61e70977c5f68a18596830
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4950578
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1211563}

--- chrome/browser/resources/side_panel/read_anything/read_anything_toolbar.ts
+++ chrome/browser/resources/side_panel/read_anything/read_anything_toolbar.ts
@@ -505,7 +505,8 @@ export class ReadAnythingToolbar extends ReadAnythingToolbarBase {
               ]),
           []);
 
-      this.openMenu_(this.$.voiceSelectionMenu, event.target as HTMLElement);
+      this.openMenu_(
+          this.$.voiceSelectionMenu, event.target as HTMLElement, true);
     }
   }
 
@@ -513,7 +514,9 @@ export class ReadAnythingToolbar extends ReadAnythingToolbarBase {
     this.openMenu_(this.$.moreOptionsMenu, event.target as HTMLElement);
   }
 
-  private openMenu_(menuToOpen: CrActionMenuElement, target: HTMLElement) {
+  private openMenu_(
+      menuToOpen: CrActionMenuElement, target: HTMLElement,
+      fullScreen: boolean = false) {
     // The button should stay active while the menu is open and deactivate when
     // the menu closes.
     menuToOpen.addEventListener('close', () => {
@@ -524,11 +527,22 @@ export class ReadAnythingToolbar extends ReadAnythingToolbarBase {
 
     const shadowRoot = this.shadowRoot;
     assert(shadowRoot);
-    menuToOpen.showAt(target, {
-      anchorAlignmentX: AnchorAlignment.AFTER_START,
-      anchorAlignmentY: AnchorAlignment.AFTER_END,
-      noOffset: true,
-    });
+    const minY = target.getBoundingClientRect().bottom;
+    if (fullScreen) {
+      menuToOpen.showAt(target, {
+        minY: minY,
+        left: 0,
+        anchorAlignmentY: AnchorAlignment.AFTER_END,
+        noOffset: true,
+      });
+    } else {
+      menuToOpen.showAt(target, {
+        minY: minY,
+        anchorAlignmentX: AnchorAlignment.AFTER_START,
+        anchorAlignmentY: AnchorAlignment.AFTER_END,
+        noOffset: true,
+      });
+    }
   }
 
   private onHighlightClick_() {
