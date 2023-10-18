7f07a8e928a7ffa56db5f65c78440928ce6173ee
Peter Kasting <pkasting@chromium.org>
2023-10-18 21:07:49 +0000

    Make StringPrintf() calls use constexpr format strings: extensions/
    Bug: 1371963
    Change-Id: I3846a1836cb9b5cf39cfd297f96f4b7858a3012b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4950589
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211742}

--- extensions/browser/file_reader.cc
+++ extensions/browser/file_reader.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
-#include "base/strings/stringprintf.h"
+#include "base/strings/strcat.h"
 #include "base/task/single_thread_task_runner.h"
 #include "extensions/browser/extension_file_task_runner.h"
 
@@ -49,12 +49,9 @@ void FileReader::ReadFilesOnFileSequence() {
       // If `file_data` is non-empty, then the file length exceeded
       // `max_resources_length_`. Otherwise, another error was encountered when
       // attempting to read the file.
-      const char* const format_string =
-          file_data->empty()
-              ? "Could not load file: '%s'."
-              : "Could not load file: '%s'. Resource size exceeded.";
-      error = base::StringPrintf(
-          format_string, resource.relative_path().AsUTF8Unsafe().c_str());
+      error = base::StrCat(
+          {"Could not load file: '", resource.relative_path().AsUTF8Unsafe(),
+           "'.", file_data->empty() ? "" : " Resource size exceeded."});
 
       // Clear `data` to avoid passing a partial result.
       data.clear();
