5752f886498050e08a50ccaae24c226fdc604b12
Sam Maier <smaier@chromium.org>
2023-10-18 02:37:58 +0000

    jni_zero: Migrating all JNI annotations in /chromecast to jni_zero copies #cleanup
    This CL is the result of a few simple sed commands, switching any import
    statement using any of the 6 JNI annotations, and switching over the gn
    targets from base:jni_java to jni_zero_java.
    
    This CL was uploaded by git cl split.
    
    R=spang@chromium.org
    
    Bug: 1456762
    Change-Id: I66a6885475f4e482d5852fbb3b3cce967acb5e6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4939412
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Commit-Queue: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211269}

--- chromecast/base/java/src/org/chromium/chromecast/base/DumpstateWriter.java
+++ chromecast/base/java/src/org/chromium/chromecast/base/DumpstateWriter.java
@@ -5,9 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chromecast.base;
 
+import org.jni_zero.CalledByNative;
+import org.jni_zero.JNINamespace;
+
 import org.chromium.base.Log;
-import org.chromium.base.annotations.CalledByNative;
-import org.chromium.base.annotations.JNINamespace;
 
 import java.io.PrintWriter;
 import java.util.HashMap;
--- chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastMetricsHelper.java
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastMetricsHelper.java
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chromecast.shell;
 
-import org.chromium.base.annotations.JNINamespace;
-import org.chromium.base.annotations.NativeMethods;
+import org.jni_zero.JNINamespace;
+import org.jni_zero.NativeMethods;
 
 /**
  * Wrapper of native CastMetricsHelper.
