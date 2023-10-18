8f293be68f936765a4fba888855f965e5527e0f5
David Benjamin <davidben@chromium.org>
2023-10-18 20:16:30 +0000

    Cap the path builder iteration limit to 20
    UMA reports that all validations below a rounding error (the
    tool shows 100%) need 10 iterations, except on Chrome OS where the
    number is 12. These numbers are also an overestimate because they
    count failed validations.
    
    Other uses of this code have picked limits of 18 and 20. Set an
    upper bound of 20, which is about twice what we have any evidence
    of anyone needing. The current limit is far, far too high. On the
    off chance this causes an issue, add a base::Feature kill switch for a
    couple releases, but I think this otherwise does not need much fanfare.
    
    Bug: 634470
    Change-Id: Ie7dd23c52b6ac0a6d3319c01767b210b4718cc3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4903550
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211710}

--- net/cert/cert_verify_proc_builtin.cc
+++ net/cert/cert_verify_proc_builtin.cc
@@ -9,12 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/feature_list.h"
 #include "base/logging.h"
 #include "base/memory/raw_ptr.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/string_piece.h"
 #include "base/values.h"
 #include "crypto/sha2.h"
+#include "net/base/features.h"
 #include "net/base/net_errors.h"
 #include "net/cert/cert_net_fetcher.h"
 #include "net/cert/cert_status_flags.h"
@@ -47,8 +49,10 @@ namespace net {
 namespace {
 
 // Very conservative iteration count limit.
-// TODO(https://crbug.com/634470): Make this smaller.
+// TODO(https://crbug.com/634470): Remove this in favor of
+// kPathBuilderIterationLimitNew.
 constexpr uint32_t kPathBuilderIterationLimit = 25000;
+constexpr uint32_t kPathBuilderIterationLimitNew = 20;
 
 constexpr base::TimeDelta kMaxVerificationTime = base::Seconds(60);
 
@@ -623,7 +627,12 @@ CertPathBuilder::Result TryBuildPath(
     }
   }
 
-  path_builder.SetIterationLimit(kPathBuilderIterationLimit);
+  if (base::FeatureList::IsEnabled(
+          features::kNewCertPathBuilderIterationLimit)) {
+    path_builder.SetIterationLimit(kPathBuilderIterationLimitNew);
+  } else {
+    path_builder.SetIterationLimit(kPathBuilderIterationLimit);
+  }
 
   return path_builder.Run();
 }
