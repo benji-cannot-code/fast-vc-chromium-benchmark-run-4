5603e5d95b41b37339b5477a607c542520b98cff
Scott Yoder <scottyoder@google.com>
2023-10-18 13:36:57 +0000

    [ios] Reenable web_share_egtest suite
    This changes the method used to tap on the web share button to use
    TapWebElementUnverified(), which simulates a tap without injecting JS.
    This seems to "trick" the WebView into enabling Transient Activation
    which then allows the web share API to succeed.
    
    Fixed: 1468173
    Change-Id: I29f8ee9d0aebf83cf556f1c4dfc1f49c411ac7eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4949191
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1211462}

--- ios/chrome/browser/web/web_share_egtest.mm
+++ ios/chrome/browser/web/web_share_egtest.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
+#import "ios/web/public/test/element_selector.h"
 #import "net/test/embedded_test_server/embedded_test_server.h"
 #import "net/test/embedded_test_server/http_request.h"
 #import "net/test/embedded_test_server/http_response.h"
@@ -79,6 +80,13 @@ std::unique_ptr<net::test_server::HttpResponse> HandleRequest(
   return std::move(http_response);
 }
 
+// Taps the Web Share button in the web view.
+void TapWebShareButton() {
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::WebViewMatcher()]
+      performAction:chrome_test_util::TapWebElementUnverified([ElementSelector
+                        selectorWithElementID:kWebShareButtonId])];
+}
+
 }  // namespace
 
 @interface WebShareTestCase : ChromeTestCase
@@ -95,15 +103,9 @@ std::unique_ptr<net::test_server::HttpResponse> HandleRequest(
 
 // Tests that a fully specified url can be shared.
 - (void)testShareUrl {
-  // TODO(crbug.com/1468173): Failing on iOS17.
-  if (@available(iOS 17.0, *)) {
-    XCTSkip(@"Tapping kWebShareButtonId is failing on iOS17 beta 5.");
-  }
-
   const GURL pageURL = self.testServer->GetURL(kWebShareValidLinkUrl);
   [ChromeEarlGrey loadURL:pageURL];
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::WebViewMatcher()]
-      performAction:chrome_test_util::TapWebElementWithId(kWebShareButtonId)];
+  TapWebShareButton();
 
   [ChromeEarlGrey tapButtonInActivitySheetWithID:@"Copy"];
 
@@ -112,15 +114,9 @@ std::unique_ptr<net::test_server::HttpResponse> HandleRequest(
 
 // Tests that a relative  url can be shared.
 - (void)testShareRelativeUrl {
-  // TODO(crbug.com/1468173): Failing on iOS17.
-  if (@available(iOS 17.0, *)) {
-    XCTSkip(@"Tapping kWebShareButtonId is failing on iOS17 beta 5.");
-  }
-
   const GURL pageURL = self.testServer->GetURL(kWebShareRelativeLinkUrl);
   [ChromeEarlGrey loadURL:pageURL];
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::WebViewMatcher()]
-      performAction:chrome_test_util::TapWebElementWithId(kWebShareButtonId)];
+  TapWebShareButton();
 
   [ChromeEarlGrey tapButtonInActivitySheetWithID:@"Copy"];
 
@@ -129,16 +125,10 @@ std::unique_ptr<net::test_server::HttpResponse> HandleRequest(
 
 // Tests that a relative url can be shared when the filename starts with "file".
 - (void)testShareRelativeFilenameUrl {
-  // TODO(crbug.com/1468173): Failing on iOS17.
-  if (@available(iOS 17.0, *)) {
-    XCTSkip(@"Tapping kWebShareButtonId is failing on iOS17 beta 5.");
-  }
-
   const GURL pageURL =
       self.testServer->GetURL(kWebShareRelativeFilenameFileUrl);
   [ChromeEarlGrey loadURL:pageURL];
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::WebViewMatcher()]
-      performAction:chrome_test_util::TapWebElementWithId(kWebShareButtonId)];
+  TapWebShareButton();
 
   [ChromeEarlGrey tapButtonInActivitySheetWithID:@"Copy"];
 
@@ -149,8 +139,7 @@ std::unique_ptr<net::test_server::HttpResponse> HandleRequest(
 - (void)testShareFileUrl {
   const GURL pageURL = self.testServer->GetURL(kWebShareFileUrl);
   [ChromeEarlGrey loadURL:pageURL];
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::WebViewMatcher()]
-      performAction:chrome_test_util::TapWebElementWithId(kWebShareButtonId)];
+  TapWebShareButton();
 
   [ChromeEarlGrey waitForWebStateContainingText:kWebShareStatusFailure];
 
@@ -160,14 +149,9 @@ std::unique_ptr<net::test_server::HttpResponse> HandleRequest(
 
 // Tests that an url object can be shared.
 - (void)testShareUrlObject {
-  // TODO(crbug.com/1468173): Failing on iOS17.
-  if (@available(iOS 17.0, *)) {
-    XCTSkip(@"Tapping kWebShareButtonId is failing on iOS17 beta 5.");
-  }
   const GURL pageURL = self.testServer->GetURL(kWebShareUrlObjectUrl);
   [ChromeEarlGrey loadURL:pageURL];
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::WebViewMatcher()]
-      performAction:chrome_test_util::TapWebElementWithId(kWebShareButtonId)];
+  TapWebShareButton();
 
   [ChromeEarlGrey tapButtonInActivitySheetWithID:@"Copy"];
 
