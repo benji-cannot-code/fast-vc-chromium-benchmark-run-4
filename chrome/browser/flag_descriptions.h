753a787bff3840c2c31f08e15650411f7097c9f0
Ryan Kalla <ryankalla@google.com>
2023-10-18 15:41:19 +0000

    Remove Private State Tokens flag
    Private State Tokens are now enabled by default and the flag is expired.
    We can clean up the code by removing the flag metadata.
    
    Bug: 1490160
    Change-Id: Ib5da48dbb981b2394bf0ad279d120f32a76ce37b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4926510
    Reviewed-by: Steven Valdez <svaldez@chromium.org>
    Commit-Queue: Ryan Kalla <ryankalla@google.com>
    Cr-Commit-Position: refs/heads/main@{#1211529}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1936,9 +1936,6 @@ extern const char kTranslateForceTriggerOnEnglishDescription[];
 extern const char kTreatInsecureOriginAsSecureName[];
 extern const char kTreatInsecureOriginAsSecureDescription[];
 
-extern const char kPrivateStateTokensName[];
-extern const char kPrivateStateTokensDescription[];
-
 extern const char kTailoredSecurityRetryForSyncUsersName[];
 extern const char kTailoredSecurityRetryForSyncUsersDescription[];
 
