df8c3e36872af5e06e3fc437df82ba9ccf993da7
Courtney Wong <courtneywong@chromium.org>
2023-10-18 23:55:15 +0000

    [Families TS Migration] Migrate shared supervision resources to TS
    Tested: Tested error and offline screens on PG WebUIs.
    
    Bug: b:293655914
    Change-Id: Ia97c6d642b1e23530fe220025c16e85176a58629
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4950470
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211844}

--- chrome/browser/resources/chromeos/supervision/supervised_user_error.ts
+++ chrome/browser/resources/chromeos/supervision/supervised_user_error.ts
@@ -3,7 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {getTemplate} from './supervised_user_error.html.js';
 
 class SupervisedUserError extends PolymerElement {
   static get is() {
@@ -11,7 +12,7 @@ class SupervisedUserError extends PolymerElement {
   }
 
   static get template() {
-    return html`{__html_template__}`;
+    return getTemplate();
   }
 }
 customElements.define(SupervisedUserError.is, SupervisedUserError);
--- chrome/browser/resources/chromeos/supervision/supervised_user_offline.ts
+++ chrome/browser/resources/chromeos/supervision/supervised_user_offline.ts
@@ -3,9 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'chrome://resources/cr_elements/cr_view_manager/cr_view_manager.js';
-
-import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {getTemplate} from './supervised_user_offline.html.js';
 
 class SupervisedUserOffline extends PolymerElement {
   static get is() {
@@ -13,7 +12,7 @@ class SupervisedUserOffline extends PolymerElement {
   }
 
   static get template() {
-    return html`{__html_template__}`;
+    return getTemplate();
   }
 }
 customElements.define(SupervisedUserOffline.is, SupervisedUserOffline);
