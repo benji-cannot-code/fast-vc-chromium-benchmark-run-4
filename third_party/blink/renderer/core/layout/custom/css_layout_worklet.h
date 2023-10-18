efdc4d286e595063068b8d3e149aa5c2ee371f75
Kent Tamura <tkent@chromium.org>
2023-10-18 03:52:19 +0000

    LayoutNG: Move layout/ng/custom/* to layout/custom/
    This CL has no behavior changes.
    
    Bug: 1492009
    Change-Id: I3be7317bf5717392e0a37715bc8ed58569c2c8fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4950311
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211296}

---
+++ third_party/blink/renderer/core/layout/custom/css_layout_worklet.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_CUSTOM_CSS_LAYOUT_WORKLET_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_CUSTOM_CSS_LAYOUT_WORKLET_H_
+
+#include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
+
+namespace blink {
+
+class ScriptState;
+class Worklet;
+
+class CORE_EXPORT CSSLayoutWorklet {
+  STATIC_ONLY(CSSLayoutWorklet);
+
+ public:
+  static Worklet* layoutWorklet(ScriptState*);
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_CUSTOM_CSS_LAYOUT_WORKLET_H_
