9f4ae55b2c040edc0026567a1233ea93912939b6
Yuichiro Hanada <yhanada@google.com>
2023-10-18 12:36:35 +0000

    Remove ArcLockScreenBridge
    All usage was removed already. Let's clean up the bridge.
    
    Bug: b:305829795
    Change-Id: I94ac1db3dad9d1c3c9d3330a977c5762e83a6f6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4942272
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211443}

--- chrome/browser/ash/arc/session/arc_service_launcher.cc
+++ chrome/browser/ash/arc/session/arc_service_launcher.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/components/arc/disk_quota/arc_disk_quota_bridge.h"
 #include "ash/components/arc/ime/arc_ime_service.h"
 #include "ash/components/arc/keyboard_shortcut/arc_keyboard_shortcut_bridge.h"
-#include "ash/components/arc/lock_screen/arc_lock_screen_bridge.h"
 #include "ash/components/arc/media_session/arc_media_session_bridge.h"
 #include "ash/components/arc/memory/arc_memory_bridge.h"
 #include "ash/components/arc/memory_pressure/arc_memory_pressure_bridge.h"
@@ -279,7 +278,6 @@ void ArcServiceLauncher::OnPrimaryUserProfilePrepared(Profile* profile) {
     ArcKeymasterBridge::GetForBrowserContext(profile);
   }
   ArcKioskBridge::GetForBrowserContext(profile);
-  ArcLockScreenBridge::GetForBrowserContext(profile);
   ArcMediaSessionBridge::GetForBrowserContext(profile);
   {
     auto* metrics_service = ArcMetricsService::GetForBrowserContext(profile);
@@ -470,7 +468,6 @@ void ArcServiceLauncher::EnsureFactoriesBuilt() {
     ArcKeymasterBridge::EnsureFactoryBuilt();
   }
   ArcKioskBridge::EnsureFactoryBuilt();
-  ArcLockScreenBridge::EnsureFactoryBuilt();
   ArcMediaSessionBridge::EnsureFactoryBuilt();
   ArcMemoryBridge::EnsureFactoryBuilt();
   ArcMemoryPressureBridge::EnsureFactoryBuilt();
