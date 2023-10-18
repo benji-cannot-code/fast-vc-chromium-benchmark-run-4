33c75dc3021f7f19eff1ed1adc976c43eec1091c
Weizhong Xia <weizhong@google.com>
2023-10-18 19:08:23 +0000

    Migrate to new baseline format - external/wpt/(service-workers, websockets)
    The new baselines format for test harness tests is machine parsable.
    Do this incrementally to make it easier to handle and review.
    
    With this change, we will be able to regenerate WPT metadata from baselines.
    
    Note to Gardeners: Please don't revert this CL if there are
    individual test failures. Pls ping and I can rebaseline for
    such tests.
    
    Bug: 1481730
    Change-Id: Icb7caaae14ec396b218b6c7553b6183143f6563c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4950359
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211662}

--- third_party/blink/web_tests/resources/testharnessreport.js
+++ third_party/blink/web_tests/resources/testharnessreport.js
@@ -279,7 +279,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             location.pathname.startsWith('/html/') ||
             location.pathname.startsWith('/css/css-') ||
             location.pathname.startsWith('/editing/') ||
-            location.pathname.startsWith('/fetch/')) {
+            location.pathname.startsWith('/fetch/') ||
+            location.pathname.startsWith('/service-workers/') ||
+            location.pathname.startsWith('/websockets/')) {
             return true;
         }
         return false;
