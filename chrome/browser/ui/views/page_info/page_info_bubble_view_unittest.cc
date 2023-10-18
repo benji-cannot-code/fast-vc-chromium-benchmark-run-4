1abe9b39996e97285cb189286d7345d7f7ded20e
Kevin Graney <kmg@google.com>
2023-10-18 11:52:07 +0000

    Enable UserBypass by default for M120+
    This is in preparation for fully launching the Finch feature.
    
    Bug: b:285316200
    Change-Id: I73009327cdd68591436beb276ec8ed7adf50e32a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4946575
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Kevin Graney <kmg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1211430}

--- chrome/browser/ui/views/page_info/page_info_bubble_view_unittest.cc
+++ chrome/browser/ui/views/page_info/page_info_bubble_view_unittest.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/browser/content_settings_uma_util.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/content_settings/core/common/cookie_blocking_3pcd_status.h"
+#include "components/content_settings/core/common/features.h"
 #include "components/content_settings/core/common/pref_names.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/page_info/core/features.h"
@@ -1136,7 +1137,8 @@ class PageInfoBubbleViewCookiesSubpageTest : public PageInfoBubbleViewTest {
  public:
   PageInfoBubbleViewCookiesSubpageTest() {
     feature_list.InitWithFeatures(
-        {privacy_sandbox::kPrivacySandboxFirstPartySetsUI}, {});
+        {privacy_sandbox::kPrivacySandboxFirstPartySetsUI},
+        {content_settings::features::kUserBypassUI});
   }
 
   void SetUp() override {
