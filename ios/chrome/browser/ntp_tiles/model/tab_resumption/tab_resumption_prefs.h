d43a0f2d03dabd1d4f9f4a68ea9fbe6d5d1e7ccb
Ewann Pelle <ewannpv@chromium.org>
2023-10-18 13:56:13 +0000

    [iOS][TR] Add restriction on restart
    When the tab presented in the tab resumption tile is opened, this tab
    should not be presented again after killing the app. To avoid that, a
    pref is added containing the last opened tab URL.
    
    Bug: 1478156
    Change-Id: I99e6acf8c208b5f3c15e60451390da417356f8a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4946410
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211472}

--- ios/chrome/browser/ntp_tiles/model/tab_resumption/tab_resumption_prefs.h
+++ ios/chrome/browser/ntp_tiles/model/tab_resumption/tab_resumption_prefs.h
@@ -6,13 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_NTP_TILES_MODEL_TAB_RESUMPTION_TAB_RESUMPTION_PREFS_H_
 #define IOS_CHROME_BROWSER_NTP_TILES_MODEL_TAB_RESUMPTION_TAB_RESUMPTION_PREFS_H_
 
+class GURL;
 class PrefRegistrySimple;
 class PrefService;
 
 namespace tab_resumption_prefs {
 
-// Pref name that disable the tab resumption tile.
+// Pref name that disables the tab resumption tile.
 extern const char kTabResumptioDisabledPref[];
+
+// Pref name that stores the last opened tab URL.
+extern const char kTabResumptionLastOpenedTabURLPref[];
 
 // Registers the prefs associated with the tab resumption tile.
 void RegisterPrefs(PrefRegistrySimple* registry);
@@ -22,6 +26,12 @@ bool IsTabResumptionDisabled(PrefService* prefs);
 
 // Disables the tab resumption tile.
 void DisableTabResumption(PrefService* prefs);
+
+// Returns `true` if the given `URL` matches the last opened tab URL.
+bool IsLastOpenedURL(GURL URL, PrefService* prefs);
+
+// Sets the last opened tab URL.
+void SetTabResumptionLastOpenedTabURL(GURL URL, PrefService* prefs);
 
 }  // namespace tab_resumption_prefs
 
