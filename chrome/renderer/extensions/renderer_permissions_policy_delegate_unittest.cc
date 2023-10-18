8f19b27f1254bbd0f98624a90a7a6c6c7cda4247
Dave Tapuska <dtapuska@chromium.org>
2023-10-18 00:06:04 +0000

    Move ResponseWorker to be a response on RequestWorker
    Have ServiceWorkerHost::RequestWorker be the same to
    LocalFrameHost::Request for the response messaging.
    
    Bug: 1364183
    Change-Id: I92d6fa3488a864de52a8594dd325c9ae7600f963
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4886272
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211199}

--- chrome/renderer/extensions/renderer_permissions_policy_delegate_unittest.cc
+++ chrome/renderer/extensions/renderer_permissions_policy_delegate_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/command_line.h"
+#include "base/test/task_environment.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "chrome/renderer/extensions/chrome_extensions_dispatcher_delegate.h"
 #include "content/public/test/mock_render_process_host.h"
@@ -42,6 +43,7 @@ class RendererPermissionsPolicyDelegateTest : public testing::Test {
   }
 
  protected:
+  base::test::SingleThreadTaskEnvironment task_environment;
   std::unique_ptr<content::MockRenderThread> render_thread_;
   std::unique_ptr<ExtensionsRendererClient> renderer_client_;
   std::unique_ptr<Dispatcher> extension_dispatcher_;
--- extensions/browser/extension_function_dispatcher.h
+++ extensions/browser/extension_function_dispatcher.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_function.h"
 #include "extensions/common/features/feature.h"
 #include "extensions/common/mojom/frame.mojom.h"
+#include "extensions/common/mojom/service_worker_host.mojom.h"
 #include "ipc/ipc_sender.h"
 
 namespace content {
@@ -83,8 +84,10 @@ class ExtensionFunctionDispatcher {
   // Message handlers.
   // Dispatches a request for service woker and the response is sent to the
   // corresponding render process in an ExtensionMsg_ResponseWorker message.
-  void DispatchForServiceWorker(mojom::RequestParamsPtr params,
-                                int render_process_id);
+  void DispatchForServiceWorker(
+      mojom::RequestParamsPtr params,
+      int render_process_id,
+      mojom::ServiceWorkerHost::RequestWorkerCallback callback);
 
   // Called when an ExtensionFunction is done executing, after it has sent
   // a response (if any) to the extension.
@@ -115,13 +118,6 @@ class ExtensionFunctionDispatcher {
   }
 
  private:
-  // Same as ResponseCallbackWrapper above, but applies to an extension
-  // function from an extension Service Worker.
-  class WorkerResponseCallbackWrapper;
-
-  // Key used to store WorkerResponseCallbackWrapper in the map
-  // |response_callback_wrappers_for_worker_|.
-  struct WorkerResponseCallbackMapKey;
 
   // Helper to create an ExtensionFunction to handle the function given by
   // |params|. Can be called on any thread.
@@ -149,13 +145,6 @@ class ExtensionFunctionDispatcher {
       browser_context_;
 
   raw_ptr<Delegate, AcrossTasksDanglingUntriaged> delegate_;
-
-  using WorkerResponseCallbackWrapperMap =
-      std::map<WorkerResponseCallbackMapKey,
-               std::unique_ptr<WorkerResponseCallbackWrapper>>;
-  // TODO(lazyboy): The map entries are cleared upon RenderProcessHost shutown,
-  // we should really be clearing it on service worker shutdown.
-  WorkerResponseCallbackWrapperMap response_callback_wrappers_for_worker_;
 
   // The set of ExtensionFunction instances waiting for responses from
   // the renderer. These are removed once the response is processed.
