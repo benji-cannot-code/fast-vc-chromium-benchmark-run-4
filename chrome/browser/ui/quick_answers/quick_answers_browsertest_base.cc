1dc03d90ed5e5674497fa4e757070c66272d6ab3
Peter Kasting <pkasting@chromium.org>
2023-10-18 08:38:43 +0000

    Make StringPrintf() calls use constexpr format strings: chrome/
    Bug: 1371963
    Change-Id: I5149a83c19377a4845a0759b632810251d53c74a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4950852
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211371}

--- chrome/browser/ui/quick_answers/quick_answers_browsertest_base.cc
+++ chrome/browser/ui/quick_answers/quick_answers_browsertest_base.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/quick_answers/quick_answers_browsertest_base.h"
 
+#include "base/strings/strcat.h"
 #include "base/strings/stringprintf.h"
 #include "chrome/test/base/chrome_test_utils.h"
 #include "chromeos/components/quick_answers/public/cpp/quick_answers_state.h"
@@ -19,10 +20,6 @@ namespace {
 constexpr char kDataUrlTemplate[] =
     "data:text/html,<html><body><span style=\"position: absolute; left: %ipx; "
     "top: %ipx;\">%s</body></html>";
-
-constexpr char kDataUrlPasswordFieldTemplate[] =
-    "data:text/html,<html><body><span style=\"position: absolute; left: %ipx; "
-    "top: %ipx;\"><input type=\"password\">%s</input></body></html>";
 
 }  // namespace
 
@@ -40,10 +37,13 @@ void QuickAnswersBrowserTestBase::ShowMenu(
   content::WebContents* web_contents =
       chrome_test_utils::GetActiveWebContents(this);
 
-  std::string data_url = base::StringPrintf(
-      params.is_password_field ? kDataUrlPasswordFieldTemplate
-                               : kDataUrlTemplate,
-      params.x, params.y, params.selected_text.c_str());
+  const std::string text =
+      params.is_password_field
+          ? base::StrCat(
+                {"<input type=\"password\">", params.selected_text, "</input>"})
+          : params.selected_text;
+  const std::string data_url =
+      base::StringPrintf(kDataUrlTemplate, params.x, params.y, text.c_str());
   ASSERT_TRUE(content::NavigateToURL(web_contents, GURL(data_url)));
 
   content::RenderFrameHost* main_frame = web_contents->GetPrimaryMainFrame();
