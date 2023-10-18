a4ee09fa069cb5454df735201cdc4c69be0e5151
Elad Alon <eladalon@chromium.org>
2023-10-18 19:07:04 +0000

    [Element Capture][Token] Rename CropIdWebContentsHelper
    Rename CropIdWebContentsHelper to SubCaptureTargetIdWebContentsHelper.
    This change anticipates an expansion of that class in a subsequent CL,
    allowing it to assume wider responsibilities. To allow easier following
    of the changes in the next CL, the renaming of the class is carried
    out in this dedicated CL, similarly to how the renaming of the files
    was done in a previous CL.
    
    Bug: 1418194
    Change-Id: I975b62f1bec9adcbd490dda0440d8621aafbe91d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4879158
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211660}

--- content/browser/media/capture/sub_capture_target_id_web_contents_helper.h
+++ content/browser/media/capture/sub_capture_target_id_web_contents_helper.h
@@ -27,9 +27,9 @@ namespace content {
 
 class NavigationHandle;
 
-class CONTENT_EXPORT CropIdWebContentsHelper final
+class CONTENT_EXPORT SubCaptureTargetIdWebContentsHelper final
     : public WebContentsObserver,
-      public WebContentsUserData<CropIdWebContentsHelper> {
+      public WebContentsUserData<SubCaptureTargetIdWebContentsHelper> {
  public:
   // Limits the number of crop-IDs a given Web-application can produce
   // so as to limit the potential for abuse.
@@ -38,10 +38,12 @@ class CONTENT_EXPORT CropIdWebContentsHelper final
   // embed such iframes.
   constexpr static size_t kMaxCropIdsPerWebContents = 100;
 
-  explicit CropIdWebContentsHelper(WebContents* web_contents);
-  CropIdWebContentsHelper(const CropIdWebContentsHelper&) = delete;
-  CropIdWebContentsHelper& operator=(const CropIdWebContentsHelper&) = delete;
-  ~CropIdWebContentsHelper() final;
+  explicit SubCaptureTargetIdWebContentsHelper(WebContents* web_contents);
+  SubCaptureTargetIdWebContentsHelper(
+      const SubCaptureTargetIdWebContentsHelper&) = delete;
+  SubCaptureTargetIdWebContentsHelper& operator=(
+      const SubCaptureTargetIdWebContentsHelper&) = delete;
+  ~SubCaptureTargetIdWebContentsHelper() final;
 
   // Produces a new crop-ID, records its association with this WebContents
   // and returns it.
@@ -64,8 +66,8 @@ class CONTENT_EXPORT CropIdWebContentsHelper final
   static base::Token GUIDToToken(const base::Uuid& guid);
 
  private:
-  friend class WebContentsUserData<CropIdWebContentsHelper>;
-  friend class CropIdWebContentsHelperTest;
+  friend class WebContentsUserData<SubCaptureTargetIdWebContentsHelper>;
+  friend class SubCaptureTargetIdWebContentsHelperTest;
 
   // WebContentsObserver implementation.
   // Cross-document navigation of the top-level document discards all crop-IDs
