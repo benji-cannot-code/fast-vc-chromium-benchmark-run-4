a91e4a5b28af19a58442a39c5540c44815215cea
Abe Boujane <boujane@google.com>
2023-10-18 21:34:42 +0000

    Add IPH config for TrackingProtection Rollback Notice
    Bug: b:304202326
    Change-Id: I24caaac689c53da9fb6cf40cf1e7940e0bd474b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4947613
    Commit-Queue: Abe Boujane <boujane@google.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211757}

--- components/feature_engagement/public/feature_list.cc
+++ components/feature_engagement/public/feature_list.cc
@@ -181,6 +181,7 @@ const base::Feature* const kAllFeatures[] = {
     &kIPHSideSearchFeature,
     &kIPHSideSearchPageActionLabelFeature,
     &kIPHTabSearchFeature,
+    &kIPHTrackingProtectionOffboardingFeature,
     &kIPHTrackingProtectionOnboardingFeature,
     &kIPHWebUITabStripFeature,
     &kIPHDesktopPwaInstallFeature,
