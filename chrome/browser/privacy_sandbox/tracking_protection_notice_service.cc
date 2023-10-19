5c14058d5447def1e076fa32982d2c43d0ee7f23
Abe Boujane <boujane@google.com>
2023-10-19 15:01:45 +0000

    Added TrackingProtection Rollback Notice
    Bug: b:304202326
    Change-Id: I9ec9ee8bc6c7f11856bf5ed4bbe0c2608f49c4d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4952218
    Commit-Queue: Abe Boujane <boujane@google.com>
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Auto-Submit: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1212154}

--- chrome/browser/privacy_sandbox/tracking_protection_notice_service.cc
+++ chrome/browser/privacy_sandbox/tracking_protection_notice_service.cc
@@ -145,15 +145,6 @@ void TrackingProtectionNoticeService::BaseIPHNotice::
   }
 }
 
-TrackingProtectionNoticeService::OnboardingNotice::OnboardingNotice(
-    Profile* profile,
-    TrackingProtectionOnboarding* onboarding_service)
-    : BaseIPHNotice(profile, onboarding_service) {}
-
-NoticeType TrackingProtectionNoticeService::OnboardingNotice::GetNoticeType() {
-  return NoticeType::kOnboarding;
-}
-
 bool TrackingProtectionNoticeService::BaseIPHNotice::MaybeShowPromo(
     Browser* browser) {
   base::Time shown_when = base::Time::Now();
@@ -176,9 +167,32 @@ bool TrackingProtectionNoticeService::BaseIPHNotice::IsPromoShowing(
   return browser->window()->IsFeaturePromoActive(GetIPHFeature());
 }
 
+TrackingProtectionNoticeService::OnboardingNotice::OnboardingNotice(
+    Profile* profile,
+    TrackingProtectionOnboarding* onboarding_service)
+    : BaseIPHNotice(profile, onboarding_service) {}
+
+NoticeType TrackingProtectionNoticeService::OnboardingNotice::GetNoticeType() {
+  return NoticeType::kOnboarding;
+}
+
 const base::Feature&
 TrackingProtectionNoticeService::OnboardingNotice::GetIPHFeature() {
   return feature_engagement::kIPHTrackingProtectionOnboardingFeature;
+}
+
+TrackingProtectionNoticeService::OffboardingNotice::OffboardingNotice(
+    Profile* profile,
+    TrackingProtectionOnboarding* onboarding_service)
+    : BaseIPHNotice(profile, onboarding_service) {}
+
+const base::Feature&
+TrackingProtectionNoticeService::OffboardingNotice::GetIPHFeature() {
+  return feature_engagement::kIPHTrackingProtectionOffboardingFeature;
+}
+
+NoticeType TrackingProtectionNoticeService::OffboardingNotice::GetNoticeType() {
+  return NoticeType::kOffboarding;
 }
 
 void TrackingProtectionNoticeService::BaseIPHNotice::OnNoticeClosed(
@@ -212,8 +226,10 @@ void TrackingProtectionNoticeService::OnShouldShowNoticeUpdated() {
           std::make_unique<OnboardingNotice>(profile_, onboarding_service_);
       InitializeTabStripTracker();
       return;
-    case NoticeType::kOffboarding:
-      // TODO(b:304202326) Create the offboarding notice here.
+    case TrackingProtectionOnboarding::NoticeType::kOffboarding:
+      offboarding_notice_ =
+          std::make_unique<OffboardingNotice>(profile_, onboarding_service_);
+      InitializeTabStripTracker();
       return;
   };
 }
@@ -234,7 +250,9 @@ void TrackingProtectionNoticeService::OnTabStripModelChanged(
   if (!selection.active_tab_changed()) {
     return;
   }
-  if (onboarding_notice_) {
+  if (offboarding_notice_) {
+    offboarding_notice_->MaybeUpdateNoticeVisibility(selection.new_contents);
+  } else if (onboarding_notice_) {
     onboarding_notice_->MaybeUpdateNoticeVisibility(selection.new_contents);
   }
 }
@@ -266,7 +284,10 @@ void TrackingProtectionNoticeService::TabHelper::DidFinishNavigation(
 
   auto* notice_service =
       TrackingProtectionNoticeFactory::GetForProfile(profile);
-  if (notice_service->onboarding_notice_) {
+  if (notice_service->offboarding_notice_) {
+    notice_service->offboarding_notice_->MaybeUpdateNoticeVisibility(
+        web_contents());
+  } else if (notice_service->onboarding_notice_) {
     notice_service->onboarding_notice_->MaybeUpdateNoticeVisibility(
         web_contents());
   }
