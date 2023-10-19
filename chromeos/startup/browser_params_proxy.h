b077b9036bd00108d12b3f035ba65ea1254dc800
Judy Wang <shidi@chromium.org>
2023-10-19 01:38:52 +0000

    Desk Profiles: Add kDeskProfiles feature flag to ChromeOS.
    Add desk profile flag to Ash/Lacros.
    
    Test: Manual
    Bug: b/306000667
    Change-Id: I10fec770f7a42dd9e69377553f473420004fa260
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4953589
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Commit-Queue: Judy Wang <shidi@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211903}

--- chromeos/startup/browser_params_proxy.h
+++ chromeos/startup/browser_params_proxy.h
@@ -153,6 +153,8 @@ class COMPONENT_EXPORT(CHROMEOS_STARTUP) BrowserParamsProxy {
 
   bool IsAppInstallServiceUriEnabled() const;
 
+  bool IsDeskProfilesEnabled() const;
+
  private:
   friend base::NoDestructor<BrowserParamsProxy>;
 
