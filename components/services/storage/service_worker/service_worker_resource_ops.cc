fe8c7c29da3afb5d7daad60bd6a12e84ebc36040
Tom Sepez <tsepez@chromium.org>
2023-10-19 16:42:56 +0000

    Also use base::span<> in NetToMojoPendingBuffer
    Follow-on from
      https://chromium-review.googlesource.com/c/chromium/src/+/4943081
    
    Perform the same conversion for the class used when going in the
    opposite direction.
    
    Bug: 1493389
    Change-Id: Ib942984fe8d1027865fde453555b286d60ba7686
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4950351
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1212213}

--- components/services/storage/service_worker/service_worker_resource_ops.cc
+++ components/services/storage/service_worker/service_worker_resource_ops.cc
@@ -318,9 +318,8 @@ class ServiceWorkerResourceReaderImpl::DataReader {
       return;
     }
 
-    uint32_t num_bytes = 0;
     MojoResult rv = network::NetToMojoPendingBuffer::BeginWrite(
-        &producer_handle_, &pending_buffer_, &num_bytes);
+        &producer_handle_, &pending_buffer_);
     switch (rv) {
       case MOJO_RESULT_INVALID_ARGUMENT:
       case MOJO_RESULT_BUSY:
@@ -339,6 +338,7 @@ class ServiceWorkerResourceReaderImpl::DataReader {
         break;
     }
 
+    uint32_t num_bytes = pending_buffer_->size();
     num_bytes = std::min(num_bytes, blink::BlobUtils::GetDataPipeChunkSize());
     scoped_refptr<network::NetToMojoIOBuffer> buffer =
         base::MakeRefCounted<network::NetToMojoIOBuffer>(pending_buffer_.get());
