616a8e2d37189cfb81ab214d330b44fabf646a62
Dmitry Vykochko <vykochko@google.com>
2023-10-19 12:23:55 +0000

    Don't use line_number from the strategy in the row class.
    This CL is a part the cell class elimination refactoring.
    It makes the row class less dependent on the strategy
    which is to be removed according to the refactoring plan.
    
    Bug: 1491373
    Change-Id: I1263a24bbaef964b0b860b24888b5f38097df6bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4946231
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Cr-Commit-Position: refs/heads/main@{#1212087}

--- chrome/browser/ui/views/autofill/popup/popup_row_view.cc
+++ chrome/browser/ui/views/autofill/popup/popup_row_view.cc
@@ -163,18 +163,21 @@ std::unique_ptr<PopupRowView> PopupRowView::Create(PopupViewViews& popup_view,
 
   return std::make_unique<PopupRowView>(
       /*a11y_selection_delegate=*/popup_view, /*selection_delegate=*/popup_view,
-      controller, std::move(strategy), std::move(new_badge_tracker));
+      controller, line_number, std::move(strategy),
+      std::move(new_badge_tracker));
 }
 
 PopupRowView::PopupRowView(
     AccessibilitySelectionDelegate& a11y_selection_delegate,
     SelectionDelegate& selection_delegate,
     base::WeakPtr<AutofillPopupController> controller,
+    int line_number,
     std::unique_ptr<PopupRowStrategy> strategy,
     std::optional<ScopedNewBadgeTrackerWithAcceptAction> new_badge_tracker)
     : a11y_selection_delegate_(a11y_selection_delegate),
       selection_delegate_(selection_delegate),
       controller_(controller),
+      line_number_(line_number),
       new_badge_tracker_(std::move(new_badge_tracker)),
       strategy_(std::move(strategy)),
       should_ignore_mouse_observed_outside_item_bounds_check_(
@@ -206,8 +209,7 @@ PopupRowView::PopupRowView(
                   view->should_ignore_mouse_observed_outside_item_bounds_check_;
               if (can_select_suggestion) {
                 view->selection_delegate_->SetSelectedCell(
-                    PopupViewViews::CellIndex{view->strategy_->GetLineNumber(),
-                                              type},
+                    PopupViewViews::CellIndex{view->line_number_, type},
                     PopupCellSelectionSource::kMouse);
               }
             },
@@ -303,7 +305,7 @@ void PopupRowView::OnViewFocused(views::View* view) {
     // a11y), but for selection purposes these non-mouse sources are similar
     // enough to treat them equally as a keyboard.
     selection_delegate_->SetSelectedCell(
-        PopupViewViews::CellIndex{strategy_->GetLineNumber(), type},
+        PopupViewViews::CellIndex{line_number_, type},
         PopupCellSelectionSource::kKeyboard);
   }
 }
@@ -378,8 +380,7 @@ bool PopupRowView::HandleKeyPressEvent(
   switch (event.windows_key_code) {
     case ui::VKEY_RETURN:
       if (*GetSelectedCell() == CellType::kContent && controller_) {
-        controller_->AcceptSuggestion(strategy_->GetLineNumber(),
-                                      base::TimeTicks::Now());
+        controller_->AcceptSuggestion(line_number_, base::TimeTicks::Now());
         return true;
       }
       return false;
@@ -405,7 +406,7 @@ void PopupRowView::RunOnAcceptedForEvent(const ui::Event& event) {
   if (new_badge_tracker_) {
     new_badge_tracker_->OnSuggestionAccepted();
   }
-  controller_->AcceptSuggestion(strategy_->GetLineNumber(), time);
+  controller_->AcceptSuggestion(line_number_, time);
 }
 
 const PopupCellView* PopupRowView::GetCellView(CellType type) const {
