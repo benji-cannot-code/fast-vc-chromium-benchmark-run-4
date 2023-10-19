1fbba924919fa5ffa32268023a66051cc0e4f615
Chad Duffin <chadduffin@chromium.org>
2023-10-19 18:10:33 +0000

    network: Toggle info bubble on click
    This change updates the logic so that the network info bubble is toggled
    when the "i" button is pressed instead of closing and immediately
    re-opening the bubble.
    
    Bug: b/303382616, b/304840723
    Test: Unit tests.
    Change-Id: I950e7257f47dc205c9c852d6bc9b96735497ae1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4955494
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212273}

--- ash/system/network/network_detailed_view.cc
+++ ash/system/network/network_detailed_view.cc
@@ -114,6 +114,7 @@ void NetworkDetailedView::OnInfoClicked() {
   if (info_bubble_tracker_.view()) {
     info_bubble_tracker_.view()->GetWidget()->CloseWithReason(
         views::Widget::ClosedReason::kCloseButtonClicked);
+    return;
   }
 
   auto info_bubble = std::make_unique<NetworkInfoBubble>(
