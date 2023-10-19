a202da7261e5a71dac14c062f57d545689f8cadd
Kevin McNee <mcnee@chromium.org>
2023-10-19 15:38:33 +0000

    Use a weak ptr for the RenderWidgetHost in InputEventAckWaiter
    If an InputEventAckWaiter outlives the observed RenderWidgetHost, it
    would not be valid to remove itself with a stale raw pointer. We now use
    a weak ptr to handle this case, as a number of other test
    InputEventObservers do.
    
    We re-enable PrerenderSessionHistoryBrowserTest.BackButtonNavigation
    which started flaking due to the introduction of an InputEventAckWaiter.
    
    Cq-Include-Trybots: luci.chromium.try:linux-bfcache-rel
    Bug: 1492879
    Change-Id: If7b5c5c1b10515d63a6b8ceef1f054f089d69d23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4953321
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212181}

--- content/public/test/browser_test_utils.cc
+++ content/public/test/browser_test_utils.cc
@@ -2803,9 +2803,9 @@ InputMsgWatcher::GetAckStateWaitIfNecessary() {
 
 InputEventAckWaiter::InputEventAckWaiter(RenderWidgetHost* render_widget_host,
                                          InputEventAckPredicate predicate)
-    : render_widget_host_(render_widget_host),
-      predicate_(predicate),
-      event_received_(false) {
+    : render_widget_host_(
+          static_cast<RenderWidgetHostImpl*>(render_widget_host)->GetWeakPtr()),
+      predicate_(predicate) {
   render_widget_host_->AddInputEventObserver(this);
 }
 
@@ -2828,7 +2828,9 @@ InputEventAckWaiter::InputEventAckWaiter(RenderWidgetHost* render_widget_host,
     : InputEventAckWaiter(render_widget_host, EventAckHasType(type)) {}
 
 InputEventAckWaiter::~InputEventAckWaiter() {
-  render_widget_host_->RemoveInputEventObserver(this);
+  if (render_widget_host_) {
+    render_widget_host_->RemoveInputEventObserver(this);
+  }
 }
 
 void InputEventAckWaiter::Wait() {
