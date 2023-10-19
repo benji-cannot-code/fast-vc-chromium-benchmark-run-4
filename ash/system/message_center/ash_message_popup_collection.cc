7faa9bd65030572ea876be9dc918c1b542b15667
Andre Le <leandre@chromium.org>
2023-10-19 00:25:29 +0000

    NotifierCollision: Avoid handling bubble and popup changes at same time
    When a bubble show or changes bounds at the same time when popup
    collection height changes (i.e. due to notification update), the bubble
    can be closed in the middle of handling bubble changes, which cause a
    crash. The long term solution for this problem is being tracked in
    b/302172146.
    
    Bug: b:305781721, b:304526370
    Change-Id: I7e3bcc8900a575a47d7ed7a5fd44b5976544c81c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4951798
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211874}

--- ash/system/message_center/ash_message_popup_collection.cc
+++ ash/system/message_center/ash_message_popup_collection.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "ash/wm/window_properties.h"
 #include "ash/wm/work_area_insets.h"
+#include "base/auto_reset.h"
 #include "base/check.h"
 #include "base/i18n/rtl.h"
 #include "base/metrics/histogram_functions.h"
@@ -83,6 +84,13 @@ AshMessagePopupCollection::NotifierCollisionHandler::
 void AshMessagePopupCollection::NotifierCollisionHandler::
     OnPopupCollectionHeightChanged() {
   if (!features::IsNotifierCollisionEnabled()) {
+    return;
+  }
+
+  // Ignore changes happen to the popup collection height when bubble changes is
+  // being handled. This is to avoid crashes (b/305781721) when we handle both
+  // the bubble and the collection height changes at the same time.
+  if (is_handling_bubble_change_) {
     return;
   }
 
@@ -169,6 +177,10 @@ void AshMessagePopupCollection::NotifierCollisionHandler::
   if (!features::IsNotifierCollisionEnabled()) {
     return;
   }
+
+  // This is to make sure that we don't close the bubble through
+  // `OnPopupCollectionHeightChanged()` to avoid crashes (b/305781721).
+  base::AutoReset<bool> reset(&is_handling_bubble_change_, true);
 
   int previous_baseline_offset = baseline_offset_;
 
