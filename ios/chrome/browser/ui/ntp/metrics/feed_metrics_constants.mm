8eb0943d0b93b70ab53866e75c51eab6d933ec9f
Federica Germinario <fedegermi@google.com>
2023-10-19 13:29:00 +0000

    [ios] Move LastInteractionTimeForGoodVisitsFollowing in PrefService
    This CL moves LastInteractionTimeForGoodVisitsFollowing out of
    NSUserDefaults.
    
    The key has been moved in the PrefService. Note that there are two
    types of PrefServices, in this case the code was moved in the
    PrefService coming from BrowserState which is per account and is
    syncable between devices.
    Note that this kind of PrefService behaves differently from
    NSUserDefaults which is per device (and not per account).
    
    Bug: 1486770
    Change-Id: I69e53379f6d21219abfb32c0d7d9427ef25a5e19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4931312
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1212116}

--- ios/chrome/browser/ui/ntp/metrics/feed_metrics_constants.mm
+++ ios/chrome/browser/ui/ntp/metrics/feed_metrics_constants.mm
@@ -23,8 +23,8 @@ NSString* const kLastInteractionTimeForGoodVisits =
     @"LastInteractionTimeForGoodVisits";
 NSString* const kLastInteractionTimeForDiscoverGoodVisits =
     @"LastInteractionTimeForGoodVisitsDiscover";
-NSString* const kLastInteractionTimeForFollowingGoodVisits =
-    @"LastInteractionTimeForGoodVisitsFollowing";
+const char kLastInteractionTimeForFollowingGoodVisits[] =
+    "LastInteractionTimeForGoodVisitsFollowing";
 const char kLastDayTimeInFeedReportedKey[] = "LastDayTimeInFeedReported";
 const char kTimeSpentInFeedAggregateKey[] = "TimeSpentInFeedAggregate";
 const char kActivityBucketLastReportedDateKey[] =
