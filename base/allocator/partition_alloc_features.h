3f056ca5162e18b119414e617f8c680627e7a031
Mingyu Lei <leimy@chromium.org>
2023-10-19 05:31:04 +0000

    MiracleParameter: make thread cache min purging bytes miracle parameter
    Bug: 1475915
    Change-Id: I5550d0934bc42471a49f3b2ad287600a1fb8db74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4930450
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211970}

--- base/allocator/partition_alloc_features.h
+++ base/allocator/partition_alloc_features.h
@@ -214,8 +214,7 @@ GetThreadCacheDefaultPurgeInterval();
 
 BASE_EXPORT BASE_DECLARE_FEATURE(
     kEnableConfigurableThreadCacheMinCachedMemoryForPurging);
-extern const BASE_EXPORT base::FeatureParam<int>
-    kThreadCacheMinCachedMemoryForPurgingBytes;
+BASE_EXPORT int GetThreadCacheMinCachedMemoryForPurgingBytes();
 
 }  // namespace features
 }  // namespace base
