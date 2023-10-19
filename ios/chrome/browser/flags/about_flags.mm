f856519b7711fab7e2ee881741f46d7b2ac91510
Rubin Deliallisi <rubindl@chromium.org>
2023-10-19 12:00:41 +0000

    [iOS]: Add flag for the Privacy Guide
    Bug: 1488447
    Change-Id: Id2ba6b3fe7f6c3e13d3aa7bfee2ad9fc4d814e90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4946415
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212075}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -99,6 +99,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/omnibox/omnibox_ui_features.h"
 #import "ios/chrome/browser/ui/popup_menu/overflow_menu/feature_flags.h"
 #import "ios/chrome/browser/ui/settings/password/password_manager_ui_features.h"
+#import "ios/chrome/browser/ui/settings/privacy/privacy_guide/features.h"
 #import "ios/chrome/browser/ui/start_surface/start_surface_features.h"
 #import "ios/chrome/browser/ui/whats_new/whats_new_util.h"
 #import "ios/chrome/browser/web/features.h"
@@ -1579,6 +1580,9 @@ const flags_ui::FeatureEntry kFeatureEntries[] = {
      flags_ui::kOsIos,
      FEATURE_VALUE_TYPE(
          password_manager::features::kSkipUndecryptablePasswords)},
+    {"privacy-guide-ios", flag_descriptions::kPrivacyGuideIosName,
+     flag_descriptions::kPrivacyGuideIosDescription, flags_ui::kOsIos,
+     FEATURE_VALUE_TYPE(kPrivacyGuideIos)},
 };
 
 bool SkipConditionalFeatureEntry(const flags_ui::FeatureEntry& entry) {
---
+++ ios/chrome/browser/ui/settings/privacy/privacy_guide/features.mm
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/settings/privacy/privacy_guide/features.h"
+
+BASE_FEATURE(kPrivacyGuideIos,
+             "PrivacyGuideIos",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+bool IsPrivacyGuideIosEnabled() {
+  return base::FeatureList::IsEnabled(kPrivacyGuideIos);
+}
