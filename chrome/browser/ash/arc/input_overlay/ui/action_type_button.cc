ab923901463664f20b3c6d60960959ffe52de039
Joseph Pang <pjlee@google.com>
2023-10-19 17:44:00 +0000

    arc/gio: Update action type button theming
    This Cl updates action type button theming for game controls beta,
    including adding a border around the unselected action type(s) and a
    background for the selected action type.
    
    Bug: b/304850954
    Test: Manual tests passed
    Change-Id: I4a4b2e4e7ecdd03ce14d61a84b4b91d94e47d3ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4953568
    Reviewed-by: Cici Ruan <cuicuiruan@google.com>
    Commit-Queue: Joseph Pang <pjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1212260}

--- chrome/browser/ash/arc/input_overlay/ui/action_type_button.cc
+++ chrome/browser/ash/arc/input_overlay/ui/action_type_button.cc
@@ -23,6 +23,8 @@ constexpr int kButtonWidth = 110;
 constexpr int kActionTypeButtonHeight = 94;
 constexpr int kActionTypeIconSize = 48;
 constexpr int kLabelIconSpacing = 8;
+constexpr int kTopSpacing = 10;
+constexpr int kBorderThickness = 1;
 
 // Gap between focus ring outer edge to label.
 constexpr float kHaloInset = -5;
@@ -43,6 +45,10 @@ ActionTypeButton::ActionTypeButton(PressedCallback callback,
   SetVisible(true);
   SetBackground(views::CreateRoundedRectBackground(SK_ColorTRANSPARENT,
                                                    /*radius=*/kCornerRadius));
+  SetBorder(views::CreateThemedRoundedRectBorder(
+      /*thickness=*/kBorderThickness,
+      /*radius=*/kCornerRadius, cros_tokens::kCrosSysHoverOnSubtle));
+
   // Set highlight path.
   views::HighlightPathGenerator::Install(
       this, std::make_unique<views::RoundRectHighlightPathGenerator>(
@@ -73,10 +79,10 @@ void ActionTypeButton::Layout() {
 
   gfx::Point image_origin = local_content_bounds.origin();
   image_origin.Offset((local_content_bounds.width() - kActionTypeIconSize) / 2,
-                      0);
+                      kTopSpacing);
   gfx::Point label_origin = local_content_bounds.origin();
   label_origin.Offset((local_content_bounds.width() - label_size.width()) / 2,
-                      kActionTypeIconSize + kLabelIconSpacing);
+                      kTopSpacing + kActionTypeIconSize + kLabelIconSpacing);
 
   image()->SetBoundsRect(gfx::Rect(
       image_origin, gfx::Size(kActionTypeIconSize, kActionTypeIconSize)));
@@ -104,17 +110,30 @@ gfx::Size ActionTypeButton::CalculatePreferredSize() const {
 void ActionTypeButton::OnThemeChanged() {
   views::Button::OnThemeChanged();
   UpdateImage();
-  RefreshTextColor();
+  RefreshColors();
 }
 
-void ActionTypeButton::RefreshTextColor() {
-  auto active_color_id = selected() ? cros_tokens::kCrosSysPrimary
-                                    : cros_tokens::kCrosSysOnSurface;
-  auto disabled_color_id = selected()
+void ActionTypeButton::RefreshColors() {
+  const bool is_selected = selected();
+  auto active_color_id = is_selected ? cros_tokens::kCrosSysPrimary
+                                     : cros_tokens::kCrosSysOnSurface;
+  auto disabled_color_id = is_selected
                                ? ash::kColorAshIconPrimaryDisabledColor
                                : ash::kColorAshIconSecondaryDisabledColor;
   SetEnabledTextColorIds(active_color_id);
   SetTextColorId(ButtonState::STATE_DISABLED, disabled_color_id);
+  SetBackground(is_selected ? views::CreateThemedRoundedRectBackground(
+                                  cros_tokens::kCrosSysHighlightShape,
+                                  /*radius=*/kCornerRadius)
+                            : views::CreateRoundedRectBackground(
+                                  SK_ColorTRANSPARENT,
+                                  /*radius=*/kCornerRadius));
+  SetBorder(is_selected
+                ? views::CreateEmptyBorder(/*thickness=*/kBorderThickness)
+                : views::CreateThemedRoundedRectBorder(
+                      /*thickness=*/kBorderThickness,
+                      /*radius=*/kCornerRadius,
+                      cros_tokens::kCrosSysHoverOnSubtle));
 }
 
 }  // namespace arc::input_overlay
