e7ce1b85c209c37233929de7c19fb5eb9f840e30
Stepan Khapugin <stkhapugin@chromium.org>
2023-10-19 12:05:35 +0000

    [iOS][Spotlight] Move spotlight access to the background thread.
    Since Spotlight API is thread-safe and we're using SpotlightInterface
    for interfacing with the system APIs, just dispatching the
    doWithRetry to the background thread is enough to move spotlight access
    to the background.
    
    Bug: none
    Change-Id: I2bd1ece71c1a1a80ca4bb8c4d722a492ce203304
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4952184
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1212079}

--- ios/chrome/app/spotlight/spotlight_interface.mm
+++ ios/chrome/app/spotlight/spotlight_interface.mm
@@ -33,17 +33,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     completionHandler:(BlockWithError)completionHandler {
   DCHECK(completionHandler);
 
-  blockToRetry(^(NSError* error) {
-    if (error && retryCount > 0) {
-      [SpotlightInterface doWithRetry:blockToRetry
-                           retryCount:retryCount - 1
-                    completionHandler:completionHandler];
-    } else {
-      dispatch_async(dispatch_get_main_queue(), ^{
-        completionHandler(error);
-      });
-    }
-  });
+  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
+                 ^{
+                   blockToRetry(^(NSError* error) {
+                     if (error && retryCount > 0) {
+                       [SpotlightInterface doWithRetry:blockToRetry
+                                            retryCount:retryCount - 1
+                                     completionHandler:completionHandler];
+                     } else {
+                       dispatch_async(dispatch_get_main_queue(), ^{
+                         completionHandler(error);
+                       });
+                     }
+                   });
+                 });
 }
 
 - (instancetype)initWithSearchableIndex:(CSSearchableIndex*)searchableIndex
