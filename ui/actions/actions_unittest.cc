1712b49739871dca1b13bcb5d67faac83bd7591b
Allen Bauer <kylixrd@chromium.org>
2023-10-19 15:48:35 +0000

    Add ActionContext class for action invocation.
    Bug: 1472023
    Change-Id: Ifd74806a43a0c65f428d54889339815704302eda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4952763
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Joseph Park <josephjoopark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212188}

--- ui/actions/actions_unittest.cc
+++ ui/actions/actions_unittest.cc
@@ -16,6 +16,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace actions {
 
+enum class ContextValues {
+  kContextNone,
+  kContextKeyboard,
+  kContextMouse,
+  kContextTouch,
+};
+
 namespace {
 
 const std::u16string kActionText = u"Test Action";
@@ -43,6 +50,10 @@ enum TestActionIds : ActionId {
 // clang-format on
 
 #include "ui/actions/action_id_macros.inc"
+
+DEFINE_UI_CLASS_PROPERTY_KEY(ContextValues,
+                             kContextValueKey,
+                             ContextValues::kContextNone)
 
 class ActionManagerTest : public testing::Test {
  public:
@@ -85,6 +96,13 @@ using ActionItemTest = ActionManagerTest;
 using ActionIdMapTest = ActionManagerTest;
 
 }  // namespace
+}  // namespace actions
+
+DECLARE_EXPORTED_UI_CLASS_PROPERTY_TYPE(, actions::ContextValues)
+
+DEFINE_UI_CLASS_PROPERTY_TYPE(actions::ContextValues)
+
+namespace actions {
 
 // Verifies that the test harness functions correctly.
 TEST_F(ActionManagerTest, Harness) {
@@ -114,7 +132,8 @@ TEST_F(ActionManagerTest, ActionRegisterAndInvoke) {
       [](int* invoked_count, const std::u16string& text,
          ActionManager* manager) {
         auto action = std::make_unique<ActionItem>(base::BindRepeating(
-            [](int* invoked_count, actions::ActionItem* action) {
+            [](int* invoked_count, actions::ActionItem* action,
+               ActionInvocationContext context) {
               ++*invoked_count;
               EXPECT_EQ(*invoked_count, action->GetInvokeCount());
               EXPECT_GE(base::TimeTicks::Now(), *action->GetLastInvokeTime());
@@ -282,7 +301,8 @@ TEST_F(ActionItemTest, ActionBuilderChildrenTest) {
   int action_invoked_count = 0;
   // clang-format off
   auto builder = ActionItem::Builder(
-      base::BindRepeating([](int* invoked_count, actions::ActionItem* action){
+      base::BindRepeating([](int* invoked_count, actions::ActionItem* action,
+          ActionInvocationContext context){
         ++*invoked_count;
       }, &action_invoked_count))
       .CopyAddressTo(&root_action)
@@ -493,6 +513,33 @@ TEST_F(ActionItemTest, TestActionWeakPtr) {
   manager.ResetActions();
   EXPECT_EQ(action_test2.get(), nullptr);
   EXPECT_EQ(action_test3.get(), nullptr);
+}
+
+TEST_F(ActionItemTest, TextActionInvocationContext) {
+  int action_invoked_count = 0;
+  base::WeakPtr<ActionItem> action_test1;
+  // clang-format off
+  auto builder = ActionItem::Builder(
+      base::BindRepeating([](int* invoked_count, actions::ActionItem* action,
+          ActionInvocationContext context){
+        ++*invoked_count;
+        ContextValues context_value = context.GetProperty(kContextValueKey);
+        EXPECT_EQ(context_value, ContextValues::kContextKeyboard);
+      },  &action_invoked_count))
+      .CopyWeakPtrTo(&action_test1)
+      .SetText(kActionText)
+      .SetActionId(kActionTest1)
+      .SetVisible(true)
+      .SetEnabled(true);
+  // clang-format on
+  auto& manager = ActionManager::GetForTesting();
+  manager.AddAction(std::move(builder).Build());
+  ASSERT_TRUE(action_test1);
+  action_test1->InvokeAction(
+      ActionInvocationContext::Builder()
+          .SetProperty(kContextValueKey, ContextValues::kContextKeyboard)
+          .Build());
+  EXPECT_EQ(action_invoked_count, 1);
 }
 
 }  // namespace actions
