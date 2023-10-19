4680b026a9689d2783bfeeb4fee5928b944c9678
Egor Pasko <pasko@chromium.org>
2023-10-19 10:29:46 +0000

    Revert "URL: Measure escaped ASCII chars in URL path"
    This reverts commit d919c8c6ca59705b67f852c2efc7d44611b4fb00.
    
    Reason for revert: crbug.com/1252531 is fixed. Recording the
    histogram takes time on a common path: GURL::GURL().
    
    Original change's description:
    > URL: Measure escaped ASCII chars in URL path
    >
    > Chrome currently unescapes percent-encoded ASCII characters in URL
    > path. However, this behavior is non-compliant with the URL Standard.
    >
    > To address this interoperability issue, we need to assess the impact
    > of fixing it.
    >
    > I worried about measuring being a hotspot since this can happen when
    > constructing a GURL or KURL. However, we added a similar UMA for URL
    > host part [1] in the past. So most likely this will be OK.
    >
    > [1] https://crrev.com/c/3152695
    >
    >
    > Bug: 1252531
    > Change-Id: Ifa1f533fae43c10c81c3bf945844156cc4ca1c34
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4713927
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    > Commit-Queue: Hayato Ito <hayato@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1176456}
    
    OBSOLETE_HISTOGRAM[URL.Path.UnescapeEscapedChar]=Not needed after crbug.com/1252531 is fixed
    
    Bug: 1252531
    Change-Id: Ida3869068772ea9dbc09030002a85fee21a40401
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4952117
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212047}

--- url/url_canon_path.cc
+++ url/url_canon_path.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/check_op.h"
-#include "base/feature_list.h"
-#include "base/metrics/histogram_functions.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "url/url_canon.h"
 #include "url/url_canon_internal.h"
@@ -271,7 +269,6 @@ bool DoPartialPathInternal(const CHAR* spec,
   absl::optional<size_t> last_invalid_percent_index;
 
   bool success = true;
-  bool unescape_escaped_char = false;
   for (size_t i = static_cast<size_t>(path.begin); i < end; i++) {
     UCHAR uch = static_cast<UCHAR>(spec[i]);
     if (sizeof(CHAR) > 1 && uch >= 0x80) {
@@ -340,8 +337,6 @@ bool DoPartialPathInternal(const CHAR* spec,
             if (!url::IsUsingDontDecodeAsciiPercentEncodedURLPath() &&
                 (unescaped_flags & UNESCAPE)) {
               // This escaped value shouldn't be escaped.  Try to copy it.
-              unescape_escaped_char = true;
-
               output->push_back(unescaped_value);
               // If we just unescaped a value within 2 output characters of the
               // '%' from a previously-detected invalid escape sequence, we
@@ -383,8 +378,6 @@ bool DoPartialPathInternal(const CHAR* spec,
       }
     }
   }
-  base::UmaHistogramBoolean("URL.Path.UnescapeEscapedChar",
-                            unescape_escaped_char);
   return success;
 }
 
