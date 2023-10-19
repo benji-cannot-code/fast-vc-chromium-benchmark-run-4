64a335a1f5930c78f1fe747c4a2be03171e8228e
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2023-10-19 15:19:45 +0000

    Move TabRevisitTracker out of the metrics directory
    This component does some bookkeeping that will be used for non-metrics
    code, so it doesn't belong in the metrics/ directory anymore.
    
    Bug: 1469337
    Change-Id: Ic7966e227ba0cb2fc589e35b938f55aaaf5c4a32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4936481
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212163}

--- chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
+++ chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/performance_manager/public/decorators/process_metrics_decorator.h"
 #include "components/performance_manager/public/features.h"
 #include "components/performance_manager/public/graph/graph.h"
-#include "components/performance_manager/public/metrics/tab_revisit_tracker.h"
+#include "components/performance_manager/public/user_tuning/tab_revisit_tracker.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/storage_partition.h"
--- components/performance_manager/user_tuning/tab_revisit_tracker.cc
+++ components/performance_manager/user_tuning/tab_revisit_tracker.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/performance_manager/public/metrics/tab_revisit_tracker.h"
+#include "components/performance_manager/public/user_tuning/tab_revisit_tracker.h"
 
 #include <algorithm>
 
