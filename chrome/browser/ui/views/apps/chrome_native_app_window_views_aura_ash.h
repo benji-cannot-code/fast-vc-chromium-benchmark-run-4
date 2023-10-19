501caa959d681818ca6509a2f18c891d95661abd
Zoraiz Naeem <zoraiznaeem@chromium.org>
2023-10-19 19:27:41 +0000

    [Rounded Windows] Round chrome apps that use ash style windows
    This CL rounds chrome apps. The top corners are rounded by painting
    a rounded header whereas the bottom two corners are rounded by
    applying rounded corners to the web content layer.
    
    Bug: b:301336446
    Change-Id: I0a8b49dc691f380302a924490ec2273286013d77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4906756
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212315}

--- chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash.h
+++ chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_APPS_CHROME_NATIVE_APP_WINDOW_VIEWS_AURA_ASH_H_
 
 #include <memory>
-#include <vector>
 
 #include "ash/public/cpp/tablet_mode_observer.h"
 #include "ash/wm/window_state.h"
@@ -145,6 +144,9 @@ class ChromeNativeAppWindowViewsAuraAsh
                                const void* key,
                                intptr_t old) override;
   void OnWindowDestroying(aura::Window* window) override;
+
+  // views::View
+  void AddedToWidget() override;
 
  private:
   FRIEND_TEST_ALL_PREFIXES(ChromeNativeAppWindowViewsAuraAshBrowserTest,
