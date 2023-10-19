e1523a4f86b9c823fe80e5779762cfe68af49925
Christian Flach <cmfcmf@chromium.org>
2023-10-19 15:06:51 +0000

    iwa: Use jitter for update discovery timer
    Similar to Chrome Extension updates, we add 20% jitter to the update
    discovery timer for IWA updates, so that it becomes less likely for
    severs to be overloaded if a lot of people start Chrome(OS) at the
    same time.
    
    To achieve this, we replace the `base::RepeatingTimer` with a
    `base::CancelableCallback` that we regularly schedule with 20% jitter.
    
    This also fixes a small bug where the install manager observation was
    only started when automatic updates were enabled - however, now that
    updates can also be triggered manually from chrome://web-app-internals
    and potentially other sources, we need to make sure that
    `OnWebAppUninstalled` is actually called.
    
    Fixed: b:305004160
    Bug: 1444407
    Change-Id: Iaddec9963b9e8f7e8a5200a131df1e7d50876a50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935851
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Christian Flach <cmfcmf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212157}

--- chrome/browser/web_applications/isolated_web_apps/isolated_web_app_update_manager.h
+++ chrome/browser/web_applications/isolated_web_apps/isolated_web_app_update_manager.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/callback_list.h"
+#include "base/cancelable_callback.h"
 #include "base/containers/circular_deque.h"
 #include "base/containers/flat_map.h"
 #include "base/files/file_error_or.h"
@@ -20,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "base/time/time.h"
-#include "base/timer/timer.h"
 #include "base/types/pass_key.h"
 #include "base/values.h"
 #include "chrome/browser/web_applications/isolated_web_apps/isolated_web_app_location.h"
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/isolated_web_apps/isolated_web_app_update_discovery_task.h"
 #include "chrome/browser/web_applications/web_app_install_manager_observer.h"
 #include "components/webapps/common/web_app_id.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 class GURL;
 class Profile;
@@ -112,10 +113,6 @@ class IsolatedWebAppUpdateManager : public WebAppInstallManagerObserver {
       const webapps::AppId& app_id,
       webapps::WebappUninstallSource uninstall_source) override;
 
-  const base::RepeatingTimer& GetUpdateDiscoveryTimerForTesting() const {
-    return update_discovery_timer_;
-  }
-
   // Used to queue update discovery tasks manually from the
   // chrome://web-app-internals page. Returns the number of tasks queued.
   size_t DiscoverUpdatesNow();
@@ -128,6 +125,10 @@ class IsolatedWebAppUpdateManager : public WebAppInstallManagerObserver {
       const IsolatedWebAppUrlInfo& url_info,
       base::OnceCallback<void(base::expected<base::Version, std::string>)>
           callback);
+
+  absl::optional<base::TimeTicks> GetNextUpdateDiscoveryTimeForTesting() const {
+    return next_update_discovery_check_.GetScheduledTime();
+  }
 
  private:
   // This queue manages update discovery and apply tasks. Tasks can be added to
@@ -216,8 +217,8 @@ class IsolatedWebAppUpdateManager : public WebAppInstallManagerObserver {
   base::flat_map<web_package::SignedWebBundleId, GURL>
   GetForceInstalledBundleIdToUpdateManifestUrlMap();
 
-  void MaybeStartUpdateDiscoveryTimer();
-  void MaybeStopUpdateDiscoveryTimer();
+  void MaybeScheduleUpdateDiscoveryCheck();
+  void MaybeResetScheduledUpdateDiscoveryCheck();
 
   void CreateUpdateApplyWaiter(
       const IsolatedWebAppUrlInfo& url_info,
@@ -261,8 +262,28 @@ class IsolatedWebAppUpdateManager : public WebAppInstallManagerObserver {
 
   bool has_started_ = false;
 
+  class NextUpdateDiscoveryCheck {
+   public:
+    NextUpdateDiscoveryCheck();
+    ~NextUpdateDiscoveryCheck();
+
+    void ScheduleWithJitter(const base::TimeDelta& base_delay,
+                            base::OnceClosure callback);
+
+    absl::optional<base::TimeTicks> GetScheduledTime() const;
+    bool IsScheduled() const;
+    void Reset();
+
+    base::Value AsDebugValue() const;
+
+   private:
+    absl::optional<std::pair<base::TimeTicks,
+                             std::unique_ptr<base::CancelableOnceClosure>>>
+        next_check_;
+  };
+
   base::TimeDelta update_discovery_frequency_;
-  base::RepeatingTimer update_discovery_timer_;
+  NextUpdateDiscoveryCheck next_update_discovery_check_;
 
   TaskQueue task_queue_;
 
