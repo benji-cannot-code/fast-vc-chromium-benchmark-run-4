d963a309bb39a50a0ce89c91e31e074688a667cd
dpapad <dpapad@chromium.org>
2023-10-19 20:58:09 +0000

    [Reland] Migrate security_interstitials to JS modules.
    Reland notes: Added missing dependency in
    //android_webview:generate_components_resources target.
    
    Security interstitials UIs are not proper WebUI pages and therefore they
    can't load any dependencies at runtime, as there is no backing
    WebUIDataSource to serve them.
    
    For this reason, these UIs resort to bundling all the code to a single
    JS and HTML file pair at buildtime. Currently this is achieved by using
    Grit which is problematic because:
     - It relies on Grit's obsolete flattenhtml=true mechanism
     - It doesn't work with JS modules (Grit simply concatenates files)
    
    Addressed here by leveraging the bundle_js() GN rule which is the
    canonical way in the codebase for generating JS bundles from
    modularized JS code.
    
    This removes one of the last few non-JS module code in the codebase, and
    paves the way for further migrating such files to TypeScript.
    
    Bug: 1347008
    Change-Id: I31a7739226520da9c53db7dd63c5f40e17f9d0c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4955633
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212388}

--- components/security_interstitials/core/browser/resources/interstitial_webview_quiet.js
+++ components/security_interstitials/core/browser/resources/interstitial_webview_quiet.js
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {HIDDEN_CLASS, preventDefaultOnPoundLinkClicks, SecurityInterstitialCommandId, sendCommand} from 'chrome://interstitials/common/resources/interstitial_common.js';
+
 /**
  * Restores the interstitial content to the initial state if the window size
  * switches to a small view.
