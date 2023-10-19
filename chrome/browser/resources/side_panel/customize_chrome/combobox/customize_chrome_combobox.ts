114570e5d72d15dc560f0b66b643b3392acd88bc
John Lee <johntlee@chromium.org>
2023-10-19 18:51:14 +0000

    Set aria-activedescendant on combobox
    When pressing up/down arrow keys to highlight items in the dropdown,
    the combobox should set aria-activedescendant to the highlighted item
    so that screen readers know which item to read out.
    
    Bug: b/301017714
    Change-Id: Iae0b408d03bd4ea81205ae23cb0103472a248e34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4950534
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1212294}

--- chrome/browser/resources/side_panel/customize_chrome/combobox/customize_chrome_combobox.ts
+++ chrome/browser/resources/side_panel/customize_chrome/combobox/customize_chrome_combobox.ts
@@ -18,6 +18,9 @@ const SELECTABLE_ITEMS_SELECTOR = '[role=option]';
 
 export type OptionElement = HTMLElement&{value?: string};
 
+/* Running count of total items. Incremented to provide unique IDs. */
+let itemCount = 0;
+
 export interface CustomizeChromeCombobox {
   $: {
     input: HTMLDivElement,
@@ -42,6 +45,7 @@ export class CustomizeChromeCombobox extends PolymerElement {
         reflectToAttribute: true,
         observer: 'onExpandedChange_',
       },
+      highlightedElement_: Object,
       label: String,
       selectedElement_: {
         type: Object,
@@ -83,6 +87,10 @@ export class CustomizeChromeCombobox extends PolymerElement {
     this.domObserver_ = null;
   }
 
+  private getAriaActiveDescendant_(): string|undefined {
+    return this.highlightedElement_?.id;
+  }
+
   private getInputLabel_(): string {
     if (this.selectedElement_) {
       return this.selectedElement_.textContent!;
@@ -106,6 +114,12 @@ export class CustomizeChromeCombobox extends PolymerElement {
   private onDomChange_() {
     this.highlightableElements_ = Array.from(
         this.querySelectorAll<HTMLElement>(HIGHLIGHTABLE_ITEMS_SELECTOR));
+
+    this.highlightableElements_.forEach(element => {
+      if (!element.id) {
+        element.id = `comboboxItem${itemCount++}`;
+      }
+    });
   }
 
   private onDropdownClick_(event: MouseEvent) {
