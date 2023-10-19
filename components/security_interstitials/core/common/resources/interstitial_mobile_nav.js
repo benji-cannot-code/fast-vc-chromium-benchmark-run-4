f99284a35e89a679e336cb1666ff86b8d2300cee
Lingqi Chi <lingqi@chromium.org>
2023-10-19 02:58:24 +0000

    Revert "Migrate security_interstitials to JS modules."
    This reverts commit 25ea6fc4df03680df9b79458807942068bcaa457.
    
    Reason for revert: 
    https://ci.chromium.org/ui/p/chromium/builders/try/android-12-x64-rel
    
    Original change's description:
    > Migrate security_interstitials to JS modules.
    >
    > Security interstitials UIs are not proper WebUI pages and therefore they
    > can't load any dependencies at runtime, as there is no backing
    > WebUIDataSource to serve them.
    >
    > For this reason, these UIs resort to bundling all the code to a single
    > JS and HTML file pair at buildtime. Currently this is achieved by using
    > Grit which is problematic because:
    >  - It relies on Grit's obsolete flattenhtml=true mechanism
    >  - It doesn't work with JS modules (Grit simply concatenates files)
    >
    > Addressed here by leveraging the bundle_js() GN rule which is the
    > canonical way in the codebase for generating JS bundles from
    > modularized JS code.
    >
    > This removes one of the last few non-JS module code in the codebase, and
    > paves the way for further migrating such files to TypeScript.
    >
    > Bug: 1347008
    > Change-Id: I130f4398746d292307982419fb865314c58135b5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3891987
    > Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    > Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    > Reviewed-by: Carlos IL <carlosil@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1211905}
    
    Bug: 1347008
    Change-Id: I2b30c1b2e029d40a52cf245033dfcf7a734d4eab
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4950801
    Auto-Submit: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211930}

--- components/security_interstitials/core/common/resources/interstitial_mobile_nav.js
+++ components/security_interstitials/core/common/resources/interstitial_mobile_nav.js
@@ -3,9 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {HIDDEN_CLASS} from './interstitial_common.js';
-
-export let mobileNav = false;
+let mobileNav = false;
 
 /**
  * For small screen mobile the navigation buttons are moved
