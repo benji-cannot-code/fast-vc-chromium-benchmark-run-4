5724ffec7717d206a030730da1ccd7cc5f3e5cd8
Elly <ellyjones@chromium.org>
2023-10-19 23:27:38 +0000

    content: pass initiator origin through for context menu
    This change:
    1. Modifies RenderFrameHostImpl to pass the initiating frame's origin
       through when invoking the renderer context menu
    2. Modifies some of the renderer context menu classes to plumb that
       origin through when loading a URL
    3. Adds an end-to-end test to the ContextMenuBrowserTest suite to
       validate this new behavior
    
    Fixed: 1457702
    Change-Id: If134600bd2aaf836bc297a110a2368e9096a8402
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4763411
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212452}

--- components/renderer_context_menu/render_view_context_menu_base.h
+++ components/renderer_context_menu/render_view_context_menu_base.h
@@ -172,12 +172,14 @@ class RenderViewContextMenuBase : public ui::SimpleMenuModel::Delegate,
   // Opens the specified URL.
   void OpenURL(const GURL& url,
                const GURL& referrer,
+               const url::Origin& initiator,
                WindowOpenDisposition disposition,
                ui::PageTransition transition);
 
   // Opens the specified URL with extra headers.
   virtual void OpenURLWithExtraHeaders(const GURL& url,
                                        const GURL& referrer,
+                                       const url::Origin& initiator,
                                        WindowOpenDisposition disposition,
                                        ui::PageTransition transition,
                                        const std::string& extra_headers,
@@ -188,6 +190,7 @@ class RenderViewContextMenuBase : public ui::SimpleMenuModel::Delegate,
   content::OpenURLParams GetOpenURLParamsWithExtraHeaders(
       const GURL& url,
       const GURL& referring_url,
+      const url::Origin& initiator,
       WindowOpenDisposition disposition,
       ui::PageTransition transition,
       const std::string& extra_headers,
