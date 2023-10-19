d9a4dba295f0ce09736ebeb8d89656a72c2f7884
Hiroki Nakagawa <nhiroki@chromium.org>
2023-10-19 15:20:34 +0000

    Prerender: Merge functions on PrerenderManager() for cleanup
    For code cleanup, this CL merges StartPrerenderSearchResultInternal()
    into StartPrerenderSearchResult() in PrerenderManager.
    
    This doesn't change functional behavior.
    
    Bug: 1485775
    Change-Id: I8465f1c02031b7a61a5588799eb2795a1ad1d89c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4951343
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212164}

--- chrome/browser/preloading/prerender/prerender_manager.h
+++ chrome/browser/preloading/prerender/prerender_manager.h
@@ -137,11 +137,6 @@ class PrerenderManager : public content::WebContentsObserver,
       const GURL& canonical_search_url,
       base::WeakPtr<content::PreloadingAttempt> attempt);
 
-  void StartPrerenderSearchResultInternal(
-      const GURL& canonical_search_url,
-      const GURL& prerendering_url,
-      base::WeakPtr<content::PreloadingAttempt> attempt);
-
   // Stops search prefetch from being upgraded to prerender.
   void UnregisterSearchPrerender();
 
