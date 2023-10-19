fa5681b772a64b617efd935f73a671f8aea1020a
Devlin Cronin <rdevlin.cronin@chromium.org>
2023-10-19 01:25:11 +0000

    [Extensions] Adjust the minimum alarm time to 30s for MV3 extensions
    Packed extensions have a limit of 60s for scheduling alarms. Relax this
    to 30s for MV3 extensions, since this matches the service worker idle
    timeout. Otherwise, it's not possible for an extension to effectively
    schedule an alarm for, say, 45 seconds from a given point.
    
    Bug: 1479570
    Change-Id: I158b5ebcd2160c7a2dc55e3ca0cd924339159fc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4950751
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211894}

--- extensions/browser/api/alarms/alarms_api_constants.cc
+++ extensions/browser/api/alarms/alarms_api_constants.cc
@@ -8,24 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extensions {
 namespace alarms_api_constants {
 
-const char kWarningMinimumDevDelay[] =
-    "Alarm delay is less than minimum of 1 minutes. In released .crx, alarm "
-    "\"*\" will fire in approximately 1 minutes.";
-
-const char kWarningMinimumReleaseDelay[] =
-    "Alarm delay is less than minimum of 1 minutes. Alarm \"*\" will fire in "
-    "approximately 1 minutes.";
-
-const char kWarningMinimumDevPeriod[] =
-    "Alarm period is less than minimum of 1 minutes. In released .crx, alarm "
-    "\"*\" will fire approximately every 1 minutes.";
-
-const char kWarningMinimumReleasePeriod[] =
-    "Alarm period is less than minimum of 1 minutes. Alarm \"*\" will fire "
-    "approximately every 1 minutes.";
-
-static_assert(kReleaseDelayMinimum == base::Minutes(1),
-              "warning message must be updated");
+base::TimeDelta GetMinimumDelay(bool is_unpacked, int manifest_version) {
+  if (is_unpacked) {
+    return alarms_api_constants::kDevDelayMinimum;
+  }
+  if (manifest_version >= 3) {
+    return alarms_api_constants::kMV3ReleaseDelayMinimum;
+  }
+  return alarms_api_constants::kMV2ReleaseDelayMinimum;
+}
 
 }  // namespace alarms_api_constants
 }  // namespace extensions
