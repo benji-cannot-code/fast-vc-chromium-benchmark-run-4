19c7daa8f3a0ed0c53dcd5d5a1ac502411837147
Ho Cheung <uioptt24@gmail.com>
2023-10-19 14:05:38 +0000

    [base] Rename DIR_SOURCE_ROOT to DIR_SRC_TEST_DATA_ROOT
    Bug: 1264897
    Change-Id: I8946189f4cf2ad19dfd6cc73df97cbd645e2d417
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4952181
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Ho Cheung <uioptt24@gmail.com>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212132}

--- courgette/base_test_unittest.cc
+++ courgette/base_test_unittest.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 
 void BaseTest::SetUp() {
-  ASSERT_TRUE(base::PathService::Get(base::DIR_SOURCE_ROOT, &test_dir_));
+  ASSERT_TRUE(base::PathService::Get(base::DIR_SRC_TEST_DATA_ROOT, &test_dir_));
   test_dir_ = test_dir_.AppendASCII("courgette");
   test_dir_ = test_dir_.AppendASCII("testdata");
 }
