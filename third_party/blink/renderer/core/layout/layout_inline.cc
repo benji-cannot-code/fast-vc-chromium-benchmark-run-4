584674cfae51996507b48e363a58b46c41807428
Morten Stenshorne <mstensho@chromium.org>
2023-10-19 12:16:13 +0000

    Clean up painting of fragmented relpos inline.
    Only process the current block container fragment when painting a
    LayoutInline that establishes a PaintLayer. We used to walk though all
    the container fragments searching for the fragment items that belonged
    to the current block container fragment. But that's not necessary if we
    just figure out which container fragment is the current one. We used to
    calculate the container fragment index using PaintController::
    CurrentFragment(), which was a pretty dubious approach, although it
    worked.
    
    With the test included, this change reduces the time spent in
    LocalFrameView::RunPaintLifecyclePhase() by about 50%. That doesn't make
    a lot of overall difference, though, because PrePaint takes more than 50
    *times* longer than actual painting, due to how NGInlineCursor works
    with fragmented content; NGInlineCursor::MoveTo(const LayoutObject&) is
    O(n), where n is the number of fragmentainers. Doing this for every
    fragmentainer means O(n^2).
    
    No behavior changes intended.
    
    Change-Id: I5d377cbd5e64b4f978f6e83c2075dcc0b4aea6ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4946473
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212084}

--- third_party/blink/renderer/core/layout/layout_inline.cc
+++ third_party/blink/renderer/core/layout/layout_inline.cc
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/core/paint/box_painter.h"
 #include "third_party/blink/renderer/core/paint/ng/ng_box_fragment_painter.h"
-#include "third_party/blink/renderer/core/paint/ng/ng_inline_box_fragment_painter.h"
 #include "third_party/blink/renderer/core/paint/object_paint_invalidator.h"
 #include "third_party/blink/renderer/core/paint/object_painter.h"
 #include "third_party/blink/renderer/core/paint/outline_painter.h"
@@ -407,8 +406,7 @@ LayoutBox* LayoutInline::CreateAnonymousBoxToSplit(
 
 void LayoutInline::Paint(const PaintInfo& paint_info) const {
   NOT_DESTROYED();
-  DCHECK(IsInLayoutNGInlineFormattingContext());
-  NGInlineBoxFragmentPainter::PaintAllFragments(*this, paint_info);
+  NOTREACHED();
 }
 
 template <typename PhysicalRectCollector>
