b702b434b4ce14032058198faad2186f53fa1ac8
rubberyuzu <yuzus@chromium.org>
2023-10-19 05:33:23 +0000

    [devtools] Send bfcache blocking details to DevTools
    This CL sends bfcache blocking details to the browser. The blocking details contain file name, function name and line/column number where the blockage happened.
    
    Note that blocking details only exist for blocklisted features that are tracked in the renderer process, and that we limit the number of details to report, so not all blocklisted features have associated details.
    
    Bug:1366675
    
    Change-Id: I38889fb9958ea03895fca90b094469e1ac4c47a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905011
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211971}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/bfcache/report-back-forward-cache-status-details-captured.js
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {page, session, dp} = await testRunner.startBlank(
+      `Test that back/forward navigations report the bfcache status`);
+
+  await dp.Page.enable();
+
+  // Navigate to Page A.
+  await page.navigate(
+    'https://devtools.test:8443/inspector-protocol/bfcache/resources/page-with-blocking-feature.html');
+
+  // Navigate to Page B.
+  await page.navigate('chrome://version');
+
+  const {result: history} = await dp.Page.getNavigationHistory();
+
+  // Enable for printing the history.
+  // testRunner.log(history);
+
+  // Do a 'back' navigation.
+  const previous = history.entries[history.currentIndex - 1];
+  await historyNavigate(previous.id);
+
+  // Do a 'forward' navigation.
+  const current = history.entries[history.currentIndex];
+  await historyNavigate(current.id);
+
+  testRunner.completeTest();
+
+  async function historyNavigate(entryId) {
+    dp.Page.navigateToHistoryEntry({entryId});
+    const {params: statusReport} = await dp.Page.onceBackForwardCacheNotUsed();
+    testRunner.log(statusReport);
+  }
+});
