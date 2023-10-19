3dbbcbc115d076de16283df8d2faa1dbe5a9890b
Hang Nguyen <hnt@google.com>
2023-10-19 02:02:15 +0000

    gpu/command_buffer/tests: Setup env var for Intel Media Compression
    As the feature flag to enable media compression is introduced
    (CL:4822820), there are strong assertions in place to ensure the
    environment variable is set before allowing minigbm to allocate
    compressed buffers.
    
    This CL sets the environment variable for Intel Media Compression in the
    test environment in GPU Command Buffer to pass the assertion check when
    creating a gbm device.
    
    Without the setup, the gl_tests_validating test suite under gl_tests
    will fail in ChromeOS VMs.
    
    Bug: 1492579
    Change-Id: I68633a45ba86e51ef13c6a951ddd40d83a434a50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4953627
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Hang Nguyen <hnt@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211909}

--- gpu/command_buffer/tests/gl_tests_main.cc
+++ gpu/command_buffer/tests/gl_tests_main.cc
@@ -20,11 +20,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/apple/scoped_nsautorelease_pool.h"
 #endif
 
+#if BUILDFLAG(IS_CHROMEOS)
+#include "ui/gfx/linux/gbm_util.h"  // nogncheck
+#endif
+
 namespace {
 
 class GlTestsSuite : public base::TestSuite {
  public:
-  GlTestsSuite(int argc, char** argv) : base::TestSuite(argc, argv) {}
+  GlTestsSuite(int argc, char** argv) : base::TestSuite(argc, argv) {
+#if BUILDFLAG(IS_CHROMEOS)
+    // TODO(b/271455200): the FeatureList has not been initialized by this
+    // point, so this call will always disable Intel media compression. We may
+    // want to move this to a later point to be able to run GL unit tests with
+    // Intel media compression enabled.
+    ui::EnsureIntelMediaCompressionEnvVarIsSet();
+#endif  // BUILDFLAG(IS_CHROMEOS)
+  }
 
  protected:
   void Initialize() override {
