85169f659ca489d34d1d27ef148fb27ca49cb761
Chad Duffin <chadduffin@chromium.org>
2023-10-19 20:26:23 +0000

    network: Add NetworkInfoBubble regression test
    This change updates the existing NetworkDetailedView unit tests to use
    the actual system tray instead of creating an instance of the
    NetworkDetailedView so that the tests are executed in an environment
    much more similar to production. These updates allow these tests to
    become a regression test for the associated bug.
    
    Bug: b/303382616, b/304840723
    Test: Unit tests.
    Change-Id: Iec3534767ba4c0d42a889c441b75e2cbe7c82235
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4932594
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212362}

--- ash/system/network/network_feature_pod_controller.cc
+++ ash/system/network/network_feature_pod_controller.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/ash_features.h"
 #include "ash/constants/quick_settings_catalogs.h"
+#include "ash/public/cpp/ash_view_ids.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
@@ -195,6 +196,7 @@ std::unique_ptr<FeatureTile> NetworkFeaturePodController::CreateTile(
       base::BindRepeating(&FeaturePodControllerBase::OnLabelPressed,
                           weak_ptr_factory_.GetWeakPtr()));
   tile_ = tile.get();
+  tile_->SetID(VIEW_ID_FEATURE_TILE_NETWORK);
   tile_->SetIconClickable(true);
   tile_->SetIconClickCallback(
       base::BindRepeating(&FeaturePodControllerBase::OnIconPressed,
