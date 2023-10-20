c0896e8b91a15e664bfd8f91db7303feaaec5b23
Osama Fathy <osamafathy@google.com>
2023-10-20 11:01:47 +0000

    OOBE: Introduce OobeCrosEvents feature flag
    Bug: b:305929689
    Change-Id: Iffea0ef57b9ef1b68d3f00a8a077727395669e1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4946457
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1212661}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1856,6 +1856,11 @@ BASE_FEATURE(kOnDeviceSpeechRecognition,
 // If enabled, CHOBOE Screen will be shown during the new user onboarding flow.
 BASE_FEATURE(kOobeChoobe, "OobeChoobe", base::FEATURE_ENABLED_BY_DEFAULT);
 
+// If enabled, CrOS events for OOBE and onboarding flow will be recorded.
+BASE_FEATURE(kOobeCrosEvents,
+             "OobeCrosEvents",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // If enabled, Drive Pinning Screen will be shown during
 // the new user onboarding flow.
 BASE_FEATURE(kOobeDrivePinning,
@@ -3787,6 +3792,10 @@ bool IsOobeSkipAssistantEnabled() {
 
 bool IsOobeChoobeEnabled() {
   return base::FeatureList::IsEnabled(kOobeChoobe);
+}
+
+bool IsOobeCrosEventsEnabled() {
+  return base::FeatureList::IsEnabled(kOobeCrosEvents);
 }
 
 bool IsOobeSoftwareUpdateEnabled() {
