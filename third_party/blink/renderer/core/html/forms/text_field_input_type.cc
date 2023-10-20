d1e72b82175a8b370e88ea4323b181aea254da19
Di Zhang <dizhangg@chromium.org>
2023-10-20 15:05:00 +0000

    Fix arrow key behavior with vertical number input types
    Arrow key navigation for number input type in vertical writing mode
    should follow physical direction that feels natural to the user.
    - Key down should move caret position to the next digit
    - Key up should move caret position to the prev digit
    - Key left should step down the value (decrease)
    - Key right should step up the value (increase)
    
    Further, the arrows in the InnerSpinButton should reflect the logic:
    - Up/Down for horizontal number input type
    - Left/Right for vertical number input type
    
    Change-Id: I87312ba7995096aba19e8480cf6dc1f75dc3e7dc
    Bug: 1490377
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4919635
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Di Zhang <dizhangg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212750}

--- third_party/blink/renderer/core/html/forms/text_field_input_type.cc
+++ third_party/blink/renderer/core/html/forms/text_field_input_type.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/dom/events/event_dispatch_forbidden_scope.h"
 #include "third_party/blink/renderer/core/dom/focus_params.h"
+#include "third_party/blink/renderer/core/dom/node_computed_style.h"
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/editing/frame_selection.h"
 #include "third_party/blink/renderer/core/events/before_text_inserted_event.h"
@@ -232,12 +233,21 @@ void TextFieldInputType::HandleKeydownEventForSpinButton(KeyboardEvent& event) {
   if (GetElement().IsDisabledOrReadOnly())
     return;
   const String& key = event.key();
-  if (key == "ArrowUp")
+  bool is_horizontal =
+      GetElement().GetComputedStyle()
+          ? GetElement().GetComputedStyle()->IsHorizontalWritingMode()
+          : true;
+
+  if ((is_horizontal && key == "ArrowUp") ||
+      (!is_horizontal && key == "ArrowRight")) {
     SpinButtonStepUp();
-  else if (key == "ArrowDown" && !event.altKey())
+  } else if (((is_horizontal && key == "ArrowDown") ||
+              (!is_horizontal && key == "ArrowLeft")) &&
+             !event.altKey()) {
     SpinButtonStepDown();
-  else
+  } else {
     return;
+  }
   GetElement().DispatchFormControlChangeEvent();
   event.SetDefaultHandled();
 }
