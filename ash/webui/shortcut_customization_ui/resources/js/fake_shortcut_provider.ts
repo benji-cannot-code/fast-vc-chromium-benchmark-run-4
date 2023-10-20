a69231552aa6c0e1ef68a4261bcae65db571aa2e
Jimmy <jimmyxgong@chromium.org>
2023-10-20 17:54:08 +0000

    shortcuts: Add metric to track side panel navigation
    Bug: b/216049298
    Test: ash_unittests, browser_tests
    Change-Id: Ifacadbaf8b579cfebfe0eb7e0836a2840695a8c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4937235
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1212851}

--- ash/webui/shortcut_customization_ui/resources/js/fake_shortcut_provider.ts
+++ ash/webui/shortcut_customization_ui/resources/js/fake_shortcut_provider.ts
@@ -9,7 +9,7 @@ import {assert} from 'chrome://resources/js/assert.js';
 
 import {AcceleratorResultData, AcceleratorsUpdatedObserverRemote, PolicyUpdatedObserverRemote, UserAction} from '../mojom-webui/ash/webui/shortcut_customization_ui/mojom/shortcut_customization.mojom-webui.js';
 
-import {Accelerator, AcceleratorConfigResult, AcceleratorSource, MojoAcceleratorConfig, MojoLayoutInfo, ShortcutProviderInterface} from './shortcut_types.js';
+import {Accelerator, AcceleratorCategory, AcceleratorConfigResult, AcceleratorSource, MojoAcceleratorConfig, MojoLayoutInfo, ShortcutProviderInterface} from './shortcut_types.js';
 
 
 /**
@@ -35,6 +35,7 @@ export class FakeShortcutProvider implements ShortcutProviderInterface {
   private addAcceleratorCallCount: number = 0;
   private removeAcceleratorCallCount: number = 0;
   private lastRecordedUserAction: UserAction;
+  private lastRecordedMainCategory: AcceleratorCategory;
 
   constructor() {
     this.methods = new FakeMethodResolver();
@@ -56,6 +57,7 @@ export class FakeShortcutProvider implements ShortcutProviderInterface {
     this.methods.register('getConflictAccelerator');
     this.methods.register('getDefaultAcceleratorsForId');
     this.methods.register('recordUserAction');
+    this.methods.register('recordMainCategoryNavigation');
     this.registerObservables();
   }
 
@@ -178,6 +180,14 @@ export class FakeShortcutProvider implements ShortcutProviderInterface {
 
   getLatestRecordedAction(): UserAction {
     return this.lastRecordedUserAction;
+  }
+
+  recordMainCategoryNavigation(category: AcceleratorCategory): void {
+    this.lastRecordedMainCategory = category;
+  }
+
+  getLatestMainCategoryNavigated(): AcceleratorCategory {
+    return this.lastRecordedMainCategory;
   }
 
   preventProcessingAccelerators(_preventProcessingAccelerators: boolean):
