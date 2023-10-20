cd3e4cf2b1d4722531d7d81dd746626251a22dd8
Munira Tursunova <moonira@google.com>
2023-10-20 09:20:10 +0000

    Add variations support for mac system-ui font
    Support font weight, style and stretch variations for Mac system-ui
    font. Migrate system-ui Mac font matching from AppKit API to CoreText.
    
    Bug: 1466857
    Validate-Test-Flakiness: skip
    Change-Id: I142d0bc0bd07e2896912ba3410b43166a544ffad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4916294
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Munira Tursunova <moonira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1212635}

--- third_party/blink/renderer/platform/fonts/mac/font_matcher_mac_test.mm
+++ third_party/blink/renderer/platform/fonts/mac/font_matcher_mac_test.mm
@@ -13,9 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 void TestSystemFontContainsString(FontSelectionValue desired_weight,
+                                  FontSelectionValue desired_slant,
+                                  FontSelectionValue desired_width,
                                   NSString* substring) {
   NSFont* font =
-      MatchNSFontFamily(font_family_names::kSystemUi, 0, desired_weight, 11);
+      MatchNSFontFamily(font_family_names::kSystemUi, 0, desired_weight,
+                        desired_slant, desired_width, 11);
   EXPECT_TRUE([font.description containsString:substring]);
 }
 
