8e1a3eff3c4159a35cd2044d50c89ca3798f0fe8
Brandon Wylie <wylieb@chromium.org>
2023-10-20 20:59:36 +0000

    [bookmarks] Build compact row for the COMPACT type
    Change-Id: I5fe9679aba60cff0db6bf1508ed86bbd5248071c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4959801
    Auto-Submit: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212961}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerCoordinator.java
@@ -244,7 +244,7 @@ public class BookmarkManagerCoordinator
                 mMediator.getDraggabilityProvider());
         dragReorderableRecyclerViewAdapter.registerDraggableType(
                 ViewType.IMPROVED_BOOKMARK_COMPACT,
-                this::buildVisualImprovedBookmarkRow,
+                this::buildCompactImprovedBookmarkRow,
                 ImprovedBookmarkRowViewBinder::bind,
                 (viewHolder, itemTouchHelper) -> {},
                 mMediator.getDraggabilityProvider());
