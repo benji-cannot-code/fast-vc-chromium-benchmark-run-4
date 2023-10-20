679ee618eaebc1455a067d4ca49ef64c60633a87
Geoff Huang <geoffhuang@google.com>
2023-10-20 05:36:30 +0000

    Use stable finch experiments for automotive beta
    Finch experiments on beta are more risky (e.g. 50% instead of 1% for
    stable).
    
    Fixed: b/304775958
    Change-Id: I11d1bcda4657487b798e47a84ef91d2a39863186
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4949476
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Geoff Huang <geoffhuang@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212585}

--- components/variations/service/variations_service_client.cc
+++ components/variations/service/variations_service_client.cc
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/variations/variations_switches.h"
 #include "ui/base/device_form_factor.h"
 
+#if BUILDFLAG(IS_ANDROID)
+#include "base/android/build_info.h"
+#endif
+
 namespace variations {
 
 version_info::Channel VariationsServiceClient::GetChannelForVariations() {
@@ -31,8 +35,16 @@ version_info::Channel VariationsServiceClient::GetChannelForVariations() {
     DVLOG(1) << "Invalid channel provided: " << forced_channel;
   }
 
+  auto channel = GetChannel();
+#if BUILDFLAG(IS_ANDROID)
+  // TODO(crbug.com/1493502): Remove this if block after automotive beta ends.
+  if (channel == version_info::Channel::BETA &&
+      base::android::BuildInfo::GetInstance()->is_automotive()) {
+    return version_info::Channel::STABLE;
+  }
+#endif
   // Return the embedder-provided channel if no forced channel is specified.
-  return GetChannel();
+  return channel;
 }
 
 Study::FormFactor VariationsServiceClient::GetCurrentFormFactor() {
