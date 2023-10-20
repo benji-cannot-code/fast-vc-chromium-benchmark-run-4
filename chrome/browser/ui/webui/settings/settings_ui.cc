e99f1c8e7b3d478a47b2cad48fc7c8aacf79ecb5
Jack Yammine <jyammine@google.com>
2023-10-20 08:34:54 +0000

    [waffle]Change how the search engine looks in settings when chosen
    Change the UI that shows the chosen default search engine in
    settings.
    
    The CL that will follow this one will make the `Change` button
    behave as intended.
    
    Mocks: https://www.figma.com/file/ii4NU1kRDDuWvStuoGfKcO/Compliance?node-id=5269%3A361689&mode=dev
    
    Screenshots: http://b/304233429#comment2
    Fixed: b:304233429
    Change-Id: I7a2a83f8f958b0a2e00cca5cd5cd720feb9b1d30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4942153
    Auto-Submit: Jack Yammine <jyammine@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212619}

--- chrome/browser/ui/webui/settings/settings_ui.cc
+++ chrome/browser/ui/webui/settings/settings_ui.cc
@@ -96,7 +96,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/privacy_sandbox/tracking_protection_settings.h"
 #include "components/safe_browsing/core/browser/hashprefix_realtime/hash_realtime_utils.h"
 #include "components/safe_browsing/core/common/features.h"
+#include "components/search_engines/search_engine_choice_utils.h"
 #include "components/signin/public/base/signin_pref_names.h"
+#include "components/signin/public/base/signin_switches.h"
 #include "components/sync/base/features.h"
 #include "content/public/browser/url_data_source.h"
 #include "content/public/browser/web_contents.h"
@@ -313,6 +315,13 @@ SettingsUI::SettingsUI(content::WebUI* web_ui)
     commerce::ShoppingServiceFactory::GetForBrowserContext(profile)
         ->FetchPriceEmailPref();
   }
+
+  const bool is_search_engine_choice_settings_ui =
+      base::FeatureList::IsEnabled(switches::kSearchEngineChoiceSettingsUi) &&
+      search_engines::IsChoiceScreenFlagEnabled(
+          search_engines::ChoicePromo::kAny);
+  html_source->AddBoolean("searchEngineChoiceSettingsUi",
+                          is_search_engine_choice_settings_ui);
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   html_source->AddBoolean(
