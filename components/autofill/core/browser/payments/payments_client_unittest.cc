27528e0efd285af3e84ceb9841773118ccf575ed
Vinny Persky <vinnypersky@google.com>
2023-10-20 21:32:18 +0000

    Use base::NumberToString() instead of std::to_string() in Payments Client unittests
    base::NumberToString() is preferred over std::to_string() in Chromium.
    This CL also resolves a local warning that appears when modifying the
    components/autofill/core/browser/payments/payments_client_unittest.cc
    file.
    
    Fixed: 1494290
    Change-Id: I87a4f7a09fd19c842b3ab28bbea384ed1293ab03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4957975
    Auto-Submit: Vinny Persky <vinnypersky@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Commit-Queue: Olivia Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1212977}

--- components/autofill/core/browser/payments/payments_client_unittest.cc
+++ components/autofill/core/browser/payments/payments_client_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/bind.h"
@@ -1135,7 +1136,7 @@ TEST_F(PaymentsClientTest, GetDetailsIncludesDetectedValuesInRequest) {
 
   // Verify that the detected values were included in the request.
   std::string detected_values_string =
-      "\"detected_values\":" + std::to_string(kAllDetectableValues);
+      "\"detected_values\":" + base::NumberToString(kAllDetectableValues);
   EXPECT_TRUE(GetUploadData().find(detected_values_string) !=
               std::string::npos);
 }
