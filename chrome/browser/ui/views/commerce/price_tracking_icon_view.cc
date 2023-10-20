2e8261b1ef2063d38f094efcfee35dbbd91395cf
Matt Jones <mdjones@chromium.org>
2023-10-20 21:31:38 +0000

    [Price tracking] Prevent reshowing FUE for accounts that have already decided
    The first run experience would always show for new devices or fresh
    installs of chrome regardless of whether the account had made a
    decision on receiving price drop emails. This patch checks to see if
    that preference was set befroe attempting to show the UI.
    
    Bug: 1494472
    Change-Id: I86604e42c6ce0a79ad4589ebf00734f1f7e09980
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4960713
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212976}

--- chrome/browser/ui/views/commerce/price_tracking_icon_view.cc
+++ chrome/browser/ui/views/commerce/price_tracking_icon_view.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/commerce/core/commerce_feature_list.h"
+#include "components/commerce/core/pref_names.h"
 #include "components/commerce/core/price_tracking_utils.h"
 #include "components/commerce/core/shopping_service.h"
 #include "components/feature_engagement/public/feature_constants.h"
@@ -316,6 +317,8 @@ bool PriceTrackingIconView::IsPriceTracking() const {
 bool PriceTrackingIconView::ShouldShowFirstUseExperienceBubble() const {
   return profile_->GetPrefs()->GetBoolean(
              prefs::kShouldShowPriceTrackFUEBubble) &&
+         !profile_->GetPrefs()->HasPrefPath(
+             commerce::kPriceEmailNotificationsEnabled) &&
          !IsPriceTracking();
 }
 
