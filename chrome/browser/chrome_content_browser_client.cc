48d762d0a8b85caebca4ca9dec1141422831d42b
Sonja <laurila@google.com>
2023-10-20 13:25:47 +0000

    Reland "[AWC] Use `PageUserData` to store the web-API-altered `can_resize`"
    This is a reland of commit 9fc9cb01021d1b6e28c8457ee7a27359ab68529a
    
    There was an issue in installing the PWA from
    `/chrome/test/data/web_apps/awc/`. I'm still not sure about the root
    cause as they failed even before running my actual tests. I now returned
    to the previous way of how to install it as that works.
    
    Example failure:
    https://ci.chromium.org/ui/p/chromium/builders/try/linux-bfcache-rel/745/overview
    
    Original change's description:
    > [AWC] Use `PageUserData` to store the web-API-altered `can_resize`
    >
    > This CL ties the web-API-altered can_resize value to the lifetime of
    > page by using `PageUserData`.
    >
    > I2P thread
    > https://groups.google.com/a/chromium.org/g/blink-dev/c/oCxWg8q_OQY/m/LUTw0T5cCQAJ
    >
    > Public explainer is available @
    > https://github.com/ivansandrk/additional-windowing-controls/blob/main/awc-explainer.md
    >
    > Private doc @ go/additional-windowing-controls
    >
    > Bug: 1466855, b:288265319
    > Change-Id: Iec3300c646653c752a54e2e0792b0a99fb6d928d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4906282
    > Commit-Queue: Sonja Laurila <laurila@google.com>
    > Reviewed-by: Mike Wasserman <msw@chromium.org>
    > Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1212020}
    
    Bug: 1466855, b:288265319
    Change-Id: I89d419748c434f9859be034c4b15bc913acce6b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4955156
    Commit-Queue: Sonja Laurila <laurila@google.com>
    Reviewed-by: Christian Flach <cmfcmf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212700}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -3131,9 +3131,6 @@ bool ChromeContentBrowserClient::GetCanResize(content::Page* page) {
   return false;
 #else
   BrowserView* browser_view = GetBrowserView(page);
-  if (!browser_view) {
-    return false;
-  }
   return browser_view && browser_view->CanResize();
 #endif  // !defined(TOOLKIT_VIEWS)
 }
