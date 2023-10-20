b9d00044980abae99a2432133a2884d855cc1c13
Xiyuan Xia <xiyuan@chromium.org>
2023-10-20 17:44:22 +0000

    crosier: Port login.Chrome and login.ChromeGaia
    - Add `ChromeOSIntegrationLoginMixIn` that provides login support
      - It provides 3 login modes:
        - kStubLogin: Login like browser tests, Good for tests only need to
          verify chrome states.
        - kTestLogin: Login using oobe test api, same as tast's default
          login. Good for test that needs to simulate real login but do
          not need Gaia ideneity;
        - kGaiaLogin: Login using Gaia like production chrome.
    - Implement login integration tests that exercise kTestLogin
      (login.Chrome) and kGaiaLogin (login.ChromeGaia).
    
    Bug: b:301445988
    Change-Id: If4d3bd0450058ff46b60539b4a12738e7bb88279
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4953973
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212847}

--- chrome/test/base/chromeos/crosier/helper/test_sudo_helper_client.h
+++ chrome/test/base/chromeos/crosier/helper/test_sudo_helper_client.h
@@ -52,6 +52,9 @@ class TestSudoHelperClient {
   // Stops the session manager daemon.
   Result StopSessionManager();
 
+  // Ensures that session manager daemon is not left running from this client.
+  void EnsureSessionManagerStopped();
+
  private:
   base::ScopedFD ConnectToServer(const base::FilePath& client_path);
 
