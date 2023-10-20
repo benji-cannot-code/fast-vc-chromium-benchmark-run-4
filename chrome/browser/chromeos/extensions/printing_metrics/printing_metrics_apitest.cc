433e89e02a034857d5e85e391b028fcaaeae05bf
Andrew Rayskiy <greengrape@google.com>
2023-10-20 20:46:24 +0000

    Restrict PrintingMetricsApiTest.GetPrintJobs to v120.0.6079.0 onwards
    This is where the Ash-side fix landed: crrev.com/c/4955677
    
    Bug: b/301590015
    Change-Id: I9bc4922e7d147304271c8daabd01a735d0ebd632
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4956640
    Reviewed-by: Viktoriia Kovalova <vkovalova@google.com>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1212952}

--- chrome/browser/chromeos/extensions/printing_metrics/printing_metrics_apitest.cc
+++ chrome/browser/chromeos/extensions/printing_metrics/printing_metrics_apitest.cc
@@ -28,9 +28,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "base/version.h"
 #include "chromeos/crosapi/mojom/printing_metrics.mojom.h"
 #include "chromeos/crosapi/mojom/test_controller.mojom.h"
 #include "chromeos/lacros/lacros_service.h"
+#include "chromeos/lacros/lacros_test_helper.h"
 #endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
 
 namespace extensions {
@@ -155,7 +157,8 @@ class PrintingMetricsApiTest : public ExtensionApiTest {
 
 IN_PROC_BROWSER_TEST_F(PrintingMetricsApiTest, GetPrintJobs) {
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
-  if (!GetTestController()) {
+  if (!GetTestController() ||
+      !chromeos::IsAshVersionAtLeastForTesting(base::Version({120, 0, 6079}))) {
     GTEST_SKIP() << "Unsupported ash version.";
   }
 #endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
