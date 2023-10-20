9e28ee52798629ca02f8c2b98c35982315a7c025
YuhanYang <yyhyyh@google.com>
2023-10-20 18:09:31 +0000

    customization: Fix crash when refreshing and disconnecting
    - When refreshing, currentRouteChanged is called first with an empty
    device list due to connectedCallback which caused the
    provider.startObserving function error.
    So we'll skip initializing and observing devices in this situation.
    
    - Put the device castExists ahead of async api function to avoid the
    potential error after disconnecting a device.
    
    Bug: b/241965717
    Test: browser_tests, and tested on DUT
    Change-Id: Id1405b99961d9e4e81d0239b32d8dddfb516500d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4957853
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1212856}

--- chrome/browser/resources/ash/settings/device_page/customize_pen_buttons_subpage.ts
+++ chrome/browser/resources/ash/settings/device_page/customize_pen_buttons_subpage.ts
@@ -87,9 +87,13 @@ export class SettingsCustomizePenButtonsSubpageElement extends
       return;
     }
     this.previousRoute_ = route;
-    if (this.hasGraphicsTablets() &&
-        (!this.selectedTablet ||
-         this.selectedTablet.id !== this.getGraphicsTabletIdFromUrl())) {
+
+    if (!this.hasGraphicsTablets()) {
+      return;
+    }
+
+    if (!this.selectedTablet ||
+        this.selectedTablet.id !== this.getGraphicsTabletIdFromUrl()) {
       await this.initializePen();
     }
     this.inputDeviceSettingsProvider_.startObserving(this.selectedTablet.id);
@@ -101,13 +105,13 @@ export class SettingsCustomizePenButtonsSubpageElement extends
    */
   private async initializePen(): Promise<void> {
     const tabletId = this.getGraphicsTabletIdFromUrl();
+    const searchedGraphicsTablet = this.graphicsTablets.find(
+        (graphicsTablet: GraphicsTablet) => graphicsTablet.id === tabletId);
+    this.selectedTablet = castExists(searchedGraphicsTablet);
     this.buttonActionList_ =
         (await this.inputDeviceSettingsProvider_
              .getActionsForGraphicsTabletButtonCustomization())
             ?.options;
-    const searchedGraphicsTablet = this.graphicsTablets.find(
-        (graphicsTablet: GraphicsTablet) => graphicsTablet.id === tabletId);
-    this.selectedTablet = castExists(searchedGraphicsTablet);
   }
 
   private getGraphicsTabletIdFromUrl(): number {
