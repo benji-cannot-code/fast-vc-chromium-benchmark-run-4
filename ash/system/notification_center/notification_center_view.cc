900965cdca21e9b7c088837296a534bf59a10c92
Ahmed Mehfooz <amehfooz@chromium.org>
2023-10-20 02:41:12 +0000

    Clean up pre-QSRevamp code from StackedNotificationBar
    Part of an overall post QSRevamp clean up of the notification
    area code.
    
    Removed the expand button since there isn't a collapsed
    mode anymore.
    Removed animation code.
    
    Bug: b:306077397
    
    Change-Id: I66c54d1d30ec00eaeb9627681721b8a98d1f0507
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4950184
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212540}

--- ash/system/notification_center/notification_center_view.cc
+++ ash/system/notification_center/notification_center_view.cc
@@ -162,7 +162,6 @@ void NotificationCenterView::SetExpanded() {
   }
 
   collapsed_ = false;
-  notification_bar_->SetExpanded();
   scroller_->SetVisible(true);
 }
 
@@ -176,7 +175,6 @@ void NotificationCenterView::SetCollapsed(bool animate) {
     StartCollapseAnimation();
   } else {
     scroller_->SetVisible(false);
-    notification_bar_->SetCollapsed();
   }
 }
 
@@ -324,7 +322,6 @@ void NotificationCenterView::AnimationEnded(const gfx::Animation* animation) {
   PreferredSizeChanged();
 
   animation_state_ = NotificationCenterAnimationState::kIdle;
-  notification_bar_->SetAnimationState(animation_state_);
   UpdateVisibility();
 }
 
@@ -336,7 +333,6 @@ void NotificationCenterView::AnimationProgressed(
   if (collapsed_ && scroller_->GetVisible() &&
       animation_->GetCurrentValue() >= 0.5) {
     scroller_->SetVisible(false);
-    notification_bar_->SetCollapsed();
   }
   PreferredSizeChanged();
 }
@@ -353,7 +349,6 @@ void NotificationCenterView::OnContentsScrolled() {
 void NotificationCenterView::StartHideStackingBarAnimation() {
   animation_->End();
   animation_state_ = NotificationCenterAnimationState::kHideStackingBar;
-  notification_bar_->SetAnimationState(animation_state_);
   animation_->SetDuration(kHideStackingBarAnimationDuration);
   animation_->Start();
 }
@@ -361,7 +356,6 @@ void NotificationCenterView::StartHideStackingBarAnimation() {
 void NotificationCenterView::StartCollapseAnimation() {
   animation_->End();
   animation_state_ = NotificationCenterAnimationState::kCollapse;
-  notification_bar_->SetAnimationState(animation_state_);
   animation_->SetDuration(kCollapseAnimationDuration);
   animation_->Start();
 }
