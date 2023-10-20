98c6423241b9173c0b36872e0c3685641ab728f5
Frank Liberato <liberato@chromium.org>
2023-10-20 23:31:15 +0000

    Replace WebContents::SetIgnoreInputEvents
    Currently, `WebContents::SetIgnoreInputEvents` takes a boolean that
    directly enables / disables input events.  This is problematic for
    cases in which multiple requests to ignore input exist at once.
    
    This change replaces it with `IgnoreInputEvents()`, which returns a
    ScopedClosureRunner.  Input events are blocked until all outstanding
    closures have been run.  As a bonus, this also prevents leaving the
    WebContents in a blocked state unintentionally.
    
    Bug: 1480496, 1472386
    Change-Id: Ia49163764518764f1ed69b947f3ec9dde9d0e642
    Low-Coverage-Reason: TRIVIAL_CHANGE This CL changes a WebContents interface and updates consumers.  Looked into adding additional tests to input_handler.cc rather than adding Low-Coverage, but it's not clear to me that it has any tests.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4950728
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213008}

--- content/browser/devtools/protocol/input_handler.h
+++ content/browser/devtools/protocol/input_handler.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/input/synthetic_pointer_action_list_params.h"
 #include "content/common/input/synthetic_smooth_scroll_gesture_params.h"
 #include "content/public/browser/render_widget_host.h"
+#include "content/public/browser/web_contents.h"
 #include "third_party/blink/public/common/input/pointer_id.h"
 #include "third_party/blink/public/common/input/web_input_event.h"
 #include "third_party/blink/public/common/input/web_mouse_event.h"
@@ -354,6 +355,8 @@ class InputHandler : public DevToolsDomainHandler, public Input::Backend {
       injectors_;
   int last_id_ = 0;
   bool ignore_input_events_ = false;
+  absl::optional<content::WebContents::ScopedIgnoreInputEvents>
+      scoped_ignore_input_events_;
   bool intercept_drags_ = false;
   DragController drag_controller_;
   const bool allow_file_access_;
