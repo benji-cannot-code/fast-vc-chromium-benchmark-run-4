0570fc5f29727988cb2ed4186f6a4b7618c01d59
Nancy Wang <nancylingwang@chromium.org>
2023-10-20 05:40:53 +0000

    Move intent_filter convert function to intent_filter_util.
    This CL is used to prepare for saving intent_filter to the AppStorage
    file. Move intent_filter convert function to intent_filter_util from
    preferred_apps_converter to share the convert functions for
    Preferred_apps and AppStorage.
    
    This CL is pure refactor and no function change.
    
    BUG=1385932
    
    Change-Id: I1f838686db38edfef0ae3d200f93b7c12bcaab06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4957496
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212586}

--- components/services/app_service/public/cpp/preferred_apps_converter.h
+++ components/services/app_service/public/cpp/preferred_apps_converter.h
@@ -11,10 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace apps {
 
-extern const char kConditionTypeKey[];
-extern const char kConditionValuesKey[];
-extern const char kValueKey[];
-extern const char kMatchTypeKey[];
 extern const char kAppIdKey[];
 extern const char kIntentFilterKey[];
 extern const char kPreferredAppsKey[];
