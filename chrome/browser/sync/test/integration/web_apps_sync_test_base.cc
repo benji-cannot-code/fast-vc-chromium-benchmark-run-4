efd1d97f3479743efdc5038063ef93974cae53c6
Dibyajyoti Pal <dibyapal@google.com>
2023-10-20 00:50:42 +0000

    Reland "[dPWA] Adding support for intent picker UX in WebAppIntegrationTestDriver (not on Lacros)"
    This is a reland of commit 36f2c72f1cfbbf8298ed7f74c6293fd0090ac54f.
    This does not contain a fix to the flakiness, but it does fix the underlying issue, i.e. the
    WebAppIntegrationTestDriver does work properly for the new
    intent picker UX (as is verified by running the tests failing in
    crrev.com/c/4919875), just not on Lacros.
    
    I could not find a reasoning behind why it was behaving this way, but
    my findings have been documented in go/lacros-intent-ux-failure,
    and can be tackled by adding support for lacros tests in
    crbug.com/1462253.
    
    Original change's description:
    > [dPWA] Adding support for intent picker UX in WebAppIntegrationTestDriver
    >
    > This CL adds support for the new intent picker UX to be used in the
    > WebAppIntegrationTestDriver by:
    > 1. Piping whether the app icon will be shown all the way from the
    > IntentPickerTabHelper to the test driver so that the latter can
    > selectively wait if the icon needs to be shown in the intent chip.
    > 2. Adds an observer that observes visibility for the intent chip
    > button, and smartly determines if it needs to wait for the chip
    > visibility to change.
    >
    > Verified by running existing WebAppIntegration tests that were
    > failing locally once the flag was removed.
    >
    > Bug: 1412856, 1357905
    > Change-Id: Ia376bccb2c2dcaaa5c4df35f9d91a3398f0830ba
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4942518
    > Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    > Reviewed-by: Elly FJ <ellyjones@chromium.org>
    > Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    > Reviewed-by: Glen Robertson <glenrob@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1211513}
    
    Bug: 1412856, 1357905
    Change-Id: I294910bc6f24c41ad2662ae0c749b7807ac27989
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4953565
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212497}

--- chrome/browser/sync/test/integration/web_apps_sync_test_base.cc
+++ chrome/browser/sync/test/integration/web_apps_sync_test_base.cc
@@ -7,10 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/extend.h"
 
-#if BUILDFLAG(IS_CHROMEOS)
-#include "chrome/browser/apps/link_capturing/link_capturing_features.h"
-#endif
-
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ash/constants/ash_features.h"
 #include "chrome/common/chrome_features.h"
@@ -18,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "chrome/browser/apps/link_capturing/link_capturing_features.h"
 #include "chrome/browser/sync/chrome_sync_client.h"
 #include "chrome/browser/web_applications/web_app_utils.h"
 #endif
@@ -28,8 +25,8 @@ WebAppsSyncTestBase::WebAppsSyncTestBase(TestType test_type)
     : SyncTest(test_type) {
   std::vector<base::test::FeatureRef> disabled_features;
 
-#if BUILDFLAG(IS_CHROMEOS)
-  // TODO(crbug.com/1357905): Update test driver to work with new UI.
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  // TODO(crbug.com/1444955): Update test driver to work with new UX on Lacros.
   disabled_features.push_back(apps::features::kLinkCapturingUiUpdate);
 #endif
 
--- chrome/browser/ui/views/location_bar/intent_chip_button_browsertest.cc
+++ chrome/browser/ui/views/location_bar/intent_chip_button_browsertest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/test/test_future.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/apps/app_service/app_registry_cache_waiter.h"
@@ -72,11 +73,11 @@ class IntentChipButtonBrowserTest
   void DoAndWaitForIntentPickerIconUpdate(Action action) {
     content::WebContents* web_contents =
         browser()->tab_strip_model()->GetActiveWebContents();
-    base::RunLoop run_loop;
+    base::test::TestFuture<bool> test_future;
     auto* tab_helper = IntentPickerTabHelper::FromWebContents(web_contents);
-    tab_helper->SetIconUpdateCallbackForTesting(run_loop.QuitClosure());
+    tab_helper->SetIconUpdateCallbackForTesting(test_future.GetCallback());
     action();
-    run_loop.Run();
+    EXPECT_TRUE(test_future.Wait());
   }
 
   void OpenNewTab(const GURL& url) {
@@ -116,10 +117,10 @@ class IntentChipButtonBrowserTest
                                      const GURL& link_url) {
     auto* tab_helper = IntentPickerTabHelper::FromWebContents(web_contents);
 
-    base::RunLoop run_loop;
-    tab_helper->SetIconUpdateCallbackForTesting(run_loop.QuitClosure());
+    base::test::TestFuture<bool> test_future;
+    tab_helper->SetIconUpdateCallbackForTesting(test_future.GetCallback());
     ClickLinkAndWait(web_contents, link_url, LinkTarget::SELF, "");
-    run_loop.Run();
+    EXPECT_TRUE(test_future.Wait());
   }
 
   // Installs a web app on the same host as InstallTestWebApp(), but with "/" as
@@ -318,11 +319,11 @@ void IntentChipButtonBrowserUiTest::ShowUi(const std::string& name) {
   auto* const web_contents =
       browser()->tab_strip_model()->GetActiveWebContents();
   auto* const tab_helper = IntentPickerTabHelper::FromWebContents(web_contents);
-  base::RunLoop run_loop;
-  tab_helper->SetIconUpdateCallbackForTesting(run_loop.QuitClosure());
+  base::test::TestFuture<bool> test_future;
+  tab_helper->SetIconUpdateCallbackForTesting(test_future.GetCallback());
   tab_helper->MaybeShowIconForApps(
       {{apps::PickerEntryType::kWeb, ui::ImageModel(), "app_id", "Test app"}});
-  run_loop.Run();
+  EXPECT_TRUE(test_future.Wait());
 }
 
 bool IntentChipButtonBrowserUiTest::VerifyUi() {
