b1f091b8335683875f2926ae4d5b0cdf5e801c67
Elie Maamari <emaamari@google.com>
2023-10-20 14:28:56 +0000

    Add support for setting up gaia password
    Bug: b:291808449
    Change-Id: I40583cbe95999337354e9b2d91592481887397af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935814
    Commit-Queue: Elie Maamari <emaamari@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212727}

--- chrome/browser/ash/login/screens/local_password_setup_screen.cc
+++ chrome/browser/ash/login/screens/local_password_setup_screen.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/webui/ash/login/local_password_setup_handler.h"
+#include "chromeos/ash/components/login/auth/public/cryptohome_key_constants.h"
 #include "chromeos/ash/services/auth_factor_config/auth_factor_config.h"
 #include "chromeos/ash/services/auth_factor_config/in_process_instances.h"
 #include "chromeos/ash/services/auth_factor_config/public/mojom/auth_factor_config.mojom-forward.h"
