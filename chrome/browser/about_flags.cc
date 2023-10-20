d2251e112bb65dc2ff18b892d050f61a942584e7
Deomid "rojer" Ryabkov <rojer9@gmail.com>
2023-10-20 12:01:29 +0000

    Remove --digest-auth-enable-secure-algorithms
    Originally added in https://chromium-review.googlesource.com/c/chromium/src/+/4611879, no longer needed.
    
    Bug: 1489851
    Change-Id: I5399c06b385247387ffd089b57f680b99b19ed7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4958473
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212675}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -10708,11 +10708,6 @@ const FeatureEntry kFeatureEntries[] = {
          password_manager::features::kFillingAcrossAffiliatedWebsitesAndroid)},
 #endif
 
-    {"digest-auth-enable-secure-algorithms",
-     flag_descriptions::kDigestAuthEnableSecureAlgorithmsName,
-     flag_descriptions::kDigestAuthEnableSecureAlgorithmsDescription, kOsAll,
-     FEATURE_VALUE_TYPE(net::features::kDigestAuthEnableSecureAlgorithms)},
-
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
     {"third-party-profile-management",
      flag_descriptions::kThirdPartyProfileManagementName,
