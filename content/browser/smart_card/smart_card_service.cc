61d3154089e3753fa7c9f147334966e7f0975d48
Andrew Rayskiy <greengrape@google.com>
2023-10-20 09:24:14 +0000

    Rename IsFrameSufficientlyIsolated() and add an RPH overload
    Two new functions:
      * HasIsolatedContextCapability(RenderFrameHost*)
      * IsIsolatedContext(RenderProcessHost*)
    
    RenderFrameHost* could have a lower WEIL than its RenderProcessHost*
    because of the cross-origin-isolated permissions policy, that's why
    these functions are declared independently.
    
    Bug: 1487254, b/302505962
    Change-Id: Ib41ffa41e1a4a39c34e7c7e8dc26124c5044a0fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4891621
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212639}

--- content/browser/smart_card/smart_card_service.cc
+++ content/browser/smart_card/smart_card_service.cc
@@ -67,7 +67,7 @@ void SmartCardService::Create(
     return;
   }
 
-  if (!IsFrameSufficientlyIsolated(render_frame_host)) {
+  if (!HasIsolatedContextCapability(render_frame_host)) {
     mojo::ReportBadMessage(
         "Frame is not sufficiently isolated to use the Smart Card API.");
     return;
