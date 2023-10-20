51377e0e61ed79dcf82713031cd7ee3601e5cc7e
Orin Jaworski <orinj@google.com>
2023-10-20 18:34:12 +0000

    [omnibox][webui] Keep popup width correct by handling LBV bounds changed
    This CL makes OmniboxPopupPresenter an observer of LocationBarView so
    that LBV bounds changes result in widget bounds updates. This fixes the
    long-existing problem of stale popup width after window size changes.
    
    Bug: 1479181
    Change-Id: Ie037b97e1f76976a9ec824cde633a8a26aafc797
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4957298
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1212866}

--- chrome/browser/ui/views/omnibox/omnibox_popup_presenter.h
+++ chrome/browser/ui/views/omnibox/omnibox_popup_presenter.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/views/controls/webview/webview.h"
+#include "ui/views/view_observer.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/widget/widget_observer.h"
 
@@ -26,7 +27,8 @@ class OmniboxController;
 // code, work with OmniboxPopupViewWebUI directly.
 class OmniboxPopupPresenter : public views::WebView,
                               public views::WidgetObserver,
-                              public OmniboxWebUIPopupChangeObserver {
+                              public OmniboxWebUIPopupChangeObserver,
+                              public views::ViewObserver {
  public:
   METADATA_HEADER(OmniboxPopupPresenter);
   explicit OmniboxPopupPresenter(LocationBarView* location_bar_view,
@@ -52,6 +54,9 @@ class OmniboxPopupPresenter : public views::WebView,
   // RealboxWebUIChangeClient:
   void OnPopupElementSizeChanged(gfx::Size size) override;
 
+  // views::ViewObserver:
+  void OnViewBoundsChanged(View* observed_view) override;
+
  private:
   friend class OmniboxPopupViewWebUITest;
 
@@ -69,6 +74,9 @@ class OmniboxPopupPresenter : public views::WebView,
 
   // Whether any call to `GetHandler` has been made.
   bool requested_handler_;
+
+  // Last reported WebUI element size.
+  gfx::Size webui_element_size_;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_OMNIBOX_OMNIBOX_POPUP_PRESENTER_H_
