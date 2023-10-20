0d795967362ce9690a9f373355b4f08e62bdf868
Daniel Classon <dclasson@google.com>
2023-10-20 18:42:46 +0000

    [Nearby Share] Add ResourceUtils and ResourceGetter
    Creates helper methods that will be used to access internal resources.
    
    Bug: b/303262190
    Change-Id: I4c342f32678851a86b645eff34ba16b328574edf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4937716
    Commit-Queue: Daniel Classon <dclasson@google.com>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Cr-Commit-Position: refs/heads/main@{#1212874}

---
+++ chrome/browser/nearby_sharing/common/nearby_share_resource_utils.h
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_NEARBY_SHARING_COMMON_NEARBY_SHARE_RESOURCE_UTILS_H_
+#define CHROME_BROWSER_NEARBY_SHARING_COMMON_NEARBY_SHARE_RESOURCE_UTILS_H_
+
+#include <string>
+
+std::u16string GetNearbyShareFeatureName();
+
+#endif  // CHROME_BROWSER_NEARBY_SHARING_COMMON_NEARBY_SHARE_RESOURCE_UTILS_H_
