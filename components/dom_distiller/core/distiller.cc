6749b5ee86755df91a64925f695ba44e52b3e567
Sergii Bykov <sbykov@google.com>
2023-10-20 23:24:42 +0000

    [Code Health] Remove expired DomDistiller.Statistics histograms
    Bug: 1448186
    Change-Id: I6f9dfb82d7bb7378f049a6828d7ae9294beb8cbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4958654
    Commit-Queue: Sergii Bykov <sbykov@google.com>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1213007}

--- components/dom_distiller/core/distiller.cc
+++ components/dom_distiller/core/distiller.cc
@@ -15,12 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/location.h"
 #include "base/memory/ptr_util.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/single_thread_task_runner.h"
-#include "base/values.h"
 #include "build/build_config.h"
 #include "components/dom_distiller/core/distiller_page.h"
 #include "components/dom_distiller/core/distiller_url_fetcher.h"
@@ -156,14 +154,6 @@ void DistillerImpl::OnPageDistillationFinished(
     started_pages_index_.erase(page_num);
     RunDistillerCallbackIfDone();
     return;
-  }
-
-  if (distiller_result->has_statistics_info() && page_num == 0) {
-    if (distiller_result->statistics_info().has_word_count()) {
-      UMA_HISTOGRAM_CUSTOM_COUNTS(
-          "DomDistiller.Statistics.FirstPageWordCount",
-          distiller_result->statistics_info().word_count(), 1, 4000, 50);
-    }
   }
 
   DCHECK(distiller_result);
