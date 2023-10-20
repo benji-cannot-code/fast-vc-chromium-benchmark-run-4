6fd5d73d923e9d43784ca8b4c842c2e734a0c103
Rune Lillesveen <futhark@chromium.org>
2023-10-20 21:03:03 +0000

    Propagate BoxSizeDependentMatrix from interpolation type
    Allow box size dependent matrix interpolations explicitly from the
    standard transform property interpolation type via
    InterpolableTransformList.
    
    Also send CSSToLengthConversionData instead of StyleResolverState for
    InterpolableTransformList::ConvertCSSValue() since the registered
    custom property transforms will now have a StyleResolverState
    available.
    
    No functional changes.
    
    Interpolation types for registered custom property <transform-list> and
    <transform-function> interpolations will be added in follow-up CLs.
    
    Bug: 911156
    Change-Id: I9f759ed49b5d53b878d2fb0a1bc397df4fe24b3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4953198
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212963}

--- third_party/blink/renderer/core/animation/css_transform_interpolation_type.cc
+++ third_party/blink/renderer/core/animation/css_transform_interpolation_type.cc
@@ -23,8 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 namespace {
 InterpolationValue ConvertTransform(TransformOperations&& transform) {
-  return InterpolationValue(
-      std::make_unique<InterpolableTransformList>(std::move(transform)));
+  return InterpolationValue(std::make_unique<InterpolableTransformList>(
+      std::move(transform),
+      TransformOperations::BoxSizeDependentMatrixBlending::kAllow));
 }
 
 InterpolationValue ConvertTransform(const TransformOperations& transform) {
@@ -83,7 +84,7 @@ InterpolationValue CSSTransformInterpolationType::MaybeConvertValue(
     const CSSValue& value,
     const StyleResolverState* state,
     ConversionCheckers& conversion_checkers) const {
-  DCHECK(state);
+  CHECK(state);
   if (auto* list_value = DynamicTo<CSSValueList>(value)) {
     CSSPrimitiveValue::LengthTypeFlags types;
     for (const CSSValue* item : *list_value) {
@@ -117,8 +118,9 @@ InterpolationValue CSSTransformInterpolationType::MaybeConvertValue(
       conversion_checkers.push_back(std::move(length_units_checker));
   }
 
-  return InterpolationValue(
-      InterpolableTransformList::ConvertCSSValue(value, state));
+  return InterpolationValue(InterpolableTransformList::ConvertCSSValue(
+      value, state->CssToLengthConversionData(),
+      TransformOperations::BoxSizeDependentMatrixBlending::kAllow));
 }
 
 InterpolationValue
