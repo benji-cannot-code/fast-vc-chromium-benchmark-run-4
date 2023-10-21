76330db0c752ac549aca5a20be7209efeb6dc35e
Christoph Schwering <schwering@google.com>
2023-10-21 13:44:46 +0000

    [Blink] Remove references to input_type_names::kDatetime
    HTMLFormControlElement::type() never returns
    `input_type_names::kDatetime`.
    
    This CL removes comparisons with `input_type_names::kDatetime` and uses
    the FormControlType enum instead of AtomicStrings in the expressions
    that referred to `input_type_names::kDatetime`.
    
    The CL does not remove the `input_type_names::kDatetime` string constant
    because appears to be a metric that counts `<input type=datetime>`: it
    calls `CountUsageIfVisible(WebFeature::kInputTypeDateTimeFallback)`.
    
    Bug: 1482526, 1484810
    Change-Id: I8c20674c57d048d3e41b0ab1ba341ce30a933e67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935121
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1213125}

--- third_party/blink/renderer/modules/accessibility/ax_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object.cc
@@ -139,6 +139,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+using mojom::blink::FormControlType;
+
 namespace {
 
 #if defined(AX_FAIL_FAST_BUILD)
@@ -4050,12 +4052,12 @@ const AXObject* AXObject::DatetimeAncestor() const {
   if (!input) {
     return nullptr;
   }
-  if (input->type() != input_type_names::kDatetimeLocal &&
-      input->type() != input_type_names::kDatetime &&
-      input->type() != input_type_names::kDate &&
-      input->type() != input_type_names::kTime &&
-      input->type() != input_type_names::kMonth &&
-      input->type() != input_type_names::kWeek) {
+  FormControlType type = input->FormControlType();
+  if (type != FormControlType::kInputDatetimeLocal &&
+      type != FormControlType::kInputDate &&
+      type != FormControlType::kInputTime &&
+      type != FormControlType::kInputMonth &&
+      type != FormControlType::kInputWeek) {
     return nullptr;
   }
   return AXObjectCache().GetOrCreate(input);
