48fb648c1e127b0efb08af1a854d1cbb9263497e
Gavin Williams <gavinwill@chromium.org>
2023-10-21 03:27:57 +0000

    Printing: Fix parameter type for local printer observer
    Loosen the parameter restrictions for addListenerCallback() so the
    DestinationStore can properly only expect LocalDestinationInfo for the
    'local-printers-updated' event.
    
    The 'local-printers-updated' event fired from PrintPreviewHandler
    should only ever send LocalDestinationInfo.
    
    Bug: b:300136694
    Change-Id: Ic52a75572582d7970bfc9043d70138ec4dd35265
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4955615
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213072}

--- chrome/browser/ui/webui/print_preview/print_preview_handler_chromeos.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_handler_chromeos.cc
@@ -421,7 +421,6 @@ void PrintPreviewHandlerChromeOS::OnHandleObserveLocalPrinters(
 void PrintPreviewHandlerChromeOS::OnLocalPrintersUpdated(
     std::vector<crosapi::mojom::LocalDestinationInfoPtr> printers) {
   FireWebUIListener("local-printers-updated",
-                    base::Value(static_cast<int>(mojom::PrinterType::kLocal)),
                     ConvertPrintersToValues(printers));
 }
 
