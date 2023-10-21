8abbafeae0dfc3f12e8f37eefcb33c3819bdf5c2
Kyungjun Lee <kyungjunlee@google.com>
2023-10-21 00:42:27 +0000

    Add a disconnect handler and reorder mojo-related variables
    Add a disconnect handler to `receivers_` to ensure that a callback
    gets removed first when a receiver gets disconnected or destroyed.
    Create `pending_download_callback_map_` to maintain receivers and
    their associated callbacks.
    Also, place `pending_download_callback_map_` before `receivers_` so
    that `pending_download_callback_map_` will be destroyed after
    `receivers_`. This reordering provides an extra guarantee that the
    callbacks get destroyed after their corresponding receivers.
    
    AX-Relnotes: n/a.
    Bug: b/305056689
    Change-Id: I1c68dc3ad3f6d760931e28b6aa28c062131dbd7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4937723
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Kyungjun Lee <kyungjunlee@google.com>
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213039}

--- chrome/browser/ash/crosapi/screen_ai_downloader_ash.cc
+++ chrome/browser/ash/crosapi/screen_ai_downloader_ash.cc
@@ -6,10 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/crosapi/screen_ai_downloader_ash.h"
 
 #include "chrome/browser/screen_ai/screen_ai_install_state.h"
+#include "mojo/public/cpp/bindings/receiver_set.h"
 
 namespace crosapi {
 
-ScreenAIDownloaderAsh::ScreenAIDownloaderAsh() = default;
+ScreenAIDownloaderAsh::ScreenAIDownloaderAsh() {
+  receivers_.set_disconnect_handler(base::BindRepeating(
+      &ScreenAIDownloaderAsh::OnDisconnected, weak_factory_.GetWeakPtr()));
+}
 
 ScreenAIDownloaderAsh::~ScreenAIDownloaderAsh() = default;
 
@@ -36,7 +40,8 @@ void ScreenAIDownloaderAsh::GetComponentFolder(
   }
 
   // Keep the callback and observe status updates.
-  pending_download_callbacks_.push_back(std::move(callback));
+  mojo::ReceiverId id = receivers_.current_receiver();
+  pending_download_callback_map_.emplace(id, std::move(callback));
   if (!install_state_observer_.IsObserving()) {
     // Adding the observer will trigger download.
     install_state_observer_.Observe(install_state);
@@ -58,7 +63,7 @@ void ScreenAIDownloaderAsh::SetLastUsageTime() {
 
 void ScreenAIDownloaderAsh::StateChanged(
     screen_ai::ScreenAIInstallState::State state) {
-  if (pending_download_callbacks_.empty()) {
+  if (pending_download_callback_map_.empty()) {
     return;
   }
 
@@ -80,10 +85,15 @@ void ScreenAIDownloaderAsh::StateChanged(
       break;
   }
 
-  for (auto& callback : pending_download_callbacks_) {
-    std::move(callback).Run(component_path);
+  for (auto& entry : pending_download_callback_map_) {
+    std::move(entry.second).Run(component_path);
   }
-  pending_download_callbacks_.clear();
+  pending_download_callback_map_.clear();
+}
+
+void ScreenAIDownloaderAsh::OnDisconnected() {
+  mojo::ReceiverId id = receivers_.current_receiver();
+  pending_download_callback_map_.erase(id);
 }
 
 }  // namespace crosapi
