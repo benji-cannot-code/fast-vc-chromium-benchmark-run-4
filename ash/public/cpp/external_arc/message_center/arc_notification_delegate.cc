7b4bfada71aed7873867a5c1b36b03e231d9742f
Shuming Hao <shuminghao@google.com>
2023-10-21 01:15:00 +0000

    Revert "Fix crash caused by closing grouped ARC notifications"
    This reverts commit e09129f33d57d0cf3ec7dabe5db9e81af97083e5.
    
    Reason for revert: No longer needed. Alternative solution in b/303500360
    
    Original change's description:
    > Fix crash caused by closing grouped ARC notifications
    >
    > [Chrome rendered ARC notifications] When closing the last notification
    > from an ARC notification group rendered in Chrome, there is a crash
    > caused by removing already destroyed ArcNotificationItem. This CL adds
    > additional check before trying to remove the item.
    >
    > Bug: 1483098
    > Test: manual test grouped notifications with Notify1.apk
    > Change-Id: I7856809eec729f9a89739962d6ac51e155f68851
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4870262
    > Reviewed-by: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    > Commit-Queue: Shuming Hao <shuminghao@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1198153}
    
    Bug: 1483098
    Change-Id: I7da3281f1b4caa0c3322d5a887e00f6c03c1ddc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4957153
    Reviewed-by: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Commit-Queue: Shuming Hao <shuminghao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1213053}

--- ash/public/cpp/external_arc/message_center/arc_notification_delegate.cc
+++ ash/public/cpp/external_arc/message_center/arc_notification_delegate.cc
@@ -32,9 +32,7 @@ ArcNotificationDelegate::CreateCustomMessageView(
 }
 
 void ArcNotificationDelegate::Close(bool by_user) {
-  if (!item_) {
-    return;
-  }
+  DCHECK(item_);
   item_->Close(by_user);
 }
 
