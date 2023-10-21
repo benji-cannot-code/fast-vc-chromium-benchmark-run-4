274faf58575161404c041d180f555b109ef34ac0
Xiaohui Chen <xiaohuic@google.com>
2023-10-21 00:10:27 +0000

    os settings: move cpp files to new location (40/N)
    * Move new display settings mojom related files
    
    Bug: b/284187997
    Change-Id: I3ee246e3128e6b35005ce6773e7f60bd8c03b289
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4953069
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213020}

--- chrome/browser/ui/webui/ash/settings/services/settings_manager/os_settings_manager.h
+++ chrome/browser/ui/webui/ash/settings/services/settings_manager/os_settings_manager.h
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/eche_app_ui/eche_app_manager.h"
 #include "base/gtest_prod_util.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_forward.h"
+#include "chrome/browser/ui/webui/ash/settings/pages/device/display_settings/display_settings_provider.h"
 #include "chrome/browser/ui/webui/ash/settings/pages/device/input_device_settings/input_device_settings_provider.h"
-#include "chrome/browser/ui/webui/settings/ash/display_settings/display_settings_provider.h"
 #include "components/keyed_service/core/keyed_service.h"
 
 class ArcAppListPrefs;
