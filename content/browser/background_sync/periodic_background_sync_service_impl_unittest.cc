a1051c6ae5140b8b24ff98f9120e093ef1dcad7e
punithnayak <npunith125@gmail.com>
2023-10-22 17:16:37 +0000

    Removing dangling pointer from PeriodicBackgroundSyncServiceImplTest
    Bug: 1485833
    Change-Id: Ie1061478eac99795d1f9a3cb635bb4536c800ec7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4951835
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Punith Nayak <npunith125@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1213244}

--- content/browser/background_sync/periodic_background_sync_service_impl_unittest.cc
+++ content/browser/background_sync/periodic_background_sync_service_impl_unittest.cc
@@ -20,6 +20,10 @@ class PeriodicBackgroundSyncServiceImplTest
     BackgroundSyncServiceImplTestHarness::SetUp();
     CreatePeriodicBackgroundSyncServiceImpl();
   }
+  void TearDown() override {
+    periodic_sync_service_impl_ = nullptr;
+    BackgroundSyncServiceImplTestHarness::TearDown();
+  }
 
  protected:
   void CreatePeriodicBackgroundSyncServiceImpl() {
@@ -75,8 +79,7 @@ class PeriodicBackgroundSyncServiceImplTest
       periodic_sync_service_remote_;
 
   // Owned by |background_sync_context_|
-  raw_ptr<PeriodicBackgroundSyncServiceImpl, DanglingUntriaged>
-      periodic_sync_service_impl_;
+  raw_ptr<PeriodicBackgroundSyncServiceImpl> periodic_sync_service_impl_;
 };
 
 // Tests
