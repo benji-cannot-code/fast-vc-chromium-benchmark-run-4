a7651cdce00a4ef7239184a90bd87f3eeaef3f55
Chuong Ho <hdchuong@google.com>
2023-10-22 23:23:23 +0000

    editor: block files app
    Previously Files app is blocked indirectly because System apps are
    blocked. Now we block files app using its app id.
    
    Bug: b:305566125
    Change-Id: If930a61f53b3d4e9cf5611801df6419eaa3a5d2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4963261
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213275}

--- chrome/browser/ash/input_method/editor_switch.cc
+++ chrome/browser/ash/input_method/editor_switch.cc
@@ -42,6 +42,7 @@ constexpr std::string_view kInputMethodEngineAllowlist[] = {
 constexpr AppType kAppTypeAllowlist[] = {
     AppType::BROWSER,
     AppType::LACROS,
+    AppType::SYSTEM_APP,
 };
 
 const char* kDomainsWithPathDenylist[][2] = {
@@ -54,6 +55,10 @@ const char* kDomainsWithPathDenylist[][2] = {
     {"mail.google", "/chat"},
     {"mail.google", "/mail"},
     {"meet.google", ""},
+};
+
+const char* kAppIdDenylist[] = {
+    "fkiggjmkendpmbegkagpmagjepfkpmeb",  //  Files App
 };
 
 constexpr int kTextLengthMaxLimit = 8000;
@@ -91,6 +96,10 @@ bool IsUrlAllowed(const char* (&denied_domains_with_paths)[N][2], GURL url) {
   return true;
 }
 
+bool IsAppAllowed(std::string_view app_id) {
+  return !base::Contains(kAppIdDenylist, app_id);
+}
+
 }  // namespace
 
 EditorSwitch::EditorSwitch(Profile* profile, std::string_view country_code)
@@ -118,7 +127,8 @@ bool EditorSwitch::CanBeTriggered() const {
          IsInputTypeAllowed(input_type_) && IsAppTypeAllowed(app_type_) &&
          IsTriggerableFromConsentStatus(current_consent_status) &&
          IsUrlAllowed(kDomainsWithPathDenylist, url_) &&
-         !net::NetworkChangeNotifier::IsOffline() && !tablet_mode_enabled_ &&
+         IsAppAllowed(app_id_) && !net::NetworkChangeNotifier::IsOffline() &&
+         !tablet_mode_enabled_ &&
          // user pref value
          profile_->GetPrefs()->GetBoolean(prefs::kOrcaEnabled) &&
          text_length_ <= kTextLengthMaxLimit;
@@ -148,6 +158,7 @@ void EditorSwitch::OnInputContextUpdated(
   input_type_ = input_context.type;
   app_type_ = text_field_contextual_info.app_type;
   url_ = text_field_contextual_info.tab_url;
+  app_id_ = text_field_contextual_info.app_key;
 }
 
 void EditorSwitch::OnActivateIme(std::string_view engine_id) {
