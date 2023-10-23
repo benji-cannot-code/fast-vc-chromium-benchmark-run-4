9de094fd7c99c55241bf5f84879a21607c65a878
Nan Lin <linnan@chromium.org>
2023-10-23 18:03:23 +0000

    Remove feature kAttributionReportingNullAggregatableReports
    The feature was default enabled in M115.
    
    https://chromiumdash.appspot.com/commit/3733a639d724a4353463a872605119d11a1e4d37
    
    Change-Id: Ida9fce6f11078056574d978746cca80a1dc64eb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4967812
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213610}

--- content/browser/attribution_reporting/attribution_storage_delegate_impl_unittest.cc
+++ content/browser/attribution_reporting/attribution_storage_delegate_impl_unittest.cc
@@ -464,10 +464,6 @@ TEST_F(AttributionStorageDelegateImplTestInvalidFeatureConfigured,
 
 TEST(AttributionStorageDelegateImplTest,
      NullAggregatableReports_IncludeSourceRegistrationTime) {
-  base::test::ScopedFeatureList scoped_feature_list(
-      attribution_reporting::features::
-          kAttributionReportingNullAggregatableReports);
-
   const auto trigger = DefaultTrigger();
 
   EXPECT_THAT(AttributionStorageDelegateImpl()
@@ -494,10 +490,6 @@ TEST(AttributionStorageDelegateImplTest,
 
 TEST(AttributionStorageDelegateImplTest,
      NullAggregatableReports_ExcludeSourceRegistrationTime) {
-  base::test::ScopedFeatureList scoped_feature_list(
-      attribution_reporting::features::
-          kAttributionReportingNullAggregatableReports);
-
   const auto trigger = TriggerBuilder()
                            .SetSourceRegistrationTimeConfig(
                                attribution_reporting::mojom::
