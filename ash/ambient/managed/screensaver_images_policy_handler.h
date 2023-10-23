626965221f7cda929fea1eb87cc9c5eb3aedd7e9
Fahad Mansoor <fahadmansoor@google.com>
2023-10-23 17:28:21 +0000

    Ambient: Clean-up unused ambient managed photo source interface
    Remove the AmbientManagedPhotoSource interface as it was not
    being used ever since the policy handler was moved to ash from
    c/b/a.
    
    Test: ash_unittest --gtest-filter=*ManagedScreensaver*
    Bug: b:280981760
    Change-Id: Ia6d357668999761a61d99b270c7bffc1d907fdb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4966740
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Fahad Mansoor <fahadmansoor@google.com>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213584}

--- ash/ambient/managed/screensaver_images_policy_handler.h
+++ ash/ambient/managed/screensaver_images_policy_handler.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ambient/managed/screensaver_image_downloader.h"
 #include "ash/ash_export.h"
-#include "ash/public/cpp/ambient/ambient_managed_photo_source.h"
 #include "base/files/file_path.h"
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
@@ -24,9 +23,11 @@ namespace ash {
 
 // Observes the policy that provides image sources for the managed screensaver
 // feature in order to download and cache the images.
-class ASH_EXPORT ScreensaverImagesPolicyHandler
-    : public AmbientManagedPhotoSource {
+class ASH_EXPORT ScreensaverImagesPolicyHandler {
  public:
+  using ScreensaverImagesRepeatingCallback =
+      base::RepeatingCallback<void(const std::vector<base::FilePath>& images)>;
+
   enum HandlerType { kSignin, kUser, kManagedGuest };
 
   static std::unique_ptr<ScreensaverImagesPolicyHandler> Create(
@@ -40,12 +41,11 @@ class ASH_EXPORT ScreensaverImagesPolicyHandler
   ScreensaverImagesPolicyHandler& operator=(
       const ScreensaverImagesPolicyHandler&) = delete;
 
-  ~ScreensaverImagesPolicyHandler() override;
+  ~ScreensaverImagesPolicyHandler();
 
-  // AmbientManagedPhotoSource overrides
-  std::vector<base::FilePath> GetScreensaverImages() override;
+  std::vector<base::FilePath> GetScreensaverImages();
   void SetScreensaverImagesUpdatedCallback(
-      ScreensaverImagesRepeatingCallback callback) override;
+      ScreensaverImagesRepeatingCallback callback);
 
   // Used for setting images in tests.
   void SetImagesForTesting(const std::vector<base::FilePath>& images);
