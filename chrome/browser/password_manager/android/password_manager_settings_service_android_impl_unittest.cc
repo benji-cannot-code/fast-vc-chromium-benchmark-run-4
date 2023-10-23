b5e52481de3bc5f74f27da00456198d05b6f7449
Ivana Žužić <izuzic@google.com>
2023-10-23 15:01:51 +0000

    [UPMLocalSettings] Auth error affects only syncing users
    Disabling password saving due to an auth error should not affect
    non-syncing users.
    
    Bug: 1443356
    Change-Id: I0504d5cd51ddf528799eb6af83cea8b88a75d2ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4966801
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1213496}

--- chrome/browser/password_manager/android/password_manager_settings_service_android_impl_unittest.cc
+++ chrome/browser/password_manager/android/password_manager_settings_service_android_impl_unittest.cc
@@ -923,7 +923,7 @@ TEST_F(PasswordManagerSettingsServiceAndroidImplTest,
 }
 
 TEST_F(PasswordManagerSettingsServiceAndroidImplTest,
-       OverrideOfferToSaveForErrorWhenNotSyncing) {
+       OfferToSaveForErrorWhenNotSyncing) {
   InitializeSettingsService(/*password_sync_enabled=*/false,
                             /*setting_sync_enabled=*/true);
   pref_service()->SetUserPref(
@@ -933,12 +933,12 @@ TEST_F(PasswordManagerSettingsServiceAndroidImplTest,
       base::Value(true));
   pref_service()->SetBoolean(
       password_manager::prefs::kSavePasswordsSuspendedByError, true);
-  EXPECT_FALSE(settings_service()->IsSettingEnabled(
+  EXPECT_TRUE(settings_service()->IsSettingEnabled(
       PasswordManagerSetting::kOfferToSavePasswords));
 }
 
 TEST_F(PasswordManagerSettingsServiceAndroidImplTest,
-       OverrideOfferToSaveForErrorWhenManaged) {
+       OfferToSaveForErrorWhenManagedNotSyncing) {
   InitializeSettingsService(/*password_sync_enabled=*/false,
                             /*setting_sync_enabled=*/true);
   pref_service()->SetUserPref(
@@ -948,10 +948,9 @@ TEST_F(PasswordManagerSettingsServiceAndroidImplTest,
       base::Value(true));
   pref_service()->SetBoolean(
       password_manager::prefs::kSavePasswordsSuspendedByError, true);
-  // Saving passwords is allowed by policy but errors enforce the stricter rule.
   pref_service()->SetManagedPref(
       password_manager::prefs::kCredentialsEnableService, base::Value(true));
-  EXPECT_FALSE(settings_service()->IsSettingEnabled(
+  EXPECT_TRUE(settings_service()->IsSettingEnabled(
       PasswordManagerSetting::kOfferToSavePasswords));
 }
 
