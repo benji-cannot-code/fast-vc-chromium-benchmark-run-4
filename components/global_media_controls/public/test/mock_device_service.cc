70f53f76c769e8398fc4f16f5bf7a546f005568a
Takumi Fujimoto <takumif@chromium.org>
2023-10-23 20:10:44 +0000

    Add a browsertest for starting casting in GMC on Chrome OS
    Bug: b/306022402
    Change-Id: I59f2ed4b19aaf41d7ba7c7ca9d541b041c7ee07b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4957493
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Jiaming Cheng <jiamingc@chromium.org>
    Auto-Submit: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213708}

--- components/global_media_controls/public/test/mock_device_service.cc
+++ components/global_media_controls/public/test/mock_device_service.cc
@@ -7,19 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace global_media_controls::test {
 
+MockDeviceListHost::MockDeviceListHost() = default;
+MockDeviceListHost::~MockDeviceListHost() = default;
+
 MockDeviceService::MockDeviceService() = default;
 MockDeviceService::~MockDeviceService() = default;
-
-mojo::PendingRemote<mojom::DeviceService> MockDeviceService::PassRemote() {
-  return receiver_.BindNewPipeAndPassRemote();
-}
-
-void MockDeviceService::ResetReceiver() {
-  receiver_.reset();
-}
-
-void MockDeviceService::FlushForTesting() {
-  receiver_.FlushForTesting();
-}
 
 }  // namespace global_media_controls::test
--- components/global_media_controls/public/test/mock_device_service.h
+++ components/global_media_controls/public/test/mock_device_service.h
@@ -14,19 +14,40 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace global_media_controls::test {
 
-class MockDeviceService : public mojom::DeviceService {
+template <class T>
+class MockMojoImpl : public T {
+ public:
+  MockMojoImpl() = default;
+  ~MockMojoImpl() override = default;
+
+  mojo::PendingRemote<T> PassRemote() {
+    return receiver_.BindNewPipeAndPassRemote();
+  }
+
+  void BindReceiver(mojo::PendingReceiver<T> pending_receiver) {
+    receiver_.Bind(std::move(pending_receiver));
+  }
+
+  void ResetReceiver() { receiver_.reset(); }
+
+  void FlushForTesting() { receiver_.FlushForTesting(); }
+
+ protected:
+  mojo::Receiver<T> receiver_{this};
+};
+
+class MockDeviceListHost : public MockMojoImpl<mojom::DeviceListHost> {
+ public:
+  MockDeviceListHost();
+  ~MockDeviceListHost() override;
+
+  MOCK_METHOD(void, SelectDevice, (const std::string& device_id));
+};
+
+class MockDeviceService : public MockMojoImpl<mojom::DeviceService> {
  public:
   MockDeviceService();
   ~MockDeviceService() override;
-
-  // Returns a remote bound to `this`.
-  mojo::PendingRemote<mojom::DeviceService> PassRemote();
-
-  // Resets the Mojo receiver bound to `this`.
-  void ResetReceiver();
-
-  // Flushes the Mojo receiver bound to `this`.
-  void FlushForTesting();
 
   MOCK_METHOD(void,
               GetDeviceListHostForSession,
@@ -41,9 +62,6 @@ class MockDeviceService : public mojom::DeviceService {
       void,
       SetDevicePickerProvider,
       (mojo::PendingRemote<mojom::DevicePickerProvider> provider_remote));
-
- private:
-  mojo::Receiver<mojom::DeviceService> receiver_{this};
 };
 
 }  // namespace global_media_controls::test
