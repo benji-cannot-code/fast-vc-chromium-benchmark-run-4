cd2a3f65668a9b25d0d121cf85af33e1c84e3520
Aryan Kaushik <aryankaushik2023@gmail.com>
2023-10-23 15:56:54 +0000

    [Policy] Unify export policy logic across platforms
    The export policy logic was different based on OS (Desktop and Android).
    The WebUI download logic (Downloading using TS) can work on all of them.
    Thus, combining it can make it easier to maintain.
    
    R=esalma@google.com
    
    Bug: 1228691
    Change-Id: I9173ab938d42729f8f15de498d0627b222eee66a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4904002
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Reviewed-by: Salma Elmahallawy <esalma@google.com>
    Commit-Queue: Aryan Kaushik <aryankaushik2023@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1213532}

--- chrome/browser/ui/webui/policy/policy_ui_handler.h
+++ chrome/browser/ui/webui/policy/policy_ui_handler.h
@@ -23,14 +23,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui_data_source.h"
 #include "content/public/browser/web_ui_message_handler.h"
 #include "extensions/buildflags/buildflags.h"
-#include "ui/shell_dialogs/select_file_dialog.h"
 
 class PrefChangeRegistrar;
 
 // The JavaScript message handler for the chrome://policy page.
-class PolicyUIHandler : public content::WebUIMessageHandler,
-                        public policy::PolicyValueAndStatusAggregator::Observer,
-                        public ui::SelectFileDialog::Listener {
+class PolicyUIHandler
+    : public content::WebUIMessageHandler,
+      public policy::PolicyValueAndStatusAggregator::Observer {
  public:
   PolicyUIHandler();
 
@@ -49,13 +48,6 @@ class PolicyUIHandler : public content::WebUIMessageHandler,
   void OnPolicyValueAndStatusChanged() override;
 
   void set_web_ui_for_test(content::WebUI* web_ui) { set_web_ui(web_ui); }
-
- protected:
-  // ui::SelectFileDialog::Listener implementation.
-  void FileSelected(const base::FilePath& path,
-                    int index,
-                    void* params) override;
-  void FileSelectionCanceled(void* params) override;
 
  private:
   void HandleExportPoliciesJson(const base::Value::List& args);
@@ -96,10 +88,6 @@ class PolicyUIHandler : public content::WebUIMessageHandler,
 
   // Build a JSON string of all the policies.
   std::string GetPoliciesAsJson();
-
-  void WritePoliciesToJSONFile(const base::FilePath& path);
-
-  scoped_refptr<ui::SelectFileDialog> export_policies_select_file_dialog_;
 
   std::unique_ptr<policy::PolicyValueAndStatusAggregator>
       policy_value_and_status_aggregator_;
