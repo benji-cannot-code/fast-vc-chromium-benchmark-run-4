4b11f25ddf248cf263aa78f0055c8f0f8d13194f
Roland Bock <rbock@google.com>
2023-10-23 12:14:27 +0000

    Remove unused OnInvalidatorClientIdChange
    This function is not implemented by any non-test handler, and the only
    test implementation was used to test itself.
    
    Bug: None
    Change-Id: Iad79b441da0a198c69bca8cd795a78aa20ceded0
    Tests: Modified unit tests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4964943
    Commit-Queue: Roland Bock <rbock@google.com>
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1213438}

--- components/invalidation/public/invalidation_handler.cc
+++ components/invalidation/public/invalidation_handler.cc
@@ -7,9 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace invalidation {
 
-void InvalidationHandler::OnInvalidatorClientIdChange(
-    const std::string& client_id) {}
-
 bool InvalidationHandler::IsPublicTopic(const Topic& topic) const {
   return false;
 }
