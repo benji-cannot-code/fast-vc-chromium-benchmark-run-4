7948367fa1781db2463d2a97e6596896f9b13be7
Brandon Wylie <wylieb@chromium.org>
2023-10-23 22:49:21 +0000

    [bookmarks] Record when the shopping filter is shown
    OBSOLETE_HISTOGRAM[Bookmarks.MobileBookmarkManager.FilterUsed]=Changed to Bookmarks.MobileBookmarkManager.FilterUsed2
    
    Bug: 1491549
    Change-Id: I10dfc138e425cefd8bfe6e4a7c784a345e5b52ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4927931
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213799}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerMediator.java
@@ -1547,6 +1547,10 @@ class BookmarkManagerMediator
         if (!filterVisible && powerFilter.contains(PowerBookmarkType.SHOPPING)) {
             onShoppingFilterToggle(false);
         }
+
+        if (filterVisible) {
+            BookmarkMetrics.reportBookmarkManagerFilterShown(BookmarkManagerFilter.SHOPPING);
+        }
     }
 
     // Testing methods.
