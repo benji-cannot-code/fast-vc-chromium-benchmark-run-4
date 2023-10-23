483bf1d45bf8babc9ef7d9d1e522be6ef9471475
Aliona DANGLA <alionadangla@google.com>
2023-10-23 17:50:58 +0000

    [iOS] Rename DisabledTabViewController into DisabledGridViewController
    Follow-up from this CL ->
    https://chromium-review.googlesource.com/c/chromium/src/+/4946712/comment/6c734a56_8392fd29/
    Where the file of DisableTabViewController been moved and renamed.
    
    Bug: 1457146
    Change-Id: I1a8641628f1a89b9a306df354f49da8c11eab774
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4956519
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1213605}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/disabled_grid_view_controller.h
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/disabled_grid_view_controller.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class GURL;
 
 // Delegate for this view controller to handle user click actions.
-@protocol DisabledTabViewControllerDelegate
+@protocol DisabledGridViewControllerDelegate
 
 // Notifies the delegate that the user tapped a link with a `URL`.
 - (void)didTapLinkWithURL:(const GURL&)URL;
@@ -25,9 +25,9 @@ class GURL;
 
 // View controller representing a view without tab grids when any of the
 // incognito tab grid, regular tab grid, and remote tab is disabled.
-@interface DisabledTabViewController : UIViewController
+@interface DisabledGridViewController : UIViewController
 
-@property(nonatomic, weak) id<DisabledTabViewControllerDelegate> delegate;
+@property(nonatomic, weak) id<DisabledGridViewControllerDelegate> delegate;
 
 // Init with page type, which decides the displayed text.
 - (instancetype)initWithPage:(TabGridPage)page NS_DESIGNATED_INITIALIZER;
--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/disabled_grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/disabled_grid_view_controller.mm
@@ -24,7 +24,7 @@ namespace {
 const CGFloat kVerticalMargin = 16.0;
 }  // namespace
 
-@interface DisabledTabViewController () <UITextViewDelegate>
+@interface DisabledGridViewController () <UITextViewDelegate>
 
 @property(nonatomic, strong) UIScrollView* scrollView;
 @property(nonatomic, strong) NSLayoutConstraint* scrollViewHeight;
@@ -33,7 +33,7 @@ const CGFloat kVerticalMargin = 16.0;
 
 @end
 
-@implementation DisabledTabViewController
+@implementation DisabledGridViewController
 
 namespace {
 // Create a NSString for the title based on `page`.
