8753a4c45c3864b00731e992cf94b037914a1209
Peter Kasting <pkasting@chromium.org>
2023-10-23 19:27:30 +0000

    Make use of UnlocalizedTimeFormatWithPattern().
    Bug: none
    Change-Id: Id89082d02556fb53b3a04bef2316f59c7d0ce444
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4962516
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213673}

--- components/sync/base/time.cc
+++ components/sync/base/time.cc
@@ -5,13 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/sync/base/time.h"
 
-#include <memory>
-
-#include "base/check.h"
-#include "base/i18n/unicodestring.h"
-#include "base/strings/utf_string_conversions.h"
-#include "third_party/icu/source/common/unicode/utypes.h"
-#include "third_party/icu/source/i18n/unicode/smpdtfmt.h"
+#include "base/i18n/time_formatting.h"
+#include "base/time/time.h"
 
 namespace syncer {
 
@@ -24,16 +19,7 @@ base::Time ProtoTimeToTime(int64_t proto_t) {
 }
 
 std::string GetTimeDebugString(const base::Time& t) {
-  // Note: We don't use some helper from base/i18n/time_formatting.h here,
-  // because those are all locale-dependent which we explicitly don't want.
-  UErrorCode status = U_ZERO_ERROR;
-  icu::SimpleDateFormat formatter(icu::UnicodeString("yyyy-MM-dd HH:mm:ss X"),
-                                  status);
-  DCHECK(U_SUCCESS(status));
-  icu::UnicodeString date_string;
-  formatter.format(static_cast<UDate>(t.InSecondsFSinceUnixEpoch() * 1000),
-                   date_string);
-  return base::UTF16ToUTF8(base::i18n::UnicodeStringToString16(date_string));
+  return base::UnlocalizedTimeFormatWithPattern(t, "yyyy-MM-dd HH:mm:ss X");
 }
 
 }  // namespace syncer
