970347a8b2856f72d02a27a8b62793e962c02a31
Darren Shen <shend@chromium.org>
2023-10-23 00:37:23 +0000

    Cache the surrounding text like we cache the caret bounds.
    We do not want surrounding text from input fields inside the editor to
    interfere with the surrounding text text in the original input field.
    
    A simple way is to track the surrounding text in Chromium but only send
    a snapshot of it when the UI is shown. We do something similar with the
    caret bounds, so reuse that logic.
    
    Bug: b/301518187
    Change-Id: I82800fb8ce7584b0b0f22b6be59f303293bc3f85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4958093
    Auto-Submit: Darren Shen <shend@chromium.org>
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213283}

--- chrome/browser/ash/input_method/editor_mediator.cc
+++ chrome/browser/ash/input_method/editor_mediator.cc
@@ -127,11 +127,6 @@ void EditorMediator::BindEditorPanelManager(
 }
 
 void EditorMediator::OnFocus(int context_id) {
-  if (mako_bubble_coordinator_.IsShowingUI() ||
-      panel_manager_.IsEditorMenuVisible()) {
-    return;
-  }
-
   if (IsAllowedForUse() && !editor_service_connector_.IsBound()) {
     SetUpNewEditorService();
   }
@@ -145,11 +140,6 @@ void EditorMediator::OnFocus(int context_id) {
 }
 
 void EditorMediator::OnBlur() {
-  if (mako_bubble_coordinator_.IsShowingUI() ||
-      panel_manager_.IsEditorMenuVisible()) {
-    return;
-  }
-
   if (text_actuator_ != nullptr) {
     text_actuator_->OnBlur();
   }
@@ -173,14 +163,8 @@ void EditorMediator::OnTabletControllerDestroyed() {
 
 void EditorMediator::OnSurroundingTextChanged(const std::u16string& text,
                                               gfx::Range selection_range) {
-  if (mako_bubble_coordinator_.IsShowingUI() ||
-      panel_manager_.IsEditorMenuVisible()) {
-    return;
-  }
+  surrounding_text_ = {.text = text, .selection_range = selection_range};
 
-  if (editor_event_proxy_ != nullptr) {
-    editor_event_proxy_->OnSurroundingTextChanged(text, selection_range);
-  }
   size_t selected_length = NonWhitespaceAndSymbolsLength(text, selection_range);
   editor_switch_->OnTextSelectionLengthChanged(selected_length);
 }
@@ -226,8 +210,12 @@ void EditorMediator::HandleTrigger(
   }
 }
 
-void EditorMediator::CacheContextCaretBounds() {
+void EditorMediator::CacheContext() {
   mako_bubble_coordinator_.CacheContextCaretBounds();
+  if (editor_event_proxy_ != nullptr) {
+    editor_event_proxy_->OnSurroundingTextChanged(
+        surrounding_text_.text, surrounding_text_.selection_range);
+  }
 }
 
 void EditorMediator::OnTextInserted() {
--- chrome/browser/ash/input_method/editor_mediator.h
+++ chrome/browser/ash/input_method/editor_mediator.h
@@ -76,7 +76,7 @@ class EditorMediator
       absl::optional<std::string_view> preset_query_id = absl::nullopt,
       absl::optional<std::string_view> freeform_text = absl::nullopt) override;
   EditorMode GetEditorMode() const override;
-  void CacheContextCaretBounds() override;
+  void CacheContext() override;
 
   // TabletModeObserver:
   void OnTabletModeStarting() override;
@@ -102,6 +102,11 @@ class EditorMediator
   EditorPanelManager& panel_manager() { return panel_manager_; }
 
  private:
+  struct SurroundingText {
+    std::u16string text;
+    gfx::Range selection_range;
+  };
+
   void OnTextFieldContextualInfoChanged(const TextFieldContextualInfo& info);
 
   void SetUpNewEditorService();
@@ -126,6 +131,8 @@ class EditorMediator
   std::unique_ptr<EditorClientConnector> editor_client_connector_;
   std::unique_ptr<EditorTextQueryProvider> text_query_provider_;
   std::unique_ptr<EditorTextActuator> text_actuator_;
+
+  SurroundingText surrounding_text_;
 
   base::ScopedObservation<Profile, ProfileObserver> profile_observation_{this};
 
