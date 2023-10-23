c71a712fbf492aa705307afd510cee286fe6770a
Jood Hajeer <jood@google.com>
2023-10-23 17:13:35 +0000

    [iOS] EGtest: Minor cleanup in AccountsTableTestCase.
    This CL has minor cleanups. And one small fix that might reduce flakiness
    in testSignInPopUpAccountOnSyncSettings.
    
    This CL includes:
    * Enabling kReplaceSyncPromosWithSignInPromos by default as fieldtrial
    features are not enabled in official builds.
    * Removing duplicate call to addFakeIdentity before
    signinWithFakeIdentity as it already calls that. Except for the tests
    that require having muplitple identities on device before signing in.
    * Add waitForAppToIdle after each forgetFakeIdentity. This change will
    probably reduce flakiness in testSignInPopUpAccountOnSyncSettings.
    
    Bug: 1493677
    Change-Id: Ic9e7ab1d524d1b79d1e564e9de7b28583165441f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4955824
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1213577}

--- ios/chrome/browser/ui/settings/google_services/accounts_table_egtest.mm
+++ ios/chrome/browser/ui/settings/google_services/accounts_table_egtest.mm
@@ -98,6 +98,9 @@ constexpr base::TimeDelta kSyncOperationTimeout = base::Seconds(10);
       [self isRunningTest:@selector(testSignOutFooterForSignInAndSyncUser)]) {
     config.features_disabled.push_back(
         syncer::kReplaceSyncPromosWithSignInPromos);
+  } else {
+    config.features_enabled.push_back(
+        syncer::kReplaceSyncPromosWithSignInPromos);
   }
 
   return config;
@@ -139,6 +142,7 @@ constexpr base::TimeDelta kSyncOperationTimeout = base::Seconds(10);
   // Forget `fakeIdentity`, screens should be popped back to the Main Settings.
   [ChromeEarlGreyUI waitForAppToIdle];
   [SigninEarlGrey forgetFakeIdentity:fakeIdentity];
+  [ChromeEarlGreyUI waitForAppToIdle];
 
   [[EarlGrey selectElementWithMatcher:SettingsSignInRowMatcher()]
       assertWithMatcher:grey_sufficientlyVisible()];
@@ -152,7 +156,6 @@ constexpr base::TimeDelta kSyncOperationTimeout = base::Seconds(10);
 // account is removed while the "Disconnect Account" dialog is up.
 - (void)testSignInPopUpAccountOnDisconnectAccount {
   FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeIdentity1];
-  [SigninEarlGrey addFakeIdentity:fakeIdentity];
 
   // Sign In `fakeIdentity`, then open the Account Settings.
   [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity];
@@ -162,6 +165,7 @@ constexpr base::TimeDelta kSyncOperationTimeout = base::Seconds(10);
   // Forget `fakeIdentity`, screens should be popped back to the Main Settings.
   [ChromeEarlGreyUI waitForAppToIdle];
   [SigninEarlGrey forgetFakeIdentity:fakeIdentity];
+  [ChromeEarlGreyUI waitForAppToIdle];
 
   [ChromeEarlGrey
       waitForSufficientlyVisibleElementWithMatcher:SettingsSignInRowMatcher()];
@@ -365,7 +369,6 @@ constexpr base::TimeDelta kSyncOperationTimeout = base::Seconds(10);
 - (void)testSwitchingAccountsWithClearedData {
   FakeSystemIdentity* fakeIdentity1 = [FakeSystemIdentity fakeIdentity1];
   FakeSystemIdentity* fakeIdentity2 = [FakeSystemIdentity fakeIdentity2];
-  [SigninEarlGrey addFakeIdentity:fakeIdentity1];
   [SigninEarlGrey addFakeIdentity:fakeIdentity2];
 
   [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity1];
@@ -440,7 +443,6 @@ constexpr base::TimeDelta kSyncOperationTimeout = base::Seconds(10);
 // account while the dialog is still opened.
 - (void)testRemovePrimaryAccountWhileSignOutConfirmation {
   FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeIdentity1];
-  [SigninEarlGrey addFakeIdentity:fakeIdentity];
 
   [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity];
 
@@ -468,7 +470,6 @@ constexpr base::TimeDelta kSyncOperationTimeout = base::Seconds(10);
 - (void)testRemoveSecondaryAccountWhileSignOutConfirmation {
   FakeSystemIdentity* fakeIdentity1 = [FakeSystemIdentity fakeIdentity1];
   FakeSystemIdentity* fakeIdentity2 = [FakeSystemIdentity fakeIdentity2];
-  [SigninEarlGrey addFakeIdentity:fakeIdentity1];
   [SigninEarlGrey addFakeIdentity:fakeIdentity2];
 
   [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity1];
@@ -499,7 +500,6 @@ constexpr base::TimeDelta kSyncOperationTimeout = base::Seconds(10);
 // in appConfigurationForTestCase.
 - (void)MAYBE_testInterruptDuringSignOutConfirmation {
   FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeIdentity1];
-  [SigninEarlGrey addFakeIdentity:fakeIdentity];
 
   [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity];
 
@@ -523,7 +523,6 @@ constexpr base::TimeDelta kSyncOperationTimeout = base::Seconds(10);
 // not affect the user's sign-in state.
 - (void)testDismissSignOutConfirmationTwice {
   FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeIdentity1];
-  [SigninEarlGrey addFakeIdentity:fakeIdentity];
 
   [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity];
 
@@ -557,7 +556,6 @@ constexpr base::TimeDelta kSyncOperationTimeout = base::Seconds(10);
 // Tests to sign out with a non managed account without syncing.
 - (void)testSignOutWithNonManagedAccountFromNoneSyncingAccount {
   FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeIdentity1];
-  [SigninEarlGrey addFakeIdentity:fakeIdentity];
 
   [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity enableSync:NO];
 
@@ -601,7 +599,6 @@ constexpr base::TimeDelta kSyncOperationTimeout = base::Seconds(10);
 // Tests that the sign-out footer is not shown when the user is not syncing.
 - (void)testSignOutFooterForSignInOnlyUser {
   FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeIdentity1];
-  [SigninEarlGrey addFakeIdentity:fakeIdentity];
 
   [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity enableSync:NO];
   [self openAccountSettings];
@@ -616,7 +613,6 @@ constexpr base::TimeDelta kSyncOperationTimeout = base::Seconds(10);
 // Tests that the sign-out footer is shown when the user is syncing.
 - (void)testSignOutFooterForSignInAndSyncUser {
   FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeIdentity1];
-  [SigninEarlGrey addFakeIdentity:fakeIdentity];
 
   [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity enableSync:YES];
   [self openAccountSettings];
