40db813bcbaec77b6e19cc73a842dd4f72c719ae
Munira Tursunova <moonira@google.com>
2023-10-23 13:30:25 +0000

    Fixed FontPalette hash computation
    FontPalette hash computation didn't take into account interpolable
    values. Also matching_family_name for normal FontPalette was computed
    incorrectly in [0]. This was causing DCHECK crash during resizing
    of the hash_table while inserting new objects in FontFallbackMap,
    compare [1].
    
    This change fixes interpolable FontPalette hash computation and
    insures that matching_family_name of normal FontPalette is empty.
    
    [0] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/css/css_font_selector.cc;l=73?q=ResolveInterpolableFontPalette&sq=&ss=chromium%2Fchromium%2Fsrc
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/fonts/font_fallback_map.cc?q=FontFallbackMap::Get&ss=chromium%2Fchromium%2Fsrc
    
    Bug: 1491006
    Change-Id: I26d023fdaea2f3392e9a9e5d067ef9ffb0c3328c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4953356
    Commit-Queue: Munira Tursunova <moonira@google.com>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213457}

--- third_party/blink/renderer/platform/fonts/font_palette_test.cc
+++ third_party/blink/renderer/platform/fonts/font_palette_test.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/fonts/font_palette.h"
 
+#include "base/memory/scoped_refptr.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/platform/testing/runtime_enabled_features_test_helpers.h"
 
@@ -25,6 +26,31 @@ TEST(FontPaletteTest, HashingAndComparison) {
   b = FontPalette::Create(AtomicString("SomePaletteReference"));
   EXPECT_NE(a->GetHash(), b->GetHash());
   EXPECT_NE(a, b);
+
+  b = FontPalette::Mix(FontPalette::Create(FontPalette::kLightPalette),
+                       FontPalette::Create(FontPalette::kDarkPalette), 30, 70,
+                       0.7, 1.0, Color::ColorSpace::kSRGB, absl::nullopt);
+  EXPECT_NE(a->GetHash(), b->GetHash());
+  EXPECT_NE(a, b);
+
+  scoped_refptr<FontPalette> c =
+      FontPalette::Mix(FontPalette::Create(FontPalette::kLightPalette),
+                       FontPalette::Create(FontPalette::kDarkPalette), 15, 35,
+                       0.7, 1.0, Color::ColorSpace::kSRGB, absl::nullopt);
+  EXPECT_NE(c->GetHash(), b->GetHash());
+  EXPECT_NE(c, b);
+
+  c = FontPalette::Mix(FontPalette::Create(FontPalette::kLightPalette),
+                       FontPalette::Create(), 30, 70, 0.7, 1.0,
+                       Color::ColorSpace::kSRGB, absl::nullopt);
+  EXPECT_NE(c->GetHash(), b->GetHash());
+  EXPECT_NE(c, b);
+
+  c = FontPalette::Mix(FontPalette::Create(FontPalette::kLightPalette),
+                       FontPalette::Create(FontPalette::kDarkPalette), 30, 70,
+                       0.7, 1.0, Color::ColorSpace::kOklab, absl::nullopt);
+  EXPECT_NE(c->GetHash(), b->GetHash());
+  EXPECT_NE(c, b);
 }
 
 TEST(FontPaletteTest, MixPaletteValue) {
