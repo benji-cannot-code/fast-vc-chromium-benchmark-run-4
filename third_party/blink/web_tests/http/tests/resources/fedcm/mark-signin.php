b58abdb9aa27030775ee48dc61b3387536150c09
Christian Biesinger <cbiesinger@chromium.org>
2023-10-23 15:20:12 +0000

    [FedCM] Remove support for the old header syntax
    Only supports the new, standard syntax:
    https://fedidcg.github.io/FedCM/#login-status-http
    
    R=yigu@chromium.org
    
    Bug: 1381501
    Change-Id: I16a62a98104ea6ca5dc0d429493bb2e50f01ec3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4957073
    Auto-Submit: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Vladimir Nechaev <nechaev@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213514}

--- third_party/blink/web_tests/http/tests/resources/fedcm/mark-signin.php
+++ third_party/blink/web_tests/http/tests/resources/fedcm/mark-signin.php
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
-header("Idp-Signin-Status: action=signin");
+header("Set-Login: logged-in");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
 ?>
--- third_party/blink/web_tests/http/tests/resources/fedcm/mark-signout.php
+++ third_party/blink/web_tests/http/tests/resources/fedcm/mark-signout.php
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<?php header("Idp-Signin-Status: action=signout-all") ?>
+<?php header("Set-Login: logged-out") ?>
 Header sent.
