a43d110dec7c94b89f4a94c12ed66aa576196441
Katie Dektar <katie@chromium.org>
2023-10-23 17:20:21 +0000

    [A11y tests] Remove ash browser test helper
    This helper was used for getting information from the ash browser
    during browsertests, but has been replaced by AutomationTestUtils.
    Removing this deprecated helper.
    
    Bug: b/301475127
    Change-Id: I5cd799a0035083975c5fa666fbece03e477ac755
    AX-Relnotes: N/A
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4959873
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213581}

--- chrome/browser/ash/accessibility/magnification_controller_browsertest.cc
+++ chrome/browser/ash/accessibility/magnification_controller_browsertest.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/accessibility/accessibility_test_utils.h"
 #include "chrome/browser/ash/accessibility/automation_test_utils.h"
 #include "chrome/browser/ash/accessibility/fullscreen_magnifier_test_helper.h"
-#include "chrome/browser/ash/accessibility/html_test_utils.h"
 #include "chrome/browser/ash/accessibility/magnification_manager.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/common/extensions/extension_constants.h"
