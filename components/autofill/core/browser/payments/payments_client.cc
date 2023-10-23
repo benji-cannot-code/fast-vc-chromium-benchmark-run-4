177e7bd8817059d2d64b9e80d616956704ca9566
Qihui Zhao <qihuizhao@google.com>
2023-10-23 16:53:28 +0000

    Create GetIbanUploadDetailsRequest to PaymentsClient
    This CL creates GetIbanUploadDetailsRequest and add
    GetIbanUploadDetails API to PaymentsClient, so that it
    can send GetIbanUploadDetails request.
    
    Bug: b:296651966
    Change-Id: I14d8049c1c68c9132f72660d1f8bfe24c737c9ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4928715
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1213568}

--- components/autofill/core/browser/payments/payments_client.cc
+++ components/autofill/core/browser/payments/payments_client.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/payments/account_info_getter.h"
 #include "components/autofill/core/browser/payments/client_behavior_constants.h"
 #include "components/autofill/core/browser/payments/payments_requests/get_details_for_enrollment_request.h"
+#include "components/autofill/core/browser/payments/payments_requests/get_iban_upload_details_request.h"
 #include "components/autofill/core/browser/payments/payments_requests/get_unmask_details_request.h"
 #include "components/autofill/core/browser/payments/payments_requests/get_upload_details_request.h"
 #include "components/autofill/core/browser/payments/payments_requests/opt_change_request.h"
@@ -328,6 +329,19 @@ void PaymentsClient::UploadCard(
   IssueRequest(std::make_unique<UploadCardRequest>(
       request_details,
       account_info_getter_->IsSyncFeatureEnabledForPaymentsServerMetrics(),
+      std::move(callback)));
+}
+
+void PaymentsClient::GetIbanUploadDetails(
+    const std::string& app_locale,
+    int64_t billing_customer_number,
+    int billable_service_number,
+    base::OnceCallback<void(AutofillClient::PaymentsRpcResult,
+                            const std::u16string&,
+                            std::unique_ptr<base::Value::Dict>)> callback) {
+  IssueRequest(std::make_unique<GetIbanUploadDetailsRequest>(
+      account_info_getter_->IsSyncFeatureEnabledForPaymentsServerMetrics(),
+      app_locale, billing_customer_number, billable_service_number,
       std::move(callback)));
 }
 
