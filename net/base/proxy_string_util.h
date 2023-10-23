0fdb3d90d9f796933f371c373ff315281f324983
Andrew Williams <awillia@chromium.org>
2023-10-23 19:56:51 +0000

    [IP Protection] Add PacResultElementToProxyChain helper
    Adds new PacResultElementToProxyChain and
    ProxyChainToPacResultElement helpers as part of the
    transition to using ProxyChains instead of ProxyServers
    directly (to support nested proxies).
    
    These helpers currently only support single-hop
    proxies.
    
    Bug: 1491092
    Change-Id: I7bf32a316c13a83725ff0e457877e09dd665a673
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4967813
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Andrew Williams <awillia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213696}

--- net/base/proxy_string_util.h
+++ net/base/proxy_string_util.h
@@ -16,8 +16,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 
 // Converts a PAC result element (commonly called a PAC string) to/from a
-// ProxyServer. Note that this only deals with a single proxy server element
-// separated out from the complete semicolon-delimited PAC result string.
+// ProxyServer / ProxyChain. Note that this only deals with a single proxy
+// element separated out from the complete semicolon-delimited PAC result
+// string.
+//
+// TODO(https://crbug.com/1491092): Update this once we add support for parsing
+// multi-hop proxies into a ProxyChain. Also, remove the ProxyServer functions
+// once we've updated the code to use ProxyChains instead (assuming these are no
+// longer needed).
 //
 // PAC result elements have the format:
 // <scheme>" "<host>[":"<port>]
@@ -34,8 +40,7 @@ namespace net {
 // If <port> is omitted, it will be assumed as the default port for the
 // chosen scheme (via ProxyServer::GetDefaultPortForScheme()).
 //
-// If parsing fails the returned proxy will have scheme
-// `ProxyServer::SCHEME_INVALID`.
+// Returns an invalid ProxyServer / ProxyChain if parsing fails.
 //
 // Examples:
 //   "PROXY foopy:19"   {scheme=HTTP, host="foopy", port=19}
@@ -44,8 +49,12 @@ namespace net {
 //   "HTTPS foopy:123"  {scheme=HTTPS, host="foopy", port=123}
 //   "QUIC foopy:123"   {scheme=QUIC, host="foopy", port=123}
 //   "BLAH xxx:xx"      INVALID
+NET_EXPORT ProxyChain
+PacResultElementToProxyChain(base::StringPiece pac_result_element);
 NET_EXPORT ProxyServer
 PacResultElementToProxyServer(base::StringPiece pac_result_element);
+NET_EXPORT std::string ProxyChainToPacResultElement(
+    const ProxyChain& proxy_chain);
 NET_EXPORT std::string ProxyServerToPacResultElement(
     const ProxyServer& proxy_server);
 
