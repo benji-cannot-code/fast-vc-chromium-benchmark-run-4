39553688725275bdf6f8990725f29898eeff551f
Salvador Guerrero Ramos <salg@google.com>
2023-10-23 22:35:34 +0000

    Fixed translate button showing on all pages
    The translate toolbar button is currently appearing on all pages
    regardless of the protocol, meaning it appears on pages such as
    about:blank and chrome://version.
    
    This CL restricts the translate button to http and https pages.
    
    Bug: 1492094
    Change-Id: I2b388557a90451e1a120802cd2e01c6d447da67b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4968079
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213790}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/adaptive/TranslateToolbarButtonControllerUnitTest.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/adaptive/TranslateToolbarButtonControllerUnitTest.java
@@ -30,6 +30,7 @@ import org.chromium.chrome.browser.translate.TranslateBridgeJni;
 import org.chromium.components.feature_engagement.EventConstants;
 import org.chromium.components.feature_engagement.Tracker;
 import org.chromium.content_public.browser.WebContents;
+import org.chromium.url.JUnitTestGURLs;
 
 /** Unit tests for {@link TranslateToolbarButtonController} */
 @RunWith(BaseRobolectricTestRunner.class)
@@ -52,6 +53,7 @@ public class TranslateToolbarButtonControllerUnitTest {
         mActionTester = new UserActionTester();
 
         when(mTab.getWebContents()).thenReturn(mWebContents);
+        when(mTab.getUrl()).thenReturn(JUnitTestGURLs.EXAMPLE_URL);
     }
 
     @After
@@ -82,5 +84,18 @@ public class TranslateToolbarButtonControllerUnitTest {
         verify(mTracker)
                 .notifyEvent(EventConstants.ADAPTIVE_TOOLBAR_CUSTOMIZATION_TRANSLATE_OPENED);
         verify(mMockTranslateBridge).manualTranslateWhenReady(mWebContents);
+    }
+
+    @Test
+    public void testShouldNotShowUpOnNonHttpUrls() {
+        when(mTab.getUrl()).thenReturn(JUnitTestGURLs.CHROME_ABOUT);
+        TranslateToolbarButtonController translateToolbarButtonController =
+                new TranslateToolbarButtonController(
+                        () -> mTab, mDrawable, "Translate button description", () -> mTracker);
+        ButtonData buttonData = translateToolbarButtonController.get(mTab);
+
+        Assert.assertFalse(buttonData.canShow());
+        Assert.assertTrue(buttonData.isEnabled());
+        Assert.assertNotNull(buttonData.getButtonSpec());
     }
 }
