a039eef798c1bf639f84083fbf9d680e728c7547
Devlin Cronin <rdevlin.cronin@chromium.org>
2023-10-23 22:34:26 +0000

    [Extensions] DCHECK if WorkerIdSet contains >1 worker per extension
    In theory, there should only be one registered "extension service
    worker" per extension (that is, the service worker associated with a
    top-level scope for an extension; this worker has access to extension
    APIs and is associated with extension events).
    
    We have a few reports that in exceptionally rare cases, this assumption
    can be violated. Add a DCHECK to gather more data here.
    
    Bug: 1493391
    Change-Id: I537a16aae97c307ba70a8070e7ecd2ffd2169fb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4944950
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213788}

--- extensions/browser/service_worker/worker_id_set.h
+++ extensions/browser/service_worker/worker_id_set.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <vector>
 
+#include "base/auto_reset.h"
 #include "extensions/browser/service_worker/worker_id.h"
 #include "extensions/common/extension_id.h"
 
@@ -34,6 +35,7 @@ class WorkerIdSet {
                                            int render_process_id) const;
 
   std::vector<WorkerId> GetAllForTesting() const;
+  static base::AutoReset<bool> AllowMultipleWorkersPerExtensionForTesting();
   size_t count_for_testing() const { return workers_.size(); }
 
  private:
