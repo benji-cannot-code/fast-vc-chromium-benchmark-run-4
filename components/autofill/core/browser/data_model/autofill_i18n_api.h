779236e1b3251eacb33b86fe93bc1df2dbd34b6d
Jose Dapena Paz <jdapena@igalia.com>
2023-10-24 08:40:03 +0000

    GCC: workaround constexpr declaration for kLegacyHierarchyCountryCode
    GCC fails to compile as a single constexpr initializing
    AddressCountryCode (a base::StrongAlias) from an std::string. To
    avoid that, we define an intermediate constexpr for the std::string
    used fot construct kLegacyHierarchyCountryCode.
    
    Bug: 819294
    Change-Id: Ic0d44a8aa82172fb8f4b4dfad08b310857985790
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4967200
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1214040}

--- components/autofill/core/browser/data_model/autofill_i18n_api.h
+++ components/autofill/core/browser/data_model/autofill_i18n_api.h
@@ -14,9 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace autofill::i18n_model_definition {
 
 // Country code that represents autofill's legacy address hierarchy model as
-// stored `kAutofillModelRules`.
+// stored `kAutofillModelRules`. As a workaround for GCC we declare the
+// std::string constexpr first.
+constexpr inline std::string kLegacyHierarchyCountryCodeString{"XX"};
 constexpr AddressCountryCode kLegacyHierarchyCountryCode =
-    AddressCountryCode("XX");
+    AddressCountryCode(kLegacyHierarchyCountryCodeString);
 
 // Creates an instance of the address hierarchy model corresponding to the
 // provided country. All the nodes have empty values, except for the country
