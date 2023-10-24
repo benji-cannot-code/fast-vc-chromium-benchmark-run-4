a81e3e8a087c3a8eeacee2936989019e5fca9b42
Nicola Tommasi <tommasin@chromium.org>
2023-10-24 05:30:43 +0000

    Tracking Protections: Add Offboarding notice in the settings page
    Screenshot: https://screenshot.googleplex.com/72uqw7TdnnpjRUS
    
    Bug: b:307275613
    Change-Id: I5ff5ca52d52adfbae7a8d04ea93766297241a3b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4966805
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1213985}

--- components/privacy_sandbox/android/javatests/src/org/chromium/components/privacy_sandbox/TrackingProtectionSettingsTest.java
+++ components/privacy_sandbox/android/javatests/src/org/chromium/components/privacy_sandbox/TrackingProtectionSettingsTest.java
@@ -6,8 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.privacy_sandbox;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import static org.mockito.Mockito.when;
@@ -60,6 +63,11 @@ public class TrackingProtectionSettingsTest {
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         mJniMocker.mock(WebsitePreferenceBridgeJni.TEST_HOOKS, mBridgeMock);
+
+        when(mDelegate.getBrowserContext()).thenReturn(mContextHandleMock);
+    }
+
+    private void launchTrackingProtectionSettings() {
         mSettingsRule.launchPreference(
                 TrackingProtectionSettings.class,
                 null,
@@ -68,8 +76,6 @@ public class TrackingProtectionSettingsTest {
                             .setTrackingProtectionDelegate(mDelegate);
                 });
         mFragment = (TrackingProtectionSettings) mSettingsRule.getPreferenceFragment();
-
-        when(mDelegate.getBrowserContext()).thenReturn(mContextHandleMock);
     }
 
     @Test
@@ -77,7 +83,26 @@ public class TrackingProtectionSettingsTest {
     public void testShowTrackingProtectionUi() {
         when(mDelegate.isBlockAll3PCDEnabled()).thenReturn(true);
         when(mDelegate.isDoNotTrackEnabled()).thenReturn(true);
+
+        launchTrackingProtectionSettings();
+
         onView(withText(R.string.privacy_sandbox_tracking_protection_description))
                 .check(matches(isDisplayed()));
+        onView(withId(R.id.preference_card)).check(doesNotExist());
+    }
+
+    @Test
+    @SmallTest
+    public void testShowTrackingProtectionOffboardingCard() {
+        when(mDelegate.isBlockAll3PCDEnabled()).thenReturn(true);
+        when(mDelegate.isDoNotTrackEnabled()).thenReturn(true);
+        when(mDelegate.shouldShowSettingsOffboardingNotice()).thenReturn(true);
+
+        launchTrackingProtectionSettings();
+
+        onView(withId(R.id.preference_card)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.close_icon)).perform(click());
+        onView(withId(R.id.preference_card)).check(doesNotExist());
     }
 }
