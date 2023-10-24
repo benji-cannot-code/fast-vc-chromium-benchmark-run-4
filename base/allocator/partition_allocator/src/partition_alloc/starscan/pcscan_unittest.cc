2df66be6e69ad0971e6fb720ce514c4415cb7ec4
David Benjamin <davidben@chromium.org>
2023-10-24 15:25:39 +0000

    Fix alignment in PartitionAllocPCScanTest.DontScanUnusedRawSize
    The cast is UB because TagAddr(source_end) is not pointer-aligned. Looks
    like this test was added in
    https://chromium-review.googlesource.com/c/chromium/src/+/3009822, which
    I gather was about scanning memory just past some allocation. It is
    probably better for the test to be aligned anyway, since I assume the
    scanner only looks for pointer-aligned pointers.
    
    But I don't understand this code very well, so reviewer please look at
    this carefully and make sure this is the correct fix. Another option
    would be to use memcpy to write the pointer in, but then the pointer
    remains misaligned.
    
    Bug: 1394755
    Change-Id: I48607f12e3154bcb4cc8fff96f2910105b455172
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4968415
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214215}

--- base/allocator/partition_allocator/src/partition_alloc/starscan/pcscan_unittest.cc
+++ base/allocator/partition_allocator/src/partition_alloc/starscan/pcscan_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/src/partition_alloc/starscan/pcscan.h"
 
 #include "base/allocator/partition_allocator/src/partition_alloc/partition_alloc-inl.h"
+#include "base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/bits.h"
 #include "base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/compiler_specific.h"
 #include "base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/cpu.h"
 #include "base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/logging.h"
@@ -675,7 +676,9 @@ TEST_F(PartitionAllocPCScanTest, DontScanUnusedRawSize) {
   // This not only points past the object, but past all extras around it.
   // However, there should be enough space between this and the end of slot, to
   // store some data.
-  uintptr_t source_end = slot_start + slot_span->GetRawSize();
+  uintptr_t source_end =
+      slot_start +
+      base::bits::AlignUp(slot_span->GetRawSize(), alignof(ValueList*));
   // Write the pointer.
   // Since we stripped the MTE-tag to get |slot_start|, we need to retag it.
   *static_cast<ValueList**>(TagAddr(source_end)) = value;
