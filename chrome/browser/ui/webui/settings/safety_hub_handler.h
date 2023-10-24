fe104116a94f6074f69169065674f1c89ea0b656
Aisulu Rakhmetullina <aisulu@google.com>
2023-10-24 19:35:20 +0000

    [SHv2] Add backend logic for entry point UI
    For the Safety Hub entry point in Privacy & Security settings page
    we need to know which modules have recommendations for the user.
    This CL adds a centralised place where all modules can integrate
    to provide that information.
    
    Original CL: https://crrev.com/c/4886807
    Author: ehez@chromium.org
    
    Bug: 1443466
    Change-Id: Idd996ac8fe05fcdda4f250c05eb4329b37bedd98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4931154
    Commit-Queue: Aisulu Rakhmetullina <aisulu@google.com>
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214393}

--- chrome/browser/ui/webui/settings/safety_hub_handler.h
+++ chrome/browser/ui/webui/settings/safety_hub_handler.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_SETTINGS_SAFETY_HUB_HANDLER_H_
 #define CHROME_BROWSER_UI_WEBUI_SETTINGS_SAFETY_HUB_HANDLER_H_
 
+#include <set>
+
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/time/clock.h"
@@ -33,6 +35,15 @@ enum class SafeBrowsingState {
 
 class SafetyHubHandler : public settings::SettingsPageUIHandler {
  public:
+  enum class SafetyHubModule {
+    kExtensions,
+    kNotifications,
+    kPasswords,
+    kSafeBrowsing,
+    kUnusedSitePermissions,
+    kVersion
+  };
+
   explicit SafetyHubHandler(Profile* profile);
 
   ~SafetyHubHandler() override;
@@ -141,11 +152,20 @@ class SafetyHubHandler : public settings::SettingsPageUIHandler {
   // Returns the data for Safe Browsing card.
   void HandleGetSafeBrowsingCardData(const base::Value::List& args);
 
+  // Fetches data for the Safe Browsing card to return data to the UI.
+  base::Value::Dict GetSafeBrowsingCardData();
+
   // Returns the data for the password card.
   void HandleGetPasswordCardData(const base::Value::List& args);
 
+  // Fetches data for the password card to return data to the UI.
+  base::Value::Dict GetPasswordCardData();
+
   // Returns the data for the version card.
   void HandleGetVersionCardData(const base::Value::List& args);
+
+  // Fetches data for the version card to return data to the UI.
+  base::Value::Dict GetVersionCardData();
 
   // Returns true if Safety Hub has recommendations.
   void HandleGetSafetyHubHasRecommendations(const base::Value::List& args);
@@ -155,6 +175,12 @@ class SafetyHubHandler : public settings::SettingsPageUIHandler {
 
   // Sends the list of notification permissions to review to the WebUI.
   void SendNotificationPermissionReviewList();
+
+  // Returns the number of extensions that should be reviewed by the user.
+  int GetNumberOfExtensionsThatNeedReview();
+
+  // Returns the set of Safety Hub modules which require the user's attention.
+  std::set<SafetyHubModule> GetSafetyHubModulesWithRecommendations();
 
   const raw_ptr<Profile, DanglingUntriaged> profile_;
 
