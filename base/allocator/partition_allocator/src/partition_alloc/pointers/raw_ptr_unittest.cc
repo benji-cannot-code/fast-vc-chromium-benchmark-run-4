46c7ba02f5edd874e7b99d9bc9a711ccae1c52b6
David Benjamin <davidben@chromium.org>
2023-10-24 05:16:49 +0000

    Use an unsigned type in BackupRefPtrTest.SpatialAlgoCompat
    The std::generate call ends up overflowing an int fairly early. As far
    as I can tell, this is just trying to deterministically fill a buffer
    with some data and the exact type doesn't matter? In that case, use
    uint32_t, not int, so we get defined overflow.
    
    Bug: 1394755
    Change-Id: I6513430dfc43a90e542695539d8759b58553a000
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4969093
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213979}

--- base/allocator/partition_allocator/src/partition_alloc/pointers/raw_ptr_unittest.cc
+++ base/allocator/partition_allocator/src/partition_alloc/pointers/raw_ptr_unittest.cc
@@ -2086,13 +2086,15 @@ TEST_F(BackupRefPtrTest, SpatialAlgoCompat) {
   ASSERT_EQ(
       requested_size,
       allocator_.root()->AllocationCapacityFromRequestedSize(requested_size));
-  size_t requested_elements = requested_size / sizeof(int);
+  size_t requested_elements = requested_size / sizeof(uint32_t);
 
-  int* ptr = reinterpret_cast<int*>(allocator_.root()->Alloc(requested_size));
-  int* ptr_end = ptr + requested_elements;
+  uint32_t* ptr =
+      reinterpret_cast<uint32_t*>(allocator_.root()->Alloc(requested_size));
+  uint32_t* ptr_end = ptr + requested_elements;
 
-  CountingRawPtr<int> protected_ptr = ptr;
-  CountingRawPtr<int> protected_ptr_end = protected_ptr + requested_elements;
+  CountingRawPtr<uint32_t> protected_ptr = ptr;
+  CountingRawPtr<uint32_t> protected_ptr_end =
+      protected_ptr + requested_elements;
 
 #if BUILDFLAG(BACKUP_REF_PTR_POISON_OOB_PTR)
   EXPECT_DEATH_IF_SUPPORTED(*protected_ptr_end = 1, "");
@@ -2100,7 +2102,7 @@ TEST_F(BackupRefPtrTest, SpatialAlgoCompat) {
 
   RawPtrCountingImpl::ClearCounters();
 
-  int gen_val = 1;
+  uint32_t gen_val = 1;
   std::generate(protected_ptr, protected_ptr_end, [&gen_val]() {
     gen_val ^= gen_val + 1;
     return gen_val;
@@ -2115,7 +2117,7 @@ TEST_F(BackupRefPtrTest, SpatialAlgoCompat) {
 
   RawPtrCountingImpl::ClearCounters();
 
-  for (CountingRawPtr<int> protected_ptr_i = protected_ptr;
+  for (CountingRawPtr<uint32_t> protected_ptr_i = protected_ptr;
        protected_ptr_i < protected_ptr_end; protected_ptr_i++) {
     *protected_ptr_i ^= *protected_ptr_i + 1;
   }
@@ -2129,7 +2131,7 @@ TEST_F(BackupRefPtrTest, SpatialAlgoCompat) {
 
   RawPtrCountingImpl::ClearCounters();
 
-  for (CountingRawPtr<int> protected_ptr_i = protected_ptr;
+  for (CountingRawPtr<uint32_t> protected_ptr_i = protected_ptr;
        protected_ptr_i < ptr_end; protected_ptr_i++) {
     *protected_ptr_i ^= *protected_ptr_i + 1;
   }
@@ -2143,7 +2145,7 @@ TEST_F(BackupRefPtrTest, SpatialAlgoCompat) {
 
   RawPtrCountingImpl::ClearCounters();
 
-  for (int* ptr_i = ptr; ptr_i < protected_ptr_end; ptr_i++) {
+  for (uint32_t* ptr_i = ptr; ptr_i < protected_ptr_end; ptr_i++) {
     *ptr_i ^= *ptr_i + 1;
   }
 
@@ -2157,7 +2159,7 @@ TEST_F(BackupRefPtrTest, SpatialAlgoCompat) {
   RawPtrCountingImpl::ClearCounters();
 
   size_t iter_cnt = 0;
-  for (int *ptr_i = protected_ptr, *ptr_i_end = protected_ptr_end;
+  for (uint32_t *ptr_i = protected_ptr, *ptr_i_end = protected_ptr_end;
        ptr_i < ptr_i_end; ptr_i++) {
     *ptr_i ^= *ptr_i + 1;
     iter_cnt++;
