cbe18b7669b2a68d246e7c4832599818003399c9
Jiewei Qian <qjw@chromium.org>
2023-10-24 23:54:09 +0000

    Revert "Correct mini player colors"
    This reverts commit 45cf4e01a9367523a1659921ebe4502c6899f3e5.
    
    Causing compile failure: https://ci.chromium.org/ui/p/chromium/builders/ci/Android%20arm64%20Builder%20All%20Targets%20(dbg)/20340/overview
    
    Original change's description:
    > Correct mini player colors
    >
    > This change brings the mini player UI colors in line with mocks.
    >
    > - Surface colors are dynamic and need to be set in code rather than XML. Also different "elevations" are needed for light and dark mode.
    > - The progress bar color matches the icon tint in light mode, but should be a light primary in dark mode (baseline_primary_80).
    > - readaloud_button_primary and readaloud_button_secondary look good in both light and dark modes by alpha blending with the background.
    >
    > Light mode screenshot: http://shortn/_9gOSRopcNR
    > Dark mode screenshot: http://shortn/_Ljb2RWONie
    >
    > Bug: b/301309267
    > Change-Id: I78bd60eaccc7f14c25909deef3098c074d0b008e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4968193
    > Reviewed-by: Sky Malice <skym@chromium.org>
    > Reviewed-by: Basia Zimirska <basiaz@google.com>
    > Commit-Queue: Ian Wells <iwells@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1214484}
    
    Bug: b/301309267
    Change-Id: Iad80c12ad676173f72a91dda2456cbc316b0a837
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4975176
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Jiewei Qian <qjw@chromium.org>
    Auto-Submit: Jiewei Qian <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214530}

--- chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/player/mini/MiniPlayerLayoutUnitTest.java
+++ chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/player/mini/MiniPlayerLayoutUnitTest.java
@@ -161,7 +161,7 @@ public class MiniPlayerLayoutUnitTest {
         mLayout.setInteractionHandler(mInteractionHandler);
         mLayout.onPlaybackStateChanged(PlaybackListener.State.PLAYING);
 
-        assertTrue(mLayout.findViewById(R.id.mini_player_container).performClick());
+        assertTrue(mLayout.findViewById(R.id.mini_player_background).performClick());
         verify(mInteractionHandler).onMiniPlayerExpandClick();
     }
 
