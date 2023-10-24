07a7fbdd0195ae23c34c6ec8fc8b4fbbd2866f0f
YuhanYang <yyhyyh@google.com>
2023-10-24 20:59:20 +0000

    customization: Update remapping dropdown aria label
    Update the aria label for remapping dropdown to be something like
    "Button one is remapped to redo".
    
    Screenshot:
    https://screenshot.googleplex.com/rnUqBazMUrLUvKP
    
    Bug: b/241965717
    Test: Visual effects on VM and DUT
    Change-Id: Id1a81995d620d257c2e94e9aae5b8a918ee10f71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935616
    Reviewed-by: David Padlipsky <dpad@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1214435}

--- chrome/browser/resources/ash/settings/device_page/customize_button_row.ts
+++ chrome/browser/resources/ash/settings/device_page/customize_button_row.ts
@@ -246,6 +246,7 @@ export class CustomizeButtonRowElement extends CustomizeButtonRowElementBase {
       dropdown.value =
           option === undefined ? NO_REMAPPING_OPTION_VALUE : originalAction;
       this.prevChoice_ = dropdown.value;
+      dropdown.setAttribute('aria-label', this.getDropdownAriaLabel_());
     });
   }
 
@@ -330,6 +331,7 @@ export class CustomizeButtonRowElement extends CustomizeButtonRowElementBase {
 
       microTask.run(() => {
         dropdown.value = KEY_COMBINATION_OPTION_VALUE;
+        dropdown.setAttribute('aria-label', this.getDropdownAriaLabel_());
         this.prevChoice_ = dropdown.value;
       });
     } else if (
@@ -343,6 +345,7 @@ export class CustomizeButtonRowElement extends CustomizeButtonRowElementBase {
       this.set('fakePref_.value', NO_REMAPPING_OPTION_VALUE);
       microTask.run(() => {
         dropdown.value = NO_REMAPPING_OPTION_VALUE;
+        dropdown.setAttribute('aria-label', this.getDropdownAriaLabel_());
         this.prevChoice_ = dropdown.value;
       });
     }
@@ -532,6 +535,20 @@ export class CustomizeButtonRowElement extends CustomizeButtonRowElementBase {
 
   private isDropdownDisabled_(): boolean {
     return this.isBeingDragged_;
+  }
+
+  private getDropdownAriaLabel_(): string {
+    const select = this.$.remappingActionDropdown;
+    const optionLabel = select.options[select.selectedIndex] ?
+        select.options[select.selectedIndex].text :
+        this.i18n('noRemappingOptionLabel');
+    if (!this.buttonRemappingName_) {
+      return optionLabel;
+    }
+
+    return this.i18n(
+        'buttonRemappingDropdownAriaLabel', this.buttonRemappingName_,
+        optionLabel);
   }
 }
 
