eb4f6de651cee02a783e25e6ec3edcde8afc47a2
Benedikt Meurer <bmeurer@chromium.org>
2023-10-24 20:53:04 +0000

    [DevTools] Correctly restore media type after print preview.
    This fixes the issue that LocalFrameView::AdjustMediaTypeForPrinting()
    backs up the current media type as follows
    
    ```
    media_type_when_not_printing_ = MediaType();
    ```
    
    and MediaType() checks if there is currently a media type override in
    place (from DevTools), returns the overridden type if so, and if not
    returns the base media_type_. The fix is as simple as replacing the
    above line with:
    
    ```
    media_type_when_not_printing_ = media_type_;
    ```
    
    to ignore the override, and instead backup the internal state of the
    LocalFrameView without being corrupted by a media type override.
    
    Also-By: Andrew Gozzard <gozzard@google.com>
    Fixed: chromium:760832
    Demo: https://output.jsbin.com/tevefu/1
    Change-Id: I4472b4fa7d189bbc9ff98774b7a212b5273a6bc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4960317
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214432}

--- third_party/blink/renderer/core/frame/local_frame_view_test.cc
+++ third_party/blink/renderer/core/frame/local_frame_view_test.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/html_element.h"
 #include "third_party/blink/renderer/core/html/html_iframe_element.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
+#include "third_party/blink/renderer/core/media_type_names.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
 #include "third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h"
 #include "third_party/blink/renderer/core/paint/paint_property_tree_printer.h"
@@ -715,6 +716,17 @@ TEST_F(LocalFrameViewTest, DarkModeDocumentBackground) {
   UpdateAllLifecyclePhasesForTest();
   frame_view->SetBaseBackgroundColor(Color(255, 0, 0));
   EXPECT_EQ(frame_view->DocumentBackgroundColor(), Color(18, 18, 18));
+}
+
+TEST_F(LocalFrameViewTest,
+       AdjustMediaTypeForPrintingRestoresMediaTypeCorrectly) {
+  auto* frame_view = GetDocument().View();
+  frame_view->SetMediaType(media_type_names::kScreen);
+  GetDocument().GetSettings()->SetMediaTypeOverride("print");
+  frame_view->AdjustMediaTypeForPrinting(true);
+  frame_view->AdjustMediaTypeForPrinting(false);
+  GetDocument().GetSettings()->SetMediaTypeOverride(g_null_atom);
+  EXPECT_EQ(frame_view->MediaType(), "screen");
 }
 
 class FencedFrameLocalFrameViewTest : private ScopedFencedFramesForTest,
