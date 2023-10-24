518941f9fb00b9899d98a6241d67a4b507d24d60
Sergii Bykov <sbykov@google.com>
2023-10-24 08:16:51 +0000

    [Code Health] Remove expired WebApp.Uninstall histograms
    Bug: 1448116
    Change-Id: I6071bcc19790f7c220c220c77a804c49805b8191
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4958338
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Sergii Bykov <sbykov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1214033}

--- chrome/browser/web_applications/jobs/uninstall/remove_web_app_job.cc
+++ chrome/browser/web_applications/jobs/uninstall/remove_web_app_job.cc
@@ -200,7 +200,6 @@ void RemoveWebAppJob::OnOsHooksUninstalled(OsHooksErrors errors) {
   CHECK(!primary_removal_result_.has_value());
   CHECK(!hooks_uninstalled_);
   hooks_uninstalled_ = true;
-  base::UmaHistogramBoolean("WebApp.Uninstall.OsHookSuccess", errors.none());
   errors_ = errors_ || errors.any();
   MaybeFinishPrimaryRemoval();
 }
@@ -209,7 +208,6 @@ void RemoveWebAppJob::OnIconDataDeleted(bool success) {
   CHECK(!primary_removal_result_.has_value());
   CHECK(!app_data_deleted_);
   app_data_deleted_ = true;
-  base::UmaHistogramBoolean("WebApp.Uninstall.IconDataSuccess", success);
   errors_ = errors_ || !success;
   MaybeFinishPrimaryRemoval();
 }
