52189adb51af12e02b2df522866f7f26b8eb38dc
Toni Barzic <tbarzic@chromium.org>
2023-10-24 17:13:35 +0000

    Remove AppsGridView::LockAppsGridOpacity
    Not useful anymore - it was used to prevent opacity updates in
    `PagedAppsGridView::UpdateOpacity()` mid fade-in animation. The
    `UpdateOpacity()` method has since been removed, so
    `LockAppsGridOpacity()` now only sets and resets `lock_opacity_`
    member variable.
    
    BUG=b:261662696
    
    Change-Id: Iba54b2b5175451269338fb0fd11cd95c7def38cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4969275
    Reviewed-by: Ana Salazar <anasalazar@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214282}

--- ash/app_list/views/paged_apps_grid_view.h
+++ ash/app_list/views/paged_apps_grid_view.h
@@ -125,7 +125,6 @@ class ASH_EXPORT PagedAppsGridView : public AppsGridView,
   void EnsureViewVisible(const GridIndex& index) override;
   absl::optional<VisibleItemIndexRange> GetVisibleItemIndexRange()
       const override;
-  base::ScopedClosureRunner LockAppsGridOpacity() override;
   bool ShouldContainerHandleDragEvents() override;
 
   // PaginationModelObserver:
@@ -349,9 +348,6 @@ class ASH_EXPORT PagedAppsGridView : public AppsGridView,
   int margin_for_gradient_mask_ = 0;
 
   void StackCardsAtBottom() override;
-
-  // If true, ignore the calls on `UpdateOpacity()`.
-  bool lock_opacity_ = false;
 
   // Whether the apps grid is currently animating  the cardified state.
   bool is_animating_cardified_state_ = false;
