b36be0b5fbde6b1219514f79c0c0c299ce18e7e4
Chuong Ho <hdchuong@google.com>
2023-10-24 10:36:45 +0000

    editor: allow feature on non-app, chrome-app
    This CL allows the feature on more surfaces
    
    Bug: b:295440693
    Change-Id: I04246bf90bf1b4e4c969e89db09c0ee2502d62b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4970916
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214092}

--- chrome/browser/ash/input_method/editor_switch.cc
+++ chrome/browser/ash/input_method/editor_switch.cc
@@ -12,7 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/input_method/editor_consent_enums.h"
 #include "chrome/browser/ash/input_method/url_utils.h"
 #include "chrome/browser/policy/profile_policy_connector.h"
+#include "chrome/browser/web_applications/web_app_id_constants.h"
+#include "chrome/common/extensions/extension_constants.h"
 #include "chromeos/constants/chromeos_features.h"
+#include "extensions/common/constants.h"
 #include "net/base/network_change_notifier.h"
 #include "ui/base/ime/text_input_type.h"
 
@@ -40,10 +43,9 @@ constexpr std::string_view kInputMethodEngineAllowlist[] = {
     "xkb:us::eng",              // US
 };
 
-constexpr AppType kAppTypeAllowlist[] = {
-    AppType::BROWSER,
-    AppType::LACROS,
-    AppType::SYSTEM_APP,
+constexpr AppType kAppTypeDenylist[] = {
+    AppType::ARC_APP,
+    AppType::CROSTINI_APP,
 };
 
 const char* kDomainsWithPathDenylist[][2] = {
@@ -61,7 +63,15 @@ const char* kDomainsWithPathDenylist[][2] = {
 constexpr int kTextLengthMaxLimit = 10000;
 
 const char* kAppIdDenylist[] = {
-    file_manager::kFileManagerSwaAppId,
+    extension_misc::kGmailAppId,        extension_misc::kCalendarAppId,
+    extension_misc::kFilesManagerAppId, extension_misc::kGoogleDocsAppId,
+    extension_misc::kGoogleSlidesAppId, extension_misc::kGoogleSheetsAppId,
+    extension_misc::kGoogleDriveAppId,  extension_misc::kGoogleKeepAppId,
+    file_manager::kFileManagerSwaAppId, web_app::kGmailAppId,
+    web_app::kGoogleChatAppId,          web_app::kGoogleMeetAppId,
+    web_app::kGoogleDocsAppId,          web_app::kGoogleSlidesAppId,
+    web_app::kGoogleSheetsAppId,        web_app::kGoogleDriveAppId,
+    web_app::kGoogleKeepAppId,          web_app::kGoogleCalendarAppId,
 };
 
 bool IsCountryAllowed(std::string_view country_code) {
@@ -77,7 +87,7 @@ bool IsInputMethodEngineAllowed(std::string_view engine_id) {
 }
 
 bool IsAppTypeAllowed(AppType app_type) {
-  return base::Contains(kAppTypeAllowlist, app_type);
+  return !base::Contains(kAppTypeDenylist, app_type);
 }
 
 bool IsTriggerableFromConsentStatus(ConsentStatus consent_status) {
