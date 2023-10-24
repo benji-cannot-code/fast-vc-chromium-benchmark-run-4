6c82f1e156a8ebabd660c9db67c19ca6115d6d84
Christian Xu <christianxu@google.com>
2023-10-24 10:58:37 +0000

    [iOS][Omnibox] Fix experimental textfield
    Experimental textfield catches the tap UIGesture to handle the pre-edit
    and autocomplete state. This breaks some behavior of the tap gesture
    in a default textfield.
    
    This CL:
    - Allows the custom tap gesture to be recognized simultaneously with the
      UITextField tap gesture.
    - Let UITextField handle the visibility of the menu.
    
    Bug: 1429190, 1384533
    Change-Id: I40708ca84864843aa42d21e6f1a70610055909ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4965401
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214104}

--- ios/chrome/browser/ui/omnibox/omnibox_text_field_experimental.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_text_field_experimental.mm
@@ -50,7 +50,7 @@ NSString* const kOmniboxFadeAnimationKey = @"OmniboxFadeAnimation";
 
 }  // namespace
 
-@interface OmniboxTextFieldExperimental ()
+@interface OmniboxTextFieldExperimental () <UIGestureRecognizerDelegate>
 
 // Font to use in regular x regular size class. If not set, the regular font is
 // used instead.
@@ -65,6 +65,9 @@ NSString* const kOmniboxFadeAnimationKey = @"OmniboxFadeAnimation";
 
 // Length of autocomplete text.
 @property(nonatomic) size_t autocompleteTextLength;
+
+// Tap gesture recognizer for this view.
+@property(nonatomic, strong) UITapGestureRecognizer* tapGestureRecognizer;
 
 @end
 
@@ -107,9 +110,11 @@ NSString* const kOmniboxFadeAnimationKey = @"OmniboxFadeAnimation";
     self.font = self.currentFont;
     [super setText:@" "];
 
-    [self addGestureRecognizer:[[UITapGestureRecognizer alloc]
-                                   initWithTarget:self
-                                           action:@selector(handleTap)]];
+    self.tapGestureRecognizer =
+        [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                action:@selector(handleTap:)];
+    self.tapGestureRecognizer.delegate = self;
+    [self addGestureRecognizer:self.tapGestureRecognizer];
   }
   return self;
 }
@@ -477,24 +482,30 @@ NSString* const kOmniboxFadeAnimationKey = @"OmniboxFadeAnimation";
   [self setAttributedText:self.attributedText];
 }
 
+#pragma mark - UIGestureRecognizerDelegate
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer*)gestureRecognizer
+    shouldRecognizeSimultaneouslyWithGestureRecognizer:
+        (UIGestureRecognizer*)otherGestureRecognizer {
+  return YES;
+}
+
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer*)gestureRecognizer {
+  if (gestureRecognizer == self.tapGestureRecognizer) {
+    return [self isPreEditing] || [self hasAutocompleteText];
+  }
+  return YES;
+}
+
 #pragma mark - UIResponder
 
 // Triggered on tap gesture recognizer.
-- (void)handleTap {
+- (void)handleTap:(UITapGestureRecognizer*)sender {
   if ([self isPreEditing]) {
     [self exitPreEditState];
-    [super selectAll:self];
-  } else if ([self hasAutocompleteText]) {
-    // Accept selection.
+  }
+  if ([self hasAutocompleteText]) {
     [self acceptAutocompleteText];
-  } else {
-    [self becomeFirstResponder];
-    UIMenuController* menuController = [UIMenuController sharedMenuController];
-    if (menuController.isMenuVisible) {
-      [menuController hideMenu];
-    } else {
-      [menuController showMenuFromView:self rect:self.bounds];
-    }
   }
 }
 
