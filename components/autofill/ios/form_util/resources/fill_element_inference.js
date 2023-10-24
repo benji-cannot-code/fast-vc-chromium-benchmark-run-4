e69964fbe48be5d8f0be87eab3c899017bd48350
Daniel White <danieltwhite@google.com>
2023-10-24 18:27:23 +0000

    [iOS] Convert inferLabelFromNext to Exported Function
    In this CL, inferLabelFromNext was removed from __gCrWeb and converted
    into an exported function.
    
    In a follow up CL, the fill_element_inference.js file will be converted
    to TypeScript.
    
    Bug: 1430814
    Change-Id: I757fb1ed49900f71843a7c0de8cb92b327aa6863
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4951793
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1214342}

--- components/autofill/ios/form_util/resources/fill_element_inference.js
+++ components/autofill/ios/form_util/resources/fill_element_inference.js
@@ -124,9 +124,9 @@ __gCrWeb.fill.inferLabelFromPrevious = function(element) {
  * @param {FormControlElement} element An element to examine.
  * @return {string} The label of element.
  */
-__gCrWeb.fill.inferLabelFromNext = function(element) {
+function inferLabelFromNext(element) {
   return inferLabelFromSibling(element, true);
-};
+}
 
 /**
  * Helper for |InferLabelForElement()| that infers a label, if possible, from
@@ -542,7 +542,7 @@ __gCrWeb.fill.inferLabelFromDefinitionList = function(element) {
 __gCrWeb.fill.inferLabelForElement = function(element) {
   let inferredLabel;
   if (__gCrWeb.fill.isCheckableElement(element)) {
-    inferredLabel = __gCrWeb.fill.inferLabelFromNext(element);
+    inferredLabel = inferLabelFromNext(element);
     if (inferenceUtil.isLabelValid(inferredLabel)) {
       return inferredLabel;
     }
@@ -605,3 +605,5 @@ __gCrWeb.fill.inferLabelForElement = function(element) {
 
   return '';
 };
+
+export {inferLabelFromNext};
