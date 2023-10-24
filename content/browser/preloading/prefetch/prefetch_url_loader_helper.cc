3bab405a8f1619b96a5516545a75b35e7d63b3bb
Hiroshige Hayashizaki <hiroshige@chromium.org>
2023-10-24 16:29:57 +0000

    Introduce PrefetchContainer::OnCookiesChanged()
    Bug: 1494771
    Change-Id: I2d3a977426f7e7f034b720b3704d521c84a7d376
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4966191
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214258}

--- content/browser/preloading/prefetch/prefetch_url_loader_helper.cc
+++ content/browser/preloading/prefetch/prefetch_url_loader_helper.cc
@@ -227,11 +227,7 @@ void OnGotPrefetchToServe(
   }
 
   if (reader.HaveDefaultContextCookiesChanged()) {
-    reader.GetPrefetchContainer()->SetPrefetchStatus(
-        PrefetchStatus::kPrefetchNotUsedCookiesChanged);
-    reader.GetPrefetchContainer()->UpdateServingPageMetrics();
-    reader.GetPrefetchContainer()->CancelStreamingURLLoaderIfNotServing();
-
+    reader.GetPrefetchContainer()->OnCookiesChanged();
     std::move(get_prefetch_callback).Run({});
     return;
   }
