393b76e9b61502f16ed1c176d4a7d9c30800b022
Vivian Pao <vpao@google.com>
2023-10-24 03:21:27 +0000

    [Promise Icons] Record metrics for promise app icon type
    Add metrics to track how often promise apps have real app icons
    available to show in the Launcher and Shelf, and how often they fallback
    to using a placeholder icon.
    
    Bug: b/306306785
    Change-Id: If47e11aa87e162b894260416b6ac54a4f17f774b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4957915
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Vivian Pao <vpao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1213933}

--- chrome/browser/apps/app_service/promise_apps/promise_app_metrics.h
+++ chrome/browser/apps/app_service/promise_apps/promise_app_metrics.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace apps {
 
 extern const char kPromiseAppLifecycleEventHistogram[];
+extern const char kPromiseAppIconTypeHistogram[];
 
 // These values are persisted to logs. Entries should not be renumbered and
 // numeric values should never be reused.
@@ -24,7 +25,17 @@ enum class PromiseAppLifecycleEvent {
   kMaxValue = kCreatedInShelf
 };
 
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class PromiseAppIconType {
+  kPlaceholderIcon = 0,
+  kRealIcon = 1,
+  kMaxValue = kRealIcon
+};
+
 void RecordPromiseAppLifecycleEvent(const PromiseAppLifecycleEvent event);
+
+void RecordPromiseAppIconType(const PromiseAppIconType icon_type);
 
 }  // namespace apps
 
