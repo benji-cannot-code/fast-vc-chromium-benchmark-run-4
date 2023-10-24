e3403d708a69ea8f150ec7e6c879936325a26b68
Stephan Hartmann <stha09@googlemail.com>
2023-10-24 20:44:04 +0000

    IWYU: add stdint.h for uint32_t in ui::PlatformMenuUtils
    Bug: 957519
    Change-Id: I8cc1bb26ec6fe7e471b0ddbde9eeed4486f25afb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4964684
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Stephan Hartmann <stha09@googlemail.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214428}

--- ui/ozone/public/platform_menu_utils.h
+++ ui/ozone/public/platform_menu_utils.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_OZONE_PUBLIC_PLATFORM_MENU_UTILS_H_
 #define UI_OZONE_PUBLIC_PLATFORM_MENU_UTILS_H_
 
+#include <stdint.h>
+
 #include <string>
 
 #include "base/component_export.h"
