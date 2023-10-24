f7236094d7bf9cdc888035b1aba2ab2aefb3f40a
Simon Zünd <szuend@chromium.org>
2023-10-24 07:13:41 +0000

    [devtools] Clear readonly array via .length
    The CL https://crrev.com/c/4965300 changes `ElementsTestRunner.events`
    to be readonly, so we have to update a test that clears the array to
    do it without re-assignment.
    
    R=pfaffe@chromium.org
    
    Bug: 1442410
    Change-Id: I8a144ec05e91558ca2da6bf7e02b10e002a27f05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4970993
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214011}

--- third_party/blink/web_tests/http/tests/devtools/elements/edit/set-outer-html-whitespace.js
+++ third_party/blink/web_tests/http/tests/devtools/elements/edit/set-outer-html-whitespace.js
@@ -31,7 +31,7 @@ import {ElementsTestRunner} from 'elements_test_runner';
     for (let i = 0; i < ElementsTestRunner.events.length; ++i)
       TestRunner.addResult(ElementsTestRunner.events[i]);
 
-    ElementsTestRunner.events = [];
+    ElementsTestRunner.events.length = 0; // 'events' is readonly.
     TestRunner.addResult("");
   }
 
