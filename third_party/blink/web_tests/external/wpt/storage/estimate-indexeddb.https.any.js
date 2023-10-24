60920cbb4f75d0334fe6ee1e238af7ded855cd09
Evan Stade <estade@chromium.org>
2023-10-24 18:02:33 +0000

    Deduplicate some WPT.
    storagemanager-estimate and estimate-indexeddb are more or less the
    same tests, the latter having been ported from the former to use
    `async`/`await`. The former probably should have been deleted when the
    latter was introduced.
    
    Since some of the tests are related to IndexedDB and some are not, this
    change keeps the IndexedDB tests in the file called `estimate-indexeddb`
    and keeps the basic tests in `storagemanager-estimate` (with minor
    updates).
    
    One wrinkle from the Chromium side is that the behavior of
    `storageManager.estimate()` is not actually specced, and as the Chromium
    implementation uses LevelDB, which behaves in mysterious ways, adding
    things to the database does NOT always increase the reported usage size.
    Both of these tests operate on large things, which typically do increase
    usage, however we noticed that for one reason or another, the test that
    adds an *uninitialized* ArrayBuffer starts failing on Windows if
    durability is set to relaxed. There are other ways to make the test fail
    as well: using a shorter name for the database, or putting small
    values, does not reliably increase the reported usage. This is all fine
    in the sense that it isn't defined behavior, but it does suggest that
    working in this area or on tests of this ilk is a bit of a minefield.
    These tests probably should not exist as WPT, at least not until quota
    behavior is specced (see https://github.com/whatwg/storage/issues/110).
    But it would also be sort of a shame to delete WPT that are passing, so
    I've left them in place for now.
    
    Bug: 1489517
    Change-Id: I6619f504ce92e428054691ac6bf54a0e14e3ce5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4968659
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214324}

--- third_party/blink/web_tests/external/wpt/storage/estimate-indexeddb.https.any.js
+++ third_party/blink/web_tests/external/wpt/storage/estimate-indexeddb.https.any.js
@@ -2,21 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: title=StorageManager: estimate() for indexeddb
 // META: script=/storage/buckets/resources/util.js
 
-test(t => {
-  assert_true('estimate' in navigator.storage);
-  assert_equals(typeof navigator.storage.estimate, 'function');
-  assert_true(navigator.storage.estimate() instanceof Promise);
-}, 'estimate() method exists and returns a Promise');
-
-promise_test(async t => {
-  const estimate = await navigator.storage.estimate();
-  assert_equals(typeof estimate, 'object');
-  assert_true('usage' in estimate);
-  assert_equals(typeof estimate.usage, 'number');
-  assert_true('quota' in estimate);
-  assert_equals(typeof estimate.quota, 'number');
-}, 'estimate() resolves to dictionary with members');
-
+// Technically, this verifies unspecced behavior. See
+// https://github.com/whatwg/storage/issues/110 for defining this behavior.
 promise_test(async t => {
   const arraySize = 1e6;
   const objectStoreName = "storageManager";
