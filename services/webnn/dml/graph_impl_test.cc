d99d5816e926ff47c606b1b39cec76e81163b785
mingmingtasd <mingming1.xu@intel.com>
2023-10-24 13:11:33 +0000

    WebNN: Define Sigmoid, Tanh and union Activation in mojo
    This CL defined 'Sigmoid', 'Tanh' and 'union Activation' in mojo,
    also combined the ops 'Clamp', 'Relu', 'Sigmoid', `Softmax` and
    `Tanh` in `union Activation` which can be fused as part of the
    preceding op (Conv2d, BatchNormalization, for example).
    
    Some unittests are added and refactored both for blink and services
    side.
    
    Bug: 1273291
    Change-Id: I1aa7ef1391bc9240ade08db0681f5487d5f6344d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4920337
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Mingming1 Xu <mingming1.xu@intel.com>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214125}

--- services/webnn/dml/graph_impl_test.cc
+++ services/webnn/dml/graph_impl_test.cc
@@ -246,7 +246,7 @@ struct Conv2dTester {
     mojom::InputOperandLayout input_layout =
         mojom::InputOperandLayout::kChannelsFirst;
     absl::optional<OperandInfo<T>> bias;
-    absl::optional<mojom::Operation::Tag> activation;
+    absl::optional<mojom::Activation::Tag> activation;
     absl::optional<ClampAttributes> clamp_attributes;
   };
   Conv2dAttributes attributes;
@@ -418,7 +418,7 @@ TEST_F(WebNNGraphDMLImplTest, BuildAndComputeSingleOperatorConv2d) {
                                .type = mojom::Operand::DataType::kFloat32,
                                .dimensions = {1},
                                .values = {-100}},
-                       .activation = mojom::Operation::Tag::kRelu},
+                       .activation = mojom::Activation::Tag::kRelu},
         .output = {.type = mojom::Operand::DataType::kFloat32,
                    .dimensions = {1, 5, 5, 1},
                    .values = {0,  0, 0, 0,  0,  0,  0,  0, 0,  0,  0,  0, 8,
@@ -444,7 +444,7 @@ TEST_F(WebNNGraphDMLImplTest, BuildAndComputeSingleOperatorConv2d) {
                                .type = mojom::Operand::DataType::kFloat16,
                                .dimensions = {1},
                                .values = Float16FromFloat32({-100})},
-                       .activation = mojom::Operation::Tag::kRelu},
+                       .activation = mojom::Activation::Tag::kRelu},
         .output = {.type = mojom::Operand::DataType::kFloat16,
                    .dimensions = {1, 5, 5, 1},
                    .values = {0,  0, 0, 0,  0,  0,  0,  0, 0,  0,  0,  0, 8,
