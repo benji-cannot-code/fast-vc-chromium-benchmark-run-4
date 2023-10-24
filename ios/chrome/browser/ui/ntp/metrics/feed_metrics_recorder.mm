3fce3beefd0ad537c710f1d28aa77e4f41f8521c
Federica Germinario <fedegermi@google.com>
2023-10-24 17:33:32 +0000

    [ios] Move LongFeedInteractionTimeDelta key out of NSUserDefaults
    This CL moves LongFeedInteractionTimeDelta out of NSUserDefaults.
    
    The key has been moved in the PrefService. Note that there are two
    types of PrefServices, in this case the code was moved in the
    PrefService coming from BrowserState which is per account and is
    syncable between devices.
    Note that this kind of PrefService behaves differently from
    NSUserDefaults which is per device (and not per account).
    
    Bug: 1486770
    Change-Id: I4387b1b5ee67a00b09f7ac4edd8e65acad83c521
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4964941
    Reviewed-by: Guillem Perez <guiperez@google.com>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214296}

--- ios/chrome/browser/ui/ntp/metrics/feed_metrics_recorder.mm
+++ ios/chrome/browser/ui/ntp/metrics/feed_metrics_recorder.mm
@@ -218,7 +218,7 @@ using feed::FeedUserActionType;
     [self recordTimeSpentInFeedIfDayIsDone];
 
     self.previousTimeInFeedForGoodVisitSession =
-        [defaults doubleForKey:kLongFeedVisitTimeAggregateKey];
+        self.prefService->GetDouble(kLongFeedVisitTimeAggregateKey);
     self.discoverPreviousTimeInFeedGV =
         self.prefService->GetDouble(kLongDiscoverFeedVisitTimeAggregateKey);
     self.followingPreviousTimeInFeedGV =
@@ -247,8 +247,8 @@ using feed::FeedUserActionType;
     }
   } else {
     // Once the NTP becomes hidden, check for Good Visit which updates
-    // `self.previousTimeInFeedForGoodVisitSession` and then we save it to
-    // defaults.
+    // `self.previousTimeInFeedForGoodVisitSession` and then we save it in
+    // PrefService.
 
     // Also calculate total aggregate for the time in feed aggregate metric.
     self.timeSpentInFeed = base::Time::Now() - self.feedBecameVisibleTime;
@@ -256,8 +256,8 @@ using feed::FeedUserActionType;
     [self checkEngagementGoodVisitWithInteraction:NO];
     self.prefService->SetDouble(kTimeSpentInFeedAggregateKey,
                                 self.timeSpentInFeed.InSecondsF());
-    [defaults setDouble:self.previousTimeInFeedForGoodVisitSession
-                 forKey:kLongFeedVisitTimeAggregateKey];
+    self.prefService->SetDouble(kLongFeedVisitTimeAggregateKey,
+                                self.previousTimeInFeedForGoodVisitSession);
     self.prefService->SetDouble(kLongDiscoverFeedVisitTimeAggregateKey,
                                 self.discoverPreviousTimeInFeedGV);
     self.prefService->SetDouble(kLongFollowingFeedVisitTimeAggregateKey,
@@ -1314,8 +1314,7 @@ using feed::FeedUserActionType;
   // Reset defaults for new session.
   NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
   [defaults setObject:nil forKey:kArticleVisitTimestampKey];
-  [defaults setDouble:0 forKey:kLongFeedVisitTimeAggregateKey];
-
+  self.prefService->ClearPref(kLongFeedVisitTimeAggregateKey);
   base::Time now = base::Time::Now();
 
   self.lastInteractionTimeForGoodVisits = now;
