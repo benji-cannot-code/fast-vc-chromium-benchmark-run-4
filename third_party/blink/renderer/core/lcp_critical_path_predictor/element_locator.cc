c4c46fe5af1c61f8221d0f37f5f28d8165435fe3
Alex N. Jose <alexnj@chromium.org>
2023-10-24 05:29:06 +0000

    [LCPP] Make ElementLocator::OfElement() return value non-optional
    The function has an absl::optional<> signature that is not needed
    per implementation. Removing it to reduce confusion and prevent
    future errors from assuming non-optional.
    
    Bug: 1491595
    Change-Id: Ib410b9da357e93389f2704e2c2336ead66ec4a4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4954300
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213983}

--- third_party/blink/renderer/core/lcp_critical_path_predictor/element_locator.cc
+++ third_party/blink/renderer/core/lcp_critical_path_predictor/element_locator.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink::element_locator {
 
-absl::optional<ElementLocator> OfElement(Element* element) {
+ElementLocator OfElement(Element* element) {
   ElementLocator locator;
 
   while (element) {
