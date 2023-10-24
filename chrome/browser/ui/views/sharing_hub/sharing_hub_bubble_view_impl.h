e142835bfc5e343fd0c98c181b764163eec1b42c
Elly <ellyjones@chromium.org>
2023-10-24 19:00:18 +0000

    desktop-sharing: remove obsolete metrics
    This change:
    * Removes metrics declarations for Sharing.SharingHubDesktop.TimeToFetch*,
      which were added as part of an experiment with an alternate design for
      the desktop sharing hub that was never shipped;
    * Removes the metrics declaration and code to gather
      Sharing.SharingHubDesktop.TimeToShow, since nobody looks at this
      metric. This simplifies the code somewhat.
    
    Fixed: 1495069
    Change-Id: I5df3c144b2993c96cd84b0c00bf883b9bae85735
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4972736
    Auto-Submit: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214367}

--- chrome/browser/ui/views/sharing_hub/sharing_hub_bubble_view_impl.h
+++ chrome/browser/ui/views/sharing_hub/sharing_hub_bubble_view_impl.h
@@ -8,15 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
-#include "base/time/time.h"
 #include "chrome/browser/share/share_attempt.h"
 #include "chrome/browser/ui/sharing_hub/sharing_hub_bubble_view.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_bubble_delegate_view.h"
 #include "ui/base/metadata/metadata_header_macros.h"
-
-namespace gfx {
-class Canvas;
-}  // namespace gfx
 
 namespace sharing_hub {
 
@@ -51,11 +46,7 @@ class SharingHubBubbleViewImpl : public SharingHubBubbleView,
 
   // LocationBarBubbleDelegateView:
   std::u16string GetAccessibleWindowTitle() const override;
-  void OnPaint(gfx::Canvas* canvas) override;
   void OnThemeChanged() override;
-
-  // Shows the bubble view.
-  void Show(DisplayReason reason);
 
   void OnActionSelected(SharingHubBubbleActionButton* button);
 
@@ -88,11 +79,6 @@ class SharingHubBubbleViewImpl : public SharingHubBubbleView,
   // The "Share link to" annotation text, which indicates to the user what
   // the 3P target options do.
   raw_ptr<views::Label> share_link_label_ = nullptr;
-
-  // The time that Show() was called. This is reset after the first time the
-  // sharing hub is painted to avoid repeatedly collecting the metric it is used
-  // for.
-  absl::optional<base::Time> show_time_;
 
   // The share attempt this bubble was opened for.
   const share::ShareAttempt attempt_;
