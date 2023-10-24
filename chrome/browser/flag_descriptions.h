e309336dd94974e4e8a55b88c411fb5c3452f7a3
Yuichiro Hanada <yhanada@google.com>
2023-10-24 01:46:51 +0000

    arc: Remove arc-trackpad-scroll-touchscreen-emulation flag
    It has been disabled by default.
    
    Bug: b:191461076
    Change-Id: I7e72c162c605dae350a5c1c1275b1a3c1bf77077
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4967692
    Reviewed-by: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Hiroki Sato <hirokisato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213884}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3150,9 +3150,6 @@ extern const char kArcSyncInstallPriorityDescription[];
 extern const char kArcTouchscreenEmulationName[];
 extern const char kArcTouchscreenEmulationDesc[];
 
-extern const char kArcTrackpadScrollTouchscreenEmulationName[];
-extern const char kArcTrackpadScrollTouchscreenEmulationDesc[];
-
 extern const char kArcVmmSwapKBShortcutName[];
 extern const char kArcVmmSwapKBShortcutDesc[];
 
