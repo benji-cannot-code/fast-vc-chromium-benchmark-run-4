a313acfe429201b15a43963f89c3d0c824a7770c
Alison Gale <agale@chromium.org>
2023-10-24 23:58:58 +0000

    Fix placeholder when no amount of tabs will get over threshold
    Change-Id: Ibc53707abdce29ef7de2d8eb8b3054a82c4063a2
    Bug: 1481051
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4970046
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Auto-Submit: Alison Gale <agale@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1214533}

--- chrome/browser/performance_manager/metrics/page_timeline_monitor.cc
+++ chrome/browser/performance_manager/metrics/page_timeline_monitor.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 #include <array>
+#include <limits>
 #include <map>
 #include <memory>
 #include <numeric>
@@ -385,8 +386,9 @@ void PageTimelineMonitor::LogCPUInterventionMetrics(
   double cpu_to_get_under_threshold =
       total_foreground_cpu_usage + total_background_cpu_usage - kThreshold;
   if (total_background_cpu_usage < cpu_to_get_under_threshold) {
-    // Use -1 to represent when closing all background tabs won't be enough
-    tabs_to_get_under_threshold = -1;
+    // Use max int to represent when closing all background tabs won't be
+    // enough.
+    tabs_to_get_under_threshold = std::numeric_limits<int>::max();
   } else {
     for (double cpu_usage : background_cpu_usage) {
       cpu_to_get_under_threshold -= cpu_usage;
