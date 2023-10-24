18b202da985ad3327f1c3b8c24b49b4a90bc3ca2
Mark Schillaci <mschillaci@google.com>
2023-10-24 13:46:22 +0000

    [Image Descriptions] Include icon engine as a known backend engine
    This CL fixes a small logic bug in the metrics calculation for the
    image descriptions feature on all platforms. The experiment to augment
    existing image labels is seeing a proportional uptick in the amount of
    unknown backend engine calls. A theory for this change in UMA data is
    that the augment experiment is triggering for a large number of icons,
    and the icon engine is not considered a "known" engine to the Chrome
    code base. With this CL we add the icon engine as a known engine.
    
    AX-Relnotes: N/A
    Bug: 1430204
    Change-Id: Ia814aab57f2280373bd0aae895a5500853a5ea7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4966764
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214159}

--- services/image_annotation/annotator.cc
+++ services/image_annotation/annotator.cc
@@ -384,7 +384,7 @@ std::map<std::string, mojom::AnnotateImageResultPtr> UnpackJsonResponse(
         }
       }
 
-      ReportEngineKnown(ocr_engine || desc_engine);
+      ReportEngineKnown(ocr_engine || desc_engine || icon_engine);
     }
 
     // Remove any description OCR data (which is lower quality) if we have
