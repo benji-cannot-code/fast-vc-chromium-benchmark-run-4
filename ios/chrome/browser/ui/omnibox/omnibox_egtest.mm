c51544ff31f1244e5e4c95d89437007fc5767fb8
David Jean <djean@chromium.org>
2023-10-24 13:24:11 +0000

    Revert "[iOS][Omnibox] Fix experimental textfield"
    This reverts commit ba99d8891af5a024d85159ce2ec2134bd76217b7.
    
    Reason for revert: breaks LocationBarEditStateTestCase/testEmptyOmnibox
    
    Original change's description:
    > [iOS][Omnibox] Fix experimental textfield
    >
    > Experimental textfield catches the tap UIGesture to handle the pre-edit
    > and autocomplete state. This breaks some behavior of the tap gesture
    > in a default textfield.
    >
    > This CL:
    > - Allows the custom tap gesture to be recognized simultaneously with the
    >   UITextField tap gesture.
    > - Let UITextField handle the visibility of the menu.
    >
    > Bug: 1429190, 1384533
    > Change-Id: I40708ca84864843aa42d21e6f1a70610055909ea
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4965401
    > Auto-Submit: Christian Xu <christianxu@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    > Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1214104}
    
    Bug: 1429190, 1384533
    Change-Id: I75a391271fb9542e559aed52502e0b72e367c086
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4969783
    Owners-Override: David Jean <djean@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: David Jean <djean@chromium.org>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214147}

--- ios/chrome/browser/ui/omnibox/omnibox_egtest.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_egtest.mm
@@ -822,7 +822,9 @@ void FocusFakebox() {
   [ChromeEarlGrey copyTextToPasteboard:@"hello"];
 
   // Tap on Omnibox on pre edit state.
-  [self tapOnPreEditTextInOmnibox];
+  // TODO(crbug.com/1442458): Find a better way to tap on the selected url.
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
+      performAction:grey_tapAtPoint(CGPointMake(0, 0))];
 
   // Wait for callout copy button to be displayed.
   [ChromeEarlGrey waitForUIElementToAppearWithMatcher:
@@ -852,7 +854,9 @@ void FocusFakebox() {
       assertWithMatcher:grey_sufficientlyVisible()];
 
   // Tap on Omnibox on pre edit state.
-  [self tapOnPreEditTextInOmnibox];
+  // TODO(crbug.com/1442458): Find a better way to tap on the selected url.
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
+      performAction:grey_tapAtPoint(CGPointMake(0, 0))];
 
   // Wait for callout cut button to be displayed.
   [ChromeEarlGrey waitForUIElementToAppearWithMatcher:
@@ -884,7 +888,9 @@ void FocusFakebox() {
   [ChromeEarlGrey copyTextToPasteboard:@"hello"];
 
   // Tap on Omnibox on pre edit state.
-  [self tapOnPreEditTextInOmnibox];
+  // TODO(crbug.com/1442458): Find a better way to tap on the selected url.
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
+      performAction:grey_tapAtPoint(CGPointMake(0, 0))];
 
   // Wait for callout paste button to be displayed.
   [ChromeEarlGrey waitForUIElementToAppearWithMatcher:
@@ -898,19 +904,6 @@ void FocusFakebox() {
   // Omnibox now should be empty.
   [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
       assertWithMatcher:chrome_test_util::OmniboxText("hello")];
-}
-
-// Taps the pre edit text in the omnibox.
-- (void)tapOnPreEditTextInOmnibox {
-  // TODO(crbug.com/1442458): Find a better way to tap on the selected url.
-  // Y offset to tap on the middle of the text.
-  const CGFloat kOmniboxTextFieldMidY = 18;
-  // X offset to tap on the beginning of the text, tapping on the left edge
-  // inserts a carret instead of showing the menu.
-  const CGFloat kOmniboxTextFieldOffsetX = 30;
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
-      performAction:grey_tapAtPoint(CGPointMake(kOmniboxTextFieldOffsetX,
-                                                kOmniboxTextFieldMidY))];
 }
 
 @end
