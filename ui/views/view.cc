029745e6fdcb7542aee31e64f48b3079da90e104
Dirk Pranke <dpranke@google.com>
2023-10-24 00:18:24 +0000

    Revert "Re-land "Enable keyboard-triggered tooltips in Views.""
    This reverts commit 228ff9278b4f9b0c02a0009d0c50615b8856edd1.
    
    Reason for revert: Somehow I made even mouse-triggered tooltips much more broken on Lacros.
    
    Original change's description:
    > Re-land "Enable keyboard-triggered tooltips in Views."
    >
    > This reverts commit f84138b85ae8ccdcf2d59ab4872929fb027c4a4f
    > and relands commit 96ec1f1f2c76a7110bc7f20f474eeb49d10deb1f (whose
    > commit message is below), but without the changes for Lacros.
    >
    > In other words, keyboard-triggered tooltips should work for
    > Windows, Linux, and Ash-Chrome, but not Lacros-Chrome.
    >
    > Making Lacros-Chrome work will come in a follow-on CL.
    >
    > > Author: Dirk Pranke <dpranke@google.com>
    > > Date:   Mon Oct 16 21:27:18 2023 +0000
    > >
    > >     Enable keyboard-triggered tooltips in Views.
    > >
    > >     This adds support for keyboard-triggered tooltips to Views; with
    > >     this, whenever a View is focused (i.e., View::Focus is called),
    > >     we  will now temporarily display a tooltip if the View has text
    > >     for one, much as if we had hovered over the view instead.
    > >
    > >     Bug: 1422046, 1068938
    > >     Change-Id: Ic72801b210132f8aac8c731266f783e52065f01e
    > >     Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4814731
    > >     Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > >     Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    > >     Reviewed-by: Scott Violet <sky@chromium.org>
    > >     Commit-Queue: Dirk Pranke <dpranke@google.com>
    > >     Cr-Commit-Position: refs/heads/main@{#1210435}
    >
    > Bug: 1422046, 1068938
    > Change-Id: I7ec5b6587dc1a28276011744191d44b3a92958f7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4953422
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Commit-Queue: Dirk Pranke <dpranke@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1213817}
    
    Bug: 1422046, 1068938
    Change-Id: I3d7c4e43d2bd11bd2fbbbe4bb913137258811872
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4969900
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213844}

--- ui/views/view.cc
+++ ui/views/view.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/dragdrop/mojom/drag_drop_types.mojom.h"
 #include "ui/base/ime/input_method.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
-#include "ui/base/ui_base_features.h"
 #include "ui/color/color_provider_manager.h"
 #include "ui/compositor/clip_recorder.h"
 #include "ui/compositor/compositor.h"
@@ -2610,11 +2609,6 @@ void View::OnBlur() {}
 void View::Focus() {
   OnFocus();
 
-#if !BUILDFLAG(IS_CHROMEOS_LACROS)
-  // TODO(crbug.com/1492220) - Get this working on Lacros as well.
-  UpdateTooltipForFocus();
-#endif
-
   // TODO(pbos): Investigate if parts of this can run unconditionally.
   if (!suppress_default_focus_handling_) {
     // Clear the native focus. This ensures that no visible native view has the
@@ -2672,17 +2666,6 @@ void View::TooltipTextChanged() {
   // TooltipManager may be null if there is a problem creating it.
   if (widget && widget->GetTooltipManager())
     widget->GetTooltipManager()->TooltipTextChanged(this);
-}
-
-void View::UpdateTooltipForFocus() {
-  if (base::FeatureList::IsEnabled(
-          ::views::features::kKeyboardAccessibleTooltipInViews) &&
-      !kShouldDisableKeyboardTooltipsForTesting) {
-    Widget* widget = GetWidget();
-    if (widget && widget->GetTooltipManager()) {
-      widget->GetTooltipManager()->UpdateTooltipForFocus(this);
-    }
-  }
 }
 
 // Drag and drop ---------------------------------------------------------------
@@ -3620,16 +3603,6 @@ void View::UpdateTooltip() {
   //             Widgets that it uses.
   if (widget && widget->GetTooltipManager())
     widget->GetTooltipManager()->UpdateTooltip();
-}
-
-bool View::kShouldDisableKeyboardTooltipsForTesting = false;
-
-void View::DisableKeyboardTooltipsForTesting() {
-  View::kShouldDisableKeyboardTooltipsForTesting = true;
-}
-
-void View::EnableKeyboardTooltipsForTesting() {
-  View::kShouldDisableKeyboardTooltipsForTesting = false;
 }
 
 // Drag and drop ---------------------------------------------------------------
