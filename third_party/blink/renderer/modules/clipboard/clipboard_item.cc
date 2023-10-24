0464cf23bcf8815ff66b2dc399eedb90bd4564f1
Anupam Snigdha <snianu@microsoft.com>
2023-10-24 22:12:42 +0000

    [Async Clipboard API] Add supports method to async clipboard API.
    In this change we are adding a new static method `supports` to the
    ClipboardItem interface to help web authors detect clipboard format
    types that are supported by Chromium.
    Github Issue: https://github.com/w3c/clipboard-apis/issues/170
    Spec: https://github.com/w3c/clipboard-apis/pull/192
    https://github.com/w3c/clipboard-apis/pull/195
    I2S: https://groups.google.com/a/chromium.org/g/blink-dev/c/pjpN9Lwv5Tk/m/KrAZRbdwAQAJ?utm_medium=email&utm_source=footer&pli=1
    
    Bug: 1483026, 1490635
    
    Change-Id: Ief7c0786833548d2fb51215cefbc39e5930af875
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4865377
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Anupam Snigdha <snianu@microsoft.com>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214477}

--- third_party/blink/renderer/modules/clipboard/clipboard_item.cc
+++ third_party/blink/renderer/modules/clipboard/clipboard_item.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
+#include "third_party/blink/renderer/core/clipboard/clipboard_mime_types.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/modules/clipboard/clipboard.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
@@ -76,6 +77,15 @@ ScriptPromise ClipboardItem::getType(ScriptState* script_state,
   exception_state.ThrowDOMException(DOMExceptionCode::kNotFoundError,
                                     "The type was not found");
   return ScriptPromise();
+}
+
+// static
+bool ClipboardItem::supports(const String& type) {
+  if (type == kMimeTypeImagePng || type == kMimeTypeTextPlain ||
+      type == kMimeTypeTextHTML) {
+    return true;
+  }
+  return !Clipboard::ParseWebCustomFormat(type).empty();
 }
 
 void ClipboardItem::Trace(Visitor* visitor) const {
