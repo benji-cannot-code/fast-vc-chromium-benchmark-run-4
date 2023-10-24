2d73b9d229d67e1abbe0942fcde452d9e9a325f5
Zoraiz Naeem <zoraiznaeem@chromium.org>
2023-10-24 17:37:51 +0000

    [Rounded Windows] Round chrome apps that use non-ash style windows
    In the CL, crrev/c/4906756, the chrome apps that uses ash style windows
    were rounded. In this CL, we handle chrome apps that do not use ash
    style windows.
    
    Bug: b:301336446
    Test: Manual
    Change-Id: I37163bcb9552e63162e540974a03f38b84888b7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4957978
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214303}

--- apps/ui/views/app_window_frame_view.h
+++ apps/ui/views/app_window_frame_view.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
-#include "ui/views/controls/button/button.h"
 #include "ui/views/window/non_client_view.h"
 
 namespace extensions {
@@ -57,7 +56,11 @@ class AppWindowFrameView : public views::NonClientFrameView {
   void SetResizeSizes(int resize_inside_bounds_size,
                       int resize_outside_bounds_size,
                       int resize_area_corner_size);
+  void SetFrameCornerRadius(int radius);
   int resize_inside_bounds_size() const { return resize_inside_bounds_size_; }
+
+ protected:
+  bool draw_frame() const { return draw_frame_; }
 
  private:
   // views::NonClientFrameView implementation.
@@ -99,6 +102,9 @@ class AppWindowFrameView : public views::NonClientFrameView {
 
   // Size in pixels of the lower-right corner resize handle.
   int resize_area_corner_size_ = 16;
+
+  // Radius for the top two corners of the frame.
+  int frame_corner_radius_ = 0;
 };
 
 }  // namespace apps
