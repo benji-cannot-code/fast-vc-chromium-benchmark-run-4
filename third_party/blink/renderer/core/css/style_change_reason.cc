f433cedbda76da301331ac791be23b66c1b20caa
Jiewei Qian <qjw@chromium.org>
2023-10-25 05:26:23 +0000

    Revert "Invalidate the entire document when turning on accessibility"
    This reverts commit 8958c2452402836d695c74cf7b95c3ed765b6354.
    
    Reason for revert: Causing consistent failure on Linux ASAN LSAN
    Sample failure: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ASan%20LSan%20Tests%20(1)/119801/overview
    
    Original change's description:
    > Invalidate the entire document when turning on accessibility
    >
    > This uncovered a few cases where we need to update the lifecycle as
    > a result of this invalidation (printing, AOM, devtools). These
    > cases may have been pre-existing bugs.
    >
    > Bug: 1485824
    >
    > Change-Id: I17b64fc35c9a76ad90c7bb29cc183c5b71e866c9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4970122
    > Reviewed-by: Mason Freed <masonf@chromium.org>
    > Auto-Submit: Chris Harrelson <chrishtr@chromium.org>
    > Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1214578}
    
    Bug: 1485824
    Change-Id: I4f1c0d56100e474fd3d1d498877704dc4e47c87c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4975220
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Jiewei Qian <qjw@chromium.org>
    Auto-Submit: Jiewei Qian <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214654}

--- third_party/blink/renderer/core/css/style_change_reason.cc
+++ third_party/blink/renderer/core/css/style_change_reason.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 namespace style_change_reason {
-const char kAccessibility[] = "Accessibility";
 const char kActiveStylesheetsUpdate[] = "ActiveStylesheetsUpdate";
 const char kAnimation[] = "Animation";
 const char kAttribute[] = "Attribute";
--- third_party/blink/renderer/core/frame/local_frame_view.cc
+++ third_party/blink/renderer/core/frame/local_frame_view.cc
@@ -3977,8 +3977,6 @@ void LocalFrameView::PaintOutsideOfLifecycle(GraphicsContext& context,
                                              const CullRect& cull_rect) {
   DCHECK(PaintOutsideOfLifecycleIsAllowed(context, *this));
 
-  UpdateAllLifecyclePhasesExceptPaint(DocumentUpdateReason::kPrinting);
-
   SCOPED_UMA_AND_UKM_TIMER(GetUkmAggregator(), LocalFrameUkmAggregator::kPaint);
 
   ForAllNonThrottledLocalFrameViews([](LocalFrameView& frame_view) {
