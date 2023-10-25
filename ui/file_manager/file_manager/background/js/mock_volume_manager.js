128f076914a255854b5ebc2273937695d8762dc0
Wenbo Jie <wenbojie@chromium.org>
2023-10-25 00:57:10 +0000

    [Files F2] Support SinglePartitionFormat flag on volumes
    Bug: b:305119034
    Change-Id: Ifeeef9cf9c84d5ab23f9d023dcca019a3540e6bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4954355
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214563}

--- ui/file_manager/file_manager/background/js/mock_volume_manager.js
+++ ui/file_manager/file_manager/background/js/mock_volume_manager.js
@@ -226,6 +226,13 @@ export class MockVolumeManager {
           /** @type VolumeManagerCommon.FileSystemType */ ('fusebox');
     }
 
+    let source = VolumeManagerCommon.Source.NETWORK;
+    if (type === VolumeManagerCommon.VolumeType.ARCHIVE) {
+      source = VolumeManagerCommon.Source.FILE;
+    } else if (type === VolumeManagerCommon.VolumeType.REMOVABLE) {
+      source = VolumeManagerCommon.Source.DEVICE;
+    }
+
     // If there's no label set it to volumeId to make it shorter to write
     // tests.
     const volumeInfo = new VolumeInfoImpl(
@@ -243,7 +250,7 @@ export class MockVolumeManager {
         false,                                      // hasMedia
         false,                                      // configurable
         false,                                      // watchable
-        VolumeManagerCommon.Source.NETWORK,         // source
+        source,                                     // source
         diskFileSystemType,                         // diskFileSystemType
         // @ts-ignore: error TS2345: Argument of type '{}' is not assignable to
         // parameter of type 'IconSet'.
