eb206f13273ba76aacca1dc6cae8fb7d02effb80
Yongshun Liu <yongshun@chromium.org>
2023-10-25 01:55:24 +0000

    bento_button: Do not show context menu when there is only one desk
    With this change, it will no longer show the context menu when there is
    only one desk.
    
    Bug: b/306689896
    Test: Manual
    Change-Id: I94e6c448e3b4ef1ba3da2ff6f453efe8e0afc3b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4972675
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214595}

--- ash/wm/desks/desk_mini_view.cc
+++ ash/wm/desks/desk_mini_view.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/desks/desk_preview_view.h"
 #include "ash/wm/desks/desk_textfield.h"
 #include "ash/wm/desks/desks_constants.h"
+#include "ash/wm/desks/desks_controller.h"
 #include "ash/wm/desks/desks_restore_util.h"
 #include "ash/wm/float/float_controller.h"
 #include "ash/wm/overview/overview_constants.h"
@@ -379,6 +380,12 @@ bool DeskMiniView::IsPointOnMiniView(const gfx::Point& screen_location) const {
 }
 
 void DeskMiniView::OpenContextMenu(ui::MenuSourceType source) {
+  // When there is only one desk, do nothing.
+  DesksController* desk_controller = DesksController::Get();
+  if (!desk_controller->CanRemoveDesks()) {
+    return;
+  }
+
   is_context_menu_open_ = true;
   base::UmaHistogramBoolean(
       owner_bar_->type() == DeskBarViewBase::Type::kDeskButton
@@ -396,7 +403,7 @@ void DeskMiniView::OpenContextMenu(ui::MenuSourceType source) {
   context_menu_ = std::make_unique<DeskActionContextMenu>(
       ContainsAppWindows(desk_)
           ? absl::make_optional(
-                DesksController::Get()->GetCombineDesksTargetName(desk_))
+                desk_controller->GetCombineDesksTargetName(desk_))
           : absl::nullopt,
       show_on_top ? views::MenuAnchorPosition::kBubbleTopRight
                   : views::MenuAnchorPosition::kBubbleBottomRight,
