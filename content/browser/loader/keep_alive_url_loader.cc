9f4576b1183a620eca166f89b4a4fdeba974feff
Jiewei Qian <qjw@chromium.org>
2023-10-25 05:41:14 +0000

    Revert "Allow in-browser keepalive req to survive longer after browser shutdown"
    This reverts commit f4e9841129f5d2b5c152d955834bdf0adca208ef.
    
    Reason for revert: Speculative revert for causing failures in
    ChromeKeepAliveURLBrowserTest.ReceiveResponseAfterBrowserShutdown
    
    Sample failure: https://ci.chromium.org/ui/b/8766293823566538369
    
    Original change's description:
    > Allow in-browser keepalive req to survive longer after browser shutdown
    >
    > As [1] points out, fetch keepalive requests should be able to
    > survive from sudden browser shutdown.
    >
    > This CL utilizes the existing mechanism from [2] to let
    > in-browser keepalive & fetchLater requests being able to complete
    > by notifying browser for a 500ms delay [3]
    >
    > The same enterprise policy introduced in [4] should still work
    > for this change.
    >
    > [1]: https://crbug.com/1465781#c50
    > [2]: https://crrev.com/c/2494195/16/content/browser/renderer_host/keep_alive_handle_factory.cc
    > [3]: https://docs.google.com/document/d/1cBfXgMdpE-vimXD1FGWeY78Dt_wPiixFcmPk9ksZfeI/edit?disco=AAAAI4jyqP4
    > [4]: https://crrev.com/c/2703054
    >
    > Bug: 1465781,1356128
    > Change-Id: Id888d6be621e2d0815469c1a64c465862d3c66a8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4951005
    > Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    > Reviewed-by: Bo Liu <boliu@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1214598}
    
    Bug: 1465781,1356128
    Change-Id: I5e58ad716a0707c746d6fdb89f09b1e703ab92ec
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4975079
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Jiewei Qian <qjw@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Jiewei Qian <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214657}

--- content/browser/loader/keep_alive_url_loader.cc
+++ content/browser/loader/keep_alive_url_loader.cc
@@ -17,9 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/policy_container_host.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_thread.h"
-#include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/url_loader_throttles.h"
-#include "content/public/common/content_client.h"
 #include "content/public/common/url_utils.h"
 #include "net/base/load_flags.h"
 #include "net/http/http_request_headers.h"
@@ -79,8 +77,7 @@ enum class FetchLaterBrowserMetricType {
   kStartedAfterInitiatorDisconnected = 1,
   kStartedByInitiator = 2,
   kCancelledAfterTimeLimit = 3,
-  kStartedWhenShutdown = 4,
-  kMaxValue = kStartedWhenShutdown,
+  kMaxValue = kCancelledAfterTimeLimit,
 };
 
 void LogFetchLaterMetric(const FetchLaterBrowserMetricType& type) {
@@ -384,8 +381,6 @@ void KeepAliveURLLoader::Start() {
   }
   base::UmaHistogramBoolean("FetchKeepAlive.Browser.Total.Started", true);
 
-  GetContentClient()->browser()->OnKeepaliveRequestStarted(browser_context_);
-
   // Asks the network service to create a URL loader with passed in params.
   network_loader_factory_->CreateLoaderAndStart(
       loader_.BindNewPipeAndPassReceiver(), request_id_, options_,
@@ -434,9 +429,6 @@ KeepAliveURLLoader::~KeepAliveURLLoader() {
   TRACE_EVENT_NESTABLE_ASYNC_END0("loading", "KeepAliveURLLoader", request_id_);
 
   disconnected_loader_timer_.Stop();
-  if (IsStarted()) {
-    GetContentClient()->browser()->OnKeepaliveRequestFinished();
-  }
 }
 
 void KeepAliveURLLoader::set_on_delete_callback(
@@ -947,17 +939,6 @@ void KeepAliveURLLoader::OnDisconnectedLoaderTimerFired() {
   LogFetchKeepAliveMetric(
       FetchKeepAliveBrowserMetricType::kCancelledAfterTimeLimit);
   DeleteSelf();
-}
-
-void KeepAliveURLLoader::Shutdown() {
-  if (!IsStarted()) {
-    CHECK(IsFetchLater());
-    LogFetchLaterMetric(FetchLaterBrowserMetricType::kStartedWhenShutdown);
-    // At this point, browser is shutting down, and renderer termination has not
-    // reached browser. It is the last chance to start loading the request from
-    // here.
-    Start();
-  }
 }
 
 bool KeepAliveURLLoader::IsFetchLater() const {
