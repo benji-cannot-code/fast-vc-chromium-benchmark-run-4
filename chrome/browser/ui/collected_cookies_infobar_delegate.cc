d4edb3f68266588ee75300eca773e46a97a98c33
Mariam Ali <alimariam@google.com>
2023-10-25 15:56:33 +0000

    Site Data: update reload info bar message string
    screenshot: http://screen/5uoST9bBhsURcpC
    
    Bug: 1492466
    Change-Id: Id5c0e9afb02b62cd32d6baa5249f91f843844cf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4974443
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Mariam Ali <alimariam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1214883}

--- chrome/browser/ui/collected_cookies_infobar_delegate.cc
+++ chrome/browser/ui/collected_cookies_infobar_delegate.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/vector_icons/vector_icons.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/ui_base_features.h"
 
 // static
 void CollectedCookiesInfoBarDelegate::Create(
@@ -34,7 +35,9 @@ CollectedCookiesInfoBarDelegate::GetIdentifier() const {
 }
 
 const gfx::VectorIcon& CollectedCookiesInfoBarDelegate::GetVectorIcon() const {
-  return vector_icons::kCookieIcon;
+  return features::IsChromeRefresh2023()
+             ? vector_icons::kSettingsChromeRefreshIcon
+             : vector_icons::kSettingsIcon;
 }
 
 std::u16string CollectedCookiesInfoBarDelegate::GetMessageText() const {
