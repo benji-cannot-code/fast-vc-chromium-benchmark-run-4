72c8dc5668db1266e6ccf3cf7a42f13b1e957758
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-10-25 15:53:49 +0000

    Fix inconsistent default value of TestSyncService::GetAccountInfo()
    The rest of the methods mimicked a user with sync-the-feature enabled
    but not this one.
    
    Change-Id: Ib5712e5a9768fff9bb6aa3366a681b3969c1f993
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4966492
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1214880}

--- components/send_tab_to_self/entry_point_display_reason_unittest.cc
+++ components/send_tab_to_self/entry_point_display_reason_unittest.cc
@@ -53,6 +53,7 @@ class EntryPointDisplayReasonTest : public ::testing::Test {
  public:
   EntryPointDisplayReasonTest() {
     pref_service_.registry()->RegisterBooleanPref(prefs::kSigninAllowed, true);
+    sync_service_.SetAccountInfo(CoreAccountInfo());
   }
 
   syncer::TestSyncService* sync_service() { return &sync_service_; }
