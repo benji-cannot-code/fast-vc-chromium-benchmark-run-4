572558f334fff1cf8d60efedc31992cf668f05d2
David Benjamin <davidben@chromium.org>
2023-10-25 15:16:10 +0000

    Avoid UB in PickleIterator::ReadBool
    Not all bit patterns are valid bools. Instead, ReadBool should read into
    a uint8_t first and then inspect the result. Additionally, always use
    the memcpy path, rather than the pointer casting path. The pointer cast
    is a strict aliasing violation (not that we enable that) and requires we
    reason about alignment. I think alignment may actually be okay here, but
    since every compiler understands memcpy, just do the straightforward and
    safe thing.
    
    Bug: 1394755
    Change-Id: I0d6b022f6018e0c0d8b24271dfb567771faf8d00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4973481
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214859}

--- base/pickle.cc
+++ base/pickle.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstdlib>
 #include <limits>
 #include <ostream>
+#include <type_traits>
 
 #include "base/bits.h"
 #include "base/numerics/safe_conversions.h"
@@ -29,13 +30,13 @@ PickleIterator::PickleIterator(const Pickle& pickle)
 
 template <typename Type>
 inline bool PickleIterator::ReadBuiltinType(Type* result) {
+  static_assert(
+      std::is_integral_v<Type> && !std::is_same_v<Type, bool>,
+      "This method is only safe with to use with types without padding bits.");
   const char* read_from = GetReadPointerAndAdvance<Type>();
   if (!read_from)
     return false;
-  if (sizeof(Type) > sizeof(uint32_t))
-    memcpy(result, read_from, sizeof(*result));
-  else
-    *result = *reinterpret_cast<const Type*>(read_from);
+  memcpy(result, read_from, sizeof(*result));
   return true;
 }
 
@@ -80,7 +81,14 @@ inline const char* PickleIterator::GetReadPointerAndAdvance(
 }
 
 bool PickleIterator::ReadBool(bool* result) {
-  return ReadBuiltinType(result);
+  // Not all bit patterns are valid bools. Avoid undefined behavior by reading a
+  // type with no padding bits, then converting to bool.
+  uint8_t v;
+  if (!ReadBuiltinType(&v)) {
+    return false;
+  }
+  *result = v != 0;
+  return true;
 }
 
 bool PickleIterator::ReadInt(int* result) {
