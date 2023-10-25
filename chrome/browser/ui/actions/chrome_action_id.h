dc4ddb2e2e2dddc17e0214e0961dab74c068fdc8
Timofey Chudakov <tchudakov@google.com>
2023-10-25 13:45:49 +0000

    [mullet m3] Rename UI string and context menu command ids.
    Autofill manual fallback will allow users to generate the Autofill
    suggestions for unclassified fields. This will be done through via
    the same Chrome context menu entry. This CL renames UI string
    and context menu command ids to accommodate for these changes.
    
    Bug: 1493361
    Change-Id: Ibf150c816760b86ffc8db47a30f60715ba218b1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4966709
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214818}

--- chrome/browser/ui/actions/chrome_action_id.h
+++ chrome/browser/ui/actions/chrome_action_id.h
@@ -499,8 +499,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E(kActionContentContextAutofillFeedback, \
     IDC_CONTENT_CONTEXT_AUTOFILL_FEEDBACK) \
   /* Autofill context menu commands */ \
-  E(kActionContentContextAutofillFallbackAutocompleteUnrecognized, \
-    IDC_CONTENT_CONTEXT_AUTOFILL_FALLBACK_AUTOCOMPLETE_UNRECOGNIZED) \
+  E(kActionContentContextAutofillFallbackAddress, \
+    IDC_CONTENT_CONTEXT_AUTOFILL_FALLBACK_ADDRESS) \
   /* Live Caption */ \
   E(kActionLiveCaption, IDC_LIVE_CAPTION) \
   /* Device API system tray icon */ \
