0ed9ba02a991d74a0f10bf9fd261d6a8fee20c5b
Etienne Pierre-doray <etiennep@chromium.org>
2023-10-25 18:19:18 +0000

    [tracing] Plumb InterceptorConfig through mojo
    This is necessary to implement interceptor in Chrome.
    The config is sent to the tracing service.
    tracing.mojom.InterceptorConfig message reflects
    perfetto::protos::gen::InterceptorConfig
    
    https://source.chromium.org/chromium/chromium/src/+/main:third_party/perfetto/protos/perfetto/config/interceptor_config.proto;l=26?q=InterceptorConfig%20proto&ss=chromium
    
    Bug: 1465855
    Change-Id: I9cbf423a53e61267b2605334989095f66a9e8443
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4887588
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214971}

--- services/tracing/public/mojom/data_source_config_mojom_traits.h
+++ services/tracing/public/mojom/data_source_config_mojom_traits.h
@@ -13,8 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "mojo/public/cpp/bindings/struct_traits.h"
 #include "services/tracing/public/mojom/perfetto_service.mojom-shared.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/perfetto/include/perfetto/tracing/core/chrome_config.h"
 #include "third_party/perfetto/include/perfetto/tracing/core/data_source_config.h"
+#include "third_party/perfetto/protos/perfetto/config/interceptor_config.gen.h"
 
 namespace mojo {
 template <>
@@ -44,6 +46,13 @@ class StructTraits<tracing::mojom::DataSourceConfigDataView,
   static const std::string& track_event_config_raw(
       const perfetto::DataSourceConfig& src) {
     return src.track_event_config_raw();
+  }
+  static absl::optional<perfetto::protos::gen::InterceptorConfig>
+  interceptor_config(const perfetto::DataSourceConfig& src) {
+    if (src.has_interceptor_config()) {
+      return src.interceptor_config();
+    }
+    return absl::nullopt;
   }
 
   static bool Read(tracing::mojom::DataSourceConfigDataView data,
