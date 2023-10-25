67bd63aeeb39ce541c1e38997ff4805b60bfdd53
Andrea Orru <andreaorru@chromium.org>
2023-10-25 08:50:55 +0000

    lacros: Check user dir is not accessed pre-login
    As part of the effort to execute more operations at login screen
    before blocking waiting for the user to login, we want to remove
    accesses to the cryptohome that happen early in the initialization
    process.
    
    This change ensures (via DCHECKs) that any access to the cryptohome
    happens after user dir initialization when prelaunching.
    
    BUG=1495212
    TEST=CQ
    tast run ${DUT} lacros.Basic
    tast run ${DUT} lacros.LoginScreenLaunch.rootfs
    
    Change-Id: Id6df5e28216beb8058e8349ca849bf53e9f46daf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4944753
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214708}

--- chrome/common/chrome_paths.cc
+++ chrome/common/chrome_paths.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chromeos/crosapi/cpp/crosapi_constants.h"  // nogncheck
 #include "chromeos/lacros/lacros_paths.h"
+#include "chromeos/startup/startup.h"  // nogncheck
 #endif
 
 namespace {
@@ -187,6 +188,12 @@ bool PathProvider(int key, base::FilePath* result) {
   base::FilePath cur;
   switch (key) {
     case chrome::DIR_USER_DATA:
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+      // Check that the user data directory is not accessed before
+      // initialization when prelaunching at login screen.
+      DCHECK(chromeos::lacros_paths::IsInitializedUserDataDir() ||
+             !chromeos::IsLaunchedWithPostLoginParams());
+#endif
       if (!GetDefaultUserDataDirectory(&cur)) {
         return false;
       }
