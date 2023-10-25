7e1f6af35a65c4516096a79dda6a96b89728d0a2
Paul Adedeji <pauladedeji@google.com>
2023-10-25 18:14:36 +0000

    Switch descriptor b's menu to a combobox.
    screenshot/44dRvFd8aL6pMk7
    
    Change-Id: I96aeef1312a075a569a3b48a3fa9044ea5306614
    Bug: b/301444784
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4970367
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1214967}

--- chrome/test/data/webui/side_panel/customize_chrome/wallpaper_search_test.ts
+++ chrome/test/data/webui/side_panel/customize_chrome/wallpaper_search_test.ts
@@ -81,8 +81,6 @@ suite('WallpaperSearchTest', () => {
       await flushTasks();
 
       assertNotStyle(
-          $$(wallpaperSearchElement, '#descriptorBtnB')!, 'display', 'none');
-      assertNotStyle(
           $$(wallpaperSearchElement, '#descriptorBtnC')!, 'display', 'none');
       assertEquals(
           2,
@@ -92,7 +90,7 @@ suite('WallpaperSearchTest', () => {
       assertEquals(
           1,
           wallpaperSearchElement.shadowRoot!
-              .querySelectorAll('#descriptorMenuB .dropdown-item')
+              .querySelectorAll('#descriptorComboboxB .dropdown-item')
               .length);
       assertEquals(
           3,
@@ -132,7 +130,8 @@ suite('WallpaperSearchTest', () => {
           wallpaperSearchElement,
           '#descriptorComboboxA .dropdown-item')!.click();
       $$<HTMLElement>(
-          wallpaperSearchElement, '#descriptorMenuB .dropdown-item')!.click();
+          wallpaperSearchElement,
+          '#descriptorComboboxB .dropdown-item')!.click();
       $$<HTMLElement>(
           wallpaperSearchElement, '#descriptorMenuC .dropdown-item')!.click();
       $$<HTMLElement>(
