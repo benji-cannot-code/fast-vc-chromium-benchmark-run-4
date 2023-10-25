cb7dfc27ee919e025f6939bdd4491b354f8ba8f7
Will Harris <wfh@chromium.org>
2023-10-25 04:47:13 +0000

    Do not use secure temp directory in browser tests
    Normally, tests have their own scoped TMP directory set by the test
    runner that is cleaned up after any test, including those that crash
    or terminate prematurely.
    
    However, when running tests as admin, the temp directory is pointed
    to %systemroot%\SystemTemp instead of the value of TMP set by the
    test runner.
    
    This causes temp files to slowly build up in %systemroot%\SystemTemp,
    which cannot be overridden by the test runner.
    
    This CL changes browser tests (e.g. content_browsertests and
    browser_tests) to instead use TMP environment variable for temporary
    files instead of %systemroot%\SystemTemp so that these files can
    be cleaned up
    
    Since browser tests are intended to verify browser behavior and
    the normal browser behavior is not running as an admin, I feel that
    this change actually improves the match between test and reality
    for theses types of tests.
    
    BUG=1487776
    
    Change-Id: I291816e5f1046d528a5c69287c4215b0b34820f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4903173
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214643}

--- content/test/content_browser_test_test.cc
+++ content/test/content_browser_test_test.cc
@@ -10,8 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_switches.h"
 #include "base/command_line.h"
 #include "base/containers/contains.h"
+#include "base/files/file_path.h"
+#include "base/files/scoped_temp_dir.h"
 #include "base/functional/bind.h"
 #include "base/location.h"
+#include "base/path_service.h"
 #include "base/process/launch.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
@@ -445,5 +448,21 @@ INSTANTIATE_TEST_SUITE_P(
     ::testing::Values(SkipLocation::kSetUpInProcessBrowserTestFixture,
                       SkipLocation::kSetUpCommandLine,
                       SkipLocation::kSetUpOnMainThread));
+
+// This test verifies that CreateUniqueTempDir always creates a dir underneath
+// the temp directory when running in a browser test. This is needed because, on
+// Windows, when running elevated, CreateUniqueTempDir would normally create a
+// temp dir in a secure location (e.g. %systemroot%\SystemTemp) but, for browser
+// tests, this behavior is explicitly overridden to avoid leaving temp files in
+// this system dir after tests complete. See BrowserTestBase::BrowserTestBase.
+IN_PROC_BROWSER_TEST_F(ContentBrowserTest, TempPathLocation) {
+  base::ScopedAllowBlockingForTesting allow_blocking;
+
+  base::ScopedTempDir scoped_dir;
+  EXPECT_TRUE(scoped_dir.CreateUniqueTempDir());
+
+  base::FilePath temp_path = base::PathService::CheckedGet(base::DIR_TEMP);
+  EXPECT_TRUE(temp_path.IsParent(scoped_dir.GetPath()));
+}
 
 }  // namespace content
