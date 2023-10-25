9e1ba08197aa7040ea579800c45ec937abf0b9bd
Timofey Chudakov <tchudakov@google.com>
2023-10-25 12:13:28 +0000

    [mullet m3] No manual fallback context menu entry when user has no AutofillProfiles stored.
    Bug: 1493361
    Change-Id: I0fdfb7c03ac594688a3e51f711001f06f7afd6b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4966529
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1214768}

--- chrome/browser/ui/autofill/autofill_context_menu_manager.cc
+++ chrome/browser/ui/autofill/autofill_context_menu_manager.cc
@@ -265,7 +265,11 @@ bool AutofillContextMenuManager::ShouldAddAutofillManualFallbackItem(
     return false;
   }
 
-  return ShouldAddAutofillManualFallbackForAutocompleteUnrecognized(driver) ||
+  if (ShouldAddAutofillManualFallbackForAutocompleteUnrecognized(driver)) {
+    return true;
+  }
+
+  return !personal_data_manager_->GetProfiles().empty() &&
          base::FeatureList::IsEnabled(
              features::kAutofillForUnclassifiedFieldsAvailable);
 }
