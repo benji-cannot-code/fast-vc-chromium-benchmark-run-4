0d42892aee19c4534f51798c36fdae2fd71b2d83
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2023-10-25 20:55:23 +0000

    Revert "Move side panel code out of PinnedToolbarActionsContainer."
    This reverts commit a46498b07782a9c1a27034354734a5420a2f983b.
    
    Reason for revert:
    LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8766232482643791473
    
    Sample failed build: https://ci.chromium.org/b/8766232482643791473
    
    If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8766232482643791473&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F4973999
    
    Original change's description:
    > Move side panel code out of PinnedToolbarActionsContainer.
    >
    > We want to make sure that PinnedToolbarActionsContainer isn't tightly
    > coupled with side panel so that it can hold non-side panel actions in
    > the future.
    >
    > Bug: b:299463334
    > Change-Id: I46412fbe083ae40e63c52b1ef555f7e57253087b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4973999
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Darryl James <dljames@chromium.org>
    > Commit-Queue: Caroline Rising <corising@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1215065}
    >
    
    Bug: b:299463334
    Change-Id: I7f5e68adce58a5ce3da0a0693e58ed46830e42bf
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4977247
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1215090}

--- chrome/browser/ui/views/toolbar/pinned_toolbar_actions_container.h
+++ chrome/browser/ui/views/toolbar/pinned_toolbar_actions_container.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/ui/toolbar/pinned_toolbar_actions_model.h"
+#include "chrome/browser/ui/views/side_panel/side_panel_coordinator.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_button.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_icon_container_view.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -75,6 +76,7 @@ class PinnedToolbarActionsContainer
   void AddPinnedActionButtonFor(const actions::ActionId& id);
   void RemovePinnedActionButtonFor(const actions::ActionId& id);
   PinnedActionToolbarButton* GetPinnedButtonFor(const actions::ActionId& id);
+  SidePanelCoordinator* GetSidePanelCoordinator();
 
   const raw_ptr<BrowserView> browser_view_;
 
