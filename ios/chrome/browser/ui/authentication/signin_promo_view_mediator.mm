0f477b5d54fa151a920dc3064160a070633746e5
Jérôme Lebel <jlebel@chromium.org>
2023-10-25 13:49:54 +0000

    [iOS] Add signin promo close user action
    This is for iOS only.
    Related to: http://crbug.com/1494171
    
    Change-Id: I16c8619c768085e7e3937aa89b8723b2d3a72d11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4966523
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214820}

--- ios/chrome/browser/ui/authentication/signin_promo_view_mediator.mm
+++ ios/chrome/browser/ui/authentication/signin_promo_view_mediator.mm
@@ -1110,6 +1110,7 @@ id<SystemIdentity> GetDisplayedIdentity(
          self.accessPoint ==
              signin_metrics::AccessPoint::ACCESS_POINT_NTP_FEED_TOP_PROMO)
       << base::SysNSStringToUTF8([self description]);
+  base::RecordAction(base::UserMetricsAction("Signin_Promo_Close"));
   self.signinPromoViewState = SigninPromoViewState::kClosed;
   const char* alreadySeenSigninViewPreferenceKey =
       AlreadySeenSigninViewPreferenceKey(self.accessPoint);
