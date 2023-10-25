0118db9773ce23e8c849194a6a3a119e30e3148d
Mitsuru Oshima <oshima@chromium.org>
2023-10-25 02:03:27 +0000

    DisplayManager cleanup
    Remove arg from AddRemoveDisplay. This was introduced in
    crrev.com/c/1269533, but it shouldn't have used this method.
    
    I updated the UpdateDisplay test API to achieve the same,
    and updated the test.
    
    I'll move AddRemoveDisplay out from DisplayManager to utility after this.
    
    Bug: 1495186
    
    Change-Id: I07445f5dedc831355fd766657159114afc3386e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4968943
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214597}

--- chrome/browser/ash/policy/display/display_resolution_handler_browsertest.cc
+++ chrome/browser/ash/policy/display/display_resolution_handler_browsertest.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/display_layout.h"
 #include "ui/display/display_switches.h"
 #include "ui/display/manager/display_manager.h"
+#include "ui/display/test/display_manager_test_api.h"
 
 namespace policy {
 
@@ -85,12 +86,13 @@ PolicyValue GetPolicySetting() {
 }
 
 void AddExternalDisplay(display::DisplayManager* display_manager) {
-  display_manager->AddRemoveDisplay(
-      {display::ManagedDisplayMode(gfx::Size(800, 600), 30.0, false, false),
-       display::ManagedDisplayMode(gfx::Size(800, 600), 60.0, false, false),
-       display::ManagedDisplayMode(gfx::Size(1280, 800), 60.0, false, false),
-       display::ManagedDisplayMode(gfx::Size(1920, 1080), 30.0, false, false),
-       display::ManagedDisplayMode(gfx::Size(1920, 1080), 60.0, false, true)});
+  display::test::DisplayManagerTestApi test_api(display_manager);
+  test_api.UpdateDisplay(
+      "1280x800,1920x1080#1920x1080%60|800x600%30|800x600%60|1280x800%60|"
+      "1920x1080%30",
+      /*from_native_platform=*/true);
+
+  // Policy change is applied in the posted task.
   base::RunLoop().RunUntilIdle();
 }
 
