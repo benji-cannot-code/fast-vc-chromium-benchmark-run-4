c590e26a157d2cb2d2a1a1198cc28d0077ec0ed8
Viktor Semeniuk <vsemeniuk@google.com>
2023-10-25 10:32:13 +0000

    Adding a separate build target for PasswordStore related files
    This CLs removes some unused includes and moves helper functions around.
    This breaks dependency cycle and allows extraction of a new build
    target. Later files from this build target will be moved to a subfolder.
    
    Bug: 1479425
    Change-Id: I08f77888f72f8b445a661eb139d5f8ad5414778b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4964683
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214741}

--- components/password_manager/core/browser/password_store.cc
+++ components/password_manager/core/browser/password_store.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/affiliation/affiliation_service.h"
 #include "components/password_manager/core/browser/password_form.h"
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
-#include "components/password_manager/core/browser/password_manager_util.h"
 #include "components/password_manager/core/browser/password_store_backend.h"
 #include "components/password_manager/core/browser/password_store_consumer.h"
 #include "components/password_manager/core/browser/password_store_util.h"
