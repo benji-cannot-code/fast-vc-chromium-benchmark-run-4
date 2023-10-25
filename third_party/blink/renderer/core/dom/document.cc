fd845995fd8f47c363dc910e6213dfe47ece9eec
Chris Harrelson <chrishtr@chromium.org>
2023-10-25 18:59:31 +0000

    Reland "Invalidate the entire document when turning on accessibility"
    This reverts commit 7083fdeec66892b9ac860a3fa5247c320e1b8600.
    
    Reason for revert: fixed failing test, by adding code to ensure AXContext is created in InspectorOverlayAgent::SetInspectTool()"
    
    Fixed: 1495644
    
    Original change's description:
    > Revert "Invalidate the entire document when turning on accessibility"
    >
    > This reverts commit 8958c2452402836d695c74cf7b95c3ed765b6354.
    >
    > Reason for revert: Causing consistent failure on Linux ASAN LSAN
    > Sample failure: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ASan%20LSan%20Tests%20(1)/119801/overview
    >
    > Original change's description:
    > > Invalidate the entire document when turning on accessibility
    > >
    > > This uncovered a few cases where we need to update the lifecycle as
    > > a result of this invalidation (printing, AOM, devtools). These
    > > cases may have been pre-existing bugs.
    > >
    > > Bug: 1485824
    > >
    > > Change-Id: I17b64fc35c9a76ad90c7bb29cc183c5b71e866c9
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4970122
    > > Reviewed-by: Mason Freed <masonf@chromium.org>
    > > Auto-Submit: Chris Harrelson <chrishtr@chromium.org>
    > > Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1214578}
    >
    > Bug: 1485824
    > Change-Id: I4f1c0d56100e474fd3d1d498877704dc4e47c87c
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4975220
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Jiewei Qian <qjw@chromium.org>
    > Auto-Submit: Jiewei Qian <qjw@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1214654}
    
    Bug: 1485824
    Change-Id: I90bec1ced0e9b9ae2f91c73f11d087c266529e00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4976744
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214997}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -2782,11 +2782,14 @@ void Document::EnsurePaintLocationDataValidForNode(
 
 void Document::GetPageDescription(uint32_t page_index,
                                   WebPrintPageDescription* description) {
-  GetPageDescription(*StyleForPage(page_index), description);
+  View()->UpdateLifecycleToLayoutClean(DocumentUpdateReason::kUnknown);
+  GetPageDescriptionNoLifecycleUpdate(*StyleForPage(page_index), description);
 }
 
-void Document::GetPageDescription(const ComputedStyle& style,
-                                  WebPrintPageDescription* description) {
+void Document::GetPageDescriptionNoLifecycleUpdate(
+    const ComputedStyle& style,
+    WebPrintPageDescription* description) {
+  DocumentLifecycle::DisallowTransitionScope scope(Lifecycle());
   const WebPrintPageDescription input_description = *description;
 
   switch (style.GetPageSizeType()) {
@@ -3188,6 +3191,14 @@ void Document::AddAXContext(AXContext* context) {
   if (!ax_object_cache_) {
     ax_object_cache_ =
         AXObjectCache::Create(*this, ComputeAXModeFromAXContexts(ax_contexts_));
+    // Invalidate style on the entire document, because accessibility
+    // needs to compute style on all elements, even those in
+    // content-visibility:auto subtrees.
+    if (documentElement()) {
+      documentElement()->SetNeedsStyleRecalc(
+          kSubtreeStyleChange, StyleChangeReasonForTracing::Create(
+                                   style_change_reason::kAccessibility));
+    }
     g_ax_object_cache_count++;
   }
 }
