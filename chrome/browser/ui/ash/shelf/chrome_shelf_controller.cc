0ea05b5788cf0cf4ca52660ce1188cb1d60d9df1
Vivian Pao <vpao@google.com>
2023-10-25 01:38:26 +0000

    [Promise Icons] Add accessible_name field to ShelfItem
    Add an accessible_name field to the ShelfItem struct. The
    accessible_name field is the text to be announced by the screen
    reader, which will differ from the main ShelfItem title for promise
    apps. The ShelfItem title for a promise app will be "waiting..." or
    "installing..." for the tooltip, while the screenreader should announce
    "<App Name, waiting..." or "<App Name>, installing".
    
    This CL sets this accessible_name field to the correct string but will
    still require a follow up CL to plumb the ShelfItem accessible_name
    through to the ShelfView for the screen reader.
    
    Bug: b/305140229
    Change-Id: Ieb5065e1e758a03cb7a750ab79d4f3dc54f85fbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4970892
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Vivian Pao <vpao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1214586}

--- chrome/browser/ui/ash/shelf/chrome_shelf_controller.cc
+++ chrome/browser/ui/ash/shelf/chrome_shelf_controller.cc
@@ -81,6 +81,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/shelf/chrome_shelf_controller_util.h"
 #include "chrome/browser/ui/ash/shelf/chrome_shelf_item_factory.h"
 #include "chrome/browser/ui/ash/shelf/chrome_shelf_prefs.h"
+#include "chrome/browser/ui/ash/shelf/shelf_controller_helper.h"
 #include "chrome/browser/ui/ash/shelf/shelf_extension_app_updater.h"
 #include "chrome/browser/ui/ash/shelf/shelf_spinner_controller.h"
 #include "chrome/browser/ui/browser.h"
@@ -1117,6 +1118,9 @@ void ChromeShelfController::OnPromiseAppUpdate(
         ShelfControllerHelper::ConvertPromiseStatusToAppStatus(update.Status());
     item.title =
         ShelfControllerHelper::GetLabelForPromiseStatus(update.Status());
+    item.accessible_name =
+        ShelfControllerHelper::GetAccessibleLabelForPromiseStatus(
+            update.Name(), update.Status());
   }
   model_->Set(index, item);
 }
@@ -1760,6 +1764,14 @@ void ChromeShelfController::ShelfItemAdded(int index) {
       if (is_promise_app != item.is_promise_app) {
         needs_update = true;
         item.is_promise_app = is_promise_app;
+      }
+
+      std::u16string accessible_name =
+          ShelfControllerHelper::GetPromiseAppAccessibleName(
+              latest_active_profile_, id.app_id);
+      if (is_promise_app && accessible_name != item.accessible_name) {
+        needs_update = true;
+        item.accessible_name = accessible_name;
       }
     }
 
