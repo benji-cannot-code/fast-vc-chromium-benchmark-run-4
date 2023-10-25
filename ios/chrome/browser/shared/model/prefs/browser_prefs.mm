5b83541014eb3de8c8824a23365f34a07a232746
Federica Germinario <fedegermi@google.com>
2023-10-25 12:16:54 +0000

    [ios] Move LastUsedFeedForGoodVisits key out of NSUserDefaults
    This CL moves LastUsedFeedForGoodVisits out of NSUserDefaults.
    
    The key has been moved in the PrefService. Note that there are two
    types of PrefServices, in this case the code was moved in the
    PrefService coming from BrowserState which is per account and is
    syncable between devices.
    Note that this kind of PrefService behaves differently from
    NSUserDefaults which is per device (and not per account).
    
    Bug: 1486770
    Change-Id: Ia7beacd29e39282f97e1539f195d26b923ef22ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4966482
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214770}

--- ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -629,6 +629,7 @@ void RegisterBrowserStatePrefs(user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterDoublePref(kLongFollowingFeedVisitTimeAggregateKey, 0.0);
   registry->RegisterDoublePref(kLongFeedVisitTimeAggregateKey, 0.0);
   registry->RegisterTimePref(kArticleVisitTimestampKey, base::Time());
+  registry->RegisterIntegerPref(kLastUsedFeedForGoodVisitsKey, 0);
 
   registry->RegisterBooleanPref(kSyncRequested, false);
 }
@@ -810,6 +811,10 @@ void MigrateObsoleteBrowserStatePrefs(PrefService* prefs) {
   // Added 10/2023.
   MigrateNSDatePreferenceFromUserDefaults(kArticleVisitTimestampKey, prefs,
                                           defaults);
+
+  // Added 10/2023.
+  MigrateIntegerPreferenceFromUserDefaults(kLastUsedFeedForGoodVisitsKey, prefs,
+                                           defaults);
 }
 
 void MigrateObsoleteUserDefault(void) {
--- ios/chrome/browser/ui/ntp/metrics/feed_metrics_constants.mm
+++ ios/chrome/browser/ui/ntp/metrics/feed_metrics_constants.mm
@@ -17,7 +17,7 @@ const char kLongDiscoverFeedVisitTimeAggregateKey[] =
     "LongDiscoverFeedInteractionTimeDelta";
 const char kLongFollowingFeedVisitTimeAggregateKey[] =
     "LongFollowingFeedInteractionTimeDelta";
-NSString* const kLastUsedFeedForGoodVisitsKey = @"LastUsedFeedForGoodVisits";
+const char kLastUsedFeedForGoodVisitsKey[] = "LastUsedFeedForGoodVisits";
 const char kLastInteractionTimeForGoodVisits[] =
     "LastInteractionTimeForGoodVisits";
 const char kLastInteractionTimeForDiscoverGoodVisits[] =
