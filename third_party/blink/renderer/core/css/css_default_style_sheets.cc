a1ee34959a69ece3c0cd0966856608c39dccc1b3
David Baron <dbaron@chromium.org>
2023-10-25 19:09:46 +0000

    Revert "Give <slot> element default unicode-bidi: isolate style."
    This reverts commit 198d41cfd6eac5fd9ee34e6314e34f938b6d31b5.
    
    Reason for revert: This reverts a change that was the first of two closely related changes.  Given that the other change, in https://crrev.com/c/4973701, turned out to have problems (and I haven't attempted to land it), I've proposed reverting both in the HTML spec at https://github.com/whatwg/html/pull/9880.  I'm therefore reverting this (the first half) until the HTML spec discussion is sorted out.
    
    Original change's description:
    > Give <slot> element default unicode-bidi: isolate style.
    >
    > This is behind the RuntimeEnabledFeatures::CSSPseudoDirEnabled() flag.
    >
    > This implements a part of the behavior described in:
    > https://github.com/whatwg/html/issues/3699#issuecomment-951423468
    > which has been specified in:
    > https://github.com/whatwg/html/pull/9554
    > https://github.com/whatwg/html/pull/9796
    >
    > Bug: 576815
    > Change-Id: Ice471838c7251fc69d87abfa723f3eef1a4c455a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800075
    > Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    > Reviewed-by: Di Zhang <dizhangg@chromium.org>
    > Commit-Queue: David Baron <dbaron@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1214439}
    
    Bug: 576815
    Change-Id: I0f52b30c345d4962a36a36e5d2651221db96bcde
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4974023
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: David Baron <dbaron@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215009}

--- third_party/blink/renderer/core/css/css_default_style_sheets.cc
+++ third_party/blink/renderer/core/css/css_default_style_sheets.cc
@@ -117,12 +117,6 @@ CSSDefaultStyleSheets::CSSDefaultStyleSheets()
   String quirks_rules = UncompressResourceAsASCIIString(IDR_UASTYLE_QUIRKS_CSS);
   quirks_style_sheet_ = ParseUASheet(quirks_rules);
 
-  if (RuntimeEnabledFeatures::CSSPseudoDirEnabled()) {
-    String dir_pseudo_rules =
-        UncompressResourceAsASCIIString(IDR_UASTYLE_DIR_PSEUDO_CSS);
-    dir_pseudo_style_sheet_ = ParseUASheet(dir_pseudo_rules);
-  }
-
   InitializeDefaultStyles();
 }
 
@@ -206,9 +200,6 @@ void CSSDefaultStyleSheets::InitializeDefaultStyles() {
   default_html_quirks_style_->AddRulesFromSheet(QuirksStyleSheet(),
                                                 ScreenEval());
   default_print_style_->AddRulesFromSheet(DefaultStyleSheet(), PrintEval());
-  if (dir_pseudo_style_sheet_) {
-    AddRulesToDefaultStyleSheets(dir_pseudo_style_sheet_, NamespaceType::kHTML);
-  }
 
   CHECK(default_html_style_->ViewTransitionsRules().empty())
       << "@view-transitions is not implemented for the UA stylesheet.";
@@ -514,7 +505,6 @@ void CSSDefaultStyleSheets::Trace(Visitor* visitor) const {
   visitor->Trace(marker_style_sheet_);
   visitor->Trace(form_controls_not_vertical_style_sheet_);
   visitor->Trace(form_controls_not_vertical_style_text_sheet_);
-  visitor->Trace(dir_pseudo_style_sheet_);
 }
 
 }  // namespace blink
