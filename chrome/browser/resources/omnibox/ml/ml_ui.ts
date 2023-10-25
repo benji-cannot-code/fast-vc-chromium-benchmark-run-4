f4c15d198d4d52095454d0e682c01665afbd94af
manukh <manukh@chromium.org>
2023-10-25 23:37:30 +0000

    [omnibox][ml][chrome://omnibox] Graph the signals v score (10/6)
    Maybe the graph can answer questions like:
    - How sensitive is the score to signal X?
    - Does the score increase monotonously with signal X?
    - Which signal is contributing most to the score?
    - Does the score per signal increment taper off?
    
    go/omnibox-ml-debug-graph
    
    Bug: 1405555
    Change-Id: I0b61cc170003c10fef306c85bec927530c934deb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4955493
    Reviewed-by: Angela Yoeurng <yoangela@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215193}

--- chrome/browser/resources/omnibox/ml/ml_ui.ts
+++ chrome/browser/resources/omnibox/ml/ml_ui.ts
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import '../strings.m.js';
 import './ml_calculator.js';
 import './ml_table.js';
+import './ml_chart.js';
 
 import {CustomElement} from 'chrome://resources/js/custom_element.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
@@ -14,6 +15,7 @@ import {Signals} from '../omnibox.mojom-webui.js';
 
 import {MlBrowserProxy} from './ml_browser_proxy.js';
 import {MlCalculatorElement} from './ml_calculator.js';
+import {MlChartElement} from './ml_chart.js';
 import {MlTableElement} from './ml_table.js';
 // @ts-ignore:next-line
 import sheet from './ml_ui.css' assert {type : 'css'};
@@ -42,16 +44,27 @@ export class MlUiElement extends CustomElement {
     const mlCalculator =
         this.getRequiredElement<MlCalculatorElement>('ml-calculator');
     const mlTable = this.getRequiredElement<MlTableElement>('ml-table');
+    const mlChart = this.getRequiredElement<MlChartElement>('ml-chart');
 
     this.getRequiredElement('#ml-sync-batch-url-scoring-disabled-warning')
         .hidden = loadTimeData.getBoolean('isMlSyncBatchUrlScoringEnabled');
     [mlCalculator, mlTable].forEach(
-        el => el.addEventListener('copied', async ({detail}) => {
-          this.notification = await detail.then(() => 'Copied!').catch(e => {
-            console.error('Failed to copy to clipboard:', e);
-            return 'Failed to copy :(';
-          });
+        el => el.addEventListener('copied', ({detail}) => {
+          detail.then(() => 'Copied!')
+              .catch(e => {
+                console.error('Failed to copy to clipboard:', e);
+                return 'Failed to copy :(';
+              })
+              .then(text => {
+                const notification =
+                    this.getRequiredElement('#copied-notification');
+                notification.textContent = text;
+                notification.classList.remove('fade-out');
+                setTimeout(() => notification.classList.add('fade-out'), 0);
+              });
         }));
+    mlCalculator.addEventListener(
+        'updated', () => mlChart.signals = mlCalculator.signals);
     mlTable.addEventListener(
         'match-selected', ({detail}) => mlCalculator.signals = detail);
 
@@ -59,17 +72,8 @@ export class MlUiElement extends CustomElement {
       // ML model was loaded.
       mlCalculator.mlBrowserProxy = this.mlBrowserProxy;
       mlTable.mlBrowserProxy = this.mlBrowserProxy;
+      mlChart.mlBrowserProxy = this.mlBrowserProxy;
     });
-  }
-
-  private set notification(text: string) {
-    const notification = this.getRequiredElement('#copied-notification');
-    notification.textContent = text;
-    notification.classList.remove('fade-out');
-    // Force dom reflow so that the browser doesn't dedupe the above and below
-    // classList changes.
-    notification.offsetHeight;
-    notification.classList.add('fade-out');
   }
 }
 
