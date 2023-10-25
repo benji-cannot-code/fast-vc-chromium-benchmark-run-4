76add4cdc66f3954caf4417fdc46acb73b8d6646
Xiaodan Zhu <zxdan@chromium.org>
2023-10-25 18:50:16 +0000

    fix the dangling raw ptr in PaginationView
    PaginationView uses RemoveChildViewT to remove its children with
    raw_ptr. It will have dangling raw_ptr error. The cl fixed the
    issue by resetting the raw_ptr while removing them.
    
    Bug: b:307408374
    Change-Id: I0769e604b20b9bceedd13bb77601dab461ef5bb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4968945
    Reviewed-by: Min Chen <minch@chromium.org>
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214988}

--- ash/style/pagination_view.cc
+++ ash/style/pagination_view.cc
@@ -235,7 +235,7 @@ class PaginationView::IndicatorContainer : public views::BoxLayoutView {
     DCHECK(buttons_.size());
     auto indicator_button = buttons_.back();
     buttons_.pop_back();
-    RemoveChildViewT(indicator_button);
+    RemoveChildViewT(std::exchange(indicator_button, nullptr));
   }
 
   // Gets indicator corresponding to the given page.
@@ -426,11 +426,8 @@ void PaginationView::CreateArrowButtons() {
 }
 
 void PaginationView::RemoveArrowButtons() {
-  RemoveChildViewT(backward_arrow_button_);
-  backward_arrow_button_ = nullptr;
-
-  RemoveChildViewT(forward_arrow_button_);
-  forward_arrow_button_ = nullptr;
+  RemoveChildViewT(std::exchange(backward_arrow_button_, nullptr));
+  RemoveChildViewT(std::exchange(forward_arrow_button_, nullptr));
 }
 
 void PaginationView::UpdateArrowButtonsVisiblity() {
@@ -486,8 +483,7 @@ void PaginationView::RemoveSelectorDot() {
     return;
   }
 
-  indicator_container_->RemoveChildViewT(selector_dot_);
-  selector_dot_ = nullptr;
+  indicator_container_->RemoveChildViewT(std::exchange(selector_dot_, nullptr));
 }
 
 void PaginationView::UpdateSelectorDot() {
