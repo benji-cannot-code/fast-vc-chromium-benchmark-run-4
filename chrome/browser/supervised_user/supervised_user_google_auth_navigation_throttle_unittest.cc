f541bf2f4cd3995bd05a036dadccbe2455ada9a3
Hans Wennborg <hans@chromium.org>
2023-10-25 13:27:12 +0000

    Fix operator== use in supervised_user_google_auth_navigation_throttle_unittest.cc
    There is an operator== overload for NavigationThrottle::ThrottleAction
    and ThrottleCheckResult (see the end of navigation_throttle.h), but not
    in the reverse order.
    
    Clang was until recently accepting the old code due to a bug.
    
    Bug: 1495664
    Change-Id: Iab36166df7374f37ffe80bf950a6b85ba3d52a00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4974445
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214802}

--- chrome/browser/supervised_user/supervised_user_google_auth_navigation_throttle_unittest.cc
+++ chrome/browser/supervised_user/supervised_user_google_auth_navigation_throttle_unittest.cc
@@ -114,8 +114,8 @@ TEST_F(SupervisedUserGoogleAuthNavigationThrottleTest,
   EXPECT_EQ(content::NavigationThrottle::PROCEED,
             CreateNavigationThrottle(GURL(kExampleURL))->WillStartRequest());
   EXPECT_EQ(
-      CreateNavigationThrottle(GURL(kGoogleSearchURL))->WillStartRequest(),
-      content::NavigationThrottle::PROCEED);
+      content::NavigationThrottle::PROCEED,
+      CreateNavigationThrottle(GURL(kGoogleSearchURL))->WillStartRequest());
   EXPECT_EQ(content::NavigationThrottle::PROCEED,
             CreateNavigationThrottle(GURL(kGoogleHomeURL))->WillStartRequest());
   EXPECT_EQ(content::NavigationThrottle::PROCEED,
@@ -153,8 +153,8 @@ TEST_F(SupervisedUserGoogleAuthNavigationThrottleTest,
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   EXPECT_EQ(
-      CreateNavigationThrottle(GURL(kGoogleSearchURL))->WillStartRequest(),
-      content::NavigationThrottle::DEFER);
+      content::NavigationThrottle::DEFER,
+      CreateNavigationThrottle(GURL(kGoogleSearchURL))->WillStartRequest());
   EXPECT_EQ(content::NavigationThrottle::DEFER,
             CreateNavigationThrottle(GURL(kGoogleHomeURL))->WillStartRequest());
   EXPECT_EQ(content::NavigationThrottle::DEFER,
@@ -162,17 +162,17 @@ TEST_F(SupervisedUserGoogleAuthNavigationThrottleTest,
 #elif BUILDFLAG(IS_ANDROID)
   SetPrimaryAccount(identity_manager(), kChildTestEmail,
                     signin::ConsentLevel::kSignin);
-  EXPECT_EQ(CreateNavigationThrottle(GURL(kGoogleSearchURL), true)
-                ->WillStartRequest(),
-            content::NavigationThrottle::DEFER);
+  EXPECT_EQ(content::NavigationThrottle::DEFER,
+            CreateNavigationThrottle(GURL(kGoogleSearchURL), true)
+                ->WillStartRequest());
   EXPECT_EQ(content::NavigationThrottle::DEFER,
             CreateNavigationThrottle(GURL(kGoogleHomeURL))->WillStartRequest());
   EXPECT_EQ(content::NavigationThrottle::DEFER,
             CreateNavigationThrottle(GURL(kYoutubeDomain))->WillStartRequest());
 #else
   EXPECT_EQ(
-      CreateNavigationThrottle(GURL(kGoogleSearchURL))->WillStartRequest(),
-      content::NavigationThrottle::PROCEED);
+      content::NavigationThrottle::PROCEED,
+      CreateNavigationThrottle(GURL(kGoogleSearchURL))->WillStartRequest());
   EXPECT_EQ(content::NavigationThrottle::PROCEED,
             CreateNavigationThrottle(GURL(kGoogleHomeURL))->WillStartRequest());
   EXPECT_EQ(content::NavigationThrottle::PROCEED,
@@ -201,8 +201,8 @@ TEST_F(SupervisedUserGoogleAuthNavigationThrottleTest,
   EXPECT_EQ(content::NavigationThrottle::PROCEED,
             CreateNavigationThrottle(GURL(kExampleURL))->WillStartRequest());
   EXPECT_EQ(
-      CreateNavigationThrottle(GURL(kGoogleSearchURL))->WillStartRequest(),
-      content::NavigationThrottle::DEFER);
+      content::NavigationThrottle::DEFER,
+      CreateNavigationThrottle(GURL(kGoogleSearchURL))->WillStartRequest());
   EXPECT_EQ(content::NavigationThrottle::DEFER,
             CreateNavigationThrottle(GURL(kGoogleHomeURL))->WillStartRequest());
   EXPECT_EQ(content::NavigationThrottle::DEFER,
