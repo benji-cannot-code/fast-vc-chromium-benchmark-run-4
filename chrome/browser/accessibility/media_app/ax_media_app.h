ad2ee3e8d6294028d2c1936bfbfbb8ed0d6b2836
Alvin Ji <alvinji@chromium.org>
2023-10-25 20:51:21 +0000

    Revert "Backlight OCR: Implemented the `DocumentUpdated` API"
    This reverts commit befe557d70b3d3553fc96a373d6f5deb0a743e27.
    
    Reason for revert: Failing on Win and Linux builder
    
    Win: https://ci.chromium.org/ui/test/chromium/ninja%3A%2F%2Fchrome%2Ftest%3Abrowser_tests%2FAXMediaAppHandlerTest.DocumentUpdated?q=VHASH%3Ada10d8dd957a4c13
    
    Linux:
    https://ci.chromium.org/ui/test/chromium/ninja%3A%2F%2Fchrome%2Ftest%3Abrowser_tests%2FAXMediaAppHandlerTest.DocumentUpdated?q=VHASH%3A91b15303e2090379
    
    Original change's description:
    > Backlight OCR: Implemented the `DocumentUpdated` API
    >
    > In an effort to make PDFs in the Media App on ChromeOS (AKA Backlight,)
    > accessible, we aim to acquire images from every page of the PDF,
    > send them to an OCR Service and build an accessibility tree from
    > the results.
    >
    > This patch implements the API that
    > informs the accessibility code that some of
    > the page of the PDF have been updated
    > and are ready for OCRing.
    >
    > Design doc:
    > https://docs.google.com/document/d/1k8OH4XGjZrLVA705Xfy6vzSwGL9DkfRt9DG5s66lRjQ/edit?usp=sharing&resourcekey=0-K_9Y1EMhNoo8Ez9RPbQNMA
    >
    > R=aleventhal@chromium.org
    >
    > AX-Relnotes: n/a.
    > bug: b/289012145
    > Change-Id: I1ef753bcd7839b097c373b1879d873161108260f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4943831
    > Auto-Submit: Nektarios Paisios <nektar@chromium.org>
    > Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    > Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    > Reviewed-by: Patti Lor <patricialor@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1214955}
    
    Change-Id: I5b43710e51cc1c88761e8a7ecff2c1e20cb18577
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4975873
    Auto-Submit: Alvin Ji <alvinji@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Alvin Ji <alvinji@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1215089}

--- chrome/browser/accessibility/media_app/ax_media_app.h
+++ chrome/browser/accessibility/media_app/ax_media_app.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
-#include "content/public/browser/browser_context.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/gfx/geometry/insets.h"
 
@@ -24,7 +23,6 @@ class AXMediaApp {
 
   virtual void OcrServiceEnabledChanged(bool enabled) = 0;
   virtual void AccessibilityEnabledChanged(bool enabled) = 0;
-  virtual content::BrowserContext* GetBrowserContext() const = 0;
   virtual SkBitmap RequestBitmap(uint64_t page_index) = 0;
   virtual void SetViewport(const gfx::Insets& viewport_box) = 0;
 
--- chrome/browser/accessibility/media_app/ax_media_app_handler_unittest.cc
+++ chrome/browser/accessibility/media_app/ax_media_app_handler_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/accessibility/accessibility_state_utils.h"
 #include "chrome/browser/accessibility/media_app/ax_media_app.h"
+#include "chrome/browser/accessibility/media_app/ax_media_app_handler.h"
 #include "chrome/browser/accessibility/media_app/ax_media_app_handler_factory.h"
 #include "chrome/browser/accessibility/media_app/test/fake_ax_media_app.h"
 #include "components/services/screen_ai/buildflags/buildflags.h"
@@ -48,9 +49,7 @@ class AXMediaAppHandlerTest : public testing::Test {
   AXMediaAppHandlerTest& operator=(const AXMediaAppHandlerTest&) = delete;
   ~AXMediaAppHandlerTest() override = default;
 
- protected:
   void SetUp() override {
-    testing::Test::SetUp();
 #if BUILDFLAG(ENABLE_SCREEN_AI_SERVICE)
     ASSERT_NE(nullptr, screen_ai::ScreenAIInstallState::GetInstance());
 #endif  // BUILDFLAG(ENABLE_SCREEN_AI_SERVICE)
@@ -61,8 +60,9 @@ class AXMediaAppHandlerTest : public testing::Test {
     ASSERT_NE(nullptr, handler_.get());
   }
 
+ protected:
 #if BUILDFLAG(ENABLE_SCREEN_AI_SERVICE)
-  TestScreenAIInstallState install_state_;
+  TestScreenAIInstallState install_state;
 #endif  // BUILDFLAG(ENABLE_SCREEN_AI_SERVICE)
   FakeAXMediaApp fake_media_app_;
   std::unique_ptr<AXMediaAppHandler> handler_;
