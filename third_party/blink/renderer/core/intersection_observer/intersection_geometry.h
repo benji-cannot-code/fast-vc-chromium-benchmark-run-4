4894db6263fffd67bdc0175b31f1f7d2cc5c8278
Xianzhu Wang <wangxianzhu@chromium.org>
2023-10-25 17:45:53 +0000

    Create RootGeometry lazily in IntersectionGeometry
    When we call IntersectionObservation::ComputeIntersection in a loop
    for intersection observations of the same intersection observer, we
    use the same RootGeometry for all invocations.
    
    Now let the caller create optional<RootGeometry> and pass it to
    ComputeIntersection, and IntersectionGeometry emplace it only when
    needed.
    
    This simplifies the code by combining the previous two versions of
    IntersectionObservation::ComputeIntersection() and
    IntersectionGeometry constructors into one, respectively.
    
    This performance impact will be probably trivial for now. This
    prepares for IntersectionOptimization to track minimum scroll delta
    per IntersectionObservation, with which ComputeIntersection will
    have more no-op invocations and layily creating RootGeometry can
    probably reduce the cost when a RootGeometry is not needed for any
    IntersectionObservation of an observer.
    
    Bug: 1400495
    Change-Id: I295ce633b83c219861788f60498c133be4a85fab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4975516
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214953}

--- third_party/blink/renderer/core/intersection_observer/intersection_geometry.h
+++ third_party/blink/renderer/core/intersection_observer/intersection_geometry.h
@@ -58,8 +58,9 @@ class CORE_EXPORT IntersectionGeometry {
 
    public:
     RootGeometry(const LayoutObject* root, const Vector<Length>& margin);
+    bool operator==(const RootGeometry&) const;
 
-    float zoom;
+    float zoom = 1.0f;
     // The root object's content rect in the root object's own coordinate system
     PhysicalRect local_root_rect;
     gfx::Transform root_to_document_transform;
@@ -85,6 +86,10 @@ class CORE_EXPORT IntersectionGeometry {
 
   static const LayoutObject* GetExplicitRootLayoutObject(const Node& root_node);
 
+  // If `root_geometry` is nullopt, it will be emplaced with `root` and
+  // `root_margin`. The caller can call this constructor again with the same
+  // `root_geometry` as long as `root` and `root_margin` are the same as the
+  // first call.
   IntersectionGeometry(const Node* root,
                        const Element& target,
                        const Vector<Length>& root_margin,
@@ -92,15 +97,7 @@ class CORE_EXPORT IntersectionGeometry {
                        const Vector<Length>& target_margin,
                        const Vector<Length>& scroll_margin,
                        unsigned flags,
-                       CachedRects* cached_rects = nullptr);
-
-  IntersectionGeometry(const RootGeometry& root_geometry,
-                       const Node& explicit_root,
-                       const Element& target,
-                       const Vector<float>& thresholds,
-                       const Vector<Length>& target_margin,
-                       const Vector<Length>& scroll_margin,
-                       unsigned flags,
+                       absl::optional<RootGeometry>& root_geometry,
                        CachedRects* cached_rects = nullptr);
 
   IntersectionGeometry(const IntersectionGeometry&) = default;
