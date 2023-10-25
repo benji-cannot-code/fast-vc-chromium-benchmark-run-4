be7949d835b17dae14e5e95c0af24a333367e67a
Brahim Chikhaoui <bchikhaoui@google.com>
2023-10-25 17:05:30 +0000

    [Sync][Lacros] Fix UkmBrowserTest.AllowedStateChanged browser_tests
    Fix UkmBrowserTest.AllowedStateChanged when kSyncChromeOSAppsToggleSharing
    is enabled.The Ukm rely on Apps toggle enabled and on Lacros, if
    kSyncChromeOSAppsToggleSharing is enabled, syncing of Apps is controlled
    via the OS, not the browser.
    
    Bug: 1330894 , b:307483579
    Change-Id: Ib7d461dff765805416e1c74804500a553931e89e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4971138
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1214920}

--- chrome/browser/metrics/ukm_browsertest.cc
+++ chrome/browser/metrics/ukm_browsertest.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/metrics_services_manager/metrics_services_manager.h"
 #include "components/signin/public/base/signin_buildflags.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
+#include "components/sync/base/features.h"
 #include "components/sync/service/sync_service.h"
 #include "components/sync/service/sync_service_impl.h"
 #include "components/sync/service/sync_token_status.h"
@@ -295,6 +296,18 @@ class UkmBrowserTestBase : public SyncTest {
       Profile* profile) {
     std::unique_ptr<SyncServiceImplHarness> harness =
         test::InitializeProfileForSync(profile, GetFakeServer()->AsWeakPtr());
+
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+    // Apps sync is controlled by a dedicated preference on Lacros,
+    // corresponding to the Apps toggle in OS Sync settings.
+    if (base::FeatureList::IsEnabled(syncer::kSyncChromeOSAppsToggleSharing)) {
+      syncer::SyncUserSettings* user_settings =
+          harness->service()->GetUserSettings();
+      // Turn on App-sync in OS Sync.
+      user_settings->SetAppsSyncEnabledByOs(true);
+    }
+#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
+
     EXPECT_TRUE(harness->SetupSync());
 
     // If unified consent is enabled, then enable url-keyed-anonymized data
