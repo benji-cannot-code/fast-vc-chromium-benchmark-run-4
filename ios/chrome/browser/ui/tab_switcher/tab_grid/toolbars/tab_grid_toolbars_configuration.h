3e86a111c19481a2ec5b62eab8bca1e023b04299
Gauthier Ambard <gambard@chromium.org>
2023-10-25 10:10:00 +0000

    [iOS] Disable TabGrid edit button when mode is not available
    When the Incognito or Regular mode is not available, the edit button in
    the TabGrid should be disabled.
    
    Fixed: 1495308
    Change-Id: Icf60c1eedb7c96adf4ef2516b3d46e9139369fad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4969780
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214735}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/toolbars/tab_grid_toolbars_configuration.h
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/toolbars/tab_grid_toolbars_configuration.h
@@ -27,6 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic) BOOL shareButton;
 @property(nonatomic) BOOL undoButton;
 
+// Returns a configuration disabling all buttons.
++ (TabGridToolbarsConfiguration*)disabledConfiguration;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TAB_GRID_TOOLBARS_TAB_GRID_TOOLBARS_CONFIGURATION_H_
--- ios/chrome/browser/ui/tab_switcher/tab_grid/toolbars/tab_grid_toolbars_configuration.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/toolbars/tab_grid_toolbars_configuration.mm
@@ -7,4 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation TabGridToolbarsConfiguration
 
++ (TabGridToolbarsConfiguration*)disabledConfiguration {
+  return [[self alloc] init];
+}
+
 @end
