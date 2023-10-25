6a076ffb81ee9dc2826120a914650901d434a3e2
Russ Hamilton <behamilton@google.com>
2023-10-25 16:06:02 +0000

    Ensure URLLoader is not destroyed until we are done with it
    Change-Id: Ib7344af634de9a6d341dc16be53de6f36cf02c22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4968515
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214888}

--- content/browser/interest_group/ad_auction_service_impl.h
+++ content/browser/interest_group/ad_auction_service_impl.h
@@ -178,6 +178,7 @@ class CONTENT_EXPORT AdAuctionServiceImpl final
                         BiddingAndAuctionData data);
   void OnGotBiddingAndAuctionServerKey(
       BiddingAndAuctionDataConstructionState state,
+      scoped_refptr<network::WrapperSharedURLLoaderFactory> loader,
       base::expected<BiddingAndAuctionServerKey, std::string> maybe_key);
 
   InterestGroupManagerImpl& GetInterestGroupManager() const;
