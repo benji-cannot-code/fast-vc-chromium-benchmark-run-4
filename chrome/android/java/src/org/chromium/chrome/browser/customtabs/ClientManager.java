561b1aa44f9aa7013400ea7788371570be367fd9
Sinan Sahin <sinansahin@google.com>
2023-10-25 21:33:43 +0000

    [EngagementSignals] Fix NPE in setEngagementSignalsCallback
    Because of the way the AndroidX API is currently implemented, the
    EngagementSignalsHandler may be null. In that case, we should return
    false from #setEngagementSignalsCallback instead of crashing.
    
    Bug: 1495591
    Change-Id: Ibea281bde503bc38d845a7b844d3f2d3a55b37b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4977237
    Reviewed-by: Kevin Grosu <kgrosu@google.com>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1215126}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/ClientManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/ClientManager.java
@@ -945,7 +945,7 @@ class ClientManager {
         return callOnSession(session, null, SessionParams::getEngagementSignalsAvailableSupplier);
     }
 
-    public @NonNull EngagementSignalsHandler getEngagementSignalsHandlerForSession(
+    public @Nullable EngagementSignalsHandler getEngagementSignalsHandlerForSession(
             CustomTabsSessionToken session) {
         return callOnSession(session, null, SessionParams::getEngagementSignalsHandler);
     }
