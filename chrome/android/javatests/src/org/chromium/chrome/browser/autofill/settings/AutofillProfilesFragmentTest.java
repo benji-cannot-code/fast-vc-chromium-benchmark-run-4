76e64e54a890d999375b51aea207cd60b874f876
Sinan Sahin <sinansahin@google.com>
2023-10-26 21:59:36 +0000

    🌱 Disable AutofillProfilesFragmentTest
    Bug: 1496405
    Change-Id: If7337354c9321be4878ab320f1626b32f4afe9c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4981181
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Auto-Submit: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Geoff Huang <geoffhuang@google.com>
    Owners-Override: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Shu Yang <shuyng@google.com>
    Reviewed-by: Geoff Huang <geoffhuang@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215774}

--- chrome/android/javatests/src/org/chromium/chrome/browser/autofill/settings/AutofillProfilesFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/autofill/settings/AutofillProfilesFragmentTest.java
@@ -35,6 +35,7 @@ import org.mockito.junit.MockitoRule;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.autofill.AutofillTestHelper;
 import org.chromium.chrome.browser.autofill.editors.EditorDialogView;
@@ -71,6 +72,7 @@ import java.util.concurrent.TimeoutException;
     ChromeFeatureList.AUTOFILL_ACCOUNT_PROFILE_STORAGE,
     ChromeFeatureList.SYNC_ENABLE_CONTACT_INFO_DATA_TYPE_IN_TRANSPORT_MODE
 })
+@DisabledTest(message = "https://crbug.com/1496405")
 public class AutofillProfilesFragmentTest {
     private static final AutofillProfile sLocalOrSyncProfile =
             AutofillProfile.builder()
