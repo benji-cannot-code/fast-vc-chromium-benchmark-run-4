dd91512a5d321490e7bfe7d9249582cc834e4271
Kent Tamura <tkent@chromium.org>
2023-10-26 08:14:16 +0000

    LayoutNG: Remove "NG" from some symbols in layout/inline/
    This CL removes "NG" from the following symbols:
    * NGSvgFragmentData
    * NGLineBreakStrategy
    * NGShapingLineBreaker
    * NGAnnotationOverhang
    * NGAnnotationMetrics
    
    This CL has no behavior changes.
    
    Bug: 149200
    Change-Id: I372294ea42765351ded6cb68376f670367749c04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4975686
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215373}

--- third_party/blink/renderer/core/layout/inline/ruby_utils.cc
+++ third_party/blink/renderer/core/layout/inline/ruby_utils.cc
@@ -97,8 +97,8 @@ PhysicalRect AdjustTextRectForEmHeight(const PhysicalRect& rect,
           PhysicalSize(new_line_height, rect.size.height)};
 }
 
-NGAnnotationOverhang GetOverhang(const InlineItemResult& item) {
-  NGAnnotationOverhang overhang;
+AnnotationOverhang GetOverhang(const InlineItemResult& item) {
+  AnnotationOverhang overhang;
   if (!item.layout_result)
     return overhang;
 
@@ -231,7 +231,7 @@ LayoutUnit CommitPendingEndOverhang(LineInfo* line_info) {
   return end_overhang;
 }
 
-NGAnnotationMetrics ComputeAnnotationOverflow(
+AnnotationMetrics ComputeAnnotationOverflow(
     const LogicalLineItems& logical_line,
     const FontHeight& line_box_metrics,
     const ComputedStyle& line_style) {
