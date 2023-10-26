b0ee5eac9eb340f05a9d063e72655e381508ed86
Abe Boujane <boujane@google.com>
2023-10-26 23:22:57 +0000

    Call OnboardingNoticeShown for when the notice was previously permanently dismissed
    We could end up in this case in Prestable where we allow for the
    onboarding prefs to be reset. This means the profile could be eligible
    to show the onboarding notice, but never be able to actually show it due
    to it being permanently dismissed in the past.
    
    Bug: b:304819106
    Change-Id: If8abbadf2f7de30f63ad1d65436f44e177dda2b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4980091
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Abe Boujane <boujane@google.com>
    Auto-Submit: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1215817}

--- chrome/browser/privacy_sandbox/tracking_protection_notice_service.cc
+++ chrome/browser/privacy_sandbox/tracking_protection_notice_service.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/location_bar_model.h"
 #include "components/privacy_sandbox/tracking_protection_onboarding.h"
 #include "components/user_education/common/feature_promo_controller.h"
+#include "components/user_education/common/feature_promo_result.h"
 #include "content/public/browser/navigation_handle.h"
 
 namespace privacy_sandbox {
@@ -124,6 +125,13 @@ void TrackingProtectionNoticeService::BaseIPHNotice::
     return;
   }
 
+  // If the promo has previously been dismissed by the user, Call the onboarding
+  // service to notify that the promo was shown.
+  if (WasPromoPreviouslyDismissed(browser)) {
+    // Call Notify the onbaording service that the promo was shown.
+    onboarding_service_->NoticeShown(GetNoticeType());
+  }
+
   // We should hide the notice at this point if the browser isn't eligible.
   if (!IsLocationBarEligible(browser)) {
     HidePromo(browser);
@@ -143,6 +151,12 @@ void TrackingProtectionNoticeService::BaseIPHNotice::
   } else {
     // TODO(b/302008359) Emit metrics
   }
+}
+
+bool TrackingProtectionNoticeService::BaseIPHNotice::
+    WasPromoPreviouslyDismissed(Browser* browser) {
+  return browser->window()->CanShowFeaturePromo(GetIPHFeature()) ==
+         user_education::FeaturePromoResult::kPermanentlyDismissed;
 }
 
 bool TrackingProtectionNoticeService::BaseIPHNotice::MaybeShowPromo(
