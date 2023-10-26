75fe4a7b92263fb0bcf99b9b1bc9aa22a3b0e371
mlcui <mlcui@google.com>
2023-10-26 01:21:16 +0000

    os_languages_page: Do not fetch language pack status with flag disabled
    This prevent unnecessary API calls.
    
    Bug: b:298128915
    Change-Id: I84d98821fe08dcf5d0f3503dba1a52e7de36009f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4975541
    Reviewed-by: Claudio M <claudiomagni@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1215257}

--- chrome/browser/resources/ash/settings/os_languages_page/languages.ts
+++ chrome/browser/resources/ash/settings/os_languages_page/languages.ts
@@ -254,6 +254,12 @@ export class SettingsLanguagesElement extends SettingsLanguagesElementBase
   private boundOnLanguagePackStatusChanged_: OmitThisParameter<
       SettingsLanguagesElement['onLanguagePackStatusChanged_']>|null = null;
 
+  // loadTimeData flags.
+  // We do not expect this to change over the lifetime of this element, so this
+  // is not included in `properties()` above.
+  private languagePacksInSettingsEnabled_ =
+      loadTimeData.getBoolean('languagePacksInSettingsEnabled');
+
   override connectedCallback(): void {
     super.connectedCallback();
 
@@ -329,7 +335,7 @@ export class SettingsLanguagesElement extends SettingsLanguagesElementBase
       this.languageSettingsPrivate_.getSpellcheckDictionaryStatuses().then(
           this.boundOnSpellcheckDictionariesChanged_);
 
-      if (loadTimeData.getBoolean('languagePacksInSettingsEnabled')) {
+      if (this.languagePacksInSettingsEnabled_) {
         // Get the initial state of language pack statuses.
         // Do so in the next microtask to prevent `connectedCallback()` from
         // failing and stalling tests.
@@ -1216,7 +1222,9 @@ export class SettingsLanguagesElement extends SettingsLanguagesElementBase
           enabledInputMethodSet.has(inputMethod));
     }
     this.set('languages.inputMethods.enabled', enabledInputMethods);
-    this.fetchMissingLanguagePackStatuses_();
+    if (this.languagePacksInSettingsEnabled_) {
+      this.fetchMissingLanguagePackStatuses_();
+    }
   }
 
   addInputMethod(id: string): void {
