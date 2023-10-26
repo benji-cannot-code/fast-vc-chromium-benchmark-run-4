7950f032f5566dba009bb4cf3bba7f2b20fc302a
anthie@google.com <anthie@google.com>
2023-10-26 10:50:44 +0000

    Record www-subdomain conflicts during Url filtering
    Adds a new histogram to record which Url conflicts differ
    only on the trivial www subdomain inclusion during url
    filtering.
    
    The conflicts are captured with a new enum type which
    distinguishes between: 1) conflicts that happen only
    because of trivial subdomain differences (and would go
    away completely if we strip the trivial subdomain),
    2) conflicts that happen due to both trivial subdomain
    differences and other matching conflicting entries
    (these conflicts will no go away if we strip trivial
    subdomains) and 3) conflicts that do not occur due to
    trivial subdomains (and will remain unchanged if we
    do trivial subdomain stripping).
    
    Bug: b/305229682
    Change-Id: Ica3b8ed4b7772fa46bbec8e3a34e87057f814c80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4936002
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Courtney Wong <courtneywong@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1215416}

--- components/supervised_user/core/browser/supervised_user_url_filter.h
+++ components/supervised_user/core/browser/supervised_user_url_filter.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <memory>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -98,6 +99,24 @@ class SupervisedUserURLFilter {
     kMaxValue = kBoth,
   };
 
+  // This enum describes the kind of conflicts between allow and block list
+  // entries that match a given input host and resolve to different filtering
+  // results.
+  // They distinguish between conflicts:
+  // 1) entirely due to trivial subdomain differences,
+  // 2) due to differences other than the trivial subdomain and
+  // 3) due to both kinds of differences.
+  // These values are logged to UMA. Entries should not be renumbered and
+  // numeric values should never be reused. Please keep in sync with
+  // "FamilyLinkFilteringSubdomainConflictType" in
+  // src/tools/metrics/histograms/enums.xml.
+  enum class FilteringSubdomainConflictType {
+    kTrivialSubdomainConflictOnly = 0,
+    kOtherConflictOnly = 1,
+    kTrivialSubdomainConflictAndOtherConflict = 2,
+    kMaxValue = kTrivialSubdomainConflictAndOtherConflict,
+  };
+
   // Provides access to functionality from services on which we don't want
   // to depend directly.
   class Delegate {
@@ -135,6 +154,7 @@ class SupervisedUserURLFilter {
   static const char* GetApprovedSitesCountHistogramNameForTest();
   static const char* GetBlockedSitesCountHistogramNameForTest();
   static const char* GetManagedSiteListConflictHistogramNameForTest();
+  static const char* GetManagedSiteListConflictTypeHistogramNameForTest();
 
   static FilteringBehavior BehaviorFromInt(int behavior_value);
 
@@ -252,6 +272,7 @@ class SupervisedUserURLFilter {
 
  private:
   friend class SupervisedUserURLFilterTest;
+  friend class SupervisedUserURLFilteringWithConflictsTest;
 
   // Converts FilteringBehavior to the SupervisedUserFilterTopLevelResult
   // histogram value in tools/metrics/histograms/enums.xml to be used in the
@@ -292,9 +313,9 @@ class SupervisedUserURLFilter {
   // (false).
   std::map<GURL, bool> url_map_;
 
-  // Maps from a hostname to whether it is manually allowed (true) or blocked
-  // (false).
-  std::map<std::string, bool> host_map_;
+  // Blocked and Allowed host lists.
+  std::set<std::string> blocked_host_list_;
+  std::set<std::string> allowed_host_list_;
 
   std::unique_ptr<Delegate> service_delegate_;
 
