de92e3ef51bfc35b888c81861798ab150e0d4c24
Avi Drissman <avi@chromium.org>
2023-10-26 22:34:06 +0000

    Remove uses of implicit conversion of ScopedTypeRef in Blink
    Implicit unwrapping of a scoper to its underlying pointer is dangerous
    and that capability is being removed. This converts uses of implicit
    conversion to be explicit in preparation for its removal, and performs
    other cleanup and modernization.
    
    Bug: 1495439
    Change-Id: I126daf500627d7b3672641890f8dde7d9bc52fbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4978749
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215796}

--- third_party/blink/renderer/core/editing/substring_util_test.mm
+++ third_party/blink/renderer/core/editing/substring_util_test.mm
@@ -141,12 +141,12 @@ TEST_F(SubStringUtilTest, SubstringUtilIframe) {
   base::apple::ScopedCFTypeRef<CFAttributedStringRef> result =
       SubstringUtil::AttributedSubstringInRange(child_frame->GetFrame(), 11, 7,
                                                 baseline_point);
-  ASSERT_NE(result, nullptr);
+  ASSERT_TRUE(result);
 
   gfx::Point point(baseline_point);
   result.reset(SubstringUtil::AttributedWordAtPoint(
       main_frame->FrameWidgetImpl(), point, baseline_point));
-  ASSERT_NE(result, nullptr);
+  ASSERT_TRUE(result);
 
   int y_before_change = baseline_point.y();
 
@@ -157,7 +157,7 @@ TEST_F(SubStringUtilTest, SubstringUtilIframe) {
   point = gfx::Point(point.x(), point.y() + 100);
   result.reset(SubstringUtil::AttributedWordAtPoint(
       main_frame->FrameWidgetImpl(), point, baseline_point));
-  ASSERT_NE(result, nullptr);
+  ASSERT_TRUE(result);
 
   EXPECT_EQ(y_before_change, baseline_point.y() - 100);
 }
