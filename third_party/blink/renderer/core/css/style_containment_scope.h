0cb2ed5c0242255f83579d55a48d49c678326279
Daniil Sakhapov <sakhapov@chromium.org>
2023-10-26 11:06:31 +0000

    Revert "Connect css counters to style containment scope tree and remove old code"
    This reverts commit 32bca80b0b2edeaf5eeaeb81069daec55dba37c1.
    
    Reason for revert: Crash reports
    
    Original change's description:
    > Connect css counters to style containment scope tree and remove old code
    >
    > This is stage 2 of CSS Counters projects. The idea is to remove old
    > style containment code from LayoutCounter and CounterNode and instead
    > connect them to StyleContainmentScopeTree.
    >
    > Design doc:
    > https://docs.google.com/document/d/1lc7OouWnelduHuRBxsQ9rmSS85q7E-h42NrOpKVkkoU
    >
    > Bug: 990657
    > Change-Id: I9db3ee4834dc388b137306927f123b2c0813cccf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905764
    > Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    > Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1209472}
    
    Bug: 990657
    Change-Id: I2e10e705677040c3cd392b18dbe69009fc09d383
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4975124
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215421}

--- third_party/blink/renderer/core/css/style_containment_scope.h
+++ third_party/blink/renderer/core/css/style_containment_scope.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_CSS_STYLE_CONTAINMENT_SCOPE_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_STYLE_CONTAINMENT_SCOPE_H_
 
-#include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/layout/layout_quote.h"
 
@@ -35,10 +34,10 @@ class StyleContainmentScope final
   CORE_EXPORT CountersScope* FindCountersScopeForElement(
       const Element&,
       const AtomicString&) const;
-  void CreateCounterNodesForLayoutObject(LayoutObject&);
+  CORE_EXPORT void CreateCounterNodesForLayoutObject(LayoutObject&);
   void CreateCounterNodeForLayoutObject(LayoutObject& object,
                                         const AtomicString& identifier);
-  void CreateCounterNodeForLayoutCounter(LayoutCounter&);
+  CORE_EXPORT void CreateCounterNodeForLayoutCounter(LayoutCounter&);
   void CreateListItemCounterNodeForLayoutObject(LayoutObject&);
   void RemoveCounterNodeForLayoutCounter(LayoutCounter&);
   void ReparentCountersToStyleScope(StyleContainmentScope&);
--- third_party/blink/renderer/core/layout/layout_view.cc
+++ third_party/blink/renderer/core/layout/layout_view.cc
@@ -111,6 +111,7 @@ class HitTestLatencyRecorder {
 LayoutView::LayoutView(ContainerNode* document)
     : LayoutNGBlockFlow(document),
       frame_view_(To<Document>(document)->View()),
+      layout_counter_count_(0),
       hit_test_count_(0),
       hit_test_cache_hits_(0),
       hit_test_cache_(MakeGarbageCollected<HitTestCache>()),
@@ -1008,7 +1009,7 @@ void LayoutView::UpdateMarkersAndCountersAfterStyleChange(
          "container query style recalcs";
 
   needs_marker_counter_update_ = false;
-  if (!HasLayoutListItems()) {
+  if (!HasLayoutCounters() && !HasLayoutListItems()) {
     return;
   }
 
@@ -1030,6 +1031,8 @@ void LayoutView::UpdateMarkersAndCountersAfterStyleChange(
     } else if (auto* inline_list_item =
                    DynamicTo<LayoutInlineListItem>(layout_object)) {
       inline_list_item->UpdateCounterStyle();
+    } else if (auto* counter = DynamicTo<LayoutCounter>(layout_object)) {
+      counter->UpdateCounter();
     }
   }
 }
