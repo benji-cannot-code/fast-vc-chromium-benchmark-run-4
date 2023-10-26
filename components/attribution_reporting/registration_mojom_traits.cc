983b850ce5ab641eeea19afe43a6bd742b14ef9a
Andrew Paseltiner <apaseltiner@chromium.org>
2023-10-26 18:30:50 +0000

    Use nullable primitives in Attribution Reporting Mojo
    Instead of wrapper types that exist only for nullability.
    
    Change-Id: If3582bd477525f0212c64d3253ebdaa8417c8617
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4317283
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215639}

--- components/attribution_reporting/registration_mojom_traits.cc
+++ components/attribution_reporting/registration_mojom_traits.cc
@@ -198,10 +198,6 @@ bool StructTraits<attribution_reporting::mojom::SourceRegistrationDataView,
     return false;
   }
 
-  if (!data.ReadDebugKey(&out->debug_key)) {
-    return false;
-  }
-
   if (!data.ReadFilterData(&out->filter_data)) {
     return false;
   }
@@ -217,6 +213,7 @@ bool StructTraits<attribution_reporting::mojom::SourceRegistrationDataView,
   out->source_event_id = data.source_event_id();
   out->max_event_level_reports = data.max_event_level_reports();
   out->priority = data.priority();
+  out->debug_key = data.debug_key();
   out->debug_reporting = data.debug_reporting();
   return out->IsValid();
 }
@@ -234,14 +231,11 @@ bool StructTraits<attribution_reporting::mojom::EventTriggerDataDataView,
                   attribution_reporting::EventTriggerData>::
     Read(attribution_reporting::mojom::EventTriggerDataDataView data,
          attribution_reporting::EventTriggerData* out) {
-  if (!data.ReadDedupKey(&out->dedup_key)) {
-    return false;
-  }
-
   if (!data.ReadFilters(&out->filters)) {
     return false;
   }
 
+  out->dedup_key = data.dedup_key();
   out->data = data.data();
   out->priority = data.priority();
   return true;
@@ -283,14 +277,11 @@ bool StructTraits<attribution_reporting::mojom::AggregatableDedupKeyDataView,
                   attribution_reporting::AggregatableDedupKey>::
     Read(attribution_reporting::mojom::AggregatableDedupKeyDataView data,
          attribution_reporting::AggregatableDedupKey* out) {
-  if (!data.ReadDedupKey(&out->dedup_key)) {
-    return false;
-  }
-
   if (!data.ReadFilters(&out->filters)) {
     return false;
   }
 
+  out->dedup_key = data.dedup_key();
   return true;
 }
 
@@ -328,15 +319,12 @@ bool StructTraits<attribution_reporting::mojom::TriggerRegistrationDataView,
     return false;
   }
 
-  if (!data.ReadDebugKey(&out->debug_key)) {
-    return false;
-  }
-
   if (!data.ReadAggregationCoordinatorOrigin(
           &out->aggregation_coordinator_origin)) {
     return false;
   }
 
+  out->debug_key = data.debug_key();
   out->debug_reporting = data.debug_reporting();
   out->source_registration_time_config = data.source_registration_time_config();
   return true;
