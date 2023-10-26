5d2856b7e9696fe9437ac92631ab2f01b7538bd8
Nicolás Peña <npm@chromium.org>
2023-10-26 19:39:20 +0000

    [FedCM] Rename hostedDomain to domainHint
    Bug: 1440178
    Change-Id: I8ac91eb47bae35105e323ab0dba2e393c3a8e261
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4973495
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215691}

--- content/browser/webid/federated_auth_request_impl.h
+++ content/browser/webid/federated_auth_request_impl.h
@@ -57,7 +57,7 @@ class CONTENT_EXPORT FederatedAuthRequestImpl
           IdpSigninStatusObserver,
       public content::FederatedIdentityModalDialogViewDelegate {
  public:
-  static constexpr char kWildcardHostedDomain[] = "*";
+  static constexpr char kWildcardDomainHint[] = "*";
 
   static void Create(RenderFrameHost*,
                      mojo::PendingReceiver<blink::mojom::FederatedAuthRequest>);
