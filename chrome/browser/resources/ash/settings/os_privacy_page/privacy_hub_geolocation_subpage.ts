8d78cbfed5d9efc9874cc2799ac6799550661697
Zauri Meshveliani <zauri@google.com>
2023-10-26 10:06:47 +0000

    privacy-hub: Create empty subpage for geolocation
    Bug: b:298166777
    Change-Id: I87a93fa92f8b94dbabc459232857eb16d9af1fb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4976165
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Zauri Meshveliani <zauri@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215404}

---
+++ chrome/browser/resources/ash/settings/os_privacy_page/privacy_hub_geolocation_subpage.ts
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview
+ * 'settings-privacy-hub-geolocation-subpage' contains a detailed overview about
+ * the state of the system geolocation access.
+ */
+
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {getTemplate} from './privacy_hub_geolocation_subpage.html.js';
+
+export class SettingsPrivacyHubGeolocationSubpage extends PolymerElement {
+  static get is() {
+    return 'settings-privacy-hub-geolocation-subpage' as const;
+  }
+
+  static get template() {
+    return getTemplate();
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [SettingsPrivacyHubGeolocationSubpage.is]:
+        SettingsPrivacyHubGeolocationSubpage;
+  }
+}
+
+customElements.define(
+    SettingsPrivacyHubGeolocationSubpage.is,
+    SettingsPrivacyHubGeolocationSubpage);
