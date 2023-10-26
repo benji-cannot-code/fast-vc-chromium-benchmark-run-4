4196c0f96756c0b49b1d7a15afc4659eb435c30e
Abhijeet Kandalkar <abhijeet@igalia.com>
2023-10-26 18:10:58 +0000

    [mac] Remove unwanted IS_MAC check
    core_audio_util_mac.h/.mm are already included only for a mac platform
    in media/audio/BUILD.gn
    
    Bug: 1413450
    Change-Id: I42703b07027291329f33552031c9888a9889b119
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4975325
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215635}

--- media/audio/mac/core_audio_util_mac.cc
+++ media/audio/mac/core_audio_util_mac.cc
@@ -17,15 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/mac/scoped_audio_unit.h"
 #include "media/base/audio_timestamp_helper.h"
 
-#if BUILDFLAG(IS_MAC)
 #include <IOKit/audio/IOAudioTypes.h>
-#endif
 
 namespace media {
 namespace core_audio_mac {
-
-#if BUILDFLAG(IS_MAC)
-
 namespace {
 
 AudioObjectPropertyScope InputOutputScope(bool is_input) {
@@ -379,7 +374,6 @@ bool IsInputDevice(AudioObjectID device_id) {
 bool IsOutputDevice(AudioObjectID device_id) {
   return GetNumStreams(device_id, false) > 0;
 }
-#endif
 
 // static
 base::TimeDelta GetHardwareLatency(AudioUnit audio_unit,
--- media/audio/mac/core_audio_util_mac.h
+++ media/audio/mac/core_audio_util_mac.h
@@ -15,9 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
-#if BUILDFLAG(IS_MAC)
 #include <CoreAudio/CoreAudio.h>
-#endif
 
 namespace media {
 namespace core_audio_mac {
