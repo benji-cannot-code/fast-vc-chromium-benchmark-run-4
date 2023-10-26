c4c3ec94da0fa22fc970af870edf72badbdfd244
Roman Arora <romanarora@chromium.org>
2023-10-26 18:08:57 +0000

    [ntp-iph] Refactor promo helper logic
    Refactor the existing NTP customize chrome promo helper logic so that it
    can be used for the various promos/iph present in the NTP.
    
    Demo(s):
    CustmizeChrome - https://drive.google.com/file/d/1j9BbnmpnwJWXgEwmx83L9Nlg2RlNaq5e/view
    CustomizeChromeRefesh - https://drive.google.com/file/d/1Ug-cvBLIRSEODsnFs3dRcB8PfljMSynZ/view
    
    Bug: 1471721
    Change-Id: I4c7432c9f25dfd7c38895ee688f50bce98027bd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4943399
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215632}

--- chrome/browser/ui/webui/new_tab_page/new_tab_page_handler.cc
+++ chrome/browser/ui/webui/new_tab_page/new_tab_page_handler.cc
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
-#include "chrome/browser/new_tab_page/customize_chrome/customize_chrome_feature_promo_helper.h"
+#include "chrome/browser/new_tab_page/feature_promo_helper/new_tab_page_feature_promo_helper.h"
 #include "chrome/browser/new_tab_page/modules/new_tab_page_modules.h"
 #include "chrome/browser/new_tab_page/promos/promo_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -55,6 +55,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/theme_resources.h"
+#include "components/feature_engagement/public/event_constants.h"
+#include "components/feature_engagement/public/feature_constants.h"
 #include "components/keyed_service/core/service_access_type.h"
 #include "components/omnibox/browser/omnibox.mojom.h"
 #include "components/prefs/pref_service.h"
@@ -414,7 +416,7 @@ NewTabPageHandler::NewTabPageHandler(
     ThemeService* theme_service,
     search_provider_logos::LogoService* logo_service,
     content::WebContents* web_contents,
-    std::unique_ptr<CustomizeChromeFeaturePromoHelper>
+    std::unique_ptr<NewTabPageFeaturePromoHelper>
         customize_chrome_feature_promo_helper,
     const base::Time& ntp_navigation_start_time,
     const std::vector<std::pair<const std::string, int>> module_id_names)
@@ -426,8 +428,7 @@ NewTabPageHandler::NewTabPageHandler(
       theme_service_(theme_service),
       profile_(profile),
       web_contents_(web_contents),
-      customize_chrome_feature_promo_helper_(
-          std::move(customize_chrome_feature_promo_helper)),
+      feature_promo_helper_(std::move(customize_chrome_feature_promo_helper)),
       ntp_navigation_start_time_(ntp_navigation_start_time),
       module_id_names_(module_id_names),
       logger_(profile,
@@ -442,7 +443,7 @@ NewTabPageHandler::NewTabPageHandler(
   CHECK(theme_service_);
   CHECK(promo_service_);
   CHECK(web_contents_);
-  CHECK(customize_chrome_feature_promo_helper_);
+  CHECK(feature_promo_helper_);
   ntp_background_service_->AddObserver(this);
   native_theme_observation_.Observe(ui::NativeTheme::GetInstanceForNativeUi());
   theme_service_observation_.Observe(theme_service_.get());
@@ -846,9 +847,12 @@ void NewTabPageHandler::SetCustomizeChromeSidePanelVisible(
   if (visible) {
     // Record usage for customize chrome promo.
     auto* tab = web_contents_.get();
-    customize_chrome_feature_promo_helper_->RecordCustomizeChromeFeatureUsage(
-        tab);
-    customize_chrome_feature_promo_helper_->CloseCustomizeChromeFeaturePromo(
+    feature_promo_helper_->RecordFeatureUsage(
+        feature_engagement::events::kCustomizeChromeOpened, tab);
+    feature_promo_helper_->CloseFeaturePromo(
+        features::IsChromeRefresh2023() && features::IsChromeWebuiRefresh2023()
+            ? feature_engagement::kIPHDesktopCustomizeChromeRefreshFeature
+            : feature_engagement::kIPHDesktopCustomizeChromeFeature,
         tab);
   }
 }
@@ -863,7 +867,8 @@ void NewTabPageHandler::IncrementCustomizeChromeButtonOpenCount() {
           1);
 }
 
-void NewTabPageHandler::MaybeShowCustomizeChromeFeaturePromo() {
+void NewTabPageHandler::MaybeShowFeaturePromo(
+    new_tab_page::mojom::IphFeature iph_feature) {
   CHECK(profile_);
   CHECK(profile_->GetPrefs());
 
@@ -871,23 +876,27 @@ void NewTabPageHandler::MaybeShowCustomizeChromeFeaturePromo() {
   // shown to avoid conflict. The sign-in dialog would be shown as soon as the
   // browser is opened, before the promo.
   bool is_signin_modal_dialog_open =
-      customize_chrome_feature_promo_helper_->IsSigninModalDialogOpen(
-          web_contents_.get());
+      feature_promo_helper_->IsSigninModalDialogOpen(web_contents_.get());
   if (is_signin_modal_dialog_open) {
     return;
   }
 
-  if (features::IsChromeRefresh2023()) {
-    customize_chrome_feature_promo_helper_
-        ->MaybeShowCustomizeChromeFeaturePromo(web_contents_.get());
-  } else {
-    const auto customize_chrome_button_open_count =
-        profile_->GetPrefs()->GetInteger(
-            prefs::kNtpCustomizeChromeButtonOpenCount);
+  if (iph_feature == new_tab_page::mojom::IphFeature::kCustomizeChrome) {
+    if (features::IsChromeRefresh2023() &&
+        features::IsChromeWebuiRefresh2023()) {
+      feature_promo_helper_->MaybeShowFeaturePromo(
+          feature_engagement::kIPHDesktopCustomizeChromeRefreshFeature,
+          web_contents_.get());
+    } else {
+      const auto customize_chrome_button_open_count =
+          profile_->GetPrefs()->GetInteger(
+              prefs::kNtpCustomizeChromeButtonOpenCount);
 
-    if (customize_chrome_button_open_count == 0) {
-      customize_chrome_feature_promo_helper_
-          ->MaybeShowCustomizeChromeFeaturePromo(web_contents_.get());
+      if (customize_chrome_button_open_count == 0) {
+        feature_promo_helper_->MaybeShowFeaturePromo(
+            feature_engagement::kIPHDesktopCustomizeChromeFeature,
+            web_contents_.get());
+      }
     }
   }
 }
--- chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.cc
+++ chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.cc
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/buildflags.h"
 #include "chrome/browser/cart/cart_handler.h"
 #include "chrome/browser/image_service/image_service_factory.h"
-#include "chrome/browser/new_tab_page/customize_chrome/customize_chrome_feature_promo_helper.h"
+#include "chrome/browser/new_tab_page/feature_promo_helper/new_tab_page_feature_promo_helper.h"
 #include "chrome/browser/new_tab_page/modules/drive/drive_handler.h"
 #include "chrome/browser/new_tab_page/modules/feed/feed_handler.h"
 #include "chrome/browser/new_tab_page/modules/history_clusters/history_clusters.mojom.h"
@@ -926,8 +926,8 @@ void NewTabPageUI::CreatePageHandler(
       std::move(pending_page_handler), std::move(pending_page), profile_,
       ntp_custom_background_service_, theme_service_,
       LogoServiceFactory::GetForProfile(profile_), web_contents(),
-      std::make_unique<CustomizeChromeFeaturePromoHelper>(),
-      navigation_start_time_, module_id_names_);
+      std::make_unique<NewTabPageFeaturePromoHelper>(), navigation_start_time_,
+      module_id_names_);
 }
 
 void NewTabPageUI::CreateCustomizeThemesHandler(
