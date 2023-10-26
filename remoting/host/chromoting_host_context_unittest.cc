5cd8f6d6b0c58f7a6efd005a09aaa11350bca8bc
Ho Cheung <uioptt24@gmail.com>
2023-10-26 17:46:40 +0000

    [remoting] Use TaskEnvironment in ChromotingHostContextTest
    SingleThreadTaskEnvironment does not support base::ThreadPool. This CL
    attempts to solve this problem. After solving this problem, this test
    should be able to pass.
    
    Bug: 1496133
    Change-Id: Ib3a7431894da2bff72fc464735cafb13e20cf31e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4976167
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Ho Cheung <uioptt24@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1215617}

--- remoting/host/chromoting_host_context_unittest.cc
+++ remoting/host/chromoting_host_context_unittest.cc
@@ -13,11 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace remoting {
 
-// A simple test that starts and stop the context. This tests the context
+// A simple test that starts and stops the context. This tests the context
 // operates properly and all threads and message loops are valid.
 TEST(ChromotingHostContextTest, StartAndStop) {
-  base::test::SingleThreadTaskEnvironment task_environment{
-      base::test::SingleThreadTaskEnvironment::MainThreadType::UI};
+  base::test::TaskEnvironment task_environment{
+      base::test::TaskEnvironment::MainThreadType::UI};
   base::RunLoop run_loop;
 
   scoped_refptr<network::TestSharedURLLoaderFactory> test_url_loader_factory;
