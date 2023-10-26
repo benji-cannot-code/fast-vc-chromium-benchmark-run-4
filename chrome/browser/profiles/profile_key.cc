2bc5a52827dc3035aacaab498480aef3a2bd4544
Elitsa Bankova <elitsa@google.com>
2023-10-26 00:24:37 +0000

    Deps: Split out the profile_key out of the browser target
    Unfortunately, we can't split out profile_key_android.cc from the
    browser target too because it indirectly depends on
    chrome/browser/profiles/profile_manager.h, which is even harder to split
    out of chrome/browser/BUILD.gn.
    
    Bug: b/307628893, b/296157719
    
    Change-Id: Ie9f4fb99344cddaf3a762d9257fa0debb5f8e7c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4951011
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Elitsa Bankova <elitsa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1215225}

--- chrome/browser/profiles/profile_key.cc
+++ chrome/browser/profiles/profile_key.cc
@@ -9,10 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "components/leveldb_proto/public/proto_database_provider.h"
 
-#if BUILDFLAG(IS_ANDROID)
-#include "chrome/browser/profiles/profile_key_android.h"
-#endif  // BUILDFLAG(IS_ANDROID)
-
 ProfileKey::ProfileKey(const base::FilePath& path, ProfileKey* original_key)
     : SimpleFactoryKey(path, original_key != nullptr /* is_off_the_record */),
       original_key_(original_key) {}
