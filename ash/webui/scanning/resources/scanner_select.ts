8ab96dd715e286208ae85cd0c6708a872a0d6c69
Ashley Prasad <ashleydp@google.com>
2023-10-26 19:35:23 +0000

    scanning: Convert scanner_select_test to TS
    - Rename file from JS to TS and update BUILD.gn.
    - Remove closure type comments.
    - Export type from scanning.
    - Fix typo in `selectFirstScanner` test name.
    
    Bug: b:305287898
    Test: browser_tests --gtest_filter=*ScanningApp*
    Change-Id: Ia0b69e09fb0bef74ce794f299d8ee0a611a98f99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4960498
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1215686}

--- ash/webui/scanning/resources/scanner_select.ts
+++ ash/webui/scanning/resources/scanner_select.ts
@@ -26,7 +26,7 @@ import {alphabeticalCompare, getScannerDisplayName, tokenToString} from './scann
 const ScannerSelectElementBase =
     I18nMixin(PolymerElement) as {new (): PolymerElement & I18nMixinInterface};
 
-class ScannerSelectElement extends ScannerSelectElementBase {
+export class ScannerSelectElement extends ScannerSelectElementBase {
   static get is() {
     return 'scanner-select' as const;
   }
