1b7310c91e8ee2d85e020e3da43c63eaad4fc268
Christian Xu <christianxu@google.com>
2023-10-26 16:18:26 +0000

    Reland "[iOS][Omnibox] Fix experimental textfield"
    This is a reland of commit ba99d8891af5a024d85159ce2ec2134bd76217b7
    
    Replace tap with longPress to trigger the edit menu as tap doesn't
    always trigger the menu in egtests.
    
    Original change's description:
    > [iOS][Omnibox] Fix experimental textfield
    >
    > Experimental textfield catches the tap UIGesture to handle the pre-edit
    > and autocomplete state. This breaks some behavior of the tap gesture
    > in a default textfield.
    >
    > This CL:
    > - Allows the custom tap gesture to be recognized simultaneously with the
    >   UITextField tap gesture.
    > - Let UITextField handle the visibility of the menu.
    >
    > Bug: 1429190, 1384533
    > Change-Id: I40708ca84864843aa42d21e6f1a70610055909ea
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4965401
    > Auto-Submit: Christian Xu <christianxu@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    > Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1214104}
    
    Bug: 1429190, 1384533
    Change-Id: I2f6e6899c8e6232d4bfac3b48d963387a6c295be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4971141
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215560}

--- ios/chrome/browser/ui/omnibox/omnibox_egtest.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_egtest.mm
@@ -822,9 +822,7 @@ void FocusFakebox() {
   [ChromeEarlGrey copyTextToPasteboard:@"hello"];
 
   // Tap on Omnibox on pre edit state.
-  // TODO(crbug.com/1442458): Find a better way to tap on the selected url.
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
-      performAction:grey_tapAtPoint(CGPointMake(0, 0))];
+  [self tapOnPreEditTextInOmnibox];
 
   // Wait for callout copy button to be displayed.
   [ChromeEarlGrey waitForUIElementToAppearWithMatcher:
@@ -854,9 +852,7 @@ void FocusFakebox() {
       assertWithMatcher:grey_sufficientlyVisible()];
 
   // Tap on Omnibox on pre edit state.
-  // TODO(crbug.com/1442458): Find a better way to tap on the selected url.
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
-      performAction:grey_tapAtPoint(CGPointMake(0, 0))];
+  [self tapOnPreEditTextInOmnibox];
 
   // Wait for callout cut button to be displayed.
   [ChromeEarlGrey waitForUIElementToAppearWithMatcher:
@@ -888,9 +884,7 @@ void FocusFakebox() {
   [ChromeEarlGrey copyTextToPasteboard:@"hello"];
 
   // Tap on Omnibox on pre edit state.
-  // TODO(crbug.com/1442458): Find a better way to tap on the selected url.
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
-      performAction:grey_tapAtPoint(CGPointMake(0, 0))];
+  [self tapOnPreEditTextInOmnibox];
 
   // Wait for callout paste button to be displayed.
   [ChromeEarlGrey waitForUIElementToAppearWithMatcher:
@@ -904,6 +898,19 @@ void FocusFakebox() {
   // Omnibox now should be empty.
   [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
       assertWithMatcher:chrome_test_util::OmniboxText("hello")];
+}
+
+// Taps the pre edit text in the omnibox.
+- (void)tapOnPreEditTextInOmnibox {
+  // TODO(crbug.com/1442458): Find a better way to tap on the selected url.
+  // Y offset to tap on the middle of the text.
+  const CGFloat kOmniboxTextFieldMidY = 18;
+  // X offset to tap on the beginning of the text, tapping on the left edge
+  // inserts a carret instead of showing the menu.
+  const CGFloat kOmniboxTextFieldOffsetX = 30;
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
+      performAction:grey_tapAtPoint(CGPointMake(kOmniboxTextFieldOffsetX,
+                                                kOmniboxTextFieldMidY))];
 }
 
 @end
@@ -946,7 +953,7 @@ void FocusFakebox() {
   [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
       assertWithMatcher:grey_sufficientlyVisible()];
   [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
-      performAction:grey_tap()];
+      performAction:grey_longPress()];
 
   // Pressing should not allow copying when omnibox is empty.
   // Wait for Copy button to appear or timeout after 2 seconds.
@@ -986,9 +993,9 @@ void FocusFakebox() {
   [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
       performAction:grey_replaceText(@"this is a test")];
 
-  // Click on the omnibox.
+  // Long press on the omnibox.
   [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
-      performAction:grey_tap()];
+      performAction:grey_longPress()];
 
   // SelectAll the omnibox field.
   GREYCondition* SelectAllButtonIsDisplayed = [GREYCondition
@@ -1057,7 +1064,7 @@ void FocusFakebox() {
       performAction:grey_replaceText(@"this is a test")];
 
   [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
-      performAction:grey_tap()];
+      performAction:grey_longPress()];
 
   // Pressing should allow select and selectAll.
   // Wait for UIMenuController to appear or timeout after 2 seconds.
