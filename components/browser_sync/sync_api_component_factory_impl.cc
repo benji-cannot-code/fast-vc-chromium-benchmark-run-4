e8fefd28752d85cf302b4f50978f3f4e4ffeb2ee
Rushan Suleymanov <rushans@google.com>
2023-10-26 15:01:42 +0000

    [Sync] Check PasswordSharingEnabled policy for sync data types
    When PasswordSharingEnabled policy is enabled or not set, the password
    sharing invitation data types are enabled. Otherwise, disable both to
    prevent sending and receiving passwords when the policy is disabled.
    
    Custom model type controller is added for outgoing password sharing
    invitation data type to check the state of the corresponding pref.
    
    Bug: 1445869
    Change-Id: Ie0f93593aabfa436b4c7e0e8480df448967efa91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4976117
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1215507}

--- components/browser_sync/sync_api_component_factory_impl.cc
+++ components/browser_sync/sync_api_component_factory_impl.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history/core/browser/sync/history_model_type_controller.h"
 #include "components/password_manager/core/browser/password_store_interface.h"
 #include "components/password_manager/core/browser/sharing/incoming_password_sharing_invitation_model_type_controller.h"
+#include "components/password_manager/core/browser/sharing/outgoing_password_sharing_invitation_model_type_controller.h"
 #include "components/password_manager/core/browser/sharing/password_receiver_service.h"
 #include "components/password_manager/core/browser/sharing/password_sender_service.h"
 #include "components/password_manager/core/browser/sync/credential_model_type_controller.h"
@@ -402,21 +403,18 @@ SyncApiComponentFactoryImpl::CreateCommonDataTypeControllers(
             std::make_unique<
                 password_manager::
                     IncomingPasswordSharingInvitationModelTypeController>(
-                sync_service, sync_client_->GetPasswordReceiverService()));
+                sync_service, sync_client_->GetPasswordReceiverService(),
+                sync_client_->GetPrefService()));
       }
 
       if (!disabled_types.Has(syncer::OUTGOING_PASSWORD_SHARING_INVITATION) &&
           sync_client_->GetPasswordSenderService()) {
-        syncer::ModelTypeControllerDelegate* delegate =
-            sync_client_->GetPasswordSenderService()
-                ->GetControllerDelegate()
-                .get();
-        controllers.push_back(std::make_unique<syncer::ModelTypeController>(
-            syncer::OUTGOING_PASSWORD_SHARING_INVITATION,
-            std::make_unique<syncer::ForwardingModelTypeControllerDelegate>(
-                delegate),
-            std::make_unique<syncer::ForwardingModelTypeControllerDelegate>(
-                delegate)));
+        controllers.push_back(
+            std::make_unique<
+                password_manager::
+                    OutgoingPasswordSharingInvitationModelTypeController>(
+                sync_service, sync_client_->GetPasswordSenderService(),
+                sync_client_->GetPrefService()));
       }
     }
   }
