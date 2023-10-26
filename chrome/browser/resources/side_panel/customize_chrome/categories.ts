3e37ea2b961c21c4b4bed0bb4c018e28186bb5fe
Riley Tatum <rtatum@google.com>
2023-10-26 22:00:45 +0000

    add category checkmark for wallpaper search
    * If flag is enabled, a wallpaper search image being set will show a
      checkmark on the wallpaper search category.
    * If flag is disabled, a wallpaper search image being set will not
      show a checkmark on any category.
    
    screenshot: http://screenshot/PSoK9KUCoXpYKyk
    
    Bug: b/300971008
    Change-Id: I0e2fd45957da9137ef401ce53a0c404c029a3b2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4981178
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1215775}

--- chrome/browser/resources/side_panel/customize_chrome/categories.ts
+++ chrome/browser/resources/side_panel/customize_chrome/categories.ts
@@ -30,6 +30,7 @@ export enum CategoryType {
   LOCAL,
   COLOR,
   COLLECTION,
+  WALLPAPER_SEARCH,
 }
 
 export const CHROME_THEME_COLLECTION_ELEMENT_ID =
@@ -83,6 +84,10 @@ export class CategoriesElement extends CategoriesElementBase {
       isLocalImageSelected_: {
         type: Boolean,
         computed: 'computeIsLocalImageSelected_(selectedCategory_)',
+      },
+      isWallpaperSearchSelected_: {
+        type: Boolean,
+        computed: 'computeIsWallpaperSearchSelected_(selectedCategory_)',
       },
       isChromeColorsSelected_: {
         type: Boolean,
@@ -172,7 +177,9 @@ export class CategoriesElement extends CategoriesElementBase {
       return {type: CategoryType.COLOR};
     }
     if (this.theme_.backgroundImage.isUploadedImage) {
-      return {type: CategoryType.LOCAL};
+      return this.theme_.backgroundImage.localBackgroundId ?
+          {type: CategoryType.WALLPAPER_SEARCH} :
+          {type: CategoryType.LOCAL};
     }
     if (this.theme_.backgroundImage.collectionId) {
       return {
@@ -189,6 +196,10 @@ export class CategoriesElement extends CategoriesElementBase {
 
   private computeIsLocalImageSelected_() {
     return this.selectedCategory_.type === CategoryType.LOCAL;
+  }
+
+  private computeIsWallpaperSearchSelected_() {
+    return this.selectedCategory_.type === CategoryType.WALLPAPER_SEARCH;
   }
 
   private computeIsChromeColorsSelected_() {
