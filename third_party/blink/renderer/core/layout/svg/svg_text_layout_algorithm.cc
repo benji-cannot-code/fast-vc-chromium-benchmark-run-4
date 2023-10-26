932acce2bebb6ca498b0ed1f45b472aa1f2083a7
Kent Tamura <tkent@chromium.org>
2023-10-26 08:14:47 +0000

    Remove SvgTextSkipZeroLengthItems flag
    It has been enabled since M117, and we have seen no issues.
    This CL has no production behavior changes.
    
    Change-Id: Ifbdeec573f2d30b59b12ac1ebe9d0940ae78dce7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4975687
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215374}

--- third_party/blink/renderer/core/layout/svg/svg_text_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/svg/svg_text_layout_algorithm.cc
@@ -135,8 +135,7 @@ void SvgTextLayoutAlgorithm::SetFlags(
   bool found_first_character = false;
   for (wtf_size_t i : sorted_item_indexes) {
     // Zero-length item is not addressable.
-    if (RuntimeEnabledFeatures::SvgTextSkipZeroLengthItemsEnabled() &&
-        items[i]->TextLength() == 0) {
+    if (items[i]->TextLength() == 0) {
       continue;
     }
     SvgPerCharacterInfo info;
