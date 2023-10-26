619beae72791dcb78a6e4047b8cf47a1d56b72aa
Maks Orlovich <morlovich@chromium.org>
2023-10-26 14:14:04 +0000

    [FLEDGE]: Start on WPT currency tests.
    For now, this just covers checking of returned bids vs. perBuyerCurrency, and the syntax of both.
    
    Change-Id: I9f6c7850d4c504671945532009856ae291b8d4b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4974015
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215496}

--- third_party/blink/web_tests/external/wpt/fledge/tentative/auction-config.https.window.js
+++ third_party/blink/web_tests/external/wpt/fledge/tentative/auction-config.https.window.js
@@ -8,7 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: variant=?6-10
 // META: variant=?11-15
 // META: variant=?16-20
-// META: variant=?21-last
+// META: variant=?21-25
+// META: variant=?26-last
 
 "use strict;"
 
@@ -276,4 +277,32 @@ makeTest({
       },
     ],
   },
+});
+
+makeTest({
+  name: 'perBuyerCurrencies with invalid currency',
+  expect: EXPECT_PROMISE_ERROR,
+  expectPromiseError: EXPECT_EXCEPTION(TypeError),
+  auctionConfigOverrides: {perBuyerCurrencies: {'*': 'Dollars'}}
+});
+
+makeTest({
+  name: 'perBuyerCurrencies with invalid currency map key',
+  expect: EXPECT_PROMISE_ERROR,
+  expectPromiseError: EXPECT_EXCEPTION(TypeError),
+  auctionConfigOverrides: {perBuyerCurrencies: {'example': 'USD'}}
+});
+
+makeTest({
+  name: 'perBuyerCurrencies with non-https currency map key',
+  expect: EXPECT_PROMISE_ERROR,
+  expectPromiseError: EXPECT_EXCEPTION(TypeError),
+  auctionConfigOverrides: {perBuyerCurrencies: {'http://example.org/': 'USD'}}
+});
+
+makeTest({
+  name: 'perBuyerCurrencies not convertible to dictionary',
+  expect: EXPECT_PROMISE_ERROR,
+  expectPromiseError: EXPECT_EXCEPTION(TypeError),
+  auctionConfigOverrides: {perBuyerCurrencies: 123}
 });
