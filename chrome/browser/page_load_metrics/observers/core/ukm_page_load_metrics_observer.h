ff26d13b82e12e60bee4a9d85867caf3c162cd7c
Harkiran Bolaria <hbolaria@google.com>
2023-10-26 10:56:14 +0000

    [CUI] Trace additional page load metrics.
    Trace NavigationToDOMContentLoadedEventFired and
    NavigationToMainFrameOnLoad with the new "interactions" category
    to match FCP and LCP logging.
    
    Add trace events to record User Timing metrics with the new
    'interactions' category, including navigation_id and URL. These
    were previously only recorded in UKM.
    
    Per b/306314585, browser tests for tracing will be added once
    the stdlib has been integrated into chromium.
    
    Bug: b/304950179
    Change-Id: I28385f45308aab58406a987bf69a6c8eb67ad9a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4952496
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Harkiran Bolaria <hbolaria@google.com>
    Cr-Commit-Position: refs/heads/main@{#1215418}

--- chrome/browser/page_load_metrics/observers/core/ukm_page_load_metrics_observer.h
+++ chrome/browser/page_load_metrics/observers/core/ukm_page_load_metrics_observer.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
+#include "base/trace_event/typed_macros.h"
 #include "components/page_load_metrics/browser/page_load_metrics_observer.h"
 #include "content/public/browser/navigation_handle_timing.h"
 #include "content/public/browser/site_instance_process_assignment.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/nqe/effective_connection_type.h"
 #include "services/metrics/public/cpp/ukm_source.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "third_party/perfetto/include/perfetto/tracing/event_context.h"
 #include "ui/base/page_transition_types.h"
 
 namespace content {
@@ -236,6 +238,9 @@ class UkmPageLoadMetricsObserver
   // Record some Responsiveness metrics that have occurred on the page until
   // the first time the page moves from the foreground to the background.
   void ReportResponsivenessAfterFirstForeground();
+
+  // Tracing helper for key page load events.
+  void EmitUserTimingEvent(base::TimeDelta duration, const char event_name[]);
 
   // Guaranteed to be non-null during the lifetime of |this|.
   raw_ptr<network::NetworkQualityTracker> network_quality_tracker_;
