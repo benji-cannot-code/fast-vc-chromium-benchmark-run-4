bc9573d35deb6e0401707772f4d3434718703b51
Hira Mahmood <hiramahmood@google.com>
2023-10-26 16:51:16 +0000

    [iOS] Create IPH bubble for parcel tracking
    This CL adds a bubble IPH to the magic stack for the parcel tracking
    feature after the user tracks a parcel for the first time.
    https://screenshot.googleplex.com/7quN9VyGCudUFZ8
    
    Bug: 1473449
    Change-Id: I40c0b9af500aba3a9d6b20b7199f8075fc3bd794
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4973904
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1215577}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.mm
@@ -18,9 +18,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ntp/set_up_list_item_type.h"
 #import "ios/chrome/browser/parcel_tracking/parcel_tracking_util.h"
 #import "ios/chrome/browser/safety_check/model/ios_chrome_safety_check_manager_constants.h"
+#import "ios/chrome/browser/shared/public/commands/parcel_tracking_opt_in_commands.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
+#import "ios/chrome/browser/shared/ui/util/layout_guide_names.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
+#import "ios/chrome/browser/shared/ui/util/util_swift.h"
 #import "ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_cells_constants.h"
 #import "ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_most_visited_action_item.h"
 #import "ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_most_visited_item.h"
@@ -1230,6 +1233,7 @@ const base::TimeDelta kSetUpListHideAnimationDuration = base::Milliseconds(250);
   _magicStack.axis = UILayoutConstraintAxisHorizontal;
   _magicStack.distribution = UIStackViewDistributionEqualSpacing;
   _magicStack.spacing = kMagicStackSpacing;
+  [self.layoutGuideCenter referenceView:_magicStack underName:kMagicStackGuide];
   _magicStack.accessibilityIdentifier = kMagicStackViewAccessibilityIdentifier;
   // Ensures modules take up entire height of the Magic Stack.
   _magicStack.alignment = UIStackViewAlignmentFill;
@@ -1405,6 +1409,7 @@ const base::TimeDelta kSetUpListHideAnimationDuration = base::Milliseconds(250);
         constraintEqualToConstant:kMagicStackEditButtonWidth],
     [editButton.heightAnchor constraintEqualToAnchor:editButton.widthAnchor]
   ]];
+  [self.parcelTrackingCommandHandler showParcelTrackingIPH];
 }
 
 // Adds two placeholder modules to Magic Stack.
