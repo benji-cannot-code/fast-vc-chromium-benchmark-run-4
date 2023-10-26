c31f7a77572e35c29278f039a7b57d71c7f1151c
David Padlipsky <dpad@google.com>
2023-10-26 21:10:04 +0000

    customization: Add shortcut_input_key element to ash/webui/common
    CL is big because it is mostly a copy paste from the shortcuts app.
    
    Icons were taken directly 1:1, shortcut_input_key was modified heavily.
    
    Bug: b/241965717
    Change-Id: Ieb74464fdaa30b3318cea14e6815dc5d765b9947
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4953566
    Auto-Submit: David Padlipsky <dpad@google.com>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215737}

---
+++ ash/webui/common/shortcut_input_key_strings.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_WEBUI_COMMON_SHORTCUT_INPUT_KEY_STRINGS_H_
+#define ASH_WEBUI_COMMON_SHORTCUT_INPUT_KEY_STRINGS_H_
+
+namespace content {
+class WebUIDataSource;
+}
+
+namespace ash {
+
+namespace common {
+
+void AddShortcutInputKeyStrings(content::WebUIDataSource* html_source);
+
+}  // namespace common
+
+}  // namespace ash
+
+#endif  // ASH_WEBUI_COMMON_SHORTCUT_INPUT_KEY_STRINGS_H_
