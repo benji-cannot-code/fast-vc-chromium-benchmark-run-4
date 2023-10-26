6d2a60a83ea16fa9f64edbaf658ed59e36f8434b
Viktor Semeniuk <vsemeniuk@google.com>
2023-10-26 14:01:25 +0000

    Add HaTS survey responses recording for Password Check
    When migrating to the new Password Manager UI HaTS recording was left
    behind. This CL adds them back.
    
    Fixed: 1496246
    Change-Id: I9d5c9340eac91a54926dc160f214c53d7785f77e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4978641
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215491}

--- chrome/browser/extensions/api/passwords_private/passwords_private_delegate_impl.cc
+++ chrome/browser/extensions/api/passwords_private/passwords_private_delegate_impl.cc
@@ -37,6 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "chrome/browser/ui/browser_window.h"
+#include "chrome/browser/ui/hats/trust_safety_sentiment_service.h"
+#include "chrome/browser/ui/hats/trust_safety_sentiment_service_factory.h"
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
 #include "chrome/browser/ui/web_applications/web_app_dialog_utils.h"
 #include "chrome/browser/web_applications/web_app_id_constants.h"
@@ -842,6 +844,12 @@ bool PasswordsPrivateDelegateImpl::UnmuteInsecureCredential(
 void PasswordsPrivateDelegateImpl::StartPasswordCheck(
     StartPasswordCheckCallback callback) {
   password_check_delegate_.StartPasswordCheck(std::move(callback));
+  auto* sentiment_service =
+      TrustSafetySentimentServiceFactory::GetForProfile(profile_);
+  if (!sentiment_service) {
+    return;
+  }
+  sentiment_service->RanPasswordCheck();
 }
 
 api::passwords_private::PasswordCheckStatus
