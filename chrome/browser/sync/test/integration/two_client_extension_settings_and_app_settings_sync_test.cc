e8cd0d530c4256732b30198da81aa4d416c8a0cb
Brahim Chikhaoui <bchikhaoui@google.com>
2023-10-26 11:55:04 +0000

    [Sync][Lacros] Fix TwoClientExtensionSettingsAndAppSettingsSyncTest
    Fix TwoClientExtensionSettingsAndAppSettingsSyncTest  sync_integration_tests when kSyncChromeOSAppsToggleSharing is enabled. On Lacros, if kSyncChromeOSAppsToggleSharing is enabled, syncing of Apps is controlled via the OS, not the browser.
    Override SetupClients to enable the OS Apps Sync for both clients for lacros.
    
    Bug: 1330894 , b:307483579
    Change-Id: I55ddd5bfaadfeb6d7ae3a020cd305a79df654723
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4976906
    Reviewed-by: Marc Treib <treib@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1215439}

--- chrome/browser/sync/test/integration/two_client_extension_settings_and_app_settings_sync_test.cc
+++ chrome/browser/sync/test/integration/two_client_extension_settings_and_app_settings_sync_test.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/test/integration/extensions_helper.h"
 #include "chrome/browser/sync/test/integration/sync_datatype_helper.h"
 #include "chrome/browser/sync/test/integration/sync_test.h"
+#include "components/sync/base/features.h"
+#include "components/sync/service/sync_service_impl.h"
 #include "content/public/test/browser_test.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -73,6 +75,22 @@ class TwoClientExtensionSettingsAndAppSettingsSyncTest
 
   bool UseVerifier() override {
     // TODO(crbug.com/1137735): rewrite tests to not use verifier.
+    return true;
+  }
+
+  bool SetupClients() override {
+    if (!SyncTest::SetupClients()) {
+      return false;
+    }
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+    // Apps sync is controlled by a dedicated preference on Lacros,
+    // corresponding to the Apps toggle in OS Sync settings.
+    // Enable the Apps toggle for both clients.
+    if (base::FeatureList::IsEnabled(syncer::kSyncChromeOSAppsToggleSharing)) {
+      GetSyncService(0)->GetUserSettings()->SetAppsSyncEnabledByOs(true);
+      GetSyncService(1)->GetUserSettings()->SetAppsSyncEnabledByOs(true);
+    }
+#endif
     return true;
   }
 };
