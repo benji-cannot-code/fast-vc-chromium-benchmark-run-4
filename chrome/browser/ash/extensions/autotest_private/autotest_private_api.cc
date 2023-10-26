5362795e46ec6a3be1992f1b3d972353baf3310c
Hidehiko Abe <hidehiko@chromium.org>
2023-10-26 17:11:32 +0000

    Remove LacrosMode.
    The concept of mode no longer makes sense as we removed LacrosSxS and
    LacrosPrimary.
    This CL removes the mode from the code more.
    
    The remaining one is autotest_private API, which currently is used
    by Tast, so that part will be cleaned up separately.
    
    This is preparation to move more code into
    chromeos/ash/components/standalone_browser with further refactoring.
    
    BUG=1494005
    TEST=Tryjob
    
    Change-Id: I3bead7d87ba3b6aad41cc0d8c3d972b84c245a54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4976378
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Georg Neis <neis@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215593}

--- chrome/browser/ash/extensions/autotest_private/autotest_private_api.cc
+++ chrome/browser/ash/extensions/autotest_private/autotest_private_api.cc
@@ -2086,14 +2086,9 @@ AutotestPrivateGetLacrosInfoFunction::ToLacrosState(
 
 // static
 api::autotest_private::LacrosMode
-AutotestPrivateGetLacrosInfoFunction::ToLacrosMode(
-    crosapi::browser_util::LacrosMode lacrosMode) {
-  switch (lacrosMode) {
-    case crosapi::browser_util::LacrosMode::kDisabled:
-      return api::autotest_private::LacrosMode::kDisabled;
-    case crosapi::browser_util::LacrosMode::kOnly:
-      return api::autotest_private::LacrosMode::kOnly;
-  }
+AutotestPrivateGetLacrosInfoFunction::ToLacrosMode(bool is_enabled) {
+  return is_enabled ? api::autotest_private::LacrosMode::kOnly
+                    : api::autotest_private::LacrosMode::kDisabled;
 }
 
 ExtensionFunction::ResponseAction AutotestPrivateGetLacrosInfoFunction::Run() {
@@ -2111,7 +2106,8 @@ ExtensionFunction::ResponseAction AutotestPrivateGetLacrosInfoFunction::Run() {
                    ? ""
                    : browser_manager->lacros_path().DirName().MaybeAsASCII())
           .Set("mode", api::autotest_private::ToString(ToLacrosMode(
-                           crosapi::browser_util::GetLacrosMode())))));
+                           crosapi::browser_util::IsLacrosEnabled())))
+          .Set("isEnabled", crosapi::browser_util::IsLacrosEnabled())));
 }
 
 ///////////////////////////////////////////////////////////////////////////////
