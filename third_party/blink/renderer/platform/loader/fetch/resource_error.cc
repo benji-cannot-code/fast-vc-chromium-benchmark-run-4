d70f3a863e507994ceb3889e3adf6885f1b7f1b6
Daniel Vogelheim <vogelheim@chromium.org>
2023-10-26 09:40:55 +0000

    Handle ORB network errors as DevTools issues
    ORB "v0.2" changes error handling from silently injecting an
    empty response to raising a network error. Since the default
    is to log errors on the console this can lead to a very "spam-y"
    console. To resolve this, we will always report ORB network
    errors (ERR_BLOCKED_BY_ORB) to DevTools as an issue, and will
    suppress console messages for the same errors.
    
    Bug: 1492995
    Change-Id: Ie8e3139331bc5135f1f3346b33f2c6cb35252475
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4956637
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215396}

--- third_party/blink/renderer/platform/loader/fetch/resource_error.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource_error.cc
@@ -215,6 +215,10 @@ bool ResourceError::WasBlockedByResponse() const {
   return error_code_ == net::ERR_BLOCKED_BY_RESPONSE;
 }
 
+bool ResourceError::WasBlockedByORB() const {
+  return error_code_ == net::ERR_BLOCKED_BY_ORB;
+}
+
 namespace {
 blink::ResourceRequestBlockedReason
 BlockedByResponseReasonToResourceRequestBlockedReason(
