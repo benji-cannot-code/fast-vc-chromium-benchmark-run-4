eb46007c15f803b931ca886a7144c2d367d03d54
dpapad <dpapad@chromium.org>
2023-10-27 22:14:48 +0000

    Remove load_time_data_deprecated.js usage in network errors UI.
    Instead use the proper ui/webui/resources/js/load_time_data.ts file.
    This CL eliminates the warning
    
    "crbug/1173575, non-JS module files deprecated."
    
    that was shown in the DevTools console of every network error UI (for
    example chrome://network-error/-10)
    
    Bug: 1173575
    Change-Id: I2d5289f25837effb8e0e97641b597e4fcfc95e66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4977279
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216390}

--- components/neterror/resources/neterror.js
+++ components/neterror/resources/neterror.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {mobileNav} from 'chrome://interstitials/common/resources/interstitial_mobile_nav.js';
+import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 
 import {HIDDEN_CLASS} from './constants.js';
 import {Runner} from './offline.js';
@@ -375,6 +376,9 @@ function onDocumentLoadOrUpdate() {
 }
 
 function onDocumentLoad() {
+  // `loadTimeDataRaw` is injected to the `window` scope from C++.
+  loadTimeData.data = window.loadTimeDataRaw;
+
   // Sets up the proper button layout for the current platform.
   const buttonsDiv = document.getElementById('buttons');
   if (primaryControlOnLeft) {
