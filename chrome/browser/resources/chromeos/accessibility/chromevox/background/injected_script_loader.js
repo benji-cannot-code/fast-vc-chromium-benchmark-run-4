8b42ff79a6d7d7d07ccec162056dc65be10c5e6d
Anastasia Helfinstein <anastasi@google.com>
2023-10-27 21:50:58 +0000

    [ChromeVox] Fix Google Docs access for tabs open before ChromeVox is
    Fixes a regression to Google Docs when the tab is open before
    ChromeVox is started.
    
    Test coming in fast follow.
    
    R=katie
    
    Docs tab is open before ChromeVox starts.
    
    AX-Relnotes: Fixes an issue with ChromeVox and Google Docs when the
    Bug: b/308009321
    Test: Manual repro, automated test coming soon
    Change-Id: Ica8630126b83b9387a9b95fc1183455ab61d3604
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4981354
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216379}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/injected_script_loader.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/injected_script_loader.js
@@ -111,7 +111,8 @@ export class InjectedScriptLoader {
   async executeCodeInAllTabs_(tabs) {
     for (const tab of tabs) {
       // Inject the ChromeVox content script code into the tab.
-      await Promise.all(this.code_.map(script => this.execute_(script, tab)));
+      await Promise.all(
+          Object.values(this.code_).map(script => this.execute_(script, tab)));
     }
   }
 
