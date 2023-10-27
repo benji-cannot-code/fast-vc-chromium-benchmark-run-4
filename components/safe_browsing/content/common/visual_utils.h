0b64acccb125ce750d455158a51eeb6ef3b03f24
Seung Jae Lim <andysjlim@chromium.org>
2023-10-27 16:45:58 +0000

    Log why visual features are not present in CSD-Phishing & PG pings
    Bug: 1493463
    Change-Id: I2a878e2719fca3f41669533cf62cd5b7d8df01ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4979734
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Seung Jae Lim <andysjlim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216192}

--- components/safe_browsing/content/common/visual_utils.h
+++ components/safe_browsing/content/common/visual_utils.h
@@ -16,6 +16,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace safe_browsing::visual_utils {
 
+// Enum used to represent the result of the function |CanExtractVisualFeatures|.
+enum class CanExtractVisualFeaturesResult {
+  kCanExtractVisualFeatures = 0,
+  kNotExtendedReporting = 1,
+  kOffTheRecord = 2,
+  kBelowMinFrame = 3,
+  kAboveZoomLevel = 4,
+  kMaxValue = kAboveZoomLevel,
+};
+
 // Computes the BlurredImage for the given input image. This involves
 // downsampling the image to a certain fixed resolution, then blurring
 // by taking an average over fixed-size blocks of pixels.
@@ -32,14 +42,16 @@ std::unique_ptr<SkBitmap> BlockMeanAverage(const SkBitmap& image,
 // Whether we can extract visual features from a page with a given size and zoom
 // level.
 #if BUILDFLAG(IS_ANDROID)
-bool CanExtractVisualFeatures(bool is_extended_reporting,
-                              bool is_off_the_record,
-                              gfx::Size size);
+CanExtractVisualFeaturesResult CanExtractVisualFeatures(
+    bool is_extended_reporting,
+    bool is_off_the_record,
+    gfx::Size size);
 #else
-bool CanExtractVisualFeatures(bool is_extended_reporting,
-                              bool is_off_the_record,
-                              gfx::Size size,
-                              double zoom_level);
+CanExtractVisualFeaturesResult CanExtractVisualFeatures(
+    bool is_extended_reporting,
+    bool is_off_the_record,
+    gfx::Size size,
+    double zoom_level);
 #endif
 
 // Extract a VisualFeatures proto from the given `bitmap`.
