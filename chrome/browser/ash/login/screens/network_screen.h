7540ae8d179bf5b6a9f96c9481f35ea1c9ccd7b8
Renato Silva <rrsilva@google.com>
2023-10-27 11:47:42 +0000

    [QuickStart][OOBE] Polish QuickStart WiFi steps.
    - Isolate the QuickStart actions when showing the screen into its own
      function.
    - Gracefully handle the 'Back' button on the network list when the
      QuickStart flow is going on.
    - Add a slot to oobe_loading_dialog so that callers can embed their own
      button and adjust the QuickStart screen step accordingly.
    - Make the default subtitle while showing the network list customizable
      so that QuickStart shows a special one requesting the user to manually
      add a network.
    
    Bug: b/307899389, b/283724988
    Change-Id: Ibccb39d8af09823b678fcbfc0801408aedff00d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4976152
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1216065}

--- chrome/browser/ash/login/screens/network_screen.h
+++ chrome/browser/ash/login/screens/network_screen.h
@@ -141,6 +141,7 @@ class NetworkScreen : public BaseScreen, public NetworkStateHandlerObserver {
       const std::string& message);
 
   void ExitQuickStartFlow();
+  void ShowStepsWhenQuickStartOngoing();
 
   // Skip this screen or automatically continue if the device is connected to
   // Ethernet for the first time in this session.
--- chrome/browser/resources/chromeos/login/screens/oobe/oobe_network.js
+++ chrome/browser/resources/chromeos/login/screens/oobe/oobe_network.js
@@ -68,6 +68,7 @@ NetworkScreenBase.$;
  * Data that is passed to the screen during onBeforeShow.
  * @typedef {{
  *   ssid: (string|undefined),
+ *   useQuickStartSubtitle: (boolean|undefined),
  * }}
  */
 let NetworkScreenData;
@@ -129,9 +130,17 @@ class NetworkScreen extends NetworkScreenBase {
         value: false,
       },
 
+      // SSID (WiFi Network Name) used during the QuickStart step.
       ssid: {
         type: String,
         value: '',
+      },
+
+      // Whether the QuickStart subtitle should be shown while showing the
+      // network list
+      useQuickStartSubtitle_: {
+        type: Boolean,
+        value: false,
       },
     };
   }
@@ -164,6 +173,9 @@ class NetworkScreen extends NetworkScreenBase {
       this.setUIStep(NetworkScreenStates.QUICK_START_CONNECTING);
       return;
     }
+
+    this.useQuickStartSubtitle_ = data && 'useQuickStartSubtitle' in data &&
+      data['useQuickStartSubtitle'];
 
     this.setUIStep(NetworkScreenStates.DEFAULT);
     this.enableWifiScans_ = true;
@@ -210,6 +222,11 @@ class NetworkScreen extends NetworkScreenBase {
     if (errorMessage) {
       return errorMessage;
     }
+
+    if (this.useQuickStartSubtitle_) {
+      return this.i18n('quickStartNetworkNeededSubtitle');
+    }
+
     return this.i18n('networkSectionSubtitle');
   }
 
