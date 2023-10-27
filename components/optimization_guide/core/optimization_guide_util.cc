81e12e1d8cc77fa0485254c09c1adc40cdff85f4
Sophie Chang <sophiechang@chromium.org>
2023-10-27 17:47:12 +0000

    Only pass one of API key or access token to remote optimization guide service
    Bug: 1496625
    Change-Id: I5fecd9aaf7fea613d4943ecc214d1ac8660195ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4982258
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216242}

--- components/optimization_guide/core/optimization_guide_util.cc
+++ components/optimization_guide/core/optimization_guide_util.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 constexpr char kAuthHeaderBearer[] = "Bearer ";
+constexpr char kApiKeyHeader[] = "X-Goog-Api-Key";
 
 optimization_guide::proto::Platform GetPlatform() {
 #if BUILDFLAG(IS_WIN)
@@ -116,10 +117,16 @@ void LogFeatureFlagsInfo(OptimizationGuideLogger* optimization_guide_logger,
 void PopulateAuthorizationRequestHeader(
     network::ResourceRequest* resource_request,
     std::string_view access_token) {
-  DCHECK(!access_token.empty());
+  CHECK(!access_token.empty());
   resource_request->headers.SetHeader(
       net::HttpRequestHeaders::kAuthorization,
       base::StrCat({kAuthHeaderBearer, access_token}));
+}
+
+void PopulateApiKeyRequestHeader(network::ResourceRequest* resource_request,
+                                 std::string_view api_key) {
+  CHECK(!api_key.empty());
+  resource_request->headers.SetHeader(kApiKeyHeader, api_key);
 }
 
 }  // namespace optimization_guide
