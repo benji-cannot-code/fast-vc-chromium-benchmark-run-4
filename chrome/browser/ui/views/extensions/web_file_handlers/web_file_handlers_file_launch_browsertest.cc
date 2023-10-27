13d81f5a8718d6c706a05a9180afac42ce2a7250
Solomon Kinard <solomonkinard@chromium.org>
2023-10-27 19:25:12 +0000

    Extensions: WFH: Require an extension and decision on extension feature
    Tests now verify behavior using the flag instead of verifying behavior
    without it. The feature should only be available behind the flag. This
    will make it easier to gate things behind the flag in a future CL that
    will simply move this feature to stable and make the flag default
    enabled.
    
    Bug: chromium:1448893
    Change-Id: Ia20702fda1f71e477bfea849b137741533096cdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4982048
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216304}

--- chrome/browser/ui/views/extensions/web_file_handlers/web_file_handlers_file_launch_browsertest.cc
+++ chrome/browser/ui/views/extensions/web_file_handlers/web_file_handlers_file_launch_browsertest.cc
@@ -469,19 +469,10 @@ class WebFileHandlersFileLaunchBrowserTest
     return intent;
   }
 
-  // Set channel to the provided argument and clear extension features.
-  void SetChannelAndResetFeatureList(version_info::Channel channel) {
-    extensions::SetCurrentChannel(channel);
-    feature_list_.Reset();
-  }
-
  private:
   extensions::TestExtensionDir extension_dir_;
 
   base::test::ScopedFeatureList feature_list_;
-  // TODO(crbug.com/1448893): Remove channel overrides when available in stable.
-  extensions::ScopedCurrentChannel current_channel_{
-      version_info::Channel::BETA};
 
   // Basic manifest for web file handlers.
   static constexpr char const kManifest[] = R"({
@@ -558,21 +549,9 @@ IN_PROC_BROWSER_TEST_F(WebFileHandlersFileLaunchBrowserTest, CallSetConsumer) {
   LaunchExtensionAndCatchResult(*extension);
 }
 
-// Reuse `WebFileHandlersFileLaunchBrowserTest` but make the following changes:
-// * Use stable channel instead of beta.
-// * Remove all extension flags, especially `kExtensionWebFileHandlers`.
-class WebFileHandlersFileLaunchOnStableChannelBrowserTest
-    : public WebFileHandlersFileLaunchBrowserTest {
- public:
-  WebFileHandlersFileLaunchOnStableChannelBrowserTest() {
-    SetChannelAndResetFeatureList(version_info::Channel::STABLE);
-  }
-};
-
 // Verify that the Ash component of this ChromeOS feature is on the stable
 // channel.
-IN_PROC_BROWSER_TEST_F(WebFileHandlersFileLaunchOnStableChannelBrowserTest,
-                       QuickOffice) {
+IN_PROC_BROWSER_TEST_F(WebFileHandlersFileLaunchBrowserTest, QuickOffice) {
   const std::string manifest =
       base::StringPrintf(R"({
     "name": "Test",
@@ -602,8 +581,7 @@ IN_PROC_BROWSER_TEST_F(WebFileHandlersFileLaunchOnStableChannelBrowserTest,
 
 // Verify that the Ash component of this ChromeOS feature is on the stable
 // channel.
-IN_PROC_BROWSER_TEST_F(WebFileHandlersFileLaunchOnStableChannelBrowserTest,
-                       DefaultInstalled) {
+IN_PROC_BROWSER_TEST_F(WebFileHandlersFileLaunchBrowserTest, DefaultInstalled) {
   static constexpr char kManifest[] = R"({
     "name": "Test",
     "version": "0.0.1",
@@ -619,9 +597,9 @@ IN_PROC_BROWSER_TEST_F(WebFileHandlersFileLaunchOnStableChannelBrowserTest,
   auto* extension = WriteToDirAndLoadDefaultInstalledExtension(kManifest);
   ASSERT_TRUE(extension);
 
-  // Web File Handlers are not supported.
-  ASSERT_FALSE(
-      extensions::WebFileHandlers::SupportsWebFileHandlers(*extension));
+  ASSERT_TRUE(extensions::WebFileHandlers::SupportsWebFileHandlers(*extension));
+  ASSERT_TRUE(
+      extensions::WebFileHandlers::CanBypassPermissionDialog(*extension));
 }
 
 // Verify `{"launch_type": "multiple-clients"}`.
