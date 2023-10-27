c0794710f56c346d054469c873acb7933d4d8bf5
sbingler <bingler@chromium.org>
2023-10-27 17:37:49 +0000

    Field test CookieSameSiteConsidersRedirectChain
    Begin field trial testing in preparation for a finch experiment.
    
    Also fix a couple tests that incorrectly assumed the feature was
    disabled.
    
    Bug: 1221316
    Change-Id: I74b9a6c9d73de0156420798efa7289616ba44a7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4980087
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216234}

--- content/browser/download/download_browsertest.cc
+++ content/browser/download/download_browsertest.cc
@@ -4230,10 +4230,9 @@ class DownloadContentSameSiteCookieTest
       public ::testing::WithParamInterface<bool> {
  public:
   DownloadContentSameSiteCookieTest() {
-    if (DoesCookieSameSiteConsiderRedirectChain()) {
-      inner_feature_list_.InitAndEnableFeature(
-          net::features::kCookieSameSiteConsidersRedirectChain);
-    }
+    inner_feature_list_.InitWithFeatureState(
+        net::features::kCookieSameSiteConsidersRedirectChain,
+        DoesCookieSameSiteConsiderRedirectChain());
   }
 
   bool DoesCookieSameSiteConsiderRedirectChain() { return GetParam(); }
