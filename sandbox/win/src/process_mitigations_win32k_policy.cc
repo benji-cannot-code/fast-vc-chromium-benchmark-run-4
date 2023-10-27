b7ddba22b52eb4bd475eb65bd007dcbd97d9bd67
Alex Gough <ajgo@chromium.org>
2023-10-27 17:46:04 +0000

    Remove unused arguments in ProcessMitigationsWin32KLockdownPolicy
    SubSystem & name are not used here and can be removed.
    
    Bug: N/A
    Change-Id: Ie1c8910c95666acbfd39fb8308d43c30a7c60065
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4981097
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216240}

--- sandbox/win/src/process_mitigations_win32k_policy.cc
+++ sandbox/win/src/process_mitigations_win32k_policy.cc
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace sandbox {
 
 bool ProcessMitigationsWin32KLockdownPolicy::GenerateRules(
-    const wchar_t* name,
-    Semantics semantics,
     LowLevelPolicy* policy) {
   PolicyRule rule(FAKE_SUCCESS);
   if (!policy->AddRule(IpcTag::GDI_GDIDLLINITIALIZE, &rule))
