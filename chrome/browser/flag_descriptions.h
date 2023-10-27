cd710b5486e12f422e606ebe276442cc633a30a2
Irem Uguz <iremuguz@google.com>
2023-10-27 11:48:29 +0000

    support-tool: Remove feature flag that guards copy token button
    The copy token button is a small UI change and since the whole UI is
    guarded by chrome://flags#support-tool, it is redundant. Remove the flag
    from UI and remove the usages of it in the code. Will remove the flag
    in a follow-up CL.
    
    Bug: b:307724327
    Change-Id: I7da9e017530799e7b5637794e81275b9447b47b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4973265
    Commit-Queue: Irem Uguz <iremuguz@google.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216066}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1853,9 +1853,6 @@ extern const char kStylusBatteryStatusDescription[];
 extern const char kSupportTool[];
 extern const char kSupportToolDescription[];
 
-extern const char kSupportToolCopyTokenButton[];
-extern const char kSupportToolCopyTokenButtonDescription[];
-
 extern const char kSupportToolScreenshot[];
 extern const char kSupportToolScreenshotDescription[];
 
