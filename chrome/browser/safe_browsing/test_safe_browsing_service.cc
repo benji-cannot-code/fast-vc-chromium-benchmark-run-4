b2843e3747f448556ca8fb3f7191c94f345cfc11
Daniel Rubery <drubery@chromium.org>
2023-10-27 22:00:30 +0000

    Send CSBRR on BYPASS_DEEP_SCAN
    The deep scanning prompt is equivalent to a warning, so we should send a
    CSBRR when it is bypassed. This CL adds that and tests it.
    
    Also serilized_download_report -> serialized_download_report.
    
    Change-Id: Ia2d6605962855e996340b080d4dbc0d602682e23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4968674
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216384}

--- chrome/browser/safe_browsing/test_safe_browsing_service.cc
+++ chrome/browser/safe_browsing/test_safe_browsing_service.cc
@@ -66,7 +66,7 @@ base::CallbackListSubscription TestSafeBrowsingService::RegisterStateCallback(
   return {};
 }
 
-std::string TestSafeBrowsingService::serilized_download_report() {
+std::string TestSafeBrowsingService::serialized_download_report() {
   return serialized_download_report_;
 }
 
--- chrome/browser/ui/webui/downloads/downloads_dom_handler_unittest.cc
+++ chrome/browser/ui/webui/downloads/downloads_dom_handler_unittest.cc
@@ -211,7 +211,7 @@ TEST_F(DownloadsDOMHandlerWithFakeSafeBrowsingTest, DiscardDangerous) {
   // Verify that dangerous download report is not sent because the feature flag
   // is disabled.
   EXPECT_TRUE(test_safe_browsing_factory_->test_safe_browsing_service()
-                  ->serilized_download_report()
+                  ->serialized_download_report()
                   .empty());
 }
 
@@ -251,7 +251,7 @@ TEST_F(DownloadsDOMHandlerWithFakeSafeBrowsingTestNewCsbrrTrigger,
   expected_report.SerializeToString(&expected_serialized_report);
   EXPECT_EQ(expected_serialized_report,
             test_safe_browsing_factory_->test_safe_browsing_service()
-                ->serilized_download_report());
+                ->serialized_download_report());
 }
 
 TEST_F(DownloadsDOMHandlerWithFakeSafeBrowsingTestNewCsbrrTrigger,
@@ -269,7 +269,7 @@ TEST_F(DownloadsDOMHandlerWithFakeSafeBrowsingTestNewCsbrrTrigger,
   // Verify that dangerous download report is not sent because the download is
   // already in complete state.
   EXPECT_TRUE(test_safe_browsing_factory_->test_safe_browsing_service()
-                  ->serilized_download_report()
+                  ->serialized_download_report()
                   .empty());
 }
 
@@ -288,7 +288,7 @@ TEST_F(DownloadsDOMHandlerWithFakeSafeBrowsingTestNewCsbrrTrigger,
 
   // Verify that dangerous download report is not sent because the URL is empty.
   EXPECT_TRUE(test_safe_browsing_factory_->test_safe_browsing_service()
-                  ->serilized_download_report()
+                  ->serialized_download_report()
                   .empty());
 }
 
@@ -310,7 +310,7 @@ TEST_F(DownloadsDOMHandlerWithFakeSafeBrowsingTestNewCsbrrTrigger,
   // Verify that dangerous download report is not sent because it's in
   // Incognito.
   EXPECT_TRUE(test_safe_browsing_factory_->test_safe_browsing_service()
-                  ->serilized_download_report()
+                  ->serialized_download_report()
                   .empty());
 }
 
