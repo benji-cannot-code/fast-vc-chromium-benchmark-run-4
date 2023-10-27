2efc45569d4922dd2163b3fafe26326a492470cf
punithnayak <npunith125@gmail.com>
2023-10-27 02:03:06 +0000

    removing dangling pointer from PlaybackCommandForwardingRendererTest
    Change-Id: Id5ddd096a9836ec442c0cb9b07bcb77ef542e806
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4963260
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Commit-Queue: Punith Nayak <npunith125@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1215894}

--- components/cast_streaming/renderer/control/playback_command_forwarding_renderer_unittest.cc
+++ components/cast_streaming/renderer/control/playback_command_forwarding_renderer_unittest.cc
@@ -79,7 +79,9 @@ class PlaybackCommandForwardingRendererTest : public testing::Test {
             remote_client_.InitWithNewEndpointAndPassReceiver());
   }
 
-  ~PlaybackCommandForwardingRendererTest() override = default;
+  ~PlaybackCommandForwardingRendererTest() override {
+    mock_renderer_ = nullptr;
+  }
 
  protected:
   void CallInitialize() {
