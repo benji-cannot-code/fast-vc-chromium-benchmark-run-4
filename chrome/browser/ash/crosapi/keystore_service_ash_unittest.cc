2132de93f54d3e1594a19415476d0f96274c21d7
Michael Ershov <miersh@google.com>
2023-10-27 15:29:34 +0000

    PlatformKeysService: Remove unique_ptr from GetTokens' result
    Wrapping the result in std::unique_ptr was probably done a
    long time ago as an optimization against copying big vectors.
    Nowadays vectors can also be std::move-d and even if the tokens
    vector is accidentally copied, it's very small.
    
    Fixed: b:308106080
    Test: Compilation, existing tests
    Change-Id: Ica9f45d94d21e8c3f086d56da0bd2bb24f2e0901
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4983489
    Commit-Queue: Michael Ershov <miersh@google.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1216144}

--- chrome/browser/ash/crosapi/keystore_service_ash_unittest.cc
+++ chrome/browser/ash/crosapi/keystore_service_ash_unittest.cc
@@ -124,11 +124,6 @@ std::vector<uint8_t> CertToBlob(
       cert_buffer, cert_buffer + CRYPTO_BUFFER_len(cert->cert_buffer()));
 }
 
-std::unique_ptr<std::vector<TokenId>> MakeTokenIds(
-    std::initializer_list<TokenId> init_list) {
-  return std::make_unique<std::vector<TokenId>>(init_list);
-}
-
 void AssertBlobEq(const mojom::KeystoreBinaryResultPtr& result,
                   const std::vector<uint8_t>& expected_blob) {
   ASSERT_TRUE(result);
@@ -575,7 +570,7 @@ TEST_F(KeystoreServiceAshTest, BadCertificateGetPublicKeyFail) {
 
 TEST_F(KeystoreServiceAshTest, GetKeyStoresEmptySuccess) {
   EXPECT_CALL(platform_keys_service_, GetTokens)
-      .WillOnce(RunOnceCallback<0>(MakeTokenIds({}), Status::kSuccess));
+      .WillOnce(RunOnceCallback<0>(std::vector<TokenId>({}), Status::kSuccess));
 
   CallbackObserver<mojom::GetKeyStoresResultPtr> observer;
   keystore_service_.GetKeyStores(observer.GetCallback());
@@ -587,8 +582,8 @@ TEST_F(KeystoreServiceAshTest, GetKeyStoresEmptySuccess) {
 
 TEST_F(KeystoreServiceAshTest, GetKeyStoresUserSuccess) {
   EXPECT_CALL(platform_keys_service_, GetTokens)
-      .WillOnce(
-          RunOnceCallback<0>(MakeTokenIds({TokenId::kUser}), Status::kSuccess));
+      .WillOnce(RunOnceCallback<0>(std::vector<TokenId>({TokenId::kUser}),
+                                   Status::kSuccess));
 
   CallbackObserver<mojom::GetKeyStoresResultPtr> observer;
   keystore_service_.GetKeyStores(observer.GetCallback());
@@ -601,7 +596,7 @@ TEST_F(KeystoreServiceAshTest, GetKeyStoresUserSuccess) {
 
 TEST_F(KeystoreServiceAshTest, GetKeyStoresDeviceSuccess) {
   EXPECT_CALL(platform_keys_service_, GetTokens)
-      .WillOnce(RunOnceCallback<0>(MakeTokenIds({TokenId::kSystem}),
+      .WillOnce(RunOnceCallback<0>(std::vector<TokenId>({TokenId::kSystem}),
                                    Status::kSuccess));
 
   CallbackObserver<mojom::GetKeyStoresResultPtr> observer;
@@ -616,7 +611,8 @@ TEST_F(KeystoreServiceAshTest, GetKeyStoresDeviceSuccess) {
 TEST_F(KeystoreServiceAshTest, GetKeyStoresDeviceUserSuccess) {
   EXPECT_CALL(platform_keys_service_, GetTokens)
       .WillOnce(RunOnceCallback<0>(
-          MakeTokenIds({TokenId::kUser, TokenId::kSystem}), Status::kSuccess));
+          std::vector<TokenId>({TokenId::kUser, TokenId::kSystem}),
+          Status::kSuccess));
 
   CallbackObserver<mojom::GetKeyStoresResultPtr> observer;
   keystore_service_.GetKeyStores(observer.GetCallback());
@@ -630,7 +626,8 @@ TEST_F(KeystoreServiceAshTest, GetKeyStoresDeviceUserSuccess) {
 
 TEST_F(KeystoreServiceAshTest, GetKeyStoresFail) {
   EXPECT_CALL(platform_keys_service_, GetTokens)
-      .WillOnce(RunOnceCallback<0>(MakeTokenIds({}), Status::kErrorInternal));
+      .WillOnce(
+          RunOnceCallback<0>(std::vector<TokenId>({}), Status::kErrorInternal));
 
   CallbackObserver<mojom::GetKeyStoresResultPtr> observer;
   keystore_service_.GetKeyStores(observer.GetCallback());
--- chrome/browser/ash/platform_keys/platform_keys_service_test_util.cc
+++ chrome/browser/ash/platform_keys/platform_keys_service_test_util.cc
@@ -16,8 +16,7 @@ Status StatusWaiter::status() {
   return Get();
 }
 
-const std::unique_ptr<std::vector<TokenId>>&
-GetTokensExecutionWaiter::token_ids() {
+const std::vector<TokenId>& GetTokensExecutionWaiter::token_ids() {
   return Get<0>();
 }
 
