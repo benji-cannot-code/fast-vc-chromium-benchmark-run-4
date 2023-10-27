ae3570c3a3516d427ef452bdc0a4b4dc46d915cf
Salma Elmahallawy <esalma@google.com>
2023-10-27 19:42:09 +0000

    [IdleTimeout] Removing unneeded code
    This removes code that was left behind from previous CL and is no
    longer used. The CL also fixes a test that fails on iOS when it is
    added to the build because there is no `clear_site_settings`.
    
    Change-Id: I646984bc482e4b3991f4bf5aa49b02f8557432a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4984706
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216316}

--- components/enterprise/idle/idle_timeout_policy_handler_unittest.cc
+++ components/enterprise/idle/idle_timeout_policy_handler_unittest.cc
@@ -12,10 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/values_util.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
 #include "base/values.h"
-#include "components/browsing_data/core/features.h"
 #include "components/enterprise/idle/action_type.h"
 #include "components/enterprise/idle/idle_pref_names.h"
 #include "components/policy/core/browser/configuration_policy_handler.h"
@@ -422,12 +420,12 @@ TEST_F(IdleTimeoutPolicyHandlerTest, SyncTypesDisabledForClearActions) {
   EXPECT_TRUE(pref_value->is_list());
   EXPECT_THAT(pref_value->GetList(),
               testing::ElementsAre(
-#if !BUILDFLAG(IS_ANDROID)
+#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
                   static_cast<int>(ActionType::kCloseBrowsers),
                   static_cast<int>(ActionType::kShowProfilePicker),
                   static_cast<int>(ActionType::kClearDownloadHistory),
                   static_cast<int>(ActionType::kClearHostedAppData),
-#endif  // !BUILDFLAG(IS_ANDROID)
+#endif  // !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
                   static_cast<int>(ActionType::kClearBrowsingHistory),
                   static_cast<int>(ActionType::kClearCookiesAndOtherSiteData),
                   static_cast<int>(ActionType::kClearCachedImagesAndFiles),
@@ -441,7 +439,11 @@ TEST_F(IdleTimeoutPolicyHandlerTest, SyncTypesDisabledForClearActions) {
   bool enabled;
   ASSERT_TRUE(
       prefs().GetBoolean(syncer::prefs::internal::kSyncPreferences, &enabled));
+#if BUILDFLAG(IS_IOS)
+  EXPECT_TRUE(enabled);
+#else
   EXPECT_FALSE(enabled);
+#endif  // BUILDFLAG(IS_IOS)
   ASSERT_TRUE(
       prefs().GetBoolean(syncer::prefs::internal::kSyncHistory, &enabled));
   EXPECT_FALSE(enabled);
