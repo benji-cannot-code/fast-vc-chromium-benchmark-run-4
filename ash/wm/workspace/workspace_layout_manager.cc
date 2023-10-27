6b05fed69c25bdbeaf99ff781b9f5464cb6eb402
Masa Fujita <massan@google.com>
2023-10-27 02:15:08 +0000

    [reland] pip-tuck: Add PipController
    This is a reland for go/chrcl/4951313 with fixes. The issue was  the
    `pip_window_observation_` not being `Reset` on `OnWindowDestroying()`
    which caused MSAN issues.
    
    -
    
    Currently there is no general controller for Picture-in-Picture (PiP)
    windows. Having a dedicated controller for PiP windows would be useful
    because:
    
    1. There are multiple parts of the code that can be simplified by
       introducing a controller that exists even when the PiP window does
       not exist, without having to rely on `WindowState` which should be
       reserved for more generic window functions. Examples include existing
       `CompleteDrag()` animations and tucking behavior for PiP which we are
       planning to implement.
    
    2. A dedicated controller would allow for consolidation of the PiP
       window management controls code.
    
    This commit introduces the new class `PipController`, and also
    transplants `UpdatePipBounds()` from `WindowState` to the new
    `PipController`.
    
    This new `PipController` holds a reference to the window it is managing
    which can be obtained with `pip_window()`, and it is set with
    `SetPipWindow()` and unset with `UnsetPipWindow()`. It also holds an
    observer that observes this window when it is set.
    
    Bug: b/305611254
    Test: ash_unittests PipControllerTest*
    Change-Id: I54fb96ba0e5d4de8220b63da8cb823ca9ea4dee4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4975544
    Auto-Submit: Masa Fujita <massan@google.com>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Masa Fujita <massan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1215898}

--- ash/wm/workspace/workspace_layout_manager.cc
+++ ash/wm/workspace/workspace_layout_manager.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/desks/desks_controller.h"
 #include "ash/wm/desks/desks_util.h"
 #include "ash/wm/fullscreen_window_finder.h"
+#include "ash/wm/pip/pip_controller.h"
 #include "ash/wm/screen_pinning_controller.h"
 #include "ash/wm/window_positioner.h"
 #include "ash/wm/window_properties.h"
@@ -574,7 +575,7 @@ void WorkspaceLayoutManager::MaybeUpdateA11yFloatingPanelOrPipBounds() const {
   for (auto* window : windows_) {
     WindowState* window_state = WindowState::Get(window);
     if (window_state->IsPip()) {
-      window_state->UpdatePipBounds();
+      Shell::Get()->pip_controller()->UpdatePipBounds();
     }
   }
 }
