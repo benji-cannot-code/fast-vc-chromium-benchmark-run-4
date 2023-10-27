6acc5767284e7850b87d2f865b1a4f95ecd5a020
Enrico Galli <enrico.galli@intel.com>
2023-10-27 00:57:17 +0000

    WebNN: Implement createContextSync method for the Mojo context type
    This CL implements the ML::createContextSync()[1] method which is only
    exposed to dedicated workers.
    
    [1] https://www.w3.org/TR/webnn/#api-ml-createcontextsync
    
    Bug: 1273291, 1488162
    Change-Id: I95e2d838fd23989fbd737975fbab49ddfc99ce15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4970360
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Commit-Queue: Enrico Galli <enrico.galli@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1215866}

--- third_party/blink/renderer/modules/ml/webnn/ml_context_mojo.h
+++ third_party/blink/renderer/modules/ml/webnn/ml_context_mojo.h
@@ -29,7 +29,8 @@ class MODULES_EXPORT MLContextMojo : public MLContext {
                                      MLContextOptions* options,
                                      ML* ml);
 
-  static MLContext* ValidateAndCreateSync(ExceptionState& exception_state,
+  static MLContext* ValidateAndCreateSync(ScriptState* script_state,
+                                          ExceptionState& exception_state,
                                           MLContextOptions* options,
                                           ML* ml);
 
@@ -57,7 +58,11 @@ class MODULES_EXPORT MLContextMojo : public MLContext {
   void CreateAsyncImpl(ScriptPromiseResolver* resolver,
                        MLContextOptions* options) override;
 
-  MLContext* CreateSyncImpl(MLContextOptions* options,
+  // Create `WebNNContext` message pipe with `ML` mojo interface, then
+  // create the context with the hardware accelerated OS machine
+  // learning API in the WebNN Service.
+  MLContext* CreateSyncImpl(ScriptState* script_state,
+                            MLContextOptions* options,
                             ExceptionState& exception_state) override;
 
  private:
