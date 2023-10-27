880f22a880c181f7b1f62fe7528a3d9c80240080
gate kibr <gkvjwa@gmail.com>
2023-10-27 14:27:15 +0000

    Fix mojo_perftests
    Reset TestSupport value after using ScopedMojoSupport helper
    
    Change-Id: I2666f89a18fdc63e72081db4076b61d2740d7d99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4963120
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1216087}

--- mojo/core/test/run_all_perftests.cc
+++ mojo/core/test/run_all_perftests.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/test/perf_test_suite.h"
 #include "mojo/core/test/scoped_mojo_support.h"
-#include "mojo/core/test/test_support_impl.h"
 #include "mojo/public/tests/test_support_private.h"
 
 int main(int argc, char** argv) {
@@ -23,6 +22,5 @@ int main(int argc, char** argv) {
 
   base::PerfTestSuite test(argc, argv);
   mojo::core::test::ScopedMojoSupport mojo_support;
-  mojo::test::TestSupport::Init(new mojo::core::test::TestSupportImpl());
   return test.Run();
 }
