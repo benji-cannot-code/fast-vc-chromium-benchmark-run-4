2c4507627faa5882598972a5bbbeb327fae6e4f9
Mark Schillaci <mschillaci@google.com>
2023-10-27 14:49:30 +0000

    [UI AX Cleanup] Simplify logic for ax export definitions
    This is a small CL with no functional changes that updates the
    definitions of ax_export and ax_base_export to be easier to read.
    
    AX-Relnotes: N/A
    Bug: b/307998342
    Change-Id: I0039540cf8fb20c3347a5e2577a591c5f940c8ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4977551
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1216102}

--- ui/accessibility/ax_base_export.h
+++ ui/accessibility/ax_base_export.h
@@ -9,24 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Defines AX_BASE_EXPORT so that functionality implemented by the
 // ui/accessibility:ax_base module can be exported to consumers.
 
-#if defined(COMPONENT_BUILD)
-#if defined(WIN32)
-
-#if defined(AX_BASE_IMPLEMENTATION)
+#if defined(COMPONENT_BUILD) && defined(WIN32) && \
+    defined(AX_BASE_IMPLEMENTATION)
 #define AX_BASE_EXPORT __declspec(dllexport)
-#else
+#elif defined(COMPONENT_BUILD) && defined(WIN32)
 #define AX_BASE_EXPORT __declspec(dllimport)
-#endif  // defined(AX_BASE_IMPLEMENTATION)
-
-#else  // defined(WIN32)
-#if defined(AX_BASE_IMPLEMENTATION)
+#elif defined(COMPONENT_BUILD) && !defined(WIN32) && \
+    defined(AX_BASE_IMPLEMENTATION)
 #define AX_BASE_EXPORT __attribute__((visibility("default")))
 #else
-#define AX_BASE_EXPORT
-#endif
-#endif
-
-#else  // defined(COMPONENT_BUILD)
 #define AX_BASE_EXPORT
 #endif
 
--- ui/accessibility/ax_export.h
+++ ui/accessibility/ax_export.h
@@ -9,24 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Defines AX_EXPORT so that functionality implemented by the
 // ui/accessibility module can be exported to consumers.
 
-#if defined(COMPONENT_BUILD)
-#if defined(WIN32)
-
-#if defined(AX_IMPLEMENTATION)
+#if defined(COMPONENT_BUILD) && defined(WIN32) && defined(AX_IMPLEMENTATION)
 #define AX_EXPORT __declspec(dllexport)
-#else
+#elif defined(COMPONENT_BUILD) && defined(WIN32)
 #define AX_EXPORT __declspec(dllimport)
-#endif  // defined(ACCESSIBILITY_IMPLEMENTATION)
-
-#else  // defined(WIN32)
-#if defined(AX_IMPLEMENTATION)
+#elif defined(COMPONENT_BUILD) && !defined(WIN32) && defined(AX_IMPLEMENTATION)
 #define AX_EXPORT __attribute__((visibility("default")))
 #else
-#define AX_EXPORT
-#endif
-#endif
-
-#else  // defined(COMPONENT_BUILD)
 #define AX_EXPORT
 #endif
 
