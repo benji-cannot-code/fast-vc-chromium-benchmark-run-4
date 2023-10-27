36b6723421f50077964caeedd95ce7e92025d9d0
Oleg Davydov <burunduk@chromium.org>
2023-10-27 08:39:29 +0000

    [file manager] Transfer dialog tests: check labels and button names
    The tests in `transfer.js`, when a confirmation dialog is involved,
    check the `.textContent` value of the whole dialog, which is
    concatenated dialog text and buttons. This prevents from switching to
    `.innerText` (see b/282177834 about why is it important), because
    concatenating for it treats whitespace differently.
    
    So, to make things simpler, this change splits the check to explicitly
    verity the dialog label and text on the dialog's buttons.
    
    Bug: b:307909989
    Change-Id: I01ea57131c676e59e0e2b814d002e59144b8362a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4978601
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Commit-Queue: Oleg Davydov <burunduk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216009}

--- ui/file_manager/integration_tests/file_manager/transfer.js
+++ ui/file_manager/integration_tests/file_manager/transfer.js
@@ -85,11 +85,19 @@ class TransferInfo {
     this.destination = opts.destination;
 
     /**
-     * The expected content of the transfer dialog (including any buttons), or
-     * undefined if no dialog is expected.
+     * The expected content of the transfer dialog, or undefined if no dialog is
+     * expected.
      * @type {string}
      */
     this.expectedDialogText = opts.expectedDialogText || undefined;
+
+    /**
+     * The expected label of the “ok” button on the dialog, if the dialog is
+     * expected.
+     * @type {string}
+     */
+    this.expectedDialogOkButtonText =
+        opts.expectedDialogOkButtonText || undefined;
 
     /**
      * True if this transfer is for a move operation, false for a copy
@@ -194,9 +202,18 @@ async function transferBetweenVolumes(transferInfo) {
 
   // If we're expecting a confirmation dialog, confirm that it is shown.
   if (transferInfo.expectedDialogText !== undefined) {
-    const {text} =
-        await remoteCall.waitForElement(appId, '.cr-dialog-container.shown');
-    chrome.test.assertEq(transferInfo.expectedDialogText, text);
+    const {innerText} = await remoteCall.waitForElement(
+        appId, '.cr-dialog-container.shown .cr-dialog-text');
+    chrome.test.assertEq(transferInfo.expectedDialogText, innerText);
+
+    // Check that the dialog contains required buttons.
+    const okButton = await remoteCall.waitForElement(
+        appId, '.cr-dialog-container.shown .cr-dialog-ok');
+    chrome.test.assertEq(
+        transferInfo.expectedDialogOkButtonText, okButton.innerText);
+    const cancelButton = await remoteCall.waitForElement(
+        appId, '.cr-dialog-container.shown .cr-dialog-cancel');
+    chrome.test.assertEq('Cancel', cancelButton.innerText);
 
     // Press OK button.
     chrome.test.assertTrue(await remoteCall.callRemoteTestUtil(
@@ -422,7 +439,8 @@ testcase.transferFromDownloadsToSharedFolder = () => {
     destination: TRANSFER_LOCATIONS.driveSharedDirectory,
     expectedDialogText:
         'Copying this item will share it with everyone who can see the ' +
-        'shared folder \'Shared\'.CopyCancel',
+        'shared folder \'Shared\'.',
+    expectedDialogOkButtonText: 'Copy',
   }));
 };
 
@@ -436,7 +454,8 @@ testcase.transferFromDownloadsToSharedFolderMove = () => {
     destination: TRANSFER_LOCATIONS.driveSharedDirectory,
     expectedDialogText:
         'Moving this item will share it with everyone who can see the ' +
-        'shared folder \'Shared\'.MoveCancel',
+        'shared folder \'Shared\'.',
+    expectedDialogOkButtonText: 'Move',
     isMove: true,
   }));
 };
@@ -495,7 +514,8 @@ testcase.transferFromDriveToTeamDrive = () => {
     destination: TRANSFER_LOCATIONS.driveTeamDriveA,
     expectedDialogText:
         'Members of \'Team Drive A\' will gain access to the copy of these ' +
-        'items.CopyCancel',
+        'items.',
+    expectedDialogOkButtonText: 'Copy',
   }));
 };
 
@@ -534,7 +554,8 @@ testcase.transferFromDownloadsToTeamDrive = () => {
     destination: TRANSFER_LOCATIONS.driveTeamDriveA,
     expectedDialogText:
         'Members of \'Team Drive A\' will gain access to the copy of these ' +
-        'items.CopyCancel',
+        'items.',
+    expectedDialogOkButtonText: 'Copy',
   }));
 };
 
@@ -548,7 +569,8 @@ testcase.transferBetweenTeamDrives = () => {
     destination: TRANSFER_LOCATIONS.driveTeamDriveA,
     expectedDialogText:
         'Members of \'Team Drive A\' will gain access to the copy of these ' +
-        'items.CopyCancel',
+        'items.',
+    expectedDialogOkButtonText: 'Copy',
   }));
 };
 
