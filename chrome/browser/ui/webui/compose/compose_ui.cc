2a30e6ea43d31f1845f03018172c467898ecc5c5
John Lee <johntlee@chromium.org>
2023-10-27 07:11:24 +0000

    Allow editing submitted input for Compose
    The #editContainer takes over the full dialog's existing height, sort
    of like a dialog over the existing submitted input and results.
    
    http://screencast/cast/NTg2MTQ3NjY2MDE1MDI3Mnw2MTZkOTI2ZS01Nw
    
    Bug: b:305099595
    Change-Id: I1847922a331167c18d61a7d7f77fe48a0b1d4e96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4977705
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215981}

--- chrome/browser/ui/webui/compose/compose_ui.cc
+++ chrome/browser/ui/webui/compose/compose_ui.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/compose_resources.h"
 #include "chrome/grit/compose_resources_map.h"
+#include "chrome/grit/generated_resources.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
@@ -57,6 +58,9 @@ ComposeUI::ComposeUI(content::WebUI* web_ui)
       {"errorRequestNotSuccessful", IDS_COMPOSE_ERROR_REQUEST_NOT_SUCCESSFUL},
       {"errorPermissionDenied", IDS_COMPOSE_ERROR_REQUEST_NOT_SUCCESSFUL},
       {"errorGeneric", IDS_COMPOSE_ERROR_GENERIC},
+      {"editButton", IDS_EDIT},
+      {"editCancelButton", IDS_CANCEL},
+      {"editUpdateButton", IDS_COMPOSE_EDIT_UPDATE_BUTTON},
   };
   source->AddLocalizedStrings(kStrings);
   source->AddString(
