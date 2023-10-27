d29ebbbbcde82957434c07b800f33f5e6b8cc022
Adam Rice <ricea@chromium.org>
2023-10-27 15:28:24 +0000

    Remove SpdyHeadersToHttpResponseVerifyCorrectness feature
    Remove the base::Feature SpdyHeadersToHttpResponseVerifyCorrectness, its
    implementation and tests. The experiment is over and it is no longer
    needed.
    
    Also remove an unused #include of "load_flags.h".
    
    BUG=1485670
    
    Change-Id: If42fb78558e9afeca86bc79dc1c017e87bc5db97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4975162
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216142}

--- net/spdy/spdy_http_utils_unittest.cc
+++ net/spdy/spdy_http_utils_unittest.cc
@@ -137,7 +137,6 @@ constexpr auto ToSimpleString = test::HttpResponseHeadersToSimpleString;
 
 enum class SpdyHeadersToHttpResponseHeadersFeatureConfig {
   kUseRawString,
-  kVerify,
   kUseBuilder
 };
 
@@ -146,9 +145,6 @@ std::string PrintToString(
   switch (config) {
     case SpdyHeadersToHttpResponseHeadersFeatureConfig::kUseRawString:
       return "RawString";
-
-    case SpdyHeadersToHttpResponseHeadersFeatureConfig::kVerify:
-      return "Verify";
 
     case SpdyHeadersToHttpResponseHeadersFeatureConfig::kUseBuilder:
       return "UseBuilder";
@@ -163,20 +159,12 @@ class SpdyHeadersToHttpResponseTest
     switch (GetParam()) {
       case SpdyHeadersToHttpResponseHeadersFeatureConfig::kUseRawString:
         feature_list_.InitWithFeatures(
-            {}, {features::kSpdyHeadersToHttpResponseUseBuilder,
-                 features::kSpdyHeadersToHttpResponseVerifyCorrectness});
-        break;
-
-      case SpdyHeadersToHttpResponseHeadersFeatureConfig::kVerify:
-        feature_list_.InitWithFeatures(
-            {features::kSpdyHeadersToHttpResponseVerifyCorrectness},
-            {features::kSpdyHeadersToHttpResponseUseBuilder});
+            {}, {features::kSpdyHeadersToHttpResponseUseBuilder});
         break;
 
       case SpdyHeadersToHttpResponseHeadersFeatureConfig::kUseBuilder:
         feature_list_.InitWithFeatures(
-            {features::kSpdyHeadersToHttpResponseUseBuilder},
-            {features::kSpdyHeadersToHttpResponseVerifyCorrectness});
+            {features::kSpdyHeadersToHttpResponseUseBuilder}, {});
         break;
     }
   }
@@ -220,7 +208,6 @@ INSTANTIATE_TEST_SUITE_P(
     SpdyHttpUtils,
     SpdyHeadersToHttpResponseTest,
     Values(SpdyHeadersToHttpResponseHeadersFeatureConfig::kUseRawString,
-           SpdyHeadersToHttpResponseHeadersFeatureConfig::kVerify,
            SpdyHeadersToHttpResponseHeadersFeatureConfig::kUseBuilder),
     ::testing::PrintToStringParamName());
 
