8c5138aaeb04f9f2af0c1d501853b0f870042c65
Patrick Noland <pnoland@google.com>
2023-10-27 15:47:20 +0000

    [Omnibox] Inset divider clip rect by view margins
    Bug: None
    Change-Id: I7d3a1fae1915b84556c83369c686fdb0ab2b62b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4979378
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1216160}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/SuggestionHorizontalDivider.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/SuggestionHorizontalDivider.java
@@ -15,6 +15,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.RecyclerView.ItemDecoration;
+import androidx.recyclerview.widget.RecyclerView.LayoutParams;
 import androidx.recyclerview.widget.RecyclerView.State;
 
 import org.chromium.chrome.browser.omnibox.R;
@@ -42,10 +43,11 @@ public class SuggestionHorizontalDivider extends ItemDecoration {
             View child = parent.getChildAt(i);
             if (!shouldDrawDivider(child, parent)) continue;
             parent.getDecoratedBoundsWithMargins(child, mBounds);
+            LayoutParams lp = (LayoutParams) child.getLayoutParams();
             canvas.clipRect(
-                    mBounds.left,
+                    mBounds.left + lp.leftMargin,
                     mBounds.bottom - mHeight,
-                    mBounds.right,
+                    mBounds.right - lp.rightMargin,
                     mBounds.bottom,
                     Op.DIFFERENCE);
         }
--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/SuggestionHorizontalDividerTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/SuggestionHorizontalDividerTest.java
@@ -15,6 +15,7 @@ import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.graphics.Region.Op;
 import android.view.View;
+import android.view.ViewGroup.MarginLayoutParams;
 
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.filters.SmallTest;
@@ -89,6 +90,12 @@ public class SuggestionHorizontalDividerTest {
     @Test
     @SmallTest
     public void testDraw() {
+        MarginLayoutParams layoutParams = new MarginLayoutParams(100, 30);
+        layoutParams.leftMargin = 13;
+        layoutParams.rightMargin = 17;
+        doReturn(new RecyclerView.LayoutParams(layoutParams))
+                .when(mChildViewWithDivider)
+                .getLayoutParams();
         doAnswer(
                         (invocation -> {
                             ((Rect) invocation.getArgument(1)).set(0, 0, 100, 30);
@@ -98,6 +105,6 @@ public class SuggestionHorizontalDividerTest {
                 .getDecoratedBoundsWithMargins(any(View.class), any(Rect.class));
 
         mDecoration.onDraw(mCanvas, mRecyclerView, mState);
-        verify(mCanvas).clipRect(0, 29, 100, 30, Op.DIFFERENCE);
+        verify(mCanvas).clipRect(13, 29, 100 - 17, 30, Op.DIFFERENCE);
     }
 }
