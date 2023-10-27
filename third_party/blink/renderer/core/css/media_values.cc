5be94ff85db225b13d9073250710010aba500c7e
Rune Lillesveen <futhark@chromium.org>
2023-10-27 11:06:17 +0000

    Get viewport styles for MediaValues from LayoutView
    Part of moving GetComputedStyle() from Node to Element.
    
    Node::GetComputedStyle will look up LayoutView for Document. Instead,
    look up the styles from the LayoutView from the Frame directly.
    
    Bug: 1496510
    Change-Id: I14b5cad458628984c9837f034801692260ddb561
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4981909
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216054}

--- third_party/blink/renderer/core/css/media_values.cc
+++ third_party/blink/renderer/core/css/media_values.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
 #include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/layout/adjust_for_absolute_zoom.h"
+#include "third_party/blink/renderer/core/layout/layout_view.h"
 #include "third_party/blink/renderer/core/media_type_names.h"
 #include "third_party/blink/renderer/core/page/chrome_client.h"
 #include "third_party/blink/renderer/core/page/page.h"
@@ -225,57 +226,50 @@ bool MediaValues::CalculateInvertedColors(LocalFrame* frame) {
 }
 
 float MediaValues::CalculateEmSize(LocalFrame* frame) {
-  DCHECK(frame);
-  DCHECK(frame->GetDocument());
-  const ComputedStyle* style = frame->GetDocument()->GetComputedStyle();
-  DCHECK(style);
-  return CSSToLengthConversionData::FontSizes(style->GetFontSizeStyle(), style)
+  CHECK(frame);
+  CHECK(frame->ContentLayoutObject());
+  const ComputedStyle& style = frame->ContentLayoutObject()->StyleRef();
+  return CSSToLengthConversionData::FontSizes(style.GetFontSizeStyle(), &style)
       .Em(/* zoom */ 1.0f);
 }
 
 float MediaValues::CalculateExSize(LocalFrame* frame) {
-  DCHECK(frame);
-  DCHECK(frame->GetDocument());
-  const ComputedStyle* style = frame->GetDocument()->GetComputedStyle();
-  DCHECK(style);
-  return CSSToLengthConversionData::FontSizes(style->GetFontSizeStyle(), style)
+  CHECK(frame);
+  CHECK(frame->ContentLayoutObject());
+  const ComputedStyle& style = frame->ContentLayoutObject()->StyleRef();
+  return CSSToLengthConversionData::FontSizes(style.GetFontSizeStyle(), &style)
       .Ex(/* zoom */ 1.0f);
 }
 
 float MediaValues::CalculateChSize(LocalFrame* frame) {
-  DCHECK(frame);
-  DCHECK(frame->GetDocument());
-  const ComputedStyle* style = frame->GetDocument()->GetComputedStyle();
-  DCHECK(style);
-  return CSSToLengthConversionData::FontSizes(style->GetFontSizeStyle(), style)
+  CHECK(frame);
+  CHECK(frame->ContentLayoutObject());
+  const ComputedStyle& style = frame->ContentLayoutObject()->StyleRef();
+  return CSSToLengthConversionData::FontSizes(style.GetFontSizeStyle(), &style)
       .Ch(/* zoom */ 1.0f);
 }
 
 float MediaValues::CalculateIcSize(LocalFrame* frame) {
-  DCHECK(frame);
-  DCHECK(frame->GetDocument());
-  const ComputedStyle* style = frame->GetDocument()->GetComputedStyle();
-  DCHECK(style);
-  return CSSToLengthConversionData::FontSizes(style->GetFontSizeStyle(), style)
+  CHECK(frame);
+  CHECK(frame->ContentLayoutObject());
+  const ComputedStyle& style = frame->ContentLayoutObject()->StyleRef();
+  return CSSToLengthConversionData::FontSizes(style.GetFontSizeStyle(), &style)
       .Ic(/* zoom */ 1.0f);
 }
 
 float MediaValues::CalculateCapSize(LocalFrame* frame) {
-  DCHECK(frame);
-  DCHECK(frame->GetDocument());
-  const ComputedStyle* style = frame->GetDocument()->GetComputedStyle();
-  DCHECK(style);
-  return CSSToLengthConversionData::FontSizes(style->GetFontSizeStyle(), style)
+  CHECK(frame);
+  CHECK(frame->ContentLayoutObject());
+  const ComputedStyle& style = frame->ContentLayoutObject()->StyleRef();
+  return CSSToLengthConversionData::FontSizes(style.GetFontSizeStyle(), &style)
       .Cap(/* zoom */ 1.0f);
 }
 
 float MediaValues::CalculateLineHeight(LocalFrame* frame) {
-  DCHECK(frame);
-  DCHECK(frame->GetDocument());
-  const ComputedStyle* style = frame->GetDocument()->GetComputedStyle();
-  DCHECK(style);
-  return AdjustForAbsoluteZoom::AdjustFloat(style->ComputedLineHeight(),
-                                            *style);
+  CHECK(frame);
+  CHECK(frame->ContentLayoutObject());
+  const ComputedStyle& style = frame->ContentLayoutObject()->StyleRef();
+  return AdjustForAbsoluteZoom::AdjustFloat(style.ComputedLineHeight(), style);
 }
 
 const String MediaValues::CalculateMediaType(LocalFrame* frame) {
