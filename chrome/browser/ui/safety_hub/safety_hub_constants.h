e38a7bf6c2b63a9a191663ae951529be145643b9
Tom Van Goethem <tov@chromium.org>
2023-10-27 17:51:01 +0000

    [SHv2] Add notification trigger logic for SB
    This CL adds the special trigger logic for Safe Browsing. More
    specifically, a notification should only be shown three times in the
    entire lifetime unless the user change the Safe Browsing settings.
    Additionally, the notification will only be shown after three days after
    the user changed the setting.
    
    Bug: 1443466
    Change-Id: I32996d335dc0e8277f03f67fc466e9730d4d869d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4974618
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Commit-Queue: Tom Van Goethem <tov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216249}

--- chrome/browser/ui/safety_hub/safety_hub_constants.h
+++ chrome/browser/ui/safety_hub/safety_hub_constants.h
@@ -10,10 +10,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace safety_hub {
 
+// Keys used to indicate the labels that should be shown in various UI items.
 extern const char kCardHeaderKey[];
 extern const char kCardSubheaderKey[];
 extern const char kCardStateKey[];
+
+// Key used for the dict of the Safe Browsing result.
 extern const char kSafetyHubSafeBrowsingStatusKey[];
+
+// Keys used for the dict stored in prefs for the menu notification.
+extern const char kSafetyHubMenuNotificationActiveKey[];
+extern const char kSafetyHubMenuNotificationAllTimeCountKey[];
+extern const char kSafetyHubMenuNotificationImpressionCountKey[];
+extern const char kSafetyHubMenuNotificationFirstImpressionKey[];
+extern const char kSafetyHubMenuNotificationLastImpressionKey[];
+extern const char kSafetyHubMenuNotificationShowAfterTimeKey[];
+extern const char kSafetyHubMenuNotificationResultKey[];
 
 // State that a top card in the SafetyHub page can be in.
 // Should be kept in sync with the corresponding enum in
