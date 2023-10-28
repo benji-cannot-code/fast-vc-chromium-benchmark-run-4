db28ad12607549aac5446be2b860713cfc4a918d
dpapad <dpapad@chromium.org>
2023-10-28 01:12:18 +0000

    WebUI: Allow mapping entire URLs and not just prefixes in bundle_js().
    This is going to be used to tell bundle_js() how to find
    chrome://resources/js/jstemplate_compiled.js to bundle it in components/neterror/resources/neterror.js so that it does not
    have to be injected from C++.
    
    Bug: 1173575
    Change-Id: I490a331413b6f75f07401e0f9383237c640dc569
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4985146
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216491}

--- ui/webui/resources/tools/tests/bundle_js/external/baz/baz.js
+++ ui/webui/resources/tools/tests/bundle_js/external/baz/baz.js
@@ -3,8 +3,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'chrome://resources/foo_resource.js';
-import 'some-fake-scheme://foo/foo.js';
-import 'some-fake-scheme://foo/baz.js';
-
-alert('Hello from src/foo.js');
+alert('Hello from external/baz/baz.js');
