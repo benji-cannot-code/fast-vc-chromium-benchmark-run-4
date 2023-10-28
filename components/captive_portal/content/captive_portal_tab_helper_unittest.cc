546505ad9bc19c415537046714ba20dfbb30cde6
punithnayak <npunith125@gmail.com>
2023-10-28 01:51:32 +0000

    Dangling pointer from CaptivePortalTabHelperTest
    Change-Id: Iae978cab56290eed1ea5ea0e1c65790013592e1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4964677
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Punith Nayak <npunith125@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1216508}

--- components/captive_portal/content/captive_portal_tab_helper_unittest.cc
+++ components/captive_portal/content/captive_portal_tab_helper_unittest.cc
@@ -87,6 +87,7 @@ class CaptivePortalTabHelperTest : public content::RenderViewHostTestHarness {
   }
 
   void TearDown() override {
+    mock_reloader_ = nullptr;
     tab_helper_.reset();
     captive_portal_service_.reset();
     content::RenderViewHostTestHarness::TearDown();
@@ -194,8 +195,7 @@ class CaptivePortalTabHelperTest : public content::RenderViewHostTestHarness {
   std::unique_ptr<CaptivePortalTabHelper> tab_helper_;
 
   // Owned by |tab_helper_|.
-  raw_ptr<testing::StrictMock<MockCaptivePortalTabReloader>, DanglingUntriaged>
-      mock_reloader_;
+  raw_ptr<testing::StrictMock<MockCaptivePortalTabReloader>> mock_reloader_;
 };
 
 TEST_F(CaptivePortalTabHelperTest, HttpSuccess) {
