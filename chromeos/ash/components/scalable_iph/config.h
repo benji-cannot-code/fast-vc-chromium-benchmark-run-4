517f9c729640865a71c81577dd136c87be12d1b4
Yuki Awano <yawano@google.com>
2023-10-28 01:32:56 +0000

    Add help app based config
    Bug: b:308010596
    Test: na
    Change-Id: I5c1c3a03fbb030db3c7387053ac068b199a2637b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4984690
    Reviewed-by: Angela Xiao <angelaxiao@chromium.org>
    Commit-Queue: Yuki Awano <yawano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1216499}

--- chromeos/ash/components/scalable_iph/config.h
+++ chromeos/ash/components/scalable_iph/config.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMEOS_ASH_COMPONENTS_SCALABLE_IPH_CONFIG_H_
 #define CHROMEOS_ASH_COMPONENTS_SCALABLE_IPH_CONFIG_H_
 
+#include "base/containers/flat_map.h"
+#include "chromeos/ash/components/scalable_iph/scalable_iph_constants.h"
 #include "chromeos/ash/components/scalable_iph/scalable_iph_delegate.h"
 
 namespace scalable_iph {
@@ -13,10 +15,14 @@ namespace scalable_iph {
 struct Config {
   Config();
   ~Config();
-  int version_number;
+  int version_number = kCurrentVersionNumber;
   UiType ui_type;
   std::unique_ptr<ScalableIphDelegate::NotificationParams> notification_params;
   std::unique_ptr<ScalableIphDelegate::BubbleParams> bubble_params;
+
+  // While you can put UI related config or any other config in `params`, usage
+  // of `params` should be limited to custom conditions.
+  base::flat_map<std::string, std::string> params;
 };
 
 std::unique_ptr<Config> GetConfig(const base::Feature& feature);
