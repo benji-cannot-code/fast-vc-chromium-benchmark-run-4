641f612c5abb1a676529d1923bb4cf45040a152c
Ho Cheung <uioptt24@gmail.com>
2023-10-28 00:37:03 +0000

    [py3.12] [tools] Remove imp usage
    And use its equivalent instead.
    
    Bug: 1487454
    Change-Id: Ie78c266106bd1f9a29fd8bd3394f233e90d330e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4983816
    Commit-Queue: Ho Cheung <uioptt24@gmail.com>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1216473}

--- tools/sublime/ninja_options_script.py
+++ tools/sublime/ninja_options_script.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright 2016 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 from __future__ import print_function
 
-import imp
+import importlib.util
 import optparse
 import os
 import shlex
@@ -25,7 +25,10 @@ import shlex
 ycm_module_path = os.path.normpath(
     os.path.join(os.path.dirname(os.path.abspath(__file__)),
     '../vim/chromium.ycm_extra_conf.py'))
-ycm_extra_conf = imp.load_source('ycm_extra_conf', ycm_module_path)
+spec = importlib.util.spec_from_file_location("ycm_extra_conf", ycm_module_path)
+ycm_extra_conf = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(ycm_extra_conf)
+
 
 def main():
   usage = "usage: %prog [options] file"
