eb4dbd52f66382bab0baba0a9fdb31c9b58ea868
Devlin Cronin <rdevlin.cronin@chromium.org>
2023-10-28 00:12:34 +0000

    [Extensions] Enable kRestrictDeveloperModeAPIs feature
    Enable the feature that allows restricting certain APIs to users in
    developer mode. This is currently only set to apply to the new
    `userScripts` API, so should have no impact on any existing extensions.
    
    Bug: 1267489
    Change-Id: Ide407669539b966695d01d35eb11851d9149376d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4985443
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216462}

--- chrome/browser/extensions/api/scripting/scripting_apitest.cc
+++ chrome/browser/extensions/api/scripting/scripting_apitest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_tab_util.h"
+#include "chrome/browser/extensions/extension_util.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/test/base/ui_test_utils.h"
@@ -725,6 +726,12 @@ class ScriptingAndUserScriptsAPITest : public ScriptingAPITest {
   ScriptingAndUserScriptsAPITest& operator=(
       const ScriptingAndUserScriptsAPITest&) = delete;
   ~ScriptingAndUserScriptsAPITest() override = default;
+
+  void SetUpOnMainThread() override {
+    ScriptingAPITest::SetUpOnMainThread();
+    // The userScripts API is only available to users in developer mode.
+    util::SetDeveloperModeForProfile(profile(), true);
+  }
 
  private:
   // The userScripts API is currently behind a channel and feature restriction.
