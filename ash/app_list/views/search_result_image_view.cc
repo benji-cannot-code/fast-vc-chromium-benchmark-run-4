e5dcc9d2c27ea8e268a305133942900a61a4a775
Wen-Chien Wang <wcwang@chromium.org>
2023-10-28 00:46:31 +0000

    [image search] Fix the disappearing focus traversal in launcher search
    When a image search result is shown in the launcher, the focus
    disappears if we move it down from the close button. This is because
    the ImagePreviewView is a image button and is a candidate to be
    focused. Fix that by setting the focus behavior to accessible only.
    
    Bug: 1352636
    Change-Id: I3deb5d47606f0d8bd34447674a3341cbe03b8212
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4985375
    Commit-Queue: Wen-Chien Wang <wcwang@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216479}

--- ash/app_list/views/search_result_image_view.cc
+++ ash/app_list/views/search_result_image_view.cc
@@ -50,7 +50,11 @@ constexpr double kDraggedImageOpacity = 0.6;
 
 class ImagePreviewView : public views::ImageButton {
  public:
-  ImagePreviewView() { SetInstallFocusRingOnFocus(false); }
+  METADATA_HEADER(ImagePreviewView);
+  ImagePreviewView() {
+    SetInstallFocusRingOnFocus(false);
+    SetFocusBehavior(FocusBehavior::ACCESSIBLE_ONLY);
+  }
   ImagePreviewView(const ImagePreviewView&) = delete;
   ImagePreviewView& operator=(const ImagePreviewView&) = delete;
   ~ImagePreviewView() override = default;
@@ -70,6 +74,9 @@ class ImagePreviewView : public views::ImageButton {
     views::FocusRing::Get(parent())->SchedulePaint();
   }
 };
+
+BEGIN_METADATA(ImagePreviewView, views::ImageButton)
+END_METADATA
 
 }  // namespace
 
