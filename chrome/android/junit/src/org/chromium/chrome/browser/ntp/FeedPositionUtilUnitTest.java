d80459b1ed34d499e95cdb6d51f1d2836c781b2d
Hailey Wang <haileywang@google.com>
2023-10-28 00:43:28 +0000

    [FIXIT] Migrate feed user model to use binned classifier
    Bug: b/299529649
    Change-Id: I16b9546714c08638a589e9c594e1b16937cad078
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4877444
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Auto-Submit: Hailey Wang <haileywang@google.com>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216478}

--- chrome/android/junit/src/org/chromium/chrome/browser/ntp/FeedPositionUtilUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/ntp/FeedPositionUtilUnitTest.java
@@ -32,9 +32,9 @@ import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.segmentation_platform.SegmentationPlatformServiceFactory;
 import org.chromium.chrome.test.util.browser.Features;
-import org.chromium.components.segmentation_platform.SegmentSelectionResult;
+import org.chromium.components.segmentation_platform.ClassificationResult;
 import org.chromium.components.segmentation_platform.SegmentationPlatformService;
-import org.chromium.components.segmentation_platform.proto.SegmentationProto.SegmentId;
+import org.chromium.components.segmentation_platform.prediction_status.PredictionStatus;
 
 /** Unit tests for {@link FeedPositionUtils} class. */
 @RunWith(BaseRobolectricTestRunner.class)
@@ -52,7 +52,7 @@ public class FeedPositionUtilUnitTest {
         MockitoAnnotations.initMocks(this);
         Profile.setLastUsedProfileForTesting(mProfile);
         SegmentationPlatformServiceFactory.setForTests(mSegmentationPlatformService);
-        setSegmentationResult(new SegmentSelectionResult(false, null));
+        setClassificationResult(new ClassificationResult(PredictionStatus.NOT_READY, null));
     }
 
     @After
@@ -60,15 +60,15 @@ public class FeedPositionUtilUnitTest {
         FeatureList.setTestValues(null);
     }
 
-    private void setSegmentationResult(SegmentSelectionResult segmentSelectionResult) {
+    private void setClassificationResult(ClassificationResult classificationResult) {
         Mockito.doAnswer(
                         invocation -> {
-                            Callback<SegmentSelectionResult> callback = invocation.getArgument(1);
-                            callback.onResult(segmentSelectionResult);
+                            Callback<ClassificationResult> callback = invocation.getArgument(3);
+                            callback.onResult(classificationResult);
                             return null;
                         })
                 .when(mSegmentationPlatformService)
-                .getSelectedSegment(eq(FEED_USER_SEGMENT_KEY), any());
+                .getClassificationResult(eq(FEED_USER_SEGMENT_KEY), any(), any(), any());
 
         FeedPositionUtils.cacheSegmentationResult();
     }
@@ -82,9 +82,8 @@ public class FeedPositionUtilUnitTest {
         setFeedPositionFlags(FeedPositionUtils.PUSH_DOWN_FEED_SMALL, "active");
         Assert.assertFalse(FeedPositionUtils.isFeedPushDownSmallEnabled());
 
-        setSegmentationResult(
-                new SegmentSelectionResult(
-                        true, SegmentId.OPTIMIZATION_TARGET_SEGMENTATION_FEED_USER));
+        setClassificationResult(
+                new ClassificationResult(PredictionStatus.SUCCEEDED, new String[] {"FeedUser"}));
         Assert.assertTrue(FeedPositionUtils.isFeedPushDownSmallEnabled());
     }
 
@@ -97,9 +96,8 @@ public class FeedPositionUtilUnitTest {
         setFeedPositionFlags(FeedPositionUtils.PUSH_DOWN_FEED_SMALL, "active");
         Assert.assertFalse(FeedPositionUtils.isFeedPushDownLargeEnabled());
 
-        setSegmentationResult(
-                new SegmentSelectionResult(
-                        true, SegmentId.OPTIMIZATION_TARGET_SEGMENTATION_FEED_USER));
+        setClassificationResult(
+                new ClassificationResult(PredictionStatus.SUCCEEDED, new String[] {"FeedUser"}));
         Assert.assertTrue(FeedPositionUtils.isFeedPushDownLargeEnabled());
     }
 
@@ -112,13 +110,12 @@ public class FeedPositionUtilUnitTest {
         setFeedPositionFlags(FeedPositionUtils.PULL_UP_FEED, "non-active");
         Assert.assertFalse(FeedPositionUtils.isFeedPullUpEnabled());
 
-        setSegmentationResult(
-                new SegmentSelectionResult(
-                        true, SegmentId.OPTIMIZATION_TARGET_SEGMENTATION_FEED_USER));
+        setClassificationResult(
+                new ClassificationResult(PredictionStatus.SUCCEEDED, new String[] {"FeedUser"}));
         Assert.assertFalse(FeedPositionUtils.isFeedPullUpEnabled());
 
-        setSegmentationResult(
-                new SegmentSelectionResult(true, SegmentId.OPTIMIZATION_TARGET_UNKNOWN));
+        setClassificationResult(
+                new ClassificationResult(PredictionStatus.SUCCEEDED, new String[] {"Other"}));
         Assert.assertTrue(FeedPositionUtils.isFeedPullUpEnabled());
     }
 
--- components/segmentation_platform/embedder/default_model/feed_user_segment_unittest.cc
+++ components/segmentation_platform/embedder/default_model/feed_user_segment_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/segmentation_platform/embedder/default_model/feed_user_segment.h"
 
 #include "components/segmentation_platform/embedder/default_model/default_model_test_base.h"
+#include "components/segmentation_platform/public/constants.h"
 
 namespace segmentation_platform {
 
@@ -24,23 +25,13 @@ TEST_F(FeedUserModelTest, ExecuteModelWithInput) {
   ExpectInitAndFetchModel();
   ASSERT_TRUE(fetched_metadata_);
 
-  std::string subsegment_key = GetSubsegmentKey(kFeedUserSegmentationKey);
   ModelProvider::Request input(11, 0);
-  ExecuteWithInputAndCheckSubsegmentName<FeedUserSegment>(
-      input, subsegment_key, /*sub_segment_name=*/"NoNTPOrHomeOpened");
-
-  input[1] = 3;
-  input[2] = 2;
-  ExecuteWithInputAndCheckSubsegmentName<FeedUserSegment>(
-      input, subsegment_key, /*sub_segment_name=*/"UsedNtpWithoutModules");
-
-  input[0] = 3;
-  ExecuteWithInputAndCheckSubsegmentName<FeedUserSegment>(
-      input, subsegment_key, /*sub_segment_name=*/"MvtOnly");
+  ExpectClassifierResults(input, {kLegacyNegativeLabel});
 
   input[8] = 3;
-  ExecuteWithInputAndCheckSubsegmentName<FeedUserSegment>(
-      input, subsegment_key, /*sub_segment_name=*/"NtpAndFeedEngagedSimple");
+  ExpectClassifierResults(
+      input, {SegmentIdToHistogramVariant(
+                 SegmentId::OPTIMIZATION_TARGET_SEGMENTATION_FEED_USER)});
 
   EXPECT_FALSE(ExecuteWithInput(/*inputs=*/{}));
   EXPECT_FALSE(ExecuteWithInput(/*inputs=*/{1, 2}));
