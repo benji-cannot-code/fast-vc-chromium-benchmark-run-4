b506413b169911ca0f87360a254bdc2ca8edb716
Sunggook Chue <sunggch@microsoft.com>
2023-10-29 01:25:50 +0000

    Add verbose level logging for mp4 muxer.
    Adds verboselevel=1 logging with captured video
    and audio frame information, and created mp4 blob
    info such as timescale, duration, fragment counts.
    
    Bug: 1497009
    Change-Id: Ie786bc8a71597aead9bf913bb89d301bb6fe3d5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4986221
    Commit-Queue: Sunggook Chue <sunggch@microsoft.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216623}

--- media/muxers/mp4_muxer.cc
+++ media/muxers/mp4_muxer.cc
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <memory>
 
+#include "base/logging.h"
 #include "base/time/time.h"
-
 namespace media {
 
 namespace {
@@ -30,6 +30,9 @@ Mp4Muxer::Mp4Muxer(AudioCodec audio_codec,
       has_audio_(has_audio) {
   CHECK(has_video_ || has_audio_);
   CHECK(!has_audio || audio_codec == AudioCodec::kAAC);
+
+  DVLOG(1) << __func__ << ", Max output interval in seconds: "
+           << max_data_output_interval_.InSeconds();
 
   // Creation can be done on a different sequence than main activities.
   DETACH_FROM_SEQUENCE(sequence_checker_);
@@ -95,8 +98,10 @@ void Mp4Muxer::MaybeForceFlush() {
 
 bool Mp4Muxer::Flush() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DVLOG(1) << __func__ << ", Flush called ";
 
   if (!mp4_muxer_delegate_->Flush()) {
+    DVLOG(1) << __func__ << ", Flush failed ";
     return false;
   }
 
