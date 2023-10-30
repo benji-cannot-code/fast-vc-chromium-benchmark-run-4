5edd28b19cd2c9d90e7a05535382c0edd6d71526
Ian Clelland <iclelland@chromium.org>
2023-10-30 18:10:58 +0000

    Ensure that permissions policy report_to param is a token.
    Bug: 1492310
    Change-Id: Ideb20dc06ec2507a4f64f5dd09e9855d406cb4a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4986133
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Auto-Submit: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217062}

--- third_party/blink/renderer/core/permissions_policy/permissions_policy_parser.cc
+++ third_party/blink/renderer/core/permissions_policy/permissions_policy_parser.cc
@@ -500,7 +500,7 @@ PermissionsPolicyParser::Node ParsingContext::ParsePermissionsPolicyToIR(
 
     if (!value.params.empty()) {
       for (const auto& param : value.params) {
-        if (param.first == "report-to") {
+        if (param.first == "report-to" && param.second.is_token()) {
           endpoint = String(param.second.GetString());
         }
       }
