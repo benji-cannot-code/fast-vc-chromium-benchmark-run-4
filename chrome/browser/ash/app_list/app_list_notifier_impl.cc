144c2d18c4610bfdaaacb45f83cd47b5a5493771
Lauren Commeignes <laurencom@chromium.org>
2023-10-30 00:53:08 +0000

    Update metric logic in app list to only start session after typing
    Prevent logging of a session ending if no session has been started.
    
    Test: "passes unittests. Works on DUT."
    Bug: b/307812616
    Change-Id: Ica4a3ca6744dfcc5e7d38730b7934b81e8ab70d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4975290
    Commit-Queue: Lauren Commeignes <laurencom@chromium.org>
    Reviewed-by: Dmitry Grebenyuk <dgrebenyuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1216732}

--- chrome/browser/ash/app_list/app_list_notifier_impl.cc
+++ chrome/browser/ash/app_list/app_list_notifier_impl.cc
@@ -156,9 +156,11 @@ void AppListNotifierImpl::OnAppListVisibilityWillChange(bool shown,
       DoStateTransition(Location::kRecentApps, State::kShown);
     // kList is not shown until a search query is entered.
   } else {
-    search_session_in_progress_ = false;
-    for (auto& observer : observers_) {
-      observer.OnSearchSessionEnded(query_);
+    if (search_session_in_progress_) {
+      search_session_in_progress_ = false;
+      for (auto& observer : observers_) {
+        observer.OnSearchSessionEnded(query_);
+      }
     }
 
     DoStateTransition(Location::kList, State::kNone);
@@ -169,7 +171,7 @@ void AppListNotifierImpl::OnAppListVisibilityWillChange(bool shown,
 }
 
 void AppListNotifierImpl::OnViewStateChanged(ash::AppListViewState state) {
-  if (state == ash::AppListViewState::kFullscreenSearch) {
+  if (state == ash::AppListViewState::kFullscreenSearch && !query_.empty()) {
     search_session_in_progress_ = true;
     for (auto& observer : observers_) {
       observer.OnSearchSessionStarted();
