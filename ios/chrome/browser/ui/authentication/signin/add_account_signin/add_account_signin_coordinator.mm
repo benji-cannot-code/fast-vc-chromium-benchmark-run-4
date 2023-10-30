56fc5b68985a1b5f57e6489d4cc79a2997678a5f
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-10-30 11:17:02 +0000

    [ios] Guard history opt-in for restore shorty/re-signin by new flag
    Besides being guarded by kReplaceSyncPromosWithSignInPromos, the logic
    is now also guarded by a new kHistoryOptInForRestoreShortyAndReSignin
    flag, exposed in chrome://flags.
    
    Bug: 1450874
    Low-Coverage-Reason: The lack of test coverage is a pre-existing issue tracked under https://crbug.com/1479572
    Change-Id: I1110deeb8597105376780646903a76b962695ce1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4984027
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Jood Hajeer <jood@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1216866}

--- ios/chrome/browser/ui/authentication/signin/add_account_signin/add_account_signin_coordinator.mm
+++ ios/chrome/browser/ui/authentication/signin/add_account_signin/add_account_signin_coordinator.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/signin/authentication_service.h"
 #import "ios/chrome/browser/signin/authentication_service_factory.h"
 #import "ios/chrome/browser/signin/chrome_account_manager_service.h"
@@ -41,8 +42,8 @@ using signin_metrics::PromoAction;
 // Coordinator to handle additional steps after the identity is added, i.e.
 // after `addAccountSigninManager` does its job.
 @property(nonatomic, strong) SigninCoordinator* postSigninManagerCoordinator;
-// Coordinator for history sync opt-in, if kReplaceSyncPromosWithSignInPromos is
-// enabled.
+// Coordinator for history sync opt-in, if kReplaceSyncPromosWithSignInPromos
+// and kHistoryOptInForRestoreShortyAndReSignin are enabled.
 @property(nonatomic, strong)
     HistorySyncPopupCoordinator* historySyncPopupCoordinator;
 // Manager that handles sign-in add account UI.
@@ -311,9 +312,12 @@ using signin_metrics::PromoAction;
   [self.postSigninManagerCoordinator stop];
   self.postSigninManagerCoordinator = nil;
 
+  const bool history_opt_in_flags_enabled =
+      base::FeatureList::IsEnabled(
+          syncer::kReplaceSyncPromosWithSignInPromos) &&
+      base::FeatureList::IsEnabled(kHistoryOptInForRestoreShortyAndReSignin);
   if (result != SigninCoordinatorResultSuccess ||
-      !base::FeatureList::IsEnabled(
-          syncer::kReplaceSyncPromosWithSignInPromos)) {
+      !history_opt_in_flags_enabled) {
     [self addAccountDoneWithSigninResult:result identity:info.identity];
     return;
   }
