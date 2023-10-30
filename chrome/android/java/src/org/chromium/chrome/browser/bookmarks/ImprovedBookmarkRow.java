cf601f81a38d117f2764a99fa49ed7d968f6a1da
Brandon Wylie <wylieb@chromium.org>
2023-10-30 20:21:52 +0000

    [bookmarks] Handle out-of-sequence view binds for menu/drawable
    The sequence of calls for ImprovedBookmarkRowViewBinder matters for it
    to work correctly. Rewiring the code so that order doesn't matter.
    
    Bug: 1496204
    Change-Id: I46abbec92c6dfeaf2c9ce6cd52a9a32e54d5d5fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4979954
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217166}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/ImprovedBookmarkRow.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/ImprovedBookmarkRow.java
@@ -58,6 +58,7 @@ public class ImprovedBookmarkRow extends ViewLookupCachingFrameLayout
 
     private boolean mDragEnabled;
     private boolean mBookmarkIdEditable;
+    private boolean mEndImageViewVisible;
     private boolean mMoreButtonVisible;
     private boolean mSelectionEnabled;
     private boolean mIsSelected;
@@ -234,12 +235,13 @@ public class ImprovedBookmarkRow extends ViewLookupCachingFrameLayout
     }
 
     void setEndImageVisible(boolean visible) {
-        mEndImageView.setVisibility(visible ? View.VISIBLE : View.GONE);
+        mEndImageViewVisible = visible;
+        updateView();
     }
 
     void setEndMenuVisible(boolean visible) {
         mMoreButtonVisible = visible;
-        mMoreButton.setVisibility(visible ? View.VISIBLE : View.GONE);
+        updateView();
     }
 
     void setEndImageRes(int res) {
@@ -256,6 +258,8 @@ public class ImprovedBookmarkRow extends ViewLookupCachingFrameLayout
         boolean moreVisible = mMoreButtonVisible && !mIsSelected && mBookmarkIdEditable;
         mCheckImageView.setVisibility(checkVisible ? View.VISIBLE : View.GONE);
         mMoreButton.setVisibility(moreVisible ? View.VISIBLE : View.GONE);
+        mEndImageView.setVisibility(
+                !moreVisible && mEndImageViewVisible ? View.VISIBLE : View.GONE);
     }
 
     ImprovedBookmarkFolderView getFolderView() {
