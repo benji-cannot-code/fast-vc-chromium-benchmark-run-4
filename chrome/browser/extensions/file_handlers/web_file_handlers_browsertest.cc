3bdcd2c13990ef69e23f42a6fc2ba5da5a1608fe
Solomon Kinard <solomonkinard@chromium.org>
2023-10-30 17:42:31 +0000

    Extensions: WFH: Rename test to WebFileHandlersBrowserTest
    The naming is easier to understand at a glance and matches the name of
    the feature.
    
    Bug: chromium:1448893
    Change-Id: Ia5807866a7d5c24986ff48dde5243d35cab29602
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4982204
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217040}

--- chrome/browser/extensions/file_handlers/web_file_handlers_browsertest.cc
+++ chrome/browser/extensions/file_handlers/web_file_handlers_browsertest.cc
@@ -14,9 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extensions {
 namespace {
 
-class FileHandlingWithoutFeatureBrowserTest : public ExtensionBrowserTest {
+class WebFileHandlersBrowserTest : public ExtensionBrowserTest {
  public:
-  FileHandlingWithoutFeatureBrowserTest() {
+  WebFileHandlersBrowserTest() {
     feature_list_.InitAndDisableFeature(
         extensions_features::kExtensionWebFileHandlers);
   }
@@ -27,7 +27,7 @@ class FileHandlingWithoutFeatureBrowserTest : public ExtensionBrowserTest {
 
 // Web File Handlers are either parsed or emit a warning, depending on the
 // current browser channel and feature enablement.
-IN_PROC_BROWSER_TEST_F(FileHandlingWithoutFeatureBrowserTest, Warning) {
+IN_PROC_BROWSER_TEST_F(WebFileHandlersBrowserTest, Warning) {
   struct {
     const char* name;
     const char* manifest;
