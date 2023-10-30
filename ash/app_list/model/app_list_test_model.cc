8513b23f94a217e12d9498a4b553fbfb2d71df4c
Ana Salazar <anasalazar@chromium.org>
2023-10-30 18:15:57 +0000

    Handle placeholder icon creation on client side
    The placeholder icon needs to be styled so that it is colored on jelly
    style according to theme.  Since the apps service sends a generated
    ImageSkia, the ideal solution for now will be to take advantage of the
    is_placeholder_icon field on the app_list-model and generate a vector
    icon that can be colored with the theme.
    
    Ideally, we should move the logic to use ImageModel later.
    
    Bug: b:266734005
    Change-Id: I40e6333784015fe28f0a8bf40cf9daee64a38ff3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4974418
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217064}

--- ash/app_list/model/app_list_test_model.cc
+++ ash/app_list/model/app_list_test_model.cc
@@ -43,7 +43,7 @@ AppListTestModel::AppListTestItem::AppListTestItem(const std::string& id,
   const int icon_dimension =
       SharedAppListConfig::instance().default_grid_icon_dimension();
   SetDefaultIconAndColor(CreateImageSkia(icon_dimension, icon_dimension),
-                         IconColor());
+                         IconColor(), /*is_placeholder_icon=*/false);
 }
 
 AppListTestModel::AppListTestItem::~AppListTestItem() = default;
