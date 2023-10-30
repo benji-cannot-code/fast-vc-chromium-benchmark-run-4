a440cf4ca1042953ddee451064537934c539beaa
Tina Wang <tinazwang@google.com>
2023-10-30 19:27:51 +0000

    [CodeHealthRotation] Remove expired histogram
    Remove expired histograms CustomTabs.SpeculationStatusOnStart,
    CustomTabs.SpeculationStatusOnSwap.
    
    Bug: 1384816
    Change-Id: I4bde58c84295d6dba83a93e074bc654826d33791
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4978337
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217129}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/HiddenTabHolder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/HiddenTabHolder.java
@@ -196,10 +196,8 @@ public class HiddenTabHolder {
             if (referrer == null) referrer = "";
 
             if (urlsMatch && TextUtils.equals(speculationReferrer, referrer)) {
-                CustomTabsConnection.recordSpeculationStatusSwapTabTaken();
                 return tab;
             } else {
-                CustomTabsConnection.recordSpeculationStatusSwapTabNotMatched();
                 tab.destroy();
                 return null;
             }
