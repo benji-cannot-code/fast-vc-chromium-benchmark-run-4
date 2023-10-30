7cf1a75496b4b3caadbf87891c9200dfb750c660
Joone Hur <joone@chromium.org>
2023-10-30 22:44:20 +0000

    ozone: demo: Fix crash in skia_demo with Intel GPU on Linux
    This CL addresses a crash issue happening when running skia_demo with
    Intel GPU on Linux.
    
    The following changes have been made to resolve these issues:
    * Invoked ui::EnsureIntelMediaCompressionEnvVarIsSet().
    * Created a single-threaded task runner for handling user input.
    * Corrected the initialization of GLSurface size.
    
    Bug: None
    Test: skia_demo
    Change-Id: I03bd6cb6dd029a3eb32c7e02cde97a430ec48e01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4981094
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Joone Hur <joone@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217243}

--- ui/ozone/demo/skia/skia_demo.cc
+++ ui/ozone/demo/skia/skia_demo.cc
@@ -24,6 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/demo/window_manager.h"
 #include "ui/ozone/public/ozone_platform.h"
 
+#if BUILDFLAG(IS_CHROMEOS)
+#include "ui/gfx/linux/gbm_util.h"  // nogncheck
+#endif
+
 int main(int argc, char** argv) {
   base::CommandLine::Init(argc, argv);
   base::AtExitManager exit_manager;
@@ -55,6 +59,17 @@ int main(int argc, char** argv) {
   ui::OzonePlatform::InitializeForUI(params);
   ui::KeyboardLayoutEngineManager::GetKeyboardLayoutEngine()
       ->SetCurrentLayoutByName("us");
+
+#if BUILDFLAG(IS_CHROMEOS)
+  ui::EnsureIntelMediaCompressionEnvVarIsSet();
+#endif
+
+  auto shutdown_cb =
+      base::BindOnce([] { LOG(FATAL) << "Failed to shutdown."; });
+
+  ui::OzonePlatform::GetInstance()->PostCreateMainMessageLoop(
+      std::move(shutdown_cb),
+      base::SingleThreadTaskRunner::GetCurrentDefault());
 
   ui::OzonePlatform::InitializeForGPU(params);
 
