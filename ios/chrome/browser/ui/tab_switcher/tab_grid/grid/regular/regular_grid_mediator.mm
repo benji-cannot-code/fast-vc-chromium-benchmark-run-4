81e44a18cd28687559988f96d1da374e2d81a9e5
Sylvain Defresne <sdefresne@chromium.org>
2023-10-30 17:44:53 +0000

    [ios] Avoid crashing when closing all tabs with optimised storage
    If optimised session storage is enabled, it is not possible to call the
    `SerializeWebStateList(...)` function. Check whether the feature is on
    before calling the method.
    
    This is a first step to avoid crashing when the "close all tabs" button
    is used. The code to support the "undo" operation will require adding
    new API to SessionRestorationService. Until this API is available the
    feature will not be enabled by default.
    
    Bug: 1383087
    Change-Id: Ic62911989e51bc003a420abb5bb42b3aa30d8be5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4955157
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217042}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/regular/regular_grid_mediator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/regular/regular_grid_mediator.mm
@@ -25,6 +25,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_paging.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/toolbars/tab_grid_toolbars_configuration.h"
 
+// To get access to UseSessionSerializationOptimizations().
+// TODO(crbug.com/1383087): remove once the feature is fully launched.
+#import "ios/web/common/features.h"
+
 // TODO(crbug.com/1457146): Needed for `TabPresentationDelegate`, should be
 // refactored.
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_view_controller.h"
@@ -65,11 +69,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       return;
     }
 
-    _closedSessionWindow = SerializeWebStateList(self.webStateList);
+    if (!web::features::UseSessionSerializationOptimizations()) {
+      _closedSessionWindow = SerializeWebStateList(self.webStateList);
+    }
     self.webStateList->CloseAllNonPinnedWebStates(
         WebStateList::CLOSE_USER_ACTION);
   } else {
-    _closedSessionWindow = SerializeWebStateList(self.webStateList);
+    if (!web::features::UseSessionSerializationOptimizations()) {
+      _closedSessionWindow = SerializeWebStateList(self.webStateList);
+    }
     self.webStateList->CloseAllWebStates(WebStateList::CLOSE_USER_ACTION);
   }
 
@@ -82,13 +90,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)undoCloseAllItems {
   base::RecordAction(
       base::UserMetricsAction("MobileTabGridUndoCloseAllRegularTabs"));
-  if (!_closedSessionWindow) {
-    return;
+  if (!web::features::UseSessionSerializationOptimizations()) {
+    if (!_closedSessionWindow) {
+      return;
+    }
+    SessionRestorationBrowserAgent::FromBrowser(self.browser)
+        ->RestoreSessionWindow(_closedSessionWindow,
+                               SessionRestorationScope::kRegularOnly);
+    _closedSessionWindow = nil;
   }
-  SessionRestorationBrowserAgent::FromBrowser(self.browser)
-      ->RestoreSessionWindow(_closedSessionWindow,
-                             SessionRestorationScope::kRegularOnly);
-  _closedSessionWindow = nil;
   [self removeEntriesFromTabRestoreService];
   _syncedClosedTabsCount = 0;
 }
