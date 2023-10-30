f3ac1707995c8a9dd4590e578de2e46e77e3c0dd
Rebekah Potter <rbpotter@chromium.org>
2023-10-30 17:57:59 +0000

    WebUI Mojo Bindings: Use TypeScript for url.mojom-webui on non-Ash
    Only ChromeOS Ash still needs the JavaScript version of the bindings to
    support legacy Closure Compiler targets. On other platforms, TypeScript
    bindings are preferred to improve typechecking. Therefore, generate the
    url.mojom-webui binding file in TypeScript on other platforms, and
    remove the special exception allowing JS bindings for this case.
    
    Bug: 1002798
    Change-Id: I4ee268e29e2b359c3a79d6368104b943bb586bd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4950731
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217052}

--- content/browser/webui/web_ui_mojo_browsertest.cc
+++ content/browser/webui/web_ui_mojo_browsertest.cc
@@ -41,8 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/test_navigation_observer.h"
 #include "content/public/test/test_utils.h"
 #include "content/shell/browser/shell.h"
-#include "content/test/data/web_ui_test.test-mojom.h"
-#include "content/test/data/web_ui_test_types.test-mojom.h"
 #include "content/test/data/web_ui_ts_test.test-mojom.h"
 #include "content/test/data/web_ui_ts_test_types.test-mojom.h"
 #include "content/test/grit/web_ui_mojo_test_resources.h"
@@ -52,6 +50,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "third_party/blink/public/common/chrome_debug_urls.h"
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "content/test/data/web_ui_test.test-mojom.h"
+#include "content/test/data/web_ui_test_types.test-mojom.h"
+#endif
+
 namespace content {
 namespace {
 
@@ -59,6 +62,7 @@ const char kMojoWebUiHost[] = "mojo-web-ui";
 const char kMojoWebUiTsHost[] = "mojo-web-ui-ts";
 const char kDummyWebUiHost[] = "dummy-web-ui";
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
 class WebUIMojoTestCacheImpl : public mojom::WebUIMojoTestCache {
  public:
   explicit WebUIMojoTestCacheImpl(
@@ -83,6 +87,7 @@ class WebUIMojoTestCacheImpl : public mojom::WebUIMojoTestCache {
   mojo::Receiver<mojom::WebUIMojoTestCache> receiver_;
   std::map<GURL, std::string> cache_;
 };
+#endif
 
 // Duplicate for the TypeScript version of the test. We can't re-use because
 // the TS interface has to be named differently to avoid conflicting symbols.
@@ -147,6 +152,7 @@ class TestWebUIController : public WebUIController {
         base::make_span(kWebUiMojoTestResources, kWebUiMojoTestResourcesSize);
 
     web_ui->SetBindings(bindings);
+#if BUILDFLAG(IS_CHROMEOS_ASH)
     {
       WebUIDataSource* data_source = WebUIDataSource::CreateAndAdd(
           web_ui->GetWebContents()->GetBrowserContext(), kMojoWebUiHost);
@@ -157,6 +163,7 @@ class TestWebUIController : public WebUIController {
       data_source->AddResourcePaths(kMojoWebUiResources);
       data_source->AddResourcePath("", IDR_WEB_UI_MOJO_HTML);
     }
+#endif
     {
       WebUIDataSource* data_source = WebUIDataSource::CreateAndAdd(
           web_ui->GetWebContents()->GetBrowserContext(), kMojoWebUiTsHost);
@@ -183,7 +190,9 @@ class TestWebUIController : public WebUIController {
   TestWebUIController& operator=(const TestWebUIController&) = delete;
 
  protected:
+#if BUILDFLAG(IS_CHROMEOS_ASH)
   std::unique_ptr<WebUIMojoTestCacheImpl> cache_;
+#endif
   std::unique_ptr<WebUITsMojoTestCacheImpl> ts_cache_;
 };
 
@@ -196,16 +205,20 @@ class CacheTestWebUIController : public TestWebUIController {
       : TestWebUIController(web_ui) {}
   ~CacheTestWebUIController() override = default;
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
   void BindInterface(
       mojo::PendingReceiver<mojom::WebUIMojoTestCache> receiver) {
     cache_ = std::make_unique<WebUIMojoTestCacheImpl>(std::move(receiver));
     ASSERT_FALSE(ts_cache_);
   }
+#endif
 
   void BindInterface(
       mojo::PendingReceiver<mojom::WebUITsMojoTestCache> receiver) {
     ts_cache_ = std::make_unique<WebUITsMojoTestCacheImpl>(std::move(receiver));
+#if BUILDFLAG(IS_CHROMEOS_ASH)
     ASSERT_FALSE(cache_);
+#endif
   }
 
   WEB_UI_CONTROLLER_TYPE_DECL();
@@ -295,8 +308,10 @@ class TestWebUIContentBrowserClient
   void RegisterBrowserInterfaceBindersForFrame(
       RenderFrameHost* render_frame_host,
       mojo::BinderMapWithContext<content::RenderFrameHost*>* map) override {
+#if BUILDFLAG(IS_CHROMEOS_ASH)
     RegisterWebUIControllerInterfaceBinder<mojom::WebUIMojoTestCache,
                                            CacheTestWebUIController>(map);
+#endif
     RegisterWebUIControllerInterfaceBinder<mojom::WebUITsMojoTestCache,
                                            CacheTestWebUIController>(map);
   }
@@ -343,7 +358,13 @@ class WebUIMojoTest : public ContentBrowserTest,
   std::unique_ptr<TestWebUIContentBrowserClient> client_;
 };
 
+// Test both JS and TS on Ash, since Ash widely uses both types of WebUI
+// bindings. Test TS only on other platforms.
+#if BUILDFLAG(IS_CHROMEOS_ASH)
 INSTANTIATE_TEST_SUITE_P(All, WebUIMojoTest, testing::Bool());
+#else
+INSTANTIATE_TEST_SUITE_P(All, WebUIMojoTest, testing::Values(true));
+#endif
 
 // Loads a WebUI page that contains Mojo JS bindings and verifies a message
 // round-trip between the page and the browser.
