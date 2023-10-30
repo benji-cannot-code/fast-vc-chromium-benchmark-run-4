1446384001e7f2e2c83a88cac0f26331e93730f7
Hira Mahmood <hiramahmood@google.com>
2023-10-30 16:28:56 +0000

    [iOS] Minor changes to Parcel Tracking Opt In Screen
    This CL makes minor UI fixes to the opt-in UI sheet (spacing, button
    colors, sheet height).
    
    Before: https://screenshot.googleplex.com/8886npQzBvJeN4K
    After: https://screenshot.googleplex.com/4c6u74y4m5g9qps
    
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL
    Bug: 1473449
    Change-Id: Iea1c32220a4df67e4f7a2b6d2ed6b9cc9f18dd6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4980147
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1216993}

--- ios/chrome/browser/ui/parcel_tracking/parcel_tracking_opt_in_view_controller.mm
+++ ios/chrome/browser/ui/parcel_tracking/parcel_tracking_opt_in_view_controller.mm
@@ -29,6 +29,10 @@ CGFloat const kTableViewCornerRadius = 10;
 CGFloat const kTableViewEstimatedRowHeight = 48;
 // Margin for the options view.
 CGFloat const kOptionsViewMargin = 17;
+// Spacing before the image.
+CGFloat const kSpacingBeforeImage = 23;
+// Size of the radio buttons.
+CGFloat const kRadioButtonSize = 20;
 }  // namespace
 
 @interface ParcelTrackingOptInViewController () <UITableViewDelegate,
@@ -59,10 +63,14 @@ CGFloat const kOptionsViewMargin = 17;
   self.image = [UIImage imageNamed:kOptInIcon];
   self.imageHasFixedSize = true;
   self.topAlignedLayout = YES;
-  self.sheetPresentationController.detents = @[
-    UISheetPresentationControllerDetent.largeDetent
-  ];
-
+  if (@available(iOS 16, *)) {
+    self.sheetPresentationController.detents = @[
+      UISheetPresentationControllerDetent.largeDetent,
+      self.preferredHeightDetent
+    ];
+  }
+  self.customSpacingAfterImage = 0;
+  self.customSpacingBeforeImageIfNoNavigationBar = kSpacingBeforeImage;
   [super viewDidLoad];
 
   // Assign table view's width anchor now that it is in the same hierarchy as
@@ -150,9 +158,9 @@ CGFloat const kOptionsViewMargin = 17;
   cell.userInteractionEnabled = YES;
   cell.textLabel.text = title;
 
-  cell.accessoryView = [[UIImageView alloc]
-      initWithImage:DefaultSymbolTemplateWithPointSize(
-                        kCircleSymbol, kSymbolAccessoryPointSize)];
+  cell.accessoryView =
+      [[UIImageView alloc] initWithImage:DefaultSymbolTemplateWithPointSize(
+                                             kCircleSymbol, kRadioButtonSize)];
   cell.accessoryView.tintColor = [UIColor colorNamed:kGrey500Color];
 
   return cell;
@@ -220,9 +228,9 @@ CGFloat const kOptionsViewMargin = 17;
   UIButton* button = self.primaryActionButton;
   if (state == UIControlStateDisabled) {
     button.userInteractionEnabled = NO;
-    [button setBackgroundColor:[UIColor colorNamed:kDisabledTintColor]];
-    [button setTitleColor:[UIColor colorNamed:kTextPrimaryColor]
-                 forState:UIControlStateDisabled];
+    [button setBackgroundColor:[UIColor colorNamed:kGrey200Color]];
+    [button setTitleColor:[UIColor colorNamed:kGrey600Color]
+                 forState:UIControlStateNormal];
   } else if (state == UIControlStateNormal) {
     button.userInteractionEnabled = YES;
     [button setBackgroundColor:[UIColor colorNamed:kBlueColor]];
