6d76d5cfef9e0874774f5ee53cdf45ec8a4d48f1
Federica Germinario <fedegermi@google.com>
2023-10-30 09:35:34 +0000

    [ios] Move ActivityBucketLastReportedDateArray out of NSUserDefaults
    This CL moves ActivityBucketLastReportedDateArray out of
    NSUserDefaults.
    
    The key has been moved in the PrefService. Note that there are two
    types of PrefServices, in this case the code was moved in the
    PrefService coming from BrowserState which is per account and is
    syncable between devices.
    Note that this kind of PrefService behaves differently from
    NSUserDefaults which is per device (and not per account).
    
    Bug: 1486770
    Change-Id: I07138715768f162cea77450eda73eb92b6f4c4ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4971133
    Reviewed-by: Guillem Perez <guiperez@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1216842}

--- ios/chrome/browser/ui/ntp/metrics/feed_metrics_recorder_unittest.mm
+++ ios/chrome/browser/ui/ntp/metrics/feed_metrics_recorder_unittest.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+#import "base/json/values_util.h"
 #import "base/test/metrics/histogram_tester.h"
 #import "base/test/metrics/user_action_tester.h"
 #import "components/feed/core/v2/public/common_enums.h"
@@ -646,9 +647,7 @@ TEST_F(FeedMetricsRecorderTest, TestComputeActivityBuckets_kNoActivity) {
   test_pref_service_.SetTime(kActivityBucketLastReportedDateKey,
                              last_activity_bucket);
   // Make sure LastReportedDateArray is empty.
-  NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
-  [defaults setObject:[NSMutableArray new]
-               forKey:kActivityBucketLastReportedDateArrayKey];
+  test_pref_service_.ClearPref(kActivityBucketLastReportedDateArrayKey);
 
   [recorder_ recordNTPDidChangeVisibility:YES];
 
@@ -663,12 +662,12 @@ TEST_F(FeedMetricsRecorderTest, TestComputeActivityBuckets_kLowActivity) {
   test_pref_service_.SetTime(kActivityBucketLastReportedDateKey,
                              last_activity_bucket);
   // Make sure LastReportedDateArray is in range 1 to 7.
-  NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
-  NSMutableArray* array = [NSMutableArray array];
+  base::Value::List listOfDates;
   for (size_t i = 0; i < 5; ++i) {
-    [array addObject:[NSDate date]];
+    listOfDates.Append(TimeToValue(base::Time::Now()));
   }
-  [defaults setObject:array forKey:kActivityBucketLastReportedDateArrayKey];
+  test_pref_service_.SetList(kActivityBucketLastReportedDateArrayKey,
+                             std::move(listOfDates));
 
   [recorder_ recordNTPDidChangeVisibility:YES];
 
@@ -683,12 +682,12 @@ TEST_F(FeedMetricsRecorderTest, TestComputeActivityBuckets_kMediumActivity) {
   test_pref_service_.SetTime(kActivityBucketLastReportedDateKey,
                              last_activity_bucket);
   // Make sure LastReportedDateArray is in range 8 to 15.
-  NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
-  NSMutableArray* array = [NSMutableArray array];
+  base::Value::List listOfDates;
   for (size_t i = 0; i < 9; ++i) {
-    [array addObject:[NSDate date]];
+    listOfDates.Append(TimeToValue(base::Time::Now()));
   }
-  [defaults setObject:array forKey:kActivityBucketLastReportedDateArrayKey];
+  test_pref_service_.SetList(kActivityBucketLastReportedDateArrayKey,
+                             std::move(listOfDates));
 
   [recorder_ recordNTPDidChangeVisibility:YES];
 
