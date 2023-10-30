fe4eb861987dc6353863a10fb560f55bf03e277e
Maggie Cai <mxcai@google.com>
2023-10-30 05:25:50 +0000

    [Shortcut] Add shortcut struct on crosapi mojom.
    This CL adds a shortcut struct on crosapi mojom and adds the mojom
    traits. This CL also deprecates the old unused shortcut struct from
    crosapi interface to avoid confusion.
    
    crosapi design: go/cros-shortcut-lacros
    
    BUG=b/304661502
    
    Change-Id: If79224f451f047f255f8daac0e79d7f2598b8ea4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4969978
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216782}

--- chromeos/crosapi/mojom/app_service_types_mojom_traits.h
+++ chromeos/crosapi/mojom/app_service_types_mojom_traits.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/app_service/public/cpp/intent_filter.h"
 #include "components/services/app_service/public/cpp/permission.h"
 #include "components/services/app_service/public/cpp/preferred_app.h"
+#include "components/services/app_service/public/cpp/shortcut/shortcut.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
 
@@ -113,7 +114,7 @@ struct StructTraits<crosapi::mojom::AppDataView, apps::AppPtr> {
   static crosapi::mojom::OptionalBool handles_intents(const apps::AppPtr& r);
 
   // This method is required for Ash-Lacros backwards compatibility.
-  static std::vector<crosapi::mojom::ShortcutPtr> deprecated_shortcuts(
+  static std::vector<crosapi::mojom::REMOVED_01Ptr> deprecated_shortcuts(
       const apps::AppPtr& r) {
     return {};
   }
@@ -389,6 +390,26 @@ struct StructTraits<crosapi::mojom::PreferredAppChangesDataView,
 
   static bool Read(crosapi::mojom::PreferredAppChangesDataView,
                    apps::PreferredAppChangesPtr* out);
+};
+
+template <>
+struct StructTraits<crosapi::mojom::AppShortcutDataView, apps::ShortcutPtr> {
+  static const std::string& host_app_id(const apps::ShortcutPtr& r) {
+    return r->host_app_id;
+  }
+
+  static const std::string& local_id(const apps::ShortcutPtr& r) {
+    return r->local_id;
+  }
+
+  static const absl::optional<std::string>& name(const apps::ShortcutPtr& r) {
+    return r->name;
+  }
+
+  static apps::IconKeyPtr icon_key(const apps::ShortcutPtr& r);
+
+  static bool Read(crosapi::mojom::AppShortcutDataView data,
+                   apps::ShortcutPtr* out);
 };
 
 }  // namespace mojo
