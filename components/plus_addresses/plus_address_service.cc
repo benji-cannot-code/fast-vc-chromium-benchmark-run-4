63ea84b8b18d8c8f8f03efa43eb3082532758871
Matt Reichhoff <mreichhoff@chromium.org>
2023-10-30 16:15:06 +0000

    Send origins to enterprise plus_addresses server
    The map is still eTLD+1 keyed, and it still assumes eTLD+1s
    come back in List calls. Upcoming changes will future-proof
    the latter; this change is merely to ensure the service has
    what it needs.
    
    Change-Id: I1e8c1dca7e8d80f3116cca5cc07af5ec994e9e22
    Bug: 1467623
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4980594
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Reviewed-by: Kirubel Aklilu <kaklilu@chromium.org>
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216986}

--- components/plus_addresses/plus_address_service.cc
+++ components/plus_addresses/plus_address_service.cc
@@ -113,7 +113,7 @@ void PlusAddressService::OfferPlusAddressCreation(
     return;
   }
   plus_address_client_.CreatePlusAddress(
-      GetEtldPlusOne(origin),
+      origin,
       // On receiving the PlusAddress...
       base::BindOnce(
           // ... first send it back to Autofill
@@ -137,7 +137,7 @@ void PlusAddressService::ReservePlusAddress(
     return;
   }
   plus_address_client_.ReservePlusAddress(
-      GetEtldPlusOne(origin),
+      origin,
       // Thin wrapper around on_completed to save the PlusAddress in the
       // success case.
       base::BindOnce(
@@ -169,7 +169,7 @@ void PlusAddressService::ConfirmPlusAddress(
     return;
   }
   plus_address_client_.ConfirmPlusAddress(
-      GetEtldPlusOne(origin), plus_address,
+      origin, plus_address,
       // Thin wrapper around on_completed to save the PlusAddress in the
       // success case.
       base::BindOnce(
