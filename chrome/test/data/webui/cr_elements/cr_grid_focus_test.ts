c314dd6ccb10c9d63e2c8e5d8326ce4ea939bb2d
dpapad <dpapad@chromium.org>
2023-10-30 19:05:40 +0000

    WebUI: Rename util_ts.ts to util.ts, part 1.
    In this first preparatory step
     1) Renaming util_ts.ts to util.ts
     2) Creating a new util_ts.ts file that simply re-exports the API of
        util.ts
    
    This is done in order to provide an incremental way of updating all
    references, which are too many to be done in a single CL.
    
    Also updating all references under the following folders:
     - ui/webui/resources/
     - chrome/test/data/webui/cr_elements/
     - chrome/test/data/webui/cr_components/
    
    Bug: 1497028
    Change-Id: Ifef97f15c0ebe7f96cc4a07c0948962965c63715
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4981847
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1217109}

--- chrome/test/data/webui/cr_elements/cr_grid_focus_test.ts
+++ chrome/test/data/webui/cr_elements/cr_grid_focus_test.ts
@@ -6,9 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/cr_grid/cr_grid.js';
 
 import {CrGridElement} from 'chrome://resources/cr_elements/cr_grid/cr_grid.js';
-import {getDeepActiveElement} from 'chrome://resources/js/util_ts.js';
+import {getDeepActiveElement} from 'chrome://resources/js/util.js';
 import {keyDownOn} from 'chrome://resources/polymer/v3_0/iron-test-helpers/mock-interactions.js';
-
 import {assertEquals} from 'chrome://webui-test/chai_assert.js';
 import {eventToPromise} from 'chrome://webui-test/test_util.js';
 
--- ui/webui/resources/js/focus_row.ts
+++ ui/webui/resources/js/focus_row.ts
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // clang-format off
 import {assert, assertInstanceof} from './assert.js';
 import {EventTracker} from './event_tracker.js';
-import {hasKeyModifiers, isRTL} from './util_ts.js';
+import {hasKeyModifiers, isRTL} from './util.js';
 // clang-format on
 
 const ACTIVE_CLASS: string = 'focus-row-active';
