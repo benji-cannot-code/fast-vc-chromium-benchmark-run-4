97bc5936e23f81e40e2620e82f862e686d72eee6
David Bertoni <dbertoni@chromium.org>
2023-10-30 09:55:33 +0000

    [Code Health] Remove uses of base::SupportsWeakPtr
    This CL removes uses of this deprecated base class by adding a
    base::WeakPtrFactory data member.
    
    In some cases, the base class was no longer being used, so it's
    simply removed.
    
    In some cases, it was being used as the base class for a supposed
    interface class. In those cases, a pure virtual member function
    was added so that implementation classes can have a WeakPtrFactory
    data member and dispense appropriate WeakPtr instances.
    
    This CL was uploaded by git cl split.
    
    R=anastasiian@chromium.org
    
    Bug: 647430
    Change-Id: Ica746b983af98f8091c87844837b57de755eca0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4984693
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216846}

--- chrome/browser/ash/login/signin/oauth2_token_fetcher.h
+++ chrome/browser/ash/login/signin/oauth2_token_fetcher.h
@@ -24,8 +24,7 @@ namespace ash {
 // OAuth2TokenFetcher is used to convert authenticated cookie jar from the
 // authentication profile into OAuth2 tokens and GAIA credentials that will be
 // used to kick off other token retrieval tasks.
-class OAuth2TokenFetcher : public base::SupportsWeakPtr<OAuth2TokenFetcher>,
-                           public GaiaAuthConsumer {
+class OAuth2TokenFetcher : public GaiaAuthConsumer {
  public:
   class Delegate {
    public:
@@ -46,6 +45,10 @@ class OAuth2TokenFetcher : public base::SupportsWeakPtr<OAuth2TokenFetcher>,
 
   void StartExchangeFromAuthCode(const std::string& auth_code,
                                  const std::string& signin_scoped_device_id);
+
+  base::WeakPtr<OAuth2TokenFetcher> AsWeakPtr() {
+    return weak_ptr_factory_.GetWeakPtr();
+  }
 
  private:
   // Decides how to proceed on GAIA `error`. If the error looks temporary,
@@ -69,6 +72,7 @@ class OAuth2TokenFetcher : public base::SupportsWeakPtr<OAuth2TokenFetcher>,
   std::string session_index_;
   std::string signin_scoped_device_id_;
   std::string auth_code_;
+  base::WeakPtrFactory<OAuth2TokenFetcher> weak_ptr_factory_{this};
 };
 
 }  // namespace ash
