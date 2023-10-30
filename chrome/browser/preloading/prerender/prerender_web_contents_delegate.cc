868f4ca8bcabb7f562452ab45676405a63908189
Hiroki Nakagawa <nhiroki@chromium.org>
2023-10-30 08:42:51 +0000

    Prerender: Disallow focusing on prerender WebContents
    Prerender WebContents created for prerender-into-new-tab should not be
    focused on.
    
    Bug: 1350676
    Change-Id: I8855b1ae767db46618a32f24449740a39a39b288
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4988548
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216826}

--- chrome/browser/preloading/prerender/prerender_web_contents_delegate.cc
+++ chrome/browser/preloading/prerender/prerender_web_contents_delegate.cc
@@ -38,6 +38,17 @@ bool PrerenderWebContentsDelegateImpl::ShouldSuppressDialogs(
   NOTREACHED_NORETURN();
 }
 
+bool PrerenderWebContentsDelegateImpl::ShouldFocusPageAfterCrash() {
+  // A prerendered page cannot be focused.
+  return false;
+}
+
+bool PrerenderWebContentsDelegateImpl::TakeFocus(content::WebContents* source,
+                                                 bool reverse) {
+  // A prerendered page cannot be focused.
+  return false;
+}
+
 void PrerenderWebContentsDelegateImpl::WebContentsCreated(
     content::WebContents* source_contents,
     int opener_render_process_id,
