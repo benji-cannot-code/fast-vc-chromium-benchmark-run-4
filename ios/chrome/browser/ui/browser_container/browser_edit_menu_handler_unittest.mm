bf343c6a2b318f53d606e6f1eced69a4c5c5fe1a
Asami Doi <asamidoi@chromium.org>
2023-10-30 16:18:40 +0000

    [ios] Delete unused header files
    Bug: N/A
    Change-Id: I06a9aa4c2ab2f28fa5872baea9ca96e447b73b2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4970417
    Auto-Submit: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216987}

--- ios/chrome/browser/ui/browser_container/browser_edit_menu_handler_unittest.mm
+++ ios/chrome/browser/ui/browser_container/browser_edit_menu_handler_unittest.mm
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
 #import "ios/chrome/browser/shared/model/web_state_list/test/fake_web_state_list_delegate.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
-#import "ios/chrome/browser/shared/model/web_state_list/web_state_list_delegate.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/ui/browser_container/browser_container_view_controller.h"
 #import "ios/chrome/browser/ui/link_to_text/link_to_text_mediator.h"
--- ios/chrome/browser/ui/partial_translate/partial_translate_mediator_unittest.mm
+++ ios/chrome/browser/ui/partial_translate/partial_translate_mediator_unittest.mm
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
 #import "ios/chrome/browser/shared/model/web_state_list/test/fake_web_state_list_delegate.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
-#import "ios/chrome/browser/shared/model/web_state_list/web_state_list_delegate.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_opener.h"
 #import "ios/chrome/browser/shared/public/commands/browser_coordinator_commands.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
