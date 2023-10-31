c60fae8486256cd31a0dce17d85b42b7140daecb
Curt Clemens <cclem@google.com>
2023-10-31 20:07:16 +0000

    [QuickStart] Move message_elapsed_timer_ into QuickStartMetrics
    As a part of a larger refactor to remove metrics-related state from the
    Connection class, let's have the QuickStartMetrics class track the
    duration of request/response pairs so that Connection doesn't have to.
    
    Test: Manual smoke test, existing unit tests provide adequate coverage
    
    Bug: b/307817810
    Change-Id: I77ff1ac93363af007868a80a1c3dc46039e1c489
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4982009
    Commit-Queue: Curt Clemens <cclem@google.com>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Cr-Commit-Position: refs/heads/main@{#1217833}

--- chrome/browser/ash/login/oobe_quick_start/connectivity/connection.h
+++ chrome/browser/ash/login/oobe_quick_start/connectivity/connection.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
-#include "base/timer/elapsed_timer.h"
 #include "base/timer/timer.h"
 #include "base/values.h"
 #include "chrome/browser/ash/login/oobe_quick_start/connectivity/account_transfer_client_data.h"
@@ -197,7 +196,6 @@ class Connection
   ConnectionAuthenticatedCallback on_connection_authenticated_;
   std::string challenge_b64url_;
   mojo::SharedRemote<mojom::QuickStartDecoder> decoder_;
-  std::unique_ptr<base::ElapsedTimer> message_elapsed_timer_;
   std::unique_ptr<AccountTransferClientData> client_data_;
   QuickStartMetrics quick_start_metrics_;
 
