4a12377a5f53a371f6cebb051971cc7430e55244
Gyuyoung Kim <gyuyoung@igalia.com>
2023-10-31 01:33:52 +0000

    [iOS] Show a keyboard when |state.value| is not nullptr.
    When running some web tests related to editing, there is
    a case that the value of a test input state is nullptr.
    It causes a timeout on the tests. So this CL passes
    |(state.value && !state.value->empty()| to *has_text*
    argument when calling showKeyboard() in
    onUpdateTextInputState().
    
    Bug: 1421239
    Change-Id: I9736aa5fa4b8a7f500ccf82696bbb9db0e38f2e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4982425
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217360}

--- content/browser/renderer_host/render_widget_host_view_ios.mm
+++ content/browser/renderer_host/render_widget_host_view_ios.mm
@@ -105,7 +105,8 @@ bool IsTesting() {
     // policy is manual.
     if (state.last_vk_visibility_request ==
         ui::mojom::VirtualKeyboardVisibilityRequest::SHOW) {
-      [self showKeyboard:!state.value->empty() withBounds:bounds];
+      [self showKeyboard:(state.value && !state.value->empty())
+              withBounds:bounds];
     } else if (state.last_vk_visibility_request ==
                ui::mojom::VirtualKeyboardVisibilityRequest::HIDE) {
       [self hideKeyboard];
@@ -117,7 +118,8 @@ bool IsTesting() {
     if (hide) {
       [self hideKeyboard];
     } else if (state.show_ime_if_needed) {
-      [self showKeyboard:!state.value->empty() withBounds:bounds];
+      [self showKeyboard:(state.value && !state.value->empty())
+              withBounds:bounds];
     }
   }
 }
