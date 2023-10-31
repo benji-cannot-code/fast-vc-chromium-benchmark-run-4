f09b33ff7b4b893c59338f29ddd0d4f4f753361a
Illia Klimov <elklm@google.com>
2023-10-31 10:55:35 +0000

    Fix the confirmation chip's animation
    If `gfx::Animation::ShouldRenderRichAnimation()` returns `false` then Chip will have instant animation. That means in `CollapseConfirmation()` started animation will fire a callback immediately, before
    `is_waiting_for_confirmation_collapse_` is set to `true`. That impacts
    `OnCollapseAnimationEnded()` never hides the confirmation chip.
    
    Bug: 1483716
    Change-Id: I697d8b2e5300cc5bff444d60d5b41cfd4e34d923
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4987084
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Elias Klim <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217483}

--- chrome/browser/ui/views/permissions/chip_controller.h
+++ chrome/browser/ui/views/permissions/chip_controller.h
@@ -110,7 +110,7 @@ class ChipController : public permissions::PermissionRequestManager::Observer,
 
   bool should_expand_for_testing();
 
-  bool is_collapse_timer_running_for_testing() {
+  bool is_collapse_timer_running_for_testing() const {
     CHECK_IS_TEST();
     return collapse_timer_.IsRunning();
   }
@@ -120,7 +120,7 @@ class ChipController : public permissions::PermissionRequestManager::Observer,
     collapse_timer_.FireNow();
   }
 
-  bool is_dismiss_timer_running_for_testing() {
+  bool is_dismiss_timer_running_for_testing() const {
     CHECK_IS_TEST();
     return dismiss_timer_.IsRunning();
   }
@@ -140,6 +140,16 @@ class ChipController : public permissions::PermissionRequestManager::Observer,
   active_permission_request_manager_for_testing() {
     CHECK_IS_TEST();
     return active_chip_permission_request_manager_;
+  }
+
+  bool is_confirmation_showing_for_testing() const {
+    CHECK_IS_TEST();
+    return is_confirmation_showing_;
+  }
+
+  bool is_waiting_for_confirmation_collapse_for_testing() const {
+    CHECK_IS_TEST();
+    return is_waiting_for_confirmation_collapse_;
   }
 
  private:
