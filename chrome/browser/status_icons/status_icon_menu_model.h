89f43ec7825521466e6309ed56057f944422476c
David Bertoni <dbertoni@chromium.org>
2023-10-31 09:16:19 +0000

    [Code Health] Remove uses of base::SupportsWeakPtr (c/b/status_icons)
    This CL removes uses of this deprecated base class by adding a
    base::WeakPtrFactory data member.
    
    In some cases, the base class was no longer being used, so it's
    simply removed.
    
    In some cases, it was being used as the base class for a supposed
    interface class. In those cases, a pure virtual member function
    was added so that implementation classes can have a WeakPtrFactory
    data member and dispense appropriate WeakPtr instances.
    
    This CL was uploaded by git cl split.
    
    R=atwilson@chromium.org
    
    Bug: 647430
    Change-Id: Ic07b1ba9fce25c2774613aa56812ac649ef5d81f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4984490
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217464}

--- chrome/browser/status_icons/status_icon_menu_model.h
+++ chrome/browser/status_icons/status_icon_menu_model.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 
 #include "base/memory/raw_ptr.h"
-#include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "ui/base/models/image_model.h"
 #include "ui/base/models/simple_menu_model.h"
@@ -23,10 +22,8 @@ class Image;
 // model state has changed and can tell the status icon to update the menu. This
 // is necessary some platforms which do not notify us before showing the menu
 // (like Ubuntu Unity).
-class StatusIconMenuModel
-    : public ui::SimpleMenuModel,
-      public ui::SimpleMenuModel::Delegate,
-      public base::SupportsWeakPtr<StatusIconMenuModel> {
+class StatusIconMenuModel : public ui::SimpleMenuModel,
+                            public ui::SimpleMenuModel::Delegate {
  public:
   class Delegate {
    public:
