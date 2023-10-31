d7f376c28f7f307f69a607cb01a2be04ec7dad09
David Bertoni <dbertoni@chromium.org>
2023-10-31 06:05:44 +0000

    [Code Health] Remove uses of base::SupportsWeakPtr (ash/wallpaper_split)
    This CL removes uses of this deprecated base class by adding a
    base::WeakPtrFactory data member.
    
    In some cases, the base class was no longer being used, so it's
    simply removed.
    
    In some cases, it was being used as the base class for a supposed
    interface class. In those cases, a pure virtual member function
    was added so that implementation classes can have a WeakPtrFactory
    data member and dispense appropriate WeakPtr instances.
    
    This CL was uploaded by git cl split.
    
    R=xiaohuic@chromium.org
    
    Bug: 647430
    Change-Id: I5015398877a8beb5c370932839cf43d6d864e056
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4984709
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217411}

--- ash/wallpaper/wallpaper_pref_manager.h
+++ ash/wallpaper/wallpaper_pref_manager.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wallpaper/wallpaper_utils/wallpaper_calculated_colors.h"
 #include "base/containers/flat_map.h"
 #include "base/files/file_path.h"
-#include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "base/observer_list_types.h"
 #include "base/strings/string_piece.h"
@@ -58,9 +57,7 @@ class WallpaperProfileHelper {
 };
 
 // Manages wallpaper preferences and tracks the currently configured wallpaper.
-class ASH_EXPORT WallpaperPrefManager
-    : public base::SupportsWeakPtr<WallpaperPrefManager>,
-      public SessionObserver {
+class ASH_EXPORT WallpaperPrefManager : public SessionObserver {
  public:
   // Names of nodes with wallpaper info in |kUserWallpaperInfo| dictionary.
   static const char kNewWallpaperAssetIdNodeName[];
