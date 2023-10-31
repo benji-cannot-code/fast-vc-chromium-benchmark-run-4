46c93ab559d640704ae335e7eea6576e145a256a
Amy Huang <akhuang@google.com>
2023-10-31 02:15:59 +0000

    Header cleanup: Add include <vector>
    Bug: 1497475
    Change-Id: Ic2fef2a36c3c6a638ea0667012dd2f6b3f8613b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4990445
    Owners-Override: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217369}

--- chromeos/ash/components/chaps_util/chaps_util.h
+++ chromeos/ash/components/chaps_util/chaps_util.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <pk11pub.h>
 
 #include <memory>
+#include <vector>
 
 #include "base/component_export.h"
 #include "base/functional/callback_forward.h"
--- chromeos/ash/components/chaps_util/chaps_util_impl.h
+++ chromeos/ash/components/chaps_util/chaps_util_impl.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROMEOS_ASH_COMPONENTS_CHAPS_UTIL_CHAPS_UTIL_IMPL_H_
 
 #include <memory>
+#include <vector>
 
 #include "base/component_export.h"
 #include "base/functional/callback_forward.h"
