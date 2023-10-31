617d9fdf4b542988154af81e878b8ae8e0cf4068
Roman Arora <romanarora@chromium.org>
2023-10-31 14:36:50 +0000

    [ntp-history] Header done action button
    Adds a done icon button to the NTP history module header.
    
    Demo: https://drive.google.com/file/d/1_DlYXCkVefyCVenTcQIwKG6n4TsH7-Dt/view
    
    Fixed: 1481257
    Change-Id: Ic03d2a672db24a67af7d342f22c45e46853f1e75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4990162
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1217569}

--- chrome/browser/resources/new_tab_page/modules/v2/history_clusters/header_tile.ts
+++ chrome/browser/resources/new_tab_page/modules/v2/history_clusters/header_tile.ts
@@ -52,6 +52,12 @@ export class HistoryClustersHeaderElementV2 extends ElementBase {
         'show-all-button-click', {bubbles: true, composed: true}));
   }
 
+  private onDoneClick_(e: Event) {
+    e.stopPropagation();
+    this.dispatchEvent(
+        new CustomEvent('done-button-click', {bubbles: true, composed: true}));
+  }
+
   private onSuggestClick_(e: Event) {
     e.stopPropagation();
     this.dispatchEvent(
@@ -70,7 +76,7 @@ export class HistoryClustersHeaderElementV2 extends ElementBase {
         {
           action: 'done',
           icon: 'modules:done',
-          text: this.i18n('modulesJourneysDoneButton'),
+          text: this.i18n('modulesHistoryDoneButton'),
         },
         {
           action: 'dismiss',
