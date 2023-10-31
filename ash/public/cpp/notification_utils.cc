a0c68e30c2d46b4369af786c72029c561a34adc7
Sean Kau <skau@chromium.org>
2023-10-31 00:32:39 +0000

    Migrate /ash/public/cpp off of accent_color
    accent_color does not handle dynamic color. As a result, we favor
    accent_color_id. Callers which do not explicitly need SkColor
    support are migrated.
    
    This CL was uploaded by git cl split.
    
    R=jamescook@chromium.org
    
    Bug: b:308208767, b:291622042
    Change-Id: Icb7e1c90111420f80711efe2095e3b6218e5a18e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4989925
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217325}

--- ash/public/cpp/notification_utils.cc
+++ ash/public/cpp/notification_utils.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/notification_utils.h"
 
-#include "chromeos/constants/chromeos_features.h"
 #include "ui/chromeos/styles/cros_tokens_color_mappings.h"
 #include "ui/gfx/vector_icon_types.h"
 #include "ui/message_center/public/cpp/notification.h"
@@ -37,35 +36,19 @@ message_center::Notification CreateSystemNotification(
                                             notifier_id,
                                             optional_fields,
                                             delegate};
-  if (chromeos::features::IsJellyEnabled()) {
-    ui::ColorId color_id = cros_tokens::kCrosSysPrimary;
-    switch (warning_level) {
-      case message_center::SystemNotificationWarningLevel::NORMAL:
-        color_id = cros_tokens::kCrosSysPrimary;
-        break;
-      case message_center::SystemNotificationWarningLevel::WARNING:
-        color_id = cros_tokens::kCrosSysWarning;
-        break;
-      case message_center::SystemNotificationWarningLevel::CRITICAL_WARNING:
-        color_id = cros_tokens::kCrosSysError;
-        break;
-    }
-    notification.set_accent_color_id(color_id);
-  } else {
-    SkColor color = kSystemNotificationColorNormal;
-    switch (warning_level) {
-      case message_center::SystemNotificationWarningLevel::NORMAL:
-        color = kSystemNotificationColorNormal;
-        break;
-      case message_center::SystemNotificationWarningLevel::WARNING:
-        color = kSystemNotificationColorWarning;
-        break;
-      case message_center::SystemNotificationWarningLevel::CRITICAL_WARNING:
-        color = kSystemNotificationColorCriticalWarning;
-        break;
-    }
-    notification.set_accent_color(color);
+  ui::ColorId color_id = cros_tokens::kCrosSysPrimary;
+  switch (warning_level) {
+    case message_center::SystemNotificationWarningLevel::NORMAL:
+      color_id = cros_tokens::kCrosSysPrimary;
+      break;
+    case message_center::SystemNotificationWarningLevel::WARNING:
+      color_id = cros_tokens::kCrosSysWarning;
+      break;
+    case message_center::SystemNotificationWarningLevel::CRITICAL_WARNING:
+      color_id = cros_tokens::kCrosSysError;
+      break;
   }
+  notification.set_accent_color_id(color_id);
 
   notification.set_system_notification_warning_level(warning_level);
   if (!small_image.is_empty())
