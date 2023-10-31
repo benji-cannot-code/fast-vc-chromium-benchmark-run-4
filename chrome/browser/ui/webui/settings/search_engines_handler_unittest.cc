91cb44532a57a5fec9bfcda580eef29c830aa610
Thiago Perrotta <tperrotta@chromium.org>
2023-10-31 11:38:55 +0000

    Disable Search Engine choice tests whenever the flag is disabled
    - C++ build flag: ENABLE_SEARCH_ENGINE_CHOICE
    - Defined in components/signin/public/base/BUILD.gn
    - Target //components/signin/public/base:signin_buildflags
    
    R=jyammine@google.com, mathias@chromium.org
    
    Bug: 1480533, 1494574
    Change-Id: I6e43d491dfe30c84173af53ab32e20931b1515bf
    Cq-Include-Trybots: luci.chromium.try:linux-rel-cft,mac-rel-cft,win-rel-cft
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4988367
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Thiago Perrotta <tperrotta@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Jack Yammine <jyammine@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217494}

--- chrome/browser/ui/webui/settings/search_engines_handler_unittest.cc
+++ chrome/browser/ui/webui/settings/search_engines_handler_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_engines/search_engines_pref_names.h"
 #include "components/search_engines/template_url.h"
 #include "components/search_engines/template_url_service.h"
+#include "components/signin/public/base/signin_buildflags.h"
 #include "components/signin/public/base/signin_switches.h"
 #include "content/public/test/browser_task_environment.h"
 #include "content/public/test/test_web_contents_factory.h"
@@ -155,6 +156,7 @@ TEST_P(SearchEnginesHandlerParametrizedTest,
   EXPECT_EQ("search-engines-changed", second_call_data.arg1()->GetString());
 }
 
+#if BUILDFLAG(ENABLE_SEARCH_ENGINE_CHOICE)
 TEST_P(SearchEnginesHandlerParametrizedTest,
        SettingTheDefaultSearchEngineRecordsHistogram) {
   base::Value::List first_call_args;
@@ -269,4 +271,5 @@ TEST_F(SearchEnginesHandlerTestWithSearchEngineChoiceEnabled,
       search_engines::kSearchEngineChoiceScreenDefaultSearchEngineTypeHistogram,
       SearchEngineType::SEARCH_ENGINE_BING, 1);
 }
+#endif
 }  // namespace settings
