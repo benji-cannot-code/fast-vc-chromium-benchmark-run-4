8728bd88fc6b1abe25dc4c230bfb7f66d6072a8b
Norge Vizcay <vizcay@google.com>
2023-10-31 18:39:11 +0000

    [AutofillDataModel] Introduce new apartment field types.
    As part of our i18n efforts in Autofill, this CL introduces 2 new
    Apartment related field types.
    
    Bug: 1464568
    Change-Id: I36bccdb4be5521af36707fa2dfb72764455c366e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4980507
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217775}

--- components/autofill/core/browser/field_types.cc
+++ components/autofill/core/browser/field_types.cc
@@ -51,7 +51,9 @@ static constexpr auto kTypeNameToFieldType =
          {"PHONE_HOME_WHOLE_NUMBER", PHONE_HOME_WHOLE_NUMBER},
          {"ADDRESS_HOME_LINE1", ADDRESS_HOME_LINE1},
          {"ADDRESS_HOME_LINE2", ADDRESS_HOME_LINE2},
+         {"ADDRESS_HOME_APT", ADDRESS_HOME_APT},
          {"ADDRESS_HOME_APT_NUM", ADDRESS_HOME_APT_NUM},
+         {"ADDRESS_HOME_APT_TYPE", ADDRESS_HOME_APT_TYPE},
          {"ADDRESS_HOME_CITY", ADDRESS_HOME_CITY},
          {"ADDRESS_HOME_STATE", ADDRESS_HOME_STATE},
          {"ADDRESS_HOME_ZIP", ADDRESS_HOME_ZIP},
@@ -161,7 +163,9 @@ bool IsFillableFieldType(ServerFieldType field_type) {
     case ADDRESS_HOME_LINE1:
     case ADDRESS_HOME_LINE2:
     case ADDRESS_HOME_LINE3:
+    case ADDRESS_HOME_APT:
     case ADDRESS_HOME_APT_NUM:
+    case ADDRESS_HOME_APT_TYPE:
     case ADDRESS_HOME_CITY:
     case ADDRESS_HOME_STATE:
     case ADDRESS_HOME_ZIP:
@@ -389,8 +393,12 @@ std::string_view FieldTypeToDeveloperRepresentationString(
       return "Sorting code";
     case ADDRESS_HOME_DEPENDENT_LOCALITY:
       return "Dependent locality";
+    case ADDRESS_HOME_APT:
+      return "Apt";
     case ADDRESS_HOME_APT_NUM:
       return "Apt num";
+    case ADDRESS_HOME_APT_TYPE:
+      return "Apt type";
     case ADDRESS_HOME_CITY:
       return "City";
     case ADDRESS_HOME_STATE:
@@ -489,7 +497,9 @@ FieldTypeGroup GroupTypeOfServerFieldType(ServerFieldType field_type) {
     case ADDRESS_HOME_LINE1:
     case ADDRESS_HOME_LINE2:
     case ADDRESS_HOME_LINE3:
+    case ADDRESS_HOME_APT:
     case ADDRESS_HOME_APT_NUM:
+    case ADDRESS_HOME_APT_TYPE:
     case ADDRESS_HOME_CITY:
     case ADDRESS_HOME_STATE:
     case ADDRESS_HOME_ZIP:
