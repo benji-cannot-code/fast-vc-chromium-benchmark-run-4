02579e870a24ba5b3fbdfedbd2b3c6053bd2dbb7
Sean Kau <skau@chromium.org>
2023-10-31 20:31:20 +0000

    Migrate /chrome/browser/ash/printing off of accent_color
    accent_color does not handle dynamic color. As a result, we favor
    accent_color_id. Callers which do not explicitly need SkColor
    support are migrated.
    
    This CL was uploaded by git cl split.
    
    R=pawliczek@chromium.org
    
    Bug: b:308208767, b:291622042
    Change-Id: Iba03a11cc221b8f3eba379e038ee58aa75416ac8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4988844
    Reviewed-by: Piotr Pawliczek <pawliczek@chromium.org>
    Commit-Queue: Piotr Pawliczek <pawliczek@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217852}

--- chrome/browser/ash/printing/cups_print_job_notification.cc
+++ chrome/browser/ash/printing/cups_print_job_notification.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/grit/generated_resources.h"
-#include "chromeos/constants/chromeos_features.h"
 #include "components/prefs/pref_service.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/chromeos/styles/cros_tokens_color_mappings.h"
@@ -237,32 +236,19 @@ void CupsPrintJobNotification::UpdateNotificationIcon() {
     case CupsPrintJob::State::STATE_PAGE_DONE:
     case CupsPrintJob::State::STATE_SUSPENDED:
     case CupsPrintJob::State::STATE_RESUMED: {
-      if (chromeos::features::IsJellyEnabled()) {
-        notification_->set_accent_color_id(cros_tokens::kCrosSysPrimary);
-      } else {
-        notification_->set_accent_color(kSystemNotificationColorNormal);
-      }
+      notification_->set_accent_color_id(cros_tokens::kCrosSysPrimary);
       notification_->set_vector_small_image(kNotificationPrintingIcon);
       break;
     }
     case CupsPrintJob::State::STATE_DOCUMENT_DONE: {
-      if (chromeos::features::IsJellyEnabled()) {
-        notification_->set_accent_color_id(cros_tokens::kCrosSysPrimary);
-      } else {
-        notification_->set_accent_color(kSystemNotificationColorNormal);
-      }
+      notification_->set_accent_color_id(cros_tokens::kCrosSysPrimary);
       notification_->set_vector_small_image(kNotificationPrintingDoneIcon);
       break;
     }
     case CupsPrintJob::State::STATE_CANCELLED:
     case CupsPrintJob::State::STATE_FAILED:
     case CupsPrintJob::State::STATE_ERROR: {
-      if (chromeos::features::IsJellyEnabled()) {
-        notification_->set_accent_color_id(cros_tokens::kCrosSysError);
-      } else {
-        notification_->set_accent_color(
-            kSystemNotificationColorCriticalWarning);
-      }
+      notification_->set_accent_color_id(cros_tokens::kCrosSysError);
       notification_->set_vector_small_image(kNotificationPrintingWarningIcon);
       break;
     }
