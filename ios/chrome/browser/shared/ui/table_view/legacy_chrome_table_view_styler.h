f74e2a43ddb7e3ffbb0c53ca2a17b6d8e692129e
Gauthier Ambard <gambard@chromium.org>
2023-10-31 12:01:29 +0000

    [iOS] Create LegacyChromeTableViewController file
    The goal is to prevent new use of this class to be added.
    For now this is a temporary file for downstream compatibility.
    
    Bug: 1497919
    Change-Id: I6d9d0a68baa040cbef2a2833628205dead7ffad0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4992901
    Commit-Queue: Louis Romero <lpromero@google.com>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217501}

---
+++ ios/chrome/browser/shared/ui/table_view/legacy_chrome_table_view_styler.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SHARED_UI_TABLE_VIEW_LEGACY_CHROME_TABLE_VIEW_STYLER_H_
+#define IOS_CHROME_BROWSER_SHARED_UI_TABLE_VIEW_LEGACY_CHROME_TABLE_VIEW_STYLER_H_
+
+// Temporary include.
+#import "ios/chrome/browser/shared/ui/table_view/chrome_table_view_styler.h"
+
+#endif  // IOS_CHROME_BROWSER_SHARED_UI_TABLE_VIEW_LEGACY_CHROME_TABLE_VIEW_STYLER_H_
