e8ea5a29552f4b6e91cf31e99a3b1b3553cbe7dd
manukh <manukh@chromium.org>
2023-10-31 21:11:39 +0000

    [omnibox][memories][journeys] Re-adds rows inheriting search scores.
    Re-adds `omnibox_history_cluster_provider_inherit_search_match_score`
    param.
    
    It was removed in crrev.com/c/4580910 because it performed poorly in
    beta. We never tried it in stable. There's renewed interest in it, so
    re-adding it to try again.
    
    When enabled, the journey row will inherit the score of its associated
    search suggestion minus 1. That will usually make the journey rank
    immediately below the search suggestion which will hopefully avoid users
    accidentally clicking the journey row when they intended the search
    suggestion.
    
    Bug: 1327076
    Change-Id: I3b6289b6c890f2126308b6a57d14ab56a325f473
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4995836
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Auto-Submit: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1217878}

--- components/history_clusters/core/config.cc
+++ components/history_clusters/core/config.cc
@@ -169,6 +169,12 @@ Config::Config() {
             "omnibox_history_cluster_provider_score",
             omnibox_history_cluster_provider_score);
 
+    omnibox_history_cluster_provider_inherit_search_match_score =
+        base::GetFieldTrialParamByFeatureAsBool(
+            internal::kOmniboxHistoryClusterProvider,
+            "omnibox_history_cluster_provider_inherit_search_match_score",
+            omnibox_history_cluster_provider_inherit_search_match_score);
+
     omnibox_history_cluster_provider_navigation_intent_score_threshold =
         base::GetFieldTrialParamByFeatureAsInt(
             internal::kOmniboxHistoryClusterProvider,
