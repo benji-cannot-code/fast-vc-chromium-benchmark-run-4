faa4caba50e7855f5a6c9ef9564fd43f8ec4c7b0
Calder Kitagawa <ckitagawa@chromium.org>
2023-10-31 17:41:47 +0000

    [Hub] Add shrink and expand animation support
    Create shrink and expand animations for Hub. These are borrowed from
    TabSwitcherLayout, but the implementation is much more ergonomic.
    
    The ShrinkExpandHubLayoutAnimatorProvider owns and attaches a
    ShrinkExpandImageView to the HubContainerView. This view is initially
    invisible.
    
    If a bitmap is needed a bitmap callback is created that will set the
    image on the ShrinkExpandImageView when ready. This callback uses weak
    references to prevent retaining refs to the view or the animator
    provider in the event it gets held in native for a prolonged duration
    and the fallback animator runs.
    
    The animator provider waits for the bitmap and some asynchronously
    supplied data regarding initial and final rects for the animation to
    be supplied. The external caller can also cancel the animation to use
    the fallback through this supplier. This is a holdover from the
    TabSwitcherLayout which must skip an animation is the
    TabListRecyclerView has too much work to do and can hopefully be
    removed in the future.
    
    Once the animator provider has all the data it will supply the
    Animator. Unless a fallback animation is requested beforehand in which
    case a fade animation will be provided for the shrink/expand case or
    the animation will proceed in the case of the new tab animation.
    
    Bug: 1492207
    Change-Id: I66fcb01ced8e6fe10a01f8b68263eef54916cbff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4975965
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1217717}

--- chrome/browser/hub/android/java/src/org/chromium/chrome/browser/hub/ShrinkExpandAnimator.java
+++ chrome/browser/hub/android/java/src/org/chromium/chrome/browser/hub/ShrinkExpandAnimator.java
@@ -49,8 +49,9 @@ public class ShrinkExpandAnimator {
         assert mView.getTranslationX() == 0.0f;
         assert mView.getTranslationY() == 0.0f;
 
-        mInitialRect = initialRect;
-        mFinalRect = finalRect;
+        // Copy these rects to ensure they aren't re-used.
+        mInitialRect = new Rect(initialRect);
+        mFinalRect = new Rect(finalRect);
         mImageMatrix = new Matrix();
     }
 
