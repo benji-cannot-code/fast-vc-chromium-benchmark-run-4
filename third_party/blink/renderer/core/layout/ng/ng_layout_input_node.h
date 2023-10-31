bf3bd02ddc80368d70daf338fcb7d425310afbd7
Morten Stenshorne <mstensho@chromium.org>
2023-10-31 23:02:40 +0000

    When comparing NGLayoutInputNodes, check type as well.
    Previously, an InlineNode and an NGBlockNode would be considered equal,
    as long as the LayoutObjects were the same.
    
    No idea if this fixes anything, but it sure didn't look good.
    
    Change-Id: I2c04644e825dd896b20ae21c474d28703516ab44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994304
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217951}

--- third_party/blink/renderer/core/layout/ng/ng_layout_input_node.h
+++ third_party/blink/renderer/core/layout/ng/ng_layout_input_node.h
@@ -303,7 +303,7 @@ class CORE_EXPORT NGLayoutInputNode {
   explicit operator bool() const { return box_ != nullptr; }
 
   bool operator==(const NGLayoutInputNode& other) const {
-    return box_ == other.box_;
+    return box_ == other.box_ && type_ == other.type_;
   }
 
   bool operator!=(const NGLayoutInputNode& other) const {
