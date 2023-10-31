f1548da8c7875dff7bc29b6fbc39ed3d014322d7
Alex Ilin <alexilin@chromium.org>
2023-10-31 12:31:26 +0000

    [DBSC] New feature flag for the refresh token binding
    Require a new "EnableChromeRefreshTokenBinding" feature flag to enable
    the LST binding.
    
    "EnableBoundSessionCredentials" must also be enabled in order to
    activate an UnexportableKeyService. This dependency could be
    eliminated but we don't have plans to launch the LST binding before the
    cookie binding, so this is unnecessary.
    
    Bug: b/308552208
    Change-Id: Ibeff5c331dd611b2102ecd9a12cd8fe554c83613
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4993545
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217509}

--- chrome/browser/signin/dice_response_handler.cc
+++ chrome/browser/signin/dice_response_handler.cc
@@ -86,12 +86,6 @@ enum DiceTokenFetchResult {
 };
 
 #if BUILDFLAG(ENABLE_BOUND_SESSION_CREDENTIALS)
-bool IsBoundSessionCredentialsEnabledForDice() {
-  return switches::IsBoundSessionCredentialsEnabled() &&
-         switches::kEnableBoundSessionCredentialsDiceSupport.Get() ==
-             switches::EnableBoundSessionCredentialsDiceSupport::kEnabled;
-}
-
 std::unique_ptr<RegistrationTokenHelper> BuildRegistrationTokenHelper(
     unexportable_keys::UnexportableKeyService& unexportable_key_service,
     base::StringPiece client_id,
@@ -111,7 +105,7 @@ CreateRegistrationTokenHelperFactory(
     return {};
   }
 
-  if (!IsBoundSessionCredentialsEnabledForDice()) {
+  if (!switches::IsChromeRefreshTokenBindingEnabled()) {
     return {};
   }
 
@@ -211,7 +205,7 @@ DiceResponseHandler::DiceTokenFetcher::DiceTokenFetcher(
       std::make_unique<AccountReconcilor::Lock>(account_reconcilor);
 #if BUILDFLAG(ENABLE_BOUND_SESSION_CREDENTIALS)
   if (!registration_token_helper_factory.is_null()) {
-    CHECK(IsBoundSessionCredentialsEnabledForDice());
+    CHECK(switches::IsChromeRefreshTokenBindingEnabled());
     StartBindingKeyGeneration(registration_token_helper_factory);
     // Wait until the binding key is generated before fetching a token.
     return;
@@ -237,7 +231,8 @@ void DiceResponseHandler::DiceTokenFetcher::OnClientOAuthSuccess(
   gaia_auth_fetcher_.reset();
   timeout_closure_.Cancel();
 #if BUILDFLAG(ENABLE_BOUND_SESSION_CREDENTIALS)
-  if (!IsBoundSessionCredentialsEnabledForDice() || !result.is_bound_to_key) {
+  if (!switches::IsChromeRefreshTokenBindingEnabled() ||
+      !result.is_bound_to_key) {
     // Pass an empty binding key if conditions don't apply. This key won't be
     // needed for anything else, so we can just clear it in place.
     wrapped_binding_key_.clear();
@@ -282,7 +277,7 @@ void DiceResponseHandler::DiceTokenFetcher::StartTokenFetch() {
 #if BUILDFLAG(ENABLE_BOUND_SESSION_CREDENTIALS)
 void DiceResponseHandler::DiceTokenFetcher::StartBindingKeyGeneration(
     const RegistrationTokenHelperFactory& registration_token_helper_factory) {
-  CHECK(IsBoundSessionCredentialsEnabledForDice());
+  CHECK(switches::IsChromeRefreshTokenBindingEnabled());
   // `base::Unretained()` is safe because `this` owns
   // `registration_token_helper_`.
   registration_token_helper_ = registration_token_helper_factory.Run(
@@ -295,7 +290,7 @@ void DiceResponseHandler::DiceTokenFetcher::StartBindingKeyGeneration(
 
 void DiceResponseHandler::DiceTokenFetcher::OnRegistrationTokenGenerated(
     absl::optional<RegistrationTokenHelper::Result> result) {
-  CHECK(IsBoundSessionCredentialsEnabledForDice());
+  CHECK(switches::IsChromeRefreshTokenBindingEnabled());
   if (result.has_value()) {
     binding_registration_token_ = std::move(result->registration_token);
     wrapped_binding_key_ = std::move(result->wrapped_binding_key);
@@ -380,7 +375,7 @@ void DiceResponseHandler::SetTaskRunner(
 #if BUILDFLAG(ENABLE_BOUND_SESSION_CREDENTIALS)
 void DiceResponseHandler::SetRegistrationTokenHelperFactoryForTesting(
     RegistrationTokenHelperFactory factory) {
-  CHECK(IsBoundSessionCredentialsEnabledForDice());
+  CHECK(switches::IsChromeRefreshTokenBindingEnabled());
   registration_token_helper_factory_ = std::move(factory);
 }
 #endif  // BUILDFLAG(ENABLE_BOUND_SESSION_CREDENTIALS)
