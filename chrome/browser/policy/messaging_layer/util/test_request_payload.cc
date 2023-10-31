4f4a32bf071a768622248b9f97611d542757cca6
Josh Hilke <jrhilke@google.com>
2023-10-31 23:59:33 +0000

    Verify generation guid in record building unit tests.
    The `generationGuid` field in `UploadEncryptedReportingRequestBuilder`
    should be verified in unit tests that build record requests. Also, add a
    helper function for improved readability to determine whether generation
    guid is required.
    
    Bug: b:305038805
    Change-Id: Ibdb35f3697755ce6d916460d6bc50703cb3e08e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4990382
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Josh Hilke <jrhilke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1217987}

--- chrome/browser/policy/messaging_layer/util/test_request_payload.cc
+++ chrome/browser/policy/messaging_layer/util/test_request_payload.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_reader.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/string_util.h"
+#include "chrome/browser/policy/messaging_layer/upload/record_upload_request_builder.h"
 #include "third_party/abseil-cpp/absl/strings/ascii.h"
 
 namespace reporting {
@@ -401,6 +402,19 @@ bool SequenceInformationRecordMatcher::MatchAndExplainRecord(
       return false;
     }
   }
+
+#if BUILDFLAG(IS_CHROMEOS)
+  if (SequenceInformationDictionaryBuilder::GenerationGuidIsRequired()) {
+    const auto* generation_guid =
+        sequence_information->FindString("generationGuid");
+    if ((!generation_guid || generation_guid->empty())) {
+      *listener << "No key named \"sequenceInformation/generationGuid\" or the "
+                   "value is not a string in record "
+                << record << '.';
+      return false;
+    }
+  }
+#endif  // BUILDFLAG(IS_CHROMEOS)
   return true;
 }
 
