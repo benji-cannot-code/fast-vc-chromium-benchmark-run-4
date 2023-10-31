4104c99e12042a8f56f42331ae4939ba3d1b3c0a
Etienne Pierre-Doray <etiennep@chromium.org>
2023-10-31 16:04:57 +0000

    [tracing] Fix for perfetto client library on win
    - Use PlatformThread::SetName in TraceEventDataSourceTest;
      ThreadIdNameManager doesn't propagate the name to the OS
    - Pass argument to TestTraceProcessorImpl::OverrideSqlModule
      by reference; passing object by moved copy (alloc-ed) across dll
      boundary doesn't work on windows because the dll is destroying
      an object that was alloc'ed in the caller's memory space.
    
    Bug: 1497783
    Change-Id: I995b92b13a04567a975711ea2cf4c0115d68fe2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4987237
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217626}

--- services/tracing/public/cpp/perfetto/trace_event_data_source_unittest.cc
+++ services/tracing/public/cpp/perfetto/trace_event_data_source_unittest.cc
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/common/task_annotator.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/task/thread_pool.h"
-#include "base/threading/thread_id_name_manager.h"
+#include "base/threading/platform_thread.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
@@ -102,9 +102,8 @@ class TraceEventDataSourceTest
     perfetto::internal::TrackRegistry::InitializeInstance();
 #endif  // !BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
 
-    old_thread_name_ =
-        base::ThreadIdNameManager::GetInstance()->GetNameForCurrentThread();
-    base::ThreadIdNameManager::GetInstance()->SetName(kTestThread);
+    old_thread_name_ = base::PlatformThread::GetName();
+    base::PlatformThread::SetName(kTestThread);
     old_process_name_ = base::test::CurrentProcessForTest::GetName();
     old_process_type_ = base::test::CurrentProcessForTest::GetType();
     base::CurrentProcess::GetInstance().SetProcessNameAndType(kTestProcess,
@@ -147,7 +146,7 @@ class TraceEventDataSourceTest
     producer_client_.reset();
 #endif  // !BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
 
-    base::ThreadIdNameManager::GetInstance()->SetName(old_thread_name_);
+    base::PlatformThread::SetName(kTestThread);
     base::CurrentProcess::GetInstance().SetProcessNameAndType(
         old_process_name_, old_process_type_);
 
