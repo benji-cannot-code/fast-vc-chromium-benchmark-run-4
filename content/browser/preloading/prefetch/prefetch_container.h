0ebcfdcc1b893bfa281dddae9a11e710c05f2c5a
Hiroshige Hayashizaki <hiroshige@chromium.org>
2023-10-31 20:20:17 +0000

    Move PrefetchDocumentManager::MatchUrl() into PrefetchService
    As the primary code path for No-Vary-Search matching was moved
    to `PrefetchService::all_prefetches_` (crbug.com/1448731),
    this CL also moves `MatchUrl()` and
    `GetAllForUrlWithoutRefAndQueryForTesting()`
    to `PrefetchService::all_prefetches_`.
    
    Bug: 1448731
    Change-Id: I982c9c921c44357417ca3e6b5dc4239a910486b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4961996
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217846}

--- content/browser/preloading/prefetch/prefetch_container.h
+++ content/browser/preloading/prefetch/prefetch_container.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/preloading/speculation_host_devtools_observer.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/global_routing_id.h"
+#include "content/public/browser/preloading_data.h"
 #include "net/http/http_no_vary_search_data.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -90,6 +91,8 @@ struct PrefetchResponseSizes {
 // `PrefetchService::MakePrefetchRequest()`.
 class CONTENT_EXPORT PrefetchContainer {
  public:
+  // When `matcher` is null (only in unit tests),
+  // `PreloadingData::GetSameURLMatcher` is used.
   PrefetchContainer(
       const GlobalRenderFrameHostId& referring_render_frame_host_id,
       const blink::DocumentToken& referring_document_token,
@@ -98,7 +101,8 @@ class CONTENT_EXPORT PrefetchContainer {
       const blink::mojom::Referrer& referrer,
       absl::optional<net::HttpNoVarySearchData> no_vary_search_expected,
       blink::mojom::SpeculationInjectionWorld world,
-      base::WeakPtr<PrefetchDocumentManager> prefetch_document_manager);
+      base::WeakPtr<PrefetchDocumentManager> prefetch_document_manager,
+      PreloadingURLMatchCallback matcher = {});
   ~PrefetchContainer();
 
   PrefetchContainer(const PrefetchContainer&) = delete;
--- content/browser/preloading/prefetch/prefetch_service.cc
+++ content/browser/preloading/prefetch/prefetch_service.cc
@@ -1523,6 +1523,18 @@ std::vector<PrefetchContainer*> PrefetchService::FindPrefetchContainerToServe(
   return matches;
 }
 
+base::WeakPtr<PrefetchContainer> PrefetchService::MatchUrl(
+    const PrefetchContainer::Key& key) const {
+  return no_vary_search::MatchUrl(key, all_prefetches_);
+}
+
+std::vector<std::pair<GURL, base::WeakPtr<PrefetchContainer>>>
+PrefetchService::GetAllForUrlWithoutRefAndQueryForTesting(
+    const PrefetchContainer::Key& key) const {
+  return no_vary_search::GetAllForUrlWithoutRefAndQueryForTesting(
+      key, all_prefetches_);
+}
+
 PrefetchService::HandlePrefetchContainerResult
 PrefetchService::HandlePrefetchContainerToServe(
     const PrefetchContainer::Key& key,
@@ -1798,6 +1810,10 @@ void PrefetchService::SetURLLoaderFactoryForTesting(
 void PrefetchService::SetNetworkContextForProxyLookupForTesting(
     network::mojom::NetworkContext* network_context) {
   g_network_context_for_proxy_lookup_for_testing = network_context;
+}
+
+base::WeakPtr<PrefetchService> PrefetchService::GetWeakPtr() {
+  return weak_method_factory_.GetWeakPtr();
 }
 
 void PrefetchService::RecordExistingPrefetchWithMatchingURL(
