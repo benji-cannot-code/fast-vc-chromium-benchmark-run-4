2ff210a3a6578f30b6f1132b474009e4b1e08a9b
Amy Huang <akhuang@google.com>
2023-10-31 00:56:23 +0000

    Header cleanup: add include <vector> because file uses std::vector.
    Bug: 1497475
    Change-Id: Id6a03cfdb90f05b56c0bd6cffe0dc5ba9852203c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4990275
    Commit-Queue: Amy Huang <akhuang@google.com>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Amy Huang <akhuang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1217339}

--- components/webauthn/android/webauthn_client_android.h
+++ components/webauthn/android/webauthn_client_android.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_WEBAUTHN_ANDROID_WEBAUTHN_CLIENT_ANDROID_H_
 
 #include <memory>
+#include <vector>
 
 #include "base/functional/callback_forward.h"
 
