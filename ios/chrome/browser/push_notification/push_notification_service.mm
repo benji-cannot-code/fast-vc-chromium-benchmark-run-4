4adecc9596219bb59eaca425454d8c2f889b4241
Daniel White <danieltwhite@google.com>
2023-10-31 13:54:40 +0000

    Reland "[iOS] Added Push Notification Authorization Status Change Metric"
    The original CL was causing the app to crash on start up because the
    CL was attempting to access the ApplicationContext before it had been
    created. This CL addresses this issue by moving the code's invocation
    further down the application's startup process.
    
    Bug: 1495399
    
    This is a reland of commit 859c954d050b2f06b6c578f587e80d32a4e4a86c
    
    Original change's description:
    > [iOS] Added Push Notification Authorization Status Change Metric
    >
    > In this CL, two new push notification metrics were added that track when
    > users switch their push notification permissions to either authorized or
    > denied.
    >
    > Bug: 1487295
    > Change-Id: I69a9328dbf136ad31a20cbce673ba36ee3c16385
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4942299
    > Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    > Reviewed-by: Ali Juma <ajuma@chromium.org>
    > Commit-Queue: Daniel White <danieltwhite@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1210915}
    
    Bug: 1487295
    Change-Id: Ie2e61055c57e3773bf93bf22aa0026cc65006fb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4952883
    Reviewed-by: Éric Noyau <noyau@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217544}

--- ios/chrome/browser/push_notification/push_notification_service.mm
+++ ios/chrome/browser/push_notification/push_notification_service.mm
@@ -9,9 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/strings/sys_string_conversions.h"
 #import "base/values.h"
 #import "components/pref_registry/pref_registry_syncable.h"
+#import "components/prefs/pref_registry_simple.h"
 #import "ios/chrome/browser/push_notification/push_notification_account_context_manager.h"
 #import "ios/chrome/browser/push_notification/push_notification_client_id.h"
 #import "ios/chrome/browser/push_notification/push_notification_client_manager.h"
+#import "ios/chrome/browser/push_notification/push_notification_util.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 
@@ -76,6 +78,15 @@ void PushNotificationService::UnregisterAccount(
 void PushNotificationService::RegisterBrowserStatePrefs(
     user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterDictionaryPref(prefs::kFeaturePushNotificationPermissions);
+}
+
+void PushNotificationService::RegisterLocalStatePrefs(
+    PrefRegistrySimple* registry) {
+  registry->RegisterIntegerPref(
+      prefs::kPushNotificationAuthorizationStatus,
+      static_cast<int>(
+          push_notification::PushNotificationSettingsAuthorizationStatus::
+              NOTDETERMINED));
 }
 
 void PushNotificationService::SetPreferences(
