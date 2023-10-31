9a117aeb170e09f497b3a4099071db08fbf21afb
Oliver Dunk <oliverdunk@chromium.org>
2023-10-31 09:16:48 +0000

    Use warning when background.scripts is specified in MV3
    For the background.scripts, background.page and background.persistent
    keys, avoid a hard-error when specifying them in MV3 and instead show a
    manifest warning. This improves compatibility across browsers:
    https://github.com/w3c/webextensions/issues/282#issuecomment-1443396585
    
    Bug: 1418934
    Change-Id: I3a10a0afc4472f3ed80f5e1eef16d13cd6c87412
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4979885
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Oliver Dunk <oliverdunk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217465}

--- chrome/common/extensions/manifest_tests/extension_manifests_platformapp_unittest.cc
+++ chrome/common/extensions/manifest_tests/extension_manifests_platformapp_unittest.cc
@@ -18,6 +18,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace extensions {
 
+namespace {
+
+static constexpr char kBackgroundDisallowedWarning[] =
+    "'background' is only allowed for extensions, legacy packaged "
+    "apps, hosted apps, login screen extensions, and chromeos "
+    "system extensions, but this is a packaged app.";
+
+static constexpr char kBackgroundScriptsDisallowedWarning[] =
+    "'background.scripts' is only allowed for extensions, legacy packaged "
+    "apps, hosted apps, login screen extensions, and chromeos system "
+    "extensions, but this is a packaged app.";
+
+static constexpr char kBackgroundPageDisallowedWarning[] =
+    "'background.page' is only allowed for extensions, legacy packaged "
+    "apps, hosted apps, login screen extensions, and chromeos system "
+    "extensions, but this is a packaged app.";
+
+}  // namespace
+
 namespace errors = manifest_errors;
 namespace keys = manifest_keys;
 
@@ -55,14 +74,6 @@ TEST_F(PlatformAppsManifestTest, PlatformApps) {
           "init_invalid_platform_app_1.json",
           "'app.launch' is only allowed for legacy packaged apps and hosted "
           "apps, but this is a packaged app."),
-      Testcase("init_invalid_platform_app_4.json",
-               "'background' is only allowed for extensions, legacy packaged "
-               "apps, hosted apps, login screen extensions, and chromeos "
-               "system extensions, but this is a packaged app."),
-      Testcase("init_invalid_platform_app_5.json",
-               "'background' is only allowed for extensions, legacy packaged "
-               "apps, hosted apps, login screen extensions, and chromeos "
-               "system extensions, but this is a packaged app."),
       Testcase("incognito_invalid_platform_app.json",
                "'incognito' is only allowed for extensions and legacy packaged "
                "apps, "
@@ -70,6 +81,13 @@ TEST_F(PlatformAppsManifestTest, PlatformApps) {
   };
   RunTestcases(warning_testcases, std::size(warning_testcases),
                EXPECT_TYPE_WARNING);
+
+  LoadAndExpectWarnings(
+      "init_invalid_platform_app_4.json",
+      {kBackgroundDisallowedWarning, kBackgroundScriptsDisallowedWarning});
+  LoadAndExpectWarnings(
+      "init_invalid_platform_app_5.json",
+      {kBackgroundDisallowedWarning, kBackgroundPageDisallowedWarning});
 }
 
 TEST_F(PlatformAppsManifestTest, PlatformAppContentSecurityPolicy) {
