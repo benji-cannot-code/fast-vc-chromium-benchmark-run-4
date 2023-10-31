968f74cc117a2703dddd423a07acef87c869bfdf
David Maunder <davidjm@google.com>
2023-10-31 18:11:59 +0000

    Cleanup FlatBuffer files when migration experiment is turned off
    This ensures we don't attempt Tab restoration with out of date
    FlatBuffer files when the experiment is turned back on.
    
    Bug: b/308432569
    Change-Id: I56ace690384c3eca0172497a18575452b49b2ef6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4987662
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217743}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabPersistentStore.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabPersistentStore.java
@@ -1491,6 +1491,7 @@ public class TabPersistentStore {
                     }
                 });
         performPersistedTabDataMaintenance(null);
+        TabStateFileManager.cleanupUnusedFiles(getStateDirectory());
     }
 
     @VisibleForTesting
