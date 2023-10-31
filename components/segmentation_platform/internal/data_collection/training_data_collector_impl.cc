7e5a20659e46864972303500445517e8121947e7
Hailey Wang <haileywang@google.com>
2023-10-31 00:51:49 +0000

    [TDC] Add vlog to tdc for debugging
    Add a vlog for every uma metric record.
    
    Bug: b/300300785
    Change-Id: Id7c4f1fc80f8d7d08153139feaf5fb4d092c863b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4986302
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1217336}

--- components/segmentation_platform/internal/data_collection/training_data_collector_impl.cc
+++ components/segmentation_platform/internal/data_collection/training_data_collector_impl.cc
@@ -314,8 +314,6 @@ bool TrainingDataCollectorImpl::CanReportTrainingData(
     RecordTrainingDataCollectionEvent(
         segment_info.segment_id(),
         stats::TrainingDataCollectionEvent::kModelInfoMissing);
-    VLOG(1) << "Upload skipped due to model version "
-            << proto::SegmentId_Name(segment_info.segment_id());
     return false;
   }
 
@@ -337,8 +335,6 @@ bool TrainingDataCollectorImpl::CanReportTrainingData(
     RecordTrainingDataCollectionEvent(
         segment_info.segment_id(),
         stats::TrainingDataCollectionEvent::kPartialDataNotAllowed);
-    VLOG(1) << "Upload skipped due to consent "
-            << proto::SegmentId_Name(segment_info.segment_id());
     return false;
   }
 
@@ -359,8 +355,6 @@ bool TrainingDataCollectorImpl::CanReportTrainingData(
       RecordTrainingDataCollectionEvent(
           segment_info.segment_id(),
           stats::TrainingDataCollectionEvent::kNotEnoughCollectionTime);
-      VLOG(1) << "Upload skipped due to new model "
-              << proto::SegmentId_Name(segment_info.segment_id());
       return false;
     }
   }
@@ -371,8 +365,6 @@ bool TrainingDataCollectorImpl::CanReportTrainingData(
     RecordTrainingDataCollectionEvent(
         segment_info.segment_id(),
         stats::TrainingDataCollectionEvent::kNotEnoughCollectionTime);
-    VLOG(1) << "Upload skipped due to missing signals "
-            << proto::SegmentId_Name(segment_info.segment_id());
     return false;
   }
 
@@ -434,8 +426,6 @@ void TrainingDataCollectorImpl::OnGetTrainingTensors(
       segment_info.segment_id(), segment_info.model_version(), input_tensors,
       output_values, output_indexes, prediction_result, selected_segment);
   if (ukm_source_id == ukm::kInvalidSourceId) {
-    VLOG(1) << "Failed to collect training data for segment:"
-            << segment_info.segment_id();
     RecordTrainingDataCollectionEvent(
         segment_info.segment_id(),
         stats::TrainingDataCollectionEvent::kUkmReportingFailed);
@@ -532,14 +522,10 @@ void TrainingDataCollectorImpl::OnGetSegmentInfoAtDecisionTime(
     scoped_refptr<InputContext> input_context,
     const proto::SegmentInfo& segment_info,
     absl::optional<ModelProvider::Request> inputs) {
+  TrainingTimings training_request = ComputeDecisionTiming(segment_info);
   if (!CanReportTrainingData(segment_info, /*include_outputs*/ false)) {
-    RecordTrainingDataCollectionEvent(
-        segment_id,
-        stats::TrainingDataCollectionEvent::kDisallowedForRecording);
     return;
   }
-
-  TrainingTimings training_request = ComputeDecisionTiming(segment_info);
 
   if (type != segment_info.model_metadata()
                   .training_outputs()
@@ -637,6 +623,7 @@ void TrainingDataCollectorImpl::OnGetTrainingTensorsAtDecisionTime(
             request_id, segment_info,
             *training_request.observation_delayed_task,
             stats::TrainingDataCollectionEvent::kDelayedTaskPosted);
+        VLOG(1) << "Delayed task posted for " << segment_info.segment_id();
       } else {
         RecordTrainingDataCollectionEvent(
             segment_info.segment_id(),
@@ -644,8 +631,6 @@ void TrainingDataCollectorImpl::OnGetTrainingTensorsAtDecisionTime(
       }
     }
   } else {
-    VLOG(1) << "Observation without timeout "
-            << proto::SegmentId_Name(segment_info.segment_id());
     RecordTrainingDataCollectionEvent(
         segment_info.segment_id(),
         stats::TrainingDataCollectionEvent::kWaitingForNonDelayedTrigger);
--- components/segmentation_platform/internal/stats.cc
+++ components/segmentation_platform/internal/stats.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/segmentation_platform/internal/stats.h"
 
+#include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
@@ -643,12 +644,67 @@ void RecordTooManyInputTensors(int tensor_size) {
       tensor_size);
 }
 
+std::string TrainingDataCollectionEventToErrorMsg(
+    TrainingDataCollectionEvent event) {
+  switch (event) {
+    case TrainingDataCollectionEvent::kImmediateCollectionStart:
+      return "Immediate Collection Start";
+    case TrainingDataCollectionEvent::kImmediateCollectionSuccess:
+      return "Immediate Collection Success";
+    case TrainingDataCollectionEvent::kModelInfoMissing:
+      return "Model Info Missing";
+    case TrainingDataCollectionEvent::kMetadataValidationFailed:
+      return "Metadata Validation Failed";
+    case TrainingDataCollectionEvent::kGetInputTensorsFailed:
+      return "Get Input Tensors Failed";
+    case TrainingDataCollectionEvent::kNotEnoughCollectionTime:
+      return "Not Enough Collection Time";
+    case TrainingDataCollectionEvent::kUkmReportingFailed:
+      return "UKM Reporting Failed";
+    case TrainingDataCollectionEvent::kPartialDataNotAllowed:
+      return "Partial Data Not Allowed";
+    case TrainingDataCollectionEvent::kContinousCollectionStart:
+      return "Continuous Collection Start";
+    case TrainingDataCollectionEvent::kContinousCollectionSuccess:
+      return "Continuous Collection Success";
+    case TrainingDataCollectionEvent::kCollectAndStoreInputsSuccess:
+      return "Collect and Store Inputs Success";
+    case TrainingDataCollectionEvent::kObservationTimeReached:
+      return "Observation Time Reached";
+    case TrainingDataCollectionEvent::kDelayedTaskPosted:
+      return "Delayed Task Posted";
+    case TrainingDataCollectionEvent::kImmediateObservationPosted:
+      return "Immediate Observation Posted";
+    case TrainingDataCollectionEvent::kWaitingForNonDelayedTrigger:
+      return "Waiting for Non Delayed Trigger";
+    case TrainingDataCollectionEvent::kHistogramTriggerHit:
+      return "Histogram Trigger Hit";
+    case TrainingDataCollectionEvent::kNoSegmentInfo:
+      return "No Segment Info";
+    case TrainingDataCollectionEvent::kDisallowedForRecording:
+      return "Disallowed for Recording";
+    case TrainingDataCollectionEvent::kObservationDisallowed:
+      return "Observation Disallowed";
+    case TrainingDataCollectionEvent::kTrainingDataMissing:
+      return "Training Data Missing";
+    case TrainingDataCollectionEvent::kOnDecisionTimeTypeMistmatch:
+      return "On Decision Time Type Mismatch";
+    case TrainingDataCollectionEvent::kDelayTriggerSampled:
+      return "Delay Trigger Sampled";
+    default:
+      return "";
+  }
+}
+
 void RecordTrainingDataCollectionEvent(SegmentId segment_id,
                                        TrainingDataCollectionEvent event) {
   base::UmaHistogramEnumeration(
       "SegmentationPlatform.TrainingDataCollectionEvents." +
           SegmentIdToHistogramVariant(segment_id),
       event);
+  VLOG(1) << "Training Data event for "
+          << SegmentIdToHistogramVariant(segment_id) << ": "
+          << TrainingDataCollectionEventToErrorMsg(event);
 }
 
 // This conversion exists because segment selector uses the result state
