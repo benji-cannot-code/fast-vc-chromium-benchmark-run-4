99b069e9e54596e96153bccf627c2dd9a302ded0
Dominique Fauteux-Chapleau <domfc@chromium.org>
2023-10-31 17:37:24 +0000

    Remove unused DUMMY_API_TOKEN
    Change-Id: I668488fa8604b3c72be0dfaa11ba4afcf0b87b18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4980845
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Nasser Al-shawwa <alshawwa@chromium.org>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217710}

--- chrome/browser/enterprise/connectors/service_provider_config.cc
+++ chrome/browser/enterprise/connectors/service_provider_config.cc
@@ -9,10 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/enterprise/connectors/service_provider_config.h"
 #include "base/json/json_reader.h"
 
-// Used to indicate an unset key/id/secret.  This works better with
-// various unit tests than leaving the token empty.
-#define DUMMY_API_TOKEN "dummytoken"
-
 namespace enterprise_connectors {
 
 namespace {
