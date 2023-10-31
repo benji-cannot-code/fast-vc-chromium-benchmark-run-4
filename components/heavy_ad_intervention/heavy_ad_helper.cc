4b152f2f4212f924916383c9091c1de4d7407f8c
dpapad <dpapad@chromium.org>
2023-10-31 17:27:40 +0000

    Move GetLocalizedHtml() from security_interstitials/ to ui/base/webui/.
    This method is useful for both security interstitials UIs as well as
    network error UIs. Moving it to ui/base/webui/web_ui_util.h allows
    reusing it in a follow-up CL.
    
    Bug: 1173575
    Change-Id: I96d0c1e23083d62fb8d3593e4c64446905995a3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4977839
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Joemer Ramos <joemerramos@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217695}

--- components/heavy_ad_intervention/heavy_ad_helper.cc
+++ components/heavy_ad_intervention/heavy_ad_helper.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <ostream>
 
 #include "components/grit/components_resources.h"
-#include "components/security_interstitials/core/common_string_util.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -42,8 +41,7 @@ std::string PrepareHeavyAdPage(const std::string& application_locale) {
   webui::SetLoadTimeDataDefaults(application_locale, &load_time_data);
 
   webui::AppendWebUiCssTextDefaults(&html);
-  return security_interstitials::common_string_util::GetLocalizedHtml(
-      html, load_time_data);
+  return webui::GetLocalizedHtml(html, load_time_data);
 }
 
 }  // namespace heavy_ad_intervention
--- ui/base/webui/web_ui_util.cc
+++ ui/base/webui/web_ui_util.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base64.h"
 #include "base/i18n/rtl.h"
+#include "base/json/json_string_value_serializer.h"
 #include "base/logging.h"
 #include "base/strings/escape.h"
 #include "base/strings/string_number_conversions.h"
@@ -235,6 +236,28 @@ std::string GetFontSize() {
 
 std::string GetTextDirection() {
   return base::i18n::IsRTL() ? "rtl" : "ltr";
+}
+
+std::string GetLocalizedHtml(base::StringPiece html_template,
+                             const base::Value::Dict& strings) {
+  // Populate $i18n{...} placeholders.
+  ui::TemplateReplacements replacements;
+  ui::TemplateReplacementsFromDictionaryValue(strings, &replacements);
+  std::string output =
+      ui::ReplaceTemplateExpressions(html_template, replacements);
+
+  // Inject data to the UI that will be used to populate loadTimeData upon
+  // initialization.
+  std::string json;
+  JSONStringValueSerializer serializer(&json);
+  serializer.Serialize(strings);
+  output.append("<script>");
+  output.append("var loadTimeDataRaw = ");
+  output.append(json);
+  output.append(";");
+  output.append("</script>");
+
+  return output;
 }
 
 }  // namespace webui
