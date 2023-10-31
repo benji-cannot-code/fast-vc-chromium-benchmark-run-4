b8f67c9e6e49c26a0268832be2cee92b3b3d70bb
Dibyajyoti Pal <dibyapal@google.com>
2023-10-31 00:15:54 +0000

    [dPWA] Launch app directly if link capturing on with single app no overlaps
    This CL ensures that if the document URL matches the scope of an
    app perfectly, the app is launched from the intent picker directly,
    bypassing the intent picker bubble.
    
    Bug: 1412856, b/304411994
    Change-Id: Ic2d7cfaf5662ff1bfe31b1c0ae222af0bc56d60e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4965667
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217311}

--- chrome/browser/ui/views/enable_link_capturing_infobar_browsertest.cc
+++ chrome/browser/ui/views/enable_link_capturing_infobar_browsertest.cc
@@ -160,7 +160,7 @@ class EnableLinkCapturingInfobarBrowserTest
       intent_chip_visibility_observer.WaitForChipToBeVisible();
       if (!intent_picker_icon->GetVisible()) {
         return testing::AssertionFailure()
-               << "Intent picker icon did not become visible.";
+               << "Intent picker icon never became visible.";
       }
     }
     EXPECT_TRUE(intent_picker_icon->GetVisible());
@@ -230,11 +230,9 @@ IN_PROC_BROWSER_TEST_F(EnableLinkCapturingInfobarBrowserTest,
 
   NavigateViaLinkClick(browser(), in_scope_url);
 
-  ASSERT_TRUE(ClickIntentPickerAndWaitForBubble());
-
   ui_test_utils::BrowserChangeObserver browser_added_waiter(
       nullptr, ui_test_utils::BrowserChangeObserver::ChangeType::kAdded);
-  intent_picker_bubble()->AcceptDialog();
+  ASSERT_TRUE(ClickIntentPickerChip());
   Browser* app_browser = browser_added_waiter.Wait();
   ASSERT_TRUE(app_browser);
 
@@ -248,13 +246,11 @@ IN_PROC_BROWSER_TEST_F(EnableLinkCapturingInfobarBrowserTest,
 
   NavigateViaLinkClick(browser(), in_scope_url);
 
-  ASSERT_TRUE(ClickIntentPickerAndWaitForBubble());
-
   Browser* app_browser;
   {
     ui_test_utils::BrowserChangeObserver browser_added_waiter(
         nullptr, ui_test_utils::BrowserChangeObserver::ChangeType::kAdded);
-    intent_picker_bubble()->AcceptDialog();
+    ASSERT_TRUE(ClickIntentPickerChip());
     app_browser = browser_added_waiter.Wait();
     ASSERT_TRUE(app_browser);
   }
@@ -305,11 +301,9 @@ IN_PROC_BROWSER_TEST_F(EnableLinkCapturingInfobarBrowserTest,
 
   NavigateViaLinkClick(browser(), in_scope_url);
 
-  ASSERT_TRUE(ClickIntentPickerAndWaitForBubble());
-
   ui_test_utils::BrowserChangeObserver browser_added_waiter(
       nullptr, ui_test_utils::BrowserChangeObserver::ChangeType::kAdded);
-  intent_picker_bubble()->AcceptDialog();
+  ASSERT_TRUE(ClickIntentPickerChip());
   Browser* app_browser = browser_added_waiter.Wait();
   ASSERT_TRUE(app_browser);
 
@@ -339,11 +333,9 @@ IN_PROC_BROWSER_TEST_F(EnableLinkCapturingInfobarBrowserTest,
 
   NavigateViaLinkClick(browser(), in_scope_url);
 
-  ASSERT_TRUE(ClickIntentPickerAndWaitForBubble());
-
   ui_test_utils::BrowserChangeObserver browser_added_waiter(
       nullptr, ui_test_utils::BrowserChangeObserver::ChangeType::kAdded);
-  intent_picker_bubble()->AcceptDialog();
+  ASSERT_TRUE(ClickIntentPickerChip());
   Browser* app_browser = browser_added_waiter.Wait();
   ASSERT_TRUE(app_browser);
 
@@ -369,13 +361,11 @@ IN_PROC_BROWSER_TEST_F(EnableLinkCapturingInfobarBrowserTest, AppLaunched) {
 
   NavigateViaLinkClick(browser(), in_scope_url);
 
-  ASSERT_TRUE(ClickIntentPickerAndWaitForBubble());
-
   Browser* app_browser;
   {
     ui_test_utils::BrowserChangeObserver browser_added_waiter(
         nullptr, ui_test_utils::BrowserChangeObserver::ChangeType::kAdded);
-    intent_picker_bubble()->AcceptDialog();
+    ASSERT_TRUE(ClickIntentPickerChip());
     app_browser = browser_added_waiter.Wait();
     ASSERT_TRUE(app_browser);
   }
@@ -414,11 +404,9 @@ IN_PROC_BROWSER_TEST_F(EnableLinkCapturingInfobarBrowserTest, BarRemoved) {
 
   NavigateViaLinkClick(browser(), in_scope_url);
 
-  ASSERT_TRUE(ClickIntentPickerAndWaitForBubble());
-
   ui_test_utils::BrowserChangeObserver browser_added_waiter(
       nullptr, ui_test_utils::BrowserChangeObserver::ChangeType::kAdded);
-  intent_picker_bubble()->AcceptDialog();
+  ASSERT_TRUE(ClickIntentPickerChip());
   Browser* app_browser = browser_added_waiter.Wait();
   ASSERT_TRUE(app_browser);
 
@@ -447,11 +435,9 @@ IN_PROC_BROWSER_TEST_F(EnableLinkCapturingInfobarBrowserTest,
   for (int i = 0; i < 2; ++i) {
     NavigateViaLinkClick(browser(), in_scope_url);
 
-    ASSERT_TRUE(ClickIntentPickerAndWaitForBubble());
-
     ui_test_utils::BrowserChangeObserver browser_added_waiter(
         nullptr, ui_test_utils::BrowserChangeObserver::ChangeType::kAdded);
-    intent_picker_bubble()->AcceptDialog();
+    ASSERT_TRUE(ClickIntentPickerChip());
     Browser* app_browser = browser_added_waiter.Wait();
     ASSERT_TRUE(app_browser);
 
@@ -477,11 +463,9 @@ IN_PROC_BROWSER_TEST_F(EnableLinkCapturingInfobarBrowserTest,
   // Now, the infobar will not show up.
   NavigateViaLinkClick(browser(), in_scope_url);
 
-  ASSERT_TRUE(ClickIntentPickerAndWaitForBubble());
-
   ui_test_utils::BrowserChangeObserver browser_added_waiter(
       nullptr, ui_test_utils::BrowserChangeObserver::ChangeType::kAdded);
-  intent_picker_bubble()->AcceptDialog();
+  ASSERT_TRUE(ClickIntentPickerChip());
   Browser* app_browser = browser_added_waiter.Wait();
   ASSERT_TRUE(app_browser);
 
@@ -496,8 +480,7 @@ IN_PROC_BROWSER_TEST_F(EnableLinkCapturingInfobarBrowserTest,
       InstallOuterAppAndInnerApp();
 
   NavigateViaLinkClick(browser(), inner_app_scoped_url);
-
-  ASSERT_TRUE(ClickIntentPickerAndWaitForBubble());
+  EXPECT_TRUE(ClickIntentPickerAndWaitForBubble());
 
   // The app list is currently not deterministically ordered, so find the
   // correct item and select that.
@@ -516,7 +499,6 @@ IN_PROC_BROWSER_TEST_F(EnableLinkCapturingInfobarBrowserTest,
   intent_picker_bubble()->AcceptDialog();
 
   Browser* app_browser = browser_added_waiter.Wait();
-
   ASSERT_TRUE(app_browser);
   EXPECT_TRUE(AppBrowserController::IsWebApp(app_browser));
   EXPECT_TRUE(AppBrowserController::IsForWebApp(app_browser, outer_app_id));
