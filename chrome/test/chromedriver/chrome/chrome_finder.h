74f40e2398cff49f146cc6db5c8020ceec1ebbfe
Stephan Hartmann <stha09@googlemail.com>
2023-10-31 15:40:00 +0000

    IWYU: add string in chrome_finder.h
    Bug: 957519
    Change-Id: I1c683c5d4e996817d0a9e4a57919422886e8808e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994605
    Reviewed-by: Vladimir Nechaev <nechaev@chromium.org>
    Auto-Submit: Stephan Hartmann <stha09@googlemail.com>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#1217609}

--- chrome/test/chromedriver/chrome/chrome_finder.h
+++ chrome/test/chromedriver/chrome/chrome_finder.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_TEST_CHROMEDRIVER_CHROME_CHROME_FINDER_H_
 #define CHROME_TEST_CHROMEDRIVER_CHROME_CHROME_FINDER_H_
 
+#include <string>
 #include <vector>
 
 #include "base/functional/callback_forward.h"
