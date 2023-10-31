cf4a17e7f41f81491269e31fe58a030dc7dbf3cd
Tina Wang <tinazwang@google.com>
2023-10-31 21:21:13 +0000

    [CodeHealthRotation] Remove expired histogram
    IRemove expired histograms CustomTabs.PredictionStatus,
    CustomTabs.PredictionToLaunch.
    
    Bug: 1384815
    Change-Id: Id51572ae13eb0f974ee06a12981907de317a1ced
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4978317
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217882}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/ClientManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/ClientManager.java
@@ -15,7 +15,6 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.os.SystemClock;
 import android.text.TextUtils;
-import android.text.format.DateUtils;
 import android.util.SparseBooleanArray;
 
 import androidx.annotation.IntDef;
@@ -79,7 +78,7 @@ class ClientManager {
         int NUM_ENTRIES = 5;
     }
 
-    // Values for the "CustomTabs.PredictionStatus" UMA histogram. Append-only.
+    // Values for the PredictionStatus. Append-only.
     @IntDef({PredictionStatus.NONE, PredictionStatus.GOOD, PredictionStatus.BAD})
     @Retention(RetentionPolicy.SOURCE)
     @interface PredictionStatus {
@@ -490,16 +489,10 @@ class ClientManager {
     public synchronized void registerLaunch(CustomTabsSessionToken session, String url) {
         @PredictionStatus
         int outcome = getPredictionOutcome(session, url);
-        RecordHistogram.recordEnumeratedHistogram(
-                "CustomTabs.PredictionStatus", outcome, PredictionStatus.NUM_ENTRIES);
 
         SessionParams params = mSessionParams.get(session);
         if (outcome == PredictionStatus.GOOD) {
-            long elapsedTimeMs = SystemClock.elapsedRealtime()
-                    - params.getLastMayLaunchUrlTimestamp();
             RequestThrottler.getForUid(params.uid).registerSuccess(params.mPredictedUrl);
-            RecordHistogram.recordCustomTimesHistogram("CustomTabs.PredictionToLaunch",
-                    elapsedTimeMs, 1, DateUtils.MINUTE_IN_MILLIS * 3, 100);
         }
         RecordHistogram.recordEnumeratedHistogram("CustomTabs.WarmupStateOnLaunch",
                 getWarmupState(session), CalledWarmup.NUM_ENTRIES);
