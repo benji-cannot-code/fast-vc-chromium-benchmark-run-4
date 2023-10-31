71b876b6a044bf6667bcccdf7f54a31ea3ae46d4
Riley Tatum <rtatum@google.com>
2023-10-31 18:41:11 +0000

    update tile color to use side panel mixer
    Change-Id: I1882cfcd776f78035beff6f7d5bd1046d2eb1344
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4980706
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217777}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -421,6 +421,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorSidePanelCustomizeChromeThemeCheckmarkBackground) \
   E_CPONLY(kColorSidePanelCustomizeChromeThemeCheckmarkForeground) \
   E_CPONLY(kColorSidePanelCustomizeChromeThemeSnapshotBackground) \
+  E_CPONLY(kColorSidePanelCustomizeChromeWallpaperSearchTileBackground) \
   E_CPONLY(kColorSidePanelCustomizeChromeWebStoreBorder) \
   E_CPONLY(kColorSidePanelDialogBackground) \
   E_CPONLY(kColorSidePanelDialogDivider) \
