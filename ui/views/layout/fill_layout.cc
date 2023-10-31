979c8dfcb03dff259615a5e0eaaa9f318a9c5283
weidongliu <liuwd8@gmail.com>
2023-10-31 06:07:58 +0000

    FillLayout uses GetPreferredSize(const SizeBounds& available_size) instead of GetPreferredSize()
    Bug: 1346889
    Change-Id: I4ac16f6dd0890a898de7685d504ba3b9df6277c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4899961
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217415}

--- ui/views/layout/fill_layout.cc
+++ ui/views/layout/fill_layout.cc
@@ -52,7 +52,7 @@ gfx::Size FillLayout::GetPreferredSize(const View* host) const {
   for (const View* child : host->children()) {
     if (!IsChildViewIgnoredByLayout(child)) {
       has_child = true;
-      result.SetToMax(child->GetPreferredSize());
+      result.SetToMax(child->GetPreferredSize(GetContentsSizeBounds(host)));
     }
   }
 
@@ -69,8 +69,9 @@ gfx::Size FillLayout::GetPreferredSize(const View* host) const {
 gfx::Size FillLayout::GetMinimumSize(const View* host) const {
   DCHECK_EQ(host_view(), host);
 
-  if (!minimum_size_enabled_)
-    return host->GetPreferredSize();
+  if (!minimum_size_enabled_) {
+    return host->GetPreferredSize(GetContentsSizeBounds(host));
+  }
 
   gfx::Size result;
 
@@ -106,6 +107,12 @@ int FillLayout::GetPreferredHeightForWidth(const View* host, int width) const {
   }
 
   return height;
+}
+
+SizeBounds FillLayout::GetContentsSizeBounds(const View* host) const {
+  return host->bounds().IsEmpty()
+             ? SizeBounds()
+             : SizeBounds(host->GetContentsBounds().size());
 }
 
 }  // namespace views
