fe5cfa3200573170262168b5638ecc8d23a38e29
Miriam Polzer <mpolzer@google.com>
2023-10-31 08:54:17 +0000

    Revert "[3PCD] Add divider above cookie dialog button not below 3PC container"
    This reverts commit bb068234122a6f0bb76f2f4741105855e4293e90.
    
    Reason for revert: Breaking PageInfoBubbleViewCookiesSubpageBrowserTest on windows
    
    Original change's description:
    > [3PCD] Add divider above cookie dialog button not below 3PC container
    >
    > This is causing us to hide the divider if the 3PC container is hidden
    >
    > Fixed: b:307994533
    > Change-Id: If0c3481179dc25778121852d491eafb1c3d83fd5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4986007
    > Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    > Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    > Reviewed-by: Elly FJ <ellyjones@chromium.org>
    > Commit-Queue: Elly FJ <ellyjones@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1217330}
    
    Change-Id: I0e23d455cfcca7b1c00d1898b17a51ccf7128640
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4992381
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Miriam Polzer <mpolzer@google.com>
    Auto-Submit: Miriam Polzer <mpolzer@google.com>
    Cr-Commit-Position: refs/heads/main@{#1217462}

--- chrome/browser/ui/views/page_info/page_info_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/page_info/page_info_bubble_view_browsertest.cc
@@ -1244,9 +1244,8 @@ IN_PROC_BROWSER_TEST_P(PageInfoBubbleViewBrowserTestCookiesSubpage,
   SetCookieControlsMode(content_settings::CookieControlsMode::kIncognitoOnly);
   OpenPageInfoAndGoToCookiesSubpage(/*fps_owner =*/{});
 
-  // FPS blocked and 3pc allowed -> button for opening cookie dialog +
-  // separator.
-  size_t kExpectedChildren = 2;
+  // FPS blocked and 3pc allowed -> only button for opening cookie dialog.
+  size_t kExpectedChildren = 1;
   auto* cookies_buttons_container =
       GetView(PageInfoViewFactory::VIEW_ID_PAGE_INFO_COOKIES_BUTTONS_CONTAINER);
   EXPECT_EQ(kExpectedChildren, cookies_buttons_container->children().size());
@@ -1274,14 +1273,13 @@ IN_PROC_BROWSER_TEST_P(PageInfoBubbleViewBrowserTestCookiesSubpage,
   OpenPageInfoAndGoToCookiesSubpage({fps_owner});
 
   if (IsParamFeatureEnabled()) {
-    size_t kExpectedChildren = 3;
+    size_t kExpectedChildren = 2;
     auto* cookies_buttons_container = GetView(
         PageInfoViewFactory::VIEW_ID_PAGE_INFO_COOKIES_BUTTONS_CONTAINER);
     EXPECT_EQ(kExpectedChildren, cookies_buttons_container->children().size());
   } else {
-    // FPS allowed and 3pc blocked -> buttons for cookie dialog and 3pc and fps
-    // and separator.
-    size_t kExpectedChildren = 4;
+    // FPS allowed and 3pc blocked -> buttons for cookie dialog and 3pc and fps.
+    size_t kExpectedChildren = 3;
     auto* cookies_buttons_container = GetView(
         PageInfoViewFactory::VIEW_ID_PAGE_INFO_COOKIES_BUTTONS_CONTAINER);
     EXPECT_EQ(kExpectedChildren, cookies_buttons_container->children().size());
@@ -1327,7 +1325,7 @@ IN_PROC_BROWSER_TEST_P(PageInfoBubbleViewBrowserTestCookiesSubpage,
   // FPS blocked and 3pc blocked -> buttons for cookie dialog and third party
   // cookies.
   if (IsParamFeatureEnabled()) {
-    size_t kExpectedChildren = 2;
+    size_t kExpectedChildren = 1;
     auto* cookies_buttons_container = GetView(
         PageInfoViewFactory::VIEW_ID_PAGE_INFO_COOKIES_BUTTONS_CONTAINER);
     EXPECT_THAT(cookies_buttons_container->children().size(),
@@ -1352,7 +1350,7 @@ IN_PROC_BROWSER_TEST_P(PageInfoBubbleViewBrowserTestCookiesSubpage,
     EXPECT_THAT(third_party_cookies_toggle->GetIsOn(), IsFalse());
     EXPECT_EQ(user_actions_stats.GetActionCount("PageInfo.Cookies.Blocked"), 1);
   } else {
-    size_t kExpectedChildren = 3;
+    size_t kExpectedChildren = 2;
     auto* cookies_buttons_container = GetView(
         PageInfoViewFactory::VIEW_ID_PAGE_INFO_COOKIES_BUTTONS_CONTAINER);
     EXPECT_EQ(kExpectedChildren, cookies_buttons_container->children().size());
@@ -1390,9 +1388,8 @@ IN_PROC_BROWSER_TEST_P(PageInfoBubbleViewBrowserTestCookiesSubpage,
 
   OpenPageInfoAndGoToCookiesSubpage({fps_owner});
 
-  // FPS allowed and 3pc allowed -> buttons for cookie dialog and fps button and
-  // separator.
-  size_t kExpectedChildren = 3;
+  // FPS allowed and 3pc allowed -> buttons for cookie dialog and fps button.
+  size_t kExpectedChildren = 2;
   auto* cookies_buttons_container =
       GetView(PageInfoViewFactory::VIEW_ID_PAGE_INFO_COOKIES_BUTTONS_CONTAINER);
   EXPECT_EQ(kExpectedChildren, cookies_buttons_container->children().size());
