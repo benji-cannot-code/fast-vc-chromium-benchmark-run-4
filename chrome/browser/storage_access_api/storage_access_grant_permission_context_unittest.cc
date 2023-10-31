d22c2ea76026024faf2c50dbf6e8a130d1a7ece5
Chris Fredrickson <cfredric@chromium.org>
2023-10-31 16:06:41 +0000

    [RWS] Reenable RWS feature in unit tests
    Fixed: 1485248
    Change-Id: I1e4a455444f70767854cb3e69d61171adbeb5fe1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994206
    Reviewed-by: Shuran Huang <shuuran@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Shuran Huang <shuuran@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217627}

--- chrome/browser/storage_access_api/storage_access_grant_permission_context_unittest.cc
+++ chrome/browser/storage_access_api/storage_access_grant_permission_context_unittest.cc
@@ -115,9 +115,6 @@ class StorageAccessGrantPermissionContextTest
                },
            }});
     } else {
-      // TODO(crbug.com/1485248) Handle first party sets (related website sets)
-      // more deliberately in the tests now that it is enabled by default
-      disabled.push_back(features::kFirstPartySets);
       disabled.push_back(blink::features::kStorageAccessAPI);
     }
 
@@ -285,6 +282,7 @@ class StorageAccessGrantPermissionContextTest
       mock_permission_prompt_factory_;
   permissions::PermissionRequestID::RequestLocalId::Generator
       request_id_generator_;
+  first_party_sets::ScopedMockFirstPartySetsHandler first_party_sets_handler_;
 };
 
 class StorageAccessGrantPermissionContextAPIDisabledTest
