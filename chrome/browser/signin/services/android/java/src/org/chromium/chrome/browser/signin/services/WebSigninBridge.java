e63d86cb06d172002f15ee264a59fce2d99cc1fb
Boris Sazonov <bsazonov@chromium.org>
2023-11-01 02:19:22 +0000

    [Android][Signin] Replace assert with throw in WebSigninBridge
    No behavior change - if either account or listener is null, the code
    would crash later in the JNI layer.
    
    Bug: 1494807
    Change-Id: I0b5fdc35e77bd8627e6f3cef1e4196948e7d573f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994812
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218051}

--- chrome/browser/signin/services/android/java/src/org/chromium/chrome/browser/signin/services/WebSigninBridge.java
+++ chrome/browser/signin/services/android/java/src/org/chromium/chrome/browser/signin/services/WebSigninBridge.java
@@ -15,6 +15,8 @@ import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.components.signin.base.CoreAccountInfo;
 import org.chromium.components.signin.base.GoogleServiceAuthError;
 
+import java.util.Objects;
+
 /**
  * Used by the web sign-in flow to detect when the flow is completed or failed. Every instance of
  * this class should be explicitly destroyed using {@link #destroy()} to correctly release native
@@ -64,7 +66,8 @@ public class WebSigninBridge {
      * @param listener The listener to be notified about sign-in completion.
      */
     private WebSigninBridge(Profile profile, CoreAccountInfo account, Listener listener) {
-        assert account != null && listener != null;
+        Objects.requireNonNull(account);
+        Objects.requireNonNull(listener);
         mNativeWebSigninBridge = WebSigninBridgeJni.get().create(profile, account, listener);
         assert mNativeWebSigninBridge != 0 : "Couldn't create native WebSigninBridge object!";
     }
