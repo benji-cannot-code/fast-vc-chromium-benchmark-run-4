71ddd245dab49468edfeff1410911181f02c74c1
Gaston Rodriguez Lopez <gastonr@microsoft.com>
2023-11-01 23:31:54 +0000

    Make Thumbthickness and Scrollbarthickness return const.
    Making ScrollbarTheme::ScrollbarThickness and
    ScrollbarThemeFluent::ThumbThickness return value const.
    
    Bug: 1479169
    Change-Id: I703d70877794c4fbcd1fbfbc8d9d6c42f2ebb5a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4998067
    Commit-Queue: Gaston Rodriguez <gastonr@microsoft.com>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218531}

--- third_party/blink/renderer/core/scroll/scrollbar_theme_fluent.cc
+++ third_party/blink/renderer/core/scroll/scrollbar_theme_fluent.cc
@@ -51,8 +51,9 @@ ScrollbarThemeFluent::ScrollbarThemeFluent() {
   }
 }
 
-int ScrollbarThemeFluent::ScrollbarThickness(float scale_from_dip,
-                                             EScrollbarWidth scrollbar_width) {
+int ScrollbarThemeFluent::ScrollbarThickness(
+    float scale_from_dip,
+    EScrollbarWidth scrollbar_width) const {
   return base::ClampRound(scrollbar_track_thickness_ * scale_from_dip);
 }
 
@@ -124,8 +125,9 @@ base::TimeDelta ScrollbarThemeFluent::OverlayScrollbarFadeOutDuration() const {
   return style_.fade_out_duration;
 }
 
-int ScrollbarThemeFluent::ThumbThickness(const float scale_from_dip,
-                                         const EScrollbarWidth scrollbar_width) {
+int ScrollbarThemeFluent::ThumbThickness(
+    const float scale_from_dip,
+    const EScrollbarWidth scrollbar_width) const {
   // The difference between track's and thumb's thicknesses should always be
   // even to have equal thumb offsets from both sides so the thumb can remain
   // in the middle of the track. Subtract one pixel if the difference is odd.
