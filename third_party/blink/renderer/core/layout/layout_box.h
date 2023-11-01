30af34f9b3f0faa3077450d5efb2387115afe0c5
Ian Kilpatrick <ikilpatrick@chromium.org>
2023-11-01 15:10:16 +0000

    [cleanup] Remove GetComputedStyleOutOfFlowInsetsFix flag.
    There should be no behaviour change.
    
    Bug: 1451020, 867616
    Change-Id: I5d9f5150d157013b5b1873c6442a4b7762b2c45d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4996271
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218223}

--- third_party/blink/renderer/core/layout/layout_box.h
+++ third_party/blink/renderer/core/layout/layout_box.h
@@ -868,7 +868,7 @@ class CORE_EXPORT LayoutBox : public LayoutBoxModelObject {
       TransformState&,
       VisualRectFlags = kDefaultVisualRectFlags) const override;
 
-  LayoutUnit ContainingBlockLogicalHeightForGetComputedStyle() const;
+  LayoutUnit ContainingBlockLogicalHeightForRelPositioned() const;
 
   LayoutUnit ContainingBlockLogicalWidthForContent() const override;
 
