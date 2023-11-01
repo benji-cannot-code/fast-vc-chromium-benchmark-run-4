812e452c3debf9c11d0d62539a9fa399b7b2918b
Elly <ellyjones@chromium.org>
2023-11-01 18:57:58 +0000

    webdialogs: remove OnDialogClosingFromKeyEvent()
    This method was only overridden in one place, where it was used to call
    OnDialogClosed() unnecessarily - in that situation, the dialog is just
    about to close anyway, so OnDialogClosed() is about to be run by
    WebDialogDelegate itself. This change deletes the method, its only
    override, and its only call site. Simplification of
    PrintPreviewDialogController as a result might be possible, since it no
    longer needs to tolerate multiple calls to OnDialogClosed().
    
    This change does not add an automated test. I tried to do so, but:
    * A unit test that doesn't depend on Views didn't work: there's no way
      to send key events to the dialog directly without Views APIs;
    * A unit test that does depend on Views required putting a single unit
      test for this code, which otherwise doesn't use Views, in a different
      part of the source tree, which seems very ugly;
    * A non-unit test that doesn't depend on Views would need to be an
      interactive ui test so that it could use ui_controls::SendKeyPress().
      Given the cost of interactive ui tests, how flaky they generally are,
      and the low risk of this code regressing in the future, this seems not
      worth writing.
    
    Instead, I manually tested that I did not regress the behavior.
    
    Bug: 1485127
    Change-Id: I804d56adff25739618646f8c6ee926a4c371342a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994890
    Auto-Submit: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218353}

--- chrome/browser/printing/print_preview_dialog_controller.cc
+++ chrome/browser/printing/print_preview_dialog_controller.cc
@@ -90,7 +90,6 @@ class PrintPreviewDialogDelegate : public ui::WebDialogDelegate {
   GURL GetDialogContentURL() const override;
   void GetDialogSize(gfx::Size* size) const override;
   std::string GetDialogArgs() const override;
-  void OnDialogClosingFromKeyEvent() override;
   void OnDialogClosed(const std::string& json_retval) override;
   void OnCloseContents(WebContents* source, bool* out_close_dialog) override;
   bool ShouldShowDialogTitle() const override;
@@ -158,10 +157,6 @@ void PrintPreviewDialogDelegate::GetDialogSize(gfx::Size* size) const {
 
 std::string PrintPreviewDialogDelegate::GetDialogArgs() const {
   return std::string();
-}
-
-void PrintPreviewDialogDelegate::OnDialogClosingFromKeyEvent() {
-  OnDialogClosed(std::string());
 }
 
 void PrintPreviewDialogDelegate::OnDialogClosed(
