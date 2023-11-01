394b168bc8c0649a88fcdc3d021a027327c1021a
Shiyi Zou <shiyi.zou@intel.com>
2023-11-01 02:46:00 +0000

    WebNN: Define elu, leakyRelu operations in mojo
    This CL implements CreateElu() and CreateLeakyRelu() for creating mojo
    operations and adds operator validation on service side.
    
    This CL also adds
      1. floating-point data type check for MLGraphBuilder::leakyRelu
      2. alpha > 0 check for MLActivation elu
      3. MLActivation softmax
    
    Bug: 1273291
    Change-Id: I8e07b027ee92f55fb78909aa71face502ff50f25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4982131
    Auto-Submit: Shiyi Zou <shiyi.zou@intel.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Commit-Queue: Shiyi Zou <shiyi.zou@intel.com>
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1218060}

--- third_party/blink/renderer/modules/ml/webnn/ml_graph_type_converter.cc
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_type_converter.cc
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ranges/algorithm.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_clamp_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_conv_2d_options.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_ml_elu_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_gemm_options.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_ml_leaky_relu_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_pad_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_pool_2d_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_resample_2d_options.h"
@@ -144,6 +146,42 @@ blink_mojom::ClampPtr CreateClamp(const OperandToIdMap& operand_to_id_map,
   return clamp_mojo;
 }
 
+blink_mojom::EluPtr CreateElu(const OperandToIdMap& operand_to_id_map,
+                              const MLOperator* elu,
+                              bool is_activation) {
+  auto elu_mojo = blink_mojom::Elu::New();
+  // Activation has no input and output operand.
+  if (!is_activation) {
+    elu_mojo->input_operand_id = GetOperatorInputId(elu, operand_to_id_map);
+    elu_mojo->output_operand_id = GetOperatorOutputId(elu, operand_to_id_map);
+  }
+
+  const auto* options = static_cast<const MLEluOptions*>(elu->Options());
+  CHECK(options);
+  elu_mojo->alpha = options->alpha();
+  return elu_mojo;
+}
+
+blink_mojom::LeakyReluPtr CreateLeakyRelu(
+    const OperandToIdMap& operand_to_id_map,
+    const MLOperator* leaky_relu,
+    bool is_activation) {
+  auto leaky_relu_mojo = blink_mojom::LeakyRelu::New();
+  // Activation has no input and output operand.
+  if (!is_activation) {
+    leaky_relu_mojo->input_operand_id =
+        GetOperatorInputId(leaky_relu, operand_to_id_map);
+    leaky_relu_mojo->output_operand_id =
+        GetOperatorOutputId(leaky_relu, operand_to_id_map);
+  }
+
+  const auto* options =
+      static_cast<const MLLeakyReluOptions*>(leaky_relu->Options());
+  CHECK(options);
+  leaky_relu_mojo->alpha = options->alpha();
+  return leaky_relu_mojo;
+}
+
 blink_mojom::InputOperandLayout BlinkInputOperandLayoutToMojo(
     blink::V8MLInputOperandLayout::Enum type) {
   switch (type) {
@@ -263,6 +301,17 @@ base::expected<OperationPtr, String> CreateConv2dOperation(
       case blink::MLOperator::OperatorKind::kClamp: {
         conv2d_mojo->activation = blink_mojom::Activation::NewClamp(CreateClamp(
             operand_to_id_map, options->activation()->Operator(), true));
+        break;
+      }
+      case blink::MLOperator::OperatorKind::kElu: {
+        conv2d_mojo->activation = blink_mojom::Activation::NewElu(CreateElu(
+            operand_to_id_map, options->activation()->Operator(), true));
+        break;
+      }
+      case blink::MLOperator::OperatorKind::kLeakyRelu: {
+        conv2d_mojo->activation =
+            blink_mojom::Activation::NewLeakyRelu(CreateLeakyRelu(
+                operand_to_id_map, options->activation()->Operator(), true));
         break;
       }
       case blink::MLOperator::OperatorKind::kRelu:
@@ -633,8 +682,14 @@ base::expected<OperationPtr, String> ConvertToMojoOperation(
       [[fallthrough]];
     case MLOperator::OperatorKind::kPow:
       return CreateElementWiseBinaryOperator(operand_to_id_map, op);
+    case MLOperator::OperatorKind::kElu:
+      return blink_mojom::Operation::NewElu(
+          CreateElu(operand_to_id_map, op, false));
     case MLOperator::OperatorKind::kGemm:
       return CreateGemmOperation(operand_to_id_map, op);
+    case MLOperator::OperatorKind::kLeakyRelu:
+      return blink_mojom::Operation::NewLeakyRelu(
+          CreateLeakyRelu(operand_to_id_map, op, false));
     case MLOperator::OperatorKind::kMatmul:
       return CreateMatmulOperation(operand_to_id_map, op);
     case MLOperator::OperatorKind::kPad:
