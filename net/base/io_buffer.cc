a64bbbf6637fd0ad95e6f6ea59ff624a5651ffb5
Tom Sepez <tsepez@chromium.org>
2023-11-01 00:37:57 +0000

    Add default constructor for net::IOBufferWithSize()
    This is a very small portion of the change at
      https://chromium-review.googlesource.com/c/chromium/src/+/4977740.
    
    It is extracted into its own CL to facilitate landing other parts
    of that large CL without modification when split by directories.
    
    Change-Id: If9234051050a57ebe7a808ccc3ad56c25fca5804
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4995779
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Auto-Submit: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218002}

--- net/base/io_buffer.cc
+++ net/base/io_buffer.cc
@@ -40,6 +40,8 @@ IOBuffer::~IOBuffer() {
   data_.ClearAndDeleteArray();
 }
 
+IOBufferWithSize::IOBufferWithSize() = default;
+
 IOBufferWithSize::IOBufferWithSize(size_t size) : IOBuffer(size) {}
 
 IOBufferWithSize::IOBufferWithSize(char* data, size_t size)
