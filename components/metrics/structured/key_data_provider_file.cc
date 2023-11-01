bbc6adea130158f4d28ffc9488c797d7d272b00f
Jong Ahn <jongahn@google.com>
2023-11-01 21:08:36 +0000

    [Structured Metrics] Add SecondaryId API to KeyDataProvider.
    This is needed to pull both the device ID and profile ID for sequenced events.
    
    
    Bug: b/290096302
    Change-Id: I413fbe80436e4380487fa2efa1c561ecd1ccc7e8
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4981092
    Reviewed-by: Andrew Bregger <andrewbregger@google.com>
    Commit-Queue: Jong Ahn <jongahn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218439}

--- components/metrics/structured/key_data_provider_file.cc
+++ components/metrics/structured/key_data_provider_file.cc
@@ -56,6 +56,11 @@ absl::optional<uint64_t> KeyDataProviderFile::GetId(
                        project_validator->key_rotation_period());
 }
 
+absl::optional<uint64_t> KeyDataProviderFile::GetSecondaryId(
+    const std::string& project_name) {
+  return absl::nullopt;
+}
+
 KeyData* KeyDataProviderFile::GetKeyData(const std::string& project_name) {
   DCHECK(IsReady());
   return key_data_.get();
