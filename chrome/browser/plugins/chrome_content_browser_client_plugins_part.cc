6a47af1205befe22b290c9ae04e0f744496b98c1
Dave Tapuska <dtapuska@chromium.org>
2023-11-01 19:47:37 +0000

    Move binding plugin host to RenderFrame from RenderProcess
    For MBI we require most interfaces to be bound to an
    AgentScheudlingGroup. Move these plugin interfaces to be bound to the
    RenderFrameHost which is part of an ASG.
    
    Bug: 993189
    Change-Id: I3c7dad47b954dc794c7d3c37735e070b3c90e662
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994907
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218388}

--- chrome/browser/plugins/chrome_content_browser_client_plugins_part.cc
+++ chrome/browser/plugins/chrome_content_browser_client_plugins_part.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pepper_permission_util.h"
 #include "components/version_info/version_info.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "extensions/buildflags/buildflags.h"
 #include "mojo/public/cpp/bindings/pending_associated_receiver.h"
@@ -131,12 +132,13 @@ ChromeContentBrowserClientPluginsPart::ChromeContentBrowserClientPluginsPart() =
 ChromeContentBrowserClientPluginsPart::
     ~ChromeContentBrowserClientPluginsPart() = default;
 
-void ChromeContentBrowserClientPluginsPart::ExposeInterfacesToRenderer(
-    service_manager::BinderRegistry* registry,
-    blink::AssociatedInterfaceRegistry* associated_registry,
-    content::RenderProcessHost* host) {
-  associated_registry->AddInterface<chrome::mojom::PluginInfoHost>(
-      base::BindRepeating(&BindPluginInfoHost, host->GetID()));
+void ChromeContentBrowserClientPluginsPart::
+    ExposeInterfacesToRendererForRenderFrameHost(
+        content::RenderFrameHost& render_frame_host,
+        blink::AssociatedInterfaceRegistry& associated_registry) {
+  associated_registry.AddInterface<chrome::mojom::PluginInfoHost>(
+      base::BindRepeating(&BindPluginInfoHost,
+                          render_frame_host.GetProcess()->GetID()));
 }
 
 bool ChromeContentBrowserClientPluginsPart::
