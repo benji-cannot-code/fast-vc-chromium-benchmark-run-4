0ce7a599f38537b70371112378526e459185849f
Lei Zhang <thestig@chromium.org>
2023-11-01 22:03:13 +0000

    Remove out-parameters from PDFiumEngine::GetRegion()
    Return a struct instead. Then:
    - Change PDFiumEngine::Highlight() to take the struct as a param in
      place of 2 separate params.
    - Use raw_ptr in the struct.
    
    Change-Id: I03304428d699dd311c9c5eeffdeead99c652a3a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4978018
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218473}

--- pdf/pdfium/pdfium_engine.h
+++ pdf/pdfium/pdfium_engine.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_map.h"
 #include "base/containers/span.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/raw_span.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
@@ -258,6 +259,16 @@ class PDFiumEngine : public PDFEngine,
    private:
     PDFiumPage::Area area_;
     PDFiumPage::LinkTarget target_;
+  };
+
+  struct RegionData {
+    RegionData(base::span<uint8_t> buffer, size_t stride);
+    RegionData(RegionData&&);
+    RegionData& operator=(RegionData&&);
+    ~RegionData();
+
+    base::raw_span<uint8_t> buffer;  // Never empty.
+    size_t stride;
   };
 
   friend class FormFillerTest;
@@ -518,11 +529,10 @@ class PDFiumEngine : public PDFEngine,
   // coordinates. (i.e. 0,0 is top left corner of plugin area)
   gfx::Rect GetScreenRect(const gfx::Rect& rect) const;
 
-  // Given an image `buffer` with `stride`, highlights `rect`.
+  // Given an image `region`, highlights `rect`.
   // `highlighted_rects` contains the already highlighted rectangles and will be
   // updated to include `rect` if `rect` has not already been highlighted.
-  void Highlight(base::span<uint8_t> buffer,
-                 size_t stride,
+  void Highlight(const RegionData& region,
                  const gfx::Rect& rect,
                  int color_red,
                  int color_green,
@@ -549,10 +559,8 @@ class PDFiumEngine : public PDFEngine,
                       const gfx::Rect& clip_rect,
                       SkBitmap& image_data);
 
-  void GetRegion(const gfx::Point& location,
-                 SkBitmap& image_data,
-                 base::span<uint8_t>& region,
-                 size_t& stride) const;
+  absl::optional<RegionData> GetRegion(const gfx::Point& location,
+                                       SkBitmap& image_data) const;
 
   // Called when the selection changes.
   void OnSelectionTextChanged();
