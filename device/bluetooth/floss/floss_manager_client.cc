aab80b34b5d5a097908bacfd1e024a96c8a10906
Ying Hsu <yinghsu@chromium.org>
2023-11-01 01:28:25 +0000

    Make scan settings optional for method StartScan
    This patch updates the signature of the D-Bus method StartScan,
    enabling the use of absl::nullopt for scan settings, which empowers
    the underlying Floss daemon to determine scan settings parameter.
    
    BUG=b:304681854
    BUG=b:300216447
    TEST=manual test with v0.2 and v0.3 Floss daemons
    
    Change-Id: Ib874ef54b4693fca6b32d1c20061ea92b0b9a0b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4957282
    Reviewed-by: Katherine Lai <laikatherine@chromium.org>
    Commit-Queue: Ying Hsu <yinghsu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218025}

--- device/bluetooth/floss/floss_manager_client.cc
+++ device/bluetooth/floss/floss_manager_client.cc
@@ -517,7 +517,7 @@ void FlossManagerClient::HandleGetFlossApiVersion(
                          << ". Valid range: "
                          << floss::version::GetMinimalSupportedVersion()
                          << " to "
-                         << floss::version::GetMinimalSupportedVersion();
+                         << floss::version::GetMaximalSupportedVersion();
   }
 }
 
