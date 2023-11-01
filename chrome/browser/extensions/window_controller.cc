4e88574b68f10bf8dddc9e886c059cf1cfc425d9
Claudio DeSouza <cdesouza@chromium.org>
2023-11-01 19:04:31 +0000

    [Extensions] Enum modernisation for windows.json
    This CL updates the codegen attribute for the enum types generated by
    windows.json. This attribute generates enum class types with
    kMixedCase key names.
    
    Note: This CL is purely mechanical and should have no effect on existing
    behaviour.
    
    Bug: 1421546
    Change-Id: I0e3685c2fb11bcf680d7df31e53f0dcca99af43e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4997033
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Claudio DeSouza <cdesouza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218362}

--- chrome/browser/extensions/window_controller.cc
+++ chrome/browser/extensions/window_controller.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/types/cxx23_to_underlying.h"
 #include "base/values.h"
 #include "chrome/browser/extensions/window_controller_list.h"
 #include "chrome/browser/profiles/profile.h"
@@ -23,13 +24,13 @@ namespace extensions {
 WindowController::TypeFilter WindowController::GetAllWindowFilter() {
   // This needs to be updated if there is a change to
   // extensions::api::windows:WindowType.
-  static_assert(api::windows::WINDOW_TYPE_LAST == 5,
+  static_assert(base::to_underlying(api::windows::WindowType::kMaxValue) == 5,
                 "Update extensions WindowController to match WindowType");
-  return ((1 << api::windows::WINDOW_TYPE_NORMAL) |
-          (1 << api::windows::WINDOW_TYPE_PANEL) |
-          (1 << api::windows::WINDOW_TYPE_POPUP) |
-          (1 << api::windows::WINDOW_TYPE_APP) |
-          (1 << api::windows::WINDOW_TYPE_DEVTOOLS));
+  return ((1 << base::to_underlying(api::windows::WindowType::kNormal)) |
+          (1 << base::to_underlying(api::windows::WindowType::kPanel)) |
+          (1 << base::to_underlying(api::windows::WindowType::kPopup)) |
+          (1 << base::to_underlying(api::windows::WindowType::kApp)) |
+          (1 << base::to_underlying(api::windows::WindowType::kDevtools)));
 }
 
 // static
@@ -37,7 +38,7 @@ WindowController::TypeFilter WindowController::GetFilterFromWindowTypes(
     const std::vector<api::windows::WindowType>& types) {
   WindowController::TypeFilter filter = kNoWindowFilter;
   for (auto& window_type : types)
-    filter |= 1 << window_type;
+    filter |= 1 << base::to_underlying(window_type);
   return filter;
 }
 
@@ -50,7 +51,8 @@ WindowController::TypeFilter WindowController::GetFilterFromWindowTypesValues(
   for (const base::Value& type : *types) {
     if (!type.is_string())
       continue;
-    filter |= 1 << api::windows::ParseWindowType(type.GetString());
+    filter |= 1 << base::to_underlying(
+                  api::windows::ParseWindowType(type.GetString()));
   }
   return filter;
 }
@@ -67,7 +69,8 @@ Browser* WindowController::GetBrowser() const {
 }
 
 bool WindowController::MatchesFilter(TypeFilter filter) const {
-  TypeFilter type = 1 << api::windows::ParseWindowType(GetWindowTypeText());
+  TypeFilter type = 1 << base::to_underlying(
+                        api::windows::ParseWindowType(GetWindowTypeText()));
   return (type & filter) != 0;
 }
 
