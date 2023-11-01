eb8690d2948ab36cd765298739a4fe78a142fea0
Morten Stenshorne <mstensho@chromium.org>
2023-11-01 06:06:34 +0000

    Improve NGLayoutInputNode debug dumping.
    Support dumping the entire document and mark the target node with '*'.
    Also support dumping to a string, in addition to LOG(INFO).
    
    When dumping the NGLayoutInputNode tree, also descend into block nodes
    inside inline nodes. This includes floats, atomic inlines,
    block-in-inline and out-of-flow positioned elements.
    
    Clean up dumping of InlineNode. Instead of:
      "InlineItem. Type: '%s'.  LayoutObject: '%s'"
    simply do:
      "InlineItem %s. %s"
    
    ... except when it's text, in which case we'll dump the actual text
    instead of the LayoutObject debug name.
    
    Omit quotes around the debug name in NGBlockNode::ToString() as well. It
    was hard to read when the element had attributes.
    
    When visiting an InlineNode, DCHECK that it doesn't have siblings.
    InlineNode::NextSibling() always returns nullptr. It should be removed.
    I'll do that in a separate CL.
    
    Add a few unit tests.
    
    Change-Id: Ic29e55511eba8927cf871fde934f6ad68d944adc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4992843
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218105}

--- third_party/blink/renderer/core/layout/ng/ng_block_node.cc
+++ third_party/blink/renderer/core/layout/ng/ng_block_node.cc
@@ -1136,7 +1136,7 @@ LayoutUnit NGBlockNode::EmptyLineBlockSize(
 }
 
 String NGBlockNode::ToString() const {
-  return String::Format("NGBlockNode: '%s'",
+  return String::Format("NGBlockNode: %s",
                         GetLayoutBox()->DebugName().Ascii().c_str());
 }
 
