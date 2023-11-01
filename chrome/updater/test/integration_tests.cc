fe18ea5482e3df87217cc7aa5241d739f173bb99
S. Ganesh <ganesh@chromium.org>
2023-11-01 05:09:29 +0000

    updater: contextual error messages for offline and Group Policy errors
    Bug: 1422075
    Change-Id: Ifdff0ae2b0d48b0721b43bf26b2e0ba28a65af47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4996683
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218089}

--- chrome/updater/test/integration_tests.cc
+++ chrome/updater/test/integration_tests.cc
@@ -2395,7 +2395,7 @@ INSTANTIATE_TEST_SUITE_P(
             false,
             "INSTALLER_RESULT=2 INSTALLER_ERROR=1603",
             1603,
-            "Fatal error during installation. ",
+            "Install error: Fatal error during installation. ",
             {},
             {},
         },
@@ -2416,7 +2416,7 @@ INSTANTIATE_TEST_SUITE_P(
             false,
             "INSTALLER_RESULT=3 INSTALLER_ERROR=99",
             99,
-            "0x63",
+            "Install error: 0x63",
             {},
             {},
         },
@@ -2449,9 +2449,8 @@ INSTANTIATE_TEST_SUITE_P(
             base::StrCat({"INSTALLER_RESULT=2 INSTALLER_ERROR=",
                           base::NumberToString(ERROR_SUCCESS_REBOOT_REQUIRED)}),
             ERROR_SUCCESS_REBOOT_REQUIRED,
-            "The requested operation is successful. Changes will not be "
-            "effective "
-            "until the system is rebooted. ",
+            "Install error: The requested operation is successful. Changes "
+            "will not be effective until the system is rebooted. ",
             {},
             {},
         },
