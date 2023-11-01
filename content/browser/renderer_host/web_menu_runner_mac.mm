86ced4c78fdc1050adbc05780f7575f0d4a9bb23
Avi Drissman <avi@chromium.org>
2023-11-01 20:45:33 +0000

    Update a broken link
    Bug: none
    Change-Id: Ie7c39db20e538918c9e4ebc8040b3819b4cfcf45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4995118
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218426}

--- content/browser/renderer_host/web_menu_runner_mac.mm
+++ content/browser/renderer_host/web_menu_runner_mac.mm
@@ -108,11 +108,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       [[NSAttributedString alloc] initWithString:title attributes:attrs];
   menuItem.attributedTitle = attrTitle;
 
-  // We set the title as well as the attributed title here. The attributed title
-  // will be displayed in the menu, but typeahead will use the non-attributed
-  // string that doesn't contain any leading or trailing whitespace. This is
-  // what Apple uses in WebKit as well:
-  // http://trac.webkit.org/browser/trunk/Source/WebKit2/UIProcess/mac/WebPopupMenuProxyMac.mm#L90
+  // Set the title as well as the attributed title here. The attributed title
+  // will be displayed in the menu, but type-ahead will use the non-attributed
+  // string that doesn't contain any leading or trailing whitespace.
+  //
+  // This is the approach that WebKit uses; see PopupMenuMac::populate():
+  // https://github.com/search?q=repo%3AWebKit/WebKit%20PopupMenuMac%3A%3Apopulate&type=code
   NSCharacterSet* whitespaceSet = [NSCharacterSet whitespaceCharacterSet];
   [menuItem setTitle:[title stringByTrimmingCharactersInSet:whitespaceSet]];
 
