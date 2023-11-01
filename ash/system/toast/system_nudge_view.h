1b18010ed28a5742239144c1c5f4d7e76194dd78
Kevin Radtke <kradtke@chromium.org>
2023-11-01 17:18:39 +0000

    Observe anchored nudge widget to update shadow bounds
    Update the nudge view's elements when widget bounds are updated by
    observing the widget, instead of relying on the widget to call a public
    function exposed by the view class.
    
    Bug: None
    Change-Id: I99cb74ce35ba35e85f37cd567564c4655b18522e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4968933
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218283}

--- ash/system/toast/system_nudge_view.h
+++ ash/system/toast/system_nudge_view.h
@@ -10,12 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/layout/flex_layout_view.h"
+#include "ui/views/widget/widget_observer.h"
 
 namespace views {
+class ImageButton;
 class ImageView;
 class Label;
 class LabelButton;
-class ImageButton;
+class Widget;
 }  // namespace views
 
 namespace ash {
@@ -27,7 +29,8 @@ class SystemShadow;
 // This view supports different configurations depending on the provided
 // nudge data parameters. It will always have a body text, and may have a
 // leading image view, a title text, and up to two buttons placed on the bottom.
-class ASH_EXPORT SystemNudgeView : public views::FlexLayoutView {
+class ASH_EXPORT SystemNudgeView : public views::FlexLayoutView,
+                                   public views::WidgetObserver {
  public:
   METADATA_HEADER(SystemNudgeView);
 
@@ -44,8 +47,16 @@ class ASH_EXPORT SystemNudgeView : public views::FlexLayoutView {
   views::LabelButton* first_button() const { return first_button_; }
   views::LabelButton* second_button() const { return second_button_; }
 
-  // Called when the device zoom scale changes, observed from the widget.
-  void UpdateShadowBounds();
+  // views::View:
+  void AddedToWidget() override;
+  void RemovedFromWidget() override;
+  void OnMouseEntered(const ui::MouseEvent& event) override;
+  void OnMouseExited(const ui::MouseEvent& event) override;
+
+  // views::WidgetObserver:
+  void OnWidgetBoundsChanged(views::Widget* widget,
+                             const gfx::Rect& new_bounds) override;
+  void OnWidgetDestroying(views::Widget* widget) override;
 
  private:
   // Owned by the views hierarchy.
@@ -57,11 +68,6 @@ class ASH_EXPORT SystemNudgeView : public views::FlexLayoutView {
   raw_ptr<views::ImageButton> close_button_ = nullptr;
 
   std::unique_ptr<SystemShadow> shadow_;
-
-  // views::View:
-  void AddedToWidget() override;
-  void OnMouseEntered(const ui::MouseEvent& event) override;
-  void OnMouseExited(const ui::MouseEvent& event) override;
 
   // Handles mouse enter/exit events to either show or hide `close_button_`.
   void HandleOnMouseHovered(const bool mouse_entered);
