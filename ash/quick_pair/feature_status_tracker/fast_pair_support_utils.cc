bd6eb033dfe57f90e51acfb7cfaad6b2fc7b4aa2
Hsin-chen Chuang <chharry@chromium.org>
2023-11-01 01:44:03 +0000

    [floss] FP/NS provider: Check power state before checking LE scan status
    This prevents generating error logs in
    GetLowEnergyScanSessionHardwareOffloadingStatus.
    
    BUG=b:270447662
    TEST=Toggle BT and verify the log
    
    Change-Id: I831fa37de782f49cccef349525197d099e097ebc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4988982
    Commit-Queue: Hsin-chen Chuang <chharry@google.com>
    Reviewed-by: Jack Shira <jackshira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1218034}

--- ash/quick_pair/feature_status_tracker/fast_pair_support_utils.cc
+++ ash/quick_pair/feature_status_tracker/fast_pair_support_utils.cc
@@ -17,7 +17,7 @@ bool HasHardwareSupport(scoped_refptr<device::BluetoothAdapter> adapter) {
     return false;
   }
 
-  if (!adapter || !adapter->IsPresent()) {
+  if (!adapter || !adapter->IsPresent() || !adapter->IsPowered()) {
     return false;
   }
 
@@ -25,6 +25,7 @@ bool HasHardwareSupport(scoped_refptr<device::BluetoothAdapter> adapter) {
     return true;
   }
 
+  // The function only returns correct status when adapter is powered.
   return adapter->GetLowEnergyScanSessionHardwareOffloadingStatus() ==
          device::BluetoothAdapter::
              LowEnergyScanSessionHardwareOffloadingStatus::kSupported;
