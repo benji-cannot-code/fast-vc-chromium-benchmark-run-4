6fd6691df957f48d23db64c4c241710bd6b35ce0
xiangdong kong <xiangdongkong@google.com>
2023-11-01 18:28:07 +0000

    feedback: Show Wifi debug logs checkbox for wifi related reports
    Visible when
    - Google internal users.
    - One of following keywords was mentioned in the description.
      - 'wifi', 'wi-fi', 'internet', 'network', 'hotspot'.
    
    The following further restriction will be implemented in followup CL:
    - Visible only when the UserFeedbackWithLowLevelDebugDataAllowed
      policy is enabled for Wifi.
    
    Bug: b/308196190
    Test: browser_tests --gtest_filter=OSFeedbackBrowserTest.MANUAL_* --run-manual
    Change-Id: I64f1e1bb4f229876c408bf63aea1ee51d7817b48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4995099
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1218329}

--- ash/webui/os_feedback_ui/resources/feedback_flow.js
+++ ash/webui/os_feedback_ui/resources/feedback_flow.js
@@ -159,6 +159,12 @@ const phoneHubRegEx =
     new RegExp('app[ ]?stream(ing)?|phone|camera[ ]?roll', 'i');
 
 /**
+ * Regular expression to check for wifi-related keywords.
+ */
+const wifiRegEx =
+    new RegExp('\\b(wifi|wi\-fi|internet|network|hotspot)\\b', 'i');
+
+/**
  * @fileoverview
  * 'feedback-flow' manages the navigation among the steps to be taken.
  */
@@ -623,7 +629,8 @@ export class FeedbackFlowElement extends PolymerElement {
 
   /** @private */
   computeShouldShowWifiDebugLogsCheckbox_() {
-    return this.feedbackContext_ && this.feedbackContext_.isInternalAccount;
+    return this.feedbackContext_ && this.feedbackContext_.isInternalAccount &&
+        wifiRegEx.test(this.description_);
   }
 
   /** @private */
