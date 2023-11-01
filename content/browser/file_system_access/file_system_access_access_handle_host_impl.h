19110f9d5b38b0c25f86dc60742852a3041d69a3
Austin Sullivan <asully@chromium.org>
2023-11-01 23:57:24 +0000

    FSA: Always initialize raw_ptr to nullptr in c/b/fsa
    See the first bullet point of the documentation here:
    https://chromium.googlesource.com/chromium/src/+/main/base/memory/raw_ptr.md
    
    Also converts one use of a true raw pointer in a member variable
    (whoops!) into a raw_ref
    
    Bug: N/A
    Change-Id: Ie3738947f63659f168589eea77d1bce73704b639
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4998597
    Reviewed-by: Christine Smith <christinesm@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218541}

--- content/browser/file_system_access/file_system_access_access_handle_host_impl.h
+++ content/browser/file_system_access/file_system_access_access_handle_host_impl.h
@@ -70,7 +70,7 @@ class FileSystemAccessAccessHandleHostImpl
   void OnDisconnect();
 
   // The FileSystemAccessManagerImpl that owns this instance.
-  const raw_ptr<FileSystemAccessManagerImpl> manager_;
+  const raw_ptr<FileSystemAccessManagerImpl> manager_ = nullptr;
 
   mojo::Receiver<blink::mojom::FileSystemAccessAccessHandleHost> receiver_;
 
