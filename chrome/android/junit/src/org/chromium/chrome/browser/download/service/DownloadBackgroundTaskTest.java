5ad556c7e15fe23e65043a71be09cfab2d811d7e
Shakti Sahu <shaktisahu@chromium.org>
2023-11-01 23:26:06 +0000

    Fixed test to execute on Android U
    Bug: 1483735
    Low-Coverage-Reason: TRIVIAL_CHANGE This is merely a code cleanup CL that doesn't introduce additional lines to be tested.
    Change-Id: I772d21d13ffe136c8f002c1bd0384ad316f639d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4941315
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Auto-Submit: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218529}

--- chrome/android/junit/src/org/chromium/chrome/browser/download/service/DownloadBackgroundTaskTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/download/service/DownloadBackgroundTaskTest.java
@@ -10,6 +10,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 
 import android.content.Context;
+import android.os.Build;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -50,8 +51,7 @@ import org.chromium.components.download.DownloadTaskType;
 
 /** Unit tests for {@link org.chromium.chrome.browser.download.service.DownloadBackgroundTask}. */
 @RunWith(BaseRobolectricTestRunner.class)
-// TODO(crbug/1483735): Update this to 34 once robolectric support is added.
-@Config(manifest = Config.NONE, sdk = 33)
+@Config(manifest = Config.NONE, sdk = Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
 @EnableFeatures(ChromeFeatureList.DOWNLOADS_MIGRATE_TO_JOBS_API)
 public class DownloadBackgroundTaskTest {
     @Rule public JniMocker mJniMocker = new JniMocker();
@@ -149,7 +149,6 @@ public class DownloadBackgroundTaskTest {
     @Test
     @Feature({"Download"})
     public void testIsUserInitiatedJob() {
-        DownloadUtils.setMinSdkVersionForUserInitiatedJobsForTesting(33);
         Assert.assertTrue(
                 DownloadUtils.isUserInitiatedJob(
                         TaskIds.DOWNLOAD_AUTO_RESUMPTION_UNMETERED_JOB_ID));
@@ -184,7 +183,6 @@ public class DownloadBackgroundTaskTest {
     @Feature({"Download"})
     @DisableFeatures(ChromeFeatureList.DOWNLOADS_MIGRATE_TO_JOBS_API)
     public void testIsUserInitiatedJobForDisabledFeature() {
-        DownloadUtils.setMinSdkVersionForUserInitiatedJobsForTesting(33);
         Assert.assertFalse(
                 DownloadUtils.isUserInitiatedJob(
                         TaskIds.DOWNLOAD_AUTO_RESUMPTION_UNMETERED_JOB_ID));
@@ -201,7 +199,6 @@ public class DownloadBackgroundTaskTest {
     @Test
     @Feature({"Download"})
     public void testScheduleTaskForUITask() {
-        DownloadUtils.setMinSdkVersionForUserInitiatedJobsForTesting(33);
         int taskType = DownloadTaskType.DOWNLOAD_AUTO_RESUMPTION_UNMETERED_TASK;
         DownloadTaskScheduler.scheduleTask(taskType, true, false, 0, 60, 120);
         Mockito.verify(mTaskScheduler, times(1)).schedule(any(), mTaskInfoCapture.capture());
@@ -221,7 +218,6 @@ public class DownloadBackgroundTaskTest {
     @Test
     @Feature({"Download"})
     public void testScheduleTaskForNonUITask() {
-        DownloadUtils.setMinSdkVersionForUserInitiatedJobsForTesting(33);
         int taskType = DownloadTaskType.DOWNLOAD_AUTO_RESUMPTION_TASK;
         DownloadTaskScheduler.scheduleTask(taskType, true, false, 0, 60, 120);
         Mockito.verify(mTaskScheduler, times(1)).schedule(any(), mTaskInfoCapture.capture());
