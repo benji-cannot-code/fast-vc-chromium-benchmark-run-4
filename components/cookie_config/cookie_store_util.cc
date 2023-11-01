12a8eff0998a1cd6ef469f28b36e8b0040868273
Will Harris <wfh@chromium.org>
2023-11-01 19:54:34 +0000

    Stop supporting import of old encrypted cookies on iOS
    In 2015, https://codereview.chromium.org/1276493002 landed to support
    the unencryption of encrypted cookies that had been accidentally
    encrypted in https://codereview.chromium.org/135183021.
    
    Since 2015, no new cookies have been encrypted on iOS so there should
    be none remaining that are still encrypted that need to be
    unencrypted - cookies usually expire far more recently than
    nine years ago.
    
    Therefore, revert the code to simplify the net::CookieCryptoDelegate,
    including the test that was added. Note: The migration from
    unencrypted to encrypted is still tested in the UpdateToEncryption
    test.
    
    BUG=517071
    
    Change-Id: I52ae06d396948e68e87ecc6230c680cf9eaf6790
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4986083
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218394}

--- components/cookie_config/cookie_store_util.cc
+++ components/cookie_config/cookie_store_util.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cookie_config {
 
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_LINUX) || \
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
     BUILDFLAG(IS_CHROMEOS)
 namespace {
 
@@ -21,29 +21,11 @@ namespace {
 // because ChromeOS and Android already protect the entire profile contents.
 class CookieOSCryptoDelegate : public net::CookieCryptoDelegate {
  public:
-  bool ShouldEncrypt() override;
   bool EncryptString(const std::string& plaintext,
                      std::string* ciphertext) override;
   bool DecryptString(const std::string& ciphertext,
                      std::string* plaintext) override;
 };
-
-bool CookieOSCryptoDelegate::ShouldEncrypt() {
-#if BUILDFLAG(IS_IOS)
-  // Cookie encryption is not necessary on iOS, due to OS-protected storage.
-  // However, due to https://codereview.chromium.org/135183021/, cookies were
-  // accidentally encrypted. In order to allow these cookies to still be used,a
-  // a CookieCryptoDelegate is provided that can decrypt existing cookies.
-  // However, new cookies will not be encrypted. The alternatives considered
-  // were not supplying a delegate at all (thus invalidating all existing
-  // encrypted cookies) or in migrating all cookies at once, which may impose
-  // startup costs.  Eventually, all cookies will get migrated as they are
-  // rewritten.
-  return false;
-#else
-  return true;
-#endif
-}
 
 bool CookieOSCryptoDelegate::EncryptString(const std::string& plaintext,
                                            std::string* ciphertext) {
@@ -65,12 +47,12 @@ base::LazyInstance<CookieOSCryptoDelegate>::DestructorAtExit
 net::CookieCryptoDelegate* GetCookieCryptoDelegate() {
   return g_cookie_crypto_delegate.Pointer();
 }
-#else   // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_LINUX) ||
+#else   // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) ||
         // BUILDFLAG(IS_CHROMEOS)
 net::CookieCryptoDelegate* GetCookieCryptoDelegate() {
-  return NULL;
+  return nullptr;
 }
-#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_LINUX) ||
+#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) ||
         // BUILDFLAG(IS_CHROMEOS)
 
 }  // namespace cookie_config
