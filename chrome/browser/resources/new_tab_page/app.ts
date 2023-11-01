e2ff80e2d2f282aa173217e51f9cc267f445ac9d
Roman Arora <romanarora@chromium.org>
2023-11-01 16:41:47 +0000

    [ntp] Remove webstore toast listener on element disconnect
    Fixed: 1475040
    Change-Id: I4045e6e8949629f38f523e345f01aea6070c0a52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4990397
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1218264}

--- chrome/browser/resources/new_tab_page/app.ts
+++ chrome/browser/resources/new_tab_page/app.ts
@@ -413,16 +413,15 @@ export class AppElement extends AppElementBase {
               this.showCustomize_ = visible;
             });
     this.showWebstoreToastListenerId_ =
-        NewTabPageProxy.getInstance()
-            .callbackRouter.showWebstoreToast.addListener(() => {
-              if (this.showCustomize_) {
-                const toast = $$<CrToastElement>(this, '#webstoreToast');
-                if (toast) {
-                  toast!.hidden = false;
-                  toast!.show();
-                }
-              }
-            });
+        this.callbackRouter_.showWebstoreToast.addListener(() => {
+          if (this.showCustomize_) {
+            const toast = $$<CrToastElement>(this, '#webstoreToast');
+            if (toast) {
+              toast!.hidden = false;
+              toast!.show();
+            }
+          }
+        });
 
     // Open Customize Chrome if there are Customize Chrome URL params.
     if (this.showCustomize_) {
@@ -473,6 +472,7 @@ export class AppElement extends AppElementBase {
     this.callbackRouter_.removeListener(this.setThemeListenerId_!);
     this.callbackRouter_.removeListener(
         this.setCustomizeChromeSidePanelVisibilityListener_!);
+    this.callbackRouter_.removeListener(this.showWebstoreToastListenerId_!);
     this.eventTracker_.removeAll();
   }
 
