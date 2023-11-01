e9cc84a1a9b8f415f961df724fd65c6c3469bfa8
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2023-11-01 00:15:45 +0000

    Import wpt@0df152f447ea95ed9e64d84bf7ed8cfc3d677e92
    Using wpt-import in Chromium 26b32f461c85ff9e9858c1985810a5c7635b3d92.
    With Chromium commits locally applied on WPT:
    4d35a57f42 "Support adding iframes with srcdoc attributes in remote context helper."
    
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I95b1f199b4b2a709e3dc7fbf4b405d16e4c68863
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994216
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1217992}

--- third_party/blink/web_tests/external/wpt/webcodecs/full-cycle-test.https.any.js
+++ third_party/blink/web_tests/external/wpt/webcodecs/full-cycle-test.https.any.js
@@ -69,6 +69,9 @@ promise_setup(async () => {
 
 async function runFullCycleTest(t, options) {
   let encoder_config = { ...ENCODER_CONFIG };
+  if (options.realTimeLatencyMode) {
+    encoder_config.latencyMode = 'realtime';
+  }
   let encoder_color_space = {};
   const w = encoder_config.width;
   const h = encoder_config.height;
@@ -161,6 +164,10 @@ async function runFullCycleTest(t, options) {
 promise_test(async t => {
   return runFullCycleTest(t, {});
 }, 'Encoding and decoding cycle');
+
+promise_test(async t => {
+  return runFullCycleTest(t, {realTimeLatencyMode: true});
+}, 'Encoding and decoding cycle with realtime latency mode');
 
 promise_test(async t => {
   if (ENCODER_CONFIG.hasEmbeddedColorSpace)
