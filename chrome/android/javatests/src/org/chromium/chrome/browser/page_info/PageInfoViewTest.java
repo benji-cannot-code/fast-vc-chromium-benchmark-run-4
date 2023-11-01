c5dc923f9115e2dde7d9b281a27b38d651cd04fc
Tommy Nyquist <nyquist@chromium.org>
2023-11-01 00:26:01 +0000

    [Android] Disable PageInfoViewTest for incrementing min SDK
    In preparation of incrementing the minimum SDK version, disabling this
    test so we can figure out a fix for it later.
    
    Bug: 1498268
    Change-Id: I62d27b4b7d952a00d5af581deb73fba2a4059e49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4996398
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Auto-Submit: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217996}

--- chrome/android/javatests/src/org/chromium/chrome/browser/page_info/PageInfoViewTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/page_info/PageInfoViewTest.java
@@ -1079,6 +1079,7 @@ public class PageInfoViewTest {
     @Test
     @MediumTest
     @Feature({"RenderTest"})
+    @DisabledTest(message = "crbug.com/1498268")
     public void testShowAdPersonalizationInfoSubPageV4() throws IOException {
         loadUrlAndOpenPageInfo(
                 mTestServerRule.getServer().getURLWithHostName("example.com", sSimpleHtml));
