2012e6a0e8c4ab030eabec7731900c9e553234fa
Luciano Pacheco <lucmult@chromium.org>
2023-11-01 06:10:13 +0000

    Files app: Extend the Office integration nudge expiry
    Bug: b:308719130
    Change-Id: I390498fd82afce0318fbdd6787733bd72d224f17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4997128
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218107}

--- ui/file_manager/file_manager/containers/nudge_container.ts
+++ ui/file_manager/file_manager/containers/nudge_container.ts
@@ -445,8 +445,7 @@ export const nudgeInfo: {[type in NudgeType]: NudgeInfo} = {
     },
     content: () => str('ONE_DRIVE_MOVED_FILE_NUDGE'),
     direction: NudgeDirection.TRAILING_DOWNWARD,
-    // Expire after 4 releases (expires when M120 hits Stable).
-    expiryDate: new Date(2023, 12, 5),
+    expiryDate: new Date(2025, 12, 5),
     selfDismiss: true,
     dismissOnKeyDown: treeDismissOnKeyDownOnTreeItem,
   },
@@ -460,8 +459,7 @@ export const nudgeInfo: {[type in NudgeType]: NudgeInfo} = {
     },
     content: () => str('DRIVE_MOVED_FILE_NUDGE'),
     direction: NudgeDirection.TRAILING_DOWNWARD,
-    // Expire after 4 releases (expires when M120 hits Stable).
-    expiryDate: new Date(2023, 12, 5),
+    expiryDate: new Date(2025, 12, 5),
     selfDismiss: true,
     dismissOnKeyDown: treeDismissOnKeyDownOnTreeItem,
   },
