76a0b7756fc094e7f3bbe955f1eccad0882e3f56
ahmedmoussa <ahmedmoussa@google.com>
2023-11-01 22:20:54 +0000

    [live_preview] Live media (Camera/Mic) preview, Base CL
    Design Doc (internal-only for now): https://docs.google.com/document/d/1G66-h7n4834XPmOzvOn3AzJBMsDIe5rrNVUOxdiBrK4/edit
    
    This is the base CL of the live preview, more to follow.
    
    Disabled-by-default behind feature flag `kCameraMicPreview`.
    
    Bug: b:296946932
    Change-Id: I704e87263d6197d189fae7be5a18c1c0bab7c1c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4771087
    Reviewed-by: Elaine Chien <elainechien@chromium.org>
    Commit-Queue: Ahmed Moussa <ahmedmoussa@google.com>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218489}

---
+++ chrome/browser/ui/views/media_preview/media_coordinator.cc
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/media_preview/media_coordinator.h"
+
+#include <memory>
+
+#include "chrome/browser/ui/views/chrome_layout_provider.h"
+#include "chrome/browser/ui/views/media_preview/media_view.h"
+#include "ui/views/background.h"
+#include "ui/views/border.h"
+#include "ui/views/view.h"
+
+MediaCoordinator::MediaCoordinator(ViewType view_type,
+                                   views::View& parent_view,
+                                   absl::optional<size_t> index,
+                                   bool is_subsection) {
+  auto* media_view =
+      parent_view.AddChildViewAt(std::make_unique<MediaView>(is_subsection),
+                                 index.value_or(parent_view.children().size()));
+
+  if (!is_subsection) {
+    auto* provider = ChromeLayoutProvider::Get();
+    const int kRoundedRadius = provider->GetCornerRadiusMetric(
+        views::ShapeContextTokens::kOmniboxExpandedRadius);
+    const int kBorderThickness =
+        provider->GetDistanceMetric(views::DISTANCE_RELATED_CONTROL_VERTICAL);
+
+    media_view->SetBorder(views::CreateThemedRoundedRectBorder(
+        kBorderThickness, kRoundedRadius, ui::kColorButtonBorder));
+    media_view->SetBackground(views::CreateThemedRoundedRectBackground(
+        ui::kColorButtonBorder, kRoundedRadius));
+  }
+}
+
+MediaCoordinator::~MediaCoordinator() = default;
---
+++ chrome/browser/ui/views/media_preview/media_coordinator.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_MEDIA_PREVIEW_MEDIA_COORDINATOR_H_
+#define CHROME_BROWSER_UI_VIEWS_MEDIA_PREVIEW_MEDIA_COORDINATOR_H_
+
+#include <stddef.h>
+
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
+namespace views {
+class View;
+}  // namespace views
+
+// MediaCoordinator sets up the media views.
+class MediaCoordinator {
+ public:
+  enum class ViewType { kBoth, kCameraOnly, kMicOnly };
+
+  MediaCoordinator(ViewType view_type,
+                   views::View& parent_view,
+                   absl::optional<size_t> index,
+                   bool is_subsection);
+  MediaCoordinator(const MediaCoordinator&) = delete;
+  MediaCoordinator& operator=(const MediaCoordinator&) = delete;
+  ~MediaCoordinator();
+};
+
+#endif  // CHROME_BROWSER_UI_VIEWS_MEDIA_PREVIEW_MEDIA_COORDINATOR_H_
