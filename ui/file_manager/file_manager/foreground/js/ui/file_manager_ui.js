f88bd284d6605ad3ee1ac917ca8959e5062e105e
Ben Reich <benreich@chromium.org>
2023-11-02 07:08:52 +0000

    [FilesTS] Remove icons.js and use icons.html.js directly
    Bug: b/289003444
    Change-Id: Icd3258593f46e501e692cae4ab72f84564dee625
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4995225
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218664}

--- ui/file_manager/file_manager/foreground/js/ui/file_manager_ui.js
+++ ui/file_manager/file_manager/foreground/js/ui/file_manager_ui.js
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import '../../elements/icons.html.js';
+
 import {assertInstanceof} from 'chrome://resources/ash/common/assert.js';
 
 import {DialogType} from '../../../common/js/dialog_type.js';
