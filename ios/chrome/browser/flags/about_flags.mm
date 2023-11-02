92df9a5fc5f787a2807036f2c2895f824db5d052
Ankush Singh <ankushkush@google.com>
2023-11-02 11:15:33 +0000

    [iOS] Add feature flag to guard batch upload from bookmarks manager
    This CL adds a feature flag for batch upload entry point from bookmarks
    manager, which would be launched as a fast followup to
    kReplaceSyncPromosWithSignInPromos.
    
    Bug: 1451511
    Change-Id: I2cba0534cdd1e878ad6e45a4b03064ddd81faf25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4995147
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218730}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -656,7 +656,8 @@ const FeatureEntry::Choice kReplaceSyncPromosWithSignInPromosChoices[] = {
      "SyncEnableWalletMetadataInTransportMode,"
      "SyncEnableWalletOfferInTransportMode,"
      "IOSPasswordSettingsBulkUploadLocalPasswords,"
-     "HistoryOptInForRestoreShortyAndReSignin"},
+     "HistoryOptInForRestoreShortyAndReSignin,"
+     "EnableBatchUploadFromBookmarksManager"},
 };
 
 const FeatureEntry::FeatureParam kOneTapForMapsConsentModeDefault[] = {
