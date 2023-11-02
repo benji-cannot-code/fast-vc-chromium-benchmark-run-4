11b64b67fc15f91c6a061bd0c910b5c58b82ccbd
Danny Wang <wangdanny@google.com>
2023-11-02 00:39:38 +0000

    customization: Show notification for peripheral customization
    Bug: b/241965717
    Test: ash_unittests
    Change-Id: I78465b5b0675cfc54c734ab27077eb0ff9a65ff4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4976086
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: Danny Wang <wangdanny@google.com>
    Cr-Commit-Position: refs/heads/main@{#1218566}

--- ash/system/input_device_settings/input_device_settings_notification_controller.h
+++ ash/system/input_device_settings/input_device_settings_notification_controller.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_SYSTEM_INPUT_DEVICE_SETTINGS_INPUT_DEVICE_SETTINGS_NOTIFICATION_CONTROLLER_H_
 
 #include "ash/ash_export.h"
+#include "ash/public/mojom/input_device_settings.mojom.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "ui/events/ash/mojom/simulate_right_click_modifier.mojom-shared.h"
@@ -60,6 +61,15 @@ class ASH_EXPORT InputDeviceSettingsNotificationController {
       ui::mojom::SixPackShortcutModifier blocked_modifier,
       ui::mojom::SixPackShortcutModifier active_modifier,
       int device_id);
+
+  // Used to display a notification when a customizable mouse is connected to
+  // the chromebook for the first time.
+  void NotifyMouseIsCustomizable(const mojom::Mouse& mouse);
+
+  // Used to display a notification when a customizable graphics tablet is
+  // connected to the chromebook for the first time.
+  void NotifyGraphicsTabletIsCustomizable(
+      const mojom::GraphicsTablet& graphics_tablet);
 
  private:
   void HandleRightClickNotificationClicked(const std::string& notification_id,
