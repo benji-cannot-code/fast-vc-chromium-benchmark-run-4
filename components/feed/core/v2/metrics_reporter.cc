f125e18496858e137658eee15a5cc8e5c2afd810
Elly <ellyjones@chromium.org>
2023-11-02 22:11:29 +0000

    sharing: remove last remnants of Crow
    Specifically, remove:
    * The feature controlling it
    * The file containing that feature, which is now empty
    * The configuration class for it
    * The build rules for it
    * The constant used for logging its metrics
    * The code responsible for deleting its old configuration component data
    
    Fixed: 1423159
    Change-Id: I2e5d2aef81fd086d937138fddf6405ba5b2a899c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4998636
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Auto-Submit: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219150}

--- components/feed/core/v2/metrics_reporter.cc
+++ components/feed/core/v2/metrics_reporter.cc
@@ -320,7 +320,6 @@ void ReportCombinedSubscriptionCountAtEngagementTime(int subscription_count) {
 bool IsGoodExplicitInteraction(FeedUserActionType action) {
   switch (action) {
     case FeedUserActionType::kAddedToReadLater:
-    case FeedUserActionType::kTappedCrowButton:
     case FeedUserActionType::kTappedFollowButton:
     case FeedUserActionType::kShare:
     case FeedUserActionType::kTappedAddToReadingList:
@@ -728,7 +727,6 @@ void MetricsReporter::OtherUserAction(const StreamType& stream_type,
       RecordInteraction(stream_type);
       break;
     case FeedUserActionType::kAddedToReadLater:
-    case FeedUserActionType::kTappedCrowButton:
     case FeedUserActionType::kTappedFollowButton:
     case FeedUserActionType::kEphemeralChange:
     case FeedUserActionType::kEphemeralChangeRejected:
