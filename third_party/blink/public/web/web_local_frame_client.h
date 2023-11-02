2e31d4149e7f2c1f968ff36dcc30fd233619f414
Jose Dapena Paz <jdapena@igalia.com>
2023-11-02 15:17:14 +0000

    IWYU: returning nullptr of WebBackgroundResourceFetchAssets scoped_refptr requires declaration
    Bug: 957519
    Change-Id: Ief6471b89bc60812866cdc2d4e76575b32c4c6c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4993184
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218823}

--- third_party/blink/public/web/web_local_frame_client.h
+++ third_party/blink/public/web/web_local_frame_client.h
@@ -70,6 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/modules/service_worker/web_service_worker_provider.h"
 #include "third_party/blink/public/platform/resource_load_info_notifier_wrapper.h"
 #include "third_party/blink/public/platform/url_loader_throttle_provider.h"
+#include "third_party/blink/public/platform/web_background_resource_fetch_assets.h"
 #include "third_party/blink/public/platform/web_common.h"
 #include "third_party/blink/public/platform/web_content_security_policy_struct.h"
 #include "third_party/blink/public/platform/web_content_settings_client.h"
