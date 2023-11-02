4b5b663c080f2a31d867ff5841fac45fe5776666
Patrick Monette <pmonette@chromium.org>
2023-11-02 17:21:18 +0000

    Add WebContentsObserver::OnFrameIsCapturingVideoStreamChanged()
    This new method will allow observers to get notified when a frame
    is currently capturing a video stream. This will be used to move the
    logic of render process backgrounding to the performance manager.
    
    To do this, the VideoCaptureHost is changed to be associated with
    the hosting RenderFrameHost, instead of the RenderProcessHost.
    
    Bug: 1077217
    Change-Id: I4bb84632fa87390fd6409c8d04c155c21994c00a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4907955
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218916}

--- content/browser/renderer_host/media/video_capture_host.cc
+++ content/browser/renderer_host/media/video_capture_host.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/browser_main_loop.h"
 #include "content/browser/renderer_host/media/media_stream_manager.h"
 #include "content/browser/renderer_host/media/video_capture_manager.h"
+#include "content/browser/renderer_host/render_frame_host_impl.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/content_browser_client.h"
@@ -29,9 +30,10 @@ namespace content {
 
 namespace {
 
-BrowserContext* GetBrowserContext(uint32_t render_process_id) {
+BrowserContext* GetBrowserContext(
+    GlobalRenderFrameHostId render_frame_host_id) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  RenderProcessHost* host = RenderProcessHost::FromID(render_process_id);
+  RenderFrameHost* host = RenderFrameHost::FromID(render_frame_host_id);
   if (host) {
     return host->GetBrowserContext();
   }
@@ -40,76 +42,77 @@ BrowserContext* GetBrowserContext(uint32_t render_process_id) {
 
 }  // namespace
 
-VideoCaptureHost::RenderProcessHostDelegate::~RenderProcessHostDelegate() =
-    default;
+VideoCaptureHost::RenderFrameHostDelegate::~RenderFrameHostDelegate() = default;
 
-// Looks up a RenderProcessHost on demand based on a given |render_process_id|
-// and invokes OnMediaStreamAdded() and OnMediaStreamRemoved(). It should be
-// called and destroyed on UI thread.
-class VideoCaptureHost::RenderProcessHostDelegateImpl
-    : public VideoCaptureHost::RenderProcessHostDelegate {
+// Looks up a RenderFrameHost on demand based on a given |render_frame_host_id|
+// and invokes OnMediaStreamAdded() and OnMediaStreamRemoved().
+class VideoCaptureHost::RenderFrameHostDelegateImpl
+    : public VideoCaptureHost::RenderFrameHostDelegate {
  public:
-  explicit RenderProcessHostDelegateImpl(uint32_t render_process_id)
-      : render_process_id_(render_process_id) {}
-
-  RenderProcessHostDelegateImpl(const RenderProcessHostDelegateImpl&) = delete;
-  RenderProcessHostDelegateImpl& operator=(
-      const RenderProcessHostDelegateImpl&) = delete;
-
-  ~RenderProcessHostDelegateImpl() override {
+  explicit RenderFrameHostDelegateImpl(
+      GlobalRenderFrameHostId render_frame_host_id)
+      : render_frame_host_id_(render_frame_host_id) {
     DCHECK_CURRENTLY_ON(BrowserThread::IO);
   }
 
-  // Helper functions that are used for notifying Browser-side RenderProcessHost
-  // if renderer is currently consuming video capture. This information is then
-  // used to determine if the renderer process should be backgrounded or not.
+  RenderFrameHostDelegateImpl(const RenderFrameHostDelegateImpl&) = delete;
+  RenderFrameHostDelegateImpl& operator=(const RenderFrameHostDelegateImpl&) =
+      delete;
+
+  ~RenderFrameHostDelegateImpl() override {
+    DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  }
+
+  // Helper functions that are used for notifying Browser-side RenderFrameHost
+  // if it is currently consuming video capture. This information is then used
+  // to determine if the frame's renderer process should be backgrounded or not.
   void NotifyStreamAdded() override {
     DCHECK_CURRENTLY_ON(BrowserThread::IO);
     GetUIThreadTaskRunner({})->PostTask(
         FROM_HERE, base::BindOnce(
-                       [](uint32_t render_process_id) {
-                         RenderProcessHost* host =
-                             RenderProcessHost::FromID(render_process_id);
+                       [](GlobalRenderFrameHostId render_frame_host_id) {
+                         RenderFrameHostImpl* host =
+                             RenderFrameHostImpl::FromID(render_frame_host_id);
                          if (host) {
-                           host->OnMediaStreamAdded();
+                           host->OnVideoStreamAdded();
                          }
                        },
-                       render_process_id_));
+                       render_frame_host_id_));
   }
 
   void NotifyStreamRemoved() override {
     DCHECK_CURRENTLY_ON(BrowserThread::IO);
     GetUIThreadTaskRunner({})->PostTask(
         FROM_HERE, base::BindOnce(
-                       [](uint32_t render_process_id) {
-                         RenderProcessHost* host =
-                             RenderProcessHost::FromID(render_process_id);
+                       [](GlobalRenderFrameHostId render_frame_host_id) {
+                         RenderFrameHostImpl* host =
+                             RenderFrameHostImpl::FromID(render_frame_host_id);
                          if (host) {
-                           host->OnMediaStreamRemoved();
+                           host->OnVideoStreamRemoved();
                          }
                        },
-                       render_process_id_));
+                       render_frame_host_id_));
   }
 
-  uint32_t GetRenderProcessId() const override {
+  GlobalRenderFrameHostId GetRenderFrameHostId() const override {
     DCHECK_CURRENTLY_ON(BrowserThread::IO);
-    return render_process_id_;
+    return render_frame_host_id_;
   }
 
  private:
-  const uint32_t render_process_id_;
+  const GlobalRenderFrameHostId render_frame_host_id_;
 };
 
-VideoCaptureHost::VideoCaptureHost(uint32_t render_process_id,
+VideoCaptureHost::VideoCaptureHost(GlobalRenderFrameHostId render_frame_host_id,
                                    MediaStreamManager* media_stream_manager)
     : VideoCaptureHost(
-          std::make_unique<RenderProcessHostDelegateImpl>(render_process_id),
+          std::make_unique<RenderFrameHostDelegateImpl>(render_frame_host_id),
           media_stream_manager) {}
 
 VideoCaptureHost::VideoCaptureHost(
-    std::unique_ptr<RenderProcessHostDelegate> delegate,
+    std::unique_ptr<RenderFrameHostDelegate> delegate,
     MediaStreamManager* media_stream_manager)
-    : render_process_host_delegate_(std::move(delegate)),
+    : render_frame_host_delegate_(std::move(delegate)),
       media_stream_manager_(media_stream_manager) {
   DVLOG(1) << __func__;
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
@@ -117,13 +120,13 @@ VideoCaptureHost::VideoCaptureHost(
 
 // static
 void VideoCaptureHost::Create(
-    uint32_t render_process_id,
+    GlobalRenderFrameHostId render_frame_host_id,
     MediaStreamManager* media_stream_manager,
     mojo::PendingReceiver<media::mojom::VideoCaptureHost> receiver) {
   DVLOG(1) << __func__;
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
   media_stream_manager->RegisterVideoCaptureHost(
-      std::make_unique<VideoCaptureHost>(render_process_id,
+      std::make_unique<VideoCaptureHost>(render_frame_host_id,
                                          media_stream_manager),
       std::move(receiver));
 }
@@ -302,7 +305,7 @@ void VideoCaptureHost::Start(
   GetUIThreadTaskRunner({})->PostTaskAndReplyWithResult(
       FROM_HERE,
       base::BindOnce(&GetBrowserContext,
-                     render_process_host_delegate_->GetRenderProcessId()),
+                     render_frame_host_delegate_->GetRenderFrameHostId()),
       base::BindOnce(&VideoCaptureHost::ConnectClient,
                      weak_factory_.GetWeakPtr(), session_id, params,
                      controller_id,
@@ -548,7 +551,7 @@ void VideoCaptureHost::DeleteVideoCaptureController(
 void VideoCaptureHost::NotifyStreamAdded() {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
   ++number_of_active_streams_;
-  render_process_host_delegate_->NotifyStreamAdded();
+  render_frame_host_delegate_->NotifyStreamAdded();
 }
 
 void VideoCaptureHost::NotifyStreamRemoved() {
@@ -560,7 +563,7 @@ void VideoCaptureHost::NotifyStreamRemoved() {
   if (number_of_active_streams_ == 0)
     return;
   --number_of_active_streams_;
-  render_process_host_delegate_->NotifyStreamRemoved();
+  render_frame_host_delegate_->NotifyStreamRemoved();
 }
 
 void VideoCaptureHost::NotifyAllStreamsRemoved() {
--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/render_frame_host_impl.h"
 
 #include <cstdint>
+#include <limits>
 #include <memory>
 #include <tuple>
 #include <unordered_map>
@@ -1779,6 +1780,10 @@ RenderFrameHostImpl::~RenderFrameHostImpl() {
     OnAudibleStateChanged(false);
   }
 
+  while (video_stream_count_) {
+    OnVideoStreamRemoved();
+  }
+
   // Resetting `document_associated_data_` destroys live `DocumentService` and
   // `DocumentUserData` instances. It is important for them to be
   // destroyed before the body of the `RenderFrameHostImpl` destructor
@@ -3230,6 +3235,14 @@ void RenderFrameHostImpl::RenderProcessGone(
   if (is_audible_)
     OnAudibleStateChanged(false);
 
+  // During fast-shutdown, avoid cleanup as the VideoCaptureHost will still send
+  // removal notifications after this function ends.
+  if (!GetProcess()->FastShutdownStarted()) {
+    while (video_stream_count_) {
+      OnVideoStreamRemoved();
+    }
+  }
+
   ++renderer_exit_count_;
 
   if (base::FeatureList::IsEnabled(features::kCrashReporting))
@@ -3738,6 +3751,31 @@ void RenderFrameHostImpl::OnAudibleStateChanged(bool is_audible) {
   }
   is_audible_ = is_audible;
   delegate_->OnFrameAudioStateChanged(this, is_audible_);
+}
+
+void RenderFrameHostImpl::OnVideoStreamAdded() {
+  CHECK_NE(video_stream_count_, std::numeric_limits<int>::max());
+  ++video_stream_count_;
+
+  // Only notify on the first video stream, as both the RenderProcessHost and
+  // the delegate only care about the existence of at least 1 stream, but not
+  // the exact count.
+  if (video_stream_count_ == 1) {
+    GetProcess()->OnMediaStreamAdded();
+    delegate_->OnFrameIsCapturingVideoStreamChanged(this, true);
+  }
+}
+
+void RenderFrameHostImpl::OnVideoStreamRemoved() {
+  CHECK(video_stream_count_);
+  --video_stream_count_;
+
+  // Only notify the delegate if this is the last video stream that was removed
+  // to match the behavior in `OnVideoStreamAdded`.
+  if (video_stream_count_ == 0) {
+    GetProcess()->OnMediaStreamRemoved();
+    delegate_->OnFrameIsCapturingVideoStreamChanged(this, false);
+  }
 }
 
 void RenderFrameHostImpl::DidAddMessageToConsole(
