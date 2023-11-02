1a316d72c2fafc260d8e8bfae69262a30f32db99
Darwin Yang <darwinyang@google.com>
2023-11-02 20:00:00 +0000

    [Autofill] Prevent click-jacking on the TTFCC dialog
    This CL adds an input protector to the TTFCC when a credit card is selected to prevent click-jacking.
    
    Bug: 1495153
    Change-Id: I0433d5ffcfbc9c3f650dc6c0bdc75b56a04fdb19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4998155
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Darwin Yang <darwinyang@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1219059}

--- chrome/browser/touch_to_fill/payments/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillCreditCardCoordinator.java
+++ chrome/browser/touch_to_fill/payments/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillCreditCardCoordinator.java
@@ -89,4 +89,8 @@ public class TouchToFillCreditCardCoordinator implements TouchToFillCreditCardCo
     PropertyModel getModelForTesting() {
         return mTouchToFillCreditCardModel;
     }
+
+    TouchToFillCreditCardMediator getMediatorForTesting() {
+        return mMediator;
+    }
 }
