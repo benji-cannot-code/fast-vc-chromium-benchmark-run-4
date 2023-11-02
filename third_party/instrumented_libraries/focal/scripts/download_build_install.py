444b678ff79d4d25ba3a8d772010c5aa33f5b05c
Tom Anderson <thomasanderson@chromium.org>
2023-11-02 00:00:16 +0000

    [Instrumented Libraries] Fix zlib1g build
    A Focal update caused zlib tests to run, which fail due to
    uninstrumented dependencies.  This CL changes the build to prevent
    test running.
    
    R=thestig
    
    Change-Id: I62ad5cb473ee9ef1f97123fa7d45f365246f38c5
    Bug: 1496000
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4985815
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218543}

--- third_party/instrumented_libraries/focal/scripts/download_build_install.py
+++ third_party/instrumented_libraries/focal/scripts/download_build_install.py
@@ -241,7 +241,7 @@ class InstrumentedPackageBuilder(object):
     """Invokes `make install'."""
     self.make(['install'] + args, **kwargs)
 
-  def build_and_install(self):
+  def build_and_install(self, targets=[]):
     """Builds and installs the DSOs.
 
     Builds the package with ./configure + make, installs it to a temporary
@@ -254,7 +254,7 @@ class InstrumentedPackageBuilder(object):
     # Some makefiles use BUILDROOT or INSTALL_ROOT instead of DESTDIR.
     args = ['DESTDIR', 'BUILDROOT', 'INSTALL_ROOT']
     make_args = ['%s=%s' % (name, self.temp_dir()) for name in args]
-    self.make(make_args)
+    self.make(make_args + targets)
 
     self.make_install(make_args)
 
@@ -523,6 +523,11 @@ class NSSBuilder(InstrumentedPackageBuilder):
           shutil.copy(full_path, self.dest_libdir())
 
 
+class ZlibBuilder(InstrumentedPackageBuilder):
+  def build_and_install(self):
+    super().build_and_install(['libz.so.1.2.11'])
+
+
 class StubBuilder(InstrumentedPackageBuilder):
   def download_build_install(self):
     self._touch(os.path.join(self._destdir, '%s.txt' % self._package))
@@ -581,6 +586,8 @@ def main():
     builder = LibcurlBuilder(args, clobber)
   elif args.build_method == 'custom_libpci3':
     builder = Libpci3Builder(args, clobber)
+  elif args.build_method == 'custom_zlib':
+    builder = ZlibBuilder(args, clobber)
   elif args.build_method == 'debian':
     builder = DebianBuilder(args, clobber)
   elif args.build_method == 'meson':
