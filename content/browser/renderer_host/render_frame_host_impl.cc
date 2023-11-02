edbc103572da9723748065dd48cb6a31c18ac0e1
Aaron Leventhal <aleventhal@google.com>
2023-11-02 19:31:13 +0000

    [A11y] Quiet down AccessibilityFatalError
    These dump without crashing reports have been around for a while, but
    there is been an unexplained uptick. This quiets down the crashes to ~5%
    of their current levels, in order to keep noise down for those who are
    tracking crashes in the system. We are keeping the 5% so that we can
    continue to collect data.
    
    These dump without crashing reports represent bad data being sent from
    a accessibility tree source, such as Blink. They can be very difficult
    to debug, and there are a number of different types of reports. Having the reports helps -- we've used them in the past to address spikes
    and improve the situation over time.
    
    Bug: 1471373
    Change-Id: Iaea0da61a9e0e2334cd47e155637b8ea583df83c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994920
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219035}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/process/kill.h"
+#include "base/rand_util.h"
 #include "base/ranges/algorithm.h"
 #include "base/state_transitions.h"
 #include "base/strings/strcat.h"
@@ -2939,9 +2940,9 @@ void RenderFrameHostImpl::AccessibilityFatalError() {
     // in addition to the DumpWithoutCrashing() for the first reset.
     render_accessibility_->FatalError();
   } else {
-    // Crash keys set in BrowserAccessibilityManager::Unserialize().
-    if (accessibility_fatal_error_count_ == 1) {
-      // Only send crash report first time -- don't skew crash stats too much.
+    if (base::RandGenerator(20) == 0) {
+      // Only report crash ~5% of the time -- don't skew crash stats too much.
+      // Crash keys were set in BrowserAccessibilityManager::Unserialize().
       base::debug::DumpWithoutCrashing();
     }
     AccessibilityReset();
