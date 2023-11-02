e46a7bef3151b6e61cd9df5d95d7bec54ec686ab
Md Shahadat Hossain Shahin <shahinmd@google.com>
2023-11-02 10:43:15 +0000

    privacy-hub: Simplify microphone row implementation
    Current implementation creates a CrLinkRowElement like element from
    scratch. This CL replaces that with the built-in CrLinkRowElement.
    
    Bug: b:308583028
    Change-Id: I08263ead019dd38bb6e621f663d6931c96dc5f8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994784
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Md Shahadat Hossain Shahin <shahinmd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1218720}

--- chrome/test/data/webui/settings/chromeos/os_privacy_page/privacy_hub_subpage_test.ts
+++ chrome/test/data/webui/settings/chromeos/os_privacy_page/privacy_hub_subpage_test.ts
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://os-settings/lazy_load.js';
 
 import {MediaDevicesProxy, PrivacyHubBrowserProxyImpl, SettingsPrivacyHubSubpage} from 'chrome://os-settings/lazy_load.js';
-import {CrToggleElement, MetricsConsentBrowserProxyImpl, OsSettingsPrivacyPageElement, PaperTooltipElement, Router, routes, SecureDnsMode, settingMojom, SettingsToggleButtonElement} from 'chrome://os-settings/os_settings.js';
+import {CrLinkRowElement, CrToggleElement, MetricsConsentBrowserProxyImpl, OsSettingsPrivacyPageElement, PaperTooltipElement, Router, routes, SecureDnsMode, settingMojom, SettingsToggleButtonElement} from 'chrome://os-settings/os_settings.js';
 import {assert} from 'chrome://resources/js/assert.js';
 import {webUIListenerCallback} from 'chrome://resources/js/cr.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
@@ -803,12 +803,12 @@ suite('<settings-privacy-hub-subpage> app permissions', () => {
   test('Navigate to the microphone subpage', async () => {
     createSubpage();
 
-    const microphoneSubpageLinkWrapper =
-        privacyHubSubpage.shadowRoot!.querySelector<HTMLButtonElement>(
-            '#microphoneSubpageLinkWrapper');
-    assertTrue(!!microphoneSubpageLinkWrapper);
+    const microphoneSubpageLink =
+        privacyHubSubpage.shadowRoot!.querySelector<CrLinkRowElement>(
+            '#microphoneSubpageLink');
+    assertTrue(!!microphoneSubpageLink);
 
-    microphoneSubpageLinkWrapper.click();
+    microphoneSubpageLink.click();
     await waitAfterNextRender(privacyHubSubpage);
 
     assertEquals(
