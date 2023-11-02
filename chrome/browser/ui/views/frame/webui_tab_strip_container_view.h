70773e7aa9e31bfd62bdf94b6e910ef2db0fd3f9
Yuheng Huang <yuhengh@chromium.org>
2023-11-02 22:23:40 +0000

    [responsive toolbar] support new tab button to overflow
    In ChromeOS tablet mode when WebUI tab strip is enabled, the new tab
    button will show up on toolbar. This CL supports new tab button to
    overflow similar to other toolbar buttons. Currently the new tab
    is created when WebUI tab strip is created and removed when WebUI
    tab strip is removed. In order to support overflowing new tab button
    we need to make new tab button permanent in the toolbar and update
    its visibility to match WebUI tab strip's presence.
    
    Bug: 1497498,1463774
    Change-Id: Id0d16c03632b93be9c42eb955d1c781744f64ad6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4986143
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219156}

--- chrome/browser/ui/views/frame/webui_tab_strip_container_view.h
+++ chrome/browser/ui/views/frame/webui_tab_strip_container_view.h
@@ -77,7 +77,6 @@ class WebUITabStripContainerView : public TabStripUIEmbedder,
   views::NativeViewHost* GetNativeViewHost();
 
   // Control button. Must only be called once.
-  std::unique_ptr<views::View> CreateNewTabButton();
   std::unique_ptr<views::View> CreateTabCounter();
 
   views::View* tab_counter() const { return tab_counter_; }
@@ -87,7 +86,6 @@ class WebUITabStripContainerView : public TabStripUIEmbedder,
   // should be called instead. View::SetVisible() isn't sufficient since
   // the container's preferred size will change.
   void SetVisibleForTesting(bool visible);
-  views::View* new_tab_button_for_testing() const { return new_tab_button_; }
   views::WebView* web_view_for_testing() const { return web_view_; }
 
   // Finish the open or close animation if it's active.
@@ -110,8 +108,6 @@ class WebUITabStripContainerView : public TabStripUIEmbedder,
   // just this direction to animate open or closed.
   void EndDragToOpen(absl::optional<WebUITabStripDragDirection>
                          fling_direction = absl::nullopt);
-
-  void NewTabButtonPressed(const ui::Event& event);
 
   void TabCounterPressed(const ui::Event& event);
 
@@ -166,7 +162,6 @@ class WebUITabStripContainerView : public TabStripUIEmbedder,
   // This field is not a raw_ptr<> because of conflicting types in an
   // initializer list.
   RAW_PTR_EXCLUSION views::View* tab_counter_ = nullptr;
-  raw_ptr<views::View> new_tab_button_ = nullptr;
 
 #if BUILDFLAG(IS_WIN)
   // If the user interacts with Windows in a way that changes the width of the
