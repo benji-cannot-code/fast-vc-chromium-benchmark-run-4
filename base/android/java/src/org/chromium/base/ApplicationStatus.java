3e94fef022464c4e50e6239b3962fe244ce47269
Andrew Grieve <agrieve@chromium.org>
2023-11-02 14:51:38 +0000

    ApplicationStatus: Add assert to aid debugging
    Bug: 1498004
    Change-Id: I0e2910dc7590585b906cf2a3e136b8e1e1e5b92d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4997853
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218789}

--- base/android/java/src/org/chromium/base/ApplicationStatus.java
+++ base/android/java/src/org/chromium/base/ApplicationStatus.java
@@ -445,7 +445,7 @@ public class ApplicationStatus {
      * @param newState New state value.
      */
     private static void onStateChange(Activity activity, @ActivityState int newState) {
-        if (activity == null) throw new IllegalArgumentException("null activity is not supported");
+        assert activity != null;
 
         if (sActivity == null
                 || newState == ActivityState.CREATED
@@ -683,6 +683,8 @@ public class ApplicationStatus {
         assert activity != null;
 
         ActivityInfo info = sActivityInfo.get(activity);
+        assert info != null
+                : "destroyed: " + activity.isDestroyed() + " finishing: " + activity.isFinishing();
         assert info.getStatus() != ActivityState.DESTROYED;
         info.getListeners().addObserver(listener);
     }
