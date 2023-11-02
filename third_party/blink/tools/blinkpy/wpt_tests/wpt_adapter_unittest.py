091873f087ecb00aaf3b2e31347bff7a0a818ff2
Jonathan Lee <jonathanjlee@google.com>
2023-11-02 21:27:32 +0000

    [wptrunner] Replace `.ini` for wdspec tests
    Per [0], return to recording expectations with TestExpectations and
    `*-expected.txt` files next to wdspec tests, and clean up the
    corresponding unused `.ini` files.
    
    [0]: https://groups.google.com/a/chromium.org/g/blink-dev/c/rOs0s4Bj3kU/m/TXTPIy0SAAAJ
    
    Bug: 1482887, 1492264
    Change-Id: I78acf9833153bb7b5f05cc5849712dae5a81e5f6
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_dbg_ng
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4995931
    Reviewed-by: Thiago Perrotta <tperrotta@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1219113}

--- third_party/blink/tools/blinkpy/wpt_tests/wpt_adapter_unittest.py
+++ third_party/blink/tools/blinkpy/wpt_tests/wpt_adapter_unittest.py
@@ -364,7 +364,7 @@ class WPTAdapterTest(unittest.TestCase):
             '--enable-sanitizer'
         ])
         with adapter.test_env() as options:
-            self.assertEqual(options.timeout_multiplier, 2)
+            self.assertAlmostEqual(options.timeout_multiplier, 5)
             run_info = self._read_run_info(options)
             self.assertTrue(run_info['sanitizer_enabled'])
 
