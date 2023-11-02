2bbee53065405b3a24b2dacb3a6b8c18afbbf3ce
Elly <ellyjones@chromium.org>
2023-11-02 20:01:15 +0000

    follow: remove #upcoming-follow-features
    This flag and the backing feature it is tied to are no longer used
    for anything.
    
    Fixed: 1490239
    Change-Id: I7d87c5f9546b70ee06c6a2ff005d3d3b2d85f2c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4998725
    Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    Auto-Submit: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219061}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/feature_guide/notifications/feature_notification_guide_service.h"
 #include "chrome/browser/file_system_access/file_system_access_features.h"
 #include "chrome/browser/flag_descriptions.h"
-#include "chrome/browser/follow/follow_features.h"
 #include "chrome/browser/ip_protection/ip_protection_switches.h"
 #include "chrome/browser/login_detection/login_detection_util.h"
 #include "chrome/browser/media/router/discovery/access_code/access_code_cast_constants.h"
@@ -9507,10 +9506,6 @@ const FeatureEntry kFeatureEntries[] = {
     {"enable-tab-audio-muting", flag_descriptions::kTabAudioMutingName,
      flag_descriptions::kTabAudioMutingDescription, kOsDesktop,
      FEATURE_VALUE_TYPE(media::kEnableTabMuting)},
-
-    {"upcoming-follow-features", flag_descriptions::kUpcomingFollowFeaturesName,
-     flag_descriptions::kUpcomingFollowFeaturesDescription, kOsAll,
-     FEATURE_VALUE_TYPE(follow::kUpcomingFollowFeatures)},
 
 #if defined(TOOLKIT_VIEWS)
     {"side-search", flag_descriptions::kSideSearchName,
