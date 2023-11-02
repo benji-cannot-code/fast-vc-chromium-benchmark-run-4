1bc66915f3dfe6731a116a466d05cffe36ffb9b2
Jonathan Lee <jonathanjlee@google.com>
2023-11-02 23:10:09 +0000

    [wpt-importer] Remove WPT metadata failure detection
    No longer needed after abandoning the `.ini` expectations format [1].
    
    [1]: https://groups.google.com/a/chromium.org/g/blink-dev/c/rOs0s4Bj3kU/m/TXTPIy0SAAAJ
    
    Fixed: 1492264
    Change-Id: Icccc4522e4b91490f6e71f3c7ec25bfd81a0802a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5003063
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1219186}

--- third_party/blink/tools/blinkpy/w3c/wpt_results_processor.py
+++ third_party/blink/tools/blinkpy/w3c/wpt_results_processor.py
@@ -21,6 +21,7 @@ from typing import (
     FrozenSet,
     Iterator,
     List,
+    Literal,
     NamedTuple,
     Optional,
     Set,
@@ -35,7 +36,6 @@ from blinkpy.common.html_diff import html_diff
 from blinkpy.common.memoized import memoized
 from blinkpy.common.system.filesystem import FileSystem
 from blinkpy.common.unified_diff import unified_diff
-from blinkpy.w3c import wpt_metadata
 from blinkpy.web_tests.port.base import Port
 from blinkpy.web_tests.models import test_failures
 from blinkpy.web_tests.models.testharness_results import (
@@ -68,6 +68,9 @@ _status_mapping = collections.OrderedDict([
     ('NOTRUN', ResultType.Failure),
 ])
 
+RunInfo = Dict[str, Any]
+TestType = Literal[tuple(wpttest.manifest_test_cls)]
+
 
 def wptrunner_to_chromium_status(status: str) -> str:
     return _status_mapping[status]
@@ -76,7 +79,7 @@ def wptrunner_to_chromium_status(status: str) -> str:
 @memoized
 def chromium_to_wptrunner_statuses(
     statuses: FrozenSet[str],
-    test_type: wpt_metadata.TestType,
+    test_type: TestType,
     subtest: bool = False,
 ) -> Set[str]:
     wptrunner_statuses = {
@@ -436,7 +439,7 @@ class WPTResultsProcessor:
 
     def suite_start(self,
                     event: Event,
-                    run_info: Optional[wpt_metadata.RunInfo] = None,
+                    run_info: Optional[RunInfo] = None,
                     **_):
         if run_info:
             self.run_info.update(run_info)
