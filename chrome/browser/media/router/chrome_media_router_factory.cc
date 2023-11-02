acd4b2458d36b724e6d5f79dc6177982efd18075
mark a. foltz <mfoltz@chromium.org>
2023-11-02 19:33:33 +0000

    [Media Router] Don't create MR for system profiles.
    There should be no need for Media Router in system profiles.  Disable
    it to reduce unexpected Media Router network traffic.
    
    Fixed: 1483551
    Change-Id: Ia17c1c0e5391cf41074badfc6d184cd7e9363b9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4995041
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219038}

--- chrome/browser/media/router/chrome_media_router_factory.cc
+++ chrome/browser/media/router/chrome_media_router_factory.cc
@@ -63,12 +63,11 @@ ChromeMediaRouterFactory::~ChromeMediaRouterFactory() = default;
 
 content::BrowserContext* ChromeMediaRouterFactory::GetBrowserContextToUse(
     content::BrowserContext* context) const {
-  // TODO(crbug.com/1483551): Figure out what to do with system profiles.
   ProfileSelections profile_selections =
       ProfileSelections::Builder()
           .WithRegular(ProfileSelection::kOwnInstance)
           .WithGuest(ProfileSelection::kOwnInstance)
-          .WithSystem(ProfileSelection::kOwnInstance)
+          .WithSystem(ProfileSelection::kNone)
           .Build();
   return profile_selections.ApplyProfileSelection(
       Profile::FromBrowserContext(context));
