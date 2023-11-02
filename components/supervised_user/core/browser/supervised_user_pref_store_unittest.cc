3324e6b8fbc8ae37e9e599a113651dcf395e8ebd
James Lee <ljjlee@google.com>
2023-11-02 14:50:46 +0000

    Don't force Safe Search for supervised users on some platforms
    Adds a "ForceGoogleSafeSearchForSupervisedUsers" feature flag which
    controls whether Chrome should force-enable Google SafeSearch.
    
    * On platforms with long-standing support for parental controls
    (Android and ChromeOS) the behaviour is unchanged (SafeSearch is
    forced-on).
    * On other platforms, Chrome does not force SafeSearch: the
    parental control setting from FamilyLink will apply instead.
    
    Bug: b/308582945, 1498825
    Change-Id: I3cac7eb0605559ece5f43876c4ceff274ae7e2aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994488
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: James Lee <ljjlee@google.com>
    Reviewed-by: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1218788}

--- components/supervised_user/core/browser/supervised_user_pref_store_unittest.cc
+++ components/supervised_user/core/browser/supervised_user_pref_store_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <string>
 
+#include "base/feature_list.h"
 #include "base/memory/ref_counted.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/values.h"
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_search_api/safe_search_util.h"
 #include "components/supervised_user/core/browser/supervised_user_pref_store.h"
 #include "components/supervised_user/core/browser/supervised_user_settings_service.h"
+#include "components/supervised_user/core/common/features.h"
 #include "components/supervised_user/core/common/pref_names.h"
 #include "components/supervised_user/core/common/supervised_user_constants.h"
 #include "components/sync/base/pref_names.h"
@@ -114,10 +116,19 @@ TEST_F(SupervisedUserPrefStoreTest, ConfigureSettings) {
   EXPECT_FALSE(fixture.changed_prefs()->FindDictByDottedPath(
       prefs::kSupervisedUserManualHosts));
 
-  // kForceGoogleSafeSearch defaults to true for supervised users.
-  EXPECT_THAT(fixture.changed_prefs()->FindBoolByDottedPath(
-                  policy::policy_prefs::kForceGoogleSafeSearch),
-              Optional(true));
+  // kForceGoogleSafeSearch defaults to true if the relevant feature flag is
+  // enabled.
+  if (base::FeatureList::IsEnabled(
+          supervised_user::kForceGoogleSafeSearchForSupervisedUsers)) {
+    EXPECT_THAT(fixture.changed_prefs()->FindBoolByDottedPath(
+                    policy::policy_prefs::kForceGoogleSafeSearch),
+                Optional(true));
+  } else {
+    EXPECT_FALSE(
+        fixture.changed_prefs()
+            ->FindBoolByDottedPath(policy::policy_prefs::kForceGoogleSafeSearch)
+            .has_value());
+  }
 
   // kForceYouTubeRestrict defaults to 'moderate' for supervised users on
   // Android and ChromeOS only.
