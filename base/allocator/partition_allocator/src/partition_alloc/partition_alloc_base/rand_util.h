eea690bad1b267c00a9cc2000e4f27a9e40d7210
Arthur Sonzogni <arthursonzogni@chromium.org>
2023-11-02 18:38:26 +0000

    PA: Remove GoogleTest dependency from the implementation.
    Dawn offers its CMake users the option not to fetch googletest
    when they don't have to build tests:
    https://source.chromium.org/chromium/chromium/src/+/main:third_party/dawn/tools/fetch_dawn_dependencies.py;l=114-117
    
    This patch removed googletest dependency from the implementation.
    
    Bug: 1464560
    Change-Id: I33b9cdba551caab0fbbfc127a9789201c6fe7e35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4974452
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218994}

--- base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/rand_util.h
+++ base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/rand_util.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/component_export.h"
-#include "base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/gtest_prod_util.h"
 #include "build/build_config.h"
 
 namespace partition_alloc {
@@ -73,6 +72,10 @@ class PA_COMPONENT_EXPORT(PARTITION_ALLOC_BASE) InsecureRandomGenerator {
   uint32_t RandUint32();
   uint64_t RandUint64();
 
+  static InsecureRandomGenerator ConstructForTesting() {
+    return InsecureRandomGenerator();
+  }
+
  private:
   InsecureRandomGenerator();
   // State.
@@ -89,14 +92,6 @@ class PA_COMPONENT_EXPORT(PARTITION_ALLOC_BASE) InsecureRandomGenerator {
   friend class ::partition_alloc::RandomGenerator;
   template <typename QuarantineEntry, size_t CountCapacity>
   friend class ::partition_alloc::internal::LightweightQuarantineList;
-
-  PA_FRIEND_TEST_ALL_PREFIXES(
-      PartitionAllocBaseRandUtilTest,
-      InsecureRandomGeneratorProducesBothValuesOfAllBits);
-  PA_FRIEND_TEST_ALL_PREFIXES(PartitionAllocBaseRandUtilTest,
-                              InsecureRandomGeneratorChiSquared);
-  PA_FRIEND_TEST_ALL_PREFIXES(PartitionAllocBaseRandUtilTest,
-                              InsecureRandomGeneratorRandDouble);
 };
 
 }  // namespace partition_alloc::internal::base
