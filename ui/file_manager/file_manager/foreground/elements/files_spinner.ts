c8c7caa0b717b728a2eeb86d1a123a2b5f7b7e3e
Ben Reich <benreich@chromium.org>
2023-11-02 06:21:29 +0000

    [FilesTS] Convert FilesSpinner
    Bug: b/289003444
    Change-Id: If52f0796aedc7ca52c6b01a916647869de78c706
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4996871
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218652}

--- ui/file_manager/file_manager/foreground/elements/files_spinner.ts
+++ ui/file_manager/file_manager/foreground/elements/files_spinner.ts
@@ -3,20 +3,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {getTemplate} from './files_spinner.html.js';
 
-/** @type {!HTMLTemplateElement} */
-const htmlTemplate = html`{__html_template__}`;
-
-/**
- * FilesSpinner.
- */
 export class FilesSpinner extends HTMLElement {
   constructor() {
     super();
 
-    const fragment = htmlTemplate.content.cloneNode(true);
+    const template = document.createElement('template');
+    template.innerHTML = getTemplate() as unknown as string;
+    const fragment = template.content.cloneNode(true);
     this.attachShadow({mode: 'open'}).appendChild(fragment);
+  }
+
+  static get is() {
+    return 'files-spinner' as const;
   }
 
   /**
@@ -26,7 +26,7 @@ export class FilesSpinner extends HTMLElement {
     if (!this.shadowRoot) {
       return;
     }
-    const host = /** @type {!HTMLElement} */ (this.shadowRoot.host);
+    const host = this.shadowRoot.host;
 
     host.setAttribute('role', 'progressbar');
     host.setAttribute('aria-disabled', 'false');
@@ -35,6 +35,10 @@ export class FilesSpinner extends HTMLElement {
   }
 }
 
-customElements.define('files-spinner', FilesSpinner);
+declare global {
+  interface HTMLElementTagNameMap {
+    [FilesSpinner.is]: FilesSpinner;
+  }
+}
 
-//# sourceURL=//ui/file_manager/file_manager/foreground/elements/files_spinner.js
+customElements.define(FilesSpinner.is, FilesSpinner);
