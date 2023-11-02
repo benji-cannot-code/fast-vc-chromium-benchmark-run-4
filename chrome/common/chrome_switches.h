7c38bcb5486131ebd753bce192eb63d4e327586e
Etienne Bergeron <etienneb@chromium.org>
2023-11-02 16:46:25 +0000

    Remove deadcode from EarlyProcessSingleton
    This code is a left-over of the CL that shipped the feature.
    see: https://chromium-review.googlesource.com/c/chromium/src/+/4779488
    
    R=gab@chromium.org
    
    Bug: 1340599
    Change-Id: I03e7974b39089f3e56acab44576581a6b347747f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994274
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218896}

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -95,7 +95,6 @@ extern const char kEnableCloudPrintProxy[];
 extern const char kEnableCriticalPersistedTabData[];
 extern const char kEnableDomainReliability[];
 extern const char kEnableDownloadWarningImprovements[];
-extern const char kEnableEarlyProcessSingleton[];
 extern const char kEnableExtensionActivityLogging[];
 extern const char kEnableExtensionActivityLogTesting[];
 extern const char kEnableHangoutServicesExtensionForTesting[];
