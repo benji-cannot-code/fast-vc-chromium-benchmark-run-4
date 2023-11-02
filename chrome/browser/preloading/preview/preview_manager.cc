de01b23f71bf99986ef16dc446c040174dba896e
keno <kenoss@chromium.org>
2023-11-02 09:32:40 +0000

    LinkPreview: Cancel preview when primary page changed
    Bug: b/299426212
    Change-Id: Icac705fcbb8934dced817be22ea70b8f4abd7892
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4982616
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218697}

--- chrome/browser/preloading/preview/preview_manager.cc
+++ chrome/browser/preloading/preview/preview_manager.cc
@@ -12,9 +12,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 PreviewManager::PreviewManager(content::WebContents* web_contents)
-    : content::WebContentsUserData<PreviewManager>(*web_contents) {}
+    : content::WebContentsObserver(web_contents),
+      content::WebContentsUserData<PreviewManager>(*web_contents) {}
 
 PreviewManager::~PreviewManager() = default;
+
+void PreviewManager::PrimaryPageChanged(content::Page& page) {
+  // When initiator page has gone, cancel preview.
+  tab_.reset();
+}
 
 void PreviewManager::InitiatePreview(const GURL& url) {
   // TODO(b:292184832): Pass more load params.
--- chrome/browser/preloading/preview/preview_manager.h
+++ chrome/browser/preloading/preview/preview_manager.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/weak_ptr.h"
+#include "content/public/browser/page.h"
+#include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 
 class GURL;
@@ -16,12 +18,16 @@ class PreviewTab;
 
 // Handles requests of preview and manages ongoing previews.
 class PreviewManager final
-    : public content::WebContentsUserData<PreviewManager> {
+    : public content::WebContentsObserver,
+      public content::WebContentsUserData<PreviewManager> {
  public:
   PreviewManager(const PreviewManager&) = delete;
   PreviewManager& operator=(const PreviewManager&) = delete;
 
   ~PreviewManager() override;
+
+  // content::WebContentsObserver implementation:
+  void PrimaryPageChanged(content::Page& page) override;
 
   void InitiatePreview(const GURL& url);
   void PromoteToNewTab();
