2d5dcb8ff5b1383ab5a16a902757023c5bd513f5
Maria Kazinova <kazinova@google.com>
2023-11-02 10:26:52 +0000

    [Passwords] Support FPF flow when 2 forms are on PSL-matched domains.
    Sometimes a forgot password form (the one, where the user inputs
    the username) is hosted on a domain, different from the domain of the
    password reset form (the one where the user creates a new password).
    In this case PasswordManager is not able to suggest the correct
    username value in the save prompt even if the field is receiving a
    correct prediction. This CL fixes that.
    
    Bug: 1498045
    Change-Id: I0b4d0cb02ab6ddabe90d930e36e3b48d0f1a2db0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994808
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1218712}

--- components/password_manager/core/browser/field_info_manager.cc
+++ components/password_manager/core/browser/field_info_manager.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/field_info_manager.h"
 
 #include "base/i18n/case_conversion.h"
+#include "components/password_manager/core/browser/psl_matching_helper.h"
 
 using autofill::FieldRendererId;
 using autofill::FormSignature;
@@ -95,8 +96,9 @@ std::vector<FieldInfo> FieldInfoManager::GetFieldInfo(
     const std::string& signon_realm) {
   std::vector<FieldInfo> relevant_info;
   for (const auto& entry : field_info_cache_) {
-    // TODO(crbug/1468297): Consider eTLD+1 and affiliated matches.
-    if (entry.field_info.signon_realm == signon_realm) {
+    // TODO(crbug/1468297): Consider affiliated matches and PSL extension list.
+    if (IsPublicSuffixDomainMatch(entry.field_info.signon_realm,
+                                  signon_realm)) {
       relevant_info.push_back(entry.field_info);
     }
   }
