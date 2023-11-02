d553157543e19c71089dda232a96e675be13070c
David Bertoni <dbertoni@chromium.org>
2023-11-02 18:41:14 +0000

    [Code Health] Remove uses of base::SupportsWeakPtr (c/b/a/login_split)
    This CL removes uses of this deprecated base class by adding a
    base::WeakPtrFactory data member.
    
    In some cases, the base class was no longer being used, so it's
    simply removed.
    
    In some cases, it was being used as the base class for a supposed
    interface class. In those cases, a pure virtual member function
    was added so that implementation classes can have a WeakPtrFactory
    data member and dispense appropriate WeakPtr instances.
    
    This CL was uploaded by git cl split.
    
    R=emaamari@google.com
    
    Bug: 647430
    Change-Id: I69013848e738b7d2ee91ed1fd6ea4e7869318b8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4982013
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Elie Maamari <emaamari@google.com>
    Reviewed-by: Ben Franz <bfranz@chromium.org>
    Commit-Queue: Elie Maamari <emaamari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1218999}

--- chrome/browser/ash/login/session/user_session_manager_unittest.cc
+++ chrome/browser/ash/login/session/user_session_manager_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/ash/login/demo_mode/demo_session.h"
 #include "chrome/browser/ash/login/users/fake_chrome_user_manager.h"
