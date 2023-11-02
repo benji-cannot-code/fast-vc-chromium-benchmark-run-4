2e48c9822f50abc3c3c1335dc39c521be53d04b1
Harald Alvestrand <hta@chromium.org>
2023-11-02 13:23:33 +0000

    Global-scope refs to ::scoped_refptr
    This is required in order to move rtc::scoped_refptr to
    webrtc::scoped_refptr, since the wrapper lives in the webrtc
    namespace.
    
    Bug: webrtc:15622
    Change-Id: Iba48bb7030e7437a29598a59e81bc79bae4d0ea6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4999086
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Harald Alvestrand <hta@chromium.org>
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218775}

--- components/webrtc/thread_wrapper.cc
+++ components/webrtc/thread_wrapper.cc
@@ -44,7 +44,7 @@ ABSL_CONST_INIT thread_local ThreadWrapper* jingle_thread_wrapper = nullptr;
 class ThreadWrapper::PostTaskLatencySampler {
  public:
   PostTaskLatencySampler(
-      scoped_refptr<base::SingleThreadTaskRunner> task_runner,
+      ::scoped_refptr<base::SingleThreadTaskRunner> task_runner,
       SampledDurationCallback task_latency_callback)
       : task_runner_(task_runner),
         task_latency_callback_(std::move(task_latency_callback)) {
@@ -84,7 +84,7 @@ class ThreadWrapper::PostTaskLatencySampler {
   }
 
   SEQUENCE_CHECKER(current_);
-  scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
+  ::scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
   base::RepeatingCallback<void(base::TimeDelta)> task_latency_callback_
       GUARDED_BY_CONTEXT(current_);
   bool should_sample_next_task_duration_ GUARDED_BY_CONTEXT(current_) = false;
@@ -112,7 +112,7 @@ void ThreadWrapper::EnsureForCurrentMessageLoop() {
 }
 
 std::unique_ptr<ThreadWrapper> ThreadWrapper::WrapTaskRunner(
-    scoped_refptr<base::SingleThreadTaskRunner> task_runner) {
+    ::scoped_refptr<base::SingleThreadTaskRunner> task_runner) {
   DCHECK(task_runner->BelongsToCurrentThread());
   return base::WrapUnique(new ThreadWrapper(task_runner));
 }
@@ -130,7 +130,7 @@ void ThreadWrapper::SetLatencyAndTaskDurationCallbacks(
 }
 
 ThreadWrapper::ThreadWrapper(
-    scoped_refptr<base::SingleThreadTaskRunner> task_runner)
+    ::scoped_refptr<base::SingleThreadTaskRunner> task_runner)
     : Thread(std::make_unique<rtc::PhysicalSocketServer>()),
       resetter_(&jingle_thread_wrapper, this, nullptr),
       task_runner_(task_runner),
