9b23c8fc8cf09bac01b569e47af049f58230effa
Khushal Sagar <khushalsagar@chromium.org>
2023-11-02 22:48:29 +0000

    blink: Reset EventListenerProperties when swapping delegate.
    Tearing down the layout tree resets state on the compositor for event
    handlers tied to the layout objects. This happens implicitly when the
    layout tree is destroyed for a cross-document navigation.
    
    With RenderDocument, the compositor has already been detached when the
    tear down occurs. Safely avoid resetting state on the compositor for
    this case.
    
    In order to avoid a behaviour change, also reset this state when the
    compositor is swapped to the new widget. In effect, instead of the
    state being reset during old Document tear down, it gets reset earlier
    when the compositor is passed between widgets.
    
    Bug: 1464791
    Change-Id: I8e567dba5b475383d1ac9ca20d391d3d1bfab974
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5001906
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219175}

--- third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
+++ third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
@@ -1886,6 +1886,10 @@ void WebFrameWidgetImpl::SetHaveScrollEventHandlers(bool has_handlers) {
 void WebFrameWidgetImpl::SetEventListenerProperties(
     cc::EventListenerClass listener_class,
     cc::EventListenerProperties listener_properties) {
+  if (widget_base_->WillBeDestroyed()) {
+    return;
+  }
+
   widget_base_->LayerTreeHost()->SetEventListenerProperties(
       listener_class, listener_properties);
 
