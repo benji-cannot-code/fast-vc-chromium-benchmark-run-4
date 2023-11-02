d2a971c455c89fa8b0334c4048eaeee41378348e
Rakina Zata Amni <rakina@chromium.org>
2023-11-02 13:04:13 +0000

    Handle DoDeferredCloseWindowSoon on a swapped out WebViewImpl
    With RenderDocument, cross-document same-site navigations will use
    new LocalFrames that will use a new WebView in case of main frames.
    After committing the new LocalFrame in the new WebView, the old
    WebView's LocalFrame will be detached, so it won't have any valid
    local_main_frame_host_remote_ or remote_main_frame_host_remote_.
    
    This caused crashes in some test that calls window.close() on the
    old document just before it gets unloaded, causing the close task
    to run after the LocalFrame gets detached, and
    DoDeferredCloseWindowSoon() tried to access the non-existent
    remote_main_frame_host_remote_.
    
    This CL fixes the crash by introducing CloseTaskHandler, which
    keeps track of the "current" Page, ensuring that the close task
    will run on the new a non-detached WebView in the case mentioned
    above.
    
    Bug: 936696
    
    Change-Id: I06b9e73766d9f97471a53029b8144609f345c797
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4875425
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218768}

--- third_party/blink/renderer/core/page/page.h
+++ third_party/blink/renderer/core/page/page.h
@@ -461,8 +461,14 @@ class CORE_EXPORT Page final : public GarbageCollected<Page>,
     return attribution_support_;
   }
 
+  // Called on a new Page, passing an old Page as the parameter, when doing a
+  // LocalFrame <-> LocalFrame swap when committing a navigation, to ensure that
+  // the close task will still be processed after the swap.
+  void TakeCloseTaskHandler(Page* old_page);
+
  private:
   friend class ScopedPagePauser;
+  class CloseTaskHandler;
 
   void InitGroup();
 
@@ -624,6 +630,8 @@ class CORE_EXPORT Page final : public GarbageCollected<Page>,
   BrowsingContextGroupInfo browsing_context_group_info_;
 
   network::mojom::AttributionSupport attribution_support_;
+
+  Member<CloseTaskHandler> close_task_handler_;
 };
 
 extern template class CORE_EXTERN_TEMPLATE_EXPORT Supplement<Page>;
