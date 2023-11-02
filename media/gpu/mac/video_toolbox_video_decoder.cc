972bf2ea70a012cef89912590713aefee7d92c27
Fritz Koenig <frkoenig@chromium.org>
2023-11-02 20:07:41 +0000

    Vp9Parser: Remove kAwaitingRefresh
    |kAwaitingRefresh| is never set. Remove handling.
    
    Bug: b:307889405
    Bug: 1490902
    Change-Id: I26c1a475699473882839d14ebcbbef6d59ca277f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5001853
    Commit-Queue: Fritz Koenig <frkoenig@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219065}

--- media/gpu/mac/video_toolbox_video_decoder.cc
+++ media/gpu/mac/video_toolbox_video_decoder.cc
@@ -259,7 +259,6 @@ void VideoToolboxVideoDecoder::Decode(scoped_refptr<DecoderBuffer> buffer,
     switch (result) {
       case AcceleratedVideoDecoder::kDecodeError:
       case AcceleratedVideoDecoder::kRanOutOfSurfaces:
-      case AcceleratedVideoDecoder::kNeedContextUpdate:
       case AcceleratedVideoDecoder::kTryAgain:
         // More specific reasons are logged to the media log.
         NotifyError(DecoderStatus::Codes::kMalformedBitstream);
