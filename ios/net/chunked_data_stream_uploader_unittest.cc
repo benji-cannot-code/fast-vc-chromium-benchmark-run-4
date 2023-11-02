626f7bb6f1bb3bd17494bd4f0b412ad7773f0dad
Tom Sepez <tsepez@chromium.org>
2023-11-02 18:24:06 +0000

    Prepare for separating owning and non-owning net:IOBuffers.
    Use IOBufferWithSize for the owning cases. Currently this is
    the same as IOBuffer, but eventually IOBuffer will evolve to be
    a non-owning class.
    
    This cl applies the changes to /ios.
    
    This CL was uploaded by git cl split.
    
    R=marq@chromium.org
    
    Bug: 1493389
    Change-Id: Ice194745f74fcd193d5d43353cd320e453dfc0ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4996448
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1218982}

--- ios/net/chunked_data_stream_uploader_unittest.cc
+++ ios/net/chunked_data_stream_uploader_unittest.cc
@@ -84,7 +84,8 @@ TEST_F(ChunkedDataStreamUploaderTest, ExternalDataReadyFirst) {
 
   // Network layer callback is called next, and the application data is expected
   // to be read to the |buffer|.
-  auto buffer = base::MakeRefCounted<net::IOBuffer>(kDefaultIOBufferSize);
+  auto buffer =
+      base::MakeRefCounted<net::IOBufferWithSize>(kDefaultIOBufferSize);
   int bytes_read = uploader_->Read(
       buffer.get(), kDefaultIOBufferSize,
       base::BindRepeating(&ChunkedDataStreamUploaderTest::CompletionCallback,
@@ -113,7 +114,8 @@ TEST_F(ChunkedDataStreamUploaderTest, ExternalDataReadyFirst) {
 // layer data available.
 TEST_F(ChunkedDataStreamUploaderTest, InternalReadReadyFirst) {
   // Network layer callback is called and the request is pending.
-  auto buffer = base::MakeRefCounted<net::IOBuffer>(kDefaultIOBufferSize);
+  auto buffer =
+      base::MakeRefCounted<net::IOBufferWithSize>(kDefaultIOBufferSize);
   int ret = uploader_->Read(
       buffer.get(), kDefaultIOBufferSize,
       base::BindRepeating(&ChunkedDataStreamUploaderTest::CompletionCallback,
@@ -143,7 +145,8 @@ TEST_F(ChunkedDataStreamUploaderTest, InternalReadReadyFirst) {
 
 // Tests that null data is correctly handled when the callback comes first.
 TEST_F(ChunkedDataStreamUploaderTest, NullContentWithReadFirst) {
-  auto buffer = base::MakeRefCounted<net::IOBuffer>(kDefaultIOBufferSize);
+  auto buffer =
+      base::MakeRefCounted<net::IOBufferWithSize>(kDefaultIOBufferSize);
   int ret = uploader_->Read(
       buffer.get(), kDefaultIOBufferSize,
       base::BindRepeating(&ChunkedDataStreamUploaderTest::CompletionCallback,
@@ -162,7 +165,8 @@ TEST_F(ChunkedDataStreamUploaderTest, NullContentWithDataFirst) {
   delegate_->SetReadData("", 0);
   uploader_->UploadWhenReady(true);
 
-  auto buffer = base::MakeRefCounted<net::IOBuffer>(kDefaultIOBufferSize);
+  auto buffer =
+      base::MakeRefCounted<net::IOBufferWithSize>(kDefaultIOBufferSize);
   int bytes_read = uploader_->Read(
       buffer.get(), kDefaultIOBufferSize,
       base::BindRepeating(&ChunkedDataStreamUploaderTest::CompletionCallback,
@@ -181,7 +185,8 @@ TEST_F(ChunkedDataStreamUploaderTest, MixedScenarioTest) {
   delegate_->SetReadData(kTestData, sizeof(kTestData));
   uploader_->UploadWhenReady(false);
 
-  auto buffer = base::MakeRefCounted<net::IOBuffer>(kDefaultIOBufferSize);
+  auto buffer =
+      base::MakeRefCounted<net::IOBufferWithSize>(kDefaultIOBufferSize);
   int bytes_read = uploader_->Read(
       buffer.get(), kDefaultIOBufferSize,
       base::BindRepeating(&ChunkedDataStreamUploaderTest::CompletionCallback,
