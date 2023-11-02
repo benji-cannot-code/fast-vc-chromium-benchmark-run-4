d2df46e2a00e82c6beb04a2ed72a3034d1bd82f5
Scott Violet <sky@chromium.org>
2023-11-02 13:32:17 +0000

    services: fix gn check
    gn check fails because the test case does not have the
    necessary dependency.
    
    Change-Id: I2cdfa762e910b54edc446e4abbfb7a718357610a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4998069
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218779}

--- services/on_device_model/pre_sandbox_init.cc
+++ services/on_device_model/pre_sandbox_init.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/on_device_model/on_device_model_service.h"
 
 #if defined(ENABLE_ML_INTERNAL)
-#include "services/on_device_model/chrome_ml_instance.h"
+#include "services/on_device_model/chrome_ml_instance.h"  // nogncheck
 #endif
 
 namespace on_device_model {
