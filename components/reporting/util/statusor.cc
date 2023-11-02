d0d0007f40e5a6f3d4037dd6fc2d5bf87d561fe3
Hong Xu <xuhong@chromium.org>
2023-11-02 19:43:37 +0000

    Make StatusOr not able to accept an OK status.
    Bug: b:300464285
    Fixed: b:300464285
    Change-Id: Ieaa848fb8e50156653e3ae51a037f8663d62a5bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4998778
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Hong Xu <xuhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1219048}

---
+++ components/reporting/util/statusor.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/reporting/util/statusor.h"
+
+#include <utility>
+
+#include "base/check.h"
+
+namespace reporting::internal {
+ErrorStatus::ErrorStatus(const ErrorStatus&) = default;
+ErrorStatus& ErrorStatus::operator=(const ErrorStatus& x) = default;
+ErrorStatus::ErrorStatus(ErrorStatus&&) = default;
+ErrorStatus& ErrorStatus::operator=(ErrorStatus&& x) = default;
+ErrorStatus::~ErrorStatus() = default;
+
+ErrorStatus::ErrorStatus(const Status& status) : Status(status) {
+  CHECK(!ok()) << "The status must not be OK";
+}
+
+ErrorStatus::ErrorStatus(Status&& status) : Status(std::move(status)) {
+  CHECK(!ok()) << "The status must not be OK";
+}
+
+}  // namespace reporting::internal
---
+++ components/reporting/util/statusor_unittest.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/reporting/util/statusor.h"
+
+#include "components/reporting/util/status.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace reporting {
+namespace {
+
+TEST(StatusOr, MoveConstructFromAndExtractToStatusImplicitly) {
+  Status status(error::INTERNAL, "internal error");
+  base::unexpected<Status> unexpected_status(status);
+  StatusOr<int> status_or(std::move(unexpected_status));
+  Status extracted_status{std::move(status_or).error()};
+  EXPECT_EQ(status, extracted_status);
+}
+
+TEST(StatusOr, CopyConstructFromAndExtractToStatusImplicitly) {
+  Status status(error::INTERNAL, "internal error");
+  base::unexpected<Status> unexpected_status(status);
+  StatusOr<int> status_or(unexpected_status);
+  Status extracted_status{status_or.error()};
+  EXPECT_EQ(status, extracted_status);
+}
+
+}  // namespace
+}  // namespace reporting
