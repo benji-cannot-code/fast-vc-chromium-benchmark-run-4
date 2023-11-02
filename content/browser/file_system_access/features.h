c4e8b7db0d0674f1e676e9104aa5aa95743c4940
Austin Sullivan <asully@chromium.org>
2023-11-02 17:54:04 +0000

    FSA: Remove flag guarding skipping after write checks
    Bug: 1247850
    Change-Id: If8c14cb306c94569e9a6f4964f088660d4cc9a7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4998490
    Reviewed-by: Christine Smith <christinesm@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218953}

--- content/browser/file_system_access/features.h
+++ content/browser/file_system_access/features.h
@@ -18,8 +18,6 @@ namespace content::features {
 // Alphabetical:
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kFileSystemAccessDragAndDropCheckBlocklist);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(
-    kFileSystemAccessSkipAfterWriteChecksIfUnchangingExtension);
-CONTENT_EXPORT BASE_DECLARE_FEATURE(
     kFileSystemAccessDirectoryIterationSymbolicLinkCheck);
 
 }  // namespace content::features
