2d25ef457341573dc492f4d70061599e31bc5c66
Bruno Braga <brunobraga@google.com>
2023-11-03 16:12:58 +0000

    [mullet m2] Add metrics for filling method used.
    I will implement the emission in a follow up. It will be done from
    the external delegate on suggestion acceptance.
    
    The goal of this metric is to understand how often users will use
    each individual filling method. Group filling is divided
    into its "sub-groups" because I believe it is a low hanging fruit
    for us to do it here. Also, there is a difference in how
    some group fillings are presented to the user, for example address
    is presented as "Fill address", while phone number is just the users
    stored information. Measuring both together is not ideal because they
    have significant different UI.
    
    Low-Coverage-Reason: Tests are in the chained follow up CL.
    
    Bug: 1459990
    Change-Id: Ie636059678f32f146b604125095ac22fd8849682
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5001995
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1219522}

--- components/autofill/core/browser/metrics/granular_filling_metrics.h
+++ components/autofill/core/browser/metrics/granular_filling_metrics.h
@@ -8,6 +8,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill::autofill_metrics {
 
+// Represents the filling method chosen by the user.
+enum AutofillFillingMethodMetric {
+  // User chose to fill the whole form. Either from the main suggestion or from
+  // the extended menu `PopupItemId::kFillEverything`.
+  kFullForm = 0,
+  // User chose to fill all name fields.
+  kGroupFillingName = 1,
+  // User chose to fill all address fields.
+  kGroupFillingAddress = 2,
+  // User chose to fill all email fields.
+  kGroupFillingEmail = 3,
+  // User chose to fill all phone number fields.
+  kGroupFillingPhoneNumber = 4,
+  // User chose to fill a specific field.
+  kFieldByFieldFilling = 5,
+  kMaxValue = kFieldByFieldFilling
+};
+
 // This metric is only relevant for granular filling, i.e. when the edit dialog
 // is opened from the Autofill popup.
 void LogEditAddressProfileDialogClosed(bool user_saved_changes);
@@ -15,6 +33,9 @@ void LogEditAddressProfileDialogClosed(bool user_saved_changes);
 // This metric is only relevant for granular filling, i.e. when the delete
 // dialog is opened from the Autofill popup.
 void LogDeleteAddressProfileDialogClosed(bool user_accepted_delete);
+
+// Logs the `AutofillFillingMethodMetric` chosen by the user.
+void LogFillingMethodUsed(AutofillFillingMethodMetric filling_method);
 
 }  // namespace autofill::autofill_metrics
 
