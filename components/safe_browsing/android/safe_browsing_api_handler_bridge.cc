ce7d3c8cb7c3af76777936b50bf07084edd4f2d0
Xinghui Lu <xinghuilu@chromium.org>
2023-11-03 18:04:47 +0000

    Add fieldtrial config for SafeBrowsingNewGmsApiForBrowseUrlDatabaseCheck on Clank.
    Introduce MockSafeBrowsingApiHandler that will replace
    MockSafetyNetApiHandler once the feature is fully launched.
    
    Since the API handler can be null in tests, introduce a new
    LookupResult enum value FAILURE_HANDLER_NULL for this case. It
    should never be logged in production code.
    
    fieldtrial config on WebView will be added in a follow-up CL.
    
    Bug: 1490736
    Change-Id: Ia9377fd82d0f322b3cdb0f4f12d682e7a83ca9c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4921408
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219611}

--- components/safe_browsing/android/safe_browsing_api_handler_bridge.cc
+++ components/safe_browsing/android/safe_browsing_api_handler_bridge.cc
@@ -139,6 +139,7 @@ SafeBrowsingJavaValidationResult GetJavaValidationResult(
     case SafeBrowsingApiLookupResult::FAILURE_API_CALL_TIMEOUT:
     case SafeBrowsingApiLookupResult::FAILURE_API_UNSUPPORTED:
     case SafeBrowsingApiLookupResult::FAILURE_API_NOT_AVAILABLE:
+    case SafeBrowsingApiLookupResult::FAILURE_HANDLER_NULL:
       is_lookup_result_recognized = true;
       break;
   }
@@ -233,6 +234,7 @@ bool IsLookupSuccessful(SafeBrowsingApiLookupResult lookup_result,
     case SafeBrowsingApiLookupResult::FAILURE_API_CALL_TIMEOUT:
     case SafeBrowsingApiLookupResult::FAILURE_API_UNSUPPORTED:
     case SafeBrowsingApiLookupResult::FAILURE_API_NOT_AVAILABLE:
+    case SafeBrowsingApiLookupResult::FAILURE_HANDLER_NULL:
       break;
   }
   if (!is_lookup_result_success) {
@@ -263,6 +265,7 @@ bool IsSafeBrowsingNonRecoverable(SafeBrowsingApiLookupResult lookup_result) {
   switch (lookup_result) {
     case SafeBrowsingApiLookupResult::FAILURE_API_UNSUPPORTED:
     case SafeBrowsingApiLookupResult::FAILURE_API_NOT_AVAILABLE:
+    case SafeBrowsingApiLookupResult::FAILURE_HANDLER_NULL:
       return true;
     case SafeBrowsingApiLookupResult::SUCCESS:
     case SafeBrowsingApiLookupResult::FAILURE:
