1d4a7f2f6e5bb6c248207990bebe1e890495bdcb
Glen Robertson <glenrob@chromium.org>
2023-11-03 06:44:49 +0000

    WebApps: Change default for preinstalled D/S/S/D to standalone tabbed apps
    Previously when Google Docs, Sheets, Slides, and Drive were
    preinstalled, they were set to open in a browser tab. Now that tabbed
    web apps are launched, D/S/S/D are moving to use a tabbed web app when
    installed. This CL updates the default to match (otherwise the
    DisplayMode will change on app update later).
    
    Excludes managed devices/profiles from the change to reduce disruption
    in enterprise environments. Will do a follow-up CL with some tests for
    the managed device detection.
    
    Doesn't currently set a home/new tab configuration, so tabbed editors
    will not have a home tab, and use the start_url for new tabs. Will
    follow up with a fix for that.
    
    Launch: launch/4273092
    Bug: b/298288181
    Change-Id: I1a8eb02c9cb3045e442d86195fed4cc9e093dabb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4938531
    Auto-Submit: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219340}

--- chrome/browser/web_applications/preinstalled_web_apps/google_docs.cc
+++ chrome/browser/web_applications/preinstalled_web_apps/google_docs.cc
@@ -5,15 +5,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/web_applications/preinstalled_web_apps/google_docs.h"
 
+#include <memory>
+#include <string>
+#include <vector>
+
 #include "base/functional/bind.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/chromeos_buildflags.h"
-#include "chrome/browser/web_applications/mojom/user_display_mode.mojom.h"
-#include "chrome/browser/web_applications/preinstalled_app_install_features.h"
+#include "chrome/browser/web_applications/mojom/user_display_mode.mojom-shared.h"
 #include "chrome/browser/web_applications/preinstalled_web_apps/preinstalled_web_app_definition_utils.h"
+#include "chrome/browser/web_applications/web_app_constants.h"
 #include "chrome/browser/web_applications/web_app_id_constants.h"
 #include "chrome/browser/web_applications/web_app_install_info.h"
 #include "chrome/grit/preinstalled_web_apps_resources.h"
+#include "components/webapps/common/web_app_id.h"
+#include "third_party/blink/public/mojom/manifest/display_mode.mojom-shared.h"
+#include "url/gurl.h"
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
 #include "chromeos/startup/browser_params_proxy.h"
@@ -119,11 +126,13 @@ bool IsDriveFsBulkPinningAvailable() {
 
 }  // namespace
 
-ExternalInstallOptions GetConfigForGoogleDocs() {
+ExternalInstallOptions GetConfigForGoogleDocs(bool is_standalone_tabbed) {
   ExternalInstallOptions options(
       /*install_url=*/GURL(
           "https://docs.google.com/document/installwebapp?usp=chrome_default"),
-      /*user_display_mode=*/mojom::UserDisplayMode::kBrowser,
+      /*user_display_mode=*/
+      is_standalone_tabbed ? mojom::UserDisplayMode::kStandalone
+                           : mojom::UserDisplayMode::kBrowser,
       /*install_source=*/ExternalInstallSource::kExternalDefault);
 
   options.user_type_allowlist = {"unmanaged", "managed", "child"};
@@ -143,18 +152,21 @@ ExternalInstallOptions GetConfigForGoogleDocs() {
 
   options.load_and_await_service_worker_registration = false;
   options.only_use_app_info_factory = true;
-  options.app_info_factory = base::BindRepeating([]() {
-    auto info = std::make_unique<WebAppInstallInfo>();
-    info->title =
-        base::UTF8ToUTF16(GetTranslatedName("Docs", kNameTranslations));
-    info->start_url =
-        GURL("https://docs.google.com/document/?usp=installed_webapp");
-    info->scope = GURL("https://docs.google.com/document/");
-    info->display_mode = DisplayMode::kBrowser;
-    info->icon_bitmaps.any =
-        LoadBundledIcons({IDR_PREINSTALLED_WEB_APPS_GOOGLE_DOCS_ICON_192_PNG});
-    return info;
-  });
+  options.app_info_factory = base::BindRepeating(
+      [](bool is_standalone_tabbed) {
+        auto info = std::make_unique<WebAppInstallInfo>();
+        info->title =
+            base::UTF8ToUTF16(GetTranslatedName("Docs", kNameTranslations));
+        info->start_url =
+            GURL("https://docs.google.com/document/?usp=installed_webapp");
+        info->scope = GURL("https://docs.google.com/document/");
+        info->display_mode =
+            is_standalone_tabbed ? DisplayMode::kTabbed : DisplayMode::kBrowser;
+        info->icon_bitmaps.any = LoadBundledIcons(
+            {IDR_PREINSTALLED_WEB_APPS_GOOGLE_DOCS_ICON_192_PNG});
+        return info;
+      },
+      is_standalone_tabbed);
 
   return options;
 }
