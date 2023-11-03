8475080073f884539ac27e66b0d729372cf6673c
zhumatthew <zhumatthew@google.com>
2023-11-03 23:58:42 +0000

    fws: Fix chrome sync upload to go to the correct profile
    When a user switches profiles using the multi-profile sign in,
    we need to handle the sync service and related services that
    tied to profile correctly.
    
    Unit tests will be added as a todo once a refactor is done to
    allow us to test the user switch.
    
    BUG=b:308637140
    TEST=FloatingWorkspaceServiceTest*
    
    Change-Id: Ie4cc22b11b4a64c590ad8f8e0e63c642150d3009
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994859
    Reviewed-by: Yanzhu Du <yzd@google.com>
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Commit-Queue: Matthew Zhu <zhumatthew@google.com>
    Cr-Commit-Position: refs/heads/main@{#1219814}

--- chrome/browser/ash/floating_workspace/floating_workspace_service.h
+++ chrome/browser/ash/floating_workspace/floating_workspace_service.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 #include "ash/public/cpp/desk_template.h"
+#include "ash/public/cpp/session/session_observer.h"
 #include "base/callback_list.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -61,12 +62,12 @@ enum class FloatingWorkspaceServiceNotificationType {
 // A keyed service to support floating workspace. Note that a periodical
 // task `CaptureAndUploadActiveDesk` will be dispatched during service
 // initialization.
-class FloatingWorkspaceService
-    : public KeyedService,
-      public message_center::NotificationObserver,
-      public syncer::SyncServiceObserver,
-      public apps::AppRegistryCache::Observer,
-      public apps::AppRegistryCacheWrapper::Observer {
+class FloatingWorkspaceService : public KeyedService,
+                                 public message_center::NotificationObserver,
+                                 public syncer::SyncServiceObserver,
+                                 public apps::AppRegistryCache::Observer,
+                                 public apps::AppRegistryCacheWrapper::Observer,
+                                 public ash::SessionObserver {
  public:
   static FloatingWorkspaceService* GetForProfile(Profile* profile);
 
@@ -110,6 +111,9 @@ class FloatingWorkspaceService
   bool is_testing_ = false;
 
  private:
+  // ash::SessionObserver:
+  void OnActiveUserSessionChanged(const AccountId& account_id) override;
+
   // AppRegistryCache::Observer
   void OnAppRegistryCacheWillBeDestroyed(
       apps::AppRegistryCache* cache) override;
@@ -227,6 +231,18 @@ class FloatingWorkspaceService
   // Updates the `is_cache_ready_` status if all the required app types are
   // initialized.
   bool AreRequiredAppTypesInitialized();
+
+  // Shuts down the observers and dependent services.
+  // This will be called when the user session changes to a different user or on
+  // service shutdown.
+  void ShutDownServicesAndObservers();
+
+  // Setups the convenience pointers to the dependent services and observers.
+  // This will be called when the service is first initialized and when the
+  // active user session is changed back to the first logged in user.
+  void SetUpServiceAndObservers(
+      syncer::SyncService* sync_service,
+      desks_storage::DeskSyncService* desk_sync_service);
 
   const raw_ptr<Profile, ExperimentalAsh> profile_;
 
--- chrome/browser/ash/floating_workspace/floating_workspace_util.cc
+++ chrome/browser/ash/floating_workspace/floating_workspace_util.cc
@@ -23,9 +23,9 @@ namespace ash {
 
 namespace {
 
-PrefService* GetPrimaryUserPrefService() {
-  auto* primary_user = user_manager::UserManager::Get()->GetPrimaryUser();
-  auto* user_profile = ProfileHelper::Get()->GetProfileByUser(primary_user);
+PrefService* GetActiveUserPrefService() {
+  auto* active_user = user_manager::UserManager::Get()->GetActiveUser();
+  auto* user_profile = ProfileHelper::Get()->GetProfileByUser(active_user);
   return user_profile->GetPrefs();
 }
 
@@ -44,7 +44,7 @@ bool IsFloatingWorkspaceV1Enabled() {
 }
 
 bool IsFloatingWorkspaceV2Enabled() {
-  PrefService* pref_service = GetPrimaryUserPrefService();
+  PrefService* pref_service = GetActiveUserPrefService();
   DCHECK(pref_service);
 
   const PrefService::Preference* floating_workspace_pref =
