fda828e1b6335123380f20ea112b7151a1291ae6
Andrew Paseltiner <apaseltiner@chromium.org>
2023-11-03 13:58:33 +0000

    Inline migration-only logic for serializing attribution source protos
    As the proto in question gains more and more fields, retaining backwards
    compatibility with earlier DB migrations is more complicated.
    
    Inlining the behavior in the migration code makes it harder to
    accidentally break.
    
    Change-Id: Ic1de8341faddcb2c0401287989cc1dff0e88df53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5003249
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219444}

--- content/browser/attribution_reporting/attribution_storage_sql_migrations.cc
+++ content/browser/attribution_reporting/attribution_storage_sql_migrations.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "components/attribution_reporting/event_report_windows.h"
 #include "components/attribution_reporting/source_type.mojom.h"
+#include "content/browser/attribution_reporting/attribution_reporting.pb.h"
 #include "content/browser/attribution_reporting/attribution_storage_sql.h"
 #include "content/browser/attribution_reporting/sql_utils.h"
 #include "net/base/schemeful_site.h"
@@ -323,14 +324,11 @@ bool To56(sql::Database& db) {
       continue;
     }
 
+    proto::AttributionReadOnlySourceData msg;
+    SetReadOnlySourceData(*event_report_windows, max_event_level_reports, msg);
+
     set_statement.Reset(/*clear_bound_vars=*/true);
-    // '-1' represents null for the randomized response rate field.
-    set_statement.BindBlob(
-        0, SerializeReadOnlySourceData(*event_report_windows,
-                                       max_event_level_reports,
-                                       /*randomized_response_rate=*/-1,
-                                       /*trigger_config=*/nullptr,
-                                       /*debug_cookie_set=*/nullptr));
+    set_statement.BindBlob(0, msg.SerializeAsString());
     set_statement.BindInt64(1, id);
     if (!set_statement.Run()) {
       return false;
