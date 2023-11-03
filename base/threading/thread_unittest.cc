ea4b89416842008fe3fdb4e4f401b74a71945b33
Peter Kasting <pkasting@chromium.org>
2023-11-03 23:05:53 +0000

    Use thread_local for thread_restrictions.cc.
    Bug: 1423437
    Change-Id: Icd7065024c95078827bc9b280bb30c44804347c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4996505
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219791}

--- base/threading/thread_unittest.cc
+++ base/threading/thread_unittest.cc
@@ -32,6 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
 
+#if DCHECK_IS_ON()
+#include "base/threading/thread_restrictions.h"
+#endif
+
 using ::testing::NotNull;
 
 using ThreadTest = PlatformTest;
@@ -162,6 +166,10 @@ TEST_F(ThreadTest, StartWithOptions_StackSize) {
   // The first time an LSAN disable is fired on a thread, the LSAN Mac runtime
   // initializes a 56k object on the stack.
   additional_space += 56 * 1024;
+#endif
+#if DCHECK_IS_ON()
+  // The thread restrictions add four BooleanWithStacks (which are ~2k each).
+  additional_space += sizeof(BooleanWithStack) * 4;
 #endif
 
   Thread a("StartWithStackSize");
