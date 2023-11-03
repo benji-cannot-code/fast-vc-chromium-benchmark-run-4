02c5b8358520527b9e002fa6bf4e85ebd8ea957d
Markus Handell <handellm@google.com>
2023-11-03 10:06:42 +0000

    Ensure vouchers are kept through encoding and packet sending.
    This change ensures survival of WebRTC vouchers through
    RTCVideoEncoder and network send operations including completion
    message.
    
    Bug: 1498378
    Change-Id: I78128bfad4a5c16eebc9a2bf250e8a5dd42230df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4992282
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Commit-Queue: Markus Handell <handellm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1219386}

--- third_party/blink/renderer/platform/p2p/ipc_socket_factory.cc
+++ third_party/blink/renderer/platform/p2p/ipc_socket_factory.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 #include "third_party/webrtc/api/async_dns_resolver.h"
 #include "third_party/webrtc/rtc_base/async_packet_socket.h"
+#include "third_party/webrtc/rtc_base/voucher.h"
 
 namespace blink {
 
@@ -102,11 +103,16 @@ class IpcPacketSocket : public rtc::AsyncPacketSocket,
   // send. The information tracked here will be used to match with the
   // P2PSendPacketMetrics from the underneath system socket.
   struct InFlightPacketRecord {
-    InFlightPacketRecord(uint64_t packet_id, size_t packet_size)
-        : packet_id(packet_id), packet_size(packet_size) {}
+    InFlightPacketRecord(uint64_t packet_id,
+                         size_t packet_size,
+                         webrtc::Voucher::Ptr voucher)
+        : packet_id(packet_id),
+          packet_size(packet_size),
+          voucher(std::move(voucher)) {}
 
     uint64_t packet_id;
     size_t packet_size;
+    webrtc::Voucher::Ptr voucher;
   };
 
   typedef std::list<InFlightPacketRecord> InFlightPacketList;
@@ -470,8 +476,8 @@ int IpcPacketSocket::SendToInternal(const void* data,
   // Ensure packet_id is not 0. It can't be the case according to
   // P2PSocketClientImpl::Send().
   DCHECK_NE(packet_id, 0uL);
-  in_flight_packet_records_.push_back(
-      InFlightPacketRecord(packet_id, data_size));
+  in_flight_packet_records_.emplace_back(packet_id, data_size,
+                                         webrtc::Voucher::Current());
   TraceSendThrottlingState();
 
   // Fake successful send. The caller ignores result anyway.
@@ -628,7 +634,7 @@ void IpcPacketSocket::OnSendComplete(
 
   CHECK(!in_flight_packet_records_.empty());
 
-  const InFlightPacketRecord& record = in_flight_packet_records_.front();
+  InFlightPacketRecord& record = in_flight_packet_records_.front();
 
   // Tracking is not turned on for TCP so it's always 0. For UDP, this will
   // cause a crash when the packet ids don't match.
@@ -637,6 +643,8 @@ void IpcPacketSocket::OnSendComplete(
 
   send_bytes_available_ = std::min(send_bytes_available_ + record.packet_size,
                                    max_in_flight_bytes_);
+
+  webrtc::Voucher::ScopedSetter setter(std::move(record.voucher));
 
   in_flight_packet_records_.pop_front();
   TraceSendThrottlingState();
