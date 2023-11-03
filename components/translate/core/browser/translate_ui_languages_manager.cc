1d6a95bfad912229d7a4235621b0a40b81330dd7
Megan Jablonski <megjablon@google.com>
2023-11-03 22:00:35 +0000

    Create Partial Translate Language List
    The partial translate backend supports a smaller set of languages than page translation. Create separate language lists so that when page translation adds new languages, they are not automatically added to partial translate. Also remove {bm ckb doi gom ilo lus mni-Mtei} which are not supported for partial translate.
    
    Bug: 1496479, 1496480
    
    Change-Id: I147f4f4a6dfc2fee675db98bd0af3692917a1a00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4989769
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Commit-Queue: Megan Jablonski <megjablon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219755}

--- components/translate/core/browser/translate_ui_languages_manager.cc
+++ components/translate/core/browser/translate_ui_languages_manager.cc
@@ -33,6 +33,7 @@ namespace translate {
 
 TranslateUILanguagesManager::TranslateUILanguagesManager(
     const base::WeakPtr<TranslateManager>& translate_manager,
+    const std::vector<std::string>& language_codes,
     const std::string& source_language,
     const std::string& target_language)
     : translate_manager_(translate_manager),
@@ -43,9 +44,6 @@ TranslateUILanguagesManager::TranslateUILanguagesManager(
   std::string locale =
       TranslateDownloadManager::GetInstance()->application_locale();
 
-  std::vector<std::string> language_codes;
-  TranslateDownloadManager::GetSupportedLanguages(
-      prefs_->IsTranslateAllowedByPolicy(), &language_codes);
   // Reserve additional space for unknown language option.
   std::vector<std::string>::size_type languages_size = language_codes.size();
   languages_size += 1;
@@ -53,7 +51,7 @@ TranslateUILanguagesManager::TranslateUILanguagesManager(
 
   // Preparing for the alphabetical order in the locale.
   std::unique_ptr<icu::Collator> collator = CreateCollator(locale);
-  for (std::string& language_code : language_codes) {
+  for (const std::string& language_code : language_codes) {
     std::u16string language_name =
         l10n_util::GetDisplayNameForLocale(language_code, locale, true);
     languages_.emplace_back(std::move(language_code), std::move(language_name));
--- components/translate/core/browser/translate_ui_languages_manager.h
+++ components/translate/core/browser/translate_ui_languages_manager.h
@@ -35,6 +35,7 @@ class TranslateUILanguagesManager {
 
   TranslateUILanguagesManager(
       const base::WeakPtr<TranslateManager>& translate_manager,
+      const std::vector<std::string>& language_codes,
       const std::string& source_language,
       const std::string& target_language);
 
