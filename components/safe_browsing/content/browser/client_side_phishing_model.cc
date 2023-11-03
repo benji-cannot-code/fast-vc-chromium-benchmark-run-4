90d4333ac515cbcfde7d840d121dc04a79de990b
Daniel Rubery <drubery@chromium.org>
2023-11-03 21:24:32 +0000

    Remove unmapped histogram logging
    The following four histograms have logging code, but no entries in
    histograms.xml. This seems to be due to incomplete deprecation, so this
    CL simply removes them.
    
    Bug: 1498037
    Change-Id: I757b3bcf8cf9e812a95ec178fc41f8ad7b77a66c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5005421
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Auto-Submit: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219710}

--- components/safe_browsing/content/browser/client_side_phishing_model.cc
+++ components/safe_browsing/content/browser/client_side_phishing_model.cc
@@ -272,8 +272,6 @@ void ClientSidePhishingModel::OnModelAndVisualTfLiteFileLoaded(
   base::File visual_tflite_model = std::move(model_and_tflite.second);
 
   bool model_valid = false;
-  int model_version_field = 0;
-
   bool tflite_valid = visual_tflite_model.IsValid();
   if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
           kOverrideCsdModelFlag) &&
@@ -287,8 +285,6 @@ void ClientSidePhishingModel::OnModelAndVisualTfLiteFileLoaded(
           base::ReadOnlySharedMemoryRegion::Create(model_str.length());
       if (mapped_region_.IsValid()) {
         model_type_ = CSDModelType::kFlatbuffer;
-        model_version_field =
-            flat::GetClientSideModel(model_str.data())->version();
         memcpy(mapped_region_.mapping.memory(), model_str.data(),
                model_str.length());
 
@@ -325,14 +321,6 @@ void ClientSidePhishingModel::OnModelAndVisualTfLiteFileLoaded(
 
   base::UmaHistogramBoolean("SBClientPhishing.ModelDynamicUpdateSuccess",
                             model_valid);
-
-  if (model_valid) {
-    // At time of writing, versions go up to 25. We set a max version of 100
-    // to give some room.
-    const int kMaxVersion = 100;
-    base::UmaHistogramExactLinear("SBClientPhishing.ModelDynamicUpdateVersion",
-                                  model_version_field, kMaxVersion + 1);
-  }
 
   if (tflite_valid) {
     visual_tflite_model_ = std::move(visual_tflite_model);
@@ -559,8 +547,6 @@ void ClientSidePhishingModel::SetModelStringForTesting(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   bool model_valid = false;
-  int model_version_field = 0;
-
   bool tflite_valid = visual_tflite_model.IsValid();
 
   // TODO (andysjlim): Move to a helper function once protobuf feature is
@@ -577,8 +563,6 @@ void ClientSidePhishingModel::SetModelStringForTesting(
           base::ReadOnlySharedMemoryRegion::Create(model_str.length());
       if (mapped_region_.IsValid()) {
         model_type_ = CSDModelType::kFlatbuffer;
-        model_version_field =
-            flat::GetClientSideModel(model_str.data())->version();
         memcpy(mapped_region_.mapping.memory(), model_str.data(),
                model_str.length());
       } else {
@@ -590,15 +574,6 @@ void ClientSidePhishingModel::SetModelStringForTesting(
 
     base::UmaHistogramBoolean("SBClientPhishing.ModelDynamicUpdateSuccess",
                               model_valid);
-
-    if (model_valid) {
-      // At time of writing, versions go up to 25. We set a max version of 100
-      // to give some room.
-      const int kMaxVersion = 100;
-      base::UmaHistogramExactLinear(
-          "SBClientPhishing.ModelDynamicUpdateVersion", model_version_field,
-          kMaxVersion + 1);
-    }
 
     if (tflite_valid) {
       visual_tflite_model_ = std::move(visual_tflite_model);
