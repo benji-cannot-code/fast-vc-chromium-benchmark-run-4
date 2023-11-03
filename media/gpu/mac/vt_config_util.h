8154fad1ae64dc5fc839b6a68852ccf75f57b8b1
Giovanni Ortuno Urquidi <ortuno@chromium.org>
2023-11-03 04:19:43 +0000

    Revert "Start implementing AV1 support for VideoToolbox"
    This reverts commit 5e941f6edae2af75a5a5b1cbb38803f79968e585.
    
    Reason for revert: Suspect for Mac10.15 failures:
    https://ci.chromium.org/p/chromium/builders/ci/Mac10.15%20Tests
    
    First failing build:
    https://ci.chromium.org/p/chromium/builders/ci/Mac10.15%20Tests/b8765511037820635393
    
    Original change's description:
    > Start implementing AV1 support for VideoToolbox
    >
    > Bug: 1493614
    > Change-Id: I57d3205021d07f36d823b28010cb0ccd6c60a924
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994860
    > Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    > Reviewed-by: Dan Sanders <sandersd@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1219077}
    
    Bug: 1493614
    Change-Id: I32c8309eec4bbdea7b1599b8de68eaa8acd6b562
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5004265
    Auto-Submit: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219312}

--- media/gpu/mac/vt_config_util.h
+++ media/gpu/mac/vt_config_util.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <CoreMedia/CoreMedia.h>
 
 #include "base/apple/scoped_cftyperef.h"
-#include "base/containers/span.h"
 #include "media/base/video_codecs.h"
 #include "media/base/video_color_space.h"
 #include "media/gpu/media_gpu_export.h"
@@ -22,10 +21,8 @@ namespace media {
 MEDIA_GPU_EXPORT base::apple::ScopedCFTypeRef<CFDictionaryRef>
 CreateFormatExtensions(CMVideoCodecType codec_type,
                        VideoCodecProfile profile,
-                       int bit_depth,
                        const VideoColorSpace& color_space,
-                       absl::optional<gfx::HDRMetadata> hdr_metadata,
-                       absl::optional<base::span<const uint8_t>> csd_box);
+                       absl::optional<gfx::HDRMetadata> hdr_metadata);
 
 }  // namespace media
 
