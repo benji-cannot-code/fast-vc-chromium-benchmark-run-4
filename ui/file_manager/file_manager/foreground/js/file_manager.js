1ef9ef0d5befdcfacf960957a86cc5f2f7515049
François Degros <fdegros@chromium.org>
2023-11-03 02:06:53 +0000

    Files App: Reacts to bulk-pinning becoming [un]available
    BUG=b:308722772
    
    Change-Id: Ia5ee22683e5b5251d3a502714d4310e1a4457984
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4995226
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219269}

--- ui/file_manager/file_manager/foreground/js/file_manager.js
+++ ui/file_manager/file_manager/foreground/js/file_manager.js
@@ -25,7 +25,7 @@ import {getKeyModifiers, queryDecoratedElement, queryRequiredElement} from '../.
 import {FakeEntryImpl} from '../../common/js/files_app_entry_types.js';
 import {FilesAppState} from '../../common/js/files_app_state.js';
 import {FilteredVolumeManager} from '../../common/js/filtered_volume_manager.js';
-import {isDlpEnabled, isDriveFsBulkPinningEnabled, isGuestOsEnabled, isJellyEnabled, isNewDirectoryTreeEnabled} from '../../common/js/flags.js';
+import {isDlpEnabled, isGuestOsEnabled, isJellyEnabled, isNewDirectoryTreeEnabled} from '../../common/js/flags.js';
 import {recordEnum, recordInterval, startInterval} from '../../common/js/metrics.js';
 import {ProgressItemState} from '../../common/js/progress_center_common.js';
 import {str} from '../../common/js/translations.js';
@@ -385,9 +385,22 @@ export class FileManager extends EventTarget {
 
     /**
      * Whether Drive is enabled. Retrieved from user preferences.
-     * @private @type {?boolean}
+     * @private @type {boolean}
      */
     this.driveEnabled_ = false;
+
+    /**
+     * Whether Drive bulk-pinning is available on this device. Retrieved from
+     * user preferences.
+     * @private @type {boolean}
+     */
+    this.bulkPinningAvailable_ = false;
+
+    /**
+     * Whether Drive bulk-pinning has been initialized in Files App.
+     * @private @type {boolean}
+     */
+    this.bulkPinningInitialized_ = false;
 
     /**
      * A fake Drive placeholder item.
@@ -835,30 +848,28 @@ export class FileManager extends EventTarget {
       fileListPromise,
       currentDirectoryPromise,
       this.setGuestMode_(),
-      this.initBulkPinning_(),
     ]);
   }
 
   /**
    * Subscribes to bulk-pinning events to ensure the store is kept up to date.
    * Also tries to retrieve a first bulk pinning progress to populate the store.
-   * Does nothing if bulk-pinning is disabled.
    * @private
    */
   async initBulkPinning_() {
-    if (!isDriveFsBulkPinningEnabled()) {
-      return;
-    }
-
     try {
       const promise = getBulkPinProgress();
 
-      // @ts-ignore: error TS7006: Parameter 'progress' implicitly has an 'any'
-      // type.
-      chrome.fileManagerPrivate.onBulkPinProgress.addListener((progress) => {
-        console.debug('Got bulk-pinning event:', progress);
-        this.store_.dispatch(updateBulkPinProgress(progress));
-      });
+      if (!this.bulkPinningInitialized_) {
+        // @ts-ignore: error TS7006: Parameter 'progress' implicitly has an
+        // 'any' type.
+        chrome.fileManagerPrivate.onBulkPinProgress.addListener((progress) => {
+          console.debug('Got bulk-pinning event:', progress);
+          this.store_.dispatch(updateBulkPinProgress(progress));
+        });
+
+        this.bulkPinningInitialized_ = true;
+      }
 
       const progress = await promise;
       if (progress) {
@@ -2006,7 +2017,7 @@ export class FileManager extends EventTarget {
     try {
       prefs = await getPreferences();
     } catch (e) {
-      console.error('Failed to retrieve preferences:', e);
+      console.error('Cannot get preferences:', e);
       return;
     }
 
@@ -2017,6 +2028,15 @@ export class FileManager extends EventTarget {
       this.driveEnabled_ = prefs.driveEnabled;
       this.toggleDriveRootOnPreferencesUpdate_();
       redraw = true;
+    }
+
+    if (this.bulkPinningAvailable_ !== prefs.driveFsBulkPinningAvailable) {
+      this.bulkPinningAvailable_ = prefs.driveFsBulkPinningAvailable;
+      console.debug(`Bulk-pinning is now ${
+          this.bulkPinningAvailable_ ? 'available' : 'unavailable'}`);
+      if (this.bulkPinningAvailable_) {
+        await this.initBulkPinning_();
+      }
     }
 
     if (this.trashEnabled !== prefs.trashEnabled) {
@@ -2031,7 +2051,7 @@ export class FileManager extends EventTarget {
       redraw = true;
     }
 
-    this.updateOfficePrefs_(prefs);
+    await this.updateOfficePrefs_(prefs);
 
     if (redraw && !isNewDirectoryTreeEnabled()) {
       // @ts-ignore: error TS2339: Property 'redraw' does not exist on type
