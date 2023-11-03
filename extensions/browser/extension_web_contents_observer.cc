9ee2e571b42504619df86d0d8f1d4c8571a0a775
Lukasz Anforowicz <lukasza@chromium.org>
2023-11-03 21:03:42 +0000

    Use `ForEachRenderFrameHost` in `ScriptInjectionTracker`.
    This CL removes from `ScriptInjectionTracker` the ad-hoc frame tracking
    based on `WebContentsObserver::RenderFrameCreated` and
    `WCO::RenderFrameDeleted`.  The CL replaces it with a call to the
    recently (in Nov 2022) added `RenderProcessHost::ForEachRenderFrameHost`
    API.
    
    There is no specific reason to think that the old code was broken, but
    let me speculatively associate this CL with bugs 1431795 and 1439642...
    Maybe it will help?
    
    Bug: 1431795, 1439642
    Change-Id: Ib7fc09859a3b0b91b88a440f701c73e3a7bb1def
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5002871
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219673}

--- extensions/browser/extension_web_contents_observer.cc
+++ extensions/browser/extension_web_contents_observer.cc
@@ -154,7 +154,6 @@ void ExtensionWebContentsObserver::RenderFrameCreated(
     content::RenderFrameHost* render_frame_host) {
   DCHECK(initialized_);
   InitializeRenderFrame(render_frame_host);
-  ScriptInjectionTracker::RenderFrameCreated(PassKey(), render_frame_host);
 
   const Extension* extension = GetExtensionFromFrame(render_frame_host, false);
   if (!extension)
@@ -198,7 +197,6 @@ void ExtensionWebContentsObserver::RenderFrameDeleted(
   ProcessManager::Get(browser_context_)
       ->UnregisterRenderFrameHost(render_frame_host);
   ExtensionApiFrameIdMap::Get()->OnRenderFrameDeleted(render_frame_host);
-  ScriptInjectionTracker::RenderFrameDeleted(PassKey(), render_frame_host);
 }
 
 void ExtensionWebContentsObserver::ReadyToCommitNavigation(
