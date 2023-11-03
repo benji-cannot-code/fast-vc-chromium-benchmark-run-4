fa816585ae9c562af1891d7bea917e867a492182
Scott Yoder <scottyoder@google.com>
2023-11-03 16:17:42 +0000

    [ios] Increase text contrast for Large Fakebox
    Screenshot with changes:
    https://screenshot.googleplex.com/cYQwG4NFoLfrDTB.png
    
    Fixed: 1498981
    Change-Id: Ia985bc3b9cc5b0efd9070d1dda363431ea0aace8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5001761
    Reviewed-by: Huiting Yu <huitingyu@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1219529}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_utils.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_utils.mm
@@ -88,6 +88,16 @@ CGFloat FakeToolbarVerticalMargin() {
        kAdaptiveLocationBarVerticalMargin);
   return vertical_margin + dynamic_type_vertical_adjustment;
 }
+
+// Returns the color to use for the Lens and Voice icons in the Fakebox.
+UIColor* FakeboxIconColor() {
+  if (IsIOSLargeFakeboxEnabled()) {
+    return [UIColor colorNamed:kGrey700Color];
+  } else if (IsMagicStackEnabled()) {
+    return [UIColor colorNamed:@"fake_omnibox_placeholder_color"];
+  }
+  return [UIColor colorNamed:kTextfieldPlaceholderColor];
+}
 }
 
 namespace content_suggestions {
@@ -251,7 +261,7 @@ void ConfigureVoiceSearchButton(UIButton* voice_search_button,
 
   UIImage* mic_image = DefaultSymbolWithPointSize(
       kMicrophoneSymbol, kSymbolContentSuggestionsPointSize);
-  voice_search_button.tintColor = SearchHintLabelColor();
+  voice_search_button.tintColor = FakeboxIconColor();
 
   [voice_search_button setImage:mic_image forState:UIControlStateNormal];
   [voice_search_button setAccessibilityLabel:l10n_util::GetNSString(
@@ -277,7 +287,7 @@ void ConfigureLensButton(UIButton* lens_button, UIView* search_tap_target) {
       kCameraLensSymbol, kSymbolContentSuggestionsPointSize);
 
   [lens_button setImage:camera_image forState:UIControlStateNormal];
-  lens_button.tintColor = SearchHintLabelColor();
+  lens_button.tintColor = FakeboxIconColor();
   lens_button.accessibilityLabel = l10n_util::GetNSString(IDS_IOS_ACCNAME_LENS);
   lens_button.accessibilityIdentifier = @"Lens";
 
@@ -308,7 +318,7 @@ BOOL ShouldShowWiderMagicStackLayer(UITraitCollection* traitCollection,
 
 UIColor* SearchHintLabelColor() {
   if (IsIOSLargeFakeboxEnabled()) {
-    return [UIColor colorNamed:kGrey700Color];
+    return [UIColor colorNamed:kGrey800Color];
   } else if (IsMagicStackEnabled()) {
     return [UIColor colorNamed:@"fake_omnibox_placeholder_color"];
   }
