e3d5eb72735bba31837131a4ea8e1828043c1fd8
Renjie Tang <renjietang@chromium.org>
2023-11-03 01:00:39 +0000

    Add a config knob in Chrome to tune the frequency of multi-port pobing.
    Change-Id: Idb63697fb409a5f06ef8ea191cb0be431d108046
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5001952
    Commit-Queue: Nick Harper <nharper@chromium.org>
    Auto-Submit: Renjie Tang <renjietang@chromium.org>
    Commit-Queue: Renjie Tang <renjietang@chromium.org>
    Reviewed-by: Nick Harper <nharper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219248}

--- net/quic/quic_proxy_client_socket_unittest.cc
+++ net/quic/quic_proxy_client_socket_unittest.cc
@@ -250,7 +250,8 @@ class QuicProxyClientSocketTest
         quic::QuicTime::Delta::FromMilliseconds(
             kDefaultRetransmittableOnWireTimeout.InMilliseconds()),
         /*migrate_idle_session=*/true, /*allow_port_migration=*/false,
-        kDefaultIdleSessionMigrationPeriod, kMaxTimeOnNonDefaultNetwork,
+        kDefaultIdleSessionMigrationPeriod, /*multi_port_probing_interval=*/0,
+        kMaxTimeOnNonDefaultNetwork,
         kMaxMigrationsToNonDefaultNetworkOnWriteError,
         kMaxMigrationsToNonDefaultNetworkOnPathDegrading,
         kQuicYieldAfterPacketsRead,
@@ -258,8 +259,7 @@ class QuicProxyClientSocketTest
             kQuicYieldAfterDurationMilliseconds),
         /*cert_verify_flags=*/0, quic::test::DefaultQuicConfig(),
         std::make_unique<TestQuicCryptoClientConfigHandle>(&crypto_config_),
-        dns_start, dns_end,
-        base::DefaultTickClock::GetInstance(),
+        dns_start, dns_end, base::DefaultTickClock::GetInstance(),
         base::SingleThreadTaskRunner::GetCurrentDefault().get(),
         /*socket_performance_watcher=*/nullptr, HostResolverEndpointResult(),
         NetLog::Get());
