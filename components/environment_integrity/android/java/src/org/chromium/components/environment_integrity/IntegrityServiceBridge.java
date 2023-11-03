55479e52a9ec251c754c32f7c961ea59789ecebe
Rupert Ben Wiser <bewise@chromium.org>
2023-11-03 10:06:01 +0000

    wei: Remove downstream dependency for WEI
    We are removing the WEI experimental code from Chromium.
    
    The first step is to get rid of the downstream dependency so that we can
    remove the downstream code. Once we have done that, we can remove all
    the WEI code.
    
    Bug: 1499011
    Change-Id: I6a0950a8cb2a621d6fe366013d45300483f80391
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5001988
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Peter Pakkenberg <pbirk@chromium.org>
    Auto-Submit: Rupert Wiser <bewise@chromium.org>
    Commit-Queue: Peter Pakkenberg <pbirk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219385}

--- components/environment_integrity/android/java/src/org/chromium/components/environment_integrity/IntegrityServiceBridge.java
+++ components/environment_integrity/android/java/src/org/chromium/components/environment_integrity/IntegrityServiceBridge.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.environment_integrity;
 
 import androidx.annotation.NonNull;
+import androidx.concurrent.futures.CallbackToFutureAdapter;
 import androidx.core.os.ExecutorCompat;
 
 import com.google.common.util.concurrent.FutureCallback;
@@ -132,7 +133,44 @@ public abstract class IntegrityServiceBridge {
     private static IntegrityServiceBridgeDelegate getDelegate() {
         ThreadUtils.checkUiThread();
         if (sDelegate == null) {
-            sDelegate = new IntegrityServiceBridgeDelegateImpl();
+            // Temporary hardcoded implementation to remove the dependency on the downstream version
+            // of this class.
+            sDelegate =
+                    new IntegrityServiceBridgeDelegate() {
+                        private static final String NOT_SUPPORTED_ERROR =
+                                "Environment Integrity not available.";
+
+                        @Override
+                        public ListenableFuture<Long> createEnvironmentIntegrityHandle(
+                                boolean ignored, int timeoutMilliseconds) {
+                            return CallbackToFutureAdapter.getFuture(
+                                    resolver -> {
+                                        resolver.setException(
+                                                new IntegrityException(
+                                                        NOT_SUPPORTED_ERROR,
+                                                        IntegrityResponse.API_NOT_AVAILABLE));
+                                        return "Placeholder.createEnvironmentIntegrityHandle";
+                                    });
+                        }
+
+                        @Override
+                        public ListenableFuture<byte[]> getEnvironmentIntegrityToken(
+                                long handle, byte[] requestHash, int timeoutMilliseconds) {
+                            return CallbackToFutureAdapter.getFuture(
+                                    resolver -> {
+                                        resolver.setException(
+                                                new IntegrityException(
+                                                        NOT_SUPPORTED_ERROR,
+                                                        IntegrityResponse.API_NOT_AVAILABLE));
+                                        return "Placeholder.getEnvironmentIntegrityToken";
+                                    });
+                        }
+
+                        @Override
+                        public boolean canUseGms() {
+                            return false;
+                        }
+                    };
         }
         return sDelegate;
     }
