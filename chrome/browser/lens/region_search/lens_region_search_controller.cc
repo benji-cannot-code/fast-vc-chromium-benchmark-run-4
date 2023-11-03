d9e4ac15ea45ad8ab589da2a81e2c1ae17435a2a
Juan Mojica <juanmojica@google.com>
2023-11-03 15:58:39 +0000

    Clean up region search static page feature.
    Bug: b:308661964, 1419211
    Change-Id: Ibd8fbcabcad62a6b793c5becc2959fd0b2217bec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5002256
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1219508}

--- chrome/browser/lens/region_search/lens_region_search_controller.cc
+++ chrome/browser/lens/region_search/lens_region_search_controller.cc
@@ -245,9 +245,6 @@ void LensRegionSearchController::OnCaptureCompleted(
   }
 
   RecordCaptureResult(lens::LensRegionSearchCaptureResult::SUCCESS);
-  if (web_contents() && lens::features::IsLensRegionSearchStaticPageEnabled()) {
-    web_contents()->ClosePage();
-  }
 }
 
 void LensRegionSearchController::WebContentsDestroyed() {
@@ -288,9 +285,6 @@ void LensRegionSearchController::CloseWithReason(
   if (screenshot_flow_) {
     screenshot_flow_->CancelCapture();
     screenshot_flow_.reset();
-  }
-  if (web_contents() && lens::features::IsLensRegionSearchStaticPageEnabled()) {
-    web_contents()->ClosePage();
   }
 }
 
--- chrome/common/webui_url_constants.h
+++ chrome/common/webui_url_constants.h
@@ -579,12 +579,6 @@ extern const char kCleanupSubPage[];
 extern const char kChromeUICastFeedbackHost[];
 #endif
 
-#if !BUILDFLAG(IS_ANDROID) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
-extern const char kChromeUILensUntrustedURL[];
-extern const char kChromeUILensURL[];
-extern const char kChromeUILensHost[];
-#endif
-
 // Extensions sub pages.
 extern const char kExtensionConfigureCommandsSubPage[];
 
