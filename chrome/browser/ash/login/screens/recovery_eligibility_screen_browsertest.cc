6a6f76f2083bae8418768368a6469b2e4de868df
Denis Kuznetsov <antrim@chromium.org>
2023-11-03 17:19:56 +0000

    Add mechanism to keep authenticated authsession alive
    Bug: b/271249180, b/238606050
    Change-Id: I9a842030bbef578128fb5127b9982054c0c5d716
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4750619
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Auto-Submit: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219550}

--- chrome/browser/ash/login/screens/recovery_eligibility_screen_browsertest.cc
+++ chrome/browser/ash/login/screens/recovery_eligibility_screen_browsertest.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/login/recovery_eligibility_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/user_creation_screen_handler.h"
 #include "chrome/test/base/fake_gaia_mixin.h"
+#include "chromeos/ash/components/cryptohome/constants.h"
 #include "chromeos/ash/components/osauth/public/auth_session_storage.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/test/browser_test.h"
@@ -97,6 +98,8 @@ class RecoveryEligibilityScreenTest : public OobeBaseTest {
     auto session_ids = cryptohome_.AddSession(user_context->GetAccountId(),
                                               /*authenticated=*/true);
     user_context->SetAuthSessionIds(session_ids.first, session_ids.second);
+    user_context->SetSessionLifetime(base::Time::Now() +
+                                     cryptohome::kAuthsessionInitialLifetime);
     if (ash::features::ShouldUseAuthSessionStorage()) {
       context->extra_factors_token =
           ash::AuthSessionStorage::Get()->Store(std::move(user_context));
--- chromeos/ash/components/login/auth/stub_authenticator.cc
+++ chromeos/ash/components/login/auth/stub_authenticator.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/notreached.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/time/time.h"
+#include "chromeos/ash/components/cryptohome/constants.h"
 #include "chromeos/ash/components/login/auth/public/auth_failure.h"
 #include "chromeos/ash/components/login/auth/public/cryptohome_key_constants.h"
 
@@ -221,6 +223,8 @@ UserContext StubAuthenticator::ExpectedUserContextWithTransformedKey() const {
   user_context.SetAuthFactorsConfiguration(
       AuthFactorsConfiguration{{password}, factors});
   user_context.SetAuthSessionIds("someauthsessionid", "broadcast");
+  user_context.SetSessionLifetime(base::Time::Now() +
+                                  cryptohome::kAuthsessionInitialLifetime);
   return user_context;
 }
 
