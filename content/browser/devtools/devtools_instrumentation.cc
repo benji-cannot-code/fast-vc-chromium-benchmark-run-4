e017a585a67783108a5c0ee2653ac32e85c20ca6
Zachary Tan <tanzachary@chromium.org>
2023-11-03 17:18:39 +0000

    [FedCM] Add devtools issues for error API
    We still show the error UI, just without the more details button when
    the error URL is cross-site with the config URL. The cross-site devtools
    issue can help with debugging when that occurs.
    
    On top of that, it makes it easier for developers to debug by decoupling
    the case where we fail to fetch a token for an IDP specific reason and
    the case where we fail to fetch a token for other reasons.
    
    Bug: 1498201
    Change-Id: I7f4feec969c0114a7fa366f44063c89ea82e62fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4998233
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Zachary Tan <tanzachary@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219547}

--- content/browser/devtools/devtools_instrumentation.cc
+++ content/browser/devtools/devtools_instrumentation.cc
@@ -326,6 +326,14 @@ FederatedAuthRequestResultToProtocol(
     case FederatedAuthRequestResult::kErrorFetchingIdTokenInvalidResponse: {
       return FederatedAuthRequestIssueReasonEnum::IdTokenInvalidResponse;
     }
+    case FederatedAuthRequestResult::kErrorFetchingIdTokenIdpErrorResponse: {
+      return FederatedAuthRequestIssueReasonEnum::IdTokenIdpErrorResponse;
+    }
+    case FederatedAuthRequestResult::
+        kErrorFetchingIdTokenCrossSiteIdpErrorResponse: {
+      return FederatedAuthRequestIssueReasonEnum::
+          IdTokenCrossSiteIdpErrorResponse;
+    }
     case FederatedAuthRequestResult::kErrorFetchingIdTokenInvalidContentType: {
       return FederatedAuthRequestIssueReasonEnum::IdTokenInvalidContentType;
     }
