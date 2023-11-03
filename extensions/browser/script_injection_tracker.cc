d4f30045eae6a52ddd8eee97e09e1606909c60df
Lukasz Anforowicz <lukasza@chromium.org>
2023-11-03 02:20:32 +0000

    Add `LifecycleState` to `ScopedScriptInjectionTrackerFailureCrashKeys`.
    Bug: 1431795, 1439642
    Change-Id: I2826960feb94fe55cb2e276c72db0423965bf301
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5002254
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219275}

--- extensions/browser/script_injection_tracker.cc
+++ extensions/browser/script_injection_tracker.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/memory/raw_ref.h"
 #include "base/ranges/algorithm.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/trace_event/typed_macros.h"
 #include "components/guest_view/browser/guest_view_base.h"
 #include "content/public/browser/browser_context.h"
@@ -965,6 +966,12 @@ base::debug::CrashKeyString* GetLastCommittedUrlCrashKey() {
   return crash_key;
 }
 
+base::debug::CrashKeyString* GetLifecycleStateCrashKey() {
+  static auto* crash_key = base::debug::AllocateCrashKeyString(
+      "lifecycle_state", base::debug::CrashKeySize::Size32);
+  return crash_key;
+}
+
 base::debug::CrashKeyString* GetDoWebViewScriptsMatchCrashKey() {
   static auto* crash_key = base::debug::AllocateCrashKeyString(
       "do_web_view_scripts_match", base::debug::CrashKeySize::Size32);
@@ -1018,6 +1025,9 @@ ScopedScriptInjectionTrackerFailureCrashKeys::
       frame.GetLastCommittedOrigin().GetDebugString());
   last_committed_url_crash_key_.emplace(GetLastCommittedUrlCrashKey(),
                                         frame_url.possibly_invalid_spec());
+  lifecycle_state_crash_key_.emplace(
+      GetLifecycleStateCrashKey(),
+      base::NumberToString(static_cast<int>(frame.GetLifecycleState())));
 
   const ExtensionRegistry* registry =
       ExtensionRegistry::Get(frame.GetBrowserContext());
