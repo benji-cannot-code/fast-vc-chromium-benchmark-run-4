fe703beff318be7c5de1bb2f2c9926790fb0fa63
Noémie St-Onge <noemies@google.com>
2023-11-03 13:34:17 +0000

    [iOS] Clean up kSettingsWillBeDismissedBugFixKillSwitch
    kSettingsWillBeDismissedBugFixKillSwitch was added in M118 to guard a
    bug fix. This CL removes the kill switch since the fix has now been
    around for a few milestones and no issues related to it have came up.
    
    Fixed: 1491460
    Change-Id: I555a68990fb5f0119e735dbedd0a2451e1b760c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5001909
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1219436}

--- ios/chrome/browser/ui/settings/settings_root_table_view_controller_unittest.mm
+++ ios/chrome/browser/ui/settings/settings_root_table_view_controller_unittest.mm
@@ -5,11 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/settings/settings_root_table_view_controller.h"
 
-#import "base/test/scoped_feature_list.h"
 #import "base/test/task_environment.h"
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
 #import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
-#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/ui/settings/settings_navigation_controller.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "testing/gtest/include/gtest/gtest.h"
@@ -173,10 +171,6 @@ TEST_F(SettingsRootTableViewControllerTest, TestDeleteToolbar) {
 // view controller will be removed from the navigation stack.
 TEST_F(SettingsRootTableViewControllerTest,
        TestSettingsWillBeDismissedCallTiming) {
-  // Make sure kill switch is enabled.
-  base::test::ScopedFeatureList feature_list(
-      kSettingsWillBeDismissedBugFixKillSwitch);
-
   FakeSettingsRootTableViewController* controller =
       [[FakeSettingsRootTableViewController alloc]
           initWithStyle:UITableViewStylePlain];
