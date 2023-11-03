4903c7ea750b1c99c4313046e1ef07f4265e1404
Monica Basta <msalama@chromium.org>
2023-11-03 10:24:59 +0000

    Fix ProfileKeyedServiceBrowserTest.GuestProfileParent_NeededServices
    The test is failing as a result of https://crrev.com/c/4995111.
    `TpcdSupportService` keyed service is created if
    `net::features::kTpcdSupportSettings` and
    `features::kPersistentOriginTrials` are enabled.
    
    Force features activation to remove differences between official and non
    official run of the tests. As the feature is integrated in the
    fieldtrial_testing_config.json which might not be considered under an
    official build. Adding it under `InitWithFeatures()` to activate it
    would neglect that difference.
    
    Bug: 1497049
    Change-Id: I76edc18bb78c4244609138c3615d7a45f62afb93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5002051
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Owners-Override: Monica Basta <msalama@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219391}

--- chrome/browser/profiles/profile_keyed_service_browsertest.cc
+++ chrome/browser/profiles/profile_keyed_service_browsertest.cc
@@ -21,8 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/common/omnibox_features.h"
 #include "components/optimization_guide/machine_learning_tflite_buildflags.h"
 #include "components/supervised_user/core/common/buildflags.h"
+#include "content/public/common/content_features.h"
 #include "content/public/test/browser_test.h"
 #include "extensions/buildflags/buildflags.h"
+#include "net/base/features.h"
 #include "pdf/buildflags.h"
 #include "printing/buildflags/buildflags.h"
 #include "third_party/blink/public/common/features.h"
@@ -168,6 +170,8 @@ class ProfileKeyedServiceBrowserTest : public InProcessBrowserTest {
           companion::visual_search::features::kVisualSearchSuggestions,
 #endif  // !BUILDFLAG(IS_ANDROID)
           blink::features::kBrowsingTopics,
+          net::features::kTpcdSupportSettings,
+          features::kPersistentOriginTrials,
 #if BUILDFLAG(BUILD_WITH_TFLITE_LIB)
           omnibox::kOnDeviceTailModel,
           omnibox::kOnDeviceHeadProviderNonIncognito,
