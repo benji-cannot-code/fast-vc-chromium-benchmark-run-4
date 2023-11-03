354bbb25d4c8e47d1cd09a919155138bf4a91513
Dave Tapuska <dtapuska@chromium.org>
2023-11-03 16:16:43 +0000

    Remove a usage of V8PerIsolateData::MainThreadIsolate
    Use the ForEachMainThreadIsolate call instead of the skyhook.
    
    Bug: 263412
    Change-Id: I59d4f7d8206841d78084135b46b0394a4cd0932d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5001748
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219528}

--- third_party/blink/renderer/core/timezone/timezone_controller.cc
+++ third_party/blink/renderer/core/timezone/timezone_controller.cc
@@ -19,8 +19,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/workers/worker_backing_thread.h"
 #include "third_party/blink/renderer/core/workers/worker_or_worklet_global_scope.h"
 #include "third_party/blink/renderer/core/workers/worker_thread.h"
-#include "third_party/blink/renderer/platform/bindings/v8_per_isolate_data.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
+#include "third_party/blink/renderer/platform/scheduler/public/main_thread.h"
+#include "third_party/blink/renderer/platform/scheduler/public/main_thread_scheduler.h"
 #include "third_party/icu/source/common/unicode/char16ptr.h"
 #include "third_party/icu/source/i18n/unicode/timezone.h"
 #include "v8/include/v8.h"
@@ -85,7 +86,11 @@ bool SetIcuTimeZoneAndNotifyV8(const String& timezone_id) {
 
   icu::TimeZone::adoptDefault(timezone.release());
 
-  NotifyTimezoneChangeToV8(V8PerIsolateData::MainThreadIsolate());
+  Thread::MainThread()
+      ->Scheduler()
+      ->ToMainThreadScheduler()
+      ->ForEachMainThreadIsolate(WTF::BindRepeating(
+          [](v8::Isolate* isolate) { NotifyTimezoneChangeToV8(isolate); }));
   WorkerThread::CallOnAllWorkerThreads(&NotifyTimezoneChangeOnWorkerThread,
                                        TaskType::kInternalDefault);
   DispatchTimeZoneChangeEventToFrames();
