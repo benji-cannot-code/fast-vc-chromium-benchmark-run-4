b311d5d41035a27cd7d4fcff28a2722fac4f64e0
Hsin-chen Chuang <chharry@chromium.org>
2023-11-03 04:33:10 +0000

    [floss] Only set FlossEnabeld / LLPrivacy / DevCoreDump on Ash
    These settings shouldn't be set by LaCrOS, and the other paltforms
    (such as linux) may not expect these on starting Chrome, either.
    
    BUG=b:308362469
    TEST=Start a user session with LaCrOS only, verify no above commands
    
    Change-Id: I33346204b3622f1770f8e2fd592fdd29c44bf5a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4988750
    Reviewed-by: Katherine Lai <laikatherine@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Hsin-chen Chuang <chharry@google.com>
    Cr-Commit-Position: refs/heads/main@{#1219316}

--- device/bluetooth/floss/floss_manager_client.cc
+++ device/bluetooth/floss/floss_manager_client.cc
@@ -299,13 +299,13 @@ void FlossManagerClient::Init(dbus::Bus* bus,
       service_name, dbus::ObjectPath(kObjectManagerPath));
   object_manager_->RegisterInterface(kManagerInterface, this);
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
   // Enable Floss and retry a few times until it is set.
   SetFlossEnabled(floss::features::IsFlossEnabled(), kSetFlossRetryCount,
                   kSetFlossRetryDelayMs,
                   base::BindOnce(&FlossManagerClient::CompleteSetFlossEnabled,
                                  weak_ptr_factory_.GetWeakPtr()));
 
-#if BUILDFLAG(IS_CHROMEOS)
   SetDevCoredump(base::BindOnce([](DBusResult<Void> ret) {
                    if (!ret.has_value()) {
                      LOG(ERROR) << "Fail to set devcoredump.\n";
@@ -313,7 +313,6 @@ void FlossManagerClient::Init(dbus::Bus* bus,
                  }),
                  base::FeatureList::IsEnabled(
                      chromeos::bluetooth::features::kBluetoothFlossCoredump));
-#endif  // BUILDFLAG(IS_CHROMEOS)
 
   SetLLPrivacy(
       base::BindOnce([](DBusResult<bool> ret) {
@@ -324,6 +323,7 @@ void FlossManagerClient::Init(dbus::Bus* bus,
         }
       }),
       base::FeatureList::IsEnabled(bluez::features::kLinkLayerPrivacy));
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 }
 
 void FlossManagerClient::HandleGetDefaultAdapter(DBusResult<int32_t> response) {
