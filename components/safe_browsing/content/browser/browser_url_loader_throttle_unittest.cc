5e55db93caebc4cac3851472310a5b09e024f2a1
Joemer Ramos <joemerramos@chromium.org>
2023-11-03 15:21:51 +0000

    [iOS] Refactor SafeBrowsingUrlCheckerImpl to merge iOS implementation
    The separate impl for iOS has caused some issues with changes being
    made in non-iOS and then not being reflected on iOS due to having
    two different constructors. This CL merges them together to prevent
    similar bugs in the future. Additionally, doing this allows us to
    opt into any additional functionality offered in the main constructor
    for iOS platforms if necessary. To do this, iOS implementations will
    need to pass a nullptr for anything related to WebContent and non-iOS
    will need to pass a nullptr for anything related to WebState since
    these classes are platform specific. This was discussed in the crbug
    attached but also mentioning here that this is okay since this change
    increases code health.
    
    Fixed: 1470823
    Change-Id: I48e898f7f296456126a10766fb2b7df1a76dbd41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994724
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219483}

--- components/safe_browsing/content/browser/browser_url_loader_throttle_unittest.cc
+++ components/safe_browsing/content/browser/browser_url_loader_throttle_unittest.cc
@@ -198,6 +198,7 @@ class MockSafeBrowsingUrlChecker : public SafeBrowsingUrlCheckerImpl {
                                    has_user_gesture,
                                    url_checker_delegate,
                                    web_contents_getter,
+                                   /*weak_web_state=*/nullptr,
                                    render_process_id,
                                    render_frame_id,
                                    frame_tree_node_id,
