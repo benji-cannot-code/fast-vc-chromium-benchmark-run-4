4e824ba72056d12ec260be5665a5d241ac7ff891
Noémie St-Onge <noemies@google.com>
2023-11-03 21:01:25 +0000

    [iOS] Fix keyboard appearing unexpectedly over Password Manager
    When opening the Password Manager through Manual Fallback (keyboard
    accessory), the keyboard would pop up over the Password Manager after a
    successful authentication. This CL prevents this from happening when the
    Auth on Entry feature is enabled.
    
    Attempted to write en EG test to prevent regression, but couldn't
    reproduce the bug in the EG test, which made the test worthless.
    
    Fixed: 1494929
    Change-Id: I9283d02bee989f0ce66b2ee5687f75886f979e6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5001925
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1219669}

--- ios/chrome/browser/ui/autofill/form_input_accessory/form_input_accessory_coordinator.mm
+++ ios/chrome/browser/ui/autofill/form_input_accessory/form_input_accessory_coordinator.mm
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/autofill/manual_fill/manual_fill_password_coordinator.h"
 #import "ios/chrome/browser/ui/bubble/bubble_constants.h"
 #import "ios/chrome/browser/ui/bubble/bubble_view_controller_presenter.h"
+#import "ios/chrome/browser/ui/settings/password/password_manager_ui_features.h"
 #import "ios/chrome/common/ui/reauthentication/reauthentication_module.h"
 #import "ios/chrome/grit/ios_branded_strings.h"
 #import "ios/chrome/grit/ios_strings.h"
@@ -376,6 +377,16 @@ const CGFloat kIPHVerticalOffset = -5;
 - (void)openPasswordManager {
   [self reset];
   [self.navigator openPasswordManager];
+
+  // The keyboard and keyboard accessory unexpectedly appear after
+  // authentication when entering the Password Manager. Resigning the first
+  // responder here fixes the issue without removing the focus on the underlying
+  // web view's field. See crbug.com/1494929.
+  if (password_manager::features::IsAuthOnEntryEnabled() ||
+      password_manager::features::IsAuthOnEntryV2Enabled()) {
+    [GetFirstResponder() resignFirstResponder];
+  }
+
   UMA_HISTOGRAM_ENUMERATION(
       "PasswordManager.ManagePasswordsReferrer",
       password_manager::ManagePasswordsReferrer::kPasswordsAccessorySheet);
