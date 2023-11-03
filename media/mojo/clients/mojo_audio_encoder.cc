1c9a31eabbb959bdc1c764d9504b72028b334770
Thomas Guilbert <tguilbert@chromium.org>
2023-11-03 17:28:05 +0000

    Restrict AAC encoding support to Android Q+
    This CL signals that AAC is unsupported on versions of Android before Q,
    instead of signaling support and then failing to create an encoder.
    
    Bug: 1498663
    Change-Id: If0f93f76e9ef89c2b700afd640bda38da38355e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5003325
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219564}

--- media/mojo/clients/mojo_audio_encoder.cc
+++ media/mojo/clients/mojo_audio_encoder.cc
@@ -6,14 +6,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/mojo/clients/mojo_audio_encoder.h"
 
 #include "base/functional/bind.h"
-#include "base/functional/callback_helpers.h"
-#include "base/logging.h"
 #include "base/task/sequenced_task_runner.h"
 #include "build/build_config.h"
 #include "media/base/audio_buffer.h"
+#include "media/base/media_switches.h"
 #include "media/mojo/common/media_type_converters.h"
 
+#if BUILDFLAG(IS_ANDROID)
+#include "media/base/android/media_codec_util.h"
+#endif
+
 namespace media {
+
+// static
+bool MojoAudioEncoder::IsSupported(AudioCodec codec) {
+  switch (codec) {
+    case AudioCodec::kAAC:
+#if BUILDFLAG(IS_ANDROID)
+      return base::FeatureList::IsEnabled(media::kPlatformAudioEncoder) &&
+             MediaCodecUtil::IsAACEncoderAvailable();
+#else
+      return base::FeatureList::IsEnabled(media::kPlatformAudioEncoder);
+#endif
+    default:
+      // We only spin up platform AudioEncoders for AAC for now.
+      return false;
+  }
+}
 
 MojoAudioEncoder::MojoAudioEncoder(
     mojo::PendingRemote<mojom::AudioEncoder> remote_encoder)
--- media/mojo/clients/mojo_audio_encoder.h
+++ media/mojo/clients/mojo_audio_encoder.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "media/base/audio_encoder.h"
 #include "media/mojo/mojom/audio_encoder.mojom.h"
-#include "media/mojo/mojom/media_types.mojom.h"
 #include "mojo/public/cpp/bindings/associated_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/remote.h"
@@ -27,6 +26,8 @@ class MojoAudioEncoder final : public AudioEncoder,
  public:
   explicit MojoAudioEncoder(
       mojo::PendingRemote<mojom::AudioEncoder> remote_encoder);
+
+  static bool IsSupported(AudioCodec codec);
 
   MojoAudioEncoder(const MojoAudioEncoder&) = delete;
   MojoAudioEncoder& operator=(const MojoAudioEncoder&) = delete;
