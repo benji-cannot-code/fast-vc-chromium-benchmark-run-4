e26a84730b182c49e957942624ea0e3db2445764
Calder Kitagawa <ckitagawa@chromium.org>
2023-11-03 17:44:45 +0000

    [Hub] Use a Supplier for ScrimAnchor
    Discovered while working on:
    https://chromium-review.googlesource.com/c/chromium/src/+/4985963
    
    The scrim anchor view is not necessarily ready when the
    HubLayoutScrimController is built for tablets. Change it to a Supplier
    which will return a non-null value by the time the scrim is shown.
    
    Bug: 1497472
    Change-Id: I153ef2e0687ec73cb316c653cbeb22ff2f0ec383
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4993802
    Reviewed-by: Sky Malice <skym@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219592}

--- chrome/browser/hub/android/java/src/org/chromium/chrome/browser/hub/HubLayoutScrimControllerUnitTest.java
+++ chrome/browser/hub/android/java/src/org/chromium/chrome/browser/hub/HubLayoutScrimControllerUnitTest.java
@@ -85,7 +85,9 @@ public class HubLayoutScrimControllerUnitTest {
 
                             mScrimController =
                                     new HubLayoutScrimController(
-                                            mScrimCoordinator, mAnchorView, () -> mIsIncognito);
+                                            mScrimCoordinator,
+                                            () -> mAnchorView,
+                                            () -> mIsIncognito);
                         });
     }
 
