c07366195e4aaaedabc8c6d4a0592e067136060c
dpapad <dpapad@chromium.org>
2023-11-03 01:32:14 +0000

    Settings: Expose experimental features prefs via chrome.settingsPrivate.
    Bug: 1497713
    Change-Id: I227a4528d10cb90df2a1980d2ad85d347160254a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5001653
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219258}

--- chrome/browser/extensions/api/settings_private/prefs_util.cc
+++ chrome/browser/extensions/api/settings_private/prefs_util.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/live_caption/pref_names.h"
 #include "components/media_router/common/pref_names.h"
 #include "components/omnibox/browser/omnibox_prefs.h"
+#include "components/optimization_guide/core/optimization_guide_prefs.h"
 #include "components/password_manager/core/common/password_manager_pref_names.h"
 #include "components/payments/core/payment_prefs.h"
 #include "components/performance_manager/public/user_tuning/prefs.h"
@@ -1103,6 +1104,23 @@ const PrefsUtil::TypedPrefMap& PrefsUtil::GetAllowlistedKeys() {
   (*s_allowlist)[performance_manager::user_tuning::prefs::
                      kManagedTabDiscardingExceptions] =
       settings_api::PrefType::PREF_TYPE_LIST;
+
+  // AI settings.
+  (*s_allowlist)
+      [optimization_guide::prefs::kModelExecutionMainToggleSettingState] =
+          settings_api::PrefType::PREF_TYPE_NUMBER;
+  (*s_allowlist)[optimization_guide::prefs::GetSettingEnabledPrefName(
+      optimization_guide::proto::ModelExecutionFeature::
+          MODEL_EXECUTION_FEATURE_COMPOSE)] =
+      settings_api::PrefType::PREF_TYPE_NUMBER;
+  (*s_allowlist)[optimization_guide::prefs::GetSettingEnabledPrefName(
+      optimization_guide::proto::ModelExecutionFeature::
+          MODEL_EXECUTION_FEATURE_TAB_ORGANIZATION)] =
+      settings_api::PrefType::PREF_TYPE_NUMBER;
+  (*s_allowlist)[optimization_guide::prefs::GetSettingEnabledPrefName(
+      optimization_guide::proto::ModelExecutionFeature::
+          MODEL_EXECUTION_FEATURE_WALLPAPER_SEARCH)] =
+      settings_api::PrefType::PREF_TYPE_NUMBER;
 
   return *s_allowlist;
 }
--- components/optimization_guide/core/optimization_guide_prefs.h
+++ components/optimization_guide/core/optimization_guide_prefs.h
@@ -21,7 +21,7 @@ extern const char kHintsFetcherHostsSuccessfullyFetched[];
 extern const char kPendingHintsProcessingVersion[];
 extern const char kPreviouslyRegisteredOptimizationTypes[];
 extern const char kStoreFilePathsToDelete[];
-extern const char kFeatureOptInMainToggleSettingState[];
+extern const char kModelExecutionMainToggleSettingState[];
 
 // Value stored in the pref.
 enum class FeatureOptInState {
