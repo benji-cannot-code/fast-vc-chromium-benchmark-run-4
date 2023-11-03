151aedb6b826cf5b1b670828963a618fa2511e64
Fredrik Söderquist <fs@opera.com>
2023-11-03 22:39:36 +0000

    Common up the SVG <mask> painting for BoxPainterBase and SVGMaskPainter
    Use the same general setup for painting the SVG <mask> in both code-
    paths by exposing a new function PaintSVGMaskLayer() on SVGMaskPainter.
    
    This removes the indirection via an Image for the BoxPainterBase code-
    path and reduces the interface of SVGMaskPainter. The new function
    relies on the caller to save GraphicsContext state. It also eliminates
    the need to depend on BackgroundImageGeometry for painting an SVG
    <mask>.
    
    Bug: 1496473
    Change-Id: I30d639a370cee4c7d7c6412d96b291034eebe6c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5002089
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219775}

--- third_party/blink/renderer/core/paint/svg_mask_painter.h
+++ third_party/blink/renderer/core/paint/svg_mask_painter.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_PAINT_SVG_MASK_PAINTER_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_PAINT_SVG_MASK_PAINTER_H_
 
-#include "third_party/blink/renderer/platform/graphics/paint/paint_record.h"
+#include "third_party/blink/renderer/platform/graphics/graphics_types.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 
 namespace gfx {
@@ -17,11 +17,11 @@ namespace blink {
 
 class DisplayItemClient;
 class GraphicsContext;
+class ImageResourceObserver;
 class LayoutObject;
 class SVGResource;
 class SVGResourceClient;
-
-enum class EMaskType : uint8_t;
+class StyleSVGMaskReferenceImage;
 
 class SVGMaskPainter {
   STATIC_ONLY(SVGMaskPainter);
@@ -30,18 +30,15 @@ class SVGMaskPainter {
   static void Paint(GraphicsContext& context,
                     const LayoutObject& layout_object,
                     const DisplayItemClient& display_item_client);
-  static PaintRecord PaintResource(SVGResource* mask_resource,
-                                   SVGResourceClient* client,
-                                   const gfx::RectF& reference_box,
-                                   float zoom);
-  static gfx::RectF ResourceBounds(SVGResource* mask_resource,
-                                   SVGResourceClient* client,
-                                   const gfx::RectF& reference_box,
-                                   float zoom);
+  static void PaintSVGMaskLayer(GraphicsContext&,
+                                const StyleSVGMaskReferenceImage&,
+                                const ImageResourceObserver&,
+                                const gfx::RectF& reference_box,
+                                const float zoom,
+                                const SkBlendMode composite_op,
+                                const bool apply_mask_type);
   static gfx::RectF ResourceBoundsForSVGChild(
       const LayoutObject& layout_object);
-  static EMaskType MaskType(SVGResource* mask_resource,
-                            SVGResourceClient* client);
   static bool MaskIsValid(SVGResource* mask_resource,
                           SVGResourceClient* client);
 };
