fc211441eb55cf87dcbf51df28e002ac4082210b
Nan Lin <linnan@chromium.org>
2023-11-04 00:06:05 +0000

    Cache attribution support in ResourceClient on creation
    The attribution support value is cached in ResourceClient on creation
    which is then used when processing the response. This fixes the issue
    that the attribution support was unavailable if the frame was already
    detached when the response was received.
    
    Bug: 1498717
    Change-Id: I5a10a07e38fab7bc4b37b1eba0c794c926504290
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5004592
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219817}

--- third_party/blink/renderer/core/frame/attribution_src_loader_test.cc
+++ third_party/blink/renderer/core/frame/attribution_src_loader_test.cc
@@ -562,9 +562,8 @@ TEST_F(AttributionSrcLoaderTest, WebDisabled_TriggerNotRegistered) {
 
   for (auto attribution_support : {network::mojom::AttributionSupport::kNone,
                                    network::mojom::AttributionSupport::kOs}) {
-    GetPage().SetAttributionSupport(attribution_support);
-
     ResourceRequest request(test_url);
+    request.SetAttributionReportingSupport(attribution_support);
     auto* resource = MakeGarbageCollected<MockResource>(test_url);
     ResourceResponse response(test_url);
     response.SetHttpStatusCode(200);
@@ -698,12 +697,11 @@ TEST_F(AttributionSrcLoaderCrossAppWebEnabledTest,
 }
 
 TEST_F(AttributionSrcLoaderCrossAppWebEnabledTest, RegisterOsTrigger) {
-  GetPage().SetAttributionSupport(
-      network::mojom::AttributionSupport::kWebAndOs);
-
   KURL test_url = ToKURL("https://example1.com/foo.html");
 
   ResourceRequest request(test_url);
+  request.SetAttributionReportingSupport(
+      network::mojom::AttributionSupport::kWebAndOs);
   auto* resource = MakeGarbageCollected<MockResource>(test_url);
   ResourceResponse response(test_url);
   response.SetHttpStatusCode(200);
