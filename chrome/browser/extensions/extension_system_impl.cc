0ab80ad22579e8d34a180fa0e0ea3aea6948a44f
Nasko Oskov <nasko@chromium.org>
2023-11-04 01:22:26 +0000

    Remove usage of NOTIFICATION_NAV_ENTRY_COMMITTED in extensions.
    The NotificationService is deprecated and should be removed, but before
    we can remove it, we need to remove all of its notifications. This CL
    removes one of those in the extensions code.
    
    The CL achieves that by refactoring the NavigationObserver object from a
    per-profile "global" object into a TabHelper which is per WebContents.
    
    The change will create a functional difference in that WebContents that
    were not tabs in the past and hit the conditions of the
    NavigationObserver will no longer do that. After discussing this with
    extensions folks, we concluded that it is actually a desired behavior
    since it is not clear how well it would work in those scenarios.
    
    Additionally, the CL removes the existing tracking of extensions ids for
    which the prompt has already been shown. It was introduced to minimize
    prompting for hosted apps, but those are now explicitly excluded from
    prompting, so the tracking isn't as necessary. It might actually be
    beneficial for each failed navigation to an extension URL when an
    extension is disabled to be prompted for, even if the first time the
    user dismissed the prompt (maybe even by accident).
    
    Bug: 268984
    Change-Id: I886556d57ead99a9250b84999b0daeb6f0ed4f6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5005524
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219842}

--- chrome/browser/extensions/extension_system_impl.cc
+++ chrome/browser/extensions/extension_system_impl.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_system_factory.h"
 #include "chrome/browser/extensions/install_verifier.h"
 #include "chrome/browser/extensions/load_error_reporter.h"
-#include "chrome/browser/extensions/navigation_observer.h"
 #include "chrome/browser/extensions/shared_module_service.h"
 #include "chrome/browser/extensions/unpacked_installer.h"
 #include "chrome/browser/extensions/update_install_gate.h"
@@ -186,8 +185,6 @@ void ExtensionSystemImpl::Shared::Init(bool extensions_enabled) {
   TRACE_EVENT0("browser,startup", "ExtensionSystemImpl::Shared::Init");
   const base::CommandLine* command_line =
       base::CommandLine::ForCurrentProcess();
-
-  navigation_observer_ = std::make_unique<NavigationObserver>(profile_);
 
   bool allow_noisy_errors =
       !command_line->HasSwitch(::switches::kNoErrorDialogs);
