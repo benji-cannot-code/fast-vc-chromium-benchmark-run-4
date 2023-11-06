18bd846cd9b408dd70d3d3edda298cc9a72e3c71
Samar <samarchehade@google.com>
2023-11-06 12:53:41 +0000

    [iOS][Choice] Add new provider APIs for choice screen promos.
    This change is part 1/3 of a three-step change:
    => https://crrev.com/c/5004058 Add new provider APIs for choice screen promos.
    * https://crrev.com/i/6632996 Only register the promo if intended for display
    * https://crrev.com/c/5001440 Update calls to the choice screen promo providers APIs
    
    Low-Coverage-Reason: TRIVIAL_CHANGE
    Bug: 1499170
    Change-Id: I20c083b93d6648e860691c3d695ac084b6bfc1e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5004058
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1220214}

--- ios/chrome/browser/providers/signin/chromium_choice.mm
+++ ios/chrome/browser/providers/signin/chromium_choice.mm
@@ -31,7 +31,16 @@ id<StandardPromoDisplayHandler> CreateChoiceDisplayHandler(
   NOTREACHED_NORETURN();
 }
 
+id<StandardPromoDisplayHandler> CreateChoiceDisplayHandler() {
+  NOTREACHED_NORETURN();
+}
+
 id<SceneAgent> CreateChoiceSceneAgent(PromosManager* promosManager) {
+  NOTREACHED_NORETURN();
+}
+
+id<SceneAgent> CreateChoiceSceneAgent(PromosManager* promosManager,
+                                      ChromeBrowserState* browserState) {
   NOTREACHED_NORETURN();
 }
 
