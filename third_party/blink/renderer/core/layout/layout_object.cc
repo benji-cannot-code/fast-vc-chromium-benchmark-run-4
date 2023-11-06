d2eeccc75076157f82ed99531a4a149ad0c63ffa
Ian Kilpatrick <ikilpatrick@chromium.org>
2023-11-06 21:12:50 +0000

    [RemoveLegacy] (Re)-Allow anonymous blocks to be containing blocks.
    We did this as part of LayoutNG originally to correctly position OOFs
    within split inlines.
    As part of the "block-in-inline" project we removed inline-splitting!
    
    As such we can re-allow anonymous blocks to be containing blocks. This
    happens when an inline (<span>) is a container for an OOF, but we
    position it relative to an anonymous wrapping block (created for
    block-in-inline purposes for example).
    
    Behind LayoutNewContainingBlock flag.
    
    Bug: 1229581
    Change-Id: I4e3e38ae490f082f1204505aeeb2e973f0fa6c33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5005819
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220512}

--- third_party/blink/renderer/core/layout/layout_object.cc
+++ third_party/blink/renderer/core/layout/layout_object.cc
@@ -1686,6 +1686,9 @@ LayoutBlock* LayoutObject::ContainingBlockForAbsolutePosition(
     AncestorSkipInfo* skip_info) const {
   NOT_DESTROYED();
   auto* container = ContainerForAbsolutePosition(skip_info);
+  if (RuntimeEnabledFeatures::LayoutNewContainingBlockEnabled()) {
+    return container ? container->InclusiveContainingBlock(skip_info) : nullptr;
+  }
   return FindNonAnonymousContainingBlock(container, skip_info);
 }
 
@@ -1693,13 +1696,17 @@ LayoutBlock* LayoutObject::ContainingBlockForFixedPosition(
     AncestorSkipInfo* skip_info) const {
   NOT_DESTROYED();
   auto* container = ContainerForFixedPosition(skip_info);
+  if (RuntimeEnabledFeatures::LayoutNewContainingBlockEnabled()) {
+    return container ? container->InclusiveContainingBlock(skip_info) : nullptr;
+  }
   return FindNonAnonymousContainingBlock(container, skip_info);
 }
 
-const LayoutBlock* LayoutObject::InclusiveContainingBlock() const {
+LayoutBlock* LayoutObject::InclusiveContainingBlock(
+    AncestorSkipInfo* skip_info) {
   NOT_DESTROYED();
   auto* layout_block = DynamicTo<LayoutBlock>(this);
-  return layout_block ? layout_block : ContainingBlock();
+  return layout_block ? layout_block : ContainingBlock(skip_info);
 }
 
 const PaintLayer* LayoutObject::ContainingScrollContainerLayer(
@@ -1736,6 +1743,7 @@ const LayoutBox* LayoutObject::ContainingScrollContainer(
 
 LayoutObject* LayoutObject::NonAnonymousAncestor() const {
   NOT_DESTROYED();
+  DCHECK(!RuntimeEnabledFeatures::LayoutNewContainingBlockEnabled());
   LayoutObject* ancestor = Parent();
   while (ancestor && ancestor->IsAnonymous())
     ancestor = ancestor->Parent();
@@ -1753,6 +1761,7 @@ LayoutObject* LayoutObject::NearestAncestorForElement() const {
 
 bool LayoutObject::IsAnonymousNGMulticolInlineWrapper() const {
   NOT_DESTROYED();
+  DCHECK(!RuntimeEnabledFeatures::LayoutNewContainingBlockEnabled());
   if (!IsLayoutNGBlockFlow() || !IsAnonymousBlock())
     return false;
 
@@ -1766,6 +1775,7 @@ bool LayoutObject::IsAnonymousNGMulticolInlineWrapper() const {
 LayoutBlock* LayoutObject::FindNonAnonymousContainingBlock(
     LayoutObject* container,
     AncestorSkipInfo* skip_info) {
+  DCHECK(!RuntimeEnabledFeatures::LayoutNewContainingBlockEnabled());
   // For inlines, we return the nearest non-anonymous enclosing
   // block. We don't try to return the inline itself. This allows us to avoid
   // having a positioned objects list in all LayoutInlines and lets us return a
--- third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.cc
+++ third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.cc
@@ -1404,7 +1404,8 @@ void NGPhysicalBoxFragment::AddOutlineRectsForInlineBox(
 
   if (ShouldIncludeBlockVisualOverflowForAnchorOnly(outline_type) &&
       !HasNonVisibleOverflow() && !HasControlClip(*this)) {
-    if (container->IsAnonymousBlock()) {
+    if (!RuntimeEnabledFeatures::LayoutNewContainingBlockEnabled() &&
+        container->IsAnonymousBlock()) {
       const auto* container_box = DynamicTo<LayoutBox>(
           container->GetLayoutObject()->NonAnonymousAncestor());
       if (!container_box)
