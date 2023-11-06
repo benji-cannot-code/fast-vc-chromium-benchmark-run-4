38a3c550c09ab69de4138f8081fe55d79a7d94ec
Norge Vizcay <vizcay@google.com>
2023-11-06 15:31:34 +0000

    [DataModel] Avoid profile default construction in payment helper.
    This CL is part of our efforts of making the AutofillProfile require a
    country code during construction (e.g. by removing the default
    constructor). The CL avoids the default initialization in
    payment helper.
    
    Bug: 1464568
    Change-Id: I31666606bc4320ea8255233e6a5449b188d0c90f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5005144
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1220291}

--- components/payments/content/payment_response_helper.h
+++ components/payments/content/payment_response_helper.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/raw_ref.h"
 #include "base/memory/weak_ptr.h"
+#include "components/autofill/core/browser/country_type.h"
+#include "components/autofill/core/browser/data_model/autofill_i18n_api.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/payments/content/payment_app.h"
 #include "third_party/blink/public/mojom/payments/payment_request.mojom.h"
@@ -81,7 +83,8 @@ class PaymentResponseHelper
 
   // A normalized copy of the shipping address, which will be included in the
   // PaymentResponse.
-  autofill::AutofillProfile shipping_address_;
+  autofill::AutofillProfile shipping_address_{
+      autofill::i18n_model_definition::kLegacyHierarchyCountryCode};
 
   // Instrument Details.
   std::string method_name_;
