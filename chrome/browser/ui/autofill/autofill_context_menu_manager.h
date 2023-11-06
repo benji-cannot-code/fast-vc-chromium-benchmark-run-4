2b2a16423e70c4fa5deebdc50cee93e0b01d7a9c
Jan Keitel <jkeitel@google.com>
2023-11-06 17:13:18 +0000

    Avoid dangling pointer to Browser in AutofillContextMenuManager.
    The context menu is, ultimately, owned by a WebContents:
    - WebContentsImpl owns WebContentsView
    - WebContentsView is implemented by WebContentsViewChildFrame, which
      owns WebContentsViewDelegate.
    - WebContentsViewDelegate is implemented by
      ChromeWebContentsViewDelegateViews, which owns
      RenderViewContextMenuBase.
    - RenderViewContextMenuBase is implemented by RenderViewContextMenu,
      which owns AutofillContextMenuManager.
    
    Therefore the lifetime of the AutofillContextMenuManager is tied to that
    of a WebContents. However, a WebContents can move from one Browser to
    another and, in particular, a WebContents can outlive the Browser that
    it started its life in if the Browser is closed after the move.
    As a result, the raw pointer that AutofillContextMenuManager keeps to
    its initial browser can become dangling.
    
    This CL removes this raw pointer and obtains it dynamically as needed.
    
    Fixed: 1493774
    Change-Id: I5604ffd914fcdd2e738cdcf90b67cd4f1cc4cc51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5003544
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1220350}

--- chrome/browser/ui/autofill/autofill_context_menu_manager.h
+++ chrome/browser/ui/autofill/autofill_context_menu_manager.h
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/context_menu_params.h"
 #include "ui/base/models/simple_menu_model.h"
 
-class Browser;
-
 namespace autofill {
 
 class AutofillField;
@@ -40,8 +38,7 @@ class AutofillContextMenuManager : public RenderViewContextMenuObserver {
 
   AutofillContextMenuManager(PersonalDataManager* personal_data_manager,
                              RenderViewContextMenuBase* delegate,
-                             ui::SimpleMenuModel* menu_model,
-                             Browser* browser);
+                             ui::SimpleMenuModel* menu_model);
   ~AutofillContextMenuManager() override;
   AutofillContextMenuManager(const AutofillContextMenuManager&) = delete;
   AutofillContextMenuManager& operator=(const AutofillContextMenuManager&) =
@@ -100,7 +97,6 @@ class AutofillContextMenuManager : public RenderViewContextMenuObserver {
   const raw_ptr<PersonalDataManager> personal_data_manager_;
   const raw_ptr<ui::SimpleMenuModel> menu_model_;
   const raw_ptr<RenderViewContextMenuBase> delegate_;
-  const raw_ptr<Browser> browser_;
   content::ContextMenuParams params_;
 };
 
