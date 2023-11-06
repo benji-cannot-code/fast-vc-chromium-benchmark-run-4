4c1320e74722c12415d5bf3552d0b7ee2c480ab9
Menard, Alexis <alexis.menard@intel.com>
2023-11-06 15:54:59 +0000

    Inspector: Fix ASSERT in debug when the hinge overlay is used and then the inspector is closed
    If the hinge overlay is used in the inspector (e.g. emulating a Surface Duo) and then the
    inspector is closed, debug builds would assert in the destructor of the overlay agent because
    the hinge object wasn't released. Let's make sure to reset the hinge object when the
    overlay is disabled.
    
    Change-Id: I44d462171b7cde9837d9e2289994f326fb6397ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5006561
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Alexis Menard <alexis.menard@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1220311}

--- third_party/blink/renderer/core/inspector/inspector_overlay_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_overlay_agent.cc
@@ -529,6 +529,7 @@ protocol::Response InspectorOverlayAgent::disable() {
   }
 
   persistent_tool_ = nullptr;
+  hinge_ = nullptr;
   PickTheRightTool();
   SetNeedsUnbufferedInput(false);
   document_to_ax_context_.clear();
