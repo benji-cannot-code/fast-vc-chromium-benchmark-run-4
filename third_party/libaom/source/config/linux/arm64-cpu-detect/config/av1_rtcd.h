2ff6fb0c08cb473e4a06d25af1d8f2d557fc063d
Wan-Teh Chang <wtc@google.com>
2023-11-06 19:19:17 +0000

    Roll src/third_party/libaom/source/libaom/ 1dbe1c7fa..7b5f66523 (31 commits)
    https://aomedia.googlesource.com/aom.git/+log/1dbe1c7fae24..7b5f665231df
    
    $ git log 1dbe1c7fa..7b5f66523 --date=short --no-merges --format='%ad %ae %s'
    2023-11-03 wtc Impose a maximum frame area of 2^30
    2023-11-03 george.steed cdef_block_neon.c: Make constrain16 adjdamp parameter signed
    2023-11-03 george.steed Remove partial vector loads from mem_neon.h
    2023-11-03 Arpad.Panyik highbd_pickrst_neon.c: implicit conversion fix
    2023-11-03 george.steed Remove unused UNINITIALIZED_IS_SAFE macro
    2023-11-03 george.steed loopfilter_neon.c: Avoid UNINITIALIZED_IS_SAFE
    2023-11-03 george.steed intrapred_neon.c: Avoid UNINITIALIZED_IS_SAFE
    2023-11-03 george.steed fwd_txfm_neon.c: Avoid tran_high_t->int32_t narrowing warnings
    2023-11-02 wtc Multiply width by height using the int64_t type
    2023-11-02 wtc Impose a smaller maximum frame dimension of 32768
    (...)
    2023-10-27 satheesh.kumar Add pointer to RD_STATS in MACROBLOCK structure
    2023-10-25 rachelbarker Improve loop restoration size selection
    2023-10-16 wtc Rename equiv_mode to intra_mode
    2023-10-27 wtc Do not call aom_internal_error in identify_regions
    2023-10-27 jingning Initialize use_dist_wtd_comp_avg in get_conv_params_wiener()
    2023-10-23 salome.thirot Add explicit Neon implementation of cdef_find_dir
    2023-09-01 Arpad.Panyik Add Neon path for av1_compute_stats_highbd
    2023-10-16 mudassir.galaganath Handle calls to av1_alloc_pc_tree_node() for error propagation
    2023-10-16 mudassir.galaganath Use pc_root pointer in ThreadData to allocate PC_TREE root
    2023-10-25 wtc Miscellaneous cleanup
    
    Created with:
      roll-dep src/third_party/libaom/source/libaom
    R=dalecurtis@chromium.org,eugene@chromium.org
    
    Bug: b:307414544
    Change-Id: I5e62cb5eee2cf645b2e76c68c7f57a8cc5a974ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5008053
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220429}

--- third_party/libaom/source/config/linux/arm64-cpu-detect/config/av1_rtcd.h
+++ third_party/libaom/source/config/linux/arm64-cpu-detect/config/av1_rtcd.h
@@ -1706,7 +1706,7 @@ void av1_wiener_convolve_add_src_c(const uint8_t* src,
                                    int y_step_q4,
                                    int w,
                                    int h,
-                                   const ConvolveParams* conv_params);
+                                   const WienerConvolveParams* conv_params);
 void av1_wiener_convolve_add_src_neon(const uint8_t* src,
                                       ptrdiff_t src_stride,
                                       uint8_t* dst,
@@ -1717,7 +1717,7 @@ void av1_wiener_convolve_add_src_neon(const uint8_t* src,
                                       int y_step_q4,
                                       int w,
                                       int h,
-                                      const ConvolveParams* conv_params);
+                                      const WienerConvolveParams* conv_params);
 #define av1_wiener_convolve_add_src av1_wiener_convolve_add_src_neon
 
 void cdef_copy_rect8_16bit_to_16bit_c(uint16_t* dst,
