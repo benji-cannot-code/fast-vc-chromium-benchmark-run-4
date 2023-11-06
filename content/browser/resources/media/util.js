406b8c2579efea5c2874aab20a047fb562988c80
dpapad <dpapad@chromium.org>
2023-11-06 20:45:59 +0000

    WebUI: Remove no-var and prefer-const exceptions in content/b/r/media.
    Fixing all violations discovered with
    git cl presubmit --files "content/browser/resources/media/*.js"
    
    Low-Coverage-Reason: TRIVIAL_CHANGE
    Bug: 792774
    Change-Id: Ib2d4c065a8ad5d0ab841936eb8282a410bd7e706
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5002884
    Reviewed-by: Tomas Gunnarsson <tommi@chromium.org>
    Commit-Queue: Tomas Gunnarsson <tommi@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220493}

--- content/browser/resources/media/util.js
+++ content/browser/resources/media/util.js
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @param optObj The object use as 'this' within f.
  */
 export function objectForEach(obj, f, optObj) {
-  var key;
+  let key;
   for (key in obj) {
     if (obj.hasOwnProperty(key)) {
       f.call(optObj, obj[key], key, obj);
@@ -36,7 +36,7 @@ export function millisecondsToString(timeMillis) {
     return num;
   }
 
-  var date = new Date(timeMillis);
+  const date = new Date(timeMillis);
   return pad(date.getUTCHours(), 2) + ':' + pad(date.getUTCMinutes(), 2) + ':' +
       pad(date.getUTCSeconds(), 2) + '.' +
       pad((date.getMilliseconds()) % 1000, 3);
