1201b44b20e1725a0a8a30c3d4bb36442377826c
John Delaney <johnidel@chromium.org>
2023-11-06 17:14:27 +0000

    Tag document.cookie accesses with whether they are ad tagged
    Bug: 1498494
    Change-Id: Ic9401c31665f0a17c45db9e9538171b030aa8261
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994130
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: John Delaney <johnidel@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220352}

--- android_webview/browser/network_service/aw_proxying_restricted_cookie_manager.h
+++ android_webview/browser/network_service/aw_proxying_restricted_cookie_manager.h
@@ -48,6 +48,7 @@ class AwProxyingRestrictedCookieManager
                     const url::Origin& top_frame_origin,
                     bool has_storage_access,
                     network::mojom::CookieManagerGetOptionsPtr options,
+                    bool is_ad_tagged,
                     GetAllForUrlCallback callback) override;
   void SetCanonicalCookie(const net::CanonicalCookie& cookie,
                           const GURL& url,
@@ -76,6 +77,7 @@ class AwProxyingRestrictedCookieManager
                         const url::Origin& top_frame_origin,
                         bool has_storage_access,
                         bool get_version_shared_memory,
+                        bool is_ad_tagged,
                         GetCookiesStringCallback callback) override;
 
   void CookiesEnabledFor(const GURL& url,
--- chrome/browser/page_load_metrics/observers/third_party_cookie_deprecation_metrics_observer_browsertest.cc
+++ chrome/browser/page_load_metrics/observers/third_party_cookie_deprecation_metrics_observer_browsertest.cc
@@ -179,6 +179,8 @@ IN_PROC_BROWSER_TEST_P(ThirdPartyCookieDeprecationObserverBrowserTest,
                                      false, 0);
   histogram_tester.ExpectBucketCount(kThirdPartyCookieAccessBlockedHistogram,
                                      true, 0);
+  histogram_tester.ExpectTotalCount(
+      "PageLoad.Clients.TPCD.TPCAccess.BlockedByExperiment.IsAdOrNonAd", 0);
 }
 
 IN_PROC_BROWSER_TEST_P(ThirdPartyCookieDeprecationObserverBrowserTest,
@@ -245,6 +247,9 @@ IN_PROC_BROWSER_TEST_P(ThirdPartyCookieDeprecationObserverBrowserTest,
   histogram_tester.ExpectBucketCount(
       "Blink.UseCounter.Features",
       blink::mojom::WebFeature::kThirdPartyCookieAccessBlockByExperiment, 0);
+  histogram_tester.ExpectBucketCount(
+      "Blink.UseCounter.Features",
+      blink::mojom::WebFeature::kThirdPartyCookieAdAccessBlockByExperiment, 0);
   histogram_tester.ExpectBucketCount(kThirdPartyCookieAccessBlockedHistogram,
                                      false, 0);
   histogram_tester.ExpectBucketCount(kThirdPartyCookieAccessBlockedHistogram,
@@ -284,6 +289,9 @@ IN_PROC_BROWSER_TEST_P(ThirdPartyCookieDeprecationObserverBrowserTest,
         blink::mojom::WebFeature::kThirdPartyCookieAccessBlockByExperiment, 1);
     histogram_tester.ExpectUniqueSample(kThirdPartyCookieAccessBlockedHistogram,
                                         true, 2);
+    histogram_tester.ExpectUniqueSample(
+        "PageLoad.Clients.TPCD.TPCAccess.BlockedByExperiment.IsAdOrNonAd",
+        false, 1);
   } else {
     histogram_tester.ExpectBucketCount(
         "Blink.UseCounter.Features",
@@ -509,5 +517,48 @@ IN_PROC_BROWSER_TEST_P(ThirdPartyCookieDeprecationObserverBrowserTest,
     histogram_tester.ExpectUniqueSample(
         "PageLoad.Clients.TPCD.TPCAccess.BlockedByExperiment.IsAdOrNonAd",
         false, 1);
+  }
+}
+
+IN_PROC_BROWSER_TEST_P(ThirdPartyCookieDeprecationObserverBrowserTest,
+                       ThirdPartyAdJavaScriptCookieRead) {
+  SetUpThirdPartyCookieExperimentWithClientState();
+
+  content::CookieChangeObserver observer(web_contents(),
+                                         /*num_expected_calls=*/2);
+  base::HistogramTester histogram_tester;
+  NavigateToPageWithFrame("a.com");
+  NavigateFrameTo("b.com", "/empty.html?isad=1");
+  content::RenderFrameHost* frame =
+      ChildFrameAt(web_contents()->GetPrimaryMainFrame(), 0);
+
+  // Write a third-party cookie.
+  EXPECT_TRUE(content::ExecJs(
+      frame, "document.cookie = 'foo=bar;SameSite=None;Secure';"));
+
+  // Read a third-party cookie.
+  EXPECT_TRUE(content::ExecJs(frame, "let x = document.cookie;"));
+  observer.Wait();
+  NavigateToUntrackedUrl();
+
+  if (IsRecordThirdPartyCookiesExperimentMetrics()) {
+    histogram_tester.ExpectBucketCount(
+        "Blink.UseCounter.Features",
+        blink::mojom::WebFeature::kThirdPartyCookieAdAccessBlockByExperiment,
+        1);
+    histogram_tester.ExpectUniqueSample(
+        "PageLoad.Clients.TPCD.TPCAccess.BlockedByExperiment.IsAdOrNonAd", true,
+        1);
+    histogram_tester.ExpectUniqueSample(
+        "PageLoad.Clients.TPCD.AdTPCAccess.BlockedByExperiment", true, 1);
+  } else {
+    histogram_tester.ExpectBucketCount(
+        "Blink.UseCounter.Features",
+        blink::mojom::WebFeature::kThirdPartyCookieAdAccessBlockByExperiment,
+        0);
+    histogram_tester.ExpectUniqueSample(
+        "PageLoad.Clients.TPCD.AdTPCAccess.BlockedByExperiment", false, 1);
+    histogram_tester.ExpectTotalCount(
+        "PageLoad.Clients.TPCD.TPCAccess.BlockedByExperiment.IsAdOrNonAd", 0);
   }
 }
