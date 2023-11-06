be9285b90a594f5739bd198f93ae961ef30a48b5
Anastasia Helfinstein <anastasi@google.com>
2023-11-06 20:41:10 +0000

    [Switch Access] Move ActionManager to explicit initialization
    R=josiahk@google.com
    
    AX-Relnotes: n/a.
    Bug: b/261227533
    Test: SwitchAccessAction*
    Change-Id: I682402e91efb53e7f659e5c592e07d0fa6a8ff13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4965633
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1220487}

--- chrome/browser/resources/chromeos/accessibility/switch_access/action_manager.js
+++ chrome/browser/resources/chromeos/accessibility/switch_access/action_manager.js
@@ -9,7 +9,7 @@ import {SwitchAccessMetrics} from './metrics.js';
 import {Navigator} from './navigator.js';
 import {SAChildNode, SARootNode} from './nodes/switch_access_node.js';
 import {SwitchAccess} from './switch_access.js';
-import {ActionResponse, MenuType, Mode} from './switch_access_constants.js';
+import {ActionResponse, ErrorType, MenuType, Mode} from './switch_access_constants.js';
 
 const MenuAction = chrome.accessibilityPrivate.SwitchAccessMenuAction;
 
@@ -34,11 +34,13 @@ export class ActionManager {
     this.menuStack_ = [];
   }
 
-  static get instance() {
-    if (!ActionManager.instance_) {
-      ActionManager.instance_ = new ActionManager();
+  static init() {
+    if (ActionManager.instance) {
+      throw SwitchAccess.error(
+          ErrorType.DUPLICATE_INITIALIZATION,
+          'Cannot call ActionManager.init() more than once.');
     }
-    return ActionManager.instance_;
+    ActionManager.instance = new ActionManager();
   }
 
   // ================= Static Methods ==================
@@ -319,3 +321,6 @@ export class ActionManager {
     }
   }
 }
+
+/** @type {ActionManager} */
+ActionManager.instance;
