415cb0c23274950073690f0dc962018b29a12fe1
Tim Sergeant <tsergeant@chromium.org>
2023-11-06 04:20:23 +0000

    Link Capturing: Improve tests for app-to-app Workspace link capturing
    Instead of testing the "AppToAppLinkCapturing" feature (which is not
    launching), move tests to use the "AppToAppLinkCapturingWorkspaceApps"
    feature. To make this work, we need to be able to override the allowlist
    of Workspace app IDs from the test.
    
    Bug: 1480903, b/303350482
    Change-Id: I57c489d7362dceb2008bb54c72fa237ba23b4f32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5006333
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220100}

--- chrome/browser/apps/link_capturing/chromeos_link_capturing_delegate.h
+++ chrome/browser/apps/link_capturing/chromeos_link_capturing_delegate.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_APPS_LINK_CAPTURING_CHROMEOS_LINK_CAPTURING_DELEGATE_H_
 
 #include "base/auto_reset.h"
+#include "base/containers/flat_set.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/apps/link_capturing/link_capturing_navigation_throttle.h"
 #include "components/webapps/common/web_app_id.h"
@@ -44,6 +45,11 @@ class ChromeOsLinkCapturingDelegate
   // Set clock used for timing to enable manipulation during tests.
   static base::AutoReset<const base::TickClock*> SetClockForTesting(
       const base::TickClock* tick_clock);
+
+  // Override the allowlist of Workspace app IDs, used for tests which can't
+  // install/interact with the real versions of these apps.
+  static base::AutoReset<base::flat_set<std::string>>
+  SetWorkspaceAppAllowlistForTesting(base::flat_set<std::string> allowlist);
 
   // apps::LinkCapturingNavigationThrottle::Delegate:
   bool ShouldCancelThrottleCreation(content::NavigationHandle* handle) override;
