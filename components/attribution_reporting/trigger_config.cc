08273e990770757a78b7925bd9ad6426f1888c6f
Andrew Paseltiner <apaseltiner@chromium.org>
2023-11-06 15:32:42 +0000

    Replace event report windows column with trigger specs in internals UI
    The default trigger specs are shown even when the corresponding
    AttributionReportingTriggerConfig feature is disabled, for simplicity.
    
    We render the trigger specs using their native JSON serialization for
    implementation simplicity and so that the rendering matches the
    registration input format. In the future, we may use a more
    structured-rendering approach, particularly once we have better
    DevTools integration.
    
    Screenshot available in https://crbug.com/1499244#c1.
    
    Bug: 1499244
    Change-Id: I3c33392123cfbf3cc724fcab90ff3d9448b92938
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5004588
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220293}

--- components/attribution_reporting/trigger_config.cc
+++ components/attribution_reporting/trigger_config.cc
@@ -346,7 +346,7 @@ const TriggerSpec* TriggerSpecs::SingleSharedSpec() const {
   return specs_.size() == 1 ? &specs_[0] : nullptr;
 }
 
-void TriggerSpecs::Serialize(base::Value::Dict& dict) const {
+base::Value::List TriggerSpecs::ToJson() const {
   base::Value::List spec_list;
   spec_list.reserve(specs_.size());
 
@@ -367,7 +367,11 @@ void TriggerSpecs::Serialize(base::Value::Dict& dict) const {
     }
   }
 
-  dict.Set(kTriggerSpecs, std::move(spec_list));
+  return spec_list;
+}
+
+void TriggerSpecs::Serialize(base::Value::Dict& dict) const {
+  dict.Set(kTriggerSpecs, ToJson());
 }
 
 TriggerSpecs::Iterator::Iterator(const TriggerSpecs& specs,
