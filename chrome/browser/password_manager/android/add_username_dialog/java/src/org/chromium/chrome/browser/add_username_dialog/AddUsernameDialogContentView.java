20853be8f42956b740b6578ce1575d0efd889b2f
Anna Tsvirchkova <atsvirchkova@google.com>
2023-11-06 13:33:46 +0000

    [TTFPassGen] Focus username by default in the add username dialog
    Bug: 1495656
    Change-Id: Ia5346391c0be64d718cfc92b45f5e59a9e04218b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4987316
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1220230}

--- chrome/browser/password_manager/android/add_username_dialog/java/src/org/chromium/chrome/browser/add_username_dialog/AddUsernameDialogContentView.java
+++ chrome/browser/password_manager/android/add_username_dialog/java/src/org/chromium/chrome/browser/add_username_dialog/AddUsernameDialogContentView.java
@@ -36,6 +36,7 @@ public class AddUsernameDialogContentView extends LinearLayout {
                         mUsernameChangedCallback.onResult(username.toString());
                     }
                 });
+        usernameInput.requestFocus();
     }
 
     void setPassword(String password) {
