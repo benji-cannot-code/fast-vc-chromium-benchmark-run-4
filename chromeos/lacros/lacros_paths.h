def23a0a272c505893d495296b2f51e33852980a
Andrea Orru <andreaorru@chromium.org>
2023-11-06 08:40:08 +0000

    Migrate DCHECKs to CHECKs in chrome_main_delegate.cc
    According to the current guidelines, we should strive
    to use CHECKs instead of DCHECKs unless there are
    performance concerns.
    
    As an intermediate step in that direction, this change
    migrates trivial DCHECKs to DUMP_WILL_BE_CHECKs
    in chrome_main_delegate.cc.
    
    BUG=None
    TEST=Try jobs
    
    Change-Id: I616751775a20680bcc2e326487218dd5308f4bdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5000645
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220140}

--- chromeos/lacros/lacros_paths.h
+++ chromeos/lacros/lacros_paths.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMEOS_LACROS_LACROS_PATHS_H_
 #define CHROMEOS_LACROS_LACROS_PATHS_H_
 
-#include "base/dcheck_is_on.h"
-
 namespace base {
 class FilePath;
 }
@@ -33,14 +31,12 @@ enum {
   PATH_END
 };
 
-#if DCHECK_IS_ON()
 // Returns true if the user data directory has been initialized,
 // false otherwise.
 bool IsInitializedUserDataDir();
 
 // Signals that the user data directory has been initialized.
 void SetInitializedUserDataDir();
-#endif
 
 // Call once to register the provide for the path keys defined above.
 void RegisterPathProvider();
