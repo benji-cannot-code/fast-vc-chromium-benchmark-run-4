0a224335e8e038a0a02f7b5fa54613c9fe4e9666
Sophey <sophey@chromium.org>
2023-11-06 00:15:41 +0000

    Add histogram for Compose context menu show status.
    Bug: b:308196753
    Change-Id: I8946eb26277950c2bb707f4e6d32c8ef2c46a3c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4995097
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220066}

--- chrome/browser/compose/compose_enabling.h
+++ chrome/browser/compose/compose_enabling.h
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_COMPOSE_COMPOSE_ENABLING_H_
 #define CHROME_BROWSER_COMPOSE_COMPOSE_ENABLING_H_
 
+#include "base/types/expected.h"
 #include "chrome/browser/compose/translate_language_provider.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "components/compose/core/browser/compose_metrics.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/translate/core/browser/translate_manager.h"
 #include "content/public/browser/context_menu_params.h"
@@ -18,8 +20,11 @@ class ComposeEnabling {
   explicit ComposeEnabling(
       TranslateLanguageProvider* translate_language_provider);
   ~ComposeEnabling();
-  bool IsEnabledForProfile(Profile* profile);
-  bool IsEnabled(Profile* profile, signin::IdentityManager* identity_manager);
+  base::expected<void, compose::ComposeShowStatus> IsEnabledForProfile(
+      Profile* profile);
+  base::expected<void, compose::ComposeShowStatus> IsEnabled(
+      Profile* profile,
+      signin::IdentityManager* identity_manager);
   void SetEnabledForTesting();
   void ClearEnabledForTesting();
   std::string GetLanguage();
@@ -37,10 +42,11 @@ class ComposeEnabling {
   raw_ptr<TranslateLanguageProvider> translate_language_provider_;
   bool enabled_for_testing_;
 
-  bool PageLevelChecks(Profile* profile,
-                       translate::TranslateManager* translate_manager,
-                       const url::Origin& top_level_frame_origin,
-                       const url::Origin& element_frame_origin);
+  base::expected<void, compose::ComposeShowStatus> PageLevelChecks(
+      Profile* profile,
+      translate::TranslateManager* translate_manager,
+      const url::Origin& top_level_frame_origin,
+      const url::Origin& element_frame_origin);
 };
 
 #endif  // CHROME_BROWSER_COMPOSE_COMPOSE_ENABLING_H_
