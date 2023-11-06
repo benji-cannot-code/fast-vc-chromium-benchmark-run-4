176ecaaf2986c1eced7aaea30ae2c82651fb3a0c
Owen Zhang <owenzhang@google.com>
2023-11-06 17:42:35 +0000

    [App Shortcuts] Build Shortcut Shelf UI.
    For app shortcuts, app items in the shelf should have a badge (the
    badge will be the app icon of the app that hosts the app shortcut).
    This updates AppIconLoaderDelegate::OnAppImageUpdated to pass the badge
    image in addition to the app image. The badge image is currently set for
    app shortcuts only.
    
    In a follow-on CL, correct behavior for active state will be implemented.
    
    BUG=b:306294395
    
    Change-Id: I92edb67636775fbead20c7315f1c79ef400babc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4990632
    Commit-Queue: Owen Zhang <owenzhang@google.com>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220364}

--- ash/app_list/views/app_list_item_view.cc
+++ ash/app_list/views/app_list_item_view.cc
@@ -1381,7 +1381,7 @@ void AppListItemView::Layout() {
 
   const int shortcut_background_container_dimension =
       app_list_config_->GetShortcutBackgroundContainerDimension();
-  const int shotcut_host_badge_icon_container_dimension =
+  const int shortcut_host_badge_icon_container_dimension =
       app_list_config_->GetShortcutHostBadgeIconContainerDimension();
 
   const gfx::Size shortcut_background_container_size =
@@ -1389,8 +1389,8 @@ void AppListItemView::Layout() {
                 shortcut_background_container_dimension);
 
   const gfx::Size shotcut_host_badge_icon_container_size =
-      gfx::Size(shotcut_host_badge_icon_container_dimension,
-                shotcut_host_badge_icon_container_dimension);
+      gfx::Size(shortcut_host_badge_icon_container_dimension,
+                shortcut_host_badge_icon_container_dimension);
 
   if (shortcut_background_container_ && has_host_badge_) {
     shortcut_background_container_->SetBackground(
@@ -1443,7 +1443,7 @@ void AppListItemView::Layout() {
     host_badge_icon_container_->SetBackground(
         views::CreateThemedRoundedRectBackground(
             cros_tokens::kCrosSysSystemOnBaseOpaque,
-            shortcut_background_container_dimension / 2, 0));
+            shortcut_host_badge_icon_container_dimension / 2, 0));
 
     host_badge_icon_container_->SetBoundsRect(host_badge_icon_container_bounds);
   }
