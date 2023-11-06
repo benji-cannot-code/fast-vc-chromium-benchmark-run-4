41e7dc2d120c1707f7def85b65abd53725395590
Md. Sadiqul Amin <sadiqul.amin@samsung.com>
2023-11-06 20:11:29 +0000

    Use base::Contains() instead of std::find()
    Use base::Contains() instead of std;:find() where appropriate for better readability.
    
    Bug: 561800
    
    Signed-off-by: Md. Sadiqul Amin <sadiqul.amin@samsung.com>
    Change-Id: I438547a93a1b200a1aeac3b106fa33a129068332
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4964841
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220471}

--- chrome/browser/ash/accessibility/speech_monitor.cc
+++ chrome/browser/ash/accessibility/speech_monitor.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/accessibility/speech_monitor.h"
 
+#include "base/containers/contains.h"
 #include "base/strings/pattern.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "content/public/browser/browser_task_traits.h"
@@ -144,8 +145,7 @@ void SpeechMonitor::WillSpeakUtteranceWithVoice(
     const content::VoiceData& voice_data) {
   if (!utterance_queue_.empty() &&
       utterance_queue_.back().text == utterance->GetText() &&
-      std::find(repeated_speech_.begin(), repeated_speech_.end(),
-                utterance->GetText()) == repeated_speech_.end()) {
+      !base::Contains(repeated_speech_, utterance->GetText())) {
     repeated_speech_.push_back(utterance->GetText());
   }
 
