a83014b967c09b27c63011efb0c2a073d96e0c92
Ian Vollick <vollick@chromium.org>
2023-11-06 15:22:39 +0000

    [ios] Fix webfont typeface factory tests
    In crrev.com/c/4994317, tests were added that use two fields for this
    struct for IS_APPLE, but the second field only exists if IS_MAC.
    
    It also appears that some tests fail once the tests build
    and this CL disables them.
    
    Bug: 1498496,1499557
    Change-Id: I9fa9a05725dbe016699a3be8c1b1b979c9a6243f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5002081
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220281}

--- third_party/blink/renderer/platform/fonts/web_font_typeface_factory_test.cc
+++ third_party/blink/renderer/platform/fonts/web_font_typeface_factory_test.cc
@@ -150,7 +150,13 @@ TEST(WebFontTypefaceFactoryTest, VariableColrV0FallbackWindowsApple) {
                                          mock_font_format_check, expectation);
 }
 
-TEST(WebFontTypefaceFactoryTest, SbixFallbackWindows) {
+#if BUILDFLAG(IS_IOS)
+// TODO(crbug.com/1499557): Currently fails on the platform.
+#define MAYBE_SbixFallbackWindows DISABLED_SbixFallbackWindows
+#else
+#define MAYBE_SbixFallbackWindows SbixFallbackWindows
+#endif
+TEST(WebFontTypefaceFactoryTest, MAYBE_SbixFallbackWindows) {
   sk_sp<SkData> data = SkData::MakeEmpty();
   MockFontFormatCheck mock_font_format_check(data);
   EXPECT_CALL(mock_font_format_check, IsSbixColorFont())
@@ -167,7 +173,14 @@ TEST(WebFontTypefaceFactoryTest, SbixFallbackWindows) {
   );
 }
 
-TEST(WebFontTypefaceFactoryTest, VariationsWinFallbackIfNeeded) {
+#if BUILDFLAG(IS_IOS)
+// TODO(crbug.com/1499557): Currently fails on the platform.
+#define MAYBE_VariationsWinFallbackIfNeeded \
+  DISABLED_VariationsWinFallbackIfNeeded
+#else
+#define MAYBE_VariationsWinFallbackIfNeeded VariationsWinFallbackIfNeeded
+#endif
+TEST(WebFontTypefaceFactoryTest, MAYBE_VariationsWinFallbackIfNeeded) {
   sk_sp<SkData> data = SkData::MakeEmpty();
   MockFontFormatCheck mock_font_format_check(data);
   EXPECT_CALL(mock_font_format_check, IsVariableFont())
