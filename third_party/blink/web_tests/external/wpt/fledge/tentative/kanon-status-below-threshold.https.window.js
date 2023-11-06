ba45b24cc6bc46e25d9d20e1ca7db1a4cc09ed6a
xtlsheep <xtlsheep@google.com>
2023-11-06 16:47:26 +0000

    Add web platform test for kAnonsStatus in reportWin().
    In the reportWin() function, KAnonStatus has four enum string values:
    notCalculated, belowThreshold, passedNotEnforced, and passedAndEnforced.
    The status is determined by two feature flags, namely
    FledgeConsiderKAnonymity and FledgeEnforceKAnonymity, along with the
    Chrome K-Anonymous server. Due to the difficulty of simulating the
    Chrome K-Anonymous server in web platform tests, the current testing
    covers only notCalculated and belowThreshold.
    
    Bug: 292274284
    Change-Id: I68e083c4075e21543b65c0a564e710a97bf06a26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4985509
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Tianyang Xu <xtlsheep@google.com>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220337}

---
+++ third_party/blink/web_tests/external/wpt/fledge/tentative/kanon-status-below-threshold.https.window.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/testdriver.js
+// META: script=/common/utils.js
+// META: script=resources/fledge-util.sub.js
+// META: script=/common/subset-tests.js
+
+"use strict;"
+
+subsetTest(promise_test, async test => {
+    const uuid = generateUuid(test);
+    await runReportTest(
+        test, uuid,
+        { reportWinSuccessCondition:
+            `browserSignals.kAnonStatus === "belowThreshold"`,
+          reportWin:
+            `sendReportTo('${createBidderReportURL(uuid)}');` },
+        // expectedReportUrls:
+        [createBidderReportURL(uuid)]);
+    },
+    'Check kAnonStatus is "belowThreshold" when FledgeConsiderKAnonymity' +
+    'is enabled and FledgeEnforceKAnonymity is disabled');
