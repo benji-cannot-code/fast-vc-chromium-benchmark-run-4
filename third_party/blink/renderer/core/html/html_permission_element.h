47d4ace26b9760067f046c33cc3346e05a1d33ba
Thomas Nguyen <tungnh@chromium.org>
2023-11-06 15:19:39 +0000

    [PEPC] Permission element subscribes permission statuses change
    Permission element on renderer process will subscribe multiple
    permissions status change from browser side, and update label
    accordingly.
    
    Bug: 1462930
    Change-Id: I2f4856817f1dad63cd72b4e388ab2baf9d6e5e47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4936652
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Kamila Hasanbega <hkamila@chromium.org>
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220278}

--- third_party/blink/renderer/core/html/html_permission_element.h
+++ third_party/blink/renderer/core/html/html_permission_element.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/html_element.h"
 #include "third_party/blink/renderer/core/html/shadow/permission_shadow_element.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_receiver_set.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 #include "third_party/blink/renderer/platform/wtf/hash_map.h"
 #include "third_party/blink/renderer/platform/wtf/hash_set.h"
@@ -22,7 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class CORE_EXPORT HTMLPermissionElement final : public HTMLElement {
+class CORE_EXPORT HTMLPermissionElement final
+    : public HTMLElement,
+      public mojom::blink::PermissionObserver {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
@@ -70,6 +73,13 @@ class CORE_EXPORT HTMLPermissionElement final : public HTMLElement {
   // PermissionService's API.
   void RequestPageEmbededPermissions();
 
+  void RegisterPermissionObserver(
+      const mojom::blink::PermissionDescriptorPtr& descriptor,
+      mojom::blink::PermissionStatus current_status);
+
+  // mojom::blink::PermissionObserver override.
+  void OnPermissionStatusChange(mojom::blink::PermissionStatus status) override;
+
   // Callback triggered when <permission>  element is registered from browser
   // process.
   void OnPageEmbeddedPermissionControlRegistered(
@@ -111,6 +121,17 @@ class CORE_EXPORT HTMLPermissionElement final : public HTMLElement {
   scoped_refptr<base::SingleThreadTaskRunner> GetTaskRunner();
 
   HeapMojoRemote<mojom::blink::PermissionService> permission_service_;
+
+  // Holds all `PermissionObserver` receivers connected with remotes in browser
+  // process. Each of them corresponds to a permission observer of one
+  // descriptor in `permission_descriptors_`.
+  // This set uses `PermissionName` as context type. Once a receiver call is
+  // triggered, we look into its name to determine which permission is changed.
+  HeapMojoReceiverSet<mojom::blink::PermissionObserver,
+                      HTMLPermissionElement,
+                      HeapMojoWrapperMode::kWithContextObserver,
+                      mojom::blink::PermissionName>
+      receivers_;
 
   //  Map holds all current permission statuses, keyed by permission name.
   using PermissionStatusMap =
