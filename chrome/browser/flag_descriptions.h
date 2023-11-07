b72515bfe411bd728d59493709c956318bf9e915
Xinghui Lu <xinghuilu@chromium.org>
2023-11-07 23:06:58 +0000

    Add chrome flag: safe-browsing-new-gms-core-api-for-browse-url-database-check
    Bug: 1490736
    Change-Id: Ic3fd03a5f4c70ca2a5a587632ff5bebf4cec7bcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5008066
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221245}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2548,6 +2548,9 @@ extern const char
 extern const char kSafeBrowsingHashPrefixRealTimeLookupsName[];
 extern const char kSafeBrowsingHashPrefixRealTimeLookupsDescription[];
 
+extern const char kSafeBrowsingNewGmsApiForBrowseUrlDatabaseCheckName[];
+extern const char kSafeBrowsingNewGmsApiForBrowseUrlDatabaseCheckDescription[];
+
 extern const char kScreenshotsForAndroidV2Name[];
 extern const char kScreenshotsForAndroidV2Description[];
 
