f8288b7579c9d2aa5f88283208a6bc3f2d5dd5f3
“Kavita <kavitasoni@chromium.org>
2023-11-07 16:51:46 +0000

    Only let user's tap on the input element to trigger autofill suggestions
    This CL prevents autofill suggestions to be shown in the case where
    user taps anywhere on the frame and Javascript shifts the focus to
    an input element and that triggers autofill suggestions.
    
    Bug: 1463935
    Change-Id: I97902ec007b5059305212da9d0f903d9690b451f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4873652
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Kavita Soni <kavitasoni@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1220975}

--- chrome/renderer/autofill/password_autofill_agent_browsertest.cc
+++ chrome/renderer/autofill/password_autofill_agent_browsertest.cc
@@ -1915,7 +1915,7 @@ TEST_F(PasswordAutofillAgentTest, KeyboardReplacingSurfaceClosed) {
 
   // Touch to fill will be shown multiple times until
   // KeyboardReplacingSurfaceClosed() gets called.
-  FocusElement(kPasswordName);
+  autofill_agent_->FormControlElementClicked(password_element_);
   EXPECT_TRUE(password_autofill_agent_->ShouldSuppressKeyboard());
   EXPECT_EQ(WebAutofillState::kNotFilled, password_element_.GetAutofillState());
 
@@ -1933,7 +1933,7 @@ TEST_F(PasswordAutofillAgentTest, KeyboardReplacingSurfaceClosed) {
   UpdateUrlForHTML(kFormHTML);
   UpdateUsernameAndPasswordElements();
   SimulateOnFillPasswordForm(fill_data_);
-  FocusElement(kPasswordName);
+  autofill_agent_->FormControlElementClicked(password_element_);
 
   // After the reload touch to fill is shown again.
   EXPECT_TRUE(password_autofill_agent_->ShouldSuppressKeyboard());
