7a79566ef4e519d530124ee1f41c911a52cc0584
Ernesto Izquierdo Clua <eic@google.com>
2023-11-07 23:31:04 +0000

    [ios][auth-on-entry2] Record visits to password settings
    Records visits to the settings page of the password manager.
    
    Bug: 1462419
    Change-Id: If7996e2e11351da46534fa42ea5af55ed57babcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4985682
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1221265}

--- ios/chrome/browser/ui/settings/password/password_settings/password_settings_coordinator.mm
+++ ios/chrome/browser/ui/settings/password/password_settings/password_settings_coordinator.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/passwords/model/ios_chrome_account_password_store_factory.h"
 #import "ios/chrome/browser/passwords/model/ios_chrome_affiliation_service_factory.h"
 #import "ios/chrome/browser/passwords/model/ios_chrome_profile_password_store_factory.h"
+#import "ios/chrome/browser/passwords/model/metrics/ios_password_manager_metrics.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
@@ -152,6 +153,11 @@ constexpr const char* kBulkMovePasswordsToAccountConfirmationDialogAccepted =
   // Alert informing the user that passwords are being prepared for
   // export.
   UIAlertController* _preparingPasswordsAlert;
+
+  // Whether the metric counting visits to the page was already recorded.
+  // Used to avoid over-recording the metric after each successful
+  // authentication.
+  BOOL _visitRecorded;
 }
 
 #pragma mark - ChromeCoordinator
@@ -195,6 +201,12 @@ constexpr const char* kBulkMovePasswordsToAccountConfirmationDialogAccepted =
 
   _mediator.consumer = _passwordSettingsViewController;
   _passwordSettingsViewController.delegate = _mediator;
+
+  // Only record visit if no auth is required, otherwise wait for successful
+  // auth.
+  if (_skipAuthenticationOnStart) {
+    [self maybeRecordVisitMetric];
+  }
 
   [self startReauthCoordinatorWithAuthOnStart:!_skipAuthenticationOnStart];
 
@@ -541,7 +553,7 @@ constexpr const char* kBulkMovePasswordsToAccountConfirmationDialogAccepted =
 
 - (void)successfulReauthenticationWithCoordinator:
     (ReauthenticationCoordinator*)coordinator {
-  // No-op.
+  [self maybeRecordVisitMetric];
 }
 
 - (void)willPushReauthenticationViewController {
@@ -690,6 +702,18 @@ constexpr const char* kBulkMovePasswordsToAccountConfirmationDialogAccepted =
 // Cancels the password export flow.
 - (void)onExportFlowCancelled {
   [_mediator exportFlowCanceled];
+}
+
+// Logs a Password Settings visit. Only logs the first time it is invoked, no-op
+// after that.
+- (void)maybeRecordVisitMetric {
+  if (_visitRecorded) {
+    return;
+  }
+
+  _visitRecorded = YES;
+  password_manager::LogPasswordManagerSurfaceVisit(
+      password_manager::PasswordManagerSurface::kPasswordSettings);
 }
 
 @end
