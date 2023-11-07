961301cf1f64108d9122eb9d8ab6952d1667d79f
Alex Turner <alexmt@chromium.org>
2023-11-07 17:50:59 +0000

    Enable Private Aggregation multi-cloud feature by default
    Enables PrivateAggregationApiMultipleCloudProviders. See the approved
    I2S here:
    https://groups.google.com/a/chromium.org/g/blink-dev/c/kze4FiMsZTY
    
    Bug: 1481254
    Change-Id: I29a1915efbca7c4a60cd6e981ed176e8ce27c013
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4986893
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221014}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1505,7 +1505,7 @@ constexpr base::FeatureParam<bool> kPrivateAggregationApiDebugModeEnabledAtAll{
 // selection will be ignored and replaced with the default.
 BASE_FEATURE(kPrivateAggregationApiMultipleCloudProviders,
              "PrivateAggregationApiMultipleCloudProviders",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kProcessHtmlDataImmediately,
              "ProcessHtmlDataImmediately",
