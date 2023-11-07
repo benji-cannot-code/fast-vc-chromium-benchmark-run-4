df99ec007239757b05294607b03f84569a55f592
Andrea Orru <andreaorru@chromium.org>
2023-11-07 17:05:56 +0000

    Record whether Lacros is enabled for each user
    In preparation of further work for preventing Lacros from prelaunching
    at login screen if no user has it activated, we record that piece of
    information in the local state.
    
    BUG=1496157
    TEST=Try jobs
    
    Change-Id: Ieaccb811dc93551054b5997f0c01df4188d69680
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4983125
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220987}

--- components/user_manager/known_user.cc
+++ components/user_manager/known_user.cc
@@ -117,6 +117,9 @@ const char kTokenHandleRotatedObsolete[] = "TokenHandleRotated";
 // Cache of the auth factors configured for the user.
 const char kAuthFactorPresenceCache[] = "AuthFactorsPresenceCache";
 
+// Records for each user whether Lacros is enabled.
+const char kLacrosEnabled[] = "lacros_enabled";
+
 // List containing all the known user preferences keys.
 const char* kReservedKeys[] = {kCanonicalEmail,
                                kGAIAIdKey,
@@ -140,7 +143,8 @@ const char* kReservedKeys[] = {kCanonicalEmail,
                                kPasswordSyncToken,
                                kOnboardingCompletedVersion,
                                kPendingOnboardingScreen,
-                               kAuthFactorPresenceCache};
+                               kAuthFactorPresenceCache,
+                               kLacrosEnabled};
 
 // List containing all known user preference keys that used to be reserved and
 // are now obsolete.
@@ -808,6 +812,10 @@ std::string KnownUser::GetPendingOnboardingScreen(const AccountId& account_id) {
   }
   // Return empty string if no screen is pending.
   return std::string();
+}
+
+void KnownUser::SetLacrosEnabled(const AccountId& account_id, bool enabled) {
+  SetBooleanPref(account_id, kLacrosEnabled, enabled);
 }
 
 bool KnownUser::UserExists(const AccountId& account_id) {
