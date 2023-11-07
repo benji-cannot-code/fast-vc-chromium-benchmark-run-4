9d0a8913488bd29bf1ecbaa332a8878f92967060
WhaleChang <whalechang@google.com>
2023-11-07 03:46:34 +0000

    Remove bluetooth-wbs-dogfood feature flag
    bluetooth-wbs-dogfood is expired.
    
    BUG=chromium:1419111
    TEST=CQ
    
    Change-Id: I6b3c8097224733e4c3d659f0a9efc25173d07e10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5004305
    Reviewed-by: Hsinyu Chao <hychao@chromium.org>
    Commit-Queue: Ching Yun Chang <whalechang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1220709}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3268,9 +3268,6 @@ extern const char kBluetoothFlossTelephonyDescription[];
 extern const char kBluetoothQualityReportName[];
 extern const char kBluetoothQualityReportDescription[];
 
-extern const char kBluetoothWbsDogfoodName[];
-extern const char kBluetoothWbsDogfoodDescription[];
-
 extern const char kBluetoothCoredumpName[];
 extern const char kBluetoothCoredumpDescription[];
 
