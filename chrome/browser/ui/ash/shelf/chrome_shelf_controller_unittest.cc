357796e45870a132326373dda0e0237c4ed47278
Jun Ishiguro <junis@google.com>
2023-11-07 01:04:58 +0000

    Modify name and short_name of the Lacros browser app
    This CL changes the name of Lacros to "Google Chrome" and the short name
    to "Chrome" in branded builds.
    
    Bug: 1479818
    Change-Id: Ia652f7642cb18ecf3320755099a43fa286c68d57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4996595
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220647}

--- chrome/browser/ui/ash/shelf/chrome_shelf_controller_unittest.cc
+++ chrome/browser/ui/ash/shelf/chrome_shelf_controller_unittest.cc
@@ -144,6 +144,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "chrome/common/pref_names.h"
+#include "chrome/grit/branded_strings.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
 #include "chrome/test/base/test_browser_window_aura.h"
 #include "chrome/test/base/testing_profile.h"
@@ -197,6 +198,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/aura/client/window_parenting_client.h"
 #include "ui/aura/window.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/models/menu_model.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_features.h"
@@ -1947,7 +1949,8 @@ TEST_F(ChromeShelfControllerTest, PreinstalledApps) {
 
 TEST_F(ChromeShelfControllerLacrosTest, LacrosPinnedByDefault) {
   InitShelfController();
-  EXPECT_EQ("Chrome", GetPinnedAppStatus());
+  std::string lacros_app_name = l10n_util::GetStringUTF8(IDS_PRODUCT_NAME);
+  EXPECT_EQ(lacros_app_name, GetPinnedAppStatus());
 }
 
 // Checks that AppService instance is updated appropriately for one Chrome app
