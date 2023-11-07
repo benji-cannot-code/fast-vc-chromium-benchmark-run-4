115f153c857323c57a78a4a71b51919aba2f0030
rajendrant <rajendrant@chromium.org>
2023-11-07 01:20:34 +0000

    Add model execution manager unittest
    Change-Id: I2a29ff47d6e53e1c9bcfcdcb04afe3891ea0cc06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5008755
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220652}

--- components/optimization_guide/core/model_execution/model_execution_manager_unittest.cc
+++ components/optimization_guide/core/model_execution/model_execution_manager_unittest.cc
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test.pb.h"
 #include "components/optimization_guide/core/model_execution/on_device_model_service_controller.h"
 #include "components/optimization_guide/core/optimization_guide_logger.h"
+#include "components/optimization_guide/core/optimization_guide_util.h"
 #include "components/signin/public/identity_manager/identity_test_environment.h"
+#include "components/variations/scoped_variations_ids_provider.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
 #include "services/network/test/test_url_loader_factory.h"
@@ -19,7 +21,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace optimization_guide {
 
+using base::test::TestMessage;
+
 namespace {
+
+constexpr char kOptimizationGuideServiceUrl[] =
+    "https://optimization-guide-server.com/?key=foo_key";
+
+TestMessage BuildTestMessage(const std::string& test_message_str) {
+  TestMessage test_message;
+  test_message.set_test(test_message_str);
+  return test_message;
+}
+
+proto::ExecuteResponse BuildTestExecuteResponse(const TestMessage& message) {
+  proto::ExecuteResponse execute_response;
+  proto::Any* any_metadata = execute_response.mutable_response_metadata();
+  any_metadata->set_type_url("type.googleapis.com/" + message.GetTypeName());
+  message.SerializeToString(any_metadata->mutable_value());
+  return execute_response;
+}
 
 class ModelExecutionManagerTest : public testing::Test {
  public:
@@ -36,6 +57,25 @@ class ModelExecutionManagerTest : public testing::Test {
         &optimization_guide_logger_);
   }
 
+  bool SimulateResponse(const std::string& content,
+                        net::HttpStatusCode http_status) {
+    return test_url_loader_factory_.SimulateResponseForPendingRequest(
+        kOptimizationGuideServiceUrl, content, http_status,
+        network::TestURLLoaderFactory::kUrlMatchPrefix);
+  }
+
+  bool SimulateSuccessfulResponse() {
+    std::string serialized_response;
+    proto::ExecuteResponse execute_response =
+        BuildTestExecuteResponse(BuildTestMessage("foo response"));
+    execute_response.SerializeToString(&serialized_response);
+    return SimulateResponse(serialized_response, net::HTTP_OK);
+  }
+
+  signin::IdentityTestEnvironment* identity_test_env() {
+    return &identity_test_env_;
+  }
+
   ModelExecutionManager* model_execution_manager() {
     return model_execution_manager_.get();
   }
@@ -43,6 +83,8 @@ class ModelExecutionManagerTest : public testing::Test {
  private:
   base::test::TaskEnvironment task_environment_;
   signin::IdentityTestEnvironment identity_test_env_;
+  variations::ScopedVariationsIdsProvider scoped_variations_ids_provider_{
+      variations::VariationsIdsProvider::Mode::kUseSignedInState};
   scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
   network::TestURLLoaderFactory test_url_loader_factory_;
   OptimizationGuideLogger optimization_guide_logger_;
@@ -59,11 +101,36 @@ TEST_F(ModelExecutionManagerTest, ExecuteModelEmptyAccessToken) {
           [](base::RunLoop* run_loop,
              OptimizationGuideModelExecutionResult result,
              std::unique_ptr<ModelQualityLogEntry> log_entry) {
-            ASSERT_FALSE(result.has_value());
-            ASSERT_EQ(log_entry.get(), nullptr);
+            EXPECT_FALSE(result.has_value());
+            EXPECT_EQ(log_entry.get(), nullptr);
             run_loop->Quit();
           },
           &run_loop));
+  run_loop.RunUntilIdle();
+}
+
+TEST_F(ModelExecutionManagerTest, ExecuteModelWithUserSignIn) {
+  base::test::TestMessage test_message;
+  test_message.set_test("some test");
+  base::RunLoop run_loop;
+  identity_test_env()->MakePrimaryAccountAvailable(
+      "test_email", signin::ConsentLevel::kSignin);
+  model_execution_manager()->ExecuteModel(
+      proto::MODEL_EXECUTION_FEATURE_COMPOSE, test_message,
+      base::BindOnce(
+          [](base::RunLoop* run_loop,
+             OptimizationGuideModelExecutionResult result,
+             std::unique_ptr<ModelQualityLogEntry> log_entry) {
+            EXPECT_TRUE(result.has_value());
+            EXPECT_EQ("foo response",
+                      ParsedAnyMetadata<TestMessage>(result.value())->test());
+            EXPECT_EQ(log_entry.get(), nullptr);
+            run_loop->Quit();
+          },
+          &run_loop));
+  identity_test_env()->WaitForAccessTokenRequestIfNecessaryAndRespondWithToken(
+      "access_token", base::Time::Max());
+  SimulateSuccessfulResponse();
   run_loop.RunUntilIdle();
 }
 
