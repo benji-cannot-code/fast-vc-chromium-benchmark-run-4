f3c56fd587d2b2e5ad2c48fee6cc2b1632105218
mcrouse <mcrouse@google.com>
2023-11-07 01:46:21 +0000

    Add basic support for request/response logging for TabOrganization.
    This adds basic internals logging support for MES specifically for tabs.
    
    Bug: b/308530549
    Change-Id: Ica3f5c09bf479824274a656f378227a4d61f825f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5007802
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Auto-Submit: Michael Crouse <mcrouse@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220670}

--- components/optimization_guide/optimization_guide_internals/resources/optimization_guide_internals.ts
+++ components/optimization_guide/optimization_guide_internals/resources/optimization_guide_internals.ts
@@ -92,6 +92,9 @@ function getLogSource(logSource: number) {
   if (logSource == 5) {
     return 'TEXT_CLASSIFIER';
   }
+  if (logSource == 6) {
+    return 'MODEL_EXECUTION';
+  }
   return logSource.toString();
 }
 
