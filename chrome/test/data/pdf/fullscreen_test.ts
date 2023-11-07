78e548dc138bcbef1e9c76050963f924255f662b
dpapad <dpapad@chromium.org>
2023-11-07 19:43:02 +0000

    WebUI: Rename util_ts.ts to util.ts, part 5 (last).
    In this part removing a couple remaining references as well as removing
    util_ts.ts completely.
    
    Also adding WebUI PLATFORM_OWNERS as OWNERS for [1], to facilitate
    future trivial mechanical changes like this one.
    
    [1] chromeos/ash/components/human_presence/resources/
    
    Fixed: 1497028
    Change-Id: I9f1b519fe56d873b829387be015d29af9da00764
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4996506
    Reviewed-by: Charles Zhao <charleszhao@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221111}

--- chrome/test/data/pdf/fullscreen_test.ts
+++ chrome/test/data/pdf/fullscreen_test.ts
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {PdfScriptingApi} from 'chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai/pdf_scripting_api.js';
 import {FittingType} from 'chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai/pdf_viewer_wrapper.js';
 import {isMac} from 'chrome://resources/js/platform.js';
-import {getDeepActiveElement} from 'chrome://resources/js/util_ts.js';
+import {getDeepActiveElement} from 'chrome://resources/js/util.js';
 import {keyDownOn} from 'chrome://resources/polymer/v3_0/iron-test-helpers/mock-interactions.js';
 import {eventToPromise} from 'chrome://webui-test/test_util.js';
 
--- ui/webui/resources/js/util_ts.ts
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2022 The Chromium Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// TODO(crbug.com/1497028): Delete this file once all references have been
-// updated to use util.ts directly.
-export {$, appendParam, getDeepActiveElement, getRequiredElement, hasKeyModifiers, htmlEscape, isRTL, isUndoKeyboardEvent, listenOnce, quoteString} from './util.js';
