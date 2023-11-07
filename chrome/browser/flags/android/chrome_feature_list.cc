060d410657de40555dce9dd3b3ce56a495666853
Darwin Yang <darwinyang@google.com>
2023-11-07 15:23:58 +0000

    [Autofill] New Save Card Confirmation Feature Flag
    This CL adds a new autofill feature flag
    `AutofillEnableSaveCardLoadingAndConfirmation` to desktop and clank for
    developing the new save card loading and confirmation screens.
    
    Bug: 1499264
    Change-Id: Id4558960b330ed74b246a23431045a2eeacd72bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5004536
    Commit-Queue: Darwin Yang <darwinyang@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220923}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -97,6 +97,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &autofill::features::kAutofillEnablePaymentsMandatoryReauth,
     &autofill::features::kAutofillEnableMovingGPayLogoToTheRightOnClank,
     &autofill::features::kAutofillEnableCvcStorageAndFilling,
+    &autofill::features::kAutofillEnableSaveCardLoadingAndConfirmation,
     &blink::features::kForceWebContentsDarkMode,
     &blink::features::kPrerender2,
     &commerce::kCommerceMerchantViewer,
