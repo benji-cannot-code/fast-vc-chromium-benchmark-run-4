e4c030cacb3ecb0a2b9260c02f12a8ff7cce1e1b
Tom Sepez <tsepez@chromium.org>
2023-11-07 19:26:09 +0000

    Prepare for separating owning and non-owning net:IOBuffers.
    Use IOBufferWithSize for the owning cases. Currently this is
    the same as IOBuffer, but eventually IOBuffer will evolve to be
    a non-owning class.
    
    This cl applies the changes to /components.
    
    This CL was uploaded by git cl split.
    
    R=ayui@chromium.org, bradnelson@chromium.org, jinsukkim@chromium.org, mfoltz@chromium.org, olka@chromium.org, xinghuilu@chromium.org
    
    Bug: 1493389
    Change-Id: I136bea1236e20d7e2f2addb69ccedb299e76e614
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4996471
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Mark Seaborn <mseaborn@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221093}

--- components/embedder_support/android/util/input_stream_unittest.cc
+++ components/embedder_support/android/util/input_stream_unittest.cc
@@ -20,6 +20,7 @@ using base::android::AttachCurrentThread;
 using base::android::ScopedJavaLocalRef;
 using embedder_support::InputStream;
 using net::IOBuffer;
+using net::IOBufferWithSize;
 using testing::_;
 using testing::DoAll;
 using testing::Ge;
@@ -50,7 +51,7 @@ class InputStreamTest : public Test {
 
     std::unique_ptr<InputStream> input_stream(
         new InputStream(counting_jstream));
-    auto buffer = base::MakeRefCounted<IOBuffer>(bytes_requested);
+    auto buffer = base::MakeRefCounted<IOBufferWithSize>(bytes_requested);
 
     EXPECT_TRUE(input_stream->Read(buffer.get(), bytes_requested, bytes_read));
     return buffer;
@@ -67,7 +68,7 @@ TEST_F(InputStreamTest, ReadEmptyStream) {
   std::unique_ptr<InputStream> input_stream(new InputStream(empty_jstream));
   const int bytes_requested = 10;
   int bytes_read = 0;
-  auto buffer = base::MakeRefCounted<IOBuffer>(bytes_requested);
+  auto buffer = base::MakeRefCounted<IOBufferWithSize>(bytes_requested);
 
   EXPECT_TRUE(input_stream->Read(buffer.get(), bytes_requested, &bytes_read));
   EXPECT_EQ(0, bytes_read);
@@ -134,7 +135,7 @@ TEST_F(InputStreamTest, DoesNotCrashWhenExceptionThrown) {
 
   const int bytes_requested = 10;
   int bytes_read = 0;
-  auto buffer = base::MakeRefCounted<IOBuffer>(bytes_requested);
+  auto buffer = base::MakeRefCounted<IOBufferWithSize>(bytes_requested);
   EXPECT_FALSE(input_stream->Read(buffer.get(), bytes_requested, &bytes_read));
   EXPECT_EQ(0, bytes_read);
 
