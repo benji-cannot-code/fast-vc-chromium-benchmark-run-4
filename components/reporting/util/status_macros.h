a6c46910a81e5ddad75162c183286b9289c66bcf
Hong Xu <xuhong@chromium.org>
2023-11-07 22:09:03 +0000

    Use base::AlwaysFalse instead of inventing our own
    Bug: b:300464285
    Change-Id: Ib7af494d8a3461369e70d558179cd5de09a47774
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5011115
    Commit-Queue: Hong Xu <xuhong@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Auto-Submit: Hong Xu <xuhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1221209}

--- components/reporting/util/status_macros.h
+++ components/reporting/util/status_macros.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_REPORTING_UTIL_STATUS_MACROS_H_
 #define COMPONENTS_REPORTING_UTIL_STATUS_MACROS_H_
 
+#include "base/types/always_false.h"
 #include "base/types/expected.h"
 #include "components/reporting/util/status.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -21,16 +22,9 @@ absl::optional<base::unexpected<Status>> ShouldReturnStatus(
 absl::optional<base::unexpected<Status>> ShouldReturnStatus(
     base::unexpected<Status>&& status);
 
-// Helper struct to display T in error message for the static_assert
-// failure.
-template <typename...>
-struct always_false {
-  static constexpr bool value = false;
-};
-
 template <typename T>
 void ShouldReturnStatus(T) {
-  static_assert(always_false<T>::value,
+  static_assert(base::AlwaysFalse<T>,
                 "T must be either Status or base::expected<Status>");
 }
 }  // namespace reporting::internal
