bfd73447674393601df80685d4f2da70fe25dddb
Amit Paul <a.paul@samsung.com>
2023-11-07 16:54:51 +0000

    Replace std::find with base::Contains
    Source code is much easier to write:
    if (base::Contains(my_vector, my_search_value))
    than:
    if (std::find(my_vector.begin(), my_vector.end(), my_search_value)
     != my_vector.end())
    
    Bug: 561800
    
    Signed-off-by: Amit Paul <a.paul@samsung.com>
    Change-Id: Ib61c3ef535fb03decd90e5f395cb9c186a2da5e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5003546
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220979}

--- content/browser/browsing_data/clear_site_data_handler.cc
+++ content/browser/browsing_data/clear_site_data_handler.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/browsing_data/clear_site_data_handler.h"
 
+#include "base/containers/contains.h"
 #include "base/functional/bind.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_split.h"
@@ -267,8 +268,7 @@ bool ClearSiteDataHandler::ParseHeader(
       net::ClearSiteDataHeaderContents(header);
   std::string output_types;
 
-  if (std::find(input_types.begin(), input_types.end(),
-                net::kDatatypeWildcard) != input_types.end()) {
+  if (base::Contains(input_types, net::kDatatypeWildcard)) {
     input_types.push_back(net::kDatatypeCookies);
     input_types.push_back(net::kDatatypeStorage);
     input_types.push_back(net::kDatatypeCache);
