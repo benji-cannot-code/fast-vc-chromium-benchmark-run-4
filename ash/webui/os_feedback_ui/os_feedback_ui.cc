125b49ca8c4222bbcdf39f0e2fe7a8f6e076ab41
dpapad <dpapad@chromium.org>
2023-11-07 23:51:24 +0000

    WebUI: Remove obsolete chrome://test URL from CSP headers.
    Fixed: 1260297
    Change-Id: I8ddd3c5a890c951a0ff78e268b4a32fc0b4e12fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5010611
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221279}

--- ash/webui/os_feedback_ui/os_feedback_ui.cc
+++ ash/webui/os_feedback_ui/os_feedback_ui.cc
@@ -160,8 +160,7 @@ OSFeedbackUI::OSFeedbackUI(
 
   source->OverrideContentSecurityPolicy(
       network::mojom::CSPDirectiveName::ScriptSrc,
-      "script-src chrome://resources chrome://test chrome://webui-test "
-      "'self';");
+      "script-src chrome://resources chrome://webui-test 'self';");
   ash::EnableTrustedTypesCSP(source);
 
   const auto resources =
--- ash/webui/personalization_app/personalization_app_ui.cc
+++ ash/webui/personalization_app/personalization_app_ui.cc
@@ -430,8 +430,7 @@ PersonalizationAppUI::PersonalizationAppUI(
 
   source->OverrideContentSecurityPolicy(
       network::mojom::CSPDirectiveName::ScriptSrc,
-      "script-src chrome://resources chrome://test chrome://webui-test "
-      "'self';");
+      "script-src chrome://resources chrome://webui-test 'self';");
 
   ash::EnableTrustedTypesCSP(source);
 
