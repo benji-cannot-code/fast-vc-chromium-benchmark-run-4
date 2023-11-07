cbcdf026fe4a96284a0bf950e174db88c8378e56
Alan Screen <awscreen@chromium.org>
2023-11-07 06:02:43 +0000

    Enable OOPPD system print cancel retry test
    Update the test SystemPrintFromPrintPreviewCancelRetry test to use the
    Print Preview done check to know when it is safe to initiate another
    print request.  This resolves the flakiness that could occur if the
    state from the first print request was still in a processing state.
    
    Bug: 1435566
    Change-Id: I9e572b436dedbc4358a8b7ee9248d66991688723
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5008693
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220741}

--- chrome/browser/printing/system_access_process_print_browsertest.cc
+++ chrome/browser/printing/system_access_process_print_browsertest.cc
@@ -2057,10 +2057,8 @@ IN_PROC_BROWSER_TEST_F(
 // From that system dialog we can cause a cancel to occur.
 // TODO(crbug.com/809738):  Expand this to also cover in-browser, once an
 // appropriate signal is available to use for tracking expected events.
-// TODO(crbug.com/1435566):  Enable this test once it works without the need
-// for --single-process-tests flag.
 IN_PROC_BROWSER_TEST_F(SystemAccessProcessSandboxedServicePrintBrowserTest,
-                       DISABLED_SystemPrintFromPrintPreviewCancelRetry) {
+                       SystemPrintFromPrintPreviewCancelRetry) {
   AddPrinter("printer1");
   SetPrinterNameForSubsequentContexts("printer1");
   PrimeForCancelInAskUserForSettings();
@@ -2074,11 +2072,18 @@ IN_PROC_BROWSER_TEST_F(SystemAccessProcessSandboxedServicePrintBrowserTest,
   ASSERT_TRUE(web_contents);
   SetUpPrintViewManager(web_contents);
 
+  // First invoke system print from Print Preview.  Must wait until the
+  // PrintPreviewUI is completely done before proceeding to the second part
+  // of this test to ensure that the client is unregistered from the
+  // `PrintBackendServiceManager`.
+  SetCheckForPrintPreviewDone(/*check=*/true);
+
   // The expected events for this are:
   // 1.  Update the print settings, which indicates to cancel the print
-  //     request.  No further printing calls are made.
-  // No print job is created because of such an early cancel.
-  SetNumExpectedMessages(/*num=*/1);
+  //     request.  No further printing calls are made.  No print job is
+  //     created because of such an early cancel.
+  // 2.  Print Preview UI is done.
+  SetNumExpectedMessages(/*num=*/2);
 
   SystemPrintFromPreviewOnceReadyAndLoaded(/*wait_for_callback=*/true);
 
@@ -2088,6 +2093,7 @@ IN_PROC_BROWSER_TEST_F(SystemAccessProcessSandboxedServicePrintBrowserTest,
 
   // Now try to initiate the system print from a Print Preview again.
   // Same number of expected events.
+  PrepareRunloop();
   ResetNumReceivedMessages();
 
   SystemPrintFromPreviewOnceReadyAndLoaded(/*wait_for_callback=*/true);
