b8e8be56a0c73607d24602b7170251a92ca4555d
Viktor Semeniuk <vsemeniuk@google.com>
2023-11-07 13:57:02 +0000

    Reland "Moving protos and unified_password_manager_proto_utils.h to android"
    This is a reland of commit 9ded58e32583f02d38ffd8ac81f27a1c5118520b
    
    Includes are updated which should fix the problem.
    
    Original change's description:
    > Moving protos and unified_password_manager_proto_utils.h to android
    >
    > Since they are used (and can be used) only on Android this CL moves
    > them from components/ to chrome/browser/password_manager/android/.
    >
    > Bug: 1479425
    > Change-Id: Ia3df5f4f106c255fa317aac5cc2b929e346f85b4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5007599
    > Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    > Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1220795}
    
    Bug: 1479425
    Change-Id: I33dfdecdee82b0cd220b6b25d41665bec04a0221
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5005151
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1220883}

--- chrome/browser/password_manager/android/unified_password_manager_proto_utils.cc
+++ chrome/browser/password_manager/android/unified_password_manager_proto_utils.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/password_manager/core/browser/password_store/unified_password_manager_proto_utils.h"
+#include "chrome/browser/password_manager/android/unified_password_manager_proto_utils.h"
 
 #include <string>
 
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/types/cxx23_to_underlying.h"
 #include "base/values.h"
-#include "components/password_manager/core/browser/protos/list_affiliated_passwords_result.pb.h"
-#include "components/password_manager/core/browser/protos/list_passwords_result.pb.h"
-#include "components/password_manager/core/browser/protos/password_with_local_data.pb.h"
+#include "chrome/browser/password_manager/android/protos/list_affiliated_passwords_result.pb.h"
+#include "chrome/browser/password_manager/android/protos/list_passwords_result.pb.h"
+#include "chrome/browser/password_manager/android/protos/password_with_local_data.pb.h"
 #include "components/password_manager/core/browser/sync/password_proto_utils.h"
 
 using autofill::FormData;
