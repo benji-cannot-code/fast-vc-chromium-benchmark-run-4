41d46b94df938354471a3f3b1df6d8f26ecf9548
Luciano Pacheco <lucmult@chromium.org>
2023-11-07 04:11:44 +0000

    Files TS: Convert file_table_unittest.js
    Bug: b:289003444
    Change-Id: Ifc735b2def305932f9a05ddc3db3d04f2791f4e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5009155
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220719}

--- ui/file_manager/file_manager/foreground/js/ui/table/table.ts
+++ ui/file_manager/file_manager/foreground/js/ui/table/table.ts
@@ -21,6 +21,8 @@ import {TableColumnModel} from './table_column_model.js';
 import {TableHeader} from './table_header.js';
 import {TableList} from './table_list.js';
 
+
+type RenderFunction = (item: any, table: Table) => ListItem;
 /**
  * Creates a new table element.
  */
@@ -138,7 +140,7 @@ export class Table extends HTMLDivElement {
    * Returns render function for row.
    * @return Render function.
    */
-  getRenderFunction(): (_: unknown, _t: Table) => ListItem {
+  getRenderFunction(): RenderFunction {
     return this.renderFunction_;
   }
 
@@ -172,7 +174,7 @@ export class Table extends HTMLDivElement {
    * Sets render function for row.
    * @param renderFunction Render function.
    */
-  setRenderFunction(renderFunction: (_item: any, _t: Table) => ListItem) {
+  setRenderFunction(renderFunction: RenderFunction) {
     if (renderFunction === this.renderFunction_) {
       return;
     }
