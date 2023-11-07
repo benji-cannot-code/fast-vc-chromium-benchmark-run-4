4a8d1231f2c3ffa6d9de5cd34b992573fc6e9d14
Xinghui Lu <xinghuilu@chromium.org>
2023-11-07 23:25:05 +0000

    Add OPEN_LEARN_MORE_LINK action in CSBRR
    internal CL: http://cl/579334204
    
    Bug: 1490668
    Change-Id: I80de57a560ee79c0ac04d9a3eee74ac4d15198e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5005866
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221264}

--- components/safe_browsing/content/browser/web_ui/safe_browsing_ui.cc
+++ components/safe_browsing/content/browser/web_ui/safe_browsing_ui.cc
@@ -1529,6 +1529,10 @@ base::Value::Dict SerializeDownloadWarningAction(
         PROCEED_DEEP_SCAN:
       action = "PROCEED_DEEP_SCAN";
       break;
+    case ClientSafeBrowsingReportRequest::DownloadWarningAction::
+        OPEN_LEARN_MORE_LINK:
+      action = "OPEN_LEARN_MORE_LINK";
+      break;
   }
   action_dict.Set("action", action);
   action_dict.Set("is_terminal_action",
