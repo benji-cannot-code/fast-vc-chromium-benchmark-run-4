9c265d7077e7e47ad50433d01ec4d0273648febf
Calder Kitagawa <ckitagawa@chromium.org>
2023-11-07 15:42:56 +0000

    [Hub] HubLayout add back support to the previous tab
    Adds back support to HubLayout via the legacy back press support.
    Modern back press support still requires registering a handler to
    BackPressManager which will be another CL.
    
    Bug: 1498614
    Change-Id: Ie2d162dff9f300422a39e78807966fc59308a0c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5002688
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1220933}

--- chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubCoordinator.java
+++ chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubCoordinator.java
@@ -14,6 +14,7 @@ import androidx.annotation.NonNull;
 
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.supplier.ObservableSupplierImpl;
+import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.components.browser_ui.widget.gesture.BackPressHandler;
 import org.chromium.components.browser_ui.widget.gesture.BackPressHandler.BackPressResult;
 
@@ -23,16 +24,24 @@ public class HubCoordinator implements BackPressHandler {
     private final @NonNull View mMainHubParent;
     private final @NonNull HubToolbarCoordinator mHubToolbarCoordinator;
     private final @NonNull HubPaneHostCoordinator mHubPaneHostCoordinator;
+    private final @NonNull HubLayoutController mHubLayoutController;
     private final @NonNull ObservableSupplierImpl<Boolean> mHandleBackPressSupplier;
     private final @NonNull PaneBackStackHandler mPaneBackStackHandler;
+    private final @NonNull ObservableSupplier<Tab> mCurrentTabSupplier;
 
     /**
      * Creates the {@link HubCoordinator}.
      *
      * @param containerView The view to attach the Hub to.
      * @param paneSupplier Used to observe the current pane.
+     * @param hubLayoutController The controller of the {@link HubLayout}.
+     * @param currentTabSupplier The supplier of the current {@link Tab}.
      */
-    public HubCoordinator(@NonNull FrameLayout containerView, @NonNull PaneManager paneManager) {
+    public HubCoordinator(
+            @NonNull FrameLayout containerView,
+            @NonNull PaneManager paneManager,
+            @NonNull HubLayoutController hubLayoutController,
+            @NonNull ObservableSupplier<Tab> currentTabSupplier) {
         Context context = containerView.getContext();
         mContainerView = containerView;
         mMainHubParent = LayoutInflater.from(context).inflate(R.layout.hub_layout, null);
@@ -45,11 +54,17 @@ public class HubCoordinator implements BackPressHandler {
         mHubPaneHostCoordinator =
                 new HubPaneHostCoordinator(hubPaneHostView, paneManager.getFocusedPaneSupplier());
 
+        mHubLayoutController = hubLayoutController;
         mHandleBackPressSupplier = new ObservableSupplierImpl<>();
+
         mPaneBackStackHandler = new PaneBackStackHandler(paneManager);
         mPaneBackStackHandler
                 .getHandleBackPressChangedSupplier()
                 .addObserver((handlesBackPress) -> updateHandleBackPressSupplier());
+
+        mCurrentTabSupplier = currentTabSupplier;
+        mCurrentTabSupplier.addObserver((tab) -> updateHandleBackPressSupplier());
+
         updateHandleBackPressSupplier();
     }
 
@@ -67,8 +82,15 @@ public class HubCoordinator implements BackPressHandler {
         // 2) Delegate to PaneBackStackHandler. - DONE
         // 3) No-op if Start Surface was the previous layout. It should be higher priority and
         //    already handle it, but add verification.
-        // 4) Hide the Hub to the most recent tab in the current TabModel.
-        if (mPaneBackStackHandler.handleBackPress() == BackPressResult.SUCCESS) {
+        // 4) Hide the Hub to the most recent tab in the current TabModel. - DONE.
+        if (mPaneBackStackHandler.getHandleBackPressChangedSupplier().get()
+                && mPaneBackStackHandler.handleBackPress() == BackPressResult.SUCCESS) {
+            return BackPressResult.SUCCESS;
+        }
+
+        Tab tab = mCurrentTabSupplier.get();
+        if (tab != null) {
+            mHubLayoutController.selectTabAndHideHubLayout(tab.getId());
             return BackPressResult.SUCCESS;
         }
         return BackPressResult.FAILURE;
@@ -85,9 +107,10 @@ public class HubCoordinator implements BackPressHandler {
         // 1) Whether the Pane's getHandleBackPressChangedSupplier is set.
         // 2) Whether the PaneBackStackHandler getHandleBackPressChangeSupplier is set. - DONE
         // 3) Whether Start Surface was the previous layout and we are not in incognito mode.
-        // 4) Whether the current TabModel has a selected tab.
+        // 4) Whether the current TabModel has a selected tab. - DONE
         boolean shouldHandleBackPress =
-                mPaneBackStackHandler.getHandleBackPressChangedSupplier().get();
+                mPaneBackStackHandler.getHandleBackPressChangedSupplier().get()
+                        || mCurrentTabSupplier.get() != null;
         mHandleBackPressSupplier.set(shouldHandleBackPress);
     }
 }
--- chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubManagerImplUnitTest.java
+++ chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubManagerImplUnitTest.java
@@ -6,9 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.hub;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import android.app.Activity;
 import android.view.View;
@@ -26,25 +30,33 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.supplier.LazyOneshotSupplier;
+import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.ui.base.TestActivity;
 
 /** Unit tests for {@link PaneManagerImpl}. */
 @RunWith(BaseRobolectricTestRunner.class)
 public class HubManagerImplUnitTest {
+    private static final int TAB_ID = 8;
+
     @Rule public MockitoRule mMockitoRule = MockitoJUnit.rule();
 
     @Rule
     public ActivityScenarioRule<TestActivity> mActivityScenarioRule =
             new ActivityScenarioRule<>(TestActivity.class);
 
+    @Mock private Tab mTab;
     @Mock private Pane mTabSwitcherPane;
+    @Mock private HubLayoutController mHubLayoutController;
 
     private Activity mActivity;
     private FrameLayout mRootView;
+    private ObservableSupplierImpl<Tab> mTabSupplier = new ObservableSupplierImpl<>();
 
     @Before
     public void setUp() {
+        when(mTab.getId()).thenReturn(TAB_ID);
         mActivityScenarioRule
                 .getScenario()
                 .onActivity(
@@ -63,7 +75,8 @@ public class HubManagerImplUnitTest {
                         .registerPane(
                                 PaneId.TAB_SWITCHER,
                                 LazyOneshotSupplier.fromValue(mTabSwitcherPane));
-        HubManager hubManager = HubManagerFactory.createHubManager(mActivity, builder);
+        HubManager hubManager =
+                HubManagerFactory.createHubManager(mActivity, builder, mTabSupplier);
 
         PaneManager paneManager = hubManager.getPaneManager();
         assertNotNull(paneManager);
@@ -76,8 +89,9 @@ public class HubManagerImplUnitTest {
     @SmallTest
     public void testHubController() {
         PaneListBuilder builder = new PaneListBuilder(new DefaultPaneOrderController());
-        HubManagerImpl hubManager = new HubManagerImpl(mActivity, builder);
+        HubManagerImpl hubManager = new HubManagerImpl(mActivity, builder, mTabSupplier);
         HubController hubController = hubManager.getHubController();
+        hubController.setHubLayoutController(mHubLayoutController);
         assertNull(hubManager.getHubCoordinatorForTesting());
 
         hubController.onHubLayoutShow();
@@ -95,5 +109,25 @@ public class HubManagerImplUnitTest {
 
         // Container is still attached and will be removed separately.
         assertEquals(mRootView, containerView.getParent());
+    }
+
+    @Test
+    @SmallTest
+    public void testBackNavigation() {
+        PaneListBuilder builder = new PaneListBuilder(new DefaultPaneOrderController());
+        HubManagerImpl hubManager = new HubManagerImpl(mActivity, builder, mTabSupplier);
+        HubController hubController = hubManager.getHubController();
+        hubController.setHubLayoutController(mHubLayoutController);
+
+        assertFalse(hubController.onHubLayoutBackPressed());
+
+        hubController.onHubLayoutShow();
+
+        assertFalse(hubController.onHubLayoutBackPressed());
+
+        mTabSupplier.set(mTab);
+        assertTrue(hubController.onHubLayoutBackPressed());
+
+        verify(mHubLayoutController).selectTabAndHideHubLayout(eq(TAB_ID));
     }
 }
