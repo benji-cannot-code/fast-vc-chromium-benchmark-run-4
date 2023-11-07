4087281ad99910c6d440918047510ce1152dc0a3
Aaron Leventhal <aleventhal@chromium.org>
2023-11-07 20:18:53 +0000

    [A11y] Temporary/wallpaper fix for missing parent manager
    This is a placeholder until we divise the proper fix.
    Converting to a DCHECK will quiet down the reports in non-Canary
    channels.
    
    Bug: 1408242
    Change-Id: I50941ebcb2d15acdfc22ee699b89ff48a875f9a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5008503
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1221149}

--- content/browser/accessibility/browser_accessibility_cocoa.mm
+++ content/browser/accessibility/browser_accessibility_cocoa.mm
@@ -1016,7 +1016,7 @@ bool content::IsNSRange(id value) {
           ->ToBrowserAccessibilityManagerMac();
   if (!manager) {
     // TODO(accessibility) Determine why this is happening.
-    SANITIZER_NOTREACHED();
+    DCHECK(false);
     return nil;
   }
   SANITIZER_CHECK(manager->GetParentView());
