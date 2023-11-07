1a212b7cf901f829463e5a4febe58781fe90f18d
Wenbo Jie <wenbojie@chromium.org>
2023-11-07 03:56:34 +0000

    [Files TS] Convert volume_manager_util and unit test to TS
    * Merge volume_manager_util into volume_manager_impl.ts because
      that's the only file which uses volume_manager_util.
    * Update definition build rule for test_build_ts because we also
      need those types in unit tests files (e.g. chrome.runtime)
    * Make VolumeInfoImpl accepts null FileSystem.
    
    Bug: b:289003444
    Test: browser_tests --gtest_filter="*.VolumeManagerTest"
    Change-Id: Ie31e28ad9957b19a519ebe879b2b249af1573beb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5003807
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220712}

--- ui/file_manager/file_manager/common/js/test_error_reporting.js
+++ ui/file_manager/file_manager/common/js/test_error_reporting.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * Asserts that promise gets rejected.
- * @param {Promise<void>} promise
+ * @param {Promise<*>} promise
  */
 export async function assertRejected(promise) {
   let triggeredError = false;
