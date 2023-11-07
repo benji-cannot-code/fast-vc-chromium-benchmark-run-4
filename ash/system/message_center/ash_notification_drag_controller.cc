46aac2e93b4b49972d44c1db5bb02d644c25633b
Shuming Hao <shuminghao@google.com>
2023-11-07 01:20:19 +0000

    Check notification type before casting to AshNotificationView
    After rendering ARC notifications in Chrome, we should be careful when casting notification view to AshNotificationView. This CL checks for notification type and notifier IDs before static casting. Eventually we want to get rid of these checks and cast all notification views to a general class like MessageView or some base class that can cover both Ash and Arc views (tracked in b/308814203).
    
    Bug: b:308842884
    Test: manual
    Change-Id: Ifa970fd9294c5111b96dfd0261eeae4b4c7a2af3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4998675
    Commit-Queue: Shuming Hao <shuminghao@google.com>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220651}

--- ash/system/message_center/ash_notification_drag_controller.cc
+++ ash/system/message_center/ash_notification_drag_controller.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/root_window_controller.h"
 #include "ash/shell.h"
 #include "ash/system/message_center/ash_notification_view.h"
+#include "ash/system/message_center/message_center_utils.h"
 #include "ash/system/notification_center/notification_center_tray.h"
 #include "ash/system/status_area_widget.h"
 #include "ash/system/unified/unified_system_tray.h"
@@ -67,6 +68,12 @@ void AshNotificationDragController::WriteDragDataForView(
     const gfx::Point& press_pt,
     ui::OSExchangeData* data) {
   // Sets the image to show during drag.
+  // TODO(b/308814203): clean the static_cast checks by replacing
+  // `AshNotificationView*` with a base class.
+  if (!message_center_utils::IsAshNotificationView(sender)) {
+    return;
+  }
+
   AshNotificationView* notification_view =
       static_cast<AshNotificationView*>(sender);
   const absl::optional<gfx::ImageSkia> drag_image =
@@ -85,6 +92,12 @@ void AshNotificationDragController::WriteDragDataForView(
 int AshNotificationDragController::GetDragOperationsForView(
     views::View* sender,
     const gfx::Point& p) {
+  // TODO(b/308814203): clean the static_cast checks by replacing
+  // `AshNotificationView*` with a base class.
+  if (!message_center_utils::IsAshNotificationView(sender)) {
+    return ui::DragDropTypes::DRAG_NONE;
+  }
+
   const absl::optional<gfx::Rect> drag_area =
       static_cast<AshNotificationView*>(sender)->GetDragAreaBounds();
 
@@ -102,6 +115,12 @@ bool AshNotificationDragController::CanStartDragForView(
     views::View* sender,
     const gfx::Point& press_pt,
     const gfx::Point& p) {
+  // TODO(b/308814203): clean the static_cast checks by replacing
+  // `AshNotificationView*` with a base class.
+  if (!message_center_utils::IsAshNotificationView(sender)) {
+    return false;
+  }
+
   const AshNotificationView* const notification_view =
       static_cast<AshNotificationView*>(sender);
   const absl::optional<gfx::Rect> drag_area =
@@ -134,6 +153,11 @@ bool AshNotificationDragController::CanStartDragForView(
 
 void AshNotificationDragController::OnWillStartDragForView(
     views::View* dragged_view) {
+  // TODO(b/308814203): clean the static_cast checks by replacing
+  // `AshNotificationView*` with a base class.
+  if (!message_center_utils::IsAshNotificationView(dragged_view)) {
+    return;
+  }
   OnNotificationDragWillStart(static_cast<AshNotificationView*>(dragged_view));
 }
 
