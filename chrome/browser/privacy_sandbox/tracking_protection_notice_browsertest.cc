f98acb498c8879e9ffefe9ffba0173a26220680d
Fiona Macintosh <fmacintosh@google.com>
2023-11-07 16:38:10 +0000

    [3PCD] Disable 3PCD by default in hats browser test
    Individual test cases are setting 3PCD state instead
    
    Bug: b:306414714
    Change-Id: I99771f0d5944e39eb56e433c731864ff6a907e98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5008576
    Reviewed-by: Abe Boujane <boujane@google.com>
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1220965}

--- chrome/browser/privacy_sandbox/tracking_protection_notice_browsertest.cc
+++ chrome/browser/privacy_sandbox/tracking_protection_notice_browsertest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/content_settings/core/browser/cookie_settings.h"
+#include "components/content_settings/core/common/features.h"
 #include "components/content_settings/core/common/pref_names.h"
 #include "components/feature_engagement/public/feature_constants.h"
 #include "components/privacy_sandbox/privacy_sandbox_features.h"
@@ -1106,7 +1107,9 @@ class TrackingProtectionHatsBaseTest : public InProcessBrowserTest {
   explicit TrackingProtectionHatsBaseTest(
       const std::vector<base::test::FeatureRefAndParams>&
           allow_and_enable_features) {
-    feature_list_.InitWithFeaturesAndParameters(allow_and_enable_features, {});
+    feature_list_.InitWithFeaturesAndParameters(
+        allow_and_enable_features,
+        {content_settings::features::kTrackingProtection3pcd});
   }
 
   void SetUpOnMainThread() override {
