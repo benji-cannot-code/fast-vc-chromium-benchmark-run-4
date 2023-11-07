44cbdf513b028bff9fdeecfaa6593dc2d4c963d1
Ian Vollick <vollick@chromium.org>
2023-11-07 15:16:47 +0000

    [ios] Add missing include
    In crrev.com/c/4979712 the feature kInvalidateLocalSurfaceIdPreCommit
    was referenced, but the corresponding header also needed to be included.
    
    Bug: 1411704
    Change-Id: Id3946f0682e57798fd01d279daa0a6fcdfadb5ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5006765
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220921}

--- content/browser/renderer_host/render_widget_host_view_ios.mm
+++ content/browser/renderer_host/render_widget_host_view_ios.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstdint>
 
 #include "base/strings/sys_string_conversions.h"
+#include "components/viz/common/features.h"
 #include "components/viz/common/surfaces/frame_sink_id_allocator.h"
 #include "content/browser/renderer_host/browser_compositor_ios.h"
 #include "content/browser/renderer_host/input/motion_event_web.h"
