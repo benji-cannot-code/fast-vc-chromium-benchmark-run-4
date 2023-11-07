4a874bb13fa0e1c4c6f665a3e80265e102414c41
Nicolás Peña <npm@chromium.org>
2023-11-07 18:14:30 +0000

    [Gardener] Disable ClearBrowsingData test on Linux
    The test is still flaky despite attempts to disable on the linux bf
    cache bot.
    
    Bug: 1487593
    Change-Id: I4de2118b0cc29b2527126f5cfbe3176e7e9e2600
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5009097
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Owners-Override: Nicolás Peña <npm@chromium.org>
    Auto-Submit: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221031}

--- chrome/browser/page_load_metrics/observers/formfill_page_load_metrics_observer_browsertest.cc
+++ chrome/browser/page_load_metrics/observers/formfill_page_load_metrics_observer_browsertest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "build/buildflag.h"
 #include "chrome/browser/browsing_data/chrome_browsing_data_remover_constants.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -146,8 +147,13 @@ IN_PROC_BROWSER_TEST_F(FormfillPageLoadMetricsObserverBrowserTest,
       blink::mojom::WebFeature::kUserDataFieldFilledPreviously, 2);
 }
 
+#if BUILDFLAG(IS_LINUX)
+#define MAYBE_ClearBrowsingData DISABLED_ClearBrowsingData
+#else
+#define MAYBE_ClearBrowsingData ClearBrowsingData
+#endif
 IN_PROC_BROWSER_TEST_F(FormfillPageLoadMetricsObserverBrowserTest,
-                       ClearBrowsingData) {
+                       MAYBE_ClearBrowsingData) {
   // TODO(https://crbug.com/1487593): Re-enable this test on bfcache bot.
   if (content::BackForwardCache::IsBackForwardCacheFeatureEnabled()) {
     return;
