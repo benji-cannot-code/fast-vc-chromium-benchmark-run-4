399c1ba70aec1df8f828de95ec9e6a8bad45ccce
manukh <manukh@chromium.org>
2023-11-07 01:45:14 +0000

    [omnibox] `NOTREACHED` -> `DCHECK` in `AsOmniboxEventProviderType()`
    `NOTREACHED` doesn't log its message to crash report. So this will help
    debug the crashes: crbug.com/1499235 .
    
    There's also an effort (crbug.com/851128) to make `NOTREACHED` crash in
    stable. If `AsOmniboxEventProviderType()` is ok with that, we can remove
    the `return UNKNOWN_PROVIDER`. Otherwise, we should replace the
    `NOTREACHED` with a `LOG` or something.
    
    Bug: 1499235
    Change-Id: I8975799658f710c6e8187dc6731a666d7ab2c415
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5007796
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220668}

--- components/omnibox/browser/autocomplete_provider.cc
+++ components/omnibox/browser/autocomplete_provider.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <string>
 
+#include "base/notreached.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/trace_event/memory_usage_estimator.h"
@@ -181,7 +182,13 @@ AutocompleteProvider::AsOmniboxEventProviderType() const {
       //   the provider in the proto and histograms.
       return metrics::OmniboxEventProto::SEARCH;
     default:
-      NOTREACHED() << "Unhandled AutocompleteProvider::Type " << type_;
+      // TODO(crbug.com/1499235) This was a NOTREACHED that we converted to help
+      //   debug crbug.com/1499235 since NOTREACHED's don't log their message in
+      //   crash reports. Should be reverted back to a NOTREACHED or
+      //   NOTREACHED_NORETURN if their logs eventually begin being logged to
+      //   crash reports.
+      DUMP_WILL_BE_NOTREACHED_NORETURN()
+          << "[NOTREACHED] Unhandled AutocompleteProvider::Type " << type_;
       return metrics::OmniboxEventProto::UNKNOWN_PROVIDER;
   }
 }
