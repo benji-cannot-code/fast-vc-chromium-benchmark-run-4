bab1d5ad7040604f1bbf2bfd926ce519c9ee969f
Tochukwu Ibe-Ekeocha <tibeekeocha@microsoft.com>
2023-11-07 10:22:20 +0000

    Triggers Re-Layout on `app-region` CSS value diff
    Currently changes to the value of the `app-region`
    property do not trigger a layout re-render. This
    means that any regions marked as draggable with
    `app-region: drag` will remain draggable event
    after being set as non-draggable
    `app-region: no-drag`. The same goes for
    non-draggable regions which are marked as
    draggable. The regions will not be updated till
    some other change triggers a layout re-render.
    
    Bug: 1493207
    Change-Id: I7fc4d7877e7f663f0bb3174767a552c24d7c0622
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4979956
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220813}

--- third_party/blink/renderer/core/css/style_engine_test.cc
+++ third_party/blink/renderer/core/css/style_engine_test.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/css/style_engine.h"
 
+#include <algorithm>
 #include <limits>
 #include <memory>
 
@@ -6961,6 +6962,50 @@ TEST_F(StyleEngineTest, UseCountCSSDeclarationAfterNestedRule) {
     </style>
   )HTML");
   EXPECT_TRUE(IsUseCounted(WebFeature::kCSSDeclarationAfterNestedRule));
+}
+
+TEST_F(StyleEngineTest, EnsureAppRegionTriggersRelayout) {
+  GetDocument().body()->setInnerHTML(R"HTML(
+    <head>
+    <style>
+      .drag {
+        app-region: drag
+      }
+      .no-drag {
+        app-region: no-drag
+      }
+    </style>
+    </head>
+    <body>
+       <div id="drag-region"></div>
+    </body>
+  )HTML");
+
+  Element* drag_element =
+      GetDocument().getElementById(AtomicString("drag-region"));
+
+  auto regions = GetDocument().AnnotatedRegions();
+  auto* it =
+      std::find_if(regions.begin(), regions.end(),
+                   [](blink::AnnotatedRegionValue s) { return s.draggable; });
+  EXPECT_EQ(it, regions.end()) << "There should be no drag regions";
+
+  drag_element->classList().Add(AtomicString("drag"));
+  UpdateAllLifecyclePhases();
+
+  regions = GetDocument().AnnotatedRegions();
+  it = std::find_if(regions.begin(), regions.end(),
+                    [](blink::AnnotatedRegionValue s) { return s.draggable; });
+  EXPECT_NE(it, regions.end()) << "There should be one drag region";
+
+  drag_element->classList().Add(AtomicString("no-drag"));
+  UpdateAllLifecyclePhases();
+
+  regions = GetDocument().AnnotatedRegions();
+  it = std::find_if(regions.begin(), regions.end(),
+                    [](blink::AnnotatedRegionValue s) { return s.draggable; });
+
+  EXPECT_EQ(it, regions.end()) << "There should be no drag regions";
 }
 
 }  // namespace blink
