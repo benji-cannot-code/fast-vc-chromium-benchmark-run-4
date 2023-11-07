816c5c166255f4164be99204511b0e6bf5053d43
sophiewen <sophiewen@chromium.org>
2023-11-07 03:16:59 +0000

    snap-groups: Fix double tap to swap windows
    Test: added
    Fixed: b/309006114
    Change-Id: I3b38458ce80d6552cae104aabedfc97432b3b4da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5008614
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220703}

--- ash/wm/splitview/split_view_divider_view.h
+++ ash/wm/splitview/split_view_divider_view.h
@@ -57,6 +57,8 @@ class SplitViewDividerView : public views::View,
   }
 
  private:
+  void SwapWindows();
+
   void OnResizeStatusChanged();
 
   // Called when the `kebab_button_` is pressed which toggles between showing or
