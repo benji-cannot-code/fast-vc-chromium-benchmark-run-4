ca6cd00d79a205a2aa0a355353bf213ca68e13a1
manukh <manukh@chromium.org>
2023-11-07 20:42:25 +0000

    [omnibox][ml][chrome://omnibox] Fix bugs & expand chart range
    - The main change is to allow zooming out on the chart and seeing more
      values. Previously, only -/+14 signals would be drawn. E.g. if the
      original typedVisits was 20, then it'd be graphed from 6 to 34. Even
      if you zoomed out, you'd see a tiny plot. With this CL, the range
      extends to fill the canvas. Before/after:
      https://screenshot.googleplex.com/6fye2qTuxaTta8x
    
    - Caches ML requests. Since we're now regenerating plots as the user
      pans/zooms, there's a lot of repeat requests.
    
    - Skips charting the full model signals; only charts the small model
      signals. The additional signals added latency with no benefit, since
      they would be hidden anyways. The upside is improved performance. The
      downside is an additional array of signals we have to keep consistent
      when the model changes. Also removes debouncing when calculating the
      charts; the improved performance made it unnecessary.
    
    - Before, the chart had 3 coordinate systems we were converting between:
      - the canvas where x=[0, 600px]
      - the zoom/pan where initially x=[0, 1] but would change as the user
        zooms/pans
      - and the signal range where x=[-14, 14]. Before this CL, this was
        constant. But with this CL, zooming/panning changed this too.
    This CL merges the [0, 1], and [-14, 14] coordinates.
    
    - Some other minor fixes to the chart.
    
    - Fixes the 'Copied!' confirmation not appearing.
    
    - Fixes a chart bug: when signals changed, the old graphs were supposed
      to be cleared. But if the user moved their mouse over the graphs, the
      old graphs would reappear.
    
    - Fixes a crash: if the debug page was opened at startup, it would crash
      because it would try to look up the value of an null optional returned
      by the ML model service.
    
    Bug: 1405555
    Change-Id: I9266ef294ce4e78440d09712a4f30989c79ae0ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5002376
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Angela Yoeurng <yoangela@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221163}

--- chrome/browser/resources/omnibox/ml/ml_browser_proxy.ts
+++ chrome/browser/resources/omnibox/ml/ml_browser_proxy.ts
@@ -22,6 +22,7 @@ export class MlBrowserProxy {
       OmniboxPageHandler.getRemote();
   private onResponseCallbacks: OnResponseCallback[] = [];
   private version: Promise<MlVersionObj>;
+  private readonly makeMlRequestCache: Record<string, Promise<number>> = {};
 
   constructor() {
     this.callbackRouter.handleNewAutocompleteResponse.addListener(
@@ -63,6 +64,8 @@ export class MlBrowserProxy {
   }
 
   makeMlRequest(signals: Signals): Promise<number> {
-    return this.handler.startMl(signals).then(({score}) => score);
+    const cacheKey = String(Object.values(signals));
+    return this.makeMlRequestCache[cacheKey] ||=
+               this.handler.startMl(signals).then(({score}) => score);
   }
 }
