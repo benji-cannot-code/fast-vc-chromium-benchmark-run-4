c7e084d5bb4d1da929092e9c43e8b3502bdcdfe7
Joemer Ramos <joemerramos@chromium.org>
2023-11-07 22:08:05 +0000

    [iOS] PasswordController Unittests IsRunningOniOS15OrLater Clean Up
    Clean up for IsRunningOniOS15OrLater since the minimum deployment
    target is now 15.0.
    
    Fixed: 1404697
    Bug: 1418068
    Change-Id: Ib5303763ce97d9b40e8d12da30e7eac9d113341a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5007018
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221208}

--- ios/chrome/browser/passwords/model/password_controller_unittest.mm
+++ ios/chrome/browser/passwords/model/password_controller_unittest.mm
@@ -1702,12 +1702,6 @@ TEST_F(PasswordControllerTest, CheckPasswordGenerationSuggestion) {
 // Tests that the user is prompted to save or update password on a succesful
 // form submission.
 TEST_F(PasswordControllerTest, ShowingSavingPromptOnSuccessfulSubmission) {
-  // TODO(crbug.com/1404697): Re-enable on iOS 14. This test is flaky on iOS 14,
-  // sometimes failing to finish loading the HTML.
-  if (!base::ios::IsRunningOnIOS15OrLater()) {
-    return;
-  }
-
   const char* kHtml = {"<html><body>"
                        "<form name='login_form' id='login_form'>"
                        "  <input type='text' name='username'>"
@@ -1773,12 +1767,6 @@ TEST_F(PasswordControllerTest, NotShowingSavingPromptWithoutSubmission) {
 // Tests that the user is not prompted to save or update password on a
 // succesful form submission while saving is disabled.
 TEST_F(PasswordControllerTest, NotShowingSavingPromptWhileSavingIsDisabled) {
-  // TODO(crbug.com/1404697): Re-enable on iOS 14. This test is flaky on iOS 14,
-  // sometimes failing to finish loading the HTML.
-  if (!base::ios::IsRunningOnIOS15OrLater()) {
-    return;
-  }
-
   const char* kHtml = {"<html><body>"
                        "<form name='login_form' id='login_form'>"
                        "  <input type='text' name='username'>"
@@ -1806,12 +1794,6 @@ TEST_F(PasswordControllerTest, NotShowingSavingPromptWhileSavingIsDisabled) {
 // form submission when there's already a credential with the same
 // username in the store.
 TEST_F(PasswordControllerTest, ShowingUpdatePromptOnSuccessfulSubmission) {
-  // TODO(crbug.com/1404697): Re-enable on iOS 14. This test is flaky on iOS 14,
-  // sometimes failing to finish loading the HTML.
-  if (!base::ios::IsRunningOnIOS15OrLater()) {
-    return;
-  }
-
   PasswordForm form(MakeSimpleForm());
   ON_CALL(*store_, GetLogins)
       .WillByDefault(WithArg<1>(InvokeConsumer(store_.get(), form)));
@@ -2205,12 +2187,6 @@ TEST_F(PasswordControllerTest,
 }
 
 TEST_F(PasswordControllerTest, PasswordMetricsNoSavedCredentials) {
-  // TODO(crbug.com/1404697): Re-enable on iOS 14. This test is flaky on iOS 14,
-  // sometimes failing to finish loading the HTML.
-  if (!base::ios::IsRunningOnIOS15OrLater()) {
-    return;
-  }
-
   base::HistogramTester histogram_tester;
   {
     ON_CALL(*store_, GetLogins)
