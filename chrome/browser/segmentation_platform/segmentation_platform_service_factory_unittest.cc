e337e3f7e01eb288c3cca5929fda90ab721042be
ssid <ssid@chromium.org>
2023-11-07 18:24:39 +0000

    [segmentation] Add cache to the client result prefs
    There are too many reads and writes on the result prefs at startup.
    Avoids reading and parsing prefs by using cache for reads.
    
    Bug: b/299529800
    Change-Id: I56842b769b597473f7ae929b1ae5c7bfa056feb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5002623
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221039}

--- chrome/browser/segmentation_platform/segmentation_platform_service_factory_unittest.cc
+++ chrome/browser/segmentation_platform/segmentation_platform_service_factory_unittest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/segmentation_platform/segmentation_platform_service_factory.h"
 
-#include "base/files/scoped_temp_dir.h"
 #include "base/functional/bind.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/test/scoped_command_line.h"
@@ -26,14 +25,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/segmentation_platform/public/features.h"
 #include "components/segmentation_platform/public/prediction_options.h"
 #include "components/segmentation_platform/public/result.h"
-#include "components/segmentation_platform/public/segment_selection_result.h"
 #include "components/segmentation_platform/public/segmentation_platform_service.h"
 #include "components/segmentation_platform/public/service_proxy.h"
 #include "components/ukm/test_ukm_recorder.h"
 #include "content/public/test/browser_task_environment.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "testing/platform_test.h"
 
 namespace segmentation_platform {
 namespace {
@@ -192,8 +189,8 @@ class SegmentationPlatformServiceFactoryTest : public testing::Test {
     PrefService* pref_service_ = testing_profile_->GetPrefs();
     std::unique_ptr<ClientResultPrefs> result_prefs_ =
         std::make_unique<ClientResultPrefs>(pref_service_);
-    return result_prefs_->ReadClientResultFromPrefs(segmentation_key)
-        .has_value();
+    return result_prefs_->ReadClientResultFromPrefs(segmentation_key) !=
+           nullptr;
   }
 
   void OnClientResultPrefUpdated(const std::string& segmentation_key) {
