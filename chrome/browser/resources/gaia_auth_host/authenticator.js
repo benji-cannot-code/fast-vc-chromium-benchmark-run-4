4940c5cf3af081bd508520257a47ccadc438d3bf
Kush Sinha <sinhak@chromium.org>
2023-11-08 10:48:26 +0000

    Clean up 'contentWindow is not available at this time' error message
    Clean up the TODO to remove the error message from authenticator.js
    webview. It was done in https://codereview.chromium.org/1086643002
    
    Bug: crbug/469522
    Change-Id: I6ad0ad3ae36e49a8af54cfeab110ac3da6ea2ae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5007605
    Auto-Submit: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221484}

--- chrome/browser/resources/gaia_auth_host/authenticator.js
+++ chrome/browser/resources/gaia_auth_host/authenticator.js
@@ -1455,9 +1455,6 @@ export class Authenticator extends EventTarget {
    * @private
    */
   isWebviewEvent_(e) {
-    // Note: <webview> prints error message to console if |contentWindow| is
-    // not defined.
-    // TODO(dzhioev): remove the message. http://crbug.com/469522
     const webviewWindow = this.webview_.contentWindow;
     return !!webviewWindow && webviewWindow === e.source;
   }
