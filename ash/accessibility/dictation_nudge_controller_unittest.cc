120461e52a30b47deb4ad97fed3c311889a665e4
Kevin Radtke <kradtke@chromium.org>
2023-11-08 20:28:19 +0000

    nudge-migration: Migrate dictation nudge to new System Nudge framework
    Use AnchoredNudgeManager to create the dictation nudge and update tests
    to match this new behavior. Guard this new code under a flag
    `IsSystemNudgeMigrationEnabled`.
    
    Migration proposal in https://goto.google.com/system-nudge-v2.
    
    screenshot: https://screenshot.googleplex.com/7Bwn9JSSYT75Lkg
    
    With the new updated design, nudges support larger illustrations instead
    of smaller vector icons. For this first iteration the nudge was updated
    to be text-only - an image can be added in a subsequent CL.
    
    Bug: b:308674967
    Change-Id: Ia10b06872659130b23a75789edc3a4f2607348d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5005465
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221841}

--- ash/accessibility/dictation_nudge_controller_unittest.cc
+++ ash/accessibility/dictation_nudge_controller_unittest.cc
@@ -8,11 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/accessibility/accessibility_controller_impl.h"
 #include "ash/accessibility/dictation_nudge.h"
 #include "ash/accessibility/dictation_nudge_controller.h"
+#include "ash/constants/ash_features.h"
 #include "ash/shell.h"
+#include "ash/system/toast/anchored_nudge_manager_impl.h"
 #include "ash/system/tray/system_nudge_label.h"
 #include "ash/test/ash_test_base.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_feature_list.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/layer_animator.h"
@@ -25,8 +28,16 @@ using ::testing::HasSubstr;
 
 namespace ash {
 
+namespace {
+
+constexpr char kDictationLanguageUpgradedNudgeId[] =
+    "dictation_language_upgraded.nudge_id";
+
+}  // namespace
+
 // Tests for showing the Dictation Nudge from AccessibilityControllerImpl.
-class DictationNudgeControllerTest : public AshTestBase {
+class DictationNudgeControllerTest : public AshTestBase,
+                                     public testing::WithParamInterface<bool> {
  public:
   DictationNudgeControllerTest() = default;
   DictationNudgeControllerTest(const DictationNudgeControllerTest&) = delete;
@@ -36,6 +47,8 @@ class DictationNudgeControllerTest : public AshTestBase {
 
   // AshTestBase:
   void SetUp() override {
+    scoped_feature_list_.InitWithFeatureState(features::kSystemNudgeMigration,
+                                              IsSystemNudgeMigrationEnabled());
     AshTestBase::SetUp();
     Shell::Get()->accessibility_controller()->dictation().SetEnabled(true);
   }
@@ -77,9 +90,30 @@ class DictationNudgeControllerTest : public AshTestBase {
 
     widget_destroyed_waiter.Wait();
   }
+
+  bool IsSystemNudgeMigrationEnabled() { return GetParam(); }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
-TEST_F(DictationNudgeControllerTest, ShowsAndHidesNudge) {
+INSTANTIATE_TEST_SUITE_P(All,
+                         DictationNudgeControllerTest,
+                         /*enable_system_nudge_migration=*/testing::Bool());
+
+TEST_P(DictationNudgeControllerTest, ShowsAndHidesNudge) {
+  if (IsSystemNudgeMigrationEnabled()) {
+    EXPECT_FALSE(AnchoredNudgeManager::Get()->IsNudgeShown(
+        kDictationLanguageUpgradedNudgeId));
+    ShowDictationLanguageUpgradedNudge("en-US", "en-US");
+    EXPECT_TRUE(AnchoredNudgeManager::Get()->IsNudgeShown(
+        kDictationLanguageUpgradedNudgeId));
+
+    // Manager handled nudge's destruction is tested in
+    // anchored_nudge_manager_impl_unittest.cc.
+    return;
+  }
+
   EXPECT_FALSE(GetDictationNudgeController());
 
   ShowDictationLanguageUpgradedNudge("en-US", "en-US");
@@ -93,7 +127,7 @@ TEST_F(DictationNudgeControllerTest, ShowsAndHidesNudge) {
   WaitForWidgetDestruction(controller, nudge);
 }
 
-TEST_F(DictationNudgeControllerTest, SetsLabelBasedOnApplicationLocale) {
+TEST_P(DictationNudgeControllerTest, SetsLabelBasedOnApplicationLocale) {
   struct {
     std::string locale;
     std::string application_locale;
@@ -107,19 +141,28 @@ TEST_F(DictationNudgeControllerTest, SetsLabelBasedOnApplicationLocale) {
   for (const auto& testcase : kTestCases) {
     ShowDictationLanguageUpgradedNudge(testcase.locale,
                                        testcase.application_locale);
+    if (IsSystemNudgeMigrationEnabled()) {
+      ASSERT_TRUE(AnchoredNudgeManager::Get()->IsNudgeShown(
+          kDictationLanguageUpgradedNudgeId));
 
-    DictationNudgeController* controller = GetDictationNudgeController();
-    ASSERT_TRUE(controller);
+      const std::string body_text = base::UTF16ToUTF8(
+          Shell::Get()->anchored_nudge_manager()->GetNudgeBodyTextForTest(
+              kDictationLanguageUpgradedNudgeId));
+      EXPECT_THAT(body_text, HasSubstr(testcase.label));
+    } else {
+      DictationNudgeController* controller = GetDictationNudgeController();
+      ASSERT_TRUE(controller);
 
-    DictationNudge* nudge =
-        static_cast<DictationNudge*>(controller->GetSystemNudgeForTesting());
-    ASSERT_TRUE(nudge);
+      DictationNudge* nudge =
+          static_cast<DictationNudge*>(controller->GetSystemNudgeForTesting());
+      ASSERT_TRUE(nudge);
 
-    std::unique_ptr<SystemNudgeLabel> label = GetDictationNudgeLabel(nudge);
-    std::string text = base::UTF16ToUTF8(label->GetText());
-    EXPECT_THAT(text, HasSubstr(testcase.label));
+      std::unique_ptr<SystemNudgeLabel> label = GetDictationNudgeLabel(nudge);
+      std::string text = base::UTF16ToUTF8(label->GetText());
+      EXPECT_THAT(text, HasSubstr(testcase.label));
 
-    WaitForWidgetDestruction(controller, nudge);
+      WaitForWidgetDestruction(controller, nudge);
+    }
   }
 }
 
