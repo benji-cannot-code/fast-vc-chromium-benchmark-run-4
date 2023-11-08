5d1fc130c72549a01c6508b856b49bfa8687451a
Sylvain Defresne <sdefresne@chromium.org>
2023-11-08 14:55:33 +0000

    [ios] Convert absl::optional<T> to std::optional<T>
    Since https://crrev.com/c/4469528, absl::optional<T> is an alias for
    std::optional<T>, so convert uses of the old type to the new one as
    it is easier to use (no needs to have public_deps on absl, shorter
    path to the header, ...).
    
    This CL converts all uses in //ios/chrome/tools and below.
    
    Bug: none
    Change-Id: I6a78af30e66ce60c89e2c292aaf289b7db437e28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5010714
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1221607}

--- ios/chrome/tools/strings/generate_localizable_strings.mm
+++ ios/chrome/tools/strings/generate_localizable_strings.mm
@@ -73,8 +73,7 @@ std::unique_ptr<ui::DataPack> LoadResourceDataPack(
 // Return nil if none is found.
 NSString* GetStringFromDataPack(const ui::DataPack& data_pack,
                                 uint16_t resource_id) {
-  absl::optional<base::StringPiece> data =
-      data_pack.GetStringPiece(resource_id);
+  std::optional<base::StringPiece> data = data_pack.GetStringPiece(resource_id);
   if (!data.has_value()) {
     return nil;
   }
@@ -149,7 +148,7 @@ NSDictionary* LoadResourcesListFromHeaders(NSArray* header_list,
         [root_header_dir stringByAppendingPathComponent:header]));
   }
 
-  absl::optional<ResourceMap> resource_map =
+  std::optional<ResourceMap> resource_map =
       LoadResourcesFromGritHeaders(headers);
   if (!resource_map) {
     return nil;
