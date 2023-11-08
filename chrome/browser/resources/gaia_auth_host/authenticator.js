4440caa03b96365ae7cedaff31f89a9fe74b3886
Kush Sinha <sinhak@chromium.org>
2023-11-08 10:52:04 +0000

    Authenticator.js: Fix docs for `isGaiaMessage_()`.
    The documentation for `isGaiaMessage_()` it outdated / incorrect. Fix
    the documentation to reflect that the method checks if a given HTML5
    message was received from Gaia.
    
    Change-Id: Iafab5375f2a4f6770eef0f3c64758a2724c23431
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5010053
    Auto-Submit: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221487}

--- chrome/browser/resources/gaia_auth_host/authenticator.js
+++ chrome/browser/resources/gaia_auth_host/authenticator.js
@@ -1022,7 +1022,8 @@ export class Authenticator extends EventTarget {
   }
 
   /**
-   * Returns true if given HTML5 message is received from the webview element.
+   * Returns true if given HTML5 message is received from `this.idpOrigin_` -
+   * which is usually Gaia.
    * @param {Object} e Payload of the received HTML5 message.
    */
   isGaiaMessage_(e) {
@@ -1030,7 +1031,8 @@ export class Authenticator extends EventTarget {
       return false;
     }
 
-    // The event origin does not have a trailing slash.
+    // The event origin does not have a trailing slash, while `idpOrigin_` does.
+    // Strip the trailing slash from `idpOrigin_` before comparison.
     if (e.origin !== this.idpOrigin_.substring(0, this.idpOrigin_.length - 1)) {
       return false;
     }
