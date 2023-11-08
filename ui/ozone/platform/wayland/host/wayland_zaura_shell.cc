29c56b1ffc12d8a8bf56dd5c0c529e90c9772f7a
Sammie Quon <sammiequon@chromium.org>
2023-11-08 22:36:20 +0000

    [reland] lacros/client: Fix issue with hidden caption buttons for snap+overview
    Previously, to hide caption buttons in overview we introduced a
    overview state to send to lacros side. However, there is at least one
    case where the server is in overview, but the client window is not.
    
    Therefore, we have to add overview state as a top level window
    property.
    
    Client CL: CL:4913288.
    
    This previously failed it was landed before the client side CL for
    version 61 which added some listeners. See https://crbug.com/1494408
    for more details.
    
    Test: manual
    Bug: 1467799
    Change-Id: Ie9017e8c31c50e47bffa0847cd5e89e9106d4d87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5010644
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221922}

--- ui/ozone/platform/wayland/host/wayland_zaura_shell.cc
+++ ui/ozone/platform/wayland/host/wayland_zaura_shell.cc
@@ -27,7 +27,7 @@ namespace ui {
 namespace {
 
 constexpr uint32_t kMinVersion = 1;
-constexpr uint32_t kMaxVersion = 61;
+constexpr uint32_t kMaxVersion = 62;
 
 }  // namespace
 
@@ -179,27 +179,11 @@ void WaylandZAuraShell::OnActivated(void* data,
 // static
 void WaylandZAuraShell::OnSetOverviewMode(void* data,
                                           struct zaura_shell* zaura_shell) {
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-  auto* self = static_cast<WaylandZAuraShell*>(data);
-  for (auto* window : self->connection_->window_manager()->GetAllWindows()) {
-    if (auto* toplevel_window = window->AsWaylandToplevelWindow()) {
-      toplevel_window->OnOverviewModeChanged(true);
-    }
-  }
-#endif
 }
 
 // static
 void WaylandZAuraShell::OnUnsetOverviewMode(void* data,
                                             struct zaura_shell* zaura_shell) {
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-  auto* self = static_cast<WaylandZAuraShell*>(data);
-  for (auto* window : self->connection_->window_manager()->GetAllWindows()) {
-    if (auto* toplevel_window = window->AsWaylandToplevelWindow()) {
-      toplevel_window->OnOverviewModeChanged(false);
-    }
-  }
-#endif
 }
 
 // static
