13ba8a9771ed99ee38e43cce0c4795205408e22f
Christoph Schwering <schwering@google.com>
2023-11-08 21:16:38 +0000

    [Autofill] Eliminate AutofillField::FormControlType()
    This CL replaces references to AutofillField::FormControlType() with
    FormFieldData::form_control_type.
    
    One important difference between AutofillField::FormControlType() and
    FormFieldData::form_control_type is that the former lumps all text-state
    form control types into one value `kText`. This corresponds to
    FormFieldData::IsTextElement().
    
    This CL changes the values of the Autofill2.FieldInfo.FormControlType
    UKM metric.
    
    Bug: 1482526, 1484810
    Change-Id: I0cbcd05680f20333562d2adb7089890525f3942e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4912478
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221883}

--- components/autofill/core/browser/metrics/autofill_metrics.cc
+++ components/autofill/core/browser/metrics/autofill_metrics.cc
@@ -2532,7 +2532,6 @@ void AutofillMetrics::FormInteractionsUkmLogger::
       .SetFieldSessionIdentifier(
           AutofillMetrics::FieldGlobalIdToHash64Bit(field.global_id()))
       .SetFieldSignature(HashFieldSignature(field.GetFieldSignature()))
-      .SetFormControlType(base::to_underlying(field.FormControlType()))
       .SetFormControlType2(base::to_underlying(field.form_control_type))
       .SetAutocompleteState(base::to_underlying(autocomplete_state));
 
