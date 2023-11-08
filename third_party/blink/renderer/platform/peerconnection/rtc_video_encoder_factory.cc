9ff2742ccac60f9fe415ad8443b4a9de70ca8b20
Martin Aedla <maedla@microsoft.com>
2023-11-08 09:37:36 +0000

    Fix for MF VEA creating encoder for wrong codec when H.264 CBP encoding is enabled
    Media Foundation VEA has separate profiles for portrait mode which causes duplicate profiles to be added. cricket::AddH264ConstrainedBaselineProfileToSupportedFormats only adds unique SDP formats, which leads SDP format and VEA profile lists to go out of sync.
    This fix ensures that SDP formats are internally unique.
    
    Bug: 1498365
    Change-Id: I0a5daf0c24d0fd123da3889537864fb63bb4272d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994452
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Auto-Submit: Martin Aedla <maedla@microsoft.com>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Chunbo Hua <chunbo.hua@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1221453}

--- third_party/blink/renderer/platform/peerconnection/rtc_video_encoder_factory.cc
+++ third_party/blink/renderer/platform/peerconnection/rtc_video_encoder_factory.cc
@@ -195,6 +195,10 @@ SupportedFormats GetSupportedFormatsInternal(
 
     absl::optional<webrtc::SdpVideoFormat> format = VEAToWebRTCFormat(profile);
     if (format) {
+      if (format->IsCodecInList(supported_formats.sdp_formats)) {
+        continue;
+      }
+
       supported_formats.profiles.push_back(profile.profile);
       supported_formats.scalability_modes.push_back(profile.scalability_modes);
       supported_formats.sdp_formats.push_back(std::move(*format));
@@ -218,6 +222,11 @@ SupportedFormats GetSupportedFormatsInternal(
 #endif
     }
   }
+
+  DCHECK_EQ(supported_formats.profiles.size(),
+            supported_formats.sdp_formats.size());
+  DCHECK_EQ(supported_formats.profiles.size(),
+            supported_formats.scalability_modes.size());
 
   return supported_formats;
 }
