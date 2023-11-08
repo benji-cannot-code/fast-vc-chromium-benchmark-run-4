e8ae5732575418042f2dd261e3b8ec84cb41c6fc
Josh Berenhaus <joshber@meta.com>
2023-11-08 00:57:33 +0000

    Fixing clang-format lint error in startup_helper.cc
    Change-Id: I09e5a648662b9b2897e4220f2e24c522771beda1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5008657
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Auto-Submit: Josh Berenhaus <joshber@meta.com>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221315}

--- content/browser/startup_helper.cc
+++ content/browser/startup_helper.cc
@@ -25,8 +25,10 @@ namespace content {
 
 std::unique_ptr<base::FieldTrialList> SetUpFieldTrialsAndFeatureList() {
   std::unique_ptr<base::FieldTrialList> field_trial_list;
-  if (!base::FieldTrialList::GetInstance())
+  if (!base::FieldTrialList::GetInstance()) {
     field_trial_list = std::make_unique<base::FieldTrialList>();
+  }
+
   const base::CommandLine* command_line =
       base::CommandLine::ForCurrentProcess();
 
