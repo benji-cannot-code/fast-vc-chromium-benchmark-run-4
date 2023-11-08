5a32471bc8016753e28a20723b2d8ca8599965bb
Matt Mueller <mattm@chromium.org>
2023-11-08 20:56:28 +0000

    remove CreateEmptySystemTrustStore, move DummySystemTrustStore to cert_verify_tool
    cert_verify_tool is the only thing using CreateEmptySystemTrustStore
    and DummySystemTrustStore now, so just remove that and move the
    definition of DummySystemTrustStore into cert_verify_tool.
    
    Change-Id: Ib1ad6a59346334bb7aba38d8211727e09b58fff8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5011678
    Reviewed-by: Hubert Chao <hchao@google.com>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221868}

--- net/cert/internal/system_trust_store.cc
+++ net/cert/internal/system_trust_store.cc
@@ -53,26 +53,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
-namespace {
-
-class DummySystemTrustStore : public SystemTrustStore {
- public:
-  bssl::TrustStore* GetTrustStore() override { return &trust_store_; }
-
-  bool IsKnownRoot(const bssl::ParsedCertificate* trust_anchor) const override {
-    return false;
-  }
-
-#if BUILDFLAG(CHROME_ROOT_STORE_SUPPORTED)
-  int64_t chrome_root_store_version() const override { return 0; }
-#endif
-
- private:
-  bssl::TrustStoreCollection trust_store_;
-};
-
-}  // namespace
-
 #if BUILDFLAG(CHROME_ROOT_STORE_SUPPORTED)
 class SystemTrustStoreChromeWithUnOwnedSystemStore : public SystemTrustStore {
  public:
@@ -312,9 +292,5 @@ void InitializeTrustStoreAndroid() {}
 #endif  // CHROME_ROOT_STORE_SUPPORTED
 
 #endif
-
-std::unique_ptr<SystemTrustStore> CreateEmptySystemTrustStore() {
-  return std::make_unique<DummySystemTrustStore>();
-}
 
 }  // namespace net
