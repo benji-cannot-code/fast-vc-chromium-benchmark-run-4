d436a04a4b117934161a7e01d6fe9bbdcd32bf07
Florian Leimgruber <fleimgruber@google.com>
2023-11-08 13:21:21 +0000

    Use DROP TABLE IF EXISTS in AutofillTable
    The code is inconsistent and drops tables in three different ways:
    1. Just DROP TABLE x
    2. DROP TABLE IF EXISTS x
    3. !DoesTableExist(x) || DROP TABLE x
    
    As of this CL, DROP TABLE IF EXISTS is used everywhere. In the cases
    that currently use pattern 1, this is a behavior change. However,
    since the state of the table is out of our control (a user might
    manually drop it), this is a desirable change.
    
    Bug: 1007974
    Change-Id: I0de0e9965983d5d7c1c678b006eb195427180b69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5007829
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1221555}

--- components/autofill/core/browser/webdata/autofill_table.cc
+++ components/autofill/core/browser/webdata/autofill_table.cc
@@ -531,9 +531,11 @@ bool DropColumn(sql::Database* db,
   ;
 }
 
-// Drops `table_name` and returns true if successful.
-bool DropTable(sql::Database* db, std::string_view table_name) {
-  return db->Execute(base::StrCat({"DROP TABLE ", table_name}).c_str());
+// Drops `table_name`, if the table exists. Returns true if the statement
+// finishes successfully, independently of whether a table was actually dropped.
+bool DropTableIfExists(sql::Database* db, std::string_view table_name) {
+  return db->Execute(
+      base::StrCat({"DROP TABLE IF EXISTS ", table_name}).c_str());
 }
 
 // Initializes `statement` with DELETE FROM `table_name`. A WHERE clause
@@ -3493,7 +3495,7 @@ bool AutofillTable::MigrateToVersion98RemoveStatusColumnMaskedCreditCards() {
 }
 
 bool AutofillTable::MigrateToVersion99RemoveAutofillProfilesTrashTable() {
-  return DropTable(db_, "autofill_profiles_trash");
+  return DropTableIfExists(db_, "autofill_profiles_trash");
 }
 
 bool AutofillTable::MigrateToVersion100RemoveProfileValidityBitfieldColumn() {
@@ -3506,7 +3508,7 @@ bool AutofillTable::MigrateToVersion100RemoveProfileValidityBitfieldColumn() {
 }
 
 bool AutofillTable::MigrateToVersion101RemoveCreditCardArtImageTable() {
-  return db_->Execute("DROP TABLE IF EXISTS credit_card_art_images");
+  return DropTableIfExists(db_, "credit_card_art_images");
 }
 
 bool AutofillTable::MigrateToVersion102AddAutofillBirthdatesTable() {
@@ -3545,7 +3547,7 @@ bool AutofillTable::MigrateToVersion105AddAutofillIbanTable() {
 
 bool AutofillTable::MigrateToVersion106RecreateAutofillIbanTable() {
   sql::Transaction transaction(db_);
-  return transaction.Begin() && DropTable(db_, kIbansTable) &&
+  return transaction.Begin() && DropTableIfExists(db_, kIbansTable) &&
          CreateTable(db_, kIbansTable,
                      {{kGuid, "VARCHAR PRIMARY KEY"},
                       {kUseCount, "INTEGER NOT NULL DEFAULT 0"},
@@ -3656,8 +3658,7 @@ bool AutofillTable::MigrateToVersion114DropLegacyAddressTables() {
        {kAutofillProfilesTable, kAutofillProfileAddressesTable,
         kAutofillProfileNamesTable, kAutofillProfileEmailsTable,
         kAutofillProfilePhonesTable, kAutofillProfileBirthdatesTable}) {
-    success = success && (!db_->DoesTableExist(deprecated_table) ||
-                          DropTable(db_, deprecated_table));
+    success = success && DropTableIfExists(db_, deprecated_table);
   }
   return success && transaction.Commit();
 }
@@ -3725,9 +3726,7 @@ bool AutofillTable::MigrateToVersion117AddProfileObservationColumn() {
 
 bool AutofillTable::MigrateToVersion118RemovePaymentsUpiVpaTable() {
   sql::Transaction transaction(db_);
-  return transaction.Begin() &&
-         (!db_->DoesTableExist(kPaymentsUpiVpaTable) ||
-          DropTable(db_, kPaymentsUpiVpaTable)) &&
+  return transaction.Begin() && DropTableIfExists(db_, kPaymentsUpiVpaTable) &&
          transaction.Commit();
 }
 
