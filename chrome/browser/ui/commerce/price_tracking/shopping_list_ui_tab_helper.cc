812b5ce58783a2ad9f6c45ed8f669cff4e97dd77
Matt Jones <mdjones@chromium.org>
2023-11-08 21:10:54 +0000

    [SFL] Correct inverted icon histogram reporting
    Bug: 1498616
    Change-Id: I1c1dbd70da5b2b32ca02bedabdc260d92e5b2dda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5014761
    Auto-Submit: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221879}

--- chrome/browser/ui/commerce/price_tracking/shopping_list_ui_tab_helper.cc
+++ chrome/browser/ui/commerce/price_tracking/shopping_list_ui_tab_helper.cc
@@ -805,7 +805,7 @@ void ShoppingListUiTabHelper::RecordPriceTrackingIconMetrics(
   // interaction was to track a product, otherwise we would have been blocked
   // above.
   if (from_icon_use) {
-    if (price_tracking_expanded) {
+    if (!price_tracking_expanded) {
       base::UmaHistogramEnumeration(histogram_name,
                                     PageActionIconInteractionState::kClicked);
     } else {
@@ -814,7 +814,7 @@ void ShoppingListUiTabHelper::RecordPriceTrackingIconMetrics(
     }
 
   } else {
-    if (price_tracking_expanded) {
+    if (!price_tracking_expanded) {
       base::UmaHistogramEnumeration(
           histogram_name, PageActionIconInteractionState::kNotClicked);
     } else {
