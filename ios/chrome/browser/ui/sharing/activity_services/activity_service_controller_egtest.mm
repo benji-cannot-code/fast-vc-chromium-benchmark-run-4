907e28dbf7eb7433ee1f4cabc9a74facdf43277f
Joemer Ramos <joemerramos@chromium.org>
2023-11-08 15:19:38 +0000

    [iOS] ActivityServiceController EG Test iOS 15 Available Guard Clean Up
    Clean up for iOS 15 available guards since the minimum deployment
    target is now 15.0.
    
    Bug: 1418068
    Change-Id: Ic466f35d63b4260db0a74b666150d3468d934e96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5007460
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221618}

--- ios/chrome/browser/ui/sharing/activity_services/activity_service_controller_egtest.mm
+++ ios/chrome/browser/ui/sharing/activity_services/activity_service_controller_egtest.mm
@@ -92,12 +92,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Verifies that Tools Menu > Share Chrome brings up the "share sheet".
 - (void)testShareChromeApp {
-  if (@available(iOS 15.0, *)) {
-    [ChromeEarlGreyUI openToolsMenu];
-    [ChromeEarlGreyUI
-        tapToolsMenuAction:grey_accessibilityID(kToolsMenuShareChromeId)];
-    [ChromeEarlGrey verifyActivitySheetVisible];
-  }
+  [ChromeEarlGreyUI openToolsMenu];
+  [ChromeEarlGreyUI
+      tapToolsMenuAction:grey_accessibilityID(kToolsMenuShareChromeId)];
+  [ChromeEarlGrey verifyActivitySheetVisible];
 }
 
 @end
