38daf40ef32e9651ae18c9b156b412d73c9edd3b
Kent Tamura <tkent@chromium.org>
2023-11-08 23:00:36 +0000

    field-sizing: Do not apply to autofilled controls
    Bug: 1447058
    Change-Id: If221a197fd913a83fd528452eb468a4de9965d24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5004358
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221938}

--- third_party/blink/renderer/core/layout/ng/ng_block_node.cc
+++ third_party/blink/renderer/core/layout/ng/ng_block_node.cc
@@ -294,7 +294,7 @@ absl::optional<LayoutUnit> ContentMinimumInlineSize(
   else
     inline_size += border_padding.InlineSum();
 
-  const bool apply_form_sizing = style.ApplyControlFixedSize();
+  const bool apply_form_sizing = style.ApplyControlFixedSize(node);
   if (block_node.IsTextControl() && apply_form_sizing) {
     return inline_size;
   }
