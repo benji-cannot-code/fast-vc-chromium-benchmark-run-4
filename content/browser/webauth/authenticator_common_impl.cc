c77990801a0f47588e8f92e1f9459c8d9063253b
Ken Buchanan <kenrb@google.com>
2023-11-08 20:31:11 +0000

    [WebAuthn] Re-enable cross-domain tests
    These tests were disabled because they were failing due to request
    timeouts being hit on Mac CI bots.
    
    I'm speculating that FakeNetwork is slow on Mac, causing the requests to
    take more than one second to complete. This bumps the request timeouts
    to 10 seconds to see if that resolves the failures.
    
    The failures don't reproduce locally.
    
    Bug: 1500554, 1498289
    Change-Id: I2eae41295df1b68d05f4442327f2b0aa637bf0bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5014545
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221846}

--- content/browser/webauth/authenticator_common_impl.cc
+++ content/browser/webauth/authenticator_common_impl.cc
@@ -1965,7 +1965,6 @@ void AuthenticatorCommonImpl::OnTimeout() {
     return;
   }
 
-  DCHECK(req_state_->request_delegate);
   if (req_state_->get_assertion_response_callback) {
     req_state_->get_assertion_result = GetAssertionResult::kTimeout;
   }
