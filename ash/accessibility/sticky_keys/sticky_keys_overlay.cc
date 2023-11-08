ef967692e87a8bc97582be3db9018e75c2011b08
Allen Bauer <kylixrd@chromium.org>
2023-11-08 18:24:14 +0000

    Add metadata to Views classes
    This CL was uploaded by git cl split.
    
    R=anastasi@google.com
    
    Bug: 1485768
    Change-Id: Id394115f467cde186a840c4d46f10b4b577062af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5014286
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Anastasia Helfinstein <anastasi@google.com>
    Auto-Submit: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1221743}

--- ash/accessibility/sticky_keys/sticky_keys_overlay.cc
+++ ash/accessibility/sticky_keys/sticky_keys_overlay.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
@@ -48,6 +50,8 @@ const int kSlideAnimationDurationMs = 100;
 ///////////////////////////////////////////////////////////////////////////////
 //  StickyKeyOverlayLabel
 class StickyKeyOverlayLabel : public views::Label {
+  METADATA_HEADER(StickyKeyOverlayLabel, views::Label)
+
  public:
   explicit StickyKeyOverlayLabel(const std::string& key_name);
 
@@ -100,11 +104,16 @@ void StickyKeyOverlayLabel::SetKeyState(StickyKeyState state) {
   SetFontList(font_list().DeriveWithStyle(style));
 }
 
+BEGIN_METADATA(StickyKeyOverlayLabel)
+END_METADATA
+
 }  // namespace
 
 ///////////////////////////////////////////////////////////////////////////////
 //  StickyKeysOverlayView
 class StickyKeysOverlayView : public views::View {
+  METADATA_HEADER(StickyKeysOverlayView, views::View)
+
  public:
   StickyKeysOverlayView();
 
@@ -200,6 +209,9 @@ void StickyKeysOverlayView::AddKeyLabel(ui::EventFlags modifier,
   AddChildView(label);
   modifier_label_map_[modifier] = label;
 }
+
+BEGIN_METADATA(StickyKeysOverlayView)
+END_METADATA
 
 ///////////////////////////////////////////////////////////////////////////////
 //  StickyKeysOverlay
