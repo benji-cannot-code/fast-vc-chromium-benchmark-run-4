354c583d116b384b6226baa1a1de14e214439b08
Anthony Cui <cuianthony@google.com>
2023-11-08 20:09:05 +0000

    Remove Config static initializer from Compose config file
    Static intializer causes any change that attempts to call GetComposeConfig() to fail the CQ check_static_initializers test and is blocking https://crrev.com/c/5005525
    This was unintentionally added in the initial CL and can be removed.
    
    Bug: b:309806534
    Change-Id: Id7437a134a7086c6cac77f1cc360e18e7cefaa6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5010385
    Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    Commit-Queue: Anthony Cui <cuianthony@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221826}

--- components/compose/core/browser/config.cc
+++ components/compose/core/browser/config.cc
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace compose {
 namespace {
-Config g_config;
-
 // Override any parameters that may be provided by Finch.
 void OverrideFieldTrialParams(Config& config) {
   config.input_min_words = base::GetFieldTrialParamByFeatureAsInt(
