8d0096a0a79be9c63ec242c3a5475fc291220a25
Koji Ishii <kojii@chromium.org>
2023-11-08 17:23:35 +0000

    [balance-text] Disable when there is an overflow
    This patch disables the bisect line breaker when there is an
    overflow, even if the `overflow-wrap` property is set.
    
    Fixed: 1488018
    Change-Id: Ice3be17a0fe9142f32a09ac2c02d3d9e9e917131
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5003922
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221702}

--- third_party/blink/renderer/core/layout/inline/line_breaker.cc
+++ third_party/blink/renderer/core/layout/inline/line_breaker.cc
@@ -710,6 +710,7 @@ void LineBreaker::PrepareNextLine(LineInfo* line_info) {
   override_break_anywhere_ = false;
   disable_phrase_ = false;
   disable_score_line_break_ = false;
+  disable_bisect_line_break_ = false;
   if (!current_style_)
     SetCurrentStyle(line_info->LineStyle());
   ComputeBaseDirection();
@@ -3222,6 +3223,8 @@ void LineBreaker::HandleOverflow(LineInfo* line_info) {
   //   css2.1/t1601-c547-indent-01-d.html
   //   virtual/text-antialias/international/bdi-neutral-wrapped.html
   disable_score_line_break_ = true;
+  // The bisect line breaker doesn't support overflowing content.
+  disable_bisect_line_break_ = true;
 
   // Restore the hyphenation states to before the loop if needed.
   DCHECK(!HasHyphen());
@@ -3251,6 +3254,8 @@ void LineBreaker::RetryAfterOverflow(LineInfo* line_info,
                                      InlineItemResults* item_results) {
   // `ScoreLineBreaker` doesn't support multi-pass line breaking.
   disable_score_line_break_ = true;
+  // The bisect line breaker doesn't support multi-pass line breaking.
+  disable_bisect_line_break_ = true;
 
   state_ = LineBreakState::kContinue;
 
