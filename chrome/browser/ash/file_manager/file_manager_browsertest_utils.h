4f7e49733a0ce14f1f92cd6168da6ec453147086
Yuki Awano <yawano@google.com>
2023-11-08 18:33:25 +0000

    Enable Google One offer banner by default
    - Refer a linked bug for details on conditions of enabled by default
      cases.
    
    Bug: b:308692056
    Fixed: b:308692056
    Test: browser_tests --gtest_filter=*LoggedInUserFilesAppBrowserTest*
    Change-Id: I574df68ba9debb1fc7829879e0f16e0a57c9668c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5008055
    Commit-Queue: Yuki Awano <yawano@google.com>
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221759}

--- chrome/browser/ash/file_manager/file_manager_browsertest_utils.h
+++ chrome/browser/ash/file_manager/file_manager_browsertest_utils.h
@@ -33,6 +33,10 @@ struct TestCase {
 
   TestCase& TabletMode();
 
+  TestCase& SetLocale(const std::string& locale);
+
+  TestCase& SetCountry(const std::string& country);
+
   TestCase& EnableGenericDocumentsProvider();
 
   TestCase& DisableGenericDocumentsProvider();
@@ -98,6 +102,8 @@ struct TestCase {
   TestCase& EnableOsFeedback();
 
   TestCase& EnableGoogleOneOfferFilesBanner();
+
+  TestCase& DisableGoogleOneOfferFilesBanner();
 
   TestCase& FeatureIds(const std::vector<std::string>& ids);
 
