1ecc5125c996f4142a3df3ee1d598a7048006ea8
Max Ihlenfeldt <max@igalia.com>
2023-11-08 06:51:44 +0000

    IWYU for net/filter/zstd_source_stream.cc
    Bug: None
    Change-Id: I7cc47f2b844264d2a1b89767559361ea8174eb4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5010396
    Auto-Submit: Max Ihlenfeldt <max@igalia.com>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221420}

--- net/filter/zstd_source_stream.cc
+++ net/filter/zstd_source_stream.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/filter/zstd_source_stream.h"
 
 #include <algorithm>
+#include <unordered_map>
 #include <utility>
 
 #define ZSTD_STATIC_LINKING_ONLY
