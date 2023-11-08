44cb3ed9f57a2b6815024a94eef19929e3125f35
Joey Arhar <jarhar@chromium.org>
2023-11-08 18:09:58 +0000

    Remove SaveAsWithDeclarativeShadowDOM flag
    This feature has been enabled by default for a long time, so I'm
    removing the flag.
    
    Bug: 1281853
    Change-Id: I07d9bc6aa2304d295ef16385ff390d269eaa6b3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5011173
    Reviewed-by: David Baron <dbaron@chromium.org>
    Auto-Submit: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221727}

--- third_party/blink/renderer/core/frame/web_frame_serializer_impl.cc
+++ third_party/blink/renderer/core/frame/web_frame_serializer_impl.cc
@@ -374,8 +374,7 @@ void WebFrameSerializerImpl::OpenTagToString(Element* element,
   String added_contents = PostActionAfterSerializeOpenTag(element, param);
   // Complete the open tag for element when it has child/children.
   if (element->HasChildren() || param->have_added_contents_before_end ||
-      (element->AuthorShadowRoot() &&
-       RuntimeEnabledFeatures::SaveAsWithDeclarativeShadowDOMEnabled())) {
+      element->AuthorShadowRoot()) {
     result.Append('>');
   }
   // Append the added contents generate in  post action of open tag.
@@ -395,8 +394,7 @@ void WebFrameSerializerImpl::EndTagToString(Element* element,
     return;
   // Write end tag when element has child/children.
   if (element->HasChildren() || param->have_added_contents_before_end ||
-      (element->AuthorShadowRoot() &&
-       RuntimeEnabledFeatures::SaveAsWithDeclarativeShadowDOMEnabled())) {
+      element->AuthorShadowRoot()) {
     result.Append("</");
     result.Append(element->nodeName().DeprecatedLower());
     result.Append('>');
@@ -449,9 +447,7 @@ void WebFrameSerializerImpl::BuildContentForNode(Node* node,
       OpenTagToString(element, param);
 
       // Process the ShadowRoot into a <template> if present.
-      auto* shadow_root = element->AuthorShadowRoot();
-      if (shadow_root &&
-          RuntimeEnabledFeatures::SaveAsWithDeclarativeShadowDOMEnabled()) {
+      if (auto* shadow_root = element->AuthorShadowRoot()) {
         ShadowRootTagToString(shadow_root, param);
         for (Node* child = shadow_root->firstChild(); child;
              child = child->nextSibling()) {
