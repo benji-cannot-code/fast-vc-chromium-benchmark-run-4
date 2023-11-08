db827d07f2da6446624445e10aaf3268c3a05882
Christoph Schwering <schwering@google.com>
2023-11-08 12:44:52 +0000

    [Autofill][Passwords] Split //c/b/touch_to_fill
    This CL splits //chrome/browser/touch_to_fill into three
    subdirectories:
    - autofill/          # new
    - common/            # existed
    - password_manager/  # new
    This is intended to ease browsing the code.
    
    For example, //chrome/browser/touch_to_fill currently contains
    touch_to_fill_controller_autofill_delegate.h, which actually appears
    to be more of a Password Manager file than an Autofill file.
    
    The CL does not update the package names of the Java classes.
    
    The landing logistics is as follows.
    1. crrev.com/c/5001426 adds placeholder //c/b/ttf/pm/a/i/BUILD.gn.
    2. crrev.com/i/6622934 adds dependency on //c/b/ttf/pm/a/i/BUILD.gn.
    3. This CL switches the the roles of //c/b/ttf/pm/a/i/BUILD.gn and
       //c/b/ttf/a/i/BUILD.gn:
       - //c/b/ttf/pm/a/i/BUILD.gn becomes the real BUILD.gn and
       - //c/b/ttf/a/i/BUILD.gn becomes a placeholder.
    4. crrev.com/i/6631954 removes dependency on //c/b/ttf/a/i/BUILD.gn.
    5. crrev.com/c/5002229 removes placeholder //c/b/ttf/a/i/BUILD.gn.
    
    The CL is essentially the result of:
    
    # Renaming payments/ to autofill/ and moving the keyboard
    # suppressor there.
    $ mkdir autofill
    $ mv payments autofill
    $ mv touch_to_fill_keyboard_suppressor*.{cc,h} autofill/android/
    
    # Moving all the rest except for common/ to into a password_manager/
    # subdirectory.
    $ mkdir password_manager
    $ mv no_passkeys \
         password_generation \
         touch_to_fill_controller_autofill_delegate*.{cc,h} \
         touch_to_fill_controller*.{cc,h} \
         touch_to_fill_controller_delegate*.{cc,h} \
         touch_to_fill_controller_webauthn_delegate*.{cc,h} \
         touch_to_fill_view_factory*.{cc,h} \
         touch_to_fill_view.h \
         password_manager/
    
    Bug: 1498976
    Skip-Translation-Screenshots-Check: True
    Change-Id: If06034659ebb755a7a0d68342ddde10d44a9ffe9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994310
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Gregory Guterman <guterman@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221545}

--- chrome/browser/ui/autofill/chrome_autofill_client.cc
+++ chrome/browser/ui/autofill/chrome_autofill_client.cc
@@ -121,7 +121,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/signin/signin_bridge.h"
 #include "chrome/browser/android/tab_android.h"
 #include "chrome/browser/flags/android/chrome_feature_list.h"
-#include "chrome/browser/touch_to_fill/payments/android/touch_to_fill_credit_card_view_impl.h"
+#include "chrome/browser/touch_to_fill/autofill/android/touch_to_fill_credit_card_view_impl.h"
 #include "chrome/browser/ui/android/autofill/autofill_accessibility_utils.h"
 #include "chrome/browser/ui/android/autofill/autofill_cvc_save_message_delegate.h"
 #include "chrome/browser/ui/android/autofill/autofill_logger_android.h"
