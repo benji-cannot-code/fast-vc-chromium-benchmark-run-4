d4e8b7a99abedf7db8c1aafa8eb7dba4cab9820f
Joe Downing <joedow@google.com>
2023-11-08 19:45:23 +0000

    Provide a new access token when reconnecting to a session
    Access tokens last ~1 hour so we can't persist these to disk and allow
    users to reconnect after 1 hour. This means we need to generate a new
    access token for each session.
    
    I've plumbed this up to the CrdAdminSessionController as there are a
    few options:
    1. Refactor DeviceCommandStartCrdSessionJob::OAuthTokenFetcher out
       of the existing class and call it in CrdAdminSessionController
       as needed
    2. Create a new OAuthTokenFetcher in //remoting for admin sessions
       and use it rather than the 'Passthrough' fetcher
    3. Update the Access token to have a longer lifetime
    4. Some other solution which the Commercial CRD folks like better
    
    Per discussion with the Commercial CRD team, we are going to go
    with option #1 and they will handle the refactoring in their code
    to provide a new access token for each reconnection.
    
    Bug: b:309446315
    Change-Id: I754eff3b42d87575b013f45f78e1107df6f6a6ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5011265
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Jeroen Dhollander <jeroendh@google.com>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221816}

--- chrome/browser/ash/policy/remote_commands/crd_admin_session_controller_unittest.cc
+++ chrome/browser/ash/policy/remote_commands/crd_admin_session_controller_unittest.cc
@@ -114,7 +114,7 @@ class RemotingServiceMock
   MOCK_METHOD(void, GetReconnectableSessionId, (SessionIdCallback));
   MOCK_METHOD(void,
               ReconnectToSession,
-              (remoting::SessionId, StartSessionCallback));
+              (remoting::SessionId, const std::string&, StartSessionCallback));
 };
 
 // Wrapper around the `RemotingServiceMock`, solving the lifetime issue
@@ -141,8 +141,10 @@ class RemotingServiceWrapper
   }
 
   void ReconnectToSession(remoting::SessionId session_id,
+                          const std::string& oauth_access_token,
                           StartSessionCallback callback) override {
-    implementation_->ReconnectToSession(session_id, std::move(callback));
+    implementation_->ReconnectToSession(session_id, oauth_access_token,
+                                        std::move(callback));
   }
 
  private:
@@ -678,8 +680,10 @@ TEST_F(CrdAdminSessionControllerTest,
       .WillOnce([&](auto callback) { std::move(callback).Run(kSessionId); });
 
   // And next we should use this session id to reconnect.
-  EXPECT_CALL(remoting_service(), ReconnectToSession(kSessionId, testing::_))
-      .WillOnce([&](remoting::SessionId, StartSupportSessionCallback callback) {
+  EXPECT_CALL(remoting_service(),
+              ReconnectToSession(kSessionId, testing::_, testing::_))
+      .WillOnce([&](remoting::SessionId, const std::string&,
+                    StartSupportSessionCallback callback) {
         std::move(callback).Run(
             StartSupportSessionResponse::NewObserver(BindObserver()));
       });
