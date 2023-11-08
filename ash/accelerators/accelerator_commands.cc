ce173852f40a36777a743d0f4fa3930b32b3354a
Jiaming Cheng <jiamingc@google.com>
2023-11-08 04:33:50 +0000

    [QsRevamp Clean Up] ash/accelerators, ash/components, ash/app_list
    Bug: b/305075031
    Change-Id: I49d251c3d0c13f32e1b0984ee7527b88407b7924
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5011360
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Jiaming Cheng <jiamingc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221391}

--- ash/accelerators/accelerator_commands.cc
+++ ash/accelerators/accelerator_commands.cc
@@ -1565,10 +1565,6 @@ void ToggleResizeLockMenu() {
 }
 
 void ToggleMessageCenterBubble() {
-  if (!features::IsQsRevampEnabled()) {
-    HandleToggleSystemTrayBubbleInternal(/*focus_message_center=*/true);
-    return;
-  }
   aura::Window* target_root = Shell::GetRootWindowForNewWindows();
   NotificationCenterTray* tray = RootWindowController::ForWindow(target_root)
                                      ->GetStatusAreaWidget()
@@ -1687,24 +1683,11 @@ void UnpinWindow() {
 
 void VolumeDown() {
   auto* audio_handler = CrasAudioHandler::Get();
-  if (features::IsQsRevampEnabled()) {
-    // Only plays the audio if unmuted.
-    if (!audio_handler->IsOutputMuted()) {
-      AcceleratorController::PlayVolumeAdjustmentSound();
-    }
-    audio_handler->DecreaseOutputVolumeByOneStep(kStepPercentage);
-    return;
+  // Only plays the audio if unmuted.
+  if (!audio_handler->IsOutputMuted()) {
+    AcceleratorController::PlayVolumeAdjustmentSound();
   }
-
-  if (audio_handler->IsOutputMuted()) {
-    audio_handler->SetOutputVolumePercent(0);
-  } else {
-    if (audio_handler->IsOutputVolumeBelowDefaultMuteLevel())
-      audio_handler->SetOutputMute(true);
-    else
-      AcceleratorController::PlayVolumeAdjustmentSound();
-    audio_handler->DecreaseOutputVolumeByOneStep(kStepPercentage);
-  }
+  audio_handler->DecreaseOutputVolumeByOneStep(kStepPercentage);
 }
 
 void VolumeMute() {
@@ -1723,27 +1706,11 @@ void VolumeMuteToggle() {
 void VolumeUp() {
   auto* audio_handler = CrasAudioHandler::Get();
   bool play_sound = false;
-  if (features::IsQsRevampEnabled()) {
-    if (audio_handler->IsOutputMuted()) {
-      audio_handler->SetOutputMute(false);
-    }
-    play_sound = audio_handler->GetOutputVolumePercent() != 100;
-    audio_handler->IncreaseOutputVolumeByOneStep(kStepPercentage);
-
-    if (play_sound) {
-      AcceleratorController::PlayVolumeAdjustmentSound();
-    }
-    return;
-  }
-
   if (audio_handler->IsOutputMuted()) {
     audio_handler->SetOutputMute(false);
-    audio_handler->AdjustOutputVolumeToAudibleLevel();
-    play_sound = true;
-  } else {
-    play_sound = audio_handler->GetOutputVolumePercent() != 100;
-    audio_handler->IncreaseOutputVolumeByOneStep(kStepPercentage);
   }
+  play_sound = audio_handler->GetOutputVolumePercent() != 100;
+  audio_handler->IncreaseOutputVolumeByOneStep(kStepPercentage);
 
   if (play_sound) {
     AcceleratorController::PlayVolumeAdjustmentSound();
