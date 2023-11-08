e72880289770fb0c9b61af7ddbbb8a2e979e5b06
Finnur Thorarinsson <finnur@chromium.org>
2023-11-08 12:04:44 +0000

    [Sheriff]: Disabling failing WebAuth test on Mac.
    Bug: 1500554, 1498289
    Change-Id: I3b09ac3bbc29bf133c151109349242b049ea4939
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5007231
    Owners-Override: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Danil Chapovalov <danilchap@chromium.org>
    Auto-Submit: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Danil Chapovalov <danilchap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221523}

--- content/browser/webauth/webauth_browsertest.cc
+++ content/browser/webauth/webauth_browsertest.cc
@@ -1882,7 +1882,14 @@ class WebAuthCrossDomainTest : public WebAuthBrowserTestBase {
       device::kWebAuthnRelatedOrigin};
 };
 
-IN_PROC_BROWSER_TEST_F(WebAuthCrossDomainTest, Create) {
+// TODO(https://crbug.com/1500554): Enable test when MacOS failure is fixed.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_Create DISABLED_Create
+#else
+#define MAYBE_Create Create
+#endif
+
+IN_PROC_BROWSER_TEST_F(WebAuthCrossDomainTest, MAYBE_Create) {
   CreateParameters parameters;
   parameters.rp_id = "foo.com";
   test_client()->set_webauthn_origins_response(
@@ -1906,7 +1913,14 @@ IN_PROC_BROWSER_TEST_F(WebAuthCrossDomainTest, CreateBadContentType) {
   EXPECT_EQ(kRpIdContentTypeMessage, result);
 }
 
-IN_PROC_BROWSER_TEST_F(WebAuthCrossDomainTest, CreateBadOrigin) {
+// TODO(https://crbug.com/1500554): Enable test when MacOS failure is fixed.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_CreateBadOrigin DISABLED_CreateBadOrigin
+#else
+#define MAYBE_CreateBadOrigin CreateBadOrigin
+#endif
+
+IN_PROC_BROWSER_TEST_F(WebAuthCrossDomainTest, MAYBE_CreateBadOrigin) {
   CreateParameters parameters;
   parameters.rp_id = "foo.com";
   test_client()->set_webauthn_origins_response("application/json",
@@ -1931,7 +1945,14 @@ IN_PROC_BROWSER_TEST_F(WebAuthCrossDomainTest, Timeout) {
   EXPECT_EQ(kNotAllowedErrorMessage, result);
 }
 
-IN_PROC_BROWSER_TEST_F(WebAuthCrossDomainTest, Get) {
+// TODO(https://crbug.com/1500554): Enable test when MacOS failure is fixed.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_Get DISABLED_Get
+#else
+#define MAYBE_Get Get
+#endif
+
+IN_PROC_BROWSER_TEST_F(WebAuthCrossDomainTest, MAYBE_Get) {
   const uint8_t kCredentialId[] = {0x61, 0x6C, 0x6C, 0x6F, 0x77, 0x65,
                                    0x64, 0x43, 0x72, 0x65, 0x64, 0x65,
                                    0x6E, 0x74, 0x69, 0x61, 0x6C};
