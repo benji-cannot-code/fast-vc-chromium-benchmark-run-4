ef59b7c2f51d16ba9c2f88803397a98ee56ceac8
Sylvain Defresne <sdefresne@chromium.org>
2023-11-08 09:57:22 +0000

    [ios] Convert absl::optional<T> to std::optional<T>
    Since https://crrev.com/c/4469528, absl::optional<T> is an alias for
    std::optional<T>, so convert uses of the old type to the new one as
    it is easier to use (no needs to have public_deps on absl, shorter
    path to the header, ...).
    
    This CL converts all uses in //ios/chrome/app and below.
    
    Bug: none
    Change-Id: I75354a80b9de446c07761a678c8d3f2e6151bbcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5010041
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1221460}

--- ios/chrome/app/post_restore_app_agent_unittest.mm
+++ ios/chrome/app/post_restore_app_agent_unittest.mm
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/test/web_task_environment.h"
 #import "testing/gmock/include/gmock/gmock.h"
 #import "testing/platform_test.h"
-#import "third_party/abseil-cpp/absl/types/optional.h"
 #import "third_party/ocmock/OCMock/OCMock.h"
 #import "third_party/ocmock/gtest_support.h"
 
