653e46f272533ecaea3223474ec19d553d193d7d
Filipa Senra <fsenra@google.com>
2023-11-08 17:04:03 +0000

    [Android][SA] Fix null pnt and not grouped by embedder for embargoed
    This CL fixes a null pointer exception on the assert of
    addEmbeddedPermission to also include embargoed origins that do not have
    a secondary pattern.
    
    This CL also makes sure that for Storage Access exceptions they are not
    grouped by embedder for All Sites.
    
    We also do some refactoring to the fetcher test class.
    
    Fix: b:309083164
    Bug: 1478113
    Change-Id: Iaededf734efb525e2cbf31e3989b3e64d420104f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5004051
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1221683}

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/WebsitePermissionsFetcher.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/WebsitePermissionsFetcher.java
@@ -394,6 +394,8 @@ public class WebsitePermissionsFetcher {
                 String embedder = exception.getSecondaryPattern();
 
                 if (isEmbeddedPermission
+                        && embedder != null
+                        && !embedder.equals(SITE_WILDCARD)
                         && mSiteSettingsCategory != null
                         && mSiteSettingsCategory.getType() == SiteSettingsCategory.Type.ALL_SITES) {
                     // AllSites should group embedded permissions by embedder.
