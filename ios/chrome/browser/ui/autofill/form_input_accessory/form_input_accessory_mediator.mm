47ce4171b74d6286e6b77c7000d6baf954de412c
Veronique Nguyen <veronguyen@google.com>
2023-11-08 23:55:20 +0000

    Reland "[iOS] Update params when Payments Bottom Sheet is about to be shown"
    This is a reland of commit 52d64f5267e8e3d0b460acbe61edf14bac13d6cd
    
    Fix variables declaration order in
    AutofillBottomSheetObserverBridgeTest.
    
    Original change's description:
    > [iOS] Update params when Payments Bottom Sheet is about to be shown
    >
    > Part 1 of the fix to fill the credit card fields properly with the
    > payments bottom sheet.
    >
    > In this CL we are adding AutofillBottomSheetObserver to observe when
    > the payments bottom sheet is about to be shown. We need to add this
    > because we encountered an issue where if the auto focus of the page is
    > on another field, the _lastSeenParams in FormInputAccessoryMediator
    > still set as that auto focus field. When we tap on another field,
    > keyboardWillShow will be called before anything else and in that
    > method, we call retrieveSuggestionsForForm with the _lastSeenParams.
    >
    > This was causing conflict with our setup call: both
    > FormInputAccessoryMediator and PaymentsSuggestionBottomSheetMediator
    > are calling retrieveSuggestionsForForm and the first call to complete
    > with a success will stop the pipeline. That means the first one to
    > complete will be setting the query form and query field. We want the
    > form and field to be the correct one in order to fill the credit card
    > fields properly.
    >
    > Bug: 1491904
    > Change-Id: I7f8b00fbd3ef6265610c21ea8d8ff6ea26293dbb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4974721
    > Reviewed-by: Tommy Martino <tmartino@chromium.org>
    > Commit-Queue: Veronique Nguyen <veronguyen@google.com>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1217202}
    
    Bug: 1491904
    Change-Id: Ia0b2e6933518c563ae01675944761262953c471b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5013090
    Commit-Queue: Veronique Nguyen <veronguyen@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1221968}

--- ios/chrome/browser/ui/autofill/form_input_accessory/form_input_accessory_mediator.mm
+++ ios/chrome/browser/ui/autofill/form_input_accessory/form_input_accessory_mediator.mm
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/autofill/ios/form_util/form_activity_observer_bridge.h"
 #import "components/autofill/ios/form_util/form_activity_params.h"
 #import "components/password_manager/core/browser/password_counter.h"
+#import "ios/chrome/browser/autofill/bottom_sheet/autofill_bottom_sheet_observer_bridge.h"
+#import "ios/chrome/browser/autofill/bottom_sheet/autofill_bottom_sheet_tab_helper.h"
 #import "ios/chrome/browser/autofill/form_input_accessory_view_handler.h"
 #import "ios/chrome/browser/autofill/form_input_suggestions_provider.h"
 #import "ios/chrome/browser/autofill/form_suggestion_tab_helper.h"
@@ -75,7 +77,8 @@ class PasswordCounterDelegateBridge
   password_manager::PasswordCounter counter_;
 };
 
-@interface FormInputAccessoryMediator () <BooleanObserver,
+@interface FormInputAccessoryMediator () <AutofillBottomSheetObserving,
+                                          BooleanObserver,
                                           FormActivityObserver,
                                           FormInputAccessoryViewDelegate,
                                           CRWWebStateObserver,
@@ -146,6 +149,10 @@ class PasswordCounterDelegateBridge
   std::unique_ptr<autofill::FormActivityObserverBridge>
       _formActivityObserverBridge;
 
+  // Bridge for the AutofillBottomSheetObserver.
+  std::unique_ptr<autofill::AutofillBottomSheetObserverBridge>
+      _autofillBottomSheetObserverBridge;
+
   // Whether suggestions have previously been shown.
   BOOL _suggestionsHaveBeenShown;
 
@@ -194,6 +201,9 @@ class PasswordCounterDelegateBridge
         _formActivityObserverBridge =
             std::make_unique<autofill::FormActivityObserverBridge>(_webState,
                                                                    self);
+        _autofillBottomSheetObserverBridge =
+            std::make_unique<autofill::AutofillBottomSheetObserverBridge>(
+                self, AutofillBottomSheetTabHelper::FromWebState(webState));
         _webStateObserverBridge =
             std::make_unique<web::WebStateObserverBridge>(self);
         webState->AddObserver(_webStateObserverBridge.get());
@@ -252,6 +262,7 @@ class PasswordCounterDelegateBridge
 - (void)dealloc {
   // TODO(crbug.com/1454777)
   DUMP_WILL_BE_CHECK(!_formActivityObserverBridge.get());
+  DUMP_WILL_BE_CHECK(!_autofillBottomSheetObserverBridge.get());
   DUMP_WILL_BE_CHECK(!_personalDataManager);
   DUMP_WILL_BE_CHECK(!_webState);
   DUMP_WILL_BE_CHECK(!_webStateList);
@@ -259,6 +270,7 @@ class PasswordCounterDelegateBridge
 
 - (void)disconnect {
   _formActivityObserverBridge.reset();
+  _autofillBottomSheetObserverBridge.reset();
   if (_personalDataManager && _personalDataManagerObserver.get()) {
     _personalDataManager->RemoveObserver(_personalDataManagerObserver.get());
     _personalDataManagerObserver.reset();
@@ -286,6 +298,7 @@ class PasswordCounterDelegateBridge
     _webStateObserverBridge.reset();
     _webState = nullptr;
     _formActivityObserverBridge.reset();
+    _autofillBottomSheetObserverBridge.reset();
   }
 }
 
@@ -296,6 +309,20 @@ class PasswordCounterDelegateBridge
 #pragma mark - KeyboardNotification
 
 - (void)keyboardWillShow:(NSNotification*)notification {
+  [self updateSuggestionsIfNeeded];
+}
+
+#pragma mark - AutofillBottomSheetObserving
+
+- (void)willShowPaymentsBottomSheetWithParams:
+    (const autofill::FormActivityParams&)params {
+  // Update params in this mediator because -keyboardWillShow will be called
+  // before the bottom sheet is being notified to show and that will call
+  // -retrieveSuggestionsForForm with the last seen params. Depending on what
+  // the current page is auto focused on, it could be the incorrect params and
+  // we need to update it.
+  _lastSeenParams = params;
+  _hasLastSeenParams = YES;
   [self updateSuggestionsIfNeeded];
 }
 
@@ -520,6 +547,10 @@ class PasswordCounterDelegateBridge
     webState->AddObserver(_webStateObserverBridge.get());
     _formActivityObserverBridge =
         std::make_unique<autofill::FormActivityObserverBridge>(webState, self);
+    _autofillBottomSheetObserverBridge =
+        std::make_unique<autofill::AutofillBottomSheetObserverBridge>(
+            self, AutofillBottomSheetTabHelper::FromWebState(webState));
+
     FormSuggestionTabHelper* tabHelper =
         FormSuggestionTabHelper::FromWebState(webState);
     if (tabHelper) {
--- ios/chrome/browser/ui/autofill/form_input_accessory/form_input_accessory_mediator_unittest.mm
+++ ios/chrome/browser/ui/autofill/form_input_accessory/form_input_accessory_mediator_unittest.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/autofill/ios/form_util/form_activity_params.h"
 #import "components/autofill/ios/form_util/form_util_java_script_feature.h"
 #import "components/autofill/ios/form_util/test_form_activity_tab_helper.h"
+#import "ios/chrome/browser/autofill/bottom_sheet/autofill_bottom_sheet_tab_helper.h"
 #import "ios/chrome/browser/autofill/form_input_suggestions_provider.h"
 #import "ios/chrome/browser/shared/model/web_state_list/test/fake_web_state_list_delegate.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
@@ -59,6 +60,9 @@ class FormInputAccessoryMediatorTest : public PlatformTest {
 
     test_web_state_->SetNavigationManager(
         std::make_unique<web::FakeNavigationManager>());
+
+    AutofillBottomSheetTabHelper::CreateForWebState(test_web_state_.get(), nil);
+
     web_state_list_.InsertWebState(0, std::move(test_web_state_),
                                    WebStateList::INSERT_ACTIVATE,
                                    WebStateOpener());
