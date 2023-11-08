293a82a82788f5ba62677d50e088ca7946c034a9
thefrog <thefrog@chromium.org>
2023-11-08 14:52:49 +0000

    Extend hash-prefix real-time lookups chrome flag to apply to Desktop
    This also includes setting the default value for the relay URL parameter
    so that it can be used when the chrome flag is enabled.
    
    The flag_descriptions file changes are moving the code out of Android-
    specific build flag checks.
    
    Bug: 1441654
    Change-Id: I70cd682c9e84ea0396457fe89836fded00b85dfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5008582
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221605}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -1550,6 +1550,12 @@ const char kExperimentalWebPlatformFeaturesName[] =
 const char kExperimentalWebPlatformFeaturesDescription[] =
     "Enables experimental Web Platform features that are in development.";
 
+const char kSafeBrowsingHashPrefixRealTimeLookupsName[] =
+    "Safe Browsing Hash Prefix Real Time Lookups";
+const char kSafeBrowsingHashPrefixRealTimeLookupsDescription[] =
+    "Enable checking URLs through Safe Browsing hash-prefix real time "
+    "protocol.";
+
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 const char kExtensionsMenuAccessControlName[] =
     "Extensions Menu Access Control";
@@ -4337,12 +4343,6 @@ const char kRevokeNotificationsPermissionIfDisabledOnAppLevelName[] =
 const char kRevokeNotificationsPermissionIfDisabledOnAppLevelDescription[] =
     "Allow revoking site-level notification permission if Chrome has no "
     "app-level notification permission on Android.";
-
-const char kSafeBrowsingHashPrefixRealTimeLookupsName[] =
-    "Safe Browsing Hash Prefix Real Time Lookups";
-const char kSafeBrowsingHashPrefixRealTimeLookupsDescription[] =
-    "Enable checking URLs through Safe Browsing hash-prefix real time "
-    "protocol.";
 
 const char kSafeBrowsingNewGmsApiForBrowseUrlDatabaseCheckName[] =
     "Safe Browsing new GMS API for browse URL database check";
