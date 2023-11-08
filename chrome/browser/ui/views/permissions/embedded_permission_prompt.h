d6968fd5f3312f57fdc144a4c51b5c9f260b32f0
Andy Paicu <andypaicu@chromium.org>
2023-11-08 09:37:02 +0000

    [PEPC] Ensure grouped permissions are prioritized as expected w/ screens
    ... sometimes only showing a partial subset of all requests.
    
    This follows the flow defined here: https://docs.google.com/document/d/1N7LiHB3YudOKBa2egjpnvkTVIaUPcxqhkBXxXnlhZ8k/edit?resourcekey=0-HhUJb_M9XCHJSgPE61wpGw#heading=h.ww9bbkn3bde7
    
    Also the "Continue not allowing" button.
    
    Bug: 1462930
    Change-Id: I670dbb8a37baef1e76815dd9685f289858712edd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5009403
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Kamila Hasanbega <hkamila@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221452}

--- chrome/browser/ui/views/permissions/embedded_permission_prompt.h
+++ chrome/browser/ui/views/permissions/embedded_permission_prompt.h
@@ -9,7 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/views/permissions/embedded_permission_prompt_base_view.h"
 #include "chrome/browser/ui/views/permissions/permission_prompt_desktop.h"
+#include "components/content_settings/core/common/content_settings_types.h"
 #include "components/permissions/permission_prompt.h"
+#include "components/permissions/permission_request.h"
+#include "components/permissions/request_type.h"
 
 class Browser;
 
@@ -74,6 +77,7 @@ class EmbeddedPermissionPrompt
   void ShowSystemSettings() override;
   base::WeakPtr<permissions::PermissionPrompt::Delegate>
   GetPermissionPromptDelegate() const override;
+  const std::vector<permissions::PermissionRequest*>& Requests() const override;
 
  private:
   static Variant DeterminePromptVariant(
@@ -81,10 +85,18 @@ class EmbeddedPermissionPrompt
       const content_settings::SettingInfo& info,
       ContentSettingsType type);
 
+  void PrioritizeAndMergeNewVariant(Variant new_variant,
+                                    ContentSettingsType type);
+
+  void RebuildRequests();
+
   Variant embedded_prompt_variant_ = Variant::kUninitialized;
   raw_ptr<EmbeddedPermissionPromptBaseView> prompt_view_;
 
   raw_ptr<permissions::PermissionPrompt::Delegate> delegate_;
+
+  std::set<ContentSettingsType> prompt_types_;
+  std::vector<permissions::PermissionRequest*> requests_;
 
   base::WeakPtrFactory<EmbeddedPermissionPrompt> weak_factory_{this};
 };
