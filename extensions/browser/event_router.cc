c9d17c4c477d1ed98fe3357eda22e316fb1b123d
Justin Lulejian <jlulejian@chromium.org>
2023-11-08 18:57:47 +0000

    [Extensions] Fix some event page metrics not emitting at correct time.
    Metric involved (incremented):
    Extensions.Events.DispatchToAckTime.ExtensionEventPage3
    
    Before this would emit for all events sent to all listeners running in
    the extension renderer process. This included when the event ran in the
    lazy background page script, but also when it ran in listeners like (but
    not limited to) popup scripts.
    
    Metric now emits if the event ran in the lazy background page script
    (specifically if the event has a listener in the lazy background
    page script context when the event is fired).
    
    Relies on assumption that only one ExtensionHost::OnEventAck() call will
    be made per event per extension process.
    
    Low-Coverage-Reason:
    EventRouter::DecrementInFlightEventsForRenderFrameHost not being tested
    is out of scope for this CL. I've filed crbug.com/1498560 to test it.
    
    Bug: 1441221
    Change-Id: I7b1fe2bc25a5295371968e9f38036811f1670b86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4980656
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221782}

--- extensions/browser/event_router.cc
+++ extensions/browser/event_router.cc
@@ -1219,7 +1219,8 @@ void EventRouter::DispatchEventToProcess(
 
 void EventRouter::DecrementInFlightEventsForServiceWorker(
     const WorkerId& worker_id,
-    int event_id) {
+    int event_id,
+    bool event_will_run_in_lazy_background_page_script) {
   auto* process = RenderProcessHost::FromID(worker_id.render_process_id);
   // Check to make sure the rendered process hasn't gone away by the time
   // we've gotten here. (It's possible it has crashed, etc.) If that's
@@ -1228,6 +1229,12 @@ void EventRouter::DecrementInFlightEventsForServiceWorker(
   if (!process) {
     return;
   }
+
+  if (event_will_run_in_lazy_background_page_script) {
+    bad_message::ReceivedBadMessage(
+        process, bad_message::ER_SW_INVALID_LAZY_BACKGROUND_PARAM);
+  }
+
   const bool worker_stopped = !ProcessManager::Get(process->GetBrowserContext())
                                    ->HasServiceWorker(worker_id);
   content::ServiceWorkerContext* service_worker_context =
@@ -1246,7 +1253,8 @@ void EventRouter::DecrementInFlightEventsForServiceWorker(
 void EventRouter::DecrementInFlightEventsForRenderFrameHost(
     int render_process_host,
     const ExtensionId& extension_id,
-    int event_id) {
+    int event_id,
+    bool event_will_run_in_lazy_background_page_script) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   auto* process = RenderProcessHost::FromID(render_process_host);
   if (!process) {
@@ -1256,7 +1264,7 @@ void EventRouter::DecrementInFlightEventsForRenderFrameHost(
   ProcessManager* pm = ProcessManager::Get(process->GetBrowserContext());
   ExtensionHost* host = pm->GetBackgroundHostForExtension(extension_id);
   if (host) {
-    host->OnEventAck(event_id);
+    host->OnEventAck(event_id, event_will_run_in_lazy_background_page_script);
   }
 }
 
