368063842d1424352affd0f5a9aebaa83ee10ab6
Michele Fan <michelefan@chromium.org>
2023-11-09 03:25:46 +0000

    snap-group: [AT-R2] Install focus ring on individual window cycle items
    1st cl in Alt + Tab round 2 refactor to address UIR feedback. The
    following items have been implemented in this cl:
    1. Install focus ring on individual items, re-consolidate the APIs
    in `WindowMiniViewBase` and its child classes;
    2. Use vector to store the child mini views hosted by
    `GroupContainerCycleView`.
    
    Temporarily disabled unit tests will be enabled or re-purposed throughout
    the round 2 refactor process.
    
    Fixed: b/309156976, b/297070130
    Demo: http://b/309156976#comment6
    Test: Modified existing tests
    Change-Id: I6de932519108dde627d1ebffe7c61c8e6b7ff326
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5011874
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222052}

--- ash/wm/overview/overview_item_base.h
+++ ash/wm/overview/overview_item_base.h
@@ -30,7 +30,6 @@ class MouseEvent;
 
 namespace views {
 class View;
-class Widget;
 }  // namespace views
 
 namespace ash {
--- ash/wm/window_mini_view.h
+++ ash/wm/window_mini_view.h
@@ -37,9 +37,6 @@ class WindowMiniViewBase : public views::View {
   WindowMiniViewBase& operator=(const WindowMiniViewBase&) = delete;
   ~WindowMiniViewBase() override;
 
-  // Shows or hides a focus ring around this.
-  void UpdateFocusState(bool focus);
-
   // Sets rounded corners on the exposed corners, the inner corners will be
   // sharp.
   void SetRoundedCornersRadius(
@@ -70,6 +67,9 @@ class WindowMiniViewBase : public views::View {
   // Returns the exposed rounded corners.
   virtual gfx::RoundedCornersF GetRoundedCorners() const = 0;
 
+  // Shows or hides a focus ring around this.
+  virtual void UpdateFocusState(bool focus) = 0;
+
  protected:
   WindowMiniViewBase();
 
@@ -77,12 +77,6 @@ class WindowMiniViewBase : public views::View {
   // otherwise the default rounded corners will be used.
   absl::optional<gfx::RoundedCornersF> header_view_rounded_corners_;
   absl::optional<gfx::RoundedCornersF> preview_view_rounded_corners_;
-
- private:
-  void InstallFocusRing();
-
-  // True if this view is focused when using keyboard navigation.
-  bool is_focused_ = false;
 };
 
 // WindowMiniView is a view which contains a header and optionally a mirror of
@@ -116,6 +110,7 @@ class ASH_EXPORT WindowMiniView : public WindowMiniViewBase,
   views::View* backdrop_view() { return backdrop_view_; }
   WindowPreviewView* preview_view() { return preview_view_; }
   const WindowPreviewView* preview_view() const { return preview_view_; }
+  bool is_mini_view_focused() const { return is_focused_; }
 
   // Sets the visibility of |backdrop_view_|. Creates it if it is null.
   void SetBackdropVisibility(bool visible);
@@ -136,6 +131,7 @@ class ASH_EXPORT WindowMiniView : public WindowMiniViewBase,
   void SetShowPreview(bool show) override;
   int TryRemovingChildItem(aura::Window* destroying_window) override;
   gfx::RoundedCornersF GetRoundedCorners() const override;
+  void UpdateFocusState(bool focus) override;
 
  protected:
   explicit WindowMiniView(aura::Window* source_window);
@@ -161,6 +157,8 @@ class ASH_EXPORT WindowMiniView : public WindowMiniViewBase,
   void OnWindowTitleChanged(aura::Window* window) override;
 
  private:
+  void InstallFocusRing();
+
   // The window this class is meant to be a header for. This class also may
   // optionally show a mirrored view of this window.
   raw_ptr<aura::Window, ExperimentalAsh> source_window_;
@@ -175,6 +173,9 @@ class ASH_EXPORT WindowMiniView : public WindowMiniViewBase,
   // Optionally shows a preview of |window_|.
   raw_ptr<WindowPreviewView, DanglingUntriaged | ExperimentalAsh>
       preview_view_ = nullptr;
+
+  // True if `this` is focused when using keyboard navigation.
+  bool is_focused_ = false;
 
   base::ScopedObservation<aura::Window, aura::WindowObserver>
       window_observation_{this};
