43393f969e3ebb7467399169fb5b564058eb2524
Sylvain Defresne <sdefresne@chromium.org>
2023-11-09 09:27:59 +0000

    [ios] Do not create SessionRestorationBrowserAgent in AttachBrowserAgents
    The agent is created on demand by LegacySessionRestorationService, so
    do not create it in AttachBrowserAgents().
    
    Remove unnecessary includes and deps.
    
    Bug: 1383087
    Change-Id: Iac76b2ced2e533fb2d3d0f3879e630fd392632f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5004298
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1222154}

--- ios/chrome/browser/main/browser_agent_util.mm
+++ ios/chrome/browser/main/browser_agent_util.mm
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/reading_list/model/reading_list_browser_agent.h"
 #import "ios/chrome/browser/send_tab_to_self/model/send_tab_to_self_browser_agent.h"
 #import "ios/chrome/browser/sessions/live_tab_context_browser_agent.h"
-#import "ios/chrome/browser/sessions/session_restoration_browser_agent.h"
-#import "ios/chrome/browser/sessions/session_service_ios.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
@@ -29,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/sync/model/sync_error_browser_agent.h"
 #import "ios/chrome/browser/tab_insertion/model/tab_insertion_browser_agent.h"
 #import "ios/chrome/browser/tabs/model/closing_web_state_observer_browser_agent.h"
-#import "ios/chrome/browser/tabs/model/features.h"
 #import "ios/chrome/browser/tabs/model/synced_window_delegate_browser_agent.h"
 #import "ios/chrome/browser/tabs/model/tab_parenting_browser_agent.h"
 #import "ios/chrome/browser/tabs/model/tab_pickup/tab_pickup_browser_agent.h"
@@ -46,10 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/web_state_list/model/session_metrics.h"
 #import "ios/chrome/browser/web_state_list/model/web_usage_enabler/web_usage_enabler_browser_agent.h"
 #import "ios/public/provider/chrome/browser/app_utils/app_utils_api.h"
-
-// To get access to UseSessionSerializationOptimizations().
-// TODO(crbug.com/1383087): remove once the feature is fully launched.
-#import "ios/web/common/features.h"
 
 void AttachBrowserAgents(Browser* browser) {
   if (breadcrumbs::IsEnabled(GetApplicationContext()->GetLocalState())) {
@@ -106,12 +99,6 @@ void AttachBrowserAgents(Browser* browser) {
 
   // UrlLoadingBrowserAgent requires UrlLoadingNotifierBrowserAgent.
   UrlLoadingBrowserAgent::CreateForBrowser(browser);
-
-  // SessionRestorartionAgent requires WebUsageEnablerBrowserAgent.
-  if (!web::features::UseSessionSerializationOptimizations()) {
-    SessionRestorationBrowserAgent::CreateForBrowser(
-        browser, [SessionServiceIOS sharedService], IsPinnedTabsEnabled());
-  }
 
   // TabUsageRecorderBrowserAgent and WebStateListMetricsBrowserAgent observe
   // the SessionRestorationBrowserAgent, so they should be created after the the
