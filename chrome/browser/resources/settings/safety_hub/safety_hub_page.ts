8c74b91fdfca5786ffa00ad180cf9e8939d5d7a2
Aisulu Rakhmetullina <aisulu@google.com>
2023-11-09 20:28:30 +0000

    [SHv2] Add metrics that record card status on click
    Safety Hub page shows 3 cards: Passwords, Chrome version and Safe
    Browsing. Each can be in one of the multiple states (e.g. SAFE, WARNING,
    etc). This CL introduces metrics to record the card state on click.
    
    Bug: 1443466, 1500401
    Change-Id: Ib5e660bde7eb94280a491f9252ac5f623c798979
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5006911
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Commit-Queue: Aisulu Rakhmetullina <aisulu@google.com>
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222500}

--- chrome/browser/resources/settings/safety_hub/safety_hub_page.ts
+++ chrome/browser/resources/settings/safety_hub/safety_hub_page.ts
@@ -18,6 +18,7 @@ import {WebUiListenerMixin} from 'chrome://resources/cr_elements/web_ui_listener
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {PasswordManagerImpl, PasswordManagerPage} from '../autofill_page/password_manager_proxy.js';
+import {MetricsBrowserProxy, MetricsBrowserProxyImpl, SafetyHubCardState} from '../metrics_browser_proxy.js';
 import {RelaunchMixin, RestartType} from '../relaunch_mixin.js';
 import {routes} from '../route.js';
 import {RouteObserverMixin, Router} from '../router.js';
@@ -96,6 +97,8 @@ export class SettingsSafetyHubPageElement extends
   private userEducationItemList_: SiteInfo[];
   private browserProxy_: SafetyHubBrowserProxy =
       SafetyHubBrowserProxyImpl.getInstance();
+  private metricsBrowserProxy_: MetricsBrowserProxy =
+      MetricsBrowserProxyImpl.getInstance();
 
   override connectedCallback() {
     super.connectedCallback();
@@ -177,6 +180,10 @@ export class SettingsSafetyHubPageElement extends
   }
 
   private onPasswordsClick_() {
+    this.metricsBrowserProxy_.recordSafetyHubCardStateClicked(
+        'Settings.SafetyHub.PasswordsCard.StatusOnClick',
+        this.passwordCardData_.state as unknown as SafetyHubCardState);
+
     PasswordManagerImpl.getInstance().showPasswordManager(
         PasswordManagerPage.CHECKUP);
   }
@@ -189,6 +196,10 @@ export class SettingsSafetyHubPageElement extends
   }
 
   private onVersionClick_() {
+    this.metricsBrowserProxy_.recordSafetyHubCardStateClicked(
+        'Settings.SafetyHub.VersionCard.StatusOnClick',
+        this.versionCardData_.state as unknown as SafetyHubCardState);
+
     if (this.versionCardData_.state === CardState.WARNING) {
       this.performRestart(RestartType.RELAUNCH);
     } else {
@@ -206,6 +217,10 @@ export class SettingsSafetyHubPageElement extends
   }
 
   private onSafeBrowsingClick_() {
+    this.metricsBrowserProxy_.recordSafetyHubCardStateClicked(
+        'Settings.SafetyHub.SafeBrowsingCard.StatusOnClick',
+        this.safeBrowsingCardData_.state as unknown as SafetyHubCardState);
+
     Router.getInstance().navigateTo(
         routes.SECURITY, /* dynamicParams= */ undefined,
         /* removeSearch= */ true);
