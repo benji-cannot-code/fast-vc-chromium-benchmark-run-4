c2dbd735f7b1cb83b1f6185b3ab3e94476ae4a4e
Chris Fredrickson <cfredric@chromium.org>
2023-11-09 15:42:29 +0000

    [RWS] Use GetIfDict API instead of reimplementing it
    This is a followup to https://crrev.com/c/5013385; it turns out
    there's a cleaner API for what I was trying to do.
    
    Change-Id: Idb58251ed4afd9381212a13c6c6dd3b05fb275f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5017196
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Shuran Huang <shuuran@chromium.org>
    Reviewed-by: Shuran Huang <shuuran@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222299}

--- chrome/browser/first_party_sets/first_party_sets_policy_service.cc
+++ chrome/browser/first_party_sets/first_party_sets_policy_service.cc
@@ -53,12 +53,8 @@ const base::Value::Dict* GetOverridesPolicyForProfile(
   }
   // The value is declared as a dict, but we assume that the user may have
   // modified the prefs file or the file may be corrupt.
-  const base::Value& value =
-      prefs->GetValue(first_party_sets::kRelatedWebsiteSetsOverrides);
-  if (!value.is_dict()) {
-    return nullptr;
-  }
-  return &value.GetDict();
+  return prefs->GetValue(first_party_sets::kRelatedWebsiteSetsOverrides)
+      .GetIfDict();
 }
 
 bool GetEnabledStateForProfile(Profile* profile) {
@@ -113,7 +109,6 @@ void FirstPartySetsPolicyService::Init() {
   // non-null `context`.
   CHECK(profile);
 
-  PrefService* prefs = profile->GetPrefs();
   pref_enabled_ = GetEnabledStateForProfile(profile);
 
   if (profile->IsSystemProfile() || profile->IsGuestSession() ||
@@ -128,7 +123,7 @@ void FirstPartySetsPolicyService::Init() {
   // dynamically refresh, and all the delegates for `context` will have the same
   // policy and thus the same config.
   content::FirstPartySetsHandler::GetInstance()->GetContextConfigForPolicy(
-      GetOverridesPolicyForProfile(prefs),
+      GetOverridesPolicyForProfile(profile->GetPrefs()),
       base::BindOnce(&FirstPartySetsPolicyService::OnProfileConfigReady,
                      weak_factory_.GetWeakPtr(),
                      // We should only clear site data if First-Party Sets is
