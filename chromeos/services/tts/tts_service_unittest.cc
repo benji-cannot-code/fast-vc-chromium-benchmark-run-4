86a08a0f1a6057f9dc39a51c748e4305971a0df7
Josiah Krutz <josiahk@google.com>
2023-11-09 01:29:22 +0000

    [TTS] Fix glitches in TTS audio output stream
    Fix glitches in TTS audio output stream when speaking a TTS voice on
    ChromeOS. Unlock pulling audio samples from multiple input buffers when
    filling output buffers.
    
    Doc: http://go/cros-tts-audio-gaps
    
    Test: Added unit tests; Manually verified eSpeak NG, Enhanced Network TTS, and Google TTS voice engine output sound good with this patch.
    Fixed: b/308214781
    Change-Id: Icdbd073557f107864e8d2a1659055338f3ebe6bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4986686
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222008}

--- chromeos/services/tts/tts_service_unittest.cc
+++ chromeos/services/tts/tts_service_unittest.cc
@@ -5,100 +5,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/services/tts/tts_service.h"
 
-#include "base/functional/bind.h"
-#include "base/strings/utf_string_conversions.h"
-#include "base/test/task_environment.h"
 #include "chromeos/services/tts/public/mojom/tts_service.mojom.h"
-#include "media/base/audio_glitch_info.h"
-#include "media/mojo/mojom/audio_data_pipe.mojom.h"
-#include "media/mojo/mojom/audio_stream_factory.mojom.h"
-#include "mojo/public/cpp/bindings/pending_receiver.h"
-#include "mojo/public/cpp/bindings/pending_remote.h"
-#include "mojo/public/cpp/bindings/receiver.h"
-#include "mojo/public/cpp/bindings/remote.h"
-#include "testing/gtest/include/gtest/gtest.h"
-
-using mojo::PendingReceiver;
-using mojo::PendingRemote;
+#include "chromeos/services/tts/tts_test_utils.h"
 
 namespace chromeos {
 namespace tts {
 namespace {
 
-using CreateOutputStreamCallback =
-    base::OnceCallback<void(media::mojom::ReadWriteAudioDataPipePtr)>;
-using CreateLoopbackStreamCallback =
-    base::OnceCallback<void(media::mojom::ReadOnlyAudioDataPipePtr)>;
-
-class MockAudioStreamFactory : public media::mojom::AudioStreamFactory {
+// Tests the TtsService interface, the main interface that handles TTS
+// requests from clients on ChromeOS. For more info, please see
+// chromeos/services/tts/public/mojom/tts_service.mojom.
+class TtsServiceTest : public TtsTestBase {
  public:
-  void CreateInputStream(
-      PendingReceiver<media::mojom::AudioInputStream> stream,
-      PendingRemote<media::mojom::AudioInputStreamClient> client,
-      PendingRemote<media::mojom::AudioInputStreamObserver> observer,
-      PendingRemote<media::mojom::AudioLog> log,
-      const std::string& device_id,
-      const media::AudioParameters& params,
-      uint32_t shared_memory_count,
-      bool enable_agc,
-      base::ReadOnlySharedMemoryRegion key_press_count_buffer,
-      media::mojom::AudioProcessingConfigPtr processing_config,
-      CreateInputStreamCallback callback) override {}
-  void AssociateInputAndOutputForAec(
-      const base::UnguessableToken& input_stream_id,
-      const std::string& output_device_id) override {}
-
-  void CreateOutputStream(
-      PendingReceiver<media::mojom::AudioOutputStream> stream,
-      mojo::PendingAssociatedRemote<media::mojom::AudioOutputStreamObserver>
-          observer,
-      PendingRemote<media::mojom::AudioLog> log,
-      const std::string& device_id,
-      const media::AudioParameters& params,
-      const base::UnguessableToken& group_id,
-      CreateOutputStreamCallback callback) override {
-    audio_output_stream_ = std::move(stream);
-    std::move(callback).Run(nullptr);
-  }
-  void BindMuter(
-      mojo::PendingAssociatedReceiver<media::mojom::LocalMuter> receiver,
-      const base::UnguessableToken& group_id) override {}
-
-  void CreateLoopbackStream(
-      PendingReceiver<media::mojom::AudioInputStream> receiver,
-      PendingRemote<media::mojom::AudioInputStreamClient> client,
-      PendingRemote<media::mojom::AudioInputStreamObserver> observer,
-      const media::AudioParameters& params,
-      uint32_t shared_memory_count,
-      const base::UnguessableToken& group_id,
-      CreateLoopbackStreamCallback callback) override {}
-
-  PendingReceiver<media::mojom::AudioOutputStream> audio_output_stream_;
-};
-
-class MockTtsEventObserver : public mojom::TtsEventObserver {
- public:
-  // mojom::TtsEventObserver:
-  void OnStart() override { start_count++; }
-
-  void OnTimepoint(int32_t char_index) override {
-    char_indices.push_back(char_index);
-  }
-
-  void OnEnd() override { end_count++; }
-
-  void OnError() override {}
-
-  int start_count = 0;
-  std::vector<int> char_indices;
-  int end_count = 0;
-};
-
-class TtsServiceTest : public testing::Test {
- public:
-  TtsServiceTest()
-      : service_(remote_service_.BindNewPipeAndPassReceiver()),
-        audio_stream_factory_(&mock_audio_stream_factory_) {}
+  TtsServiceTest() : service_(remote_service_.BindNewPipeAndPassReceiver()) {}
+  TtsServiceTest(const TtsServiceTest&) = delete;
+  TtsServiceTest& operator=(const TtsServiceTest&) = delete;
   ~TtsServiceTest() override = default;
 
  protected:
@@ -125,11 +46,8 @@ class TtsServiceTest : public testing::Test {
   // testing::Test:
   void SetUp() override { service_.set_keep_process_alive_for_testing(true); }
 
-  base::test::TaskEnvironment task_environment_;
   mojo::Remote<mojom::TtsService> remote_service_;
   TtsService service_;
-  MockAudioStreamFactory mock_audio_stream_factory_;
-  mojo::Receiver<media::mojom::AudioStreamFactory> audio_stream_factory_;
 };
 
 TEST_F(TtsServiceTest, DisconnectPlaybackStream) {
