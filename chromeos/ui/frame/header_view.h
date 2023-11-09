7868b58f96130a364ae755a98faf21aba359dc89
Allen Bauer <kylixrd@chromium.org>
2023-11-09 15:28:07 +0000

    Add metadata to Views classes
    This CL was uploaded by git cl split.
    
    R=oshima@chromium.org
    
    Bug: 1485768
    Change-Id: Ib1f3d56b20d760a5c0aeec5d1bb963e674ec7a37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5014510
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222290}

--- chromeos/ui/frame/header_view.h
+++ chromeos/ui/frame/header_view.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ui/frame/immersive/immersive_fullscreen_controller_delegate.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_observer.h"
+#include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/display/display_observer.h"
 #include "ui/views/view.h"
 
