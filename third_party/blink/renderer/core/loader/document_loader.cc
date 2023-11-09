479ffa3a9997e49e9f640bf28fef04ac7dc275d8
Jeremy Roman <jbroman@chromium.org>
2023-11-09 04:52:04 +0000

    Remove EarlyHintsPreloadForNavigationOptIn runtime-enabled feature.
    It was left behind when this feature was cleaned up earlier this year:
      https://chromium-review.googlesource.com/c/chromium/src/+/4135568
    
    Bug: 671310
    Change-Id: I9691571d2cce6517720dd40efd9a09e73ceef716
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5016283
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Auto-Submit: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222082}

--- third_party/blink/renderer/core/loader/document_loader.cc
+++ third_party/blink/renderer/core/loader/document_loader.cc
@@ -3153,9 +3153,7 @@ void DocumentLoader::RecordUseCountersForCommit() {
       hints_context.CountClientHints(type);
   }
 
-  if (RuntimeEnabledFeatures::EarlyHintsPreloadForNavigationOptInEnabled(
-          frame_->DomWindow()) &&
-      !early_hints_preloaded_resources_.empty()) {
+  if (!early_hints_preloaded_resources_.empty()) {
     CountUse(WebFeature::kEarlyHintsPreload);
   }
 
