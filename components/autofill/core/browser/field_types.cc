90a696aaeb3d8d8f48eca06007e381ba0bb2a753
Din Nezametdinov <shaikhitdin@google.com>
2023-11-09 10:39:03 +0000

    [Passwords] Add new field type for rolling out Username First Flow with intermediate values
    Add field type that will be used for new server predictions based on
    votes from Username First Flow with intermediate values.
    DD: go/uff-k-10-dd
    
    Server side counterpart: cl/580164663
    
    Bug: 1470586
    Change-Id: I02d44cbe988a694e84223a150d8215ed371ed1a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4987320
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Din Nezametdinov <shaikhitdin@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222176}

--- components/autofill/core/browser/field_types.cc
+++ components/autofill/core/browser/field_types.cc
@@ -133,7 +133,9 @@ static constexpr auto kTypeNameToFieldType =
           ADDRESS_HOME_OVERFLOW_AND_LANDMARK},
          {"ADDRESS_HOME_BETWEEN_STREETS_OR_LANDMARK",
           ADDRESS_HOME_BETWEEN_STREETS_OR_LANDMARK},
-         {"SINGLE_USERNAME_FORGOT_PASSWORD", SINGLE_USERNAME_FORGOT_PASSWORD}});
+         {"SINGLE_USERNAME_FORGOT_PASSWORD", SINGLE_USERNAME_FORGOT_PASSWORD},
+         {"SINGLE_USERNAME_WITH_INTERMEDIATE_VALUES",
+          SINGLE_USERNAME_WITH_INTERMEDIATE_VALUES}});
 
 bool IsFillableFieldType(ServerFieldType field_type) {
   switch (field_type) {
@@ -222,6 +224,7 @@ bool IsFillableFieldType(ServerFieldType field_type) {
     case CONFIRMATION_PASSWORD:
     case SINGLE_USERNAME:
     case SINGLE_USERNAME_FORGOT_PASSWORD:
+    case SINGLE_USERNAME_WITH_INTERMEDIATE_VALUES:
       return true;
 
     // Not fillable credential fields.
@@ -309,6 +312,7 @@ std::string_view FieldTypeToDeveloperRepresentationString(
     case USERNAME:
     case SINGLE_USERNAME:
     case SINGLE_USERNAME_FORGOT_PASSWORD:
+    case SINGLE_USERNAME_WITH_INTERMEDIATE_VALUES:
       return "Username";
     case USERNAME_AND_EMAIL_ADDRESS:
       return "Username and email";
@@ -557,6 +561,7 @@ FieldTypeGroup GroupTypeOfServerFieldType(ServerFieldType field_type) {
     case SINGLE_USERNAME:
     case NOT_USERNAME:
     case SINGLE_USERNAME_FORGOT_PASSWORD:
+    case SINGLE_USERNAME_WITH_INTERMEDIATE_VALUES:
       return FieldTypeGroup::kPasswordField;
 
     case NO_SERVER_DATA:
