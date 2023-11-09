7bc76faea2b3019cdcedbe22c79496ef63bc97ad
Qihui Zhao <qihuizhao@google.com>
2023-11-09 01:00:02 +0000

    Refactor MockTestPaymentsClient class into standalone file
    This is pre-requisite refactor CL that moves MockTestPaymentsClient
    out and creates into its own file, with this change the following
    implementation which makes server calls via payments_client can
    simply mock methods in MockTestPaymentsClient.
    
    Bug: b:296651874
    Change-Id: I5393790ac800ccaf875aab12714a73e80f966c94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5009282
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1221995}

---
+++ components/autofill/core/browser/payments/mock_test_payments_client.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill/core/browser/payments/mock_test_payments_client.h"
+
+namespace autofill {
+
+MockTestPaymentsClient::MockTestPaymentsClient()
+    : payments::TestPaymentsClient(
+          /*url_loader_factory=*/nullptr,
+          /*identity_manager=*/nullptr,
+          /*personal_data_manager=*/nullptr) {}
+
+MockTestPaymentsClient::~MockTestPaymentsClient() = default;
+
+}  // namespace autofill
