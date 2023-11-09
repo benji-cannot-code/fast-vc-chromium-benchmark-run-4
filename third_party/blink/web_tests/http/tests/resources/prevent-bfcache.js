5cffa3aa1f8b25d2f0c6d5a83eac35c2fa4b39a9
Fergal Daly <fergal@chromium.org>
2023-11-09 06:00:59 +0000

    Add library to prevent BFCache being used in internal web tests.
    Use it in one test.
    
    The overall goal is to remove use of unload handlers as a mechanism of preventing BFCache. Secondary goal is to abstract away the mechanism so that it can be changed in the future if needed.
    
    Bug: 1488371
    Change-Id: Ia3d591dd613eadafeef769bf871a369b3231b556
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5014043
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222099}

---
+++ third_party/blink/web_tests/http/tests/resources/prevent-bfcache.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+
+// When the returned promised resolve, BFCache will not be used for the current
+// page.
+function preventBFCache() {
+  return new Promise(resolve => {
+    let webSocket = new WebSocket('ws://127.0.0.1:8880/echo');
+    webSocket.onopen = () => { resolve(42); };
+  });
+}
