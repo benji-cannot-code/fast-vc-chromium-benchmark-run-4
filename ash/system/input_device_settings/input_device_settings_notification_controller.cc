7254c195e5ad76dd282e1c54c69097205d31743b
YuhanYang <yyhyyh@google.com>
2023-11-09 19:00:01 +0000

    customization: Call notification if device not in pref list
    Bug: b/241965717
    Test: ash_unittests
    Change-Id: Icce6d5a752e52d011cf5eaa23c6180270eba05b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5004253
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1222432}

--- ash/system/input_device_settings/input_device_settings_notification_controller.cc
+++ ash/system/input_device_settings/input_device_settings_notification_controller.cc
@@ -425,7 +425,7 @@ void InputDeviceSettingsNotificationController::NotifyMouseFirstTimeConnected(
   seen_mouse_list.Append(mouse.device_key);
   prefs->SetList(prefs::kPeripheralNotificationMiceSeen,
                  std::move(seen_mouse_list));
-  // TODO(yyhyyh@): Push peripheral notification.
+  NotifyMouseIsCustomizable(mouse);
 }
 
 void InputDeviceSettingsNotificationController::
@@ -450,7 +450,7 @@ void InputDeviceSettingsNotificationController::
   seen_graphics_tablet_list.Append(graphics_tablet->device_key);
   prefs->SetList(prefs::kPeripheralNotificationGraphicsTabletsSeen,
                  std::move(seen_graphics_tablet_list));
-  // TODO(yyhyyh@): Push peripheral notification.
+  NotifyGraphicsTabletIsCustomizable(*graphics_tablet);
 }
 
 void InputDeviceSettingsNotificationController::
