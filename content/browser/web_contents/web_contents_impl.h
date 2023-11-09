2cae6b04c050ba1ee26574222535f96b6d442952
Jeremy Roman <jbroman@chromium.org>
2023-11-09 22:22:53 +0000

    Mark WebContentsImpl::GetDelegate as final.
    It's marginally more efficient if LTO doesn't see through it right
    away since it enables inlining this trivial function, and it's
    called multiple times back-to-back in some functions right now.
    
    It's also not reasonable to override this further anyway, since
    some functions do assume |delegate_| is the right delegate.
    
    I don't expect this to have a noticeable impact, but since it's
    half a kilobyte smaller and I've already written the CL, why not?
    
    Change-Id: Ieda20706256fe7200a9d31c4c3e67479e949724b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5017063
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222534}

--- content/browser/web_contents/web_contents_impl.h
+++ content/browser/web_contents/web_contents_impl.h
@@ -335,7 +335,7 @@ class CONTENT_EXPORT WebContentsImpl : public WebContents,
   std::string GetTitleForMediaControls();
 
   // WebContents ------------------------------------------------------
-  WebContentsDelegate* GetDelegate() override;
+  WebContentsDelegate* GetDelegate() final;
   void SetDelegate(WebContentsDelegate* delegate) override;
   NavigationControllerImpl& GetController() override;
   BrowserContext* GetBrowserContext() override;
