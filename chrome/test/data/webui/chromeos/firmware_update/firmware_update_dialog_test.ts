d6ce78eaa87edf6937f786b64f620473c4676ff4
Ashley Prasad <ashleydp@google.com>
2023-11-09 19:05:02 +0000

    Firmware UI: Migrate tests off of js2gtest
    - Convert file from JS to CC.
    - Update test cases to use IN_PROC_BROWSER_TEST_F.
    - Remove the unified test pattern.
    - Update BUILD.gn.
    
    Bug: b:309673937, b:243706697
    Test: browser_tests --gtest_filter=*FirmwareUpdate*
    Change-Id: I905d5a97fb318d75a0bef48c7223e1031d06adc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4936522
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222440}

--- chrome/test/data/webui/chromeos/firmware_update/firmware_update_dialog_test.ts
+++ chrome/test/data/webui/chromeos/firmware_update/firmware_update_dialog_test.ts
@@ -19,7 +19,7 @@ import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chromeo
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
 import {isVisible} from 'chrome://webui-test/test_util.js';
 
-export function firmwareUpdateDialogTest() {
+suite('FirmwareUpdateDialogTest', () => {
   let updateDialogElement: FirmwareUpdateDialogElement|null = null;
 
   setup(() => {
@@ -188,4 +188,4 @@ export function firmwareUpdateDialogTest() {
     assertFalse(isVisible(updateDialogElement.shadowRoot.querySelector(
         '#indeterminateProgressBar')));
   });
-}
+});
--- chrome/test/data/webui/chromeos/firmware_update/peripheral_updates_list_test.ts
+++ chrome/test/data/webui/chromeos/firmware_update/peripheral_updates_list_test.ts
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://accessory-update/peripheral_updates_list.js';
+
 import {fakeFirmwareUpdates} from 'chrome://accessory-update/fake_data.js';
 import {FakeUpdateProvider} from 'chrome://accessory-update/fake_update_provider.js';
 import {FirmwareUpdate} from 'chrome://accessory-update/firmware_update.mojom-webui.js';
@@ -15,7 +17,7 @@ import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chromeo
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
 import {isVisible} from 'chrome://webui-test/test_util.js';
 
-export function peripheralUpdatesListTest() {
+suite('PeripheralUpdatesListTest', () => {
   let peripheralUpdateListElement: PeripheralUpdateListElement|null = null;
 
   let provider: FakeUpdateProvider|null = null;
@@ -91,4 +93,4 @@ export function peripheralUpdatesListTest() {
           assertTrue(isVisible(upToDateText));
         });
   });
-}
+});
