51cb0f1abe1b0d8146257d30e34e6752485a2975
Katie Dektar <katie@chromium.org>
2023-11-09 23:24:31 +0000

    [A11y] Adds FaceGaze feature and loads FaceGaze JS when pref is set
    Together, this allows FaceGaze controller to observe mouse location
    events when enabled and to stop observing when the pref is disabled.
    
    AX-Relnotes: N/A
    Bug: b/309121742
    Test: New
    Change-Id: I0603bfc2ee4875462e5cf51e9fc7a120cec4418b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5014865
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222612}

--- ash/accessibility/accessibility_controller_impl.h
+++ ash/accessibility/accessibility_controller_impl.h
@@ -234,6 +234,7 @@ class ASH_EXPORT AccessibilityControllerImpl : public AccessibilityController,
   Feature& cursor_highlight() const;
   Feature& dictation() const;
   Feature& color_correction() const;
+  Feature& face_gaze() const;
   Feature& floating_menu() const;
   Feature& focus_highlight() const;
   FeatureWithDialog& fullscreen_magnifier() const;
@@ -562,6 +563,7 @@ class ASH_EXPORT AccessibilityControllerImpl : public AccessibilityController,
   void UpdateLargeCursorFromPref();
   void UpdateLiveCaptionFromPref();
   void UpdateCursorColorFromPrefs();
+  void UpdateFaceGazeFromPrefs();
   void UpdateColorCorrectionFromPrefs();
   void UpdateSwitchAccessKeyCodesFromPref(SwitchAccessCommand command);
   void UpdateSwitchAccessAutoScanEnabledFromPref();
