594057b4ffdcbb5f450b52c8a9462eacb0e6ddd4
Denis Kuznetsov <antrim@chromium.org>
2023-11-09 13:15:34 +0000

    Fix some items introduced by crrev.com/c/5014265
    Bug: b/304963851
    Change-Id: Ic26609ad29ba20ea47160a844af5584d3eef18ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5012730
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Auto-Submit: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Istvan Nagy <iscsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1222223}

--- chrome/browser/ash/login/debug_overlay_browsertest.cc
+++ chrome/browser/ash/login/debug_overlay_browsertest.cc
@@ -25,6 +25,7 @@ constexpr char kScreensPanel[] = "DebuggerPanelScreens";
 constexpr int kOobeScreensCount = 52;
 constexpr int kLoginScreensCount = 48;
 constexpr int kOsInstallScreensCount = 2;
+constexpr int kLocalPasswordScreenCount = 3;
 
 std::string ElementsInPanel(const std::string& panel) {
   return base::StrCat({"$('", panel, "').children.length"});
@@ -82,8 +83,9 @@ class DebugOverlayScreensTest : public DebugOverlayTest,
   // DebugOverlayTest:
   void SetUpCommandLine(base::CommandLine* command_line) override {
     DebugOverlayTest::SetUpCommandLine(command_line);
-    if (!GetParam())
+    if (!GetParam()) {
       return;
+    }
     command_line->AppendSwitch(switches::kAllowOsInstall);
   }
 };
@@ -96,8 +98,13 @@ IN_PROC_BROWSER_TEST_P(DebugOverlayScreensTest, ExpectScreenButtonsCount) {
   test::OobeJS().CreateVisibilityWaiter(true, kDebugOverlay)->Wait();
 
   int screens_count = kOobeScreensCount;
-  if (switches::IsOsInstallAllowed())
+  if (switches::IsOsInstallAllowed()) {
     screens_count += kOsInstallScreensCount;
+  }
+  // Account for screens under feature flags.
+  if (features::AreLocalPasswordsEnabledForConsumers()) {
+    screens_count += kLocalPasswordScreenCount;
+  }
 
   test::OobeJS().ExpectEQ(ElementsInPanel(kScreensPanel), screens_count);
 }
@@ -111,7 +118,13 @@ IN_PROC_BROWSER_TEST_F(DebugOverlayOnLoginTest, ExpectScreenButtonsCount) {
   test::OobeJS().ExpectVisible(kDebugButton);
   test::OobeJS().ClickOn(kDebugButton);
   test::OobeJS().CreateVisibilityWaiter(true, kDebugOverlay)->Wait();
-  test::OobeJS().ExpectEQ(ElementsInPanel(kScreensPanel), kLoginScreensCount);
+
+  int screens_count = kLoginScreensCount;
+  // Account for screens under feature flags.
+  if (features::AreLocalPasswordsEnabledForConsumers()) {
+    screens_count += kLocalPasswordScreenCount;
+  }
+  test::OobeJS().ExpectEQ(ElementsInPanel(kScreensPanel), screens_count);
 }
 
 }  // namespace ash
