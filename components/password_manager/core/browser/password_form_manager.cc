f67de08464a60e2e03eb26028a3f0c26eea4c846
Jan Keitel <jkeitel@google.com>
2023-11-09 10:39:38 +0000

    Remove calls from PasswordFormManager to PasswordChangeSuccessTracker.
    These calls were used to signal the end of a password change flow.
    With the planned removal of the PasswordChangeSuccessTracker, they can
    also be removed.
    
    Bug: 1500551
    Change-Id: I1b710ab3b85ce3c191ba3b834fcd0d4ade844fea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5013317
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1222177}

--- components/password_manager/core/browser/password_form_manager.cc
+++ components/password_manager/core/browser/password_form_manager.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/form_parsing/form_data_parser.h"
 #include "components/password_manager/core/browser/form_parsing/password_field_prediction.h"
 #include "components/password_manager/core/browser/interactions_stats.h"
-#include "components/password_manager/core/browser/password_change_success_tracker_impl.h"
 #include "components/password_manager/core/browser/password_feature_manager.h"
 #include "components/password_manager/core/browser/password_form.h"
 #include "components/password_manager/core/browser/password_form_filling.h"
@@ -94,11 +93,6 @@ bool FormContainsFieldWithName(const FormData& form,
       };
   return base::ranges::any_of(form.fields, equals_element_case_insensitive,
                               &FormFieldData::name);
-}
-
-bool IsPhished(const PasswordForm& credentials) {
-  return credentials.password_issues.find(InsecureType::kPhished) !=
-         credentials.password_issues.end();
 }
 
 void LogUsingPossibleUsername(PasswordManagerClient* client,
@@ -387,39 +381,13 @@ void PasswordFormManager::Save() {
     newly_blocklisted_ = false;
   }
 
-  // This is potentially the conclusion of a password change flow. It might also
-  // not be related to such a flow at all, but the tracker will figure it out.
-  PasswordChangeSuccessTracker::EndEvent end_event =
-      HasGeneratedPassword() ? PasswordChangeSuccessTracker::EndEvent::
-                                   kManualFlowGeneratedPasswordChosen
-                             : PasswordChangeSuccessTracker::EndEvent::
-                                   kManualFlowOwnPasswordChosen;
-  client_->GetPasswordChangeSuccessTracker()->OnChangePasswordFlowCompleted(
-      parsed_submitted_form_->url,
-      base::UTF16ToUTF8(GetPendingCredentials().username_value), end_event,
-      IsPhished(GetPendingCredentials()));
-
   password_save_manager_->Save(observed_form(), *parsed_submitted_form_);
-
   client_->UpdateFormManagers();
 }
 
 void PasswordFormManager::Update(const PasswordForm& credentials_to_update) {
-  // This is potentially the conclusion of a password change flow. It might also
-  // not be related to such a flow at all, but the tracker will figure it out.
-  PasswordChangeSuccessTracker::EndEvent end_event =
-      HasGeneratedPassword() ? PasswordChangeSuccessTracker::EndEvent::
-                                   kManualFlowGeneratedPasswordChosen
-                             : PasswordChangeSuccessTracker::EndEvent::
-                                   kManualFlowOwnPasswordChosen;
-  client_->GetPasswordChangeSuccessTracker()->OnChangePasswordFlowCompleted(
-      parsed_submitted_form_->url,
-      base::UTF16ToUTF8(GetPendingCredentials().username_value), end_event,
-      IsPhished(credentials_to_update));
-
   password_save_manager_->Update(credentials_to_update, observed_form(),
                                  *parsed_submitted_form_);
-
   client_->UpdateFormManagers();
 }
 
