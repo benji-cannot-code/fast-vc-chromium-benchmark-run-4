3c413876af5c65080d67924af744ecde54d7f65d
Jagadesh P <jagadeshjai1999@gmail.com>
2023-11-09 20:06:25 +0000

    [DanglingPointer] Remove dangling pointer in manifest_browsertest.cc
    And Minor Code formatting.
    
    Bug: 1291138
    Change-Id: I2056cca1068ae0620b2c6768dc4fea70d5fe694f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4988222
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Jagadesh Jai <jagadeshjai1999@gmail.com>
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222486}

--- content/browser/manifest/manifest_browsertest.cc
+++ content/browser/manifest/manifest_browsertest.cc
@@ -58,10 +58,7 @@ class ManifestBrowserTest;
 // Mock of a WebContentsDelegate that catches messages sent to the console.
 class MockWebContentsDelegate : public WebContentsDelegate {
  public:
-  MockWebContentsDelegate(WebContents* web_contents, ManifestBrowserTest* test)
-      : web_contents_(web_contents),
-        test_(test) {
-  }
+  explicit MockWebContentsDelegate(ManifestBrowserTest* test) : test_(test) {}
 
   bool DidAddMessageToConsole(WebContents* source,
                               blink::mojom::ConsoleMessageLevel log_level,
@@ -74,8 +71,7 @@ class MockWebContentsDelegate : public WebContentsDelegate {
   }
 
  private:
-  raw_ptr<WebContents, DanglingUntriaged> web_contents_;
-  raw_ptr<ManifestBrowserTest> test_;
+  raw_ptr<ManifestBrowserTest> test_ = nullptr;
 };
 
 class ManifestBrowserTest : public ContentBrowserTest,
@@ -99,8 +95,8 @@ class ManifestBrowserTest : public ContentBrowserTest,
     ContentBrowserTest::SetUpOnMainThread();
     DCHECK(shell()->web_contents());
 
-    mock_web_contents_delegate_ = std::make_unique<MockWebContentsDelegate>(
-        shell()->web_contents(), this);
+    mock_web_contents_delegate_ =
+        std::make_unique<MockWebContentsDelegate>(this);
     shell()->web_contents()->SetDelegate(mock_web_contents_delegate_.get());
     Observe(shell()->web_contents());
     ASSERT_TRUE(embedded_test_server()->Start());
@@ -126,9 +122,7 @@ class ManifestBrowserTest : public ContentBrowserTest,
     return *manifest_;
   }
 
-  const GURL& manifest_url() const {
-    return manifest_url_;
-  }
+  const GURL& manifest_url() const { return manifest_url_; }
 
   int GetConsoleErrorCount() const {
     // The IPCs reporting console errors are not FIFO with the manifest IPCs.
@@ -203,11 +197,12 @@ bool MockWebContentsDelegate::DidAddMessageToConsole(
     const std::u16string& message,
     int32_t line_no,
     const std::u16string& source_id) {
-  DCHECK(source == web_contents_);
+  DCHECK_EQ(source->GetDelegate(), this);
 
   if (log_level == blink::mojom::ConsoleMessageLevel::kError ||
-      log_level == blink::mojom::ConsoleMessageLevel::kWarning)
+      log_level == blink::mojom::ConsoleMessageLevel::kWarning) {
     test_->OnReceivedConsoleError(message);
+  }
   return false;
 }
 
@@ -427,8 +422,9 @@ IN_PROC_BROWSER_TEST_F(ManifestBrowserTest, CorsManifestWithAcessControls) {
 
   ASSERT_TRUE(NavigateToURL(shell(), test_url));
 
-  std::string manifest_link = cors_embedded_test_server()->GetURL(
-      "/manifest/manifest-cors.json").spec();
+  std::string manifest_link = cors_embedded_test_server()
+                                  ->GetURL("/manifest/manifest-cors.json")
+                                  .spec();
   ASSERT_TRUE(ExecJs(shell(), "setManifestTo('" + manifest_link + "')"));
 
   GetManifestAndWait();
@@ -612,8 +608,10 @@ std::unique_ptr<net::test_server::HttpResponse> CustomHandleRequestForCookies(
   }
 
   const auto& iter = request.headers.find("Cookie");
-  if (iter == request.headers.end() || request.relative_url != "/manifest.json")
+  if (iter == request.headers.end() ||
+      request.relative_url != "/manifest.json") {
     return nullptr;
+  }
 
   std::unique_ptr<net::test_server::BasicHttpResponse> http_response(
       new net::test_server::BasicHttpResponse());
@@ -638,8 +636,7 @@ IN_PROC_BROWSER_TEST_F(ManifestBrowserTest, UseCredentialsSendCookies) {
   ASSERT_TRUE(custom_embedded_test_server->Start());
 
   ASSERT_TRUE(SetCookie(shell()->web_contents()->GetBrowserContext(),
-                        custom_embedded_test_server->base_url(),
-                        "foobar"));
+                        custom_embedded_test_server->base_url(), "foobar"));
 
   ASSERT_TRUE(NavigateToURL(
       shell(), custom_embedded_test_server->GetURL("/index.html")));
@@ -673,8 +670,10 @@ std::unique_ptr<net::test_server::HttpResponse> CustomHandleRequestForNoCookies(
   }
 
   const auto& iter = request.headers.find("Cookie");
-  if (iter != request.headers.end() || request.relative_url != "/manifest.json")
+  if (iter != request.headers.end() ||
+      request.relative_url != "/manifest.json") {
     return nullptr;
+  }
 
   std::unique_ptr<net::test_server::BasicHttpResponse> http_response(
       new net::test_server::BasicHttpResponse());
@@ -698,8 +697,7 @@ IN_PROC_BROWSER_TEST_F(ManifestBrowserTest, NoUseCredentialsNoCookies) {
   ASSERT_TRUE(custom_embedded_test_server->Start());
 
   ASSERT_TRUE(SetCookie(shell()->web_contents()->GetBrowserContext(),
-                        custom_embedded_test_server->base_url(),
-                        "foobar"));
+                        custom_embedded_test_server->base_url(), "foobar"));
 
   ASSERT_TRUE(NavigateToURL(
       shell(), custom_embedded_test_server->GetURL("/index.html")));
@@ -921,4 +919,4 @@ IN_PROC_BROWSER_TEST_F(ManifestFencedFrameBrowserTest,
   run_loop.Run();
 }
 
-} // namespace content
+}  // namespace content
