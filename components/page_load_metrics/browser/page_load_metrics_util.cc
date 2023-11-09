ab635881a10748a5dc28c33a01106af49409c533
Takashi Toyoshima <toyoshim@chromium.org>
2023-11-09 16:33:27 +0000

    Remove a stale metric, UserPerceivedPageVisit.PageVisitFinalStatus
    This was added by crrev.com/c/3225841, but we haven't registered
    this to the histograms.xml. As it meant the metric wasn't used
    until today, let's remove it now. Even removing this UMA, we have
    an equivalent UKM with the same name.
    
    Bug: n/a
    Change-Id: I04508e0b0ea6bc9bc20fc8e55a9a7c9ac3299cba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5007890
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Auto-Submit: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222326}

--- components/page_load_metrics/browser/page_load_metrics_util.cc
+++ components/page_load_metrics/browser/page_load_metrics_util.cc
@@ -365,8 +365,6 @@ PageVisitFinalStatus RecordPageVisitFinalStatusForTiming(
                             ? PageVisitFinalStatus::kReachedFCP
                             : PageVisitFinalStatus::kAborted;
   }
-  base::UmaHistogramEnumeration("UserPerceivedPageVisit.PageVisitFinalStatus",
-                                page_visit_status);
   ukm::builders::UserPerceivedPageVisit pageVisitBuilder(source_id);
   pageVisitBuilder.SetPageVisitFinalStatus(static_cast<int>(page_visit_status));
   pageVisitBuilder.Record(ukm::UkmRecorder::Get());
