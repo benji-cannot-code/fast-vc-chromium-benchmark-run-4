f02851a7542b66a56ba62686a73fbce114145a59
Jan Keitel <jkeitel@google.com>
2023-11-10 15:47:38 +0000

    Remove Android O version requirements from Android Autofill java classes.
    Low-Coverage-Reason: LARGE_SCALE_REFACTOR
    Bug: 1501199
    Change-Id: Ic18809c49c5d002ab4f8b6eacdc4824d556d54d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018481
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1222946}

--- components/android_autofill/browser/java/src/org/chromium/components/autofill/AutofillManagerWrapper.java
+++ components/android_autofill/browser/java/src/org/chromium/components/autofill/AutofillManagerWrapper.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.view.autofill.AutofillManager;
 import android.view.autofill.AutofillValue;
 
-import androidx.annotation.RequiresApi;
 import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.CollectionUtil;
@@ -26,7 +25,6 @@ import java.util.ArrayList;
 /**
  * The class to call Android's AutofillManager.
  */
-@RequiresApi(Build.VERSION_CODES.O)
 public class AutofillManagerWrapper {
     // Don't change TAG, it is used for runtime log.
     // NOTE: As a result of the above, the tag below still references the name of this class from
--- components/android_autofill/browser/test_support/java/src/org/chromium/components/autofill/AutofillProviderTestHelper.java
+++ components/android_autofill/browser/test_support/java/src/org/chromium/components/autofill/AutofillProviderTestHelper.java
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.autofill;
 
-import android.os.Build;
-
-import androidx.annotation.RequiresApi;
-
 import org.jni_zero.JNINamespace;
 import org.jni_zero.NativeMethods;
 
@@ -17,7 +13,6 @@ import org.chromium.content_public.browser.WebContents;
 /**
  * The help class for Autofill Provider test to access the native code.
  */
-@RequiresApi(Build.VERSION_CODES.O)
 @JNINamespace("autofill")
 public class AutofillProviderTestHelper {
     /**
