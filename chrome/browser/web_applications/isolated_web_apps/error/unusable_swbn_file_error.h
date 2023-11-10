431af1fd61023966df7c9f6d76e877e140984ead
Oleksandr Peletskyi <peletskyi@google.com>
2023-11-10 22:19:58 +0000

    iwa: Close the cached readers
    On uninstallation of the IWA we should close the opened signed web
    bundle before trying to delete it. This CL provides this functionality.
    
    Bug: b/287594073
    Test: added unittests
    Change-Id: I8e1624eb8aa16ae4620f7d46d71eb23d4a9b9d03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5017816
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Commit-Queue: Oleksandr Peletskyi <peletskyi@google.com>
    Auto-Submit: Oleksandr Peletskyi <peletskyi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1223163}

--- chrome/browser/web_applications/isolated_web_apps/error/unusable_swbn_file_error.h
+++ chrome/browser/web_applications/isolated_web_apps/error/unusable_swbn_file_error.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/types/expected.h"
 #include "components/web_package/mojom/web_bundle_parser.mojom.h"
 #include "components/web_package/signed_web_bundles/signed_web_bundle_signature_verifier.h"
 
