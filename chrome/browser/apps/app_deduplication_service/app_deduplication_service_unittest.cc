917773919c2fa5fb05d560b1e64145e4d58c19ed
Elitsa Bankova <elitsa@google.com>
2023-11-10 03:11:44 +0000

    Cleanup: Remove App Provisioning references from App Dedupe Service
    The deduplication service no longer uses the App Provisioning chrome
    component to download the data so removing the references.
    
    Bug: b/309882835
    Change-Id: I768ca95502c853061738a0b2f312a7430f7edd84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5013530
    Commit-Queue: Elitsa Bankova <elitsa@google.com>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Vivian Pao <vpao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1222720}

--- chrome/browser/apps/app_deduplication_service/app_deduplication_service_unittest.cc
+++ chrome/browser/apps/app_deduplication_service/app_deduplication_service_unittest.cc
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_deduplication_service/app_deduplication_server_connector.h"
 #include "chrome/browser/apps/app_deduplication_service/app_deduplication_service_factory.h"
 #include "chrome/browser/apps/app_deduplication_service/proto/deduplication_data.pb.h"
-#include "chrome/browser/apps/app_provisioning_service/app_provisioning_data_manager.h"
-#include "chrome/browser/apps/app_provisioning_service/proto/app_data.pb.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/common/chrome_features.h"
