4a218d4d6df73781f8b5cc765a05a0de7edb30eb
Denis Kuznetsov <antrim@chromium.org>
2023-11-10 11:49:33 +0000

    LoginDisplayHost: explicitly mark user's intent when showing GAIA screen
    Bug: b/291808450
    Change-Id: I781757389aa943344a73ad2e493ab53264db5e9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018116
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Reviewed-by: Elie Maamari <emaamari@google.com>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222860}

--- chrome/browser/ash/login/ui/login_display_host_mojo.cc
+++ chrome/browser/ash/login/ui/login_display_host_mojo.cc
@@ -562,6 +562,28 @@ void LoginDisplayHostMojo::OnBrowserCreated() {
 }
 
 void LoginDisplayHostMojo::ShowGaiaDialog(const AccountId& prefilled_account) {
+  GetWizardContext()->knowledge_factor_setup =
+      WizardContext::KnowledgeFactorSetup();
+
+  GetWizardContext()->knowledge_factor_setup.auth_setup_flow =
+      WizardContext::AuthChangeFlow::kInitialSetup;
+
+  ShowGaiaDialogImpl(prefilled_account);
+}
+
+void LoginDisplayHostMojo::StartUserRecovery(
+    const AccountId& account_to_recover) {
+  GetWizardContext()->knowledge_factor_setup =
+      WizardContext::KnowledgeFactorSetup();
+
+  GetWizardContext()->knowledge_factor_setup.auth_setup_flow =
+      WizardContext::AuthChangeFlow::kRecovery;
+
+  ShowGaiaDialogImpl(account_to_recover);
+}
+
+void LoginDisplayHostMojo::ShowGaiaDialogImpl(
+    const AccountId& prefilled_account) {
   EnsureOobeDialogLoaded();
   DCHECK(GetOobeUI());
 
@@ -576,11 +598,6 @@ void LoginDisplayHostMojo::ShowGaiaDialog(const AccountId& prefilled_account) {
   // Refresh wallpaper once OobeDialogState is propagated after showing the
   // dialog.
   UpdateWallpaper(prefilled_account);
-}
-
-void LoginDisplayHostMojo::StartUserRecovery(
-    const AccountId& account_to_recover) {
-  ShowGaiaDialog(account_to_recover);
 }
 
 void LoginDisplayHostMojo::ShowOsInstallScreen() {
