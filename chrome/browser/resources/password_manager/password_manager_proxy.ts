2cf7973c0cf3c78dea11e3c9fc492525639462b6
Takashi Sakamoto <tasak@google.com>
2023-11-10 01:30:03 +0000

    Revert "[SHv2] Fix dismissing notification on SH visit"
    This reverts commit 2dd3036390690bad02f96b694225de6f5bc93a68.
    
    Reason for revert: suspect SettingsTest.SafetyHubPage failure on linux-chromeos-chrome 
    
    First build failure:
    https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/37091/overview
    
    Original change's description:
    > [SHv2] Fix dismissing notification on SH visit
    >
    > This CL is a fix for not dismissing the menu notification when the user
    > visits the Safety Hub UI, or the Password Checkup page. This is needed
    > to avoid presenting void notifications to users. Additionally, the
    > correct command for the password module is used, which opens the checkup
    > page and not the general password manager.
    >
    > Bug: 1443466, 1500955
    > Change-Id: I12da53f5cfc2f80925f1cb89392b838f9ec4d345
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5011512
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Commit-Queue: Tom Van Goethem <tov@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    > Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1222278}
    
    Bug: 1443466, 1500955
    Change-Id: I04e7992725d6cddacf913a5f77428b22f325be02
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5017082
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1222677}

--- chrome/browser/resources/password_manager/password_manager_proxy.ts
+++ chrome/browser/resources/password_manager/password_manager_proxy.ts
@@ -378,9 +378,6 @@ export interface PasswordManagerProxy {
    * @param ids The ids for the password entries being moved.
    */
   movePasswordsToAccount(ids: number[]): void;
-
-  /** Dismiss the menu notifications for the Safety Hub password module. */
-  dismissSafetyHubPasswordMenuNotification(): void;
 }
 
 /**
@@ -604,10 +601,6 @@ export class PasswordManagerImpl implements PasswordManagerProxy {
 
   movePasswordsToAccount(ids: number[]) {
     chrome.passwordsPrivate.movePasswordsToAccount(ids);
-  }
-
-  dismissSafetyHubPasswordMenuNotification() {
-    chrome.send('dismissSafetyHubPasswordMenuNotification');
   }
 
   static getInstance(): PasswordManagerProxy {
