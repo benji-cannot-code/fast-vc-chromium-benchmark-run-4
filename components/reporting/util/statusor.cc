788a94bcefd7b93e878a1eb3671e020011eecdd1
Hong Xu <xuhong@chromium.org>
2023-11-10 22:04:31 +0000

    Clean up the params in ErrorStatus constructors
    Missed from CL:5002260
    
    Bug: b:308851937
    Change-Id: Ibaf11393752fc172e3cc9e1dba3fef5051b289e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5020410
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Auto-Submit: Hong Xu <xuhong@google.com>
    Commit-Queue: Hong Xu <xuhong@google.com>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223154}

--- components/reporting/util/statusor.cc
+++ components/reporting/util/statusor.cc
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace reporting::internal {
 ErrorStatus::ErrorStatus(const ErrorStatus&) = default;
-ErrorStatus& ErrorStatus::operator=(const ErrorStatus& x) = default;
+ErrorStatus& ErrorStatus::operator=(const ErrorStatus&) = default;
 ErrorStatus::ErrorStatus(ErrorStatus&&) = default;
-ErrorStatus& ErrorStatus::operator=(ErrorStatus&& x) = default;
+ErrorStatus& ErrorStatus::operator=(ErrorStatus&&) = default;
 ErrorStatus::~ErrorStatus() = default;
 
 ErrorStatus::ErrorStatus(const Status& status) : Status(status) {
@@ -23,5 +23,4 @@ ErrorStatus::ErrorStatus(const Status& status) : Status(status) {
 ErrorStatus::ErrorStatus(Status&& status) : Status(std::move(status)) {
   CHECK(!ok()) << "The status must not be OK";
 }
-
 }  // namespace reporting::internal
