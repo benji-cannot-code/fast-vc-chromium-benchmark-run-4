7ef3d4336eeaf3e46e9bfb9fe0ad8e94433ec272
Richard Chui <richui@chromium.org>
2023-11-10 03:05:28 +0000

    focus-mode: First time user flow
    This CL implements a different UX treatment for the first time user
    flow. This is signified by a user never having started a focus session
    before (and is kept track of by checking if the focus mode prefs have
    been set before.
    
    Changes include:
     - Updates to the quicksettings tile to funnel users to the panel.
     - The focus mode panel provides education text.
    
    Bug: b/297981998
    Test: manual, added 3 new tests
    Change-Id: Ic37c4d81bdffd622989fa29f2680be264ecec8bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5008995
    Commit-Queue: Richard Chui <richui@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222717}

--- ash/system/focus_mode/focus_mode_detailed_view_unittest.cc
+++ ash/system/focus_mode/focus_mode_detailed_view_unittest.cc
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/constants/ash_features.h"
+#include "ash/constants/ash_pref_names.h"
 #include "ash/shell.h"
+#include "ash/strings/grit/ash_strings.h"
 #include "ash/style/icon_button.h"
 #include "ash/style/pill_button.h"
 #include "ash/style/switch.h"
@@ -19,15 +21,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/model/system_tray_model.h"
 #include "ash/system/tray/fake_detailed_view_delegate.h"
 #include "ash/system/tray/hover_highlight_view.h"
-#include "ash/system/tray/tri_view.h"
 #include "ash/test/ash_test_base.h"
 #include "base/i18n/time_formatting.h"
-#include "base/strings/string_number_conversions.h"
-#include "base/strings/stringprintf.h"
-#include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/events/keycodes/keyboard_codes_posix.h"
 #include "ui/message_center/message_center.h"
 #include "ui/views/controls/label.h"
@@ -50,6 +49,12 @@ class FocusModeDetailedViewTest : public AshTestBase {
 
     widget_ = CreateFramelessTestWidget();
     widget_->SetFullscreen(true);
+
+    // Focus Mode considers it to be a first time user flow if
+    // `kFocusModeDoNotDisturb` has never been set by the user before. For
+    // normal feature testing purposes, we will intentionally set it so that the
+    // pref will not be marked as using the default value.
+    prefs()->SetBoolean(prefs::kFocusModeDoNotDisturb, true);
 
     CreateFakeFocusModeDetailedView();
   }
@@ -81,6 +86,10 @@ class FocusModeDetailedViewTest : public AshTestBase {
 
   views::Label* GetToggleRowSubLabel() {
     return focus_mode_detailed_view_->toggle_view_->sub_text_label();
+  }
+
+  bool IsToggleRowSubLabelVisible() {
+    return GetToggleRowSubLabel() && GetToggleRowSubLabel()->GetVisible();
   }
 
   PillButton* GetToggleRowButton() {
@@ -116,6 +125,10 @@ class FocusModeDetailedViewTest : public AshTestBase {
 
   views::Label* GetEndTimeLabel() {
     return focus_mode_detailed_view_->end_time_label_;
+  }
+
+  PrefService* prefs() {
+    return Shell::Get()->session_controller()->GetActivePrefService();
   }
 
   FakeDetailedViewDelegate detailed_view_delegate_;
@@ -232,8 +245,7 @@ TEST_F(FocusModeDetailedViewTest, ToggleRow) {
     EXPECT_EQ(active, focus_mode_controller->in_focus_session());
     EXPECT_EQ(active ? u"Focusing" : u"Focus", GetToggleRowLabel()->GetText());
 
-    EXPECT_EQ(active,
-              GetToggleRowSubLabel() && GetToggleRowSubLabel()->GetVisible());
+    EXPECT_EQ(active, IsToggleRowSubLabelVisible());
 
     if (active) {
       EXPECT_EQ(focus_mode_util::GetFormattedEndTimeString(
@@ -498,6 +510,31 @@ TEST_F(FocusModeDetailedViewTest, TimerViewVisibility) {
   EXPECT_EQ(focus_mode_util::GetFormattedEndTimeString(base::Time::Now() +
                                                        session_duration),
             GetEndTimeLabel()->GetText());
+}
+
+// Verify that the toggle row sublabel is shown in the first time user flow.
+TEST_F(FocusModeDetailedViewTest, FirstTimeUserFlow) {
+  // Clear `kFocusModeDoNotDisturb` to trigger the first time user flow.
+  prefs()->ClearPref(prefs::kFocusModeDoNotDisturb);
+
+  // Recreate the detailed view so that the UI is updated after we set the user
+  // pref.
+  CreateFakeFocusModeDetailedView();
+
+  // Verify that the first time user flow text is displayed.
+  EXPECT_TRUE(IsToggleRowSubLabelVisible());
+  EXPECT_EQ(GetToggleRowSubLabel()->GetText(),
+            l10n_util::GetStringUTF16(
+                IDS_ASH_STATUS_TRAY_FOCUS_MODE_FIRST_TIME_SUBLABEL));
+
+  // Start and stop a focus session. This puts us back into the focus panel
+  // outside of the first time user flow.
+  LeftClickOn(GetToggleRowButton());
+  CreateFakeFocusModeDetailedView();
+  LeftClickOn(GetToggleRowButton());
+
+  // Verify that the first time user flow text no longer is displayed.
+  EXPECT_FALSE(IsToggleRowSubLabelVisible());
 }
 
 }  // namespace ash
--- ash/system/focus_mode/focus_mode_feature_pod_controller.cc
+++ ash/system/focus_mode/focus_mode_feature_pod_controller.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/system/focus_mode/focus_mode_controller.h"
 #include "ash/system/focus_mode/focus_mode_util.h"
-#include "ash/system/unified/feature_pod_button.h"
 #include "ash/system/unified/feature_tile.h"
 #include "ash/system/unified/unified_system_tray_controller.h"
 #include "base/check.h"
@@ -66,6 +65,16 @@ QsFeatureCatalogName FocusModeFeaturePodController::GetCatalogName() {
 
 void FocusModeFeaturePodController::OnIconPressed() {
   auto* controller = FocusModeController::Get();
+
+  // As part of the first time user flow, if the user has never started a
+  // session before, we want to direct them to the focus panel so they can get
+  // more context and change focus settings instead of starting a session
+  // immediately.
+  if (!controller->HasStartedSessionBefore()) {
+    OnLabelPressed();
+    return;
+  }
+
   TrackToggleUMA(/*target_toggle_state=*/!controller->in_focus_session());
   controller->ToggleFocusMode();
 }
@@ -96,6 +105,14 @@ void FocusModeFeaturePodController::UpdateUI() {
   tile_->SetLabel(label_text);
   tile_->SetIconButtonTooltipText(label_text);
   tile_->SetTooltipText(label_text);
+
+  // As part of the first time user flow, if the user has never started a
+  // session before, we hide the session duration sublabel since they are not
+  // able to start a focus session immediately anyway.
+  if (!controller->HasStartedSessionBefore()) {
+    tile_->SetSubLabelVisibility(false);
+    return;
+  }
 
   const base::TimeDelta session_duration_remaining =
       in_focus_session ? controller->end_time() - base::Time::Now()
