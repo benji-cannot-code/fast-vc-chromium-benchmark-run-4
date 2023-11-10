04c6ee2a87a6ecad23b782213a096a17ca7a152a
Joshua Pawlicki <waffles@chromium.org>
2023-11-10 16:01:58 +0000

    Updater: Provide keychain path to pkgbuild.
    pkgbuild is not finding the right key.
    
    Bug: 1500694
    Change-Id: I73611f4b57934aa395c0eef14a6364b87b5e802c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018809
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Noah Rose Ledesma <noahrose@google.com>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222957}

--- chrome/updater/mac/signing/pipeline.py
+++ chrome/updater/mac/signing/pipeline.py
@@ -112,6 +112,8 @@ def _package_and_sign_pkg(paths, config):
         os.path.join(paths.input, config.packaging_dir, 'signing'),
         '--sign',
         config.installer_identity,
+        '--keychain',
+        os.environ["INSTALLER_KEYCHAIN"],
         '--timestamp',
         pkg_path,
     ])
