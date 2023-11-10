14b9c6789381633620a14edf863fa2902eb64f1b
Daniel Andersson <dandersson@chromium.org>
2023-11-10 23:28:12 +0000

    desk_profiles: Change CreateNewDeskForSavedDesk to return non-const
    This function is called when creating a new desk that will be populated
    with windows from a saved desk. For desk profiles Save & Recall, we will
    need to set the lacros profile ID of a newly created desk. Rather than
    adding this rather niche parameter to the function, the function can
    just return non-const and let the caller modify the new desk.
    
    Also fixed an innocuous bug where the lacros profile ID prefs for a new
    desk was not saved. With the way the code was written, the desk would in
    any case have the right lacros profile ID when restored (0), but we
    might as well make sure that this is also saved.
    
    BUG=b:308823936
    
    Change-Id: Iefc7b926d2b8fbeecf189eeb783891029fb61d6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5020414
    Commit-Queue: Daniel Andersson <dandersson@chromium.org>
    Reviewed-by: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223194}

--- ash/wm/desks/desks_controller.cc
+++ ash/wm/desks/desks_controller.cc
@@ -638,6 +638,7 @@ void DesksController::NewDesk(DesksCreationRemovalSource source) {
     }
     desks_restore_util::UpdatePrimaryUserDeskGuidsPrefs();
     desks_restore_util::UpdatePrimaryUserDeskNamesPrefs();
+    desks_restore_util::UpdatePrimaryUserDeskLacrosProfileIdPrefs();
     desks_restore_util::UpdatePrimaryUserDeskMetricsPrefs();
     UMA_HISTOGRAM_ENUMERATION(kNewDeskHistogramName, source);
     ReportDesksCountHistogram();
@@ -1229,7 +1230,7 @@ void DesksController::CaptureActiveDeskAsSavedDesk(
       current_account_id_);
 }
 
-const Desk* DesksController::CreateNewDeskForSavedDesk(
+Desk* DesksController::CreateNewDeskForSavedDesk(
     DeskTemplateType template_type,
     const std::u16string& customized_desk_name) {
   DCHECK(CanCreateDesks());
