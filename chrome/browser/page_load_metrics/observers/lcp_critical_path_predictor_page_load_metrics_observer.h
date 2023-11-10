2ec59ef93287fc055631b5522e2b765390253026
Yoichi Osato <yoichio@chromium.org>
2023-11-10 05:05:15 +0000

    [LCPP] Improve TimingPredictor with multiple locators.
    This CL updates the LCP timing predictor to use multiple locator
    rather than only top 1.
    In renderer, the predictor decides the LCP is the candidate if it is in
    set_lcp_element_locators.
    This CL also updates the browser observer UMA report to judge if its
    prediction is collect.
    
    Bug: 1493255
    Change-Id: I3f06d10cfd0fc45340fdb558a6fd3eeb0a741a79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5004479
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222745}

--- chrome/browser/page_load_metrics/observers/lcp_critical_path_predictor_page_load_metrics_observer.h
+++ chrome/browser/page_load_metrics/observers/lcp_critical_path_predictor_page_load_metrics_observer.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_PAGE_LOAD_METRICS_OBSERVERS_LCP_CRITICAL_PATH_PREDICTOR_PAGE_LOAD_METRICS_OBSERVER_H_
 #define CHROME_BROWSER_PAGE_LOAD_METRICS_OBSERVERS_LCP_CRITICAL_PATH_PREDICTOR_PAGE_LOAD_METRICS_OBSERVER_H_
 
+#include <vector>
+
 #include "chrome/browser/predictors/lcp_critical_path_predictor/lcp_critical_path_predictor_util.h"
 #include "components/page_load_metrics/browser/page_load_metrics_observer.h"
 #include "content/public/browser/page_user_data.h"
@@ -57,7 +59,7 @@ class LcpCriticalPathPredictorPageLoadMetricsObserver
   ~LcpCriticalPathPredictorPageLoadMetricsObserver() override;
 
   void SetLcpElementLocator(const std::string& lcp_element_locator,
-                            bool is_predicted);
+                            absl::optional<uint32_t> predicted_lcp_index);
   void SetLcpInfluencerScriptUrls(
       const std::vector<GURL>& lcp_influencer_scripts);
   // Append fetched font URLs to the list to be passed to LCPP.
@@ -96,9 +98,11 @@ class LcpCriticalPathPredictorPageLoadMetricsObserver
 
   absl::optional<predictors::LcppDataInputs> lcpp_data_inputs_;
 
-  // True iff one of `lcp_element_locator` via SetLcpElementLocator was
-  // predicted as true LCP.
-  bool lcp_timing_was_predicted_ = false;
+  // Prediction result. This keeps SetLcpElementLocator's second argument.
+  // `predicted_lcp_index` is predicted index of `lcp_element_locators` in
+  // LCPCriticalPathPredictorNavigationTimeHint.
+  // absl::nullopt value means the LCP didn't hit any of `lcp_element_locators`.
+  std::vector<absl::optional<uint32_t>> predicted_lcp_indexes_;
 
   base::WeakPtrFactory<LcpCriticalPathPredictorPageLoadMetricsObserver>
       weak_factory_{this};
