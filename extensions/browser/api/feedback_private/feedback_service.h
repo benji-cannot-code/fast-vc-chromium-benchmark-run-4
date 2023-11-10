198d3996ac21cdb920af3f5274e75a06884a8d56
xiangdong kong <xiangdongkong@google.com>
2023-11-10 21:47:39 +0000

    feedback: Attach wifi debug logs to reports
    - The attachment name is "iwlwifi_firmware_dumps.bz2".
    - The expected file path is "/var/log/wifi/iwlwifi_firmware_dumps.bz2".
    
    It will be added if send_wifi_debug_logs is true which is possible only
    when the UserFeedbackWithLowLevelDebugDataAllowed policy includes one of
    the following values:
    - "all" or
    - "wifi".
    
    Bug: b/308196190
    Test: extensions_unittests --gtest_filter=FeedbackServiceTest.*
    Change-Id: I619dea32cc9a755a70b0f9566c42fd8686ed5058
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018998
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1223144}

--- extensions/browser/api/feedback_private/feedback_service.h
+++ extensions/browser/api/feedback_private/feedback_service.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef EXTENSIONS_BROWSER_API_FEEDBACK_PRIVATE_FEEDBACK_SERVICE_H_
 #define EXTENSIONS_BROWSER_API_FEEDBACK_PRIVATE_FEEDBACK_SERVICE_H_
 
+#include "base/files/file_path.h"
 #include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
@@ -65,6 +66,9 @@ class FeedbackService : public base::RefCountedThreadSafe<FeedbackService> {
       SendFeedbackCallback callback);
 
   FeedbackPrivateDelegate* GetFeedbackPrivateDelegate() { return delegate_; }
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  void SetLogFilesRootPathForTesting(const base::FilePath& log_file_root);
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
  protected:
   virtual ~FeedbackService();
@@ -103,6 +107,8 @@ class FeedbackService : public base::RefCountedThreadSafe<FeedbackService> {
       const FeedbackParams& params,
       scoped_refptr<feedback::FeedbackData> feedback_data,
       const std::string& compressed_histograms);
+  // Root file path for log files. It can be overwritten for testing purpose.
+  base::FilePath log_file_root_{FILE_PATH_LITERAL("/var/log/")};
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
   void OnAllLogsFetched(const FeedbackParams& params,
                         scoped_refptr<feedback::FeedbackData> feedback_data);
