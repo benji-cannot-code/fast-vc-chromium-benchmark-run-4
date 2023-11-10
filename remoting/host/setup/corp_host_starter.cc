b4fb3106032a4e9800b71871432ca920d3a4e334
Joe Downing <joedow@google.com>
2023-11-10 20:09:57 +0000

    Add some new Result values to HostStarter
    This CL adds some new error codes to the HostStarter Result enum
    so we can provide better error messaging when a failure occurs.
    This CL also runs the on_done callback when we fail to start a
    corp host during the provisioning process.
    
    Bug: b:297857922
    Change-Id: Ib0051b52d56700ac20dab0d71b3628aa7462e284
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5020986
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223098}

--- remoting/host/setup/corp_host_starter.cc
+++ remoting/host/setup/corp_host_starter.cc
@@ -222,7 +222,7 @@ void CorpHostStarter::OnProvisionCorpMachineResponse(
   authorization_code_ = internal::GetAuthorizationCode(*response);
   if (authorization_code_.empty()) {
     LOG(ERROR) << "No authorization code returned by the Directory.";
-    std::move(on_done_).Run(START_ERROR);
+    std::move(on_done_).Run(REGISTRATION_ERROR);
     return;
   }
 
@@ -305,8 +305,7 @@ void CorpHostStarter::OnHostStarted(DaemonController::AsyncResult result) {
     LOG(ERROR) << "Failed to start host: " << result;
     // TODO(joedow): Decide whether to delete the host instance or update its
     // offline reason in the Directory instead.
-    // TODO(joedow): Check to see if we need to run on_done here. If so, also
-    // check to see if the OAuth-based HostStarter needs that change as well.
+    std::move(on_done_).Run(START_ERROR);
     return;
   }
   std::move(on_done_).Run(START_COMPLETE);
@@ -353,9 +352,10 @@ void CorpHostStarter::NotifyError(const ProtobufHttpStatus& status) {
     }
   }
 
-  // TODO(joedow): Add more error codes to HostStarter and use them here.
   switch (error_code) {
     case ProtobufHttpStatus::Code::PERMISSION_DENIED:
+      std::move(on_done_).Run(PERMISSION_DENIED);
+      return;
     case ProtobufHttpStatus::Code::UNAUTHENTICATED:
       std::move(on_done_).Run(OAUTH_ERROR);
       return;
