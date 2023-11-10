34eae79294b6c6148cae2b75d8f7925214f54875
Aya ElAttar <ayaelattar@chromium.org>
2023-11-10 16:21:19 +0000

    DLP: Show reporting events in reporting tab
    Show reporting events in a table in the reporting tab
    in chrome://dlp-internals
    
    Bug: b/307248322
    Change-Id: Ie4c763e199f94bf0a9e4fdacd3f4fe0e36afba93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5017904
    Auto-Submit: Aya Elsayed <ayaelattar@chromium.org>
    Commit-Queue: Luca Accorsi <accorsi@google.com>
    Reviewed-by: Luca Accorsi <accorsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1222963}

---
+++ chrome/browser/resources/dlp_internals/dlp_utils.ts
@@ -0,0 +1,53 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {DlpEvent_Mode, DlpEvent_Restriction, DlpEvent_UserType, EndpointType, EventDestination_Component} from './dlp_internals.mojom-webui.js';
+
+export const EndpointTypeMap = {
+  [EndpointType.kDefault]: 'Default',
+  [EndpointType.kUrl]: 'URL',
+  [EndpointType.kClipboardHistory]: 'Clipboard History',
+  [EndpointType.kUnknownVm]: 'Unknown VM',
+  [EndpointType.kArc]: 'Arc',
+  [EndpointType.kBorealis]: 'Borealis',
+  [EndpointType.kCrostini]: 'Crostini',
+  [EndpointType.kPluginVm]: 'Plugin VM',
+  [EndpointType.kLacros]: 'Lacros',
+};
+
+export const DestinationComponentMap = {
+  [EventDestination_Component.kUndefinedComponent]: 'Undefined Component',
+  [EventDestination_Component.kArc]: 'ARC',
+  [EventDestination_Component.kCrostini]: 'Crostini',
+  [EventDestination_Component.kPluginVm]: 'PluginVm',
+  [EventDestination_Component.kUsb]: 'USB',
+  [EventDestination_Component.kDrive]: 'GoogleDrive',
+  [EventDestination_Component.kOnedrive]: 'OneDrive',
+};
+
+export const EventRestrictionMap = {
+  [DlpEvent_Restriction.kUndefinedRestriction]: 'Undefined Restriction',
+  [DlpEvent_Restriction.kClipboard]: 'Clipboard',
+  [DlpEvent_Restriction.kScreenshot]: 'Screenshot',
+  [DlpEvent_Restriction.kScreencast]: 'Screencast',
+  [DlpEvent_Restriction.kPrinting]: 'Printing',
+  [DlpEvent_Restriction.kEprivacy]: 'ePrivacy',
+  [DlpEvent_Restriction.kFiles]: 'Files',
+};
+
+export const EventModeMap = {
+  [DlpEvent_Mode.kUndefinedMode]: 'Undefined Mode',
+  [DlpEvent_Mode.kBlock]: 'Block',
+  [DlpEvent_Mode.kReport]: 'Report',
+  [DlpEvent_Mode.kWarn]: 'Warning',
+  [DlpEvent_Mode.kWarnProceed]: 'Warning Proceeded',
+};
+
+export const EventUserTypeMap = {
+  [DlpEvent_UserType.kUndefinedUserType]: 'Undefined User',
+  [DlpEvent_UserType.kRegular]: 'Regular',
+  [DlpEvent_UserType.kManagedGuest]: 'Managed Guest',
+  [DlpEvent_UserType.kKiosk]: 'Kiosk',
+};
