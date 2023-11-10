66751d1b1c4e8d6270a34cf605667bd1dad90da8
Xiaochen Zhou <xiaochenzh@chromium.org>
2023-11-10 21:57:58 +0000

    [Privacy Sandbox] Fix race conditions in attestations parsing.
    The attestations_parse_progress_, file_version_, and attestations_map_
    are accessed by both UI thread and thread pool. This CL changes to use
    the `PostTaskAndReplyWithResult` pattern to to remove undefined
    behavior due to data races.
    
    This should also fix the other issue that the parsing progress is
    marked as finished before the final assignment of the attestations map
    in UI thread. When an attestations query takes place at this moment,
    the metrics will record the file is corrupted because it sees the
    progress is finished but the attestations map is not there.
    
    Note chrome component updater's `ComponentReady` always runs on the UI
    thread.
    
    Bug: 1498498
    Change-Id: I376849599f8980d82cc01e8b4bfc95dab321318c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994136
    Commit-Queue: Xiaochen Zhou <xiaochenzh@chromium.org>
    Reviewed-by: Garrett Tanzer <gtanzer@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223149}

--- components/privacy_sandbox/privacy_sandbox_attestations/privacy_sandbox_attestations_unittest.cc
+++ components/privacy_sandbox/privacy_sandbox_attestations/privacy_sandbox_attestations_unittest.cc
@@ -461,8 +461,6 @@ TEST_F(PrivacySandboxAttestationsFeatureEnabledTest, LoadAttestationsFile) {
                 net::SchemefulSite(GURL(site)),
                 PrivacySandboxAttestationsGatedAPI::kProtectedAudience),
             Status::kAllowed);
-
-  base::RunLoop().RunUntilIdle();
 }
 
 TEST_F(PrivacySandboxAttestationsFeatureEnabledTest,
