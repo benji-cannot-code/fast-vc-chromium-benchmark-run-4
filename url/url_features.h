fb09b04b5c1d30bbecc06ab3a5ca4d8d6699b66c
Hayato Ito <hayato@chromium.org>
2023-11-10 01:35:55 +0000

    Add a feature flag for standard-compliant non-special scheme URL parsing
    This CL is separate from the main CL (https://crrev.com/c/4929491).
    As a first step, this CL adds a URL feature flag and a virtual test with
    it.
    
    Bug: 1416006
    Change-Id: I12533902bffff2e74bd48b98833c1dd13aeddf5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5015730
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222683}

--- url/url_features.h
+++ url/url_features.h
@@ -23,6 +23,10 @@ COMPONENT_EXPORT(URL) bool IsRecordingIDNA2008Metrics();
 // See url::kStandardCompliantHostCharacters for details.
 COMPONENT_EXPORT(URL) bool IsUsingStandardCompliantHostCharacters();
 
+// Returns true if kStandardCompliantNonSpecialSchemeURLParsing feature is
+// enabled. See url::kStandardCompliantNonSpecialSchemeURLParsing for details.
+COMPONENT_EXPORT(URL) bool IsUsingStandardCompliantNonSpecialSchemeURLParsing();
+
 // When enabled, allows resolving of a bare fragment containing a colon against
 // a non-hierarchical URL. (For example '#foo:bar' against 'about:blank'.)
 COMPONENT_EXPORT(URL)
@@ -33,6 +37,11 @@ BASE_DECLARE_FEATURE(kResolveBareFragmentWithColonOnNonHierarchical);
 // https://crbug.com/1416013 for details.
 COMPONENT_EXPORT(URL)
 BASE_DECLARE_FEATURE(kStandardCompliantHostCharacters);
+
+// When enabled, Chrome uses standard-compliant URL parsing for non-special
+// scheme URLs. See https://crbug.com/1416006 for details.
+COMPONENT_EXPORT(URL)
+BASE_DECLARE_FEATURE(kStandardCompliantNonSpecialSchemeURLParsing);
 
 }  // namespace url
 
