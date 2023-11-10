17434b6cffa4c192d54efbd80ab49e86e1402c5d
David Pennington <dpenning@chromium.org>
2023-11-10 03:56:18 +0000

    remove non http/https urls from organizing
    Change-Id: I8d359cacbba530468eaa2e31e5158cf7ae7afd2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018468
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Auto-Submit: David Pennington <dpenning@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222730}

--- chrome/browser/ui/tabs/organization/tab_organization_service_unittest.cc
+++ chrome/browser/ui/tabs/organization/tab_organization_service_unittest.cc
@@ -21,6 +21,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/page_transition_types.h"
 
+namespace {
+
+constexpr char kValidURL[] = "http://zombo.com";
+constexpr char kInvalidURL[] = "chrome://page";
+
+}  // anonymous namespace
+
 class TabOrganizationServiceTest : public BrowserWithTestWindowTest {
  public:
   TabOrganizationServiceTest() = default;
@@ -42,6 +49,8 @@ class TabOrganizationServiceTest : public BrowserWithTestWindowTest {
     std::unique_ptr<content::WebContents> web_contents =
         content::WebContentsTester::CreateTestWebContents(profile_.get(),
                                                           nullptr);
+    content::WebContentsTester::For(web_contents.get())
+        ->NavigateAndCommit(GURL(kValidURL));
 
     content::WebContents* web_contents_ptr = web_contents.get();
 
@@ -156,6 +165,36 @@ TEST_F(TabOrganizationServiceTest, SessionFromBrowserPopulatesRequest) {
   std::unique_ptr<TabOrganizationSession> session =
       TabOrganizationSession::CreateSessionForBrowser(browser1, service());
   EXPECT_EQ(session->request()->tab_datas().size(), 4u);
+
+  session->StartRequest();
+  EXPECT_NE(session->request()->response(), nullptr);
+  EXPECT_EQ(session->tab_organizations().size(), 1u);
+
+  TabOrganization* organization = session->GetNextTabOrganization();
+  ASSERT_TRUE(organization);
+
+  organization->Accept();
+  EXPECT_EQ(browser1->tab_strip_model()->group_model()->ListTabGroups().size(),
+            1u);
+}
+
+TEST_F(TabOrganizationServiceTest,
+       TabOrganizationSessionCreateSessionForBrowserNoInvalidTabDatas) {
+  const int valid_tab_count = 2;
+  Browser* browser1 = AddBrowser();
+  for (int i = 0; i < valid_tab_count; i++) {
+    AddTabToBrowser(browser1, 0);
+  }
+
+  // Add an invalid tab.
+  content::WebContents* invalid_web_contents = AddTabToBrowser(browser1, 0);
+  content::WebContentsTester::For(invalid_web_contents)
+      ->NavigateAndCommit(GURL(kInvalidURL));
+
+  std::unique_ptr<TabOrganizationSession> session =
+      TabOrganizationSession::CreateSessionForBrowser(browser1, service());
+  EXPECT_EQ(static_cast<int>(session->request()->tab_datas().size()),
+            valid_tab_count);
 
   session->StartRequest();
   EXPECT_NE(session->request()->response(), nullptr);
