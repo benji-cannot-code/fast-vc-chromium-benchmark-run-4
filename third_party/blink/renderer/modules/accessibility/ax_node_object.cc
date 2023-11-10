7927f4b6171773a5fa958be5a8d947138a7260ef
Joone Hur <joone@chromium.org>
2023-11-10 00:40:50 +0000

    [A11y] Ensure aside tag has complementary role with role="complementary"
    When the aside element explicitly has the role="complementary",
    it should retain this role regardless of whether it's nested within
    article, nav, section, or another complementary element.
    
    Bug: None
    Test: All/DumpAccessibilityTreeTest.AccessibilityLandmark/*
    Change-Id: Ia2a4f317d054a20755b0a1ab3af4c99bdc4a5232
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4999348
    Commit-Queue: Joone Hur <joone@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222655}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -878,6 +878,10 @@ bool AXNodeObject::IsDescendantOfLandmarkDisallowedElement() const {
   if (!GetNode())
     return false;
 
+  if (AriaRoleAttribute() == ax::mojom::blink::Role::kComplementary) {
+    return false;
+  }
+
   auto role_names = GetLandmarkIsNotAllowedAncestorRoles(RoleValue());
 
   for (AXObject* parent = ParentObjectUnignored(); parent;
