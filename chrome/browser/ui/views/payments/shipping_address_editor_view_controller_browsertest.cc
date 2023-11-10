6b29316e1d8d6465298f05e70a915690b6166b3b
Norge Vizcay <vizcay@google.com>
2023-11-10 16:05:24 +0000

    Avoid default initialization of profile in shipping editor.
    This CL is part of our efforts of making the AutofillProfile require a
    country code during construction (e.g. by removing the default
    constructor). The CL avoids the default initialization in shipping_address_editor_view_controller.cc
    
    Bug: 1464568
    Change-Id: I3a9d888233de570195d048499718d0f2a317bfc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5017901
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Auto-Submit: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222958}

--- chrome/browser/ui/views/payments/shipping_address_editor_view_controller_browsertest.cc
+++ chrome/browser/ui/views/payments/shipping_address_editor_view_controller_browsertest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/payments/payment_request_dialog_view_ids.h"
 #include "chrome/browser/ui/views/payments/validating_textfield.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
+#include "components/autofill/core/browser/country_type.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/autofill/core/browser/geo/autofill_country.h"
 #include "components/autofill/core/browser/geo/test_region_data_loader.h"
@@ -543,11 +544,9 @@ IN_PROC_BROWSER_TEST_F(DISABLED_PaymentRequestShippingAddressEditorTest,
                        SelectingIncompleteAddress) {
   NavigateTo("/payment_request_dynamic_shipping_test.html");
   // Add incomplete address.
-  autofill::AutofillProfile profile;
+  autofill::AutofillProfile profile(
+      AddressCountryCode(base::UTF16ToUTF8(kCountryWithoutStatesCode)));
   profile.SetInfo(autofill::NAME_FULL, kNameFull, kLocale);
-  // Also set non-default country, to make sure proper fields will be used.
-  profile.SetInfo(autofill::ADDRESS_HOME_COUNTRY, kCountryWithoutStates,
-                  kLocale);
   AddAutofillProfile(profile);
 
   InvokePaymentRequestUI();
@@ -650,7 +649,8 @@ IN_PROC_BROWSER_TEST_F(DISABLED_PaymentRequestShippingAddressEditorTest,
                        MAYBE_FocusFirstInvalidField_NotName) {
   NavigateTo("/payment_request_dynamic_shipping_test.html");
   // Add address with only the name set, so that another view takes focus.
-  autofill::AutofillProfile profile;
+  autofill::AutofillProfile profile(
+      autofill::i18n_model_definition::kLegacyHierarchyCountryCode);
   profile.SetInfo(autofill::NAME_FULL, kNameFull, "fr_CA");
   AddAutofillProfile(profile);
 
