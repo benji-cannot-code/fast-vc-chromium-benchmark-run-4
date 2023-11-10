9b42599f31c2aaf04c5e30a8a7b9cc139bfffe0b
Daniel Andersson <dandersson@chromium.org>
2023-11-10 23:50:44 +0000

    desk_profiles: Detect the profile summary of the primary user
    This CL adds code to detect which of the profile summaries that are
    received from lacros represent the primary user. It does this by
    checking if the email of a LacrosProfileSummary matches the one in the
    primary user's session.
    
    For now, the profile id is simply stored in DeskProfilesAsh. It will
    later be used when clients query for information of the primary user
    profile.
    
    BUG=b:309694734
    
    Change-Id: I058b4d4c19614d92ed20ea69e28d436dceb46a68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5019436
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Commit-Queue: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223209}

--- chrome/browser/ash/crosapi/desk_profiles_ash.cc
+++ chrome/browser/ash/crosapi/desk_profiles_ash.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "ash/session/session_controller_impl.h"
+#include "ash/shell.h"
 #include "base/containers/cxx20_erase_vector.h"
 #include "base/ranges/algorithm.h"
 
@@ -36,8 +38,24 @@ void DeskProfilesAsh::BindReceiver(
 
 void DeskProfilesAsh::OnProfileUpsert(
     std::vector<mojom::LacrosProfileSummaryPtr> profiles) {
+  // Get the email of the primary user. This is done to figure out which summary
+  // that we receive from lacros represents the primary user. We only need to do
+  // this once.
+  std::string primary_user_email;
+  if (primary_user_profile_id_ == 0 && ash::Shell::HasInstance()) {
+    if (const auto* session =
+            ash::Shell::Get()->session_controller()->GetPrimaryUserSession()) {
+      primary_user_email = session->user_info.account_id.GetUserEmail();
+    }
+  }
+
   for (auto& prof : profiles) {
-    auto& entry = UpsertProfile(ConvertProfileSummary(std::move(prof)));
+    ash::LacrosProfileSummary summary = ConvertProfileSummary(std::move(prof));
+    if (!primary_user_email.empty() && primary_user_email == summary.email) {
+      primary_user_profile_id_ = summary.profile_id;
+    }
+
+    auto& entry = UpsertProfile(std::move(summary));
     for (auto& observer : observers_) {
       observer.OnProfileUpsert(entry);
     }
