85ee1c948a5cb614a695ce3711955459f095bb45
Yongshun Liu <yongshun@chromium.org>
2023-11-10 20:12:16 +0000

    ash: Remove non-jelly paths under saved desks
    This removes non-jelly paths under saved desks.
    
    Bug: b/308012781
    Test: Manual
    Change-Id: Id5fefc0fd359531df894830529bab286ac09096c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5003483
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223102}

--- ash/wm/desks/templates/saved_desk_save_desk_button.cc
+++ ash/wm/desks/templates/saved_desk_save_desk_button.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/style_util.h"
 #include "ash/wm/desks/templates/saved_desk_constants.h"
 #include "ash/wm/overview/overview_utils.h"
-#include "chromeos/constants/chromeos_features.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/gfx/vector_icon_types.h"
 #include "ui/views/controls/focus_ring.h"
@@ -40,9 +39,7 @@ SavedDeskSaveDeskButton::SavedDeskSaveDeskButton(
 
   SetBorder(std::make_unique<views::HighlightBorder>(
       kSaveDeskCornerRadius,
-      chromeos::features::IsJellyrollEnabled()
-          ? views::HighlightBorder::Type::kHighlightBorderNoShadow
-          : views::HighlightBorder::Type::kHighlightBorder2));
+      views::HighlightBorder::Type::kHighlightBorderNoShadow));
 
   SetEnableBackgroundBlur(true);
 }
