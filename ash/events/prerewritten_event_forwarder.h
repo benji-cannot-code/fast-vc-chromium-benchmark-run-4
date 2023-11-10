a0b770064b3b0dcf315065cc924bf9ac5e0f7c95
Jimmy <jimmyxgong@chromium.org>
2023-11-10 22:42:28 +0000

    shortcuts: Add OnPrerewriteKeyInputEvent to PrewrittenEventForwarder
    OnPrerewriteKeyInputEvent notifies clients of the initial key event
    before it gets processed by other rewriters.
    
    Bug: b/308843852, b/216049298
    Test: ash_unittests
    Change-Id: I3d2a26d3c8b5c84fb969a13f0d7003f017b46c2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5002247
    Reviewed-by: David Padlipsky <dpad@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223170}

--- ash/events/prerewritten_event_forwarder.h
+++ ash/events/prerewritten_event_forwarder.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_EVENTS_PREREWRITTEN_EVENT_FORWARDER_H_
 
 #include "ash/ash_export.h"
+#include "base/observer_list.h"
 #include "ui/events/event.h"
 #include "ui/events/event_rewriter.h"
 
@@ -16,6 +17,12 @@ namespace ash {
 // to clients that want events before any subsequent event rewrites.
 class ASH_EXPORT PrerewrittenEventForwarder : public ui::EventRewriter {
  public:
+  class Observer : public base::CheckedObserver {
+   public:
+    // Called whenever a key event is pressed/released.
+    virtual void OnPrerewriteKeyInputEvent(const ui::KeyEvent& event) = 0;
+  };
+
   PrerewrittenEventForwarder();
   PrerewrittenEventForwarder(const PrerewrittenEventForwarder&) = delete;
   PrerewrittenEventForwarder& operator=(const PrerewrittenEventForwarder&) =
@@ -26,6 +33,12 @@ class ASH_EXPORT PrerewrittenEventForwarder : public ui::EventRewriter {
   ui::EventDispatchDetails RewriteEvent(
       const ui::Event& event,
       const Continuation continuation) override;
+
+  void AddObserver(Observer* observer);
+  void RemoveObserver(Observer* observer);
+
+ private:
+  base::ObserverList<Observer> observers_;
 };
 
 }  // namespace ash
