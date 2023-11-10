49dec4c0f30b60d2a1a69175353609b7c57ad828
Aishwarya Rajesh <aishwaryarj@google.com>
2023-11-10 00:00:38 +0000

    Add ChromePureJavaExceptionReporter report to examine intents received in ChromeTabbedActivity
    Bug: 1484026
    Change-Id: I4511955b6810c687287649e20d783ae32ea1b3c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5002698
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1222638}

--- chrome/android/java/src/org/chromium/chrome/browser/LaunchIntentDispatcher.java
+++ chrome/android/java/src/org/chromium/chrome/browser/LaunchIntentDispatcher.java
@@ -248,6 +248,10 @@ public class LaunchIntentDispatcher {
      */
     public static boolean isCustomTabIntent(Intent intent) {
         if (intent == null) return false;
+        Log.w(
+                TAG,
+                "CustomTabsIntent#shouldAlwaysUseBrowserUI() = "
+                        + CustomTabsIntent.shouldAlwaysUseBrowserUI(intent));
         if (CustomTabsIntent.shouldAlwaysUseBrowserUI(intent)
                 || !intent.hasExtra(CustomTabsIntent.EXTRA_SESSION)) {
             return false;
