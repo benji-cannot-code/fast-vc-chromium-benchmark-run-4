d067ec7f7c1bbd01e961d15e3423c57833810fe5
Stephen White <senorblanco@chromium.org>
2023-11-10 22:07:15 +0000

    WebGPU GL: Add RenderAttachment texture usage.
    Add RenderAttachment internal texture usage to DawnGLTextureRepresentation.
    
    This allows transfers to offscreen imageBitmap with alphaMode="opaque"
    to work correctly.
    
    Bug: dawn:2187
    Change-Id: Ice4d5db55bae9c8e49bd8f176d041532a2f86968
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5020359
    Commit-Queue: Stephen White <senorblanco@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223156}

--- gpu/command_buffer/service/shared_image/dawn_gl_texture_representation.cc
+++ gpu/command_buffer/service/shared_image/dawn_gl_texture_representation.cc
@@ -52,7 +52,8 @@ wgpu::Texture DawnGLTextureRepresentation::BeginAccess(
 
   // Add internal TextureBinding usage for copyTextureForBrowser().
   wgpu::DawnTextureInternalUsageDescriptor internalDesc;
-  internalDesc.internalUsage = wgpu::TextureUsage::TextureBinding;
+  internalDesc.internalUsage =
+      wgpu::TextureUsage::TextureBinding | wgpu::TextureUsage::RenderAttachment;
 
   texture_descriptor.nextInChain = &internalDesc;
 
