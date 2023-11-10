209b026850c697892cc85d517284806a014f4796
Joe Downing <joedow@google.com>
2023-11-10 00:53:11 +0000

    Update It2Me host and friends to use a raw access_token
    This CL adds a new kAccessToken message field which will be used to
    provide a raw access token (i.e. not prefixed with `oauth2:`.
    The impetus for this change is that today every caller which calls
    into the CRD support host stack via Mojo needs to prefix their
    access_token with 'oauth2:' which is error-prone and un-necessary.
    
    Though this would typically be a straight-forward change, there are
    two cases that need to be covered for compatibility:
    1. The website changes are not in production yet so the It2Me NMH
       needs to handle both old and new message fields for now
    2. Lacros and Ash can have version skew so some translation is needed
       to maintain compatibility.
    
    #1 will be resolved prior to M121 branch point but #2 will require
    that the shim code stick around until ~M124. I've added TODOs in the
    relevant code blocks to remove the shims when it is safe to do so.
    
    Please note that there are also changes needed in the remote_command
    directory however the compat code I've added means that I can split
    those out into a separate CL and have them approved by the OWNERs for
    that directory.
    
    Bug: b:309446315
    
    Change-Id: I4827f16a5085737d43eab14af07e898b91c2dd38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5014191
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222659}

--- remoting/host/it2me/it2me_constants.cc
+++ remoting/host/it2me/it2me_constants.cc
@@ -14,6 +14,7 @@ const char kFeatureAuthorizedHelper[] = "authorizedHelper";
 const char kConnectMessage[] = "connect";
 const char kUserName[] = "userName";
 const char kAuthServiceWithToken[] = "authServiceWithToken";
+const char kAccessToken[] = "accessToken";
 const char kLocalJid[] = "localJid";
 const char kDirectoryBotJidValue[] = "remoting@bot.talk.google.com";
 const char kSuppressUserDialogs[] = "suppressUserDialogs";
