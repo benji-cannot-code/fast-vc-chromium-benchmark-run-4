21ea110009ca93ad65a95d8ab086fea63e326bc1
Peter Birk Pakkenberg <pbirk@chromium.org>
2023-11-10 12:12:51 +0000

    Add metrics and flag for WebView Media Integrity API
    These metrics and the flag will be used by code in //clank.
    
    Bug: 1493963
    Change-Id: If7d34787b434949078fddcc37affed6533b29e8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5001520
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Peter Pakkenberg <pbirk@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222867}

--- android_webview/browser/aw_feature_map.cc
+++ android_webview/browser/aw_feature_map.cc
@@ -44,6 +44,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kWebViewSupervisedUserSiteBlock,
     &base::features::kCollectAndroidFrameTimelineMetrics,
     &features::kWebViewInjectPlatformJsApis,
+    &features::kWebViewMediaIntegrityApi,
 };
 
 // static
