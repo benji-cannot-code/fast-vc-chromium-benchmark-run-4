df439e1d7a6dd49e98c216877b82b84231cfe7fd
Timofey Chudakov <tchudakov@google.com>
2023-11-10 09:34:22 +0000

    [AF] Remove BAM::FillOrPreviewVirtualCardInformation.
    Virtual credit cards are not persisted in Chrome. They differ from
    other CC types by the record type field set to autofill::CreditCard.
    Previously, that was done by `BAM::FillOrPreviewVirtualCardInformation`.
    This CL factors out that complexity from BAM as part of the effort to
    simplify its API.
    
    `BAM::FillOrPreviewVirtualCardInformation` was used from:
    * AutofillExternalDelegate
    * TouchToFillDelegateAndroidImpl
    * Tests
    
    Both AED and TTFDelegate already had conditional logic to fill in
    either regular or virtual credit cards. This CL moves logic to set
    virtual CC record type from the BAM to the callers.
    
    See go/autofill-filling-api-refactoring to find details about
    BAM API simplification.
    
    Bug: 1330108
    Change-Id: Idf6955dc05195cc1b921b2d69b5af43a2c03a76e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5012903
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1222820}

--- chrome/browser/touch_to_fill/payments/android/touch_to_fill_delegate_android_impl_unittest.cc
+++ chrome/browser/touch_to_fill/payments/android/touch_to_fill_delegate_android_impl_unittest.cc
@@ -101,13 +101,6 @@ class MockBrowserAutofillManager : public TestBrowserAutofillManager {
                const CreditCard* credit_card,
                const AutofillTriggerDetails& trigger_details));
   MOCK_METHOD(void,
-              FillOrPreviewVirtualCardInformation,
-              (mojom::ActionPersistence action_persistence,
-               const std::string& guid,
-               const FormData& form,
-               const FormFieldData& field,
-               const AutofillTriggerDetails& trigger_details));
-  MOCK_METHOD(void,
               DidShowSuggestions,
               (bool has_autofill_suggestions,
                const FormData& form,
@@ -705,7 +698,7 @@ TEST_F(TouchToFillDelegateAndroidImplUnitTest, CardSelectionClosesTheSheet) {
   TryToShowTouchToFill(/*expected_success=*/true);
 
   EXPECT_CALL(autofill_client_, HideTouchToFillCreditCard).Times(1);
-  touch_to_fill_delegate_->SuggestionSelected(credit_card.server_id(), false);
+  touch_to_fill_delegate_->SuggestionSelected(credit_card.guid(), false);
 }
 
 TEST_F(TouchToFillDelegateAndroidImplUnitTest, CardSelectionFillsCardForm) {
@@ -716,7 +709,7 @@ TEST_F(TouchToFillDelegateAndroidImplUnitTest, CardSelectionFillsCardForm) {
   TryToShowTouchToFill(/*expected_success=*/true);
 
   EXPECT_CALL(*browser_autofill_manager_, FillOrPreviewCreditCardForm);
-  touch_to_fill_delegate_->SuggestionSelected(credit_card.server_id(), false);
+  touch_to_fill_delegate_->SuggestionSelected(credit_card.guid(), false);
 }
 
 TEST_F(TouchToFillDelegateAndroidImplUnitTest,
@@ -728,8 +721,8 @@ TEST_F(TouchToFillDelegateAndroidImplUnitTest,
 
   TryToShowTouchToFill(/*expected_success=*/true);
 
-  EXPECT_CALL(*browser_autofill_manager_, FillOrPreviewVirtualCardInformation);
-  touch_to_fill_delegate_->SuggestionSelected(credit_card.server_id(), true);
+  EXPECT_CALL(*browser_autofill_manager_, FillOrPreviewCreditCardForm);
+  touch_to_fill_delegate_->SuggestionSelected(credit_card.guid(), true);
 }
 
 TEST_F(TouchToFillDelegateAndroidImplUnitTest,
