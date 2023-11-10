eda94c34d2e4286049b7644610fd3567d86c7f63
Takashi Toyoshima <toyoshim@chromium.org>
2023-11-10 15:39:26 +0000

    LinkPreview: Apply chrome/ specific binder policies also for preview
    Currently, preview mode takes the same binder policy set with
    prerendering in content, but I forgot to handle it for chrome/ specific
    set handled in ChromeContentBrowserClient. This patch is a follow-up
    to apply the same policy set also in chrome/.
    
    Bug: b/299240273
    Change-Id: I1dc59456279c5206ad7d7ef18d685fcc7dbffb75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5020107
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222937}

--- chrome/browser/chrome_content_browser_client_binder_policies.cc
+++ chrome/browser/chrome_content_browser_client_binder_policies.cc
@@ -59,3 +59,9 @@ void RegisterChromeMojoBinderPoliciesForSameOriginPrerendering(
   RegisterPoliciesForNonAssociatedInterfaces(policy_map);
   RegisterPoliciesForChannelAssociatedInterfaces(policy_map);
 }
+
+void RegisterChromeMojoBinderPoliciesForPreview(
+    content::MojoBinderPolicyMap& policy_map) {
+  RegisterPoliciesForNonAssociatedInterfaces(policy_map);
+  RegisterPoliciesForChannelAssociatedInterfaces(policy_map);
+}
