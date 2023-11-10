1e6733192a93c297817efe2c802a0d1b8e3c5b26
conniekxu <conniekxu@chromium.org>
2023-11-10 23:02:30 +0000

    CrOS Settings: Add search label to ManageAppNotifications subpage
    When feature OsSettingsRevampWayfinding is enabled, add the search box
    to the ManageAppNotifications subpage to allow user to filter out apps
    by the app's name.
    
    Screenshot: https://screenshot.googleplex.com/BKexgtj9SqHxajT
    
          NotificationsManagerSubpageTest*
    
    Bug: b/309973973
    Test: browser_tests --gtest_filter=*OsAppsPageAppNotificationsPageApp
    Change-Id: Iabc5f3d3c834f72fa68c6c4ad944d87c9965d3e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018193
    Commit-Queue: Connie Xu <conniekxu@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1223182}

--- chrome/browser/resources/ash/settings/os_apps_page/app_notifications_page/app_notifications_manager_subpage.ts
+++ chrome/browser/resources/ash/settings/os_apps_page/app_notifications_page/app_notifications_manager_subpage.ts
@@ -9,11 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * for sending notifications for the apps.
  */
 
+import 'chrome://resources/cr_elements/cr_shared_style.css.js';
 import './app_notification_row.js';
 
 import {assert} from 'chrome://resources/js/assert.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
+import {isRevampWayfindingEnabled} from '../../common/load_time_booleans.js';
 import {App, AppNotificationsHandlerInterface, AppNotificationsObserverReceiver} from '../../mojom-webui/app_notification_handler.mojom-webui.js';
 import {isAppInstalled} from '../os_apps_page.js';
 
@@ -31,16 +33,31 @@ export class SettingsAppNotificationsManagerSubpage extends PolymerElement {
 
   static get properties() {
     return {
+      searchTerm: {
+        type: String,
+      },
+
       appList_: {
         type: Array,
         value: [],
       },
+
+      /**
+       * List of apps filtered through a search term.
+       */
+      filteredAppList_: {
+        type: Array,
+        value: () => [],
+        computed: 'computeFilteredAppList_(appList_.*, searchTerm)',
+      },
     };
   }
 
+  searchTerm: string;
   private appList_: App[];
   private appNotificationsObserverReceiver_: AppNotificationsObserverReceiver|
       null;
+  private filteredAppList_: App[];
   private mojoInterfaceProvider_: AppNotificationsHandlerInterface;
 
   constructor() {
@@ -55,6 +72,9 @@ export class SettingsAppNotificationsManagerSubpage extends PolymerElement {
   }
 
   override connectedCallback(): void {
+    // This page only exists when revamp wayfinding is enabled.
+    assert(isRevampWayfindingEnabled());
+
     super.connectedCallback();
 
     this.startObservingAppNotifications_();
@@ -105,11 +125,31 @@ export class SettingsAppNotificationsManagerSubpage extends PolymerElement {
     this.appList_ = appList;
   }
 
+  private computeFilteredAppList_(): App[] {
+    if (this.appList_.length === 0) {
+      return [];
+    }
+
+    if (!this.searchTerm) {
+      return [...this.appList_];
+    }
+
+    const lowerCaseSearchTerm = this.searchTerm.toLocaleLowerCase();
+    return this.appList_.filter(app => {
+      assert(app.title);
+      return app.title.toLocaleLowerCase().includes(lowerCaseSearchTerm);
+    });
+  }
+
   /**
    * A function used for sorting languages alphabetically.
    */
   private alphabeticalSort_(first: App, second: App): number {
     return first.title!.localeCompare(second.title!);
+  }
+
+  private isAppListEmpty_(appList: App[]): boolean {
+    return appList.length === 0;
   }
 }
 
