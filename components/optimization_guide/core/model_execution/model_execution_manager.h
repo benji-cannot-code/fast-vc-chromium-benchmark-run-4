169a5f22513e15cc3d934fe897e3be5373ecb324
Clark DuVall <cduvall@chromium.org>
2023-11-11 17:16:20 +0000

    Reland "Add on-device session support to OptimizationGuideModelExecutor"
    This is a reland of commit 7194988c96789f477f0fdfd1ecb5ed31eed005b9
    
    This has a fix to the proto building code in the unittest after the
    changes in http://crrev.com/c/5021117.
    
    Original change's description:
    > Add on-device session support to OptimizationGuideModelExecutor
    >
    > This adds a new Session interface to OptimizationGuideModelExecutor
    > which is backed by OnDeviceModelServiceController. This allows adding
    > context to a session before executing any input.
    >
    > Binary-Size: Requires [de]serializing a proto increasing binary size.
    > Bug: b/304890244
    > Change-Id: I6cc035d478d3b52c5645102fe3a6ba2313afce23
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018973
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    > Commit-Queue: Clark DuVall <cduvall@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1223279}
    
    Binary-Size: Requires [de]serializing a proto increasing binary size.
    Bug: b/304890244
    Change-Id: I4b986f7f8d78ae44698af70fe5af87d03bcb6af0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5021622
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Auto-Submit: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223327}

--- components/optimization_guide/core/model_execution/model_execution_manager.h
+++ components/optimization_guide/core/model_execution/model_execution_manager.h
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/optimization_guide_features.h"
 #include "components/optimization_guide/core/optimization_guide_model_executor.h"
 #include "components/optimization_guide/proto/model_execution.pb.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
+#include "services/on_device_model/public/mojom/on_device_model.mojom.h"
 #include "url/gurl.h"
 
 class OptimizationGuideLogger;
@@ -31,7 +33,6 @@ class IdentityManager;
 namespace optimization_guide {
 
 class ModelExecutionFetcher;
-class OnDeviceModelExecutionConfigInterpreter;
 class OnDeviceModelServiceController;
 
 class ModelExecutionManager {
@@ -51,6 +52,9 @@ class ModelExecutionManager {
   void ExecuteModel(proto::ModelExecutionFeature feature,
                     const google::protobuf::MessageLite& request_metadata,
                     OptimizationGuideModelExecutionResultCallback callback);
+
+  std::unique_ptr<OptimizationGuideModelExecutor::Session> StartSession(
+      proto::ModelExecutionFeature feature);
 
  private:
   // Invoked when the model execution result is available.
@@ -83,14 +87,6 @@ class ModelExecutionManager {
   // Controller for the on-device service.
   std::unique_ptr<OnDeviceModelServiceController>
       on_device_model_service_controller_;
-
-  // Interpreter of the on-device model execution configuration.
-  std::unique_ptr<OnDeviceModelExecutionConfigInterpreter>
-      on_device_model_execution_config_interpreter_;
-
-  // The path for the on-device model. Can be empty when it was not populated
-  // yet. Can be overridden from command-line.
-  base::FilePath on_device_model_path_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
