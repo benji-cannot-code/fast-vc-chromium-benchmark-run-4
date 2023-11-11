b0db01db39f7228bc46fe3321a13d40dfcdc8c29
Clark DuVall <cduvall@chromium.org>
2023-11-11 05:11:08 +0000

    Add on-device session support to OptimizationGuideModelExecutor
    This adds a new Session interface to OptimizationGuideModelExecutor
    which is backed by OnDeviceModelServiceController. This allows adding
    context to a session before executing any input.
    
    Binary-Size: Requires [de]serializing a proto increasing binary size.
    Bug: b/304890244
    Change-Id: I6cc035d478d3b52c5645102fe3a6ba2313afce23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018973
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223279}

--- components/optimization_guide/core/model_execution/on_device_model_service_controller.h
+++ components/optimization_guide/core/model_execution/on_device_model_service_controller.h
@@ -7,8 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string_view>
 
+#include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
+#include "components/optimization_guide/core/optimization_guide_model_executor.h"
+#include "components/optimization_guide/proto/model_execution.pb.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "services/on_device_model/public/mojom/on_device_model.mojom.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -18,6 +21,7 @@ class FilePath;
 }  // namespace base
 
 namespace optimization_guide {
+class OnDeviceModelExecutionConfigInterpreter;
 
 // Controls the lifetime of the on-device model service, loading and unloading
 // of the models, and executing them via the service.
@@ -33,14 +37,15 @@ class OnDeviceModelServiceController {
 
   // Initializes the on-device model controller with the parameters, to be ready
   // to load models and execute.
-  void Init(const base::FilePath& model_path);
+  void Init(const base::FilePath& model_path,
+            std::unique_ptr<OnDeviceModelExecutionConfigInterpreter>
+                config_interpreter);
 
-  // Executes the model for `input` and the response will be sent to
-  // `streaming_responder`. This will load the model if needed, before
-  // execution.
-  void Execute(std::string_view input,
-               mojo::PendingRemote<on_device_model::mojom::StreamingResponder>
-                   streaming_responder);
+  // Starts a session for `feature`. This will start the service and load the
+  // model if it is not already loaded. The session will handle updating
+  // context, executing input, and sending the response.
+  std::unique_ptr<OptimizationGuideModelExecutor::Session> StartSession(
+      proto::ModelExecutionFeature feature);
 
   // Launches the on-device model-service.
   virtual void LaunchService() = 0;
@@ -53,10 +58,16 @@ class OnDeviceModelServiceController {
   // Invoked at the end of model load, to continue with model execution.
   void OnLoadModelResult(const std::optional<std::string>& error);
 
+  // Called when the model assets have been loaded from disk and are ready to be
+  // sent to the service.
+  void OnModelAssetsLoaded(
+      mojo::PendingReceiver<on_device_model::mojom::OnDeviceModel> model,
+      on_device_model::ModelAssets assets);
+
   base::FilePath model_path_;
+  std::unique_ptr<OnDeviceModelExecutionConfigInterpreter> config_interpreter_;
   mojo::Remote<on_device_model::mojom::OnDeviceModelService> service_remote_;
   mojo::Remote<on_device_model::mojom::OnDeviceModel> model_remote_;
-  mojo::Remote<on_device_model::mojom::Session> session_remote_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
