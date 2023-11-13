564abcfb844ccce770c77bad8584e79a2551231e
Ana Salazar <anasalazar@chromium.org>
2023-11-13 17:54:26 +0000

    Allow progress indicator to have a round cap
    Progress ring should have rounded edges according to spec.
    
    Before: https://screenshot.googleplex.com/6ourKDi9upSXYcG
    After: https://screenshot.googleplex.com/79zAsJee9YREPqb
    
    Bug: b:310034036
    Change-Id: I738ae05abc2b3c63a4510a67b7eb2c89dbccdc40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5024495
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223711}

--- ash/app_list/views/app_list_item_view.cc
+++ ash/app_list/views/app_list_item_view.cc
@@ -2170,6 +2170,7 @@ void AppListItemView::UpdateProgressIndicatorState() {
                          : ProgressIndicator::kProgressComplete;
             },
             base::Unretained(this)));
+    progress_indicator_->SetHasRoundCap(true);
     progress_indicator_->SetInnerIconVisible(false);
     progress_indicator_->SetInnerRingVisible(false);
     progress_indicator_->SetOuterRingStrokeWidth(2.0);
--- ash/shelf/shelf_app_button.cc
+++ ash/shelf/shelf_app_button.cc
@@ -1347,6 +1347,7 @@ void ShelfAppButton::UpdateProgressRingBounds() {
                          : ProgressIndicator::kProgressComplete;
             },
             base::Unretained(this)));
+    progress_indicator_->SetHasRoundCap(true);
     progress_indicator_->SetInnerIconVisible(false);
     progress_indicator_->SetInnerRingVisible(false);
     progress_indicator_->SetOuterRingStrokeWidth(2.0);
