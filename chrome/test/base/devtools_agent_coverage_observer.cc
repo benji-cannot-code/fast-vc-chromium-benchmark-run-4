1df6d4aac377e16ddf01e9f695fc45fc41492134
Ben Reich <benreich@chromium.org>
2023-11-13 21:16:21 +0000

    [js-code-coverage] Invert constructor delegation
    The DevToolsAgentCoverageObserver had a 2 parameter constructor
    delegating to a single parameter constructor, let's invert the logic to
    make the initializer list a little easier to parse.
    
    Bug: None
    Test: browser_tests --gtest_filter=*NewTabPage* --devtools-code-coverage
    Change-Id: I74ab3bac1fdf8daf84e0cb09405a6e8a4355f5e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5019902
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223856}

--- chrome/test/base/devtools_agent_coverage_observer.cc
+++ chrome/test/base/devtools_agent_coverage_observer.cc
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/devtools_agent_coverage_observer.h"
 
 #include <ostream>
+#include <utility>
 
 #include "base/containers/contains.h"
+#include "base/functional/callback_helpers.h"
 #include "base/logging.h"
 #include "content/public/test/browser_test_utils.h"
 
@@ -28,15 +30,15 @@ std::ostream& operator<<(std::ostream& out, DevToolsAgentHost* h) {
 
 DevToolsAgentCoverageObserver::DevToolsAgentCoverageObserver(
     base::FilePath devtools_code_coverage_dir)
-    : devtools_code_coverage_dir_(devtools_code_coverage_dir) {
-  DevToolsAgentHost::AddObserver(this);
-}
+    : DevToolsAgentCoverageObserver(devtools_code_coverage_dir,
+                                    base::NullCallback()) {}
 
 DevToolsAgentCoverageObserver::DevToolsAgentCoverageObserver(
     base::FilePath devtools_code_coverage_dir,
     ShouldInspectDevToolsAgentHostCallback should_inspect_callback)
-    : DevToolsAgentCoverageObserver(devtools_code_coverage_dir) {
-  should_inspect_callback_ = std::move(should_inspect_callback);
+    : devtools_code_coverage_dir_(devtools_code_coverage_dir),
+      should_inspect_callback_(std::move(should_inspect_callback)) {
+  DevToolsAgentHost::AddObserver(this);
 }
 
 DevToolsAgentCoverageObserver::~DevToolsAgentCoverageObserver() = default;
