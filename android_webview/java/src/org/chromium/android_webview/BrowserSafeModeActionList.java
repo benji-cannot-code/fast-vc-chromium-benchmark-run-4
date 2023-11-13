c7c358b7229f33b34b60251bc9e2838cb5348f9b
Christoph Schwering <schwering@google.com>
2023-11-13 19:36:33 +0000

    [Autofill] Remove ChromeAutocompleteSafeModeAction
    WebView does not support Autocomplete since Android O.
    Therefore, this CL removes ChromeAutocompleteSafeModeAction.
    
    Bug: 1501199
    Change-Id: Iccd3d68560deb24e6ffce3da7f8b6e13161c6d13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018590
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223779}

--- android_webview/java/src/org/chromium/android_webview/BrowserSafeModeActionList.java
+++ android_webview/java/src/org/chromium/android_webview/BrowserSafeModeActionList.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.android_webview;
 
 import org.chromium.android_webview.autofill.AndroidAutofillSafeModeAction;
-import org.chromium.android_webview.autofill.ChromeAutocompleteSafeModeAction;
 import org.chromium.android_webview.common.Lifetime;
 import org.chromium.android_webview.common.SafeModeAction;
 import org.chromium.android_webview.common.origin_trial.DisableOriginTrialsSafeModeAction;
@@ -28,7 +27,6 @@ public final class BrowserSafeModeActionList {
     public static final SafeModeAction[] sList = {
             new VariationsSeedSafeModeAction(),
             new AndroidAutofillSafeModeAction(),
-            new ChromeAutocompleteSafeModeAction(),
             new NoopSafeModeAction(),
             new FastVariationsSeedSafeModeAction(),
             new AwSafeBrowsingSafeModeAction(),
--- android_webview/javatests/src/org/chromium/android_webview/test/SafeModeTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/SafeModeTest.java
@@ -1016,29 +1016,21 @@ public class SafeModeTest extends AwParameterizedTest {
         histogramWatcherBuilder.expectIntRecords(
                 "Android.WebView.SafeMode.ActionName",
                 SafeModeController.sSafeModeActionLoggingMap.get(
-                        SafeModeActionIds.DISABLE_CHROME_AUTOCOMPLETE),
-                SafeModeController.sSafeModeActionLoggingMap.get(
                         SafeModeActionIds.DISABLE_AW_SAFE_BROWSING),
                 SafeModeController.sSafeModeActionLoggingMap.get(
                         SafeModeActionIds.DISABLE_ORIGIN_TRIALS));
         watcher = histogramWatcherBuilder.build();
-        successAction1 = new TestSafeModeAction(SafeModeActionIds.DISABLE_CHROME_AUTOCOMPLETE);
-        successAction2 = new TestSafeModeAction(SafeModeActionIds.DISABLE_AW_SAFE_BROWSING);
-        TestSafeModeAction successAction3 =
-                new TestSafeModeAction(SafeModeActionIds.DISABLE_ORIGIN_TRIALS);
-        allSuccessful =
-                asSet(successAction1.getId(), successAction2.getId(), successAction3.getId());
+        successAction1 = new TestSafeModeAction(SafeModeActionIds.DISABLE_AW_SAFE_BROWSING);
+        successAction2 = new TestSafeModeAction(SafeModeActionIds.DISABLE_ORIGIN_TRIALS);
+        allSuccessful = asSet(successAction1.getId(), successAction2.getId());
         SafeModeController.getInstance()
-                .registerActions(
-                        new SafeModeAction[] {successAction1, successAction2, successAction3});
+                .registerActions(new SafeModeAction[] {successAction1, successAction2});
         SafeModeController.getInstance().executeActions(allSuccessful);
         watcher.assertExpected(
                 "SafeModeAction names should be recorded: ["
                         + successAction1.getId()
                         + ", "
                         + successAction2.getId()
-                        + ", "
-                        + successAction3.getId()
                         + "]");
         Assert.assertEquals(
                 "successAction1 should have been executed exactly 1 time",
@@ -1048,10 +1040,6 @@ public class SafeModeTest extends AwParameterizedTest {
                 "successAction2 should have been executed exactly 1 time",
                 1,
                 successAction2.getCallCount());
-        Assert.assertEquals(
-                "successAction3 should have been executed exactly 1 time",
-                1,
-                successAction3.getCallCount());
     }
 
     @Test
