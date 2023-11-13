60ed96df529cd170ae7c0a4f3206c8e104187a90
Ali Juma <ajuma@chromium.org>
2023-11-13 16:57:13 +0000

    [iOS] Add EG matcher for custom symbols
    This adds a new EG matcher for custom symbols. Since custom symbols
    are not loaded in the test process, there's no current way to create
    a matcher for them directly in the test process.
    
    This will be used in:
    https://chrome-internal-review.googlesource.com/c/chrome/ios_internal/+/6655161
    
    Bug: 1469647
    Change-Id: I8bb9d09efa0acdebeec73ffc4c36a45aa7f2cf6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018154
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223688}

--- ios/chrome/test/earl_grey/chrome_matchers_app_interface.h
+++ ios/chrome/test/earl_grey/chrome_matchers_app_interface.h
@@ -50,6 +50,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Matcher for element with an image defined by its name in the main bundle.
 + (id<GREYMatcher>)imageViewWithImageNamed:(NSString*)imageName;
 
+// Matcher for element with a custom symbol defined by its name and point size
+// in the main bundle.
++ (id<GREYMatcher>)imageViewWithCustomSymbolNamed:(NSString*)symbolName
+                                        pointSize:(CGFloat)pointSize;
+
 // Matcher for element with an image corresponding to `imageID` and
 // accessibility trait UIAccessibilityTraitButton.
 + (id<GREYMatcher>)buttonWithImage:(int)imageID;
