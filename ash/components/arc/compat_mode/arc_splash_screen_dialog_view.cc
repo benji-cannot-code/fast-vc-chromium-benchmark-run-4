6acaebafe553ca5d3797ffddc1943a91cbe90f1b
Allen Bauer <kylixrd@chromium.org>
2023-11-13 18:44:30 +0000

    Add metadata to Views classes
    This CL was uploaded by git cl split.
    
    R=takise@chromium.org
    
    Bug: 1485768
    Change-Id: I3534d2748cafbf0c40dca28cd3eba4c29caaa716
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5013792
    Auto-Submit: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1223755}

--- ash/components/arc/compat_mode/arc_splash_screen_dialog_view.cc
+++ ash/components/arc/compat_mode/arc_splash_screen_dialog_view.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/window.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/color/color_id.h"
@@ -63,6 +64,8 @@ namespace {
 // Draws the blue-ish highlight border to the parent view according to the
 // highlight path.
 class HighlightBorder : public views::View {
+  METADATA_HEADER(HighlightBorder, views::View)
+
  public:
   HighlightBorder() = default;
   HighlightBorder(const HighlightBorder&) = delete;
@@ -102,6 +105,9 @@ class HighlightBorder : public views::View {
     canvas->DrawRoundRect(rect, (*rrect).GetSimpleRadius(), flags);
   }
 };
+
+BEGIN_METADATA(HighlightBorder)
+END_METADATA
 
 }  // namespace
 
