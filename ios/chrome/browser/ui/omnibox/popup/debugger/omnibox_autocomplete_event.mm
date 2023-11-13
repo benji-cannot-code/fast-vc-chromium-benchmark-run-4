db5b6023e8fe5861e52edf49e6048152ade110d5
Ameur Hosni <ameurhosni@google.com>
2023-11-13 15:54:42 +0000

    [IOS] Add events logging to omnibox debugger
    Low-Coverage-Reason: EXPERIMENTAL_CODE Code guarded by experimental system flag
    Bug: None
    Change-Id: I61971dbe45390bb18ed68fa59bdd3b3b5195afe5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4989441
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1223644}

---
+++ ios/chrome/browser/ui/omnibox/popup/debugger/omnibox_autocomplete_event.mm
@@ -0,0 +1,41 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/omnibox/popup/debugger/omnibox_autocomplete_event.h"
+
+#import "components/omnibox/browser/autocomplete_controller.h"
+#import "ios/chrome/browser/ui/omnibox/popup/autocomplete_match_formatter.h"
+#import "ios/chrome/browser/ui/omnibox/popup/debugger/omnibox_event.h"
+
+@implementation OmniboxAutocompleteEvent
+
+- (OmniboxAutocompleteEvent*)initWithAutocompleteController:
+    (AutocompleteController*)controller {
+  self = [super init];
+
+  if (self) {
+    self.matches = [[NSMutableArray alloc] init];
+    self.autocompleteControllerIsDone = controller->done();
+    for (auto acm : controller->result()) {
+      AutocompleteMatchFormatter* matcher =
+          [[AutocompleteMatchFormatter alloc] initWithMatch:acm];
+      [self.matches addObject:matcher];
+    }
+  }
+  return self;
+}
+
+- (NSString*)title {
+  return [NSString
+      stringWithFormat:@"Result update %@", self.autocompleteControllerIsDone
+                                                ? @"(Final)"
+                                                : @"(Processing)"];
+}
+
+- (EventType)type {
+  return kAutocompleteUpdate;
+}
+
+@end
