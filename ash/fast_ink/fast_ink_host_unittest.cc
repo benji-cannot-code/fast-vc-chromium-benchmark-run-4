b5da9880b621514f5fd4124946b107653b22ffe9
Colin Blundell <blundell@chromium.org>
2023-11-13 10:39:43 +0000

    [//ash] Have FastInkHost hold ClientSI
    This CL changes FastInkHost to hold onto the ClientSharedImage rather
    than the Mailbox that it wraps. This will be necessary for a followup
    change that moves the MapSharedImage() API to be on ClientSharedImage.
    
    There is no functional change.
    
    Bug: 1500874
    Change-Id: I5a83df9bc7ce0d4f9d07de97ec6b859ddf5821cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5012736
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223546}

--- ash/fast_ink/fast_ink_host_unittest.cc
+++ ash/fast_ink/fast_ink_host_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/quads/compositor_render_pass.h"
 #include "components/viz/common/quads/texture_draw_quad.h"
 #include "components/viz/common/resources/resource_id.h"
+#include "gpu/command_buffer/client/client_shared_image.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_tree_host.h"
@@ -193,7 +194,7 @@ TEST_P(FastInkHostTest, CorrectFrameSubmittedToLayerTreeFrameSink) {
 TEST_P(FastInkHostTest, DelayPaintingUntilReceivingFirstBeginFrame) {
   // Buffer is not initialized when there is no begin frame received.
   ASSERT_FALSE(fast_ink_host_->gpu_memory_buffer_for_test());
-  ASSERT_TRUE(fast_ink_host_->mailbox_for_test().IsZero());
+  ASSERT_FALSE(fast_ink_host_->client_si_for_test());
   EXPECT_EQ(fast_ink_host_->get_pending_bitmaps_size_for_test(), 0);
 
   int pending_bitmaps_size = 0;
@@ -222,9 +223,9 @@ TEST_P(FastInkHostTest, DelayPaintingUntilReceivingFirstBeginFrame) {
     gfx::GpuMemoryBuffer* gpu_memory_buffer =
         fast_ink_host_->gpu_memory_buffer_for_test();
     if (use_one_si_for_fast_ink_host_resources_) {
-      ASSERT_FALSE(fast_ink_host_->mailbox_for_test().IsZero());
+      ASSERT_TRUE(fast_ink_host_->client_si_for_test());
     } else {
-      ASSERT_TRUE(fast_ink_host_->mailbox_for_test().IsZero());
+      ASSERT_FALSE(fast_ink_host_->client_si_for_test());
     }
 
     ASSERT_TRUE(gpu_memory_buffer);
@@ -238,8 +239,8 @@ TEST_P(FastInkHostTest, DelayPaintingUntilReceivingFirstBeginFrame) {
     gpu_memory_buffer->Unmap();
   } else {
     // MappableSI should be initialized after receiving the first begin frame.
-    gpu::Mailbox mailbox = fast_ink_host_->mailbox_for_test();
-    ASSERT_FALSE(mailbox.IsZero());
+    ASSERT_TRUE(fast_ink_host_->client_si_for_test());
+    auto mailbox = fast_ink_host_->client_si_for_test()->mailbox();
     // Pending bitmaps should be drawn and cleared.
     EXPECT_EQ(fast_ink_host_->get_pending_bitmaps_size_for_test(), 0);
 
