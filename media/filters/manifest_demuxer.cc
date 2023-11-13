479e59ee72af079a87fbfc59502d2e0f135072b5
Ted Meyer <tmathmeyer@chromium.org>
2023-11-13 21:47:14 +0000

    Clear streams on deletion before ChunkDemuxer
    ManifestDemuxerStream contains a weakptr ref to DemuxerStreams owned by
    ChunkDemuxer, which is just about to be destroyed. By clearing the
    streams first, we avoid any dangling pointer refs.
    
    Bug: 1266991
    Change-Id: I3ebd0857965cbebcf10c46f9a4bb2a8e33f5edcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5008799
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223883}

--- media/filters/manifest_demuxer.cc
+++ media/filters/manifest_demuxer.cc
@@ -68,6 +68,7 @@ ManifestDemuxer::~ManifestDemuxer() {
   DCHECK(media_task_runner_->RunsTasksInCurrentSequence());
   impl_->Stop();
   impl_.reset();
+  streams_.clear();
   chunk_demuxer_.reset();
 }
 
