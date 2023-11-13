e36a8e4121b2d33024c1d21b70400836dda2c8e1
Nina Satragno <nsatragno@chromium.org>
2023-11-13 23:03:01 +0000

    [webauthn] Remove expired flag
    Remove web-authentication-filter-google-passkeys flag from about:flags.
    It has served its purpose.
    
    Fixed: 1498134
    Change-Id: I3a07564dccd9429bb727e5369f6dcfabc552f24f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5026476
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223936}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2072,9 +2072,6 @@ extern const char kWallpaperGooglePhotosSharedAlbumsDescription[];
 extern const char kWallpaperPerDeskName[];
 extern const char kWallpaperPerDeskDescription[];
 
-extern const char kWebAuthnFilterGooglePasskeysName[];
-extern const char kWebAuthnFilterGooglePasskeysDescription[];
-
 extern const char kWebAuthnScreenReaderModeName[];
 extern const char kWebAuthnScreenReaderModeDescription[];
 
