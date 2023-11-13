78db9265a6192b6e0c4de4a656ce2969df8e934f
Jagadesh P <jagadeshjai1999@gmail.com>
2023-11-13 14:32:55 +0000

    [DanglingPointer] Remove dangling pointer in history_counter_browsertest.cc
    Clearing up the pointers acquired in |Setup| during |TearDown|.
    
    And Minor Code formatting.
    
    Bug: 1291138
    Change-Id: Iec54e548b15a9145645acc2aae675e0b57cee26b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018556
    Auto-Submit: Jagadesh Jai <jagadeshjai1999@gmail.com>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223602}

--- chrome/browser/browsing_data/counters/history_counter_browsertest.cc
+++ chrome/browser/browsing_data/counters/history_counter_browsertest.cc
@@ -48,13 +48,16 @@ class HistoryCounterTest : public InProcessBrowserTest {
     SetDeletionPeriodPref(browsing_data::TimePeriod::ALL_TIME);
   }
 
+  void TearDownOnMainThread() override {
+    fake_web_history_service_.reset();
+    history_service_ = nullptr;
+  }
+
   void AddVisit(const std::string url) {
     history_service_->AddPage(GURL(url), time_, history::SOURCE_BROWSED);
   }
 
-  const base::Time& GetCurrentTime() {
-    return time_;
-  }
+  const base::Time& GetCurrentTime() { return time_; }
 
   void SetTime(base::Time time) { time_ = time; }
 
@@ -96,8 +99,9 @@ class HistoryCounterTest : public InProcessBrowserTest {
       has_synced_visits_ = history_result->has_synced_visits();
     }
 
-    if (run_loop_ && finished_)
+    if (run_loop_ && finished_) {
       run_loop_->Quit();
+    }
   }
 
   history::WebHistoryService* GetFakeWebHistoryService(Profile* profile,
@@ -119,7 +123,7 @@ class HistoryCounterTest : public InProcessBrowserTest {
 
  private:
   std::unique_ptr<base::RunLoop> run_loop_;
-  raw_ptr<history::HistoryService, DanglingUntriaged> history_service_;
+  raw_ptr<history::HistoryService> history_service_ = nullptr;
   std::unique_ptr<history::FakeWebHistoryService> fake_web_history_service_;
   base::Time time_;
 
@@ -136,9 +140,9 @@ IN_PROC_BROWSER_TEST_F(HistoryCounterTest, DuplicateVisits) {
   ASSERT_TRUE(base::Time::FromUTCString("1 Jul 2020 10:00 GMT", &time));
   SetTime(time);
 
-  AddVisit("https://www.google.com");   // 1 item
+  AddVisit("https://www.google.com");  // 1 item
   AddVisit("https://www.google.com");
-  AddVisit("https://www.chrome.com");   // 2 items
+  AddVisit("https://www.chrome.com");  // 2 items
   AddVisit("https://www.chrome.com");
   AddVisit("https://www.chrome.com");
   AddVisit("https://www.example.com");  // 3 items
@@ -149,9 +153,9 @@ IN_PROC_BROWSER_TEST_F(HistoryCounterTest, DuplicateVisits) {
   AddVisit("https://www.example.com");
 
   RevertTimeInDays(1);
-  AddVisit("https://www.chrome.com");   // 6 items
+  AddVisit("https://www.chrome.com");  // 6 items
   AddVisit("https://www.chrome.com");
-  AddVisit("https://www.google.com");   // 7 items
+  AddVisit("https://www.google.com");  // 7 items
   AddVisit("https://www.chrome.com");
   AddVisit("https://www.google.com");
   AddVisit("https://www.google.com");
@@ -309,8 +313,8 @@ IN_PROC_BROWSER_TEST_F(HistoryCounterTest, Synced) {
                                    base::Unretained(this)));
 
   history::FakeWebHistoryService* service =
-    static_cast<history::FakeWebHistoryService*>(GetFakeWebHistoryService(
-        profile, false));
+      static_cast<history::FakeWebHistoryService*>(
+          GetFakeWebHistoryService(profile, false));
 
   // No entries locally and no entries in Sync.
   service->SetupFakeResponse(true /* success */, net::HTTP_OK);
@@ -341,7 +345,7 @@ IN_PROC_BROWSER_TEST_F(HistoryCounterTest, Synced) {
   // To err on the safe side, if the server request fails, we assume that there
   // might be some items on the server.
   service->SetupFakeResponse(true /* success */,
-                                              net::HTTP_INTERNAL_SERVER_ERROR);
+                             net::HTTP_INTERNAL_SERVER_ERROR);
   counter.Restart();
   WaitForCounting();
   EXPECT_EQ(0u, GetLocalResult());
@@ -349,7 +353,7 @@ IN_PROC_BROWSER_TEST_F(HistoryCounterTest, Synced) {
 
   // Same when the entire query fails.
   service->SetupFakeResponse(false /* success */,
-                                              net::HTTP_INTERNAL_SERVER_ERROR);
+                             net::HTTP_INTERNAL_SERVER_ERROR);
   counter.Restart();
   WaitForCounting();
   EXPECT_EQ(0u, GetLocalResult());
