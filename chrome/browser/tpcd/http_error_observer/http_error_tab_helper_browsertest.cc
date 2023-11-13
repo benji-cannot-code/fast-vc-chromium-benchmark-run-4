66f422bf30127b33826f84e281dc29566ae792b2
Fiona Macintosh <fmacintosh@google.com>
2023-11-13 15:57:41 +0000

    Update HTTPErrProcBrowserTest for 3PCD
    Explicitly disable 3PCD feature for 3PC allowed test case
    
    Bug: b:306414714
    Change-Id: I798e817d62e1e35a359b7c7ac7ce89740ebf5995
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5021495
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223648}

--- chrome/browser/tpcd/http_error_observer/http_error_tab_helper_browsertest.cc
+++ chrome/browser/tpcd/http_error_observer/http_error_tab_helper_browsertest.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/tpcd/http_error_observer/http_error_tab_helper.h"
 
+#include "base/feature_list.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/content_settings/cookie_settings_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -15,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/content_settings/core/browser/cookie_settings.h"
+#include "components/content_settings/core/common/content_settings.h"
+#include "components/content_settings/core/common/features.h"
 #include "components/content_settings/core/common/pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "components/ukm/test_ukm_recorder.h"
@@ -71,7 +75,10 @@ class HTTPErrProcBrowserTest : public InProcessBrowserTest {
                     : content_settings::CookieControlsMode::kOff));
     scoped_refptr<content_settings::CookieSettings> settings =
         CookieSettingsFactory::GetForProfile(browser()->profile());
-    ASSERT_EQ(settings->ShouldBlockThirdPartyCookies(), enabled);
+    ASSERT_EQ(
+        settings->ShouldBlockThirdPartyCookies(),
+        enabled || base::FeatureList::IsEnabled(
+                       content_settings::features::kTrackingProtection3pcd));
   }
 
   // Checks that the HTTP Error has been properly recorded in the metrics
@@ -128,9 +135,20 @@ IN_PROC_BROWSER_TEST_F(HTTPErrProcBrowserTest, NoErr) {
             0u);
 }
 
+class HTTPErrProcPre3pcdBrowserTest : public HTTPErrProcBrowserTest {
+ public:
+  HTTPErrProcPre3pcdBrowserTest() {
+    scoped_feature_list_.InitAndDisableFeature(
+        content_settings::features::kTrackingProtection3pcd);
+  }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+};
+
 // Check that the ThirdPartyCookieBreakageIndicator UKM is sent on HTTP Error
 // without cookies blocked
-IN_PROC_BROWSER_TEST_F(HTTPErrProcBrowserTest, WithCookiesWithErr) {
+IN_PROC_BROWSER_TEST_F(HTTPErrProcPre3pcdBrowserTest, WithCookiesWithErr) {
   ukm::TestAutoSetUkmRecorder ukm_recorder;
 
   SetThirdPartyCookieBlocking(false);
