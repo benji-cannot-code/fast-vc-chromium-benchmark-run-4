dbc642eaad6608070b46df2002e7533504b17862
Wenbo Jie <wenbojie@chromium.org>
2023-11-13 00:40:39 +0000

    [Files F2] Enable Trash tests for the new tree
    * Support new tree selectors in nudge_container.
    * Add additional check before showing Trash in the navigation.
    * Use ui entry key existence to decide if we need to render Trash/
      Drive root, previously we use the entry existence which is not
      correct, because the entry won't be deleted from the store
      immediately when Pref changes.
    * Remove trash nudge and its corresponding browser tests.
    
    Demo: nudge on OneDrive http://screen/4wvhRuhGtsFmkdk.png
    
    Bug: b:301344220
    Test: browser_tests --gtest_filter="Trash/FilesAppBrowserTest*"
    Change-Id: I1d9f86e2afe22cc932915411e44d637c0fc7799c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5016125
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223425}

--- chrome/browser/ash/file_manager/file_manager_browsertest.cc
+++ chrome/browser/ash/file_manager/file_manager_browsertest.cc
@@ -2169,10 +2169,10 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("trashDeleteFromTrash")
             .NewDirectoryTree()
             .FeatureIds({"screenplay-38573550-c60a-4009-ba92-c0af1420fde6"}),
-        // TODO(b/301344220): enable the tests
-        // TestCase("trashDeleteFromTrashOriginallyFromDrive")
-        //     .NewDirectoryTree()
-        //     .FeatureIds({"screenplay-38573550-c60a-4009-ba92-c0af1420fde6"}),
+        TestCase("trashDeleteFromTrashOriginallyFromDrive")
+            .EnableDriveTrash()
+            .NewDirectoryTree()
+            .FeatureIds({"screenplay-38573550-c60a-4009-ba92-c0af1420fde6"}),
         TestCase("trashNoTasksInTrashRoot").NewDirectoryTree(),
         TestCase("trashDoubleClickOnFileInTrashRootShowsDialog")
             .NewDirectoryTree(),
@@ -2186,17 +2186,17 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("trashDragDropRootPerformsTrashAction").NewDirectoryTree(),
         TestCase("trashDragDropNonModifiableEntriesCantBeTrashed")
             .NewDirectoryTree(),
-        // TestCase("trashDontShowTrashRootOnSelectFileDialog").NewDirectoryTree(),
+        TestCase("trashDontShowTrashRootOnSelectFileDialog").NewDirectoryTree(),
         TestCase("trashDontShowTrashRootWhenOpeningAsAndroidFilePicker")
             .NewDirectoryTree(),
         TestCase("trashEnsureOldEntriesArePeriodicallyRemoved")
             .NewDirectoryTree(),
         TestCase("trashDragDropOutOfTrashPerformsRestoration")
             .NewDirectoryTree(),
-        // TestCase("trashTogglingTrashEnabledPrefUpdatesDirectoryTree")
-        //     .NewDirectoryTree(),
-        // TestCase("trashTogglingTrashEnabledNavigatesAwayFromTrashRoot")
-        //     .NewDirectoryTree(),
+        TestCase("trashTogglingTrashEnabledPrefUpdatesDirectoryTree")
+            .NewDirectoryTree(),
+        TestCase("trashTogglingTrashEnabledNavigatesAwayFromTrashRoot")
+            .NewDirectoryTree(),
         TestCase("trashCantRestoreWhenParentDoesntExist").NewDirectoryTree(),
         TestCase("trashInfeasibleActionsForFileDisabledAndHiddenInTrashRoot")
             .NewDirectoryTree(),
@@ -2206,7 +2206,6 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
             .NewDirectoryTree(),
         TestCase("trashAllActionsDisabledForBlankSpaceInTrashRoot")
             .NewDirectoryTree(),
-        // TestCase("trashNudgeShownOnFirstTrashOperation").NewDirectoryTree(),
         TestCase("trashStaleTrashInfoFilesAreRemovedAfterOneHour")
             .NewDirectoryTree(),
         // Section end - browser tests for new directory tree
@@ -2254,7 +2253,6 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("trashInfeasibleActionsForFolderDisabledAndHiddenInTrashRoot"),
         TestCase("trashExtractAllForZipHiddenAndDisabledInTrashRoot"),
         TestCase("trashAllActionsDisabledForBlankSpaceInTrashRoot"),
-        TestCase("trashNudgeShownOnFirstTrashOperation"),
         TestCase("trashStaleTrashInfoFilesAreRemovedAfterOneHour")));
 
 WRAPPED_INSTANTIATE_TEST_SUITE_P(
