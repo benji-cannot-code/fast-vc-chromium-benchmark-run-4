ecebd3bed4fcd692e39f9101e1343d374876d350
Denis Kuznetsov <antrim@chromium.org>
2023-11-13 14:50:28 +0000

    Refactoring: move some of the OOBE screen files under /osauth/
    Bug: None
    Change-Id: I789668bdebf6f7a68d75cfd5a0ed75b54fa0d793
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5020812
    Auto-Submit: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Istvan Nagy <iscsi@google.com>
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223609}

--- chrome/browser/ash/login/screens/osauth/cryptohome_recovery_setup_screen.h
+++ chrome/browser/ash/login/screens/osauth/cryptohome_recovery_setup_screen.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_ASH_LOGIN_SCREENS_CRYPTOHOME_RECOVERY_SETUP_SCREEN_H_
-#define CHROME_BROWSER_ASH_LOGIN_SCREENS_CRYPTOHOME_RECOVERY_SETUP_SCREEN_H_
+#ifndef CHROME_BROWSER_ASH_LOGIN_SCREENS_OSAUTH_CRYPTOHOME_RECOVERY_SETUP_SCREEN_H_
+#define CHROME_BROWSER_ASH_LOGIN_SCREENS_OSAUTH_CRYPTOHOME_RECOVERY_SETUP_SCREEN_H_
 
 #include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
@@ -65,4 +65,4 @@ class CryptohomeRecoverySetupScreen : public BaseScreen {
 
 }  // namespace ash
 
-#endif  // CHROME_BROWSER_ASH_LOGIN_SCREENS_CRYPTOHOME_RECOVERY_SETUP_SCREEN_H_
+#endif  // CHROME_BROWSER_ASH_LOGIN_SCREENS_OSAUTH_CRYPTOHOME_RECOVERY_SETUP_SCREEN_H_
