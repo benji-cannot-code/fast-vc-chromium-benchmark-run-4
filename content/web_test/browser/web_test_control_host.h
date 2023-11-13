823f1bc02cee6c6e165d2c6be50c94acafd50b18
Stefan Zager <szager@chromium.org>
2023-11-13 20:09:34 +0000

    Add per-test-case tracing and archive trace output for web_tests
    Re-land after revert:
    
    https://chromium-review.googlesource.com/c/chromium/src/+/5016143
    
    This CL fixes three bugs in the original:
    
    - a javascript syntax error in results.html
    - Previously, when a passing test with a trace file was expanded in
      results. html, the trace file was automatically downloaded without
      clicking on the link.
    - a crash when the feature is used due to destructing a RunLoop while
      it was still running.
    
    Bug: None
    Change-Id: I8b25ca55a12f58dffbebb492ef7665e8416a13fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018180
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223805}

--- content/web_test/browser/web_test_control_host.h
+++ content/web_test/browser/web_test_control_host.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_process_host_observer.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/web_test/browser/leak_detector.h"
+#include "content/web_test/browser/web_test_tracing_controller.h"
 #include "content/web_test/common/web_test.mojom.h"
 #include "content/web_test/common/web_test_runtime_flags.h"
 #include "mojo/public/cpp/bindings/associated_receiver_set.h"
@@ -125,10 +126,8 @@ class WebTestControlHost : public WebContentsObserver,
   WebTestControlHost(const WebTestControlHost&) = delete;
   WebTestControlHost& operator=(const WebTestControlHost&) = delete;
 
-  // True if the controller is ready for testing.
-  bool PrepareForWebTest(const TestInfo& test_info);
-  // True if the controller was reset successfully.
-  bool ResetBrowserAfterWebTest();
+  void PrepareForWebTest(const TestInfo& test_info);
+  void ResetBrowserAfterWebTest();
 
   // Allows WebTestControlHost to track all WebContents created by tests, either
   // by Javascript or by C++ code in the browser.
@@ -447,6 +446,8 @@ class WebTestControlHost : public WebContentsObserver,
       non_associated_receiver_bindings_;
 
   base::ScopedTempDir writable_directory_for_tests_;
+
+  absl::optional<WebTestTracingController> tracing_controller_;
 
   enum class NextPointerLockAction {
     kWillSucceed,
