99e3b00bec1117d8894087ee82a0077c7420733f
Yongshun Liu <yongshun@chromium.org>
2023-11-13 21:11:23 +0000

    bento_button: Enable right click on the desk button
    This adds context menu to the desk button. It re-uses the shelf view as
    the context menu controller so that they show the same menu items. In
    addition, `is_hovered_` is purged since it's the exact same duplicate
    information that we can get directly from the button state.
    
    Bug: b/295555460
    Test: ash_unittests
    Change-Id: Iaa41393667a34a9edaf4d559234f3ca2b8d2513f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5023131
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223849}

--- ash/wm/desks/desk_button/desk_button.cc
+++ ash/wm/desks/desk_button/desk_button.cc
@@ -10,8 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/screen_util.h"
 #include "ash/shelf/desk_button_widget.h"
+#include "ash/shelf/hotseat_widget.h"
 #include "ash/shelf/shelf.h"
 #include "ash/shelf/shelf_navigation_widget.h"
+#include "ash/shelf/shelf_view.h"
 #include "ash/shelf/shelf_widget.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
@@ -29,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/layer.h"
 #include "ui/events/event.h"
 #include "ui/gfx/canvas.h"
+#include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/rounded_corners_f.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/views/accessibility/view_accessibility.h"
@@ -196,10 +199,17 @@ DeskButton::DeskButton(DeskButtonWidget* desk_button_widget)
   TypographyProvider::Get()->StyleLabel(TypographyToken::kCrosButton1,
                                         *desk_name_label_);
 
+  // Use shelf view as the context menu controller so that right click on the
+  // desk button shows the same context menu.
+  set_context_menu_controller(
+      desk_button_widget_->shelf()->hotseat_widget()->GetShelfView());
+
   DesksController::Get()->AddObserver(this);
+  Shell::Get()->AddShellObserver(this);
 }
 
 DeskButton::~DeskButton() {
+  Shell::Get()->RemoveShellObserver(this);
   DesksController::Get()->RemoveObserver(this);
 }
 
@@ -218,6 +228,10 @@ void DeskButton::OnExpandedStateUpdate(bool expanded) {
   Layout();
 }
 
+bool DeskButton::GetHovered() const {
+  return GetState() == ButtonState::STATE_HOVERED;
+}
+
 void DeskButton::SetActivation(bool is_activated) {
   if (is_activated_ == is_activated) {
     return;
@@ -229,7 +243,7 @@ void DeskButton::SetActivation(bool is_activated) {
                               !is_activated_);
 
   if (!force_expanded_state_) {
-    if (!is_activated_ && (is_hovered_ || is_focused_)) {
+    if (!is_activated_ && (GetHovered() || is_focused_)) {
       desk_button_widget_->SetExpanded(true);
     } else {
       desk_button_widget_->SetExpanded(false);
@@ -297,46 +311,44 @@ void DeskButton::GetAccessibleNodeData(ui::AXNodeData* node_data) {
   GetViewAccessibility().OverrideNextFocus(shelf_widget);
 }
 
-void DeskButton::OnMouseEntered(const ui::MouseEvent& event) {
-  if (is_hovered_) {
+void DeskButton::OnMouseEvent(ui::MouseEvent* event) {
+  if (event->type() == ui::ET_MOUSE_PRESSED &&
+      event->IsOnlyRightMouseButton()) {
+    MaybeShowContextMenuForEvent(event);
     return;
   }
 
-  is_hovered_ = true;
-
-  UpdateShelfAutoHideDisabler(disable_shelf_auto_hide_hover_, !is_hovered_);
-
-  if (is_activated_) {
-    return;
-  }
-
-  if (!is_expanded_ && !force_expanded_state_) {
-    // TODO(b/272383056): Would be better to have the widget register a
-    // callback like "preferred_expanded_state_changed".
-    desk_button_widget_->SetExpanded(true);
-  }
-
-  MaybeUpdateDeskSwitchButtonVisibility(
-      SwitchButtonUpdateSource::kDeskButtonUpdate);
+  views::Button::OnMouseEvent(event);
 }
 
-void DeskButton::OnMouseExited(const ui::MouseEvent& event) {
-  if (!is_hovered_) {
+void DeskButton::OnGestureEvent(ui::GestureEvent* event) {
+  if (event->type() == ui::ET_GESTURE_LONG_PRESS ||
+      event->type() == ui::ET_GESTURE_LONG_TAP) {
+    MaybeShowContextMenuForEvent(event);
     return;
   }
 
-  is_hovered_ = false;
+  views::Button::OnGestureEvent(event);
+}
 
-  UpdateShelfAutoHideDisabler(disable_shelf_auto_hide_hover_, !is_hovered_);
+void DeskButton::StateChanged(ButtonState old_state) {
+  // When shell is destroying, child views under desk button will be removed
+  // first, then it triggers `Button::ViewHierarchyChanged()`, as a result,
+  // button state will be set to normal. In such case, it should *not* do
+  // anything to the desk button widget or the chevron buttons.
+  if (is_shell_destroying_ || (GetState() != ButtonState::STATE_NORMAL &&
+                               GetState() != ButtonState::STATE_HOVERED)) {
+    return;
+  }
+
+  UpdateShelfAutoHideDisabler(disable_shelf_auto_hide_hover_, !GetHovered());
 
   if (is_activated_) {
     return;
   }
 
-  if (is_expanded_ && !force_expanded_state_ && !is_focused_) {
-    // TODO(b/272383056): Would be better to have the widget register a
-    // callback like "preferred_expanded_state_changed".
-    desk_button_widget_->SetExpanded(false);
+  if (!force_expanded_state_ && !is_focused_) {
+    desk_button_widget_->SetExpanded(GetHovered());
   }
 
   MaybeUpdateDeskSwitchButtonVisibility(
@@ -392,6 +404,10 @@ void DeskButton::AboutToRequestFocusFromTabTraversal(bool reverse) {
 
 void DeskButton::OnViewBlurred(views::View* observed_view) {
   MaybeContract();
+}
+
+void DeskButton::OnShellDestroying() {
+  is_shell_destroying_ = true;
 }
 
 void DeskButton::OnButtonPressed() {
@@ -472,7 +488,7 @@ void DeskButton::MaybeUpdateDeskSwitchButtonVisibility(
   const int prev_desk_index = target_desk_index - 1;
   const int next_desk_index = target_desk_index + 1;
   const bool show_desk_switch_buttons =
-      (is_hovered_ || is_focused_) && !is_activated_ && is_expanded_;
+      (GetHovered() || is_focused_) && !is_activated_ && is_expanded_;
   auto is_valid_desk_index = [desk_controller](int index) {
     return 0 <= index && index < desk_controller->GetNumberOfDesks();
   };
@@ -509,6 +525,26 @@ void DeskButton::MaybeUpdateDeskSwitchButtonVisibility(
           desk_controller->GetDeskName(next_desk_index)));
     }
   }
+}
+
+void DeskButton::MaybeShowContextMenuForEvent(ui::LocatedEvent* event) {
+  if (!is_activated_) {
+    ui::MenuSourceType source_type = ui::MenuSourceType::MENU_SOURCE_MOUSE;
+    if (event->type() == ui::ET_GESTURE_LONG_PRESS) {
+      source_type = ui::MenuSourceType::MENU_SOURCE_LONG_PRESS;
+    } else if (event->type() == ui::ET_GESTURE_LONG_TAP) {
+      source_type = ui::MenuSourceType::MENU_SOURCE_LONG_TAP;
+    }
+    gfx::Point location_in_screen(event->location());
+    View::ConvertPointToScreen(this, &location_in_screen);
+    ShowContextMenu(location_in_screen, source_type);
+    MaybeUpdateDeskSwitchButtonVisibility(
+        SwitchButtonUpdateSource::kDeskButtonUpdate);
+    MaybeContract();
+  }
+
+  event->SetHandled();
+  event->StopPropagation();
 }
 
 void DeskButton::UpdateShelfAutoHideDisabler(
--- ash/wm/desks/desk_button/desk_button.h
+++ ash/wm/desks/desk_button/desk_button.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/shelf/shelf.h"
+#include "ash/shell_observer.h"
 #include "ash/wm/desks/desks_controller.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/controls/button/button.h"
@@ -57,7 +58,8 @@ class ASH_EXPORT DeskSwitchButton : public views::ImageButton {
 // buttons allow for traversal to the left or right desk, while clicking on the
 // button itself opens up a desk bar.
 class ASH_EXPORT DeskButton : public views::Button,
-                              public DesksController::Observer {
+                              public DesksController::Observer,
+                              public ShellObserver {
  public:
   METADATA_HEADER(DeskButton);
 
@@ -66,7 +68,6 @@ class ASH_EXPORT DeskButton : public views::Button,
   DeskButton& operator=(const DeskButton&) = delete;
   ~DeskButton() override;
 
-  bool is_hovered() const { return is_hovered_; }
   bool is_activated() const { return is_activated_; }
   DeskSwitchButton* prev_desk_button() const { return prev_desk_button_; }
   DeskSwitchButton* next_desk_button() const { return next_desk_button_; }
@@ -77,6 +78,8 @@ class ASH_EXPORT DeskButton : public views::Button,
 
   // Updates label text and visibility of children.
   void OnExpandedStateUpdate(bool expanded);
+
+  bool GetHovered() const;
 
   void SetActivation(bool is_activated);
 
@@ -105,8 +108,9 @@ class ASH_EXPORT DeskButton : public views::Button,
 
   // views::Button:
   void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
-  void OnMouseEntered(const ui::MouseEvent& event) override;
-  void OnMouseExited(const ui::MouseEvent& event) override;
+  void OnMouseEvent(ui::MouseEvent* event) override;
+  void OnGestureEvent(ui::GestureEvent* event) override;
+  void StateChanged(ButtonState old_state) override;
   View* GetTooltipHandlerForPoint(const gfx::Point& point) override;
 
   // DesksController::Observer:
@@ -123,6 +127,9 @@ class ASH_EXPORT DeskButton : public views::Button,
 
   // views::ViewObserver:
   void OnViewBlurred(views::View* observed_view) override;
+
+  // ShellObserver:
+  void OnShellDestroying() override;
 
   // Toggles the button's `is_activated_` state and adjusts the button's style
   // to reflect the new activation state.
@@ -141,6 +148,11 @@ class ASH_EXPORT DeskButton : public views::Button,
   // Determines whether the desk switch buttons can be shown. `source` is used
   // to calculate target active desk index before desk switch.
   void MaybeUpdateDeskSwitchButtonVisibility(SwitchButtonUpdateSource source);
+
+  // Shows the context menu for the given located event when the button is not
+  // activated. Please note, it re-uses the shelf view as the context menu
+  // controller so that they show the same menu items.
+  void MaybeShowContextMenuForEvent(ui::LocatedEvent* event);
 
   // Updates the shelf auto-hide disabler given `should_enable_shelf_auto_hide`.
   void UpdateShelfAutoHideDisabler(
@@ -170,9 +182,6 @@ class ASH_EXPORT DeskButton : public views::Button,
   // Tracks whether the button is currently in expanded state.
   bool is_expanded_ = false;
 
-  // Tracks whether the button is currently being hovered.
-  bool is_hovered_ = false;
-
   // Tracks whether the button is currently activated (i.e. whether the desk
   // button has been pressed).
   bool is_activated_ = false;
@@ -183,6 +192,9 @@ class ASH_EXPORT DeskButton : public views::Button,
   // Indicates that the shelf is horizontal and therefore the button should
   // always be expanded.
   bool force_expanded_state_ = false;
+
+  // Indicates that shell is destroying.
+  bool is_shell_destroying_ = false;
 
   // Used to suspend the shelf from audo-hiding when the button is activated or
   // hovered.
