2468f09f44d5e47d5d4620c7df0018d36a9b2ca2
Jagadesh P <jagadeshjai1999@gmail.com>
2023-11-13 21:39:23 +0000

    [DanglingPointer] Remove dangling pointer in device_chooser_extension_browsertest.cc
    Bug: 1291138
    Change-Id: If0edf78503c943f854b40e3ace6de8f9e30f798e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5020541
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Auto-Submit: Jagadesh Jai <jagadeshjai1999@gmail.com>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223870}

--- chrome/browser/ui/views/extensions/device_chooser_extension_browsertest.cc
+++ chrome/browser/ui/views/extensions/device_chooser_extension_browsertest.cc
@@ -65,6 +65,11 @@ class DeviceChooserExtensionBrowserTest
     ASSERT_EQ(url, web_contents()->GetLastCommittedURL());
   }
 
+  void TearDownOnMainThread() override {
+    extension_ = nullptr;
+    ExtensionBrowserTest::TearDownOnMainThread();
+  }
+
   const std::string& extension_id() { return extension_->id(); }
 
   content::WebContents* web_contents() {
@@ -108,8 +113,9 @@ class DeviceChooserExtensionBrowserTest
       // Ensure we don't downcast the ExtensionsToolbarButton.
       if (views::IsViewClass<ToolbarActionView>(child)) {
         auto* action = static_cast<ToolbarActionView*>(child);
-        if (is_visible(action))
+        if (is_visible(action)) {
           result.push_back(action);
+        }
       }
     }
     return result;
@@ -131,7 +137,7 @@ class DeviceChooserExtensionBrowserTest
   }
 
  private:
-  raw_ptr<const extensions::Extension, DanglingUntriaged> extension_ = nullptr;
+  raw_ptr<const extensions::Extension> extension_ = nullptr;
 };
 
 IN_PROC_BROWSER_TEST_P(DeviceChooserExtensionBrowserTest,
