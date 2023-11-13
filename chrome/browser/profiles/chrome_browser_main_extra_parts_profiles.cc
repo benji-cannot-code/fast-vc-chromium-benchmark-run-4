d5f31fd82013cf9aec4126a1bbdd3677b789048a
Theodore Olsauskas-Warren <sauski@google.com>
2023-11-13 14:23:53 +0000

    Remove remaining access context audit code
    Remaining code was used to clean up database and has sufficient soak
    time to reach stable.
    
    This CL removes that code, finalising the removal.
    
    Bug: 1101675, 1493452
    Change-Id: I53eca6fb6532c7aa44344060db35238dcf2dc70a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5023889
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223599}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/breadcrumbs/breadcrumb_manager_keyed_service_factory.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/browsing_data/access_context_audit_service_factory.h"
 #include "chrome/browser/browsing_data/browsing_data_history_observer_service.h"
 #include "chrome/browser/browsing_data/chrome_browsing_data_lifetime_manager_factory.h"
 #include "chrome/browser/browsing_data/chrome_browsing_data_remover_delegate_factory.h"
@@ -540,7 +539,6 @@ void ChromeBrowserMainExtraPartsProfiles::
   AboutSigninInternalsFactory::GetInstance();
   AboutThisSiteServiceFactory::GetInstance();
   AcceptLanguagesServiceFactory::GetInstance();
-  AccessContextAuditServiceFactory::GetInstance();
   AccessibilityLabelsServiceFactory::GetInstance();
   AccountBookmarkSyncServiceFactory::GetInstance();
   AccountConsistencyModeManagerFactory::GetInstance();
