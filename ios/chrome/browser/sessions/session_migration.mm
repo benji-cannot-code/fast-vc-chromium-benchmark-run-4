687abd8e63bd2ac7a0f915653d84d4a1d14968c6
Sylvain Defresne <sdefresne@chromium.org>
2023-11-13 13:47:24 +0000

    [ios] Pass identifiers to CRWSessionStorage constructor
    Change -[CRWSessionStorage initWithProto:] to take the identifiers
    for the object in addition to the protobuf representation. This
    allow to create those objects on a background thread (needed to
    perform the load on a background thread).
    
    Fixes a bug where the snapshots were lost when migrating from the
    optimised to the legacy session storage format (as the identifier
    were reinitialized by -initWithProto: and not from the value read
    from the optimised session storage files).
    
    Bug: 1383087
    Change-Id: Iac72d01eae1f096c914ee9313ff2f68c1c0c8be8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5017944
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223579}

--- ios/chrome/browser/sessions/session_migration.mm
+++ ios/chrome/browser/sessions/session_migration.mm
@@ -99,7 +99,8 @@ base::FilePath LegacyWebSessionFilename(const base::FilePath& path,
 // Loads optimized WebState's state from `path` and converts it to
 // CRWSessionStorage*.
 [[nodiscard]] CRWSessionStorage* LoadSessionStorageFromOptimized(
-    const base::FilePath& path) {
+    const base::FilePath& path,
+    web::WebStateID web_state_id) {
   // Load the data and metadata.
   web::proto::WebStateStorage storage;
   if (!ParseProto(path.Append(kWebStateStorageFilename), storage)) {
@@ -111,10 +112,9 @@ base::FilePath LegacyWebSessionFilename(const base::FilePath& path,
     return nil;
   }
 
-  CRWSessionStorage* session =
-      [[CRWSessionStorage alloc] initWithProto:storage];
-  session.stableIdentifier = [[NSUUID UUID] UUIDString];
-  return session;
+  return [[CRWSessionStorage alloc] initWithProto:storage
+                                 uniqueIdentifier:web_state_id
+                                 stableIdentifier:[[NSUUID UUID] UUIDString]];
 }
 
 // Loads optimized session from `path` and converts it to SessionWindowIOS.
@@ -136,7 +136,8 @@ base::FilePath LegacyWebSessionFilename(const base::FilePath& path,
     const auto ident = web::WebStateID::FromSerializedValue(item.identifier());
     const base::FilePath item_dir = OptimizedWebStateDirectory(path, ident);
 
-    CRWSessionStorage* session = LoadSessionStorageFromOptimized(item_dir);
+    CRWSessionStorage* session =
+        LoadSessionStorageFromOptimized(item_dir, ident);
     if (!session) {
       return nil;
     }
@@ -373,7 +374,7 @@ void MigrateSessionToOptimizedWithCleanup(
     const base::FilePath& dest,
     const base::FilePath& web_sessions,
     ::sessions::TabRestoreService* restore_service) {
-  MigrationResult result =
+  const MigrationResult result =
       MigrateSessionToOptimizedInternal(from, dest, web_sessions);
 
   switch (result.status) {
