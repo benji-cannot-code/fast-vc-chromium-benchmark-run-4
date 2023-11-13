b4c0b97f5d19b3fc01c214ddd14c4b85c591a3db
andrewxu <andrewxu@chromium.org>
2023-11-13 18:26:44 +0000

    [SysUi download integration] Add primary text to tote downloads
    This CL adds primary text to the downloads displayed in holding space.
    
    Bug: b/308213441,b/307347158
    Change-Id: I4becfdbf77dce408f3950da9443d92b3915a6cc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5001751
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1223744}

--- chrome/browser/ui/ash/download_status/display_metadata.h
+++ chrome/browser/ui/ash/download_status/display_metadata.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_ASH_DOWNLOAD_STATUS_DISPLAY_METADATA_H_
 #define CHROME_BROWSER_UI_ASH_DOWNLOAD_STATUS_DISPLAY_METADATA_H_
 
+#include <string>
+
 #include "base/files/file_path.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
@@ -25,6 +27,9 @@ struct DisplayMetadata {
 
   // The received bytes of download.
   absl::optional<int64_t> received_bytes;
+
+  // The primary text of the displayed download.
+  absl::optional<std::u16string> text;
 
   // The total bytes of download.
   absl::optional<int64_t> total_bytes;
