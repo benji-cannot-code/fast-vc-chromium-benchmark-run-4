d898e5a3f83cd08ead7e6dc66036bdde2ac8121a
Jonathan Ross <jonross@chromium.org>
2023-11-13 23:02:53 +0000

    Throttle LayerTreeHostImpl Draw when Evicted
    When evicted all drawn frames are eventually rejected by Viz. This is
    wasted work.
    
    We also want to be able to delete certain viz::TransferableResources
    that are held by the ActiveTree. If we do this, when attempting to draw
    while evicted would be trying to reference invalidate resources.
    
    So instead we block draw while evicted. Instead we will wait for a new
    tree to Activate that is for a newer viz::Surface
    
    Bug: 1449273
    Change-Id: I7315b007bbd21e2b82485f813f34062b5c763205
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5001404
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223935}

--- cc/base/features.cc
+++ cc/base/features.cc
@@ -166,4 +166,8 @@ BASE_FEATURE(kUseMapRectForPixelMovement,
              "UseMapRectForPixelMovement",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kEvictionThrottlesDraw,
+             "EvictionThrottlesDraw",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 }  // namespace features
