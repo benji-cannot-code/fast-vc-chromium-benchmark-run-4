3a33ef5a505172d3aecf7186ce809f07a57c95f8
Edman Anjos <edmanp@google.com>
2023-11-13 23:34:00 +0000

    kiosk: Remove Chrome Apps from enterprise.kioskInput API
    With this change Chrome Apps can no longer access the Kiosk input API.
    This does not affect users as the API has not launched yet.
    
    The browser test now uses a web app with a companion extension.
    
    Bug: b:308905671
    Test: testing/xvfb.py tools/autotest.py -C out_/Default --run_all chrome/browser/ash/login/app_mode/test/kiosk_enterprise_input_api_browsertest.cc
    Change-Id: I0f259a13f41e5423e18a355aa85d5829cea89b44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5007993
    Reviewed-by: Ben Franz <bfranz@chromium.org>
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223958}

---
+++ chrome/test/data/extensions/api_test/enterprise_kiosk_input/service_worker.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.runtime.onMessageExternal.addListener(
+  ({ message, data }, _sender, sendResponse) => {
+    switch (message) {
+      case "is_api_available":
+        runIsApiAvailable(sendResponse);
+        return true;
+      case "set_input_method":
+        runSetInputMethod(data, sendResponse);
+        return true;
+    }
+  }
+);
+
+function runIsApiAvailable(sendResponse) {
+  sendResponse(chrome.enterprise?.kioskInput != null);
+}
+
+function runSetInputMethod(data, sendResponse) {
+  chrome.enterprise.kioskInput.setCurrentInputMethod(
+    { inputMethodId: data },
+    () => sendResponse(chrome.runtime.lastError?.message ?? true)
+  );
+}
+
+// Trigger `kReadyMessageReceived` in `extension_force_install_mixin.cc`.
+chrome.test?.sendMessage && chrome.test.sendMessage('ready')
