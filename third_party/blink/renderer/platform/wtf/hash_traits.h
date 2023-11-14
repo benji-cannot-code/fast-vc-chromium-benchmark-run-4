0fd41507e3ceaab4474dc4e0281b100ecf5347bd
Jeremy Roman <jbroman@chromium.org>
2023-11-14 05:17:41 +0000

    WTF: Use C++20 concepts in WTF::GenericHashTraits.
    This is objectively cooler than SFINAE.
    
    Bug: 1502036
    Change-Id: I003719471fc480b6fa50c8f6433deb2edb912d2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029014
    Auto-Submit: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224094}

--- third_party/blink/renderer/platform/wtf/hash_traits.h
+++ third_party/blink/renderer/platform/wtf/hash_traits.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string.h>
 
+#include <concepts>
 #include <limits>
 #include <memory>
 #include <type_traits>
@@ -232,7 +233,7 @@ struct EnumHashTraits : internal::IntOrEnumHashTraits<T, -128, -127> {
   static_assert(std::is_enum_v<T>);
 };
 
-template <typename T, typename Enable = void>
+template <typename T>
 struct GenericHashTraits : internal::GenericHashTraitsBase<T> {
   static_assert(!std::is_integral_v<T>);
   static_assert(!std::is_enum_v<T>);
@@ -240,16 +241,16 @@ struct GenericHashTraits : internal::GenericHashTraitsBase<T> {
 };
 
 template <typename T>
-struct GenericHashTraits<T, std::enable_if_t<std::is_integral_v<T>>>
-    : IntHashTraits<T> {};
+  requires std::integral<T>
+struct GenericHashTraits<T> : IntHashTraits<T> {};
 
 template <typename T>
-struct GenericHashTraits<T, std::enable_if_t<std::is_enum_v<T>>>
-    : EnumHashTraits<T> {};
+  requires std::is_enum_v<T>
+struct GenericHashTraits<T> : EnumHashTraits<T> {};
 
 template <typename T>
-struct GenericHashTraits<T, std::enable_if_t<std::is_floating_point_v<T>>>
-    : internal::GenericHashTraitsBase<T> {
+  requires std::floating_point<T>
+struct GenericHashTraits<T> : internal::GenericHashTraitsBase<T> {
   static unsigned GetHash(T key) { return HashFloat(key); }
   static bool Equal(T a, T b) { return FloatEqualForHash(a, b); }
   static constexpr T EmptyValue() { return std::numeric_limits<T>::infinity(); }
