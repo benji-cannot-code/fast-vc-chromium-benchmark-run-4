8874570810b981f5a985687488d70d827bb186c4
Emre Kanlikilicer <emreknlk@google.com>
2023-11-14 16:32:54 +0000

    Addressing the GET vs POST call correctness when dictionary is empty
    Bug: 1501241: GCPW backend request is incorrectly set with POST type
    Change-Id: Ic2247989dc66fd232fa23d8a00a2f3c0dea7b011
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5019501
    Reviewed-by: Tien Mai <tienmai@chromium.org>
    Reviewed-by: Yusuf Sengul <yusufsn@google.com>
    Commit-Queue: Emre Kanlikilicer <emreknlk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1224327}

--- chrome/credential_provider/gaiacp/win_http_url_fetcher.cc
+++ chrome/credential_provider/gaiacp/win_http_url_fetcher.cc
@@ -112,8 +112,11 @@ class HttpServiceRequest {
         base::StringPiece(response_.data(), response_.size()),
         base::JSON_PARSE_CHROMIUM_EXTENSIONS |
             base::JSON_ALLOW_TRAILING_COMMAS);
-    if (!result || !result->is_dict()) {
-      LOGFN(ERROR) << "Failed to read json result from server response";
+    if (!result) {
+      LOGFN(ERROR) << "base::JSONReader::Read returned 0";
+      result.reset();
+    } else if (!result->is_dict()) {
+      LOGFN(ERROR) << "json result is not a dictionary";
       result.reset();
     }
 
@@ -428,16 +431,26 @@ HRESULT WinHttpUrlFetcher::BuildRequestAndFetchResultFromHttpService(
   DCHECK(request_result);
 
   std::string request_body;
-  if (!base::JSONWriter::Write(request_dict, &request_body)) {
+  if (!request_dict.empty() &&
+      !base::JSONWriter::Write(request_dict, &request_body)) {
     LOGFN(ERROR) << "base::JSONWriter::Write failed";
+    return E_FAIL;
+  }
+  if ((request_dict.empty() && !request_body.empty()) ||
+      (!request_dict.empty() && request_body.empty())) {
+    LOGFN(ERROR) << "Mismatch between request dict and body";
     return E_FAIL;
   }
 
   for (unsigned int try_count = 0; try_count <= request_retries; ++try_count) {
     HttpServiceRequest* request = HttpServiceRequest::Create(
         request_url, access_token, headers, request_body, request_timeout);
-    if (!request)
+    if (!request) {
+      LOGFN(ERROR)
+          << "Could not create an HttpServiceRequest object. request url: "
+          << request_url.spec() << " request body: " << request_body;
       return E_FAIL;
+    }
 
     auto extracted_param =
         request->WaitForResponseFromHttpService(request_timeout);
@@ -445,6 +458,7 @@ HRESULT WinHttpUrlFetcher::BuildRequestAndFetchResultFromHttpService(
       continue;
 
     *request_result = std::move(extracted_param);
+
     const base::Value::Dict* error_detail =
         (*request_result)->GetDict().FindDict(kErrorKeyInRequestResult);
     if (!error_detail)
@@ -461,6 +475,7 @@ HRESULT WinHttpUrlFetcher::BuildRequestAndFetchResultFromHttpService(
     }
   }
 
+  LOGFN(ERROR) << "Unable to serve http service request";
   return E_FAIL;
 }
 
