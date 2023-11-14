cfa83e194b2c2a11e68727cc3e4ed576c4157403
Nihar Majmudar <niharm@google.com>
2023-11-14 22:09:59 +0000

    [realbox][cr23] Update realbox expanded state layout to cr23.
    This CL makes updates to the realbox expanded state layout for cr23.
    Updates include new action chip design, realbox padding and realbox
    focus selector.
    
    Before: https://screenshot.googleplex.com/3nu9tWaSrxCNfNG
    After: https://screenshot.googleplex.com/A3WoXshDz7Sqx3P
    
    Bug: 1496336
    Change-Id: I12d9d5dea6d1113940da2daed8cb6a76ba72e5f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4995124
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Khalid Peer <khalidpeer@chromium.org>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1224563}

--- ui/webui/resources/cr_components/omnibox/realbox_dropdown.ts
+++ ui/webui/resources/cr_components/omnibox/realbox_dropdown.ts
@@ -57,6 +57,12 @@ export class RealboxDropdownElement extends PolymerElement {
         value: false,
       },
 
+      expandedStateLayoutChromeRefresh: {
+        type: Boolean,
+        value: () => loadTimeData.getBoolean('realboxCr23ExpandedStateLayout'),
+        reflectToAttribute: true,
+      },
+
       /**
        * Whether the secondary side was at any point available to be shown.
        */
@@ -118,6 +124,7 @@ export class RealboxDropdownElement extends PolymerElement {
   }
 
   canShowSecondarySide: boolean;
+  expandedStateLayoutChromeRefresh: boolean;
   hadSecondarySide: boolean;
   hasSecondarySide: boolean;
   result: AutocompleteResult;
