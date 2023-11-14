138ca0952852f8e09795489404102277cf950741
Xiaohan Wang <xhwang@chromium.org>
2023-11-14 01:10:05 +0000

    media: Set HDRMetadata in DecryptingVideoDecoder
    The CDM interface doesn't support HDRMetadata yet, so the decoded
    VideoFrame doesn't have HDRMetadata set. In this case, use the config's
    HDRMetadata.
    
    Bug: b/183748013
    Test: Added unit tests
    Change-Id: If1cd72082db7c5954789c0a59c9b0159b057fe56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5021246
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224009}

--- media/base/test_helpers.cc
+++ media/base/test_helpers.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/decoder_buffer.h"
 #include "media/base/media_util.h"
 #include "media/base/mock_filters.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/gfx/geometry/rect.h"
 
 using ::testing::_;
@@ -225,6 +226,24 @@ VideoDecoderConfig TestVideoConfig::NormalEncrypted(VideoCodec codec,
 VideoDecoderConfig TestVideoConfig::NormalRotated(VideoRotation rotation) {
   return GetTestConfig(VideoCodec::kVP8, MinProfile(VideoCodec::kVP8),
                        VideoColorSpace::JPEG(), rotation, kNormalSize, false);
+}
+
+VideoDecoderConfig TestVideoConfig::NormalHdr(VideoCodec codec) {
+  auto config = Normal(codec);
+  config.set_color_space_info(
+      VideoColorSpace::FromGfxColorSpace(gfx::ColorSpace::CreateHDR10()));
+  config.set_hdr_metadata(
+      gfx::HDRMetadata::PopulateUnspecifiedWithDefaults(absl::nullopt));
+  return config;
+}
+
+VideoDecoderConfig TestVideoConfig::NormalHdrEncrypted(VideoCodec codec) {
+  auto config = NormalEncrypted(codec);
+  config.set_color_space_info(
+      VideoColorSpace::FromGfxColorSpace(gfx::ColorSpace::CreateHDR10()));
+  config.set_hdr_metadata(
+      gfx::HDRMetadata::PopulateUnspecifiedWithDefaults(absl::nullopt));
+  return config;
 }
 
 // static
