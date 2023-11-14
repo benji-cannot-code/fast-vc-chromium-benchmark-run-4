d8248b1dc1124bc004e614f59ae3247de9d180d3
Jood Hajeer <jood@google.com>
2023-11-14 10:32:07 +0000

    [iOS] Add feature flag to guard account settings review promo
    This CL adds a feature flag for account settings review promo that will
    be shown in the bookmarks and reading lists manager, when the
    corresponding type is disabled.
    
    This will be launched as a fast followup to
    kReplaceSyncPromosaWithSignInPromos.
    
    Bug: 1459255
    Change-Id: I5f37792ac9739ef43febb29e3160dd6002c76035
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5024523
    Commit-Queue: Jood Hajeer <jood@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224182}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -646,7 +646,8 @@ const FeatureEntry::Choice kReplaceSyncPromosWithSignInPromosChoices[] = {
      "SyncEnableWalletOfferInTransportMode,"
      "IOSPasswordSettingsBulkUploadLocalPasswords,"
      "HistoryOptInForRestoreShortyAndReSignin,"
-     "EnableBatchUploadFromBookmarksManager"},
+     "EnableBatchUploadFromBookmarksManager,"
+     "EnableReviewAccountSettingsPromo"},
 };
 
 const FeatureEntry::FeatureParam kOneTapForMapsConsentModeDefault[] = {
