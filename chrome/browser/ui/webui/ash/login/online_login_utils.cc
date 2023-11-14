1a9e4c2d055f19349b75049af9c11cc7df50fe2e
Renato Silva <rrsilva@google.com>
2023-11-14 13:49:54 +0000

    [OOBE] Move utility for checking family link
    Rename ShouldCheckUserTypeBeforeAllowing into IsFamilyLinkAllowed and
    move it into online_login_utils for an upcoming refactoring of ChromeOS
    account creation.
    
    Bug: b/292242156
    Change-Id: I2ef15c40f55dbd2a0851cebafd3c9426192a04f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5028658
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1224244}

--- chrome/browser/ui/webui/ash/login/online_login_utils.cc
+++ chrome/browser/ui/webui/ash/login/online_login_utils.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/signin_partition_manager.h"
 #include "chrome/browser/ash/login/ui/login_display_host_webui.h"
 #include "chrome/browser/ash/login/ui/signin_ui.h"
+#include "chrome/browser/ash/settings/cros_settings.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_process_platform_part.h"
 #include "chrome/common/chrome_features.h"
@@ -222,6 +223,19 @@ AccountId GetAccountId(const std::string& authenticated_email,
   }
 
   return account_id;
+}
+
+bool IsFamilyLinkAllowed() {
+  if (!features::IsFamilyLinkOnSchoolDeviceEnabled()) {
+    return false;
+  }
+
+  CrosSettings* cros_settings = CrosSettings::Get();
+  bool family_link_allowed = false;
+  cros_settings->GetBoolean(kAccountsPrefFamilyLinkAccountsAllowed,
+                            &family_link_allowed);
+
+  return family_link_allowed;
 }
 
 }  // namespace login
