755baf4a2ae9d602199e7ca0f3421325851fd26c
Aaron Leventhal <aleventhal@google.com>
2023-11-14 23:53:40 +0000

    [A11y] Alert events must only be fired on alert roles
    In patchset 1, we move the failing check from Linux-specific code to
    all of the places it needs to be checked in cross-platform code.
    Previously, the alert only failed in some situations on Linux.
    This is stricter and catches the error more often.
    
    The last patchset contains all of the necessary fixes. This CL is
    necessary to green the a11y CI bot.
    
    As part of this change, I'm simplifying the call sites that had
    a hack for MacOS to use alerts where every other OS uses AnnounceText().
    I tracked down when we started doing that and spoke with the engineer,
    and tested the case in question. It seems that AnnounceText() is working
    fine on Mac as long as VoiceOver is running.
    
    Fixed: 1493738,1377831,1369912
    Change-Id: I82449659bd6ca10fd66acc6ae4f8681716aaf7f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4980070
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224618}

--- ui/accessibility/platform/ax_platform_node_auralinux.cc
+++ ui/accessibility/platform/ax_platform_node_auralinux.cc
@@ -4014,7 +4014,6 @@ void AXPlatformNodeAuraLinux::OnAriaCurrentChanged() {
 }
 
 void AXPlatformNodeAuraLinux::OnAlertShown() {
-  DCHECK(ui::IsAlert(GetRole()));
   atk_object_notify_state_change(ATK_OBJECT(GetOrCreateAtkObject()),
                                  ATK_STATE_SHOWING, TRUE);
 }
