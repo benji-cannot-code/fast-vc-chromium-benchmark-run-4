b759dab7629e6d707996c2e1f0023dbe6d69c954
Max Ihlenfeldt <max@igalia.com>
2023-11-14 18:32:43 +0000

    IWYU for c/user_education/common/product_messaging_controller.cc
    Bug: None
    Change-Id: I3939f872c69bec7a16d61b3c1cf502a652a7efe9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5030000
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Max Ihlenfeldt <max@igalia.com>
    Auto-Submit: Max Ihlenfeldt <max@igalia.com>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224405}

--- components/user_education/common/product_messaging_controller.cc
+++ components/user_education/common/product_messaging_controller.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <sstream>
 #include <utility>
+#include <vector>
 
 #include "base/containers/contains.h"
 #include "base/functional/callback.h"
