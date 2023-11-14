397bf8c5fed71cf1dc7eb6ecbbb328a92fb9ccf3
Christine Smith <christinesm@chromium.org>
2023-11-14 16:24:10 +0000

    [Code Health] Remove expired Blink.Plugin.UpdateTime histogram
    Removes an expired histogram. Per the code health instruction doc,
    an obsoletion message is not required for this change.
    
    Bug: 1448153
    Change-Id: I95d249be767e6f858228a275ff56812e695bcf3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5017649
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Christine Smith <christinesm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224324}

--- third_party/blink/renderer/core/page/plugin_data.cc
+++ third_party/blink/renderer/core/page/plugin_data.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/page/plugin_data.h"
 
-#include "base/metrics/histogram_macros.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/blink/public/common/thread_safe_browser_interface_broker_proxy.h"
 #include "third_party/blink/public/mojom/plugins/plugin_registry.mojom-blink.h"
@@ -101,8 +100,6 @@ void PluginData::RefreshBrowserSidePluginCache() {
 void PluginData::UpdatePluginList() {
   if (updated_)
     return;
-
-  SCOPED_UMA_HISTOGRAM_TIMER("Blink.Plugin.UpdateTime");
   ResetPluginData();
   updated_ = true;
 
