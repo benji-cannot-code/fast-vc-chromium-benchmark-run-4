ea4e53d93cae5924d656de21bb17f0ed2cdcf081
Steinar H. Gunderson <sesse@chromium.org>
2023-11-14 12:30:41 +0000

    Fix comparison of inherited variables in MPC.
    On an MPC partial hit, if both parents had inherited custom properties,
    we'd compare these inadvertently by pointer instead of value. If we had
    a recalc that involved constructing the StyleVariables object because
    the element they were on was recalculated (like in the style perftest,
    which always invalidates from the root), this would cause us to always
    get a mismatch and then always think we needed to reapply inherited
    properties.
    
    Of course, the MPC should probably at some point get replaced with the
    new StyleVariables object, but we're not modifying the replacement
    logic here.
    
    Style perftest (Zen 2, LTO but no PGO):
    
      Initial style (µs)     Before     After    Perf      95% CI (BCa)
      =================== ========= ========= ======= =================
      ECommerce                8776      8768   +0.1%  [ -0.1%,  +0.3%]
      Encyclopedia            87010     86828   +0.2%  [ +0.0%,  +0.4%]
      Extension              117365    117634   -0.2%  [ -0.5%,  -0.0%]
      News                    37648     37581   +0.2%  [ -0.0%,  +0.4%]
      Search                  10835     10855   -0.2%  [ -0.3%,  -0.0%]
      Social1                 22259     21739   +2.4%  [ +2.2%,  +2.6%]
      Social2                 14311     14297   +0.1%  [ -0.1%,  +0.3%]
      Sports                  43983     44061   -0.2%  [ -0.3%,  -0.1%]
      Video                   32252     32197   +0.2%  [ +0.0%,  +0.3%]
      Geometric mean                            +0.3%  [ +0.1%,  +0.4%]
    
      Recalc style (µs)      Before     After    Perf      95% CI (BCa)
      =================== ========= ========= ======= =================
      ECommerce                9575      7038  +36.0%  [+35.7%, +36.4%]
      Encyclopedia            68143     68492   -0.5%  [ -0.7%,  -0.3%]
      Extension              109665    110233   -0.5%  [ -0.9%,  -0.3%]
      News                    32824     31884   +2.9%  [ +2.7%,  +3.4%]
      Search                   6564      5637  +16.5%  [+16.2%, +16.8%]
      Social1                 14431     13652   +5.7%  [ +5.4%,  +5.9%]
      Social2                 10792     10806   -0.1%  [ -0.4%,  +0.1%]
      Sports                  23556     23632   -0.3%  [ -0.5%,  -0.1%]
      Video                   20890     19038   +9.7%  [ +9.5%, +10.0%]
      Geometric mean                            +7.2%  [ +7.0%,  +7.4%]
    
    Change-Id: I1281113ea334eda68586cdfa7471b4b9299d562a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5023983
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224217}

--- third_party/blink/renderer/core/css/resolver/style_resolver.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver.cc
@@ -2151,8 +2151,9 @@ bool StyleResolver::CacheSuccess::InheritedVariablesChanged(
   if (!cached_matched_properties) {
     return false;
   }
-  return cached_matched_properties->computed_style->InheritedVariables() !=
-         builder.InheritedVariables();
+  return !base::ValuesEquivalent(
+      cached_matched_properties->computed_style->InheritedVariables(),
+      builder.InheritedVariables());
 }
 
 bool StyleResolver::CacheSuccess::LineHeightChanged(
