660d222d52925cb257f39546224f34d9376f37e1
Elmehdi Rahmaoui <erahmaoui@google.com>
2023-11-14 10:39:24 +0000

    [iOS] Improve Unit Conversion VC's close button contrast
    This cl replaces the close button's xmark color from tertiary to
    secondary improving the overall contrast in dark mode.
    
    Bug: 1501799
    Change-Id: I20626b33e1d1adeaec6b28080c6150e7f136893d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029653
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1224185}

--- ios/chrome/browser/ui/unit_conversion/unit_conversion_view_controller.mm
+++ ios/chrome/browser/ui/unit_conversion/unit_conversion_view_controller.mm
@@ -178,7 +178,7 @@ ios::provider::UnitType TypeByUnit(NSUnit* unit) {
   [closeButton
       setImage:SymbolWithPalette(closeIcon,
                                  @[
-                                   [UIColor colorNamed:kTextTertiaryColor],
+                                   [UIColor colorNamed:kTextSecondaryColor],
                                    [UIColor colorNamed:kGrey200Color]
                                  ])
       forState:UIControlStateNormal];
