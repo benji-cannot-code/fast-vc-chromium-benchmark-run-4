647d3b9aea6e0169f4a45cdca8a1ed9df1f7d786
Peter Kasting <pkasting@chromium.org>
2023-11-14 00:38:03 +0000

    Support NO_UNIQUE_ADDRESS on Windows, and ban [[no_unique_address]].
    For Reasons, we need to use [[msvc::no_unique_address]] on Windows. Make
    the NO_UNIQUE_ADDRESS macro do this and ban direct use of the standard
    attribute. :(
    
    Bug: 1414621
    Change-Id: I026f789b56af7eebeb222fcd78a63ae4c4a76473
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5026084
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1223993}

--- base/compiler_specific.h
+++ base/compiler_specific.h
@@ -111,7 +111,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // References:
 // * https://en.cppreference.com/w/cpp/language/attributes/no_unique_address
 // * https://wg21.link/dcl.attr.nouniqueaddr
-#if HAS_CPP_ATTRIBUTE(no_unique_address)
+#if defined(COMPILER_MSVC) && HAS_CPP_ATTRIBUTE(msvc::no_unique_address)
+// Unfortunately MSVC ignores [[no_unique_address]] (see
+// https://devblogs.microsoft.com/cppblog/msvc-cpp20-and-the-std-cpp20-switch/#msvc-extensions-and-abi),
+// and clang-cl matches it for ABI compatibility reasons. We need to prefer
+// [[msvc::no_unique_address]] when available if we actually want any effect.
+#define NO_UNIQUE_ADDRESS [[msvc::no_unique_address]]
+#elif HAS_CPP_ATTRIBUTE(no_unique_address)
 #define NO_UNIQUE_ADDRESS [[no_unique_address]]
 #else
 #define NO_UNIQUE_ADDRESS
