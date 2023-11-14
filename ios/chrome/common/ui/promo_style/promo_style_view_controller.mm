448a6380a9cc5ab9fbb5acba07cca8b70e226268
Joemer Ramos <joemerramos@chromium.org>
2023-11-14 09:22:13 +0000

    Fix No Background Color On FRE
    This CL changes a line that shouldn't have initialized a new
    configuration and instead use the existing UIButton configuration.
    
    Fixed: 1500877
    Change-Id: I6a519f57f0de0099197fe3fdeab72cd5b4662814
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5024657
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1224154}

--- ios/chrome/common/ui/promo_style/promo_style_view_controller.mm
+++ ios/chrome/common/ui/promo_style/promo_style_view_controller.mm
@@ -1081,7 +1081,7 @@ const CGFloat kHeaderImageShadowShadowInset = 20;
   _buttonUpdated = YES;
   HighlightButton* primaryActionButton = self.primaryActionButton;
   UIButtonConfiguration* buttonConfiguration =
-      [UIButtonConfiguration plainButtonConfiguration];
+      primaryActionButton.configuration;
   buttonConfiguration.attributedTitle = nil;
   buttonConfiguration.title = self.primaryActionString;
   primaryActionButton.configuration = buttonConfiguration;
