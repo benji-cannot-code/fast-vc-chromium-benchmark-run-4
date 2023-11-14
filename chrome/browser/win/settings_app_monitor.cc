60a327b6dc85b71954b79a9b8545b68539bcb41a
Christine Smith <christinesm@chromium.org>
2023-11-14 16:05:41 +0000

    [Code Health] Remove expired DefaultBrowser.Win10ChooserInvoked histogram
    Removes an expired histogram. Per the code health instruction doc,
    an obsoletion message is not required for this change.
    
    Bug: 1448174
    Change-Id: Ic482d6c916876c3c819f820d2cdf4fda722c721f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5017504
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Christine Smith <christinesm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224314}

--- chrome/browser/win/settings_app_monitor.cc
+++ chrome/browser/win/settings_app_monitor.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/location.h"
 #include "base/memory/scoped_refptr.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/strings/pattern.h"
 #include "base/strings/string_util.h"
 #include "base/synchronization/lock.h"
@@ -253,11 +252,12 @@ void SettingsAppMonitor::AutomationControllerDelegate::MaybeInvokeChooser(
 
   // Invoke the dialog and record whether it was successful.
   Microsoft::WRL::ComPtr<IUIAutomationInvokePattern> invoke_pattern;
-  bool succeeded = SUCCEEDED(browser_button->GetCachedPatternAs(
-                       UIA_InvokePatternId, IID_PPV_ARGS(&invoke_pattern))) &&
-                   invoke_pattern && SUCCEEDED(invoke_pattern->Invoke());
-
-  UMA_HISTOGRAM_BOOLEAN("DefaultBrowser.Win10ChooserInvoked", succeeded);
+  if (!invoke_pattern) {
+    return;
+  }
+  browser_button->GetCachedPatternAs(UIA_InvokePatternId,
+                                     IID_PPV_ARGS(&invoke_pattern));
+  invoke_pattern->Invoke();
 }
 
 SettingsAppMonitor::SettingsAppMonitor(Delegate* delegate)
