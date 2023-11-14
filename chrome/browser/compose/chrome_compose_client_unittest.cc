a64cc4f87c58a76cb5384158034d6a43120aaa6d
Trevor Perrier <perrier@chromium.org>
2023-11-14 21:23:33 +0000

    Add UMA to record Compose selection length
    The selection length is recorded once per session start in UTF8 bytes.
    
    Bug: b:308197604
    Change-Id: I977ae760d075cd5245b8c1d43bc65896d418e4ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029276
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224540}

--- chrome/browser/compose/chrome_compose_client_unittest.cc
+++ chrome/browser/compose/chrome_compose_client_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
+#include "base/strings/utf_string_conversion_utils.h"
 #include "base/test/bind.h"
 #include "base/test/gmock_callback_support.h"
 #include "base/test/metrics/histogram_tester.h"
@@ -237,8 +238,6 @@ class ChromeComposeClientTest : public BrowserWithTestWindowTest {
 };
 
 TEST_F(ChromeComposeClientTest, TestCompose) {
-  base::HistogramTester histogram_tester;
-
   ShowDialogAndBindMojo();
   EXPECT_CALL(model_executor(), ExecuteModel(_, _, _))
       .WillOnce(testing::WithArg<2>(testing::Invoke(
@@ -266,12 +265,12 @@ TEST_F(ChromeComposeClientTest, TestCompose) {
   EXPECT_EQ("Cucumbers", result->result);
 
   // Check that a response result OK metric was emitted.
-  histogram_tester.ExpectUniqueSample(compose::kComposeResponseStatus,
-                                      compose::mojom::ComposeStatus::kOk, 1);
+  histograms().ExpectUniqueSample(compose::kComposeResponseStatus,
+                                  compose::mojom::ComposeStatus::kOk, 1);
   // Check that a response duration OK metric was emitted.
-  histogram_tester.ExpectTotalCount(compose::kComposeResponseDurationOk, 1);
+  histograms().ExpectTotalCount(compose::kComposeResponseDurationOk, 1);
   // Check that a no response duration Error metric was emitted.
-  histogram_tester.ExpectTotalCount(compose::kComposeResponseDurationError, 0);
+  histograms().ExpectTotalCount(compose::kComposeResponseDurationError, 0);
 }
 
 TEST_F(ChromeComposeClientTest, TestComposeParams) {
@@ -336,7 +335,6 @@ TEST_F(ChromeComposeClientTest, TestComposeNoResponse) {
 // Tests that we return an error if Optimization Guide is unable to parse the
 // response. In this case the response will be absl::nullopt.
 TEST_F(ChromeComposeClientTest, TestComposeNoParsedAny) {
-  base::HistogramTester histogram_tester;
   ShowDialogAndBindMojo();
   EXPECT_CALL(model_executor(), ExecuteModel(_, _, _))
       .WillOnce(testing::WithArg<2>(testing::Invoke(
@@ -361,13 +359,12 @@ TEST_F(ChromeComposeClientTest, TestComposeNoParsedAny) {
   EXPECT_EQ(compose::mojom::ComposeStatus::kTryAgain, result->status);
 
   // Check that a response result Try-Again metric was emitted.
-  histogram_tester.ExpectUniqueSample(compose::kComposeResponseStatus,
-                                      compose::mojom::ComposeStatus::kTryAgain,
-                                      1);
+  histograms().ExpectUniqueSample(compose::kComposeResponseStatus,
+                                  compose::mojom::ComposeStatus::kTryAgain, 1);
   // Check that a response duration Error metric was emitted.
-  histogram_tester.ExpectTotalCount(compose::kComposeResponseDurationError, 1);
+  histograms().ExpectTotalCount(compose::kComposeResponseDurationError, 1);
   // Check that a no response duration OK metric was emitted.
-  histogram_tester.ExpectTotalCount(compose::kComposeResponseDurationOk, 0);
+  histograms().ExpectTotalCount(compose::kComposeResponseDurationOk, 0);
 }
 
 TEST_F(ChromeComposeClientTest, TestOptimizationGuideDisabled) {
@@ -864,8 +861,18 @@ TEST_F(ChromeComposeClientTest, TestAutoCompose) {
   EXPECT_CALL(model_executor(), ExecuteModel(_, _, _))
       .WillOnce(base::test::RunOnceClosure(execute_model_future.GetCallback()));
 
-  SetSelection(u"testing alpha bravo charlie");
+  std::u16string selected_text = u"ŧëśŧĩňĝ âľpħâ ƅřâɤō ĉħâŗľĩë";
+  std::string selected_text_utf8 = base::UTF16ToUTF8(selected_text);
+  SetSelection(selected_text);
   ShowDialogAndBindMojo();
+
+  // Check that the UTF8 byte length has zero counts.
+  histograms().ExpectBucketCount(compose::kComposeDialogSelectionLength,
+                                 base::UTF16ToUTF8(selected_text).size(), 0);
+  // Check that the number of UTF8 code points has one count.
+  histograms().ExpectBucketCount(
+      compose::kComposeDialogSelectionLength,
+      base::CountUnicodeCharacters(selected_text_utf8).value(), 1);
 
   base::test::TestFuture<compose::mojom::OpenMetadataPtr> open_test_future;
   page_handler()->RequestInitialState(open_test_future.GetCallback());
@@ -882,6 +889,9 @@ TEST_F(ChromeComposeClientTest, TestAutoComposeTooLong) {
   words += u" b c";
   SetSelection(words);
   ShowDialogAndBindMojo();
+
+  histograms().ExpectBucketCount(compose::kComposeDialogSelectionLength,
+                                 base::UTF16ToUTF8(words).size(), 1);
 
   base::test::TestFuture<compose::mojom::OpenMetadataPtr> open_test_future;
   page_handler()->RequestInitialState(open_test_future.GetCallback());
