bbce67aebc7ff9df3b767e87a74d4bc72e5b75cd
Stephen McGruer <smcgruer@chromium.org>
2023-11-14 19:54:47 +0000

    [Autofill] Launch card art + name on iOS
    This has been approved for 100% launch (with a holdback), and
    so per Finch guidelines changing the feature flags to default
    true. The following feature flags are now enabled by default
    on iOS:
    
      - AutofillEnableCardArtImage
      - AutofillEnableCardProductName
      - AutofillUseTwoDotsForLastFourDigits
    
    Launch - launch/4251753
    
    Bug: 1444048
    Change-Id: I9b6bafb543b7cda73df759f14c07b0039a65d9b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029580
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224486}

--- components/autofill/core/common/autofill_payments_features.cc
+++ components/autofill/core/common/autofill_payments_features.cc
@@ -33,7 +33,11 @@ BASE_FEATURE(kAutofillEnableAndroidNKeyForFidoAuthentication,
 // Payments Autofill UI.
 BASE_FEATURE(kAutofillEnableCardArtImage,
              "AutofillEnableCardArtImage",
+#if BUILDFLAG(IS_IOS)
+             base::FEATURE_ENABLED_BY_DEFAULT);
+#else
              base::FEATURE_DISABLED_BY_DEFAULT);
+#endif
 
 // When enabled, server will return card art images of the exact required
 // dimension.
@@ -45,7 +49,11 @@ BASE_FEATURE(kAutofillEnableCardArtServerSideStretching,
 // Payments Autofill UI.
 BASE_FEATURE(kAutofillEnableCardProductName,
              "AutofillEnableCardProductName",
+#if BUILDFLAG(IS_IOS)
+             base::FEATURE_ENABLED_BY_DEFAULT);
+#else
              base::FEATURE_DISABLED_BY_DEFAULT);
+#endif
 
 // When enabled, we will store CVC for both local and server credit cards. This
 // will also allow the users to autofill their CVCs on checkout pages.
@@ -179,11 +187,10 @@ BASE_FEATURE(kAutofillEnableStickyManualFallbackForCards,
 BASE_FEATURE(kAutofillEnableUpdateVirtualCardEnrollment,
              "AutofillEnableUpdateVirtualCardEnrollment",
 #if BUILDFLAG(IS_IOS)
-             base::FEATURE_DISABLED_BY_DEFAULT
+             base::FEATURE_DISABLED_BY_DEFAULT);
 #else
-             base::FEATURE_ENABLED_BY_DEFAULT
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif
-);
 
 // When enabled, the vcn enroll screen will present a loading spinner while
 // enrolling the card to the server and present a confirmation screen with the
@@ -251,7 +258,7 @@ BASE_FEATURE(kAutofillUpstreamAllowAllEmailDomains,
 // card number. (E.g., '•• 8888' rather than '•••• 8888').
 BASE_FEATURE(kAutofillUseTwoDotsForLastFourDigits,
              "AutofillUseTwoDotsForLastFourDigits",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // When this and the above `kAutofillEnablePaymentsMandatoryReauth` are both
 // enabled, in use-cases where we would not have triggered any user-visible
