094226baacd6b5737f77928eb5ffcff8d3d95a80
Md. Sadiqul Amin <sadiqul.amin@samsung.com>
2023-11-14 17:59:52 +0000

    Use base::Contains() instead of std::find()
    Use base::Contains() instead of std;:find() where appropriate for better readability.
    
    Bug: 561800
    
    Signed-off-by: Md. Sadiqul Amin <sadiqul.amin@samsung.com>
    Change-Id: Id0db6523ae621ca64e33a20864a04a2c50eaa760
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5016127
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224390}

--- components/commerce/core/shopping_service_test_base.cc
+++ components/commerce/core/shopping_service_test_base.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/commerce/core/shopping_service_test_base.h"
 
 #include "base/command_line.h"
+#include "base/containers/contains.h"
 #include "base/containers/flat_map.h"
 #include "base/memory/ref_counted.h"
 #include "base/notreached.h"
@@ -291,8 +292,7 @@ OptimizationMetadata MockOptGuideDecider::BuildDiscountsResponse(
 
   std::vector<DiscountClusterType> checked_cluster_types;
   for (const auto& info_to_check : infos) {
-    if (std::find(checked_cluster_types.begin(), checked_cluster_types.end(),
-                  info_to_check.cluster_type) != checked_cluster_types.end()) {
+    if (base::Contains(checked_cluster_types, info_to_check.cluster_type)) {
       continue;
     }
     checked_cluster_types.push_back(info_to_check.cluster_type);
