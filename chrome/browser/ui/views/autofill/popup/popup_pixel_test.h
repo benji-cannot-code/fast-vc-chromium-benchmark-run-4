ca14ea5b31cc6744bf7f2815bf201ce1077fb75f
Jan Keitel <jkeitel@google.com>
2023-11-14 09:35:07 +0000

    Use concepts in PopupPixelTest.
    Bug: 1469871
    Change-Id: I2abfa977631c7171a79366dd785abf37d0ad5bf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029656
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1224160}

--- chrome/browser/ui/views/autofill/popup/popup_pixel_test.h
+++ chrome/browser/ui/views/autofill/popup/popup_pixel_test.h
@@ -6,13 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_AUTOFILL_POPUP_POPUP_PIXEL_TEST_H_
 #define CHROME_BROWSER_UI_VIEWS_AUTOFILL_POPUP_POPUP_PIXEL_TEST_H_
 
+#include <concepts>
 #include <string>
 #include <tuple>
-#include <type_traits>
 
 #include "base/i18n/rtl.h"
 #include "base/memory/raw_ptr.h"
 #include "base/strings/strcat.h"
+#include "chrome/browser/ui/autofill/autofill_popup_view_delegate.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -36,13 +37,11 @@ using TestParameterType = std::tuple<bool, bool>;
 // A base class to do pixel tests for classes that derive from `PopupBaseView`.
 // By default, the test class has two parameters: Dark vs light mode and RTL vs
 // LTR for the text direction of the browser language.
-template <typename View, typename Controller>
+template <std::derived_from<PopupBaseView> View,
+          std::derived_from<AutofillPopupViewDelegate> Controller>
 class PopupPixelTest : public UiBrowserTest,
                        public testing::WithParamInterface<TestParameterType> {
  public:
-  static_assert(std::is_base_of_v<PopupBaseView, View>);
-  static_assert(std::is_base_of_v<AutofillPopupViewDelegate, Controller>);
-
   PopupPixelTest() = default;
   ~PopupPixelTest() override = default;
 
