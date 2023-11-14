03abd20a30ac2119c58e0349c33a18e46184db15
Ming-Ying Chung <mych@chromium.org>
2023-11-14 08:01:29 +0000

    [keepalive-migration] Check mixed content when RFH is alive
    - The existing implementation in Blink [1] does not block mixed
      content [2] fetch keepalive requests, tracked by
      https://crbug.com/1500989.
    - The migration proxied fetch keepalive request loading via browser.
    - After recent change [3], the proxied requests are handled in browser
      first, and the redirects/responses will be forwarded back to
      renderer later (if still alive). It is better for browser to perform
      the same checks as what are done in renderer, including mixed content
      checking.
    
    [1]: https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/loader/frame_fetch_context.cc;l=660-663;drc=719c18366c0226fa751f38693b00f822d1b7ea9b
    [2]: https://www.w3.org/TR/mixed-content/
    [3]: https://crrev.com/c/4717687
    
    Bug: 1356128
    Change-Id: Ifc7efe8d2b015bcf4d9d3f8229bb0997852847c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4988570
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224130}

--- content/browser/loader/keep_alive_url_loader.h
+++ content/browser/loader/keep_alive_url_loader.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "base/types/pass_key.h"
 #include "content/common/content_export.h"
+#include "content/public/browser/weak_document_ptr.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
@@ -42,6 +43,7 @@ namespace content {
 class BrowserContext;
 class KeepAliveURLLoaderService;
 class PolicyContainerHost;
+class WeakDocumentPtr;
 
 // A URLLoader for loading a fetch keepalive request via the browser process,
 // including requests generated from the following JS API calls:
@@ -102,6 +104,8 @@ class CONTENT_EXPORT KeepAliveURLLoader
   // service if it is still connected.
   // `delete_callback` is a callback to delete this object.
   // `policy_container_host` must not be null.
+  // `weak_document_ptr` should point to the document that initiates
+  // `resource_request`.
   KeepAliveURLLoader(
       int32_t request_id,
       uint32_t options,
@@ -110,6 +114,7 @@ class CONTENT_EXPORT KeepAliveURLLoader
       const net::MutableNetworkTrafficAnnotationTag& traffic_annotation,
       scoped_refptr<network::SharedURLLoaderFactory> network_loader_factory,
       scoped_refptr<PolicyContainerHost> policy_container_host,
+      WeakDocumentPtr weak_document_ptr,
       BrowserContext* browser_context,
       std::vector<std::unique_ptr<blink::URLLoaderThrottle>> throttles,
       base::PassKey<KeepAliveURLLoaderService>);
@@ -298,6 +303,12 @@ class CONTENT_EXPORT KeepAliveURLLoader
   // initiates this loader alive until `this` is destroyed.
   // It is never null.
   scoped_refptr<PolicyContainerHost> policy_container_host_;
+
+  // Points to the document that initiates this loader.
+  // It may become null at any moment whenever the RenderFrameHost it points to
+  // is deleted or navigates to a different document. See its classdoc for more
+  // details.
+  WeakDocumentPtr weak_document_ptr_;
 
   // The BrowserContext that initiates this loader.
   // It is ensured to outlive this because it owns KeepAliveURLLoaderService
