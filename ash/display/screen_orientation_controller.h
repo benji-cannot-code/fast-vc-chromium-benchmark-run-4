3b5f92fbe34109b7a98fe694c2e6a7bd3ae2ae14
Thomas Lukaszewicz <tluk@chromium.org>
2023-11-14 19:46:57 +0000

    [display] Propagate change state on display config update
    This CL updates OnDidProcessDisplayChanges() to propagate a struct
    of changes to the display configuration. This API is currently only
    implemented by ChromeOS Ash.
    
    There are several DisplayManager methods that modify the existing
    display configuration and these methods can call each other in a
    nested fashion. BeginEndNotifier exists to allow a single call to
    OnWillProcessDisplayChanges() and OnDidProcessDisplayChanges() on
    either side of an atomic change in display configuration state.
    
    This CL introduces DisplayManager::PendingDisplayChanges which is
    intended to track all display config changes that occur before
    the last BeginEndNotifier falls out of scope. At this time
    this is translated to a DisplayConfigurationChange struct more
    appropriate for observers and propagated in the call to
    OnDidProcessDisplayChanges().
    
    This CL also adds a test to ensure what is propagated by
    the DisplayManagerObserver is consistent with what is propagated by
    the DisplayObserver.
    
    Follow up CLs will also
      - update exo to leverage this as a means to consume an atomic
        update to the display configuration state.
    
      - consolidate the other existing mechanisms that communicate
        display configuration changes. For e.g. see
        DisplayManager::Delegate::Pre[Post]DisplayConfigurationChange()
        and downstream methods.
    
      - update clients that rely on other means to infer display
        configuration changes (using OnDisplayAdded[Removed]() for e.g.)
    
    Bug: 1427019, 1408304, 1498683
    Change-Id: I87bdf62b5fe90b7160c03e1f4597059288173959
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4973206
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1224481}

--- ash/display/screen_orientation_controller.h
+++ ash/display/screen_orientation_controller.h
@@ -156,7 +156,8 @@ class ASH_EXPORT ScreenOrientationController
 
   // display::DisplayManagerObserver:
   void OnWillProcessDisplayChanges() override;
-  void OnDidProcessDisplayChanges() override;
+  void OnDidProcessDisplayChanges(
+      const DisplayConfigurationChange& configuration_change) override;
 
  private:
   friend class ScreenOrientationControllerTestApi;
--- components/exo/wayland/zcr_remote_shell_impl.h
+++ components/exo/wayland/zcr_remote_shell_impl.h
@@ -102,7 +102,8 @@ class WaylandRemoteShell : public ash::TabletModeObserver,
 
   // display::DisplayManagerObserver:
   void OnWillProcessDisplayChanges() override;
-  void OnDidProcessDisplayChanges() override;
+  void OnDidProcessDisplayChanges(
+      const DisplayConfigurationChange& configuration_change) override;
 
   // Overridden from ash::TabletModeObserver:
   void OnTabletModeStarted() override;
