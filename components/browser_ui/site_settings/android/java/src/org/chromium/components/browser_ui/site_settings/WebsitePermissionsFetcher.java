2d80faa1da3ff5a636e7503bb097dddd88dca827
Hailey Wang <haileywang@google.com>
2023-11-14 18:31:54 +0000

    Revert "Reland "[Android][SA] Fix null pnt and not grouped by embedder for embargoed""
    This reverts commit 9a957477a9917a808ec30d53f089dc2ed873058e.
    
    Reason for revert: Causing tests failure on builder "android-arm64-tests"
    Bug: 1502189
    
    Original change's description:
    > Reland "[Android][SA] Fix null pnt and not grouped by embedder for embargoed"
    >
    > This is a reland of commit 1e1ddc3fdef10e5155c2e9933dd54c14394efbe0
    >
    > Enabled the Storage Access feature in all the added tests since the
    > fieldtrial flag does not enable the feature for tests in branded builds.
    > Also added NonNull notations for the callback since it cannot be null.
    >
    > Fixed: b:309083164
    > Original change's description:
    > > [Android][SA] Fix null pnt and not grouped by embedder for embargoed
    > >
    > > This CL fixes a null pointer exception on the assert of
    > > addEmbeddedPermission to also include embargoed origins that do not have
    > > a secondary pattern.
    > >
    > > This CL also makes sure that for Storage Access exceptions they are not
    > > grouped by embedder for All Sites.
    > >
    > > We also do some refactoring to the fetcher test class.
    > >
    > > Fix: b:309083164
    > > Bug: 1478113
    > > Change-Id: Iaededf734efb525e2cbf31e3989b3e64d420104f
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5004051
    > > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > > Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    > > Commit-Queue: Filipa Senra <fsenra@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1221683}
    >
    > Bug: 1478113
    > Change-Id: I6b941a9d7b860eb37e0d1448f161ad578cc3aeee
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5012692
    > Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    > Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Auto-Submit: Filipa Senra <fsenra@google.com>
    > Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1223596}
    
    Bug: 1478113
    Change-Id: I4583ff5242be68bb96c3112c5845a26a990f7c1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5030792
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Hailey Wang <haileywang@google.com>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Auto-Submit: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1224404}

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/WebsitePermissionsFetcher.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/WebsitePermissionsFetcher.java
@@ -164,7 +164,7 @@ public class WebsitePermissionsFetcher {
      *
      * @param callback The callback to run when the fetch is complete.
      */
-    public void fetchAllPreferences(@NonNull WebsitePermissionsCallback callback) {
+    public void fetchAllPreferences(WebsitePermissionsCallback callback) {
         var fetcherInternal = new WebsitePermissionFetcherInternal();
         fetcherInternal.fetchAllPreferences(callback);
     }
@@ -176,7 +176,7 @@ public class WebsitePermissionsFetcher {
      * @param callback The callback to run when the fetch is complete.
      */
     public void fetchPreferencesForCategory(
-            SiteSettingsCategory category, @NonNull WebsitePermissionsCallback callback) {
+            SiteSettingsCategory category, WebsitePermissionsCallback callback) {
         var fetcherInternal = new WebsitePermissionFetcherInternal();
         fetcherInternal.fetchPreferencesForCategory(category, callback);
     }
@@ -190,9 +190,8 @@ public class WebsitePermissionsFetcher {
      * @param callback The callback to run when the fetch is complete.
      */
     public void fetchPreferencesForCategoryAndPopulateFpsInfo(
-            SiteSettingsDelegate siteSettingsDelegate,
-            SiteSettingsCategory category,
-            @NonNull WebsitePermissionsCallback callback) {
+            SiteSettingsDelegate siteSettingsDelegate, SiteSettingsCategory category,
+            WebsitePermissionsCallback callback) {
         var fetcherInternal = new WebsitePermissionFetcherInternal();
         fetcherInternal.fetchPreferencesForCategoryAndPopulateFpsInfo(
                 siteSettingsDelegate, category, callback);
@@ -213,7 +212,7 @@ public class WebsitePermissionsFetcher {
          *
          * @param callback The callback to run when the fetch is complete.
          */
-        public void fetchAllPreferences(@NonNull WebsitePermissionsCallback callback) {
+        public void fetchAllPreferences(WebsitePermissionsCallback callback) {
             TaskQueue queue = new TaskQueue();
 
             addAllFetchers(queue);
@@ -240,7 +239,7 @@ public class WebsitePermissionsFetcher {
          * @param callback The callback to run when the fetch is complete.
          */
         public void fetchPreferencesForCategory(
-                SiteSettingsCategory category, @NonNull WebsitePermissionsCallback callback) {
+                SiteSettingsCategory category, WebsitePermissionsCallback callback) {
             TaskQueue queue = createFetchersForCategory(category);
 
             queue.add(new PermissionsAvailableCallbackRunner(callback));
@@ -275,9 +274,8 @@ public class WebsitePermissionsFetcher {
          * @param callback The callback to run when the fetch is complete.
          */
         public void fetchPreferencesForCategoryAndPopulateFpsInfo(
-                SiteSettingsDelegate siteSettingsDelegate,
-                SiteSettingsCategory category,
-                @NonNull WebsitePermissionsCallback callback) {
+                SiteSettingsDelegate siteSettingsDelegate, SiteSettingsCategory category,
+                WebsitePermissionsCallback callback) {
             TaskQueue queue = createFetchersForCategory(category);
             queue.add(new FirstPartySetsInfoFetcher(siteSettingsDelegate));
 
@@ -396,8 +394,6 @@ public class WebsitePermissionsFetcher {
                 String embedder = exception.getSecondaryPattern();
 
                 if (isEmbeddedPermission
-                        && embedder != null
-                        && !embedder.equals(SITE_WILDCARD)
                         && mSiteSettingsCategory != null
                         && mSiteSettingsCategory.getType() == SiteSettingsCategory.Type.ALL_SITES) {
                     // AllSites should group embedded permissions by embedder.
@@ -662,10 +658,9 @@ public class WebsitePermissionsFetcher {
         }
 
         private class PermissionsAvailableCallbackRunner extends Task {
-            private final @NonNull WebsitePermissionsCallback mCallback;
+            private final WebsitePermissionsCallback mCallback;
 
-            private PermissionsAvailableCallbackRunner(
-                    @NonNull WebsitePermissionsCallback callback) {
+            private PermissionsAvailableCallbackRunner(WebsitePermissionsCallback callback) {
                 mCallback = callback;
             }
 
