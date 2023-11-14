2e28aa8ad8324345c473953831f2172295ee7db0
Tao Wu <wutao@chromium.org>
2023-11-14 22:21:18 +0000

    assistant: Shuffle Launcher search chips
    The order of launcher search queries will change every time when showing
    the IPH.
    
    Bug: b/307368089
    Test: added new unittest
    Test: ash_unittests --gtest_filter=*LauncherSearchIphViewTest*
    Change-Id: I68b23de0b14f5ceea6932684f6d1003923247805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5030652
    Reviewed-by: Yuki Awano <yawano@google.com>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224576}

--- ash/assistant/ui/main_stage/launcher_search_iph_view.h
+++ ash/assistant/ui/main_stage/launcher_search_iph_view.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "ash/public/cpp/app_list/app_list_client.h"
+#include "base/memory/raw_ptr.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/events/event.h"
 #include "ui/views/controls/link.h"
@@ -17,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/view.h"
 
 namespace ash {
+
+class ChipView;
 
 class LauncherSearchIphView : public views::View {
   METADATA_HEADER(LauncherSearchIphView, views::View)
@@ -54,17 +57,28 @@ class LauncherSearchIphView : public views::View {
       bool show_assistant_chip = true);
   ~LauncherSearchIphView() override;
 
+  // views::View:
+  void VisibilityChanged(views::View* starting_from, bool is_visible) override;
+
+  std::vector<raw_ptr<ChipView>> GetChipsForTesting();
+
  private:
   // TODO(b/272370530): Use string id for internationalization.
   void RunLauncherSearchQuery(const std::u16string& query);
 
   void OpenAssistantPage();
 
-  raw_ptr<Delegate> delegate_;
+  void CreateQueryChips(views::View* actions_container);
+
+  void ShuffleChipsQuery();
+
+  raw_ptr<Delegate> delegate_ = nullptr;
 
   std::unique_ptr<ScopedIphSession> scoped_iph_session_;
 
   bool show_assistant_chip_ = false;
+
+  std::vector<raw_ptr<ChipView>> chips_;
 
   base::WeakPtrFactory<LauncherSearchIphView> weak_ptr_factory_{this};
 };
