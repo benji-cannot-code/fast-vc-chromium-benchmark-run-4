8f0ee6c41ab22de35a1831ab9c017cf94bb1777a
Peter Boström <pbos@chromium.org>
2023-11-14 00:04:03 +0000

    Downgrade NOTREACHED in PointerEventFactory
    This downgrades a NOTREACHED inside
    blink::PointerEventFactory::PointerTypeNameForWebPointPointerType which is one of the top report signatures on Windows M119.
    
    Downgrading a NOTREACHED to DUMP_WILL_BE_NOTREACHED_NORETURN excludes it
    from the kNotReachedIsFatal experiment and avoids stability risk during
    experiment rollout.
    
    Bug: 851128, 1423607
    Change-Id: I5c4f93ce59edbec755de2a33138c352ec98b037e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5024320
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223972}

--- third_party/blink/renderer/core/events/pointer_event_factory.cc
+++ third_party/blink/renderer/core/events/pointer_event_factory.cc
@@ -155,7 +155,7 @@ const AtomicString& PointerEventFactory::PointerTypeNameForWebPointPointerType(
     case WebPointerProperties::PointerType::kMouse:
       return pointer_type_names::kMouse;
     default:
-      NOTREACHED();
+      DUMP_WILL_BE_NOTREACHED_NORETURN();
       return g_empty_atom;
   }
 }
