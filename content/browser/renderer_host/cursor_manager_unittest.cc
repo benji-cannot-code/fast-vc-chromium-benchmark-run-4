cead471ae659c22408c468634e8dcb11f25c25cf
Jagadesh P <jagadeshjai1999@gmail.com>
2023-11-14 03:28:53 +0000

    [DanglingPointer] Remove dangling pointer in CursorManager Test
    Bug: 1485833
    Change-Id: I3f672e00c888bb86f1d2928158a8d3b243a089bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4986342
    Auto-Submit: Jagadesh Jai <jagadeshjai1999@gmail.com>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Jagadesh Jai <jagadeshjai1999@gmail.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224066}

--- content/browser/renderer_host/cursor_manager_unittest.cc
+++ content/browser/renderer_host/cursor_manager_unittest.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
-#include "base/memory/raw_ptr.h"
 #include "build/build_config.h"
 #include "content/browser/renderer_host/mock_render_widget_host.h"
 #include "content/browser/renderer_host/render_widget_host_impl.h"
@@ -50,8 +49,9 @@ class MockRenderWidgetHostViewForCursors : public TestRenderWidgetHostView {
  public:
   MockRenderWidgetHostViewForCursors(RenderWidgetHost* host, bool top_view)
       : TestRenderWidgetHostView(host) {
-    if (top_view)
+    if (top_view) {
       cursor_manager_ = std::make_unique<CursorManager>(this);
+    }
   }
 
   void DisplayCursor(const ui::Cursor& cursor) override {
@@ -82,8 +82,8 @@ class CursorManagerTest : public testing::Test {
         base::WrapRefCounted(SiteInstanceGroup::CreateForTesting(
             browser_context_.get(), process_host_.get()));
     widget_host_ = MakeNewWidgetHost();
-    top_view_ =
-        new MockRenderWidgetHostViewForCursors(widget_host_.get(), true);
+    top_view_ = std::make_unique<MockRenderWidgetHostViewForCursors>(
+        widget_host_.get(), true);
   }
 
   std::unique_ptr<RenderWidgetHostImpl> MakeNewWidgetHost() {
@@ -94,9 +94,7 @@ class CursorManagerTest : public testing::Test {
   }
 
   void TearDown() override {
-    if (top_view_)
-      delete top_view_;
-
+    top_view_.reset();
     widget_host_ = nullptr;
     process_host_->Cleanup();
     site_instance_group_.reset();
@@ -110,10 +108,7 @@ class CursorManagerTest : public testing::Test {
   std::unique_ptr<MockRenderProcessHost> process_host_;
   scoped_refptr<SiteInstanceGroup> site_instance_group_;
   std::unique_ptr<RenderWidgetHostImpl> widget_host_;
-
-  // Tests should set this to nullptr if they've already triggered its
-  // destruction.
-  raw_ptr<MockRenderWidgetHostViewForCursors, DanglingUntriaged> top_view_;
+  std::unique_ptr<MockRenderWidgetHostViewForCursors> top_view_;
 
   MockRenderWidgetHostDelegate delegate_;
 };
@@ -123,13 +118,13 @@ class CursorManagerTest : public testing::Test {
 // Verify basic CursorManager functionality when no OOPIFs are present.
 TEST_F(CursorManagerTest, CursorOnSingleView) {
   // Simulate mouse over the top-level frame without an UpdateCursor message.
-  top_view_->GetCursorManager()->UpdateViewUnderCursor(top_view_);
+  top_view_->GetCursorManager()->UpdateViewUnderCursor(top_view_.get());
 
   // The view should be using the default cursor.
   EXPECT_EQ(top_view_->cursor(), ui::Cursor());
 
   // Update the view with a non-default cursor.
-  top_view_->GetCursorManager()->UpdateCursor(top_view_, kCursorHand);
+  top_view_->GetCursorManager()->UpdateCursor(top_view_.get(), kCursorHand);
 
   // Verify the RenderWidgetHostView now uses the correct cursor.
   EXPECT_EQ(top_view_->cursor(), kCursorHand);
@@ -168,7 +163,7 @@ TEST_F(CursorManagerTest, CursorOverMultipleChildViews) {
       new MockRenderWidgetHostViewForCursors(widget_host2.get(), false));
 
   // Initialize each View to a different cursor.
-  top_view_->GetCursorManager()->UpdateCursor(top_view_, kCursorHand);
+  top_view_->GetCursorManager()->UpdateCursor(top_view_.get(), kCursorHand);
   top_view_->GetCursorManager()->UpdateCursor(child_view1.get(), kCursorCross);
   top_view_->GetCursorManager()->UpdateCursor(child_view2.get(),
                                               kCursorPointer);
@@ -187,7 +182,7 @@ TEST_F(CursorManagerTest, CursorOverMultipleChildViews) {
   EXPECT_EQ(top_view_->cursor(), kCursorPointer);
   top_view_->GetCursorManager()->UpdateCursor(child_view2.get(), kCursorCross);
   EXPECT_EQ(top_view_->cursor(), kCursorCross);
-  top_view_->GetCursorManager()->UpdateCursor(top_view_, kCursorHand);
+  top_view_->GetCursorManager()->UpdateCursor(top_view_.get(), kCursorHand);
   EXPECT_EQ(top_view_->cursor(), kCursorCross);
 
   // Similarly, destroying child_view1 should have no effect on the cursor,
@@ -200,7 +195,7 @@ TEST_F(CursorManagerTest, CursorOverMultipleChildViews) {
 
 TEST_F(CursorManagerTest,
        CustomCursorDisallowedScope_CustomCursorsAreNotAllowed) {
-  top_view_->GetCursorManager()->UpdateCursor(top_view_, kCursorCustom);
+  top_view_->GetCursorManager()->UpdateCursor(top_view_.get(), kCursorCustom);
   EXPECT_EQ(top_view_->cursor(), kCursorCustom);
 
   {
@@ -217,7 +212,8 @@ TEST_F(CursorManagerTest,
        CustomCursorDisallowedScope_CustomCursorsAreNotAllowedAboveSizeLimit) {
   const ui::Cursor kCursorCustomLarge = CreateCustomCursor(20, 50);
 
-  top_view_->GetCursorManager()->UpdateCursor(top_view_, kCursorCustomLarge);
+  top_view_->GetCursorManager()->UpdateCursor(top_view_.get(),
+                                              kCursorCustomLarge);
   EXPECT_EQ(top_view_->cursor(), kCursorCustomLarge);
 
   {
@@ -234,7 +230,8 @@ TEST_F(CursorManagerTest,
        CustomCursorDisallowedScope_CustomCursorsAreAllowedBelowSizeLimit) {
   const ui::Cursor kCursorCustomLarge = CreateCustomCursor(20, 35);
 
-  top_view_->GetCursorManager()->UpdateCursor(top_view_, kCursorCustomLarge);
+  top_view_->GetCursorManager()->UpdateCursor(top_view_.get(),
+                                              kCursorCustomLarge);
   EXPECT_EQ(top_view_->cursor(), kCursorCustomLarge);
 
   {
@@ -251,7 +248,8 @@ TEST_F(CursorManagerTest,
        CustomCursorDisallowedScope_CustomCursorSubjectToMultipleSizeLimits) {
   const ui::Cursor kCursorCustomLarge = CreateCustomCursor(20, 35);
 
-  top_view_->GetCursorManager()->UpdateCursor(top_view_, kCursorCustomLarge);
+  top_view_->GetCursorManager()->UpdateCursor(top_view_.get(),
+                                              kCursorCustomLarge);
   EXPECT_EQ(top_view_->cursor(), kCursorCustomLarge);
 
   {
@@ -276,7 +274,7 @@ TEST_F(CursorManagerTest,
 
 TEST_F(CursorManagerTest,
        CustomCursorDisallowedScope_OtherCursorsStillAllowed) {
-  top_view_->GetCursorManager()->UpdateCursor(top_view_, kCursorHand);
+  top_view_->GetCursorManager()->UpdateCursor(top_view_.get(), kCursorHand);
   EXPECT_EQ(top_view_->cursor(), kCursorHand);
 
   {
@@ -291,7 +289,7 @@ TEST_F(CursorManagerTest,
 
 TEST_F(CursorManagerTest,
        CustomCursorDisallowedScope_CustomCursorSetDuringScope) {
-  top_view_->GetCursorManager()->UpdateCursor(top_view_, kCursorHand);
+  top_view_->GetCursorManager()->UpdateCursor(top_view_.get(), kCursorHand);
   EXPECT_EQ(top_view_->cursor(), kCursorHand);
 
   {
@@ -300,7 +298,7 @@ TEST_F(CursorManagerTest,
             /*max_dimension_dips=*/0);
     EXPECT_EQ(top_view_->cursor(), kCursorHand);
 
-    top_view_->GetCursorManager()->UpdateCursor(top_view_, kCursorCustom);
+    top_view_->GetCursorManager()->UpdateCursor(top_view_.get(), kCursorCustom);
     EXPECT_EQ(top_view_->cursor(), kCursorPointer);
   }
 
@@ -309,7 +307,7 @@ TEST_F(CursorManagerTest,
 
 TEST_F(CursorManagerTest,
        CustomCursorDisallowedScope_CustomCursorRemovedDuringScope) {
-  top_view_->GetCursorManager()->UpdateCursor(top_view_, kCursorCustom);
+  top_view_->GetCursorManager()->UpdateCursor(top_view_.get(), kCursorCustom);
   EXPECT_EQ(top_view_->cursor(), kCursorCustom);
 
   {
@@ -318,7 +316,7 @@ TEST_F(CursorManagerTest,
             /*max_dimension_dips=*/0);
     EXPECT_EQ(top_view_->cursor(), kCursorPointer);
 
-    top_view_->GetCursorManager()->UpdateCursor(top_view_, kCursorHand);
+    top_view_->GetCursorManager()->UpdateCursor(top_view_.get(), kCursorHand);
     EXPECT_EQ(top_view_->cursor(), kCursorHand);
   }
 
@@ -326,7 +324,7 @@ TEST_F(CursorManagerTest,
 }
 
 TEST_F(CursorManagerTest, CustomCursorDisallowedScope_MultipleScopes) {
-  top_view_->GetCursorManager()->UpdateCursor(top_view_, kCursorCustom);
+  top_view_->GetCursorManager()->UpdateCursor(top_view_.get(), kCursorCustom);
   EXPECT_EQ(top_view_->cursor(), kCursorCustom);
 
   auto disallow_scope1 =
@@ -355,7 +353,7 @@ TEST_F(CursorManagerTest, CustomCursorDisallowedScope_CustomCursorViewFocused) {
   std::unique_ptr<MockRenderWidgetHostViewForCursors> child_view(
       new MockRenderWidgetHostViewForCursors(widget_host.get(), false));
 
-  top_view_->GetCursorManager()->UpdateCursor(top_view_, kCursorHand);
+  top_view_->GetCursorManager()->UpdateCursor(top_view_.get(), kCursorHand);
   top_view_->GetCursorManager()->UpdateCursor(child_view.get(), kCursorCustom);
   EXPECT_EQ(top_view_->cursor(), kCursorHand);
 
@@ -378,7 +376,7 @@ TEST_F(CursorManagerTest,
   std::unique_ptr<MockRenderWidgetHostViewForCursors> child_view(
       new MockRenderWidgetHostViewForCursors(widget_host.get(), false));
 
-  top_view_->GetCursorManager()->UpdateCursor(top_view_, kCursorHand);
+  top_view_->GetCursorManager()->UpdateCursor(top_view_.get(), kCursorHand);
   top_view_->GetCursorManager()->UpdateCursor(child_view.get(), kCursorCustom);
   top_view_->GetCursorManager()->UpdateViewUnderCursor(child_view.get());
   EXPECT_EQ(top_view_->cursor(), kCursorCustom);
@@ -389,7 +387,7 @@ TEST_F(CursorManagerTest,
             /*max_dimension_dips=*/0);
     EXPECT_EQ(top_view_->cursor(), kCursorPointer);
 
-    top_view_->GetCursorManager()->UpdateViewUnderCursor(top_view_);
+    top_view_->GetCursorManager()->UpdateViewUnderCursor(top_view_.get());
     EXPECT_EQ(top_view_->cursor(), kCursorHand);
   }
 
