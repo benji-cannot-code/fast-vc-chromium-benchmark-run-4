3d6369e8e545b21d463f0a24eab2fb6c0f5b1904
April Zhou <aprilzhou@google.com>
2023-11-14 19:48:06 +0000

    fws: Disable floating workspace when there is crash loop. Make fws policy check null safe.
    1. When chrome crashes within 30 secs for more than 3 times, OS daemon
       will set safe-mode flag. Under such case, disable fws capturing and
       restoring.
    2. When fws is disabled due to safe mode, send corresponding
       notification.
    3. Update the fws policy check to be null safe - when no active user,
       fws check should return false instead causing crash.
    
    Test: Manual tested.
    Bug: b:309530704
    Change-Id: Ide3ffda974322b71d1e6f646e9af6dd469049ee6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018465
    Reviewed-by: Yanzhu Du <yzd@google.com>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: April Zhou <aprilzhou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1224483}

--- chrome/browser/ash/floating_workspace/floating_workspace_util.cc
+++ chrome/browser/ash/floating_workspace/floating_workspace_util.cc
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_pref_names.h"
+#include "ash/constants/ash_switches.h"
 #include "base/check.h"
+#include "base/command_line.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
@@ -25,8 +27,9 @@ namespace {
 
 PrefService* GetActiveUserPrefService() {
   auto* active_user = user_manager::UserManager::Get()->GetActiveUser();
-  auto* user_profile = ProfileHelper::Get()->GetProfileByUser(active_user);
-  return user_profile->GetPrefs();
+  return active_user
+             ? ProfileHelper::Get()->GetProfileByUser(active_user)->GetPrefs()
+             : nullptr;
 }
 
 }  // namespace
@@ -45,8 +48,9 @@ bool IsFloatingWorkspaceV1Enabled() {
 
 bool IsFloatingWorkspaceV2Enabled() {
   PrefService* pref_service = GetActiveUserPrefService();
-  DCHECK(pref_service);
-
+  if (!pref_service) {
+    return false;
+  }
   const PrefService::Preference* floating_workspace_pref =
       pref_service->FindPreference(prefs::kFloatingWorkspaceEnabled);
 
@@ -56,6 +60,7 @@ bool IsFloatingWorkspaceV2Enabled() {
     // If there is a policy managing the pref, return what is set by policy.
     return pref_service->GetBoolean(prefs::kFloatingWorkspaceEnabled);
   }
+
   // TODO(b/297795546): Remove external ash feature flag.
   return features::IsFloatingWorkspaceV2Enabled();
 }
@@ -64,6 +69,15 @@ bool IsInternetConnected() {
   NetworkStateHandler* nsh = NetworkHandler::Get()->network_state_handler();
   return nsh != nullptr &&
          nsh->ConnectedNetworkByType(NetworkTypePattern::Default()) != nullptr;
+}
+
+bool IsSafeMode() {
+  return base::CommandLine::ForCurrentProcess()->HasSwitch(
+      ash::switches::kSafeMode);
+}
+
+bool ShouldHandleRestartRestore() {
+  return IsFloatingWorkspaceV2Enabled() && !IsSafeMode();
 }
 
 }  // namespace floating_workspace_util
