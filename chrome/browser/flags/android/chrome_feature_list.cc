5561d5ecdf026e7c0215e25cbb1683b359d08da7
Brandon Fong <bjfong@google.com>
2023-11-14 22:14:32 +0000

    [X-Device Restore] Launch Thumbnail Placeholder to 100% stable
    Enable the flag by default for the thumbnail placeholders for 100% stable release.
    
    Bug: 1426921
    Change-Id: Ibc128a972b526b64287b4b65ac67d9f0fb5c773c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029227
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1224571}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -1090,7 +1090,7 @@ BASE_FEATURE(kTestDefaultEnabled,
 
 BASE_FEATURE(kThumbnailPlaceholder,
              "ThumbnailPlaceholder",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kToolbarMicIphAndroid,
              "ToolbarMicIphAndroid",
