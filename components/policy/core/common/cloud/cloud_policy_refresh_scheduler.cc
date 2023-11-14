b3ca0f9a247eed924457b66f50137d80d3612755
Evan Liu <evliu@google.com>
2023-11-14 00:21:36 +0000

    Remove expired Enterprise.PolicyInvalidationsStartupTime histogram
    This CL removes the expired Enterprise.PolicyInvalidationsStartupTime
    histogram.
    
    Bug: 1499378
    Change-Id: Ibb2d0d7aab364c6eb582fcaa8e67e8f9c0fead4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5026368
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1223980}

--- components/policy/core/common/cloud/cloud_policy_refresh_scheduler.cc
+++ components/policy/core/common/cloud/cloud_policy_refresh_scheduler.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/logging.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/rand_util.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/time/clock.h"
@@ -100,8 +99,7 @@ CloudPolicyRefreshScheduler::CloudPolicyRefreshScheduler(
       refresh_delay_ms_(kDefaultRefreshDelayMs),
       refresh_delay_salt_ms_(static_cast<int64_t>(
           base::RandGenerator(kRandomSaltDelayMaxValueMs))),
-      invalidations_available_(false),
-      creation_time_(GetClock()->Now()) {
+      invalidations_available_(false) {
   client_->AddObserver(this);
   store_->AddObserver(this);
   network_connection_tracker_->AddNetworkConnectionObserver(this);
@@ -150,13 +148,6 @@ void CloudPolicyRefreshScheduler::RefreshSoon(PolicyFetchReason reason) {
 
 void CloudPolicyRefreshScheduler::SetInvalidationServiceAvailability(
     bool is_available) {
-  if (!creation_time_.is_null()) {
-    base::TimeDelta elapsed = GetClock()->Now() - creation_time_;
-    UMA_HISTOGRAM_MEDIUM_TIMES("Enterprise.PolicyInvalidationsStartupTime",
-                               elapsed);
-    creation_time_ = base::Time();
-  }
-
   if (is_available == invalidations_available_) {
     // No change in state.
     return;
