7b988fa7105c3236c08d3564b3dde2a19b30eea8
Toni Barzic <tbarzic@chromium.org>
2023-11-14 17:42:20 +0000

    Shorten app list promise app replacement animation
    Make the app list item view animation duration for replacing promise app
    in line with the spec. Also, better handle the case where delay to load
    the installed app icon is longer than the replacement duration - the app
    list item view will now keep using the "fallback" icon from the promise
    app even after the replacement animation until the actual icon gets
    loaded.
    
    BUG= b:304383596
    
    Change-Id: Ib1efdc5a91c61895d300a0deeac824f7ed0205d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5027944
    Reviewed-by: Ana Salazar <anasalazar@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224383}

--- ash/app_list/views/app_list_item_view.cc
+++ ash/app_list/views/app_list_item_view.cc
@@ -125,7 +125,7 @@ constexpr float kPromiseIconScaleInstalling = 28.0f / 36.0f;
 
 // The duration of the animation to animate an app list item view in as a
 // promise app replacement.
-constexpr base::TimeDelta kSwapPromiseIconDuration = base::Milliseconds(1000);
+constexpr base::TimeDelta kSwapPromiseIconDuration = base::Milliseconds(100);
 
 // The amount of space between the progress ring and the promise app background.
 constexpr gfx::Insets kProgressRingMargin = gfx::Insets(-2);
@@ -744,9 +744,13 @@ void AppListItemView::UpdateIconView(bool update_item_icon) {
     }
   }
 
-  const ui::ImageModel& image_model = ShouldUseFallbackIconImageModel()
-                                          ? fallback_icon_image_model_
-                                          : icon_image_model_;
+  const bool use_fallback_icon = ShouldUseFallbackIconImageModel();
+  const ui::ImageModel& image_model =
+      use_fallback_icon ? fallback_icon_image_model_ : icon_image_model_;
+  if (!use_fallback_icon && !fallback_icon_image_model_.IsEmpty()) {
+    fallback_icon_image_model_ = ui::ImageModel();
+  }
+
   gfx::ImageSkia image_icon;
   if (image_model.IsImage()) {
     image_icon = image_model.GetImage().AsImageSkia();
@@ -761,6 +765,10 @@ void AppListItemView::UpdateIconView(bool update_item_icon) {
 bool AppListItemView::ShouldUseFallbackIconImageModel() const {
   if (fallback_icon_image_model_.IsEmpty()) {
     return false;
+  }
+
+  if (prefer_fallback_icon_) {
+    return true;
   }
 
   if (!item_weak_) {
@@ -1884,6 +1892,7 @@ void AppListItemView::AnimateInFromPromiseApp(
   forced_progress_indicator_value_ = 0.999999f;
   UpdateProgressIndicatorState();
 
+  prefer_fallback_icon_ = true;
   fallback_icon_image_model_ = fallback_image;
   UpdateIconView(/*update_item_icon=*/false);
 
@@ -1934,7 +1943,12 @@ void AppListItemView::OnAnimatedInFromPromiseApp(
   // Clear background set as a result of adding progress indicator.
   SetBackground(nullptr);
 
-  fallback_icon_image_model_ = ui::ImageModel();
+  prefer_fallback_icon_ = false;
+
+  if (!ShouldUseFallbackIconImageModel()) {
+    fallback_icon_image_model_ = ui::ImageModel();
+  }
+
   GetIconView()->DestroyLayer();
   UpdateIconView(/*update_item_icon=*/true);
 
