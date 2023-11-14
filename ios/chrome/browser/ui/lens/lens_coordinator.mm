e4f1dba9a6a10f63665eadf7bfd65b93f756cd03
Jason Hu <hujasonx@google.com>
2023-11-14 18:07:55 +0000

    [Lens]Open Lens in same tab if the current tab is a NTP
    Bug: 1501320
    Change-Id: I7b068b2b7ae7f1cbdc8bcf9faf61abd6439ea97c
    Low-Coverage-Reason: HARD_TO_TEST:Lens sdk is disabled for tests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029493
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1224395}

--- ios/chrome/browser/ui/lens/lens_coordinator.mm
+++ ios/chrome/browser/ui/lens/lens_coordinator.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
+#import "ios/chrome/browser/shared/model/url/url_util.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list_observer_bridge.h"
 #import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
@@ -408,10 +409,20 @@ const base::TimeDelta kCloseLensViewTimeout = base::Seconds(10);
 - (void)openWebLoadParams:(const web::NavigationManager::WebLoadParams&)params {
   if (!self.browser)
     return;
-  UrlLoadParams loadParams = UrlLoadParams::InNewTab(params);
-  loadParams.SetInBackground(NO);
+  web::WebState* webState =
+      self.browser->GetWebStateList()->GetActiveWebState();
+  UrlLoadParams loadParams;
+
+  // Open in the current tab if the current tab is a NTP.
+  if (webState && IsUrlNtp(webState->GetLastCommittedURL())) {
+    loadParams = UrlLoadParams::InCurrentTab(params);
+    self.loadingWebState = webState;
+  } else {
+    loadParams = UrlLoadParams::InNewTab(params);
+    loadParams.append_to = OpenPosition::kCurrentTab;
+    loadParams.SetInBackground(NO);
+  }
   loadParams.in_incognito = self.browser->GetBrowserState()->IsOffTheRecord();
-  loadParams.append_to = OpenPosition::kCurrentTab;
   UrlLoadingBrowserAgent::FromBrowser(self.browser)->Load(loadParams);
 }
 
