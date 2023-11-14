c5ce6b7ac148bb99112da485dc0ec49b2ec05662
Xianzhu Wang <wangxianzhu@chromium.org>
2023-11-14 20:39:21 +0000

    Add ExpandCompositedCullRect into webview ProductionSupportedFlagList
    Bug: 1123042
    Change-Id: I5ca25ab98b859b116746ccb34c99e01e18347f99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5025890
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224515}

--- third_party/blink/renderer/platform/graphics/paint/cull_rect.cc
+++ third_party/blink/renderer/platform/graphics/paint/cull_rect.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/adapters.h"
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/renderer/platform/geometry/layout_rect.h"
 #include "third_party/blink/renderer/platform/graphics/paint/geometry_mapper.h"
 #include "third_party/blink/renderer/platform/graphics/paint/scroll_paint_property_node.h"
@@ -24,21 +25,12 @@ namespace {
 constexpr int kReasonablePixelLimit = kIntMaxForLayoutUnit;
 constexpr int kChangedEnoughMinimumDistance = 512;
 
-BASE_FEATURE(kExpandCompositedCullRect,
-             "ExpandCompositedCullRect",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
-// Number of pixels to expand in root coordinates for cull rect under
-// composited scroll translation or other composited transform.
-constexpr base::FeatureParam<int>
-    kPixelDistanceToExpand(&kExpandCompositedCullRect, "pixels", 4000);
-
 // Returns the number of pixels to expand the cull rect for composited scroll
 // and transform.
 int LocalPixelDistanceToExpand(
     const TransformPaintPropertyNode& root_transform,
     const TransformPaintPropertyNode& local_transform) {
-  static int pixel_distance_to_expand = kPixelDistanceToExpand.Get();
+  static int pixel_distance_to_expand = features::kPixelDistanceToExpand.Get();
   float scale = GeometryMapper::SourceToDestinationApproximateMinimumScale(
       root_transform, local_transform);
   // A very big scale may be caused by non-invertable near non-invertable
