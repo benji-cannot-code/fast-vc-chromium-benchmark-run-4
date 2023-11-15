02c64324f23854992e16b7065d6409039470bf23
John Palmer <jopalmer@chromium.org>
2023-11-15 05:55:57 +0000

    Emoji-picker: Enable build_webui() performance optimizations [4/4]
    Actually enable optimize_webui
    
    (Some minor changes that got missed earlier in the tests also fixed)
    
    Gets us around 15% faster to onload
    
    Resources.pak size change:
    before:35431527
    after:35449079
    17kb size increase, which seems pretty small
    
    emoji picker rollup file:266414 bytes (266kb)
    
    BUG: b:307665429
    Change-Id: Id2bed365af4124d38f7def3efbd41d48820fcec1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4988623
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224765}

--- chrome/test/data/webui/chromeos/emoji_picker/emoji_picker_validation_gif_test.ts
+++ chrome/test/data/webui/chromeos/emoji_picker/emoji_picker_validation_gif_test.ts
@@ -2,8 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-import 'chrome://emoji-picker/app.js';
-
 import {EmojiGroupElement, EmojiPickerApiProxyImpl, EmojiPickerApp, GIF_VALIDATION_DATE, TRENDING} from 'chrome://emoji-picker/emoji_picker.js';
 import {assert} from 'chrome://resources/js/assert.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
