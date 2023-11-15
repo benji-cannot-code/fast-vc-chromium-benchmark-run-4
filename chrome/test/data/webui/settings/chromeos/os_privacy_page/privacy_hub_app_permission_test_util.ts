ae944f16549e4b6c6c4022b74a4a95287cfac2c0
Md Shahadat Hossain Shahin <shahinmd@google.com>
2023-11-15 10:38:00 +0000

    privacy-hub: Add app list to the camera subpage
    UI:
    https://screenshot.googleplex.com/83G57Uv72eLzhyF
    https://screenshot.googleplex.com/AvqAyaVSYNh6dBK
    
    Bug: b:310581495
    Change-Id: I4a64b4a80586b422b0048f1ef8b238a23d59c5ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5023888
    Commit-Queue: Md Shahadat Hossain Shahin <shahinmd@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1224841}

---
+++ chrome/test/data/webui/settings/chromeos/os_privacy_page/privacy_hub_app_permission_test_util.ts
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {appPermissionHandlerMojom} from 'chrome://os-settings/os_settings.js';
+import {AppType, PermissionType, TriState} from 'chrome://resources/cr_components/app_management/app_management.mojom-webui.js';
+import {createTriStatePermission} from 'chrome://resources/cr_components/app_management/permission_util.js';
+
+type App = appPermissionHandlerMojom.App;
+
+export function createApp(
+    id: string, name: string, permissionType: PermissionType,
+    permissionValue: TriState): App {
+  const app: App = {id, name, type: AppType.kWeb, permissions: {}};
+  app.permissions[permissionType] = createTriStatePermission(
+      permissionType, permissionValue, /*is_managed=*/ false);
+  return app;
+}
