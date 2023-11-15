a27c0be46896dd6bbc0be244d2be232b0bb3806c
Colin Blundell <blundell@chromium.org>
2023-11-15 08:26:36 +0000

    [//gpu] Pass ClientSI to GPU thread in SIIInProcess::DestroySI(ClientSI)
    In preparation for having ClientSharedImage own its associated GMB, this
    CL passes the ClientSI instance given to SIIInProcess::DestroySI() to
    the GPU thread. This ensures that it will stay alive until the Mailbox
    is destroyed, which is when the GMB should be destroyed (and currently
    is destroyed).
    
    Bug: 1500611
    Change-Id: I45888538ae024f3666d6ab0874349e3e19aca311
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5028360
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224797}

--- gpu/command_buffer/service/shared_image_interface_in_process.cc
+++ gpu/command_buffer/service/shared_image_interface_in_process.cc
@@ -707,7 +707,13 @@ void SharedImageInterfaceInProcess::DestroySharedImage(
     const SyncToken& sync_token,
     scoped_refptr<ClientSharedImage> client_shared_image) {
   CHECK(client_shared_image->HasOneRef());
-  DestroySharedImage(sync_token, client_shared_image->mailbox());
+  // Use sync token dependency to ensure that the destroy task does not run
+  // before sync token is released.
+  ScheduleGpuTask(
+      base::BindOnce(
+          &SharedImageInterfaceInProcess::DestroyClientSharedImageOnGpuThread,
+          base::Unretained(this), std::move(client_shared_image)),
+      {sync_token});
 }
 
 void SharedImageInterfaceInProcess::DestroySharedImageOnGpuThread(
@@ -725,6 +731,11 @@ void SharedImageInterfaceInProcess::DestroySharedImageOnGpuThread(
     base::AutoLock lock(lock_);
     gpu_memory_buffers_.erase(mailbox);
   }
+}
+
+void SharedImageInterfaceInProcess::DestroyClientSharedImageOnGpuThread(
+    scoped_refptr<ClientSharedImage> client_shared_image) {
+  DestroySharedImageOnGpuThread(client_shared_image->mailbox());
 }
 
 void SharedImageInterfaceInProcess::WaitSyncTokenOnGpuThread(
