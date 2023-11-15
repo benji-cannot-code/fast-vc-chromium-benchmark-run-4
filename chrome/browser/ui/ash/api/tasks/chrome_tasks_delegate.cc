1ec20ddf987c6506fa8dad0a454135af97f82671
Ben Becker <benbecker@chromium.org>
2023-11-15 19:32:46 +0000

    Tasks-Api: Add AddTask and UpdateTaskTitle to delegate
    This CL adds functions that can create new tasks and update the title of
    an existing task.
    
    Bug: b/305085993
    Change-Id: If02ba7be5758652273db67c1a5e1f96a5ac86e39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5030505
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Ben Becker <benbecker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225082}

--- chrome/browser/ui/ash/api/tasks/chrome_tasks_delegate.cc
+++ chrome/browser/ui/ash/api/tasks/chrome_tasks_delegate.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/api/tasks/tasks_client.h"
 #include "base/functional/bind.h"
+#include "base/functional/callback_helpers.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
@@ -155,6 +156,23 @@ void ChromeTasksDelegate::SendCompletedTasks() {
   TasksClientImpl* client = GetActiveAccountClient();
   CHECK(client);
   client->OnGlanceablesBubbleClosed();
+}
+
+void ChromeTasksDelegate::AddTask(const std::string& task_list_id,
+                                  const std::string& title) {
+  CHECK(active_account_id_.is_valid());
+  TasksClientImpl* client = GetActiveAccountClient();
+  CHECK(client);
+  client->AddTask(task_list_id, title);
+}
+
+void ChromeTasksDelegate::UpdateTaskTitle(const std::string& task_list_id,
+                                          const std::string& task_id,
+                                          const std::string& title) {
+  CHECK(active_account_id_.is_valid());
+  TasksClientImpl* client = GetActiveAccountClient();
+  CHECK(client);
+  client->UpdateTask(task_list_id, task_id, title, base::DoNothing());
 }
 
 TasksClientImpl* ChromeTasksDelegate::GetActiveAccountClient() const {
