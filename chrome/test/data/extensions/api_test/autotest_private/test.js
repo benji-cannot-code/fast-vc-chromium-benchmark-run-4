47f84a4bd82037e5fea3a01a3da0238303c3a1dc
Kendrake Tsui <kendraketsui@google.com>
2023-11-15 00:26:47 +0000

    Add group_name to isFieldTrialActive autotest API
    The current isFieldTrialActive implementation only checks if a field
    trial exists by checking the trial name.
    
    Add a check to make sure that the group name also matches. This is a
    more robust check since each field trial is defined by a trial name and
    group name.
    
    BUG=b:294278740
    
    Change-Id: Id970187b6a3b1d1a92a617e5253dbf2883022af1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5021060
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Miriam Zimmerman <mutexlox@chromium.org>
    Commit-Queue: Kendrake Tsui <kendraketsui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1224647}

--- chrome/test/data/extensions/api_test/autotest_private/test.js
+++ chrome/test/data/extensions/api_test/autotest_private/test.js
@@ -1551,17 +1551,26 @@ var holdingSpaceTests = [
 ];
 
 var isFieldTrialActiveTests = [
-  function getActiveTrial() {
-    chrome.autotestPrivate.isFieldTrialActive("ActiveTrialForTest",
-      chrome.test.callbackPass(enabled => {
-        chrome.test.assertTrue(enabled);
-      }));
+  function getActiveTrialActiveGroup() {
+    chrome.autotestPrivate.isFieldTrialActive(
+        'ActiveTrialForTest', 'GroupForTest',
+        chrome.test.callbackPass(enabled => {
+          chrome.test.assertTrue(enabled);
+        }));
+  },
+  function getActiveTrialInactiveGroup() {
+    chrome.autotestPrivate.isFieldTrialActive(
+        'ActiveTrialForTest', 'WrongGroupForTest',
+        chrome.test.callbackPass(enabled => {
+          chrome.test.assertFalse(enabled);
+        }));
   },
   function getInactiveTrial() {
-    chrome.autotestPrivate.isFieldTrialActive("InactiveTrialForTest",
-      chrome.test.callbackPass(enabled => {
-        chrome.test.assertFalse(enabled);
-      }));
+    chrome.autotestPrivate.isFieldTrialActive(
+        'InactiveTrialForTest', 'GroupForTest',
+        chrome.test.callbackPass(enabled => {
+          chrome.test.assertFalse(enabled);
+        }));
   }
 ];
 
