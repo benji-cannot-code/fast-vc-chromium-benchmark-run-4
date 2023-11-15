ac097da30346438c03250ce7cc7ad982daa5e077
Yuichiro Hanada <yhanada@google.com>
2023-11-15 04:37:07 +0000

    Remove RightClickConversion metrics
    OBSOLETE_HISTOGRAMS[Arc.CompatMode.RightClickConversion]=The feature was shipped.
    
    Bug: 1501882
    Change-Id: Ie6f9e89f19edc494f3342c4051938cf5094b9610
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029335
    Reviewed-by: Hiroki Sato <hirokisato@chromium.org>
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Youkichi Hosoi <youkichihosoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224746}

--- ash/components/arc/compat_mode/touch_mode_mouse_rewriter.cc
+++ ash/components/arc/compat_mode/touch_mode_mouse_rewriter.cc
@@ -110,10 +110,6 @@ ui::EventDispatchDetails TouchModeMouseRewriter::RewriteEvent(
   const ui::MouseEvent& mouse_event = *event.AsMouseEvent();
   if (mouse_event.IsRightMouseButton() || mouse_event.IsLeftMouseButton()) {
     if (!in_resize_locked) {
-      if (mouse_event.IsRightMouseButton()) {
-        RecordRightClickConversionResultHistogram(
-            RightClickConversionResultHistogramResult::kNotConverted);
-      }
       return SendEvent(continuation, &event);
     }
 
@@ -206,9 +202,6 @@ ui::EventDispatchDetails TouchModeMouseRewriter::RewriteMouseClickEvent(
         base::BindOnce(&TouchModeMouseRewriter::SendReleaseEvent,
                        weak_ptr_factory_.GetWeakPtr(), event, continuation),
         kLongPressInterval);
-
-    RecordRightClickConversionResultHistogram(
-        RightClickConversionResultHistogramResult::kConverted);
 
     // Send the press event now.
     ui::MouseEvent press_event(
--- ash/components/arc/compat_mode/touch_mode_mouse_rewriter_unittest.cc
+++ ash/components/arc/compat_mode/touch_mode_mouse_rewriter_unittest.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/components/arc/compat_mode/metrics.h"
 #include "base/run_loop.h"
-#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "ui/events/test/event_generator.h"
 #include "ui/views/test/views_test_base.h"
@@ -95,8 +94,6 @@ class TouchModeMouseRewriterTest : public views::ViewsTestBase {
       : views::ViewsTestBase(
             base::test::TaskEnvironment::TimeSource::MOCK_TIME) {}
   ~TouchModeMouseRewriterTest() override = default;
-
-  base::HistogramTester histogram_tester;
 };
 
 TEST_F(TouchModeMouseRewriterTest, RightClickConvertedToLongPress) {
@@ -117,10 +114,6 @@ TEST_F(TouchModeMouseRewriterTest, RightClickConvertedToLongPress) {
   generator.PressRightButton();
   EXPECT_TRUE(view->left_pressed());
   EXPECT_FALSE(view->right_pressed());
-
-  histogram_tester.ExpectUniqueSample(
-      "Arc.CompatMode.RightClickConversion",
-      RightClickConversionResultHistogramResult::kConverted, 1);
 
   // Immediately release the right button. It will not generate any event.
   generator.ReleaseRightButton();
@@ -158,10 +151,6 @@ TEST_F(TouchModeMouseRewriterTest, DisabledForWindow) {
   // Press the right button.
   generator.PressRightButton();
   EXPECT_TRUE(view2->right_pressed());
-
-  histogram_tester.ExpectUniqueSample(
-      "Arc.CompatMode.RightClickConversion",
-      RightClickConversionResultHistogramResult::kNotConverted, 1);
 
   // Immediately release the right button.
   generator.ReleaseRightButton();
