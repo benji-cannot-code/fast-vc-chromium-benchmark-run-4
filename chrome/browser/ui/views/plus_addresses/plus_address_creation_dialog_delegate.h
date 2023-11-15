3ab3f6fd6f0cef4ca7b6a07f60983f4f0dc434a3
Kirubel Aklilu <kaklilu@chromium.org>
2023-11-15 15:54:55 +0000

    [PlusAddress - Desktop] Make the description link open the settings page
    The destination of the link is provided by the kPlusAddressManagementUrl
    parameter and is consistent with the link in chrome://settings.
    
    Screencast:
    https://screencast.googleplex.com/cast/NjU2NjUzMzE4OTg2MTM3NnwyMzM3N2IzYy1iOA
    
    Bug: 1467623 b/310185326
    Change-Id: Idbbc6fb50838d0738312689e60b2cbee59f49e88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029590
    Reviewed-by: Matt Reichhoff <mreichhoff@chromium.org>
    Commit-Queue: Kirubel Aklilu <kaklilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224962}

--- chrome/browser/ui/views/plus_addresses/plus_address_creation_dialog_delegate.h
+++ chrome/browser/ui/views/plus_addresses/plus_address_creation_dialog_delegate.h
@@ -9,10 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "components/plus_addresses/plus_address_types.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
-#include "ui/views/controls/button/md_text_button.h"
+
+namespace content {
+class WebContents;
+}  // namespace content
 
 namespace views {
 class Label;
+class MdTextButton;
 }  // namespace views
 
 namespace plus_addresses {
@@ -26,6 +30,7 @@ class PlusAddressCreationDialogDelegate : public views::BubbleDialogDelegate {
 
   PlusAddressCreationDialogDelegate(
       base::WeakPtr<PlusAddressCreationController> controller,
+      content::WebContents* web_contents,
       const std::string& primary_email_address);
   PlusAddressCreationDialogDelegate(const PlusAddressCreationDialogDelegate&) =
       delete;
@@ -36,6 +41,9 @@ class PlusAddressCreationDialogDelegate : public views::BubbleDialogDelegate {
   // WidgetDelegate:
   bool ShouldShowCloseButton() const override;
   void OnWidgetInitialized() override;
+
+  // Navigates to the link shown in the dialog's description.
+  void OpenSettingsLink(content::WebContents* web_contents);
 
   // TODO(crbug.com/1467623): Pull these into a shared delegate interface for
   // both Desktop & Android views.
