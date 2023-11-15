6b6c6f843e1d287bcaaa965496d7b8fc68b7f247
Samar <samarchehade@google.com>
2023-11-15 15:51:32 +0000

    [iOS][Waffle] Copy search engine choice code over from ios_internal/
    This is step 1/2 of a two-part change:
    => https://crrev.com/c/5030236 Copy search engine choice code over from ios_internal/
    * https://crrev.com/i/6669634 Delete duplicate repo from ios_internal
    
    Low-Coverage-Reason: LARGE_SCALE_REFACTOR
    Bug: 306123679
    Change-Id: I474937f73e98c17ecad754aa7462f9b2bc7b64bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5030236
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224956}

---
+++ ios/chrome/browser/ui/search_engine_choice/why_am_i_seeing_this/why_am_i_seeing_this_view_controller.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_SEARCH_ENGINE_CHOICE_WHY_AM_I_SEEING_THIS_WHY_AM_I_SEEING_THIS_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_UI_SEARCH_ENGINE_CHOICE_WHY_AM_I_SEEING_THIS_WHY_AM_I_SEEING_THIS_VIEW_CONTROLLER_H_
+
+#import <UIKit/UIKit.h>
+
+#import "ios/chrome/browser/shared/ui/table_view/legacy_chrome_table_view_controller.h"
+
+@class WhyAmISeeingThisViewController;
+
+// Delegate protocol for WhyAmISeeingThisViewController.
+@protocol WhyAmISeeingThisDelegate <NSObject>
+
+// Called when the user taps the dismiss button.
+- (void)learnMoreDone:(WhyAmISeeingThisViewController*)viewController;
+
+@end
+
+// A base view controller for showing an informational screen.
+@interface WhyAmISeeingThisViewController : LegacyChromeTableViewController
+
+// View controller delegate.
+@property(nonatomic, weak) id<WhyAmISeeingThisDelegate> delegate;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_SEARCH_ENGINE_CHOICE_WHY_AM_I_SEEING_THIS_WHY_AM_I_SEEING_THIS_VIEW_CONTROLLER_H_
