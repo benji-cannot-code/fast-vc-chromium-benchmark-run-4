4d324554bc586d17ad087c472ed627691b5cf467
Dustin J. Mitchell <djmitche@chromium.org>
2023-11-15 00:18:09 +0000

    Use ProxyChains in NetworkServiceProxyDelegate::MergeProxyRules
    This otherwise causes crashes when using multi-proxy chains.
    
    Change-Id: I41009a7430dd6b62de3c168abfebdba1644b907b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5026633
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224639}

--- services/network/network_service_proxy_delegate.h
+++ services/network/network_service_proxy_delegate.h
@@ -75,6 +75,9 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkServiceProxyDelegate
   }
 
  private:
+  friend class NetworkServiceProxyDelegateTest;
+  FRIEND_TEST_ALL_PREFIXES(NetworkServiceProxyDelegateTest, MergeProxyRules);
+
   // Checks if this CustomProxyConfig is supporting IP Protection.
   bool IsForIpProtection();
 
