bf19471a921ff487884a3130130ce350742f243f
Aida Zolic <aidazolic@chromium.org>
2023-11-15 07:30:46 +0000

    Revert "dlp: Add is_dlp_managed field to DownloadItem"
    This reverts commit 8c0671e18afb585ed18b3841fcfe49ba27ec9346.
    
    Reason for revert: The project was deprioritized for now, so I'm reverting the landed changes as we probably won't continue this in the near future.
    
    Original change's description:
    > dlp: Add is_dlp_managed field to DownloadItem
    >
    > Adds a field and related accessors to mark a DownloadItem as managed by
    > DLP, i.e. that there is at least one DLP Files Restriction rule that
    > applies to that downloads, so the file might be protected and some of
    > the actions on it blocked/warned and/or reported to the admin.
    >
    > R=qinmin@chromium.org
    >
    > Fixed: b:302506453
    > Change-Id: I7a8288e7cdc36dd30b4c7237af94d7f44c140107
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4909956
    > Reviewed-by: Min Qin <qinmin@chromium.org>
    > Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1205797}
    
    Change-Id: I8ac9b71892d9d50917fc3109a570b7a0450f5fd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5028319
    Auto-Submit: Aida Zolic <aidazolic@chromium.org>
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224780}

--- components/download/public/common/download_item_impl.h
+++ components/download/public/common/download_item_impl.h
@@ -325,7 +325,6 @@ class COMPONENTS_DOWNLOAD_EXPORT DownloadItemImpl
   bool RequireSafetyChecks() const override;
   bool IsParallelDownload() const override;
   DownloadCreationType GetDownloadCreationType() const override;
-  bool IsDlpManaged() const override;
   ::network::mojom::CredentialsMode GetCredentialsMode() const override;
   const absl::optional<net::IsolationInfo>& GetIsolationInfo() const override;
   void OnContentCheckCompleted(DownloadDangerType danger_type,
@@ -335,7 +334,6 @@ class COMPONENTS_DOWNLOAD_EXPORT DownloadItemImpl
   void SetOpened(bool opened) override;
   void SetLastAccessTime(base::Time last_access_time) override;
   void SetDisplayName(const base::FilePath& name) override;
-  void SetIsDlpManaged(bool is_managed) override;
   std::string DebugString(bool verbose) const override;
   void SimulateErrorForTesting(DownloadInterruptReason reason) override;
 
@@ -879,9 +877,6 @@ class COMPONENTS_DOWNLOAD_EXPORT DownloadItemImpl
   // The InsecureDownloadStatus if determined.
   InsecureDownloadStatus insecure_download_status_ =
       InsecureDownloadStatus::UNKNOWN;
-
-  // Whether this download is covered by any Data Leak Prevention (DLP) policy.
-  bool is_dlp_managed_ = false;
 
   THREAD_CHECKER(thread_checker_);
 
