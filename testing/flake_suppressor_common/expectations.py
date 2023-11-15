2c28de1b341247bd7070db3967441fb693ea0a4b
Jie Sheng <jiesheng@google.com>
2023-11-15 21:10:59 +0000

    [flake suppressor improvement] 3/N: Fixed a query error on flake suppressor.
    TODO:
    1. Add a Pass status for all flaky tests when it has passed runs.
    2. Updated the Skip adding, since only consistently timeout
    test needs to add the Skip status.
    
    Bug: 1358735
    Change-Id: I00ac6868b8774779b80395b2d00b4a7ae47f2985
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029803
    Auto-Submit: Jie Sheng <jiesheng@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jie Sheng <jiesheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1225142}

--- testing/flake_suppressor_common/expectations.py
+++ testing/flake_suppressor_common/expectations.py
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import base64
 import collections
-import datetime
 from datetime import timedelta
 import os
 import posixpath
