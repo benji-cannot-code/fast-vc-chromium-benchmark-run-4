511c0b035cce89f4ccf57b136064fa613d241228
David Benjamin <davidben@chromium.org>
2023-11-15 02:26:51 +0000

    Remove some stale ignore-certificate-errors histograms
    These histograms have been expired since M77.
    
    OBSOLETE_HISTOGRAMS=Histogram expired in M77
    
    Fixed: 1499443
    Change-Id: I7621511564363481a0e470554294202b67e64e73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029228
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224707}

--- services/network/network_service.cc
+++ services/network/network_service.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/check_op.h"
-#include "base/command_line.h"
 #include "base/debug/crash_logging.h"
 #include "base/debug/dump_without_crashing.h"
 #include "base/environment.h"
@@ -407,14 +406,6 @@ void NetworkService::Initialize(mojom::NetworkServiceParamsPtr params,
   if (!params->environment.empty()) {
     SetEnvironment(std::move(params->environment));
   }
-
-  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
-
-  // Record this once per session, though the switch is applied on a
-  // per-NetworkContext basis.
-  UMA_HISTOGRAM_BOOLEAN(
-      "Net.Certificate.IgnoreCertificateErrorsSPKIListPresent",
-      command_line->HasSwitch(switches::kIgnoreCertificateErrorsSPKIList));
 
   if (params->system_dns_resolver) {
     SetSystemDnsResolver(std::move(params->system_dns_resolver));
