cc3f18167f51c52b13d485268dcbd4dfb18b3f7a
Sarah Krakowiak Criel <skrakowi@chromium.org>
2023-11-15 19:20:24 +0000

    Implement suspicious site detection for RT lookups
    Bug: 1500403
    Change-Id: I0beb56c443ccef30a3ac22c5c8ec8e544c6260ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5009909
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Sarah Krakowiak <skrakowi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225076}

--- components/safe_browsing/core/browser/url_realtime_mechanism.h
+++ components/safe_browsing/core/browser/url_realtime_mechanism.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/core/browser/db/database_manager.h"
 #include "components/safe_browsing/core/browser/realtime/url_lookup_service_base.h"
 #include "components/safe_browsing/core/browser/safe_browsing_lookup_mechanism.h"
+#include "components/safe_browsing/core/browser/url_checker_delegate.h"
 #include "components/safe_browsing/core/common/proto/realtimeapi.pb.h"
 #include "url/gurl.h"
 
@@ -53,7 +54,10 @@ class UrlRealTimeMechanism : public SafeBrowsingLookupMechanism {
       scoped_refptr<base::SequencedTaskRunner> ui_task_runner,
       base::WeakPtr<RealTimeUrlLookupServiceBase> url_lookup_service_on_ui,
       WebUIDelegate* webui_delegate,
-      MechanismExperimentHashDatabaseCache experiment_cache_selection);
+      MechanismExperimentHashDatabaseCache experiment_cache_selection,
+      scoped_refptr<UrlCheckerDelegate> url_checker_delegate,
+      const base::RepeatingCallback<content::WebContents*()>&
+          web_contents_getter);
   UrlRealTimeMechanism(const UrlRealTimeMechanism&) = delete;
   UrlRealTimeMechanism& operator=(const UrlRealTimeMechanism&) = delete;
   ~UrlRealTimeMechanism() override;
@@ -130,6 +134,9 @@ class UrlRealTimeMechanism : public SafeBrowsingLookupMechanism {
       absl::optional<ThreatSource> threat_source,
       bool real_time_request_failed);
 
+  void MaybePerformSuspiciousSiteDetection(
+      RTLookupResponse::ThreatInfo::VerdictType rt_verdict_type);
+
   SEQUENCE_CHECKER(sequence_checker_);
 
   // This is used only for logging purposes, primarily (but not exclusively) to
@@ -173,6 +180,14 @@ class UrlRealTimeMechanism : public SafeBrowsingLookupMechanism {
   // chrome://safe-browsing and in unit tests. If non-null, guaranteed to
   // outlive this object by contract.
   raw_ptr<WebUIDelegate> webui_delegate_ = nullptr;
+
+  // This object is used to call the |NotifySusiciousSiteDetected| method on
+  // URLs with suspicious verdicts.
+  scoped_refptr<UrlCheckerDelegate> url_checker_delegate_;
+
+  // This stores the callback method that will be used to obtain WebContents,
+  // which is needed to call the |NotifySusiciousSiteDetected| trigger.
+  base::RepeatingCallback<content::WebContents*()> web_contents_getter_;
 
   // If the URL is classified as safe in cache manager during real time
   // lookup.
--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -290,6 +290,10 @@ constexpr base::FeatureParam<int> kStrictDownloadTimeoutMilliseconds{
     &kStrictDownloadTimeout, "TimeoutMilliseconds",
     /*default_value=*/7000};
 
+BASE_FEATURE(kSuspiciousSiteDetectionRTLookups,
+             "SuspiciousSiteDetectionRTLookups",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kSuspiciousSiteTriggerQuotaFeature,
              "SafeBrowsingSuspiciousSiteTriggerQuota",
              base::FEATURE_ENABLED_BY_DEFAULT);
@@ -388,6 +392,7 @@ constexpr struct {
     {&kSevenZipEvaluationEnabled, true},
     {&kSimplifiedUrlDisplay, true},
     {&kStrictDownloadTimeout, true},
+    {&kSuspiciousSiteDetectionRTLookups, false},
     {&kSuspiciousSiteTriggerQuotaFeature, true},
     {&kTailoredSecurityIntegration, true},
     {&kThreatDomDetailsTagAndAttributeFeature, false},
