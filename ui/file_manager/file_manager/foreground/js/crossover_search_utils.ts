857deffedbc9397ad4a2f0f56719ce967bcc4201
Wenbo Jie <wenbojie@chromium.org>
2023-11-15 04:15:40 +0000

    [Files F2] Enable SearchWithLaunchParams test for the new tree
    * Use volume manager roots for scanning in the
      crossover_search_utils.
    * Convert crossover_search_utils to TS.
    
    Bug: b:307656688
    Test: browser_tests --gtest_filter="Search/FilesApp*"
    Change-Id: I9e38730e1f6467756d17d582d3ed2eeeadc7ecc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5019246
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224741}

---
+++ ui/file_manager/file_manager/foreground/js/crossover_search_utils.ts
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {FilesAppEntry} from '../../externs/files_app_entry_interfaces.js';
+import {VolumeManager} from '../../externs/volume_manager.js';
+
+import {DirectoryModel} from './directory_model.js';
+
+/**
+ * Return DirectoryEntry of the first root directory (all volume display root
+ * directories) that contains one or more query-matched files, returns null if
+ * no such directory is found.
+ *
+ * @param volumeManager The volume manager.
+ * @param dirModel The directory model.
+ * @param searchQuery Search query.
+ */
+export async function findQueryMatchedDirectoryEntry(
+    volumeManager: VolumeManager, dirModel: DirectoryModel,
+    searchQuery: string): Promise<DirectoryEntry|FilesAppEntry|null> {
+  for (let i = 0; i < volumeManager.volumeInfoList.length; i++) {
+    const volumeInfo = volumeManager.volumeInfoList.item(i);
+    // Make sure the volume root is resolved before scanning.
+    await volumeInfo.resolveDisplayRoot();
+    const dirEntry = volumeInfo.displayRoot;
+
+    let isEntryFound = false;
+    function entriesCallback() {
+      isEntryFound = true;
+    }
+    function errorCallback(error: Error) {
+      console.warn(error.stack || error);
+    }
+
+    const scanner = dirModel.createScannerFactory(dirEntry, searchQuery)();
+    await new Promise<void>(
+        resolve => scanner.scan(entriesCallback, resolve, errorCallback));
+    if (isEntryFound) {
+      return dirEntry;
+    }
+  }
+  return null;
+}
