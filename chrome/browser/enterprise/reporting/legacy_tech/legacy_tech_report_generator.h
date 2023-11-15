907f60860404c7dcb12e96e6e60de50bbf2f923e
Sandor Major <sandormajor@google.com>
2023-11-15 16:15:00 +0000

    Add expectation for LegacyTechServiceTest/MatchedAndUpload test
    Bug: b:299640963
    Change-Id: I8deddea53a5548eacaa0f8833a08f6a54a0b7d70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5017600
    Auto-Submit: Sandor «Alex» Major <sandormajor@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224974}

--- chrome/browser/enterprise/reporting/legacy_tech/legacy_tech_report_generator.h
+++ chrome/browser/enterprise/reporting/legacy_tech/legacy_tech_report_generator.h
@@ -31,6 +31,9 @@ class LegacyTechReportGenerator {
                    uint64_t column);
     LegacyTechData(const LegacyTechData& other);
     ~LegacyTechData();
+
+    bool operator==(const LegacyTechData&) const = default;
+
     std::string type;
     base::Time timestamp;
     GURL url;
