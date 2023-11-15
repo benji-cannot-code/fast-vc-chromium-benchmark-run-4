62a597fc9c24b14343569c713a2b97e88c377dd2
David Benjamin <davidben@chromium.org>
2023-11-15 17:39:07 +0000

    Use a wrapping addition in InstantRestrictedIDCache
    It is unclear to me whether wrapping is actually correct (rather than a
    checked numeric), but since we have tests to assert wrapping behavior,
    I've gone with that for now. If this is not the correct fix and the test
    is wrong, reviewers, reviewers, please suggest an alternative fix.
    
    Bug: 1394755
    Change-Id: I045b5c25f7d3f8366f01d984b42a3b4c624a1ac7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5034298
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225013}

--- chrome/renderer/instant_restricted_id_cache.h
+++ chrome/renderer/instant_restricted_id_cache.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/containers/lru_cache.h"
 #include "base/gtest_prod_util.h"
+#include "base/numerics/wrapping_math.h"
 #include "chrome/common/search/instant_types.h"
 
 // In InstantExtended, iframes are used to display objects which can only be
@@ -108,7 +109,8 @@ void InstantRestrictedIDCache<T>::AddItems(const ItemVector& items) {
   }
 
   for (size_t i = 0; i < items.size(); ++i) {
-    InstantRestrictedID id = ++last_restricted_id_;
+    InstantRestrictedID id = base::WrappingAdd(last_restricted_id_, 1);
+    last_restricted_id_ = id;
     cache_.Put(id, items[i]);
     if (i == 0)
       last_add_start_ = --cache_.rend();
