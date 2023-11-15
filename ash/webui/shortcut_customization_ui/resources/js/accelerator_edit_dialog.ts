985ed310c4070ebe51cc2206b176663eafd30fed
Longbo Wei <longbowei@google.com>
2023-11-15 20:26:36 +0000

    shortcuts: Add emptyState in the dialog when there are no shortcuts
    * When there's no enabled shortcuts in the dialog and the viewState is
      VIEW, show "No shortcut assigned" message.
    
    - specs:      http://screen/maJj7uAUoTsbLxP
    - screenshot: http://screen/Y2oLtLxVrp294bu
    
    Bug: b/216049298
    Test: browser_test
    Change-Id: I18a27dba57e2a126f5e46116970c1b238c71bc33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5031392
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Longbo Wei <longbowei@google.com>
    Cr-Commit-Position: refs/heads/main@{#1225117}

--- ash/webui/shortcut_customization_ui/resources/js/accelerator_edit_dialog.ts
+++ ash/webui/shortcut_customization_ui/resources/js/accelerator_edit_dialog.ts
@@ -258,6 +258,11 @@ export class AcceleratorEditDialogElement extends
         this.defaultAcceleratorsWithConflict.size === 0;
   }
 
+  protected isEmptyState(): boolean {
+    return this.pendingNewAcceleratorState === ViewState.VIEW &&
+        this.getSortedFilteredAccelerators(this.acceleratorInfos).length === 0;
+  }
+
   protected acceleratorLimitNotReached(): boolean {
     let originalAcceleratorsCount = 0;
     for (const acceleratorInfo of this.acceleratorInfos) {
