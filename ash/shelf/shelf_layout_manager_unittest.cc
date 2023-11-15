9e6606a306194624b5c5907e1af2e0732f4ad66a
Alex Newcomer <newcomer@google.com>
2023-11-15 00:06:28 +0000

    Status Area:Reduce control button edge spacing to match spec
    UX says it should match the vertical insets
    
    Before:
    https://screenshot.googleplex.com/iz5ztCLy6ndyCPq.png
    
    After:
    https://screenshot.googleplex.com/B3uH59sbLTUwqR6.png
    
    Change-Id: Ibfa494beb1e687563b79b687c02443d70937f1b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5014488
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Auto-Submit: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224633}

--- ash/shelf/shelf_layout_manager_unittest.cc
+++ ash/shelf/shelf_layout_manager_unittest.cc
@@ -4994,8 +4994,6 @@ TEST_P(NavigationWidgetRTLTest, VerifyHomeButtonBounds) {
   const gfx::Rect display_bounds = GetPrimaryDisplay().bounds();
   Shelf* shelf = GetPrimaryShelf();
   ShelfNavigationWidget* navigation_widget = shelf->navigation_widget();
-  const gfx::Size widget_size_in_home_launcher =
-      navigation_widget->GetWindowBoundsInScreen().size();
 
   auto fetch_home_button_screen_bounds =
       [](const ShelfNavigationWidget* navigation_widget,
@@ -5051,10 +5049,6 @@ TEST_P(NavigationWidgetRTLTest, VerifyHomeButtonBounds) {
   waiter.WaitForAnimation();
 
   ASSERT_EQ(HotseatState::kHidden, GetShelfLayoutManager()->hotseat_state());
-
-  const gfx::Size widget_size_in_hidden_state =
-      navigation_widget->GetWindowBoundsInScreen().size();
-  EXPECT_EQ(widget_size_in_home_launcher, widget_size_in_hidden_state);
 
   // Verify home button bounds in the hidden state.
   {
