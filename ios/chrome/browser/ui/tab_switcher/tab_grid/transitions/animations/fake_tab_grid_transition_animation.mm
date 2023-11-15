950ccb31948bdc6e3466cb87679b292623677650
Ewann Pelle <ewannpv@chromium.org>
2023-11-15 13:24:00 +0000

    [iOS][2/N] TabGrid transition layout implementation.
    This CL adds the TabGridTransitionAnimationGroup interface, used to
    combine a set of transition animations.
    
    Low-Coverage-Reason: Animations are not done.
    Bug: 1499268
    Change-Id: I9f0fb11318274271c97d3481bf5d3bd85f7e1cd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5012652
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224880}

---
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/transitions/animations/fake_tab_grid_transition_animation.mm
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/tab_switcher/tab_grid/transitions/animations/fake_tab_grid_transition_animation.h"
+
+@implementation FakeTransitionAnimation
+
+#pragma mark - TabGridTransitionAnimation
+
+- (void)animateWithCompletion:(ProceduralBlock)completion {
+  self.animationCount++;
+  completion();
+}
+
+@end
