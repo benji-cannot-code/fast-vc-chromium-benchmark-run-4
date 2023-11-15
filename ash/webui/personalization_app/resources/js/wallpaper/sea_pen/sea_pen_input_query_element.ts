3b062b3aa3de1befc707f97d4ac909dc6d3a8710
Jason Thai <jasontt@chromium.org>
2023-11-15 18:44:36 +0000

    p13n: styling for query search
    Not everything is finalized but this update makes it look closer to
    spec.
    
    BUG=b:309683842
    TEST=http://shortn/_YAEIdVax1S
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I253de33789a58c77a38d08a63fdb3c6cddaaca3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5030803
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1225052}

--- ash/webui/personalization_app/resources/js/wallpaper/sea_pen/sea_pen_input_query_element.ts
+++ ash/webui/personalization_app/resources/js/wallpaper/sea_pen/sea_pen_input_query_element.ts
@@ -9,7 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
+import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
+import 'chrome://resources/polymer/v3_0/iron-iconset-svg/iron-iconset-svg.js';
 import 'chrome://resources/cr_elements/cr_input/cr_input.js';
+import '../../../common/icons.html.js';
+import '../../../css/wallpaper.css.js';
+import '../../../css/cros_button_style.css.js';
 
 import {assert} from 'chrome://resources/js/assert.js';
 
