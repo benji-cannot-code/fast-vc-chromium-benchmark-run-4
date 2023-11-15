98a1b5d1ae5595e42881b158e9158bd1c69f966c
Andy Paicu <andypaicu@chromium.org>
2023-11-15 10:03:40 +0000

    [PEPC] Fix grouped permissions requests and add tests that verify label text
    The initial bug was caused by a missing "return" which meant that "equal" variants would still result in the code reaching the end of the function and removing the existing prompt types. Now this behaves correctly.
    
    The rest of the CL includes updates to interactive tests to check the label(s) text for the base flow case as well as separate text-checking tests for the rest of the possible combinations for mic+camera permissions.
    
    Bug: 1462930
    Change-Id: I17e3fc4f820f144a0840d4dff851d415d92b72d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029747
    Reviewed-by: Kamila Hasanbega <hkamila@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Thomas Nguyen <tungnh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224832}

--- chrome/browser/ui/views/permissions/embedded_permission_prompt_base_view.cc
+++ chrome/browser/ui/views/permissions/embedded_permission_prompt_base_view.cc
@@ -19,21 +19,43 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ui_base_features.h"
 #include "ui/views/bubble/bubble_frame_view.h"
 #include "ui/views/controls/button/md_text_button.h"
+#include "ui/views/controls/label.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/view_class_properties.h"
 #include "ui/views/views_features.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/window/dialog_client_view.h"
 
+DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(EmbeddedPermissionPromptBaseView,
+                                      kMainViewId);
+DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(EmbeddedPermissionPromptBaseView,
+                                      kLabelViewId1);
+DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(EmbeddedPermissionPromptBaseView,
+                                      kLabelViewId2);
+
 namespace {
 
 constexpr int BODY_TOP_MARGIN = 10;
 constexpr int DISTANCE_BUTTON_VERTICAL = 8;
 
+void AddElementIdentifierToLabel(views::Label& label, size_t index) {
+  ui::ElementIdentifier id;
+  switch (index) {
+    case 0:
+      id = EmbeddedPermissionPromptBaseView::kLabelViewId1;
+      break;
+    case 1:
+      id = EmbeddedPermissionPromptBaseView::kLabelViewId2;
+      break;
+    default:
+      return;
+  }
+
+  label.SetProperty(views::kElementIdentifierKey, id);
+}
+
 }  // namespace
 
-DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(EmbeddedPermissionPromptBaseView,
-                                      kMainViewId);
 
 const std::vector<permissions::PermissionRequest*>&
 EmbeddedPermissionPromptBaseView::Delegate::Requests() const {
@@ -171,6 +193,7 @@ void EmbeddedPermissionPromptBaseView::AddRequestLine(
       std::make_unique<views::Label>(line.message));
   label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
   label->SetMultiLine(true);
+  AddElementIdentifierToLabel(*label, index);
 
   if (features::IsChromeRefresh2023()) {
     label->SetTextStyle(views::style::STYLE_BODY_3);
