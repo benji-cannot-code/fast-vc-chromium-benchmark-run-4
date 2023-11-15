e1d803412c7d82f49ad840c99cacd89f9e5e7358
Evan Liu <evliu@google.com>
2023-11-15 21:32:10 +0000

    Remove expired Net.SSLCertVerificationTime histogram
    This CL removes the expired Net.SSLCertVerificationTime histogram.
    
    OBSOLETE_HISTOGRAMS=Remove expired histograms.
    
    Bug: 1384896
    Change-Id: I11bd593162d2fb1b332ca9a67fe18e250877fa7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5026369
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1225159}

--- net/socket/ssl_client_socket_impl.h
+++ net/socket/ssl_client_socket_impl.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
-#include "base/time/time.h"
 #include "net/base/completion_once_callback.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/io_buffer.h"
@@ -256,7 +255,6 @@ class SSLClientSocketImpl : public SSLClientSocket,
   const raw_ptr<SSLClientContext> context_;
 
   std::unique_ptr<CertVerifier::Request> cert_verifier_request_;
-  base::TimeTicks start_cert_verification_time_;
 
   // Result from Cert Verifier.
   int cert_verification_result_;
