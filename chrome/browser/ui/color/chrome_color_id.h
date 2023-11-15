90a2c8717a8fb2af9449cc552de1d57b50018b1b
stluong <stluong@chromium.org>
2023-11-15 21:13:09 +0000

    Fix side panel combobox title color
    Split the color definition for the side panel combobox title color from
    the side panel label title since they have different background colors.
    The combobox title is re-defined to use the original color definition
    before regression.
    
    Fixed: 1502410
    Change-Id: I2c8921eb1e9f5fa13d6920c79afee7cfa129b22e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5032338
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1225145}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -433,6 +433,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorSidePanelDivider) \
   E_CPONLY(kColorSidePanelEditFooterBorder) \
   E_CPONLY(kColorSidePanelComboboxEntryIcon) \
+  E_CPONLY(kColorSidePanelComboboxEntryTitle) \
   E_CPONLY(kColorSidePanelEntryIcon) \
   E_CPONLY(kColorSidePanelEntryDropdownIcon) \
   E_CPONLY(kColorSidePanelEntryTitle) \
