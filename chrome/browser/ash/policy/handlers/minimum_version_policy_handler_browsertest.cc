793445e813c7c4c751b532d8fc5e02153a06e1ef
Sergiy Belozorov <sergiyb@chromium.org>
2023-11-15 17:36:41 +0000

    Create and annotate tests for enforced updates with CUJ screenplays
    Bug: b:308932300
    Change-Id: I0564a232425032e88201f95da0d1a02f38fc684b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5033271
    Commit-Queue: Sergiy Belozorov <sergiyb@chromium.org>
    Reviewed-by: Victor Gabriel Savu <vsavu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1225010}

--- chrome/browser/ash/policy/handlers/minimum_version_policy_handler_browsertest.cc
+++ chrome/browser/ash/policy/handlers/minimum_version_policy_handler_browsertest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/run_loop.h"
 #include "base/strings/stringprintf.h"
+#include "base/test/gtest_tags.h"
 #include "base/test/scoped_chromeos_version_info.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/time/default_clock.h"
@@ -271,6 +272,37 @@ IN_PROC_BROWSER_TEST_F(MinimumVersionPolicyTest, CriticalUpdateOnLoginScreen) {
   EXPECT_FALSE(ash::LoginScreenTestApi::IsOobeDialogVisible());
 }
 
+IN_PROC_BROWSER_TEST_F(MinimumVersionPolicyTest,
+                       CriticalUpdateOnLoginScreenForEolDevice) {
+  // Configure "Enforce updates" in "Auto-update settings" in Admin Console:
+  // - Choose the latest version in "if they are not running at least version"
+  // - Set "Extend this period where devices which are not receiving automatic
+  //   updates are not yet blocked to" to "No warning"
+  // - Then ensure the user is logged out of a AUE device running an older
+  //   version and can no longer log in.
+  // COM_FOUND_CUJ23_TASK5_WF2
+  base::AddTagToTestResult("feature_id",
+                           "screenplay-6267b5f4-e674-4b94-920a-d99c83f701eb");
+
+  EXPECT_FALSE(ash::LoginScreenTestApi::IsOobeDialogVisible());
+  fake_update_engine_client_->set_eol_date(
+      base::DefaultClock::GetInstance()->Now() - base::Days(1));
+
+  // Set new value for policy and check update required screen is shown on the
+  // login screen.
+  SetDevicePolicyAndWaitForSettingChange(
+      CreateMinimumVersionSingleRequirementPolicyValue(
+          kNewVersion, kNoWarning, kNoWarning,
+          false /* unmanaged_user_restricted */));
+  ash::OobeScreenWaiter(ash::UpdateRequiredView::kScreenId).Wait();
+  EXPECT_TRUE(ash::LoginScreenTestApi::IsOobeDialogVisible());
+
+  // Revoke policy and check update required screen is hidden.
+  SetDevicePolicyAndWaitForSettingChange(base::Value::Dict());
+  ash::OobeScreenExitWaiter(ash::UpdateRequiredView::kScreenId).Wait();
+  EXPECT_FALSE(ash::LoginScreenTestApi::IsOobeDialogVisible());
+}
+
 IN_PROC_BROWSER_TEST_F(MinimumVersionPolicyTest, PRE_CriticalUpdateInSession) {
   // Login the user into the session and mark as managed.
   LoginManagedUser();
@@ -498,6 +530,15 @@ IN_PROC_BROWSER_TEST_F(MinimumVersionPolicyTest,
 }
 
 IN_PROC_BROWSER_TEST_F(MinimumVersionPolicyTest, LastDayNotificationOnLogin) {
+  // Configure "Enforce updates" in "Auto-update settings" in Admin Console:
+  // - Choose the latest version in "if they are not running at least version"
+  // - Set "Block devices & user sessions after" to "1 week"
+  // - Then ensure that the device running an older version receives a
+  //   notification.
+  // COM_FOUND_CUJ23_TASK3_WF1
+  base::AddTagToTestResult("feature_id",
+                           "screenplay-62ab245b-b322-43df-8b01-370688e2c228");
+
   DisconectAllNetworks();
   EXPECT_FALSE(
       display_service_tester_->GetNotification(kUpdateRequiredNotificationId));
@@ -541,6 +582,16 @@ IN_PROC_BROWSER_TEST_F(MinimumVersionPolicyTest,
 
 IN_PROC_BROWSER_TEST_F(MinimumVersionPolicyTest,
                        NotificationOnUnmanagedUserEnabled) {
+  // Configure "Enforce updates" in "Auto-update settings" in Admin Console:
+  // - Choose the latest version in "if they are not running at least version"
+  // - Set "Extend this period where devices which are not receiving automatic
+  //   updates are not yet blocked to" to "1 week".
+  // - Then ensure that the AUE device running an older version receives a
+  //   notification and user is not logged out.
+  // COM_FOUND_CUJ23_TASK5_WF1
+  base::AddTagToTestResult("feature_id",
+                           "screenplay-7a1101a5-03c1-4cfd-a0c9-495145151a8b");
+
   fake_update_engine_client_->set_eol_date(
       base::DefaultClock::GetInstance()->Now() - base::Days(1));
   LoginUnmanagedUser();
