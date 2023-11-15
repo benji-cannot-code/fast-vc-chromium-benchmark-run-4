60732bc4ab3b592d44b4d7bc9e4843fa5fcdbfcf
Abigail Klein <abigailbklein@google.com>
2023-11-15 23:15:32 +0000

    [Read Anything] Only show the label the first 3 times.
    Store the number of times the omnibox icon label was shown in a pref.
    When the pref reaches the max (3), do not show the label anymore.
    
    Bug: 1266555
    Change-Id: I7a699c8c789e722e90df85c07fe7ba3fb1f54275
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5014113
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1225203}

--- chrome/browser/sync/prefs/chrome_syncable_prefs_database.cc
+++ chrome/browser/sync/prefs/chrome_syncable_prefs_database.cc
@@ -301,6 +301,7 @@ enum {
   kReadAloudSpeed = 100243,
   kReadAloudHighlightingEnabled = 100244,
   kHttpsFirstModeIncognito = 100245,
+  kAccessibilityReadAnythingOmniboxIconLabelShownCount = 100246,
   // See components/sync_preferences/README.md about adding new entries here.
   // vvvvv IMPORTANT! vvvvv
   // Note to the reviewer: IT IS YOUR RESPONSIBILITY to ensure that new syncable
@@ -312,7 +313,7 @@ enum {
 
 const auto& SyncablePreferences() {
   // Non-iOS specific list of syncable preferences.
-  static constexpr auto kChromeSyncablePrefsAllowlist = base::MakeFixedFlatMap<
+  static const auto kChromeSyncablePrefsAllowlist = base::MakeFixedFlatMap<
       base::StringPiece, sync_preferences::SyncablePrefMetadata>({
 #if BUILDFLAG(IS_ANDROID)
     {language::prefs::kAppLanguagePromptShown,
@@ -386,6 +387,11 @@ const auto& SyncablePreferences() {
           sync_preferences::MergeBehavior::kNone}},
         {prefs::kAccessibilityReadAnythingHighlightColor,
          {syncable_prefs_ids::kAccessibilityReadAnythingHighlightColor,
+          syncer::PREFERENCES, sync_preferences::PrefSensitivity::kNone,
+          sync_preferences::MergeBehavior::kNone}},
+        {prefs::kAccessibilityReadAnythingOmniboxIconLabelShownCount,
+         {syncable_prefs_ids::
+              kAccessibilityReadAnythingOmniboxIconLabelShownCount,
           syncer::PREFERENCES, sync_preferences::PrefSensitivity::kNone,
           sync_preferences::MergeBehavior::kNone}},
         {prefs::kLensRegionSearchEnabled,
