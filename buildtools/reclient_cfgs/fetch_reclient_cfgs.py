1b00d19f822c41f2e048b6ea3d4fbdb81e94f735
Philipp Wollermann <philwo@google.com>
2023-11-15 07:58:49 +0000

    reclient: Auto-format Python scripts with Black and "git cl format"
    YAPF is not deterministic, so for cases where it allows different variants, I applied manual judgment or the style of the auto-formatter "Black", so that the output looks nice and follows a common style.
    
    I also normalized all quotes to preferably use double-quotes.
    
    Finally, I verified by re-running "git cl format" that YAPF no longer makes any changes and is happy with the current formatting.
    
    Change-Id: I15347b52c5ec3fb78dbb478d2808203e6f8c48ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5019991
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Philipp Wollermann <philwo@chromium.org>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Auto-Submit: Philipp Wollermann <philwo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224786}

--- buildtools/reclient_cfgs/fetch_reclient_cfgs.py
+++ buildtools/reclient_cfgs/fetch_reclient_cfgs.py
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2021 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
 """This script is used to fetch reclient cfgs.
 
 This is renamed to configure_reclient_cfgs.py, delete this file if there are no
