4f2b09fc599c4195dd4834df12e8a4ee3d5b9123
Arup Barua <arup.barua@samsung.com>
2023-11-15 17:54:18 +0000

    [AF] Check and recreate Android view before usage
    With this CL, the window is null-checked before showing prompt for expiration date fix flow
    
    Bug: 1051032
    Change-Id: Ib53ab8a8423e036b408bd04863dbf6b0da318e26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5032195
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225020}

--- chrome/browser/ui/android/autofill/card_expiration_date_fix_flow_view_android.cc
+++ chrome/browser/ui/android/autofill/card_expiration_date_fix_flow_view_android.cc
@@ -48,6 +48,11 @@ void CardExpirationDateFixFlowViewAndroid::PromptDismissed(
 }
 
 void CardExpirationDateFixFlowViewAndroid::Show() {
+  if (!web_contents_->GetNativeView() ||
+      !web_contents_->GetNativeView()->GetWindowAndroid()) {
+    return;  // No window attached (yet or anymore).
+  }
+
   JNIEnv* env = base::android::AttachCurrentThread();
 
   ScopedJavaLocalRef<jstring> dialog_title =
@@ -71,8 +76,10 @@ void CardExpirationDateFixFlowViewAndroid::Show() {
 
 void CardExpirationDateFixFlowViewAndroid::ControllerGone() {
   controller_ = nullptr;
-  JNIEnv* env = base::android::AttachCurrentThread();
-  Java_AutofillExpirationDateFixFlowBridge_dismiss(env, java_object_);
+  if (java_object_) {
+    JNIEnv* env = base::android::AttachCurrentThread();
+    Java_AutofillExpirationDateFixFlowBridge_dismiss(env, java_object_);
+  }
 }
 
 CardExpirationDateFixFlowViewAndroid::~CardExpirationDateFixFlowViewAndroid() {
