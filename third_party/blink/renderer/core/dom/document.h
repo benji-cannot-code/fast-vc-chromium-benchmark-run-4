fa5b3c925acb41dcc00246fc1605326b3cbf9193
Sam Davis Omekara (from Dev Box) <samomekarajr@microsoft.com>
2023-11-15 20:18:57 +0000

    Introduce Get methods for ColorProviders in Blink
    This CL is a follow up on CL:4898493, where color providers were
    propagated to exist in Blink::Page. This CL introduces Get methods for
    the color providers in Blink::Page and helper in the Document.
    
    Subsequent CLs will see us use Document::GetClorProviderForPainting
    to paint ThemePainterDefault and LayoutTheme.
    
    Bug: 1430181
    Change-Id: Ie8cd9e9147aeae9d701e318d7c6c2b2205902d99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018430
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Commit-Queue: Sam Davis Omekara <samomekarajr@microsoft.com>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225113}

--- third_party/blink/renderer/core/dom/document.h
+++ third_party/blink/renderer/core/dom/document.h
@@ -125,6 +125,10 @@ enum class CSPDisposition : int32_t;
 }  // namespace mojom
 }  // namespace network
 
+namespace ui {
+class ColorProvider;
+}  // namespace ui
+
 namespace blink {
 
 class AXContext;
@@ -1850,6 +1854,9 @@ class CORE_EXPORT Document : public ContainerNode,
   void UpdateForcedColors();
   bool InForcedColorsMode() const;
   bool InDarkMode();
+
+  const ui::ColorProvider* GetColorProviderForPainting(
+      mojom::blink::ColorScheme color_scheme) const;
 
   // Capture the toggle event during parsing either by HTML parser or XML
   // parser.
--- third_party/blink/renderer/core/page/page.h
+++ third_party/blink/renderer/core/page/page.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/page/browsing_context_group_info.h"
 #include "third_party/blink/public/mojom/devtools/inspector_issue.mojom-blink-forward.h"
 #include "third_party/blink/public/mojom/fenced_frame/fenced_frame.mojom-blink.h"
+#include "third_party/blink/public/mojom/frame/color_scheme.mojom-blink-forward.h"
 #include "third_party/blink/public/mojom/frame/text_autosizer_page_info.mojom-blink.h"
 #include "third_party/blink/public/mojom/page/page.mojom-blink.h"
 #include "third_party/blink/public/mojom/page/page_visibility_state.mojom-blink.h"
@@ -164,6 +165,9 @@ class CORE_EXPORT Page final : public GarbageCollected<Page>,
 
   void UpdateColorProviders(
       const ColorProviderColorMaps& color_provider_colors);
+  const ui::ColorProvider* GetColorProviderForPainting(
+      mojom::blink::ColorScheme color_scheme,
+      bool in_forced_colors) const;
 
   void InitialStyleChanged();
   void UpdateAcceleratedCompositingSettings();
