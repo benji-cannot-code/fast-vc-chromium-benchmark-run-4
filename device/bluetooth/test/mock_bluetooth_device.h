89350c4e386c2a738431c712055a8293080eaf5d
Katherine Lai <laikatherine@google.com>
2023-11-15 17:02:51 +0000

    [Fast Pair] Add support for retroactive pairing for BLE HID
    For BLE devices, since we cannot connect to a message stream to
    retrieve the model ID and the BLE address is already known, the
    only remaining parameter needed is the model ID, which we
    retrieve via GATT characteristic
    
    BUG=b:308092093
    TEST=unit tests
    TEST=manually test initial and retroactive pairing for HID
    
    Change-Id: Ic93cffff190b5927f49e00d23e0781dd1c04efe0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4995811
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Jack Shira <jackshira@google.com>
    Commit-Queue: Katherine Lai <laikatherine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224991}

--- device/bluetooth/test/mock_bluetooth_device.h
+++ device/bluetooth/test/mock_bluetooth_device.h
@@ -156,6 +156,8 @@ class MockBluetoothDevice : public BluetoothDevice {
 
   void SetPaired(bool paired) { paired_ = paired; }
 
+  void SetType(device::BluetoothTransport transport) { transport_ = transport; }
+
  private:
   uint32_t bluetooth_class_;
   absl::optional<std::string> name_;
@@ -163,6 +165,7 @@ class MockBluetoothDevice : public BluetoothDevice {
   BluetoothDevice::UUIDSet uuids_;
   bool connected_;
   bool paired_;
+  device::BluetoothTransport transport_;
 
   // Used by tests to save callbacks that will be run in the future.
   base::queue<base::OnceClosure> pending_callbacks_;
