6fb0fc32ecf52ca5bd284fccb177e6a055b15711
Benjamin Keen <bkeen@google.com>
2023-11-15 22:45:25 +0000

    Delay showing bubble, for video pip, until after scrim animation completes
    This change is in response to UX feedback, in order to keep the animation timing consistent between document and video pip.
    
    Bug: 1498597
    Change-Id: Id8b00f5f48ce01b1ce1f9082a7657d1a37e0e44a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5002663
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Benjamin Keen <bkeen@google.com>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225189}

--- chrome/browser/picture_in_picture/auto_pip_setting_overlay_view.h
+++ chrome/browser/picture_in_picture/auto_pip_setting_overlay_view.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_is_test.h"
 #include "base/functional/callback.h"
+#include "base/observer_list_types.h"
 #include "base/timer/timer.h"
 #include "chrome/browser/picture_in_picture/auto_pip_setting_view.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
@@ -46,8 +47,7 @@ class AutoPipSettingOverlayView : public views::View,
 
   // Create and show the AutoPipSettingView bubble. The parent parameter will be
   // set as the bubble's parent window.
-  virtual void ShowBubble(gfx::NativeView parent,
-                          PipWindowType pip_window_type);
+  virtual void ShowBubble(gfx::NativeView parent);
 
   views::View* get_background_for_testing() const {
     CHECK_IS_TEST();
@@ -66,6 +66,18 @@ class AutoPipSettingOverlayView : public views::View,
 
   AutoPipSettingView* get_view_for_testing() { return auto_pip_setting_view_; }
 
+  class AutoPipSettingOverlayViewObserver : public base::CheckedObserver {
+   public:
+    virtual void OnAutoPipSettingOverlayViewHidden() = 0;
+  };
+
+  void AddObserver(AutoPipSettingOverlayViewObserver* observer) {
+    observers_.AddObserver(observer);
+  }
+  void RemoveObserver(AutoPipSettingOverlayViewObserver* observer) {
+    observers_.RemoveObserver(observer);
+  }
+
  private:
   // We temporarily own the setting view during init, but usually this is null.
   // Keep it separate so one doesn't accidentally use it.  It's likely that you
@@ -79,6 +91,7 @@ class AutoPipSettingOverlayView : public views::View,
   raw_ptr<views::Widget> widget_ = nullptr;
   gfx::Size bubble_size_;
   std::unique_ptr<base::OneShotTimer> show_timer_;
+  base::ObserverList<AutoPipSettingOverlayViewObserver> observers_;
   base::WeakPtrFactory<AutoPipSettingOverlayView> weak_factory_{this};
 
   // Callback used to hide the semi-opaque background layer.
@@ -86,6 +99,13 @@ class AutoPipSettingOverlayView : public views::View,
 
   // Perform a linear fade in of |layer|.
   void FadeInLayer(ui::Layer* layer);
+
+  // Returns true if |show_timer_| is currently running.
+  bool IsShowTimerRunning();
+
+  // Called whenever the AutoPipSettingOverlayView is hidden, in order to notify
+  // observers.
+  void NotifyAutoPipSettingOverlayViewHidden();
 };
 
 #endif  // CHROME_BROWSER_PICTURE_IN_PICTURE_AUTO_PIP_SETTING_OVERLAY_VIEW_H_
