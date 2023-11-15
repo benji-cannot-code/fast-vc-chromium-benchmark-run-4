172150e76ff746f2df91e2d8ead524971534fa93
John Lee <johntlee@chromium.org>
2023-11-15 20:00:38 +0000

    Add separate UI for when the user's current wallpaper is from search
    http://screencast/cast/NjM5Njc0MTk1OTAyNDY0MHxmNGRkNjczYi0zMw
    
    Bug: b:306714070
    Change-Id: I1d26e81805c0a4b3841b55101b2a781d1b72ca99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5030982
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1225101}

--- chrome/browser/resources/side_panel/customize_chrome/appearance.ts
+++ chrome/browser/resources/side_panel/customize_chrome/appearance.ts
@@ -33,6 +33,7 @@ export interface AppearanceElement {
     followThemeToggle: HTMLElement,
     followThemeToggleControl: CrToggleElement,
     uploadedImageButton: HTMLButtonElement,
+    searchedImageButton: HTMLButtonElement,
   };
 }
 
@@ -103,6 +104,12 @@ export class AppearanceElement extends PolymerElement {
         type: Boolean,
         value: false,
         computed: 'computeShowUploadedImageButton_(theme_)',
+      },
+
+      showSearchedImageButton_: {
+        type: Boolean,
+        value: false,
+        computed: 'computeShowSearchedImageButton_(theme_)',
       },
 
       showManagedDialog_: Boolean,
@@ -201,7 +208,15 @@ export class AppearanceElement extends PolymerElement {
     return !!(
         this.chromeRefresh2023Enabled_ && this.theme_ &&
         this.theme_.backgroundImage &&
-        this.theme_.backgroundImage.isUploadedImage);
+        this.theme_.backgroundImage.isUploadedImage &&
+        !this.theme_.backgroundImage.localBackgroundId);
+  }
+
+  private computeShowSearchedImageButton_(): boolean {
+    return !!(
+        this.chromeRefresh2023Enabled_ && this.theme_ &&
+        this.theme_.backgroundImage &&
+        this.theme_.backgroundImage.localBackgroundId);
   }
 
   private onEditThemeClicked_() {
@@ -219,6 +234,10 @@ export class AppearanceElement extends PolymerElement {
 
   private onUploadedImageButtonClick_() {
     this.pageHandler_.chooseLocalCustomBackground();
+  }
+
+  private onSearchedImageButtonClick_() {
+    this.dispatchEvent(new CustomEvent('wallpaper-search-click'));
   }
 
   private onSetClassicChromeClicked_() {
