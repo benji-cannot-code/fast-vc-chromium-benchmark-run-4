e14fbfe49b7d2cc9c497225d808490d4ab5d4366
Brahim Chikhaoui <bchikhaoui@google.com>
2023-11-15 16:34:36 +0000

    [OOBE][CodeHealth] refactor DisplaySizeScreen browser_test
    Enhance DisplaySizeScreenTest to leverage TestFuture's capabilities.
    
    Bug: b:310841884 , b:304436402
    Change-Id: I4cc07496e4ede0e7cc15c756c0139eaed041e13a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029875
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1224981}

--- chrome/browser/ash/login/screens/display_size_screen_browsertest.cc
+++ chrome/browser/ash/login/screens/display_size_screen_browsertest.cc
@@ -49,8 +49,8 @@ class DisplaySizeScreenTest : public OobeBaseTest {
         WizardController::default_controller()->GetScreen<DisplaySizeScreen>();
 
     original_callback_ = display_size_screen->get_exit_callback_for_testing();
-    display_size_screen->set_exit_callback_for_testing(base::BindRepeating(
-        &DisplaySizeScreenTest::HandleScreenExit, base::Unretained(this)));
+    display_size_screen->set_exit_callback_for_testing(
+        screen_result_waiter_.GetRepeatingCallback());
     OobeBaseTest::SetUpOnMainThread();
   }
 
@@ -65,13 +65,10 @@ class DisplaySizeScreenTest : public OobeBaseTest {
         DisplaySizeScreenView::kScreenId);
   }
 
-  void WaitForScreenExit() {
-    if (result_.has_value()) {
-      return;
-    }
-    base::test::TestFuture<void> waiter;
-    quit_closure_ = waiter.GetCallback();
-    EXPECT_TRUE(waiter.Wait());
+  DisplaySizeScreen::Result WaitForScreenExitResult() {
+    DisplaySizeScreen::Result result = screen_result_waiter_.Take();
+    original_callback_.Run(result);
+    return result;
   }
 
   std::vector<float> GetAvailableSizes() {
@@ -100,23 +97,13 @@ class DisplaySizeScreenTest : public OobeBaseTest {
     return current_size_index;
   }
 
-  DisplaySizeScreen::ScreenExitCallback original_callback_;
-  absl::optional<DisplaySizeScreen::Result> result_;
-
  protected:
   base::test::ScopedFeatureList feature_list_;
   LoginManagerMixin login_manager_mixin_{&mixin_host_};
 
  private:
-  void HandleScreenExit(DisplaySizeScreen::Result result) {
-    result_ = result;
-    original_callback_.Run(result);
-    if (quit_closure_) {
-      std::move(quit_closure_).Run();
-    }
-  }
-
-  base::OnceClosure quit_closure_;
+  base::test::TestFuture<DisplaySizeScreen::Result> screen_result_waiter_;
+  DisplaySizeScreen::ScreenExitCallback original_callback_;
 };
 
 IN_PROC_BROWSER_TEST_F(DisplaySizeScreenTest, InitialSliderValue) {
@@ -126,8 +113,8 @@ IN_PROC_BROWSER_TEST_F(DisplaySizeScreenTest, InitialSliderValue) {
   test::OobeJS().ExpectAttributeEQ("value", kSizeSlider, GetCurrentSizeIndex());
 
   test::OobeJS().TapOnPath(kNextButton);
-  OobeScreenExitWaiter(DisplaySizeScreenView::kScreenId).Wait();
-  EXPECT_EQ(result_.value(), DisplaySizeScreen::Result::kNext);
+  DisplaySizeScreen::Result result = WaitForScreenExitResult();
+  EXPECT_EQ(result, DisplaySizeScreen::Result::kNext);
 
   PrefService* prefs = ProfileManager::GetActiveUserProfile()->GetPrefs();
   EXPECT_TRUE(prefs->HasPrefPath(prefs::kOobeDisplaySizeFactorDeferred));
@@ -144,8 +131,8 @@ IN_PROC_BROWSER_TEST_F(DisplaySizeScreenTest, PrefUpdatedMaxSize) {
   }
 
   test::OobeJS().TapOnPath(kNextButton);
-  OobeScreenExitWaiter(DisplaySizeScreenView::kScreenId).Wait();
-  EXPECT_EQ(result_.value(), DisplaySizeScreen::Result::kNext);
+  DisplaySizeScreen::Result result = WaitForScreenExitResult();
+  EXPECT_EQ(result, DisplaySizeScreen::Result::kNext);
 
   PrefService* prefs = ProfileManager::GetActiveUserProfile()->GetPrefs();
   EXPECT_TRUE(prefs->HasPrefPath(prefs::kOobeDisplaySizeFactorDeferred));
@@ -165,8 +152,8 @@ IN_PROC_BROWSER_TEST_F(DisplaySizeScreenTest, PrefUpdatedMinSize) {
   }
 
   test::OobeJS().TapOnPath(kNextButton);
-  OobeScreenExitWaiter(DisplaySizeScreenView::kScreenId).Wait();
-  EXPECT_EQ(result_.value(), DisplaySizeScreen::Result::kNext);
+  DisplaySizeScreen::Result result = WaitForScreenExitResult();
+  EXPECT_EQ(result, DisplaySizeScreen::Result::kNext);
 
   PrefService* prefs = ProfileManager::GetActiveUserProfile()->GetPrefs();
   EXPECT_TRUE(prefs->HasPrefPath(prefs::kOobeDisplaySizeFactorDeferred));
