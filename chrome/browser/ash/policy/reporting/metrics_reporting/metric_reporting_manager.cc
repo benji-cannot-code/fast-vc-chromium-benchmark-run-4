51720d987168187571ebbbb34cc4fc7280d63522
Hong Xu <xuhong@chromium.org>
2023-11-16 00:50:48 +0000

    Create a immediate priority report queue for fatal crash events
    Fatal crash events are emitted by healthd periodically and we should
    report them immediately after receiving them.
    
    DanglingUntriaged-notes: No structural change to existing code.
    
    Bug: b:266018440
    Change-Id: Iff17a9b952e71ef1782fa3892b2e45e297ee9044
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5034967
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Hong Xu <xuhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1225246}

--- chrome/browser/ash/policy/reporting/metrics_reporting/metric_reporting_manager.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/metric_reporting_manager.cc
@@ -244,6 +244,9 @@ MetricReportingManager::MetricReportingManager(
       source_info);
   event_report_queue_ = delegate_->CreateMetricReportQueue(
       EventType::kDevice, Destination::EVENT_METRIC, Priority::SLOW_BATCH,
+      /*rate_limiter=*/nullptr, source_info);
+  immediate_event_report_queue_ = delegate_->CreateMetricReportQueue(
+      EventType::kDevice, Destination::EVENT_METRIC, Priority::IMMEDIATE,
       /*rate_limiter=*/nullptr, std::move(source_info));
   DelayedInit();
 
@@ -270,6 +273,7 @@ void MetricReportingManager::Shutdown() {
   telemetry_report_queue_.reset();
   user_telemetry_report_queue_.reset();
   event_report_queue_.reset();
+  immediate_event_report_queue_.reset();
   user_event_report_queue_.reset();
   app_event_report_queue_.reset();
   website_event_report_queue_.reset();
@@ -681,7 +685,7 @@ void MetricReportingManager::InitFatalCrashCollectors() {
 
   if (base::FeatureList::IsEnabled(kEnableFatalCrashEventsObserver)) {
     event_observer_managers_.emplace_back(delegate_->CreateEventObserverManager(
-        FatalCrashEventsObserver::Create(), event_report_queue_.get(),
+        FatalCrashEventsObserver::Create(), immediate_event_report_queue_.get(),
         &reporting_settings_, ash::kReportDeviceCrashReportInfo,
         metrics::kReportDeviceCrashReportInfoDefaultValue,
         /*collector_pool=*/this));
