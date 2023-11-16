6d1b44c83f131efdb514efa498980947fce56c8a
Eriko Kurimoto <elkurin@chromium.org>
2023-11-16 01:06:36 +0000

    TabletState: Remove TabletModeObserver from WMHelper
    This CL removes InTabletMode and Add/RemoveTabletModeObserver from
    WMHelper and let WMHelper users use dipslay::Screen instead.
    
    Bug: 1502114
    Change-Id: I8090f3172b944a7daaafa11731d3e10ae94ae80b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5028991
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225251}

--- components/exo/wayland/zcr_remote_shell_impl.h
+++ components/exo/wayland/zcr_remote_shell_impl.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/wayland/wayland_display_observer.h"
 #include "components/exo/wayland/zcr_remote_shell.h"
 #include "components/exo/wayland/zcr_remote_shell_event_mapping.h"
+#include "ui/display/display_observer.h"
 #include "ui/display/manager/display_manager_observer.h"
 
 namespace exo {
@@ -55,8 +56,7 @@ class WaylandRemoteOutput : public WaylandDisplayObserver {
 
 // Implements remote shell interface and monitors workspace state needed
 // for the remote shell interface.
-class WaylandRemoteShell : public ash::TabletModeObserver,
-                           public display::DisplayObserver,
+class WaylandRemoteShell : public display::DisplayObserver,
                            public SeatObserver,
                            public display::DisplayManagerObserver {
  public:
@@ -103,11 +103,6 @@ class WaylandRemoteShell : public ash::TabletModeObserver,
   // display::DisplayManagerObserver:
   void OnWillProcessDisplayChanges() override;
   void OnDidProcessDisplayChanges() override;
-
-  // Overridden from ash::TabletModeObserver:
-  void OnTabletModeStarted() override;
-  void OnTabletModeEnding() override;
-  void OnTabletModeEnded() override;
 
   // Overridden from SeatObserver:
   void OnSurfaceFocused(Surface* gained_focus,
