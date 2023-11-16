221651069f537e8995e5befb39688e0a4cdbcf87
Darren Shen <shend@chromium.org>
2023-11-16 04:49:08 +0000

    Add picker view.
    Currently just an empty Widget with a border and no bounds.
    
    Bug: b/310087382
    Change-Id: I8e9996bf214d3cb8ca8de641aff36961273a2fcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029097
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225311}

---
+++ ash/picker/views/picker_view.h
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_PICKER_VIEWS_PICKER_VIEW_H_
+#define ASH_PICKER_VIEWS_PICKER_VIEW_H_
+
+#include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/views/view.h"
+#include "ui/views/widget/unique_widget_ptr.h"
+#include "ui/views/widget/widget_delegate.h"
+
+namespace views {
+class Widget;
+class NonClientFrameView;
+}  // namespace views
+
+namespace ash {
+
+// View for the Picker widget.
+class PickerView : public views::WidgetDelegateView {
+ public:
+  METADATA_HEADER(PickerView);
+
+  PickerView();
+  PickerView(const PickerView&) = delete;
+  PickerView& operator=(const PickerView&) = delete;
+  ~PickerView() override;
+
+  static views::UniqueWidgetPtr CreateWidget();
+
+  // views::WidgetDelegateView:
+  std::unique_ptr<views::NonClientFrameView> CreateNonClientFrameView(
+      views::Widget* widget) override;
+};
+
+}  // namespace ash
+
+#endif  // ASH_PICKER_VIEWS_PICKER_VIEW_H_
