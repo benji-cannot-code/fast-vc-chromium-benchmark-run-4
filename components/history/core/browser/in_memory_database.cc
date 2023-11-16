a5c3d0ee9c104f6898949f81a4062cf80191f8f7
Etienne Bergeron <etienneb@chromium.org>
2023-11-16 19:59:25 +0000

    Remove explicit exclusive_locking option
    This CL is removing the explicit database option exclusive_locking
    since it is the default since https://crrev.com/817386.
    
    This CL has no behavior changes.
    
    R=grt@chromium.org
    
    Bug: 1120969
    Change-Id: I7d6f415ade90e0161621767a00dadaf9b73b6fe7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029318
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225670}

--- components/history/core/browser/in_memory_database.cc
+++ components/history/core/browser/in_memory_database.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace history {
 
 InMemoryDatabase::InMemoryDatabase()
-    : db_({.exclusive_locking = true, .page_size = 4096, .cache_size = 500}) {}
+    : db_({.page_size = 4096, .cache_size = 500}) {}
 
 InMemoryDatabase::~InMemoryDatabase() = default;
 
