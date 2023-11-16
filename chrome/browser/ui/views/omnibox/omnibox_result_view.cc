c10cbe60a56bf568d5a62b7756537c05616439be
Orin Jaworski <orinj@google.com>
2023-11-16 19:14:52 +0000

    [omnibox][actions] Show match selection indicator on instant keywords
    For the keyword mode refresh, instant keywords can be accessed via
    their own suggestion. In such cases we will show the match selection
    indicator, the blue bar at the left side of the match. Before, it
    wasn't shown because keywords were accessed via button row buttons.
    Now both cases are handled: indicator shown for instant keywords and
    hidden for ordinary button type keywords.
    
    Screenshots:
    - http://screen/5z6AfbhKXvsDTrY
    - http://screen/5oykvsoVNpQzqcG
    - http://screen/7a3T7JMrf3GCRCN
    
    Bug: 1495562
    Change-Id: I51cda9ab66abf6aeaed9f0d699c95116600b4b70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5034474
    Reviewed-by: Khalid Peer <khalidpeer@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1225641}

--- chrome/browser/ui/views/omnibox/omnibox_result_view.cc
+++ chrome/browser/ui/views/omnibox/omnibox_result_view.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/actions/omnibox_pedal.h"
 #include "components/omnibox/browser/autocomplete_match_type.h"
 #include "components/omnibox/browser/omnibox.mojom-shared.h"
+#include "components/omnibox/browser/omnibox_client.h"
 #include "components/omnibox/browser/omnibox_controller.h"
 #include "components/omnibox/browser/omnibox_edit_model.h"
 #include "components/omnibox/browser/omnibox_field_trial.h"
@@ -442,9 +443,20 @@ void OmniboxResultView::ApplyThemeAndRefreshIcons(bool force_reapply_styles) {
 
   // The selection indicator indicates when the suggestion is focused. Do not
   // show the selection indicator if an auxiliary button is selected.
-  selection_indicator_->SetVisible(selected &&
-                                   popup_view_->GetSelection().state ==
-                                       OmniboxPopupSelection::NORMAL);
+  if (OmniboxFieldTrial::IsKeywordModeRefreshEnabled() &&
+      match_.HasInstantKeyword(
+          popup_view_->controller()->client()->GetTemplateURLService())) {
+    const OmniboxPopupSelection::LineState line_state =
+        popup_view_->GetSelection().state;
+    selection_indicator_->SetVisible(
+        selected &&
+        (line_state == OmniboxPopupSelection::LineState::NORMAL ||
+         line_state == OmniboxPopupSelection::LineState::KEYWORD_MODE));
+  } else {
+    selection_indicator_->SetVisible(selected &&
+                                     popup_view_->GetSelection().state ==
+                                         OmniboxPopupSelection::NORMAL);
+  }
 }
 
 void OmniboxResultView::OnSelectionStateChanged() {
