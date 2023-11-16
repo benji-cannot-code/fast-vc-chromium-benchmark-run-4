252634a39d797dd5240861bc18109a1e9829e5f1
Michelle <michellegc@google.com>
2023-11-16 02:59:26 +0000

    Set editor menu as disabled once query is submitted.
    Disable editor menu buttons and textfield once a query is submitted.
    This is to prevent the user from trying to submit another query after
    they've already pressed a preset chip or entered a freeform prompt.
    
    (The EditorMenuView will remain open until the editor WebUI triggered by
    the query has loaded and is shown. Then, the EditorMenuView is
    automatically closed once it loses focus.)
    
    Bug: b:302043565
    Change-Id: Ia0b096628b1fba0dfa727aaf5c3cd0178a656055
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5035457
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1225291}

--- chrome/browser/ui/views/editor_menu/editor_menu_controller_impl.cc
+++ chrome/browser/ui/views/editor_menu/editor_menu_controller_impl.cc
@@ -158,6 +158,7 @@ void EditorMenuControllerImpl::OnChipButtonPressed(
     return;
   }
 
+  DisableEditorMenu();
   card_session_->panel_manager.StartEditingFlowWithPreset(
       std::string(text_query_id));
 }
@@ -168,6 +169,7 @@ void EditorMenuControllerImpl::OnTextfieldArrowButtonPressed(
     return;
   }
 
+  DisableEditorMenu();
   card_session_->panel_manager.StartEditingFlowWithFreeform(
       base::UTF16ToUTF8(text));
 }
@@ -254,6 +256,13 @@ void EditorMenuControllerImpl::OnEditorCardHidden() {
   // The currently visible card is closing and being removed from the user's
   // view, the EditorCardSession has ended.
   card_session_ = nullptr;
+}
+
+void EditorMenuControllerImpl::DisableEditorMenu() {
+  auto* editor_menu_view = editor_menu_widget_->GetContentsView();
+  if (views::IsViewClass<EditorMenuView>(editor_menu_view)) {
+    views::AsViewClass<EditorMenuView>(editor_menu_view)->DisableMenu();
+  }
 }
 
 }  // namespace chromeos::editor_menu
--- chrome/browser/ui/views/editor_menu/editor_menu_view.h
+++ chrome/browser/ui/views/editor_menu/editor_menu_view.h
@@ -67,6 +67,8 @@ class EditorMenuView : public views::View, public views::WidgetObserver {
 
   void ResetPreTargetHandler();
 
+  void DisableMenu();
+
   views::View* chips_container_for_testing() { return chips_container_; }
 
   EditorMenuTextfieldView* textfield_for_testing() { return textfield_; }
