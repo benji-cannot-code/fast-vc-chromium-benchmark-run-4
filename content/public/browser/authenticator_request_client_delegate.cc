8d6d1d30aec9e6f9a71a2b6b9c2f938d19a76c7b
Lin Guo <sinyu890807@gmail.com>
2023-11-16 03:28:50 +0000

    Eliminate all checks for BUILDFLAG(IS_ANDROID) and remove any redundant code present in authenticator_request_client_delegate.cc.
    The authenticator_request_client_delegate.cc file is excluded from the Android build by GN configuration. Consequently, the presence of a BUILDFLAG(IS_ANDROID) check in content/public/browser/authenticator_request_client_delegate.cc is unnecessary.
    
    Change-Id: Id4d8eb3fc877746c693e5239f528287d8fa4c622
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5028575
    Commit-Queue: Zengfeng Ye <zenye@microsoft.com>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225296}

--- content/public/browser/authenticator_request_client_delegate.cc
+++ content/public/browser/authenticator_request_client_delegate.cc
@@ -43,7 +43,6 @@ bool WebAuthenticationDelegate::OriginMayUseRemoteDesktopClientOverride(
   return false;
 }
 
-#if !BUILDFLAG(IS_ANDROID)
 absl::optional<std::string>
 WebAuthenticationDelegate::MaybeGetRelyingPartyIdOverride(
     const std::string& claimed_relying_party_id,
@@ -97,7 +96,6 @@ WebAuthenticationRequestProxy* WebAuthenticationDelegate::MaybeGetRequestProxy(
     const url::Origin& caller_origin) {
   return nullptr;
 }
-#endif  // !IS_ANDROID
 
 #if BUILDFLAG(IS_MAC)
 absl::optional<WebAuthenticationDelegate::TouchIdAuthenticatorConfig>
@@ -114,15 +112,6 @@ WebAuthenticationDelegate::GetGenerateRequestIdCallback(
   return base::NullCallback();
 }
 #endif
-
-#if BUILDFLAG(IS_ANDROID)
-base::android::ScopedJavaLocalRef<jobject>
-WebAuthenticationDelegate::GetIntentSender(WebContents* web_contents) {
-  return nullptr;
-}
-#endif
-
-#if !BUILDFLAG(IS_ANDROID)
 
 AuthenticatorRequestClientDelegate::AuthenticatorRequestClientDelegate() =
     default;
@@ -240,7 +229,5 @@ void AuthenticatorRequestClientDelegate::FinishCollectToken() {}
 
 void AuthenticatorRequestClientDelegate::OnRetryUserVerification(int attempts) {
 }
-
-#endif  // !IS_ANDROID
 
 }  // namespace content
