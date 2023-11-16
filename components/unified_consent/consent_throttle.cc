f5d9b8d3154beed0fac6041938797bf06db26ea2
Tommy C. Li <tommycli@chromium.org>
2023-11-16 18:33:14 +0000

    [unified_consent] Fix ConsentThrottle crash from mutate during iterate
    We have evidence that this crash is caused by enqueuing new requests
    within the callback itself.
    
    This CL makes the throttle robust to callbacks that enqueue new
    requests, and adds a unit test for that case.
    
    Bug: 1483454
    Change-Id: I452370ea0a90b2586c67c161220dc36f40aaede8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5030755
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225601}

--- components/unified_consent/consent_throttle.cc
+++ components/unified_consent/consent_throttle.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <algorithm>
+
 #include "components/unified_consent/consent_throttle.h"
 #include "base/functional/bind.h"
 #include "base/time/time.h"
@@ -44,12 +46,15 @@ void ConsentThrottle::OnUrlKeyedDataCollectionConsentStateChanged(
     return;
   }
 
-  for (auto& request_callback : enqueued_request_callbacks_) {
+  request_processing_timer_.Stop();
+
+  // The request callbacks can modify the vector while running. Swap the vector
+  // onto the stack to prevent crashing. https://crbug.com/1483454.
+  std::vector<RequestCallback> callbacks;
+  std::swap(callbacks, enqueued_request_callbacks_);
+  for (auto& request_callback : callbacks) {
     FulfillRequestCallback(consent_state, std::move(request_callback));
   }
-
-  enqueued_request_callbacks_.clear();
-  request_processing_timer_.Stop();
 }
 
 void ConsentThrottle::EnqueueRequest(RequestCallback callback) {
@@ -72,12 +77,16 @@ void ConsentThrottle::EnqueueRequest(RequestCallback callback) {
 }
 
 void ConsentThrottle::OnTimeoutExpired() {
-  for (auto& request_callback : enqueued_request_callbacks_) {
-    CHECK_EQ(consent_helper_->GetConsentState(),
-             UrlKeyedDataCollectionConsentHelper::State::kInitializing);
+  CHECK_EQ(consent_helper_->GetConsentState(),
+           UrlKeyedDataCollectionConsentHelper::State::kInitializing);
+
+  // The request callbacks can modify the vector while running. Swap the vector
+  // onto the stack to prevent crashing. https://crbug.com/1483454.
+  std::vector<RequestCallback> callbacks;
+  std::swap(callbacks, enqueued_request_callbacks_);
+  for (auto& request_callback : callbacks) {
     std::move(request_callback).Run(false);
   }
-  enqueued_request_callbacks_.clear();
 }
 
 }  // namespace unified_consent
