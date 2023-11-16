4c36cf01e0d2e36220d9fbd3f2cabbf1cc6b3775
Shu-yu Guo <syg@chromium.org>
2023-11-16 08:02:20 +0000

    [gin] Remove array change-by-copy flag
    The feature has shipped since M110
    
    Bug: v8:12764
    Change-Id: I759456825e61c4cf6630cad264e5f3fc2ac171d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5035293
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Auto-Submit: Shu-yu Guo <syg@chromium.org>
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225364}

--- gin/gin_features.h
+++ gin/gin_features.h
@@ -58,7 +58,6 @@ GIN_EXPORT BASE_DECLARE_FEATURE(kV8TurboFastApiCalls);
 GIN_EXPORT BASE_DECLARE_FEATURE(kV8UseLibmTrigFunctions);
 GIN_EXPORT extern const base::FeatureParam<base::TimeDelta>
     kV8MemoryReducerStartDelay;
-GIN_EXPORT BASE_DECLARE_FEATURE(kJavaScriptChangeArrayByCopy);
 GIN_EXPORT BASE_DECLARE_FEATURE(kJavaScriptRabGsab);
 GIN_EXPORT BASE_DECLARE_FEATURE(kJavaScriptRegExpUnicodeSets);
 GIN_EXPORT BASE_DECLARE_FEATURE(kJavaScriptSymbolAsWeakMapKey);
