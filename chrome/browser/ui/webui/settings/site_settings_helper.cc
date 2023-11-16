5a4664289418c5e67a736265bd5a08bafd447e4b
Sina Firoozabadi <sinafirooz@google.com>
2023-11-16 02:03:17 +0000

    Remove includes left behind from crrev.com/c/5008933.
    Bug: 1420307
    Change-Id: I143bf6224e80375c7d955e7a1cfc5233fc8d7034
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5027831
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Sina Firoozabadi <sinafirooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225269}

--- chrome/browser/ui/webui/settings/site_settings_helper.cc
+++ chrome/browser/ui/webui/settings/site_settings_helper.cc
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/content_settings_pattern.h"
 #include "components/content_settings/core/common/content_settings_utils.h"
 #include "components/permissions/contexts/bluetooth_chooser_context.h"
-#include "components/permissions/features.h"
 #include "components/permissions/object_permission_context_base.h"
 #include "components/permissions/permission_decision_auto_blocker.h"
 #include "components/permissions/permission_util.h"
--- content/browser/child_process_security_policy_impl.cc
+++ content/browser/child_process_security_policy_impl.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "build/build_config.h"
-#include "components/permissions/features.h"
 #include "content/browser/bad_message.h"
 #include "content/browser/isolated_origin_util.h"
 #include "content/browser/process_lock.h"
