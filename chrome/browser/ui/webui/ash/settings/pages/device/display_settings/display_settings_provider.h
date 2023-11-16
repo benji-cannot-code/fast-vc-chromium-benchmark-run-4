2e850e2bd6bff2c25a954cd8b97c10ced8aec0d8
Thomas Lukaszewicz <tluk@chromium.org>
2023-11-16 17:41:55 +0000

    Reland "[display] Propagate change state on display config update"
    This is a reland of commit 11e791d0b2c6d2bbbd1fd43345a64aaf320b49cb
    
    See diff against patchset 1. Missing a reset() in the test
    fixture's TearDown() method.
    
    Original change's description:
    > [display] Propagate change state on display config update
    >
    > This CL updates OnDidProcessDisplayChanges() to propagate a struct
    > of changes to the display configuration. This API is currently only
    > implemented by ChromeOS Ash.
    >
    > There are several DisplayManager methods that modify the existing
    > display configuration and these methods can call each other in a
    > nested fashion. BeginEndNotifier exists to allow a single call to
    > OnWillProcessDisplayChanges() and OnDidProcessDisplayChanges() on
    > either side of an atomic change in display configuration state.
    >
    > This CL introduces DisplayManager::PendingDisplayChanges which is
    > intended to track all display config changes that occur before
    > the last BeginEndNotifier falls out of scope. At this time
    > this is translated to a DisplayConfigurationChange struct more
    > appropriate for observers and propagated in the call to
    > OnDidProcessDisplayChanges().
    >
    > This CL also adds a test to ensure what is propagated by
    > the DisplayManagerObserver is consistent with what is propagated by
    > the DisplayObserver.
    >
    > Follow up CLs will also
    >   - update exo to leverage this as a means to consume an atomic
    >     update to the display configuration state.
    >
    >   - consolidate the other existing mechanisms that communicate
    >     display configuration changes. For e.g. see
    >     DisplayManager::Delegate::Pre[Post]DisplayConfigurationChange()
    >     and downstream methods.
    >
    >   - update clients that rely on other means to infer display
    >     configuration changes (using OnDisplayAdded[Removed]() for e.g.)
    >
    > Bug: 1427019, 1408304, 1498683
    > Change-Id: I87bdf62b5fe90b7160c03e1f4597059288173959
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4973206
    > Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1224481}
    
    Bug: 1427019, 1408304, 1498683
    Change-Id: I68f75fcd649f92f66a41e3d712ade37e7982b5d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5034657
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225571}

--- chrome/browser/ui/webui/ash/settings/pages/device/display_settings/display_settings_provider.h
+++ chrome/browser/ui/webui/ash/settings/pages/device/display_settings/display_settings_provider.h
@@ -42,7 +42,8 @@ class DisplaySettingsProvider : public mojom::DisplaySettingsProvider,
   void OnTabletModeEventsBlockingChanged() override;
 
   // display::DisplayManagerObserver:
-  void OnDidProcessDisplayChanges() override;
+  void OnDidProcessDisplayChanges(
+      const DisplayConfigurationChange& configuration_change) override;
 
  private:
   mojo::RemoteSet<mojom::TabletModeObserver> tablet_mode_observers_;
