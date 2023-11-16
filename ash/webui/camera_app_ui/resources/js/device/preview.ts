3c9c4fcdc57a16f42b632b1982d394e70c1a974e
Dorah <dorahkim@google.com>
2023-11-16 16:11:26 +0000

    CCA: Enable auto QR code detection
    This CL enables auto QR code detection in a preview of Photo mode. It
    starts a scanner with a 200ms interval for the first 3 minutes and slows
    down the interval to 1000ms after that. This 3 minutes timer is reset if
    a user is considered as active, such as taking a photo or changing
    cameras.
    
    Bug: b:298581154
    Test: Checked from a log.
    Change-Id: Ie4adc2c7ecb4f5b9b84095113f5c400830e82e1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4852462
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Dorah Kim <dorahkim@google.com>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225526}

--- ash/webui/camera_app_ui/resources/js/device/preview.ts
+++ ash/webui/camera_app_ui/resources/js/device/preview.ts
@@ -10,11 +10,15 @@ import {
   assertInstanceof,
 } from '../assert.js';
 import {queuedAsyncCallback} from '../async_job_queue.js';
+import * as barcodeChip from '../barcode_chip.js';
 import * as dom from '../dom.js';
 import {reportError} from '../error.js';
 import * as expert from '../expert.js';
 import {FaceOverlay} from '../face.js';
+import {Flag} from '../flag.js';
 import {Point} from '../geometry.js';
+import {BarcodeScanner} from '../models/barcode.js';
+import * as loadTimeData from '../models/load_time_data.js';
 import {DeviceOperator, parseMetadata} from '../mojo/device_operator.js';
 import {
   AndroidControlAeAntibandingMode,
@@ -41,6 +45,7 @@ import {
   ErrorType,
   Facing,
   getVideoTrackSettings,
+  Mode,
   PreviewVideo,
   Resolution,
 } from '../type.js';
@@ -60,6 +65,11 @@ export class Preview {
    * Video element to capture the stream.
    */
   private video = dom.get('#preview-video', HTMLVideoElement);
+
+  /**
+   * A barcode scanner to detect barcodes. Only used in Photo mode.
+   */
+  private barcodeScanner: BarcodeScanner|null = null;
 
   /**
    * The observer endpoint for preview metadata.
@@ -111,6 +121,8 @@ export class Preview {
 
   private enableFaceOverlay = false;
 
+  private readonly autoQRFlag = loadTimeData.getChromeFlag(Flag.AUTO_QR);
+
   /**
    * @param onNewStreamNeeded Callback to request new stream.
    */
@@ -118,6 +130,14 @@ export class Preview {
     expert.addObserver(
         expert.ExpertOption.SHOW_METADATA,
         queuedAsyncCallback('keepLatest', () => this.updateShowMetadata()));
+
+    // Reset the auto QR code scanner timer after taking a photo
+    state.addObserver(state.State.TAKING, (taking, _) => {
+      if (!state.get(Mode.PHOTO) || taking) {
+        return;
+      }
+      this.barcodeScanner?.resetTimer();
+    });
   }
 
   getVideo(): PreviewVideo {
@@ -359,6 +379,15 @@ export class Preview {
           this.onPreviewExpired === null || this.onPreviewExpired.isSignaled());
       this.onPreviewExpired = new WaitableEvent();
       state.set(state.State.STREAMING, true);
+
+      // Enable auto QR code scanner in Photo mode preview
+      if (state.get(Mode.PHOTO) && this.autoQRFlag) {
+        this.barcodeScanner = new BarcodeScanner(this.video, (value) => {
+          barcodeChip.show(value);
+        });
+
+        this.barcodeScanner?.resetTimer();
+      }
     } catch (e) {
       await this.close();
       throw e;
@@ -370,6 +399,9 @@ export class Preview {
    * Closes the preview.
    */
   async close(): Promise<void> {
+    this.barcodeScanner?.stop();
+    this.barcodeScanner = null;
+
     this.clearWatchdog();
     // Pause video element to avoid black frames during transition.
     this.video.pause();
