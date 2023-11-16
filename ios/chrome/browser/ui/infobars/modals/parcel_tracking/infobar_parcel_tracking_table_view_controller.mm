9d0f7a2a955d022c399ba7863e307fd6f06470a0
Hira Mahmood <hiramahmood@google.com>
2023-11-16 20:26:58 +0000

    [iOS] Fix spacing in Parcel Tracking Infobar Modal
    This CL fixes the spacing between the "Untrack This Package" button
    and the "Report an issue" button by adding the "Report an issue" button
    as a section footer (previously a table footer).
    
    Before: https://screenshot.googleplex.com/7qHEGEGsv3v3Gqu
    
    After: https://screenshot.googleplex.com/3Rm6CpXHL7syngu
    
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL
    Bug: 1473449
    Change-Id: I225fde7f0c4926c90528238012c37d9bdd9b9dbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5037451
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225683}

--- ios/chrome/browser/ui/infobars/modals/parcel_tracking/infobar_parcel_tracking_table_view_controller.mm
+++ ios/chrome/browser/ui/infobars/modals/parcel_tracking/infobar_parcel_tracking_table_view_controller.mm
@@ -89,10 +89,6 @@ NSString* const kCarrierKey = @"carrier";
   self.navigationItem.leftBarButtonItem = cancelButton;
   self.navigationController.navigationBar.prefersLargeTitles = NO;
 
-  self.tableView.tableFooterView = [self reportIssueButton];
-  self.tableView.tableFooterView.frame =
-      CGRectMake(0, 0, self.tableView.bounds.size.width, kFooterFrameHeight);
-
   [self loadModel];
 }
 
@@ -150,8 +146,11 @@ NSString* const kCarrierKey = @"carrier";
                  addTarget:self
                     action:@selector(trackButtonWasPressed:)
           forControlEvents:UIControlEventTouchUpInside];
-      tableViewTextButtonCell.separatorInset =
-          UIEdgeInsetsMake(0, 0, 0, self.tableView.bounds.size.width);
+      // Hide the separator if "Track This Parcel" button is displayed.
+      if (!trackingParcels_) {
+        tableViewTextButtonCell.separatorInset =
+            UIEdgeInsetsMake(0, 0, 0, self.tableView.bounds.size.width);
+      }
       break;
     }
     case ItemType::kTrackingNumber: {
@@ -168,6 +167,16 @@ NSString* const kCarrierKey = @"carrier";
   [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
 
   return cell;
+}
+
+- (UIView*)tableView:(UITableView*)tableView
+    viewForFooterInSection:(NSInteger)section {
+  return [self reportIssueButton];
+}
+
+- (CGFloat)tableView:(UITableView*)tableView
+    heightForFooterInSection:(NSInteger)section {
+  return kFooterFrameHeight;
 }
 
 #pragma mark - InfobarParcelTrackingModalConsumer
@@ -194,7 +203,6 @@ NSString* const kCarrierKey = @"carrier";
       [UIButtonConfiguration plainButtonConfiguration];
   buttonConfiguration.attributedTitle = [self reportIssueString];
   button.configuration = buttonConfiguration;
-  button.contentVerticalAlignment = UIControlContentVerticalAlignmentTop;
   [button addTarget:self
                 action:@selector(reportIssueButtonWasPressed:)
       forControlEvents:UIControlEventTouchUpInside];
