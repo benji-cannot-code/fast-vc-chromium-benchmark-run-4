6f8f57e5c188f662a55a8be0949f6023c8a63fa5
Ankush Singh <ankushkush@google.com>
2023-11-16 19:38:13 +0000

    [CodeHealth] Fix ownership of InfoBubble instance in TooltipIcon
    As per the current usage, there is no clear reason why TooltipIcon
    should not own the instance. This *might* help avoid leakage if
    OnWidgetDestroyed() is not called in any case.
    
    Bug: 1462022
    Change-Id: I2c3a65a0e4604f9865a332da8f6a5826d91db8bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5024995
    Reviewed-by: Joseph Park <josephjoopark@chromium.org>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1225661}

--- ui/views/bubble/tooltip_icon.cc
+++ ui/views/bubble/tooltip_icon.cc
@@ -25,10 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace views {
 
 TooltipIcon::TooltipIcon(const std::u16string& tooltip, int tooltip_icon_size)
-    : tooltip_(tooltip),
-      tooltip_icon_size_(tooltip_icon_size),
-
-      bubble_(nullptr) {
+    : tooltip_(tooltip), tooltip_icon_size_(tooltip_icon_size) {
   SetFocusBehavior(PlatformStyle::kDefaultFocusBehavior);
   set_suppress_default_focus_handling();
   FocusRing::Install(this);
@@ -140,7 +137,7 @@ void TooltipIcon::ShowBubble() {
 
   SetDrawAsHovered(true);
 
-  bubble_ = new InfoBubble(this, anchor_point_arrow_, tooltip_);
+  bubble_ = std::make_unique<InfoBubble>(this, anchor_point_arrow_, tooltip_);
   bubble_->set_preferred_width(preferred_width_);
   // When shown due to a gesture event, close on deactivate (i.e. don't use
   // "focusless").
@@ -171,7 +168,7 @@ void TooltipIcon::OnWidgetDestroyed(Widget* widget) {
 
   SetDrawAsHovered(false);
   mouse_watcher_.reset();
-  bubble_ = nullptr;
+  bubble_.reset();
 }
 
 BEGIN_METADATA(TooltipIcon, ImageView)
