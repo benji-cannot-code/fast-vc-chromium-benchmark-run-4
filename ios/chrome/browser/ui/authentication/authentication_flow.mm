d7c741f8921739c65c79da8517041a4116e75d6b
Vincent Boisselle <vincb@google.com>
2023-11-16 21:35:25 +0000

    [ios][up] Dont show management dialog when browser already managed
    If the browser is already managed by the platform or by any other
    machine level source, it is not necessary to inform the user again
    that their browser is about to be managed as it is already
    the case.
    
    Bug: b/309958656
    Change-Id: I61ad1a54e8ffe57ba364f0cfe0d142b944ed8707
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5017143
    Reviewed-by: Menghan Yang <myuu@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225731}

--- ios/chrome/browser/ui/authentication/authentication_flow.mm
+++ ios/chrome/browser/ui/authentication/authentication_flow.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/sync/service/sync_service.h"
 #import "components/sync/service/sync_user_settings.h"
 #import "ios/chrome/browser/flags/ios_chrome_flag_descriptions.h"
+#import "ios/chrome/browser/policy/browser_policy_connector_ios.h"
 #import "ios/chrome/browser/policy/cloud/user_policy_switch.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
@@ -57,6 +58,13 @@ enum AuthenticationState {
   CLEANUP_BEFORE_DONE,
   DONE
 };
+
+// Returns yes if the browser has machine level policies.
+bool HasMachineLevelPolicies() {
+  BrowserPolicyConnectorIOS* policy_connector =
+      GetApplicationContext()->GetBrowserPolicyConnector();
+  return policy_connector && policy_connector->HasMachineLevelPolicies();
+}
 
 }  // namespace
 
@@ -669,6 +677,12 @@ enum AuthenticationState {
 - (BOOL)shouldShowManagedConfirmationForHostedDomain:(NSString*)hostedDomain {
   if ([hostedDomain length] == 0) {
     // No hosted domain, don't show the dialog as there is no host.
+    return NO;
+  }
+
+  if (HasMachineLevelPolicies()) {
+    // Don't show the dialog if the browser has already machine level policies
+    // as the user already knows that their browser is managed.
     return NO;
   }
 
