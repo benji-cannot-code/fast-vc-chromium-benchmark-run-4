1dae4aef36cbbc0d7d76006507be9eb101860d61
Dorah <dorahkim@google.com>
2023-11-16 12:20:23 +0000

    CCA: Create a feature flag for auto QR detection
    This CL creates a feature flag for auto QR code detection in ChromeOS
    Camera App.
    
    Bug: b:298581154
    Test: Checked from a log.
    
    Change-Id: I2d39401c2628c614f8a36910ce1f8d2955237dde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4988386
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Dorah Kim <dorahkim@google.com>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225435}

--- chrome/browser/ash/system_web_apps/apps/camera_app/chrome_camera_app_ui_delegate.cc
+++ chrome/browser/ash/system_web_apps/apps/camera_app/chrome_camera_app_ui_delegate.cc
@@ -334,6 +334,8 @@ void ChromeCameraAppUIDelegate::PopulateLoadTimeData(
   source->AddString("board_name", base::SysInfo::GetLsbReleaseBoard());
   source->AddString("device_type",
                     DeviceTypeToString(chromeos::GetDeviceType()));
+  source->AddBoolean("auto_qr", base::FeatureList::IsEnabled(
+                                    ash::features::kCameraAppAutoQRDetection));
 
   const PrefService* prefs = Profile::FromWebUI(web_ui_)->GetPrefs();
   source->AddBoolean("video_capture_disallowed",
