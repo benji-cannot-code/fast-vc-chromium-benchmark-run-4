bdbeb407806cec539f23a522cfea5bcfed3baf46
Ella Ge <eirage@google.com>
2023-11-16 19:18:16 +0000

    [Gardener] Revert "[OOBE][CodeHealth] refactor DrivePinningScreen browser_test"
    This reverts commit 9ca1ce86f6b5d84d8c895e396396c21928eb0859.
    
    Reason for revert: Test failure on builder:
    https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/37236/overview
    
    Original change's description:
    > [OOBE][CodeHealth] refactor DrivePinningScreen browser_test
    >
    > Enhance DrivePinningScreenTest to leverage TestFuture's capabilities.
    >
    > Bug: b:310837130 , b:304436402
    > Change-Id: I8e786d45b1d929588bed859147285a67369295fd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029775
    > Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    > Reviewed-by: Osama Fathy <osamafathy@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1225472}
    
    Bug: b:310837130 , b:304436402
    Change-Id: Ia136155f045b3a294623752df9efa51b2fc592ee
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5038451
    Owners-Override: Ella Ge <eirage@google.com>
    Auto-Submit: Ella Ge <eirage@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1225644}

--- chrome/browser/ash/login/screens/drive_pinning_screen_browsertest.cc
+++ chrome/browser/ash/login/screens/drive_pinning_screen_browsertest.cc
@@ -72,8 +72,8 @@ class DrivePinningBaseScreenTest : public OobeBaseTest {
         WizardController::default_controller()->GetScreen<DrivePinningScreen>();
 
     original_callback_ = drive_pining_screen->get_exit_callback_for_testing();
-    drive_pining_screen->set_exit_callback_for_testing(
-        screen_result_waiter_.GetRepeatingCallback());
+    drive_pining_screen->set_exit_callback_for_testing(base::BindRepeating(
+        &DrivePinningBaseScreenTest::HandleScreenExit, base::Unretained(this)));
   }
 
   void SetPinningManagerProgress(Progress progress) {
@@ -93,19 +93,32 @@ class DrivePinningBaseScreenTest : public OobeBaseTest {
         DrivePinningScreenView::kScreenId);
   }
 
-  DrivePinningScreen::Result WaitForScreenExitResult() {
-    DrivePinningScreen::Result result = screen_result_waiter_.Take();
-    original_callback_.Run(result);
-    return result;
+  void WaitForScreenExit() {
+    if (result_.has_value()) {
+      return;
+    }
+    base::test::TestFuture<void> waiter;
+    quit_closure_ = waiter.GetCallback();
+    EXPECT_TRUE(waiter.Wait());
   }
+
+  DrivePinningScreen::ScreenExitCallback original_callback_;
+  absl::optional<DrivePinningScreen::Result> result_;
 
  protected:
   base::test::ScopedFeatureList feature_list_;
   LoginManagerMixin login_manager_mixin_{&mixin_host_};
 
  private:
-  base::test::TestFuture<DrivePinningScreen::Result> screen_result_waiter_;
-  DrivePinningScreen::ScreenExitCallback original_callback_;
+  void HandleScreenExit(DrivePinningScreen::Result result) {
+    result_ = result;
+    original_callback_.Run(result);
+    if (quit_closure_) {
+      std::move(quit_closure_).Run();
+    }
+  }
+
+  base::OnceClosure quit_closure_;
 };
 
 class DrivePinningScreenTest
@@ -132,11 +145,11 @@ IN_PROC_BROWSER_TEST_F(DrivePinningScreenTest, Accept) {
       kSpaceInformationPath);
   test::OobeJS().TapOnPath(kNextButtonPath);
 
-  DrivePinningScreen::Result result = WaitForScreenExitResult();
+  WaitForScreenExit();
 
   EXPECT_TRUE(ProfileManager::GetPrimaryUserProfile()->GetPrefs()->GetBoolean(
       prefs::kOobeDrivePinningEnabledDeferred));
-  EXPECT_EQ(result, DrivePinningScreen::Result::NEXT);
+  EXPECT_EQ(result_.value(), DrivePinningScreen::Result::NEXT);
 }
 
 IN_PROC_BROWSER_TEST_F(DrivePinningScreenTest, Decline) {
@@ -154,11 +167,11 @@ IN_PROC_BROWSER_TEST_F(DrivePinningScreenTest, Decline) {
   test::OobeJS().TapOnPath(kToggleButtonPath);
   test::OobeJS().TapOnPath(kNextButtonPath);
 
-  DrivePinningScreen::Result result = WaitForScreenExitResult();
+  WaitForScreenExit();
 
   EXPECT_FALSE(ProfileManager::GetPrimaryUserProfile()->GetPrefs()->GetBoolean(
       prefs::kOobeDrivePinningEnabledDeferred));
-  EXPECT_EQ(result, DrivePinningScreen::Result::NEXT);
+  EXPECT_EQ(result_.value(), DrivePinningScreen::Result::NEXT);
 }
 
 IN_PROC_BROWSER_TEST_P(DrivePinningScreenTest, ScreenSkippedOnError) {
@@ -166,20 +179,13 @@ IN_PROC_BROWSER_TEST_P(DrivePinningScreenTest, ScreenSkippedOnError) {
   current_progress.stage = GetParam();
 
   SetPinningManagerProgress(current_progress);
-  LoginDisplayHost::default_host()
-      ->GetWizardContextForTesting()
-      ->skip_choobe_for_tests = true;
+  ShowDrivePinningScreen();
 
-  login_manager_mixin_.LoginAsNewRegularUser();
-  OobeScreenExitWaiter(GetFirstSigninScreen()).Wait();
-
-  // All screens prior to the Drive Pinning screen are
-  // skipped on non-branded builds.
-  DrivePinningScreen::Result result = WaitForScreenExitResult();
+  WaitForScreenExit();
 
   EXPECT_FALSE(ProfileManager::GetPrimaryUserProfile()->GetPrefs()->GetBoolean(
       prefs::kOobeDrivePinningEnabledDeferred));
-  EXPECT_EQ(result, DrivePinningScreen::Result::NOT_APPLICABLE);
+  EXPECT_EQ(result_.value(), DrivePinningScreen::Result::NOT_APPLICABLE);
 }
 
 INSTANTIATE_TEST_SUITE_P(All,
@@ -291,9 +297,8 @@ IN_PROC_BROWSER_TEST_F(DrivePinningIntegrationServiceTest,
   test::OobeJS().ExpectVisiblePath(kDrivePinningDialoguePath);
   test::OobeJS().TapOnPath(kNextButtonPath);
 
-  DrivePinningScreen::Result result = WaitForScreenExitResult();
+  WaitForScreenExit();
 
-  EXPECT_EQ(result, DrivePinningScreen::Result::NEXT);
   histogram_tester.ExpectUniqueSample(
       "FileBrowser.GoogleDrive.BulkPinning.CHOOBEScreenInitializations", 1, 2);
 }
