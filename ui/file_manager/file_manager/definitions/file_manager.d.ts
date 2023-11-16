7b82264252763dcf9caf6dbe54738da012527428
François Degros <fdegros@chromium.org>
2023-11-16 06:21:48 +0000

    Files app: Convert error_counter to TypeScript
    BUG=b:289003444
    
    Change-Id: Ifccae7164207fa40dd7235f9a68b2a02f32b3251
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5033962
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Auto-Submit: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225333}

--- ui/file_manager/file_manager/definitions/file_manager.d.ts
+++ ui/file_manager/file_manager/definitions/file_manager.d.ts
@@ -35,6 +35,7 @@ declare global {
     appID: string;
     fileManager: FileManager;
     IN_TEST: boolean;
+    JSErrorCount: number;
     store: Store;
     /** Log action data in the console for debugging purpose. */
     DEBUG_STORE: boolean;
