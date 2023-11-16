a722b09b4b2a224f64689890b7ed60f00a423583
Avi Drissman <avi@chromium.org>
2023-11-16 01:15:41 +0000

    Switch bit counting to std functions in /media
    Switch users of CountLeadingZeroBits and CountTrailingZeroBits to use
    std:: functions in <bit>.
    
    Bug: 1414634
    Change-Id: I71e4020d3c6ff086948488edac2845dbe7e0983d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5034261
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1225254}

--- media/gpu/vaapi/av1_vaapi_video_encoder_delegate.cc
+++ media/gpu/vaapi/av1_vaapi_video_encoder_delegate.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/gpu/vaapi/av1_vaapi_video_encoder_delegate.h"
 
+#include <bit>
 #include <utility>
 
 #include "base/bits.h"
@@ -276,8 +277,7 @@ void DownscaleSegmentMap(const uint8_t* src_seg_map,
   // compute the log, since we know the segment size ratios of going to be
   // powers of two.
   const uint32_t log_seg_size_ratio =
-      base::bits::CountLeadingZeroBits(src_seg_size) -
-      base::bits::CountLeadingZeroBits(dst_seg_size);
+      std::countl_zero(src_seg_size) - std::countl_zero(dst_seg_size);
   const uint32_t src_width =
       base::bits::AlignUp(static_cast<uint32_t>(coded_size.width()),
                           src_seg_size) /
