f32629987a334e9f63c2559d5f0d0bcade26879e
Dmitry Vykochko <vykochko@google.com>
2023-11-16 12:33:39 +0000

    Move compose suggestion view creation from strategy to factory.
    This CL is a part the cell class elimination refactoring.
    
    Bug: 1491373
    Change-Id: Iac25834c2d9ff3dcd109fa333c04a3a9a8aa846d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5028328
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Cr-Commit-Position: refs/heads/main@{#1225437}

--- chrome/browser/ui/views/autofill/popup/popup_row_strategy_unittest.cc
+++ chrome/browser/ui/views/autofill/popup/popup_row_strategy_unittest.cc
@@ -32,7 +32,6 @@ namespace {
 
 enum class StrategyType {
   kSuggestion,
-  kComposeSuggestion,
 };
 
 struct RowStrategyTestdata {
@@ -58,11 +57,6 @@ const RowStrategyTestdata kTestcases[] = {
                            PopupItemId::kAutocompleteEntry},
         .line_number = 1,
         .strategy_type = StrategyType::kSuggestion,
-    },
-    RowStrategyTestdata{
-        .popup_item_ids = {PopupItemId::kCompose},
-        .line_number = 0,
-        .strategy_type = StrategyType::kComposeSuggestion,
     }};
 
 }  // namespace
@@ -89,9 +83,6 @@ class PopupRowStrategyTest : public ChromeViewsTestBase {
       case StrategyType::kSuggestion:
         return std::make_unique<PopupSuggestionStrategy>(
             controller().GetWeakPtr(), line_number);
-      case StrategyType::kComposeSuggestion:
-        return std::make_unique<PopupComposeSuggestionStrategy>(
-            controller().GetWeakPtr(), line_number, /*show_new_badge=*/false);
     }
   }
 
