0c1cd30b85685da0ce6b9702a8cf40447c807a8d
junwei <junwei.fu@intel.com>
2023-11-16 12:09:10 +0000

    [WebNN][CrOS]: Support Element-wise binary, softmax and reshape operators
    1. Element-wise binary mathematically equivalent to:
       <output_operand> = <lhs_operand> <operation_kind> <rhs_operand>;
    2. Softmax operator computes the softmax values of the 2-D input tensor
       along axis 1.
    3. Reshape operator alters the shape of input operand to the output.
    
    Bug: 1273291
    Change-Id: I62ec3dc664684cd5ef65734ee13a2c9a14dca3d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5003613
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    Commit-Queue: Junwei Fu <junwei.fu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1225429}

--- third_party/blink/renderer/modules/ml/webnn/ml_graph_test.cc
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_test.cc
@@ -79,8 +79,7 @@ struct ElementWiseBinaryTester {
 };
 
 TEST_P(MLGraphTest, ElementWiseBinaryTest) {
-  SKIP_TEST_ON_UNSUPPORTED_BACKEND(BackendType::kModelLoader);
-  V8TestingScope scope;
+  MLGraphV8TestingScope scope;
   {
     // Test element-wise add operator for two 0-D scalars.
     // The expected results should be the sum of the values of the two input
@@ -291,8 +290,7 @@ struct PowTester {
 };
 
 TEST_P(MLGraphTest, PowTest) {
-  SKIP_TEST_ON_UNSUPPORTED_BACKEND(BackendType::kModelLoader);
-  V8TestingScope scope;
+  MLGraphV8TestingScope scope;
   {
     // Test element-wise pow operator with exponent = 2.
     PowTester<float>{.lhs = {.type = V8MLOperandType::Enum::kFloat32,
@@ -1513,8 +1511,7 @@ struct ReshapeTester {
 };
 
 TEST_P(MLGraphTest, ReshapeTest) {
-  SKIP_TEST_ON_UNSUPPORTED_BACKEND(BackendType::kModelLoader);
-  V8TestingScope scope;
+  MLGraphV8TestingScope scope;
   {
     // Test reshaping 1-D 1-element tensor to 0-D scalar.
     ReshapeTester<float>{.input = {.type = V8MLOperandType::Enum::kFloat32,
