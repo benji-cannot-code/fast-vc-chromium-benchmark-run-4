4423628491445fcfb4808a331f7d73c3cfa2f46f
Takashi Sakamoto <tasak@google.com>
2023-11-16 00:31:31 +0000

    [PA] Make allocator_shim and allocator_core component().
    Bug: 1429450
    Change-Id: I31e96409e58e67be91e99a2cbe45c8a43269a248
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5028077
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1225234}

--- base/allocator/partition_allocator/src/partition_alloc/shim/allocator_shim_default_dispatch_to_partition_alloc.h
+++ base/allocator/partition_allocator/src/partition_alloc/shim/allocator_shim_default_dispatch_to_partition_alloc.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace allocator_shim::internal {
 
-class PA_COMPONENT_EXPORT(PARTITION_ALLOC) PartitionAllocMalloc {
+class PA_COMPONENT_EXPORT(ALLOCATOR_SHIM) PartitionAllocMalloc {
  public:
   // Returns true if ConfigurePartitions() has completed, meaning that the
   // allocators are effectively set in stone.
@@ -28,49 +28,49 @@ class PA_COMPONENT_EXPORT(PARTITION_ALLOC) PartitionAllocMalloc {
   static partition_alloc::PartitionRoot* AlignedAllocator();
 };
 
-PA_COMPONENT_EXPORT(PARTITION_ALLOC)
+PA_COMPONENT_EXPORT(ALLOCATOR_SHIM)
 void* PartitionMalloc(const AllocatorDispatch*, size_t size, void* context);
 
-PA_COMPONENT_EXPORT(PARTITION_ALLOC)
+PA_COMPONENT_EXPORT(ALLOCATOR_SHIM)
 void* PartitionMallocUnchecked(const AllocatorDispatch*,
                                size_t size,
                                void* context);
 
-PA_COMPONENT_EXPORT(PARTITION_ALLOC)
+PA_COMPONENT_EXPORT(ALLOCATOR_SHIM)
 void* PartitionCalloc(const AllocatorDispatch*,
                       size_t n,
                       size_t size,
                       void* context);
 
-PA_COMPONENT_EXPORT(PARTITION_ALLOC)
+PA_COMPONENT_EXPORT(ALLOCATOR_SHIM)
 void* PartitionMemalign(const AllocatorDispatch*,
                         size_t alignment,
                         size_t size,
                         void* context);
 
-PA_COMPONENT_EXPORT(PARTITION_ALLOC)
+PA_COMPONENT_EXPORT(ALLOCATOR_SHIM)
 void* PartitionAlignedAlloc(const AllocatorDispatch* dispatch,
                             size_t size,
                             size_t alignment,
                             void* context);
 
-PA_COMPONENT_EXPORT(PARTITION_ALLOC)
+PA_COMPONENT_EXPORT(ALLOCATOR_SHIM)
 void* PartitionAlignedRealloc(const AllocatorDispatch* dispatch,
                               void* address,
                               size_t size,
                               size_t alignment,
                               void* context);
 
-PA_COMPONENT_EXPORT(PARTITION_ALLOC)
+PA_COMPONENT_EXPORT(ALLOCATOR_SHIM)
 void* PartitionRealloc(const AllocatorDispatch*,
                        void* address,
                        size_t size,
                        void* context);
 
-PA_COMPONENT_EXPORT(PARTITION_ALLOC)
+PA_COMPONENT_EXPORT(ALLOCATOR_SHIM)
 void PartitionFree(const AllocatorDispatch*, void* object, void* context);
 
-PA_COMPONENT_EXPORT(PARTITION_ALLOC)
+PA_COMPONENT_EXPORT(ALLOCATOR_SHIM)
 size_t PartitionGetSizeEstimate(const AllocatorDispatch*,
                                 void* address,
                                 void* context);
---
+++ base/allocator/partition_allocator/src/partition_alloc/shim/empty.cc
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This empty.cc is built when use_allocator_shim is false to avoid
+// the situation: no object files are linked to build allocator_shim.dll.
+// If allocator_shim target's sources has only header files, no object
+// files will be generated and we will see the following error:
+// "lld_link:error: <root>: undefined symbol: _DllMainCRTStartup."
