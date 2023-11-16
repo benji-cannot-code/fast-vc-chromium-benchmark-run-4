a317b9597763cd86bfd426caa367f560b3ceff89
Stepan Khapugin <stkhapugin@chromium.org>
2023-11-16 15:48:51 +0000

    [Code Health][iOS] Move ios/c/b/metrics to model subfolder
    Prepares the move by creating dummy files for internal code to depend
    upon before the ios_internal files are updated.
    
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/<foo>/ will be moved to
    ios/c/b/<foo>/model/ to showcase that they are model files.
    
    Bug: 1319852
    Change-Id: I1207268998816702d86507f5495cfb8288b6d89d
    Low-Coverage-Reason: TRIVIAL_CHANGE Only moves files
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5036447
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225511}

---
+++ ios/chrome/browser/metrics/model/metrics_app_interface.h
@@ -0,0 +1,6 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/metrics/metrics_app_interface.h"
