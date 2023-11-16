1a7fff3cf16251a6796bce321ed37524d29f3406
David Pennington <dpenning@chromium.org>
2023-11-16 21:35:51 +0000

    [Mojo] Fix conversion js strings to/from mojo String16 for Unicode.
    - stringToMojoString16
    
      - this method was pulling the wrong character codes for multi code
    point characters, resulting in improper output
      - instead, iterate over each code point  to get the correct output.
    
    
    - mojoString16ToString
      - Instead of using to fromcodepoint, use fromCharCode since thats what
    mojo accepts,
    
    Tests are provided which convert emojis including graphemes back and
    forth.
    
    Change-Id: Iaa7bff70553a30f59772949aac4fb8ea31569ec1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5027656
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Auto-Submit: David Pennington <dpenning@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1225732}

--- chrome/test/data/webui/js/mojo_type_util_test.ts
+++ chrome/test/data/webui/js/mojo_type_util_test.ts
@@ -19,6 +19,19 @@ suite('MojoTypeUtilTest', () => {
     assertEquals(mojoString16ToString({data: [0x4f60, 0x597d]}), '你好');
   });
 
+  test('emojis', () => {
+    assertEquals('❤️', mojoString16ToString(stringToMojoString16('❤️')));
+    assertEquals(
+        '👨‍👨‍👦',
+        mojoString16ToString(stringToMojoString16('👨‍👨‍👦')));
+    assertEquals('🇯🇵', mojoString16ToString(stringToMojoString16('🇯🇵')));
+    assertEquals('🇺🇳', mojoString16ToString(stringToMojoString16('🇺🇳')));
+    assertEquals(
+        '👨‍👨‍👦🇯🇵👨‍👨‍👦a你❤️👨‍👨‍👦',
+        mojoString16ToString(stringToMojoString16(
+            '👨‍👨‍👦🇯🇵👨‍👨‍👦a你❤️👨‍👨‍👦')));
+  });
+
   test('Can convert strings to mojo Urls', () => {
     assertDeepEquals(stringToMojoUrl(''), {url: ''});
     assertDeepEquals(
