ece7da6dda0c858429bf4fd57aaf3971542d240b
André Kempe <andre.kempe@arm.com>
2023-11-16 10:28:29 +0000

    Implement integration tests for AllocationTraceRecorder
    This CL provides an integration test for the AllocationTraceRecorder.
    The test verifies that the correct user defined stream is added to the
    Crashpad report upon crashing and that the content of this stream is a
    Protocol Buffers stream as defined by AllocationTraceRecorder.
    
    Since the tests are included even if the recorder is disabled, the CL
    also contains fixes to existing errors.
    
    Change-Id: I07e0e7eada761486cc3cab3df6f5acf2eddefbae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4894116
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Richard Townsend <richard.townsend@arm.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Andre Kempe <andre.kempe@arm.com>
    Cr-Commit-Position: refs/heads/main@{#1225398}

--- components/crash/core/app/crashpad_handler_main.cc
+++ components/crash/core/app/crashpad_handler_main.cc
@@ -20,9 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(ENABLE_ALLOCATION_STACK_TRACE_RECORDER)
-#include "components/allocation_recorder/crash_handler/allocation_recorder_holder.h"
-#include "components/allocation_recorder/crash_handler/stream_data_source_factory.h"
-#include "components/allocation_recorder/crash_handler/user_stream_data_source.h"
+#include "components/allocation_recorder/crash_handler/allocation_recorder_holder.h"  // nogncheck
+#include "components/allocation_recorder/crash_handler/stream_data_source_factory.h"  // nogncheck
+#include "components/allocation_recorder/crash_handler/user_stream_data_source.h"  // nogncheck
 #endif
 
 extern "C" {
