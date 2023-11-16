91e4a88e3a804f5044c99c9cfba35926d2e2b1f4
Hira Mahmood <hiramahmood@google.com>
2023-11-16 20:45:55 +0000

    [iOS] Fix font size and spacing for Parcel Tracking Magic Stack module
    This CL fixes the title font size and the status bar bottom margin
    for the parcel tracking Magic Stack module.
    
    Before: https://screenshot.googleplex.com/7oxENkS2JkG5LP9
    
    After: https://screenshot.googleplex.com/8S8taUGr8QinKR7
    
    Bug: 1473449
    Change-Id: I3093e8237e7ed9e32851682cfc91ab473a0a2741
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5037234
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1225693}

--- ios/chrome/browser/ui/content_suggestions/cells/parcel_tracking_view.mm
+++ ios/chrome/browser/ui/content_suggestions/cells/parcel_tracking_view.mm
@@ -47,6 +47,7 @@ const CGFloat kStatusBarViewSpacing = 6.0f;
 const CGFloat kStatusBarWidth = 61.0f;
 const CGFloat kStatusBarHeight = 6.0f;
 const CGFloat kStatusBarCornerRadius = 3.0f;
+const CGFloat kStatusBarMarginFromBottom = 5.0f;
 
 }  // namespace
 
@@ -156,7 +157,7 @@ const CGFloat kStatusBarCornerRadius = 3.0f;
   if (previousTraitCollection.preferredContentSizeCategory !=
       self.traitCollection.preferredContentSizeCategory) {
     _titleLabel.font =
-        CreateDynamicFont(UIFontTextStyleBody, UIFontWeightSemibold);
+        CreateDynamicFont(UIFontTextStyleFootnote, UIFontWeightSemibold);
   }
 }
 
@@ -164,7 +165,7 @@ const CGFloat kStatusBarCornerRadius = 3.0f;
   _titleLabel = [[UILabel alloc] init];
   _titleLabel.isAccessibilityElement = NO;
   _titleLabel.font =
-      CreateDynamicFont(UIFontTextStyleBody, UIFontWeightSemibold);
+      CreateDynamicFont(UIFontTextStyleFootnote, UIFontWeightSemibold);
   _titleLabel.adjustsFontForContentSizeCategory = YES;
   _titleLabel.numberOfLines = 2;
   _titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
@@ -235,8 +236,11 @@ const CGFloat kStatusBarCornerRadius = 3.0f;
     [_imageContainer.widthAnchor constraintEqualToConstant:kIconContainerWidth],
     [_imageContainer.heightAnchor
         constraintEqualToAnchor:_imageContainer.widthAnchor],
-    [rightVerticalStackView.heightAnchor
-        constraintGreaterThanOrEqualToConstant:kIconContainerWidth],
+    [rightVerticalStackView.bottomAnchor
+        constraintEqualToAnchor:_imageContainer.bottomAnchor
+                       constant:-kStatusBarMarginFromBottom],
+    [rightVerticalStackView.topAnchor
+        constraintEqualToAnchor:_imageContainer.topAnchor],
     [horizontalStackView.topAnchor constraintEqualToAnchor:self.topAnchor],
     [horizontalStackView.bottomAnchor
         constraintEqualToAnchor:self.bottomAnchor],
