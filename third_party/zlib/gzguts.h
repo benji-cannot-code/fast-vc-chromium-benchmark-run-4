2424714082cc19b0447f7fee6bcf0a0385ffc1d4
Adenilson Cavalcanti <cavalcantii@chromium.org>
2023-11-16 20:29:38 +0000

    [zlib] Sync with zlib 1.2.13.1, patch 8 of N
    Ported:
     - Avoid compiler complaints if _TIME_BITS defined when building zlib.
    
    See: https://github.com/madler/zlib/commit/a566e156b3fa07b566ddbf6801b517a9dba04fa3
    
    Bug: 1447443
    Change-Id: I951ee420c28ccffb0f1aa6b10495c7d381275f02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5035139
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225685}

--- third_party/zlib/gzguts.h
+++ third_party/zlib/gzguts.h
@@ -8,9 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #  ifndef _LARGEFILE_SOURCE
 #    define _LARGEFILE_SOURCE 1
 #  endif
-#  ifdef _FILE_OFFSET_BITS
-#    undef _FILE_OFFSET_BITS
-#  endif
+#  undef _FILE_OFFSET_BITS
+#  undef _TIME_BITS
 #endif
 
 #ifdef HAVE_HIDDEN
