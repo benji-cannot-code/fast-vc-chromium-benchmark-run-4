073bda84c4fe0844bee3ba220feb83ebf6bd717b
Li Lin <llin@chromium.org>
2023-11-17 21:38:24 +0000

    Growth Framework: Add load complete callback for loading campaigns.
    The callback will allow getting the load complete signal without
    observing the CampaignManager which make coordinating with loading
    other component easier.
    
    The callback will used in follow up a CL to coordinate with loading
    demo mode component.
    
    Bug: b/298467444
    Change-Id: Idf7f60f3fe204a87860040d6c0ea421498053a33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5041592
    Reviewed-by: Yilkal Abe <yilkal@chromium.org>
    Commit-Queue: Li Lin <llin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226350}

--- chromeos/ash/components/growth/campaigns_manager_unittest.cc
+++ chromeos/ash/components/growth/campaigns_manager_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/task_environment.h"
+#include "base/test/test_future.h"
 #include "base/time/time.h"
 #include "base/version.h"
 #include "chromeos/ash/components/growth/campaigns_model.h"
@@ -140,7 +141,9 @@ class CampaignsManagerTest : public testing::Test {
         .WillOnce(InvokeCallbackArgument<0, CampaignComponentLoadedCallback>(
             temp_dir_.GetPath()));
 
-    campaigns_manager_->LoadCampaigns();
+    base::test::TestFuture<void> load_completed_waiter;
+    campaigns_manager_->LoadCampaigns(load_completed_waiter.GetCallback());
+    ASSERT_TRUE(load_completed_waiter.Wait());
     observer.Wait();
 
     ASSERT_TRUE(observer.load_completed());
@@ -586,7 +589,7 @@ TEST_F(CampaignsManagerTest, LoadCampaignsFailed) {
       .WillOnce(InvokeCallbackArgument<0, CampaignComponentLoadedCallback>(
           absl::nullopt));
 
-  campaigns_manager_->LoadCampaigns();
+  campaigns_manager_->LoadCampaigns(base::DoNothing());
   observer.Wait();
 
   ASSERT_TRUE(observer.load_completed());
@@ -610,7 +613,7 @@ TEST_F(CampaignsManagerTest, LoadCampaignsNoFile) {
       .WillOnce(InvokeCallbackArgument<0, CampaignComponentLoadedCallback>(
           temp_dir_.GetPath()));
 
-  campaigns_manager_->LoadCampaigns();
+  campaigns_manager_->LoadCampaigns(base::DoNothing());
   observer.Wait();
 
   ASSERT_TRUE(observer.load_completed());
@@ -625,22 +628,8 @@ TEST_F(CampaignsManagerTest, LoadCampaignsNoFile) {
 
 TEST_F(CampaignsManagerTest, LoadCampaignsInvalidFile) {
   base::HistogramTester histogram_tester;
-  TestCampaignsManagerObserver observer;
-  campaigns_manager_->AddObserver(&observer);
 
-  ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
-  base::FilePath campaigns_file(temp_dir_.GetPath().Append(kCampaignsFileName));
-
-  base::WriteFile(campaigns_file, "abc");
-
-  EXPECT_CALL(mock_client_, LoadCampaignsComponent(_))
-      .WillOnce(InvokeCallbackArgument<0, CampaignComponentLoadedCallback>(
-          temp_dir_.GetPath()));
-
-  campaigns_manager_->LoadCampaigns();
-  observer.Wait();
-
-  ASSERT_TRUE(observer.load_completed());
+  LoadComponentAndVerifyLoadComplete("abc");
 
   ASSERT_EQ(nullptr, campaigns_manager_->GetCampaignBySlot(Slot::kDemoModeApp));
 
@@ -651,22 +640,7 @@ TEST_F(CampaignsManagerTest, LoadCampaignsInvalidFile) {
 }
 
 TEST_F(CampaignsManagerTest, LoadCampaignsEmptyFile) {
-  TestCampaignsManagerObserver observer;
-  campaigns_manager_->AddObserver(&observer);
-
-  ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
-  base::FilePath campaigns_file(temp_dir_.GetPath().Append(kCampaignsFileName));
-
-  base::WriteFile(campaigns_file, "");
-
-  EXPECT_CALL(mock_client_, LoadCampaignsComponent(_))
-      .WillOnce(InvokeCallbackArgument<0, CampaignComponentLoadedCallback>(
-          campaigns_file));
-
-  campaigns_manager_->LoadCampaigns();
-  observer.Wait();
-
-  ASSERT_TRUE(observer.load_completed());
+  LoadComponentAndVerifyLoadComplete("");
 
   ASSERT_EQ(nullptr, campaigns_manager_->GetCampaignBySlot(Slot::kDemoModeApp));
 }
