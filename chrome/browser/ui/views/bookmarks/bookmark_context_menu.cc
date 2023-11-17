4b84825996ff1518610fca8b3843693c88593eca
Jan Keitel <jkeitel@google.com>
2023-11-17 07:24:08 +0000

    Use MenuRunner constructor with unique_ptr in bookmarks.
    This makes it explicit that MenuRunner takes ownership of the
    MenuItemView passed to it in its constructor.
    
    As a side task, this CL cleans up a couple of dangling pointers.
    
    Bug: 1501294
    Change-Id: I9b06cc3aa83d4d663a8852bb151bad7de6e51e0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5034920
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225948}

--- chrome/browser/ui/views/bookmarks/bookmark_context_menu.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_context_menu.cc
@@ -5,8 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/bookmarks/bookmark_context_menu.h"
 
+#include <memory>
+
 #include "base/command_line.h"
 #include "base/i18n/rtl.h"
+#include "base/memory/ptr_util.h"
 #include "base/no_destructor.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/app/chrome_command_ids.h"
@@ -55,12 +58,11 @@ BookmarkContextMenu::BookmarkContextMenu(
           selection)),
       parent_widget_(parent_widget),
       menu_(new views::MenuItemView(this)),
-      menu_runner_(new views::MenuRunner(menu_,
-                                         views::MenuRunner::HAS_MNEMONICS |
-                                             views::MenuRunner::IS_NESTED |
-                                             views::MenuRunner::CONTEXT_MENU)),
-      observer_(nullptr),
       close_on_remove_(close_on_remove) {
+  menu_runner_ = std::make_unique<views::MenuRunner>(
+      base::WrapUnique<views::MenuItemView>(menu_),
+      views::MenuRunner::HAS_MNEMONICS | views::MenuRunner::IS_NESTED |
+          views::MenuRunner::CONTEXT_MENU);
   ui::SimpleMenuModel* menu_model = controller_->menu_model();
   for (size_t i = 0; i < menu_model->GetItemCount(); ++i) {
     views::MenuModelAdapter::AppendMenuItemFromModel(
--- chrome/browser/ui/views/bookmarks/bookmark_menu_controller_views.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_menu_controller_views.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/bookmarks/bookmark_menu_controller_views.h"
 
+#include "base/memory/ptr_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/ui/bookmarks/bookmark_stats.h"
 #include "chrome/browser/ui/views/bookmarks/bookmark_bar_view.h"
@@ -40,8 +41,8 @@ BookmarkMenuController::BookmarkMenuController(Browser* browser,
   int run_type = 0;
   if (for_drop)
     run_type |= views::MenuRunner::FOR_DROP;
-  menu_runner_ =
-      std::make_unique<views::MenuRunner>(menu_delegate_->menu(), run_type);
+  menu_runner_ = std::make_unique<views::MenuRunner>(
+      base::WrapUnique<MenuItemView>(menu_delegate_->menu()), run_type);
 }
 
 void BookmarkMenuController::RunMenuAt(BookmarkBarView* bookmark_bar) {
