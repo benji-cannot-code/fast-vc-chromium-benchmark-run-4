1a8cb01288c0d477152db39b040d31562ae5b307
Jan Keitel <jkeitel@google.com>
2023-11-17 07:41:03 +0000

    Use modern MenuRunner constructor in Menu browsertests.
    On top of that, clean up some dangling pointers in tests.
    
    Bug: 1501294
    Change-Id: I94796ef37782cbfadf701a44ca0652ef3dd6db5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5033108
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1225949}

--- chrome/browser/ui/views/menu_test_base.cc
+++ chrome/browser/ui/views/menu_test_base.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/menu_test_base.h"
 
+#include <utility>
+
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/test/base/interactive_test_utils.h"
 #include "chrome/test/base/ui_test_utils.h"
@@ -55,13 +57,16 @@ void MenuTestBase::SetUp() {
 
   views::test::DisableMenuClosureAnimations();
 
-  menu_ = new views::MenuItemView(this);
+  auto menu_owning = std::make_unique<views::MenuItemView>(/*delegate=*/this);
+  menu_ = menu_owning.get();
   BuildMenu(menu_);
-  menu_runner_ =
-      std::make_unique<views::MenuRunner>(menu_, GetMenuRunnerFlags());
+  menu_runner_ = std::make_unique<views::MenuRunner>(std::move(menu_owning),
+                                                     GetMenuRunnerFlags());
 }
 
 void MenuTestBase::TearDown() {
+  button_ = nullptr;
+  menu_ = nullptr;
   // We cancel the menu first because certain operations (like a menu opened
   // with views::MenuRunner::FOR_DROP) don't take kindly to simply pulling the
   // runner out from under them.
