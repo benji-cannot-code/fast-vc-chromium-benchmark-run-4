79a751ae6ea303040342ab2662ab51b90a79ae16
Mark Polner <polner@google.com>
2023-11-17 00:38:53 +0000

    [Nearby Presence] Add credential save success metrics.
    Added metrics to report the success of each save operation in
    NearbyPresenceCredentialStorage.
    
    Bug: b/287334365
    Test: Added unit tests.
    Change-Id: I619329adc5020fa6959a2de3ffd0f038b0e2d9e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5008508
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Mark Polner <polner@google.com>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#1225834}

--- chrome/browser/ash/nearby/presence/credential_storage/metrics/credential_storage_metrics.h
+++ chrome/browser/ash/nearby/presence/credential_storage/metrics/credential_storage_metrics.h
@@ -9,6 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash::nearby::presence::metrics {
 
 void RecordCredentialStorageInitializationResult(bool success);
+void RecordCredentialStorageSaveLocalPublicCredentialsResult(bool success);
+void RecordCredentialStorageSaveRemotePublicCredentialsResult(bool success);
+void RecordCredentialStorageSavePrivateCredentialsResult(bool success);
 
 }  // namespace ash::nearby::presence::metrics
 
--- chrome/browser/ash/nearby/presence/credential_storage/nearby_presence_credential_storage_unittest.cc
+++ chrome/browser/ash/nearby/presence/credential_storage/nearby_presence_credential_storage_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/nearby/presence/credential_storage/nearby_presence_credential_storage.h"
 #include "base/memory/raw_ptr.h"
 #include "base/test/bind.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/task_environment.h"
 #include "chromeos/ash/services/nearby/public/mojom/nearby_presence.mojom.h"
 #include "components/leveldb_proto/testing/fake_db.h"
@@ -363,6 +364,8 @@ class NearbyPresenceCredentialStorageTest : public testing::Test {
       local_public_db_;
   raw_ptr<leveldb_proto::test::FakeDB<::nearby::internal::SharedCredential>>
       remote_public_db_;
+
+  base::HistogramTester histogram_tester_;
 };
 
 TEST_F(NearbyPresenceCredentialStorageTest, InitializeDatabases_Successful) {
@@ -480,6 +483,13 @@ TEST_F(NearbyPresenceCredentialStorageTest, SaveCredentials_Local_Success) {
 
   ASSERT_EQ(3u, local_public_db_entries_.size());
   ASSERT_EQ(3u, private_db_entries_.size());
+
+  histogram_tester_.ExpectUniqueSample(
+      "Nearby.Presence.Credentials.Storage.SaveLocalPublicCredentials.Result",
+      /*bucket: success=*/true, 1);
+  histogram_tester_.ExpectUniqueSample(
+      "Nearby.Presence.Credentials.Storage.SavePrivateCredentials.Result",
+      /*bucket: success=*/true, 1);
 }
 
 TEST_F(NearbyPresenceCredentialStorageTest, SaveCredentials_Local_PublicFails) {
@@ -518,6 +528,15 @@ TEST_F(NearbyPresenceCredentialStorageTest, SaveCredentials_Local_PublicFails) {
 
     run_loop.Run();
   }
+
+  // Private credentials should not have an entry, as a save attempt did
+  // not take place.
+  histogram_tester_.ExpectUniqueSample(
+      "Nearby.Presence.Credentials.Storage.SaveLocalPublicCredentials.Result",
+      /*bucket: success=*/false, 1);
+  histogram_tester_.ExpectTotalCount(
+      "Nearby.Presence.Credentials.Storage.SavePrivateCredentials.Result",
+      /*count=*/0);
 }
 
 TEST_F(NearbyPresenceCredentialStorageTest,
@@ -556,6 +575,13 @@ TEST_F(NearbyPresenceCredentialStorageTest,
 
     run_loop.Run();
   }
+
+  histogram_tester_.ExpectUniqueSample(
+      "Nearby.Presence.Credentials.Storage.SaveLocalPublicCredentials.Result",
+      /*bucket: success=*/true, 1);
+  histogram_tester_.ExpectUniqueSample(
+      "Nearby.Presence.Credentials.Storage.SavePrivateCredentials.Result",
+      /*bucket: success=*/false, 1);
 }
 
 TEST_F(NearbyPresenceCredentialStorageTest, SaveCredentials_Remote_Success) {
@@ -590,6 +616,10 @@ TEST_F(NearbyPresenceCredentialStorageTest, SaveCredentials_Remote_Success) {
   }
 
   ASSERT_EQ(1u, remote_public_db_entries_.size());
+
+  histogram_tester_.ExpectUniqueSample(
+      "Nearby.Presence.Credentials.Storage.SaveRemotePublicCredentials.Result",
+      /*bucket: success=*/true, 1);
 }
 
 TEST_F(NearbyPresenceCredentialStorageTest,
@@ -619,6 +649,10 @@ TEST_F(NearbyPresenceCredentialStorageTest,
 
     run_loop.Run();
   }
+
+  histogram_tester_.ExpectUniqueSample(
+      "Nearby.Presence.Credentials.Storage.SaveRemotePublicCredentials.Result",
+      /*bucket: success=*/false, 1);
 }
 
 TEST_F(NearbyPresenceCredentialStorageTest,
