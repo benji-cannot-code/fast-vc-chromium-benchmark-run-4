8aaf6659dc3dcfe7d83e2450b16ba661eabc94d2
Dan Sanders <sandersd@chromium.org>
2023-11-17 20:36:43 +0000

    [media] VTVD: Require hardware decoding support on Apple Silicon
    Apple Silicon devices appear to be able to hardware decode any
    resolution. Intel devices have a cutoff for H.264 around 480p, which
    breaks some tests.
    
    Bug: 1331597
    Change-Id: I387b32f5385e7d00cf521936e4272fa9f981464b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5038123
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226315}

--- media/gpu/mac/video_toolbox_h264_accelerator.cc
+++ media/gpu/mac/video_toolbox_h264_accelerator.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/sys_byteorder.h"
+#include "build/build_config.h"
 #include "media/base/media_log.h"
 
 namespace media {
@@ -210,7 +211,13 @@ VideoToolboxH264Accelerator::Status VideoToolboxH264Accelerator::SubmitDecode(
   }
 
   VideoToolboxDecompressionSessionMetadata session_metadata = {
+#if defined(ARCH_CPU_X86_FAMILY)
+      // Allow software decoding on Intel hardware where the cutoff is around
+      // 480p and breaks tests.
+      /*allow_software_decoding=*/true,
+#else
       /*allow_software_decoding=*/false,
+#endif  // defined(ARCH_CPU_X86_FAMILY)
       /*is_hbd=*/false,
       /*has_alpha=*/false,
       /*visible_rect=*/pic->visible_rect()};
