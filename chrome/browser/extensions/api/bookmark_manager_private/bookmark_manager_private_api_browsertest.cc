0d8fabbf803313cdbc16cdc4101e5127430ad7ae
Jagadesh P <jagadeshjai1999@gmail.com>
2023-11-17 01:55:08 +0000

    [DanglingPointer] Remove dangling pointer in bookmark_manager_private_api_browsertest.cc
    Bug: 1291138
    Change-Id: I7db8b4d5e275e69f670ab84e254d9ba337b9535b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018665
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Auto-Submit: Jagadesh Jai <jagadeshjai1999@gmail.com>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225869}

--- chrome/browser/extensions/api/bookmark_manager_private/bookmark_manager_private_api_browsertest.cc
+++ chrome/browser/extensions/api/bookmark_manager_private/bookmark_manager_private_api_browsertest.cc
@@ -30,6 +30,11 @@ class BookmarkManagerPrivateApiBrowsertest : public InProcessBrowserTest {
     model_ = WaitForBookmarkModel();
   }
 
+  void TearDownOnMainThread() override {
+    model_ = nullptr;
+    InProcessBrowserTest::TearDownOnMainThread();
+  }
+
   BookmarkModel* model() { return model_; }
 
  private:
@@ -40,7 +45,7 @@ class BookmarkManagerPrivateApiBrowsertest : public InProcessBrowserTest {
     return model;
   }
 
-  raw_ptr<BookmarkModel, DanglingUntriaged> model_;
+  raw_ptr<BookmarkModel> model_ = nullptr;
 };
 
 IN_PROC_BROWSER_TEST_F(BookmarkManagerPrivateApiBrowsertest,
