5f6b9c24a838dea94bf9205e09ecd2c6a064b4fd
John Abd-El-Malek <jam@chromium.org>
2023-11-17 21:59:48 +0000

    Update code to refer to new location of ml repo
    Bug: b/301661843
    Change-Id: Ib494d86653a267d3b8e957ae39e4d4c0c088e58a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5037755
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Owners-Override: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226366}

--- services/on_device_model/chrome_ml_instance.h
+++ services/on_device_model/chrome_ml_instance.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_ON_DEVICE_MODEL_CHROME_ML_INSTANCE_H_
 #define SERVICES_ON_DEVICE_MODEL_CHROME_ML_INSTANCE_H_
 
-#include "third_party/ml/public/chrome_ml.h"
+#include "components/optimization_guide/internal/public/chrome_ml.h"
 
 namespace on_device_model {
 
