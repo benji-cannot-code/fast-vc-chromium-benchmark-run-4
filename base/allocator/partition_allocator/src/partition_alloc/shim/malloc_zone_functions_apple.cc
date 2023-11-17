becd8ce7987a5c8edc1733b6ffc3e77ab4eb8353
Stephan Hartmann <stha09@googlemail.com>
2023-11-17 16:53:30 +0000

    libstdc++: replace deprecated std::is_pod<T> in //base
    std::is_pod is deprecated since C++20. Replace with std::trivial and
    std::is_standard_layout. Avoids a lot of warnings.
    
    Bug: 957519
    Change-Id: Idc06ac91a4363bb61b71f1e3b3646422856b25d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994566
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#1226170}

--- base/allocator/partition_allocator/src/partition_alloc/shim/malloc_zone_functions_apple.cc
+++ base/allocator/partition_allocator/src/partition_alloc/shim/malloc_zone_functions_apple.cc
@@ -14,7 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace allocator_shim {
 
 MallocZoneFunctions g_malloc_zones[kMaxZoneCount];
-static_assert(std::is_pod_v<MallocZoneFunctions>,
+static_assert(std::is_trivial_v<MallocZoneFunctions> &&
+                  std::is_standard_layout_v<MallocZoneFunctions>,
               "MallocZoneFunctions must be POD");
 
 void StoreZoneFunctions(const ChromeMallocZone* zone,
