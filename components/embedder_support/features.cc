07c17cfd1da70f2dfe32860a813af7c5cb20bc86
Yusuke Sato <yusukes@google.com>
2023-11-17 21:55:51 +0000

    Add a new form-factor client hints value for XR
    Use "XR" as the hint when --enable-features=ClientHintsXRFormFactor
    is passed to Chrome. This makes the client hints implementation even
    closer to the draft at https://wicg.github.io/ua-client-hints/.
    
    BUG=1442238
    TEST=components_unittests
    TEST=Launch Clank with --enable-features=ClientHintsFormFactor,ClientHintsXRFormFactor.
     Inspect Clank in a desktop Chrome and run
     `navigator.userAgentData.getHighEntropyValues(["formFactor"])
     .then(function(v) { console.log(v); })` in the console. Verify that
     form_factor is set to {"Mobile","XR"}.
    TEST=Do the same test with the "Desktop site" menu checked.
    
    Change-Id: Id07af43e18ba31959f54decc5ae0eaadd462b606
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4985586
    Reviewed-by: Victor Tan <victortan@chromium.org>
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Dustin Mitchell <djmitche@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226359}

---
+++ components/embedder_support/features.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/embedder_support/features.h"
+
+#include "base/feature_list.h"
+
+namespace embedder_support {
+
+BASE_FEATURE(kClientHintsXRFormFactor,
+             "ClientHintsXRFormFactor",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+}  // namespace embedder_support
---
+++ components/embedder_support/features.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_EMBEDDER_SUPPORT_FEATURES_H_
+#define COMPONENTS_EMBEDDER_SUPPORT_FEATURES_H_
+
+#include "base/feature_list.h"
+
+namespace embedder_support {
+
+// Sets "XR" in the form factor filed of client hints.
+BASE_DECLARE_FEATURE(kClientHintsXRFormFactor);
+
+}  // namespace embedder_support
+
+#endif  // COMPONENTS_EMBEDDER_SUPPORT_FEATURES_H_
