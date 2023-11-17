8109f95e942a3b708d4b6583e918ba8ad45f9faa
Louis Romero <lpromero@google.com>
2023-11-17 09:36:42 +0000

    Update TopAlignedImageView to clip the bottom instead of pillarbox
    Prior to this CL, when a landscape image was displayed, its height
    matched the view height, and its width was smaller than the view's
    width, ending up with a pillarbox effect (vertical bands on each side of
    the image).
    With this CL, preparing the way for landscape grid cells, the image is
    now always matching the image width, even if that means clipping the
    bottom or adding an horizontal band at the bottom.
    
    For example, with the upcoming new layout, we will get this:
    https://screenshot.googleplex.com/96o97eEY2AJsF8q
    instead of this:
    https://screenshot.googleplex.com/Aq5tG7RjioQSqty (notice the
    vertical white bands on the sides of the screenshots.)
    
    Bug: 1485895
    Change-Id: I3b71a8977bce6376dc44cac467db25633e05efa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5033729
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1225980}

--- ios/chrome/browser/shared/ui/elements/top_aligned_image_view.h
+++ ios/chrome/browser/shared/ui/elements/top_aligned_image_view.h
@@ -9,17 +9,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 
 // The standard UIImageView zooms to the center of the image when it aspect
-// fills. TopAlignedImageView aligns to the top of the image in all cases.
-// When the image is portrait, the image matches the width of the view in all
-// cases. When the image is landscape, the image matches the height of the view
-// in all cases.
+// fills. TopAlignedImageView aligns to the top and width of the image in all
+// cases.
 @interface TopAlignedImageView : UIView
+
 // The image displayed in the image view.
 @property(nonatomic) UIImage* image;
 
 - (instancetype)init NS_DESIGNATED_INITIALIZER;
 - (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
 - (instancetype)initWithCoder:(NSCoder*)aDecoder NS_UNAVAILABLE;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_SHARED_UI_ELEMENTS_TOP_ALIGNED_IMAGE_VIEW_H_
