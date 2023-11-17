f8d084c15df969b713f57582c5f5b81a9d4639c9
Tarun Bansal <tbansal@google.com>
2023-11-17 07:13:28 +0000

    Add metrics around visibility and enable state of features.
    Bug: 1497713
    Change-Id: I1afa8bd516fa4c0a1b8d707477ccbd81eb12b613
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5032373
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225946}

--- components/optimization_guide/core/optimization_guide_util.h
+++ components/optimization_guide/core/optimization_guide_util.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/optimization_guide_permissions_util.h"
 #include "components/optimization_guide/core/optimization_guide_switches.h"
 #include "components/optimization_guide/proto/common_types.pb.h"
+#include "components/optimization_guide/proto/model_execution.pb.h"
 #include "components/optimization_guide/proto/models.pb.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
@@ -39,6 +40,11 @@ struct ResourceRequest;
 namespace optimization_guide {
 
 enum class OptimizationGuideDecision;
+
+// Returns the equivalent string name for a `feature`. The returned string can
+// be used to index persistent data (e.g., prefs, histograms etc.).
+std::string_view GetStringNameForModelExecutionFeature(
+    proto::ModelExecutionFeature feature);
 
 // Returns false if the host is an IP address, localhosts, or an invalid
 // host that is not supported by the remote optimization guide.
