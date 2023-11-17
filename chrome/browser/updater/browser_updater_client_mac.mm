2a600bb26a508b8ab7be5796c129a9f545847409
Joshua Pawlicki <waffles@chromium.org>
2023-11-17 18:38:24 +0000

    Installer: Remove .want_full_install file.
    1 - Remove writing the file during the install. The install still
        updates the tag using ksadmin.
    2 - In cases where Chrome registers with an updater via keystone_glue,
        assume a full update is always needed. This code is moribund and
        will only be used if we flip kUseChromiumUpdater back to false,
        which would be an emergency circumstance in any event due to other
        issues with Keystone, and very unlikely to happen in M121.
    3 - In cases where Chrome registers with an updater via
        browser_updater_client, do not look for a .want_full_install file.
        In practice, this happens only if Chrome is not currently
        registered with the updater. If .want_full_install exists, Chrome
        should be registered (since the updater must have run
        .keystone_install at some point and a diff update failed), but if
        this ever happens, it is acceptable to take one additional diff
        update.
    
    Fixed: 1503126
    Change-Id: I33dd4f567bc6ac9090b705136d6e68d2863051cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5037535
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226237}

--- chrome/browser/updater/browser_updater_client_mac.mm
+++ chrome/browser/updater/browser_updater_client_mac.mm
@@ -9,24 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/apple/bundle_locations.h"
 #include "base/apple/foundation_util.h"
-#include "base/strings/strcat.h"
 #include "chrome/browser/google/google_brand.h"
 #include "chrome/browser/updater/browser_updater_client_util.h"
 #include "chrome/common/channel_info.h"
 #include "components/version_info/version_info.h"
-
-namespace {
-
-std::string GetTag() {
-  std::string contents;
-  base::ReadFileToString(
-      base::apple::OuterBundlePath().Append(".want_full_installer"), &contents);
-  return base::StrCat(
-      {chrome::GetChannelName(chrome::WithExtendedStable(true)),
-       contents == version_info::GetVersionNumber() ? "-full" : ""});
-}
-
-}  // namespace
 
 std::string BrowserUpdaterClient::GetAppId() {
   return base::apple::BaseBundleID();
@@ -37,7 +23,7 @@ updater::RegistrationRequest BrowserUpdaterClient::GetRegistrationRequest() {
   req.app_id = GetAppId();
   google_brand::GetBrand(&req.brand_code);
   req.version = base::Version(version_info::GetVersionNumber());
-  req.ap = GetTag();
+  req.ap = chrome::GetChannelName(chrome::WithExtendedStable(true));
   req.existence_checker_path = base::apple::OuterBundlePath();
   return req;
 }
