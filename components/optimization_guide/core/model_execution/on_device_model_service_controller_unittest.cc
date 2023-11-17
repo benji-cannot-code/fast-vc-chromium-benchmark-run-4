57aa663909a6ef478311775d6e71870982ee0f56
Sophie Chang <sophiechang@chromium.org>
2023-11-17 23:21:53 +0000

    Merge context proto with execute proto for on-device session
    Bug: b:311752409
    Change-Id: I1aec8a518d6e3d73e910c1fe4795ea53cff2ebd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5041603
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Auto-Submit: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226407}

--- components/optimization_guide/core/model_execution/on_device_model_service_controller_unittest.cc
+++ components/optimization_guide/core/model_execution/on_device_model_service_controller_unittest.cc
@@ -162,12 +162,17 @@ class OnDeviceModelServiceControllerTest : public testing::Test {
 
     // Execute call prefixes with execute:.
     auto& substitution = *input_config.add_execute_substitutions();
-    substitution.set_string_template("execute:%s");
+    substitution.set_string_template("execute:%s%s");
     substitution.add_substitutions()
         ->add_candidates()
         ->mutable_proto_field()
         ->add_proto_descriptors()
         ->set_tag_number(2);
+    auto* proto_field = substitution.add_substitutions()
+                            ->add_candidates()
+                            ->mutable_proto_field();
+    proto_field->add_proto_descriptors()->set_tag_number(3);
+    proto_field->add_proto_descriptors()->set_tag_number(1);
 
     // Context call prefixes with context:.
     auto& context_substitution =
@@ -203,7 +208,7 @@ class OnDeviceModelServiceControllerTest : public testing::Test {
   void ExecuteModel(OptimizationGuideModelExecutor::Session& session,
                     std::string_view input) {
     proto::ComposeRequest request;
-    request.set_user_input(std::string(input));
+    request.mutable_page_metadata()->set_page_url(std::string(input));
     session.ExecuteModel(
         request,
         base::BindRepeating(&OnDeviceModelServiceControllerTest::OnResponse,
@@ -258,7 +263,7 @@ TEST_F(OnDeviceModelServiceControllerTest, ModelExecutionWithContext) {
   EXPECT_TRUE(response_received_);
   const std::vector<std::string> expected_responses = ConcatResponses({
       "Context: ctx:bar off:0 max:10\n",
-      "Input: execute:baz\n",
+      "Input: execute:barbaz\n",
   });
   EXPECT_EQ(*response_received_, expected_responses.back());
   EXPECT_THAT(streamed_responses_, ElementsAreArray(expected_responses));
@@ -278,7 +283,7 @@ TEST_F(OnDeviceModelServiceControllerTest,
   std::vector<std::string> expected_responses = ConcatResponses({
       "Context: ctx:contex off:0 max:10\n",
       "Context: t off:10 max:4\n",
-      "Input: execute:foo\n",
+      "Input: execute:contextfoo\n",
   });
   EXPECT_EQ(*response_received_, expected_responses.back());
   EXPECT_THAT(streamed_responses_, ElementsAreArray(expected_responses));
@@ -300,7 +305,7 @@ TEST_F(OnDeviceModelServiceControllerTest,
       "Context: s lo off:10 max:4\n",
       "Context: ng c off:14 max:4\n",
       "Context: onte off:18 max:4\n",
-      "Input: execute:foo\n",
+      "Input: execute:this is long contextfoo\n",
   });
   EXPECT_EQ(*response_received_, expected_responses.back());
   EXPECT_THAT(streamed_responses_, ElementsAreArray(expected_responses));
@@ -318,7 +323,7 @@ TEST_F(OnDeviceModelServiceControllerTest,
   EXPECT_TRUE(response_received_);
   std::vector<std::string> expected_responses = ConcatResponses({
       "Context: ctx:this i off:0 max:10\n",
-      "Input: execute:foo\n",
+      "Input: execute:this is long contextfoo\n",
   });
   EXPECT_EQ(*response_received_, expected_responses.back());
   EXPECT_THAT(streamed_responses_, ElementsAreArray(expected_responses));
@@ -350,7 +355,7 @@ TEST_F(OnDeviceModelServiceControllerTest, ModelExecutionNoMinContext) {
       "Context: ctx: off:0 max:4\n",
       "Context: cont off:4 max:4\n",
       "Context: ext off:8 max:4\n",
-      "Input: execute:foo\n",
+      "Input: execute:contextfoo\n",
   });
   EXPECT_EQ(*response_received_, expected_responses.back());
   EXPECT_THAT(streamed_responses_, ElementsAreArray(expected_responses));
