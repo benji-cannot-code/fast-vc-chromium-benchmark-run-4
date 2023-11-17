09171e4f7167ce22e4390b9a106a31d912b1990d
Scott Violet <sky@chromium.org>
2023-11-17 19:20:39 +0000

    on-device: Makes LoadModelResult enum to loading
    This will be used be used if we need to avoid certain gpus.
    
    Bug: b:301662165
    Change-Id: I32556c64d08eebff6d9fcf600f6dc16dae471309
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5033472
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226266}

--- services/on_device_model/on_device_model_service.h
+++ services/on_device_model/on_device_model_service.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define SERVICES_ON_DEVICE_MODEL_ON_DEVICE_MODEL_SERVICE_H_
 
 #include "base/component_export.h"
+#include "base/types/expected.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/unique_receiver_set.h"
 #include "services/on_device_model/public/cpp/on_device_model.h"
@@ -39,8 +40,8 @@ class COMPONENT_EXPORT(ON_DEVICE_MODEL) OnDeviceModelService
       GetEstimatedPerformanceClassCallback callback) override;
 
  private:
-  static std::unique_ptr<OnDeviceModel> CreateModel(
-      mojom::LoadModelParamsPtr params);
+  static base::expected<std::unique_ptr<OnDeviceModel>, mojom::LoadModelResult>
+  CreateModel(mojom::LoadModelParamsPtr params);
 
   mojo::Receiver<mojom::OnDeviceModelService> receiver_;
   mojo::UniqueReceiverSet<mojom::OnDeviceModel> model_receivers_;
