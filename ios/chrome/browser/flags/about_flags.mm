79065b641dad3363736dfea4b8955cf382498f8c
Vincent Boisselle <vincb@google.com>
2023-11-17 20:26:47 +0000

    [ios][up] chrome://flag for user policy notif feature
    Change-Id: I3b6790383fee34a47c59d7734922b4973a871d31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5041157
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1226306}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -1628,6 +1628,11 @@ const flags_ui::FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kIOSPasswordSignInUffName,
      flag_descriptions::kIOSPasswordSignInUffDescription, flags_ui::kOsIos,
      FEATURE_VALUE_TYPE(password_manager::features::kIOSPasswordSignInUff)},
+    {"enable-user-policy-notif",
+     flag_descriptions::kShowUserPolicyNotificationAtStartupIfNeededName,
+     flag_descriptions::kShowUserPolicyNotificationAtStartupIfNeededDescription,
+     flags_ui::kOsIos,
+     FEATURE_VALUE_TYPE(policy::kShowUserPolicyNotificationAtStartupIfNeeded)},
 };
 
 bool SkipConditionalFeatureEntry(const flags_ui::FeatureEntry& entry) {
