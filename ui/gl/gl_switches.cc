a53338a39c18b45381b4f9c892bbdfdb463a4dd1
Peng Huang <penghuang@chromium.org>
2023-11-17 22:45:20 +0000

    Add feature::kANGLEDebugLayer for enabling angle debug layer
    For Graphite, we may share d3d11 device between ANGLE and graphite,
    and I would like to have a finch flag to enable debug layer to
    help us diagnose API usage errors.
    
    Bug: 1489643
    Change-Id: Ic5fce28ed42d50819f02b246f001e1029d346ccf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5037450
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226390}

--- ui/gl/gl_switches.cc
+++ ui/gl/gl_switches.cc
@@ -292,6 +292,11 @@ BASE_FEATURE(kVulkanFromANGLE,
              "VulkanFromANGLE",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Enable ANGLE's debug layer.
+BASE_FEATURE(kANGLEDebugLayer,
+             "ANGLEDebugLayer",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 bool IsDefaultANGLEVulkan() {
 #if defined(MEMORY_SANITIZER)
   return false;
