dbe89c31e13156bdc008753978f65198b33450ef
Xiaohan Wang <xhwang@chromium.org>
2023-11-17 23:58:26 +0000

    Downscale gfx::Image for Search
    When we search for a gfx::Image directly, whether we are using Google
    lens or other search engine, downscale it if needed before sending it
    to the search engine.
    
    Bug: 1453681
    Change-Id: I35a10ccdb8739544f1575fbbb00e7a38e32fbf8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5039168
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226427}

--- components/lens/lens_features.cc
+++ components/lens/lens_features.cc
@@ -90,11 +90,8 @@ constexpr base::FeatureParam<bool> kDismissLoadingStateOnDidFinishLoad{
 constexpr base::FeatureParam<bool> kDismissLoadingStateOnPrimaryPageChanged{
     &kLensStandalone, "dismiss-loading-state-on-primary-page-changed", false};
 
-constexpr base::FeatureParam<int> kMaxPixelsForRegionSearch{
-    &kLensImageCompression, "region-search-dimensions-max-pixels", 1000};
-
-constexpr base::FeatureParam<int> kMaxAreaForRegionSearch{
-    &kLensImageCompression, "region-search-dimensions-max-area", 1000000};
+constexpr base::FeatureParam<int> kMaxAreaForImageSearch{
+    &kLensImageCompression, "dimensions-max-area", 1000000};
 
 constexpr base::FeatureParam<int> kMaxPixelsForImageSearch{
     &kLensImageCompression, "dimensions-max-pixels", 1000};
@@ -126,12 +123,8 @@ bool GetEnableLatencyLogging() {
          base::FeatureList::IsEnabled(kLensStandalone);
 }
 
-int GetMaxPixelsForRegionSearch() {
-  return kMaxPixelsForRegionSearch.Get();
-}
-
-int GetMaxAreaForRegionSearch() {
-  return kMaxAreaForRegionSearch.Get();
+int GetMaxAreaForImageSearch() {
+  return kMaxAreaForImageSearch.Get();
 }
 
 int GetMaxPixelsForImageSearch() {
