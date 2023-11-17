794c20c657a0946d9dbd7c716b77d91a2ecfdebf
Hanfeng Zhu <hanfeng@google.com>
2023-11-17 00:51:28 +0000

    Implement the sekelon of boarding pass controller
    Bug:1502330
    
    
    Low-Coverage-Reason: HARD_TO_TEST This CL just contains skeleton code
    
    Change-Id: I9b7229cbed2f73b7b0d2321937235e078df8ed98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029336
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Hanfeng Zhu <hanfeng@google.com>
    Auto-Submit: Hanfeng Zhu <hanfeng@google.com>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225844}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -152,6 +152,7 @@ import org.chromium.chrome.browser.ui.fold_transitions.FoldTransitionController;
 import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
 import org.chromium.chrome.browser.ui.system.StatusBarColorController;
 import org.chromium.chrome.browser.ui.system.StatusBarColorController.StatusBarColorProvider;
+import org.chromium.chrome.browser.wallet.BoardingPassController;
 import org.chromium.chrome.features.start_surface.StartSurface;
 import org.chromium.components.browser_ui.accessibility.PageZoomCoordinator;
 import org.chromium.components.browser_ui.accessibility.PageZoomCoordinatorDelegate;
@@ -334,6 +335,7 @@ public class RootUiCoordinator
     private FoldTransitionController mFoldTransitionController;
     private RestoreTabsFeatureHelper mRestoreTabsFeatureHelper;
     private @Nullable EdgeToEdgeController mE2eController;
+    private @Nullable BoardingPassController mBoardingPassController;
 
     /**
      * Create a new {@link RootUiCoordinator} for the given activity.
@@ -706,6 +708,11 @@ public class RootUiCoordinator
             mE2eController = null;
         }
 
+        if (mBoardingPassController != null) {
+            mBoardingPassController.destroy();
+            mBoardingPassController = null;
+        }
+
         mActivity = null;
     }
 
@@ -838,6 +845,7 @@ public class RootUiCoordinator
         initMerchantTrustSignals();
         initScrollCapture();
         initializeEdgeToEdgeController(mActivity, mActivityTabProvider);
+        initBoardingPassDetector();
 
         new OneShotCallback<>(mProfileSupplier, this::initHistoryClustersCoordinator);
 
@@ -1756,6 +1764,12 @@ public class RootUiCoordinator
         mRestoreTabsFeatureHelper.maybeShowPromo(mActivity, mProfileSupplier.get(),
                 mTabCreatorManagerSupplier.get(), getBottomSheetController(),
                 gtsTabListModelSizeSupplier, scrollGTSToRestoredTabsCallback);
+    }
+
+    private void initBoardingPassDetector() {
+        if (ChromeFeatureList.isEnabled(ChromeFeatureList.BOARDING_PASS_DETECTOR)) {
+            mBoardingPassController = new BoardingPassController(mActivityTabProvider);
+        }
     }
 
     // Testing methods
