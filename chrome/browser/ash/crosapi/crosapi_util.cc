376d91187fc424daf6c21189324edadb7c766749
Martin Kreichgauer <martinkr@google.com>
2023-11-17 17:13:35 +0000

    Add crosapi service for accessing GPM passkeys
    The `PasskeyAuthenticator` crosapi service gives Lacros assertion access
    to GPM passkeys. Ash has access to the security domain secret needed to
    decrypt key material on the `WebauthnCredential` entities that represent
    passkeys in Sync.
    
    Bug: 1223853
    Change-Id: I1a165340304e0b98207429a883c002e501d29d94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4956453
    Reviewed-by: Adam Langley <agl@chromium.org>
    Auto-Submit: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226183}

--- chrome/browser/ash/crosapi/crosapi_util.cc
+++ chrome/browser/ash/crosapi/crosapi_util.cc
@@ -115,6 +115,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/crosapi/mojom/networking_attributes.mojom.h"
 #include "chromeos/crosapi/mojom/networking_private.mojom.h"
 #include "chromeos/crosapi/mojom/parent_access.mojom.h"
+#include "chromeos/crosapi/mojom/passkeys.mojom.h"
 #include "chromeos/crosapi/mojom/policy_service.mojom.h"
 #include "chromeos/crosapi/mojom/power.mojom.h"
 #include "chromeos/crosapi/mojom/prefs.mojom.h"
@@ -297,7 +298,7 @@ constexpr InterfaceVersionEntry MakeInterfaceVersionEntry() {
   return {T::Uuid_, T::Version_};
 }
 
-static_assert(crosapi::mojom::Crosapi::Version_ == 123,
+static_assert(crosapi::mojom::Crosapi::Version_ == 124,
               "If you add a new crosapi, please add it to "
               "kInterfaceVersionEntries below.");
 
@@ -383,6 +384,7 @@ constexpr InterfaceVersionEntry kInterfaceVersionEntries[] = {
     MakeInterfaceVersionEntry<crosapi::mojom::NetworkingAttributes>(),
     MakeInterfaceVersionEntry<crosapi::mojom::NetworkingPrivate>(),
     MakeInterfaceVersionEntry<crosapi::mojom::NetworkSettingsService>(),
+    MakeInterfaceVersionEntry<crosapi::mojom::PasskeyAuthenticator>(),
     MakeInterfaceVersionEntry<crosapi::mojom::PolicyService>(),
     MakeInterfaceVersionEntry<crosapi::mojom::Power>(),
     MakeInterfaceVersionEntry<crosapi::mojom::Prefs>(),
