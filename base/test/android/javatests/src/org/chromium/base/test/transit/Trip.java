6ba6b19930ab2a291209b9b01c04b25aa96665cc
Henrique Nakashima <hnakashima@chromium.org>
2023-11-17 20:49:50 +0000

    [Android] Implement TransitAsserts#assertFinalDestination()
    Example failure:
    
    [FAILURE] org.chromium.chrome.browser.toolbar.top.TabSwitcherActionMenuPTTest#testClosingAllRegularTabs_DoNotFinishActivity:
    java.lang.AssertionError: Expected final destination to be <S4: PageStation>, but was <S5: TabSwitcherStation>
      [1] (FINISHED) <S1: EntryPageStation>
      [2] (FINISHED) <S2: NewTabPageStation>
      [3] (FINISHED) <S3: NewTabPageStation>
      [4] (FINISHED) <S4: PageStation>
      [5] (ACTIVE) <S5: TabSwitcherStation>
    
     at org.chromium.base.test.transit.TransitAsserts.raiseAssertion(TransitAsserts.java:27)
     at org.chromium.base.test.transit.TransitAsserts.assertFinalDestination(TransitAsserts.java:19)
     at org.chromium.chrome.browser.toolbar.top.TabSwitcherActionMenuPTTest.testClosingAllRegularTabs_DoNotFinishActivity(TabSwitcherActionMenuPTTest.java:118)
    
    Bug: 1489446
    Change-Id: I98bf583a8c2b6fccf943f7199dfbf16629d64be5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5031372
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226324}

--- base/test/android/javatests/src/org/chromium/base/test/transit/Trip.java
+++ base/test/android/javatests/src/org/chromium/base/test/transit/Trip.java
@@ -115,5 +115,6 @@ public class Trip extends Transition {
             mOrigin.setStateFinished();
         }
         mDestination.setStateActive();
+        TrafficControl.notifyActiveStationChanged(mDestination);
     }
 }
