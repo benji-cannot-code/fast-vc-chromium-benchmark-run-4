ba2073a292f762bbd297b0d93950d49f68910702
Samar <samarchehade@google.com>
2023-11-17 11:17:14 +0000

    [Android][Signin] Create AccountManagerDelegateException
    This change is step 1/3 of a three-part change:
    => https://crrev.com/c/5033622 Create
    AccountManagerDelegateException
    * https://crrev.com/i/6673975 Do not fallback to an empty list of
    accounts
    * https://crrev.com/c/5033764 Add a backoff mechanism to fetch the
    accounts
    
    Bug: 1502123
    Change-Id: Ic0d20bea51f35441b96e16297b6aec9dfea4f086
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5033622
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226005}

---
+++ components/signin/public/android/java/src/org/chromium/components/signin/AccountManagerDelegateException.java
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.signin;
+
+/**
+ * AccountManagerDelegateException encapsulates errors that can happen while getting list of
+ * accounts.
+ */
+public class AccountManagerDelegateException extends Exception {
+    public AccountManagerDelegateException(String message) {
+        super(message);
+    }
+}
