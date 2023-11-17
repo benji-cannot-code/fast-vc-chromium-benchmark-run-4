b5b193f3aa0c2b58704f6295dc5b75643d6a3e5e
Ben Reich <benreich@chromium.org>
2023-11-17 00:59:35 +0000

    [FilesTS] Convert DisplayPanel, PanelItem and unit test
    Bug: b/289003444
    Test: browser_tests --gtest_filter=FileManagerJsTest.DisplayPanel
    Change-Id: I8eb4a5f371b1a7cd9ac369477e2f907128286ba0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5013907
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225849}

--- ui/file_manager/file_manager/foreground/elements/xf_button.ts
+++ ui/file_manager/file_manager/foreground/elements/xf_button.ts
@@ -30,6 +30,10 @@ export class PanelButton extends HTMLElement {
     this.attachShadow({mode: 'open'}).appendChild(fragment);
   }
 
+  static get is() {
+    return 'xf-button' as const;
+  }
+
   /**
    * Registers this instance to listen to these attribute changes.
    */
@@ -80,4 +84,11 @@ export class PanelButton extends HTMLElement {
   }
 }
 
-window.customElements.define('xf-button', PanelButton);
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [PanelButton.is]: PanelButton;
+  }
+}
+
+window.customElements.define(PanelButton.is, PanelButton);
