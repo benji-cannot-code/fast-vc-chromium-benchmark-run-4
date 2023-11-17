975b9b04ebcbb708c5f02496e53b946dddf9efa4
Ashley Prasad <ashleydp@google.com>
2023-11-17 18:02:25 +0000

    Firmware UI: Set test host using prod const
    - Extract url_constants into a separate source_set.
    - Add url_constants source set as a public dependency in
      browser tests and firmware update to enable using const
      in targets that depend on it.
    - Replace host name const with imported const from prod
      code in browser test.
    - Remove TODO.
    
    Bug: b:309953643
    Test: browser_test --gtest_filter=*FirmwareUpdate*
    Change-Id: I6e51bfed7af3d4dfbc20faa56bb38aa57b13b927
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5025653
    Reviewed-by: Camden Bickel <cambickel@google.com>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1226212}

--- chrome/test/data/webui/chromeos/firmware_update/firmware_update_browsertest.cc
+++ chrome/test/data/webui/chromeos/firmware_update/firmware_update_browsertest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "ash/webui/firmware_update_ui/url_constants.h"
 #include "chrome/test/base/web_ui_mocha_browser_test.h"
 #include "content/public/test/browser_test.h"
 
@@ -13,13 +14,11 @@ namespace ash {
 
 namespace {
 
-// TODO(b/309953643): Configure test support for firmware update and update code
-// to use same const as prod.
-constexpr char kFirmwareUpdateHost[] = "accessory-update";
-
 class FirmwareUpdateAppBrowserTest : public WebUIMochaBrowserTest {
  public:
-  FirmwareUpdateAppBrowserTest() { set_test_loader_host(kFirmwareUpdateHost); }
+  FirmwareUpdateAppBrowserTest() {
+    set_test_loader_host(::ash::kChromeUIFirmwareUpdateAppHost);
+  }
 };
 
 IN_PROC_BROWSER_TEST_F(FirmwareUpdateAppBrowserTest, FakeUpdateController) {
