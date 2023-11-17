ad6b690fac1692b7239ca6dd5d69b91279d2213d
Julie Jeongeun Kim <jkim@igalia.com>
2023-11-17 09:50:48 +0000

    [Code Health] Fix dangling raw_ptr in HistoryMenuBridge
    This CL destroys HistoryMenuBridge before resetting `profile_`
    not to have dangling raw_ptr.
    
    Bug: 1462043
    Change-Id: I7c65c15c0a5c06b53a15e417c2d87bfd8fa7239e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5036006
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1225984}

--- chrome/browser/ui/cocoa/history_menu_bridge_unittest.mm
+++ chrome/browser/ui/cocoa/history_menu_bridge_unittest.mm
@@ -581,9 +581,9 @@ TEST_F(HistoryMenuBridgeLifetimeTest, ShutdownAfterProfile) {
   std::unique_ptr<TestingProfile> profile = profile_builder.Build();
 
   auto bridge = std::make_unique<HistoryMenuBridge>(profile.get());
-  profile.reset();
   // Should not crash.
   bridge.reset();
+  profile.reset();
 }
 
 // Does a full setup and tear down of the bridge.
@@ -598,6 +598,7 @@ TEST_F(HistoryMenuBridgeLifetimeTest, ShutdownBeforeProfile) {
 
   auto bridge = std::make_unique<HistoryMenuBridge>(profile.get());
   bridge.reset();
+  profile.reset();
 }
 
 // Initializes the menu, then destroys the Profile but keeps the
