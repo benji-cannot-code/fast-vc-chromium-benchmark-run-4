d8a19c7f18487e27100f8d41449334fda5d66eb7
Christoph Schwering <schwering@google.com>
2023-11-17 11:55:44 +0000

    [Autofill] Remove FormFieldData::selection_{start,end}
    This CL removes FormFieldData::selection_{start,end}.
    
    Bug: 1490372, 1484810
    Change-Id: I0d6accf719ffd4324e59932ab76f53568b0cd6dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5033418
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1226025}

--- chrome/renderer/autofill/form_autofill_browsertest.cc
+++ chrome/renderer/autofill/form_autofill_browsertest.cc
@@ -2340,7 +2340,6 @@ TEST_F(FormAutofillTest, WebFormControlElementToFormField) {
   ASSERT_NE(nullptr, frame);
 
   WebFormControlElement element = GetFormControlElementById("element");
-  element.SetSelectionRange(1, 4);
 
   FormFieldData result1;
   WebFormControlElementToFormField(WebFormElement(), element, nullptr,
@@ -2355,8 +2354,6 @@ TEST_F(FormAutofillTest, WebFormControlElementToFormField) {
 
   expected.value.clear();
   EXPECT_FORM_FIELD_DATA_EQUALS(expected, result1);
-  EXPECT_EQ(0u, result1.selection_start);
-  EXPECT_EQ(0u, result1.selection_end);
 
   FormFieldData result2;
   WebFormControlElementToFormField(WebFormElement(), element, nullptr,
@@ -2364,10 +2361,6 @@ TEST_F(FormAutofillTest, WebFormControlElementToFormField) {
 
   expected.value = u"value";
   EXPECT_FORM_FIELD_DATA_EQUALS(expected, result2);
-  EXPECT_EQ(1u, result2.selection_start);
-  EXPECT_EQ(4u, result2.selection_end);
-  EXPECT_EQ(u"alu", result2.GetSelection());
-  EXPECT_EQ(u"alu", result2.GetSelectionAsStringView());
 }
 
 // We should be able to extract a text field with autocomplete="off".
