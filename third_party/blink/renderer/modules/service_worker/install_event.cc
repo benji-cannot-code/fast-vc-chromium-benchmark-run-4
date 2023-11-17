e863e4957a093ce65890c5a146051cb13c3efbcc
Takashi Nakayama <tnak@chromium.org>
2023-11-17 09:17:23 +0000

    Change SW Static Router ID from string to uint32
    This CL changes the type of `ServiceWorkerRouterEvaluator::Result::id_`
    from `std::string` to `std::uint32_t`. Since `id_` is not used now,
    this does not involve breaking changes.
    This CL also introduce the maximum number of router that can be
    registered, controlled by `kServiceWorkerMaxRouterSize`.
    
    Bug: 1490445
    Change-Id: Id098792ef195ce41dbe351cbc57e3bf735d7f1cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5040080
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Takashi Nakayama <tnak@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225973}

--- third_party/blink/renderer/modules/service_worker/install_event.cc
+++ third_party/blink/renderer/modules/service_worker/install_event.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/service_worker/install_event.h"
 
+#include "third_party/blink/public/common/service_worker/service_worker_router_rule.h"
 #include "third_party/blink/public/mojom/service_worker/service_worker_router_rule.mojom-blink.h"
 #include "third_party/blink/public/platform/web_security_origin.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
@@ -163,6 +164,11 @@ void InstallEvent::ConvertServiceWorkerRouterRules(
     rules.rules.emplace_back(*r);
   } else {
     CHECK(v8_rules->IsRouterRuleSequence());
+    if (v8_rules->GetAsRouterRuleSequence().size() >=
+        kServiceWorkerMaxRouterSize) {
+      exception_state.ThrowTypeError("Too many router rules.");
+      return;
+    }
     for (const blink::RouterRule* rule : v8_rules->GetAsRouterRuleSequence()) {
       auto r = ConvertV8RouterRuleToBlink(rule, base_url, exception_state);
       if (!r) {
