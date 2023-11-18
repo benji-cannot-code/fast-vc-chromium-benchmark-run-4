a3f5d8e4a364ee5e9b50600cfbadf80f8337e9d9
Guillem Perez <guiperez@google.com>
2023-11-18 02:57:22 +0000

    [iOS] Create the Notifications Promo View.
    Created the notifications promo view to then be added to the Feed Top
    Section View Controller by a follow up CL. This CL adds the necessary
    files to be able to create the notifications promo view.
    
    Change-Id: I7d1e05a791715452026f18f28d98ac4190e0ef02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5038791
    Reviewed-by: Tina Wang <tinazwang@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Reviewed-by: Marke Hallowell <wry@chromium.org>
    Auto-Submit: Guillem Perez <guiperez@google.com>
    Commit-Queue: Guillem Perez <guiperez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1226474}

---
+++ ios/chrome/browser/ui/ntp/feed_top_section/notifications_promo_view_constants.mm
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/ntp/feed_top_section/notifications_promo_view_constants.h"
+
+NSString* const kNotificationsPromoCloseButtonId =
+    @"NotificationsPromoCloseButtonId";
