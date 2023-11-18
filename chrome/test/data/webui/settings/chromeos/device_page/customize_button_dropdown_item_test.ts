b352381cae8b6199a3cc46572b373b493a9d7000
YuhanYang <yyhyyh@google.com>
2023-11-18 01:18:28 +0000

    customization: Create new dropdown item
    Create a new element as the button dropdown that displays icons
    to match the UI/UX requirements.
    
    Bug: b/241965717
    Test: browser_tests and visual effect on DUT
    Change-Id: I4f9fdc4b28a9402f017fa8db9c61bcad26a85f8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5021621
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226468}

---
+++ chrome/test/data/webui/settings/chromeos/device_page/customize_button_dropdown_item_test.ts
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://os-settings/lazy_load.js';
+import 'chrome://resources/polymer/v3_0/iron-test-helpers/mock-interactions.js';
+
+import {CustomizeButtonDropdownItemElement} from 'chrome://os-settings/lazy_load.js';
+import {assert} from 'chrome://resources/js/assert.js';
+import {assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
+
+suite('<customize-button-dropdown-item>', () => {
+  let dropdownItem: CustomizeButtonDropdownItemElement;
+
+  setup(() => {
+    assert(window.trustedTypes);
+    document.body.innerHTML = window.trustedTypes.emptyHTML;
+  });
+
+  teardown(async () => {
+    if (!dropdownItem) {
+      return;
+    }
+    dropdownItem.remove();
+    await flushTasks();
+  });
+
+  function initializeDropdownItem() {
+    dropdownItem =
+        document.createElement(CustomizeButtonDropdownItemElement.is);
+    document.body.appendChild(dropdownItem);
+    return flushTasks();
+  }
+
+  test('Initialize customize button dropdown item', async () => {
+    await initializeDropdownItem();
+
+    assertTrue(!!dropdownItem);
+  });
+});
