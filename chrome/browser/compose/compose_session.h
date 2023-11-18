7e82bf6d0c84c907065a78e832e04dd869fb7f63
Jeffrey Cohen <jeffreycohen@google.com>
2023-11-18 06:48:44 +0000

    Add latency to quality logging
    Bug: b:310725358
    Change-Id: I09df31b2a5ba19ab65b391ab118299d43f868d1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5027674
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226512}

--- chrome/browser/compose/compose_session.h
+++ chrome/browser/compose/compose_session.h
@@ -11,10 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/check_op.h"
+#include "base/timer/elapsed_timer.h"
 #include "chrome/browser/compose/inner_text_extractor.h"
 #include "chrome/common/compose/compose.mojom.h"
 #include "components/autofill/core/common/unique_ids.h"
 #include "components/compose/core/browser/compose_metrics.h"
+#include "components/optimization_guide/core/model_quality/model_quality_logs_uploader.h"
 #include "components/optimization_guide/core/optimization_guide_model_executor.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -45,9 +47,11 @@ class ComposeSession : public compose::mojom::ComposeSessionPageHandler {
   // form field on which it was triggered.
   using ComposeCallback = base::OnceCallback<void(const std::u16string&)>;
 
-  ComposeSession(content::WebContents* web_contents,
-                 optimization_guide::OptimizationGuideModelExecutor* executor,
-                 ComposeCallback callback = base::NullCallback());
+  ComposeSession(
+      content::WebContents* web_contents,
+      optimization_guide::OptimizationGuideModelExecutor* executor,
+      optimization_guide::ModelQualityLogsUploader* model_quality_logs_uploader,
+      ComposeCallback callback = base::NullCallback());
   ~ComposeSession() override;
 
   // Binds this to a Compose webui.
@@ -116,7 +120,7 @@ class ComposeSession : public compose::mojom::ComposeSessionPageHandler {
  private:
   void ProcessError(compose::mojom::ComposeStatus status);
   void ModelExecutionCallback(
-      base::TimeTicks request_start,
+      base::ElapsedTimer* request_start,
       int request_id,
       optimization_guide::OptimizationGuideModelStreamingExecutionResult result,
       std::unique_ptr<optimization_guide::ModelQualityLogEntry> log_entry);
@@ -182,6 +186,8 @@ class ComposeSession : public compose::mojom::ComposeSessionPageHandler {
 
   std::unique_ptr<optimization_guide::ModelQualityLogEntry> modeling_log_entry_;
 
+  std::optional<optimization_guide::ModelQualityLogsUploader*>
+      model_quality_logs_uploader_;
   base::WeakPtrFactory<ComposeSession> weak_ptr_factory_;
 };
 
