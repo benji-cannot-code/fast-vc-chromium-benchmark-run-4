01c230cec0d24f461fc5793c7e2e012271129599
Ted Meyer <tmathmeyer@chromium.org>
2023-11-18 03:06:44 +0000

    CreateRendition should not return Or type
    There is no failure case anymore, so HlsRendition::Create should always
    just return a unique ptr
    
    Bug: 1266991
    Change-Id: Ib497418293b78da19eb2e73199d6bc4f7af0b036
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5036020
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226490}

--- media/filters/hls_rendition_impl_unittest.cc
+++ media/filters/hls_rendition_impl_unittest.cc
@@ -89,8 +89,8 @@ class HlsRenditionImplUnittest : public testing::Test {
     auto playlist = std::move(parsed).value();
     auto duration = playlist->GetComputedDuration();
     return std::make_unique<HlsRenditionImpl>(mock_mdeh_.get(), mock_hrh_.get(),
-                                             "test", std::move(playlist),
-                                             duration, uri);
+                                              "test", std::move(playlist),
+                                              duration, uri);
   }
 
   std::unique_ptr<HlsRenditionImpl> MakeLiveRendition(
@@ -103,8 +103,8 @@ class HlsRenditionImplUnittest : public testing::Test {
       return nullptr;
     }
     return std::make_unique<HlsRenditionImpl>(mock_mdeh_.get(), mock_hrh_.get(),
-                                             "test", std::move(parsed).value(),
-                                             absl::nullopt, uri);
+                                              "test", std::move(parsed).value(),
+                                              absl::nullopt, uri);
   }
 
   MOCK_METHOD(void, CheckStateComplete, (base::TimeDelta delay), ());
