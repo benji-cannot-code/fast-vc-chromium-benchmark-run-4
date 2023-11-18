d2b7cddb27e21f1e1bbd1893f69e4ec0c32f9fb2
Etienne Pierre-doray <etiennep@chromium.org>
2023-11-18 06:47:13 +0000

    [task] Annotate flow for jobs
    The CL adds TaskTracker::WillEnqueueJob to add flow annotation
    on JobTaskSource.
    
    This CL fixes 2 cases missing a correct sequence_num:
    - Task returned from TaskSource::Clear()
    - Task created by JobTaskSource
    Since
    https://chromium-review.googlesource.com/c/chromium/src/+/4887296
    In those cases, sequence_num is only used for tracing flow.
    
    Bug: 1473394
    Change-Id: Ia1aa07a3745d0a1a29e438b66003d362a750b3f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4893196
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226509}

--- base/task/thread_pool/job_task_source.h
+++ base/task/thread_pool/job_task_source.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/synchronization/condition_variable.h"
 #include "base/task/common/checked_lock.h"
+#include "base/task/common/task_annotator.h"
 #include "base/task/post_job.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool/task.h"
@@ -46,6 +47,9 @@ class BASE_EXPORT JobTaskSource : public TaskSource {
       scoped_refptr<internal::JobTaskSource> task_source) {
     return JobHandle(std::move(task_source));
   }
+
+  // Called before the task source is enqueued to initialize task metadata.
+  void WillEnqueue(int sequence_num, TaskAnnotator& annotator);
 
   // Notifies this task source that max concurrency was increased, and the
   // number of worker should be adjusted.
@@ -193,7 +197,7 @@ class BASE_EXPORT JobTaskSource : public TaskSource {
   // TaskSource:
   RunStatus WillRunTask() override;
   Task TakeTask(TaskSource::Transaction* transaction) override;
-  Task Clear(TaskSource::Transaction* transaction) override;
+  absl::optional<Task> Clear(TaskSource::Transaction* transaction) override;
   bool DidProcessTask(TaskSource::Transaction* transaction) override;
   bool WillReEnqueue(TimeTicks now,
                      TaskSource::Transaction* transaction) override;
@@ -214,13 +218,14 @@ class BASE_EXPORT JobTaskSource : public TaskSource {
 
   std::atomic<uint32_t> assigned_task_ids_{0};
 
-  const Location from_here_;
   RepeatingCallback<size_t(size_t)> max_concurrency_callback_;
 
   // Worker task set by the job owner.
   RepeatingCallback<void(JobDelegate*)> worker_task_;
   // Task returned from TakeTask(), that calls |worker_task_| internally.
   RepeatingClosure primary_task_;
+
+  TaskMetadata task_metadata_;
 
   const TimeTicks ready_time_;
   raw_ptr<PooledTaskRunnerDelegate, LeakedDanglingUntriaged> delegate_;
--- base/task/thread_pool/job_task_source_unittest.cc
+++ base/task/thread_pool/job_task_source_unittest.cc
@@ -126,7 +126,7 @@ TEST_F(ThreadPoolJobTaskSourceTest, Clear) {
   {
     EXPECT_EQ(1U, task_source->GetRemainingConcurrency());
     auto task = registered_task_source_c.Clear();
-    std::move(task.task).Run();
+    EXPECT_FALSE(task);
     registered_task_source_c.DidProcessTask();
     EXPECT_EQ(0U, task_source->GetRemainingConcurrency());
   }
@@ -138,7 +138,7 @@ TEST_F(ThreadPoolJobTaskSourceTest, Clear) {
   // Another outstanding RunStatus can still call Clear.
   {
     auto task = registered_task_source_d.Clear();
-    std::move(task.task).Run();
+    EXPECT_FALSE(task);
     registered_task_source_d.DidProcessTask();
     EXPECT_EQ(0U, task_source->GetRemainingConcurrency());
   }
