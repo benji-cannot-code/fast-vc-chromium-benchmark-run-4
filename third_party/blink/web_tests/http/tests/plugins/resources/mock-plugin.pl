1e24ea94f4974baef15325a0ce3215697a3afe31
Dave Tapuska <dtapuska@chromium.org>
2023-11-19 18:48:40 +0000

    [ppapi] Move some plugin tests into ppapi subdirectory
    In preparation for disabling ppapi on every platform but ChromeOS
    move the tests into a ppapi subdirectory so we can easily skip it.
    
    Rewrite some tests to use the WebTest plugin.
    
    Teach WebTest plugin about a scriptable object that "loaded" is defined
    as a true boolean.
    
    Bug: b:303417958
    Change-Id: Icd7680e89897f01e99e6cd53f380f8f795cda0e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5041191
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1226582}

--- third_party/blink/web_tests/http/tests/plugins/resources/mock-plugin.pl
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/perl -wT
-use strict;
-
-print "Content-Type: application/x-blink-test-plugin\n\n";
-print "This is a mock plugin. It does pretty much nothing.";
