33625e441c6f370ef5a4ef57f63c3e75976c2af0
anthie@google.com <anthie@google.com>
2023-11-20 18:51:09 +0000

    Reland "Refactor SU Preferences IsURLFilteringEnabled"
    This is a reland of commit 6a6e2c5b36a8b8472e96d0d51f089cbe371cdbb0
    
    The change compared to the original commit is the test suite supervised_user_regional_url_filter_browsertest.cc
    which is now consuming the migrated method (the previously
    merged change collided with updates on the test suite).
    
    Original change's description:
    > Refactor SU Preferences IsURLFilteringEnabled
    >
    > Move the method IsURLFilteringEnabled from
    > SUS to the SU Pref library.
    > Also adapt the test cases for the method for both
    > supervised and non-supervised users, with the corresponding
    > feature flag on/off.
    >
    > Bug: b/306376651
    > Change-Id: Ifcf84ca249d66056f11a88845e5c2dc526451380
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5006448
    > Commit-Queue: Anthi Orfanou <anthie@google.com>
    > Reviewed-by: David Roger <droger@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Tomek Jurkiewicz <tju@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1225539}
    
    Bug: b/306376651
    Change-Id: I211b3935303bd5f8f11bb11895fc3bb2fbc10df3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5040563
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Tomek Jurkiewicz <tju@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1226938}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -275,6 +275,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(ENABLE_SUPERVISED_USERS)
 #include "chrome/browser/supervised_user/supervised_user_service_factory.h"
+#include "components/supervised_user/core/browser/supervised_user_preferences.h"
 #include "components/supervised_user/core/browser/supervised_user_service.h"
 #include "components/supervised_user/core/browser/supervised_user_url_filter.h"
 #endif
@@ -3566,10 +3567,10 @@ bool RenderViewContextMenu::IsSaveLinkAsEnabled() const {
 
 #if BUILDFLAG(ENABLE_SUPERVISED_USERS)
   Profile* const profile = Profile::FromBrowserContext(browser_context_);
-  supervised_user::SupervisedUserService* supervised_user_service =
-      SupervisedUserServiceFactory::GetForProfile(profile);
-  if (supervised_user_service &&
-      supervised_user_service->IsURLFilteringEnabled()) {
+  CHECK(profile);
+  if (supervised_user::IsUrlFilteringEnabled(*profile->GetPrefs())) {
+    supervised_user::SupervisedUserService* supervised_user_service =
+        SupervisedUserServiceFactory::GetForProfile(profile);
     supervised_user::SupervisedUserURLFilter* url_filter =
         supervised_user_service->GetURLFilter();
     // Use the URL filter's synchronous call to check if a site has been
@@ -3924,11 +3925,10 @@ void RenderViewContextMenu::ExecInspectBackgroundPage() {
 #if BUILDFLAG(ENABLE_SUPERVISED_USERS)
 void RenderViewContextMenu::CheckSupervisedUserURLFilterAndSaveLinkAs() {
   Profile* const profile = Profile::FromBrowserContext(browser_context_);
-  supervised_user::SupervisedUserService* supervised_user_service =
-      SupervisedUserServiceFactory::GetForProfile(profile);
-
-  if (supervised_user_service &&
-      supervised_user_service->IsURLFilteringEnabled()) {
+  CHECK(profile);
+  if (supervised_user::IsUrlFilteringEnabled(*profile->GetPrefs())) {
+    supervised_user::SupervisedUserService* supervised_user_service =
+        SupervisedUserServiceFactory::GetForProfile(profile);
     supervised_user::SupervisedUserURLFilter* url_filter =
         supervised_user_service->GetURLFilter();
     url_filter->GetFilteringBehaviorForURLWithAsyncChecks(
--- chrome/browser/supervised_user/supervised_user_regional_url_filter_browsertest.cc
+++ chrome/browser/supervised_user/supervised_user_regional_url_filter_browsertest.cc
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/supervised_user/core/browser/fetcher_config.h"
 #include "components/supervised_user/core/browser/kids_chrome_management_client.h"
 #include "components/supervised_user/core/browser/proto/kidschromemanagement_messages.pb.h"
-#include "components/supervised_user/core/browser/supervised_user_service.h"
+#include "components/supervised_user/core/browser/supervised_user_preferences.h"
 #include "components/supervised_user/core/common/features.h"
 #include "components/supervised_user/test_support/kids_management_api_server_mock.h"
 #include "components/variations/variations_switches.h"
@@ -211,8 +211,8 @@ class SupervisedUserRegionalURLFilterTest
   }
 
   bool IsUrlFilteringEnabled() const {
-    return SupervisedUserServiceFactory::GetForProfile(browser()->profile())
-        ->IsURLFilteringEnabled();
+    return supervised_user::IsUrlFilteringEnabled(
+        *browser()->profile()->GetPrefs());
   }
 
  private:
