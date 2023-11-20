660d54a092d0b2cfe53c4943e5cf558a238ac1d5
mlcui <mlcui@google.com>
2023-11-20 00:46:49 +0000

    language_pack_manager: Use test-local `FakeDlcserviceClient`
    This cleans up the `SetUp()` and `TearDown()` methods significantly. The
    global `DlcserviceClient` is set in the constructor, and unset in the
    destructor, so this effectively provides a scoped test
    `DlcserviceClient`.
    
    Three other unit tests (ambient_controller, time_of_day_utils and
    handwriting_model_loader) use the same pattern.
    
    This also removes a DanglingUntriaged pointer as a result.
    
    Bug: b:294162606, 1291138
    Change-Id: I361179b9b9e21e982333aee47afb0c7b24f7eb9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5031715
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Claudio M <claudiomagni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226639}

--- chromeos/ash/components/language_packs/language_pack_manager_unittest.cc
+++ chromeos/ash/components/language_packs/language_pack_manager_unittest.cc
@@ -89,20 +89,12 @@ DlcState CreateInstalledState() {
 class LanguagePackManagerTest : public testing::Test {
  public:
   void SetUp() override {
-    // The Fake DLC Service needs to be initialized before we instantiate
-    // LanguagePackManager.
-    DlcserviceClient::InitializeFake();
-    dlcservice_client_ =
-        static_cast<FakeDlcserviceClient*>(DlcserviceClient::Get());
-
     session_manager_ = std::make_unique<session_manager::SessionManager>();
 
     ResetPackResult();
 
     base::RunLoop().RunUntilIdle();
   }
-
-  void TearDown() override { DlcserviceClient::Shutdown(); }
 
   void InstallTestCallback(const PackResult& pack_result) {
     pack_result_ = pack_result;
@@ -122,8 +114,7 @@ class LanguagePackManagerTest : public testing::Test {
 
  protected:
   PackResult pack_result_;
-  raw_ptr<FakeDlcserviceClient, DanglingUntriaged | ExperimentalAsh>
-      dlcservice_client_;
+  FakeDlcserviceClient dlcservice_client_;
   std::unique_ptr<session_manager::SessionManager> session_manager_;
 
  private:
@@ -136,8 +127,8 @@ class LanguagePackManagerTest : public testing::Test {
 };
 
 TEST_F(LanguagePackManagerTest, InstallSuccessTest) {
-  dlcservice_client_->set_install_error(dlcservice::kErrorNone);
-  dlcservice_client_->set_install_root_path("/path");
+  dlcservice_client_.set_install_error(dlcservice::kErrorNone);
+  dlcservice_client_.set_install_root_path("/path");
 
   // Test UMA metrics: pre-condition.
   base::HistogramTester histogram_tester;
@@ -167,7 +158,7 @@ TEST_F(LanguagePackManagerTest, InstallSuccessTest) {
 }
 
 TEST_F(LanguagePackManagerTest, InstallFailureTest) {
-  dlcservice_client_->set_install_error(dlcservice::kErrorInternal);
+  dlcservice_client_.set_install_error(dlcservice::kErrorInternal);
 
   // Test UMA metrics: pre-condition.
   base::HistogramTester histogram_tester;
@@ -209,8 +200,8 @@ TEST_F(LanguagePackManagerTest, InstallWrongIdTest) {
 
 // Check that the callback is actually called.
 TEST_F(LanguagePackManagerTest, InstallCallbackTest) {
-  dlcservice_client_->set_install_error(dlcservice::kErrorNone);
-  dlcservice_client_->set_install_root_path("/path");
+  dlcservice_client_.set_install_error(dlcservice::kErrorNone);
+  dlcservice_client_.set_install_root_path("/path");
 
   testing::StrictMock<CallbackForTesting> callback;
   EXPECT_CALL(callback, Callback(_));
@@ -221,12 +212,12 @@ TEST_F(LanguagePackManagerTest, InstallCallbackTest) {
 }
 
 TEST_F(LanguagePackManagerTest, GetPackStateSuccessTest) {
-  dlcservice_client_->set_get_dlc_state_error(dlcservice::kErrorNone);
+  dlcservice_client_.set_get_dlc_state_error(dlcservice::kErrorNone);
   dlcservice::DlcState dlc_state;
   dlc_state.set_state(dlcservice::DlcState_State_INSTALLED);
   dlc_state.set_is_verified(true);
   dlc_state.set_root_path("/path");
-  dlcservice_client_->set_dlc_state(dlc_state);
+  dlcservice_client_.set_dlc_state(dlc_state);
 
   // Test UMA metrics: pre-condition.
   base::HistogramTester histogram_tester;
@@ -252,7 +243,7 @@ TEST_F(LanguagePackManagerTest, GetPackStateSuccessTest) {
 }
 
 TEST_F(LanguagePackManagerTest, GetPackStateFailureTest) {
-  dlcservice_client_->set_get_dlc_state_error(dlcservice::kErrorInternal);
+  dlcservice_client_.set_get_dlc_state_error(dlcservice::kErrorInternal);
 
   // Test UMA metrics: pre-condition.
   base::HistogramTester histogram_tester;
@@ -290,7 +281,7 @@ TEST_F(LanguagePackManagerTest, GetPackStateWrongIdTest) {
 
 // Check that the callback is actually called.
 TEST_F(LanguagePackManagerTest, GetPackStateCallbackTest) {
-  dlcservice_client_->set_get_dlc_state_error(dlcservice::kErrorNone);
+  dlcservice_client_.set_get_dlc_state_error(dlcservice::kErrorNone);
 
   testing::StrictMock<CallbackForTesting> callback;
   EXPECT_CALL(callback, Callback(_));
@@ -301,7 +292,7 @@ TEST_F(LanguagePackManagerTest, GetPackStateCallbackTest) {
 }
 
 TEST_F(LanguagePackManagerTest, RemovePackSuccessTest) {
-  dlcservice_client_->set_uninstall_error(dlcservice::kErrorNone);
+  dlcservice_client_.set_uninstall_error(dlcservice::kErrorNone);
 
   // Test UMA metrics: pre-condition.
   base::HistogramTester histogram_tester;
@@ -330,7 +321,7 @@ TEST_F(LanguagePackManagerTest, RemovePackSuccessTest) {
 }
 
 TEST_F(LanguagePackManagerTest, RemovePackFailureTest) {
-  dlcservice_client_->set_uninstall_error(dlcservice::kErrorInternal);
+  dlcservice_client_.set_uninstall_error(dlcservice::kErrorInternal);
 
   // Test UMA metrics: pre-condition.
   base::HistogramTester histogram_tester;
@@ -372,7 +363,7 @@ TEST_F(LanguagePackManagerTest, RemovePackWrongIdTest) {
 
 // Check that the callback is actually called.
 TEST_F(LanguagePackManagerTest, RemovePackCallbackTest) {
-  dlcservice_client_->set_uninstall_error(dlcservice::kErrorNone);
+  dlcservice_client_.set_uninstall_error(dlcservice::kErrorNone);
 
   testing::StrictMock<CallbackForTesting> callback;
   EXPECT_CALL(callback, Callback(_));
@@ -385,13 +376,13 @@ TEST_F(LanguagePackManagerTest, RemovePackCallbackTest) {
 TEST_F(LanguagePackManagerTest, InstallObserverTest) {
   LanguagePackManager manager;
 
-  dlcservice_client_->set_install_error(dlcservice::kErrorNone);
-  dlcservice_client_->set_install_root_path("/path");
+  dlcservice_client_.set_install_error(dlcservice::kErrorNone);
+  dlcservice_client_.set_install_root_path("/path");
   const DlcState dlc_state = CreateInstalledState();
   MockObserver observer;
 
   EXPECT_CALL(observer, OnPackStateChanged(_)).Times(0);
-  dlcservice_client_->NotifyObserversForTest(dlc_state);
+  dlcservice_client_.NotifyObserversForTest(dlc_state);
 
   // Add an Observer and expect it to be notified.
   manager.AddObserver(&observer);
@@ -400,7 +391,7 @@ TEST_F(LanguagePackManagerTest, InstallObserverTest) {
           FieldsAre(AllOf(Field(&PackResult::feature_id, kHandwritingFeatureId),
                           Field(&PackResult::language_code, "de"))))
       .Times(1);
-  dlcservice_client_->NotifyObserversForTest(dlc_state);
+  dlcservice_client_.NotifyObserversForTest(dlc_state);
 
   base::RunLoop().RunUntilIdle();
 }
@@ -408,8 +399,8 @@ TEST_F(LanguagePackManagerTest, InstallObserverTest) {
 TEST_F(LanguagePackManagerTest, RemoveObserverTest) {
   LanguagePackManager manager;
 
-  dlcservice_client_->set_install_error(dlcservice::kErrorNone);
-  dlcservice_client_->set_install_root_path("/path");
+  dlcservice_client_.set_install_error(dlcservice::kErrorNone);
+  dlcservice_client_.set_install_root_path("/path");
   const DlcState dlc_state = CreateInstalledState();
   MockObserver observer;
 
@@ -420,12 +411,12 @@ TEST_F(LanguagePackManagerTest, RemoveObserverTest) {
           FieldsAre(AllOf(Field(&PackResult::feature_id, kHandwritingFeatureId),
                           Field(&PackResult::language_code, "de"))))
       .Times(1);
-  dlcservice_client_->NotifyObserversForTest(dlc_state);
+  dlcservice_client_.NotifyObserversForTest(dlc_state);
 
   // Remove the Observer and there should be no more notifications.
   manager.RemoveObserver(&observer);
   EXPECT_CALL(observer, OnPackStateChanged(_)).Times(0);
-  dlcservice_client_->NotifyObserversForTest(dlc_state);
+  dlcservice_client_.NotifyObserversForTest(dlc_state);
 
   base::RunLoop().RunUntilIdle();
 }
@@ -471,8 +462,8 @@ TEST_F(LanguagePackManagerTest, IsPackAvailableFalseTest) {
 }
 
 TEST_F(LanguagePackManagerTest, InstallBasePackSuccess) {
-  dlcservice_client_->set_install_error(dlcservice::kErrorNone);
-  dlcservice_client_->set_install_root_path("/path");
+  dlcservice_client_.set_install_error(dlcservice::kErrorNone);
+  dlcservice_client_.set_install_root_path("/path");
 
   // Test UMA metrics: pre-condition.
   base::HistogramTester histogram_tester;
@@ -497,7 +488,7 @@ TEST_F(LanguagePackManagerTest, InstallBasePackSuccess) {
 }
 
 TEST_F(LanguagePackManagerTest, InstallBasePackFailureTestFailure) {
-  dlcservice_client_->set_install_error(dlcservice::kErrorInternal);
+  dlcservice_client_.set_install_error(dlcservice::kErrorInternal);
 
   // Test UMA metrics: pre-condition.
   base::HistogramTester histogram_tester;
@@ -523,8 +514,8 @@ TEST_F(LanguagePackManagerTest, InstallBasePackFailureTestFailure) {
 TEST_F(LanguagePackManagerTest, UpdatePacksForOobeNotOobeTest) {
   // Set session as user logged in.
   session_manager_->SetSessionState(session_manager::SessionState::ACTIVE);
-  dlcservice_client_->set_install_error(dlcservice::kErrorNone);
-  dlcservice_client_->set_install_root_path("/path");
+  dlcservice_client_.set_install_error(dlcservice::kErrorNone);
+  dlcservice_client_.set_install_root_path("/path");
 
   testing::StrictMock<CallbackForTesting> callback;
   EXPECT_CALL(callback, Callback(_)).Times(0);
@@ -537,8 +528,8 @@ TEST_F(LanguagePackManagerTest, UpdatePacksForOobeNotOobeTest) {
 TEST_F(LanguagePackManagerTest, UpdatePacksForOobeSuccessTest) {
   session_manager_->SetSessionState(session_manager::SessionState::OOBE);
 
-  dlcservice_client_->set_install_error(dlcservice::kErrorNone);
-  dlcservice_client_->set_install_root_path("/path");
+  dlcservice_client_.set_install_error(dlcservice::kErrorNone);
+  dlcservice_client_.set_install_root_path("/path");
 
   // Test UMA metrics: pre-condition.
   base::HistogramTester histogram_tester;
@@ -568,8 +559,8 @@ TEST_F(LanguagePackManagerTest, UpdatePacksForOobeSuccessTest) {
 TEST_F(LanguagePackManagerTest, UpdatePacksForOobeSuccess2Test) {
   session_manager_->SetSessionState(session_manager::SessionState::OOBE);
 
-  dlcservice_client_->set_install_error(dlcservice::kErrorNone);
-  dlcservice_client_->set_install_root_path("/path");
+  dlcservice_client_.set_install_error(dlcservice::kErrorNone);
+  dlcservice_client_.set_install_root_path("/path");
 
   // Test UMA metrics: pre-condition.
   base::HistogramTester histogram_tester;
@@ -599,8 +590,8 @@ TEST_F(LanguagePackManagerTest, UpdatePacksForOobeSuccess2Test) {
 TEST_F(LanguagePackManagerTest, UpdatePacksForOobeWrongLocaleTest) {
   session_manager_->SetSessionState(session_manager::SessionState::OOBE);
 
-  dlcservice_client_->set_install_error(dlcservice::kErrorNone);
-  dlcservice_client_->set_install_root_path("/path");
+  dlcservice_client_.set_install_error(dlcservice::kErrorNone);
+  dlcservice_client_.set_install_root_path("/path");
 
   // Test UMA metrics: pre-condition.
   base::HistogramTester histogram_tester;
@@ -627,7 +618,7 @@ TEST_F(LanguagePackManagerTest, UpdatePacksForOobeWrongLocaleTest) {
 TEST_F(LanguagePackManagerTest, UpdatePacksForOobeFailureTest) {
   session_manager_->SetSessionState(session_manager::SessionState::OOBE);
 
-  dlcservice_client_->set_install_error(dlcservice::kErrorInternal);
+  dlcservice_client_.set_install_error(dlcservice::kErrorInternal);
 
   LanguagePackManager::UpdatePacksForOobe(
       "es-es", base::BindOnce(&LanguagePackManagerTest::OobeTestCallback,
