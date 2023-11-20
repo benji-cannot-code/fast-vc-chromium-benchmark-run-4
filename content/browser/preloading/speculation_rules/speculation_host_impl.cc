5b40d21942e0bb3e3adfe0a704a039087a9866c4
Liviu Tinta <liviutinta@chromium.org>
2023-11-20 21:09:44 +0000

    Ship No-Vary-Search header and hint
    The way these two features interact is:
    - No-Vary-Search response header allows the prefetch cache to use
    inexact matching if the prefetch already has received head at time
    of navigation.
    - No-Vary-Search hint allows the navigation to wait for the head
    of the in progress prefetch that has inexact match with navigation
    URL based on its No-Vary-Search hint.
    
    In order to ship we use an approach similar to
    go/graduating-from-finch#optional-leave-a-finch-hook to allow
    unshipping and return to Origin Trial by using Finch config
    without requiring a binary re-spin of Chrome.
    
    The following are added:
    - a feature param enabled by default
    (kPrefetchNoVarySearchShippedByDefault) attached to the feature
    PrefetchNoVarySearch which marks No-Vary-Search response header
    as shipped by default. Disabling this feature param via Finch
    unships No-Vary-Search header.
    - a base feature for runtime enabled feature
    SpeculationRulesNoVarySearchHint disabled by default. This allows
    us to turn off No-Vary-Search hint functionality in the origin
    trial independently of No-Vary-Search response header
    functionality
    - a runtime enabled feature
    SpeculationRulesNoVarySearchHintShippedByDefault enabled by
    default which marks Speculation Rules No-Vary-Search hint as
    shipped by default. Disabling this runtime enabled feature
    via Finch unships Speculation Rules No-Vary-Search hint.
    
    The kill-switch functionality will work in the following ways:
    - to unship No-Vary-Search response header while allowing
    Origin Trial to continue: disable
    kPrefetchNoVarySearchShippedByDefault via Finch.
    - to unship Speculation Rules No-Vary-Search hint functionality
    while allowing Origin Trial to continue: disable
    SpeculationRulesNoVarySearchHintShippedByDefault via Finch.
    - to stop Speculation Rules No-Vary-Search hint functionality
    in the origin trial: disable both
    SpeculationRulesNoVarySearchHintShippedByDefault and
    SpeculationRulesNoVarySearchHint via Finch.
    - to unship both functionality for all including Origin Trial
    users: disable SpeculationRulesNoVarySearchHintShippedByDefault,
    SpeculationRulesNoVarySearchHint, PrefetchNoVarySearch via Finch.
    
    Links to Intents to Ship:
    - NVS hint: https://groups.google.com/a/chromium.org/g/blink-dev/c/TZSsl2iCuA4/m/5BVuLhoBCAAJ
    - NVS header: https://groups.google.com/a/chromium.org/g/blink-dev/c/XnG5BF3uoeE/m/th2iMQcBCAAJ
    
    Bug: 1416659, 1378075
    Change-Id: I4d57417e3b64c00ad638499b4eafef304155d162
    Low-Coverage-Reason: TRIVIAL_CHANGE: Renaming PrefetchDocumentManager::EnableNoVarySearchSupport to EnableNoVarySearchSupportFromOriginTrial.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5014325
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Liviu Tinta <liviutinta@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227011}

--- content/browser/preloading/speculation_rules/speculation_host_impl.cc
+++ content/browser/preloading/speculation_rules/speculation_host_impl.cc
@@ -82,7 +82,7 @@ void SpeculationHostImpl::EnableNoVarySearchSupport() {
       PrefetchDocumentManager::GetOrCreateForCurrentDocument(
           &render_frame_host());
   CHECK(prefetch_document_manager);
-  prefetch_document_manager->EnableNoVarySearchSupport();
+  prefetch_document_manager->EnableNoVarySearchSupportFromOriginTrial();
 }
 
 void SpeculationHostImpl::InitiatePreview(const GURL& url) {
