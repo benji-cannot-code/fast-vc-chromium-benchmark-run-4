6a4b67415731f1b6ed49a22c1f475384cc3be987
Florian Leimgruber <fleimgruber@google.com>
2023-11-20 15:04:11 +0000

    Simplify form import requirement checking + metrics
    Currently, FormDataImporter::LogAddressFormImportRequirementMetric()
    is responsible for:
    - Checking if the profile meets the minimum import requirements
    - Logging this to autofill-internals.
    - Emitting UMA metrics around it.
    
    This leads to interesting looking lines like this:
      bool all_fulfilled = LogAddressFormImportRequirementMetric(
                                 candidate_profile, import_log_buffer) &&
                             !has_invalid_information;
    
    This CL:
    - Removes FormDataImporter::LogAddressFormImportRequirementMetric().
    - Instead uses the existing IsMinimumAddress() function from
      profile_requirement_utils.h to compute `all_fulfilled`.
    - Adds an optional LogBuffer parameter to IsMinimumAddress(), making
      it responsible to log to autofill-internals.
    - Adds autofill_metrics::LogAddressFormImportRequirementMetric(),
      which logs UMA metrics around the import requirements, essentially
      what is currently done in
      FormDataImporter::LogAddressFormImportRequirementMetric().
    - Merges LogAddressFormImportCountrySpecificFieldRequirementsMetric()
      into the new LogAddressFormImportRequirementMetric().
    
    The Cl also makes some cosmetical changes in profile_requirement_utils:
    - Renames GetAutofillProfileRequirementResult() to
      ValidateProfileImportRequirements().
    - Moves kMinimumAddressRequirementViolations to an anonymous namespace
      in the cc file.
    - Rephrases some of the comments.
    
    Some technical notes:
    - By splitting the metric emission and the actual completeness check,
      requirements are checked twice now. Arguably, this is worth the better
      separation of the logic.
    - Currently, the logic constructs a flat_set from the vector of
      requirements, to efficiently check whether some of them are violated.
      Since only ~4-ish requirements are explicitly checked that way, this
      CL instead just uses base::Contains(). This is likely faster than
      sorting.
    
    Bug: 1503645
    Change-Id: Iae2d734e53d2efac243932f7752e58052daa786d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5040498
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1226813}

--- components/autofill/core/browser/metrics/profile_import_metrics.cc
+++ components/autofill/core/browser/metrics/profile_import_metrics.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/core/browser/metrics/profile_import_metrics.h"
 
+#include "base/containers/contains.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/strcat.h"
 #include "components/autofill/core/browser/metrics/autofill_metrics_utils.h"
+#include "components/autofill/core/browser/profile_requirement_utils.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 
 namespace autofill::autofill_metrics {
@@ -71,11 +73,25 @@ void LogAddressFormImportRequirementMetric(
                                 metric);
 }
 
-void LogAddressFormImportCountrySpecificFieldRequirementsMetric(
-    bool is_zip_missing,
-    bool is_state_missing,
-    bool is_city_missing,
-    bool is_line1_missing) {
+void LogAddressFormImportRequirementMetric(const AutofillProfile& profile) {
+  std::vector<AddressProfileImportRequirementMetric> requirements =
+      ValidateProfileImportRequirements(profile);
+  for (AddressProfileImportRequirementMetric& requirement : requirements) {
+    LogAddressFormImportRequirementMetric(requirement);
+  }
+
+  bool is_zip_missing = base::Contains(
+      requirements,
+      AddressProfileImportRequirementMetric::kZipRequirementViolated);
+  bool is_state_missing = base::Contains(
+      requirements,
+      AddressProfileImportRequirementMetric::kStateRequirementViolated);
+  bool is_city_missing = base::Contains(
+      requirements,
+      AddressProfileImportRequirementMetric::kCityRequirementViolated);
+  bool is_line1_missing = base::Contains(
+      requirements,
+      AddressProfileImportRequirementMetric::kLine1RequirementViolated);
   const auto metric =
       static_cast<AddressProfileImportCountrySpecificFieldRequirementsMetric>(
           (is_zip_missing ? 0b1 : 0) | (is_state_missing ? 0b10 : 0) |
