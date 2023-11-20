097e96499af9fb1dac7e86142bf7a2c86a45e81f
Adithya Srinivasan <adithyas@chromium.org>
2023-11-20 16:48:48 +0000

    Prefetch: Enable PrefetchNewLimits by default
    Enables the feature by default and removes the
    fieldtrial_testing_config for the feature; in preparation roll out to
    100% of Stable users. See bug for Stable experiment results.
    
    This also includes a test fix in preloading_decider_unittest.cc;
    we remove an explicit call to
    PreloadingDecider::GetForCurrentDocument() as that method is called
    in ~PrefetchContainer() when ResetPrefetch is called. The test
    using it should have been guarded behind PrefetchNewLimits to begin
    with (CandidateCanBeReprefetchedAfterDiscard).
    
    Bug: 1445086
    Change-Id: Idcafd011686b6a2b2c56208fb18f21bc74e495f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5037590
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226868}

--- content/browser/preloading/prefetch/prefetch_service_unittest.cc
+++ content/browser/preloading/prefetch/prefetch_service_unittest.cc
@@ -2375,7 +2375,8 @@ class PrefetchServiceLimitedPrefetchesTest : public PrefetchServiceTest {
           {{"ineligible_decoy_request_probability", "0"},
            {"prefetch_container_lifetime_s", "-1"},
            {"max_srp_prefetches", "2"}}}},
-        {network::features::kPrefetchNoVarySearch});
+        {network::features::kPrefetchNoVarySearch,
+         ::features::kPrefetchNewLimits});
   }
 };
 
@@ -5302,7 +5303,7 @@ class PrefetchServiceWaitForMultiplePrefetchesBlockedUntilHeadTest
           }},
          // For this test class we need to enable kPrefetchNoVarySearch.
          {network::features::kPrefetchNoVarySearch, {}}},
-        {});
+        {::features::kPrefetchNewLimits});
   }
 };
 
