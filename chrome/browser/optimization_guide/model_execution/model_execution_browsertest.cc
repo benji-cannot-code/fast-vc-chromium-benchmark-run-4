4e95cb6b25db8a2ce4caa23232e48b9d6f9fd32a
Sreeja Kamishetty <sreejakshetty@chromium.org>
2023-11-20 18:39:27 +0000

    Reland "Add model request response logging with model quality entry"
    This is a reland of commit b2b483a665eb3d87f3d257a96e75528b72379ebb
    
    Original change's description:
    > Add model request response logging with model quality entry
    >
    > This CL integrates the ModelExecutionManager::ExecuteModel and
    > ModelExecutionManager::OnModelExecuteResponse which stores the request
    > and response into the LogAiDataRequest.
    >
    > We add templated helpers to get corresponding Request and Response. We pass the ModelQualityLogEntry which owns LogAiDataRequest to the feature api to log quality metrics and upload the data.
    >
    > Bug: b/301301447
    > Change-Id: I2958b97e8d0c82d11d4d8afaaa6cf5e49a30d865
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4982225
    > Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    > Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    > Commit-Queue: Sreeja Kamishetty <sreejakshetty@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1219699}
    
    Bug: b/301301447
    Change-Id: Ic31ae3f75fc985cf69c14b09f97c1baaff90bf1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5004823
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226921}

--- chrome/browser/optimization_guide/model_execution/model_execution_browsertest.cc
+++ chrome/browser/optimization_guide/model_execution/model_execution_browsertest.cc
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/optimization_guide_features.h"
 #include "components/optimization_guide/core/optimization_guide_logger.h"
 #include "components/optimization_guide/core/optimization_guide_switches.h"
+#include "components/optimization_guide/core/optimization_guide_util.h"
+#include "components/optimization_guide/proto/model_quality_service.pb.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "net/dns/mock_host_resolver.h"
@@ -33,19 +35,29 @@ enum class ModelExecutionRemoteResponseType {
   kSuccessful = 0,
   kUnsuccessful = 1,
   kMalformed = 2,
+  kErrorFiltered = 3,
+  kUnsupportedLanguage = 4,
 };
 
-TestMessage BuildTestMessage(const std::string& test_message_str) {
-  TestMessage test_message;
-  test_message.set_test(test_message_str);
-  return test_message;
-}
-
-proto::ExecuteResponse BuildTestExecuteResponse(const TestMessage& message) {
+proto::ExecuteResponse BuildComposeResponse(const std::string& output) {
+  proto::ComposeResponse compose_response;
+  compose_response.set_output(output);
   proto::ExecuteResponse execute_response;
   proto::Any* any_metadata = execute_response.mutable_response_metadata();
-  any_metadata->set_type_url("type.googleapis.com/" + message.GetTypeName());
-  message.SerializeToString(any_metadata->mutable_value());
+  any_metadata->set_type_url("type.googleapis.com/" +
+                             compose_response.GetTypeName());
+  compose_response.SerializeToString(any_metadata->mutable_value());
+  auto response_data =
+      optimization_guide::ParsedAnyMetadata<proto::ComposeResponse>(
+          *any_metadata);
+  EXPECT_TRUE(response_data);
+  return execute_response;
+}
+
+proto::ExecuteResponse BuildTestErrorExecuteResponse(
+    const proto::ErrorState& state) {
+  proto::ExecuteResponse execute_response;
+  execute_response.mutable_error_response()->set_error_state(state);
   return execute_response;
 }
 
@@ -126,7 +138,7 @@ class ModelExecutionBrowserTestBase : public InProcessBrowserTest {
   // Executes the model for the feature, waits until the response is received,
   // and returns the response.
   void ExecuteModel(proto::ModelExecutionFeature feature,
-                    const TestMessage& request_metadata,
+                    const google::protobuf::MessageLite& request_metadata,
                     Profile* profile = nullptr) {
     if (!profile) {
       profile = browser()->profile();
@@ -153,7 +165,27 @@ class ModelExecutionBrowserTestBase : public InProcessBrowserTest {
       base::OnceClosure on_model_execution_closure,
       OptimizationGuideModelExecutionResult result,
       std::unique_ptr<ModelQualityLogEntry> log_entry) {
+    if (result.has_value() ||
+        result.error().error() == OptimizationGuideModelExecutionError::
+                                      ModelExecutionError::kFiltered ||
+        result.error().error() ==
+            OptimizationGuideModelExecutionError::ModelExecutionError::
+                kUnsupportedLanguage) {
+      EXPECT_TRUE(log_entry);
+      proto::LogAiDataRequest* log_ai_data_request =
+          log_entry.get()->log_ai_data_request();
+      EXPECT_NE(log_ai_data_request, nullptr);
+      EXPECT_EQ(log_ai_data_request->feature_case(),
+                proto::LogAiDataRequest::FeatureCase::kCompose);
+      EXPECT_TRUE(log_ai_data_request->has_compose());
+      EXPECT_TRUE(log_ai_data_request->mutable_compose()->has_request_data());
+    }
+
     if (result.has_value()) {
+      EXPECT_TRUE(log_entry.get()
+                      ->log_ai_data_request()
+                      ->mutable_compose()
+                      ->has_response_data());
       model_execution_result_ = base::ok(result.value());
     } else {
       model_execution_result_ = base::unexpected(result.error());
@@ -178,7 +210,7 @@ class ModelExecutionBrowserTestBase : public InProcessBrowserTest {
     if (response_type_ == ModelExecutionRemoteResponseType::kSuccessful) {
       std::string serialized_response;
       proto::ExecuteResponse execute_response =
-          BuildTestExecuteResponse(BuildTestMessage("foo response"));
+          BuildComposeResponse("foo response");
       execute_response.SerializeToString(&serialized_response);
       response->set_code(net::HTTP_OK);
       response->set_content(serialized_response);
@@ -188,6 +220,22 @@ class ModelExecutionBrowserTestBase : public InProcessBrowserTest {
     } else if (response_type_ == ModelExecutionRemoteResponseType::kMalformed) {
       response->set_code(net::HTTP_OK);
       response->set_content("Not a proto");
+    } else if (response_type_ ==
+               ModelExecutionRemoteResponseType::kErrorFiltered) {
+      std::string serialized_response;
+      proto::ExecuteResponse execute_response = BuildTestErrorExecuteResponse(
+          proto::ErrorState::ERROR_STATE_FILTERED);
+      execute_response.SerializeToString(&serialized_response);
+      response->set_code(net::HTTP_OK);
+      response->set_content(serialized_response);
+    } else if (response_type_ ==
+               ModelExecutionRemoteResponseType::kUnsupportedLanguage) {
+      std::string serialized_response;
+      proto::ExecuteResponse execute_response = BuildTestErrorExecuteResponse(
+          proto::ErrorState::ERROR_STATE_UNSUPPORTED_LANGUAGE);
+      execute_response.SerializeToString(&serialized_response);
+      response->set_code(net::HTTP_OK);
+      response->set_content(serialized_response);
     } else {
       NOTREACHED();
     }
@@ -231,7 +279,9 @@ class ModelExecutionDisabledBrowserTest : public ModelExecutionBrowserTestBase {
 
 IN_PROC_BROWSER_TEST_F(ModelExecutionDisabledBrowserTest,
                        ModelExecutionDisabled) {
-  ExecuteModel(proto::MODEL_EXECUTION_FEATURE_COMPOSE, BuildTestMessage("foo"));
+  proto::ComposeRequest request;
+  request.set_user_input("a user typed this");
+  ExecuteModel(proto::MODEL_EXECUTION_FEATURE_COMPOSE, request);
   EXPECT_TRUE(model_execution_result_.has_value());
   EXPECT_FALSE(model_execution_result_->has_value());
   EXPECT_EQ(OptimizationGuideModelExecutionError::ModelExecutionError::
@@ -250,7 +300,9 @@ class ModelExecutionEnabledBrowserTest : public ModelExecutionBrowserTestBase {
 IN_PROC_BROWSER_TEST_F(ModelExecutionEnabledBrowserTest,
                        ModelExecutionDisabledInIncognito) {
   Browser* otr_browser = CreateIncognitoBrowser(browser()->profile());
-  ExecuteModel(proto::MODEL_EXECUTION_FEATURE_COMPOSE, BuildTestMessage("foo"),
+  proto::ComposeRequest request;
+  request.set_user_input("a user typed this");
+  ExecuteModel(proto::MODEL_EXECUTION_FEATURE_COMPOSE, request,
                otr_browser->profile());
   EXPECT_TRUE(model_execution_result_.has_value());
   EXPECT_FALSE(model_execution_result_->has_value());
@@ -262,7 +314,9 @@ IN_PROC_BROWSER_TEST_F(ModelExecutionEnabledBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(ModelExecutionEnabledBrowserTest,
                        ModelExecutionFailsNoUserSignIn) {
-  ExecuteModel(proto::MODEL_EXECUTION_FEATURE_COMPOSE, BuildTestMessage("foo"));
+  proto::ComposeRequest request;
+  request.set_user_input("a user typed this");
+  ExecuteModel(proto::MODEL_EXECUTION_FEATURE_COMPOSE, request);
   EXPECT_TRUE(model_execution_result_.has_value());
   EXPECT_FALSE(model_execution_result_->has_value());
   EXPECT_EQ(OptimizationGuideModelExecutionError::ModelExecutionError::
@@ -276,12 +330,14 @@ IN_PROC_BROWSER_TEST_F(ModelExecutionEnabledBrowserTest,
   EnableSignin();
   SetExpectedBearerAccessToken("Bearer access_token");
 
-  ExecuteModel(proto::MODEL_EXECUTION_FEATURE_COMPOSE, BuildTestMessage("foo"));
+  proto::ComposeRequest request;
+  request.set_user_input("a user typed this");
+  ExecuteModel(proto::MODEL_EXECUTION_FEATURE_COMPOSE, request);
   EXPECT_TRUE(model_execution_result_.has_value());
   EXPECT_TRUE(model_execution_result_->has_value());
-  EXPECT_EQ(
-      "foo response",
-      ParsedAnyMetadata<TestMessage>(model_execution_result_->value())->test());
+  auto response = ParsedAnyMetadata<proto::ComposeResponse>(
+      model_execution_result_->value());
+  EXPECT_EQ("foo response", response->output());
 }
 
 IN_PROC_BROWSER_TEST_F(ModelExecutionEnabledBrowserTest,
@@ -290,7 +346,9 @@ IN_PROC_BROWSER_TEST_F(ModelExecutionEnabledBrowserTest,
   SetExpectedBearerAccessToken("Bearer access_token");
   SetResponseType(ModelExecutionRemoteResponseType::kUnsuccessful);
 
-  ExecuteModel(proto::MODEL_EXECUTION_FEATURE_COMPOSE, BuildTestMessage("foo"));
+  proto::ComposeRequest request;
+  request.set_user_input("a user typed this");
+  ExecuteModel(proto::MODEL_EXECUTION_FEATURE_COMPOSE, request);
   EXPECT_TRUE(model_execution_result_.has_value());
   EXPECT_FALSE(model_execution_result_->has_value());
   EXPECT_EQ(OptimizationGuideModelExecutionError::ModelExecutionError::
@@ -305,13 +363,47 @@ IN_PROC_BROWSER_TEST_F(ModelExecutionEnabledBrowserTest,
   SetExpectedBearerAccessToken("Bearer access_token");
   SetResponseType(ModelExecutionRemoteResponseType::kMalformed);
 
-  ExecuteModel(proto::MODEL_EXECUTION_FEATURE_COMPOSE, BuildTestMessage("foo"));
+  proto::ComposeRequest request;
+  request.set_user_input("a user typed this");
+  ExecuteModel(proto::MODEL_EXECUTION_FEATURE_COMPOSE, request);
   EXPECT_TRUE(model_execution_result_.has_value());
   EXPECT_FALSE(model_execution_result_->has_value());
   EXPECT_EQ(OptimizationGuideModelExecutionError::ModelExecutionError::
                 kGenericFailure,
             model_execution_result_->error().error());
   EXPECT_TRUE(model_execution_result_->error().transient());
+}
+
+IN_PROC_BROWSER_TEST_F(ModelExecutionEnabledBrowserTest,
+                       ModelExecutionFailsForErrorFilteredResponse) {
+  EnableSignin();
+  SetExpectedBearerAccessToken("Bearer access_token");
+  SetResponseType(ModelExecutionRemoteResponseType::kErrorFiltered);
+
+  proto::ComposeRequest request;
+  request.set_user_input("a user typed this");
+  ExecuteModel(proto::MODEL_EXECUTION_FEATURE_COMPOSE, request);
+  EXPECT_TRUE(model_execution_result_.has_value());
+  EXPECT_FALSE(model_execution_result_->has_value());
+  EXPECT_EQ(
+      OptimizationGuideModelExecutionError::ModelExecutionError::kFiltered,
+      model_execution_result_->error().error());
+}
+
+IN_PROC_BROWSER_TEST_F(ModelExecutionEnabledBrowserTest,
+                       ModelExecutionFailsForUnsupportedLanguageResponse) {
+  EnableSignin();
+  SetExpectedBearerAccessToken("Bearer access_token");
+  SetResponseType(ModelExecutionRemoteResponseType::kUnsupportedLanguage);
+
+  proto::ComposeRequest request;
+  request.set_user_input("a user typed this");
+  ExecuteModel(proto::MODEL_EXECUTION_FEATURE_COMPOSE, request);
+  EXPECT_TRUE(model_execution_result_.has_value());
+  EXPECT_FALSE(model_execution_result_->has_value());
+  EXPECT_EQ(OptimizationGuideModelExecutionError::ModelExecutionError::
+                kUnsupportedLanguage,
+            model_execution_result_->error().error());
 }
 
 class ModelExecutionInternalsPageBrowserTest
@@ -336,7 +428,9 @@ IN_PROC_BROWSER_TEST_F(ModelExecutionInternalsPageBrowserTest,
   EnableSignin();
   SetExpectedBearerAccessToken("Bearer access_token");
 
-  ExecuteModel(proto::MODEL_EXECUTION_FEATURE_COMPOSE, BuildTestMessage("foo"));
+  proto::ComposeRequest request;
+  request.set_user_input("foo");
+  ExecuteModel(proto::MODEL_EXECUTION_FEATURE_COMPOSE, request);
   EXPECT_TRUE(model_execution_result_.has_value());
   EXPECT_TRUE(model_execution_result_->has_value());
   CheckInternalsLog("ExecuteModel");
--- components/optimization_guide/core/model_execution/model_execution_manager.h
+++ components/optimization_guide/core/model_execution/model_execution_manager.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/optimization_guide_features.h"
 #include "components/optimization_guide/core/optimization_guide_model_executor.h"
 #include "components/optimization_guide/proto/model_execution.pb.h"
+#include "components/optimization_guide/proto/model_quality_service.pb.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "services/on_device_model/public/mojom/on_device_model.mojom.h"
 #include "url/gurl.h"
@@ -60,6 +61,7 @@ class ModelExecutionManager {
   // Invoked when the model execution result is available.
   void OnModelExecuteResponse(
       proto::ModelExecutionFeature feature,
+      std::unique_ptr<proto::LogAiDataRequest> log_ai_data_request,
       OptimizationGuideModelExecutionResultCallback callback,
       base::expected<const proto::ExecuteResponse,
                      OptimizationGuideModelExecutionError> execute_response);
