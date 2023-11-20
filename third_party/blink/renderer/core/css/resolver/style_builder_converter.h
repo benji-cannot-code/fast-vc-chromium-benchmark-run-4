3e9c4de5b94294f4f66c5b3d8edd69261111dbdd
Rune Lillesveen <futhark@chromium.org>
2023-11-20 15:16:09 +0000

    Parsing and computed style support for inset-area
    The property does not have any effects on computed/used inset properties
    yet.
    
    Bug: 1477314
    Change-Id: I9a59eba5e5a9e21281306fe1be0008b1502eab11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5033631
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226819}

--- third_party/blink/renderer/core/css/resolver/style_builder_converter.h
+++ third_party/blink/renderer/core/css/resolver/style_builder_converter.h
@@ -376,6 +376,8 @@ class StyleBuilderConverter {
                                                     const CSSValue&);
   static ScopedCSSNameList* ConvertTimelineScope(StyleResolverState&,
                                                  const CSSValue&);
+
+  static InsetArea ConvertInsetArea(StyleResolverState&, const CSSValue&);
 };
 
 template <typename T>
