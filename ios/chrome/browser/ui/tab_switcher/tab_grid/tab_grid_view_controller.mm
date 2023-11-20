2cad0022915fc26e0d96476e860b813e12ba34bc
Louis Romero <lpromero@google.com>
2023-11-20 16:26:16 +0000

    Add view controller containment calls for grid containers
    Grid containers were not part of the view controller hierarchy when
    introduced in crrev/c/4946712.
    
    Bug: 1485895
    Change-Id: Iab757769561064f81c19ae4fdf8021e06512d124
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5040520
    Auto-Submit: Louis Romero <lpromero@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226860}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_view_controller.mm
@@ -1009,6 +1009,9 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
   scrollView.contentInsetAdjustmentBehavior =
       UIScrollViewContentInsetAdjustmentNever;
 
+  [self addChildViewController:self.incognitoGridContainerViewController];
+  [self addChildViewController:self.regularGridContainerViewController];
+  [self addChildViewController:self.remoteGridContainerViewController];
   UIStackView* gridsStack = [[UIStackView alloc] initWithArrangedSubviews:@[
     self.incognitoGridContainerViewController.view,
     self.regularGridContainerViewController.view,
@@ -1019,6 +1022,10 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
 
   [scrollView addSubview:gridsStack];
   [self.view addSubview:scrollView];
+  [self.incognitoGridContainerViewController
+      didMoveToParentViewController:self];
+  [self.regularGridContainerViewController didMoveToParentViewController:self];
+  [self.remoteGridContainerViewController didMoveToParentViewController:self];
 
   self.scrollView = scrollView;
   self.scrollView.scrollEnabled = YES;
