a7436fc10bb2d9964cd1f48f2ce71071e8a40753
Juan Mojica <juanmojica@google.com>
2023-11-20 18:02:09 +0000

    Fix unbranded unit tests to work when branded companion feature is enabled.
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Bug: b:310047213
    Change-Id: I5657956e879e28065dd0b5a5b16756a6421ca24a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5035004
    Reviewed-by: Caroline Rising <corising@google.com>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1226899}

--- chrome/browser/ui/views/side_panel/side_panel_coordinator_unittest.cc
+++ chrome/browser/ui/views/side_panel/side_panel_coordinator_unittest.cc
@@ -81,11 +81,10 @@ std::unique_ptr<KeyedService> BuildSidePanelService(
 class SidePanelCoordinatorTest : public TestWithBrowserView {
  public:
   void SetUp() override {
-    SetUpFeatureList();
     TestWithBrowserView::SetUp();
 
-    AddTab(browser_view()->browser(), GURL("http://foo1.com"));
-    AddTab(browser_view()->browser(), GURL("http://foo2.com"));
+    AddTabToBrowser(GURL("http://foo1.com"));
+    AddTabToBrowser(GURL("http://foo2.com"));
 
     // Add a kSideSearch entry to the contextual registry for the first tab.
     browser_view()->browser()->tab_strip_model()->ActivateTabAt(0);
@@ -138,14 +137,6 @@ class SidePanelCoordinatorTest : public TestWithBrowserView {
               SidePanelEntry::Id::kSideSearch);
   }
 
-  virtual void SetUpFeatureList() {
-    // TODO(b/310047213): Fix tests from failing when companion enabled.
-    // Companion adds a contextual entry when the browser starts. Disable to
-    // prevent.
-    feature_list_.InitAndDisableFeature(
-        companion::features::internal::kSidePanelCompanion);
-  }
-
   void VerifyEntryExistenceAndValue(absl::optional<SidePanelEntry*> entry,
                                     SidePanelEntry::Id id) {
     ASSERT_TRUE(entry.has_value());
@@ -174,6 +165,15 @@ class SidePanelCoordinatorTest : public TestWithBrowserView {
 
   bool ComboboxViewExists() {
     return coordinator_->header_combobox_ != nullptr;
+  }
+
+  void AddTabToBrowser(const GURL& tab_url) {
+    AddTab(browser_view()->browser(), tab_url);
+    // Remove the companion entry if it present.
+    auto* registry =
+        SidePanelRegistry::Get(browser_view()->GetActiveWebContents());
+    registry->Deregister(
+        SidePanelEntry::Key(SidePanelEntry::Id::kSearchCompanion));
   }
 
  protected:
@@ -1764,6 +1764,7 @@ TEST_F(SidePanelCoordinatorTest, DeregisterAndReturnView) {
 class SidePanelPinningCoordinatorTest : public SidePanelCoordinatorTest {
  public:
   void SetUp() override {
+    scoped_feature_list_.InitWithFeatures({features::kSidePanelPinning}, {});
     SidePanelCoordinatorTest::SetUp();
     content::WebContents* const web_contents =
         browser_view()->browser()->tab_strip_model()->GetWebContentsAt(0);
@@ -1801,13 +1802,6 @@ class SidePanelPinningCoordinatorTest : public SidePanelCoordinatorTest {
         base::BindRepeating(
             &SidePanelPinningCoordinatorTest::BuildPinnedToolbarActionsModel));
     return factories;
-  }
-
-  void SetUpFeatureList() override {
-    // TODO(b/310047213): Fix tests from failing when companion enabled.
-    scoped_feature_list_.InitWithFeatures(
-        {features::kSidePanelPinning},
-        {companion::features::internal::kSidePanelCompanion});
   }
 
   static std::unique_ptr<KeyedService> BuildPinnedToolbarActionsModel(
@@ -1965,8 +1959,8 @@ class SidePanelCoordinatorLoadingContentTest : public SidePanelCoordinatorTest {
   void SetUp() override {
     TestWithBrowserView::SetUp();
 
-    AddTab(browser_view()->browser(), GURL("http://foo1.com"));
-    AddTab(browser_view()->browser(), GURL("http://foo2.com"));
+    AddTabToBrowser(GURL("http://foo1.com"));
+    AddTabToBrowser(GURL("http://foo2.com"));
 
     coordinator_ = SidePanelUtil::GetSidePanelCoordinatorForBrowser(browser());
     global_registry_ = SidePanelCoordinator::GetGlobalSidePanelRegistry(
