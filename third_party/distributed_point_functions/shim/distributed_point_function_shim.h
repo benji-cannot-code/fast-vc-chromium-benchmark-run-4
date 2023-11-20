f0ffff0394439a4fd5a078686365c523aca9bea3
Dan McArdle <dmcardle@chromium.org>
2023-11-20 17:10:21 +0000

    Log status when squashing absl::StatusOr into std::optional
    In the distributed_point_functions shim, we squash absl::StatusOr into
    std::optional. This CL adds logging so we aren't silently dropping
    statuses in the error branches.
    
    I tried to align the use of absl::StatusOr with the style used in the
    Abseil documentation: <https://abseil.io/docs/cpp/guides/status>.
    
    Bug: 1499970
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED https://crbug.com/1501035
    Change-Id: I7a4dc6df111096d7546e5c2e946299ece824d2f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5015283
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226882}

--- third_party/distributed_point_functions/shim/distributed_point_function_shim.h
+++ third_party/distributed_point_functions/shim/distributed_point_function_shim.h
@@ -13,10 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/numeric/int128.h"
 #include "third_party/distributed_point_functions/dpf/distributed_point_function.pb.h"
 
-// This header is the interface between Chromium code and the third-party
-// distributed_point_functions library. For more info, see the comment in
-// ../BUILD.gn above the //third_party/distributed_point_functions target.
-
 namespace distributed_point_functions {
 
 // Generates a pair of keys for a DPF that evaluates to `beta` when given
