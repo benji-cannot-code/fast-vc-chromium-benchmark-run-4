b83a322710f8cfc1cd0d465fcfce320a46f5e09d
Vivian Pao <vpao@google.com>
2023-11-20 22:49:49 +0000

    Revert "Use kCrOsStandardIcon effects for promise icons"
    This reverts commit daac169f38fe82b0aa22f7a648ed4b857195b915.
    
    Reason for revert: All promise icons started being square icons sitting inside the circular frame (b/312101947)
    
    Original change's description:
    > Use kCrOsStandardIcon effects for promise icons
    >
    > This aligns better with effects that will be applied to the installed
    > app icon, making the promise icon closer to the final app icon shown in
    > shelf and launcher.
    >
    > BUG=b:261907495
    >
    > Change-Id: I09b7b6404b5d1ef74fd1212369bd71987dfecb47
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5028055
    > Reviewed-by: Vivian Pao <vpao@google.com>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1225051}
    
    BUG=b:261907495
    
    Change-Id: If6aec640d522a868a36ff41a063dc37e77a9bfd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5039683
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Vivian Pao <vpao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1227079}

--- chrome/browser/ash/app_list/app_service/app_service_promise_app_icon_loader.cc
+++ chrome/browser/ash/app_list/app_service/app_service_promise_app_icon_loader.cc
@@ -63,7 +63,7 @@ void AppServicePromiseAppIconLoader::FetchImage(const std::string& id) {
     return;
   }
   CallLoadIcon(apps::PackageId::FromString(id).value(),
-               apps::IconEffects::kCrOsStandardIcon);
+               apps::IconEffects::kCrOsStandardMask);
 }
 
 void AppServicePromiseAppIconLoader::ClearImage(const std::string& id) {
