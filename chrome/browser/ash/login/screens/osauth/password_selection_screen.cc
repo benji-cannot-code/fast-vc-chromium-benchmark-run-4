9bd3caaa98fc52cc3dc3156b7b737b9e8c1fc16b
Denis Kuznetsov <antrim@chromium.org>
2023-11-20 16:12:51 +0000

    Let PasswordSelectionScreen handlle support recovery flow
    With Local passwords we will need to detect which password need to
    be reset as a part of recovery flow. We already have screen with
    similar logic, but during recovery this should happen automatically.
    
    Bug: b/291808450
    Change-Id: I97af9b1bbd6e097e82a23f9f70595428380fed11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5040409
    Auto-Submit: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226849}

--- chrome/browser/ash/login/screens/osauth/password_selection_screen.cc
+++ chrome/browser/ash/login/screens/osauth/password_selection_screen.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/login/auth/auth_factor_editor.h"
 #include "chromeos/ash/components/login/auth/public/user_context.h"
 #include "chromeos/ash/services/auth_factor_config/auth_factor_config.h"
+#include "chromeos/ash/services/auth_factor_config/auth_factor_config_utils.h"
 #include "chromeos/ash/services/auth_factor_config/in_process_instances.h"
 #include "chromeos/ash/services/auth_factor_config/password_factor_editor.h"
 
@@ -125,6 +126,25 @@ void PasswordSelectionScreen::ProcessOptions() {
     LOG(WARNING) << "Login using Smartcard, no password should be set up";
     exit_callback_.Run(Result::NOT_APPLICABLE);
     return;
+  }
+
+  // If user is going through recovery, detect which password need to be updated
+  // and return immediately.
+  if (context()->knowledge_factor_setup.auth_setup_flow ==
+      WizardContext::AuthChangeFlow::kRecovery) {
+    CHECK(auth_factors_config_.HasConfiguredFactor(
+        cryptohome::AuthFactorType::kPassword))
+        << "User need to have a password that should be updated";
+    if (auth::IsLocalPassword(*auth_factors_config_.FindFactorByType(
+            cryptohome::AuthFactorType::kPassword))) {
+      exit_callback_.Run(Result::LOCAL_PASSWORD_FORCED);
+      return;
+    } else {
+      CHECK(auth::IsGaiaPassword(*auth_factors_config_.FindFactorByType(
+          cryptohome::AuthFactorType::kPassword)));
+      exit_callback_.Run(Result::GAIA_PASSWORD_FALLBACK);
+      return;
+    }
   }
 
   if (context()->skip_post_login_screens_for_tests) {
