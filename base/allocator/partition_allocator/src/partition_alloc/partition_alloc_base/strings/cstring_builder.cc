9bb79edab435a69370f38e9c3077d025d1898b3f
Takashi Sakamoto <tasak@google.com>
2023-11-20 03:18:34 +0000

    [PA] Fix missing #include <limits>
    std::numeric_limits needs <limits> header.
    
    Bug: 1503397
    Change-Id: Ic2259e9363a43c2f5a85c48ac8c6f8df2e452fe5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5042728
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226657}

--- base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/strings/cstring_builder.cc
+++ base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/strings/cstring_builder.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cmath>
 #include <cstring>
+#include <limits>
 
 #if BUILDFLAG(PA_DCHECK_IS_ON)
 #include "partition_alloc/partition_alloc_base/check.h"
