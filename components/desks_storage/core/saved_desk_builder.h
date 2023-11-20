7b9d4d375360b0d6397dabe7e98c8f9050040fc6
Daniel Andersson <dandersson@chromium.org>
2023-11-20 19:35:57 +0000

    desk_profiles: Add lacros profile IDs to template conversion
    This CL adds support for handling lacros profile IDs in DeskTemplate
    conversion functions. These functions are used to convert between
    ash::DeskTemplate and a base::Value.
    
    A lacros profile ID can occur in two places when saving a desk:
    
      1. The desk template itself.
      2. Individual lacros browser windows in the desk.
    
    BUG=b:308823936
    
    Change-Id: I059ebe73716f78e2a35df1e676424e551ca9cb11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5026494
    Reviewed-by: Yanzhu Du <yzd@google.com>
    Commit-Queue: Daniel Andersson <dandersson@chromium.org>
    Reviewed-by: Avynn Donaghe <avynn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1226958}

--- components/desks_storage/core/saved_desk_builder.h
+++ components/desks_storage/core/saved_desk_builder.h
@@ -179,6 +179,7 @@ class SavedDeskBrowserBuilder {
   SavedDeskBrowserBuilder& SetActiveTabIndex(int index);
   SavedDeskBrowserBuilder& SetUrls(std::vector<GURL> urls);
   SavedDeskBrowserBuilder& SetIsLacros(bool is_lacros);
+  SavedDeskBrowserBuilder& SetLacrosProfileId(uint64_t lacros_profile_id);
   SavedDeskBrowserBuilder& AddTabGroupBuilder(
       SavedDeskTabGroupBuilder tab_group);
 
@@ -195,6 +196,7 @@ class SavedDeskBrowserBuilder {
   std::vector<SavedDeskTabGroupBuilder> tab_group_builders_;
   absl::optional<int> active_tab_index_;
   absl::optional<int> first_non_pinned_tab_index_;
+  absl::optional<uint64_t> lacros_profile_id_;
   std::vector<GURL> urls_;
 };
 
@@ -268,6 +270,9 @@ class SavedDeskBuilder {
   SavedDeskBuilder& SetPolicyShouldLaunchOnStartup(
       bool should_launch_on_startup);
 
+  // Sets the optional lacros profile association.
+  SavedDeskBuilder& SetLacrosProfileId(uint64_t lacros_profile_id);
+
   // Adds an app window.
   SavedDeskBuilder& AddAppWindow(BuiltApp built_app);
 
@@ -287,6 +292,7 @@ class SavedDeskBuilder {
   base::Time updated_time_;
   base::Value policy_value_;
   bool policy_should_launch_on_startup_ = false;
+  absl::optional<uint64_t> lacros_profile_id_;
   std::vector<BuiltApp> built_apps_;
 };
 
