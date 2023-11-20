1601ca29382548200d93c4f143805e1e97a91241
Florian Leimgruber <fleimgruber@google.com>
2023-11-20 17:00:45 +0000

    Add SyncDecoupleAddressPaymentSettings to field trial config
    Bug: 1435431
    Change-Id: Ifed4ea2245e6ad7137bd88544509d130893d9354
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5024102
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1226872}

--- chrome/browser/ui/views/autofill/payments/save_card_bubble_views_browsertest.cc
+++ chrome/browser/ui/views/autofill/payments/save_card_bubble_views_browsertest.cc
@@ -66,6 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/signin_buildflags.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/identity_test_utils.h"
+#include "components/sync/base/features.h"
 #include "components/sync/service/sync_service_impl.h"
 #include "components/sync/test/fake_server.h"
 #include "components/sync/test/fake_server_network_resources.h"
@@ -961,6 +962,10 @@ class SaveCardBubbleViewsSyncTransportFullFormBrowserTest
       disabled_features.push_back(
           features::kAutofillMoveLegalTermsAndIconForNewCardEnrollment);
     }
+    // Since server card saves upload address information, they are only offered
+    // when addresses are being synced. Enable CONTACT_INFO in transport mode.
+    enabled_features.push_back(
+        syncer::kSyncEnableContactInfoDataTypeInTransportMode);
     feature_list_.InitWithFeatures(enabled_features, disabled_features);
   }
 
