e0b5ad90fd88c7cddd4310dc92568164358e51ec
Alex Ilin <alexilin@chromium.org>
2023-11-20 15:26:41 +0000

    [DBSC] Expose GoogleURLLoaderThrottle::ShouldDeferRequestForBoundSession
    ShouldDeferRequestForBoundSession() is needed for implementing a custom
    throttle based on the same logic in https://crrev.com/c/4995066.
    
    Making this method public and static also improves the test clarity as
    we can test this function directly.
    
    Bug: b/311595637
    Change-Id: Idd42e91938de893a7320a0ffdc7c4b5484edcf52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5040495
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226826}

--- chrome/common/google_url_loader_throttle.h
+++ chrome/common/google_url_loader_throttle.h
@@ -41,6 +41,13 @@ class GoogleURLLoaderThrottle
   static void UpdateCorsExemptHeader(
       network::mojom::NetworkContextParams* params);
 
+#if BUILDFLAG(ENABLE_BOUND_SESSION_CREDENTIALS)
+  static bool ShouldDeferRequestForBoundSession(
+      const GURL& request_url,
+      chrome::mojom::BoundSessionThrottlerParams*
+          bound_session_throttler_params);
+#endif  // BUILDFLAG(ENABLE_BOUND_SESSION_CREDENTIALS)
+
   // blink::URLLoaderThrottle:
   void DetachFromCurrentSequence() override;
   void WillStartRequest(network::ResourceRequest* request,
@@ -59,7 +66,6 @@ class GoogleURLLoaderThrottle
 #endif
  private:
 #if BUILDFLAG(ENABLE_BOUND_SESSION_CREDENTIALS)
-  bool ShouldDeferRequestForBoundSession(const GURL& request_url) const;
   void OnDeferRequestForBoundSessionCompleted(
       BoundSessionRequestThrottledHandler::UnblockAction resume);
   void ResumeOrCancelRequest(
