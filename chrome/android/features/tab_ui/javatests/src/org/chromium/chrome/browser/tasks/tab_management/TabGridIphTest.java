18ab8657586f5b7b027a7a24c55bd321f033e27d
Calder Kitagawa <ckitagawa@chromium.org>
2023-11-21 22:47:18 +0000

    Disable flaky TabGridIphTest#testRenderIph_Landscape
    Bug: 1504246
    Change-Id: I877db825c1fc43e499ee01ffe45cd6fd5aefeeeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5051520
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227647}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridIphTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridIphTest.java
@@ -232,6 +232,7 @@ public class TabGridIphTest {
     @Test
     @MediumTest
     @Feature({"RenderTest"})
+    @DisabledTest(message = "https://crbug.com/1504246")
     public void testRenderIph_Landscape() throws IOException {
         ChromeTabbedActivity cta = mActivityTestRule.getActivity();
 
