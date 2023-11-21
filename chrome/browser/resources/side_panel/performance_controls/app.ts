b36c2df2c0192c7297ac807a70c18cff1e86696e
Charles Meng <charlesmeng@chromium.org>
2023-11-21 01:19:45 +0000

    [Performance Side Panel] Add placeholder battery/memory saver cards
    Cards have a placeholder title and are otherwise empty.
    
    Bug: 1502312
    Change-Id: I294b8e04737cd059ae8eaa8940b11162a739ce16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5046114
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Charles Meng <charlesmeng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227159}

--- chrome/browser/resources/side_panel/performance_controls/app.ts
+++ chrome/browser/resources/side_panel/performance_controls/app.ts
@@ -3,9 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '//performance-side-panel.top-chrome/shared/sp_shared_style.css.js';
+import 'chrome://performance-side-panel.top-chrome/shared/sp_shared_style.css.js';
+import './battery_saver_card.js';
+import './memory_saver_card.js';
 
-import {ColorChangeUpdater} from '//resources/cr_components/color_change_listener/colors_css_updater.js';
+import {ColorChangeUpdater} from 'chrome://resources/cr_components/color_change_listener/colors_css_updater.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './app.html.js';
---
+++ chrome/browser/resources/side_panel/performance_controls/battery_saver_card.ts
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://performance-side-panel.top-chrome/shared/sp_heading.js';
+import 'chrome://performance-side-panel.top-chrome/shared/sp_shared_style.css.js';
+
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {getTemplate} from './battery_saver_card.html.js';
+
+export interface BatterySaverCardElement {
+  $: {};
+}
+
+export class BatterySaverCardElement extends PolymerElement {
+  static get is() {
+    return 'battery-saver-card';
+  }
+
+  static get template() {
+    return getTemplate();
+  }
+
+  static get properties() {
+    return {};
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'battery-saver-card': BatterySaverCardElement;
+  }
+}
+customElements.define(BatterySaverCardElement.is, BatterySaverCardElement);
