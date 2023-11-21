5309e2a09ba802a3cee27a2e3eb60c70b1557515
Timofey Chudakov <tchudakov@google.com>
2023-11-21 19:16:09 +0000

    [AF] Remove GetDeletionConfirmationText from BrowserAutofillManager.
    This CL implements the principle that UI strings must be managed in
    the UI controllers. This CL also implements all missing tests.
    
    Next steps: check if AutofillPopupDelegate::GetDeletionConfirmationText
    is still needed or it should be removed.
    
    Bug: 1330108
    Change-Id: I5a224a17280bf510abb7925561ffa09822f4476a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5046553
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1227520}

--- components/autofill/core/browser/autofill_external_delegate.cc
+++ components/autofill/core/browser/autofill_external_delegate.cc
@@ -709,8 +709,7 @@ bool AutofillExternalDelegate::GetDeletionConfirmationText(
     Suggestion::BackendId backend_id,
     std::u16string* title,
     std::u16string* body) {
-  return manager_->GetDeletionConfirmationText(value, popup_item_id, backend_id,
-                                               title, body);
+  return false;
 }
 
 bool AutofillExternalDelegate::RemoveSuggestion(
