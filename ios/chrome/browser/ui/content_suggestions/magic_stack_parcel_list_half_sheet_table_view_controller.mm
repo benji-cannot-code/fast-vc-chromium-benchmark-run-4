dead0cf1182aae28967529b1982ee2bdf9e0b448
Hira Mahmood <hiramahmood@google.com>
2023-11-21 22:25:07 +0000

    [iOS] Fix misc parcel tracking accessibility bugs
    This CL:
    * Fixes the size of the ellipsis button in the  “see more” view
    * Adds an underline to the link in the opt-in screen
    * Fixes a bug where the title of the modules in the “see more” screen
    are cut off in large text sizes
    
    "Before" images are in the linked bugs.
    After "see more" screen:
    https://screenshot.googleplex.com/8ckqaaPp66PM2g9
    After opt-in screen:
    https://screenshot.googleplex.com/7qNYSJ2FiACpgXj
    
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED for opt-in VC
    Fixed: 1504004, 1503987
    Bug: 1504008
    Change-Id: I900e5621032faaea78fb9c42da9aebf1216e7cad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5050605
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1227628}

--- ios/chrome/browser/ui/content_suggestions/magic_stack_parcel_list_half_sheet_table_view_controller.mm
+++ ios/chrome/browser/ui/content_suggestions/magic_stack_parcel_list_half_sheet_table_view_controller.mm
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const CGFloat kEllipsisButtonPointSize = 15;
+const CGFloat kEllipsisButtonPointSize = 24;
 const CGFloat kParcelCellMargin = 16;
 
 enum SectionIdentifier : NSInteger {
@@ -76,6 +76,8 @@ enum ItemType : NSInteger {
                           forAxis:UILayoutConstraintAxisHorizontal];
     self.ellipsisButton = [[UIButton alloc] init];
 
+    UIImage* ellipsisImage = DefaultSymbolWithPointSize(
+        kEllipsisCircleFillSymbol, kEllipsisButtonPointSize);
     UIImageSymbolConfiguration* config = [UIImageSymbolConfiguration
         configurationWithPointSize:kEllipsisButtonPointSize
                             weight:UIImageSymbolWeightMedium
@@ -86,8 +88,7 @@ enum ItemType : NSInteger {
           [UIColor colorNamed:kGroupedPrimaryBackgroundColor]
         ]];
     config = [config configurationByApplyingConfiguration:colorConfig];
-    UIImage* ellipsisImage =
-        DefaultSymbolWithConfiguration(kEllipsisCircleFillSymbol, colorConfig);
+    ellipsisImage = [ellipsisImage imageByApplyingSymbolConfiguration:config];
     [self.ellipsisButton setImage:ellipsisImage forState:UIControlStateNormal];
 
     __weak __typeof(self) weakSelf = self;
