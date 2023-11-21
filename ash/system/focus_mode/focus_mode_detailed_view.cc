fb8f917a905f96d2caaafe9b6d95abff6440b65b
Andrew Pantera <andp@chromium.org>
2023-11-21 05:54:43 +0000

    focus-mode: Add task chip carousel
    Add a scroll view to the task view to display up to 5 tasks and allow a
    user to select one.
    
    Change-Id: I42605d030f4a4ad1f5a337ddf89be16e073da5e2
    Bug: b/306271409
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4998422
    Commit-Queue: Andrew Pantera <andp@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Auto-Submit: Andrew Pantera <andp@chromium.org>
    Reviewed-by: Richard Chui <richui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227236}

--- ash/system/focus_mode/focus_mode_detailed_view.cc
+++ ash/system/focus_mode/focus_mode_detailed_view.cc
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/highlight_path_generator.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/focus/focus_manager.h"
-#include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/box_layout_view.h"
 #include "ui/views/view_class_properties.h"
 #include "ui/views/view_observer.h"
