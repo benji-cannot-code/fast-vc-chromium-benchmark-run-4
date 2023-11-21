e1902e69654e2088347cc76fa86840241de09408
Sylvain Defresne <sdefresne@chromium.org>
2023-11-21 10:49:10 +0000

    [ios] Extend Browser API and improve its implementation
    Add a new method Browser::CreateTemporary(...) that allow creation
    of a Browser that is not connected to any SceneState (i.e. can be
    used to temporary hold WebState and benefit from the BrowserAgents
    but without being presented to the user).
    
    Change Browser implementation to avoid separate allocations for the
    WebStateList and the WebStateListDelegate (by inheriting from the
    delegate to benefit from the optimisation of zero-size parent class).
    
    Extend BrowserWebStateListDelegate to allow controlling whether it
    will attach tab helpers and/or force realization of a WebState when
    it is inserted/activated in the WebStateList.
    
    Add unit tests for BrowserWebStateListDelegate.
    
    Bug: 1383087
    Change-Id: I0505a6082e3adfb1dd6a07eee049148fd911e3e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5040786
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227312}

--- ios/chrome/browser/main/browser_impl.mm
+++ ios/chrome/browser/main/browser_impl.mm
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/check.h"
 #import "base/memory/ptr_util.h"
 #import "ios/chrome/browser/main/browser_agent_util.h"
-#import "ios/chrome/browser/main/browser_web_state_list_delegate.h"
 #import "ios/chrome/browser/shared/coordinator/scene/scene_state.h"
 #import "ios/chrome/browser/shared/model/browser/browser_observer.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
@@ -18,21 +17,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 BrowserImpl::BrowserImpl(ChromeBrowserState* browser_state,
                          SceneState* scene_state,
-                         BrowserImpl* active_browser)
-    : browser_state_(browser_state),
-      command_dispatcher_([[CommandDispatcher alloc] init]),
+                         BrowserImpl* active_browser,
+                         InsertionPolicy insertion_policy,
+                         ActivationPolicy activation_policy)
+    : BrowserWebStateListDelegate(insertion_policy, activation_policy),
+      browser_state_(browser_state),
+      web_state_list_(this),
       scene_state_(scene_state),
+      command_dispatcher_([[CommandDispatcher alloc] init]),
       active_browser_(active_browser ?: this) {
   DCHECK(browser_state_);
-  DCHECK(scene_state_);
   DCHECK(active_browser_);
-
-  // Only realize the WebState on activation for the active Browser.
-  const bool force_realization_on_activation = active_browser_ == this;
-  web_state_list_delegate_ = std::make_unique<BrowserWebStateListDelegate>(
-      force_realization_on_activation);
-  web_state_list_ =
-      std::make_unique<WebStateList>(web_state_list_delegate_.get());
+  DCHECK(command_dispatcher_);
 
   AttachBrowserAgents(this);
 }
@@ -48,7 +44,7 @@ ChromeBrowserState* BrowserImpl::GetBrowserState() {
 }
 
 WebStateList* BrowserImpl::GetWebStateList() {
-  return web_state_list_.get();
+  return &web_state_list_;
 }
 
 CommandDispatcher* BrowserImpl::GetCommandDispatcher() {
@@ -87,8 +83,10 @@ Browser* BrowserImpl::CreateInactiveBrowser() {
   CHECK(!IsInactive()) << "This browser already is the inactive one.";
   CHECK(!inactive_browser_.get())
       << "This browser already links to its inactive counterpart.";
-  inactive_browser_ =
-      std::make_unique<BrowserImpl>(browser_state_, scene_state_, this);
+  inactive_browser_ = std::make_unique<BrowserImpl>(
+      browser_state_, scene_state_, /*active_browser=*/this,
+      BrowserImpl::InsertionPolicy::kAttachTabHelpers,
+      BrowserImpl::ActivationPolicy::kDoNothing);
   return inactive_browser_.get();
 }
 
@@ -101,5 +99,17 @@ void BrowserImpl::DestroyInactiveBrowser() {
 // static
 std::unique_ptr<Browser> Browser::Create(ChromeBrowserState* browser_state,
                                          SceneState* scene_state) {
-  return std::make_unique<BrowserImpl>(browser_state, scene_state);
+  return std::make_unique<BrowserImpl>(
+      browser_state, scene_state, /*active_browser=*/nullptr,
+      BrowserImpl::InsertionPolicy::kAttachTabHelpers,
+      BrowserImpl::ActivationPolicy::kForceRealization);
+}
+
+// static
+std::unique_ptr<Browser> Browser::CreateTemporary(
+    ChromeBrowserState* browser_state) {
+  return std::make_unique<BrowserImpl>(
+      browser_state, /*scene_state=*/nil, /*active_browser=*/nullptr,
+      BrowserImpl::InsertionPolicy::kDoNothing,
+      BrowserImpl::ActivationPolicy::kDoNothing);
 }
--- ios/chrome/browser/sessions/session_restoration_browser_agent_unittest.mm
+++ ios/chrome/browser/sessions/session_restoration_browser_agent_unittest.mm
@@ -159,8 +159,7 @@ class SessionRestorationBrowserAgentTest : public PlatformTest {
     // constructor.
     browser_ = std::make_unique<TestBrowser>(
         chrome_browser_state_.get(),
-        std::make_unique<BrowserWebStateListDelegate>(
-            /* force_realization_on_activation */ true));
+        std::make_unique<BrowserWebStateListDelegate>());
   }
 
   void TearDown() override {
