eb3801003c3aa705d4a917fb0884f25fc43befa0
David Benjamin <davidben@chromium.org>
2023-11-21 07:04:20 +0000

    Fix UBSan issue with AdSamplerTriggerTest
    AdSamplerTrigger is a WebContentsUserData, so it cannot be created
    with a null WebContents. See this UBSan report:
    https://luci-milo.appspot.com/ui/inv/build-8763996839238408977/test-results?q=AdSamplerTriggerTestFinch.FrequencyDenominatorFeature&sortby=&groupby=
    
    Since this isn't actually doing anything with the AdSamplerTrigger, just
    reading an internal variable only exported for this test, just unit test
    the function that reads base::Feature directly.
    
    Bug: 1394755
    Change-Id: I597e5b112be8ed7154947bf9d53c985b1989ea86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5043362
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227248}

--- components/safe_browsing/content/browser/triggers/ad_sampler_trigger.cc
+++ components/safe_browsing/content/browser/triggers/ad_sampler_trigger.cc
@@ -171,6 +171,10 @@ void AdSamplerTrigger::CreateAdSampleReport() {
                             MAX_ACTIONS);
 }
 
+size_t AdSamplerTrigger::GetSamplerFrequencyDenominatorForTest() {
+  return GetSamplerFrequencyDenominator();
+}
+
 void AdSamplerTrigger::SetSamplerFrequencyForTest(size_t denominator) {
   sampler_frequency_denominator_ = denominator;
 }
