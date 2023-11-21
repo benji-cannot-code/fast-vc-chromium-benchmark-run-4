9fe1d35292c1c282c3e1d2e471e70bc9e2ed69e2
danakj <danakj@chromium.org>
2023-11-21 22:28:31 +0000

    Update rust crates: font-types, read-fonts, skrifa, serde, serde_derive
    I ran gnrt update which rolled:
    font-types: 0.4.0 => 0.4.1
    serde: 1.0.192 => 1.0.193
    serde_derive: 1.0.192 => 1.0.193
    
    There are also epoch-changing updates for read-fonts and skrifa which
    pick up their LICENSE files to quiet warnings when running gnrt vendor:
    read-fonts: 0.11.2 => 0.13.2
    skrifa: 0.10.1 => 0.13.0
    
    I then ran gnrt vendor, and gnrt gen.
    
    Also:
    - The paths in //skia/BUILD.gn are updated for the new epochs.
    - The old epoch README.chromium and BUILD.gn files are deleted.
    
    Bug: 1503143
    Change-Id: I0f59792b8bf9746395f0d39a84b4affc38d92b34
    Cq-Include-Trybots: luci.chromium.try:android-rust-arm32-rel,android-rust-arm64-dbg,android-rust-arm64-rel,linux-rust-x64-dbg,linux-rust-x64-rel,mac-rust-x64-dbg,win-rust-x64-dbg,win-rust-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5050282
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Auto-Submit: danakj <danakj@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227634}

---
+++ third_party/rust/chromium_crates_io/vendor/read-fonts-0.13.2/src/tables/eblc.rs
@@ -0,0 +1,6 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+//! The [EBLC (Embedded Bitmap Location)](https://docs.microsoft.com/en-us/typography/opentype/spec/eblc) table
+
+use super::bitmap::*;
+
+include!("../../generated/generated_eblc.rs");
