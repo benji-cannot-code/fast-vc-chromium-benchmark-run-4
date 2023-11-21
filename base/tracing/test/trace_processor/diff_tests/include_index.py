efde2dbf52ff8f5bb53fdb17fa34c3db189bdcb5
Rasika Navarange <rasikan@google.com>
2023-11-21 16:19:38 +0000

    Use Chrome test/data directory for diff tests
    Pass the local base/tracing/test/data directory to the diff test TestSuite. This allows developers to update test data locally.
    
    PERFETTO_TESTS=`autoninja -C out/Default perfetto_diff_tests && out/Default/bin/run_perfetto_diff_tests`
    
    Change-Id: If5a0a1d97bbea7d0bfab6d33c1fec0ab1d44f8ee
    Bug: b/308929429
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5049421
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Rasika Navarange <rasikan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1227413}

--- base/tracing/test/trace_processor/diff_tests/include_index.py
+++ base/tracing/test/trace_processor/diff_tests/include_index.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os
 from typing import List
 
 from python.generators.diff_tests import testing
@@ -12,8 +13,12 @@ from chrome.tests_scroll_jank import ChromeScrollJankStdlib
 from chrome.tests_chrome_interactions import ChromeInteractions
 
 def fetch_all_diff_tests(index_path: str) -> List['testing.TestCase']:
+  test_data_path = os.path.abspath(os.path.join(__file__, '../../../data'))
   return [
-      *ChromeScrollJankStdlib(index_path, 'chrome', 'ChromeScrollJankStdlib').fetch(),
-      *ChromeStdlib(index_path, 'chrome', 'Chrome').fetch(),
-      *ChromeInteractions(index_path, 'chrome', 'ChromeInteractions').fetch(),
+      *ChromeScrollJankStdlib(index_path, 'chrome', 'ChromeScrollJankStdlib',
+                              test_data_path).fetch(),
+      *ChromeStdlib(index_path, 'chrome', 'ChromeStdlib',
+                    test_data_path).fetch(),
+      *ChromeInteractions(index_path, 'chrome', 'ChromeInteractions',
+                          test_data_path).fetch(),
       ]
