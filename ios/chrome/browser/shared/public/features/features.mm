debfb41f8a57fddd6c1a77154dbea10b6f48645c
Christian Xu <christianxu@google.com>
2023-11-21 16:54:09 +0000

    [iOS] Add flag for the default omnibox position in promos
    Bug: 1503638
    Change-Id: Ia4d56aebdd0b276e0e07647e89b61a1354153bef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5049984
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227430}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -291,6 +291,15 @@ bool IsBottomOmniboxPromoFlagEnabled(BottomOmniboxPromoType type) {
   return false;
 }
 
+BASE_FEATURE(kBottomOmniboxPromoDefaultPosition,
+             "BottomOmniboxPromoDefaultPosition",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+const char kBottomOmniboxPromoDefaultPositionParam[] =
+    "BottomOmniboxPromoDefaultPositionParam";
+const char kBottomOmniboxPromoDefaultPositionParamTop[] = "Top";
+const char kBottomOmniboxPromoDefaultPositionParamBottom[] = "Bottom";
+
 BASE_FEATURE(kOnlyAccessClipboardAsync,
              "OnlyAccessClipboardAsync",
              base::FEATURE_DISABLED_BY_DEFAULT);
