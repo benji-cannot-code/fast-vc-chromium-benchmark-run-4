64b0e178043af0fc6af06c27d7f985cc7481f4fe
Minoru Chikamune <chikamune@chromium.org>
2023-11-21 01:11:41 +0000

    [CodeHealth] Remove expired Setup.Install.AddAppContainerAce histogram
    As a part of the CodeHealthRotation, this CL removes the expired
    histograms.
    
    OBSOLETE_HISTOGRAM[Setup.Install.AddAppContainerAce]=The histogram was expired as of M77.
    
    Bug: 1499485
    Change-Id: I97844106366e1898914892f91a7539d2acc0d625
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5045010
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227149}

--- chrome/installer/setup/install_worker.cc
+++ chrome/installer/setup/install_worker.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/logging.h"
-#include "base/metrics/histogram_functions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/version.h"
@@ -882,9 +881,6 @@ void AddInstallWorkItems(const InstallParams& install_params,
                   CONTAINER_INHERIT_ACE | OBJECT_INHERIT_ACE);
               success = success_target && success_temp;
             }
-
-            base::UmaHistogramBoolean("Setup.Install.AddAppContainerAce",
-                                      success);
             return success;
           },
           target_path, temp_path),
