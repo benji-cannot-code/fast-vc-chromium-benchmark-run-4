116002d7e7b75ca87164970888e53b64755777df
Josh Hilke <jrhilke@google.com>
2023-11-21 08:17:22 +0000

    Centralize record upload json constants.
    This change moves json constants for record upload requests and
    responses into central locations. Previously they were copied in
    multiple files.
    
    Bug: b:308837872
    Change-Id: Ife71eba18cacea0efc196645bab5b84982451a7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5046163
    Commit-Queue: Josh Hilke <jrhilke@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Hong Xu <xuhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1227268}

--- chrome/browser/policy/messaging_layer/upload/record_upload_request_builder_unittest.cc
+++ chrome/browser/policy/messaging_layer/upload/record_upload_request_builder_unittest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/management/management_service.h"
 #include "components/policy/core/common/management/scoped_management_service_override_for_testing.h"
 #include "components/reporting/resources/resource_manager.h"
+#include "components/reporting/util/record_upload_request_json_keys.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -326,8 +327,8 @@ TEST_P(RecordUploadRequestBuilderTest, AcceptRequestId) {
   ASSERT_TRUE(request_payload.has_value());
   EXPECT_THAT(request_payload.value(),
               IsEncryptionKeyRequestUploadRequestValid(need_encryption_key()));
-  auto* payload_request_id = request_payload->FindString(
-      UploadEncryptedReportingRequestBuilder::kRequestId);
+  auto* payload_request_id =
+      request_payload->FindString(reporting::json_keys::kRequestIdKey);
   EXPECT_THAT(*payload_request_id, ::testing::StrEq(request_id));
 }
 
