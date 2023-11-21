45679e985111d901e72740608e54f28c9651ab33
Devlin Cronin <rdevlin.cronin@chromium.org>
2023-11-21 02:32:18 +0000

    [Extensions] Remove STARTUP_PAGES type for SettingsApiBubbleDelegate
    Remove the STARTUP_PAGES type for SettingsApiBubbleDelegate. We no
    longer show this bubble.
    
    Bug: 1503268
    Change-Id: I2f1accb83d4ff0c8704ddb6c3bfa0f08802252e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5037625
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227191}

--- chrome/browser/extensions/settings_api_helpers.cc
+++ chrome/browser/extensions/settings_api_helpers.cc
@@ -22,11 +22,17 @@ namespace extensions {
 
 namespace {
 
+enum class OverrideType {
+  kStartupPages,
+  kHomePage,
+  kSearchEngine,
+};
+
 // Returns which |extension| (if any) is overriding a particular |type| of
 // setting.
 const Extension* FindOverridingExtension(
     content::BrowserContext* browser_context,
-    SettingsApiOverrideType type) {
+    OverrideType type) {
   const ExtensionSet& extensions =
       ExtensionRegistry::Get(browser_context)->enabled_extensions();
   ExtensionPrefsHelper* prefs_helper =
@@ -37,22 +43,26 @@ const Extension* FindOverridingExtension(
        ++it) {
     const SettingsOverrides* settings = SettingsOverrides::Get(it->get());
     if (settings) {
-      if (type == BUBBLE_TYPE_HOME_PAGE && !settings->homepage)
+      if (type == OverrideType::kHomePage && !settings->homepage) {
         continue;
-      if (type == BUBBLE_TYPE_STARTUP_PAGES && settings->startup_pages.empty())
+      }
+      if (type == OverrideType::kStartupPages &&
+          settings->startup_pages.empty()) {
         continue;
-      if (type == BUBBLE_TYPE_SEARCH_ENGINE && !settings->search_engine)
+      }
+      if (type == OverrideType::kSearchEngine && !settings->search_engine) {
         continue;
+      }
 
       std::string key;
       switch (type) {
-        case BUBBLE_TYPE_HOME_PAGE:
+        case OverrideType::kHomePage:
           key = prefs::kHomePage;
           break;
-        case BUBBLE_TYPE_STARTUP_PAGES:
+        case OverrideType::kStartupPages:
           key = prefs::kRestoreOnStartup;
           break;
-        case BUBBLE_TYPE_SEARCH_ENGINE:
+        case OverrideType::kSearchEngine:
           key = prefs::kDefaultSearchProviderEnabled;
           break;
       }
@@ -75,7 +85,7 @@ const Extension* FindOverridingExtension(
 
 const Extension* GetExtensionOverridingHomepage(
     content::BrowserContext* browser_context) {
-  return FindOverridingExtension(browser_context, BUBBLE_TYPE_HOME_PAGE);
+  return FindOverridingExtension(browser_context, OverrideType::kHomePage);
 }
 
 const Extension* GetExtensionOverridingNewTabPage(
@@ -92,12 +102,12 @@ const Extension* GetExtensionOverridingNewTabPage(
 
 const Extension* GetExtensionOverridingStartupPages(
     content::BrowserContext* browser_context) {
-  return FindOverridingExtension(browser_context, BUBBLE_TYPE_STARTUP_PAGES);
+  return FindOverridingExtension(browser_context, OverrideType::kStartupPages);
 }
 
 const Extension* GetExtensionOverridingSearchEngine(
     content::BrowserContext* browser_context) {
-  return FindOverridingExtension(browser_context, BUBBLE_TYPE_SEARCH_ENGINE);
+  return FindOverridingExtension(browser_context, OverrideType::kSearchEngine);
 }
 
 const Extension* GetExtensionOverridingProxy(
