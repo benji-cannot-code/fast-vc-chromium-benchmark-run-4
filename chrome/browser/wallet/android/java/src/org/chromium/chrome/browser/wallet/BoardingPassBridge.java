c158b7c0db0e691abf0da899a71114170957a19b
Hanfeng Zhu <hanfeng@google.com>
2023-11-21 23:12:18 +0000

    Implement the skeleton of boarding pass jni bridge
    It will be called by BoardingPassController on allowed URLs.
    
    Bug:1502402
    
    Low-Coverage-Reason: HARD_TO_TEST This CL just contains skeleton code
    
    
    Change-Id: Ia1447dd13efc9fd7c30b6e9ac936933a753210ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5039558
    Commit-Queue: Hanfeng Zhu <hanfeng@google.com>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Auto-Submit: Hanfeng Zhu <hanfeng@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227659}

---
+++ chrome/browser/wallet/android/java/src/org/chromium/chrome/browser/wallet/BoardingPassBridge.java
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.wallet;
+
+import org.jni_zero.JNINamespace;
+import org.jni_zero.NativeMethods;
+
+import org.chromium.base.Callback;
+import org.chromium.base.Promise;
+
+import java.util.Arrays;
+import java.util.List;
+
+/** Provides native methods for java code to call. */
+@JNINamespace("wallet")
+public class BoardingPassBridge {
+
+    private BoardingPassBridge() {}
+
+    /**
+     * Detects boarding pass barcode raw string from web page.
+     *
+     * @return Detected boarding passes raw string.
+     */
+    public static Promise<List<String>> detectBoardingPass() {
+        Promise<List<String>> promise = new Promise<>();
+        BoardingPassBridgeJni.get()
+                .detectBoardingPass(
+                        boardingPasses -> {
+                            if (!promise.isRejected()) {
+                                promise.fulfill(Arrays.asList(boardingPasses));
+                            }
+                        });
+        return promise;
+    }
+
+    @NativeMethods
+    public interface Natives {
+
+        void detectBoardingPass(Callback<String[]> callback);
+    }
+}
