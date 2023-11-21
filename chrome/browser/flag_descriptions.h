46e9d768cf40951a4d5998ce2ab61baaa491bf1d
Jacob Francis <francisjp@google.com>
2023-11-21 21:12:54 +0000

    [Read Anything] Add flag for enabling rules based algorithm.
    - This CL adds a flag for the rules based algorithm and is enabled by
      default.
    
    AX-RelNotes: N/A
    Bug: 1502966
    Change-Id: I1c53983ccc9c2f4424bacff0eb2b517029acb61c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5039211
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Jacob Francis <francisjp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1227596}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2762,6 +2762,9 @@ extern const char kReadAnythingDescription[];
 extern const char kReadAnythingWithScreen2xName[];
 extern const char kReadAnythingWithScreen2xDescription[];
 
+extern const char kReadAnythingWithAlgorithmName[];
+extern const char kReadAnythingWithAlgorithmDescription[];
+
 extern const char kReadAnythingWebUIToolbarName[];
 extern const char kReadAnythingWebUIToolbarDescription[];
 
