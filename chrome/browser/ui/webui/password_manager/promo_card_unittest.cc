ccf34b5fde242fc986d80da517546724157eb841
Trent Apted <tapted@chromium.org>
2023-11-21 01:50:00 +0000

    [User education] Disable segfaulting tests.
    Revert in https://crrev.com/c/5047692 failed due to conflicts.
    
    Tests fail since https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/37304/ with
    
    [ RUN      ] PromoCardShortcutTest.ShouldShowPromoFirstThreeTimes
    Received signal 11 SEGV_MAPERR 000000000040
    ...
    #4 0x55592615e26d ui::CalculateIdleTime()
    #5 0x555922e50a80 user_education::PollingIdleObserver::GetCurrentState()
    #6 0x55591dc67320 user_education::FeaturePromoSessionManager::Init()
    #7 0x5559247a76bd UserEducationServiceFactory::BuildServiceInstanceForBrowserContext()
    #8 0x5559256f9b97 KeyedServiceFactory::GetServiceForContext()
    #9 0x555928fe1606 PasswordManagerShortcutPromo::ShouldShowPromo()
    #10 0x55591ca55d9f password_manager::PromoCardShortcutTest_ShouldShowPromoFirstThreeTimes_Test::TestBody()
    
    Bug: 1479773
    Change-Id: I35b0337b2ab95dc89c90e640cf95184be74e87ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5048010
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Auto-Submit: Trent Apted <tapted@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227176}

--- chrome/browser/ui/webui/password_manager/promo_card_unittest.cc
+++ chrome/browser/ui/webui/password_manager/promo_card_unittest.cc
@@ -433,7 +433,8 @@ TEST_F(PromoCardShortcutTest, NoPromoIfShortcutInstalled) {
   EXPECT_FALSE(promo->ShouldShowPromo());
 }
 
-TEST_F(PromoCardShortcutTest, ShouldShowPromoFirstThreeTimes) {
+// TODO(dfried): Fix. Segfaults.
+TEST_F(PromoCardShortcutTest, DISABLED_ShouldShowPromoFirstThreeTimes) {
   ASSERT_THAT(pref_service()->GetList(prefs::kPasswordManagerPromoCardsList),
               IsEmpty());
   std::unique_ptr<PasswordPromoCardBase> promo =
@@ -448,7 +449,8 @@ TEST_F(PromoCardShortcutTest, ShouldShowPromoFirstThreeTimes) {
   EXPECT_FALSE(promo->ShouldShowPromo());
 }
 
-TEST_F(PromoCardShortcutTest, PromoNotShownAfterDismiss) {
+// TODO(dfried): Fix. Segfaults.
+TEST_F(PromoCardShortcutTest, DISABLED_PromoNotShownAfterDismiss) {
   base::HistogramTester histogram_tester;
   ASSERT_THAT(pref_service()->GetList(prefs::kPasswordManagerPromoCardsList),
               IsEmpty());
