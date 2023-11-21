dfd7445b529987c1490261a48b29feb815914a52
Qihui Zhao <qihuizhao@google.com>
2023-11-21 15:58:28 +0000

    Add IbanSaveManager functionality to send UploadIban request
    This CL adds functionality to IbanSaveManager so that it can make
    `UploadIban` request via payments_client. Additionally, a callback
    method OnUserDidDecideOnUploadSave has been introduced to manage the
    response received from the server.
    
    Bug: b:296651801
    Change-Id: Iaa64430b4d1f764c57e1e87fb2e472d3285430a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5006706
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1227401}

--- components/autofill/core/browser/payments/mock_test_payments_network_interface.h
+++ components/autofill/core/browser/payments/mock_test_payments_network_interface.h
@@ -34,6 +34,12 @@ class MockTestPaymentsNetworkInterface : public payments::TestPaymentsNetworkInt
        (base::OnceCallback<void(AutofillClient::PaymentsRpcResult,
                                 const std::u16string&)>)),
       (override));
+  MOCK_METHOD(
+      void,
+      UploadIban,
+      (const UploadIbanRequestDetails&,
+       (base::OnceCallback<void(AutofillClient::PaymentsRpcResult)> callback)),
+      (override));
 };
 
 }  // namespace autofill
