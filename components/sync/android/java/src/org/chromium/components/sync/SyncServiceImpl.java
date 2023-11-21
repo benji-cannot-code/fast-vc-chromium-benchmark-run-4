293f84dbe70b09ca8f2b0ca8fde305ebfa319ea6
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-11-21 12:15:25 +0000

    [sync] Add logic to clear per-account settings on Desktop/Android
    This CL prepares to adopt per-account settings on these platforms by
    addressing one of the requirements: clearing settings when the account
    is no longer considered "known" (i.e. in the cookie jar on Desktop, or
    in the AccountManager on Android). Similar logic already existed for
    iOS. This change is a no-op for the moment since the prefs are never
    really set in the first place.
    For the moment the clearing code is a bit spread out across
    platform-specific layers. Implementing a more central solution is left
    for next CLs.
    
    Bug: 1471860
    Change-Id: Id4cbfdb9ed4c1c75209e65413884b49d3de33b48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5040784
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1227332}

--- components/sync/android/java/src/org/chromium/components/sync/SyncServiceImpl.java
+++ components/sync/android/java/src/org/chromium/components/sync/SyncServiceImpl.java
@@ -15,6 +15,8 @@ import org.json.JSONException;
 
 import org.chromium.base.Callback;
 import org.chromium.base.ThreadUtils;
+import org.chromium.components.signin.AccountManagerFacadeProvider;
+import org.chromium.components.signin.AccountsChangeObserver;
 import org.chromium.components.signin.base.CoreAccountInfo;
 import org.chromium.components.signin.base.GoogleServiceAuthError;
 
@@ -33,7 +35,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
  *
  * <p>TODO(crbug.com/1451811): Update to no reference UI thread.
  */
-public class SyncServiceImpl implements SyncService {
+public class SyncServiceImpl implements SyncService, AccountsChangeObserver {
     // Pointer to the C++ counterpart object. Set on construction and reset on destroy() to avoid
     // a dangling pointer.
     private long mSyncServiceAndroidBridge;
@@ -57,11 +59,14 @@ public class SyncServiceImpl implements SyncService {
         ThreadUtils.assertOnUiThread();
         assert ptr != 0;
         mSyncServiceAndroidBridge = ptr;
+        keepSettingsOnlyForAccountManagerAccounts();
+        AccountManagerFacadeProvider.getInstance().addObserver(this);
     }
 
     /** Signals the native SyncService is being shutdown and this object mustn't be used anymore. */
     @CalledByNative
     private void destroy() {
+        AccountManagerFacadeProvider.getInstance().removeObserver(this);
         mSyncServiceAndroidBridge = 0;
     }
 
@@ -420,6 +425,27 @@ public class SyncServiceImpl implements SyncService {
         SyncServiceImplJni.get().triggerRefresh(mSyncServiceAndroidBridge);
     }
 
+    @Override
+    /* AccountsChangeObserver implementation. */
+    public void onCoreAccountInfosChanged() {
+        keepSettingsOnlyForAccountManagerAccounts();
+    }
+
+    private void keepSettingsOnlyForAccountManagerAccounts() {
+        AccountManagerFacadeProvider.getInstance()
+                .getCoreAccountInfos()
+                .then(
+                        accounts -> {
+                            String[] gaiaIds =
+                                    accounts.stream()
+                                            .map(CoreAccountInfo::getGaiaId)
+                                            .toArray(String[]::new);
+                            SyncServiceImplJni.get()
+                                    .keepAccountSettingsPrefsOnlyForUsers(
+                                            mSyncServiceAndroidBridge, gaiaIds);
+                        });
+    }
+
     /**
      * Invokes the onResult method of the callback from native code.
      */
@@ -547,5 +573,8 @@ public class SyncServiceImpl implements SyncService {
         void triggerRefresh(long nativeSyncServiceAndroidBridge);
 
         long getLastSyncedTimeForDebugging(long nativeSyncServiceAndroidBridge);
+
+        void keepAccountSettingsPrefsOnlyForUsers(
+                long nativeSyncServiceAndroidBridge, String[] gaiaIds);
     }
 }
--- components/sync/android/sync_service_android_bridge.cc
+++ components/sync/android/sync_service_android_bridge.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
+#include "components/signin/public/base/gaia_id_hash.h"
 #include "components/signin/public/identity_manager/account_info.h"
 #include "components/sync/android/jni_headers/SyncServiceImpl_jni.h"
 #include "components/sync/base/user_selectable_type.h"
@@ -336,4 +337,17 @@ jlong SyncServiceAndroidBridge::GetLastSyncedTimeForDebugging(JNIEnv* env) {
       native_sync_service_->GetLastSyncedTimeForDebugging();
   return static_cast<jlong>(
       (last_sync_time - base::Time::UnixEpoch()).InMicroseconds());
+}
+
+void SyncServiceAndroidBridge::KeepAccountSettingsPrefsOnlyForUsers(
+    JNIEnv* env,
+    const base::android::JavaParamRef<jobjectArray>& gaia_ids) {
+  std::vector<std::string> gaia_id_strings;
+  AppendJavaStringArrayToStringVector(env, gaia_ids, &gaia_id_strings);
+  std::vector<signin::GaiaIdHash> gaia_id_hashes;
+  for (const std::string& gaia_id_string : gaia_id_strings) {
+    gaia_id_hashes.push_back(signin::GaiaIdHash::FromGaiaId(gaia_id_string));
+  }
+  native_sync_service_->GetUserSettings()->KeepAccountSettingsPrefsOnlyForUsers(
+      gaia_id_hashes);
 }
