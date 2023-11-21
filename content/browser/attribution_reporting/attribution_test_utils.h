d334959adbbfb842113b1ea3d287b0e88e772cda
Andrew Paseltiner <apaseltiner@chromium.org>
2023-11-21 16:23:28 +0000

    Avoid crashing on unary channels in Attribution Reporting privacy math
    In particular, if the trigger specs were empty, the GetNumStates and
    DoRandomizedResponse functions would try to dereference an invalid
    iterator.
    
    Bug: 1504144
    Change-Id: I66b1cad89ba4052afb6fdb34b1179e726cac7b05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5049113
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227415}

--- content/browser/attribution_reporting/attribution_test_utils.h
+++ content/browser/attribution_reporting/attribution_test_utils.h
@@ -51,6 +51,8 @@ namespace content {
 
 class AttributionTrigger;
 class CommonSourceInfo;
+class RandomizedResponseData;
+
 struct FakeEventLevelReport;
 
 enum class RateLimitResult : int;
@@ -334,6 +336,8 @@ std::ostream& operator<<(std::ostream& out,
                          const AttributionInfo& attribution_info);
 
 std::ostream& operator<<(std::ostream& out, const FakeEventLevelReport&);
+
+std::ostream& operator<<(std::ostream& out, const RandomizedResponseData&);
 
 std::ostream& operator<<(std::ostream& out, const StorableSource& source);
 
