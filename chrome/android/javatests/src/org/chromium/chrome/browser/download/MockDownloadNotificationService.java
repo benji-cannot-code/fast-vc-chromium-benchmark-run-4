2a67c036dbe4f652123ded5a64c1fb1ccb176201
Min Qin <qinmin@chromium.org>
2023-11-21 23:44:12 +0000

    [Download cleanup] Remove EXTRA_IS_AUTO_RESUMPTION in DownloadNotificationService
    Autoresumption is now handled in native code, so java side no longer
    needs this variable.
    
    Low-Coverage-Reason: remove code, no new code.
    Change-Id: I023b80973b4f500948f1a8d70b4c5b66f97e9bc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5050895
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227683}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/MockDownloadNotificationService.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/MockDownloadNotificationService.java
@@ -11,8 +11,6 @@ import android.app.Notification;
 import android.content.Intent;
 import android.graphics.Bitmap;
 
-import org.junit.Assert;
-
 import org.chromium.base.IntentUtils;
 import org.chromium.chrome.browser.profiles.OTRProfileID;
 import org.chromium.components.offline_items_collection.ContentId;
@@ -188,6 +186,5 @@ public class MockDownloadNotificationService extends DownloadNotificationService
     @Override
     void resumeDownload(Intent intent) {
         mResumedDownloads.add(IntentUtils.safeGetStringExtra(intent, EXTRA_DOWNLOAD_CONTENTID_ID));
-        Assert.assertTrue(IntentUtils.safeGetBooleanExtra(intent, EXTRA_IS_AUTO_RESUMPTION, false));
     }
 }
