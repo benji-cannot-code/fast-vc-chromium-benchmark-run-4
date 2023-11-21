f4c26526c1edab325a68861b924146755c98a250
Jimmy <jimmyxgong@chromium.org>
2023-11-21 19:06:08 +0000

    shortcuts: Import ShortcutInputProvider webui-mojom to shortcut customization app
    Supplies the shortcut customization app with the necessary packages
    to utilize the ShortcutInputProvider mojom interface.
    
    Bug: b/308843852, b/216049298
    Change-Id: I49793495164d73a3751904a4494cc6420cb5150c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5028475
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1227513}

---
+++ ash/webui/shortcut_customization_ui/resources/js/shortcut_input_mojo_interface_provider.ts
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assert} from 'chrome://resources/js/assert.js';
+
+import {ShortcutInputProvider, ShortcutInputProviderInterface} from '../mojom-webui/shortcut_input_provider.mojom-webui.js';
+
+/**
+ * @fileoverview
+ * Provides singleton access to mojo interfaces.
+ */
+
+let shortcutInputProvider: ShortcutInputProviderInterface|null;
+
+export function getShortcutInputProvider(): ShortcutInputProviderInterface {
+  if (!shortcutInputProvider) {
+    shortcutInputProvider = ShortcutInputProvider.getRemote();
+  }
+
+  assert(!!shortcutInputProvider);
+  return shortcutInputProvider;
+}
