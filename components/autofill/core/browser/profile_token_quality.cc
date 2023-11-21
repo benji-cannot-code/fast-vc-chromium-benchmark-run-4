363a04c3139dd1da36918f73af1024eb46405540
Florian Leimgruber <fleimgruber@google.com>
2023-11-21 13:24:09 +0000

    Emit Autofill.ProfileTokenQuality.ObservationCountBeforeSubmission*
    As defined in go/profile-token-quality-metrics. These metrics are
    very similar to the metrics emitted on start-up. But they are implicitly
    skewed towards users that submit more forms, rather than users that
    restart their browser a lot.
    
    Bug: 1453650
    Change-Id: I4907916f8904e23ae68e98b87e0e67a112272f1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5033620
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1227350}

--- components/autofill/core/browser/profile_token_quality.cc
+++ components/autofill/core/browser/profile_token_quality.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/field_type_utils.h"
 #include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/form_structure.h"
+#include "components/autofill/core/browser/metrics/profile_token_quality_metrics.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/autofill_l10n_util.h"
@@ -178,6 +179,9 @@ void ProfileTokenQuality::SaveObservationsForFilledFormForAllSubmittedProfiles(
           features::kAutofillTrackProfileTokenQuality)) {
     return;
   }
+
+  autofill_metrics::LogObservationCountBeforeSubmissionMetric(form_structure,
+                                                              pdm);
 
   std::set<std::string> guids_seen;
   for (const std::unique_ptr<AutofillField>& field : form_structure) {
