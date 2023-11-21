a831717e6f6263d73b4dc978e36f27eaf0ee7828
Ken Rockot <rockot@google.com>
2023-11-21 21:07:14 +0000

    on_device_model: Fix DXC in Windows sandbox
    Fixed: b:312293887
    Change-Id: I272b952484ae79045903af2ee2a583534dbc03ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5050945
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1227593}

--- services/on_device_model/pre_sandbox_init.cc
+++ services/on_device_model/pre_sandbox_init.cc
@@ -3,7 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/base_paths.h"
+#include "base/files/file_path.h"
 #include "base/logging.h"
+#include "base/native_library.h"
+#include "base/path_service.h"
+#include "build/build_config.h"
 #include "services/on_device_model/on_device_model_service.h"
 
 #if defined(ENABLE_ML_INTERNAL)
@@ -21,6 +26,16 @@ bool OnDeviceModelService::PreSandboxInit() {
     return false;
   }
 #endif
+
+#if defined(DAWN_USE_BUILT_DXC) && BUILDFLAG(IS_WIN)
+  base::FilePath module_path;
+  if (base::PathService::Get(base::DIR_MODULE, &module_path)) {
+    // Preload DXC requirements if enabled.
+    base::LoadNativeLibrary(module_path.Append(L"dxil.dll"), nullptr);
+    base::LoadNativeLibrary(module_path.Append(L"dxcompiler.dll"), nullptr);
+  }
+#endif
+
   return true;
 }
 
