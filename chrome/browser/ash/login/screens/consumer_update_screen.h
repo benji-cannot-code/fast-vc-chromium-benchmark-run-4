eb1b220f79b688e7d23f370fa46335cb73932fc4
Brahim Chikhaoui <bchikhaoui@google.com>
2023-11-21 10:16:26 +0000

    [OOBE] Fix VersionUpdater UpdateStatusChanged
    Ignore all install ProcessMode status from update_engine except for
    IDLE state, Currently UpdateEngine is responsible for DLC install
    and observing status change may be related to installing DLC
    instead of the update request.
    Exit the screen if non_idle state received after 180 seconds.
    Add unit_test to versionUpdater cover ignoring install DLC status
    and RetryingOnIDLE and ExitOnRetryCheckTimeout .
    Add unit_test  to UpdateScreen cover worst case scenario if 180 seconds
    elapsedscreen should exit.
    
    Bug: b:306372039 , b:301329121 , b:294901561
    Change-Id: I70b6479c800eaf2c2c098f384514efcc7949d513
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5002236
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Reviewed-by: Miriam Polzer <mpolzer@google.com>
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1227298}

--- chrome/browser/ash/login/screens/consumer_update_screen.h
+++ chrome/browser/ash/login/screens/consumer_update_screen.h
@@ -42,6 +42,7 @@ class ConsumerUpdateScreen : public BaseScreen,
     UPDATE_NOT_REQUIRED,
     UPDATE_ERROR,
     NOT_APPLICABLE,
+    CHECK_TIMEOUT,
   };
 
   // This enum is tied directly to the OobeConsumerUpdateScreenSkippedReason UMA
--- chrome/browser/ash/login/screens/update_screen_browsertest.cc
+++ chrome/browser/ash/login/screens/update_screen_browsertest.cc
@@ -992,7 +992,7 @@ IN_PROC_BROWSER_TEST_P(UpdateScreenTest,
   ASSERT_EQ(update_engine_client()->reboot_after_update_call_count(), 1);
 
   // Simulate the situation where reboot does not happen in time.
-  ASSERT_TRUE(version_updater_->GetRebootTimerForTesting()->IsRunning());
+  ASSERT_TRUE(version_updater_->get_reboot_timer_for_testing()->IsRunning());
   mocked_task_runner->FastForwardBy(kTimeDefaultWaiting);
 
   test::OobeJS().ExpectHiddenPath(kRestartingDialog);
@@ -1092,7 +1092,7 @@ IN_PROC_BROWSER_TEST_P(UpdateScreenTest, DISABLED_UpdateScreenSteps) {
   ASSERT_EQ(update_engine_client()->reboot_after_update_call_count(), 1);
 
   // Simulate the situation where reboot does not happen in time.
-  ASSERT_TRUE(version_updater_->GetRebootTimerForTesting()->IsRunning());
+  ASSERT_TRUE(version_updater_->get_reboot_timer_for_testing()->IsRunning());
   mocked_task_runner->FastForwardBy(kTimeDefaultWaiting);
 
   test::OobeJS().ExpectHiddenPath(kBetterUpdateCheckingForUpdatesDialog);
