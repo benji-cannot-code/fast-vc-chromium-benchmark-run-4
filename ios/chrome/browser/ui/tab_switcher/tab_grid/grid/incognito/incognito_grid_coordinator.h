b1ecdf2cb62ab7ba6f100e472139af1afe0de4ad
Gauthier Ambard <gambard@chromium.org>
2023-11-21 10:31:09 +0000

    [iOS] Clear Regular/Incognito TabGrid children on intents
    When the intents are launched, Chrome should stop child
    coordinators.
    The call should be forwarded to the incognito/regular tab grid.
    The call to the stopChidCoordinators method was forgotten during
    a bad merge.
    
    Fixed: 1501318
    Change-Id: I5d6d268e38a47845f3d2a45d712260e61d5dda1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5033865
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227303}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/incognito/incognito_grid_coordinator.h
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/incognito/incognito_grid_coordinator.h
@@ -64,7 +64,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)setIncognitoBrowser:(Browser*)incognitoBrowser;
 
 // Stops all child coordinators.
-- (void)stopChidCoordinators;
+- (void)stopChildCoordinators;
 
 @end
 
--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/regular/regular_grid_coordinator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/regular/regular_grid_coordinator.mm
@@ -152,7 +152,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #pragma mark - Public
 
-- (void)stopChidCoordinators {
+- (void)stopChildCoordinators {
   [self.gridViewController dismissModals];
 }
 
