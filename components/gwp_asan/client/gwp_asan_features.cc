c83d50d42ea7afed28b5f823421b65c6be5d69a2
Kalvin Lee <kdlee@chromium.org>
2023-11-21 11:47:24 +0000

    GWP-ASan: Enable for Android
    This CL scopes enablement to 64-bit Android, since GWP-ASan hooks are
    not currently built for 32-bit Android.
    
    This CL sets up new default parameters for Android s.t. they mirror the
    (internally named) experiment group `EnabledLowerTotalPages`.
    
    Bug: b:287426302
    Change-Id: Ife6c6e4c3dfc427b0e9ef99796ea515404718cdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5038919
    Reviewed-by: Titouan Rigoudy <titouan@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Ali Hijazi <ahijazi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227318}

--- components/gwp_asan/client/gwp_asan_features.cc
+++ components/gwp_asan/client/gwp_asan_features.cc
@@ -10,7 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gwp_asan::internal {
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_LINUX) || \
-    BUILDFLAG(IS_CHROMEOS)
+    BUILDFLAG(IS_CHROMEOS) ||                                          \
+    (BUILDFLAG(IS_ANDROID) && defined(ARCH_CPU_64_BITS))
 constexpr base::FeatureState kDefaultEnabled = base::FEATURE_ENABLED_BY_DEFAULT;
 #else
 constexpr base::FeatureState kDefaultEnabled =
