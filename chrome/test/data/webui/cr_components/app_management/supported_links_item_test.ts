2621d6d3c8c03afb6bb07e05ae9efe2c49a97059
Lu Huang <luhua@microsoft.com>
2023-11-22 20:54:05 +0000

    dPWA: Add close button accessibility label to supported links dialog
    Problem: Screen reader was not announcing "close" for the close button of the web app settings supported links dialog.
    
    Solution: This CL adds `close-text` to the close button of the `cr-dialog` so that the screen reader announces "close".
    
    Bug: 1501000
    Change-Id: Icec25f93f0e2327c3f8897f62e99237a1d0bffd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5051816
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Lu Huang <luhua@microsoft.com>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1228181}

--- chrome/test/data/webui/cr_components/app_management/supported_links_item_test.ts
+++ chrome/test/data/webui/cr_components/app_management/supported_links_item_test.ts
@@ -31,6 +31,7 @@ suite('AppManagementSupportedLinksItemElement', function() {
 
     loadTimeData.resetForTesting({
       cancel: 'Cancel',
+      close: 'Close',
       appManagementIntentSettingsDialogTitle: 'Supported Links',
       appManagementIntentSettingsTitle: '<a href="#">Supported Links</a>',
       appManagementIntentOverlapDialogTitle: 'Change as preferred app',
