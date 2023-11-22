d093d85c10680783cde9aed7223bbae0ed953d23
Viktor Semeniuk <vsemeniuk@google.com>
2023-11-22 12:32:29 +0000

    Revert "[bfcache] Enable BFCache for pages with MediaDevicesDispatcherHost"
    This reverts commit 66672075274a0ef27c64b118ee5ebffab5f39e3b.
    
    Reason for revert: Test BackForwardCacheMetricsTest.AllFeaturesCovered  consistently fails on fuchsia-arm64-rel(https://ci.chromium.org/ui/p/chromium/builders/ci/fuchsia-arm64-rel)
    
    The First failing build https://ci.chromium.org/ui/b/8763739778664780513
    
    Original change's description:
    > [bfcache] Enable BFCache for pages with MediaDevicesDispatcherHost
    >
    > This CL makes kMediaDevicesDispatcherHost no longer a blocker for BFCache. This means that pages that have used the media devices API can be cached when they don't access any MediaStreamTrack. This feature is disabled by default by the newly added feature flag `EnableBackForwardCacheForPagesWithMediaDevicesDispatcherHost`
    >
    > Bug: 1502395
    > Change-Id: I36f71242d03955fd566f9bb800f6413fc788bd61
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029613
    > Reviewed-by: Mingyu Lei <leimy@chromium.org>
    > Commit-Queue: Kurumi Muto <mkurumi@google.com>
    > Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1227870}
    
    Bug: 1502395
    Change-Id: I848ef8d76b940a175f5694a5ad78cca2617b4362
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5050053
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Owners-Override: Viktor Semeniuk <vsemeniuk@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1227927}

--- content/browser/renderer_host/media/media_devices_dispatcher_host.cc
+++ content/browser/renderer_host/media/media_devices_dispatcher_host.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/command_line.h"
-#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/run_loop.h"
@@ -43,13 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 using blink::mojom::MediaDeviceType;
-
-namespace features {
-// When enabled, MediaDevicesDispatcherHost does not block back/forward cache.
-BASE_FEATURE(kEnableBackForwardCacheForPagesWithMediaDevicesDispatcherHost,
-             "EnableBackForwardCacheForPagesWithMediaDevicesDispatcherHost",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-}  // namespace features
 
 namespace content {
 
@@ -102,15 +94,11 @@ void MediaDevicesDispatcherHost::Create(
                        if (!render_frame_host)
                          return;
 
-                       if (!base::FeatureList::IsEnabled(
-                               features::
-                                   kEnableBackForwardCacheForPagesWithMediaDevicesDispatcherHost)) {
-                         BackForwardCache::DisableForRenderFrameHost(
-                             render_frame_host,
-                             BackForwardCacheDisable::DisabledReason(
-                                 BackForwardCacheDisable::DisabledReasonId::
-                                     kMediaDevicesDispatcherHost));
-                       }
+                       BackForwardCache::DisableForRenderFrameHost(
+                           render_frame_host,
+                           BackForwardCacheDisable::DisabledReason(
+                               BackForwardCacheDisable::DisabledReasonId::
+                                   kMediaDevicesDispatcherHost));
                      },
                      render_frame_host_id));
 }
