b417430cc9146f2577a9019e41d168bf7e760993
Aashna <aashnajena@google.com>
2023-11-22 10:50:42 +0000

    Move MediaIntegrityApiStatus to package android_webview.common
    MediaIntegrityApiStatus is an IntDef having downstream references.
    Moving it to package `common` to ensure that future authors are
    aware of the downstream repercussions of this IntDef.
    
    Bug: b/301393651
    Change-Id: Id3bf363b6052ef7a791cae6790a3d154b058926c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5050166
    Commit-Queue: Aashna Jena <aashnajena@google.com>
    Reviewed-by: Peter Pakkenberg <pbirk@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227904}

---
+++ android_webview/java/src/org/chromium/android_webview/common/MediaIntegrityApiStatus.java
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.android_webview.common;
+
+import androidx.annotation.IntDef;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE_USE)
+@Retention(RetentionPolicy.SOURCE)
+@IntDef({
+    MediaIntegrityApiStatus.DISABLED,
+    MediaIntegrityApiStatus.ENABLED_WITHOUT_APP_IDENTITY,
+    MediaIntegrityApiStatus.ENABLED
+})
+public @interface MediaIntegrityApiStatus {
+    int DISABLED = 0;
+    int ENABLED_WITHOUT_APP_IDENTITY = 1;
+    int ENABLED = 2;
+}
