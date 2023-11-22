fd7cc4ed06ada19d388d6c43be8046c8ab96d3f3
Frank Liberato <liberato@chromium.org>
2023-11-22 19:39:48 +0000

    [codehealth] Fix profile_picker_view_browsertest
    When UrlLoadObserver was switched away from the notification service,
    ProfilePickerCreationFlowBrowserTest.CreateForceSignedInProfile
    started failing on mac.  This CL fixes it, and re-enables it for mac.
    
    Change-Id: Ifcabec2ed1c0cdace3f30fe044357b16cb2f0956
    Bug: 1174764
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5046744
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1228141}

--- chrome/browser/ui/views/profiles/profile_picker_force_signin_dialog_host.cc
+++ chrome/browser/ui/views/profiles/profile_picker_force_signin_dialog_host.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/profiles/profile_picker_force_signin_dialog_host.h"
 
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/views/profiles/profile_picker_force_signin_dialog_delegate.h"
 #include "ui/views/controls/webview/webview.h"
 #include "ui/views/window/dialog_delegate.h"
 #include "url/gurl.h"
--- chrome/browser/ui/views/profiles/profile_picker_force_signin_dialog_host.h
+++ chrome/browser/ui/views/profiles/profile_picker_force_signin_dialog_host.h
@@ -7,10 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_PROFILES_PROFILE_PICKER_FORCE_SIGNIN_DIALOG_HOST_H_
 
 #include "base/memory/raw_ptr.h"
+#include "chrome/browser/ui/views/profiles/profile_picker_force_signin_dialog_delegate.h"
 #include "ui/gfx/native_widget_types.h"
 
 class GURL;
-class ProfilePickerForceSigninDialogDelegate;
+
+namespace content {
+class WebContents;
+}  // namespace content
 
 namespace views {
 class DialogDelegateView;
@@ -43,6 +47,9 @@ class ProfilePickerForceSigninDialogHost {
   void DisplayErrorMessage();
 
   views::DialogDelegateView* GetDialogDelegateViewForTesting() const;
+  content::WebContents* get_web_contents_for_testing() const {
+    return delegate_->GetWebContentsForTesting();
+  }
 
  private:
   friend class ProfilePickerForceSigninDialogDelegate;
