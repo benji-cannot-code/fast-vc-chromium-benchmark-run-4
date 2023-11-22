fb31c7af89c8cd45e89e8df43290af223e72ae3a
Rakina Zata Amni <rakina@chromium.org>
2023-11-22 08:24:45 +0000

    Change tests using unload to pagehide
    These tests that use the unload handler can actually use pagehide, which
    is not deprecated.
    
    Bug: 1488371
    Change-Id: Ia8cb4fe7bd33ed4f99ce35526bfedf07675cae7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5039553
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Auto-Submit: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227859}

--- content/browser/renderer_host/render_frame_host_impl.h
+++ content/browser/renderer_host/render_frame_host_impl.h
@@ -3189,9 +3189,9 @@ class CONTENT_EXPORT RenderFrameHostImpl
       SitePerProcessBrowserTest,
       IsDetachedSubframeObservableDuringUnloadHandlerCrossProcess);
   FRIEND_TEST_ALL_PREFIXES(SitePerProcessSSLBrowserTest,
-                           UnloadHandlersArePowerful);
+                           PagehideHandlersArePowerful);
   FRIEND_TEST_ALL_PREFIXES(SitePerProcessSSLBrowserTest,
-                           UnloadHandlersArePowerfulGrandChild);
+                           PagehideHandlersArePowerfulGrandChild);
   FRIEND_TEST_ALL_PREFIXES(RenderFrameHostImplTest, ExpectedMainWorldOrigin);
   FRIEND_TEST_ALL_PREFIXES(RenderFrameHostImplTest,
                            RendererInitiatedCloseIsCancelledIfPageIsntPrimary);
