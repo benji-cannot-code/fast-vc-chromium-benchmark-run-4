5fcd673dfc2aec088c99077df7fe0fd6b4e9f4f5
Fredrik Söderquist <fs@opera.com>
2023-11-22 22:57:07 +0000

    Remove unused argument to GraphicsContext::DrawTextPasses
    The `auto_dark_mode` parameter isn't passed along to the closure, so it
    can be removed. Since two out of three callers were not handling/using
    the parameter fix them up so that they do. Use default capture by
    reference in all three cases.
    
    Change-Id: I987548b33f1de8c66d29c953b83f2875d1b378e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5050229
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1228226}

--- third_party/blink/renderer/platform/graphics/graphics_context.h
+++ third_party/blink/renderer/platform/graphics/graphics_context.h
@@ -546,7 +546,7 @@ class PLATFORM_EXPORT GraphicsContext {
                                  const AutoDarkMode& auto_dark_mode);
 
   template <typename DrawTextFunc>
-  void DrawTextPasses(const AutoDarkMode& auto_dark_mode, const DrawTextFunc&);
+  void DrawTextPasses(const DrawTextFunc&);
 
   void BeginLayer(const cc::PaintFlags&);
 
