ced4d64976117b68d08b8d121f29d88cbe562a1b
Nihar Majmudar <niharm@google.com>
2023-11-22 16:05:38 +0000

    [realbox][cr23] Update action chip focus state.
    This CL updates the action chip tabbed focus state in the realbox. The
    border on focus should be outside of the chip.
    
    Before: https://screenshot.googleplex.com/VEKvSnxCmVTobFV
    After: https://screenshot.googleplex.com/7n4xhSMYiGq8JQt
    Figma: https://www.figma.com/file/qbe2W1Htg1uAjt4kd5Im47/GM3-Realbox?type=design&node-id=1881-20729&mode=design&t=1Tvn15eEr3o2uANq-0
    
    Bug: 1503168
    Change-Id: I554aa63c01314639f462a435e0671f22c0d73a23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5038337
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Khalid Peer <khalidpeer@chromium.org>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1228018}

--- ui/webui/resources/cr_components/omnibox/realbox_match.ts
+++ ui/webui/resources/cr_components/omnibox/realbox_match.ts
@@ -77,6 +77,18 @@ export class RealboxMatchElement extends PolymerElement {
         reflectToAttribute: true,
       },
 
+      expandedStateIconsChromeRefresh: {
+        type: Boolean,
+        value: () => loadTimeData.getBoolean('realboxCr23ExpandedStateLayout'),
+      },
+
+      /** Whether action chip will have an outset focus ring. */
+      hasOutsetActionFocusRing: {
+        type: Boolean,
+        computed: `computeHasOutsetActionFocusRing_(match.actions)`,
+        reflectToAttribute: true,
+      },
+
       /**
        * Whether the match features an image (as opposed to an icon or favicon).
        */
@@ -178,6 +190,8 @@ export class RealboxMatchElement extends PolymerElement {
   }
 
   override ariaLabel: string;
+  expandedStateIconsChromeRefresh: boolean;
+  hasOutsetActionFocusRing: boolean;
   hasImage: boolean;
   match: AutocompleteMatch;
   matchIndex: number;
@@ -332,6 +346,11 @@ export class RealboxMatchElement extends PolymerElement {
             this.renderTextWithClassifications_(
                     decodeString16(match.description), match.descriptionClass)
                 .innerHTML);
+  }
+
+  private computeHasOutsetActionFocusRing_() {
+    return this.expandedStateIconsChromeRefresh &&
+        this.match?.actions?.length > 0;
   }
 
   private computeTailSuggestPrefix_(): string {
