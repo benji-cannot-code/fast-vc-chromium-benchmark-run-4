a81e3277f9879d7958bd81cc924d8e2f6113483d
Minoru Chikamune <chikamune@chromium.org>
2023-11-22 02:19:27 +0000

    [CodeHealth] Remove expired Net.PublicKeyPinSuccess histogram
    As a part of the CodeHealthRotation, this CL removes the expired
    histograms.
    
    OBSOLETE_HISTOGRAM[Net.PublicKeyPinSuccess]=The histogram was expired as of M77.
    
    Bug: 1499448
    Change-Id: I34811081acff2aef537c255a0d06c0471c7e30bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5045030
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227750}

--- net/http/transport_security_state.cc
+++ net/http/transport_security_state.cc
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/metrics/field_trial.h"
 #include "base/metrics/field_trial_params.h"
-#include "base/metrics/histogram_functions.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
@@ -446,19 +444,10 @@ TransportSecurityState::PKPStatus TransportSecurityState::CheckPublicKeyPins(
     return PKPStatus::OK;
   }
 
-  PKPStatus pin_validity = CheckPublicKeyPinsImpl(
-      host_port_pair, is_issued_by_known_root, public_key_hashes,
-      served_certificate_chain, validated_certificate_chain, report_status,
-      network_anonymization_key, pinning_failure_log);
-
-  // Don't track statistics when a local trust anchor would override the pinning
-  // anyway.
-  if (!is_issued_by_known_root)
-    return pin_validity;
-
-  UMA_HISTOGRAM_BOOLEAN("Net.PublicKeyPinSuccess",
-                        pin_validity == PKPStatus::OK);
-  return pin_validity;
+  return CheckPublicKeyPinsImpl(host_port_pair, is_issued_by_known_root,
+                                public_key_hashes, served_certificate_chain,
+                                validated_certificate_chain, report_status,
+                                network_anonymization_key, pinning_failure_log);
 }
 
 bool TransportSecurityState::HasPublicKeyPins(const std::string& host) {
