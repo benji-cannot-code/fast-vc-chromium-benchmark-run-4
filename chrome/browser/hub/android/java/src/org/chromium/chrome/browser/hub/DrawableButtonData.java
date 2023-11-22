07da93a07c2e425557e81cf423a5dbe39db60721
Calder Kitagawa <ckitagawa@chromium.org>
2023-11-22 17:59:35 +0000

    [Hub] Add a DrawableButtonData
    This is largely duplicated from ResourceButtonData, but the Drawable is
    not a resource. This is important because for the Tab Switcher we will
    need to use the TabSwitcherDrawable which is dynamically updated.
    
    Change-Id: I90ce4ed81886c22e759e9261fa9450a083e69079
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5050986
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1228080}

---
+++ chrome/browser/hub/android/java/src/org/chromium/chrome/browser/hub/DrawableButtonData.java
@@ -0,0 +1,55 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.hub;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+
+import java.util.Objects;
+
+/** Resolves a non-resource drawable's button data. */
+public class DrawableButtonData implements DisplayButtonData {
+    private final @StringRes int mTextRes;
+    private final Drawable mDrawable;
+
+    /**
+     * @param textRes The text resource to resolve.
+     * @param drawable The non-resource {@link Drawable} to display.
+     */
+    public DrawableButtonData(@StringRes int textRes, Drawable drawable) {
+        mTextRes = textRes;
+        mDrawable = drawable;
+    }
+
+    @Override
+    public String resolveText(Context context) {
+        return context.getString(mTextRes);
+    }
+
+    @Override
+    public Drawable resolveIcon(Context context) {
+        return mDrawable;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mTextRes, mDrawable);
+    }
+
+    @Override
+    public boolean equals(@Nullable Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o instanceof DrawableButtonData that) {
+            return mTextRes == that.mTextRes && mDrawable.equals(that.mDrawable);
+        }
+        return false;
+    }
+}
