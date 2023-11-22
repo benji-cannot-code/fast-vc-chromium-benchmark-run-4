453c789f52a9a6e3980bbcb3c3ec3564b5dd4cac
Tom Sepez <tsepez@chromium.org>
2023-11-22 00:04:50 +0000

    Pass span argument to StreamingUtf8Validator.
    Prefer spans to discrete pointer/length pairs. Also prefer the
    convention that routines taking "bytes" should use uint8_t.
    
    Bug: 1490484
    Change-Id: I580f870f9d6302aff7a809a5766d802a57445047
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5011108
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227693}

--- base/i18n/streaming_utf8_validator.h
+++ base/i18n/streaming_utf8_validator.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/containers/span.h"
 #include "base/i18n/base_i18n_export.h"
 
 namespace base {
@@ -42,12 +43,12 @@ class BASE_I18N_EXPORT StreamingUtf8Validator {
 
   // Trivial destructor intentionally omitted.
 
-  // Validate |size| bytes starting at |data|. If the concatenation of all calls
+  // Validate bytes described by |data|. If the concatenation of all calls
   // to AddBytes() since this object was constructed or reset is a valid UTF-8
   // string, returns VALID_ENDPOINT. If it could be the prefix of a valid UTF-8
   // string, returns VALID_MIDPOINT. If an invalid byte or UTF-8 sequence was
   // present, returns INVALID.
-  State AddBytes(const char* data, size_t size);
+  State AddBytes(base::span<const uint8_t> data);
 
   // Return the object to a freshly-constructed state so that it can be re-used.
   void Reset();
