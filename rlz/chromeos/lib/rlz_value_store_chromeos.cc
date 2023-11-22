de94147167dfe53afab5099c9f784b7921f70f95
Minoru Chikamune <chikamune@chromium.org>
2023-11-22 02:11:36 +0000

    [CodeHealth] Remove expired Rlz.SetRlzPingSent histogram
    As a part of the CodeHealthRotation, this CL removes the expired
    histograms.
    
    OBSOLETE_HISTOGRAM[Rlz.SetRlzPingSent]=The histogram was expired as of M85.
    
    Bug: 1499476
    Change-Id: I4ca413b7432c2bc0fee6b34b0d2291879c0c047a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5043670
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227748}

--- rlz/chromeos/lib/rlz_value_store_chromeos.cc
+++ rlz/chromeos/lib/rlz_value_store_chromeos.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/path_service.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
@@ -143,12 +142,10 @@ void SetRlzPingSent(int retry_count) {
 // Callback invoked for DebugDaemonClient::SetRlzPingSent.
 void OnSetRlzPingSent(int retry_count, bool success) {
   if (success) {
-    UMA_HISTOGRAM_BOOLEAN("Rlz.SetRlzPingSent", true);
     return;
   }
 
   if (retry_count >= RlzValueStoreChromeOS::kMaxRetryCount) {
-    UMA_HISTOGRAM_BOOLEAN("Rlz.SetRlzPingSent", false);
     LOG(ERROR) << "Setting " << ash::system::kShouldSendRlzPingKey
                << " failed after " << RlzValueStoreChromeOS::kMaxRetryCount
                << " attempts.";
