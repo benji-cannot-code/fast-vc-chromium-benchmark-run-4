c60207bb57320b064660160c11800fc15ba30366
Bo Majewski <majewski@chromium.org>
2023-11-22 06:04:53 +0000

    [Files]: Separating callback from other parameters.
    RecentSource uses an approach where parameters given to GetRecentFiles
    are stored in the optional field. That field is reset when the recent
    source is done with collecting files. If it is done because the scan
    finished, this is fine. If it is done because the timeout occurred, it
    is possible that whoever holds to those params gets a null pointer,
    which causes a crash.
    
    This CL moves the callback argument out of params. This was what caused
    the params to be forced to be move-only type. By moving callback out we
    can make the params a copyable object. Thus early termination of the
    slow will not empty the params for stragglers.
    
    Bug: b:311934766
    Change-Id: I357133b6dc2ec45c1673c59b08c17067f7a333db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5051783
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227815}

--- chrome/browser/ash/fileapi/recent_source.cc
+++ chrome/browser/ash/fileapi/recent_source.cc
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/fileapi/recent_source.h"
 
-#include <utility>
-
-#include "base/check.h"
-
 namespace ash {
 
 RecentSource::Params::Params(storage::FileSystemContext* file_system_context,
@@ -16,23 +12,25 @@ RecentSource::Params::Params(storage::FileSystemContext* file_system_context,
                              const std::string& query,
                              const base::Time& cutoff_time,
                              const base::TimeTicks& end_time,
-                             FileType file_type,
-                             GetRecentFilesCallback callback)
+                             FileType file_type)
     : file_system_context_(file_system_context),
       origin_(origin),
       query_(query),
       cutoff_time_(cutoff_time),
       file_type_(file_type),
-      end_time_(end_time),
-      callback_(std::move(callback)) {
-  DCHECK(!callback_.is_null());
-}
+      end_time_(end_time) {}
+
+RecentSource::Params::Params(const Params& params)
+    : file_system_context_(params.file_system_context_),
+      origin_(params.origin_),
+      query_(params.query_),
+      cutoff_time_(params.cutoff_time_),
+      file_type_(params.file_type_),
+      end_time_(params.end_time_) {}
 
 bool RecentSource::Params::IsLate() const {
   return base::TimeTicks::Now() > end_time_;
 }
-
-RecentSource::Params::Params(Params&& other) = default;
 
 RecentSource::Params::~Params() = default;
 
