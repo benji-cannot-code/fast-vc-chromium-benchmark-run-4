7719302a4c0adba5afc269573666204ff9fa9c35
Rune Lillesveen <futhark@chromium.org>
2023-11-22 08:10:01 +0000

    Pass self WritingDirectionMode into ComputeOutOfFlowInsets
    WritingModeDirection was already retrieved on the call site.
    
    In preparation for applying inset-area to auto insets.
    
    Bug: 1477314
    Change-Id: I33b3fd3330ad09ab2c14207913183a0aa45b25bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5050164
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227849}

--- third_party/blink/renderer/core/layout/ng/ng_out_of_flow_layout_part.cc
+++ third_party/blink/renderer/core/layout/ng/ng_out_of_flow_layout_part.cc
@@ -1855,7 +1855,7 @@ OutOfFlowLayoutPart::TryCalculateOffset(
 
   const LogicalOofInsets insets = ComputeOutOfFlowInsets(
       candidate_style, node_info.constraint_space.AvailableSize(),
-      anchor_evaluator);
+      candidate_writing_direction, anchor_evaluator);
 
   {
     auto& document = node_info.node.GetDocument();
