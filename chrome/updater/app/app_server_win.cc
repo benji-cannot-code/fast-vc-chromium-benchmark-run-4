5905967be761cff5dd7e0e44051a9c8d0ab3f10f
Joshua Pawlicki <waffles@chromium.org>
2023-11-22 14:23:53 +0000

    Updater: Repair missing ksadmin shims during server startup.
    Also increases ksadmin logging to help diagnose errors.
    
    Fixed: 1503195
    Change-Id: I304873c67e6919a330118c486808e2ba7c9a01c9
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5049823
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227963}

--- chrome/updater/app/app_server_win.cc
+++ chrome/updater/app/app_server_win.cc
@@ -504,4 +504,11 @@ bool AppServerWin::MigrateLegacyUpdaters(
   return true;
 }
 
+void AppServerWin::RepairUpdater(UpdaterScope scope, bool is_internal) {
+  if (AreComInterfacesPresent(scope, is_internal)) {
+    return;
+  }
+  InstallComInterfaces(scope, is_internal);
+}
+
 }  // namespace updater
