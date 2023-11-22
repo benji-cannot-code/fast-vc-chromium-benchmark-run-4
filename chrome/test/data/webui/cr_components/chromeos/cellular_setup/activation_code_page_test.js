dd9afde15ab98a3b1f5baa99977f226207e8bb72
Ujjwal Pande <ujjwalpande@google.com>
2023-11-22 20:26:06 +0000

    esim-setup: Display warning on carrier locked devices
    Changes to display warning during eSIM setup if device
    is carrier locked.
    UX: https://docs.google.com/presentation/d/1lz6MT8S6b43omOQ8WCmKPlSWXKjW-fGtKBTTKi5gMxE/edit?resourcekey=0-4nMoXfvIeEkayQqkxEbf4A#slide=id.g22545e482c0_2_0
    
    BUG=b:294133684
    
    Change-Id: Id481247c87aad1b6596841befbd939837f5c04cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4785668
    Commit-Queue: Ujjwal Pande <ujjwalpande@google.com>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/main@{#1228171}

--- chrome/test/data/webui/cr_components/chromeos/cellular_setup/activation_code_page_test.js
+++ chrome/test/data/webui/cr_components/chromeos/cellular_setup/activation_code_page_test.js
@@ -6,7 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://os-settings/strings.m.js';
 import 'chrome://resources/ash/common/cellular_setup/activation_code_page.js';
 
+import {MojoInterfaceProviderImpl} from 'chrome://resources/ash/common/network/mojo_interface_provider.js';
+import {DeviceStateType, NetworkType} from 'chrome://resources/mojo/chromeos/services/network_config/public/mojom/network_types.mojom-webui.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {FakeNetworkConfig} from 'chrome://webui-test/chromeos/fake_network_config_mojom.js';
 import {flushTasks, waitAfterNextRender} from 'chrome://webui-test/polymer_test_util.js';
 import {eventToPromise} from 'chrome://webui-test/test_util.js';
 
@@ -30,6 +33,8 @@ suite('CrComponentsActivationCodePageTest', function() {
   /** @type {function(Function, number)} */
   let intervalFunction = null;
 
+  let networkConfigRemote;
+
   function flushAsync() {
     flush();
     // Use setTimeout to wait for the next macrotask.
@@ -50,6 +55,16 @@ suite('CrComponentsActivationCodePageTest', function() {
   function stopStreamFunction(stream) {}
 
   setup(async function() {
+    networkConfigRemote = new FakeNetworkConfig();
+    MojoInterfaceProviderImpl.getInstance().remote_ = networkConfigRemote;
+    loadTimeData.overrideValues({'isCellularCarrierLockEnabled': true});
+    networkConfigRemote.setDeviceStateForTest({
+      type: NetworkType.kCellular,
+      deviceState: DeviceStateType.kEnabled,
+      isCarrierLocked: true,
+    });
+    await flushAsync();
+
     activationCodePage = document.createElement('activation-code-page');
     await activationCodePage.setFakesForTesting(
         FakeBarcodeDetector, FakeImageCapture, setIntervalFunction,
@@ -612,4 +627,35 @@ suite('CrComponentsActivationCodePageTest', function() {
         activationCodeUpdatedEvent.detail.activationCode,
         ACTIVATION_CODE_VALID);
   });
+
+  test('check carrier lock warning', async function() {
+    assertTrue(!!activationCodePage.$$('#carrierLockWarningContainer'));
+  });
+
+  test(
+      'check carrier lock warning not displayed for consumer devices',
+      async function() {
+        networkConfigRemote.setDeviceStateForTest({
+          type: NetworkType.kCellular,
+          deviceState: DeviceStateType.kEnabled,
+          isCarrierLocked: false,
+        });
+        await flushAsync();
+        const page = document.createElement('activation-code-page');
+        assertFalse(!!page.$$('#carrierLockWarningContainer'));
+      });
+
+  test(
+      'check carrier lock warning not displayed with feature flag disabled',
+      async function() {
+        loadTimeData.overrideValues({'isCellularCarrierLockEnabled': false});
+        networkConfigRemote.setDeviceStateForTest({
+          type: NetworkType.kCellular,
+          deviceState: DeviceStateType.kEnabled,
+          isCarrierLocked: true,
+        });
+        await flushAsync();
+        const page = document.createElement('activation-code-page');
+        assertFalse(!!page.$$('#carrierLockWarningContainer'));
+      });
 });
