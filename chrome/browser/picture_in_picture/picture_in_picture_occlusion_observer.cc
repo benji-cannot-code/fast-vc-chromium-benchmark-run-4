90ccc7e62110ff5a799a36224f568b11806e6231
Tommy Steimel <steimel@chromium.org>
2023-11-22 14:57:44 +0000

    picture-in-picture: Add PictureInPictureOcclusionTracker
    This CL adds a PictureInPictureOcclusionTracker, which keeps track of
    picture-in-picture widgets and widgets that need to know when they're
    occluded by a picture-in-picture window, and notifies observers when
    the occlusion state changes.
    
    This will be used to ensure that security-sensitive UI does not get
    covered up by a picture-in-picture window, which would leave the user
    vulnerable to spoofing.
    
    This CL just adds the tracker, but does not register any
    picture-in-picture widgets to it nor any observers.
    
    Bug: 1498673
    Change-Id: I24b554ded8eeb39a76f6659d99076184f4fd539a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5041595
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227983}

---
+++ chrome/browser/picture_in_picture/picture_in_picture_occlusion_observer.cc
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/picture_in_picture/picture_in_picture_occlusion_observer.h"
+
+PictureInPictureOcclusionObserver::PictureInPictureOcclusionObserver() =
+    default;
+
+PictureInPictureOcclusionObserver::~PictureInPictureOcclusionObserver() =
+    default;
