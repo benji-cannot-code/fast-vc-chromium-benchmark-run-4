acb770ea4b1433ffbf7f2c068b1a4b90167d8b3c
Calder Kitagawa <ckitagawa@chromium.org>
2023-11-22 14:26:38 +0000

    [Tab Model] Add TabModelUtils#selectTabById
    Add a method to select a tab by its ID. This will be used extensively in
    https://chromium-review.googlesource.com/c/chromium/src/+/5049824
    
    Bug: 1495121
    Change-Id: I9c3e5abc719ea12462e3bd7e585716c6f4bd470e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5050984
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227967}

--- chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelUtils.java
+++ chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelUtils.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tabmodel;
 
+import androidx.annotation.NonNull;
+
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabSelectionType;
 import org.chromium.content_public.browser.WebContents;
@@ -144,6 +146,27 @@ public class TabModelUtils {
         if (tab == null) return null;
 
         return tab.getWebContents();
+    }
+
+    /**
+     * Selects a tab by its ID in the tab model selector.
+     *
+     * @param selector The {@link TabModelSelector} to act on.
+     * @param tabId The tab ID to select.
+     * @param type {@link TabSelectionType} how the tab selection was initiated.
+     * @param skipLoadingTab Whether to skip loading the Tab.
+     */
+    public static void selectTabById(
+            @NonNull TabModelSelector selector,
+            int tabId,
+            @TabSelectionType int tabSelectionType,
+            boolean skipLoadingTab) {
+        if (tabId == Tab.INVALID_TAB_ID) return;
+
+        TabModel model = selector.getModelForTabId(tabId);
+        if (model == null) return;
+
+        model.setIndex(getTabIndexById(model, tabId), tabSelectionType, skipLoadingTab);
     }
 
     /**
