5f8c3f86b3ee98c901710d11c4758265e4e7e0c1
Ankush Singh <ankushkush@google.com>
2023-11-22 09:17:55 +0000

    [sync] Fix value logged in some metrics
    Incorrect values were logged in Sync.BatchUpload.Requests and
    Sync.UnsyncedDataOnSignout. This CL fixes that.
    
    OBSOLETE_HISTOGRAMS=Sync.BatchUpload.Requests, Sync.UnsyncedDataOnSignout are replaced by Sync.BatchUpload.Requests2, Sync.UnsyncedDataOnSignout2 respectively because they reported incorrect values.
    
    Bug: 1483204,1496731
    Change-Id: I6c8c2ebc0b9a41102733b008910eea750f7b222f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5046560
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227882}

--- ios/chrome/browser/ui/authentication/signout_action_sheet/signout_action_sheet_coordinator.mm
+++ ios/chrome/browser/ui/authentication/signout_action_sheet/signout_action_sheet_coordinator.mm
@@ -285,8 +285,8 @@ typedef NS_ENUM(NSUInteger, SignedInUserState) {
                  syncer::CONTACT_INFO});
   if (!set.Empty()) {
     for (syncer::ModelType type : set) {
-      base::UmaHistogramEnumeration("Sync.UnsyncedDataOnSignout",
-                                    syncer::ModelTypeForHistograms(type));
+      base::UmaHistogramEnumeration("Sync.UnsyncedDataOnSignout2",
+                                    syncer::ModelTypeHistogramValue(type));
     }
     [self startActionSheetCoordinatorForSignout];
   } else {
