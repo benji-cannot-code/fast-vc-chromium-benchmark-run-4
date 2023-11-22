023ad310b190c21f27af70fe0a8c5f9d640f49b5
Dibyajyoti Pal <dibyapal@google.com>
2023-11-22 17:40:07 +0000

    [PWA/IPH] Set up IPH feature services for link capturing bubble
    This CL sets up the IPH services to show a custom action IPH bubble
    that redirects to the app settings page if accepted.
    
    Currently, the IPH is configured to always show up, and will be
    controlled by guardrails implemented in the web_app system.
    
    Bug: 1412856, b/302136219
    Change-Id: I6f9bd60ddfe889ce7b80762c6af21b8c50fbc883
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5031337
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1228070}

--- components/feature_engagement/public/feature_list.h
+++ components/feature_engagement/public/feature_list.h
@@ -446,6 +446,11 @@ DEFINE_VARIATION_PARAM(kIPHScalableIphHelpAppBasedTenFeature,
 DEFINE_VARIATION_PARAM(kIPHScalableIphGamingFeature, "IPH_ScalableIphGaming");
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+DEFINE_VARIATION_PARAM(kIPHDesktopPWAsLinkCapturingLaunch,
+                       "IPH_DesktopPWAsLinkCapturingLaunch");
+#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+
 #if !BUILDFLAG(IS_ANDROID) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
 DEFINE_VARIATION_PARAM(kIPHiOSPasswordPromoDesktopFeature,
                        "IPH_iOSPasswordPromoDesktop");
@@ -679,6 +684,10 @@ constexpr flags_ui::FeatureEntry::FeatureVariation
         VARIATION_ENTRY(kIPHScalableIphHelpAppBasedTenFeature),
         VARIATION_ENTRY(kIPHScalableIphGamingFeature),
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+        VARIATION_ENTRY(kIPHDesktopPWAsLinkCapturingLaunch),
+#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
 
 #if !BUILDFLAG(IS_ANDROID) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
         VARIATION_ENTRY(kIPHiOSPasswordPromoDesktopFeature),
