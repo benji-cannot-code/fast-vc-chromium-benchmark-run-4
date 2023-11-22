77ff773a83e690b3dbf81987b13832a0406c3b18
Mason Freed <masonf@chromium.org>
2023-11-22 01:02:15 +0000

    Reland: Put back the dialog SupportsFocus change
    The [1] CL attempted to change SupportsFocus for dialogs to always
    be true, but that caused some dialogs to become keyboard focusable
    when they shouldn't be.
    
    This CL re-lands that behavior, but with the keyboard focusability
    fixed: IsKeyboardFocusable needed to look explicitly at the things
    that allow keyboard focusable dialogs, such as tabindex. Those are
    handled by the base class Element::SupportsFocus.
    
    This also adds a DCHECK in focus_controller to make sure things
    that are IsKeyboardFocusable() do not have negative tabindexes.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/4959753
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/web_tests/external/wpt/html/semantics/interactive-elements/the-dialog-element/dialog-focusability.html
    
    Bug: 1499838
    Change-Id: I7e87d1f707c35545e48e53bba58b5ba4cbce5124
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5034524
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Reviewed-by: Di Zhang <dizhangg@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227721}

--- third_party/blink/renderer/core/page/focus_controller.cc
+++ third_party/blink/renderer/core/page/focus_controller.cc
@@ -527,6 +527,9 @@ inline bool IsNonFocusableFocusScopeOwner(Element& element) {
 }
 
 inline bool ShouldVisit(Element& element) {
+  DCHECK(!element.IsKeyboardFocusable() ||
+         FocusController::AdjustedTabIndex(element) >= 0)
+      << "Keyboard focusable element with negative tabindex" << element;
   return element.IsKeyboardFocusable() || element.DelegatesFocus() ||
          IsNonFocusableFocusScopeOwner(element);
 }
