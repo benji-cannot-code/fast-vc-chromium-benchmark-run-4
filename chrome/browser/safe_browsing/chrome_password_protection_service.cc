54736b8b7453ee3d12f0936ed5e45d6046401bab
Ivana Žužić <izuzic@google.com>
2023-11-22 18:22:50 +0000

    [UPMLocalSettings] LaunchCheckupOnDevice takes the account as argument
    The syncing account is passed from ChromePasswordManagerClient to
    CredentialLeakControllerAndroid and PasswordCheckupLauncherHelperImpl
    such that it could be used in Java to decide to show the Password
    Check UI for the account or local. Passing to Java and showing the UI
    comes in a follow-up.
    
    I've passed a new parameter to LaunchCheckupOnDevice (there is change
    in logic).
    
    Bug: b/306669939, 1504180
    Change-Id: Ia2c901ff53f5592718d54e6dcaf2afea4503dda8
    Coverage-Reason: Pre-existing lack of test coverage in the lines where
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5034924
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1228098}

--- chrome/browser/safe_browsing/chrome_password_protection_service.cc
+++ chrome/browser/safe_browsing/chrome_password_protection_service.cc
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/form_parsing/form_data_parser.h"
 #include "components/password_manager/core/browser/insecure_credentials_helper.h"
 #include "components/password_manager/core/browser/leak_detection_dialog_utils.h"
+#include "components/password_manager/core/browser/password_sync_util.h"
 #include "components/password_manager/core/browser/ui/password_check_referrer.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_service.h"
@@ -1051,9 +1052,14 @@ void ChromePasswordProtectionService::OpenChangePasswordUrl(
 #if BUILDFLAG(IS_ANDROID)
     JNIEnv* env = base::android::AttachCurrentThread();
     PasswordCheckupLauncherHelperImpl checkup_launcher;
+    const syncer::SyncService* sync_service =
+        SyncServiceFactory::GetForProfile(profile_);
+    std::string account = password_manager::sync_util::
+        GetAccountEmailIfSyncFeatureEnabledIncludingPasswords(sync_service);
     checkup_launcher.LaunchCheckupOnDevice(
         env, web_contents->GetTopLevelNativeWindow(),
-        password_manager::PasswordCheckReferrerAndroid::kPhishedWarningDialog);
+        password_manager::PasswordCheckReferrerAndroid::kPhishedWarningDialog,
+        account);
 #endif
 #if BUILDFLAG(FULL_SAFE_BROWSING)
     // Opens chrome://settings/passwords/check in a new tab.
