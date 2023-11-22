b6d1f9664da62174602455d1067af0d32a4ae523
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2023-11-22 09:39:53 +0000

    Revert "Update rust crates: font-types, read-fonts, skrifa, serde, serde_derive"
    This reverts commit 05cad0ed0aa1e14f4e6ee02569a84df20266269c.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5711712474890240
    
    Sample build with failed test: https://ci.chromium.org/b/8763766841545042945
    Affected test(s):
    [ninja://chrome/test:browser_tests/BrowsingDataModelBrowserTest.AttributionReportingAccessReportedCorrectly/All.0](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:browser_tests%2FBrowsingDataModelBrowserTest.AttributionReportingAccessReportedCorrectly%2FAll.0?q=VHash%3Aaf77ddf40439f3da)
    [ninja://chrome/test:browser_tests/BrowsingDataModelBrowserTest.AttributionReportingAccessReportedCorrectly/All.1](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:browser_tests%2FBrowsingDataModelBrowserTest.AttributionReportingAccessReportedCorrectly%2FAll.1?q=VHash%3Aaf77ddf40439f3da)
    [ninja://chrome/test:browser_tests/BrowsingDataModelBrowserTest.AttributionReportingAccessReportedCorrectly/All.2](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:browser_tests%2FBrowsingDataModelBrowserTest.AttributionReportingAccessReportedCorrectly%2FAll.2?q=VHash%3Aaf77ddf40439f3da)
    [ninja://chrome/test:browser_tests/BrowsingDataModelBrowserTest.AttributionReportingAccessReportedCorrectly/All.3](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:browser_tests%2FBrowsingDataModelBrowserTest.AttributionReportingAccessReportedCorrectly%2FAll.3?q=VHash%3Aaf77ddf40439f3da)
    [ninja://chrome/test:browser_tests/BrowsingDataModelBrowserTest.AuctionWinReportedCorrectly/All.0](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:browser_tests%2FBrowsingDataModelBrowserTest.AuctionWinReportedCorrectly%2FAll.0?q=VHash%3Aaf77ddf40439f3da)
    and 134 more ...
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5711712474890240&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F5050282&type=BUG
    
    Original change's description:
    > Update rust crates: font-types, read-fonts, skrifa, serde, serde_derive
    >
    > I ran gnrt update which rolled:
    > font-types: 0.4.0 => 0.4.1
    > serde: 1.0.192 => 1.0.193
    > serde_derive: 1.0.192 => 1.0.193
    >
    > There are also epoch-changing updates for read-fonts and skrifa which
    > pick up their LICENSE files to quiet warnings when running gnrt vendor:
    > read-fonts: 0.11.2 => 0.13.2
    > skrifa: 0.10.1 => 0.13.0
    >
    > I then ran gnrt vendor, and gnrt gen.
    >
    > Also:
    > - The paths in //skia/BUILD.gn are updated for the new epochs.
    > - The old epoch README.chromium and BUILD.gn files are deleted.
    >
    > Bug: 1503143
    > Change-Id: I0f59792b8bf9746395f0d39a84b4affc38d92b34
    > Cq-Include-Trybots: luci.chromium.try:android-rust-arm32-rel,android-rust-arm64-dbg,android-rust-arm64-rel,linux-rust-x64-dbg,linux-rust-x64-rel,mac-rust-x64-dbg,win-rust-x64-dbg,win-rust-x64-rel
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5050282
    > Reviewed-by: Dominik Röttsches <drott@chromium.org>
    > Commit-Queue: danakj <danakj@chromium.org>
    > Auto-Submit: danakj <danakj@chromium.org>
    > Reviewed-by: Florin Malita <fmalita@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1227634}
    >
    
    Bug: 1503143
    Change-Id: Id4585c2acd25bd6e7010022b0e062118636be14d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5051915
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Owners-Override: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227888}

--- third_party/rust/chromium_crates_io/vendor/read-fonts-0.13.2/src/tables/cblc.rs
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-//! The [CBLC (Color Bitmap Location)](https://docs.microsoft.com/en-us/typography/opentype/spec/cblc) table
-
-use super::bitmap::*;
-
-include!("../../generated/generated_cblc.rs");
