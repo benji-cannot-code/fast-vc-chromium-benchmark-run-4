08c4e03fd1ed03e24ad25a2ac9ff996b7eccc10a
Eliot Courtney <edcourtney@google.com>
2023-11-22 14:58:54 +0000

    Return framesink for exo windows.
    After recent changes to the framesink in exo, the correct framesink ID
    is no longer available using Window::GetFrameSinkId. This change lets
    FrameThrottlingController get correct framesink IDs for exo windows.
    
    Bug: 1504379
    Test: local debug logging
    Change-Id: Iab67fad94541a8005b759788da67630b6133f4c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5052005
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227984}

--- ash/frame_throttler/frame_throttling_controller.cc
+++ ash/frame_throttler/frame_throttling_controller.cc
@@ -27,10 +27,20 @@ namespace ash {
 
 namespace {
 
+std::unique_ptr<ThottleControllerWindowDelegate> instance = nullptr;
+
+viz::FrameSinkId GetFrameSinkId(const aura::Window* window) {
+  if (instance) {
+    return instance->GetFrameSinkIdForWindow(window);
+  }
+  return window->GetFrameSinkId();
+}
+
 void CollectFrameSinkIds(const aura::Window* window,
                          base::flat_set<viz::FrameSinkId>* frame_sink_ids) {
-  if (window->GetFrameSinkId().is_valid()) {
-    frame_sink_ids->insert(window->GetFrameSinkId());
+  auto id = GetFrameSinkId(window);
+  if (id.is_valid()) {
+    frame_sink_ids->insert(id);
     return;
   }
   for (auto* child : window->children()) {
@@ -50,7 +60,7 @@ void CollectBrowserFrameSinkIdsInWindow(
   if (inside_browser || ash::AppType::BROWSER ==
                             static_cast<ash::AppType>(
                                 window->GetProperty(aura::client::kAppType))) {
-    const auto& id = window->GetFrameSinkId();
+    auto id = GetFrameSinkId(window);
     if (id.is_valid() && ids.contains(id))
       frame_sink_ids->insert(id);
     inside_browser = true;
@@ -63,6 +73,11 @@ void CollectBrowserFrameSinkIdsInWindow(
 }
 
 }  // namespace
+
+void SetThottleControllerWindowDelegate(
+    std::unique_ptr<ThottleControllerWindowDelegate> delegate) {
+  instance = std::move(delegate);
+}
 
 ThrottleCandidates::ThrottleCandidates() = default;
 
@@ -337,7 +352,7 @@ void FrameThrottlingController::CollectLacrosWindowsInWindow(
   }
 
   if (inside_lacros) {
-    const auto& id = window->GetFrameSinkId();
+    auto id = GetFrameSinkId(window);
     if (id.is_valid() && ids.contains(id)) {
       DCHECK(lacros_window);
       candidates->insert(std::make_pair(lacros_window, id));
@@ -357,7 +372,7 @@ void FrameThrottlingController::CollectLacrosCandidates(
     aura::Window* window,
     base::flat_map<aura::Window*, viz::FrameSinkId>* candidates,
     aura::Window* lacros_window) {
-  const auto& id = window->GetFrameSinkId();
+  auto id = GetFrameSinkId(window);
   if (id.is_valid()) {
     DCHECK(lacros_window);
     candidates->insert(std::make_pair(lacros_window, id));
