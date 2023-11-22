0b586404f713254b918045245fcd60a93986fc84
Sky Malice <skym@chromium.org>
2023-11-22 00:08:54 +0000

    Minor bookmark style adjustments.
    Change-Id: I8933c9edd45138c3b698c338d916b112ffffa161
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018617
    Commit-Queue: Sky Malice <skym@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227695}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/ImprovedBookmarkRowCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/ImprovedBookmarkRowCoordinator.java
@@ -59,7 +59,7 @@ public class ImprovedBookmarkRowCoordinator {
 
         propertyModel.set(BookmarkManagerProperties.BOOKMARK_ID, bookmarkId);
 
-        // Title
+        // Title.
         if (displayPref == BookmarkRowDisplayPref.COMPACT && bookmarkItem.isFolder()) {
             propertyModel.set(
                     ImprovedBookmarkRowProperties.TITLE,
@@ -107,7 +107,7 @@ public class ImprovedBookmarkRowCoordinator {
             propertyModel.set(ImprovedBookmarkRowProperties.ACCESSORY_VIEW, null);
         }
 
-        // Icon
+        // Icon.
         resolveImagesForBookmark(propertyModel, bookmarkItem);
 
         return propertyModel;
