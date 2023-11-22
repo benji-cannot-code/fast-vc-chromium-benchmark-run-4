d482ea69ccff6f46ab6c7b7478dcc91fdf9f57b4
Peng Huang <penghuang@chromium.org>
2023-11-22 01:15:53 +0000

    graphite: enable wgpu::FeatureName::BGRA8UnormStorage
    Bug: 1504267
    Change-Id: Id8bed9d0dfff8732e6568f7059d686495664587c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5050281
    Reviewed-by: Austin Eng <enga@chromium.org>
    Auto-Submit: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227725}

--- gpu/command_buffer/service/dawn_context_provider.cc
+++ gpu/command_buffer/service/dawn_context_provider.cc
@@ -334,6 +334,7 @@ bool DawnContextProvider::Initialize(
   }
 
   const wgpu::FeatureName kOptionalFeatures[] = {
+      wgpu::FeatureName::BGRA8UnormStorage,
       wgpu::FeatureName::DualSourceBlending,
       wgpu::FeatureName::MultiPlanarFormatExtendedUsages,
       wgpu::FeatureName::MultiPlanarFormatP010,
