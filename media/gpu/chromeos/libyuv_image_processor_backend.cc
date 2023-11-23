b01449750cea6e89293df33e53579ac171e4a174
Jianfeng Liu <liujianfeng1994@gmail.com>
2023-11-23 04:13:29 +0000

    media/gpu: Add NV12 to ARGB conversion to libyuv image processor
    Add support for NV12 to ARGB conversion in the
    LibYUVImageProcessorBackend. At the moment only linux uses this
    conversion method so we make it linux only.
    
    Bug:1501504
    
    Change-Id: Ie350dbc082da99e4006d1c97a6ebb409ae7c8dbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5019187
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Reviewed-by: Justin Green <greenjustin@google.com>
    Commit-Queue: Jianfeng Liu <liujianfeng1994@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1228270}

--- media/gpu/chromeos/libyuv_image_processor_backend.cc
+++ media/gpu/chromeos/libyuv_image_processor_backend.cc
@@ -48,6 +48,9 @@ static constexpr struct {
 #define CONV(in, out, trans, result) \
   {Fourcc::in, Fourcc::out, Transform::trans, SupportResult::result}
     // Conversion.
+#if BUILDFLAG(IS_LINUX)
+    CONV(NV12, AR24, kConversion, Supported),
+#endif
     CONV(NV12, NV12, kConversion, Supported),
     CONV(YM16, NV12, kConversion, Supported),
     CONV(YM16, YU12, kConversion, Supported),
@@ -423,6 +426,12 @@ int LibYUVImageProcessorBackend::DoConversion(const VideoFrame* const input,
           fr->GetWritableVisibleData(VideoFrame::kUVPlane)), \
       fr->stride(VideoFrame::kUVPlane)
 
+#if BUILDFLAG(IS_LINUX)
+#define ARGB_DATA(fr)                                 \
+  fr->GetWritableVisibleData(VideoFrame::kARGBPlane), \
+      fr->stride(VideoFrame::kARGBPlane)
+#endif
+
 #define LIBYUV_FUNC(func, i, o)                      \
   libyuv::func(i, o, output->visible_rect().width(), \
                output->visible_rect().height())
@@ -559,6 +568,15 @@ int LibYUVImageProcessorBackend::DoConversion(const VideoFrame* const input,
                          Y_UV_DATA_W_10BIT(output));
     }
   }
+
+#if BUILDFLAG(IS_LINUX)
+  if (output->format() == PIXEL_FORMAT_ARGB) {
+    if (input_config_.fourcc == Fourcc(Fourcc::NV12)) {
+      return LIBYUV_FUNC(NV12ToARGB, Y_UV_DATA(input),
+                         ARGB_DATA(output));
+    }
+  }
+#endif
 
 #undef Y_U_V_DATA
 #undef Y_V_U_DATA
