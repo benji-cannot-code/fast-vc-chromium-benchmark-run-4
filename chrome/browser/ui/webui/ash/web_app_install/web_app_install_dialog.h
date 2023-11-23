c0a39355aa0f8c569a28466f3e7ad69c18b4d44a
Clement Yan <clamclamyan@google.com>
2023-11-23 03:48:07 +0000

    Pass install arguments to app install dialog from omnibox install flow.
    Creates a struct of dialog parameters in `web_app_dialog_utils` to
    display in install dialog and passes it to `web_app_install_dialog`.
    
    Bug: b/309861957
    Change-Id: I57a628da0e5e15eeadb3ebb581ade26998ccda21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5019260
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Clement Yan <clamclamyan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1228259}

--- chrome/browser/ui/webui/ash/web_app_install/web_app_install_dialog.h
+++ chrome/browser/ui/webui/ash/web_app_install/web_app_install_dialog.h
@@ -8,8 +8,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/ash/system_web_dialog_delegate.h"
 #include "chrome/browser/ui/webui/ash/web_app_install/web_app_install.mojom.h"
+// TODO(b/308717267): Remove this dependency when moving to mojom struct
+// definition for Lacros support.
+#include "chrome/browser/web_applications/web_app_install_info.h"
+#include "components/webapps/browser/installable/installable_data.h"
 
 namespace ash::web_app_install {
+
+struct ChromeOsAppInstallDialogParams {
+  std::optional<SkBitmap> icon_bitmap;
+  std::string name;
+  GURL url;
+  std::string description;
+  std::vector<webapps::Screenshot> screenshots;
+
+  ChromeOsAppInstallDialogParams(
+      std::unique_ptr<web_app::WebAppInstallInfo> web_app_info,
+      std::vector<webapps::Screenshot> screenshots);
+  ~ChromeOsAppInstallDialogParams();
+};
 
 // Defines the web dialog used for installing a web app.
 class WebAppInstallDialog : public SystemWebDialogDelegate {
@@ -19,7 +36,8 @@ class WebAppInstallDialog : public SystemWebDialogDelegate {
 
   // Creates and shows a new dialog for installing a web app. Returns true
   // if a new dialog has been effectively created.
-  static bool Show(gfx::NativeWindow parent);
+  static bool Show(gfx::NativeWindow parent,
+                   ChromeOsAppInstallDialogParams params);
 
   void OnDialogShown(content::WebUI* webui) override;
 
