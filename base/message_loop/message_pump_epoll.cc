1fc7de5ea6c29676895d714b4c50ded6674ef5f8
Jianhui Dai <jianhui.j.dai@intel.com>
2023-11-23 02:38:33 +0000

    MessagePumpEpoll: Use `epoll_create1` instead of `epoll_create`
    This CL migrates MessagePumpEpoll to use `epoll_create1` instead of
    the deprecated `epoll_create` for creating new epoll instances.[1]
    
    Additionally, many CrOS `seccomp` polices only permit the `epoll_create1`.
    
    [1] epoll_create(2) - Linux manual page
    https://man7.org/linux/man-pages/man2/epoll_create.2.html
    
    Bug: 1504426
    Change-Id: I35fcfdd88980011d0f9b86038ff02317098ee3ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5051846
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Auto-Submit: Jianhui J Dai <jianhui.j.dai@intel.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1228252}

--- base/message_loop/message_pump_epoll.cc
+++ base/message_loop/message_pump_epoll.cc
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 
 MessagePumpEpoll::MessagePumpEpoll() {
-  epoll_.reset(epoll_create(/*ignored_but_must_be_positive=*/1));
+  epoll_.reset(epoll_create1(/*flags=*/0));
   PCHECK(epoll_.is_valid());
 
   wake_event_.reset(eventfd(0, EFD_NONBLOCK));
