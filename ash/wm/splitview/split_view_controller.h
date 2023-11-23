66ac79eb2da52de2b1e4caafe6e048eaaa8bfaca
Sophie Wen <sophiewen@chromium.org>
2023-11-23 03:58:23 +0000

    snap-groups: Add LayoutDividerController to handle divider requests
    This adds `LayoutDividerController` to handle resizing via the
    divider.
    
    Currently only `SplitViewController` is a `LayoutDividerController`.
    `SnapGroup` will also implement `LayoutDividerController`.
    
    This is needed so that each `LayoutDividerController` can handle
    resizing via its divider position.
    
    Bug: b/310017895
    Change-Id: Ic33b201a4e95dedf559275d979320cf8b899a5d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029275
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1228262}

--- ash/wm/splitview/split_view_controller.h
+++ ash/wm/splitview/split_view_controller.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/overview_observer.h"
 #include "ash/wm/overview/overview_types.h"
 #include "ash/wm/snap_group/snap_group_controller.h"
+#include "ash/wm/splitview/layout_divider_controller.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "ash/wm/window_state_observer.h"
 #include "ash/wm/wm_event.h"
@@ -98,7 +99,8 @@ class ASH_EXPORT SplitViewController : public aura::WindowObserver,
                                        public AccessibilityObserver,
                                        public ash::KeyboardControllerObserver,
                                        public wm::ActivationChangeObserver,
-                                       public SnapGroupController::Observer {
+                                       public SnapGroupController::Observer,
+                                       public LayoutDividerController {
  public:
   // `LEFT` and `RIGHT` are named for the positions to which they correspond in
   // clamshell mode or primary-landscape-oriented tablet mode. In portrait-
@@ -453,6 +455,12 @@ class ASH_EXPORT SplitViewController : public aura::WindowObserver,
   // SnapGroupController::Observer:
   void OnSnapGroupCreated() override;
   void OnSnapGroupRemoved() override;
+
+  // LayoutDividerController:
+  void StartResizeWithDivider(const gfx::Point& location_in_screen) override;
+  void UpdateResizeWithDivider(const gfx::Point& location_in_screen) override;
+  void EndResizeWithDivider(const gfx::Point& location_in_screen) override;
+  aura::Window::Windows GetLayoutWindows() const override;
 
  private:
   friend class SplitViewControllerTest;
