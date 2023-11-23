1b756d7ad330cd005eea916c45a06dbf9d6a601d
Mickey Burks <mickeyburks@chromium.org>
2023-11-23 15:10:01 +0000

    [What's New] Send Accept-Language header when checking for page existence
    Bug: 1504642
    Change-Id: Ie71e2610aa9ba8426426c4bd633f5d44a6b68c75
    Low-Coverage-Reason: HARD_TO_TEST Difficult to mock network request
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5055595
    Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1228457}

--- chrome/browser/ui/webui/whats_new/whats_new_util.cc
+++ chrome/browser/ui/webui/whats_new/whats_new_util.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/reduce_accept_language_controller_delegate.h"
 #include "net/base/url_util.h"
 #include "net/http/http_util.h"
 #include "services/network/public/cpp/resource_request.h"
@@ -233,6 +234,20 @@ class WhatsNewFetcher : public BrowserListObserver {
         g_browser_process->system_network_context_manager()
             ->GetURLLoaderFactory();
     auto request = std::make_unique<network::ResourceRequest>();
+
+    // Inform the server of the top browser language via the
+    // Accept-Language header.
+    if (auto* profile = browser->profile()) {
+      if (auto* delegate =
+              profile->GetReduceAcceptLanguageControllerDelegate()) {
+        auto languages = delegate->GetUserAcceptLanguages();
+        if (!languages.empty()) {
+          request->headers.SetHeader(request->headers.kAcceptLanguage,
+                                     languages.front());
+        }
+      }
+    }
+
     // Don't allow redirects when checking if the page is valid for the current
     // milestone.
     request->url = server_url;
