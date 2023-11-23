40c93e70b651f5d12c9de417669b71554d2e552a
Mohammed Abdon <mohammedabdon@google.com>
2023-11-23 11:11:11 +0000

    Introduce Essential search manager
    Essential search manager is the class which is responsible for fetching
    SOCS cookie and adding it to the user cookie jar to make sure that
    search done through google.com would only use essential cookie and data.
    
    Bug: b/312542928
    Change-Id: Ic298469a56134cd8feec046b5f36a00b7dfa07b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5050802
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Mohammed Abdon <mohammedabdon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1228377}

--- chromeos/constants/chromeos_features.cc
+++ chromeos/constants/chromeos_features.cc
@@ -118,6 +118,11 @@ BASE_FEATURE(kDisableQuickAnswersV2Translation,
              "DisableQuickAnswersV2Translation",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Enables Essential Search in Omnibox for both launcher and browser.
+BASE_FEATURE(kEssentialSearch,
+             "EssentialSearch",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Enable experimental goldfish web app isolation.
 BASE_FEATURE(kExperimentalWebAppStoragePartitionIsolation,
              "ExperimentalWebAppStoragePartitionIsolation",
@@ -258,6 +263,10 @@ bool IsDeskProfilesEnabled() {
 #else
   return base::FeatureList::IsEnabled(kDeskProfiles);
 #endif
+}
+
+bool IsEssentialSearchEnabled() {
+  return base::FeatureList::IsEnabled(kEssentialSearch);
 }
 
 bool IsIWAForTelemetryExtensionAPIEnabled() {
