2d714593eb9ffc827ca58fb8a484d3d0827b781f
Elad Alon <eladalon@chromium.org>
2023-11-23 13:06:58 +0000

    [CSC][Zoom] Implement getMinZoomLevel and getMaxZoomLevel
    The values used in the browser process are derived
    from constant factors derived in Blink, allowing for
    a simple implementation that just reads them synchronously.
    
    Bug: 1466247
    Change-Id: I91510a12df484f198eb1ae4b2cf2d6e7ce2b6bbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5054203
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Auto-Submit: Elad Alon <eladalon@chromium.org>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Commit-Queue: Tove Petersson <tovep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1228406}

--- third_party/blink/renderer/modules/mediastream/capture_controller.cc
+++ third_party/blink/renderer/modules/mediastream/capture_controller.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/mediastream/capture_controller.h"
 
+#include "third_party/blink/public/common/page/page_zoom.h"
 #include "third_party/blink/renderer/modules/mediastream/media_stream_video_track.h"
 #include "third_party/blink/renderer/modules/mediastream/user_media_client.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
@@ -97,15 +98,17 @@ ScriptPromise CaptureController::sendWheel(CapturedWheelAction* action) {
 }
 
 int CaptureController::getMinZoomLevel() {
-  // TODO(crbug.com/1466247): Implement.
-  NOTIMPLEMENTED();
-  return 0;
+  // We expect `100 * kMinimumPageZoomFactor` to be an integer. But if it's not,
+  // over-reporting the minimum is preferable, as it would mean the application
+  // still asks to set zoom levels which aren't below the minimum.
+  return static_cast<int>(std::ceil(100 * kMinimumPageZoomFactor));
 }
 
 int CaptureController::getMaxZoomLevel() {
-  // TODO(crbug.com/1466247): Implement.
-  NOTIMPLEMENTED();
-  return 0;
+  // We expect `100 * kMaximumPageZoomFactor` to be an integer. But if it's not,
+  // under-reporting the maximum is preferable, as it would mean the application
+  // still asks to set zoom levels which aren't above the maximum.
+  return static_cast<int>(std::floor(100 * kMaximumPageZoomFactor));
 }
 
 ScriptPromise CaptureController::getZoomLevel() {
