3348066a6d970c702f476adeff2b7f01bc528bff
Dominique Fauteux-Chapleau <domfc@chromium.org>
2023-11-23 13:26:19 +0000

    Clean up kEnableLocalScanAfterPreview
    Bug: b/312706269
    Change-Id: I5e0e4900465b484d8fdce6d56f0fdc7db52548e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5054299
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1228411}

--- chrome/browser/ui/webui/print_preview/print_preview_handler_unittest.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_handler_unittest.cc
@@ -1606,10 +1606,6 @@ class ContentAnalysisPrintPreviewHandlerTest
     : public PrintPreviewHandlerTest,
       public testing::WithParamInterface<bool> {
  public:
-  ContentAnalysisPrintPreviewHandlerTest() {
-    feature_list_.InitAndEnableFeature(features::kEnableLocalScanAfterPreview);
-  }
-
   void SetUp() override {
     enterprise_connectors::ContentAnalysisDelegate::SetFactoryForTesting(
         base::BindRepeating(
@@ -1669,7 +1665,6 @@ class ContentAnalysisPrintPreviewHandlerTest
   void WaitForScan() { run_loop_.Run(); }
 
  private:
-  base::test::ScopedFeatureList feature_list_;
   base::RunLoop run_loop_;
 
   // This installs a fake SDK manager that creates fake SDK clients when
