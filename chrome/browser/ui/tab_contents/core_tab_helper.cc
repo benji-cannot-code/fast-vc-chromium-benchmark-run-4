d97763896e0d7c273fb1e94e5e6fd853c9d29e1a
Daniel Cheng <dcheng@chromium.org>
2023-11-23 00:58:14 +0000

    Use base::FunctionRef for BrowserPluginGuestManager.
    The callbacks are always invoked synchronously, so using
    base::FunctionRef allows the callers to use capturing lambdas and write
    simpler code.
    
    Change-Id: I2b4269670862fa75d5ce62053fbe92e85b7f7cb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5051069
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Owners-Override: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1228240}

--- chrome/browser/ui/tab_contents/core_tab_helper.cc
+++ chrome/browser/ui/tab_contents/core_tab_helper.cc
@@ -470,8 +470,9 @@ bool CoreTabHelper::GetStatusTextForWebContents(std::u16string* status_text,
     if (!guest_manager)
       return false;
     return guest_manager->ForEachGuest(
-        source, base::BindRepeating(&CoreTabHelper::GetStatusTextForWebContents,
-                                    status_text));
+        source, [&](content::WebContents* contents) {
+          return GetStatusTextForWebContents(status_text, contents);
+        });
 #else  // !BUILDFLAG(ENABLE_EXTENSIONS)
     return false;
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
@@ -552,8 +553,9 @@ bool CoreTabHelper::GetStatusTextForWebContents(std::u16string* status_text,
     return false;
 
   return guest_manager->ForEachGuest(
-      source, base::BindRepeating(&CoreTabHelper::GetStatusTextForWebContents,
-                                  status_text));
+      source, [&](content::WebContents* contents) {
+        return GetStatusTextForWebContents(status_text, contents);
+      });
 #else  // !BUILDFLAG(ENABLE_EXTENSIONS)
   return false;
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
--- content/public/browser/browser_plugin_guest_manager.cc
+++ content/public/browser/browser_plugin_guest_manager.cc
@@ -7,8 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-bool BrowserPluginGuestManager::ForEachGuest(WebContents* owner_web_contents,
-                                             const GuestCallback& callback) {
+bool BrowserPluginGuestManager::ForEachGuest(
+    WebContents* owner_web_contents,
+    base::FunctionRef<bool(WebContents*)> fn) {
   return false;
 }
 
