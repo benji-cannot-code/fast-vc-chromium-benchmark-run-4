b4284e6f62010561f8efa4fb909e762dc3cea300
Ernesto Izquierdo Clua <eic@google.com>
2023-11-23 23:31:36 +0000

    [ios][auth-on-entry2] Add more egtests for PWM dismissal
    This patch tests the proper dismissal of the Password Manager UI when
    local authentication fails while the user has navigated several levels
    into the UI (e.g. Password Manager > Password Checkup > Password Issues)
    
    Bug: 1462419
    Change-Id: Ic323a09a10c6a0c7e5b756a904351a15d6fbe906
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5037481
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1228600}

--- ios/chrome/browser/ui/settings/password/password_manager_egtest.mm
+++ ios/chrome/browser/ui/settings/password/password_manager_egtest.mm
@@ -63,8 +63,10 @@ using chrome_test_util::ButtonWithAccessibilityLabelId;
 using chrome_test_util::NavigationBarCancelButton;
 using chrome_test_util::NavigationBarDoneButton;
 using chrome_test_util::PasswordsTableViewMatcher;
+using chrome_test_util::SettingsCollectionView;
 using chrome_test_util::SettingsDoneButton;
 using chrome_test_util::SettingsMenuBackButton;
+using chrome_test_util::SettingsNavigationBar;
 using chrome_test_util::TabGridEditButton;
 using chrome_test_util::TextFieldForCellWithLabelId;
 using chrome_test_util::TurnTableViewSwitchOn;
@@ -82,7 +84,9 @@ using password_manager_test_utils::kScrollAmount;
 using password_manager_test_utils::NavigationBarEditButton;
 using password_manager_test_utils::OpenPasswordManager;
 using password_manager_test_utils::PasswordDetailPassword;
+using password_manager_test_utils::PasswordSettingsTableView;
 using password_manager_test_utils::PasswordTextfieldForUsernameAndSites;
+using password_manager_test_utils::ReauthenticationController;
 using password_manager_test_utils::SavePasswordForm;
 using password_manager_test_utils::TapNavigationBarEditButton;
 using password_manager_test_utils::UsernameTextfieldForUsernameAndSites;
@@ -558,6 +562,12 @@ void CheckPasswordManagerWidgetPromoInstructionScreenVisible(
       assertWithMatcher:grey_sufficientlyVisible()];
 }
 
+void CheckVisibilityOfElement(id<GREYMatcher> matcher, bool is_visible) {
+  [[EarlGrey selectElementWithMatcher:matcher]
+      assertWithMatcher:is_visible ? grey_sufficientlyVisible()
+                                   : grey_notVisible()];
+}
+
 // Opens the instructions for enabling the Password Manager Widget.
 void OpenPasswordManagerWidgetPromoInstructions() {
   OpenPasswordManager();
@@ -700,7 +710,10 @@ void OpenPasswordManagerWidgetPromoInstructions() {
           isRunningTest:@selector(testOpenPasswordManagerWithSuccessfulAuth)] ||
       [self isRunningTest:@selector(testOpenPasswordManagerWithFailedAuth)] ||
       [self isRunningTest:@selector
-            (testOpenPasswordManagerWithWithoutPasscodeSet)]) {
+            (testOpenPasswordManagerWithWithoutPasscodeSet)] ||
+      [self isRunningTest:@selector
+            (testOpenPasswordSettingsSubmenuWithFailedAuth)] ||
+      [self isRunningTest:@selector(testAddNewPasswordWithFailedAuth)]) {
     config.features_enabled.push_back(
         password_manager::features::kIOSPasswordAuthOnEntry);
     config.features_enabled.push_back(
@@ -2298,6 +2311,43 @@ void OpenPasswordManagerWidgetPromoInstructions() {
       assertWithMatcher:grey_textFieldValue(@"new password")];
 }
 
+// Validates that the Password Manager UI is dismissed if local authentication
+// fails while within the Add Password UI.
+- (void)testAddNewPasswordWithFailedAuth {
+  OpenPasswordManager();
+
+  // Press "Add".
+  [[EarlGrey selectElementWithMatcher:AddPasswordToolbarButton()]
+      performAction:grey_tap()];
+
+  CheckVisibilityOfElement(/*matcher=*/AddPasswordSaveButton(),
+                           /*is_visible=*/true);
+
+  [PasswordSettingsAppInterface mockReauthenticationModuleExpectedResult:
+                                    ReauthenticationResult::kFailure];
+  [PasswordSettingsAppInterface
+      mockReauthenticationModuleShouldReturnSynchronously:NO];
+
+  [[AppLaunchManager sharedManager] backgroundAndForegroundApp];
+
+  // Verify that Add Password UI is covered by Reauthentication UI until local
+  // authentication is passed.
+  CheckVisibilityOfElement(/*matcher=*/AddPasswordSaveButton(),
+                           /*is_visible=*/false);
+  CheckVisibilityOfElement(/*matcher=*/ReauthenticationController(),
+                           /*is_visible=*/true);
+
+  [PasswordSettingsAppInterface mockReauthenticationModuleReturnMockedResult];
+
+  // Password Manager should be dismissed leaving the Settings UI visible.
+  CheckVisibilityOfElement(/*matcher=*/AddPasswordSaveButton(),
+                           /*is_visible=*/false);
+  CheckVisibilityOfElement(/*matcher=*/ReauthenticationController(),
+                           /*is_visible=*/false);
+  CheckVisibilityOfElement(/*matcher=*/SettingsCollectionView(),
+                           /*is_visible=*/true);
+}
+
 // Checks that entering too long note while adding passwords blocks the save
 // button and displays a footer explanation.
 - (void)testAddPasswordLayoutWithLongNotes {
@@ -2713,9 +2763,43 @@ void OpenPasswordManagerWidgetPromoInstructions() {
   OpenPasswordManager();
   OpenSettingsSubmenu();
 
-  [[EarlGrey selectElementWithMatcher:grey_accessibilityID(
-                                          kPasswordsSettingsTableViewId)]
-      assertWithMatcher:grey_sufficientlyVisible()];
+  CheckVisibilityOfElement(/*matcher=*/PasswordSettingsTableView(),
+                           /*is_visible=*/true);
+}
+
+// Tests that the Password Manager UI is dismissed when local authentication
+// fails while within the Password Settings UI.
+- (void)testOpenPasswordSettingsSubmenuWithFailedAuth {
+  OpenPasswordManager();
+  OpenSettingsSubmenu();
+
+  CheckVisibilityOfElement(/*matcher=*/PasswordSettingsTableView(),
+                           /*is_visible=*/true);
+
+  [PasswordSettingsAppInterface mockReauthenticationModuleExpectedResult:
+                                    ReauthenticationResult::kFailure];
+  [PasswordSettingsAppInterface
+      mockReauthenticationModuleShouldReturnSynchronously:NO];
+
+  [[AppLaunchManager sharedManager] backgroundAndForegroundApp];
+
+  // Password Settings shouldn't be visible until successful authentication.
+  CheckVisibilityOfElement(/*matcher=*/PasswordSettingsTableView(),
+                           /*is_visible=*/false);
+  CheckVisibilityOfElement(/*matcher=*/ReauthenticationController(),
+                           /*is_visible=*/true);
+
+  [PasswordSettingsAppInterface mockReauthenticationModuleReturnMockedResult];
+
+  // The Password Manager UI should be gone leaving the Settings UI visible.
+  CheckVisibilityOfElement(/*matcher=*/PasswordSettingsTableView(),
+                           /*is_visible=*/false);
+  CheckVisibilityOfElement(/*matcher=*/PasswordsTableViewMatcher(),
+                           /*is_visible=*/false);
+  CheckVisibilityOfElement(/*matcher=*/ReauthenticationController(),
+                           /*is_visible=*/false);
+  CheckVisibilityOfElement(/*matcher=*/SettingsCollectionView(),
+                           /*is_visible=*/true);
 }
 
 // Tests that the detail text in this row reflects the status of the system
@@ -3398,9 +3482,8 @@ void OpenPasswordManagerWidgetPromoInstructions() {
   // Failed auth should dismiss the Password Manager, the Settings menu is
   // displayed.
   [PasswordSettingsAppInterface mockReauthenticationModuleReturnMockedResult];
-  [[EarlGrey
-      selectElementWithMatcher:chrome_test_util::SettingsCollectionView()]
-      assertWithMatcher:grey_sufficientlyVisible()];
+  CheckVisibilityOfElement(/*matcher=*/SettingsCollectionView(),
+                           /*is_visible=*/true);
 
   // Check password manager visit metric.
   CheckPasswordManagerVisitMetricCount(0);
--- ios/chrome/browser/ui/settings/password/password_manager_egtest_utils.mm
+++ ios/chrome/browser/ui/settings/password/password_manager_egtest_utils.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/strings/string_number_conversions.h"
 #import "base/strings/sys_string_conversions.h"
 #import "ios/chrome/browser/ui/settings/password/password_checkup/password_checkup_constants.h"
+#import "ios/chrome/browser/ui/settings/password/password_settings/password_settings_constants.h"
 #import "ios/chrome/browser/ui/settings/password/password_settings_app_interface.h"
 #import "ios/chrome/browser/ui/settings/password/passwords_table_view_constants.h"
 #import "ios/chrome/browser/ui/settings/password/reauthentication/reauthentication_constants.h"
@@ -148,6 +149,10 @@ id<GREYMatcher> DeleteButtonForUsernameAndSites(NSString* username,
 id<GREYMatcher> ReauthenticationController() {
   return grey_accessibilityID(
       password_manager::kReauthenticationViewControllerAccessibilityIdentifier);
+}
+
+id<GREYMatcher> PasswordSettingsTableView() {
+  return grey_accessibilityID(kPasswordsSettingsTableViewId);
 }
 
 GREYElementInteraction* GetInteractionForIssuesListItem(
