3f2ba35a6a15cb5db147d9721cc103c7a7709cdb
Ben Franz <bfranz@google.com>
2023-11-23 08:44:08 +0000

    kiosk: Close all browser windows at the beginning of a ChromeApp kiosk session
    This closes all browser windows that may have been opened whilst the
    kiosk session was started. Those browser windows are not immediately
    visible but could be accessed through backdoors, so we should close
    them.
    
    Bug: b/312113018
    Test: ChromeAppKioskShouldClosePreexistingBrowsers
    Change-Id: I3193df9257cdf76a424ea950a856de0c5defb06f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5046569
    Reviewed-by: Irina Fedorova <irfedorova@google.com>
    Reviewed-by: Jeroen Dhollander <jeroendh@google.com>
    Commit-Queue: Ben Franz <bfranz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1228336}

--- chrome/browser/chromeos/app_mode/kiosk_browser_window_handler.cc
+++ chrome/browser/chromeos/app_mode/kiosk_browser_window_handler.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/app_mode/kiosk_browser_window_handler.h"
 #include <memory>
 
+#include "base/check_deref.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/task/single_thread_task_runner.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_policies.h"
@@ -46,6 +47,20 @@ bool IsAshWithLacrosEnabled() {
 #endif
 }
 
+std::string GetUrlOfActiveTab(const Browser* browser) {
+  content::WebContents* active_tab =
+      browser->tab_strip_model()->GetActiveWebContents();
+  return active_tab ? active_tab->GetVisibleURL().spec() : std::string();
+}
+
+void CloseAllBrowserWindows() {
+  for (auto* browser : CHECK_DEREF(BrowserList::GetInstance())) {
+    LOG(WARNING) << "kiosk: Closing unexpected browser window with url: "
+                 << GetUrlOfActiveTab(browser);
+    browser->window()->Close();
+  }
+}
+
 }  // namespace
 
 const char kKioskNewBrowserWindowHistogram[] = "Kiosk.NewBrowserWindow";
@@ -77,6 +92,11 @@ KioskBrowserWindowHandler::KioskBrowserWindowHandler(
                          weak_ptr_factory_.GetWeakPtr()));
 #endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
 
+  if (!web_app_name.has_value()) {
+    // If this is ChromeApp kiosk, close all preexisting browser windows to
+    // avoid potential kiosk escapes.
+    CloseAllBrowserWindows();
+  }
   BrowserList::AddObserver(this);
 }
 
@@ -85,10 +105,7 @@ KioskBrowserWindowHandler::~KioskBrowserWindowHandler() {
 }
 
 void KioskBrowserWindowHandler::HandleNewBrowserWindow(Browser* browser) {
-  content::WebContents* active_tab =
-      browser->tab_strip_model()->GetActiveWebContents();
-  std::string url_string =
-      active_tab ? active_tab->GetVisibleURL().spec() : std::string();
+  std::string url_string = GetUrlOfActiveTab(browser);
 
   if (KioskSettingsNavigationThrottle::IsSettingsPage(url_string)) {
     base::UmaHistogramEnumeration(kKioskNewBrowserWindowHistogram,
