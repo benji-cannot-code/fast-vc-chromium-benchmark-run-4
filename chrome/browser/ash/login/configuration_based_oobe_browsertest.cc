05f1b5ff876aefc3151ff65b7a8a7c34905a7834
Kevin McNee <mcnee@chromium.org>
2023-11-23 16:28:30 +0000

    Remove MockNotificationObserver
    This is not being used.
    
    Bug: 268984
    Change-Id: Ic1cebcd9932109adc2ee21d86c713a229ee3787e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5055106
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1228489}

--- chrome/browser/ash/login/configuration_based_oobe_browsertest.cc
+++ chrome/browser/ash/login/configuration_based_oobe_browsertest.cc
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/language/core/browser/pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/test/browser_test.h"
-#include "content/public/test/mock_notification_observer.h"
 #include "ui/base/ime/ash/input_method_util.h"
 
 namespace ash {
