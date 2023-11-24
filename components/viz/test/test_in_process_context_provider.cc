035e7391953dbd2a87486e0911d71290228e681a
kylechar <kylechar@chromium.org>
2023-11-24 20:14:42 +0000

    Remove TestContextType::kGLES2WithRaster
    No tests uses this mode anymore so it, along with the corresponding
    RasterImplementationGLES usage, can be removed.
    
    Bug: 1453633
    Change-Id: I5acf1026582afa0c174fc6ed5fd9d5a64eba653b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5059510
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1228873}

--- components/viz/test/test_in_process_context_provider.cc
+++ components/viz/test/test_in_process_context_provider.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/service/gl/gpu_service_impl.h"
 #include "components/viz/test/test_gpu_service_holder.h"
 #include "gpu/command_buffer/client/gles2_implementation.h"
-#include "gpu/command_buffer/client/raster_implementation_gles.h"
 #include "gpu/command_buffer/client/shared_memory_limits.h"
 #include "gpu/command_buffer/common/context_creation_attribs.h"
 #include "gpu/config/skia_limits.h"
@@ -67,11 +66,9 @@ gpu::ContextResult TestInProcessContextProvider::BindToCurrentSequence() {
   gpu::ContextCreationAttribs attribs;
   attribs.bind_generates_resource = false;
 
-  if (type_ == TestContextType::kGLES2 ||
-      type_ == TestContextType::kGLES2WithRaster) {
+  if (type_ == TestContextType::kGLES2) {
     attribs.enable_gles2_interface = true;
-    attribs.enable_raster_interface =
-        type_ == TestContextType::kGLES2WithRaster;
+    attribs.enable_raster_interface = false;
     attribs.enable_oop_rasterization = false;
 
     gles2_context_ = std::make_unique<gpu::GLInProcessContext>();
@@ -102,12 +99,6 @@ gpu::ContextResult TestInProcessContextProvider::BindToCurrentSequence() {
       ContextSupport(), base::SingleThreadTaskRunner::GetCurrentDefault());
   cache_controller_->SetLock(GetLock());
 
-  if (type_ == TestContextType::kGLES2WithRaster) {
-    gles2_raster_impl_ =
-        std::make_unique<gpu::raster::RasterImplementationGLES>(
-            ContextGL(), ContextSupport(), ContextCapabilities());
-  }
-
   is_bound_ = true;
   return gpu::ContextResult::kSuccess;
 }
@@ -120,9 +111,8 @@ gpu::gles2::GLES2Interface* TestInProcessContextProvider::ContextGL() {
 
 gpu::raster::RasterInterface* TestInProcessContextProvider::RasterInterface() {
   CheckValidThreadOrLockAcquired();
-  CHECK_NE(type_, TestContextType::kGLES2);
-  return raster_context_ ? raster_context_->GetImplementation()
-                         : gles2_raster_impl_.get();
+  CHECK(raster_context_);
+  return raster_context_->GetImplementation();
 }
 
 gpu::ContextSupport* TestInProcessContextProvider::ContextSupport() {
