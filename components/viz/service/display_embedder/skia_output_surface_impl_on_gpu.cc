b5db3d1aee5d3cb74b089aac89279ca1fa91c4da
Peng Huang <penghuang@chromium.org>
2023-11-24 21:12:04 +0000

    Fix crash related context lost
    The problem is because SkiaOutputDeviceGL::ctor will call MakeCurrent
    again for enabling swap timestamp on the GLSurface. It can trigger
    context loss. Fix the problem by moving related code to
    SkiaOutputSurfaceImplOnGpu::InitializeForGL(). And we will detect the
    context lost there.
    
    Bug: 1412675
    Change-Id: Ib675190e7b5d2613a46d6ef497d2fc22b06d66a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5054411
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Auto-Submit: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1228882}

--- components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
+++ components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
@@ -1986,6 +1986,10 @@ bool SkiaOutputSurfaceImplOnGpu::InitializeForGL() {
       if (!gl_surface_) {
         return false;
       }
+
+      if (gl_surface_->SupportsSwapTimestamps()) {
+        gl_surface_->SetEnableSwapTimestamps();
+      }
     }
 
 #if BUILDFLAG(IS_MAC)
