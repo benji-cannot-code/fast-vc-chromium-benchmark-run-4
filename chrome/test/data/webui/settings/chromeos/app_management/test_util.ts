219e9c390ecf1d525fb1019476efcaf664346f70
Nergi Rahardi <nergi@google.com>
2023-11-24 06:16:55 +0000

    [Per-App Language] Create new app-language picker dialog
    Single-level language picker supporting suggested, and filtered
    supported languages. The selection mechanism is built manually as
    opposed to using `selection-enabled` system in `iron-list`, since
    there're some state management issues when managing `selected` props
    between 2 different iron-list (+ the filtered list from search query).
    
    TODO:
    - Sends selected language to ARC
    - Add last-selected-language (from other apps language selection)
    - Add this dialog to both entry points (app_mgmt & os_language_page)
    
    Doc: go/arcfw:arc-per-app-language
    UX spec: go/per-app-language-ux-spec
    Demo:
    https://drive.google.com/file/d/1WM33N57JFlJBVsMWOtYzcPAdD992Fjol/view?usp=sharing
    
    Bug: b:261200827
    Test: out_unittests/Debug/browser_tests
    --gtest_filter=*AppLanguageSelection*
    
    Change-Id: Iaf8fd5a0c6baa576ce0e30c54cfb52331380ab59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5040071
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Nergi Rahardi <nergi@google.com>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1228661}

--- chrome/test/data/webui/settings/chromeos/app_management/test_util.ts
+++ chrome/test/data/webui/settings/chromeos/app_management/test_util.ts
@@ -44,7 +44,7 @@ export function replaceStore(): TestAppManagementStore {
   return store;
 }
 
-export function isHidden(element: Element): boolean {
+export function isHidden(element: Element|null): boolean {
   return !isVisible(element);
 }
 
