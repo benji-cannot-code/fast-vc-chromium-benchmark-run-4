fba17fb0eb52afe88821757149054890f08a1c10
Henrique Nakashima <hnakashima@chromium.org>
2023-11-24 19:29:39 +0000

    [Android] Declare sCCTTextFragmentLookupAPIEnabled in ChromeFeatureList
    MutableFlagWithSafeDefault instance are being centralized in the
    FeatureLists.
    
    This is patch 1/3 to move the internal MutableFlagWithSafeDefault
    instance upstream
    
    Bug: 1504869
    Change-Id: I2364db7388b1ff0b7956fff701203b2403c08f23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5054507
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1228867}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -737,4 +737,9 @@ public abstract class ChromeFeatureList {
                             sFlagsCachedFullBrowser,
                             sFlagsCachedInMinimalBrowser,
                             sTestCachedFlags));
+
+    // MutableFlagWithSafeDefault instances
+    /* Alphabetical: */
+    public static final MutableFlagWithSafeDefault sCctTextFragmentLookupApiEnabled =
+            new MutableFlagWithSafeDefault(CCT_TEXT_FRAGMENT_LOOKUP_API_ENABLED, false);
 }
