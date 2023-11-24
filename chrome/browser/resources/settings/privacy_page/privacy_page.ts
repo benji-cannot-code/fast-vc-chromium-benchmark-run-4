04844627c7943733e9e4fe8fde3d67b807a03bf4
Mariam Ali <alimariam@google.com>
2023-11-24 12:49:56 +0000

    PS-Cleanup: Remove old cookies page entry point in privacy settings page
    Bug: 1378703, 1449833
    Change-Id: I0636c9d781778c2911a657c096fe27ce61e6584c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5054270
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Mariam Ali <alimariam@google.com>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1228745}

--- chrome/browser/resources/settings/privacy_page/privacy_page.ts
+++ chrome/browser/resources/settings/privacy_page/privacy_page.ts
@@ -57,7 +57,6 @@ interface BlockAutoplayStatus {
 export interface SettingsPrivacyPageElement {
   $: {
     clearBrowsingData: CrLinkRowElement,
-    cookiesLinkRow: CrLinkRowElement,
     permissionsLinkRow: CrLinkRowElement,
     securityLinkRow: CrLinkRowElement,
   };
@@ -102,8 +101,6 @@ export class SettingsPrivacyPageElement extends SettingsPrivacyPageElementBase {
           return loadTimeData.getBoolean('enableSafeBrowsingSubresourceFilter');
         },
       },
-
-      cookieSettingDescription_: String,
 
       enableBlockAutoplayContentSetting_: {
         type: Boolean,
@@ -172,11 +169,6 @@ export class SettingsPrivacyPageElement extends SettingsPrivacyPageElementBase {
             loadTimeData.getBoolean('isPrivacySandboxRestrictedNoticeEnabled'),
       },
 
-      isPrivacySandboxSettings4_: {
-        type: Boolean,
-        value: () => loadTimeData.getBoolean('isPrivacySandboxSettings4'),
-      },
-
       is3pcdRedesignEnabled_: {
         type: Boolean,
         value: () =>
@@ -226,12 +218,12 @@ export class SettingsPrivacyPageElement extends SettingsPrivacyPageElementBase {
           }
 
           if (routes.COOKIES) {
-            const selector =
-                loadTimeData.getBoolean('isPrivacySandboxSettings4') ?
-                '#thirdPartyCookiesLinkRow' :
-                '#cookiesLinkRow';
-            map.set(`${routes.COOKIES.path}_${routes.PRIVACY.path}`, selector);
-            map.set(`${routes.COOKIES.path}_${routes.BASIC.path}`, selector);
+            map.set(
+                `${routes.COOKIES.path}_${routes.PRIVACY.path}`,
+                '#thirdPartyCookiesLinkRow');
+            map.set(
+                `${routes.COOKIES.path}_${routes.BASIC.path}`,
+                '#thirdPartyCookiesLinkRow');
           }
 
           if (routes.TRACKING_PROTECTION) {
@@ -336,7 +328,6 @@ export class SettingsPrivacyPageElement extends SettingsPrivacyPageElementBase {
   private showClearBrowsingDataDialog_: boolean;
   private showPrivacyGuideDialog_: boolean;
   private enableSafeBrowsingSubresourceFilter_: boolean;
-  private cookieSettingDescription_: string;
   private enableBlockAutoplayContentSetting_: boolean;
   private blockAutoplayStatus_: BlockAutoplayStatus;
   private blockMidiByDefault_: boolean;
@@ -348,7 +339,6 @@ export class SettingsPrivacyPageElement extends SettingsPrivacyPageElementBase {
   private showNotificationPermissionsReview_: boolean;
   private isPrivacySandboxRestricted_: boolean;
   private isPrivacySandboxRestrictedNoticeEnabled_: boolean;
-  private isPrivacySandboxSettings4_: boolean;
   private is3pcdRedesignEnabled_: boolean;
   private privateStateTokensEnabled_: boolean;
   private autoPictureInPictureEnabled_: boolean;
@@ -388,13 +378,6 @@ export class SettingsPrivacyPageElement extends SettingsPrivacyPageElementBase {
         'onBlockAutoplayStatusChanged',
         (status: BlockAutoplayStatus) =>
             this.onBlockAutoplayStatusChanged_(status));
-
-    this.siteSettingsPrefsBrowserProxy_.getCookieSettingDescription().then(
-        (description: string) => this.cookieSettingDescription_ = description);
-
-    this.addWebUiListener(
-        'cookieSettingDescriptionChanged',
-        (description: string) => this.cookieSettingDescription_ = description);
 
     if (this.safetyCheckNotificationPermissionsEnabled_ && !this.isGuest_) {
       this.addWebUiListener(
@@ -595,23 +578,9 @@ export class SettingsPrivacyPageElement extends SettingsPrivacyPageElementBase {
     }
   }
 
-  private isPrivacySandboxSettings3Enabled_(): boolean {
-    return !this.isPrivacySandboxRestricted_ &&
-        !this.isPrivacySandboxSettings4_;
-  }
-
-  private isPrivacySandboxSettings4Enabled_(): boolean {
-    return (!this.isPrivacySandboxRestricted_ ||
-            this.isPrivacySandboxRestrictedNoticeEnabled_) &&
-        this.isPrivacySandboxSettings4_;
-  }
-
-  private isPrivacySandboxSettings4CookiesPageEnabled_(): boolean {
-    return this.isPrivacySandboxSettings4_ && !this.is3pcdRedesignEnabled_;
-  }
-
-  private isPrivacySandboxSettings3CookiesPageEnabled_(): boolean {
-    return !this.isPrivacySandboxSettings4_ && !this.is3pcdRedesignEnabled_;
+  private shouldShowAdPrivacy_(): boolean {
+    return !this.isPrivacySandboxRestricted_ ||
+        this.isPrivacySandboxRestrictedNoticeEnabled_;
   }
 
   private onSafetyHubButtonClick_() {
