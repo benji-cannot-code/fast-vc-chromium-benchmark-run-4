ae7379666bed668f32828eb42c8c10f755a1c4f5
Ben Reich <benreich@chromium.org>
2023-11-24 06:44:00 +0000

    [FilesTS] Convert main_window_component to TS
    Bug: b/289003444
    Change-Id: I3c201be162ed15b7ff2c50d969ebcfae7d0f2e57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5054666
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1228667}

--- ui/file_manager/file_manager/common/js/entry_utils.ts
+++ ui/file_manager/file_manager/common/js/entry_utils.ts
@@ -16,6 +16,7 @@ import {createDOMError} from './dom_utils.js';
 import {EntryList, FakeEntryImpl, VolumeEntry} from './files_app_entry_types.js';
 import {isArcVmEnabled, isPluginVmEnabled} from './flags.js';
 import {collator, getEntryLabel} from './translations.js';
+import {TrashEntry} from './trash.js';
 import {FileErrorToDomError} from './util.js';
 import {VolumeManagerCommon} from './volume_manager_types.js';
 
@@ -314,7 +315,7 @@ export function isTrashRoot(entry: Entry|FilesAppEntry) {
 /**
  * Returns true if the given entry is a descendent of Trash.
  */
-export function isTrashEntry(entry: Entry|FilesAppEntry) {
+export function isTrashEntry(entry: Entry|FilesAppEntry): entry is TrashEntry {
   return isTrashRootType(getRootType(entry));
 }
 
