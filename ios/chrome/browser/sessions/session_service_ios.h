0029ea3c5421abab77ed8b31ca4cf6642a1c4a34
Sylvain Defresne <sdefresne@chromium.org>
2023-11-24 15:24:36 +0000

    [ios] Add an explicit shutdown to SessionServiceIOS
    The TaskRunner will be destroyed after the ChromeBrowserState is
    destroyed, so explicitly stop the SessionServiceIOS (and remove
    its reference to the TaskRunner) before this happen.
    
    Fixed: 1502687
    Change-Id: Ib98048bd774f5add6557f87f1ca6635a39fb4ef7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5054091
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1228800}

--- ios/chrome/browser/sessions/session_service_ios.h
+++ ios/chrome/browser/sessions/session_service_ios.h
@@ -28,6 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (instancetype)init NS_UNAVAILABLE;
 
+// Called before destroying the task runner.
+- (void)shutdown;
+
 // Requests that `closure` is invoked when all pending background tasks
 // are complete. The `closure` may be invoked on a background sequence,
 // so it must be safe to be called from any sequence. Consider using
--- ios/chrome/browser/sessions/session_service_ios.mm
+++ ios/chrome/browser/sessions/session_service_ios.mm
@@ -90,6 +90,9 @@ using SaveSessionCallback =
 
 - (instancetype)init NS_UNAVAILABLE;
 
+// Called before destroying the task runner.
+- (void)shutdown;
+
 // Schedules a new requests to save data at `path` after `delay` using
 // `factory`. Ignored if a request scheduled for `path` with a closer
 // deadline is already scheduled.
@@ -121,6 +124,11 @@ using SaveSessionCallback =
     DCHECK(!_callback.is_null());
   }
   return self;
+}
+
+- (void)shutdown {
+  _callback = base::NullCallback();
+  _timer.Stop();
 }
 
 - (void)scheduleRequestForPath:(NSString*)path
@@ -238,6 +246,12 @@ using SaveSessionCallback =
         initWithCallback:base::BindRepeating(savingBlock)];
   }
   return self;
+}
+
+- (void)shutdown {
+  [_pendingRequests shutdown];
+  _pendingRequests = nil;
+  _taskRunner.reset();
 }
 
 - (void)shutdownWithClosure:(base::OnceClosure)closure {
