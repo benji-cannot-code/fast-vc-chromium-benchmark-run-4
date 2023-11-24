81ea393cf0a077188f92def47a6b6b0b68d71350
James Lee <ljjlee@google.com>
2023-11-24 11:09:26 +0000

    Disable flaky tests in InterestGroupBrowserTest on Android
    See crbug/1502155#c10 for context.
    
    Bug: 1502155
    Change-Id: Id27989cd3c61455431b8a78a0f18a276f71975b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5057778
    Auto-Submit: James Lee <ljjlee@google.com>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Owners-Override: James Lee <ljjlee@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: James Lee <ljjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1228723}

--- content/browser/interest_group/interest_group_browsertest.cc
+++ content/browser/interest_group/interest_group_browsertest.cc
@@ -17751,7 +17751,8 @@ IN_PROC_BROWSER_TEST_F(
 }
 
 // TODO(https://crbug.com/1502296): Fails too often on ChromeOS bots.
-#if BUILDFLAG(IS_CHROMEOS)
+// TODO(crbug/1502155): times out on Android bots.
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS)
 #define MAYBE_RunAdAuctionWithAdditionalBid \
   DISABLED_RunAdAuctionWithAdditionalBid
 #else
@@ -17923,7 +17924,8 @@ IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest,
 }
 
 // TODO(https://crbug.com/1502296): Fails too often on ChromeOS bots.
-#if BUILDFLAG(IS_CHROMEOS)
+// TODO(crbug/1502155): times out on Android bots.
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS)
 #define MAYBE_RunAdAuctionWithWinningAdditionalBidFromTwoWithNT \
   DISABLED_RunAdAuctionWithWinningAdditionalBidFromTwoWithNT
 #else
