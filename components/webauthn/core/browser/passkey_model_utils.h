514f197c682a73ee2f31b53a483633081ca27144
Martin Kreichgauer <martinkr@google.com>
2023-11-24 21:31:35 +0000

    sync: add PasskeysModel::CreatePasskey()
    CreatePasskey() generates a new WEBAUTHN_CREDENTIAL entity and adds it
    into the model. It takes care of generating and encrypting the private
    key, as well as shadowing existing credentials for the same RP ID/user
    ID pair.
    
    Bug: 1223853
    Change-Id: Idca66766a07265966adaf832c3b5caefd73df98c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5046019
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1228889}

--- components/webauthn/core/browser/passkey_model_utils.h
+++ components/webauthn/core/browser/passkey_model_utils.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/span.h"
+#include "components/webauthn/core/browser/passkey_model.h"
 
 namespace sync_pb {
 class WebauthnCredentialSpecifics;
@@ -25,6 +26,12 @@ namespace webauthn::passkey_model_utils {
 // this function, if applicable for the use case.
 std::vector<sync_pb::WebauthnCredentialSpecifics> FilterShadowedCredentials(
     base::span<const sync_pb::WebauthnCredentialSpecifics> passkeys);
+
+std::pair<sync_pb::WebauthnCredentialSpecifics, std::vector<uint8_t>>
+GeneratePasskeyAndEncryptSecrets(std::string_view rp_id,
+                                 const PasskeyModel::UserEntity& user_entity,
+                                 base::span<const uint8_t> trusted_vault_key,
+                                 int32_t trusted_vault_key_version);
 
 // Attempts to decrypt data from the `encrypted_data` field of `in` and
 // deserialize it into `out`. The return value indicates whether decryption and
