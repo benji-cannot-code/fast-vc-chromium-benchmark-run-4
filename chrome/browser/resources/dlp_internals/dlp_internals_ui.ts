4665efc8bc630b55a80176877601ed1417547056
Aya ElAttar <ayaelattar@chromium.org>
2023-11-24 13:56:43 +0000

    Add files tab in dlp-internals
    Added files tabs in html and added Typescript code
    to fetch the database entries
    
    Bug: b:304263732
    
    Change-Id: I1cbd6ba486335a7a05ecda0ff32749650947fc0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5056843
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Luca Accorsi <accorsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1228764}

--- chrome/browser/resources/dlp_internals/dlp_internals_ui.ts
+++ chrome/browser/resources/dlp_internals/dlp_internals_ui.ts
@@ -12,7 +12,7 @@ import './strings.m.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
-import {ContentRestriction, DataTransferEndpoint, DlpEvent, DlpEvent_Mode, DlpEvent_Restriction, DlpEvent_UserType, EndpointType, EventDestination, Level, PageHandler, PageHandlerInterface, ReportingObserverReceiver, WebContentsInfo} from './dlp_internals.mojom-webui.js';
+import {ContentRestriction, DataTransferEndpoint, DlpEvent, DlpEvent_Mode, DlpEvent_Restriction, DlpEvent_UserType, EndpointType, EventDestination, FileDatabaseEntry, Level, PageHandler, PageHandlerInterface, ReportingObserverReceiver, WebContentsInfo} from './dlp_internals.mojom-webui.js';
 import {getTemplate} from './dlp_internals_ui.html.js';
 import {ContentRestrictionMap, DestinationComponentMap, EndpointTypeMap, EventModeMap, EventRestrictionMap, EventUserTypeMap, LevelMap, WebContentsElement} from './dlp_utils.js';
 
@@ -45,12 +45,14 @@ class DlpInternalsUi extends PolymerElement {
 
     this.fetchClipboardSourceInfo();
     this.fetchContentRestrictionsInfo();
+    this.fetchFilesDatabaseEntries();
   }
 
   // Names of the top level page tabs.
   private tabNames_: string[] = [
     'Clipboard',
     'OnScreen',
+    'Files',
     'Reporting',
   ];
 
@@ -77,6 +79,9 @@ class DlpInternalsUi extends PolymerElement {
 
   // Web Contents Info.
   private webContentsElements_: WebContentsElement[] = [];
+
+  // Files Database Entries.
+  private filesEntries_: FileDatabaseEntry[] = [];
 
   private readonly pageHandler_: PageHandlerInterface;
   private readonly reportingObserver_: ReportingObserverReceiver;
@@ -126,6 +131,10 @@ class DlpInternalsUi extends PolymerElement {
     }
   }
 
+  private endpointTypeToString(type: EndpointType): string {
+    return EndpointTypeMap[type] || 'invalid';
+  }
+
   private async fetchContentRestrictionsInfo(): Promise<void> {
     this.pageHandler_.getContentRestrictionsInfo()
         .then((value: {webContentsInfo: WebContentsInfo[]}) => {
@@ -153,8 +162,27 @@ class DlpInternalsUi extends PolymerElement {
     }
   }
 
-  private endpointTypeToString(type: EndpointType): string {
-    return EndpointTypeMap[type] || 'invalid';
+  private async fetchFilesDatabaseEntries(): Promise<void> {
+    this.pageHandler_.getFilesDatabaseEntries()
+        .then((value: {dbEntries: FileDatabaseEntry[]}) => {
+          this.setFilesDatabaseEntries(value.dbEntries);
+        })
+        .catch((e: object) => {
+          console.warn(`getFilesDatabaseEntries failed: ${JSON.stringify(e)}`);
+        });
+  }
+
+  private setFilesDatabaseEntries(entries: FileDatabaseEntry[]) {
+    this.filesEntries_ = entries;
+    if (entries.length) {
+      this.notifySplices('filesEntries_', [{
+                           index: 0,
+                           addedCount: this.filesEntries_.length,
+                           object: this.filesEntries_,
+                           type: 'splice',
+                           removed: [],
+                         }]);
+    }
   }
 
   /** Implements ReportingObserverInterface */
@@ -218,6 +246,15 @@ class DlpInternalsUi extends PolymerElement {
 
   levelToString(level: Level): string {
     return LevelMap[level];
+  }
+
+  creationTimeToString(timestampSeconds: bigint): string {
+    if (timestampSeconds) {
+      const timestampMilli: number = Number(timestampSeconds) * 1000;
+      const timestamp: Date = new Date(timestampMilli);
+      return timestamp.toLocaleString();
+    }
+    return 'undefined';
   }
 }
 
