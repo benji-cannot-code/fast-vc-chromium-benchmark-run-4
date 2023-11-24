aeebb67473d309398ea109736a6e996dc5bf7b50
Tommy Martino <tmartino@google.com>
2023-11-24 20:28:58 +0000

    [iOS] Autofill Across iframes: Add child frames to extracted forms
    This CL adds new logic to the form extraction JS code, to scan a form
    for any iframes, generate a remote frame token for any that are found,
    and return that token in the `child_frames` field.
    
    Key bit that are out-of-scope for this CL:
    
    * Actually posting messages to the child frame, and registering the
      child frame with a registrar in the browser. The IDs returned to the
      browser layer currently don't mean anything (though they are correctly
      formatted).
    
    * Unowned iframes (those that aren't part of a <form>) are ignored for
      now.
    
    Bug: 1440471
    Change-Id: I03d6f2dc6fcc9ef1474af362661f7f7086072bc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5037479
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Auto-Submit: Tommy Martino <tmartino@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1228875}

---
+++ components/autofill/ios/form_util/resources/child_frame_registration_test.ts
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Test access into the Child Frame Registration lib.
+ */
+
+import {registerChildFrame} from '//components/autofill/ios/form_util/resources/child_frame_registration_lib.js';
+import {gCrWeb} from '//ios/web/public/js_messaging/resources/gcrweb.js';
+
+/**
+ * Calls registerChildFrame on each frame in the document. This is a convenience
+ * method for testing from the C++ layer.
+ * @return {string[]} The list of remote IDs sent to the child frames.
+ */
+function registerAllChildFrames(): string[] {
+  const ids: string[] = [];
+  for (const frame of document.getElementsByTagName('iframe')) {
+    ids.push(registerChildFrame((frame as HTMLIFrameElement)));
+  }
+  return ids;
+}
+
+gCrWeb.childFrameRegistrationTesting = {
+  registerChildFrame,
+  registerAllChildFrames,
+};
