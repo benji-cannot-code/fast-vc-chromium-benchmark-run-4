ef35bc8970cebf3c98e58cadcdadba7c0323cd60
Wenbo Jie <wenbojie@chromium.org>
2023-11-26 23:53:19 +0000

    [Files F2] Support the new search logic in the new tree
    When search is active and the search location is not the
    current folder, we should unselected the current directory in the
    tree: the tree item is not highlighted in the tree but the user
    can still click it to make it selected, and search will also be
    cleared after clicking.
    
    Demo:
    * Downloads: http://go/scrcast/NjU0NjcwNjkxNjE3OTk2OHw3ZTlmZTdkNS01ZA
    * Drive: http://go/scrcast/NTM4OTM0MTY0MjcxOTIzMnxkNDE2OGE3NS02MQ
    
    Bug: b:279092120
    Change-Id: I708561a0bccc14df64cc83123db079694808c21f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5039566
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229114}

--- chrome/browser/ash/file_manager/file_manager_browsertest.cc
+++ chrome/browser/ash/file_manager/file_manager_browsertest.cc
@@ -2014,6 +2014,11 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
             .NewDirectoryTree(),
         TestCase("searchFileSystemProvider").NewDirectoryTree(),
         TestCase("changingDirectoryClosesSearch").NewDirectoryTree(),
+        TestCase("verifyDriveLocationOption").NewDirectoryTree(),
+        TestCase("unselectCurrentDirectoryInTreeOnSearchInDownloads")
+            .NewDirectoryTree(),
+        TestCase("unselectCurrentDirectoryInTreeOnSearchInDrive")
+            .NewDirectoryTree(),
         // Section end - browser tests for new directory tree
         TestCase("searchDownloadsWithResults"),
         TestCase("searchDownloadsWithNoResults"),
@@ -2052,7 +2057,9 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("searchImageByContent").EnableLocalImageSearch(),
         TestCase("changingDirectoryClosesSearch"),
         TestCase("searchQueryLaunchParam"),
-        TestCase("verifyDriveLocationOption")));
+        TestCase("verifyDriveLocationOption"),
+        TestCase("unselectCurrentDirectoryInTreeOnSearchInDownloads"),
+        TestCase("unselectCurrentDirectoryInTreeOnSearchInDrive")));
 
 WRAPPED_INSTANTIATE_TEST_SUITE_P(
     Metrics, /* metrics.js */
