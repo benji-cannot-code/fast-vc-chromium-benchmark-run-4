4800c8059e5c3610d4693e10570e53423ea2da10
Dave Tapuska <dtapuska@chromium.org>
2023-11-27 22:26:23 +0000

    Pass isolate into dummy schedulers
    - Require isolate to be passed into dummy frame scheduler
    - Allow null isolate in page and agent group scheduler until all
    callsites are rewritten.
    - Provide a variant of NullExecutionContext that takes the isolate.
    
    Bug: 1315595
    Change-Id: I6c530ab0faee8c2fbe258503ce242e894f42c5d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5063692
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229558}

--- third_party/blink/renderer/platform/scheduler/public/dummy_schedulers.h
+++ third_party/blink/renderer/platform/scheduler/public/dummy_schedulers.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "third_party/blink/renderer/platform/platform_export.h"
+#include "v8/include/v8-forward.h"
 
 namespace blink {
 class FrameScheduler;
@@ -33,9 +34,12 @@ class WebThreadScheduler;
 // - Detached frames (should be fixed with frame:document lifetime refactoring).
 // - Tests
 
-PLATFORM_EXPORT std::unique_ptr<FrameScheduler> CreateDummyFrameScheduler();
-PLATFORM_EXPORT std::unique_ptr<PageScheduler> CreateDummyPageScheduler();
-PLATFORM_EXPORT AgentGroupScheduler* CreateDummyAgentGroupScheduler();
+PLATFORM_EXPORT std::unique_ptr<FrameScheduler> CreateDummyFrameScheduler(
+    v8::Isolate* isolate);
+PLATFORM_EXPORT std::unique_ptr<PageScheduler> CreateDummyPageScheduler(
+    v8::Isolate* isolate = nullptr);
+PLATFORM_EXPORT AgentGroupScheduler* CreateDummyAgentGroupScheduler(
+    v8::Isolate* isolate = nullptr);
 PLATFORM_EXPORT std::unique_ptr<WebThreadScheduler>
 CreateDummyWebMainThreadScheduler();
 
