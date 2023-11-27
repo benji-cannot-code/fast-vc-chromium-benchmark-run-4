de7f6cf9995a6b641fa6f64a51f029e0d7db6487
Nancy Wang <nancylingwang@chromium.org>
2023-11-27 09:52:59 +0000

    Load the gray spinner icon when the publisher is not available.
    When the AppStorage is enabled, apps are published before publishers
    are ready. If the app is launched when the publisher is not available,
    load the gray spinner icon.
    
    Modify AppServiceProxy to save the launch parameters if the publisher
    is not available. When the publisher registers, check the saved launch
    requests, and launch the app.
    
    If the app is removed, close the spinner, and remove the saved launch
    requests.
    
    BUG=1385932, b:307623365
    
    Change-Id: I845b4fb47be542541ff3146deadaaea4b6a7ce7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4848915
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229224}

--- chrome/browser/apps/app_service/app_service_proxy_ash.h
+++ chrome/browser/apps/app_service/app_service_proxy_ash.h
@@ -118,6 +118,7 @@ class AppServiceProxyAsh : public AppServiceProxyBase,
   apps::AppInstallService& AppInstallService();
 
   // apps::AppServiceProxyBase overrides:
+  void RegisterPublisher(AppType app_type, AppPublisher* publisher) override;
   void Uninstall(const std::string& app_id,
                  UninstallSource uninstall_source,
                  gfx::NativeWindow parent_window) override;
@@ -308,6 +309,10 @@ class AppServiceProxyAsh : public AppServiceProxyBase,
       base::flat_map<apps::ShortcutId,
                      std::unique_ptr<apps::ShortcutRemovalDialog>>;
 
+  // Map of app ID to a list of launch params.
+  using LaunchRequests =
+      std::map<std::string, std::vector<std::unique_ptr<LaunchParams>>>;
+
   bool IsValidProfile() override;
   void Initialize() override;
 
@@ -365,6 +370,11 @@ class AppServiceProxyAsh : public AppServiceProxyBase,
   // apps::AppServiceProxyBase overrides:
   void InitializePreferredAppsForAllSubscribers() override;
   void OnPreferredAppsChanged(PreferredAppChangesPtr changes) override;
+  // Displays spinner, and store the launch parameters to implement the launch
+  // task when the publisher is ready.
+  void OnPublisherNotReadyForLaunch(
+      const std::string& app_id,
+      std::unique_ptr<LaunchParams> launch_request) override;
   bool MaybeShowLaunchPreventionDialog(const apps::AppUpdate& update) override;
   void OnLaunched(LaunchCallback callback,
                   LaunchResult&& launch_result) override;
@@ -599,6 +609,9 @@ class AppServiceProxyAsh : public AppServiceProxyBase,
   // from the outstanding callback queue.
   std::list<std::pair<base::RepeatingCallback<bool(void)>, base::OnceClosure>>
       callback_list_;
+
+  // The launch requests when the publisher is not available.
+  LaunchRequests launch_requests_;
 
   base::flat_map<AppType, ShortcutPublisher*> shortcut_publishers_;
 
