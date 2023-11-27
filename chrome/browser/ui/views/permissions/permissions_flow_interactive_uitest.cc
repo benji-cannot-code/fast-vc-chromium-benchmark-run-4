5524d4f0fcedbae2729c310003c0cbcd5102d482
David Benjamin <davidben@chromium.org>
2023-11-27 01:00:34 +0000

    Fix some more incompatibilities with std::variant from narrowing
    I missed some more in
    https://chromium-review.googlesource.com/c/chromium/src/+/5013963
    
    Bug: 1373620
    Change-Id: I3c742bfcd41bffd6b64002a794e599722e6660bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5049641
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229121}

--- chrome/browser/ui/views/permissions/permissions_flow_interactive_uitest.cc
+++ chrome/browser/ui/views/permissions/permissions_flow_interactive_uitest.cc
@@ -147,7 +147,7 @@ IN_PROC_BROWSER_TEST_F(PermissionsFlowInteractiveUITest,
       PressButton(kLocationIconElementId),
       WaitForShow(PageInfoMainView::kPermissionsElementId),
       NameChildView(PageInfoMainView::kPermissionsElementId,
-                    kFirstPermissionRow, 0),
+                    kFirstPermissionRow, 0u),
       CheckViewProperty(
           kFirstPermissionRow, &PermissionToggleRowView::GetRowTitleForTesting,
           l10n_util::GetStringUTF16(IDS_SITE_SETTINGS_TYPE_NOTIFICATIONS)));
@@ -173,7 +173,7 @@ IN_PROC_BROWSER_TEST_F(PermissionsFlowInteractiveUITest,
       PressButton(kLocationIconElementId),
       WaitForShow(PageInfoMainView::kPermissionsElementId),
       NameChildView(PageInfoMainView::kPermissionsElementId,
-                    kFirstPermissionRow, 0),
+                    kFirstPermissionRow, 0u),
       CheckViewProperty(
           kFirstPermissionRow, &PermissionToggleRowView::GetRowTitleForTesting,
           l10n_util::GetStringUTF16(IDS_SITE_SETTINGS_TYPE_CAMERA)));
