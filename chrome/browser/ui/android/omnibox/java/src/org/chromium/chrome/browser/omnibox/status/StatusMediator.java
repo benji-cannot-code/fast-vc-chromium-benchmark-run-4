22913c70be20ff8a41c217f4bf49f474d0c0d840
Ted Choc <tedchoc@chromium.org>
2023-11-27 17:35:13 +0000

    Fix exception of accessing TabModelSelectorProfileSupplier early.
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelSelectorProfileSupplier.java;drc=3fc60b07b02a7d33167f48006536e406f0d8d588;l=105
    
    Introduced here:
    https://chromium-review.googlesource.com/c/chromium/src/+/5035419
    
    Bug: 1504616
    Change-Id: I6e7818243ed0d384aa4aaff49cef4059d04da2b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5055640
    Auto-Submit: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229396}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/status/StatusMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/status/StatusMediator.java
@@ -558,11 +558,11 @@ public class StatusMediator
             return SearchEngineUtils.getFallbackNavigationIcon(mBrandedColorScheme);
         }
 
-        var profile = mProfileSupplier.get();
-        if (profile == null) {
+        if (!mProfileSupplier.hasValue()) {
             return SearchEngineUtils.getFallbackSearchIcon(mBrandedColorScheme);
         }
 
+        var profile = mProfileSupplier.get();
         return SearchEngineUtils.getForProfile(profile).getSearchEngineLogo(mBrandedColorScheme);
     }
 
