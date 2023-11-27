334957968cb06453a2f8db4e25931371b20b9854
Geoff Huang <geoffhuang@google.com>
2023-11-27 23:10:38 +0000

    Add feature flag for vertical automotive back button toolbar
    Bug: 1503905
    Change-Id: I5af336af8a97fe0e7305daa22fbfaf17ab29a0df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5046581
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229604}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2033,6 +2033,9 @@ extern const char kUserNotesSidePanelDescription[];
 extern const char kUseSearchClickForRightClickName[];
 extern const char kUseSearchClickForRightClickDescription[];
 
+extern const char kVerticalAutomotiveBackButtonToolbarName[];
+extern const char kVerticalAutomotiveBackButtonToolbarDescription[];
+
 extern const char kVideoConferenceName[];
 extern const char kVideoConferenceDescription[];
 
