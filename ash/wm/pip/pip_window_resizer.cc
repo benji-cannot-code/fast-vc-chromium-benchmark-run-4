95fdfbb534f4422b09b8e834de785b44d38320f6
Kazuki Takise <takise@google.com>
2023-11-27 23:06:25 +0000

    Don't skip FlingOrSwipe() with ET_GESTURE_SWIPE
    `PipWindowResizer::FlingOrSwipe` is invoked in response to either
    `ET_SCROLL_FLING_START` or `ET_GESTURE_SWIPE`, but the function
    is skipped when it's called with `ET_GESTURE_SWIPE`.
    
    crrev.com/c/4884643 introduced the logic to skip this function for
    the "tilt" effect to ensure that PiP gets flinged after the
    ongoing gesture is completed, but this is causing inconsistency
    of states between `PipWindowResizer` and
    `ToplevelWindowEventHandler`.
    
    As the tilting effect is still behind the flag, we should
    prioritize fixing bugs for pinch resize, so this change reverts
    this specific logic. We'll explore other ways to properly support
    tilting later.
    
    Bug: b/313043450
    Test: Manually verified that PiP always got flinged to a display edge.
    Change-Id: Ie5984e17ac3b03665bf629b843532e558f408d0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5061449
    Auto-Submit: Kazuki Takise <takise@chromium.org>
    Commit-Queue: Kazuki Takise <takise@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229598}

--- ash/wm/pip/pip_window_resizer.cc
+++ ash/wm/pip/pip_window_resizer.cc
@@ -425,7 +425,8 @@ void PipWindowResizer::RevertDrag() {
 }
 
 void PipWindowResizer::FlingOrSwipe(ui::GestureEvent* event) {
-  if (event->type() != ui::ET_SCROLL_FLING_START) {
+  if (event->type() != ui::ET_SCROLL_FLING_START &&
+      event->type() != ui::ET_GESTURE_SWIPE) {
     return;
   }
 
