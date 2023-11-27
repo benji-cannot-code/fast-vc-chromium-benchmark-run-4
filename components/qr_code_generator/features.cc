33eeb88272540efaf1b64fd6ccecb4dee79622a0
Lukasz Anforowicz <lukasza@chromium.org>
2023-11-27 16:40:09 +0000

    Make `RustyQrCodeGenerator` enabled by default on non-CrOS platforms.
    Launch approval: https://launch.corp.google.com/launch/4248932
    
    Metrics from the Stable channel show no impact on the guardrail Metrics
    and an improvement in QR metrics.  More details can be found in the
    (Google-internal) doc here:
    https://docs.google.com/document/d/1JIwR5715Fce4gEa_vIft8pG6IB8tj5LawJus7vd9jng/edit?usp=sharing&resourcekey=0-zAG2e9OamVhueaeRaQY5rQ
    
    Bug: 1431991
    Change-Id: I48a6dd6f2267cd00ec5c579c30bcdeff53817600
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5061588
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229354}

--- components/qr_code_generator/features.cc
+++ components/qr_code_generator/features.cc
@@ -6,11 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/qr_code_generator/features.h"
 
 #include "base/feature_list.h"
+#include "build/build_config.h"
 
 namespace qr_code_generator {
 
 BASE_FEATURE(kRustyQrCodeGeneratorFeature,
              "RustyQrCodeGenerator",
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
+    BUILDFLAG(IS_ANDROID)
+             base::FEATURE_ENABLED_BY_DEFAULT);
+#else
              base::FEATURE_DISABLED_BY_DEFAULT);
+#endif
 
 }  // namespace qr_code_generator
