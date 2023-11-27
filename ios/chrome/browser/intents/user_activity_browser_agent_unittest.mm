84210ab9da1835bbb53beddab4f14af28a7a065a
Federica Germinario <fedegermi@google.com>
2023-11-27 14:38:36 +0000

    [ios] Add test coverage for intent OpenNewIncognitoTabIntent
    This CL adds a unittest for intent: OpenNewIncognitoTabIntent
    
    Bug: 1491344
    Change-Id: I33113aa7a9b63fea13db1f32a60b139c19f70176
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5054073
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229304}

--- ios/chrome/browser/intents/user_activity_browser_agent_unittest.mm
+++ ios/chrome/browser/intents/user_activity_browser_agent_unittest.mm
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/common/intents/OpenInChromeIntent.h"
 #import "ios/chrome/common/intents/OpenLatestTabIntent.h"
 #import "ios/chrome/common/intents/OpenLensIntent.h"
+#import "ios/chrome/common/intents/OpenNewIncognitoTabIntent.h"
 #import "ios/chrome/common/intents/OpenNewTabIntent.h"
 #import "ios/chrome/common/intents/OpenReadingListIntent.h"
 #import "ios/chrome/common/intents/OpenRecentTabsIntent.h"
@@ -990,6 +991,29 @@ TEST_F(UserActivityBrowserAgentTest,
   user_activity_browser_agent_->ContinueUserActivity(mock_user_activity, YES);
 
   EXPECT_EQ(VIEW_HISTORY,
+            [connection_information_ startupParameters].postOpeningAction);
+}
+
+// Tests that Chrome respond to open a new incognito tab.
+TEST_F(UserActivityBrowserAgentTest,
+       ContinueUserActivityIntentOpenNewIncognitoTab) {
+  NSUserActivity* user_activity =
+      [[NSUserActivity alloc] initWithActivityType:kSiriOpenNewIncognitoTab];
+
+  OpenNewIncognitoTabIntent* intent = [[OpenNewIncognitoTabIntent alloc] init];
+  INInteraction* interaction = [[INInteraction alloc] initWithIntent:intent
+                                                            response:nil];
+  id mock_user_activity = CreateMockNSUserActivity(user_activity, interaction);
+
+  user_activity_browser_agent_->ContinueUserActivity(mock_user_activity, YES);
+
+  EXPECT_EQ(ApplicationModeForTabOpening::INCOGNITO,
+            [connection_information_ startupParameters].applicationMode);
+  EXPECT_EQ(GURL(kChromeUINewTabURL),
+            [connection_information_ startupParameters].completeURL);
+  EXPECT_EQ(GURL(kChromeUINewTabURL),
+            [connection_information_ startupParameters].externalURL);
+  EXPECT_EQ(NO_ACTION,
             [connection_information_ startupParameters].postOpeningAction);
 }
 
