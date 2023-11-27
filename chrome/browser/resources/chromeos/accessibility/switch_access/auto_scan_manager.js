644540be0600229ff4884e2f697dac0e5e10a096
Anastasia Helfinstein <anastasi@google.com>
2023-11-27 20:59:31 +0000

    [Switch Access] Clean up AutoScanManager
    R=josiahk@google.com
    
    AX-Relnotes: n/a.
    Bug: b/261227533
    Test: SwitchAccessAutoScanManagerTest.*
    Change-Id: Ib038202d8506c25577eff77bf8202656de397129
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5011118
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1229493}

--- chrome/browser/resources/chromeos/accessibility/switch_access/auto_scan_manager.js
+++ chrome/browser/resources/chromeos/accessibility/switch_access/auto_scan_manager.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {Navigator} from './navigator.js';
 import {SwitchAccess} from './switch_access.js';
-import {Mode} from './switch_access_constants.js';
+import {ErrorType, Mode} from './switch_access_constants.js';
 
 /**
  * Class to handle auto-scan behavior.
@@ -13,28 +13,10 @@ import {Mode} from './switch_access_constants.js';
 export class AutoScanManager {
   /** @private */
   constructor() {
-    /**
-     * Auto-scan interval ID.
-     * @private {number|undefined}
-     */
+    /** @private {number|undefined} */
     this.intervalID_;
 
-    /**
-     * Length of the auto-scan interval for most contexts, in milliseconds.
-     * @private {number}
-     */
-    this.primaryScanTime_ = AutoScanManager.NOT_INITIALIZED;
-
-    /**
-     * Length of auto-scan interval for the on-screen keyboard in milliseconds.
-     * @private {number}
-     */
-    this.keyboardScanTime_ = AutoScanManager.NOT_INITIALIZED;
-
-    /**
-     * Whether auto-scanning is enabled.
-     * @private {boolean}
-     */
+    /** @private {boolean} */
     this.isEnabled_ = false;
 
     /**
@@ -42,22 +24,36 @@ export class AutoScanManager {
      * @private {boolean}
      */
     this.inKeyboard_ = false;
+
+    /**
+     * Length of auto-scan interval for the on-screen keyboard in milliseconds.
+     * @private {number}
+     */
+    this.keyboardScanTime_ = NOT_INITIALIZED;
+
+    /**
+     * Length of the auto-scan interval for most contexts, in milliseconds.
+     * @private {number}
+     */
+    this.primaryScanTime_ = NOT_INITIALIZED;
   }
 
   // ============== Static Methods ================
 
-  static get instance() {
-    if (!AutoScanManager.instance_) {
-      AutoScanManager.instance_ = new AutoScanManager();
+  static init() {
+    if (AutoScanManager.instance) {
+      throw SwitchAccess.error(
+          ErrorType.DUPLICATE_INITIALIZATION,
+          'Cannot call AutoScanManager.init() more than once.');
     }
-    return AutoScanManager.instance_;
+    AutoScanManager.instance = new AutoScanManager();
   }
 
   /**
    * Restart auto-scan under the current settings if it is currently running.
    */
   static restartIfRunning() {
-    if (AutoScanManager.instance.isRunning_()) {
+    if (AutoScanManager.instance?.isRunning_()) {
       AutoScanManager.instance.stop_();
       AutoScanManager.instance.start_();
     }
@@ -130,15 +126,15 @@ export class AutoScanManager {
    * @private
    */
   start_() {
-    if (this.primaryScanTime_ === AutoScanManager.NOT_INITIALIZED ||
-        this.intervalID_ || SwitchAccess.mode === Mode.POINT_SCAN) {
+    if (this.primaryScanTime_ === NOT_INITIALIZED || this.intervalID_ ||
+        SwitchAccess.mode === Mode.POINT_SCAN) {
       return;
     }
 
     let currentScanTime = this.primaryScanTime_;
 
     if (SwitchAccess.improvedTextInputEnabled() && this.inKeyboard_ &&
-        this.keyboardScanTime_ !== AutoScanManager.NOT_INITIALIZED) {
+        this.keyboardScanTime_ !== NOT_INITIALIZED) {
       currentScanTime = this.keyboardScanTime_;
     }
 
@@ -161,8 +157,13 @@ export class AutoScanManager {
   }
 }
 
+/** @type {AutoScanManager} */
+AutoScanManager.instance;
+
+// Private to module.
+
 /**
  * Sentinel value that indicates an uninitialized scan time.
- * @type {number}
+ * @const {number}
  */
-AutoScanManager.NOT_INITIALIZED = -1;
+const NOT_INITIALIZED = -1;
