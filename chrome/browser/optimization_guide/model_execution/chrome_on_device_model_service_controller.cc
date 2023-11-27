e8209bc945af3b519e632bcfaa51fd59e73e4c66
Clark DuVall <cduvall@chromium.org>
2023-11-27 23:01:36 +0000

    Add feature to record on-device metrics on startup
    This will get the estimated performance class and record a histogram.
    
    Bug: b/301660696
    Change-Id: I29a2dbf95ecd36848182bd589ec1c7fffc5795cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5064439
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229591}

--- chrome/browser/optimization_guide/model_execution/chrome_on_device_model_service_controller.cc
+++ chrome/browser/optimization_guide/model_execution/chrome_on_device_model_service_controller.cc
@@ -39,7 +39,8 @@ ChromeOnDeviceModelServiceController::~ChromeOnDeviceModelServiceController() {
 
 void ChromeOnDeviceModelServiceController::LaunchService() {
   CHECK(
-      base::FeatureList::IsEnabled(features::kOptimizationGuideOnDeviceModel));
+      base::FeatureList::IsEnabled(features::kOptimizationGuideOnDeviceModel) ||
+      base::FeatureList::IsEnabled(features::kLogOnDeviceMetricsOnStartup));
   if (service_remote_) {
     return;
   }
--- components/optimization_guide/core/optimization_guide_features.cc
+++ components/optimization_guide/core/optimization_guide_features.cc
@@ -331,6 +331,12 @@ BASE_FEATURE(kOptimizationGuideOnDeviceModel,
              "OptimizationGuideOnDeviceModel",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Whether the on device service is launched after a delay on startup to log
+// metrics.
+BASE_FEATURE(kLogOnDeviceMetricsOnStartup,
+             "LogOnDeviceMetricsOnStartup",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 size_t MaxRelatedSearchesCacheSize() {
   return GetFieldTrialParamByFeatureAsInt(
       kExtractRelatedSearchesFromPrefetchedZPSResponse,
@@ -967,6 +973,13 @@ int GetOnDeviceModelCrashCountBeforeDisable() {
       &features::kOptimizationGuideOnDeviceModel,
       "on_device_model_disable_crash_count", 3};
   return kOnDeviceModelDisableCrashCount.Get();
+}
+
+base::TimeDelta GetOnDeviceStartupMetricDelay() {
+  static const base::FeatureParam<base::TimeDelta> kOnDeviceStartupMetricDelay{
+      &kLogOnDeviceMetricsOnStartup, "on_device_startup_metric_delay",
+      base::Minutes(2)};
+  return kOnDeviceStartupMetricDelay.Get();
 }
 
 }  // namespace features
