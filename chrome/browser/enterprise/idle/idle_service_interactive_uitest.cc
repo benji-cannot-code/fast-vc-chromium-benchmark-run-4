cdf0fdf3fb5b2bf109e3b8a2bcc6df9fce20bab7
Irina Fedorova <irfedorova@google.com>
2023-11-27 09:45:04 +0000

    [Code Health] Enable IdleServiceTest.JustShowProfilePicker test
    The test was crashing on tear down, fix it with destruction profile
    picker.
    
    Fixed: 1502154
    Change-Id: Ibf95c583392936ffd3ef1526548cbfe7483d9018
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5055822
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Irina Fedorova <irfedorova@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1229221}

--- chrome/browser/enterprise/idle/idle_service_interactive_uitest.cc
+++ chrome/browser/enterprise/idle/idle_service_interactive_uitest.cc
@@ -135,6 +135,8 @@ class IdleServiceTest : public InProcessBrowserTest {
 
     keep_alive_ = std::make_unique<ScopedKeepAlive>(
         KeepAliveOrigin::BROWSER, KeepAliveRestartOption::DISABLED);
+
+    InProcessBrowserTest::SetUpInProcessBrowserTestFixture();
   }
 
   void TearDownOnMainThread() override {
@@ -153,10 +155,20 @@ class IdleServiceTest : public InProcessBrowserTest {
       // already doing the same thing.
       keep_alive_.reset();
     }
+
+    if (ProfilePicker::IsOpen()) {
+      // `ProfilePicker` prevents browsers from being closed. We need to destroy
+      // `ProfilePicker` first, and then tear down browsers.
+      ProfilePicker::Hide();
+    }
+
     InProcessBrowserTest::TearDownOnMainThread();
   }
 
-  void TearDownInProcessBrowserTestFixture() override { keep_alive_.reset(); }
+  void TearDownInProcessBrowserTestFixture() override {
+    keep_alive_.reset();
+    InProcessBrowserTest::TearDownInProcessBrowserTestFixture();
+  }
 
   void SetIdleTimeoutPolicies(
       policy::MockConfigurationPolicyProvider& policy_provider,
@@ -613,8 +625,7 @@ IN_PROC_BROWSER_TEST_F(IdleServiceTest, JustCloseBrowsers) {
   EXPECT_FALSE(ProfilePicker::IsOpen());
 }
 
-// TODO(crbug.com/1326685): Figure out why this test fails during teardown.
-IN_PROC_BROWSER_TEST_F(IdleServiceTest, DISABLED_JustShowProfilePicker) {
+IN_PROC_BROWSER_TEST_F(IdleServiceTest, JustShowProfilePicker) {
   EXPECT_CALL(idle_time_provider(), CalculateIdleTime())
       .WillOnce(Return(base::Seconds(58)));
   Profile* profile = browser()->profile();
@@ -638,8 +649,6 @@ IN_PROC_BROWSER_TEST_F(IdleServiceTest, DISABLED_JustShowProfilePicker) {
   EXPECT_FALSE(IsDialogOpen());
   EXPECT_FALSE(GetIdleBubble(browser()));
   EXPECT_TRUE(ProfilePicker::IsOpen());
-
-  // Browsers are still open.
   EXPECT_EQ(1, GetBrowserCount(profile));
 }
 
