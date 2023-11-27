fdbcef0b30cb1764aad17a2e467638c31590d18f
Asami Doi <asamidoi@chromium.org>
2023-11-27 16:12:50 +0000

    [ios] Add a wrapper class for SnapshotID
    This CL adds a wrapper class of SnapshotID. The wrapper class is not
    used currently and it will be used in the future.
    
    Bug: 1502841
    Change-Id: I3fe9bb89318e890ebead108a65db88f33acdd197
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5033605
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229338}

---
+++ ios/chrome/browser/snapshots/model/snapshot_id_wrapper.mm
@@ -0,0 +1,49 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/snapshots/model/snapshot_id_wrapper.h"
+
+#import "base/apple/foundation_util.h"
+
+@implementation SnapshotIDWrapper {
+  SnapshotID snapshot_id_;
+}
+
+- (instancetype)initWithSnapshotID:(SnapshotID)snapshot_id {
+  if ((self = [super init])) {
+    snapshot_id_ = snapshot_id;
+  }
+  return self;
+}
+
+- (instancetype)initWithIdentifier:(int32_t)identifier {
+  if ((self = [super init])) {
+    snapshot_id_ = SnapshotID(identifier);
+  }
+  return self;
+}
+
+- (SnapshotID)snapshot_id {
+  return snapshot_id_;
+}
+
+- (int32_t)identifier {
+  return snapshot_id_.identifier();
+}
+
+- (NSUInteger)hash {
+  return std::hash<int32_t>{}(self.identifier);
+}
+
+- (BOOL)isEqual:(NSObject*)other {
+  if (![other isKindOfClass:[SnapshotIDWrapper class]]) {
+    return NO;
+  }
+
+  return self.snapshot_id ==
+         base::apple::ObjCCastStrict<SnapshotIDWrapper>(other).snapshot_id;
+}
+
+@end
