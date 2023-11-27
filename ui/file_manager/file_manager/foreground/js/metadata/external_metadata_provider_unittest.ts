85d4a61bd8173e0d7f1f6b822bdb08301187dedc
Peter Marshall <petermarshall@chromium.org>
2023-11-27 23:00:03 +0000

    TS-ify external_metadata_provider and test
    Bug: b/289003444
    Change-Id: Iff3bfbf62be902facfdf9e7b4a6d8680c7b86796
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5056064
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229588}

--- ui/file_manager/file_manager/foreground/js/metadata/external_metadata_provider_unittest.ts
+++ ui/file_manager/file_manager/foreground/js/metadata/external_metadata_provider_unittest.ts
@@ -6,44 +6,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {assertEquals} from 'chrome://webui-test/chromeos/chai_assert.js';
 
 import {installMockChrome} from '../../../common/js/mock_chrome.js';
-import {reportPromise} from '../../../common/js/test_error_reporting.js';
 
 import {ExternalMetadataProvider} from './external_metadata_provider.js';
 import {MetadataRequest} from './metadata_request.js';
 
-/** @const @type {!Entry} */
-const entryA = /** @type {!Entry} */ ({
+const entryA = {
   toURL: function() {
     return 'filesystem://A';
   },
   isFile: true,
-});
+} as Entry;
 
-/** @const @type {!Entry} */
-const entryB = /** @type {!Entry} */ ({
+const entryB = {
   toURL: function() {
     return 'filesystem://B';
   },
   isFile: true,
-});
+} as Entry;
 
 /**
  * Mock chrome APIs.
- * @type {!Object}
  */
 let mockChrome;
 
-/** @param {()=>void} callback */
-export function testExternalMetadataProviderBasic(callback) {
-  // Setup mock chrome APIs.
+export async function testExternalMetadataProviderBasic() {
+  // Set up mock chrome APIs.
   mockChrome = {
     fileManagerPrivate: {
-      /**
-       * @param {!Array<!Entry>} entries
-       * @param {!Array<string>} names
-       * @param {function(Array<Object>):void} callback
-       */
-      getEntryProperties: function(entries, names, callback) {
+      getEntryProperties: function(
+          entries: Entry[], names: string[],
+          callback: (props: object[]) => void) {
         assertEquals(2, entries.length);
         assertEquals('filesystem://A', entries[0]?.toURL());
         assertEquals('filesystem://B', entries[1]?.toURL());
@@ -76,28 +68,23 @@ export function testExternalMetadataProviderBasic(callback) {
   installMockChrome(mockChrome);
 
   const provider = new ExternalMetadataProvider();
-  reportPromise(
-      provider
-          .get([
-            new MetadataRequest(entryA, ['modificationTime', 'size']),
-            new MetadataRequest(entryB, ['modificationTime', 'size']),
-          ])
-          .then(results => {
-            assertEquals(2, results.length);
-            assertEquals(
-                new Date(2015, 0, 1).toString(),
-                results[0].modificationTime.toString());
-            assertEquals(1024, results[0].size);
-            assertEquals(true, results[0].isMachineRoot);
-            assertEquals(true, results[0].isExternalMedia);
-            assertEquals(true, results[0].isArbitrarySyncFolder);
-            assertEquals(
-                new Date(2015, 1, 2).toString(),
-                results[1].modificationTime.toString());
-            assertEquals(2048, results[1].size);
-            assertEquals(false, results[1].isMachineRoot);
-            assertEquals(false, results[1].isExternalMedia);
-            assertEquals(false, results[1].isArbitrarySyncFolder);
-          }),
-      callback);
+  const results = await provider.get([
+    new MetadataRequest(entryA, ['modificationTime', 'size']),
+    new MetadataRequest(entryB, ['modificationTime', 'size']),
+  ]);
+  assertEquals(2, results.length);
+  assertEquals(
+      new Date(2015, 0, 1).toString(),
+      results[0]!.modificationTime!.toString());
+  assertEquals(1024, results[0]!.size);
+  assertEquals(true, results[0]!.isMachineRoot);
+  assertEquals(true, results[0]!.isExternalMedia);
+  assertEquals(true, results[0]!.isArbitrarySyncFolder);
+  assertEquals(
+      new Date(2015, 1, 2).toString(),
+      results[1]!.modificationTime!.toString());
+  assertEquals(2048, results[1]!.size);
+  assertEquals(false, results[1]!.isMachineRoot);
+  assertEquals(false, results[1]!.isExternalMedia);
+  assertEquals(false, results[1]!.isArbitrarySyncFolder);
 }
