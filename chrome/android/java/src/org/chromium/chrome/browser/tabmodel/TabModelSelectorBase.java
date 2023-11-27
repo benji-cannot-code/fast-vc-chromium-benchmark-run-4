2ca604c62fa190fc1b07eddd2dd794c6c2e543e8
Calder Kitagawa <ckitagawa@chromium.org>
2023-11-27 20:00:25 +0000

    [Tab Model] Add TabModelSelector#getCurrentModelTabCountSupplier
    Add a TransitiveObservableSupplier for tab count. This can be used to
    replace TabCountProvider in a follow-up CL.
    
    Bug: 1499464
    Change-Id: I484c09428727c8bb7cb03fb262cdf6987012f732
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5054605
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229460}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelSelectorBase.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelSelectorBase.java
@@ -47,6 +47,7 @@ public abstract class TabModelSelectorBase
     private final ObservableSupplierImpl<TabModel> mTabModelSupplier =
             new ObservableSupplierImpl<>();
     private final TransitiveObservableSupplier<TabModel, Tab> mCurrentTabSupplier;
+    private final TransitiveObservableSupplier<TabModel, Integer> mCurrentModelTabCountSupplier;
 
     private final ObserverList<TabModelSelectorObserver> mObservers = new ObserverList<>();
     private final ObserverList<IncognitoTabModelObserver> mIncognitoObservers =
@@ -78,7 +79,10 @@ public abstract class TabModelSelectorBase
         mTabModelSupplier.addObserver(mIncognitoReauthDialogDelegateCallback);
         mCurrentTabSupplier =
                 new TransitiveObservableSupplier<>(
-                        mTabModelSupplier, p -> p.getCurrentTabSupplier());
+                        mTabModelSupplier, tabModel -> tabModel.getCurrentTabSupplier());
+        mCurrentModelTabCountSupplier =
+                new TransitiveObservableSupplier<>(
+                        mTabModelSupplier, tabModel -> tabModel.getTabCountSupplier());
     }
 
     protected final void initialize(TabModel normalModel, IncognitoTabModel incognitoModel) {
@@ -206,6 +210,11 @@ public abstract class TabModelSelectorBase
     @Override
     public @NonNull ObservableSupplier<Tab> getCurrentTabSupplier() {
         return mCurrentTabSupplier;
+    }
+
+    @Override
+    public @NonNull ObservableSupplier<Integer> getCurrentModelTabCountSupplier() {
+        return mCurrentModelTabCountSupplier;
     }
 
     @Override
