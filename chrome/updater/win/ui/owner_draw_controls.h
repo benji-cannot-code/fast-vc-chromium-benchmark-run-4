8461e925f4d734419e5a320d86ed0f87227ffc13
Sorin Jianu <sorin@chromium.org>
2023-11-27 05:45:08 +0000

    updater: fix wtl includes.
    Fixed: 1504668
    Change-Id: Id6c931a00adea9a7b2d3605d40e8b874987ddb33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5055736
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229174}

--- chrome/updater/win/ui/owner_draw_controls.h
+++ chrome/updater/win/ui/owner_draw_controls.h
@@ -9,17 +9,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 
 #include "base/win/atl.h"
+
+#include <atltypes.h>
+
 #include "chrome/updater/win/ui/ui_constants.h"
-
-// These headers must be included after base/win/atl.h.
-#include "./atlapp.h"
-#include "./atltypes.h"
-
+#include "third_party/wtl/include/atlapp.h"
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunused-but-set-variable"
 #pragma clang diagnostic ignored "-Wmissing-braces"
-#include "./atlctrls.h"
-#include "./atlframe.h"
+#include "third_party/wtl/include/atlctrls.h"
+#include "third_party/wtl/include/atlframe.h"
 #pragma clang diagnostic pop
 
 namespace updater::ui {
