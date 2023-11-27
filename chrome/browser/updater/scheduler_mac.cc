a94447bc00d6d76f47261a467b95fcbb0661ec06
Joshua Pawlicki <waffles@chromium.org>
2023-11-27 17:48:49 +0000

    Reland "Updater: Repair missing ksadmin shims during server startup."
    This is a reland of commit e65ccc626c9a5aef0b48cdaa342c1a75e6eff3ee
    
    Original change's description:
    > Updater: Repair missing ksadmin shims during server startup.
    >
    > Also increases ksadmin logging to help diagnose errors.
    >
    > Fixed: 1503195
    > Change-Id: I304873c67e6919a330118c486808e2ba7c9a01c9
    > Low-Coverage-Reason: COVERAGE_UNDERREPORTED
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5049823
    > Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    > Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    > Reviewed-by: Sorin Jianu <sorin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1227963}
    
    Change-Id: Id2adf035d8bfbfcd5e6a972e9ab5f997aa7e8568
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED
    Fixed: 1503195
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5054377
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229405}

--- chrome/browser/updater/scheduler_mac.cc
+++ chrome/browser/updater/scheduler_mac.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/updater/browser_updater_client_util.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/updater/updater_scope.h"
-#include "chrome/updater/util/util.h"
+#include "chrome/updater/util/mac_util.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace updater {
