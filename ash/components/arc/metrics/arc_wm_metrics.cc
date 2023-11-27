a02c9b2b14f812cc9ae8db7af9c3951127097a4a
Lingyu Feng <lingyufeng@chromium.org>
2023-11-27 06:57:46 +0000

    Revert "Increase upper limits of ARC WM UMA histograms"
    This reverts commit c24aba192ee521bda4788606dce6e06d44a3c9a5. 
    
    Reason for revert: When revising a metric, we should create a new histogram with a new name instead of revising the old one. See https://chromium.googlesource.com/chromium/src.git/+/HEAD/tools/metrics/histograms/README.md#revising-histograms 
    The new metric will be added later.
    
    Original change's description:
    > Increase upper limits of ARC WM UMA histograms
    >
    > Currently, in UMA dashboard, some arc wm histograms
    > (Arc.WM.WindowMaximizedDelayTime, Arc.WM.WindowClosedDelayTime,
    > Arc.WM.WindowEnterTabletModeDelayTime,
    > Arc.WM.WindowExitTabletModeDelayTime) are showing large-value data
    > beyond their maximum buckets. This CL increases the upper limits of
    > these histograms to better understand the distribution of these
    > large-value data points.
    >
    > TEST=manually checked chrome://histograms
    >
    > Change-Id: I2a7ee15a69563b6d2a9c08334c9e04a280fabd45
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5038497
    > Auto-Submit: Lingyu Feng <lingyufeng@chromium.org>
    > Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    > Commit-Queue: Lingyu Feng <lingyufeng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1227199}
    
    Change-Id: I7a00ddc4ae2e0b24b592b2f9e31b6e644539762e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5056944
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Auto-Submit: Lingyu Feng <lingyufeng@chromium.org>
    Commit-Queue: Lingyu Feng <lingyufeng@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1229182}

--- ash/components/arc/metrics/arc_wm_metrics.cc
+++ ash/components/arc/metrics/arc_wm_metrics.cc
@@ -119,7 +119,7 @@ class ArcWmMetrics::WindowStateChangeObserver
             ArcWmMetrics::GetWindowEnterTabletModeTimeHistogramName(app_type),
             window_operation_elapsed_timer_.Elapsed(),
             /*minimum=*/base::Milliseconds(1),
-            /*maximum=*/base::Seconds(5), 100);
+            /*maximum=*/base::Seconds(2), 100);
       }
     } else {
       if (state->IsMaximized()) {
@@ -127,7 +127,7 @@ class ArcWmMetrics::WindowStateChangeObserver
             ArcWmMetrics::GetWindowMaximizedTimeHistogramName(app_type),
             window_operation_elapsed_timer_.Elapsed(),
             /*minimum=*/base::Milliseconds(1),
-            /*maximum=*/base::Seconds(3), 100);
+            /*maximum=*/base::Seconds(2), 100);
       } else if (state->IsMinimized()) {
         base::UmaHistogramCustomTimes(
             ArcWmMetrics::GetWindowMinimizedTimeHistogramName(app_type),
@@ -139,7 +139,7 @@ class ArcWmMetrics::WindowStateChangeObserver
             ArcWmMetrics::GetWindowExitTabletModeTimeHistogramName(app_type),
             window_operation_elapsed_timer_.Elapsed(),
             /*minimum=*/base::Milliseconds(1),
-            /*maximum=*/base::Seconds(5), 100);
+            /*maximum=*/base::Seconds(2), 100);
       }
     }
   }
@@ -182,7 +182,7 @@ class ArcWmMetrics::WindowCloseObserver : public aura::WindowObserver {
         ArcWmMetrics::GetArcWindowClosedTimeHistogramName(),
         window_close_elapsed_timer_.Elapsed(),
         /*minimum=*/base::Milliseconds(1),
-        /*maximum=*/base::Seconds(3), 100);
+        /*maximum=*/base::Seconds(2), 100);
   }
 
   // Tracks the elapsed time from the window closing operation happens until the
