c683de9dcaf1d8b525bb1d0dc2c81c9c9c2cd3cb
Mustafa Emre Acer <meacer@chromium.org>
2023-11-27 21:11:51 +0000

    Insecure Forms: Reuse the insecure action check
    This CL effectively treats forms with javascript action URLs as secure
    for the purposes of the interstitial.
    
    Bug: 1501150
    Change-Id: I887095f26e2bc78d663675a88c712ff0d6826a41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5050417
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229508}

--- components/autofill/core/browser/autofill_browser_util.cc
+++ components/autofill/core/browser/autofill_browser_util.cc
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/form_structure.h"
 #include "services/network/public/cpp/is_potentially_trustworthy.h"
 
-namespace {
-// Matches the blink check for mixed content.
+namespace autofill {
+
 bool IsInsecureFormAction(const GURL& action_url) {
   // blob: and filesystem: URLs never hit the network, and access is restricted
   // to same-origin contexts, so they are not blocked. Some forms use
@@ -23,9 +23,6 @@ bool IsInsecureFormAction(const GURL& action_url) {
   }
   return !network::IsUrlPotentiallyTrustworthy(action_url);
 }
-}  // namespace
-
-namespace autofill {
 
 bool IsFormOrClientNonSecure(const AutofillClient& client,
                              const FormData& form) {
