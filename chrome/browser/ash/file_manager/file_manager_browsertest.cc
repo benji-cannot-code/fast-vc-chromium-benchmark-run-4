ed682d13444b9407d7a82593044044f24d786a23
Wenbo Jie <wenbojie@chromium.org>
2023-11-27 04:38:49 +0000

    [Files F2] Add a new browser test for drag move on the tree
    When we drag a file and move across different tree items, the
    tree item should be expanded and selected if the drag stays there.
    
    Bug: b:286488664
    Test: browser_tests --gtest_filter="DirectoryTree/*"
    Change-Id: Icc223d6ddf0caa2f7a128697711f688eb60d65d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5056055
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229161}

--- chrome/browser/ash/file_manager/file_manager_browsertest.cc
+++ chrome/browser/ash/file_manager/file_manager_browsertest.cc
@@ -855,6 +855,7 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
             .NewDirectoryTree(),
         TestCase("directoryTreeExpandFolderOnDelayExpansionVolume")
             .NewDirectoryTree(),
+        TestCase("directoryTreeExpandAndSelectedOnDragMove").NewDirectoryTree(),
         // Section end - browser tests for new directory tree
         TestCase("directoryTreeActiveDirectory"),
         TestCase("directoryTreeSelectedDirectory"),
@@ -867,7 +868,8 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
             "directoryTreeExpandFolderWithHiddenFileAndShowHiddenFilesOff"),
         TestCase("directoryTreeExpandFolderWithHiddenFileAndShowHiddenFilesOn"),
         TestCase("directoryTreeExpandFolderOnNonDelayExpansionVolume"),
-        TestCase("directoryTreeExpandFolderOnDelayExpansionVolume")));
+        TestCase("directoryTreeExpandFolderOnDelayExpansionVolume"),
+        TestCase("directoryTreeExpandAndSelectedOnDragMove")));
 
 WRAPPED_INSTANTIATE_TEST_SUITE_P(
     DirectoryTreeContextMenu, /* directory_tree_context_menu.js */
