aea3c4e0ac1e786b00d1ceff46f03a660b4ecdd2
Andrew Grieve <agrieve@chromium.org>
2023-11-27 21:08:21 +0000

    De-flake chrome_bundle_smoke_test by ensuring native is loaded
    Had a job fail where the error was about the native library not have
    being loaded:
    https://ci.chromium.org/ui/p/chromium/builders/try/android-pie-arm64-dbg/7616
    
    Bug: 1295552
    Change-Id: I6df174dc24a821837b2e3242a4f02a8cca28239b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5063989
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229505}

--- chrome/browser/test_dummy/internal/android/java/src/org/chromium/chrome/browser/test_dummy/TestDummyActivity.java
+++ chrome/browser/test_dummy/internal/android/java/src/org/chromium/chrome/browser/test_dummy/TestDummyActivity.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import org.chromium.base.CommandLine;
 import org.chromium.base.StrictModeContext;
+import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.chrome.modules.test_dummy.TestDummyModuleProvider;
 
 /** Helper activity to launch test dummy module.  */
@@ -22,6 +23,7 @@ public class TestDummyActivity extends AppCompatActivity {
             finish();
             return;
         }
+        LibraryLoader.getInstance().ensureInitialized();
         if (TestDummyModuleProvider.isModuleInstalled()) {
             onModuleInstalled(true);
         } else {
