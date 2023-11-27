98ed0c8740730ba7001f403d903c909b23e6347e
Dan Sanders <sandersd@chromium.org>
2023-11-27 23:08:45 +0000

    [media] Add requirement for //media flag/feature expiry documentation
    Bug: 1504289
    Change-Id: If594c6cd2f11e900af0d0dd692e5c409adba4ad2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5051355
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229601}

--- media/base/media_switches.cc
+++ media/base/media_switches.cc
@@ -3,6 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// 2023-10-22: Newly added flags and features should document an owner and
+// expiry condition. The expiry condition is freeform and could be a date,
+// experiment, bug, the deletion of a file, etc.
+
 #include "media/base/media_switches.h"
 
 #include "base/command_line.h"
@@ -883,11 +887,15 @@ BASE_FEATURE(kVideoBlitColorAccuracy,
 
 #if BUILDFLAG(IS_APPLE)
 // Use VideoToolbox for AV1 hardware decoding.
+// Owner: dalecurtis@chromium.org, sandersd@chromium.org
+// Expiry: When enabled by default for a full release cycle
 BASE_FEATURE(kVideoToolboxAv1Decoding,
              "VideoToolboxAv1Decoding",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Use the new VideoToolboxVideoDecoder for hardware decoding.
+// Owner: sandersd@chromium.org
+// Expiry: When VTVideoDecodeAccelerator is deleted
 BASE_FEATURE(kVideoToolboxVideoDecoder,
              "VideoToolboxVideoDecoder",
              base::FEATURE_DISABLED_BY_DEFAULT);
