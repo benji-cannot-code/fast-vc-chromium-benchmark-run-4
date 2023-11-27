2f69889b83b7e7d548ec3bb12e06c91781678666
Traian Captan <tcaptan@chromium.org>
2023-11-27 20:47:23 +0000

    Add auto sizes parsing support
    Spec:
    https://html.spec.whatwg.org/#parsing-a-sizes-attribute
    https://whatpr.org/html/9493/images.html#parsing-a-sizes-attribute
    
    R=​pdr
    
    Bug: 1359051
    Change-Id: I7b7b321b9845ff16faa6a556f571f664c441304f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5051849
    Commit-Queue: Traian Captan <tcaptan@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229479}

--- third_party/blink/renderer/core/css/parser/sizes_attribute_parser.h
+++ third_party/blink/renderer/core/css/parser/sizes_attribute_parser.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/css/media_values.h"
 #include "third_party/blink/renderer/core/css/parser/media_query_parser.h"
+#include "third_party/blink/renderer/core/html/html_image_element.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
@@ -20,8 +21,12 @@ class CORE_EXPORT SizesAttributeParser {
   STACK_ALLOCATED();
 
  public:
-  SizesAttributeParser(MediaValues*, const String&, const ExecutionContext*);
+  SizesAttributeParser(MediaValues*,
+                       const String&,
+                       const ExecutionContext*,
+                       const HTMLImageElement* = nullptr);
 
+  bool IsAuto();
   float Size();
 
  private:
@@ -36,6 +41,8 @@ class CORE_EXPORT SizesAttributeParser {
   float size_{};
   bool size_was_set_{};
   bool is_valid_{};
+  bool is_auto_{};
+  const HTMLImageElement* img_{};
 };
 
 }  // namespace blink
--- third_party/blink/renderer/core/html/html_image_element.h
+++ third_party/blink/renderer/core/html/html_image_element.h
@@ -80,6 +80,9 @@ class CORE_EXPORT HTMLImageElement final
   unsigned LayoutBoxWidth() const;
   unsigned LayoutBoxHeight() const;
 
+  bool IsBeingRendered() const;
+  bool AllowAutoSizes() const;
+
   const String& currentSrc() const;
 
   bool IsServerMap() const;
