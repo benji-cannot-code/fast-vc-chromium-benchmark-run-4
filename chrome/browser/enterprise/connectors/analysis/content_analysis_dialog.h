c9d0eb53130677ad72ee107864082ae68ec46dd1
weidongliu <liuwd8@gmail.com>
2023-11-27 02:57:19 +0000

    Clean up usage of ImageView::SetImage(gfx::ImageSkia) under /chrome/browser/enterprise.
    There is no change in behavior.
    ui::ImageModel should be preferred to gfx::ImageSkia.
    
    This CL was uploaded by git cl split.
    
    R=domfc@chromium.org
    
    Bug: 1099602
    Change-Id: I531532079dac35bb594afda471fc0396e258df80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5040161
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Auto-Submit: wd l <liuwd8@gmail.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229144}

--- chrome/browser/enterprise/connectors/analysis/content_analysis_dialog.h
+++ chrome/browser/enterprise/connectors/analysis/content_analysis_dialog.h
@@ -25,10 +25,6 @@ namespace content {
 class WebContents;
 }  // namespace content
 
-namespace gfx {
-class ImageSkia;
-}  // namespace gfx
-
 namespace views {
 class BoxLayoutView;
 class ImageView;
@@ -167,7 +163,7 @@ class ContentAnalysisDialog : public views::DialogDelegate,
   bool ShouldUseDarkTopImage() const;
 
   // Returns the appropriate top image depending on `dialog_state_`.
-  const gfx::ImageSkia* GetTopImage() const;
+  ui::ImageModel GetTopImage() const;
 
   // Accessors used to validate the views in tests.
   views::ImageView* GetTopImageForTesting() const;
