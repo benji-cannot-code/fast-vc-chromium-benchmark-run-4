c1b049aa8da8a65d729acf4469c1d9160ef55e72
Devlin Cronin <rdevlin.cronin@chromium.org>
2023-11-28 22:59:40 +0000

    [Extensions] Remove ExtensionMessageBubbleController
    The remaining active bubble that was previously based on
    ExtensionMessageBubbleController has been moved off the old
    infrastructure. This means we can fully remove
    ExtensionMessageBubbleController and the remaining associated classes:
    - ExtensionMessageBubbleController
    - ExtensionMessageBubbleController::Delegate
    - DevModeBubbleDelegate
    - SuspiciousExtensionBubbleDelegate
    - TestExtensionMessageBubbleDelegate
    Along with the unit tests for each and the integration in the
    ToolbarActionsModel (`has_active_bubble`).
    
    Bug: 1503268
    Change-Id: Ib85836671a959d15fc1112a3019c18d6e5f23bfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5049612
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230260}

--- extensions/browser/extension_prefs.cc
+++ extensions/browser/extension_prefs.cc
@@ -2572,6 +2572,7 @@ void ExtensionPrefs::MigrateObsoleteExtensionPrefs() {
 
       // Added 2023-11.
       "ack_proxy_bubble",
+      "ack_wiped",
   };
 
   for (auto key_value : extensions_dictionary) {
