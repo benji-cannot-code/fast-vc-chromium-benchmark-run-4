088184fa175a878ded457a98a78eab72e6c300e9
Bohdan Tyshchenko <bohdanty@google.com>
2023-11-28 20:20:20 +0000

    [OOBE][CodeHealth] - Refactor Enable Debugging Screen Handler enum
    Change enums inside the Enable Debugging Screen Handler to match C++
    styleguide.
    
    Bug: b:279732127
    Change-Id: I579bdb5820fe7e2158dff40a1ca264fb6cea12d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5062817
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Bohdan Tyshchenko <bohdanty@google.com>
    Cr-Commit-Position: refs/heads/main@{#1230092}

--- chrome/browser/ash/login/screens/enable_debugging_screen.cc
+++ chrome/browser/ash/login/screens/enable_debugging_screen.cc
@@ -95,7 +95,7 @@ void EnableDebuggingScreen::HandleLearnMore() {
 }
 
 void EnableDebuggingScreen::HandleRemoveRootFSProtection() {
-  UpdateUIState(EnableDebuggingScreenView::UI_STATE_WAIT);
+  UpdateUIState(EnableDebuggingScreenView::kUIStateWait);
   DebugDaemonClient::Get()->RemoveRootfsVerification(
       base::BindOnce(&EnableDebuggingScreen::OnRemoveRootfsVerification,
                      weak_ptr_factory_.GetWeakPtr()));
@@ -105,7 +105,7 @@ void EnableDebuggingScreen::HandleRemoveRootFSProtection() {
 // screen and reboots the machine.
 void EnableDebuggingScreen::OnRemoveRootfsVerification(bool success) {
   if (!success) {
-    UpdateUIState(EnableDebuggingScreenView::UI_STATE_ERROR);
+    UpdateUIState(EnableDebuggingScreenView::kUIStateError);
     return;
   }
 
@@ -118,7 +118,7 @@ void EnableDebuggingScreen::OnRemoveRootfsVerification(bool success) {
 }
 
 void EnableDebuggingScreen::WaitForCryptohome() {
-  UpdateUIState(EnableDebuggingScreenView::UI_STATE_WAIT);
+  UpdateUIState(EnableDebuggingScreenView::kUIStateWait);
   UserDataAuthClient* client = UserDataAuthClient::Get();
   client->WaitForServiceToBeAvailable(base::BindOnce(
       &EnableDebuggingScreen::OnCryptohomeDaemonAvailabilityChecked,
@@ -131,7 +131,7 @@ void EnableDebuggingScreen::OnCryptohomeDaemonAvailabilityChecked(
            << service_is_available;
   if (!service_is_available) {
     LOG(ERROR) << "Crypthomed is not available.";
-    UpdateUIState(EnableDebuggingScreenView::UI_STATE_ERROR);
+    UpdateUIState(EnableDebuggingScreenView::kUIStateError);
     return;
   }
 
@@ -146,7 +146,7 @@ void EnableDebuggingScreen::OnDebugDaemonServiceAvailabilityChecked(
            << service_is_available;
   if (!service_is_available) {
     LOG(ERROR) << "Debug daemon is not available.";
-    UpdateUIState(EnableDebuggingScreenView::UI_STATE_ERROR);
+    UpdateUIState(EnableDebuggingScreenView::kUIStateError);
     return;
   }
 
@@ -162,26 +162,26 @@ void EnableDebuggingScreen::OnQueryDebuggingFeatures(bool success,
            << ", success=" << success << ", features=" << features_flag;
   if (!success ||
       features_flag == debugd::DevFeatureFlag::DEV_FEATURES_DISABLED) {
-    UpdateUIState(EnableDebuggingScreenView::UI_STATE_ERROR);
+    UpdateUIState(EnableDebuggingScreenView::kUIStateError);
     return;
   }
 
   if ((features_flag &
        debugd::DevFeatureFlag::DEV_FEATURE_ROOTFS_VERIFICATION_REMOVED) == 0) {
-    UpdateUIState(EnableDebuggingScreenView::UI_STATE_REMOVE_PROTECTION);
+    UpdateUIState(EnableDebuggingScreenView::kUIStateRemoveProtection);
     return;
   }
 
   if ((features_flag & DebugDaemonClient::DEV_FEATURE_ALL_ENABLED) !=
       DebugDaemonClient::DEV_FEATURE_ALL_ENABLED) {
-    UpdateUIState(EnableDebuggingScreenView::UI_STATE_SETUP);
+    UpdateUIState(EnableDebuggingScreenView::kUIStateSetup);
   } else {
-    UpdateUIState(EnableDebuggingScreenView::UI_STATE_DONE);
+    UpdateUIState(EnableDebuggingScreenView::kUIStateDone);
   }
 }
 
 void EnableDebuggingScreen::HandleSetup(const std::string& password) {
-  UpdateUIState(EnableDebuggingScreenView::UI_STATE_WAIT);
+  UpdateUIState(EnableDebuggingScreenView::kUIStateWait);
   DebugDaemonClient::Get()->EnableDebuggingFeatures(
       password,
       base::BindOnce(&EnableDebuggingScreen::OnEnableDebuggingFeatures,
@@ -190,18 +190,18 @@ void EnableDebuggingScreen::HandleSetup(const std::string& password) {
 
 void EnableDebuggingScreen::OnEnableDebuggingFeatures(bool success) {
   if (!success) {
-    UpdateUIState(EnableDebuggingScreenView::UI_STATE_ERROR);
+    UpdateUIState(EnableDebuggingScreenView::kUIStateError);
     return;
   }
 
-  UpdateUIState(EnableDebuggingScreenView::UI_STATE_DONE);
+  UpdateUIState(EnableDebuggingScreenView::kUIStateDone);
 }
 
 void EnableDebuggingScreen::UpdateUIState(
     EnableDebuggingScreenView::UIState state) {
-  if (state == EnableDebuggingScreenView::UI_STATE_SETUP ||
-      state == EnableDebuggingScreenView::UI_STATE_ERROR ||
-      state == EnableDebuggingScreenView::UI_STATE_DONE) {
+  if (state == EnableDebuggingScreenView::kUIStateSetup ||
+      state == EnableDebuggingScreenView::kUIStateError ||
+      state == EnableDebuggingScreenView::kUIStateDone) {
     PrefService* prefs = g_browser_process->local_state();
     prefs->ClearPref(prefs::kDebuggingFeaturesRequested);
     prefs->CommitPendingWrite();
