ea25888ab8543cc61883203df5538933f7bc926c
Angus L. M. McLean IV <angusmclean@google.com>
2023-11-28 20:37:37 +0000

    Remove gif rendering feature flag
    This feature is no longer being worked on. There are no usages of this
    flag.
    
    Bug: b:265441374
    Change-Id: I003fc9b68c06aea2104f150c005d9b4c49c1621c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5067578
    Commit-Queue: Angus McLean <angusmclean@google.com>
    Reviewed-by: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1230124}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1323,9 +1323,6 @@ BASE_FEATURE(kGesturePropertiesDBusService,
 // native screen capture tool.
 BASE_FEATURE(kGifRecording, "GifRecording", base::FEATURE_DISABLED_BY_DEFAULT);
 
-// Enables the displaying of animated gifs in ash.
-BASE_FEATURE(kGifRendering, "GifRendering", base::FEATURE_DISABLED_BY_DEFAULT);
-
 // Enables a Files banner about Google One offer.
 BASE_FEATURE(kGoogleOneOfferFilesBanner,
              "GoogleOneOfferFilesBanner",
@@ -3493,10 +3490,6 @@ bool IsGalleryAppPdfEditNotificationEnabled() {
 
 bool IsGifRecordingEnabled() {
   return base::FeatureList::IsEnabled(kGifRecording);
-}
-
-bool IsGifRenderingEnabled() {
-  return base::FeatureList::IsEnabled(kGifRendering);
 }
 
 bool IsGrowthFrameworkEnabled() {
