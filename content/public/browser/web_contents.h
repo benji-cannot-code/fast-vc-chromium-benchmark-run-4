5390a96f191902c5f171db1a8b054a295185058e
mikt <mikt@google.com>
2023-11-28 05:46:31 +0000

    [MO] Add ADVANCED_MEMORY_SAFETY_CHECKS() to content::WebContents
    Add `ADVANCED_MEMORY_SAFETY_CHECKS()` to:
    - `content::WebContents`.
    
    This enforces additional memory safety checks by overloading operator
    new/delete. The feature is behind a feature flag disabled by default.
    
    We will experiment later with the flag to understand how much the
    feature will cost.
    
    WebContents has been chosen, because it is likely to have
    lifetime bugs and is used massively.
    
    Bug: 1505316
    Change-Id: Ia8e63262323ee34757a3dae28366ff00c0a50ee2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5061370
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Cr-Commit-Position: refs/heads/main@{#1229745}

--- content/public/browser/web_contents.h
+++ content/public/browser/web_contents.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/function_ref.h"
 #include "base/location.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/safety_checks.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/process/kill.h"
@@ -134,6 +135,10 @@ class PreloadingAttempt;
 // See navigation_controller.h for more details.
 class WebContents : public PageNavigator,
                     public base::SupportsUserData {
+  // Do not remove this macro!
+  // The macro is maintained by the memory safety team.
+  ADVANCED_MEMORY_SAFETY_CHECKS();
+
  public:
   struct CONTENT_EXPORT CreateParams {
     explicit CreateParams(
