8df300377aa549fcdd8c137d059fcfcdb289f27a
Lukasz Anforowicz <lukasza@chromium.org>
2023-11-28 22:03:59 +0000

    QRCodeGenerator::Generate: gracefully reject "impossible" min_version.
    `QRCodeGenerator::Generate` accepts an optional `min_version` parameter
    (which roughly corresponds to the size of the generated QR codes).  Some
    callers pass a hardcoded value here to guarantee a minimum size of the
    generated QR code (e.g. so that painting a dino in the middle doesn't
    interfere with error correction's ability to decode the QR code).
    There are no callers where `min_version` is computed based on other
    (potentially untrustworthy) input.  Therefore, invalid `min_version`
    values are "impossible".
    
    Before this CL, "impossible" values of `min_version` would lead to Rust
    panics (when validating the value in `qr_code_generator_ffi_glue.rs`) or
    to a `CHECK` (when using `checked_cast<...>` on C++ side of the FFI
    glue).  This would lead to bug reports like https://crbug.com/1505660
    and https://crbug.com/1505643 where fuzzing has produced an
    "impossible" `min_version`.  After this CL, "impossible" values are
    handled more gracefully - returning an indication of "failure" instead
    of crashing via a `panic` or `CHECK`.
    
    Fixed: 1505643, 1505660
    Bug: 1431991
    Change-Id: I598f5d02602ee5b12dff2fc949d48cb0a1057c15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5068074
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230229}

--- components/qr_code_generator/qr_code_generator_unittest.cc
+++ components/qr_code_generator/qr_code_generator_unittest.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/qr_code_generator/qr_code_generator.h"
 
+#include <limits>
+#include <optional>
+
 #include "base/logging.h"
 #include "base/rand_util.h"
 #include "base/test/scoped_feature_list.h"
@@ -285,6 +288,15 @@ TEST_P(QRCodeGeneratorTest, HugeInput) {
   std::vector<uint8_t> huge_input(QRCodeGenerator::kMaxInputSize + 1);
   QRCodeGenerator qr;
   ASSERT_FALSE(qr.Generate(huge_input));
+}
+
+TEST_P(QRCodeGeneratorTest, InvalidMinVersion) {
+  std::vector<uint8_t> huge_input(QRCodeGenerator::kMaxInputSize + 1);
+  QRCodeGenerator qr;
+  ASSERT_FALSE(qr.Generate(huge_input, std::make_optional(41)));
+  ASSERT_FALSE(qr.Generate(
+      huge_input, std::make_optional(std::numeric_limits<int>::max())));
+  ASSERT_FALSE(qr.Generate(huge_input, std::make_optional(-1)));
 }
 
 INSTANTIATE_TEST_SUITE_P(RustEnabled,
