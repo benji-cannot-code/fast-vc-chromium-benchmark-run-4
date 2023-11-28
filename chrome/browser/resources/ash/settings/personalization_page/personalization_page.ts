22aab71e05ee1e169fb120212d5373f81a20c1e9
conniekxu <conniekxu@chromium.org>
2023-11-28 18:29:40 +0000

    CrOS Settings: Add row icons for Personalization section L1 page
    When feature OsSettingsRevampWayfinding is enabled, add row icons for
    the Apps section L1 page. This CL also updates the personalization search
    result icons to make it match to the newly added row icon.
    
    Feature enabled:
    - Row icon: https://screenshot.googleplex.com/grp4N35bJEQpXjZ
    - Search result icon: https://screenshot.googleplex.com/5abZCnuy9GtMrwX
    
    Feature disabled:
    - Row icon: https://screenshot.googleplex.com/8yogAygsWiAw7UY
    - Search result icon: https://screenshot.googleplex.com/3pYWU5jC8T5PLvw
    
    Bug: b/311201764
    Change-Id: I426f38204924dd294f4e228d6abf4d5360e4f290
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5055059
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Connie Xu <conniekxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230027}

--- chrome/browser/resources/ash/settings/personalization_page/personalization_page.ts
+++ chrome/browser/resources/ash/settings/personalization_page/personalization_page.ts
@@ -69,6 +69,12 @@ export class SettingsPersonalizationPageElement extends
         this.i18n('personalizationHubSubtitle');
   }
 
+  private getPersonalizationRowIcon_(): string {
+    return this.isRevampWayfindingEnabled_ ?
+        'os-settings:personalization-revamp' :
+        '';
+  }
+
   private openPersonalizationHub_(): void {
     this.personalizationHubBrowserProxy_.openPersonalizationHub();
   }
