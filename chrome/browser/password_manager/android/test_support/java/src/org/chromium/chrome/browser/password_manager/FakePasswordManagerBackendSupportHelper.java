c743eba8ff579c6d3beca276e4f3a398133334b2
Ivana Žužić <izuzic@google.com>
2023-11-28 14:20:34 +0000

    Create FakePasswordManagerBackendSupportHelper
    The FakePasswordManagerBackendSupportHelper is created to be used in
    integration tests.
    
    Bug: 1346235
    Change-Id: I79dbdd5176fcea9b2ba212e86c7235d73b434a18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5062816
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229881}

---
+++ chrome/browser/password_manager/android/test_support/java/src/org/chromium/chrome/browser/password_manager/FakePasswordManagerBackendSupportHelper.java
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.password_manager;
+
+/** Fake {@link PasswordManagerBackendSupportHelper} to be used in integration tests. */
+public class FakePasswordManagerBackendSupportHelper extends PasswordManagerBackendSupportHelper {
+    private static FakePasswordManagerBackendSupportHelper sInstance;
+
+    private boolean mBackendPresent;
+    private boolean mUpdateNeeded;
+
+    @Override
+    public boolean isBackendPresent() {
+        return mBackendPresent;
+    }
+
+    @Override
+    public boolean isUpdateNeeded() {
+        return mUpdateNeeded;
+    }
+
+    public void setBackendPresent(boolean backendPresent) {
+        mBackendPresent = backendPresent;
+    }
+
+    public void setUpdateNeeded(boolean updateNeeded) {
+        mUpdateNeeded = updateNeeded;
+    }
+}
