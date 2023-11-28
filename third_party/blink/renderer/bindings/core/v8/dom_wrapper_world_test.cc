9fc882888c9bb0937a571b307618d18a20517991
Dave Tapuska <dtapuska@chromium.org>
2023-11-28 20:58:49 +0000

    Rename AllWorldsInCurrentThread to AllWorldsInIsolate
    In anticipation of the WorldMap being moved into V8PerIsolateData
    we rename the AllWorldsInCurrentThread and pass in the Isolate.
    
    Bug: 263412
    Change-Id: Iafc7ff45780f860a5b38e85088f6e6f531cd4626
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5063514
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230161}

--- third_party/blink/renderer/bindings/core/v8/dom_wrapper_world_test.cc
+++ third_party/blink/renderer/bindings/core/v8/dom_wrapper_world_test.cc
@@ -31,19 +31,19 @@ void WorkerThreadFunc(
   thread->InitializeOnBackingThread(
       WorkerBackingThreadStartupData::CreateDefault());
 
+  v8::Isolate* isolate = thread->GetIsolate();
   // Worlds on the main thread should not be visible from the worker thread.
   Vector<scoped_refptr<DOMWrapperWorld>> initial_worlds;
-  DOMWrapperWorld::AllWorldsInCurrentThread(initial_worlds);
+  DOMWrapperWorld::AllWorldsInIsolate(isolate, initial_worlds);
   EXPECT_TRUE(initial_worlds.empty());
 
   // Create worlds on the worker thread and verify them.
-  v8::Isolate* isolate = thread->GetIsolate();
   auto worker_world1 =
       DOMWrapperWorld::Create(isolate, DOMWrapperWorld::WorldType::kWorker);
   auto worker_world2 =
       DOMWrapperWorld::Create(isolate, DOMWrapperWorld::WorldType::kWorker);
   Vector<scoped_refptr<DOMWrapperWorld>> worlds;
-  DOMWrapperWorld::AllWorldsInCurrentThread(worlds);
+  DOMWrapperWorld::AllWorldsInIsolate(isolate, worlds);
   EXPECT_EQ(worlds.size(), initial_worlds.size() + 2);
   worlds.clear();
 
@@ -65,7 +65,7 @@ TEST(DOMWrapperWorldTest, Basic) {
   DOMWrapperWorld& main_world = DOMWrapperWorld::MainWorld(isolate);
   EXPECT_TRUE(main_world.IsMainWorld());
   Vector<scoped_refptr<DOMWrapperWorld>> initial_worlds;
-  DOMWrapperWorld::AllWorldsInCurrentThread(initial_worlds);
+  DOMWrapperWorld::AllWorldsInIsolate(isolate, initial_worlds);
   int32_t used_isolated_world_id = DOMWrapperWorld::kMainWorldId;
   for (const auto& world : initial_worlds) {
     if (world->IsIsolatedWorld()) {
@@ -84,12 +84,12 @@ TEST(DOMWrapperWorldTest, Basic) {
   EXPECT_TRUE(isolated_world2->IsIsolatedWorld());
   Vector<scoped_refptr<DOMWrapperWorld>> worlds;
   EXPECT_TRUE(DOMWrapperWorld::NonMainWorldsExistInMainThread());
-  DOMWrapperWorld::AllWorldsInCurrentThread(worlds);
+  DOMWrapperWorld::AllWorldsInIsolate(isolate, worlds);
   EXPECT_EQ(worlds.size(), initial_worlds.size() + 2);
   worlds.clear();
   isolated_world1.reset();
   isolated_world2.reset();
-  DOMWrapperWorld::AllWorldsInCurrentThread(worlds);
+  DOMWrapperWorld::AllWorldsInIsolate(isolate, worlds);
   EXPECT_EQ(worlds.size(), initial_worlds.size());
   worlds.clear();
 
@@ -111,7 +111,7 @@ TEST(DOMWrapperWorldTest, Basic) {
   EXPECT_TRUE(
       worker_world_ids.insert(worker_world3->GetWorldId()).is_new_entry);
   EXPECT_TRUE(DOMWrapperWorld::NonMainWorldsExistInMainThread());
-  DOMWrapperWorld::AllWorldsInCurrentThread(worlds);
+  DOMWrapperWorld::AllWorldsInIsolate(isolate, worlds);
   EXPECT_EQ(worlds.size(), initial_worlds.size() + 3);
   worlds.clear();
   worker_world1->Dispose();
@@ -120,7 +120,7 @@ TEST(DOMWrapperWorldTest, Basic) {
   worker_world1.reset();
   worker_world2.reset();
   worker_world3.reset();
-  DOMWrapperWorld::AllWorldsInCurrentThread(worlds);
+  DOMWrapperWorld::AllWorldsInIsolate(isolate, worlds);
   EXPECT_EQ(worlds.size(), initial_worlds.size());
   worlds.clear();
 
@@ -138,7 +138,7 @@ TEST(DOMWrapperWorldTest, Basic) {
   test::EnterRunLoop();
 
   // Worlds on the worker thread should not be visible from the main thread.
-  DOMWrapperWorld::AllWorldsInCurrentThread(worlds);
+  DOMWrapperWorld::AllWorldsInIsolate(isolate, worlds);
   EXPECT_EQ(worlds.size(), initial_worlds.size());
   worlds.clear();
 }
--- third_party/blink/renderer/core/typed_arrays/dom_array_buffer_test.cc
+++ third_party/blink/renderer/core/typed_arrays/dom_array_buffer_test.cc
@@ -5,9 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/typed_arrays/dom_array_buffer.h"
 
-#include "base/task/single_thread_task_runner.h"
 #include "gin/array_buffer.h"
-#include "gin/public/isolate_holder.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/platform/scheduler/test/renderer_scheduler_test_support.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_testing.h"
@@ -15,41 +13,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class DOMArrayBufferTest : public testing::Test {
- public:
-  void SetUp() override {
-    gin::IsolateHolder::Initialize(gin::IsolateHolder::kStrictMode,
-                                   gin::ArrayBufferAllocator::SharedInstance());
-    isolate_holder_ = std::make_unique<gin::IsolateHolder>(
-        scheduler::GetSingleThreadTaskRunnerForTesting(),
-        gin::IsolateHolder::IsolateType::kBlinkWorkerThread);
-  }
-
-  void TearDown() override { isolate_holder_.reset(); }
-
-  v8::Isolate* isolate() const { return isolate_holder_->isolate(); }
-
- private:
-  std::unique_ptr<gin::IsolateHolder> isolate_holder_;
-};
-
-TEST_F(DOMArrayBufferTest, TransferredArrayBufferIsDetached) {
+TEST(DOMArrayBufferTest, TransferredArrayBufferIsDetached) {
   V8TestingScope v8_scope;
   ArrayBufferContents src(10, 4, ArrayBufferContents::kNotShared,
                           ArrayBufferContents::kZeroInitialize);
   auto* buffer = DOMArrayBuffer::Create(src);
   ArrayBufferContents dst;
-  ASSERT_TRUE(buffer->Transfer(isolate(), dst, v8_scope.GetExceptionState()));
+  ASSERT_TRUE(buffer->Transfer(v8_scope.GetIsolate(), dst,
+                               v8_scope.GetExceptionState()));
   ASSERT_FALSE(v8_scope.GetExceptionState().HadException());
   ASSERT_EQ(true, buffer->IsDetached());
 }
 
-TEST_F(DOMArrayBufferTest, TransferredEmptyArrayBufferIsDetached) {
+TEST(DOMArrayBufferTest, TransferredEmptyArrayBufferIsDetached) {
   V8TestingScope v8_scope;
   ArrayBufferContents src;
   auto* buffer = DOMArrayBuffer::Create(src);
   ArrayBufferContents dst;
-  ASSERT_TRUE(buffer->Transfer(isolate(), dst, v8_scope.GetExceptionState()));
+  ASSERT_TRUE(buffer->Transfer(v8_scope.GetIsolate(), dst,
+                               v8_scope.GetExceptionState()));
   ASSERT_FALSE(v8_scope.GetExceptionState().HadException());
   ASSERT_EQ(true, buffer->IsDetached());
 }
