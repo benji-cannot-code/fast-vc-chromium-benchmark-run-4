91fa01482f738bca630994ce69387da6d00b8e74
Geoff Huang <geoffhuang@google.com>
2023-11-28 20:46:15 +0000

    Reland "Use a vertical automotive toolbar if the screen is wide enough"
    This is a reland of commit 3b33ae89543550a794311c5704859b9220b21bfb
    
    Original change's description:
    > Use a vertical automotive toolbar if the screen is wide enough
    >
    > Before:
    > * ChromeActivity: https://hsv.googleplex.com/4800828373204992
    > * SettingsActivity: https://hsv.googleplex.com/5404679165968384
    > * Tab Switcher: https://hsv.googleplex.com/5426305534263296
    > * RTL: https://hsv.googleplex.com/6356334329987072
    >
    > After:
    > * ChromeActivity: https://hsv.googleplex.com/5039499638734848
    > * SettingsActivity: https://hsv.googleplex.com/6213823355682816
    > * Tab Switcher: https://hsv.googleplex.com/4560398519894016
    > * RTL: https://hsv.googleplex.com/4764426746986496
    >
    > The cutoff in this cl is set at sw600dp-w840dp, this may change after
    > more discussion with UX.
    >
    > Testing instructions:
    > 1. Go to chrome://flags in the url bar
    > 2. Search for the "Enable vertical automotive back button toolbar" flag and enable it.
    > 3. Restart chrome 2 times (since it is a cached flag).
    > 4. Make sure your screen resolution is sw600dp-w840dp (on Seahawk emulator, I did `adb shell wm density 120` to achieve this).
    >
    > Bug: 1503905
    > Change-Id: Icea8c833fd301c05985378f6589dcee3cb2206ef
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5046110
    > Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1229605}
    
    Bug: 1503905
    Change-Id: Ib0e30270e41ceaa4595320c459a15c21b8d0652b
    Low-Coverage-Reason: Code being flagged is already being tested indirectly in BackButtonToolbarTest
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5062519
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Geoff Huang <geoffhuang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1230139}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/FullscreenAlertDialog.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/FullscreenAlertDialog.java
@@ -18,6 +18,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.Toolbar;
 
 import org.chromium.base.BuildInfo;
+import org.chromium.components.browser_ui.util.AutomotiveUtils;
 
 /**
  * Full screen AlertDialog in Clank
@@ -45,7 +46,10 @@ public class FullscreenAlertDialog extends AlertDialog {
         if (BuildInfo.getInstance().isAutomotive) {
             View automotiveLayout =
                     LayoutInflater.from(mContext)
-                            .inflate(R.layout.automotive_layout_with_back_button_toolbar, null);
+                            .inflate(
+                                    AutomotiveUtils.getAutomotiveLayoutWithBackButtonToolbar(
+                                            mContext),
+                                    null);
             ((ViewGroup) automotiveLayout).addView(view);
             mAutomotiveToolbar = automotiveLayout.findViewById(R.id.back_button_toolbar);
             super.setView(automotiveLayout);
@@ -68,7 +72,9 @@ public class FullscreenAlertDialog extends AlertDialog {
                     (ViewGroup)
                             LayoutInflater.from(mContext)
                                     .inflate(
-                                            R.layout.automotive_layout_with_back_button_toolbar,
+                                            AutomotiveUtils
+                                                    .getAutomotiveLayoutWithBackButtonToolbar(
+                                                            mContext),
                                             null);
             automotiveLayout.addView(view, params);
             mAutomotiveToolbar = automotiveLayout.findViewById(R.id.back_button_toolbar);
@@ -103,7 +109,10 @@ public class FullscreenAlertDialog extends AlertDialog {
             if (BuildInfo.getInstance().isAutomotive) {
                 View automotiveLayout =
                         LayoutInflater.from(mContext)
-                                .inflate(R.layout.automotive_layout_with_back_button_toolbar, null);
+                                .inflate(
+                                        AutomotiveUtils.getAutomotiveLayoutWithBackButtonToolbar(
+                                                mContext),
+                                        null);
                 mAutomotiveToolbar = automotiveLayout.findViewById(R.id.back_button_toolbar);
                 ViewStub stub = automotiveLayout.findViewById(R.id.original_layout);
                 stub.setLayoutResource(layoutResId);
@@ -122,7 +131,9 @@ public class FullscreenAlertDialog extends AlertDialog {
                         (ViewGroup)
                                 LayoutInflater.from(mContext)
                                         .inflate(
-                                                R.layout.automotive_layout_with_back_button_toolbar,
+                                                AutomotiveUtils
+                                                        .getAutomotiveLayoutWithBackButtonToolbar(
+                                                                mContext),
                                                 null);
                 mAutomotiveToolbar = automotiveLayout.findViewById(R.id.back_button_toolbar);
                 automotiveLayout.addView(view);
