94349d57af2f4ec1c602b5f8f03410de6380e340
Eliot Courtney <edcourtney@google.com>
2023-11-28 01:20:22 +0000

    Add test checking exo windows can be throttled.
    Bug: 1504379
    Test: added unittest
    Change-Id: I520df6af9d575e29c356dcafdd0818dfcabe5de2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5061451
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229669}

--- components/exo/shell_surface_unittest.cc
+++ components/exo/shell_surface_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/capture_mode/capture_mode_test_util.h"
 #include "ash/constants/app_types.h"
 #include "ash/frame/non_client_frame_view_ash.h"
+#include "ash/frame_throttler/frame_throttling_controller.h"
 #include "ash/public/cpp/test/shell_test_api.h"
 #include "ash/public/cpp/window_properties.h"
 #include "ash/shell.h"
@@ -3404,6 +3405,25 @@ TEST_F(ShellSurfaceTest, ThrottleFrameRate) {
   window->SetProperty(ash::kFrameRateThrottleKey, false);
 
   shell_surface->root_surface()->RemoveSurfaceObserver(&observer);
+}
+
+TEST_F(ShellSurfaceTest, ThrottleFrameRateViaController) {
+  // Set app type to lacros so we can check ash::kFrameRateThrottleKey property
+  // as well.
+  auto shell_surface = test::ShellSurfaceBuilder({20, 20})
+                           .SetAppType(ash::AppType::LACROS)
+                           .BuildShellSurface();
+  aura::Window* window = shell_surface->GetWidget()->GetNativeWindow();
+
+  ash::FrameThrottlingController* frame_throttling_controller =
+      ash::Shell::Get()->frame_throttling_controller();
+  frame_throttling_controller->StartThrottling({window});
+
+  EXPECT_THAT(frame_throttling_controller->GetFrameSinkIdsToThrottle(),
+              testing::UnorderedElementsAreArray(
+                  {shell_surface->GetSurfaceId().frame_sink_id()}));
+
+  EXPECT_TRUE(window->GetProperty(ash::kFrameRateThrottleKey));
 }
 
 namespace {
