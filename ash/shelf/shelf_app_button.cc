c3044f72bd4a4cb05ea470e84f9feaf9007f39cd
Ana Salazar <anasalazar@chromium.org>
2023-11-28 20:25:01 +0000

    Update all instances of the progress indicator to rounded caps
    Previously, we only had rounded caps for the app list and shelf apps
    progress indicators.  To keep consistency across surfaces, make the
    round caps as the default strategy to paint the progress indicator.
    
    Additionally, modify the painting algorithm to adapt to the rounded caps:
    
    - For drawing the arc during the pending animation, if start < end,
      instead of drawing two paths created by CreatePathSegment(), generate
      both paths separately and join them, so only one path is drawn and the
      intersection is accounted as part of the path.
    
    - For drawing at 0 progress, skip creating a path in CreatePathSegment()
      if start == end to make sure that nothing is drawn, since the rounded
      caps may be drawn at a zero-length segment.
    
    See bug for screen recording of the result.
    
    Bug: b:311460259
    Change-Id: I2f200d31c6fa0f462f6f85c5c77bfae2f3231f94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5064110
    Reviewed-by: David Black <dmblack@google.com>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230100}

--- ash/shelf/shelf_app_button.cc
+++ ash/shelf/shelf_app_button.cc
@@ -1416,7 +1416,6 @@ void ShelfAppButton::UpdateProgressRingBounds() {
                          : ProgressIndicator::kProgressComplete;
             },
             base::Unretained(this)));
-    progress_indicator_->SetHasRoundCap(true);
     progress_indicator_->SetInnerIconVisible(false);
     progress_indicator_->SetInnerRingVisible(false);
     progress_indicator_->SetOuterRingStrokeWidth(2.0);
