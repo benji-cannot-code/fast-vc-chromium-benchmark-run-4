7d7b377bba88d59cc4cdabc925480954345a7f54
Ian Wells <iwells@chromium.org>
2023-11-28 20:40:37 +0000

    Allow pausing voice previews, and show buffering state
    This change makes the voice menu's preview play buttons stateful:
    * when loading the preview, show a spinner instead
    * when playing, show a pause icon, and pause when clicked
    * when paused, stopped, or in error, show a play icon, and play when clicked
    
    Bug: b/284358569
    Change-Id: I0ba6c2910c5ddd45c788bbe07725c31c61592fca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5062523
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230129}

--- chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/player/expanded/MenuItem.java
+++ chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/player/expanded/MenuItem.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
@@ -50,6 +51,8 @@ public class MenuItem extends FrameLayout {
     private final @Action int mActionType;
     private final Menu mMenu;
     private final LinearLayout mLayout;
+    private final ImageView mPlayButton;
+    private final ProgressBar mPlayButtonSpinner;
     private Callback<Boolean> mToggleHandler;
 
     /**
@@ -125,6 +128,9 @@ public class MenuItem extends FrameLayout {
                 break;
         }
         addView(layout);
+
+        mPlayButton = (ImageView) findViewById(R.id.play_button);
+        mPlayButtonSpinner = (ProgressBar) findViewById(R.id.spinner);
     }
 
     void setToggleHandler(Callback<Boolean> handler) {
@@ -132,9 +138,8 @@ public class MenuItem extends FrameLayout {
     }
 
     void addPlayButton() {
-        ImageView playButton = (ImageView) findViewById(R.id.play_button);
-        playButton.setVisibility(View.VISIBLE);
-        playButton.setOnClickListener(
+        mPlayButton.setVisibility(View.VISIBLE);
+        mPlayButton.setOnClickListener(
                 (view) -> {
                     mMenu.onPlayButtonClicked(mId);
                 });
@@ -151,6 +156,24 @@ public class MenuItem extends FrameLayout {
         } else if (mActionType == Action.RADIO) {
             getRadioButton().setChecked(value);
         }
+    }
+
+    void showPlayButtonSpinner() {
+        mPlayButton.setVisibility(View.GONE);
+        mPlayButtonSpinner.setVisibility(View.VISIBLE);
+    }
+
+    void showPlayButton() {
+        mPlayButtonSpinner.setVisibility(View.GONE);
+        mPlayButton.setVisibility(View.VISIBLE);
+    }
+
+    void setPlayButtonStopped() {
+        mPlayButton.setImageResource(R.drawable.mini_play_button);
+    }
+
+    void setPlayButtonPlaying() {
+        mPlayButton.setImageResource(R.drawable.mini_pause_button);
     }
 
     private void setEndView(LinearLayout layout, View view) {
