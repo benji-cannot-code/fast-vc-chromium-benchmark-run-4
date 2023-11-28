c57c8cec3c9e77290d57c5da190f9df325adffdf
mikt <mikt@google.com>
2023-11-28 10:57:34 +0000

    [MO] Add ADVANCED_MEMORY_SAFETY_CHECKS() to RenderProcessHost
    Add `ADVANCED_MEMORY_SAFETY_CHECKS()` to:
    - `content::RenderProcessHost`.
    
    This enforces additional memory safety checks by overloading operator
    new/delete. The feature is behind a feature flag disabled by default.
    
    We will experiment later with the flag to understand how much the
    feature will cost.
    
    RenderProcessHost has been chosen, because it is likely to have
    lifetime bugs and is used massively.
    
    Bug: 1505316
    Change-Id: I12d9e51b3be349cc8df486529b04582d3a0b102a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5062642
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229831}

--- content/public/browser/render_process_host.h
+++ content/public/browser/render_process_host.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/clang_profiling_buildflags.h"
 #include "base/containers/id_map.h"
 #include "base/functional/function_ref.h"
+#include "base/memory/safety_checks.h"
 #include "base/process/kill.h"
 #include "base/process/process.h"
 #include "base/supports_user_data.h"
@@ -122,6 +123,10 @@ class Renderer;
 class CONTENT_EXPORT RenderProcessHost : public IPC::Sender,
                                          public IPC::Listener,
                                          public base::SupportsUserData {
+  // Do not remove this macro!
+  // The macro is maintained by the memory safety team.
+  ADVANCED_MEMORY_SAFETY_CHECKS();
+
  public:
   using iterator = base::IDMap<RenderProcessHost*>::iterator;
 
