76e4357e1356ae15437e52ee964aa49364610af8
Tao Wu <wutao@chromium.org>
2023-11-28 00:13:39 +0000

    Roll src/chromeos/assistant/internal/ 98f474322..10ab73447 (1 commit)
    https://chrome-internal.googlesource.com/chrome/assistant.git/+log/98f4743229f8..10ab734474bc
    
    $ git log 98f474322..10ab73447 --date=short --no-merges --format='%ad %ae %s'
    2023-11-22 wutao assistant: Remove alarm related files
    
    Created with:
      roll-dep src/chromeos/assistant/internal
    
    Bug: b/300168375
    Test: build and run tests locally
    Change-Id: If9b8d99b4da8d82b1928a8e6125d0b1e31b45197
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5064113
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Yuki Awano <yawano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1229625}

--- chromeos/ash/services/libassistant/test_support/fake_assistant_client.cc
+++ chromeos/ash/services/libassistant/test_support/fake_assistant_client.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/services/libassistant/grpc/utils/timer_utils.h"
 #include "chromeos/ash/services/libassistant/public/cpp/assistant_timer.h"
 #include "chromeos/assistant/internal/proto/shared/proto/v2/delegate/event_handler_interface.pb.h"
-#include "chromeos/assistant/internal/test_support/fake_alarm_timer_manager.h"
 
 namespace ash::libassistant {
 
