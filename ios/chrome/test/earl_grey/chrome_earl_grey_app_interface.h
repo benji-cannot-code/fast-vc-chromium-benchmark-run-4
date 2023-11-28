c75a56a7873438ccabde96ff42a62b654309c55e
Samar <samarchehade@google.com>
2023-11-28 18:39:36 +0000

    [iOS][Waffle] Fix failing FRE EgTests
    The API used to check for the choice screen was updated in https://crrev.com/c/5060271 but I forgot to also update the test code. This CL corrects that mistake.
    
    Bug: 1492453, b/289998773
    Change-Id: I136a14eb36c8a8a935a789764b24d99809cc8164
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5067255
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1230050}

--- ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
+++ ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
@@ -695,7 +695,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - Default Search Engine Choice Screen Utilities
 
 // Returns YES if the search engine choice screen will be shown.
-+ (BOOL)isChoiceEnabled;
++ (BOOL)IsSearchEngineChoiceScreenEnabledFre;
 
 #pragma mark - First Run Utilities
 
