a9f1e3ba1dcdaf44eb6eb4034ed3af3f3c5b34e0
Peilin Wang <peilinwang@google.com>
2023-11-28 21:09:22 +0000

    [OmniboxTextAndAnimationOptimizations] Enable by default
    40% finch experiment showed improvements to fcp/lcp and search latency
    
    Bug: 1432727
    Change-Id: I079ecb59c3ad71a8620682f0e8db494209ce315c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5067717
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Peilin Wang <peilinwang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1230179}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -1006,7 +1006,7 @@ BASE_FEATURE(kRestoreTabsOnFRE,
 
 BASE_FEATURE(kScrollToTLDOptimization,
              "ScrollToTLDOptimization",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kShowNtpAtStartupAndroid,
              "ShowNtpAtStartupAndroid",
