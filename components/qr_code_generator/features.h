231ab7f8b868a1cbd253e06df27ed80dea57f065
Lukasz Anforowicz <lukasza@chromium.org>
2023-11-28 01:49:26 +0000

    Remove `enable_rust_qr` build flag.
    QR code generator implemented in Rust has shipped to Windows, MacOS,
    Linux, and Android (see https://crrev.com/c/5061588) and is launching to
    ChromeOS.  This means that soon Rust toolchain will be required to build
    QR support in Chrome - this CL makes an early, small step in this
    direction by removing the `enable_rust_qr` build flag.
    
    Removing the `enable_rust_qr` build flag is a prerequisite for removing
    the C++ implementation of the QR code generator.  Removal of the C++
    implementation will not happen until ChromeOS launch process completes
    (hopefully in a month or so) but removing the `enable_rust_qr` build
    flag early should help to flush out any potential issues early (e.g.
    help to identify places where Rust toolchain needs to be added in
    third-party builds of Chromium).
    
    Bug: 1431991
    Change-Id: Ib612b96c153c0d311ce6c31ae0415a67c1a3f6a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5064208
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229679}

--- components/qr_code_generator/features.h
+++ components/qr_code_generator/features.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_QR_CODE_GENERATOR_FEATURES_H_
 
 #include "base/feature_list.h"
-#include "components/qr_code_generator/rust_buildflags.h"
 
 namespace qr_code_generator {
 
@@ -15,10 +14,8 @@ namespace qr_code_generator {
 // but product code should instead use `IsRustyQrCodeGeneratorFeatureEnabled`.
 BASE_DECLARE_FEATURE(kRustyQrCodeGeneratorFeature);
 
-// Returns true if Rust should be used for QR code generation:
-// 1) the GN-level `enable_rust_qr` is true.
-// *and*
-// 2) the `"RustyQrCodeGenerator"` base::Feature has been enabled.
+// Returns true if Rust should be used for QR code generation - i.e. if
+// the `"RustyQrCodeGenerator"` base::Feature has been enabled.
 //
 // If Rust is used for QR code generation then:
 // 1) //components/qr_code_generator becomes a thin wrapper around a 3rd-party
@@ -30,11 +27,7 @@ BASE_DECLARE_FEATURE(kRustyQrCodeGeneratorFeature);
 // See https://crbug.com/1431991 for more details about the feature and the
 // Rust QR Code Generator project.
 inline bool IsRustyQrCodeGeneratorFeatureEnabled() {
-#if BUILDFLAG(ENABLE_RUST_QR)
   return base::FeatureList::IsEnabled(kRustyQrCodeGeneratorFeature);
-#else
-  return false;
-#endif
 }
 
 }  // namespace qr_code_generator
