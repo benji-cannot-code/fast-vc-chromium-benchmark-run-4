a1fcb9a87aa3c346e822e87a095e0235731eceb8
Jason Lin <lxj@google.com>
2023-11-28 10:59:43 +0000

    Add flag content-settings-partitioning
    Bug: b/307193732
    Change-Id: I48809e1e63cd24750b0708d0046f038b33e43723
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5055037
    Commit-Queue: Jason Lin <lxj@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229832}

--- components/content_settings/core/common/features.cc
+++ components/content_settings/core/common/features.cc
@@ -88,5 +88,9 @@ BASE_FEATURE(kTpcdHeuristicsGrants,
 const base::FeatureParam<bool> kTpcdReadHeuristicsGrants{
     &kTpcdHeuristicsGrants, kTpcdReadHeuristicsGrantsName, false};
 
+BASE_FEATURE(kContentSettingsPartitioning,
+             "ContentSettingsPartitioning",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 }  // namespace features
 }  // namespace content_settings
