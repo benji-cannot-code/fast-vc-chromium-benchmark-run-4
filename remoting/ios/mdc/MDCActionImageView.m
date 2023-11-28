660320ae946cfdf17c8f31223f23b5cfecaa4242
Olivier ROBIN <olivierrobin@google.com>
2023-11-28 15:04:46 +0000

    [Remoting/IOS] Remove imports of MaterialAnimationTiming.h
    AnimationTiming was removed in crrev.com/c/5063840
    The component is actually not used, so remove the import.
    
    Fixed: 1505725
    Change-Id: I31990103bfa42ee43f3f9f798c6ea811e2a8870c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5063061
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Owners-Override: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229896}

--- remoting/ios/mdc/MDCActionImageView.m
+++ remoting/ios/mdc/MDCActionImageView.m
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "remoting/ios/mdc/MDCActionImageView.h"
 
 #import <Foundation/Foundation.h>
-#import <MaterialComponents/MaterialAnimationTiming.h>
 
 static const CGFloat kIconRotationRadians = 0.375f * 2 * M_PI;
 static const CGFloat kIconShrinkScale = 0.4f;
