13ce29f1bf3be9ea6aaeb130ec1ab078507fe113
Jan Keitel <jkeitel@google.com>
2023-11-28 13:32:30 +0000

    Include extension hosts in custom cursor suppression.
    Prior to this CL, the CustomCursorSuppressor suppressed custom cursors
    in all WebContents that belonged to a Browser object, but not
    WebContents that host extension content. This CL extends the custom
    cursor suppression to all WebContents that belong to an extension
    for any of the profiles for which Browser objects exist.
    
    Bug: 1497985
    Change-Id: I9648cd735cc2969ee9b08e814230574ab75134e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5023870
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1229867}

--- extensions/browser/extension_host_registry.cc
+++ extensions/browser/extension_host_registry.cc
@@ -185,4 +185,10 @@ void ExtensionHostRegistry::RemoveObserver(Observer* observer) {
   observers_.RemoveObserver(observer);
 }
 
+void ExtensionHostRegistry::Shutdown() {
+  for (Observer& observer : observers_) {
+    observer.OnExtensionHostRegistryShutdown(this);
+  }
+}
+
 }  // namespace extensions
