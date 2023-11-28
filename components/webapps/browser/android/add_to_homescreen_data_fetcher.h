a7bbbac4bf2e5bdd78493c9ef9545e1c1e77ddbb
Ella Ge <eirage@chromium.org>
2023-11-28 21:55:06 +0000

    Let AddToHomescreenDataFetcher installable result decide app_type
    AddToHomescreenDataFetcher will provide the app_type base on if the
    result is webapk compatible directly, instead of shortcut_info_.source.
    This should be a no-op change, just cleaning up the logic
    
    Bug: 1502255
    Change-Id: I0f30629c2690a6f721ec5d9d3511e22da10837cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5067735
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230227}

--- components/webapps/browser/android/add_to_homescreen_data_fetcher.h
+++ components/webapps/browser/android/add_to_homescreen_data_fetcher.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/cancelable_task_tracker.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
+#include "components/webapps/browser/android/add_to_homescreen_params.h"
 #include "components/webapps/browser/android/shortcut_info.h"
 #include "components/webapps/browser/installable/installable_logging.h"
 #include "components/webapps/common/web_page_metadata_agent.mojom.h"
@@ -46,6 +47,7 @@ class AddToHomescreenDataFetcher {
     virtual void OnDataAvailable(
         const ShortcutInfo& info,
         const SkBitmap& primary_icon,
+        AddToHomescreenParams::AppType app_type,
         const InstallableStatusCode installable_status) = 0;
 
    protected:
