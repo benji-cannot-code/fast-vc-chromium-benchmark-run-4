e4e20dfe2dd13d1b315a52c8d210fc4981e4ccda
Boris Sazonov <bsazonov@chromium.org>
2023-11-28 11:03:49 +0000

    [UNO] Fix AuthenticationFlowTest when ReplaceSyncPromosWithSignInPromos is on
    Prior to this CL,
    TestUserPolicyForManagedAccountForSigninConsentLevelWhenEligible
    was failing with ReplaceSyncPromosWithSignInPromos enabled by default
    (the test wasn't expecting that a managed confirmation would be shown
    during the flow). This CL explicitly disables
    ReplaceSyncPromosWithSignInPromos in the test, because the flow with
    both features enabled is tested by
    TestShowManagedConfirmationForSigninConsentLevelIfAllFeaturesEnabled.
    
    Bug: 1490558
    Change-Id: Ic2b1b11cc74c399259cf80837fc9ed2a80dcc49d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5062772
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229833}

--- ios/chrome/browser/ui/authentication/authentication_flow_unittest.mm
+++ ios/chrome/browser/ui/authentication/authentication_flow_unittest.mm
@@ -609,7 +609,8 @@ TEST_F(AuthenticationFlowTest,
        TestUserPolicyForManagedAccountForSigninConsentLevelWhenEligible) {
   base::test::ScopedFeatureList scoped_feature_list;
   scoped_feature_list.InitWithFeatures(
-      {policy::kUserPolicyForSigninAndNoSyncConsentLevel}, {});
+      {policy::kUserPolicyForSigninAndNoSyncConsentLevel},
+      {syncer::kReplaceSyncPromosWithSignInPromos});
 
   CreateAuthenticationFlow(
       PostSignInAction::kNone, managed_identity_,
