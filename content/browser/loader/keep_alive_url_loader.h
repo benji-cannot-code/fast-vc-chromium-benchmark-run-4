2b207033b59f35e1194de9d2758a30645d4a4e03
Ming-Ying Chung <mych@chromium.org>
2023-11-28 11:27:15 +0000

    Reland "[fetch-later] Show requests in DevTools when Document is alive"
    This is a reland of commit ace4985d887375341489f511ae425390542371c7
    
    Fixed potential flakiness caused by already started
    `disconnected_loader_timer_`.
    
    Original change's description:
    > [fetch-later] Show requests in DevTools when Document is alive
    >
    > Per discussion in [1], there is currently no good way to log a fetch
    > keepalive request if RenderFrameHost is gone.
    >
    > This CL just adds DevTools network logging for fetchLater requests (
    > a new JS API that is also a fetch keepalive request, but entirely
    > handled in Browser process) when its initiator RenderFrameHost is still
    > alive.
    >
    > [1]: https://chromestatus.com/feature/4654499737632768?context=myfeatures&gate=4947446974644224
    >
    > Bug: 1465781
    > Change-Id: I94bb341f0c7c98132c9789a79f7cf61002cc8b27
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5035741
    > Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    > Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    > Reviewed-by: Danil Somsikov <dsv@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1227819}
    
    Bug: 1465781
    Change-Id: If229e3f336f17dff42cd85e6f6d32405259acdd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5057111
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229843}

--- content/browser/loader/keep_alive_url_loader.h
+++ content/browser/loader/keep_alive_url_loader.h
@@ -43,6 +43,7 @@ namespace content {
 class BrowserContext;
 class KeepAliveURLLoaderService;
 class PolicyContainerHost;
+class RenderFrameHostImpl;
 class WeakDocumentPtr;
 
 // A URLLoader for loading a fetch keepalive request via the browser process,
@@ -179,6 +180,10 @@ class CONTENT_EXPORT KeepAliveURLLoader
   // called. Otherwise, returns false by default.
   bool IsStarted() const;
 
+  // Returns a pointer to the RenderFrameHostImpl of the request initiator
+  // document if it is still alive. Otherwise, returns nullptr;
+  RenderFrameHostImpl* GetInitiator() const;
+
   // Receives actions from renderer.
   // `network::mojom::URLLoader` overrides:
   void FollowRedirect(
@@ -252,6 +257,9 @@ class CONTENT_EXPORT KeepAliveURLLoader
 
   // The ID to identify the request being loaded by this loader.
   const int32_t request_id_;
+
+  // The ID to identify the request used by DevTools
+  const std::string devtools_request_id_;
 
   // A bitfield of the options of the request being loaded.
   // See services/network/public/mojom/url_loader_factory.mojom.
