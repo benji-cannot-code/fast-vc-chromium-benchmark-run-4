58551241d96a9e334e60654e55713026542530de
dpapad <dpapad@chromium.org>
2023-11-28 20:57:03 +0000

    WebUI: Move web_ui_test_handler.h,cc to a CrOS-only folder.
    Bug: 1347740
    Change-Id: I7e17793de7961755070a2e431b25ddd4d02f2f5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5055201
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230158}

--- chrome/test/base/ash/web_ui_browser_test.cc
+++ chrome/test/base/ash/web_ui_browser_test.cc
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
-#include "chrome/browser/ui/webui/web_ui_test_handler.h"
+#include "chrome/browser/ui/webui/ash/web_ui_test_handler.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/test/base/ash/js_test_api.h"
