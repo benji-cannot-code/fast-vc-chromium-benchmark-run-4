c7e19306b272280c9ffcde936f3b23878473446f
Hiroshige Hayashizaki <hiroshige@chromium.org>
2023-11-28 23:58:53 +0000

    Remove WorkerScriptLoader::MaybeCreateLoaderForResponse
    After
    https://chromium-review.googlesource.com/c/chromium/src/+/5048055
    it's clearer that `MaybeCreateLoaderForResponse()` is no-op
    for `WorkerScriptLoader` as it's no-op for
    `ServiceWorkerMainResourceLoaderInterceptor`.
    
    Bug: 1403746
    Change-Id: I8f433ca180dc3f84fb1c5f3a46539a589505d998
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5064534
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230317}

--- content/browser/service_worker/service_worker_main_resource_loader_interceptor.h
+++ content/browser/service_worker/service_worker_main_resource_loader_interceptor.h
@@ -82,6 +82,9 @@ class CONTENT_EXPORT ServiceWorkerMainResourceLoaderInterceptor final
   absl::optional<SubresourceLoaderParams> MaybeCreateSubresourceLoaderParams()
       override;
 
+  // MaybeCreateLoaderForResponse() should NOT overridden here, because
+  // `WorkerScriptLoader` assumes so.
+
  private:
   friend class ServiceWorkerMainResourceLoaderInterceptorTest;
 
