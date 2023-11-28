c0ab860e147ef9b8a816eb4ed9c9a92ac19b25ac
Wes Okuhara <wesokuhara@google.com>
2023-11-28 18:46:25 +0000

    CrOS Settings: Update icon for storage search results
    When the wayfinding revamp is enabled, the  icon in storage-related
    search results should match the storage icon shown in the row. Also, the
    kHardDrive icon was renamed to kStorage.
    Screenshot: http://screen/4YB9KNtwg6KuTS9.png
    
    Bug: b:311201917
    Test: Manually verify UI
    Change-Id: I038a8c2dee00b394d127d8cf297453e227882edd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5064114
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230062}

--- chrome/browser/ui/webui/ash/settings/pages/storage/storage_section.cc
+++ chrome/browser/ui/webui/ash/settings/pages/storage/storage_section.cc
@@ -34,7 +34,7 @@ const std::vector<SearchConcept>& GetDefaultSearchConcepts() {
   static const base::NoDestructor<std::vector<SearchConcept>> tags({
       {IDS_OS_SETTINGS_TAG_STORAGE,
        mojom::kStorageSubpagePath,
-       mojom::SearchResultIcon::kHardDrive,
+       mojom::SearchResultIcon::kStorage,
        mojom::SearchResultDefaultRank::kMedium,
        mojom::SearchResultType::kSubpage,
        {.subpage = mojom::Subpage::kStorage},
@@ -48,7 +48,7 @@ const std::vector<SearchConcept>& GetExternalStorageSearchConcepts() {
   static const base::NoDestructor<std::vector<SearchConcept>> tags({
       {IDS_OS_SETTINGS_TAG_EXTERNAL_STORAGE,
        mojom::kExternalStorageSubpagePath,
-       mojom::SearchResultIcon::kHardDrive,
+       mojom::SearchResultIcon::kStorage,
        mojom::SearchResultDefaultRank::kMedium,
        mojom::SearchResultType::kSubpage,
        {.subpage = mojom::Subpage::kExternalStorage}},
@@ -140,7 +140,7 @@ mojom::Section StorageSection::GetSection() const {
 }
 
 mojom::SearchResultIcon StorageSection::GetSectionIcon() const {
-  return mojom::SearchResultIcon::kHardDrive;
+  return mojom::SearchResultIcon::kStorage;
 }
 
 const char* StorageSection::GetSectionPath() const {
@@ -158,11 +158,11 @@ bool StorageSection::LogMetric(mojom::Setting setting,
 void StorageSection::RegisterHierarchy(HierarchyGenerator* generator) const {
   generator->RegisterTopLevelSubpage(
       IDS_SETTINGS_STORAGE_TITLE, mojom::Subpage::kStorage,
-      mojom::SearchResultIcon::kHardDrive,
+      mojom::SearchResultIcon::kStorage,
       mojom::SearchResultDefaultRank::kMedium, mojom::kStorageSubpagePath);
   generator->RegisterNestedSubpage(
       IDS_SETTINGS_STORAGE_EXTERNAL, mojom::Subpage::kExternalStorage,
-      mojom::Subpage::kStorage, mojom::SearchResultIcon::kHardDrive,
+      mojom::Subpage::kStorage, mojom::SearchResultIcon::kStorage,
       mojom::SearchResultDefaultRank::kMedium,
       mojom::kExternalStorageSubpagePath);
 }
