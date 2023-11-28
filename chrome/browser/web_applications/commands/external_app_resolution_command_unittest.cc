ebad46d325cb179393c37c0323853ce7dc30ce35
Dibyajyoti Pal <dibyapal@google.com>
2023-11-28 01:48:45 +0000

    [dPWA] Track install result while installing from info for external installs
    A previous CL (crrev.com/c/5009910) fixed the InstallFromInfo command where the tracking of the install result was moved from the job only to the command so that installs are only tracked once. However, that broke the metrics collection of ExternalAppResolutionCommand for paths that install an app from the app_info.
    
    This CL adds a way of tracking the install metrics for a successful
    installation from web app info for external installs. This also updates
    existing tests so that future breakages can be easily caught if any.
    
    Bug: 1504201
    Change-Id: Ic92a4c9f9859ada6b68dc17850985fd50a4b44c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5050894
    Auto-Submit: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229678}

--- chrome/browser/web_applications/commands/external_app_resolution_command_unittest.cc
+++ chrome/browser/web_applications/commands/external_app_resolution_command_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_map.h"
 #include "base/run_loop.h"
 #include "base/test/bind.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/test_future.h"
 #include "base/test/to_vector.h"
 #include "chrome/browser/web_applications/commands/callback_command.h"
@@ -715,6 +716,7 @@ TEST_F(ExternalAppResolutionCommandTest, InstallURLLoadFailed) {
 }
 
 TEST_F(ExternalAppResolutionCommandTest, InstallWithWebAppInfoSucceeds) {
+  base::HistogramTester tester;
   const GURL kWebAppUrl("https://foo.example");
   ExternalInstallOptions options(kWebAppUrl,
                                  mojom::UserDisplayMode::kStandalone,
@@ -747,9 +749,14 @@ TEST_F(ExternalAppResolutionCommandTest, InstallWithWebAppInfoSucceeds) {
             mojom::UserDisplayMode::kStandalone);
   EXPECT_EQ(registrar().GetLatestAppInstallSource(app_id),
             webapps::WebappInstallSource::EXTERNAL_DEFAULT);
+
+  // Ensure that the WebApp.Install.Result histogram is only measured once.
+  tester.ExpectBucketCount("WebApp.Install.Result", /*sample=*/true,
+                           /*expected_count=*/1);
 }
 
 TEST_F(ExternalAppResolutionCommandTest, InstallWithWebAppInfoFails) {
+  base::HistogramTester tester;
   const GURL kWebAppUrl("https://foo.example");
   ExternalInstallOptions options(kWebAppUrl,
                                  mojom::UserDisplayMode::kStandalone,
@@ -775,6 +782,8 @@ TEST_F(ExternalAppResolutionCommandTest, InstallWithWebAppInfoFails) {
   EXPECT_FALSE(result.app_id.has_value());
 
   EXPECT_FALSE(id.has_value());
+  tester.ExpectBucketCount("WebApp.Install.Result", /*sample=*/false,
+                           /*expected_count=*/1);
 }
 
 TEST_F(ExternalAppResolutionCommandTest, SucessInstallForcedContainerWindow) {
