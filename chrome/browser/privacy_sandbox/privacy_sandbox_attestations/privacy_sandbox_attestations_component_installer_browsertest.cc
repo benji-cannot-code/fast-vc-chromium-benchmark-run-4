8d8ff0fbe4e29a369fb5f7f2eaa00de76b6c46ca
Xiaochen Zhou <xiaochenzh@chromium.org>
2023-11-28 18:09:04 +0000

    [Privacy Sandbox] Remove the unnecessary scoped feature list in attestation browser test.
    The attestation feature is on by default. This scoped feature list is
    no longer needed.
    
    Change-Id: If83b4a56f870d1b902643d9b207761049824745f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5067953
    Commit-Queue: Xiaochen Zhou <xiaochenzh@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229996}

--- chrome/browser/privacy_sandbox/privacy_sandbox_attestations/privacy_sandbox_attestations_component_installer_browsertest.cc
+++ chrome/browser/privacy_sandbox/privacy_sandbox_attestations/privacy_sandbox_attestations_component_installer_browsertest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/path_service.h"
 #include "base/run_loop.h"
-#include "base/test/scoped_feature_list.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/version.h"
 #include "chrome/browser/browser_process.h"
@@ -44,7 +43,6 @@ class PrivacySandboxAttestationsBrowserTest
       component_updater::PrivacySandboxAttestationsComponentInstallerPolicy;
 
  private:
-  base::test::ScopedFeatureList attestations_feature_;
   PrivacySandboxAttestationsMixin privacy_sandbox_attestations_mixin_{
       &mixin_host_};
 };
