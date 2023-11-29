11a9b2847ee1d3a30d8fe9a6c57ee313b5ff8603
Lambros Lambrou <lambroslambrou@chromium.org>
2023-11-29 22:45:02 +0000

    [remoting] C++ wrapper for GNOME DisplayConfig APIs
    This provides a C++ wrapper around the GNOME D-Bus APIs for getting the
    current display config, and applying a new config.
    
    This is incomplete - not all returned fields or properties are stored.
    But this implements the parts we will expect to use, and it is easy to
    add support for more properties.
    
    Bug: b:268730721
    Low-Coverage-Reason: HARD_TO_TEST: Callbacks from GNOME D-Bus API are hard to cover.
    
    Change-Id: I0669471dc293132791756764e3fb32d368a8048f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4865594
    Reviewed-by: Erik Jensen <rkjnsn@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230969}

---
+++ remoting/host/linux/scoped_glib.h
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_HOST_LINUX_SCOPED_GLIB_H_
+#define REMOTING_HOST_LINUX_SCOPED_GLIB_H_
+
+#include <gio/gio.h>
+
+#include <memory>
+
+// This file defines some scoper classes and utilities for some GLib types that
+// are not derived from GObject.
+
+namespace remoting {
+
+namespace internal {
+
+struct GVariantDeleter {
+  void operator()(GVariant* variant);
+};
+
+}  // namespace internal
+
+using ScopedGVariant = std::unique_ptr<GVariant, internal::GVariantDeleter>;
+
+// Takes ownership of a GVariant reference. If the reference is floating, it
+// will be sunk.
+ScopedGVariant TakeGVariant(GVariant* variant);
+
+}  // namespace remoting
+
+#endif  // REMOTING_HOST_LINUX_SCOPED_GLIB_H_
