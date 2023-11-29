69dbae3ea3d59ffe658e91fd53978589a128f7dc
Daniel Cheng <dcheng@chromium.org>
2023-11-29 17:47:03 +0000

    Use std::move() to pass PressedCallback in //components/media_message_center.
    PressedCallback will become non-copyable when it is updated to work with
    OnceCallbacks.
    
    This CL was uploaded by git cl split.
    
    Bug: 1326449
    Change-Id: I2dbc30047b9acf485136ea965c448ab236880a4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5069479
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230739}

--- components/media_message_center/media_notification_view_modern_impl.cc
+++ components/media_message_center/media_notification_view_modern_impl.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/media_message_center/media_notification_view_modern_impl.h"
 
+#include <utility>
+
 #include "base/containers/contains.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/ranges/algorithm.h"
@@ -86,7 +88,7 @@ class MediaButton : public views::ImageButton {
  public:
   METADATA_HEADER(MediaButton);
   MediaButton(PressedCallback callback, int icon_size, gfx::Size button_size)
-      : ImageButton(callback), icon_size_(icon_size) {
+      : ImageButton(std::move(callback)), icon_size_(icon_size) {
     SetHasInkDropActionOnClick(true);
     views::InstallRoundRectHighlightPathGenerator(this, gfx::Insets(),
                                                   button_size.height() / 2);
