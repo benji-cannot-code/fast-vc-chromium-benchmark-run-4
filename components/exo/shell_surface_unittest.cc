63d15fe761af60a4b11dee261614ec5b68fafc89
Zoraiz Naeem <zoraiznaeem@chromium.org>
2023-11-29 21:17:21 +0000

    [Exo/Nit] Rename methods to `SetWindowCornersRadii`
    Bug: b:308254636
    Change-Id: Ic82ce72145395dedea9e4f5ad35f19ecda1532a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5034769
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230906}

--- components/exo/shell_surface_unittest.cc
+++ components/exo/shell_surface_unittest.cc
@@ -2882,7 +2882,7 @@ TEST_F(ShellSurfaceTest, ShadowBoundsWithScaleFactor) {
   EXPECT_EQ(gfx::Rect(0, 0, 256, 256), shadow->content_bounds());
 }
 
-TEST_F(ShellSurfaceTest, ShadowRoundedCornersForRoundedWindows) {
+TEST_F(ShellSurfaceTest, ShadowRoundedCorners) {
   constexpr gfx::Point kOrigin(20, 20);
   constexpr int kWindowCornerRadius = 12;
 
@@ -2910,7 +2910,7 @@ TEST_F(ShellSurfaceTest, ShadowRoundedCornersForRoundedWindows) {
   EXPECT_EQ(shadow->rounded_corner_radius_for_testing(), 0);
 
   // Have a window with radius of 12dp.
-  shell_surface->SetWindowCornerRadii(
+  shell_surface->SetWindowCornersRadii(
       gfx::RoundedCornersF(kWindowCornerRadius));
   root_surface->Commit();
 
@@ -2919,7 +2919,7 @@ TEST_F(ShellSurfaceTest, ShadowRoundedCornersForRoundedWindows) {
   EXPECT_EQ(shadow->rounded_corner_radius_for_testing(), kWindowCornerRadius);
 
   // Have a window with radius of 0dp.
-  shell_surface->SetWindowCornerRadii(gfx::RoundedCornersF());
+  shell_surface->SetWindowCornersRadii(gfx::RoundedCornersF());
   root_surface->Commit();
 
   shadow = wm::ShadowController::GetShadowForWindow(window);
