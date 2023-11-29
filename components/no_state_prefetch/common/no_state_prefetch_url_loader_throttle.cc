cf11c62498a72946b39e77b3e21e3cd83c678bdb
Taiyo Mizuhashi <taiyo@chromium.org>
2023-11-29 05:32:30 +0000

    [Code Health] Remove expired histogram: Prerender.NoStatePrefetchResponseTypes
    This CL removes above expired histogram in M85.
    
    This CL is a part of Chrome Code Health Rotation.
    
    OBSOLETE_HISTOGRAM[Prerender.NoStatePrefetchResponseTypes]=expired after M85.
    
    Bug: 1089304
    Change-Id: Ief5dab189f0d3605196f5a3fd22f71f88095c908
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5060896
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230452}

--- components/no_state_prefetch/common/no_state_prefetch_url_loader_throttle.cc
+++ components/no_state_prefetch/common/no_state_prefetch_url_loader_throttle.cc
@@ -31,12 +31,6 @@ void CallCancelPrerenderForUnsupportedScheme(
       ->CancelPrerenderForUnsupportedScheme();
 }
 
-// Returns true if the response has a "no-store" cache control header.
-bool IsNoStoreResponse(const network::mojom::URLResponseHead& response_head) {
-  return response_head.headers &&
-         response_head.headers->HasHeaderValue("cache-control", "no-store");
-}
-
 }  // namespace
 
 NoStatePrefetchURLLoaderThrottle::NoStatePrefetchURLLoaderThrottle(
@@ -139,9 +133,6 @@ void NoStatePrefetchURLLoaderThrottle::WillRedirectRequest(
     net::HttpRequestHeaders* /* modified_headers */,
     net::HttpRequestHeaders* /* modified_cors_exempt_headers */) {
   redirect_count_++;
-  RecordPrefetchResponseReceived(
-      histogram_prefix_, blink::IsRequestDestinationFrame(request_destination_),
-      true /* is_redirect */, IsNoStoreResponse(response_head));
 
   std::string follow_only_when_prerender_shown_header;
   if (response_head.headers) {
@@ -169,9 +160,6 @@ void NoStatePrefetchURLLoaderThrottle::WillProcessResponse(
     bool* defer) {
   bool is_main_resource =
       blink::IsRequestDestinationFrame(request_destination_);
-  RecordPrefetchResponseReceived(histogram_prefix_, is_main_resource,
-                                 true /* is_redirect */,
-                                 IsNoStoreResponse(*response_head));
   RecordPrefetchRedirectCount(histogram_prefix_, is_main_resource,
                               redirect_count_);
 }
