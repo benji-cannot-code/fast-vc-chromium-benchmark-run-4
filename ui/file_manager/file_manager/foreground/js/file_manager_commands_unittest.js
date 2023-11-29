92837388beb485eab45971a4e8dfe86c92f6738c
François Degros <fdegros@chromium.org>
2023-11-29 01:52:21 +0000

    Files app: Remove `done` callbacks from some unit tests
    BUG=b:289003444
    
    Change-Id: Iac7a0d2d77081ded65059d3d0b9091ce30c2acf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5069555
    Auto-Submit: François Degros <fdegros@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230385}

--- ui/file_manager/file_manager/foreground/js/file_manager_commands_unittest.js
+++ ui/file_manager/file_manager/foreground/js/file_manager_commands_unittest.js
@@ -32,9 +32,8 @@ function getMetricName(metricIndex) {
 /**
  * Checks that the `toggle-holding-space` command is appropriately enabled/
  * disabled given the current selection state and executes as expected.
- * @param {()=>void} done
  */
-export async function testToggleHoldingSpaceCommand(done) {
+export async function testToggleHoldingSpaceCommand() {
   // Verify `toggle-holding-space` command exists.
   const command = CommandHandler.getCommand('toggle-holding-space');
   assertNotEquals(command, undefined);
@@ -269,16 +268,13 @@ export async function testToggleHoldingSpaceCommand(done) {
         testCase.expect.isAdd ? 'pin-to-holding-space' :
                                 'unpin-from-holding-space');
   }
-
-  done();
 }
 
 /**
  * Checks that the 'extract-all' command is enabled or disabled
  * dependent on the current selection.
- * @param {()=>void} done
  */
-export async function testExtractAllCommand(done) {
+export async function testExtractAllCommand() {
   // Check: `extract-all` command exists.
   const command = CommandHandler.getCommand('extract-all');
   assertNotEquals(command, undefined);
@@ -415,15 +411,12 @@ export async function testExtractAllCommand(done) {
   zipCommand.canExecute(event, fileManager);
   assertTrue(event.canExecute);
   assertFalse(event.command.hidden);
-
-  done();
 }
 
 /**
  * Tests that rename command should be disabled for Recent entry.
- * @param {()=>void} done
  */
-export async function testRenameCommand(done) {
+export async function testRenameCommand() {
   // Check: `rename` command exists.
   const command = CommandHandler.getCommand('rename');
   assertNotEquals(command, undefined);
@@ -484,8 +477,6 @@ export async function testRenameCommand(done) {
   command.canExecute(event, fileManager);
   assertFalse(event.canExecute);
   assertFalse(event.command.hidden);
-
-  done();
 }
 
 /**
@@ -546,9 +537,8 @@ async function createAndAddNonInteractiveDownloadsVolume() {
  * disabled and hidden when there are no selected entries but the current
  * directory is on a non-interactive volume (e.g. when the blank space in a
  * non-interactive directory is right clicked).
- * @param {()=>void} done
  */
-export async function testCommandsForNonInteractiveVolumeAndNoEntries(done) {
+export async function testCommandsForNonInteractiveVolumeAndNoEntries() {
   const nonInteractiveVolumeInfo =
       await createAndAddNonInteractiveDownloadsVolume();
 
@@ -625,17 +615,14 @@ export async function testCommandsForNonInteractiveVolumeAndNoEntries(done) {
     assertFalse(event.canExecute);
     assertTrue(event.command.hidden);
   }
-
-  done();
 }
 
 /**
  * Tests that the paste, cut, copy, new-folder, delete, move-to-trash,
  * paste-into-folder, rename, extract-all and zip-selection commands should be
  * disabled and hidden for an entry on a non-interactive volume.
- * @param {()=>void} done
  */
-export async function testCommandsForEntriesOnNonInteractiveVolume(done) {
+export async function testCommandsForEntriesOnNonInteractiveVolume() {
   // Create non-interactive volume.
   const nonInteractiveVolumeInfo =
       await createAndAddNonInteractiveDownloadsVolume();
@@ -722,6 +709,4 @@ export async function testCommandsForEntriesOnNonInteractiveVolume(done) {
     assertFalse(event.canExecute);
     assertTrue(event.command.hidden);
   }
-
-  done();
 }
