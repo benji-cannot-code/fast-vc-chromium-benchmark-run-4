e404b14d443fb5cac1d15ccb04a426f4af7b3565
Andrew Paseltiner <apaseltiner@chromium.org>
2023-11-29 19:05:33 +0000

    Add parsers for summary_window_operator and summary_buckets
    https://github.com/WICG/attribution-reporting-api/pull/1111
    
    But do not use them yet. This will make it easier to experiment with
    proofs-of-concept for Full Flex prioritization, e.g.
    crrev.com/c/5037210, after which we will start using these new functions
    in TriggerSpecs::Parse().
    
    This change has no effect on web-visible behavior.
    
    OBSOLETE_HISTOGRAM[Conversions.SourceRegistrationError8]=Replaced by Conversions.SourceRegistrationError9
    
    Bug: 1504219
    Change-Id: I78fbcc62071a2b56051e07811ff5ded0d58b2e9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5049377
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230803}

--- components/attribution_reporting/test_utils.h
+++ components/attribution_reporting/test_utils.h
@@ -25,6 +25,7 @@ class AggregationKeys;
 class DestinationSet;
 class EventReportWindows;
 class SuitableOrigin;
+class SummaryBuckets;
 
 struct AggregatableDedupKey;
 struct EventTriggerData;
@@ -61,6 +62,8 @@ std::ostream& operator<<(std::ostream&, const SuitableOrigin&);
 std::ostream& operator<<(std::ostream&, const AggregatableDedupKey&);
 
 std::ostream& operator<<(std::ostream&, const OsRegistrationItem&);
+
+std::ostream& operator<<(std::ostream&, const SummaryBuckets&);
 
 std::ostream& operator<<(std::ostream&, const TriggerSpec&);
 
