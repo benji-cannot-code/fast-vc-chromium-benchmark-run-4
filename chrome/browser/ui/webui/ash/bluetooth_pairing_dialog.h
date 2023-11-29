75332124b94a4011764c9412f1df90a91331c41c
Elly <ellyjones@chromium.org>
2023-11-29 19:39:43 +0000

    webui: remove overrides in bluetooth pairing dialog
    Rather than overriding GetDialogSize() and GetDialogArgs(), use
    the setters inherited from the base WebDialogDelegate class. This
    allows not storing device_data.
    
    Bug: 1485127
    Change-Id: Icfb24e8714fda2768b464c5b3711a10ac7b6814b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5053921
    Auto-Submit: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230832}

--- chrome/browser/ui/webui/ash/bluetooth_pairing_dialog.h
+++ chrome/browser/ui/webui/ash/bluetooth_pairing_dialog.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/strings/string_piece.h"
-#include "base/values.h"
 #include "chrome/browser/ui/webui/ash/system_web_dialog_delegate.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chromeos/ash/services/bluetooth_config/public/mojom/cros_bluetooth_config.mojom-forward.h"
@@ -52,15 +51,9 @@ class BluetoothPairingDialog : public SystemWebDialogDelegate {
   std::string Id() override;
   void AdjustWidgetInitParams(views::Widget::InitParams* params) override;
 
-  // ui::WebDialogDelegate
-  void GetDialogSize(gfx::Size* size) const override;
-  std::string GetDialogArgs() const override;
-
   // The canonical Bluetooth address of a device when pairing a specific device,
   // otherwise |kChromeUIBluetoothPairingURL|.
   std::string dialog_id_;
-
-  base::Value::Dict device_data_;
 };
 
 class BluetoothPairingDialogUI;
