f9ba21485d63664d11111981c4e383b230404273
Daniil Sakhapov <sakhapov@chromium.org>
2023-11-29 16:04:44 +0000

    Check if math expression has percentages deep inside
    Currently, only the top level category is checked to have a percentage
    dependency, check all the nodes inside the expression as well,
    as it can be the case like:
    NumberProducingFunction(Percentage) * Pixels.
    The top level category is Length in such case and we try to resolve it
    with ComputeLength, where percentages are not allowed.
    
    Note: the added test would crash before the CL, as it would try to
    GetDouble() of percentage which is not allowed in that context.
    
    Bug: 1505817
    Change-Id: Iefd13c0d190c3b8adf463ea244f81331635e14fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5064155
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230671}

--- third_party/blink/renderer/core/css/css_math_expression_node.h
+++ third_party/blink/renderer/core/css/css_math_expression_node.h
@@ -144,6 +144,7 @@ class CORE_EXPORT CSSMathExpressionNode
   bool HasPercentage() const {
     return category_ == kCalcPercent || category_ == kCalcPercentLength;
   }
+  virtual bool InvolvesPercentage() const { return HasPercentage(); }
 
   // Returns the unit type of the math expression *without doing any type
   // conversion* (e.g., 1px + 1em needs type conversion to resolve).
@@ -435,6 +436,8 @@ class CORE_EXPORT CSSMathExpressionOperation final
     return IsMinOrMax() || IsClamp() || IsSteppedValueFunction() ||
            IsTrigonometricFunction() || IsSignRelatedFunction();
   }
+
+  bool InvolvesPercentage() const final;
 
   String CSSTextAsClamp() const;
 
--- third_party/blink/renderer/core/css/css_primitive_value.cc
+++ third_party/blink/renderer/core/css/css_primitive_value.cc
@@ -193,11 +193,22 @@ bool CSSPrimitiveValue::IsPercentage() const {
   return To<CSSMathFunctionValue>(this)->IsPercentage();
 }
 
+bool CSSPrimitiveValue::IsResolvableLength() const {
+  return IsLength() && !InvolvesPercentage();
+}
+
 bool CSSPrimitiveValue::HasPercentage() const {
   if (IsNumericLiteralValue()) {
     return To<CSSNumericLiteralValue>(this)->IsPercentage();
   }
   return To<CSSMathFunctionValue>(this)->ExpressionNode()->HasPercentage();
+}
+
+bool CSSPrimitiveValue::InvolvesPercentage() const {
+  if (IsNumericLiteralValue()) {
+    return To<CSSNumericLiteralValue>(this)->IsPercentage();
+  }
+  return To<CSSMathFunctionValue>(this)->ExpressionNode()->InvolvesPercentage();
 }
 
 bool CSSPrimitiveValue::IsTime() const {
@@ -481,7 +492,7 @@ double CSSPrimitiveValue::ConversionToCanonicalUnitsScaleFactor(
 
 Length CSSPrimitiveValue::ConvertToLength(
     const CSSLengthResolver& length_resolver) const {
-  if (IsLength()) {
+  if (IsResolvableLength()) {
     return ComputeLength<Length>(length_resolver);
   }
   if (IsPercentage()) {
