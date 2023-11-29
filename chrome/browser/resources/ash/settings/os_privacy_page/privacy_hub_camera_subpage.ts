565a9607da0825ba61ba0f30d1a2eefe3795161d
Md Shahadat Hossain Shahin <shahinmd@google.com>
2023-11-29 11:35:08 +0000

    privacy-hub: Fix camera access status row click issue
    Do not fire the click handler of #accessStatusRow in the privacy hub
    camera subpage when the toggle is disabled.
    
    Bug: b:313068615
    Change-Id: Iea65d73d8f65c63b588d44379ba746d8f667a639
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5056845
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Md Shahadat Hossain Shahin <shahinmd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1230560}

--- chrome/browser/resources/ash/settings/os_privacy_page/privacy_hub_camera_subpage.ts
+++ chrome/browser/resources/ash/settings/os_privacy_page/privacy_hub_camera_subpage.ts
@@ -208,6 +208,10 @@ export class SettingsPrivacyHubCameraSubpage extends
   }
 
   private onAccessStatusRowClick_(): void {
+    if (this.shouldDisableCameraToggle_) {
+      return;
+    }
+
     this.getCameraToggle_().click();
   }
 
