30b7ee60e941448f75d788cdce37af720706055a
Peter Williamson <petewil@chromium.org>
2023-11-29 05:38:32 +0000

    Revert "Build a flag to enable the cco feature."
    This reverts commit 675a36e9d65ca0ad9e95555d43395da4c4189dcd.
    
    Reason for revert: landed before it was totally ready.
    
    Original change's description:
    > Build a flag to enable the cco feature.
    >
    > Bug: b:307956097
    > Change-Id: Id416991a967939baeaefbf5908a7f80f004eb051
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5068896
    > Commit-Queue: Peter Williamson <petewil@chromium.org>
    > Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1230442}
    
    Bug: b:307956097
    Change-Id: Id550f65ebd188ec095fd4cdcccea6d8109ab43aa
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5067068
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Auto-Submit: Peter Williamson <petewil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230455}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/buildflag.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/common/buildflags.h"
-#include "components/compose/buildflags.h"
 #include "components/nacl/common/buildflags.h"
 #include "components/paint_preview/buildflags/buildflags.h"
 #include "components/signin/public/base/signin_buildflags.h"
@@ -4532,11 +4531,6 @@ extern const char kPromiseIconsDescription[];
 extern const char kEnableAudioFocusEnforcementName[];
 extern const char kEnableAudioFocusEnforcementDescription[];
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
-
-#if BUILDFLAG(ENABLE_COMPOSE)
-extern const char kComposeName[];
-extern const char kComposeDescription[];
-#endif  // BUILDFLAG(ENABLE_COMPOSE)
 
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
 extern const char kThirdPartyProfileManagementName[];
