8601e61319ec06cdf31584abd45e8006c5a89d0e
Alex N. Jose <alexnj@chromium.org>
2023-11-29 15:28:42 +0000

    [LCPP] Deflake DelayAsyncWhenLcpInHtml web_tests.
    Bug: 1505567
    Change-Id: I72ee827ac358386ed8edd9ba8da126b5421d23bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5067584
    Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230652}

--- third_party/blink/web_tests/http/tests/delay-async-when-lcp-in-html/delay-async-when-lcp-in-html.php
+++ third_party/blink/web_tests/http/tests/delay-async-when-lcp-in-html/delay-async-when-lcp-in-html.php
@@ -67,13 +67,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       const entries = list.getEntries();
       for (const entry of entries) {
         assert_equals(entry.entryType, 'largest-contentful-paint');
-        logScript('lcp:' + entry.id)
+        logScript('lcp')
       }
     });
     observer.observe({type: 'largest-contentful-paint', buffered: true});
 
     function finish() {
-      assert_array_equals(window.result, ['lcp:lcp_image_b', 'async.js', 'async.js']);
+      const firstLcpIndex = window.result.indexOf('lcp');
+      assert_true(firstLcpIndex >= 0);
+      for (let i = 0; i < window.result.length; i++) {
+        if (window.result[i] == 'async.js') {
+          assert_true(i > firstLcpIndex);
+        }
+      }
       done();
     }
     window.addEventListener('load', finish);
