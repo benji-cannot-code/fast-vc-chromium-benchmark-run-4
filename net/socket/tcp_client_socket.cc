ccff5409762fe5826e8b80a06e4661940d98c42b
Richard Chen <richche@chromium.org>
2023-11-29 03:03:40 +0000

    [CodeHealth] Remove expired Net.TcpRtt.AtDisconnect histogram
    Part of the Chrome code health rotation, this CL removes
    the expired Net.TcpRtt.AtDisconnect histogram and
    cleans up its usages.
    
    OBSOLETE_HISTOGRAM[Net.TcpRtt.AtDisconnect]=expired.
    
    Fixed: 1499451
    Change-Id: Ia2954e0a1efda29d0ee31c86a219bebd31fd3d98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5063732
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Commit-Queue: Richard Chen <richche@google.com>
    Cr-Commit-Position: refs/heads/main@{#1230409}

--- net/socket/tcp_client_socket.cc
+++ net/socket/tcp_client_socket.cc
@@ -348,7 +348,6 @@ void TCPClientSocket::DoDisconnect() {
   }
 
   total_received_bytes_ = 0;
-  EmitTCPMetricsHistogramsOnDisconnect();
 
   // If connecting or already connected, record that the socket has been
   // disconnected.
@@ -563,14 +562,6 @@ int TCPClientSocket::OpenSocket(AddressFamily family) {
   socket_->SetDefaultOptionsForClient();
 
   return OK;
-}
-
-void TCPClientSocket::EmitTCPMetricsHistogramsOnDisconnect() {
-  base::TimeDelta rtt;
-  if (socket_->GetEstimatedRoundTripTime(&rtt)) {
-    UMA_HISTOGRAM_CUSTOM_TIMES("Net.TcpRtt.AtDisconnect", rtt,
-                               base::Milliseconds(1), base::Minutes(10), 100);
-  }
 }
 
 void TCPClientSocket::EmitConnectAttemptHistograms(int result) {
