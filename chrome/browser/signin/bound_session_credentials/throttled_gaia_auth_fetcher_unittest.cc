d8a0f5f51540888efae5edfa2e36f694aaf577ac
Alex Ilin <alexilin@chromium.org>
2023-11-29 11:06:33 +0000

    [DBSC] Add throttling for /OAuthMultiLogin requests
    Adding this more as a precaution for the case when the short-term cookie
    expires in between /ListAccounts and /OAuthMultiLogin requests, which is
    quite improbable.
    
    Bug: b/311595637
    Change-Id: I2642cf21c3ed957dbb5e329033e00565544abc43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5061653
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230554}

--- chrome/browser/signin/bound_session_credentials/throttled_gaia_auth_fetcher_unittest.cc
+++ chrome/browser/signin/bound_session_credentials/throttled_gaia_auth_fetcher_unittest.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/signin_switches.h"
 #include "google_apis/gaia/gaia_auth_consumer.h"
 #include "google_apis/gaia/gaia_auth_fetcher.h"
-#include "google_apis/gaia/gaia_urls.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
 #include "services/network/test/test_url_loader_factory.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -41,6 +40,10 @@ class MockGaiaAuthConsumer : public GaiaAuthConsumer {
               (const GoogleServiceAuthError& error),
               (override));
   MOCK_METHOD(void, OnLogOutSuccess, (), (override));
+  MOCK_METHOD(void,
+              OnOAuthMultiloginFinished,
+              (const OAuthMultiloginResult&),
+              (override));
 };
 
 class MockBoundSessionRequestThrottledHandler
@@ -62,16 +65,6 @@ chrome::mojom::BoundSessionThrottlerParamsPtr CreateNonBlockingParams() {
       "example.org", "/", base::Time::Now() - base::Seconds(10));
 }
 
-GURL ListAccountsUrl() {
-  return GaiaUrls::GetInstance()->ListAccountsURLWithSource(
-      gaia::GaiaSource(kGaiaSourceType).ToString());
-}
-
-GURL LogOutUrl() {
-  return GaiaUrls::GetInstance()->LogOutURLWithSource(
-      gaia::GaiaSource(kGaiaSourceType).ToString());
-}
-
 }  // namespace
 
 class ThrottledGaiaAuthFetcherTest : public testing::Test {
@@ -86,14 +79,19 @@ class ThrottledGaiaAuthFetcherTest : public testing::Test {
         std::move(request_throttled_handler));
   }
 
-  bool HasRequest(const GURL& url) {
-    return test_url_loader_factory_.IsPending(url.spec());
-  }
-
-  void CompleteRequest(const GURL& url) {
-    EXPECT_TRUE(HasRequest(url));
-    test_url_loader_factory_.SimulateResponseForPendingRequest(url.spec(),
-                                                               /*content=*/"");
+  void CompleteRequest() {
+    if (size_t request_count =
+            test_url_loader_factory_.pending_requests()->size();
+        request_count != 1) {
+      ADD_FAILURE() << "Expected exactly one pending requests but found "
+                    << request_count;
+      return;
+    }
+    GURL request_url =
+        test_url_loader_factory_.GetPendingRequest(0)->request.url;
+    test_url_loader_factory_.SimulateResponseForPendingRequest(
+        request_url.spec(),
+        /*content=*/"");
   }
 
   StrictMock<MockGaiaAuthConsumer>& consumer() { return mock_consumer_; }
@@ -126,7 +124,7 @@ TEST_F(ThrottledGaiaAuthFetcherTest, ThrottleListAccounts) {
   ASSERT_TRUE(unblock_callback);
   std::move(unblock_callback).Run(UnblockAction::kResume);
   EXPECT_CALL(consumer(), OnListAccountsSuccess(_));
-  CompleteRequest(ListAccountsUrl());
+  CompleteRequest();
 }
 
 TEST_F(ThrottledGaiaAuthFetcherTest, ThrottleListAccountsCancel) {
@@ -148,7 +146,7 @@ TEST_F(ThrottledGaiaAuthFetcherTest, ListAccountsNotThrottledNoBoundSessions) {
 
   fetcher()->StartListAccounts();
   EXPECT_CALL(consumer(), OnListAccountsSuccess(_));
-  CompleteRequest(ListAccountsUrl());
+  CompleteRequest();
 }
 
 TEST_F(ThrottledGaiaAuthFetcherTest,
@@ -159,7 +157,22 @@ TEST_F(ThrottledGaiaAuthFetcherTest,
 
   fetcher()->StartListAccounts();
   EXPECT_CALL(consumer(), OnListAccountsSuccess(_));
-  CompleteRequest(ListAccountsUrl());
+  CompleteRequest();
+}
+
+TEST_F(ThrottledGaiaAuthFetcherTest, ThrottleMultilogin) {
+  CreateFetcher(CreateBlockingParams());
+  UnblockRequestCallback unblock_callback;
+  EXPECT_CALL(*request_throttled_handler(), HandleRequestBlockedOnCookie(_))
+      .WillOnce(MoveArg<0>(&unblock_callback));
+
+  fetcher()->StartOAuthMultilogin(
+      gaia::MultiloginMode::MULTILOGIN_PRESERVE_COOKIE_ACCOUNTS_ORDER,
+      {{"token1", "id1"}, {"token2", "id2"}}, "cc_result");
+  ASSERT_TRUE(unblock_callback);
+  std::move(unblock_callback).Run(UnblockAction::kResume);
+  EXPECT_CALL(consumer(), OnOAuthMultiloginFinished(_));
+  CompleteRequest();
 }
 
 TEST_F(ThrottledGaiaAuthFetcherTest, OtherRequestNotThrottled) {
@@ -169,5 +182,5 @@ TEST_F(ThrottledGaiaAuthFetcherTest, OtherRequestNotThrottled) {
 
   fetcher()->StartLogOut();
   EXPECT_CALL(consumer(), OnLogOutSuccess());
-  CompleteRequest(LogOutUrl());
+  CompleteRequest();
 }
