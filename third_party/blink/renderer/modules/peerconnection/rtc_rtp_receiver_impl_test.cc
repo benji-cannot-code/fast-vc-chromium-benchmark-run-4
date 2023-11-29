c34fb520d74300b629ec9ccf528777f8f0bb17e7
Tony Herre <toprice@chromium.org>
2023-11-29 14:06:02 +0000

    Allow createEncodedStreams on PCs without encodedInsertableStreams param
    Allow creating Encoded Transforms for any Receivers and Senders, so
    long as createEncodedStreams() is called by JS synchronously after
    sender/receiver creation. This is achieved by setting up a WebRTC
    transform on all transceivers, but "short circuiting" it if JS hasn't
    set up its own transform within an event loop spin of creation. This
    will make the transform just pass frames to be transformed directly back
    without invoking the cost of a thread hop or any JS work.
    
    The existing behaviour (dropping all frames until a JS transform is
    installed) is preserved for PCs created with {encodedInsertableStreams:
    true}.
    
    This implements the algorithm defined in https://www.w3.org/TR/2023/WD-webrtc-encoded-transform-20231012/#stream-creation,
    but for the Chromium createEncodedStreams() method, improving
    conformance incrementally.
    
    
    Bug: 1502781
    Change-Id: Ie36d8ed8f431afa97307030646d3b207bf14cf7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5040731
    Commit-Queue: Tony Herre <toprice@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230611}

--- third_party/blink/renderer/modules/peerconnection/rtc_rtp_receiver_impl_test.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_rtp_receiver_impl_test.cc
@@ -13,12 +13,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/test/scoped_feature_list.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/platform/scheduler/test/renderer_scheduler_test_support.h"
 #include "third_party/blink/public/platform/web_string.h"
 #include "third_party/blink/public/web/web_heap.h"
 #include "third_party/blink/renderer/modules/peerconnection/mock_peer_connection_dependency_factory.h"
 #include "third_party/blink/renderer/modules/peerconnection/mock_peer_connection_impl.h"
+#include "third_party/blink/renderer/modules/peerconnection/peer_connection_features.h"
 #include "third_party/blink/renderer/modules/peerconnection/test_webrtc_stats_report_obtainer.h"
 #include "third_party/blink/renderer/modules/peerconnection/testing/mock_rtp_receiver.h"
 #include "third_party/blink/renderer/modules/peerconnection/webrtc_media_stream_track_adapter_map.h"
@@ -27,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/testing/io_task_runner_testing_platform_support.h"
 #include "third_party/webrtc/api/stats/rtc_stats_report.h"
 #include "third_party/webrtc/api/stats/rtcstats_objects.h"
+
+using base::test::ScopedFeatureList;
 
 namespace blink {
 
@@ -63,7 +67,7 @@ class RTCRtpReceiverImplTest : public ::testing::Test {
 
   std::unique_ptr<RTCRtpReceiverImpl> CreateReceiver(
       scoped_refptr<webrtc::MediaStreamTrackInterface> webrtc_track,
-      bool encoded_insertable_streams = false) {
+      bool require_encoded_insertable_streams = false) {
     std::unique_ptr<blink::WebRtcMediaStreamTrackAdapterMap::AdapterRef>
         track_ref;
     base::RunLoop run_loop;
@@ -81,7 +85,7 @@ class RTCRtpReceiverImplTest : public ::testing::Test {
         mock_webrtc_receiver_.get(), std::move(track_ref), {});
     state.Initialize();
     return std::make_unique<RTCRtpReceiverImpl>(
-        peer_connection_, std::move(state), encoded_insertable_streams);
+        peer_connection_, std::move(state), require_encoded_insertable_streams);
   }
 
   scoped_refptr<blink::TestWebRTCStatsReportObtainer> GetStats() {
@@ -113,6 +117,9 @@ class RTCRtpReceiverImplTest : public ::testing::Test {
 };
 
 TEST_F(RTCRtpReceiverImplTest, CreateReceiver) {
+  ScopedFeatureList feature_list;
+  feature_list.InitAndEnableFeature(kWebRtcEncodedTransformsPerStreamCreation);
+
   scoped_refptr<blink::MockWebRtcAudioTrack> webrtc_track =
       blink::MockWebRtcAudioTrack::Create("webrtc_track");
   receiver_ = CreateReceiver(webrtc_track);
@@ -120,7 +127,8 @@ TEST_F(RTCRtpReceiverImplTest, CreateReceiver) {
   EXPECT_EQ(receiver_->Track()->Id().Utf8(), webrtc_track->id());
   EXPECT_EQ(receiver_->state().track_ref()->webrtc_track().get(),
             webrtc_track.get());
-  EXPECT_FALSE(receiver_->GetEncodedAudioStreamTransformer());
+
+  EXPECT_TRUE(receiver_->GetEncodedAudioStreamTransformer());
   EXPECT_FALSE(receiver_->GetEncodedVideoStreamTransformer());
 }
 
@@ -168,12 +176,39 @@ TEST_F(RTCRtpReceiverImplTest, GetStats) {
 }
 
 TEST_F(RTCRtpReceiverImplTest, CreateReceiverWithInsertableStreams) {
+  ScopedFeatureList feature_list;
+  feature_list.InitAndEnableFeature(kWebRtcEncodedTransformsPerStreamCreation);
+
   scoped_refptr<blink::MockWebRtcAudioTrack> webrtc_track =
       blink::MockWebRtcAudioTrack::Create("webrtc_track");
-  receiver_ = CreateReceiver(webrtc_track,
-                             /*encoded_insertable_streams=*/true);
+  receiver_ = CreateReceiver(webrtc_track, true);
   EXPECT_TRUE(receiver_->GetEncodedAudioStreamTransformer());
   // There should be no video transformer in audio receivers.
+  EXPECT_FALSE(receiver_->GetEncodedVideoStreamTransformer());
+}
+
+TEST_F(RTCRtpReceiverImplTest,
+       CreateReceiverWithInsertableStreamsWithoutFeature) {
+  ScopedFeatureList feature_list;
+  feature_list.InitAndDisableFeature(kWebRtcEncodedTransformsPerStreamCreation);
+
+  scoped_refptr<blink::MockWebRtcAudioTrack> webrtc_track =
+      blink::MockWebRtcAudioTrack::Create("webrtc_track");
+  receiver_ = CreateReceiver(webrtc_track, true);
+  // Audio transformer should still be created.
+  EXPECT_TRUE(receiver_->GetEncodedAudioStreamTransformer());
+}
+
+TEST_F(RTCRtpReceiverImplTest,
+       CreateReceiverWithOutInsertableStreamsParamWithoutFeature) {
+  ScopedFeatureList feature_list;
+  feature_list.InitAndDisableFeature(kWebRtcEncodedTransformsPerStreamCreation);
+
+  scoped_refptr<blink::MockWebRtcAudioTrack> webrtc_track =
+      blink::MockWebRtcAudioTrack::Create("webrtc_track");
+  receiver_ = CreateReceiver(webrtc_track, false);
+  // No Transformers should be created.
+  EXPECT_FALSE(receiver_->GetEncodedAudioStreamTransformer());
   EXPECT_FALSE(receiver_->GetEncodedVideoStreamTransformer());
 }
 
