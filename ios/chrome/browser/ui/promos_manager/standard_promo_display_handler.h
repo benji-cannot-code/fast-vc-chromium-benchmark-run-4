d7c9ef898f51ada2fd07dd43ffc7e5fadabfcf59
Robbie Gibson <rkgibson@google.com>
2023-11-29 19:37:22 +0000

    [iOS] Add extra comment to promos manager display handler
    This is a new code behavior requirement.
    
    Bug: none
    Change-Id: I0d945ffc817045d9a7b619419f5aa2949bed3de9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5066977
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1230830}

--- ios/chrome/browser/ui/promos_manager/standard_promo_display_handler.h
+++ ios/chrome/browser/ui/promos_manager/standard_promo_display_handler.h
@@ -17,6 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // When the Promos Manager determines it's time to display the promo
 // `identifier`, it will call `handleDisplay`.
+// If this method is used to display promos, the promo must alert the Promos
+// Manager when it is dismissed by calling the `PromosManagerCoordinator`'s
+// `promoWasDismissed` method. the `PromosManagerUIHandler` protocol can be used
+// for this purpose.
 @protocol StandardPromoDisplayHandler <PromoProtocol>
 
 @required
