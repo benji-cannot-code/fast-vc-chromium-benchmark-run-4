e06a993c031fd8467b7ff8e73d0cd5e2c1b8c7cd
Jason Thai <jasontt@chromium.org>
2023-11-29 01:48:36 +0000

    p13n: fix incorrect template option value
    Address bug pointed out in http://shortn/_l6QXTozaiL where the value is
    used instead of the translation.
    
    BUG=b:311020151
    TEST=browser_tests --gtest_filter="*SeaPenQuery*"
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: Idc709e53c2441e59a33f0403fb35839caf5a975a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5069394
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Reviewed-by: Jerry Liu <pzliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1230383}

--- ash/webui/personalization_app/resources/js/wallpaper/utils.ts
+++ ash/webui/personalization_app/resources/js/wallpaper/utils.ts
@@ -258,11 +258,11 @@ export function getSampleSeaPenTemplates(): SeaPenTemplate[] {
           '<city>',
           [
             {
-              value: 'Paris',
+              value: 'Test - Paris',
               translation: loadTimeData.getString('templateOptionParis'),
             },
             {
-              value: 'New York',
+              value: 'Test - New York',
               translation: loadTimeData.getString('templateOptionNewYork'),
             },
           ],
@@ -271,11 +271,11 @@ export function getSampleSeaPenTemplates(): SeaPenTemplate[] {
           '<style>',
           [
             {
-              value: 'photography',
+              value: 'Test - photography',
               translation: loadTimeData.getString('templateOptionPhotography'),
             },
             {
-              value: 'watercolor',
+              value: 'Test - watercolor',
               translation: loadTimeData.getString('templateOptionWatercolor'),
             },
           ],
