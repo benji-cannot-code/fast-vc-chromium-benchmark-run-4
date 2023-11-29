fea564b0dbb48b526d6c71ccf4f5f9fa209e5c3a
Bohdan Tyshchenko <bohdanty@google.com>
2023-11-29 02:06:15 +0000

    [OOBE][CodeHealth] - Refactor Signin Fatal Error Screen enum
    Change enums inside the Signin Fatal Error Screen to match C++
    styleguide.
    
    Bug: b:279732127
    Change-Id: I60f139d3f36b54a99d736cb158218b563791ec1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5063152
    Commit-Queue: Bohdan Tyshchenko <bohdanty@google.com>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1230389}

--- chrome/browser/ash/login/wizard_controller.cc
+++ chrome/browser/ash/login/wizard_controller.cc
@@ -1467,7 +1467,7 @@ void WizardController::OnSamlConfirmPasswordScreenExit(
       return;
     case SamlConfirmPasswordScreen::Result::kTooManyAttempts:
       ShowSignInFatalErrorScreen(
-          SignInFatalErrorScreen::Error::SCRAPED_PASSWORD_VERIFICATION_FAILURE,
+          SignInFatalErrorScreen::Error::kScrapedPasswordVerificationFailure,
           base::Value::Dict());
   }
 }
