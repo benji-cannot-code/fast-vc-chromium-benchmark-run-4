6efdcde480cfb9853270446450d2e027b0289da1
Pete Williamson <petewil@chromium.org>
2023-11-29 04:00:50 +0000

    Add a chrome flag for AutofillContentEditables
    The compose feature needs to use a chrome flag to turn on the dependent
    AutofillContentEditables feature persistently.  This change adds
    a chrome flag to the existing feature flag for autofill content editables.
    
    Bug: b:307956097
    Change-Id: I2748c2c636f24f8ebeafc9094e5342a22a7d8435
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5067582
    Auto-Submit: Peter Williamson <petewil@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Peter Williamson <petewil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230426}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1892,6 +1892,9 @@ extern const char kSystemKeyboardLockDescription[];
 extern const char kSystemSoundsName[];
 extern const char kSystemSoundsDescription[];
 
+extern const char kAutofillContentEditablesName[];
+extern const char kAutofillContentEditablesDescription[];
+
 extern const char kSyncAutofillWalletCredentialDataName[];
 extern const char kSyncAutofillWalletCredentialDataDescription[];
 
