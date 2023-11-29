7dcd78e9ba6f5fc268e95129f13d340fad91eaff
Emily Shack <emshack@chromium.org>
2023-11-29 20:30:36 +0000

    Tab Organization: Improve results state focus
    Per greenlines, makes the tab organization results state a focusable
    group where tab will only hit the first (or most recently focused) list
    item, and then continue on past the list. Arrow keys are still available
    for traversing the list.
    
    Also makes the Learn More button focusable and improves its focused
    state.
    
    Bug: 1469126
    Change-Id: I82df96eb6cd9dbe27af9d328dad5c306f5c5e745
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5063485
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230873}

--- chrome/browser/resources/tab_search/tab_organization_results.ts
+++ chrome/browser/resources/tab_search/tab_organization_results.ts
@@ -39,12 +39,21 @@ export class TabOrganizationResultsElement extends PolymerElement {
 
   static get properties() {
     return {
-      tabs: Array,
+      tabs: {
+        type: Array,
+        observer: 'onTabsChange_',
+      },
+
       name: String,
 
       availableHeight: {
         type: Number,
         observer: 'onAvailableHeightChange_',
+      },
+
+      lastFocusedIndex_: {
+        type: Number,
+        value: 0,
       },
 
       tabDatas_: {
@@ -59,6 +68,7 @@ export class TabOrganizationResultsElement extends PolymerElement {
   name: string;
   availableHeight: number;
 
+  private lastFocusedIndex_: number;
   private tabDatas_: TabData[];
 
   static get template() {
@@ -69,6 +79,14 @@ export class TabOrganizationResultsElement extends PolymerElement {
     return this.tabs.map(
         tab => new TabData(
             tab, TabItemType.OPEN_TAB, new URL(tab.url.url).hostname));
+  }
+
+  private onTabsChange_() {
+    this.lastFocusedIndex_ = 0;
+  }
+
+  private getTabIndex_(index: number): number {
+    return index === this.lastFocusedIndex_ ? 0 : -1;
   }
 
   private onAvailableHeightChange_() {
@@ -116,6 +134,7 @@ export class TabOrganizationResultsElement extends PolymerElement {
       const selectedItemCloseButton =
           selectedItem.shadowRoot!.querySelector(`cr-icon-button`)!;
       selectedItemCloseButton.focus();
+      this.lastFocusedIndex_ = this.$.selector.indexOf(selectedItem);
     }
   }
 
