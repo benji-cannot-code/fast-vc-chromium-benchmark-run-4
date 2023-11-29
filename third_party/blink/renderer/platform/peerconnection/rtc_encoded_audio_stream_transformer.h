8a5f151ec247b71957af133ba29498d32a409087
David Baron <dbaron@chromium.org>
2023-11-29 15:40:09 +0000

    Revert "Allow createEncodedStreams on PCs without encodedInsertableStreams param"
    This reverts commit e5518aa87b722a83596d1fb2a7506d0ab76f1207.
    
    Reason for revert: reliable failures of test fast/peerconnection/RTCPeerconnection-insertable-streams.html on https://ci.chromium.org/p/chromium/builders/ci/mac13-arm64-rel-tests (looks like the test is crashing)
    
    Original change's description:
    > Allow createEncodedStreams on PCs without encodedInsertableStreams param
    >
    > Allow creating Encoded Transforms for any Receivers and Senders, so
    > long as createEncodedStreams() is called by JS synchronously after
    > sender/receiver creation. This is achieved by setting up a WebRTC
    > transform on all transceivers, but "short circuiting" it if JS hasn't
    > set up its own transform within an event loop spin of creation. This
    > will make the transform just pass frames to be transformed directly back
    > without invoking the cost of a thread hop or any JS work.
    >
    > The existing behaviour (dropping all frames until a JS transform is
    > installed) is preserved for PCs created with {encodedInsertableStreams:
    > true}.
    >
    > This implements the algorithm defined in https://www.w3.org/TR/2023/WD-webrtc-encoded-transform-20231012/#stream-creation,
    > but for the Chromium createEncodedStreams() method, improving
    > conformance incrementally.
    >
    >
    > Bug: 1502781
    > Change-Id: Ie36d8ed8f431afa97307030646d3b207bf14cf7a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5040731
    > Commit-Queue: Tony Herre <toprice@chromium.org>
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Reviewed-by: Harald Alvestrand <hta@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1230611}
    
    Bug: 1502781
    Change-Id: I10193d5f5b4954536169fa9455b03042966e14e2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5072012
    Auto-Submit: David Baron <dbaron@chromium.org>
    Owners-Override: David Baron <dbaron@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1230655}

--- third_party/blink/renderer/platform/peerconnection/rtc_encoded_audio_stream_transformer.h
+++ third_party/blink/renderer/platform/peerconnection/rtc_encoded_audio_stream_transformer.h
@@ -63,8 +63,6 @@ class PLATFORM_EXPORT RTCEncodedAudioStreamTransformer {
     void SendFrameToSink(
         std::unique_ptr<webrtc::TransformableAudioFrameInterface> frame);
 
-    void StartShortCircuiting();
-
    private:
     explicit Broker(RTCEncodedAudioStreamTransformer* transformer_);
     void ClearTransformer();
@@ -124,8 +122,6 @@ class PLATFORM_EXPORT RTCEncodedAudioStreamTransformer {
 
   scoped_refptr<Broker> GetBroker();
 
-  void StartShortCircuiting();
-
  private:
   const scoped_refptr<Broker> broker_;
   const rtc::scoped_refptr<webrtc::FrameTransformerInterface> delegate_;
@@ -134,7 +130,6 @@ class PLATFORM_EXPORT RTCEncodedAudioStreamTransformer {
       GUARDED_BY(sink_lock_);
   base::Lock source_lock_;
   TransformerCallback transformer_callback_ GUARDED_BY(source_lock_);
-  bool short_circuit_ GUARDED_BY(sink_lock_) = false;
 };
 
 }  // namespace blink
