aa0b147a9acf0f12ec7137a0bda1b94060d078b8
Rebekah Potter <rbpotter@chromium.org>
2023-11-29 17:46:21 +0000

    [CodeHealth] Remove leaked ptr in file_system_chooser_test_helpers.cc
    Release the pointer to the dialog parameters:
    (1) After filling them in, so that once the test checks and destroys
        them there is no dangling reference.
    (2) At destruction for both test factories and test file select
        dialogs
    
    Bug: 1462074
    Change-Id: I39bdf6e9132435f58ee341ef0a964804aef2c8a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5032846
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Daseul Lee <dslee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230738}

--- content/public/test/file_system_chooser_test_helpers.cc
+++ content/public/test/file_system_chooser_test_helpers.cc
@@ -47,6 +47,10 @@ class CancellingSelectFileDialog : public ui::SelectFileDialog {
       } else {
         out_params_->caller = absl::nullopt;
       }
+
+      // Free the pointer since output parameters should only be written to
+      // once.
+      out_params_ = nullptr;
     }
     listener_->FileSelectionCanceled(params);
   }
@@ -58,7 +62,11 @@ class CancellingSelectFileDialog : public ui::SelectFileDialog {
   bool HasMultipleFileTypeChoicesImpl() override { return false; }
 
  private:
-  ~CancellingSelectFileDialog() override { out_params_ = nullptr; }
+  ~CancellingSelectFileDialog() override {
+    if (out_params_) {
+      out_params_ = nullptr;
+    }
+  }
   raw_ptr<SelectFileDialogParams> out_params_;
 };
 
@@ -98,6 +106,9 @@ class FakeSelectFileDialog : public ui::SelectFileDialog {
       } else {
         out_params_->caller = absl::nullopt;
       }
+
+      // Clean up the output parameters; they should only be filled in once.
+      out_params_ = nullptr;
     }
     // The selected files are passed by reference to the listener. Ensure they
     // outlive the dialog if it is immediately deleted by the listener.
@@ -117,9 +128,13 @@ class FakeSelectFileDialog : public ui::SelectFileDialog {
   bool HasMultipleFileTypeChoicesImpl() override { return false; }
 
  private:
-  ~FakeSelectFileDialog() override = default;
+  ~FakeSelectFileDialog() override {
+    if (out_params_) {
+      out_params_ = nullptr;
+    }
+  }
   std::vector<ui::SelectedFileInfo> result_;
-  raw_ptr<SelectFileDialogParams, LeakedDanglingUntriaged> out_params_;
+  raw_ptr<SelectFileDialogParams> out_params_;
 };
 
 }  // namespace
@@ -154,7 +169,9 @@ FakeSelectFileDialogFactory::FakeSelectFileDialogFactory(
     SelectFileDialogParams* out_params)
     : result_(std::move(result)), out_params_(out_params) {}
 
-FakeSelectFileDialogFactory::~FakeSelectFileDialogFactory() = default;
+FakeSelectFileDialogFactory::~FakeSelectFileDialogFactory() {
+  out_params_ = nullptr;
+}
 
 ui::SelectFileDialog* FakeSelectFileDialogFactory::Create(
     ui::SelectFileDialog::Listener* listener,
