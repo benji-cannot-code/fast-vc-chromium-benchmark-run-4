79b8bdab19d20ec94e4d5132adc520269d3f80cb
Dominic Battre <battre@chromium.org>
2023-11-29 17:01:37 +0000

    Improve classification of address names and types
    We observed that many BR/MX address forms ask the user to assign a
    label to an address (e.g. "My home address") or a type of an address
    ("House", "Flat", ...). Due to our choice of regular expressions
    these fields were often misclassified.
    This CL expands the definition of the ADDRESS_NAME_IGNORED regex
    pattern to be less prone to false positives.
    
    Bug: 1505585
    Change-Id: I30c146d50dd3f00bbeeb552c7def7377687b19d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5063233
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230703}

--- components/autofill/core/browser/form_parsing/name_field.cc
+++ components/autofill/core/browser/form_parsing/name_field.cc
@@ -174,7 +174,9 @@ std::unique_ptr<FullNameField> FullNameField::Parse(
   bool should_ignore =
       ParseField(scanner, kNameIgnoredRe, name_ignored_patterns, nullptr,
                  {log_manager, "kNameIgnoredRe"}) ||
-      ParseField(scanner, kAddressNameIgnoredRe, address_name_ignored_patterns,
+      // This pattern fully migrated to the MatchPattern mechanism. There
+      // is no regular expression in autofill_regex_constants.h anymore.
+      ParseField(scanner, kNoLegacyPattern, address_name_ignored_patterns,
                  nullptr, {log_manager, "kAddressNameIgnoredRe"});
   scanner->Rewind();
   if (should_ignore)
@@ -241,9 +243,10 @@ FirstTwoLastNamesField::ParseComponentNames(AutofillScanner* scanner,
   while (!scanner->IsEnd()) {
     // Skip over address label fields, which can have misleading names
     // e.g. "title" or "name".
-    if (ParseField(scanner, kAddressNameIgnoredRe,
-                   address_name_ignored_patterns, nullptr,
-                   {log_manager, "kAddressNameIgnoredRe"})) {
+    // This pattern fully migrated to the MatchPattern mechanism. There is no
+    // regular expression in autofill_regex_constants.h anymore.
+    if (ParseField(scanner, kNoLegacyPattern, address_name_ignored_patterns,
+                   nullptr, {log_manager, "kAddressNameIgnoredRe"})) {
       continue;
     }
 
@@ -348,7 +351,9 @@ FirstLastNameField::ParseNameSurnameLabelSequence(
   bool should_ignore =
       ParseField(scanner, kNameIgnoredRe, name_ignored_patterns, nullptr,
                  {log_manager, "kNameIgnoredRe"}) ||
-      ParseField(scanner, kAddressNameIgnoredRe, address_name_ignored_patterns,
+      // This pattern fully migrated to the MatchPattern mechanism. There is no
+      // regular expression in autofill_regex_constants.h anymore.
+      ParseField(scanner, kNoLegacyPattern, address_name_ignored_patterns,
                  nullptr, {log_manager, "kAddressNameIgnoredRe"});
   scanner->Rewind();
 
@@ -446,9 +451,10 @@ FirstLastNameField::ParseSpecificComponentSequence(
   while (!scanner->IsEnd()) {
     // Skip over address label fields, which can have misleading names
     // e.g. "title" or "name".
-    if (ParseField(scanner, kAddressNameIgnoredRe,
-                   address_name_ignored_patterns, nullptr,
-                   {log_manager, "kAddressNameIgnoredRe"})) {
+    // This pattern fully migrated to the MatchPattern mechanism. There is no
+    // regular expression in autofill_regex_constants.h anymore.
+    if (ParseField(scanner, kNoLegacyPattern, address_name_ignored_patterns,
+                   nullptr, {log_manager, "kAddressNameIgnoredRe"})) {
       continue;
     }
 
