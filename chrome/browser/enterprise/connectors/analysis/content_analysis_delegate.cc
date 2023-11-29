3a077a6ce402044d451915304af7f2d2a8997596
Dominique Fauteux-Chapleau <domfc@chromium.org>
2023-11-29 16:55:11 +0000

    Revert "Add image scanning support to cloud content scanning"
    This reverts commit ae9536cf4ba5b7ed039bec483cb653bb363ebe6d.
    
    Reason for revert: unexpected side effects, see b/311679168
    
    Original change's description:
    > Add image scanning support to cloud content scanning
    >
    > This CL also adds the mime type of the clipboard content to the request.
    >
    > Bug: b/311679168
    > Change-Id: Ia84be934a9d92b0c19444d3e690573ee53945f49
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5049611
    > Reviewed-by: Roger Tawa <rogerta@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1228107}
    
    Bug: b/311679168
    Change-Id: Iaad48087e5ed9623755102414ca663de6d9231e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5071036
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230693}

--- chrome/browser/enterprise/connectors/analysis/content_analysis_delegate.cc
+++ chrome/browser/enterprise/connectors/analysis/content_analysis_delegate.cc
@@ -693,7 +693,6 @@ void ContentAnalysisDelegate::PrepareTextRequest() {
                        weak_ptr_factory_.GetWeakPtr()));
 
     PrepareRequest(BULK_DATA_ENTRY, request.get());
-    request->set_content_type(data_.clipboard_mime_type);
     UploadTextForDeepScanning(std::move(request));
   }
 }
@@ -721,7 +720,6 @@ void ContentAnalysisDelegate::PrepareImageRequest() {
                        weak_ptr_factory_.GetWeakPtr()));
 
     PrepareRequest(BULK_DATA_ENTRY, request.get());
-    request->set_content_type(data_.clipboard_mime_type);
     UploadImageForDeepScanning(std::move(request));
   }
 }
