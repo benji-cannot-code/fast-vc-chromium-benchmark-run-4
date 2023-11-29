2e5766246ed991d36a02c32c737d3c0cdbc16a10
Charles William Dick <cwd@google.com>
2023-11-29 05:35:44 +0000

    Chrome Battery Saver deprecate kEnabled policy
    In order to converge Chrome and ChromeOS battery saver policy, first
    deprecate the kEnabled item from the BatterySaverModeAvailability policy
    yaml.
    
    The SimplePolicyHandler is replaced with an IntRangePolicyHandler which
    clamps kEnabled policy values to kEnabledOnBattery.
    
    Bug: b:298161313
    Test: browser_tests, manually verifyclamping  with managedchrome user
    Change-Id: I707ed3fca86b073d28f87006e44bceaf2fcf5947
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5017844
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Charles Dick <cwd@google.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230454}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/memory/ptr_util.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/values.h"
 #include "build/branding_buildflags.h"
 #include "build/build_config.h"
@@ -1988,9 +1989,6 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
     base::Value::Type::STRING },
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS_ASH)
-  { key::kBatterySaverModeAvailability,
-    performance_manager::user_tuning::prefs::kBatterySaverModeState,
-    base::Value::Type::INTEGER },
   { key::kTabDiscardingExceptions,
     performance_manager::user_tuning::prefs::kManagedTabDiscardingExceptions,
     base::Value::Type::LIST },
@@ -2886,6 +2884,19 @@ std::unique_ptr<ConfigurationPolicyHandlerList> BuildHandlerList(
           key::kRelatedWebsiteSetsEnabled,
           prefs::kPrivacySandboxRelatedWebsiteSetsEnabled,
           base::Value::Type::BOOLEAN)));
+
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
+    BUILDFLAG(IS_CHROMEOS_ASH)
+  // Clamp the deprecated kEnabled policy value to kEnabledBelowThreshold.
+  handlers->AddHandler(std::make_unique<IntRangePolicyHandler>(
+      key::kBatterySaverModeAvailability,
+      performance_manager::user_tuning::prefs::kBatterySaverModeState,
+      base::strict_cast<int>(performance_manager::user_tuning::prefs::
+                                 BatterySaverModeState::kDisabled),
+      base::strict_cast<int>(performance_manager::user_tuning::prefs::
+                                 BatterySaverModeState::kEnabledBelowThreshold),
+      /*clamp=*/true));
+#endif
 
   return handlers;
 }
