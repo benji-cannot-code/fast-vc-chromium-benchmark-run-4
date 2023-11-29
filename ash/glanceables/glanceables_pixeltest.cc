3a486466a57af4c5e1e168b4f56ee7b4bff35ed8
Sylvie Liu <sylvieliu@chromium.org>
2023-11-29 17:26:01 +0000

    Adjust the focus ring of CalendarDateCellView for Glanceables
    This CL updates the focus ring of each date cell to resolve the issue
    that it's cropped with the update of Calendar for Glanceables UI. Now
    its radius should be 19px. Also fix today's button so that its floating.
    
    Screenshot:
    https://screenshot.googleplex.com/6gzcaEiGBLMvJRq
    
    Bug: b:312232795
    Change-Id: I735e3fb190c5d341a5e89deff919f10acf0d7ad3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5062525
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Sylvie Liu <sylvieliu@chromium.org>
    Reviewed-by: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230720}

--- ash/glanceables/glanceables_pixeltest.cc
+++ ash/glanceables/glanceables_pixeltest.cc
@@ -192,7 +192,7 @@ TEST_F(GlanceablesPixelTest, GlanceablesCalendarHeight) {
   ASSERT_TRUE(GetDateTray()->is_active());
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
-      "glanceables_calendar_height", /*revision_number=*/0,
+      "glanceables_calendar_height", /*revision_number=*/1,
       GetGlanceableTrayBubble()->GetBubbleView()));
 }
 
