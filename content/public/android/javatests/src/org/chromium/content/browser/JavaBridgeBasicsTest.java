6909a6a97c3e3bab57e75723b511f83a2322cd0e
Sakib Shabir Tantray <s1.tantray@samsung.com>
2023-11-29 04:47:35 +0000

    Refactor javatests  to use multiline strings
    Chromium Java code now supports multiline strings (text blocks).See
    https://groups.google.com/a/chromium.org/g/chromium-dev/c/ipjObhGvPxg
    
    Bug: 1498789
    
    Change-Id: I2ad38206075b083d4b7d697cd8c307889a80d30c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5047556
    Reviewed-by: Shubham Gupta <shubham.gupta@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Commit-Queue: Sakib Shabir <s1.tantray@samsung.com>
    Cr-Commit-Position: refs/heads/main@{#1230439}

--- content/public/android/javatests/src/org/chromium/content/browser/JavaBridgeBasicsTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/JavaBridgeBasicsTest.java
@@ -651,8 +651,12 @@ public class JavaBridgeBasicsTest {
         Assert.assertEquals(
                 "true",
                 executeJavaScriptAndGetStringResult(
-                        "(function() { delete globalInner; gc(); return (typeof globalInner =="
-                                + " 'undefined'); })()"));
+                        """
+                        (function() {
+                                delete globalInner;
+                                gc();
+                                return (typeof globalInner == 'undefined');
+                         })()"""));
         // Force GC on the Java side again. The bridge had to release the inner object, so it must
         // be collected this time.
         Runtime.getRuntime().gc();
@@ -719,9 +723,13 @@ public class JavaBridgeBasicsTest {
         Assert.assertEquals(
                 "true",
                 executeJavaScriptAndGetStringResult(
-                        "(function() { "
-                                + "delete inner1; delete inner2; gc(); "
-                                + "return (typeof inner1 == 'undefined'); })()"));
+                        """
+                        (function() {
+                                delete inner1;
+                                delete inner2;
+                                gc();
+                                return (typeof inner1 == 'undefined');
+                        })()"""));
         // Force GC on the Java side again. The bridge had to release the inner object, so it must
         // be collected this time.
         Runtime.getRuntime().gc();
