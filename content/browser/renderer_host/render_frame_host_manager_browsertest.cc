dbd79075924df945c3c4d451cdc242ac929157e3
Fergal Daly <fergal@chromium.org>
2023-11-29 06:05:19 +0000

    Enable unload for RenderFrameHostManagerUnloadBrowserTest.
    Tests in this class use unload. Many can just use pagehide but
    SubframeTerminationPing_SendBeacon and AddAndRemoveEventListenersAffectingSuddenTerminationDisablerState test all 3 of the unload-time events, so rather than just deleting the unload case, this makes it work.
    
    Bug: 1488371
    Change-Id: Ia90f3bcca4501e2671669fb56881a9d0bee64a90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5064131
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230460}

--- content/browser/renderer_host/render_frame_host_manager_browsertest.cc
+++ content/browser/renderer_host/render_frame_host_manager_browsertest.cc
@@ -90,6 +90,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/test/url_request/url_request_failed_job.h"
 #include "testing/gmock/include/gmock/gmock-matchers.h"
 #include "third_party/blink/public/common/chrome_debug_urls.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/web_preferences/web_preferences.h"
 
 using base::ASCIIToUTF16;
@@ -5529,6 +5530,11 @@ class RenderFrameHostManagerUnloadBrowserTest
     rfh->SetSubframeUnloadTimeoutForTesting(base::Seconds(30));
   }
 
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    RenderFrameHostManagerTest::SetUpCommandLine(command_line);
+    feature_list_.InitAndDisableFeature(blink::features::kDeprecateUnload);
+  }
+
  protected:
   void SetUpOnMainThread() override {
     // Request interceptor needs to be installed before the test server is
@@ -5566,6 +5572,7 @@ class RenderFrameHostManagerUnloadBrowserTest
   std::string request_content_ GUARDED_BY(lock_);
   bool saw_request_url_ GUARDED_BY(lock_) = false;
   std::unique_ptr<base::RunLoop> run_loop_ GUARDED_BY(lock_);
+  base::test::ScopedFeatureList feature_list_;
 };
 
 // Ensure that after a main frame with a cross-site iframe is itself navigated
