05eca88e0026ac786ed2f38cc7775b3f6b2e2183
Theodore Olsauskas-Warren <sauski@google.com>
2023-11-29 09:25:17 +0000

    Bubble Frame View: Allow Character Break
    CL exposes the ability to allow character breaks in the subtitle of
    the BubbleFrameView.
    
    This functionality is used by the CookiesControlsBubbleViewImpl to
    allow the correct wrapping of long origins.
    
    Bug: 1486733
    Change-Id: I94680fc9ec6f151e224a1a8eb2b2cd94f6bdd440
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4945791
    Reviewed-by: Elaine Chien <elainechien@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1230516}

--- chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_bubble_view_impl.cc
+++ chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_bubble_view_impl.cc
@@ -34,6 +34,7 @@ CookieControlsBubbleViewImpl::CookieControlsBubbleViewImpl(
   SetShowCloseButton(true);
   SetButtons(ui::DIALOG_BUTTON_NONE);
   SetProperty(views::kElementIdentifierKey, kCookieControlsBubble);
+  SetSubtitleAllowCharacterBreak(true);
 }
 
 CookieControlsBubbleViewImpl::~CookieControlsBubbleViewImpl() = default;
