b6d299aba3411271897e2719f009588244105adf
Sylvain Defresne <sdefresne@chromium.org>
2023-11-29 10:15:29 +0000

    [ios] Limit visibility of targets in //ios/chrome/browser/sessions
    Only expose the targets that corresponds to the new API and helper.
    The other targets are internal only and should not be used from the
    rest of the code.
    
    This will limit the risk of new usage appearing in the code.
    
    Bug: 1383087
    Change-Id: Ica40c934cc3b63fdd02b35c4f28777aa3e6313a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5053939
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230532}

--- ios/chrome/browser/shared/coordinator/scene/scene_controller.mm
+++ ios/chrome/browser/shared/coordinator/scene/scene_controller.mm
@@ -72,7 +72,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/reading_list/model/reading_list_browser_agent.h"
 #import "ios/chrome/browser/screenshot/model/screenshot_delegate.h"
 #import "ios/chrome/browser/sessions/session_saving_scene_agent.h"
-#import "ios/chrome/browser/sessions/session_service_ios.h"
 #import "ios/chrome/browser/shared/coordinator/default_browser_promo/non_modal_default_browser_promo_scheduler_scene_agent.h"
 #import "ios/chrome/browser/shared/coordinator/layout_guide/layout_guide_scene_agent.h"
 #import "ios/chrome/browser/shared/coordinator/scene/scene_ui_provider.h"
