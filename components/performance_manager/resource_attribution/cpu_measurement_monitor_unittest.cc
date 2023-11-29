3d092123e1a7028af0c751f87e79db367632e041
Joe Mason <joenotcharles@google.com>
2023-11-29 17:36:17 +0000

    Implement MemoryMeasurementProvider
    Adds a MemoryMeasurementDelegate, with an interface modelled on
    CPUMeasurementDelegate and a default implementation using
    MemoryInstrumentation (based on ProcessMetricsDecorator.)
    
    MemoryMeasurementProvider uses the delegate to get memory summaries
    (PMF and RSS) from processes, and attributes memory to Frame, Worker
    and Page ResourceContexts associated with each process.
    
    A followup will call MemoryMeasurementProvider from QueryScheduler to
    implement memory summary queries with the ResourceAttribution API.
    
    Bug: 1471683
    Change-Id: I7cd0282e396ae53f1e4be4a3af55e3f6d67b7ebc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5063455
    Reviewed-by: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1230728}

--- components/performance_manager/resource_attribution/cpu_measurement_monitor_unittest.cc
+++ components/performance_manager/resource_attribution/cpu_measurement_monitor_unittest.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <memory>
-#include <ostream>
 #include <utility>
 #include <vector>
 
@@ -37,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/performance_manager/test_support/graph_test_harness.h"
 #include "components/performance_manager/test_support/mock_graphs.h"
 #include "components/performance_manager/test_support/performance_manager_test_harness.h"
+#include "components/performance_manager/test_support/resource_attribution/gtest_util.h"
 #include "components/performance_manager/test_support/resource_attribution/simulated_cpu_measurement_delegate.h"
 #include "components/performance_manager/test_support/run_in_graph.h"
 #include "content/public/browser/web_contents.h"
@@ -45,30 +45,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/navigation_simulator.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
-#include "third_party/abseil-cpp/absl/types/variant.h"
 #include "url/gurl.h"
 
 namespace performance_manager::resource_attribution {
-
-// Test result printers. These need to go in the same namespace as the type
-// being printed.
-
-std::ostream& operator<<(std::ostream& os, const ResultMetadata& metadata) {
-  return os << "measurement_time:" << metadata.measurement_time;
-}
-
-std::ostream& operator<<(std::ostream& os, const CPUTimeResult& result) {
-  return os << "cpu:" << result.cumulative_cpu
-            << ",start_time:" << result.start_time
-            << ",metadata:" << result.metadata << " ("
-            << (result.metadata.measurement_time - result.start_time) << ")";
-}
-
-std::ostream& operator<<(std::ostream& os, const ResourceContext& context) {
-  absl::visit([&os](const auto& token) { os << token.ToString(); }, context);
-  return os;
-}
 
 namespace {
 
@@ -77,7 +56,6 @@ using ::testing::Conditional;
 using ::testing::Field;
 using ::testing::IsEmpty;
 using ::testing::Not;
-using ::testing::Optional;
 
 constexpr base::TimeDelta kTimeBetweenMeasurements = base::Minutes(5);
 
