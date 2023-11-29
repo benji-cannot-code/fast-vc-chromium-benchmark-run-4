cb45d6be16a47a1bfdf92fc82d1cda12ca41e91f
Jérôme Lebel <jlebel@chromium.org>
2023-11-29 14:44:47 +0000

    [iOS] Add API for more button SearchEngineChoice UI
    Bug: b/312913025
    Change-Id: I929f9476ab78c6608faae1d0f555f8ee63b849a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5071173
    Reviewed-by: Samar Chehade-Lepleux <samarchehade@google.com>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230624}

--- ios/chrome/browser/ui/search_engine_choice/search_engine_choice_coordinator.mm
+++ ios/chrome/browser/ui/search_engine_choice/search_engine_choice_coordinator.mm
@@ -145,6 +145,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [_viewController enablePrimaryButton];
 }
 
+- (void)didReachBottom {
+  // Needs implementation.
+}
+
 #pragma mark - SearchEngineChoiceViewControllerDelegate
 
 - (void)showLearnMore {
