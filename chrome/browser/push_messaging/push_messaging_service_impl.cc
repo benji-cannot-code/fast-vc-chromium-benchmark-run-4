7bebabd22349a31099ff3a4eedaefcfe9050b1af
William Xu <willxu@google.com>
2023-11-30 17:20:21 +0000

    Reliably record notification permission revocations.
    First, record if there is a ScopedRevocationReporter on the stack in the onContentSettingsChanged path. If there is one, record which PermissionSourceUI it came from. If there isn't one, manually call PermissionRevoked and record an 'UNIDENTIFIED' PermissionSourceUI.
    
    We suspect that on Android, UNIDENTIFIED actually maps to ANDROID_SETTINGS, since permission toggles in Android are not handled in Chrome directly (i.e. in the OS Settings), and differences are detected and observed via the onContentSettingsChanged method. In these cases there is no explicit ScopedRevocationReporter created which means our current logging misses these events.
    
    Bug: 1473118
    Change-Id: Ib6d0df909c22210d313a12db5d9d082b25376aee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4824826
    Commit-Queue: William Xu <willxu@google.com>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231368}

--- chrome/browser/push_messaging/push_messaging_service_impl.cc
+++ chrome/browser/push_messaging/push_messaging_service_impl.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/gcm_driver/instance_id/instance_id_driver.h"
 #include "components/gcm_driver/instance_id/instance_id_profile_service.h"
 #include "components/permissions/permission_manager.h"
+#include "components/permissions/permission_uma_util.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
@@ -1437,7 +1438,13 @@ void PushMessagingServiceImpl::OnContentSettingChanged(
       barrier_closure.Run();
       continue;
     }
-
+    if (!permissions::PermissionUmaUtil::ScopedRevocationReporter::
+            IsInstanceInScope()) {
+      permissions::PermissionUmaUtil::PermissionRevoked(
+          ContentSettingsType::NOTIFICATIONS,
+          permissions::PermissionSourceUI::UNIDENTIFIED,
+          app_identifier.origin(), profile_);
+    }
     UnexpectedChange(app_identifier,
                      blink::mojom::PushUnregistrationReason::PERMISSION_REVOKED,
                      barrier_closure);
--- chrome/browser/push_messaging/push_messaging_service_unittest.cc
+++ chrome/browser/push_messaging/push_messaging_service_unittest.cc
@@ -6,17 +6,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <string>
+#include <utility>
 #include <vector>
 
 #include "base/command_line.h"
 #include "base/functional/bind.h"
 #include "base/run_loop.h"
 #include "base/test/bind.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/gcm/gcm_profile_service_factory.h"
+#include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/permissions/permission_manager_factory.h"
 #include "chrome/browser/push_messaging/push_messaging_app_identifier.h"
 #include "chrome/browser/push_messaging/push_messaging_features.h"
@@ -29,6 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/gcm_driver/fake_gcm_client_factory.h"
 #include "components/gcm_driver/fake_gcm_profile_service.h"
 #include "components/gcm_driver/gcm_profile_service.h"
+#include "components/history/core/browser/history_database_params.h"
+#include "components/history/core/browser/history_service.h"
+#include "components/history/core/test/test_history_database.h"
 #include "components/permissions/permission_manager.h"
 #include "content/public/browser/push_messaging_service.h"
 #include "content/public/common/content_features.h"
@@ -107,6 +113,13 @@ std::unique_ptr<KeyedService> BuildFakeGCMProfileService(
   return gcm::FakeGCMProfileService::Build(static_cast<Profile*>(context));
 }
 
+std::unique_ptr<KeyedService> BuildTestHistoryService(
+    content::BrowserContext* context) {
+  auto service = std::make_unique<history::HistoryService>();
+  service->Init(history::TestHistoryDatabaseParamsForPath(context->GetPath()));
+  return service;
+}
+
 }  // namespace
 
 class PushMessagingServiceTest : public ::testing::Test {
@@ -115,6 +128,9 @@ class PushMessagingServiceTest : public ::testing::Test {
     // Override the GCM Profile service so that we can send fake messages.
     gcm::GCMProfileServiceFactory::GetInstance()->SetTestingFactory(
         &profile_, base::BindRepeating(&BuildFakeGCMProfileService));
+
+    HistoryServiceFactory::GetInstance()->SetTestingFactory(
+        &profile_, base::BindRepeating(&BuildTestHistoryService));
   }
 
   ~PushMessagingServiceTest() override = default;
@@ -248,6 +264,60 @@ class PushMessagingServiceTest : public ::testing::Test {
       block_async_;
 #endif  // BUILDFLAG(IS_ANDROID)
 };
+
+TEST_F(PushMessagingServiceTest, RecordsRevocationAndSourceUiNoReporterTest) {
+  base::HistogramTester histograms;
+
+  PushMessagingServiceImpl* push_service = profile()->GetPushMessagingService();
+  ASSERT_TRUE(push_service);
+
+  const GURL origin(kTestOrigin);
+  SetPermission(origin, CONTENT_SETTING_ALLOW);
+
+  ASSERT_EQ(blink::mojom::PermissionStatus::GRANTED,
+            push_service->GetPermissionStatus(origin, true /* user_visible */));
+
+  Subscribe(push_service, origin);
+
+  SetPermission(origin, CONTENT_SETTING_DEFAULT);
+
+  histograms.ExpectUniqueSample(
+      "Permissions.Action.Notifications",
+      static_cast<int>(permissions::PermissionAction::REVOKED), 1);
+  histograms.ExpectUniqueSample(
+      "Permissions.Revocation.Notifications.SourceUI",
+      static_cast<int>(permissions::PermissionSourceUI::UNIDENTIFIED), 1);
+}
+
+TEST_F(PushMessagingServiceTest, RecordsRevocationAndSourceUiWithReporterTest) {
+  base::HistogramTester histograms;
+
+  PushMessagingServiceImpl* push_service = profile()->GetPushMessagingService();
+  ASSERT_TRUE(push_service);
+
+  const GURL origin(kTestOrigin);
+  SetPermission(origin, CONTENT_SETTING_ALLOW);
+
+  ASSERT_EQ(blink::mojom::PermissionStatus::GRANTED,
+            push_service->GetPermissionStatus(origin, true /* user_visible */));
+
+  Subscribe(push_service, origin);
+
+  const auto source_ui = permissions::PermissionSourceUI::SITE_SETTINGS;
+  {
+    permissions::PermissionUmaUtil::ScopedRevocationReporter
+        scoped_revocation_reporter(profile(), origin, origin,
+                                   ContentSettingsType::NOTIFICATIONS,
+                                   source_ui);
+    SetPermission(origin, CONTENT_SETTING_DEFAULT);
+  }
+
+  histograms.ExpectUniqueSample(
+      "Permissions.Action.Notifications",
+      static_cast<int>(permissions::PermissionAction::REVOKED), 1);
+  histograms.ExpectUniqueSample("Permissions.Revocation.Notifications.SourceUI",
+                                static_cast<int>(source_ui), 1);
+}
 
 // Fails too often on Linux TSAN builder: http://crbug.com/1211350.
 #if BUILDFLAG(IS_LINUX) && defined(THREAD_SANITIZER)
