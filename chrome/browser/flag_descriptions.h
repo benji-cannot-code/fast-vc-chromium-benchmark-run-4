77185f45ce95526acddd0ac1af82f72500607a13
Pete Williamson <petewil@chromium.org>
2023-11-30 18:56:53 +0000

    Add the CCO edit flag.
    Bug: b:307956097
    Change-Id: I5292669c4ddb817e99a00906ad6555d8b8b000d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5071873
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Peter Williamson <petewil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231431}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/buildflag.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/common/buildflags.h"
+#include "components/compose/buildflags.h"
 #include "components/nacl/common/buildflags.h"
 #include "components/paint_preview/buildflags/buildflags.h"
 #include "components/signin/public/base/signin_buildflags.h"
@@ -4534,6 +4535,11 @@ extern const char kPromiseIconsDescription[];
 extern const char kEnableAudioFocusEnforcementName[];
 extern const char kEnableAudioFocusEnforcementDescription[];
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+
+#if BUILDFLAG(ENABLE_COMPOSE)
+extern const char kComposeName[];
+extern const char kComposeDescription[];
+#endif  // BUILDFLAG(ENABLE_COMPOSE)
 
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
 extern const char kThirdPartyProfileManagementName[];
