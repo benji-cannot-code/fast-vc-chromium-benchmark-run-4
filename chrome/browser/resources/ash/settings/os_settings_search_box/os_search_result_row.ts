b81dac6d77814dffc8f0a020d14c720a62f68a65
conniekxu <conniekxu@chromium.org>
2023-11-30 19:32:24 +0000

    CrOS Settings: Update search result icons to match row icons
    Update search results icons for the About ChromiumOS page.
    
    screenshots:
    - https://screenshot.googleplex.com/6LXESMftxTx9Lfp
    - https://screenshot.googleplex.com/6UmahEnmXP828RV
    - https://screenshot.googleplex.com/7sYYnJ4ZAb3P8Yq
    - https://screenshot.googleplex.com/6gYaCUMzjyx3QyG
    - https://screenshot.googleplex.com/uTf3SC2ZrJYwTc7
    
    Bug: b/313713553
    Change-Id: I98237851aae002d093dbc5039863b3220b2f244a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5074396
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Connie Xu <conniekxu@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231458}

--- chrome/browser/resources/ash/settings/os_settings_search_box/os_search_result_row.ts
+++ chrome/browser/resources/ash/settings/os_settings_search_box/os_search_result_row.ts
@@ -699,6 +699,9 @@ export class OsSearchResultRowElement extends OsSearchResultRowElementBase {
         return 'os-settings:camera';
       case SearchResultIcon.kCellular:
         return 'os-settings:cellular';
+      case SearchResultIcon.kCheckForUpdate:
+        return isRevampEnabled ? 'os-settings:about-update-complete' :
+                                 'os-settings:chrome';
       case SearchResultIcon.kChrome:
         return 'os-settings:chrome';
       case SearchResultIcon.kChromeVox:
@@ -707,8 +710,14 @@ export class OsSearchResultRowElement extends OsSearchResultRowElementBase {
         return 'os-settings:clock';
       case SearchResultIcon.kContrast:
         return 'os-settings:contrast';
+      case SearchResultIcon.kDetailedBuild:
+        return isRevampEnabled ? 'os-settings:about-additional-details' :
+                                 'os-settings:chrome';
       case SearchResultIcon.kDeveloperTags:
         return 'os-settings:developer-tags';
+      case SearchResultIcon.kDiagnostics:
+        return isRevampEnabled ? 'os-settings:about-diagnostics' :
+                                 'os-settings:chrome';
       case SearchResultIcon.kDictation:
         return 'os-settings:dictation';
       case SearchResultIcon.kDisplay:
@@ -719,6 +728,9 @@ export class OsSearchResultRowElement extends OsSearchResultRowElementBase {
         return 'os-settings:settings-ethernet';
       case SearchResultIcon.kFingerprint:
         return 'os-settings:fingerprint';
+      case SearchResultIcon.kFirmwareUpdates:
+        return isRevampEnabled ? 'os-settings:about-firmware-updates' :
+                                 'os-settings:chrome';
       case SearchResultIcon.kFolder:
         return 'os-settings:folder-outline';
       case SearchResultIcon.kFolderShared:
@@ -732,6 +744,9 @@ export class OsSearchResultRowElement extends OsSearchResultRowElementBase {
                                  'os-settings:google-drive';
       case SearchResultIcon.kGooglePlay:
         return 'os-settings:google-play';
+      case SearchResultIcon.kHelp:
+        return isRevampEnabled ? 'os-settings:about-help' :
+                                 'os-settings:chrome';
       case SearchResultIcon.kHotspot:
         return 'os-settings:hotspot';
       case SearchResultIcon.kInstantTethering:
@@ -773,6 +788,9 @@ export class OsSearchResultRowElement extends OsSearchResultRowElementBase {
         return 'os-settings:power';
       case SearchResultIcon.kPrinter:
         return 'os-settings:print';
+      case SearchResultIcon.kReleaseNotes:
+        return isRevampEnabled ? 'os-settings:about-release-notes' :
+                                 'os-settings:chrome';
       case SearchResultIcon.kReset:
         return isRevampEnabled ? 'os-settings:startup' : 'os-settings:restore';
       case SearchResultIcon.kRestore:
