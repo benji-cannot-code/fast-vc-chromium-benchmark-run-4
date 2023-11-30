e2e674b50522acb48997753b0c5c57aaa6d3babf
Antonio Sartori <antoniosartori@chromium.org>
2023-11-30 06:58:25 +0000

    Revert "[UIA] Views text offsets computation"
    This reverts commit 57dc072b6dd5c704fa95179ff2f7b4f4ccb9625f.
    
    Reason for revert: Likely culprit of failing TextfieldTest.AccessibleGraphemeOffsets, see crbug.com/1506421
    
    Original change's description:
    > [UIA] Views text offsets computation
    >
    > Implement text offsets computation for accessibility reasons for
    > single line text content of the Label and Textfield views.
    >
    > **Only on Windows when UIA is enabled**.
    >
    > Bug: 1496497
    > Change-Id: Ia1c7890f707fb282abb39b1e41e9abbfc7f35e53
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4852850
    > Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    > Auto-Submit: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    > Commit-Queue: Scott Violet <sky@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1231033}
    
    Bug: 1496497, 1506421
    Change-Id: I2f97417c3e8be2341914b53230ab3321f0a05620
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5076612
    Owners-Override: Antonio Sartori <antoniosartori@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231171}

--- ui/views/controls/label.cc
+++ ui/views/controls/label.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
-#include "ui/accessibility/accessibility_features.h"
 #include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/base/clipboard/clipboard.h"
 #include "ui/base/clipboard/scoped_clipboard_writer.h"
@@ -35,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/text_utils.h"
 #include "ui/strings/grit/ui_strings.h"
 #include "ui/views/accessibility/view_accessibility.h"
-#include "ui/views/accessibility/views_utilities_aura.h"
 #include "ui/views/background.h"
 #include "ui/views/cascading_property.h"
 #include "ui/views/controls/menu/menu_runner.h"
@@ -1006,42 +1004,6 @@ bool Label::CanHandleAccelerators() const {
   return HasFocus() && GetRenderTextForSelectionController() &&
          View::CanHandleAccelerators();
 }
-
-void Label::GetAccessibleNodeData(ui::AXNodeData* node_data) {
-  View::GetAccessibleNodeData(node_data);
-
-#if BUILDFLAG(SUPPORTS_AX_TEXT_OFFSETS)
-  // If the accessible name changed since the last time we computed the text
-  // offsets, we need to recompute them.
-  if (::features::IsUiaProviderEnabled() &&
-      ax_name_used_to_compute_offsets_ != GetAccessibleName()) {
-    GetViewAccessibility().ClearTextOffsets();
-    ax_name_used_to_compute_offsets_.clear();
-    if (RefreshAccessibleTextOffsets()) {
-      ax_name_used_to_compute_offsets_ = GetAccessibleName();
-    }
-  }
-#endif  // BUILDFLAG(SUPPORTS_AX_TEXT_OFFSETS)
-}
-
-#if BUILDFLAG(SUPPORTS_AX_TEXT_OFFSETS)
-bool Label::RefreshAccessibleTextOffsets() {
-  MaybeBuildDisplayText();
-  // TODO(https://crbug.com/1485632): Add support for multiline textfields.
-  if (!display_text_ || display_text_->multiline()) {
-    return false;
-  }
-
-  GetViewAccessibility().OverrideCharacterOffsets(
-      ComputeTextOffsets(display_text_.get()));
-
-  WordBoundaries boundaries = ComputeWordBoundaries(GetText());
-  GetViewAccessibility().OverrideWordStarts(boundaries.starts);
-  GetViewAccessibility().OverrideWordEnds(boundaries.ends);
-
-  return true;
-}
-#endif  // BUILDFLAG(SUPPORTS_AX_TEXT_OFFSETS)
 
 void Label::OnDeviceScaleFactorChanged(float old_device_scale_factor,
                                        float new_device_scale_factor) {
