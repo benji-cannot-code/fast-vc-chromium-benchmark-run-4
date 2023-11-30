49fab94343bd69e7d182300f236f7f251df47f63
Kazuki Takise <takise@google.com>
2023-11-30 01:24:54 +0000

    Don't apply collision detection while PiP is dragged or tucked
    Currently collision detection is applied whenever the client
    resizes PiP, but we don't want this while PiP is being pinch
    resized because PiP can be detached from any display edge while
    being pinch resized.
    
    Also while PiP is tucked (or being dragged to be tucked), we need
    PiP to be placed partially outside of the display.
    
    Bug: b/308714524
    Test: exo_unittests
    
    Change-Id: I62607f74e73d02ec9be3c3b64e9331362a0cd5a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5069783
    Auto-Submit: Kazuki Takise <takise@chromium.org>
    Reviewed-by: Eliot Courtney <edcourtney@chromium.org>
    Commit-Queue: Kazuki Takise <takise@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231072}

--- components/exo/client_controlled_shell_surface.cc
+++ components/exo/client_controlled_shell_surface.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/client_controlled_state.h"
 #include "ash/wm/collision_detection/collision_detection_utils.h"
 #include "ash/wm/drag_details.h"
+#include "ash/wm/pip/pip_controller.h"
 #include "ash/wm/pip/pip_positioner.h"
 #include "ash/wm/splitview/split_view_controller.h"
 #include "ash/wm/toplevel_window_event_handler.h"
@@ -982,7 +983,8 @@ void ClientControlledShellSurface::SetWidgetBounds(const gfx::Rect& bounds,
     // resting postiion. The resting position should be fully controlled by
     // chrome afterwards because Android isn't aware of Chrome OS System UI.
     bool is_resizing_without_rotation =
-        !display_rotating_with_pip_ &&
+        !display_rotating_with_pip_ && !IsDragging() &&
+        !ash::Shell::Get()->pip_controller()->is_tucked() &&
         GetWindowState()->GetCurrentBoundsInScreen().size() != bounds.size();
     if (GetWindowState()->IsPip() &&
         (!ash::PipPositioner::HasSnapFraction(GetWindowState()) ||
