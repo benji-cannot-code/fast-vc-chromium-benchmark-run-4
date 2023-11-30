d1207dbc0cc08243efb08f4d0ddd883fb583d94a
Ana Salazar <anasalazar@chromium.org>
2023-11-30 18:35:42 +0000

    Use constants for promise icon bounding
    Instead of calculating the promise icon size and bounds using a scale
    factor, define a set of constants for every supported case in both the
    shelf and launcher.
    
    Currently, the launcher supports 48px and 64px standard icons, while the
    shelf supports 36px.
    
    This will eliminate floating point errors that may cause icons to look
    off-center from the promise ring.
    
    go/cros-promise-spec.
    
    Bug: b:310032874
    Change-Id: I54c50a36cb43a7b572af0f899ca2a3953be9d804
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5072114
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231416}

--- ash/public/cpp/app_list/app_list_config.cc
+++ ash/public/cpp/app_list/app_list_config.cc
@@ -189,6 +189,24 @@ int BadgeIconBorderMarginForType(ash::AppListConfigType type) {
   }
 }
 
+int PromiseIconDimensionInstalling(ash::AppListConfigType type) {
+  switch (type) {
+    case ash::AppListConfigType::kRegular:
+      return 52;
+    case ash::AppListConfigType::kDense:
+      return 36;
+  }
+}
+
+int PromiseIconDimensionPending(ash::AppListConfigType type) {
+  switch (type) {
+    case ash::AppListConfigType::kRegular:
+      return 48;
+    case ash::AppListConfigType::kDense:
+      return 32;
+  }
+}
+
 int ItemIconInFolderIconMargin() {
   return 2;
 }
@@ -250,8 +268,9 @@ AppListConfig::AppListConfig(AppListConfigType type)
       shortcut_host_badge_icon_dimension_(HostBadgeIconDimensionForType(type)),
       shortcut_host_badge_icon_border_margin_(
           BadgeIconBorderMarginForType(type)),
-      shortcut_background_border_margin_(
-          ShortcutIconBorderMarginForType(type)) {}
+      shortcut_background_border_margin_(ShortcutIconBorderMarginForType(type)),
+      promise_icon_dimension_installing_(PromiseIconDimensionInstalling(type)),
+      promise_icon_dimension_pending_(PromiseIconDimensionPending(type)) {}
 
 AppListConfig::AppListConfig(const AppListConfig& base_config, float scale_x)
     : type_(base_config.type_),
@@ -291,7 +310,11 @@ AppListConfig::AppListConfig(const AppListConfig& base_config, float scale_x)
       shortcut_host_badge_icon_border_margin_(
           Scale(base_config.shortcut_host_badge_icon_border_margin_, scale_x)),
       shortcut_background_border_margin_(
-          Scale(base_config.shortcut_background_border_margin_, scale_x)) {}
+          Scale(base_config.shortcut_background_border_margin_, scale_x)),
+      promise_icon_dimension_installing_(
+          Scale(base_config.promise_icon_dimension_installing_, scale_x)),
+      promise_icon_dimension_pending_(
+          Scale(base_config.promise_icon_dimension_pending_, scale_x)) {}
 
 AppListConfig::~AppListConfig() = default;
 
--- ash/public/cpp/app_list/app_list_config.h
+++ ash/public/cpp/app_list/app_list_config.h
@@ -226,6 +226,12 @@ class ASH_PUBLIC_EXPORT AppListConfig {
   int shortcut_background_border_margin() const {
     return shortcut_background_border_margin_;
   }
+  int promise_icon_dimension_installing() const {
+    return promise_icon_dimension_installing_;
+  }
+  int promise_icon_dimension_pending() const {
+    return promise_icon_dimension_pending_;
+  }
   int GetShortcutHostBadgeIconContainerDimension() const;
   int GetShortcutBackgroundContainerDimension() const;
   gfx::Size GetShortcutIconSize() const;
@@ -324,6 +330,12 @@ class ASH_PUBLIC_EXPORT AppListConfig {
 
   // The dimension of a background container border of a shortcut.
   const int shortcut_background_border_margin_;
+
+  // The preferred icon size for the promise apps on installing state.
+  const int promise_icon_dimension_installing_;
+
+  // The preferred icon size for the promise apps on pending state.
+  const int promise_icon_dimension_pending_;
 };
 
 }  // namespace ash
