50e690e0ce878f413ffdc0105cc8928e8b3fd0d1
Hongyu Long <hongyulong@chromium.org>
2023-11-30 22:41:33 +0000

    focus-mode: Add Tasks UI to the Contextual Panel
    This CL will add a radio/check icon button and a label into the
    contextual panel.
    
    Bug: b/306271414
    Test: Manual & unittest
    Change-Id: I63e748f04227ef478a2c95965edf4b037b0e85e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018981
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Richard Chui <richui@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Hongyu Long <hongyulong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231574}

--- ash/system/focus_mode/focus_mode_tray.h
+++ ash/system/focus_mode/focus_mode_tray.h
@@ -6,10 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_SYSTEM_FOCUS_MODE_FOCUS_MODE_TRAY_H_
 #define ASH_SYSTEM_FOCUS_MODE_FOCUS_MODE_TRAY_H_
 
+#include "ash/ash_export.h"
 #include "ash/system/focus_mode/focus_mode_controller.h"
 #include "ash/system/tray/tray_background_view.h"
-#include "base/memory/raw_ptr.h"
 #include "ui/base/metadata/metadata_header_macros.h"
+
+namespace views {
+class ImageButton;
+}
 
 namespace ash {
 
@@ -20,8 +24,8 @@ class TrayBubbleWrapper;
 // progress bar is displayed around the tray displaying how much time is left in
 // the focus session. The tray also controls a bubble that is shown when the
 // button is clicked.
-class FocusModeTray : public TrayBackgroundView,
-                      public FocusModeController::Observer {
+class ASH_EXPORT FocusModeTray : public TrayBackgroundView,
+                                 public FocusModeController::Observer {
   METADATA_HEADER(FocusModeTray, TrayBackgroundView)
 
  public:
@@ -29,11 +33,6 @@ class FocusModeTray : public TrayBackgroundView,
   FocusModeTray(const FocusModeTray&) = delete;
   FocusModeTray& operator=(const FocusModeTray&) = delete;
   ~FocusModeTray() override;
-
-  TrayBubbleWrapper* tray_bubble_wrapper_for_testing() { return bubble_.get(); }
-  FocusModeCountdownView* countdown_view_for_testing() {
-    return countdown_view_;
-  }
 
   // TrayBackgroundView:
   void ClickedOutsideBubble() override;
@@ -51,7 +50,19 @@ class FocusModeTray : public TrayBackgroundView,
   void OnTimerTick() override;
   void OnSessionDurationChanged() override;
 
+  TrayBubbleWrapper* tray_bubble_wrapper_for_testing() { return bubble_.get(); }
+  FocusModeCountdownView* countdown_view_for_testing() {
+    return countdown_view_;
+  }
+  const views::ImageButton* GetRadioButtonForTesting() const;
+  const views::Label* GetTaskTitleForTesting() const;
+
  private:
+  friend class FocusModeTrayTest;
+
+  // TODO(b/314022131): Move `TaskItemView` to its own files.
+  class TaskItemView;
+
   // Updates the image and color of the icon.
   void UpdateTrayIcon();
 
@@ -61,11 +72,29 @@ class FocusModeTray : public TrayBackgroundView,
   // Calls `UpdateUI` on `countdown_view_` if it exists.
   void MaybeUpdateCountdownViewUI();
 
+  // Called when the user clicks the radio button to mark a selected task as
+  // completed.
+  void OnCompleteTask();
+
+  // Called when the animation in `AnimateBubbleResize` starts.
+  void OnBubbleResizeAnimationStarted();
+  // Called when the animation in `AnimateBubbleResize` ends.
+  void OnBubbleResizeAnimationEnded();
+  // Animates resizing the bubble view after `task_item_view_` has been removed
+  // from the bubble.
+  void AnimateBubbleResize();
+
   // Image view of the focus mode lamp.
   const raw_ptr<views::ImageView> image_view_;
 
   // The main content view of the bubble.
   raw_ptr<FocusModeCountdownView, DanglingUntriaged> countdown_view_ = nullptr;
+
+  // A box layout view which has a radio/check icon and a label for a selected
+  // task.
+  raw_ptr<TaskItemView> task_item_view_ = nullptr;
+
+  raw_ptr<views::BoxLayoutView> bubble_view_container_ = nullptr;
 
   // The bubble that appears after clicking the tray button.
   std::unique_ptr<TrayBubbleWrapper> bubble_;
