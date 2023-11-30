d9f30b123a42ea7812e641d6a43723718ccd1c40
Daniel Cheng <dcheng@chromium.org>
2023-11-30 23:21:58 +0000

    Plumb VcEffectState's button callback through as a RepeatingClosure.
    When PressedCallback gains support for OnceCallbacks, it will no longer
    be copyable. Since VcEffectState only cares about RepeatingCallbacks,
    just plumb that type through directly so it can easily be shared by all
    the involved buttons.
    
    Bug: 1326449
    Change-Id: I8db9196d45566fbfd862f6fb68d5566576e9df1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5076594
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231594}

--- ash/system/video_conference/effects/video_conference_tray_effects_manager_types.h
+++ ash/system/video_conference/effects/video_conference_tray_effects_manager_types.h
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "ash/ash_export.h"
+#include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
-#include "ui/views/controls/button/button.h"
 
 namespace gfx {
 struct VectorIcon;
@@ -43,7 +43,7 @@ class ASH_EXPORT VcEffectState {
   VcEffectState(const gfx::VectorIcon* icon,
                 const std::u16string& label_text,
                 int accessible_name_id,
-                views::Button::PressedCallback button_callback,
+                base::RepeatingClosure button_callback,
                 std::optional<int> state_value = std::nullopt);
 
   VcEffectState(const VcEffectState&) = delete;
@@ -55,7 +55,7 @@ class ASH_EXPORT VcEffectState {
   const gfx::VectorIcon* icon() const { return icon_; }
   const std::u16string& label_text() const { return label_text_; }
   int accessible_name_id() const { return accessible_name_id_; }
-  const views::Button::PressedCallback& button_callback() const {
+  const base::RepeatingClosure& button_callback() const {
     return button_callback_;
   }
 
@@ -73,7 +73,7 @@ class ASH_EXPORT VcEffectState {
   // Callback that's bound to the delegate's `OnEffectActivated` function,
   // with the effect's ID and the actual (integer) value (e.g.
   // kBackgroundBlurMedium) member as arguments.
-  views::Button::PressedCallback button_callback_;
+  base::RepeatingClosure button_callback_;
 
   // The state value.
   std::optional<int> state_value_;
