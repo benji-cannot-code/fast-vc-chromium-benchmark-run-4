bc0be57e57ee78d7974bb124480e869a8c90aca6
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2023-11-30 16:53:21 +0000

    Revert "Move allowlist check to native code"
    This reverts commit 8cd912804e039159d4ac665597e899e959c5815d.
    
    Reason for revert:
    LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8762987001342534337
    
    Sample failed build: https://ci.chromium.org/b/8762987001342534337
    
    If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8762987001342534337&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F5069216
    
    Original change's description:
    > Move allowlist check to native code
    >
    >
    > Bug: 1502402
    > Change-Id: Ic7725de4ae895aed36842d2c900c61202709e74f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5069216
    > Auto-Submit: Hanfeng Zhu <hanfeng@google.com>
    > Reviewed-by: Min Qin <qinmin@chromium.org>
    > Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    > Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1231341}
    >
    
    Bug: 1502402
    Change-Id: Iff1afffb6f957ccfbfba4b7bf41545ac3ab217da
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5077609
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1231349}

--- chrome/browser/wallet/android/boarding_pass_bridge.cc
+++ chrome/browser/wallet/android/boarding_pass_bridge.cc
@@ -3,21 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/android/jni_string.h"
 #include "base/android/scoped_java_ref.h"
-#include "chrome/browser/wallet/android/boarding_pass_detector.h"
 #include "chrome/browser/wallet/android/jni_headers/BoardingPassBridge_jni.h"
 
-using base::android::ConvertJavaStringToUTF8;
 using base::android::JavaParamRef;
 
 namespace wallet {
-
-static jboolean JNI_BoardingPassBridge_ShouldDetect(
-    JNIEnv* env,
-    const JavaParamRef<jstring>& jurl) {
-  return BoardingPassDetector::ShouldDetect(ConvertJavaStringToUTF8(env, jurl));
-}
 
 static void JNI_BoardingPassBridge_DetectBoardingPass(
     JNIEnv* env,
