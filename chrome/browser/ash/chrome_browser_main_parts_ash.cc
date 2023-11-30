68f5fe9fe5bc25ececf2358a38d4e186f64eb4ea
Michal Mazur <michamazur@google.com>
2023-11-30 18:12:38 +0000

    carrier_lock: destroy carrier lock manager before GCM driver
    Fix UAF crash when stopping Chrome.
    
    Bug: 1494751
    Test: build with AddressSanitizer and test with enabled carrier lock:
          `--enable-features=CellularCarrierLock`
    
    Change-Id: I2e4fc8d7233b43710112bf93c5492b133f2de533
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5036992
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Michał Mazur <michamazur@google.com>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/main@{#1231394}

--- chrome/browser/ash/chrome_browser_main_parts_ash.cc
+++ chrome/browser/ash/chrome_browser_main_parts_ash.cc
@@ -1517,6 +1517,7 @@ void ChromeBrowserMainPartsAsh::PostMainMessageLoopRun() {
   SystemProxyManager::Shutdown();
   report_controller_.reset();
   crostini_unsupported_action_notifier_.reset();
+  carrier_lock_manager_.reset();
 
   BootTimesRecorder::Get()->AddLogoutTimeMarker("UIMessageLoopEnded", true);
 
