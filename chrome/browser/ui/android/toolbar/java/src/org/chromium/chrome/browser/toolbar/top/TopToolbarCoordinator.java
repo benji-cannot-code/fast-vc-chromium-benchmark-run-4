b1ae620bc3b5de3bbfe263c73631019906b126c2
Wenyu Fu <wenyufu@google.com>
2023-11-30 23:41:31 +0000

    Fix ToolbarTest#testToggleTabStripVisibility
    Original test failed on bots due to transition not kicked off. Instead of relying on Activity calling #onConfigurationChanged for ComponentCallbacks, call the method on TabStripTransitionCoordinator directly.
    
    Tested via avd emulator on automotive (where the test originally failed)
    
    Bug: 1502889
    Change-Id: I8fac1b1d554e2b36f929b1ef1b8a8b4be74af6f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5037715
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1231614}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/TopToolbarCoordinator.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/TopToolbarCoordinator.java
@@ -868,6 +868,11 @@ public class TopToolbarCoordinator implements Toolbar {
         return mStartSurfaceToolbarCoordinator;
     }
 
+    /** Returns the {@link TabStripTransitionCoordinator}. */
+    public TabStripTransitionCoordinator getTabStripTransitionCoordinatorForTesting() {
+        return mTabStripTransitionCoordinator;
+    }
+
     @Override
     public void setBrowsingModeHairlineVisibility(boolean isVisible) {
         mToolbarLayout.setHairlineVisibility(isVisible);
