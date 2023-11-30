da8baa75fba1e76bac54db79d5cee7777e802c7a
Colin Blundell <blundell@chromium.org>
2023-11-30 10:26:10 +0000

    [//gpu] Move ScopedMapping from SharedImageInterface to ClientSI
    This is pure cleanup following moving SII::MapSharedImage() to
    ClientSI::Map().
    
    Bug: 1500612
    Change-Id: Id6410e8e68734d265c4e4601cefb2d15bfc907cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5033638
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231229}

--- third_party/blink/renderer/platform/graphics/canvas_resource.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource.cc
@@ -703,7 +703,7 @@ scoped_refptr<StaticBitmapImage> CanvasResourceRasterSharedImage::Bitmap() {
 
   SkImageInfo image_info = CreateSkImageInfo();
   if (!is_accelerated_) {
-    std::unique_ptr<gpu::SharedImageInterface::ScopedMapping> mapping;
+    std::unique_ptr<gpu::ClientSharedImage::ScopedMapping> mapping;
     void* memory = nullptr;
     size_t stride = 0;
     if (base::FeatureList::IsEnabled(kAlwaysUseMappableSIForSoftwareCanvas)) {
@@ -791,7 +791,7 @@ void CanvasResourceRasterSharedImage::CopyRenderingResultsToGpuMemoryBuffer(
   }
   auto* sii =
       ContextProviderWrapper()->ContextProvider()->SharedImageInterface();
-  std::unique_ptr<gpu::SharedImageInterface::ScopedMapping> mapping;
+  std::unique_ptr<gpu::ClientSharedImage::ScopedMapping> mapping;
   void* memory = nullptr;
   size_t stride = 0;
   if (base::FeatureList::IsEnabled(kAlwaysUseMappableSIForSoftwareCanvas)) {
