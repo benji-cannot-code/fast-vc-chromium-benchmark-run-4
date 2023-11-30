c0d8ac3be180e1c068b20c32826ee3a55251478a
Per K <perkj@chromium.org>
2023-11-30 08:03:54 +0000

    Replace UdpPacketSocket::SignalReadPacket with Notify
    Instead of using sigslot with raw pointers to data, use absl::AnyInvocable with a structured class.
    
    Bug: webrtc:11943
    Change-Id: Ic35de0b07dcfe962a4e6cd48de9f2bd71781d53f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5062695
    Commit-Queue: Per Kjellander <perkj@chromium.org>
    Reviewed-by: Erik Jensen <rkjnsn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231184}

--- remoting/protocol/chromium_socket_factory_unittest.cc
+++ remoting/protocol/chromium_socket_factory_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/webrtc/rtc_base/async_packet_socket.h"
+#include "third_party/webrtc/rtc_base/network/received_packet.h"
 #include "third_party/webrtc/rtc_base/socket_address.h"
 #include "third_party/webrtc/rtc_base/time_utils.h"
 
@@ -50,19 +51,19 @@ class ChromiumSocketFactoryTest : public testing::Test,
         rtc::SocketAddress("127.0.0.1", 0), 0, 0));
     ASSERT_TRUE(socket_.get() != nullptr);
     EXPECT_EQ(socket_->GetState(), rtc::AsyncPacketSocket::STATE_BOUND);
-    socket_->SignalReadPacket.connect(this,
-                                      &ChromiumSocketFactoryTest::OnPacket);
+    socket_->RegisterReceivedPacketCallback(
+        [&](rtc::AsyncPacketSocket* socket, const rtc::ReceivedPacket& packet) {
+          OnPacket(socket, packet);
+        });
   }
 
   void OnPacket(rtc::AsyncPacketSocket* socket,
-                const char* data,
-                size_t size,
-                const rtc::SocketAddress& address,
-                const int64_t& packet_time) {
+                const rtc::ReceivedPacket& packet) {
     EXPECT_EQ(socket, socket_.get());
-    last_packet_.assign(data, data + size);
-    last_address_ = address;
-    last_packet_time_ = packet_time;
+    last_packet_.assign(packet.payload().data(),
+                        packet.payload().data() + packet.payload().size());
+    last_address_ = packet.source_address();
+    last_packet_time_ = packet.arrival_time()->us();
     run_loop_.Quit();
   }
 
