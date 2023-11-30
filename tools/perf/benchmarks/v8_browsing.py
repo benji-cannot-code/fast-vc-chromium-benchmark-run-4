31f8d9282419579b61638dd24e59611dcbdbc983
Rakib M. Hasan <rmhasan@google.com>
2023-11-30 17:41:34 +0000

    Replace TODO's assigned to rmhasan@ with TODO's assigned to johnchen@
    Bug: None
    Change-Id: I10aabbdca730055618c0f87e3c731ea955aac214
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5077527
    Auto-Submit: Rakib Hasan <rmhasan@google.com>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231378}

--- tools/perf/benchmarks/v8_browsing.py
+++ tools/perf/benchmarks/v8_browsing.py
@@ -29,9 +29,8 @@ class _V8BrowsingBenchmark(v8_helper.V8PerfMixin, perf_benchmark.PerfBenchmark):
 class V8DesktopBrowsingBenchmark(_V8BrowsingBenchmark):
   """See _V8BrowsingBenchmark."""
   PLATFORM = 'desktop'
-  # TODO(rmhasan): Remove the SUPPORTED_PLATFORMS lists.
-  # SUPPORTED_PLATFORMS is deprecated, please put system specifier tags
-  # from expectations.config in SUPPORTED_PLATFORM_TAGS.
+  # TODO(johnchen): Remove either the SUPPORTED_PLATFORMS or
+  # SUPPORTED_PLATFORMS_TAGS lists. Only one is necessary.
   SUPPORTED_PLATFORMS = [story.expectations.ALL_DESKTOP]
   SUPPORTED_PLATFORM_TAGS = [platforms.DESKTOP]
 
@@ -48,9 +47,8 @@ class V8MobileBrowsingBenchmark(
     _V8BrowsingBenchmark):
   """See _V8BrowsingBenchmark."""
   PLATFORM = 'mobile'
-  # TODO(rmhasan): Remove the SUPPORTED_PLATFORMS lists.
-  # SUPPORTED_PLATFORMS is deprecated, please put system specifier tags
-  # from expectations.config in SUPPORTED_PLATFORM_TAGS.
+  # TODO(johnchen): Remove either the SUPPORTED_PLATFORMS or
+  # SUPPORTED_PLATFORMS_TAGS lists. Only one is necessary.
   SUPPORTED_PLATFORMS = [story.expectations.ALL_MOBILE]
   SUPPORTED_PLATFORM_TAGS = [platforms.MOBILE]
 
@@ -66,9 +64,8 @@ class V8MobileBrowsingBenchmark(
 class V8FutureDesktopBrowsingBenchmark(_V8BrowsingBenchmark):
   """See _V8BrowsingBenchmark."""
   PLATFORM = 'desktop'
-  # TODO(rmhasan): Remove the SUPPORTED_PLATFORMS lists.
-  # SUPPORTED_PLATFORMS is deprecated, please put system specifier tags
-  # from expectations.config in SUPPORTED_PLATFORM_TAGS.
+  # TODO(johnchen): Remove either the SUPPORTED_PLATFORMS or
+  # SUPPORTED_PLATFORMS_TAGS lists. Only one is necessary.
   SUPPORTED_PLATFORMS = [story.expectations.ALL_DESKTOP]
   SUPPORTED_PLATFORM_TAGS = [platforms.DESKTOP]
 
@@ -89,9 +86,8 @@ class V8FutureDesktopBrowsingBenchmark(_V8BrowsingBenchmark):
 class V8FutureMobileBrowsingBenchmark(_V8BrowsingBenchmark):
   """See _V8BrowsingBenchmark."""
   PLATFORM = 'mobile'
-  # TODO(rmhasan): Remove the SUPPORTED_PLATFORMS lists.
-  # SUPPORTED_PLATFORMS is deprecated, please put system specifier tags
-  # from expectations.config in SUPPORTED_PLATFORM_TAGS.
+  # TODO(johnchen): Remove either the SUPPORTED_PLATFORMS or
+  # SUPPORTED_PLATFORMS_TAGS lists. Only one is necessary.
   SUPPORTED_PLATFORMS = [story.expectations.ALL_MOBILE]
   SUPPORTED_PLATFORM_TAGS = [platforms.MOBILE]
 
