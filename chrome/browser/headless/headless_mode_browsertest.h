89f588b1ea8eda7db0e3182acd8a654940500d4c
Peter Kvitek <kvitekp@chromium.org>
2023-11-30 18:32:23 +0000

    Reland "[headless] Disable info bars in headless mode"
    This is a reland of commit add2c56b62f05c997687bd51b179e3aa9f596ce2
    
    Reference to the global 'switches' namespace without a leading ::
    broke the CfT build that under #if includes infobar_switches.h
    which defines namespace 'switches' within 'infobar' namespace
    thus preventing all references to global switches to be resolved
    without leading double colons. This CL adds a leading double colon
    to ensure global namespace resolution.
    
    Original change's description:
    > [headless] Disable info bars in headless mode
    >
    > Infobars cause unexpected view port size changes which mess up
    > most headless applications, so this CL disables infobars when
    > Chrome is running in headless mode. No user can see them anyway.
    >
    > Bug: 1505588
    > Change-Id: I0d7d6585aef3b83887b09441316d9c88519bc77d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5034960
    > Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1230140}
    
    Bug: 1505588
    Change-Id: Icfedb007f076d3a881fa22c715d1873a5d2dc717
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5069035
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231414}

--- chrome/browser/headless/headless_mode_browsertest.h
+++ chrome/browser/headless/headless_mode_browsertest.h
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+namespace content {
+class WebContents;
+}
+
 namespace views {
 class Widget;
 }
@@ -41,6 +45,8 @@ class HeadlessModeBrowserTest : public InProcessBrowserTest {
   bool headful_mode() const { return headful_mode_; }
 
   void AppendHeadlessCommandLineSwitches(base::CommandLine* command_line);
+
+  content::WebContents* GetActiveWebContents();
 
  private:
   bool headful_mode_ = false;
