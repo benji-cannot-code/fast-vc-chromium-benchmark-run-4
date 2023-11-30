dc527b7d3111de412eca967b99cf05e4e8855476
Andrew Rayskiy <greengrape@google.com>
2023-11-30 18:04:18 +0000

    Replace base::Passed in frame_impl_browsertest.cc
    The same behavior can be achieved via base::BindLambdaForTesting.
    
    Bug: 1326449
    Change-Id: I957a6b5cae9212afc8428d44e8ecb86052d4544c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5073014
    Commit-Queue: Zijie He <zijiehe@google.com>
    Reviewed-by: Zijie He <zijiehe@google.com>
    Auto-Submit: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1231387}

--- fuchsia_web/webengine/browser/frame_impl_browsertest.cc
+++ fuchsia_web/webengine/browser/frame_impl_browsertest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/fuchsia/fuchsia_logging.h"
 #include "base/fuchsia/mem_buffer_util.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/test/bind.h"
 #include "base/test/test_future.h"
 #include "base/test/test_timeouts.h"
 #include "build/build_config.h"
@@ -550,19 +551,18 @@ class ChunkedHttpTransactionFactory : public net::test_server::HttpResponse {
 IN_PROC_BROWSER_TEST_F(FrameImplTest, NavigationEventDuringPendingLoad) {
   auto frame = FrameForTest::Create(context(), {});
 
-  ChunkedHttpTransactionFactory* factory = new ChunkedHttpTransactionFactory;
+  auto factory = std::make_unique<ChunkedHttpTransactionFactory>();
   base::RunLoop transaction_created_run_loop;
   factory->SetOnResponseCreatedCallback(
       transaction_created_run_loop.QuitClosure());
   embedded_test_server()->RegisterRequestHandler(base::BindRepeating(
       &net::test_server::HandlePrefixedRequest, "/pausable",
-      base::BindRepeating(
-          [](std::unique_ptr<ChunkedHttpTransactionFactory> out_factory,
-             const net::test_server::HttpRequest&)
+      base::BindLambdaForTesting(
+          [&](const net::test_server::HttpRequest&)
               -> std::unique_ptr<net::test_server::HttpResponse> {
-            return out_factory;
-          },
-          base::Passed(base::WrapUnique(factory)))));
+            CHECK(factory);
+            return std::move(factory);
+          })));
 
   net::test_server::EmbeddedTestServerHandle test_server_handle;
   ASSERT_TRUE(test_server_handle =
