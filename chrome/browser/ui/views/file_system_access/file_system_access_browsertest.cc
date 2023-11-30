bcdc4416d29e9b1692c3d91b26e1d49dff83bc0f
Daniel Rubery <drubery@chromium.org>
2023-11-30 00:35:20 +0000

    Set PDF to SAMPLED_PING
    This file type is a huge amount of traffic but a very small amount of
    warnings. It's not a good tradeoff to be sending download pings for
    this file type. By setting it's file type policy to SAMPLED_PING, we
    will only contact Safe Browsing about a small fraction of downloads.
    This preserves some visibility (in case we need to revert this),
    without paying the ongoing cost of delaying user downloads.
    
    Bug: 1495557
    Change-Id: Ic5eb7fca22078be984b3b34f48c731adad88ab05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4960970
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Christine Smith <christinesm@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231035}

--- chrome/browser/ui/views/file_system_access/file_system_access_browsertest.cc
+++ chrome/browser/ui/views/file_system_access/file_system_access_browsertest.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/network_session_configurator/common/network_switches.h"
 #include "components/permissions/permission_util.h"
 #include "components/safe_browsing/buildflags.h"
+#include "components/safe_browsing/content/common/file_type_policies_test_util.h"
 #include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/test/back_forward_cache_util.h"
@@ -374,6 +375,19 @@ IN_PROC_BROWSER_TEST_F(FileSystemAccessBrowserSlowLoadTest, WaitUntilLoaded) {
 
 #if BUILDFLAG(FULL_SAFE_BROWSING)
 IN_PROC_BROWSER_TEST_F(FileSystemAccessBrowserTest, SafeBrowsing) {
+  safe_browsing::FileTypePoliciesTestOverlay policies;
+  std::unique_ptr<safe_browsing::DownloadFileTypeConfig> file_type_config =
+      std::make_unique<safe_browsing::DownloadFileTypeConfig>();
+  auto* file_type = file_type_config->mutable_default_file_type();
+  file_type->set_uma_value(-1);
+  file_type->set_ping_setting(safe_browsing::DownloadFileType::FULL_PING);
+  auto* platform_settings = file_type->add_platform_settings();
+  platform_settings->set_danger_level(
+      safe_browsing::DownloadFileType::NOT_DANGEROUS);
+  platform_settings->set_auto_open_hint(
+      safe_browsing::DownloadFileType::ALLOW_AUTO_OPEN);
+  policies.SwapConfig(file_type_config);
+
   const std::string file_name("test.pdf");
   const base::FilePath test_file = temp_dir_.GetPath().AppendASCII(file_name);
 
