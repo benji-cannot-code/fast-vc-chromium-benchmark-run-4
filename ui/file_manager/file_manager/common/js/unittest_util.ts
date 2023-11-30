13a2813550f2b6840a839c9c0c1d78872ece31c0
cassycc <cassycc@google.com>
2023-11-30 06:09:54 +0000

    Files app: Convert unittest_util to TS
    Bug: b/289003444
    Change-Id: I00d5dc32afc363279b828fc2e6e54483d4bf5608
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5075715
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1231155}

---
+++ ui/file_manager/file_manager/common/js/unittest_util.ts
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {XfBase} from '../../widgets/xf_base.js';
+
+/**
+ * Wait for the update (render/re-render) of the `element` to be finished
+ * in unit test.
+ *
+ */
+export async function waitForElementUpdate(element: HTMLElement):
+    Promise<void> {
+  // For LitElement we explicitly await the internal updateComplete promise.
+  if (element instanceof XfBase) {
+    await element.updateComplete;
+    // Wait for nested LitElements to finish rendering. Assumes that all nested
+    // elements' render() complete in microtasks.
+    return new Promise(resolve => setTimeout(resolve, 0));
+  }
+  // For others, wait for the next animation frame.
+  return new Promise(resolve => window.requestAnimationFrame(() => resolve()));
+}
