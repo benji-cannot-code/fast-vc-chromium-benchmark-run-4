ff23d60ed767de25b6c0220665ca13dc156167f3
Lily Chen <chlily@chromium.org>
2023-11-30 22:30:52 +0000

    [ImprovedDownloadWarnings] Fix bypass dialog bug due to item reuse
    This fixes a bug that was due to the nature of iron-list reusing the
    item views when items change. From Polymer documentation:
    
    "it is important that all state of the list template be bound to the
    model driving it, since the view may be reused with a new model at any
    time. Particularly, any state that may change as the result of a user
    interaction with the list item must be bound to the model to avoid view
    state inconsistency."
    
    The bug was that, if the bypass warning confirmation dialog was shown,
    and the item it's attached to is independently deleted (e.g. via the
    native UI download bubble), then the bypass warning dialog will still
    be shown, incorrectly attached to the next item (which may not be
    dangerous). See screencast for an example.
    
    The fix is to move the dialog logic to the manager element as opposed to
    the item element. The item informs the manager via a custom event that
    the "Save dangerous" button has been clicked, which causes the dialog
    to be shown. When removing an item, if the removed item has a dialog
    open, it is hidden.
    
    Screencasts:
    https://drive.google.com/drive/folders/15iUrFmnoq32bh7jrJZordu1dSqhaaXCR
    
    Bug: 1465966
    Change-Id: I4dc6941fb18141081ac4c7e284b86aaa2736572d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5067453
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231564}

--- chrome/browser/resources/downloads/manager.ts
+++ chrome/browser/resources/downloads/manager.ts
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import './strings.m.js';
+import './bypass_warning_confirmation_dialog.js';
 import './item.js';
 import './toolbar.js';
 import 'chrome://resources/cr_components/managed_footnote/managed_footnote.js';
@@ -36,6 +37,14 @@ export interface DownloadsManagerElement {
     'toolbar': DownloadsToolbarElement,
     'downloadsList': IronListElement,
   };
+}
+
+type SaveDangerousClickEvent = CustomEvent<{id: string}>;
+
+declare global {
+  interface HTMLElementEventMap {
+    'save-dangerous-click': SaveDangerousClickEvent;
+  }
 }
 
 const DownloadsManagerElementBase = FindShortcutMixin(PolymerElement);
@@ -79,6 +88,11 @@ export class DownloadsManagerElement extends DownloadsManagerElementBase {
         notify: true,
       },
 
+      bypassDialogItemId_: {
+        type: String,
+        value: '',
+      },
+
       lastFocused_: Object,
 
       listBlurred_: Boolean,
@@ -94,6 +108,7 @@ export class DownloadsManagerElement extends DownloadsManagerElementBase {
   private hasShadow_: boolean;
   private inSearchMode_: boolean;
   private spinnerActive_: boolean;
+  private bypassDialogItemId_: string;
 
   private announcerDebouncer_: Debouncer|null = null;
   private mojoHandler_: PageHandlerInterface;
@@ -121,7 +136,6 @@ export class DownloadsManagerElement extends DownloadsManagerElementBase {
     }
   }
 
-  /** @override */
   override connectedCallback() {
     super.connectedCallback();
 
@@ -156,7 +170,6 @@ export class DownloadsManagerElement extends DownloadsManagerElementBase {
         e => this.onToastClicked_(e);
   }
 
-  /** @override */
   override disconnectedCallback() {
     super.disconnectedCallback();
 
@@ -164,6 +177,39 @@ export class DownloadsManagerElement extends DownloadsManagerElementBase {
         id => assert(this.mojoEventTarget_.removeListener(id)));
 
     this.eventTracker_.removeAll();
+  }
+
+  private onSaveDangerousClick_(e: SaveDangerousClickEvent) {
+    const bypassItem = this.items_.find(item => item.id === e.detail.id);
+    if (bypassItem) {
+      this.bypassDialogItemId_ = bypassItem.id;
+    }
+  }
+
+  private shouldShowBypassWarningDialog_(): boolean {
+    return this.bypassDialogItemId_ !== '';
+  }
+
+  private computeBypassWarningDialogFileName_(): string {
+    const bypassItem =
+        this.items_.find(item => item.id === this.bypassDialogItemId_);
+    return bypassItem?.fileName || '';
+  }
+
+  private hideBypassWarningDialog_() {
+    this.bypassDialogItemId_ = '';
+  }
+
+  private onBypassWarningConfirmationDialogClose_() {
+    const dialog = this.shadowRoot!.querySelector(
+        'download-bypass-warning-confirmation-dialog');
+    assert(dialog);
+    assert(this.bypassDialogItemId_ !== '');
+    if (dialog.wasConfirmed()) {
+      this.mojoHandler_!.saveDangerousRequiringGesture(
+          this.bypassDialogItemId_);
+    }
+    this.hideBypassWarningDialog_();
   }
 
   private clearAll_() {
@@ -309,6 +355,9 @@ export class DownloadsManagerElement extends DownloadsManagerElementBase {
   private removeItem_(index: number) {
     const removed = this.items_.splice(index, 1);
     this.updateHideDates_(index, index);
+    if (removed.some(item => item.id === this.bypassDialogItemId_)) {
+      this.hideBypassWarningDialog_();
+    }
     this.notifySplices('items_', [{
                          index: index,
                          addedCount: 0,
