82ea739eae54262fc99c7a2d53cfd36d3fd23820
Minoru Chikamune <chikamune@chromium.org>
2023-11-30 08:46:27 +0000

    Add LowPriorityAsyncScriptExecution related UMAs
    This CL adds the following UMAs.
    
    - Blink.Script.PostTaskWithLowPriorityUntilTimeout.RaceTaskPriority
    
    Record which task priority's task was run.
    
    - Blink.Script.PostTaskWithLowPriorityUntilTimeout.Time
    - Blink.Script.PostTaskWithLowPriorityUntilTimeout.Time.{LowerPriority, NormalPriority}
    
    Measures the duration between when a script is ready to run, to when the
    script is about to run.
    
    Bug: 1348467
    Change-Id: I008e4e1a7171a6604fc4607100fd1b67c929bf92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5075936
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231200}

--- third_party/blink/renderer/core/script/script_runner.cc
+++ third_party/blink/renderer/core/script/script_runner.cc
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/feature_list.h"
+#include "base/metrics/histogram_functions.h"
+#include "base/strings/strcat.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/trace_event/typed_macros.h"
 #include "third_party/blink/public/common/features.h"
@@ -48,6 +50,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class RaceTaskPriority {
+  kLowerPriority = 0,
+  kNormalPriority = 1,
+  kMaxValue = kNormalPriority,
+};
+
+const char* RaceTaskPriorityToString(RaceTaskPriority task_priority) {
+  switch (task_priority) {
+    case RaceTaskPriority::kLowerPriority:
+      return "LowerPriority";
+    case RaceTaskPriority::kNormalPriority:
+      return "NormalPriority";
+  }
+}
+
 void PostTaskWithLowPriorityUntilTimeout(
     const base::Location& from_here,
     base::OnceClosure task,
@@ -63,17 +82,37 @@ void PostTaskWithLowPriorityUntilTimeout(
   // |task| doesn't run more than once. |task| runs on either of
   // |lower_priority_task_runner| and |normal_priority_task_runner| whichever
   // comes first.
-  auto run_task_once =
-      [](scoped_refptr<RefCountedOnceClosure> ref_counted_task) {
-        if (!ref_counted_task->data.is_null())
-          std::move(ref_counted_task->data).Run();
-      };
+  auto run_task_once = [](scoped_refptr<RefCountedOnceClosure> ref_counted_task,
+                          RaceTaskPriority task_priority,
+                          base::TimeTicks post_task_time) {
+    if (!ref_counted_task->data.is_null()) {
+      auto duration = base::TimeTicks::Now() - post_task_time;
+      std::move(ref_counted_task->data).Run();
+      base::UmaHistogramEnumeration(
+          "Blink.Script.PostTaskWithLowPriorityUntilTimeout.RaceTaskPriority",
+          task_priority);
+      base::UmaHistogramMediumTimes(
+          "Blink.Script.PostTaskWithLowPriorityUntilTimeout.Time", duration);
+      base::UmaHistogramMediumTimes(
+          base::StrCat(
+              {"Blink.Script.PostTaskWithLowPriorityUntilTimeout.Time.",
+               RaceTaskPriorityToString(task_priority)}),
+          duration);
+    }
+  };
+
+  base::TimeTicks post_task_time = base::TimeTicks::Now();
 
   lower_priority_task_runner->PostTask(
-      from_here, WTF::BindOnce(run_task_once, ref_counted_task));
+      from_here,
+      WTF::BindOnce(run_task_once, ref_counted_task,
+                    RaceTaskPriority::kLowerPriority, post_task_time));
 
   normal_priority_task_runner->PostDelayedTask(
-      from_here, WTF::BindOnce(run_task_once, ref_counted_task), timeout);
+      from_here,
+      WTF::BindOnce(run_task_once, ref_counted_task,
+                    RaceTaskPriority::kNormalPriority, post_task_time),
+      timeout);
 }
 
 }  // namespace
