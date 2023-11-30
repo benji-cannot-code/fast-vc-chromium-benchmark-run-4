85d3e250d77bc2f22e260fcc275affe601bfe2e5
Marc Treib <treib@chromium.org>
2023-11-30 15:35:15 +0000

    Cleanup: Deprecate unused fields in Sync's TabNavigation proto
    The newly-deprecated and no-longer-populated fields are:
    * navigation_redirect
    * last_navigation_redirect_url
    * replaced_navigation
    * task_id
    * ancestor_taks_id
    * page_language
    * blocked_state
    
    All of these were populated in Chromium, but never read again.
    Some of them required a bunch of plumbing, which is also removed.
    
    The fields are marked as deprecated rather than removed entirely
    because there are still server-side references to them.
    
    Bug: 1489597
    Change-Id: Ifd93bc199fc22079e6e64abdb877531ceb6fe33e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5065951
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Mahmoud Rashad <mmrashad@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231326}

--- chrome/browser/ui/sync/tab_contents_synced_tab_delegate.cc
+++ chrome/browser/ui/sync/tab_contents_synced_tab_delegate.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync_sessions/sync_sessions_client.h"
 #include "components/sync_sessions/synced_window_delegate.h"
 #include "components/sync_sessions/synced_window_delegates_getter.h"
-#include "components/translate/content/browser/content_record_page_language.h"
 #include "content/public/browser/favicon_status.h"
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/navigation_entry.h"
@@ -90,14 +89,6 @@ GURL TabContentsSyncedTabDelegate::GetVirtualURLAtIndex(int i) const {
   DCHECK(web_contents_);
   NavigationEntry* entry = GetPossiblyPendingEntryAtIndex(web_contents_, i);
   return entry ? entry->GetVirtualURL() : GURL();
-}
-
-std::string TabContentsSyncedTabDelegate::GetPageLanguageAtIndex(int i) const {
-  DCHECK(web_contents_);
-  NavigationEntry* entry = GetPossiblyPendingEntryAtIndex(web_contents_, i);
-  // If we don't have an entry, return empty language.
-  return entry ? translate::GetPageLanguageFromNavigation(entry)
-               : std::string();
 }
 
 void TabContentsSyncedTabDelegate::GetSerializedNavigationAtIndex(
