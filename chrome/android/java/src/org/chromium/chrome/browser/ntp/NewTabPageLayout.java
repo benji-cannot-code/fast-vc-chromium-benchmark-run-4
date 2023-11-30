a7274b17edd8288e2bc8d572a326601ca9ff269e
Sky Malice <skym@chromium.org>
2023-11-30 18:03:27 +0000

    NTP to Ntp in query_tiles Java package.
    Bug: 1505533
    Change-Id: I0affa609d502bbfa2dcc5d8c712cb1e0a2697424
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5063356
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231386}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageLayout.java
@@ -313,7 +313,7 @@ public class NewTabPageLayout extends LinearLayout {
         initializeLensButton();
         initializeLayoutChangeListener();
 
-        if (searchProviderIsGoogle && QueryTileUtils.isQueryTilesEnabledOnNTP()) {
+        if (searchProviderIsGoogle && QueryTileUtils.isQueryTilesEnabledOnNtp()) {
             mQueryTileSection =
                     new QueryTileSection(
                             findViewById(R.id.query_tiles), profile, mManager::performSearchQuery);
@@ -484,7 +484,7 @@ public class NewTabPageLayout extends LinearLayout {
         assert mMvTilesContainerLayout != null;
 
         int maxRows = 2;
-        if (searchProviderIsGoogle && QueryTileUtils.isQueryTilesEnabledOnNTP()) {
+        if (searchProviderIsGoogle && QueryTileUtils.isQueryTilesEnabledOnNtp()) {
             maxRows = QueryTileSection.getMaxRowsForMostVisitedTiles(getContext());
         }
 
