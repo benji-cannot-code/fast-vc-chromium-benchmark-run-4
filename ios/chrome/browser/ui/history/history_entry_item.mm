8d3216c0fa005f1d3e7b5e536de31f2fc20e5f0d
David Jean <djean@chromium.org>
2023-11-30 08:34:48 +0000

    [ios] Fix VO for history cell not identifying as a button
    Bug: 1364592
    Change-Id: I4ad7800931f8f943eafe2e126e496a7a72648e3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5068005
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: David Jean <djean@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231194}

--- ios/chrome/browser/ui/history/history_entry_item.mm
+++ ios/chrome/browser/ui/history/history_entry_item.mm
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   cell.metadataLabel.text = self.timeText;
   cell.isAccessibilityElement = YES;
   cell.accessibilityCustomActions = self.accessibilityActions;
+  cell.accessibilityTraits |= UIAccessibilityTraitButton;
   [cell configureUILayout];
 }
 
