1a6116bb27443142af3d52adad3e62c23d3e3bf0
Steven Bennetts <stevenjb@chromium.org>
2023-11-30 01:07:42 +0000

    Ash: Capitve Portal: Logging improvements
    This removes some redundant logic, fixes a logging bug,
    and adds some portal notification logging.
    
    BUG=b:313453892
    TEST=none
    
    Change-Id: I2808534578fe5b1a83eb177f6d65e421a0eca6e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5063815
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231063}

--- chrome/browser/ui/ash/network/network_portal_notification_controller.cc
+++ chrome/browser/ui/ash/network/network_portal_notification_controller.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/notifications/system_notification_helper.h"
 #include "chrome/browser/ui/ash/network/network_portal_signin_controller.h"
 #include "chrome/grit/generated_resources.h"
+#include "chromeos/ash/components/network/network_event_log.h"
 #include "chromeos/ash/components/network/network_handler.h"
 #include "chromeos/ash/components/network/network_state.h"
 #include "chromeos/ash/components/network/network_state_handler.h"
@@ -44,6 +45,12 @@ namespace ash {
 namespace {
 
 const char kNotifierNetworkPortalDetector[] = "ash.network.portal-detector";
+
+bool IsPortalState(NetworkState::PortalState portal_state) {
+  return portal_state == NetworkState::PortalState::kPortal ||
+         portal_state == NetworkState::PortalState::kPortalSuspected ||
+         portal_state == NetworkState::PortalState::kProxyAuthRequired;
+}
 
 std::unique_ptr<message_center::Notification> CreateNotification(
     const NetworkState* network,
@@ -120,6 +127,7 @@ class NotificationDelegateImpl : public message_center::NotificationDelegate {
 void NotificationDelegateImpl::Click(
     const absl::optional<int>& button_index,
     const absl::optional<std::u16string>& reply) {
+  NET_LOG(USER) << "Captive Portal notification: Click";
   NetworkPortalSigninController::Get()->ShowSignin(
       NetworkPortalSigninController::SigninSource::kNotification);
   CloseNotification();
@@ -147,10 +155,11 @@ NetworkPortalNotificationController::~NetworkPortalNotificationController() {
 void NetworkPortalNotificationController::PortalStateChanged(
     const NetworkState* network,
     NetworkState::PortalState portal_state) {
-  if (!network ||
-      (portal_state != NetworkState::PortalState::kPortal &&
-       portal_state != NetworkState::PortalState::kPortalSuspected &&
-       portal_state != NetworkState::PortalState::kProxyAuthRequired)) {
+  if (!network || !IsPortalState(portal_state)) {
+    if (!last_network_guid_.empty() && IsPortalState(last_portal_state_)) {
+      NET_LOG(EVENT) << "Captive Portal notification: Close for "
+                     << last_network_guid_;
+    }
     last_network_guid_.clear();
     last_portal_state_ = portal_state;
 
@@ -167,8 +176,10 @@ void NetworkPortalNotificationController::PortalStateChanged(
   }
 
   // Don't do anything if we're currently activating the device.
-  if (MobileActivator::GetInstance()->RunningActivation())
+  if (MobileActivator::GetInstance()->RunningActivation()) {
+    NET_LOG(EVENT) << "Captive Portal notification: Skip (mobile activation)";
     return;
+  }
 
   // Don't do anything if notification for |network| already was
   // displayed with the same portal_state.
@@ -179,6 +190,8 @@ void NetworkPortalNotificationController::PortalStateChanged(
   last_network_guid_ = network->guid();
   last_portal_state_ = portal_state;
 
+  NET_LOG(EVENT) << "Captive Portal notification: Show for "
+                 << NetworkId(network) << " PortalState: " << portal_state;
   base::UmaHistogramEnumeration("Network.NetworkPortalNotificationState",
                                 portal_state);
 
