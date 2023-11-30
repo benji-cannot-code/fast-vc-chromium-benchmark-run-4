6f2b36835c893bd18c926753e59d13014e4071ed
Sky Malice <skym@chromium.org>
2023-11-30 20:07:07 +0000

    NTP to Ntp in toolbar Java package.
    Bug: 1505533
    Change-Id: I1a6cc63fe1667721329d339d5a458c9216a7e52f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5062342
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1231476}

--- chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/VoiceToolbarButtonControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/VoiceToolbarButtonControllerTest.java
@@ -124,7 +124,7 @@ public final class VoiceToolbarButtonControllerTest {
     @Test
     @MediumTest
     @Restriction(RESTRICTION_TYPE_NON_LOW_END_DEVICE)
-    public void testVoiceButtonInToolbarIsDisabledOnNTP() {
+    public void testVoiceButtonInToolbarIsDisabledOnNtp() {
         // Ensure the button starts visible.
         ViewUtils.waitForVisibleView(
                 allOf(
