62d2554fcb33ea6deb715ea3ec87d977e406bf59
Hanfeng Zhu <hanfeng@google.com>
2023-11-30 16:06:27 +0000

    Move allowlist check to native code
    Bug: 1502402
    Change-Id: Ic7725de4ae895aed36842d2c900c61202709e74f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5069216
    Auto-Submit: Hanfeng Zhu <hanfeng@google.com>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1231341}

---
+++ chrome/browser/wallet/android/boarding_pass_detector.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_WALLET_ANDROID_BOARDING_PASS_DETECTOR_H_
+#define CHROME_BROWSER_WALLET_ANDROID_BOARDING_PASS_DETECTOR_H_
+
+#include <string>
+
+namespace wallet {
+
+class BoardingPassDetector {
+ public:
+  // Decides whether to run boarding pass detection on given url.
+  static bool ShouldDetect(const std::string& url);
+};
+
+}  // namespace wallet
+
+#endif  // CHROME_BROWSER_WALLET_ANDROID_BOARDING_PASS_DETECTOR_H_
