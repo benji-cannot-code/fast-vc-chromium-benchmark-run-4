98b6b432167e8c340d375dd90973989aee42fdf1
Cam Bickel <cambickel@google.com>
2023-11-30 00:27:20 +0000

    firmware_update: Set "requests" FwupdFeatureFlag when v2 flag is enabled
    This CL adds a step in the FwupdClient init function that calls the
    fwupd method "SetFeatureFlags" if the FirmwareUpdateUIV2 flag is
    enabled. Specifically, it calls "SetFeatureFlags" with the flag
    "requests", which tells fwupd that our client is capable of receiving
    interactive user requests (e.g. "unplug device to continue update").
    
    See https://github.com/fwupd/fwupd/blob/main/libfwupd/fwupd-enums.h for
    the full list of FwupdFeatureFlags.
    
    I've tested this on my DUT by running Firmware Updates with and without
    the v2 flag, and encountered no abnormal behavior.
    
    Test: chromeos_unittests
    Bug: b/308669841
    Change-Id: I7a52db1b4a226c5e9014bed9b8db4f902a1ae057
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5040907
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Camden Bickel <cambickel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1231025}

--- chromeos/ash/components/dbus/fwupd/fake_fwupd_client.cc
+++ chromeos/ash/components/dbus/fwupd/fake_fwupd_client.cc
@@ -53,4 +53,7 @@ void FakeFwupdClient::InstallUpdate(const std::string& device_id,
     observer.OnInstallResponse(install_success_);
 }
 
+// Implement stub method to satisfy interface.
+void FakeFwupdClient::SetFwupdFeatureFlags() {}
+
 }  // namespace ash
--- chromeos/ash/components/dbus/fwupd/fake_fwupd_client.h
+++ chromeos/ash/components/dbus/fwupd/fake_fwupd_client.h
@@ -28,6 +28,7 @@ class COMPONENT_EXPORT(ASH_DBUS_FWUPD) FakeFwupdClient : public FwupdClient {
                      FirmwareInstallOptions options) override;
 
  private:
+  void SetFwupdFeatureFlags() override;
   bool install_success_ = false;
 };
 
