9a7ee7341d42501533e5e6742a1596ca448259ac
Taylor Bergquist <tbergquist@chromium.org>
2023-11-30 00:51:43 +0000

    Use TabData to filter tabs during trigger scoring.
    This matches the logic used elsewhere in the service, taking into account things like already grouped tabs, pinned tabs, and non-web tabs.
    
    Change-Id: I1b13131e174fec84dcdc1176142d64011743236f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5046155
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231049}

--- chrome/browser/ui/tabs/organization/trigger.cc
+++ chrome/browser/ui/tabs/organization/trigger.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/time/default_tick_clock.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/tabs/organization/tab_data.h"
 #include "chrome/browser/ui/tabs/organization/trigger_policies.h"
 #include "chrome/browser/ui/tabs/tab_group_model.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -15,14 +16,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Just counts the number of tabs in the browser.
-float MVPScoringFunction(const TabStripModel* const model) {
-  int num_tabs_not_in_group = 0;
+float MVPScoringFunction(TabStripModel* const model) {
+  int num_eligible_tabs = 0;
   for (int i = 0; i < model->count(); i++) {
-    if (model->GetTabGroupForTab(i) == absl::nullopt) {
-      num_tabs_not_in_group++;
+    if (TabData(model, model->GetWebContentsAt(i)).IsValidForOrganizing()) {
+      num_eligible_tabs++;
     }
   }
-  return num_tabs_not_in_group;
+  return num_eligible_tabs;
 }
 }  // namespace
 
@@ -37,7 +38,7 @@ TabOrganizationTrigger::TabOrganizationTrigger(
 TabOrganizationTrigger::~TabOrganizationTrigger() = default;
 
 bool TabOrganizationTrigger::ShouldTrigger(
-    const TabStripModel* const tab_strip_model) const {
+    TabStripModel* const tab_strip_model) const {
   const float score = scoring_function_.Run(tab_strip_model);
   if (score < score_threshold_) {
     return false;
