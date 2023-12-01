d02efd4ac6320e4e19ee305ea9ec71ec87407231
Pu Shi <pushi@google.com>
2023-12-01 22:17:59 +0000

    [Phone Hub] Add feature flag for Phone Hub Structured Metrics
    Bug: b/308674745
    Change-Id: Id84e1240085006777bafa865a748d8282caac847
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5072115
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Jong Ahn <jongahn@chromium.org>
    Commit-Queue: Pu Shi <pushi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1232205}

--- components/metrics/structured/structured_metrics_features.cc
+++ components/metrics/structured/structured_metrics_features.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/metrics/structured/structured_metrics_features.h"
 
+#include "base/feature_list.h"
+
 namespace metrics::structured {
 
 BASE_FEATURE(kEventSequenceLogging,
@@ -27,6 +29,10 @@ BASE_FEATURE(kNearbyShareMetrics,
 BASE_FEATURE(kEnabledStructuredMetricsService,
              "EnableStructuredMetricsService",
              base::FEATURE_ENABLED_BY_DEFAULT);
+
+BASE_FEATURE(kPhoneHubStructuredMetrics,
+             "PhoneHubStructuredMetrics",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 constexpr base::FeatureParam<int> kLimitFilesPerScanParam{
     &features::kStructuredMetrics, "file_limit", 50};
