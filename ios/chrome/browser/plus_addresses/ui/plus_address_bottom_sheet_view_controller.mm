7e84de5ad9fa3c10e2f57535a5ff87d560b441ed
Matt Reichhoff <mreichhoff@chromium.org>
2023-12-01 22:24:13 +0000

    [iOS] Ensure the plus address modal is destroyed on dismiss
    There's no intended functionality difference with this CL, as the
    down swipe already closed the bottom sheet. This change simply
    ensures the normal dismissal process is followed, and adds test
    coverage for the swipe down case.
    
    Bug: 1467623, b/295076363
    Change-Id: Ida73ab57f6ec00f1a54dbf3c0148bd9f82affb2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5075439
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232210}

--- ios/chrome/browser/plus_addresses/ui/plus_address_bottom_sheet_view_controller.mm
+++ ios/chrome/browser/plus_addresses/ui/plus_address_bottom_sheet_view_controller.mm
@@ -54,6 +54,7 @@ NSAttributedString* DescriptionMessage() {
 
 @interface PlusAddressBottomSheetViewController () <
     ConfirmationAlertActionHandler,
+    UIAdaptivePresentationControllerDelegate,
     UITextViewDelegate>
 
 @end
@@ -121,6 +122,7 @@ NSAttributedString* DescriptionMessage() {
   [super viewDidLoad];
 
   self.actionHandler = self;
+  self.presentationController.delegate = self;
   // Disable the primary button until such time as the reservation is complete.
   // If reserving an address fails, we should inform the user and not attempt to
   // fill any fields on the page.
@@ -179,6 +181,13 @@ NSAttributedString* DescriptionMessage() {
   [_browserCoordinatorHandler dismissPlusAddressBottomSheet];
   // Returns NO as the app is handling the opening of the URL.
   return NO;
+}
+
+#pragma mark - UIAdaptivePresentationControllerDelegate
+
+- (void)presentationControllerDidDismiss:
+    (UIPresentationController*)presentationController {
+  [_browserCoordinatorHandler dismissPlusAddressBottomSheet];
 }
 
 #pragma mark - Private
