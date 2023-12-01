6fe1e1e2a2cbd9f6811c7728ea921b32282d94bc
Norge Vizcay <vizcay@google.com>
2023-12-01 14:17:35 +0000

    [AutofillDataModel] Use i18n address model for addresses only.
    The launch of the i18n address model will only cover addresses at first.
    This CL makes makes this more explicit and it removes an issue where
    i18n parsing was affecting the name tree.
    
    Bug: 1464568
    Change-Id: I6aabd7fd1ec615403dff31e15196d629a6a5ee26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5076865
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1231903}

--- components/autofill/core/browser/data_model/autofill_structured_address_component.cc
+++ components/autofill/core/browser/data_model/autofill_structured_address_component.cc
@@ -467,7 +467,9 @@ void AddressComponent::ParseValueAndAssignSubcomponents() {
   }
 
   bool parsing_successful =
-      base::FeatureList::IsEnabled(features::kAutofillUseI18nAddressModel)
+      base::FeatureList::IsEnabled(features::kAutofillUseI18nAddressModel) &&
+              GroupTypeOfServerFieldType(GetStorageType()) ==
+                  FieldTypeGroup::kAddress
           ? ParseValueAndAssignSubcomponentsByI18nParsingRules()
           : ParseValueAndAssignSubcomponentsByRegularExpressions();
 
@@ -508,7 +510,9 @@ bool AddressComponent::ParseValueAndAssignSubcomponentsByRegularExpressions() {
 
 void AddressComponent::
     TryParseValueAndAssignSubcomponentsRespectingSetValues() {
-  if (base::FeatureList::IsEnabled(features::kAutofillUseI18nAddressModel)) {
+  if (base::FeatureList::IsEnabled(features::kAutofillUseI18nAddressModel) &&
+      GroupTypeOfServerFieldType(GetStorageType()) ==
+          FieldTypeGroup::kAddress) {
     const AddressCountryCode country_code = AddressCountryCode(
         base::UTF16ToUTF8(GetRootNode().GetValueForType(ADDRESS_HOME_COUNTRY)));
 
