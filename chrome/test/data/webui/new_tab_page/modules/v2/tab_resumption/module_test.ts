1cb998d893cd30a309edd804c2225a2c415f5256
Marlon Facey <mfacey@chromium.org>
2023-12-01 18:42:01 +0000

    [ntp xdtr] Add header for ntp xdtr
    Added disable, info and customize module actions to module header.
    Updated translation strings and wired the buttons as well.
    
    Header: https://screenshot.googleplex.com/BkCkMPmH9jKUPR5
    Info: https://screenshot.googleplex.com/BYNvxdieirdn3tF
    
    Bug: 1506648
    Change-Id: I7d2783e31c9eee4dd8da61ebc58df0a6d30595ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5074627
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232060}

--- chrome/test/data/webui/new_tab_page/modules/v2/tab_resumption/module_test.ts
+++ chrome/test/data/webui/new_tab_page/modules/v2/tab_resumption/module_test.ts
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {Tab} from 'chrome://new-tab-page/history_types.mojom-webui.js';
 import {tabResumptionDescriptor, TabResumptionModuleElement, TabResumptionProxyImpl} from 'chrome://new-tab-page/lazy_load.js';
+import {$$} from 'chrome://new-tab-page/new_tab_page.js';
 import {PageHandlerRemote} from 'chrome://new-tab-page/tab_resumption.mojom-webui.js';
 import {assertEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {waitAfterNextRender} from 'chrome://webui-test/polymer_test_util.js';
@@ -14,18 +15,16 @@ import {installMock} from '../../../test_support.js';
 
 function createSampleTabs(count: number): Tab[] {
   return new Array(count).fill(0).map(
-      (_, i) => createSampleTab(i.toString(), 'Test Device'));
+      (_, i) => createSampleTab({sessionTag: i.toString()}));
 }
 
 function createSampleTab(
-    sessionTag: string,
-    sessionName: string,
     overrides?: Partial<Tab>,
     ): Tab {
   const tab: Tab = Object.assign(
       {
-        sessionTag: sessionTag,
-        sessionName: sessionName,
+        sessionTag: 'Test Tag',
+        sessionName: 'Test Device',
         url: {url: 'https://www.foo.com'},
         title: 'Test Tab Title',
         relativeTime: '0 seconds ago',
@@ -69,6 +68,39 @@ suite('NewTabPageModulesTabResumptionModuleTest', () => {
     test('Module instance created successfully', async () => {
       const moduleElement = await initializeModule(createSampleTabs(1));
       assertTrue(!!moduleElement);
+    });
+
+    test('Header element populated with correct data', async () => {
+      // Arrange.
+      const moduleElement = await initializeModule(createSampleTabs(1));
+
+      // Assert.
+      assertTrue(!!moduleElement);
+      const headerElement = $$(moduleElement, 'ntp-module-header-v2');
+      assertTrue(!!headerElement);
+      const actionMenu = $$(headerElement, 'cr-action-menu');
+      assertTrue(!!actionMenu);
+
+      const actionMenuItems =
+          [...actionMenu.querySelectorAll('button.dropdown-item')];
+      assertEquals(3, actionMenuItems.length);
+      ['disable', 'info', 'customize-module'].forEach((action, index) => {
+        assertEquals(
+            action, actionMenuItems[index]!.getAttribute('data-action'));
+      });
+    });
+
+    test('Header info button click opens info dialog', async () => {
+      // Arrange.
+      const moduleElement = await initializeModule(createSampleTabs(1));
+
+      // Assert.
+      assertTrue(!!moduleElement);
+      const headerElement = $$(moduleElement, 'ntp-module-header-v2');
+      assertTrue(!!headerElement);
+      headerElement!.dispatchEvent(new Event('info-button-click'));
+
+      assertTrue(!!$$(moduleElement, 'ntp-info-dialog'));
     });
   });
 });
