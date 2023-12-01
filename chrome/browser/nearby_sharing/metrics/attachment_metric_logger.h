5b4a64f5722a98e2c7ab57d5159b87ca1ec2d977
Jack Shira <jackshira@google.com>
2023-12-01 21:44:57 +0000

    [Nearby Share] Add attachment metric logger
    Add an attachment metric logger class, and wire it up as
    an observer. This logs attachment metrics for each file and
    text attachment that is present in a transfer.
    
    Test: manually verified via chrome://metrics-internals
    Bug: b/313520497
    
    Change-Id: I4ca05f59e25ca997f75aa817624a089d4316213c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5063686
    Reviewed-by: Jong Ahn <jongahn@chromium.org>
    Reviewed-by: Daniel Classon <dclasson@google.com>
    Commit-Queue: Jack Shira <jackshira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1232190}

---
+++ chrome/browser/nearby_sharing/metrics/attachment_metric_logger.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_NEARBY_SHARING_METRICS_ATTACHMENT_METRIC_LOGGER_H_
+#define CHROME_BROWSER_NEARBY_SHARING_METRICS_ATTACHMENT_METRIC_LOGGER_H_
+
+#include "chrome/browser/nearby_sharing/nearby_sharing_service.h"
+#include "chrome/browser/nearby_sharing/transfer_metadata.h"
+
+namespace nearby::share::metrics {
+
+class AttachmentMetricLogger : public NearbySharingService::Observer {
+ public:
+  AttachmentMetricLogger();
+  ~AttachmentMetricLogger() override;
+
+  // NearbySharingService::Observer
+  void OnTransferCompleted(const ShareTarget& share_target,
+                           TransferMetadata::Status status) override;
+};
+
+}  // namespace nearby::share::metrics
+
+#endif  // CHROME_BROWSER_NEARBY_SHARING_METRICS_ATTACHMENT_METRIC_LOGGER_H_
--- chrome/browser/nearby_sharing/nearby_sharing_service_impl.h
+++ chrome/browser/nearby_sharing/nearby_sharing_service_impl.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/nearby_sharing/incoming_frames_reader.h"
 #include "chrome/browser/nearby_sharing/incoming_share_target_info.h"
 #include "chrome/browser/nearby_sharing/local_device_data/nearby_share_local_device_data_manager.h"
+#include "chrome/browser/nearby_sharing/metrics/attachment_metric_logger.h"
 #include "chrome/browser/nearby_sharing/metrics/discovery_metric_logger.h"
 #include "chrome/browser/nearby_sharing/metrics/throughput_metric_logger.h"
 #include "chrome/browser/nearby_sharing/nearby_file_handler.h"
@@ -620,6 +621,8 @@ class NearbySharingServiceImpl
       discovery_metric_logger_;
   std::unique_ptr<nearby::share::metrics::ThroughputMetricLogger>
       throughput_metric_logger_;
+  std::unique_ptr<nearby::share::metrics::AttachmentMetricLogger>
+      attachment_metric_logger_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
