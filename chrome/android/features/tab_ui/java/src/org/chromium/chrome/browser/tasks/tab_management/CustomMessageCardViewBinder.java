2fcbb9db5fe7864e045c910fc68af64ac894fb15
Brandon Fong <bjfong@google.com>
2023-12-01 23:25:46 +0000

    [Tab Declutter] Adjust model properties for custom message card view
    Adjust the base properties of the custom message card view class.
    
    Bug: 1487664
    Change-Id: I03fd7d9812a86542eb22c9e587bf24f37dc2f5ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5080598
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1232249}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/CustomMessageCardViewBinder.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/CustomMessageCardViewBinder.java
@@ -28,9 +28,8 @@ public class CustomMessageCardViewBinder {
                     model.get(CustomMessageCardViewProperties.MESSAGE_CARD_VIEW));
         } else if (CARD_ALPHA == propertyKey) {
             viewHolder.mContentView.setAlpha(model.get(CARD_ALPHA));
-        } else if (CustomMessageCardViewProperties.IS_INCOGNITO == propertyKey) {
-            viewHolder.mProvider.setIsIncognito(
-                    model.get(CustomMessageCardViewProperties.IS_INCOGNITO));
+        } else if (MessageCardViewProperties.IS_INCOGNITO == propertyKey) {
+            viewHolder.mProvider.setIsIncognito(model.get(MessageCardViewProperties.IS_INCOGNITO));
         }
     }
 }
