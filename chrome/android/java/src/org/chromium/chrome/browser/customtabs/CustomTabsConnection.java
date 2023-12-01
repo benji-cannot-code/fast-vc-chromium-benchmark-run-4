eaaf69cf1ee53b4090761f9f8e938ad1d7221c1d
Jinsuk Kim <jinsukkim@chromium.org>
2023-12-01 00:19:30 +0000

    [CCT] 1P APIs as deprecated by default
    In preparation of deleting some 1P APIs marked as deprecated,
    this CL lets the feature flag on public repo be true by default.
    
    Bug: 1474699
    Change-Id: I11d0573aa100118ebfe9c440086135d712d51b4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5076306
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Kevin Grosu <kgrosu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1231631}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsConnection.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsConnection.java
@@ -2020,7 +2020,7 @@ public class CustomTabsConnection {
             ArrayList<String> foundTextFragments) {}
 
     protected boolean isCCTAPIDeprecated(String featureParamName) {
-        return false;
+        return true;
     }
 
     /**
