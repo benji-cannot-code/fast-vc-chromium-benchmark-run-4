263e7030787a94eeb80cb0dc1f25e6bc9b188a9d
Alexander Hendrich <hendrich@chromium.org>
2023-12-01 13:06:15 +0000

    Revert "[Waffle] Reprompting logic for search engine choice screen"
    This reverts commit a5cb9ffa83ddc18695fe6c2fbf431905255d538d.
    
    Reason for revert: fails FirstRunParameterizedInteractiveUiTest on multiple bots, see FirstRunParameterizedInteractiveUiTest
    
    Original change's description:
    > [Waffle] Reprompting logic for search engine choice screen
    >
    > Pass a JSON dictionary through Finch, where keys are country codes,
    > and values are Chrome version strings.
    > The wildcard country '*' represents all countries.
    >
    > Example: {'*': '2.0.0.0', 'BE': '5.0.0.0'}
    > This reprompts users in Belgium who made the choice strictly before version
    > 5, and users in all other countries who made the choice strictly before
    > version 2.
    >
    > Note: this has no effect for users with the parameter
    > `kSearchEngineChoiceTriggerForTaggedProfilesOnly` set to `true`.
    >
    > The reprompting is done by wiping the search engine choice prefs.
    >
    > Bug: b/312691477
    > Change-Id: I317c74fd5be43a31ef0942882a3aed5e45d5721c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5057564
    > Reviewed-by: Chris Thompson <cthomp@chromium.org>
    > Reviewed-by: Jack Yammine <jyammine@google.com>
    > Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    > Commit-Queue: David Roger <droger@chromium.org>
    > Reviewed-by: Orin Jaworski <orinj@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1231860}
    
    Bug: b/312691477
    Change-Id: I32ec464c81405ff7b2fda08acd60f3217fcdd240
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5077492
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Alexander Hendrich <hendrich@chromium.org>
    Auto-Submit: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231883}

--- components/search_engines/search_engines_switches.cc
+++ components/search_engines/search_engines_switches.cc
@@ -31,9 +31,4 @@ BASE_FEATURE(kSearchEngineChoiceTrigger,
 const base::FeatureParam<bool> kSearchEngineChoiceTriggerForTaggedProfilesOnly{
     &kSearchEngineChoiceTrigger, /*name=*/"for_tagged_profiles_only", false};
 
-const base::FeatureParam<std::string> kSearchEngineChoiceTriggerRepromptParams{
-    &kSearchEngineChoiceTrigger,
-    /*name=*/"reprompt",
-    /*default_value=*/"{}"};
-
 }  // namespace switches
