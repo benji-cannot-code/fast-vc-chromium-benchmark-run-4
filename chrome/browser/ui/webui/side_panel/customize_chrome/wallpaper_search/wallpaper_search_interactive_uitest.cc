9f02aa60f11726780dac9aa2d13a1ed4d7509285
Paul Adedeji <pauladedeji@google.com>
2023-12-01 18:21:50 +0000

    Hide wallpaper search if gm3 is disabled.
    Change-Id: I20b4cd6329874f1ed599d0d6072b4a0d305bb1b4
    Bug: 314196605
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5078449
    Auto-Submit: Paul Adedeji <pauladedeji@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232038}

--- chrome/browser/ui/webui/side_panel/customize_chrome/wallpaper_search/wallpaper_search_interactive_uitest.cc
+++ chrome/browser/ui/webui/side_panel/customize_chrome/wallpaper_search/wallpaper_search_interactive_uitest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/url_loader_interceptor.h"
 #include "ui/base/interaction/element_identifier.h"
 #include "ui/base/interaction/interaction_sequence.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/gfx/codec/png_codec.h"
 #include "ui/views/interaction/interaction_test_util_views.h"
 
@@ -41,7 +42,9 @@ class WallpaperSearchInteractiveTest : public InteractiveBrowserTest {
     scoped_feature_list_.InitWithFeatures(
         /*enabled_features=*/{ntp_features::kCustomizeChromeWallpaperSearch,
                               optimization_guide::features::
-                                  kOptimizationGuideModelExecution},
+                                  kOptimizationGuideModelExecution,
+                              features::kChromeRefresh2023,
+                              features::kChromeWebuiRefresh2023},
         /*disabled_features=*/{});
     InteractiveBrowserTest::SetUp();
   }
