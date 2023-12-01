7b25516d785814799217511b80f89a79a9b5ce23
danakj <danakj@chromium.org>
2023-12-01 20:15:01 +0000

    Make license paths absolute and avoid wildcards
    The license checker in //tools/licenses/licences.py requires paths to
    be absolute from the source root or relative from the README.chromium
    file's directory. The checker also does not handle wildcards, but it
    accepts a comma-separated list.
    
    So make gnrt vendor create license file paths in this format, and
    change the `license_file` config option to `license_files` instead
    for crates with more than one file to specify.
    
    Verify there are no errors by building the GN target:
      components/cronet:generate_license
    
    R=lukasza@chromium.org
    
    Bug: 1494118
    Change-Id: Ib53a930b9d983527d1cfa8f309ca1db99146cabf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5067239
    Commit-Queue: danakj <danakj@chromium.org>
    Auto-Submit: danakj <danakj@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232133}

--- tools/crates/gnrt/lib/config.rs
+++ tools/crates/gnrt/lib/config.rs
@@ -110,5 +110,5 @@ pub struct CrateConfig {
     #[serde(default)]
     pub license: Option<String>,
     #[serde(default)]
-    pub license_file: Option<String>,
+    pub license_files: Vec<String>,
 }
