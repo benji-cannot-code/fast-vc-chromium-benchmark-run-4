afbc68d7c54597e99f69fb7181eaaf42cf7b68ca
Wang Ningyuan <ningyuan@google.com>
2023-12-01 06:27:51 +0000

    arc: forward all patchpanel devices to ActiveNetworksChanged
    This Changes the behavior of net.mojom::ActiveNetworksChanged: In
    addition to active networks, virtual devices from with an associated
    physical interface, but no active network on it are also forwarded.
    
    BUG=b:308365031
    TEST=autoninja -C out/Default ash_components_unittests &&
    out/Default/ash_components_unittests --gtest_filter="*ArcNetUtils*"
    TEST=manual test checking inactive interface reported to ARC
    TEST=manual test checking host VPN forwarded to ARC
    
    Change-Id: Ib14a04764684e749dc45b02a8741a56c0b7d9962
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029623
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Commit-Queue: Ningyuan Wang <ningyuan@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231761}

--- ash/components/arc/net/arc_net_host_impl.h
+++ ash/components/arc/net/arc_net_host_impl.h
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_checker.h"
 #include "base/values.h"
 #include "chromeos/ash/components/dbus/patchpanel/patchpanel_client.h"
-#include "chromeos/ash/components/dbus/patchpanel/patchpanel_service.pb.h"
 #include "chromeos/ash/components/network/network_connection_observer.h"
 #include "chromeos/ash/components/network/network_profile_handler.h"
 #include "chromeos/ash/components/network/network_state_handler_observer.h"
@@ -124,7 +123,7 @@ class ArcNetHostImpl : public KeyedService,
 
  private:
   const ash::NetworkState* GetDefaultNetworkFromChrome();
-  void UpdateActiveNetworks(
+  void UpdateHostNetworks(
       const std::vector<patchpanel::NetworkDevice>& devices);
 
   // Due to a race in Chrome, GetNetworkStateFromGuid() might not know about
