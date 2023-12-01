ddac7c4221af9176cfed23d30f9f991552f1aaef
Jinli Wu <wujinli.cn@gmail.com>
2023-12-01 04:15:10 +0000

    cc: Update the relationship between the current_rect and tiling_size when tiling_size changes
    |has_xxx_rect_tiles_| refers to whether current_rect and tiling_size overlap.
    Once tiling_size changes, it also needs to be recalculated.
    
    Bug: 1505318
    Change-Id: Ie1306f9409d3bcb57565c47e41b634805b1504f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5058117
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Auto-Submit: 吴金立 <wujinli.cn@gmail.com>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231731}

--- cc/tiles/picture_layer_tiling.h
+++ cc/tiles/picture_layer_tiling.h
@@ -512,6 +512,8 @@ class CC_EXPORT PictureLayerTiling {
   gfx::Rect EnclosingLayerRectFromContentsRect(
       const gfx::Rect& contents_rect) const;
 
+  void SetTilingSize(const gfx::Size& tiling_size);
+
   // Given properties.
   const gfx::AxisTransform2d raster_transform_;
   const raw_ptr<PictureLayerTilingClient> client_;
