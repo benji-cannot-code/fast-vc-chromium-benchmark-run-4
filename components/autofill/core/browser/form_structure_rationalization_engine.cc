8d2d0515a2abe244dd54b78bd9aa2092ae53422f
Dominic Battre <battre@chromium.org>
2023-12-01 15:59:25 +0000

    Evaluate Field condition
    This CL introduces the evaluation of field conditions for a new
    declarative form structure rationalization engine.
    
    Field conditions define requirements that fields need to meet to
    be eligible for rationalization.
    
    Bug: 1505775
    Change-Id: Ice0756176f76fbe417fda30c15d2bde441b70283
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5076864
    Commit-Queue: Dominic Battre <battre@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1231948}

--- components/autofill/core/browser/form_structure_rationalization_engine.cc
+++ components/autofill/core/browser/form_structure_rationalization_engine.cc
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/feature_list.h"
 #include "base/no_destructor.h"
+#include "components/autofill/core/browser/form_parsing/form_field.h"
+#include "components/autofill/core/browser/form_parsing/regex_patterns.h"
+#include "components/autofill/core/common/form_field_data.h"
 
 namespace autofill::rationalization {
 
@@ -125,6 +128,28 @@ bool IsEnvironmentConditionFulfilled(const EnvironmentCondition& env,
 
   if (env.feature && !base::FeatureList::IsEnabled(*env.feature)) {
     return false;
+  }
+
+  return true;
+}
+
+bool IsFieldConditionFulfilledIgnoringLocation(
+    const FieldCondition& condition,
+    const LanguageCode& page_language,
+    PatternSource pattern_source,
+    const AutofillField& field) {
+  if (condition.possible_overall_types.has_value() &&
+      !condition.possible_overall_types->contains(
+          field.Type().GetStorableType())) {
+    return false;
+  }
+
+  if (condition.regex_reference_match.has_value()) {
+    base::span<const MatchPatternRef> patterns = GetMatchPatterns(
+        condition.regex_reference_match.value(), page_language, pattern_source);
+    if (!FormField::FieldMatchesMatchPatternRef(patterns, field)) {
+      return false;
+    }
   }
 
   return true;
