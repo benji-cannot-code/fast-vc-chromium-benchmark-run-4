c51d78535403e60c4b66ab53e83ed106737213e1
Andrew Bregger <andrewbregger@google.com>
2023-12-01 21:08:11 +0000

    SM: Fixing an mis-classification of CrOS events
    CrOS Events were not being classified as a profile event because it is
    set as a device event in the xml.
    
    NO_BUG
    
    Change-Id: Ifd8a485dc267886c7dd766152b6b9a0b9f3fac74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5080415
    Commit-Queue: Andrew Bregger <andrewbregger@google.com>
    Commit-Queue: Jong Ahn <jongahn@chromium.org>
    Reviewed-by: Jong Ahn <jongahn@chromium.org>
    Auto-Submit: Andrew Bregger <andrewbregger@google.com>
    Cr-Commit-Position: refs/heads/main@{#1232164}

--- components/metrics/structured/structured_metrics_recorder.cc
+++ components/metrics/structured/structured_metrics_recorder.cc
@@ -466,7 +466,9 @@ bool StructuredMetricsRecorder::IsDeviceEvent(const Event& event) const {
   }
   const auto* project_validator = validators->first;
 
-  return project_validator->id_scope() == IdScope::kPerDevice;
+  // Sequence events are marked as per-device but use the profile keys.
+  return !event.IsEventSequenceType() &&
+         project_validator->id_scope() == IdScope::kPerDevice;
 }
 
 bool StructuredMetricsRecorder::IsProfileEvent(const Event& event) const {
@@ -478,7 +480,9 @@ bool StructuredMetricsRecorder::IsProfileEvent(const Event& event) const {
   }
   const auto* project_validator = validators->first;
 
-  return project_validator->id_scope() == IdScope::kPerProfile;
+  // Sequence events are marked as per-device but use the profile keys.
+  return event.IsEventSequenceType() ||
+         project_validator->id_scope() == IdScope::kPerProfile;
 }
 
 bool StructuredMetricsRecorder::CanProvideMetrics() {
