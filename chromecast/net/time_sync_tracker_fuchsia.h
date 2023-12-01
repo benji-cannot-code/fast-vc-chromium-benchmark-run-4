5f13249d8be11bbedcab7aea5259196f10a8905b
Haines Sy <hainesy@google.com>
2023-12-01 23:53:19 +0000

    Reland "[fuchsia] Use UTC_CLOCK_SYNCHRONIZED to wait for time sync"
    This reverts commit 0979d96f5e12cd5f1ed5579c5bfcef3be72aeb32.
    
    Reason for revert: fix typo on include
    Bug: crbug.com/1507157
    
    Original change's description:
    > Revert "[fuchsia] Use UTC_CLOCK_SYNCHRONIZED to wait for time sync"
    >
    > This reverts commit e00a329f0e349d780010a514b468411bc24272ce.
    >
    > Reason for revert:
    > LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8762872473201876817
    >
    > Sample failed build: https://ci.chromium.org/b/8762872473201876817
    >
    > If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8762872473201876817&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F5069690
    >
    > Original change's description:
    > > [fuchsia] Use UTC_CLOCK_SYNCHRONIZED to wait for time sync
    > >
    > > Bug: 312747122
    > > Test: Manual
    > > Change-Id: I7852609b1d1b8c764392736286494d52040b57e3
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5069690
    > > Reviewed-by: Zijie He <zijiehe@google.com>
    > > Commit-Queue: Haines Sy <hainesy@google.com>
    > > Reviewed-by: David Song <wintermelons@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1232214}
    > >
    >
    > Bug: 312747122
    > Change-Id: I3abd454dd729c7009d827603fba8a1bceee14903
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5079728
    > Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1232232}
    
    Bug: 312747122
    Change-Id: I7938e2be5da65887c74f1c2429bed790a7f638de
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5080959
    Reviewed-by: David Song <wintermelons@google.com>
    Reviewed-by: Zijie He <zijiehe@google.com>
    Commit-Queue: Haines Sy <hainesy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1232262}

--- chromecast/net/time_sync_tracker_fuchsia.h
+++ chromecast/net/time_sync_tracker_fuchsia.h
@@ -1,11 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2020 The Chromium Authors
+// Copyright 2020 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #ifndef CHROMECAST_NET_TIME_SYNC_TRACKER_FUCHSIA_H_
 #define CHROMECAST_NET_TIME_SYNC_TRACKER_FUCHSIA_H_
 
+#include <lib/async/cpp/wait.h>
 #include <lib/zx/clock.h>
 
 #include "base/memory/scoped_refptr.h"
@@ -31,12 +32,9 @@ class TimeSyncTrackerFuchsia : public TimeSyncTracker {
   bool IsTimeSynced() const final;
 
  private:
-  void Poll();
-
   const scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
   zx::unowned_clock utc_clock_;
-
-  bool is_polling_ = false;
+  std::unique_ptr<async::WaitOnce> wait_for_utc_;
   bool is_time_synced_ = false;
 
   base::WeakPtr<TimeSyncTrackerFuchsia> weak_this_;
