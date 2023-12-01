47301d9051d6f7b0c9f5c322e8e615d7dd9c2d05
James Cook <jamescook@chromium.org>
2023-12-01 18:35:22 +0000

    chromeos: Add extra file manager JS test coverage for deletion cases
    As part of the go/crosier effort we're removing Tast tests from the
    chromium commit queue. Per go/crosier-conversion we're either porting
    these tests to C++ GTest tests in the chromium tree or we're extending
    existing tests. This CL extends existing browser_tests integration test
    coverage of the file manager to cover some extra cases from the Tast
    test filemanager.Deletion.
    
    Bug: b:314192562
    Test: browser_tests --gtest_filter=*FilesAppBrowserTest*trash*
    Change-Id: I9df2c302975f227934ee9f1a4af987af76af521d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5068600
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232053}

--- ui/file_manager/integration_tests/file_manager/breadcrumbs.js
+++ ui/file_manager/integration_tests/file_manager/breadcrumbs.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Tests that breadcrumbs work.
  */
 
-import {ENTRIES, EntryType, getCaller, getUserActionCount, pending, repeatUntil, RootPath, sendTestMessage, TestEntryInfo} from '../test_util.js';
+import {createNestedTestFolders, ENTRIES, EntryType, getCaller, getUserActionCount, pending, repeatUntil, RootPath, sendTestMessage, TestEntryInfo} from '../test_util.js';
 import {testcase} from '../testcase.js';
 
 import {remoteCall, setupAndWaitUntilReady} from './background.js';
@@ -67,46 +67,6 @@ testcase.breadcrumbsDownloadsTranslation = async () => {
   await remoteCall.waitUntilCurrentDirectoryIsChanged(
       appId, '/Os meus ficheiros/Transferências/photos');
 };
-
-/**
- * Creates a folder test entry from a folder |path|.
- * @param {string} path The folder path.
- * @return {!TestEntryInfo}
- */
-function createTestFolder(path) {
-  const name = path.split('/').pop();
-  return new TestEntryInfo({
-    targetPath: path,
-    nameText: name,
-    type: EntryType.DIRECTORY,
-    lastModifiedTime: 'Jan 1, 1980, 11:59 PM',
-    sizeText: '--',
-    typeText: 'Folder',
-  });
-}
-
-/**
- * Returns an array of nested folder test entries, where |depth| controls
- * the nesting. For example, a |depth| of 4 will return:
- *
- *   [0]: nested-folder0
- *   [1]: nested-folder0/nested-folder1
- *   [2]: nested-folder0/nested-folder1/nested-folder2
- *   [3]: nested-folder0/nested-folder1/nested-folder2/nested-folder3
- *
- * @param {number} depth The nesting depth.
- * @return {!Array<!TestEntryInfo>}
- */
-function createNestedTestFolders(depth) {
-  const nestedFolderTestEntries = [];
-
-  for (let path = 'nested-folder0', i = 0; i < depth; ++i) {
-    nestedFolderTestEntries.push(createTestFolder(path));
-    path += `/nested-folder${i + 1}`;
-  }
-
-  return nestedFolderTestEntries;
-}
 
 /**
  * Tests that the breadcrumbs correctly render a short (3 component) path.
