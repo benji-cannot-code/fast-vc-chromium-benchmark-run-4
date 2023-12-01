515e1bbe54c3b5e292e160c54a7df7449e7a057e
Jan Keitel <jkeitel@google.com>
2023-12-01 15:15:16 +0000

    Add a separate AutofillSessionState metric for bottom sheet sessions.
    This CL introduces a Autofill.WebView.AutofillSessionWithBottomSheet
    UMA that is recorded whenever an Autofill session started with
    showing a bottom sheet. The existing "*.AutofillSession" metric
    will continue to be emitted in these cases.
    
    The purpose of this metric is to understand whether there is any
    change in session states if a bottom sheet was shown - e.g., whether
    there are fewer or more cases in which a user submits the form,
    makes a manual change to a field, etc.
    
    Bug: 1502097
    Change-Id: Ifd35f7e0339aa2b6711c980c191c2fd84f3e7fea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5076706
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1231928}

--- components/android_autofill/browser/java/src/org/chromium/components/autofill/AutofillProvider.java
+++ components/android_autofill/browser/java/src/org/chromium/components/autofill/AutofillProvider.java
@@ -160,6 +160,7 @@ public class AutofillProvider {
         FormData form;
         if (mRequest != null) {
             form = mRequest.getForm();
+            mAutofillUMA.onVirtualStructureProvided();
         } else {
             form = mPrefillRequest.getForm();
             mStructureProvidedForPrefillRequest = true;
@@ -169,7 +170,6 @@ public class AutofillProvider {
             AutofillManagerWrapper.log(
                     "onProvideAutoFillVirtualStructure fields:" + structure.getChildCount());
         }
-        mAutofillUMA.onVirtualStructureProvided();
     }
 
     /**
@@ -266,13 +266,15 @@ public class AutofillProvider {
         Rect absBound = transformToWindowBounds(new RectF(x, y, x + width, y + height));
         if (mRequest != null) notifyViewExitBeforeDestroyRequest();
         transformFormFieldToContainViewCoordinates(formData);
+        mAutofillUMA.onSessionStarted(mAutofillManager.isDisabled());
         mRequest =
                 new AutofillRequest(
                         formData, new FocusField((short) focus, absBound), hasServerPrediction);
-        if (!maybeShowBottomSheet(focus)) {
+        if (maybeShowBottomSheet(focus)) {
+            mAutofillUMA.onBottomSheetShown();
+        } else {
             notifyVirtualViewEntered(mContainerView, focus, absBound);
         }
-        mAutofillUMA.onSessionStarted(mAutofillManager.isDisabled());
         if (hasServerPrediction) {
             mAutofillUMA.onServerTypeAvailable(formData, /* afterSessionStarted= */ false);
         }
