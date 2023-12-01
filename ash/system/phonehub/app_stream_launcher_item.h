ff5fb0064d602b27b5071543d82dd12c7b05fd34
Daniel Cheng <dcheng@chromium.org>
2023-12-01 23:16:32 +0000

    Use a repeating callback for AppStreamLauncherItem's pressed callback.
    PressedCallback will become move-only when it supports OnceCallbacks.
    Since AppStreamLauncherItem wants to use the same callback for the
    button and label, and it doesn't care about the `ui::Event`, just pass a
    RepeatingClosure through to be shared instead.
    
    Bug: 1326449
    Change-Id: Ic019752bf19047eddd1145378b13f89b1128e920
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5078790
    Reviewed-by: Pu Shi <pushi@google.com>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232238}

--- ash/system/phonehub/app_stream_launcher_item.h
+++ ash/system/phonehub/app_stream_launcher_item.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/system/phonehub/phone_hub_recent_app_button.h"
+#include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "chromeos/ash/components/phonehub/notification.h"
 #include "ui/base/metadata/metadata_header_macros.h"
@@ -26,7 +27,7 @@ class ASH_EXPORT AppStreamLauncherItem : public views::View {
 
  public:
   AppStreamLauncherItem(
-      views::ImageButton::PressedCallback callback,
+      base::RepeatingClosure callback,
       const phonehub::Notification::AppMetadata& app_metadata);
 
   ~AppStreamLauncherItem() override;
