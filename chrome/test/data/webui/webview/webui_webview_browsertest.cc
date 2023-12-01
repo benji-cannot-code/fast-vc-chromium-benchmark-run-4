c2ac1ab9b3bded67bc397d8f061318592cb4f54e
Keren Zhu <kerenzhu@chromium.org>
2023-12-01 22:13:54 +0000

    gardender: deflake WebUIWebViewCoverageDisabledBrowserTest.AddContentScriptsWithNewWindowAPI
    This test is flaky in test suite bf_cache_browser_tests, where BFCache
    is disabled.
    
    Bug: 1506989
    Change-Id: Ic1cd17f89cac3dcca779eff5b7946bfcbd2cb563
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5075161
    Auto-Submit: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232204}

--- chrome/test/data/webui/webview/webui_webview_browsertest.cc
+++ chrome/test/data/webui/webview/webui_webview_browsertest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/test_switches.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "chrome/test/base/web_ui_mocha_browser_test.h"
+#include "content/public/browser/back_forward_cache.h"
 #include "content/public/browser/context_menu_params.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/render_widget_host.h"
@@ -194,6 +195,12 @@ class WebUIWebViewCoverageDisabledBrowserTest : public WebUIWebViewBrowserTest {
 #endif
 IN_PROC_BROWSER_TEST_F(WebUIWebViewCoverageDisabledBrowserTest,
                        MAYBE_AddContentScriptsWithNewWindowAPI) {
+  if (!content::BackForwardCache::IsBackForwardCacheFeatureEnabled()) {
+    // The case below currently is flaky on the linux-bfcache-rel bot with
+    // back/forward cache disabled, so return early.
+    // TODO(https://crbug.com/1506989): re-enable this test.
+    return;
+  }
   ASSERT_TRUE(RunTestCase("AddContentScriptsWithNewWindowAPI",
                           GetTestUrl("guest_from_opener.html").spec()));
 }
