1bb7857432276ff90bcf9abe72ee705407e209c8
Orin Jaworski <orinj@google.com>
2023-12-01 18:59:11 +0000

    [omnibox][actions] Add test coverage & bug fix for keyword mode refresh
    This CL does two things:
    - Adds a unit test to cover space triggering of keyword mode.
    - Fixes a bug in space triggering by respecting the user preference.
    
    Bug: 1495562
    Change-Id: I12222da642fc9fbd44efc4e7d7d3dc56c271c4c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5074839
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jennifer Serrano <jennserrano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1232077}

--- chrome/browser/ui/views/omnibox/omnibox_popup_view_views_test.h
+++ chrome/browser/ui/views/omnibox/omnibox_popup_view_views_test.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_OMNIBOX_OMNIBOX_POPUP_VIEW_VIEWS_TEST_H_
 #define CHROME_BROWSER_UI_VIEWS_OMNIBOX_OMNIBOX_POPUP_VIEW_VIEWS_TEST_H_
 
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/autocomplete/chrome_autocomplete_scheme_classifier.h"
 #include "chrome/browser/themes/test/theme_service_changed_waiter.h"
 #include "chrome/browser/themes/theme_service.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/in_process_browser_test.h"
 #include "components/omnibox/browser/omnibox_controller.h"
 #include "components/omnibox/browser/omnibox_edit_model.h"
+#include "components/omnibox/common/omnibox_features.h"
 #include "ui/views/widget/widget.h"
 
 // Base class for omnibox browser and ui tests.
@@ -100,6 +102,8 @@ class OmniboxPopupViewViewsTest : public InProcessBrowserTest {
 
  private:
   OmniboxTriggeredFeatureService triggered_feature_service_;
+  base::test::ScopedFeatureList feature_list_{
+      omnibox::kOmniboxKeywordModeRefresh};
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_OMNIBOX_OMNIBOX_POPUP_VIEW_VIEWS_TEST_H_
