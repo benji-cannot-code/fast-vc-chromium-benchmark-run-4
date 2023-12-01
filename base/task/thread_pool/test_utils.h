00653ed8ba2bcad92ee1c01fe20c36e8f5d6152a
Daniel Cheng <dcheng@chromium.org>
2023-12-01 01:18:50 +0000

    Remove base::Passed usage from MockJobTask.
    MockJobTask supports construction from a OnceCallback or a
    RepeatingCallback. Internally, it stored a RepeatingCallback, and uses
    base::Passed() to adapt a OnceCallback into a RepeatingCallback.
    However:
    
    - base::Passed() is a pre-C++11-ism that has been replaced by
      std::move() for many years now.
    - Using base::Passed() to use a move-only type with a RepeatingCallback
      can have unintuitive effects if the callback is run more than
      once—subsequent invocations of the callback after the first will
      receive a moved-from value.
    
    For these reasons, base::Passed() is being removed from the codebase.
    MockJobTask now stores a variant of OnceCallback+RepeatingCallback, but
    all other behaviour remains the same as before. Attempting to run the
    OnceCallback variant more than once will simply fail (as it would have
    before).
    
    Bug: 1326449
    Change-Id: I5c0ccd30b67cf88e340a8c99fe832b72043b3983
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5066870
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231678}

--- base/task/thread_pool/test_utils.h
+++ base/task/thread_pool/test_utils.h
@@ -22,9 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool/task_tracker.h"
 #include "base/task/thread_pool/thread_group.h"
 #include "base/task/thread_pool/worker_thread_observer.h"
-#include "base/thread_annotations.h"
 #include "build/build_config.h"
 #include "testing/gmock/include/gmock/gmock.h"
+#include "third_party/abseil-cpp/absl/types/variant.h"
 
 namespace base {
 namespace internal {
@@ -87,7 +87,7 @@ class MockPooledTaskRunnerDelegate : public PooledTaskRunnerDelegate {
 class MockJobTask : public base::RefCountedThreadSafe<MockJobTask> {
  public:
   // Gives |worker_task| to requesting workers |num_tasks_to_run| times.
-  MockJobTask(base::RepeatingCallback<void(JobDelegate*)> worker_task,
+  MockJobTask(RepeatingCallback<void(JobDelegate*)> worker_task,
               size_t num_tasks_to_run);
 
   // Gives |worker_task| to a single requesting worker.
@@ -98,9 +98,7 @@ class MockJobTask : public base::RefCountedThreadSafe<MockJobTask> {
 
   // Updates the remaining number of time |worker_task| runs to
   // |num_tasks_to_run|.
-  void SetNumTasksToRun(size_t num_tasks_to_run) {
-    remaining_num_tasks_to_run_ = num_tasks_to_run;
-  }
+  void SetNumTasksToRun(size_t num_tasks_to_run);
 
   size_t GetMaxConcurrency(size_t worker_count) const;
   void Run(JobDelegate* delegate);
@@ -115,7 +113,7 @@ class MockJobTask : public base::RefCountedThreadSafe<MockJobTask> {
 
   ~MockJobTask();
 
-  base::RepeatingCallback<void(JobDelegate*)> worker_task_;
+  absl::variant<OnceClosure, RepeatingCallback<void(JobDelegate*)>> task_;
   std::atomic_size_t remaining_num_tasks_to_run_;
 };
 
