fb7ea72bd795c2b47b8ff17de5c0b13022fea5d8
Daniel Brinkers <brinky@chromium.org>
2023-12-01 09:56:01 +0000

    dlp: call CreateFileAccessCallback from UI thread
    BUG=b:313426297
    
    Change-Id: I1b34917f037044f52eda070424a896093465d143
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5062799
    Commit-Queue: Daniel Brinkers <brinky@google.com>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Luca Accorsi <accorsi@google.com>
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231829}

--- content/browser/file_system/file_system_url_loader_factory.cc
+++ content/browser/file_system/file_system_url_loader_factory.cc
@@ -147,6 +147,15 @@ class FileSystemEntryURLLoader
       net_error = net::ERR_INVALID_URL;
     }
 
+    if (request.request_initiator &&
+        file_access::ScopedFileAccessDelegate::HasInstance()) {
+      file_access_ =
+          file_access::ScopedFileAccessDelegate::RequestFilesAccessIOCallback(
+              file_access::ScopedFileAccessDelegate::Get()
+                  ->CreateFileAccessCallback(
+                      request.request_initiator->GetURL()));
+    }
+
     io_task_runner->PostTask(
         FROM_HERE, base::BindOnce(&FileSystemEntryURLLoader::StartOnIOThread,
                                   AsWeakPtr(), request, net_error,
@@ -192,15 +201,6 @@ class FileSystemEntryURLLoader
         &FileSystemEntryURLLoader::OnMojoDisconnect, base::Unretained(this)));
 
     client_.Bind(std::move(client_remote));
-
-    if (request.request_initiator &&
-        file_access::ScopedFileAccessDelegate::HasInstance()) {
-      file_access_ =
-          file_access::ScopedFileAccessDelegate::RequestFilesAccessIOCallback(
-              file_access::ScopedFileAccessDelegate::Get()
-                  ->CreateFileAccessCallback(
-                      request.request_initiator->GetURL()));
-    }
 
     // If checks which were performed on the UI thread failed, don't proceed
     // any further and error out.
