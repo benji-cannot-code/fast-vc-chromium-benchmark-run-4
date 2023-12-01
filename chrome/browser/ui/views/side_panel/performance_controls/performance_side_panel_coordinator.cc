7aa93ab6ed3960052742b211c8714b9e299a756e
Alison Gale <agale@chromium.org>
2023-12-01 19:41:53 +0000

    Add interactive ui test for opening the performance side panel
    Bug: 1502317
    Change-Id: Ia63376b6d5a073e4f308f02bda8fe9640709f1e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5076522
    Reviewed-by: Charles Meng <charlesmeng@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232114}

--- chrome/browser/ui/views/side_panel/performance_controls/performance_side_panel_coordinator.cc
+++ chrome/browser/ui/views/side_panel/performance_controls/performance_side_panel_coordinator.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_content_proxy.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_coordinator.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/models/image_model.h"
 #include "ui/views/vector_icons.h"
+#include "ui/views/view_class_properties.h"
 
 using SidePanelWebUIViewT_PerformanceSidePanelUI =
     SidePanelWebUIViewT<PerformanceSidePanelUI>;
@@ -69,6 +71,8 @@ PerformanceSidePanelCoordinator::CreatePerformanceWebUIView() {
       base::RepeatingClosure(), base::RepeatingClosure(), std::move(wrapper));
   view->SetVisible(true);
   SidePanelUtil::GetSidePanelContentProxy(view.get())->SetAvailable(true);
+  view->SetProperty(views::kElementIdentifierKey,
+                    kPerformanceSidePanelWebViewElementId);
   return view;
 }
 
