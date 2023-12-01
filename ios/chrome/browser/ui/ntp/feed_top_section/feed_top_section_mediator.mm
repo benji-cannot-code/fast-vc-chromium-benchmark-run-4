dc5c730038c7e9b5e90507dfea337b5fc84f8b8c
Guillem Perez <guiperez@google.com>
2023-12-01 20:29:11 +0000

    [iOS] Fix layout issue Top of Feed Promo.
    Added missing relayout method call removed by crrev.com/c/5031499 by error. Relocated to run only when the promo is closed by the X which is the only time where the NTP needs relayout.
    
    - Signin promo can only be dismissed by clicking X or Signing in, which reloads the NTP so there is no need for relayout.
    
    Change-Id: I511a62a873a2cad50db4558a9d9a2dd0a5b38ecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5074911
    Auto-Submit: Guillem Perez <guiperez@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Commit-Queue: Guillem Perez <guiperez@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1232144}

--- ios/chrome/browser/ui/ntp/feed_top_section/feed_top_section_mediator.mm
+++ ios/chrome/browser/ui/ntp/feed_top_section/feed_top_section_mediator.mm
@@ -118,6 +118,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     (SigninPromoViewMediator*)mediator {
   [self.NTPDelegate handleFeedTopSectionClosed];
   [self.consumer hidePromo];
+  [self.NTPDelegate updateFeedLayout];
 }
 
 #pragma mark - Private
