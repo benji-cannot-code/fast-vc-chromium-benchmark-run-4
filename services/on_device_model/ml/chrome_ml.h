19b877443e7f86e8619a110aacd9eb0b662a92fe
Clark DuVall <cduvall@chromium.org>
2023-12-01 00:22:56 +0000

    Adds GPU blocklist logic to performance estimator
    Change-Id: Iee4773c26020553cdde1ae89be1221fba205df97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5074902
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231637}

--- services/on_device_model/ml/chrome_ml.h
+++ services/on_device_model/ml/chrome_ml.h
@@ -33,6 +33,9 @@ class ChromeML {
   // Exposes the raw ChromeMLAPI functions defined by the library.
   const ChromeMLAPI& api() const { return *api_; }
 
+  // Whether or not the GPU is blocklisted.
+  bool IsGpuBlocked() const;
+
  private:
   static std::unique_ptr<ChromeML> Create();
 
