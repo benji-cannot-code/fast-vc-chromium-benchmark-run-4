40b3c7b2f13d727b26746c1272bd51b9f62b2999
Martin Verde <thesalsa@google.com>
2023-12-01 22:50:59 +0000

    Add an installer for the Anti-Fingerprinting Blocklist component.
    Registration of the installer is guarded by the
    kEnableNetworkServiceResourceBlockList feature so that users that enable it will
    load the blocklist. The blocklist is not actually used yet.
    
    R=aakallam@chromium.org, aakallam@google.com, rizvis@google.com, waffles@chromium.org
    
    Bug: 308781576
    Change-Id: I3dc4dd8a663dc0925bb2ac39886ac63738e2e201
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5063509
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Martin Verde <thesalsa@google.com>
    Reviewed-by: Alex Kallam <aakallam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232230}

--- chrome/browser/component_updater/registration.cc
+++ chrome/browser/component_updater/registration.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/component_updater/registration.h"
 
+#include "afp_blocked_domain_list_component_installer.h"
 #include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
@@ -40,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/component_updater/tpcd_metadata_component_installer.h"
 #include "chrome/browser/component_updater/trust_token_key_commitments_component_installer.h"
 #include "chrome/common/buildflags.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_paths.h"
 #include "components/component_updater/component_updater_service.h"
 #include "components/component_updater/installer_policies/autofill_states_component_installer.h"
@@ -141,6 +143,10 @@ void RegisterComponentsForUpdate() {
   RegisterFirstPartySetsComponent(cus);
   RegisterMaskedDomainListComponent(cus);
   RegisterPrivacySandboxAttestationsComponent(cus);
+  if (base::FeatureList::IsEnabled(
+          features::kEnableNetworkServiceResourceBlockList)) {
+    RegisterAntiFingerprintingBlockedDomainListComponent(cus);
+  }
 
   base::FilePath path;
   if (base::PathService::Get(chrome::DIR_USER_DATA, &path)) {
@@ -148,6 +154,13 @@ void RegisterComponentsForUpdate() {
 
     // Clean up any remaining desktop sharing hub state.
     component_updater::DeleteDesktopSharingHub(path);
+
+    // Clean up any existing versions of the blocklist if the feature is
+    // disabled.
+    if (!base::FeatureList::IsEnabled(
+            features::kEnableNetworkServiceResourceBlockList)) {
+      DeleteAntiFingerprintingBlockedDomainListComponent(path);
+    }
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
     if (base::SysInfo::IsRunningOnChromeOS()) {
