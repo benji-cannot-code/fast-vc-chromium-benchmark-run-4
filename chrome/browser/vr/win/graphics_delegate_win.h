c20d22b5320a0614d34f40084b9c472010a10d2d
Colin Blundell <blundell@chromium.org>
2023-12-01 11:31:59 +0000

    [//chrome] Eliminate GraphicsDelegateWin creating GMBHandle explicitly
    SII::CreateSharedImage(BufferUsage) now returns a ClientSharedImage from
    which the client can clone the underlying GMBHandle. This is all the
    functionality that GraphicsDelegateWin requires to avoid having to
    explicitly create the GMBHandle that it uses. This CL does that port.
    There are no functional changes.
    
    Bug: 1431340
    Change-Id: I1f1c4e26be8937908c31b36093be9afb008bb698
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5039491
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231853}

--- chrome/browser/vr/win/graphics_delegate_win.h
+++ chrome/browser/vr/win/graphics_delegate_win.h
@@ -10,12 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "chrome/browser/vr/graphics_delegate.h"
 #include "gpu/GLES2/gl2extchromium.h"
+#include "gpu/command_buffer/client/client_shared_image.h"
 #include "gpu/command_buffer/client/gles2_interface.h"
 #include "gpu/ipc/client/gpu_channel_host.h"
 #include "mojo/public/cpp/platform/platform_handle.h"
 #include "services/viz/public/cpp/gpu/context_provider_command_buffer.h"
 #include "ui/gfx/geometry/size.h"
-#include "ui/gfx/gpu_memory_buffer.h"
 
 namespace gpu {
 class SharedImageInterface;
@@ -50,10 +50,9 @@ class GraphicsDelegateWin : public GraphicsDelegate {
   raw_ptr<gpu::gles2::GLES2Interface> gl_ = nullptr;
   raw_ptr<gpu::SharedImageInterface> sii_ = nullptr;
   gfx::Size last_size_;
-  gpu::Mailbox mailbox_;  // Corresponding to our target GpuMemoryBuffer.
+  scoped_refptr<gpu::ClientSharedImage> client_shared_image_;
   GLuint dest_texture_id_ = 0;
   GLuint draw_frame_buffer_ = 0;
-  gfx::GpuMemoryBufferHandle buffer_handle_;
   // Sync point after access to the buffer is done.
   gpu::SyncToken access_done_sync_token_;
 };
