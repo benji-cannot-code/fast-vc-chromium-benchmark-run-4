5ed1f3cfdf871f927ad4168195c0d77824761d54
Fredrik Söderquist <fs@opera.com>
2023-12-01 01:49:24 +0000

    Move image-related accessors out of BackgroundImageGeometry
    Remove the ImageInterpolationQuality() and DynamicRangeLimit() accessors
    from BackgroundImageGeometry. They always use box_->StyleRef(), which is
    the same as BoxPainterBase::style_.
    
    Bug: 1500572
    Change-Id: I6c8b73cb859e230f58930630cdf9748ecbff2d4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5077017
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231686}

--- third_party/blink/renderer/core/paint/box_painter_base.cc
+++ third_party/blink/renderer/core/paint/box_painter_base.cc
@@ -1214,8 +1214,9 @@ void BoxPainterBase::PaintFillLayer(const PaintInfo& paint_info,
                                             gfx::SizeF(geometry.TileSize()));
 
     image_rendering_settings_context.emplace(
-        context, geometry.ImageInterpolationQuality(),
-        geometry.DynamicRangeLimit());
+        context, style_.GetInterpolationQuality(),
+        static_cast<cc::PaintFlags::DynamicRangeLimit>(
+            style_.DynamicRangeLimit()));
   }
 
   PhysicalBoxStrut border = ComputeSnappedBorders();
