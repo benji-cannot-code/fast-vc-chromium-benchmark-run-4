30375177115e56d0c7e1130a44b0584b0eb6bd89
Joe Mason <joenotcharles@google.com>
2023-12-02 00:01:31 +0000

    Don't test private_memory_footprint on iOS
    Reenable the test.
    
    R=vollick
    
    Bug: 1506552
    Change-Id: I47079b09d50641dae6133b4c02ea1313cee860e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5078439
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232267}

--- components/performance_manager/resource_attribution/memory_measurement_provider_browsertest.cc
+++ components/performance_manager/resource_attribution/memory_measurement_provider_browsertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/test/bind.h"
+#include "build/build_config.h"
 #include "components/performance_manager/public/graph/graph.h"
 #include "components/performance_manager/public/resource_attribution/query_results.h"
 #include "components/performance_manager/public/resource_attribution/resource_contexts.h"
@@ -89,9 +90,13 @@ auto MemorySummaryResultIsPositive() {
       Field("metadata", &MemorySummaryResult::metadata,
             Field("measurement_time", &ResultMetadata::measurement_time,
                   AllOf(Gt(base::TimeTicks()), Lt(base::TimeTicks::Now())))),
-      Field("resident_set_size_kb", &MemorySummaryResult::resident_set_size_kb,
-            Gt(0u)),
+#if BUILDFLAG(IS_IOS)
+  // iOS doesn't support private_memory_footprint, so it's always 0.
+#else
       Field("private_footprint_kb", &MemorySummaryResult::private_footprint_kb,
+            Gt(0u)),
+#endif
+      Field("resident_set_size_kb", &MemorySummaryResult::resident_set_size_kb,
             Gt(0u))));
 }
 
