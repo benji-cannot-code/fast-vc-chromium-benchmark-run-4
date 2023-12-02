0b9adaa2a06c60b6fdd7177e9552f0409aa3e603
Kurt Catti-Schmidt (SCHMIDT) <kschmi@microsoft.com>
2023-12-02 01:00:48 +0000

    [Text Contrast] Only querying registry values on Chromium side
    This change queries contrast and gamma values from DirectWrite via
    `IDWriteRenderingParams` and stores them in `FontRenderParams`.
    
    These values aren't currently used for anything yet, but will soon be
    plumbed into Skia.
    
    All new behaviors are behind the flag
    "kUseGammaContrastRegistrySettings". If this flag is disabled,
    `FontRenderParams` values for contrast and gamma are set to Skia
    defaults.
    
    Bug: 1160653
    Change-Id: I449d5922da31be6ae162e2da91e4c5a79f4abb08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5031537
    Auto-Submit: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1232285}

--- ui/gfx/font_render_params.h
+++ ui/gfx/font_render_params.h
@@ -21,6 +21,10 @@ namespace gfx {
 struct GFX_EXPORT FontRenderParams {
   bool operator==(const FontRenderParams& other) const {
     return antialiasing == other.antialiasing &&
+#if BUILDFLAG(IS_WIN)
+           text_contrast == other.text_contrast &&
+           text_gamma == other.text_gamma &&
+#endif  // #if BUILDFLAG(IS_WIN)
            subpixel_positioning == other.subpixel_positioning &&
            autohinter == other.autohinter && use_bitmaps == other.use_bitmaps &&
            hinting == other.hinting &&
@@ -72,6 +76,11 @@ struct GFX_EXPORT FontRenderParams {
   // subpixel order.
   SubpixelRendering subpixel_rendering = SUBPIXEL_RENDERING_NONE;
 
+#if BUILDFLAG(IS_WIN)
+  float text_contrast = SK_GAMMA_CONTRAST;
+  float text_gamma = SK_GAMMA_EXPONENT;
+#endif  // BUILDFLAG(IS_WIN)
+
   static SkPixelGeometry SubpixelRenderingToSkiaPixelGeometry(
       SubpixelRendering subpixel_rendering);
 };
@@ -110,7 +119,7 @@ GFX_EXPORT FontRenderParams GetFontRenderParams(
     const FontRenderParamsQuery& query,
     std::string* family_out);
 
-#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_WIN)
 // Clears GetFontRenderParams()'s cache. Intended to be called by tests that are
 // changing Fontconfig's configuration.
 GFX_EXPORT void ClearFontRenderParamsCacheForTest();
---
+++ ui/gfx/font_util_win.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_GFX_FONT_UTIL_WIN_H_
+#define UI_GFX_FONT_UTIL_WIN_H_
+
+#include "base/win/registry.h"
+
+namespace gfx {
+
+class FontUtilWin {
+ public:
+  static base::win::RegKey GetTextSettingsRegistryKey(REGSAM access = KEY_READ);
+  static float GetContrastFromRegistry();
+  static float GetGammaFromRegistry();
+};
+
+}  // namespace gfx
+
+#endif  // UI_GFX_FONT_UTIL_WIN_H_
