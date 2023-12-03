dc4cd8074c3f71b1bbb31e0ee0ad393bfc442f66
Luke Warlow <luke@warlow.dev>
2023-12-03 18:04:18 +0000

    Fix invoker light dismiss handling for popover
    Update NearestTargetPopoverForInvoker method to account for both
    invokertarget and popovertarget.
    
    Bug: 1494737
    Change-Id: Icb9cc4d821ccb1c45a9b61890972ede7b38ddec8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5079904
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Luke <lukewarlow156@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1232496}

--- third_party/blink/renderer/core/html/forms/html_form_control_element.h
+++ third_party/blink/renderer/core/html/forms/html_form_control_element.h
@@ -117,6 +117,8 @@ class CORE_EXPORT HTMLFormControlElement : public HTMLElement,
   AtomicString popoverTargetAction() const;
   void setPopoverTargetAction(const AtomicString& value);
 
+  HTMLElement* invokeTargetElement();
+
   AtomicString invokeAction() const;
   void setInvokeAction(const AtomicString& value);
 
