081ed44a9be4553322592419bac44941cc724a1a
Luke Fisk-Lennon <lukefl@google.com>
2023-12-03 23:26:32 +0000

    Add feature flag for system emoji picker global variant grouping
    Emoji picker global variant grouping syncs skin tone and gender
    preferences across emojis in each group.
    
    Bug: b:314237647
    Change-Id: Id093913b05ce20e104d4151a61f9498756f997eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5076444
    Reviewed-by: Chuong Ho <hdchuong@chromium.org>
    Reviewed-by: Zac Partridge <zacpartridge@google.com>
    Commit-Queue: Luke Fisk-Lennon <lukefl@google.com>
    Cr-Commit-Position: refs/heads/main@{#1232520}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3881,6 +3881,9 @@ extern const char kImeSystemEmojiPickerClipboardDescription[];
 extern const char kImeSystemEmojiPickerSearchExtensionName[];
 extern const char kImeSystemEmojiPickerSearchExtensionDescription[];
 
+extern const char kImeSystemEmojiPickerVariantGroupingName[];
+extern const char kImeSystemEmojiPickerVariantGroupingDescription[];
+
 extern const char kImeStylusHandwritingName[];
 extern const char kImeStylusHandwritingDescription[];
 
