1012a7c335b83b396a0edeb171998f3d73b0878a
xiangdong kong <xiangdongkong@google.com>
2023-12-04 18:47:38 +0000

    feedback: Convert share_data_page to TS
    - Rename file from JS to TS and update BUILD.gn.
    - Replace mixinBehavior with I18nMixin.
    - Replace querySelector with strictQuery.
    - Remove underscores from private properties and functions.
    
    Bug: b:310963279
    Test: browser_tests --gtest_filter=*OSFeedbackBrowserTest*  --gtest_also_run_disabled_tests
    Test: Manually test in vm.
    Change-Id: I77880f99491486a57385d6891542e3e2f326e288
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5078132
    Reviewed-by: Camden Bickel <cambickel@google.com>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1232850}

--- chrome/test/data/webui/chromeos/os_feedback_ui/share_data_page_test.js
+++ chrome/test/data/webui/chromeos/os_feedback_ui/share_data_page_test.js
@@ -50,6 +50,7 @@ suite('shareDataPageTestSuite', () => {
         /** @type {!ShareDataPageElement} */ (
             document.createElement('share-data-page'));
     assertTrue(!!page);
+    page.feedbackContext = fakeEmptyFeedbackContext;
     document.body.appendChild(page);
     return flushTasks();
   }
