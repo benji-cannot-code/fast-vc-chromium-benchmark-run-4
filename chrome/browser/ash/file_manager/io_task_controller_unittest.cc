1239b9c43ceb459c4e46124ed86f4393f380c3be
cassycc <cassycc@google.com>
2023-12-04 05:01:19 +0000

    IOTaskController: Make the progress status of tasks in the queue visible
    Introduce IOTaskController::TaskStatuses() which returns the progress
    status of each task currently in the queue. The motivation for this
    change is so OneDriveUploadHandler can figure out if there is already a
    task added to the queue to prevent duplicate tasks in http://crrev.com/c/5061490.
    
    Bug: b/310042504
    Change-Id: I103f30b7da61cede756c0578577a35be077af94c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5069775
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1232561}

--- chrome/browser/ash/file_manager/io_task_controller_unittest.cc
+++ chrome/browser/ash/file_manager/io_task_controller_unittest.cc
@@ -84,6 +84,8 @@ TEST_F(IOTaskControllerTest, SimpleQueueing) {
   auto task_id = io_task_controller_.Add(
       std::make_unique<DummyIOTask>(source_urls, dest, OperationType::kCopy));
   EXPECT_EQ(1, io_task_controller_.wake_lock_counter_for_tests());
+  EXPECT_FALSE(io_task_controller_.TaskStatuses().empty());
+  EXPECT_EQ(io_task_controller_.TaskStatuses()[0].get().task_id, task_id);
 
   // Wait for the callbacks posted to the main sequence to finish.
   {
@@ -94,6 +96,8 @@ TEST_F(IOTaskControllerTest, SimpleQueueing) {
         .WillOnce(RunClosure(run_loop.QuitClosure()));
     run_loop.Run();
   }
+
+  EXPECT_TRUE(io_task_controller_.TaskStatuses().empty());
 
   // Cancel() should have no effect once a task is completed.
   io_task_controller_.Cancel(task_id);
@@ -133,6 +137,8 @@ TEST_F(IOTaskControllerTest, Cancel) {
 
   auto task_id = io_task_controller_.Add(
       std::make_unique<DummyIOTask>(source_urls, dest, OperationType::kMove));
+  EXPECT_FALSE(io_task_controller_.TaskStatuses().empty());
+  EXPECT_EQ(io_task_controller_.TaskStatuses()[0].get().task_id, task_id);
 
   // Cancel should synchronously send a progress status.
   EXPECT_CALL(observer,
@@ -141,6 +147,7 @@ TEST_F(IOTaskControllerTest, Cancel) {
                   Field(&ProgressStatus::task_id, task_id), base_matcher)));
 
   io_task_controller_.Cancel(task_id);
+  EXPECT_TRUE(io_task_controller_.TaskStatuses().empty());
 
   // No more observer notifications should come after Cancel() as the task is
   // deleted.
@@ -179,6 +186,8 @@ TEST_F(IOTaskControllerTest, PauseResume) {
 
   auto task_id = io_task_controller_.Add(
       std::make_unique<DummyIOTask>(source_urls, dest, OperationType::kMove));
+  EXPECT_FALSE(io_task_controller_.TaskStatuses().empty());
+  EXPECT_EQ(io_task_controller_.TaskStatuses()[0].get().task_id, task_id);
 
   // Pause should synchronously send a progress status.
   PauseParams pause_params;
@@ -189,6 +198,7 @@ TEST_F(IOTaskControllerTest, PauseResume) {
                             Field(&ProgressStatus::pause_params, pause_params),
                             base_matcher)));
   io_task_controller_.Pause(task_id, pause_params);
+  EXPECT_FALSE(io_task_controller_.TaskStatuses().empty());
 
   // ProgressPausedTasks should synchronously send another paused status update.
   EXPECT_CALL(observer, OnIOTaskStatus(AllOf(
@@ -204,6 +214,7 @@ TEST_F(IOTaskControllerTest, PauseResume) {
                   Field(&ProgressStatus::state, State::kInProgress),
                   Field(&ProgressStatus::task_id, task_id), base_matcher)));
   io_task_controller_.Resume(task_id, ResumeParams());
+  EXPECT_FALSE(io_task_controller_.TaskStatuses().empty());
 
   // Wait for the task to finish successfully.
   EXPECT_CALL(observer,
@@ -255,6 +266,8 @@ TEST_F(IOTaskControllerTest, CompleteWithError) {
   auto task_id = io_task_controller_.Add(std::make_unique<DummyIOTask>(
       source_urls, dest, OperationType::kMove,
       /*show_notification=*/true, /*progress_succeeds=*/false));
+  EXPECT_FALSE(io_task_controller_.TaskStatuses().empty());
+  EXPECT_EQ(io_task_controller_.TaskStatuses()[0].get().task_id, task_id);
 
   // CompleteWithError should synchronously send a progress status.
   EXPECT_CALL(observer,
@@ -266,6 +279,8 @@ TEST_F(IOTaskControllerTest, CompleteWithError) {
                                    base_matcher)));
   io_task_controller_.CompleteWithError(
       task_id, PolicyError(PolicyErrorType::kDlp, /*blocked_files=*/2));
+
+  EXPECT_TRUE(io_task_controller_.TaskStatuses().empty());
 
   // No more observer notifications should come after CompleteWithError as the
   // task is deleted.
