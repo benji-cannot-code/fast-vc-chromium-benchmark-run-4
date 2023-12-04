7c5e8c4be83ebf63e11ca6710fa36fd0ab83d8b7
Noah Rose Ledesma <noahrose@google.com>
2023-12-04 19:32:07 +0000

    Revert "Reland "[Signin][Uno-D] Add UnoDesktopM0 in fieldtrial testing config""
    This reverts commit 680a5c8e3161bfd23a798767a989ad2d3515e762.
    
    Reason for revert: Consistent test failures for BrowsingDataRemoverWithPasswordsAccountStorageBrowserTest & PasswordManagerSyncTest. First bad build: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests%20(Wayland)/47872/overview
    
    Original change's description:
    > Reland "[Signin][Uno-D] Add UnoDesktopM0 in fieldtrial testing config"
    >
    > This is a reland of commit 4e8e57bfba73f427b3ddb1af73a60f5bff48edb0
    >
    > Original change's description:
    > > [Signin][Uno-D] Add UnoDesktopM0 in fieldtrial testing config
    > >
    > > Bug: b/307214665
    > > Change-Id: I808e4da79941c4563affeda498770e3270da6259
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5067373
    > > Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    > > Reviewed-by: David Roger <droger@chromium.org>
    > > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > > Cr-Commit-Position: refs/heads/main@{#1232674}
    >
    > Bug: b/307214665
    > Change-Id: Icf19642b06d7a29acabaf3b28a5660bd89577e2f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5079363
    > Reviewed-by: David Roger <droger@chromium.org>
    > Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    > Auto-Submit: Gabriel Oliveira <gabolvr@google.com>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1232805}
    
    Bug: b/307214665
    Change-Id: I262110cd7dc0771860b53b671867f0b829219635
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5084670
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1232886}

--- chrome/browser/policy/cloud/user_policy_signin_service_browsertest.cc
+++ chrome/browser/policy/cloud/user_policy_signin_service_browsertest.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/test_support/policy_storage.h"
 #include "components/prefs/pref_service.h"
 #include "components/prefs/pref_test_utils.h"
-#include "components/signin/public/base/signin_switches.h"
 #include "components/signin/public/identity_manager/accounts_mutator.h"
 #include "components/signin/public/identity_manager/identity_test_utils.h"
 #include "components/sync/base/command_line_switches.h"
@@ -113,15 +112,11 @@ class UserPolicySigninServiceTest : public InProcessBrowserTest,
         &FakeGaia::HandleRequest, base::Unretained(&fake_gaia_)));
 
     bool disallow_managed_profile_signout = GetParam();
-    std::vector<base::test::FeatureRef> enabled_features = {};
-    std::vector<base::test::FeatureRef> disabled_features = {
-        switches::kUnoDesktop};
     if (disallow_managed_profile_signout) {
-      enabled_features.push_back(kDisallowManagedProfileSignout);
+      feature_list_.InitAndEnableFeature(kDisallowManagedProfileSignout);
     } else {
-      disabled_features.push_back(kDisallowManagedProfileSignout);
+      feature_list_.InitAndDisableFeature(kDisallowManagedProfileSignout);
     }
-    feature_list_.InitWithFeatures(enabled_features, disabled_features);
   }
 
   ~UserPolicySigninServiceTest() override {
