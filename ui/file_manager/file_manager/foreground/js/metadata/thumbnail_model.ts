9138aa66b7569a808e0c3aadd4fd381e73adc831
Ben Reich <benreich@chromium.org>
2023-12-04 22:41:06 +0000

    [FilesTS] Convert list_thumbnail_loader and unittest
    Bug: b/289003444
    Change-Id: I296f443c8b84335f6e69c8a8f90b72e83e4a9111
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5082777
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233022}

--- ui/file_manager/file_manager/foreground/js/metadata/thumbnail_model.ts
+++ ui/file_manager/file_manager/foreground/js/metadata/thumbnail_model.ts
@@ -25,7 +25,7 @@ export interface ThumbnailMetadataItem {
   };
   thumbnail: {
     url?: string,
-    urlError?: Error,
+    urlError?: Error&{errorDescription?: string},
     transform?: ImageTransformation,
     transformError?: Error,
   };
