fac42196309d200b65be2b034f517a0ad91f233d
Matt Reichhoff <mreichhoff@chromium.org>
2023-12-04 15:00:48 +0000

    [iOS] Add initial icon to plus_address bottom sheet
    This CL is purely cosmetic, and simply adds a shared mail icon to
    the plus_addresses bottom sheet. The real/branded icon(s) will be
    added in a follow-up.
    
    Screenshot:
    https://screenshot.googleplex.com/7vAoPkJfZpECeri
    
    Bug: 1467623, b/295076363
    Change-Id: I56b94af1e1605b5b81f389bfd7971b662655c0da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5079396
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232703}

--- ios/chrome/browser/plus_addresses/ui/plus_address_bottom_sheet_constants.mm
+++ ios/chrome/browser/plus_addresses/ui/plus_address_bottom_sheet_constants.mm
@@ -11,3 +11,5 @@ NSString* const kPlusAddressModalDescriptionAccessibilityIdentifier =
 const CGFloat kPrimaryAddressBottomMargin = 20;
 
 const CGFloat kBeforeImageTopMargin = 10;
+
+const CGFloat kImageSize = 26;
