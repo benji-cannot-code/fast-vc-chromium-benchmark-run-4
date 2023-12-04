d3ab55489f64537f9bc7f788b19aa1a8c468e102
Dave Tapuska <dtapuska@chromium.org>
2023-12-04 16:01:45 +0000

    Pass WorkletGlobalScope into WorkletModuleScriptFetcher
    - In a future CL (https://chromium-review.googlesource.com/c/chromium/src/+/5080355/1) we will want
    to load the isolate from the GlobalScope.
    - This change fixes a TODO that was long written in the code.
    
    Change-Id: I9c57a94ed8920645e36a63c87034d75f926739c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5074810
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232742}

--- third_party/blink/renderer/core/loader/modulescript/worklet_module_script_fetcher.h
+++ third_party/blink/renderer/core/loader/modulescript/worklet_module_script_fetcher.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class ResourceFetcher;
+class WorkletGlobalScope;
 
 // WorkletModuleScriptFetcher is an implementation of ModuleScriptFetcher
 // interface for Worklets. This implements the custom "perform the fetch" hook
@@ -27,7 +28,7 @@ class CORE_EXPORT WorkletModuleScriptFetcher final
     : public GarbageCollected<WorkletModuleScriptFetcher>,
       public ModuleScriptFetcher {
  public:
-  WorkletModuleScriptFetcher(WorkletModuleResponsesMap*,
+  WorkletModuleScriptFetcher(WorkletGlobalScope*,
                              base::PassKey<ModuleScriptLoader>);
 
   // Implements ModuleScriptFetcher.
@@ -37,18 +38,14 @@ class CORE_EXPORT WorkletModuleScriptFetcher final
              ModuleGraphLevel,
              ModuleScriptFetcher::Client*) override;
 
+  void Trace(Visitor* visitor) const override;
+
  private:
   // Implements ResourceClient
   void NotifyFinished(Resource*) override;
   String DebugName() const override { return "WorkletModuleScriptFetcher"; }
 
-  // TODO(nhiroki): In general, CrossThreadPersistent is heavy and should not be
-  // owned by objects that can frequently be created like this class. Instead of
-  // retaining a reference to WorkletModuleResponsesMap, this class should
-  // access the map via WorkletGlobalScope::GetModuleResponsesMap().
-  // Bonus: WorkletGlobalScope can provide ResourceFetcher, too.
-  CrossThreadPersistent<WorkletModuleResponsesMap> module_responses_map_;
-
+  const Member<WorkletGlobalScope> global_scope_;
   KURL url_;
   ModuleType expected_module_type_;
 };
