86d25173d5757e4d920865a935168afd0940ab3f
Brandon Wylie <wylieb@chromium.org>
2023-12-04 22:40:30 +0000

    [bookmarks] Support empty state views for improved bookmarks
    The improved bookmarks manager is never "empty" because the search box
    is always present. Added a function to ignore certain view types in
    SelectableListLayout when deciding to show the empty view.
    
    Bug: 1507102
    Change-Id: I1c876956bada85bdc0b26ead7a9655acd8492e3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5078681
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233021}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/util/BookmarkTestUtil.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/util/BookmarkTestUtil.java
@@ -63,6 +63,7 @@ public class BookmarkTestUtil {
     }
 
     /** Opens the mobile bookmarks folder in the bookmarks manager. */
+    // TODO(crbug.com/1467286): Remove use of waitForIdleSync here.
     public static void openMobileBookmarks(
             RecyclerView recyclerView,
             BookmarkDelegate bookmarkDelegate,
@@ -70,6 +71,18 @@ public class BookmarkTestUtil {
         openRootFolder(recyclerView, bookmarkDelegate, bookmarkModel);
 
         onView(withText("Mobile bookmarks")).perform(click());
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
+    }
+
+    /** Opens the reading list folder in the bookmarks manager. */
+    // TODO(crbug.com/1467286): Remove use of waitForIdleSync here.
+    public static void openReadingList(
+            RecyclerView recyclerView,
+            BookmarkDelegate bookmarkDelegate,
+            BookmarkModel bookmarkModel) {
+        openRootFolder(recyclerView, bookmarkDelegate, bookmarkModel);
+
+        onView(withText("Reading list")).perform(click());
         InstrumentationRegistry.getInstrumentation().waitForIdleSync();
     }
 
