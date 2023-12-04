c1281f7c30c2cd1e73111553c8a95718eddfbaff
Jeffrey Young <cowmoo@google.com>
2023-12-04 21:19:52 +0000

    p13n: move SeaPen reducers to own file
    Prepare for code sharing between VC Background and
    Personalization. Refactor into smaller individual
    reducers. Introduce an adapter layer between
    Personalization and SeaPen reducer to reduce
    friction when SeaPen reducer has to be shared with
    VC Background.
    
    Bug: b:312208348
    Test: browser_tests --gtest_filter=*SeaPen*
    Change-Id: Ib83c9d69ff7fc4d6f0d3d0b6ca2aa9d1285980aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5053574
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Commit-Queue: Jeffrey Young <cowmoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1232975}

---
+++ ash/webui/personalization_app/resources/js/wallpaper/sea_pen/sea_pen_state.ts
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {FilePath} from 'chrome://resources/mojo/mojo/public/mojom/base/file_path.mojom-webui.js';
+
+import {SeaPenThumbnail} from '../../../sea_pen.mojom-webui.js';
+
+import {RecentSeaPenData} from './constants.js';
+
+export interface SeaPenState {
+  thumbnails: SeaPenThumbnail[]|null;
+  thumbnailsLoading: boolean;
+  recentImages: FilePath[]|null;
+  recentImageData: Record<FilePath['path'], RecentSeaPenData>;
+}
+
+export function emptyState(): SeaPenState {
+  return {
+    thumbnails: null,
+    thumbnailsLoading: false,
+    recentImages: null,
+    recentImageData: {},
+  };
+}
