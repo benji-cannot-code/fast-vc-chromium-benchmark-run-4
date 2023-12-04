a659f811f01f0564d3fdbd26cb72e6967ba70382
Daniel Cheng <dcheng@chromium.org>
2023-12-04 01:18:06 +0000

    Use googletest flag macros to access googletest flags in //content/browser.
    The implementation details of flags can change; fixing this proactively
    makes it easier to roll googletest in chromium.
    
    This CL was uploaded by git cl split.
    
    Bug: 1409870
    Change-Id: Iffd9ce917489a8ab174bb0e3a219d4f6ff8c7501
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5081582
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232532}

--- content/browser/permissions/permission_controller_impl_unittest.cc
+++ content/browser/permissions/permission_controller_impl_unittest.cc
@@ -258,7 +258,7 @@ TEST_F(PermissionControllerImplTest,
     }
 
     if (test_case.expect_death) {
-      ::testing::FLAGS_gtest_death_test_style = "threadsafe";
+      GTEST_FLAG_SET(death_test_style, "threadsafe");
       base::MockCallback<RequestsCallback> callback;
       EXPECT_DEATH_IF_SUPPORTED(
           PermissionControllerRequestPermissionsFromCurrentDocument(
@@ -345,7 +345,7 @@ TEST_F(PermissionControllerImplTest,
     }
 
     if (test_case.expect_death) {
-      ::testing::FLAGS_gtest_death_test_style = "threadsafe";
+      GTEST_FLAG_SET(death_test_style, "threadsafe");
       base::MockCallback<RequestsCallback> callback;
       EXPECT_DEATH_IF_SUPPORTED(
           PermissionControllerRequestPermissions(
