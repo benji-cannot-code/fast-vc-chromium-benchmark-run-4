9290a517d404db5f62a38bc6be0a516689c2e11b
Matt Reichhoff <mreichhoff@chromium.org>
2023-12-04 20:04:52 +0000

    [iOS] Animate plus_address modal dismissal
    This change is intended to be purely cosmetic.
    
    Bug: 1467623, b/295076363
    Change-Id: Ia3783e7e53745c96eb43a58839f9dcad2dd27570
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5081992
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232917}

--- ios/chrome/browser/plus_addresses/coordinator/plus_address_bottom_sheet_coordinator.mm
+++ ios/chrome/browser/plus_addresses/coordinator/plus_address_bottom_sheet_coordinator.mm
@@ -71,7 +71,7 @@ constexpr CGFloat kHalfSheetCornerRadius = 20;
 
 - (void)stop {
   [super stop];
-  [_viewController.presentingViewController dismissViewControllerAnimated:NO
+  [_viewController.presentingViewController dismissViewControllerAnimated:YES
                                                                completion:nil];
   _viewController = nil;
   _mediator = nil;
