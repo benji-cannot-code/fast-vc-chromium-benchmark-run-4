1d8fddf176cf83b65c8b267038e4cc9794d31527
Kent Tamura <tkent@chromium.org>
2023-12-04 08:26:40 +0000

    Ruby: Fix invalid layout tree generation
    This CL fixes a bug that LayoutRubyBase can have anonymous blocks and an
    inline-table as children.
    
    LayoutRubyBase is a subclass of LayoutBlockFlow, and it doesn't allow to
    mix blocks and inline-level objects.
    
    Root cause:
    When a table-internal box is added to LayoutRubyBase,
    LayoutBlockFlow::AddChild() assumed it as a block box, but
    LayoutObject::AddChild() creates an inline-table wrapper for it.
    LayoutBlockFlow::AddChild() moved existing inline children to an
    anonymous block.
    
    Fix:
    We should make sure LayoutRubyBase doesn't create anonymous block
    children.  LayoutBlockFlow::AddChild() correctly assumes table-internal
    boxes as inline boxes for LayoutRubyBase parents.
    
    Now we can remove anonymous box handling in LayotuObject::AddChild(),
    which was added by crrev.com/1225797
    
    Bug: 1503372
    Change-Id: Ibaae749c44f6a243070fcef8289369d5b1506490
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5083099
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232592}

--- third_party/blink/renderer/core/layout/layout_object.h
+++ third_party/blink/renderer/core/layout/layout_object.h
@@ -808,6 +808,9 @@ class CORE_EXPORT LayoutObject : public GarbageCollected<LayoutObject>,
 #endif
 
   void AddAbsoluteRectForLayer(gfx::Rect& result);
+
+ protected:
+  // A helper for AddChild().
   bool RequiresAnonymousTableWrappers(const LayoutObject*) const;
 
  public:
