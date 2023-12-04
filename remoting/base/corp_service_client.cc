538524149c8966dffde8a7455d455759b1f6318b
Joe Downing <joedow@google.com>
2023-12-04 18:47:27 +0000

    Include package version in provisioning requests
    Also: Roll src/remoting/internal/ 03a871a60..55265d75e (1 commit)
    
    Bug: b:297857922
    Change-Id: I6ebd6709ddde67252d88c00c1dd2b802606704b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5083893
    Commit-Queue: Gary Kacmarcik <garykac@chromium.org>
    Auto-Submit: Joe Downing <joedow@chromium.org>
    Reviewed-by: Gary Kacmarcik <garykac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232849}

--- remoting/base/corp_service_client.cc
+++ remoting/base/corp_service_client.cc
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/base/corp_service_client.h"
 
 #include "base/functional/bind.h"
+#include "base/strings/stringize_macros.h"
 #include "remoting/base/protobuf_http_request.h"
 #include "remoting/base/protobuf_http_request_config.h"
 #include "remoting/base/service_urls.h"
+#include "remoting/base/version.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
 #if BUILDFLAG(REMOTING_INTERNAL)
@@ -77,11 +79,11 @@ void CorpServiceClient::ProvisionCorpMachine(
           policy_exception_justification:
             "Not implemented."
         })");
-  ExecuteRequest(traffic_annotation,
-                 internal::GetMachineProvisioningRequestPath(),
-                 internal::GetMachineProvisioningRequest(
-                     owner_email, fqdn, public_key, existing_host_id),
-                 std::move(callback));
+  ExecuteRequest(
+      traffic_annotation, internal::GetMachineProvisioningRequestPath(),
+      internal::GetMachineProvisioningRequest(
+          owner_email, fqdn, public_key, STRINGIZE(VERSION), existing_host_id),
+          std::move(callback));
 }
 
 void CorpServiceClient::ReportProvisioningError(
@@ -122,8 +124,8 @@ void CorpServiceClient::ReportProvisioningError(
         })");
   ExecuteRequest(
       traffic_annotation, internal::GetReportProvisioningErrorRequestPath(),
-      internal::GetReportProvisioningErrorRequest(host_id, error_message),
-      std::move(callback));
+      internal::GetReportProvisioningErrorRequest(
+          host_id, error_message, STRINGIZE(VERSION)), std::move(callback));
 }
 
 void CorpServiceClient::CancelPendingRequests() {
