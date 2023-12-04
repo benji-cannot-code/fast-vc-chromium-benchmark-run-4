1addf214fb403c1bf72fc4b957ab05ff203c2b82
Avi Drissman <avi@chromium.org>
2023-12-04 22:31:12 +0000

    Replace various uses of Log2Floor and Log2Ceiling with std calls.
    This CL was uploaded by git cl split.
    
    R=edchin@google.com
    
    Bug: 1414634
    Change-Id: I5a1814617cfbd67ba39dd1b674aa01084ef57d71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5084909
    Commit-Queue: edchin <edchin@google.com>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: edchin <edchin@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1233012}

--- chrome/browser/compose/compose_text_usage_logger.cc
+++ chrome/browser/compose/compose_text_usage_logger.cc
@@ -6,8 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/compose/compose_text_usage_logger.h"
 
 #include <algorithm>
+#include <bit>
+#include <cstdint>
 
-#include "base/bits.h"
 #include "base/strings/string_tokenizer.h"
 #include "base/strings/string_util.h"
 #include "components/autofill/content/browser/content_autofill_driver.h"
@@ -42,10 +43,7 @@ size_t RoundDownToPowerOfTwo(int64_t n) {
   if (n < 0) {
     return -1;
   }
-  if (n == 0) {
-    return 0;
-  }
-  return 1ull << (base::bits::Log2Floor(n));
+  return std::bit_floor<uint64_t>(n);
 }
 
 }  // namespace
