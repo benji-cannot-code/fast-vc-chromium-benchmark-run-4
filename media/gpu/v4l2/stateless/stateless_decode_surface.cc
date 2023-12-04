5d987541d59d34cae66fb43e2bfaa10d069ab7f1
Fritz Koenig <frkoenig@chromium.org>
2023-12-04 20:12:25 +0000

    v4l2/stateless: Track reference frames with surface
    The surface needs to know which frames are used
    as references so that they can be held onto until
    they are done being used.
    
    Cq-Include-Trybots: luci.chromium.try:linux-v4l2-codec-rel
    Bug: b:278935312
    
    Change-Id: Ic0f8be17bcd0abea00862fc11d2b94168f0882cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5069049
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Commit-Queue: Fritz Koenig <frkoenig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232925}

--- media/gpu/v4l2/stateless/stateless_decode_surface.cc
+++ media/gpu/v4l2/stateless/stateless_decode_surface.cc
@@ -5,11 +5,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/gpu/v4l2/stateless/stateless_decode_surface.h"
 
+#include "base/logging.h"
+#include "media/gpu/macros.h"
+
 namespace media {
 
 StatelessDecodeSurface::StatelessDecodeSurface(uint32_t frame_id)
-    : frame_id_(frame_id) {}
+    : frame_id_(frame_id) {
+  DVLOGF(4) << "Creating surface with id : " << frame_id_;
+}
 
-StatelessDecodeSurface::~StatelessDecodeSurface() {}
+StatelessDecodeSurface::~StatelessDecodeSurface() {
+  DVLOGF(4) << "Releasing surface with id : " << frame_id_;
+}
+
+uint64_t StatelessDecodeSurface::GetReferenceTimestamp() const {
+  const uint32_t kMicrosecondsToNanoseconds = 1000;
+  return frame_id_ * kMicrosecondsToNanoseconds;
+}
+
+void StatelessDecodeSurface::SetReferenceSurfaces(
+    std::vector<scoped_refptr<StatelessDecodeSurface>> ref_surfaces) {
+  DCHECK(reference_surfaces_.empty());
+
+  reference_surfaces_ = std::move(ref_surfaces);
+}
 
 }  // namespace media
--- media/gpu/v4l2/stateless/stateless_decode_surface.h
+++ media/gpu/v4l2/stateless/stateless_decode_surface.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_GPU_V4L2_STATELESS_STATELESS_DECODE_SURFACE_H_
 #define MEDIA_GPU_V4L2_STATELESS_STATELESS_DECODE_SURFACE_H_
 
+#include <vector>
+
 #include "base/memory/ref_counted.h"
 #include "media/base/video_frame.h"
 
@@ -18,6 +20,12 @@ class StatelessDecodeSurface : public base::RefCounted<StatelessDecodeSurface> {
   StatelessDecodeSurface(const StatelessDecodeSurface&) = delete;
   StatelessDecodeSurface& operator=(const StatelessDecodeSurface&) = delete;
 
+  int32_t FrameID() const { return frame_id_; }
+
+  uint64_t GetReferenceTimestamp() const;
+  void SetReferenceSurfaces(
+      std::vector<scoped_refptr<StatelessDecodeSurface>> ref_surfaces);
+
  protected:
   virtual ~StatelessDecodeSurface();
   friend class base::RefCounted<StatelessDecodeSurface>;
@@ -26,6 +34,11 @@ class StatelessDecodeSurface : public base::RefCounted<StatelessDecodeSurface> {
   // Identify this surface so that it can be matched up the the uncompressed
   // buffer when it is done being decompressed.
   const uint32_t frame_id_;
+
+  // Frames that this frames uses for references. These are held onto until the
+  // decode is done so that they are not reused while they need to be
+  // referenced.
+  std::vector<scoped_refptr<StatelessDecodeSurface>> reference_surfaces_;
 };
 
 }  // namespace media
