d5ec6e57eb458873bea761ea9d6bc01aeb4514e0
Gabriel Oliveira <gabolvr@google.com>
2023-12-04 17:52:37 +0000

    Reland "[Signin][Uno-D] Add UnoDesktopM0 in fieldtrial testing config"
    This is a reland of commit 4e8e57bfba73f427b3ddb1af73a60f5bff48edb0
    
    Original change's description:
    > [Signin][Uno-D] Add UnoDesktopM0 in fieldtrial testing config
    >
    > Bug: b/307214665
    > Change-Id: I808e4da79941c4563affeda498770e3270da6259
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5067373
    > Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    > Reviewed-by: David Roger <droger@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1232674}
    
    Bug: b/307214665
    Change-Id: Icf19642b06d7a29acabaf3b28a5660bd89577e2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5079363
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Auto-Submit: Gabriel Oliveira <gabolvr@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1232805}

--- chrome/browser/sync/test/integration/single_client_webauthn_credentials_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_webauthn_credentials_sync_test.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/test/integration/webauthn_credentials_helper.h"
 #include "chrome/browser/ui/browser.h"
 #include "components/password_manager/core/browser/features/password_manager_features_util.h"
+#include "components/signin/public/base/signin_switches.h"
 #include "components/sync/base/client_tag_hash.h"
 #include "components/sync/base/features.h"
 #include "components/sync/base/model_type.h"
@@ -104,7 +105,10 @@ CreateEntityWithCustomClientTagHash(
 
 class SingleClientWebAuthnCredentialsSyncTest : public SyncTest {
  public:
-  SingleClientWebAuthnCredentialsSyncTest() : SyncTest(SINGLE_CLIENT) {}
+  SingleClientWebAuthnCredentialsSyncTest() : SyncTest(SINGLE_CLIENT) {
+    feature_list_.InitAndDisableFeature(switches::kUnoDesktop);
+  }
+
   ~SingleClientWebAuthnCredentialsSyncTest() override = default;
 
   // Injects a new WEBAUTHN_CREDENTIAL type server entity and returns the
@@ -141,6 +145,9 @@ class SingleClientWebAuthnCredentialsSyncTest : public SyncTest {
   webauthn::PasskeySyncBridge& GetModel() {
     return webauthn_credentials_helper::GetModel(kSingleProfile);
   }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
 };
 
 // Adding a local passkey should sync to the server.
