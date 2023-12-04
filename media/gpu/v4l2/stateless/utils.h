1b74073cf724533bb7fc45acd19055d14b79fa1e
Fritz Koenig <frkoenig@chromium.org>
2023-12-04 19:38:09 +0000

    v4l2/stateless: Poll for buffer events
    Buffers that are done being used will notify the file
    descriptor. As V4L2 is asynchronous this is a way to
    know when a buffer is completely done being decoded into.
    By waiting on this event the decoder can be notified
    that a buffer is ready to be passed off to the display.
    
    Once the event has occurred a task is posted. This avoids
    needing to have a separate thread to do the dequing and
    having to handle concurrency.
    
    Cq-Include-Trybots: luci.chromium.try:linux-v4l2-codec-rel
    Bug: b:278935312
    Change-Id: I3e9c4b618987e61548acbd66580f9de13bc0cba1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5069636
    Commit-Queue: Fritz Koenig <frkoenig@chromium.org>
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232896}

--- media/gpu/v4l2/stateless/utils.h
+++ media/gpu/v4l2/stateless/utils.h
@@ -6,12 +6,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_GPU_V4L2_STATELESS_UTILS_H_
 #define MEDIA_GPU_V4L2_STATELESS_UTILS_H_
 
+#include <poll.h>
+
+#include "base/functional/callback_helpers.h"
 #include "media/gpu/v4l2/stateless/device.h"
 #include "media/video/video_decode_accelerator.h"
 
 namespace media {
 VideoDecodeAccelerator::SupportedProfiles GetSupportedDecodeProfiles(
     Device* device);
+void WaitOnceForEvents(struct pollfd event,
+                       base::OnceClosure dequeue_callback,
+                       base::OnceClosure error_callback);
 std::string IoctlToString(uint64_t request);
 }  // namespace media
 #endif  // MEDIA_GPU_V4L2_STATELESS_UTILS_H_
