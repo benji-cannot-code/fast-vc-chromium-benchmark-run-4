49c6cb236187ea4cbca191c7471a9d8b46475860
mayuko aiba <mayukoaiba@google.com>
2023-12-04 02:31:03 +0000

    BrowserManager: Extract lacros process handlers and make BrowserLauncher
    This CL extracts lacros process management of
    BrowserManager::StartWithLogFile and makes class BrowserLauncher.
    
          BrowserLauncherTest.*
    
    Bug: 1495590
    Test: tast run $DUT lacros.Activate.keep_alive
    Change-Id: I22e4ab36f33a0634a0630150f5add5ce04a9b361
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5039071
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Mayuko Aiba <mayukoaiba@google.com>
    Cr-Commit-Position: refs/heads/main@{#1232543}

--- chrome/browser/ash/crosapi/browser_manager.h
+++ chrome/browser/ash/crosapi/browser_manager.h
@@ -20,10 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
-#include "base/process/process.h"
 #include "base/scoped_observation.h"
 #include "base/time/time.h"
 #include "chrome/browser/ash/crosapi/browser_action.h"
+#include "chrome/browser/ash/crosapi/browser_launcher.h"
 #include "chrome/browser/ash/crosapi/browser_manager_observer.h"
 #include "chrome/browser/ash/crosapi/browser_service_host_observer.h"
 #include "chrome/browser/ash/crosapi/browser_util.h"
@@ -716,6 +716,8 @@ class BrowserManager : public session_manager::SessionManagerObserver,
 
   void PerformAction(std::unique_ptr<BrowserAction> action);
 
+  crosapi::BrowserLauncher browser_launcher_;
+
   // NOTE: The state is exposed to tests via autotest_private.
   State state_ = State::NOT_INITIALIZED;
 
@@ -745,9 +747,6 @@ class BrowserManager : public session_manager::SessionManagerObserver,
   // Time when the lacros process was resumed (when pre-launching at login
   // screen).
   base::TimeTicks lacros_resume_time_;
-
-  // Process handle for the lacros-chrome process.
-  base::Process lacros_process_;
 
   // Remembers the request from Lacros-chrome whether it needs to be
   // relaunched. Reset on new process start in any cases.
