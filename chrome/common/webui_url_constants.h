0ae99f2f5718a49aeea37a13c63ceeef594f5123
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2023-12-04 20:25:07 +0000

    Remove unused download shelf URL constant
    There are no more references to these constants in the codebase
    
    Bug: None
    Change-Id: Ibb925c6e5e93351d54c0784cf7aa513f813db79a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5075485
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232937}

--- chrome/common/webui_url_constants.h
+++ chrome/common/webui_url_constants.h
@@ -503,8 +503,6 @@ extern const char kChromeUITabStripURL[];
 #if !BUILDFLAG(IS_ANDROID)
 extern const char kChromeUICommanderHost[];
 extern const char kChromeUICommanderURL[];
-extern const char kChromeUIDownloadShelfHost[];
-extern const char kChromeUIDownloadShelfURL[];
 extern const char kChromeUITabSearchHost[];
 extern const char kChromeUITabSearchURL[];
 #endif
