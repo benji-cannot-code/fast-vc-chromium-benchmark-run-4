7a6b27eb4735fff73b58050f1558968f992dee2e
Hongyu Long <hongyulong@chromium.org>
2023-12-04 22:45:23 +0000

    focus-mode: Animation to shrink/expand task container
    This CL adds the animation to shrink task_view_container if
    chip_carousel has tasks and the user clicks the radio_button to mark the
    task as completed, or clicks the deselect_button to remove the selected
    task. Also, we add the animation to expand the task_view_container if
    chip_carousel has tasks and the user has selected a task to focus.
    
    When the shrink/expand animation happens, if there is a view below
    task_view_container, the view will be simultaneously shift up/down.
    
    Bug: b/306272008
    Test: Manual & unittest
    Change-Id: Id22dbf4654505794a4d8d3ad7cdb00a34fd6a655
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5078103
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Hongyu Long <hongyulong@chromium.org>
    Reviewed-by: Richard Chui <richui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233027}

--- ash/system/focus_mode/focus_mode_detailed_view.h
+++ ash/system/focus_mode/focus_mode_detailed_view.h
@@ -12,15 +12,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/tray_detailed_view.h"
 #include "base/timer/timer.h"
 #include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/views/view_observer.h"
 
 namespace views {
 class BoxLayoutView;
 class Label;
-}
+}  // namespace views
 
 namespace ash {
 
 class FocusModeCountdownView;
+class FocusModeTaskView;
 class HoverHighlightView;
 class IconButton;
 class RoundedContainer;
@@ -28,9 +30,9 @@ class Switch;
 class SystemTextfield;
 
 // This view displays the focus panel settings that a user can set.
-class ASH_EXPORT FocusModeDetailedView
-    : public TrayDetailedView,
-      public FocusModeController::Observer {
+class ASH_EXPORT FocusModeDetailedView : public TrayDetailedView,
+                                         public FocusModeController::Observer,
+                                         public views::ViewObserver {
  public:
   METADATA_HEADER(FocusModeDetailedView);
 
@@ -38,6 +40,9 @@ class ASH_EXPORT FocusModeDetailedView
   FocusModeDetailedView(const FocusModeDetailedView&) = delete;
   FocusModeDetailedView& operator=(const FocusModeDetailedView&) = delete;
   ~FocusModeDetailedView() override;
+
+  // views::ViewObserver:
+  void OnViewBoundsChanged(View* observed_view) override;
 
  private:
   class TimerTextfieldController;
@@ -70,6 +75,10 @@ class ASH_EXPORT FocusModeDetailedView
   // textfield or selects a task from the list, this view only shows the
   // selected saved task item view and the header.
   void CreateTaskView();
+
+  // Performs an animation to shift the visible container views below
+  // `task_view_container_`.
+  void OnTaskViewAnimate(const int shift_height);
 
   // Creates the DND rounded container. This view will be visible only when
   // there is no active focus session. The toggle button in this view will
@@ -133,6 +142,12 @@ class ASH_EXPORT FocusModeDetailedView
   // A label that displays the end time of the focus session when focus is
   // active.
   raw_ptr<views::Label> end_time_label_ = nullptr;
+
+  // Records the height of the `task_view_container_`.
+  int task_view_container_height_ = 0;
+  // The view contains a header view and a `focus_mode_task_view_`.
+  raw_ptr<RoundedContainer> task_view_container_ = nullptr;
+  raw_ptr<FocusModeTaskView> focus_mode_task_view_ = nullptr;
 
   // This view contains a toggle for turning on/off DND.
   raw_ptr<RoundedContainer> do_not_disturb_view_ = nullptr;
