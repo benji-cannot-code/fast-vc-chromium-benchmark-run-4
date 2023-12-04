a08fa5842c85ff7cae9e1742251946ea0ee6e5a2
Matt Reichhoff <mreichhoff@chromium.org>
2023-12-04 20:11:18 +0000

    [iOS] Prevent plus_address modal spinner from changing layout
    This change adds a container with fixed width, and ensures the spinner
    is centered. It does not change functional behavior and is cosmetic
    only. The goal is that other views not be re-positioned when the
    spinner is shown.
    
    Screencast:
    https://screencast.googleplex.com/cast/NTAwNTg0NTg5MTkwNzU4NHxkZGIyMWZiMS1kOQ
    
    Bug: 1467623, b/295076363
    Change-Id: I15f363ac64f82f2fa05dd32c2cae9428d65a084a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5082046
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232923}

--- ios/chrome/browser/plus_addresses/ui/plus_address_bottom_sheet_view_controller.mm
+++ ios/chrome/browser/plus_addresses/ui/plus_address_bottom_sheet_view_controller.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_action_handler.h"
 #import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.h"
+#import "ios/chrome/common/ui/util/constraints_ui_util.h"
 #import "ios/chrome/common/ui/util/dynamic_type_util.h"
 #import "ios/chrome/common/ui/util/text_view_util.h"
 #import "ios/chrome/common/ui/util/ui_util.h"
@@ -87,9 +88,7 @@ NSAttributedString* DescriptionMessage() {
 - (void)viewDidLoad {
   // Set the properties read by the super when constructing the
   // views in `-[ConfirmationAlertViewController viewDidLoad]`.
-  _activityIndicator = [[UIActivityIndicatorView alloc]
-      initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleMedium];
-  self.aboveTitleView = _activityIndicator;
+  [self setupAboveTitleView];
   self.image = DefaultSymbolTemplateWithPointSize(kMailFillSymbol, kImageSize);
   self.imageHasFixedSize = true;
   self.titleString = l10n_util::GetNSString(IDS_PLUS_ADDRESS_MODAL_TITLE);
@@ -264,6 +263,20 @@ NSAttributedString* DescriptionMessage() {
   descriptionView.attributedText = description;
   descriptionView.textAlignment = NSTextAlignmentCenter;
   return descriptionView;
+}
+
+- (void)setupAboveTitleView {
+  _activityIndicator = [[UIActivityIndicatorView alloc]
+      initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleMedium];
+
+  // Create a container view such that the activity indicator showing doesn't
+  // cause the layout to jump.
+  UIView* container = [[UIView alloc] initWithFrame:CGRectZero];
+  [container addSubview:_activityIndicator];
+  _activityIndicator.translatesAutoresizingMaskIntoConstraints = NO;
+  container.translatesAutoresizingMaskIntoConstraints = NO;
+  AddSameConstraints(container, _activityIndicator);
+  self.aboveTitleView = container;
 }
 
 @end
