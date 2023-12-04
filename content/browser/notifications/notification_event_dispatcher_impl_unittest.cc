dddf81be90d430760c60c347f9fee77b89a49729
Leena Kaushik <l1.kaushik@samsung.com>
2023-12-04 18:13:20 +0000

    Remove unused import in NotificationEventDispatcherImplTest
    This was missed in crrev.com/c/4962742
    
    Bug: None
    
    Change-Id: I7c0df22916ec6f49fbfca6b19c4c4be8b3e952c9
    Signed-off-by: Leena Kaushik <l1.kaushik@samsung.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4895968
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Shubham Gupta <shubham.gupta@chromium.org>
    Commit-Queue: Shubham Gupta <shubham.gupta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232822}

--- content/browser/notifications/notification_event_dispatcher_impl_unittest.cc
+++ content/browser/notifications/notification_event_dispatcher_impl_unittest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/functional/callback_helpers.h"
-#include "base/memory/raw_ptr.h"
 #include "base/test/task_environment.h"
 #include "base/test/test_simple_task_runner.h"
 #include "content/browser/renderer_host/render_frame_host_impl.h"
