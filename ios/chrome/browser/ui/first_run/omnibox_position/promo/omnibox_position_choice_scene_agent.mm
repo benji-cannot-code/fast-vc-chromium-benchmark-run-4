ecb31b896dbffac8c160b02489e57a79bbc7d6e6
Christian Xu <christianxu@google.com>
2023-12-04 11:10:12 +0000

    [iOS][OmniboxPosition] Prevent fullscreen promo to be shown after FRE
    Add pref observation to deregister the fullscreen promo when a preferred
    omnibox position is set.
    
    Bug: 1503638
    Change-Id: Iee0368a85eb09eb33c44aa00bd4e861ede955873
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5076867
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1232632}

--- ios/chrome/browser/ui/first_run/omnibox_position/promo/omnibox_position_choice_scene_agent.mm
+++ ios/chrome/browser/ui/first_run/omnibox_position/promo/omnibox_position_choice_scene_agent.mm
@@ -11,9 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/policy/browser_state_policy_connector.h"
 #import "ios/chrome/browser/promos_manager/promos_manager.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
+#import "ios/chrome/browser/shared/model/prefs/pref_backed_boolean.h"
+#import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "ios/chrome/browser/ui/first_run/omnibox_position/omnibox_position_choice_util.h"
 
-@interface OmniboxPositionChoiceSceneAgent ()
+@interface OmniboxPositionChoiceSceneAgent () <BooleanObserver>
 
 @property(nonatomic, assign) PromosManager* promosManager;
 
@@ -21,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation OmniboxPositionChoiceSceneAgent {
   base::WeakPtr<ChromeBrowserState> _browserState;
+  /// Pref tracking if the preferred omnibox position is bottom.
+  PrefBackedBoolean* _bottomOmniboxPref;
 }
 
 - (instancetype)initWithPromosManager:(PromosManager*)promosManager
@@ -34,6 +38,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }
   }
   return self;
+}
+
+#pragma mark - SceneStateObserver
+
+- (void)sceneStateDidEnableUI:(SceneState*)sceneState {
+  [self setupObservers];
+}
+
+- (void)sceneStateDidDisableUI:(SceneState*)sceneState {
+  [self tearDownObservers];
 }
 
 - (void)sceneState:(SceneState*)sceneState
@@ -52,6 +66,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 }
 
+#pragma mark - Boolean Observer
+
+- (void)booleanDidChange:(id<ObservableBoolean>)observableBoolean {
+  ChromeBrowserState* browserState = _browserState.get();
+  if (!browserState) {
+    return;
+  }
+
+  if (observableBoolean == _bottomOmniboxPref) {
+    if (browserState->GetPrefs() && self.promosManager &&
+        !ShouldShowOmniboxPositionChoiceIPHPromo(browserState->GetPrefs())) {
+      self.promosManager->DeregisterPromo(
+          promos_manager::Promo::OmniboxPosition);
+    }
+  }
+}
+
+#pragma mark - Private
+
 /// Register the Omnibox Position Choice Screen in the promo manager if we want
 /// to display the promo.
 - (void)registerPromoIfNecessary {
@@ -66,6 +99,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   } else {
     self.promosManager->DeregisterPromo(promos_manager::Promo::OmniboxPosition);
   }
+}
+
+/// Sets up pref observation.
+- (void)setupObservers {
+  ChromeBrowserState* browserState = _browserState.get();
+  if (!browserState) {
+    return;
+  }
+  _bottomOmniboxPref =
+      [[PrefBackedBoolean alloc] initWithPrefService:browserState->GetPrefs()
+                                            prefName:prefs::kBottomOmnibox];
+  [_bottomOmniboxPref setObserver:self];
+  // Initialize to the correct value.
+  [self booleanDidChange:_bottomOmniboxPref];
+}
+
+/// Tears down pref observation.
+- (void)tearDownObservers {
+  [_bottomOmniboxPref stop];
+  [_bottomOmniboxPref setObserver:nil];
+  _bottomOmniboxPref = nil;
 }
 
 @end
