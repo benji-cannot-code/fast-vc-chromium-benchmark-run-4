f323ed6b3145847cc4cbe8fdbe6f9f1a3cbc1fe1
Viktor Semeniuk <vsemeniuk@google.com>
2023-12-05 15:50:36 +0000

    Adding feature flag for kRemoveUPMUnenrollment
    This CL also adds entry to fieldtrial_testing_config.json
    
    Bug: 1507820
    Change-Id: I519046981e146d2502f6cf9f3a82f05235ce002d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5087969
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1233361}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2567,6 +2567,9 @@ extern const char kRelatedSearchesAllLanguageDescription[];
 extern const char kRelatedSearchesName[];
 extern const char kRelatedSearchesDescription[];
 
+extern const char kRemoveUPMUnenrollmentName[];
+extern const char kRemoveUPMUnenrollmentDescription[];
+
 extern const char kRequestDesktopSiteAdditionsName[];
 extern const char kRequestDesktopSiteAdditionsDescription[];
 
