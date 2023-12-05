cb08461c90d23907faa8cb9349e3107236b9936d
Tom Anderson <thomasanderson@chromium.org>
2023-12-05 01:05:11 +0000

    [X11] Add WmSync for synchronizing with the window manager
    This class (along with GeometryCache that was recently added)
    is required for a ui::X11Window to reliably track bounds changes.
    
    R=sky
    
    Bug: 739898
    Change-Id: I0cfc0d976b128d5adc58097436ffbe39235105b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5080937
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1233115}

---
+++ ui/gfx/x/wm_sync_unittest.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/gfx/x/wm_sync.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+#include "ui/gfx/x/connection.h"
+
+namespace x11 {
+
+TEST(WmSyncTest, Basic) {
+  Connection* connection = Connection::Get();
+
+  bool synced = false;
+  WmSync wm_sync(connection,
+                 base::BindOnce([](bool* synced) { *synced = true; }, &synced));
+  while (!synced) {
+    connection->DispatchAll();
+  }
+}
+
+}  // namespace x11
