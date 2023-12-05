b439c2f919ea3fbf6cc38c898007351d157df1cc
Jason Lin <lxj@google.com>
2023-12-05 04:37:29 +0000

    Revert "[1/N] Use IS_OZONE_{X11, WAYLAND}"
    This reverts commit 1f809267ecc9f5c08545e0116b1c1b7009cefe05.
    
    Reason for revert: crbug.com/1508220
    
    Original change's description:
    > [1/N] Use IS_OZONE_{X11, WAYLAND}
    >
    > This CL substitutes OZONE_PLATFORM_* to IS_OZONE_* which is
    > platform-independent flags in //chrome/*.
    >
    > This CL does not change the behavior.
    >
    > Bug: 1500494
    > Change-Id: Ibc1b993e8993154d7e879d1878dbc8462a71c592
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5083096
    > Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1233114}
    
    Bug: 1500494
    Change-Id: I1c6fdc4d0ddc642ba7063997b80b8606b20cae74
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5086571
    Owners-Override: Jason Lin <lxj@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Jason Lin <lxj@google.com>
    Reviewed-by: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233183}

--- chrome/test/chromedriver/keycode_text_conversion.h
+++ chrome/test/chromedriver/keycode_text_conversion.h
@@ -9,8 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "build/build_config.h"
-#include "ui/base/ozone_buildflags.h"
 #include "ui/events/keycodes/keyboard_codes.h"
+
+#if BUILDFLAG(IS_OZONE)
+#include "ui/ozone/buildflags.h"
+#endif
 
 // These functions only support conversion of characters in the BMP
 // (Basic Multilingual Plane).
@@ -40,9 +43,7 @@ bool SwitchToUSKeyboardLayout();
 
 #if BUILDFLAG(IS_OZONE)
 void InitializeOzoneKeyboardEngineManager();
-#endif  // BUILDFLAG(IS_OZONE)
-
-#if BUILDFLAG(IS_OZONE_X11)
+#if BUILDFLAG(OZONE_PLATFORM_X11)
 // Uses X11 implementation if there is an X display.
 // TODO(crbug.com/987939): Support XKB.
 bool ConvertKeyCodeToTextOzone(ui::KeyboardCode key_code,
@@ -53,6 +54,7 @@ bool ConvertCharToKeyCodeOzone(char16_t key,
                                ui::KeyboardCode* key_code,
                                int* necessary_modifiers,
                                std::string* error_msg);
-#endif  // BUILDFLAG(IS_OZONE_X11)
+#endif  // BUILDFLAG(OZONE_PLATFORM_X11)
+#endif  // BUILDFLAG(IS_OZONE)
 
 #endif  // CHROME_TEST_CHROMEDRIVER_KEYCODE_TEXT_CONVERSION_H_
