331c9409ff7d0348eb2329edd0294a04c338b889
Mustaq Ahmed <mustaq@google.com>
2023-12-05 21:44:40 +0000

    Move a failing CHECK to non-empty rectangles.
    We recently added those CHECKs from DCHECKs further down in the
    call path, missed the empty rect check in-between!
    
    Fixed: 1505173
    Change-Id: I0b8fe8044801146dad7bb7a7243a9e735a970e8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5090688
    Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    Auto-Submit: Mustaq Ahmed <mustaq@chromium.org>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233581}

--- third_party/blink/renderer/core/input/event_handler.cc
+++ third_party/blink/renderer/core/input/event_handler.cc
@@ -1727,12 +1727,6 @@ bool EventHandler::BestNodeForHitTestResult(
     return false;
   }
 
-  CHECK(location.BoundingBox().Contains(location.Point()) ||
-        (location.BoundingBox().Right() == LayoutUnit::Max() &&
-         location.Point().left == LayoutUnit::Max()) ||
-        (location.BoundingBox().Bottom() == LayoutUnit::Max() &&
-         location.Point().top == LayoutUnit::Max()));
-
   gfx::Point touch_hotspot =
       frame_->View()->ConvertToRootFrame(location.RoundedPoint());
   gfx::Rect touch_rect =
@@ -1741,6 +1735,12 @@ bool EventHandler::BestNodeForHitTestResult(
   if (touch_rect.IsEmpty()) {
     return false;
   }
+
+  CHECK(location.BoundingBox().Contains(location.Point()) ||
+        (location.BoundingBox().Right() == LayoutUnit::Max() &&
+         location.Point().left == LayoutUnit::Max()) ||
+        (location.BoundingBox().Bottom() == LayoutUnit::Max() &&
+         location.Point().top == LayoutUnit::Max()));
 
   HeapVector<Member<Node>, 11> nodes(result.ListBasedTestResult());
 
