29f8e0fbb25c24c5435564d6ddaf1acd27bd304b
Avi Drissman <avi@chromium.org>
2023-12-05 00:43:27 +0000

    Replace various uses of Log2Floor and Log2Ceiling with std calls.
    This CL was uploaded by git cl split.
    
    R=mmenke@chromium.org
    
    Bug: 1414634
    Change-Id: I3812fc46a07555aeb47eb795e298d1256332b154
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5084968
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233099}

--- net/tools/huffman_trie/trie/trie_bit_buffer.cc
+++ net/tools/huffman_trie/trie/trie_bit_buffer.cc
@@ -5,9 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/tools/huffman_trie/trie/trie_bit_buffer.h"
 
+#include <bit>
+#include <cstdint>
 #include <ostream>
 
-#include "base/bits.h"
 #include "base/check.h"
 #include "net/tools/huffman_trie/bit_writer.h"
 
@@ -49,7 +50,7 @@ void TrieBitBuffer::WritePosition(uint32_t position, int32_t* last_position) {
     int32_t delta = position - *last_position;
     DCHECK(delta > 0) << "delta position is not positive.";
 
-    uint8_t number_of_bits = base::bits::Log2Floor(delta) + 1;
+    uint8_t number_of_bits = std::bit_width<uint32_t>(delta);
     DCHECK(number_of_bits <= kMaxBitLength)
         << "positive position delta too large.";
 
@@ -141,7 +142,7 @@ uint32_t TrieBitBuffer::WriteToBitWriter(BitWriter* writer) {
       uint32_t target = element.position;
       DCHECK(target < current) << "Reference is not backwards";
       uint32_t delta = current - target;
-      uint8_t delta_number_of_bits = base::bits::Log2Floor(delta) + 1;
+      uint8_t delta_number_of_bits = std::bit_width(delta);
       DCHECK(delta_number_of_bits < 32) << "Delta too large";
       writer->WriteBits(delta_number_of_bits, 5);
       writer->WriteBits(delta, delta_number_of_bits);
