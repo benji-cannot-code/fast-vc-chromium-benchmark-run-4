44dfdcafed470eabe4188c0a692b91073dc0104c
Julie Jeongeun Kim <jkim@igalia.com>
2023-12-05 12:26:28 +0000

    [CodeHealth] Fix dangling raw_ptr in QuitWithAppsController
    This CL fixes dangling raw_ptr by resetting `notification_profile_`
    when ProfileManager is destroyed.
    
    Bug: 1462042
    Change-Id: I68d36c83ac02d2849beb717b6d45d473e6a11133
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5047614
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233274}

--- chrome/browser/ui/cocoa/apps/quit_with_apps_controller_mac.h
+++ chrome/browser/ui/cocoa/apps/quit_with_apps_controller_mac.h
@@ -9,6 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/raw_ptr.h"
+#include "base/scoped_observation.h"
+#include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/profiles/profile_manager_observer.h"
 #include "ui/message_center/public/cpp/notification_delegate.h"
 
 class PrefRegistrySimple;
@@ -20,7 +23,8 @@ class Notification;
 
 // QuitWithAppsController checks whether any apps are running and shows a
 // notification to quit all of them.
-class QuitWithAppsController : public message_center::NotificationDelegate {
+class QuitWithAppsController : public message_center::NotificationDelegate,
+                               public ProfileManagerObserver {
  public:
   static const char kQuitWithAppsNotificationID[];
 
@@ -33,6 +37,9 @@ class QuitWithAppsController : public message_center::NotificationDelegate {
   void Close(bool by_user) override;
   void Click(const absl::optional<int>& button_index,
              const absl::optional<std::u16string>& reply) override;
+
+  // ProfileManagerObserver:
+  void OnProfileManagerDestroying() override;
 
   // Attempt to quit Chrome. This will display a notification and return false
   // if there are apps running.
@@ -49,7 +56,10 @@ class QuitWithAppsController : public message_center::NotificationDelegate {
   // the instance here because when we want to cancel the notification we need
   // to provide the profile which was used to add the notification previously.
   // Not owned by this class.
-  raw_ptr<Profile, LeakedDanglingUntriaged> notification_profile_ = nullptr;
+  raw_ptr<Profile> notification_profile_ = nullptr;
+
+  base::ScopedObservation<ProfileManager, ProfileManagerObserver>
+      profile_manager_observation_{this};
 
   // Whether to suppress showing the notification for the rest of the session.
   bool suppress_for_session_ = false;
