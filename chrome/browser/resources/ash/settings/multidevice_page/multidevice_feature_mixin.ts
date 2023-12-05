15ede8299cfaffb89e1528ea8cc4a1abb459b26a
Moteva <moteva@google.com>
2023-12-05 17:36:26 +0000

    CrOS Settings: Change icon colours for Connected Devices page
    When feature OsSettingsRevampWayfinding is enabled, the icons will use
    the new ChromeOS specific colours.
    Also changed the related search result's icon.
    
    Connected Devices: https://screenshot.googleplex.com/3DVY7aqmJaaa3vE
    Search: https://screenshot.googleplex.com/GfEHvMKmaGrji86
    Android phone: https://screenshot.googleplex.com/9ud6VQ83pEhSmvws
    Mock: http://shortn/_ml8VGaviOS
    
    BUG=b/311201264
    TEST=Manually tested that the colour changes when the feature is
    enabled.
    
    Change-Id: Ibfd56f633fd421bf63254fe1e09f80e50c18c67e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5080516
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Nikki Moteva <moteva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1233426}

--- chrome/browser/resources/ash/settings/multidevice_page/multidevice_feature_mixin.ts
+++ chrome/browser/resources/ash/settings/multidevice_page/multidevice_feature_mixin.ts
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {I18nMixin, I18nMixinInterface} from 'chrome://resources/cr_elements/i18n_mixin.js';
 import {dedupingMixin, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
+import {isRevampWayfindingEnabled} from '../common/load_time_booleans.js';
 import {Constructor} from '../common/types.js';
 
 import {MultiDeviceFeature, MultiDeviceFeatureState, MultiDevicePageContentData, MultiDeviceSettingsMode, PhoneHubFeatureAccessStatus} from './multidevice_constants.js';
@@ -239,9 +240,13 @@ export const MultiDeviceFeatureMixin = dedupingMixin(
          * (i.e. [iron-iconset-svg name]:[SVG <g> tag id]) for a given feature.
          */
         getIconName(feature: MultiDeviceFeature): string {
+          const deviceIcon = isRevampWayfindingEnabled() ?
+              'os-settings:connected-devices-android-phone' :
+              'os-settings:multidevice-better-together-suite';
+
           switch (feature) {
             case MultiDeviceFeature.BETTER_TOGETHER_SUITE:
-              return 'os-settings:multidevice-better-together-suite';
+              return deviceIcon;
             case MultiDeviceFeature.SMART_LOCK:
               return 'os-settings:multidevice-smart-lock';
             case MultiDeviceFeature.PHONE_HUB:
@@ -249,7 +254,7 @@ export const MultiDeviceFeatureMixin = dedupingMixin(
             case MultiDeviceFeature.PHONE_HUB_NOTIFICATIONS:
             case MultiDeviceFeature.PHONE_HUB_TASK_CONTINUATION:
             case MultiDeviceFeature.ECHE:
-              return 'os-settings:multidevice-better-together-suite';
+              return deviceIcon;
             case MultiDeviceFeature.WIFI_SYNC:
               return 'os-settings:multidevice-wifi-sync';
             default:
