c70c235a05fe76d7c1e874b697f66c0426c0d2f7
Sky Malice <skym@chromium.org>
2023-12-05 17:30:11 +0000

    Remove fully qualified setAlphaComponent.
    Change-Id: I239542b119e1825b4df972a27ac905ba22322d3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5087091
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233421}

--- chrome/android/junit/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutTabTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutTabTest.java
@@ -161,7 +161,7 @@ public class StripLayoutTabTest {
 
         // Normal.
         expectedColor =
-                androidx.core.graphics.ColorUtils.setAlphaComponent(
+                ColorUtils.setAlphaComponent(
                         SemanticColorUtils.getDefaultIconColorAccent1(mContext),
                         (int) (StripLayoutTab.DIVIDER_FOLIO_LIGHT_OPACITY * 255));
         assertEquals(
