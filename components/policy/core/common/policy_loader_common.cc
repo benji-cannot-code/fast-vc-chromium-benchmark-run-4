4899d23b206cd4be204ff4f94f1192caee98ea7e
Joel Hockey <joelhockey@chromium.org>
2023-12-05 07:53:16 +0000

    Remove reference to deprecated kChromeCleanupReportingEnabled
    Policy deprecated in crrev.com/c/4834768, but not all code deleted
    before M122.
    
    Windows build failing after VERSION update
    
    Change-Id: I98e3b9518d00cb46d280bb4bb162cb9b06ecdf7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5087235
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Jason Lin <lxj@google.com>
    Owners-Override: Jason Lin <lxj@google.com>
    Commit-Queue: Jason Lin <lxj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1233186}

--- components/policy/core/common/policy_loader_common.cc
+++ components/policy/core/common/policy_loader_common.cc
@@ -71,8 +71,6 @@ const char* kSensitivePolicies[] = {
     key::kMetricsReportingEnabled,
 #endif
 #if BUILDFLAG(IS_WIN)
-    key::kChromeCleanupEnabled,
-    key::kChromeCleanupReportingEnabled,
     key::kSafeBrowsingForTrustedSourcesEnabled,
 #endif
 };
