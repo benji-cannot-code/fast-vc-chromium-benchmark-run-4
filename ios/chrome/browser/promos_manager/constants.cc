461c1f3d5f16b8eb4508cf334b632daa92ba2f4d
Samar <samarchehade@google.com>
2023-12-05 18:07:05 +0000

    [iOS][Waffle] Remove promos manager code for the choice screen
    We decided to use an app state agent instead as a triggering mechanism, so we can now delete the code that relates to the promos manager for the choice screen.
    
    This change is step (3/3) of a three-step change:
    * https://crrev.com/c/5053885 Display the choice screen with an app agent
    * https://crrev.com/i/6706554 Clean up obsolete code from the internal provider
    => https://crrev.com/c/5060571 Remove obsolete promos manager code
    
    Bug: b/306576460
    Change-Id: I6fdf0ee4a5951d533f403d5ae766705842fc7f0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5060571
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1233451}

--- ios/chrome/browser/promos_manager/constants.cc
+++ ios/chrome/browser/promos_manager/constants.cc
@@ -42,10 +42,6 @@ std::optional<Promo> PromoForName(base::StringPiece promo) {
   if (promo == "promos_manager::Promo::WhatsNew")
     return promos_manager::Promo::WhatsNew;
 
-  if (promo == "promos_manager::Promo::Choice") {
-    return promos_manager::Promo::Choice;
-  }
-
   if (promo == "promos_manager::Promo::PostRestoreDefaultBrowserAlert") {
     return promos_manager::Promo::PostRestoreDefaultBrowserAlert;
   }
@@ -81,8 +77,6 @@ base::StringPiece ShortNameForPromo(Promo promo) {
       return "PostRestoreSignInAlert";
     case promos_manager::Promo::WhatsNew:
       return "WhatsNew";
-    case promos_manager::Promo::Choice:
-      return "Choice";
     case promos_manager::Promo::PostRestoreDefaultBrowserAlert:
       return "PostRestoreDefaultBrowserAlert";
     case promos_manager::Promo::DefaultBrowserRemindMeLater:
