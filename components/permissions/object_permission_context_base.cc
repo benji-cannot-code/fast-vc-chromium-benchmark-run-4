b3cf2eac01e75fefa29a574f8fa97c0683fd6957
Daseul Lee <dslee@chromium.org>
2023-12-05 17:17:18 +0000

    [SafetyCheck] Track last visit time for allowlisted chooser permissions.
    Previously, Safety Check was launched for permissions that are
    ContentSetting-based. This change is the first step to include chooser
    permissions, which are WebsiteSetting-based. Initially, we will start
    with allowlisted chooser permissions and expand it to include others.
    
    Bug: 1498540
    Change-Id: I09af3156d962ea29bb34bc3a5dc77ed936898dd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5002407
    Reviewed-by: Florian Jacky <fjacky@chromium.org>
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Daseul Lee <dslee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233410}

--- components/permissions/object_permission_context_base.cc
+++ components/permissions/object_permission_context_base.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/containers/contains.h"
+#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/observer_list.h"
 #include "base/strings/utf_string_conversions.h"
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/content_settings/core/common/content_settings_pattern.h"
+#include "components/permissions/features.h"
 #include "url/origin.h"
 
 namespace permissions {
@@ -304,9 +306,20 @@ void ObjectPermissionContextBase::SaveWebsiteSetting(
   }
   base::Value::Dict website_setting_value;
   website_setting_value.Set(kObjectListKey, std::move(objects_list));
+
+  content_settings::ContentSettingConstraints constraints;
+#if !BUILDFLAG(IS_ANDROID)
+  if (base::FeatureList::IsEnabled(
+          features::kRecordChooserPermissionLastVisitedTimestamps)) {
+    if (content_settings::CanTrackLastVisit(data_content_settings_type_)) {
+      constraints.set_track_last_visit_for_autoexpiration(true);
+    }
+  }
+#endif  // !BUILDFLAG(IS_ANDROID)
+
   host_content_settings_map_->SetWebsiteSettingDefaultScope(
       origin.GetURL(), GURL(), data_content_settings_type_,
-      base::Value(std::move(website_setting_value)));
+      base::Value(std::move(website_setting_value)), constraints);
 }
 
 void ObjectPermissionContextBase::ScheduleSaveWebsiteSetting(
