9ae57a28db9d6ca4f7d405359be8fc06cd413e46
Ed Chin <edchin@google.com>
2023-12-05 01:16:04 +0000

    Fix regenerate to be compose instead of rewrite
    A rewrite request has to have either a tone or length.
    Regenerate just sends the same compose request again.
    The backend is random enough where the same output will not
    be repeated.
    
    Bug: 314323506
    Change-Id: I8ce1214f8c41dbd5685973ba64588895349993bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5078385
    Commit-Queue: edchin <edchin@google.com>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233116}

--- chrome/browser/resources/compose/app.ts
+++ chrome/browser/resources/compose/app.ts
@@ -336,10 +336,6 @@ export class ComposeAppElement extends ComposeAppElementBase {
     }
   }
 
-  private onRefresh_() {
-    this.rewrite_(/*style=*/ {});
-  }
-
   private onSubmit_() {
     this.isSubmitEnabled_ = this.$.textarea.validate();
     if (!this.isSubmitEnabled_) {
