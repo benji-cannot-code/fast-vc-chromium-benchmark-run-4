795c34a93308a93cc5cb86641dc9f79674405a97
Lucas Radaelli <lucasradaelli@google.com>
2023-12-05 13:25:31 +0000

    [a11y] Implements module loading for atp.
        This change implements JS module loading for atp.
    
        Full details discussed at http://go/atp-file-loading.
    
        Bug: b:266856702
        Test: V8ManagerTest.*
        AX-Relnotes: n/a.'
    
    Change-Id: Ic61a64ac5e0d251e3ad63c2e6ca6c4d9a20e013f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5035290
    Commit-Queue: Lucas Radaelli <lucasradaelli@google.com>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233295}

--- services/accessibility/features/mojo/test/shim.js
+++ services/accessibility/features/mojo/test/shim.js
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// The ATP V8 instance does not define 'self', which is needed by mojom.
-const self = this;
 
 // ATP doesn't have console.* yet, so shim these over.
 const console = atpconsole;
