7ecadbbd50b76e62dc5c76fc9461f7c9069c48eb
Kush Sinha <sinhak@chromium.org>
2023-12-05 12:00:08 +0000

    Ash: Add getDeviceId message handler in lock screen
    Add a message handler for "getDeviceId" message from Gaia. Please check
    the design document in http://b/309928475 for context.
    
    Bug: b/313815893
    Test: Manually
    Change-Id: Ia1682f2d4f6c881689ceff93cb48e4af7a9d950f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5070482
    Reviewed-by: Andrey Davydov <andreydav@google.com>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233268}

--- chrome/browser/ui/webui/ash/lock_screen_reauth/lock_screen_reauth_handler.h
+++ chrome/browser/ui/webui/ash/lock_screen_reauth/lock_screen_reauth_handler.h
@@ -40,6 +40,7 @@ class LockScreenReauthHandler : public content::WebUIMessageHandler {
   void HandleUpdateUserPassword(const base::Value::List&);
   void HandleOnPasswordTyped(const base::Value::List& value);
   void HandleWebviewLoadAborted(int error_code);
+  void HandleGetDeviceId(const std::string& callback_id);
 
   bool IsAuthenticatorLoaded(base::OnceClosure callback);
 
