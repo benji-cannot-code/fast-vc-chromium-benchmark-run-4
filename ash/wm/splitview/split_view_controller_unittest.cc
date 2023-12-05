d31476da26166de8b6589ea9ab8cc270ca23ea01
sophiewen <sophiewen@chromium.org>
2023-12-05 18:04:12 +0000

    snap-groups: Reland "Move bounds calculations to split_view_utils"
    This is a reland of https://crrev.com/c/5076373.
    
    This refactors snapped bounds calculations from SplitViewController to
    `split_view_utils::CalculateSnappedWindowBoundsInScreen()`. This is
    needed for SnapGroups, to get the snapped bounds independent of
    `SplitViewController::split_view_divider_`, since each SnapGroup will
    have its own divider. Each SnapGroup will pass its divider's divider
    position and divider width to `CalculateSnappedWindowBoundsInScreen()`.
    
    Also moves other util functions to split_view_utils.
    
    Test: existing
    Change-Id: I40f8c69bf06cbe83a74482a38b5e00f2bb7d1da4
    Fixed: b/296935443
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5063361
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1231113}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5086489
    Cr-Commit-Position: refs/heads/main@{#1233448}

--- ash/wm/splitview/split_view_controller_unittest.cc
+++ ash/wm/splitview/split_view_controller_unittest.cc
@@ -3873,7 +3873,7 @@ TEST_F(SplitViewKeyboardTest, PushUpBottomWindow) {
                               display::Display::RotationSource::ACTIVE);
   EXPECT_EQ(chromeos::OrientationType::kPortraitPrimary,
             test_api.GetCurrentOrientation());
-  EXPECT_FALSE(split_view_controller()->IsPhysicalLeftOrTop(
+  EXPECT_FALSE(IsPhysicalLeftOrTop(
       SplitViewController::SnapPosition::kSecondary, bottom_window.get()));
 
   const gfx::Rect keyboard_bounds =
@@ -3967,7 +3967,7 @@ TEST_F(SplitViewKeyboardTest, PushUpBottomWindowLimitHeight) {
                               display::Display::RotationSource::ACTIVE);
   EXPECT_EQ(chromeos::OrientationType::kPortraitPrimary,
             test_api.GetCurrentOrientation());
-  EXPECT_FALSE(split_view_controller()->IsPhysicalLeftOrTop(
+  EXPECT_FALSE(IsPhysicalLeftOrTop(
       SplitViewController::SnapPosition::kSecondary, bottom_window.get()));
 
   const gfx::Rect keyboard_bounds =
@@ -4058,8 +4058,8 @@ TEST_F(SplitViewKeyboardTest, RestoreByActivatingTopWindow) {
                               display::Display::RotationSource::ACTIVE);
   EXPECT_EQ(chromeos::OrientationType::kPortraitPrimary,
             test_api.GetCurrentOrientation());
-  EXPECT_TRUE(split_view_controller()->IsPhysicalLeftOrTop(
-      SplitViewController::SnapPosition::kPrimary, top_window.get()));
+  EXPECT_TRUE(IsPhysicalLeftOrTop(SplitViewController::SnapPosition::kPrimary,
+                                  top_window.get()));
 
   const gfx::Rect keyboard_bounds =
       keyboard_controller()->GetKeyboardWindow()->GetBoundsInScreen();
@@ -4126,7 +4126,7 @@ TEST_F(SplitViewKeyboardTest, NoInputField) {
                               display::Display::RotationSource::ACTIVE);
   EXPECT_EQ(chromeos::OrientationType::kPortraitPrimary,
             test_api.GetCurrentOrientation());
-  EXPECT_FALSE(split_view_controller()->IsPhysicalLeftOrTop(
+  EXPECT_FALSE(IsPhysicalLeftOrTop(
       SplitViewController::SnapPosition::kSecondary, bottom_window.get()));
 
   const gfx::Rect orig_bottom_bounds = bottom_window->GetBoundsInScreen();
