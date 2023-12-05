70aece2b05f5dad89684a1fc8668032ccf21b7e9
Jérémie Boulic <jboulic@chromium.org>
2023-12-05 01:47:25 +0000

    Files F2: directory-changed custom event
    The DirectoryChangeEvent custom event now belongs to the DirectoryModel.
    Its type definition file is not needed anymore.
    
    Bug: b:289003444
    Change-Id: I7742447707641fb4a42ce79c3d95c7b42084c84e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5086427
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233130}

--- ui/file_manager/file_manager/foreground/js/app_state_controller.js
+++ ui/file_manager/file_manager/foreground/js/app_state_controller.js
@@ -219,7 +219,7 @@ export class AppStateController {
   onDirectoryChanged_(event) {
     const
         customEvent = /**
-                         @type {import('../../definitions/directory_change_event.js').DirectoryChangeEvent}
+                         @type {import('./directory_model.js').DirectoryChangeEvent}
                            */
         (event);
     if (!customEvent.detail.newDirEntry) {
