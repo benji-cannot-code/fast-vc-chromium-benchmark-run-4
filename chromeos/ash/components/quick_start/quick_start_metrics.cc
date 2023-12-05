74c2a05fbbe05734ecea048bce9cf76a20d6db59
Kush Sinha <sinhak@chromium.org>
2023-12-05 12:06:22 +0000

    QuickStart: Add metrics for overall attestation success / failure
    Bug: b/314143137
    Change-Id: I33902b14301da83dd78697be608603be7003e1d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5079745
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1233270}

--- chromeos/ash/components/quick_start/quick_start_metrics.cc
+++ chromeos/ash/components/quick_start/quick_start_metrics.cc
@@ -15,6 +15,8 @@ namespace {
 
 constexpr const char kAttestationCertificateFailureReasonHistogramName[] =
     "QuickStart.AttestationCertificate.FailureReason";
+constexpr const char kAttestationCertificateFetchResultHistogramName[] =
+    "QuickStart.AttestationCertificate.FetchResult";
 constexpr const char kWifiTransferResultHistogramName[] =
     "QuickStart.WifiTransferResult";
 constexpr const char kWifiTransferResultFailureReasonHistogramName[] =
@@ -158,6 +160,11 @@ void QuickStartMetrics::RecordAttestationCertificateRequestEnded(
   if (error_code) {
     base::UmaHistogramEnumeration(
         kAttestationCertificateFailureReasonHistogramName, error_code.value());
+    base::UmaHistogramBoolean(kAttestationCertificateFetchResultHistogramName,
+                              false);
+  } else {
+    base::UmaHistogramBoolean(kAttestationCertificateFetchResultHistogramName,
+                              true);
   }
 }
 
