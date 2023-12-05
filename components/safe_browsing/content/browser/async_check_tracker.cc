e82d190319e49616beb2f9f0d42b2ccb67793ca8
Xinghui Lu <xinghuilu@chromium.org>
2023-12-05 17:07:52 +0000

    [AsyncSB] Introduce async_sb_checker in BrowserUrlLoaderThrottle
    When async real time SB check is enabled, BULT will create two
    UrlCheckerOnSB: one for sync check, and one for async check. The sync
    check only performs local database check. It defers
    WillProcessResponse if it is not yet completed. The async check, on
    the other hand, performs real-time check. If BULT is ready to proceed
    (sync check is completed and WillProcessResponse is called), but the
    async check has not yet completed, it will be transferred to
    AsyncCheckTracker to continue the check.
    
    Below is a breakdown of the expected behavior:
    Definition:
      * Event A: Sync check is completed.
      * Event B: Async check is completed.
      * Event C: WillProcessResponse is called.
      * defer: UrlLoader is deferred.
      * transfer: Async checker is transferred to tracker.
    Order of events and the expected behavior:
      * A -> B -> C: no defer, no transfer.
      * A -> C -> B: no defer, transfer.
      * B -> A -> C: no defer, no transfer.
      * B -> C -> A: defer, no transfer.
      * C -> A -> B: defer, transfer.
      * C -> B -> A: defer, no transfer.
    
    Cases that the URL is dangerous:
    If sync check returns dangerous:
      * the async check will be canceled immediately if it is not
        completed.
    If async check returns dangerous:
      * If it has not yet been transferred to tracker:
        * the sync check will be canceled immediately.
      * If it is transferred to tracker:
        * The tracker may trigger a warning. This will be implemented in
          a follow up CL.
    
    Bug: 1501194
    Change-Id: I82dc33e255c7624826c9b5be331567983186b125
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5055594
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233405}

--- components/safe_browsing/content/browser/async_check_tracker.cc
+++ components/safe_browsing/content/browser/async_check_tracker.cc
@@ -4,7 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "components/safe_browsing/content/browser/async_check_tracker.h"
+
 #include "components/safe_browsing/content/browser/base_ui_manager.h"
+#include "components/safe_browsing/core/common/features.h"
+#include "content/public/browser/browser_thread.h"
 
 namespace safe_browsing {
 
@@ -25,7 +28,22 @@ AsyncCheckTracker::AsyncCheckTracker(content::WebContents* web_contents,
     : content::WebContentsUserData<AsyncCheckTracker>(*web_contents),
       ui_manager_(std::move(ui_manager)) {}
 
-AsyncCheckTracker::~AsyncCheckTracker() = default;
+AsyncCheckTracker::~AsyncCheckTracker() {
+  if (!base::FeatureList::IsEnabled(kSafeBrowsingOnUIThread)) {
+    content::GetIOThreadTaskRunner({})->DeleteSoon(FROM_HERE,
+                                                   std::move(pending_checker_));
+  }
+}
+
+void AsyncCheckTracker::TransferUrlChecker(
+    std::unique_ptr<UrlCheckerOnSB> checker) {
+  // TODO(crbug.com/1501194): Replace callbacks in checker.
+  pending_checker_ = std::move(checker);
+}
+
+bool AsyncCheckTracker::HasPendingCheckerForTesting() {
+  return !!pending_checker_;
+}
 
 base::WeakPtr<AsyncCheckTracker> AsyncCheckTracker::GetWeakPtr() {
   return weak_factory_.GetWeakPtr();
