b185ddb3df862c67eaae28384ad6f839e4477231
dpapad <dpapad@chromium.org>
2023-12-05 17:53:24 +0000

    WebUI: Add CrLitElement superclass holding additional functionality.
    Specifically it holds logic that populates a `this.$` helper object to
    access children DOM nodes, similar to Polymer.
    
    Also making other necessary changes for the tests to work
     - Updating tools/typescript/path_mappings.py to automatically add
       path mappings for the //third_party/lit/v3_0:build_ts dependency.
     - Updating webui::EnableTrustedTypesCSP() to add 'lit-html' in the
       trusted types CSP header.
    
    Bug: 1503223
    Change-Id: Id3cbb3f5e3e6637e9852dae4dd874a7a6140588c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5078922
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233439}

--- tools/typescript/path_mappings.py
+++ tools/typescript/path_mappings.py
@@ -49,6 +49,13 @@ def _add_third_party_polymer_mappings(path_mappings, root_src_dir):
   ]
 
 
+def _add_third_party_lit_mappings(path_mappings, root_gen_dir):
+  path_mappings[f'//third_party/lit/v3_0:build_ts'] = [
+      ('//resources/lit/v3_0/lit.rollup.js',
+       f'{root_gen_dir}/third_party/lit/v3_0/lit.d.ts'),
+  ]
+
+
 # Ash-only
 def _add_ash_mappings(path_mappings, root_gen_dir, root_src_dir):
   path_mappings['//ash/webui/common/resources:build_ts'] = [(
@@ -75,6 +82,7 @@ def GetDepToPathMappings(root_gen_dir, root_src_dir, platform):
 
   _add_ui_webui_resources_mappings(path_mappings, root_gen_dir)
   _add_third_party_polymer_mappings(path_mappings, root_src_dir)
+  _add_third_party_lit_mappings(path_mappings, root_gen_dir)
 
   if platform == 'chromeos_ash':
     _add_ash_mappings(path_mappings, root_gen_dir, root_src_dir)
