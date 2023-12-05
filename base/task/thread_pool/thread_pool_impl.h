7439fa48e632d528634b8346d883b5bc370e4089
Sean Maher <spvw@chromium.org>
2023-12-05 07:57:27 +0000

    Change the boundary of ThreadGroupImpl and factor out delegate
    This CL does not add or remove code, just performs a refactor to allow
    adding different types of ThreadGroups. ThreadGroupImpl now contains
    all code related to interacting with WaitableEvents.
    
    Bug: 1481215
    Change-Id: I2cac839d59fea95cb4774e16fdb6d8a6dc91ce73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5026797
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Sean Maher <spvw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233194}

--- base/task/thread_pool/thread_pool_impl.h
+++ base/task/thread_pool/thread_pool_impl.h
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool/task_source.h"
 #include "base/task/thread_pool/task_tracker.h"
 #include "base/task/thread_pool/thread_group.h"
-#include "base/task/thread_pool/thread_group_impl.h"
 #include "base/task/thread_pool/thread_pool_instance.h"
 #include "base/task/updateable_sequenced_task_runner.h"
 #include "base/thread_annotations.h"
