be8d300d17759f606ca918a82a676148c502692c
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2023-12-05 21:47:15 +0000

    Revert "[heap. blink_gc_plugin] Fix Pdfium exemption for plugin check"
    This reverts commit 4fc1a13e812b1a553a9bb6db35b285c754c69112.
    
    Reason for revert:
    LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8762553893711656161
    
    Sample failed build: https://ci.chromium.org/b/8762553893711656161
    
    If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8762553893711656161&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F5087869
    
    Original change's description:
    > [heap. blink_gc_plugin] Fix Pdfium exemption for plugin check
    >
    > Exemption was looking for "pdfium/" in the path but that doesn't hold
    > for standalone pdfium.
    >
    > Drive-by: remove unused `ignored_classes`.
    >
    > Bug: chromium:1470687
    > Change-Id: I14694e8ee66b4cf208fe38d36bbfb27770ffca17
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5087869
    > Commit-Queue: Omer Katz <omerkatz@chromium.org>
    > Auto-Submit: Omer Katz <omerkatz@chromium.org>
    > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1233239}
    >
    
    Bug: chromium:1470687
    Change-Id: I6ec617f306a6a5663049a1baa490a8a6be3b6441
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5090712
    Reviewed-by: Hao Liu <haoliuk@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Stefan Zager <szager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1233584}

--- tools/clang/blink_gc_plugin/BlinkGCPluginConsumer.cpp
+++ tools/clang/blink_gc_plugin/BlinkGCPluginConsumer.cpp
@@ -91,9 +91,9 @@ BlinkGCPluginConsumer::BlinkGCPluginConsumer(
   options_.checked_namespaces.insert("cppgc");
 
   // Add Pdfium subfolders containing GCed classes.
-  for (const auto& pdfium_dir : Config::kPdfiumDirectories) {
-    options_.checked_directories.push_back(pdfium_dir);
-  }
+  options_.checked_directories.push_back("fpdfsdk/");
+  options_.checked_directories.push_back("fxjs/");
+  options_.checked_directories.push_back("xfa/");
 
   // Ignore GC implementation files.
   options_.ignored_directories.push_back(
@@ -632,7 +632,8 @@ bool BlinkGCPluginConsumer::IsIgnoredClass(RecordInfo* info) {
   const std::string SameSizeAs = "SameSizeAs";
   if (info->name().compare(0, SameSizeAs.size(), SameSizeAs) == 0)
     return true;
-  return false;
+  return (options_.ignored_classes.find(info->name()) !=
+          options_.ignored_classes.end());
 }
 
 bool BlinkGCPluginConsumer::InIgnoredDirectory(RecordInfo* info) {
