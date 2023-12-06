c01d835f12099f62c6ba5a3418d25aaa02b0d98e
Calder Kitagawa <ckitagawa@chromium.org>
2023-12-06 21:36:13 +0000

    [Tab Switcher] Migrate MultiThumbnailCardProvider to supplier
    MultiThumbnailCardProvider should use a supplier for the current
    TabModelFilter rather than TabModelSelector.
    
    Bug: 1505772
    Change-Id: I685559bdc1670cf4ce0583a0413687ff53c41630
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5089634
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234182}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherCoordinator.java
@@ -225,12 +225,14 @@ public class TabSwitcherCoordinator
 
             mTabSwitcherCustomViewManager = new TabSwitcherCustomViewManager(mMediator);
 
+            var currentTabModelFilterSupplier =
+                    tabModelSelector.getTabModelFilterProvider().getCurrentTabModelFilterSupplier();
             mMultiThumbnailCardProvider =
                     new MultiThumbnailCardProvider(
                             activity,
                             mBrowserControlsStateProvider,
                             tabContentManager,
-                            tabModelSelector);
+                            currentTabModelFilterSupplier);
 
             PseudoTab.TitleProvider titleProvider =
                     (context, tab) -> {
@@ -478,7 +480,8 @@ public class TabSwitcherCoordinator
                 }
             }
 
-            mMultiThumbnailCardProvider.initWithNative();
+            mMultiThumbnailCardProvider.initWithNative(
+                    mTabModelSelector.getModel(false).getProfile());
             mMediator.initWithNative(mSnackbarManager);
             // TODO(crbug.com/1222762): Only call setUpPriceTracking in GRID TabSwitcher.
             setUpPriceTracking(mActivity, mModalDialogManager);
