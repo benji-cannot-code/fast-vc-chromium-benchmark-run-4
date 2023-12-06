a806f6c96737f6bfcc9c7e5bba1267ec6072b9e2
Alex Chen <alexwchen@chromium.org>
2023-12-06 17:05:16 +0000

    Remove ClientSidePhishingProtectionAllowed policy
    ClientSidePhishingProtectionAllowed was added to chrome on CrOS in M114
    as unlisted. Since Standard Safe Browsing is an essential service for
    Data Processor Mode on ChromeOS, this policy is no longer needed.
    Since the policy was never added to Admin Console and no UI was added
    to allow end-users to set the policy, the pref and policy can be
    removed completely.
    
    BYPASS_POLICY_COMPATIBILITY_CHECK=The policy was never exposed to
    end-users on Admin Console and was left as unlisted.
    
    Bug: b:280429868
    Change-Id: If8fd85a87bcd9ed935583bd0c0c5fa529e550e02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5084990
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Alex Chen <alexwchen@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233997}

--- components/safe_browsing/core/common/safe_browsing_prefs.cc
+++ components/safe_browsing/core/common/safe_browsing_prefs.cc
@@ -71,8 +71,6 @@ GURL GetSimplifiedURL(const GURL& url) {
 namespace prefs {
 const char kSafeBrowsingCsdPingTimestamps[] =
     "safebrowsing.csd_ping_timestamps";
-const char kSafeBrowsingCsdPhishingProtectionAllowedByPolicy[] =
-    "safebrowsing.csd_phishing_protection_allowed_by_policy";
 const char kSafeBrowsingDeepScanningEnabled[] =
     "safebrowsing.deep_scanning_enabled";
 const char kSafeBrowsingEnabled[] = "safebrowsing.enabled";
@@ -222,11 +220,6 @@ bool IsSafeBrowsingExtensionControlled(const PrefService& prefs) {
              ->IsExtensionControlled();
 }
 
-bool IsCsdPhishingProtectionAllowed(const PrefService& prefs) {
-  return prefs.GetBoolean(
-      prefs::kSafeBrowsingCsdPhishingProtectionAllowedByPolicy);
-}
-
 bool IsSafeBrowsingExtensionProtectionAllowed(const PrefService& prefs) {
   return prefs.GetBoolean(
       prefs::kSafeBrowsingExtensionProtectionAllowedByPolicy);
@@ -258,8 +251,6 @@ void RecordExtendedReportingMetrics(const PrefService& prefs) {
 
 void RegisterProfilePrefs(PrefRegistrySimple* registry) {
   registry->RegisterListPref(prefs::kSafeBrowsingCsdPingTimestamps);
-  registry->RegisterBooleanPref(
-      prefs::kSafeBrowsingCsdPhishingProtectionAllowedByPolicy, true);
   registry->RegisterBooleanPref(prefs::kSafeBrowsingScoutReportingEnabled,
                                 false);
   registry->RegisterBooleanPref(
@@ -437,10 +428,6 @@ base::Value::List GetSafeBrowsingPoliciesList(PrefService* prefs) {
   preferences_list.Append(login_urls);
   preferences_list.Append(prefs::kPasswordProtectionLoginURLs);
 
-  preferences_list.Append(prefs->GetBoolean(
-      prefs::kSafeBrowsingCsdPhishingProtectionAllowedByPolicy));
-  preferences_list.Append(
-      prefs::kSafeBrowsingCsdPhishingProtectionAllowedByPolicy);
   preferences_list.Append(prefs->GetBoolean(
       prefs::kSafeBrowsingExtensionProtectionAllowedByPolicy));
   preferences_list.Append(
