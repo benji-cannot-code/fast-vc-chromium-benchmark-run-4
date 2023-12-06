00ea2e1c2ebdde4b33e5cd2e1d2b81644bf44849
Shibalik Mohapatra <shibalik@chromium.org>
2023-12-06 22:18:32 +0000

    [CodeHealth] Remove dangling pointer from webrequest_rules_registry.h
    Bug: 1462075
    Change-Id: I282c127e97374d36d292d23a685d9d67c268524f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5089792
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234207}

--- extensions/browser/api/declarative_webrequest/webrequest_rules_registry.cc
+++ extensions/browser/api/declarative_webrequest/webrequest_rules_registry.cc
@@ -48,8 +48,7 @@ WebRequestRulesRegistry::WebRequestRulesRegistry(
     : RulesRegistry(browser_context,
                     declarative_webrequest_constants::kOnRequest,
                     cache_delegate,
-                    rules_registry_id),
-      browser_context_(browser_context) {}
+                    rules_registry_id) {}
 
 std::set<const WebRequestRule*> WebRequestRulesRegistry::GetMatches(
     const WebRequestData& request_data_without_ids) const {
@@ -153,7 +152,7 @@ std::string WebRequestRulesRegistry::AddRulesImpl(
   std::string error;
   RulesVector new_webrequest_rules;
   new_webrequest_rules.reserve(rules.size());
-  const Extension* extension = ExtensionRegistry::Get(browser_context_)
+  const Extension* extension = ExtensionRegistry::Get(browser_context())
                                    ->enabled_extensions()
                                    .GetByID(extension_id);
   RulesMap& registered_rules = webrequest_rules_[extension_id];
@@ -287,7 +286,8 @@ WebRequestRulesRegistry::~WebRequestRulesRegistry() = default;
 
 base::Time WebRequestRulesRegistry::GetExtensionInstallationTime(
     const std::string& extension_id) const {
-  return ExtensionPrefs::Get(browser_context_)->GetLastUpdateTime(extension_id);
+  return ExtensionPrefs::Get(browser_context())
+      ->GetLastUpdateTime(extension_id);
 }
 
 void WebRequestRulesRegistry::ClearCacheOnNavigation() {
