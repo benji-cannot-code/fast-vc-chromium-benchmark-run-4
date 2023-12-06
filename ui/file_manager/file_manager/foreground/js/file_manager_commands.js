aa88275d52a6dd89ba7ca087dfa7e4368f1ea501
Ben Reich <benreich@chromium.org>
2023-12-06 22:58:08 +0000

    [FilesTS] Update entries == 0 to entries.length === 0
    In preparation for converting file_manager_commands, a TS error gets
    thrown as the 2 types are mismatched (entries is an array whilst 0 is a
    number). Make them similar to avoid this error getting lost in the
    conversion.
    
    Bug: b/289003444
    Change-Id: I7ba305ac7fdcd9f86ae1cc49a0157adb1a89ef1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5093008
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234223}

--- ui/file_manager/file_manager/foreground/js/file_manager_commands.js
+++ ui/file_manager/file_manager/foreground/js/file_manager_commands.js
@@ -1278,7 +1278,7 @@ CommandHandler.COMMANDS_['new-folder'] = new (class extends FilesCommand {
       // remove new-folder command.
       // @ts-ignore: error TS2367: This comparison appears to be unintentional
       // because the types 'FileSystemEntry[]' and 'number' have no overlap.
-      if (entries == 0 &&
+      if (entries.length === 0 &&
           !CommandUtil.currentVolumeIsInteractive(fileManager)) {
         event.canExecute = false;
         event.command.setHidden(true);
@@ -1864,7 +1864,8 @@ CommandHandler.COMMANDS_['paste'] = new (class extends FilesCommand {
     } else if (
         // @ts-ignore: error TS2367: This comparison appears to be unintentional
         // because the types 'FileSystemEntry[]' and 'number' have no overlap.
-        entries == 0 && !CommandUtil.currentVolumeIsInteractive(fileManager)) {
+        entries.length === 0 &&
+        !CommandUtil.currentVolumeIsInteractive(fileManager)) {
       // If blank space was clicked and current volume is non-interactive,
       // remove paste command.
       event.canExecute = false;
@@ -2082,7 +2083,7 @@ CommandHandler.cutCopyCommand_ = new (class extends FilesCommand {
       // remove cut/copy command.
       // @ts-ignore: error TS2367: This comparison appears to be unintentional
       // because the types 'FileSystemEntry[]' and 'number' have no overlap.
-      if (entries == 0 &&
+      if (entries.length === 0 &&
           !CommandUtil.currentVolumeIsInteractive(fileManager)) {
         command.setHidden(true);
         return false;
