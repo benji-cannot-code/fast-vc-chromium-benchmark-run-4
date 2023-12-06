7623213eac7beb006db82cb98ca63a0c1559638d
Luciano Pacheco <lucmult@chromium.org>
2023-12-06 02:28:58 +0000

    Files TS: Convert runtime_loaded_test_util.js to TS
    Bug: b:289003444
    Change-Id: I7cef389f160109478640526118775d570e3b59aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5087050
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233723}

--- ui/file_manager/file_manager/definitions/file_manager.d.ts
+++ ui/file_manager/file_manager/definitions/file_manager.d.ts
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import type {FileManagerBase} from '../background/js/file_manager_base.js';
 import type {VolumeManager} from '../externs/volume_manager.js';
 import type {MetadataModel} from '../foreground/js/metadata/metadata_model.js';
+import type {FileManagerUI} from '../foreground/js/ui/file_manager_ui.js';
 
 /**
  * Type definition for foreground/js/file_manager.js:FileManager.
@@ -21,6 +22,7 @@ interface FileManager {
   dialogType: DialogType;
   directoryModel: DirectoryModel;
   directoryTreeNamingController: DirectoryTreeNamingController;
+  ui: FileManagerUI;
 }
 
 interface AppState {
