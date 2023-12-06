0da0f0a2f9bae98066c3a8746fb48b886f8c0255
Sky Malice <skym@chromium.org>
2023-12-06 22:05:35 +0000

    Minor refactor to StripLayoutHelperManager.
    Change-Id: I705de82658be13512e9d80893d39fb1dddab74ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5088057
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1234201}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
@@ -516,7 +516,7 @@ public class StripLayoutHelper implements StripLayoutTab.StripLayoutTabDelegate 
      *
      * @return Whether the tab strip is full.
      */
-    protected boolean isTabStripFull() {
+    private boolean isTabStripFull() {
         return mCachedTabWidth < TabUiThemeUtil.getMaxTabStripTabWidthDp();
     }
 
