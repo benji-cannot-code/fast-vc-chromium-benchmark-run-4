696f6925db01e06cfb2f88ffdac55429ba50dd35
Vasiliy Telezhnikov <vasilyt@chromium.org>
2023-12-06 18:43:50 +0000

    Block NVidia GPUs on Android from using Vulkan
    NVidia SHILED got upgrade to Android 11 and now picked up using vulkan,
    but it has some unresolved issues.
    
    Bug: 1508791
    Change-Id: Id17e35a3dd2c7aaf537795950035239919b777ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5095473
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234052}

--- gpu/vulkan/vulkan_util.h
+++ gpu/vulkan/vulkan_util.h
@@ -20,11 +20,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gpu {
 
-constexpr uint32_t kVendorARM = 0x13b5;
-constexpr uint32_t kVendorQualcomm = 0x5143;
-constexpr uint32_t kVendorImagination = 0x1010;
-constexpr uint32_t kVendorGoogle = 0x1AE0;
-constexpr uint32_t kDeviceSwiftShader = 0xC0DE;
+inline constexpr uint32_t kVendorARM = 0x13b5;
+inline constexpr uint32_t kVendorQualcomm = 0x5143;
+inline constexpr uint32_t kVendorImagination = 0x1010;
+inline constexpr uint32_t kVendorGoogle = 0x1AE0;
+inline constexpr uint32_t kDeviceSwiftShader = 0xC0DE;
+inline constexpr uint32_t kVendorNvidia = 0x10DE;
 
 struct GPUInfo;
 class VulkanInfo;
