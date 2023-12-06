1caa14ad2a5680624e1f17e6edd519f0bc1bad81
Louis Romero <lpromero@google.com>
2023-12-06 16:37:12 +0000

    Check that the items list has been set before looking up items
    This CL adds a DUMP_WILL_BE_CHECK for the list of items prior to doing a
    lookup. It is an error to make a lookup if the list is not set.
    (Any lookup with the list being nil would return 0 instead of
    NSNotFound).
    
    Bug: 1502888
    Change-Id: Idae6b40db45a9196ab799ab8e0a02bfa97c0fa79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5094374
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1233979}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_view_controller.mm
@@ -1312,8 +1312,8 @@ NSString* GridCellAccessibilityIdentifier(NSUInteger index) {
                                         (web::WebStateID)selectedItemID
                                           snapshot:(GridSnapshot*)snapshot {
   // Consistency check: `item`'s ID is not in `items`.
-  // (using DCHECK rather than DCHECK_EQ to avoid a checked_cast on NSNotFound).
-  DCHECK([self indexOfItemWithID:item.identifier] == NSNotFound);
+  DCHECK_EQ(static_cast<NSInteger>([self indexOfItemWithID:item.identifier]),
+            NSNotFound);
 
   // Store the identifier of the current item at the given index, if any, prior
   // to model updates.
@@ -1590,6 +1590,7 @@ NSString* GridCellAccessibilityIdentifier(NSUInteger index) {
 // Returns the index in `self.items` of the first item whose identifier is
 // `identifier`.
 - (NSUInteger)indexOfItemWithID:(web::WebStateID)identifier {
+  DUMP_WILL_BE_CHECK(self.items);
   auto selectedTest =
       ^BOOL(TabSwitcherItem* item, NSUInteger index, BOOL* stop) {
         return item.identifier == identifier;
