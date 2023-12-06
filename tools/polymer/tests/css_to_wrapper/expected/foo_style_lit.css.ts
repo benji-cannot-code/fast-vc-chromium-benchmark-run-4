ebdb3b3376bf6ffa89e178e678d011c11c98d5d6
dpapad <dpapad@chromium.org>
2023-12-06 00:57:42 +0000

    WebUI: Support wrapping of Lit element CSS styles in css_to_wrapper().
    Adding two new wrapper template types 'style-lit' and 'vars-lit',
    equivalent to the existing 'style' and 'vars' types respectively (which
    refer to Polymer).
    
    This is part of the ongoing experimentation to offer Lit as an
    alternative to Polymer.
    
    Bug: 1503223
    Change-Id: I25717308a99899bfc4b002685d101cb7448d8d36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5086187
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233684}

---
+++ tools/polymer/tests/css_to_wrapper/expected/foo_style_lit.css.ts
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import {css} from 'chrome://resources/lit/v3_0/lit.rollup.js';
+import './other_vars.css.js';
+
+export function getCss() {
+  return css`div {
+  color: blue;
+}`;
+}
