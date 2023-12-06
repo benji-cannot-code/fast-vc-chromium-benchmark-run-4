1bd6f994c2efb24e0aac2a5f7d9a3372e5dd8620
Brittany Hartmire <bhartmire@google.com>
2023-12-06 18:08:36 +0000

    [QuickStart]: Update metrics enum filepath in comments
    Our enums were recently moved to their own file. This CL updates our
    comment documentation to reflect their new filepath.
    
    Test: CQ dry run
    Change-Id: I2f1a6032b2093eb5739dd18e6286cb28e90efc47
    Bug: b/288882657
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5090071
    Reviewed-by: Brittany Hartmire <bhartmire@google.com>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1234034}

--- chromeos/ash/components/quick_start/quick_start_metrics.h
+++ chromeos/ash/components/quick_start/quick_start_metrics.h
@@ -16,9 +16,9 @@ namespace ash::quick_start {
 class QuickStartMetrics {
  public:
   // This enum is tied directly to a UMA enum defined in
-  // //tools/metrics/histograms/enums.xml, and should always reflect it (do not
-  // change one without changing the other). Entries should be never modified
-  // or deleted. Only additions possible.
+  // //tools/metrics/histograms/metadata/quickstart/enums.xml, and should always
+  // reflect it (do not change one without changing the other). Entries should
+  // be never modified or deleted. Only additions possible.
   enum class ScreenName {
     kOther = 0,  // We don't expect this value to ever be emitted.
     kNone = 1,  // There is no previous screen when automatically resuming after
@@ -49,9 +49,9 @@ class QuickStartMetrics {
   };
 
   // This enum is tied directly to a UMA enum defined in
-  // //tools/metrics/histograms/enums.xml, and should always reflect it (do not
-  // change one without changing the other). Entries should be never modified
-  // or deleted. Only additions possible.
+  // //tools/metrics/histograms/metadata/quickstart/enums.xml, and should always
+  // reflect it (do not change one without changing the other). Entries should
+  // be never modified or deleted. Only additions possible.
   enum class AdvertisingMethod {
     kQrCode = 0,
     kPin = 1,
@@ -59,9 +59,10 @@ class QuickStartMetrics {
   };
 
   // This enum is tied directly to a UMA enum defined in
-  // //tools/metrics/histograms/enums.xml and should always reflect it. The UMA
-  // enum cannot use |device::BluetoothAdvertisement::ErrorCode| directly,
-  // because it is missing the required |kMaxValue| field.
+  // //tools/metrics/histograms/metadata/quickstart/enums.xml and should always
+  // reflect it. The UMA enum cannot use
+  // |device::BluetoothAdvertisement::ErrorCode| directly, because it is missing
+  // the required |kMaxValue| field.
   enum class FastPairAdvertisingErrorCode {
     kUnsupportedPlatform = 0,
     kAdvertisementAlreadyExists = 1,
@@ -80,9 +81,9 @@ class QuickStartMetrics {
   };
 
   // This enum is tied directly to a UMA enum defined in
-  // //tools/metrics/histograms/enums.xml, and should always reflect it (do not
-  // change one without changing the other). Entries should be never modified
-  // or deleted. Only additions possible.
+  // //tools/metrics/histograms/metadata/quickstart/enums.xml, and should always
+  // reflect it (do not change one without changing the other). Entries should
+  // be never modified or deleted. Only additions possible.
   enum class HandshakeErrorCode {
     kFailedToReadResponse = 0,
     kFailedToParse = 1,
@@ -95,9 +96,9 @@ class QuickStartMetrics {
   };
 
   // This enum is tied directly to a UMA enum defined in
-  // //tools/metrics/histograms/enums.xml, and should always reflect it (do not
-  // change one without changing the other). Entries should be never modified
-  // or deleted. Only additions possible.
+  // //tools/metrics/histograms/metadata/quickstart/enums.xml, and should always
+  // reflect it (do not change one without changing the other). Entries should
+  // be never modified or deleted. Only additions possible.
   enum class MessageType {
     kWifiCredentials = 0,
     kBootstrapConfigurations = 1,
@@ -111,9 +112,9 @@ class QuickStartMetrics {
   };
 
   // This enum is tied directly to a UMA enum defined in
-  // //tools/metrics/histograms/enums.xml, and should always reflect it (do not
-  // change one without changing the other). Entries should be never modified
-  // or deleted. Only additions possible.
+  // //tools/metrics/histograms/metadata/quickstart/enums.xml, and should always
+  // reflect it (do not change one without changing the other). Entries should
+  // be never modified or deleted. Only additions possible.
   enum class MessageReceivedErrorCode {
     kTimeOut = 0,
     kDeserializationFailure = 1,
@@ -129,14 +130,13 @@ class QuickStartMetrics {
     kUnknownError = 0,
     kBadRequest = 1,
     kAttestationNotSupportedOnDevice = 2,
-
     kMaxValue = kAttestationNotSupportedOnDevice,
   };
 
   // This enum is tied directly to a UMA enum defined in
-  // //tools/metrics/histograms/enums.xml, and should always reflect it (do not
-  // change one without changing the other). Entries should be never modified
-  // or deleted. Only additions possible.
+  // //tools/metrics/histograms/metadata/quickstart/enums.xml, and should always
+  // reflect it (do not change one without changing the other). Entries should
+  // be never modified or deleted. Only additions possible.
   enum class WifiTransferResultFailureReason {
     kConnectionDroppedDuringAttempt = 0,
     kEmptyResponseBytes = 1,
@@ -153,9 +153,9 @@ class QuickStartMetrics {
   };
 
   // This enum is tied directly to a UMA enum defined in
-  // //tools/metrics/histograms/enums.xml, and should always reflect it (do not
-  // change one without changing the other). Entries should be never modified
-  // or deleted. Only additions possible.
+  // //tools/metrics/histograms/metadata/quickstart/enums.xml, and should always
+  // reflect it (do not change one without changing the other). Entries should
+  // be never modified or deleted. Only additions possible.
   enum class GaiaTransferResultFailureReason {
     kNoAccountsReceivedFromPhone = 0,
     kIneligibleAccount = 1,
