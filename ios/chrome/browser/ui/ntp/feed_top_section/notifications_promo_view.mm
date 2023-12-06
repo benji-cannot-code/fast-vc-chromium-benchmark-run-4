e691bb8a12251514687371fd372a9aa12a0cb83c
Mirko Bonadei <mbonadei@chromium.org>
2023-12-06 18:53:01 +0000

    Remove forward declaration for iOS 14 bots compatibility.
    The forward declaration was added by https://crrev.com/c/5089951.
    On https://crrev.com/c/5094375 this should have been fixed and the
    forward declaration not needed anymore.
    
    Bug: 1508803
    Change-Id: I3decf927846165e910b7f0846982ce33a4647a50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5094459
    Auto-Submit: Mirko Bonadei <mbonadei@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Reviewed-by: Guillem Perez <guiperez@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1234059}

--- ios/chrome/browser/ui/ntp/feed_top_section/notifications_promo_view.mm
+++ ios/chrome/browser/ui/ntp/feed_top_section/notifications_promo_view.mm
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 #import "ios/chrome/common/ui/util/pointer_interaction_util.h"
-#import "ios/chrome/common/ui/util/sdk_forward_declares.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util.h"
 
