2b4d7b7ac0d01df22dd66b273a61c8506b7090c5
Michelle <michellegc@google.com>
2023-12-06 07:44:03 +0000

    Update editor menu badge styling.
    Update badge styling (rounded corners, font, insets) according to latest
    specs.
    
    Also rename to EditorMenuBadgeView since we have removed the gradient
    background.
    
    Bug: b:315032166
    Change-Id: I46f92f74136f2f172e1b50c61061791e764183fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5092587
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1233813}

--- chrome/browser/ui/views/editor_menu/editor_menu_view.cc
+++ chrome/browser/ui/views/editor_menu/editor_menu_view.cc
@@ -15,8 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "base/task/sequenced_task_runner.h"
+#include "chrome/browser/ui/views/editor_menu/editor_menu_badge_view.h"
 #include "chrome/browser/ui/views/editor_menu/editor_menu_chip_view.h"
-#include "chrome/browser/ui/views/editor_menu/editor_menu_gradient_badge.h"
 #include "chrome/browser/ui/views/editor_menu/editor_menu_textfield_view.h"
 #include "chrome/browser/ui/views/editor_menu/editor_menu_view_delegate.h"
 #include "chrome/browser/ui/views/editor_menu/utils/pre_target_handler.h"
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/background.h"
-#include "ui/views/badge_painter.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/textfield/textfield.h"
@@ -57,6 +56,8 @@ constexpr gfx::Insets kTitleContainerInsets = gfx::Insets::TLBR(12, 16, 12, 14);
 
 // Min width in rewrite mode to ensure there is space for the chips.
 constexpr int kEditorMenuRewriteModeMinWidth = 288;
+
+constexpr int kBadgeHorizontalPadding = 8;
 
 // Spacing to apply between and around chips.
 constexpr int kChipsHorizontalPadding = 8;
@@ -211,11 +212,10 @@ void EditorMenuView::AddTitleContainer() {
       views::style::CONTEXT_DIALOG_TITLE, views::style::STYLE_HEADLINE_5));
   title->SetEnabledColorId(ui::kColorSysOnSurface);
 
-  auto* badge = title_container_->AddChildView(
-      std::make_unique<EditorMenuGradientBadge>());
-  badge->SetProperty(
-      views::kMarginsKey,
-      gfx::Insets::VH(0, views::BadgePainter::kBadgeHorizontalMargin));
+  auto* badge =
+      title_container_->AddChildView(std::make_unique<EditorMenuBadgeView>());
+  badge->SetProperty(views::kMarginsKey,
+                     gfx::Insets::VH(0, kBadgeHorizontalPadding));
 
   auto* spacer =
       title_container_->AddChildView(std::make_unique<views::View>());
