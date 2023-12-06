aa0d56282a77ba06317d6160d0b65fb497ed7f7f
Mark Schillaci <mschillaci@google.com>
2023-12-06 21:42:27 +0000

    Add accessibility performance histograms with linear bucket spacing
    This CL adds new histograms that match the existing performance related
    accessibility histograms. We add new histograms so we can change the
    bucket ranges to exponential spacing from 1 microsecond to 1 second
    with a total of 50 buckets. This will give more granularity in the
    data than the existing histograms.
    
    OBSOLETE_HISTOGRAM[Accessibility.Performance.HandleAXEvents]=Replaced by Accessibility.Performance.HandleAXEvents2
    OBSOLETE_HISTOGRAM[Accessibility.Performance.Tree.Unserialize]=Replaced by Accessibility.Performance.Tree.Unserialize2
    OBSOLETE_HISTOGRAM[Accessibility.Performance.ProcessDeferredAccessibilityEvents]=Replaced by Accessibility.Performance.ProcessDeferredAccessibilityEvents2
    OBSOLETE_HISTOGRAM[Accessibility.Performance.SendPendingAccessibilityEvents.PostLoad]=Replaced by Accessibility.Performance.SendPendingAccessibilityEvents.PostLoad2
    OBSOLETE_HISTOGRAM[Accessibility.Performance.SendPendingAccessibilityEvents]=Replaced by Accessibility.Performance.SendPendingAccessibilityEvents2
    OBSOLETE_HISTOGRAM[Accessibility.Performance.BrowserAccessibilityManager::OnAccessibilityEvents]=Replaced by Accessibility.Performance.BrowserAccessibilityManager::OnAccessibilityEvents2
    OBSOLETE_HISTOGRAM[Accessibility.Performance.AXTree.Destroy]=Replaced by Accessibility.Performance.AXTree.Destroy2
    
    AX-Relnotes: N/A
    Bug: N/A
    Change-Id: Ic1f7e28a8c84b29f682b17953506c26c026f66fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5028232
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234185}

--- ui/accessibility/ax_tree_unittest.cc
+++ ui/accessibility/ax_tree_unittest.cc
@@ -459,7 +459,7 @@ TEST(AXTreeTest, LeaveOrphanedDeletedSubtreeFails) {
   AXTree tree(initial_state);
 
   histogram_tester.ExpectTotalCount(
-      "Accessibility.Performance.Tree.Unserialize", 1);
+      "Accessibility.Performance.Tree.Unserialize2", 1);
 
   // This should fail because we delete a subtree rooted at id=2
   // but never update it.
@@ -477,7 +477,7 @@ TEST(AXTreeTest, LeaveOrphanedDeletedSubtreeFails) {
       "Accessibility.Reliability.Tree.UnserializeError",
       AXTreeUnserializeError::kPendingNodes, 1);
   histogram_tester.ExpectTotalCount(
-      "Accessibility.Performance.Tree.Unserialize", 2);
+      "Accessibility.Performance.Tree.Unserialize2", 2);
 #endif
 }
 
@@ -490,7 +490,7 @@ TEST(AXTreeTest, LeaveOrphanedNewChildFails) {
   AXTree tree(initial_state);
 
   histogram_tester.ExpectTotalCount(
-      "Accessibility.Performance.Tree.Unserialize", 1);
+      "Accessibility.Performance.Tree.Unserialize2", 1);
 
   // This should fail because we add a new child to the root node
   // but never update it.
@@ -508,7 +508,7 @@ TEST(AXTreeTest, LeaveOrphanedNewChildFails) {
       "Accessibility.Reliability.Tree.UnserializeError",
       AXTreeUnserializeError::kPendingNodes, 1);
   histogram_tester.ExpectTotalCount(
-      "Accessibility.Performance.Tree.Unserialize", 2);
+      "Accessibility.Performance.Tree.Unserialize2", 2);
 #endif
 }
 
@@ -521,7 +521,7 @@ TEST(AXTreeTest, DuplicateChildIdFails) {
   AXTree tree(initial_state);
 
   histogram_tester.ExpectTotalCount(
-      "Accessibility.Performance.Tree.Unserialize", 1);
+      "Accessibility.Performance.Tree.Unserialize2", 1);
 
   // This should fail because a child id appears twice.
   AXTreeUpdate update;
@@ -540,7 +540,7 @@ TEST(AXTreeTest, DuplicateChildIdFails) {
       "Accessibility.Reliability.Tree.UnserializeError",
       AXTreeUnserializeError::kDuplicateChild, 1);
   histogram_tester.ExpectTotalCount(
-      "Accessibility.Performance.Tree.Unserialize", 1);
+      "Accessibility.Performance.Tree.Unserialize2", 1);
 #endif
 }
 
@@ -558,7 +558,7 @@ TEST(AXTreeTest, InvalidReparentingFails) {
   AXTree tree(initial_state);
 
   histogram_tester.ExpectTotalCount(
-      "Accessibility.Performance.Tree.Unserialize", 1);
+      "Accessibility.Performance.Tree.Unserialize2", 1);
 
   // This should fail because node 3 is reparented from node 2 to node 1
   // without deleting node 1's subtree first.
@@ -581,7 +581,7 @@ TEST(AXTreeTest, InvalidReparentingFails) {
       "Accessibility.Reliability.Tree.UnserializeError",
       AXTreeUnserializeError::kReparent, 1);
   histogram_tester.ExpectTotalCount(
-      "Accessibility.Performance.Tree.Unserialize", 1);
+      "Accessibility.Performance.Tree.Unserialize2", 1);
 #endif
 }
 
