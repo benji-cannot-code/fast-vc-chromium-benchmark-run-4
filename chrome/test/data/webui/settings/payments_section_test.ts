9d562f203c114200c0eab87e2de87e82e3e85992
Vipul Vinod Koul <koulvipul@google.com>
2023-12-06 01:36:15 +0000

    [CVV Storage] Adding metrics for user actions related to CVC bulk delete
    This CL logs the metrics for the following user actions-
    1. Click on the bulk delete CVC hyperlink
    2. Accept the delete option on the dialog window
    3. Cancel the delete option on the dialog window
    
    Metrics tracker- https://docs.google.com/spreadsheets/d/11sg2txJ6Ut3XWu23Gr6sndr0nNPVRLv8bpqDKaUXZDI/edit#gid=1838911229&range=130:130
    
    Design doc- go/pay-autofill-cvv-storage-chrome
    
    Bug: 1505237
    Change-Id: I41c0f5dcc149e604d142864706d79d0b98df6d9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5061790
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Commit-Queue: Vipul Koul <koulvipul@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233702}

--- chrome/test/data/webui/settings/payments_section_test.ts
+++ chrome/test/data/webui/settings/payments_section_test.ts
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // clang-format off
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {PaymentsManagerImpl, SettingsSimpleConfirmationDialogElement} from 'chrome://settings/lazy_load.js';
-import {CrButtonElement, loadTimeData, MetricsBrowserProxyImpl, PrivacyElementInteractions, SettingsToggleButtonElement} from 'chrome://settings/settings.js';
+import {CrButtonElement, CvcDeletionUserAction, loadTimeData, MetricsBrowserProxyImpl, PrivacyElementInteractions, SettingsToggleButtonElement} from 'chrome://settings/settings.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 
 import {createCreditCardEntry, TestPaymentsManager} from './autofill_fake_data.js';
@@ -611,6 +611,8 @@ suite('PaymentsSection', function() {
           loadTimeData.overrideValues({
             cvcStorageAvailable: true,
           });
+          const testMetricsBrowserProxy = new TestMetricsBrowserProxy();
+          MetricsBrowserProxyImpl.setInstance(testMetricsBrowserProxy);
 
           const creditCard = createCreditCardEntry();
           creditCard.cvc = '•••';
@@ -656,8 +658,19 @@ suite('PaymentsSection', function() {
               PaymentsManagerImpl.getInstance() as TestPaymentsManager;
           const expectations = getDefaultExpectations();
 
+          assertEquals(2, testMetricsBrowserProxy.getCallCount('recordAction'));
+          assertEquals(
+              CvcDeletionUserAction.HYPERLINK_CLICKED,
+              testMetricsBrowserProxy.getArgs('recordAction')[0]);
           if (shouldTriggerBulkDelete) {
             expectations.bulkDeleteAllCvcs = 1;
+            assertEquals(
+                CvcDeletionUserAction.DIALOG_ACCEPTED,
+                testMetricsBrowserProxy.getArgs('recordAction')[1]);
+          } else {
+            assertEquals(
+                CvcDeletionUserAction.DIALOG_CANCELLED,
+                testMetricsBrowserProxy.getArgs('recordAction')[1]);
           }
           paymentsManagerProxy.assertExpectations(expectations);
         });
