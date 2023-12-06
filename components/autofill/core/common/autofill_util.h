ae6a91fa7f12829f07c481ea22bc0ca8fa65bee4
Timofey Chudakov <tchudakov@google.com>
2023-12-06 07:16:20 +0000

    [mullet m3] Compute popup type for manual fallback.
    Mullet M3 adds suggestion generation flows for unclassified fields.
    AED needs to be adapted to correctly compute the PopupType.
    
    Previously, the popup type was computed based on the type of the
    triggering field. Manual fallbacks add another popup type control
    flow: the type is determined by the triggering source. This CL
    also adds the tests for the new AED behaviour.
    
    Bug: 1493361
    Change-Id: Ide3ede648d6bedbe79798e2386fdc51803fd4b38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5076707
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1233804}

--- components/autofill/core/common/autofill_util.h
+++ components/autofill/core/common/autofill_util.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/feature_list.h"
+#include "components/autofill/core/common/aliases.h"
 #include "components/autofill/core/common/form_field_data.h"
 #include "components/autofill/core/common/mojom/autofill_types.mojom-shared.h"
 #include "url/gurl.h"
@@ -80,6 +81,21 @@ mojom::SubmissionIndicatorEvent ToSubmissionIndicatorEvent(
 
 // Strips any authentication data, as well as query and ref portions of URL.
 GURL StripAuthAndParams(const GURL& gurl);
+
+// Checks if the user triggered Autofill on a field manually through the Chrome
+// context menu.
+bool IsAutofillManuallyTriggered(
+    AutofillSuggestionTriggerSource trigger_source);
+
+// Checks if the user triggered address Autofill on a field manually through the
+// Chrome context menu.
+bool IsAddressAutofillManuallyTriggered(
+    AutofillSuggestionTriggerSource trigger_source);
+
+// Checks if the user triggered payments Autofill on a field manually through
+// the Chrome context menu.
+bool IsPaymentsAutofillManuallyTriggered(
+    AutofillSuggestionTriggerSource trigger_source);
 
 }  // namespace autofill
 
