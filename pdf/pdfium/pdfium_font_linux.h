9dfa208aad6b5ee010381a2fd8a95f4a1107c941
Lei Zhang <thestig@chromium.org>
2023-12-06 18:47:41 +0000

    Simplify PDF/Linux GetFontTable() function
    Remove a parameter where the only caller always passes in 0. Along the
    way, also update a comment that still mentions PPAPI.
    
    Change-Id: I06dac2038c77eb041af11c508cc76bfbe5205b56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5090312
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1234056}

--- pdf/pdfium/pdfium_font_linux.h
+++ pdf/pdfium/pdfium_font_linux.h
@@ -8,11 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chrome_pdf {
 
-// Initializes a Linux-specific font mapper.
+// Initializes a Linux-specific font mapper that sends font requests to Blink.
 // This is necessary because font loading does not work in the sandbox on Linux.
-// Depending on how this process is initialized, it either:
-// a) sends font requests to Blink.
-// b) proxies font requests via PPAPI.
 void InitializeLinuxFontMapper();
 
 }  // namespace chrome_pdf
