6ebf7dfda12ebcf6738be76ca2b4934b44e2f971
Hira Mahmood <hiramahmood@google.com>
2023-12-06 19:47:21 +0000

    [iOS] Fix table view width in parcel tracking opt in screen
    Currently, the table view in the parcel tracking opt-in screen is too
    wide in landscape orientation, causing the radio buttons to be
    untappable.
    
    This CL:
    * fixes the constraints of the table view in landscape mode
    * changes all instances of "optionsView" to "tableView". they are
    currently used interchangeably, which is confusing.
    
    Before: https://screenshot.googleplex.com/96awFhTywJfBPdL
    
    After: https://screenshot.googleplex.com/Mst5m8vdoAZmkN4
    
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED there is an egtest
    Fixed: 1499092
    Change-Id: I18ec94d7ddd2e6bd85aae414dd4a39ccb761ff93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5087790
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1234107}

--- ios/chrome/browser/ui/parcel_tracking/parcel_tracking_opt_in_view_controller.mm
+++ ios/chrome/browser/ui/parcel_tracking/parcel_tracking_opt_in_view_controller.mm
@@ -25,8 +25,6 @@ namespace {
 NSString* const kOptInIcon = @"parcel_tracking_icon_new";
 // Radius size of the table view.
 CGFloat const kTableViewCornerRadius = 10;
-// Horizontal margin for the view.
-CGFloat const kHorizontalMargin = 24;
 // Spacing before the image.
 CGFloat const kSpacingBeforeImage = 23;
 // Size of the radio buttons.
@@ -42,12 +40,12 @@ CGFloat const kRadioButtonSize = 20;
 @implementation ParcelTrackingOptInViewController {
   UITableView* _tableView;
   IOSParcelTrackingOptInStatus _selection;
-  NSLayoutConstraint* _optionsViewHeightConstraint;
+  NSLayoutConstraint* _tableViewHeightConstraint;
 }
 
 - (void)viewDidLoad {
-  UIView* optionsView = [self createOptionsView];
-  self.underTitleView = optionsView;
+  UIView* tableView = [self createTableView];
+  self.underTitleView = tableView;
   self.titleString =
       l10n_util::GetNSString(IDS_IOS_PARCEL_TRACKING_OPT_IN_TITLE);
   self.primaryActionString =
@@ -69,10 +67,9 @@ CGFloat const kRadioButtonSize = 20;
   // Assign table view's width anchor now that it is in the same hierarchy as
   // the top view.
   [NSLayoutConstraint activateConstraints:@[
-    [optionsView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor
-                                              constant:kHorizontalMargin],
-    [optionsView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor
-                                               constant:-kHorizontalMargin],
+    [tableView.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
+    [tableView.widthAnchor
+        constraintEqualToAnchor:tableView.superview.widthAnchor],
   ]];
 
   [self setPrimaryButtonConfiguration];
@@ -81,9 +78,7 @@ CGFloat const kRadioButtonSize = 20;
 
 - (void)viewWillLayoutSubviews {
   [super viewWillLayoutSubviews];
-  [self updateOptionsViewHeightConstraint];
-  self.view.directionalLayoutMargins =
-      NSDirectionalEdgeInsetsMake(0, kHorizontalMargin, 0, kHorizontalMargin);
+  [self updateTableViewHeightConstraint];
 }
 
 #pragma mark - ConfirmationAlertViewController
@@ -107,6 +102,7 @@ CGFloat const kRadioButtonSize = 20;
       textAttributes, linkAttributes);
   subtitle.delegate = self;
   subtitle.selectable = YES;
+  subtitle.textContainer.lineFragmentPadding = 0;
 }
 
 #pragma mark - ConfirmationAlertActionHandler
@@ -219,7 +215,7 @@ CGFloat const kRadioButtonSize = 20;
 #pragma mark - Private
 
 // Creates the view with the "always track" and "ask to track" options.
-- (UITableView*)createOptionsView {
+- (UITableView*)createTableView {
   _tableView = [[UITableView alloc] initWithFrame:CGRectZero
                                             style:UITableViewStylePlain];
   _tableView.layer.cornerRadius = kTableViewCornerRadius;
@@ -233,20 +229,20 @@ CGFloat const kRadioButtonSize = 20;
   _tableView.separatorInset = UIEdgeInsetsZero;
   [_tableView registerClass:TableViewTextCell.class
       forCellReuseIdentifier:@"cell"];
-  _optionsViewHeightConstraint =
+  _tableViewHeightConstraint =
       [_tableView.heightAnchor constraintEqualToConstant:0];
-  _optionsViewHeightConstraint.active = YES;
+  _tableViewHeightConstraint.active = YES;
 
   return _tableView;
 }
 
-// Updates the optionsView's height constraint.
-- (void)updateOptionsViewHeightConstraint {
+// Updates the tableView's height constraint.
+- (void)updateTableViewHeightConstraint {
   CGFloat totalCellHeight = 0;
   for (UITableViewCell* cell in _tableView.visibleCells) {
     totalCellHeight += cell.frame.size.height;
   }
-  _optionsViewHeightConstraint.constant = totalCellHeight;
+  _tableViewHeightConstraint.constant = totalCellHeight;
 }
 
 // Sets the configurationUpdateHandler for the primaryActionButton to handle the
