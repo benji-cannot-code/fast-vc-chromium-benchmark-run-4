08f88ce6c784bf97bdeda1748e63293a3e6dfcc0
Christos Froussios <cfroussios@google.com>
2023-12-06 02:55:04 +0000

    Revert "[Office upload] Introduce OnAbandonedUpload() to avoid duplicate uploads"
    This reverts commit 1d3e9f7ee006a0d3c9cebda884528bfa3b7e4983.
    
    Reason for revert:
    OneDriveUploadHandlerTest.NoDuplicateUploads fails on / Linux Chromium OS ASan LSan Tests
    
    First failure: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/54987/overview
    
    Original change's description:
    > [Office upload] Introduce OnAbandonedUpload() to avoid duplicate uploads
    >
    > Introduce OfficeTaskResult::kFileAlreadyBeingUploaded for when an
    > upload for a file is abandoned as there already exists an active upload
    > for the same file.
    >
    > Introduce OneDriveUploadHandler::FileAlreadyBeingUploaded() which checks
    > for any existing upload (copy/move) IO tasks for the same file.
    >
    > If the file is already being uploaded, call the newly introduced
    > OneDriveUploadHandler::OnAbandonedUpload() to end the upload in an
    > abandoned state and to call the callback with
    > OfficeTaskResult::kFileAlreadyBeingUploaded. There will be no
    > UploadResult metric and no notifications for this end state, unlike
    > OnSuccessfulUpload() and OnFailedUpload().
    >
    > Bug: b/310042504
    > Change-Id: Ia9348b3ada0cd1285e0bb845bcbc19cd0030bc11
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5061490
    > Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    > Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    > Reviewed-by: Nigel Tao <nigeltao@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1233582}
    
    Bug: b/310042504
    Change-Id: I65da85ff3395f9db40ae667f9849f0f040af5aeb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5092208
    Reviewed-by: Christos Froussios <cfroussios@google.com>
    Auto-Submit: Christos Froussios <cfroussios@google.com>
    Commit-Queue: Christos Froussios <cfroussios@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Christos Froussios <cfroussios@google.com>
    Cr-Commit-Position: refs/heads/main@{#1233737}

--- chrome/browser/ui/webui/ash/cloud_upload/cloud_open_metrics.cc
+++ chrome/browser/ui/webui/ash/cloud_upload/cloud_open_metrics.cc
@@ -455,16 +455,10 @@ void CloudOpenMetrics::CheckForInconsistencies(
       }
     } else {
       // TransferRequired was kCopy or kMove.
-      if (task_result.logged()) {
-        if (task_result.value == OfficeTaskResult::kCancelledAtConfirmation ||
-            task_result.value == OfficeTaskResult::kFileAlreadyBeingUploaded) {
-          // The cloud upload flow was exited at the Move Confirmation Dialog or
-          // the upload was abandoned.
-          ExpectNotLogged(upload_result);
-        } else {
-          // The upload should have succeeded or failed.
-          ExpectLogged(upload_result);
-        }
+      if (task_result.logged() &&
+          task_result.value != OfficeTaskResult::kCancelledAtConfirmation) {
+        // The cloud upload flow was not exited at the Move Confirmation Dialog.
+        ExpectLogged(upload_result);
       }
       // SourceVolume should not match the CloudProvider.
       if (google_drive) {
@@ -532,6 +526,7 @@ void CloudOpenMetrics::CheckForInconsistencies(
       ExpectNotLogged(move_error);
       // TaskResult should be kFailedToUpload.
       ExpectLogged(task_result);
+      // UploadHandler tests don't log TaskResult.
       if (task_result.logged()) {
         switch (task_result.value) {
           case OfficeTaskResult::kFailedToUpload:
@@ -546,7 +541,6 @@ void CloudOpenMetrics::CheckForInconsistencies(
           case OfficeTaskResult::kCancelledAtFallback:
           case OfficeTaskResult::kCancelledAtSetup:
           case OfficeTaskResult::kLocalFileTask:
-          case OfficeTaskResult::kFileAlreadyBeingUploaded:
             SetWrongValueLogged(task_result);
             break;
         }
