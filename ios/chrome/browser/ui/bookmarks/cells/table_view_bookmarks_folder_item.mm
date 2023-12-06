3dadf5dfacea51f5d8e121a1f23e3c7823da52e7
Mike Dougherty <michaeldo@chromium.org>
2023-12-06 21:19:18 +0000

    Align separator insets of bookmark cells with text
    Fixed: 1445906
    Change-Id: Ied6ae5950fb818cf4731ff3148c85db6e0f7515b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5087167
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Éric Noyau <noyau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234162}

--- ios/chrome/browser/ui/bookmarks/cells/table_view_bookmarks_folder_item.mm
+++ ios/chrome/browser/ui/bookmarks/cells/table_view_bookmarks_folder_item.mm
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 // Width by which to indent folder cell's content. This is multiplied by the
 // `indentationLevel` of the cell.
-const CGFloat kFolderCellIndentationWidth = 32.0;
+const CGFloat kFolderCellIndentationWidth = 44.0;
 // The amount in points by which to inset horizontally the cell contents.
 const CGFloat kFolderCellHorizonalInset = 17.0;
 }  // namespace
@@ -72,6 +72,10 @@ const CGFloat kFolderCellHorizonalInset = 17.0;
           kFolderCellIndentationWidth * self.indentationLevel;
       folderCell.folderImageView.image =
           [UIImage imageNamed:@"bookmark_blue_folder"];
+      CGFloat separatorInset =
+          kFolderCellHorizonalInset +
+          (kFolderCellIndentationWidth * (self.indentationLevel + 1));
+      folderCell.separatorInset = UIEdgeInsetsMake(0, separatorInset, 0, 0);
       break;
     }
   }
