797fb0f159e8d1e0b14351d897e74a0882a48fa1
Ian Clelland <iclelland@chromium.org>
2023-12-06 19:09:06 +0000

    Use spaceship operator for TaskAttributionId
    Change-Id: Ica4b526b2e8b3f342e77b806b75e3a311033ad76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5087210
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234075}

--- third_party/blink/public/common/scheduler/task_attribution_id.h
+++ third_party/blink/public/common/scheduler/task_attribution_id.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_PUBLIC_COMMON_SCHEDULER_TASK_ATTRIBUTION_ID_H_
 #define THIRD_PARTY_BLINK_PUBLIC_COMMON_SCHEDULER_TASK_ATTRIBUTION_ID_H_
 
+#include <compare>
 #include <cstdint>
 
 namespace blink::scheduler {
@@ -24,13 +25,10 @@ class TaskAttributionId {
   TaskAttributionIdType value() const { return value_; }
 
   bool operator==(const TaskAttributionId& id) const {
-    return id.value_ == value_;
+    return value_ == id.value_;
   }
-  bool operator!=(const TaskAttributionId& id) const {
-    return id.value_ != value_;
-  }
-  bool operator<(const TaskAttributionId& id) const {
-    return value_ < id.value_;
+  std::strong_ordering operator<=>(const TaskAttributionId& id) const {
+    return value_ <=> id.value_;
   }
   TaskAttributionId NextId() const { return TaskAttributionId(value_ + 1); }
 
