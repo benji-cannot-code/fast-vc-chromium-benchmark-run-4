48aa7b314b8f41a522c8fbd126e67cce693f1424
Brian Begnoche <bcb@chromium.org>
2023-12-06 15:12:53 +0000

    Revert "[LCPP] Call TimingPredictor fallback at window.onload or later LCP"
    This reverts commit b05342ab322925f3d7dac43fa6ff7bc60a030a83.
    
    Reason for revert: Fails lcp_critical_path_predictor/timing_predictor_fallback_nohit.php failing "blink_web_tests on Ubuntu-20.04" failing on builder "chromium/ci/WebKit Linux MSAN".
    
    Original change's description:
    > [LCPP] Call TimingPredictor fallback at window.onload or later LCP
    >
    > LCP Critical Path Predictor(LCPP) learns and predicts what element
    > would be LCP in the next loading.
    > LCP TimingPredictor is a callback API that occurs if a LCP
    > element is painted and predicted as actual LCP(LCP can be reported
    > twice or more in a load usually.)
    > This API would be used to delay P2 tasks to improve the actual LCP
    > timing(or this is our Core Web Vital metric).
    > Design Doc(google internal):
    > https://docs.google.com/document/d/1V5PFwVTDonJ1RE52ZI5WGRihn07atHLApnIWsZ3fgfM/edit?pli=1&resourcekey=0-6we9zT_a97B6ZH0jRalL3g
    >
    > However there are cases that callback is never called or too late.
    > Then, this CL implements the fallback algorithm
    > There are 2 cases:
    > 1. At window.onload, LCP occurs multiply before window.onload but no
    > LCP prediction was hit. Call the fallback at this timing.
    > 2. No LCP occur before window.onload. Call the fallback when the first
    > LCP occurs after that. There are several factors delay the first LCP
    > (resource fetch, paint, visible element is put after onload, etc.)
    >
    > This CL also move the callback implementation from Document to
    > LCPCriticalPathPredictor to clarify the ownership.
    >
    > Bug: 1493255
    > Change-Id: I0f65e7dd1e155bd9eb358259f72328536f6a7ee4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5075372
    > Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1233760}
    
    Bug: 1493255,1508884
    Change-Id: I56913b6d1d0c7a5959d02c1aa7b8b59630a36a1e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5094895
    Auto-Submit: Brian Begnoche <bcb@chromium.org>
    Owners-Override: Brian Begnoche <bcb@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Brian Begnoche <bcb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233941}

--- third_party/blink/web_tests/http/tests/lcp_critical_path_predictor/prioritize_lcp_image.php
+++ third_party/blink/web_tests/http/tests/lcp_critical_path_predictor/prioritize_lcp_image.php
@@ -2,7 +2,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <!doctype html>
 <script src="/priorities/resources/common.js"></script>
 <script type=module>
-import {setupLCPTest} from "./resources/common.js";
+import {mojo} from "/gen/mojo/public/js/bindings.js";
+import {NonAssociatedWebTestControlHostRemote} from "/gen/content/web_test/common/web_test.mojom.m.js";
+import {ByteString} from "/gen/mojo/public/mojom/base/byte_string.mojom.m.js";
+import {LCPCriticalPathPredictorNavigationTimeHint} from "/gen/third_party/blink/public/mojom/lcp_critical_path_predictor/lcp_critical_path_predictor.mojom.m.js";
 
 if (!window.testRunner) {
   console.log("This test requires window.testRunner.")
@@ -10,7 +13,24 @@ if (!window.testRunner) {
 
 testRunner.dumpAsText();
 testRunner.waitUntilDone();
-await setupLCPTest(["lcp_image_id.pb"]);
+if (window.location.search != "?start") {
+  const hint = new LCPCriticalPathPredictorNavigationTimeHint();
+
+  const resp = await fetch("/gen/third_party/blink/renderer/core/lcp_critical_path_predictor/test_proto/lcp_image_id.pb");
+
+  const bytes = new ByteString;
+  bytes.data = new Uint8Array(await resp.arrayBuffer());
+  hint.lcpElementLocators = [bytes];
+  // All fields are non-nullable.
+  hint.lcpInfluencerScripts = [];
+  hint.fetchedFonts = [];
+
+  const web_test_control_host_remote = new NonAssociatedWebTestControlHostRemote();
+  web_test_control_host_remote.$.bindNewPipeAndPassReceiver().bindInBrowser('process');
+  web_test_control_host_remote.setLCPPNavigationHint(hint);
+
+  window.location.search = '?start';
+}
 </script>
 <?php
 // Do not output the HTML below this PHP block until the test is reloaded with
@@ -36,8 +56,8 @@ if ($_SERVER['QUERY_STRING'] != "start")
   }, "Ensure non-LCPP hinted images were loaded unaffected with Medium priority.")
 
   promise_test(async t => {
-    const lcp_element = await internals.LCPPrediction(document);
-    assert_equals(lcp_element, "/#lcp_image");
+    const lcp_elment = await internals.LCPPrediction(document);
+    assert_equals(lcp_elment, "/#lcp_image");
   }, "Ensure document::RunLCPPredictedCallbacks is called with LCP element locator.")
 </script>
 <img src="/resources/square.png" id="lcp_image">
