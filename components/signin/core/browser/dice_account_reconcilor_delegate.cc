2fb6b908ade1f52463b9cdd98981aacfc39cd1b4
Gabriel Oliveira <gabolvr@google.com>
2023-12-06 14:53:20 +0000

    [Uno-D] Keep signed in account when clearing browsing data with UNO
    Bug: b/311142664
    Change-Id: I9f82f954736a5b03db510b34b2ff76061df003c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5079784
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1233926}

--- components/signin/core/browser/dice_account_reconcilor_delegate.cc
+++ components/signin/core/browser/dice_account_reconcilor_delegate.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/signin_client.h"
 #include "components/signin/public/base/signin_metrics.h"
 #include "components/signin/public/base/signin_pref_names.h"
+#include "components/signin/public/base/signin_switches.h"
 #include "components/signin/public/identity_manager/accounts_mutator.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/primary_account_mutator.h"
@@ -216,6 +217,11 @@ ConsentLevel DiceAccountReconcilorDelegate::GetConsentLevelForPrimaryAccount()
     return ConsentLevel::kSignin;
   }
 #endif
+
+  if (base::FeatureList::IsEnabled(switches::kUnoDesktop)) {
+    return ConsentLevel::kSignin;
+  }
+
   // In some cases, clearing the primary account is not allowed regardless of
   // the consent level (e.g. cloud-managed profiles). In these cases, the dice
   // account reconcilor delegate should never remove the primary account
@@ -352,6 +358,14 @@ void DiceAccountReconcilorDelegate::OnAccountsCookieDeletedByUserAction(
   if (!identity_manager_->HasPrimaryAccount(consent_level)) {
     return;
   }
+
+  // In the UNO model the primary account should not be signed out if the
+  // account cookie is deleted by user action.
+  if (base::FeatureList::IsEnabled(switches::kUnoDesktop) &&
+      !identity_manager_->HasPrimaryAccount(ConsentLevel::kSync)) {
+    return;
+  }
+
 #if BUILDFLAG(ENABLE_SUPERVISED_USERS)
   if (IsAccountSupervised(identity_manager_) &&
       base::FeatureList::IsEnabled(
