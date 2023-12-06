1e84863dd61a1f8456b8f4a3950b9215ff15b805
Dave Tapuska <dtapuska@chromium.org>
2023-12-06 17:24:03 +0000

    RenderThread - Remove some dead code
    Unused, redundant, a burden they bear, dragging down the program,
    a weight to spare.
    
    Bug: None
    Change-Id: I12023f75caf98a751a9c5cd3147bbbadcd1c3437
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5088829
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234010}

--- content/public/test/mock_render_thread.cc
+++ content/public/test/mock_render_thread.cc
@@ -65,8 +65,6 @@ class MockRenderMessageFilterImpl : public mojom::RenderMessageFilter {
 MockRenderThread::MockRenderThread()
     : next_routing_id_(kFirstGeneratedRoutingId),
       mock_render_message_filter_(new MockRenderMessageFilterImpl()) {
-  RenderThreadImpl::SetRenderMessageFilterForTesting(
-      mock_render_message_filter_.get());
 }
 
 MockRenderThread::~MockRenderThread() {
