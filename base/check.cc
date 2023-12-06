2769968ab61ae35176fd57fa162f2fe0c6da00be
Peter Boström <pbos@chromium.org>
2023-12-06 07:45:01 +0000

    Fix DCHECK_op on official-dcheck
    This fixes accidental throttling between unrelated DCHECK_op failures
    detected on the official-DCHECK builders.
    
    This effectively reverts crrev.com/c/5058138 as the failure is not
    specific to iOS.
    
    Bug: 1503632, 1505315
    Cq-Include-Trybots: luci.chromium.try:win32-official,win-official,mac-official,linux-official,fuchsia-official
    Change-Id: Ibd4e5c5b2a10f15f1d91df424f32b5a5e88dc159
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5089796
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1233814}

--- base/check.cc
+++ base/check.cc
@@ -80,7 +80,6 @@ class NotReachedLogMessage : public LogMessage {
 
 class DCheckLogMessage : public LogMessage {
  public:
-  using LogMessage::LogMessage;
   DCheckLogMessage(const base::Location& location, LogSeverity severity)
       : LogMessage(location.file_name(), location.line_number(), severity),
         location_(location) {}
@@ -96,7 +95,6 @@ class DCheckLogMessage : public LogMessage {
 
 class DumpWillBeCheckLogMessage : public LogMessage {
  public:
-  using LogMessage::LogMessage;
   DumpWillBeCheckLogMessage(const base::Location& location,
                             LogSeverity severity)
       : LogMessage(location.file_name(), location.line_number(), severity),
@@ -181,8 +179,7 @@ CheckError CheckError::DCheck(const char* condition,
 
 CheckError CheckError::DCheckOp(char* log_message_str,
                                 const base::Location& location) {
-  auto* const log_message = new DCheckLogMessage(
-      location.file_name(), location.line_number(), LOGGING_DCHECK);
+  auto* const log_message = new DCheckLogMessage(location, LOGGING_DCHECK);
   log_message->stream() << log_message_str;
   free(log_message_str);
   return CheckError(log_message);
