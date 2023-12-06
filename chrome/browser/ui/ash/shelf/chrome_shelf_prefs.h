413a0c5ea876764e948795e50f4cfcae2d13af78
mlcui <mlcui@google.com>
2023-12-06 05:41:48 +0000

    chrome_shelf_prefs: Use base::span for function arguments
    See https://abseil.io/tips/93 for more details about using spans as
    arguments.
    
    Bug: None
    Change-Id: I4d0e6c0d27b034e455b5f44635f1321d17c3f02a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5092127
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1233777}

--- chrome/browser/ui/ash/shelf/chrome_shelf_prefs.h
+++ chrome/browser/ui/ash/shelf/chrome_shelf_prefs.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/containers/span.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/scoped_observation.h"
@@ -76,7 +77,7 @@ class ChromeShelfPrefs : public app_list::AppListSyncableService::Observer {
   // `PinnedLauncherApps` policy.
   void SetPinPosition(const ash::ShelfID& shelf_id,
                       const ash::ShelfID& shelf_id_before,
-                      const std::vector<ash::ShelfID>& shelf_ids_after,
+                      base::span<const ash::ShelfID> shelf_ids_after,
                       bool pinned_by_policy);
 
   // Makes GetPinnedAppsFromSync() return an empty list. Avoids test failures
