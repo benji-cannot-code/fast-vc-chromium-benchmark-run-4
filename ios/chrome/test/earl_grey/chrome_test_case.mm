7fd708f246f67746f86c314a74b71adf32a9b64d
Weizhong Xia <weizhong@google.com>
2023-12-06 16:44:27 +0000

    [Code Health][iOS] Move ios/chrome/browser/web to model subfolder
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/<foo>/ will be moved to
    ios/c/b/<foo>/model/ to showcase that they are model files.
    
    Make public dependency available for test and migrate it.
    
    Bug: 1480376
    Change-Id: Ib1cdc5ddec723303c6c014d2e798b5e4806ff207
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5089949
    Auto-Submit: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233984}

--- ios/chrome/test/earl_grey/chrome_test_case.mm
+++ ios/chrome/test/earl_grey/chrome_test_case.mm
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/test/ios/wait_util.h"
 #import "ios/chrome/browser/policy/policy_earl_grey_utils.h"
 #import "ios/chrome/browser/ui/ntp/new_tab_page_feature.h"
-#import "ios/chrome/browser/web/features.h"
+#import "ios/chrome/browser/web/model/features.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
