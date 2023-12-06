94983dee697c8d06747d7f78ba55d73e6b7d796c
Simon Hangl <simonha@google.com>
2023-12-06 12:26:06 +0000

    Make ExtensionEventHistogramValueTest handle multiline event entries
    This CL changes the ExtensionEventHistogramValueTest implementation
    such that it can also handle multiline entries in
    extensions/browser/extension_event_histogram_value.h . With the current
    implementation a multiline entry (e.g. due to a long event name)
    results in an unexpected test error that is hard to understand.
    
    Change-Id: Id2495912c055b46811f00bf767a2ad39932928d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5079783
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1233879}

--- extensions/browser/extension_event_histogram_value_unittest.cc
+++ extensions/browser/extension_event_histogram_value_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/path_service.h"
+#include "base/ranges/algorithm.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/metrics/histogram_enum_reader.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -40,12 +41,16 @@ TEST(ExtensionEventHistogramValueTest, CheckEnums) {
   std::string file_contents;
   ASSERT_TRUE(base::ReadFileToString(event_histogram_value, &file_contents));
 
+  file_contents.erase(base::ranges::remove_if(file_contents, ::isspace),
+                      file_contents.end());
+
   for (const auto& entry : *enums) {
     // Check that the C++ file has a definition equal to the histogram file.
     // NOTE: For now, we do this in a simple, but reasonably effective, manner:
-    // expecting to find the string "ENTRY = <value>" somewhere in the file.
+    // expecting to find the string "ENTRY=<value>" somewhere in the file
+    // (ignoring whitespaces).
     std::string expected_string =
-        base::StringPrintf("%s = %d,", entry.second.c_str(), entry.first);
+        base::StringPrintf("%s=%d,", entry.second.c_str(), entry.first);
     EXPECT_TRUE(base::Contains(file_contents, expected_string))
         << "Failed to find entry " << entry.second << " with value "
         << entry.first << ". Make sure events::HistogramValue and the "
