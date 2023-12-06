4ee6c870d37cec39de6701a8611537d41217a07b
Marc Treib <treib@chromium.org>
2023-12-06 11:58:55 +0000

    SyncSchedulerImpl: Replace JobPriority by RespectGlobalBackoff
    RespectGlobalBackoff is a StrongAlias<bool>, replacing the previous
    two-entry enum. The new name better expresses what it actually does.
    No behavior changes.
    
    Bug: 1448012
    Change-Id: I6b62594c97f03b34f19cffc4c4248eab18b5929c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5088767
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Auto-Submit: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233873}

--- components/sync/engine/sync_scheduler_impl_unittest.cc
+++ components/sync/engine/sync_scheduler_impl_unittest.cc
@@ -570,8 +570,8 @@ TEST_F(SyncSchedulerImplTest, ConfigWithBackingOff) {
                                      {THEMES}, ready_task.Get());
   RunLoop();
 
-  // RunLoop() will trigger TryCanaryJob which will retry configuration.
-  // Since retry_task was already called it shouldn't be called again.
+  // RunLoop() will trigger a sync cycle job which will retry configuration.
+  // Since ready_task was already called it shouldn't be called again.
   RunLoop();
 
   Mock::VerifyAndClearExpectations(syncer());
@@ -1621,7 +1621,7 @@ TEST_F(SyncSchedulerImplTest, ServerConnectionChangeDuringBackoff) {
   PumpLoop();  // Run the nudge, that will fail and schedule a quick retry.
   ASSERT_TRUE(scheduler()->IsGlobalBackoff());
 
-  // Before we run the scheduled canary, trigger a server connection change.
+  // Before we run the scheduled retry, trigger a server connection change.
   scheduler()->OnConnectionStatusChange(
       network::mojom::ConnectionType::CONNECTION_WIFI);
   connection()->SetServerReachable();
@@ -1629,41 +1629,9 @@ TEST_F(SyncSchedulerImplTest, ServerConnectionChangeDuringBackoff) {
   base::RunLoop().RunUntilIdle();
 }
 
-// This was supposed to test the scenario where we receive a nudge while a
-// connection change canary is scheduled, but has not run yet.  Since we've made
-// the connection change canary synchronous, this is no longer possible.
-TEST_F(SyncSchedulerImplTest, ConnectionChangeCanaryPreemptedByNudge) {
-  UseMockDelayProvider();
-  EXPECT_CALL(*delay(), GetDelay).WillRepeatedly(Return(base::Milliseconds(0)));
-
-  StartSyncScheduler(base::Time());
-  connection()->SetServerNotReachable();
-  connection()->UpdateConnectionStatus();
-
-  EXPECT_CALL(*syncer(), NormalSyncShare)
-      .WillOnce(DoAll(Invoke(SimulateConnectionFailure), Return(false)))
-      .WillOnce(DoAll(Invoke(SimulateNormalSuccess), Return(true)))
-      .WillOnce(DoAll(Invoke(SimulateNormalSuccess), QuitLoopNowAction(true)));
-
-  scheduler()->ScheduleLocalNudge(THEMES);
-
-  PumpLoop();  // To get PerformDelayedNudge called.
-  PumpLoop();  // Run the nudge, that will fail and schedule a quick retry.
-  ASSERT_TRUE(scheduler()->IsGlobalBackoff());
-
-  // Before we run the scheduled canary, trigger a server connection change.
-  scheduler()->OnConnectionStatusChange(
-      network::mojom::ConnectionType::CONNECTION_WIFI);
-  PumpLoop();
-  connection()->SetServerReachable();
-  connection()->UpdateConnectionStatus();
-  scheduler()->ScheduleLocalNudge(THEMES);
-  base::RunLoop().RunUntilIdle();
-}
-
-// Tests that we don't crash trying to run two canaries at once if we receive
-// extra connection status change notifications.  See crbug.com/190085.
-TEST_F(SyncSchedulerImplTest, DoubleCanaryInConfigure) {
+// Tests that there's no crash trying to run two jobs at once if the scheduler
+// received extra connection status change notifications.  See crbug.com/190085.
+TEST_F(SyncSchedulerImplTest, DoubleConnectionChangeDuringConfigure) {
   EXPECT_CALL(*syncer(), ConfigureSyncShare)
       .WillRepeatedly(
           DoAll(Invoke(SimulateConfigureConnectionFailure), Return(true)));
@@ -1682,7 +1650,7 @@ TEST_F(SyncSchedulerImplTest, DoubleCanaryInConfigure) {
   PumpLoop();  // Run the nudge, that will fail and schedule a quick retry.
 }
 
-TEST_F(SyncSchedulerImplTest, PollFromCanaryAfterAuthError) {
+TEST_F(SyncSchedulerImplTest, PollAfterAuthError) {
   scheduler()->OnReceivedPollIntervalUpdate(base::Milliseconds(15));
 
   SyncShareTimes times;
@@ -1699,9 +1667,8 @@ TEST_F(SyncSchedulerImplTest, PollFromCanaryAfterAuthError) {
   // Run to wait for polling.
   RunLoop();
 
-  // Normally OnCredentialsUpdated calls TryCanaryJob that doesn't run Poll,
-  // but after poll finished with auth error from poll timer it should retry
-  // poll once more
+  // Normally OnCredentialsUpdated runs a non-poll job, but after a poll
+  // finished with an auth error, it should retry polling once more.
   EXPECT_CALL(*syncer(), PollSyncShare)
       .WillOnce(
           DoAll(Invoke(SimulatePollSuccess), RecordSyncShare(&times, true)));
@@ -1977,10 +1944,10 @@ TEST_F(SyncSchedulerImplTest, InterleavedNudgesStillRestart) {
   EXPECT_EQ(base::TimeDelta(), GetPendingWakeupTimerDelay());
   EXPECT_TRUE(scheduler()->IsGlobalBackoff());
 
-  // Triggers HISTORY PerformDelayedNudge(), which should no-op, because
-  // we're no long healthy, and normal priorities shouldn't go through, but it
-  // does need to setup the |pending_wakeup_timer_|. The delay should be ~60
-  // seconds, so verifying it's greater than 50 should be safe.
+  // Triggers HISTORY PerformDelayedNudge(), which should no-op, because the
+  // scheduler is in global backoff. However, it does need to setup the
+  // `pending_wakeup_timer_`. The delay should be ~60 seconds, so verifying it's
+  // greater than 50 should be safe.
   PumpLoop();
   EXPECT_TRUE(BlockTimerIsRunning());
   EXPECT_LT(base::Seconds(50), GetPendingWakeupTimerDelay());
