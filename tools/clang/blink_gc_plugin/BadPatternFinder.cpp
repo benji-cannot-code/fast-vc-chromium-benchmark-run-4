1083ec1fe97851c45b21ec2654efde2adab8fd5c
Omer Katz <omerkatz@chromium.org>
2023-12-06 12:53:15 +0000

    [heap, blink_gc_plugin] Add option to disable collections check
    This CL temporarily disables the check by default, adds an option to
    disable it via flags, and removes the built in pdfium exemption.
    The check remains enabled for blink via the BUILD.gn file.
    After this CL rolls, we can add the disable flag to Pdfium's BUILD.gn
    and re-enable the check by default.
    
    Bug: chromium:1470687
    Change-Id: Ibe308408e629195be2a6bb13c31a957ad06972e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5093797
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233888}

--- tools/clang/blink_gc_plugin/BadPatternFinder.cpp
+++ tools/clang/blink_gc_plugin/BadPatternFinder.cpp
@@ -473,14 +473,6 @@ class PaddingInGCedMatcher : public MatchFinder::MatchCallback {
   DiagnosticsReporter& diagnostics_;
 };
 
-bool IsInPdfiumFolder(const clang::ASTContext& ast_context) {
-  const clang::SourceManager& source_manager = ast_context.getSourceManager();
-  clang::StringRef filename =
-      source_manager.getFileEntryForID(source_manager.getMainFileID())
-          ->getName();
-  return filename.contains("/pdfium/");
-}
-
 }  // namespace
 
 void FindBadPatterns(clang::ASTContext& ast_context,
@@ -497,9 +489,7 @@ void FindBadPatterns(clang::ASTContext& ast_context,
 
   CollectionOfGarbageCollectedMatcher collection_of_gc(diagnostics,
                                                        record_cache);
-  if (options.enable_off_heap_collections_of_gced_check &&
-      (options.enable_off_heap_collections_of_gced_check_pdfium ||
-       !IsInPdfiumFolder(ast_context))) {
+  if (options.enable_off_heap_collections_of_gced_check) {
     collection_of_gc.Register(match_finder);
   }
 
