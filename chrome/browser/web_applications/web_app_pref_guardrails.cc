6d743381d4e595a068b63c9b7559781774c9e8cd
Dibyajyoti Pal <dibyapal@google.com>
2023-12-07 00:45:02 +0000

    [dPWA] Create guardrails for Link Capturing IPH bubble
    This CL creates guardrails for the IPH bubble to be triggered via
    link captured app launches using the new guardrail architecture, and
    adds some basic tests to verify that things are working fine.
    
    Once crrev.com/c/5078136 lands, this will be directly invoked from
    the correct callsites in the web_applications/ code with more E2E
    tests that will verify the full flow properly.
    
    Bug: 1412856, b/313491283
    Change-Id: I40d8954548e33861a08846db7de473a55c755e23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5085814
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234262}

--- chrome/browser/web_applications/web_app_pref_guardrails.cc
+++ chrome/browser/web_applications/web_app_pref_guardrails.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/web_applications/web_app_constants.h"
 #include "chrome/browser/web_applications/web_app_prefs_utils.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "components/prefs/scoped_user_pref_update.h"
@@ -46,6 +47,15 @@ WebAppPrefGuardrails WebAppPrefGuardrails::GetForMlInstallPrompt(
   return WebAppPrefGuardrails(
       pref_service, web_app::kMlPromoGuardrails, web_app::kMlPromoPrefNames,
       webapps::features::kMaxDaysForMLPromotionGuardrailStorage.Get());
+}
+
+// static
+WebAppPrefGuardrails WebAppPrefGuardrails::GetForLinkCapturingIPH(
+    PrefService* pref_service) {
+  return WebAppPrefGuardrails(
+      pref_service, web_app::kIPHLinkCapturingGuardrails,
+      web_app::kIPHLinkCapturingPrefNames,
+      features::kLinkCapturingIPHGuardrailStorageDuration.Get());
 }
 
 WebAppPrefGuardrails::~WebAppPrefGuardrails() = default;
