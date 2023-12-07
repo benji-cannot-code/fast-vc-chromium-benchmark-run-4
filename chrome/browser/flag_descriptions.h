c936ed58bdde3dc5b5cd25c5fc1c02e139723c1d
Jae Hoon Kim <kimjae@google.com>
2023-12-07 01:16:09 +0000

    Delete allow-repeated-updates flag
    Bug: b:315043489
    Test: CQ
    Change-Id: I411c6e0290d628bd8f13cfbd2c60790e604ffb01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5090786
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Auto-Submit: Jae Hoon Kim <kimjae@chromium.org>
    Commit-Queue: Jae Hoon Kim <kimjae@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234278}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3184,9 +3184,6 @@ extern const char kAllowDevtoolsInSystemUIDescription[];
 extern const char kAllowEapDefaultCasWithoutSubjectVerificationName[];
 extern const char kAllowEapDefaultCasWithoutSubjectVerificationDescription[];
 
-extern const char kAllowRepeatedUpdatesName[];
-extern const char kAllowRepeatedUpdatesDescription[];
-
 extern const char kAllowScrollSettingsName[];
 extern const char kAllowScrollSettingsDescription[];
 
