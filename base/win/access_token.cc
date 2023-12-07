98f984bdfcad10bfd9cc793161e90a0afaad3b98
Stefan Smolen <ssmole@microsoft.com>
2023-12-07 18:23:59 +0000

    Use RemoveAllPrivileges instead of removing SE_CHANGE_NOTIFY_NAME
    The RPC to LSASS from the name to LUID resolution of
    SE_CHANGE_NOTIFY_NAME is suspected to introduce failures for some
    users. Removing all the privileges without resolving LUID names is
    a simple way to avoid this dependency.
    
    Bug: 1509036
    Change-Id: Ic377b58f14ce358efbba9183014871ee7ffcae15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5096360
    Reviewed-by: James Forshaw <forshaw@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Stefan Smolen <ssmole@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1234596}

--- base/win/access_token.cc
+++ base/win/access_token.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/containers/span.h"
 #include "base/numerics/checked_math.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
@@ -605,6 +606,31 @@ absl::optional<bool> AccessToken::SetPrivilege(const std::wstring& name,
 bool AccessToken::RemovePrivilege(const std::wstring& name) {
   return AdjustPrivilege(token_, name.c_str(), SE_PRIVILEGE_REMOVED)
       .has_value();
+}
+
+bool AccessToken::RemoveAllPrivileges() {
+  absl::optional<std::vector<char>> privileges_buffer =
+      GetTokenInfo(token_.get(), TokenPrivileges);
+  if (!privileges_buffer ||
+      (privileges_buffer->size() < sizeof(TOKEN_PRIVILEGES))) {
+    return false;
+  }
+  auto* const token_privileges = GetType<TOKEN_PRIVILEGES>(privileges_buffer);
+  if (privileges_buffer->size() <
+      (offsetof(TOKEN_PRIVILEGES, Privileges) +
+       sizeof(LUID_AND_ATTRIBUTES) * token_privileges->PrivilegeCount)) {
+    return false;
+  }
+
+  for (auto privileges = base::make_span(&token_privileges->Privileges[0],
+                                         token_privileges->PrivilegeCount);
+       auto& privilege : privileges) {
+    privilege.Attributes = SE_PRIVILEGE_REMOVED;
+  }
+  return ::AdjustTokenPrivileges(
+      token_.get(), /*DisableAllPrivileges=*/FALSE, token_privileges,
+      static_cast<DWORD>(privileges_buffer->size()),
+      /*PreviousState=*/nullptr, /*ReturnLength=*/nullptr);
 }
 
 bool AccessToken::is_valid() const {
