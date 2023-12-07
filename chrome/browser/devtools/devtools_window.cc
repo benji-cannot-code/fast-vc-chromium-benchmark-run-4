b600613eb5b74de141f48fcaf0dad8c3ab9192c9
Wolfgang Beyer <wolfi@chromium.org>
2023-12-07 09:27:39 +0000

    [DevTools] base::feature for AIDA temperature and pass to DevTools
    corresponding DevTools CL: https://crrev.com/c/5093838
    
    Bug: b/299423255
    Change-Id: Ieaec83281288f1314e9638627c81a3b410a61fab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5094760
    Auto-Submit: Wolfgang Beyer <wolfi@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234390}

--- chrome/browser/devtools/devtools_window.cc
+++ chrome/browser/devtools/devtools_window.cc
@@ -1253,7 +1253,10 @@ GURL DevToolsWindow::GetDevToolsURL(Profile* profile,
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
       if (base::FeatureList::IsEnabled(::features::kDevToolsConsoleInsights)) {
         url += "&enableAida=true&aidaApiKey=" +
-               features::kDevToolsConsoleInsightsApiKey.Get();
+               features::kDevToolsConsoleInsightsApiKey.Get() +
+               "&aidaTemperature=" +
+               base::NumberToString(
+                   features::kDevToolsConsoleInsightsTemperature.Get());
       }
 #endif
       break;
