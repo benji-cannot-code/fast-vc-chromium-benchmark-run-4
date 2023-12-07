c6c9f503487cc2a9e8aff687bb927f452544cc76
Ryan Sultanem <rsult@google.com>
2023-12-07 14:10:08 +0000

    [Code Health] Remove Signin.A* expired histograms
    Expired histograms removed:
    - Signin.AllAccessPointActions
    - Signin.AndroidAccountConsistencyFirstRunActions
    
    Also removed some struct parameters that was only set and used with the
    histograms.
    
    Fixed: 1463701, b:303789286
    Change-Id: I5a836f230c9c7576dc4d3466aaa8cdb19f93b934
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5079433
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234461}

--- chrome/browser/resources/gaia_auth_host/authenticator.js
+++ chrome/browser/resources/gaia_auth_host/authenticator.js
@@ -69,7 +69,6 @@ export let SyncTrustedVaultKeys;
  *   password: string,
  *   usingSAML: boolean,
  *   publicSAML: boolean,
- *   chooseWhatToSync: boolean,
  *   skipForNow: boolean,
  *   sessionIndex: string,
  *   trusted: boolean,
@@ -118,7 +117,6 @@ export let AuthParams;
 
 const SIGN_IN_HEADER = 'google-accounts-signin';
 const EMBEDDED_FORM_HEADER = 'google-accounts-embedded';
-const LOCATION_HEADER = 'location';
 const SERVICE_ID = 'chromeoslogin';
 const BLANK_PAGE_URL = 'about:blank';
 
@@ -268,7 +266,6 @@ const messageHandlers = {
       this.password_ = msg.password;
     }
 
-    this.chooseWhatToSync_ = msg.chooseWhatToSync;
     // We need to dispatch only first event, before user enters password.
     this.dispatchEvent(new CustomEvent('attemptLogin', {detail: msg.email}));
   },
@@ -410,7 +407,6 @@ export class Authenticator extends EventTarget {
     this.email_ = null;
     this.password_ = null;
     this.gaiaId_ = null, this.sessionIndex_ = null;
-    this.chooseWhatToSync_ = false;
     this.skipForNow_ = false;
     /** @type {AuthMode} */
     this.authMode = AuthMode.DEFAULT;
@@ -552,7 +548,6 @@ export class Authenticator extends EventTarget {
     this.gaiaId_ = null;
     this.password_ = null;
     this.readyFired_ = false;
-    this.chooseWhatToSync_ = false;
     this.skipForNow_ = false;
     this.sessionIndex_ = null;
     this.trusted_ = true;
@@ -1023,13 +1018,6 @@ export class Authenticator extends EventTarget {
         this.setEmail_(email);
         this.gaiaId_ = signinDetails['obfuscatedid'].slice(1, -1);
         this.sessionIndex_ = signinDetails['sessionindex'];
-      } else if (headerName === LOCATION_HEADER) {
-        // If the "choose what to sync" checkbox was clicked, then the
-        // continue URL will contain a source=3 field.
-        assert(header.value !== undefined);
-        const location =
-            decodeURIComponent(/** @type {string} */ (header.value));
-        this.chooseWhatToSync_ = !!location.match(/(\?|&)source=3($|&)/);
       }
     }
   }
@@ -1281,7 +1269,6 @@ export class Authenticator extends EventTarget {
             usingSAML: this.authFlow === AuthFlow.SAML,
             scrapedSAMLPasswords: scrapedPasswords,
             publicSAML: this.samlAclUrl_ || false,
-            chooseWhatToSync: this.chooseWhatToSync_,
             skipForNow: this.skipForNow_,
             sessionIndex: this.sessionIndex_ || '',
             trusted: this.trusted_,
