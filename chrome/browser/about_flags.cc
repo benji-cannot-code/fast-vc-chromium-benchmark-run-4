145ab193c40388d1177b24ed4303227d4a602a77
Andrey Zaytsev <andzaytsev@google.com>
2023-12-07 11:49:34 +0000

    Site Data Improvements: Cleanup flag code
    Bug: b:314745446
    Change-Id: Ifbd268dba26becb949d34fc1ded23b1ddee2609c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5084300
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Andrey Zaytsev <andzaytsev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1234425}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -246,7 +246,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/notifications/chime/android/features.h"
 #include "chrome/browser/push_messaging/push_messaging_features.h"
 #include "components/browser_ui/photo_picker/android/features.h"
-#include "components/browser_ui/site_settings/android/features.h"
 #include "components/content_creation/notes/core/note_features.h"
 #include "components/external_intents/android/external_intents_features.h"
 #include "components/messages/android/messages_feature.h"
@@ -8779,12 +8778,6 @@ const FeatureEntry kFeatureEntries[] = {
      kOsAll,
      FEATURE_VALUE_TYPE(
          privacy_sandbox::kPrivacySandboxProactiveTopicsBlocking)},
-
-#if BUILDFLAG(IS_ANDROID)
-    {"site-data-improvements", flag_descriptions::kSiteDataImprovementsName,
-     flag_descriptions::kSiteDataImprovementsDescription, kOsAndroid,
-     FEATURE_VALUE_TYPE(browser_ui::kSiteDataImprovements)},
-#endif  // BUILDFLAG(IS_ANDROID)
 
     {"animated-image-resume", flag_descriptions::kAnimatedImageResumeName,
      flag_descriptions::kAnimatedImageResumeDescription, kOsAll,
