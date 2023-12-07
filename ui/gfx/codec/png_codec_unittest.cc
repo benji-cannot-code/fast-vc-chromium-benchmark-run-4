ec6eb3321842dc6e4b94112bb899bee6457b909f
Daniel Cheng <dcheng@chromium.org>
2023-12-07 20:52:56 +0000

    Add missing include for setw(), et cetera in png codec unittest.
    Right now, it's transitively getting the include from googletest.
    
    Bug: 1409870
    Change-Id: I8ad66c86c23a753dc286967c93ce5b09b8175436
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5100022
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Leon Scroggins <scroggo@google.com>
    Commit-Queue: Leon Scroggins <scroggo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1234688}

--- ui/gfx/codec/png_codec_unittest.cc
+++ ui/gfx/codec/png_codec_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <cmath>
+#include <iomanip>
 
 #include "base/base_paths.h"
 #include "base/check.h"
