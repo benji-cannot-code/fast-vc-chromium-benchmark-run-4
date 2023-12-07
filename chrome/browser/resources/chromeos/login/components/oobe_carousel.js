24a7c431e6d13b9a6fc602b0c2c7308eac058181
Simon Ziegltrum <ziegltrum@google.com>
2023-12-07 17:03:33 +0000

    [OOBE] Responsive overflow behavior on oobe_carousel
    - Add oobe_scrollable_behavior to add visible scroll indicators for oobe_slides taller than the container
    - Make the chevron bar intransparent
    - Pause auto transition on hover
    - Update styling of scroll indicators
    - Update styling of readmore button bar
    - Merge new slide durations into default
    
    https://screenshot.googleplex.com/7dsLJSp7HFWJe6r
    https://drive.google.com/file/d/1nqb2Lblc0XOeYzePKyoESU6cgFWEau-y/view?resourcekey=0-IgsAxfBJ1zYCKQHjjOhiJA
    https://drive.google.com/file/d/1rW6c4gtpk_hcgvGT4SXmaEVCZm0M0Yvr/view?usp=drive_link&resourcekey=0-kZt-5XAUCrpFW4rLRj63Rg
    
    Carousel UI spec: https://www.figma.com/file/NInmq1YYm3BgxueLIFgUYN/CrOS-Next---OOBE?type=design&node-id=2-103&mode=design&t=tbD7MdSnhnJyKWrF-0
    
    Scroll indicator mocks: https://www.figma.com/file/NInmq1YYm3BgxueLIFgUYN/CrOS-Next---OOBE?type=design&node-id=4532-45270&mode=design&t=C691FipiErmoflfT-0#637291355
    
    Bug: b/294843506 b/313695268 b/289793104
    Change-Id: Ied20bf0b1b2cdd3f693b9fbf4deb42c23518c692
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5071395
    Commit-Queue: Simon Ziegltrum <ziegltrum@google.com>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1234545}

--- chrome/browser/resources/chromeos/login/components/oobe_carousel.js
+++ chrome/browser/resources/chromeos/login/components/oobe_carousel.js
@@ -22,12 +22,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import '//resources/cr_elements/cr_icon_button/cr_icon_button.js';
 import '//resources/cr_elements/icons.html.js';
+import './common_styles/oobe_common_styles.css.js';
 
 import {loadTimeData} from '//resources/ash/common/load_time_data.m.js';
-import {html, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {html, mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {OobeScrollableBehavior, OobeScrollableBehaviorInterface} from './behaviors/oobe_scrollable_behavior.js';
+
+/**
+ * @constructor
+ * @extends {PolymerElement}
+ * @implements {OobeScrollableBehaviorInterface}
+ */
+const OobeCarouselBase =
+    mixinBehaviors([OobeScrollableBehavior], PolymerElement);
+
 
 /** @polymer */
-class OobeCarousel extends PolymerElement {
+class OobeCarousel extends OobeCarouselBase {
   static get is() {
     return 'oobe-carousel';
   }
@@ -64,7 +76,7 @@ class OobeCarousel extends PolymerElement {
        */
       slideDurationInSeconds: {
         type: Number,
-        value: 10,
+        value: 8,
         observer: 'restartAutoTransition_',
       },
 
@@ -119,6 +131,18 @@ class OobeCarousel extends PolymerElement {
     this.prepareCarousel_();
     this.restartAutoTransition_();
     this.hideNonActiveSlides_();
+
+    const slidesContainer = this.shadowRoot.querySelector('#slidesContainer');
+    slidesContainer.addEventListener(
+        'mouseenter', (e) => this.stopAutoTransition_());
+    slidesContainer.addEventListener(
+        'mouseleave', (e) => this.startAutoTransition_());
+
+    const scrollContainer = this.shadowRoot.querySelector('#scrollContainer');
+    if (!scrollContainer || !slidesContainer) {
+      return;
+    }
+    this.initScrollableObservers(scrollContainer, slidesContainer);
   }
 
   /**
@@ -126,7 +150,7 @@ class OobeCarousel extends PolymerElement {
    * Count slides and create dots. Set a11y label on slides.
    */
   prepareCarousel_() {
-    this.slides = this.$.slot.assignedElements();
+    this.slides = this.shadowRoot.querySelector('#slot').assignedElements();
     this.totalSlides = this.slides.length;
     this.dots = [...Array(this.totalSlides).keys()];
     for (let i = 0; i < this.totalSlides; ++i) {
@@ -172,7 +196,7 @@ class OobeCarousel extends PolymerElement {
   startAutoTransition_() {
     if (this.autoTransition && this.slideDurationInSeconds != null) {
       this.timerID = setInterval(
-          this.moveNext.bind(this), (this.slideDurationInSeconds * 1000));
+          () => this.moveNext(), (this.slideDurationInSeconds * 1000));
     }
   }
 
@@ -323,6 +347,10 @@ class OobeCarousel extends PolymerElement {
     // Call offsetWidth to apply |toStyle| and render |toElement|.
     void toElement.offsetWidth;
 
+    // Trigger oobe_scroll_behavior to update scroll indicators
+    // as scrollbar can appear immediately on unhiding a tall slide
+    this.applyScrollClassTags_();
+
     toElement.classList.add('animated');
     fromElement.classList.add('animated');
     toElement.classList.remove(toStyle);
@@ -332,6 +360,10 @@ class OobeCarousel extends PolymerElement {
     toElement.addEventListener('transitionend', OobeCarousel.removeAnimateTo_);
     fromElement.addEventListener(
         'transitionend', OobeCarousel.removeAnimateFrom_);
+    // Trigger oobe_scroll_behavior to update scroll indicators
+    // in case the transition was from a tall to a narrow slide
+    fromElement.addEventListener(
+        'transitionend', () => this.applyScrollClassTags_(), {once: true});
   }
 
   /**
