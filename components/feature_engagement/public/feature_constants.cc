326228594052bca81af7a12c4dc7682d38c98805
Dibyajyoti Pal <dibyapal@google.com>
2023-12-07 18:29:39 +0000

    Disable PWA IPH install feature from Chromium end
    This CL might be a possible fix for crashes like go/crash/2411c4934607bb6e in the linked bug.
    This can be enabled from Finch in the future should
    a launch need to happen.
    
    Bug: 1509128
    Change-Id: I1be3c4b27c244abdccd706d5e8e71160c8c4177d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5096545
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234599}

--- components/feature_engagement/public/feature_constants.cc
+++ components/feature_engagement/public/feature_constants.cc
@@ -156,7 +156,7 @@ BASE_FEATURE(kIPHDesktopSnoozeFeature,
              base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kIPHDesktopPwaInstallFeature,
              "IPH_DesktopPwaInstall",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kIPHProfileSwitchFeature,
              "IPH_ProfileSwitch",
              base::FEATURE_ENABLED_BY_DEFAULT);
