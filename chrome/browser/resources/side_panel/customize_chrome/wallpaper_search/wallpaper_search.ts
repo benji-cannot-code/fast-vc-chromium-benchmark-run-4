00ee7e4b5d401f324bce01b3296f8be288a99750
Paul Adedeji <pauladedeji@google.com>
2023-12-07 19:24:54 +0000

    Add feedback for switch between error UI and search UI.
    Moves focus whenever there is a change in wallpaper search's status. If there's an error, it focuses on the error. If it resolves, then it refocuses on wallpaper search.
    
    *video demo in bug comments*
    
    Change-Id: I1eccd628251cfaa2f57b1013376ea5523d3b0997
    Bug: 315020782
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5100628
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1234639}

--- chrome/browser/resources/side_panel/customize_chrome/wallpaper_search/wallpaper_search.ts
+++ chrome/browser/resources/side_panel/customize_chrome/wallpaper_search/wallpaper_search.ts
@@ -88,6 +88,7 @@ export interface WallpaperSearchElement {
     descriptorComboboxB: CustomizeChromeCombobox,
     descriptorComboboxC: CustomizeChromeCombobox,
     descriptorMenuD: CrActionMenuElement,
+    error: HTMLElement,
     feedbackButtons: CrFeedbackButtonsElement,
     heading: SpHeading,
     historyCard: HTMLElement,
@@ -148,6 +149,7 @@ export class WallpaperSearchElement extends WallpaperSearchElementBase {
       status_: {
         type: WallpaperSearchStatus,
         value: WallpaperSearchStatus.kOk,
+        observer: 'onStatusChange_',
       },
       submitBtnText_: {
         type: String,
@@ -523,6 +525,14 @@ export class WallpaperSearchElement extends WallpaperSearchElementBase {
         CustomizeChromeAction.WALLPAPER_SEARCH_RESULT_IMAGE_SELECTED);
     this.wallpaperSearchHandler_.setBackgroundToWallpaperSearchResult(
         e.model.item.id, WindowProxy.getInstance().now());
+  }
+
+  private onStatusChange_() {
+    if (this.status_ === WallpaperSearchStatus.kOk) {
+      this.$.wallpaperSearch.focus();
+    } else {
+      this.$.error.focus();
+    }
   }
 
   private shouldShowFeedbackButtons_() {
