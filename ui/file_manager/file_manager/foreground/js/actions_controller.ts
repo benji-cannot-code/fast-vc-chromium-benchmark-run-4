612ddf8620cf2bffc253a8113086cadd23db0d43
Ben Reich <benreich@chromium.org>
2023-12-07 04:43:32 +0000

    [FilesTS] Remove directoryModel from ActionsController constructor
    This field is not used at all, avoid plumbing it through and remove it
    from the constructor.
    
    Bug: b/289003444
    Change-Id: If1143c77b396971877497131dc9a7715aa77ee16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5095836
    Auto-Submit: Ben Reich <benreich@chromium.org>
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234345}

--- ui/file_manager/file_manager/foreground/js/actions_controller.ts
+++ ui/file_manager/file_manager/foreground/js/actions_controller.ts
@@ -11,7 +11,6 @@ import type {VolumeManager} from '../../externs/volume_manager.js';
 import {XfTree} from '../../widgets/xf_tree.js';
 
 import {Action, ActionsModel} from './actions_model.js';
-import {DirectoryModel} from './directory_model.js';
 import {EventType, FileSelectionHandler} from './file_selection.js';
 import {FolderShortcutsDataModel} from './folder_shortcuts_data_model.js';
 import {MetadataSetEvent} from './metadata/metadata_cache_set.js';
@@ -35,7 +34,6 @@ export class ActionsController {
   constructor(
       private readonly volumeManager_: VolumeManager,
       private readonly metadataModel_: MetadataModel,
-      _directoryModel: DirectoryModel,
       private readonly shortcutsModel_: FolderShortcutsDataModel,
       private readonly selectionHandler_: FileSelectionHandler,
       private readonly ui_: FileManagerUI) {
