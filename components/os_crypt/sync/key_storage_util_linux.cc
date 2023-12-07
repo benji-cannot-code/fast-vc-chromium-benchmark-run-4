a46bbd7adc604562bcab53b09c026826fb9fd276
Lei Zhang <thestig@chromium.org>
2023-12-07 23:14:48 +0000

    Remove NOTREACHED() usage inside key_storage_util_linux.cc
    The NOTREACHED() is at the end of a function after a switch statement
    that covers all its cases. This is unnecessary, as Chromium builds with
    -Wswitch.
    
    Change-Id: I21aac8ed38653c64a293a26d0feb5cdb1974e660
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5090544
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Christos Froussios <cfroussios@google.com>
    Cr-Commit-Position: refs/heads/main@{#1234754}

--- components/os_crypt/sync/key_storage_util_linux.cc
+++ components/os_crypt/sync/key_storage_util_linux.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/logging.h"
-#include "base/notreached.h"
 
 namespace {
 
@@ -73,9 +72,6 @@ SelectedLinuxBackend SelectBackend(const std::string& type,
     case base::nix::DESKTOP_ENVIRONMENT_OTHER:
       return SelectedLinuxBackend::BASIC_TEXT;
   }
-
-  NOTREACHED();
-  return SelectedLinuxBackend::BASIC_TEXT;
 }
 
 bool WriteBackendUse(const base::FilePath& user_data_dir, bool use) {
