a2f1061068584bd10d51aec9e3ea8e18ac6ebd37
Stepan Khapugin <stkhapugin@chromium.org>
2023-12-07 09:40:24 +0000

    [iOS][omnibox] Improve popout omnibox appearance.
    Improves the visual presentation of the popout omnibox:
    1. no border
    2. correct rounded corners
    3. added drop shadow
    4. removed extra bottom padding
    
    Low-Coverage-Reason: HARD_TO_TEST UI changes like padding and corner radius
    Bug: none
    Change-Id: If34ad28210ecfbc5f01ed2544dcef8ada145fcda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5002525
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234394}

--- ios/chrome/browser/ui/omnibox/popup/omnibox_popup_view_controller.mm
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_popup_view_controller.mm
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/omnibox/common/omnibox_features.h"
 #import "ios/chrome/browser/net/crurl.h"
 #import "ios/chrome/browser/shared/public/features/system_flags.h"
-#import "ios/chrome/browser/shared/ui/elements/self_sizing_table_view.h"
 #import "ios/chrome/browser/shared/ui/util/keyboard_observer_helper.h"
 #import "ios/chrome/browser/shared/ui/util/layout_guide_names.h"
 #import "ios/chrome/browser/shared/ui/util/rtl_geometry.h"
@@ -33,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/omnibox/popup/omnibox_popup_row_cell.h"
 #import "ios/chrome/browser/ui/omnibox/popup/omnibox_popup_row_cell_experimental.h"
 #import "ios/chrome/browser/ui/omnibox/popup/popup_match_preview_delegate.h"
+#import "ios/chrome/browser/ui/omnibox/popup/popup_table_view.h"
 #import "ios/chrome/browser/ui/toolbar/buttons/toolbar_configuration.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
@@ -188,8 +188,8 @@ BOOL ShouldDismissKeyboardOnScroll() {
     _carouselAttributeProvider.cache = self.largeIconCache;
   }
   self.tableView =
-      [[SelfSizingTableView alloc] initWithFrame:CGRectZero
-                                           style:UITableViewStyleGrouped];
+      [[PopupTableView alloc] initWithFrame:CGRectZero
+                                      style:UITableViewStyleGrouped];
   self.tableView.delegate = self;
   self.tableView.dataSource = self;
   self.view = self.tableView;
@@ -277,6 +277,8 @@ BOOL ShouldDismissKeyboardOnScroll() {
     /// safe area (too close to the leading edge). This is ok because the entire
     /// rows act as touch targets.
     self.viewRespectsSystemMinimumLayoutMargins = NO;
+    self.tableView.contentInsetAdjustmentBehavior =
+        UIScrollViewContentInsetAdjustmentNever;
   }
 
   [self.tableView setDirectionalLayoutMargins:NSDirectionalEdgeInsetsMake(
---
+++ ios/chrome/browser/ui/omnibox/popup/popup_table_view.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_OMNIBOX_POPUP_POPUP_TABLE_VIEW_H_
+#define IOS_CHROME_BROWSER_UI_OMNIBOX_POPUP_POPUP_TABLE_VIEW_H_
+
+#import "ios/chrome/browser/shared/ui/elements/self_sizing_table_view.h"
+
+// Omnibox Popup variant of the self-sizing table view.
+@interface PopupTableView : SelfSizingTableView
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_OMNIBOX_POPUP_POPUP_TABLE_VIEW_H_
