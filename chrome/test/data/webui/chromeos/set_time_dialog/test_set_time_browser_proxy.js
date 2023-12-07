75b9a2b39b8e65b977a994250b5c9930303ad635
Wes Okuhara <wesokuhara@google.com>
2023-12-07 06:50:25 +0000

    Set Time Dialog: Extract TestSetTimeBrowserProxy to separate file
    Bug: b:308829256
    Test: browser_tests --gtest_filter="SetTimeDialogBrowserTest*"
    Change-Id: Iac135449264f28c7a967a16993918c399a0b2e95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5090075
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1234357}

---
+++ chrome/test/data/webui/chromeos/set_time_dialog/test_set_time_browser_proxy.js
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {SetTimeBrowserProxy} from 'chrome://set-time/set_time_browser_proxy.js';
+import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
+
+/** @implements {SetTimeBrowserProxy} */
+export class TestSetTimeBrowserProxy extends TestBrowserProxy {
+  constructor() {
+    super([
+      'sendPageReady',
+      'setTimeInSeconds',
+      'setTimezone',
+      'dialogClose',
+      'doneClicked',
+    ]);
+  }
+
+  /** @override */
+  sendPageReady() {
+    this.methodCalled('sendPageReady');
+  }
+
+  /** @override */
+  setTimeInSeconds(timeInSeconds) {
+    this.methodCalled('setTimeInSeconds', timeInSeconds);
+  }
+
+  /** @override */
+  setTimezone(timezone) {
+    this.methodCalled('setTimezone', timezone);
+  }
+
+  /** @override */
+  dialogClose() {
+    this.methodCalled('dialogClose');
+  }
+
+  /** @override */
+  doneClicked() {
+    this.methodCalled('doneClicked');
+  }
+}
