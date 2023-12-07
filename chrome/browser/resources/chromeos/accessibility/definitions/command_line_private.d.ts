48d17ab268e392362a5227289056c4adaa679d4b
Josiah Krutz <josiahk@google.com>
2023-12-07 01:19:53 +0000

    [TS] Migrate accessibility/common/flags.js to TypeScript
    Migrate chrome/browser/resources/chromeos/accessibility/common/
    array_util.js to Typescript.
    
    AX-Relnotes: N/A.
    Bug: 267329383
    Test: out/chrome --enable-magnifier-debug-draw-rect, and verify rectangle draws with fullscreen magnifier enabled.
    Change-Id: Ifc96351d0e4bfc539355050683e300fec882007e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5096578
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Auto-Submit: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1234280}

---
+++ chrome/browser/resources/chromeos/accessibility/definitions/command_line_private.d.ts
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Definitions for chrome.commandLinePrivate API
+ * Generated from: chrome/common/extensions/api/command_line_private.json
+ * run `tools/json_schema_compiler/compiler.py
+ * chrome/common/extensions/api/command_line_private.json -g ts_definitions` to
+ * regenerate.
+ */
+
+
+
+declare namespace chrome {
+  export namespace commandLinePrivate {
+
+    export function hasSwitch(
+        name: string, callback: (result: boolean) => void): void;
+
+  }
+}
