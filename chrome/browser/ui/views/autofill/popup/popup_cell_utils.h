ddd79b98278005920df4a04103f7bb83f0ec83b5
Bruno Braga <brunobraga@google.com>
2023-12-07 14:13:53 +0000

    [mullet m2] Maximum popup width is respected.
    problem: When adding labels to the popup, we make it so that
    each individual label has the max width equals to the popup max width.
    Since there can be more than one label per row, this leads to a popup
    whose width is larger than its expected max value.
    
    solution:
    
    1. Make `FormatLabel` receive the width as param.
    2. In the label case, divide the width by the number of labels in
    the row.
    
    notes:
    
    1. Granular filling feature introduces a new max width to the label.
    2. This change is no-op to the current behaviour.
    
    before: https://screenshot.googleplex.com/43L9xRy2bHjLr3B
    after: https://screenshot.googleplex.com/fNzbshgTZRAD35r
    
    Bug: 1459990
    Change-Id: I9d8e9b847b39ec49b9674aa6159962613eb0448b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5057557
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1234463}

--- chrome/browser/ui/views/autofill/popup/popup_cell_utils.h
+++ chrome/browser/ui/views/autofill/popup/popup_cell_utils.h
@@ -110,7 +110,8 @@ void AddSuggestionContentToView(
 
 void FormatLabel(views::Label& label,
                  const Suggestion::Text& text,
-                 PopupType popup_type);
+                 PopupType popup_type,
+                 int maximum_width_single_line);
 
 // Creates a label for the suggestion's main text.
 std::unique_ptr<views::Label> CreateMainTextLabel(
@@ -126,6 +127,8 @@ std::vector<std::unique_ptr<views::View>> CreateAndTrackSubtextViews(
     const Suggestion& suggestion,
     PopupType popup_type,
     int text_style = views::style::STYLE_SECONDARY);
+
+int GetMaxPopupAddressProfileWidth();
 
 void AddSuggestionStrategyContentCellChildren(PopupRowContentView* view,
                                               const Suggestion& suggestion,
--- chrome/browser/ui/views/autofill/popup/popup_row_factory_utils.cc
+++ chrome/browser/ui/views/autofill/popup/popup_row_factory_utils.cc
@@ -212,8 +212,9 @@ std::unique_ptr<PopupRowContentView> CreatePopupRowContentView(
       popup_cell_utils::CreateMainTextLabel(
           suggestion.main_text,
           GetMainTextStyleForPopupItemId(suggestion.popup_item_id));
-  popup_cell_utils::FormatLabel(*main_text_label, suggestion.main_text,
-                                popup_type);
+  popup_cell_utils::FormatLabel(
+      *main_text_label, suggestion.main_text, popup_type,
+      popup_cell_utils::GetMaxPopupAddressProfileWidth());
   popup_cell_utils::AddSuggestionContentToView(
       suggestion, std::move(main_text_label),
       popup_cell_utils::CreateMinorTextLabel(suggestion.minor_text),
@@ -237,8 +238,9 @@ std::unique_ptr<PopupRowWithButtonView> CreateAutocompleteRowWithDeleteButton(
       popup_cell_utils::CreateMainTextLabel(
           kSuggestion.main_text,
           GetMainTextStyleForPopupItemId(kSuggestion.popup_item_id));
-  popup_cell_utils::FormatLabel(*main_text_label, kSuggestion.main_text,
-                                controller->GetPopupType());
+  popup_cell_utils::FormatLabel(
+      *main_text_label, kSuggestion.main_text, controller->GetPopupType(),
+      popup_cell_utils::GetMaxPopupAddressProfileWidth());
   popup_cell_utils::AddSuggestionContentToView(
       kSuggestion, std::move(main_text_label),
       popup_cell_utils::CreateMinorTextLabel(kSuggestion.minor_text),
