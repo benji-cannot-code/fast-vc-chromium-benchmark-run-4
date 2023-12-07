18ab21951e609479382939b5635f6116ea5c5a26
Victor Tan <victortan@google.com>
2023-12-07 20:19:56 +0000

    Rename enable new ALPS codepoint feature name for H2
    Change-Id: Ibce45411a5f3e7b46db7262653eba315476cdbd3
    Bug: b/289087287
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5095621
    Commit-Queue: Victor Tan <victortan@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234668}

--- chrome/browser/client_hints/client_hints_browsertest.cc
+++ chrome/browser/client_hints/client_hints_browsertest.cc
@@ -117,7 +117,7 @@ constexpr unsigned expected_pre_merge_third_party_client_hints_number = 16u;
 constexpr char kDefaultFeatures[] =
     "UserAgentClientHint,CriticalClientHint,AcceptCHFrame,"
     "ClientHintsFormFactor,ClientHintsPrefersReducedTransparency,"
-    "UseAlpsNewCodepoint";
+    "UseNewAlpsCodepointHttp2";
 
 // All of the status codes from HttpResponseHeaders::IsRedirectResponseCode.
 const net::HttpStatusCode kRedirectStatusCodes[] = {
@@ -3446,7 +3446,7 @@ class CriticalClientHintsBrowserTest : public InProcessBrowserTest {
     // sec-ch-dpr is not included.
     feature_list->InitFromCommandLine(
         "UserAgentClientHint,CriticalClientHint,AcceptCHFrame",
-        "ClientHintsDPR,UseAlpsNewCodepoint");
+        "ClientHintsDPR,UseNewAlpsCodepointHttp2");
     scoped_feature_list_.InitWithFeatureList(std::move(feature_list));
 
     InProcessBrowserTest::SetUp();
@@ -4037,7 +4037,7 @@ class ClientHintsBrowserTestWithEmulatedMedia
       : ClientHintsBrowserTestWithEmulatedMedia(
             "UserAgentClientHint,AcceptCHFrame,"
             "ClientHintsPrefersReducedTransparency,",
-            "UseAlpsNewCodepoint") {}
+            "UseNewAlpsCodepointHttp2") {}
 
   ClientHintsBrowserTestWithEmulatedMedia(const std::string& enable_features,
                                           const std::string& disable_features)
