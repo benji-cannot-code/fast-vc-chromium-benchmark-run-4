f3c97b3135cb5a847e3e8a173b4f173bd4dadba9
Alan Screen <awscreen@chromium.org>
2023-12-08 20:20:43 +0000

    Make OOPPD JobPrint feature parameter default enabled
    The out-of-process print drivers feature uses parameters to control
    some aspects of the feature.  Use of out-of-process for submitting a
    job to print is differentiated from queries by use of a JobPrint
    parameter.
    
    Update the JobPrint parameter to be enabled by default.  Note that this
    is only relevant when the main feature is enabled.
    
    Replace a remaining instance of directly referencing the feature
    parameter with the helper method for checking against the feature and
    the corresponding enterprise policy.
    
    Correct PDFExtensionPrintingTest test setup to setup a fake printer.
    Since these tests automatically perform printing, such behavior only
    makes sense if there is a printer to submit to.  The checks within the
    OOPPD logic exposed this existing issue.
    
    Bug: 1508397
    Change-Id: I0855810c18b66c3fa743d56e7dc54df4f74a1204
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5089789
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235189}

--- printing/printing_features.cc
+++ printing/printing_features.cc
@@ -91,7 +91,7 @@ BASE_FEATURE(kEnableOopPrintDrivers,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 const base::FeatureParam<bool> kEnableOopPrintDriversJobPrint{
-    &kEnableOopPrintDrivers, "JobPrint", false};
+    &kEnableOopPrintDrivers, "JobPrint", true};
 
 const base::FeatureParam<bool> kEnableOopPrintDriversSandbox{
     &kEnableOopPrintDrivers, "Sandbox", false};
