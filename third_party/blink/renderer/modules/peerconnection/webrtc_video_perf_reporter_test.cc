8effded5144333f28feb628e7d90a03b73f45be8
Prudhvikumar Bommana <pbommana@google.com>
2023-12-08 23:46:04 +0000

    Revert "[CodeHealth] Avoid GC-forbidden field in PeerConnectionDependencyFactory"
    This reverts commit 15dc12665b36eac013f2dde5f180df371dbfd928.
    
    Reason for revert: crbug.com/1509326 leads to a crash
    
    Original change's description:
    > [CodeHealth] Avoid GC-forbidden field in PeerConnectionDependencyFactory
    >
    > Make WebrtcVideoPerfReporter garbage-collected, and use HeapMojoRemote
    > instead of mojo::Remote there.
    >
    > Bug: 1426376
    > Change-Id: I778080738c2e9f4b1cb6c015ea29a5205964ba6e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5082441
    > Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    > Reviewed-by: Henrik Boström <hbos@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1233950}
    
    Bug: 1426376
    Change-Id: Ib2c0864c06a1a2fd79503f066d5342fe25b4971f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5107028
    Owners-Override: Prudhvikumar Bommana <pbommana@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Prudhvikumar Bommana <pbommana@google.com>
    Cr-Commit-Position: refs/heads/main@{#1235306}

--- third_party/blink/renderer/modules/peerconnection/webrtc_video_perf_reporter_test.cc
+++ third_party/blink/renderer/modules/peerconnection/webrtc_video_perf_reporter_test.cc
@@ -47,15 +47,15 @@ class WebrtcVideoPerfReporterTest : public ::testing::Test {
  public:
   WebrtcVideoPerfReporterTest() {
     mock_recorder_ = std::make_unique<MockWebrtcVideoPerfRecorder>();
-    reporter_ = MakeGarbageCollected<WebrtcVideoPerfReporter>(
+    reporter_.Initialize(
         blink::scheduler::GetSingleThreadTaskRunnerForTesting(),
-        /* notifier */ nullptr, mock_recorder_->CreatePendingRemote());
+        mock_recorder_->CreatePendingRemote());
   }
 
  protected:
   test::TaskEnvironment task_environment_;
   std::unique_ptr<MockWebrtcVideoPerfRecorder> mock_recorder_;
-  Persistent<WebrtcVideoPerfReporter> reporter_;
+  WebrtcVideoPerfReporter reporter_;
 };
 
 TEST_F(WebrtcVideoPerfReporterTest, StoreWebrtcVideoStats) {
@@ -79,7 +79,7 @@ TEST_F(WebrtcVideoPerfReporterTest, StoreWebrtcVideoStats) {
         EXPECT_EQ(kExpectedFeaturesA, *features);
         EXPECT_EQ(kExpectedVideoStats, *video_stats);
       });
-  reporter_->StoreWebrtcVideoStats(kStatsKeyA, kVideoStats);
+  reporter_.StoreWebrtcVideoStats(kStatsKeyA, kVideoStats);
   base::RunLoop().RunUntilIdle();
 
   // Toggle the booleans.
@@ -98,7 +98,7 @@ TEST_F(WebrtcVideoPerfReporterTest, StoreWebrtcVideoStats) {
         EXPECT_EQ(kExpectedFeaturesB, *features);
         EXPECT_EQ(kExpectedVideoStats, *video_stats);
       });
-  reporter_->StoreWebrtcVideoStats(kStatsKeyB, kVideoStats);
+  reporter_.StoreWebrtcVideoStats(kStatsKeyB, kVideoStats);
   base::RunLoop().RunUntilIdle();
 }
 
