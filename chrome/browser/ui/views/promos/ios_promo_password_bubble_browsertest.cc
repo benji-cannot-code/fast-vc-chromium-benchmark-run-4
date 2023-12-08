8542d52cd684e56e94be7c8b6d39f8964f2600f0
Nicolas MacBeth <nicolasmacbeth@google.com>
2023-12-08 20:40:34 +0000

    [iOS Desktop password promo] Cleaned up feature flag
    Cleaned up feature flag for iOS Desktop Acquisition promo for passwords. Only kept the QR code variant of the promo. launch/4243343
    
    Bug: 1490032
    Change-Id: I3c2b2fe8ecc3ed6dfdd589b9e44b8941fdfb1797
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5096416
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1235200}

--- chrome/browser/ui/views/promos/ios_promo_password_bubble_browsertest.cc
+++ chrome/browser/ui/views/promos/ios_promo_password_bubble_browsertest.cc
@@ -44,21 +44,11 @@ class IOSPromoPasswordBubbleTest : public DialogBrowserTest {
         BrowserView::GetBrowserViewForBrowser(browser())
             ->toolbar_button_provider();
 
-    if (name == "qr_code") {
-      IOSPromoPasswordBubble::ShowBubble(
-          button_provider->GetAnchorView(PageActionIconType::kManagePasswords),
-          button_provider->GetPageActionIconView(
-              PageActionIconType::kManagePasswords),
-          IOSPromoPasswordBubble::PromoVariant::QR_CODE_VARIANT, browser());
-    } else {
-      // default
-      IOSPromoPasswordBubble::ShowBubble(
-          button_provider->GetAnchorView(PageActionIconType::kManagePasswords),
-          button_provider->GetPageActionIconView(
-              PageActionIconType::kManagePasswords),
-          IOSPromoPasswordBubble::PromoVariant::GET_STARTED_BUTTON_VARIANT,
-          browser());
-    }
+    IOSPromoPasswordBubble::ShowBubble(
+        button_provider->GetAnchorView(PageActionIconType::kManagePasswords),
+        button_provider->GetPageActionIconView(
+            PageActionIconType::kManagePasswords),
+        browser());
   }
 
  private:
@@ -66,9 +56,5 @@ class IOSPromoPasswordBubbleTest : public DialogBrowserTest {
 };
 
 IN_PROC_BROWSER_TEST_F(IOSPromoPasswordBubbleTest, InvokeUi_default) {
-  ShowAndVerifyUi();
-}
-
-IN_PROC_BROWSER_TEST_F(IOSPromoPasswordBubbleTest, InvokeUi_qr_code) {
   ShowAndVerifyUi();
 }
