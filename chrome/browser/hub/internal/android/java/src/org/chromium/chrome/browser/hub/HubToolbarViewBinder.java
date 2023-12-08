68001c300a57b1e55626d61787d0a7ff0696eb1e
Sky Malice <skym@chromium.org>
2023-12-08 16:26:00 +0000

    Add pane switcher to hub toolbar.
    Bug: 1496708
    Change-Id: Ibbefd424572133e6cbc57299cbfbc79c8f95c567
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5082269
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1235057}

--- chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubToolbarViewBinder.java
+++ chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubToolbarViewBinder.java
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.hub;
 
 import static org.chromium.chrome.browser.hub.HubToolbarProperties.ACTION_BUTTON_DATA;
+import static org.chromium.chrome.browser.hub.HubToolbarProperties.PANE_SWITCHER_BUTTON_DATA;
+import static org.chromium.chrome.browser.hub.HubToolbarProperties.PANE_SWITCHER_INDEX;
 import static org.chromium.chrome.browser.hub.HubToolbarProperties.SHOW_ACTION_BUTTON_TEXT;
 
 import org.chromium.ui.modelutil.PropertyKey;
@@ -16,8 +18,12 @@ public class HubToolbarViewBinder {
     /** Stateless propagation of properties. */
     public static void bind(PropertyModel model, HubToolbarView view, PropertyKey key) {
         if (key == ACTION_BUTTON_DATA || key == SHOW_ACTION_BUTTON_TEXT) {
-            view.setActionButtonData(
-                    model.get(ACTION_BUTTON_DATA), model.get(SHOW_ACTION_BUTTON_TEXT));
+            view.setActionButton(model.get(ACTION_BUTTON_DATA), model.get(SHOW_ACTION_BUTTON_TEXT));
+        } else if (key == PANE_SWITCHER_BUTTON_DATA) {
+            view.setPaneSwitcherButtonData(
+                    model.get(PANE_SWITCHER_BUTTON_DATA), model.get(PANE_SWITCHER_INDEX));
+        } else if (key == PANE_SWITCHER_INDEX) {
+            view.setPaneSwitcherIndex(model.get(PANE_SWITCHER_INDEX));
         }
     }
 }
