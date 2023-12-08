4516a2703cd1b008a89f95e88e228c7fa19b871d
Alexander Cooper <alcooper@chromium.org>
2023-12-08 19:06:40 +0000

    Simplify OpenXr input generation
    This simplifies the route that OpenXr takes to create it's input sources
    in three main ways:
    
    1) Improved plumbing of `hand_input_enabled` logic.
    Rather than plumbing a bool down from `GetInputState, since the feature
    is enabled on the session, the state of the feature is now assigned to
    the `OpenXrInputHelper` and any necessary children at creation or
    initialization depending on the pattern used by the class. This allows
    for better reasoning about lazy-initialized objects in the class.
    This also allows the OpenXrInputHelper to just always query for hand
    data and allow the Controller to simply return null if the feature is
    not enabled.
    
    2) Create an "Update" method for OpenXrController
    While the OpenXrInputHelper still drives the `xrSyncActions` call since
    that call needs to be fed the data from both controllers, the
    controllers themselves now also have a chance to update any data that
    they may want to update. This is currently just setting the interaction
    profile at an earlier time and updating hand data if it's enabled. This
    does require splitting the `GetHandTrackingData` method into an `Update`
    and an actual `build/query` step, but it allows us to store the struct
    as a member and thus can prevent allocation/deallocation on a per-frame
    basis, and better sets us up to allow future extensions for hand
    tracking.
    
    3) Move Gamepad building into the OpenXrController class
    The logic currently only looks at the interaction profiles to determine
    the type of gamepad that should be built; but the currently proposed
    infrastructure for hand extensions would not add them to that map,
    leaving them to always be `kNone`. While this very well may be the case,
    the methods in question *also* pass around a Controller object to query
    from, so it's also cleaner from that standpoint to move the method into
    the `OpenXrController` class.
    
    Also cleans up a couple unused methods and moves other methods that are
    now purely internal to be private on `OpenXrController`.
    
    Bug: 1504076
    Change-Id: I6a26038d2ef02263c7302d697c84602322426476
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5097334
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235150}

--- device/vr/openxr/openxr_input_helper.h
+++ device/vr/openxr/openxr_input_helper.h
@@ -27,9 +27,12 @@ class OpenXRInputHelper {
       const OpenXrExtensionHelper& extension_helper,
       XrSession session,
       XrSpace local_space,
+      bool hand_input_enabled,
       std::unique_ptr<OpenXRInputHelper>* helper);
 
-  OpenXRInputHelper(XrSession session, XrSpace local_space);
+  OpenXRInputHelper(XrSession session,
+                    XrSpace local_space,
+                    bool hand_input_enabled);
 
   OpenXRInputHelper(const OpenXRInputHelper&) = delete;
   OpenXRInputHelper& operator=(const OpenXRInputHelper&) = delete;
@@ -37,7 +40,6 @@ class OpenXRInputHelper {
   ~OpenXRInputHelper();
 
   std::vector<mojom::XRInputSourceStatePtr> GetInputState(
-      bool hand_input_enabled,
       XrTime predicted_display_time);
 
   XrResult OnInteractionProfileChanged();
@@ -45,8 +47,6 @@ class OpenXRInputHelper {
   bool ReceivedExitGesture() { return received_exit_gesture_; }
 
  private:
-  absl::optional<Gamepad> GetWebXRGamepad(const OpenXrController& controller);
-
   XrResult Initialize(XrInstance instance,
                       XrSystemId system,
                       const OpenXrExtensionHelper& extension_helper);
@@ -73,6 +73,7 @@ class OpenXRInputHelper {
 
   std::unique_ptr<OpenXRPathHelper> path_helper_;
   bool received_exit_gesture_ = false;
+  bool hand_input_enabled_ = false;
 };
 
 }  // namespace device
