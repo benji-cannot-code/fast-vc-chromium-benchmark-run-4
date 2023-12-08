85d2475294a70b0843602083d37512d6110a0b17
Kalvin Lee <kdlee@chromium.org>
2023-12-08 02:08:46 +0000

    PA: Restore `PartitionFreelistEntry` naming
    We have readied a cleaner freelist implementation dispatch mechanism and
    intend to use that rather than splattering `EncodedNextFreelistEntry`
    and `PoolOffsetFreelistEntry` everywhere in PA code.
    
    Bug: 1461983
    Change-Id: Ief108dcca5ddc74838735e347adbc20af89a89a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5097706
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1234819}

--- base/allocator/partition_allocator/src/partition_alloc/hardening_unittest.cc
+++ base/allocator/partition_allocator/src/partition_alloc/hardening_unittest.cc
@@ -44,8 +44,8 @@ TEST(HardeningTest, PartialCorruption) {
   // Even if it looks reasonable (valid encoded pointer), freelist corruption
   // detection will make the code crash, because shadow_ doesn't match
   // encoded_next_.
-  EncodedNextFreelistEntry::EmplaceAndInitForTest(root.ObjectToSlotStart(data),
-                                                  to_corrupt, false);
+  PartitionFreelistEntry::EmplaceAndInitForTest(root.ObjectToSlotStart(data),
+                                                to_corrupt, false);
   EXPECT_DEATH(root.Alloc(kAllocSize), "");
 }
 
@@ -66,8 +66,8 @@ TEST(HardeningTest, OffHeapPointerCrashing) {
 
   // See "PartialCorruption" above for details. This time, make shadow_
   // consistent.
-  EncodedNextFreelistEntry::EmplaceAndInitForTest(root.ObjectToSlotStart(data),
-                                                  to_corrupt, true);
+  PartitionFreelistEntry::EmplaceAndInitForTest(root.ObjectToSlotStart(data),
+                                                to_corrupt, true);
 
   // Crashes, because |to_corrupt| is not on the same superpage as data.
   EXPECT_DEATH(root.Alloc(kAllocSize), "");
@@ -87,7 +87,7 @@ TEST(HardeningTest, MetadataPointerCrashing) {
 
   uintptr_t slot_start = root.ObjectToSlotStart(data);
   auto* metadata = SlotSpanMetadata::FromSlotStart(slot_start);
-  EncodedNextFreelistEntry::EmplaceAndInitForTest(slot_start, metadata, true);
+  PartitionFreelistEntry::EmplaceAndInitForTest(slot_start, metadata, true);
 
   // Crashes, because |metadata| points inside the metadata area.
   EXPECT_DEATH(root.Alloc(kAllocSize), "");
@@ -118,8 +118,8 @@ TEST(HardeningTest, SuccessfulCorruption) {
   root.Free(data2);
   root.Free(data);
 
-  EncodedNextFreelistEntry::EmplaceAndInitForTest(root.ObjectToSlotStart(data),
-                                                  to_corrupt, true);
+  PartitionFreelistEntry::EmplaceAndInitForTest(root.ObjectToSlotStart(data),
+                                                to_corrupt, true);
 
 #if BUILDFLAG(USE_FREESLOT_BITMAP)
   // This part crashes with freeslot bitmap because it detects freelist
