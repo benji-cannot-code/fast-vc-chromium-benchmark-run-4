a710f92a043a40cb12752cebcb704524f289c38c
Siye Liu <siliu@microsoft.com>
2023-12-08 23:09:09 +0000

    Remove CompositionUpdateBeforeBeforeInput runtime feature flag.
    The feature flag was introduced in crrev.com/1200044 to guard the new
    behavior of compositionupdate event firing order. The new behavior was
    shipped in 119. There is no issue filed against the behavior since then
    and is confirmed in
    https://github.com/w3c/uievents/issues/354#issuecomment-1847713182.
    
    Change-Id: Ib90621f8fd1236d9cadd08782150245c19c46971
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5106828
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Siye Liu <siliu@microsoft.com>
    Reviewed-by: Sanket Joshi <sajos@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1235290}

--- third_party/blink/renderer/core/editing/ime/input_method_controller.cc
+++ third_party/blink/renderer/core/editing/ime/input_method_controller.cc
@@ -68,7 +68,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_theme.h"
 #include "third_party/blink/renderer/core/page/focus_controller.h"
 #include "third_party/blink/renderer/core/page/page.h"
-#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 
@@ -453,12 +452,10 @@ void InputMethodController::InsertTextDuringCompositionWithEvents(
   if (!target)
     return;
 
-  if (RuntimeEnabledFeatures::CompositionUpdateBeforeBeforeInputEnabled()) {
-    DispatchCompositionUpdateEvent(frame, text);
-    // 'compositionupdate' event handler may destroy document.
-    if (!IsAvailable()) {
-      return;
-    }
+  DispatchCompositionUpdateEvent(frame, text);
+  // 'compositionupdate' event handler may destroy document.
+  if (!IsAvailable()) {
+    return;
   }
 
   DispatchBeforeInputFromComposition(
@@ -467,14 +464,6 @@ void InputMethodController::InsertTextDuringCompositionWithEvents(
   // 'beforeinput' event handler may destroy document.
   if (!IsAvailable())
     return;
-
-  if (!RuntimeEnabledFeatures::CompositionUpdateBeforeBeforeInputEnabled()) {
-    DispatchCompositionUpdateEvent(frame, text);
-    // 'compositionupdate' event handler may destroy document.
-    if (!IsAvailable()) {
-      return;
-    }
-  }
 
   // TODO(editing-dev): The use of UpdateStyleAndLayout
   // needs to be audited. see http://crbug.com/590369 for more details.
