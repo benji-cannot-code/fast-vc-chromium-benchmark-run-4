3793e3fb9ae0c84e30d8d80932330db3b9d62f16
Joemer Ramos <joemerramos@chromium.org>
2023-12-08 20:20:33 +0000

    [iOS] Download API_AVAILABLE iOS 14 Guard Clean Up
    Clean up for iOS 14 API_AVAILABLE guards since the minimum deployment
    target is now 15.0.
    
    Bug: 1418068
    Change-Id: Ia7b966406abd150d581bc28524245beff732f111
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029574
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235188}

--- ios/web/download/crw_web_view_download.mm
+++ ios/web/download/crw_web_view_download.mm
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, strong) NSURLRequest* request;
 
 // Download object of a web resource.
-@property(nonatomic, strong) WKDownload* download API_AVAILABLE(ios(14.5));
+@property(nonatomic, strong) WKDownload* download;
 
 @end
 
@@ -24,8 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (instancetype)initWithPath:(NSString*)destination
                      request:(NSURLRequest*)request
                      webview:(WKWebView*)webview
-                    delegate:(id<CRWWebViewDownloadDelegate>)delegate
-    API_AVAILABLE(ios(14.5)) {
+                    delegate:(id<CRWWebViewDownloadDelegate>)delegate {
   self = [super init];
   if (self) {
     self.destinationPath = destination;
@@ -36,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return self;
 }
 
-- (void)startDownload API_AVAILABLE(ios(14.5)) {
+- (void)startDownload {
   [self.webView startDownloadUsingRequest:self.request
                         completionHandler:^(WKDownload* download) {
                           download.delegate = self;
@@ -44,7 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                         }];
 }
 
-- (void)cancelDownload:(ProceduralBlock)completion API_AVAILABLE(ios(14.5)) {
+- (void)cancelDownload:(ProceduralBlock)completion {
   [self.download cancel:^(NSData* resumeData) {
     if (completion) {
       completion();
@@ -58,19 +57,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     decideDestinationUsingResponse:(NSURLResponse*)response
                  suggestedFilename:(NSString*)suggestedFilename
                  completionHandler:
-                     (void (^)(NSURL* destination))completionHandler
-    API_AVAILABLE(ios(14.5)) {
+                     (void (^)(NSURL* destination))completionHandler {
   NSURL* destinationURL = [NSURL fileURLWithPath:self.destinationPath];
   completionHandler(destinationURL);
 }
 
-- (void)downloadDidFinish:(WKDownload*)download API_AVAILABLE(ios(14.5)) {
+- (void)downloadDidFinish:(WKDownload*)download {
   [self.delegate downloadDidFinish];
 }
 
 - (void)download:(WKDownload*)download
     didFailWithError:(NSError*)error
-          resumeData:(NSData*)resumeData API_AVAILABLE(ios(14.5)) {
+          resumeData:(NSData*)resumeData {
   [self.delegate downloadDidFailWithError:error];
 }
 
--- ios/web/public/download/crw_web_view_download.h
+++ ios/web/public/download/crw_web_view_download.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @protocol CRWWebViewDownload <NSObject>
 
 // Cancels the download with a completion block.
-- (void)cancelDownload:(ProceduralBlock)completion API_AVAILABLE(ios(14.5));
+- (void)cancelDownload:(ProceduralBlock)completion;
 
 @end
 
