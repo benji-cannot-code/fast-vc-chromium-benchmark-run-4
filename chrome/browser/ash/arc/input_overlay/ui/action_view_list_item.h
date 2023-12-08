b00084f5da3851ce4d46752122ff6f86d7f96444
Khaya Kingston <khayak@google.com>
2023-12-08 00:18:45 +0000

    arc/gio: Remove class Nudge and its usage
    This CL removes the nudge.h and nudge.cc files, removes all references
    to Nudge, the AddNudgeWidget function, the RemoveNudgeWidget function,
    two related unit tests,ButtonOptionsMenu::ShowEduNudgeForEditingTip(),
    ActionViewListItem::ShowEduNudgeForEditingTip(), and
    EditLabels::ShowEduNudgeForEditingTip() functions.
    
    Bug: b/312536968
    Test: unit_tests --gtest_filter="EditingListTest*"
    Test: unit_tests --gtest_filter="DisplayOverlayControllerTest*"
    Test: unit_tests --gtest_filter="ButtonOptionsMenuTest*"
    Change-Id: Ib4c1c0de9510abdbadf8dc7e40ad90210b4040bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5078387
    Commit-Queue: Khaya Kingston <khayak@google.com>
    Reviewed-by: Cici Ruan <cuicuiruan@google.com>
    Reviewed-by: Prameet Shah <phshah@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234778}

--- chrome/browser/ash/arc/input_overlay/ui/action_view_list_item.h
+++ chrome/browser/ash/arc/input_overlay/ui/action_view_list_item.h
@@ -29,8 +29,6 @@ class ActionViewListItem : public ActionEditView {
   // ActionEditView:
   void OnActionNameUpdated() override;
 
-  void ShowEduNudgeForEditingTip();
-
  private:
   friend class EditLabelTest;
 
