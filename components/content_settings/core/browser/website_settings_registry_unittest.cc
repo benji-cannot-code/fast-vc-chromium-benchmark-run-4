97ed4cbd5a218959487bbb11cbf9e2c6082424b6
Gyuyoung Kim <gyuyoung@igalia.com>
2023-12-08 01:57:21 +0000

    [iOS Blink] Register a couple of content settings types
    This CL registers 'app-banner' and 'site-engagement'
    content settings types to the website settings registry
    for iOS Blink port in order to enable
    AppBannerSettingsHelperTest.
    
    TESTS: AppBannerSettingsHelperTest.* unit tests.
    
    Bug: 1476736
    Change-Id: I89f429dab1b274338051b9f6c3ea05244370cf55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5094813
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Cait Phillips <caitkp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234817}

--- components/content_settings/core/browser/website_settings_registry_unittest.cc
+++ components/content_settings/core/browser/website_settings_registry_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/values.h"
+#include "build/blink_buildflags.h"
 #include "build/build_config.h"
 #include "components/content_settings/core/browser/website_settings_info.h"
 #include "components/content_settings/core/common/content_settings.h"
@@ -63,7 +64,7 @@ TEST_F(WebsiteSettingsRegistryTest, GetByName) {
 }
 
 TEST_F(WebsiteSettingsRegistryTest, GetPlatformDependent) {
-#if BUILDFLAG(IS_IOS)
+#if !BUILDFLAG(USE_BLINK)
   // App banner shouldn't be registered on iOS.
   EXPECT_FALSE(registry()->Get(ContentSettingsType::APP_BANNER));
 #else
