6f3ea5882c7a17fb0933b7625e4e770a0f508f40
Joemer Ramos <joemerramos@chromium.org>
2023-12-08 22:44:16 +0000

    [iOS] NewTabPageHeader API_AVAILABLE iOS 14 Guard Clean Up
    Clean up for iOS 14 API_AVAILABLE guards since the minimum deployment
    target is now 15.0.
    
    Low-Coverage-Reason: TRIVIAL_CHANGE
    Bug: 1418068
    Change-Id: I950db2ce8856e9fa5a8f3de0ddc1fc37c93e01a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5028085
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235274}

--- ios/chrome/browser/ui/ntp/new_tab_page_header_view_controller.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_header_view_controller.mm
@@ -577,23 +577,20 @@ NSString* const kScribbleFakeboxElementId = @"fakebox";
               requestElementsInRect:(CGRect)rect
                          completion:
                              (void (^)(NSArray<UIScribbleElementIdentifier>*
-                                           elements))completion
-    API_AVAILABLE(ios(14.0)) {
+                                           elements))completion {
   completion(@[ kScribbleFakeboxElementId ]);
 }
 
 - (BOOL)indirectScribbleInteraction:(UIIndirectScribbleInteraction*)interaction
                    isElementFocused:
-                       (UIScribbleElementIdentifier)elementIdentifier
-    API_AVAILABLE(ios(14.0)) {
+                       (UIScribbleElementIdentifier)elementIdentifier {
   DCHECK(elementIdentifier == kScribbleFakeboxElementId);
   return self.toolbarDelegate.fakeboxScribbleForwardingTarget.isFirstResponder;
 }
 
 - (CGRect)
     indirectScribbleInteraction:(UIIndirectScribbleInteraction*)interaction
-                frameForElement:(UIScribbleElementIdentifier)elementIdentifier
-    API_AVAILABLE(ios(14.0)) {
+                frameForElement:(UIScribbleElementIdentifier)elementIdentifier {
   DCHECK(elementIdentifier == kScribbleFakeboxElementId);
 
   // Imitate the entire location bar being scribblable.
@@ -606,7 +603,7 @@ NSString* const kScribbleFakeboxElementId = @"fakebox";
                      referencePoint:(CGPoint)focusReferencePoint
                          completion:
                              (void (^)(UIResponder<UITextInput>* focusedInput))
-                                 completion API_AVAILABLE(ios(14.0)) {
+                                 completion {
   if (!self.toolbarDelegate.fakeboxScribbleForwardingTarget.isFirstResponder) {
     [self.toolbarDelegate.fakeboxScribbleForwardingTarget becomeFirstResponder];
   }
@@ -616,8 +613,7 @@ NSString* const kScribbleFakeboxElementId = @"fakebox";
 
 - (BOOL)indirectScribbleInteraction:(UIIndirectScribbleInteraction*)interaction
          shouldDelayFocusForElement:
-             (UIScribbleElementIdentifier)elementIdentifier
-    API_AVAILABLE(ios(14.0)) {
+             (UIScribbleElementIdentifier)elementIdentifier {
   DCHECK(elementIdentifier == kScribbleFakeboxElementId);
   return YES;
 }
