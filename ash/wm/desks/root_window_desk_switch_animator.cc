0ec31cb3a3c43673123c1c46d76a502cdf35211a
Daniel Andersson <dandersson@chromium.org>
2023-12-08 18:39:00 +0000

    ash: desk switching to handle failed screenshots gracefully
    When switching desks, screenshots are made of the starting and target
    desks. If taking a screenshot fails two times, then we will hit a
    `NOTREACHED`. From there on, the failed desk switching can put ash in a
    weird state.
    
    This CL removes the `NOTREACHED` from `RootWindowDeskSwitchAnimator` and
    changes `DeskAnimationBase` so that if any of its animators (one per
    display) shows signs of distress, the whole project is abandoned. The
    target desk is activated immediately without any animation.
    
    TEST=Manual testing on device
    BUG=b:313301644
    
    Change-Id: I0ef3bba4538df2d906ebe25b7fb431237d8bdca2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5100575
    Reviewed-by: Yongshun Liu <yongshun@chromium.org>
    Commit-Queue: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235124}

--- ash/wm/desks/root_window_desk_switch_animator.cc
+++ ash/wm/desks/root_window_desk_switch_animator.cc
@@ -491,8 +491,7 @@ void RootWindowDeskSwitchAnimator::OnStartingDeskScreenshotTaken(
       TakeStartingDeskScreenshot();
     } else {
       LOG(ERROR) << "Received multiple empty screenshots of the starting desk.";
-      NOTREACHED();
-      starting_desk_screenshot_taken_ = true;
+      screenshot_failed_ = true;
       delegate_->OnStartingDeskScreenshotTaken(ending_desk_index_);
     }
 
@@ -513,8 +512,7 @@ void RootWindowDeskSwitchAnimator::OnEndingDeskScreenshotTaken(
       TakeEndingDeskScreenshot();
     } else {
       LOG(ERROR) << "Received multiple empty screenshots of the ending desk.";
-      NOTREACHED();
-      ending_desk_screenshot_taken_ = true;
+      screenshot_failed_ = true;
       delegate_->OnEndingDeskScreenshotTaken();
     }
 
