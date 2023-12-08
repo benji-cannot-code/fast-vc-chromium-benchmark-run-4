4d767554bf37b90e8e0504d19223107a50d53a9e
Ce Chen <cch@chromium.org>
2023-12-08 21:21:49 +0000

    [omnibox] launch on device head bigger model for Mobile.
    See cl/574517881 for 40/40 ramp up study metric summary.
    
    Bug: 925072
    Change-Id: I2dfc321d8fddfca55b3ffe2619ecc622670eaca6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4950137
    Commit-Queue: C Ch <cch@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235229}

--- components/omnibox/browser/omnibox_field_trial.cc
+++ components/omnibox/browser/omnibox_field_trial.cc
@@ -613,11 +613,9 @@ std::string OmniboxFieldTrial::OnDeviceHeadModelLocaleConstraint(
                    : &omnibox::kOnDeviceHeadProviderNonIncognito;
   std::string constraint = base::GetFieldTrialParamValueByFeature(
       *feature, kOnDeviceHeadModelLocaleConstraint);
-#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
   if (constraint.empty()) {
     constraint = "500000";
   }
-#endif  // !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
   return constraint;
 }
 
