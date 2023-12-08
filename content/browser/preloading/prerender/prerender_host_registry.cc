f6f8a0a7fa7ab0cdf54b61c3afb7cec90e9d0f70
Taiyo Mizuhashi <taiyo@chromium.org>
2023-12-08 08:34:15 +0000

    Prerender: Propagate new tab trigger cancellation to initiator WC
    The cancellation of new-tab hosts can be invoked through either the
    initiator WebContents’s PrerenderHostRegistry or PHR on WC created for
    new-tab hosts. This CL propagates latter cancellation to initiator WC’s
    PHR to perform the followings:
    - Clear the corresponding entry of
    prerender_new_tab_handle_by_frame_tree_node_id_ in the initiator PHR.
    This will eventually destroy WC created for new-tab hosts.
    - Notify the initiator PrerendererImpl of cancellation (for consistency)
    
    This CL implements this by asynchronously calling initiator PHR’s
    CancelNewTabHostInternal on the CancelHostInternal on the new-tab PHR.
    For more information, please refer to this doc:
    https://docs.google.com/document/d/1JSVM-Q03rsn9bAzlRdLsA_FMjPeJTI8KonEjorX27Xo
    
    Bug: 1350676, 1499759
    Change-Id: I2e7e2bf42132a1f16f99993fef2430996fa65259
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5020281
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234906}

--- content/browser/preloading/prerender/prerender_host_registry.cc
+++ content/browser/preloading/prerender/prerender_host_registry.cc
@@ -1027,6 +1027,29 @@ bool PrerenderHostRegistry::CancelHostInternal(
 
   NotifyCancel(prerender_host->frame_tree_node_id(), reason);
 
+  // Under kPrerender2InNewTab, if the host we are attempting to cancel is the
+  // new-tab host and initiator WebContents's PrerenderHostRegistry for this
+  // host is still alive, invoke the initiator WebContents's
+  // CancelNewTabHostInternal to destroy PrerenderNewTabHandle and WebContents
+  // that this new-tab host belongs to. This will eventually destroy `this`, so
+  // it should be performed asynchronously.
+  if (base::FeatureList::IsEnabled(blink::features::kPrerender2InNewTab)) {
+    CHECK(prerender_host->initiator_web_contents());
+    WebContentsImpl* initiator_web_contents = static_cast<WebContentsImpl*>(
+        prerender_host->initiator_web_contents().get());
+    if (web_contents() != initiator_web_contents &&
+        !initiator_web_contents->IsBeingDestroyed()) {
+      base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
+          FROM_HERE,
+          base::BindOnce(
+              base::IgnoreResult(
+                  &PrerenderHostRegistry::CancelNewTabHostInternal),
+              initiator_web_contents->GetPrerenderHostRegistry()->GetWeakPtr(),
+              frame_tree_node_id,
+              PrerenderCancellationReason(reason.final_status())));
+    }
+  }
+
   // Asynchronously delete the prerender host.
   ScheduleToDeleteAbandonedHost(std::move(prerender_host), reason);
   return true;
@@ -1052,7 +1075,6 @@ bool PrerenderHostRegistry::CancelNewTabHostInternal(
   prerender_new_tab_handle_by_frame_tree_node_id_.erase(iter);
   NotifyCancel(frame_tree_node_id, reason);
   handle->CancelPrerendering(reason);
-
   return true;
 }
 
@@ -1219,6 +1241,12 @@ PrerenderHost* PrerenderHostRegistry::FindHostByUrlForTesting(
     }
   }
   return nullptr;
+}
+
+bool PrerenderHostRegistry::HasNewTabHandleByIdForTesting(
+    int frame_tree_node_id) {
+  return prerender_new_tab_handle_by_frame_tree_node_id_.contains(
+      frame_tree_node_id);
 }
 
 void PrerenderHostRegistry::CancelAllHostsForTesting() {
