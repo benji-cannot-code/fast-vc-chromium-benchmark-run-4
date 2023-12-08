bf33d1761f9692f17937f8f18aee2f031c6d00a1
Anton Maliev <amaliev@chromium.org>
2023-12-08 19:07:00 +0000

    Enable ThirdPartyCookieHeuristics by default in TOT.
    In preparation for ramping the launch study to 100% (expected by Finch freeze on 12/13) we are updating the code to use the enabled study in tests by default. Also cleaning up the study from the Finchtrial testing config.
    
    Change-Id: I2be24fb2861f3ff08499a20c2fd926e572e516fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5100932
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Anton Maliev <amaliev@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235151}

--- chrome/browser/tpcd/heuristics/opener_heuristic_browsertest.cc
+++ chrome/browser/tpcd/heuristics/opener_heuristic_browsertest.cc
@@ -572,10 +572,10 @@ IN_PROC_BROWSER_TEST_P(OpenerHeuristicPastInteractionGrantBrowserTest,
 // TODO(crbug.com/1506932) Flaky on mac.
 #if BUILDFLAG(IS_MAC)
 #define MAYBE_AdTaggedPopupPastInteractionIsReported_WithStorageAccessGrant \
-    DISABLED_AdTaggedPopupPastInteractionIsReported_WithStorageAccessGrant
+  DISABLED_AdTaggedPopupPastInteractionIsReported_WithStorageAccessGrant
 #else
 #define MAYBE_AdTaggedPopupPastInteractionIsReported_WithStorageAccessGrant \
-    AdTaggedPopupPastInteractionIsReported_WithStorageAccessGrant
+  AdTaggedPopupPastInteractionIsReported_WithStorageAccessGrant
 #endif
 IN_PROC_BROWSER_TEST_P(
     OpenerHeuristicPastInteractionGrantBrowserTest,
@@ -1343,6 +1343,8 @@ class OpenerHeuristicBackfillGrantBrowserTest
   OpenerHeuristicBackfillGrantBrowserTest() {
     tpcd_heuristics_grants_params_["TpcdBackfillPopupHeuristicsGrants"] =
         GetParam() ? "10m" : "0s";
+    tpcd_heuristics_grants_params_
+        ["TpcdWritePopupCurrentInteractionHeuristicsGrants"] = "0s";
   }
 
   void SetUpOnMainThread() override {
