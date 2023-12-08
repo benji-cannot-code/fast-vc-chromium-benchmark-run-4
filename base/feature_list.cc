b9d3ebb1e2e422d957366d034ba55433643913c1
Peter Boström <pbos@chromium.org>
2023-12-08 23:05:11 +0000

    Increase configurable-DCHECK loglevel to ERROR
    DCHECK failures are always errors that are sometimes fatal. INFO is a
    weird loglevel for it.
    
    Bug: None
    Change-Id: I7abbbfd7177fad0a268ecbc6cbb7a35792500fd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5106550
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235286}

--- base/feature_list.cc
+++ base/feature_list.cc
@@ -608,7 +608,7 @@ void FeatureList::SetInstance(std::unique_ptr<FeatureList> instance) {
           "gtest_internal_run_death_test")) {
     logging::LOGGING_DCHECK = logging::LOG_FATAL;
   } else {
-    logging::LOGGING_DCHECK = logging::LOG_INFO;
+    logging::LOGGING_DCHECK = logging::LOG_ERROR;
   }
 #endif  // BUILDFLAG(DCHECK_IS_CONFIGURABLE)
 }
