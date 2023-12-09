2651b9e24a2d921454bd1112e5f8b8c4526b3ca2
Ming-Ying Chung <mych@chromium.org>
2023-12-09 00:55:51 +0000

    [deprecate-unload] Enable unload for RenderFrameHostManagerTest.NestedUnload
    by moving it to another test suite
    RenderFrameHostManagerUnloadBrowserTest
    
    Bug: 1432116
    Change-Id: I18da46253ed4a91eaa2ffbcab9ad1024641aaa6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5098305
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235334}

--- content/browser/renderer_host/render_frame_host_manager_browsertest.cc
+++ content/browser/renderer_host/render_frame_host_manager_browsertest.cc
@@ -5544,8 +5544,6 @@ class RenderFrameHostManagerUnloadBrowserTest
         base::Unretained(this)));
 
     RenderFrameHostManagerTest::SetUpOnMainThread();
-
-    StartEmbeddedServer();
   }
 
  private:
@@ -5582,6 +5580,7 @@ class RenderFrameHostManagerUnloadBrowserTest
 // site isolation if the iframe was in its own process.
 IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerUnloadBrowserTest,
                        SubframeTerminationPing_SendBeacon) {
+  StartEmbeddedServer();
   // See BackForwardCache::DisableForTestingReason for explanation.
   DisableBackForwardCache(BackForwardCacheImpl::TEST_USES_UNLOAD_EVENT);
   const std::string unload_event_names[] = {"unload", "pagehide",
@@ -5623,6 +5622,7 @@ IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerUnloadBrowserTest,
 // site isolation if the iframe was in its own process.
 IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerUnloadBrowserTest,
                        SubframeTerminationPing_Image) {
+  StartEmbeddedServer();
   // See BackForwardCache::DisableForTestingReason for explanation.
   DisableBackForwardCache(BackForwardCacheImpl::TEST_REQUIRES_NO_CACHING);
 
@@ -5658,6 +5658,7 @@ IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerUnloadBrowserTest,
 // termination ping.
 IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerUnloadBrowserTest,
                        SubframeTerminationPingWhenWindowCloses) {
+  StartEmbeddedServer();
   EXPECT_TRUE(NavigateToURL(
       shell(), embedded_test_server()->GetURL("a.com", "/title1.html")));
   FrameTreeNode* root = static_cast<WebContentsImpl*>(shell()->web_contents())
@@ -5699,6 +5700,7 @@ IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerUnloadBrowserTest,
 // the iframe's process eventually exits.
 IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerUnloadBrowserTest,
                        SubframeProcessGoesAwayAfterUnloadTimeout) {
+  StartEmbeddedServer();
   // See BackForwardCache::DisableForTestingReason for explanation.
   DisableBackForwardCache(BackForwardCacheImpl::TEST_REQUIRES_NO_CACHING);
 
@@ -5731,6 +5733,7 @@ IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerUnloadBrowserTest,
 // See https://crbug.com/857274.
 IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerUnloadBrowserTest,
                        PostMessageToParentWhenSubframeNavigates) {
+  StartEmbeddedServer();
   GURL main_url(embedded_test_server()->GetURL(
       "a.com", "/cross_site_iframe_factory.html?a(b)"));
   EXPECT_TRUE(NavigateToURL(shell(), main_url));
@@ -5783,6 +5786,7 @@ IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerUnloadBrowserTest,
 // https://crbug.com/867274.
 IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerUnloadBrowserTest,
                        PendingDeleteRFHProcessShutdownDoesNotRemoveSubframes) {
+  StartEmbeddedServer();
   GURL first_url(embedded_test_server()->GetURL("a.com", "/title1.html"));
   EXPECT_TRUE(NavigateToURL(shell(), first_url));
   FrameTreeNode* root = static_cast<WebContentsImpl*>(shell()->web_contents())
@@ -5840,6 +5844,7 @@ IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerUnloadBrowserTest,
 IN_PROC_BROWSER_TEST_P(
     RenderFrameHostManagerUnloadBrowserTest,
     AddAndRemoveEventListenersAffectingSuddenTerminationDisablerState) {
+  StartEmbeddedServer();
   const std::string sudden_termination_disabler_event_names[] = {
       "unload", "beforeunload", "pagehide", "visibilitychange"};
 
@@ -6544,7 +6549,7 @@ IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerTest,
 // A1(B(A2)) and a navigation from B->A3, committing A3 will unload B. However,
 // unloading B will also detach A2, and A2's unload handler can detach B since
 // it can script A1 and remove the frame owner element synchronously.
-IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerTest, NestedUnload) {
+IN_PROC_BROWSER_TEST_P(RenderFrameHostManagerUnloadBrowserTest, NestedUnload) {
   // These tests require site isolation to trigger the (formerly problematic)
   // delayed detach of the remote frame when swapping in the new local frame.
   if (!AreAllSitesIsolatedForTesting())
