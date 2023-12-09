008c24c3441cf701c1fcdcbf28331bea1f4bc377
Chase Phillips <cmp@chromium.org>
2023-12-09 01:35:51 +0000

    guest_view: Improve SetOwnerHost() with per-owner type checks
    This change modifies GuestViewBase to have additional methods to support
    indicating when a GuestView instance is owned by an Extension, a WebUI,
    or a Controlled Frame embedder.
    
    As part of this change, there's a prepared but currently disabled change
    to return an owner host URL rather than an empty string in the
    Controlled Frame case. This can't be enabled currently without further
    changes to ScriptContextSet which will occur in a later CL.
    
    While here, modify functions in manager and delegates with const as
    needed.
    
    Bug: 1233993, b/315342642
    Change-Id: Icd6b8165825db549eb64db0337db280b984bf6c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5090771
    Auto-Submit: Chase Phillips <cmp@chromium.org>
    Reviewed-by: Ovidio Ruiz-Henríquez <odejesush@chromium.org>
    Reviewed-by: W. James Maclean <wjmaclean@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Chase Phillips <cmp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235351}

--- components/guest_view/browser/guest_view_base.cc
+++ components/guest_view/browser/guest_view_base.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/guest_view/common/guest_view_constants.h"
 #include "components/zoom/zoom_controller.h"
 #include "content/public/browser/file_select_listener.h"
+#include "content/public/browser/isolated_web_apps_policy.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_view_host.h"
@@ -381,7 +382,7 @@ content::NavigationController& GuestViewBase::GetController() {
   return web_contents()->GetController();
 }
 
-GuestViewManager* GuestViewBase::GetGuestViewManager() {
+GuestViewManager* GuestViewBase::GetGuestViewManager() const {
   return GuestViewManager::FromBrowserContext(browser_context());
 }
 
@@ -416,6 +417,10 @@ WebContents* GuestViewBase::GetOwnerWebContents() {
 content::RenderFrameHost* GuestViewBase::GetProspectiveOuterDocument() {
   DCHECK(!attached());
   return owner_rfh();
+}
+
+const GURL& GuestViewBase::GetOwnerLastCommittedURL() const {
+  return owner_rfh()->GetLastCommittedURL();
 }
 
 const GURL& GuestViewBase::GetOwnerSiteURL() const {
@@ -858,10 +863,35 @@ void GuestViewBase::UpdateGuestSize(const gfx::Size& new_size,
   guest_size_ = new_size;
 }
 
+bool GuestViewBase::IsOwnedByExtension() const {
+  return GetGuestViewManager()->IsOwnedByExtension(this);
+}
+
+bool GuestViewBase::IsOwnedByWebUI() const {
+  return owner_rfh()->GetMainFrame()->GetWebUI();
+}
+
+bool GuestViewBase::IsOwnedByControlledFrameEmbedder() const {
+  return GetGuestViewManager()->IsOwnedByControlledFrameEmbedder(this);
+}
+
 void GuestViewBase::SetOwnerHost() {
-  owner_host_ = GetGuestViewManager()->IsOwnedByExtension(this)
-                    ? owner_rfh()->GetLastCommittedURL().host()
-                    : std::string();
+  const GURL& url = GetOwnerLastCommittedURL();
+  if (IsOwnedByExtension()) {
+    owner_host_ = url.host();
+  } else if (IsOwnedByWebUI()) {
+    owner_host_ = std::string();
+  } else if (IsOwnedByControlledFrameEmbedder()) {
+    // TODO(cmp): This function must return an empty string currently since
+    // events that pass a non-empty string are validated and will break
+    // Controlled Frame. A future CL will modify the validation to support the
+    // Controlled Frame case and allow us to use the following:
+    // owner_host_ = url.spec();
+    owner_host_ = std::string();
+  } else {
+    owner_host_ = std::string();
+  }
+  return;
 }
 
 bool GuestViewBase::CanBeEmbeddedInsideCrossProcessFrames() const {
