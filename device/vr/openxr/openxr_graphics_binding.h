2280f40acb193f596934449375496015057f7e87
vikas soni <vikassoni@chromium.org>
2023-12-09 03:56:43 +0000

    Switch to GMBHandles in OpenXrGraphicsBindingOpenGLES.
    Switch to using GMBHandles instead of GMB in OpenXrGraphicsBindingOpenGLES.
    Use ScopedHardwareBufferHandle in device::SwapChainInfo which is used to
    create GMBHandles.
    
    Bug: 1431491
    Change-Id: I45f35760238aa7c7805f0c6e3afb9556c004c7fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5096457
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Auto-Submit: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235372}

--- device/vr/openxr/openxr_graphics_binding.h
+++ device/vr/openxr/openxr_graphics_binding.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(IS_ANDROID)
+#include "base/android/scoped_hardware_buffer_handle.h"
 #include "gpu/ipc/common/gpu_memory_buffer_impl_android_hardware_buffer.h"
 #include "ui/gl/scoped_egl_image.h"
 #endif
@@ -75,8 +76,8 @@ struct SwapChainInfo {
   // This property isn't android-specific but it is currently unused on Windows.
   gfx::Size shared_buffer_size{0, 0};
 
-  // Shared GpuMemoryBuffer
-  std::unique_ptr<gpu::GpuMemoryBufferImplAndroidHardwareBuffer> gmb;
+  // This owns a single reference to an AHardwareBuffer object.
+  base::android::ScopedHardwareBufferHandle scoped_ahb_handle;
 
   // This object keeps the image alive while processing a frame. That's
   // required because it owns underlying resources, and must still be
