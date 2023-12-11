4a1b50d64494b8b88d14ae216b0f7b6e7213d12e
Robbie McElrath <rmcelrath@chromium.org>
2023-12-11 10:18:29 +0000

    code-health: Migrate modules/smart_card to unittests_v2
    Bug: 1507326
    Change-Id: Ibb2ac2982d8ceb9fa556d5afcbc76cfef7c4c2b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5101142
    Commit-Queue: Daniel d'Andrada <dandrader@google.com>
    Auto-Submit: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Daniel d'Andrada <dandrader@google.com>
    Cr-Commit-Position: refs/heads/main@{#1235651}

--- third_party/blink/renderer/modules/smart_card/smart_card_error_unittest.cc
+++ third_party/blink/renderer/modules/smart_card/smart_card_error_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/testing/dummy_page_holder.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
+#include "third_party/blink/renderer/platform/testing/task_environment.h"
 
 namespace blink {
 
@@ -30,6 +31,8 @@ class PromiseRejectedFunction : public ScriptFunction::Callable {
 };
 
 TEST(SmartCardError, RejectWithoutScriptStateScope) {
+  test::TaskEnvironment task_environment;
+
   std::unique_ptr<DummyPageHolder> page_holder =
       DummyPageHolder::CreateAndCommitNavigation(KURL());
 
