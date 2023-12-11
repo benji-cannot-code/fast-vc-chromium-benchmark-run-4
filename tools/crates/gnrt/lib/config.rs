8bc54fb12912f5baf007d5d187bea2f377925882
Lukasz Anforowicz <lukasza@chromium.org>
2023-12-11 20:49:52 +0000

    [gnrt] Skip bin targets by default (with opt-in via gnrt_config.toml).
    This CL is motivated by the desire to address the second requirement
    below, while making sure that the first requirement doesn't regress:
    
    * Requirement 1: `gnrt gen` needs to generate `BUILD.gn` targets for
      _some_ binary targets.  Currently this is required only for a single
      target: `cxxbridge`.
    
    * Requirement 2: All generated `BUILD.gn` targets should build.  Before
      this CL this requirement wouldn't hold for some binary targets - for
      example the `uwuifier` target from the Chromium section of the
      Comprehensive Rust course would fail to build - see
      https://crbug.com/1509606.
    
    This CL skips binary targets by default, because:
    
    * This helps to address the second requirement above
    * Transitive dependencies only follow the library targets (i.e. skipping
      binary targets should not introduce dangling dependencies)
    * Binary targets seem to be needed rarely in practice
    
    This CL adds a new `gnrt_config.toml` entry that allows an opt-in for
    coverinng binary targets when needed.  This helps to address the first
    requirement above.
    
    This CL also introduces the `BuildConfig::get_combined_set` method to
    avoid duplication of code related to processing
    `CrateConfig::bin_targets` (introduced in this CL) and
    `CrateConfig::remove_deps` (already exists before this CL).
    
    Bug: 1509606
    Change-Id: I9e7131c54a0ee4fd602c663a338770bc8a2bced4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5110823
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235925}

--- tools/crates/gnrt/lib/config.rs
+++ tools/crates/gnrt/lib/config.rs
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //! Configures gnrt behavior. Types match `gnrt_config.toml` fields. Currently
 //! only used for std bindings.
 
-use std::collections::{BTreeMap, HashMap};
+use std::collections::{BTreeMap, HashMap, HashSet};
 
 use serde::Deserialize;
 
@@ -23,6 +23,20 @@ pub struct BuildConfig {
     /// name. Config is additive with `all_config`.
     #[serde(rename = "crate")]
     pub per_crate_config: BTreeMap<String, CrateConfig>,
+}
+
+impl BuildConfig {
+    /// Combines the global and per-crate `CrateConfig` for a single
+    /// `Vec<String>` entry.
+    pub fn get_combined_set(
+        &self,
+        package_name: &str,
+        entry_getter: impl Fn(&CrateConfig) -> &Vec<String>,
+    ) -> HashSet<&str> {
+        let all: Option<&Vec<String>> = Some(entry_getter(&self.all_config));
+        let per: Option<&Vec<String>> = self.per_crate_config.get(package_name).map(entry_getter);
+        all.into_iter().chain(per).flatten().map(String::as_str).collect()
+    }
 }
 
 /// Configures GN output for this session.
@@ -95,6 +109,11 @@ pub struct CrateConfig {
     pub extra_build_script_input_roots: Vec<std::path::PathBuf>,
     #[serde(default)]
     pub extra_kv: HashMap<String, serde_json::Value>,
+    /// Names of binary targets to include.  This list is empty by default,
+    /// which means that the default generated `BUILD.gn` will only cover
+    /// the library target (if any) of the package.
+    #[serde(default)]
+    pub bin_targets: Vec<String>,
 
     // Third-party crate settings.
     #[serde(default)]
@@ -111,4 +130,36 @@ pub struct CrateConfig {
     pub license: Option<String>,
     #[serde(default)]
     pub license_files: Vec<String>,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_get_combined_set_with_global_and_per_crate_entry() {
+        let all_config =
+            CrateConfig { bin_targets: vec!["foo".to_string()], ..CrateConfig::default() };
+        let crate_config =
+            CrateConfig { bin_targets: vec!["bar".to_string()], ..CrateConfig::default() };
+        let build_config = BuildConfig {
+            all_config,
+            per_crate_config: [("some_crate".to_string(), crate_config)].into_iter().collect(),
+            ..BuildConfig::default()
+        };
+        let combined_set = build_config.get_combined_set("some_crate", |cfg| &cfg.bin_targets);
+        assert_eq!(combined_set.len(), 2);
+        assert!(combined_set.contains("foo"));
+        assert!(combined_set.contains("bar"));
+    }
+
+    #[test]
+    fn test_get_combined_set_with_only_global_entry() {
+        let all_config =
+            CrateConfig { bin_targets: vec!["foo".to_string()], ..CrateConfig::default() };
+        let build_config = BuildConfig { all_config, ..BuildConfig::default() };
+        let combined_set = build_config.get_combined_set("some_crate", |cfg| &cfg.bin_targets);
+        assert_eq!(combined_set.len(), 1);
+        assert!(combined_set.contains("foo"));
+    }
 }
