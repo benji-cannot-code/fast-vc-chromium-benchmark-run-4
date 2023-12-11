5024ab591f6a433f58d1f696be5ff7ce66f38986
Roman Arora <romanarora@chromium.org>
2023-12-11 23:11:24 +0000

    [ntp-modules] Add new HaTS trigger scenarios
    Creates four new HaTS survey launch scenarios based on a user's
    interactions (or lack of) with the Desktop NTP modules.
    
    Adds a new `OnModuleInteraction` function that is called by the WebUI
    whenever a user disables, dismisses, or uses a module (Clicking on a
    module's `more actions` button is treated as a usage interaction). The
    handler function looks for an associated trigger id for the given
    interaction type and module id, and if there is one attempts to trigger
    the associated HaTS survey.
    
    Adds logic that tracks how many times specific modules have been loaded
    and how many times a user has interacted with a particular module in
    order to determine if a module can be considered as ignored by the user
    and trigger any associated HaTS survey.
    
    Proposal: https://docs.google.com/document/d/1EcwjK1Ke1DAbf75AXXg4EqZ8bRpZJ7KAhfPGUclVcCY/edit?resourcekey=0-yhvVc1gSBDxaYESsSfOQig#heading=h.2j10m26yl7ww
    
    Bug: 1497510
    Change-Id: I864e7213b77ab8dd507e16b82d606e627d34c7db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5010043
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Auto-Submit: Roman Arora <romanarora@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236018}

--- chrome/browser/resources/new_tab_page/modules/v2/modules.ts
+++ chrome/browser/resources/new_tab_page/modules/v2/modules.ts
@@ -201,13 +201,6 @@ export class ModulesV2Element extends AppElementBase {
         this.maxColumnCount_ * SUPPORTED_MODULE_WIDTHS[0].value +
         (this.maxColumnCount_ - 1) * CONTAINER_GAP_WIDTH;
     this.loadModules_();
-
-    const onModuleUse = (e: Event) => {
-      e.stopPropagation();
-      NewTabPageProxy.getInstance().handler.onModulesUsed();
-    };
-    this.addEventListener('usage', onModuleUse, {once: true});
-    this.addEventListener('menu-button-click', onModuleUse, {once: true});
   }
 
   private moduleDisabled_(
@@ -432,9 +425,8 @@ export class ModulesV2Element extends AppElementBase {
     // Notify the user.
     this.$.undoToast.show();
 
-    recordOccurrence('NewTabPage.Modules.Dismissed');
-    recordOccurrence(
-        `NewTabPage.Modules.Dismissed.${wrapper.module.descriptor.id}`);
+    NewTabPageProxy.getInstance().handler.onDismissModule(
+        wrapper.module.descriptor.id);
   }
 
   private onUndoButtonClick_() {
--- chrome/test/data/webui/new_tab_page/modules/module_wrapper_test.ts
+++ chrome/test/data/webui/new_tab_page/modules/module_wrapper_test.ts
@@ -4,7 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {ModuleDescriptor, ModuleWrapperElement} from 'chrome://new-tab-page/lazy_load.js';
-import {WindowProxy} from 'chrome://new-tab-page/new_tab_page.js';
+import {NewTabPageProxy, WindowProxy} from 'chrome://new-tab-page/new_tab_page.js';
+import {PageCallbackRouter, PageHandlerRemote} from 'chrome://new-tab-page/new_tab_page.mojom-webui.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {assertDeepEquals, assertEquals, assertThrows} from 'chrome://webui-test/chai_assert.js';
 import {fakeMetricsPrivate, MetricsTracker} from 'chrome://webui-test/metrics_test_support.js';
@@ -14,6 +15,7 @@ import {eventToPromise} from 'chrome://webui-test/test_util.js';
 import {createElement, initNullModule, installMock} from '../test_support.js';
 
 suite('NewTabPageModulesModuleWrapperTest', () => {
+  let handler: TestMock<PageHandlerRemote>;
   let moduleWrapper: ModuleWrapperElement;
   let metrics: MetricsTracker;
   let windowProxy: TestMock<WindowProxy>;
@@ -23,6 +25,10 @@ suite('NewTabPageModulesModuleWrapperTest', () => {
     loadTimeData.overrideValues({
       navigationStartTime: 0.0,
     });
+    handler = installMock(
+        PageHandlerRemote,
+        (mock: PageHandlerRemote) =>
+            NewTabPageProxy.setInstance(mock, new PageCallbackRouter()));
     metrics = fakeMetricsPrivate();
     windowProxy = installMock(WindowProxy);
     moduleWrapper = new ModuleWrapperElement();
@@ -77,11 +83,36 @@ suite('NewTabPageModulesModuleWrapperTest', () => {
     };
 
     // Act.
-    moduleElement.dispatchEvent(new Event('usage'));
+    moduleElement.dispatchEvent(new Event('usage', {bubbles: true}));
 
     // Assert.
     assertEquals(1, metrics.count('NewTabPage.Modules.Usage'));
     assertEquals(1, metrics.count('NewTabPage.Modules.Usage.foo'));
+  });
+
+  suite('Redesigned modules', () => {
+    suiteSetup(() => {
+      loadTimeData.overrideValues({modulesRedesignedEnabled: true});
+    });
+
+    ['usage', 'menu-button-click'].forEach((eventName: string) => {
+      test(
+          `module ${eventName} event triggers onModuleUsed
+              function`,
+          () => {
+            const moduleId = 'foo';
+            const moduleElement = createElement();
+            moduleWrapper.module = {
+              descriptor: new ModuleDescriptor(moduleId, initNullModule),
+              element: moduleElement,
+            };
+
+            moduleElement.dispatchEvent(new Event(eventName, {bubbles: true}));
+
+            assertEquals(1, handler.getCallCount('onModuleUsed'));
+            assertEquals(moduleId, handler.getArgs('onModuleUsed')[0]);
+          });
+    });
   });
 
   test('clicking info button records click and module id', () => {
