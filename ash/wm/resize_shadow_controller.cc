8703eeba9e1016c2556b2e3734bc347d08306351
Zoraiz Naeem <zoraiznaeem@chromium.org>
2023-12-11 17:52:41 +0000

    [Nit] Removed unused imports
    Bug: b:314329290
    Change-Id: Iffcd8b07f0d70f175ff5bb8a61e9198c1903ef6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5108244
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235808}

--- ash/wm/resize_shadow_controller.cc
+++ ash/wm/resize_shadow_controller.cc
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/wm/overview/overview_controller.h"
 #include "ash/wm/resize_shadow.h"
-#include "base/debug/stack_trace.h"
-#include "base/logging.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "chromeos/ui/base/window_state_type.h"
 #include "chromeos/ui/frame/frame_utils.h"
