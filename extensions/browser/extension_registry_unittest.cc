c3ad2f0ca109b2c70b9edaaacd0613ec7bd7c0ba
EmiliaPaz <emiliapaz@chromium.org>
2023-12-11 18:41:08 +0000

    [Extensions cleanup] Update GetByID() for terminated extensions
    Use registry->terminated_extensions().GetByID() instead of
    registry->GetExtensionById(extension_id, ExtensionRegistry::TERMINATED).
    
    Bug: 1505907
    Change-Id: I6b51380618b3ab27a4c3f82000c6d8c1a3364b03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5078666
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235847}

--- extensions/browser/extension_registry_unittest.cc
+++ extensions/browser/extension_registry_unittest.cc
@@ -184,8 +184,7 @@ TEST_F(ExtensionRegistryTest, GetExtensionById) {
       registry.GetExtensionById(enabled->id(), ExtensionRegistry::EVERYTHING));
   EXPECT_TRUE(registry.enabled_extensions().GetByID(enabled->id()));
   EXPECT_FALSE(registry.disabled_extensions().GetByID(enabled->id()));
-  EXPECT_FALSE(
-      registry.GetExtensionById(enabled->id(), ExtensionRegistry::TERMINATED));
+  EXPECT_FALSE(registry.terminated_extensions().GetByID(enabled->id()));
   EXPECT_FALSE(
       registry.GetExtensionById(enabled->id(), ExtensionRegistry::BLOCKLISTED));
 
@@ -194,8 +193,7 @@ TEST_F(ExtensionRegistryTest, GetExtensionById) {
       registry.GetExtensionById(disabled->id(), ExtensionRegistry::EVERYTHING));
   EXPECT_FALSE(registry.enabled_extensions().GetByID(disabled->id()));
   EXPECT_TRUE(registry.disabled_extensions().GetByID(disabled->id()));
-  EXPECT_FALSE(
-      registry.GetExtensionById(disabled->id(), ExtensionRegistry::TERMINATED));
+  EXPECT_FALSE(registry.terminated_extensions().GetByID(disabled->id()));
   EXPECT_FALSE(registry.GetExtensionById(disabled->id(),
                                          ExtensionRegistry::BLOCKLISTED));
 
@@ -204,8 +202,7 @@ TEST_F(ExtensionRegistryTest, GetExtensionById) {
                                         ExtensionRegistry::EVERYTHING));
   EXPECT_FALSE(registry.enabled_extensions().GetByID(terminated->id()));
   EXPECT_FALSE(registry.disabled_extensions().GetByID(terminated->id()));
-  EXPECT_TRUE(registry.GetExtensionById(terminated->id(),
-                                        ExtensionRegistry::TERMINATED));
+  EXPECT_TRUE(registry.terminated_extensions().GetByID(terminated->id()));
   EXPECT_FALSE(registry.GetExtensionById(terminated->id(),
                                          ExtensionRegistry::BLOCKLISTED));
 
@@ -214,8 +211,7 @@ TEST_F(ExtensionRegistryTest, GetExtensionById) {
                                         ExtensionRegistry::EVERYTHING));
   EXPECT_FALSE(registry.enabled_extensions().GetByID(blocklisted->id()));
   EXPECT_FALSE(registry.disabled_extensions().GetByID(blocklisted->id()));
-  EXPECT_FALSE(registry.GetExtensionById(blocklisted->id(),
-                                         ExtensionRegistry::TERMINATED));
+  EXPECT_FALSE(registry.terminated_extensions().GetByID(blocklisted->id()));
   EXPECT_TRUE(registry.GetExtensionById(blocklisted->id(),
                                         ExtensionRegistry::BLOCKLISTED));
 
