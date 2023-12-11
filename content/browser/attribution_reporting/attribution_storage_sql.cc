7a51d7bbf94426b0951195e35820d3ede63c4d9b
Andrew Paseltiner <apaseltiner@chromium.org>
2023-12-11 17:42:40 +0000

    Exclude expired sources from sources-per-source-origin limit
    Per steps 5-7 of
    https://wicg.github.io/attribution-reporting-api/#ref-for-max-pending-sources-per-source-origin%E2%91%A0
    expired sources should not be considered for this limit, but expired
    sources are only actually deleted/deactivated at a particular frequency,
    and only when there is no pending report for the source, so under
    certain circumstances such sources would still be counted for the
    purposes of this limit, violating the specification.
    
    Bug: 1510433
    Change-Id: I718c0f8d15d8f0f0de6b6f9abf1b144e1a19e6fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5110899
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235800}

--- content/browser/attribution_reporting/attribution_storage_sql.cc
+++ content/browser/attribution_reporting/attribution_storage_sql.cc
@@ -527,7 +527,7 @@ StoreSourceResult AttributionStorageSql::StoreSource(
 
   const std::string serialized_source_origin =
       common_info.source_origin().Serialize();
-  if (!HasCapacityForStoringSource(serialized_source_origin)) {
+  if (!HasCapacityForStoringSource(serialized_source_origin, source_time)) {
     if (int64_t file_size = StorageFileSizeKB(path_to_database_);
         file_size > -1) {
       base::UmaHistogramCounts10M(
@@ -1985,11 +1985,13 @@ void AttributionStorageSql::ClearAllDataAllTime(bool delete_rate_limit_data) {
 }
 
 bool AttributionStorageSql::HasCapacityForStoringSource(
-    const std::string& serialized_origin) {
+    const std::string& serialized_origin,
+    const base::Time now) {
   sql::Statement statement(db_.GetCachedStatement(
       SQL_FROM_HERE,
       attribution_queries::kCountActiveSourcesFromSourceOriginSql));
   statement.BindString(0, serialized_origin);
+  statement.BindTime(1, now);
   if (!statement.Step()) {
     return false;
   }
