1080598b739d3603d6893a88a72dd43dca6e1d50
David Bienvenu <davidbienvenu@chromium.org>
2023-12-11 17:29:12 +0000

    Stop ExtensionPolicyTest from creating shortcut in start menu
    Use ScopedPathOverride(base::DIR_START_MENU) to prevent
    ExtensionPolicyTest.ExtensionInstallBlocklistSharedModules creating a
    shortcut in the Windows start menu.
    
    Bug: 1405159
    Change-Id: I9bec1141a58c263b071aba9f26f818c694954544
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5105274
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235794}

--- chrome/browser/policy/extension_policy_browsertest.cc
+++ chrome/browser/policy/extension_policy_browsertest.cc
@@ -89,6 +89,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/switches.h"
 
 #if BUILDFLAG(IS_WIN)
+#include "base/base_paths_win.h"
 #include "base/win/win_util.h"
 #endif
 
@@ -360,6 +361,14 @@ class ExtensionPolicyTest : public ExtensionPolicyTestBase {
 
  private:
   web_app::OsIntegrationManager::ScopedSuppressForTesting os_hooks_suppress_;
+#if BUILDFLAG(IS_WIN)
+  // This is needed to stop
+  // ExtensionPolicyTest.ExtensionInstallBlocklistSharedModules from creating a
+  // shortcut in the Windows start menu. The override needs to last until the
+  // test is destroyed, because Windows shortcut tasks which create the shortcut
+  // can run after the test body returns.
+  base::ScopedPathOverride override_start_dir{base::DIR_START_MENU};
+#endif  // BUILDFLAG(IS_WIN
 };
 
 // Allows tests to wait for renderer process creation.
@@ -2486,7 +2495,7 @@ class ExtensionPolicyTest2Contexts : public PolicyTest {
  protected:
   void SetTabSpecificPermissionsForURL(const extensions::Extension* extension,
                                        int tab_id,
-                                       GURL& url,
+                                       const GURL& url,
                                        int url_scheme) {
     extensions::URLPatternSet new_hosts;
     new_hosts.AddOrigin(url_scheme, url);
