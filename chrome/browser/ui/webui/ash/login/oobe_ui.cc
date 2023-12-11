4988b40ea1edc268554b70dfc0c06e30bfb565bb
Renato Silva <rrsilva@google.com>
2023-12-11 16:30:06 +0000

    [OOBE] Make QuickStart a common screen
    QuickStart will also be used for the Add Person flow in the near
    future. Move the related files to the common scrrens folder.
    
    Bug: b/313036925, b/313405566
    Change-Id: Idfba757861cff4744c42b263141bc37dae283335
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5079472
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1235770}

--- chrome/browser/ui/webui/ash/login/oobe_ui.cc
+++ chrome/browser/ui/webui/ash/login/oobe_ui.cc
@@ -429,8 +429,9 @@ void OobeUI::ConfigureOobeDisplay() {
 
     AddScreenHandler(std::make_unique<DemoPreferencesScreenHandler>());
 
-    AddScreenHandler(std::make_unique<QuickStartScreenHandler>());
   }
+
+  AddScreenHandler(std::make_unique<QuickStartScreenHandler>());
 
   AddScreenHandler(std::make_unique<NetworkScreenHandler>());
 
