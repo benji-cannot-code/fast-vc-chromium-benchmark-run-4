8b9c5111dfefb2cb9702e96722c1e0a75faa0f03
Mohamed Amir Yosef <mamir@chromium.org>
2023-12-11 16:17:56 +0000

    [Passwords] Invoke CleanUpFillerAndReportOutcome() when filling
    CleanUpFillerAndReportOutcome() has two responsibilities:
    1- Report PasswordManager.TouchToFill.Outcome
    2- Do the cleanup and deleting the filler.
    
    This is called in all flows where the touch-to-fill UI is dismissed
    except after filling credentials.
    
    TouchToFillControllerAutofillDelegate::FillCredential() does report
    metrics already, and it seems plausiable that it should also clear the
    filler state since the UI is dismissed afterward.
    
    This CL is calling CleanUpFillerAndReportOutcome() also in the
    FillCredential() method.
    
    This is necessary since more clean-up will be added in follow-up CLs
    and it helps to have it in one place in the code.
    
    Bug: 1504098
    Change-Id: I2746532713cc173aa45edea420df492ab08bbf17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5110799
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1235764}

--- chrome/browser/touch_to_fill/password_manager/touch_to_fill_controller_autofill_delegate.cc
+++ chrome/browser/touch_to_fill/password_manager/touch_to_fill_controller_autofill_delegate.cc
@@ -275,8 +275,8 @@ void TouchToFillControllerAutofillDelegate::FillCredential(
         credential.username());
   }
 
-  base::UmaHistogramEnumeration("PasswordManager.TouchToFill.Outcome",
-                                TouchToFillOutcome::kCredentialFilled);
+  CleanUpFillerAndReportOutcome(TouchToFillOutcome::kCredentialFilled,
+                                /*show_virtual_keyboard=*/false);
   std::move(action_complete_).Run();
 }
 
