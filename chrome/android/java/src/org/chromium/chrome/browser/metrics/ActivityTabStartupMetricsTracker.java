9730d8d7c572f576c774a4ee3d6f359c67f650a8
Harkiran Bolaria <hbolaria@google.com>
2023-12-11 16:36:01 +0000

    [CUI] Add instant event for launch start and SQL data model for startups.
    Add launch start events, as launches are not guaranteed to
    go to content, and we would like to include metrics in this
    case.
    
    Add SQL data model to associate chrome launch start, launch
    to first content, and launch cause. This data model follows the
    structure of the page_loads.sql code, and is used to populate the
    Perfetto CUI plugin tracks per aosp/2828891.
    
    See example trace:
    https://ui.perfetto.dev/#!/?s=ee516c424f644b9066f34ce929ebd5ca5cd79c4ccd3ab195ce4d6ff8e1cb5917
    
    See screenshot of Perfetto plugin:
    https://screenshot.googleplex.com/82Wpy3D9zSrTQB2.
    
    Bug: b/307730037
    Change-Id: Ib3e3e08eb619479b8a057187431557f565bd48f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5023900
    Commit-Queue: Harkiran Bolaria <hbolaria@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1235777}

--- chrome/android/java/src/org/chromium/chrome/browser/metrics/ActivityTabStartupMetricsTracker.java
+++ chrome/android/java/src/org/chromium/chrome/browser/metrics/ActivityTabStartupMetricsTracker.java
@@ -106,6 +106,7 @@ public class ActivityTabStartupMetricsTracker {
             long activityId, ObservableSupplier<TabModelSelector> tabModelSelectorSupplier) {
         mActivityId = activityId;
         mActivityStartTimeMs = SystemClock.uptimeMillis();
+        TraceEvent.startupActivityStart(mActivityId, mActivityStartTimeMs);
         tabModelSelectorSupplier.addObserver(this::registerObservers);
         SafeBrowsingApiBridge.setOneTimeSafetyNetApiUrlCheckObserver(
                 this::updateSafetyNetCheckTime);
