1f9551d6f712da3dafc46be18f37be1542f5b47f
Thuong Phan <thuongphan@chromium.org>
2023-12-11 18:40:02 +0000

    p13n: make Delete Wallpaper button work and refetch Sea Pen images
    Make Delete Wallpaper button work by deleting the selected Sea Pen image
    from disk. Also refetch the Sea Pen images after adding/deleting file to
    reflect the change immediately.
    Demo: http://go/scrcast/NjA3MDE3MzE0NzU5NDc1MnxhNzlmYzE0NS1lMg
    
    BUG=b/314374481
    
    Test: ash_unittests --gtest_filter='*SeaPen*'
    Test: browser_tests --gtest_filter='*SeaPen*'
    Change-Id: Ic4a64d58ad26075937ffb947b5ea078a22078fa8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5080898
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Commit-Queue: Thuong Phan <thuongphan@chromium.org>
    Reviewed-by: Jerry Liu <pzliu@google.com>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235846}

--- ash/wallpaper/wallpaper_controller_impl.h
+++ ash/wallpaper/wallpaper_controller_impl.h
@@ -306,6 +306,11 @@ class ASH_EXPORT WallpaperControllerImpl
                                   const base::FilePath& file_path,
                                   SetWallpaperCallback callback) override;
 
+  void DeleteRecentSeaPenImage(
+      const AccountId& account_id,
+      const base::FilePath& file_path,
+      DeleteRecentSeaPenImageCallback callback) override;
+
   void ConfirmPreviewWallpaper() override;
   void CancelPreviewWallpaper() override;
   void UpdateCurrentWallpaperLayout(const AccountId& account_id,
--- chrome/browser/ui/ash/test_wallpaper_controller.cc
+++ chrome/browser/ui/ash/test_wallpaper_controller.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/wallpaper/wallpaper_types.h"
 #include "ash/wallpaper/wallpaper_drag_drop_delegate.h"
 #include "base/containers/adapters.h"
+#include "base/files/file_util.h"
 #include "base/notreached.h"
 #include "base/ranges/algorithm.h"
 #include "base/task/sequenced_task_runner.h"
@@ -232,6 +233,18 @@ void TestWallpaperController::SetSeaPenWallpaperFromFile(
   wallpaper_info_ = ash::WallpaperInfo();
   wallpaper_info_->type = ash::WallpaperType::kSeaPen;
   std::move(callback).Run(/*success=*/true);
+}
+
+void TestWallpaperController::DeleteRecentSeaPenImage(
+    const AccountId& account_id,
+    const base::FilePath& sea_pen_file_path,
+    DeleteRecentSeaPenImageCallback callback) {
+  if (base::PathExists(sea_pen_file_path) &&
+      base::DeleteFile(sea_pen_file_path)) {
+    std::move(callback).Run(/*success=*/true);
+    return;
+  }
+  std::move(callback).Run(/*success=*/false);
 }
 
 void TestWallpaperController::ConfirmPreviewWallpaper() {
