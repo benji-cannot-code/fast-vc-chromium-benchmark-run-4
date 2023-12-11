ba685e28a3ca7073f0e7ca6a3938811c407885f4
Alex Ilin <alexilin@chromium.org>
2023-12-11 15:34:28 +0000

    [gardener] Disable failing TabGridIncognitoReauthPromoTests
    Bug: 1510419
    Change-Id: I36723755db0c043a8ea7a37c01b90ba0fc2f9f75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5110283
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235742}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridIncognitoReauthPromoTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridIncognitoReauthPromoTest.java
@@ -32,6 +32,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -74,6 +75,7 @@ public class TabGridIncognitoReauthPromoTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1510419")
     public void testIncognitoReauthPromoShown() {
         final ChromeTabbedActivity cta = mActivityTestRule.getActivity();
 
@@ -87,6 +89,7 @@ public class TabGridIncognitoReauthPromoTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1510419")
     public void testSnackBarShown_WhenClickingReviewActionProvider() {
         final ChromeTabbedActivity cta = mActivityTestRule.getActivity();
 
@@ -134,6 +137,7 @@ public class TabGridIncognitoReauthPromoTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1510419")
     public void testIncognitoReauthPromo_NoThanks_HidesTheCard() {
         final ChromeTabbedActivity cta = mActivityTestRule.getActivity();
 
