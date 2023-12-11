1f2cebf51c0966b6caf1a3f8e39439a198f43964
Vipul Vinod Koul <koulvipul@google.com>
2023-12-11 23:24:32 +0000

    [CVV Storage] Adding metrics for delete operation of a local card
    When the local card is deleted via the settings page, log the same and
    also log the optional data (CVC and Nickname) if present.
    
    Metrics tracker- https://docs.google.com/spreadsheets/d/11sg2txJ6Ut3XWu23Gr6sndr0nNPVRLv8bpqDKaUXZDI/edit#gid=1838911229&range=133:133
    
    Bug: 1505237
    Change-Id: Id020a90b0023b6a6fd45af57fbb9bf229c690ec4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5098396
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Commit-Queue: Vipul Koul <koulvipul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1236024}

--- chrome/test/data/extensions/api_test/autofill_private/test.js
+++ chrome/test/data/extensions/api_test/autofill_private/test.js
@@ -25,7 +25,8 @@ var NUMBER = '4111 1111 1111 1111';
 var EXP_MONTH = '02';
 var EXP_YEAR = '2999';
 var CVC = '987';
-MASKED_CVC = '•••';
+var MASKED_CVC = '•••';
+var NICKNAME = 'nickname';
 var IBAN_VALUE = 'AD1400080001001234567890';
 var INVALID_IBAN_VALUE = 'AD14000800010012345678900';
 
@@ -398,7 +399,7 @@ var availableTests = [
                       cardNumber: MASKED_NUMBER,
                       expirationMonth: EXP_MONTH,
                       expirationYear: EXP_YEAR,
-                      nickname: undefined,
+                      nickname: NICKNAME,
                       cvc: MASKED_CVC
                     }],
                     filterCardProperties(cardList));
@@ -409,6 +410,7 @@ var availableTests = [
             cardNumber: NUMBER,
             expirationMonth: EXP_MONTH,
             expirationYear: EXP_YEAR,
+            nickname: NICKNAME,
             cvc: CVC
           });
         }));
@@ -608,6 +610,26 @@ var availableTests = [
     }));
   },
 
+  function removeExistingCard() {
+    chrome.autofillPrivate.getCreditCardList(chrome.test.callbackPass(function(
+        cardList) {
+      // The card from the addNewCreditCard function should still be there.
+      chrome.test.assertEq(1, cardList.length);
+      var cardGuid = cardList[0].guid;
+
+      // Set up the callback that verifies that the card was correctly
+      // deleted.
+      chrome.test.listenOnce(
+          chrome.autofillPrivate.onPersonalDataChanged,
+          chrome.test.callbackPass(function(addressList, cardList, ibanList) {
+            chrome.test.assertEq(0, cardList.length);
+          }));
+
+      // Remove the card with the given guid.
+      chrome.autofillPrivate.removeEntry(cardGuid);
+    }));
+  },
+
   function removeEntry() {
     var guid;
 
@@ -724,6 +746,9 @@ var TESTS_FOR_CONFIG = {
   'updateExistingIbanWithNickname':
       ['addNewIbanNoNickname', 'updateExistingIban_WithNickname'],
   'removeExistingIban': ['addNewIbanNoNickname', 'removeExistingIban'],
+  'removeExistingCard': ['addNewCreditCardWithoutCvc', 'removeExistingCard'],
+  'removeExistingCard_WithCvcAndNickname':
+      ['addNewCreditCard', 'removeExistingCard'],
   'isValidIban': ['isValidIban'],
   'authenticateUserAndFlipMandatoryAuthToggle':
       ['authenticateUserAndFlipMandatoryAuthToggle'],
