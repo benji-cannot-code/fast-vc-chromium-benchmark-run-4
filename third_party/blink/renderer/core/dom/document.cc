afa078b43eb253f6e5519c1552734e6c817f5523
Fredrik Söderquist <fs@opera.com>
2023-12-11 19:02:48 +0000

    Add better DCHECK for incorrect ScheduleSVGResourceInvalidation() calls
    Try to catch these as they happen (i.e with a more relevant stack)
    rather than later on.
    
    Bug: 1509918
    Change-Id: Ie1f17c0850193b44cc2afe0261120b83fdc1eafc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5110419
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1235860}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -2872,6 +2872,7 @@ void Document::UpdateUseShadowTreesIfNeeded() {
 }
 
 void Document::ScheduleSVGResourceInvalidation(LocalSVGResource& resource) {
+  DCHECK(InStyleRecalc() || GetStyleEngine().InDetachLayoutTree());
   svg_resources_needing_invalidation_.insert(&resource);
 }
 
