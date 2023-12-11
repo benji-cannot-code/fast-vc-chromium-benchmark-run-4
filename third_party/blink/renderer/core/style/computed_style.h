bb3e99ed2fabe5001aa6094879c9b4a7be9836f9
Kent Tamura <tkent@chromium.org>
2023-12-11 20:46:57 +0000

    align-content for blocks: Should not work with ruby-text
    Bug: 1500511
    Change-Id: I916a60c0769e746c75f073d6335ba15057a1c6d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5109116
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235923}

--- third_party/blink/renderer/core/style/computed_style.h
+++ third_party/blink/renderer/core/style/computed_style.h
@@ -2430,6 +2430,7 @@ class ComputedStyle final : public ComputedStyleBase {
     return display == EDisplay::kBlock || display == EDisplay::kListItem ||
            display == EDisplay::kInlineBlock ||
            display == EDisplay::kFlowRoot ||
+           display == EDisplay::kFlowRootListItem ||
            display == EDisplay::kInlineFlowRootListItem ||
            display == EDisplay::kTableCell ||
            display == EDisplay::kTableCaption;
