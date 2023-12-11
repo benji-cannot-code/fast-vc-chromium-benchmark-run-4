ef34ec174903b0030a03dc48575ea629f7a17129
Louise Brett <loubrett@google.com>
2023-12-11 03:28:35 +0000

    Show the install dialog from the GIOC flow.
    This CL does 2 things:
    1. Replaces ChromeOsAppInstallDialogParams with mojom::DialogArgsPtr
       for passing app data to the dialog.
    2. Shows the install dialog from the GIOC flow.
    
    The actual install from the GIOC flow will happen in a later CL.
    
    Bug: 1488697, b/302413312
    Change-Id: I31a66d7233aa29f27bd8e79e08aa8e2d14fa7519
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5083000
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Commit-Queue: Louise Brett <loubrett@google.com>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235572}

--- chrome/browser/ui/webui/ash/app_install/app_install_dialog.h
+++ chrome/browser/ui/webui/ash/app_install/app_install_dialog.h
@@ -9,25 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/app_install/app_install.mojom.h"
 #include "chrome/browser/ui/webui/ash/app_install/app_install_ui.h"
 #include "chrome/browser/ui/webui/ash/system_web_dialog_delegate.h"
-// TODO(b/308717267): Remove this dependency when moving to mojom struct
-// definition for Lacros support.
-#include "chrome/browser/web_applications/web_app_install_info.h"
-#include "components/webapps/browser/installable/installable_data.h"
 
 namespace ash::app_install {
 
-struct ChromeOsAppInstallDialogParams {
-  GURL icon_url;
-  std::string name;
-  GURL url;
-  std::string description;
-  std::vector<webapps::Screenshot> screenshots;
-
-  ChromeOsAppInstallDialogParams(const web_app::WebAppInstallInfo& web_app_info,
-                                 std::vector<webapps::Screenshot> screenshots);
-  ~ChromeOsAppInstallDialogParams();
-  ChromeOsAppInstallDialogParams(ChromeOsAppInstallDialogParams&&);
-};
+const int kIconSize = 32;
 
 // Defines the web dialog used for installing an app.
 class AppInstallDialog : public SystemWebDialogDelegate {
@@ -40,7 +25,7 @@ class AppInstallDialog : public SystemWebDialogDelegate {
 
   // Displays the dialog.
   void Show(gfx::NativeWindow parent,
-            ChromeOsAppInstallDialogParams params,
+            mojom::DialogArgsPtr args,
             base::OnceCallback<void(bool accepted)> dialog_accepted_callback);
   // Callers must set whether the install was successful or not.
   void SetInstallSuccess(bool success);
