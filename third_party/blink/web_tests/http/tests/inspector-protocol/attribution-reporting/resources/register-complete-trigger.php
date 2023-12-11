cc49e31b1d2b16a8fbf4baa14377480d16255833
Thomas Quintanilla <tquintanilla@google.com>
2023-12-11 22:51:33 +0000

    Add DevTools event for Attribution Reporting trigger registrations
    The next step in integrating Attribution Reporting API in DevTools.
    
    Bug: 1494446
    Change-Id: If7f5b7359ee767f5c02ba78171e722fe602a4348
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5100203
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Thomas Quintanilla <tquintanilla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235997}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/attribution-reporting/resources/register-complete-trigger.php
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+header('Attribution-Reporting-Register-Trigger: {"aggregatable_source_registration_time":"exclude","aggregatable_deduplication_keys":[{"deduplication_key":"1"}],"aggregatable_trigger_data":[{"key_piece":"0x0"}],"aggregatable_values":{"a": 2},"debug_key":"3","debug_reporting":true,"event_trigger_data":[{"priority":"0","trigger_data":"0"}],"filters":[{"b":[]}],"not_filters":[{"c":[],"_lookback_window":2}],"trigger_context_id":"123"}');
+?>
