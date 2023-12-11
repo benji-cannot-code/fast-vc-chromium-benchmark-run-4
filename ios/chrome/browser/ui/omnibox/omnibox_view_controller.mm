7b262944d74a1a2550ad0d1edff95a9354f45015
Joemer Ramos <joemerramos@chromium.org>
2023-12-11 15:23:54 +0000

    [iOS] Omnibox API_AVAILABLE iOS 14 Guard Clean Up
    Clean up for iOS 14 API_AVAILABLE guards since the minimum deployment
    target is now 15.0.
    
    Bug: 1418068
    Change-Id: I0e0b08260b2fb6b5ad8001ee6662be8fa4aa7aaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029696
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235735}

--- ios/chrome/browser/ui/omnibox/omnibox_view_controller.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_view_controller.mm
@@ -749,8 +749,8 @@ const CGFloat kClearButtonImageSize = 17.0f;
 
 #pragma mark - UIScribbleInteractionDelegate
 
-- (void)scribbleInteractionWillBeginWriting:(UIScribbleInteraction*)interaction
-    API_AVAILABLE(ios(14.0)) {
+- (void)scribbleInteractionWillBeginWriting:
+    (UIScribbleInteraction*)interaction {
   if (self.textField.isPreEditing) {
     [self.textField exitPreEditState];
     [self.textField setText:[[NSAttributedString alloc] initWithString:@""]
@@ -760,8 +760,8 @@ const CGFloat kClearButtonImageSize = 17.0f;
   [self.textField clearAutocompleteText];
 }
 
-- (void)scribbleInteractionDidFinishWriting:(UIScribbleInteraction*)interaction
-    API_AVAILABLE(ios(14.0)) {
+- (void)scribbleInteractionDidFinishWriting:
+    (UIScribbleInteraction*)interaction {
   [self cleanupOmniboxAfterScribble];
 
   // Dismiss any inline autocomplete. The user expectation is to not have it.
