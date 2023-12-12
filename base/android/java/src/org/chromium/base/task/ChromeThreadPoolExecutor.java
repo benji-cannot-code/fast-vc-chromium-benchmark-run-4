1d3ee129e033469e82f42ba5ccc432a8937be1e5
Sam Maier <smaier@chromium.org>
2023-12-12 16:48:35 +0000

    Fixit: unbreak AsyncTaskTest on new Android SDKs
    Starting on API 31, this test is broken. I believe that the OS
    implementation of AsyncTask has changed, and we really don't care to
    deal with android.os.AsyncTask as it's been long purged from our
    codebase.
    
    Bug: 1510585
    Change-Id: Ic4fb3fd0d36319f7e8cc47212d46909d838aa00e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5113179
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236366}

--- base/android/java/src/org/chromium/base/task/ChromeThreadPoolExecutor.java
+++ base/android/java/src/org/chromium/base/task/ChromeThreadPoolExecutor.java
@@ -9,9 +9,6 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 
 import androidx.annotation.VisibleForTesting;
 
-import org.chromium.build.BuildConfig;
-
-import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -73,23 +70,10 @@ class ChromeThreadPoolExecutor extends ThreadPoolExecutor {
     @SuppressWarnings("NoAndroidAsyncTaskCheck")
     private static String getClassName(Runnable runnable) {
         Class blamedClass = runnable.getClass();
-        try {
-            if (blamedClass == AsyncTask.NamedFutureTask.class) {
+        if (blamedClass == AsyncTask.NamedFutureTask.class) {
                 blamedClass = ((AsyncTask.NamedFutureTask) runnable).getBlamedClass();
             } else if (blamedClass.getEnclosingClass() == android.os.AsyncTask.class) {
-                // This gets the AsyncTask that produced the runnable.
-                Field field = blamedClass.getDeclaredField("this$0");
-                field.setAccessible(true);
-                blamedClass = field.get(runnable).getClass();
-            }
-        } catch (NoSuchFieldException e) {
-            if (BuildConfig.ENABLE_ASSERTS) {
-                throw new RuntimeException(e);
-            }
-        } catch (IllegalAccessException e) {
-            if (BuildConfig.ENABLE_ASSERTS) {
-                throw new RuntimeException(e);
-            }
+            blamedClass = android.os.AsyncTask.class;
         }
         return blamedClass.getName();
     }
