139c65647c6bdc8563b5c378d0c3e11a1bb31c4f
Ian Wells <iwells@chromium.org>
2023-12-12 23:20:27 +0000

    Add feature flag checked after readability check returns true
    The new flag allows activating experiments after a readable page is
    visited. It is default-enabled because the feature is already controlled
    by the default-disabled ReadAloud flag.
    
    Bug: b/315998018
    Change-Id: I674c74c0b4892a8aff58c45d9347aa8dd449e55a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5117823
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Reviewed-by: Andrea Gomez <andreaxg@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1236651}

--- chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/ReadAloudControllerUnitTest.java
+++ chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/ReadAloudControllerUnitTest.java
@@ -64,6 +64,7 @@ import org.chromium.chrome.modules.readaloud.Player;
 import org.chromium.chrome.modules.readaloud.ReadAloudPlaybackHooks;
 import org.chromium.chrome.modules.readaloud.contentjs.Highlighter;
 import org.chromium.chrome.test.util.browser.Features;
+import org.chromium.chrome.test.util.browser.Features.DisableFeatures;
 import org.chromium.chrome.test.util.browser.Features.EnableFeatures;
 import org.chromium.chrome.test.util.browser.tabmodel.MockTabModelSelector;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
@@ -82,7 +83,7 @@ import java.util.List;
 /** Unit tests for {@link ReadAloudController}. */
 @RunWith(BaseRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
-@EnableFeatures(ChromeFeatureList.READALOUD)
+@EnableFeatures({ChromeFeatureList.READALOUD, ChromeFeatureList.READALOUD_PLAYBACK})
 public class ReadAloudControllerUnitTest {
     private static final GURL sTestGURL = JUnitTestGURLs.EXAMPLE_URL;
 
@@ -1102,6 +1103,17 @@ public class ReadAloudControllerUnitTest {
                 .getValue()
                 .onFailure(sTestGURL.getSpec(), new Throwable("Something went wrong"));
         histogram.assertExpected();
+    }
+
+    @Test
+    @DisableFeatures(ChromeFeatureList.READALOUD_PLAYBACK)
+    public void testReadAloudPlaybackFlagCheckedAfterReadability() {
+        mController.maybeCheckReadability(sTestGURL);
+        verify(mHooksImpl, times(1))
+                .isPageReadable(eq(sTestGURL.getSpec()), mCallbackCaptor.capture());
+        mCallbackCaptor.getValue().onSuccess(sTestGURL.getSpec(), true, false);
+
+        assertFalse(mController.isReadable(mTab));
     }
 
     private void onPlaybackSuccess(Playback playback) {
