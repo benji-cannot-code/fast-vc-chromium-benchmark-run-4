16fd6077ba9247373ac98efa011949015ce1bdad
Wenyu Fu <wenyufu@google.com>
2023-12-12 23:23:57 +0000

    [Toolbar] Do now allow capture during toolbar button animation
    Bug: 1508208, 1508996
    Change-Id: Ib737da2361073b17eaf8d37e403053ffe81f7aaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5116816
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236653}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/CaptureReadinessResult.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/CaptureReadinessResult.java
@@ -42,7 +42,7 @@ public class CaptureReadinessResult {
     /**
      * Reasons to block toolbar captures. Treat this list as append only and keep it in sync with
      * TopToolbarBlockCaptureReason in enums.xml, as well as the proto in chrome_track_event.proto.
-     **/
+     */
     @IntDef({
         TopToolbarBlockCaptureReason.UNKNOWN,
         TopToolbarBlockCaptureReason.TOOLBAR_OR_RESULT_NULL,
@@ -58,6 +58,7 @@ public class CaptureReadinessResult {
         TopToolbarBlockCaptureReason.COMPOSITOR_IN_MOTION,
         TopToolbarBlockCaptureReason.NTP_Y_TRANSLATION,
         TopToolbarBlockCaptureReason.FULLSCREEN,
+        TopToolbarBlockCaptureReason.TABLET_BUTTON_ANIMATION_IN_PROGRESS,
         TopToolbarBlockCaptureReason.NUM_ENTRIES
     })
     @Retention(RetentionPolicy.SOURCE)
@@ -76,7 +77,8 @@ public class CaptureReadinessResult {
         int COMPOSITOR_IN_MOTION = 11;
         int NTP_Y_TRANSLATION = 12;
         int FULLSCREEN = 13;
-        int NUM_ENTRIES = 14;
+        int TABLET_BUTTON_ANIMATION_IN_PROGRESS = 14;
+        int NUM_ENTRIES = 15;
     }
 
     public static CaptureReadinessResult readyForced() {
