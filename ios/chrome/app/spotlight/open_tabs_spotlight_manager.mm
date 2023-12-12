d2f8f94fd9d6cf08980b64baf454bddbc089ef79
Stepan Khapugin <stkhapugin@chromium.org>
2023-12-12 11:04:49 +0000

    [iOS][Spotlight] Use a single WSListObserverBridge for all web states.
    Instead of keeping a map of observers, just keep a single observer.
    
    Bug: none
    Change-Id: If90498cd3552c201612e6be1b67e55588e89d065
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5110042
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236241}

--- ios/chrome/app/spotlight/open_tabs_spotlight_manager.mm
+++ ios/chrome/app/spotlight/open_tabs_spotlight_manager.mm
@@ -59,10 +59,9 @@ const int kBatchSize = 100;
   std::map<web::WebStateID, GURL> _lastCommittedURLs;
   // Tracks the number of open tabs with this URL.
   std::map<GURL, NSUInteger> _knownURLCounts;
-  // Bridges that observe all web state lists in all non-incognito browsers.
+  // Bridge that observes all web state lists in all non-incognito browsers.
   // Used to keep track of closing tabs.
-  std::map<WebStateList*, std::unique_ptr<WebStateListObserverBridge>>
-      _webStateListObserverBridges;
+  std::unique_ptr<WebStateListObserverBridge> _webStateListObserverBridge;
 
   // At full reindex of a WebStateList, this queue is used for WebStates that
   // require indexing.
@@ -112,6 +111,8 @@ const int kBatchSize = 100;
         std::make_unique<BrowserListObserverBridge>(self);
     _webStateObserverBridge =
         std::make_unique<web::WebStateObserverBridge>(self);
+    _webStateListObserverBridge =
+        std::make_unique<WebStateListObserverBridge>(self);
     _browserList->AddObserver(_browserListObserverBridge.get());
     [self startObservingAllWebStates];
   }
@@ -163,9 +164,7 @@ const int kBatchSize = 100;
   WebStateList* webStateList = browser->GetWebStateList();
   [self addAllURLsFromWebStateList:webStateList];
 
-  _webStateListObserverBridges[webStateList] =
-      std::make_unique<WebStateListObserverBridge>(self);
-  webStateList->AddObserver(_webStateListObserverBridges[webStateList].get());
+  webStateList->AddObserver(_webStateListObserverBridge.get());
 }
 
 - (void)browserList:(const BrowserList*)browserList
@@ -174,11 +173,7 @@ const int kBatchSize = 100;
 
   [self removeAllURLsFromWebStateList:webStateList];
 
-  if (_webStateListObserverBridges[webStateList]) {
-    webStateList->RemoveObserver(
-        _webStateListObserverBridges[webStateList].get());
-    _webStateListObserverBridges.erase(webStateList);
-  }
+  webStateList->RemoveObserver(_webStateListObserverBridge.get());
 }
 
 - (void)browserListWillShutdown:(const BrowserList*)browserList {
@@ -215,9 +210,7 @@ const int kBatchSize = 100;
 - (void)webStateListDestroyed:(WebStateList*)webStateList {
   [self removeAllURLsFromWebStateList:webStateList];
 
-  webStateList->RemoveObserver(
-      _webStateListObserverBridges[webStateList].get());
-  _webStateListObserverBridges.erase(webStateList);
+  webStateList->RemoveObserver(_webStateListObserverBridge.get());
 }
 
 #pragma mark - CRWWebStateObserver
@@ -460,15 +453,12 @@ const int kBatchSize = 100;
       WebState* webState = webStateList->GetWebStateAt(i);
       webState->RemoveObserver(_webStateObserverBridge.get());
     }
+    webStateList->RemoveObserver(_webStateListObserverBridge.get());
   }
-  _webStateObserverBridge = std::make_unique<web::WebStateObserverBridge>(self);
 
-  // Stop observing all web state lists
-  for (auto it = _webStateListObserverBridges.begin();
-       it != _webStateListObserverBridges.end(); ++it) {
-    it->first->RemoveObserver(it->second.get());
-  }
-  _webStateListObserverBridges.clear();
+  _webStateObserverBridge = std::make_unique<web::WebStateObserverBridge>(self);
+  _webStateListObserverBridge =
+      std::make_unique<WebStateListObserverBridge>(self);
 }
 
 - (void)startObservingAllWebStateLists {
@@ -478,9 +468,7 @@ const int kBatchSize = 100;
 
   for (Browser* browser : _browserList->AllRegularBrowsers()) {
     WebStateList* webStateList = browser->GetWebStateList();
-    _webStateListObserverBridges[webStateList] =
-        std::make_unique<WebStateListObserverBridge>(self);
-    webStateList->AddObserver(_webStateListObserverBridges[webStateList].get());
+    webStateList->AddObserver(_webStateListObserverBridge.get());
   }
 }
 
