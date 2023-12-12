bbab030859deee4f1ccb5681157a4a085369ab11
Andrey Kosyakov <caseq@chromium.org>
2023-12-12 17:29:17 +0000

    Deflake inspector-protocol/fenced-frame/device-emulation-fenced-frame.https.js
    Runtime.evaluate is one of the commands that will not be re-issued after frame
    commits navigation, so it should not be used while frame is navigating.
    
    Bug: 1424817
    Change-Id: I876b5bdfce29f459a7ba37a6b27693caf0950b65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5113109
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236392}

--- third_party/blink/web_tests/http/tests/inspector-protocol/fenced-frame/device-emulation-fenced-frame.https.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/fenced-frame/device-emulation-fenced-frame.https.js
@@ -23,13 +23,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   let ffSession = session.createChild(sessionId);
   let ffdp = ffSession.protocol;
 
-  // Disable MockScreenOrientation.
-  await ffSession.evaluate('testRunner.disableMockScreenOrientation()');
-
   // Wait for FF to finish loading.
   await ffdp.Page.enable();
   ffdp.Page.setLifecycleEventsEnabled({enabled: true});
   await ffdp.Page.onceLifecycleEvent(event => event.params.name === 'load');
+
+  // Disable MockScreenOrientation.
+  await ffSession.evaluate('testRunner.disableMockScreenOrientation()');
 
   function dumpMetrics() {
     return {
