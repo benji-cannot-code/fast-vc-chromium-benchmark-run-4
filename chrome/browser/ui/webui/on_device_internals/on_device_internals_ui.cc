1c27e4429ed415d846505d26719d7038e424c7d2
Clark DuVall <cduvall@chromium.org>
2023-12-12 20:47:50 +0000

    Add better error handling to chrome://on-device-internals
    Bug: b/314399473
    Change-Id: I04f3dd4db39af08a0a197ade18b5a943103f6c57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5117960
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Auto-Submit: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236544}

--- chrome/browser/ui/webui/on_device_internals/on_device_internals_ui.cc
+++ chrome/browser/ui/webui/on_device_internals/on_device_internals_ui.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/service_process_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui_data_source.h"
+#include "mojo/public/cpp/bindings/callback_helpers.h"
 #include "services/on_device_model/public/cpp/model_assets.h"
 
 OnDeviceInternalsUI::OnDeviceInternalsUI(content::WebUI* web_ui)
@@ -60,13 +61,17 @@ OnDeviceInternalsUI::GetService() {
         content::ServiceProcessHost::Options()
             .WithDisplayName("On-Device Model Service")
             .Pass());
+    service_.reset_on_disconnect();
   }
   return *service_.get();
 }
 
 void OnDeviceInternalsUI::GetEstimatedPerformanceClass(
     GetEstimatedPerformanceClassCallback callback) {
-  GetService().GetEstimatedPerformanceClass(std::move(callback));
+  GetService().GetEstimatedPerformanceClass(
+      mojo::WrapCallbackWithDefaultInvokeIfNotRun(
+          std::move(callback),
+          on_device_model::mojom::PerformanceClass::kError));
 }
 
 void OnDeviceInternalsUI::OnModelAssetsLoaded(
