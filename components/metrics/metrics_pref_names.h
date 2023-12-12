335683086b8964cc4317ed0544320cb50a669206
Yulun Zeng <yulunz@chromium.org>
2023-12-12 22:31:12 +0000

    Generate and store a source value to randomize limited entropy field trials.
    During start-up, EntropyState will attempt to load the previously used source value from prefs, and if there is no such value, EntropyState will generate a new value from base::UnguessableToken, and store that to prefs for reuse. Note that this behavior will be true only after callers of these methods are added, which will happen in subsequent CLs.
    
    If the client has been started with –reset-variations-state, during start-up EntropyState will always generate a new source value and store it into prefs, regardless of whether a value is already available there. (As above, the start-up integration itself will be added in subsequent CLs.)
    
    If UMA opt-in status changes, the value from prefs will be cleared. And a new value will only be generated upon restart. This is implemented through EntropyState::ClearPrefs.
    
    There are no changes to the existing low or high entropy sources.
    
    There will be a follow-up CL to sync this source value between Ash and LaCrOS analogous to how the other entropy sources are currently sync'ed.
    
    Bug: b/315349895
    Change-Id: I11e2717eb77ef747034fbc2226433516473a94e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5105107
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Yulun Zeng <yulunz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236605}

--- components/metrics/metrics_pref_names.h
+++ components/metrics/metrics_pref_names.h
@@ -19,6 +19,7 @@ extern const char kMetricsFileMetricsMetadata[];
 extern const char kMetricsDefaultOptIn[];
 extern const char kMetricsInitialLogs[];
 extern const char kMetricsInitialLogsMetadata[];
+extern const char kMetricsLimitedEntropySource[];
 extern const char kMetricsLogRecordId[];
 extern const char kMetricsLowEntropySource[];
 extern const char kMetricsOldLowEntropySource[];
