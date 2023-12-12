be36e744ea633453a634229c87fffdfbd8468541
Hong Xu <xuhong@chromium.org>
2023-12-12 04:42:16 +0000

    Use a dedicated destination for crash events
    Due to the complexity on the server side, a dedicated destination is
    required.
    
    Bug: b:266018440
    Change-Id: I15e4523bbffb065379cd933fa3edfdd8132c72bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5112648
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Auto-Submit: Hong Xu <xuhong@google.com>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Commit-Queue: Weilun Shi <sweilun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236151}

--- chrome/browser/ash/policy/reporting/metrics_reporting/metric_reporting_manager_unittest.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/metric_reporting_manager_unittest.cc
@@ -357,9 +357,6 @@ class MetricReportingManagerTest
     event_queue_ptr_ = CreateMockMetricReportQueueHelper(
         mock_delegate_.get(), EventType::kDevice, Destination::EVENT_METRIC,
         Priority::SLOW_BATCH);
-    immediate_event_queue_ptr_ = CreateMockMetricReportQueueHelper(
-        mock_delegate_.get(), EventType::kDevice, Destination::EVENT_METRIC,
-        Priority::IMMEDIATE);
     user_telemetry_queue_ptr_ = CreateMockMetricReportQueueHelper(
         mock_delegate_.get(), EventType::kUser, Destination::TELEMETRY_METRIC,
         Priority::MANUAL_BATCH);
@@ -377,6 +374,9 @@ class MetricReportingManagerTest
         mock_delegate_.get(), EventType::kUser, Destination::EVENT_METRIC,
         Priority::SLOW_BATCH, metrics::kWebsiteEventsTotalSize,
         metrics::kWebsiteEventsWindow, metrics::kWebsiteEventsBucketCount);
+    crash_event_queue_ptr_ = CreateMockMetricReportQueueHelper(
+        mock_delegate_.get(), EventType::kDevice, Destination::CRASH_EVENTS,
+        Priority::IMMEDIATE);
 
     auto telemetry_queue = std::make_unique<test::FakeMetricReportQueue>();
     telemetry_queue_ptr_ = telemetry_queue.get();
@@ -404,8 +404,6 @@ class MetricReportingManagerTest
   raw_ptr<test::FakeMetricReportQueue, DanglingUntriaged | ExperimentalAsh>
       event_queue_ptr_;
   raw_ptr<test::FakeMetricReportQueue, DanglingUntriaged | ExperimentalAsh>
-      immediate_event_queue_ptr_;
-  raw_ptr<test::FakeMetricReportQueue, DanglingUntriaged | ExperimentalAsh>
       peripheral_queue_ptr_;
   raw_ptr<test::FakeMetricReportQueue, DanglingUntriaged | ExperimentalAsh>
       user_telemetry_queue_ptr_;
@@ -417,6 +415,8 @@ class MetricReportingManagerTest
       website_event_queue_ptr_;
   raw_ptr<test::FakeMetricReportQueue, DanglingUntriaged | ExperimentalAsh>
       heartbeat_queue_ptr_;
+  raw_ptr<test::FakeMetricReportQueue, DanglingUntriaged | ExperimentalAsh>
+      crash_event_queue_ptr_;
 
   std::unique_ptr<::testing::NiceMock<MockDelegate>> mock_delegate_;
 };
@@ -607,7 +607,7 @@ TEST_P(MetricReportingManagerEventTest, Default) {
   ON_CALL(
       *mock_delegate_ptr,
       CreateEventObserverManager(
-          _, immediate_event_queue_ptr_.get(), _,
+          _, crash_event_queue_ptr_.get(), _,
           test_case.setting_data.enable_setting_path,
           test_case.setting_data.setting_enabled_default_value, _, init_delay))
       .WillByDefault([&]() {
