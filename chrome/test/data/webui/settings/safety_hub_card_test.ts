97523fecd156cc1931dacd9fa1793292673fa7db
Side Yilmaz <sideyilmaz@chromium.org>
2023-12-12 18:33:20 +0000

    [SHv2] Small UI polishes on safety check page
    This CL updates:
    - section headers in Safety Check page
    - color of the separator line of the module
    - info state icon of the card
    
    Bug: 1510490, 1510464, 1510501, 1443466
    Change-Id: I2322e8bff77e947af89026c6bccf26d8315ef2e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5110027
    Reviewed-by: Tom Van Goethem <tov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236447}

--- chrome/test/data/webui/settings/safety_hub_card_test.ts
+++ chrome/test/data/webui/settings/safety_hub_card_test.ts
@@ -54,7 +54,7 @@ suite('SafetyHubCard', function() {
     // Check icon for INFO state.
     testElement.data = getMockDataForState(CardState.INFO);
     flushTasks();
-    assertEquals('cr:error', testElement.$.icon.icon);
+    assertEquals('cr:info', testElement.$.icon.icon);
     assertTrue(testElement.$.icon.classList.contains('grey'));
 
     // Check icon for WEAK state.
