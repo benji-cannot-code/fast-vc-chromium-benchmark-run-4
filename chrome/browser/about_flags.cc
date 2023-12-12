66ea21acc7161ac3d0a34ff0d76a1fb252765c12
Matt Jones <mdjones@chromium.org>
2023-12-12 18:26:36 +0000

    [fixit] Clean up simplified save flow flag
    The simplified bookmarks save flow has launched and we no longer need
    the flag.
    
    Bug: 1508625, b:315204761
    Change-Id: I5b7cf59e2a4d126a0555bd19cb7bea5164da88c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5111844
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236442}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -139,7 +139,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/performance_manager/public/features.h"
 #include "components/permissions/features.h"
 #include "components/policy/core/common/features.h"
-#include "components/power_bookmarks/core/flag_descriptions.h"
 #include "components/power_bookmarks/core/power_bookmark_features.h"
 #include "components/privacy_sandbox/privacy_sandbox_features.h"
 #include "components/query_tiles/switches.h"
@@ -10226,14 +10225,6 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kEnablePerDeskZOrderDescription, kOsCrOS,
      FEATURE_VALUE_TYPE(ash::features::kEnablePerDeskZOrder)},
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
-
-#if !BUILDFLAG(IS_ANDROID)
-    {"simplified-bookmark-save-flow",
-     power_bookmarks::flag_descriptions::kSimplifiedBookmarkSaveFlowName,
-     power_bookmarks::flag_descriptions::kSimplifiedBookmarkSaveFlowName,
-     kOsDesktop,
-     FEATURE_VALUE_TYPE(power_bookmarks::kSimplifiedBookmarkSaveFlow)},
-#endif
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
     {"gallery-app-pdf-edit-notification",
