9aad7465b04628bdeb044633c03b0cf2ab6827fd
Tim Sergeant <tsergeant@chromium.org>
2023-12-12 04:20:57 +0000

    App Service: Clean up AppUpdateWaiter and variants
    This changes AppUpdateWaiter to use the same implementation style as
    other Waiter classes in the same file, and then reimplements App
    specific specializations (e.g. AppReadinessWaiter) as subclasses of
    AppUpdateWaiter.
    
    This simplifies the implementation and ensures a consistent API for the
    different Waiter classes.
    
    Change-Id: I44c79115a886aa3749e43a7b12eb6bc7cb5adb49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5108725
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236146}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_chromeos_browsertest.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_chromeos_browsertest.cc
@@ -1366,7 +1366,7 @@ IN_PROC_BROWSER_TEST_F(PreventCloseBrowserNonClientFrameViewChromeOSTest,
           return update.AllowClose().has_value() && update.AllowClose().value();
         }));
     ClearWebAppSettings();
-    waiter.Wait();
+    waiter.Await();
   }
 
   {
