e63095c39b33bfdfdf8439e9f8da11f28ba111c0
Miguel Casas <mcasas@chromium.org>
2023-12-12 22:23:19 +0000

    media/gpu/v4l2: Fix timestamp conversion in TimeDeltaToTimeVal()
    Said function is used to create the timestamp for the enqueued
    OUTPUT queue buffer, timestamp that the driver propagates to the
    associated decoded video frame (if any). Current code has a
    rounding problem so two timestamps e.g. 1000us and 1001us end
    up being the same, making the Renderer client (DecoderStream)
    to think that the hardware decoder is broken. This CL fixes
    that by implementing the calculations inline.
    
    Bug: b:279980150
    Change-Id: I1e7428b9bbf29956264f23da9fe22b3449f0243d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5117633
    Reviewed-by: Hang Nguyen <hnt@chromium.org>
    Commit-Queue: Miguel Casas-Sanchez <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236592}

--- media/gpu/v4l2/v4l2_utils.cc
+++ media/gpu/v4l2/v4l2_utils.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/contains.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/ranges/algorithm.h"
 #include "build/build_config.h"
 #include "media/base/video_codecs.h"
@@ -476,7 +477,12 @@ base::TimeDelta TimeValToTimeDelta(const struct timeval& timeval) {
 }
 
 struct timeval TimeDeltaToTimeVal(base::TimeDelta time_delta) {
-  return base::Time::FromTimeSpec(time_delta.ToTimeSpec()).ToTimeVal();
+  const int64_t time_delta_linear = time_delta.InMicroseconds();
+  constexpr int64_t kMicrosecondsPerSecond = 1000 * 1000;
+  return {.tv_sec = base::checked_cast<__time_t>(time_delta_linear /
+                                                 kMicrosecondsPerSecond),
+          .tv_usec = base::checked_cast<__suseconds_t>(time_delta_linear %
+                                                       kMicrosecondsPerSecond)};
 }
 
 }  // namespace media
