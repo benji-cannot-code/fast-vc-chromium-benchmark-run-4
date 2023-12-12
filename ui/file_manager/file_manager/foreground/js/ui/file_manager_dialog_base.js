a2d0e7484663f2a277b0bb4b981ccf5045984f0a
Bo Majewski <majewski@chromium.org>
2023-12-12 05:48:47 +0000

    [Files,TS]: Converts dialogs.js to TypeScript.
    Bug: b:289003444, 1509537
    Change-Id: I9e9f6381f69e54fd1badc0da89ca2a51fe8eef46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5113819
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236170}

--- ui/file_manager/file_manager/foreground/js/ui/file_manager_dialog_base.js
+++ ui/file_manager/file_manager/foreground/js/ui/file_manager_dialog_base.js
@@ -14,7 +14,7 @@ import {BaseDialog} from './dialogs.js';
 // base class 'BaseDialog'.
 export class FileManagerDialogBase extends BaseDialog {
   /**
-   * @param {HTMLElement} parentNode Parent node of the dialog.
+   * @param {HTMLDialogElement} parentNode Parent node of the dialog.
    */
   constructor(parentNode) {
     super(parentNode);
@@ -29,7 +29,7 @@ export class FileManagerDialogBase extends BaseDialog {
    */
   initDom() {
     super.initDom();
-    super.hasModalContainer = true;
+    this.hasModalContainer = true;
   }
 
   /**
--- ui/file_manager/file_manager/foreground/js/ui/files_alert_dialog.js
+++ ui/file_manager/file_manager/foreground/js/ui/files_alert_dialog.js
@@ -14,7 +14,7 @@ import {AlertDialog} from './dialogs.js';
 // class 'AlertDialog'.
 export class FilesAlertDialog extends AlertDialog {
   /**
-   * @param {!HTMLElement} parentNode
+   * @param {!HTMLDialogElement} parentNode
    */
   constructor(parentNode) {
     super(parentNode);
@@ -29,7 +29,7 @@ export class FilesAlertDialog extends AlertDialog {
    */
   initDom() {
     super.initDom();
-    super.hasModalContainer = true;
+    this.hasModalContainer = true;
 
     // @ts-ignore: error TS2531: Object is possibly 'null'.
     this.frame.classList.add('files-alert-dialog');
