7d8bce97f6310bb659fae97283f85c9363873510
Guillem Perez <guiperez@google.com>
2023-12-12 21:21:20 +0000

    [iOS] Replaced a DCHECK to show notifications settings.
    Replaced a dcheck from the settings tab to make sure the notifications
    tab opens even when price tracking is not enabled.
    
    Change-Id: I330b7eb9a4bd4868284748a2a4c71b6c258c6e0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5116028
    Reviewed-by: Tina Wang <tinazwang@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Guillem Perez <guiperez@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Guillem Perez <guiperez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1236566}

--- ios/chrome/browser/ui/settings/settings_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/settings_table_view_controller.mm
@@ -1386,7 +1386,8 @@ UIImage* GetBrandedGoogleServicesSymbol() {
           [[AutofillProfileTableViewController alloc] initWithBrowser:_browser];
       break;
     case SettingsItemTypeNotifications:
-      DCHECK(IsPriceNotificationsEnabled());
+      DCHECK(IsPriceNotificationsEnabled() ||
+             IsContentPushNotificationsEnabled());
       [self showNotifications];
       break;
     case SettingsItemTypeVoiceSearch:
