e8423909e0684b663e32f411ad3c494908173d39
Maksim Moskvitin <mmoskvitin@google.com>
2023-12-12 14:15:00 +0000

    [Blink][CodeHealth] Migrate display_lock tests to blink_unittests_v2
    Fixed: 1507247
    Change-Id: I627da8a333b2516a7166de79f22cd7703ab06e76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5096322
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1236296}

--- third_party/blink/renderer/core/display_lock/display_lock_context_test.cc
+++ third_party/blink/renderer/core/display_lock/display_lock_context_test.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/core/testing/core_unit_test_helper.h"
 #include "third_party/blink/renderer/platform/testing/runtime_enabled_features_test_helpers.h"
+#include "third_party/blink/renderer/platform/testing/task_environment.h"
 #include "third_party/blink/renderer/platform/testing/unit_test_helpers.h"
 
 namespace blink {
@@ -195,6 +196,8 @@ class DisplayLockContextTest : public testing::Test,
   const int FAKE_FIND_ID = 1;
 
  private:
+  test::TaskEnvironment task_environment;
+
   frame_test_helpers::WebViewHelper web_view_helper_;
 };
 
