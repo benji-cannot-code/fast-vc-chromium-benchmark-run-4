604dd77d17f2f478fa87ae7ee02c0e3a1ba9f3a3
Harkiran Bolaria <hbolaria@google.com>
2023-12-12 13:47:35 +0000

    [CUI] Add data model for web content interaction.
    Previous patch crrev.com/4968843.
    
    Bug: b/b/307484548
    Change-Id: I0cfce5823d52927b02185288e1969ad657eb17fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5040564
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Harkiran Bolaria <hbolaria@google.com>
    Cr-Commit-Position: refs/heads/main@{#1236286}

--- base/tracing/stdlib/chrome/interactions.sql
+++ base/tracing/stdlib/chrome/interactions.sql
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 INCLUDE PERFETTO MODULE chrome.page_loads;
 INCLUDE PERFETTO MODULE chrome.startups;
+INCLUDE PERFETTO MODULE chrome.web_content_interactions;
 
 -- All critical user interaction events, including type and table with
 -- associated metrics.
@@ -47,4 +48,12 @@ SELECT
       THEN first_visible_content_ts - startup_begin_ts
     ELSE 0
   END AS dur
-FROM chrome_startups;
+FROM chrome_startups
+UNION ALL
+SELECT
+  id AS scoped_id,
+  'chrome_web_content_interactions' AS type,
+  'InteractionToFirstPaint' AS name,
+  ts,
+  dur
+FROM chrome_web_content_interactions;
