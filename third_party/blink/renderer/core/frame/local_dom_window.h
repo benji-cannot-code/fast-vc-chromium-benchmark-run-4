558a215abc13ec96eca641389e5282cb4ad6aaad
Sonja <laurila@google.com>
2023-12-12 09:25:16 +0000

    [AWC] Prompt window-management permission for min/max/restore APIs
    This CL adds prompting for the necessary permission if the
    window.minimize|maximize|restore() APIs are called without the
    permission being granted before.
    
    Bug: 1507772, 1466855
    Change-Id: I2cb686303e55720edb67078e16dd0babd3222c22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5096911
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Sonja Laurila <laurila@google.com>
    Cr-Commit-Position: refs/heads/main@{#1236217}

--- third_party/blink/renderer/core/frame/local_dom_window.h
+++ third_party/blink/renderer/core/frame/local_dom_window.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/functional/callback_forward.h"
 #include "base/task/single_thread_task_runner.h"
 #include "services/metrics/public/cpp/ukm_recorder.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
@@ -38,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/frame/history_user_activation_state.h"
 #include "third_party/blink/public/common/metrics/post_message_counter.h"
 #include "third_party/blink/public/common/tokens/tokens.h"
+#include "third_party/blink/public/mojom/permissions/permission.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_value.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/document.h"
@@ -539,9 +541,25 @@ class CORE_EXPORT LocalDOMWindow final : public DOMWindow,
   // given window, it cannot be taken away.
   void SetHasStorageAccess();
 
-  void maximize(ExceptionState&);
-  void minimize(ExceptionState&);
-  void restore(ExceptionState&);
+  void OnMaximizePermissionRequestComplete(
+      ScriptPromiseResolver* resolver,
+      mojom::blink::PermissionStatus status);
+  void OnMinimizePermissionRequestComplete(
+      ScriptPromiseResolver* resolver,
+      mojom::blink::PermissionStatus status);
+  void OnRestorePermissionRequestComplete(
+      ScriptPromiseResolver* resolver,
+      mojom::blink::PermissionStatus status);
+
+  using AdditionalWindowingControlsActionCallback =
+      base::OnceCallback<void(mojom::blink::PermissionStatus)>;
+  ScriptPromise MaybePromptWindowManagementPermission(
+      ScriptPromiseResolver* resolver,
+      AdditionalWindowingControlsActionCallback callback);
+
+  ScriptPromise maximize(ScriptState*, ExceptionState&);
+  ScriptPromise minimize(ScriptState*, ExceptionState&);
+  ScriptPromise restore(ScriptState*, ExceptionState&);
   void setResizable(bool resizable, ExceptionState&);
 
  protected:
