c431f0db8241ad86d64700b4e18fdf72dde6360f
Katie Dektar <katie@chromium.org>
2023-12-12 18:11:42 +0000

    [Select to speak] Convert select_to_speak.js to typescript
    Test:Existing, Manual with various STS settings and start speech methods
    Bug:b/270623046
    AX-Relnotes:N/A
    
    Change-Id: I55a1b7d7f822ead519ad81d994494c7ca049baca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5110524
    Reviewed-by: Anastasia Helfinstein <anastasi@google.com>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236427}

--- chrome/browser/resources/chromeos/accessibility/select_to_speak/input_handler.ts
+++ chrome/browser/resources/chromeos/accessibility/select_to_speak/input_handler.ts
@@ -137,11 +137,11 @@ export class InputHandler {
     document.addEventListener('copy', evt => this.onClipboardCopy_(evt));
     chrome.accessibilityPrivate.onSelectToSpeakKeysPressedChanged.addListener(
         (keysPressed) => {
-          this.onKeysPressedChanged_(new Set(keysPressed));
+          this.onKeysPressedChanged(new Set(keysPressed));
         });
     chrome.accessibilityPrivate.onSelectToSpeakMouseChanged.addListener(
         (eventType, mouseX, mouseY) => {
-          this.onMouseEvent_(eventType, mouseX, mouseY);
+          this.onMouseEvent(eventType, mouseX, mouseY);
         });
   }
 
@@ -181,8 +181,9 @@ export class InputHandler {
    *     coordinates.
    * @param mouseY The mouse y coordinate in global screen
    *     coordinates.
+   * Visible for testing.
    */
-  private onMouseEvent_(
+  onMouseEvent(
       type: chrome.accessibilityPrivate.SyntheticMouseEventType, mouseX: number,
       mouseY: number): void {
     if (type === chrome.accessibilityPrivate.SyntheticMouseEventType.PRESS) {
@@ -273,7 +274,7 @@ export class InputHandler {
   /**
    * Visible for testing.
    */
-  private onKeysPressedChanged_(keysCurrentlyPressed: Set<number>) : void {
+  onKeysPressedChanged(keysCurrentlyPressed: Set<number>): void {
     if (keysCurrentlyPressed.size > this.keysCurrentlyDown_.size) {
       // If a key was pressed.
       for (const key of keysCurrentlyPressed) {
