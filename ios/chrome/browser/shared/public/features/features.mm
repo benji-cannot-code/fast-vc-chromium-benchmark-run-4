3131f52e57aeb3eea3c80f5327e8c79a203b183b
Alexis Hetu <sugoi@chromium.org>
2023-12-12 16:01:59 +0000

    [iOS] Enable Payments Bottom Sheet by default
    The Payments Bottom Sheet feature is now approved for launch, so
    enable it by default.
    
    Bug: 1450214
    Change-Id: I831cdf48d825e7a7b177600cbe3f460282d4bd07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5116938
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236341}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -49,7 +49,7 @@ BASE_FEATURE(kIOSKeyboardAccessoryUpgrade,
 
 BASE_FEATURE(kIOSPaymentsBottomSheet,
              "IOSPaymentsBottomSheet",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kTestFeature, "TestFeature", base::FEATURE_DISABLED_BY_DEFAULT);
 
