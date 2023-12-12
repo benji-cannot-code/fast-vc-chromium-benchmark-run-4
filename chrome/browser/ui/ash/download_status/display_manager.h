b99f132e7f756a30e2454a882522fa5424c1d525
andrewxu <andrewxu@chromium.org>
2023-12-12 00:32:53 +0000

    [Reland][SysUi download integration] Add the download cancel button
    The original CL was reverted due to adding a flaky test.
    This CL fixes the flakiness by using a notification blocker to
    prevent notification popups from hiding the holding space tray.
    
    The patch set 1 is the previous CL that got reverted.
    
    The previous CL description:
    
    In this CL, DisplayManager attaches the cancel button metadata to
    the display metadata if the download is able to be canceled.
    
    This CL also implements the entry point to cancel download from
    holding space chips.
    
    This CL adds a string for the download cancellation command text.
    Its screenshot is attached to the issue.
    
    Bug: b/307353486
    Change-Id: I76bfa504e9ddc6fea49a266f6ebb04715cbc0c2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5106868
    Reviewed-by: David Black <dmblack@google.com>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236056}

--- chrome/browser/ui/ash/download_status/display_manager.h
+++ chrome/browser/ui/ash/download_status/display_manager.h
@@ -10,15 +10,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/memory/raw_ptr.h"
+
 class Profile;
 
-namespace crosapi::mojom {
+namespace crosapi {
+
+class DownloadStatusUpdaterAsh;
+
+namespace mojom {
 class DownloadStatus;
-}  // namespace crosapi::mojom
+}  // namespace mojom
+
+}  // namespace crosapi
 
 namespace ash::download_status {
 
 class DisplayClient;
+struct DisplayMetadata;
 
 // Acts as an intermediary between Lacros download updates and Ash displayed
 // download updates by:
@@ -30,7 +39,8 @@ class DisplayClient;
 // such as pausing the download, to `DownloadStatusUpdaterAsh` for handling.
 class DisplayManager {
  public:
-  explicit DisplayManager(Profile* profile);
+  DisplayManager(Profile* profile,
+                 crosapi::DownloadStatusUpdaterAsh* download_status_updater);
   DisplayManager(const DisplayManager&) = delete;
   DisplayManager& operator=(const DisplayManager&) = delete;
   ~DisplayManager();
@@ -39,9 +49,19 @@ class DisplayManager {
   void Update(const crosapi::mojom::DownloadStatus& download_status);
 
  private:
+  // Calculates the metadata to display the download update specified by
+  // `download_status`. This function should be called only when the specified
+  // download can be displayed.
+  DisplayMetadata CalculateDisplayMetadata(
+      const crosapi::mojom::DownloadStatus& download_status);
+
   // Removes the displayed download specified by `guid` from all clients. No op
   // if the specified download is not displayed.
   void Remove(const std::string& guid);
+
+  // Used to handle download actions, including pausing, resuming, and canceling
+  // downloads. NOTE: `download_status_updater_` owns this instance.
+  const raw_ptr<crosapi::DownloadStatusUpdaterAsh> download_status_updater_;
 
   // Responsible for displaying download updates.
   // All clients are ready when `DisplayManager` is created to ensure
