48260309d9631cb13dd738694d4ab6a82a46eca3
Maksim Moskvitin <mmoskvitin@google.com>
2023-12-12 16:53:01 +0000

    [Blink][CodeHealth] Migrate view_transition tests to blink_unittests_v2
    Fixed: 1507248
    Change-Id: I7be95947702f346592af4994781e22413b142123
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5098599
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1236368}

--- third_party/blink/renderer/core/view_transition/view_transition_test.cc
+++ third_party/blink/renderer/core/view_transition/view_transition_test.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/compositing/paint_artifact_compositor.h"
 #include "third_party/blink/renderer/platform/testing/find_cc_layer.h"
 #include "third_party/blink/renderer/platform/testing/paint_test_configurations.h"
+#include "third_party/blink/renderer/platform/testing/task_environment.h"
 #include "third_party/blink/renderer/platform/testing/unit_test_helpers.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
@@ -188,6 +189,9 @@ class ViewTransitionTest : public testing::Test,
   }
 
  protected:
+  test::TaskEnvironment task_environment_{
+      test::TaskEnvironment::RealMainThreadScheduler{}};
+
   std::unique_ptr<frame_test_helpers::WebViewHelper> web_view_helper_;
 };
 
