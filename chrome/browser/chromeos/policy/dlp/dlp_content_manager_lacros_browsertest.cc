3bd36569a38b07b1293db7b1f367f81750cde870
Luca Accorsi <accorsi@google.com>
2023-12-12 15:00:21 +0000

    dlp: Request file access from Lacros
    With this CL we request file access when pasting or dropping files
    in Lacros. The proper UI is shown in Ash via mojo communication from
    Lacros.
    
    Bug: b/314907867
    
    Change-Id: Ia0bbe944932f3abe2b21d34724caca42eedd4ab2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5093876
    Reviewed-by: Roland Bock <rbock@google.com>
    Commit-Queue: Luca Accorsi <accorsi@google.com>
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236316}

--- chrome/browser/chromeos/policy/dlp/dlp_content_manager_lacros_browsertest.cc
+++ chrome/browser/chromeos/policy/dlp/dlp_content_manager_lacros_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/bind.h"
 #include "chrome/browser/chromeos/policy/dlp/test/dlp_content_manager_test_helper.h"
+#include "chrome/browser/chromeos/policy/dlp/test/mock_dlp_crosapi.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chromeos/crosapi/mojom/dlp.mojom.h"
 #include "chromeos/lacros/lacros_service.h"
@@ -22,34 +23,6 @@ namespace {
 
 const std::u16string kAppId = u"app_id";
 constexpr char kScreenShareLabel[] = "label";
-
-// TODO(crbug.com/1291609): Mock Lacros side of crosapi instead when possible.
-// This will allow these tests to be just unit_tests, not
-// lacros_chrome_browsertests.
-class MockDlpCrosapi : public crosapi::mojom::Dlp {
- public:
-  MOCK_METHOD(void,
-              DlpRestrictionsUpdated,
-              (const std::string&, crosapi::mojom::DlpRestrictionSetPtr),
-              (override));
-  MOCK_METHOD(void,
-              CheckScreenShareRestriction,
-              (crosapi::mojom::ScreenShareAreaPtr,
-               const std::u16string&,
-               CheckScreenShareRestrictionCallback),
-              (override));
-  MOCK_METHOD(void,
-              OnScreenShareStarted,
-              (const std::string&,
-               crosapi::mojom::ScreenShareAreaPtr,
-               const ::std::u16string&,
-               ::mojo::PendingRemote<crosapi::mojom::StateChangeDelegate>),
-              (override));
-  MOCK_METHOD(void,
-              OnScreenShareStopped,
-              (const std::string&, crosapi::mojom::ScreenShareAreaPtr),
-              (override));
-};
 
 }  // namespace
 
