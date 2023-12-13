9259c98c21fe396da5852bd0f120dff3238f90dc
Peilin Wang <peilinwang@google.com>
2023-12-13 21:50:29 +0000

    [CCTTextFragmentLookupApiEnabled] Cleanup for fixit
    Bug: 1385901
    Change-Id: I65c9276aa5415787e02b5719d904c23f17c8140e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5118298
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Peilin Wang <peilinwang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1237199}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -204,7 +204,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kCCTResizableSideSheetForThirdParties,
     &kCCTResourcePrefetch,
     &kCCTTabModalDialog,
-    &kCCTTextFragmentLookupApiEnabled,
     &kDontAutoHideBrowserControls,
     &kCacheDeprecatedSystemLocationSetting,
     &kChromeSharingHub,
@@ -587,10 +586,6 @@ BASE_FEATURE(kCCTResourcePrefetch,
 
 BASE_FEATURE(kCCTTabModalDialog,
              "CCTTabModalDialog",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
-BASE_FEATURE(kCCTTextFragmentLookupApiEnabled,
-             "CCTTextFragmentLookupApiEnabled",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDontAutoHideBrowserControls,
