0f8c1b2ef7e5c07a214e51457375898f7b56b594
Tao Wu <wutao@chromium.org>
2023-12-13 01:01:11 +0000

    assistant: Remove assistant_client_v1
    Bug: b/300168375
    Test: Passed all the tests and manually checked main functions
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: Ia790a88f2b60371180343a440636ef8e172789a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5118380
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236703}

--- chromeos/ash/services/libassistant/grpc/assistant_client_impl.h
+++ chromeos/ash/services/libassistant/grpc/assistant_client_impl.h
@@ -11,18 +11,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ref.h"
-#include "base/scoped_observation.h"
-#include "chromeos/ash/services/libassistant/grpc/assistant_client_v1.h"
+#include "chromeos/ash/services/libassistant/grpc/assistant_client.h"
 #include "chromeos/ash/services/libassistant/grpc/external_services/grpc_services_initializer.h"
 #include "chromeos/ash/services/libassistant/grpc/services_status_provider.h"
 
 namespace ash::libassistant {
 
 class GrpcLibassistantClient;
+class ServicesStatusObserver;
 
-// This class wraps the libassistant grpc client and exposes V2 APIs for
-// ChromeOS to use.
-class AssistantClientImpl : public AssistantClientV1 {
+class AssistantClientImpl : public AssistantClient {
  public:
   AssistantClientImpl(
       std::unique_ptr<assistant_client::AssistantManager> assistant_manager,
@@ -31,7 +29,7 @@ class AssistantClientImpl : public AssistantClientV1 {
 
   ~AssistantClientImpl() override;
 
-  // AssistantClientV1 overrides:
+  // AssistantClient:
   void StartServices(ServicesStatusObserver* services_status_observer) override;
   bool StartGrpcServices() override;
   void StartGrpcHttpConnectionClient(
@@ -50,9 +48,13 @@ class AssistantClientImpl : public AssistantClientV1 {
   void GetSpeakerIdEnrollmentInfo(
       const GetSpeakerIdEnrollmentInfoRequest& request,
       base::OnceCallback<void(bool user_model_exists)> on_done) override;
+  void ResetAllDataAndShutdown() override;
   void SendDisplayRequest(const OnDisplayRequestRequest& request) override;
   void AddDisplayEventObserver(
       GrpcServicesObserver<OnAssistantDisplayEventRequest>* observer) override;
+  void ResumeCurrentStream() override;
+  void PauseCurrentStream() override;
+  void SetExternalPlaybackState(const MediaStatus& status_proto) override;
   void AddDeviceStateEventObserver(
       GrpcServicesObserver<OnDeviceStateEventRequest>* observer) override;
   void AddMediaActionFallbackEventObserver(
@@ -82,11 +84,12 @@ class AssistantClientImpl : public AssistantClientV1 {
       override;
   void GetAssistantSettings(
       const ::assistant::ui::SettingsUiSelector& selector,
-      const std::string& use_id,
+      const std::string& user_id,
       base::OnceCallback<
           void(const ::assistant::api::GetAssistantSettingsResponse&)> on_done)
       override;
   void SetLocaleOverride(const std::string& locale) override;
+  std::string GetDeviceId() override;
 
   // Audio-related functionality:
   void EnableListening(bool listening_enabled) override;
--- chromeos/ash/services/libassistant/grpc/utils/settings_utils.h
+++ chromeos/ash/services/libassistant/grpc/utils/settings_utils.h
@@ -8,21 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/assistant/internal/proto/shared/proto/v2/config_settings_interface.pb.h"
 
-namespace assistant_client {
-struct VoicelessResponse;
-}  // namespace assistant_client
-
 namespace ash::libassistant {
-
-// Populates a |GetAssistantSettingsResponse| proto with corresponding fields
-// from a |VoicelessResponse| struct.
-::assistant::api::GetAssistantSettingsResponse ToGetSettingsResponseProto(
-    const assistant_client::VoicelessResponse& voiceless_response);
-
-// Populates an |UpdateAssistantSettingsResponse| proto with corresponding
-// fields from a |VoicelessResponse| struct.
-::assistant::api::UpdateAssistantSettingsResponse ToUpdateSettingsResponseProto(
-    const assistant_client::VoicelessResponse& voiceless_response);
 
 // Returns a serialized proto of |SettingsUi| by default (or a serialized proto
 // of |GetSettingsUiResponse| if |include_header| is true) if the response
