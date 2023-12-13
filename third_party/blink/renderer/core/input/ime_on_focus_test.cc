4aeb181a25f5095d36803ac278da0b7889903989
Maksim Moskvitin <mmoskvitin@google.com>
2023-12-13 00:45:37 +0000

    [Blink][CodeHealth] Migrate input tests to blink_unittests_v2
    1. TaskEnvironment used in tests that fails w/o it.
    2. Test helpers from t/b/r/c/editing, that are used quite commonly by
    various tests outside editing, are moved from blink_core_tests_editing
    to list to unit_test_support source set. Needed to compile input tests
    within blink_unittests_v2 and otherwise one of the more correct ways to
    plumb such deps.
    
    Fixed: 1507263
    Change-Id: I93c49d6ef811708fd399f82c87e14cc71097f7e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5116142
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236691}

--- third_party/blink/renderer/core/input/ime_on_focus_test.cc
+++ third_party/blink/renderer/core/input/ime_on_focus_test.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/frame_test_helpers.h"
 #include "third_party/blink/renderer/core/frame/web_local_frame_impl.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
+#include "third_party/blink/renderer/platform/testing/task_environment.h"
 #include "third_party/blink/renderer/platform/testing/unit_test_helpers.h"
 #include "third_party/blink/renderer/platform/testing/url_loader_mock_factory.h"
 #include "third_party/blink/renderer/platform/testing/url_test_helpers.h"
@@ -39,6 +40,8 @@ class ImeOnFocusTest : public testing::Test {
                          gfx::Point tap_point = gfx::Point(-1, -1),
                          const AtomicString& focus_element = g_null_atom,
                          String frame = "");
+
+  test::TaskEnvironment task_environment_;
 
   String base_url_;
   frame_test_helpers::WebViewHelper web_view_helper_;
--- third_party/blink/renderer/core/input/touch_action_test.cc
+++ third_party/blink/renderer/core/input/touch_action_test.cc
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_view.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
 #include "third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h"
+#include "third_party/blink/renderer/platform/testing/task_environment.h"
 #include "third_party/blink/renderer/platform/testing/unit_test_helpers.h"
 #include "third_party/blink/renderer/platform/testing/url_loader_mock_factory.h"
 #include "third_party/blink/renderer/platform/testing/url_test_helpers.h"
@@ -144,6 +145,8 @@ class TouchActionTest : public testing::Test {
   void SendTouchEvent(WebView*, WebInputEvent::Type, gfx::Point client_point);
   WebViewImpl* SetupTest(String file);
   void RunTestOnTree(ContainerNode* root, WebView*);
+
+  test::TaskEnvironment task_environment_;
 
   String base_url_;
   frame_test_helpers::WebViewHelper web_view_helper_;
