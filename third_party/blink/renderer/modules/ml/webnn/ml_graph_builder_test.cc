921107df6fe1fe6cbee1851c6ed0f877f2ecaba0
Zelin Liu <zelin@chromium.org>
2023-12-13 16:31:54 +0000

    code-health: Migrate modules/ml to unit_tests_v2
    Bug: 1507318
    Change-Id: I060d23988b57603766a1ed3497f4438944f824e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5114274
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Commit-Queue: Zelin Liu <zelin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236976}

--- third_party/blink/renderer/modules/ml/webnn/ml_graph_builder_test.cc
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_builder_test.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/ml/webnn/ml_graph_test_base.h"
 #include "third_party/blink/renderer/modules/ml/webnn/ml_operand.h"
 #include "third_party/blink/renderer/modules/ml/webnn/ml_operator.h"
+#include "third_party/blink/renderer/platform/testing/task_environment.h"
 
 namespace blink {
 
@@ -34,6 +35,7 @@ class MLGraphBuilderTest : public testing::Test {
  public:
   MLGraphBuilderTest() = default;
   ~MLGraphBuilderTest() override = default;
+  test::TaskEnvironment task_environment_;
 };
 
 TEST_F(MLGraphBuilderTest, InputTest) {
--- third_party/blink/renderer/modules/ml/webnn/ml_graph_test_base.h
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_test_base.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/ml/webnn/ml_graph_builder_utils.h"
 #include "third_party/blink/renderer/modules/ml/webnn/ml_operand.h"
 #include "third_party/blink/renderer/platform/heap/persistent.h"
+#include "third_party/blink/renderer/platform/testing/task_environment.h"
 
 #if BUILDFLAG(BUILD_WEBNN_ON_CROS)
 #include "third_party/blink/renderer/modules/ml/webnn/ml_graph_test_cros.h"
@@ -85,6 +86,7 @@ class MLGraphTestBase : public ::testing::Test,
  private:
   // The execution mode for testing build and compute graph (e.g. async, sync.).
   ExecutionMode GetExecutionMode();
+  test::TaskEnvironment task_environment_;
 };
 
 // This class performs backend specific setup.
