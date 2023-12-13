314f584a407f001caec40bdecf3f46c9c0eb9b52
Calder Kitagawa <ckitagawa@chromium.org>
2023-12-13 02:30:29 +0000

    [Fixit] Attempt to fix TabGridIncognitoReauthPromoTest
    Speculative fix (will monitor test reviver). This does not repro on any
    device I own nor on any emulators (including those on CI/CQ).
    
    What appears to be happening is multiple copies of the Incognito card
    are being displayed. One suspect for this is batching causing reset to
    be delayed. Another is a lingering static variables for whether the
    message card was shown. Fix this by both unbatching the test and
    resetting the static value.
    
    This same fix may apply to TabGridIphTest.
    
    Also use onViewWaiting in the event the message cards are delayed.
    
    This will likely pass CQ because we don't have any arm builders on
    CQ that run chrome_public_test_apk only on CI :/
    
    Bug: 1510419
    Change-Id: I8e2c7a93520c34374bb75c1d1405cb9bd48b9055
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5117558
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1236747}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridIphTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridIphTest.java
@@ -53,6 +53,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.DisabledTest;
+import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
@@ -95,6 +96,7 @@ import java.io.IOException;
 // TODO(https://crbug.com/1362059): The message cards aren't shown the first time when entering GTS
 // with Start surface enabled.
 @DisableFeatures({ChromeFeatureList.ARCHIVE_TAB_SERVICE, ChromeFeatureList.START_SURFACE_ANDROID})
+@DoNotBatch(reason = "Batching can cause message state to leak between tests.")
 public class TabGridIphTest {
     private ModalDialogManager mModalDialogManager;
     private Tracker mTracker;
@@ -135,6 +137,8 @@ public class TabGridIphTest {
     @After
     public void tearDown() {
         ActivityTestUtils.clearActivityOrientation(mActivityTestRule.getActivity());
+        TestThreadUtils.runOnUiThreadBlocking(
+                TabSwitcherCoordinator::resetHasAppendedMessagesForTesting);
     }
 
     @Test
