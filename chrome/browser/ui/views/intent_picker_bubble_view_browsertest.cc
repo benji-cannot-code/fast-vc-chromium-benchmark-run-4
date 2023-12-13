9a5600d216c06e5c7c88690b3c7574c96fa599bc
Mikihito Matsuura <mikt@google.com>
2023-12-13 00:46:46 +0000

    Revert "[dPWA] Field trial testing configs for link capturing experiment"
    This reverts commit 07a50f82085e32bcf3f9f45fc93ba72118150019.
    
    Reason for revert:
    `ComputeAppSizeCommandBrowserTest.RetrieveWebAppSize` failing on
    Mac13 Tests:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac13%20Tests/6940/overview
    
    
    Original change's description:
    > [dPWA] Field trial testing configs for link capturing experiment
    >
    > This CL experiments with field trial testing configs for the link
    > capturing experiment and fixing tests that break. For the broken
    > tests:
    > 1. The IntentPickerBubbleViewBrowserTest is parameterized to work
    > for both default on and default off behavior.
    > 2. WebAppIntegration tests will be fixed in a follow-up CL (I will
    > revive crrev.com/c/4959176).
    > 3. Other tests have trivial fixes, and mostly constitutes changing
    > a `NavigateToUrlAndWait()` to a `ui_test_utils::NavigateToURL()`.
    > This is because the former simulates a link click, while the
    > latter simualtes a navigation by typing in an URL on the browser.
    > For all the tests where this change was made, this is not
    > a breaking change, and doesn't modify the behavior under test itself.
    >
    > Bug: 1412856, b/315155715
    > Change-Id: I4b64dae93c860010b70e01819b1ea09f703de075
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5105856
    > Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1236463}
    
    Bug: 1412856, b/315155715
    Change-Id: I3963f22f7dde79d6246c98d0cffe565a2ce02980
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5118838
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Mikihito Matsuura <mikt@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Mikihito Matsuura <mikt@google.com>
    Cr-Commit-Position: refs/heads/main@{#1236693}

--- chrome/browser/ui/views/intent_picker_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/intent_picker_bubble_view_browsertest.cc
@@ -39,7 +39,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class IntentPickerBrowserTest : public web_app::WebAppNavigationBrowserTest {
  public:
-  IntentPickerBrowserTest() = default;
+  IntentPickerBrowserTest() {
+    std::vector<base::test::FeatureRef> disabled_features = {
+        // TODO(crbug.com/1001189): Stop disabling Paint Holding.
+        blink::features::kPaintHolding};
+    scoped_feature_list_.InitWithFeatures({}, disabled_features);
+  }
 
   template <typename Action>
   testing::AssertionResult DoAndWaitForIntentPickerIconUpdate(Action action) {
@@ -112,6 +117,9 @@ class IntentPickerBrowserTest : public web_app::WebAppNavigationBrowserTest {
     EXPECT_EQ(test_web_app_id(), app_info[0].launch_name);
     EXPECT_EQ(GetAppName(), app_info[0].display_name);
   }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 // Tests to do with the behavior of the intent picker icon in the omnibox. Does
@@ -120,31 +128,7 @@ class IntentPickerBrowserTest : public web_app::WebAppNavigationBrowserTest {
 // separately in intent_chip_button_browsertest.cc.
 class IntentPickerIconBrowserTest
     : public IntentPickerBrowserTest,
-      public ::testing::WithParamInterface<std::tuple<std::string, bool>> {
- public:
-  // TODO(crbug.com/1001189): Stop disabling Paint Holding.
-  IntentPickerIconBrowserTest() {
-    feature_list_.InitWithFeaturesAndParameters(
-        apps::test::GetFeaturesToEnableLinkCapturingUX(
-            /*override_captures_by_default=*/IsLinkCapturingEnabled()),
-        {blink::features::kPaintHolding});
-  }
-
-  bool IsLinkCapturingEnabled() { return std::get<bool>(GetParam()); }
-
-  std::string rel() { return std::get<std::string>(GetParam()); }
-
-  bool IsDefaultOnEnabled() {
-#if BUILDFLAG(IS_CHROMEOS)
-    return false;
-#else
-    return IsLinkCapturingEnabled();
-#endif  // BUILDFLAG(IS_CHROMEOS)
-  }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
-};
+      public ::testing::WithParamInterface<std::string> {};
 
 // Tests that clicking a link from a tabbed browser to outside the scope of an
 // installed app does not show the intent picker.
@@ -156,7 +140,7 @@ IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
       https_server().GetURL(GetAppUrlHost(), GetOutOfScopeUrlPath());
   NavigateToLaunchingPage(browser());
   ASSERT_TRUE(ExpectLinkClickNotCapturedIntoAppBrowser(
-      browser(), out_of_scope_url, rel()));
+      browser(), out_of_scope_url, GetParam()));
 
   views::Button* intent_picker_view = GetIntentPickerIcon();
   EXPECT_FALSE(intent_picker_view->GetVisible());
@@ -176,9 +160,6 @@ IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
 #endif
 IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
                        MAYBE_NavigationToInScopeLinkShowsIntentPicker) {
-  if (IsDefaultOnEnabled()) {
-    GTEST_SKIP() << "Default On will launch app by default";
-  }
   InstallTestWebApp();
 
   const GURL in_scope_url =
@@ -188,8 +169,8 @@ IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
 
   base::RunLoop run_loop;
   tab_helper->SetIconUpdateCallbackForTesting(run_loop.QuitClosure());
-  ASSERT_TRUE(
-      ExpectLinkClickNotCapturedIntoAppBrowser(browser(), in_scope_url, rel()));
+  ASSERT_TRUE(ExpectLinkClickNotCapturedIntoAppBrowser(browser(), in_scope_url,
+                                                       GetParam()));
   run_loop.Run();
 
   views::Button* intent_picker_icon = GetIntentPickerIcon();
@@ -217,9 +198,9 @@ IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
   views::Button* intent_picker_icon = GetIntentPickerIcon();
 
   // OpenNewTab opens a new tab and focus on the new tab.
-  OpenNewTab(in_scope_url, /*rel=*/rel());
+  OpenNewTab(in_scope_url, /*rel=*/GetParam());
   EXPECT_TRUE(intent_picker_icon->GetVisible());
-  OpenNewTab(out_of_scope_url, /*rel=*/rel());
+  OpenNewTab(out_of_scope_url, /*rel=*/GetParam());
   EXPECT_FALSE(intent_picker_icon->GetVisible());
 
   chrome::SelectPreviousTab(browser());
@@ -229,7 +210,7 @@ IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
 }
 
 // Tests that the navigation in iframe doesn't affect intent picker icon
-IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
+IN_PROC_BROWSER_TEST_F(IntentPickerIconBrowserTest,
                        IframeNavigationDoesNotAffectIntentPicker) {
   InstallTestWebApp();
 
@@ -279,11 +260,11 @@ IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
 
   OpenNewTab(in_scope_url);
   EXPECT_TRUE(intent_picker_icon->GetVisible());
-  ASSERT_TRUE(DoAndWaitForIntentPickerIconUpdate(
-      [this, redirect_url, out_of_scope_url] {
-        ClickLinkAndWaitForURL(GetWebContents(), redirect_url, out_of_scope_url,
-                               LinkTarget::SELF, rel());
-      }));
+  ASSERT_TRUE(DoAndWaitForIntentPickerIconUpdate([this, redirect_url,
+                                                  out_of_scope_url] {
+    ClickLinkAndWaitForURL(GetWebContents(), redirect_url, out_of_scope_url,
+                           LinkTarget::SELF, GetParam());
+  }));
   EXPECT_FALSE(intent_picker_icon->GetVisible());
 }
 
@@ -297,7 +278,7 @@ IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
 #define MAYBE_DoNotShowIconAndBubbleOnServicePages \
   DoNotShowIconAndBubbleOnServicePages
 #endif
-IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
+IN_PROC_BROWSER_TEST_F(IntentPickerIconBrowserTest,
                        MAYBE_DoNotShowIconAndBubbleOnServicePages) {
   InstallTestWebApp();
 
@@ -331,7 +312,7 @@ IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
 #else
 #define MAYBE_DoNotShowIconOnErrorPages DoNotShowIconOnErrorPages
 #endif  // BUILDFLAG(IS_MAC)
-IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
+IN_PROC_BROWSER_TEST_F(IntentPickerIconBrowserTest,
                        MAYBE_DoNotShowIconOnErrorPages) {
   InstallTestWebApp();
   InstallTestWebApp("www.google.com", "/");
@@ -368,7 +349,7 @@ IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
 #else
 #define MAYBE_PushStateURLChangeTest PushStateURLChangeTest
 #endif
-IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
+IN_PROC_BROWSER_TEST_F(IntentPickerIconBrowserTest,
                        MAYBE_PushStateURLChangeTest) {
   // Note: The test page is served from embedded_test_server() as https_server()
   // always returns empty responses.
@@ -398,29 +379,17 @@ IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
 INSTANTIATE_TEST_SUITE_P(
     All,
     IntentPickerIconBrowserTest,
-    testing::Combine(testing::Values("", "noopener", "noreferrer", "nofollow"),
-#if BUILDFLAG(IS_CHROMEOS)
-                     testing::Values(false)),
-#else
-                     testing::Values(true, false)),
-#endif  // BUILDFLAG(IS_CHROMEOS)
-    [](const testing::TestParamInfo<std::tuple<std::string, bool>>& info) {
-      std::string test_name;
-      test_name = std::get<std::string>(info.param);
-      test_name.append(std::get<bool>(info.param) ? "DefaultOn" : "DefaultOff");
-      return test_name;
-    });
+    testing::Values("", "noopener", "noreferrer", "nofollow"));
 
 class IntentPickerIconBrowserBubbleTest
     : public IntentPickerBrowserTest,
       public testing::WithParamInterface<bool> {
  public:
-  // TODO(crbug.com/1001189): Stop disabling Paint Holding.
   IntentPickerIconBrowserBubbleTest() {
     feature_list_.InitWithFeaturesAndParameters(
         apps::test::GetFeaturesToEnableLinkCapturingUX(
             /*override_captures_by_default=*/GetParam()),
-        {blink::features::kPaintHolding});
+        {});
   }
   bool LinkCapturingEnabledByDefault() const { return GetParam(); }
 
@@ -486,11 +455,6 @@ INSTANTIATE_TEST_SUITE_P(,
                            return info.param ? "DefaultOn" : "DefaultOff";
                          });
 
-// This test only works when link capturing is set to default off for desktop
-// platforms, as prerendering navigations are aborted during link captured app
-// launches. See LinkCapturingNavigationThrottle::MaybeCreate for more
-// information.
-// TODO(b/297256243): Investigate prerendering integration with link capturing.
 class IntentPickerIconPrerenderingBrowserTest
     : public IntentPickerIconBrowserTest {
  public:
@@ -559,14 +523,7 @@ IN_PROC_BROWSER_TEST_P(IntentPickerIconPrerenderingBrowserTest,
 INSTANTIATE_TEST_SUITE_P(
     All,
     IntentPickerIconPrerenderingBrowserTest,
-    testing::Combine(testing::Values("", "noopener", "noreferrer", "nofollow"),
-                     testing::Values(false)),
-    [](const testing::TestParamInfo<std::tuple<std::string, bool>>& info) {
-      std::string test_name;
-      test_name = std::get<std::string>(info.param);
-      test_name.append("DefaultOff");
-      return test_name;
-    });
+    testing::Values("", "noopener", "noreferrer", "nofollow"));
 
 class IntentPickerIconFencedFrameBrowserTest
     : public IntentPickerIconBrowserTest {
@@ -610,15 +567,4 @@ IN_PROC_BROWSER_TEST_P(IntentPickerIconFencedFrameBrowserTest,
 INSTANTIATE_TEST_SUITE_P(
     All,
     IntentPickerIconFencedFrameBrowserTest,
-    testing::Combine(testing::Values("", "noopener", "noreferrer", "nofollow"),
-#if BUILDFLAG(IS_CHROMEOS)
-                     testing::Values(false)),
-#else
-                     testing::Values(true, false)),
-#endif
-    [](const testing::TestParamInfo<std::tuple<std::string, bool>>& info) {
-      std::string test_name;
-      test_name = std::get<std::string>(info.param);
-      test_name.append(std::get<bool>(info.param) ? "DefaultOn" : "DefaultOff");
-      return test_name;
-    });
+    testing::Values("", "noopener", "noreferrer", "nofollow"));
--- chrome/browser/ui/web_applications/web_app_dark_mode_browsertest.cc
+++ chrome/browser/ui/web_applications/web_app_dark_mode_browsertest.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
 #include "chrome/browser/web_applications/web_app.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
-#include "chrome/test/base/ui_test_utils.h"
 #include "components/embedder_support/switches.h"
 #include "components/page_load_metrics/browser/page_load_metrics_test_waiter.h"
 #include "content/public/test/browser_test.h"
@@ -242,7 +241,7 @@ IN_PROC_BROWSER_TEST_F(WebAppDarkModeOriginTrialBrowserTest, OriginTrial) {
     UpdateAwaiter update_awaiter(provider.install_manager());
 
     serve_token = false;
-    EXPECT_TRUE(ui_test_utils::NavigateToURL(browser(), GURL(kTestWebAppUrl)));
+    NavigateToURLAndWait(browser(), GURL(kTestWebAppUrl));
 
     update_awaiter.AwaitUpdate();
   }
