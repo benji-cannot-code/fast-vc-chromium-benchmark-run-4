1e89bf4dea2b27c54681a75f20300019da840b50
David Jacobo <djacobo@google.com>
2023-12-13 07:26:31 +0000

    gio: add missing base::flat_map includes
    This CL adds missing includes on c/b/ash/arc/input_overlay/
    
    Bug: none
    Test: build
    Change-Id: I9085fdc6e64e4c4f6a9a86e4e8ab75201760b082
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5118389
    Auto-Submit: David Jacobo <djacobo@chromium.org>
    Reviewed-by: Cici Ruan <cuicuiruan@google.com>
    Commit-Queue: David Jacobo <djacobo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236816}

--- chrome/browser/ash/arc/input_overlay/arc_input_overlay_manager.h
+++ chrome/browser/ash/arc/input_overlay/arc_input_overlay_manager.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/components/arc/mojom/app.mojom.h"
 #include "ash/constants/ash_features.h"
+#include "base/containers/flat_map.h"
 #include "base/memory/raw_ptr.h"
 #include "base/scoped_multi_source_observation.h"
 #include "base/scoped_observation.h"
