47ec8feac414ed03cde52944f646094aa90dd7d8
Sophey Dong <sophey@chromium.org>
2023-12-13 18:29:42 +0000

    Revert "kiosk: Use AshBrowserTestStarter for better test debugging"
    This reverts commit cc5d013a9719c2816f7590f43e1d12f69f831118.
    
    Reason for revert: https://crbug.com/1511288, ChromeAppKioskLacrosTest.NonKioskAppLaunchError failures
    
    Original change's description:
    > kiosk: Use AshBrowserTestStarter for better test debugging
    >
    > KioskAshBrowserTestStarter previously didn't call through to the general
    > AshBrowserTestStarter. This meant that we were missing out on lacros
    > logs when tests fail on the bots or for local debugging.
    >
    > Bug: b/313631093
    > Test: WebKioskLacrosTest
    > Test: ChromeAppKioskLacrosTest
    > Change-Id: I96f2ae1d8fc0a345b3655bb8538268e5366014a1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5067426
    > Reviewed-by: Irina Fedorova <irfedorova@google.com>
    > Commit-Queue: Ben Franz <bfranz@chromium.org>
    > Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1236887}
    
    Bug: b/313631093, 1511288
    Change-Id: I03b9531b9a5b2018e0165aa0fdc0d6464454a1c6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5120193
    Owners-Override: Sophey Dong <sophey@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Sophey Dong <sophey@chromium.org>
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237062}

--- chrome/browser/ash/login/app_mode/test/kiosk_ash_browser_test_starter.cc
+++ chrome/browser/ash/login/app_mode/test/kiosk_ash_browser_test_starter.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_switches.h"
 #include "base/check.h"
 #include "base/command_line.h"
+#include "base/environment.h"
 #include "chrome/browser/ash/crosapi/browser_manager.h"
 #include "chrome/browser/ash/crosapi/browser_util.h"
 #include "chrome/browser/ash/crosapi/fake_device_ownership_waiter.h"
@@ -17,21 +18,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/policy_map.h"
 #include "components/policy/core/common/policy_types.h"
 #include "components/policy/policy_constants.h"
+#include "testing/gtest/include/gtest/gtest.h"
 
 namespace ash {
 
 bool KioskAshBrowserTestStarter::HasLacrosArgument() {
-  return ash_browser_test_starter_.HasLacrosArgument();
+  return base::CommandLine::ForCurrentProcess()->HasSwitch(
+      ash::switches::kLacrosChromePath);
 }
 
 void KioskAshBrowserTestStarter::PrepareEnvironmentForKioskLacros() {
-  CHECK(ash_browser_test_starter_.PrepareEnvironmentForLacros());
+  DCHECK(HasLacrosArgument());
+  std::unique_ptr<base::Environment> env(base::Environment::Create());
+  ASSERT_TRUE(scoped_temp_dir_xdg_.CreateUniqueTempDir());
+  env->SetVar("XDG_RUNTIME_DIR", scoped_temp_dir_xdg_.GetPath().AsUTF8Unsafe());
 
-  // The `kDisableLacrosKeepAliveForTesting` switch is set by
-  // `AshBrowserTestStarter`, but kiosk launch relies on `KeepAlive`, so remove
-  // it again.
-  base::CommandLine::ForCurrentProcess()->RemoveSwitch(
-      switches::kDisableLacrosKeepAliveForTesting);
+  base::CommandLine::ForCurrentProcess()->AppendSwitch(
+      ash::switches::kAshEnableWaylandServer);
+
+  std::vector<std::string> lacros_args = {
+      // Disable gpu process in Lacros since hardware accelerated rendering is
+      // not possible yet in Ash X11 backend. See details in crbug/1478369.
+      "--disable-gpu",
+      // Disable gpu sandbox in Lacros since it fails in Linux emulator
+      // environment.
+      // See details in crbug/1483530.
+      "--disable-gpu-sandbox"};
+  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      ash::switches::kLacrosChromeAdditionalArgs,
+      base::JoinString(lacros_args, "####"));
 }
 
 void KioskAshBrowserTestStarter::SetLacrosAvailabilityPolicy() {
@@ -46,7 +61,9 @@ void KioskAshBrowserTestStarter::SetLacrosAvailabilityPolicy() {
 }
 
 void KioskAshBrowserTestStarter::SetUpBrowserManager() {
-  ash_browser_test_starter_.SetUpBrowserManager();
+  DCHECK(HasLacrosArgument());
+  crosapi::BrowserManager::Get()->set_device_ownership_waiter_for_testing(
+      std::make_unique<crosapi::FakeDeviceOwnershipWaiter>());
 }
 
 }  // namespace ash
