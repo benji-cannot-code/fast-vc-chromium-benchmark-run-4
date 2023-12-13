1611d750d2e647242d0d385c05d6222f6d085d28
Scott Haseley <shaseley@chromium.org>
2023-12-13 21:33:14 +0000

    [blink scheduler] Remove kMainThreadHighPriorityImageLoading flag
    This was enabled by default in M118, and it has been in stable for a
    couple milestones.
    
    Bug: 1416030, 1510595
    Change-Id: I7f9cb17da4c03311c4661fce89f1d88341e49511
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5120126
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237189}

--- third_party/blink/renderer/platform/loader/fetch/resource_fetcher.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource_fetcher.cc
@@ -1388,9 +1388,7 @@ std::unique_ptr<URLLoader> ResourceFetcher::CreateURLLoader(
       }
     }
   } else if (request.GetRequestContext() ==
-                 mojom::blink::RequestContextType::IMAGE &&
-             base::FeatureList::IsEnabled(
-                 features::kMainThreadHighPriorityImageLoading)) {
+             mojom::blink::RequestContextType::IMAGE) {
     if (auto* frame_or_worker_scheduler = GetFrameOrWorkerScheduler()) {
       if (auto* frame_scheduler =
               frame_or_worker_scheduler->ToFrameScheduler()) {
--- third_party/blink/web_tests/virtual/main-thread-high-priority-image-loading/README.md
+++
@@ -1,5 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-This virtual suite runs tests with the MainThreadHighPriorityImageLoading
-feature disabled.
-
-Bug: crbug.com/1416030
