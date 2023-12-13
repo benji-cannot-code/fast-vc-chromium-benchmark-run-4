d19b4b0ffc1560b7e6a65515379292e673e70d9a
Haihan Chen <haihan@google.com>
2023-12-13 18:27:38 +0000

    [URL Filtering] Add url categories to rt_lookup tab
    chrome://safe-browsing rt lookup tab shows url categories field from protego response. Screenshot: https://screenshot.googleplex.com/9BnRD6RWEPPCFaX
    
    Bug: b/288892761
    Change-Id: I90c51d474da8318ec74b13a58d3a2122e06e63da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5105054
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Nasser Al-shawwa <alshawwa@chromium.org>
    Commit-Queue: Haihan Chen <haihan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1237060}

--- components/safe_browsing/content/browser/web_ui/safe_browsing_ui.cc
+++ components/safe_browsing/content/browser/web_ui/safe_browsing_ui.cc
@@ -2379,6 +2379,12 @@ std::string SerializeURTLookupResponse(const RTLookupResponse& response) {
   }
   response_dict.Set("threat_infos", std::move(threat_info_list));
 
+  base::Value::List url_categories_list;
+  for (const std::string& url_category : response.url_categories()) {
+    url_categories_list.Append(url_category);
+  }
+  response_dict.Set("url_categories", std::move(url_categories_list));
+
   std::string response_serialized;
   JSONStringValueSerializer serializer(&response_serialized);
   serializer.set_pretty_print(true);
