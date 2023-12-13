cf1fbc69c7d6b6138487383bb7d570c0049e2892
Jérôme Lebel <jlebel@chromium.org>
2023-12-13 16:20:32 +0000

    [iOS][Waffle] Update FakeOmniboxView colors based on UITraitCollection
    This patch fixes issue introduced with crrev.com/c/5103088.
    With this path, FakeOmniboxView updates itself the 2 CALayers colors
    when the dark mode/light mode are changed.
    A new empty FakeOmniboxView is not created anymore.
    
    Bug: 1509738
    Change-Id: I8912975248b87c0e819ea9aa6f23c48d6f1a4b80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5119727
    Reviewed-by: Samar Chehade-Lepleux <samarchehade@google.com>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236973}

--- ios/chrome/browser/ui/search_engine_choice/fake_omnibox/fake_omnibox_view.h
+++ ios/chrome/browser/ui/search_engine_choice/fake_omnibox/fake_omnibox_view.h
@@ -21,8 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     NS_DESIGNATED_INITIALIZER;
 
 - (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
-
 - (instancetype)initWithCoder:(NSCoder*)coder NS_UNAVAILABLE;
+- (instancetype)init NS_UNAVAILABLE;
 
 @property(nonatomic, strong) UIImage* faviconImage;
 
