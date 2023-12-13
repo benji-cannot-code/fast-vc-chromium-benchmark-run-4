a9d7a56f2194d93874c2c39d7a2b8295b41c7302
Mei Liang <meiliang@chromium.org>
2023-12-13 20:28:33 +0000

    [Discounts][fixit] Clean up the kNtpHistoryClustersModuleDiscounts flag
    Bug: b:309863117
    Change-Id: I7f9457c9c78cfcadb22f8e302d5f582d18101d0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5113008
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Reviewed-by: Zhiyuan Cai <zhiyuancai@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1237135}

--- components/autofill/core/browser/payments/autofill_offer_manager.cc
+++ components/autofill/core/browser/payments/autofill_offer_manager.cc
@@ -135,9 +135,7 @@ void AutofillOfferManager::GetShoppingServiceOfferForUrl(
     const GURL& url,
     AsyncOfferCallback callback) {
   if (shopping_service_delegate_ &&
-      (shopping_service_delegate_->IsDiscountEligibleToShowOnNavigation() ||
-       (base::FeatureList::IsEnabled(
-           ntp_features::kNtpHistoryClustersModuleDiscounts)))) {
+      shopping_service_delegate_->IsDiscountEligibleToShowOnNavigation()) {
     shopping_service_delegate_->GetDiscountInfoForUrls(
         {url}, base::BindOnce(
                    &AutofillOfferManager::HandleShoppingServiceResponse,
--- components/autofill/core/browser/payments/offer_notification_handler.cc
+++ components/autofill/core/browser/payments/offer_notification_handler.cc
@@ -74,10 +74,7 @@ void OfferNotificationHandler::UpdateOfferNotificationVisibility(
     AutofillClient* client) {
   const GURL url = client->GetLastCommittedPrimaryMainFrameURL();
 
-  bool url_contains_discount_utm_tag =
-      base::FeatureList::IsEnabled(
-          ntp_features::kNtpHistoryClustersModuleDiscounts) &&
-      commerce::UrlContainsDiscountUtmTag(url);
+  bool url_contains_discount_utm_tag = commerce::UrlContainsDiscountUtmTag(url);
   bool valid_offer_exists_for_url_on_device = ValidOfferExistsForUrl(url);
 
   bool show_offer_notification_for_server_retrieved_offer = false;
@@ -120,7 +117,7 @@ void OfferNotificationHandler::UpdateOfferNotificationVisibility(
   // backend and store locally, If we have shown an offer stored on the device
   // above, the offer will not be shown here. Otherwise the offer retrieved here
   // from the server is shown if kShowDiscountOnNavigation is enabled or
-  // kNtpHistoryClustersModuleDiscounts is enabled with the discount UTM.
+  // the commited url contains the the discount UTM.
   auto shopping_service_callback =
       show_offer_notification_for_server_retrieved_offer
           ? base::BindOnce(&OfferNotificationHandler::
@@ -173,9 +170,7 @@ bool OfferNotificationHandler::
 
   // If the URL contains the expected UTM tags, the notification should show
   // automatically if the offer has not been shown before.
-  if (base::FeatureList::IsEnabled(
-          ntp_features::kNtpHistoryClustersModuleDiscounts) &&
-      commerce::UrlContainsDiscountUtmTag(url)) {
+  if (commerce::UrlContainsDiscountUtmTag(url)) {
     return ShouldAutoPopupForHistoryClustersModuleDiscounts(
         offer_has_been_shown_before);
   }
