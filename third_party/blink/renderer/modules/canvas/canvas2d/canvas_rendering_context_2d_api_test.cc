24aa77424e16cf477bb0589b090c3d56b19f0650
Antonio Sartori <antoniosartori@chromium.org>
2023-12-13 19:57:41 +0000

    [IdentifiabilityStudy] Clean up settings for active sampling
    This CL cleans up unused settings of the IdentifiabilityStudy feature.
    
    Change-Id: I0821574135f0afab595e685b9dd5cffdcc3a099b
    Bug: b/313902719
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5088367
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237120}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d_api_test.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d_api_test.cc
@@ -357,7 +357,6 @@ class ActiveSettingsProvider : public IdentifiabilityStudySettingsProvider {
   bool IsTypeAllowed(IdentifiableSurface::Type type) const override {
     return true;
   }
-  bool ShouldActivelySample() const override { return false; }
 
  private:
   const bool enabled_ = true;
