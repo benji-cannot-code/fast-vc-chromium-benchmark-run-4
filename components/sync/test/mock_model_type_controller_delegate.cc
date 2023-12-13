40a44a19b73243d9b173c6a28dc4550d23db1870
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-12-13 12:48:57 +0000

    [android] Test PasswordModelTypeController's handling of local UPM pref
    Unit test crrev.com/c/5088712.
    
    Bug: 1495626
    Change-Id: I3b12e4a716cf66e78c6c1e8df1921bb3ce5ba94d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5110140
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1236907}

---
+++ components/sync/test/mock_model_type_controller_delegate.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/sync/test/mock_model_type_controller_delegate.h"
+
+namespace syncer {
+
+MockModelTypeControllerDelegate::MockModelTypeControllerDelegate() = default;
+
+MockModelTypeControllerDelegate::~MockModelTypeControllerDelegate() = default;
+
+}  // namespace syncer
