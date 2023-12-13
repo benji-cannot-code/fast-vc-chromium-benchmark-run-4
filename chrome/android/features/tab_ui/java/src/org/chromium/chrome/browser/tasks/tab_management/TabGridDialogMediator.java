e6b21eb98e038a87943bb0751a53cd268c936971
Calder Kitagawa <ckitagawa@chromium.org>
2023-12-13 02:27:14 +0000

    [Tab Switcher] Use ValueChangedCallback for TabModelFilterSupplier
    This is a follow up to migrating to TabModelFilterSupplier. Remove
    easy access to mCurrentTabModelFilter by using a ValueChangedCallback
    and make the recommended method of accessing the value the supplier.
    
    Bug: 1505772
    Change-Id: I4bd4f3fee14f16df9bf6cd88e9b655b66b9f64fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5117640
    Reviewed-by: Sky Malice <skym@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236744}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogMediator.java
@@ -17,6 +17,7 @@ import androidx.appcompat.content.res.AppCompatResources;
 
 import org.chromium.base.Callback;
 import org.chromium.base.ResettersForTesting;
+import org.chromium.base.ValueChangedCallback;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.supplier.Supplier;
@@ -109,7 +110,8 @@ public class TabGridDialogMediator
     private final Context mContext;
     private final PropertyModel mModel;
     private final ObservableSupplier<TabModelFilter> mCurrentTabModelFilterSupplier;
-    private final Callback<TabModelFilter> mOnTabModelFilterChanged = this::onTabModelFilterChanged;
+    private final ValueChangedCallback<TabModelFilter> mOnTabModelFilterChanged =
+            new ValueChangedCallback<>(this::onTabModelFilterChanged);
     private final TabModelObserver mTabModelObserver;
     private final TabCreatorManager mTabCreatorManager;
     private final DialogController mDialogController;
@@ -121,7 +123,6 @@ public class TabGridDialogMediator
     private final SnackbarManager mSnackbarManager;
     private final String mComponentName;
 
-    private TabModelFilter mCurrentTabModelFilter;
     private TabGroupTitleEditor mTabGroupTitleEditor;
     private Supplier<TabListEditorController> mTabListEditorControllerSupplier;
     private boolean mTabListEditorSetup;
@@ -274,7 +275,7 @@ public class TabGridDialogMediator
                     }
                 };
 
-        onTabModelFilterChanged(
+        mOnTabModelFilterChanged.onResult(
                 mCurrentTabModelFilterSupplier.addObserver(mOnTabModelFilterChanged));
 
         // Setup ScrimView click Runnable.
@@ -402,7 +403,7 @@ public class TabGridDialogMediator
 
     /** Destroy any members that needs clean up. */
     public void destroy() {
-        removeCurrentTabModelFilterObservers();
+        removeTabModelFilterObserver(mCurrentTabModelFilterSupplier.get());
         mCurrentTabModelFilterSupplier.removeObserver(mOnTabModelFilterChanged);
         KeyboardVisibilityDelegate.getInstance()
                 .removeKeyboardVisibilityListener(mKeyboardVisibilityListener);
@@ -744,20 +745,19 @@ public class TabGridDialogMediator
         return true;
     }
 
-    private void onTabModelFilterChanged(TabModelFilter filter) {
-        if (mCurrentTabModelFilter == filter) return;
+    private void onTabModelFilterChanged(
+            @Nullable TabModelFilter newFilter, @Nullable TabModelFilter oldFilter) {
+        removeTabModelFilterObserver(oldFilter);
 
-        updateColorProperties(mContext, filter.isIncognito());
-
-        removeCurrentTabModelFilterObservers();
-
-        filter.addObserver(mTabModelObserver);
-        mCurrentTabModelFilter = filter;
+        if (newFilter != null) {
+            updateColorProperties(mContext, newFilter.isIncognito());
+            newFilter.addObserver(mTabModelObserver);
+        }
     }
 
-    private void removeCurrentTabModelFilterObservers() {
-        if (mCurrentTabModelFilter != null) {
-            mCurrentTabModelFilter.removeObserver(mTabModelObserver);
+    private void removeTabModelFilterObserver(@Nullable TabModelFilter filter) {
+        if (filter != null) {
+            filter.removeObserver(mTabModelObserver);
         }
     }
 
