53b6cc388d7a224432d87ed3f57dc5633b104368
David Bienvenu <davidbienvenu@chromium.org>
2023-12-13 22:18:25 +0000

    Stop ExtensionProtocolTest from adding shortcut to start menu
    Use ScopedPathOverride of DIR_START_MENU to stop
    ExtensionProtocolTests from creating web app shortcuts in the real
    Windows start menu.
    
    Bug: 1405159
    Change-Id: I38fb109ff6feb1c187af75bce0972d0fb4b57093
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5120545
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237220}

--- chrome/browser/devtools/protocol/devtools_protocol_browsertest.cc
+++ chrome/browser/devtools/protocol/devtools_protocol_browsertest.cc
@@ -73,6 +73,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/boringssl/src/include/openssl/ssl.h"
 #include "url/origin.h"
 
+#if BUILDFLAG(IS_WIN)
+#include "base/base_paths_win.h"
+#include "base/test/scoped_path_override.h"
+#endif
+
 using DevToolsProtocolTest = DevToolsProtocolTestBase;
 using testing::AllOf;
 using testing::Contains;
@@ -935,6 +940,13 @@ class ExtensionProtocolTest : public DevToolsProtocolTest {
   extensions::ExtensionService* extension_service_;
   extensions::ExtensionRegistry* extension_registry_;
   content::WebContents* background_web_contents_;
+#if BUILDFLAG(IS_WIN)
+  // This is needed to stop ExtensionProtocolTestsfrom creating a
+  // shortcut in the Windows start menu. The override needs to last until the
+  // test is destroyed, because Windows shortcut tasks which create the shortcut
+  // can run after the test body returns.
+  base::ScopedPathOverride override_start_dir{base::DIR_START_MENU};
+#endif  // BUILDFLAG(IS_WIN
 };
 
 IN_PROC_BROWSER_TEST_F(ExtensionProtocolTest, ReloadTracedExtension) {
