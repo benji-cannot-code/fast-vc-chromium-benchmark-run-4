873bcf7032641df32aafa05ddc189602e7f40fb6
David Benjamin <davidben@chromium.org>
2023-12-13 00:10:51 +0000

    Default initialize all fields of PartialTranslateRequest
    The unit tests create a PartialTranslateRequest without filling in all
    the fields. This is fine for the string types, which are default,
    initialized, but bool is not. Default-initialize everything, so we don't
    need to risk problems with uninitialized fields.
    
    This fixes the following UBSan error:
    https://luci-milo.appspot.com/ui/inv/build-8761892290068215089/test-results?q=PartialTranslateManagerTest.CreateResponse&sortby=&groupby=
    
    While I'm here, I noticed an unnecessary copy of
    PartialTranslateRequest. Fix that by using a const reference.
    
    Bug: 1394755
    Change-Id: I100441f28b2266afa22f546c01e258c3c2b2c51f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5116946
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236669}

--- components/translate/content/browser/partial_translate_manager.h
+++ components/translate/content/browser/partial_translate_manager.h
@@ -41,7 +41,7 @@ struct PartialTranslateRequest {
   // Whether or not |source_language| should be applied as a hint for backend
   // language detection. Otherwise, backend translation is forced using
   // |source_language|.
-  bool apply_lang_hint;
+  bool apply_lang_hint = false;
 };
 
 // Indicates the outcome of a Partial Translate request.
@@ -88,7 +88,7 @@ class PartialTranslateManager {
   // call |callback| once the request is completed (unless another request
   // subsumes it).
   void StartPartialTranslate(content::WebContents* web_contents,
-                             PartialTranslateRequest request,
+                             const PartialTranslateRequest& request,
                              PartialTranslateCallback callback);
 
  private:
