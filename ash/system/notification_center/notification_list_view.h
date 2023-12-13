e059f58c69667388558f6ff7cd6e07228084981a
Ahmed Mehfooz <amehfooz@chromium.org>
2023-12-13 20:58:01 +0000

    Protect against crashes in NotificationListView
    Use unique_ptrs for MessageViewContainer and it's children.
    Add a protective check to make sure we do not try to remove
    a message view that has already been removed.
    
    Unfortunately I'm not able to reproduce the crash so we do
    not have a regression test.
    
    Bug: b:315990620
    
    Change-Id: I8b9fa225498371160d4c45edc97a7c54bb1890b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5119996
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237174}

--- ash/system/notification_center/notification_list_view.h
+++ ash/system/notification_center/notification_list_view.h
@@ -158,7 +158,7 @@ class ASH_EXPORT NotificationListView
 
  protected:
   // Virtual for testing.
-  virtual message_center::MessageView* CreateMessageView(
+  virtual std::unique_ptr<message_center::MessageView> CreateMessageView(
       const message_center::Notification& notification);
 
   void ConfigureMessageView(message_center::MessageView* message_view);
