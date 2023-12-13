1b0ea94c70d2909c37615bdb62eabed9c0e322d7
Yao Xiao <yaoxia@chromium.org>
2023-12-13 16:49:39 +0000

    [CodeHealth] Remove expired UKM histograms
    OBSOLETE_HISTOGRAM[UKM.UserDemographics.Status]=No longer needed
    OBSOLETE_HISTOGRAM[UKM.ConsentObserver.AllowedForAllProfiles]=No longer needed
    
    Bug: 1508058,1508025
    Change-Id: If3b7ca065b493beefcf23b72818b7bcd2ba5a4cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5094900
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236984}

--- components/metrics/demographics/demographic_metrics_provider.cc
+++ components/metrics/demographics/demographic_metrics_provider.cc
@@ -153,7 +153,7 @@ void DemographicMetricsProvider::LogUserDemographicsStatusInHistogram(
                                 status == UserDemographicsStatus::kSuccess);
       return;
     case MetricsLogUploader::MetricServiceType::UKM:
-      base::UmaHistogramEnumeration("UKM.UserDemographics.Status", status);
+      // UKM Metrics doesn't have demographic metrics.
       return;
     case MetricsLogUploader::MetricServiceType::STRUCTURED_METRICS:
       // Structured Metrics doesn't have demographic metrics.
