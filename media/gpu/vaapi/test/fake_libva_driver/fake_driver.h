f83c46c608f3416fcc782bd6a3b1bcad78302702
Bijan Choobineh <bchoobineh@chromium.org>
2023-12-13 20:03:37 +0000

    Fake VAAPI VAImage Functionality
    Previously the Fake VAAPI driver was setup without implementing any
    VAImage logic. This resulted in not being able to use the
    VaapiDmaBufVideoFrameMapper as it required VAImage functionality.
    
    The update creates a new FakeImage class which represents a VAImage.
    This class is used to implement necessary VA functions needed for the
    VaapiDmaBufVideoFrameMapper. The frame mapper factory was also updated
    to no longer use the generic mapper and now use the Vaapi mapper for the
    Fake VAAPI Driver.
    
    BUG: b:307769458
    Test: Ran vda tests on crowd_run_256X144_fr15_bd8_sub8X8_l1.ivf
    
    Change-Id: Idc92f5c376cc5188dccdb31966edca777c4171dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5051547
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Auto-Submit: Bijan Choobineh‎ <bchoobineh@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237124}

--- media/gpu/vaapi/test/fake_libva_driver/fake_driver.h
+++ media/gpu/vaapi/test/fake_libva_driver/fake_driver.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/vaapi/test/fake_libva_driver/fake_buffer.h"
 #include "media/gpu/vaapi/test/fake_libva_driver/fake_config.h"
 #include "media/gpu/vaapi/test/fake_libva_driver/fake_context.h"
+#include "media/gpu/vaapi/test/fake_libva_driver/fake_image.h"
 #include "media/gpu/vaapi/test/fake_libva_driver/fake_surface.h"
 #include "media/gpu/vaapi/test/fake_libva_driver/object_tracker.h"
 #include "media/gpu/vaapi/test/fake_libva_driver/scoped_bo_mapping_factory.h"
@@ -63,6 +64,14 @@ class FakeDriver {
   const FakeBuffer& GetBuffer(FakeBuffer::IdType id);
   void DestroyBuffer(FakeBuffer::IdType id);
 
+  void CreateImage(const VAImageFormat& format,
+                   int width,
+                   int height,
+                   VAImage* va_image);
+  bool ImageExists(FakeImage::IdType id);
+  const FakeImage& GetImage(FakeImage::IdType id);
+  void DestroyImage(FakeImage::IdType id);
+
  private:
   // |scoped_bo_mapping_factory_| is used by FakeSurface to map BOs. It needs
   // to be declared before |surface_| since we pass a reference to
@@ -73,6 +82,10 @@ class FakeDriver {
   ObjectTracker<FakeSurface> surface_;
   ObjectTracker<FakeContext> context_;
   ObjectTracker<FakeBuffer> buffers_;
+
+  // The FakeImage instances in |images_| reference FakeBuffer instances in
+  // |buffers_|, so the latter should outlive the former.
+  ObjectTracker<FakeImage> images_;
 };
 
 }  // namespace media::internal
