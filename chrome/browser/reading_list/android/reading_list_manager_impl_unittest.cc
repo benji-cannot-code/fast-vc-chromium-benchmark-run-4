fb06182b1b82734c9cf2b937e081339418fe37e1
Brandon Wylie <wylieb@chromium.org>
2023-12-13 00:31:18 +0000

    [reading list] Allow ReadingListManager to share ids across instances
    Bug: 1501998
    Change-Id: I8a4f2f8b99971849ad01a57ea5509fc7c1e0d5f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5109666
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236682}

--- chrome/browser/reading_list/android/reading_list_manager_impl_unittest.cc
+++ chrome/browser/reading_list/android/reading_list_manager_impl_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/reading_list/android/reading_list_manager_impl.h"
 
+#include <cstdint>
 #include <memory>
 #include <string>
 #include <utility>
@@ -70,8 +71,10 @@ class ReadingListManagerImplTest : public testing::Test {
     reading_list_model_ = std::make_unique<ReadingListModelImpl>(
         std::move(storage), syncer::StorageType::kUnspecified,
         syncer::WipeModelUponSyncDisabledBehavior::kNever, &clock_);
-    manager_ =
-        std::make_unique<ReadingListManagerImpl>(reading_list_model_.get());
+    manager_ = std::make_unique<ReadingListManagerImpl>(
+        reading_list_model_.get(),
+        base::BindRepeating([](int64_t* id) { return (*id)++; },
+                            base::Owned(std::make_unique<int64_t>(0))));
     manager_->AddObserver(observer());
 
     return storage_ptr;
