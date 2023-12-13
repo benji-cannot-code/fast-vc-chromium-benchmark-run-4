ac98b5a6fbff69224353aede2477bd190a748873
Avi Drissman <avi@chromium.org>
2023-12-13 00:28:46 +0000

    Update Crashpad to 9f896f2581c076ec028935b8d9703551eb73a796
    337b4f797187 [mac] mach_absolute_time() ->
                 clock_gettime_nsec_np(CLOCK_UPTIME_RAW)
    9f896f2581c0 Qualify bit_cast with base::
    
    Bug: 1506769
    Change-Id: I5e7d5e69b4e6bc4b7150ad9aec415a8b7805aef6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5117342
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236680}

--- third_party/crashpad/crashpad/client/ios_handler/exception_processor.mm
+++ third_party/crashpad/crashpad/client/ios_handler/exception_processor.mm
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <type_traits>
 #include <typeinfo>
 
-#include "base/bit_cast.h"
 #include "base/format_macros.h"
 #include "base/logging.h"
 #include "base/memory/free_deleter.h"
--- third_party/crashpad/crashpad/util/linux/memory_map.cc
+++ third_party/crashpad/crashpad/util/linux/memory_map.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string.h>
 #include <sys/sysmacros.h>
 
-#include "base/bit_cast.h"
 #include "base/check_op.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
