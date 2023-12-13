94ae7ad31da595ec482c3b00af64ebf7b9719d41
Ted Meyer <tmathmeyer@chromium.org>
2023-12-13 20:21:30 +0000

    Add PipelineIntegrationTest for HLS content
    Bug: 1266991
    Bug: 1486731
    Change-Id: I4b902327f9a6ccd5515cc7e78af5a64e5b09af87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5113408
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237127}

--- media/test/pipeline_integration_test.cc
+++ media/test/pipeline_integration_test.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/test/bind.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "media/base/cdm_callback_promise.h"
@@ -3032,6 +3033,16 @@ TEST_F(PipelineIntegrationTest, BasicPlaybackHi10P) {
 
   ASSERT_TRUE(WaitUntilOnEnded());
 }
+
+#if BUILDFLAG(ENABLE_HLS_DEMUXER)
+TEST_F(PipelineIntegrationTest, BasicHlsManifestPlayback) {
+  base::test::ScopedFeatureList enable_hls{kBuiltInHlsPlayer};
+  ASSERT_EQ(PIPELINE_OK, StartPipelineWithHlsManifest("bear.m3u8"));
+  Play();
+  ASSERT_TRUE(WaitUntilOnEnded());
+  EXPECT_EQ("6bc0ecac3fea91d9591cb3197d28b196", GetVideoHash());
+}
+#endif
 
 // Verify that full-range H264 video has the right color space.
 TEST_F(PipelineIntegrationTest, Fullrange_H264) {
