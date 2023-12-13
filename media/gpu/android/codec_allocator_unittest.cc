5fea54c2d7408dcbaf07a1bc3793ec00dd85f2e4
Daniel Cheng <dcheng@chromium.org>
2023-12-13 15:09:45 +0000

    Fix error with pointer arithmetic on incomplete types in media tests.
    Newer version of Googletest attempt to pretty print the contents of a
    vector, but this requires the full definition of the type.
    
    Bug: 1409870
    Change-Id: I0d5da70a347cb9982659be79003716692865fd6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5117982
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236943}

--- media/gpu/android/codec_allocator_unittest.cc
+++ media/gpu/android/codec_allocator_unittest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/tick_clock.h"
 #include "base/time/time.h"
 #include "media/base/android/mock_media_codec_bridge.h"
+#include "media/base/subsample_entry.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
--- media/gpu/android/fake_codec_allocator.cc
+++ media/gpu/android/fake_codec_allocator.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/task/sequenced_task_runner.h"
 #include "media/base/android/mock_media_codec_bridge.h"
+#include "media/base/subsample_entry.h"
 #include "media/gpu/android/codec_allocator.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
