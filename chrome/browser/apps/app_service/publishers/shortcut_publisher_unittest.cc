276ab5315c98523372bc529b94d368d6e1ba29d4
Nancy Wang <nancylingwang@chromium.org>
2023-12-13 04:03:03 +0000

    Fix the broken tests when AppStorage is enabled(Part 9).
    When the kAppServiceStorage flag is enabled, AppService blocks the
    app publishers creating until it's ready, after reading from the
    AppStorage file. This affects some tests,e.g.
    ShortcutPublisherTest.LaunchShortcut_CallsCorrectPublisher.
    
    Modify the test code to wait for AppServiceProxy to be ready after
    reading the AppStorage file in the test setup phase.
    
    BUG=1385932, b:307623506
    
    Change-Id: If593c01c8b57c0f66dd8e9ed8b877147f31712ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5109296
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236778}

--- chrome/browser/apps/app_service/publishers/shortcut_publisher_unittest.cc
+++ chrome/browser/apps/app_service/publishers/shortcut_publisher_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_future.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
+#include "chrome/browser/apps/app_service/app_service_test.h"
 #include "chrome/browser/apps/app_service/publishers/app_publisher.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/test/base/testing_profile.h"
@@ -100,6 +101,7 @@ class ShortcutPublisherTest : public testing::Test {
     scoped_feature_list_.InitAndEnableFeature(
         chromeos::features::kCrosWebAppShortcutUiUpdate);
     profile_ = std::make_unique<TestingProfile>();
+    WaitForAppServiceProxyReady(proxy());
   }
 
   Profile* profile() { return profile_.get(); }
