0912206916dfcc5e577b8aa35596c0b34701bc6c
Ana Salazar <anasalazar@chromium.org>
2023-12-13 20:25:09 +0000

    Update loading ring color for promise apps
    Ring color for promise icons was updated due to contrast issues in
    light mode.
    
    go/cros-promise-spec
    
    Bug: b:310056794
    Change-Id: I952c73c7a38c6870ec8ed6a36e8c244d0ea7deeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5120192
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237130}

--- ash/app_list/views/app_list_item_view.cc
+++ ash/app_list/views/app_list_item_view.cc
@@ -2184,8 +2184,7 @@ void AppListItemView::UpdateProgressIndicatorState() {
     progress_indicator_->SetColorId(cros_tokens::kCrosSysHighlightShape);
     progress_indicator_->SetOuterRingTrackVisible(true);
   } else {
-    progress_indicator_->SetColorId(
-        cros_tokens::kCrosSysSystemPrimaryContainer);
+    progress_indicator_->SetColorId(cros_tokens::kCrosSysPrimary);
     progress_indicator_->SetOuterRingTrackVisible(false);
   }
 
