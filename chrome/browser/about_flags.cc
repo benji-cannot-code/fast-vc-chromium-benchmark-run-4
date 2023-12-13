999889f3b8206a0bdd7919c5402fb7833f8dc471
Alexander Cooper <alcooper@chromium.org>
2023-12-13 01:04:05 +0000

    Remove WebXR Cardboard Flag
    The WebXR Cardboard runtime was enabled in M119 and has since hit
    stable with no reported issues. As such, and given that the flag to
    enable it expires in M122, remove the flag that would allow disabling
    this feature.
    
    Note that the runtime can still be overridden via the
    "force-webxr-runtime" flag.
    
    Fixed: 1508553
    Change-Id: I9a922f3d6187b3ddeee0e32d96a99191b857e064
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5109553
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236704}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -303,10 +303,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/cocoa/screentime/screentime_features.h"
 #endif  // BUILDFLAG(IS_MAC)
 
-#if BUILDFLAG(ENABLE_CARDBOARD)
-#include "device/vr/public/cpp/features.h"
-#endif  // ENABLE_CARDBOARD
-
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 #include "chrome/browser/extensions/cws_info_service.h"
 #include "extensions/common/extension_features.h"
@@ -3977,11 +3973,6 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kVerboseLoggingInNaclDescription, kOsAll,
      MULTI_VALUE_TYPE(kVerboseLoggingInNaclChoices)},
 #endif  // ENABLE_NACL
-#if BUILDFLAG(ENABLE_CARDBOARD)
-    {"enable-cardboard", flag_descriptions::kEnableCardboardName,
-     flag_descriptions::kEnableCardboardDescription, kOsAndroid,
-     FEATURE_VALUE_TYPE(device::features::kEnableCardboard)},
-#endif  // ENABLE_CARDBOARD
 #if BUILDFLAG(ENABLE_EXTENSIONS)
     {"extensions-on-chrome-urls",
      flag_descriptions::kExtensionsOnChromeUrlsName,
