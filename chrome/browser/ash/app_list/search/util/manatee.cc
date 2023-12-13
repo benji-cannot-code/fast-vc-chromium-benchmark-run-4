d68831dc99f0edf7a1332929c493569796913f95
Negeen Daudi <negeend@google.com>
2023-12-13 04:45:45 +0000

    Util file for manatee
    Test: out/Default/unit_tests --gtest_filter="*ManateeTest*"
    Bug: b/314868265
    Change-Id: I799804e594acaef9e3a5d8ac50ec23d07f24faa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5102532
    Reviewed-by: Lauren Commeignes <laurencom@chromium.org>
    Reviewed-by: Amanda Deacon <amandadeacon@chromium.org>
    Commit-Queue: Negeen Daudi <negeend@google.com>
    Cr-Commit-Position: refs/heads/main@{#1236785}

---
+++ chrome/browser/ash/app_list/search/util/manatee.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/app_list/search/util/manatee.h"
+
+#include <numeric>
+#include <optional>
+#include <vector>
+
+namespace app_list {
+
+std::optional<double> GetWordSimilarity(const std::vector<double>& vector1,
+                                        const std::vector<double>& vector2) {
+  if (vector1.size() != vector2.size()) {
+    return std::nullopt;
+  }
+  return std::inner_product(vector1.begin(), vector1.end(), vector2.begin(), 0);
+}
+
+}  // namespace app_list
