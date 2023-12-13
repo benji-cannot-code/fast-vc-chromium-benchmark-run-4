ed6cdafeb90ad3f0443612fb0b095a4bb45bc330
Andrew Wolfers <aswolfers@chromium.org>
2023-12-13 02:11:13 +0000

    Add vulkan disable params for non-Android
    Bug: b:292249282
    Change-Id: Iac275b2f1147f830057c7b51d1ab64e6e6e7159a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5111680
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Andrew Wolfers <aswolfers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236741}

--- gpu/vulkan/vulkan_util.cc
+++ gpu/vulkan/vulkan_util.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_ANDROID)
 #include "base/android/build_info.h"
-#include "gpu/config/gpu_finch_features.h"  //nogncheck
 #endif
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -46,8 +45,6 @@ namespace gpu {
 
 namespace {
 
-#if BUILDFLAG(IS_ANDROID)
-
 bool IsDeviceBlocked(base::StringPiece field, base::StringPiece block_list) {
   auto disable_patterns = base::SplitString(
       block_list, "|", base::TRIM_WHITESPACE, base::SPLIT_WANT_ALL);
@@ -58,6 +55,8 @@ bool IsDeviceBlocked(base::StringPiece field, base::StringPiece block_list) {
   }
   return false;
 }
+
+#if BUILDFLAG(IS_ANDROID)
 
 int GetEMUIVersion() {
   const auto* build_info = base::android::BuildInfo::GetInstance();
@@ -410,9 +409,11 @@ VkResult VulkanQueuePresentKHRHook(VkQueue queue,
   return vkQueuePresentKHR(queue, pPresentInfo);
 }
 
-bool CheckVulkanCompabilities(const VulkanInfo& vulkan_info,
-                              const GPUInfo& gpu_info,
-                              std::string enable_by_device_name) {
+bool CheckVulkanCompatibilities(const VulkanInfo& vulkan_info,
+                                const GPUInfo& gpu_info,
+                                const std::string& enable_by_device_name,
+                                const std::string& disable_by_renderer,
+                                const std::string& disable_by_driver) {
 // Android uses AHB and SyncFD for interop. They are imported into GL with other
 // API.
 #if !BUILDFLAG(IS_ANDROID)
@@ -426,8 +427,16 @@ bool CheckVulkanCompabilities(const VulkanInfo& vulkan_info,
   constexpr char kMemoryObjectExtension[] = "GL_EXT_memory_object_fd";
   constexpr char kSemaphoreExtension[] = "GL_EXT_semaphore_fd";
 #endif
+  if (IsDeviceBlocked(gpu_info.gl_renderer, disable_by_renderer)) {
+    return false;
+  }
+
+  if (IsDeviceBlocked(gpu_info.gpu.driver_version, disable_by_driver)) {
+    return false;
+  }
+
   // If Chrome and ANGLE share the same VkQueue, they can share vulkan
-  // resource without those extensions. 
+  // resource without those extensions.
   if (!base::FeatureList::IsEnabled(features::kVulkanFromANGLE)) {
     // If both Vulkan and GL are using native GPU (non swiftshader), check
     // necessary extensions for GL and Vulkan interop.
@@ -463,17 +472,11 @@ bool CheckVulkanCompabilities(const VulkanInfo& vulkan_info,
       return true;
   }
 
-  const base::FeatureParam<std::string> disable_patterns(
-      &features::kVulkan, "disable_by_gl_renderer", "");
-
-  if (IsDeviceBlocked(gpu_info.gl_renderer, disable_patterns.Get())) {
+  if (IsDeviceBlocked(gpu_info.gl_renderer, disable_by_renderer)) {
     return false;
   }
 
-  const base::FeatureParam<std::string> disable_driver_patterns(
-      &features::kVulkan, "disable_by_gl_driver", "");
-  if (IsDeviceBlocked(gpu_info.gpu.driver_version,
-                      disable_driver_patterns.Get())) {
+  if (IsDeviceBlocked(gpu_info.gpu.driver_version, disable_by_driver)) {
     return false;
   }
 
