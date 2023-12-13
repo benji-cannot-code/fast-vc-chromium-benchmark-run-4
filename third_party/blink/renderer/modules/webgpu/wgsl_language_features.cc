3acd239b79824795723649b218ad61da5dbf4df7
Corentin Wallez <cwallez@chromium.org>
2023-12-13 22:43:53 +0000

    webgpu: Collect the wgslLanguageFeatures from dawn::wire.
    This CL correctly collects the WGSL language features from the dawn::wire
    Instance object to populate gpu.wgslLanguageFeatures but does so in
    hacky way by creating a temporary dawn::wire::WireClient object. This is
    to work around the getter being synchronous, while the creation of the
    DawnControlClient (which holds the thread's WGPUInstance) is
    asynchronous.
    
    Bug: dawn:2260
    Change-Id: I9e993e3aa41b98c5b29aca4bb0591859a72c7c59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5099506
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237237}

--- third_party/blink/renderer/modules/webgpu/wgsl_language_features.cc
+++ third_party/blink/renderer/modules/webgpu/wgsl_language_features.cc
@@ -5,9 +5,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/webgpu/wgsl_language_features.h"
 
+#include "third_party/blink/renderer/modules/webgpu/dawn_enum_conversions.h"
+
 namespace blink {
 
-WGSLLanguageFeatures::WGSLLanguageFeatures() = default;
+WGSLLanguageFeatures::WGSLLanguageFeatures(
+    const std::vector<WGPUWGSLFeatureName>& features) {
+  for (const auto& dawn_feature : features) {
+    V8WGSLFeatureName v8_feature{
+        V8WGSLFeatureName::Enum::kPointerCompositeAccess};
+    if (FromDawnEnum(dawn_feature, &v8_feature)) {
+      features_.insert(v8_feature.AsString());
+    }
+  }
+}
 
 bool WGSLLanguageFeatures::has(const String& feature) const {
   return features_.Contains(feature);
--- third_party/blink/renderer/platform/graphics/gpu/dawn_control_client_holder.h
+++ third_party/blink/renderer/platform/graphics/gpu/dawn_control_client_holder.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <dawn/dawn_proc_table.h>
 #include <dawn/webgpu.h>
+#include <vector>
 
 #include "gpu/command_buffer/client/webgpu_interface.h"
 #include "third_party/blink/renderer/platform/graphics/gpu/webgpu_resource_provider_cache.h"
@@ -76,6 +77,13 @@ class PLATFORM_EXPORT DawnControlClientHolder
 
   base::WeakPtrFactory<DawnControlClientHolder> weak_ptr_factory_{this};
 };
+
+// Slightly hacky way to get the wgslLanguageFeatures without accessing the
+// DawnControlClient because it is initialized asynchronously on workers.
+// TODO(crbug.com/1246805): Remove this hack when the DawnControlClient can be
+// initialized synchronously on workers and query from its WGPUInstance
+// instead.
+PLATFORM_EXPORT std::vector<WGPUWGSLFeatureName> GatherWGSLFeatures();
 
 }  // namespace blink
 
