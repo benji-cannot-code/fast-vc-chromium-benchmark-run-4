6d8bc155e630338b342f136a9b90aa05fddfd67b
Yao Xiao <yaoxia@chromium.org>
2023-12-13 16:03:37 +0000

    Fix FeatureParam<std::string>::Get()
    Issue: When the underlying string is empty, the current method
    mistakenly considers it to be a "missing key" case and returns the
    default_value.
    
    This CL fixes this issue by explicitly checking for the "missing key"
    case.
    
    Bug: 1510989
    Change-Id: I5580215a2556c23ca5b9833e132ae92dc27804a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5117559
    Reviewed-by: Jack Yammine <jyammine@google.com>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236969}

--- chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_service_browsertest.cc
+++ chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_service_browsertest.cc
@@ -3262,9 +3262,7 @@ IN_PROC_BROWSER_TEST_F(GooglePFTestFieldTrialOverride,
 class GooglePFTestDefaultFieldTrialValue : public GooglePFTest {
  public:
   GooglePFTestDefaultFieldTrialValue() {
-    feature_list_.InitAndEnableFeatureWithParameters(
-        kSearchNavigationPrefetch,
-        {{"suggest_prefetch_param", ""}, {"navigation_prefetch_param", ""}});
+    feature_list_.InitAndEnableFeature(kSearchNavigationPrefetch);
   }
 
  private:
