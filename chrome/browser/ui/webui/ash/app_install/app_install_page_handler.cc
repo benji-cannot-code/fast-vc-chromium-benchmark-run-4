ca2a8f95ab5a4de27344e33bcfdce2e8c179be53
Louise Brett <loubrett@google.com>
2023-12-13 06:47:41 +0000

    Add "open app" functionality from install dialog
    When the app is installed from the dialog, callers must pass in
    the app_id if the install was successful.
    
    Bug: b/307146278
    Change-Id: I80b1bae5a7e0b497658f1d39b5c4f198a1a46153
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5114039
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Louise Brett <loubrett@google.com>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236807}

--- chrome/browser/ui/webui/ash/app_install/app_install_page_handler.cc
+++ chrome/browser/ui/webui/ash/app_install/app_install_page_handler.cc
@@ -5,16 +5,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/ash/app_install/app_install_page_handler.h"
 
+#include "chrome/browser/apps/app_service/app_service_proxy.h"
+#include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/ui/webui/ash/app_install/app_install.mojom.h"
+#include "components/services/app_service/public/cpp/app_launch_util.h"
 
 namespace ash::app_install {
 
 AppInstallPageHandler::AppInstallPageHandler(
+    Profile* profile,
     mojom::DialogArgsPtr args,
     base::OnceCallback<void(bool accepted)> dialog_accepted_callback,
     mojo::PendingReceiver<mojom::PageHandler> pending_page_handler,
     CloseDialogCallback close_dialog_callback)
-    : dialog_args_{std::move(args)},
+    : profile_{profile},
+      dialog_args_{std::move(args)},
       dialog_accepted_callback_{std::move(dialog_accepted_callback)},
       receiver_{this, std::move(pending_page_handler)},
       close_dialog_callback_{std::move(close_dialog_callback)} {}
@@ -42,10 +47,22 @@ void AppInstallPageHandler::InstallApp(InstallAppCallback callback) {
   std::move(dialog_accepted_callback_).Run(true);
 }
 
-void AppInstallPageHandler::OnInstallComplete(bool success) {
-  if (install_app_callback_) {
-    std::move(install_app_callback_).Run(success);
+void AppInstallPageHandler::OnInstallComplete(const std::string* app_id) {
+  if (app_id) {
+    app_id_ = *app_id;
   }
+  if (install_app_callback_) {
+    std::move(install_app_callback_).Run(/*success=*/app_id);
+  }
+}
+
+void AppInstallPageHandler::LaunchApp() {
+  if (app_id_.empty()) {
+    mojo::ReportBadMessage("Unable to launch app without an app_id.");
+    return;
+  }
+  apps::AppServiceProxyFactory::GetForProfile(profile_)->Launch(
+      app_id_, ui::EF_NONE, apps::LaunchSource::kFromInstaller);
 }
 
 }  // namespace ash::app_install
