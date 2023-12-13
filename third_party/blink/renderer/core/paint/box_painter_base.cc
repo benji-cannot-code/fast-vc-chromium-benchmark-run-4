734439fa4f41a309b71a1f3820f4835a0f3a5c56
Fredrik Söderquist <fs@opera.com>
2023-12-13 10:33:30 +0000

    Split out context information from BackgroundImageGeometry
    This moves the functionality that depends on the box/fragment/other
    entity that the image geometry is computed for. The context class is
    named BoxBackgroundPaintContext.
    
    This is an as verbatim code movement operation as possible given the
    circumstances. In a future CL the context object will be completely
    separated from BackgroundImageGeometry, and users will be changed to
    create an instances of BoxBackgroundPaintContext instead.
    
    Bug: 1500572
    Change-Id: I7a2478b064a88a4279ab0bd35c1e72d49eeb2a54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5094017
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1236872}

--- third_party/blink/renderer/core/paint/box_painter_base.cc
+++ third_party/blink/renderer/core/paint/box_painter_base.cc
@@ -814,7 +814,7 @@ inline bool CanUseBottomLayerFastPath(const BoxPainterBase::FillLayerInfo& info,
   DCHECK(info.should_paint_color || info.should_paint_image);
 
   // Painting a background image from an ancestor onto a cell is a complex case.
-  if (geometry.CellUsingContainerBackground()) {
+  if (geometry.GetContext().CellUsingContainerBackground()) {
     return false;
   }
   // Complex cases not handled on the fast path.
@@ -1184,7 +1184,7 @@ void BoxPainterBase::PaintFillLayer(const PaintInfo& paint_info,
       composite_op = SkBlendMode::kSrcOver;
     }
 
-    const ComputedStyle& image_style = geometry.ImageStyle(style_);
+    const ComputedStyle& image_style = geometry.GetContext().ImageStyle(style_);
 
     // If the "image" referenced by the FillLayer is an SVG <mask> reference
     // (and this is a layer for a mask), then repeat, position, clip, origin and
@@ -1192,8 +1192,9 @@ void BoxPainterBase::PaintFillLayer(const PaintInfo& paint_info,
     if (bg_layer.GetType() == EFillLayerType::kMask) {
       if (const auto* mask_source =
               ToMaskSourceIfSVGMask(*fill_layer_info.image)) {
-        const PhysicalRect positioning_area = geometry.ComputePositioningArea(
-            paint_info, bg_layer, scrolled_paint_rect);
+        const PhysicalRect positioning_area =
+            geometry.GetContext().ComputePositioningArea(paint_info, bg_layer,
+                                                         scrolled_paint_rect);
         const gfx::RectF reference_box(gfx::SizeF(positioning_area.size));
         const float zoom = image_style.EffectiveZoom();
 
@@ -1202,15 +1203,18 @@ void BoxPainterBase::PaintFillLayer(const PaintInfo& paint_info,
         context.Translate(positioning_area.X().ToFloat(),
                           positioning_area.Y().ToFloat());
         SVGMaskPainter::PaintSVGMaskLayer(
-            context, *mask_source, geometry.ImageClient(), reference_box, zoom,
-            composite_op, bg_layer.MaskMode() == EFillMaskMode::kMatchSource);
+            context, *mask_source, geometry.GetContext().ImageClient(),
+            reference_box, zoom, composite_op,
+            bg_layer.MaskMode() == EFillMaskMode::kMatchSource);
         return;
       }
     }
+    DCHECK_GE(document_.Lifecycle().GetState(),
+              DocumentLifecycle::kPrePaintClean);
     geometry.Calculate(paint_info, bg_layer, scrolled_paint_rect);
 
-    image = fill_layer_info.image->GetImage(geometry.ImageClient(), document_,
-                                            image_style,
+    image = fill_layer_info.image->GetImage(geometry.GetContext().ImageClient(),
+                                            document_, image_style,
                                             gfx::SizeF(geometry.TileSize()));
 
     image_rendering_settings_context.emplace(context,
@@ -1236,12 +1240,12 @@ void BoxPainterBase::PaintFillLayer(const PaintInfo& paint_info,
 
   absl::optional<RoundedInnerRectClipper> clip_to_border;
   if (fill_layer_info.is_rounded_fill) {
-    DCHECK(!geometry.CanCompositeBackgroundAttachmentFixed());
+    DCHECK(!geometry.GetContext().CanCompositeBackgroundAttachmentFixed());
     clip_to_border.emplace(context, rect, border_rect);
   }
 
   if (bg_layer.Clip() == EFillBox::kText) {
-    DCHECK(!geometry.CanCompositeBackgroundAttachmentFixed());
+    DCHECK(!geometry.GetContext().CanCompositeBackgroundAttachmentFixed());
     PaintFillLayerTextFillBox(paint_info, fill_layer_info, image.get(),
                               composite_op, geometry, rect, scrolled_paint_rect,
                               object_has_multiple_boxes);
@@ -1250,7 +1254,7 @@ void BoxPainterBase::PaintFillLayer(const PaintInfo& paint_info,
 
   // We use BackgroundClip paint property when CanFastScrollFixedAttachment().
   absl::optional<GraphicsContextStateSaver> background_clip_state_saver;
-  if (!geometry.CanCompositeBackgroundAttachmentFixed()) {
+  if (!geometry.GetContext().CanCompositeBackgroundAttachmentFixed()) {
     switch (bg_layer.Clip()) {
       case EFillBox::kFillBox:
       // Spec: For elements with associated CSS layout box, the used values for
