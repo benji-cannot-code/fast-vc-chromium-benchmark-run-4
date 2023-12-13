5935477b73539d1aaf9be8cc6fbe60fded948d25
Cam Bickel <cambickel@google.com>
2023-12-13 21:39:19 +0000

    firmware_update: Update "waiting for user" dialog to match spec
    This CL updates the dialog to match the new spec, based on recent
    conversations with the UX team.
    (Discussion here: http://shortn/_jIne8GiVFY)
    
    First of all, I've added a min-height to the update dialog so that
    the dialog height doesn't change so much when the dialog content
    changes.
    
    Second, the "waiting for user" screens now display the current
    progress, but with a greyed-out bar and "Paused (XX%)" text.
    
    I've also updated the update dialog tests to make more use of
    "isVisible", in addition to the new test I've added.
    
    Screenshot of new dialog:
    https://screenshot.googleplex.com/8bsDWyNQkVQ7v2o
    
    Screencast of new dialog:
    https://drive.google.com/file/d/1srPwSTIbWSQZbiEbXDIMPcNgD85joq4E/view?usp=sharing&resourcekey=0-4TskuY6LPDUYVljcnTZzjg
    
    Test: browser_tests
    Test: Tested on my VM + DUT
    Bug: b/308669841
    Change-Id: I94119d535d016345f27f059cc349a95c3b62d3fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5097806
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Camden Bickel <cambickel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1237192}

--- chrome/test/data/webui/chromeos/firmware_update/firmware_update_dialog_test.ts
+++ chrome/test/data/webui/chromeos/firmware_update/firmware_update_dialog_test.ts
@@ -127,11 +127,11 @@ suite('FirmwareUpdateDialogTest', () => {
         getTextContent('#progress'),
         loadTimeData.getString('restartingFooterText'));
     // Check that the indeterminate progress is shown.
-    assertTrue(!!updateDialogElement.shadowRoot.querySelector(
-        '#indeterminateProgressBar'));
+    assertTrue(isVisible(updateDialogElement.shadowRoot.querySelector(
+        '#indeterminateProgressBar')));
     // No percentage progress bar.
-    assertFalse(
-        !!updateDialogElement.shadowRoot.querySelector('#updateProgressBar'));
+    assertFalse(isVisible(
+        updateDialogElement.shadowRoot.querySelector('#updateProgressBar')));
   });
 
   test('UpdateDialogContent', async () => {
@@ -172,11 +172,11 @@ suite('FirmwareUpdateDialogTest', () => {
         getTextContent('#progress'),
         loadTimeData.getString('restartingFooterText'));
     // Check that the indeterminate progress is shown.
-    assertTrue(!!updateDialogElement.shadowRoot.querySelector(
-        '#indeterminateProgressBar'));
+    assertTrue(isVisible(updateDialogElement.shadowRoot.querySelector(
+        '#indeterminateProgressBar')));
     // No percentage progress bar.
-    assertFalse(
-        !!updateDialogElement.shadowRoot.querySelector('#updateProgressBar'));
+    assertFalse(isVisible(
+        updateDialogElement.shadowRoot.querySelector('#updateProgressBar')));
   });
 
   test('ProgressBarAppears', async () => {
@@ -226,11 +226,11 @@ suite('FirmwareUpdateDialogTest', () => {
         getTextContent('#progress'),
         loadTimeData.getString('restartingFooterText'));
     // Check that the indeterminate progress is shown.
-    assertTrue(!!updateDialogElement.shadowRoot.querySelector(
-        '#indeterminateProgressBar'));
+    assertTrue(isVisible(updateDialogElement.shadowRoot.querySelector(
+        '#indeterminateProgressBar')));
     // No percentage progress bar.
-    assertFalse(
-        !!updateDialogElement.shadowRoot.querySelector('#updateProgressBar'));
+    assertFalse(isVisible(
+        updateDialogElement.shadowRoot.querySelector('#updateProgressBar')));
   });
 
   test('UpdateDialogContent_DeviceRequest_V2Disabled', async () => {
@@ -376,13 +376,20 @@ suite('FirmwareUpdateDialogTest', () => {
       assertEquals(
           getTextContent('#updateDialogBody'),
           loadTimeData.getString(expectedString));
+      assertEquals(
+          getTextContent('#progress'),
+          loadTimeData.getStringF('waitingFooterText', 70));
 
-      // Check that the indeterminate progress is shown.
-      assertTrue(!!updateDialogElement.shadowRoot.querySelector(
-          '#indeterminateProgressBar'));
-      // No percentage progress bar.
-      assertFalse(
-          !!updateDialogElement.shadowRoot.querySelector('#updateProgressBar'));
+      // Percentage progress should be shown when waiting for user action, but
+      // the bar should be disabled.
+      assertTrue(isVisible(
+          updateDialogElement.shadowRoot.querySelector('#updateProgressBar')));
+      assertTrue(
+          !!updateDialogElement.shadowRoot.querySelector('#updateProgressBar')!
+                .hasAttribute('disabled'));
+      // Indeterminate progress should not be shown.
+      assertFalse(isVisible(updateDialogElement.shadowRoot.querySelector(
+          '#indeterminateProgressBar')));
     }
   });
 });
