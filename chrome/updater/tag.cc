94d448d5bc5647987c22b592c5e41998aabaa0a5
S. Ganesh <ganesh@chromium.org>
2023-12-13 02:42:29 +0000

    Use a memory mapped file when reading the tag from the binary
    A memory-mapped file is more efficient as compared to reading the entire
    file into memory, since it does not affect commit size.
    
    Fixed: 1472820
    Change-Id: I679d296b18689158484794f570cf49517d362cc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5118388
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236750}

--- chrome/updater/tag.cc
+++ chrome/updater/tag.cc
@@ -15,9 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/contains.h"
+#include "base/containers/span.h"
 #include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
+#include "base/files/memory_mapped_file.h"
 #include "base/logging.h"
 #include "base/no_destructor.h"
 #include "base/strings/escape.h"
@@ -765,7 +767,7 @@ std::string ReadTag(std::vector<uint8_t>::const_iterator begin,
 
 std::unique_ptr<tagging::BinaryInterface> CreateBinary(
     const base::FilePath& file,
-    const std::vector<uint8_t>& contents) {
+    base::span<const uint8_t> contents) {
   if (file.MatchesExtension(FILE_PATH_LITERAL(".exe"))) {
     return CreatePEBinary(contents);
   } else if (file.MatchesExtension(FILE_PATH_LITERAL(".msi"))) {
@@ -785,10 +787,13 @@ std::string BinaryReadTagString(const base::FilePath& file) {
     return ParseTagBuffer(ReadFileTail(file));
   }
 
-  // TODO(crbug.com/1472820): Can we do similar for EXEs, or can we consider PE
-  // parsing to be a safer implementation?
-  const std::vector<uint8_t> contents = ReadEntireFile(file);
-  std::unique_ptr<tagging::BinaryInterface> bin = CreateBinary(file, contents);
+  base::MemoryMappedFile mapped_file;
+  if (!mapped_file.Initialize(file)) {
+    LOG(ERROR) << __func__ << ": Unknown or empty file: " << file;
+    return {};
+  }
+  std::unique_ptr<tagging::BinaryInterface> bin =
+      CreateBinary(file, mapped_file.bytes());
   if (!bin) {
     LOG(ERROR) << __func__ << ": Could not parse binary: " << file;
     return {};
