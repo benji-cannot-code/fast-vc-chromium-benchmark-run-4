01264b0c4f6eb23957cb46e3d0bb8a34b6293033
Wei4 Wang <wei4.wang@intel.com>
2023-12-14 03:40:58 +0000

    [ComputePressure] Add PressureServiceForWorker.
    This CL adds a class named PressureServiceForWorker which holds
    functions only for workers.
    
    Bug: 1500467
    Change-Id: Ief9d92dc470c141846a1fe9d2701980a70363e93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4951586
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Wei4 Wang <wei4.wang@intel.com>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237362}

--- content/browser/worker_host/shared_worker_host.h
+++ content/browser/worker_host/shared_worker_host.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/unguessable_token.h"
 #include "content/browser/browser_interface_broker_impl.h"
 #include "content/browser/buckets/bucket_context.h"
+#include "content/browser/compute_pressure/pressure_service_for_worker.h"
 #include "content/browser/renderer_host/code_cache_host_impl.h"
 #include "content/browser/renderer_host/policy_container_host.h"
 #include "content/common/content_export.h"
@@ -32,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/remote.h"
 #include "mojo/public/cpp/bindings/unique_receiver_set.h"
 #include "net/base/network_isolation_key.h"
+#include "services/device/public/mojom/pressure_manager.mojom.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
 #include "services/network/public/cpp/cross_origin_embedder_policy.h"
 #include "services/network/public/mojom/url_loader_factory.mojom-forward.h"
@@ -144,6 +146,8 @@ class CONTENT_EXPORT SharedWorkerHost : public blink::mojom::SharedWorkerHost,
       mojo::PendingReceiver<blink::mojom::BlobURLStore> receiver);
   void CreateBucketManagerHost(
       mojo::PendingReceiver<blink::mojom::BucketManagerHost> receiver);
+  void BindPressureService(
+      mojo::PendingReceiver<device::mojom::PressureManager> receiver);
 
   // Causes this instance to be deleted, which will terminate the worker. May
   // be done based on a UI action.
@@ -318,6 +322,8 @@ class CONTENT_EXPORT SharedWorkerHost : public blink::mojom::SharedWorkerHost,
   std::set<blink::mojom::WebFeature> used_features_;
 
   std::unique_ptr<SharedWorkerContentSettingsProxyImpl> content_settings_;
+
+  std::unique_ptr<PressureServiceForWorker<SharedWorkerHost>> pressure_service_;
 
   // This is kept alive during the lifetime of the shared worker, since it's
   // associated with Mojo interfaces (ServiceWorkerContainer and
