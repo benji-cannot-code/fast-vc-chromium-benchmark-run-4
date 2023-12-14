f15661e94e8bb537e807e38870378f4eb6c983ad
sophiewen <sophiewen@chromium.org>
2023-12-14 20:54:11 +0000

    snap-groups: Fix SplitViewDivider resizing
    This CL does the following:
    - Moves `SetTransform()` from overview_utils to window_util
    - Adds a WindowProperty `kExcludeFromTransientTreeTransformKey` to
    exclude SplitViewDivider from transient transforms during splitview
    resizing
    
    Demo: https://buganizer.corp.google.com/issues/313574803#comment8
    
    Test: manual
    Bug: b/313574803
    Change-Id: I0da2e35afe9c5bdbc42b1320903b5a98e5c10f41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5114159
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237706}

--- ash/wm/overview/overview_item.cc
+++ ash/wm/overview/overview_item.cc
@@ -798,7 +798,7 @@ void OverviewItem::OnOverviewItemContinuousScroll(
   } else {
     gfx::Transform transform = gfx::Tween::TransformValueBetween(
         scroll_ratio, gfx::Transform(), target_transform);
-    SetTransform(window, transform);
+    window_util::SetTransform(window, transform);
   }
 }
 
@@ -1188,7 +1188,7 @@ void OverviewItem::PerformItemSpawnedAnimation(
   // window.
   gfx::Transform initial_transform = target_transform;
   initial_transform.Scale(kInitialScaler, kInitialScaler);
-  SetTransform(window, initial_transform);
+  window_util::SetTransform(window, initial_transform);
   transform_window_.SetOpacity(kInitialScaler);
 
   ScopedOverviewTransformWindow::ScopedAnimationSettings animation_settings;
@@ -1205,7 +1205,7 @@ void OverviewItem::PerformItemSpawnedAnimation(
         base::BindOnce(&OverviewItem::OnItemSpawnedAnimationCompleted,
                        weak_ptr_factory_.GetWeakPtr())});
   }
-  SetTransform(window, target_transform);
+  window_util::SetTransform(window, target_transform);
   transform_window_.SetOpacity(kTargetScaler);
 
   if (cannot_snap_widget_) {
@@ -1265,7 +1265,7 @@ void OverviewItem::SetItemBounds(const gfx::RectF& target_bounds,
         base::BindOnce(&OverviewItem::OnItemBoundsAnimationEnded,
                        weak_ptr_factory_.GetWeakPtr())});
   }
-  SetTransform(window, transform);
+  window_util::SetTransform(window, transform);
   transform_window_.SetClipping(clip_rect);
 }
 
--- ash/wm/overview/scoped_overview_transform_window_unittest.cc
+++ ash/wm/overview/scoped_overview_transform_window_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test/ash_test_base.h"
 #include "ash/wm/overview/overview_utils.h"
 #include "ash/wm/window_state.h"
+#include "ash/wm/window_util.h"
 #include "base/numerics/safe_conversions.h"
 #include "ui/aura/window.h"
 #include "ui/compositor/layer.h"
@@ -264,7 +265,7 @@ TEST_F(ScopedOverviewTransformWindowTest, InvisibleTransients) {
   EXPECT_FALSE(child2->IsVisible());
 
   auto transform = gfx::Transform::MakeTranslation(10.f, 10.f);
-  SetTransform(window.get(), transform);
+  window_util::SetTransform(window.get(), transform);
   EXPECT_EQ(transform, window->transform());
   EXPECT_EQ(transform, child->transform());
   EXPECT_TRUE(child2->transform().IsIdentity());
