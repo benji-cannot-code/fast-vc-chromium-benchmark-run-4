14035cac1a84812ca451e43f7b27cf659b515b6d
Nathan Muggli <nmuggli@google.com>
2023-12-14 00:09:10 +0000

    documentScan: Add startScan and cancelScan
    documentScan.startScan will start a scan for an already open scanner
    handle, after confirming that the user wants to allow the scan.
    
    All the real work is done in the previously-added DocumentScanAPIHandler
    function.  This CL adds the JS-facing API function and matching
    apitests.
    
    Bug: b:297435705
    Change-Id: I2a59274dab29c70dc54c114e18a5337a15763a03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5086027
    Commit-Queue: Nathan Muggli <nmuggli@google.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Benjamin Gordon <bmgordon@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237275}

---
+++ chrome/test/data/extensions/api_test/document_scan/start_scan_denied.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// These tests should be run with the get scanner list permission allowed and
+// the start scan permission denied (and the extension should not be trusted).
+chrome.test.runTests([
+  async function startScanDenied() {
+    const scannerHandle = await getScannerHandle();
+    chrome.test.assertNe(null, scannerHandle);
+
+    const options = {
+      format: 'format',
+    };
+    chrome.documentScan.startScan(scannerHandle, options, response => {
+      chrome.test.assertEq(chrome.documentScan.OperationResult.ACCESS_DENIED,
+                           response.result);
+      chrome.test.assertEq(scannerHandle, response.scannerHandle);
+      chrome.test.assertEq(null, response.job);
+      chrome.test.succeed();
+    });
+  }
+]);
