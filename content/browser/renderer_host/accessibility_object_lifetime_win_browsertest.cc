988efce7e63f473c9c86934b85de6be5ecd5be66
Mike Jackson <mjackson@microsoft.com>
2023-12-14 21:38:31 +0000

    Fix dangling pointer in AccessibilityObjectLifetimeWinBrowserTest
    The class we have a reference to is deleted during the WM_NCDESTROY message, which we never receive because we remove the filter during the WM_DESTROY message. Consequently, this is our last opportunity to clean up `legacy_render_widget_host_HWND_`
    
    Bug: None
    Change-Id: I66bd16a3ba6fff5c023ca2061557409a489a213e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5122085
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Mike Jackson <mjackson@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1237750}

--- content/browser/renderer_host/accessibility_object_lifetime_win_browsertest.cc
+++ content/browser/renderer_host/accessibility_object_lifetime_win_browsertest.cc
@@ -131,6 +131,7 @@ class AccessibilityTeardownTestMessageFilter : public ui::HWNDMessageFilter {
       // Verify that the legacy window does not crash when asked for an
       // accessibility object.
       legacy_render_widget_host_HWND_->GetOrCreateWindowRootAccessible(false);
+      legacy_render_widget_host_HWND_ = nullptr;
 
       // Remove ourselves as a subclass.
       ui::HWNDSubclass::RemoveFilterFromAllTargets(this);
@@ -140,8 +141,7 @@ class AccessibilityTeardownTestMessageFilter : public ui::HWNDMessageFilter {
   }
 
  private:
-  raw_ptr<LegacyRenderWidgetHostHWND, DanglingUntriaged>
-      legacy_render_widget_host_HWND_;
+  raw_ptr<LegacyRenderWidgetHostHWND> legacy_render_widget_host_HWND_;
 };
 
 IN_PROC_BROWSER_TEST_F(AccessibilityObjectLifetimeWinBrowserTest,
