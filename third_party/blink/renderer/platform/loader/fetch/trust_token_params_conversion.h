38852e905eea767a56ed202ded68e8d2bb4d9e89
Sophey Dong <sophey@chromium.org>
2023-12-14 00:05:20 +0000

    Revert "Remove version from mojom TrustTokenParams"
    This reverts commit 4a07ee3a471ebda51f6da62b3ac37c7d0fc8dbb9.
    
    Reason for revert: https://crbug.com/1511379, TrustTokenToMojomTest.BadVersion is failing.
    
    Original change's description:
    > Remove version from mojom TrustTokenParams
    >
    > Fixed: 1419873
    > Change-Id: I8e9af17093d5256521af1bdf7b1151c3955daf11
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5062632
    > Reviewed-by: Aykut Bulut <aykutb@google.com>
    > Reviewed-by: Bo Liu <boliu@chromium.org>
    > Reviewed-by: Dominic Farolino <dom@chromium.org>
    > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > Auto-Submit: Jonathan Feinberg <feinberg@google.com>
    > Reviewed-by: Nate Chapin <japhet@chromium.org>
    > Commit-Queue: Jonathan Feinberg <feinberg@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1237006}
    
    Change-Id: I85c5f3172bfb42ac3127cc2df5f2e40cf2707657
    Bug: 1511379
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5120905
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Owners-Override: Sophey Dong <sophey@chromium.org>
    Auto-Submit: Sophey Dong <sophey@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1237272}

--- third_party/blink/renderer/platform/loader/fetch/trust_token_params_conversion.h
+++ third_party/blink/renderer/platform/loader/fetch/trust_token_params_conversion.h
@@ -10,9 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/public/platform/web_common.h"
 
-namespace network::mojom::blink {
+namespace network {
+namespace mojom {
+namespace blink {
 class TrustTokenParams;
-}  // namespace network::mojom::blink
+
+}  // namespace blink
+}  // namespace mojom
+}  // namespace network
 
 namespace blink {
 
