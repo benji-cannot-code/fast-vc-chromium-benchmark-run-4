96fa203e0cb0d7a6dc09810035e9cea036c1c4be
Kevin Lubick <kjlubick@google.com>
2023-12-14 18:11:11 +0000

    Remove final use of legacy SkFontMgr methods
    This fixed one case of overloading a deprecated method (font_loader)
    and one case of implicitly depending on the default font manager
    by depending on the default SkTypeface deserialization logic.
    Other places, e.g. printing [1], already explicitly passed in
    the system font manager.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:printing/common/metafile_utils.cc;l=302-333;drc=24da48fcb484a544fe2be720172ec56af19532c2
    
    Bug: b:305780908
    Change-Id: I2778be72deb59d14abceb119f2ff7e0c5b3b3b90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5105288
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Kevin Lubick <kjlubick@chromium.org>
    Commit-Queue: Kevin Lubick <kjlubick@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237611}

--- components/services/font/public/cpp/font_loader.cc
+++ components/services/font/public/cpp/font_loader.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "components/services/font/public/cpp/font_service_thread.h"
 #include "pdf/buildflags.h"
-#include "skia/ext/font_utils.h"
 #include "third_party/skia/include/core/SkFontMgr.h"
 
 namespace font_service {
@@ -65,9 +64,10 @@ SkStreamAsset* FontLoader::openStream(const FontIdentity& identity) {
   }
 }
 
-sk_sp<SkTypeface> FontLoader::makeTypeface(const FontIdentity& identity) {
+sk_sp<SkTypeface> FontLoader::makeTypeface(const FontIdentity& identity,
+                                           sk_sp<SkFontMgr> mgr) {
   TRACE_EVENT0("fonts", "FontServiceThread::makeTypeface");
-  return SkFontConfigInterface::makeTypeface(identity, skia::DefaultFontMgr());
+  return SkFontConfigInterface::makeTypeface(identity, mgr);
 }
 
 // Additional cross-thread accessible methods.
