48767117e6066008247ec6ce744953fe33f2d2bd
Mei Liang <meiliang@chromium.org>
2023-12-14 21:20:52 +0000

    [CodeHealth] Rewrite LegacyFindInPageTest.NavigationByKeyEvent to Kombucha
    As part of Test Code Health Rotation (go/test-health-rotation-instructions)
    
    Bug: 1509945
    Change-Id: Ice7817c9e540d6919fda38ba96c1a1810ba84d79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5121001
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237742}

--- chrome/browser/ui/views/find_bar_view.cc
+++ chrome/browser/ui/views/find_bar_view.cc
@@ -66,6 +66,8 @@ void SetCommonButtonAttributes(views::ImageButton* button) {
 
 DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(FindBarView, kElementId);
 DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(FindBarView, kTextField);
+DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(FindBarView, kPreviousButtonElementId);
+DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(FindBarView, kNextButtonElementId);
 
 class FindBarMatchCountLabel : public views::Label {
  public:
@@ -225,6 +227,8 @@ FindBarView::FindBarView(FindBarHost* host) {
               .SetAccessibleName(
                   l10n_util::GetStringUTF16(IDS_ACCNAME_PREVIOUS))
               .SetID(VIEW_ID_FIND_IN_PAGE_PREVIOUS_BUTTON)
+              .SetProperty(views::kElementIdentifierKey,
+                           kPreviousButtonElementId)
               .SetTooltipText(
                   l10n_util::GetStringUTF16(IDS_FIND_IN_PAGE_PREVIOUS_TOOLTIP))
               .SetCallback(base::BindRepeating(&FindBarView::FindNext,
@@ -234,6 +238,7 @@ FindBarView::FindBarView(FindBarHost* host) {
               .CopyAddressTo(&find_next_button_)
               .SetAccessibleName(l10n_util::GetStringUTF16(IDS_ACCNAME_NEXT))
               .SetID(VIEW_ID_FIND_IN_PAGE_NEXT_BUTTON)
+              .SetProperty(views::kElementIdentifierKey, kNextButtonElementId)
               .SetTooltipText(
                   l10n_util::GetStringUTF16(IDS_FIND_IN_PAGE_NEXT_TOOLTIP))
               .SetCallback(base::BindRepeating(&FindBarView::FindNext,
