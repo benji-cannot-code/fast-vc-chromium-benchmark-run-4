3b4d59b88043ecfd296c5bc93a0433a84fe8aa4a
David Bienvenu <davidbienvenu@chromium.org>
2023-12-14 15:27:44 +0000

    Fix ProfileKeyedServiceBrowserTest in branded builds
    This is a partial revert of crrev.com/c/5085710, which
    crrev.com/c/5083205 inadvertently broke.
    
    Per behamilton, crrev.com/c/5083205 prevents KAnonymityService factory
    from getting created in the test, so the expectations need to be
    changed.
    
    Bug: 1506961
    Change-Id: I702b9ee530cfd3661d25f81a9a6a0b547930af54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5105160
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237522}

--- chrome/browser/profiles/profile_keyed_service_browsertest.cc
+++ chrome/browser/profiles/profile_keyed_service_browsertest.cc
@@ -247,9 +247,6 @@ IN_PROC_BROWSER_TEST_F(ProfileKeyedServiceBrowserTest,
 #endif
     "HidDeviceManager",
     "HostContentSettingsMap",
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-    "KAnonymityServiceFactory",
-#endif
     "MediaRouter",
     "MediaRouterUIService",
     "NotificationDisplayService",
@@ -429,9 +426,6 @@ IN_PROC_BROWSER_TEST_F(ProfileKeyedServiceBrowserTest,
     "InstallVerifier",
     "InstanceIDProfileService",
     "InvalidationService",
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-    "KAnonymityServiceFactory",
-#endif // BUILDFLAG(GOOGLE_CHROME_BRANDING)
     "LanguageSettingsPrivateDelegate",
     "LazyBackgroundTaskQueue",
 #if BUILDFLAG(ENABLE_SUPERVISED_USERS)
