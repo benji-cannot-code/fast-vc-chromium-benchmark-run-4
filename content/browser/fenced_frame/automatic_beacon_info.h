662cc9f380b0240391c069ce2c49274232868e11
Liam Brady <lbrady@google.com>
2023-12-14 17:37:15 +0000

    Fix automatic beacon data not overwriting old beacon data.
    Automatic beacon data should be allowed to overwrite existing data. At
    the moment, that does not happen. This CL fixes that behavior to match
    what's expected in both the explainer and the spec.
    
    Bug: 1511352
    Change-Id: I23dd768b0105d139e3f369ed3c45d57d3c913b0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5120549
    Commit-Queue: Liam Brady <lbrady@google.com>
    Reviewed-by: Garrett Tanzer <gtanzer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237573}

--- content/browser/fenced_frame/automatic_beacon_info.h
+++ content/browser/fenced_frame/automatic_beacon_info.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 struct CONTENT_EXPORT AutomaticBeaconInfo {
+  AutomaticBeaconInfo();
+
   AutomaticBeaconInfo(
       const std::string& data,
       const std::vector<blink::FencedFrame::ReportingDestination>& destinations,
