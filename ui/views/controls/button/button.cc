233a7bf0c8cc3da8889a98ccff1917ca0ee9fa15
Caroline Rising <corising@chromium.org>
2023-12-14 15:41:41 +0000

    [sidepanelpinning] Make button check anchor count when made visible.
    This issue is exposed when we add the anchor highlight before the button
    has been made visible due to animations. This change checks the buttons
    anchor count if visibility changes to true to update the highlight
    accordingly.
    
    This also fixes another bug where |anchor_count_| is not respected when
    a drag finishes.
    
    Bug: b:313474579, 1510641
    Change-Id: I367cfafc01442336bc27682e534dc069b7520366
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5113040
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237527}

--- ui/views/controls/button/button.cc
+++ ui/views/controls/button/button.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/animation/ink_drop.h"
 #include "ui/views/animation/ink_drop_highlight.h"
 #include "ui/views/animation/ink_drop_impl.h"
+#include "ui/views/animation/ink_drop_state.h"
 #include "ui/views/controls/button/button_controller.h"
 #include "ui/views/controls/button/button_controller_delegate.h"
 #include "ui/views/controls/button/checkbox.h"
@@ -232,6 +233,12 @@ Button::ButtonState Button::GetState() const {
 
 void Button::SetState(ButtonState state) {
   if (state == state_) {
+    if (anchor_count_ > 0) {
+      // Update the highlight for the |anchor_count_| even if the state hasn't
+      // changed. This catches cases where button visibility might have changed
+      // but the state hasn't.
+      SetHighlighted(true);
+    }
     return;
   }
 
@@ -396,10 +403,15 @@ void Button::SetFocusPainter(std::unique_ptr<Painter> focus_painter) {
 }
 
 void Button::SetHighlighted(bool highlighted) {
-  InkDrop::Get(ink_drop_view_)
-      ->AnimateToState(highlighted ? views::InkDropState::ACTIVATED
-                                   : views::InkDropState::DEACTIVATED,
-                       nullptr);
+  // Do nothing if the ink drop's target state matches what we are trying to set
+  // since same state transitions may restart animations.
+  InkDropState state = highlighted ? views::InkDropState::ACTIVATED
+                                   : views::InkDropState::DEACTIVATED;
+  if (InkDrop::Get(ink_drop_view_)->GetInkDrop()->GetTargetInkDropState() ==
+      state) {
+    return;
+  }
+  InkDrop::Get(ink_drop_view_)->AnimateToState(state, nullptr);
 }
 
 Button::ScopedAnchorHighlight Button::AddAnchorHighlight() {
@@ -586,8 +598,12 @@ void Button::OnDragDone() {
   if (state_ != STATE_DISABLED) {
     SetState(STATE_NORMAL);
   }
-  InkDrop::Get(ink_drop_view_)
-      ->AnimateToState(InkDropState::HIDDEN, nullptr /* event */);
+  if (anchor_count_ > 0) {
+    SetHighlighted(true);
+  } else {
+    InkDrop::Get(ink_drop_view_)
+        ->AnimateToState(InkDropState::HIDDEN, nullptr /* event */);
+  }
 }
 
 void Button::OnPaint(gfx::Canvas* canvas) {
