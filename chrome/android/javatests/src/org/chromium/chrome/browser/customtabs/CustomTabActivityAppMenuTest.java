a77a80301a8196d5390a22415ce71b081039cc8c
Zach Katz <katzz@google.com>
2023-12-14 21:40:47 +0000

    [Fixit] Restrict CustomTabActivityAppMenuTest#testShareMenuItem from Android Auto
    The share menu item is disabled for Android Auto and the test should be too.[1]
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:chrome/android/java/src/org/chromium/chrome/browser/share/ShareUtils.java;l=36?q=customtabs%20automotive
    
    Change-Id: I56952cbcd37d1689907b2677b06cfa0c23c39671
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5121948
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Geoff Huang <geoffhuang@google.com>
    Commit-Queue: Zach Katz <katzz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1237754}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityAppMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityAppMenuTest.java
@@ -55,6 +55,7 @@ import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.base.test.util.PackageManagerWrapper;
+import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.app.metrics.LaunchCauseMetrics;
 import org.chromium.chrome.browser.browserservices.intents.BrowserServicesIntentDataProvider;
 import org.chromium.chrome.browser.browserservices.intents.BrowserServicesIntentDataProvider.CustomTabsUiType;
@@ -77,6 +78,7 @@ import org.chromium.components.webapps.WebappsUtils;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
 import org.chromium.ui.modelutil.PropertyModel;
+import org.chromium.ui.test.util.DeviceRestriction;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -440,6 +442,7 @@ public class CustomTabActivityAppMenuTest {
     /** Tests if the default share item can be shown in the app menu. */
     @Test
     @SmallTest
+    @Restriction(DeviceRestriction.RESTRICTION_TYPE_NON_AUTO)
     public void testShareMenuItem() throws Exception {
         Intent intent = createMinimalCustomTabIntent();
         intent.putExtra(CustomTabsIntent.EXTRA_DEFAULT_SHARE_MENU_ITEM, true);
