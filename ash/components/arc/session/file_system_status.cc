e68afb6d1bee43d945f7804c3bc5772f0eaad7e4
Avi Drissman <avi@chromium.org>
2023-12-14 18:37:13 +0000

    Switch to using bit_cast
    It's possible to use bit_cast here instead of reinterpret_cast, so do
    so.
    
    Bug: none
    Change-Id: Ib4a38dcec98808b57a758b5b4107309e2811aa68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5117152
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237637}

--- ash/components/arc/session/file_system_status.cc
+++ ash/components/arc/session/file_system_status.cc
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <linux/magic.h>
 #include <sys/statvfs.h>
 
+#include <array>
 #include <string>
 
+#include "base/bit_cast.h"
 #include "base/containers/span.h"
 #include "base/files/file_util.h"
 #include "base/logging.h"
@@ -68,13 +70,13 @@ bool FileSystemStatus::IsSystemImageExtFormat(const base::FilePath& path) {
     return false;
   }
 
-  uint8_t buf[2];
-  if (!file.ReadAndCheck(0x400 + 0x38, base::make_span(buf, sizeof(buf)))) {
+  std::array<uint8_t, 2> buf;
+  if (!file.ReadAndCheck(0x400 + 0x38, base::make_span(buf))) {
     PLOG(ERROR) << "File read error on system image file: " << path.value();
     return false;
   }
 
-  uint16_t magic_le = *reinterpret_cast<uint16_t*>(buf);
+  uint16_t magic_le = base::bit_cast<uint16_t>(buf);
 #if defined(ARCH_CPU_LITTLE_ENDIAN)
   return magic_le == EXT4_SUPER_MAGIC;
 #else
