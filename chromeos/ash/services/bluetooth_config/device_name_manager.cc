f8edde68107f237d0be55ffdd8838a8e335d807e
Chad Duffin <chadduffin@chromium.org>
2023-12-14 21:49:31 +0000

    bluetooth: Migrate BlueZ nicknames to Floss
    This change introduces the logic necessary to migrate the nicknames of
    Bluetooth devices from the BlueZ format to the Floss format. For more
    information see
    https://docs.google.com/document/d/1CwojQaGOFuAvgak4UPD6BRz-N8j_nyVtQBewWXml3T0/edit?resourcekey=0-2Ii-SFuX3gJd7pHBfgTl3w&tab=t.0.
    
    Floss disabled: https://drive.google.com/file/d/1rtIA8gX7a_b1K87SRW2sxznijwIJw94l/view?usp=sharing
    Floss enabled: https://drive.google.com/file/d/1wHU4AHs7wbiuAvWlpaHR8x1xDVO5_u03/view?usp=sharing
    Floss re-disabled: https://drive.google.com/file/d/1FtX_6lOfhonvoLEGLE0eTJbh4S_kC9dG/view?usp=sharing
    
    Bug: b/304520576
    Test: Unit tests and verified on DUT.
    Change-Id: I69bf045c2dd3e003a86a3829f98f14d2ad2995af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5119372
    Reviewed-by: Hsin-chen Chuang <chharry@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237756}

--- chromeos/ash/services/bluetooth_config/device_name_manager.cc
+++ chromeos/ash/services/bluetooth_config/device_name_manager.cc
@@ -7,6 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash::bluetooth_config {
 
+const char DeviceNameManager::kDeviceIdToNicknameMapPrefName[] =
+    "bluetooth.floss_device_id_to_nickname_map";
+
+const char DeviceNameManager::kDeviceIdToNicknameMapPrefNameLegacy[] =
+    "bluetooth.device_id_to_nickname_map";
+
 DeviceNameManager::DeviceNameManager() = default;
 
 DeviceNameManager::~DeviceNameManager() = default;
