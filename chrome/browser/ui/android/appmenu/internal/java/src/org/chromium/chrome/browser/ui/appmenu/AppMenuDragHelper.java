9e106824f5dc5bf8f0f346584f0738e40046681a
punithnayak <npunith125@gmail.com>
2023-12-14 19:47:10 +0000

    Remove expired histograms WrenchMenu.TouchDuration
    Bug: 1508003
    Change-Id: I968185fb1d3878326ade726a436148276a73c8b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5116512
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Commit-Queue: Punith Nayak <npunith125@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1237683}

--- chrome/browser/ui/android/appmenu/internal/java/src/org/chromium/chrome/browser/ui/appmenu/AppMenuDragHelper.java
+++ chrome/browser/ui/android/appmenu/internal/java/src/org/chromium/chrome/browser/ui/appmenu/AppMenuDragHelper.java
@@ -20,7 +20,6 @@ import android.widget.ListView;
 import androidx.annotation.IntDef;
 import androidx.annotation.VisibleForTesting;
 
-import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.browser.ui.appmenu.internal.R;
 
@@ -172,8 +171,6 @@ class AppMenuDragHelper {
         if (eventActionMasked == MotionEvent.ACTION_CANCEL) {
             mAppMenu.dismiss();
             return true;
-        } else if (eventActionMasked == MotionEvent.ACTION_UP) {
-            RecordHistogram.recordTimesHistogram("WrenchMenu.TouchDuration", timeSinceDown);
         }
 
         mIsSingleTapCanceled |= timeSinceDown > mTapTimeout;
