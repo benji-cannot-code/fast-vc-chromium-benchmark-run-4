becaf539d6f545b425af5a1bfdc80f1cd30cca72
Patrick Monette <pmonette@chromium.org>
2023-12-14 04:34:44 +0000

    Fix naming for FrameNodeImpl::outer_document_for_fenced_frame
    During development, the name of this variable changed but the old name
    was not replaced everywhere. This led to the current situation where
    the same variable has 2 names depending on which file it is.
    
    Change-Id: I8c0bffa4f478785fd7bfbeabc0e2089f1adb12a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5116902
    Auto-Submit: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1237375}

--- components/performance_manager/v8_memory/v8_detailed_memory_unittest.cc
+++ components/performance_manager/v8_memory/v8_detailed_memory_unittest.cc
@@ -584,13 +584,13 @@ TEST_F(V8DetailedMemoryDecoratorTest, MultipleIsolatesInRenderer) {
   blink::LocalFrameToken frame1_id = blink::LocalFrameToken();
   auto frame1 = CreateNode<FrameNodeImpl>(
       process.get(), page.get(), /*parent_frame_node=*/nullptr,
-      /*fenced_frame_embedder_frame_node=*/nullptr, /*render_frame_id=*/1,
+      /*outer_document_for_fenced_frame=*/nullptr, /*render_frame_id=*/1,
       frame1_id);
 
   blink::LocalFrameToken frame2_id = blink::LocalFrameToken();
   auto frame2 = CreateNode<FrameNodeImpl>(
       process.get(), page.get(), /*parent_frame_node=*/nullptr,
-      /*fenced_frame_embedder_frame_node=*/nullptr,
+      /*outer_document_for_fenced_frame=*/nullptr,
       /*render_frame_id=*/2, frame2_id);
   {
     auto data = NewPerProcessV8MemoryUsage(2);
@@ -642,13 +642,13 @@ TEST_F(V8DetailedMemoryDecoratorTest, DataIsDistributed) {
   blink::LocalFrameToken frame1_id = blink::LocalFrameToken();
   auto frame1 = CreateNode<FrameNodeImpl>(
       process.get(), page.get(), /*parent_frame_node=*/nullptr,
-      /*fenced_frame_embedder_frame_node=*/nullptr,
+      /*outer_document_for_fenced_frame=*/nullptr,
       /*render_frame_id=*/1, frame1_id);
 
   blink::LocalFrameToken frame2_id = blink::LocalFrameToken();
   auto frame2 = CreateNode<FrameNodeImpl>(
       process.get(), page.get(), /*parent_frame_node=*/nullptr,
-      /*fenced_frame_embedder_frame_node=*/nullptr,
+      /*outer_document_for_fenced_frame=*/nullptr,
       /*render_frame_id=*/2, frame2_id);
   {
     auto data = NewPerProcessV8MemoryUsage(1);
@@ -1970,7 +1970,7 @@ TEST_F(V8DetailedMemoryDecoratorTest, DedicatedWorkers) {
   blink::LocalFrameToken frame_id = blink::LocalFrameToken();
   auto frame = CreateNode<FrameNodeImpl>(
       process.get(), page.get(), /*parent_frame_node=*/nullptr,
-      /*fenced_frame_embedder_frame_node=*/nullptr,
+      /*outer_document_for_fenced_frame=*/nullptr,
       /*render_frame_id=*/1, frame_id);
 
   blink::DedicatedWorkerToken worker_id = blink::DedicatedWorkerToken();
@@ -2015,7 +2015,7 @@ TEST_F(V8DetailedMemoryDecoratorTest, CanvasMemory) {
   blink::LocalFrameToken frame_id = blink::LocalFrameToken();
   auto frame = CreateNode<FrameNodeImpl>(
       process.get(), page.get(), /*parent_frame_node=*/nullptr,
-      /*fenced_frame_embedder_frame_node=*/nullptr,
+      /*outer_document_for_fenced_frame=*/nullptr,
       /*render_frame_id=*/1, frame_id);
 
   {
