2175261559d2fd70c73d14efff7b48d547c85d5f
Gauthier Ambard <gambard@chromium.org>
2023-12-14 18:03:30 +0000

    [iOS] Handle potential duplicate TabGrid entering animation
    It seems that the animation to enter the tab grid can be triggered
    twice in a row and the check to know if the BVC is presented or not
    is only updated at the end of the animation.
    
    Fixed: 1506555, 1508881, 1508885
    Change-Id: I886238fef0a0db7f807e4b5be900a8360cd10404
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5122534
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237605}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/transitions/legacy_grid_transition_layout.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/transitions/legacy_grid_transition_layout.mm
@@ -76,11 +76,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)populateWithSnapshotsFromView:(UIView*)view middleRect:(CGRect)rect {
-  if (!view) {
-    // TODO(crbug.com/1506555): Temporary investigation to see if there is a
-    // misconfiguration in the transition.
-    base::debug::DumpWithoutCrashing();
-  }
   self.cell.mainTabView = [view resizableSnapshotViewFromRect:rect
                                            afterScreenUpdates:YES
                                                 withCapInsets:UIEdgeInsetsZero];
