f6a381c23cd456b8d6ad678ab7084376e38e62de
conniekxu <conniekxu@chromium.org>
2023-12-14 02:00:11 +0000

    CrOS Settings: Use slot instead of creating a temporary element
    Use slot could achieve the same effect and make the code more clear.
    
    Screenshot: https://screenshot.googleplex.com/5TCKXv7KE4bPD29
    
    Bug: b/314982773
    Change-Id: I9cd181a924fd57e52f4f58826120b48a4a69e7d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5118979
    Commit-Queue: Connie Xu <conniekxu@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1237333}

--- chrome/test/data/webui/settings/chromeos/os_files_page/files_settings_card_test.ts
+++ chrome/test/data/webui/settings/chromeos/os_files_page/files_settings_card_test.ts
@@ -82,12 +82,11 @@ suite('<files-settings-card>', () => {
     await flushTasks();
   }
 
-  function getGoogleDriveRow(): CrLinkRowElement {
-    const googleDriveRow =
-        filesSettingsCard.shadowRoot!.querySelector<CrLinkRowElement>(
-            '#googleDriveRow')!;
-    assert(googleDriveRow);
-    return googleDriveRow;
+  function getGoogleDriveRowSubLabel(): HTMLElement {
+    const subLabel =
+        filesSettingsCard.shadowRoot!.getElementById('googleDriveSubLabel');
+    assert(subLabel);
+    return subLabel;
   }
 
   async function assertSubpageTriggerFocused(
@@ -337,9 +336,7 @@ suite('<files-settings-card>', () => {
           await createFilesSettingsCard();
           filesSettingsCard.setPrefValue('gdata.disabled', true);
           flush();
-
-          const googleDriveRow = getGoogleDriveRow();
-          assertEquals('Not signed in', googleDriveRow.subLabel);
+          assertEquals('Not signed in', getGoogleDriveRowSubLabel().innerText);
         });
 
     test(
@@ -349,8 +346,8 @@ suite('<files-settings-card>', () => {
           filesSettingsCard.setPrefValue('drivefs.bulk_pinning_enabled', false);
           flush();
 
-          const googleDriveRow = getGoogleDriveRow();
-          assertTrue(googleDriveRow.subLabel.startsWith('Signed in as'));
+          assertTrue(
+              getGoogleDriveRowSubLabel().innerText.startsWith('Signed in as'));
         });
 
     test(
@@ -359,8 +356,7 @@ suite('<files-settings-card>', () => {
           filesSettingsCard.setPrefValue('drivefs.bulk_pinning_enabled', true);
           flush();
 
-          const googleDriveRow = getGoogleDriveRow();
-          assertEquals('File sync on', googleDriveRow.subLabel);
+          assertEquals('File sync on', getGoogleDriveRowSubLabel().innerText);
         });
 
     test('cycling through the prefs updates the sublabel texts', async () => {
@@ -369,16 +365,16 @@ suite('<files-settings-card>', () => {
       filesSettingsCard.setPrefValue('drivefs.bulk_pinning_enabled', false);
       flush();
 
-      const googleDriveRow = getGoogleDriveRow();
-      assertEquals('Not signed in', googleDriveRow.subLabel);
+      const googleDriveRowSubLabel = getGoogleDriveRowSubLabel();
+      assertEquals('Not signed in', googleDriveRowSubLabel.innerText);
 
       filesSettingsCard.setPrefValue('gdata.disabled', false);
       flush();
-      assertTrue(googleDriveRow.subLabel.startsWith('Signed in as'));
+      assertTrue(googleDriveRowSubLabel.innerText.startsWith('Signed in as'));
 
       filesSettingsCard.setPrefValue('drivefs.bulk_pinning_enabled', true);
       flush();
-      assertEquals('File sync on', googleDriveRow.subLabel);
+      assertEquals('File sync on', googleDriveRowSubLabel.innerText);
     });
   });
 
@@ -399,12 +395,12 @@ suite('<files-settings-card>', () => {
       filesSettingsCard.setPrefValue('gdata.disabled', true);
       flush();
 
-      const googleDriveRow = getGoogleDriveRow();
-      assertEquals('Not signed in', googleDriveRow.subLabel);
+      const googleDriveRowSubLabel = getGoogleDriveRowSubLabel();
+      assertEquals('Not signed in', googleDriveRowSubLabel.innerText);
 
       filesSettingsCard.setPrefValue('gdata.disabled', false);
       flush();
-      assertTrue(googleDriveRow.subLabel.startsWith('Signed in as'));
+      assertTrue(googleDriveRowSubLabel.innerText.startsWith('Signed in as'));
     });
 
     test(
