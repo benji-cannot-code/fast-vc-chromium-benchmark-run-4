c02b7c17397f49e7b0985496a16e390c85fd2629
Norge Vizcay <vizcay@google.com>
2023-12-14 09:11:48 +0000

    Use the updated StartUploadRequest in AutofillCrowdsourcingManager tests
    Bug: 1505969
    Change-Id: I35e611cadd9c91d8f6cc3d1689d7d3e546d19bb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5117545
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1237435}

--- components/autofill/core/browser/crowdsourcing/autofill_crowdsourcing_manager_test_api.h
+++ components/autofill/core/browser/crowdsourcing/autofill_crowdsourcing_manager_test_api.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
+// Friend class to AutofillCrowdsourcingManager to allow tests to interact with
+// and query the internal components of the class.
 class AutofillCrowdsourcingManagerTestApi {
  public:
   static std::unique_ptr<AutofillCrowdsourcingManager> CreateManagerForApiKey(
