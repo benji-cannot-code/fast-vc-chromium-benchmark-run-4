b92ae80ac86ad5780817ae37cb58c4adfaa64e7d
John Chen <johnchen@chromium.org>
2023-12-14 01:18:40 +0000

    Prepare moving bots for win-10_amd_laptop-perf
    The bots for win-10_amd_laptop-perf will be moved from MTV to SFO.
    win-2-b220 has already been moved and renamed to win-141-e504.
    The rest of the bots will be moved in the next few days.
    
    Making two config changes in preparation for the move:
    * Slightly relax OS version filter. The OS is expected to change from
      Windows-10-19043.1052 to Windows-10-19043.2364 after the move.
    * Reduce shard count from 5 to 3. The plan is to move 5 bots at a time,
      so that only 5 bots are available sometimes. As it turns out, we were
      only using 3 shards any way, even though 5 shards were allocated.
    
    Change-Id: Ib59618bcd0f2bf11e40f2a32a2acfff01b29668d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5121074
    Commit-Queue: John Chen <johnchen@chromium.org>
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1237313}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -928,7 +928,7 @@ BUILDERS = {
             # that we can be informed if this
             # version ever changes or becomes inconsistent. It is important
             # that bots are homogeneous. See crbug.com/988045 for history.
-            'os': 'Windows-10-19043.1052',
+            'os': 'Windows-10-19043',
             'gpu': '1002:1638-30.0.13033.1000',
             'synthetic_product_name': 'OMEN by HP Laptop 16-c0xxx [ ] (HP)',
         },
@@ -952,7 +952,7 @@ BUILDERS = {
             # that we can be informed if this
             # version ever changes or becomes inconsistent. It is important
             # that bots are homogeneous. See crbug.com/988045 for history.
-            'os': 'Windows-10-19043.1052',
+            'os': 'Windows-10-19043',
             'gpu': '1002:1638-30.0.13033.1000',
             'synthetic_product_name': 'OMEN by HP Laptop 16-c0xxx [ ] (HP)',
         },
