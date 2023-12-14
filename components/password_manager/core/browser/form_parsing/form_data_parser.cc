56c77181c69a1ed2e62844ee2b0807b649da2fe4
Din Nezametdinov <shaikhitdin@google.com>
2023-12-14 17:39:42 +0000

    [Passwords] Don't show save prompt on passwords of length one
    Some of the PIN and OTP codes are split into 1 digit fields.
    Currently, one of the digits is suggested in the save prompt.
    Don't suggest save prompt if password's length is equal to one.
    
    
    Bug: 
    Change-Id: I02fa8950e452d31973b2310a451338c18f8f5196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5112399
    Commit-Queue: Din Nezametdinov <shaikhitdin@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1237577}

--- components/password_manager/core/browser/form_parsing/form_data_parser.cc
+++ components/password_manager/core/browser/form_parsing/form_data_parser.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/autofill_regex_constants.h"
 #include "components/autofill/core/common/autofill_regexes.h"
 #include "components/autofill/core/common/form_data.h"
+#include "components/autofill/core/common/form_field_data.h"
 #include "components/autofill/core/common/unique_ids.h"
 #include "components/password_manager/core/browser/features/password_features.h"
 #include "components/password_manager/core/browser/password_form.h"
@@ -1072,6 +1073,10 @@ std::unique_ptr<PasswordForm> AssemblePasswordForm(
   return result;
 }
 
+bool FieldValueIsTooShortForSaving(const FormFieldData* field) {
+  return field && GetFieldValue(*field).size() <= 1;
+}
+
 }  // namespace
 
 FormDataParser::FormDataParser() = default;
@@ -1196,6 +1201,15 @@ FormDataParser::ParseAndReturnUsernameDetection(
 
   base::UmaHistogramEnumeration("PasswordManager.UsernameDetectionMethod",
                                 method);
+
+  // OTPs and PIN codes are usually split across several 1-digit fields.
+  // Don't automatically show Save/Update prompt in such case.
+  if (mode == Mode::kSaving &&
+      ((!significant_fields.new_password &&
+        FieldValueIsTooShortForSaving(significant_fields.password)) ||
+       FieldValueIsTooShortForSaving(significant_fields.new_password))) {
+    significant_fields.is_fallback = true;
+  }
 
   return {
       AssemblePasswordForm(form_data, significant_fields,
