a7e2c197cd85c81a995e4ca4ebc953a2a76b4dee
Christoph Schwering <schwering@google.com>
2023-12-14 19:44:48 +0000

    [Autofill] Rename ToSafeServerFieldType() to ToSafeFieldType()
    Produced by
    $ git ls-files | \
      grep '\.\(h\|cc\|mm\|idl\|ts\|js\|java\)$' | \
      xargs grep -l 'ServerFieldType' | \
      xargs sed --in-place -s 's/ToSafeServerFieldType/ToSafeFieldType/g'
    
    Bug: 1511368, 1007974, 1488408
    Change-Id: I0996dc363e5791bc1e86c0f6a966e34393b3adbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5120695
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237679}

--- components/autofill/core/browser/form_structure.cc
+++ components/autofill/core/browser/form_structure.cc
@@ -380,7 +380,7 @@ std::string ServerTypesToString(const AutofillField* field) {
       buffer << ", ";
     }
     ServerFieldType server_type =
-        ToSafeServerFieldType(field_prediction.type(), NO_SERVER_DATA);
+        ToSafeFieldType(field_prediction.type(), NO_SERVER_DATA);
     buffer << FieldTypeToStringView(server_type);
   }
   return "[" + buffer.str() + "]";
@@ -391,7 +391,7 @@ bool HasPasswordManagerPrediction(const FieldSuggestion& field_suggestion) {
   return base::ranges::any_of(
       field_suggestion.predictions(), [](const auto& prediction) {
         auto group_type = GroupTypeOfServerFieldType(
-            ToSafeServerFieldType(prediction.type(), NO_SERVER_DATA));
+            ToSafeFieldType(prediction.type(), NO_SERVER_DATA));
         return group_type == FieldTypeGroup::kPasswordField ||
                group_type == FieldTypeGroup::kUsernameField;
       });
@@ -405,7 +405,7 @@ void MergePasswordManagerPredictions(
   CHECK_NE(&merge_to_predictions, &merge_from_predictions);
   for (const auto& prediction : merge_from_predictions.predictions()) {
     FieldTypeGroup group_type = GroupTypeOfServerFieldType(
-        ToSafeServerFieldType(prediction.type(), NO_SERVER_DATA));
+        ToSafeFieldType(prediction.type(), NO_SERVER_DATA));
     // Only add predictions relevant for PasswordManager.
     if (group_type == FieldTypeGroup::kPasswordField ||
         group_type == FieldTypeGroup::kUsernameField) {
@@ -898,8 +898,8 @@ void FormStructure::ProcessQueryResponse(
                                     : field->server_predictions()[0].source(),
           .server_type2 =
               field->server_predictions().size() >= 2
-                  ? ToSafeServerFieldType(field->server_predictions()[1].type(),
-                                          NO_SERVER_DATA)
+                  ? ToSafeFieldType(field->server_predictions()[1].type(),
+                                    NO_SERVER_DATA)
                   : NO_SERVER_DATA,
           .prediction_source2 = field->server_predictions().size() >= 2
                                     ? field->server_predictions()[1].source()
