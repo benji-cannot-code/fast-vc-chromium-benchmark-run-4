d5fbfe5b9f593b243e653a5bd006ed90c4dede94
Bo Majewski <majewski@chromium.org>
2023-12-14 01:16:36 +0000

    [Files,TS]: Converts files_alert_dialog.js to TypeScript.
    Bug: b:289003444
    Change-Id: Ie3ef7b4dc646a5da4fd249861b7227495ca549e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5118199
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237311}

--- ui/file_manager/file_manager/foreground/js/ui/file_manager_ui.ts
+++ ui/file_manager/file_manager/foreground/js/ui/file_manager_ui.ts
@@ -732,7 +732,7 @@ export class FileManagerUI {
               displayName, response.currentProfileId);
 
           // Show the dialog.
-          this.alertDialog.showWithTitle(title, message, null, null, null);
+          this.alertDialog.showWithTitle(title, message);
         });
   }
 
