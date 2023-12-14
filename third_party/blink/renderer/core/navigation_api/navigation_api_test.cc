a96c75eadb24231c5e20eddc994457452fe49fbb
Mark Rowe <markrowe@chromium.org>
2023-12-14 06:35:23 +0000

    Migrate third_party/blink/renderer/core/navigation_api tests to blink_unittests_v2
    Fixed: 1507272
    Change-Id: Ifa7c9bf7fd4946041f31074135bd01fba7e04a7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5117864
    Auto-Submit: Mark Rowe <markrowe@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237399}

--- third_party/blink/renderer/core/navigation_api/navigation_api_test.cc
+++ third_party/blink/renderer/core/navigation_api/navigation_api_test.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/frame_test_helpers.h"
 #include "third_party/blink/renderer/core/loader/document_loader.h"
 #include "third_party/blink/renderer/core/loader/frame_load_request.h"
+#include "third_party/blink/renderer/platform/testing/task_environment.h"
 #include "third_party/blink/renderer/platform/testing/unit_test_helpers.h"
 #include "third_party/blink/renderer/platform/testing/url_test_helpers.h"
 
@@ -31,6 +32,7 @@ class NavigationApiTest : public testing::Test {
   void TearDown() override {
     url_test_helpers::UnregisterAllURLsAndClearMemoryCache();
   }
+  test::TaskEnvironment task_environment_;
 };
 
 class BeginNavigationClient : public frame_test_helpers::TestWebFrameClient {
