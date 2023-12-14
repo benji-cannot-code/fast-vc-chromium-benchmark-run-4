f231bcfcddb7342325bf20bdf4e39ef1b1063bbe
Clark DuVall <cduvall@chromium.org>
2023-12-14 23:20:39 +0000

    Add a fatal error fn to ml API to allow for better crash reports
    Bug: 1508410
    Change-Id: Iac06aad0c7edef8c9ff5b48e8ee9204b21f851f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5123037
    Auto-Submit: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237803}

--- services/on_device_model/ml/chrome_ml_api.h
+++ services/on_device_model/ml/chrome_ml_api.h
@@ -16,6 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 extern "C" {
 
+// A function used to handle fatal errors.
+using ChromeMLFatalErrorFn = void (*)(const char* msg);
+
 // A scheduling function used to run arbitrary async tasks. Given to
 // CreateModelExecutor() and called into by ChromeML as needed. When called, the
 // value of `context` is the same value given to CreateModelExecutor().
@@ -153,6 +156,9 @@ struct ChromeMLAPI {
   // Initializes the Dawn proc table. This must be called before any other
   // functions.
   void (*InitDawnProcs)(const DawnProcTable& procs);
+
+  // Sets an error handling function for fatal errors.
+  void (*SetFatalErrorFn)(ChromeMLFatalErrorFn error_fn) = nullptr;
 
   // Creates a new ChromeML model instance as described by `model`. The returned
   // object can be destroyed by passing it to DestroyModel(). `context` is
