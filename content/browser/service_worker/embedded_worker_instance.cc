788fc7171acec8a4eb2f289eda0832d71807d429
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2023-12-14 23:28:49 +0000

    Ensure `context_` exists.
    This is a bug fix for crbug.com/1157715.
    The `context_` was accessed even if it becomes invalid, which caused the
    crash in `content::EmbeddedWorkerInstance::OnWorkerVersionDoomed()`.
    
    Bug: 1157715
    Change-Id: Ib02aa17683a0d5dfd549ca1c2ffd515a54f1200b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5117771
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237809}

--- content/browser/service_worker/embedded_worker_instance.cc
+++ content/browser/service_worker/embedded_worker_instance.cc
@@ -660,6 +660,9 @@ void EmbeddedWorkerInstance::OnWorkerVersionInstalled() {
 }
 
 void EmbeddedWorkerInstance::OnWorkerVersionDoomed() {
+  if (!context_) {
+    return;
+  }
   ServiceWorkerDevToolsManager::GetInstance()->WorkerVersionDoomed(
       process_id(), worker_devtools_agent_route_id(),
       base::WrapRefCounted(context_->wrapper()), owner_version_->version_id());
@@ -1116,6 +1119,7 @@ void EmbeddedWorkerInstance::NotifyForegroundServiceWorkerRemoved() {
 mojo::PendingRemote<network::mojom::URLLoaderFactory>
 EmbeddedWorkerInstance::MakeScriptLoaderFactoryRemote(
     std::unique_ptr<blink::PendingURLLoaderFactoryBundle> script_bundle) {
+  CHECK(context_);
   mojo::PendingRemote<network::mojom::URLLoaderFactory>
       script_loader_factory_remote;
 
