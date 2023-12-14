d939b49a07b4121f3a961b61357084f66a7b6cc0
Michael Lippautz <mlippautz@chromium.org>
2023-12-14 19:41:19 +0000

    DOMWindow: Set wrapper reference to global proxy object
    The JS global proxy object already points to the
    {Local,Remote}DOMWindow via internal field. Make
    DOMWindow::main_world_wrapper_ point to the global proxy object as
    well. Avoid using the already existing AssociateWithWrapper() methods
    as they require that a context is properly set up.
    
    {Local,Remote}WindowProxy's global_proxy_ field becomes a regular
    traced reference without setting up the class id.  The GC treats it
    the same way for liveness but heap snapshot generation would not
    consider it as a candidate to merge nodes with a JS object as it's not
    ScriptWrappable's::main_world_wrapper_.
    
    This way Wrappable/Wrapper pairs (represented by ScriptWrappable/V8
    objects) are properly set up in the entire codebase and may on a
    high-level considered as one object or "entity".  This allows heap
    snapshot generation to merge the nodes without considering the class
    id state on the reference. Ultimately, this allows us to get rid of
    the class id eventually and speed up all TracedReference creation.
    
    Delete WPT test:
    performance-timeline/tentative/performance-entry-source-deleted-frame.html
    
    Rationale:
    - The test was added to check that a PerformanceEntry does not leak
      Window objects via its source property when PerformanceEntry objects
      are stashed away to be consumed later.
    - The test merely checks that we return a null Window for a detached
      Window. This is not what it was added for.
    - The test fails for FF and Safari [1]
    - Writing this test properly is really hard: The JS wrapper is already
      materialized in the iframe via `performance.mark()`. Thus accessing
      `childEntry` from the main frame leaks the other iframe via the JS
      wrapper constructor (that points to its context). In order to avoid
      this, one must stash the Window itself in a weak ref, perform GC at
      the event loop, and then check the weakref. Performing GC at the
      event loop is not supported in WPT as of today.
    
    [1] https://wpt.fyi/results/performance-timeline/tentative?label=master&label=experimental&aligned&q=performance-timeline%2Ftentative%2Fperformance-entry-source-deleted-frame.html
    
    Tests: DevTools frontend e2e memory/*
    Bug: chromium:1218404
    Change-Id: I4e3273c6ca7da8e3241b4f9ac78c76932c52bce2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2953842
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237676}

--- third_party/blink/renderer/core/frame/dom_window.h
+++ third_party/blink/renderer/core/frame/dom_window.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_FRAME_DOM_WINDOW_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_FRAME_DOM_WINDOW_H_
 
+#include "base/memory/scoped_refptr.h"
 #include "services/network/public/mojom/cross_origin_opener_policy.mojom-blink.h"
 #include "third_party/blink/public/common/messaging/message_port_channel.h"
 #include "third_party/blink/public/common/tokens/tokens.h"
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class ContextLifecycleNotifier;
+class DOMWrapperWorld;
 class InputDeviceCapabilitiesConstants;
 class LocalDOMWindow;
 class Location;
@@ -32,6 +34,7 @@ class SerializedScriptValue;
 class UserActivation;
 class WindowPostMessageOptions;
 class WindowProxyManager;
+struct WrapperTypeInfo;
 
 struct BlinkTransferableMessage;
 
@@ -77,6 +80,11 @@ class CORE_EXPORT DOMWindow : public WindowProperties {
       v8::Isolate*,
       const WrapperTypeInfo*,
       v8::Local<v8::Object> wrapper) final;
+  v8::Local<v8::Object> AssociateWithWrapper(
+      v8::Isolate* isolate,
+      scoped_refptr<DOMWrapperWorld> world,
+      const WrapperTypeInfo* wrapper_type_info,
+      v8::Local<v8::Object> wrapper);
 
   // EventTarget overrides:
   const AtomicString& InterfaceName() const override;
