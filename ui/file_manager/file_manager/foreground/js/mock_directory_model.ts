117c8e591e53d6c1a5eb7b050f4ab25d8d59432a
Wenbo Jie <wenbojie@chromium.org>
2023-12-15 01:07:43 +0000

    [Files TS] Convert fake_file_selection_handler to TS
    * Also update the FileSelectionHandler to use FilesEventTarget
    
    Bug: b:289003444
    Change-Id: Ie6ccde8052d0669efb379e085c663c428b9e9279
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5117708
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237853}

--- ui/file_manager/file_manager/foreground/js/mock_directory_model.ts
+++ ui/file_manager/file_manager/foreground/js/mock_directory_model.ts
@@ -60,20 +60,14 @@ export function createFakeDirectoryModel(): DirectoryModel {
       return undefined;
     }
 
-    /**
-     */
     getCurrentVolumeInfo(): VolumeInfo|null {
       return null;
     }
 
-    /**
-     */
     getCurrentRootType(): RootType|null {
       return null;
     }
 
-    /**
-     */
     getFileList(): FileListModel|null {
       return null;
     }
@@ -93,6 +87,10 @@ export function createFakeDirectoryModel(): DirectoryModel {
 
     isReadOnly() {
       return false;
+    }
+
+    getFileListSelection() {
+      return new EventTarget();
     }
   }
 
