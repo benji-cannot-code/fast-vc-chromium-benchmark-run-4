43ad2a12f910b1da9c89c8abb6b660e4a9007c3b
Tomasz Wiszkowski <ender@google.com>
2023-12-15 20:51:13 +0000

    Add section header to query tiles.
    Change-Id: I16ccfaacc8652d64b61f0537c762afee83aac2c1
    Screen: http://screen/9vy7HaxXEzWmKpq
    Bug: 1490333
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5124313
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1238239}

--- components/omnibox/browser/suggestion_group_util.cc
+++ components/omnibox/browser/suggestion_group_util.cc
@@ -9,16 +9,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/lazy_instance.h"
 #include "components/omnibox/browser/omnibox_field_trial.h"
 #include "components/omnibox/common/omnibox_features.h"
+#include "components/strings/grit/components_strings.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/omnibox_proto/groups.pb.h"
+#include "ui/base/l10n/l10n_util.h"
 
 namespace omnibox {
 namespace {
 GroupConfig CreateGroup(GroupSection section,
                         GroupConfig::RenderType render_type =
-                            GroupConfig_RenderType_DEFAULT_VERTICAL) {
+                            GroupConfig_RenderType_DEFAULT_VERTICAL,
+                        absl::optional<int32_t> header_text = {}) {
   GroupConfig group;
   group.set_section(section);
   group.set_render_type(render_type);
+  if (header_text) {
+    group.set_header_text(l10n_util::GetStringUTF8(*header_text));
+  }
   return group;
 }
 
@@ -44,7 +51,8 @@ const GroupConfigMap& BuildDefaultGroups() {
           CreateGroup(SECTION_MOBILE_QUERY_TILES,
               OmniboxFieldTrial::kQueryTilesShowAsCarousel.Get()
               ? GroupConfig_RenderType_HORIZONTAL
-              : GroupConfig_RenderType_DEFAULT_VERTICAL)},
+              : GroupConfig_RenderType_DEFAULT_VERTICAL,
+              IDS_OMNIBOX_HEADER_POPULAR_TOPICS)},
         // clang-format on
     };
   }
