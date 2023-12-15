589fc9a8b03de5553adf1efa520cf50cc586765d
Ian Vollick <vollick@chromium.org>
2023-12-15 18:18:21 +0000

    Revert "Avoid use of RenderViewHost in RenderViewContextMenu"
    This reverts commit 3fd0e9663bec4f2e83f6313d36e33acbb190ca7b.
    
    Reason for revert: Need to check on metrics change.
    
    Original change's description:
    > Avoid use of RenderViewHost in RenderViewContextMenu
    >
    > With this change, we switch callers to getting a RFH (either for the
    > appropriate subframe or the primary main frame of the associated web
    > contents) as suggested by mcnee@.
    >
    > Bug: 1129073,1393234
    > Change-Id: Ia6e511b2cf030bed13312604d96440e796226bc8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5117649
    > Commit-Queue: Ian Vollick <vollick@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1237758}
    
    Bug: 1129073,1393234
    Change-Id: I86c6bc6e26522a9bd8cda2914798c6e178a5e2c5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5124532
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238145}

--- chrome/browser/renderer_context_menu/mock_render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/mock_render_view_context_menu.cc
@@ -246,8 +246,7 @@ void MockRenderViewContextMenu::AddPdfOcrMenuItem() {
 #endif  // BUILDFLAG(ENABLE_SCREEN_AI_SERVICE)
 }
 
-content::RenderFrameHost* MockRenderViewContextMenu::GetRenderFrameHost()
-    const {
+content::RenderViewHost* MockRenderViewContextMenu::GetRenderViewHost() const {
   return nullptr;
 }
 
--- components/renderer_context_menu/render_view_context_menu_proxy.h
+++ components/renderer_context_menu/render_view_context_menu_proxy.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 class BrowserContext;
-class RenderFrameHost;
+class RenderViewHost;
 class WebContents;
 }
 
@@ -53,9 +53,9 @@ class MenuModel;
 //       fetcher_->SetRequestContext(context);
 //       content::AssociateURLFetcherWithRenderView(
 //           fetcher_.get(),
-//           proxy_->GetRenderFrameHost()->GetSiteInstance()->GetSite(),
-//           proxy_->GetRenderFrameHost()->GetProcess()->GetID(),
-//           proxy_->GetRenderFrameHost()->GetRoutingID());
+//           proxy_->GetRenderViewHost()->GetSiteInstance()->GetSite(),
+//           proxy_->GetRenderViewHost()->GetProcess()->GetID(),
+//           proxy_->GetRenderViewHost()->GetRoutingID());
 //       fetcher_->Start();
 //     }
 //
@@ -122,7 +122,7 @@ class RenderViewContextMenuProxy {
   virtual void AddPdfOcrMenuItem() = 0;
 
   // Retrieve the given associated objects with a context menu.
-  virtual content::RenderFrameHost* GetRenderFrameHost() const = 0;
+  virtual content::RenderViewHost* GetRenderViewHost() const = 0;
   virtual content::WebContents* GetWebContents() const = 0;
   virtual content::BrowserContext* GetBrowserContext() const = 0;
 };
