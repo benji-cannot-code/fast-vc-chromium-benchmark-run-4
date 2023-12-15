2e6cbfe4dc6e0fee64bd2d294111c64042963cf2
Lingyu Feng <lingyufeng@chromium.org>
2023-12-15 05:20:22 +0000

    Reland "Reland "Reland "Add UMA for window bounds change latency when display rotates"""
    This is a reland of commit 8fe621554344764c955f88be0c13448d37760b72
    
    Code change see diff between Patchset 1 and Patchset 2.
    
    TEST=Cq-Include-Trybots: luci.chromium.try:linux_chromium_chromeos_msan_rel_ng,linux_chromium_chromeos_asan_rel_ng
    
    Original change's description:
    > Reland "Reland "Add UMA for window bounds change latency when display rotates""
    >
    > This is a reland of commit 32d7041f93211c2331b3476be204eee744bab73e
    >
    > This CL fixes the failing tests described in https://crrev.com/c/5099543 by unobserving ash::Shell in ArcWmMetrics when the DTOR of ash::Shell is called.
    > Please see diff between Patchset 1 and Patchset 2.
    >
    > Original change's description:
    > > Reland "Add UMA for window bounds change latency when display rotates"
    > >
    > > This is a reland of commit cebf5ac9845acd586d993a8bdf29a17f7bc97f0d
    > >
    > > As described in b:315115820, some of the unit tests were failing on "Linux ChromiumOS MSan Tests" builders. This CL fixes this issue by releasing arc_wm_metrics_ before calling ash::AshTestBase::TearDown(). See diff between Patchset 3 and Patchset 4.
    > >
    > > Original change's description:
    > > > Add UMA for window bounds change latency when display rotates
    > > >
    > > > This CL adds UMA to measure the time elapses from display rotation until
    > > > the window bounds of the top maximized window is changed.
    > > >
    > > > BUG=b:295119139
    > > > TEST=manually checked chrome://histograms
    > > > TEST=ArcWmMetricsTest.*
    > > >
    > > > Change-Id: Ied60ebfe2c9ec4306b9de69d53e5f073b143efde
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5040921
    > > > Reviewed-by: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    > > > Commit-Queue: Lingyu Feng <lingyufeng@chromium.org>
    > > > Auto-Submit: Lingyu Feng <lingyufeng@chromium.org>
    > > > Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    > > > Cr-Commit-Position: refs/heads/main@{#1233826}
    > >
    > > TEST=enable MSan then out/msan/ash_components_unittests
    > >
    > > Bug: b:295119139, b:315115820
    > > Change-Id: I591ecc75eff7c712ace4a444c6ff8257daeaa176
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5098664
    > > Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    > > Commit-Queue: Lingyu Feng <lingyufeng@chromium.org>
    > > Auto-Submit: Lingyu Feng <lingyufeng@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1234388}
    >
    > TEST=MSan: browser_tests, sync_integration_tests, interactive_ui_tests
    >
    > Bug: b:295119139, b:315115820
    > Change-Id: I71ac94cfe6e793c6073440fc5be182265e75e3d4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5108859
    > Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    > Auto-Submit: Lingyu Feng <lingyufeng@chromium.org>
    > Commit-Queue: Lingyu Feng <lingyufeng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1236085}
    
    Bug: b:295119139, b:315115820
    Change-Id: I9185131734788f0113039f143ddcb20846c04b27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5113667
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Lingyu Feng <lingyufeng@chromium.org>
    Auto-Submit: Lingyu Feng <lingyufeng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237926}

--- ash/components/arc/metrics/arc_wm_metrics.h
+++ ash/components/arc/metrics/arc_wm_metrics.h
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "ash/constants/app_types.h"
+#include "ash/rotator/screen_rotation_animator.h"
+#include "ash/rotator/screen_rotation_animator_observer.h"
+#include "ash/shell.h"
+#include "ash/shell_observer.h"
 #include "base/scoped_multi_source_observation.h"
 #include "base/scoped_observation.h"
 #include "ui/aura/env.h"
@@ -26,6 +30,8 @@ namespace arc {
 
 class ArcWmMetrics : public aura::EnvObserver,
                      public aura::WindowObserver,
+                     public ash::ScreenRotationAnimatorObserver,
+                     public ash::ShellObserver,
                      public display::DisplayObserver {
  public:
   ArcWmMetrics();
@@ -45,6 +51,8 @@ class ArcWmMetrics : public aura::EnvObserver,
   static std::string GetWindowExitTabletModeTimeHistogramName(
       ash::AppType app_type);
 
+  static std::string GetWindowRotateTimeHistogramName(ash::AppType app_type);
+
   // aura::EnvObserver
   void OnWindowInitialized(aura::Window* new_window) override;
 
@@ -57,6 +65,15 @@ class ArcWmMetrics : public aura::EnvObserver,
   // display::DisplayObserver:
   void OnDisplayTabletStateChanged(display::TabletState state) override;
 
+  // ash::ScreenRotationAnimatorObserver:
+  void OnScreenCopiedBeforeRotation() override;
+  void OnScreenRotationAnimationFinished(ash::ScreenRotationAnimator* animator,
+                                         bool canceled) override;
+
+  // ash::ShellObserver:
+  void OnRootWindowWillShutdown(aura::Window* root_window) override;
+  void OnShellDestroying() override;
+
  private:
   friend class ArcWmMetricsTest;
 
@@ -64,7 +81,10 @@ class ArcWmMetrics : public aura::EnvObserver,
 
   class WindowCloseObserver;
 
+  class WindowRotationObserver;
+
   void OnOperationCompleted(aura::Window* window);
+  void OnWindowRotationCompleted(aura::Window* window);
 
   void OnWindowCloseRequested(aura::Window* window);
   void OnWindowCloseCompleted(aura::Window* window);
@@ -84,12 +104,24 @@ class ArcWmMetrics : public aura::EnvObserver,
   base::flat_map<aura::Window*, std::unique_ptr<WindowStateChangeObserver>>
       exiting_tablet_mode_observing_windows_;
 
+  // The map of windows that being observed by WindowRotationObserver and
+  // their corresponding observers.
+  base::flat_map<aura::Window*, std::unique_ptr<WindowRotationObserver>>
+      rotation_observing_windows_;
+
   base::ScopedObservation<aura::Env, aura::EnvObserver> env_observation_{this};
 
   base::ScopedMultiSourceObservation<aura::Window, aura::WindowObserver>
       window_observations_{this};
 
   display::ScopedDisplayObserver display_observer_{this};
+
+  base::ScopedMultiSourceObservation<ash::ScreenRotationAnimator,
+                                     ash::ScreenRotationAnimatorObserver>
+      screen_rotation_observations_{this};
+
+  base::ScopedObservation<ash::Shell, ash::ShellObserver> shell_observation_{
+      this};
 
   base::WeakPtrFactory<ArcWmMetrics> weak_ptr_factory_{this};
 };
