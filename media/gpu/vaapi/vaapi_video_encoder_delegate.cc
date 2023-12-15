abbb4ced12abc52fbec0c43a0e1b0d9978b3e11f
Hirokazu Honda <hiroh@chromium.org>
2023-12-15 06:52:53 +0000

    media/gpu/vaapi: Enable to drop frame in VP8 encoding
    This CL adds a drop frame support to VA-API vp8 encoding.
    
    Design doc: go/frame-drop-VEA
    
    Bug: b:280363228
    Test: media_unittests
    Test: video_encode_accelerator_tests
    
    Change-Id: I8c7a685500919ca21b31b3f83ea0cb1ed763b04c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5052467
    Reviewed-by: Justin Green <greenjustin@google.com>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237946}

--- media/gpu/vaapi/vaapi_video_encoder_delegate.cc
+++ media/gpu/vaapi/vaapi_video_encoder_delegate.cc
@@ -56,6 +56,7 @@ base::TimeDelta VaapiVideoEncoderDelegate::EncodeJob::timestamp() const {
 }
 
 VABufferID VaapiVideoEncoderDelegate::EncodeJob::coded_buffer_id() const {
+  CHECK(coded_buffer_);
   return coded_buffer_->id();
 }
 
@@ -81,6 +82,7 @@ VaapiVideoEncoderDelegate::EncodeResult&
 VaapiVideoEncoderDelegate::EncodeResult::operator=(EncodeResult&&) = default;
 
 VABufferID VaapiVideoEncoderDelegate::EncodeResult::coded_buffer_id() const {
+  CHECK(coded_buffer_);
   return coded_buffer_->id();
 }
 
@@ -126,6 +128,14 @@ bool VaapiVideoEncoderDelegate::Encode(EncodeJob& encode_job) {
     return false;
   }
 
+  if (result == PrepareEncodeJobResult::kDrop) {
+    // An encoder must not drop a keyframe.
+    CHECK(!encode_job.IsKeyframeRequested());
+    DVLOGF(3) << "Drop frame";
+    encode_job.DropFrame();
+    return true;
+  }
+
   if (!vaapi_wrapper_->ExecuteAndDestroyPendingBuffers(
           encode_job.input_surface_id())) {
     VLOGF(1) << "Failed to execute encode";
@@ -139,6 +149,11 @@ absl::optional<VaapiVideoEncoderDelegate::EncodeResult>
 VaapiVideoEncoderDelegate::GetEncodeResult(
     std::unique_ptr<EncodeJob> encode_job) {
   TRACE_EVENT0("media,gpu", "VAVEDelegate::GetEncodeResult");
+  if (encode_job->IsFrameDropped()) {
+    return absl::make_optional<EncodeResult>(nullptr,
+                                             GetMetadata(*encode_job, 0u));
+  }
+
   const VASurfaceID va_surface_id = encode_job->input_surface_id();
   const uint64_t encoded_chunk_size = vaapi_wrapper_->GetEncodedChunkSize(
       encode_job->coded_buffer_id(), va_surface_id);
