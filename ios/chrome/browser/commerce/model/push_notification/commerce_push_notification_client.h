203139d60dde0798a7113cbe711b543e58115644
Marke Hallowell <wry@chromium.org>
2023-12-15 22:57:32 +0000

    Removes OptimizationGuidePushNotificationClient and associated tests.
    Bug: b/315149599
    Change-Id: I4a409bdfef6ab8235360f993db6a8f5415aa5440
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5126697
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Marke Hallowell <wry@chromium.org>
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238303}

--- ios/chrome/browser/commerce/model/push_notification/commerce_push_notification_client.h
+++ ios/chrome/browser/commerce/model/push_notification/commerce_push_notification_client.h
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_COMMERCE_MODEL_PUSH_NOTIFICATION_COMMERCE_PUSH_NOTIFICATION_CLIENT_H_
 #define IOS_CHROME_BROWSER_COMMERCE_MODEL_PUSH_NOTIFICATION_COMMERCE_PUSH_NOTIFICATION_CLIENT_H_
 
+#import "components/optimization_guide/proto/push_notification.pb.h"
 #import "ios/chrome/browser/bookmarks/model/local_or_syncable_bookmark_model_factory.h"
 #import "ios/chrome/browser/commerce/model/shopping_service_factory.h"
-#import "ios/chrome/browser/optimization_guide/model/optimization_guide_push_notification_client.h"
 #import "ios/chrome/browser/push_notification/model/push_notification_client.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
 
