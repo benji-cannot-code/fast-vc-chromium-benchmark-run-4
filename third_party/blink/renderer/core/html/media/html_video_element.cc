ca4fc945da5b07bf527f9a583e432dc97245dcc4
punithnayak <npunith125@gmail.com>
2023-12-15 04:58:59 +0000

    Remove expired histograms Media.VideoPersistence.ControlsType
    Bug: 1508071
    Change-Id: I633afc768ee8aca476c0bca6f327c9079f3f2f43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5118694
    Commit-Queue: Punith Nayak <npunith125@gmail.com>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1237919}

--- third_party/blink/renderer/core/html/media/html_video_element.cc
+++ third_party/blink/renderer/core/html/media/html_video_element.cc
@@ -73,17 +73,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-namespace {
-
-// This enum is used to record histograms. Do not reorder.
-enum VideoPersistenceControlsType {
-  kNative = 0,
-  kCustom = 1,
-  kMaxValue = 1,
-};
-
-}  // anonymous namespace
-
 HTMLVideoElement::HTMLVideoElement(Document& document)
     : HTMLMediaElement(html_names::kVideoTag, document),
       remoting_interstitial_(nullptr),
@@ -277,14 +266,6 @@ void HTMLVideoElement::SetPersistentStateInternal(bool persistent) {
   is_auto_picture_in_picture_ = persistent;
 
   if (persistent) {
-    // Record the type of video. If it is already fullscreen, it is a video with
-    // native controls, otherwise it is assumed to be with custom controls.
-    // This is only recorded when entering this mode.
-    base::UmaHistogramEnumeration("Media.VideoPersistence.ControlsType",
-                                  IsFullscreen()
-                                      ? VideoPersistenceControlsType::kNative
-                                      : VideoPersistenceControlsType::kCustom);
-
     Element* fullscreen_element =
         Fullscreen::FullscreenElementFrom(GetDocument());
     // Only set the video in persistent mode if it is not using native controls
