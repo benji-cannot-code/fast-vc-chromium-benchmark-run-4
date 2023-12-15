d7868deab0ded89dc943dbb05b0d1039c43d236f
Tsuyoshi Horo <horo@chromium.org>
2023-12-15 08:31:05 +0000

    Remove WebURLLoaderThrottleProviderForFrame
    I introduced WebURLLoaderThrottleProviderForFrame interface at
    https://crrev.com/c/4806662, because I wanted to avoid using
    `int render_frame_id` in Blink. But https://crrev.com/c/3751627 replaced
    `int render_frame_id` argument of CreateThrottles() method with
    `LocalFrameToken`. So we don't need the interface anymore. We can call
    URLLoaderThrottleProvider::CreateThrottles() with a LocalFrameToken
    from blink::LoaderFactoryForFrame.
    
    Bug: 1379780
    Change-Id: I3ad3f3f4c42d2b68701881fa9bff11e348186c69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5124956
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237973}

--- third_party/blink/public/web/web_local_frame_client.h
+++ third_party/blink/public/web/web_local_frame_client.h
@@ -715,8 +715,7 @@ class BLINK_EXPORT WebLocalFrameClient {
     return nullptr;
   }
 
-  virtual std::unique_ptr<WebURLLoaderThrottleProviderForFrame>
-  CreateWebURLLoaderThrottleProviderForFrame() {
+  virtual URLLoaderThrottleProvider* GetURLLoaderThrottleProvider() {
     return nullptr;
   }
 
