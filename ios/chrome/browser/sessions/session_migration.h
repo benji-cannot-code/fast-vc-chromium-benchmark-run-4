7646cb3a7bda407a7eb248fd77f7e23d1657a95b
Sylvain Defresne <sdefresne@chromium.org>
2023-12-15 08:49:20 +0000

    [ios] Add function to migrate all session in directories
    When the optimised session storage feature (or flag) is enabled,
    the data needs to be migrated to the corresponding format on disk.
    
    Since the migration can take time (especially for users with many
    tabs), it will have to be performed in background during startup,
    before creating the SessionRestorationService.
    
    Those functions will be used to perform the migration and return
    the status, which will allow the code to select the implementation
    of the service to use.
    
    Bug: 1383087
    Change-Id: I513e103c6a5ca4d2e2d8bc2ac37ef487d6c600e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5116645
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1237978}

--- ios/chrome/browser/sessions/session_migration.h
+++ ios/chrome/browser/sessions/session_migration.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_SESSIONS_SESSION_MIGRATION_H_
 
 #include <string>
+#include <vector>
 
 namespace base {
 class FilePath;
@@ -17,6 +18,12 @@ class TabRestoreService;
 }  // namespace sessions
 
 namespace ios::sessions {
+
+// Status of the storage migration.
+enum class MigrationStatus {
+  kSuccess,
+  kFailure,
+};
 
 // Migrates session named `name` in `path` from legacy to optimized.
 //
@@ -47,6 +54,24 @@ void MigrateNamedSessionToLegacy(
     const base::FilePath& directory,
     const std::string& name,
     ::sessions::TabRestoreService* restore_service);
+
+// Migrates all sessions found in `paths` from legacy to optimized format
+// and returns the status of the migration.
+//
+// If the migration was a success, all storage is in optimized format and
+// all legacy data has been deleted. Otherwise, the original data is left
+// untouched and the partially migrated data deleted.
+MigrationStatus MigrateSessionsInPathsToOptimized(
+    const std::vector<base::FilePath>& paths);
+
+// Migrates all sessions found in `paths` from optimized to legacy format
+// and returns the status of the migration.
+//
+// If the migration was a success, all storage is in optimized format and
+// all optimized data has been deleted. Otherwise, the original data is
+// left untouched and the partially migrated data deleted.
+MigrationStatus MigrateSessionsInPathsToLegacy(
+    const std::vector<base::FilePath>& paths);
 
 }  // namespace ios::sessions
 
