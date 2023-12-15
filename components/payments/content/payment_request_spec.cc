1aa047bd4f8973ff2a13c5d406b9adfbbb2b3600
Christoph Schwering <schwering@google.com>
2023-12-15 16:27:41 +0000

    [Autofill] Rename ServerFieldType to FieldType 3/16
    This CL does the renaming in //components/payments.
    
    Produced by
    $ git ls-files components/payments | \
      grep '\.\(h\|cc\|mm\)$' | \
      xargs grep -l 'ServerFieldType' | \
      xargs sed --in-place -s 's/\<ServerFieldType\>/FieldType/g'
    
    Bug: 1511368, 1007974, 1488408
    Change-Id: I57b9fa29be059d00e6c2ac7b7ec868d9b4cfe111
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5126232
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1238090}

--- components/payments/content/payment_request_spec.cc
+++ components/payments/content/payment_request_spec.cc
@@ -127,7 +127,7 @@ void PaymentRequestSpec::Retry(
 }
 
 std::u16string PaymentRequestSpec::GetShippingAddressError(
-    autofill::ServerFieldType type) {
+    autofill::FieldType type) {
   if (!details_->shipping_address_errors)
     return std::u16string();
 
@@ -165,8 +165,7 @@ std::u16string PaymentRequestSpec::GetShippingAddressError(
   return std::u16string();
 }
 
-std::u16string PaymentRequestSpec::GetPayerError(
-    autofill::ServerFieldType type) {
+std::u16string PaymentRequestSpec::GetPayerError(autofill::FieldType type) {
   if (!payer_errors_)
     return std::u16string();
 
