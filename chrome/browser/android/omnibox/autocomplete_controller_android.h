c7401f1c3f0f3b70cf29b711b8c8a05cabca58df
Marlon Facey <mfacey@chromium.org>
2023-12-15 20:54:38 +0000

    Revert "[Omnibox] Add android platform to pref based consent helper field trial"
    This reverts commit a4170d62ca7429f62677f0a56465829668b710b5.
    
    Reason for revert: Ultimately this did not fix the destruction order 
    issue. Also it sets a bad precedence of destroying objects that 
    are still kept in the java profile_keyed_map.
    
    Original change's description:
    > [Omnibox] Add android platform to pref based consent helper field trial
    >
    > Add android to the omnibox pref based data collection consent
    > helper field trial config. Also fixes issue with
    > ChromeAutocompleteProviderClient trying to access PrefService after it has already been destroyed. Specifically, the profile and profile keyed services are destroyed before the android profile notifies the autocomplete controller android to destroy itself. When the autocomplete
    > controller android is notified to destroy itself the autocomplete
    > controller tries to access the profile through the pref_change_registrar.
    >
    > Problem and approaches discussed here: https://bugs.chromium.org/p/chromium/issues/detail?id=1505824
    >
    > Bug: 1465653, 1493773
    > Change-Id: I87bf9595f755053e03c33695ce3e3e5cb7e4f4e4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5050604
    > Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    > Reviewed-by: Patrick Noland <pnoland@chromium.org>
    > Commit-Queue: Marlon Facey <mfacey@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1230879}
    
    Bug: 1465653, 1493773
    Change-Id: I385c43292c6c65ef1ad9ef5ddf5efb9d765067ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5117204
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238245}

--- chrome/browser/android/omnibox/autocomplete_controller_android.h
+++ chrome/browser/android/omnibox/autocomplete_controller_android.h
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_android.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
-#include "base/scoped_observation.h"
-#include "chrome/browser/profiles/profile_observer.h"
 #include "components/omnibox/browser/autocomplete_controller.h"
 #include "components/omnibox/browser/autocomplete_input.h"
 
@@ -21,8 +19,7 @@ class ChromeAutocompleteProviderClient;
 class Profile;
 
 // The native part of the Java AutocompleteController class.
-class AutocompleteControllerAndroid : public AutocompleteController::Observer,
-                                      ProfileObserver {
+class AutocompleteControllerAndroid : public AutocompleteController::Observer {
  public:
   AutocompleteControllerAndroid(
       JNIEnv* env,
@@ -104,9 +101,6 @@ class AutocompleteControllerAndroid : public AutocompleteController::Observer,
   void OnResultChanged(AutocompleteController* controller,
                        bool default_match_changed) override;
 
-  // ProfileObserver overrides.
-  void OnProfileWillBeDestroyed(Profile* profile) override;
-
   // Notifies the Java AutocompleteController that suggestions were received
   // based on the text the user typed in last.
   void NotifySuggestionsReceived(
@@ -136,7 +130,7 @@ class AutocompleteControllerAndroid : public AutocompleteController::Observer,
   // Destruction of Profile triggers destruction of both
   // C++ AutocompleteControllerAndroid and Java AutocompleteController objects.
   // Guaranteed to be non-null.
-  base::android::ScopedJavaGlobalRef<jobject> java_controller_;
+  const base::android::ScopedJavaGlobalRef<jobject> java_controller_;
 
   // AutocompleteController associated with this client. As this is directly
   // associated with the |provider_client_| and indirectly with |profile_|
@@ -145,8 +139,6 @@ class AutocompleteControllerAndroid : public AutocompleteController::Observer,
   // Invalidated only immediately before the AutocompleteControllerAndroid is
   // destroyed.
   std::unique_ptr<AutocompleteController> autocomplete_controller_;
-
-  base::ScopedObservation<Profile, ProfileObserver> profile_observation_{this};
 
   // Factory used to create asynchronously invoked callbacks.
   // Retained throughout the lifetime of the AutocompleteControllerAndroid.
