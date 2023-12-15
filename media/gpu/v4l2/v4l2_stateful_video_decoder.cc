a688fdc71c997e338d50a5f5932ce4da299bab49
Miguel Casas <mcasas@chromium.org>
2023-12-15 19:54:52 +0000

    media/gpu/V4L2StatefulVD: Propagate timestamp after H.264 frame copy
    This CL sets explicitly the timestamp of any DecoderBuffer that is
    sliced out of the (presumably) larger DecoderBuffer used as input
    for the H.264 slice/reassemble mini class Process().
    
    This is needed to please the Renderer's DecoderStream, which dislikes
    decoded video frames with 0 timestamp (or with a timestamp going "back"
    compared to the last seen one). This bug wasn't caught in v_d_a_test
    because other clients (e.g. webrtc) don't care about timestamps.
    
    Bug: b:279980150
    Change-Id: I09d7f86a3796122a9195d9629aa11c3daeddcb5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5125653
    Commit-Queue: Hang Nguyen <hnt@chromium.org>
    Reviewed-by: Hang Nguyen <hnt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238215}

--- media/gpu/v4l2/v4l2_stateful_video_decoder.cc
+++ media/gpu/v4l2/v4l2_stateful_video_decoder.cc
@@ -1178,6 +1178,7 @@ H264FrameReassembler::Process(scoped_refptr<DecoderBuffer> buffer,
       whole_frames.emplace_back(std::make_pair(
           DecoderBuffer::CopyFrom(buffer_pointer, found_nalu_size),
           base::DoNothing()));
+      whole_frames.back().first->set_timestamp(buffer->timestamp());
 
       buffer_pointer += found_nalu_size;
       remaining_buffer_size -= found_nalu_size;
