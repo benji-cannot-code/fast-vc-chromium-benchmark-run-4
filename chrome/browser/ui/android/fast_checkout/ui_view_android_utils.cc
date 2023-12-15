611f64b618c3c1b850b967b433d51d8e1a7159aa
Christoph Schwering <schwering@google.com>
2023-12-15 13:55:56 +0000

    [Autofill] Rename ServerFieldType to FieldType 11/16
    This CL does the renaming in //chrome/browser/fast_checkout/ and //chrome/browser/ui/android/fast_checkout.
    
    Produced by
    $ git ls-files chrome/browser/fast_checkout/ chrome/browser/ui/android/fast_checkout | \
      grep '\.\(h\|cc\|mm\)$' | \
      xargs grep -l 'ServerFieldType' | \
      xargs sed --in-place -s 's/\<ServerFieldType\>/FieldType/g'
    
    Bug: 1511368, 1007974, 1488408
    Change-Id: I4afdc2e7556b3e0d0e869e445f74020c194705ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5124714
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238037}

--- chrome/browser/ui/android/fast_checkout/ui_view_android_utils.cc
+++ chrome/browser/ui/android/fast_checkout/ui_view_android_utils.cc
@@ -24,7 +24,7 @@ using ::base::android::ConvertUTF8ToJavaString;
 using ::base::android::JavaRef;
 
 void MaybeSetInfo(autofill::AutofillProfile* profile,
-                  autofill::ServerFieldType type,
+                  autofill::FieldType type,
                   const JavaRef<jstring>& value,
                   const std::string& locale) {
   if (value) {
@@ -33,7 +33,7 @@ void MaybeSetInfo(autofill::AutofillProfile* profile,
 }
 
 void MaybeSetRawInfo(autofill::AutofillProfile* profile,
-                     autofill::ServerFieldType type,
+                     autofill::FieldType type,
                      const JavaRef<jstring>& value) {
   if (value) {
     profile->SetRawInfo(type, ConvertJavaStringToUTF16(value));
--- chrome/browser/ui/android/fast_checkout/ui_view_android_utils_unittest.cc
+++ chrome/browser/ui/android/fast_checkout/ui_view_android_utils_unittest.cc
@@ -27,7 +27,7 @@ TEST(FastCheckoutUIViewAndroidUtils, CreateFastCheckoutAutofillProfile) {
   EXPECT_EQ(profile.guid(), parsed_profile->guid());
   EXPECT_EQ(profile.language_code(), parsed_profile->language_code());
 
-  const autofill::ServerFieldType types[] = {
+  const autofill::FieldType types[] = {
       autofill::NAME_FULL,
       autofill::NAME_FIRST,
       autofill::NAME_MIDDLE,
@@ -48,7 +48,7 @@ TEST(FastCheckoutUIViewAndroidUtils, CreateFastCheckoutAutofillProfile) {
       autofill::PHONE_HOME_WHOLE_NUMBER,
   };
 
-  for (autofill::ServerFieldType type : types) {
+  for (autofill::FieldType type : types) {
     EXPECT_EQ(profile.GetRawInfo(type), parsed_profile->GetRawInfo(type));
   }
 }
