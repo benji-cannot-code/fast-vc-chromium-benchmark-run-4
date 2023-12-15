7a60ebd28804c32d9525bbc77557d89cb96c0bbc
Ryan Sultanem <rsult@google.com>
2023-12-15 10:04:45 +0000

    [ChromeSigninBubble] Simplify the avatar button logic when the bubble is shown
    Want to achieve:
    Disable the avatar button action while the chrome signin bubble is shown
    but keep the button in an active state from the UI perspective, so
    that the button seems active (for the bubble) but not doing it's
    initial purpose (showing the profile menu).
    
    Following up on https://crrev.com/c/5038754: this CL introduced a
    complex logic where the button would be disabled but trying to
    override the active UI properties.
    It appeared later while testing that the solution was not complete,
    and some cases with different darker themes would break the text
    visibility.
    
    This CL removes the previous mentioned logic, and implement a
    way simpler one: simply bypass the button action while the
    bubble is shown. This way the state of the button is not affected
    directly so the UI is not as well, only the effect of clicking the
    button is ignored while the bubble is shown.
    
    Bug: b/311185939
    Change-Id: Iac5cd4ede3411a50c66f34a0ecd8b92c61dd81e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5120711
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237992}

--- chrome/browser/ui/views/profiles/dice_web_signin_interception_bubble_view.cc
+++ chrome/browser/ui/views/profiles/dice_web_signin_interception_bubble_view.cc
@@ -145,7 +145,7 @@ DiceWebSigninInterceptionBubbleView::CreateBubble(
   if (bubble_parameters.interception_type ==
       WebSigninInterceptor::SigninInterceptionType::kChromeSignin) {
     AvatarToolbarButton* button = GetAvatarToolbarButton(*browser);
-    button->DisableActionButton();
+    button->SetButtonActionDisabled(true);
     button->ShowSignInText();
   }
 
@@ -297,7 +297,7 @@ void DiceWebSigninInterceptionBubbleView::OnWebUIUserChoice(
   if (bubble_parameters_.interception_type ==
       WebSigninInterceptor::SigninInterceptionType::kChromeSignin) {
     AvatarToolbarButton* button = GetAvatarToolbarButton(*browser_);
-    button->ResetActionButton();
+    button->SetButtonActionDisabled(false);
     button->HideSignInText();
   }
   std::move(callback_).Run(result);
