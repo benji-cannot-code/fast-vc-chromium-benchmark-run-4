fa002d55c37107b9b7ef4dc167a0f86db73e3e52
Yifan Luo <lyf@chromium.org>
2023-12-15 04:59:39 +0000

    [Private Network Access] Fix flaky tests
    Add timeout=long for flaky pna tests
    (mixed-content-fetch,
    fenced-frame-subresource-fetch) and add them to
    slowtests.
    
    Bug: 1508056, 1484710
    Change-Id: Ib6330efc429bb2ad54207477044558f8a940579f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5119462
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Commit-Queue: Yifan Luo <lyf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237920}

--- third_party/blink/web_tests/external/wpt/fetch/private-network-access/mixed-content-fetch.tentative.https.window.js
+++ third_party/blink/web_tests/external/wpt/fetch/private-network-access/mixed-content-fetch.tentative.https.window.js
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=/common/utils.js
 // META: script=resources/support.sub.js
+// META: timeout=long
 //
 // Spec: https://wicg.github.io/private-network-access
 //
