c42e2cee4f474c3e99f025004660a132dbd9032a
Glenn Hartmann <hartmanng@chromium.org>
2023-12-15 15:24:50 +0000

    Add KeyedService WebApkSyncService to own WebApkSyncBridge.
    Also adds WebApkSyncServiceFactory.
    
    Bug: 1425518
    Change-Id: I914c51299cfe1824a0e76281b4cd865f60d0cebf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5083279
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238072}

---
+++ chrome/browser/android/webapk/webapk_sync_service.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/android/webapk/webapk_sync_service.h"
+
+#include "base/functional/callback_helpers.h"
+#include "chrome/browser/android/webapk/webapk_sync_service_factory.h"
+
+namespace webapk {
+
+// static
+WebApkSyncService* WebApkSyncService::GetForProfile(Profile* profile) {
+  return WebApkSyncServiceFactory::GetForProfile(profile);
+}
+
+WebApkSyncService::WebApkSyncService(Profile* profile) {
+  database_factory_ = std::make_unique<WebApkDatabaseFactory>(profile);
+  sync_bridge_ = std::make_unique<WebApkSyncBridge>(database_factory_.get(),
+                                                    base::DoNothing());
+}
+
+WebApkSyncService::~WebApkSyncService() = default;
+
+}  // namespace webapk
