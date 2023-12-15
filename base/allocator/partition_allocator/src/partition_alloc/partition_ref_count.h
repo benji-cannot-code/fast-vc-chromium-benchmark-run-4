874824ad58de01d7c4439a595057354bacedc93b
Bartek Nowierski <bartekn@chromium.org>
2023-12-15 06:01:53 +0000

    [PA][BRP] Obliterate support for extras offset
    It was used only for BRP "before allocation" mode, which is now gone
    (crrev.com/c/5117767). And we aren't planning to go back that route.
    
    Previously we used it also for a leading cookie in debug builds. But
    that just complicated code, and is long gone too.
    
    Having any leading extras requires setting up a separate partition for
    aligned allocations, because they conflict with each other. But that
    support is now gone as well (crrev.com/c/5119422).
    
    Bug: 1511221
    Change-Id: I43839d2ed41a2cd5db29f048373bb25d274fb175
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5119424
    Reviewed-by: Mikihito Matsuura <mikt@google.com>
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237935}

--- base/allocator/partition_allocator/src/partition_alloc/partition_ref_count.h
+++ base/allocator/partition_allocator/src/partition_alloc/partition_ref_count.h
@@ -399,8 +399,6 @@ static_assert(kAlignment % alignof(PartitionRefCount) == 0,
 // Allocate extra space for the reference count to satisfy the alignment
 // requirement.
 static constexpr size_t kInSlotRefCountBufferSize = sizeof(PartitionRefCount);
-// TODO(bartekn): Remove, as we no longer support non-zero offsets.
-constexpr size_t kPartitionRefCountOffsetAdjustment = 0;
 constexpr size_t kPartitionPastAllocationAdjustment = 0;
 
 #if BUILDFLAG(ENABLE_DANGLING_RAW_PTR_CHECKS)
@@ -476,7 +474,6 @@ static_assert(sizeof(PartitionRefCount) <= kInSlotRefCountBufferSize,
 #else  // BUILDFLAG(ENABLE_BACKUP_REF_PTR_SUPPORT)
 
 static constexpr size_t kInSlotRefCountBufferSize = 0;
-constexpr size_t kPartitionRefCountOffsetAdjustment = 0;
 
 #endif  // BUILDFLAG(ENABLE_BACKUP_REF_PTR_SUPPORT)
 
