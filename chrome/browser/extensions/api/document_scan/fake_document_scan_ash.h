911a33f67e4a5b47b8fd7a7e37dc2d57904a62c6
Nathan Muggli <nmuggli@google.com>
2023-12-15 02:14:47 +0000

    documentScan: Add handler to get option groups
    getOptionGroups takes a scanner handle returned from openScanner and
    queries the backend for the option groups for that scanner.  All of the
    work happens on the backend - this CL simply forwards messages back and
    forth and converts messages.
    
    Also did some cleanup in the mojom file and associated files.  Updated
    the GetOptionGroupsResponse message to change 'options' to 'groups'.
    
    Bug: b:313730364
    Change-Id: I38d0eb3b2d0a30abeca4ea162b50d6cd41de00ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5096496
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Nathan Muggli <nmuggli@google.com>
    Reviewed-by: Benjamin Gordon <bmgordon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237885}

--- chrome/browser/extensions/api/document_scan/fake_document_scan_ash.h
+++ chrome/browser/extensions/api/document_scan/fake_document_scan_ash.h
@@ -34,6 +34,8 @@ class FakeDocumentScanAsh : public crosapi::mojom::DocumentScan {
   void OpenScanner(const std::string& client_id,
                    const std::string& scanner_id,
                    OpenScannerCallback callback) override;
+  void GetOptionGroups(const std::string& scanner_handle,
+                       GetOptionGroupsCallback callback) override;
   void CloseScanner(const std::string& scanner_handle,
                     CloseScannerCallback callback) override;
   void StartPreparedScan(const std::string& scanner_handle,
@@ -44,8 +46,6 @@ class FakeDocumentScanAsh : public crosapi::mojom::DocumentScan {
   void SetOptions(const std::string& scanner_handle,
                   std::vector<crosapi::mojom::OptionSettingPtr> options,
                   SetOptionsCallback callback) override;
-  void GetOptionGroups(const std::string& scanner_handle,
-                       GetOptionGroupsCallback callback) override;
   void CancelScan(const std::string& job_handle,
                   CancelScanCallback callback) override;
 
