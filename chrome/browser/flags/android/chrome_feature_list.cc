b14e2422ac386c815c08ffe731ad44e6d8189ffd
Peilin Wang <peilinwang@google.com>
2023-12-15 18:30:43 +0000

    [CCTPrefetchDelayShowOnStart] Cleanup for fixit
    Bug: 1385901
    Change-Id: Ibbe35eeb360225b0de98c51ad5a3b87217421922
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5122813
    Commit-Queue: Peilin Wang <peilinwang@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238152}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -189,7 +189,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kCCTIntentFeatureOverrides,
     &kCCTMinimized,
     &kCCTPageInsightsHub,
-    &kCCTPrefetchDelayShowOnStart,
     &kCCTRealTimeEngagementSignals,
     &kCCTReportParallelRequestStatus,
     &kCCTResizableForThirdParties,
@@ -533,10 +532,6 @@ BASE_FEATURE(kCCTMinimized, "CCTMinimized", base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kCCTPageInsightsHub,
              "CCTPageInsightsHub",
              base::FEATURE_DISABLED_BY_DEFAULT);
-
-BASE_FEATURE(kCCTPrefetchDelayShowOnStart,
-             "CCTPrefetchDelayShowOnStart",
-             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kCCTRealTimeEngagementSignals,
              "CCTRealTimeEngagementSignals",
