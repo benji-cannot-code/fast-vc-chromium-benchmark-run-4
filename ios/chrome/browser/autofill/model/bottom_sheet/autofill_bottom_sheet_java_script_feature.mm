dca7382a68e50a69e06015cc0a87e39d1c3fecfd
Alexis Hetu <sugoi@chromium.org>
2023-12-15 15:18:28 +0000

    [iOS] Don't Trigger bottom sheets on autofocus
    This CL makes it so that the Payments Bottom Sheet can no longer be
    triggered by autofocus events.
    
    For the Password Bottom Sheet, it still triggers on autofocus events
    by default, but that behaviour can now be disabled by disabling
    the new kIOSPasswordBottomSheetAutofocus feature.
    
    Bug: 1510884, 1510887
    Change-Id: I0801173d476305836f8de4252a7cbf6d13d3a1d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5111083
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238069}

--- ios/chrome/browser/autofill/model/bottom_sheet/autofill_bottom_sheet_java_script_feature.mm
+++ ios/chrome/browser/autofill/model/bottom_sheet/autofill_bottom_sheet_java_script_feature.mm
@@ -51,7 +51,8 @@ AutofillBottomSheetJavaScriptFeature::~AutofillBottomSheetJavaScriptFeature() =
 
 void AutofillBottomSheetJavaScriptFeature::AttachListeners(
     const std::vector<autofill::FieldRendererId>& renderer_ids,
-    web::WebFrame* frame) {
+    web::WebFrame* frame,
+    bool allow_autofocus) {
   // TODO(crbug.com/1383214): Properly handle WebFrame destruction.
   if (!frame) {
     return;
@@ -63,6 +64,7 @@ void AutofillBottomSheetJavaScriptFeature::AttachListeners(
   }
   base::Value::List parameters;
   parameters.Append(std::move(renderer_id_list));
+  parameters.Append(allow_autofocus);
   CallJavaScriptFunction(frame, "bottomSheet.attachListeners", parameters);
 }
 
