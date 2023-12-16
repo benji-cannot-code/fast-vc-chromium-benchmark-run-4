454322112550720d5ae81e9a224ebb414c0b9658
Adam Langley <agl@chromium.org>
2023-12-16 03:06:15 +0000

    webauthn: use JSON to communicate with Play Services.
    When CredMan is enabled on Android 14, Clank sends JSON into CredMan,
    and receives JSON in response. However, for Android 13 and below CredMan
    doesn't exist and so we communicate with Play Services directly.
    However, that means that we have two different serialisation formats,
    and that the Android ≤13 one will be increasingly untested.
    
    This change sends JSON into Play Services, and handles JSON if Play
    Services responds with it. Where there are still some fields outside of
    the JSON (which are Bundle values in CredMan), the bulk of the
    complexity is unified and, hopefully, we can drop the majority of the
    Play Services serialisation code in time. This also means that we can
    stop adding to the serialisation code: we only need to update JSON
    serialisation now.
    
    While the JSON functions are well tested, this change is broadly
    untestable because it connects JSON to Play Services and our standard
    testing frameworks don't cover Chrome↔Play Services interactions well.
    At least, in time, it'll reduce the amount of such code however!
    
    The test testMakeCredential_noEligibleParameters2() is dropped because
    it sets an invalid enum value. That wasn't caught previously, but the
    Mojo code doesn't accept it and, anyway, we should have confidence in
    our invariants and that invalid structures aren't getting passed around.
    
    Low-Coverage-Reason: see above.
    Change-Id: I67bddd5b4d93cb8805c98833f418fd87f1d601db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5124672
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238370}

--- device/fido/features.h
+++ device/fido/features.h
@@ -179,6 +179,10 @@ BASE_DECLARE_FEATURE(kWebAuthnChromeImplementedInvariant);
 COMPONENT_EXPORT(DEVICE_FIDO)
 BASE_DECLARE_FEATURE(kAllowExtensionsToSetWebAuthnRpIds);
 
+// Send and receive JSON from Play Services.
+COMPONENT_EXPORT(DEVICE_FIDO)
+BASE_DECLARE_FEATURE(kWebAuthnAndroidFidoJson);
+
 }  // namespace device
 
 #endif  // DEVICE_FIDO_FEATURES_H_
--- services/device/public/cpp/device_feature_map.cc
+++ services/device/public/cpp/device_feature_map.cc
@@ -21,6 +21,7 @@ namespace {
 const base::Feature* const kFeaturesExposedToJava[] = {
     &device::kWebAuthnAndroidCredMan,
     &device::kWebAuthnAndroidCredManForHybrid,
+    &device::kWebAuthnAndroidFidoJson,
     &device::kWebAuthnAndroidHybridClientUi,
     &device::kWebAuthnAndroidIncognitoConfirmation,
     &device::kWebAuthnCableViaCredMan,
