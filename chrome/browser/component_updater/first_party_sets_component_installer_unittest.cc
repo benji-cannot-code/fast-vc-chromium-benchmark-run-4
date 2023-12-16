573a234cf57f206991d046a4527862a011669b0c
Chris Fredrickson <cfredric@chromium.org>
2023-12-16 00:18:48 +0000

    Remove prefix-based test allowlisting
    This approach scales better as more tests are added, and limits the
    scope of the "private" access into the SUT.
    
    Change-Id: Id1d551c40dd7724da0641902a0a4770fcc999389
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5123227
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238337}

--- chrome/browser/component_updater/first_party_sets_component_installer_unittest.cc
+++ chrome/browser/component_updater/first_party_sets_component_installer_unittest.cc
@@ -54,14 +54,15 @@ class FirstPartySetsComponentInstallerTest : public ::testing::Test {
 };
 
 TEST_F(FirstPartySetsComponentInstallerTest, NonexistentFile_OnComponentReady) {
-  ASSERT_TRUE(
-      base::DeleteFile(FirstPartySetsComponentInstallerPolicy::GetInstalledPath(
+  ASSERT_TRUE(base::DeleteFile(
+      FirstPartySetsComponentInstallerPolicy::GetInstalledPathForTesting(
           component_install_dir_.GetPath())));
 
   base::test::TestFuture<base::Version, base::File> future;
   FirstPartySetsComponentInstallerPolicy(future.GetCallback())
-      .ComponentReady(base::Version(), component_install_dir_.GetPath(),
-                      base::Value::Dict());
+      .ComponentReadyForTesting(base::Version(),
+                                component_install_dir_.GetPath(),
+                                base::Value::Dict());
 
   std::tuple<base::Version, base::File> got = future.Take();
   EXPECT_FALSE(std::get<0>(got).IsValid());
@@ -70,8 +71,8 @@ TEST_F(FirstPartySetsComponentInstallerTest, NonexistentFile_OnComponentReady) {
 
 TEST_F(FirstPartySetsComponentInstallerTest,
        NonexistentFile_OnRegistrationComplete) {
-  ASSERT_TRUE(
-      base::DeleteFile(FirstPartySetsComponentInstallerPolicy::GetInstalledPath(
+  ASSERT_TRUE(base::DeleteFile(
+      FirstPartySetsComponentInstallerPolicy::GetInstalledPathForTesting(
           component_install_dir_.GetPath())));
 
   base::test::TestFuture<base::Version, base::File> future;
@@ -94,13 +95,13 @@ TEST_F(FirstPartySetsComponentInstallerTest, LoadsSets_OnComponentReady) {
   auto policy = std::make_unique<FirstPartySetsComponentInstallerPolicy>(
       future.GetCallback());
 
-  ASSERT_TRUE(
-      base::WriteFile(FirstPartySetsComponentInstallerPolicy::GetInstalledPath(
-                          component_install_dir_.GetPath()),
-                      expectation));
+  ASSERT_TRUE(base::WriteFile(
+      FirstPartySetsComponentInstallerPolicy::GetInstalledPathForTesting(
+          component_install_dir_.GetPath()),
+      expectation));
 
-  policy->ComponentReady(version, component_install_dir_.GetPath(),
-                         base::Value::Dict());
+  policy->ComponentReadyForTesting(version, component_install_dir_.GetPath(),
+                                   base::Value::Dict());
 
   std::tuple<base::Version, base::File> got = future.Take();
   EXPECT_TRUE(std::get<0>(got).IsValid());
@@ -125,12 +126,12 @@ TEST_F(FirstPartySetsComponentInstallerTest, IgnoreNewSets_NoInitialComponent) {
   base::ScopedTempDir install_dir;
   ASSERT_TRUE(install_dir.CreateUniqueTempDirUnderPath(
       component_install_dir_.GetPath()));
-  ASSERT_TRUE(
-      base::WriteFile(FirstPartySetsComponentInstallerPolicy::GetInstalledPath(
-                          install_dir.GetPath()),
-                      "first party sets content"));
-  policy.ComponentReady(base::Version("0.0.1"), install_dir.GetPath(),
-                        base::Value::Dict());
+  ASSERT_TRUE(base::WriteFile(
+      FirstPartySetsComponentInstallerPolicy::GetInstalledPathForTesting(
+          install_dir.GetPath()),
+      "first party sets content"));
+  policy.ComponentReadyForTesting(base::Version("0.0.1"), install_dir.GetPath(),
+                                  base::Value::Dict());
 
   env_.RunUntilIdle();
 }
@@ -146,11 +147,12 @@ TEST_F(FirstPartySetsComponentInstallerTest, IgnoreNewSets_OnComponentReady) {
   base::ScopedTempDir dir_v1;
   ASSERT_TRUE(
       dir_v1.CreateUniqueTempDirUnderPath(component_install_dir_.GetPath()));
-  ASSERT_TRUE(
-      base::WriteFile(FirstPartySetsComponentInstallerPolicy::GetInstalledPath(
-                          dir_v1.GetPath()),
-                      sets_v1));
-  policy.ComponentReady(version, dir_v1.GetPath(), base::Value::Dict());
+  ASSERT_TRUE(base::WriteFile(
+      FirstPartySetsComponentInstallerPolicy::GetInstalledPathForTesting(
+          dir_v1.GetPath()),
+      sets_v1));
+  policy.ComponentReadyForTesting(version, dir_v1.GetPath(),
+                                  base::Value::Dict());
 
   std::tuple<base::Version, base::File> got = future.Take();
   EXPECT_TRUE(std::get<0>(got).IsValid());
@@ -164,12 +166,12 @@ TEST_F(FirstPartySetsComponentInstallerTest, IgnoreNewSets_OnComponentReady) {
   base::ScopedTempDir dir_v2;
   ASSERT_TRUE(
       dir_v2.CreateUniqueTempDirUnderPath(component_install_dir_.GetPath()));
-  ASSERT_TRUE(
-      base::WriteFile(FirstPartySetsComponentInstallerPolicy::GetInstalledPath(
-                          dir_v2.GetPath()),
-                      sets_v2));
-  policy.ComponentReady(base::Version("0.0.1"), dir_v2.GetPath(),
-                        base::Value::Dict());
+  ASSERT_TRUE(base::WriteFile(
+      FirstPartySetsComponentInstallerPolicy::GetInstalledPathForTesting(
+          dir_v2.GetPath()),
+      sets_v2));
+  policy.ComponentReadyForTesting(base::Version("0.0.1"), dir_v2.GetPath(),
+                                  base::Value::Dict());
 
   env_.RunUntilIdle();
 }
@@ -177,7 +179,7 @@ TEST_F(FirstPartySetsComponentInstallerTest, IgnoreNewSets_OnComponentReady) {
 TEST_F(FirstPartySetsComponentInstallerTest, GetInstallerAttributes) {
   FirstPartySetsComponentInstallerPolicy policy(base::DoNothing());
 
-  EXPECT_THAT(policy.GetInstallerAttributes(), IsEmpty());
+  EXPECT_THAT(policy.GetInstallerAttributesForTesting(), IsEmpty());
 }
 
 }  // namespace component_updater
