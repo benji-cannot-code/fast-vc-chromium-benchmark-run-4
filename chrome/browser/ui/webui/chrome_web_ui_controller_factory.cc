a522093533bed6feeeabc16361cef89d44ad7cd3
Rebekah Potter <rbpotter@chromium.org>
2023-12-16 00:42:16 +0000

    Device log: Update to use build_webui
    Roll out build_webui() to device log, and change the folder structure
    to match convention:
    
    c/browser/resources/device_log_ui --> c/browser/resources/device_log
    
    device_log_ui.* move to a dedicated device_log/ folder within
    chrome/browser/ui/webui
    
    This is a prerequisite for migrating the folder to TypeScript.
    
    Bug: 1511798
    Change-Id: Ic5675e3a0ddcc6ced5c89aa73ddf62ab21456bee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5124076
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238344}

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/components/components_ui.h"
 #include "chrome/browser/ui/webui/constrained_web_dialog_ui.h"
 #include "chrome/browser/ui/webui/crashes_ui.h"
-#include "chrome/browser/ui/webui/device_log_ui.h"
+#include "chrome/browser/ui/webui/device_log/device_log_ui.h"
 #include "chrome/browser/ui/webui/download_internals/download_internals_ui.h"
 #include "chrome/browser/ui/webui/engagement/site_engagement_ui.h"
 #include "chrome/browser/ui/webui/flags/flags_ui.h"
--- chrome/browser/ui/webui/device_log/device_log_ui.cc
+++ chrome/browser/ui/webui/device_log/device_log_ui.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/webui/device_log_ui.h"
+#include "chrome/browser/ui/webui/device_log/device_log_ui.h"
 
 #include <memory>
 #include <string>
@@ -13,7 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/ui/webui/webui_util.h"
 #include "chrome/common/url_constants.h"
-#include "chrome/grit/dev_ui_browser_resources.h"
+#include "chrome/grit/device_log_resources.h"
+#include "chrome/grit/device_log_resources_map.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/device_event_log/device_event_log.h"
 #include "content/public/browser/web_contents.h"
@@ -151,9 +152,9 @@ DeviceLogUI::DeviceLogUI(content::WebUI* web_ui)
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
   html->UseStringsJs();
-  html->AddResourcePath("device_log_ui.css", IDR_DEVICE_LOG_UI_CSS);
-  html->AddResourcePath("device_log_ui.js", IDR_DEVICE_LOG_UI_JS);
-  html->SetDefaultResource(IDR_DEVICE_LOG_UI_HTML);
+  html->AddResourcePaths(base::make_span(kDeviceLogResources,
+                                         kDeviceLogResourcesSize));
+  html->SetDefaultResource(IDR_DEVICE_LOG_DEVICE_LOG_UI_HTML);
 }
 
 DeviceLogUI::~DeviceLogUI() {
