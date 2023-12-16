e8e21c50e91678bf4503158bbf72a102b93896fc
Tomasz Wiszkowski <ender@google.com>
2023-12-16 01:25:32 +0000

    Clean up unused SpecialLocaleWrapper feature flag.
    Note: depends on crrev.com/i/6782412.
    
    Bug: 1385901
    Change-Id: I128008486b27df1527fc0bb67b284959fce736e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5126121
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238355}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -275,7 +275,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kFeedPositionAndroid,
     &kSearchResumptionModuleAndroid,
     &kShareSheetMigrationAndroid,
-    &kSpecialLocaleWrapper,
     &kSuppressToolbarCaptures,
     &kTabDragDropAndroid,
     &kTabAndLinkDragDropAndroid,
@@ -865,10 +864,6 @@ BASE_FEATURE(kShowScrollableMVTOnNtpPhoneAndroid,
 
 BASE_FEATURE(kShareSheetMigrationAndroid,
              "ShareSheetMigrationAndroid",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
-BASE_FEATURE(kSpecialLocaleWrapper,
-             "SpecialLocaleWrapper",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSuppressToolbarCaptures,
