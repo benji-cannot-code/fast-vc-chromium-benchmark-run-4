a73f88ea119ba86f7e509f09328ba846e02a763f
Clark DuVall <cduvall@chromium.org>
2023-12-17 23:55:05 +0000

    Add crash key to ChromeML error fn
    The error message was still not showing up in crash reports.
    
    Bug: 1512318
    Change-Id: Id820047d354caa1807228a867921712f65bc4826
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5129432
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238469}

--- services/on_device_model/ml/chrome_ml.cc
+++ services/on_device_model/ml/chrome_ml.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_paths.h"
 #include "base/check.h"
 #include "base/compiler_specific.h"
+#include "base/debug/crash_logging.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/memory/ref_counted.h"
@@ -58,6 +59,7 @@ void LogGpuBlocked(GpuBlockedReason reason) {
 }
 
 void FatalErrorFn(const char* msg) {
+  SCOPED_CRASH_KEY_STRING256("ChromeML", "error_msg", msg);
   CHECK(false) << "ChromeML Error: " << msg;
 }
 
