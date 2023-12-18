11ce16131effced3a8b208908e5e98706d0fa57b
Yue Zhang <yuezhanggg@chromium.org>
2023-12-18 22:33:21 +0000

    [PriceDrop] Support skipping delayed init for ShoppingPersistedTabData
    Currently on startup, ShoppingPersistedTabData are initialized in a
    delayed style and won't be ready until onDeferredStartup is called.
    This is to minimize the performance impact that the initialization
    have on startup. However, this would mean that the data could be ready
    too late if the very first page after startup is NTP and we try to
    render the price change module.
    
    To deal with this issue, we add a new method in this CL to skip
    the delayed init and init immediately for tabs that might have
    price drops. This could potentially regress startup performance,
    however, we'll experiment this approach considering that (1) having
    data ready for the very first NTP after startup is very important for
    the feature (2) the number of tabs that might have price drop should
    be small. Also, we guard this new code path behind a Finch parameter,
    so that we can disable it anytime if we need to.
    
    This CL is the implementation of this part in the design doc: https://docs.google.com/document/d/1eUYWgGn5VbemTYgSsD6mLU_3DS8xZvDR8_60HYKY6XY/edit?tab=t.0#bookmark=id.8r4uwrg0t2l4.
    
    Bug: 1501138
    Change-Id: Icdf4718e67229e686a25f3d72518d6b2a8f7eb26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5114220
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238813}

--- chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/state/ShoppingPersistedTabDataService.java
+++ chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/state/ShoppingPersistedTabDataService.java
@@ -13,6 +13,7 @@ import androidx.annotation.VisibleForTesting;
 import org.chromium.base.Callback;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.shared_preferences.SharedPreferencesManager;
+import org.chromium.chrome.browser.flags.BooleanCachedFieldTrialParameter;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.preferences.ChromeSharedPreferences;
 import org.chromium.chrome.browser.profiles.Profile;
@@ -32,6 +33,12 @@ import java.util.concurrent.atomic.AtomicInteger;
  * refactor that will move it out of current folder.
  */
 public class ShoppingPersistedTabDataService {
+    private static final BooleanCachedFieldTrialParameter
+            SKIP_SHOPPING_PERSISTED_TAB_DATA_DELAYED_INITIALIZATION =
+                    new BooleanCachedFieldTrialParameter(
+                            ChromeFeatureList.PRICE_CHANGE_MODULE,
+                            "skip_shopping_persisted_tab_data_delayed_initialization",
+                            true);
     private static ProfileKeyedMap<ShoppingPersistedTabDataService> sProfileToPriceDropService;
     private static ShoppingPersistedTabDataService sServiceForTesting;
 
@@ -183,7 +190,8 @@ public class ShoppingPersistedTabDataService {
                         if (counter.incrementAndGet() == currentTabsWithPriceDrop.size()) {
                             callback.onResult(sortShoppingPersistedTabDataWithPriceDrops(results));
                         }
-                    });
+                    },
+                    SKIP_SHOPPING_PERSISTED_TAB_DATA_DELAYED_INITIALIZATION.getValue());
         }
     }
 
