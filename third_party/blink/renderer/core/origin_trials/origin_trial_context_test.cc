f88d50ae7f2ffeccb5e383bff61d63383527a0c6
Alex Turner <alexmt@chromium.org>
2023-12-18 02:04:42 +0000

    [Code Health] Migrate core/origin_trials to blink_unittests_v2
    Bug: 1507273
    Change-Id: I28db3ad3d25078b11037781f5ac65c8743e7bba3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5117147
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238484}

--- third_party/blink/renderer/core/origin_trials/origin_trial_context_test.cc
+++ third_party/blink/renderer/core/origin_trials/origin_trial_context_test.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/testing/dummy_page_holder.h"
 #include "third_party/blink/renderer/core/testing/null_execution_context.h"
 #include "third_party/blink/renderer/platform/testing/runtime_enabled_features_test_helpers.h"
+#include "third_party/blink/renderer/platform/testing/task_environment.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
 #include "third_party/blink/renderer/platform/weborigin/security_origin.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
@@ -208,6 +209,7 @@ class OriginTrialContextTest : public testing::Test {
   }
 
  protected:
+  test::TaskEnvironment task_environment_;
   MockTokenValidator* token_validator_;
   Persistent<NullExecutionContext> execution_context_;
   base::HistogramTester histogram_tester_;
