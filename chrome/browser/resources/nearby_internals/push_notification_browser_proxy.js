6b9295d846df548bf06b506b3a2f7d74786dcfbe
Alex Kingsborough <akingsb@google.com>
2023-12-18 20:49:04 +0000

    [Chime] Adding piping to debug UI
    Add piping for the Push Notification keyed service to allow it to be
    called from the chrome://nearby-internals debug webUI page
    
    Tested: Manually tested on DUT
    Bug: b/306399332
    Change-Id: I4ebe380e89aa4919858e324cff6d7a661e195d14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5006104
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Commit-Queue: Alex Kingsborough <akingsb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1238728}

---
+++ chrome/browser/resources/nearby_internals/push_notification_browser_proxy.js
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+export class PushNotificationBrowserProxy {
+  /**
+   * Initializes web contents in the WebUI handler.
+   */
+  initialize() {
+    chrome.send('InitializePushNotificationHandler');
+  }
+
+  /**
+   * Triggers adding the push notification handler as a push notification
+   * client.
+   */
+  SendAddPushNotificationClient() {
+    chrome.send('AddPushNotificationClient');
+  }
+
+  /** @return {!PushNotificationBrowserProxy} */
+  static getInstance() {
+    return instance || (instance = new PushNotificationBrowserProxy());
+  }
+}
+
+/** @type {?PushNotificationBrowserProxy} */
+let instance = null;
