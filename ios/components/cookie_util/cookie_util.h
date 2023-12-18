af3fae3ec8661a86f06605238597efe27be9ccbe
Will Harris <wfh@chromium.org>
2023-12-18 19:14:22 +0000

    Remove iOS crypto_delegate references
    iOS never encrypts, because cookie_config::GetCookieCryptoDelegate
    always returns nullptr on this platform.
    
    Hence, all this code is redundant and can be safely removed,
    simplyfying the codebase.
    
    This CL contains no functional changes, but is useful for a future
    CL that changes the lifetime semantics for net::CookieCryptoDelegate.
    
    BUG=1511730
    
    Change-Id: I8423e510350edbe7fc7b318672c2a022972941bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5124753
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238633}

--- ios/components/cookie_util/cookie_util.h
+++ ios/components/cookie_util/cookie_util.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cookies/canonical_cookie.h"
 
 namespace net {
-class CookieCryptoDelegate;
 class CookieStore;
 class SystemCookieStore;
 class NetLog;
@@ -54,22 +53,14 @@ struct CookieStoreConfig {
   // If `path` is empty, then this specifies an in-memory cookie store.
   // With in-memory cookie stores, `session_cookie_mode` must be
   // EPHEMERAL_SESSION_COOKIES.
-  // Note: If `crypto_delegate` is non-null, it must outlive any CookieStores
-  // created using this config.
   CookieStoreConfig(const base::FilePath& path,
                     SessionCookieMode session_cookie_mode,
-                    CookieStoreType cookie_store_type,
-                    net::CookieCryptoDelegate* crypto_delegate);
+                    CookieStoreType cookie_store_type);
   ~CookieStoreConfig();
 
   const base::FilePath path;
   const SessionCookieMode session_cookie_mode;
   const CookieStoreType cookie_store_type;
-
-  // Used to provide encryption hooks for the cookie store. The
-  // CookieCryptoDelegate must outlive any cookie store created with this
-  // config.
-  net::CookieCryptoDelegate* crypto_delegate;
 };
 
 // Creates a cookie store which is internally either a CookieMonster or a
