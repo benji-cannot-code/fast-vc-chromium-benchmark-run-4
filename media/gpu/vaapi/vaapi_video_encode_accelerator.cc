e672b9d15fc61c919049493ed118d7e793efe0cc
Hirokazu Honda <hiroh@chromium.org>
2023-12-18 20:24:46 +0000

    media/gpu/vaapiVEA: Add VEA config to error status message
    This CL adds VideoEncodeAccelerator::Config string to the encoder
    error status message in order to find the encoder configuration when
    an initialization error happens.
    
    Bug: None
    Test: None
    Change-Id: I6197ed0550ea6fd5b47ef8855f5fc42af2de8f9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5119427
    Auto-Submit: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Justin Green <greenjustin@google.com>
    Commit-Queue: Justin Green <greenjustin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1238699}

--- media/gpu/vaapi/vaapi_video_encode_accelerator.cc
+++ media/gpu/vaapi/vaapi_video_encode_accelerator.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/shared_memory_mapping.h"
 #include "base/memory/unsafe_shared_memory_region.h"
 #include "base/numerics/safe_conversions.h"
+#include "base/strings/strcat.h"
 #include "base/strings/stringprintf.h"
 #include "base/task/bind_post_task.h"
 #include "base/task/sequenced_task_runner.h"
@@ -368,7 +369,8 @@ void VaapiVideoEncodeAccelerator::InitializeTask(const Config& config) {
   DCHECK_GT(ave_config.max_num_ref_frames, 0u);
   if (!encoder_->Initialize(config, ave_config)) {
     NotifyError({EncoderStatus::Codes::kEncoderInitializationError,
-                 "Failed initializing encoder"});
+                 base::StrCat({"Failed initializing encoder. config: ",
+                               config.AsHumanReadableString()})});
     return;
   }
 
@@ -390,7 +392,8 @@ void VaapiVideoEncodeAccelerator::InitializeTask(const Config& config) {
       num_frames_in_flight_ * std::max<size_t>(1, config.spatial_layers.size());
   if (!vaapi_wrapper_->CreateContext(encoder_->GetCodedSize())) {
     NotifyError({EncoderStatus::Codes::kEncoderInitializationError,
-                 "Failed creating VAContext"});
+                 base::StrCat({"Failed creating VAContext. config: ",
+                               config.AsHumanReadableString()})});
     return;
   }
 
