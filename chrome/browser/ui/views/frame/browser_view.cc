fe5546b90f02dbdcd00026f249d05772dc806e57
Sonja <laurila@google.com>
2023-12-18 16:48:54 +0000

    Don't run SynchronizeVisualProperties on Mac when display-state updates
    This is a temporary solution to prevent escalation until I get a loaner
    macbook to investigate the bug.
    
    Bug: 1503145
    Change-Id: I680b404e9c3c669de273f4d7358856b158b0e2b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5126179
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Sonja Laurila <laurila@google.com>
    Cr-Commit-Position: refs/heads/main@{#1238602}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -2590,6 +2590,8 @@ void BrowserView::OnCanResizeFromWebAPIChanged() {
 }
 
 void BrowserView::SynchronizeRenderWidgetHostVisualPropertiesForMainFrame() {
+// TODO(crbug.com/1503145): Investigate and fix on MacOS.
+#if !BUILDFLAG(IS_MAC)
   content::WebContents* web_contents = GetActiveWebContents();
   if (!web_contents || !web_contents->GetPrimaryMainFrame()) {
     return;
@@ -2599,6 +2601,7 @@ void BrowserView::SynchronizeRenderWidgetHostVisualPropertiesForMainFrame() {
           web_contents->GetPrimaryMainFrame()->GetRenderWidgetHost()) {
     render_widget_host->SynchronizeVisualProperties();
   }
+#endif
 }
 
 void BrowserView::OnWidgetSizeConstraintsChanged(views::Widget* widget) {
