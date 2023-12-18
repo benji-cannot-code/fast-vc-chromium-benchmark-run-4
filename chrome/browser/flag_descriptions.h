ec567dfc2f5711c8117d8d2bcf08a78cd04b5b38
Henrique Nakashima <hnakashima@chromium.org>
2023-12-18 21:24:38 +0000

    [Android] Cleanup kInterestFeedV2Autoplay deprecated flag
    Bug: 1385901
    Change-Id: I3239431901fc1571db1d13aeee229219d8e90393
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5123235
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238755}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2392,9 +2392,6 @@ extern const char kInterestFeedV2Description[];
 extern const char kInterestFeedV2HeartsName[];
 extern const char kInterestFeedV2HeartsDescription[];
 
-extern const char kInterestFeedV2AutoplayName[];
-extern const char kInterestFeedV2AutoplayDescription[];
-
 extern const char kMediaPickerAdoptionStudyName[];
 extern const char kMediaPickerAdoptionStudyDescription[];
 
--- components/feed/feed_feature_list.h
+++ components/feed/feed_feature_list.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace feed {
 
 BASE_DECLARE_FEATURE(kInterestFeedV2);
-BASE_DECLARE_FEATURE(kInterestFeedV2Autoplay);
 BASE_DECLARE_FEATURE(kInterestFeedV2Hearts);
 BASE_DECLARE_FEATURE(kInterestFeedV2Scrolling);
 
