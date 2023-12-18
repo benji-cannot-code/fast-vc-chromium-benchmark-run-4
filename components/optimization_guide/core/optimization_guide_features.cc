c726f457bd9b43f4c88f668833f36ad7c44a66a6
Sophie Chang <sophiechang@chromium.org>
2023-12-18 21:13:26 +0000

    Hook up delivery of safety model via opt guide delivery
    Bug: b:302395013
    Change-Id: I10738d267837e2e1cec717c00eee5faac315d32b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5128756
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1238746}

--- components/optimization_guide/core/optimization_guide_features.cc
+++ components/optimization_guide/core/optimization_guide_features.cc
@@ -315,6 +315,11 @@ BASE_FEATURE(kLogOnDeviceMetricsOnStartup,
              "LogOnDeviceMetricsOnStartup",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Whether to download the text safety classifier model.
+BASE_FEATURE(kTextSafetyClassifier,
+             "TextSafetyClassifier",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 size_t MaxRelatedSearchesCacheSize() {
   return GetFieldTrialParamByFeatureAsInt(
       kExtractRelatedSearchesFromPrefetchedZPSResponse,
@@ -1032,6 +1037,18 @@ bool GetOnDeviceModelRetractUnsafeContent() {
           &kOptimizationGuideOnDeviceModel, "on_device_retract_unsafe_content",
           false};
   return kOnDeviceModelShouldRetractUnsafeContent.Get();
+}
+
+bool GetOnDeviceModelMustUseSafetyModel() {
+  static const base::FeatureParam<bool>
+      kOnDeviceModelShouldRetractUnsafeContent{
+          &kOptimizationGuideOnDeviceModel, "on_device_must_use_safety_model",
+          false};
+  return kOnDeviceModelShouldRetractUnsafeContent.Get();
+}
+
+bool ShouldDownloadTextSafetyClassifierModel() {
+  return base::FeatureList::IsEnabled(kTextSafetyClassifier);
 }
 
 }  // namespace features
