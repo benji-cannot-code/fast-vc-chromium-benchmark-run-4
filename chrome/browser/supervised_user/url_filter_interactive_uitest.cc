c2ce75f8edbeb0454e14e347f8a83394bfcdd4cf
anthie@google.com <anthie@google.com>
2023-12-18 19:24:34 +0000

    Parametrize interactive filtering test with different accounts
    This change parametrizes url_filter_interactive_uitest.cc so that
    it can be run with different households. This is a pre-requisite
    for adding different test accounts (with different DMA preferences)
    for DMA5-2 regression coverage.
    
    The DMA relevant OTAs will be added when b/316104036, which fixes
    their passwords, is done.
    
    Bug: b/315794138, b/316104036
    Change-Id: I458a7d789d742ff7e023fa47edd093ef0ddbade0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5116602
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tomek Jurkiewicz <tju@google.com>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1238642}

--- chrome/browser/supervised_user/url_filter_interactive_uitest.cc
+++ chrome/browser/supervised_user/url_filter_interactive_uitest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/types/strong_alias.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 #include "chrome/test/interaction/interactive_browser_test.h"
 #include "chrome/test/supervised_user/family_live_test.h"
@@ -26,10 +27,13 @@ static constexpr std::string_view kPermissionRequestUrl =
 
 // TODO(b/303401498): Use dedicated RPCs in supervised user e2e desktop tests
 // instead of clicking around the pages.
-class UrlFilterUiTest : public InteractiveBrowserTestT<FamilyLiveTest> {
+class UrlFilterUiTest
+    : public InteractiveBrowserTestT<FamilyLiveTest>,
+      public testing::WithParamInterface<supervised_user::FamilyIdentifier> {
  public:
   UrlFilterUiTest()
       : InteractiveBrowserTestT<FamilyLiveTest>(
+            /*family_identifier=*/GetParam(),
             /*extra_enabled_hosts=*/std::vector<std::string>(
                 {"example.com", "www.pornhub.com"})) {}
 
@@ -164,7 +168,7 @@ class UrlFilterUiTest : public InteractiveBrowserTestT<FamilyLiveTest> {
   }
 };
 
-IN_PROC_BROWSER_TEST_F(UrlFilterUiTest, ParentBlocksPage) {
+IN_PROC_BROWSER_TEST_P(UrlFilterUiTest, ParentBlocksPage) {
   DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kChildElementId);
   DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kParentControlsTab);
 
@@ -201,7 +205,7 @@ IN_PROC_BROWSER_TEST_F(UrlFilterUiTest, ParentBlocksPage) {
       WaitForStateChange(kChildElementId, RemoteApprovalButtonAppeared()));
 }
 
-IN_PROC_BROWSER_TEST_F(UrlFilterUiTest, ParentAllowsPageBlockedBySafeSites) {
+IN_PROC_BROWSER_TEST_P(UrlFilterUiTest, ParentAllowsPageBlockedBySafeSites) {
   DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kChildElementId);
   DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kParentControlsTab);
 
@@ -237,7 +241,7 @@ IN_PROC_BROWSER_TEST_F(UrlFilterUiTest, ParentAllowsPageBlockedBySafeSites) {
       WaitForStateChange(kChildElementId, PageWithMatchingTitle("Pornhub")));
 }
 
-IN_PROC_BROWSER_TEST_F(UrlFilterUiTest,
+IN_PROC_BROWSER_TEST_P(UrlFilterUiTest,
                        ParentAprovesPermissionRequestForBlockedSite) {
   DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kChildElementId);
   DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kParentControlsTab);
@@ -286,6 +290,13 @@ IN_PROC_BROWSER_TEST_F(UrlFilterUiTest,
       ParentApprovesPermissionRequest(kParentApprovalTab),
       WaitForStateChange(kChildElementId, PageWithMatchingTitle("Pornhub")));
 }
+
+INSTANTIATE_TEST_SUITE_P(
+    All,
+    UrlFilterUiTest,
+    // TODO(b/315794138): Add the rest of DMA-configured accounts.
+    testing::Values(supervised_user::FamilyIdentifier("FAMILY")),
+    [](const auto& info) { return info.param->data(); });
 
 }  // namespace
 }  // namespace supervised_user
--- chrome/test/supervised_user/family_live_test.h
+++ chrome/test/supervised_user/family_live_test.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/test/scoped_feature_list.h"
+#include "base/types/strong_alias.h"
 #include "chrome/browser/signin/e2e_tests/live_test.h"
 #include "chrome/browser/signin/e2e_tests/signin_util.h"
 #include "chrome/browser/signin/e2e_tests/test_accounts_util.h"
@@ -22,6 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace supervised_user {
 
+using FamilyIdentifier =
+    base::StrongAlias<class FamilyIdentifierTag, std::string>;
+
 // A LiveTest which assumes a specific structure of provided test accounts,
 // which are forming a family:
 // * head of household,
@@ -30,7 +34,12 @@ class FamilyLiveTest : public signin::test::LiveTest {
  public:
   FamilyLiveTest();
   // Navigation will be allowed to extra hosts.
-  explicit FamilyLiveTest(const std::vector<std::string>& extra_enabled_hosts);
+  explicit FamilyLiveTest(FamilyIdentifier family_identifier);
+  // The provided family identifier will be used to select the test accounts.
+  // Navigation will be allowed to extra hosts.
+  FamilyLiveTest(FamilyIdentifier family_identifier,
+                 const std::vector<std::string>& extra_enabled_hosts);
+
   ~FamilyLiveTest() override;
 
   // Turns on sync and closes auxiliary tabs.
@@ -45,8 +54,16 @@ class FamilyLiveTest : public signin::test::LiveTest {
   // explicitly added to `extra_enabled_hosts`.
   GURL GetRoutedUrl(std::string_view url_spec) const;
 
-  FamilyMember& head_of_household() { return *head_of_household_; }
-  FamilyMember& child() { return *child_; }
+  FamilyMember& head_of_household() {
+    CHECK(head_of_household_) << "No head of household found in family: " +
+                                     std::string(family_identifier_->data());
+    return *head_of_household_;
+  }
+  FamilyMember& child() {
+    CHECK(child_) << "No child found in family: " +
+                         std::string(family_identifier_->data());
+    return *child_;
+  }
 
  private:
   base::test::ScopedFeatureList features{
@@ -54,12 +71,15 @@ class FamilyLiveTest : public signin::test::LiveTest {
 
   // Extracts requested account, which must exist.
   signin::test::TestAccount GetTestAccount(std::string_view account_name) const;
+  // Checks if test account exists.
+  bool TestAccountExists(std::string_view account_name) const;
 
   // Creates a new browser signed in to the specified account, which must
   // exist.
   std::unique_ptr<FamilyMember> MakeSignedInBrowser(
       std::string_view account_name);
 
+  FamilyIdentifier family_identifier_;
   std::unique_ptr<FamilyMember> head_of_household_;
   std::unique_ptr<FamilyMember> child_;
 
