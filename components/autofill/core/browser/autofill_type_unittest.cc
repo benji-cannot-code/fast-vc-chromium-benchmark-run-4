efdb8d97c6efc81a98765c059a4db068be3339f9
Christoph Schwering <schwering@google.com>
2023-12-18 11:29:26 +0000

    [Autofill] Rename HtmlFieldTypeToBestCorrespondingServerFieldType() to HtmlFieldTypeToBestCorrespondingFieldType()
    Produced by
    $ git ls-files | \
      grep '\.\(h\|cc\|mm\)$' | \
      xargs grep -l 'ServerFieldType' | \
      xargs sed --in-place -s 's/\<HtmlFieldTypeToBestCorrespondingServerFieldType\>/HtmlFieldTypeToBestCorrespondingFieldType/g'
    
    Bug: 1511368, 1007974, 1488408
    Change-Id: Ied6df94c3988441711e09bdc4d4fa9b9ded80d46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5130229
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238555}

--- components/autofill/core/browser/autofill_type_unittest.cc
+++ components/autofill/core/browser/autofill_type_unittest.cc
@@ -124,7 +124,7 @@ INSTANTIATE_TEST_SUITE_P(
                    base::to_underlying(HtmlFieldType::kMaxValue)));
 
 TEST_P(AutofillTypeTestForHtmlFieldTypes, GroupsOfHtmlFieldTypes) {
-  if (HtmlFieldTypeToBestCorrespondingServerFieldType(html_field_type()) ==
+  if (HtmlFieldTypeToBestCorrespondingFieldType(html_field_type()) ==
       UNKNOWN_TYPE) {
     return;
   }
--- components/autofill/core/browser/field_types.cc
+++ components/autofill/core/browser/field_types.cc
@@ -676,8 +676,7 @@ FieldTypeGroup GroupTypeOfHtmlFieldType(HtmlFieldType field_type) {
   NOTREACHED_NORETURN();
 }
 
-FieldType HtmlFieldTypeToBestCorrespondingServerFieldType(
-    HtmlFieldType field_type) {
+FieldType HtmlFieldTypeToBestCorrespondingFieldType(HtmlFieldType field_type) {
   switch (field_type) {
     case HtmlFieldType::kUnspecified:
       return UNKNOWN_TYPE;
