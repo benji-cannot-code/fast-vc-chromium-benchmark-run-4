b756f3d0c75a377ec31852a3ec9a12f00e64f8ac
Hong Xu <xuhong@chromium.org>
2023-12-18 22:52:29 +0000

    Remove existing uses of the default constructor of `reporting::StatusOr`
    As a cautious measure, because existing code may have assumed that the
    default constructor creates an error status, but the current behavior
    (after migrating to `base::expected`) is a default correct value.
    
    In the future, the default constructor is still allowed to be used, with
    the knowledge that the default constructor constructs a default correct
    value, not an error status.
    
    This mirrors the effort in crrev.com/c/5132907.
    
    The detection of the uses of this constructor is helped by the following
    patch:
    
    	diff --git a/base/types/expected.h b/base/types/expected.h
    	index 00ec66517780..ff454ccbd57a 100644
    	--- a/base/types/expected.h
    	+++ b/base/types/expected.h
    	@@ -15,6 +15,10 @@
    	 #include "base/types/expected_internal.h"  // IWYU pragma: export
    	 #include "third_party/abseil-cpp/absl/utility/utility.h"
    
    	+namespace reporting::internal {
    	+  class ErrorStatus;
    	+}
    	+
    	 // Class template `expected<T, E>` is a vocabulary type which contains an
    	 // expected value of type `T`, or an error `E`. The class skews towards behaving
    	 // like a `T`, because its intended use is when the expected type is contained.
    	@@ -289,6 +293,9 @@ class [[nodiscard]] expected final {
    	   // [expected.object.ctor], constructors
    	   constexpr expected() noexcept = default;
    
    	+  expected() noexcept
    	+  requires(std::is_same_v<error_type, reporting::internal::ErrorStatus>) = delete;
    	+
    	   // Converting copy and move constructors. These constructors are explicit if
    	   // either the value or error type is not implicitly convertible from `rhs`'s
    	   // corresponding type.
    
    Bug: b:307554035
    Change-Id: I676acc87a106d597b3557fe551d2a985061d776f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5133548
    Commit-Queue: Hong Xu <xuhong@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238833}

--- components/reporting/util/statusor.cc
+++ components/reporting/util/statusor.cc
@@ -8,8 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/check.h"
+#include "base/no_destructor.h"
+#include "base/types/expected.h"
 
-namespace reporting::internal {
+namespace reporting {
+namespace internal {
 ErrorStatus::ErrorStatus(const ErrorStatus&) = default;
 ErrorStatus& ErrorStatus::operator=(const ErrorStatus&) = default;
 ErrorStatus::ErrorStatus(ErrorStatus&&) = default;
@@ -23,4 +26,11 @@ ErrorStatus::ErrorStatus(const Status& status) : Status(status) {
 ErrorStatus::ErrorStatus(Status&& status) : Status(std::move(status)) {
   CHECK(!ok()) << "The status must not be OK";
 }
-}  // namespace reporting::internal
+}  // namespace internal
+
+base::unexpected<Status> CreateUnknownErrorStatusOr() {
+  static base::NoDestructor<base::unexpected<Status>> status_not_initialized(
+      Status(error::UNKNOWN, "Not initialized"));
+  return *status_not_initialized;
+}
+}  // namespace reporting
