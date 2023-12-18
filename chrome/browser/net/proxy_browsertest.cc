83cb240778736372b3e0f08196fcae1ed7c9b1ef
Erik Chen <erikchen@chromium.org>
2023-12-18 23:41:03 +0000

    login_handler: Remove last browser test dependencies on notifications.
    This CL removes the last dependencies from browser test code on the
    notifications fired by LoginHandler. This CL has no intended behavior
    change.
    
    Bug: 1371177
    Change-Id: Ib0ac596e72b65076415d7b6a5bbb7b1503090c04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5125334
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238856}

--- chrome/browser/net/proxy_browsertest.cc
+++ chrome/browser/net/proxy_browsertest.cc
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/bind.h"
+#include "base/test/run_until.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
-#include "chrome/browser/auth_notification_types.h"
 #include "chrome/browser/net/proxy_test_utils.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -24,8 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/browser_thread.h"
-#include "content/public/browser/notification_details.h"
-#include "content/public/browser/notification_source.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
@@ -62,35 +60,6 @@ void VerifyProxyScript(Browser* browser) {
                       "hasError;"));
 }
 
-// This class observes chrome::NOTIFICATION_AUTH_NEEDED and supplies
-// the credential which is required by the test proxy server.
-// "foo:bar" is the required username and password for our test proxy server.
-class LoginPromptObserver : public content::NotificationObserver {
- public:
-  LoginPromptObserver() : auth_handled_(false) {}
-
-  LoginPromptObserver(const LoginPromptObserver&) = delete;
-  LoginPromptObserver& operator=(const LoginPromptObserver&) = delete;
-
-  void Observe(int type,
-               const content::NotificationSource& source,
-               const content::NotificationDetails& details) override {
-    if (type == chrome::NOTIFICATION_AUTH_NEEDED) {
-      LoginNotificationDetails* login_details =
-          content::Details<LoginNotificationDetails>(details).ptr();
-      // |login_details->handler()| is the associated LoginHandler object.
-      // SetAuth() will close the login dialog.
-      login_details->handler()->SetAuth(u"foo", u"bar");
-      auth_handled_ = true;
-    }
-  }
-
-  bool auth_handled() const { return auth_handled_; }
-
- private:
-  bool auth_handled_;
-};
-
 // Test that the browser can establish a WebSocket connection via a proxy
 // that requires basic authentication. This test also checks the headers
 // arrive at WebSocket server.
@@ -102,13 +71,6 @@ IN_PROC_BROWSER_TEST_F(ProxyBrowserTest, BasicAuthWSConnect) {
 
   content::WebContents* tab =
       browser()->tab_strip_model()->GetActiveWebContents();
-  content::NavigationController* controller = &tab->GetController();
-  content::NotificationRegistrar registrar;
-  // The proxy server will request basic authentication.
-  // |observer| supplies the credential.
-  LoginPromptObserver observer;
-  registrar.Add(&observer, chrome::NOTIFICATION_AUTH_NEEDED,
-                content::Source<content::NavigationController>(controller));
 
   content::TitleWatcher watcher(tab, u"PASS");
   watcher.AlsoWaitForTitle(u"FAIL");
@@ -121,9 +83,12 @@ IN_PROC_BROWSER_TEST_F(ProxyBrowserTest, BasicAuthWSConnect) {
       browser(), ws_server.GetURL("proxied_request_check.html")
                      .ReplaceComponents(replacements)));
 
+  ASSERT_TRUE(base::test::RunUntil(
+      []() { return LoginHandler::GetAllLoginHandlersForTest().size() == 1; }));
+  LoginHandler::GetAllLoginHandlersForTest().front()->SetAuth(u"foo", u"bar");
+
   const std::u16string result = watcher.WaitAndGetTitle();
   EXPECT_TRUE(base::EqualsASCII(result, "PASS"));
-  EXPECT_TRUE(observer.auth_handled());
 }
 
 // Fetches a PAC script via an http:// URL, and ensures that requests to
