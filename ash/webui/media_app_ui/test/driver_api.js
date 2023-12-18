aba45b1617af09caa3abad3801ad5ce5ba1b19ac
Trent Apted <tapted@chromium.org>
2023-12-18 05:01:41 +0000

    Convert the media app "guest_query_receiver.js" to TypeScript.
    The type definitions in `driver_api.js` are also converted to simplify
    the conversion. But the .js file is still consumed by Closure for the
    "host" driver.js, so it's kept around for now.
    
    Bug: b/314827247
    Change-Id: I658cef5f7d9628825e03f0e83d2ba1c8298225ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5091307
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Reviewed-by: Patti Lor <patricialor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238501}

--- ash/webui/media_app_ui/test/driver_api.js
+++ ash/webui/media_app_ui/test/driver_api.js
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/** @fileoverview Deprecated JS types for host side. Delete! */
+
 /**
  * Reply to test messages. Contents depend on the test message sent.
  * @typedef {{
