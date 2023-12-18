c3149295f75e5cab449b10b918ee093e8a25842e
Mohamed Heikal <mheikal@chromium.org>
2023-12-18 22:16:00 +0000

    Cleanup AndroidSearchEngineChoiceNotification field trial code
    Flag has been default enabled with no finch trial running for some time
    now. Remove flag code and use the supplied defaults.
    
    Bug: 1385901
    Change-Id: I8a25e02252fe2418b305b1491bcca5fee0e7c967
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5116257
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1238801}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -162,7 +162,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kAndroidAppIntegration,
     &kAndroidHatsRefactor,
     &kAndroidHub,
-    &kAndroidSearchEngineChoiceNotification,
     &kAndroidImprovedBookmarks,
     &kAndroidNoVisibleHintForTablets,
     &kAndroidVisibleUrlTruncation,
@@ -434,10 +433,6 @@ BASE_FEATURE(kAndroidHatsRefactor,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAndroidHub, "AndroidHub", base::FEATURE_DISABLED_BY_DEFAULT);
-
-BASE_FEATURE(kAndroidSearchEngineChoiceNotification,
-             "AndroidSearchEngineChoiceNotification",
-             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAndroidImprovedBookmarks,
              "AndroidImprovedBookmarks",
