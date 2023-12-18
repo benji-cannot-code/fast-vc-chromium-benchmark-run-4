19746fb92417abd0436ad974df9210acbf11b9a3
Andres Calderon Jaramillo <andrescj@chromium.org>
2023-12-18 18:56:43 +0000

    V4L2VD: Explicitly check for DRM_FORMAT_MOD_LINEAR
    This CL simply replaces an `if (layout->modifier()` with
    `if (layout->modifier() != DRM_FORMAT_MOD_LINEAR` to make it explicit
    that we're checking for the presence of a non-linear modifier and not
    for just the presence of any modifier.
    
    There should be no functional change because at the time of this CL,
    DRM_FORMAT_MOD_LINEAR happens to be 0 [1].
    
    [1] https://source.chromium.org/chromiumos/chromiumos/codesearch/+/main:src/third_party/kernel/upstream/include/uapi/drm/drm_fourcc.h;l=488;drc=728c15b4b5f3369cbde73d5e0f14701ab370f985
    
    Change-Id: I8fa366feddd230bc61a814bb2bc6cc5fd2e3bf0f
    Fixed: b:316627749
    Test: None
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5122483
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Commit-Queue: Miguel Casas-Sanchez <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238625}

--- media/gpu/v4l2/v4l2_video_decoder.cc
+++ media/gpu/v4l2/v4l2_video_decoder.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/gpu/v4l2/v4l2_video_decoder.h"
 
+#include <drm_fourcc.h>
+
 #include <algorithm>
 
 #include "base/containers/contains.h"
@@ -737,7 +739,7 @@ CroStatus V4L2VideoDecoder::SetupOutputFormat(const gfx::Size& size,
     }
 
     VLOGF(1) << "buffer modifier: " << std::hex << layout->modifier();
-    if (layout->modifier() &&
+    if (layout->modifier() != DRM_FORMAT_MOD_LINEAR &&
         layout->modifier() != gfx::NativePixmapHandle::kNoModifier) {
       absl::optional<struct v4l2_format> modifier_format =
           output_queue_->SetModifierFormat(layout->modifier(), picked_size);
