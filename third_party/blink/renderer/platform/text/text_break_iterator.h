df2bbd1041df0e8bd919f351d23c213b99603b6b
Bastian Kersting <bkersting@google.com>
2023-12-18 08:08:55 +0000

    Revert "[BreakIterator] Refactor by adding `Context`"
    This reverts commit a8b10f34a154feb602d10097d2aca0b9d48af19c.
    
    Reason for revert: This introduced errors on the Linux MSAN builder. E.g. https://ci.chromium.org/ui/p/chromium/builders/ci/WebKit%20Linux%20MSAN/24235/overview
    
    Original change's description:
    > [BreakIterator] Refactor by adding `Context`
    >
    > This patch refactors `LazyLineBreakIterator`, in particular,
    > its hottest function `NextBreakablePosition` by introducing
    > the `Context` struct.
    >
    > The `Context` struct caches 3 characters at the index, and is
    > responsible for advancing the current index. This refactoring
    > is a preparation for improving the performance of
    > `PreviousBreakOpportunity`.
    >
    > Note, the current index isn't part of the struct, because
    > doing so seems to have a negative impact.
    >
    > This patch has no behavior changes.
    >
    > Bug: 1512434
    > Change-Id: I051eed75c2a666b8ef183154c0a7adebba7d3a85
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5119978
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Auto-Submit: Koji Ishii <kojii@chromium.org>
    > Commit-Queue: Kent Tamura <tkent@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1238475}
    
    Bug: 1512434
    Change-Id: Ia925812ee0e6d93c66b018e7dddf50baf33977c8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5130190
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238525}

--- third_party/blink/renderer/platform/text/text_break_iterator.h
+++ third_party/blink/renderer/platform/text/text_break_iterator.h
@@ -228,9 +228,6 @@ class PLATFORM_EXPORT LazyLineBreakIterator final {
  private:
   FRIEND_TEST_ALL_PREFIXES(TextBreakIteratorTest, Strictness);
 
-  template <typename CharacterType>
-  struct Context;
-
   const AtomicString& LocaleWithKeyword() const;
   void InvalidateLocaleWithKeyword();
 
