02f9eb677c323d94f81c34b1c461d31549c6d4c3
Dmitry Vykochko <vykochko@google.com>
2023-12-18 19:47:34 +0000

    Revert "Get ContentAutofillDriverFactory::DidFinishNavigation early return back."
    This reverts commit e957c5e67218403ebfee98dffcc7e568e479b8d7.
    
    Reason for revert: The CL was landed for temp experimentation, the hypothesis of the "Autofill.Funnel.ParsedAsType.*" metrics drop reason was proofed and the CL can be reverted
    
    Original change's description:
    > Get ContentAutofillDriverFactory::DidFinishNavigation early return back.
    >
    > crrev.com/c/4896011 removed an early return
    > `ContentAutofillDriverFactory::DidFinishNavigation`.
    >
    > We believe that this was the root cause of a change in our metrics.
    > The purpose of this CL is to temporarily bring the lines back to
    > verify the hypothesis. Once verified, the temporary lines will be
    > removed again.
    >
    > We believe that `driver->Reset()` at the end of
    > `ContentAutofillDriverFactory::DidFinishNavigation` led to resetting
    > the metric instance variable and that this caused to a change in the
    > number of submitted samples, which affected metrics of not-submitted
    > Address and Credit Card forms.
    >
    > Bug: 1492636
    > Change-Id: I9e2175f0480f762a418392a94259da1f142722ce
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5014086
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Christoph Schwering <schwering@google.com>
    > Reviewed-by: Dominic Battre <battre@chromium.org>
    > Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1224822}
    
    Bug: 1492636
    Change-Id: Idea933bb3f8a04b9c9024c9d6441f4e2e43dab76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5104775
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238666}

--- components/android_autofill/browser/autofill_provider_android_unittest.cc
+++ components/android_autofill/browser/autofill_provider_android_unittest.cc
@@ -943,11 +943,6 @@ class AutofillProviderAndroidTestHidingLogic
     sub_frame_ = content::RenderFrameHostTester::For(main_frame())
                      ->AppendChild(std::string("child"));
     sub_frame_ = NavigateAndCommitFrame(sub_frame_, GURL("https://bar.com"));
-    // Make sure the driver (and the manager) is created as there is an early
-    // return in `ContentAutofillDriverFactory::DidFinishNavigation` before
-    // `DriverForFrame()` call.
-    ContentAutofillDriverFactory::FromWebContents(web_contents())
-        ->DriverForFrame(sub_frame_);
   }
 
   void TearDown() override {
@@ -1005,7 +1000,7 @@ TEST_F(AutofillProviderAndroidTestHidingLogic, HideInMainFrameOnDestruction) {
 // *sub frame* hides the popup.
 TEST_F(AutofillProviderAndroidTestHidingLogic, HideInSubFrameOnDestruction) {
   AskForValuesToFill(sub_frame_);
-  EXPECT_CALL(provider_bridge(), HideDatalistPopup).Times(AtLeast(1));
+  EXPECT_CALL(provider_bridge(), Reset);
   NavigateAndCommitFrame(sub_frame_, GURL("https://bar.com/"));
   // Verify and clear before TearDown() closes the popup.
   Mock::VerifyAndClearExpectations(&provider_bridge());
@@ -1048,7 +1043,7 @@ TEST_F(AutofillProviderAndroidTestHidingLogic,
        FollowAskForValuesInDifferentFrames) {
   AskForValuesToFill(main_frame());
   AskForValuesToFill(sub_frame_);
-  EXPECT_CALL(provider_bridge(), HideDatalistPopup).Times(AtLeast(1));
+  EXPECT_CALL(provider_bridge(), Reset);
   NavigateAndCommitFrame(sub_frame_, GURL("https://bar.com/"));
 }
 
