8d79824dfda10e250127f801024c5a46f9e612ac
Christoph Schwering <schwering@google.com>
2023-12-19 12:22:19 +0000

    [Autofill] Remove FillOrPreviewField() from AutofillManager
    All callers call BAM::FillOrPreviewField(), so there's no need for
    a virtual AM::FillOrPreviewField().
    
    Unfortunately this comes at the cost of a static_cast in
    ComposeManagerImpl.
    
    Bug: 1007974
    Change-Id: Iba252f20488b213e88771e3039ba0a94db9d21b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5116540
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1239066}

--- components/compose/core/browser/compose_manager_impl.cc
+++ components/compose/core/browser/compose_manager_impl.cc
@@ -13,11 +13,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "components/autofill/core/browser/autofill_client.h"
 #include "components/autofill/core/browser/autofill_manager.h"
+#include "components/autofill/core/browser/browser_autofill_manager.h"
 #include "components/compose/core/browser/compose_client.h"
 #include "components/compose/core/browser/compose_features.h"
 #include "components/compose/core/browser/compose_metrics.h"
 
 namespace {
+
 // Passes the autofill `text` back into the `field` the dialog was opened on.
 // Called upon insertion.
 void FillTextWithAutofill(base::WeakPtr<autofill::AutofillManager> manager,
@@ -29,10 +31,11 @@ void FillTextWithAutofill(base::WeakPtr<autofill::AutofillManager> manager,
   if (!manager) {
     return;
   }
-  manager->FillOrPreviewField(
-      autofill::mojom::ActionPersistence::kFill,
-      autofill::mojom::TextReplacement::kReplaceSelection, form, field,
-      trimmed_text, autofill::PopupItemId::kCompose);
+  static_cast<autofill::BrowserAutofillManager*>(manager.get())
+      ->FillOrPreviewField(autofill::mojom::ActionPersistence::kFill,
+                           autofill::mojom::TextReplacement::kReplaceSelection,
+                           form, field, trimmed_text,
+                           autofill::PopupItemId::kCompose);
 }
 
 }  // namespace
