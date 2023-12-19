60eabb5e2f39a165769419218425c0065e01ddb6
Joel Hockey <joelhockey@chromium.org>
2023-12-19 08:59:26 +0000

    EyeDropperView test for cursor and touch movement
    Bug: b/316075687
    Change-Id: I094d0cebfd290ce88779216546562b9493865d7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5125876
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238996}

--- components/eye_dropper/eye_dropper_view.cc
+++ components/eye_dropper/eye_dropper_view.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ui/aura/client/screen_position_client.h"
-#include "ui/display/screen.h"
 #endif
 
 namespace eye_dropper {
@@ -48,8 +47,6 @@ class EyeDropperView::ViewPositionHandler {
 
   // Timer used for updating the window location.
   base::RepeatingTimer timer_;
-  gfx::Point last_cursor_position_ =
-      display::Screen::GetScreen()->GetCursorScreenPoint();
   raw_ptr<EyeDropperView> owner_;
 };
 
@@ -66,14 +63,8 @@ EyeDropperView::ViewPositionHandler::~ViewPositionHandler() {
 }
 
 void EyeDropperView::ViewPositionHandler::UpdateViewPosition() {
-  // The view can be moved by either mouse or touch. Only move it to the cursor
-  // position when cursor changes.
-  gfx::Point cursor_position =
-      display::Screen::GetScreen()->GetCursorScreenPoint();
-  if (std::exchange(last_cursor_position_, cursor_position) !=
-      cursor_position) {
-    owner_->UpdatePosition(cursor_position);
-  }
+  owner_->OnCursorPositionUpdate(
+      display::Screen::GetScreen()->GetCursorScreenPoint());
 }
 
 class EyeDropperView::ScreenCapturer
@@ -234,7 +225,7 @@ EyeDropperView::EyeDropperView(gfx::NativeView parent,
   pre_dispatch_handler_ =
       std::make_unique<PreEventDispatchHandler>(this, event_handler);
   widget->Show();
-  CaptureInputIfNeeded();
+  CaptureInput();
   auto* screen = display::Screen::GetScreen();
   gfx::Point initial_position = screen->GetCursorScreenPoint();
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -257,6 +248,15 @@ EyeDropperView::EyeDropperView(gfx::NativeView parent,
 EyeDropperView::~EyeDropperView() {
   if (GetWidget()) {
     GetWidget()->CloseNow();
+  }
+}
+
+void EyeDropperView::OnCursorPositionUpdate(gfx::Point cursor_position) {
+  // The view can be moved by either mouse or touch. Only move it to the cursor
+  // position when cursor changes.
+  if (std::exchange(last_cursor_position_, cursor_position) !=
+      cursor_position) {
+    UpdatePosition(std::move(cursor_position));
   }
 }
 
