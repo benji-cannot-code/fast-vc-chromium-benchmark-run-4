1318bb509c466fc8fd9bcd8f36793d1671f1e823
Marc Treib <treib@chromium.org>
2023-12-19 16:23:44 +0000

    SyncToSigninMigration: Add ReadingList coverage to EG test
    This extends the EG test added in crrev.com/c/5119751 to also cover
    the migration of a reading list entry.
    
    Bug: 1486420
    Change-Id: Ic9aa5babfe9163465bcc304e99f1ea0992d1bda7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5134511
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239178}

--- ios/chrome/browser/ui/reading_list/reading_list_egtest_utils.mm
+++ ios/chrome/browser/ui/reading_list/reading_list_egtest_utils.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/reading_list/reading_list_app_interface.h"
 #import "ios/chrome/browser/ui/reading_list/reading_list_constants.h"
+#import "ios/chrome/common/ui/table_view/table_view_cells_constants.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
@@ -32,6 +33,12 @@ id<GREYMatcher> ReadingListItem(NSString* entryTitle) {
 id<GREYMatcher> VisibleReadingListItem(NSString* entryTitle) {
   return grey_allOf(grey_accessibilityID(entryTitle),
                     grey_kindOfClassName(@"TableViewURLCell"),
+                    grey_sufficientlyVisible(), nil);
+}
+
+id<GREYMatcher> VisibleLocalItemIcon(NSString* title) {
+  return grey_allOf(grey_ancestor(ReadingListItem(title)),
+                    grey_accessibilityID(kTableViewURLCellMetadataImageID),
                     grey_sufficientlyVisible(), nil);
 }
 
