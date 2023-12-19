e99a092be29dd2024e78749bcdfe3e3b447f55f9
Sebastien Lalancette <seblalancette@chromium.org>
2023-12-19 20:13:04 +0000

    [DTC] Update DTC E2E Test To Use Latest Policy
    Updated to the latest DTC policy, as the old one has been marked
    as deprecated. Also remove the feature flag parameter, as it has been
    completely removed.
    
    Change-Id: I6b7a21d954dbaae9d5a12bcec28531cd4b3f03c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5137197
    Commit-Queue: Arthur Wang <wuwang@chromium.org>
    Reviewed-by: Arthur Wang <wuwang@chromium.org>
    Auto-Submit: Sébastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239290}

--- chrome/test/enterprise/e2e/connector/common/device_trust_ui_test.py
+++ chrome/test/enterprise/e2e/connector/common/device_trust_ui_test.py
@@ -33,7 +33,7 @@ flags.DEFINE_string(
 _POLICY_URL = 'chrome://policy'
 _CONNECTOR_INTERNAL_URL = 'chrome://connectors-internals'
 _POLICY_CONTEXT_AWARE_ACCESS_SIGNALS_ALLOWLIST = (
-    'ContextAwareAccessSignalsAllowlist')
+    'BrowserContextAwareAccessSignalsAllowlist')
 _TIMEOUT = 10
 
 
@@ -61,7 +61,6 @@ def main(argv):
   exclude_switches = ['disable-background-networking']
   chrome_options = webdriver.ChromeOptions()
   chrome_options.add_experimental_option('excludeSwitches', exclude_switches)
-  chrome_options.add_argument('--enable-features=DeviceTrustConnectorEnabled')
   chrome_options.add_argument('--enable-stats-collection-bindings')
   results = {}
   test_cases = ['key_creation', 'key_load']
