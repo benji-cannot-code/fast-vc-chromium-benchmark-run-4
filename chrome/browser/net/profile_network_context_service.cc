c4cf0ab423a6f45454729fff86c5ee94f72e7879
Hubert Chao <hchao@chromium.org>
2023-12-19 17:51:16 +0000

    Cert Management Enterprise Policy: Implement CADistrustedCertificates as future_on policy.
    Change-Id: Ia05e10d0fb39099dfdf91262bd0d9cd2ea2b1e76
    Bug: 1477317
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5067456
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Victor Gabriel Savu <vsavu@google.com>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239218}

--- chrome/browser/net/profile_network_context_service.cc
+++ chrome/browser/net/profile_network_context_service.cc
@@ -85,6 +85,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/network_service.mojom.h"
 #include "third_party/blink/public/common/features.h"
 
+#if BUILDFLAG(CHROME_CERTIFICATE_POLICIES_SUPPORTED)
+#include "net/cert/asn1_util.h"
+#endif
+
 #if BUILDFLAG(IS_CHROMEOS)
 #include "chrome/browser/certificate_provider/certificate_provider.h"
 #include "chrome/browser/certificate_provider/certificate_provider_service.h"
@@ -308,6 +312,11 @@ ProfileNetworkContextService::ProfileNetworkContextService(Profile* profile)
           &ProfileNetworkContextService::ScheduleUpdateCertificatePolicy,
           base::Unretained(this)));
 
+  pref_change_registrar_.Add(
+      prefs::kCADistrustedCertificates,
+      base::BindRepeating(
+          &ProfileNetworkContextService::ScheduleUpdateCertificatePolicy,
+          base::Unretained(this)));
 #endif
 
   pref_change_registrar_.Add(
@@ -381,6 +390,7 @@ void ProfileNetworkContextService::RegisterProfilePrefs(
   registry->RegisterListPref(prefs::kHSTSPolicyBypassList);
 #if BUILDFLAG(CHROME_CERTIFICATE_POLICIES_SUPPORTED)
   registry->RegisterListPref(prefs::kCACertificates);
+  registry->RegisterListPref(prefs::kCADistrustedCertificates);
 #endif
 }
 
@@ -536,6 +546,7 @@ cert_verifier::mojom::AdditionalCertificatesPtr
 ProfileNetworkContextService::GetCertificatePolicy() {
   net::CertificateList additional_untrusted_anchors;
   net::CertificateList additional_trust_anchors;
+  std::vector<std::vector<uint8_t>> additional_distrusted_spkis;
   auto* prefs = profile_->GetPrefs();
 
   for (const base::Value& cert_b64 : prefs->GetList(prefs::kCACertificates)) {
@@ -557,9 +568,24 @@ ProfileNetworkContextService::GetCertificatePolicy() {
     }
   }
 
+  for (const base::Value& cert_b64 :
+       prefs->GetList(prefs::kCADistrustedCertificates)) {
+    std::string decoded;
+    if (!base::Base64Decode(cert_b64.GetString(), &decoded)) {
+      continue;
+    }
+    base::StringPiece spki_piece;
+    bool success = net::asn1::ExtractSPKIFromDERCert(decoded, &spki_piece);
+    if (success) {
+      additional_distrusted_spkis.push_back(
+          std::vector<uint8_t>(spki_piece.begin(), spki_piece.end()));
+    }
+  }
+
   return cert_verifier::mojom::AdditionalCertificates::New(
       std::move(additional_untrusted_anchors),
-      std::move(additional_trust_anchors));
+      std::move(additional_trust_anchors),
+      std::move(additional_distrusted_spkis));
 }
 
 void ProfileNetworkContextService::UpdateCertificatePolicy() {
--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -2070,6 +2070,9 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
   { key::kCACertificates,
     prefs::kCACertificates,
     base::Value::Type::LIST },
+  { key::kCADistrustedCertificates,
+    prefs::kCADistrustedCertificates,
+    base::Value::Type::LIST },
 #endif // BUILDFLAG(CHROME_CERTIFICATE_POLICIES_SUPPORTED)
 };
 // clang-format on
