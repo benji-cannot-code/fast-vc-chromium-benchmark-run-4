d94a29e19a7d433db894269b0fb7f3e8da84b8e6
Fiona Macintosh <fmacintosh@google.com>
2023-12-19 14:01:46 +0000

    [IP] Create a new pref for the IP protection settings toggle
    See browser UI section in PRD: go/gnatcatcher-prd. This pref will be
    linked to a toggle in settings that lets users turn on/off the IP
    protection feature.
    
    Bug: b:316171695
    Change-Id: I307c666d5f0d682322e4f4ceb32b2ce35c2e613c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5131716
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239108}

--- components/privacy_sandbox/tracking_protection_settings.h
+++ components/privacy_sandbox/tracking_protection_settings.h
@@ -43,6 +43,9 @@ class TrackingProtectionSettings
   // (i.e. without mitigations).
   bool AreAllThirdPartyCookiesBlocked() const;
 
+  // Returns whether IP protection is enabled.
+  bool IsIpProtectionEnabled() const;
+
   // From TrackingProtectionOnboarding::Observer
   void OnTrackingProtectionOnboardingUpdated(
       TrackingProtectionOnboarding::OnboardingStatus onboarding_status)
@@ -55,6 +58,7 @@ class TrackingProtectionSettings
   void OnDoNotTrackEnabledPrefChanged();
   void OnBlockAllThirdPartyCookiesPrefChanged();
   void OnTrackingProtection3pcdPrefChanged();
+  void OnIpProtectionPrefChanged();
 
   base::ObserverList<TrackingProtectionSettingsObserver>::Unchecked observers_;
   PrefChangeRegistrar pref_change_registrar_;
