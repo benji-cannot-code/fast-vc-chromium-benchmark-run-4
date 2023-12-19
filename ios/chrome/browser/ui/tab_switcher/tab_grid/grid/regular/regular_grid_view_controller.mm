81ec1cba705d366eb23926fac932d43e355eaf61
Louis Romero <lpromero@google.com>
2023-12-19 09:58:40 +0000

    Don't show the InactiveTabsPreambleHeader when Inactive Tabs is disabled
    This CL fixes the test
    InactiveTabsTestCase/testSettingsChangesPopsInactiveTabs when
    chrome://flags#tab-grid-compositional-layout is enabled.
    
    Indeed, with the current (flow) layout, this doesn't happen because the
    layout delegate specifies a CGSizeZero for the header
    (https://source.chromium.org/chromium/chromium/src/+/main:ios/chrome/browser/ui/tab_switcher/tab_grid/grid/regular/regular_grid_view_controller.mm;l=73-75;drc=fb8d5d4c50bbc9c566a849377705b10905bb7209)
    
    But this method is not called with the new compositional layout API, so
    we need to take care of hiding it when configuring it.
    
    Bug: 1485895
    Change-Id: If990dd88cf00c668fe2f2c8a0b6cd60feeac5b89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5132335
    Auto-Submit: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1239009}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/regular/regular_grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/regular/regular_grid_view_controller.mm
@@ -379,6 +379,7 @@ constexpr base::TimeDelta kInactiveTabsHeaderAnimationDuration =
     [weakSelf didTapInactiveTabsSettingsLink];
   };
   header.daysThreshold = _inactiveTabsDaysThreshold;
+  header.hidden = !IsInactiveTabsEnabled();
 }
 
 @end
--- ios/chrome/browser/ui/tab_switcher/tab_grid/inactive_tabs/inactive_tabs_preamble_header.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/inactive_tabs/inactive_tabs_preamble_header.mm
@@ -63,7 +63,6 @@ const CGFloat kHorizontalPadding = 16;
 }
 
 - (void)setDaysThreshold:(NSInteger)daysThreshold {
-  DCHECK_NE(daysThreshold, kInactiveTabsDisabledByUser);
   _daysThreshold = daysThreshold;
 
   // Update the text view's attributed text.
