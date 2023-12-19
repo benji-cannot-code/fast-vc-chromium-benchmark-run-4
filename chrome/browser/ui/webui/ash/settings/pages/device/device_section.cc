8fb5623799a65aa30b2ca348f10fda983d905c20
YuhanYang <yyhyyh@google.com>
2023-12-19 21:01:17 +0000

    customization: Update help section on mouse button page
    Put help section in a banner, and add a mouse icon.
    
    Screenshot:
    - https://screenshot.googleplex.com/Pgt7ey8bVZAHpM5
    Mock:
    - https://screenshot.googleplex.com/BKQquMMasqQzftT.png
    
    Bug: b/241965717
    Test: Visual effect on DUT and VM
    Change-Id: I53e829026f24053ec68638021ecea2b2375c364f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5126264
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1239327}

--- chrome/browser/ui/webui/ash/settings/pages/device/device_section.cc
+++ chrome/browser/ui/webui/ash/settings/pages/device/device_section.cc
@@ -1773,6 +1773,8 @@ void DeviceSection::AddCustomizeButtonsPageStrings(
        IDS_SETTINGS_CUSTOMIZE_BUTTONS_RENAMING_DIALOG_TITLE},
       {"customizeButtonSubpageDescription",
        IDS_SETTINGS_CUSTOMIZE_BUTTONS_SUBPAGE_DESCRIPTION},
+      {"customizeMouseButtonsNudgeHeader",
+       IDS_SETTINGS_CUSTOMIZE_MOUSE_BUTTONS_NUDGE_HEADER},
       {"customizeMouseButtonsTitle",
        IDS_SETTINGS_CUSTOMIZE_MOUSE_BUTTONS_TITLE},
       {"disbableOptionLabel", IDS_SETTINGS_DISABLE_OPTION_LABEL},
