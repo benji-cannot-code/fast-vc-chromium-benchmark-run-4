d87e011a806fb4fcdb6acf6f23ad80a244c22368
Andrea Gomez <andreaxg@google.com>
2023-12-19 17:23:56 +0000

    Record if and why the user is ineligible for ReadAloud
    Adds an enum to record if the user is ineligible for ReadAloud and an
    enum to record why they're ineligible.
    
    Bug: 315531599
    Change-Id: Ib505c9ba10470d706694911305e962fcbb82d327
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5106001
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Andrea Gomez <andreaxg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1239204}

--- chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/ReadAloudMetrics.java
+++ chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/ReadAloudMetrics.java
@@ -5,14 +5,61 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.readaloud;
 
+import androidx.annotation.IntDef;
 import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.metrics.RecordHistogram;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 public class ReadAloudMetrics {
     @VisibleForTesting public static String READABILITY_SUCCESS = "ReadAloud.IsPageReadable";
 
+    @VisibleForTesting
+    public static String INELIGIBILITY_REASON = "ReadAloud.Eligibility.IneligiblityReason";
+
+    @VisibleForTesting
+    public static String IS_USER_ELIGIBLE = "ReadAloud.Eligibility.IsUserEligible";
+
+    /**
+     * The reason why we clear the prepared message.
+     *
+     * <p>Needs to stay in sync with ReadAloudIneligibilityReason in enums.xml. These values are
+     * persisted to logs. Entries should not be renumbered and numeric values should never be
+     * reused.
+     */
+    @IntDef({
+        IneligibilityReason.UNKNOWN,
+        IneligibilityReason.FEATURE_FLAG_DISABLED,
+        IneligibilityReason.INCOGNITO_MODE,
+        IneligibilityReason.MSBB_DISABLED,
+        IneligibilityReason.POLICY_DISABLED,
+        IneligibilityReason.DEFAULT_SEARCH_ENGINE_GOOGLE_FALSE,
+        IneligibilityReason.COUNT
+    })
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface IneligibilityReason {
+        int UNKNOWN = 0;
+        int FEATURE_FLAG_DISABLED = 1;
+        int INCOGNITO_MODE = 2;
+        int MSBB_DISABLED = 3;
+        int POLICY_DISABLED = 4;
+        int DEFAULT_SEARCH_ENGINE_GOOGLE_FALSE = 5;
+        // Always update COUNT to match the last reason in the list.
+        int COUNT = 6;
+    }
+
     public static void recordIsPageReadable(boolean successful) {
         RecordHistogram.recordBooleanHistogram(READABILITY_SUCCESS, successful);
+    }
+
+    public static void recordIsUserEligible(boolean eligible) {
+        RecordHistogram.recordBooleanHistogram(IS_USER_ELIGIBLE, eligible);
+    }
+
+    public static void recordIneligibilityReason(@IneligibilityReason int reason) {
+        RecordHistogram.recordEnumeratedHistogram(
+                INELIGIBILITY_REASON, reason, IneligibilityReason.COUNT);
     }
 }
--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/adaptive/settings/AdaptiveToolbarSettingsFragmentTest.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/adaptive/settings/AdaptiveToolbarSettingsFragmentTest.java
@@ -88,6 +88,7 @@ public class AdaptiveToolbarSettingsFragmentTest {
         AdaptiveToolbarFeatures.setProfile(mProfile);
 
         VoiceRecognitionUtil.setIsVoiceSearchEnabledForTesting(true);
+        UnifiedConsentServiceBridge.setUrlKeyedAnonymizedDataCollectionEnabled(true);
 
         TemplateUrlServiceFactory.setInstanceForTesting(mTemplateUrlService);
     }
@@ -358,7 +359,6 @@ public class AdaptiveToolbarSettingsFragmentTest {
     @SmallTest
     @EnableFeatures(ChromeFeatureList.READALOUD)
     public void testReadAloudOption_Enabled() {
-        UnifiedConsentServiceBridge.setUrlKeyedAnonymizedDataCollectionEnabled(true);
         FragmentScenario<AdaptiveToolbarSettingsFragment> scenario =
                 FragmentScenario.launchInContainer(
                         AdaptiveToolbarSettingsFragment.class,
