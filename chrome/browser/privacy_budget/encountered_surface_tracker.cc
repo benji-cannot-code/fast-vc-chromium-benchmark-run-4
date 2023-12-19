ad7b3e5546869bd8cdeb22943393471b2310faa7
Antonio Sartori <antoniosartori@chromium.org>
2023-12-19 14:04:48 +0000

    [IdentifiabilityStudy] Use an LRU cache for encountered surfaces
    This CL revisits https://crrev.com/c/5125952, which was a quick fix
    for a bug in tracking encountered surfaces.
    
    On a second thought, it might be better to use a standard LRU cache in
    order to deduplicate reported surfaces, instead of relying on a
    "manual" caching logic.
    
    Change-Id: I8f1319f633ab9a73ca85472789cb33be70cf7679
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5130928
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239109}

--- chrome/browser/privacy_budget/encountered_surface_tracker.cc
+++ chrome/browser/privacy_budget/encountered_surface_tracker.cc
@@ -5,25 +5,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/privacy_budget/encountered_surface_tracker.h"
 
-#include <set>
-#include <vector>
+#include <stdint.h>
+#include <utility>
 
-#include "base/rand_util.h"
-#include "base/time/time.h"
-#include "chrome/browser/privacy_budget/identifiability_study_state.h"
+#include "base/containers/lru_cache.h"
 #include "third_party/blink/public/common/privacy_budget/identifiable_surface.h"
 
-const unsigned EncounteredSurfaceTracker::kMaxTrackedSurfaces;
-const unsigned EncounteredSurfaceTracker::kMaxTrackedSources;
+const unsigned EncounteredSurfaceTracker::kMaxTrackedEntries;
 
-EncounteredSurfaceTracker::EncounteredSurfaceTracker() {
-  Reset();
-}
+EncounteredSurfaceTracker::EncounteredSurfaceTracker()
+    : surfaces_(base::LRUCacheSet<std::pair<uint64_t, uint64_t>>(
+          kMaxTrackedEntries)) {}
 
 EncounteredSurfaceTracker::~EncounteredSurfaceTracker() = default;
 
 void EncounteredSurfaceTracker::Reset() {
-  surfaces_.clear();
+  surfaces_.Clear();
 }
 
 bool EncounteredSurfaceTracker::IsNewEncounter(uint64_t source_id,
@@ -32,24 +29,9 @@ bool EncounteredSurfaceTracker::IsNewEncounter(uint64_t source_id,
       blink::IdentifiableSurface::Type::kReservedInternal)
     return false;
 
-  auto it = surfaces_.find(surface);
-  if (it == surfaces_.end()) {
-    // We need to add an entry for this surface, possibly bumping an entry out.
-    surfaces_.insert(std::make_pair(surface, std::set<uint64_t>{source_id}));
-    if (surfaces_.size() > kMaxTrackedSurfaces) {
-      // Remove a random one.
-      surfaces_.erase(base::RandInt(0, kMaxTrackedSurfaces));
-    }
-    return true;
-  }
-
-  if (it->second.contains(source_id))
+  if (surfaces_.Get(std::make_pair(source_id, surface)) != surfaces_.end()) {
     return false;
-
-  it->second.insert(source_id);
-  if (it->second.size() > kMaxTrackedSources) {
-    // Remove a random one.
-    it->second.erase(base::RandInt(0, kMaxTrackedSources));
   }
+  surfaces_.Put(std::make_pair(source_id, surface));
   return true;
 }
