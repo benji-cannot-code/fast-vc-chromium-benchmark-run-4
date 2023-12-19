75f3c04e7ff3eead2463b2664925b9996e5eb49f
Rakina Zata Amni <rakina@chromium.org>
2023-12-19 23:41:58 +0000

    Add metrics for time from compositor creation to nav commit/RFH swap
    This metric will help us see if the surface embedding time
    (currently happens at RFH swap) can improve significantly if we
    move it to commit time instead.
    
    Change-Id: I63fe66fab1f78d8638cb909c34b791ffae310e01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5086331
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239440}

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -3702,6 +3702,8 @@ void RenderWidgetHostImpl::MaybeDispatchBufferedFrameSinkRequest() {
     return;
   }
 
+  create_frame_sink_timestamp_ = base::TimeTicks::Now();
+
   std::move(create_frame_sink_callback_).Run(view_->GetFrameSinkId());
 }
 
