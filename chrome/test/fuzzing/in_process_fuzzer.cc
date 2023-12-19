c3eb3c2332a8f29be15e678f7d4f435b7f4eed0a
Adrian Taylor <adetaylor@chromium.org>
2023-12-19 15:01:54 +0000

    Initialize test timeouts in in_process_fuzzer.
    Bug: 1508313
    Change-Id: Ibd08d75469aaf8dae14f3fc03fffbb6cee32046c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5087694
    Reviewed-by: Paul Semel <paulsemel@chromium.org>
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239131}

--- chrome/test/fuzzing/in_process_fuzzer.cc
+++ chrome/test/fuzzing/in_process_fuzzer.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/test/bind.h"
 #include "base/test/scoped_run_loop_timeout.h"
+#include "base/test/test_timeouts.h"
 #include "chrome/test/base/chrome_test_launcher.h"
 #include "chrome/test/fuzzing/in_process_fuzzer.h"
 #include "chrome/test/fuzzing/in_process_fuzzer_buildflags.h"
@@ -216,6 +217,11 @@ int main(int argc, char** argv) {
     chromium_arguments.push_back(FILE_PATH_LITERAL("--no-zygote"));
     chromium_arguments.push_back(FILE_PATH_LITERAL("--disable-gpu"));
     base::CommandLine::ForCurrentProcess()->InitFromArgv(chromium_arguments);
+
+    // Various bits of setup are done by base::TestSuite::Initialize.
+    // As we're not a functional test suite, most of those things are not
+    // necessary, but at least this is:
+    TestTimeouts::Initialize();
   }
 
   FuzzTestLauncherDelegate* fuzzer_launcher_delegate =
