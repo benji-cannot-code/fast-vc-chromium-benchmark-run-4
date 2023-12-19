e7810dc3a4c3d34e2fac2657a65415ec4a585978
Christian Dullweber <dullweber@chromium.org>
2023-12-19 13:39:38 +0000

    ContentSettings: Add ProviderInterface::GetRule() method
    Add a ProviderInterface::GetRule() method to implement faster
    than linear settings lookups. Since there are lots of different
    providers, the method has a default implementation until all providers
    are updated.
    
    Bug: b/316530672
    Change-Id: Id58f11d59957ca2b8fc19eef3bb0d84e7afeba34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5124718
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Patricia Alfonso <trishalfonso@google.com>
    Cr-Commit-Position: refs/heads/main@{#1239100}

---
+++ components/content_settings/core/browser/content_settings_provider.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/content_settings/core/browser/content_settings_provider.h"
+#include "base/notreached.h"
+
+namespace content_settings {
+
+std::unique_ptr<OwnedRule> ProviderInterface::GetRule(
+    const GURL& primary_url,
+    const GURL& secondary_url,
+    ContentSettingsType content_type,
+    bool off_the_record,
+    const PartitionKey& partition_key) const {
+  // TODO(b/316530672): Remove default implementation when all providers are
+  // implemented.
+  NOTIMPLEMENTED();
+  return nullptr;
+}
+
+}  // namespace content_settings
