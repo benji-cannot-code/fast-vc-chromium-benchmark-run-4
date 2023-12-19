f4bb56b650702f0f899316e1685a499e1dcf63b9
Wolfgang Billenstein <bwolfgang@google.com>
2023-12-19 08:54:44 +0000

    [Autofill] Add UMA Autofill.PrefilledFields.{Address, CreditCard}
    Adds UMA for counting if a field had a pre-filled value on page load.
    
    Bug: 1496673
    Change-Id: Icae0803b735fa57922eaf234df87d7817119dec0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5054272
    Commit-Queue: Wolfgang Billenstein <bwolfgang@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1238993}

--- components/autofill/core/browser/metrics/quality_metrics.cc
+++ components/autofill/core/browser/metrics/quality_metrics.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/metrics/autofill_metrics_utils.h"
 #include "components/autofill/core/browser/metrics/field_filling_stats_and_score_metrics.h"
 #include "components/autofill/core/browser/metrics/granular_filling_metrics_utils.h"
+#include "components/autofill/core/browser/metrics/placeholder_metrics.h"
 #include "components/autofill/core/browser/metrics/precedence_over_autocomplete_metrics.h"
 #include "components/autofill/core/browser/metrics/shadow_prediction_metrics.h"
 #include "components/autofill/core/browser/validation.h"
@@ -200,6 +201,10 @@ void LogQualityMetrics(
           AddFillingStatsForAutofillFillingMethod(
               *field, address_field_stats_by_filling_method);
         }
+
+        LogPreFilledFields(
+            std::string(FormTypeToStringView(form_type_of_field)),
+            field->initial_value_changed());
       }
     }
 
