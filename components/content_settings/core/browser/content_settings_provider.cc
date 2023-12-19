f2c9aad5a8cfb77fc404a9899ba655aef9f486ad
Christian Dullweber <dullweber@chromium.org>
2023-12-19 16:24:47 +0000

    ContentSettings: Add feature and call ProviderInterface::GetRule()
    Add base::Feature and call into ProviderInterface::GetRule() from
    HostContentSettingsMap. Add default implementation using rule iterator
    to test the implementation before all providers are implemented.
    
    
    Bug: b/316530672
    Change-Id: I5b270ae6ac810b0bef62ff1af2188a1c38d17726
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5130488
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Patricia Alfonso <trishalfonso@google.com>
    Cr-Commit-Position: refs/heads/main@{#1239179}

--- components/content_settings/core/browser/content_settings_provider.cc
+++ components/content_settings/core/browser/content_settings_provider.cc
@@ -4,7 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "components/content_settings/core/browser/content_settings_provider.h"
-#include "base/notreached.h"
+#include "base/feature_list.h"
+#include "components/content_settings/core/browser/content_settings_rule.h"
+#include "components/content_settings/core/common/features.h"
 
 namespace content_settings {
 
@@ -16,7 +18,20 @@ std::unique_ptr<OwnedRule> ProviderInterface::GetRule(
     const PartitionKey& partition_key) const {
   // TODO(b/316530672): Remove default implementation when all providers are
   // implemented.
-  NOTIMPLEMENTED();
+  auto it = GetRuleIterator(content_type, off_the_record, partition_key);
+  while (it && it->HasNext()) {
+    auto rule = it->Next();
+    if (rule->primary_pattern.Matches(primary_url) &&
+        rule->secondary_pattern.Matches(secondary_url) &&
+        (base::FeatureList::IsEnabled(
+             content_settings::features::kActiveContentSettingExpiry) ||
+         rule->metadata.expiration().is_null() ||
+         rule->metadata.expiration() >= base::Time::Now())) {
+      return std::make_unique<OwnedRule>(
+          std::move(rule->primary_pattern), std::move(rule->secondary_pattern),
+          rule->TakeValue(), std::move(rule->metadata));
+    }
+  }
   return nullptr;
 }
 
