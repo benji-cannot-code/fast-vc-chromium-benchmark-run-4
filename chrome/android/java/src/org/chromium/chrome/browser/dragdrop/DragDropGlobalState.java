1605db208b0fa1915ff2a6aab3d575f8321976fe
Sirisha Kavuluru <skavuluru@google.com>
2023-12-19 03:30:51 +0000

    [TLDD] Tab drag as window - Hide shadow when drag enters destination strip. Avoid strip tab shadow inflation
    Changes in this commit:
    * With drag as window FF - Hide drag shadow when drag enters destination strip (we already hide for source strip). Since drag as window uses a large shadow, we do not want the shadow to cover the strip so users can view the drop area. Moved dragSourceView to global static state to access this in destination strip to invoke view hide on drag enter event.
    * Inflate Strip tab drag shadow only when drag as tab FF is enabled. This shadow is not used for drag as window
    * Re-wrote TabDragSourceTest for better readability - the tests were previously using same instance of TabDragSource and overriding getCurrentInstance() to simulate source vs destination mid-test. Updated to maintain separate instances now to make the test cleaner and easier to read.
    
    Demo for hiding drag shadow: https://screencast.googleplex.com/cast/NDg0MzcwMzUyODA2Mjk3NnxhNjQ0MjM3NC0yZQ
    
    Bug: 1508450
    Change-Id: I39355f5e725cfe0a2bda98bf1c22c71b75d87906
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5089631
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1238933}

--- chrome/android/java/src/org/chromium/chrome/browser/dragdrop/DragDropGlobalState.java
+++ chrome/android/java/src/org/chromium/chrome/browser/dragdrop/DragDropGlobalState.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.dragdrop;
 
+import android.view.View.DragShadowBuilder;
+
 import org.chromium.chrome.browser.multiwindow.MultiWindowUtils;
 import org.chromium.chrome.browser.tab.Tab;
 
@@ -13,17 +15,17 @@ public final class DragDropGlobalState {
 
     private static DragDropGlobalState sInstance = new DragDropGlobalState();
 
-    public boolean dragShadowShowing;
     public int dragSourceInstanceId = MultiWindowUtils.INVALID_INSTANCE_ID;
     public Tab tabBeingDragged;
+    public DragShadowBuilder dragShadowBuilder;
 
     public static DragDropGlobalState getInstance() {
         return sInstance;
     }
 
     public void reset() {
-        dragShadowShowing = false;
         dragSourceInstanceId = MultiWindowUtils.INVALID_INSTANCE_ID;
         tabBeingDragged = null;
+        dragShadowBuilder = null;
     }
 }
