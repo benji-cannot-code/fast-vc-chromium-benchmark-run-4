3f0553c96eedae5413c00556439cb6ad609e0bda
Courtney Wong <courtneywong@chromium.org>
2023-12-19 22:09:29 +0000

    crosier: Add supervised user login fixture and crosier test
    This test logs in with a production GAIA supervised user account.
    
    Tested: Ran the login test 100 times
    
    Bug: b:315490929
    Change-Id: I399217e0a71469ac9abbdd38c53585780ce3cfde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5105859
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239373}

--- chrome/test/base/chromeos/crosier/test_accounts.h
+++ chrome/test/base/chromeos/crosier/test_accounts.h
@@ -10,8 +10,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace crosier {
 
+// Used to store the accounts that make up a family from the test account pool.
+struct FamilyAccounts {
+  struct User {
+    std::string email;
+    std::string password;
+  };
+
+  FamilyAccounts();
+  FamilyAccounts(const FamilyAccounts&);
+  ~FamilyAccounts();
+
+  User parent;
+  User unicorn;
+  User geller;
+  User griffin;
+};
+
 // Randomly picks a gaia test account from the test accounts pool.
 void GetGaiaTestAccount(std::string& out_email, std::string& out_password);
+
+// Returns the set of accounts relevant to a supervised user login from the test
+// accounts pool. This includes the supervised user accounts, parent accounts,
+// and EDU accounts associated with the supervised user.
+FamilyAccounts GetFamilyTestAccounts();
 
 }  // namespace crosier
 
