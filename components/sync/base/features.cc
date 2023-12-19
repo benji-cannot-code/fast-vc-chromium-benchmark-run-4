a58816d88fa8fb3cf05d7c141007b51c485b7c06
Marc Treib <treib@chromium.org>
2023-12-19 15:31:31 +0000

    SyncSchedulerImpl: Use WallClockTimer for polling (behind Feature)
    As opposed to the regular OneShotTimer, WallClockTimer keeps counting
    while the device is suspended, which is exactly what's desired for poll
    requests.
    Since this has the potential to influence Sync traffic, the new logic
    is behind a (disabled-by-default) Feature.
    
    Bug: 1497926
    Change-Id: I4be2558bb5a8349506557cf2939fc8b2a8219e70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5126059
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1239150}

--- components/sync/base/features.cc
+++ components/sync/base/features.cc
@@ -215,4 +215,8 @@ BASE_FEATURE(kSyncIncreaseNudgeDelayForSingleClient,
              "SyncIncreaseNudgeDelayForSingleClient",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kSyncSchedulerUseWallClockTimer,
+             "SyncSchedulerUseWallClockTimer",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 }  // namespace syncer
