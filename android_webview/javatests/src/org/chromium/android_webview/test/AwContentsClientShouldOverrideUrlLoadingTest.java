608609f4ac8d2350b5d023a7859420894c0a15c3
Bo Liu <boliu@chromium.org>
2023-12-19 22:52:56 +0000

    Deflake webview instrumentation tests
    Revert https://chromium-review.googlesource.com/c/chromium/src/+/5131563
    which disabled the flaky tests.
    
    Effectively revert https://chromium-review.googlesource.com/c/chromium/src/+/5120512
    which caused the tests to crash flakily.
    
    Fixed: 1512112
    Bug: 1511310
    Cq-Include-Trybots: luci.chromium.try:android-12l-x64-dbg
    Change-Id: If79aaaf23b6d8be1ce255fa5f910d51d0491bc8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5137836
    Auto-Submit: Bo Liu <boliu@chromium.org>
    Reviewed-by: vikas soni <vikassoni@chromium.org>
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239413}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientShouldOverrideUrlLoadingTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientShouldOverrideUrlLoadingTest.java
@@ -36,7 +36,6 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.CriteriaNotSatisfiedException;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.components.policy.AbstractAppRestrictionsProvider;
@@ -60,7 +59,6 @@ import java.util.concurrent.TimeUnit;
 /** Tests for the WebViewClient.shouldOverrideUrlLoading() method. */
 @RunWith(Parameterized.class)
 @UseParametersRunnerFactory(AwJUnit4ClassRunnerWithParameters.Factory.class)
-@DisabledTest(message = "Multiple test cases failing, crbug.com/1512112")
 public class AwContentsClientShouldOverrideUrlLoadingTest extends AwParameterizedTest {
     @Rule public AwActivityTestRule mActivityTestRule;
 
