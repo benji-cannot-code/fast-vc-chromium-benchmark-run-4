d79774e2aca1d844f3be0efab5efc62e59f76129
Gabriel Charette <gab@chromium.org>
2023-12-20 21:37:31 +0000

    [base] Support Semaphore::TimedWait(TimeDelta::Max())
    R=spvw@chromium.org
    
    Bug: 1481215
    Change-Id: Ica71653a18e29a7e7f582784be078998c9cf5411
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5141546
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Sean Maher <spvw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239803}

--- base/task/thread_pool/semaphore/semaphore_posix.cc
+++ base/task/thread_pool/semaphore/semaphore_posix.cc
@@ -66,6 +66,11 @@ void Semaphore::Wait() {
 }
 
 bool Semaphore::TimedWait(TimeDelta timeout) {
+  if (timeout.is_max()) {
+    Wait();
+    return true;
+  }
+
   // Compute the time for end of timeout.
   const struct timespec ts = TimeDeltaToAbsTimeSpec(timeout);
 
