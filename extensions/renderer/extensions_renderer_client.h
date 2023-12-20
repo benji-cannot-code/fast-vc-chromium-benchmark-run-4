3546992e96457d0596f407fafa427f8ef452139e
Jose Dapena Paz <jdapena@igalia.com>
2023-12-20 17:04:14 +0000

    IWYU: usage of unique_ptr requires include in extensions_renderer_client.h
    Bug: 957519
    Change-Id: Id84c42466b58daa3982e23ada4b1b9fbc7505e1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5140566
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239716}

--- extensions/renderer/extensions_renderer_client.h
+++ extensions/renderer/extensions_renderer_client.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef EXTENSIONS_RENDERER_EXTENSIONS_RENDERER_CLIENT_H_
 #define EXTENSIONS_RENDERER_EXTENSIONS_RENDERER_CLIENT_H_
 
+#include <memory>
+
 #include "extensions/common/extension_id.h"
 #include "extensions/renderer/extensions_renderer_api_provider.h"
 
