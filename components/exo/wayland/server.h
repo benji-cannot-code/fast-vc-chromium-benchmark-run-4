d5602fb268967a9f1c89848f2402521ce6e4280c
Thomas Lukaszewicz <tluk@chromium.org>
2023-12-20 01:12:27 +0000

    Reland "[exo] Ensure exo clients always see a valid Output configuration"
    This is a reland of commit b16cf07e368261015b40ff95f93a5f1699fd69cd
    
    See diff against patchset 1. Changes:
    
      - Fixed ASAN unittest issue due to not waiting for
        WaylandDisplayOutput to clear itself after the test had finished
    
      - Fixed UAF crash in Exo caused by disconnecting client caught by
        wayland tast tests (see upstream patch)
    
    Original change's description:
    > [exo] Ensure exo clients always see a valid Output configuration
    >
    > This CL switches exo::Server to consume atomic display configuration
    > updates from DisplayManager via the DisplayManagerObserevr interface.
    >
    > exo::Server will now process added displays before removed displays
    > to guarantee that the invariant that there is always a Display
    > defined is reflected by exo's clients.
    >
    > Bug: 1427019, 1408304, 1498683
    > Change-Id: Iaeae459f65cada22d35b29ca4e2fb75b7ff436fb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5025534
    > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1226615}
    
    Bug: 1427019, 1408304, 1498683, b:312267772
    Change-Id: I03586bf5a68a83fb084341a61a9ed054a8226736
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5052140
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239481}

--- components/exo/wayland/server.h
+++ components/exo/wayland/server.h
@@ -15,9 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
+#include "base/scoped_observation.h"
 #include "base/time/time.h"
 #include "components/exo/wayland/scoped_wl.h"
-#include "ui/display/display_observer.h"
+#include "ui/display/manager/display_manager.h"
+#include "ui/display/manager/display_manager_observer.h"
 
 struct wl_resource;
 struct wl_client;
@@ -45,7 +47,7 @@ class WaylandWatcher;
 
 // This class is a thin wrapper around a Wayland display server. All Wayland
 // requests are dispatched into the given Exosphere display.
-class Server : public display::DisplayObserver {
+class Server : public display::DisplayManagerObserver {
  public:
   using ServerGetter = base::RepeatingCallback<Server*(wl_display*)>;
   using StartCallback = base::OnceCallback<void(bool)>;
@@ -95,9 +97,9 @@ class Server : public display::DisplayObserver {
   // Send all buffered events to the clients.
   void Flush();
 
-  // Overridden from display::DisplayObserver:
-  void OnDisplayAdded(const display::Display& new_display) override;
-  void OnDisplayRemoved(const display::Display& old_display) override;
+  // display::DisplayManagerObserver:
+  void OnDidProcessDisplayChanges(
+      const DisplayConfigurationChange& configuration_change) override;
 
   wl_resource* GetOutputResource(wl_client* client, int64_t display_id);
 
@@ -130,7 +132,6 @@ class Server : public display::DisplayObserver {
   base::flat_map<int64_t, std::unique_ptr<WaylandDisplayOutput>> outputs_;
   std::unique_ptr<WaylandDataDeviceManager> data_device_manager_data_;
   std::unique_ptr<WaylandSeat> seat_data_;
-  display::ScopedDisplayObserver display_observer_{this};
   std::unique_ptr<wayland::WaylandWatcher> wayland_watcher_;
   std::unique_ptr<WaylandDmabufFeedbackManager> wayland_feedback_manager_;
 
@@ -141,6 +142,10 @@ class Server : public display::DisplayObserver {
   std::unique_ptr<WaylandRemoteShellData> remote_shell_data_;
   std::unique_ptr<UiControls> ui_controls_holder_;
   std::unique_ptr<ClientTracker> client_tracker_;
+
+  base::ScopedObservation<display::DisplayManager,
+                          display::DisplayManagerObserver>
+      display_manager_observation_{this};
 };
 
 }  // namespace wayland
