645a7775bcf3e8aeb816018c2302bcccdd5a22c3
Peter Kotwicz <pkotwicz@chromium.org>
2023-12-20 20:49:09 +0000

    [Reland] Add method to DeviceLockBridge in order to reuse code for IBAN
    This CL adds DeviceLockBridge::LaunchDeviceLockUiIfNeededBeforeRunningCallback()
    in order to share code between AutofillSaveCardDelegateAndroid and
    AutofillSaveIbanDelegate.
    
    BUG=309163906
    
    Change-Id: Iaa65be7ac81e7b1631576b3c28534fb44407c97d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5122371
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Geoff Huang <geoffhuang@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239793}

--- components/browser_ui/device_lock/android/test_device_lock_bridge.h
+++ components/browser_ui/device_lock/android/test_device_lock_bridge.h
@@ -1,5 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-
 // Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -21,24 +20,20 @@ class TestDeviceLockBridge : public DeviceLockBridge {
 
   ~TestDeviceLockBridge() override;
 
-  bool ShouldShowDeviceLockUi() override;
-
-  bool RequiresDeviceLock() override;
-
-  void LaunchDeviceLockUiBeforeRunningCallback(
+  void LaunchDeviceLockUiIfNeededBeforeRunningCallback(
       ui::WindowAndroid* window_android,
-      DeviceLockConfirmedCallback callback) override;
+      DeviceLockRequirementMetCallback callback) override;
 
-  void SimulateDeviceLockComplete(bool is_device_lock_set);
+  void SimulateFinishedCheckingDeviceLockRequirements(
+      bool are_device_lock_requirements_met);
 
-  void SetShouldShowDeviceLockUi(bool should_show_device_lock_ui);
-
-  bool device_lock_ui_was_shown();
+  bool did_start_checking_device_lock_requirements() {
+    return did_start_checking_device_lock_requirements_;
+  }
 
  private:
-  bool should_show_device_lock_ui_ = false;
-  bool device_lock_ui_was_shown_ = false;
-  DeviceLockConfirmedCallback callback_;
+  bool did_start_checking_device_lock_requirements_ = false;
+  DeviceLockRequirementMetCallback callback_;
 };
 
 }  // namespace autofill
