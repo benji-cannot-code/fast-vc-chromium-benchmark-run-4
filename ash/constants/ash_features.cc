2e7d45eb576ae0257181127494ddac47e42030d5
mlcui <mlcui@google.com>
2023-12-20 08:05:55 +0000

    ime: Disable DLC for handwriting if `ondevice_handwriting` is disabled
    This generally affects 2GB devices, and will cause them to fall back to
    using the NaCl code path instead of the (currently broken) handwriting
    code path.
    
    We should revert this CL once we have fixed DLC issues with boards with
    `ondevice_handwriting` disabled.
    
    See attached bug for more details.
    
    Bug: b:316981973
    Change-Id: Iab9001d601e3fec10edef68d2a710309b6e02dad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5137597
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239578}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1348,11 +1348,15 @@ BASE_FEATURE(kGrowthCampaignsInDemoMode,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Enables new on-device recognition for legacy handwriting input.
+// This flag should be OVERRIDDEN for devices which do not have on-device
+// handwriting (b/316981973). Please check before using this flag.
 BASE_FEATURE(kHandwritingLegacyRecognition,
              "HandwritingLegacyRecognition",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Enables downloading the handwriting libraries via DLC.
+// This flag should be OVERRIDDEN for devices which do not have on-device
+// handwriting (b/316981973). Please check before using this flag.
 BASE_FEATURE(kHandwritingLibraryDlc,
              "HandwritingLibraryDlc",
              base::FEATURE_ENABLED_BY_DEFAULT);
