9c752353a78eb27309f60249b904d0fa803c55ac
Norge Vizcay <vizcay@google.com>
2023-12-20 13:48:56 +0000

    Fix missing call to base class copy constructor in AutofillProfile.
    Bug: 1440504
    Change-Id: I17cedfb3c6a4a884889c8ac151da10d19dd741f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5139321
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1239659}

--- components/autofill/core/browser/data_model/autofill_profile.cc
+++ components/autofill/core/browser/data_model/autofill_profile.cc
@@ -264,8 +264,6 @@ AutofillProfile CreateStarterProfile(
 
 }  // namespace
 
-AutofillProfile::AutofillProfile(AddressCountryCode country_code)
-    : AutofillProfile(Source::kLocalOrSyncable, country_code) {}
 
 AutofillProfile::AutofillProfile(const std::string& guid,
                                  Source source,
@@ -283,8 +281,12 @@ AutofillProfile::AutofillProfile(Source source, AddressCountryCode country_code)
                       source,
                       country_code) {}
 
+AutofillProfile::AutofillProfile(AddressCountryCode country_code)
+    : AutofillProfile(Source::kLocalOrSyncable, country_code) {}
+
 AutofillProfile::AutofillProfile(const AutofillProfile& profile)
-    : phone_number_(this),
+    : AutofillDataModel(profile),
+      phone_number_(this),
       address_(profile.GetAddress()),
       token_quality_(this) {
   operator=(profile);
