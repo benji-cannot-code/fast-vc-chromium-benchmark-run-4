a2385afcbd8905a1750ae1882250bfdec3a36b69
Mohamed Amir Yosef <mamir@chromium.org>
2023-12-20 09:11:36 +0000

    [Passwords] Populate shared credentials cache only if feature enabled
    The cache of shared credentials is only needed to mark those
    credentials as notified upon user interaction with the sharing
    notification UI.
    
    Therefore, there is no need to populate the cache if the
    SharedPasswordNotificationUI is disabled.
    
    This also protects against marking shared credentials as notified when
    the UI didn't adapt to notify the user about them.
    
    Bug: 1504098
    Change-Id: Ib4da165ed92fb72076e46aad5aa7403c64e3e5be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5134890
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239595}

--- components/password_manager/core/browser/credential_cache.cc
+++ components/password_manager/core/browser/credential_cache.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "components/autofill/core/common/autofill_features.h"
+#include "components/password_manager/core/browser/features/password_features.h"
 #include "components/password_manager/core/browser/origin_credential_store.h"
 #include "components/password_manager/core/browser/password_form.h"
 #include "url/origin.h"
@@ -50,7 +51,13 @@ void CredentialCache::SaveCredentialsAndBlocklistedForOrigin(
   for (const PasswordForm* form : best_matches) {
     if (form->type == PasswordForm::Type::kReceivedViaSharing &&
         !form->sharing_notification_displayed) {
-      unnotified_shared_credentials.push_back(*form);
+      // The cache is only useful when the sharing notification UI is displayed
+      // since it is used to mark those credentials as notified after the user
+      // interacts with the UI.
+      if (base::FeatureList::IsEnabled(
+              password_manager::features::kSharedPasswordNotificationUI)) {
+        unnotified_shared_credentials.push_back(*form);
+      }
     }
   }
   GetOrCreateCredentialStore(origin).SaveUnnotifiedSharedCredentials(
