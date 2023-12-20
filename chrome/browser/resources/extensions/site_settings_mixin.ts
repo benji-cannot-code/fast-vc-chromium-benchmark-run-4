a8288cb78ac8646f6b87a26ead85c3bdaec2f5d4
dpapad <dpapad@chromium.org>
2023-12-20 23:01:16 +0000

    WebUI: Deploy "import type" imports in c/b/r/extensions.
    Bug: 1494527
    Change-Id: I08c48c8209b046f15d30f3fe8dbaacef5053ddd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5131970
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239883}

--- chrome/browser/resources/extensions/site_settings_mixin.ts
+++ chrome/browser/resources/extensions/site_settings_mixin.ts
@@ -9,10 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * multiple pages.
  */
 
-import {ChromeEvent} from '/tools/typescript/definitions/chrome_event.js';
-import {dedupingMixin, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import type {ChromeEvent} from '/tools/typescript/definitions/chrome_event.js';
+import type {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {dedupingMixin} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
-import {ItemDelegate} from './item.js';
+import type {ItemDelegate} from './item.js';
 
 type Constructor<T> = new (...args: any[]) => T;
 
