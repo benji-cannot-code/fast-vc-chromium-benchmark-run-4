4c70a9734139b50ccf6a3872226485993555aaae
Trent Apted <tapted@chromium.org>
2023-12-20 22:33:53 +0000

    media_app: Zap obsolete Closure jsdoc in driver.ts
    Theser were kept in an upstream CL to retain git file-move tracking.
    
    Bug: b/314827247
    Change-Id: I481916ba127193f227f26a52b9e113d9936c3d71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5118965
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Reviewed-by: Patti Lor <patricialor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239850}

--- ash/webui/media_app_ui/test/driver.ts
+++ ash/webui/media_app_ui/test/driver.ts
@@ -39,11 +39,10 @@ export class GuestDriver {
    * Sends a query to the guest that repeatedly runs a query selector until
    * it returns an element.
    *
-   * @param {string} query the querySelector to run in the guest.
-   * @param {string=} opt_property a property to request on the found element.
-   * @param {!Object=} opt_commands test commands to execute on the element.
-   * @return Promise<string> JSON.stringify()'d value of the property, or
-   *   tagName if unspecified.
+   * @param query the querySelector to run in the guest.
+   * @param property a property to request on the found element.
+   * @param commands test commands to execute on the element.
+   * @return JSON.stringify()'d value of the property or tagName if unspecified.
    */
   async waitForElementInGuest(
       query: string, property?: string, commands: Object = {}) {
@@ -57,7 +56,6 @@ export class GuestDriver {
 
 /**
  * Runs the given `testCase` in the guest context.
- * @param {string} testCase
  */
 export async function runTestInGuest(testCase: string) {
   const message: TestMessageRunTestCase = {testCase};
@@ -65,10 +63,6 @@ export async function runTestInGuest(testCase: string) {
   await guestMessagePipe.sendMessage('run-test-case', message);
 }
 
-/**
- * @param {!Object=} data
- * @return {!Promise<!TestMessageResponseData>}
- */
 export async function sendTestMessage(data?: Object):
     Promise<TestMessageResponseData> {
   await testMessageHandlersReady;
@@ -78,7 +72,6 @@ export async function sendTestMessage(data?: Object):
 /**
  * Gets a concatenated list of errors on the currently loaded files. Note the
  * currently open file is always at index 0.
- * @return {!Promise<string>}
  */
 export async function getFileErrors(): Promise<string> {
   await testMessageHandlersReady;
@@ -97,8 +90,6 @@ export class FakeWritableFileSink {
 
   constructor(public data = new Blob()) {}
 
-
-  /** @param {?BufferSource|!Blob|string|!WriteParams} data */
   async write(dataParam: BufferSource|Blob|string|WriteParams) {
     const position = 0;  // Assume no seeks.
     if (!dataParam) {
@@ -118,7 +109,6 @@ export class FakeWritableFileSink {
       this.data.slice(position + dataSize),
     ]);
   }
-  /** @param {number} size */
   async truncate(size: number) {
     this.data = this.data.slice(0, size);
   }
@@ -126,13 +116,11 @@ export class FakeWritableFileSink {
   async close() {
     this.resolveClose(this.data);
   }
-  /** @param {number} offset */
   async seek(_offset: number) {
     throw new Error('seek() not implemented.');
   }
 }
 
-/** @implements FileSystemHandle  */
 export class FakeFileSystemHandle implements FileSystemHandle {
   kind: FileSystemHandleKind = 'file';
   constructor(public name: string = 'fake_file.png') {}
@@ -141,7 +129,6 @@ export class FakeFileSystemHandle implements FileSystemHandle {
   }
 }
 
-/** @implements FileSystemFileHandle  */
 export class FakeFileSystemFileHandle extends FakeFileSystemHandle implements
     FileSystemFileHandle {
   override kind: 'file' = 'file';
@@ -173,12 +160,10 @@ export class FakeFileSystemFileHandle extends FakeFileSystemHandle implements
     writable.close = () => sink.close();
     return writable;
   }
-  /** @override */
   async getFile() {
     return this.getFileSync();
   }
 
-  /** @return {!File} */
   getFileSync() {
     return new File(
         [this.lastWritable.data], this.name,
@@ -186,7 +171,6 @@ export class FakeFileSystemFileHandle extends FakeFileSystemHandle implements
   }
 }
 
-/** @implements FileSystemDirectoryHandle  */
 export class FakeFileSystemDirectoryHandle extends FakeFileSystemHandle
     implements FileSystemDirectoryHandle {
   override kind: 'directory' = 'directory';
@@ -203,14 +187,12 @@ export class FakeFileSystemDirectoryHandle extends FakeFileSystemHandle
 
   /**
    * Use to populate `FileSystemFileHandle`s for tests.
-   * @param {!FakeFileSystemFileHandle} fileHandle
    */
   addFileHandleForTest(fileHandle: FakeFileSystemFileHandle) {
     this.files.push(fileHandle);
   }
   /**
    * Helper to get all entries as File.
-   * @return {!Array<!File>}
    */
   getFilesSync(): File[] {
     return this.files.map(f => f.getFileSync());
@@ -230,29 +212,17 @@ export class FakeFileSystemDirectoryHandle extends FakeFileSystemHandle
                         Promise.reject((createNamedError(
                             'NotFoundError', `File ${name} not found`)));
   }
-  /** @override */
   async getDirectoryHandle(
       _name: string, _options?: FileSystemGetDirectoryOptions):
       Promise<FakeFileSystemDirectoryHandle> {
     throw new Error('Not implemented');
   }
-  /**
-   * @override
-   * @return {!AsyncIterable<!Array<string|!FileSystemHandle>>}
-   * @suppress {reportUnknownTypes} suppress [JSC_UNKNOWN_EXPR_TYPE] for `yield
-   * [file.name, file]`.
-   */
+
   async * entries(): AsyncIterable<Array<string|FileSystemHandle>> {
     for (const file of this.files) {
       yield [file.name, file];
     }
   }
-  /**
-   * @override
-   * @return {!AsyncIterable<string>}
-   * @suppress {reportUnknownTypes} suppress [JSC_UNKNOWN_EXPR_TYPE] for `yield
-   * file.name`.
-   */
   async * keys(): AsyncIterable<string> {
     for (const file of this.files) {
       yield file.name;
@@ -268,7 +238,6 @@ export class FakeFileSystemDirectoryHandle extends FakeFileSystemHandle
       yield file;
     }
   }
-  /** @override */
   async removeEntry(name: string, _options: FileSystemRemoveOptions) {
     // Remove file handle from internal state.
     const fileHandleIndex = this.files.findIndex(f => f.name === name);
@@ -276,7 +245,6 @@ export class FakeFileSystemDirectoryHandle extends FakeFileSystemHandle
     this.lastDeleted = this.files.splice(fileHandleIndex, 1)[0];
   }
 
-  /** @override */
   resolve() {
     return Promise.resolve(null);
   }
@@ -284,12 +252,6 @@ export class FakeFileSystemDirectoryHandle extends FakeFileSystemHandle
 
 /**
  * Structure to define a test file.
- * @typedef{{
- *   name: (string|undefined),
- *   type: (string|undefined),
- *   lastModified: (number|undefined),
- *   arrayBuffer: (function(): (!Promise<!ArrayBuffer>)|undefined)
- * }}
  */
 export interface FileDesc {
   name?: string;
@@ -300,8 +262,6 @@ export interface FileDesc {
 
 /**
  * Creates a mock directory with the provided files in it.
- * @param {!Array<!FileDesc>=} files
- * @return {!Promise<!FakeFileSystemDirectoryHandle>}
  */
 export async function createMockTestDirectory(files: FileDesc[] = [{}]):
     Promise<FakeFileSystemDirectoryHandle> {
