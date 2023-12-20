c5013d1f0ede959b685d28db04d66a67e3c6d1cf
kylechar <kylechar@chromium.org>
2023-12-20 22:58:54 +0000

    Use concepts with BindPostTask()
    Use concepts to enforce that callback return value is void rather than a
    static_assert. This leads to better compiler diagnostic error messages.
    
    Bug: none
    Change-Id: I1b1476ea4184721a0601b68164d9e12b2ba57d6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5140346
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239880}

--- base/task/bind_post_task.h
+++ base/task/bind_post_task.h
@@ -65,15 +65,11 @@ namespace base {
 // returned callback is destroyed without being run then |callback| will be
 // be destroyed on |task_runner|.
 template <typename ReturnType, typename... Args>
+  requires std::is_void_v<ReturnType>
 OnceCallback<void(Args...)> BindPostTask(
     scoped_refptr<TaskRunner> task_runner,
     OnceCallback<ReturnType(Args...)> callback,
     const Location& location = FROM_HERE) {
-  static_assert(std::is_same_v<ReturnType, void>,
-                "OnceCallback must have void return type in order to produce a "
-                "closure for PostTask(). Use base::IgnoreResult() to drop the "
-                "return value if desired.");
-
   using Helper = internal::BindPostTaskTrampoline<OnceCallback<void(Args...)>>;
 
   return base::BindOnce(
@@ -86,15 +82,11 @@ OnceCallback<void(Args...)> BindPostTask(
 // the returned callback is destroyed a task will be posted to destroy the input
 // |callback| on |task_runner|.
 template <typename ReturnType, typename... Args>
+  requires std::is_void_v<ReturnType>
 RepeatingCallback<void(Args...)> BindPostTask(
     scoped_refptr<TaskRunner> task_runner,
     RepeatingCallback<ReturnType(Args...)> callback,
     const Location& location = FROM_HERE) {
-  static_assert(std::is_same_v<ReturnType, void>,
-                "RepeatingCallback must have void return type in order to "
-                "produce a closure for PostTask(). Use base::IgnoreResult() to "
-                "drop the return value if desired.");
-
   using Helper =
       internal::BindPostTaskTrampoline<RepeatingCallback<void(Args...)>>;
 
@@ -115,6 +107,7 @@ RepeatingCallback<void(Args...)> BindPostTask(
 // - Only use this helper as a last resort if none of the above apply.
 
 template <typename ReturnType, typename... Args>
+  requires std::is_void_v<ReturnType>
 OnceCallback<void(Args...)> BindPostTaskToCurrentDefault(
     OnceCallback<ReturnType(Args...)> callback,
     const Location& location = FROM_HERE) {
@@ -123,6 +116,7 @@ OnceCallback<void(Args...)> BindPostTaskToCurrentDefault(
 }
 
 template <typename ReturnType, typename... Args>
+  requires std::is_void_v<ReturnType>
 RepeatingCallback<void(Args...)> BindPostTaskToCurrentDefault(
     RepeatingCallback<ReturnType(Args...)> callback,
     const Location& location = FROM_HERE) {
