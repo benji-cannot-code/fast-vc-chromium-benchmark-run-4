cccf8d74563f21823b78765e74977e4913596a75
Glen Robertson <glenrob@chromium.org>
2023-12-20 02:56:40 +0000

    Fix bug reference for fixing nogncheck
    Previous bug reference was invalid.
    
    Change-Id: Ia27903b90279d1c14ddcfdbc3cba830e6bb41e4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5133628
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Auto-Submit: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239523}

--- chrome/browser/ui/web_applications/web_app_browsertest.cc
+++ chrome/browser/ui/web_applications/web_app_browsertest.cc
@@ -135,7 +135,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/test/dialog_test.h"
 #include "ui/views/widget/any_widget_observer.h"
 #include "ui/views/widget/widget.h"
-// TODO(crbug/4925196): enable gn check once it learn about conditional includes
+// TODO(crbug.com/1125897): Enable gn check once it handles conditional includes
 #include "components/metrics/structured/structured_events.h"  // nogncheck
 #endif
 
