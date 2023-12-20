efa5a3ed21bd04a916ba19c3f0011fe043b3723a
Rebekah Potter <rbpotter@chromium.org>
2023-12-20 18:39:27 +0000

    WebUI: Fork cr_toast/ between Ash and Browser
    - Creating a new ash/webui/common/resources/cr_elements folder with
      build_webui target
    - Creating new chrome/test/data/webui/chromeos/ash_common/cr_elements
      test folder with a build_webui_tests target
    - Forking cr_toast/* into the Ash cr_elements/ folder and forking the
      corresponding tests to the test folder
    - Updating scanning app to point to the Ash fork.
    
    Bug: 1512231
    Change-Id: I9f963bfb1b2e0836aef345fd13430ffbb0f5369a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5120934
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239764}

---
+++ chrome/test/data/webui/chromeos/ash_common/cr_elements/ash_common_cr_elements_browsertest.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/test/base/web_ui_mocha_browser_test.h"
+#include "content/public/test/browser_test.h"
+
+typedef WebUIMochaBrowserTest AshCommonCrElementsTest;
+
+IN_PROC_BROWSER_TEST_F(AshCommonCrElementsTest, CrToast) {
+  RunTest("chromeos/ash_common/cr_elements/cr_toast_test.js", "mocha.run()");
+}
+
+IN_PROC_BROWSER_TEST_F(AshCommonCrElementsTest, CrToastManager) {
+  RunTest("chromeos/ash_common/cr_elements/cr_toast_manager_test.js",
+          "mocha.run()");
+}
