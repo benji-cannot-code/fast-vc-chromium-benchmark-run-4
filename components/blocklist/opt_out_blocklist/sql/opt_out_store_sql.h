be106b33a6d7c7482445377b428fed2ee7299b0f
Austin Sullivan <asully@chromium.org>
2023-12-20 22:12:10 +0000

    sql: Use BuiltInRecovery for OptOutStoreSQL
    sql::Recovery is being deprecated in favor of sql::BuiltInRecovery
    
    Bug: 1385500
    Change-Id: I2249bdabb734c299013f0db405ef7c98702a4ebe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4875294
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239825}

--- components/blocklist/opt_out_blocklist/sql/opt_out_store_sql.h
+++ components/blocklist/opt_out_blocklist/sql/opt_out_store_sql.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/threading/thread_checker.h"
 #include "base/time/time.h"
@@ -26,6 +27,8 @@ class Database;
 }  // namespace sql
 
 namespace blocklist {
+
+BASE_DECLARE_FEATURE(kOptOutStoreSQLUseBuiltInRecoveryIfSupported);
 
 // OptOutStoreSQL is an instance of OptOutStore
 // which is implemented using a SQLite database.
