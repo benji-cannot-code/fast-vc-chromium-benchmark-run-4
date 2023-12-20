ef0ef2de5201736a73316e89a32ad9de02740f5f
Alan Cutter <alancutter@chromium.org>
2023-12-20 00:41:53 +0000

    [AIS] Remove redundant manifest_id field
    This field is redundant with package_id.identifier(). Remove redundancy
    to avoid confusion on whether to CHECK that they're the same value or
    not.
    
    Change-Id: I415541b11b68ad43ef0d95d5a2b7715ae1b33d9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5138415
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239467}

--- chrome/browser/apps/app_service/app_install/app_install_types.h
+++ chrome/browser/apps/app_service/app_install/app_install_types.h
@@ -37,6 +37,7 @@ struct AndroidAppInstallData {};
 std::ostream& operator<<(std::ostream& out, const AndroidAppInstallData& data);
 
 // Web app specific data for use during web app installation.
+// AppInstallData::package_id.identifier() holds the manifest identity.
 struct WebAppInstallData {
   WebAppInstallData();
   WebAppInstallData(const WebAppInstallData&);
@@ -44,8 +45,6 @@ struct WebAppInstallData {
   WebAppInstallData& operator=(const WebAppInstallData&);
   WebAppInstallData& operator=(WebAppInstallData&&);
   ~WebAppInstallData();
-
-  GURL manifest_id;
 
   GURL original_manifest_url;
 
