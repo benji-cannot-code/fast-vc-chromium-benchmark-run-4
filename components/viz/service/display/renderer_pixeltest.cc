901989a0facef71aba525fa9639e357d2d995497
kylechar <kylechar@chromium.org>
2023-12-20 22:29:43 +0000

    Enable BypassRenderPassQuads on Android
    The finch results from stable 50% didn't present any blockers.
    
    Bug: 1367413
    Change-Id: I8ea5f43dccf0c016d174427d8ac2f74246348083
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5135897
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239843}

--- components/viz/service/display/renderer_pixeltest.cc
+++ components/viz/service/display/renderer_pixeltest.cc
@@ -1321,24 +1321,7 @@ TEST_P(RendererPixelTest, BypassableTextureQuad_Rotation_ClipRect) {
                                      .SetAvgAbsErrorLimit(40)));
 }
 
-// Tests that exercise render pass bypass code. When the feature is enabled by
-// default these can be merged back into RendererPixelTest.
-class RendererPixelBypassTest : public RendererPixelTest {
- public:
-  RendererPixelBypassTest() {
-    feature_list_.InitAndEnableFeature(features::kAllowBypassRenderPassQuads);
-  }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
-};
-
-INSTANTIATE_TEST_SUITE_P(,
-                         RendererPixelBypassTest,
-                         testing::ValuesIn(GetRendererTypes()),
-                         testing::PrintToStringParamName());
-
-TEST_P(RendererPixelBypassTest, BypassableRenderPassQuad) {
+TEST_P(RendererPixelTest, BypassableRenderPassQuad) {
   AggregatedRenderPassId root_pass_id{1};
   AggregatedRenderPassId child_pass_id{2};
   AggregatedRenderPassId grand_child_pass_id{3};
@@ -1439,7 +1422,7 @@ TEST_P(RendererPixelBypassTest, BypassableRenderPassQuad) {
       cc::ExactPixelComparator()));
 }
 
-TEST_P(RendererPixelBypassTest, BypassableRenderPassQuad_DoubleBypass) {
+TEST_P(RendererPixelTest, BypassableRenderPassQuad_DoubleBypass) {
   AggregatedRenderPassId root_pass_id{1};
   AggregatedRenderPassId child_pass_id{2};
   AggregatedRenderPassId grand_child_pass_id{3};
@@ -1538,8 +1521,7 @@ TEST_P(RendererPixelBypassTest, BypassableRenderPassQuad_DoubleBypass) {
       cc::ExactPixelComparator()));
 }
 
-TEST_P(RendererPixelBypassTest,
-       BypassableRenderPassQuad_DoubleBypass_ScaledClip) {
+TEST_P(RendererPixelTest, BypassableRenderPassQuad_DoubleBypass_ScaledClip) {
   AggregatedRenderPassId root_pass_id{1};
   AggregatedRenderPassId child_pass_id{2};
   AggregatedRenderPassId grand_child_pass_id{3};
