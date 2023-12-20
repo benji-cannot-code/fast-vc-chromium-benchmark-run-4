284dff490de70fa3667467b7144bef1ab6fe3cdb
Peter Kasting <pkasting@chromium.org>
2023-12-20 01:13:49 +0000

    bind_internal.h cleanup: Guard remaining static_asserts.
    Most existing static_asserts have already been guarded using the
    "assert inside lambda inside templated struct inside std::conjunction"
    trick already documented in bind.h. However, three asserts that
    indicated Bind misuse (as opposed to invariants inside the Bind
    machinery) had not been so guarded, and I tripped over some of them
    while trying to refactor. So guard them too.
    
    The net effect is that Yet More Cases of misuse will only print one
    failing static_assert and no further compiler errors. This also ensures
    the result of BindOnce()/BindRepeating() is always a
    BindFailedCheckPreviousErrors in such cases, instead of often being
    void. That in turn makes downstream usage/handling more consistent.
    
    Bug: none
    Change-Id: I64160680971d6df48b96dba62f23c94e2a6b6c35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5127097
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239482}

--- third_party/blink/renderer/platform/wtf/functional.h
+++ third_party/blink/renderer/platform/wtf/functional.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_WTF_FUNCTIONAL_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_WTF_FUNCTIONAL_H_
 
+#include <concepts>
 #include <utility>
 
 #include "base/dcheck_is_on.h"
@@ -366,9 +367,13 @@ auto BindOnce(FunctionType&& function, BoundParameters&&... bound_parameters) {
   auto cb = base::BindOnce(std::forward<FunctionType>(function),
                            std::forward<BoundParameters>(bound_parameters)...);
 #if DCHECK_IS_ON()
-  using WrapperType = ThreadCheckingCallbackWrapper<decltype(cb)>;
-  cb = base::BindOnce(&WrapperType::Run,
-                      std::make_unique<WrapperType>(std::move(cb)));
+  // Avoid spewing more errors if the call above failed.
+  if constexpr (!std::same_as<decltype(cb),
+                              base::BindFailedCheckPreviousErrors>) {
+    using WrapperType = ThreadCheckingCallbackWrapper<decltype(cb)>;
+    cb = base::BindOnce(&WrapperType::Run,
+                        std::make_unique<WrapperType>(std::move(cb)));
+  }
 #endif
   return cb;
 }
@@ -384,9 +389,13 @@ auto BindRepeating(FunctionType function,
   auto cb = base::BindRepeating(
       function, std::forward<BoundParameters>(bound_parameters)...);
 #if DCHECK_IS_ON()
-  using WrapperType = ThreadCheckingCallbackWrapper<decltype(cb)>;
-  cb = base::BindRepeating(&WrapperType::Run,
-                           std::make_unique<WrapperType>(std::move(cb)));
+  // Avoid spewing more errors if the call above failed.
+  if constexpr (!std::same_as<decltype(cb),
+                              base::BindFailedCheckPreviousErrors>) {
+    using WrapperType = ThreadCheckingCallbackWrapper<decltype(cb)>;
+    cb = base::BindRepeating(&WrapperType::Run,
+                             std::make_unique<WrapperType>(std::move(cb)));
+  }
 #endif
   return cb;
 }
