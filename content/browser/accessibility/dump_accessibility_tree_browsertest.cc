06d08c6c344c95648c6e5fa91107ab581865f4ed
Valerie Young <spectranaut@igalia.com>
2023-12-20 22:06:46 +0000

    Remove out of date comment
    Bug: 0
    Change-Id: I6aa3e12a7b94336d15adb20e79b739b81315f10a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5137501
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Valerie Young <spectranaut@igalia.com>
    Reviewed-by: Joanmarie Diggs <jdiggs@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1239818}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -3329,7 +3329,6 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
   RunHtmlTest(FILE_PATH_LITERAL("table-multiple-row-and-column-headers.html"));
 }
 
-// TODO(https://crbug.com/1367886): De-flake and reenable.
 IN_PROC_BROWSER_TEST_P(YieldingParserDumpAccessibilityTreeTest,
                        AccessibilityTableMultipleRowAndColumnHeaders) {
   RunHtmlTest(FILE_PATH_LITERAL("table-multiple-row-and-column-headers.html"));
