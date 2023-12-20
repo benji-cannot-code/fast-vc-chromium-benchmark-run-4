b4e92ecf0e188e6cb5ae22d79f6436e2e9967d83
Patrick Monette <pmonette@chromium.org>
2023-12-20 03:40:54 +0000

    Use GlobalRenderFrameHostId in AudioStreamMonitor
    Change-Id: Ie53e61abe3908f5360cf644dc0b119459c2c042d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5074953
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Auto-Submit: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239532}

--- content/browser/renderer_host/media/audio_output_stream_observer_impl.h
+++ content/browser/renderer_host/media/audio_output_stream_observer_impl.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_RENDERER_HOST_MEDIA_AUDIO_OUTPUT_STREAM_OBSERVER_IMPL_H_
 #define CONTENT_BROWSER_RENDERER_HOST_MEDIA_AUDIO_OUTPUT_STREAM_OBSERVER_IMPL_H_
 
+#include "content/public/browser/global_routing_id.h"
 #include "media/mojo/mojom/audio_output_stream.mojom.h"
 
 namespace content {
@@ -29,8 +30,7 @@ class AudioOutputStreamObserverImpl
   void DidChangeAudibleState(bool is_audible) override;
 
  private:
-  const int render_process_id_;
-  const int render_frame_id_;
+  const GlobalRenderFrameHostId render_frame_host_id_;
   const int stream_id_;
   bool did_start_playing_ = false;
 
