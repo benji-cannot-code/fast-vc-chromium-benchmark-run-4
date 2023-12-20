7d9d099ba83b5f8f859b669caa69877d1921ddf7
Reilly Grant <reillyg@chromium.org>
2023-12-20 21:39:39 +0000

    android_deps: Configure 3pp to download TFLite
    This change instructs the 3pp packager to create new CIPD packages for
    the TensorFlow Lite in Google Play services library.
    
    The CL depending on this may not land for a while (if ever) and so this
    change will be reverted after the packages have been generated.
    
    Bug: 1513481
    Change-Id: I658cfb95e0b05d151d8489dc084e37f3be287112
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5141128
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239805}

--- third_party/android_deps/libs/com_google_android_gms_play_services_tasks/3pp/fetch.py
+++ third_party/android_deps/libs/com_google_android_gms_play_services_tasks/3pp/fetch.py
@@ -20,7 +20,7 @@ SPEC = fetch_common.Spec(repo_url=_REPO_URL,
                          module_name='play-services-tasks',
                          file_ext='aar',
                          patch_version='cr1',
-                         version_override=None,
+                         version_override='18.0.2',
                          version_filter=None)
 
 
