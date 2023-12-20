0e0f37a48d0d41cc3e109f7ee4f0f1ebe2832222
Austin Sullivan <asully@chromium.org>
2023-12-20 16:37:47 +0000

    sql: Use BuiltInRecovery for FirstPartySetsDatabase
    sql::Recovery is being deprecated in favor of sql::BuiltInRecovery
    
    Bug: 1385500
    Change-Id: If776f350bf5a56cc8672b59412898ab717a2e6f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4875354
    Reviewed-by: Shuran Huang <shuuran@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239706}

--- content/browser/first_party_sets/database/first_party_sets_database.h
+++ content/browser/first_party_sets/database/first_party_sets_database.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
+#include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/sequence_checker.h"
 #include "base/thread_annotations.h"
@@ -33,6 +34,8 @@ class Statement;
 }  // namespace sql
 
 namespace content {
+
+BASE_DECLARE_FEATURE(kFirstPartySetsDatabaseUseBuiltInRecoveryIfSupported);
 
 // Wraps its own `sql::Database` instance on behalf of the First-Party Sets
 // database implementation. This class must be accessed and destroyed on the
