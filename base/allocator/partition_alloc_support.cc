becd1eb96b4c9f70d89d8eb3b0f1651fd4b551de
Bartek Nowierski <bartekn@chromium.org>
2023-12-21 09:32:56 +0000

    [PA] Always split partitions once feature flags are known
    So far we've been creating new partition only when BRP was enabled.
    The reason is that finch settings aren't known at the time of the first
    allocation. Once they become known, we can no longer switch the original
    partition to BRP, thus we need to create a new one.
    
    By always creating a new partition, we'll enable easy testing of other
    features that can't be easily enabled on-the-fly in an already created
    partition.
    
    Change-Id: I8063c94cf8a0d82796ec166fbe647e03da762ced
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5138895
    Reviewed-by: Mikihito Matsuura <mikt@google.com>
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1240061}

--- base/allocator/partition_alloc_support.cc
+++ base/allocator/partition_alloc_support.cc
@@ -855,7 +855,6 @@ PartitionAllocSupport::GetBrpConfiguration(const std::string& process_type) {
   CHECK(base::FeatureList::GetInstance());
 
   bool enable_brp = false;
-  bool split_main_partition = false;
   bool process_affected_by_brp_flag = false;
   size_t ref_count_size = 0;
 
@@ -901,7 +900,6 @@ PartitionAllocSupport::GetBrpConfiguration(const std::string& process_type) {
 
       case base::features::BackupRefPtrMode::kEnabled:
         enable_brp = true;
-        split_main_partition = true;
         break;
     }
 
@@ -927,7 +925,6 @@ PartitionAllocSupport::GetBrpConfiguration(const std::string& process_type) {
 
   return {
       enable_brp,
-      split_main_partition,
       process_affected_by_brp_flag,
       ref_count_size,
   };
@@ -1142,10 +1139,8 @@ void PartitionAllocSupport::ReconfigureAfterFeatureListInit(
   allocator_shim::ConfigurePartitions(
       allocator_shim::EnableBrp(brp_config.enable_brp),
       allocator_shim::EnableMemoryTagging(enable_memory_tagging),
-      memory_tagging_reporting_mode,
-      allocator_shim::SplitMainPartition(brp_config.split_main_partition ||
-                                         enable_memory_tagging),
-      brp_config.ref_count_size, bucket_distribution,
+      memory_tagging_reporting_mode, brp_config.ref_count_size,
+      bucket_distribution,
       allocator_shim::SchedulerLoopQuarantine(scheduler_loop_quarantine),
       scheduler_loop_quarantine_capacity_in_bytes,
       scheduler_loop_quarantine_capacity_count,
