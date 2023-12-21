7261d421152fd7e1dc40bc63142aa527c17af56b
Peter Kvitek <kvitekp@chromium.org>
2023-12-21 21:23:43 +0000

    [DevTools] Set focus to the newly created target
    DevTools protocol Target.createTarget() does not activate newly
    created target causing unexpected behavior (focus notifications
    lost for one).
    
    This CL changes Target.createTarget() behavior so that it explicitly
    sets focus to created web contents unless the |background| argument
    is set.
    
    Bug: 1454012
    Change-Id: If2f2927afe1c236c479f16f282829393a9d7fdb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5141131
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1240315}

--- chrome/browser/devtools/protocol/target_handler.cc
+++ chrome/browser/devtools/protocol/target_handler.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/common/webui_url_constants.h"
 #include "content/public/browser/devtools_agent_host.h"
+#include "content/public/browser/web_contents.h"
 #include "content/public/common/url_constants.h"
 #include "content/public/common/url_utils.h"
 
@@ -136,6 +137,10 @@ protocol::Response TargetHandler::CreateTarget(
   Navigate(&params);
   if (!params.navigated_or_inserted_contents)
     return protocol::Response::ServerError("Failed to open a new tab");
+
+  if (!create_in_background) {
+    params.navigated_or_inserted_contents->Focus();
+  }
 
   if (for_tab.value_or(false)) {
     *out_target_id = content::DevToolsAgentHost::GetOrCreateForTab(
