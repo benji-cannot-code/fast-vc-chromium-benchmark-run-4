57a18cfd6cb0cf933ad70e2947269d06b18792c0
Rebekah Potter <rbpotter@chromium.org>
2023-12-21 23:35:45 +0000

    WebUI: Fork cr-buttons, cr-dialog, cr-input for Ash
    Forking all these elements together since cr-dialog depends on
    cr-input and cr-icon-button.
    
    Bug: 1512231
    Change-Id: Ibe2f5b9ad48e67af64b13a097c270eee62535783
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5136517
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1240380}

--- chrome/test/data/webui/chromeos/scanning/multi_page_scan_test.ts
+++ chrome/test/data/webui/chromeos/scanning/multi_page_scan_test.ts
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://webui-test/chromeos/mojo_webui_test_support.js';
 import 'chrome://scanning/multi_page_scan.js';
 
+import {CrButtonElement} from 'chrome://resources/ash/common/cr_elements/cr_button/cr_button.js';
 import {loadTimeData} from 'chrome://resources/ash/common/load_time_data.m.js';
 import {strictQuery} from 'chrome://resources/ash/common/typescript_utils/strict_query.js';
-import {CrButtonElement} from 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import {assert} from 'chrome://resources/js/assert.js';
 import {MultiPageScanElement} from 'chrome://scanning/multi_page_scan.js';
 import {AppState} from 'chrome://scanning/scanning_app_types.js';
