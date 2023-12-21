fe29846cf375969ecac9f7fce6e6bde7c87a1d89
Michael Checo <michaelcheco@google.com>
2023-12-21 19:07:38 +0000

    shimless: Convert onboarding-choose-wp-disable-method-page to TS
    Bug: b:315002705
    Change-Id: I5040127ac16f11599fdbe87da040a7d61d7761c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5096387
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1240253}

--- ash/webui/shimless_rma/resources/onboarding_choose_wp_disable_method_page.ts
+++ ash/webui/shimless_rma/resources/onboarding_choose_wp_disable_method_page.ts
@@ -6,13 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import './shimless_rma_shared.css.js';
 import './base_page.js';
 
-import {I18nBehavior, I18nBehaviorInterface} from 'chrome://resources/ash/common/i18n_behavior.js';
-import {mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
 
 import {getShimlessRmaService} from './mojo_interface_provider.js';
 import {getTemplate} from './onboarding_choose_wp_disable_method_page.html.js';
 import {ShimlessRmaServiceInterface, StateResult} from './shimless_rma.mojom-webui.js';
 import {disableNextButton, enableNextButton, focusPageTitle} from './shimless_rma_util.js';
+import {OnSelectedChangedEvent} from './events.js';
 
 /**
  * @fileoverview
@@ -23,19 +24,12 @@ import {disableNextButton, enableNextButton, focusPageTitle} from './shimless_rm
  * status.
  */
 
-/**
- * @constructor
- * @extends {PolymerElement}
- * @implements {I18nBehaviorInterface}
- */
-const OnboardingChooseWpDisableMethodPageBase =
-    mixinBehaviors([I18nBehavior], PolymerElement);
+const OnboardingChooseWpDisableMethodPageBase = I18nMixin(PolymerElement);
 
-/** @polymer */
 export class OnboardingChooseWpDisableMethodPage extends
-    OnboardingChooseWpDisableMethodPageBase {
+  OnboardingChooseWpDisableMethodPageBase {
   static get is() {
-    return 'onboarding-choose-wp-disable-method-page';
+    return 'onboarding-choose-wp-disable-method-page' as const;
   }
 
   static get template() {
@@ -45,12 +39,10 @@ export class OnboardingChooseWpDisableMethodPage extends
   static get properties() {
     return {
       /**
-       * Set by shimless_rma.js.
-       * @type {boolean}
+       * Set by shimless_rma.ts.
        */
       allButtonsDisabled: Boolean,
 
-      /** @private */
       hwwpMethod: {
         type: String,
         value: '',
@@ -58,24 +50,17 @@ export class OnboardingChooseWpDisableMethodPage extends
     };
   }
 
-  constructor() {
-    super();
-    /** @private {ShimlessRmaServiceInterface} */
-    this.shimlessRmaService = getShimlessRmaService();
-  }
+  shimlessRmaService: ShimlessRmaServiceInterface = getShimlessRmaService();
+  allButtonsDisabled: boolean;
+  private hwwpMethod: string;
 
-  /** @override */
-  ready() {
+  override ready() {
     super.ready();
 
     focusPageTitle(this);
   }
 
-  /**
-   * @param {!CustomEvent<{value: string}>} event
-   * @protected
-   */
-  onHwwpDisableMethodSelectionChanged(event) {
+  protected onHwwpDisableMethodSelectionChanged(event: OnSelectedChangedEvent) {
     this.hwwpMethod = event.detail.value;
     const disabled = !this.hwwpMethod;
     if (disabled) {
@@ -85,8 +70,7 @@ export class OnboardingChooseWpDisableMethodPage extends
     }
   }
 
-  /** @return {!Promise<!{stateResult: !StateResult}>} */
-  onNextButtonClick() {
+  onNextButtonClick(): Promise<{stateResult: StateResult}> {
     if (this.hwwpMethod === 'hwwpDisableMethodManual') {
       return this.shimlessRmaService.chooseManuallyDisableWriteProtect();
     } else if (this.hwwpMethod === 'hwwpDisableMethodRsu') {
@@ -97,6 +81,12 @@ export class OnboardingChooseWpDisableMethodPage extends
   }
 }
 
+declare global {
+  interface HTMLElementTagNameMap {
+    [OnboardingChooseWpDisableMethodPage.is]: OnboardingChooseWpDisableMethodPage;
+  }
+}
+
 customElements.define(
-    OnboardingChooseWpDisableMethodPage.is,
-    OnboardingChooseWpDisableMethodPage);
+  OnboardingChooseWpDisableMethodPage.is,
+  OnboardingChooseWpDisableMethodPage);
