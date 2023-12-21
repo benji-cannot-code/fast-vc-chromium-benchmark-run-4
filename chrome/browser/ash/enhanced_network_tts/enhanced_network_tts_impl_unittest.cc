25de5cbacb74798cbad304387330efd4364d47d5
Maks Orlovich <morlovich@chromium.org>
2023-12-21 17:58:38 +0000

    Migrate to TestURLLoaderFactory::GetSafeWeakWrapper() in /chrome/browser/ash/enhanced_network_tts
    ...instead of directly using WeakWrapperSharedURLLoaderFactory, which is
    prone to creating dangling pointers.  This is in preparation for removing the
    DanglingUntriaged annotation in WeakWrapperSharedURLLoaderFactory, which
    silences the warnings about such scenarios.
    
    This CL was uploaded by git cl split.
    
    R=dtseng@chromium.org
    
    Change-Id: I56157a6bbf3901c7961d4309d86dc751d53a1067
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5144769
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Auto-Submit: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1240211}

--- chrome/browser/ash/enhanced_network_tts/enhanced_network_tts_impl_unittest.cc
+++ chrome/browser/ash/enhanced_network_tts/enhanced_network_tts_impl_unittest.cc
@@ -33,9 +33,7 @@ namespace {
 class TestServerURLLoaderFactory {
  public:
   TestServerURLLoaderFactory()
-      : shared_loader_factory_(
-            base::MakeRefCounted<network::WeakWrapperSharedURLLoaderFactory>(
-                &loader_factory_)) {}
+      : shared_loader_factory_(loader_factory_.GetSafeWeakWrapper()) {}
   TestServerURLLoaderFactory(const TestServerURLLoaderFactory&) = delete;
   TestServerURLLoaderFactory& operator=(const TestServerURLLoaderFactory&) =
       delete;
