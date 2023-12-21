2c00438f64f05f33a933ff5074c29fab79d42484
Danny Wang <wangdanny@google.com>
2023-12-21 00:36:21 +0000

    customization: Validate device lists
    Add ValidateDeviceLists function to verify if there is no duplicate vid
    pid in every two device metadata lists.
    
    Bug: b/241965717
    Test: ash_unittests
    Change-Id: Icb49f6aa2c0155d85a818240bf3b42d2100fdf4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5133004
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: Danny Wang <wangdanny@google.com>
    Cr-Commit-Position: refs/heads/main@{#1239926}

--- ash/system/input_device_settings/input_device_settings_controller_impl.cc
+++ ash/system/input_device_settings/input_device_settings_controller_impl.cc
@@ -1434,9 +1434,14 @@ void InputDeviceSettingsControllerImpl::DispatchGraphicsTabletSettingsChanged(
 mojom::CustomizationRestriction
 InputDeviceSettingsControllerImpl::GetMouseCustomizationRestriction(
     const ui::InputDevice& mouse) {
-  const auto* metadata = GetMouseMetadata(mouse);
-  if (metadata) {
-    return metadata->customization_restriction;
+  const auto* mouse_metadata = GetMouseMetadata(mouse);
+  if (mouse_metadata) {
+    return mouse_metadata->customization_restriction;
+  }
+  const auto* keyboard_mouse_combo_metadata =
+      GetKeyboardMouseComboMetadata(mouse);
+  if (keyboard_mouse_combo_metadata) {
+    return keyboard_mouse_combo_metadata->customization_restriction;
   }
 
   return mojom::CustomizationRestriction::kDisableKeyEventRewrites;
