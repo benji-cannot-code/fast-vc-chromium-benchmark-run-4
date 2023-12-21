a244aa9592a6c425d2fabf6f8780d5eada74106f
Austin Sullivan <asully@chromium.org>
2023-12-21 14:13:03 +0000

    sql: Use BuiltInRecovery for BrowsingTopicsSiteDataStorage
    sql::Recovery is being deprecated in favor of sql::BuiltInRecovery
    
    Bug: 1385500
    Change-Id: Ib4500199d7dc2f5042e908bcf965f464676bba0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4874827
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1240124}

--- content/browser/browsing_topics/browsing_topics_site_data_storage.h
+++ content/browser/browsing_topics/browsing_topics_site_data_storage.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/containers/flat_set.h"
+#include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
@@ -26,6 +27,9 @@ class Statement;
 }  // namespace sql
 
 namespace content {
+
+BASE_DECLARE_FEATURE(
+    kBrowsingTopicsSiteDataStorageUseBuiltInRecoveryIfSupported);
 
 class CONTENT_EXPORT BrowsingTopicsSiteDataStorage {
  public:
