83fec7244e04a418d0c66f02d7b4e1b586455bda
Joshua Hood <jdh@chromium.org>
2023-12-21 15:30:17 +0000

    [3PCD] Add RuntimeEnabledFeatures entry for 1P 3PC DT
    Forthcoming CLs will make use of this new runtime enabled feature.
    
    Bug: 1513317
    Change-Id: Ib93b91a64061b660948570c64f56670e193bf882
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5137956
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Joshua Hood <jdh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1240153}

--- third_party/blink/common/origin_trials/persistent_origin_trials.cc
+++ third_party/blink/common/origin_trials/persistent_origin_trials.cc
@@ -25,6 +25,7 @@ bool IsTrialPersistentToNextResponse(base::StringPiece trial_name) {
       // Production persistent origin trials follow below:
       "WebViewXRequestedWithDeprecation",
       "Tpcd",
+      "Tpcd1p",
   };
   return base::Contains(kPersistentTrials, trial_name);
 }
