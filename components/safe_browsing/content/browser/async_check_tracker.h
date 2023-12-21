4edb22c34fd932ca75b505d0ad2e18453def9fe9
Xinghui Lu <xinghuilu@chromium.org>
2023-12-21 23:16:56 +0000

    [AsyncSB] Show interstitial in AsyncCheckTracker when UrlCheckerOnSB completes.
    AsyncCheckTracker will show an interstitial if the check is completed
    between BrowserUrlLoaderThrottle::WillProcessResponse and
    WebContentsObserver::DidFinishNavigation. When the complete callback
    from UrlCheckerOnSB is called, AsyncCheckTracker will cache the
    result and wait for DidFinishNavigation to be called. When
    DidFinishNavigation is called, AsyncCheckTracker will trigger a
    warning via BaseUIManager::DisplayBlockingPage.
    
    The information of post commit error message will be added in
    UrlCheckerOnSB::OnCompleteCheckResult in a follow up CL.
    
    Code references:
    1) The check on the severest threat is from
    SafeBrowsingNavigationThrottle::WillFailRequest[1].
    2) Setting render_process_id and render_frame_token is from
    ClientSideDetectionHost::MaybeShowPhishingWarning[2].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:components/safe_browsing/content/browser/safe_browsing_navigation_throttle.cc;l=45;drc=2f1cf47e4272b96f848d0f25fd1ca3b08db01183
    [2] https://source.chromium.org/chromium/chromium/src/+/main:components/safe_browsing/content/browser/client_side_detection_host.cc;l=734;drc=8e78783dc1f7007bad46d657c9f332614e240fd8
    
    Bug: 1501194
    Change-Id: I8f861b7d790f79a012056bac665582bb66cfd6de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5127688
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1240374}

--- components/safe_browsing/content/browser/async_check_tracker.h
+++ components/safe_browsing/content/browser/async_check_tracker.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/content/browser/url_checker_on_sb.h"
 #include "components/security_interstitials/core/unsafe_resource.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 
 namespace safe_browsing {
@@ -31,7 +32,8 @@ class BaseUIManager;
 // This class should only be called on the UI thread.
 // TODO(crbug.com/1501194): Implement this class.
 class AsyncCheckTracker
-    : public content::WebContentsUserData<AsyncCheckTracker> {
+    : public content::WebContentsUserData<AsyncCheckTracker>,
+      public content::WebContentsObserver {
  public:
   static AsyncCheckTracker* GetOrCreateForWebContents(
       content::WebContents* web_contents,
@@ -52,6 +54,9 @@ class AsyncCheckTracker
   // Takes ownership of `checker`.
   void TransferUrlChecker(std::unique_ptr<UrlCheckerOnSB> checker);
 
+  // content::WebContentsObserver methods:
+  void DidFinishNavigation(content::NavigationHandle* handle) override;
+
   bool HasPendingCheckerForTesting();
 
   base::WeakPtr<AsyncCheckTracker> GetWeakPtr();
@@ -59,9 +64,13 @@ class AsyncCheckTracker
  private:
   friend class content::WebContentsUserData<AsyncCheckTracker>;
   friend class SBBrowserUrlLoaderThrottleTestBase;
+  friend class AsyncCheckTrackerTest;
 
   AsyncCheckTracker(content::WebContents* web_contents,
                     scoped_refptr<BaseUIManager> ui_manager);
+
+  // Called by `UrlCheckerOnSB`, when the check completes.
+  void PendingCheckerCompleted(UrlCheckerOnSB::OnCompleteCheckResult result);
 
   // Used to display a warning.
   scoped_refptr<BaseUIManager> ui_manager_;
@@ -69,6 +78,10 @@ class AsyncCheckTracker
   // Pending Safe Browsing checks on the current page.
   // TODO(crbug.com/1501194): Support holding multiple checkers.
   std::unique_ptr<UrlCheckerOnSB> pending_checker_;
+
+  // Set to true if interstitial should be shown after DidFinishNavigation is
+  // called. Reset to false after interstitial is triggered.
+  bool show_interstitial_after_finish_navigation_ = false;
 
   base::WeakPtrFactory<AsyncCheckTracker> weak_factory_{this};
 
