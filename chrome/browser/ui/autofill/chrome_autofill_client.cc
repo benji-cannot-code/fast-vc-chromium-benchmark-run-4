ff0e4967642b074eb05e5cf59c777b841688142a
Darwin Yang <darwinyang@google.com>
2023-12-21 15:57:13 +0000

    [autofill][refactor][noop] No-op Refactor on `ChromeAutofillClient`.
    Instead of getting `SaveCardBubbleControllerImpl` and saving it to a
    variable just to call a single method. Skip saving the controller to a
    variable and directly call the method on the gotten controller.
    
    See original CL: https://chromium-review.googlesource.com/c/chromium/src/+/5050554/comment/e9e76605_8f684349.
    
    Bug: b/317088051
    Change-Id: Ia48c90ca28e9227f82652424666c69b30ab5b36c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5136742
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Commit-Queue: Darwin Yang <darwinyang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1240165}

--- chrome/browser/ui/autofill/chrome_autofill_client.cc
+++ chrome/browser/ui/autofill/chrome_autofill_client.cc
@@ -823,9 +823,8 @@ void ChromeAutofillClient::ConfirmSaveCreditCardLocally(
 #else
   // Do lazy initialization of SaveCardBubbleControllerImpl.
   SaveCardBubbleControllerImpl::CreateForWebContents(web_contents());
-  SaveCardBubbleControllerImpl* controller =
-      SaveCardBubbleControllerImpl::FromWebContents(web_contents());
-  controller->OfferLocalSave(card, options, std::move(callback));
+  SaveCardBubbleControllerImpl::FromWebContents(web_contents())
+      ->OfferLocalSave(card, options, std::move(callback));
 #endif
 }
 
@@ -871,10 +870,9 @@ void ChromeAutofillClient::ConfirmSaveCreditCardToCloud(
 #else
   // Do lazy initialization of SaveCardBubbleControllerImpl.
   SaveCardBubbleControllerImpl::CreateForWebContents(web_contents());
-  SaveCardBubbleControllerImpl* controller =
-      SaveCardBubbleControllerImpl::FromWebContents(web_contents());
-  controller->OfferUploadSave(card, legal_message_lines, options,
-                              std::move(callback));
+  SaveCardBubbleControllerImpl::FromWebContents(web_contents())
+      ->OfferUploadSave(card, legal_message_lines, options,
+                        std::move(callback));
 #endif
 }
 
