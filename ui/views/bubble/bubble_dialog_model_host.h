b68d3a0533ae466cd447c7a6ae06fe84c318ab3e
Peter Boström <pbos@chromium.org>
2023-12-21 23:02:02 +0000

    Remove BubbleDialogModelHostContentsView Init()
    Small cleanup that prevents missing (or double) initialization.
    
    Also contains a fix where UpdateSpacingAndMargins() will set empty
    margins if there are no fields. Previously it just didn't get called if
    there were no fields.
    
    Bug: 1511034
    Change-Id: Ife34a63bdd7b47c488e3a6a7754082ab8302c5be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5146070
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1240365}

--- ui/views/bubble/bubble_dialog_model_host.h
+++ ui/views/bubble/bubble_dialog_model_host.h
@@ -81,8 +81,6 @@ class VIEWS_EXPORT BubbleDialogModelHost : public BubbleDialogDelegate,
   View* GetInitiallyFocusedView() override;
   void OnWidgetInitialized() override;
 
-  View* GetContentsViewForTesting();
-
   // ui::DialogModelHost:
   void Close() override;
   void OnDialogButtonChanged() override;
