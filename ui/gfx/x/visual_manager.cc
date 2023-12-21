1252172780c8449d40b189d41c4180f97dc53880
Jose Dapena Paz <jdapena@igalia.com>
2023-12-21 14:22:14 +0000

    IWYU: missing include for usage of bitset in visual_manager.cc
    Bug: 957519
    Change-Id: I2a48ca66c6d83c626faec3a29508737135cbc359
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5140466
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1240130}

--- ui/gfx/x/visual_manager.cc
+++ ui/gfx/x/visual_manager.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gfx/x/visual_manager.h"
 
+#include <bitset>
+
 #include "base/strings/string_number_conversions.h"
 #include "ui/gfx/x/atom_cache.h"
 #include "ui/gfx/x/event.h"
