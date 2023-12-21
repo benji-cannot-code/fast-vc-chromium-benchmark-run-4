95e948efa4aaf2f8a5918caad056721ff0a9e49f
Mason Freed <masonf@chromium.org>
2023-12-21 23:59:43 +0000

    Remove Part tracking completely
    While there was a flag (DOMPartsAPIActivePartTracking) to enable
    or disable this behavior, there was overhead associated with tracking
    that setting. This CL removes it entirely.
    
    It also had bugs (see crbug.com/1473990).
    
    Bug: 1453291
    Fixed: 1473990
    Change-Id: I8a5d7c8d49bbd65e7b85afcf702124e533fa4e6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4915886
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1240385}

--- third_party/blink/renderer/core/dom/node.cc
+++ third_party/blink/renderer/core/dom/node.cc
@@ -2200,28 +2200,6 @@ void Node::InvalidateIfHasEffectiveAppearance() const {
   layout_object->SetSubtreeShouldDoFullPaintInvalidation();
 }
 
-void Node::UpdateForRemovedDOMParts(ContainerNode& insertion_point) {
-  if (LIKELY(!RuntimeEnabledFeatures::DOMPartsAPIEnabled())) {
-    return;
-  }
-  if (auto* parts = GetDOMParts()) {
-    for (Part* part : *parts) {
-      part->PartDisconnected(*this);
-    }
-  }
-}
-
-void Node::UpdateForInsertedDOMParts(ContainerNode& insertion_point) {
-  if (LIKELY(!RuntimeEnabledFeatures::DOMPartsAPIEnabled())) {
-    return;
-  }
-  if (auto* parts = GetDOMParts()) {
-    for (Part* part : *parts) {
-      part->PartConnected(*this, insertion_point);
-    }
-  }
-}
-
 Node::InsertionNotificationRequest Node::InsertedInto(
     ContainerNode& insertion_point) {
   DCHECK(!ChildNeedsStyleInvalidation());
@@ -2234,7 +2212,6 @@ Node::InsertionNotificationRequest Node::InsertedInto(
     insertion_point.GetDocument().IncrementNodeCount();
 #endif
   }
-  UpdateForInsertedDOMParts(insertion_point);
   if (ParentOrShadowHostNode()->IsInShadowTree())
     SetFlag(kIsInShadowTreeFlag);
   if (auto* cache = GetDocument().ExistingAXObjectCache()) {
@@ -2255,7 +2232,6 @@ void Node::RemovedFrom(ContainerNode& insertion_point) {
     insertion_point.GetDocument().DecrementNodeCount();
 #endif
   }
-  UpdateForRemovedDOMParts(insertion_point);
   if (IsInShadowTree() && !ContainingTreeScope().RootNode().IsShadowRoot())
     ClearFlag(kIsInShadowTreeFlag);
   if (auto* cache = GetDocument().ExistingAXObjectCache()) {
