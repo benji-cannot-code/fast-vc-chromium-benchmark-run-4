6a68980f0021fd56a8879bed12fff195d77c4733
Zoraiz Naeem <zoraiznaeem@chromium.org>
2023-12-21 19:40:02 +0000

    [Nit] Use `ShouldWindowHaveRoundedCorners()` helper function
    Bug: b/287060338
    Change-Id: I8d00b62e5240fa56a738793a7793e00aaf889fb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5147088
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1240270}

--- ash/wm/wm_shadow_controller_delegate.cc
+++ ash/wm/wm_shadow_controller_delegate.cc
@@ -123,10 +123,8 @@ bool WmShadowControllerDelegate::ShouldShowShadowForWindow(
 
 bool WmShadowControllerDelegate::ShouldHaveRoundedShadowForWindow(
     const aura::Window* window) {
-  // Apply rounded corner to shadow, only if the frame associated with the
-  // `window` has rounded corners. Rounded corners on the frame are applied
-  // based on the `WindowStateType` of `window`.
-  return chromeos::GetFrameCornerRadius(window) > 0;
+  // Apply rounded corner to shadow, if the `window` has rounded corners.
+  return chromeos::ShouldWindowHaveRoundedCorners(window);
 }
 
 bool WmShadowControllerDelegate::ShouldUpdateShadowOnWindowPropertyChange(
