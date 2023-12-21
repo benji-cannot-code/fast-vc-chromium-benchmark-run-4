eaa5678d5337f6d3dfc0f4fdf44fe0c9ff435ead
Lily Chen <chlily@chromium.org>
2023-12-21 03:55:40 +0000

    [Downloads] Don't record PROCEED_DEEP_SCAN action when cancelling
    This fixes a bug that caused PROCEED_DEEP_SCAN to be (attempted to be)
    added to DownloadItemWarningData when the download is cancelled.
    
    The bug was due to incorrect use of fallthrough.
    
    Bug: 1499022
    Change-Id: Iaf314bc630e11ad5cde5016216673baef1af5ede
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5141148
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239992}

--- chrome/browser/download/bubble/download_bubble_ui_controller.cc
+++ chrome/browser/download/bubble/download_bubble_ui_controller.cc
@@ -280,7 +280,8 @@ void DownloadBubbleUIController::ProcessDownloadButtonPress(
       break;
     case DownloadCommands::CANCEL:
       model->SetActionedOn(true);
-      [[fallthrough]];
+      commands.ExecuteCommand(command);
+      break;
     case DownloadCommands::BYPASS_DEEP_SCANNING:
       DownloadItemWarningData::AddWarningActionEvent(
           model->GetDownloadItem(), warning_surface,
