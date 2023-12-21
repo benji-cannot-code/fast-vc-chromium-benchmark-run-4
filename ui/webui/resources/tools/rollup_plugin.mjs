5ba3ac0361c365d7b6a2aea8d42a013f401a66d5
dpapad <dpapad@chromium.org>
2023-12-21 00:20:32 +0000

    WebUI: Include Lit in prod builds and exclude Lit bundle in bundle_js().
    This is in preparation of starting a gradual cr_elements migration to
    Lit. Currently limiting the visibility of
    //third_party/lit/v3_0:build_ts to ui/webui/resources/cr_elements/.
    
    Note: bundle_js() logic has currently a limitation when it comes to
    excluding external paths (it can do so correctly for prefix paths, but
    not for complete paths). Adding an assertion to catch such cases, and
    using a prefix external path for Lit (chrome://resources/lit/v3_0/) to workaround the problem for now, even though only a single file is
    actually ever used (chrome://resources/lit/v3_0/lit.rollup.js).
    
    Bug: 1503223
    Change-Id: I172eefe7ac8aa02de4b0b4b00db0bee14b8f465d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5124288
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239923}

--- ui/webui/resources/tools/rollup_plugin.mjs
+++ ui/webui/resources/tools/rollup_plugin.mjs
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Plugin for rollup to correctly resolve resources.
  */
 import * as path from 'path';
+import * as assert from 'node:assert';
 
 function normalizeSlashes(filepath) {
   return filepath.replace(/\\/gi, '/');
@@ -49,6 +50,8 @@ function getPathForUrl(source, origin, urlPrefix, urlSrcPath, excludes) {
   if (source === urlPrefix) {
     // Handle case where 'urlPrefix` matches the entire `source` URL and
     // therefore is not just a prefix, but a complete URL.
+    assert.ok(!excludes.includes(source),
+        'Excluding non-prefix external paths not supprted yet.');
     return urlSrcPath;
   }
 
