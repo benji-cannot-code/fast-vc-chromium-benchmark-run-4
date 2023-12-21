852367360c386bc8ac4e311be0e78ee006c86072
Miguel Casas <mcasas@chromium.org>
2023-12-21 22:07:16 +0000

    media/gpu/v4l2: Refactor V4L2FrameRateControl to not use V4L2Device
    This CL refactors V4L2FrameRateControl to use a RepeatingCallback to
    access the Ioctl it needs, instead of holding on to V4L2Device as a
    whole. This is needed for the next CL, and should introduce no new
    functionality. In particular V4L2Device lifetime semantics would not
    be changed since it's kept as a ref counted both on ToT, directly,
    and inside the RepeatingCallback with this CL.
    
    Bug: b:279980150
    Change-Id: I20e58b663c0faa9e1046791a3eb97c3877c64a37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5147407
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Commit-Queue: Miguel Casas-Sanchez <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1240336}

--- media/gpu/v4l2/v4l2_framerate_control.cc
+++ media/gpu/v4l2/v4l2_framerate_control.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/gpu/v4l2/v4l2_framerate_control.h"
 
+#include <linux/videodev2.h>
+
 #include "base/command_line.h"
 #include "base/sequence_checker.h"
 #include "base/strings/string_number_conversions.h"
@@ -13,6 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/media_switches.h"
 
 namespace {
+// Numerical value of ioctl() OK return value;
+constexpr int kIoctlOk = 0;
+
 double GetUserFrameRate() {
   const base::CommandLine* cmd_line = base::CommandLine::ForCurrentProcess();
   if (!cmd_line->HasSwitch(switches::kHardwareVideoDecodeFrameRate))
@@ -32,9 +37,8 @@ double GetUserFrameRate() {
   return 0.0;
 }
 
-bool FrameRateControlPresent(scoped_refptr<media::V4L2Device> device) {
-  DCHECK(device);
-
+bool FrameRateControlPresent(
+    const media::V4L2FrameRateControl::IoctlAsCallback& ioctl_cb) {
   struct v4l2_streamparm parms;
   memset(&parms, 0, sizeof(parms));
   parms.type = V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE;
@@ -53,7 +57,7 @@ bool FrameRateControlPresent(scoped_refptr<media::V4L2Device> device) {
             << " fps.";
   }
 
-  if (device->Ioctl(VIDIOC_S_PARM, &parms) != 0) {
+  if (ioctl_cb.Run(VIDIOC_S_PARM, &parms) != kIoctlOk) {
     VLOG(1) << "Failed to issue VIDIOC_S_PARM command";
     return false;
   }
@@ -72,10 +76,10 @@ static constexpr base::TimeDelta kFrameIntervalFor24fps =
     base::Milliseconds(41);
 
 V4L2FrameRateControl::V4L2FrameRateControl(
-    scoped_refptr<V4L2Device> device,
+    const IoctlAsCallback& ioctl_cb,
     scoped_refptr<base::SequencedTaskRunner> task_runner)
-    : device_(device),
-      framerate_control_present_(FrameRateControlPresent(device)),
+    : ioctl_cb_(ioctl_cb),
+      framerate_control_present_(FrameRateControlPresent(ioctl_cb_)),
       current_frame_duration_avg_ms_(0),
       last_frame_display_time_(base::TimeTicks::Now()),
       frame_duration_moving_average_(kMovingAverageWindowSize),
@@ -111,7 +115,7 @@ void V4L2FrameRateControl::UpdateFrameRate() {
     TRACE_COUNTER_ID1("media,gpu", "V4L2 estimated frame rate (Hz)", this,
                       std::round(frame_duration_avg.ToHz()));
 
-    if (device_->Ioctl(VIDIOC_S_PARM, &parms) != 0) {
+    if (ioctl_cb_.Run(VIDIOC_S_PARM, &parms) != kIoctlOk) {
       LOG(ERROR) << "Failed to issue VIDIOC_S_PARM command";
       TRACE_EVENT0("media,gpu", "V4L2 VIDIOC_S_PARM call failed");
     }
