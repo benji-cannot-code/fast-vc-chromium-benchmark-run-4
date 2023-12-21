2d6c32c689ba9bb53968620c6efbc74885b05c4c
Norge Vizcay <vizcay@google.com>
2023-12-21 12:48:58 +0000

    Convert MatchQuantifier into a class enum.
    Bug: 1440504
    Change-Id: I0c67246842f616528a483b75ecf3056a6e43cbca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5144587
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1240100}

--- components/autofill/core/browser/data_model/autofill_structured_address_utils.cc
+++ components/autofill/core/browser/data_model/autofill_structured_address_utils.cc
@@ -239,15 +239,15 @@ std::string NoCapturePattern(const std::string& pattern,
   std::string quantifier;
   switch (options.quantifier) {
     // Makes the match optional.
-    case MATCH_OPTIONAL:
+    case MatchQuantifier::kOptional:
       quantifier = "?";
       break;
     // Makes the match lazy meaning that it is avoided if possible.
-    case MATCH_LAZY_OPTIONAL:
+    case MatchQuantifier::kLazyOptional:
       quantifier = "??";
       break;
     // Makes the match required.
-    case MATCH_REQUIRED:
+    case MatchQuantifier::kRequired:
       quantifier = "";
   }
 
@@ -265,15 +265,15 @@ std::string CaptureTypeWithAffixedPattern(const FieldType& type,
   std::string quantifier;
   switch (options.quantifier) {
     // Makes the match optional.
-    case MATCH_OPTIONAL:
+    case MatchQuantifier::kOptional:
       quantifier = "?";
       break;
     // Makes the match lazy meaning that it is avoided if possible.
-    case MATCH_LAZY_OPTIONAL:
+    case MatchQuantifier::kLazyOptional:
       quantifier = "??";
       break;
     // Makes the match required.
-    case MATCH_REQUIRED:
+    case MatchQuantifier::kRequired:
       quantifier = "";
   }
 
