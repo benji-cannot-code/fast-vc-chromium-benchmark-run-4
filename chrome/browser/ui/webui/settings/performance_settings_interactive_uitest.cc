93f3a6c68f98884d3de9b4f70a8695f7f9aeea7d
Alison Gale <agale@chromium.org>
2023-12-21 18:21:23 +0000

    Add battery saver browser test mixin
    To simplify test helpers, we will create two mixins, one for battery
    saver setup and the other for memory saver (with support for tabs and
    discarding). Additionally there will be Kombucha utils that define
    many of the common interactions.
    
    I want to send this out now to get feedback on this structure before I
    start working on the memory saver one which will be a slightly larger
    change.
    
    Bug: 1507067
    Change-Id: I1337b26ec91c16536e6bf71715df3978f0be4cfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5132765
    Commit-Queue: Alison Gale <agale@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Alison Gale <agale@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1240228}

--- chrome/browser/ui/webui/settings/performance_settings_interactive_uitest.cc
+++ chrome/browser/ui/webui/settings/performance_settings_interactive_uitest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/performance_manager/public/user_tuning/battery_saver_mode_manager.h"
 #include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/performance_controls/test_support/battery_saver_browser_test_mixin.h"
 #include "chrome/browser/ui/webui/feedback/feedback_dialog.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/test/base/ui_test_utils.h"
@@ -424,13 +425,9 @@ IN_PROC_BROWSER_TEST_F(PerformanceSettingsMultiStateModeInteractiveTest,
 }
 
 #if !BUILDFLAG(IS_CHROMEOS)
-class BatterySettingsInteractiveTest : public InteractiveBrowserTest {
+class BatterySettingsInteractiveTest
+    : public BatterySaverBrowserTestMixin<InteractiveBrowserTest> {
  public:
-  void SetUp() override {
-    SetUpFakeBatterySampler();
-    InteractiveBrowserTest::SetUp();
-  }
-
   void SetUpOnMainThread() override {
     InteractiveBrowserTest::SetUpOnMainThread();
     ASSERT_TRUE(embedded_test_server()->InitializeAndListen());
@@ -442,21 +439,9 @@ class BatterySettingsInteractiveTest : public InteractiveBrowserTest {
     InteractiveBrowserTest::TearDownOnMainThread();
   }
 
-  void SetUpFakeBatterySampler() {
-    auto test_sampling_event_source =
-        std::make_unique<base::test::TestSamplingEventSource>();
-    auto test_battery_level_provider =
-        std::make_unique<base::test::TestBatteryLevelProvider>();
-
-    sampling_source_ = test_sampling_event_source.get();
-    battery_level_provider_ = test_battery_level_provider.get();
-    test_battery_level_provider->SetBatteryState(
-        base::test::TestBatteryLevelProvider::CreateBatteryState(1, true, 100));
-
-    battery_state_sampler_ =
-        base::BatteryStateSampler::CreateInstanceForTesting(
-            std::move(test_sampling_event_source),
-            std::move(test_battery_level_provider));
+  base::BatteryLevelProvider::BatteryState GetFakeBatteryState() override {
+    return base::test::TestBatteryLevelProvider::CreateBatteryState(1, true,
+                                                                    100);
   }
 
   auto ClickElement(const ui::ElementIdentifier& contents_id,
@@ -520,11 +505,6 @@ class BatterySettingsInteractiveTest : public InteractiveBrowserTest {
   }
 
  private:
-  raw_ptr<base::test::TestSamplingEventSource, DanglingUntriaged>
-      sampling_source_;
-  raw_ptr<base::test::TestBatteryLevelProvider, DanglingUntriaged>
-      battery_level_provider_;
-  std::unique_ptr<base::BatteryStateSampler> battery_state_sampler_;
   base::test::ScopedFeatureList scoped_feature_list_;
 };
 
