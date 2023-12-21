bd9e8ca9dac4d98bc517e5a63c324e4e8d434bab
Sonja <laurila@google.com>
2023-12-21 17:13:42 +0000

    Add missing AWC flag to protect calling visual properties
    Missing the flag caused a bug where the zoom level changes during
    navigation.
    
    The likely reason is that the zoom level is not ready yet and this line
    [1] returns the default zoom level instead of the real one and then the
    visual properties are simultaneously updated by
    `WebContentsObserver::PrimaryPageChanged` observer function [2].
    
    Likely fix will be to use another observer that gets triggered later but
    this needs more investigation once I get my loaner macbook.
    
    More context in the bug on comment#30.
    
    [1]: https://source.chromium.org/chromium/chromium/src/+/main:content/browser/renderer_host/render_widget_host_impl.cc;l=1046;drc=8a6341c2240d2ceaa54dd331c548d4cf3807ab6b
    [2]: https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/views/frame/browser_view.cc;l=2616;drc=97adbc10d2cde13dde9d71a0475e1167498ac9af
    
    Bug: 1503145
    Change-Id: I464b6dd0e3562f41db18d9c17fed065ef3ded9af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5145967
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1240192}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -227,6 +227,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_switches.h"
 #include "extensions/common/command.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/permissions/permission_utils.h"
 #include "ui/accessibility/accessibility_features.h"
 #include "ui/accessibility/ax_enums.mojom.h"
@@ -2587,6 +2588,10 @@ void BrowserView::OnCanResizeFromWebAPIChanged() {
 }
 
 void BrowserView::SynchronizeRenderWidgetHostVisualPropertiesForMainFrame() {
+  if (!base::FeatureList::IsEnabled(
+          blink::features::kDesktopPWAsAdditionalWindowingControls)) {
+    return;
+  }
   content::WebContents* web_contents = GetActiveWebContents();
   if (!web_contents || !web_contents->GetPrimaryMainFrame()) {
     return;
