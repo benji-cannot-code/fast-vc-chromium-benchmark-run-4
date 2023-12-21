9652937357b74d90e075de83f081ca057ac3d451
Bartek Nowierski <bartekn@chromium.org>
2023-12-21 07:48:47 +0000

    [BRP] Remove PartitionAllocBackupRefPtrForAsh experiment
    This makes it effectively turned on.
    
    This also removes the plumbing related to ExperimentalAsh, but leaves
    the (defunct) alias, as it's spread throughout the code. To be removed
    soon.
    
    Change-Id: If5f56e86d4b0ad12c9201d3f6ce4af0045681dbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5143059
    Auto-Submit: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Reviewed-by: Mikihito Matsuura <mikt@google.com>
    Cr-Commit-Position: refs/heads/main@{#1240034}

--- base/allocator/partition_alloc_features.h
+++ base/allocator/partition_alloc_features.h
@@ -166,8 +166,6 @@ extern const BASE_EXPORT base::FeatureParam<bool>
 extern const BASE_EXPORT base::FeatureParam<BucketDistributionMode>
     kPartitionAllocBucketDistributionParam;
 
-BASE_EXPORT BASE_DECLARE_FEATURE(kPartitionAllocBackupRefPtrForAsh);
-
 BASE_EXPORT BASE_DECLARE_FEATURE(kLowerPAMemoryLimitForNonMainRenderers);
 BASE_EXPORT BASE_DECLARE_FEATURE(kPartitionAllocPCScanMUAwareScheduler);
 BASE_EXPORT BASE_DECLARE_FEATURE(kPartitionAllocPCScanStackScanning);
