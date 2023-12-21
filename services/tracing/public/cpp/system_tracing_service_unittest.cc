d5a94953020de35769d3a184d1878491d0d5aba0
Chinglin Yu <chinglinyu@chromium.org>
2023-12-21 03:57:05 +0000

    [Tracing w/SDK] enable async socket connection in sandboxed processes
    This CL enables async socket connection when system tracing is enabled
    in sandboxed child processes. The async socket connection makes a mojo
    request to open the producer socket in the browser process and receives
    the open socket handle from the mojo response.
    
    The implementation of connecting the producer socket is migrated to use
    socket syscalls directly without using perfetto::base::UnixSocket.
    
    Bug: b/245116106
    
    Change-Id: I441132d622bc31194136d3e764cf8c50ffdece1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5057031
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Commit-Queue: Chinglin Yu <chinglinyu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239994}

--- services/tracing/public/cpp/system_tracing_service_unittest.cc
+++ services/tracing/public/cpp/system_tracing_service_unittest.cc
@@ -47,6 +47,7 @@ class SystemTracingServiceTest : public testing::Test {
   }
 
   void TearDown() override {
+    system_service_ = nullptr;
     // Restore the value of Perfetto producer socket name env variable.
     if (saved_producer_sock_env_) {
       ASSERT_EQ(0,
