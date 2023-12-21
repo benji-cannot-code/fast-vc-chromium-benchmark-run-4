258632cc35cd16d93a1841f0b667c8bb036c0e1c
Yuzu Saijo <yuzus@chromium.org>
2023-12-21 06:04:11 +0000

    Revert "Don't send IME Candidate selection a11y event in autocomplete suggestion"
    This reverts commit ce14d426ba259a9fb5c2c5b4b3fb6e7c50a13b7a.
    
    Reason for revert: crbug.com/1513611 The new test cases introduced in this CL are constantly failing on bots.
    
    Original change's description:
    > Don't send IME Candidate selection a11y event in autocomplete suggestion
    >
    > Sending an event when showing autocomplete suggestion will distrupt
    > user's typing for ChromeVox users.
    >
    > This change:
    > - Adds |is_user_selecting| flag to CandidateWindow.
    > - Let our binding to infer the flag for now. TODO is to plumb it from
    >   the extension.
    > - Makes CandidateView and CandidateWindowView dispatch an accessibility
    >   event only when user is actually choosing item from the candidates.
    >
    > This is a part of http://go/chromevox-jp-ime.
    >
    > Bug: b:259380284, b:259379909, b:300576550
    > Test: CandidateWindowViewTest, InputMethodEngineTest, SpokenFeedbackWithCandidateWindowTest
    > Change-Id: I64ddab961ea1612b3e8804c752537468f15c1e50
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4884639
    > Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    > Commit-Queue: Hiroki Sato <hirokisato@chromium.org>
    > Reviewed-by: Darren Shen <shend@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1239903}
    
    Bug: b:259380284, b:259379909, b:300576550
    Change-Id: I7bcd7220f9ab11e2e85cf45c4ac997b002975720
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5143370
    Auto-Submit: Yuzu Saijo <yuzus@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Yuzu Saijo <yuzus@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1240021}

--- chrome/browser/ash/input_method/input_method_engine.h
+++ chrome/browser/ash/input_method/input_method_engine.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/containers/span.h"
 #include "base/memory/raw_ptr.h"
 #include "base/scoped_observation.h"
 #include "base/types/expected.h"
@@ -350,10 +349,6 @@ class InputMethodEngine : virtual public TextInputMethod,
                           ui::ime::InputMethodMenuItem* property);
 
   void OnScreenProjectionChanged(bool is_projected);
-
-  // Infers if the user is choosing from a candidate from the window.
-  // TODO(b/300576550): get this information from IME.
-  bool InferIsUserSelecting(base::span<const Candidate> candidates);
 
   // The current candidate window.
   ui::CandidateWindow candidate_window_;
