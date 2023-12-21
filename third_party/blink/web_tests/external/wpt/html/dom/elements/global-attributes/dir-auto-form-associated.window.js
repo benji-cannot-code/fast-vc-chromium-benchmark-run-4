c58bfb3d630d2f3355edffbcdc2393c22d0c8bc1
Jonathan Lee <jonathanjlee@google.com>
2023-12-21 20:03:49 +0000

    Import wpt@8ac73b1966e968e9224a7869e5d1bf38b0a340cc
    Using wpt-import in Chromium 1f28199107e4d6fa7b6f19afbee7f596d39296c3.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    estade@chromium.org, asully@chromium.org, ayui@chromium.org, garykac@chromium.org, jsbell@chromium.org:
      external/wpt/clipboard-apis
    mstensho@chromium.org:
      external/wpt/css/css-break
    mustaq@chromium.org:
      external/wpt/html/user-activation
    rego@igalia.com:
      external/wpt/css/css-grid
      external/wpt/css/selectors
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I617336b4cc6825cfbc2acdbf0bd8a986fb1eb6f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5142870
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Jie Sheng <jiesheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1240280}

--- third_party/blink/web_tests/external/wpt/html/dom/elements/global-attributes/dir-auto-form-associated.window.js
+++ third_party/blink/web_tests/external/wpt/html/dom/elements/global-attributes/dir-auto-form-associated.window.js
@@ -19,9 +19,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     t.add_cleanup(() => input.remove());
     input.type = type;
     assert_equals(input.type, type);
-    input.dir = "auto";
-    input.value = "\u05D0"; // The Hebrew letter Alef (strongly RTL)
     document.body.append(input);
+
+    input.setAttribute("value", "\u05D0"); // The Hebrew letter Alef (strongly RTL)
+    assert_true(input.matches(":dir(ltr)"));
+    input.removeAttribute("value");
+
+    input.dir = "auto";
+    input.setAttribute("value", "\u05D0");
+    assert_true(input.matches(":dir(rtl)"));
+    input.removeAttribute("value");
+    assert_true(input.matches(":dir(ltr)"));
+
+    input.value = "\u05D0";
     assert_true(input.matches(":dir(rtl)"));
   }, `<input dir=auto type=${type}> directionality`);
 });
