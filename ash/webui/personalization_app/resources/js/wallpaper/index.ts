7c32633ee7aa3d00f95b1333450c96dd85eb6a89
Erica Lee <ericamlee@google.com>
2023-12-22 04:22:55 +0000

    p13n: move wallpaper grid item
    Moved wallpaper grid item into ash/webui/common/resources to share it
    with VC background.
    
    https://screencast.googleplex.com/cast/NTU5MTU3Nzc2MTE1MzAyNHxkN2U4ZTVmYy03MQ
    
    BUG=b:312208348
    TEST=browser_tests *PersonalizationApp*
    
    Change-Id: Ib5d6ab43966cc5eba631d3cc62f45f7ce6f7d22c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5123938
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Commit-Queue: Erica Lee <ericamlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1240449}

--- ash/webui/personalization_app/resources/js/wallpaper/index.ts
+++ ash/webui/personalization_app/resources/js/wallpaper/index.ts
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/ash/common/wallpaper_grid_item_element.js';
 import './google_photos_albums_element.js';
 import './google_photos_collection_element.js';
 import './google_photos_photos_by_album_id_element.js';
@@ -10,7 +11,6 @@ import './google_photos_photos_element.js';
 import './google_photos_zero_state_element.js';
 import './local_images_element.js';
 import './time_of_day_wallpaper_dialog_element.js';
-import './wallpaper_grid_item_element.js';
 import './wallpaper_collections_element.js';
 import './wallpaper_error_element.js';
 import './wallpaper_fullscreen_element.js';
