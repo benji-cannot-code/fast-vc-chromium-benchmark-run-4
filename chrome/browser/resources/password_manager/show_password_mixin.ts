19675b65c26a18527d5f388de2ec60644539acfc
dpapad <dpapad@chromium.org>
2023-12-22 11:36:27 +0000

    WebUI: Deploy "import type" imports in c/b/r/password_manager.
    Bug: 1494527
    Change-Id: I5efd3e2d69f4009977dc787ca6beaa5753ba1581
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5138324
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1240519}

--- chrome/browser/resources/password_manager/show_password_mixin.ts
+++ chrome/browser/resources/password_manager/show_password_mixin.ts
@@ -4,7 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
-import {dedupingMixin, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import type {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {dedupingMixin} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 type Constructor<T> = new (...args: any[]) => T;
 
