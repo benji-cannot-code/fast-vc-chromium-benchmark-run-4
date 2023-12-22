0eb2217c953919e0f20f02e5e37355bc2ce26418
Miguel Casas <mcasas@chromium.org>
2023-12-22 01:49:26 +0000

    media/gpu/V4L2StatefulVD: Use V4L2FrameRateControl
    This CL plugs said class to tweak the driver internal performance
    settings, just like V4L2VideoDecoder does on ToT.
    
    Bug: b:279980150
    Change-Id: Icb49e8a43e67e7678e0aaa5cc470b55bdbb8436a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5146969
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Commit-Queue: Miguel Casas-Sanchez <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1240412}

--- media/gpu/v4l2/v4l2_stateful_video_decoder.cc
+++ media/gpu/v4l2/v4l2_stateful_video_decoder.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "media/base/media_log.h"
 #include "media/gpu/macros.h"
+#include "media/gpu/v4l2/v4l2_framerate_control.h"
 #include "media/gpu/v4l2/v4l2_queue.h"
 #include "media/gpu/v4l2/v4l2_utils.h"
 #include "media/video/h264_parser.h"
@@ -380,6 +381,10 @@ void V4L2StatefulVideoDecoder::Initialize(const VideoDecoderConfig& config,
     // This will also Deallocate() all buffers and issue a VIDIOC_STREAMOFF.
     OUTPUT_queue_.reset();
     CAPTURE_queue_.reset();
+
+    framerate_control_ = std::make_unique<V4L2FrameRateControl>(
+        base::BindRepeating(&HandledIoctl, device_fd_.get()),
+        base::SequencedTaskRunner::GetCurrentDefault());
   }
 
   // At this point we initialize the |OUTPUT_queue_| only, following
@@ -976,7 +981,9 @@ void V4L2StatefulVideoDecoder::TryAndDequeueCAPTUREQueueBuffers() {
         VLOGF(3) << wrapped_frame->AsHumanReadableString();
         output_cb_.Run(std::move(wrapped_frame));
       } else {
+        DCHECK_EQ(queue_type, V4L2_MEMORY_DMABUF);
         VLOGF(3) << video_frame->AsHumanReadableString();
+        framerate_control_->AttachToVideoFrame(video_frame);
         output_cb_.Run(std::move(video_frame));
       }
 
