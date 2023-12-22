f4a92c87a7027ce1a4d41facd9267424851aed57
Jihad Hanna <jihadghanna@google.com>
2023-12-22 17:38:35 +0000

    Remove out-parameter from label extraction functions
    This CL removes the string out-parameter from label extraction functions
    and makes the return value a string.
    
    Bug: 1007974
    Change-Id: I51fba9f6ba6a460022ecf402c53771cbc6645fe8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5148473
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Auto-Submit: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1240604}

--- components/autofill/content/renderer/form_autofill_util_browsertest.cc
+++ components/autofill/content/renderer/form_autofill_util_browsertest.cc
@@ -406,9 +406,8 @@ TEST_F(FormAutofillUtilsTest, InferLabelForElementTest) {
 
     FormFieldData::LabelSource label_source =
         FormFieldData::LabelSource::kUnknown;
-    std::u16string label;
-    InferLabelForElement(form_target, label, label_source);
-    EXPECT_EQ(test_case.expected_label, label);
+    EXPECT_EQ(test_case.expected_label,
+              InferLabelForElement(form_target, label_source));
   }
 }
 
@@ -456,10 +455,8 @@ TEST_F(FormAutofillUtilsTest, InferLabelSourceTest) {
 
     FormFieldData::LabelSource label_source =
         FormFieldData::LabelSource::kUnknown;
-    std::u16string label;
-    EXPECT_TRUE(autofill::form_util::InferLabelForElement(form_target, label,
-                                                          label_source));
-    EXPECT_EQ(kLabelSourceExpectedLabel, label);
+    EXPECT_EQ(kLabelSourceExpectedLabel,
+              InferLabelForElement(form_target, label_source));
     EXPECT_EQ(test_case.label_source, label_source);
   }
 }
