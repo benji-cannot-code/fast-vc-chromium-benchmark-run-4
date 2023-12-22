3cc1b7fceadb270364bfa44dccf8fae29b228986
Quentin Pubert <qpubert@google.com>
2023-12-22 16:01:33 +0000

    [iOS] Hide Save to Drive if DownloadTask or WebState destroyed
    This CL adds the necessary code to ensure the Save to Drive UI is hidden
    if the associated DownloadTask is destroyed or if the associated
    WebState is hidden or destroyed. Namely:
    - The owner of SaveToDriveCoordinator now directly forwards it the
      DownloadTask it receives through SaveToDriveCommands implementation.
    - A SaveToDriveMediator class is added. It is owned by the coordinator
      and it observes a DownloadTask and WebState.
    - A DownloadTaskObserverBridge which enables the Objective-C mediator
      to act as a C++ DownloadTaskObserver.
    
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL
    Bug: 1495352
    Change-Id: I5ce236c8039223dafade9b9a1ea42c8f8209c53f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5122771
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1240584}

--- ios/chrome/browser/ui/browser_view/browser_coordinator.mm
+++ ios/chrome/browser/ui/browser_view/browser_coordinator.mm
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <memory>
 #import <optional>
 
-#import "base/apple/foundation_util.h"
 #import "base/metrics/histogram_functions.h"
 #import "base/scoped_observation.h"
 #import "base/strings/sys_string_conversions.h"
@@ -238,7 +237,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/public/provider/chrome/browser/text_zoom/text_zoom_api.h"
 #import "ios/public/provider/chrome/browser/voice_search/voice_search_api.h"
 #import "ios/public/provider/chrome/browser/voice_search/voice_search_controller.h"
-#import "ios/web/public/download/download_task.h"
 #import "ui/base/device_form_factor.h"
 #import "ui/base/l10n/l10n_util.h"
 
@@ -2642,10 +2640,7 @@ enum class ToolbarKind {
   _saveToDriveCoordinator = [[SaveToDriveCoordinator alloc]
       initWithBaseViewController:self.viewController
                          browser:self.browser
-                        fileName:base::apple::FilePathToNSString(
-                                     downloadTask->GenerateFileName())
-                        fileSize:downloadTask->GetTotalBytes()
-                        webState:downloadTask->GetWebState()];
+                    downloadTask:downloadTask];
   [_saveToDriveCoordinator start];
 }
 
--- ios/web_view/internal/cwv_download_task.mm
+++ ios/web_view/internal/cwv_download_task.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #import "ios/web/public/download/download_task.h"
 #include "ios/web/public/download/download_task_observer.h"
+#import "ios/web/public/download/download_task_observer_bridge.h"
 #include "ios/web_view/internal/cwv_web_view_internal.h"
 #include "net/base/mac/url_conversions.h"
 #include "net/base/net_errors.h"
@@ -25,31 +26,12 @@ NSErrorDomain const CWVDownloadErrorDomain =
 NSInteger const CWVDownloadErrorFailed = -100;
 NSInteger const CWVDownloadErrorAborted = -101;
 
-@interface CWVDownloadTask ()
-
-// Called when the download task has started, downloaded a chunk of data or
-// the download has been completed.
-- (void)downloadWasUpdated;
+@interface CWVDownloadTask () <CRWDownloadTaskObserver>
 
 @end
 
-namespace {
-// Bridges C++ observer method calls to Objective-C.
-class DownloadTaskObserverBridge : public web::DownloadTaskObserver {
- public:
-  explicit DownloadTaskObserverBridge(CWVDownloadTask* task) : task_(task) {}
-
-  void OnDownloadUpdated(web::DownloadTask* task) override {
-    [task_ downloadWasUpdated];
-  }
-
- private:
-  __weak CWVDownloadTask* task_ = nil;
-};
-}  // namespace
-
 @implementation CWVDownloadTask {
-  std::unique_ptr<DownloadTaskObserverBridge> _observerBridge;
+  std::unique_ptr<web::DownloadTaskObserverBridge> _observerBridge;
   std::unique_ptr<web::DownloadTask> _internalTask;
 }
 
@@ -85,7 +67,7 @@ class DownloadTaskObserverBridge : public web::DownloadTaskObserver {
     (std::unique_ptr<web::DownloadTask>)internalTask {
   self = [super init];
   if (self) {
-    _observerBridge = std::make_unique<DownloadTaskObserverBridge>(self);
+    _observerBridge = std::make_unique<web::DownloadTaskObserverBridge>(self);
     _internalTask = std::move(internalTask);
     _internalTask->AddObserver(_observerBridge.get());
   }
@@ -104,9 +86,10 @@ class DownloadTaskObserverBridge : public web::DownloadTaskObserver {
   _internalTask->Cancel();
 }
 
-#pragma mark - Private
+#pragma mark - CRWDownloadTaskObserver
 
-- (void)downloadWasUpdated {
+- (void)downloadUpdated:(web::DownloadTask*)task {
+  CHECK_EQ(_internalTask.get(), task);
   switch (_internalTask->GetState()) {
     case web::DownloadTask::State::kInProgress: {
       if ([_delegate
@@ -131,6 +114,8 @@ class DownloadTaskObserverBridge : public web::DownloadTaskObserver {
     }
   }
 }
+
+#pragma mark - Private
 
 - (void)notifyFinishWithErrorCode:(int)errorCode {
   NSError* error = nil;
