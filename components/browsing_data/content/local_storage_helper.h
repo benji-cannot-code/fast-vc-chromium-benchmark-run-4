d8a2a1e3ceedcbb0aaa016ec0db787895de3c1a2
Nan Lin <linnan@chromium.org>
2023-12-22 14:06:20 +0000

    [Code Health] Remove AcrossTasksDanglingUntriaged from local_storage_helper
    No test failures with AcrossTasksDanglingUntriaged tag removed.
    
    Bug: 1512637
    Change-Id: I849e5afeea6cf2e20460d6fe7088d5c7cddc085b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5142512
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1240551}

--- components/browsing_data/content/local_storage_helper.h
+++ components/browsing_data/content/local_storage_helper.h
@@ -55,7 +55,7 @@ class LocalStorageHelper : public base::RefCounted<LocalStorageHelper> {
   friend class base::RefCounted<LocalStorageHelper>;
   virtual ~LocalStorageHelper();
 
-  raw_ptr<content::DOMStorageContext, AcrossTasksDanglingUntriaged>
+  raw_ptr<content::DOMStorageContext>
       dom_storage_context_;  // Owned by the context
 };
 
