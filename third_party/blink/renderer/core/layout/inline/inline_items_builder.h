0319272757b7e4bb030eb79329f49e1996bb0d9f
Kent Tamura <tkent@chromium.org>
2023-12-23 04:12:58 +0000

    Layout: Introduce TransformedString
    It represents a text transformed by a LayoutText. This CL makes
    InlineItemsBuilder use it, but it has no behavior changes because now
    TransformedString just wraps a StringView.
    
    This is a preparation to fix crbug.com/486880 and crbug.com/1433232.
    
    Bug: 486880, 1433232
    Change-Id: I0aa967fac13f31633bb42c68aec7125e2d86ba2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5144091
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1240659}

--- third_party/blink/renderer/core/layout/inline/inline_items_builder.h
+++ third_party/blink/renderer/core/layout/inline/inline_items_builder.h
@@ -24,6 +24,7 @@ class ComputedStyle;
 class LayoutInline;
 class LayoutObject;
 class LayoutText;
+class TransformedString;
 struct InlineNodeData;
 
 // InlineItemsBuilder builds a string and a list of InlineItem from inlines.
@@ -211,17 +212,23 @@ class InlineItemsBuilderTemplate {
   // LayoutObject.
   InlineItem& Append(InlineItem::InlineItemType, UChar, LayoutObject*);
 
-  void AppendCollapseWhitespace(const StringView,
+  void AppendText(const TransformedString& transformed, LayoutText&);
+  void AppendCollapseWhitespace(const TransformedString&,
                                 const ComputedStyle*,
                                 LayoutText*);
-  void AppendPreserveWhitespace(const String&,
+  void AppendPreserveWhitespace(const TransformedString&,
                                 const ComputedStyle*,
                                 LayoutText*);
-  void AppendPreserveNewline(const String&, const ComputedStyle*, LayoutText*);
+  void AppendPreserveNewline(const TransformedString&,
+                             const ComputedStyle*,
+                             LayoutText*);
 
   void AppendForcedBreakCollapseWhitespace(LayoutObject*);
   void AppendForcedBreak(LayoutObject*);
-  bool AppendTextChunks(const String& string, LayoutText& layout_text);
+  bool AppendTextChunks(const TransformedString& transformed,
+                        LayoutText& layout_text);
+  void AppendTransformedString(const TransformedString& transformed,
+                               const LayoutText& layout_text);
   void ExitAndEnterSvgTextChunk(LayoutText& layout_text);
   void EnterSvgTextChunk(const ComputedStyle* style);
 
@@ -234,9 +241,9 @@ class InlineItemsBuilderTemplate {
   void RestoreTrailingCollapsibleSpaceIfRemoved();
   void RestoreTrailingCollapsibleSpace(InlineItem*);
 
-  void AppendTextItem(const StringView, LayoutText* layout_object);
+  void AppendTextItem(const TransformedString&, LayoutText* layout_object);
   InlineItem& AppendTextItem(InlineItem::InlineItemType type,
-                             const StringView,
+                             const TransformedString&,
                              LayoutText* layout_object);
   void AppendEmptyTextItem(LayoutText* layout_object);
 
@@ -247,13 +254,14 @@ class InlineItemsBuilderTemplate {
   bool MayBeBidiEnabled() const;
 
   bool ShouldInsertBreakOpportunityAfterLeadingPreservedSpaces(
-      const String&,
+      StringView,
       const ComputedStyle&,
       unsigned index = 0) const;
-  void InsertBreakOpportunityAfterLeadingPreservedSpaces(const String&,
-                                                         const ComputedStyle&,
-                                                         LayoutText*,
-                                                         unsigned* start);
+  void InsertBreakOpportunityAfterLeadingPreservedSpaces(
+      const TransformedString&,
+      const ComputedStyle&,
+      LayoutText*,
+      unsigned* start);
 
   friend class InlineItemsBuilderTest;
 };
