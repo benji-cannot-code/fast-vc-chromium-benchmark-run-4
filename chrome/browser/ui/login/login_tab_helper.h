3060c5d86157cb20f3d27297a04ec093c93992d0
Erik Chen <erikchen@chromium.org>
2023-12-27 01:06:16 +0000

    Improve LoginHandler (Part 5 / N)
    This CL is a refactor with no intended behavior change.
    
    This CL moves the extension processing logic out of LoginHandler and
    into Flow. This simplifies much of the logic in LoginHandler and
    LoginTabHelper.
    
    Bug: 1371177
    Change-Id: Iafabd9196d7c4f0e3e4cea8fd6f7b906417c301b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5152171
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1241071}

--- chrome/browser/ui/login/login_tab_helper.h
+++ chrome/browser/ui/login/login_tab_helper.h
@@ -71,6 +71,9 @@ class LoginTabHelper : public content::WebContentsObserver,
   WillProcessMainFrameUnauthorizedResponse(
       content::NavigationHandle* navigation_handle);
 
+  void RegisterExtensionCancelledNavigation(
+      const content::GlobalRequestID& request_id);
+
  protected:
   explicit LoginTabHelper(content::WebContents* web_contents);
   virtual std::unique_ptr<LoginHandler> CreateLoginHandler(
@@ -83,9 +86,6 @@ class LoginTabHelper : public content::WebContentsObserver,
 
   void HandleCredentials(
       const std::optional<net::AuthCredentials>& credentials);
-
-  void RegisterExtensionCancelledNavigation(
-      const content::GlobalRequestID& request_id);
 
   // When the user enters credentials into the login prompt, they are populated
   // in the auth cache and then page is reloaded to re-send the request with the
