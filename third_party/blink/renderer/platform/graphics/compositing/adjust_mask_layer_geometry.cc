fb1f561d0a03421fa2c2d5254373e36087ae7449
Philip Rogers <pdr@chromium.org>
2023-12-28 22:13:27 +0000

    Fix float-cast overflow in AdjustMaskLayerGeometry
    `outset` can overflow so this patch adds clamping. I manually verified
    this fixes the crash on a UBSAN build.
    
    Bug: 1500666
    Change-Id: I23eafd1688c1c81457b538075e6b00f34c8d961b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5155247
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1241420}

--- third_party/blink/renderer/platform/graphics/compositing/adjust_mask_layer_geometry.cc
+++ third_party/blink/renderer/platform/graphics/compositing/adjust_mask_layer_geometry.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/graphics/paint/geometry_mapper.h"
 #include "third_party/blink/renderer/platform/graphics/paint/transform_paint_property_node.h"
+#include "third_party/blink/renderer/platform/wtf/math_extras.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/geometry/vector2d_f.h"
 
@@ -31,10 +32,11 @@ void AdjustMaskLayerGeometry(const TransformPaintPropertyNode& transform,
   // Map a screen pixel into the layer.
   GeometryMapper::SourceToDestinationRect(TransformPaintPropertyNode::Root(),
                                           transform, pixel_rect);
-  int outset = ceilf(std::max(pixel_rect.width(), pixel_rect.height()) * 2);
   // Don't expand too far in extreme cases.
   constexpr int kMaxOutset = 1000;
-  outset = std::min(kMaxOutset, outset);
+  int outset =
+      ClampTo(std::ceil(std::max(pixel_rect.width(), pixel_rect.height()) * 2),
+              0, kMaxOutset);
   layer_offset -= gfx::Vector2dF(outset, outset);
   layer_bounds += gfx::Size(2 * outset, 2 * outset);
 }
