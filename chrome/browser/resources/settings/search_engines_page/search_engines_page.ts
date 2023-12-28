5cec00ff82b2bef54ee0bb30232a424f4db843cf
Fabio Tirelo <ftirelo@chromium.org>
2023-12-28 20:02:10 +0000

    [Site Search policy] View details of managed search engines
    This CL allows the user to view details of search engines created by
    mandatory policy by:
    - Adding a new "Details" button to the corresponding row in the search
      engines table to open the edit dialog
    - Apply the following changes to the edit dialog:
      - Change title to "Search engine"
      - Mark all input fields as readonly
      - Hide the "Cancel" button
      - Set the action button's text to "Done"
    
    Mocks: go/cbe-sitesearch (slide 23)
    Screenshots: b/309458596#comment6
    
    Fixed: b/309458596
    Bug: b/309458596
    Change-Id: Iba57935ddc93fe0d15290fefbc0389cac9f33dc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5154151
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1241398}

--- chrome/browser/resources/settings/search_engines_page/search_engines_page.ts
+++ chrome/browser/resources/settings/search_engines_page/search_engines_page.ts
@@ -176,7 +176,7 @@ export class SettingsSearchEnginesPageElement extends
         'search-engines-changed', this.enginesChanged_.bind(this));
 
     this.addEventListener(
-        'edit-search-engine',
+        'view-or-edit-search-engine',
         e => this.onEditSearchEngine_(e as SearchEngineEditEvent));
 
     this.addEventListener(
