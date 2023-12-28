74ea2fc3c7b0089f879d32f837d881978fb83f87
EmiliaPaz <emiliapaz@chromium.org>
2023-12-28 15:01:43 +0000

    Replace SetProminent() with SetStyle() in ash/
    Previously, MdTextButton only had 2 styles that could be set with
    SetProminent(). With the addition of a new style, now button style is
    set with SetStyle.
    
    Therefore, this remove calls of SetProminent() in favor of SetStyle(ButtonStyle::kProminent) or SetStyle(ButtonStyle::kDefault)
    
    No functionality changed.
    
    Bug: 1406008
    Change-Id: Ib58bbb0b5fcb0ffb954d615d0396cfae990e9d4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5154011
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1241359}

--- ash/components/arc/net/passpoint_dialog_view.cc
+++ ash/components/arc/net/passpoint_dialog_view.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/network/network_event_log.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/ui_base_types.h"
 #include "ui/chromeos/devicetype_utils.h"
 #include "ui/strings/grit/ui_strings.h"
 #include "ui/views/accessibility/view_accessibility.h"
@@ -233,7 +234,7 @@ std::unique_ptr<views::View> PasspointDialogView::MakeButtonsView() {
                   weak_factory_.GetWeakPtr(), /*allow=*/false))
               .SetText(l10n_util::GetStringUTF16(
                   IDS_ASH_ARC_PASSPOINT_APP_APPROVAL_DONT_ALLOW_BUTTON))
-              .SetProminent(false)
+              .SetStyle(ui::ButtonStyle::kDefault)
               .SetIsDefault(false),
           views::Builder<views::MdTextButton>()  // Allow button.
               .CopyAddressTo(&allow_button_)
@@ -242,7 +243,7 @@ std::unique_ptr<views::View> PasspointDialogView::MakeButtonsView() {
                   weak_factory_.GetWeakPtr(), /*allow=*/true))
               .SetText(l10n_util::GetStringUTF16(
                   IDS_ASH_ARC_PASSPOINT_APP_APPROVAL_ALLOW_BUTTON))
-              .SetProminent(true)
+              .SetStyle(ui::ButtonStyle::kProminent)
               .SetIsDefault(true))
       .Build();
 }
