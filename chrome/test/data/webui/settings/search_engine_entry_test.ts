d28fabbc4d5d2f6a596ff897a543f36fd00a2fb8
Fabio Tirelo <ftirelo@chromium.org>
2023-12-28 17:27:18 +0000

    [Code cleanup] Remove unused code in the Search Engines page
    This is a follow-up of crrev.com/c/5137338, and simply removes a class
    from search engine settings that is no longer used.
    
    Bug: b/309458596
    Change-Id: I20162c1cffb3b461f5aa12df1a39761d8f4a1d6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5147709
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1241378}

--- chrome/test/data/webui/settings/search_engine_entry_test.ts
+++ chrome/test/data/webui/settings/search_engine_entry_test.ts
@@ -44,7 +44,7 @@ suite('SearchEngineEntryTest', function() {
         entry.shadowRoot!.querySelector('#shortcut-column')!.textContent);
     assertEquals(
         searchEngine.url,
-        entry.shadowRoot!.querySelector('#url-column-padded')!.textContent);
+        entry.shadowRoot!.querySelector('#url-column')!.textContent);
   });
 
   // Tests that columns are hidden and shown appropriately.
@@ -63,12 +63,10 @@ suite('SearchEngineEntryTest', function() {
     // Test query URL column visibility.
     entry.showQueryUrl = true;
     assertFalse(
-        entry.shadowRoot!.querySelector<HTMLElement>(
-                             '#url-column-padded')!.hidden);
+        entry.shadowRoot!.querySelector<HTMLElement>('#url-column')!.hidden);
     entry.showQueryUrl = false;
     assertTrue(
-        entry.shadowRoot!.querySelector<HTMLElement>(
-                             '#url-column-padded')!.hidden);
+        entry.shadowRoot!.querySelector<HTMLElement>('#url-column')!.hidden);
   });
 
   // Open and return the action menu
@@ -290,7 +288,7 @@ suite('EnterpriseSiteSearchEntryTests', function() {
         entry.shadowRoot!.querySelector('#shortcut-column')!.textContent);
     assertEquals(
         searchEngine.url,
-        entry.shadowRoot!.querySelector('#url-column-padded')!.textContent);
+        entry.shadowRoot!.querySelector('#url-column')!.textContent);
   });
 
   // Verifies that the "edit" and "activate" buttons and the 3-dot action menu
