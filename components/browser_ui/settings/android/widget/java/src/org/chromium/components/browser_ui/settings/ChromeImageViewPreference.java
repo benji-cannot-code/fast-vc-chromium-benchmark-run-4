7b90ffe89da41a7167330f1d82756d3033d34583
Moha <arabm@google.com>
2023-12-28 21:29:51 +0000

    Set "hidden" button clickable to false
    This is to avoid talkbalk mistakenly reading the otherwise hidden
    image button.
    
    Bug: 1512905
    Change-Id: Ice8668bc0b853b08e7212f152dc6523ef08d64af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5148819
    Commit-Queue: Andrey Zaytsev <andzaytsev@google.com>
    Auto-Submit: Mohamad Arab <arabm@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1241414}

--- components/browser_ui/settings/android/widget/java/src/org/chromium/components/browser_ui/settings/ChromeImageViewPreference.java
+++ components/browser_ui/settings/android/widget/java/src/org/chromium/components/browser_ui/settings/ChromeImageViewPreference.java
@@ -217,6 +217,7 @@ public class ChromeImageViewPreference extends Preference {
             mButton.setPadding(0, 0, 0, 0);
             mButton.setOnClickListener(null);
             mButton.setContentDescription(null);
+            mButton.setClickable(false);
             return;
         }
 
