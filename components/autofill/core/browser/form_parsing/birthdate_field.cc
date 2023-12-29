e7391cc3ad64885b635119e5d6e0e8640e0db98e
Christoph Schwering <schwering@google.com>
2023-12-29 15:19:52 +0000

    [Autofill] Replace MatchFieldType constants with FormControlType
    This CL eliminates the temporary aliases of the MatchFieldType
    constants. The CL is produced by the following text replacement:
    
    - sed 's/MatchFieldType::kTextArea\>/FormControlType::kTextArea/g'
    - sed 's/MatchFieldType::kText\>/FormControlType::kInputText/g'
    - sed 's/MatchFieldType::kSearch\>/FormControlType::kInputSearch/g'
    - sed 's/MatchFieldType::kNumber\>/FormControlType::kInputNumber/g'
    - sed 's/MatchFieldType::kTelephone\>/FormControlType::kInputTelephone/g'
    - sed 's/MatchFieldType::kPassword\>/FormControlType::kInputPassword/g'
    - sed 's/MatchFieldType::kEmail\>/FormControlType::kInputEmail/g'
    - sed 's/MatchFieldType::kSelectOne\>/FormControlType::kSelectOne/g'
    - sed 's/MatchFieldType::kSelectList\>/FormControlType::kSelectList/g'
    
    Bug: 1514693
    Change-Id: I014c8ffea09101ba16e5242981f3fe55a018afd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5148821
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1241515}

--- components/autofill/core/browser/form_parsing/birthdate_field.cc
+++ components/autofill/core/browser/form_parsing/birthdate_field.cc
@@ -63,7 +63,7 @@ bool BirthdateField::IsSelectWithIncreasingValues(AutofillScanner* scanner,
   AutofillField* field = scanner->Cursor();
   if (!MatchesFormControlType(
           field->form_control_type,
-          {MatchFieldType::kSelectOne, MatchFieldType::kSelectList})) {
+          {FormControlType::kSelectOne, FormControlType::kSelectList})) {
     return false;
   }
   auto options = field->options;
@@ -90,7 +90,7 @@ bool BirthdateField::IsLikelyBirthdateYearSelectField(
   AutofillField* field = scanner->Cursor();
   if (!MatchesFormControlType(
           field->form_control_type,
-          {MatchFieldType::kSelectOne, MatchFieldType::kSelectList})) {
+          {FormControlType::kSelectOne, FormControlType::kSelectList})) {
     return false;
   }
   auto options = field->options;
