d3149374172b2b76ce63aefbaafbf0534035934f
Guoxing Zhao <charleszhao@chromium.org>
2023-12-29 00:33:54 +0000

    [VC] add more functions for GetBackgroundImageInfo.
    (1) Add callbacks for Set/Get background images.
    
    (2) Add GetBackgroundImageFileNames and GetBackgroundImageInfo which
        are splitted from GetRecentlyUsedBackgroundImages, because in
        webui we want to be able to load images on request. This is also
        more consistent with wallpaper logic.
    
    (3) Tests are added accordingly.
    
    Bug=b/308498859
    TEST="autoninja -C  out/Test chrome/test:ash_unittests && out/Test/ash_unittests --gtest_filter=CameraEffectsControllerTest.*"
    
    Change-Id: I7ba522f51149c9d5071582b01f10b25fc8d0529a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5143227
    Commit-Queue: Charles Zhao <charleszhao@chromium.org>
    Reviewed-by: CJ Huang <chenjih@google.com>
    Cr-Commit-Position: refs/heads/main@{#1241439}

--- ash/system/video_conference/bubble/set_camera_background_view.cc
+++ ash/system/video_conference/bubble/set_camera_background_view.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/camera/camera_effects_controller.h"
 #include "ash/system/video_conference/bubble/bubble_view.h"
 #include "ash/system/video_conference/bubble/bubble_view_ids.h"
+#include "base/functional/callback_helpers.h"
 #include "base/memory/weak_ptr.h"
 #include "skia/ext/image_operations.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -113,7 +114,7 @@ class RecentlyUsedImageButton : public views::ImageButton {
   METADATA_HEADER(RecentlyUsedImageButton, views::ImageButton)
 
  public:
-  RecentlyUsedImageButton(const std::string& filename,
+  RecentlyUsedImageButton(const base::FilePath& filename,
                           const std::string& jpeg_bytes,
                           const int expected_width)
       : ImageButton(
@@ -140,7 +141,8 @@ class RecentlyUsedImageButton : public views::ImageButton {
 
   // Apply background replace when the button is clicked on.
   void OnButtonClicked(const ui::Event& event) {
-    GetCameraEffectsController()->SetBackgroundImage(filename_);
+    GetCameraEffectsController()->SetBackgroundImage(filename_,
+                                                     base::DoNothing());
   }
 
  private:
