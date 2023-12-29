7b7495098148a5015619e6ce1bcb35eac55d8378
Christoph Schwering <schwering@google.com>
2023-12-29 16:10:59 +0000

    [Autofill] Remove AwAutofillClient::GetAutocompleteHistoryManager()
    Since Android O, Autofill WebView does not use
    AutocompleteHistoryManager anymore.
    
    Bug: 1501199
    Change-Id: I031471373188ebc9a9f410c17006cc61d74e709b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5149777
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1241522}

--- android_webview/browser/aw_autofill_client.cc
+++ android_webview/browser/aw_autofill_client.cc
@@ -93,8 +93,7 @@ autofill::PersonalDataManager* AwAutofillClient::GetPersonalDataManager() {
 
 autofill::AutocompleteHistoryManager*
 AwAutofillClient::GetAutocompleteHistoryManager() {
-  return AwBrowserContext::FromWebContents(&GetWebContents())
-      ->GetAutocompleteHistoryManager();
+  NOTREACHED_NORETURN();
 }
 
 PrefService* AwAutofillClient::GetPrefs() {
--- android_webview/browser/aw_browser_context.h
+++ android_webview/browser/aw_browser_context.h
@@ -102,7 +102,6 @@ class AwBrowserContext : public content::BrowserContext,
   void SetWebLayerRunningInSameProcess(JNIEnv* env);
 
   AwFormDatabaseService* GetFormDatabaseService();
-  autofill::AutocompleteHistoryManager* GetAutocompleteHistoryManager();
   CookieManager* GetCookieManager();
 
   bool IsDefaultBrowserContext() const;
