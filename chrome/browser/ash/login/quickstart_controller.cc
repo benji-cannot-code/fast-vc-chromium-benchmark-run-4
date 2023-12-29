da14f21b5024f17efe557047d482c91c83c2a18e
Renato Silva <rrsilva@google.com>
2023-12-29 13:56:22 +0000

    [QuickStart] Add Signing in UI
    Adds the 'Signing in...' UI in QuickStart that can either show only
    the user's email, or the user's full name and avatar. A new UI state
    for account creation keeps the cancel button hidden once account
    creation starts.
    
    Also remove legacy steps that were being used for showing the WiFi
    and FIDO credentials when they were being received.
    
    Screenshots:
    Email only - https://screenshot.googleplex.com/5cNn2iVhuGJajZd
    Full avatar - https://screenshot.googleplex.com/3XcmWw73yNzNPT2
    
    Test: Manual on Pixelbook & Pixel7
    Bug: b/283724988
    Change-Id: If3143efc4f98f5b81698d73a7eae710d0bf91129
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5136335
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Cr-Commit-Position: refs/heads/main@{#1241502}

--- chrome/browser/ash/login/quickstart_controller.cc
+++ chrome/browser/ash/login/quickstart_controller.cc
@@ -251,7 +251,7 @@ void QuickStartController::OnStatusChanged(
     case Step::REQUESTING_GOOGLE_ACCOUNT_INFO:
       return;
     case Step::GOOGLE_ACCOUNT_INFO_RECEIVED:
-      UpdateUiState(UiState::TRANSFERRING_GAIA_CREDENTIALS);
+      UpdateUiState(UiState::SIGNING_IN);
       bootstrap_controller_->AttemptGoogleAccountTransfer();
       return;
     case Step::TRANSFERRING_GOOGLE_ACCOUNT_DETAILS:
@@ -264,8 +264,8 @@ void QuickStartController::OnStatusChanged(
       CHECK(controller_state_ == ControllerState::CONNECTED);
       if (absl::holds_alternative<FidoAssertionInfo>(status.payload)) {
         QS_LOG(INFO) << "Successfully received FIDO assertion.";
-        fido_ = absl::get<FidoAssertionInfo>(status.payload);
-        UpdateUiState(UiState::SHOWING_FIDO);
+        // TODO(b/283724988) - Update with authorization code.
+        UpdateUiState(UiState::SIGNING_IN);
         SavePhoneInstanceID();
       } else {
         CHECK(absl::holds_alternative<ErrorCode>(status.payload));
@@ -401,7 +401,7 @@ void QuickStartController::ResetState() {
   entry_point_.reset();
   qr_code_data_.reset();
   pin_.reset();
-  fido_.reset();
+  user_info_ = UserInfo();
   wifi_name_.reset();
   controller_state_ = ControllerState::NOT_ACTIVE;
   ui_state_.reset();
--- chrome/browser/resources/chromeos/login/debug/debug.js
+++ chrome/browser/resources/chromeos/login/debug/debug.js
@@ -1842,7 +1842,7 @@ const createAssistantZippy = (type, isMinor, isNativeIcons) => {
       id: 'quick-start',
       kind: ScreenKind.NORMAL,
       handledSteps:
-          'verification,connecting_to_wifi,gaia_credentials,fido_assertion_received',
+          'verification,connecting_to_wifi,signing_in',
       states: [
         {
           id: 'PinVerification',
@@ -1864,15 +1864,19 @@ const createAssistantZippy = (type, isMinor, isNativeIcons) => {
           },
         },
         {
-          id: 'TransferringGaiaCreds',
+          id: 'SigninInEmailOnly',
           trigger: (screen) => {
-            screen.showTransferringGaiaCredentials();
+            screen.showSigningInStep();
+            screen.setUserEmail('test_email@gmail.com');
           },
         },
         {
-          id: 'TransferredGaiaCreds',
+          id: 'SigninInFullAvatar',
           trigger: (screen) => {
-            screen.showFidoAssertionReceived('testUser@gmail.com');
+            screen.showSigningInStep();
+            screen.setUserEmail('test_email@gmail.com');
+            screen.setUserFullName('Test User');
+            screen.setUserAvatarUrl('https://lh3.googleusercontent.com/a/ACg8ocISjvU-p0Gz_kIBamP3jit_Y8PrQVU4AbIvQrUEZ04d=s96-c');
           },
         },
       ],
