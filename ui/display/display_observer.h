ae8d98d43dfe426a5bd6a58bda9273a6f53ebcec
Lukasz Anforowicz <lukasza@chromium.org>
2024-01-02 18:27:16 +0000

    Manual raw_ptr rewrite: //ui/display/display_observer.h
    Bug: 1446392
    Change-Id: Ie9e99d310d8466d5193c02b633c71c6685310024
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5155669
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242031}

--- ui/display/display_observer.h
+++ ui/display/display_observer.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
-#include "base/memory/raw_ptr_exclusion.h"
+#include "base/memory/raw_ptr.h"
 #include "base/observer_list_types.h"
 #include "ui/display/display_export.h"
 
@@ -79,9 +79,7 @@ class DISPLAY_EXPORT ScopedOptionalDisplayObserver {
   ~ScopedOptionalDisplayObserver();
 
  private:
-  // This field is not a raw_ptr<> because it was filtered by the rewriter for:
-  // #union
-  RAW_PTR_EXCLUSION DisplayObserver* observer_ = nullptr;
+  raw_ptr<DisplayObserver> observer_ = nullptr;
 };
 
 class DISPLAY_EXPORT ScopedDisplayObserver
