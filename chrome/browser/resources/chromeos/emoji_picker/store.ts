d7b11b61c312e09d3299b9514df8d9594ca73877
Grey Wang <greywang@google.com>
2024-01-02 01:04:34 +0000

    [Emoji Picker] Fix RecentlyUsedStore.savePreferredVariant()
    When default variant emoji is inserted, saveReferredVariant() will be invoked with `baseEmoji == undefined`; here we fix the logic to handle it properly.
    
    Bug: b/318025762
    Change-Id: I38483551514bb72f56a5cfeddda68897dd94caf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5157010
    Commit-Queue: Grey Wang <greywang@google.com>
    Reviewed-by: Zac Partridge <zacpartridge@google.com>
    Cr-Commit-Position: refs/heads/main@{#1241795}

--- chrome/browser/resources/chromeos/emoji_picker/store.ts
+++ chrome/browser/resources/chromeos/emoji_picker/store.ts
@@ -75,8 +75,9 @@ export class RecentlyUsedStore {
    *    otherwise.
    */
   savePreferredVariant(variant: string, baseEmoji?: string) {
+    // If `baseEmoji == undefined`, then variant itself is a base emoji.
     if (!baseEmoji) {
-      return false;
+      baseEmoji = variant;
     }
 
     const preference = this.store.data.preference;
