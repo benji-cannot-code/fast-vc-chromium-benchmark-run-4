3084e48ca7164b6a7546f5188c3f4d992ebe3992
Sonja <laurila@google.com>
2024-01-02 11:25:20 +0000

    [AWC] Prompt window-management permission for window.setResizable API
    This CL adds prompting for the necessary permission if the
    window.setResizable(bool) API is called without the permission being
    granted before-hand.
    
    Bug: 1507772, 1466855
    Change-Id: Ide5afee0df1e4ab8a2ed65c2a45c9c8ea26dd958
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5119680
    Auto-Submit: Sonja Laurila <laurila@google.com>
    Commit-Queue: Sonja Laurila <laurila@google.com>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Christian Flach <cmfcmf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1241861}

--- third_party/blink/renderer/modules/awc/additional_windowing_controls.h
+++ third_party/blink/renderer/modules/awc/additional_windowing_controls.h
@@ -30,10 +30,10 @@ class AdditionalWindowingControls {
   static ScriptPromise restore(ScriptState*,
                                LocalDOMWindow&,
                                ExceptionState& exception_state);
-  static void setResizable(ScriptState*,
-                           LocalDOMWindow&,
-                           bool resizable,
-                           ExceptionState& exception_state);
+  static ScriptPromise setResizable(ScriptState*,
+                                    LocalDOMWindow&,
+                                    bool resizable,
+                                    ExceptionState& exception_state);
 };
 
 }  // namespace blink
