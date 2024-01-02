cd1446a965fad808543ae704a45e537f9f06ec6f
Negeen Daudi <negeend@google.com>
2024-01-02 00:08:59 +0000

    Fix GetWordSimilarity to use cosine similarity.
    Test: out/Default/unit_tests --gtest_filter="*ManateeTest*"
    Bug: b/314868265
    Change-Id: Ib797137e3debd7e79ef995bd6f50729cb005f190
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5150539
    Reviewed-by: Amanda Deacon <amandadeacon@chromium.org>
    Reviewed-by: CJ Huang <chenjih@google.com>
    Commit-Queue: Negeen Daudi <negeend@google.com>
    Cr-Commit-Position: refs/heads/main@{#1241786}

--- chrome/browser/ash/app_list/search/util/manatee.h
+++ chrome/browser/ash/app_list/search/util/manatee.h
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace app_list {
 
+// Returns cosine similarity between two vectors. It requires that both
+// vectors be the same size, and returns std::nullopt if not. Values can
+// range between [-1, 1] where a higher value indicates a greater
+// similarity.
 std::optional<double> GetWordSimilarity(const std::vector<double>& vector1,
                                         const std::vector<double>& vector2);
 
