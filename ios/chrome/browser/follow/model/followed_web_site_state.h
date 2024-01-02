a736f1549f1aa1700a13bd2794a82d5ca4b09a73
Vincent Boisselle <vincb@google.com>
2024-01-02 17:17:24 +0000

    [Code Health][iOS] Move ios/c/b/follow to model subfolder
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/<foo>/ will be moved to
    ios/c/b/<foo>/model/ to showcase that they are model files.
    
    This is the last part of the chain of CLs for the model/ migration:
      (DONE) #1 Migrate downstream and use forwarder where needed
             (http://crrev.com/c/5059496)
      (DONE) #2 Use model/ in internal: crrev.com/i/6706632
             (https://crrev.com/i/6082837)
    THIS >>#3 Finish the migration of the not yet migrated dependencies. <<
    
    
    
    Bug: 1319852
    Change-Id: Ic2b78fdb98369d9f3269b3c7626dba9375b95adf
    Low-Coverage-Reason: Code migration
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5120315
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1241986}

--- ios/chrome/browser/follow/model/followed_web_site_state.h
+++ ios/chrome/browser/follow/model/followed_web_site_state.h
@@ -3,9 +3,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_FOLLOW_MODEL_FOLLOWED_WEB_SITE_STATE_H_
-#define IOS_CHROME_BROWSER_FOLLOW_MODEL_FOLLOWED_WEB_SITE_STATE_H_
+#ifndef IOS_CHROME_BROWSER_FOLLOW_FOLLOWED_WEB_SITE_STATE_H_
+#define IOS_CHROME_BROWSER_FOLLOW_FOLLOWED_WEB_SITE_STATE_H_
 
-#import "ios/chrome/browser/follow/followed_web_site_state.h"
+#import <Foundation/Foundation.h>
 
-#endif  // IOS_CHROME_BROWSER_FOLLOW_MODEL_FOLLOWED_WEBSITE_STATE_H_
+// Enum representing the followed website states.
+typedef NS_ENUM(NSInteger, FollowedWebSiteState) {
+  FollowedWebSiteStateStateUnknown = 0,
+  FollowedWebSiteStateStateInactive,
+  FollowedWebSiteStateStateActive,
+  FollowedWebSiteStateStateAwaitingContent,
+};
+
+#endif  // IOS_CHROME_BROWSER_FOLLOW_FOLLOWED_WEBSITE_STATE_H_
