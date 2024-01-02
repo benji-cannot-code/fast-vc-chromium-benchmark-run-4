44fe3d2e5ffd368571c41161f78ca8cd3f887842
Maggie Chen <magchen@chromium.org>
2024-01-02 23:36:21 +0000

    Fix DCHECK for SharedBitmap to ShareImage conversion
    Fix DCHECK in DisplayResourceProviderSoftware::LockForRead and in
    ClientResourceProvider::ReceiveReturnsFromParent when
    features::kSharedBitmapToSharedImage is enabled.
    
    Bug:1434885
    
    Change-Id: I88bb3cc579d0e83e8561021f352964db200c85cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5160613
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242192}

--- components/viz/service/display/display_resource_provider_software.cc
+++ components/viz/service/display/display_resource_provider_software.cc
@@ -31,8 +31,6 @@ DisplayResourceProviderSoftware::DisplayResourceProviderSoftware(
               ? sync_point_manager_->CreateSyncPointOrderData()
               : nullptr) {
   DCHECK(shared_bitmap_manager);
-  DCHECK((shared_image_manager && sync_point_manager) ||
-         !base::FeatureList::IsEnabled(features::kSharedBitmapToSharedImage));
 
   memory_tracker_ = std::make_unique<gpu::MemoryTypeTracker>(nullptr);
 }
@@ -49,8 +47,8 @@ DisplayResourceProviderSoftware::LockForRead(ResourceId id) {
   ChildResource* resource = GetResource(id);
   DCHECK(!resource->is_gpu_resource_type());
 
-  if (shared_image_manager_ &&
-      resource->transferable.mailbox_holder.mailbox.IsSharedImage()) {
+  if (resource->transferable.mailbox_holder.mailbox.IsSharedImage()) {
+    DCHECK(shared_image_manager_ && sync_point_manager_);
     auto it = resource_shared_images_.find(id);
     if (it == resource_shared_images_.end()) {
       const SharedBitmapId& shared_bitmap_id =
