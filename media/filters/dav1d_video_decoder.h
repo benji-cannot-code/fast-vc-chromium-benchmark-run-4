5ba44e04514bb9576fdc1b60c615fe8c874364e1
Lukasz Anforowicz <lukasza@chromium.org>
2024-01-02 21:31:33 +0000

    Change `Dav1dVideoDecoder::dav1d_decoder_` into `std::unique_ptr`.
    Before this CL the `dav1d_decoder_` field was a native C++ pointer
    (skipped by the automatic rewrite into `raw_ptr<T>` because of a
    technicality).  Instead of manually rewriting the pointer into
    `raw_ptr<T>` this CL changes the pointer into `std::unique_ptr`
    with a custom deleter - this follows the advice from
    https://chromium.googlesource.com/chromium/src/+/main/docs/dangling_ptr_guide.md#object-vended-from-c-api
    
    Bug: 1446392
    Change-Id: I18aa3e44e9e9a350b70309d18db4e962f771e071
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5153984
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242141}

--- media/filters/dav1d_video_decoder.h
+++ media/filters/dav1d_video_decoder.h
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/functional/callback_forward.h"
-#include "base/memory/raw_ptr.h"
-#include "base/memory/raw_ptr_exclusion.h"
 #include "base/memory/ref_counted_memory.h"
 #include "base/sequence_checker.h"
 #include "media/base/media_log.h"
@@ -94,9 +92,10 @@ class MEDIA_EXPORT Dav1dVideoDecoder : public OffloadableVideoDecoder {
 
   // The allocated decoder; null before Initialize() and anytime after
   // CloseDecoder().
-  // This field is not a raw_ptr<> because it was filtered by the rewriter for:
-  // #addr-of
-  RAW_PTR_EXCLUSION Dav1dContext* dav1d_decoder_ = nullptr;
+  struct Dav1dContextDeleter {
+    void operator()(Dav1dContext* ptr);
+  };
+  std::unique_ptr<Dav1dContext, Dav1dContextDeleter> dav1d_decoder_;
 };
 
 // Helper class for creating a Dav1dVideoDecoder which will offload all AV1
