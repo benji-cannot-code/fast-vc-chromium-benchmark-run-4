1c87719e88a6e38f8e52aae2b06db532f9006ec7
Andy Phan <andyphan@chromium.org>
2024-01-02 18:02:28 +0000

    [CodeHealth] Remove expired histogram SSL.NonWebbyMixedContentLoaded
    This histogram expired 3 years ago.
    
    Bug: 1508023
    Change-Id: I35c4472dd034ac2b9508cbf9827658e3d97b4ed8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5151928
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242010}

--- content/browser/renderer_host/mixed_content_checker.cc
+++ content/browser/renderer_host/mixed_content_checker.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/memory/ptr_util.h"
 #include "base/metrics/field_trial_params.h"
-#include "base/metrics/histogram_macros.h"
 #include "content/browser/preloading/prerender/prerender_final_status.h"
 #include "content/browser/preloading/prerender/prerender_metrics.h"
 #include "content/browser/renderer_host/frame_tree.h"
@@ -264,12 +263,6 @@ bool MixedContentChecker::ShouldBlockInternal(
   // TODO(https://crbug.com/1030307): decide whether CORS-enabled is really the
   // right way to draw this distinction.
   if (!ShouldTreatURLSchemeAsCorsEnabled(url)) {
-    // Record non-webby mixed content to see if it is rare enough that it can be
-    // gated behind an enterprise policy. This excludes URLs that are considered
-    // potentially-secure such as blob: and filesystem:, which are special-cased
-    // in `IsUrlPotentiallySecure()` and cause an early-return because of the
-    // `InWhichFrameIsContentMixed()` check above.
-    UMA_HISTOGRAM_BOOLEAN("SSL.NonWebbyMixedContentLoaded", true);
     return false;
   }
 
