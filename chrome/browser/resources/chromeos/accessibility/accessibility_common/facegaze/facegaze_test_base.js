a311021974dc7df8137c3e6c7ccd7e9920601cfa
Akihiro Ota <akihiroota@chromium.org>
2024-01-02 16:15:33 +0000

    FaceGaze: Split mouse and gesture logic into dedicated classes
    This change moves all mouse-related logic into MouseController and all
    gesture-related logic into GestureDetector. This change is a pure
    refactor and does not change any feature logic.
    
    Bug: b:309121742
    Change-Id: I25dbcf7a60ff03f35d67f258443799257a27332f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5141926
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1241944}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/facegaze_test_base.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/facegaze_test_base.js
@@ -59,8 +59,10 @@ FaceGazeTestBase = class extends E2ETestBase {
     const module =
         await import('/accessibility_common/accessibility_common_loader.js');
     await importModule(
-        ['Action', 'FaceGaze', 'FacialGesture'],
-        '/accessibility_common/facegaze/facegaze.js');
+        ['Action', 'FaceGaze'], '/accessibility_common/facegaze/facegaze.js');
+    await importModule(
+        ['FacialGesture'],
+        '/accessibility_common/facegaze/gesture_detector.js');
     accessibilityCommon = new module.AccessibilityCommon();
     assertNotNullNorUndefined(accessibilityCommon);
     assertNotNullNorUndefined(Action);
@@ -100,15 +102,10 @@ FaceGazeTestBase = class extends E2ETestBase {
 
   /**
    * @param {!FacialGesture} gesture
-   * @return {?Action}
+   * @return {Action|undefined}
    */
   getActionForGesture(gesture) {
-    const data = this.getFaceGaze().gestureToActionData_.get(gesture);
-    if (!data) {
-      return null;
-    }
-
-    return data.action;
+    return this.getFaceGaze().gestureToAction_.get(gesture);
   }
 
   /**
@@ -116,7 +113,7 @@ FaceGazeTestBase = class extends E2ETestBase {
    * @param {!chrome.accessibilityPrivate.ScreenPoint} location
    */
   setMouseLocation(location) {
-    this.getFaceGaze().mouseLocation_ = location;
+    this.getFaceGaze().mouseController_.mouseLocation_ = location;
   }
 
   /** @param {!MockFaceLandmarkerResult} result */
