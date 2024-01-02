15eca3202ec01f48f635b75bec20f97e803d29e7
Brian Ryner <bryner@google.com>
2024-01-02 23:48:54 +0000

    Fix restore of os.environ to work as intended.
    Assigning an object equal to os.environ continues to reference the
    same object. Make a copy and restore from that instead. Note that
    we cannot simply assign the copy back to os.environ, as that would
    replace the specialized implementation of os.environ with a standard
    dict object.
    
    Bug: 1511316
    Change-Id: I12577332830330c5763c7462fa14044636669ce7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5135776
    Auto-Submit: Brian Ryner <bryner@google.com>
    Commit-Queue: Brian Ryner <bryner@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242198}

--- tools/clang/scripts/build.py
+++ tools/clang/scripts/build.py
@@ -87,7 +87,7 @@ def GetWinSDKDir():
     return win_sdk_dir
 
   # Don't let vs_toolchain overwrite our environment.
-  environ_bak = os.environ
+  environ_bak = dict(os.environ)
 
   sys.path.append(os.path.join(CHROMIUM_DIR, 'build'))
   import vs_toolchain
@@ -103,7 +103,8 @@ def GetWinSDKDir():
       vs_path = os.environ['GYP_MSVS_OVERRIDE_PATH']
     dia_path = os.path.join(vs_path, 'DIA SDK', 'bin', 'amd64')
 
-  os.environ = environ_bak
+  os.environ.clear()
+  os.environ.update(environ_bak)
   return win_sdk_dir
 
 
