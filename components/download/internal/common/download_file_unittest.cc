e9358e447de810225b2cf52592282a017ae7bd75
Lukasz Anforowicz <lukasza@chromium.org>
2024-01-02 21:21:44 +0000

    Manual raw_ptr rewrite: //components/.../download_file_unittest.cc
    Bug: 1446392
    Change-Id: I084b4ca68cbbb7dc8c3cb7b5629fb539872d168f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5154050
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242136}

--- components/download/internal/common/download_file_unittest.cc
+++ components/download/internal/common/download_file_unittest.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "base/functional/bind.h"
 #include "base/location.h"
-#include "base/memory/raw_ptr_exclusion.h"
+#include "base/memory/raw_ptr.h"
 #include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/task/sequenced_task_runner.h"
@@ -156,8 +156,8 @@ class DownloadFileTest : public testing::Test {
       : observer_(new StrictMock<MockDownloadDestinationObserver>),
         observer_factory_(observer_.get()),
         input_stream_(nullptr),
-        additional_streams_(
-            std::vector<StrictMock<MockInputStream>*>{nullptr, nullptr}),
+        additional_streams_(std::vector<raw_ptr<StrictMock<MockInputStream>>>{
+            nullptr, nullptr}),
         bytes_(-1),
         bytes_per_sec_(-1) {}
 
@@ -292,6 +292,11 @@ class DownloadFileTest : public testing::Test {
           base::ReadFileToString(download_file_->FullPath(), &disk_data));
       EXPECT_EQ(expected_data_, disk_data);
     }
+
+    // Clear `raw_ptr`s before they become dangling pointers after resetting
+    // `download_file_` below.
+    input_stream_ = nullptr;
+    additional_streams_.clear();
 
     // Make sure the Browser and File threads outlive the DownloadFile
     // to satisfy thread checks inside it.
@@ -438,7 +443,7 @@ class DownloadFileTest : public testing::Test {
   }
 
   // Prepare a byte stream to write to the file sink.
-  void PrepareStream(StrictMock<MockInputStream>** stream,
+  void PrepareStream(raw_ptr<StrictMock<MockInputStream>>* stream,
                      int64_t offset,
                      bool create_stream,
                      bool will_finish,
@@ -499,12 +504,10 @@ class DownloadFileTest : public testing::Test {
 
   // Stream for sending data into the download file.
   // Owned by download_file_; will be alive for lifetime of download_file_.
-  // This field is not a raw_ptr<> because it was filtered by the rewriter for:
-  // #addr-of
-  RAW_PTR_EXCLUSION StrictMock<MockInputStream>* input_stream_;
+  raw_ptr<StrictMock<MockInputStream>> input_stream_;
 
   // Additional streams to test multiple stream write.
-  std::vector<StrictMock<MockInputStream>*> additional_streams_;
+  std::vector<raw_ptr<StrictMock<MockInputStream>>> additional_streams_;
 
   // Sink callback data for stream.
   mojo::SimpleWatcher::ReadyCallback sink_callback_;
@@ -1145,10 +1148,13 @@ TEST_F(DownloadFileTest, MultipleStreamsFirstStreamWriteAllData) {
   // called.
   EXPECT_FALSE(download_file_->InProgress());
 
-  additional_streams_[0] = new StrictMock<MockInputStream>();
+  // Clear `raw_ptr`s before they become dangling pointers after the
+  // `AddInputStream` call below.
+  input_stream_ = nullptr;
+  additional_streams_.clear();
+
   download_file_->AddInputStream(
-      std::unique_ptr<MockInputStream>(additional_streams_[0]),
-      stream_0_length - 1);
+      std::make_unique<StrictMock<MockInputStream>>(), stream_0_length - 1);
   base::RunLoop().RunUntilIdle();
 
   SourceStreamTestData stream_data_0(0, stream_0_length, true);
