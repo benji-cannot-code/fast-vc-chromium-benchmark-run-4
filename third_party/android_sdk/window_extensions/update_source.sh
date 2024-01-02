faa9a208788307238b9aa6213d1a5a992328f9e7
Andrew Grieve <agrieve@chromium.org>
2024-01-02 18:10:57 +0000

    Android: Make androidx.window.extensions library available
    This does not yet add it to chrome.
    
    Bug: 1415351
    Change-Id: Ie4075e9e0f9ffc7d89fe60950c01b45b69552f46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5148729
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242020}

---
+++ third_party/android_sdk/window_extensions/update_source.sh
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+
+set -e
+cd $(dirname $0)
+
+# Clone androidx repo.
+rm -rf temp-git-clone
+git clone --depth 1 https://android.googlesource.com/platform/frameworks/support temp-git-clone
+
+# Update sources.txt
+cd temp-git-clone
+find window/extensions -path "*/main/*.java" | sort > ../sources.txt
+cd ..
+
+# Copy the .java files.
+rm -r java
+for path in $(cat sources.txt); do
+  mkdir -p java/$(dirname "$path")
+  cp "temp-git-clone/$path" "java/$path"
+done
+
+rm -rf temp-git-clone
+
+echo "Job's Done."
---
+++ third_party/androidx/local_modifications/window/java/org/chromium/window/WindowUtil.java
@@ -0,0 +1,52 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+package org.chromium.window;
+
+import android.content.Context;
+
+import androidx.window.extensions.WindowExtensionsProvider;
+import androidx.window.extensions.core.util.function.Consumer;
+import androidx.window.extensions.layout.WindowLayoutComponent;
+import androidx.window.extensions.layout.WindowLayoutInfo;
+
+/**
+ * Helpers for using androidx.window.extensions APIs.
+ *
+ * <p>For documentation, see:
+ * https://android.googlesource.com/platform/frameworks/support/+/androidx-main/window/extensions/extensions/src/main/java/androidx/window/extensions/layout/WindowLayoutComponent.java
+ */
+public class WindowUtil {
+    private static final WindowLayoutComponent sWindowLayoutComponent;
+
+    static {
+        WindowLayoutComponent value = null;
+        try {
+            var extensions = WindowExtensionsProvider.getWindowExtensions();
+            // Level 2 provides addWindowLayoutInfoListener(Context, Consumer).
+            // See comments at https://cs.android.com/search?q=symbol:VENDOR_API_LEVEL_2
+            if (extensions.getVendorApiLevel() >= 2) {
+                value = extensions.getWindowLayoutComponent();
+            }
+        } catch (Throwable t) {
+        }
+        sWindowLayoutComponent = value;
+    }
+
+    private WindowUtil() {}
+
+    /** Returns whether the required system library is loaded. */
+    public static boolean isAvailable() {
+        return sWindowLayoutComponent != null;
+    }
+
+    public static void addWindowLayoutInfoListener(
+            Context context, Consumer<WindowLayoutInfo> consumer) {
+        sWindowLayoutComponent.addWindowLayoutInfoListener(context, consumer);
+    }
+
+    public static void removeWindowLayoutInfoListener(Consumer<WindowLayoutInfo> consumer) {
+        sWindowLayoutComponent.removeWindowLayoutInfoListener(consumer);
+    }
+}
