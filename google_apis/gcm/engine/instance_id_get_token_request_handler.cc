cd95e09ed915180529c7d82b88709e29b51f9b71
Ari Chivukula <arichiv@chromium.org>
2024-01-02 11:34:36 +0000

    Remove expired histogram InstanceID.GetToken.NetErrorCode
    The following histogram was marked as expired:
      - InstanceID.GetToken.NetErrorCode (expired as of M94)
    
    Let's clean it up.
    
    OBSOLETE_HISTOGRAMS=Expired as of M94 and removed as of 2023-12-18.
    
    Bug: 1508014
    Change-Id: I6a2fd59b68b6533ed95803c538b770e18b29d933
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5131146
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1241864}

--- google_apis/gcm/engine/instance_id_get_token_request_handler.cc
+++ google_apis/gcm/engine/instance_id_get_token_request_handler.cc
@@ -71,10 +71,4 @@ void InstanceIDGetTokenRequestHandler::ReportStatusToUMA(
   }
 }
 
-void InstanceIDGetTokenRequestHandler::ReportNetErrorCodeToUMA(
-    int net_error_code) {
-  base::UmaHistogramSparse("InstanceID.GetToken.NetErrorCode",
-                           std::abs(net_error_code));
-}
-
 }  // namespace gcm
