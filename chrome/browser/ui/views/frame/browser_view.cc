aa4e95753d97701ac9e28bdda0ff43263818f2a8
Sonja <laurila@google.com>
2024-01-02 19:01:14 +0000

    [AWC] Ensure updating titlebar when setResizable API is called
    Adds calling InvalidateLayout() when resizability has changed to
    reflect the changes there to for example show/hide the chrome's
    maximize button.
    
    Bug: 1466855, b/309956837
    Change-Id: I9ade02341821a963b907f0365b16c91f1a74d639
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5149635
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Auto-Submit: Sonja Laurila <laurila@google.com>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242058}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -2585,6 +2585,7 @@ void BrowserView::OnCanResizeFromWebAPIChanged() {
 
   cached_can_resize_from_web_api_ = can_resize;
   GetWidget()->OnSizeConstraintsChanged();
+  InvalidateLayout();  // To show/hide the maximize button.
 }
 
 void BrowserView::SynchronizeRenderWidgetHostVisualPropertiesForMainFrame() {
