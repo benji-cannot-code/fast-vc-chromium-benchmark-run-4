444837c665d02365241b6642af5e75c06c789713
Anna Tsvirchkova <atsvirchkova@google.com>
2024-01-02 10:24:55 +0000

    [Gardener] Fix failing TouchToFillCreditCardRenderTest
    Bug: 1515061
    Change-Id: I09714abfc01f53f78dff7baf3f26a66581e8fa64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5154249
    Owners-Override: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Auto-Submit: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1241848}

--- chrome/browser/touch_to_fill/autofill/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillCreditCardRenderTest.java
+++ chrome/browser/touch_to_fill/autofill/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillCreditCardRenderTest.java
@@ -34,7 +34,6 @@ import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
-import org.chromium.chrome.browser.autofill.AutofillTestHelper;
 import org.chromium.chrome.browser.autofill.PersonalDataManager.CreditCard;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.night_mode.ChromeNightModeTestUtils;
@@ -74,7 +73,7 @@ public class TouchToFillCreditCardRenderTest {
     @Rule
     public final ChromeRenderTestRule mRenderTestRule =
             ChromeRenderTestRule.Builder.withPublicCorpus()
-                    .setRevision(13)
+                    .setRevision(14)
                     .setBugComponent(Component.UI_BROWSER_AUTOFILL)
                     .build();
 
@@ -86,7 +85,7 @@ public class TouchToFillCreditCardRenderTest {
                     "Visa",
                     "4111111111111111",
                     "05",
-                    AutofillTestHelper.nextYear(),
+                    "2100",
                     true,
                     "Visa",
                     "• • • • 1111",
@@ -97,7 +96,7 @@ public class TouchToFillCreditCardRenderTest {
                     "MasterCard",
                     "5555555555554444",
                     "08",
-                    AutofillTestHelper.nextYear(),
+                    "2100",
                     true,
                     "Mastercard",
                     "• • • • 4444",
@@ -108,7 +107,7 @@ public class TouchToFillCreditCardRenderTest {
                     "MasterCard",
                     "5454545454545454",
                     "11",
-                    AutofillTestHelper.nextYear(),
+                    "2100",
                     false,
                     "MasterCard-GPay",
                     "• • • • 5454",
@@ -119,7 +118,7 @@ public class TouchToFillCreditCardRenderTest {
                     "Discover",
                     "6011111111111117",
                     "09",
-                    AutofillTestHelper.nextYear(),
+                    "2100",
                     true,
                     "Discover",
                     "• • • • 1117",
@@ -130,7 +129,7 @@ public class TouchToFillCreditCardRenderTest {
                     "American Express",
                     "378282246310005",
                     "10",
-                    AutofillTestHelper.nextYear(),
+                    "2100",
                     true,
                     "American Express",
                     "• • • • 0005",
@@ -141,7 +140,7 @@ public class TouchToFillCreditCardRenderTest {
                     /* name= */ "MasterCard-GPay",
                     /* number= */ "5454545454545454",
                     /* month= */ "11",
-                    /* year= */ AutofillTestHelper.nextYear(),
+                    /* year= */ "2100",
                     /* network= */ "Mastercard",
                     /* iconId= */ R.drawable.mc_metadata_card,
                     /* cardNameForAutofillDisplay= */ "MasterCard-GPay",
