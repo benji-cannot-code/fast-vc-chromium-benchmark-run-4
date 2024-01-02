023c7ffc5b1fc79a3b31dd5c9019fa9707969add
Ian Wells <iwells@chromium.org>
2024-01-02 21:30:55 +0000

    Create helper class for "sticky" synthetic field trials
    Given a feature flag overridden by an ordinary "base" field trial,
    readaloud::SyntheticTrial registers a synthetic trial based on the
    base trial and can reactivate it on subsequent startups if the base
    trial name hasn't changed.
    
    This will be useful for Read Aloud to register trials at specific times.
    The immediate use case is to register the trial only after an eligible
    page is loaded. This helper can allow other uses in the future, like
    registering a trial only if a page has highlight/autoscroll support, for
    example.
    
    This change extracts ScopedTestMetricsService from
    first_run_service_unittest.cc to be shared between tests
    that need to verify synthetic trial activation.
    
    Bug: b/316375886
    Change-Id: I5f54b00929421c18f4135146bed03213fd1ae35d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5123987
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242140}

--- chrome/browser/android/preferences/browser_prefs_android.cc
+++ chrome/browser/android/preferences/browser_prefs_android.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/preferences/clipboard_android.h"
 #include "chrome/browser/android/search_permissions/search_permissions_service.h"
 #include "chrome/browser/notifications/notification_platform_bridge_android.h"
+#include "chrome/browser/readaloud/android/prefs.h"
 #include "chrome/browser/webauthn/android/cable_module_android.h"
 #include "components/content_settings/core/common/pref_names.h"
 #include "components/pref_registry/pref_registry_syncable.h"
@@ -17,6 +18,7 @@ namespace android {
 
 void RegisterPrefs(PrefRegistrySimple* registry) {
   RegisterClipboardAndroidPrefs(registry);
+  readaloud::RegisterLocalPrefs(registry);
   webauthn::authenticator::RegisterLocalState(registry);
 }
 
--- chrome/browser/readaloud/android/prefs.cc
+++ chrome/browser/readaloud/android/prefs.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/android/pref_service_android.h"
+#include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
 #include "components/prefs/scoped_user_pref_update.h"
 
@@ -36,6 +37,10 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterBooleanPref(
       prefs::kListenToThisPageEnabled, true,
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
+}
+
+void RegisterLocalPrefs(PrefRegistrySimple* registry) {
+  registry->RegisterDictionaryPref(prefs::kReadAloudSyntheticTrials);
 }
 
 void JNI_ReadAloudPrefs_GetVoices(JNIEnv* env,
