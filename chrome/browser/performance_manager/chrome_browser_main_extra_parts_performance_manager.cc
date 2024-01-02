63eefb9940beca370ac6fccef27755961348e91c
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2024-01-02 16:01:02 +0000

    Clean up abandonned Probabilistic Memory Saver
    Bug: None
    Change-Id: I41d989c859b3308746bc3b400923a912bb27e009
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5140306
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1241930}

--- chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
+++ chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
@@ -73,7 +73,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/performance_manager/policies/memory_saver_mode_policy.h"
 #include "chrome/browser/performance_manager/policies/page_discarding_helper.h"
 #include "chrome/browser/performance_manager/policies/page_freezing_policy.h"
-#include "chrome/browser/performance_manager/policies/probabilistic_memory_saver_policy.h"
 #include "chrome/browser/performance_manager/policies/urgent_page_discarding_policy.h"
 #include "chrome/browser/performance_manager/public/user_tuning/battery_saver_mode_manager.h"
 #include "chrome/browser/performance_manager/public/user_tuning/performance_detection_manager.h"
@@ -201,14 +200,6 @@ void ChromeBrowserMainExtraPartsPerformanceManager::CreatePoliciesAndDecorators(
 
   graph->PassToGraph(
       std::make_unique<performance_manager::policies::MemorySaverModePolicy>());
-
-  if (base::FeatureList::IsEnabled(
-          performance_manager::features::kProbabilisticProactiveDiscarding) &&
-      performance_manager::features::kProactiveDiscardingSimulationMode.Get()) {
-    graph->PassToGraph(
-        std::make_unique<performance_manager::ProbabilisticMemorySaverPolicy>(
-            /*simulation_mode=*/true));
-  }
 #endif  // !BUILDFLAG(IS_ANDROID)
 
   graph->PassToGraph(
