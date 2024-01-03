94a08e3a2ee4bbe295e0f27651ebee7f6f05e6a2
Mark Polner <polner@google.com>
2024-01-03 05:05:00 +0000

    [Multidevice Setup] Revamp start multidevice setup UI.
    This CL updates the UI in the start setup page inside the
    multidevice setup flow. According to the mocks at
    go/cros-multidevice-setup-ui-2023, this CL:
    
    - Updates the header string.
    - Removes the multidevice summary sub-text.
    - Updates the footnote about feature availability.
    - Updates the feature list with a new presentation/icons.
    - Introduces new strings for each feature
    - Updates the 'connect' button language.
    - Updates line-spacing in multidevice <ui-page>.
    
    This CL also:
    
    - Updates the separator to semantic colors for correct
    appearance in dark mode.
    - Removes unused code to open a link from the now-removed
    summary text.
    
    Since ash/ cannot rely on Chrome/, some icons and translations
    are duplicated from OS settings. We continue to use
    cr-button instead of oobe-text-button for the same reason.
    
    Some features are not flagged to appear (ie,
    Instant Tethering and Wi-Fi Sync), as we do not have valid
    flags for these features. Since we don't check for policy
    in the other Feature flags, these features are hard-coded
    to display.
    
    Screenshots:
    
    Before:
    - Light: https://screenshot.googleplex.com/5nc6mgo3TcHPSKR
    
    After:
    - Light: https://screenshot.googleplex.com/garWqHvtt3vJnCy.png
    - Dark: https://screenshot.googleplex.com/Boa5vDVGLM672Y8.png
    
    Fixed: b/306657029
    Bug: b/268482023
    Test: Manual verification in OOBE and OS Settings.
    Change-Id: I91c31b2c9154440479eda081e4a181745c9f84e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5112920
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Mark Polner <polner@google.com>
    Cr-Commit-Position: refs/heads/main@{#1242289}

--- chrome/browser/ui/webui/ash/multidevice_setup/multidevice_setup_localized_strings_provider.cc
+++ chrome/browser/ui/webui/ash/multidevice_setup/multidevice_setup_localized_strings_provider.cc
@@ -52,10 +52,22 @@ constexpr webui::LocalizedString kLocalizedStringsWithoutPlaceholders[] = {
      IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_OFFLINE_DEVICE_OPTION},
     {"startSetupPageFeatureMirrorPhoneNotifications",
      IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_MIRROR_PHONE_NOTIFICATIONS},
-    {"startSetupPageFeatureWifiSync",
-     IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_WIFI_SYNC},
+    {"startSetupPageFeatureWifiSyncTitle",
+     IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_WIFI_SYNC_TITLE},
+    {"startSetupPageFeatureWifiSyncDescription",
+     IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_WIFI_SYNC_DESCRIPTION},
     {"startSetupPageFeatureCameraRoll",
      IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_CAMERA_ROLL},
+    {"startSetupPageFeaturePhoneHubTitle",
+     IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_PHONE_HUB_TITLE},
+    {"startSetupPageFeaturePhoneHubDescription",
+     IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_PHONE_HUB_DESCRIPTION},
+    {"startSetupPageFeatureInstantTetheringTitle",
+     IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_INSTANT_TETHERING_TITLE},
+    {"startSetupPageFeatureInstantTetheringDescription",
+     IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_INSTANT_TETHERING_DESCRIPTION},
+    {"startSetupPageFeatureSmartLockTitle",
+     IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_SMART_LOCK_TITLE},
     {"startSetupPageFeatureListInstallApps",
      IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_INSTALL_APPS_DESCRIPTION},
     {"startSetupPageFeatureListAddFeatures",
@@ -70,7 +82,8 @@ constexpr webui::LocalizedString kLocalizedStringsWithoutPlaceholders[] = {
     {"tryAgain", IDS_MULTIDEVICE_SETUP_TRY_AGAIN_LABEL},
     {"dialogAccessibilityTitle",
      IDS_MULTIDEVICE_SETUP_DIALOG_ACCESSIBILITY_TITLE},
-};
+    {"startSetupPageFeatureListHeader",
+     IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_FEATURE_LIST_HEADER}};
 
 struct LocalizedStringWithName {
   LocalizedStringWithName(const char* name,
@@ -105,17 +118,17 @@ GetLocalizedStringsWithPlaceholders() {
                 kFootnoteMarker));
 
         localized_strings.emplace_back(
-            "startSetupPageFeatureListHeader",
-            l10n_util::GetStringFUTF16(
-                IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_FEATURE_LIST_HEADER,
-                ui::GetChromeOSDeviceName()));
-
-        localized_strings.emplace_back(
             "startSetupPageFeatureListAwm",
             l10n_util::GetStringFUTF16(
                 IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_AWM_DESCRIPTION,
                 base::UTF8ToUTF16(
                     GetBoardSpecificMessagesLearnMoreUrl().spec())));
+
+        localized_strings.emplace_back(
+            "startSetupPageFeatureSmartLockDescription",
+            l10n_util::GetStringFUTF16(
+                IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_SMART_LOCK_DESCRIPTION,
+                ui::GetChromeOSDeviceName()));
 
         return localized_strings;
       }());
@@ -130,11 +143,6 @@ void AddLocalizedStrings(content::WebUIDataSource* html_source) {
 
   html_source->AddBoolean("phoneHubEnabled",
                           base::FeatureList::IsEnabled(features::kPhoneHub));
-
-  html_source->AddBoolean(
-      "phoneHubCameraRollEnabled",
-      base::FeatureList::IsEnabled(features::kPhoneHub) &&
-          base::FeatureList::IsEnabled(features::kPhoneHubCameraRoll));
 
   html_source->AddBoolean("wifiSyncEnabled", base::FeatureList::IsEnabled(
                                                  features::kWifiSyncAndroid));
@@ -161,10 +169,6 @@ void AddLocalizedValuesToBuilder(::login::LocalizedValuesBuilder* builder) {
                 base::UTF8ToUTF16(
                     GetBoardSpecificBetterTogetherSuiteLearnMoreUrl().spec()));
 
-  builder->AddF("startSetupPageFeatureListHeader",
-                IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_FEATURE_LIST_HEADER,
-                ui::GetChromeOSDeviceName());
-
   builder->AddF("startSetupPageFootnote",
                 IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_FOOTNOTE,
                 kFootnoteMarker);
@@ -173,6 +177,10 @@ void AddLocalizedValuesToBuilder(::login::LocalizedValuesBuilder* builder) {
       "startSetupPageFeatureListAwm",
       IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_AWM_DESCRIPTION,
       base::UTF8ToUTF16(GetBoardSpecificMessagesLearnMoreUrl().spec()));
+
+  builder->AddF("startSetupPageFeatureSmartLockDescription",
+                IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_SMART_LOCK_DESCRIPTION,
+                ui::GetChromeOSDeviceName());
 }
 
 }  // namespace ash::multidevice_setup
