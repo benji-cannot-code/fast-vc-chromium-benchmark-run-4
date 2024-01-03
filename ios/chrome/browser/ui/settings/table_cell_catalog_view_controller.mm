c108fe31f8cfd77c62c14e18513efccc9f57ec21
Rafał Godlewski <rgod@google.com>
2024-01-03 09:38:42 +0000

    [iOS] Add multi line text edit item to table cell catalog
    Low-Coverage-Reason: EXPERIMENTAL_CODE
    Change-Id: I8b506267435e630064cc69d32b4a6492b6772008
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5149622
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1242341}

--- ios/chrome/browser/ui/settings/table_cell_catalog_view_controller.mm
+++ ios/chrome/browser/ui/settings/table_cell_catalog_view_controller.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_info_button_item.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_link_header_footer_item.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_multi_detail_text_item.h"
+#import "ios/chrome/browser/shared/ui/table_view/cells/table_view_multi_line_text_edit_item.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_switch_item.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_tabs_search_suggested_history_item.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_text_button_item.h"
@@ -67,6 +68,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
   ItemTypeSearchHistorySuggestedItem,
   ItemTypeTextAccessoryNoImage,
   ItemTypeTextEditItem,
+  ItemTypeTextMultiLineEditItem,
   ItemTypeURLWithActivityIndicator,
   ItemTypeURLWithActivityIndicatorStopped,
   ItemTypeURLWithTimestamp,
@@ -382,6 +384,15 @@ typedef NS_ENUM(NSInteger, ItemType) {
   tableViewMultiDetailTextItem.accessoryType =
       UITableViewCellAccessoryDisclosureIndicator;
   [model addItem:tableViewMultiDetailTextItem
+      toSectionWithIdentifier:SectionIdentifierText];
+
+  TableViewMultiLineTextEditItem* tableViewMultiLineTextEditItem =
+      [[TableViewMultiLineTextEditItem alloc]
+          initWithType:ItemTypeTextMultiLineEditItem];
+  tableViewMultiLineTextEditItem.label = @"Multi Line Edit Text Item";
+  tableViewMultiLineTextEditItem.text =
+      @"This is possibly a very very very very very very long multi line text";
+  [model addItem:tableViewMultiLineTextEditItem
       toSectionWithIdentifier:SectionIdentifierText];
 
   TableViewSwitchItem* tableViewSwitchItem =
