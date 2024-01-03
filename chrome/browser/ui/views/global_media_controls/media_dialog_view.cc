8bf0e50f5084998bbca07e9ac6c70274dcc96bf7
Evan Liu <evliu@google.com>
2024-01-03 06:46:16 +0000

    Replace the caption settings button with a label button
    This CL removes the caption settings icon from the caption bubble header
    and turns the language label into a button. The branded Google Translate
    icon is shown in the language label when Live Translate is enabled.
    
    Demo: go/language-label-demo
    
    Bug: 1161569
    Change-Id: Idf0924be54ff17f52c6a3a4afa67e10bfc22ae8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5136890
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Anthony Cui <cuianthony@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242299}

--- chrome/browser/ui/views/global_media_controls/media_dialog_view.cc
+++ chrome/browser/ui/views/global_media_controls/media_dialog_view.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
-#include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/media/router/media_router_feature.h"
 #include "chrome/browser/profiles/profile.h"
@@ -558,8 +557,9 @@ void MediaDialogView::InitializeLiveTranslateSection() {
       profile_->GetPrefs()->GetBoolean(prefs::kLiveCaptionEnabled));
 
   auto live_translate_image = std::make_unique<views::ImageView>();
-  live_translate_image->SetImage(ui::ImageModel::FromVectorIcon(
-      kTranslateChromeRefreshIcon, ui::kColorIcon, kImageWidthDip));
+  live_translate_image->SetImage(
+      ui::ImageModel::FromVectorIcon(vector_icons::kTranslateChromeRefreshIcon,
+                                     ui::kColorIcon, kImageWidthDip));
   live_translate_container->AddChildView(std::move(live_translate_image));
 
   auto live_translate_label_wrapper = std::make_unique<View>();
