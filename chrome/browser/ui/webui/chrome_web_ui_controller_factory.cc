bb07df9e202dfe2e637326889eefc23de7ab1cb7
Irem Uguz <iremuguz@google.com>
2024-01-03 14:50:38 +0000

    support-tool: Redirect to Ash when URL is trigerred on Lacros
    We will execute the chrome://support-tool URL on Ash when it's run from
    Lacros using the Lacros url redirection. This will enable all Ash data
    collectors that collect system information on Lacros-primary devices.
    
    The demo of how it looks can be found here
    https://drive.google.com/file/d/1bGQp9IZMdxFSVwvgOYNzlHivfJc7dmIn/view?usp=sharing
    
    Bug: b:254050226
    Change-Id: I4473bbe9ea6f77edbe84334333cf0634493074a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5164287
    Commit-Queue: Irem Uguz <iremuguz@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242420}

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -1177,6 +1177,7 @@ ChromeWebUIControllerFactory::GetListOfAcceptableURLs() {
     GURL(chrome::kChromeUISetTimeURL),
     GURL(chrome::kChromeUISlowURL),
     GURL(chrome::kChromeUISmbShareURL),
+    GURL(chrome::kChromeUISupportToolURL),
     GURL(chrome::kChromeUISysInternalsUrl),
     GURL(chrome::kChromeUIUntrustedCroshURL),
     GURL(chrome::kChromeUIUntrustedTerminalURL),
