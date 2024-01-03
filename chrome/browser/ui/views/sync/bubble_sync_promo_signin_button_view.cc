3b5a31fed7961f8474150afc8a70d50cc0d11e89
EmiliaPaz <emiliapaz@chromium.org>
2024-01-03 15:49:37 +0000

    Replace SetProminent() with SetStyle(ButtonStyle::kProminent)
    Previously, MdTextButton only had 2 styles that could be set with
    SetProminent(). With the addition of a new style, now button style is
    set with SetStyle.
    
    Therefore, this remove calls of SetProminent() in favor of SetStyle(ButtonStyle::kProminent). This CL also removes an unnecesary
    call to SetProminent(), since it was already calling SetStyle().
    
    Bug: 1406008
    Change-Id: I5a6273b883c187e7e12d9415c0a0a5c8ac60f65e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5153857
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242451}

--- chrome/browser/ui/views/sync/bubble_sync_promo_signin_button_view.cc
+++ chrome/browser/ui/views/sync/bubble_sync_promo_signin_button_view.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/base/ui_base_types.h"
 #include "ui/gfx/canvas.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/button/md_text_button.h"
@@ -80,7 +81,7 @@ BubbleSyncPromoSigninButtonView::BubbleSyncPromoSigninButtonView(
                        .SetCallback(std::move(callback))
                        .SetText(l10n_util::GetStringUTF16(
                            IDS_PROFILES_DICE_SIGNIN_BUTTON))
-                       .SetProminent(true))
+                       .SetStyle(ui::ButtonStyle::kProminent))
       .BuildChildren();
 }
 
