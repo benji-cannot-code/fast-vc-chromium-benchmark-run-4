eb57b83495dfea4daf35152a34bd9295707c935c
Jun Kokatsu <jkokatsu@google.com>
2024-01-03 21:28:59 +0000

    Add url_canon_icu_fuzzer
    This change add a fuzzer for `ICUCharsetConverter::ConvertFromUTF16`.
    
    Bug: 1514667
    Change-Id: Ia0cbc4f019fef9f8ae4041edee04256434ed8dbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5155427
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Jun Kokatsu <jkokatsu@google.com>
    Commit-Queue: Jun Kokatsu <jkokatsu@google.com>
    Reviewed-by: Chris Bookholt <bookholt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242631}

---
+++ url/url_canon_icu_fuzzer.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <string>
+
+#include "url/url_canon_icu.h"
+#include "url/url_canon_icu_test_helpers.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  if (size < 1) {
+    return 0;
+  }
+
+  url::test::UConvScoper conv("utf-8");
+  url::ICUCharsetConverter converter(conv.converter());
+  url::RawCanonOutput<1024> output;
+
+  converter.ConvertFromUTF16(reinterpret_cast<const char16_t*>(data),
+                             size / sizeof(const char16_t), &output);
+  return 0;
+}
