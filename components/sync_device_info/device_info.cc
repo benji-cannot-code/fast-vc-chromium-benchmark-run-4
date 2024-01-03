2b454445cac91761bd153d207c004e66902a31c4
Mikel Astiz <mastiz@chromium.org>
2024-01-03 16:41:41 +0000

    [sync] Precompute DeviceInfo for each DeviceInfoSpecifics
    Eventually, the goal is to improve performance for functions such as
    DeviceInfoSyncBridge::GetAllDeviceInfo().
    
    As a first step, the internal map maintained by DeviceInfoSyncBridge is
    extended to maintain, for each proto, the converted DeviceInfo class.
    This incurs in some memory overhead but it is deemed negligible for the
    vast majority of users (a few kilobytes in total for the 99th
    percentile).
    
    This change doesn't immediately come with performance benefits as
    GetAllDeviceInfo()'s signature enforces the need to make deep copies.
    As per added TODOs, follow-up patches will update this signature to
    avoid these expensive copies.
    
    Change-Id: I88631d694516954e919299c6da967110440401a5
    Bug: b/316374607
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5164029
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242476}

--- components/sync_device_info/device_info.cc
+++ components/sync_device_info/device_info.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/memory/ptr_util.h"
 #include "components/sync/protocol/device_info_specifics.pb.h"
 #include "components/sync/protocol/sync_enums.pb.h"
 
@@ -213,5 +214,11 @@ void DeviceInfo::set_fcm_registration_token(const std::string& fcm_token) {
 void DeviceInfo::set_interested_data_types(const ModelTypeSet& data_types) {
   interested_data_types_ = data_types;
 }
+
+std::unique_ptr<DeviceInfo> DeviceInfo::Clone() const {
+  return base::WrapUnique(new DeviceInfo(*this));
+}
+
+DeviceInfo::DeviceInfo(const DeviceInfo& other) = default;
 
 }  // namespace syncer
