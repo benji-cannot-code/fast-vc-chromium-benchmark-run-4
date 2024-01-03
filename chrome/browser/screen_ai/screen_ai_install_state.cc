ac44c280bd95bfb4574419207606d95343030fe5
Ramin Halavati <rhalavati@chromium.org>
2024-01-03 15:22:03 +0000

    Update DebugWithoutCrashing in ScreenAIInstallState.
    A variable is aliased so that it would be available in debug dumps.
    
    AX-Relnotes: n/a
    Bug: 1510566
    Change-Id: I87da986208b4de4d3c0aec081cd7e33373a7944b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5164290
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242437}

--- chrome/browser/screen_ai/screen_ai_install_state.cc
+++ chrome/browser/screen_ai/screen_ai_install_state.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/check_is_test.h"
+#include "base/debug/alias.h"
 #include "base/debug/dump_without_crashing.h"
 #include "base/files/file_path.h"
 #include "base/functional/bind.h"
@@ -195,6 +196,7 @@ void ScreenAIInstallState::SetState(State state) {
   // TODO(crbug.com/1508404): Remove after crash root cause is found.
   if ((state == State::kDownloaded || state == State::kReady) &&
       !IsComponentAvailable()) {
+    base::debug::Alias(&state);
     base::debug::DumpWithoutCrashing();
     state = State::kFailed;
   }
