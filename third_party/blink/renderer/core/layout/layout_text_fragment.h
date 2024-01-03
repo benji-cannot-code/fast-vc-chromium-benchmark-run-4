a06433d1aae9038a3d041a38345d239642acd268
Kent Tamura <tkent@chromium.org>
2024-01-03 16:39:43 +0000

    Refactor blink::LayoutText
    * Make ApplyTextTransform() a const method
    * Make SecureText() a const method
    * Rename ApplyTextTransform() to TransformAndSecureText()
    * Make it public, and use it from InlineNode and InlineItemsBuilder
    * Rename TransformText() to TransformAndSecureOriginalText() for
      consistency
    
    This CL is a preparation to fix crbug.com/486880, and should have no
    behavior changes.
    
    Bug: 486880
    Change-Id: I46b7497dd2371d0ff4f36ba1906394b505169eae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5149117
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242474}

--- third_party/blink/renderer/core/layout/layout_text_fragment.h
+++ third_party/blink/renderer/core/layout/layout_text_fragment.h
@@ -93,7 +93,7 @@ class CORE_EXPORT LayoutTextFragment : public LayoutText {
 
   void SetTextFragment(String, unsigned start, unsigned length);
 
-  void TransformText() override;
+  void TransformAndSecureOriginalText() override;
 
   const char* GetName() const override {
     NOT_DESTROYED();
