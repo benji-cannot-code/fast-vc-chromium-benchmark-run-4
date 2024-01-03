32a70a16ccc525587be1fefe988630bcaffd9505
Maria Kazinova <kazinova@google.com>
2024-01-03 12:11:35 +0000

    Prioritize password predictions over single username ones for saving.
    Some forms receive both single username and password predictions.
    Classifiers cannot produce such classifications, but for top websites
    with many overrides deployed and multiple forms being reused for
    different flows, this is possible.
    
    Single username predictions are historically prioritized even though
    they are less reliable. This leads to PasswordManager ignoring
    password fields and not attempting to save values inputted by the user.
    
    This CL ensures passwords are saved, while filling of both single
    usernames and passwords remains possible.
    
    Bug: b/317988046
    Change-Id: I29c4b93e6b8be73b92ba138213193a654d13645e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5159027
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242370}

--- components/password_manager/core/browser/form_parsing/form_data_parser_unittest.cc
+++ components/password_manager/core/browser/form_parsing/form_data_parser_unittest.cc
@@ -2969,6 +2969,9 @@ TEST_F(FormParserTest, ContradictingPasswordPredictionAndAutocomplete) {
 }
 
 TEST_F(FormParserTest, SingleUsernamePrediction) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitAndDisableFeature(
+      password_manager::features::kUseServerPredictionsOnSaveParsing);
   CheckTestData({
       {
           .description_for_logging = "1 field",
@@ -2989,6 +2992,30 @@ TEST_F(FormParserTest, SingleUsernamePrediction) {
                   {.form_control_type = FormControlType::kInputPassword,
                    .prediction = {.type = autofill::PASSWORD}},
               },
+      },
+  });
+}
+
+// Password predictions should have priority over single username predictions
+// when the form is parsed for saving to avoid losing the password.
+TEST_F(FormParserTest, BothSingleUsernameAndPasswordPredictions) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitAndEnableFeature(
+      password_manager::features::kUseServerPredictionsOnSaveParsing);
+  CheckTestData({
+      {
+          .description_for_logging =
+              "Form with both SINGLE_USERNAME and PASSWORD predictions.",
+          .fields =
+              {
+                  {.role = ElementRole::USERNAME,
+                   .form_control_type = FormControlType::kInputText,
+                   .prediction = {.type = autofill::SINGLE_USERNAME}},
+                  {.role_saving = ElementRole::CURRENT_PASSWORD,
+                   .form_control_type = FormControlType::kInputPassword,
+                   .prediction = {.type = autofill::PASSWORD}},
+              },
+          .fallback_only = false,
       },
   });
 }
