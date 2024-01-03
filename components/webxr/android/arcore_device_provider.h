b05266535c331a60f4ae6e5e4490611ebb4c5584
Alexander Cooper <alcooper@chromium.org>
2024-01-03 01:01:38 +0000

    Query for OpenXr Runtime status on Android startup
    Attempts to create an XrInstance before creating an `OpenXrDevice` on
    Android. This ensures that the OpenXr runtime can actually be supported
    on the current device before we create the device that would attempt to
    supply an OpenXr Session. This early creation of the XrInstance also
    allows us to query for the supported environment blend modes at device
    creation and allows us to accurately report if the device can support
    a non-opaque blend mode, at which point it will take priority over
    ARCore for "immersive-ar" sessions as well.
    
    Note that initial plumbing of the service/webcontents was based on:
    https://crrev.com/c/4632076
    
    Bug: 1435548,1458584
    Change-Id: Ibb8e04014c67da03cd9bab92f011901d2d88d607
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5146928
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242227}

--- components/webxr/android/arcore_device_provider.h
+++ components/webxr/android/arcore_device_provider.h
@@ -29,7 +29,8 @@ class ArCoreDeviceProvider : public device::VRDeviceProvider {
   ArCoreDeviceProvider& operator=(const ArCoreDeviceProvider&) = delete;
 
   ~ArCoreDeviceProvider() override;
-  void Initialize(device::VRDeviceProviderClient* client) override;
+  void Initialize(device::VRDeviceProviderClient* client,
+                  content::WebContents* initializing_web_contents) override;
   bool Initialized() override;
 
  private:
--- components/webxr/android/cardboard_device_provider.h
+++ components/webxr/android/cardboard_device_provider.h
@@ -29,7 +29,8 @@ class CardboardDeviceProvider : public device::VRDeviceProvider {
   CardboardDeviceProvider(const CardboardDeviceProvider&) = delete;
   CardboardDeviceProvider& operator=(const CardboardDeviceProvider&) = delete;
 
-  void Initialize(device::VRDeviceProviderClient* client) override;
+  void Initialize(device::VRDeviceProviderClient* client,
+                  content::WebContents* initializing_web_contents) override;
   bool Initialized() override;
 
  private:
