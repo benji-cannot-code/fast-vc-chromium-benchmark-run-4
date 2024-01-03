adabf26fe9f31f19d960b1df7d104f2c87b5946f
Nihar Majmudar <niharm@google.com>
2024-01-03 20:28:06 +0000

    [realbox][cr23] Update hover color for action chip and chevron icon.
    Before: https://screenshot.googleplex.com/A9H2wYmMQvti8JK
    After: https://screenshot.googleplex.com/3LAftnxyVfqK4iE
    
    Bug: 1514371
    Change-Id: Ib41ab5a62d69062c7da0dab56d64fb8e50d280eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5152310
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1242595}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -384,7 +384,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorRealboxResultsActionChipFocusOutline) \
   E_CPONLY(kColorRealboxResultsBackground) \
   E_CPONLY(kColorRealboxResultsBackgroundHovered) \
-  E_CPONLY(kColorRealboxResultsControlBackgroundHovered) \
   E_CPONLY(kColorRealboxResultsDimSelected) \
   E_CPONLY(kColorRealboxResultsFocusIndicator) \
   E_CPONLY(kColorRealboxResultsForeground) \
