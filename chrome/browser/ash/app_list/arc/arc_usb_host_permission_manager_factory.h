afc5597ac80237480ea87963d3a911ed3f6de17e
Arthur Milchior <arthurmilchior@chromium.org>
2024-01-03 13:19:44 +0000

    Implement the non-deprecated builder
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    In order to be usable with std::make_unique, constructor and
    destructor had to became public. This is consistent with most other
    keyed services.
    
    As the builder delegate the building to the `Create` method, it is
    also changed to return a `std::unique_ptr`.
    
    Bug: 1396138
    Change-Id: I247e27189bb32f659c0ba23f13bac644b80440ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4799912
    Reviewed-by: Long Cheng <lgcheng@google.com>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242390}

--- chrome/browser/ash/app_list/arc/arc_usb_host_permission_manager_factory.h
+++ chrome/browser/ash/app_list/arc/arc_usb_host_permission_manager_factory.h
@@ -31,7 +31,7 @@ class ArcUsbHostPermissionManagerFactory : public ProfileKeyedServiceFactory {
   ArcUsbHostPermissionManagerFactory();
   ~ArcUsbHostPermissionManagerFactory() override;
 
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
