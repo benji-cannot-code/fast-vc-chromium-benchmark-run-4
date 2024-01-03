d408bbc2254fc2069224142b6bc1768b47723efa
Alex Rudenko <alexrudenko@chromium.org>
2024-01-03 06:09:27 +0000

    DevTools: emulate focus only if instructed
    Fixed: 1492129
    Change-Id: Icbdfbb6c7355f01659873b62b6a2f4a723af9df5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5151324
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242295}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/emulation/set-focus-emulation-enabled-multi-session.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  await testRunner.startBlank('Tests that page focus emulation is not reset by unrelated sessions');
+
+  const page = await testRunner.createPage();
+
+  const session1 = await page.createSession();
+  const session2 = await page.createSession();
+
+  async function logFocus(session, label) {
+    testRunner.log(`hasFocus(${label}):` + await session.evaluate('document.hasFocus()'));
+  }
+
+  await logFocus(session1, 'session1, before emulation');
+  await logFocus(session2, 'session2, before emulation');
+
+  await session1.protocol.Emulation.setFocusEmulationEnabled({enabled: true});
+
+  await logFocus(session1, 'session1, after emulation');
+  await logFocus(session2, 'session2, after emulation');
+
+  await session2.disconnect();
+
+  await logFocus(session1, 'session1, after emulation, after session2 disconnect');
+
+  await session1.disconnect();
+
+  const session3 = await page.createSession();
+  await logFocus(session3, 'session3, after emulation, after session1 disconnect');
+
+  testRunner.completeTest();
+})
