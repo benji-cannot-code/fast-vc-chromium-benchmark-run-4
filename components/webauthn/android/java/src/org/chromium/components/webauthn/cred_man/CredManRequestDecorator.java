d4bd5b2d3d8d14850dee0bedc1d0714783f1e325
Adem Derinel <derinel@google.com>
2024-01-03 08:02:54 +0000

    [Android][CredMan] Introduce app and browser request decorators
    As mentioned in [1], apps should not set the origin in requests.
    This change has no impact. Future CLs will switch the WebAuthnMode
    depending on where the Authenticator is used.
    
    [1]: https://developer.android.com/training/sign-in/passkeys#sign-in
    
    Bug: 1511193, b/316884583
    Low-Coverage-Reason: TRIVIAL_CHANGE the decorators are simple
    Change-Id: I136f34ecffc2d93d31cc386ffa55f292e65f7ac5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5149699
    Auto-Submit: Adem Derinel <derinel@google.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1242316}

--- components/webauthn/android/java/src/org/chromium/components/webauthn/cred_man/CredManRequestDecorator.java
+++ components/webauthn/android/java/src/org/chromium/components/webauthn/cred_man/CredManRequestDecorator.java
@@ -11,7 +11,7 @@ import android.credentials.GetCredentialRequest;
 import android.os.Bundle;
 
 /** Interface for code that will update the CredMan request bundles or requests. */
-interface CredManRequestDecorator {
+public interface CredManRequestDecorator {
     void updateCreateCredentialRequestBundle(
             Bundle input, CredManCreateCredentialRequestHelper helper);
 
