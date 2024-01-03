84e8040953d563e8e628a855d9c703ecc94bf390
Ella Ge <eirage@chromium.org>
2024-01-03 22:58:52 +0000

    Remove BlockInstallPromptIfIgnoreRecently
    This partially reverts crrev.com/ce94dede47e1cbadfd093167404956010ccb7a3d
    
    The ML prompt is going well, and it has a similar guardrail block.
    It is unlikely that we need a separate ignore block for the legacy logic
    There is no need to experiment with this flag.
    
    Bug: 1466023
    Change-Id: Id1f27962f0e5ea5fa233ee7ab9d640151ccc38e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5166287
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Auto-Submit: Ella Ge <eirage@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242665}

--- components/webapps/browser/features.h
+++ components/webapps/browser/features.h
@@ -41,7 +41,6 @@ extern const base::FeatureParam<int> kInstallPromptGlobalGuardrails_IgnoreCount;
 extern const base::FeatureParam<base::TimeDelta>
     kInstallPromptGlobalGuardrails_IgnorePeriod;
 BASE_DECLARE_FEATURE(kWebApkInstallFailureNotification);
-BASE_DECLARE_FEATURE(kBlockInstallPromptIfIgnoreRecently);
 #endif  // BUILDFLAG(IS_ANDROID)
 
 BASE_DECLARE_FEATURE(kCreateShortcutIgnoresManifest);
