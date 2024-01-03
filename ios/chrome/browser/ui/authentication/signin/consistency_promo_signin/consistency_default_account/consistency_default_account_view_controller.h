75130ce4c880498be7d0a5a098aa008e8d148756
Arthur Milchior <arthurmilchior@chromium.org>
2024-01-03 14:04:38 +0000

    [iOS] The escape key close account chooser.
    This in particular ensure that Chrome does not crash due to improperly
    closed view.
    
    Fixed: 1505958
    Change-Id: Ibcce801b214edbf08c684863ebe10681b34d5bbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5152892
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Reviewed-by: Menghan Yang <myuu@google.com>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242401}

--- ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_default_account/consistency_default_account_view_controller.h
+++ ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_default_account/consistency_default_account_view_controller.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Delegate protocol for ConsistencyDefaultAccountViewController.
 @protocol ConsistencyDefaultAccountActionDelegate <NSObject>
 
-// Called when the user taps on the skip button.
+// Called when the user taps on the skip or cancel button.
 - (void)consistencyDefaultAccountViewControllerSkip:
     (ConsistencyDefaultAccountViewController*)viewController;
 // Called when the user taps on the identity chooser button.
