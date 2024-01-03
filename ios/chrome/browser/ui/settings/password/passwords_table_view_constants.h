b0392a32cb4a18a5eb11e09632bb2369b5923e4a
Rafał Godlewski <rgod@google.com>
2024-01-03 18:26:20 +0000

    [iOS] Remove obsolete passwords table view constants TODO
    Bug: 1486512
    Change-Id: I3383e1b2f93a732e513a5521e86b7287893d52d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5164968
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242535}

--- ios/chrome/browser/ui/settings/password/passwords_table_view_constants.h
+++ ios/chrome/browser/ui/settings/password/passwords_table_view_constants.h
@@ -7,10 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_UI_SETTINGS_PASSWORD_PASSWORDS_TABLE_VIEW_CONSTANTS_H_
 
 #import <Foundation/Foundation.h>
-#import "ios/chrome/browser/shared/ui/list_model/list_model.h"
 
-// TODO(crbug.com/1486512): Acronyms in Objective-C identifiers should be
-// written in all caps.
+#import "ios/chrome/browser/shared/ui/list_model/list_model.h"
 
 // The accessibility identifier of the Password Manager table view.
 extern NSString* const kPasswordsTableViewID;
