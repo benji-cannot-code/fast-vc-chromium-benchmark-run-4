ba8131358124babd76b13af7e45c46d16297b31d
Emily Shack <emshack@chromium.org>
2024-01-03 17:07:49 +0000

    [TabOrganization] Theme the "Not Started" image with CR23 colors
    the current image uses the default blue colors for the svg, but when
    a user is using a custom color theme, it looks not themed. To fix this
    I added a css file that provides variables for the SVG and then mapped
    them to the color provider colors when gm3 is enabled.
    
    examples can be seen here... https://drive.google.com/corp/drive/u/0/folders/1pLaoehRFaVdsfjrrZTAhK7s86jUtTe2q?resourcekey=0-nDrRnLXXG7--0r22XG-Z4g
    
    Change-Id: Ie93be249a8b02fc74b0afad21216765e2b9b158a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5118712
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242485}

--- chrome/browser/resources/tab_search/tab_organization_not_started.ts
+++ chrome/browser/resources/tab_search/tab_organization_not_started.ts
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import './strings.m.js';
+import './tab_organization_not_started_image.js';
 import './tab_organization_shared_style.css.js';
 
 import {WebUiListenerMixin} from 'chrome://resources/cr_elements/web_ui_listener_mixin.js';
--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -593,6 +593,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorTabSearchCardBackground) \
   E_CPONLY(kColorTabSearchBackground) \
   E_CPONLY(kColorTabSearchDivider) \
+  E_CPONLY(kColorTabSearchImageTabContentBottom) \
+  E_CPONLY(kColorTabSearchImageTabContentTop) \
+  E_CPONLY(kColorTabSearchImageTabText) \
+  E_CPONLY(kColorTabSearchImageWindowFrame) \
   E_CPONLY(kColorTabSearchMediaIcon) \
   E_CPONLY(kColorTabSearchMediaRecordingIcon) \
   E_CPONLY(kColorTabSearchPrimaryForeground) \
