32a5084ef298980bc2008e7ec8d32f3c1bb87488
Andy Phan <andyphan@chromium.org>
2024-01-03 19:56:09 +0000

    [CodeHealth] Remove expired histogram Extensions.PowerfulCount
    This histogram has been expired for two years.
    
    Bug: 1499401
    Change-Id: I5877c7284c63f1ada0e8514394df4ada43a1a480
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5154112
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242579}

--- chrome/browser/ui/webui/management/management_ui_handler.cc
+++ chrome/browser/ui/webui/management/management_ui_handler.cc
@@ -17,8 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/functional/callback_helpers.h"
-#include "base/metrics/histogram_functions.h"
-#include "base/metrics/user_metrics.h"
 #include "base/strings/escape.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -236,8 +234,6 @@ const char kOverview[] = "overview";
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
 const char kCustomerLogo[] = "customerLogo";
-
-const char kPowerfulExtensionsCountHistogram[] = "Extensions.PowerfulCount";
 
 namespace {
 
@@ -1267,14 +1263,8 @@ void ManagementUIHandler::HandleGetExtensions(const base::Value::List& args) {
       extensions::ExtensionRegistry::Get(Profile::FromWebUI(web_ui()))
           ->enabled_extensions();
 
-  base::Value::List powerful_extensions = GetPowerfulExtensions(extensions);
-
-  // The number of extensions to be reported in chrome://management with
-  // powerful permissions.
-  base::UmaHistogramCounts1000(kPowerfulExtensionsCountHistogram,
-                               powerful_extensions.size());
-
-  ResolveJavascriptCallback(args[0] /* callback_id */, powerful_extensions);
+  ResolveJavascriptCallback(args[0] /* callback_id */,
+                            GetPowerfulExtensions(extensions));
 }
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
