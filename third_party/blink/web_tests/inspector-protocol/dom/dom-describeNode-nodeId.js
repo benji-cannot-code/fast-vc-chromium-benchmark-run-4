13b3216a37d2e2ef2c740dc4d933ae4e3b57ce64
Andrey Kosyakov <caseq@chromium.org>
2024-01-03 22:12:00 +0000

    Report nodeId if available in DOM.describeNode
    Bug: 1515175
    Change-Id: I91cfc025feffaf573bf7d28fae25bfe33b66354f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5166367
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Auto-Submit: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242656}

---
+++ third_party/blink/web_tests/inspector-protocol/dom/dom-describeNode-nodeId.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+
+(async function(testRunner) {
+  const {dp} = await testRunner.startHTML(
+    `<div id='div'></div>`,
+    'Tests that DOM.describeNode returns a nodeId iff one is currently assigned.');
+
+  function nodeId(description) {
+    const nodeId = description.node.nodeId;
+    if (typeof nodeId !== 'number')
+      return nodeId;
+    return nodeId !== 0 ? 'present' : '0';
+  }
+
+  const {result} = await dp.Runtime.evaluate({expression: `document.getElementById('div')`});
+  const objectId = result.result.objectId;
+  let description = (await dp.DOM.describeNode({objectId})).result;
+
+  testRunner.log(`NodeId before DOM agent enabled: ${nodeId(description)}`);
+
+  await dp.DOM.getDocument();
+
+  description = (await dp.DOM.describeNode({objectId})).result;
+  testRunner.log(`NodeId after DOM.getDocument: ${nodeId(description)}`);
+
+  const requestedNode = (await dp.DOM.requestNode({objectId})).result;
+
+  description = (await dp.DOM.describeNode({objectId})).result;
+  testRunner.log(`NodeId after node requested: ${nodeId(description)}`);
+
+  testRunner.completeTest();
+})
