bceda320512827bdc83cde05b1f1707e24bbfa9b
Chris Fredrickson <cfredric@chromium.org>
2024-01-03 20:44:51 +0000

    Add nullptr checks in WebTestStorageAccessManager
    I don't expect this to fix any of the flakes in the linked bug, but
    hopefully it'll eliminate some possible culprits. Or make them explode
    in a clearer way!
    
    Bug: 1512480
    Change-Id: I9aa4ba96388264ea66c0da9b4939ab2a1dbc876c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5165787
    Commit-Queue: danakj <danakj@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242603}

--- content/web_test/browser/web_test_storage_access_manager.cc
+++ content/web_test/browser/web_test_storage_access_manager.cc
@@ -19,7 +19,8 @@ namespace content {
 
 WebTestStorageAccessManager::WebTestStorageAccessManager(
     BrowserContext* browser_context)
-    : browser_context_(browser_context) {}
+    : browser_context_(
+          base::raw_ref<content::BrowserContext>::from_ptr(browser_context)) {}
 
 WebTestStorageAccessManager::~WebTestStorageAccessManager() = default;
 
@@ -29,6 +30,7 @@ void WebTestStorageAccessManager::SetStorageAccess(
     const bool blocked,
     blink::test::mojom::StorageAccessAutomation::SetStorageAccessCallback
         callback) {
+  CHECK(callback);
   if (!ContentSettingsPattern::FromString(origin).IsValid() ||
       !ContentSettingsPattern::FromString(embedding_origin).IsValid()) {
     std::move(callback).Run(false);
@@ -48,8 +50,11 @@ void WebTestStorageAccessManager::SetStorageAccess(
   // cause Storage Access API grants to become relevant.
   StoragePartition* storage_partition =
       browser_context_->GetDefaultStoragePartition();
-  storage_partition->GetCookieManagerForBrowserProcess()
-      ->BlockThirdPartyCookies(blocked);
+  CHECK(storage_partition);
+  network::mojom::CookieManager* cookie_manager =
+      storage_partition->GetCookieManagerForBrowserProcess();
+  CHECK(cookie_manager);
+  cookie_manager->BlockThirdPartyCookies(blocked);
   storage_partition->FlushNetworkInterfaceForTesting();
   std::move(callback).Run(true);
 }
