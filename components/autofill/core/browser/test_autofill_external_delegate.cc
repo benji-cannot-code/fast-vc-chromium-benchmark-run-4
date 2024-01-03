1e9748d687914b5bf83c51a206b84276be0617c2
Bruno Braga <brunobraga@google.com>
2024-01-03 11:14:44 +0000

    [mullet m3] Remove unnecessary checkSuggestions method.
    The current BAM test has hardcoded checkSuggestions expecting
    specific count of suggestions when asserting them. This is not
    necessary since one can simply use a vector to check this count.
    
    Removing this check makes it possible to more easily parameterize
    tests and avoid unnecessary calls.
    
    Bug: 1493361
    Change-Id: Ifbfb18913766c169c661b29aa303d7988df15ee9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5150736
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1242360}

--- components/autofill/core/browser/test_autofill_external_delegate.cc
+++ components/autofill/core/browser/test_autofill_external_delegate.cc
@@ -87,14 +87,13 @@ void TestAutofillExternalDelegate::WaitForPopupHidden() {
 
 void TestAutofillExternalDelegate::CheckSuggestions(
     FieldGlobalId field_id,
-    size_t expected_num_suggestions,
-    const Suggestion expected_suggestions[]) {
+    const std::vector<Suggestion>& expected_suggestions) {
   // Ensure that these results are from the most recent query.
   EXPECT_TRUE(on_suggestions_returned_seen_);
 
   EXPECT_EQ(field_id, field_id_);
-  ASSERT_LE(expected_num_suggestions, suggestions_.size());
-  for (size_t i = 0; i < expected_num_suggestions; ++i) {
+  ASSERT_EQ(expected_suggestions.size(), suggestions_.size());
+  for (size_t i = 0; i < suggestions_.size(); ++i) {
     SCOPED_TRACE(base::StringPrintf("i: %" PRIuS, i));
     EXPECT_EQ(expected_suggestions[i].main_text.value,
               suggestions_[i].main_text.value);
@@ -105,7 +104,6 @@ void TestAutofillExternalDelegate::CheckSuggestions(
     EXPECT_EQ(expected_suggestions[i].popup_item_id,
               suggestions_[i].popup_item_id);
   }
-  ASSERT_EQ(expected_num_suggestions, suggestions_.size());
 }
 
 void TestAutofillExternalDelegate::CheckSuggestionsNotReturned(
@@ -116,7 +114,7 @@ void TestAutofillExternalDelegate::CheckSuggestionsNotReturned(
 }
 
 void TestAutofillExternalDelegate::CheckNoSuggestions(FieldGlobalId field_id) {
-  CheckSuggestions(field_id, 0, nullptr);
+  CheckSuggestions(field_id, {});
 }
 
 void TestAutofillExternalDelegate::CheckSuggestionCount(
