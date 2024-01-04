e1c55640c7fed7afb5beae9055cfd368151ed05e
Rafał Godlewski <rgod@google.com>
2024-01-04 09:58:39 +0000

    [iOS] Remove initializing navigation controller in password picker
    It was not needed or used in the first place. Additionally adds
    cleaning up all sharing coordinators on swipe/dismiss of people
    picker.
    
    Bug: 1463882
    Change-Id: I75c9fdcc2b2c8e27fdfd28058d64979e02957d1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5151171
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1242808}

--- ios/chrome/browser/ui/settings/password/password_sharing/password_picker_coordinator.mm
+++ ios/chrome/browser/ui/settings/password/password_sharing/password_picker_coordinator.mm
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/password_manager/core/browser/ui/credential_ui_entry.h"
 #import "ios/chrome/browser/favicon/ios_chrome_favicon_loader_factory.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
-#import "ios/chrome/browser/shared/ui/table_view/table_view_navigation_controller.h"
 #import "ios/chrome/browser/shared/ui/table_view/table_view_utils.h"
 #import "ios/chrome/browser/ui/settings/password/password_sharing/password_picker_coordinator_delegate.h"
 #import "ios/chrome/browser/ui/settings/password/password_sharing/password_picker_mediator.h"
@@ -16,13 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/password/password_sharing/password_picker_view_controller_presentation_delegate.h"
 
 @interface PasswordPickerCoordinator () <
-    PasswordPickerViewControllerPresentationDelegate> {
+    PasswordPickerViewControllerPresentationDelegate,
+    UIAdaptivePresentationControllerDelegate> {
   std::vector<password_manager::CredentialUIEntry> _credentials;
 }
-
-// The navigation controller displaying the view controller.
-@property(nonatomic, strong)
-    TableViewNavigationController* navigationController;
 
 // Main view controller for this coordinator.
 @property(nonatomic, strong) PasswordPickerViewController* viewController;
@@ -64,21 +60,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                               self.browser->GetBrowserState())];
   self.viewController.imageDataSource = self.mediator;
   self.mediator.consumer = self.viewController;
-  self.navigationController =
-      [[TableViewNavigationController alloc] initWithTable:self.viewController];
-  [self.navigationController
-      setModalPresentationStyle:UIModalPresentationFormSheet];
-  self.navigationController.navigationBar.prefersLargeTitles = NO;
 
-  UISheetPresentationController* sheetPresentationController =
-      self.navigationController.sheetPresentationController;
-  if (sheetPresentationController) {
-    sheetPresentationController.detents = @[
-      [UISheetPresentationControllerDetent mediumDetent],
-      [UISheetPresentationControllerDetent largeDetent]
-    ];
-  }
-
+  CHECK(self.baseNavigationController);
+  self.baseNavigationController.presentationController.delegate = self;
   // Disable animation so that it looks as if the loaded password data replaces
   // the spinner view displayed from the parent coordinator.
   [self.baseNavigationController pushViewController:self.viewController
@@ -86,7 +70,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)stop {
-  self.navigationController = nil;
   self.viewController = nil;
   self.mediator = nil;
 }
@@ -102,6 +85,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       withSelectedCredential:
           (const password_manager::CredentialUIEntry&)credential {
   [self.delegate passwordPickerCoordinator:self didSelectCredential:credential];
+}
+
+#pragma mark - UIAdaptivePresentationControllerDelegate
+
+- (void)presentationControllerDidDismiss:
+    (UIPresentationController*)presentationController {
+  [self.delegate passwordPickerCoordinatorWasDismissed:self];
 }
 
 @end
