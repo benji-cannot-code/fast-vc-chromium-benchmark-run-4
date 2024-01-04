c42faed6a126fcb5655518b2a0341ad349832888
Chris Harrelson <chrishtr@chromium.org>
2024-01-04 18:26:31 +0000

    Ensure an AXContext before painting.
    Before this CL, if a node was highlighted in an iframe, an AXContext
    may not have been initialized before painting began, which is a
    rendering lifecycle error.
    
    Fixed: 1515088
    
    Change-Id: I0f95ad59c3c982b43b2ac4beddc76414a67d22c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5165954
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243003}

---
+++ third_party/blink/web_tests/inspector-protocol/overlay/overlay-in-iframe.js
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {page, session, dp} = await testRunner.startHTML(``,
+    "Verifies that overlay of an iframe element doesn't crash.");
+
+  await session.evaluate(() => {
+    var iframe = document.createElement('iframe');
+    iframe.style.position = 'absolute';
+    iframe.style.top = '200px';
+    iframe.style.left = '200px';
+    iframe.style.width = '500px';
+    iframe.style.height = '500px';
+    document.body.appendChild(iframe);
+    iframe.contentWindow.document.body.innerHTML = `
+      <div style="width:100px;height:100px;background:orange"></div>
+    `;
+  });
+
+  await dp.DOM.enable();
+  await dp.Emulation.enable();
+  await dp.Overlay.enable();
+
+  const root = (await dp.DOM.getDocument()).result.root;
+  const iframeDiv = (await dp.DOM.getNodeForLocation({x: 250, y: 250})).result.nodeId;
+
+  const result = await dp.Overlay.highlightNode({
+    highlightConfig: {contentColor: {r: 0, g: 128, b: 0, a: 0.5}},
+    nodeId: iframeDiv,
+  });
+
+  // Wait for overlay rendering to finish by requesting an animation frame.
+  await session.evaluate(() => {
+    return new Promise(resolve => requestAnimationFrame(resolve));
+  });
+
+  testRunner.completeTest();
+});
