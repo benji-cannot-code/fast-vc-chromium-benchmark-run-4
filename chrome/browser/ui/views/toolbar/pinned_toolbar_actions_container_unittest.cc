8ad5915541b6b60d5ce2117ef2226eb00c218b0b
Yuheng Huang <yuhengh@chromium.org>
2024-01-04 19:51:04 +0000

    Side Panel Pinning: Disable AnimatingLayoutManager
    Currently there are 2 AnimatingLayoutManager living side by
    side and it's causing race condition when both of them are trying
    to animate. Since we know that only one AnimatingLayoutManager in the
    toolbar view works, ExtensionsToolbarContainer has had animation for
    many years, this CL temporally removes the animation in
    PinnedToolbarActionsContainer until the issue is addressed.
    
    Screencast: https://screencast.googleplex.com/cast/NTI5MDM1ODgxOTc4MjY1Nnw0YzBlMWM0Ni1iYQ
    Bug: 1513183,1514477
    Change-Id: Ifc9d13a8d3747c19823b37af23bcb477688421ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5142528
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243044}

--- chrome/browser/ui/views/toolbar/pinned_toolbar_actions_container_unittest.cc
+++ chrome/browser/ui/views/toolbar/pinned_toolbar_actions_container_unittest.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ui_base_features.h"
 #include "ui/compositor/layer_tree_owner.h"
 #include "ui/events/base_event_utils.h"
-#include "ui/views/layout/animating_layout_manager_test_util.h"
 
 class PinnedToolbarActionsContainerTest : public TestWithBrowserView {
  public:
@@ -365,12 +364,7 @@ TEST_F(PinnedToolbarActionsContainerTest, MovingActionsUpdateOrderUsingDrag) {
   ui::mojom::DragOperation output_drag_op = ui::mojom::DragOperation::kNone;
   std::move(drop_cb).Run(drop_event, output_drag_op,
                          /*drag_image_layer_owner=*/nullptr);
-#if BUILDFLAG(IS_MAC)
-  // TODO(crbug.com/1045212): we avoid using animations on Mac due to the lack
-  // of support in unit tests. Therefore this is a no-op.
-#else
-  views::test::WaitForAnimatingLayoutManager(container());
-#endif
+
   // Verify the order gets updated in the ui.
   toolbar_buttons = GetChildToolbarButtons();
   ASSERT_EQ(toolbar_buttons.size(), 2u);
