211ec642658d60ad2fb35e4265b8dee4c761e81b
Luciano Pacheco <lucmult@chromium.org>
2024-01-04 07:18:39 +0000

    Files TS: Remove some externs
    Trying again CL:5125873
    
    Removed unused:
    * app_window_common.js
    * drive_sync_handler.js
    * drag_target.js
    * papaer_elements.js
    * search_item.js
    
    Migrate some interfaces close to the implementation:
    * externs/exif_entry.js => metadata/exif_constants.ts
    * externs/command_handler_deps.js => command_handler.ts
    
    Applied ESlint fixes as per new rules.
    
    Bug: b:289003444
    Change-Id: I2ea8e37c03975d6c248bb320c79b138697743178
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5166470
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242793}

--- ui/file_manager/file_manager/foreground/elements/files_quick_view.ts
+++ ui/file_manager/file_manager/foreground/elements/files_quick_view.ts
@@ -17,6 +17,7 @@ import type {FilesMetadataBox} from './files_metadata_box.js';
 import {getTemplate} from './files_quick_view.html.js';
 import type {FilesSafeMedia} from './files_safe_media.js';
 
+
 export interface FilesQuickView {
   $: {
     contentPanel: HTMLDivElement,
