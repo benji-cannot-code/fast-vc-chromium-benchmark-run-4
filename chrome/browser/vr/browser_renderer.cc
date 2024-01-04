9161f92b44f4a93a4027445d2d0e1f0f7a47b664
Alexander Cooper <alcooper@chromium.org>
2024-01-04 21:28:13 +0000

    Clean up GraphicsDelegate code
    Now that GVR has been removed, this cleans up the GraphicsDelegate
    interface that had a few virtual or non-pure virtual methods solely due
    to the need to interface with the gvr graphics delegate.
    
    Note that though not tagged for cleanup, the UI::DrawWebXr method would
    now always be receiving a texture of 0 and thus always no-oping.
    
    Fixed: 1493735
    Change-Id: I182473274668b83e16757662765305e59ff24ccd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5170135
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243106}

--- chrome/browser/vr/browser_renderer.cc
+++ chrome/browser/vr/browser_renderer.cc
@@ -58,9 +58,7 @@ void BrowserRenderer::Draw(FrameType frame_type,
       graphics_delegate_->GetRenderInfo(frame_type, head_pose);
   UpdateUi(render_info, current_time, frame_type);
 
-  graphics_delegate_->InitializeBuffers();
   if (frame_type == kWebXrFrame) {
-    DrawWebXr();
     if (ui_->HasWebXrOverlayElementsToDraw()) {
       DrawWebXrOverlay(render_info);
     }
@@ -76,17 +74,6 @@ void BrowserRenderer::Draw(FrameType frame_type,
   scheduler_delegate_->SubmitDrawnFrame(frame_type, head_pose);
 }
 
-void BrowserRenderer::DrawWebXr() {
-  TRACE_EVENT0("gpu", __func__);
-  graphics_delegate_->PrepareBufferForWebXr();
-
-  int texture_id;
-  GraphicsDelegate::Transform uv_transform;
-  graphics_delegate_->GetWebXrDrawParams(&texture_id, &uv_transform);
-  ui_->DrawWebXr(texture_id, uv_transform);
-  graphics_delegate_->OnFinishedDrawingBuffer();
-}
-
 void BrowserRenderer::DrawWebXrOverlay(const RenderInfo& render_info) {
   TRACE_EVENT0("gpu", __func__);
   // Calculate optimized viewport and corresponding render info.
@@ -98,16 +85,12 @@ void BrowserRenderer::DrawWebXrOverlay(const RenderInfo& render_info) {
   const auto& webxr_overlay_render_info =
       graphics_delegate_->GetOptimizedRenderInfoForFovs(fovs);
 
-  graphics_delegate_->PrepareBufferForWebXrOverlayElements();
   ui_->DrawWebVrOverlayForeground(webxr_overlay_render_info);
-  graphics_delegate_->OnFinishedDrawingBuffer();
 }
 
 void BrowserRenderer::DrawBrowserUi(const RenderInfo& render_info) {
   TRACE_EVENT0("gpu", __func__);
-  graphics_delegate_->PrepareBufferForBrowserUi();
   ui_->Draw(render_info);
-  graphics_delegate_->OnFinishedDrawingBuffer();
 }
 
 void BrowserRenderer::OnPause() {
@@ -171,11 +154,7 @@ void BrowserRenderer::UpdateUi(const RenderInfo& render_info,
   ui_->OnBeginFrame(current_time, render_info.head_pose);
 
   if (ui_->SceneHasDirtyTextures()) {
-    if (!graphics_delegate_->RunInSkiaContext(base::BindOnce(
-            &UiInterface::UpdateSceneTextures, base::Unretained(ui_.get())))) {
-      browser_->ForceExitVr();
-      return;
-    }
+    ui_->UpdateSceneTextures();
   }
   ReportElementVisibilityStatusForTesting(timing_start);
 
