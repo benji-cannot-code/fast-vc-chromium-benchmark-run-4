4fdb8f56fc2b0c46d949c021da0ac56f8a6773e2
Kenichi Ishibashi <bashi@chromium.org>
2024-01-04 03:19:54 +0000

    Ignore disconnect signals when a QUIC connection migration is happening on the default network
    Suppose that both wifi and mobile networks are active and the default
    network is wifi. When wifi becomes unavailable, there could be two
    signals from NetworkChangeNotifier via NetworkObserver to trigger
    a QUIC connection migration:
    - OnNetworkDisconnected() to signal wifi becomes inactive
    - OnNetworkMadeDefault() to signal mobile becomes the default network
    
    The former doesn't require proving, but the latter requires proving.
    
    The order of these signals is not guaranteed. Before this CL,
    QuicChromiumClientSession didn't handle the case where
    OnNetworkMadeDefault() comes first and the OnNetworkDisconnected()
    comes next. There was no valid connection IDs until proving finished,
    but QuicChromiumClientSession::OnNetworkDisconnectedV2() tried to
    migrate immediately, resulting in a migration failure.
    
    To avoid the problem, ignore disconnect signals when a migration is
    happening on the default network.
    
    The existing QuicConnectionMigrationTest.Basic test will cover this
    change once it starts running on bots.
    
    Bug: 1509458
    Change-Id: I9b348ad82ef004800356f5b63c1f702c1d09ccca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5138419
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1242768}

--- net/quic/quic_chromium_client_session.h
+++ net/quic/quic_chromium_client_session.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/flat_map.h"
+#include "base/feature_list.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/raw_ptr_exclusion.h"
 #include "base/observer_list.h"
@@ -1118,6 +1119,15 @@ class NET_EXPORT_PRIVATE QuicChromiumClientSession
 
   base::WeakPtrFactory<QuicChromiumClientSession> weak_factory_{this};
 };
+
+namespace features {
+
+// When enabled, network disconnect signals don't trigger immediate migration
+// when there is an ongoing migration with probing.
+NET_EXPORT BASE_DECLARE_FEATURE(
+    kQuicMigrationIgnoreDisconnectSignalDuringProbing);
+
+}  // namespace features
 
 }  // namespace net
 
