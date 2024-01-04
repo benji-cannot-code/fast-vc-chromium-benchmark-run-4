72fd20bb6e4485dd3ee143de45e2b67567c5fc9b
Elmehdi Rahmaoui <erahmaoui@google.com>
2024-01-04 16:15:17 +0000

    [iOS] Add the label to display the number of remaining tabs of a group.
    The aim of this CL is to configure the bottom right view of a group cell
    to display the number of remaining tabs when the number of tabs in the
    group exceeds 7.
    
    See the screenshot: https://drive.google.com/file/d/1sHJj-4Sdy4uRhgDL04DnXemuvnJ3vBn8/view?usp=sharing&resourcekey=0-UqYgeSQRn75hfytM5fQbtw
    
    Change-Id: I197e08606e0957b33413289db9f380b1556cf264
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5149765
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242921}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_view_controller.mm
@@ -1721,9 +1721,9 @@ NSString* GroupGridCellAccessibilityIdentifier(NSUInteger index) {
       // can be updated manually to view the different group tab configurations.
       NSArray<GroupTabInfo*>* groupTabInfos = @[
         snapshotFavicon, snapshotFavicon, snapshotFavicon, snapshotFavicon,
-        snapshotFavicon, snapshotFavicon
+        snapshotFavicon, snapshotFavicon, snapshotFavicon
       ];
-      [cell configureWithGroupTabInfos:groupTabInfos];
+      [cell configureWithGroupTabInfos:groupTabInfos totalTabsCount:101];
     }
   }];
 
