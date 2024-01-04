11fa0698c2c02594e541e2fb7b1b9ac95965378f
Peter Boström <pbos@chromium.org>
2024-01-04 21:51:49 +0000

    Replace logging::LOG_WARNING with LOGGING_
    Old alias is deprecated. Will be removed in a separate CL to avoid
    broader rollbacks in case I missed one or this is inadvertently used
    outside src/.
    
    Bug: 849450
    Change-Id: Ice531cb20395fba9c68b51bff9d1eef6ff86e8ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5167918
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243124}

--- base/metrics/statistics_recorder_unittest.cc
+++ base/metrics/statistics_recorder_unittest.cc
@@ -756,7 +756,7 @@ TEST_P(StatisticsRecorderTest, GlobalCallbackCalled) {
 
 TEST_P(StatisticsRecorderTest, LogOnShutdownNotInitialized) {
   ResetVLogInitialized();
-  logging::SetMinLogLevel(logging::LOG_WARNING);
+  logging::SetMinLogLevel(logging::LOGGING_WARNING);
   InitializeStatisticsRecorder();
   EXPECT_FALSE(VLOG_IS_ON(1));
   EXPECT_FALSE(IsVLogInitialized());
@@ -766,7 +766,7 @@ TEST_P(StatisticsRecorderTest, LogOnShutdownNotInitialized) {
 
 TEST_P(StatisticsRecorderTest, LogOnShutdownInitializedExplicitly) {
   ResetVLogInitialized();
-  logging::SetMinLogLevel(logging::LOG_WARNING);
+  logging::SetMinLogLevel(logging::LOGGING_WARNING);
   InitializeStatisticsRecorder();
   EXPECT_FALSE(VLOG_IS_ON(1));
   EXPECT_FALSE(IsVLogInitialized());
