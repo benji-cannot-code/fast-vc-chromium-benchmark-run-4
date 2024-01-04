8650ac777d866a9c74f7e0bd59b132c3d122e7ea
Luciano Pacheco <lucmult@chromium.org>
2024-01-04 02:39:58 +0000

    Files TS: Add some ES lint rules
    Copy some lint rules from:
    chrome/browser/resources/ash/settings/.eslintrc.js
    chrome/browser/resources/.eslintrc.js
    
    The lints are intended to:
    - Enforce using === / !== instead of == / !=
    - Enforce removing redundant type, rule "no-inferrable-type", following
    the style guide
    - Enforce preferring arrow function when applicable
    
    Bug: b:289003444
    Change-Id: I25075ab70003d209aeae6ec6fc4114757af47bd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5092308
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242758}

--- ui/file_manager/.eslintrc.js
+++ ui/file_manager/.eslintrc.js
@@ -4,20 +4,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 module.exports = {
+  // Disable clang-format because it produces odd formatting for these rules.
+  // clang-format off
   'rules' : {
+    'no-fallthrough' : 'error',
+    'eqeqeq' : ['error', 'always', {'null' : 'ignore'}],
     'no-console' : 'off',
+    /**
+     * https://google.github.io/styleguide/tsguide.html#type-inference
+     */
+    '@typescript-eslint/no-inferrable-types': [
+      'error',
+      {
+        // Function parameters may have explicit types for clearer APIs.
+        ignoreParameters: true,
+        // Class properties may have explicit types for clearer APIs.
+        ignoreProperties: true,
+      },
+    ],
+    /**
+     * https://google.github.io/styleguide/tsguide.html#function-expressions
+     */
+    'prefer-arrow-callback': 'error',
   },
-
-  'overrides' : [{
-                'files' : ['**/*.ts'],
-                'parser' : '../../third_party/node/node_modules/@typescript-eslint/parser/dist/index.js',
-                'plugins' :
-                          [
-                            '@typescript-eslint',
-                          ],
-                'rules' : {
-                  // rule override goes here.
-                  'no-fallthrough' : 'error',
-                },
-              }],
+  // clang-format on
 };
