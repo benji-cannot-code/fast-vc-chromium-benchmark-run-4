f2daef27e34db6545b23a14f60e65116fa33a53e
Guillem Perez <guiperez@google.com>
2024-01-04 00:05:05 +0000

    [iOS] Refactored method to close the promo from the main thread.
    This CL refactors a method to close the promo from the main thread and
    makes the code cleaner with one less if statement. Adds the pref update to the main thread as well. Otherwise it crashes.
    
    Low-Coverage-Reason: TRIVIAL_CHANGE
    Bug: b/316952831
    Change-Id: I25811bec4ad4d72e05431018dc9456886b36ada8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5161053
    Auto-Submit: Guillem Perez <guiperez@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Guillem Perez <guiperez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1242695}

--- ios/chrome/browser/ui/ntp/feed_top_section/feed_top_section_mediator.mm
+++ ios/chrome/browser/ui/ntp/feed_top_section/feed_top_section_mediator.mm
@@ -146,16 +146,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)notificationsPromoViewMainButtonWasTapped {
   // Show the Notifications promo alert.
-  PushNotificationService* service =
-      GetApplicationContext()->GetPushNotificationService();
-  id<SystemIdentity> identity = self.authenticationService->GetPrimaryIdentity(
-      signin::ConsentLevel::kSignin);
   __weak FeedTopSectionMediator* weakSelf = self;
   // Request displaying the OS notifications permission prompt.
   [PushNotificationUtil requestPushNotificationPermission:^(
                             BOOL granted, BOOL promptShown, NSError* error) {
     if (error) {
-      [self updateFeedTopSectionWhenClosed];
+      [self closeNotificationPromoAndEnablePref:NO];
       return;
     }
     if (!promptShown && !granted) {
@@ -166,48 +162,50 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       dispatch_async(dispatch_get_main_queue(), ^{
         [weakSelf
                 .notificationsPresenter presentPushNotificationPermissionAlert];
-        [self updateFeedTopSectionWhenClosed];
       });
       return;
     }
     if (promptShown && granted) {
       // If the OS prompt is shown and the user granted notifications access,
       // save the preference and close the promo.
-      // This callback can be executed on a background thread, make sure the UI
-      // is updated on the main thread.
-      dispatch_async(dispatch_get_main_queue(), ^{
-        service->SetPreference(identity.gaiaID,
-                               PushNotificationClientId::kContent, true);
-        [self updateFeedTopSectionWhenClosed];
-      });
-      return;
-    }
-    if (promptShown && !granted) {
-      // If the OS prompt is shown and the user denied notifications access,
-      // close the promo.
-      // This callback can be executed on a background thread, make sure the UI
-      // is updated on the main thread.
-      dispatch_async(dispatch_get_main_queue(), ^{
-        [self updateFeedTopSectionWhenClosed];
-      });
+      [self closeNotificationPromoAndEnablePref:YES];
       return;
     }
     if (!promptShown && granted) {
       // If the OS prompt has been previously shown but notifications are not
       // active on Chrome activate the notifications. This is an edge case.
-      // This callback can be executed on a background thread, make sure the UI
-      // is updated on the main thread.
-      dispatch_async(dispatch_get_main_queue(), ^{
-        service->SetPreference(identity.gaiaID,
-                               PushNotificationClientId::kContent, true);
-        [self updateFeedTopSectionWhenClosed];
-      });
+      [self closeNotificationPromoAndEnablePref:YES];
+      return;
+    }
+    if (promptShown && !granted) {
+      // If the OS prompt is shown and the user denied notifications access,
+      // close the promo.
+      [self closeNotificationPromoAndEnablePref:NO];
       return;
     }
   }];
 }
 
 #pragma mark - Private
+
+// Helper method to close the promo on the main thread. Takes `enablePref` as a
+// parameter which toggles the pref ON only.
+- (void)closeNotificationPromoAndEnablePref:(BOOL)enablePref {
+  // This callback can be executed on a background thread, make sure the UI
+  // is updated on the main thread.
+  dispatch_async(dispatch_get_main_queue(), ^{
+    if (enablePref) {
+      PushNotificationService* service =
+          GetApplicationContext()->GetPushNotificationService();
+      id<SystemIdentity> identity =
+          self.authenticationService->GetPrimaryIdentity(
+              signin::ConsentLevel::kSignin);
+      service->SetPreference(identity.gaiaID,
+                             PushNotificationClientId::kContent, true);
+    }
+    [self updateFeedTopSectionWhenClosed];
+  });
+}
 
 // Handles closing the promo, and the NTP and Feed Top Section layout when the
 // promo is closed.
