04f68336100cc97b0d5e8f23072c6b151a1aa0ce
Peilin Wang <peilinwang@google.com>
2024-01-04 23:44:39 +0000

    [AndroidVisibleUrlTruncation] Enable by default
    After ramping up to 50% stable, ANRs from scrollToTLD have dropped
    significantly.
    
    Bug: 1462512
    Change-Id: I1ff4e9f46c45fa1b1f2ca64d7213d40e0dc288be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5170371
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Peilin Wang <peilinwang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1243148}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -418,7 +418,7 @@ BASE_FEATURE(kAndroidNoVisibleHintForTablets,
 
 BASE_FEATURE(kAndroidVisibleUrlTruncation,
              "AndroidVisibleUrlTruncation",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAnimatedImageDragShadow,
              "AnimatedImageDragShadow",
