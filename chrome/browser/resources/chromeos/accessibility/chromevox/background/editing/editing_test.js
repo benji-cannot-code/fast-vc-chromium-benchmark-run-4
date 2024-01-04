b40b8c2611478dc80dc0b28042fd2eb020989413
Anastasia Helfinstein <anastasi@google.com>
2024-01-04 18:25:31 +0000

    [ChromeVox] Rename AutomationRichEditableText to RichEditableText
    AX-Relnotes: n/a.
    Bug: b/267329383
    Change-Id: Idb8a75fef00312033fd323609364db5b133b0f26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5166852
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243002}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/editing/editing_test.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/editing/editing_test.js
@@ -1908,7 +1908,7 @@ AX_TEST_F(
       state = {editable: true};
       handler = new TextEditHandler(input);
       assertEquals(
-          'AutomationRichEditableText', handler.editableText_.constructor.name,
+          'RichEditableText', handler.editableText_.constructor.name,
           'Incorrect object for textarea html tag.');
 
       // A rich editable via state.
@@ -1917,7 +1917,7 @@ AX_TEST_F(
       state = {editable: true, richlyEditable: true};
       handler = new TextEditHandler(input);
       assertEquals(
-          'AutomationRichEditableText', handler.editableText_.constructor.name,
+          'RichEditableText', handler.editableText_.constructor.name,
           'Incorrect object for richly editable state.');
 
       // A rich editable via contenteditable. (aka <div contenteditable>).
@@ -1926,7 +1926,7 @@ AX_TEST_F(
       state = {editable: true};
       handler = new TextEditHandler(input);
       assertEquals(
-          'AutomationRichEditableText', handler.editableText_.constructor.name,
+          'RichEditableText', handler.editableText_.constructor.name,
           'Incorrect object for content editable.');
 
       // A rich editable via contenteditable. (aka <div
@@ -1936,7 +1936,7 @@ AX_TEST_F(
       state = {editable: true};
       handler = new TextEditHandler(input);
       assertEquals(
-          'AutomationRichEditableText', handler.editableText_.constructor.name,
+          'RichEditableText', handler.editableText_.constructor.name,
           'Incorrect object for content editable true.');
 
       // Note that it is not possible to have <div
@@ -2217,8 +2217,7 @@ AX_TEST_F('ChromeVoxEditingTest', 'TablesWithEmptyCells', async function() {
       .expectSpeech('A', 'selected')
 
       // Non-breaking spaces (\u00a0) get preprocessed later by PrimaryTts
-      // to ' '. This comes as part of speak line output in
-      // AutomationRichEditableText.
+      // to ' '. This comes as part of speak line output in RichEditableText.
       .call(doCmd('nativeNextCharacter'))
       .call(() => textField.setSelection(1, 1))
       .expectSpeech('\u00a0', 'row 1 column 1')
