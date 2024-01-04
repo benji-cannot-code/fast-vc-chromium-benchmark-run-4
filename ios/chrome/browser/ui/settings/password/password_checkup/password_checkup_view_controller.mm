560b843ac31cf96fb15dba6a6315472233b208c1
Rafał Godlewski <rgod@google.com>
2024-01-04 19:13:53 +0000

    [iOS] Remove obsolete password checkup vc TODOs
    The bug was marked as WontFix.
    
    Bug: 1453276
    Change-Id: I513d6f5c7a79dc14c9a05f651b921c2360858368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5168685
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1243032}

--- ios/chrome/browser/ui/settings/password/password_checkup/password_checkup_view_controller.mm
+++ ios/chrome/browser/ui/settings/password/password_checkup/password_checkup_view_controller.mm
@@ -421,7 +421,6 @@ void SetUpTrailingIconAndAccessoryType(
   [self updatePasswordCheckupTimestampDetailText];
 }
 
-// TODO(crbug.com/1453276): Make the coordinator present the alert instead.
 - (void)showErrorDialogWithMessage:(NSString*)message {
   NSString* title = l10n_util::GetNSString(
       IDS_IOS_PASSWORD_CHECKUP_HOMEPAGE_ERROR_DIALOG_TITLE);
@@ -434,8 +433,6 @@ void SetUpTrailingIconAndAccessoryType(
       [UIAlertAction actionWithTitle:l10n_util::GetNSString(IDS_OK)
                                style:UIAlertActionStyleDefault
                              handler:nil];
-  // TODO(crbug.com/1453276): Once fixed, setting the accessibilityIdentifier
-  // will no longer be neeeded since it will be handled by the AlertCoordinator.
   okAction.accessibilityIdentifier =
       [l10n_util::GetNSString(IDS_OK) stringByAppendingString:@"AlertAction"];
   [alert addAction:okAction];
