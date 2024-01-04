8b079e7288d3093b5ce96f3a467fbcfda64ff66a
Sylvie Liu <sylvieliu@google.com>
2024-01-04 00:44:18 +0000

    Change the calendar default view to show more dates
    This CL changed the calendar default view so that there are at least two
    more rows under today's row. Here are some cases:
    - If there are already at least two more rows under today's row, do
      nothing.
    - If there are less than two rows under today's row:
      - If the calendar bubble is big enough, scroll up today's row so that
        today's row is visible and there are two rows under today's row.
      - If the calendar bubble is small, scroll to today's row.
    - If the up next view exists:
      - If the calendar bubble is big enough, scroll up today's row so that
        today's row is visible and there are two rows under today's row
        that is not hidden by the up next view.
      - If the calendar bubble is small or today's date is hidden by the up
        next view, scroll to today's row.
    
    Screenshot:
    https://screenshot.googleplex.com/9kM86nKhCoyNmCW
    
    Bug: b:306483442
    Change-Id: I7f35f99e3459fe89498c1ca98e566b83785ee7d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5124059
    Commit-Queue: Sylvie Liu <sylvieliu@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242720}

--- ash/glanceables/glanceables_pixeltest.cc
+++ ash/glanceables/glanceables_pixeltest.cc
@@ -116,7 +116,7 @@ TEST_F(GlanceablesPixelTest, GlanceablesZeroState) {
   GetGlanceableTrayBubble()->GetTasksView()->ScrollViewToVisible();
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
-      "glanceables_zero_state", /*revision_number=*/6,
+      "glanceables_zero_state", /*revision_number=*/7,
       GetGlanceableTrayBubble()->GetBubbleView()));
 }
 
@@ -188,7 +188,7 @@ TEST_F(GlanceablesPixelTest, GlanceablesCalendarHeight) {
   ASSERT_TRUE(GetDateTray()->is_active());
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
-      "glanceables_calendar_height", /*revision_number=*/1,
+      "glanceables_calendar_height", /*revision_number=*/2,
       GetGlanceableTrayBubble()->GetBubbleView()));
 }
 
