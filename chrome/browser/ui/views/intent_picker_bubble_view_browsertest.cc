107cd4b7b699f905b47642ae58c5f72aa4022884
Tim Sergeant <tsergeant@chromium.org>
2024-01-04 10:08:45 +0000

    Link Capturing: Update tracking bug for failing Intent Picker mac tests
    Bug: 1515480
    Change-Id: I8fc6bb5b059b48e48d345ab41f99da89c054882e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5167055
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242826}

--- chrome/browser/ui/views/intent_picker_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/intent_picker_bubble_view_browsertest.cc
@@ -166,7 +166,7 @@ IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
 
 // Tests that clicking a link from a tabbed browser to within the scope of an
 // installed app shows the intent picker icon in Omnibox.
-// TODO(crbug.com/1427908): Flaky on Mac.
+// TODO(crbug.com/1515480): Flaky on Mac.
 #if BUILDFLAG(IS_MAC)
 #define MAYBE_NavigationToInScopeLinkShowsIntentPicker \
   DISABLED_NavigationToInScopeLinkShowsIntentPicker
@@ -196,7 +196,7 @@ IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
   EXPECT_TRUE(intent_picker_icon->GetVisible());
 }
 
-// TODO(crbug.com/1395393): This test is flaky on Mac.
+// TODO(crbug.com/1515480): This test is flaky on Mac.
 #if BUILDFLAG(IS_MAC)
 #define MAYBE_IconVisibilityAfterTabSwitching \
   DISABLED_IconVisibilityAfterTabSwitching
@@ -255,9 +255,9 @@ IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
   EXPECT_TRUE(intent_picker_icon->GetVisible());
 }
 
-// TODO(crbug.com/1399441): This test is flaky. Re-enable this test.
 // Tests that the intent picker icon is not visible if the navigation redirects
 // to a URL that doesn't have an installed PWA.
+// TODO(crbug.com/1515480): This test is flaky. Re-enable this test.
 #if BUILDFLAG(IS_MAC)
 #define MAYBE_DoesNotShowIntentPickerWhenRedirectedOutOfScope \
   DISABLED_DoesNotShowIntentPickerWhenRedirectedOutOfScope
@@ -289,7 +289,7 @@ IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
 
 // Test that navigating to service pages (chrome://) will hide the intent picker
 // icon.
-// TODO(crbug.com/1478654): Re-enable this test
+// TODO(crbug.com/1515480): Re-enable this test
 #if BUILDFLAG(IS_MAC)
 #define MAYBE_DoNotShowIconAndBubbleOnServicePages \
   DISABLED_DoNotShowIconAndBubbleOnServicePages
@@ -325,8 +325,8 @@ IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
 }
 
 // Test that error pages do not show the intent picker icon.
+// TODO(https://crbug.com/1515480): Fix the test.
 #if BUILDFLAG(IS_MAC)
-// TODO(https://crbug.com/1478654): Fix the test.
 #define MAYBE_DoNotShowIconOnErrorPages DISABLED_DoNotShowIconOnErrorPages
 #else
 #define MAYBE_DoNotShowIconOnErrorPages DoNotShowIconOnErrorPages
@@ -362,7 +362,7 @@ IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
 
 // Test that loading a page with pushState() call that changes URL updates the
 // intent picker view.
-// TODO(crbug.com/1484208): Re-enable this test
+// TODO(crbug.com/1515480): Re-enable this test
 #if BUILDFLAG(IS_MAC)
 #define MAYBE_PushStateURLChangeTest DISABLED_PushStateURLChangeTest
 #else
