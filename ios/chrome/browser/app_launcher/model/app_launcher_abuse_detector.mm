469e7ca658841855666cae908028dea55049cae3
Ali Juma <ajuma@chromium.org>
2024-01-04 16:06:03 +0000

    [iOS] Prevent launching Chrome from within Chrome
    AppLauncherAbuseDetector has logic to block URLs that try to launch
    Chrome from within Chrome, since these are typically only used for
    abuse (legitimate use cases for these URLs all involve other apps
    wanting to launch Chrome, not random websites trying to launch
    Chrome from within Chrome).
    
    This logic was unintentionally bypassed by the changes in
    https://crrev.com/c/4936008. This CL moves this logic to
    AppLauncherTabHelper, merging it with existing logic to block the
    same kinds of URLs that was previously specific to iframes. This
    CL also moves the histogram that was recorded for these URLs in
    AppLauncherAbuseDetector to AppLauncherTabHelper.
    
    Bug: 1515137
    Change-Id: Iedb1398ed40f208d8ce4d31a44d5620388181e30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5165632
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242904}

--- ios/chrome/browser/app_launcher/model/app_launcher_abuse_detector.mm
+++ ios/chrome/browser/app_launcher/model/app_launcher_abuse_detector.mm
@@ -5,25 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/app_launcher/model/app_launcher_abuse_detector.h"
 
-#import "base/metrics/histogram_macros.h"
 #import "base/strings/sys_string_conversions.h"
 #import "ios/chrome/browser/app_launcher/model/app_launching_state.h"
 #import "url/gurl.h"
 
 const int kMaxAllowedConsecutiveExternalAppLaunches = 2;
-
-namespace {
-
-// Returns true iff `url`'s scheme is one that is used by other apps to launch
-// Chrome.
-bool HasChromeAppLaunchScheme(const GURL& url) {
-  return url.SchemeIs("googlechrome-x-callback") ||
-         url.SchemeIs("chromium-x-callback") || url.SchemeIs("googlechrome") ||
-         url.SchemeIs("chromium") || url.SchemeIs("googlechromes") ||
-         url.SchemeIs("chromiums");
-}
-
-}  // namespace
 
 @interface AppLauncherAbuseDetector ()
 // Maps between external application redirection key and state.
@@ -67,14 +53,6 @@ bool HasChromeAppLaunchScheme(const GURL& url) {
 
 - (ExternalAppLaunchPolicy)launchPolicyForURL:(const GURL&)URL
                             fromSourcePageURL:(const GURL&)sourcePageURL {
-  // Treat an attempt to launch Chrome from within Chrome as abuse.
-  bool isChromeLaunchAttempt = HasChromeAppLaunchScheme(URL);
-  UMA_HISTOGRAM_BOOLEAN("IOS.AppLauncher.AppURLHasChromeLaunchScheme",
-                        isChromeLaunchAttempt);
-  if (isChromeLaunchAttempt) {
-    return ExternalAppLaunchPolicyBlock;
-  }
-
   NSString* key = [[self class] stateKeyForAppURL:URL sourceURL:sourcePageURL];
   // Don't block apps that are not registered with the abuse detector.
   if (!_appLaunchingStates[key]) {
