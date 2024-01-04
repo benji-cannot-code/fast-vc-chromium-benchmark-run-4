f9db8177430218bded27cd5acc5c3c0a426e13e0
Gauthier Ambard <gambard@chromium.org>
2024-01-04 16:59:33 +0000

    [iOS] Remove items from BaseGridViewController
    Remove the last usages, mostly around model mutation.
    
    Fixed: 1513100
    Change-Id: Idecac89c3a512bef481f6933768da14065316f12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5153170
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1242957}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_view_controller+Testing.h
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_view_controller+Testing.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
+#import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_view_controller+subclassing.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_view_controller.h"
 
 @class TabSwitcherItem;
@@ -15,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Testing category to expose private properties used for tests only.
 @interface BaseGridViewController (Testing)
 
-@property(nonatomic, readonly) NSMutableArray<TabSwitcherItem*>* items;
+@property(nonatomic, readonly) GridDiffableDataSource* diffableDataSource;
 @property(nonatomic, readonly) NSUInteger selectedIndex;
 @property(nonatomic, readonly) UICollectionView* collectionView;
 
