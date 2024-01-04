3ede9c449ebb43f4383e832c65f07a1e9d349503
Maksim Moskvitin <mmoskvitin@google.com>
2024-01-04 16:16:25 +0000

    [TrustedVault] Remove some stale TODOs
    There is a decent test coverage now, e.g.
    TrustedVaultKeysSharingLacrosBrowserTest and
    AshTrustedVaultKeysSharingSyncTest.
    
    Bug: 1434667
    Change-Id: I4693258e1a88c7499f64744a21ba91e23de48cc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5169347
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Auto-Submit: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1242923}

--- chrome/browser/lacros/trusted_vault/crosapi_trusted_vault_client.h
+++ chrome/browser/lacros/trusted_vault/crosapi_trusted_vault_client.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Implements TrustedVaultClient interface by plumbing calls to Ash-side
 // TrustedVaultClient via Crosapi.
-// TODO(crbug.com/1434667): Add coverage by browser tests.
 class CrosapiTrustedVaultClient
     : public trusted_vault::TrustedVaultClient,
       public crosapi::mojom::TrustedVaultBackendObserver {
