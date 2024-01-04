e9f359927524d52669118d977c3fd63f5b8e0c0e
David Bertoni <dbertoni@chromium.org>
2024-01-04 10:09:24 +0000

    [Code Health] Remove a use of base::SupportsWeakPtr. (invalidation)
    Bug: 647430
    Change-Id: Ib524b3be628661a0261b1e5c5398a7ae9f99d07a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5166235
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Roland Bock <rbock@google.com>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1242827}

--- components/invalidation/impl/fake_ack_handler.h
+++ components/invalidation/impl/fake_ack_handler.h
@@ -21,9 +21,7 @@ class Invalidation;
 
 // This AckHandler implementation colaborates with the FakeInvalidationService
 // to enable unit tests to assert that invalidations are being acked properly.
-class INVALIDATION_EXPORT FakeAckHandler
-    : public AckHandler,
-      public base::SupportsWeakPtr<FakeAckHandler> {
+class INVALIDATION_EXPORT FakeAckHandler final : public AckHandler {
  public:
   FakeAckHandler();
   ~FakeAckHandler() override;
@@ -61,6 +59,8 @@ class INVALIDATION_EXPORT FakeAckHandler
   InvalidationVector acked_invalidations_;
 
   std::map<Topic, AckHandle> unrecovered_drop_events_;
+
+  base::WeakPtrFactory<FakeAckHandler> weak_ptr_factory_{this};
 };
 
 }  // namespace invalidation
