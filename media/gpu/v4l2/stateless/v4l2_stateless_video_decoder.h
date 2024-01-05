a5872e39aa7f59ff61c5914ce85da55226dd026f
Fritz Koenig <frkoenig@chromium.org>
2024-01-05 20:54:51 +0000

    v4l2/stateless: Flush buffers on EOS
    An EOS packet can come at two different times.
    
    a. When there are no outstanding requests the
       callback can be run right away.
    
    b. When buffers are still in the pipeline then
       the callback must be delayed until they are
       done being processed. This requires keeping
       track of the timestamps of the last frame
       enqueued and the last frame dequeued
    
    Cq-Include-Trybots: luci.chromium.try:linux-v4l2-codec-rel
    Bug: b:278935312
    
    Change-Id: I77b4c06b08022fb7c960009d03ef811d83de7f9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5174070
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Commit-Queue: Fritz Koenig <frkoenig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243596}

--- media/gpu/v4l2/stateless/v4l2_stateless_video_decoder.h
+++ media/gpu/v4l2/stateless/v4l2_stateless_video_decoder.h
@@ -140,9 +140,6 @@ class MEDIA_GPU_EXPORT V4L2StatelessVideoDecoder
   // need to be queued up as there may not be free input buffers available.
   void ServiceDecodeRequestQueue();
 
-  // Clear all of the pending decode buffers.
-  void Flush(DecodeCB decode_cb);
-
   SEQUENCE_CHECKER(decoder_sequence_checker_);
 
   const scoped_refptr<StatelessDevice> device_;
@@ -152,7 +149,7 @@ class MEDIA_GPU_EXPORT V4L2StatelessVideoDecoder
   OutputCB output_cb_ GUARDED_BY_CONTEXT(decoder_sequence_checker_);
 
   // Hold the callback that came in with the EOS signal until the rest of the
-  // frames have finished decoding._;
+  // frames have finished decoding.
   DecodeCB flush_cb_ GUARDED_BY_CONTEXT(decoder_sequence_checker_);
 
   // Video decoder used to parse stream headers by software.
@@ -171,11 +168,6 @@ class MEDIA_GPU_EXPORT V4L2StatelessVideoDecoder
   // Aspect ratio from config to use for output frames.
   VideoAspectRatio aspect_ratio_;
 
-  // Signals that there weren't enough surfaces to decode, so
-  // |ServiceDecodeRequestQueue| should be called after dequeuing the next input
-  // buffer.
-  bool paused_waiting_for_surface_ = false;
-
   // Int32 safe ID generator, starting at 0. Generated IDs are used to uniquely
   // identify a Decode() request for stateless backends. BitstreamID is just
   // a "phantom type" (see StrongAlias), essentially just a name.
@@ -188,6 +180,13 @@ class MEDIA_GPU_EXPORT V4L2StatelessVideoDecoder
   struct FrameID {};
   base::IdTypeU64<FrameID>::Generator frame_id_generator_
       GUARDED_BY_CONTEXT(decoder_sequence_checker_);
+
+  // On an EOS the decoder must make sure that all the frames have made it
+  // through the queue. To do that the |last_frame_id_generated_| holds the id
+  // of the last input buffer while |last_frame_id_dequeued_| holds the id of
+  // the last output buffer.
+  uint64_t last_frame_id_generated_;
+  uint64_t last_frame_id_dequeued_;
 
   base::LRUCache<int32_t, base::TimeDelta> bitstream_id_to_timestamp_;
 
