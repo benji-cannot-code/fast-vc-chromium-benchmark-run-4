b51f1bd2fe554c583ef1a48c7529c8dd3a2e0e46
Alex Newcomer <newcomer@google.com>
2024-01-05 16:29:38 +0000

    Update rounding behavior for battery fill level
    There are only 10 dips to play with for battery fill level.
    
    The old behavior always rounds down (floor rounding) so a charge
    level of 29% and a charge level of 20% both only have 2 pixels of
    fill height.
    
    UX prefers we normally round below 50% battery and floor round
    otherwise. This means that a charge level of 25% would have 3 pixels
    of fill, so it would look more accurate at lower battery levels.
    
    Bug: b:302345829
    Change-Id: I21fa6854953ede757dc63d045e11fe69b565bc6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5171985
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243452}

--- ash/system/power/battery_image_source.cc
+++ ash/system/power/battery_image_source.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/power/battery_image_source.h"
 
 #include <algorithm>
+#include <cmath>
 
 #include "ash/constants/ash_features.h"
 #include "ash/resources/vector_icons/vector_icons.h"
@@ -68,9 +69,17 @@ void BatteryImageSource::Draw(gfx::Canvas* canvas) {
   // |charge_level| is a value between 0 and the visual height of the icon
   // representing the number of device pixels the battery image should be
   // shown charged. The exception is when |charge_level| is very low; in this
-  // case, still draw 1dip of charge.
-  float charge_level =
-      std::floor(info_.charge_percent / 100.0 * icon_bounds.height());
+  // case, still draw 1 dip of charge. There are only 10 dips to indicate charge
+  // level. If the level is always floor rounded (as was the historical
+  // behavior) 25% charge looks very low. Similarly, always rounding normally
+  // makes 75% look abnormally high. To help mitigate this, UX prefers floor
+  // rounding above 50%, and normal rounding below 50%.
+  const float unrounded_charge_level =
+      info_.charge_percent / 100.0 * icon_bounds.height();
+  float charge_level = info_.charge_percent <= 50
+                           ? std::round(unrounded_charge_level)
+                           : std::floor(unrounded_charge_level);
+
   const float min_charge_level = dsf * kMinVisualChargeLevel;
   charge_level =
       std::clamp(charge_level, min_charge_level, icon_bounds.height());
