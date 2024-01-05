74c8b3cc61d04c2d1ac080627922e1a39d70dad2
David Black <dmblack@google.com>
2024-01-05 20:30:38 +0000

    Restore palette tools related histograms.
    This CL restores palette tools related histograms that were removed
    due to expiration/obsoletion. Per details in bug, these histograms are
    being restored to establish a baseline for some upcoming changes.
    
    This CL is a partial revert of the following CLs:
    * https://chromium-review.googlesource.com/c/chromium/src/+/5010348
    * https://chromium-review.googlesource.com/c/chromium/src/+/5008551
    * https://chromium-review.googlesource.com/c/chromium/src/+/4994207
    * https://chromium-review.googlesource.com/c/chromium/src/+/4903166
    * https://chromium-review.googlesource.com/c/chromium/src/+/4853123
    * https://chromium-review.googlesource.com/c/chromium/src/+/4300430
    * https://chromium-review.googlesource.com/c/chromium/src/+/4300449
    
    Bug: b:318727009
    Change-Id: Ibc5c75ff6d8f2227504cc9b33c033808fccb912e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5167926
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1243587}

--- ash/system/palette/common_palette_tool.h
+++ ash/system/palette/common_palette_tool.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/palette/palette_tool.h"
 #include "ash/system/tray/view_click_listener.h"
 #include "base/memory/raw_ptr.h"
+#include "base/time/time.h"
 
 namespace gfx {
 struct VectorIcon;
@@ -33,6 +34,7 @@ class CommonPaletteTool : public PaletteTool, public ViewClickListener {
   // PaletteTool:
   void OnViewDestroyed() override;
   void OnEnable() override;
+  void OnDisable() override;
 
   // ViewClickListener:
   void OnViewClicked(views::View* sender) override;
@@ -48,6 +50,11 @@ class CommonPaletteTool : public PaletteTool, public ViewClickListener {
   views::View* CreateDefaultView(const std::u16string& name);
 
   raw_ptr<HoverHighlightView, DanglingUntriaged> highlight_view_ = nullptr;
+
+ private:
+  // `start_time_` is initialized when the tool becomes active.
+  // Used for recording UMA metrics.
+  base::TimeTicks start_time_;
 };
 
 }  // namespace ash
--- ash/system/palette/palette_tool_manager_unittest.cc
+++ ash/system/palette/palette_tool_manager_unittest.cc
@@ -66,6 +66,9 @@ class PaletteToolManagerTest : public ::testing::Test,
     NOTREACHED();
     return nullptr;
   }
+  void RecordPaletteOptionsUsage(PaletteTrayOptions option,
+                                 PaletteInvocationMethod method) override {}
+  void RecordPaletteModeCancellation(PaletteModeCancelType type) override {}
 
   // PaletteTool::Delegate:
   void EnableTool(PaletteToolId tool_id) override {}
