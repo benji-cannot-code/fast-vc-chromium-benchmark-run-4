f19fc818d7a90e24e91611a67a94af1365ebb35a
Nishant Joshi <nishantj@google.com>
2024-01-05 19:15:20 +0000

    Change docstring to use a raw string since it uses unescaped backslashes.
    This gets rid of DeprecationWarning messages about using an invalid escape sequence. (\o)
    
    Change-Id: Ie4b01f532a2af1a42e355fc4b849435d85d4de4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5117467
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Nishant Joshi <nishantj@google.com>
    Reviewed-by: Hao Wu <haowoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1243532}

--- tools/variations/split_variations_cmd.py
+++ tools/variations/split_variations_cmd.py
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""A script to split Chrome variations into two sets.
+r"""A script to split Chrome variations into two sets.
 
 Chrome runs with many experiments and variations (field trials) that are
 randomly selected based on a configuration from a server. They lead to
