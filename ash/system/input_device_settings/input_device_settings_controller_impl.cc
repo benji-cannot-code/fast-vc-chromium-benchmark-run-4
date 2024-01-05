9ef029c0571ad836a6806e246fbb6da1ddcf509b
David Padlipsky <dpad@google.com>
2024-01-05 00:56:34 +0000

    customization: Do not call pen buttons "Middle Button" for tablets
    If a graphics tablet pen button is a middle click, it should not be
    named "Middle Button".
    
    Bug: b/241965717
    Change-Id: I54358760ea1c525b9f4826d44266183affef8826
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5122838
    Reviewed-by: Danny Wang <wangdanny@google.com>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1243215}

--- ash/system/input_device_settings/input_device_settings_controller_impl.cc
+++ ash/system/input_device_settings/input_device_settings_controller_impl.cc
@@ -247,10 +247,11 @@ bool IsGraphicsTabletPenButton(const mojom::Button& button) {
 
 void AddButtonToButtonRemappingList(
     const mojom::Button& button,
-    std::vector<mojom::ButtonRemappingPtr>& button_remappings) {
+    std::vector<mojom::ButtonRemappingPtr>& button_remappings,
+    bool is_mouse_button_remapping) {
   std::string button_name;
   // If its a middle click, give it the default middle button name.
-  if (button.is_customizable_button() &&
+  if (is_mouse_button_remapping && button.is_customizable_button() &&
       button.get_customizable_button() == mojom::CustomizableButton::kMiddle) {
     button_name = l10n_util::GetStringUTF8(
         IDS_SETTINGS_CUSTOMIZATION_MIDDLE_BUTTON_DEFAULT_NAME);
@@ -266,7 +267,7 @@ void AddButtonToButtonRemappingList(
                            });
 
     int button_number = button_remappings.size() + 1;
-    if (iter != button_remappings.end()) {
+    if (is_mouse_button_remapping && iter != button_remappings.end()) {
       --button_number;
     }
     button_name = l10n_util::GetStringFUTF8(
@@ -1808,7 +1809,8 @@ void InputDeviceSettingsControllerImpl::OnMouseButtonPressed(
     return;
   }
 
-  AddButtonToButtonRemappingList(button, button_remappings);
+  AddButtonToButtonRemappingList(button, button_remappings,
+                                 /*is_mouse_button_remapping=*/true);
   mouse_pref_handler_->UpdateMouseSettings(
       active_pref_service_, policy_handler_->mouse_policies(), mouse);
   DispatchCustomizableMouseButtonPressed(mouse, button);
@@ -1856,12 +1858,14 @@ void InputDeviceSettingsControllerImpl::OnGraphicsTabletButtonPressed(
   }
 
   if (IsGraphicsTabletPenButton(button)) {
-    AddButtonToButtonRemappingList(button, pen_button_remappings);
+    AddButtonToButtonRemappingList(button, pen_button_remappings,
+                                   /*is_mouse_button_remapping=*/false);
     DispatchCustomizablePenButtonPressed(graphics_tablet, button);
     metrics_manager_->RecordNewButtonRegisteredMetrics(
         button, kGraphicsTabletPenDeviceType);
   } else {
-    AddButtonToButtonRemappingList(button, tablet_button_remappings);
+    AddButtonToButtonRemappingList(button, tablet_button_remappings,
+                                   /*is_mouse_button_remapping=*/false);
     DispatchCustomizableTabletButtonPressed(graphics_tablet, button);
     metrics_manager_->RecordNewButtonRegisteredMetrics(
         button, kGraphicsTabletDeviceType);
