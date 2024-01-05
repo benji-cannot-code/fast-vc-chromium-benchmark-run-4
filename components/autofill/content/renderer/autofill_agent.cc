8c66e24dd61fd3250c2d4e66982d5bf5a96c1665
Jan Keitel <jkeitel@google.com>
2024-01-05 12:21:53 +0000

    Introduce test fixture for Autofill renderer tests.
    The test fixture takes care of setting up the Autofill and PWM agents
    and mocks the AutofillDriver that is the AutofillAgent's remote
    receiver.
    
    Bug: 63573
    Change-Id: I421e82d75630f0cb12c6990e7447a691c3ea50be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5171568
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1243371}

--- components/autofill/content/renderer/autofill_agent.cc
+++ components/autofill/content/renderer/autofill_agent.cc
@@ -113,7 +113,7 @@ namespace {
 
 // Time to wait in ms to ensure that only a single select or datalist change
 // will be acted upon, instead of multiple in close succession (debounce time).
-size_t kWaitTimeForOptionsChangesMs = 50;
+constexpr base::TimeDelta kWaitTimeForOptionsChanges = base::Milliseconds(50);
 
 using FormAndField = std::pair<FormData, FormFieldData>;
 
@@ -626,10 +626,10 @@ void AutofillAgent::OpenTextDataListChooser(const WebInputElement& element) {
 // Notifies the AutofillDriver about changes in the <datalist> options in
 // batches.
 //
-// A batch ends if no event occurred for `kWaitTimeForOptionsChangesMs`
-// milliseconds. For a given batch, the AutofillDriver is informed only about
-// the last field. That is, if within one batch the options of different
-// fields changed, all but one of these events will be lost.
+// A batch ends if no event occurred for `kWaitTimeForOptionsChanges`.
+// For a given batch, the AutofillDriver is informed only about the last field.
+// That is, if within one batch the options of different fields changed, all but
+// one of these events will be lost.
 void AutofillAgent::DataListOptionsChanged(const WebInputElement& element) {
   DCHECK(MaybeWasOwnedByFrame(element, unsafe_render_frame()));
 
@@ -643,7 +643,7 @@ void AutofillAgent::DataListOptionsChanged(const WebInputElement& element) {
   }
 
   datalist_option_change_batch_timer_.Start(
-      FROM_HERE, base::Milliseconds(kWaitTimeForOptionsChangesMs),
+      FROM_HERE, kWaitTimeForOptionsChanges,
       base::BindRepeating(&AutofillAgent::BatchDataListOptionChange,
                           weak_ptr_factory_.GetWeakPtr(), element));
 }
@@ -1211,26 +1211,24 @@ AutofillAgent::ProccessFormsAndReturnIssues() {
 
 void AutofillAgent::ExtractForms(base::OneShotTimer& timer,
                                  base::OnceCallback<void(bool)> callback) {
-  static constexpr base::TimeDelta kThrottle = base::Milliseconds(100);
   if (!is_dom_content_loaded_ || timer.IsRunning()) {
     if (!callback.is_null()) {
       std::move(callback).Run(/*success=*/false);
     }
     return;
   }
-  timer.Start(FROM_HERE, kThrottle,
+  timer.Start(FROM_HERE, kFormsSeenThrottle,
               base::BindOnce(&AutofillAgent::ExtractFormsUnthrottled,
                              base::Unretained(this), std::move(callback)));
 }
 
 void AutofillAgent::ExtractFormsForPasswordAutofillAgent(
     base::OneShotTimer& timer) {
-  static constexpr base::TimeDelta kThrottle = base::Milliseconds(100);
   if (!is_dom_content_loaded_ || timer.IsRunning()) {
     return;
   }
   timer.Start(
-      FROM_HERE, kThrottle,
+      FROM_HERE, kFormsSeenThrottle,
       base::BindOnce(
           &AutofillAgent::ExtractFormsUnthrottled, base::Unretained(this),
           base::BindOnce(
@@ -1341,10 +1339,10 @@ void AutofillAgent::SelectControlDidChange(
 // Notifies the AutofillDriver about changes in the <select> or <selectlist>
 // options in batches.
 //
-// A batch ends if no event occurred for `kWaitTimeForOptionsChangesMs`
-// milliseconds. For a given batch, the AutofillDriver is informed only about
-// the last FormData. That is, if within one batch the options of different
-// forms changed, all but one of these events will be lost.
+// A batch ends if no event occurred for `kWaitTimeForOptionsChanges`. For a
+// given batch, the AutofillDriver is informed only about the last FormData.
+// That is, if within one batch the options of different forms changed, all but
+// one of these events will be lost.
 void AutofillAgent::SelectOrSelectListFieldOptionsChanged(
     const blink::WebFormControlElement& element) {
   DCHECK(MaybeWasOwnedByFrame(element, unsafe_render_frame()));
@@ -1358,7 +1356,7 @@ void AutofillAgent::SelectOrSelectListFieldOptionsChanged(
   }
 
   select_or_selectlist_option_change_batch_timer_.Start(
-      FROM_HERE, base::Milliseconds(kWaitTimeForOptionsChangesMs),
+      FROM_HERE, kWaitTimeForOptionsChanges,
       base::BindRepeating(&AutofillAgent::BatchSelectOrSelectListOptionChange,
                           weak_ptr_factory_.GetWeakPtr(), element));
 }
