a105392dd75576581d1596eb28beac411cc87a60
Wes Okuhara <wesokuhara@google.com>
2024-01-05 23:32:12 +0000

    CrOS Settings: Convert FakeInputMethodPrivate to TS
    Bug: b:270728282
    Test: browser_tests --gtest_filter="OSSettings*"
    Change-Id: Id9310ccccb84a2dff0772f8085365ea6d0449df1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5171227
    Reviewed-by: Nikki Moteva <moteva@google.com>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1243667}

---
+++ chrome/test/data/webui/settings/chromeos/fake_input_method_private.ts
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Fake of the chrome.inputMethodsPrivate API for testing. Only
+ * methods that are called during testing have been implemented.
+ */
+
+export class FakeInputMethodPrivate {
+  getCurrentInputMethod(): Promise<null> {
+    return Promise.resolve(null);
+  }
+
+  setCurrentInputMethod(): Promise<void> {
+    return Promise.resolve();
+  }
+
+  getLanguagePackStatus():
+      Promise<chrome.inputMethodPrivate.LanguagePackStatus> {
+    return Promise.resolve(
+        chrome.inputMethodPrivate.LanguagePackStatus.UNKNOWN);
+  }
+
+  get onChanged() {
+    return {
+      addListener: () => {
+          // Nothing to do here.
+      },
+      removeListener: () => {
+          // Nothing to do here.
+      },
+    };
+  }
+}
