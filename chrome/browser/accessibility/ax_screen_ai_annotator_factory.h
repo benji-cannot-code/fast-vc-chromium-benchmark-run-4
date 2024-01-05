b84cc3687bfbdde112614de7ba12ed520f51dfff
Arthur Milchior <arthurmilchior@chromium.org>
2024-01-05 17:56:07 +0000

    Implements the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one being deprecated,
    this CL replaces it by the first one.
    
    This also ensures that, instead of applying `WrapUnique` to a `new` raw
    ptr, it directly uses `make_unique`.
    
    Change-Id: If275f477ee3ad42f6a729b3edc665d8adaffb5ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798157
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243488}

--- chrome/browser/accessibility/ax_screen_ai_annotator_factory.h
+++ chrome/browser/accessibility/ax_screen_ai_annotator_factory.h
@@ -36,7 +36,7 @@ class AXScreenAIAnnotatorFactory : public ProfileKeyedServiceFactory {
   ~AXScreenAIAnnotatorFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
