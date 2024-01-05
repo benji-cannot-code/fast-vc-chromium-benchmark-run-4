e56583c0feda680a775e067718b0ebfa3e74cdea
Noah Rose Ledesma <noahrose@google.com>
2024-01-05 17:14:24 +0000

    Allow components to be uninstalled w/o registering
    Previously attempting to uninstall a component before it was registered
    could cause a crash due to a null task runner. The task runner
    initialization has been moved to the installer's constructor.
    
    Additionally, Uninstall now operates without relying on registration
    info to locate the install directory.
    
    Bug: 1515745
    Change-Id: Iede175c6bfd62469af12d8a6ea54fee6acb656c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5173653
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243465}

--- chrome/browser/component_updater/masked_domain_list_component_installer.cc
+++ chrome/browser/component_updater/masked_domain_list_component_installer.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/component_updater/masked_domain_list_component_installer.h"
 
 #include "base/logging.h"
+#include "base/task/task_traits.h"
 #include "components/component_updater/installer_policies/masked_domain_list_component_installer_policy.h"
 #include "content/public/browser/network_service_instance.h"
 #include "services/network/public/mojom/network_service.mojom.h"
@@ -32,7 +33,9 @@ void RegisterMaskedDomainListComponent(ComponentUpdateService* cus) {
             }
           }));
 
-  base::MakeRefCounted<ComponentInstaller>(std::move(policy))
-      ->Register(cus, base::OnceClosure(), base::TaskPriority::USER_BLOCKING);
+  base::MakeRefCounted<ComponentInstaller>(std::move(policy),
+                                           /*action_handler=*/nullptr,
+                                           base::TaskPriority::USER_BLOCKING)
+      ->Register(cus, base::OnceClosure());
 }
 }  // namespace component_updater
