d5e33b852c075900a347a4b4e7646ca91d9bf41f
Yue Zhang <yuezhanggg@chromium.org>
2024-01-05 17:26:14 +0000

    [OG] Fix country code for PageContentAnnotationsService
    Currently, PageContentAnnotationsService queries country code
    directly from VariationsService, which will return an empty string
    for country code on local builds. This makes functionalities with
    country limitation not work on local builds. This CL fixes this
    issue by applying a default country code when country code is not
    available in VariationsService.
    
    Change-Id: Ib7f1abbdce3c3edd87e737780eba963779d78297
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5171200
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243472}

--- chrome/browser/optimization_guide/page_content_annotations_service_factory.cc
+++ chrome/browser/optimization_guide/page_content_annotations_service_factory.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/optimization_guide_features.h"
 #include "components/search_engines/template_url_service.h"
 #include "components/variations/service/variations_service.h"
+#include "components/variations/service/variations_service_utils.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/storage_partition.h"
 
@@ -136,11 +137,8 @@ PageContentAnnotationsServiceFactory::BuildServiceInstanceForBrowserContext(
   ZeroSuggestCacheService* zero_suggest_cache_service =
       ZeroSuggestCacheServiceFactory::GetForProfile(profile);
   if (optimization_guide_keyed_service && history_service) {
-    std::string country_code;
-    if (g_browser_process->variations_service()) {
-      country_code =
-          g_browser_process->variations_service()->GetStoredPermanentCountry();
-    }
+    std::string country_code =
+        GetCurrentCountryCode(g_browser_process->variations_service());
     return std::make_unique<optimization_guide::PageContentAnnotationsService>(
         std::make_unique<ChromeAutocompleteProviderClient>(profile),
         g_browser_process->GetApplicationLocale(), country_code,
