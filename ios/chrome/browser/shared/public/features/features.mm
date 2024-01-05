049b60c83d0f8ac08017d2461a087ce54294c717
Ed Chin <edchin@google.com>
2024-01-05 20:09:35 +0000

    Cleanup launched experiment tagging LE
    Land after removing usage downstream in https://crrev.com/i/6840077
    
    Fixed: 1440322
    Change-Id: Ic404b8e0abb1ad108b15844c8fcb7adc1349fb55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5171541
    Commit-Queue: edchin <edchin@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243574}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -390,10 +390,6 @@ BASE_FEATURE(kEnableFeedAblation,
              "EnableFeedAblation",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kEnableFeedExperimentTagging,
-             "EnableFeedExperimentTagging",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 BASE_FEATURE(kFeedDisableHotStartRefresh,
              "FeedDisableHotStartRefresh",
              base::FEATURE_DISABLED_BY_DEFAULT);
@@ -705,10 +701,6 @@ bool IsIOSHideFeedWithSearchChoiceTargeted() {
 
 bool IsFeedAblationEnabled() {
   return base::FeatureList::IsEnabled(kEnableFeedAblation);
-}
-
-bool IsFeedExperimentTaggingEnabled() {
-  return base::FeatureList::IsEnabled(kEnableFeedExperimentTagging);
 }
 
 bool IsFeedHotStartRefreshDisabled() {
