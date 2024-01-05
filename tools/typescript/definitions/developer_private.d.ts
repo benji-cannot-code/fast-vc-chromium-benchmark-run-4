c19a01c41541ee8139a429d22b65a3f39676a92a
Tom Van Goethem <tov@chromium.org>
2024-01-05 22:54:20 +0000

    Reland "[SHv2] Dismiss notification when visiting PWM or extensions manager"
    Relanding after revert with a fix for failing ChromeOS test. The fix is
    just enabling the Safety Hub feature of the test.
    
    Original change's description:
    > Revert "[SHv2] Dismiss notification when visiting PWM or extensions manager"
    >
    > This reverts commit 5bb3e1e715431ee7c7125d5071de9b80f26d4395.
    >
    > Reason for revert:
    > `ExtensionsActivityLogTest.TestSafetyHubMenuNotificationDismissed` is
    > failing on linux-chromeos-chrome
    > https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/37796/overview
    >
    > Original change's description:
    > > [SHv2] Dismiss notification when visiting PWM or extensions manager
    > >
    > > With this CL, menu notification for the passwords and extensions module
    > > of Safety Hub are dismissed when either the password manager page or
    > > extensions manager page are visited.
    > >
    > > Bug: 1443466, 1500955
    > > Change-Id: I9aa5d5410d6c1ef681ee5b94fdc8af347377dad4
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5053765
    > > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > > Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    > > Commit-Queue: Tom Van Goethem <tov@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1236556}
    >
    > Bug: 1443466, 1500955
    > Change-Id: Ic8e0ad66f9ebeede3b166edf03f93ba8ac074378
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5117079
    > Owners-Override: Mikihito Matsuura <mikt@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Mikihito Matsuura <mikt@google.com>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1236687}
    
    Bug: 1443466, 1500955
    Change-Id: Ia9e7970e7459487755a98d02482cb8d5e0e34165
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5130987
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: Tom Van Goethem <tov@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243653}

--- tools/typescript/definitions/developer_private.d.ts
+++ tools/typescript/definitions/developer_private.d.ts
@@ -489,6 +489,7 @@ declare global {
           Promise<MatchingExtensionInfo[]>;
       export function updateSiteAccess(
           site: string, updates: ExtensionSiteAccessUpdate[]): Promise<void>;
+      export function dismissSafetyHubExtensionsMenuNotification(): void;
 
       export const onItemStateChanged: ChromeEvent<(data: EventData) => void>;
       export const onProfileStateChanged:
