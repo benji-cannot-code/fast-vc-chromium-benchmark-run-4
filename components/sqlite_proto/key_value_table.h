97b6d0ad63c1320af4bc98826c5e73d9fa8c454f
David Bertoni <dbertoni@chromium.org>
2024-01-05 22:49:46 +0000

    [Code Health] Remove some uses of base::SupportsWeakPtr. (sqlite)
    Bug: 647430
    Change-Id: Ie56d2a0a42eb928803e7a305ac281a951fde4c00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5160335
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243650}

--- components/sqlite_proto/key_value_table.h
+++ components/sqlite_proto/key_value_table.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/memory/weak_ptr.h"
 #include "sql/statement.h"
 
 namespace google {
@@ -53,7 +54,7 @@ std::string GetDeleteAllSql(const std::string& table_name);
 // relative to the related classes. Making KeyValueTable<T> stateless instead
 // could be a better way to resolve these lifetime issues in the long run.
 template <typename T>
-class KeyValueTable : public base::SupportsWeakPtr<KeyValueTable<T>> {
+class KeyValueTable {
  public:
   explicit KeyValueTable(const std::string& table_name);
   // Virtual for testing.
@@ -71,8 +72,13 @@ class KeyValueTable : public base::SupportsWeakPtr<KeyValueTable<T>> {
                           sql::Database* db);
   virtual void DeleteAllData(sql::Database* db);
 
+  base::WeakPtr<KeyValueTable<T>> AsWeakPtr() {
+    return weak_ptr_factory_.GetWeakPtr();
+  }
+
  private:
   const std::string table_name_;
+  base::WeakPtrFactory<KeyValueTable<T>> weak_ptr_factory_{this};
 };
 
 template <typename T>
