46503988b8824451d73f17bccb1759267f7c2fa3
Angela Xiao <angelaxiao@chromium.org>
2024-01-05 01:48:38 +0000

    QA Rich Card: migrate to BoxLayout
    For certain views, it's better to use BoxLayout instead of FlexLayout
    to ensure the views remain correctly bounded. This specifically applies
    to the horizontal children views of the rich card `content_view_` for
    example. Migrate the applicable FlexLayout views to use BoxLayout.
    
    Test: tested on DUT
    Bug: b/317120267
    Change-Id: I4fe208c6340ab8d7e50c977cc33130631c35995a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5170136
    Commit-Queue: Angela Xiao <angelaxiao@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243238}

--- chrome/browser/ui/quick_answers/ui/rich_answers_translation_view.h
+++ chrome/browser/ui/quick_answers/ui/rich_answers_translation_view.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/events/event_handler.h"
 #include "ui/views/controls/webview/webview.h"
+#include "ui/views/layout/box_layout_view.h"
 #include "ui/views/view.h"
 
 namespace quick_answers {
@@ -34,9 +35,9 @@ class RichAnswersTranslationView : public RichAnswersView {
  private:
   void InitLayout();
   void AddLanguageTitle(const std::string& locale, bool is_header_view);
-  views::FlexLayoutView* AddLanguageText(const std::string& language_text,
-                                         bool maybe_append_buttons);
-  void AddReadAndCopyButtons(views::FlexLayoutView* translated_text_view);
+  views::BoxLayoutView* AddLanguageText(const std::string& language_text,
+                                        bool maybe_append_buttons);
+  void AddReadAndCopyButtons(views::BoxLayoutView* container_view);
   void OnReadButtonPressed(const std::string& read_text,
                            const std::string& locale);
   void OnCopyButtonPressed(const std::string& copy_text);
