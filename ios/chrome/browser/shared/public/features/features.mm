254a852285270c4a207dc2d8cc9efe996dca5295
Ed Chin <edchin@google.com>
2024-01-06 02:40:30 +0000

    Remove feed session based refresh
    Land after downstream usage removal in https://crrev.com/i/6840074.
    
    This CL is related to https://crrev.com/c/5173288, which cleans up
    all ACR flag related code. But I extracted this "session" piece
    separately because the removal was more complicated. This CL should
    be a lot more readable than if I had jumbled it together.
    
    Bug: 1479655
    Change-Id: I6941b727ba708554ad8d94936964708bf001e360
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5172027
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: edchin <edchin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1243720}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -450,18 +450,12 @@ const char kBackgroundRefreshIntervalInSeconds[] =
     "BackgroundRefreshIntervalInSeconds";
 const char kBackgroundRefreshMaxAgeInSeconds[] =
     "BackgroundRefreshMaxAgeInSeconds";
-const char kEnableFeedSessionCloseForegroundRefresh[] =
-    "EnableFeedSessionCloseForegroundRefresh";
 const char kEnableFeedAppCloseForegroundRefresh[] =
     "EnableFeedAppCloseForegroundRefresh";
 const char kEnableFeedAppCloseBackgroundRefresh[] =
     "EnableFeedAppCloseBackgroundRefresh";
-const char kFeedRefreshEngagementCriteriaType[] =
-    "FeedRefreshEngagementCriteriaType";
 const char kAppCloseBackgroundRefreshIntervalInSeconds[] =
     "AppCloseBackgroundRefreshIntervalInSeconds";
-const char kFeedRefreshTimerTimeoutInSeconds[] =
-    "FeedRefreshTimerTimeoutInSeconds";
 const char kFeedSeenRefreshThresholdInSeconds[] =
     "FeedSeenRefreshThresholdInSeconds";
 const char kFeedUnseenRefreshThresholdInSeconds[] =
@@ -607,13 +601,6 @@ bool IsFeedInvisibleForegroundRefreshEnabled() {
   return base::FeatureList::IsEnabled(kEnableFeedInvisibleForegroundRefresh);
 }
 
-bool IsFeedSessionCloseForegroundRefreshEnabled() {
-  return base::GetFieldTrialParamByFeatureAsBool(
-      kEnableFeedInvisibleForegroundRefresh,
-      kEnableFeedSessionCloseForegroundRefresh,
-      /*default=*/false);
-}
-
 bool IsFeedAppCloseForegroundRefreshEnabled() {
   return base::GetFieldTrialParamByFeatureAsBool(
       kEnableFeedInvisibleForegroundRefresh,
@@ -626,15 +613,6 @@ bool IsFeedAppCloseBackgroundRefreshEnabled() {
          IsFeedAppCloseBackgroundRefreshEnabledOnly();
 }
 
-FeedRefreshEngagementCriteriaType GetFeedRefreshEngagementCriteriaType() {
-  return (FeedRefreshEngagementCriteriaType)
-      base::GetFieldTrialParamByFeatureAsInt(
-          kEnableFeedInvisibleForegroundRefresh,
-          kFeedRefreshEngagementCriteriaType,
-          /*default_value=*/
-          (int)FeedRefreshEngagementCriteriaType::kSimpleEngagement);
-}
-
 double GetAppCloseBackgroundRefreshIntervalInSeconds() {
   double override_value = [[NSUserDefaults standardUserDefaults]
       doubleForKey:@"AppCloseBackgroundRefreshIntervalInSeconds"];
@@ -644,17 +622,6 @@ double GetAppCloseBackgroundRefreshIntervalInSeconds() {
   return base::GetFieldTrialParamByFeatureAsDouble(
       kEnableFeedInvisibleForegroundRefresh,
       kAppCloseBackgroundRefreshIntervalInSeconds,
-      /*default=*/base::Minutes(5).InSecondsF());
-}
-
-double GetFeedRefreshTimerTimeoutInSeconds() {
-  double override_value = [[NSUserDefaults standardUserDefaults]
-      doubleForKey:@"FeedRefreshTimerTimeoutInSeconds"];
-  if (override_value > 0.0) {
-    return override_value;
-  }
-  return base::GetFieldTrialParamByFeatureAsDouble(
-      kEnableFeedInvisibleForegroundRefresh, kFeedRefreshTimerTimeoutInSeconds,
       /*default=*/base::Minutes(5).InSecondsF());
 }
 
