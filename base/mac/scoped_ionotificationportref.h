726a48dfce4b5494e29c54006647732b1a3905f9
Solomon Kinard <solomonkinard@chromium.org>
2024-01-07 14:27:25 +0000

    mac: CodeHealth: Spelling correction
    Bug: None
    Change-Id: Id16b594268488ef9661931099da1b326138534e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5174021
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243844}

--- base/mac/scoped_ionotificationportref.h
+++ base/mac/scoped_ionotificationportref.h
@@ -21,7 +21,7 @@ struct ScopedIONotificationPortRefTraits {
   }
 };
 
-}  // namepsace internal
+}  // namespace internal
 
 using ScopedIONotificationPortRef =
     ScopedGeneric<IONotificationPortRef,
