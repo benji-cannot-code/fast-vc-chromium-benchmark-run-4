9ddc49953610ccfd2a4bd92dd44731ea633acabb
Jérôme Lebel <jlebel@chromium.org>
2024-01-08 13:52:21 +0000

    [iOS][Waffle] Follow-up after accessibility review
    When a collapsed cell is selected, VoiceOver need to read the title
    only.
    When the user collapses a cell, VoiceOver says "Collapsed" to confirm
    the action.
    
    Fixed: b/318692213
    Change-Id: I142a54c9145d0193a7ce321593d7723347ac601e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5168571
    Reviewed-by: Samar Chehade-Lepleux <samarchehade@google.com>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244023}

--- ios/chrome/browser/ui/search_engine_choice/search_engine_choice_table/cells/snippet_search_engine_cell.mm
+++ ios/chrome/browser/ui/search_engine_choice/search_engine_choice_table/cells/snippet_search_engine_cell.mm
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/search_engine_choice/search_engine_choice_table/cells/snippet_search_engine_cell.h"
 
 #import "base/check.h"
+#import "base/check_op.h"
+#import "base/notreached.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/ui/search_engine_choice/search_engine_choice_constants.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
@@ -231,16 +233,22 @@ constexpr NSTimeInterval kSnippetAnimationDurationInSecond = .3;
 // Called by the chevron button.
 - (void)chevronToggleAction:(id)sender {
   switch (_snippetState) {
-    case SnippetState::kShown:
+    case SnippetState::kShown: {
       // Need to hide the snippet.
       [self updateCellWithSnippetSate:SnippetState::kHidden animate:YES];
+      NSString* collapsedFeedback = l10n_util::GetNSString(
+          IDS_IOS_SEARCH_ENGINE_ACCESSIBILITY_SNIPPET_COLLAPSED);
+      UIAccessibilityPostNotification(UIAccessibilityAnnouncementNotification,
+                                      collapsedFeedback);
       break;
-    case SnippetState::kHidden:
+    }
+    case SnippetState::kHidden: {
       // Need to show the snippet.
       [self updateCellWithSnippetSate:SnippetState::kShown animate:YES];
       UIAccessibilityPostNotification(UIAccessibilityAnnouncementNotification,
                                       self.snippetLabel.text);
       break;
+    }
   }
   if (self.chevronToggledBlock) {
     self.chevronToggledBlock(_snippetState);
@@ -323,12 +331,15 @@ constexpr NSTimeInterval kSnippetAnimationDurationInSecond = .3;
 #pragma mark - Accessibility
 
 - (NSString*)accessibilityLabel {
-  NSString* accessibilityLabel = self.nameLabel.text;
-  if (self.snippetLabel.text.length > 0) {
-    accessibilityLabel = [NSString
-        stringWithFormat:@"%@. %@", accessibilityLabel, self.snippetLabel.text];
+  CHECK_NE(self.snippetLabel.text.length, 0ul);
+  switch (_snippetState) {
+    case SnippetState::kShown:
+      return [NSString stringWithFormat:@"%@. %@", self.nameLabel.text,
+                                        self.snippetLabel.text];
+    case SnippetState::kHidden:
+      return self.nameLabel.text;
   }
-  return accessibilityLabel;
+  NOTREACHED_NORETURN();
 }
 
 - (NSArray<NSString*>*)accessibilityUserInputLabels {
