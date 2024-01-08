cb97412f950f72816dd1ef8b68067c3a5ec288f5
Artem Sumaneev <asumaneev@google.com>
2024-01-08 11:31:29 +0000

    [enrollment] Remove AD enrollment status.
    Remove kActiveDirectoryPolicyFetchFailed from EnrollmentStatus.
    
    Bug: b:314101940
    Test: none
    Change-Id: Ib248b867a0d6b4f5b0fb871410652c726376223c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5076718
    Auto-Submit: Artem Sumaneev <asumaneev@google.com>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Reviewed-by: Jackson Tadie <jacksontadie@google.com>
    Reviewed-by: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1243977}

--- chrome/browser/ash/policy/enrollment/enrollment_status.cc
+++ chrome/browser/ash/policy/enrollment/enrollment_status.cc
@@ -39,8 +39,6 @@ std::string_view ToStringView(EnrollmentStatus::Code enrollment_code) {
       return "RegistrationCertFetchFailed";
     case EnrollmentStatus::Code::kNoMachineIdentification:
       return "NoMachineIdentification";
-    case EnrollmentStatus::Code::kActiveDirectoryPolicyFetchFailed:
-      return "ActiveDirectoryPolicyFetchFailed";
     case EnrollmentStatus::Code::kDmTokenStoreFailed:
       return "DmTokenStoreFailed";
     case EnrollmentStatus::Code::kMayNotBlockDevMode:
