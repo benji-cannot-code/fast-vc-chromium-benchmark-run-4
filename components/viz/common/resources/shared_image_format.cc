3997411af5dc7ab516cd85bf0b841b65ce741aee
Saifuddin Hitawala <hitawala@chromium.org>
2024-01-08 21:57:59 +0000

    Multiplanar: Add PlaneConfig::Y_U_V_A support
    Add PlaneConfig::Y_U_V_A support to SharedImageFormat. This is needed
    for viz render pixeltests that use I420A pixel format and perform pixel
    upload through VideoResourceUpdater.
    
    Bug: 1465939
    Change-Id: I8e5cbf8d96fa771a20c7926593a5da2aa0efd0db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5173333
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244293}

--- components/viz/common/resources/shared_image_format.cc
+++ components/viz/common/resources/shared_image_format.cc
@@ -127,6 +127,8 @@ const char* PlaneConfigToString(SharedImageFormat::PlaneConfig plane) {
       return "Y_UV";
     case SharedImageFormat::PlaneConfig::kY_UV_A:
       return "Y_UV_A";
+    case SharedImageFormat::PlaneConfig::kY_U_V_A:
+      return "Y_U_V_A";
   }
 }
 
@@ -201,6 +203,8 @@ int SharedImageFormat::NumberOfPlanes() const {
       return 2;
     case PlaneConfig::kY_UV_A:
       return 3;
+    case PlaneConfig::kY_U_V_A:
+      return 4;
   }
 }
 
@@ -298,12 +302,15 @@ gfx::Size SharedImageFormat::GetPlaneSize(int plane_index,
     return size;
   }
 
-  // Y plane is always size
+  // First plane is always Y plane and it is always size (not subsampled).
   if (plane_index == 0) {
     return size;
   }
   // A plane is always size
   if (plane_config() == PlaneConfig::kY_UV_A && plane_index == 2) {
+    return size;
+  }
+  if (plane_config() == PlaneConfig::kY_U_V_A && plane_index == 3) {
     return size;
   }
 
@@ -330,6 +337,7 @@ int SharedImageFormat::NumChannelsInPlane(int plane_index) const {
   switch (plane_config()) {
     case PlaneConfig::kY_U_V:
     case PlaneConfig::kY_V_U:
+    case PlaneConfig::kY_U_V_A:
       return 1;
     case PlaneConfig::kY_UV:
       return plane_index == 1 ? 2 : 1;
@@ -407,6 +415,7 @@ bool SharedImageFormat::HasAlpha() const {
     case PlaneConfig::kY_UV:
       return false;
     case PlaneConfig::kY_UV_A:
+    case PlaneConfig::kY_U_V_A:
       return true;
   }
 }
