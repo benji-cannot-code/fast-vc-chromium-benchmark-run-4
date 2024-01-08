67d0f9dd6d276d2ce3362baae0279d257aa18ade
Jimmy <jimmyxgong@chromium.org>
2024-01-08 19:34:56 +0000

    shortcuts: Don't show edit icon for locked shortcuts with no shortcuts assigned
    Fixes a bug in which the edit icon would appear for actions that are
    locked and with no accelerators assigned.
    
    before: https://screenshot.googleplex.com/AcvigLepCbbwk3o
    after: https://screenshot.googleplex.com/SpUM2p34woR6mk8
    
    Bug: b/216049298
    Change-Id: I132b7491144e8d6d663f7f7710a80603701b11b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5174592
    Reviewed-by: Longbo Wei <longbowei@google.com>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244201}

--- ash/webui/shortcut_customization_ui/resources/js/accelerator_row.ts
+++ ash/webui/shortcut_customization_ui/resources/js/accelerator_row.ts
@@ -85,7 +85,7 @@ export class AcceleratorRowElement extends AcceleratorRowElementBase {
   layoutStyle: LayoutStyle;
   action: number;
   source: AcceleratorSource;
-  private isLocked: boolean;
+  protected isLocked: boolean;
   private shortcutInterfaceProvider: ShortcutProviderInterface =
       getShortcutProvider();
 
