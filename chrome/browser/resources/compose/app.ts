e4cff02f0c1323bbb41c7aec6351da5409f5cc70
Dan Harrington <harringtond@chromium.org>
2024-01-08 19:17:16 +0000

    Update compose streaming ui
    * controls are hidden while streaming
    * length/tone controls still affect layout while hidden
    * controls fade-in when streaming completes.
    
    Bug: b:318853549
    Change-Id: Ic304632d493816a678ceb36bf5f31220d969d18d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5174490
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244189}

--- chrome/browser/resources/compose/app.ts
+++ chrome/browser/resources/compose/app.ts
@@ -127,6 +127,11 @@ export class ComposeAppElement extends ComposeAppElementBase {
         value: false,
         reflectToAttribute: true,
       },
+      loadingIndicatorShown_: {
+        type: Boolean,
+        reflectToAttribute: true,
+        computed: 'isLoadingIndicatorShown_(loading_, partialResponse_)',
+      },
       response_: {
         type: Object,
         value: null,
@@ -501,10 +506,13 @@ export class ComposeAppElement extends ComposeAppElementBase {
 
   private composeResponseReceived_(response: ComposeResponse) {
     this.response_ = response;
-    this.partialResponse_ = undefined;
     this.loading_ = false;
     this.undoEnabled_ = response.undoAvailable;
     this.feedbackState_ = CrFeedbackOption.UNSPECIFIED;
+    if (this.partialResponse_) {
+      this.animator_.transitionToFinalOutput();
+    }
+    this.partialResponse_ = undefined;
     this.requestUpdateScroll();
     switch (this.lastTriggerElement_) {
       case TriggerElement.SUBMIT_INPUT:
@@ -527,8 +535,8 @@ export class ComposeAppElement extends ComposeAppElementBase {
     this.partialResponse_ = partialResponse;
   }
 
-  private loadingIndictorShown_(): boolean {
-    return this.loading_ && !this.hasPartialResponse_();
+  private isLoadingIndicatorShown_(): boolean {
+    return this.loading_ && !this.partialResponse_;
   }
 
   private hasSuccessfulResponse_(): boolean {
@@ -537,6 +545,11 @@ export class ComposeAppElement extends ComposeAppElementBase {
 
   private hasPartialResponse_(): boolean {
     return Boolean(this.partialResponse_);
+  }
+
+
+  private hasPartialOrCompleteResponse_(): boolean {
+    return Boolean(this.partialResponse_) || this.hasSuccessfulResponse_();
   }
 
   private hasFailedResponse_(): boolean {
