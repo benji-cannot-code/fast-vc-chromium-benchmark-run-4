0672d88eda111ecb7b83f2d3fc250dff549f4b5e
Michelle <michellegc@google.com>
2024-01-08 05:07:22 +0000

    Add PickerItemView and PickerSectionView to show Picker results.
    PickerItemView is a clickable list item with text and an icon. Currently
    just used for showing search results, but later will also be used for
    clickable category items.
    
    PickerSectionView is a section of related items. Currently just shows
    items in a list layout, but later will also support other layouts (e.g.
    for emoji or image items).
    
    Bug: b:316790638
    Change-Id: I6758b5514d9c00986ed467f22e016b05d9998ea5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5175074
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243914}

---
+++ ash/picker/views/picker_item_view.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_PICKER_VIEWS_PICKER_ITEM_VIEW_H_
+#define ASH_PICKER_VIEWS_PICKER_ITEM_VIEW_H_
+
+#include <string>
+
+#include "ash/ash_export.h"
+#include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/views/controls/button/button.h"
+
+namespace ash {
+
+// View for a Picker list item. Has text and an icon.
+class ASH_EXPORT PickerItemView : public views::Button {
+  METADATA_HEADER(PickerItemView, views::Button)
+
+ public:
+  PickerItemView(views::Button::PressedCallback callback,
+                 const std::u16string& text);
+  PickerItemView(const PickerItemView&) = delete;
+  PickerItemView& operator=(const PickerItemView&) = delete;
+  ~PickerItemView() override;
+};
+
+}  // namespace ash
+
+#endif  // ASH_PICKER_VIEWS_PICKER_ITEM_VIEW_H_
--- ash/picker/views/picker_search_results_view.h
+++ ash/picker/views/picker_search_results_view.h
@@ -8,13 +8,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/picker/model/picker_search_results.h"
+#include "base/containers/span.h"
 #include "base/functional/callback_forward.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/view.h"
 
+namespace views {
+class ScrollView;
+}  // namespace views
+
 namespace ash {
 
 class PickerSearchResult;
+class PickerSectionView;
 
 class ASH_EXPORT PickerSearchResultsView : public views::View {
  public:
@@ -32,13 +38,25 @@ class ASH_EXPORT PickerSearchResultsView : public views::View {
   // Replaces the current search results with `results`.
   void SetSearchResults(const PickerSearchResults& results);
 
-  // views::View:
-  bool OnMousePressed(const ui::MouseEvent& event) override;
-  void OnMouseReleased(const ui::MouseEvent& event) override;
+  base::span<const raw_ptr<PickerSectionView>> section_views_for_testing()
+      const {
+    return section_views_;
+  }
 
  private:
+  // Runs `select_search_result_callback_` on `result`. Note that only one
+  // result can be selected (and subsequently calling this method will do
+  // nothing).
+  void SelectSearchResult(const PickerSearchResult& result);
+
   SelectSearchResultCallback select_search_result_callback_;
   PickerSearchResults search_results_;
+
+  // The scroll view that contains all the section_views_.
+  raw_ptr<views::ScrollView> scroll_view_ = nullptr;
+
+  // The views for each section of results.
+  std::vector<raw_ptr<PickerSectionView>> section_views_;
 };
 
 }  // namespace ash
