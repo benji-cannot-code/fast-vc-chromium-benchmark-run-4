2a9b4b8df127c7e16f22b2817ccd46de50ed2f65
Hirthanan Subenderan <hirthanan@google.com>
2024-01-08 18:48:28 +0000

    Design and implement check membership strategy phases for 28DA use case
    Consists of two phases: First phase check membership 3 most likely days.
    Depending on the membership result we perform a binary search across
    the remaining 25 days to determine the last ping date.
    
    Design: https://goto.google.com/28da-check-membership-rollout
    
    BUG=chromium:1327407
    
    Change-Id: If4523f4b8d0fdd69e9afe35e9103668af7b2a2f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4988920
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Hirthanan Subenderan <hirthanan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1244166}

--- chromeos/ash/components/report/prefs/fresnel_pref_names.h
+++ chromeos/ash/components/report/prefs/fresnel_pref_names.h
@@ -30,6 +30,8 @@ COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_REPORT)
 extern const char kDeviceActiveLastKnownIsActiveCurrentPeriodMinus1[];
 COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_REPORT)
 extern const char kDeviceActiveLastKnownIsActiveCurrentPeriodMinus2[];
+COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_REPORT)
+extern const char kDeviceActive28DayActivePingCache[];
 
 }  // namespace ash::report::prefs
 
