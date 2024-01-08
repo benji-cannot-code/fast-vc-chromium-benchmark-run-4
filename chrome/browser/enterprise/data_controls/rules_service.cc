64ea7b3695797d6946350caf4c0487e7358d4924
Dominique Fauteux-Chapleau <domfc@chromium.org>
2024-01-08 18:18:34 +0000

    Implement RulesService::GetPasteVerdict
    This CL also refactors unit tests to be more concise and has more
    coverage.
    
    Bug: b/313090937
    Change-Id: If465589622f6482976d311e25acea8066d29536a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5173712
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1244151}

--- chrome/browser/enterprise/data_controls/rules_service.cc
+++ chrome/browser/enterprise/data_controls/rules_service.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/enterprise/data_controls/rules_service.h"
+
 #include "base/memory/ptr_util.h"
 #include "base/no_destructor.h"
 #include "chrome/browser/profiles/profile.h"
@@ -24,6 +25,32 @@ Verdict RulesService::GetPrintVerdict(const GURL& printed_page_url) const {
                                    {.source = {
                                         .url = printed_page_url,
                                     }});
+}
+
+Verdict RulesService::GetPasteVerdict(
+    const content::ClipboardEndpoint& source,
+    const content::ClipboardEndpoint& destination,
+    const content::ClipboardMetadata& metadata) const {
+  ActionContext context;
+  if (source.data_transfer_endpoint() &&
+      source.data_transfer_endpoint()->IsUrlType()) {
+    context.source.url = *source.data_transfer_endpoint()->GetURL();
+  }
+  if (source.browser_context()) {
+    context.source.incognito =
+        Profile::FromBrowserContext(source.browser_context())
+            ->IsIncognitoProfile();
+  }
+  if (destination.data_transfer_endpoint() &&
+      destination.data_transfer_endpoint()->IsUrlType()) {
+    context.destination.url = *destination.data_transfer_endpoint()->GetURL();
+  }
+  if (destination.browser_context()) {
+    context.destination.incognito =
+        Profile::FromBrowserContext(destination.browser_context())
+            ->IsIncognitoProfile();
+  }
+  return rules_manager_.GetVerdict(Rule::Restriction::kClipboard, context);
 }
 
 // ----------------------------------
