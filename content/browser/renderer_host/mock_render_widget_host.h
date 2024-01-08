14d88b0abac0ec397ea6ad926e22be48c7e1d2bc
Aman Verma <amanvr@google.com>
2024-01-08 15:23:43 +0000

    Move content/browser/renderer_host/input/input_router_impl.h into //content/common/input
    This refactoring is a part of bigger project to handle input on viz
    thread (go/input-on-viz). We are moving input related interfaces that
    RenderWidgetHostImpl uses to //content/common/input. This would provide
    a single stack in //content/common/input that we quickly enter in either
    the browser or GPU viz which would allow for relatively clean movement
    of code. GPU process can link in the same input stack and forward the
    data to the renderer using the same code.
    
    Bug: b/305903095
    Change-Id: I59085c5f5e3fd7159cbab481e90ff8dba2f33fe9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5159007
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Aman Verma <amanvr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1244061}

--- content/browser/renderer_host/mock_render_widget_host.h
+++ content/browser/renderer_host/mock_render_widget_host.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/renderer_host/render_view_host_impl.h"
 
+#include "content/browser/renderer_host/input/fling_scheduler.h"
 #include "content/browser/renderer_host/input/mock_input_router.h"
 #include "content/common/input/event_with_latency_info.h"
 #include "content/test/mock_widget_input_handler.h"
--- content/common/input/input_router_impl.h
+++ content/common/input/input_router_impl.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_BROWSER_RENDERER_HOST_INPUT_INPUT_ROUTER_IMPL_H_
-#define CONTENT_BROWSER_RENDERER_HOST_INPUT_INPUT_ROUTER_IMPL_H_
+#ifndef CONTENT_COMMON_INPUT_INPUT_ROUTER_IMPL_H_
+#define CONTENT_COMMON_INPUT_INPUT_ROUTER_IMPL_H_
 
 #include <stdint.h>
 
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/task/sequenced_task_runner.h"
 #include "cc/input/touch_action.h"
-#include "content/browser/renderer_host/input/fling_scheduler.h"
 #include "content/common/content_export.h"
 #include "content/common/input/gesture_event_queue.h"
 #include "content/common/input/input_event_stream_validator.h"
@@ -26,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/input/passthrough_touch_event_queue.h"
 #include "content/common/input/touch_action_filter.h"
 #include "content/common/input/touchpad_pinch_event_queue.h"
-#include "content/public/common/input/native_web_keyboard_event.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "third_party/blink/public/mojom/input/input_event_result.mojom-shared.h"
@@ -291,4 +289,4 @@ class CONTENT_EXPORT InputRouterImpl
 
 }  // namespace content
 
-#endif  // CONTENT_BROWSER_RENDERER_HOST_INPUT_INPUT_ROUTER_IMPL_H_
+#endif  // CONTENT_COMMON_INPUT_INPUT_ROUTER_IMPL_H_
