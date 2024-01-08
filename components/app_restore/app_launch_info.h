858dc9cfdbd8f0a097f327c9ea1b696f238f2f15
Sammie Quon <sammiequon@chromium.org>
2024-01-08 22:22:24 +0000

    app_restore: Convert absl::optional to std::optional
    As of CL:1204351, absl::optional is now a type alias for
    std::optional. We should migrate toward it.
    
    Bug: b/318409847
    Test: existing tests
    Change-Id: I3610e0adbcd3916253bd25103a3ac960188103b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5171462
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244310}

--- components/app_restore/app_launch_info.h
+++ components/app_restore/app_launch_info.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_APP_RESTORE_APP_LAUNCH_INFO_H_
 #define COMPONENTS_APP_RESTORE_APP_LAUNCH_INFO_H_
 
+#include <optional>
 #include <vector>
 
 #include "base/component_export.h"
@@ -13,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/app_service/public/cpp/app_launch_util.h"
 #include "components/services/app_service/public/cpp/intent.h"
 #include "components/tab_groups/tab_group_info.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/base/window_open_disposition.h"
 #include "url/gurl.h"
 
@@ -60,28 +60,28 @@ struct COMPONENT_EXPORT(APP_RESTORE) AppLaunchInfo {
   ~AppLaunchInfo();
 
   std::string app_id;
-  absl::optional<int32_t> window_id;
-  absl::optional<int32_t> event_flag;
-  absl::optional<int32_t> container;
-  absl::optional<int32_t> disposition;
-  absl::optional<GURL> override_url;
-  absl::optional<int32_t> arc_session_id;
-  absl::optional<int64_t> display_id;
-  absl::optional<std::string> handler_id;
+  std::optional<int32_t> window_id;
+  std::optional<int32_t> event_flag;
+  std::optional<int32_t> container;
+  std::optional<int32_t> disposition;
+  std::optional<GURL> override_url;
+  std::optional<int32_t> arc_session_id;
+  std::optional<int64_t> display_id;
+  std::optional<std::string> handler_id;
   std::vector<GURL> urls;
-  absl::optional<int32_t> active_tab_index;
-  absl::optional<int32_t> first_non_pinned_tab_index;
+  std::optional<int32_t> active_tab_index;
+  std::optional<int32_t> first_non_pinned_tab_index;
   std::vector<base::FilePath> file_paths;
   apps::IntentPtr intent = nullptr;
-  absl::optional<bool> app_type_browser;
-  absl::optional<std::string> app_name;
+  std::optional<bool> app_type_browser;
+  std::optional<std::string> app_name;
   // For Browsers only, represents tab groups associated with this browser
   // instance if there are any. This is only used in Desks Storage, tab groups
   // in full restore are persistsed by sessions. This field is not converted to
   // base::Value in base value conversions.
   std::vector<tab_groups::TabGroupInfo> tab_group_infos;
   // Lacros only, the ID of the lacros profile that this browser uses.
-  absl::optional<uint64_t> lacros_profile_id;
+  std::optional<uint64_t> lacros_profile_id;
 };
 
 }  // namespace app_restore
