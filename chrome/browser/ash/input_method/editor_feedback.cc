e427974234f488a04f31c98b2d9de30114f65d22
Darren Shen <shend@chromium.org>
2024-01-08 03:23:11 +0000

    Avoid sending variations for Orca product ID.
    For privacy, we do not want to collect variations for Orca. Right now,
    variations is included in all reports.
    
    Because reports can be loaded from disk, whether we include variations
    or not for a report should be persisted to disk. However, it is too
    complicated to modify how reports are persisted to disk.
    
    Therefore, we hardcode the product IDs where we do not want to collect
    variations. A FeedbackReport now takes in the product ID as well.
    
    Note: We could've directly taken a boolean in FeedbackReport to
    control whether variations should be included (instead of indirectly
    via product ID). But FeedbackReport's constructors are called from
    two places: one in FeedbackReport and one in FeedbackUploader. So
    this approach means we need the product ID check in two different
    places, which is bad. Therefore, we just pass the product ID and
    check it once in FeedbackReport.
    
    Bug: b/307804234
    Change-Id: I8d123e2b2dd7299c03ae7f506114351fa2fddd9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4976085
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243901}

--- chrome/browser/ash/input_method/editor_feedback.cc
+++ chrome/browser/ash/input_method/editor_feedback.cc
@@ -19,8 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash::input_method {
 namespace {
 
-constexpr int kOrcaFeedbackProductId = 5314436;
-
 base::WeakPtr<feedback::FeedbackUploader> GetFeedbackUploaderFromContext(
     content::BrowserContext* context) {
   feedback::FeedbackUploader* uploader =
