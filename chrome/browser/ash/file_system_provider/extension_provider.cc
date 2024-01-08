22bb32a50db25e291f18bc545fd2497c151cf6f8
Ben Reich <benreich@chromium.org>
2024-01-08 01:37:28 +0000

    [FspCache] Introduce CachedFileSystem
    This is currently just a pass through wrapper around ProvidedFileSystem
    that simply adds additional logging capabilities. It is only enabled
    when the feature flag is on and will serve as the basis for intercepting
    calls to the FSP.
    
    VLOG(1) have been added for operations which are likely to affect a
    local content cache, whilst VLOG(2) exist for any operation which (even
    when a cache is implemented) will likely pass through to the underlying
    ProvidedFileSystem.
    
    Bug: b/317137739
    Change-Id: I6f06b3b8e8b5165bc63237e6640a52c23985b146
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5172831
    Reviewed-by: Cassy Chun-Crogan <cassycc@google.com>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243892}

--- chrome/browser/ash/file_system_provider/extension_provider.cc
+++ chrome/browser/ash/file_system_provider/extension_provider.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/app_icon/app_icon_source.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
+#include "chrome/browser/ash/file_system_provider/cached_file_system.h"
 #include "chrome/browser/ash/file_system_provider/mount_request_handler.h"
 #include "chrome/browser/ash/file_system_provider/odfs_metrics.h"
 #include "chrome/browser/ash/file_system_provider/provided_file_system.h"
@@ -98,6 +99,11 @@ ExtensionProvider::CreateProvidedFileSystem(
     Profile* profile,
     const ProvidedFileSystemInfo& file_system_info) {
   DCHECK(profile);
+  if (chromeos::features::IsFileSystemProviderContentCacheEnabled()) {
+    return std::make_unique<ThrottledFileSystem>(
+        std::make_unique<CachedFileSystem>(
+            std::make_unique<ProvidedFileSystem>(profile, file_system_info)));
+  }
   return std::make_unique<ThrottledFileSystem>(
       std::make_unique<ProvidedFileSystem>(profile, file_system_info));
 }
