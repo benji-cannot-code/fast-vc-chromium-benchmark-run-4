eb8b41003eeb1a80ceca34f24b8f3fb9cf009cd0
Jan Lanik <janlanik@google.com>
2024-01-08 22:27:42 +0000

    Do not trigger OS notifications from browser
    Browser should not trigger system level notifications for geolocation
    access on ChromeOS. We are using the standard browser surfaces
    to communicate that the geolocation is off and the OS notification
    is therefore superfluous.
    
    Bug: b/312485657
    Change-Id: I5eef88db1956dddbaee5508423dda62d51cf0ef7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5119918
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Auto-Submit: Jan Láník <janlanik@google.com>
    Cr-Commit-Position: refs/heads/main@{#1244313}

--- chrome/browser/ui/ash/privacy_hub/geolocation_switch_interactive_uitest.cc
+++ chrome/browser/ui/ash/privacy_hub/geolocation_switch_interactive_uitest.cc
@@ -136,9 +136,7 @@ IN_PROC_BROWSER_TEST_F(GeolocationSwitchInteractiveTest,
   SetSystemPermission(ash::GeolocationAccessLevel::kDisallowed);
 
   ExpectAndApproveBrowserPrompt(false);
-  // TODO(b/312485657): This is where the bug is. The OS notification should not
-  // appear. Uncomment when fixed.
-  // ExpectOSNotification(false);
+  ExpectOSNotification(false);
 }
 
 // TODO(b/312485657): Enable the testcase.
@@ -148,9 +146,7 @@ IN_PROC_BROWSER_TEST_F(GeolocationSwitchInteractiveTest,
   SetSystemPermission(ash::GeolocationAccessLevel::kDisallowed);
 
   ExpectAndApproveBrowserPrompt(false);
-  // TODO(b/312485657): this is where the bug is. The OS notification should not
-  // appear. Uncomment when fixed.
-  // ExpectOSNotification(false);
+  ExpectOSNotification(false);
 }
 
 IN_PROC_BROWSER_TEST_F(GeolocationSwitchInteractiveTest,
@@ -167,7 +163,7 @@ IN_PROC_BROWSER_TEST_F(GeolocationSwitchInteractiveTest, BrowserAskSystemOnly) {
   SetSystemPermission(ash::GeolocationAccessLevel::kOnlyAllowedForSystem);
 
   ExpectAndApproveBrowserPrompt(true);
-  ExpectOSNotification(true);
+  ExpectOSNotification(false);
 }
 
 IN_PROC_BROWSER_TEST_F(GeolocationSwitchInteractiveTest,
@@ -176,7 +172,7 @@ IN_PROC_BROWSER_TEST_F(GeolocationSwitchInteractiveTest,
   SetSystemPermission(ash::GeolocationAccessLevel::kDisallowed);
 
   ExpectAndApproveBrowserPrompt(true);
-  ExpectOSNotification(true);
+  ExpectOSNotification(false);
 }
 
 IN_PROC_BROWSER_TEST_F(GeolocationSwitchInteractiveTest,
@@ -194,7 +190,7 @@ IN_PROC_BROWSER_TEST_F(GeolocationSwitchInteractiveTest,
   SetSystemPermission(ash::GeolocationAccessLevel::kOnlyAllowedForSystem);
 
   ExpectAndApproveBrowserPrompt(false);
-  ExpectOSNotification(true);
+  ExpectOSNotification(false);
 }
 
 IN_PROC_BROWSER_TEST_F(GeolocationSwitchInteractiveTest,
@@ -203,5 +199,5 @@ IN_PROC_BROWSER_TEST_F(GeolocationSwitchInteractiveTest,
   SetSystemPermission(ash::GeolocationAccessLevel::kDisallowed);
 
   ExpectAndApproveBrowserPrompt(false);
-  ExpectOSNotification(true);
+  ExpectOSNotification(false);
 }
