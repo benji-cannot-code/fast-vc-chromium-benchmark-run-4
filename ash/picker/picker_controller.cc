c17ffb2b0de86b0999b889f8b28a26504600e181
Michelle <michellegc@google.com>
2024-01-08 01:11:01 +0000

    Add PickerGifView with basic gif showing functionality.
    This is for showing gifs in the new ChromeOS picker feature. Various
    details are still TBD (e.g. how many gifs can be shown, whether to
    always animate or only animate on hover, resolution requirements), so
    just add some basic functionality to experiment with for now.
    
    Gifs are planned to be fetched from Google's tenor API:
    https://developers.google.com/tenor
    
    Then, the gifs are decoded out of process using the data_decoder
    service, via ash::image_util API. The decoded frames are stored in
    PickerGifView, which handles frame updates. Note that the gifs are
    downscaled when they are decoded if needed to reduce the total size of
    all frames to `kMaxImageSizeInBytes`:
    https://source.chromium.org/chromium/chromium/src/+/main:ash/public/cpp/image_util.cc;l=156;drc=4597c83760e5b831ec153cba625e571d0a4ec6f9
    
    Bug: b:316817118, b:316936723
    
    Change-Id: Ia1bed51d6a640bb4aa1dbfc5ee1d823b35fa9564
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5124918
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243889}

--- ash/picker/picker_controller.cc
+++ ash/picker/picker_controller.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/picker/picker_controller.h"
 
 #include <string_view>
+#include <utility>
 
 #include "ash/constants/ash_switches.h"
 #include "ash/picker/model/picker_search_results.h"
@@ -13,8 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/picker/views/picker_view.h"
 #include "ash/picker/views/picker_view_delegate.h"
 #include "ash/public/cpp/ash_web_view_factory.h"
+#include "ash/public/cpp/image_util.h"
 #include "ash/public/cpp/picker/picker_client.h"
 #include "base/command_line.h"
+#include "base/functional/bind.h"
 #include "base/hash/sha1.h"
 
 namespace ash {
@@ -96,6 +99,13 @@ void PickerController::ToggleWidget(
 std::unique_ptr<AshWebView> PickerController::CreateWebView(
     const AshWebView::InitParams& params) {
   return client_->CreateWebView(params);
+}
+
+void PickerController::LoadAndDecodeGif(const GURL& url,
+                                        DecodeGifCallback callback) {
+  client_->DownloadGifToString(
+      url,
+      base::BindOnce(&image_util::DecodeAnimationData, std::move(callback)));
 }
 
 void PickerController::StartSearch(const std::u16string& query,
