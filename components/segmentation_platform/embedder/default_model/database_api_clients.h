653bc22ad6d86edcfd7f113f38be45c1fcbc2f79
Roman Arora <romanarora@chromium.org>
2024-01-08 17:39:31 +0000

    [segmentation] Add Sum & Group query
    Adds a new sum and group db query to be leveraged for recording and
    recalling aggregate event metric values for a given event name. This
    capability will initially be leveraged for the following proposal:
    
    https://docs.google.com/document/d/15OXGrpdNlmWyPLUcabNvkpCXV1JRWyM_Md__HdglVM0/edit?resourcekey=0-JRhu2OeY7B2V0hkCdmQ6Yw&tab=t.0#heading=h.bat9awopsp53
    
    Bug: 1464649
    Change-Id: If48d8aa1f04118117b4739f4e5ff663905b8a78c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5122753
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1244126}

--- components/segmentation_platform/embedder/default_model/database_api_clients.h
+++ components/segmentation_platform/embedder/default_model/database_api_clients.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_SEGMENTATION_PLATFORM_EMBEDDER_DEFAULT_MODEL_DATABASE_API_CLIENTS_H_
 
 #include <memory>
+#include <set>
+#include <string>
 
 #include "base/strings/string_piece.h"
 #include "components/segmentation_platform/internal/metadata/metadata_writer.h"
@@ -35,6 +37,13 @@ class DatabaseApiClients : public DefaultModelProvider {
   static void AddSumQuery(MetadataWriter& writer,
                           base::StringPiece metric_name,
                           int days);
+
+  // Helper to write a query to get the sum of metric values for a given
+  // `event_name` and a collection of `metric_names` for the past `days`.
+  static void AddSumGroupQuery(MetadataWriter& writer,
+                               base::StringPiece event_name,
+                               const std::set<std::string>& metric_names,
+                               int days);
 
   // ModelProvider implementation.
   std::unique_ptr<ModelConfig> GetModelConfig() override;
