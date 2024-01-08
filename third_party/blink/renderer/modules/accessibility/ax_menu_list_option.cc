b98515bd94fc85140085bedf0ecd607db97ef7cf
Aaron Leventhal <aleventhal@google.com>
2024-01-08 21:51:56 +0000

    [A11y] Speculative fix for null ref in AXMenuListOption
    Note that we plan to remove AXMenuList* classes eventually.
    
    Fixed: 1514429
    Change-Id: I1e54a620843b1a71141e35b6df7963c0c3db1056
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5178150
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Jacques Newman <janewman@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1244288}

--- third_party/blink/renderer/modules/accessibility/ax_menu_list_option.cc
+++ third_party/blink/renderer/modules/accessibility/ax_menu_list_option.cc
@@ -165,7 +165,8 @@ bool AXMenuListOption::ComputeAccessibilityIsIgnored(
     return true;
   }
 
-  return ParentObject()->ComputeAccessibilityIsIgnored(ignored_reasons);
+  return !ParentObject() ||
+         ParentObject()->ComputeAccessibilityIsIgnored(ignored_reasons);
 }
 
 void AXMenuListOption::GetRelativeBounds(
