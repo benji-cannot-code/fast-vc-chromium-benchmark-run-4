4fe0b562374a77bbe0f0ad3cfb58867f248e7755
David Tseng <dtseng@google.com>
2024-01-08 16:26:48 +0000

    [a11y-extensions]: clear directories on copy support
    This change provides the initial mechanism for cleaning targets prior to
    copying and applying it to almost all of ChromeVox.
    
    Some inspiration taken from
    build/android/gyp/copy_ex.py
    
    R=anastasi@google.com, katie@chromium.org
    
    Bug: b/317071070
    Test: on a clean build, run browser_tests. On incremental builds, verify removing files results in refreshed directories (without said file). Test on device with a braille display after manually verifying on device files contents.
    Change-Id: I8ab4000489aff8c2d4557a8a15d4745203b44a52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5142246
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1244094}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/phonetic_data.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/phonetic_data.js
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Provides phonetic disambiguation functionality across multiple
  * languages for ChromeVox.
  */
-import {PhoneticDictionaries} from './phonetic_dictionaries.js';
+import {PhoneticDictionaries} from '../phonetic_dictionaries.js';
 
 export class PhoneticData {
   /**
