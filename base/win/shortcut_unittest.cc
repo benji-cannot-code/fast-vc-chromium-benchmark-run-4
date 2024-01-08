b1d7bda7c2dc285c20cd5f7ccfa2c72c0120dcf0
Austin Sullivan <asully@chromium.org>
2024-01-08 18:48:08 +0000

    Code Health: Add span variant of base::ReadFile()
    Callers within //base have been migrated. Other callers will be
    migrated in follow-up CLs
    
    Bug: 1333521, 1490484
    Change-Id: If00d50f9c3be126662ea9df6ea09133c49b3a79d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5138261
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244165}

--- base/win/shortcut_unittest.cc
+++ base/win/shortcut_unittest.cc
@@ -151,7 +151,7 @@ TEST_F(ShortcutTest, CreateAndResolveShortcut) {
   EXPECT_TRUE(ResolveShortcut(link_file_, &resolved_name, nullptr));
 
   char read_contents[std::size(kFileContents)];
-  base::ReadFile(resolved_name, read_contents, std::size(read_contents));
+  ReadFile(resolved_name, read_contents);
   EXPECT_STREQ(kFileContents, read_contents);
 }
 
@@ -164,7 +164,7 @@ TEST_F(ShortcutTest, ResolveShortcutWithArgs) {
   EXPECT_TRUE(ResolveShortcut(link_file_, &resolved_name, &args));
 
   char read_contents[std::size(kFileContents)];
-  base::ReadFile(resolved_name, read_contents, std::size(read_contents));
+  ReadFile(resolved_name, read_contents);
   EXPECT_STREQ(kFileContents, read_contents);
   EXPECT_EQ(link_properties_.arguments, args);
 }
@@ -216,7 +216,7 @@ TEST_F(ShortcutTest, UpdateShortcutUpdateOnlyTargetAndResolve) {
   EXPECT_TRUE(ResolveShortcut(link_file_, &resolved_name, nullptr));
 
   char read_contents[std::size(kFileContents2)];
-  base::ReadFile(resolved_name, read_contents, std::size(read_contents));
+  ReadFile(resolved_name, read_contents);
   EXPECT_STREQ(kFileContents2, read_contents);
 }
 
