299d4f8bbfa101f510091f997d2dcfbcbb55406a
Abhijeet Kandalkar <abhijeet@igalia.com>
2024-01-08 15:01:03 +0000

    [ios-blink] Minimal AudioInputStream low latency implementation
    A minimal implementation of AudioInputStream has been added to this CL
    to make recording work. In the current minimal implementation, only
    mono channel input streams are supported.
    
    This implementation was manually tested on iPhone 13 with iOS 17.1.1
    
    This change builds on the work done in below earlier CLs.
    - https://crrev.com/c/5098306
    - https://crrev.com/c/5103948
    - https://crrev.com/c/5106188
    - https://crrev.com/c/5109204
    
    TEST: ios-deploy --justlaunch --bundle \
          out/Debug-iphoneos/content_shell.app --args=" \
          --auto-accept-camera-and-microphone-capture \
          https://web-recorder.glitch.me"
    
    Change-Id: If4bee7e841fc0a734a2a7ffca5d5a69e1a3a83d0
    Bug: 1413450
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5007480
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244049}

--- media/audio/ios/audio_manager_ios.h
+++ media/audio/ios/audio_manager_ios.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 
 class AUHALStream;
+class AUAudioInputStream;
 
 // iOS implementation of the AudioManager singleton. This class is internal
 // to the audio output and only internal users can call methods not exposed by
@@ -69,6 +70,10 @@ class MEDIA_EXPORT AudioManagerIOS : public AudioManagerApple {
 
   std::string GetDefaultInputDeviceID() override;
   std::string GetDefaultOutputDeviceID() override;
+
+  // Used to track destruction of input and output streams.
+  void ReleaseOutputStream(AudioOutputStream* stream) override;
+  void ReleaseInputStream(AudioInputStream* stream) override;
 
   // Implementation of AudioIOStreamClient.
   void ReleaseOutputStreamUsingRealDevice(AudioOutputStream* stream,
@@ -128,8 +133,9 @@ class MEDIA_EXPORT AudioManagerIOS : public AudioManagerApple {
 
  private:
   // Tracks all constructed input and output streams.
-  std::list<AUHALStream*> output_streams_;
   std::list<AudioInputStream*> basic_input_streams_;
+  std::list<AUAudioInputStream*> low_latency_input_streams_;
+  std::list<AUHALStream*> output_streams_;
 };
 
 }  // namespace media
