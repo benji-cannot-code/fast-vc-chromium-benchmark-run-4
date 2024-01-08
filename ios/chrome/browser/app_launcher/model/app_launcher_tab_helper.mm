3901f8a708b03dbd0c696b0cbc218fe8c43a2cb0
Olivier ROBIN <olivierrobin@google.com>
2024-01-08 13:19:14 +0000

    Delay navigation until app foreground after external app launches
    When an external application is launched chrome is not always put
    in background (this is notably the case with system application that
    show a confirmation popup to the user).
    In that case, a quick navigation when Chrome is inactive foreground
    could lead to spoofing.
    
    This CL postpone navigations until the Chrome is back in active state
    to avoid showing the wrong page to the user.
    
    Bug: 942807
    Change-Id: I11635ba6827b9572a99058c0781aa0f978cc3a34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5158433
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244012}

--- ios/chrome/browser/app_launcher/model/app_launcher_tab_helper.mm
+++ ios/chrome/browser/app_launcher/model/app_launcher_tab_helper.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/reading_list/model/reading_list_model_factory.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/shared/model/url/url_util.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/web/common/url_scheme_util.h"
 #import "ios/web/public/navigation/navigation_item.h"
 #import "ios/web/public/navigation/navigation_manager.h"
@@ -149,6 +150,8 @@ void AppLauncherTabHelper::RequestToLaunchApp(const GURL& url,
         delegate_->LaunchAppForTabHelper(
             this, url,
             base::BindOnce(&AppLauncherTabHelper::OnAppLaunchCompleted,
+                           weak_factory_.GetWeakPtr()),
+            base::BindOnce(&AppLauncherTabHelper::AppNoLongerInactive,
                            weak_factory_.GetWeakPtr()));
       }
       return;
@@ -183,6 +186,8 @@ void AppLauncherTabHelper::OnShowAppLaunchAlertDone(const GURL& url,
   delegate_->LaunchAppForTabHelper(
       this, url,
       base::BindOnce(&AppLauncherTabHelper::OnAppLaunchTried,
+                     weak_factory_.GetWeakPtr()),
+      base::BindOnce(&AppLauncherTabHelper::AppNoLongerInactive,
                      weak_factory_.GetWeakPtr()));
 }
 
@@ -201,6 +206,21 @@ void AppLauncherTabHelper::ShowFailureAlertDone(bool user_allowed) {
 }
 
 void AppLauncherTabHelper::OnAppLaunchCompleted(bool success) {
+  if (success && !base::FeatureList::IsEnabled(
+                     kInactiveNavigationAfterAppLaunchKillSwitch)) {
+    return;
+  }
+  LaunchAppRequestCompleted();
+}
+
+void AppLauncherTabHelper::AppNoLongerInactive() {
+  if (!base::FeatureList::IsEnabled(
+          kInactiveNavigationAfterAppLaunchKillSwitch)) {
+    LaunchAppRequestCompleted();
+  }
+}
+
+void AppLauncherTabHelper::LaunchAppRequestCompleted() {
   is_app_launch_request_pending_ = false;
   is_prompt_active_ = false;
 
--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -839,3 +839,7 @@ int TimeUntilShowingCompactedSetUpList() {
 bool IsExternalActionSchemeHandlingEnabled() {
   return base::FeatureList::IsEnabled(kIOSExternalActionURLs);
 }
+
+BASE_FEATURE(kInactiveNavigationAfterAppLaunchKillSwitch,
+             "kInactiveNavigationAfterAppLaunchKillSwitch",
+             base::FEATURE_DISABLED_BY_DEFAULT);
