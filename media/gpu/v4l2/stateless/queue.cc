ac2b5e0f9010f21e79e232d853d57a59cad1b0ff
Fritz Koenig <frkoenig@chromium.org>
2024-01-08 19:36:04 +0000

    v4l2/stateless: Use |decoder_| reference buffer count for allocation
    Allocating the correct number of reference frames requires
    querying the |decoder_|. This is done from
    |V4L2StatelessVideoDecoder| instead of adding logic to the
    |*Queue|s because the |decoder_| object which holds the
    information lives there.
    
    Cq-Include-Trybots: luci.chromium.try:linux-v4l2-codec-rel
    Bug: b:278935312
    Change-Id: Ied8f0da1830983e6b2c3f158fdd559eb8dd7f83f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5174017
    Commit-Queue: Fritz Koenig <frkoenig@chromium.org>
    Reviewed-by: Steve Cho <stevecho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244203}

--- media/gpu/v4l2/stateless/queue.cc
+++ media/gpu/v4l2/stateless/queue.cc
@@ -40,19 +40,19 @@ BaseQueue::~BaseQueue() {
   }
 }
 
-bool BaseQueue::AllocateBuffers(uint32_t num_planes) {
+bool BaseQueue::AllocateBuffers(uint32_t num_planes, size_t num_buffers) {
   DVLOGF(4);
   CHECK(device_);
   CHECK(num_planes);
 
   const auto count =
-      device_->RequestBuffers(buffer_type_, memory_type_, BufferMinimumCount());
+      device_->RequestBuffers(buffer_type_, memory_type_, num_buffers);
 
   if (!count) {
     return false;
   }
 
-  DVLOGF(2) << BufferMinimumCount() << " buffers request " << *count
+  DVLOGF(2) << num_buffers << " buffers request " << *count
             << " buffers allocated for " << Description() << " queue.";
   buffers_.reserve(*count);
 
@@ -153,9 +153,9 @@ bool InputQueue::SetupFormat(const gfx::Size resolution) {
   return true;
 }
 
-bool InputQueue::PrepareBuffers() {
+bool InputQueue::PrepareBuffers(size_t num_buffers) {
   DVLOGF(4);
-  return AllocateBuffers(kNumberInputPlanes);
+  return AllocateBuffers(kNumberInputPlanes, num_buffers);
 }
 
 void InputQueue::Reclaim(Buffer& buffer) {
@@ -236,10 +236,6 @@ bool InputQueue::SubmitCompressedFrameData(void* ctrls,
 
 std::string InputQueue::Description() {
   return "input";
-}
-
-uint32_t InputQueue::BufferMinimumCount() {
-  return 1;
 }
 
 std::unique_ptr<OutputQueue> OutputQueue::Create(
@@ -365,10 +361,10 @@ scoped_refptr<VideoFrame> OutputQueue::CreateVideoFrame(uint32_t index) {
       std::move(dmabuf_fds), base::TimeDelta());
 }
 
-bool OutputQueue::PrepareBuffers() {
+bool OutputQueue::PrepareBuffers(size_t num_buffers) {
   DVLOGF(4);
 
-  if (!AllocateBuffers(buffer_format_.NumPlanes())) {
+  if (!AllocateBuffers(buffer_format_.NumPlanes(), num_buffers)) {
     return false;
   }
 
@@ -466,12 +462,6 @@ bool OutputQueue::QueueBufferByFrameID(uint64_t frame_id) {
 
 std::string OutputQueue::Description() {
   return "output";
-}
-
-uint32_t OutputQueue::BufferMinimumCount() {
-  // TODO(frkoenig) : VP9 can have up to a max of 8 reference frames. This
-  // function will eventually need to request buffers based on the codec/clip.
-  return 10;
 }
 
 }  // namespace media
