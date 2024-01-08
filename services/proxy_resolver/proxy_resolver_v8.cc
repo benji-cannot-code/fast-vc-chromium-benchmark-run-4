8cc3632e70c8c672ed5d204a1cd776181ea23e15
Nikolaos Papaspyrou <nikolaos@chromium.org>
2024-01-08 12:20:22 +0000

    Fix use of v8::Unlocker in ProxyResolver
    According to V8's API, a thread has to exit the isolate before using
    v8::Unlocker and enter it again afterwards. This CL fixes the use in
    ProxyResolverV8::Context::DnsResolveCallbackHelper, to adhere to that.
    
    Bug: v8:13270
    Bug: 1515391
    Change-Id: Id92f3944a974f2f0d33bc44e4abc851d678e52f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5168625
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Nikolaos Papaspyrou <nikolaos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243992}

--- services/proxy_resolver/proxy_resolver_v8.cc
+++ services/proxy_resolver/proxy_resolver_v8.cc
@@ -762,10 +762,12 @@ class ProxyResolverV8::Context {
     bool terminate = false;
 
     {
+      args.GetIsolate()->Exit();
       v8::Unlocker unlocker(args.GetIsolate());
       success =
           context->js_bindings()->ResolveDns(hostname, op, &result, &terminate);
     }
+    args.GetIsolate()->Enter();
 
     if (terminate)
       args.GetIsolate()->TerminateExecution();
