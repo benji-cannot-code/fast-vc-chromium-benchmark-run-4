dcfb9f6e83d963ce78a37db8ff3df3e2291f1cd6
Oleksandr Sarapulov <olsa@google.com>
2024-01-08 08:20:35 +0000

    Reland "Add support of EC keys import to Chaps soft slot"
    This is a reland of commit 99a05d2d82d6e89179e70d818696b0087cb73bf2
    Fixed memory leak and small formatting.
    
    Original change's description:
    > Add support of EC keys import to Chaps soft slot
    >
    > Add support of EC keys import to chaps and also
    > add unit tests.
    >
    > Bug: b:286049619
    > Change-Id: I076bd0e93612a3e8488da9483b5bb546417fc04e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5001508
    > Commit-Queue: Oleksandr Sarapulov <olsa@google.com>
    > Reviewed-by: David Benjamin <davidben@chromium.org>
    > Reviewed-by: Michael Ershov <miersh@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1235672}
    
    Bug: b:286049619
    Change-Id: I642690a3403668ea7171b5cce582f2aeb56ead1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5111321
    Commit-Queue: Oleksandr Sarapulov <olsa@google.com>
    Reviewed-by: Michael Ershov <miersh@google.com>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243939}

--- chromeos/ash/components/chaps_util/pkcs12_validator.cc
+++ chromeos/ash/components/chaps_util/pkcs12_validator.cc
@@ -18,6 +18,8 @@ constexpr char kPkcs12CertImportFailed[] =
     "Chaps util cert import failed with ";
 constexpr int kMaxAttemptUniqueNicknameCreation = 100;
 constexpr const char kDefaultNickname[] = "Unknown org";
+constexpr const char kPrivateKeyInstalledMsg[] =
+    "Private key is already installed in slot";
 
 // Custom CERTCertificateList object allows to avoid calls to PORT_FreeArena()
 // after every usage of CERTCertificateList.
@@ -213,7 +215,7 @@ Pkcs12ReaderStatusCode CanFindInstalledKey(PK11SlotInfo* slot,
   Pkcs12ReaderStatusCode res = pkcs12_reader.DoesKeyForCertExist(
       slot, Pkcs12ReaderCertSearchType::kPlainType, scoped_cert);
   if (res == Pkcs12ReaderStatusCode::kSuccess) {
-    LOG(WARNING) << "Private key is already installed in slot";
+    LOG(WARNING) << kPrivateKeyInstalledMsg;
     is_key_installed = true;
     return Pkcs12ReaderStatusCode::kSuccess;
   }
@@ -227,7 +229,7 @@ Pkcs12ReaderStatusCode CanFindInstalledKey(PK11SlotInfo* slot,
   res = pkcs12_reader.DoesKeyForCertExist(
       slot, Pkcs12ReaderCertSearchType::kDerType, scoped_cert);
   if (res == Pkcs12ReaderStatusCode::kSuccess) {
-    LOG(WARNING) << "Private key is already installed in slot";
+    LOG(WARNING) << kPrivateKeyInstalledMsg;
     is_key_installed = true;
     return Pkcs12ReaderStatusCode::kSuccess;
   }
