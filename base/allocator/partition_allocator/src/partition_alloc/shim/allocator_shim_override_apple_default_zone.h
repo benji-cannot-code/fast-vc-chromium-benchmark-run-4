9d58be82cf9045c34ef90dcef92047720316c083
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-01-08 13:50:30 +0000

    PA: Fix linking error on mac.
    Three functions were declared in "allocator_shim", but implemented
    in "allocator_core". They were not exported, so the linker isn't able
    to find their definition in component build.
    
    Bug: chromium:1516557
    Fixed: chromium:1516557
    Change-Id: I51e812c5076478a752ed9d819cf2d6b37c87201f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5172673
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244022}

--- base/allocator/partition_allocator/src/partition_alloc/shim/allocator_shim_override_apple_default_zone.h
+++ base/allocator/partition_allocator/src/partition_alloc/shim/allocator_shim_override_apple_default_zone.h
@@ -25,17 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "partition_alloc/partition_alloc_buildflags.h"
 #include "partition_alloc/partition_alloc_check.h"
 #include "partition_alloc/partition_alloc_constants.h"
+#include "partition_alloc/partition_root.h"
 #include "partition_alloc/shim/early_zone_registration_constants.h"
-
-namespace partition_alloc {
-
-// Defined in
-// partition_alloc/partition_root.cc
-void PartitionAllocMallocHookOnBeforeForkInParent();
-void PartitionAllocMallocHookOnAfterForkInParent();
-void PartitionAllocMallocHookOnAfterForkInChild();
-
-}  // namespace partition_alloc
 
 namespace allocator_shim {
 
