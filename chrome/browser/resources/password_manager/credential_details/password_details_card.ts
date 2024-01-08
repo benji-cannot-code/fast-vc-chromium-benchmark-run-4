a359fd904f4b7f70e9d79914ac01a8f9dd9a5d5a
Karol Sygiet <sygiet@google.com>
2024-01-08 11:09:07 +0000

    Add option in password details card to move saved psw to account
    This CL add new text in the password card details, which when clicked
    will show move saved password to account dialog. This CL also modifies
    move_password_dialog to look different depending on whether it was
    triggered for multiple or for single credential.
    
    Mock:
    https://screenshot.googleplex.com/AjrRvAF3xmNk5i7
    Implementation:
    https://screenshot.googleplex.com/ACVJaPasibb8htv
    
    Dialog mock:
    https://screenshot.googleplex.com/AHz5hFQE3SYawLo
    Dialog implementation:
    https://screenshot.googleplex.com/37DzLfjBpDakMtb
    
    Bug: 1503146
    Change-Id: I9dcf03887e8d0a886197ce0bcab0aa1887fd96b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5114984
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Reviewed-by: Andrii Natiahlyi <natiahlyi@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243972}

--- chrome/browser/resources/password_manager/credential_details/password_details_card.ts
+++ chrome/browser/resources/password_manager/credential_details/password_details_card.ts
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/cr_elements/cr_icons.css.js';
 import 'chrome://resources/cr_elements/cr_input/cr_input_style.css.js';
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import 'chrome://resources/cr_elements/cr_icons.css.js';
@@ -16,6 +17,7 @@ import '../dialogs/edit_password_dialog.js';
 import '../dialogs/multi_store_delete_password_dialog.js';
 import '../sharing/share_password_flow.js';
 import '../sharing/metrics_utils.js';
+import '../dialogs/move_single_password_dialog.js';
 
 import type {CrToastElement} from '//resources/cr_elements/cr_toast/cr_toast.js';
 import {HelpBubbleMixin} from 'chrome://resources/cr_components/help_bubble/help_bubble_mixin.js';
@@ -91,6 +93,8 @@ export class PasswordDetailsCardElement extends PasswordDetailsCardElementBase {
 
       showEditPasswordDialog_: Boolean,
       showDeletePasswordDialog_: Boolean,
+      showMovePasswordDialog_: Boolean,
+
 
       showShareButton_: {
         type: Boolean,
@@ -116,12 +120,22 @@ export class PasswordDetailsCardElement extends PasswordDetailsCardElementBase {
           return loadTimeData.getBoolean('enableSendPasswords');
         },
       },
+
+      enableButterOnDesktopFollowup_: {
+        type: Boolean,
+        value() {
+          return loadTimeData.getBoolean('enableButterOnDesktopFollowup');
+        },
+      },
+
+      isUsingAccountStore: Boolean,
     };
   }
 
   password: chrome.passwordsPrivate.PasswordUiEntry;
   groupName: string;
   iconUrl: string;
+  isUsingAccountStore: boolean;
   private toastMessage_: string;
   private showEditPasswordDialog_: boolean;
   private passwordSharingDisabled_: boolean;
@@ -129,6 +143,8 @@ export class PasswordDetailsCardElement extends PasswordDetailsCardElementBase {
   private showShareFlow_: boolean;
   private showShareButton_: boolean;
   private enableSendPasswords_: boolean;
+  private showMovePasswordDialog_: boolean;
+  private enableButterOnDesktopFollowup_: boolean;
 
   private isFederated_(): boolean {
     return !!this.password.federationText;
@@ -282,6 +298,25 @@ export class PasswordDetailsCardElement extends PasswordDetailsCardElementBase {
         this.i18n(
             'passwordDetailsCardDeleteButtonNoUsernameAriaLabel',
             this.getCredentialTypeString_());
+  }
+
+  private computeMovePasswordText_(): TrustedHTML {
+    return this.i18nAdvanced('moveSinglePassword');
+  }
+
+  private movePasswordClicked_(e: Event): void {
+    e.preventDefault();
+    this.showMovePasswordDialog_ = true;
+  }
+
+  private showMovePasswordEntry_(): boolean {
+    return this.enableButterOnDesktopFollowup_ && this.isUsingAccountStore &&
+        this.password.storedIn ===
+        chrome.passwordsPrivate.PasswordStoreSet.DEVICE;
+  }
+
+  private onMovePasswordDialogClose_(): void {
+    this.showMovePasswordDialog_ = false;
   }
 
   maybeRegisterSharingHelpBubble(): void {
