8cca3ee0761e781409b28c3a89bc5d049f430ac0
Minoru Chikamune <chikamune@chromium.org>
2024-01-08 11:45:45 +0000

    Warm-up service workers from bookmarks, omnibox, etc
    This CL enables us to warm up service workers from bookmarks, omnibox,
    etc.
    
    Bug: 1431792
    Change-Id: I07944a033d8f1ffc35e2e2c7a9f86295b7554aed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5125387
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243982}

--- chrome/browser/predictors/loading_predictor.cc
+++ chrome/browser/predictors/loading_predictor.cc
@@ -16,9 +16,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/predictors/predictors_features.h"
 #include "chrome/browser/predictors/resource_prefetch_predictor.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/service_worker_context.h"
+#include "content/public/browser/storage_partition.h"
+#include "content/public/common/origin_util.h"
 #include "net/base/network_anonymization_key.h"
 #include "services/network/public/cpp/request_destination.h"
 #include "third_party/blink/public/common/features.h"
+#include "third_party/blink/public/common/storage_key/storage_key.h"
 #include "url/origin.h"
 
 #if BUILDFLAG(IS_ANDROID)
@@ -91,6 +95,44 @@ bool IsPreconnectExpensive() {
 #endif
 }
 
+void MaybeWarmUpServiceWorker(const GURL& url, Profile* profile) {
+  if (!base::FeatureList::IsEnabled(
+          blink::features::kSpeculativeServiceWorkerWarmUp)) {
+    return;
+  }
+
+  if (!profile) {
+    return;
+  }
+
+  content::StoragePartition* storage_partition =
+      profile->GetDefaultStoragePartition();
+
+  if (!storage_partition) {
+    return;
+  }
+
+  content::ServiceWorkerContext* service_worker_context =
+      storage_partition->GetServiceWorkerContext();
+
+  if (!service_worker_context) {
+    return;
+  }
+
+  if (!content::OriginCanAccessServiceWorkers(url)) {
+    return;
+  }
+
+  const blink::StorageKey key =
+      blink::StorageKey::CreateFirstParty(url::Origin::Create(url));
+
+  if (!service_worker_context->MaybeHasRegistrationForStorageKey(key)) {
+    return;
+  }
+
+  service_worker_context->WarmUpServiceWorker(url, key, base::DoNothing());
+}
+
 }  // namespace
 
 LoadingPredictor::LoadingPredictor(const LoadingPredictorConfig& config,
@@ -118,6 +160,8 @@ bool LoadingPredictor::PrepareForPageLoad(
     absl::optional<PreconnectPrediction> preconnect_prediction) {
   if (shutdown_)
     return true;
+
+  MaybeWarmUpServiceWorker(url, profile_);
 
   if (origin == HintOrigin::OMNIBOX) {
     // Omnibox hints are lightweight and need a special treatment.
--- content/browser/service_worker/service_worker_context_wrapper.cc
+++ content/browser/service_worker/service_worker_context_wrapper.cc
@@ -917,7 +917,7 @@ void ServiceWorkerContextWrapper::StartServiceWorkerForNavigationHint(
 void ServiceWorkerContextWrapper::WarmUpServiceWorker(
     const GURL& document_url,
     const blink::StorageKey& key,
-    ServiceWorkerContextCore::WarmUpServiceWorkerCallback callback) {
+    WarmUpServiceWorkerCallback callback) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
   if (!context_core_) {
@@ -1659,7 +1659,7 @@ void ServiceWorkerContextWrapper::DidStartServiceWorkerForNavigationHint(
 }
 
 void ServiceWorkerContextWrapper::DidFindRegistrationForWarmUp(
-    ServiceWorkerContextCore::WarmUpServiceWorkerCallback callback,
+    WarmUpServiceWorkerCallback callback,
     blink::ServiceWorkerStatusCode status,
     scoped_refptr<ServiceWorkerRegistration> registration) {
   TRACE_EVENT1("ServiceWorker", "DidFindRegistrationForWarmUp", "status",
@@ -1687,7 +1687,7 @@ void ServiceWorkerContextWrapper::DidFindRegistrationForWarmUp(
 
 void ServiceWorkerContextWrapper::DidWarmUpServiceWorker(
     const GURL& scope,
-    ServiceWorkerContextCore::WarmUpServiceWorkerCallback callback,
+    WarmUpServiceWorkerCallback callback,
     blink::ServiceWorkerStatusCode code) {
   TRACE_EVENT2("ServiceWorker", "DidWarmUpServiceWorker", "url", scope.spec(),
                "code", blink::ServiceWorkerStatusToString(code));
