499893bfc4fb9758c4a8deeb59713f34b5f9639a
EmiliaPaz <emiliapaz@chromium.org>
2024-01-08 22:12:38 +0000

    [views] Fix toolbar button inkdrop
    crrev.com/c/5121173 introduced ToolbarButtonHighlightPathGenerator
    for ToolbarButton to allow ToolbarChipButton to have flat edges.
    This caused a regression where the "three dot" button inkdrop had the
    wrong insets.
    
    This CL fixes it by adding the toolbar indrop insets to the new
    highlight path generator, as it was done beforehand.
    
    Regression: https://drive.google.com/file/d/116f1SBJ_8bDaoi9xYdtc8SFHjtM5LQym/view?usp=sharing
    Fixed: https://drive.google.com/file/d/1fAbs0kZbTkHrnTPqM-mZW5JyEMJPpTd6/view?usp=sharing
    
    Bug: 1514349
    Change-Id: Ie05dcc0154a22f629c1c04702f8a43739b3689db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5163679
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244306}

--- chrome/browser/ui/views/toolbar/toolbar_button.cc
+++ chrome/browser/ui/views/toolbar/toolbar_button.cc
@@ -72,6 +72,7 @@ class ToolbarButtonHighlightPathGenerator
   // HighlightPathGenerator:
   SkPath GetHighlightPath(const views::View* view) override {
     gfx::Rect rect(view->size());
+    rect.Inset(GetToolbarInkDropInsets(view));
 
     SkPath path;
     const SkScalar left_radius =
