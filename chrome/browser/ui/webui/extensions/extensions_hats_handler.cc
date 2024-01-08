1086f791624b44b9d2c690229dd1484803aabb44
Adam Psarouthakis <psarouthakis@google.com>
2024-01-08 16:51:48 +0000

    Extension Hats Survey Config
    This CL updates the survey config file to include the
    Extensions Hats Survey. It also updates previous implementations
    to properly use Chrome features.
    
    Bug: 1495737
    Change-Id: I84b917b2d2e1ad8c0bc796b19e1f2dc8057bcf93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5158648
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Florian Jacky <fjacky@chromium.org>
    Commit-Queue: Adam Psarouthakis <psarouthakis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1244105}

--- chrome/browser/ui/webui/extensions/extensions_hats_handler.cc
+++ chrome/browser/ui/webui/extensions/extensions_hats_handler.cc
@@ -131,6 +131,9 @@ SurveyStringData ExtensionsHatsHandler::CreateSurveyStringsForNoInteraction() {
            base::NumberToString(number_installed_extensions_on_load_)},
           {"Time on extension page in minutes",
            base::NumberToString(time_spent_on_page.InMinutes())},
+          {"Number of extensions removed", ""},
+          {"Number of extensions kept", ""},
+          {"Number of non-trigger extensions removed", ""},
           {"Client Channel", client_channel_}};
 }
 
@@ -144,7 +147,7 @@ void ExtensionsHatsHandler::RequestHatsSurvey(bool require_same_origin,
     return;
   }
   hats_service->LaunchDelayedSurveyForWebContents(
-      "HappinessTrackingSurveysExtensionsSafetyHub", web_ui()->GetWebContents(),
+      kHatsSurveyTriggerExtensions, web_ui()->GetWebContents(),
       features::kHappinessTrackingSurveysExtensionsSafetyHubTime.Get()
           .InMilliseconds(),
       {}, string_data, require_same_origin);
