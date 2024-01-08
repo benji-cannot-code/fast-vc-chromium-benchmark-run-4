06a0e9c87dda56807407e595b69e6ebaf9f93d28
Henrique Nakashima <hnakashima@chromium.org>
2024-01-08 19:35:45 +0000

    [Android] Create CachedFlagSharedPreferences, move CachedFlag keys there
    This is a step to move CachedFlag into //base.
    
    Bug: 1483469,1442347
    Change-Id: Ib5fd72d4d1503bb9fb8c22b57099c4410bec234a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5165969
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244202}

--- chrome/browser/preferences/android/java/src/org/chromium/chrome/browser/preferences/AllPreferenceKeyRegistries.java
+++ chrome/browser/preferences/android/java/src/org/chromium/chrome/browser/preferences/AllPreferenceKeyRegistries.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.preferences;
 
 import androidx.annotation.VisibleForTesting;
 
+import org.chromium.base.cached_flags.CachedFlagsSharedPreferences;
 import org.chromium.base.shared_preferences.KnownPreferenceKeyRegistries;
 import org.chromium.base.shared_preferences.PreferenceKeyRegistry;
 import org.chromium.build.annotations.CheckDiscard;
@@ -17,7 +18,7 @@ import java.util.Set;
 public class AllPreferenceKeyRegistries {
     @VisibleForTesting
     static final Set<PreferenceKeyRegistry> KNOWN_REGISTRIES =
-            Set.of(ChromeSharedPreferences.REGISTRY);
+            Set.of(ChromeSharedPreferences.REGISTRY, CachedFlagsSharedPreferences.REGISTRY);
 
     public static void initializeKnownRegistries() {
         KnownPreferenceKeyRegistries.initializeKnownRegistries(KNOWN_REGISTRIES);
