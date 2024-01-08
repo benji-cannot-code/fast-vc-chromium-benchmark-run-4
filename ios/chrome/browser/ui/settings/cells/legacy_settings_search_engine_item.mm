6acda698d60db791e7a7ca870b8f35b5d51dc281
Jérôme Lebel <jlebel@chromium.org>
2024-01-08 11:23:29 +0000

    [iOS][Waffle] Add embedded favIcons in the search engine settings
    This patch adds SettingsSearchEngineItem/Cell to use the embedded
    fav icons instead of downloading them.
    SettingsSearchEngineItem and LegacySettingsSearchEngineItem implements
    SettingsSearchEngineItem protocol to simplify the code in the view
    controller.
    SettingsSearchEngineItem protocol and LegacySettingsSearchEngineItem
    will be removed once the search engine feature flag will be removed.
    
    SearchEngineTableViewControllerTest has been updated to always
    run with `kSearchEngineChoiceTrigger` flag disabled.
    All the unittests with kSearchEngineChoiceTrigger enabled are moved
    into `SearchEngineTableViewControllerChoiceScreenTest`.
    Tests in `SearchEngineTableViewControllerChoiceScreenTest` need to be
    updated so we don't load the Chrome prepopulated search engines.
    
    Before:
    https://drive.google.com/file/d/1H_ZD8VYb1JfT49pyAcaHQISdbny_uF-O/view?resourcekey=0--CyV4ui_WVjnGTCRIBq0pA
    https://drive.google.com/file/d/1nErUOh5zcGNSzi9ideakUR64WnwtOJaJ/view?resourcekey=0-bPp977adPJ2xDXxUfgQCbQ
    
    After:
    https://drive.google.com/file/d/1f1QVuKx61lEyVCina9y4rcprc9M95xn1/view?resourcekey=0-5KW3PJbLHCKYj4LgjzbVBg
    https://drive.google.com/file/d/1qTa8oQzQbKxTufn1AourfgSwF1u0GlUc/view?resourcekey=0-0F2sc5441QnlSmPyez7hJg
    
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL
    Fixed: b/318050393
    Change-Id: If68970f9d3dee677209dcadcca4cce7988e3a14b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5147801
    Reviewed-by: Samar Chehade-Lepleux <samarchehade@google.com>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243976}

--- ios/chrome/browser/ui/settings/cells/legacy_settings_search_engine_item.mm
+++ ios/chrome/browser/ui/settings/cells/legacy_settings_search_engine_item.mm
@@ -27,11 +27,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation LegacySettingsSearchEngineItem
 
+@synthesize enabled = _enabled;
+@synthesize text = _text;
+@synthesize detailText = _detailText;
+
 - (instancetype)initWithType:(NSInteger)type {
   self = [super initWithType:type];
   if (self) {
-      self.cellClass = TableViewURLCell.class;
-      _enabled = YES;
+    self.cellClass = TableViewURLCell.class;
+    _enabled = YES;
   }
   return self;
 }
