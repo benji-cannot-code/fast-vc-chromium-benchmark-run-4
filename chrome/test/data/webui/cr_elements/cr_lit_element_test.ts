9c162b69b48ca8cdcace4bd01bcc8ad58e5d132c
dpapad <dpapad@chromium.org>
2024-01-08 07:59:09 +0000

    WebUI: Use Polymer-style attribute/property mapping in CrLitElement.
    This is done to make CrLitElement behave closer to how PolymerElement
    works, and make it easier to migrate Polymer UIs to Lit, as well as make
    Lit and Polymer code which will co-exist for a while more consistent.
    
    Bug: 1503223
    Change-Id: Ie1389e708cf71e9270f5284b471e3dbaadd27066
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5172368
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243938}

--- chrome/test/data/webui/cr_elements/cr_lit_element_test.ts
+++ chrome/test/data/webui/cr_elements/cr_lit_element_test.ts
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {getTrustedHTML} from 'chrome://resources/js/static_types.js';
 import {CrLitElement, html} from 'chrome://resources/lit/v3_0/lit.rollup.js';
 import {assertEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
 
@@ -17,6 +18,22 @@ class CrDummyLitElement extends CrLitElement {
   static get is() {
     return 'cr-dummy-lit';
   }
+
+  static override get properties() {
+    return {
+      fooBarBoolean: {type: Boolean},
+      fooBarString: {type: String},
+
+      fooBarStringCustom: {
+        attribute: 'foobarstringcustom',
+        type: String,
+      },
+    };
+  }
+
+  fooBarBoolean: boolean = false;
+  fooBarString: string = 'hello';
+  fooBarStringCustom: string = 'hola';
 
   override render() {
     return html`
@@ -36,6 +53,10 @@ customElements.define(CrDummyLitElement.is, CrDummyLitElement);
 
 
 suite('CrLitElement', function() {
+  setup(function() {
+    document.body.innerHTML = window.trustedTypes!.emptyHTML;
+  });
+
   test('DollarSign', async function() {
     const element = document.createElement('cr-dummy-lit');
     document.body.appendChild(element);
@@ -45,5 +66,20 @@ suite('CrLitElement', function() {
     assertEquals('Hello Foo', element.$.foo.textContent);
     assertTrue(!!element.$.bar);
     assertEquals('Hello Bar', element.$.bar.textContent);
+  });
+
+  // Test that properties are initialized correctly from attributes.
+  test('PropertiesAttributesNameMapping', function() {
+    document.body.innerHTML = getTrustedHTML`
+      <cr-dummy-lit foo-bar-boolean foo-bar-string="world"
+          foobarstringcustom="custom">
+      </cr-dummy-lit>
+    `;
+
+    const element = document.body.querySelector('cr-dummy-lit');
+    assertTrue(!!element);
+    assertTrue(element.fooBarBoolean);
+    assertEquals('world', element.fooBarString);
+    assertEquals('custom', element.fooBarStringCustom);
   });
 });
