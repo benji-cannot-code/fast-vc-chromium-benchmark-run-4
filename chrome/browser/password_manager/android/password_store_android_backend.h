8ee8582c22e8c3c601d0f220b36d1b51672e4119
Ioana Pandele <ioanap@chromium.org>
2024-01-08 12:23:29 +0000

    [UPMLocalPwd] Only compute the account in external methods
    This is a pure refactoring.
    
    This CL separates the account computation logic out into the external
    (PasswordStore-facing) methods. This makes is easier to apply
    account-based changes, since they only need to be done at the external layer.
    
    Bug: 306673712
    Change-Id: I2143ae193c7999f5b6425d503519ceec935d5f45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5126851
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243994}

--- chrome/browser/password_manager/android/password_store_android_backend.h
+++ chrome/browser/password_manager/android/password_store_android_backend.h
@@ -218,7 +218,8 @@ class PasswordStoreAndroidBackend
   // PasswordStoreOperation that invoked this method and |delay| is the amount
   // of time by which the call to this method was delayed. Calls
   // GetAutofillableLogins from the PasswordStoreAndroidBackendDispatcherBridge.
-  void GetAutofillableLoginsInternal(LoginsOrErrorReply callback,
+  void GetAutofillableLoginsInternal(std::string account,
+                                     LoginsOrErrorReply callback,
                                      PasswordStoreOperation operation,
                                      base::TimeDelta delay);
 
@@ -233,13 +234,15 @@ class PasswordStoreAndroidBackend
                                       base::TimeDelta delay);
 
   // Gets logins matching |form|.
-  void GetLoginsInternal(const PasswordFormDigest& form,
+  void GetLoginsInternal(std::string account,
+                         const PasswordFormDigest& form,
                          bool include_psl,
                          LoginsOrErrorReply callback,
                          PasswordStoreOperation operation);
 
   // Updates the form in storage with |form|.
-  void UpdateLoginInternal(const PasswordForm& form,
+  void UpdateLoginInternal(std::string account,
+                           const PasswordForm& form,
                            PasswordChangesOrErrorReply callback);
 
   // Removes |form|.
@@ -247,7 +250,6 @@ class PasswordStoreAndroidBackend
   // |delay| is the amount of time by which the call to this method was delayed.
   void RemoveLoginInternal(std::string account,
                            const PasswordForm& form,
-
                            PasswordChangesOrErrorReply callback,
                            PasswordStoreOperation operation,
                            base::TimeDelta delay);
@@ -287,6 +289,7 @@ class PasswordStoreAndroidBackend
   // |operation| is the PasswordStoreOperation  that invoked this method and
   // |delay| is the amount of time by which the call to this method was delayed.
   void FilterAndRemoveLogins(
+      std::string account,
       const base::RepeatingCallback<bool(const GURL&)>& url_filter,
       base::Time delete_begin,
       base::Time delete_end,
@@ -298,6 +301,7 @@ class PasswordStoreAndroidBackend
   // Filters logins that match |origin_filer| and asynchronously disables
   // autosignin by updating stored logins.
   void FilterAndDisableAutoSignIn(
+      std::string account,
       const base::RepeatingCallback<bool(const GURL&)>& origin_filter,
       PasswordChangesOrErrorReply completion,
       LoginsResultOrError result);
