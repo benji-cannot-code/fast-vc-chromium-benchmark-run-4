96f49a86a1d01741468dc8567d8fe5e2fc34e473
Anastasiia N <anastasiian@chromium.org>
2024-01-08 13:02:01 +0000

    cros: Decode mediator public key from hex
    Decode the result value of GetRecoveryHsmPublicKey from hex
    to keep consistent format between serialization/deserialization.
    
    This CL changes the format of the key stored in the recovery
    metadata on disk, and therefore will trigger a recovery migration.
    Note that after https://crrev.com/c/5168565 is merged - the recovery
    id will not be updated on migration.
    
    Bug: b:313074705
    Change-Id: Ic09507a15726c1e00c8e323200f91a8931a0f73d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5163894
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244000}

--- chromeos/ash/components/login/auth/recovery/service_constants.cc
+++ chromeos/ash/components/login/auth/recovery/service_constants.cc
@@ -4,9 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chromeos/ash/components/login/auth/recovery/service_constants.h"
+
 #include "ash/constants/ash_switches.h"
 #include "base/command_line.h"
-
+#include "base/strings/string_number_conversions.h"
 #include "url/gurl.h"
 
 namespace ash {
@@ -76,10 +77,12 @@ GURL GetRecoveryServiceBaseURL() {
 }  // namespace
 
 std::string GetRecoveryHsmPublicKey() {
-  if (IsUsingTestEnvironment()) {
-    return kTestingHsmPublicKey;
-  }
-  return kHsmPublicKey;
+  std::string decoded_key;
+  const bool result = base::HexStringToString(
+      IsUsingTestEnvironment() ? kTestingHsmPublicKey : kHsmPublicKey,
+      &decoded_key);
+  CHECK(result);
+  return decoded_key;
 }
 
 std::string GetRecoveryLedgerName() {
