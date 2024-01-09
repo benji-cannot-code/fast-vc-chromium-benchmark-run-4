8a1b8c263be71d7dd7eaf057c95827be84e298fd
Mohamad Ahmadi <mahmadi@chromium.org>
2024-01-09 03:45:41 +0000

    [omnibox] Use AutocompleteInput::IsZeroSuggest() where possible
    Uses AutocompleteInput::IsZeroSuggest() convenience method where the input focus type is compared to OmniboxFocusType::INTERACTION_DEFAULT
    for improved readability and adoption of the said helper function.
    
    Bug: 1381287
    Change-Id: I9ac2d421a7085242081af5a84cc64007adab90aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5178153
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244443}

--- components/omnibox/browser/open_tab_provider.cc
+++ components/omnibox/browser/open_tab_provider.cc
@@ -96,8 +96,7 @@ OpenTabProvider::~OpenTabProvider() = default;
 void OpenTabProvider::Start(const AutocompleteInput& input,
                             bool minimal_changes) {
   matches_.clear();
-  if (input.focus_type() != metrics::OmniboxFocusType::INTERACTION_DEFAULT ||
-      input.text().empty()) {
+  if (input.IsZeroSuggest() || input.text().empty()) {
     return;
   }
 
