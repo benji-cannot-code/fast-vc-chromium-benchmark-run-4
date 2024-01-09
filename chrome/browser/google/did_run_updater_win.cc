e139e9a0d2f5d762270aedd5f93d8c01d1c88bd1
Joshua Pawlicki <waffles@chromium.org>
2024-01-09 14:14:09 +0000

    Write did-run to registry asynchronously.
    My understanding is that generally we allow registry writes on the main
    sequence. However, they can take time if some other application is
    calling RegFlushKey (which blocks modifications to the entire hive), or
    something else is affecting registry write performance on the machines.
    
    Since neither the sequencing nor outcome of recording did-run is
    relevant to the callers, making the operation non-blocking seems easy
    enough.
    
    Fixed: 1509160
    Change-Id: I3d760bbe13c2b6427705c02524441c1ea0ce87b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5164608
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244641}

--- chrome/browser/google/did_run_updater_win.cc
+++ chrome/browser/google/did_run_updater_win.cc
@@ -9,5 +9,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 void DidRunUpdater::OnRenderProcessHostCreated(
     content::RenderProcessHost* process_host) {
-  installer::UpdateDidRunState(true);
+  installer::UpdateDidRunState();
 }
