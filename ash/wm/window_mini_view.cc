a2ab19dca690c0e567f11523d2953d2f7431db05
Sammie Quon <sammiequon@chromium.org>
2024-01-09 01:07:27 +0000

    wm: Only disable fast rounded corner if rounded window flag is off
    Fixed: b/316018723
    Change-Id: I76a28c438ef10988e4395b7a67c739a8130ce0fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5147122
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244390}

--- ash/wm/window_mini_view.cc
+++ ash/wm/window_mini_view.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/window_mini_view_header_view.h"
 #include "ash/wm/window_preview_view.h"
 #include "ash/wm/window_util.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "chromeos/ui/base/window_properties.h"
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/aura/client/aura_constants.h"
@@ -146,6 +147,9 @@ void WindowMiniView::RefreshPreviewRoundedCorners(bool show) {
   layer->SetRoundedCornerRadius(GetRoundedCornersForPreviewView(
       source_window_, backdrop_view_, preview_view_->GetBoundsInScreen(),
       layer->transform().To2dScale().x(), show, preview_view_rounded_corners_));
+  if (!chromeos::features::IsRoundedWindowsEnabled()) {
+    layer->SetIsFastRoundedCorner(true);
+  }
 }
 
 void WindowMiniView::RefreshHeaderViewRoundedCorners() {
