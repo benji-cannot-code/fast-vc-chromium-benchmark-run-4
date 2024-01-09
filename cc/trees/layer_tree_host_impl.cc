77b0e4b699443f63dd9e179b07fb6d45c04fb5f5
Maggie Chen <magchen@chromium.org>
2024-01-09 17:31:16 +0000

    Update SharedBitmap to SharedImage conversion
    1. Refactor AllocateSharedMemory()
    2. Move shared_image from BitmapSoftwareBacking to the base class
       ResourcePool::SoftwareBacking.
    3. Instead of using the id from |shared_bitmap_id|, use
       |shared_image->mailbox| directly.
    
    Bug: 1434885
    Change-Id: I1d8a4a33d771efe6c50fcd8f18d1d2cdf419ccd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5171163
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244748}

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram.h"
 #include "base/notreached.h"
 #include "base/numerics/safe_conversions.h"
-#include "base/process/memory.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/stringprintf.h"
 #include "base/system/sys_info.h"
@@ -143,24 +142,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 namespace {
-
-base::UnsafeSharedMemoryRegion AllocateSharedMemory(
-    const gfx::Size& size,
-    viz::SharedImageFormat format) {
-  size_t bytes = 0;
-  if (!viz::ResourceSizes::MaybeSizeInBytes(size, format, &bytes)) {
-    DLOG(ERROR) << "AllocateMappedBitmap with size that overflows";
-    size_t alloc_size = std::numeric_limits<int>::max();
-    base::TerminateBecauseOutOfMemory(alloc_size);
-  }
-
-  auto shared_memory = base::UnsafeSharedMemoryRegion::Create(bytes);
-  if (!shared_memory.IsValid()) {
-    DLOG(ERROR) << "Failed to allocate shared memory";
-    base::TerminateBecauseOutOfMemory(bytes);
-  }
-  return shared_memory;
-}
 
 // In BuildHitTestData we iterate all layers to find all layers that overlap
 // OOPIFs, but when the number of layers is greater than
@@ -4712,14 +4693,19 @@ void LayerTreeHostImpl::CreateUIResource(UIResourceId uid,
     auto* sii = layer_tree_frame_sink_->shared_image_interface();
     CHECK(sii);
 
-    auto unsafe_region = AllocateSharedMemory(upload_size, format);
-    mapping = unsafe_region.Map();
+    const size_t buffer_size = gfx::BufferSizeForBufferFormat(
+        upload_size, gfx::BufferFormat::RGBA_8888);
+    auto shared_memory_region =
+        base::UnsafeSharedMemoryRegion::Create(buffer_size);
+    mapping = shared_memory_region.Map();
+    CHECK(shared_memory_region.IsValid() && mapping.IsValid());
+
     gfx::GpuMemoryBufferHandle handle;
     handle.type = gfx::SHARED_MEMORY_BUFFER;
     handle.offset = 0;
     handle.stride = static_cast<int32_t>(gfx::RowSizeForBufferFormat(
         upload_size.width(), gfx::BufferFormat::RGBA_8888, 0));
-    handle.region = unsafe_region.Duplicate();
+    handle.region = std::move(shared_memory_region);
 
     client_shared_image = sii->CreateSharedImage(
         format, upload_size, color_space, kTopLeft_GrSurfaceOrigin,
