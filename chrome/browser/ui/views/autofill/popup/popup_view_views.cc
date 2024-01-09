00a689f73b32d23b7be1277341db3645943c5d1d
Jan Keitel <jkeitel@google.com>
2024-01-09 11:08:00 +0000

    Trigger a11y announcement when showing the Compose nudge.
    Bug: b/316411175
    Change-Id: I91398129f6c940aeffaaa15e9840b988eb18b27e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5176934
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Dmitry Vykochko <vykochko@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1244573}

--- chrome/browser/ui/views/autofill/popup/popup_view_views.cc
+++ chrome/browser/ui/views/autofill/popup/popup_view_views.cc
@@ -206,6 +206,11 @@ bool PopupViewViews::Show(
     absl::get<PopupWarningView*>(rows_[0])->NotifyAccessibilityEvent(
         ax::mojom::Event::kAlert, true);
   }
+  // Compose popups are announced separately.
+  if (controller_->GetMainFillingProduct() == FillingProduct::kCompose) {
+    AxAnnounce(
+        l10n_util::GetStringUTF16(IDS_COMPOSE_SUGGESTION_AX_MESSAGE_ON_SHOW));
+  }
 
   return true;
 }
