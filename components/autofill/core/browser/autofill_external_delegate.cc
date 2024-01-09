a93ceeba9cd412b294eceb175adedd9528dedf6d
Theo Cristea <theocristea@google.com>
2024-01-09 15:35:51 +0000

    [mullet m2] Propagate group filling when triggered from email
    Triggering group filling from an email field used to trigger full form
    filling. Now this is fixed.
    
    Demo: http://go/scrcast/NTc0NDAzNTQ3NzA2MTYzMnwxNGZhNDU0NC03Mw
    
    Bug: 1459990
    Change-Id: I7bfe8637d1ed6bf9d5eba726096376029b4c802f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5177004
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Cr-Commit-Position: refs/heads/main@{#1244678}

--- components/autofill/core/browser/autofill_external_delegate.cc
+++ components/autofill/core/browser/autofill_external_delegate.cc
@@ -1153,7 +1153,8 @@ void AutofillExternalDelegate::FillAutofillFormData(
     static constexpr auto kAutofillAddressSuggestions =
         base::MakeFixedFlatSet<PopupItemId>(
             {PopupItemId::kAddressEntry, PopupItemId::kFillFullAddress,
-             PopupItemId::kFillFullPhoneNumber, PopupItemId::kFillFullName,
+             PopupItemId::kFillFullPhoneNumber, PopupItemId::kFillFullEmail,
+             PopupItemId::kFillFullName,
              PopupItemId::kFillEverythingFromAddressProfile});
     const AutofillField* autofill_trigger_field = GetQueriedAutofillField();
     if (autofill_trigger_field &&
