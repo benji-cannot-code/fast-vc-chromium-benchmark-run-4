405fe8c5a823463846beae2e8091798e5e8034eb
Longbo Wei <longbowei@google.com>
2024-01-09 23:49:57 +0000

    shortcuts: Explicitly call `close` method on the restoreDialog
    Problem:
    * After clicking `Reset` or `Cancel` button in restoreDialog, pressing
      ctrl+f won't focus on search bar as expected.
    
    Reason:
    * Right now, We rely on polymer's property binding to remove the
      cr-dialog from the DOM(<template is="dom-if">), and it won't call
      'close' method on cr-dialog, so the `modalContextOpen` won't be
      updated as expected, causing handleFindShortcut return early.
       - see code: http://gpaste/4787131476606976
    
    Fix:
    * Explicitly call close method on the restoreDialog, so the
      modalContextOpen can be updated as expected.
    
    Before: http://go/scrcast/NDU1NDIzNjM4ODk2NjQwMHxjMjY3NzJhYS0yNQ
    after:  http://go/scrcast/NTc5OTMyOTM1NTc5MjM4NHxkMDU3NWQzYy1jZg
    
    Bug: b/216049298
    Change-Id: Ia914d29dcf3f6cc3da99edbf24c1983c951182b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5182018
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Longbo Wei <longbowei@google.com>
    Cr-Commit-Position: refs/heads/main@{#1244970}

--- ash/webui/shortcut_customization_ui/resources/js/shortcut_customization_app.ts
+++ ash/webui/shortcut_customization_ui/resources/js/shortcut_customization_app.ts
@@ -19,6 +19,7 @@ import {loadTimeData} from 'chrome://resources/ash/common/load_time_data.m.js';
 import {NavigationViewPanelElement} from 'chrome://resources/ash/common/navigation_view_panel.js';
 import {strictQuery} from 'chrome://resources/ash/common/typescript_utils/strict_query.js';
 import {ColorChangeUpdater} from 'chrome://resources/cr_components/color_change_listener/colors_css_updater.js';
+import {CrDialogElement} from 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
 import {CrToolbarSearchFieldElement} from 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar_search_field.js';
 import {FindShortcutMixin} from 'chrome://resources/cr_elements/find_shortcut_mixin.js';
 import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
@@ -320,7 +321,7 @@ export class ShortcutCustomizationAppElement extends
   }
 
   protected onCancelRestoreButtonClicked(): void {
-    this.closeRestoreAllDialog();
+    strictQuery('#restoreDialog', this.shadowRoot, CrDialogElement).close();
   }
 
   protected onConfirmRestoreButtonClicked(): void {
@@ -328,7 +329,7 @@ export class ShortcutCustomizationAppElement extends
       // TODO(jimmyxgong): Explore error state with restore all.
       if (result.result === AcceleratorConfigResult.kSuccess) {
         this.shortcutProvider.recordUserAction(UserAction.kResetAll);
-        this.closeRestoreAllDialog();
+        strictQuery('#restoreDialog', this.shadowRoot, CrDialogElement).close();
       }
     });
   }
