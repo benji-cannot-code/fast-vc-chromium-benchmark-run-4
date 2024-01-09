973fb2e8d83f7367dc0458de3d35f7636f1abe3e
Pin-chih Lin <johnylin@chromium.org>
2024-01-09 06:57:45 +0000

    CrOS: Add experimental feature flag for CRAS DSP Offload
    Expose CrOSLateBootAudioOffloadCrasDSPToSOF to be controllable in
    browser chrome://flags.
    
    State description:
    - Default: depends on Finch experiment state.
    - Enabled: offloads CRAS DSP to Audio Firmware (SOF) if supported.
    - Disabled: never offloads CRAS DSP.
    
    BUG=b:188647460
    TEST=Deploy to Marasov and test the feature toggle in chrome://flags.
         After audio playback via Speaker, check the offload state by
         command `cras_test_client --dump_dsp_offload`.
    
    Change-Id: Ie67abc317494044011d9028b6d99e20b94b3146a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5173450
    Reviewed-by: Li-Yu Yu <aaronyu@google.com>
    Auto-Submit: Pin-chih Lin <johnylin@chromium.org>
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Commit-Queue: Pin-chih Lin <johnylin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244493}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3226,6 +3226,9 @@ extern const char kAudioHFPSwbDescription[];
 extern const char kAudioHFPOffloadName[];
 extern const char kAudioHFPOffloadDescription[];
 
+extern const char kAudioOffloadCrasDSPToSOFName[];
+extern const char kAudioOffloadCrasDSPToSOFDescription[];
+
 extern const char kAudioSuppressSetRTCAudioActiveName[];
 extern const char kAudioSuppressSetRTCAudioActiveDescription[];
 
