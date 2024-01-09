a3b1bfe023448419a13863add562c90967b94414
Dustin J. Mitchell <djmitche@chromium.org>
2024-01-09 23:27:00 +0000

    Factor IP-Prot functionality into a separate ProxyDelegate
    Historically, NetworkServiceProxyDelegate has worked in two "modes",
    either in service of prefetch proxy, or IP protection, as distinguished
    by a boolean in the custom config's rules.
    
    As a step on the way to untangling this situation, this CL factors the
    IP-Protection-related functionality out into a separate ProxyDelegate
    implementation, but for the moment delegates from
    NetworkServiceProxyDelegate to that new delegate when in IP-Protection
    mode. Critically to the correctness of this CL, all of the
    NetworkServiceProxyDelegate tests continue to pass (except three, all of
    which did an incomplete job of putting the delegate in IP-Protection
    mode).
    
    A follow-up will move the prefetch-proxy-related functionality to a new,
    so-named class, and select only one of the two delegates depending on
    the desired mode. At that time the redundant tests currently in
    `network_service_proxy_delegate_unittest.cc` will be deleted.
    
    Bug: 1476881
    Change-Id: Ieedf800683fb3b6777b794417c1472a037b1d19c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5169651
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244950}

--- services/network/network_service_proxy_delegate.h
+++ services/network/network_service_proxy_delegate.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "net/base/proxy_delegate.h"
-#include "services/network/ip_protection/ip_protection_config_cache.h"
+#include "services/network/ip_protection/ip_protection_proxy_delegate.h"
 #include "services/network/masked_domain_list/network_service_proxy_allow_list.h"
 #include "services/network/public/mojom/network_context.mojom.h"
 
@@ -46,7 +46,10 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkServiceProxyDelegate
 
   void SetIpProtectionConfigCache(
       std::unique_ptr<IpProtectionConfigCache> ipp_config_cache) {
-    ipp_config_cache_ = std::move(ipp_config_cache);
+    if (ipp_proxy_delegate_) {
+      ipp_proxy_delegate_->SetIpProtectionConfigCache(
+          std::move(ipp_config_cache));
+    }
   }
 
   // net::ProxyDelegate implementation:
@@ -68,7 +71,9 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkServiceProxyDelegate
       net::ProxyResolutionService* proxy_resolution_service) override;
 
   IpProtectionConfigCache* GetIpProtectionConfigCache() {
-    return ipp_config_cache_.get();
+    return ipp_proxy_delegate_
+               ? ipp_proxy_delegate_->GetIpProtectionConfigCache()
+               : nullptr;
   }
 
  private:
@@ -112,7 +117,7 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkServiceProxyDelegate
 
   raw_ptr<net::ProxyResolutionService> proxy_resolution_service_ = nullptr;
 
-  std::unique_ptr<IpProtectionConfigCache> ipp_config_cache_;
+  std::unique_ptr<IpProtectionProxyDelegate> ipp_proxy_delegate_;
 };
 
 }  // namespace network
