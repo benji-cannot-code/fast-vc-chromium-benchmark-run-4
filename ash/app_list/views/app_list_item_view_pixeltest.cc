5b181b6406ed9ec60be2190268bc74f7b68eeef0
Ana Salazar <anasalazar@chromium.org>
2024-01-09 17:52:54 +0000

    Change app icon mask
    The masked app icon provided to the Launcher/ Shelf is off-center, which is noticeable to the user when a progress ring is drawn around the promise icon.
    
    Update the mask file used to provide icons on the apps service with a new centered mask file.
    
    Bug: b:314018036
    Change-Id: I9b86d14248342c862057ca662b4c536268f4c215
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5177267
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Vivian Pao <vpao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1244754}

--- ash/app_list/views/app_list_item_view_pixeltest.cc
+++ ash/app_list/views/app_list_item_view_pixeltest.cc
@@ -428,7 +428,7 @@ TEST_P(AppListViewPromiseAppPixelTest, PromiseAppWaiting) {
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       base::JoinString({"promise_app_waiting", GenerateScreenshotName()}, "."),
-      /*revision_number=*/0, GetItemViewAt(0), GetItemViewAt(1)));
+      /*revision_number=*/1, GetItemViewAt(0), GetItemViewAt(1)));
 }
 
 TEST_P(AppListViewPromiseAppPixelTest, PromiseAppInstalling) {
@@ -456,7 +456,7 @@ TEST_P(AppListViewPromiseAppPixelTest, PromiseAppInstalling) {
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       base::JoinString({"promise_app_installing", GenerateScreenshotName()},
                        "."),
-      /*revision_number=*/0, GetItemViewAt(0), GetItemViewAt(1)));
+      /*revision_number=*/1, GetItemViewAt(0), GetItemViewAt(1)));
 }
 
 class AppListItemViewWebAppShortcutPixelTest
