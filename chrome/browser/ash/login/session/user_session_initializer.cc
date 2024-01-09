b2cd147a4c1a0a968f023ef8591468c7b413cd5a
Yiren Wang <yrw@chromium.org>
2024-01-09 02:15:45 +0000

    Remove Shell::Get()->media_notification_provider()
    Replace it with MediaNotificationProvider::Get() which is more widely
    used already.
    
    Bug: 1463105
    Change-Id: I8e266e0b23d0180669345398c1ec669ad39d6819
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5178588
    Commit-Queue: Yiren Wang <yrw@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244407}

--- chrome/browser/ash/login/session/user_session_initializer.cc
+++ chrome/browser/ash/login/session/user_session_initializer.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_pref_names.h"
-#include "ash/shell.h"
 #include "ash/system/media/media_notification_provider.h"
 #include "base/files/file_util.h"
 #include "base/path_service.h"
@@ -316,7 +315,7 @@ void UserSessionInitializer::OnUserSessionStarted(bool is_primary_user) {
 
     CrasAudioHandler::Get()->RefreshNoiseCancellationState();
 
-    Shell::Get()->media_notification_provider()->OnPrimaryUserSessionStarted();
+    MediaNotificationProvider::Get()->OnPrimaryUserSessionStarted();
     if (base::FeatureList::IsEnabled(media::kShowForceRespectUiGainsToggle)) {
       CrasAudioHandler::Get()->RefreshForceRespectUiGainsState();
     }
