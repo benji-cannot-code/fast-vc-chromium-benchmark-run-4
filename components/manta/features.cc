b70156fcf96beef6657d90b2ff32b1ed23384f73
Chuong Ho <hdchuong@google.com>
2024-01-09 06:48:03 +0000

    editor: add a flag to switch between prod and autopush server
    This CL switches to use prod server by default. Editor previously
    uses autopush server.
    
    Bug: b:318610628
    Change-Id: I3099bfe932672d5318a552861ed310c0fe80a65a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5174898
    Reviewed-by: Chuong Ho <hdchuong@chromium.org>
    Reviewed-by: Xinglong Luan <alanlxl@chromium.org>
    Reviewed-by: Mehrab N <mehrab@chromium.org>
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244489}

--- components/manta/features.cc
+++ components/manta/features.cc
@@ -11,8 +11,17 @@ namespace manta::features {
 
 BASE_FEATURE(kMantaService, "MantaService", base::FEATURE_ENABLED_BY_DEFAULT);
 
+// Enables Orca Prod Server
+BASE_FEATURE(kOrcaUseProdServer,
+             "OrcaUseProdServer",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 bool IsMantaServiceEnabled() {
   return base::FeatureList::IsEnabled(kMantaService);
+}
+
+bool IsOrcaUseProdServerEnabled() {
+  return base::FeatureList::IsEnabled(kOrcaUseProdServer);
 }
 
 }  // namespace manta::features
--- components/manta/orca_provider.cc
+++ components/manta/orca_provider.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/values.h"
 #include "components/endpoint_fetcher/endpoint_fetcher.h"
+#include "components/manta/features.h"
 #include "components/manta/manta_status.h"
 #include "components/manta/proto/manta.pb.h"
 #include "components/signin/public/base/consent_level.h"
@@ -29,8 +30,9 @@ namespace {
 constexpr char kOauthConsumerName[] = "manta_orca";
 constexpr char kHttpMethod[] = "POST";
 constexpr char kHttpContentType[] = "application/x-protobuf";
-constexpr char kEndpointUrl[] =
+constexpr char kAutopushEndpointUrl[] =
     "https://autopush-aratea-pa.sandbox.googleapis.com/generate";
+constexpr char kProdEndpointUrl[] = "https://aratea-pa.googleapis.com/generate";
 constexpr char kOAuthScope[] = "https://www.googleapis.com/auth/mdi.aratea";
 constexpr base::TimeDelta kTimeoutMs = base::Seconds(30);
 
@@ -53,6 +55,11 @@ absl::optional<Tone> GetTone(const std::string& tone) {
 
   return iter != tone_map.end() ? absl::optional<Tone>(iter->second)
                                 : absl::nullopt;
+}
+
+std::string GetEndpointUrl() {
+  return features::IsOrcaUseProdServerEnabled() ? kProdEndpointUrl
+                                                : kAutopushEndpointUrl;
 }
 
 absl::optional<proto::Request> ComposeRequest(
@@ -146,7 +153,7 @@ void OrcaProvider::Call(const std::map<std::string, std::string>& input,
   request.value().SerializeToString(&serialized_request);
 
   std::unique_ptr<EndpointFetcher> fetcher = CreateEndpointFetcher(
-      GURL{kEndpointUrl}, {kOAuthScope}, serialized_request);
+      GURL{GetEndpointUrl()}, {kOAuthScope}, serialized_request);
 
   EndpointFetcher* const fetcher_ptr = fetcher.get();
   MantaProtoResponseCallback internal_callback = base::BindOnce(
