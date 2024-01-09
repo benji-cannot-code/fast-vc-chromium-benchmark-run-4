ca3c061b7a57f57dc4d001924c3447af480d2200
Marja Hölttä <marja@chromium.org>
2024-01-09 15:51:38 +0000

    [v8 compile hints] Enable crowdsouced compile hints by default
    Bug: chromium:1406506
    Change-Id: Ibf485d5443a7720d29996d56e9901cf9452286f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5178196
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Marja Hölttä <marja@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244685}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1626,7 +1626,7 @@ BASE_FEATURE(kForceProduceCompileHints,
 
 BASE_FEATURE(kConsumeCompileHints,
              "ConsumeCompileHints",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kLocalCompileHints,
              "LocalCompileHints",
