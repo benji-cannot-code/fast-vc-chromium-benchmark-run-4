7fbeb8d1a37f663a87f58a2413c3dbd8bb6b4003
Garrett Tanzer <gtanzer@chromium.org>
2024-01-09 14:11:45 +0000

    Fenced frames: Remove const accessor for FencedFrameProperties
    Now that FencedFrameProperties is a class with private fields, there is
    no need to have a const vs. nonconst accessor. Mutation of the object is
    mediated by class methods.
    
    Change-Id: I8054f6555e7fb16b4fc2726b36e813a392a37ef7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5176873
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Garrett Tanzer <gtanzer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244640}

--- content/browser/renderer_host/frame_tree_node.cc
+++ content/browser/renderer_host/frame_tree_node.cc
@@ -908,14 +908,7 @@ bool FrameTreeNode::IsInFencedFrameTree() const {
   return fenced_frame_status_ != FencedFrameStatus::kNotNestedInFencedFrame;
 }
 
-const absl::optional<FencedFrameProperties>&
-FrameTreeNode::GetFencedFrameProperties(
-    FencedFramePropertiesNodeSource node_source) {
-  return GetFencedFramePropertiesForEditing(node_source);
-}
-
-absl::optional<FencedFrameProperties>&
-FrameTreeNode::GetFencedFramePropertiesForEditing(
+absl::optional<FencedFrameProperties>& FrameTreeNode::GetFencedFrameProperties(
     FencedFramePropertiesNodeSource node_source) {
   if (node_source == FencedFramePropertiesNodeSource::kFrameTreeRoot) {
     return frame_tree().root()->fenced_frame_properties_;
@@ -939,7 +932,7 @@ FrameTreeNode::GetFencedFramePropertiesForEditing(
 void FrameTreeNode::MaybeResetFencedFrameAutomaticBeaconReportEventData(
     blink::mojom::AutomaticBeaconType event_type) {
   absl::optional<FencedFrameProperties>& properties =
-      GetFencedFramePropertiesForEditing();
+      GetFencedFrameProperties();
   // `properties` will exist for both fenced frames as well as iframes loaded
   // with a urn:uuid.
   if (!properties) {
@@ -955,7 +948,7 @@ void FrameTreeNode::SetFencedFrameAutomaticBeaconReportEventData(
     bool once,
     bool cross_origin_exposed) {
   absl::optional<FencedFrameProperties>& properties =
-      GetFencedFramePropertiesForEditing();
+      GetFencedFrameProperties();
   // `properties` will exist for both fenced frames as well as iframes loaded
   // with a urn:uuid. This allows URN iframes to call this function without
   // getting bad-messaged.
@@ -1095,7 +1088,7 @@ FrameTreeNode::FindSharedStorageBudgetMetadata() {
 absl::optional<std::u16string>
 FrameTreeNode::GetEmbedderSharedStorageContextIfAllowed() {
   absl::optional<FencedFrameProperties>& properties =
-      GetFencedFramePropertiesForEditing();
+      GetFencedFrameProperties();
   // We only return embedder context for frames that are same origin with the
   // fenced frame root or ancestor URN iframe.
   if (!properties || !properties->mapped_url().has_value() ||
