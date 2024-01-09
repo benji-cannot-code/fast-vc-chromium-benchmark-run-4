c1a78aa4e6f04a1cf47399fe0bf1c1e49b251a02
Austin Sullivan <asully@chromium.org>
2024-01-09 10:57:17 +0000

    Code Health: Remove unused FileUtilsWrapper::PathIsWritable method
    Change-Id: Ieed0ffb10b5e5bc0484d2d72670aeb385b1e5c8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5169818
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244570}

--- chrome/browser/web_applications/file_utils_wrapper.cc
+++ chrome/browser/web_applications/file_utils_wrapper.cc
@@ -16,10 +16,6 @@ bool FileUtilsWrapper::PathExists(const base::FilePath& path) {
   return base::PathExists(path);
 }
 
-bool FileUtilsWrapper::PathIsWritable(const base::FilePath& path) {
-  return base::PathIsWritable(path);
-}
-
 bool FileUtilsWrapper::DirectoryExists(const base::FilePath& path) {
   return base::DirectoryExists(path);
 }
