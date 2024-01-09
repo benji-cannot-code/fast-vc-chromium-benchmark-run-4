2c26f33c3f0263284865b26a442eb4c3f9af2144
James Sun <jamessun@google.com>
2024-01-09 18:11:51 +0000

    [IBAN][Clank]Add skeleton IBAN Clank bottom sheet bridge
    This CL defines the skeleton bridge on C++ and Java sides, more
    functions, i.e. coordinator, mediator, etc. will be added shortly after.
    Reason of putting C++ and Java bridge together is to make this CL more
    completed.
    
    Bug: b:309163412
    Change-Id: I4c7282b762d69d10e32d098d90d8aeef227c0d17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5132121
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Commit-Queue: James Sun <jamessun@google.com>
    Cr-Commit-Position: refs/heads/main@{#1244761}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/iban/AutofillSaveIbanBottomSheetBridge.java
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.autofill.iban;
+
+import androidx.annotation.VisibleForTesting;
+
+import org.jni_zero.CalledByNative;
+import org.jni_zero.JNINamespace;
+
+/** JNI wrapper to trigger Android bottom sheet prompting the user to save their IBAN locally. */
+@JNINamespace("autofill")
+class AutofillSaveIbanBottomSheetBridge {
+    private long mNativeAutofillSaveIbanBottomSheetBridge;
+
+    @CalledByNative
+    @VisibleForTesting
+    AutofillSaveIbanBottomSheetBridge(long nativeAutofillSaveIbanBottomSheetBridge) {
+        mNativeAutofillSaveIbanBottomSheetBridge = nativeAutofillSaveIbanBottomSheetBridge;
+    }
+
+    @CalledByNative
+    public void requestShowContent(String ibanLabel) {}
+}
