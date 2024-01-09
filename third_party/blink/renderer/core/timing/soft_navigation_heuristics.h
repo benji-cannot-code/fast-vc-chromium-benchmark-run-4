64be6e19183d9114b02257229a207282535bdba6
Scott Haseley <shaseley@chromium.org>
2024-01-09 23:49:03 +0000

    TaskAttribution: Remove TaskAttributionInfo prefinalizer
    TaskAttributionInfo disposal tracking is needed to reset the soft nav
    heuristics when all descendant tasks of potential soft nav tasks have
    completed. Instead of using a prefinalizer for this, we can observe the
    set of root tasks becoming empty using a custom weakness callback, like
    so:
     1. Change the set to HeapHashSet<WeakMember<TaskAttributionInfo>>
     2. Trace() the set. This will register a weak container callback, which
        will clear weak members of the set.
     3. After tracing the set, if the set is non-empty, add a custom
        weakness callback. This will run after the weak container callback,
        and it can check if the set became empty.
    
    This enables removing the prefinalizer, which will also enable moving to
    a per-isolate TaskAttributionTracker without needing to store the
    tracker or isolate in the TaskAttributionInfo.
    
    Bug: 1351643
    Change-Id: Ib66b23492f18cec0572e6b8813f8a131f6f79699
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5078469
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244969}

--- third_party/blink/renderer/core/timing/soft_navigation_heuristics.h
+++ third_party/blink/renderer/core/timing/soft_navigation_heuristics.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/scheduler/task_attribution_id.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
+#include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_set.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/scheduler/public/task_attribution_tracker.h"
 #include "third_party/blink/renderer/platform/supplementable.h"
@@ -74,7 +75,6 @@ class CORE_EXPORT SoftNavigationHeuristics
   // TaskAttributionTracker::Observer's implementation.
   void OnCreateTaskScope(scheduler::TaskAttributionInfo&,
                          ScriptState*) override;
-  void OnTaskDisposal(const scheduler::TaskAttributionInfo&) override;
   ExecutionContext* GetExecutionContext() override;
 
   void RecordPaint(LocalFrame*,
@@ -88,8 +88,17 @@ class CORE_EXPORT SoftNavigationHeuristics
   // current_event_parameters_ and return true. Otherwise, return false.
   bool PopNestedEventParametersIfNeeded();
 
+  // This method is called during the weakness processing stage of garbage
+  // collection, and it's used to detect `potential_soft_navigation_tasks_`
+  // becoming empty.
+  void ProcessCustomWeakness(const LivenessBroker& info);
+
   bool GetInitialInteractionEncounteredForTest() {
     return initial_interaction_encountered_;
+  }
+
+  scheduler::TaskAttributionIdType GetLastInteractionTaskIdForTest() const {
+    return last_interaction_task_id_;
   }
 
  private:
@@ -125,9 +134,8 @@ class CORE_EXPORT SoftNavigationHeuristics
 
   PerInteractionData* GetCurrentInteractionData(scheduler::TaskAttributionId);
 
-  WTF::HashSet<scheduler::TaskAttributionIdType>
-      potential_soft_navigation_task_ids_;
-  size_t disposed_soft_navigation_tasks_ = 0;
+  HeapHashSet<WeakMember<const scheduler::TaskAttributionInfo>>
+      potential_soft_navigation_tasks_;
   WTF::HashMap<scheduler::TaskAttributionIdType,
                absl::optional<scheduler::TaskAttributionId>>
       soft_navigation_descendant_cache_;
@@ -160,12 +168,15 @@ class CORE_EXPORT SoftNavigationHeuristics
   EventParameters top_event_parameters_;
   WTF::Deque<EventParameters> nested_event_parameters_;
   EventParameters* current_event_parameters_ = nullptr;
+  // Used to synchronize resetting the heuristic when
+  // `potential_soft_navigation_tasks_` becomes empty during GC.
+  bool has_potential_soft_navigation_task_ = false;
 };
 
 // This class defines a scope that would cover click or navigation related
 // events, in order for the SoftNavigationHeuristics class to be able to keep
 // track of them and their descendant tasks.
-class SoftNavigationEventScope {
+class CORE_EXPORT SoftNavigationEventScope {
  public:
   SoftNavigationEventScope(SoftNavigationHeuristics* heuristics,
                            SoftNavigationHeuristics::EventScopeType type,
