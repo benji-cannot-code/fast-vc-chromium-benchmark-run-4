dcea867f9752dfa2029800f7577b92377adb8db8
Friedrich Horschig <fhorschig@chromium.org>
2024-01-09 17:19:53 +0000

    [Android] Inline platform-check from keyboard accessory helper
    The helper is outated since the feature is gone. It's also misleading in
    that it returns true on WebView where a popup could open.
    None of the usages (outside the renderer, fixed in the parent CL) affect
    WebView since it shouldn't use CreditCard popup code.
    A lot of usages were tautological (i.e. inside another BUILD guard).
    
    Bug: 1515887
    Change-Id: Ifaecd36d9fc35cca23773b27a820d513c6c27d1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5176971
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244742}

--- components/autofill/core/common/autofill_util.cc
+++ components/autofill/core/common/autofill_util.cc
@@ -12,10 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/command_line.h"
-#include "base/feature_list.h"
 #include "base/i18n/rtl.h"
-#include "base/metrics/field_trial.h"
-#include "base/metrics/field_trial_params.h"
 #include "base/no_destructor.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
@@ -23,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_split.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
-#include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/autofill_switches.h"
 
 namespace autofill {
@@ -54,14 +50,6 @@ struct Compare : base::CaseInsensitiveCompareASCII<Char> {
 bool IsShowAutofillSignaturesEnabled() {
   return base::CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kShowAutofillSignatures);
-}
-
-bool IsKeyboardAccessoryEnabled() {
-#if BUILDFLAG(IS_ANDROID)
-  return true;
-#else  // !BUILDFLAG(IS_ANDROID)
-  return false;
-#endif
 }
 
 bool IsPrefixOfEmailEndingWithAtSign(const std::u16string& full_string,
