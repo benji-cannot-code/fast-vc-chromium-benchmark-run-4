4dcc53f7241a6edbd76a8ae8028f2d6ba50e1e2b
Artsiom Mitrokhin <amitrokhin@google.com>
2024-01-09 21:27:14 +0000

    Auto-enable calendar changes for glanceables stable launch
    Previously, `GlanceablesV2CalendarView` required `GlanceablesV2` to be
    turned on. This CL auto-enables it if one of the stable launch view
    flags is enabled.
    
    Bug: b/315829928
    Change-Id: I8a02baa431c3fbfde56c0bb75656f7cb3d78c1ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5181594
    Commit-Queue: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Sylvie Liu <sylvieliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244878}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -3528,7 +3528,8 @@ bool IsGlanceablesV2ClassroomTeacherViewEnabled() {
 }
 
 bool IsGlanceablesV2CalendarViewEnabled() {
-  return base::FeatureList::IsEnabled(kGlanceablesV2CalendarView);
+  return base::FeatureList::IsEnabled(kGlanceablesV2CalendarView) ||
+         AreAnyGlanceablesTimeManagementViewsEnabled();
 }
 
 bool IsGlanceablesV2ErrorMessageEnabled() {
