6d2fe44430cae9ac178cccbd15bfbd96909d12f0
Dibyajyoti Pal <dibyapal@google.com>
2024-01-09 20:01:15 +0000

    [Code Health Rotation] Migrate WebAppFrameToolbarInteractiveUiTest to Kombucha
    This CL attempts to deflake the WebAppFrameToolbar interactive UI tests
    by migrating it to the Kombucha framework, and enabling it. To do so, it
    does the following:
    1. Breaks up the big test into smaller ones, each verifying small
    behaviors.
    2. Converts actions like simulating focus advances to Kombucha verbs.
    
    Bug: 1176121
    Change-Id: Ic07adad2ccf54fdf7e3ccdeaef9dff3d0b644568
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5160468
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244833}

--- chrome/browser/ui/views/toolbar/reload_button.cc
+++ chrome/browser/ui/views/toolbar/reload_button.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/command_updater.h"
 #include "chrome/browser/external_protocol/external_protocol_handler.h"
 #include "chrome/browser/themes/theme_properties.h"
+#include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/view_ids.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/vector_icons/vector_icons.h"
@@ -28,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/metrics.h"
+#include "ui/views/view_class_properties.h"
 #include "ui/views/widget/widget.h"
 
 // ReloadButton ---------------------------------------------------------------
@@ -53,6 +55,7 @@ ReloadButton::ReloadButton(CommandUpdater* command_updater)
   SetTriggerableEventFlags(ui::EF_LEFT_MOUSE_BUTTON |
                            ui::EF_MIDDLE_MOUSE_BUTTON);
   SetAccessibleName(l10n_util::GetStringUTF16(IDS_ACCNAME_RELOAD));
+  SetProperty(views::kElementIdentifierKey, kReloadButtonElementId);
   SetID(VIEW_ID_RELOAD_BUTTON);
 }
 
