fb785ab44b4935e53964e36710afd79fba98a61f
Ted Choc <tedchoc@chromium.org>
2024-01-09 16:55:36 +0000

    Remove getLastUsedRegularProfile from TabSwitcherMessageManager.
    This also removes the usage from TabSwitcherCoordinator.
    
    In cases where ProfileManager.isInitialized() was checked, this moves
    those calls to places where the Profile is guaranteed to be non-null.
    
    This unconditionally enables the large message format because the
    PriceTracking feature was not reliably available, and the registration
    "should" have no negative side-effects.
    
    Bug: 1410601
    Change-Id: I5da56243f48801599d130744f3e13e4d931cc6e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5178887
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244724}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherMessageManagerUnitTest.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherMessageManagerUnitTest.java
@@ -131,7 +131,7 @@ public class TabSwitcherMessageManagerUnitTest {
         mMessageManager.addObserver(mMessageUpdateObserver);
 
         mMessageManager.setPriceMessageServiceForTesting(mPriceMessageService);
-        mMessageManager.initWithNative();
+        mMessageManager.initWithNative(mProfile);
 
         assertTrue(mCurrentTabModelFilterSupplier.hasObservers());
     }
