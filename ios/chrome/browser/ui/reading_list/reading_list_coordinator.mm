832fed99a5619446f9082cde2e869f7f418a8046
Jood Hajeer <jood@google.com>
2024-01-09 13:43:36 +0000

    [iOS] Show signin promo in reading list manager with left-behind data
    This promo was hidden from the RL manager in crrev.com/c/4580010 to
    avoid enabling Bookmarks for users with sync left-behind data that used
    to cause duplicates.
    Currently, these duplicates are handled in a separate upload promo in
    the bookmarks manager, so it is safe to show the sign-in promo again in
    the RL manager.
    
    The change is controlled through kEnableBatchUploadFromBookmarksManager.
    
    Fixed: 1474603
    Change-Id: I8b5d90b0887c8b72cbfe8c7f10c51a71bae773ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5171576
    Commit-Queue: Jood Hajeer <jood@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244629}

--- ios/chrome/browser/ui/reading_list/reading_list_coordinator.mm
+++ ios/chrome/browser/ui/reading_list/reading_list_coordinator.mm
@@ -685,9 +685,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   } else {
     const std::string lastSignedInGaiaId =
         _prefService->GetString(prefs::kGoogleServicesLastSyncingGaiaId);
-    // If the last signed-in user did not remove data during sign-out, don't
-    // show the signin promo.
-    self.shouldShowSignInPromo = lastSignedInGaiaId.empty();
+    // Show the promo if the last syncing user signed out and chose to clear
+    // data, or if the feature kEnableBatchUploadFromBookmarksManager is
+    // enabled.
+    self.shouldShowSignInPromo =
+        lastSignedInGaiaId.empty() ||
+        base::FeatureList::IsEnabled(kEnableBatchUploadFromBookmarksManager);
   }
 }
 
