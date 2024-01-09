cca43bc61c8958da6f31424f0fb77ed00dff7ec4
Daniel Classon <dclasson@google.com>
2024-01-09 23:22:21 +0000

    [Nearby Presence] Add Fake BluetoothDevice, pass Device to GattClient
    Uses FakeBluetoothDevice for testing, left mostly unimplemented
    for now. More unit testing in following CLs. Pass a mojo remote
    to the GattClient to call adapter methods.
    
    Bug: b/316395226
    Test: Unit tests.
    Change-Id: I55b2149991e5a7ad2c2581fa181f672da21d679b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5132560
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Daniel Classon <dclasson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1244943}

--- chrome/services/sharing/nearby/platform/ble_v2_gatt_client.cc
+++ chrome/services/sharing/nearby/platform/ble_v2_gatt_client.cc
@@ -5,13 +5,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/services/sharing/nearby/platform/ble_v2_gatt_client.h"
 
+#include "base/logging.h"
 #include "base/notreached.h"
 
 namespace nearby::chrome {
 
-BleV2GattClient::BleV2GattClient() = default;
+BleV2GattClient::BleV2GattClient(
+    mojo::PendingRemote<bluetooth::mojom::Device> device)
+    : remote_device_(std::move(device)) {
+  // TODO(b/311430390): For now, just tear down the connection on disconnect. In
+  // the future, this should call Disconnect.
+  remote_device_.reset_on_disconnect();
+}
 
-BleV2GattClient::~BleV2GattClient() = default;
+BleV2GattClient::~BleV2GattClient() {
+  Disconnect();
+}
 
 bool BleV2GattClient::DiscoverServiceAndCharacteristics(
     const Uuid& service_uuid,
@@ -56,8 +65,9 @@ bool BleV2GattClient::SetCharacteristicSubscription(
 }
 
 void BleV2GattClient::Disconnect() {
-  // TODO(b/311430390): Implement this function.
-  NOTIMPLEMENTED();
+  // TODO(b/311430390): For now, just tear down the connection when we call
+  // Disconnect. In the future this should clean up state.
+  remote_device_.reset();
 }
 
 }  // namespace nearby::chrome
--- chrome/services/sharing/nearby/platform/ble_v2_gatt_client.h
+++ chrome/services/sharing/nearby/platform/ble_v2_gatt_client.h
@@ -6,13 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_SERVICES_SHARING_NEARBY_PLATFORM_BLE_V2_GATT_CLIENT_H_
 #define CHROME_SERVICES_SHARING_NEARBY_PLATFORM_BLE_V2_GATT_CLIENT_H_
 
+#include "device/bluetooth/public/mojom/device.mojom.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
+#include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/nearby/src/internal/platform/implementation/ble_v2.h"
 
 namespace nearby::chrome {
 
 class BleV2GattClient : public ::nearby::api::ble_v2::GattClient {
  public:
-  BleV2GattClient();
+  explicit BleV2GattClient(
+      mojo::PendingRemote<bluetooth::mojom::Device> device);
   ~BleV2GattClient() override;
 
   BleV2GattClient(const BleV2GattClient&) = delete;
@@ -37,6 +41,9 @@ class BleV2GattClient : public ::nearby::api::ble_v2::GattClient {
       absl::AnyInvocable<void(absl::string_view value)>
           on_characteristic_changed_cb) override;
   void Disconnect() override;
+
+ private:
+  mojo::Remote<bluetooth::mojom::Device> remote_device_;
 };
 
 }  // namespace nearby::chrome
