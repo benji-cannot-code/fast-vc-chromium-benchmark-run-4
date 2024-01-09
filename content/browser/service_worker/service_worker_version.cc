7ad67206021b190e1185ecd5b9a407290aa5fd1e
punithnayak <npunith125@gmail.com>
2024-01-09 10:37:39 +0000

    Remove global mojo::ReportBadMessage() / mojo::GetMessageCallback() functions in service_worker_version.cc
    Bug: 1213505
    Change-Id: Ib46f5e57495d0da1a73f645e49114eb540b691fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5175072
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244564}

--- content/browser/service_worker/service_worker_version.cc
+++ content/browser/service_worker/service_worker_version.cc
@@ -1595,7 +1595,8 @@ void ServiceWorkerVersion::ClaimClients(ClaimClientsCallback callback) {
       context_->GetLiveRegistration(registration_id_);
   // Registration must be kept alive by ServiceWorkerGlobalScope#registration.
   if (!registration) {
-    mojo::ReportBadMessage("ClaimClients: No live registration");
+    associated_interface_receiver_.ReportBadMessage(
+        "ClaimClients: No live registration");
     // ReportBadMessage() will kill the renderer process, but Mojo complains if
     // the callback is not run. Just run it with nonsense arguments.
     std::move(callback).Run(blink::mojom::ServiceWorkerErrorType::kUnknown,
@@ -1680,7 +1681,7 @@ void ServiceWorkerVersion::OpenPaymentHandlerWindow(
   }
 
   if (!url.is_valid() || !key_.origin().IsSameOriginWith(url)) {
-    mojo::ReportBadMessage(
+    associated_interface_receiver_.ReportBadMessage(
         "Received PaymentRequestEvent#openWindow() request for a cross-origin "
         "URL.");
     receiver_.reset();
@@ -1721,7 +1722,7 @@ void ServiceWorkerVersion::PostMessageToClient(
 
   if (container_host->url().DeprecatedGetOriginAsURL() !=
       script_url_.DeprecatedGetOriginAsURL()) {
-    mojo::ReportBadMessage(
+    associated_interface_receiver_.ReportBadMessage(
         "Received Client#postMessage() request for a cross-origin client.");
     receiver_.reset();
     return;
@@ -1749,7 +1750,7 @@ void ServiceWorkerVersion::PostMessageToClient(
     // ServiceWorkerObjectHost not associated with its
     // ServiceWorkerContainerHost. If that world should occur, we should queue
     // the message instead of crashing.
-    mojo::ReportBadMessage(
+    associated_interface_receiver_.ReportBadMessage(
         "Received Client#postMessage() request for a reserved client.");
     receiver_.reset();
     return;
@@ -1775,14 +1776,14 @@ void ServiceWorkerVersion::FocusClient(const std::string& client_uuid,
   }
   if (container_host->url().DeprecatedGetOriginAsURL() !=
       script_url_.DeprecatedGetOriginAsURL()) {
-    mojo::ReportBadMessage(
+    associated_interface_receiver_.ReportBadMessage(
         "Received WindowClient#focus() request for a cross-origin client.");
     receiver_.reset();
     return;
   }
   if (!container_host->IsContainerForWindowClient()) {
     // focus() should be called only for WindowClient.
-    mojo::ReportBadMessage(
+    associated_interface_receiver_.ReportBadMessage(
         "Received WindowClient#focus() request for a non-window client.");
     receiver_.reset();
     return;
@@ -1804,7 +1805,7 @@ void ServiceWorkerVersion::NavigateClient(const std::string& client_uuid,
 
   if (!url.is_valid() ||
       !base::Uuid::ParseCaseInsensitive(client_uuid).is_valid()) {
-    mojo::ReportBadMessage(
+    associated_interface_receiver_.ReportBadMessage(
         "Received unexpected invalid URL/UUID from renderer process.");
     receiver_.reset();
     return;
@@ -1831,14 +1832,14 @@ void ServiceWorkerVersion::NavigateClient(const std::string& client_uuid,
   }
   if (container_host->url().DeprecatedGetOriginAsURL() !=
       script_url_.DeprecatedGetOriginAsURL()) {
-    mojo::ReportBadMessage(
+    associated_interface_receiver_.ReportBadMessage(
         "Received WindowClient#navigate() request for a cross-origin client.");
     receiver_.reset();
     return;
   }
   if (!container_host->IsContainerForWindowClient()) {
     // navigate() should be called only for WindowClient.
-    mojo::ReportBadMessage(
+    associated_interface_receiver_.ReportBadMessage(
         "Received WindowClient#navigate() request for a non-window client.");
     receiver_.reset();
     return;
@@ -1899,7 +1900,7 @@ void ServiceWorkerVersion::RegisterRouter(
     RegisterRouterCallback callback) {
   if (!IsStaticRouterEnabled()) {
     // This renderer should have called this only when the feature is enabled.
-    receiver_.ReportBadMessage(
+    associated_interface_receiver_.ReportBadMessage(
         "Unexpected router registration call during the feature is disabled.");
     return;
   }
@@ -1912,7 +1913,7 @@ void ServiceWorkerVersion::RegisterRouter(
       // The renderer should have denied calling both RegisterRouter() and
       // AddRoutes().
       CHECK(router_evaluator());
-      receiver_.ReportBadMessage(
+      associated_interface_receiver_.ReportBadMessage(
           "The ServiceWorker router rules are set twice.");
       return;
   }
@@ -1920,7 +1921,7 @@ void ServiceWorkerVersion::RegisterRouter(
     // The renderer should have denied calling this method while the setup
     // fails.
     // TODO(crbug.com/1371756): revisit this to confirm no case for this error.
-    receiver_.ReportBadMessage(
+    associated_interface_receiver_.ReportBadMessage(
         "Failed to configure a router. Possibly a syntax error");
     return;
   }
@@ -1933,7 +1934,7 @@ void ServiceWorkerVersion::AddRoutes(
     RegisterRouterCallback callback) {
   if (!IsStaticRouterEnabled()) {
     // This renderer should have called this only when the feature is enabled.
-    receiver_.ReportBadMessage(
+    associated_interface_receiver_.ReportBadMessage(
         "Unexpected router registration call during the feature is disabled.");
     return;
   }
@@ -1945,7 +1946,7 @@ void ServiceWorkerVersion::AddRoutes(
       // The renderer should have denied calling both RegisterRouter() and
       // AddRoutes().
       CHECK(router_evaluator());
-      receiver_.ReportBadMessage(
+      associated_interface_receiver_.ReportBadMessage(
           "The ServiceWorker router rules are set twice.");
       return;
   }
@@ -1953,7 +1954,7 @@ void ServiceWorkerVersion::AddRoutes(
     // The renderer should have denied calling this method while the setup
     // fails.
     // TODO(crbug.com/1371756): revisit this to confirm no case for this error.
-    receiver_.ReportBadMessage(
+    associated_interface_receiver_.ReportBadMessage(
         "Failed to configure a router. Possibly a syntax error");
     return;
   }
@@ -1997,7 +1998,7 @@ void ServiceWorkerVersion::OpenWindow(
   }
 
   if (!url.is_valid()) {
-    mojo::ReportBadMessage(
+    associated_interface_receiver_.ReportBadMessage(
         "Received unexpected invalid URL from renderer process.");
     receiver_.reset();
     return;
