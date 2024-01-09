4b3753bda2e77910a48f05e695d06b88cbed3612
Nicolas Dossou-Gbete <dgn@chromium.org>
2024-01-09 13:18:36 +0000

    🧇 Defer to component helpers to enable updated settings on Android
    Deferring to `search_engines::ShouldShowUpdatedSettings()` matches the
    behaviour of iOS and fixes a divergence in how the flags behave on
    Android compared to Desktop as well.
    
    Bug: b:315126434
    Bug: b:318820137
    Change-Id: I797993cf16c8884050fe881457e0a896b68d6cb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5169070
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244616}

--- components/search_engines/android/template_url_service_android.h
+++ components/search_engines/android/template_url_service_android.h
@@ -139,6 +139,11 @@ class TemplateUrlServiceAndroid : public TemplateURLServiceObserver {
   // returns.
   jboolean IsEeaChoiceCountry(JNIEnv* env);
 
+  // Returns whether the version of the search engines settings screen showing
+  // additional search engine info should be shown.
+  // TODO(b/318824817): To be removed post-launch.
+  jboolean ShouldShowUpdatedSettings(JNIEnv* env);
+
  private:
   bool IsDefaultSearchEngineGoogle();
 
