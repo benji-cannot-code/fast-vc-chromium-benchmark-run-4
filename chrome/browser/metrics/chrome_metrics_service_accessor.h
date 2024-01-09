53c51706cc6bf600d0e3d8cc13318ea690969043
Sean Maher <spvw@chromium.org>
2024-01-09 17:06:37 +0000

    Remove unused ChromeProcessSingleton references in metrics service
    ProcessSingleton no longer uses synthetic field trials.
    
    Change-Id: Id41d7f914aad778222869d04de4c678ea467865e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5164092
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Auto-Submit: Sean Maher <spvw@chromium.org>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Commit-Queue: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1244736}

--- chrome/browser/metrics/chrome_metrics_service_accessor.h
+++ chrome/browser/metrics/chrome_metrics_service_accessor.h
@@ -21,7 +21,6 @@ class BrowserProcessImpl;
 class CampaignsManagerClientImpl;
 class ChromeMetricsServiceClient;
 class ChromePasswordManagerClient;
-class ChromeProcessSingleton;
 class ChromeVariationsServiceClient;
 class HttpsFirstModeService;
 class NavigationMetricsRecorder;
@@ -159,7 +158,6 @@ class ChromeMetricsServiceAccessor : public metrics::MetricsServiceAccessor {
   friend class segmentation_platform::FieldTrialRegisterImpl;
   friend class ChromeMetricsServiceClient;
   friend class ChromePasswordManagerClient;
-  friend class ChromeProcessSingleton;
   friend class NavigationMetricsRecorder;
   friend class ChromeBrowserMainExtraPartsGpu;
   friend class Browser;
