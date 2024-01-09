a0c26c6c926d34c6ed4682905808e691e330b043
Anthony Garant <anthonygarant@chromium.org>
2024-01-09 22:33:15 +0000

    Enforce conversions permission policy checks in AttributionHost
    Add permission policy check for RegisterDataHost,
    RegisterNavigationDataHost and
    NotifyNavigationWithBackgroundRegistrationsWillStart in AttributionHost.
    
    The permission policy checks are currently performed in the renderer,
    enforce the checks in browser in case of a compromised renderer.
    
    Bug: 1092937
    Change-Id: Ib16e69e657d4bf7e10b78317b8aca85b2dedca43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5160052
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Anthony Garant <anthonygarant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244917}

--- content/browser/attribution_reporting/attribution_host.cc
+++ content/browser/attribution_reporting/attribution_host.cc
@@ -323,6 +323,11 @@ AttributionHost::TopFrameOriginForSecureContext() {
     return absl::nullopt;
   }
 
+  if (!render_frame_host->IsFeatureEnabled(
+          blink::mojom::PermissionsPolicyFeature::kAttributionReporting)) {
+    return absl::nullopt;
+  }
+
   return suitable_top_frame_origin;
 }
 
