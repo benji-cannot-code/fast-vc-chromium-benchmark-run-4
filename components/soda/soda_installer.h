95239ffbe6b33474acd13b79dcc1a4d69d5aa714
Rob Schonberger <robsc@chromium.org>
2024-01-09 00:31:47 +0000

    soda: Add InstalledLanguages to SodaInstaller, and use in CROS.
    Add a method to SodaInstaller to return the list of all installed language codes.
    
    Make use of this method to update the file paths returned in ChromeOS
    to return _all_ currently installed languages.
    
    Bug: b:257166700
    
    Change-Id: I34c0fb2954eb9b7b2cd9b210718ca9fd6112b707
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5176191
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    Commit-Queue: Rob Schonberger <robsc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244377}

--- components/soda/soda_installer.h
+++ components/soda/soda_installer.h
@@ -137,6 +137,8 @@ class COMPONENT_EXPORT(SODA_INSTALLER) SodaInstaller {
       LanguageCode language_code = LanguageCode::kNone);
   bool IsAnyLanguagePackInstalledForTesting() const;
 
+  const std::set<LanguageCode> InstalledLanguages() const;
+
  protected:
   // Registers the preference tracking the installed SODA language packs.
   static void RegisterRegisteredLanguagePackPref(PrefRegistrySimple* registry);
