08846f287f17271055a29df532d01f00a0dbf94c
Ahmed Fakhry <afakhry@chromium.org>
2024-01-10 04:06:10 +0000

    capture_mode: Set `camera_preview_view_` before its initialization
    If initialization of `CameraPreviewView` is done its ctor before
    the instance is set to `camera_preview_view_`, it may lead to an
    event that triggers an operation requiring `camera_preview_view_`
    causing a crash.
    
    This CL splits construction from initialization.
    
    Fixed: b/319148690
    Change-Id: Icbcbb43c4f90d6f9dc834d7e8381cdcc48b52669
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5183396
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245084}

--- ash/capture_mode/capture_mode_camera_preview_view.cc
+++ ash/capture_mode/capture_mode_camera_preview_view.cc
@@ -131,8 +131,6 @@ CameraPreviewView::CameraPreviewView(
   accessibility_observation_.Observe(Shell::Get()->accessibility_controller());
   RefreshResizeButtonVisibility();
   UpdateResizeButtonTooltip();
-  capture_mode_util::MaybeUpdateCaptureModePrivacyIndicators();
-  CaptureModeController::Get()->MaybeUpdateVcPanel();
 }
 
 CameraPreviewView::~CameraPreviewView() {
@@ -141,6 +139,15 @@ CameraPreviewView::~CameraPreviewView() {
     controller->capture_mode_session()->OnCameraPreviewDestroyed();
   capture_mode_util::MaybeUpdateCaptureModePrivacyIndicators();
   controller->MaybeUpdateVcPanel();
+}
+
+void CameraPreviewView::Initialize() {
+  CHECK(GetWidget()) << "The view should have been added to a widget first.";
+
+  camera_video_renderer_.Initialize();
+
+  capture_mode_util::MaybeUpdateCaptureModePrivacyIndicators();
+  CaptureModeController::Get()->MaybeUpdateVcPanel();
 }
 
 void CameraPreviewView::SetIsCollapsible(bool value) {
@@ -252,8 +259,6 @@ void CameraPreviewView::AddedToWidget() {
   // `Initialize()` will create a layer tree frame sink for the `host_window()`
   // and we're not allowed to change the event targeting policy after that.
   DisableEventHandlingInCameraVideoHostHierarchy();
-
-  camera_video_renderer_.Initialize();
 }
 
 bool CameraPreviewView::OnMousePressed(const ui::MouseEvent& event) {
