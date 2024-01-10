0752b55aed9ccedcf624bba489cc61abfdaa5c86
David Bertoni <dbertoni@chromium.org>
2024-01-10 20:39:27 +0000

    [Code Health] Remove a use of base::SupportsWeakPtr. (prefs)
    Bug: 647430
    Change-Id: I91c6661c171de0880559833e4c6638d2372d38fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5172923
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245487}

--- components/prefs/json_pref_store.cc
+++ components/prefs/json_pref_store.cc
@@ -288,7 +288,8 @@ void JsonPrefStore::ReadPrefsAsync(ReadErrorDelegate* error_delegate) {
   // Weakly binds the read task so that it doesn't kick in during shutdown.
   file_task_runner_->PostTaskAndReplyWithResult(
       FROM_HERE, base::BindOnce(&ReadPrefsFromDisk, path_),
-      base::BindOnce(&JsonPrefStore::OnFileRead, AsWeakPtr()));
+      base::BindOnce(&JsonPrefStore::OnFileRead,
+                     weak_ptr_factory_.GetWeakPtr()));
 }
 
 void JsonPrefStore::CommitPendingWrite(
@@ -395,7 +396,7 @@ void JsonPrefStore::RegisterOnNextSuccessfulWriteReply(
             &PostWriteCallback, base::OnceCallback<void(bool success)>(),
             base::BindOnce(
                 &JsonPrefStore::RunOrScheduleNextSuccessfulWriteCallback,
-                AsWeakPtr()),
+                weak_ptr_factory_.GetWeakPtr()),
             base::SequencedTaskRunner::GetCurrentDefault()));
   }
 }
@@ -412,7 +413,7 @@ void JsonPrefStore::RegisterOnNextWriteSynchronousCallbacks(
           &PostWriteCallback, std::move(callbacks.second),
           base::BindOnce(
               &JsonPrefStore::RunOrScheduleNextSuccessfulWriteCallback,
-              AsWeakPtr()),
+              weak_ptr_factory_.GetWeakPtr()),
           base::SequencedTaskRunner::GetCurrentDefault()));
 }
 
@@ -466,7 +467,8 @@ void JsonPrefStore::OnFileRead(std::unique_ptr<ReadResult> read_result) {
   if (pref_filter_) {
     filtering_in_progress_ = true;
     PrefFilter::PostFilterOnLoadCallback post_filter_on_load_callback(
-        base::BindOnce(&JsonPrefStore::FinalizeFileRead, AsWeakPtr(),
+        base::BindOnce(&JsonPrefStore::FinalizeFileRead,
+                       weak_ptr_factory_.GetWeakPtr(),
                        initialization_successful));
     pref_filter_->FilterOnLoad(std::move(post_filter_on_load_callback),
                                std::move(unfiltered_prefs));
--- components/prefs/json_pref_store.h
+++ components/prefs/json_pref_store.h
@@ -39,11 +39,10 @@ class WriteCallbacksObserver;
 }  // namespace base
 
 // A writable PrefStore implementation that is used for user preferences.
-class COMPONENTS_PREFS_EXPORT JsonPrefStore
+class COMPONENTS_PREFS_EXPORT JsonPrefStore final
     : public PersistentPrefStore,
       public base::ImportantFileWriter::DataSerializer,
-      public base::ImportantFileWriter::BackgroundDataSerializer,
-      public base::SupportsWeakPtr<JsonPrefStore> {
+      public base::ImportantFileWriter::BackgroundDataSerializer {
  public:
   struct ReadResult;
 
@@ -124,6 +123,10 @@ class COMPONENTS_PREFS_EXPORT JsonPrefStore
       base::OnceClosure on_next_successful_write_reply);
 
   void OnStoreDeletionFromDisk() override;
+
+  base::WeakPtr<JsonPrefStore> AsWeakPtr() {
+    return weak_ptr_factory_.GetWeakPtr();
+  }
 
 #if defined(UNIT_TEST)
   base::ImportantFileWriter& get_writer() { return writer_; }
@@ -213,6 +216,8 @@ class COMPONENTS_PREFS_EXPORT JsonPrefStore
   base::OnceClosure on_next_successful_write_reply_;
 
   SEQUENCE_CHECKER(sequence_checker_);
+
+  base::WeakPtrFactory<JsonPrefStore> weak_ptr_factory_{this};
 };
 
 #endif  // COMPONENTS_PREFS_JSON_PREF_STORE_H_
