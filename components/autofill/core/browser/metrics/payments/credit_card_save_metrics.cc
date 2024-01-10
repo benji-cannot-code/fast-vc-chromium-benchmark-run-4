11488e2fd39a4cb31bf4225b67c675196ebae443
Yishui Liu <yishuil@google.com>
2024-01-10 21:28:33 +0000

    [CVC storage] Add metrics cases for card save with CVC
    Add sub-category metrics for credit card with CVC save events under existing card save events.
    
    Bug: 1492775
    Change-Id: Ife869d6baa8474b5d6c8eac668c234e5adf0aa31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5168846
    Commit-Queue: Yishui Liu <yishuil@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1245514}

--- components/autofill/core/browser/metrics/payments/credit_card_save_metrics.cc
+++ components/autofill/core/browser/metrics/payments/credit_card_save_metrics.cc
@@ -105,6 +105,11 @@ void LogSaveCardPromptOfferMetric(
                                       ".WithSameLastFourButDifferentExpiration",
                                   metric);
   }
+  if (options.card_save_type ==
+      AutofillClient::CardSaveType::kCardSaveWithCvc) {
+    base::UmaHistogramEnumeration(
+        metric_with_destination_and_show + ".SavingWithCvc", metric);
+  }
 
   if (security_level != security_state::SecurityLevel::SECURITY_LEVEL_COUNT) {
     base::UmaHistogramEnumeration(
@@ -151,6 +156,11 @@ void LogSaveCardPromptResultMetric(
     base::UmaHistogramEnumeration(metric_with_destination_and_show +
                                       ".WithSameLastFourButDifferentExpiration",
                                   metric);
+  }
+  if (options.card_save_type ==
+      AutofillClient::CardSaveType::kCardSaveWithCvc) {
+    base::UmaHistogramEnumeration(
+        metric_with_destination_and_show + ".SavingWithCvc", metric);
   }
 
   if (security_level != security_state::SecurityLevel::SECURITY_LEVEL_COUNT) {
