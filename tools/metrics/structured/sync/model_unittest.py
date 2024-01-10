fde628e0e8cc802198f102b0593d62ca12bbfe2c
Eugene Xie <eugenex@google.com>
2024-01-10 20:35:16 +0000

    Fix imports for model_unittest.py
    Fix the import statements for model_unittest.py.  Imports the dependency
    on model as sync.model to be consistent with how the model and
    model_util modules are resolved by gen_events.py and gen_validator.py.
    However, in order for model_unittest.py to be run correctly, a wrapper
    Python file was created in the parent directory (same directory as
    gen_events.py and gen_validator.py).  This way, the model_unittest.py
    can still be run from the Chromium repository, albeit indirectly.
    
    NO_BUG
    
    Change-Id: I8e7843071d04bf9dd44dd5d259d16df5aee852be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5179252
    Reviewed-by: Andrew Bregger <andrewbregger@google.com>
    Reviewed-by: Jong Ahn <jongahn@chromium.org>
    Commit-Queue: Eugene Xie <eugenex@google.com>
    Cr-Commit-Position: refs/heads/main@{#1245482}

--- tools/metrics/structured/sync/model_unittest.py
+++ tools/metrics/structured/sync/model_unittest.py
@@ -8,11 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 # TODO(crbug.com/1148168): Set up these tests to run on the tryjobs.
 
-import sync.model
+import sync.model as model
 import unittest
-
-from model import Model
-
 
 class ModelTest(unittest.TestCase):
   """Tests for model.py"""
@@ -39,7 +36,7 @@ class ModelTest(unittest.TestCase):
   def assert_model_raises(self, xml):
     raised = False
     try:
-      Model(xml)
+      model.Model(xml)
     except ValueError:
       raised = True
     self.assertTrue(raised)
@@ -88,7 +85,7 @@ class ModelTest(unittest.TestCase):
           </project>
           </structured-metrics>"""
 
-    data = Model(xml)
+    data = model.Model(xml)
 
     self.assertEqual(len(data.projects), 2)
     project_one, project_two = data.projects
