a1c4f2a8068d93309ecce0540e9790dfa9291123
Ernesto Izquierdo Clua <eic@google.com>
2024-01-10 16:41:35 +0000

    Resign first responder before requesting auth.
    Bug: 1509416
    Change-Id: Ie8f91d37a8bfb83e770f45bbaaf46699b4cd02a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5171685
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245316}

--- ios/chrome/browser/ui/settings/password/reauthentication/reauthentication_view_controller.mm
+++ ios/chrome/browser/ui/settings/password/reauthentication/reauthentication_view_controller.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/check.h"
 #import "base/metrics/histogram_macros.h"
+#import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/ui/settings/password/branded_navigation_item_title_view.h"
 #import "ios/chrome/browser/ui/settings/password/create_password_manager_title_view.h"
 #import "ios/chrome/browser/ui/settings/password/reauthentication/reauthentication_constants.h"
@@ -135,6 +136,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (![_reauthModule canAttemptReauth]) {
     return;
   }
+
+  // Hide keyboard otherwise the first responder can get focused after getting
+  // the authentication result.
+  [GetFirstResponder() resignFirstResponder];
 
   __weak __typeof(self) weakSelf = self;
   [_reauthModule
