274735fe879307a7719c3ac06d5d49fbd56fe9a8
Quentin Pubert <qpubert@google.com>
2024-01-10 11:51:50 +0000

    [iOS] Remove DownloadTaskSaveToDriveData from Download manager code
    This CL removes dependencies on `DownloadTaskSaveToDriveData` from
    Download manager code. Instead, the `DriveTabHelper` can directly
    provide an upload task object associated with a given download task.
    
    Bug: 1495354
    Change-Id: I70d225a2607baecbb05216857e507a1f68cc767e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5158287
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1245213}

--- ios/chrome/browser/download/model/download_manager_tab_helper.mm
+++ ios/chrome/browser/download/model/download_manager_tab_helper.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/notreached.h"
 #import "ios/chrome/browser/download/model/download_manager_tab_helper_delegate.h"
 #import "ios/chrome/browser/drive/model/drive_tab_helper.h"
+#import "ios/chrome/browser/drive/model/upload_task.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/web/public/download/download_task.h"
 
@@ -143,9 +144,9 @@ bool DownloadManagerTabHelper::WillDownloadTaskBeSavedToDrive() const {
   }
   DriveTabHelper* drive_tab_helper =
       DriveTabHelper::FromWebState(task_->GetWebState());
-  std::optional<DownloadTaskSaveToDriveData> save_to_drive_data =
-      drive_tab_helper->GetDownloadTaskSaveToDriveData();
-  return save_to_drive_data && save_to_drive_data->task == task_.get();
+  UploadTask* upload_task =
+      drive_tab_helper->GetUploadTaskForDownload(task_.get());
+  return upload_task && !upload_task->IsDone();
 }
 
 WEB_STATE_USER_DATA_KEY_IMPL(DownloadManagerTabHelper)
