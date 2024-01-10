2b0519fb214abf32b4552796e013fd3c4bcaec58
Federica Germinario <fedegermi@google.com>
2024-01-10 13:04:03 +0000

    [ios] Remove local state prefs deprecated in 2021
    This CL removes some legacy prefs.
    
    Bug: none
    Change-Id: If05b54296a8f79dc429796f0a205dfc228b59249
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5176939
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245234}

--- ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -109,12 +109,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ui/base/l10n/l10n_util.h"
 
 namespace {
-// Deprecated 09/2021
-const char kTrialGroupPrefName[] = "location_permissions.trial_group";
-
-// Deprecated 10/2021
-const char kSigninBottomSheetShownCount[] =
-    "ios.signin.bottom_sheet_shown_count";
 
 // Deprecated 03/2022
 const char kShowReadingListInBookmarkBar[] = "bookmark_bar.show_reading_list";
@@ -372,10 +366,6 @@ void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
 
   registry->RegisterBooleanPref(prefs::kLensCameraAssistedSearchPolicyAllowed,
                                 true);
-
-  registry->RegisterIntegerPref(kTrialGroupPrefName, 0);
-
-  registry->RegisterIntegerPref(kSigninBottomSheetShownCount, 0);
 
   registry->RegisterIntegerPref(kFRETrialGroupPrefName, 0);
 
@@ -725,12 +715,6 @@ void RegisterBrowserStatePrefs(user_prefs::PrefRegistrySyncable* registry) {
 
 // This method should be periodically pruned of year+ old migrations.
 void MigrateObsoleteLocalStatePrefs(PrefService* prefs) {
-  // Added 09/2021
-  prefs->ClearPref(kTrialGroupPrefName);
-
-  // Added 10/2021
-  prefs->ClearPref(kSigninBottomSheetShownCount);
-
   // Added 04/2022
   prefs->ClearPref(kFRETrialGroupPrefName);
 
