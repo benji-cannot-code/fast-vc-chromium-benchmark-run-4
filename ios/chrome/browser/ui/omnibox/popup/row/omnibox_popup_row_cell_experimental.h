3a4215ca211e25e8de94a84ea736f02b34c11cf6
Christian Xu <christianxu@google.com>
2024-01-10 19:08:21 +0000

    [iOS][Omnibox][Popup] Move row files to a folder
    Move popup row files to a different folder to declutter the popup folder. The future content configuration will also be added to this folder.
    
    Bug: None
    Change-Id: Ib4599e4df6e6fa3b98d97b9a20d5006abfd52a81
    Low-Coverage-Reason: TRIVIAL_CHANGE Moving files.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5140431
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245429}

--- ios/chrome/browser/ui/omnibox/popup/row/omnibox_popup_row_cell_experimental.h
+++ ios/chrome/browser/ui/omnibox/popup/row/omnibox_popup_row_cell_experimental.h
@@ -3,11 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_UI_OMNIBOX_POPUP_OMNIBOX_POPUP_ROW_CELL_EXPERIMENTAL_H_
-#define IOS_CHROME_BROWSER_UI_OMNIBOX_POPUP_OMNIBOX_POPUP_ROW_CELL_EXPERIMENTAL_H_
+#ifndef IOS_CHROME_BROWSER_UI_OMNIBOX_POPUP_ROW_OMNIBOX_POPUP_ROW_CELL_EXPERIMENTAL_H_
+#define IOS_CHROME_BROWSER_UI_OMNIBOX_POPUP_ROW_OMNIBOX_POPUP_ROW_CELL_EXPERIMENTAL_H_
 
 #import <UIKit/UIKit.h>
-#import "ios/chrome/browser/ui/omnibox/popup/omnibox_popup_row_cell.h"
+#import "ios/chrome/browser/ui/omnibox/popup/row/omnibox_popup_row_cell.h"
 
 @protocol AutocompleteSuggestion;
 @protocol FaviconRetriever;
@@ -49,4 +49,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @end
 
-#endif  // IOS_CHROME_BROWSER_UI_OMNIBOX_POPUP_OMNIBOX_POPUP_ROW_CELL_EXPERIMENTAL_H_
+#endif  // IOS_CHROME_BROWSER_UI_OMNIBOX_POPUP_ROW_OMNIBOX_POPUP_ROW_CELL_EXPERIMENTAL_H_
