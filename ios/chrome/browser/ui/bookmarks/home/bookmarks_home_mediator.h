161db84e4762c3cb3135afe250ad6855b7f1d698
Jood Hajeer <jood@google.com>
2024-01-10 13:37:53 +0000

    Reland "[iOS] Promo to upload bookmarks should be limited to left-behind ones"
    This is a reland of commit c8123bec897b539a37b49f8fe29376594c8b089d
    Rebased on top of the fix in crrev.com/c/5176917.
    No changes were made to this CL.
    
    Original change's description:
    > [iOS] Promo to upload bookmarks should be limited to left-behind ones
    >
    > The batch upload promo from bookmarks manager should not be shown if the
    > user has already uploaded the sync-left behind bookmarks.
    > This should be the solution to avoid the confusion that happens because
    > both local and sync left-behind bookmarks are in the local store.
    >
    > To guarantee that the promo should never be shown after the user
    > chooses to upload these bookmarks, this CL introduces a pref for that,
    > and writes/reads to it.
    >
    > Note: the left-behind bookmarks in this usage are: all the bookmarks
    > that exist in the local store, where the current account is the last syncing account, till the user uploads them using this promo.
    >
    > Fixed: 1513110
    > Change-Id: Id5fef0ed0d0a88f42401e3c7ad7a3699dcff63c7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5164234
    > Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    > Reviewed-by: David Roger <droger@chromium.org>
    > Commit-Queue: Jood Hajeer <jood@google.com>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1244122}
    
    Change-Id: If927da4ae3f6a57f2f72ff860f3bfc7e653dcc4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5176916
    Commit-Queue: Jood Hajeer <jood@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245248}

--- ios/chrome/browser/ui/bookmarks/home/bookmarks_home_mediator.h
+++ ios/chrome/browser/ui/bookmarks/home/bookmarks_home_mediator.h
@@ -22,6 +22,10 @@ class BookmarkModel;
 class BookmarkNode;
 }  // namespace bookmarks
 
+namespace user_prefs {
+class PrefRegistrySyncable;
+}  // namespace user_prefs
+
 typedef NS_ENUM(NSInteger, BookmarksHomeSectionIdentifier) {
   // Section to invite the user to sign in and sync.
   BookmarksHomeSectionIdentifierPromo = kSectionIdentifierEnumZero,
@@ -91,6 +95,9 @@ class BookmarkModel;
 // the root level, `displayedBookmarkModel` returns the localOrSyncable storage.
 @property(nonatomic, assign, readonly)
     bookmarks::BookmarkModel* displayedBookmarkModel;
+
+// Registers the feature preferences.
++ (void)registerBrowserStatePrefs:(user_prefs::PrefRegistrySyncable*)registry;
 
 // Designated initializer.
 // `localOrSyncableBookmarkModel` must not be `nullptr`. It should also be
