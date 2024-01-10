f61d740d4b7233fad04d23654ceda233c6ddb912
Sylvain Defresne <sdefresne@chromium.org>
2024-01-10 10:20:36 +0000

    [ios] Use constants for histogram recording session load/save timing
    Instead of duplicating the strings, use named constants to store the
    name of the histogram used to record the session load/save timing.
    This is the recommendation when using histograms.
    
    Bug: 1383087
    Change-Id: I68171050ba113606f55b87257e7fc95cbd7319c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5178002
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245186}

--- ios/chrome/browser/sessions/session_restoration_browser_agent.mm
+++ ios/chrome/browser/sessions/session_restoration_browser_agent.mm
@@ -313,7 +313,7 @@ void SessionRestorationBrowserAgent::RestoreSession() {
                      directory:browser_->GetBrowserState()->GetStatePath()];
 
   RestoreSessionWindow(session_window);
-  base::UmaHistogramTimes("Session.WebStates.LoadingTimeOnMainThread",
+  base::UmaHistogramTimes(kSessionHistogramLoadingTime,
                           base::TimeTicks::Now() - start_time);
 }
 
