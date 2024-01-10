2e677f58411804ec5d1fce72f82697fd85a1f227
Ewann Pelle <ewannpv@chromium.org>
2024-01-10 12:36:09 +0000

    [iOS][TS][1/2] Add drag & drop tests
    Tests that moving tabs using drag and drop interactions works as intended.
    
    Bug: 1515507, 1493679
    Change-Id: I7e87221c7f08f562aa86a41d596996f7ccc2034d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5172675
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245227}

--- ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_constants.swift
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_constants.swift
@@ -12,6 +12,7 @@ struct TabStripConstants {
   struct CollectionView {
     static let inset: CGFloat = 4
     static let tabStripCellReuseIdentifier = "tabStripCellReuseIdentifier"
+    static let tabStripCellPrefixIdentifier = "tabStripCellPrefixIdentifier"
   }
 
   /// Tab item constants.
--- ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_view_controller.swift
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_view_controller.swift
@@ -181,6 +181,8 @@ class TabStripViewController: UIViewController, TabStripCellDelegate,
     layout.needsUpdate = true
     diffableDataSource?.apply(snapshot, animatingDifferences: animatingDifferences)
     layout.needsUpdate = false
+
+    updateVisibleCellIdentifiers()
   }
 
   /// Creates the registrations of the different cells used in the collection view.
@@ -190,6 +192,7 @@ class TabStripViewController: UIViewController, TabStripCellDelegate,
       cell.setTitle(item.title)
       cell.loading = item.showsActivity
       cell.delegate = self
+      cell.accessibilityIdentifier = self.tabTripCellAccessibilityIdentifier(index: indexPath.item)
 
       weak var weakSelf = self
       item.fetchFavicon { (item: TabSwitcherItem?, image: UIImage?) -> Void in
@@ -257,6 +260,22 @@ class TabStripViewController: UIViewController, TabStripCellDelegate,
     let closeActions = UIMenu(options: .displayInline, children: [close, closeOthers])
 
     return UIMenu(children: [share, closeActions])
+  }
+
+  // Update visible cells identifier, following a reorg of cells.
+  func updateVisibleCellIdentifiers() {
+    for indexPath in collectionView.indexPathsForVisibleItems {
+      if let cell = collectionView.cellForItem(at: indexPath) {
+        cell.accessibilityIdentifier = tabTripCellAccessibilityIdentifier(index: indexPath.item)
+      }
+    }
+  }
+
+  // Returns the accessibility identifier to set on a TabStripCell when
+  // positioned at the given index.
+  func tabTripCellAccessibilityIdentifier(index: Int) -> String {
+    return String(
+      format: "%@%ld", TabStripConstants.CollectionView.tabStripCellPrefixIdentifier, index)
   }
 
   // MARK: - TabStripNewTabButtonDelegate
