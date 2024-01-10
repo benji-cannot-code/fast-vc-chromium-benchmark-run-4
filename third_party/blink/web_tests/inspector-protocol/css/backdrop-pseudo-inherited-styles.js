ddc1314c37aa8bc14702787e6270578ef1e149c1
Rune Lillesveen <futhark@chromium.org>
2024-01-10 20:17:45 +0000

    [devtools] Correct ::backdrop inheritance
    ::backdrop inherits from originating element if
    BackdropInheritOriginating runtime feature is enabled. Make sure
    devtools take that into account.
    
    Bug: 827397
    Change-Id: I29969badbd1d64e6146f6e09faeebec32544f0eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5177042
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245471}

---
+++ third_party/blink/web_tests/inspector-protocol/css/backdrop-pseudo-inherited-styles.js
@@ -0,0 +1,33 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  var {page, session, dp} = await testRunner.startHTML(`
+<style>
+  /* This style should be inherited by the ::backdrop pseudo */
+  :root {
+    --foo: bar;
+  }
+  dialog {
+    color: green;
+  }
+  dialog::backdrop {
+    opacity: 0.5;
+  }
+</style>
+<dialog id="inspected"></dialog>
+`, 'Tests that ::backdrop inherits from its originating element');
+
+  let CSSHelper = await testRunner.loadScript('../resources/css-helper.js');
+  let cssHelper = new CSSHelper(testRunner, dp);
+
+  await dp.DOM.enable();
+  await dp.CSS.enable();
+
+  await session.evaluate(() => document.getElementById("inspected").showModal());
+
+  let documentNodeId = await cssHelper.requestDocumentNodeId();
+  let dialogNodeId = (await dp.DOM.querySelector({nodeId: documentNodeId, selector: "#inspected"})).result.nodeId;
+  let backdropNodeId = (await dp.DOM.describeNode({nodeId: dialogNodeId})).result.node.pseudoElements[0].nodeId;
+
+  await cssHelper.loadAndDumpMatchingRulesForNode(backdropNodeId);
+  testRunner.completeTest();
+});
