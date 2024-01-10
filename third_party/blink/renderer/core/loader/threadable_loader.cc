b4ba088e9f39ee0d3644d83de577580ef33cd2d2
Ming-Ying Chung <mych@chromium.org>
2024-01-10 09:04:55 +0000

    Move duration UMA logging for fetch keepalive requests to proper place.
    The current implementation logs nothing after loader is detached.
    It should happen in the detached ThreadableLoader instead of
    FetchManager Loader.
    
    Bug: 1356128
    Change-Id: Ib618b3ff93f5bbe10ad6d1fe07170e5c566a4f13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5137541
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Auto-Submit: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245158}

--- third_party/blink/renderer/core/loader/threadable_loader.cc
+++ third_party/blink/renderer/core/loader/threadable_loader.cc
@@ -34,8 +34,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/metrics/histogram_functions.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/time/time.h"
 #include "services/network/public/cpp/cors/cors_error_status.h"
 #include "services/network/public/mojom/cors.mojom-blink.h"
 #include "services/network/public/mojom/fetch_api.mojom-blink.h"
@@ -70,16 +72,20 @@ namespace {
 class DetachedClient final : public GarbageCollected<DetachedClient>,
                              public ThreadableLoaderClient {
  public:
-  explicit DetachedClient(ThreadableLoader* loader) : loader_(loader) {}
+  explicit DetachedClient(ThreadableLoader* loader)
+      : loader_(loader), detached_time_(base::TimeTicks::Now()) {}
   ~DetachedClient() override = default;
 
   void DidFinishLoading(uint64_t identifier) override {
+    LogKeepAliveDuration("Succeeded");
     self_keep_alive_.Clear();
   }
   void DidFail(uint64_t identifier, const ResourceError&) override {
+    LogKeepAliveDuration("Failed");
     self_keep_alive_.Clear();
   }
   void DidFailRedirectCheck(uint64_t identifier) override {
+    LogKeepAliveDuration("Failed");
     self_keep_alive_.Clear();
   }
   void Trace(Visitor* visitor) const override {
@@ -88,9 +94,22 @@ class DetachedClient final : public GarbageCollected<DetachedClient>,
   }
 
  private:
+  void LogKeepAliveDuration(const std::string& name) {
+    CHECK(name == "Succeeded" || name == "Failed");
+    base::TimeDelta duration_after_detached =
+        base::TimeTicks::Now() - detached_time_;
+    base::UmaHistogramMediumTimes(
+        "FetchKeepAlive.Renderer.DurationAfterDetached",
+        duration_after_detached);
+    base::UmaHistogramMediumTimes(
+        "FetchKeepAlive.Renderer.DurationAfterDetached." + name,
+        duration_after_detached);
+  }
+
   SelfKeepAlive<DetachedClient> self_keep_alive_{this};
   // Keep it alive.
   const Member<ThreadableLoader> loader_;
+  base::TimeTicks detached_time_;
 };
 
 }  // namespace
