38b2b9309be57f53fa9e1bd0bba1bd67afd865f4
Bo Liu <boliu@chromium.org>
2024-01-10 23:34:15 +0000

    Check for active file chooser in ShowCreatedWindow
    Bug: 1504324
    Change-Id: I84afa271b8b33b5019067536e75640b80b8c7515
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5185860
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245596}

--- content/common/features.h
+++ content/common/features.h
@@ -124,6 +124,7 @@ CONTENT_EXPORT BASE_DECLARE_FEATURE(kWarmUpNetworkProcess);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kWebAssemblyDynamicTiering);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kWebOTPAssertionFeaturePolicy);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kWebRtcUseGpuMemoryBufferVideoFrames);
+CONTENT_EXPORT BASE_DECLARE_FEATURE(kWindowOpenFileSelectFix);
 
 // Please keep features in alphabetical order.
 
