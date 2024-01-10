26f61b995989227e50c374bc7e033b3ef9f5221f
Artem Sumaneev <asumaneev@google.com>
2024-01-10 14:42:50 +0000

    [enrollment] Add state retrieval error.
    Add error for state retrieval request:
      * Use network and invalid request error same as state availability
        request.
      * Add new response error.
    
    Bug: b:309921228
    Test: none
    Change-Id: I245cb947eba74187a89f93ca56360f49b29685ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5054139
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Roland Bock <rbock@google.com>
    Commit-Queue: Artem Sumaneev <asumaneev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1245265}

--- chrome/browser/ash/policy/enrollment/auto_enrollment_client_impl_unittest.cc
+++ chrome/browser/ash/policy/enrollment/auto_enrollment_client_impl_unittest.cc
@@ -912,7 +912,7 @@ TEST_F(AutoEnrollmentClientImplTest, ForcedReEnrollmentStateRetrivalfailure) {
   ServerRepliesEmptyResponseForAsyncJob(&device_state_job);
 
   // Verify that no enrollment has been done, and no state has been retrieved.
-  EXPECT_EQ(state_, kAutoEnrollmentLegacyServerError);
+  EXPECT_EQ(state_, ToState(AutoEnrollmentStateRetrievalResponseError{}));
   EXPECT_FALSE(HasServerBackedState());
 
   // Verify all jobs have finished.
@@ -1269,7 +1269,9 @@ TEST_F(AutoEnrollmentClientImplTest,
 
   ExpectHashDanceNetworkErrorHistogram(-net::ERR_FAILED);
   EXPECT_EQ(failed_job_type_, GetExpectedStateRetrievalJobType());
-  EXPECT_EQ(state_, kAutoEnrollmentLegacyConnectionError);
+  EXPECT_EQ(state_, ToState(AutoEnrollmentDMServerError{
+                        .dm_error = DM_STATUS_REQUEST_FAILED,
+                        .network_error = net::ERR_FAILED}));
   EXPECT_FALSE(HasServerBackedState());
 }
 
@@ -1387,7 +1389,9 @@ TEST_F(AutoEnrollmentClientImplTest,
 
   ExpectHashDanceNetworkErrorHistogram(-net::ERR_FAILED);
   EXPECT_EQ(failed_job_type_, GetExpectedStateRetrievalJobType());
-  EXPECT_EQ(state_, kAutoEnrollmentLegacyConnectionError);
+  EXPECT_EQ(state_, ToState(AutoEnrollmentDMServerError{
+                        .dm_error = DM_STATUS_REQUEST_FAILED,
+                        .network_error = net::ERR_FAILED}));
   EXPECT_FALSE(HasServerBackedState());
 
   // Second, the server fails with an internal error.
@@ -1399,7 +1403,8 @@ TEST_F(AutoEnrollmentClientImplTest,
   ExpectHashDanceRequestStatusHistogram(DM_STATUS_TEMPORARY_UNAVAILABLE,
                                         /*dm_status_count=*/1);
   EXPECT_EQ(failed_job_type_, GetExpectedStateRetrievalJobType());
-  EXPECT_EQ(state_, kAutoEnrollmentLegacyServerError);
+  EXPECT_EQ(state_, ToState(AutoEnrollmentDMServerError{
+                        .dm_error = DM_STATUS_TEMPORARY_UNAVAILABLE}));
   EXPECT_FALSE(HasServerBackedState());
 
   // Third, the server responds with a correct server state.
@@ -1736,7 +1741,9 @@ TEST_F(PsmHelperInitialEnrollmentTest,
   // Verify initial enrollment state retrieval.
   if (kExpectedMembershipResult) {
     EXPECT_EQ(failed_job_type_, GetExpectedStateRetrievalJobType());
-    EXPECT_EQ(state_, kAutoEnrollmentLegacyConnectionError);
+    EXPECT_EQ(state_, ToState(AutoEnrollmentDMServerError{
+                          .dm_error = DM_STATUS_REQUEST_FAILED,
+                          .network_error = net::ERR_FAILED}));
   } else {
     EXPECT_EQ(state_, AutoEnrollmentResult::kNoEnrollment);
   }
@@ -1825,7 +1832,8 @@ TEST_F(PsmHelperInitialEnrollmentTest, PsmSucceedAndStateRetrievalFailed) {
   // Verify initial enrollment state retrieval.
   if (kExpectedMembershipResult) {
     EXPECT_EQ(failed_job_type_, GetExpectedStateRetrievalJobType());
-    EXPECT_EQ(state_, kAutoEnrollmentLegacyServerError);
+    EXPECT_EQ(state_, ToState(AutoEnrollmentDMServerError{
+                          .dm_error = DM_STATUS_TEMPORARY_UNAVAILABLE}));
   } else {
     EXPECT_EQ(state_, AutoEnrollmentResult::kNoEnrollment);
   }
@@ -1855,7 +1863,7 @@ TEST_F(PsmHelperInitialEnrollmentTest, PsmSucceedAndStateRetrievalIsEmpty) {
   EXPECT_EQ(kExpectedPsmDeterminationTimestamp, GetPsmDeterminationTimestamp());
 
   EXPECT_EQ(state_retrieval_job_type_, GetExpectedStateRetrievalJobType());
-  EXPECT_EQ(state_, kAutoEnrollmentLegacyServerError);
+  EXPECT_EQ(state_, ToState(AutoEnrollmentStateRetrievalResponseError{}));
   EXPECT_FALSE(HasServerBackedState());
 }
 
