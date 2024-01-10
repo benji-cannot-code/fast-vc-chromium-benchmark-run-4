f60310d74a81949f93f93f39da95def6f6b18392
Austin Sullivan <asully@chromium.org>
2024-01-10 22:18:29 +0000

    Code Health: Add span overloads of Pickle::WriteData and ::WriteBytes
    Migrates all callers in //base. Other callers will later be migrated
    in follow-up CLs
    
    Bug: 1490484
    Change-Id: Iaafab315bb42fc9fdb3229648ac2eff13aaa653c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5136740
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1245542}

--- base/pickle_unittest.cc
+++ base/pickle_unittest.cc
@@ -127,7 +127,7 @@ TEST(PickleTest, EncodeDecode) {
   pickle.WriteString16(teststring16);
   pickle.WriteString(testrawstring);
   pickle.WriteString16(testrawstring16);
-  pickle.WriteData(testdata, testdatalen);
+  pickle.WriteData(std::string_view(testdata, testdatalen));
   VerifyResult(pickle);
 
   // test copy constructor
@@ -441,7 +441,8 @@ TEST(PickleTest, Resize) {
   // note that any data will have a 4-byte header indicating the size
   const size_t payload_size_after_header = unit - sizeof(uint32_t);
   Pickle pickle;
-  pickle.WriteData(data_ptr, payload_size_after_header - sizeof(uint32_t));
+  pickle.WriteData(
+      std::string_view(data_ptr, payload_size_after_header - sizeof(uint32_t)));
   size_t cur_payload = payload_size_after_header;
 
   // note: we assume 'unit' is a power of 2
@@ -449,13 +450,13 @@ TEST(PickleTest, Resize) {
   EXPECT_EQ(pickle.payload_size(), payload_size_after_header);
 
   // fill out a full page (noting data header)
-  pickle.WriteData(data_ptr, unit - sizeof(uint32_t));
+  pickle.WriteData(std::string_view(data_ptr, unit - sizeof(uint32_t)));
   cur_payload += unit;
   EXPECT_EQ(unit * 2, pickle.capacity_after_header());
   EXPECT_EQ(cur_payload, pickle.payload_size());
 
   // one more byte should double the capacity
-  pickle.WriteData(data_ptr, 1);
+  pickle.WriteData(std::string_view(data_ptr, 1u));
   cur_payload += 8;
   EXPECT_EQ(unit * 4, pickle.capacity_after_header());
   EXPECT_EQ(cur_payload, pickle.payload_size());
@@ -498,7 +499,7 @@ TEST(PickleTest, EqualsOperator) {
 TEST(PickleTest, EvilLengths) {
   Pickle source;
   std::string str(100000, 'A');
-  source.WriteData(str.c_str(), 100000);
+  source.WriteData(std::string_view(str.c_str(), 100000u));
   // ReadString16 used to have its read buffer length calculation wrong leading
   // to out-of-bounds reading.
   PickleIterator iter(source);
@@ -524,7 +525,7 @@ TEST(PickleTest, EvilLengths) {
 // Check we can write zero bytes of data and 'data' can be NULL.
 TEST(PickleTest, ZeroLength) {
   Pickle pickle;
-  pickle.WriteData(nullptr, 0);
+  pickle.WriteData(std::string_view());
 
   PickleIterator iter(pickle);
   const char* outdata;
