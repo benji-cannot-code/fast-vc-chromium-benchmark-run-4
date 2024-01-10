0ef827ba337d90945ccd54ed2c799a7edda9cfb8
Vignesh Shenvi <vshenvi@google.com>
2024-01-10 21:25:17 +0000

    Revert "Show a system nudge when website telemetry reporting is enabled"
    This reverts commit 24edf4a0298edbe9a08fc4c9e9842abdf50f31cb.
    
    Reason for revert: PMs and the privacy team realized we do not need the system nudge anymore. The management UI changes should suffice.
    
    Original change's description:
    > Show a system nudge when website telemetry reporting is enabled
    >
    > This change introduces a new nudge controller that will be used to
    > display a system nudge when website telemetry reporting is enabled for
    > the first time. The nudge will also include a "Learn more" button that
    > will route users to the help desk article for additional information.
    >
    > Bug: b:315215165
    > Change-Id: I559e073c54fc78a4c7b18a7c1e78d73a4c900575
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5116801
    > Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    > Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    > Reviewed-by: Hong Xu <xuhong@google.com>
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Reviewed-by: Andre Le <leandre@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1243640}
    
    Bug: b:315215165
    Change-Id: I6a07520a82904fd03fa5abec2be59199d6d6d400
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5186735
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1245512}

--- chrome/browser/chromeos/reporting/metric_reporting_prefs.cc
+++ chrome/browser/chromeos/reporting/metric_reporting_prefs.cc
@@ -26,8 +26,6 @@ void RegisterProfilePrefs(::user_prefs::PrefRegistrySyncable* registry) {
       ::reporting::metrics::kDefaultWebsiteTelemetryCollectionRate
           .InMilliseconds());
   registry->RegisterDictionaryPref(kWebsiteUsage);
-  registry->RegisterBooleanPref(kReportWebsiteTelemetryPreviouslyEnabled,
-                                false);
 }
 
 bool IsWebsiteUrlAllowlisted(const GURL& url,
