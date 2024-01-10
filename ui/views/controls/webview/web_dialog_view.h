6b1215e210e69fad22b060715173323f926faca9
Zoraiz Naeem <zoraiznaeem@chromium.org>
2024-01-10 01:53:19 +0000

    [Rounded Windows] Add rounding logic for system dialogs that have frames in ash
    Dialogs are classified by whether they have non-client views (frames)
    or not. In ash, the dialogs with frames share the same non-client frames
    as other top-level windows in ash. Since the top-level windows are now
    rounded so should the window look alike dialogs.
    
    Bug: b/316776867, b/305247402, b/319328649
    Change-Id: I4c1b79be5807a90a042746ad355609ef9091ca93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5127124
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245040}

--- ui/views/controls/webview/web_dialog_view.h
+++ ui/views/controls/webview/web_dialog_view.h
@@ -29,6 +29,10 @@ class BrowserContext;
 class RenderFrameHost;
 }  // namespace content
 
+namespace gfx {
+class RoundedCornersF;
+}  // namespace gfx
+
 namespace views {
 
 // A kind of webview that can notify its delegate when its content is ready.
@@ -132,6 +136,7 @@ class WEBVIEW_EXPORT WebDialogView : public ClientView,
   bool ShouldCenterDialogTitleText() const override;
   bool HandleContextMenu(content::RenderFrameHost& render_frame_host,
                          const content::ContextMenuParams& params) override;
+  FrameKind GetWebDialogFrameKind() const override;
 
   // content::WebContentsDelegate:
   void SetContentsBounds(content::WebContents* source,
@@ -168,6 +173,8 @@ class WEBVIEW_EXPORT WebDialogView : public ClientView,
   bool CheckMediaAccessPermission(content::RenderFrameHost* render_frame_host,
                                   const url::Origin& security_origin,
                                   blink::mojom::MediaStreamType type) override;
+
+  void SetWebViewCornersRadii(const gfx::RoundedCornersF& radii);
 
  private:
   friend class WebDialogViewUnitTest;
