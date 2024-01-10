400279bfbc3577d49bb09bfc4857cc2780a1164b
Koji Ishii <kojii@chromium.org>
2024-01-10 22:02:22 +0000

    [text-spacing-trim] Re-implement line-start logic
    This patch re-implements line-start logic as:
    * `InlineNode::ShapeText` to handle "start of paragraph"; i.e.,
      start of blocks and start of lines after forced breaks.
    * Change `ShapingLineBreaker` not to handle lines after forced
      breaks.
    * Support `trim-start`.
    
    Also updates tests:
    * Merge the `normal` and `space-first` tests by using variants.
    * Merge wrap and `<br>` tests.
    * Add `trim-start` variant.
    
    Bug: 1463891
    Change-Id: I39847c1b8902de1ffaf4b27d481d4dc7a6b0d5b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5182841
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245529}

--- third_party/blink/renderer/core/layout/inline/inline_node.cc
+++ third_party/blink/renderer/core/layout/inline/inline_node.cc
@@ -112,6 +112,8 @@ class ReusingTextShaper final {
         shaper_(data->text_content),
         allow_shape_cache_(allow_shape_cache) {}
 
+  void SetOptions(ShapeOptions options) { options_ = options; }
+
   scoped_refptr<const ShapeResult> Shape(const InlineItem& start_item,
                                          const Font& font,
                                          unsigned end_offset) {
@@ -133,6 +135,7 @@ class ReusingTextShaper final {
     return result;
   }
 
+ private:
   scoped_refptr<ShapeResult> ShapeWithoutCache(const InlineItem& start_item,
                                                const Font& font,
                                                unsigned end_offset) {
@@ -165,6 +168,7 @@ class ReusingTextShaper final {
                                    reusable_shape_result->StartIndex()),
                           shape_result.get());
         offset = shape_result->EndIndex();
+        options_.han_kerning_start = false;
       }
       DCHECK_LT(offset, reusable_shape_result->EndIndex());
       DCHECK(shape_result->NumCharacters() == 0 ||
@@ -182,7 +186,6 @@ class ReusingTextShaper final {
     return shape_result;
   }
 
- private:
   void AppendShapeResult(const ShapeResult& shape_result, ShapeResult* target) {
     DCHECK(target->NumCharacters() == 0 ||
            target->EndIndex() == shape_result.StartIndex());
@@ -230,17 +233,20 @@ class ReusingTextShaper final {
     if (data_.segments) {
       return data_.segments->ShapeText(
           &shaper_, &font, direction, start_offset, end_offset,
-          base::checked_cast<unsigned>(&start_item - data_.items.begin()));
+          base::checked_cast<unsigned>(&start_item - data_.items.begin()),
+          options_);
     }
     RunSegmenter::RunSegmenterRange range =
         start_item.CreateRunSegmenterRange();
     range.end = end_offset;
-    return shaper_.Shape(&font, direction, start_offset, end_offset, range);
+    return shaper_.Shape(&font, direction, start_offset, end_offset, range,
+                         options_);
   }
 
   InlineItemsData& data_;
   const HeapVector<InlineItem>* const reusable_items_;
   HarfBuzzShaper shaper_;
+  ShapeOptions options_;
   const bool allow_shape_cache_;
 };
 
@@ -1318,6 +1324,7 @@ void InlineNode::ShapeText(InlineItemsData* data,
 
   // Provide full context of the entire node to the shaper.
   ReusingTextShaper shaper(data, previous_items, allow_shape_cache);
+  bool is_next_start_of_paragraph = true;
 
   DCHECK(!data->segments ||
          data->segments->EndOffset() == text_content.length());
@@ -1326,6 +1333,7 @@ void InlineNode::ShapeText(InlineItemsData* data,
     InlineItem& start_item = (*items)[index];
     if (start_item.Type() != InlineItem::kText || !start_item.Length()) {
       index++;
+      is_next_start_of_paragraph = start_item.IsForcedLineBreak();
       continue;
     }
 
@@ -1343,6 +1351,15 @@ void InlineNode::ShapeText(InlineItemsData* data,
              font.GetFontDescription().WidthVariant() != kRegularWidth);
     }
 #endif
+    shaper.SetOptions({
+        .han_kerning_start =
+            is_next_start_of_paragraph &&
+            RuntimeEnabledFeatures::CSSTextSpacingTrimEnabled() &&
+            ShouldTrimStartOfParagraph(
+                font.GetFontDescription().GetTextSpacingTrim()) &&
+            HanKerning::MaybeOpen(text_content[start_item.StartOffset()]),
+    });
+    is_next_start_of_paragraph = false;
     TextDirection direction = start_item.Direction();
     unsigned end_index = index + 1;
     unsigned end_offset = start_item.EndOffset();
