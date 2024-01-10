06e0432077a5f9da67b38ee0a00d3bf1861cb46b
Alex Gough <ajgo@chromium.org>
2024-01-10 01:52:37 +0000

    Remove unused type tags in sandbox
    Some type tags are unused and can be deleted, plus some tidying
    up in policy_engine_params.h.
    
    Bug: n/a
    Change-Id: Iddcb9e492a86ab43ccb000500fd80c99b2af89bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5182138
    Reviewed-by: James Forshaw <forshaw@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245039}

--- sandbox/win/src/policy_engine_params.h
+++ sandbox/win/src/policy_engine_params.h
@@ -56,9 +56,6 @@ namespace sandbox {
 //  have each 1) the address of the parameter 2) a numeric id that encodes the
 //  original C++ type. This allows the policy to treat any set of supported
 //  argument types uniformily and with some type safety.
-//
-//  TODO(cpu): support not fully implemented yet for unicode string and will
-//  probably add other types as well.
 class ParameterSet {
  public:
   ParameterSet() : real_type_(INVALID_TYPE), address_(nullptr) {}
@@ -117,12 +114,13 @@ class ParameterSet {
 // in ParameterSetEx with a template function ParamPickerMake to do the
 // parameter type deduction.
 
-// Base template class. Not implemented so using unsupported types should
-// fail to compile.
+// Base template class. Fails to compile to force use of implemented wrappers.
 template <typename T>
 class ParameterSetEx : public ParameterSet {
  public:
-  explicit ParameterSetEx(const void* address);
+  explicit ParameterSetEx(const void* address) {
+    static_assert(false, "Type not supported.");
+  }
 };
 
 template <>
@@ -158,13 +156,6 @@ class ParameterSetEx<uint32_t> : public ParameterSet {
  public:
   explicit ParameterSetEx(const void* address)
       : ParameterSet(UINT32_TYPE, address) {}
-};
-
-template <>
-class ParameterSetEx<UNICODE_STRING> : public ParameterSet {
- public:
-  explicit ParameterSetEx(const void* address)
-      : ParameterSet(UNISTR_TYPE, address) {}
 };
 
 template <typename T>
