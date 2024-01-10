86227f71469f801be10efab31fb4c8290e93ea31
Jiahe Zhang <jiahe.zhang@intel.com>
2024-01-10 08:19:01 +0000

    Follow-up to the kSingleVideoFrameRateThrottling feature
    This CL:
    - Adds Compositing.FrameRateDecider.ToggleFrameRateCase to
      histograms.xml and uses a 1/1000 subsampling.
    - Fixes a bug of taking videos with display framerate as perfect
      cadence.
    
    Bug: 1478671
    Change-Id: I93a230990ef6fa9f30cfbf4cacc0d261a045bab1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5134879
    Commit-Queue: Jianlin Qiu <jianlin.qiu@intel.com>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Jianlin Qiu <jianlin.qiu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1245142}

--- components/viz/service/display/frame_rate_decider.h
+++ components/viz/service/display/frame_rate_decider.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
 #include "base/memory/raw_ptr.h"
+#include "base/rand_util.h"
 #include "base/time/time.h"
 #include "components/viz/common/surfaces/surface_id.h"
 #include "components/viz/service/surfaces/surface_observer.h"
@@ -23,9 +24,10 @@ class SurfaceManager;
 enum class ToggleFrameRateCase : uint8_t {
   kNone = 0,
   kHardwareSupported = 1,
-  kSingleVideoPerfectCadence = 2,
-  kSingleVideoNoPerfectCadence = 3,
-  kMultipleVideos = 4,
+  kSingleVideoPerfectCadenceMatchesDisplay = 2,
+  kSingleVideoPerfectCadenceDiffersFromDisplay = 3,
+  kSingleVideoNoPerfectCadence = 4,
+  kMultipleVideos = 5,
   kMaxValue = kMultipleVideos
 };
 
@@ -111,6 +113,8 @@ class VIZ_SERVICE_EXPORT FrameRateDecider : public SurfaceObserver {
   base::TimeDelta current_preferred_frame_interval_;
 
   size_t min_num_of_frames_to_toggle_interval_;
+
+  base::MetricsSubSampler metrics_subsampler_;
 
   const raw_ptr<SurfaceManager> surface_manager_;
   const raw_ptr<Client> client_;
