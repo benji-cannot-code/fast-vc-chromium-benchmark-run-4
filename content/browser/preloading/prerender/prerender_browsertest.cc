70e8ca77bb64d4ca3b29b5bcec9436fce86334b9
Prudhvikumar Bommana <pbommana@google.com>
2024-01-10 18:23:44 +0000

    Revert "Prerender: stop prerender from overcounting not triggered failure"
    This reverts commit f16e811f86df29d768a63a918676058cef54125b.
    
    Reason for revert: The Cl lead to multiple Browser crashes on latest Canary(crbug.com/1517106,crbug.com/1517137 )
    
    Original change's description:
    > Prerender: stop prerender from overcounting not triggered failure
    >
    > Before this CL, PrerenderHostRegistry records
    > BatterySaver/PreloadingDisabled etc every time omnibox receives a new
    > input, but this recording logic is not consistent to other final
    > statuses. The inconsistent logic results in extremely high failure
    > rate in UMA, but that does not really reflect the things we'd like to
    > know.
    > This CL rewrites this logic. WebContentsImpl::StartPrerendering will
    > always creates a new PrerenderHandle instance. For PrerenderHandles
    > whose frame_tree_node_id_ is invalid, it means that the prerender
    > attempt was rejected by pre-checks. But we keeps this instance alive
    > to track the historical attempts.
    >
    >
    > See this document for rational.
    > https://docs.google.com/document/d/1a1mlf_JxMD2018bRZtzCR0J0eqFPY_0myBOGdcV12ts/edit#heading=h.7omvz2uyeznq
    >
    > Bug: 1505293
    > Change-Id: I55c6a1d521fdb7fd522a1ce808224cd5720abff1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5069215
    > Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    > Reviewed-by: Alan Cutter <alancutter@chromium.org>
    > Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    > Reviewed-by: Huanpo Lin <robertlin@chromium.org>
    > Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1244460}
    
    Bug: 1505293,1517106,1517137
    Change-Id: I13086d7be746e367b153e3ffad261b91fea2e40a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5186334
    Owners-Override: Prudhvikumar Bommana <pbommana@google.com>
    Commit-Queue: Prudhvikumar Bommana <pbommana@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1245389}

--- content/browser/preloading/prerender/prerender_browsertest.cc
+++ content/browser/preloading/prerender/prerender_browsertest.cc
@@ -6166,8 +6166,7 @@ IN_PROC_BROWSER_TEST_P(PrerenderEmbedderHostBlocklistedBrowserTest,
   std::unique_ptr<PrerenderHandle> prerender_handle =
       AddEmbedderTriggeredPrerenderAsync(kEmbedderPrerender);
 
-  EXPECT_TRUE(prerender_handle);
-  EXPECT_FALSE(prerender_handle->WasSuccessfullyTriggeredForTesting());
+  EXPECT_FALSE(prerender_handle);
   EXPECT_EQ(GetHostForUrl(kEmbedderPrerender),
             RenderFrameHost::kNoFrameTreeNodeId);
   histogram_tester().ExpectUniqueSample(
@@ -6923,8 +6922,7 @@ IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest, EmbedderPrerenderToNonHttpUrl) {
       AddEmbedderTriggeredPrerenderAsync(kPrerenderUrl);
 
   // Both the creation of PrerenderHandle and PrerenderHost should fail.
-  EXPECT_TRUE(prerender_handle);
-  EXPECT_FALSE(prerender_handle->WasSuccessfullyTriggeredForTesting());
+  EXPECT_FALSE(prerender_handle);
   EXPECT_EQ(GetHostForUrl(kPrerenderUrl), RenderFrameHost::kNoFrameTreeNodeId);
   histogram_tester().ExpectUniqueSample(
       "Prerender.Experimental.PrerenderHostFinalStatus.Embedder_"
@@ -9219,8 +9217,7 @@ IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest, StartByEmbeddersMultipleTimes) {
   // limit.
   std::unique_ptr<PrerenderHandle> prerender_handle3 =
       AddEmbedderTriggeredPrerenderAsync(kThirdPrerenderingUrl);
-  EXPECT_TRUE(prerender_handle3);
-  EXPECT_FALSE(prerender_handle3->WasSuccessfullyTriggeredForTesting());
+  EXPECT_FALSE(prerender_handle3);
 
   histogram_tester().ExpectBucketCount(
       "Prerender.Experimental.PrerenderHostFinalStatus.Embedder_"
@@ -9274,8 +9271,7 @@ IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest,
   // Start the third embedder triggered prerendering; this should hit the limit.
   std::unique_ptr<PrerenderHandle> prerender_handle3 =
       AddEmbedderTriggeredPrerenderAsync(kEmbedderPrerenderingUrl3);
-  EXPECT_TRUE(prerender_handle3);
-  EXPECT_FALSE(prerender_handle3->WasSuccessfullyTriggeredForTesting());
+  EXPECT_FALSE(prerender_handle3);
 
   histogram_tester().ExpectBucketCount(
       "Prerender.Experimental.PrerenderHostFinalStatus.Embedder_"
@@ -9938,8 +9934,7 @@ void PrerenderBrowserTest::TestEmbedderTriggerWithUnsupportedScheme(
   // Start prerendering by embedder triggered prerendering.
   std::unique_ptr<PrerenderHandle> prerender_handle =
       AddEmbedderTriggeredPrerenderAsync(prerendering_url, preloading_attempt);
-  EXPECT_TRUE(prerender_handle);
-  EXPECT_FALSE(prerender_handle->WasSuccessfullyTriggeredForTesting());
+  EXPECT_FALSE(prerender_handle);
 
   histogram_tester().ExpectUniqueSample(
       "Prerender.Experimental.PrerenderHostFinalStatus.Embedder_"
