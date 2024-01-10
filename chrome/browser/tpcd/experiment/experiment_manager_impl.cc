f9cb52f02bc7730c9e3ec8de44bc2d72b21c50c9
Anton Maliev <amaliev@chromium.org>
2024-01-10 00:44:00 +0000

    Follow-ups to ChromeOS eligibility fix.
    Addresses feedback on https://chromium-review.googlesource.com/c/chromium/src/+/5131754:
    - Removing force-eligibility flag from impl.
    - Consider only OTR profiles for guest.
    
    Fixed: b/302798031
    Change-Id: I0292d65043d505015c0af34b6470195d22bbcb23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5133214
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Anton Maliev <amaliev@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244994}

--- chrome/browser/tpcd/experiment/experiment_manager_impl.cc
+++ chrome/browser/tpcd/experiment/experiment_manager_impl.cc
@@ -52,12 +52,6 @@ bool NeedsOnboardingForExperiment() {
 
 }  // namespace
 
-// TODO(b/302798031): This flag is needed to deflake
-// ExperimentManagerImplSyntheticTrialTest on CQ. Remove once test is fixed.
-const base::FeatureParam<bool> kForceProfilesEligibleForTesting{
-    &features::kCookieDeprecationFacilitatedTesting, "force_profiles_eligible",
-    false};
-
 // static
 ExperimentManagerImpl* ExperimentManagerImpl::GetForProfile(Profile* profile) {
   if (!base::FeatureList::IsEnabled(
@@ -137,9 +131,7 @@ void ExperimentManagerImpl::SetClientEligibility(
 
   // Wait to run callback when decision is made in
   // `CaptureEligibilityInLocalStatePref`
-  if (!kForceProfilesEligibleForTesting.Get()) {
-    client_is_eligible_ = client_is_eligible_ && is_eligible;
-  }
+  client_is_eligible_ = client_is_eligible_ && is_eligible;
   callbacks_.push_back(std::move(on_eligibility_decision_callback));
 }
 
