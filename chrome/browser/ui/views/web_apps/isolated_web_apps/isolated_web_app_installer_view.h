2926bb79fbd59655d85620f88c81221244426fa3
Robbie McElrath <rmcelrath@chromium.org>
2024-01-11 00:47:37 +0000

    iwa: Support capturing nested dialogs in installer screenshot tests
    This adds support for taking screenshots of nested dialogs in the
    installer. Unfortunately I couldn't figure out how to capture the
    entire installer, so only the child dialog is captured.
    
    This copies and slightly simplifies the screenshot code from
    TestBrowserDialog, which couldn't be reused because it only supports
    tests that create a single Widget, but nested dialogs create a second.
    
    Bug: 1479140
    Change-Id: I245f17e4174e50b52729781e413ca3ee2a42c0b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5171757
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Zelin Liu <zelin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245631}

--- chrome/browser/ui/views/web_apps/isolated_web_apps/isolated_web_app_installer_view.h
+++ chrome/browser/ui/views/web_apps/isolated_web_apps/isolated_web_app_installer_view.h
@@ -38,6 +38,10 @@ class IsolatedWebAppInstallerView : public views::View {
   METADATA_HEADER(IsolatedWebAppInstallerView, views::View)
 
  public:
+  static constexpr char kInstallerWidgetName[] = "IsolatedWebAppInstaller";
+  static constexpr char kNestedDialogWidgetName[] =
+      "IsolatedWebAppInstallerDialog";
+
   class Delegate {
    public:
     virtual void OnSettingsLinkClicked() = 0;
