48c65463acc773f21471ced38e094b12790445c9
Nergi Rahardi <nergi@google.com>
2024-01-11 02:26:53 +0000

    [Per-App Language] Add metrics to record app language changes
    - Added new enum to indicate dialog open entry point
    - Using a hashed locale based on LocaleCodeISO639 for metrics breakdown
    
    The goal is to know the demographic of users and how many users start to
    use the feature.
    
    Bug: b:261200827
    Test: out_unittests/Debug/browser_tests
    Change-Id: Ie20b97331f5e38772cdee53b173615d4b80d95b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5180033
    Auto-Submit: Nergi Rahardi <nergi@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Lingyu Feng <lingyufeng@chromium.org>
    Commit-Queue: Nergi Rahardi <nergi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1245663}

--- chrome/test/data/webui/settings/chromeos/common/app_language_selection_dialog/app_language_selection_dialog_test.ts
+++ chrome/test/data/webui/settings/chromeos/common/app_language_selection_dialog/app_language_selection_dialog_test.ts
@@ -6,10 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://os-settings/lazy_load.js';
 
 import {AppLanguageSelectionDialogElement, AppLanguageSelectionItemElement} from 'chrome://os-settings/lazy_load.js';
-import {AppManagementStore, CrButtonElement, CrSearchFieldElement, IronListElement} from 'chrome://os-settings/os_settings.js';
+import {AppLanguageSelectionDialogEntryPoint, AppManagementStore, CrButtonElement, CrSearchFieldElement, IronListElement} from 'chrome://os-settings/os_settings.js';
 import {App, AppType} from 'chrome://resources/cr_components/app_management/app_management.mojom-webui.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {assertEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {fakeMetricsPrivate, MetricsTracker} from 'chrome://webui-test/metrics_test_support.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
 import {isVisible} from 'chrome://webui-test/test_util.js';
 
@@ -38,8 +39,10 @@ suite('<app-language-selection-dialog>', () => {
   let fakeHandler: FakePageHandler;
   let searchField: CrSearchFieldElement;
   let confirmButton: CrButtonElement;
+  let metrics: MetricsTracker;
 
   setup(async () => {
+    metrics = fakeMetricsPrivate();
     appLanguageSelectionDialog =
         document.createElement('app-language-selection-dialog');
 
@@ -53,11 +56,14 @@ suite('<app-language-selection-dialog>', () => {
 
   async function addDialog(
       arcConfig: AppConfig, appId: string,
-      lastSetAppLocalePref: chrome.settingsPrivate.PrefObject =
-          defaultPref): Promise<void> {
+      lastSetAppLocalePref: chrome.settingsPrivate.PrefObject = defaultPref,
+      entryPoint: AppLanguageSelectionDialogEntryPoint =
+          AppLanguageSelectionDialogEntryPoint.APPS_MANAGEMENT_PAGE):
+      Promise<void> {
     const arcApp: App = await fakeHandler.addApp(appId, arcConfig);
     await fakeHandler.flushPipesForTesting();
     appLanguageSelectionDialog.app = arcApp;
+    appLanguageSelectionDialog.entryPoint = entryPoint;
     appLanguageSelectionDialog.prefs = {
       arc: {last_set_app_locale: lastSetAppLocalePref},
     };
@@ -372,7 +378,7 @@ suite('<app-language-selection-dialog>', () => {
       });
 
   test(
-      'Confirm selection, selectedLocale should move to test locale',
+      'Confirm selection, selectedLocale should be set to test locale',
       async () => {
         const appId = 'confirm-selection';
         const testLocaleTag = 'testLocaleTag';
@@ -400,6 +406,79 @@ suite('<app-language-selection-dialog>', () => {
 
         const app = AppManagementStore.getInstance().data.apps[appId];
         assertEquals(testLocaleTag, app!.selectedLocale!.localeTag);
+      });
+
+  test(
+      'Open dialog from AppsManagementPage and confirm selection, ' +
+          'metrics recorded',
+      async () => {
+        const appId = 'open-dialog-from-apps-management-page';
+        const testLocaleTag = 'testLocaleTag';
+        const testDisplayName = 'testDisplayName';
+        const arcOptions: AppConfig = {
+          type: AppType.kArc,
+          supportedLocales: [{
+            localeTag: testLocaleTag,
+            displayName: testDisplayName,
+            nativeDisplayName: '',
+          }],
+        };
+        await addDialog(
+            arcOptions, appId, defaultPref,
+            AppLanguageSelectionDialogEntryPoint.APPS_MANAGEMENT_PAGE);
+
+        const filteredItems = getFilteredItems();
+        assertEquals(1, filteredItems.length);
+        filteredItems[0]!.shadowRoot!.querySelector<HTMLElement>(
+                                         listItemId)!.click();
+        // Test language should be selected.
+        assertLanguageItem(
+            filteredItems, /* idx= */ 0, testDisplayName,
+            /* isSelected= */ true, ListType.FILTERED);
+        confirmButton.click();
+        await fakeHandler.flushPipesForTesting();
+
+        assertEquals(
+            1,
+            metrics.count(
+                'Arc.AppLanguageSwitch.AppsManagementPage.TargetLanguage',
+                testLocaleTag));
+      });
+  test(
+      'Open dialog from LanguagesPage and confirm selection, ' +
+          'metrics recorded',
+      async () => {
+        const appId = 'open-dialog-from-languages-page';
+        const testLocaleTag = 'testLocaleTag';
+        const testDisplayName = 'testDisplayName';
+        const arcOptions: AppConfig = {
+          type: AppType.kArc,
+          supportedLocales: [{
+            localeTag: testLocaleTag,
+            displayName: testDisplayName,
+            nativeDisplayName: '',
+          }],
+        };
+        await addDialog(
+            arcOptions, appId, defaultPref,
+            AppLanguageSelectionDialogEntryPoint.LANGUAGES_PAGE);
+
+        const filteredItems = getFilteredItems();
+        assertEquals(1, filteredItems.length);
+        filteredItems[0]!.shadowRoot!.querySelector<HTMLElement>(
+                                         listItemId)!.click();
+        // Test language should be selected.
+        assertLanguageItem(
+            filteredItems, /* idx= */ 0, testDisplayName,
+            /* isSelected= */ true, ListType.FILTERED);
+        confirmButton.click();
+        await fakeHandler.flushPipesForTesting();
+
+        assertEquals(
+            1,
+            metrics.count(
+                'Arc.AppLanguageSwitch.LanguagesPage.TargetLanguage',
+                testLocaleTag));
       });
 
   test(
