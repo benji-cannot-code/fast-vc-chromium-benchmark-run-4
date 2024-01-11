56ff972d24b8c5da31e975ac18c24fd0bc508e44
Dibyajyoti Pal <dibyapal@google.com>
2024-01-11 19:13:45 +0000

    [dPWA] Fix punycode start_url host name in detailed install dialog
    This CL uses URL eliding APIs to format the start_url host name in the
    detailed install UI dialog to not show Punycode.
    url_formatter::FormatUrlForSecurityDisplay() has not been used because
    it does not work for RTL characters.
    
    Before: https://screenshot.googleplex.com/Ap3HrHtSH2goXPD
    After: https://screenshot.googleplex.com/4DYJFGQdS9n2xoD
    
    Bug: b/318397472
    Change-Id: Ieecac9e7dd91a14caf552415c3dcb85ea961a6b9
    Fixed: 1514764
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5190466
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Dibyajyoti Pal <dibyapal@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1246006}

--- chrome/browser/ui/views/web_apps/web_app_detailed_install_dialog.cc
+++ chrome/browser/ui/views/web_apps/web_app_detailed_install_dialog.cc
@@ -3,8 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/ui/views/web_apps/web_app_detailed_install_dialog.h"
+
 #include <memory>
 #include <numeric>
+#include <string>
 #include <vector>
 
 #include "base/functional/bind.h"
@@ -12,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ref.h"
 #include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
-#include "base/numerics/safe_conversions.h"
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/feature_engagement/tracker_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -22,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/toolbar_button_provider.h"
 #include "chrome/browser/ui/views/page_action/page_action_icon_view.h"
-#include "chrome/browser/ui/views/web_apps/web_app_detailed_install_dialog.h"
 #include "chrome/browser/ui/views/web_apps/web_app_info_image_source.h"
 #include "chrome/browser/ui/web_applications/web_app_dialogs.h"
 #include "chrome/browser/web_applications/web_app_helpers.h"
@@ -33,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/feature_engagement/public/tracker.h"
 #include "components/prefs/pref_service.h"
 #include "components/strings/grit/components_strings.h"
+#include "components/url_formatter/elide_url.h"
 #include "components/webapps/browser/installable/installable_data.h"
 #include "components/webapps/browser/installable/ml_install_operation_tracker.h"
 #include "components/webapps/common/constants.h"
@@ -363,7 +365,9 @@ void ShowWebAppDetailedInstallDialog(
                             gfx::Size(kIconSize, kIconSize));
 
   auto title = install_info->title;
-  auto start_url_host = install_info->start_url.host();
+  std::u16string start_url_host_formatted_for_display =
+      url_formatter::FormatUrlForDisplayOmitSchemePathAndTrivialSubdomains(
+          install_info->start_url);
   const std::u16string description = gfx::TruncateString(
       install_info->description, webapps::kMaximumDescriptionLength,
       gfx::CHARACTER_BREAK);
@@ -379,7 +383,7 @@ void ShowWebAppDetailedInstallDialog(
           .SetInternalName("WebAppDetailedInstallDialog")
           .SetIcon(ui::ImageModel::FromImageSkia(icon_image))
           .SetTitle(title)
-          .SetSubtitle(base::UTF8ToUTF16(start_url_host))
+          .SetSubtitle(start_url_host_formatted_for_display)
           .AddParagraph(
               ui::DialogModelLabel(description).set_is_secondary(),
               l10n_util::GetStringUTF16(
