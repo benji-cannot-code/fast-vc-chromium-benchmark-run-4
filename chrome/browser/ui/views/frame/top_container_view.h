6b7ab92bd6f9e543ecee647c560bbf819ee11073
Thomas Lukaszewicz <tluk@chromium.org>
2024-01-11 08:30:09 +0000

    [top chrome] Fix paint invalidation issue during immersive mode
    Custom themes with gradients or background images will paint in
    such a way that keeps the image position fixed wrt the image
    position on the browser window (see
    TopContainerBackground::PaintThemeAlignedImage).
    
    In order to retain image position when painting the
    TopContainerView as it slides down in immersive mode the view
    must be repainted to avoid visual artifacts.
    
    Calling View::SchedulePaint() alone is insufficient as the API
    does not correctly repaint child views that host their own layers.
    
    As an immediate fix the TopContainerView also schedules a paint on
    child view layers during immersive reveal. Follow up work will
    address the root issue in View::SchedulePaint.
    
    Bug: 1502757, 1516979
    Change-Id: Ib733eac422dc57dac6a66d6053064d6222f4a956
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5182178
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245738}

--- chrome/browser/ui/views/frame/top_container_view.h
+++ chrome/browser/ui/views/frame/top_container_view.h
@@ -24,6 +24,8 @@ class TopContainerView : public views::View {
   TopContainerView& operator=(const TopContainerView&) = delete;
   ~TopContainerView() override;
 
+  void OnImmersiveRevealUpdated();
+
   // views::View overrides:
   void PaintChildren(const views::PaintInfo& paint_info) override;
   void ChildPreferredSizeChanged(views::View* child) override;
