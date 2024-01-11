13cae01f2bdb1a27d3328f7dd8e8b2137299ecee
Lily Chen <chlily@chromium.org>
2024-01-11 21:58:44 +0000

    [ImprovedDownloadWarnings] Show toast for discard dangerous
    We currently show a "toast" notification when the user removes an item
    (or clears all items) from chrome://downloads. This CL adds a similar
    toast for discarding a dangerous file.
    
    Modifies the strings on the toast in consultation with UXW. There are
    two variants of the string. One of them applies to files that are
    completely on disk, and indicates to the user that deleting the file
    from download history still keeps the actual file on disk.
    
    The undo behavior is the same as before: Undoing deletion of normal
    files is possible, but dangerous and insecure files are removed directly
    without a chance to undo.
    
    Also adds a CSS variable to allow customizing the width of the toast
    because the added string may be too long for the default width.
    
    Screenshots:
    https://drive.google.com/drive/folders/1YurMbHeYKZ29sllLGVOGmGCvd0O2GY47
    
    Bug: 1465966
    Change-Id: Icf1487e386dcf8fc8b5d8f86a9fa95b45996fdf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5182177
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1246090}

--- chrome/test/data/webui/downloads/item_test.ts
+++ chrome/test/data/webui/downloads/item_test.ts
@@ -545,7 +545,8 @@ suite('item tests', function() {
     toastManager.show('', /* hideSlotted= */ false);
     assertFalse(toastManager.slottedHidden);
     item.getMoreActionsButton().click();
-    const removeButton = item.shadowRoot!.querySelector<HTMLElement>('#remove');
+    const removeButton =
+        item.shadowRoot!.querySelector<HTMLElement>('#discard-dangerous');
     assertTrue(!!removeButton);
     removeButton.click();
     assertTrue(toastManager.slottedHidden);
@@ -564,7 +565,8 @@ suite('item tests', function() {
     toastManager.show('', /* hideSlotted= */ false);
     assertFalse(toastManager.slottedHidden);
     item.getMoreActionsButton().click();
-    const removeButton = item.shadowRoot!.querySelector<HTMLElement>('#remove');
+    const removeButton =
+        item.shadowRoot!.querySelector<HTMLElement>('#discard-dangerous');
     assertTrue(!!removeButton);
     removeButton.click();
     assertTrue(toastManager.slottedHidden);
