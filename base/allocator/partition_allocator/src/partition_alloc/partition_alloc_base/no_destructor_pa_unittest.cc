816005b8391e6edc7a3f697191c994eca645bffc
Daniel Cheng <dcheng@chromium.org>
2024-01-11 19:01:19 +0000

    Make PA's NoDestructor compatible with trivially destructible types.
    There are some types which are only trivially destructible on certain
    platforms, e.g. std::mutex. The current prohibition makes it rather
    annoying to use NoDestructor with such a type.
    
    Making NoDestructor constexpr also allows the constinit qualifier to be
    used with NoDestructor when the type is trivially destructible, avoiding
    the need to use a wrapper function at all.
    
    Change-Id: Ifc6b6f44bd9a0bd57ac09fe43c5625cb90c06134
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5181785
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245998}

---
+++ base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/no_destructor_pa_unittest.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <vector>
+
+#include "partition_alloc/partition_alloc_base/no_destructor.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace partition_alloc::internal::base {
+
+static constinit NoDestructor<int> an_int;
+
+static auto& GetVector() {
+  static NoDestructor<std::vector<int>> a_vec({1, 2, 3});
+  return *a_vec;
+}
+
+TEST(NoDestructorTest, Basic) {
+  // Should always be the same instance.
+  auto* instance = &GetVector();
+  ASSERT_EQ(instance, &GetVector());
+
+  EXPECT_THAT(*instance, ::testing::ElementsAre(1, 2, 3));
+
+  EXPECT_EQ(0, *an_int);
+}
+
+}  // namespace partition_alloc::internal::base
