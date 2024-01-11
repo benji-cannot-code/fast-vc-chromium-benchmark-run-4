6e25b32618eb1ee6e70943cac98ab31eca26e7d1
Xiaoming Shi <xiaomings@google.com>
2024-01-11 23:45:23 +0000

    Remove unused include from h264_bitstream_buffer.h
    Removed the unused include of "media/base/video_frame.h" to reduce
    dependency.
    
    Bug: b/319697087
    
    Change-Id: I0de701aaaf670b140c9408c7e42d89de06777a60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5186682
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Xiaoming Shi <xiaomings@google.com>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1246156}

--- media/filters/h264_bitstream_buffer.h
+++ media/filters/h264_bitstream_buffer.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/numerics/safe_conversions.h"
 #include "media/base/media_export.h"
-#include "media/base/video_frame.h"
 #include "media/video/h264_parser.h"
 
 namespace media {
