2668e50512f03e4bc6b6ee8dd42d85c83e75f709
Matt Jones <mdjones@chromium.org>
2024-01-11 20:57:33 +0000

    Reland "[Price tracking] Replace the UI logic in the tab helper"
    This is a reland of commit b66586a1b102ace6bac7d09452d6d0ec49cead07
    
    This reland updates the traffic annotation safe_list.txt for the
    updated location of the annotation for price tracking and re-adds
    the metadata removed here: https://crrev.com/c/5122392
    
    Original change's description:
    > [Price tracking] Replace the UI logic in the tab helper
    >
    > This patch switches the logic for the price tracking chip to use
    > the new button controller. A few obsolete tests have been cleaned
    > up as a result.
    >
    > Bug: b:301440117
    > Change-Id: If1bab9e6a1ba309caaf27f5582ed96e6eccf51f1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5013799
    > Reviewed-by: Mei Liang <meiliang@chromium.org>
    > Reviewed-by: Emily Shack <emshack@chromium.org>
    > Commit-Queue: Matthew Jones <mdjones@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1242007}
    
    Bug: b:301440117
    Change-Id: I7af7fb673a6b9ebcdc098598c572507ece7f1094
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5166387
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1246052}

--- chrome/browser/ui/commerce/price_tracking_page_action_controller_unittest.cc
+++ chrome/browser/ui/commerce/price_tracking_page_action_controller_unittest.cc
@@ -3,7 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/ui/commerce/price_tracking_page_action_controller.h"
+
 #include <memory>
+#include <optional>
 
 #include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
@@ -11,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/mock_callback.h"
 #include "base/test/task_environment.h"
-#include "chrome/browser/ui/commerce/price_tracking_page_action_controller.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/commerce/core/commerce_feature_list.h"
 #include "components/commerce/core/mock_shopping_service.h"
@@ -159,7 +161,7 @@ TEST_F(PriceTrackingPageActionControllerUnittest, IconNotShown_NoProductInfo) {
   ASSERT_FALSE(controller.ShouldShowForNavigation().has_value());
   ASSERT_FALSE(controller.WantsExpandedUi());
 
-  EXPECT_CALL(notify_host_callback_, Run()).Times(1);
+  EXPECT_CALL(notify_host_callback_, Run()).Times(testing::AtLeast(1));
   EXPECT_CALL(*shopping_service_, IsSubscribed(testing::_, testing::_))
       .Times(0);
   shopping_service_->SetResponseForGetProductInfoForUrl(std::nullopt);
