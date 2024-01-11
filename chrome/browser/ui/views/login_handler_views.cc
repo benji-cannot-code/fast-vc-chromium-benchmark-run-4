e63c1e2fa6e84aca930841d0208bef26e8c9bc48
Erik Chen <erikchen@chromium.org>
2024-01-11 03:30:37 +0000

    Revert "Improve LoginHandler (Part 8 / N)"
    This reverts commit f70211ea73ec01fd1ea786f64bea5d302cf163ac.
    
    Reason for revert: 
    
    Likely responsible for crash: https://bugs.chromium.org/p/chromium/issues/detail?id=1516256
    
    There is another source of re-entrancy from the android http-auth implementation.
    
    
    Original change's description:
    > Improve LoginHandler (Part 8 / N)
    >
    > This CL is a refactor with no intended behavior change. This CL removes
    > asynchronous startup code from LoginHandler as it's no longer necessary.
    >
    > Bug: 1371177
    > Change-Id: I7645dbe63aa3d99d3ad5df44776cecd486ca2e63
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5153442
    > Reviewed-by: Rachel Blum <groby@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Erik Chen <erikchen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1241290}
    
    Bug: 1371177
    Change-Id: I335a6696cf666973c44ad365f392ab0ec322b504
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5188233
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245681}

--- chrome/browser/ui/views/login_handler_views.cc
+++ chrome/browser/ui/views/login_handler_views.cc
@@ -47,7 +47,7 @@ class LoginHandlerViews : public LoginHandler {
 
  protected:
   // LoginHandler:
-  bool BuildViewImpl(const std::u16string& authority,
+  void BuildViewImpl(const std::u16string& authority,
                      const std::u16string& explanation,
                      LoginModelData* login_model_data) override {
     DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
@@ -55,7 +55,6 @@ class LoginHandlerViews : public LoginHandler {
 
     dialog_ = new Dialog(this, web_contents(), authority, explanation,
                          login_model_data);
-    return true;
   }
 
   void CloseDialog() override {
