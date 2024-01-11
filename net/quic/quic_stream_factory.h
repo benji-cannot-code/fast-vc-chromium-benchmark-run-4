6d9f70446120df18121a9c10e99e42b2706a9ec8
Kenichi Ishibashi <bashi@chromium.org>
2024-01-11 04:15:21 +0000

    Use scoped_refptr for QuicStreamFactory's task runner
    SequencedTaskRunner should be kept as scoped_refptr to avoid dangling
    pointer.
    
    Change-Id: I9e8f5495ba141f15f485bbf42e910425429fa7cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5184571
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245692}

--- net/quic/quic_stream_factory.h
+++ net/quic/quic_stream_factory.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/lru_cache.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/time/tick_clock.h"
@@ -716,7 +717,7 @@ class NET_EXPORT_PRIVATE QuicStreamFactory
 
   raw_ptr<const base::TickClock, DanglingUntriaged> tick_clock_ = nullptr;
 
-  raw_ptr<base::SequencedTaskRunner, DanglingUntriaged> task_runner_ = nullptr;
+  scoped_refptr<base::SequencedTaskRunner> task_runner_ = nullptr;
 
   const raw_ptr<SSLConfigService> ssl_config_service_;
 
