2381019d45a10d5861009eb2bc66bd35f162d8e8
Theo Cristea <theocristea@google.com>
2024-01-11 10:08:31 +0000

    [mullet m2] Suggestion's main text for all name fields is now NAME_FULL
    Demo: http://go/scrcast/NDk1NTk2MTUyNDQ4NjE0NHwzYjg3YTAxNy03MA
    
    Bug: 1459990
    Change-Id: I7e49112116768184319efbbb550d972e6134daba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5185336
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Cr-Commit-Position: refs/heads/main@{#1245752}

--- components/autofill/core/browser/autofill_suggestion_generator_unittest.cc
+++ components/autofill/core/browser/autofill_suggestion_generator_unittest.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "components/autofill/core/browser/autofill_suggestion_generator.h"
+
 #include <string>
 #include <vector>
 
@@ -16,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/uuid.h"
 #include "components/autofill/core/browser/autofill_granular_filling_utils.h"
-#include "components/autofill/core/browser/autofill_suggestion_generator.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/autofill/core/browser/data_model/autofill_wallet_usage_data.h"
@@ -32,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/test_personal_data_manager.h"
 #include "components/autofill/core/browser/ui/popup_item_ids.h"
 #include "components/autofill/core/browser/ui/suggestion.h"
+#include "components/autofill/core/browser/ui/suggestion_test_helpers.h"
 #include "components/autofill/core/browser/webdata/autofill_webdata_service.h"
 #include "components/autofill/core/common/autofill_clock.h"
 #include "components/autofill/core/common/autofill_features.h"
@@ -762,6 +764,21 @@ TEST_F(AutofillSuggestionGeneratorTest,
   EXPECT_EQ(u"12345678910", suggestions[0].main_text.value);
 }
 
+TEST_F(AutofillSuggestionGeneratorTest,
+       CreateSuggestionsFromProfiles_PartialNameFieldHasFullNameMainText) {
+  base::test::ScopedFeatureList features(
+      features::kAutofillGranularFillingAvailable);
+  AutofillProfile profile = test::GetFullProfile();
+
+  EXPECT_THAT(
+      suggestion_generator()->CreateSuggestionsFromProfiles(
+          {&profile}, {NAME_FIRST, NAME_LAST},
+          /*last_targeted_fields=*/absl::nullopt, NAME_FIRST,
+          /*trigger_field_max_length=*/0),
+      SuggestionVectorMainTextsAre(Suggestion::Text(
+          profile.GetRawInfo(NAME_FULL), Suggestion::Text::IsPrimary(true))));
+}
+
 // TODO(crbug.com/1459990): Move AutofillChildrenSuggestionGeneratorTest.
 // CreateSuggestionsFromProfiles_GroupFillingLabels_* tests under this fixture.
 // Text fixture for label generation related tests. Parameterized by triggering
@@ -996,13 +1013,13 @@ TEST_F(
     AutofillChildrenSuggestionGeneratorTest,
     CreateSuggestionsFromProfiles_GroupFillingLabels_AddFillNameAndDifferentiatingLabel) {
   AutofillProfile profile_1 = test::GetFullProfile();
-  profile_1.SetRawInfo(NAME_FULL, u"Cersei Lannister");
+  profile_1.SetRawInfo(ADDRESS_HOME_ZIP, u"100100");
 
   AutofillProfile profile_2 = test::GetFullProfile();
-  profile_2.SetRawInfo(NAME_FULL, u"Cersei Baratheon");
+  profile_2.SetRawInfo(ADDRESS_HOME_ZIP, u"200200");
 
-  // `profile_1` and `profile_2` have the same `NAME_FIRST`, which will lead to
-  // the necessity of a differentiating label (`NAME_FULL`).
+  // `profile_1` and `profile_2` have the same `NAME_FULL`, which will lead to
+  // the necessity of a differentiating label (`ADDRESS_HOME_ZIP`).
   std::vector<Suggestion> suggestions =
       suggestion_generator()->CreateSuggestionsFromProfiles(
           {&profile_1, &profile_2}, {NAME_FIRST},
@@ -1010,9 +1027,10 @@ TEST_F(
           /*trigger_field_max_length=*/0);
 
   ASSERT_EQ(suggestions.size(), 2u);
-  EXPECT_EQ(suggestions[0].labels,
-            std::vector<std::vector<Suggestion::Text>>({{Suggestion::Text(
-                u"Fill full name - Cersei Lannister, 666 Erebus St.")}}));
+  EXPECT_EQ(
+      suggestions[0].labels,
+      std::vector<std::vector<Suggestion::Text>>(
+          {{Suggestion::Text(u"Fill full name - 666 Erebus St., 100100")}}));
 }
 
 TEST_F(AutofillChildrenSuggestionGeneratorTest,
@@ -1026,7 +1044,7 @@ TEST_F(AutofillChildrenSuggestionGeneratorTest,
   EXPECT_THAT(suggestions,
               ElementsAre(Field(
                   &Suggestion::main_text,
-                  Suggestion::Text(profile().GetInfo(NAME_FIRST, app_locale()),
+                  Suggestion::Text(profile().GetInfo(NAME_FULL, app_locale()),
                                    Suggestion::Text::IsPrimary(true)))));
 
   // The children suggestions should be.
@@ -1127,7 +1145,7 @@ TEST_F(
   EXPECT_THAT(suggestions[0],
               EqualsFieldByFieldFillingSuggestion(
                   PopupItemId::kAddressFieldByFieldFilling,
-                  profile().GetInfo(NAME_FIRST, app_locale()), NAME_FIRST,
+                  profile().GetInfo(NAME_FULL, app_locale()), NAME_FIRST,
                   Suggestion::Guid(profile().guid()), {{}}));
 }
 
