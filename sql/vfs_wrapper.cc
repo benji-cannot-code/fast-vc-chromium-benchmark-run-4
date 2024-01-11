7bd3033e184c4275283aa7da3afe213092e314ed
Evan Stade <estade@chromium.org>
2024-01-11 17:18:54 +0000

    SQLite: sidestep Mac crash when acting on non-existent file
    Chrome tries to propagate TimeMachine exclusions from the main
    database file to its journal file. In some cases, the database
    file doesn't exist, which causes a DCHECK. This change papers
    over that by verifying the database file exists before calling
    base utilities on it.
    
    It's not clear if the existence of a -journal file without
    the database file is an error case or can happen normally.
    If it is an error case, then we rely on other SQLite code to
    notice and error out (but not crash). If it is not an error
    case, then the worst thing that will happen due to this change
    is that a -journal file may not be excluded from TimeMachine.
    The journal file is much smaller than the database file, so
    this outcome, while not ideal, is hardly terrible.
    
    crbug.com/25959 has the original justification for this
    behavior, and in that example the journal files are less than
    2% of the size of the database files.
    
    Bug: 1493504
    Change-Id: Ib15a97572b46f54c03699568cf46999cb80c78aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5187142
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245920}

--- sql/vfs_wrapper.cc
+++ sql/vfs_wrapper.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_APPLE)
 #include "base/apple/backup_util.h"
+#include "base/files/file_util.h"
 #endif
 
 #if BUILDFLAG(IS_FUCHSIA)
@@ -207,8 +208,10 @@ int Open(sqlite3_vfs* vfs, const char* file_name, sqlite3_file* wrapper_file,
     base::StringPiece file_name_string_piece(file_name);
     size_t dash_index = file_name_string_piece.rfind('-');
     if (dash_index != base::StringPiece::npos) {
-      base::StringPiece db_name(file_name, dash_index);
-      if (base::apple::GetBackupExclusion(base::FilePath(db_name))) {
+      base::FilePath database_file_path(
+          base::StringPiece(file_name, dash_index));
+      if (base::PathExists(database_file_path) &&
+          base::apple::GetBackupExclusion(database_file_path)) {
         base::apple::SetBackupExclusion(base::FilePath(file_name_string_piece));
       }
     }
