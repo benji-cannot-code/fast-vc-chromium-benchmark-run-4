e1625e1c0f2f9407bba7fd12d533c6679ed909d9
Elmehdi Rahmaoui <erahmaoui@google.com>
2024-01-11 15:28:44 +0000

    [iOS] Update the label font in group grid cell
    Before: https://drive.google.com/file/d/1ruzrQTS_-fny2psM9b8GdqJLyNZ73pHp/view?usp=sharing&resourcekey=0-7wC-D_k7wTCG9hRsWUQTEw
    
    After: https://drive.google.com/file/d/16gzLuo94pv3yFzuH8rrQmYdSM6AiPwUi/view?usp=sharing&resourcekey=0-UIpX2R2vxzxvRgX2qJpt2Q
    
    Bug: 1501837
    Change-Id: Ie88466ff69c4cb75a519e0d54d6a65fb7c188ee9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5182961
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245860}

--- ios/chrome/browser/shared/ui/util/uikit_ui_util.mm
+++ ios/chrome/browser/shared/ui/util/uikit_ui_util.mm
@@ -363,7 +363,7 @@ NSAttributedString* TextForTabGroupCount(int count, CGFloat font_size) {
     string = @"99+";
   }
 
-  UIFont* font = [UIFont systemFontOfSize:font_size weight:UIFontWeightLight];
+  UIFont* font = [UIFont systemFontOfSize:font_size weight:UIFontWeightMedium];
   UIFontDescriptor* descriptor = [font.fontDescriptor
       fontDescriptorWithDesign:UIFontDescriptorSystemDesignRounded];
   font = [UIFont fontWithDescriptor:descriptor size:font_size];
