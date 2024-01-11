4e79e462b2a7bb733875a73b4146dbcc5bdd618e
Jood Hajeer <jood@google.com>
2024-01-11 13:25:28 +0000

    [iOS] Fix failing testNoSyncPromoIfSyncToSigninEnabled
    The test is failing because it should be testing the old flow; before
    kEnableReviewAccountSettingsPromo. So this fix disables the feature for
    this test.
    
    There are other tests for the same flow with this feature enabled.
    
    Fixed: 1517375
    Change-Id: I5cd52166cdb5f7200c4e1340ba325ae0faf9138c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5185440
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1245793}

--- ios/chrome/browser/ui/bookmarks/home/bookmarks_promo_egtest.mm
+++ ios/chrome/browser/ui/bookmarks/home/bookmarks_promo_egtest.mm
@@ -79,6 +79,7 @@ using chrome_test_util::SettingsDoneButton;
                    (testNoSyncPromoIfSyncToSigninEnabled)]) {
     config.features_enabled.push_back(
         syncer::kReplaceSyncPromosWithSignInPromos);
+    config.features_disabled.push_back(kEnableReviewAccountSettingsPromo);
   } else if ([self isRunningTest:@selector
                    (testSigninWithSyncPassphraseAndTurnOnSync)]) {
     config.features_disabled.push_back(
