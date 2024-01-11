a74160e1b1e8c299f7153dba1e0052f06f1c0896
Florian Leimgruber <fleimgruber@google.com>
2024-01-11 15:16:15 +0000

    Move autofill profile and contact info sync util to webdata/addresses
    Since this is sync related code, it shouldn't live in the top level
    components/autofill/core/browser/ directory.
    
    This CL was auto-generated using:
      git mv components/autofill/core/browser/*sync*
             components/autofill/core/browser/webdata/addresses/
      ./tools/git/mass-rename.sh
      git cl format
    
    Bug: 1007974
    Change-Id: I4b5514f25fe83fd7bde3849e08cee0ca1c964ce4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5185181
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1245845}

--- components/autofill/core/browser/webdata/addresses/contact_info_sync_bridge.cc
+++ components/autofill/core/browser/webdata/addresses/contact_info_sync_bridge.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/ranges/algorithm.h"
 #include "base/uuid.h"
-#include "components/autofill/core/browser/contact_info_sync_util.h"
+#include "components/autofill/core/browser/webdata/addresses/contact_info_sync_util.h"
 #include "components/autofill/core/browser/webdata/autofill_webdata_service.h"
 #include "components/sync/base/features.h"
 #include "components/sync/base/model_type.h"
