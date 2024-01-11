fa8d765110e75bedeb9d93fb8436a13df0d53433
Max Ihlenfeldt <max@igalia.com>
2024-01-11 14:41:45 +0000

    IWYU for chrome/browser/ui/tabs/organization/trigger_observer.h
    Bug: None
    Change-Id: Ifff498b4a35f2139f3454d368be75d73c6277538
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5116504
    Auto-Submit: Max Ihlenfeldt <max@igalia.com>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Commit-Queue: Max Ihlenfeldt <max@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1245824}

--- chrome/browser/ui/tabs/organization/trigger_observer.h
+++ chrome/browser/ui/tabs/organization/trigger_observer.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_TABS_ORGANIZATION_TRIGGER_OBSERVER_H_
 
 #include <memory>
+#include <unordered_map>
 
 #include "base/functional/callback.h"
 #include "chrome/browser/ui/browser_list_observer.h"
