123798f36d839ffcd8cfb7693b51a9723f837356
Cheng Zhao <zcbenz@gmail.com>
2024-01-11 14:51:22 +0000

    Return const reference in Badge::GetText
    Bug: 1400029
    Change-Id: Id29b733c35d1824a064103a1f67608d744770b83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5129826
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245829}

--- ui/views/controls/badge.cc
+++ ui/views/controls/badge.cc
@@ -15,7 +15,7 @@ Badge::Badge(const std::u16string& text) : text_(text) {}
 
 Badge::~Badge() = default;
 
-std::u16string Badge::GetText() const {
+const std::u16string& Badge::GetText() const {
   return text_;
 }
 
