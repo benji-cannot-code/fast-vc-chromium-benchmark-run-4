6fdf970ad862fd4e1be5d974a896bf9c8aa72555
Benjamin Williams <bwwilliams@google.com>
2024-01-11 18:31:22 +0000

    [iOS] Creates DockingPromoAppAgent to register promo on app launch.
    This change introduces DockingPromoAppAgent which conditionally
    registers/deregisters the new DockingPromo for display at app launch.
    
    Bug: 1516668
    Change-Id: I393c76da7f5687e1e8a63955b27ba80a31b0dbe2
    Low-Coverage-Reason: Unit tests coming in fast-follow CLs.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5178266
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1245977}

---
+++ ios/chrome/app/docking_promo_app_agent.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_APP_DOCKING_PROMO_APP_AGENT_H_
+#define IOS_CHROME_APP_DOCKING_PROMO_APP_AGENT_H_
+
+#import "ios/chrome/app/application_delegate/app_state_agent.h"
+
+class PromosManager;
+
+// App agent that displays the Docking Promo when needed.
+@interface DockingPromoAppAgent : NSObject <AppStateAgent>
+
+- (instancetype)initWithPromosManager:(PromosManager*)promosManager;
+
+@end
+
+#endif  // IOS_CHROME_APP_DOCKING_PROMO_APP_AGENT_H_
--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/app/application_storage_metrics.h"
 #import "ios/chrome/app/blocking_scene_commands.h"
 #import "ios/chrome/app/deferred_initialization_runner.h"
+#import "ios/chrome/app/docking_promo_app_agent.h"
 #import "ios/chrome/app/enterprise_app_agent.h"
 #import "ios/chrome/app/fast_app_terminate_buildflags.h"
 #import "ios/chrome/app/features.h"
@@ -534,6 +535,20 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
                     identityManager:IdentityManagerFactory::GetForBrowserState(
                                         chromeBrowserState)
                          localState:GetApplicationContext()->GetLocalState()]];
+
+  if (IsDockingPromoEnabled()) {
+    switch (DockingPromoExperimentTypeEnabled()) {
+      case DockingPromoDisplayTriggerArm::kDuringFRE:
+        break;
+      case DockingPromoDisplayTriggerArm::kAfterFRE:
+      case DockingPromoDisplayTriggerArm::kAppLaunch:
+        [self.appState
+            addAgent:[[DockingPromoAppAgent alloc]
+                         initWithPromosManager:PromosManagerFactory::
+                                                   GetForBrowserState(
+                                                       chromeBrowserState)]];
+    }
+  }
 
   // Perform any background initialisation that is required and then
   // migrate to the next stage.
