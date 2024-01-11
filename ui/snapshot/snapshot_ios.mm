f9712ab14cc16ed8bad4227176c29e5798f53f89
Avi Drissman <avi@chromium.org>
2024-01-11 23:25:01 +0000

    Revert "Remove GrabWindowSnapshot"
    This reverts commit 957ab645e0ff596922c3c41a2f2c44a9714f07a0.
    
    Reason for revert: Compile error in iOS code, not caught by trybots
    
    Original change's description:
    > Remove GrabWindowSnapshot
    >
    > It's sync, and all callers were removed in the previous CL in the
    > series.
    >
    > Bug: 1517390
    > Change-Id: I5c21bdb560eca366d3904c27784a924ed997ec15
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5189766
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Commit-Queue: Avi Drissman <avi@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1246103}
    
    Bug: 1517390
    Change-Id: I2d6a38727bcf68bc5874926eaeb3a2a0cfebd661
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5191446
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1246141}

--- ui/snapshot/snapshot_ios.mm
+++ ui/snapshot/snapshot_ios.mm
@@ -48,6 +48,23 @@ bool GrabViewSnapshot(gfx::NativeView view,
   return false;
 }
 
+bool GrabWindowSnapshot(gfx::NativeWindow window,
+                        const gfx::Rect& snapshot_bounds,
+                        gfx::Image* image) {
+  UIWindow* source_window = window.Get();
+  if (source_window == nil) {
+    return false;
+  }
+
+  UIImage* snapshot = GetViewSnapshot(source_window.rootViewController.view,
+                                      snapshot_bounds.ToCGRect());
+  if (snapshot) {
+    *image = gfx::Image(snapshot);
+    return true;
+  }
+  return false;
+}
+
 void GrabViewSnapshotAsync(gfx::NativeView view,
                            const gfx::Rect& source_rect,
                            GrabSnapshotImageCallback callback) {
@@ -60,16 +77,7 @@ void GrabWindowSnapshotAsync(gfx::NativeWindow window,
                              const gfx::Rect& source_rect,
                              GrabSnapshotImageCallback callback) {
   gfx::Image image;
-
-  UIWindow* source_window = window.Get();
-  if (source_window) {
-    UIImage* snapshot = GetViewSnapshot(source_window.rootViewController.view,
-                                        snapshot_bounds.ToCGRect());
-    if (snapshot) {
-      image = gfx::Image(snapshot);
-    }
-  }
-
+  GrabWindowSnapshot(window, source_rect, &image);
   std::move(callback).Run(image);
 }
 
