2fbe7547a9b10c8e0cf6f8c9971283068d26a857
EmiliaPaz <emiliapaz@chromium.org>
2024-01-12 20:00:44 +0000

    [Extensions c2s] Fix extensions menu bottom rounded margins
    Extensions menu should have rounded margins. However, bottom margins
    were not rounded because the last view is a hover button that has its
    own margins that appear "over" the dialog margins.
    
    This is fixed by moving the bottom dialog margins from the menu items section to the parent scroll view (which is the last direct child of
    the extensions menu).
    
    Screenshots:
    - Before: https://drive.google.com/file/d/1dCenlZ5yS8xcDdA8Jm-fXMSrUIRF80d8/view?usp=sharing
    - After: https://drive.google.com/file/d/1uC_4qOL6jwzXKuSZ0RQ2Vngc8LXFzw8v/view?usp=sharing
    
    Bug: 1517746
    Change-Id: I91d66b6d574491333e6889e5ce34a7a2397660b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5191647
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1246623}

--- chrome/browser/ui/views/extensions/extensions_menu_main_page_view.cc
+++ chrome/browser/ui/views/extensions/extensions_menu_main_page_view.cc
@@ -666,6 +666,14 @@ ExtensionsMenuMainPageView::ExtensionsMenuMainPageView(
               .SetDrawOverflowIndicator(false)
               .SetHorizontalScrollBarMode(
                   views::ScrollView::ScrollBarMode::kDisabled)
+              // Add bottom dialog margins since it's the last view. Its last
+              // child view will be a HoverButton with has its own inset. Thus,
+              // we subtract such insets from the dialog bottom inset.
+              .SetProperty(views::kMarginsKey,
+                           gfx::Insets::TLBR(0, 0,
+                                             dialog_insets.bottom() -
+                                                 hover_button_vertical_spacing,
+                                             0))
               .SetContents(
                   views::Builder<views::BoxLayoutView>()
                       .SetOrientation(views::BoxLayout::Orientation::kVertical)
@@ -694,15 +702,6 @@ ExtensionsMenuMainPageView::ExtensionsMenuMainPageView(
                           // Menu items section.
                           views::Builder<views::BoxLayoutView>()
                               .CopyAddressTo(&menu_items_)
-                              // Add bottom dialog margins since it's the last
-                              // element.
-                              .SetProperty(
-                                  views::kMarginsKey,
-                                  gfx::Insets::TLBR(
-                                      0, 0,
-                                      dialog_insets.bottom() -
-                                          hover_button_vertical_spacing,
-                                      0))
                               .SetOrientation(
                                   views::BoxLayout::Orientation::kVertical))))
 
