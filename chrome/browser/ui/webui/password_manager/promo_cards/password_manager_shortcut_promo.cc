07aeb37f3553dcdd7074954cbf949743ba0de5c9
Viktor Semeniuk <vsemeniuk@google.com>
2024-01-12 09:04:55 +0000

    Prevent shortcut promo card from appearing if web apps are disabled
    Password Manager shortcut is web app. So if web apps are disabled for a
    profile it shouldn't be possible to add Password Manager shortcut.
    
    Fixed: 1517622
    Change-Id: Ic47feb3b87d2cca012735e8687f98b55989f74fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5190294
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Andrii Natiahlyi <natiahlyi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1246337}

--- chrome/browser/ui/webui/password_manager/promo_cards/password_manager_shortcut_promo.cc
+++ chrome/browser/ui/webui/password_manager/promo_cards/password_manager_shortcut_promo.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/user_education/user_education_service.h"
 #include "chrome/browser/user_education/user_education_service_factory.h"
 #include "chrome/browser/web_applications/web_app_helpers.h"
+#include "chrome/browser/web_applications/web_app_utils.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/password_manager/core/browser/password_manager_constants.h"
@@ -36,7 +37,7 @@ password_manager::PromoCardType PasswordManagerShortcutPromo::GetPromoCardType()
 }
 
 bool PasswordManagerShortcutPromo::ShouldShowPromo() const {
-  if (is_shortcut_installed_) {
+  if (is_shortcut_installed_ || !web_app::AreWebAppsEnabled(profile_)) {
     return false;
   }
 
