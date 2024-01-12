1e2aa5ccee7aefc6886a885d7bfc7221d19be14a
Tommy Martino <tmartino@google.com>
2024-01-12 16:01:06 +0000

    [iOS] Autofill Across iframes: Set/get parent on drivers
    This implements the GetDriver method of the AutofillDriverIOS, one of
    the missing methods needed to enable filling across frames.
    
    Because frames on iOS aren't directly aware of their place in the tree,
    this implementation relies on the same events as remote token
    registration. When a parent frame first dispatches a remote token to a
    child frame, it now also notifies the registrar of the new token and
    provides a callback. When the child frame registers itself using that
    new remote token, the callback is run. This allows the child->parent
    relationship to be established in the browser.
    
    Bug: 1441921
    Change-Id: I54fd0ba102e3314daa992f136dd9ba6d325f18c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5170847
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1246470}

--- components/autofill/ios/form_util/child_frame_registrar.mm
+++ components/autofill/ios/form_util/child_frame_registrar.mm
@@ -25,6 +25,12 @@ void ChildFrameRegistrar::RegisterMapping(RemoteFrameToken remote,
                                           LocalFrameToken local) {
   // TODO(crbug.com/1440471): Handle double registration
   lookup_map_[remote] = local;
+
+  // Check if we're waiting for this token and run the pending callback, if any.
+  auto pending = pending_callbacks_.extract(remote);
+  if (pending) {
+    std::move(pending.mapped()).Run(local);
+  }
 }
 
 void ChildFrameRegistrar::ProcessRegistrationMessage(base::Value* message) {
@@ -49,6 +55,22 @@ void ChildFrameRegistrar::ProcessRegistrationMessage(base::Value* message) {
   }
 
   RegisterMapping(RemoteFrameToken(*remote), LocalFrameToken(*local));
+}
+
+void ChildFrameRegistrar::DeclareNewRemoteToken(
+    RemoteFrameToken remote,
+    base::OnceCallback<void(LocalFrameToken)> callback) {
+  std::optional<LocalFrameToken> child_token = LookupChildFrame(remote);
+
+  // If the child frame has already registered itself, we can set the parent
+  // directly.
+  if (child_token) {
+    std::move(callback).Run(*child_token);
+    return;
+  }
+
+  // Otherwise, store the relationship for later.
+  pending_callbacks_[remote] = std::move(callback);
 }
 
 ChildFrameRegistrar* ChildFrameRegistrar::GetOrCreateForWebState(
