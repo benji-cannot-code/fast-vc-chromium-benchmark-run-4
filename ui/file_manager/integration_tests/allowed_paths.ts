8cfa1d8e067514df2391e91fb865cfe5e68b60cd
cassycc <cassycc@google.com>
2024-01-12 04:09:27 +0000

    Files app: Convert allowed_paths to TS
    Bug: b/289003444
    Change-Id: Idbff5f4f017f96b2965f80dc0805430f8863190b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5149314
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1246236}

--- ui/file_manager/integration_tests/allowed_paths.ts
+++ ui/file_manager/integration_tests/allowed_paths.ts
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * Paths that can be handled by the dialog opener in native code.
  * @enum {string}
- * @const
  */
 export const AllowedPaths = {
   NATIVE_PATH: 'nativePath',
--- ui/file_manager/integration_tests/file_manager/choose_entry.ts
+++ ui/file_manager/integration_tests/file_manager/choose_entry.ts
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/**
+ * @fileoverview
+ * @suppress {checkTypes}
+ */
+
 import {CHOOSE_ENTRY_PROPERTY} from './choose_entry_const.js';
 
 /**
