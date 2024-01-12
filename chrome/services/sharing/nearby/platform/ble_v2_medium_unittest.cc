b6c78b6a6702c2b336cdcd2ef8a1321eda31f32b
Daniel Classon <dclasson@google.com>
2024-01-12 19:48:53 +0000

    [Nearby Presence] Add Nearby features file, extended advertising flag
    Creates a new file specifically for flags to be used on the Nearby
    platform. We plan to add several new flags to incrementally test
    combinations of device support for our BLE V2 features, including the
    GATT Server, GATT Client, extended advertising, extended scanning, etc.
    
    More info at go/cros-nearby-blev2-testing
    
    This file adds in the Extended Advertising flag, which will cause the
    IsExtendedAdvertisements() API to return false when disabled.
    
    the state of the feature in /etc/chrome_dev.conf.
    
    Test: Verified with print statements that the flag value reflects
    Bug: b/316395769
    Change-Id: I104b682f060676abf088d58cb59daa27048092eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5181924
    Reviewed-by: Jack Shira <jackshira@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Daniel Classon <dclasson@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1246612}

--- chrome/services/sharing/nearby/platform/ble_v2_medium_unittest.cc
+++ chrome/services/sharing/nearby/platform/ble_v2_medium_unittest.cc
@@ -3,11 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/services/sharing/nearby/platform/ble_v2_medium.h"
+
 #include <memory>
 
 #include "base/memory/raw_ptr.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
-#include "chrome/services/sharing/nearby/platform/ble_v2_medium.h"
+#include "chrome/services/sharing/nearby/common/nearby_features.h"
 #include "chrome/services/sharing/nearby/platform/count_down_latch.h"
 #include "chrome/services/sharing/nearby/test_support/fake_adapter.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -241,6 +244,27 @@ TEST_F(BleV2MediumTest, TestScanning_IgnoreIrrelevantAdvertisement) {
 
   EXPECT_TRUE(scanning_session->stop_scanning().ok());
   run_loop.Run();
+}
+
+TEST_F(BleV2MediumTest, IsExtendedAdvertisementsAvailable_FlagDisabled) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitWithFeatures(
+      /*enabled_features=*/{},
+      /*disabled_features=*/{
+          ::features::kEnableNearbyBleV2ExtendedAdvertising});
+
+  EXPECT_FALSE(ble_v2_medium_->IsExtendedAdvertisementsAvailable());
+}
+
+TEST_F(BleV2MediumTest, IsExtendedAdvertisementsAvailable_FlagEnabled) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitWithFeatures(
+      /*enabled_features=*/{::features::kEnableNearbyBleV2ExtendedAdvertising},
+      /*disabled_features=*/{});
+
+  // Once we check for hardware support, we should enforce that both the
+  // feature flag AND hardware support need to be true for this to be true.
+  EXPECT_TRUE(ble_v2_medium_->IsExtendedAdvertisementsAvailable());
 }
 
 }  // namespace nearby::chrome
