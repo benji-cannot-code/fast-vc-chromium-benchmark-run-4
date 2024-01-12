cff7c35252e6eddd8957a2e9900ac502719d0bd4
Alex Gough <ajgo@chromium.org>
2024-01-12 19:19:16 +0000

    Log additional data when winstation creation fails
    Winstation failures spike with gle=8 around releases. Store
    a bit more data to ensure we can easily see the sandbox
    result and last error in dumps.
    
    Bug: 1396219
    Change-Id: I1d5fc30432852062b609ed489252d42144015b31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5190923
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1246597}

--- sandbox/policy/sandbox.cc
+++ sandbox/policy/sandbox.cc
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // BUILDFLAG(IS_MAC)
 
 #if BUILDFLAG(IS_WIN)
+#include "base/debug/alias.h"
+#include "base/notreached.h"
 #include "base/process/process_info.h"
 #include "sandbox/policy/win/sandbox_win.h"
 #include "sandbox/win/src/sandbox.h"
@@ -60,7 +62,13 @@ bool Sandbox::Initialize(sandbox::mojom::Sandbox sandbox_type,
       // will be broken. This has to run before threads and windows are created.
       ResultCode result = broker_services->CreateAlternateDesktop(
           Desktop::kAlternateWinstation);
-      CHECK(result == SBOX_ALL_OK);
+      if (result != SBOX_ALL_OK) {
+        // TODO(crbug.com/1396219) Gather some extra data when this fails.
+        DWORD gle = GetLastError();
+        base::debug::Alias(&result);
+        base::debug::Alias(&gle);
+        NOTREACHED_NORETURN();
+      }
     }
     return true;
   }
