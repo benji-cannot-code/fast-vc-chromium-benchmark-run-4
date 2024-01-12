462b968b6bb7a69bbca052bb9b658e015e9d74eb
Wes Okuhara <wesokuhara@google.com>
2024-01-12 03:24:40 +0000

    CrOS Settings: Convert search_engine_test.js to TS
    Additionally updates the test coverage to handle the existing UI and
    removes obsolete code in the element.
    
    Bug: b:270728282
    Test: browser_tests --gtest_filter="OSSetings*SearchEngine*"
    Change-Id: I2075bf35813d20e881906abf53ee06823c7d8d2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5185282
    Reviewed-by: Nikki Moteva <moteva@google.com>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1246200}

---
+++ chrome/test/data/webui/settings/chromeos/os_search_page/test_search_engines_browser_proxy.ts
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {SearchEnginesBrowserProxy, SearchEnginesInfo} from 'chrome://os-settings/os_settings.js';
+import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
+
+export class TestSearchEnginesBrowserProxy extends TestBrowserProxy implements
+    SearchEnginesBrowserProxy {
+  constructor(private searchEngineInfo: SearchEnginesInfo) {
+    super([
+      'setDefaultSearchEngine',
+      'getSearchEnginesList',
+      'openBrowserSearchSettings',
+    ]);
+  }
+
+  setDefaultSearchEngine(modelIndex: number): void {
+    this.methodCalled('setDefaultSearchEngine', modelIndex);
+  }
+
+  getSearchEnginesList(): Promise<SearchEnginesInfo> {
+    this.methodCalled('getSearchEnginesList');
+    return Promise.resolve(this.searchEngineInfo);
+  }
+
+  openBrowserSearchSettings(): void {
+    this.methodCalled('openBrowserSearchSettings');
+  }
+}
