8c31d7e6db20a8c68fbf3f007edd838922d9670e
Helmut Januschka <helmut@januschka.com>
2024-01-12 09:23:30 +0000

    components: Refactor Quit.*Deprecated() RunLoop usage
    Bug: 922118
    Change-Id: I5e806b2c16dc0cb0ab8d508b9623586683d16113
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5173316
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1246342}

--- components/dom_distiller/standalone/content_extractor_browsertest.cc
+++ components/dom_distiller/standalone/content_extractor_browsertest.cc
@@ -332,7 +332,8 @@ class ContentExtractor : public ContentBrowserTest {
  protected:
   // Creates the DomDistillerService and creates and starts the extraction
   // request.
-  void Start() {
+  void Start(base::OnceClosure quit_closure) {
+    quit_closure_ = std::move(quit_closure);
     const base::CommandLine& command_line =
         *base::CommandLine::ForCurrentProcess();
     FileToUrlMap file_to_url_map;
@@ -411,7 +412,7 @@ class ContentExtractor : public ContentBrowserTest {
     requests_.clear();
     service_.reset();
     base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
-        FROM_HERE, base::RunLoop::QuitCurrentWhenIdleClosureDeprecated());
+        FROM_HERE, std::move(quit_closure_));
   }
 
   size_t pending_tasks_;
@@ -427,12 +428,15 @@ class ContentExtractor : public ContentBrowserTest {
   std::string output_data_;
   std::unique_ptr<google::protobuf::io::StringOutputStream>
       protobuf_output_stream_;
+
+  base::OnceClosure quit_closure_;
 };
 
 IN_PROC_BROWSER_TEST_F(ContentExtractor, MANUAL_ExtractUrl) {
+  base::RunLoop loop;
   SetDistillerJavaScriptWorldId(content::ISOLATED_WORLD_ID_CONTENT_END);
-  Start();
-  base::RunLoop().Run();
+  Start(loop.QuitWhenIdleClosure());
+  loop.Run();
 }
 
 }  // namespace dom_distiller
