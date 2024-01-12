c540c6c9bf258ec86772164db2f3793b4c70993a
Kamal Choudhury <kamalchoudhury@google.com>
2024-01-12 15:05:30 +0000

    Revert^2 "[gbb-cct] Migrating only functions with no C++ dependencies from FeedProcessScopeDependencyProvider to ProcessScopeDependencyProvider."
    This reverts commit cf837412701a94fa95bb9a769aafe47e780446b3.
    
    Reason for revert: Reland now that test breakage has been avoided with https://chrome-internal-review.googlesource.com/c/clank/internal/apps/+/6858334
    
    Original change's description:
    > Revert "[gbb-cct] Migrating only functions with no C++ dependencies from FeedProcessScopeDependencyProvider to ProcessScopeDependencyProvider."
    >
    > This reverts commit 4f962a3c84f6427400afdda408afe5758dd6ce16.
    >
    > Reason for revert: Causing build failures on arm-unpublished-builder-rel. See https://crbug.com/1517285
    >
    > Original change's description:
    > > [gbb-cct] Migrating only functions with no C++ dependencies from FeedProcessScopeDependencyProvider to ProcessScopeDependencyProvider.
    > >
    > > This is done so that non-Feed XSurface clients are not depending on Feed library (as part of Code Refactoring of Chrome Implementation of Page Insights Hub). This CL is only migrating the members not depending on the C++ classes and thus the ProcessScopeDependencyProvider is inherited from the class for now to not break other code.
    > >
    > > For further reference go to: go/chrome-pih-feed-dependency-removal
    > >
    > > Change-Id: Ibfe744572aba75e5eebf018c207cd91448e85c8a
    > > Bug: b/297999162
    > > Low-Coverage-Reason: The tests have been just migrated and no new functions have been added and thus there is no change in test coverage.
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5119740
    > > Reviewed-by: Dan H <harringtond@chromium.org>
    > > Commit-Queue: Kamal Choudhury <kamalchoudhury@google.com>
    > > Reviewed-by: Edmund Wright <edmundw@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1245200}
    >
    > Bug: 1517285
    > Bug: b/297999162
    > Change-Id: I925b5f0b6b661d03fdaf6171f24b386460914b59
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5182657
    > Owners-Override: Ashley Newson <ashleynewson@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Ashley Newson <ashleynewson@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1245237}
    
    Bug: 1517285
    Bug: b/297999162
    Change-Id: I0b53a69489f7df4d977debc636c42752c105a617
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5189628
    Commit-Queue: Kamal Choudhury <kamalchoudhury@google.com>
    Reviewed-by: Edmund Wright <edmundw@google.com>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1246448}

--- chrome/browser/xsurface_provider/android/java/src/org/chromium/chrome/browser/xsurface_provider/ProcessScopeDependencyProviderImplTest.java
+++ chrome/browser/xsurface_provider/android/java/src/org/chromium/chrome/browser/xsurface_provider/ProcessScopeDependencyProviderImplTest.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.feed;
+package org.chromium.chrome.browser.xsurface_provider;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -22,12 +22,12 @@ import org.chromium.chrome.test.util.browser.Features;
 import org.chromium.chrome.test.util.browser.Features.DisableFeatures;
 import org.chromium.chrome.test.util.browser.Features.EnableFeatures;
 
-/** Tests for FeedProcessScopeDependencyProvider. */
+/** Tests for ProcessScopeDependencyProviderImpl. */
 @RunWith(BaseRobolectricTestRunner.class)
-public final class FeedProcessScopeDependencyProviderTest {
+public final class ProcessScopeDependencyProviderImplTest {
     @Rule public TestRule mFeaturesProcessorRule = new Features.JUnitProcessor();
 
-    FeedProcessScopeDependencyProvider mProvider;
+    ProcessScopeDependencyProviderImpl mProvider;
     boolean mMetricsReportingEnabled;
 
     private PrivacyPreferencesManager mStubPrivacyPrefsManager =
@@ -95,7 +95,7 @@ public final class FeedProcessScopeDependencyProviderTest {
 
     @Before
     public void setUp() {
-        mProvider = new FeedProcessScopeDependencyProvider("key", mStubPrivacyPrefsManager);
+        mProvider = new ProcessScopeDependencyProviderImpl("key", mStubPrivacyPrefsManager);
     }
 
     @Test
