899cbf86ef573dbd16146f52e261a721a8577ce1
Artem Sumaneev <asumaneev@google.com>
2024-01-12 13:21:26 +0000

    [enrollment] Cleanup legacy auto-enrollment errors from fetcher.
    Replace legacy errors with new ones in EnrollmentStateFetcher.
    
    Bug: b:309921228
    Test: none
    Change-Id: I30794706a482414920bd4df4c295a8f6a30c2fd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5071092
    Reviewed-by: Roland Bock <rbock@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Artem Sumaneev <asumaneev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1246413}

--- chrome/browser/ash/policy/enrollment/enrollment_state_fetcher.cc
+++ chrome/browser/ash/policy/enrollment/enrollment_state_fetcher.cc
@@ -8,11 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string_view>
 #include <tuple>
+#include <variant>
 
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_switches.h"
 #include "base/check.h"
 #include "base/functional/callback_forward.h"
+#include "base/functional/overloaded.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram_functions.h"
@@ -78,14 +80,36 @@ std::string_view AutoEnrollmentStateToUmaSuffix(AutoEnrollmentState state) {
     }
   }
 
-  const AutoEnrollmentLegacyError error =
-      AutoEnrollmentErrorToLegacyError(state.error());
-  switch (error) {
-    case AutoEnrollmentLegacyError::kConnectionError:
-      return kUMASuffixConnectionError;
-    case AutoEnrollmentLegacyError::kServerError:
-      return kUMASuffixServerError;
-  }
+  // TODO(b/309921228): Add more suffixes.
+  return std::visit(
+      base::Overloaded{
+          [](AutoEnrollmentLegacyError legacy_error) {
+            switch (legacy_error) {
+              case AutoEnrollmentLegacyError::kConnectionError:
+                return kUMASuffixConnectionError;
+              case AutoEnrollmentLegacyError::kServerError:
+                return kUMASuffixServerError;
+            }
+          },
+          [](AutoEnrollmentSafeguardTimeoutError) {
+            return kUMASuffixConnectionError;
+          },
+          [](AutoEnrollmentSystemClockSyncError) {
+            return kUMASuffixConnectionError;
+          },
+          [](const AutoEnrollmentDMServerError& error) {
+            return error.network_error.has_value() ? kUMASuffixConnectionError
+                                                   : kUMASuffixServerError;
+          },
+          [](AutoEnrollmentStateAvailabilityResponseError) {
+            return kUMASuffixServerError;
+          },
+          [](AutoEnrollmentPsmError) { return kUMASuffixServerError; },
+          [](AutoEnrollmentStateRetrievalResponseError) {
+            return kUMASuffixServerError;
+          },
+      },
+      state.error());
 }
 
 // The DeterminationContext is used to store state and cache computed values
