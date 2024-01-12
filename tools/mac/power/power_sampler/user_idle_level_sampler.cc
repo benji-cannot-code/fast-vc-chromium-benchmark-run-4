767d328b786051e6a2704be6cd504eacdf2c151b
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-01-12 08:44:59 +0000

    Reland "Rename {absl => std}::optional in //tools/"
    This is a reland of commit b263d258c9e4899afaa24036bde76d7e9e1d537c
    
    Original change's description:
    > Rename {absl => std}::optional in //tools/
    >
    > Automated patch, intended to be effectively a no-op.
    >
    > Context:
    > https://groups.google.com/a/chromium.org/g/cxx/c/nBD_1LaanTc/m/ghh-ZZhWAwAJ?utm_medium=email&utm_source=footer
    >
    > As of https://crrev.com/1204351, absl::optional is now a type alias for
    > std::optional. We should migrate toward it.
    >
    > Script:
    > ```
    > function replace {
    >   echo "Replacing $1 by $2"
    >   git grep -l "$1" \
    >     | cut -f1 -d: \
    >     | grep \
    >       -e "^tools" \
    >     | sort \
    >     | uniq \
    >     | grep \
    >       -e "\.h" \
    >       -e "\.cc" \
    >       -e "\.mm" \
    >       -e "\.py" \
    >     | xargs sed -i "s/$1/$2/g"
    > }
    > replace "absl::make_optional" "std::make_optional"
    > replace "absl::optional" "std::optional"
    > replace "absl::nullopt" "std::nullopt"
    > replace "absl::in_place" "std::in_place"
    > replace "absl::in_place_t" "std::in_place_t"
    > replace "\"third_party\/abseil-cpp\/absl\/types\/optional.h\"" "<optional>"
    > git cl format
    > ```
    >
    > Bug: chromium:1500249
    > Change-Id: I7536d03fa09fb48f8cf7113eba8eb727d5efba7b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5185809
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1245840}
    
    Bug: chromium:1500249
    Change-Id: I0abd981c73e34bec6ed83eafed20098cf82e4d12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5190270
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1246334}

--- tools/mac/power/power_sampler/user_idle_level_sampler.cc
+++ tools/mac/power/power_sampler/user_idle_level_sampler.cc
@@ -8,15 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/sysctl.h>
 #include <sys/types.h>
 
+#include <optional>
+
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace power_sampler {
 
 namespace {
 
-absl::optional<int> GetIntSysCtl(const std::vector<int>& mib_name) {
+std::optional<int> GetIntSysCtl(const std::vector<int>& mib_name) {
   int value = 0;
   size_t size = sizeof(value);
   int ret = sysctl(const_cast<int*>(mib_name.data()), mib_name.size(), &value,
@@ -30,7 +31,7 @@ absl::optional<int> GetIntSysCtl(const std::vector<int>& mib_name) {
     return value;
   }
 
-  return absl::nullopt;
+  return std::nullopt;
 }
 
 }  // namespace
--- tools/memory/partition_allocator/inspect_utils.h
+++ tools/memory/partition_allocator/inspect_utils.h
@@ -16,9 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/types.h>
 #include <unistd.h>
 
+#include <optional>
+
 #include "base/files/file.h"
 #include "build/build_config.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
 
 #if BUILDFLAG(IS_MAC)
 #include <mach/mach.h>
@@ -81,14 +82,14 @@ class RawBuffer {
   const T* get() const { return reinterpret_cast<const T*>(buffer_); }
   char* get_buffer() { return buffer_; }
 
-  static absl::optional<RawBuffer<T>> ReadFromProcessMemory(
+  static std::optional<RawBuffer<T>> ReadFromProcessMemory(
       RemoteProcessMemoryReader& reader,
       uintptr_t address) {
     RawBuffer<T> buf;
     bool ok = reader.ReadMemory(reinterpret_cast<unsigned long>(address),
                                 sizeof(T), buf.get_buffer());
     if (!ok)
-      return absl::nullopt;
+      return std::nullopt;
 
     return {buf};
   }
