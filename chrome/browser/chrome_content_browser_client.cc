c2b85114644c16229aadff88561b6bb533d644e1
Alex Moshchuk <alexmos@chromium.org>
2024-01-12 05:29:23 +0000

    Dispatch SiteInstanceGotProcess() only when both process and site are set.
    All uses of ContentBrowserClient::SiteInstanceGotProcess() reference
    the SiteInstance's site URL, but in certain cases the site URL may be
    set *after* a SiteInstance's process is set.  This happens when
    creating a SiteInstance that's not for any specific URL, such as for a
    blank tab; if the SiteInstance ends up being used for a subsequent
    navigation, its site will be assigned at response time, but the
    process will be created earlier.  This behavior is undesirable since
    the callers will not get another signal that both the site URL and
    process are available, and the current logic in
    SiteInstanceGotProcess() (such as adding extensions to ProcessMap)
    won't work correctly with an empty site URL.  This isn't an issue for
    extensions today, since we'll end up throwing away the unassigned
    SiteInstance and its process and create a new SiteInstance and process
    at response time due to a mismatch in RenderProcessHostPrivilege, but
    longer term we'd like to both remove those privilege buckets and allow
    the unassigned SiteInstance and its initial process to be reused by
    navigations to extensions, so this CL is a prerequisite for that.
    
    This CL fixes the issue by dispatching SiteInstanceGotProcess() only
    when the process and the site have both been assigned for a particular
    SiteInstance, which is what its use cases really want.  It also
    renames the function to SiteInstanceGotProcessAndSite() to better
    convey the semantics and removes an unused override in Cast code.
    
    Bug: 1506082, 1485586
    Change-Id: Id13593d4f1056d31f9a626e7ffbac30e59e45f28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5142354
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1246267}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -2259,7 +2259,7 @@ bool ChromeContentBrowserClient::ShouldEmbeddedFramesTryToReuseExistingProcess(
 #endif
 }
 
-void ChromeContentBrowserClient::SiteInstanceGotProcess(
+void ChromeContentBrowserClient::SiteInstanceGotProcessAndSite(
     SiteInstance* site_instance) {
   CHECK(site_instance->HasProcess());
 
@@ -2281,7 +2281,7 @@ void ChromeContentBrowserClient::SiteInstanceGotProcess(
 #endif
 
   for (auto& part : extra_parts_) {
-    part->SiteInstanceGotProcess(site_instance);
+    part->SiteInstanceGotProcessAndSite(site_instance);
   }
 }
 
