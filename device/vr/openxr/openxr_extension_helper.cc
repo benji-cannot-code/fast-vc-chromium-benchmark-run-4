e9a0beb8707a5b99d6a962f9aadb1b4192d2f94f
Alexander Cooper <alcooper@chromium.org>
2024-01-12 23:21:24 +0000

    Fix IsFeatureSupported for HandInput
    Bug: 1435548
    Change-Id: Id075543bf88e3dd8b8905889e9cd122e90ab4e4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5193384
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1246729}

--- device/vr/openxr/openxr_extension_helper.cc
+++ device/vr/openxr/openxr_extension_helper.cc
@@ -16,6 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/vr/openxr/openxr_scene_understanding_manager.h"
 #include "device/vr/openxr/openxr_stage_bounds_provider_basic.h"
 #include "device/vr/public/mojom/xr_session.mojom.h"
+// Included on all platforms so that we can check the extension names, even
+// if they may not be supported there.
+#include "third_party/openxr/dev/xr_android.h"
 
 #if BUILDFLAG(IS_ANDROID)
 #include "device/vr/openxr/android/openxr_hand_tracker_android.h"
@@ -182,7 +185,8 @@ bool OpenXrExtensionHelper::IsFeatureSupported(
       return IsExtensionSupported(XR_EXT_HAND_TRACKING_EXTENSION_NAME) &&
              (IsExtensionSupported(XR_EXT_HAND_INTERACTION_EXTENSION_NAME) ||
               IsExtensionSupported(XR_MSFT_HAND_INTERACTION_EXTENSION_NAME) ||
-              IsExtensionSupported(XR_FB_HAND_TRACKING_AIM_EXTENSION_NAME));
+              IsExtensionSupported(XR_FB_HAND_TRACKING_AIM_EXTENSION_NAME) ||
+              IsExtensionSupported(XR_ANDROID_HAND_GESTURE_EXTENSION_NAME));
     case device::mojom::XRSessionFeature::HIT_TEST:
       return IsExtensionSupported(XR_MSFT_SCENE_UNDERSTANDING_EXTENSION_NAME);
     case device::mojom::XRSessionFeature::SECONDARY_VIEWS:
