15733f7530a7f105fc9528354b4d6b59691c9c1f
Feras Aldahlawi <feras@chromium.org>
2024-01-13 02:25:45 +0000

    Move KeySystemsImpl class to its own file
    This is a refactoring CL to follow the common pattern of separating
    an interface from it's implementation.
    
    Bug: b/267698934
    Change-Id: I9768a283f5dcf3e53de009782a2740c7f4c75b35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5173626
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Feras Aldahlawi <feras@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1246800}

--- media/base/key_systems.h
+++ media/base/key_systems.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/decrypt_config.h"
 #include "media/base/eme_constants.h"
 #include "media/base/media_export.h"
-#include "media/media_buildflags.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace media {
@@ -29,6 +28,8 @@ namespace media {
 // |key_system| to every method. http://crbug.com/457438
 class MEDIA_EXPORT KeySystems {
  public:
+  // TODO(b/267698934): Remove this API when migrating KeySystems to be a
+  // per-frame instance.
   // Returns the KeySystems singleton which may or may not be updated yet.
   static KeySystems* GetInstance();
 
@@ -95,12 +96,10 @@ class MEDIA_EXPORT KeySystems {
       const std::string& key_system) const = 0;
 
  protected:
-  virtual ~KeySystems() {}
+  virtual ~KeySystems() = default;
 };
 
-// TODO(ddorwin): WebContentDecryptionModuleSessionImpl::initializeNewSession()
-// is violating this rule! https://crbug.com/249976.
-// Use for prefixed EME only!
+// Returns whether a key systems is supported with init data type.
 MEDIA_EXPORT bool IsSupportedKeySystemWithInitDataType(
     const std::string& key_system,
     EmeInitDataType init_data_type);
