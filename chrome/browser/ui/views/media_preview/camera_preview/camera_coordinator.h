99b550d8e276074b326da5202ed3789ebde83dde
Bryant Chandler <bryantchandler@chromium.org>
2024-01-13 00:27:21 +0000

    [media_preview] Switch to SimpleComboboxModel instead of custom models
    This also changes to use the new is_system_default field on
    media::AudioDeviceDescription.
    
    Change-Id: I4eb1b3f2024636bacf6fb9d5244580f36abf4e7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5190230
    Reviewed-by: Ahmed Moussa <ahmedmoussa@google.com>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1246766}

--- chrome/browser/ui/views/media_preview/camera_preview/camera_coordinator.h
+++ chrome/browser/ui/views/media_preview/camera_preview/camera_coordinator.h
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "chrome/browser/ui/views/media_preview/camera_preview/camera_mediator.h"
-#include "chrome/browser/ui/views/media_preview/camera_preview/camera_selector_combobox_model.h"
 #include "chrome/browser/ui/views/media_preview/camera_preview/camera_view_controller.h"
 #include "chrome/browser/ui/views/media_preview/camera_preview/video_stream_coordinator.h"
+#include "ui/base/models/simple_combobox_model.h"
 #include "ui/views/view_tracker.h"
 
 // Acts as a middle man between the ViewController and the Mediator.
@@ -30,7 +30,7 @@ class CameraCoordinator {
   // Invoked from the ViewController when a combobox selection has been made.
   void OnVideoSourceChanged(std::optional<size_t> selected_index);
 
-  const CameraSelectorComboboxModel& GetComboboxModelForTest() const {
+  const ui::SimpleComboboxModel& GetComboboxModelForTest() const {
     return combobox_model_;
   }
 
@@ -45,9 +45,12 @@ class CameraCoordinator {
 
   CameraMediator camera_mediator_;
   views::ViewTracker camera_view_tracker_;
-  CameraSelectorComboboxModel combobox_model_;
+  ui::SimpleComboboxModel combobox_model_;
   std::string active_device_id_;
   base::flat_set<std::string> eligible_camera_ids_;
+  // This list must be kept in sync with the `combobox_model_` so that indices
+  // align.
+  std::vector<media::VideoCaptureDeviceInfo> eligible_device_infos_;
   std::optional<CameraViewController> camera_view_controller_;
   std::optional<VideoStreamCoordinator> video_stream_coordinator_;
 };
--- chrome/browser/ui/views/media_preview/mic_preview/mic_coordinator.h
+++ chrome/browser/ui/views/media_preview/mic_preview/mic_coordinator.h
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/media_preview/mic_preview/audio_stream_coordinator.h"
 #include "chrome/browser/ui/views/media_preview/mic_preview/mic_mediator.h"
-#include "chrome/browser/ui/views/media_preview/mic_preview/mic_selector_combobox_model.h"
 #include "chrome/browser/ui/views/media_preview/mic_preview/mic_view_controller.h"
+#include "ui/base/models/simple_combobox_model.h"
 #include "ui/views/view_tracker.h"
 
 namespace media {
@@ -35,7 +35,7 @@ class MicCoordinator {
   // Invoked from the ViewController when a combobox selection has been made.
   void OnAudioSourceChanged(std::optional<size_t> selected_index);
 
-  const MicSelectorComboboxModel& GetComboboxModelForTest() const {
+  const ui::SimpleComboboxModel& GetComboboxModelForTest() const {
     return combobox_model_;
   }
 
@@ -54,9 +54,12 @@ class MicCoordinator {
 
   MicMediator mic_mediator_;
   views::ViewTracker mic_view_tracker_;
-  MicSelectorComboboxModel combobox_model_;
+  ui::SimpleComboboxModel combobox_model_;
   std::string active_device_id_;
   base::flat_set<std::string> eligible_mic_ids_;
+  // This list must be kept in sync with the `combobox_model_` so that indices
+  // align.
+  std::vector<media::AudioDeviceDescription> eligible_device_infos_;
   std::optional<MicViewController> mic_view_controller_;
   std::optional<AudioStreamCoordinator> audio_stream_coordinator_;
 };
