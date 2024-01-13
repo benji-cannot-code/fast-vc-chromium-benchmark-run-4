7ab75725aa34b28bb63eee52b363cab422df45ba
Adam Langley <agl@chromium.org>
2024-01-13 00:41:22 +0000

    webauthn: add initial dialog window code.
    This isn't complete, but I have to make cuts in order to make
    reasonably-sized CLs. Currently this code can show MagicArch in a window
    but the UI state is unreachable save for the browser test.
    
    Bug: 1459620
    Change-Id: Iad9bef649d20d2affd599157bee4f7cc31e95d39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5187328
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1246770}

---
+++ chrome/browser/ui/webauthn/authenticator_request_window.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_WEBAUTHN_AUTHENTICATOR_REQUEST_WINDOW_H_
+#define CHROME_BROWSER_UI_WEBAUTHN_AUTHENTICATOR_REQUEST_WINDOW_H_
+
+namespace content {
+class WebContents;
+}
+
+class AuthenticatorRequestDialogModel;
+
+// Open a dialog window to render a step of the WebAuthn UI.
+void ShowAuthenticatorRequestWindow(AuthenticatorRequestDialogModel* model);
+
+#endif  // CHROME_BROWSER_UI_WEBAUTHN_AUTHENTICATOR_REQUEST_WINDOW_H_
