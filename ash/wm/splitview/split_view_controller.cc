6ad61d50ae6bc98fa1d5eda2243e1bae1f58478e
Michele Fan <michelefan@chromium.org>
2024-01-13 01:36:16 +0000

    snap-group: Fix crashes when divider is animating or being dragged
    This cl fixed two crashes: due to the unsuccessful `EndOverview()`
    when divider is animating or being resized().
    
          http://b/315549001#comment3
    
    Demo: http://b/313517079#comment14,
    Test: Added unit tests, manual + existing
    Fixed: b/313517079, b/315549001
    Change-Id: I7126210f2ee2ba0d06a5c84b95e3fed16a3ceabf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5107058
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1246795}

--- ash/wm/splitview/split_view_controller.cc
+++ ash/wm/splitview/split_view_controller.cc
@@ -623,7 +623,6 @@ void SplitViewController::SnapWindow(aura::Window* window,
                                      float snap_ratio) {
   DCHECK(window && CanSnapWindow(window, snap_ratio));
   DCHECK_NE(snap_position, SnapPosition::kNone);
-  DCHECK(!IsResizingWithDivider());
   if (IsDividerAnimating()) {
     StopSnapAnimation();
   }
