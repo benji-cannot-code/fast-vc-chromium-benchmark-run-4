601db55e1c5ecf72eb6906860e6effbb46ad7a2b
Darren Shen <shend@chromium.org>
2024-01-15 02:45:30 +0000

    Add flags for enabling Picker on ChromeOS.
    Bug: b/318575769
    Change-Id: Id725c2a7ba944b7c6d9edb8bf7d2e1797440a6ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5183016
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Auto-Submit: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1246995}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3232,6 +3232,12 @@ extern const char kArcExternalStorageAccessDescription[];
 extern const char kAshEnableUnifiedDesktopName[];
 extern const char kAshEnableUnifiedDesktopDescription[];
 
+extern const char kAshPickerName[];
+extern const char kAshPickerDescription[];
+
+extern const char kAshPickerFeatureKeyName[];
+extern const char kAshPickerFeatureKeyDescription[];
+
 extern const char kAsynchronousScannerDiscoveryName[];
 extern const char kAsynchronousScannerDiscoveryDescription[];
 
