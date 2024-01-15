0efd7d57aa5f47f36e32544cc9526002101aab13
Nicolas Dossou-Gbete <dgn@chromium.org>
2024-01-15 16:06:40 +0000

    🧇 Add histograms to track SearchProviderOverride usage
    Adds the Search.SearchProviderOverrideStatus histogram
    
    Bug: 1517316
    Bug: b:302675777
    Change-Id: I55281aa1e520f63c16907891c95fb8a35fb983c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5181077
    Auto-Submit: Nicolas Dossou-Gbété <dgn@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247193}

--- components/search_engines/template_url_prepopulate_data.cc
+++ components/search_engines/template_url_prepopulate_data.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/feature_list.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/rand_util.h"
 #include "base/ranges/algorithm.h"
 #include "build/build_config.h"
@@ -1729,6 +1730,23 @@ std::vector<std::unique_ptr<TemplateURLData>> GetPrepopulatedTemplateURLData(
   return t_urls;
 }
 
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class SearchProviderOverrideStatus {
+  // No preferences are available for `prefs::kSearchProviderOverrides`.
+  kNoPref = 0,
+
+  // The preferences for `prefs::kSearchProviderOverrides` do not contain valid
+  // template URLs.
+  kEmptyPref = 1,
+
+  // The preferences for `prefs::kSearchProviderOverrides` contain valid
+  // template URL(s).
+  kPrefHasValidUrls = 2,
+
+  kMaxValue = kPrefHasValidUrls
+};
+
 std::vector<std::unique_ptr<TemplateURLData>> GetOverriddenTemplateURLData(
     PrefService* prefs) {
   std::vector<std::unique_ptr<TemplateURLData>> t_urls;
@@ -1746,6 +1764,14 @@ std::vector<std::unique_ptr<TemplateURLData>> GetOverriddenTemplateURLData(
       }
     }
   }
+
+  base::UmaHistogramEnumeration(
+      "Search.SearchProviderOverrideStatus",
+      !t_urls.empty() ? SearchProviderOverrideStatus::kPrefHasValidUrls
+                      : (prefs->HasPrefPath(prefs::kSearchProviderOverrides)
+                             ? SearchProviderOverrideStatus::kEmptyPref
+                             : SearchProviderOverrideStatus::kNoPref));
+
   return t_urls;
 }
 
