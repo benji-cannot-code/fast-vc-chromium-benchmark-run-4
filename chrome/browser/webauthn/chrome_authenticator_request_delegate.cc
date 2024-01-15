1b6bdeaadcd840aab661a364f9f1ddd63be413ce
Ken Buchanan <kenrb@google.com>
2024-01-15 20:24:28 +0000

    [WebAuthn] Remove kWebAuthnRequireUIForComplexDiscoveries flag
    This has been enabled by default since M118.
    
    Bug: N/A
    Change-Id: Icf27315828383d4c9c5ff14cd5299ac1b21ec2d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5190773
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247282}

--- chrome/browser/webauthn/chrome_authenticator_request_delegate.cc
+++ chrome/browser/webauthn/chrome_authenticator_request_delegate.cc
@@ -728,9 +728,7 @@ void ChromeAuthenticatorRequestDelegate::ConfigureDiscoveries(
 
   // Without the UI enabled, discoveries like caBLE, Android AOA, iCloud
   // keychain, and the enclave, don't make sense.
-  if (base::FeatureList::IsEnabled(
-          device::kWebAuthnRequireUIForComplexDiscoveries) &&
-      disable_ui_) {
+  if (disable_ui_) {
     return;
   }
 
