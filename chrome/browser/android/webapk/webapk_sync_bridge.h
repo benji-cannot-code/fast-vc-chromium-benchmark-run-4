00b3137ac38d40c667843817496ecc78a1468bde
Finnur Thorarinsson <finnur@chromium.org>
2024-01-15 16:30:24 +0000

    [PwaRestore]: Remove hard-coded values for WebApkDatabase.
    This removes the hard-coded values that the WebApkDatabase
    was returning to all clients. The integration test now
    supplies its own data for verification.
    
    Bug: 1481570
    Low-Coverage-Reason: There is an end-to-end integration test in place. It doesn't make sense that all the coverage numbers be zero.
    Change-Id: Icc5c4da357c85b38626f8f30592df7650658d48b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5178669
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247206}

--- chrome/browser/android/webapk/webapk_sync_bridge.h
+++ chrome/browser/android/webapk/webapk_sync_bridge.h
@@ -69,6 +69,11 @@ class WebApkSyncBridge : public syncer::ModelTypeSyncBridge {
   std::string GetClientTag(const syncer::EntityData& entity_data) override;
   std::string GetStorageKey(const syncer::EntityData& entity_data) override;
 
+  void RegisterDoneInitializingCallback(
+      base::OnceCallback<void(bool)> init_done_callback);
+  void MergeSyncDataForTesting(
+      std::vector<std::vector<std::string>> app_vector);
+
   // internal helpers, exposed for testing.
   bool AppWasUsedRecently(const sync_pb::WebApkSpecifics* specifics) const;
 
@@ -171,6 +176,7 @@ class WebApkSyncBridge : public syncer::ModelTypeSyncBridge {
   Registry registry_;
   std::unique_ptr<base::Clock> clock_;
   std::unique_ptr<AbstractWebApkSpecificsFetcher> webapk_specifics_fetcher_;
+  base::OnceCallback<void(bool)> init_done_callback_;
 
   base::WeakPtrFactory<WebApkSyncBridge> weak_ptr_factory_{this};
 };
