8d8bff610da700f6db98e9b8c7268f24c927eee8
Adam Rice <ricea@chromium.org>
2024-01-15 05:20:26 +0000

    iwyu: proxy_chain.cc: Add algorithm.h include
    net/base/proxy_chain.cc uses base::ranges::all_of() so explicitly
    include "base/ranges/algorithm.h".
    
    Also replace an <iostream> include with a hopefully cheaper <iosfwd>
    include in the header file proxy_chain.h.
    
    Change-Id: If9e8c45c4b29ddbc557bd893fb7e2f8041679388
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5192309
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Ciara McMullin <ciaramcmullin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1247011}

--- net/base/proxy_chain.h
+++ net/base/proxy_chain.h
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <iosfwd>
 #include <optional>
-#include <ostream>
 #include <string>
 #include <string_view>
 #include <tuple>
