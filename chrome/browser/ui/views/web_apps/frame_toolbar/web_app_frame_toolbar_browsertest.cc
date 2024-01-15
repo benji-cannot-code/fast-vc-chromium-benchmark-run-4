47e7942dff33a4e7e8fdaa6259e5d4178e606d76
Sonja <laurila@google.com>
2024-01-15 12:06:41 +0000

    Reland "borderless: Loosen minimum size for window.open / window.resizeTo|By"
    This is a reland of commit 4da654e4c414ef33349f3d7efe01e6ccdc6fce30
    
    Original change's description:
    > borderless: Loosen minimum size for window.open / window.resizeTo|By
    >
    > - Introduces a new constant for specific for borderless apps so that
    > borderless apps can create smaller windows (at smallest 18x18px).
    > - Moves minimum-size adjusting for window.open calls to chrome/
    > instead of blink if the opener_frame is borderless and navigation policy
    > is a popup.
    > - Uses borderless-specific minimum-size for window.resizeTo|By in
    > blink, if they are called in an app with borderless display-mode. The
    > correct permission is ensured in chrome/ side.
    >
    > Note: When window.open is called, display-mode for the new frame is
    > still undefined as the app hasn't loaded yet and thus using the opener
    > frame.
    >
    > Bug: b/307182741, b/307160156
    > Change-Id: Id8b37e930d0aaa4293629a82c48714c3049ae7cc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4999178
    > Commit-Queue: Sonja Laurila <laurila@google.com>
    > Reviewed-by: Mike Wasserman <msw@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1242003}
    
    Bug: b/307182741, b/307160156
    Change-Id: I39b6fc72efd359d7f1315352e4f2a1e6e16409f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5177189
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Sonja Laurila <laurila@google.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247109}

--- chrome/browser/ui/views/web_apps/frame_toolbar/web_app_frame_toolbar_browsertest.cc
+++ chrome/browser/ui/views/web_apps/frame_toolbar/web_app_frame_toolbar_browsertest.cc
@@ -85,6 +85,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/test/test_extension_dir.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "third_party/blink/public/common/features.h"
+#include "third_party/blink/public/common/widget/constants.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/hit_test.h"
 #include "ui/compositor/scoped_animation_duration_scale_mode.h"
@@ -700,7 +701,9 @@ IN_PROC_BROWSER_TEST_F(BorderlessIsolatedWebAppBrowserTest,
   EXPECT_FALSE(popup_browser_view->IsBorderlessModeEnabled());
 }
 
-IN_PROC_BROWSER_TEST_F(BorderlessIsolatedWebAppBrowserTest, PopupSize) {
+IN_PROC_BROWSER_TEST_F(
+    BorderlessIsolatedWebAppBrowserTest,
+    PopupSize_CanSubceedMinimumWindowSize_And_InnerAndOuterSizesAreCorrect) {
   InstallAndLaunchIsolatedWebApp(/*uses_borderless=*/true);
   GrantWindowManagementPermission();
   ASSERT_TRUE(browser_view()->IsBorderlessModeEnabled());
@@ -709,10 +712,15 @@ IN_PROC_BROWSER_TEST_F(BorderlessIsolatedWebAppBrowserTest, PopupSize) {
       EvalJs(browser_view()->GetActiveWebContents(), "window.location.href")
           .ExtractString();
 
-  BrowserView* popup_browser_view =
-      OpenPopup("window.open('" + url +
-                "', '', 'location=0, status=0, scrollbars=0, "
-                "left=0, top=0, width=400, height=300');");
+  // width and height set should be less than `blink::kMinimumWindowSize` to
+  // ensure that for borderless apps, it's possible to subceed the limit.
+  const std::string kWindowOpenScript = base::StrCat(
+      {"window.open('", url,
+       "', '', 'location=0, status=0, scrollbars=0, left=0, top=0, width=",
+       base::NumberToString(blink::kMinimumBorderlessWindowSize), ", height=",
+       base::NumberToString(blink::kMinimumBorderlessWindowSize), "');"});
+  BrowserView* popup_browser_view = OpenPopup(kWindowOpenScript);
+
   EXPECT_TRUE(popup_browser_view->IsBorderlessModeEnabled());
   auto* popup_web_contents = popup_browser_view->GetActiveWebContents();
 
@@ -722,8 +730,8 @@ IN_PROC_BROWSER_TEST_F(BorderlessIsolatedWebAppBrowserTest, PopupSize) {
                                            kBorderlessAppOnloadTitle);
   EXPECT_EQ(init_title_watcher.WaitAndGetTitle(), kBorderlessAppOnloadTitle);
 
-  constexpr int kExpectedWidth = 400, kExpectedHeight = 300;
-  gfx::Size expected_size(kExpectedWidth, kExpectedHeight);
+  gfx::Size expected_size(blink::kMinimumBorderlessWindowSize,
+                          blink::kMinimumBorderlessWindowSize);
 
 // For ChromeOS the resizable borders are "outside of the window" where as for
 // Linux they are "inside of the window".
@@ -734,14 +742,17 @@ IN_PROC_BROWSER_TEST_F(BorderlessIsolatedWebAppBrowserTest, PopupSize) {
   constexpr int kFrameInsets =
       2 * OpaqueBrowserFrameViewLayout::kFrameBorderThickness;
   // window.open() sets the inner size to match with the given size.
-  gfx::Size expected_outer_size(kExpectedWidth + kFrameInsets,
-                                kExpectedHeight + kFrameInsets);
+  gfx::Size expected_outer_size(
+      blink::kMinimumBorderlessWindowSize + kFrameInsets,
+      blink::kMinimumBorderlessWindowSize + kFrameInsets);
   WaitForWindowSizeCorrectlyUpdated(popup_browser_view, expected_size,
                                     expected_outer_size);
 #endif
 }
 
-IN_PROC_BROWSER_TEST_F(BorderlessIsolatedWebAppBrowserTest, PopupResize) {
+IN_PROC_BROWSER_TEST_F(
+    BorderlessIsolatedWebAppBrowserTest,
+    PopupResize_CanSubceedMinimumWindowSize_And_InnerAndOuterSizesAreCorrect) {
   InstallAndLaunchIsolatedWebApp(/*uses_borderless=*/true);
   GrantWindowManagementPermission();
   ASSERT_TRUE(browser_view()->IsBorderlessModeEnabled());
@@ -776,12 +787,20 @@ IN_PROC_BROWSER_TEST_F(BorderlessIsolatedWebAppBrowserTest, PopupResize) {
                                                          kResizeTitle);
 
   EXPECT_TRUE(ExecJs(popup_web_contents, kOnResizeScript));
-  EXPECT_TRUE(ExecJs(popup_web_contents, "window.resizeTo(600,500)"));
+
+  // width and height set should be less than `blink::kMinimumWindowSize` to
+  // ensure that for borderless apps, it's possible to subceed the limit.
+  const std::string kResizeToScript = content::JsReplace(
+      R"(
+    window.resizeTo($1,$1)
+  )",
+      base::NumberToString(blink::kMinimumBorderlessWindowSize));
+  EXPECT_TRUE(ExecJs(popup_web_contents, kResizeToScript));
   std::ignore = resized_title_watcher.WaitAndGetTitle();
   EXPECT_EQ(popup_web_contents->GetTitle(), kResizeTitle);
 
-  constexpr int kExpectedWidth = 600, kExpectedHeight = 500;
-  gfx::Size expected_size(kExpectedWidth, kExpectedHeight);
+  gfx::Size expected_size(blink::kMinimumBorderlessWindowSize,
+                          blink::kMinimumBorderlessWindowSize);
 
 #if BUILDFLAG(IS_CHROMEOS)
   WaitForWindowSizeCorrectlyUpdated(popup_browser_view, expected_size,
@@ -790,8 +809,9 @@ IN_PROC_BROWSER_TEST_F(BorderlessIsolatedWebAppBrowserTest, PopupResize) {
   constexpr int kFrameInsets =
       2 * OpaqueBrowserFrameViewLayout::kFrameBorderThickness;
   // window.resizeTo() sets the outer size to match with the given size.
-  gfx::Size expected_inner_size(kExpectedWidth - kFrameInsets,
-                                kExpectedHeight - kFrameInsets);
+  gfx::Size expected_inner_size(
+      blink::kMinimumBorderlessWindowSize - kFrameInsets,
+      blink::kMinimumBorderlessWindowSize - kFrameInsets);
   WaitForWindowSizeCorrectlyUpdated(popup_browser_view, expected_inner_size,
                                     expected_size);
 #endif
--- content/browser/renderer_host/render_widget_host_view_base.h
+++ content/browser/renderer_host/render_widget_host_view_base.h
@@ -166,6 +166,9 @@ class CONTENT_EXPORT RenderWidgetHostViewBase : public RenderWidgetHostView,
   gfx::PointF TransformRootPointToViewCoordSpace(
       const gfx::PointF& point) override;
 
+  // Returns the value for whether the auto-resize has been enabled or not.
+  bool IsAutoResizeEnabled();
+
   virtual void UpdateIntrinsicSizingInfo(
       blink::mojom::IntrinsicSizingInfoPtr sizing_info);
 
