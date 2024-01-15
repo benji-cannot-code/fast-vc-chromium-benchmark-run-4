fcf557cbd54508b14ee000c1e8e27655e4b5bbaa
Thomas Nguyen <tungnh@chromium.org>
2024-01-15 13:30:42 +0000

    [gardening] Disable flaky tests on Windows
    Disable flaky tests SidTest.FromNamedCapabilityVector and
    SidTest.NamedCapability.
    
    Bug: 1518451
    Change-Id: I8000df572471cd0c5b494dfc2e4cbc090ad7a078
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5198367
    Owners-Override: Thomas Nguyen <tungnh@chromium.org>
    Reviewed-by: Florian Jacky <fjacky@chromium.org>
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247139}

--- base/win/sid_unittest.cc
+++ base/win/sid_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/scoped_handle.h"
 #include "base/win/scoped_localalloc.h"
 #include "base/win/win_util.h"
+#include "build/branding_buildflags.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
@@ -153,7 +154,13 @@ TEST(SidTest, KnownCapability) {
   }
 }
 
-TEST(SidTest, NamedCapability) {
+// TODO(crbug.com/1518451): Disable flaky tests.
+#if defined(OFFICIAL_BUILD) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#define MAYBE_NamedCapability DISABLED_NamedCapability
+#else
+#define MAYBE_NamedCapability NamedCapability
+#endif
+TEST(SidTest, MAYBE_NamedCapability) {
   const std::wstring capabilities[] = {L"",
                                        L"InternetClient",
                                        L"InternetClientServer",
@@ -260,7 +267,13 @@ TEST(SidTest, FromSddlStringVector) {
   ASSERT_TRUE(TestFromSddlStringVector({}));
 }
 
-TEST(SidTest, FromNamedCapabilityVector) {
+// TODO(crbug.com/1518451): Disable flaky tests.
+#if defined(OFFICIAL_BUILD) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#define MAYBE_FromNamedCapabilityVector DISABLED_FromNamedCapabilityVector
+#else
+#define MAYBE_FromNamedCapabilityVector FromNamedCapabilityVector
+#endif
+TEST(SidTest, MAYBE_FromNamedCapabilityVector) {
   std::vector<std::wstring> capabilities = {L"",
                                             L"InternetClient",
                                             L"InternetClientServer",
