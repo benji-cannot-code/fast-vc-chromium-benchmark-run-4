03e65aefe0b4a5b20e8dfc765d6986c9cfe74f33
Avi Drissman <avi@chromium.org>
2024-01-15 21:03:57 +0000

    Remove GrabViewSnapshot from render_frame_host_impl_browsertest
    The async version is now wired up, and the sync version is deprecated.
    The code to call the async version is already there so switch to
    always using it.
    
    Note that this test was accidentally passing on Linux and Android
    because it didn’t test the result bool and assumed the sync screenshot
    call succeeded when in fact it was a no-op. This CL marks the whole
    test as skipped, but switches in the correct API so that the test
    owners can take their time and make it work.
    
    Bug: 1517390
    Change-Id: I7d06eac5ed66824a591eb3bd06944442a345ba7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5190329
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: William Liu <liuwilliam@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247293}

--- content/browser/renderer_host/render_frame_host_impl_browsertest.cc
+++ content/browser/renderer_host/render_frame_host_impl_browsertest.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/mock_callback.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/scoped_run_loop_timeout.h"
+#include "base/test/test_future.h"
 #include "base/test/test_timeouts.h"
 #include "build/build_config.h"
 #include "components/ukm/test_ukm_recorder.h"
@@ -7870,17 +7871,12 @@ viz::SurfaceId GetFirstSurfaceIdAfterNavigation(RenderWidgetHostView* view) {
 // BFCache, we explicitly set its fallback surface to the current View to avoid
 // visual glitches.
 //
-// TODO(https://crbug.com/1472026): Investigate and re-enable.
-#if BUILDFLAG(IS_WIN)
-#define MAYBE_NewContentTimeoutIsSetWhenLeavingBFCacheWithViewTransition \
-  DISABLED_NewContentTimeoutIsSetWhenLeavingBFCacheWithViewTransition
-#else
-#define MAYBE_NewContentTimeoutIsSetWhenLeavingBFCacheWithViewTransition \
-  NewContentTimeoutIsSetWhenLeavingBFCacheWithViewTransition
-#endif
+// This test is disabled because it's flaky on Windows, fails on iOS, and when
+// updated to use correct APIs, started failing consistently on Android and
+// Linux. TODO(https://crbug.com/1472026): Investigate and re-enable.
 IN_PROC_BROWSER_TEST_F(
     RenderFrameHostImplBrowserTestWithBFCacheAndViewTransition,
-    MAYBE_NewContentTimeoutIsSetWhenLeavingBFCacheWithViewTransition) {
+    DISABLED_NewContentTimeoutIsSetWhenLeavingBFCacheWithViewTransition) {
   // "red_jank_second_pageshow.html" janks the renderer on the second pageshow
   // event.
   const GURL url_red(embedded_test_server()->GetURL(
@@ -7924,10 +7920,11 @@ IN_PROC_BROWSER_TEST_F(
           .IsSameOrNewerThan(first_surface_id_after_nav_after_bfcache_restore));
 
   {
-    gfx::Image screenshot;
-    ui::GrabViewSnapshot(web_contents()->GetView()->GetNativeView(),
-                         gfx::Rect(web_contents()->GetSize()), &screenshot);
-    AssertBitmapOfColor(screenshot.AsBitmap(), SK_ColorGREEN);
+    base::test::TestFuture<gfx::Image> future;
+    ui::GrabViewSnapshotAsync(web_contents()->GetView()->GetNativeView(),
+                              gfx::Rect(web_contents()->GetSize()),
+                              future.GetCallback());
+    AssertBitmapOfColor(future.Take().AsBitmap(), SK_ColorGREEN);
   }
 
   ASSERT_TRUE(rwhi_red->IsContentRenderingTimeoutRunning());
@@ -7939,10 +7936,11 @@ IN_PROC_BROWSER_TEST_F(
   // `first_surface_id_after_nav_` of the `DelegatedFrameHost{Android}`. This
   // should have no effects on the screen.
   {
-    gfx::Image screenshot;
-    ui::GrabViewSnapshot(web_contents()->GetView()->GetNativeView(),
-                         gfx::Rect(web_contents()->GetSize()), &screenshot);
-    AssertBitmapOfColor(screenshot.AsBitmap(), SK_ColorGREEN);
+    base::test::TestFuture<gfx::Image> future;
+    ui::GrabViewSnapshotAsync(web_contents()->GetView()->GetNativeView(),
+                              gfx::Rect(web_contents()->GetSize()),
+                              future.GetCallback());
+    AssertBitmapOfColor(future.Take().AsBitmap(), SK_ColorGREEN);
   }
 }
 
