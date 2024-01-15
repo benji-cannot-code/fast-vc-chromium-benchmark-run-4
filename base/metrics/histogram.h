ec70ec54808c113338bc6056504b6ff3d0c681f7
Alex Ilin <alexilin@chromium.org>
2024-01-15 19:51:04 +0000

    [metrics] Avoid creating temporary std::string in Uma functions
    Change the type of Histogram::Factory::name_ from
    `const raw_ref<const std::string>` to `const std::string_view`. The
    latter doesn't require a temporary std::string construction for string
    literals. Semantically, they are both non-owning strings, so this
    shouldn't change code behavior.
    
    Bug: TBD
    Change-Id: I05d5a5a51f5b08b07eded4c5c8168a6a56e68b9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5123735
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247275}

--- base/metrics/histogram.h
+++ base/metrics/histogram.h
@@ -273,6 +273,23 @@ class BASE_EXPORT Histogram : public HistogramBase {
       base::PickleIterator* iter);
   static HistogramBase* DeserializeInfoImpl(base::PickleIterator* iter);
 
+  static HistogramBase* FactoryGetInternal(std::string_view name,
+                                           Sample minimum,
+                                           Sample maximum,
+                                           size_t bucket_count,
+                                           int32_t flags);
+  static HistogramBase* FactoryTimeGetInternal(std::string_view name,
+                                               base::TimeDelta minimum,
+                                               base::TimeDelta maximum,
+                                               size_t bucket_count,
+                                               int32_t flags);
+  static HistogramBase* FactoryMicrosecondsTimeGetInternal(
+      std::string_view name,
+      base::TimeDelta minimum,
+      base::TimeDelta maximum,
+      size_t bucket_count,
+      int32_t flags);
+
   // Create a snapshot containing all samples (both logged and unlogged).
   // Implementation of SnapshotSamples method with a more specific type for
   // internal use.
@@ -358,7 +375,7 @@ class BASE_EXPORT LinearHistogram : public Histogram {
   // it's not NULL, the last element in the array must has a NULL in its
   // "description" field.
   static HistogramBase* FactoryGetWithRangeDescription(
-      const std::string& name,
+      std::string_view name,
       Sample minimum,
       Sample maximum,
       size_t bucket_count,
@@ -392,6 +409,17 @@ class BASE_EXPORT LinearHistogram : public Histogram {
   friend BASE_EXPORT HistogramBase* DeserializeHistogramInfo(
       base::PickleIterator* iter);
   static HistogramBase* DeserializeInfoImpl(base::PickleIterator* iter);
+
+  static HistogramBase* FactoryGetInternal(std::string_view name,
+                                           Sample minimum,
+                                           Sample maximum,
+                                           size_t bucket_count,
+                                           int32_t flags);
+  static HistogramBase* FactoryTimeGetInternal(std::string_view name,
+                                               TimeDelta minimum,
+                                               TimeDelta maximum,
+                                               size_t bucket_count,
+                                               int32_t flags);
 
   // For some ranges, we store a printable description of a bucket range.
   // If there is no description, then GetAsciiBucketRange() uses parent class
@@ -488,6 +516,9 @@ class BASE_EXPORT BooleanHistogram : public LinearHistogram {
   class Factory;
 
  private:
+  static HistogramBase* FactoryGetInternal(std::string_view name,
+                                           int32_t flags);
+
   BooleanHistogram(const char* name, const BucketRanges* ranges);
   BooleanHistogram(const char* name,
                    const BucketRanges* ranges,
@@ -563,6 +594,11 @@ class BASE_EXPORT CustomHistogram : public Histogram {
   friend BASE_EXPORT HistogramBase* DeserializeHistogramInfo(
       base::PickleIterator* iter);
   static HistogramBase* DeserializeInfoImpl(base::PickleIterator* iter);
+
+  static HistogramBase* FactoryGetInternal(
+      std::string_view name,
+      const std::vector<Sample>& custom_ranges,
+      int32_t flags);
 
   static bool ValidateCustomRanges(const std::vector<Sample>& custom_ranges);
 };
