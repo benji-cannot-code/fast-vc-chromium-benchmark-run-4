eea4fd5750547f8bfd22f57f42bae78ac2dad4a4
Simon Zünd <szuend@chromium.org>
2024-01-15 07:58:28 +0000

    [inspector] Add 'charset' to Network.Response
    MIME type and charset are parsed out of the Content-Type header so
    technically the information is already available in the
    "responseReceived" event.
    
    Since MIME type is already a separate field and charset is crucial to
    decode text resources that are sent in binary form (e.g. SVG) we also
    include it in the "responseReceived" events. This also saves clients
    from implementing a Content-Type parser.
    
    Bug: 1509336
    Change-Id: Ieac4a126aab62f975bdce4b5ae886abe3f96d63a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5134651
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247044}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/resources/content-type-utf16.php
@@ -0,0 +1,5 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+  header("Content-Type: text/html; charset=utf-16");
+  echo base64_decode("PABwAD4ASQDxAHQA6wByAG4A4gB0AGkA9ABuAOAAbABpAHoA5gB0AGkA+ABuAAMmNNgG3zwALwBwAD4A");
+?>
---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/response-charset.js
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {page, session, dp} = await testRunner.startBlank(
+      `Tests that Network.Response includes the charset`);
+
+  await dp.Network.enable();
+  await dp.Network.setCacheDisabled({cacheDisabled: true});
+  testRunner.log('Network Enabled');
+
+  {
+    testRunner.log('Fetching with no charset');
+    session.evaluate(`fetch('/inspector-protocol/network/resources/content-type.php?contentType=${encodeURI('text/html')}');`);
+    const event = await dp.Network.onceResponseReceived();
+    testRunner.log(`Network.Response.charset: ${event.params.response.charset}`);
+    await dp.Network.onceLoadingFinished();  // Prevent racing with other tests.
+  }
+
+  {
+    testRunner.log('Fetching with utf-8');
+    session.evaluate(`fetch('/inspector-protocol/network/resources/content-type.php?contentType=${encodeURI('text/html; charset=utf-8')}');`);
+    const event = await dp.Network.onceResponseReceived();
+    testRunner.log(`Network.Response.charset: ${event.params.response.charset}`);
+    await dp.Network.onceLoadingFinished();  // Prevent racing with other tests.
+  }
+
+  {
+    testRunner.log('Fetching with iso-8869-1');
+    session.evaluate(`fetch('/inspector-protocol/network/resources/content-type.php?contentType=${encodeURI('text/html; charset=iso-8859-1')}');`);
+    const event = await dp.Network.onceResponseReceived();
+    testRunner.log(`Network.Response.charset: ${event.params.response.charset}`);
+    await dp.Network.onceLoadingFinished();  // Prevent racing with other tests.
+  }
+
+  {
+    testRunner.log('Fetching utf-16 content');
+    session.evaluate(`fetch('/inspector-protocol/network/resources/content-type-utf16.php');`);
+    const event = await dp.Network.onceResponseReceived();
+    testRunner.log(`Network.Response.charset: ${event.params.response.charset}`);
+    await dp.Network.onceLoadingFinished();
+    const {result: {body}} = await dp.Network.getResponseBody({requestId: event.params.requestId});
+    testRunner.log(`Response body: ${body}`);
+  }
+  testRunner.completeTest();
+});
