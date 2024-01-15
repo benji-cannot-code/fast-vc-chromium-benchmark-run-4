a704d0b6eb359cf5160c8780cb8b9ea4b4e44542
Elad Alon <eladalon@chromium.org>
2024-01-15 12:11:08 +0000

    [CSC] Add browser tests covering source-change
    Add browser tests for Captured Surface Control APIs,
    ensuring that change of source ("share this tab instead")
    is handled correctly.
    
    Bug: 1466247
    Change-Id: Id781c9c6387619771e6e22c08c68d8247d3db7f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5140330
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247116}

--- content/public/test/browser_test_utils.h
+++ content/public/test/browser_test_utils.h
@@ -32,11 +32,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/quads/compositor_frame.h"
 #include "content/public/browser/commit_deferring_condition.h"
 #include "content/public/browser/devtools_agent_host.h"
+#include "content/public/browser/global_routing_id.h"
 #include "content/public/browser/render_frame_metadata_provider.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_process_host_observer.h"
 #include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/web_contents_delegate.h"
+#include "content/public/browser/web_contents_media_capture_id.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/common/isolated_world_ids.h"
 #include "content/public/common/page_type.h"
@@ -75,6 +77,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_WIN)
 #include "base/win/scoped_handle.h"
 #endif
+
+#if !BUILDFLAG(IS_ANDROID)
+#include "content/public/test/mock_captured_surface_controller.h"
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 namespace gfx {
 class Point;
@@ -2396,6 +2402,14 @@ RegisterWebContentsCreationCallback(
 // is a standalone executable without an Info.plist.
 bool EnableNativeWindowActivation();
 #endif  // BUILDFLAG(IS_MAC)
+
+#if !BUILDFLAG(IS_ANDROID)
+// Set the global factory for CapturedSurfaceController objects.
+void SetCapturedSurfaceControllerFactoryForTesting(
+    base::RepeatingCallback<std::unique_ptr<MockCapturedSurfaceController>(
+        GlobalRenderFrameHostId,
+        WebContentsMediaCaptureId)> factory);
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 }  // namespace content
 
