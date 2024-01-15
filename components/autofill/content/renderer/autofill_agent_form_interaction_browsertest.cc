7dc325934e7a9c960e3988df1ea7e9f02173b7bd
Friedrich Horschig <fhorschig@chromium.org>
2024-01-15 10:19:05 +0000

    [Autofill] Add Todo to parameterize agent tests.
    For real this time. Noop change.
    
    Bug: 1516614
    Change-Id: I66556cefd933da0da16dad60817743490c64c77d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5197828
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1247090}

--- components/autofill/content/renderer/autofill_agent_form_interaction_browsertest.cc
+++ components/autofill/content/renderer/autofill_agent_form_interaction_browsertest.cc
@@ -106,6 +106,7 @@ AutofillSuggestionTriggerSource TriggerSourceOnTextareaFocus() {
 class AutofillAgentFormInteractionTest : public test::AutofillRendererTest {
  public:
   void SetUp() override {
+    // TODO(crbug.com/63573): parameterize tests over AutofillAgent::Config.
     test::AutofillRendererTest::SetUp();
     web_view_->SetDefaultPageScaleLimits(1, 4);
 
