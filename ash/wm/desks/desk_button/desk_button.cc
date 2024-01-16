20140d202f6b2bd4d729dce4e1c5c57513ecb423
Yongshun Liu <yongshun@chromium.org>
2024-01-16 23:47:49 +0000

    bento_button: Fix focus ring for desk button
    This fixes the missing focus ring for desk button. It also fixes the
    focus ring issue when desk name is too long.
    
    Bug: b/319907883, b/296110609
    Test: ash_unittests
    Change-Id: Ie1f9588da33250a41dbf7f55ba3fa129302cf07a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5198425
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247828}

--- ash/wm/desks/desk_button/desk_button.cc
+++ ash/wm/desks/desk_button/desk_button.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/size.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/background.h"
+#include "ui/views/controls/focus_ring.h"
 #include "ui/views/controls/highlight_path_generator.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/flex_layout.h"
@@ -140,6 +141,11 @@ DeskButton::DeskButton(DeskButtonWidget* desk_button_widget)
                                    views::MaximumFlexSizeRule::kPreferred));
 
   SetupFocus(this);
+
+  // TODO(yongshun): Remove this when the desk button is re-structured with the
+  // new specs.
+  views::FocusRing::Get(this)->SetHasFocusPredicate(base::BindRepeating(
+      [](const views::View* view) { return view->HasFocus(); }));
 
   // The previous desk button should always be on the left and the next desk
   // button on the right even in RTL mode to respect the direction of the desk
@@ -393,8 +399,19 @@ void DeskButton::AboutToRequestFocusFromTabTraversal(bool reverse) {
       ->MaybeFocusOut(reverse);
 }
 
+// TODO(yongshun): Remove this when the desk button is re-structured with the
+// new specs.
+void DeskButton::OnViewFocused(views::View* observed_view) {
+  views::Button::OnViewFocused(observed_view);
+  views::FocusRing::Get(this)->SchedulePaint();
+}
+
+// TODO(yongshun): Remove this when the desk button is re-structured with the
+// new specs.
 void DeskButton::OnViewBlurred(views::View* observed_view) {
   MaybeContract();
+  views::Button::OnViewBlurred(observed_view);
+  views::FocusRing::Get(this)->SchedulePaint();
 }
 
 void DeskButton::OnShellDestroying() {
