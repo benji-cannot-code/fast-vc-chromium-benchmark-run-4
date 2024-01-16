bdc24c7fee3b4c694972086522e9881c1c1bfd60
Elijah Hewer <hewer@chromium.org>
2024-01-16 22:43:58 +0000

    Pine: Add prototype context menu
    Add a work-in-progress inline context menu for Pine. Note that the
    menu offers no functionality at the moment and the default selection
    in the radio is hard coded.
    
    Bug: b/318749478
    Test: manual + unit
    Change-Id: I1d9665660337c9612ff0d4da9fb3ce010baa12a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5183318
    Reviewed-by: Min Chen <minch@chromium.org>
    Commit-Queue: Elijah Hewer <hewer@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247800}

--- ash/wm/window_restore/pine_unittest.cc
+++ ash/wm/window_restore/pine_unittest.cc
@@ -8,10 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/test/ash_test_base.h"
 #include "ash/wm/overview/overview_controller.h"
+#include "ash/wm/overview/overview_grid.h"
 #include "ash/wm/overview/overview_session.h"
+#include "ash/wm/overview/overview_test_util.h"
 #include "ash/wm/overview/overview_types.h"
+#include "ash/wm/window_restore/pine_contents_view.h"
+#include "ash/wm/window_restore/pine_context_menu_model.h"
 #include "ash/wm/window_restore/window_restore_controller.h"
 #include "base/test/scoped_feature_list.h"
+#include "ui/views/controls/button/image_button.h"
+#include "ui/views/view_utils.h"
 
 namespace ash {
 
@@ -34,6 +40,27 @@ TEST_F(PineTest, Show) {
   ASSERT_TRUE(overview_session);
   EXPECT_EQ(OverviewEnterExitType::kPine,
             overview_session->enter_exit_overview_type());
+}
+
+TEST_F(PineTest, ShowContextMenuOnSettingsButtonClicked) {
+  Shell::Get()->window_restore_controller()->MaybeStartPineOverviewSession();
+
+  // Get the active Pine widget.
+  OverviewGrid* grid = GetOverviewGridForRoot(Shell::GetPrimaryRootWindow());
+  ASSERT_TRUE(grid);
+  auto* pine_widget = grid->pine_widget_for_testing();
+  ASSERT_TRUE(pine_widget);
+
+  // The context menu should not be open.
+  PineContentsView* const contents_view =
+      views::AsViewClass<PineContentsView>(pine_widget->GetContentsView());
+  ASSERT_TRUE(contents_view);
+  PineContextMenuModel* context_menu = contents_view->context_menu_model_.get();
+  EXPECT_FALSE(context_menu);
+
+  // Click on the settings button, the context menu should appear.
+  LeftClickOn(contents_view->settings_button_view_.get());
+  EXPECT_TRUE(contents_view->context_menu_model_.get());
 }
 
 }  // namespace ash
