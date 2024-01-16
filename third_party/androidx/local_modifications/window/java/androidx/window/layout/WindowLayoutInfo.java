0ea5b1d1099cdc9461a57e54d5973ef9e4827f96
Sam Maier <smaier@chromium.org>
2024-01-16 02:07:10 +0000

    Making package-private method public to fix isolated split issue
    Isolated splits don't allow package-private access across splits. This
    is one that R8 has identified. More details in
    go/package-private-issues-in-chrome.
    
    Bug: 1517307
    Change-Id: I665691c9b4dcee84f36440251922a6763818e306
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5199266
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247340}

--- third_party/androidx/local_modifications/window/java/androidx/window/layout/WindowLayoutInfo.java
+++ third_party/androidx/local_modifications/window/java/androidx/window/layout/WindowLayoutInfo.java
@@ -11,7 +11,7 @@ import java.util.List;
  */
 public class WindowLayoutInfo {
 
-  List<DisplayFeature> getDisplayFeatures() {
-   return null;
-  }
+    public List<DisplayFeature> getDisplayFeatures() {
+        return null;
+    }
 }
