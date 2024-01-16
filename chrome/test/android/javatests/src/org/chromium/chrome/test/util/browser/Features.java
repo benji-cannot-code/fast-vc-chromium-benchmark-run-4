cf540cb83d7e4bb19282c59497bbb9b223b98353
Henrique Nakashima <hnakashima@chromium.org>
2024-01-16 18:04:51 +0000

    [Android] //chrome Features.*Processors honors //base annotations
    Make //chrome's Features.JUnitProcessor and
    Features.InstrumentationProcessor compatible with both //chrome and
    //base Features.EnableFeatures and Features.DisableFeatures.
    
    This forward compatibility allows changing the annotations before the
    processor, which is necessary for downstream tests that rely on
    InstrumentationProcessor being a default rule in
    ChromeJUnit4ClassRunner.
    
    Bug: 1442353
    Change-Id: I27ab88df504521364c677a46bb5069f73b65f317
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5199306
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247595}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/Features.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/Features.java
@@ -68,7 +68,11 @@ public class Features extends FeaturesBase {
      */
     public static class JUnitProcessor extends BaseJUnitProcessor {
         public JUnitProcessor() {
-            super(EnableFeatures.class, DisableFeatures.class);
+            super(
+                    EnableFeatures.class,
+                    DisableFeatures.class,
+                    org.chromium.base.test.util.Features.EnableFeatures.class,
+                    org.chromium.base.test.util.Features.DisableFeatures.class);
             getInstance();
         }
 
@@ -91,7 +95,11 @@ public class Features extends FeaturesBase {
      */
     public static class InstrumentationProcessor extends BaseInstrumentationProcessor {
         public InstrumentationProcessor() {
-            super(EnableFeatures.class, DisableFeatures.class);
+            super(
+                    EnableFeatures.class,
+                    DisableFeatures.class,
+                    org.chromium.base.test.util.Features.EnableFeatures.class,
+                    org.chromium.base.test.util.Features.DisableFeatures.class);
             getInstance();
         }
 
@@ -107,6 +115,13 @@ public class Features extends FeaturesBase {
                 sInstance.enable(((EnableFeatures) annotation).value());
             } else if (annotation instanceof DisableFeatures) {
                 sInstance.disable(((DisableFeatures) annotation).value());
+            } else if (annotation instanceof org.chromium.base.test.util.Features.EnableFeatures) {
+                sInstance.enable(
+                        ((org.chromium.base.test.util.Features.EnableFeatures) annotation).value());
+            } else if (annotation instanceof org.chromium.base.test.util.Features.DisableFeatures) {
+                sInstance.disable(
+                        ((org.chromium.base.test.util.Features.DisableFeatures) annotation)
+                                .value());
             }
         }
     }
