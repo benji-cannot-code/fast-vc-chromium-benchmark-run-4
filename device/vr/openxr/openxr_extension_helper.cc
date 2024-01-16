490061845e98c4784189e4c7fb7edf89e4ab5f26
Alexander Cooper <alcooper@chromium.org>
2024-01-16 22:42:44 +0000

    Fork MSFT planes/hit-test logic into a child class
    Creates a base class for the SceneUnderstanding logic and refactors out
    the bits that clearly leverage the MSFT extensions into that base class.
    Note that at this time the various openxr_scene/openxr_object classes
    that this child creates have not yet been renamed, as it's unclear if we
    will want corresponding types for other extensions or not. The Android
    Raycast extension functions similarly to this in that it also does not
    maintain subscriptions and has a single method to get a hit test that
    will require a space, time, origin, and direction, so this shape should
    work for that implementation as well.
    
    Also does some minor IWYU cleanup and converts usage of absl::optional
    in both files to std::optional. Converts one outptr that shouldn't be
    allowed to be null to a mutable reference.
    
    Bug: 1435548
    Change-Id: I22332e78d640923ca738ab783ff03c0b409c9d24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5191248
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247798}

--- device/vr/openxr/openxr_extension_helper.cc
+++ device/vr/openxr/openxr_extension_helper.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/vr/openxr/openxr_anchor_manager.h"
 #include "device/vr/openxr/openxr_hand_tracker.h"
 #include "device/vr/openxr/openxr_hand_tracker_meta.h"
-#include "device/vr/openxr/openxr_scene_understanding_manager.h"
+#include "device/vr/openxr/openxr_scene_understanding_manager_msft.h"
 #include "device/vr/openxr/openxr_stage_bounds_provider_basic.h"
 #include "device/vr/public/mojom/xr_session.mojom.h"
 // Included on all platforms so that we can check the extension names, even
@@ -104,6 +104,7 @@ OpenXrExtensionHelper::OpenXrExtensionHelper(
           const_cast<PFN_xrCreateSpatialAnchorSpaceMSFT*>(
               &extension_methods_.xrCreateSpatialAnchorSpaceMSFT)));
 
+  // MSFT Scene Understanding Methods
   std::ignore = xrGetInstanceProcAddr(
       instance, "xrEnumerateSceneComputeFeaturesMSFT",
       reinterpret_cast<PFN_xrVoidFunction*>(
@@ -241,8 +242,12 @@ std::unique_ptr<OpenXRSceneUnderstandingManager>
 OpenXrExtensionHelper::CreateSceneUnderstandingManager(
     XrSession session,
     XrSpace base_space) const {
-  return std::make_unique<OpenXRSceneUnderstandingManager>(*this, session,
-                                                           base_space);
+  if (IsExtensionSupported(XR_MSFT_SCENE_UNDERSTANDING_EXTENSION_NAME)) {
+    return std::make_unique<OpenXRSceneUnderstandingManagerMSFT>(*this, session,
+                                                                 base_space);
+  }
+
+  return nullptr;
 }
 
 std::unique_ptr<OpenXrStageBoundsProvider>
