7273eeb24867bad08377cc0d67f1f0671abbba2d
Denny Huang <dennyh@google.com>
2024-01-16 06:19:25 +0000

    [Telemetry] Report result for SetAudioGain in Management API
    Setting audio gain should only be available for input audio nodes.
    Implement this in `SetAudioGain` in Management API, including changing
    the interface to have a boolean response.
    
    Bug: b/303391740, b/317014106
    Test: unit_tests --gtest_filter="TelemetryManagementServiceAshTest*"
    Test: browser_tests --gtest_filter="TelemetryExtension*"
    Test: testing/xvfb.py build/lacros/test_runner.py test out_linux_lacros/Release/lacros_chrome_browsertests --gtest_filter="TelemetryExtension*"
    Test: testing/xvfb.py ./build/lacros/test_runner.py test out_linux_lacros/Release/lacros_chrome_browsertests --ash-chrome-path-override=lacros_version_skew_tests_v$version/test_ash_chrome --gtest_filter="TelemetryExtension*"
    Change-Id: Ia6fb2b9a80acd959438e4c287f186eca4ba76c14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5196714
    Reviewed-by: Kerker Yang <kerker@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Denny Huang <dennyh@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1247374}

--- chrome/browser/chromeos/extensions/telemetry/api/management/management_api.h
+++ chrome/browser/chromeos/extensions/telemetry/api/management/management_api.h
@@ -57,7 +57,7 @@ class OsManagementSetAudioGainFunction : public ManagementApiFunctionBase {
   // BaseTelemetryExtensionApiGuardFunction:
   void RunIfAllowed() override;
 
-  void OnResult();
+  void OnResult(bool is_success);
 };
 
 class OsManagementSetAudioVolumeFunction : public ManagementApiFunctionBase {
