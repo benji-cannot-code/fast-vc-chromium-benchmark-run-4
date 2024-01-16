69901f36e0a40a7bb01fa08374d66cb23750d631
Sharmin Zaman <sharminzaman@google.com>
2024-01-16 07:50:45 +0000

    [Shortstand] Fix web app shelf context menu browser tests
    When Shortstand is enabled, a submenu option no longer exists in a
    context menu as standalone windows are the new default display mode.
    This CL fixes tests breaking as a result of this.
    
    Bug: 319547425
    Change-Id: If4b87c0724cace51564de7307c14471c09f23fd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5196833
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247396}

--- chrome/browser/ui/ash/shelf/app_service/app_service_shelf_context_menu_browsertest.cc
+++ chrome/browser/ui/ash/shelf/app_service/app_service_shelf_context_menu_browsertest.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/test/base/in_process_browser_test.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "components/webapps/common/web_app_id.h"
 #include "content/public/common/content_features.h"
 #include "content/public/test/browser_test.h"
@@ -43,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class AppServiceShelfContextMenuBrowserTest : public InProcessBrowserTest {
  public:
   AppServiceShelfContextMenuBrowserTest() = default;
+
   ~AppServiceShelfContextMenuBrowserTest() override = default;
 
   struct MenuSection {
@@ -80,10 +82,14 @@ class AppServiceShelfContextMenuBrowserTest : public InProcessBrowserTest {
 
     return result;
   }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 class AppServiceShelfContextMenuWebAppBrowserTest
-    : public AppServiceShelfContextMenuBrowserTest {
+    : public AppServiceShelfContextMenuBrowserTest,
+      public testing::WithParamInterface<bool> {
  public:
   AppServiceShelfContextMenuWebAppBrowserTest() {
     scoped_feature_list_.InitWithFeatures(
@@ -102,11 +108,13 @@ class AppServiceShelfContextMenuWebAppBrowserTest
       return views::kOpenIcon;
   }
 
+  bool IsShortstandEnabled() { return GetParam(); }
+
  private:
   base::test::ScopedFeatureList scoped_feature_list_;
 };
 
-IN_PROC_BROWSER_TEST_F(AppServiceShelfContextMenuWebAppBrowserTest,
+IN_PROC_BROWSER_TEST_P(AppServiceShelfContextMenuWebAppBrowserTest,
                        WindowCommandCheckedForMinimalUi) {
   Profile* profile = browser()->profile();
   base::UserActionTester user_action_tester;
@@ -116,6 +124,16 @@ IN_PROC_BROWSER_TEST_F(AppServiceShelfContextMenuWebAppBrowserTest,
   web_app_install_info->display_mode = blink::mojom::DisplayMode::kMinimalUi;
   webapps::AppId app_id =
       web_app::test::InstallWebApp(profile, std::move(web_app_install_info));
+
+  // When Shortstand is enabled, the display mode can no longer be changed
+  // through the context menu. The submenu is replaced with a 'New Window'
+  // command.
+  if (IsShortstandEnabled()) {
+    std::optional<MenuSection> menu_section =
+        GetContextMenuSectionForAppCommand(app_id, ash::LAUNCH_NEW);
+    ASSERT_TRUE(menu_section);
+    return;
+  }
 
   // Activate open in window menu item.
   std::optional<MenuSection> menu_section =
@@ -134,8 +152,14 @@ IN_PROC_BROWSER_TEST_F(AppServiceShelfContextMenuWebAppBrowserTest,
       menu_section->sub_model->IsItemCheckedAt(menu_section->command_index));
 }
 
-IN_PROC_BROWSER_TEST_F(AppServiceShelfContextMenuWebAppBrowserTest,
+IN_PROC_BROWSER_TEST_P(AppServiceShelfContextMenuWebAppBrowserTest,
                        SetOpenInTabbedWindow) {
+  // As the display mode can no longer be changed through the context menu when
+  // Shortstand is enabled, this test is skipped.
+  if (IsShortstandEnabled()) {
+    GTEST_SKIP();
+  }
+
   Profile* profile = browser()->profile();
   base::UserActionTester user_action_tester;
 
@@ -162,8 +186,13 @@ IN_PROC_BROWSER_TEST_F(AppServiceShelfContextMenuWebAppBrowserTest,
   EXPECT_TRUE(app_browser->app_controller()->has_tab_strip());
 }
 
-IN_PROC_BROWSER_TEST_F(AppServiceShelfContextMenuWebAppBrowserTest,
+IN_PROC_BROWSER_TEST_P(AppServiceShelfContextMenuWebAppBrowserTest,
                        SetOpenInBrowserTab) {
+  // As the display mode can no longer be changed through the context menu when
+  // Shortstand is enabled, this test is skipped.
+  if (IsShortstandEnabled()) {
+    GTEST_SKIP();
+  }
   Profile* profile = browser()->profile();
   base::UserActionTester user_action_tester;
 
@@ -184,8 +213,14 @@ IN_PROC_BROWSER_TEST_F(AppServiceShelfContextMenuWebAppBrowserTest,
   EXPECT_EQ(user_action_tester.GetActionCount("WebApp.SetWindowMode.Tab"), 1);
 }
 
-IN_PROC_BROWSER_TEST_F(AppServiceShelfContextMenuWebAppBrowserTest,
+IN_PROC_BROWSER_TEST_P(AppServiceShelfContextMenuWebAppBrowserTest,
                        LaunchNewMenuItemDynamicallyChanges) {
+  // As the display mode can no longer be changed through the context menu when
+  // Shortstand is enabled, this test is skipped.
+  if (IsShortstandEnabled()) {
+    GTEST_SKIP();
+  }
+
   Profile* profile = browser()->profile();
   auto web_app_install_info = std::make_unique<web_app::WebAppInstallInfo>();
   web_app_install_info->start_url = GURL("https://example.org");
@@ -222,6 +257,9 @@ IN_PROC_BROWSER_TEST_F(AppServiceShelfContextMenuWebAppBrowserTest,
                   launch_new_submodel->GetCommandIdAt(launch_new_item_index)));
   }
 }
+INSTANTIATE_TEST_SUITE_P(All,
+                         AppServiceShelfContextMenuWebAppBrowserTest,
+                         ::testing::Bool());
 
 class AppServiceShelfContextMenuTabbedWebAppBrowserTest
     : public AppServiceShelfContextMenuBrowserTest {
