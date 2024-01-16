85752dec22698d00069893d666d0d64cd3f462f9
Eliot Courtney <edcourtney@google.com>
2024-01-16 01:34:51 +0000

    Sort windows from top to bottom for display update
    This CL sorts windows from top to bottom when performing a display
    configuration update (like display scale change). One reason to do this
    is when the windows get larger, if we resize the topmost one first, it
    will occlude continue to anything below it while they are also resized
    to be larger. We pre-order DFS the window 🌲 from bottom-most to
    top-most and collect the windows to avoid quadratic lookup.
    
    In general, there is no best order which optimises non-atomic changes,
    but top to bottom is one of the more reasonable ones.
    
    Bug: 1509181
    Test: added unit test + local testing
    Change-Id: I1e263068b430334ac6b112495cb5881f0ae13e8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5118683
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247334}

--- ash/wm/workspace/workspace_layout_manager.cc
+++ ash/wm/workspace/workspace_layout_manager.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/window_util.h"
 #include "ash/wm/wm_event.h"
 #include "ash/wm/workspace/backdrop_controller.h"
+#include "base/containers/adapters.h"
 #include "base/containers/contains.h"
 #include "base/memory/raw_ptr.h"
 #include "ui/aura/client/aura_constants.h"
@@ -528,8 +529,12 @@ void WorkspaceLayoutManager::AdjustAllWindowsBoundsForWorkAreaChange(
   // We also do this when developers running Aura on a desktop manually resize
   // the host window.
   // We also need to do this when the work area insets changes.
-  for (aura::Window* window : windows_)
+  // Update the windows from top-most to bottom-most so when windows get bigger
+  // they occlude windows below them first.
+  auto ordered_windows = window_util::SortWindowsBottomToTop(windows_);
+  for (aura::Window* window : base::Reversed(ordered_windows)) {
     WindowState::Get(window)->OnWMEvent(event);
+  }
 }
 
 void WorkspaceLayoutManager::UpdateShelfVisibility() {
