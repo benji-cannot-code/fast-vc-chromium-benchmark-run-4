dfe9dd6bcfaf390ee14e552dacc01da924ce6423
EmiliaPaz <emiliapaz@chromium.org>
2024-01-16 20:24:24 +0000

    [Extensions cleanup] Remove unused method
    Bug: n/a
    Change-Id: Ic858cc3fd2d8faed9b19df7c050d92e052de62e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5193231
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247684}

--- chrome/browser/ui/views/extensions/extension_context_menu_controller.h
+++ chrome/browser/ui/views/extensions/extension_context_menu_controller.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class ToolbarActionViewController;
 
 namespace views {
-class Button;
 class MenuModelAdapter;
 class MenuRunner;
 }  // namespace views
@@ -42,8 +41,6 @@ class ExtensionContextMenuController : public views::ContextMenuController {
   bool IsMenuRunning() const;
 
  private:
-  void RunExtensionContextMenu(views::Button* source);
-
   // Callback for MenuModelAdapter.
   void OnMenuClosed();
 
