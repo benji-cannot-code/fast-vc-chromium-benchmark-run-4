d969aa696b881af16bd1293bc3803df9a9d99035
Marlon Facey <mfacey@chromium.org>
2024-01-16 22:36:39 +0000

    Revert "[NTP Quests] Added score threshold"
    This reverts commit 95089d1e330955ff25b74794a75a3ab143d76e8a.
    
    Reason for revert: Caused a regression due to empty clusters
    
    Original change's description:
    > [NTP Quests] Added score threshold
    >
    > Added a feature param to vary the score threshold for showing history
    > cluster modules. Also used that feature param to filter the clusters.
    >
    > Bug: 1512158
    > Change-Id: I9c0b178c93098252bd18f7d6235a21ae59ea47f2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5169411
    > Commit-Queue: Marlon Facey <mfacey@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Roman Arora <romanarora@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1245999}
    
    Bug: 1512158
    Change-Id: I9b705be704dddf24dc797955c1cbacd230a9097b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5201194
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Owners-Override: Prudhvikumar Bommana <pbommana@google.com>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247792}

--- components/search/ntp_features.cc
+++ components/search/ntp_features.cc
@@ -459,8 +459,6 @@ const char kNtpHistoryClustersModuleCategoriesBoostlistParam[] =
     "NtpHistoryClustersModuleCategoriesBoostlistParam";
 const char kNtpHistoryClustersModuleMaxClustersParam[] =
     "NtpHistoryClustersModuleMaxClustersParam";
-const char kNtpHistoryClustersModuleScoreThresholdParam[] =
-    "NtpHistoryClustersModuleScoreThresholdParam";
 const char kNtpRealboxWidthBehaviorParam[] = "NtpRealboxWidthBehaviorParam";
 const char kNtpTabResumptionModuleDataParam[] =
     "NtpTabResumptionModuleDataParam";
