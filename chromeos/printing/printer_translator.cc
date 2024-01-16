159919dc84645e3de9a181a3fb253dd1a0a968d0
Sparik Hayrapetyan <ust@google.com>
2024-01-16 11:15:11 +0000

    [custom-ppd] Set user_supplied_ppd_url from policy if available
    Parse the `user_supplied_ppd_uri` field from printer policy into the
    chromeos::Printer structure. This will result in using the PPD file
    specified by `user_supplied_ppd_uri` for printer setup.
    
    Bug: b/317272963
    Change-Id: Iec7df46812a0bf3961da77b58c49f8324067a849
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5143126
    Commit-Queue: Sparik Hayrapetyan <ust@google.com>
    Reviewed-by: Piotr Pawliczek <pawliczek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247444}

--- chromeos/printing/printer_translator.cc
+++ chromeos/printing/printer_translator.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/printing/cups_printer_status.h"
 #include "chromeos/printing/printer_configuration.h"
 #include "chromeos/printing/uri.h"
+#include "url/gurl.h"
 #include "url/url_constants.h"
 
 namespace chromeos {
@@ -38,6 +39,7 @@ const char kUUID[] = "uuid";
 const char kPpdResource[] = "ppd_resource";
 const char kAutoconf[] = "autoconf";
 const char kGuid[] = "guid";
+const char kUserSuppliedPpdUri[] = "user_supplied_ppd_uri";
 
 // Populates the |printer| object with corresponding fields from |value|.
 // Returns false if |value| is missing a required field.
@@ -118,16 +120,22 @@ bool ValidateAndSetPpdReference(const base::Value::Dict& ppd_resource,
                                 Printer& printer) {
   std::optional<bool> autoconf = ppd_resource.FindBool(kAutoconf);
   const std::string* effective_model = ppd_resource.FindString(kEffectiveModel);
+  const std::string* user_supplied_ppd_uri =
+      ppd_resource.FindString(kUserSuppliedPpdUri);
 
-  bool is_autoconf = autoconf.value_or(false);
-  bool has_effective_model = effective_model && !effective_model->empty();
-  bool is_valid = is_autoconf != has_effective_model;
-  if (!is_valid) {
+  const bool is_autoconf = autoconf.value_or(false);
+  const bool has_effective_model = effective_model && !effective_model->empty();
+  const bool has_user_supplied_ppd_uri =
+      user_supplied_ppd_uri && !user_supplied_ppd_uri->empty();
+
+  const bool has_exactly_one_ppd_resource =
+      is_autoconf + has_effective_model + has_user_supplied_ppd_uri == 1;
+  if (!has_exactly_one_ppd_resource) {
     LOG(WARNING) << base::StringPrintf(
-        "Managed printer '%s' has invalid %s values: is_autoconf: %d, "
-        "has_effective_model: %d",
+        "Managed printer '%s' must have exactly one %s value: is_autoconf: %d, "
+        "has_effective_model: %d, has_user_supplied_ppd_uri: %d",
         printer.display_name().c_str(), kPpdResource, is_autoconf,
-        has_effective_model);
+        has_effective_model, has_user_supplied_ppd_uri);
     return false;
   }
 
@@ -137,6 +145,18 @@ bool ValidateAndSetPpdReference(const base::Value::Dict& ppd_resource,
   if (has_effective_model) {
     printer.mutable_ppd_reference()->effective_make_and_model =
         *effective_model;
+  }
+  if (has_user_supplied_ppd_uri) {
+    GURL url(*user_supplied_ppd_uri);
+    if (!url.is_valid() || !url.SchemeIsHTTPOrHTTPS()) {
+      LOG(WARNING) << base::StringPrintf(
+          "Managed printer '%s' has invalid %s.%s: '%s'",
+          printer.display_name().c_str(), kPpdResource, kUserSuppliedPpdUri,
+          user_supplied_ppd_uri->c_str());
+      return false;
+    }
+    printer.mutable_ppd_reference()->user_supplied_ppd_url =
+        *user_supplied_ppd_uri;
   }
 
   return true;
