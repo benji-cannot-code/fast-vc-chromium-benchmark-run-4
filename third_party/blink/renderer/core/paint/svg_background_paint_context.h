f0e720816eac0c89b235a07dc9f5d193924cb50c
Fredrik Söderquist <fs@opera.com>
2024-01-16 18:29:59 +0000

    Apply zoom to coordinate space, not geometry in SVGMaskPainter
    Some <image>s - like gradients - contains some geometry information
    derived from the zoomed geometry. These may paint incorrectly when zoom
    differs from one. Adjust the coordinate space instead of the geometry to
    handle these cases correctly.
    
    Fixed: 1516749
    Change-Id: I25ccebbbb372b4ad237c3dad30f5a147b2b2de87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5197811
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1247612}

--- third_party/blink/renderer/core/paint/svg_background_paint_context.h
+++ third_party/blink/renderer/core/paint/svg_background_paint_context.h
@@ -27,8 +27,6 @@ class SVGBackgroundPaintContext {
 
   gfx::RectF VisualOverflowRect() const;
   gfx::RectF ReferenceBox(GeometryBox) const;
-  bool UsesZoomedCoordinates() const;
-  float EffectiveZoom() const;
   const ComputedStyle& Style() const;
 
  private:
