9759a3ff66e365b54a8c1024cb3e9c2349436bc9
Florian Leimgruber <fleimgruber@google.com>
2024-01-16 10:45:35 +0000

    Don't emit shadow predictions for all pairs of PatternSources
    Currently, on Chrome-branded builds with pattern provider enabled,
    predictions for the default, experimental and next-gen set are computed.
    Shadow predictions for all pairs are emitted.
    
    Given that every new regex will need to make it through next-gen ->
    experimental -> default, comparing next-gen with default doesn't make
    much sense. This CL removes this shadow prediction histogram.
    
    This enables a key simplification: Instead of computing default,
    experimental and next-gen predictions for every form, it suffices to
    compute only two of them; either default and experimental or
    experimental and next-gen, depending on whether default or
    experimental is the active source*.
    This reduces the pattern provider overhead from 3x to 2x.
    
    (The actual logic is slightly more complicated, since ML predictions
     exist as well: When ML is active, shadow predictions against the
     pattern source that would otherwise be active are computed. When ML is
     enabled as an inactive prediction source, shadow predictions for it
     are computed in addition to the PatternProvider shadow predictions)
    
    OBSOLETE_HISTOGRAM[Autofill.ShadowPredictions.NextGenToDefault]=See CL
    description
    
    * As per design doc, only default (99% of users) and experimental
      patterns (1% of users) will be active in practice.
    https://docs.google.com/document/d/12Fr4wWozA3Rd62D9Imf_86gQVb3BEI8_NCDpNsrkULw/edit?tab=t.0
    
    Bug: 1479353
    Change-Id: Ie13a834447d98317c7e0b2d6499e261423a7f3c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5197929
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1247436}

--- components/autofill/core/browser/metrics/shadow_prediction_metrics_unittest.cc
+++ components/autofill/core/browser/metrics/shadow_prediction_metrics_unittest.cc
@@ -167,14 +167,9 @@ TEST_F(AutofillShadowPredictionMetricsTest,
 #if BUILDFLAG(USE_INTERNAL_AUTOFILL_PATTERNS)
   histogram_tester.ExpectBucketCount(
       "Autofill.ShadowPredictions.ExperimentalToDefault", kNoPrediction, 2);
-  histogram_tester.ExpectBucketCount(
-      "Autofill.ShadowPredictions.NextGenToDefault", kNoPrediction, 2);
 #else
   EXPECT_THAT(histogram_tester.GetAllSamples(
                   "Autofill.ShadowPredictions.ExperimentalToDefault"),
-              IsEmpty());
-  EXPECT_THAT(histogram_tester.GetAllSamples(
-                  "Autofill.ShadowPredictions.NextGenToDefault"),
               IsEmpty());
 #endif
 }
@@ -214,19 +209,6 @@ TEST_F(AutofillShadowPredictionMetricsTest,
       UnorderedElementsAre(
           Bucket(kNameFullSamePredictionValueAgrees, 1),
           Bucket(kSearchTermDifferentPredictionsValueAgreesWithNew, 1)));
-
-  EXPECT_THAT(histogram_tester.GetAllSamples(
-                  "Autofill.ShadowPredictions.NextGenToDefault"),
-              UnorderedElementsAre(
-                  Bucket(kNameFullDifferentPredictionsValueAgreesWithOld, 1),
-                  Bucket(kSearchTermSamePredictionValueDisagrees, 1)));
-
-  EXPECT_THAT(
-      histogram_tester.GetAllSamples(
-          "Autofill.ShadowPredictions.NextGenToExperimental"),
-      UnorderedElementsAre(
-          Bucket(kNameFullDifferentPredictionsValueAgreesWithOld, 1),
-          Bucket(kEmailAddressDifferentPredictionsValueAgreesWithOld, 1)));
 }
 
 // Test that Autofill.ShadowPredictions.DefaultHeuristicToDefaultServer compares
