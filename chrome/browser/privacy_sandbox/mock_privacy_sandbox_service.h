bb87094c29077216dcd72f506f28e3276d4cae40
Nicola Tommasi <tommasin@chromium.org>
2024-01-16 11:04:08 +0000

    Proactive Topics blocking: Add GetFirstLevelTopics API
    Adds a new API to get all the first level topics (topics which have no
    parents).
    
    Bug: 1517554
    Change-Id: I5509ece1d0d5735cdf2e02a5ca59778325064f90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5185259
    Reviewed-by: Abe Boujane <boujane@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247441}

--- chrome/browser/privacy_sandbox/mock_privacy_sandbox_service.h
+++ chrome/browser/privacy_sandbox/mock_privacy_sandbox_service.h
@@ -82,6 +82,10 @@ class MockPrivacySandboxService : public PrivacySandboxService {
               (),
               (const, override));
   MOCK_METHOD(std::vector<privacy_sandbox::CanonicalTopic>,
+              GetFirstLevelTopics,
+              (),
+              (const, override));
+  MOCK_METHOD(std::vector<privacy_sandbox::CanonicalTopic>,
               GetBlockedTopics,
               (),
               (const, override));
--- chrome/browser/privacy_sandbox/privacy_sandbox_service_impl.cc
+++ chrome/browser/privacy_sandbox/privacy_sandbox_service_impl.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/webui_url_constants.h"
 #include "chromeos/components/mgs/managed_guest_session_utils.h"
 #include "components/browsing_topics/browsing_topics_service.h"
+#include "components/browsing_topics/common/semantic_tree.h"
 #include "components/content_settings/core/browser/cookie_settings.h"
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings_types.h"
@@ -860,6 +861,31 @@ PrivacySandboxServiceImpl::GetBlockedTopics() const {
 
   SortAndDeduplicateTopicsForDisplay(blocked_topics);
   return blocked_topics;
+}
+
+std::vector<privacy_sandbox::CanonicalTopic>
+PrivacySandboxServiceImpl::GetFirstLevelTopics() const {
+  static const base::NoDestructor<std::vector<privacy_sandbox::CanonicalTopic>>
+      kFirstLevelTopics([]() -> std::vector<privacy_sandbox::CanonicalTopic> {
+        browsing_topics::SemanticTree semantic_tree;
+
+        auto topics = semantic_tree.GetFirstLevelTopicsInCurrentTaxonomy();
+        std::vector<privacy_sandbox::CanonicalTopic> first_level_topics;
+        first_level_topics.reserve(topics.size());
+        std::transform(
+            topics.begin(), topics.end(),
+            std::back_inserter(first_level_topics),
+            [&](const browsing_topics::Topic& topic) {
+              return privacy_sandbox::CanonicalTopic(
+                  topic, blink::features::kBrowsingTopicsTaxonomyVersion.Get());
+            });
+
+        SortAndDeduplicateTopicsForDisplay(first_level_topics);
+
+        return first_level_topics;
+      }());
+
+  return *kFirstLevelTopics;
 }
 
 void PrivacySandboxServiceImpl::SetTopicAllowed(
