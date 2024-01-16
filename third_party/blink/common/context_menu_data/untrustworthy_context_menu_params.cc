bd87069b631389598c80913393aa2657161d3a62
Jeremy Roman <jbroman@chromium.org>
2024-01-16 15:03:05 +0000

    Initialize UntrustworthyContextMenuParams::is_image_media_plugin_document.
    In the original CL:
      https://chromium-review.googlesource.com/c/chromium/src/+/5181634
    
    This was initialized in only two of the three structs it was plumbed
    through. UBsan thankfully caught the last.
    
    Change-Id: I4437fe15420a612ac806a5158d2d2a40e6151541
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5190338
    Auto-Submit: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247510}

--- third_party/blink/common/context_menu_data/untrustworthy_context_menu_params.cc
+++ third_party/blink/common/context_menu_data/untrustworthy_context_menu_params.cc
@@ -15,6 +15,7 @@ UntrustworthyContextMenuParams::UntrustworthyContextMenuParams()
       x(0),
       y(0),
       has_image_contents(false),
+      is_image_media_plugin_document(false),
       media_flags(0),
       spellcheck_enabled(false),
       is_editable(false),
