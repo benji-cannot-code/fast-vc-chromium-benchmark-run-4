2869bf4368bb74b3c6aec13f7c07e1d8e4c0ddb8
W. James MacLean <wjmaclean@chromium.org>
2024-01-16 08:49:29 +0000

    Remove unused include.
    None of the resources in the removed .h file appear to be in used in
    the file including it.
    
    Bug: n/a
    Change-Id: I3a800b5a3dbf4f05adb94bf5532ffa81347b2d6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5199086
    Reviewed-by: W. James Maclean <wjmaclean@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247403}

--- chrome/browser/extensions/chrome_content_browser_client_extensions_part.cc
+++ chrome/browser/extensions/chrome_content_browser_client_extensions_part.cc
@@ -72,7 +72,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension_urls.h"
 #include "extensions/common/manifest_constants.h"
 #include "extensions/common/manifest_handlers/background_info.h"
-#include "extensions/common/manifest_handlers/web_accessible_resources_info.h"
 #include "extensions/common/mojom/manifest.mojom-shared.h"
 #include "extensions/common/permissions/permissions_data.h"
 #include "extensions/common/switches.h"
