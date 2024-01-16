697a6e6950760771ced29f1e8d8a434e7ebfbad7
Ce Chen <cch@chromium.org>
2024-01-16 21:34:01 +0000

    [omnibox] fix bug on device head results are not selected correctly.
    The fix is to correctly set the score of a non-leaf candidate to the maximum score in its sub tree.
    
    After this fix: http://screenshot/327YzdNQsWwb7kn
    
    Bug: 1506547
    Change-Id: I80b50e6904b676f254f66411ca107dc71d1de398
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5105448
    Commit-Queue: C Ch <cch@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247745}

--- components/omnibox/browser/on_device_head_model.cc
+++ components/omnibox/browser/on_device_head_model.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
+#include "components/omnibox/browser/omnibox_field_trial.h"
 
 namespace {
 // The offset of the root node for the tree. The first two bytes is reserved to
@@ -32,7 +33,7 @@ struct MatchCandidate {
   bool is_complete_suggestion;
 
   // If is_complete_suggestion is true, this is the score for the suggestion;
-  // Otherwise it will be set as max_score_as_root of the node.
+  // Otherwise it will be set as the maximum score for its sub tree.
   uint32_t score;
 
   // The address of the node in the model file. It is not required if
@@ -294,10 +295,20 @@ bool ReadNextChild(OnDeviceModelParams* params, MatchCandidate* candidate) {
       candidate->score = score_or_address;
       candidate->is_complete_suggestion = true;
     } else {
-      // For non leaf child, score has been set as the max_score_as_root
-      // found at the beginning of the current node.
       candidate->address = score_or_address;
       candidate->is_complete_suggestion = false;
+
+      // TODO(crbug.com/1506547): remove this guard after evaluating the fix.
+      if (OmniboxFieldTrial::ShouldApplyOnDeviceHeadModelSelectionFix()) {
+        MatchCandidate unused_candidate;
+        uint32_t address = params->GetModelFileStream()->tellg();
+        uint32_t max_score = ReadMaxScoreAsRoot(
+            params, score_or_address, &unused_candidate, &is_successful);
+        params->GetModelFileStream()->seekg(address);
+        if (is_successful) {
+          candidate->score = max_score;
+        }
+      }
     }
     delete[] last_block;
   }
