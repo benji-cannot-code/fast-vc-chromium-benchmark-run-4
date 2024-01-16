de67a1ee6549def975c4d0ee9ab2f1a60b18ff50
Eliot Courtney <edcourtney@google.com>
2024-01-16 01:17:27 +0000

    Mark the shelf as opaque for occlusion
    On display scale change, exo windows are briefly the wrong size due to
    async configuration update. If they are maximized and the display is
    zoomed in, if the shelf cannot occlude what is below it, exo windows can
    have their occlusion state changed temporarily. For example, if the top
    window is resized to be smaller, the below maximized window will be
    unoccluded where the shelf is until it is also resized.
    
    So, let the shelf occlude things below it.
    
    Bug: 1399478
    Test: tested in a follow up CL as part of occlusion work
    Change-Id: I6da60ae41604e9ec9f8922419e942f65f3145bf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5119061
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Auto-Submit: Eliot Courtney <edcourtney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247330}

--- components/exo/wayland/zaura_shell_unittest.cc
+++ components/exo/wayland/zaura_shell_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/session/session_controller_impl.h"
+#include "ash/shelf/shelf.h"
 #include "ash/shell.h"
 #include "ash/wm/desks/desks_util.h"
 #include "ash/wm/window_util.h"
@@ -379,10 +380,13 @@ TEST_F(ZAuraSurfaceTest, OcclusionIncludesOffScreenArea) {
   std::unique_ptr<Buffer> buffer(
       new Buffer(exo_test_helper()->CreateGpuMemoryBuffer(buffer_size)));
   // This is scaled by 1.5 - set the bounds to (-60, 75, 120, 150) in screen
-  // coordinates so 75% of it is outside of the 100x100 screen.
+  // coordinates so 75% of it is outside of the screen.
   surface().window()->SetBounds(gfx::Rect(-40, 50, 80, 100));
   surface().Attach(buffer.get());
   surface().Commit();
+
+  ash::Shelf::ForWindow(surface().window())
+      ->SetAutoHideBehavior(ash::ShelfAutoHideBehavior::kAlwaysHidden);
 
   surface().OnWindowOcclusionChanged(aura::Window::OcclusionState::UNKNOWN,
                                      aura::Window::OcclusionState::VISIBLE);
