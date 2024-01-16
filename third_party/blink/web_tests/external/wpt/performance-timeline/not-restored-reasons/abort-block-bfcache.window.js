4b6220d0a16caf1531be6c35d7113c2d4c51db23
rubberyuzu <yuzus@chromium.org>
2024-01-16 07:47:13 +0000

    [bfcache] Add wpt for parser abort and bfcache
    This CL adds a WPT that aborts parser in the middle of loading and tests if the page is restored from bfcache, and checks the reason.
    
    Due to crbug.com/1517496 , I needed to add a failing expectation for linux-chrome. This is now being investigated and will be removed in the future.
    Bug:1517496, 1518720
    
    Change-Id: Ia20c5d1f673ffc69fc71c76e1f5c3022dd5c80cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5109299
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247395}

---
+++ third_party/blink/web_tests/external/wpt/performance-timeline/not-restored-reasons/abort-block-bfcache.window.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title=Aborting a parser should block bfcache
+// META: script=/resources/testharness.js
+// META: script=/resources/testharnessreport.js
+// META: timeout=long
+
+
+async_test(t => {
+  if (!sessionStorage.getItem("pageVisited")) {
+    // This is the first time loading the page.
+    sessionStorage.setItem("pageVisited", 1);
+    t.step_timeout(() => {
+        // Go to another page and instantly come back to this page.
+        location.href = new URL("../resources/going-back.html", window.location);
+    }, 0);
+    // Abort parsing in the middle of loading the page.
+    window.stop();
+  } else {
+    const nrr = performance.getEntriesByType('navigation')[0].notRestoredReasons;
+    assert_equals(nrr.reasons.length, 1);
+    assert_equals(nrr.reasons[0], "parser-aborted");
+    t.done();
+  }
+}, "aborting a parser should block bfcache.");
