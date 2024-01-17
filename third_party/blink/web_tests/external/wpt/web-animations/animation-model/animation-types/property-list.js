d97011a0f849780e2e4832cdb416eaf2e8814f70
Koji Ishii <kojii@chromium.org>
2024-01-17 04:25:56 +0000

    [text-spacing-trim] Enable for testing
    Bug: 1463891
    Change-Id: I826f37b738ea2576c0960ee81248913da5f7629c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5186514
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247955}

--- third_party/blink/web_tests/external/wpt/web-animations/animation-model/animation-types/property-list.js
+++ third_party/blink/web_tests/external/wpt/web-animations/animation-model/animation-types/property-list.js
@@ -1287,7 +1287,7 @@ const gCSSProperties2 = {
     ]
   },
   'text-autospace': {
-    // https://drafts.csswg.org/css-text-4/#text-spacing-property
+    // https://drafts.csswg.org/css-text-4/#text-autospace-property
     types: [
       { type: 'discrete', options: [ [ 'normal', 'no-autospace' ] ] }
     ]
@@ -1357,9 +1357,9 @@ const gCSSProperties2 = {
     }
   },
   'text-spacing-trim': {
-    // https://svgwg.org/svg2-draft/painting.html#TextRenderingProperty
+    // https://drafts.csswg.org/css-text-4/#text-spacing-trim-property
     types: [
-      { type: 'discrete', options: [ [ 'auto', 'space-all' ] ] }
+      { type: 'discrete', options: [ [ 'normal', 'space-all' ] ] }
     ]
   },
   'text-transform': {
