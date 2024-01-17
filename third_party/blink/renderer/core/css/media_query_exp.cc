89e7d98bce394a070a7dc653ba3b9be85c4cb198
Luke Warlow <lwarlow@igalia.com>
2024-01-17 01:54:14 +0000

    Retire PrefersReducedTransparency flag
    Bug: 1424879
    Change-Id: I8ad10be29c943cc9387aa6f552c375a7a538f402
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5202465
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247899}

--- third_party/blink/renderer/core/css/media_query_exp.cc
+++ third_party/blink/renderer/core/css/media_query_exp.cc
@@ -132,9 +132,8 @@ static inline bool FeatureWithValidIdent(const String& media_feature,
     return ident == CSSValueID::kNoPreference || ident == CSSValueID::kReduce;
   }
 
-  if (RuntimeEnabledFeatures::PrefersReducedTransparencyEnabled() &&
-      media_feature ==
-          media_feature_names::kPrefersReducedTransparencyMediaFeature) {
+  if (media_feature ==
+      media_feature_names::kPrefersReducedTransparencyMediaFeature) {
     return ident == CSSValueID::kNoPreference || ident == CSSValueID::kReduce;
   }
 
