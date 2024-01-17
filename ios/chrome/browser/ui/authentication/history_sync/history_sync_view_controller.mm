a66da9a21d9218490d50774e3f3031cfffe309f8
Jérôme Lebel <jlebel@chromium.org>
2024-01-17 09:08:36 +0000

    [iOS] Add more button for history sync opt-in dialog
    HistorySyncViewController needs to have a more button for small screens.
    
    Fixed: 1475212
    Change-Id: I8aec6b5b9e26924c70f58816fe5e89ee2299882e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5201199
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248028}

--- ios/chrome/browser/ui/authentication/history_sync/history_sync_view_controller.mm
+++ ios/chrome/browser/ui/authentication/history_sync/history_sync_view_controller.mm
@@ -17,6 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)viewDidLoad {
   self.view.accessibilityIdentifier = kHistorySyncViewAccessibilityIdentifier;
   self.shouldHideBanner = YES;
+  self.scrollToEndMandatory = YES;
+  self.readMoreString =
+      l10n_util::GetNSString(IDS_IOS_FIRST_RUN_SCREEN_READ_MORE);
   self.headerImageType = PromoStyleImageType::kAvatar;
   self.headerBackgroundImage =
       [UIImage imageNamed:@"history_sync_opt_in_background"];
