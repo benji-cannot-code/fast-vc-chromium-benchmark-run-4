0f4db6b6837c7dc1a7be8e7aa8ea845d8e7d235a
Sky Malice <skym@chromium.org>
2024-01-17 22:38:29 +0000

    Fix color of Hub toolbar tab count icon.
    The TabSwitcherDrawable is constructed to watch for tint changes in
    its base class TintedDrawable. However plain Drawable has two other
    tint methods:
    * setTint(int)
    * setTIntList(ColorStateList)
    
    This is particularly tricky, because depending on the api level,
    support libraries will call into different paths. The current code
    seems to be assuming its bespoke TintedDrawable is the only game in
    town, which is definitely not the case anymore, really api 21+.
    
    This change tries to fix this class of problem by updating the paint
    instead in setColorFilter(ColorFilter), which all paths currently
    share. The other approach would be to do this on every draw frame, but
    that is likely less efficient.
    
    Bug: 1507839
    Change-Id: I61a63e6a5a4c99b338d503f5d6109d42c83b16c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5207239
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248418}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/TabSwitcherDrawable.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/TabSwitcherDrawable.java
@@ -6,10 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.toolbar;
 
 import android.content.Context;
-import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.ColorFilter;
 import android.graphics.Paint.Align;
 import android.graphics.Rect;
 import android.graphics.Typeface;
@@ -126,9 +126,13 @@ public class TabSwitcherDrawable extends TintedDrawable {
         return mTint.getColorForState(getState(), 0);
     }
 
-    @Override
-    public void setTint(ColorStateList tint) {
-        super.setTint(tint);
+    private void updatePaint() {
         if (mTextPaint != null) mTextPaint.setColor(getColorForState());
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter colorFilter) {
+        super.setColorFilter(colorFilter);
+        updatePaint();
     }
 }
