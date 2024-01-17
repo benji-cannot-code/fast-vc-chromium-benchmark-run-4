6e8e709c8b3b352216ae006fe07d66fb10196f6b
Kevin Radtke <kradtke@chromium.org>
2024-01-17 00:31:38 +0000

    nudge-migration: Add `SystemNudgeMigration` chrome flag
    This feature can now be accessed through
    chrome://flags#enable-system-nudge-migration
    
    Bug: None
    Change-Id: I3b6c33afde565ceb2886643e8e44497152e61df8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5195564
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247852}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3631,6 +3631,9 @@ extern const char kEnableNotificationImageDragDescription[];
 extern const char kEnableNotifierCollisionName[];
 extern const char kEnableNotifierCollisionDescription[];
 
+extern const char kEnableSystemNudgeMigrationName[];
+extern const char kEnableSystemNudgeMigrationDescription[];
+
 extern const char kEnableOAuthIppName[];
 extern const char kEnableOAuthIppDescription[];
 
