6eb0924dfbb84dfc297a8e29708777e04c8197f9
Weizhong Xia <weizhong@google.com>
2024-01-17 22:34:03 +0000

    Import wpt@11652c978b897c7b9398942c450f517621634d97
    Using wpt-import in Chromium 77cbd9413c7ecc0c04a4dee48d5dbd5272244e1e.
    With Chromium commits locally applied on WPT:
    572074cb06 "Remove empty closed paths"
    
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    andruud@chromium.org:
      external/wpt/css/css-properties-values-api
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I78724ab1f5a4c8f5a6a3156ec9fce93333a74aa7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5200958
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1248412}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/storage/set_cookie/cookie_path_invalid.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/storage/set_cookie/cookie_path_invalid.py
@@ -1,14 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import pytest
-from webdriver.bidi.modules.network import NetworkStringValue
-from webdriver.bidi.modules.storage import PartialCookie, BrowsingContextPartitionDescriptor
-from .. import assert_cookie_is_set
+from .. import create_cookie
+from webdriver.bidi.modules.storage import BrowsingContextPartitionDescriptor
 import webdriver.bidi.error as error
 
 pytestmark = pytest.mark.asyncio
-
-COOKIE_NAME = 'SOME_COOKIE_NAME'
-COOKIE_VALUE = 'SOME_COOKIE_VALUE'
 
 
 @pytest.mark.parametrize(
@@ -26,11 +22,5 @@ async def test_path_invalid_values(bidi_session, top_context, test_page, origin,
 
     with pytest.raises(error.UnableToSetCookieException):
         await bidi_session.storage.set_cookie(
-            cookie=PartialCookie(
-                name=COOKIE_NAME,
-                value=NetworkStringValue(COOKIE_VALUE),
-                domain=domain_value(),
-                path=path,
-                secure=True
-            ),
+            cookie=create_cookie(domain=domain_value(), path=path),
             partition=partition)
