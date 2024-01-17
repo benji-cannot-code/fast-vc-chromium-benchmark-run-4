ea642d44230464ac4c3ccfd10b061108006a40be
John Lee <johntlee@chromium.org>
2024-01-17 22:22:34 +0000

    Compose: Add generic animation for non-first-run dialogs
    Adds a simple animation of fade in and slide in for any dialog that
    is not hidden and is not the first-run dialog.
    
    Bug: b:301367640
    Change-Id: I209dc36808e074114876c6e4a250b9fc474610db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5203642
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248396}

--- chrome/browser/resources/compose/app.ts
+++ chrome/browser/resources/compose/app.ts
@@ -289,10 +289,6 @@ export class ComposeAppElement extends ComposeAppElementBase {
       // the FRE causes the dialog to show the MSBB state if MSBB is not
       // enabled, and the main app state otherwise.
       this.showFirstRunDialog_ = !initialState.freComplete;
-      if (this.showFirstRunDialog_) {
-        this.animator_.transitionToFirstRun();
-      }
-
       this.showMSBBDialog_ =
           initialState.freComplete && !initialState.msbbState;
       this.shouldShowMSBBDialog_ = !initialState.msbbState;
@@ -326,6 +322,13 @@ export class ComposeAppElement extends ComposeAppElementBase {
           this.editedInput_ = appState.editedInput!;
         }
       }
+
+      if (this.showFirstRunDialog_) {
+        this.animator_.transitionToFirstRun();
+      } else {
+        this.animator_.transitionInDialog();
+      }
+
       // Wait for one timeout to flush Polymer tasks, then wait for the next
       // render.
       setTimeout(() => {
