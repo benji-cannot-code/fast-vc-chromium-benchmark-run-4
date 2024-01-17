c1744bc53736f4d843e20123b28d1120ddadffe0
Sky Malice <skym@chromium.org>
2024-01-17 17:23:50 +0000

    Update toolbar icons on color scheme change.
    Bug: 1507839
    Change-Id: I738e2140ff8cc4386760fe88f9cd11d04a85d96e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5200759
    Auto-Submit: Sky Malice <skym@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248188}

--- chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubColors.java
+++ chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubColors.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.hub;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.graphics.Color;
 
 import androidx.annotation.ColorInt;
@@ -16,12 +17,17 @@ import org.chromium.components.browser_ui.styles.SemanticColorUtils;
 
 /** Util class to handle various color operations shared between hub classes. */
 public final class HubColors {
+    private static final int[][] SELECTED_AND_NORMAL_STATES =
+            new int[][] {new int[] {android.R.attr.state_selected}, new int[] {}};
+
     private HubColors() {}
 
+    /** Returns the color scheme from a pane with a fallback for null. */
     public static @HubColorScheme int getColorSchemeSafe(@Nullable Pane pane) {
         return pane == null ? HubColorScheme.DEFAULT : pane.getColorScheme();
     }
 
+    /** Returns the background color generic surfaces should use per the given color scheme. */
     public static @ColorInt int getBackgroundColor(
             Context context, @HubColorScheme int colorScheme) {
         switch (colorScheme) {
@@ -33,5 +39,39 @@ public final class HubColors {
                 assert false;
                 return Color.TRANSPARENT;
         }
+    }
+
+    /** Returns the color most icons should use per the given color scheme. */
+    public static @ColorInt int getIconColor(Context context, @HubColorScheme int colorScheme) {
+        switch (colorScheme) {
+            case HubColorScheme.DEFAULT:
+                return SemanticColorUtils.getDefaultIconColor(context);
+            case HubColorScheme.INCOGNITO:
+                return ContextCompat.getColor(context, R.color.white_alpha_70);
+            default:
+                assert false;
+                return Color.TRANSPARENT;
+        }
+    }
+
+    /** Returns the color selected icons should use per the given color scheme. */
+    public static @ColorInt int getSelectedIconColor(
+            Context context, @HubColorScheme int colorScheme) {
+        switch (colorScheme) {
+            case HubColorScheme.DEFAULT:
+                return SemanticColorUtils.getDefaultIconColorAccent1(context);
+            case HubColorScheme.INCOGNITO:
+                return ContextCompat.getColor(context, R.color.default_control_color_active_dark);
+            default:
+                assert false;
+                return Color.TRANSPARENT;
+        }
+    }
+
+    /** Convenience method to make a selectable {@link ColorStateList} from two input colors. */
+    public static ColorStateList getSelectableIconList(
+            @ColorInt int selectedColor, @ColorInt int normalColor) {
+        int[] colors = new int[] {selectedColor, normalColor};
+        return new ColorStateList(SELECTED_AND_NORMAL_STATES, colors);
     }
 }
