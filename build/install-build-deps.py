c19ead37b1e97801ba094d2d52df72444d15d504
Brad Triebwasser <btriebw@chromium.org>
2024-01-17 23:24:50 +0000

    Reland "Add xorg core and video dummy dependencies."
    This reverts commit 503656f463b639483d15cf6d4a2254e388e58258.
    
    Reason for revert: Seems to not be the cause of the slowdown in crbug.com/1513713#c26
    
    Original change's description:
    > Revert "Add xorg core and video dummy dependencies."
    >
    > This reverts commit b4e42a4384ded4f44021f76271852cc269868b1f.
    >
    > Reason for revert: crbug.com/1513713. Unsure if this is the cause, but given the experimental nature of this, I'll revert for longer so we can observe it tests go back to normal.
    >
    > Original change's description:
    > > Add xorg core and video dummy dependencies.
    > >
    > > Follow-up to revert crrev.com/c/5166237. Adds the xcore core binaries
    > > and dummy driver instead of the entire xorg package which installs lots
    > > of video / input drivers.
    > >
    > > Bug: 1417069, 1513713
    > > Change-Id: I3075508e470c1ed11628c13f2bfb1250c4fc26c5
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5171264
    > > Commit-Queue: Brad Triebwasser <btriebw@chromium.org>
    > > Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1243456}
    >
    > Bug: 1417069, 1513713
    > Change-Id: I7e125619942855581c41e4b841c7b831595b7a1f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5180983
    > Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Brad Triebwasser <btriebw@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1244837}
    
    Bug: 1417069, 1513713
    Change-Id: I60d3ebc6b9f69971d2bb9022c9abacef09dbc09a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5202221
    Reviewed-by: Alan Zhao <ayzhao@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Brad Triebwasser <btriebw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248450}

--- build/install-build-deps.py
+++ build/install-build-deps.py
@@ -375,6 +375,8 @@ def lib_list():
       "libxrender1",
       "libxtst6",
       "x11-utils",
+      "xserver-xorg-core",  # TODO(crbug.com/1417069): Experimental.
+      "xserver-xorg-video-dummy",  # TODO(crbug.com/1417069): Experimental.
       "xvfb",
       "zlib1g",
   ]
