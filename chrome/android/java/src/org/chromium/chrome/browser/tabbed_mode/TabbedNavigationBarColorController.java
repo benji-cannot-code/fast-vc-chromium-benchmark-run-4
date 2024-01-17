6664cdc4c063c9e46e37815c8cf1e5abbf2bb0e3
Sky Malice <skym@chromium.org>
2024-01-17 23:42:42 +0000

    Remove unused mOverviewModeHiding.
    Change-Id: Iab4cae9b4e91256e21ec43cc668d5ba561e991a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5208230
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248464}

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedNavigationBarColorController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedNavigationBarColorController.java
@@ -54,7 +54,6 @@ class TabbedNavigationBarColorController {
 
     private @ColorInt int mNavigationBarColor;
     private boolean mForceDarkNavigationBarColor;
-    private boolean mOverviewModeHiding;
     private boolean mIsInFullscreen;
     private float mNavigationBarScrimFraction;
 
@@ -144,20 +143,13 @@ class TabbedNavigationBarColorController {
                         LayoutType.TAB_SWITCHER,
                         new LayoutStateObserver() {
                             @Override
-                            public void onStartedShowing(int layoutType) {
-                                mOverviewModeHiding = false;
+                            public void onStartedShowing(@LayoutType int layoutType) {
                                 updateNavigationBarColor();
                             }
 
                             @Override
-                            public void onStartedHiding(int layoutType) {
-                                mOverviewModeHiding = true;
+                            public void onStartedHiding(@LayoutType int layoutType) {
                                 updateNavigationBarColor();
-                            }
-
-                            @Override
-                            public void onFinishedHiding(int layoutType) {
-                                mOverviewModeHiding = false;
                             }
                         });
         mLayoutManager.addObserver(mLayoutStateObserver);
