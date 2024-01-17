846af79e8f8013249ec5faceaa18a8cae64c4849
mark a. foltz <mfoltz@chromium.org>
2024-01-17 23:13:58 +0000

    [Cast Certificate] Add utility function for printing certificate chains.
    When passed the commandline flag --cast-log-certificate-chain, Chrome
    will log the Cast certificate chain as PEM to VLOG(3).
    
    Change-Id: I753cdf51214736ead198b2422a08614ea874f9c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5207740
    Reviewed-by: Ryan Chung <ryanchung@chromium.org>
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248435}

---
+++ components/media_router/common/providers/cast/certificate/cast_cert_printer.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/media_router/common/providers/cast/certificate/cast_cert_printer.h"
+
+#include <string>
+#include <vector>
+
+#include "base/strings/strcat.h"
+#include "net/cert/x509_certificate.h"
+
+namespace cast_certificate {
+
+std::string CastCertificateChainAsPEM(const std::vector<std::string>& certs) {
+  std::string result;
+  std::string pem_encoded;
+  for (auto cert : certs) {
+    bool success =
+        net::X509Certificate::GetPEMEncodedFromDER(cert, &pem_encoded);
+    if (success) {
+      base::StrAppend(&result, {pem_encoded});
+    } else {
+      base::StrAppend(&result, {"*** ERROR DECODING CERTIFICATE ***"});
+    }
+  }
+  return result;
+}
+
+}  // namespace cast_certificate
--- components/media_router/common/providers/cast/certificate/cast_cert_validator.cc
+++ components/media_router/common/providers/cast/certificate/cast_cert_validator.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
+#include "components/media_router/common/providers/cast/certificate/cast_cert_printer.h"
 #include "components/media_router/common/providers/cast/certificate/cast_crl.h"
 #include "net/cert/time_conversions.h"
 #include "net/cert/x509_util.h"
@@ -361,6 +362,12 @@ CastCertError VerifyDeviceCertUsingCustomTrustStore(
     const CastCRL* fallback_crl,
     CRLPolicy crl_policy,
     bssl::TrustStore* trust_store) {
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kCastLogDeviceCertChain)) {
+    VLOG(3) << "Cast Cert Chain for validation:\n"
+            << CastCertificateChainAsPEM(certs);
+  }
+
   if (!trust_store)
     return VerifyDeviceCert(certs, time, context, policy, crl, fallback_crl,
                             crl_policy);
