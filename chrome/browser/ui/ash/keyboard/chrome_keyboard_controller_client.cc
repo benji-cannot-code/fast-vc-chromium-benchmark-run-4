d45036fdb2c9bfadc274bf51907826068f6117a3
Mark Rowe <markrowe@chromium.org>
2024-01-17 03:37:33 +0000

    ChromeKeyboardControllerClient: Fix dangling reference to PrefService
    Use a unique_ptr for PrefChangeRegistrar to ensure that it does not
    outlive the PrefService that it references.
    
    Bug: 1512677
    Change-Id: I5439fb18631ddfcafc27b4168610f5b8be7f3d42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5155331
    Auto-Submit: Mark Rowe <markrowe@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247940}

--- chrome/browser/ui/ash/keyboard/chrome_keyboard_controller_client.cc
+++ chrome/browser/ui/ash/keyboard/chrome_keyboard_controller_client.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/ash/keyboard/chrome_keyboard_controller_client.h"
 
+#include <memory>
 #include <utility>
 #include <vector>
 
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/ash/keyboard/chrome_keyboard_web_contents.h"
 #include "chrome/common/pref_names.h"
+#include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_service.h"
 #include "components/session_manager/core/session_manager.h"
 #include "content/public/browser/web_contents.h"
@@ -122,7 +124,7 @@ void ChromeKeyboardControllerClient::Shutdown() {
 
   if (session_manager::SessionManager::Get())
     session_manager::SessionManager::Get()->RemoveObserver(this);
-  pref_change_registrar_.RemoveAll();
+  pref_change_registrar_.reset();
 
   if (keyboard::KeyboardUIController::HasInstance()) {
     // In classic Ash, keyboard::KeyboardController owns ChromeKeyboardUI which
@@ -403,21 +405,22 @@ void ChromeKeyboardControllerClient::OnSessionStateChanged() {
           ash::features::kTouchVirtualKeyboardPolicyListenPrefsAtLogin)) {
     // We need to listen for pref changes even in login screen to control the
     // virtual keyboard behavior on the login screen.
-    pref_change_registrar_.RemoveAll();
+    pref_change_registrar_.reset();
   } else {
     if (!session_manager::SessionManager::Get()->IsSessionStarted()) {
       // Reset the registrar so that prefs are re-registered after a crash.
-      pref_change_registrar_.RemoveAll();
+      pref_change_registrar_.reset();
       return;
     }
-    if (!pref_change_registrar_.IsEmpty()) {
+    if (pref_change_registrar_) {
       return;
     }
   }
 
   Profile* profile = ProfileManager::GetPrimaryUserProfile();
-  pref_change_registrar_.Init(profile->GetPrefs());
-  pref_change_registrar_.Add(
+  pref_change_registrar_ = std::make_unique<PrefChangeRegistrar>();
+  pref_change_registrar_->Init(profile->GetPrefs());
+  pref_change_registrar_->Add(
       prefs::kTouchVirtualKeyboardEnabled,
       base::BindRepeating(
           &ChromeKeyboardControllerClient::SetVirtualKeyboardBehaviorFromPrefs,
@@ -427,7 +430,7 @@ void ChromeKeyboardControllerClient::OnSessionStateChanged() {
 
 void ChromeKeyboardControllerClient::SetVirtualKeyboardBehaviorFromPrefs() {
   using keyboard::KeyboardEnableFlag;
-  const PrefService* service = pref_change_registrar_.prefs();
+  const PrefService* service = pref_change_registrar_->prefs();
   if (service->HasPrefPath(prefs::kTouchVirtualKeyboardEnabled)) {
     // Since these flags are mutually exclusive, setting one clears the other.
     SetEnableFlag(service->GetBoolean(prefs::kTouchVirtualKeyboardEnabled)
