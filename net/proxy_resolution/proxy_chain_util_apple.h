eba3a7f52c9e343da42a752f6b45121625238347
Tsuyoshi Horo <horo@chromium.org>
2024-01-17 23:33:40 +0000

    Reland "Rename ProxyChain and ProxyResolver Apple classes"
    This reverts commit a7a7732586d82b8b2ca2bb5b5c0254d3f6eae9b6.
    
    Reason for revert: Reverted https://chromium-review.googlesource.com/c/chromium/src/+/5190661.
    
    Original change's description:
    > Revert "Rename ProxyChain and ProxyResolver Apple classes"
    >
    > This reverts commit 782da25142cbbe236eafa5d216911072d34655f0.
    >
    > Reason for revert: Blocker for reverting https://chromium-review.googlesource.com/c/chromium/src/+/5190661
    >
    > Original change's description:
    > > Rename ProxyChain and ProxyResolver Apple classes
    > >
    > > ProxyChain and ProxyResolver are used both by macOS and iOS, so this
    > > follows the naming convention to change to something more suggestive.
    > >
    > > Bug: None
    > > Change-Id: I1f6bb242d22e5fe57bc036d6504a6ddcca0e4164
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5198057
    > > Reviewed-by: Adam Langley <agl@chromium.org>
    > > Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    > > Commit-Queue: Tiago Vignatti <tvignatti@igalia.com>
    > > Cr-Commit-Position: refs/heads/main@{#1247698}
    >
    > Bug: None
    > Change-Id: Ie730f53e449617aec63c1983ee3f4692ba6b3e16
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5202120
    > Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Tsuyoshi Horo <horo@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1247868}
    
    Bug: None
    Change-Id: I4940dda287c5cfea6215152c6c5c1e9d8a8dab95
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5202538
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248457}

--- net/proxy_resolution/proxy_chain_util_apple.h
+++ net/proxy_resolution/proxy_chain_util_apple.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NET_PROXY_RESOLUTION_PROXY_CHAIN_UTIL_MAC_H_
-#define NET_PROXY_RESOLUTION_PROXY_CHAIN_UTIL_MAC_H_
+#ifndef NET_PROXY_RESOLUTION_PROXY_CHAIN_UTIL_APPLE_H_
+#define NET_PROXY_RESOLUTION_PROXY_CHAIN_UTIL_APPLE_H_
 
 #include <CoreFoundation/CoreFoundation.h>
 
@@ -24,4 +24,4 @@ NET_EXPORT ProxyChain ProxyDictionaryToProxyChain(CFStringRef proxy_type,
 
 }  // namespace net
 
-#endif  // NET_PROXY_RESOLUTION_PROXY_CHAIN_UTIL_MAC_H_
+#endif  // NET_PROXY_RESOLUTION_PROXY_CHAIN_UTIL_APPLE_H_
--- net/proxy_resolution/proxy_config_service_mac.cc
+++ net/proxy_resolution/proxy_config_service_mac.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "base/task/sequenced_task_runner.h"
 #include "net/base/net_errors.h"
-#include "net/proxy_resolution/proxy_chain_util_mac.h"
+#include "net/proxy_resolution/proxy_chain_util_apple.h"
 #include "net/proxy_resolution/proxy_info.h"
 
 namespace net {
