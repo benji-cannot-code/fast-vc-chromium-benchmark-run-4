8cac0aa1bd46902b89220fdb99a6a0c3eda7e39f
Lei Zhang <thestig@chromium.org>
2024-01-17 18:14:21 +0000

    Make PrintPreviewDialogController not ref-counted
    This class does not benefit from being ref-counted, so use
    std::unique_ptr to manage its lifetime instead. Along the way, better
    document when to use the PrintPreviewDialogController ctor versus
    GetInstance().
    
    Change-Id: I7b6933c7657a4e402397985a815c202a787cf390
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5202615
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248230}

--- chrome/browser/browser_process_impl.cc
+++ chrome/browser/browser_process_impl.cc
@@ -1340,7 +1340,7 @@ void BrowserProcessImpl::CreatePrintPreviewDialogController() {
 #if BUILDFLAG(ENABLE_PRINT_PREVIEW)
   DCHECK(!print_preview_dialog_controller_);
   print_preview_dialog_controller_ =
-      base::MakeRefCounted<printing::PrintPreviewDialogController>();
+      std::make_unique<printing::PrintPreviewDialogController>();
 #else
   NOTIMPLEMENTED();
 #endif
