e0e0d6e72ddb6927eaa3a7f3d28c357d179a9bda
Jacob Francis <francisjp@google.com>
2024-01-17 19:25:13 +0000

    [Read Anything] Add Links toggle button
    - This CL adds the required resources for creating the new button.
    - It Adds the new toggle button.
    - This CL does not implement the buttons actual behavior.
    
    AX-RelNotes: N/A
    
    Low-Coverage-Reason: TRIVIAL_CHANGE untested code has no implementation
    Bug: 1442693
    Change-Id: Id9b0d01b730b64c59bfd44bc29b0794a81dc937e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5123856
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Jacob Francis <francisjp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1248285}

--- chrome/browser/ui/views/side_panel/read_anything/read_anything_button_view.h
+++ chrome/browser/ui/views/side_panel/read_anything/read_anything_button_view.h
@@ -6,9 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_SIDE_PANEL_READ_ANYTHING_READ_ANYTHING_BUTTON_VIEW_H_
 #define CHROME_BROWSER_UI_VIEWS_SIDE_PANEL_READ_ANYTHING_READ_ANYTHING_BUTTON_VIEW_H_
 
-#include "base/functional/callback_forward.h"
+#include <string>
+
 #include "ui/base/metadata/metadata_header_macros.h"
-#include "ui/gfx/image/image_skia.h"
+#include "ui/color/color_id.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/view.h"
 
@@ -23,10 +24,7 @@ class ReadAnythingButtonView : public views::ImageButton {
   METADATA_HEADER(ReadAnythingButtonView, views::ImageButton)
 
  public:
-  ReadAnythingButtonView(const views::ImageButton::PressedCallback callback,
-                         const gfx::VectorIcon& icon,
-                         int icon_size,
-                         SkColor icon_color,
+  ReadAnythingButtonView(views::ImageButton::PressedCallback callback,
                          const std::u16string& tooltip);
   ReadAnythingButtonView(const ReadAnythingButtonView&) = delete;
   ReadAnythingButtonView& operator=(const ReadAnythingButtonView&) = delete;
