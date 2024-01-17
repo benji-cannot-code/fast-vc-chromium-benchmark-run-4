2423a89482ec8d4881640026dbb2474aaa59a07d
S. Ganesh <ganesh@chromium.org>
2024-01-17 20:51:12 +0000

    Add interfaces to library based on attributes in the IDL template
    This CL introduces a `addToLibrary` list attribute in `BEGIN_INTERFACE`
    for IDL templates. `addToLibrary` specifies the interfaces that should
    be listed in the `library` section of the generated IDL. For instance:
    * `"addToLibrary": ["user", "system"]` will add the `IInterfaceUser` and
    the `IInterfaceSystem` to the `library` section.
    * `"addToLibrary": ["", "user", "system"]` will add `IInterface`,
    `IInterfaceUser` and `IInterfaceSystem` to the `library` section.
    * `"addToLibrary": ["user"]` will add only `IInterfaceUser` to the
    `library` section.
    
    This CL reduces the chance for errors in having an interface defined but
    not listed in the `library` section.
    
    Bug: 1496516
    Change-Id: Ibac02ace5883a05a9148cd094086792be434bfde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5203525
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248345}

--- third_party/win_build_output/midl/chrome/updater/app/server/win/x64/updater_idl.h
+++ third_party/win_build_output/midl/chrome/updater/app/server/win/x64/updater_idl.h
@@ -225,20 +225,6 @@ typedef struct UpdaterSystemClass UpdaterSystemClass;
 #endif 	/* __UpdaterSystemClass_FWD_DEFINED__ */
 
 
-#ifndef __IUpdaterUser_FWD_DEFINED__
-#define __IUpdaterUser_FWD_DEFINED__
-typedef interface IUpdaterUser IUpdaterUser;
-
-#endif 	/* __IUpdaterUser_FWD_DEFINED__ */
-
-
-#ifndef __IUpdaterSystem_FWD_DEFINED__
-#define __IUpdaterSystem_FWD_DEFINED__
-typedef interface IUpdaterSystem IUpdaterSystem;
-
-#endif 	/* __IUpdaterSystem_FWD_DEFINED__ */
-
-
 #ifndef __IUpdateStateUser_FWD_DEFINED__
 #define __IUpdateStateUser_FWD_DEFINED__
 typedef interface IUpdateStateUser IUpdateStateUser;
@@ -328,6 +314,20 @@ typedef interface IUpdaterAppStatesCallbackUser IUpdaterAppStatesCallbackUser;
 typedef interface IUpdaterAppStatesCallbackSystem IUpdaterAppStatesCallbackSystem;
 
 #endif 	/* __IUpdaterAppStatesCallbackSystem_FWD_DEFINED__ */
+
+
+#ifndef __IUpdaterUser_FWD_DEFINED__
+#define __IUpdaterUser_FWD_DEFINED__
+typedef interface IUpdaterUser IUpdaterUser;
+
+#endif 	/* __IUpdaterUser_FWD_DEFINED__ */
+
+
+#ifndef __IUpdaterSystem_FWD_DEFINED__
+#define __IUpdaterSystem_FWD_DEFINED__
+typedef interface IUpdaterSystem IUpdaterSystem;
+
+#endif 	/* __IUpdaterSystem_FWD_DEFINED__ */
 
 
 /* header files for imported files */
--- third_party/win_build_output/midl/chrome/updater/app/server/win/x86/updater_legacy_idl.h
+++ third_party/win_build_output/midl/chrome/updater/app/server/win/x86/updater_legacy_idl.h
@@ -497,6 +497,27 @@ typedef interface IPolicyStatusSystem IPolicyStatusSystem;
 #endif 	/* __IPolicyStatusSystem_FWD_DEFINED__ */
 
 
+#ifndef __IPolicyStatusValue_FWD_DEFINED__
+#define __IPolicyStatusValue_FWD_DEFINED__
+typedef interface IPolicyStatusValue IPolicyStatusValue;
+
+#endif 	/* __IPolicyStatusValue_FWD_DEFINED__ */
+
+
+#ifndef __IPolicyStatusValueUser_FWD_DEFINED__
+#define __IPolicyStatusValueUser_FWD_DEFINED__
+typedef interface IPolicyStatusValueUser IPolicyStatusValueUser;
+
+#endif 	/* __IPolicyStatusValueUser_FWD_DEFINED__ */
+
+
+#ifndef __IPolicyStatusValueSystem_FWD_DEFINED__
+#define __IPolicyStatusValueSystem_FWD_DEFINED__
+typedef interface IPolicyStatusValueSystem IPolicyStatusValueSystem;
+
+#endif 	/* __IPolicyStatusValueSystem_FWD_DEFINED__ */
+
+
 #ifndef __IPolicyStatus2_FWD_DEFINED__
 #define __IPolicyStatus2_FWD_DEFINED__
 typedef interface IPolicyStatus2 IPolicyStatus2;
@@ -537,27 +558,6 @@ typedef interface IPolicyStatus3User IPolicyStatus3User;
 typedef interface IPolicyStatus3System IPolicyStatus3System;
 
 #endif 	/* __IPolicyStatus3System_FWD_DEFINED__ */
-
-
-#ifndef __IPolicyStatusValue_FWD_DEFINED__
-#define __IPolicyStatusValue_FWD_DEFINED__
-typedef interface IPolicyStatusValue IPolicyStatusValue;
-
-#endif 	/* __IPolicyStatusValue_FWD_DEFINED__ */
-
-
-#ifndef __IPolicyStatusValueUser_FWD_DEFINED__
-#define __IPolicyStatusValueUser_FWD_DEFINED__
-typedef interface IPolicyStatusValueUser IPolicyStatusValueUser;
-
-#endif 	/* __IPolicyStatusValueUser_FWD_DEFINED__ */
-
-
-#ifndef __IPolicyStatusValueSystem_FWD_DEFINED__
-#define __IPolicyStatusValueSystem_FWD_DEFINED__
-typedef interface IPolicyStatusValueSystem IPolicyStatusValueSystem;
-
-#endif 	/* __IPolicyStatusValueSystem_FWD_DEFINED__ */
 
 
 #ifndef __IProcessLauncher_FWD_DEFINED__
@@ -7913,10 +7913,6 @@ EXTERN_C const IID IID_IProcessLauncher2;
 
 /* library UpdaterLegacyLib */
 /* [helpstring][version][uuid] */ 
-
-
-
-
 
 
 
