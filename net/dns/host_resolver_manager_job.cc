87df53f32016164ecc7188d31e6e9c784c499ff7
Kenichi Ishibashi <bashi@chromium.org>
2024-01-17 06:47:33 +0000

    Remove GetScheme() in HostResoverManager::Job
    Instead, add a helper function to check whether a scheme is `https`
    or `wss`.
    
    Bug: chromium:1518698
    Change-Id: I37e95b222f98b0b85db197f03bb07485b85fadcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5202680
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247994}

--- net/dns/host_resolver_manager_job.cc
+++ net/dns/host_resolver_manager_job.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/public/secure_dns_mode.h"
 #include "net/log/net_log_with_source.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
+#include "url/url_constants.h"
 
 namespace net {
 
@@ -86,15 +87,13 @@ base::Value::Dict NetLogJobAttachParams(const NetLogSource& source,
   return dict;
 }
 
-// Returns empty string if `host` has no known scheme.
-// TODO(bashi): Avoid duplication.
-base::StringPiece GetScheme(
+bool IsSchemeHttpsOrWss(
     const absl::variant<url::SchemeHostPort, std::string>& host) {
-  if (absl::holds_alternative<url::SchemeHostPort>(host)) {
-    return absl::get<url::SchemeHostPort>(host).scheme();
+  if (!absl::holds_alternative<url::SchemeHostPort>(host)) {
+    return false;
   }
-
-  return base::StringPiece();
+  base::StringPiece scheme = absl::get<url::SchemeHostPort>(host).scheme();
+  return scheme == url::kHttpsScheme || scheme == url::kWssScheme;
 }
 
 }  // namespace
@@ -911,8 +910,7 @@ void HostResolverManager::Job::RecordJobHistograms(
         // Skip http- and ws-schemed hosts. Although they query HTTPS records,
         // successful queries are reported as errors, which would skew the
         // metrics.
-        (GetScheme(key_.host) == url::kHttpsScheme ||
-         GetScheme(key_.host) == url::kWssScheme) &&
+        IsSchemeHttpsOrWss(key_.host) &&
         IsGoogleHostWithAlpnH3(HostResolver::GetHostname(key_.host))) {
       bool has_metadata = !results.GetMetadatas().empty();
       base::UmaHistogramExactLinear(
