e7a7beea72d38f0a887e5421ccb6d69787c2e5b2
Jianhui Dai <jianhui.j.dai@intel.com>
2024-01-17 01:32:18 +0000

    MessagePumpEpoll: Enable MessagePumpEpoll by default on Linux/Android
    This CL sets 'g_use_epoll' to TRUE to ensure the MessagePumpEpoll is
    enabled by default on Linux/Android. This eliminates the need to
    manually invoke `InitializeFeatures()` during process startup.
    
    This CL also exports `kMessagePumpEpoll` for unit tests to set correct
    feature state.
    
    Bug: 1243354
    Change-Id: I5a76b4dd0a52db43c724ead00a06ff498ce08718
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5118787
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Jianhui J Dai <jianhui.j.dai@intel.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1247881}

--- base/message_loop/message_pump_libevent.cc
+++ base/message_loop/message_pump_libevent.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/auto_reset.h"
 #include "base/compiler_specific.h"
-#include "base/feature_list.h"
 #include "base/files/file_util.h"
 #include "base/logging.h"
 #include "base/notreached.h"
@@ -40,15 +39,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-namespace {
-
 #if BUILDFLAG(ENABLE_MESSAGE_PUMP_EPOLL)
+namespace {
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_ANDROID)
+bool g_use_epoll = true;
+#else
+// TODO(crbug.com/1243354): Enable by default on chromeos.
 bool g_use_epoll = false;
+#endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_ANDROID)
+}  // namespace
 
 BASE_FEATURE(kMessagePumpEpoll, "MessagePumpEpoll", FEATURE_ENABLED_BY_DEFAULT);
-#endif
-
-}  // namespace
+#endif  // BUILDFLAG(ENABLE_MESSAGE_PUMP_EPOLL)
 
 MessagePumpLibevent::FdWatchController::FdWatchController(
     const Location& from_here)
--- base/message_loop/message_pump_libevent.h
+++ base/message_loop/message_pump_libevent.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_export.h"
 #include "base/compiler_specific.h"
+#include "base/feature_list.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/raw_ptr_exclusion.h"
 #include "base/memory/weak_ptr.h"
@@ -24,6 +25,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 struct event_base;
 struct event;
 namespace base {
+
+#if BUILDFLAG(ENABLE_MESSAGE_PUMP_EPOLL)
+BASE_EXPORT BASE_DECLARE_FEATURE(kMessagePumpEpoll);
+#endif  // BUILDFLAG(ENABLE_MESSAGE_PUMP_EPOLL)
 
 class MessagePumpEpoll;
 
