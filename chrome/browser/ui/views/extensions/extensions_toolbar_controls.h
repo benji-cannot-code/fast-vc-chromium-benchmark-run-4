7d60761319f65153bd5dc5829e3388e5f425f3df
EmiliaPaz <emiliapaz@chromium.org>
2024-01-17 20:46:42 +0000

    [Extensions c2] Move update methods from controls to container
    Move update methods from extensions toolbar controls to extensions
    toolbar container, since buttons were moved to the latter one.
    
    No functionality changed
    
    Bug: 1416603
    Change-Id: I5a80f3fac71ef1bd76012a8c97c88774055634ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5187430
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248342}

--- chrome/browser/ui/views/extensions/extensions_toolbar_controls.h
+++ chrome/browser/ui/views/extensions/extensions_toolbar_controls.h
@@ -6,19 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSIONS_TOOLBAR_CONTROLS_H_
 #define CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSIONS_TOOLBAR_CONTROLS_H_
 
-#include <memory>
-
 #include "base/memory/raw_ptr.h"
-#include "extensions/browser/permissions_manager.h"
+#include "url/origin.h"
 
-namespace content {
-class WebContents;
-}
-
-class Browser;
 class ExtensionsToolbarButton;
 class ExtensionsRequestAccessButton;
-class ToolbarActionViewController;
 
 class ExtensionsToolbarControls {
  public:
@@ -30,22 +22,6 @@ class ExtensionsToolbarControls {
       delete;
   ~ExtensionsToolbarControls();
 
-  // Update the controls given whether `is_restricted_url`, the `actions` and
-  // the user `site_setting` in the `current_web_contents`.
-  void UpdateControls(
-      bool is_restricted_url,
-      const std::vector<std::unique_ptr<ToolbarActionViewController>>& actions,
-      extensions::PermissionsManager::UserSiteSetting site_setting,
-      content::WebContents* current_web_contents,
-      Browser* browser);
-
-  // Updates `request_access_button_` visibility given the user `site_setting`
-  // and `actions` in `web_contents`.
-  void UpdateRequestAccessButton(
-      const std::vector<std::unique_ptr<ToolbarActionViewController>>& actions,
-      extensions::PermissionsManager::UserSiteSetting site_setting,
-      content::WebContents* web_contents);
-
   // Hides the confirmation message in the request access button.
   void ResetConfirmation();
 
@@ -56,14 +32,6 @@ class ExtensionsToolbarControls {
   bool IsShowingConfirmationFor(const url::Origin& origin) const;
 
  private:
-  // Updates `extensions_button_` icon given `actions`, the user `site_setting`
-  // and whether `is_restricted_url` in `web_contents`.
-  void UpdateExtensionsButton(
-      const std::vector<std::unique_ptr<ToolbarActionViewController>>& actions,
-      extensions::PermissionsManager::UserSiteSetting site_setting,
-      content::WebContents* web_contents,
-      bool is_restricted_url);
-
   const raw_ptr<ExtensionsRequestAccessButton> request_access_button_;
   const raw_ptr<ExtensionsToolbarButton> extensions_button_;
 };
