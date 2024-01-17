34635b1fefba32cd7e6fadee2136acd478dac55c
Charles Meng <charlesmeng@chromium.org>
2024-01-17 17:53:42 +0000

    Revert "Kcer: Split GetKeyInfo"
    This reverts commit 02c439796393e29c28763c1272ca112cde4c6747.
    
    Reason for revert: Libfuzzer Upload Chrome OS ASan experiencing compile failures, e.g. https://ci.chromium.org/ui/p/chromium/builders/ci/Libfuzzer%20Upload%20Chrome%20OS%20ASan/107898/overview
    
    Original change's description:
    > Kcer: Split GetKeyInfo
    >
    > The original idea was to retrieve all the attributes at once
    > because that can be done efficiently with the PKCS#11 interface.
    > Unfortunately in cases when the size of attributes is not known and
    > they can be missing, this doesn’t actually work well. E.g. if one
    > attribute was never set and the provided buffer for another one is
    > too small, the PKCS#11 interface can just return that some attributes
    > are missing and then the caller has to retry with a different set
    > of attributes to check that the “buffer is too small” error was
    > not hidden by the “attribute is missing” one.
    > So for custom attributes that are often never set it’s easier to
    > retrieve them separately.
    >
    > Introduce GetKeyPermissions and GetCertProvisioningProfileId and
    > refactor the existing code to implement them.
    >
    > Bug: b:287015636
    > Test: *Kcer* unit tests
    > Change-Id: I90f45890c49c75843fadcc487867f759db426818
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5176968
    > Commit-Queue: Michael Ershov <miersh@google.com>
    > Reviewed-by: Pavol Marko <pmarko@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1248086}
    
    Bug: b:287015636
    Change-Id: Ie510155b348f7b96cd7c5e5bd4e8432ca35ff8b1
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5204879
    Reviewed-by: Michael Ershov <miersh@google.com>
    Commit-Queue: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1248211}

--- chromeos/components/kcer/kcer_impl.h
+++ chromeos/components/kcer/kcer_impl.h
@@ -85,11 +85,6 @@ class COMPONENT_EXPORT(KCER) KcerImpl : public Kcer {
   void GetAvailableTokens(GetAvailableTokensCallback callback) override;
   void GetTokenInfo(Token token, GetTokenInfoCallback callback) override;
   void GetKeyInfo(PrivateKeyHandle key, GetKeyInfoCallback callback) override;
-  void GetKeyPermissions(PrivateKeyHandle key,
-                         GetKeyPermissionsCallback callback) override;
-  void GetCertProvisioningProfileId(
-      PrivateKeyHandle key,
-      GetCertProvisioningProfileIdCallback callback) override;
   void SetKeyNickname(PrivateKeyHandle key,
                       std::string nickname,
                       StatusCallback callback) override;
@@ -149,14 +144,6 @@ class COMPONENT_EXPORT(KCER) KcerImpl : public Kcer {
 
   void GetKeyInfoWithToken(
       GetKeyInfoCallback callback,
-      base::expected<PrivateKeyHandle, Error> key_or_error);
-
-  void GetKeyPermissionsWithToken(
-      GetKeyPermissionsCallback callback,
-      base::expected<PrivateKeyHandle, Error> key_or_error);
-
-  void GetCertProvisioningProfileIdWithToken(
-      GetCertProvisioningProfileIdCallback callback,
       base::expected<PrivateKeyHandle, Error> key_or_error);
 
   void SetKeyNicknameWithToken(
