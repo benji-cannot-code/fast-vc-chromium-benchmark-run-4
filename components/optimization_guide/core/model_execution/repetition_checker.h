cf4651f0343e952316744f907b34858515e9e650
Clark DuVall <cduvall@chromium.org>
2024-01-17 16:42:05 +0000

    Add repetition checking for on device model output
    This adds a couple finch params and logic to detect when the model is
    repeating. We can control the min # of chars to check for repeats, and
    how many repeats to trigger on. If a repeat is detected, the output will
    be halted.
    
    Bug: b:319473563
    Change-Id: I3681b82d94a266700b164df152fb81445b56eaac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5201557
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248176}

---
+++ components/optimization_guide/core/model_execution/repetition_checker.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_OPTIMIZATION_GUIDE_CORE_MODEL_EXECUTION_REPETITION_CHECKER_H_
+#define COMPONENTS_OPTIMIZATION_GUIDE_CORE_MODEL_EXECUTION_REPETITION_CHECKER_H_
+
+#include <string_view>
+
+namespace optimization_guide {
+
+// Returns true if `text` has a suffix which repeats `num_repeats` times with at
+// least a length of `min_chars`.
+bool HasRepeatingSuffix(int min_chars, int num_repeats, std::string_view text);
+
+}  // namespace optimization_guide
+
+#endif  // COMPONENTS_OPTIMIZATION_GUIDE_CORE_MODEL_EXECUTION_REPETITION_CHECKER_H_
