02ce9b4ffa1c7ee677e00f435d09459737e6fa5b
Angus L. M. McLean IV <angusmclean@google.com>
2024-01-17 22:34:22 +0000

    tote tour: Add first interaction preference
    Adds a preference to track when an interaction first takes place after
    showing the holding space wallpaper nudge. This is measured from first
    session start.
    
    This will enable submitting metrics about the impact the nudge has on
    users' usage of the holding space.
    
    This also, in the process of moving eligibility checks, fixes a bug
    where user newness would not be stored properly if they user did not
    perform a drag operating on first session.
    
    Bug: b:311411775, b:320507003
    Change-Id: Ic2035f07e9bb02b3af6a1b9c26a39af08eb17873
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5038003
    Reviewed-by: David Black <dmblack@google.com>
    Commit-Queue: Angus McLean <angusmclean@google.com>
    Cr-Commit-Position: refs/heads/main@{#1248413}

--- ash/user_education/holding_space_wallpaper_nudge/holding_space_wallpaper_nudge_prefs.cc
+++ ash/user_education/holding_space_wallpaper_nudge/holding_space_wallpaper_nudge_prefs.cc
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "ash/constants/ash_features.h"
+#include "ash/user_education/holding_space_wallpaper_nudge/holding_space_wallpaper_nudge_metrics.h"
 #include "base/json/values_util.h"
+#include "base/strings/strcat.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
 
@@ -28,8 +30,12 @@ constexpr char kNudgeShownCountCounterfactual[] =
     "ash.holding_space.wallpaper_nudge.shown_count_counterfactual";
 constexpr char kNudgeShownCount[] =
     "ash.holding_space.wallpaper_nudge.shown_count";
+constexpr char kTimeOfFirstInteractionPrefPrefix[] =
+    "ash.holding_space.wallpaper_nudge.interaction_time.";
 constexpr char kUserEligibleForNudge[] =
     "ash.holding_space.wallpaper_nudge.user_eligible";
+constexpr char kUserFirstEligibleSessionTime[] =
+    "ash.holding_space.wallpaper_nudge.first_eligible_session_time";
 
 // Helpers ---------------------------------------------------------------------
 
@@ -47,9 +53,25 @@ std::string GetNudgeTimePrefName() {
              : kLastTimeNudgeShown;
 }
 
+// Gets the name of the pref that stores the first time `interaction` happened
+// since the most recent nudge.
+std::string GetTimeOfFirstInteractionPrefName(
+    holding_space_wallpaper_nudge_metrics::Interaction interaction) {
+  return base::StrCat(
+      {kTimeOfFirstInteractionPrefPrefix,
+       holding_space_wallpaper_nudge_metrics::ToString(interaction),
+       ".first_time"});
+}
+
 }  // namespace
 
 // Utilities -------------------------------------------------------------------
+
+std::optional<base::Time> GetTimeOfFirstEligibleSession(PrefService* prefs) {
+  auto* pref = prefs->FindPreference(kUserFirstEligibleSessionTime);
+  return pref->IsDefaultValue() ? std::nullopt
+                                : base::ValueToTime(pref->GetValue());
+}
 
 std::optional<base::Time> GetLastTimeNudgeWasShown(PrefService* prefs) {
   auto* pref = prefs->FindPreference(GetNudgeTimePrefName());
@@ -77,12 +99,44 @@ void MarkNudgeShown(PrefService* prefs) {
   prefs->SetUint64(GetNudgeCountPrefName(), GetNudgeShownCount(prefs) + 1u);
 }
 
+bool MarkTimeOfFirstEligibleSession(PrefService* prefs) {
+  CHECK(features::IsHoldingSpaceWallpaperNudgeEnabled());
+  if (prefs->FindPreference(kUserFirstEligibleSessionTime)->IsDefaultValue()) {
+    prefs->SetTime(kUserFirstEligibleSessionTime, base::Time::Now());
+    return true;
+  }
+  return false;
+}
+
+bool MarkTimeOfFirstInteraction(
+    PrefService* prefs,
+    holding_space_wallpaper_nudge_metrics::Interaction interaction) {
+  CHECK(features::IsHoldingSpaceWallpaperNudgeEnabled());
+  if (!GetTimeOfFirstEligibleSession(prefs).has_value()) {
+    return false;
+  }
+
+  const auto pref_name = GetTimeOfFirstInteractionPrefName(interaction);
+  if (prefs->FindPreference(pref_name)->IsDefaultValue()) {
+    prefs->SetTime(pref_name, base::Time::Now());
+    return true;
+  }
+  return false;
+}
+
 void RegisterProfilePrefs(PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(kUserEligibleForNudge, false);
   registry->RegisterTimePref(kLastTimeNudgeShownCounterfactual, base::Time());
   registry->RegisterTimePref(kLastTimeNudgeShown, base::Time());
+  registry->RegisterTimePref(kUserFirstEligibleSessionTime, base::Time());
   registry->RegisterUint64Pref(kNudgeShownCountCounterfactual, 0u);
   registry->RegisterUint64Pref(kNudgeShownCount, 0u);
+
+  for (const auto interaction :
+       holding_space_wallpaper_nudge_metrics::kAllInteractionsSet) {
+    registry->RegisterTimePref(GetTimeOfFirstInteractionPrefName(interaction),
+                               base::Time());
+  }
 }
 
 bool SetUserEligibility(PrefService* prefs, bool eligible) {
