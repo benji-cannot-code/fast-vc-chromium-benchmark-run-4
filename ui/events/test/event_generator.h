f38dbc2986075404d02a0f323e3a4ee943f8c2fd
Mitsuru Oshima <oshima@chromium.org>
2024-01-17 21:25:40 +0000

    [Reland] Make the event type check for d&d more explicit
    This is reland of crrev.com/c/5142213
    See PS2 for the  fix. (data_offer has to be captured and released)
    
    A user may release a mouse button that is not a part of d&d operation,
    and it can still cancel the drag and drop. Instead, explicitly check
    the left button (or other buttons), to decide if the drag and drop
    should be canceled.
    
    A few extra changes
    * Moved ShellClientData from shell_unittest.cc for reuse
    * Added wl_pointer support to TestClient
    * changed set_data so that it returns the pointer of moved object.
      I added DestroyData as template can't take nullptr any more.
    * Added generic button support to EventGenerator
    
    Bug: 1513068
    Test: covered by unittest
    Change-Id: I6445fdb6ffb4620535787e724acf1c6181955806
    Low-Coverage-Reason: 　TRIVIAL_CHANGE  logging code.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5202938
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248367}

--- ui/events/test/event_generator.h
+++ ui/events/test/event_generator.h
@@ -187,6 +187,10 @@ class EventGenerator {
   void set_flags(int flags) { flags_ = flags; }
   int flags() const { return flags_; }
 
+  // Generates a button press / release event.
+  void PressButton(int flag);
+  void ReleaseButton(int flag);
+
   // Generates a left button press event.
   void PressLeftButton();
 
@@ -496,8 +500,6 @@ class EventGenerator {
 
   void SetCurrentScreenLocation(const gfx::Point& point);
   void UpdateCurrentDispatcher(const gfx::Point& point);
-  void PressButton(int flag);
-  void ReleaseButton(int flag);
 
   gfx::Point GetLocationInCurrentRoot() const;
   gfx::Point CenterOfWindow(const EventTarget* window) const;
