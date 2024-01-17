7aa551f83706b8acff28bf6d4cfabe6be4ec9567
Jacob Francis <francisjp@google.com>
2024-01-17 22:40:41 +0000

    [A11y Typescript] Switch Access uses path mappings
    AX-RelNotes: N/A
    
    Bug: b/315150183
    Change-Id: I8fbf3e89a80416e8b5c541620df7eaf811faaf14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5190945
    Commit-Queue: Jacob Francis <francisjp@google.com>
    Auto-Submit: Jacob Francis <francisjp@google.com>
    Reviewed-by: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1248421}

--- chrome/browser/resources/chromeos/accessibility/switch_access/focus_ring_manager.ts
+++ chrome/browser/resources/chromeos/accessibility/switch_access/focus_ring_manager.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {RectUtil} from '../common/rect_util.js';
+import {RectUtil} from '/common/rect_util.js';
 
 import {MenuManager} from './menu_manager.js';
 import {SAChildNode, SANode} from './nodes/switch_access_node.js';
