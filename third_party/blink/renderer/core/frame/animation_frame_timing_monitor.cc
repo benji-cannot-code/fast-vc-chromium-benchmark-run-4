4a77d58143f4be4d8f710085f28cd851df4b60bc
Noam Rosenthal <nrosenthal@chromium.org>
2024-01-17 10:46:25 +0000

    Removing redundant feature flags for LoAF
    We've been monitoring LoAFs and sending UKMs for a while,
    it's time to remove the flags.
    
    LongAnimationFrameMonitoring was marked experimental but implied by
    LongAnimationFrameUKM which was stable.
    LongAnimationFrameTiming is still there and marked
    experimental, which keeps the web-facing API unshipped atm.
    
    Bug: 1518487
    Change-Id: I3c8038c115b10bc78642f78095def6536c66dd2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5197688
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248059}

--- third_party/blink/renderer/core/frame/animation_frame_timing_monitor.cc
+++ third_party/blink/renderer/core/frame/animation_frame_timing_monitor.cc
@@ -272,10 +272,6 @@ void RecordLongAnimationFrameTrace(const AnimationFrameTimingInfo& info,
 void AnimationFrameTimingMonitor::RecordLongAnimationFrameUKMAndTrace(
     const AnimationFrameTimingInfo& info) {
   RecordLongAnimationFrameTrace(info, this);
-  if (!RuntimeEnabledFeatures::LongAnimationFrameUKMEnabled()) {
-    return;
-  }
-
   ukm::UkmRecorder* recorder = client_.MainFrameUkmRecorder();
   ukm::SourceId source_id = client_.MainFrameUkmSourceId();
   if (!recorder || source_id == ukm::kInvalidSourceId) {
