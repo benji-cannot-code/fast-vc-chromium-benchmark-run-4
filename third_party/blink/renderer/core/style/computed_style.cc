51384dbe95987b3fda62e57461014036f060fe80
Kent Tamura <tkent@chromium.org>
2024-01-17 04:36:04 +0000

    Fix offset mapping from a DOM text to a rendered text with text-transform:math-auto
    text-transform:math-auto should provide WTF::TextOffsetMap
    
    Bug: 750990
    Change-Id: Ife81dc7774aa6d301271a9709da886038777b010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5201564
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247957}

--- third_party/blink/renderer/core/style/computed_style.cc
+++ third_party/blink/renderer/core/style/computed_style.cc
@@ -96,6 +96,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/size_assertions.h"
 #include "third_party/blink/renderer/platform/wtf/text/case_map.h"
 #include "third_party/blink/renderer/platform/wtf/text/math_transform.h"
+#include "third_party/blink/renderer/platform/wtf/text/text_offset_map.h"
 #include "ui/base/ui_base_features.h"
 #include "ui/gfx/geometry/point_f.h"
 
@@ -1835,7 +1836,7 @@ static String DisableNewGeorgianCapitalLetters(const String& text) {
 
 namespace {
 
-String ApplyMathAutoTransform(const String& text) {
+String ApplyMathAutoTransform(const String& text, TextOffsetMap* offset_map) {
   if (text.length() != 1) {
     return text;
   }
@@ -1848,7 +1849,11 @@ String ApplyMathAutoTransform(const String& text) {
   Vector<UChar> transformed_text(U16_LENGTH(transformed_char));
   int i = 0;
   U16_APPEND_UNSAFE(transformed_text, i, transformed_char);
-  return String(transformed_text);
+  String transformed_string = String(transformed_text);
+  if (offset_map) {
+    offset_map->Append(text.length(), transformed_string.length());
+  }
+  return transformed_string;
 }
 
 }  // namespace
@@ -1873,7 +1878,7 @@ String ComputedStyle::ApplyTextTransform(const String& text,
       return case_map.ToLower(text, offset_map);
     }
     case ETextTransform::kMathAuto:
-      return ApplyMathAutoTransform(text);
+      return ApplyMathAutoTransform(text, offset_map);
   }
   NOTREACHED();
 }
