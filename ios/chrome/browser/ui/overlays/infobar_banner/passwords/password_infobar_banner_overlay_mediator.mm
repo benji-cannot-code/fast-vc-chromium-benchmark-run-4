618649caa73c766c0ea71ab44cf4cc6e29bdbc04
Vincent Boisselle <vincb@google.com>
2024-01-17 15:01:02 +0000

    Reland "[ios] Track duration of the password save/update infobar"
    This is a reland of commit f82042dd87f27e5ecf7f4a26f089ac862196918d
    
    With a minor fix that only concerned the unittest (client code was
    fine).
    
    Original change's description:
    > [ios] Track duration of the password save/update infobar
    >
    > This CL adds the PasswordManager.iOS.InfoBar.{Type}Duration.{Moment}
    > metric which is summarized as follow:
    > """
    > Records the duration of the password save/update infobar that is
    > presented after using new credentials. Recorded when the
    > password infobar banner is dismissed.
    >
    > Token key "Type":
    >  - variant "Save": the save infobar
    >  - variant "Update": the update infobar
    >
    > Token key "Moment":
    >  - variant "All": all moments
    >  - variant "OnDeletion": when deleting the infobar
    >  - variant "OnDismiss: when dismissing the infobar
    > """
    >
    > This CL has also other effects:
    >  * Record metrics as soon as the infobar is dismissed instead of
    >    waiting until deletion. This should be no op as the infobar can
    >    only be presented once during the lifetime of the delegate, hence
    >    there shouldn't be a difference in the metrics when recording right
    >    after dismissal VS on delegate deletion. This has the advantage of
    >    speeding up the recording of infobar metrics on dismiss.
    >
    > Change-Id: If056bd6b5d28e1cc88b0809019ff5d3e1560dba7
    > Bug: b/318687052, b/317337446, 1394793
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5172194
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    > Reviewed-by: Tommy Martino <tmartino@chromium.org>
    > Commit-Queue: Vincent Boisselle <vincb@google.com>
    > Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1247568}
    
    Bug: b/318687052, b/317337446, 1394793
    Change-Id: I402bdbb17707f24f47b1e6eaa496612733983026
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5201409
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1248131}

--- ios/chrome/browser/ui/overlays/infobar_banner/passwords/password_infobar_banner_overlay_mediator.mm
+++ ios/chrome/browser/ui/overlays/infobar_banner/passwords/password_infobar_banner_overlay_mediator.mm
@@ -52,12 +52,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return DefaultInfobarOverlayRequestConfig::RequestSupport();
 }
 
-#pragma mark - InfobarOverlayRequestMediator
+#pragma mark - InfobarBannerDelegate
 
 - (void)bannerInfobarButtonWasPressed:(UIButton*)sender {
   // This can happen if the user quickly navigates to another website while the
-  // banner is still appearing, causing the banner to be triggered before being
-  // removed.
+  // banner is still appearing, where the infobar owning the delegate is deleted
+  // before handling the button action.
   if (!self.passwordDelegate) {
     return;
   }
@@ -74,6 +74,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }
   }
   [self dismissOverlay];
+}
+
+#pragma mark - InfobarBannerOverlayMediator
+
+- (void)finishDismissal {
+  if (self.passwordDelegate) {
+    // If the infobar owning the delegate isn't yet deleted, report the infobar
+    // as gone right now. The infobar outlives the banner UI when the state of
+    // the page hasn't changed after dimissing the banner.
+    //
+    // Not having a delegate at this moment happens when navigating away from
+    // the page on which the banner is displayed, where the infobar delegate is
+    // deleted before the dismiss callback is called.
+    self.passwordDelegate->InfobarGone();
+  }
+
+  [super finishDismissal];
 }
 
 #pragma mark - Private
