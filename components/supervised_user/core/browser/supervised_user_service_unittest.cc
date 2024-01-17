1c829d5ff33ad7b8aa52faf286d14c291ea20398
Nohemi Fernandez <fernandex@chromium.org>
2024-01-17 13:17:34 +0000

    Move URL filter type definition to supervised_user constants.
    Bug: b/318813196
    Change-Id: I6cba68bbe874a361a167d2b47acf1d22a9ca895e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5201391
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Duong Dac <ddac@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248091}

--- components/supervised_user/core/browser/supervised_user_service_unittest.cc
+++ components/supervised_user/core/browser/supervised_user_service_unittest.cc
@@ -118,7 +118,7 @@ TEST_F(SupervisedUserServiceTest, WebFilterTypeOnPrefsChange) {
   histogram_tester.ExpectUniqueSample(
       SupervisedUserURLFilter::GetWebFilterTypeHistogramNameForTest(),
       /*sample=*/
-      SupervisedUserURLFilter::WebFilterType::kTryToBlockMatureSites,
+      WebFilterType::kTryToBlockMatureSites,
       /*expected_bucket_count=*/0);
 
   // Tests filter "allow all sites".
@@ -126,7 +126,7 @@ TEST_F(SupervisedUserServiceTest, WebFilterTypeOnPrefsChange) {
   histogram_tester.ExpectBucketCount(
       SupervisedUserURLFilter::GetWebFilterTypeHistogramNameForTest(),
       /*sample=*/
-      SupervisedUserURLFilter::WebFilterType::kAllowAllSites,
+      WebFilterType::kAllowAllSites,
       /*expected_count=*/1);
 
   // Tests filter "only allow certain sites".
@@ -138,7 +138,7 @@ TEST_F(SupervisedUserServiceTest, WebFilterTypeOnPrefsChange) {
   histogram_tester.ExpectBucketCount(
       SupervisedUserURLFilter::GetWebFilterTypeHistogramNameForTest(),
       /*sample=*/
-      SupervisedUserURLFilter::WebFilterType::kCertainSites,
+      WebFilterType::kCertainSites,
       /*expected_count=*/1);
 
   histogram_tester.ExpectTotalCount(
