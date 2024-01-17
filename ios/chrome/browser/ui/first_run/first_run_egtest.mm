340a30b1b24c6de13136fcb1e6fa736e3f6ba23b
Arthur Milchior <arthurmilchior@chromium.org>
2024-01-17 09:59:36 +0000

    [iOS][Waffle] Remove early return in some tests.
    The tests are now runnable upstream, there is no reason to disable
    them anymore.
    
    This CL also removes a method that were not used anymore.
    
    Bug: None
    Change-Id: Ia8e4cf21d71bfd1533ac0e23e69bbcf610e888e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5201490
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248045}

--- ios/chrome/browser/ui/first_run/first_run_egtest.mm
+++ ios/chrome/browser/ui/first_run/first_run_egtest.mm
@@ -1492,10 +1492,6 @@ void DismissDefaultBrowserAndOmniboxPositionSelectionScreens() {
 // button is correctly updated when the user selects a search engine then
 // scrolls down and that it correctly sets the default search engine.
 - (void)testSearchEngineChoiceScreenSelectThenScroll {
-  if (![ChromeEarlGreyAppInterface IsSearchEngineChoiceScreenEnabledFre]) {
-    // Do not run this test if the choice screen is not enabled.
-    return;
-  }
   // Skips sign-in.
   [[self elementInteractionWithGreyMatcher:
              chrome_test_util::PromoStyleSecondaryActionButtonMatcher()
@@ -1532,10 +1528,6 @@ void DismissDefaultBrowserAndOmniboxPositionSelectionScreens() {
 // primary button is correctly updated when the user scrolls down then selects a
 // search engine and that it correctly sets the default search engine.
 - (void)testSearchEngineChoiceScreenScrollThenSelect {
-  if (![ChromeEarlGreyAppInterface IsSearchEngineChoiceScreenEnabledFre]) {
-    // Do not run this test if the choice screen is not enabled.
-    return;
-  }
   // Skips sign-in.
   [[self elementInteractionWithGreyMatcher:
              chrome_test_util::PromoStyleSecondaryActionButtonMatcher()
