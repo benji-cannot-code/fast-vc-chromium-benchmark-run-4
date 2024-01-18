846168b80ff93f238c090d584db7d537b385337f
Colin Blundell <blundell@chromium.org>
2024-01-18 12:28:49 +0000

    Revert "Lazily create the TextureLayer for WebGPU"
    This reverts commit 914cc2432e9c6a2ffc40fad894dcd6b6fe3ba93b.
    
    Reason for revert: Several WebGPU demo pages don't render on 122.0.6254.0 (screen remains black). Repro'd on ToT and confirmed that the demo pages render with revert of this CL.
    
    Demo pages that exhibit the problem:
    
    https://webgpu.github.io/webgpu-samples/samples/texturedCube
    https://webgpu.github.io/webgpu-samples/samples/cameras
    https://webgpu.github.io/webgpu-samples/samples/cubemap
    https://webgpu.github.io/webgpu-samples/samples/gameOfLife
    
    Original change's description:
    > Lazily create the TextureLayer for WebGPU
    >
    > If the TextureLayer of canvas already created, its background color
    > will be drawn to the layer. But if TextureLayer has no texture, this
    > layer can not be composited. This CL delays creating TextureLayer for
    > WebGPU until GetNewTexture, ensuring TextureLayer can be composited.
    >
    > Bug: 1516905
    > Change-Id: I957d0e2d5a026933cd36e82dea96d83ae9d034a6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5196366
    > Commit-Queue: Kai Ninomiya <kainino@chromium.org>
    > Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    > Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1248001}
    
    Bug: 1516905
    Change-Id: I70b785dd52ee3c658df10fed986b14ac27ed80fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5211365
    Auto-Submit: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1248707}

--- third_party/blink/renderer/modules/webgpu/gpu_canvas_context.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_canvas_context.cc
@@ -506,6 +506,7 @@ void GPUCanvasContext::configure(const GPUCanvasConfiguration* descriptor,
   // actually make the contents opaque.
   switch (alpha_mode_) {
     case V8GPUCanvasAlphaMode::Enum::kOpaque: {
+      CcLayer()->SetContentsOpaque(true);
       if (!alpha_clearer_ ||
           !alpha_clearer_->IsCompatible(device_->GetHandle(),
                                         swap_texture_descriptor_.format)) {
@@ -517,6 +518,7 @@ void GPUCanvasContext::configure(const GPUCanvasConfiguration* descriptor,
     }
     case V8GPUCanvasAlphaMode::Enum::kPremultiplied:
       alpha_clearer_ = nullptr;
+      CcLayer()->SetContentsOpaque(false);
       break;
   }
 
