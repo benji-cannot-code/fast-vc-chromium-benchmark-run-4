e14ac72d35381cfd2f4cd1f69c31d282be3e375d
Yue Zhang <yuezhanggg@chromium.org>
2024-01-18 16:53:07 +0000

    [PriceDrop] Register price change module
    Bug: 1501138
    Low-Coverage-Reason: TRIVIAL_CHANGE
    Change-Id: I61190ba3c53c393c0e6f7fa2d6b75c681b883e8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5202687
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248829}

--- chrome/browser/commerce/price_change/android/java/src/org/chromium/chrome/browser/price_change/PriceChangeModuleBuilder.java
+++ chrome/browser/commerce/price_change/android/java/src/org/chromium/chrome/browser/price_change/PriceChangeModuleBuilder.java
@@ -12,6 +12,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 
 import org.chromium.base.Callback;
+import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.chrome.browser.magic_stack.ModuleDelegate;
 import org.chromium.chrome.browser.magic_stack.ModuleProvider;
 import org.chromium.chrome.browser.magic_stack.ModuleProviderBuilder;
@@ -23,16 +24,16 @@ import org.chromium.ui.modelutil.PropertyModel;
 /** {@link ModuleProviderBuilder} that builds the price change module. */
 public class PriceChangeModuleBuilder implements ModuleProviderBuilder {
     private final Context mContext;
-    private final Profile mProfile;
+    private final ObservableSupplier<Profile> mProfileSupplier;
     private final TabModelSelector mTabModelSelector;
 
     /** Pass in the dependencies needed to build {@link PriceChangeModuleCoordinator}. */
     public PriceChangeModuleBuilder(
             @NonNull Context context,
-            @NonNull Profile profile,
+            @NonNull ObservableSupplier<Profile> profileSupplier,
             @NonNull TabModelSelector tabModelSelector) {
         mContext = context;
-        mProfile = profile;
+        mProfileSupplier = profileSupplier;
         mTabModelSelector = tabModelSelector;
     }
 
@@ -43,7 +44,7 @@ public class PriceChangeModuleBuilder implements ModuleProviderBuilder {
             @NonNull Callback<ModuleProvider> onModuleBuiltCallback) {
         PriceChangeModuleCoordinator coordinator =
                 new PriceChangeModuleCoordinator(
-                        mContext, mProfile, mTabModelSelector, moduleDelegate);
+                        mContext, mProfileSupplier.get(), mTabModelSelector, moduleDelegate);
         onModuleBuiltCallback.onResult(coordinator);
         return true;
     }
