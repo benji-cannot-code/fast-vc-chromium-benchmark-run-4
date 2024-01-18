563ad4d3b735d85e5235567323fd6fffe71fc82d
Colin Blundell <blundell@chromium.org>
2024-01-18 10:44:05 +0000

    [Blink] Convert SI RASTER usage to RASTER_READ | RASTER_WRITE
    There are no functional changes, as RASTER is a constant that expands
    to RASTER_READ | RASTER_WRITE. This will allow us to subsequently (1)
    eliminate RASTER and (2) refine all RASTER_READ | RASTER_WRITE usage
    based on where raster reads and writes are actually necessary.
    
    Bug: 1519074
    Change-Id: I83180798eb38da494bf060031659d68d52bd8e7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5210410
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248675}

--- third_party/blink/renderer/platform/graphics/canvas_resource.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource.cc
@@ -488,7 +488,8 @@ CanvasResourceRasterSharedImage::CanvasResourceRasterSharedImage(
     // TODO(crbug.com/1518735): Determine whether FRAMEBUFFER_HINT can be
     // eliminated.
     shared_image_usage_flags = shared_image_usage_flags |
-                               gpu::SHARED_IMAGE_USAGE_RASTER |
+                               gpu::SHARED_IMAGE_USAGE_RASTER_READ |
+                               gpu::SHARED_IMAGE_USAGE_RASTER_WRITE |
                                gpu::SHARED_IMAGE_USAGE_OOP_RASTERIZATION |
                                gpu::SHARED_IMAGE_USAGE_GLES2_READ |
                                gpu::SHARED_IMAGE_USAGE_GLES2_FRAMEBUFFER_HINT;
@@ -504,8 +505,9 @@ CanvasResourceRasterSharedImage::CanvasResourceRasterSharedImage(
     // Currently in the process of adding with a killswitch.
     // TODO(crbug.com/1518427): Remove this killswitch post-safe rollout.
     if (base::FeatureList::IsEnabled(kAddSharedImageRasterUsageWithNonOOPR)) {
-      shared_image_usage_flags =
-          shared_image_usage_flags | gpu::SHARED_IMAGE_USAGE_RASTER;
+      shared_image_usage_flags = shared_image_usage_flags |
+                                 gpu::SHARED_IMAGE_USAGE_RASTER_READ |
+                                 gpu::SHARED_IMAGE_USAGE_RASTER_WRITE;
     }
   }
 
@@ -1253,7 +1255,8 @@ CanvasResourceSwapChain::CanvasResourceSwapChain(
                    gpu::SHARED_IMAGE_USAGE_SCANOUT;
 
   if (use_oop_rasterization_) {
-    usage = usage | gpu::SHARED_IMAGE_USAGE_RASTER |
+    usage = usage | gpu::SHARED_IMAGE_USAGE_RASTER_READ |
+            gpu::SHARED_IMAGE_USAGE_RASTER_WRITE |
             gpu::SHARED_IMAGE_USAGE_OOP_RASTERIZATION;
   } else {
     // The GLES2_WRITE flag is needed due to raster being over GL.
@@ -1262,7 +1265,8 @@ CanvasResourceSwapChain::CanvasResourceSwapChain(
     // Currently in the process of adding with a killswitch.
     // TODO(crbug.com/1518427): Remove this killswitch post-safe rollout.
     if (base::FeatureList::IsEnabled(kAddSharedImageRasterUsageWithNonOOPR)) {
-      usage = usage | gpu::SHARED_IMAGE_USAGE_RASTER;
+      usage = usage | gpu::SHARED_IMAGE_USAGE_RASTER_READ |
+              gpu::SHARED_IMAGE_USAGE_RASTER_WRITE;
     }
   }
 
