304b0c3f192ee30c3a0397375109b6b5d0b644bb
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2024-01-18 05:31:36 +0000

    locked-mode: Add fullscreen locked mode to about:flags
    Bug: b/304856218
    Change-Id: I2abab0de2136ad18f66991409e5fa2855fe8b50c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5202755
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Jun W. <jungoog@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248599}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4296,6 +4296,9 @@ extern const char kEnableBorderlessPrintingDescription[];
 extern const char kLacrosColorManagementName[];
 extern const char kLacrosColorManagementDescription[];
 
+extern const char kLockedModeName[];
+extern const char kLockedModeDescription[];
+
 extern const char kOneGroupPerRendererName[];
 extern const char kOneGroupPerRendererDescription[];
 
