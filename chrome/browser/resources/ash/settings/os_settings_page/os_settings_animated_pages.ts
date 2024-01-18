087d328638957908fed21ab3b23e4c5230f03fc1
Wes Okuhara <wesokuhara@google.com>
2024-01-18 18:27:23 +0000

    CrOS Settings: Refactor page backdrop to have room for managed header
    The page backdrop should take up all available vertical space at
    minimum. In order to account for the space of the managed header, the
    page backdrop is refactored to not use a fixed height calculation and
    instead use flexbox to dynamically resize the height.
    Screenshots:
    - Long page http://screen/6qrc3KyKUm34vgZ.png
    - Short page http://screen/5jGzXKnjy7g5ckY.png
    
    Bug: b:320515330
    Test: Manually verify UI
    Change-Id: I4598542359fbf85ae6d9966e954215210845bc1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5207564
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1248909}

--- chrome/browser/resources/ash/settings/os_settings_page/os_settings_animated_pages.ts
+++ chrome/browser/resources/ash/settings/os_settings_page/os_settings_animated_pages.ts
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 import 'chrome://resources/polymer/v3_0/iron-pages/iron-pages.js';
-import '../settings_shared.css.js';
 
 import {assert} from 'chrome://resources/js/assert.js';
 import {IronPagesElement} from 'chrome://resources/polymer/v3_0/iron-pages/iron-pages.js';
