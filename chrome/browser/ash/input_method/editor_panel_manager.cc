0b4a6a3c089c33b4c86c4ed3ad2d8875c548f221
Chuong Ho <hdchuong@google.com>
2024-01-18 23:18:03 +0000

    editor: add metrics for promo card
    This CL records metrics for the editor promo card.
    
    Bug: b:319160080
    Change-Id: Ibcebf0c82a28dc898c1f01dcfe10b6fb6a1a36d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5199733
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Zac Partridge <zacpartridge@google.com>
    Cr-Commit-Position: refs/heads/main@{#1249124}

--- chrome/browser/ash/input_method/editor_panel_manager.cc
+++ chrome/browser/ash/input_method/editor_panel_manager.cc
@@ -111,6 +111,8 @@ void EditorPanelManager::OnPromoCardDismissed() {
 
 void EditorPanelManager::OnPromoCardDeclined() {
   delegate_->OnPromoCardDeclined();
+  delegate_->GetMetricsRecorder()->LogEditorState(
+      EditorStates::kPromoCardExplicitDismissal);
 }
 
 void EditorPanelManager::StartEditingFlow() {
@@ -170,6 +172,10 @@ void EditorPanelManager::LogEditorMode(
     for (EditorBlockedReason blocked_reason : delegate_->GetBlockedReasons()) {
       logger->LogEditorState(ToEditorStatesMetric(blocked_reason));
     }
+  }
+
+  if (mode == crosapi::mojom::EditorPanelMode::kPromoCard) {
+    logger->LogEditorState(EditorStates::kPromoCardImpression);
   }
 }
 
