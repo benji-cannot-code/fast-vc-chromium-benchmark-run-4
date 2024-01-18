4b00b6691a1a6b68a6e24998935ea254926011f4
Vasiliy Telezhnikov <vasilyt@chromium.org>
2024-01-18 14:54:46 +0000

    Fix memory dumps for android media
    SharedImages don't own memory directly, so we're trying to guess which
    images own memory to decide what number to report. Eventually this lead
    to result being in accurate. To avoid this, we will report memory that
    is used by TextureOwners which actually owner buffers, and make SI to
    report 0.
    
    Bug: 1466805
    Change-Id: I32b20b02222b413ebb2ceb780536449cb667e9f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5123712
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248749}

--- gpu/command_buffer/service/texture_owner.cc
+++ gpu/command_buffer/service/texture_owner.cc
@@ -7,9 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/atomic_sequence_num.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/trace_event/memory_dump_manager.h"
 #include "gpu/command_buffer/service/abstract_texture_android.h"
 #include "gpu/command_buffer/service/decoder_context.h"
 #include "gpu/command_buffer/service/feature_info.h"
@@ -21,6 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gpu {
 namespace {
+
+// Generates process-unique IDs to use for tracing resources.
+base::AtomicSequenceNumber g_next_texture_owner_tracing_id;
 
 std::unique_ptr<AbstractTextureAndroid> CreateTexture(
     SharedContextState* context_state) {
@@ -44,9 +49,13 @@ TextureOwner::TextureOwner(bool binds_texture_on_update,
       binds_texture_on_update_(binds_texture_on_update),
       context_state_(std::move(context_state)),
       texture_(std::move(texture)),
-      task_runner_(base::SingleThreadTaskRunner::GetCurrentDefault()) {
+      task_runner_(base::SingleThreadTaskRunner::GetCurrentDefault()),
+      tracing_id_(g_next_texture_owner_tracing_id.GetNext()) {
   DCHECK(context_state_);
   context_state_->AddContextLostObserver(this);
+
+  base::trace_event::MemoryDumpManager::GetInstance()->RegisterDumpProvider(
+      this, "TextureOwner", base::SingleThreadTaskRunner::GetCurrentDefault());
 }
 
 TextureOwner::TextureOwner(bool binds_texture_on_update,
@@ -55,9 +64,16 @@ TextureOwner::TextureOwner(bool binds_texture_on_update,
           base::SingleThreadTaskRunner::GetCurrentDefault()),
       binds_texture_on_update_(binds_texture_on_update),
       texture_(std::move(texture)),
-      task_runner_(base::SingleThreadTaskRunner::GetCurrentDefault()) {}
+      task_runner_(base::SingleThreadTaskRunner::GetCurrentDefault()),
+      tracing_id_(g_next_texture_owner_tracing_id.GetNext()) {
+  base::trace_event::MemoryDumpManager::GetInstance()->RegisterDumpProvider(
+      this, "TextureOwner", base::SingleThreadTaskRunner::GetCurrentDefault());
+}
 
 TextureOwner::~TextureOwner() {
+  base::trace_event::MemoryDumpManager::GetInstance()->UnregisterDumpProvider(
+      this);
+
   bool have_context = true;
   std::optional<ui::ScopedMakeCurrent> scoped_make_current;
   if (!context_state_) {
