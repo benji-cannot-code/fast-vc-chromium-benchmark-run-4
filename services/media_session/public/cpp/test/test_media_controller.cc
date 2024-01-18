11b23322e297d2a0763ad4a4f8a9baec5adfdcfc
Yiren Wang <yrw@chromium.org>
2024-01-18 23:59:43 +0000

    Migrate MediaHostTest to use public mock objects
    Use media_session::test::MockMediaSessionMojoObserver and
    media_session::test::TestMediaController to replace the mock objects
    in MediaHostTest so that the testing/mocking methods can be managed in
    services/media_session/public/cpp/test/ only.
    
    Resolve TODOs in TestMediaController.
    
    Bug: 1510700
    Change-Id: I6465a41b95295f7bc1d330a7fb60b379a4aea3f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5208318
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Yiren Wang <yrw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249146}

--- services/media_session/public/cpp/test/test_media_controller.cc
+++ services/media_session/public/cpp/test/test_media_controller.cc
@@ -222,6 +222,11 @@ TestMediaController::CreateMediaControllerRemote() {
   return remote;
 }
 
+void TestMediaController::BindMediaControllerReceiver(
+    mojo::PendingReceiver<mojom::MediaController> receiver) {
+  receiver_.Bind(std::move(receiver));
+}
+
 void TestMediaController::Suspend() {
   ++suspend_count_;
 }
@@ -272,11 +277,11 @@ void TestMediaController::SkipAd() {
 }
 
 void TestMediaController::EnterPictureInPicture() {
-  // TODO(crbug.com/1040263): Implement EnterPictureInPicture.
+  ++enter_picture_in_picture_count_;
 }
 
 void TestMediaController::ExitPictureInPicture() {
-  // TODO(crbug.com/1040263): Implement ExitPictureInPicture.
+  ++exit_picture_in_picture_count_;
 }
 
 void TestMediaController::Raise() {
@@ -313,6 +318,16 @@ void TestMediaController::SimulateMediaSessionMetadataChanged(
 
 void TestMediaController::Flush() {
   observers_.FlushForTesting();
+}
+
+int TestMediaController::GetActiveObserverCount() {
+  int count = 0;
+  for (auto& observer : observers_) {
+    if (observer.is_connected()) {
+      count++;
+    }
+  }
+  return count;
 }
 
 }  // namespace test
