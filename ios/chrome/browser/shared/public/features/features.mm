f75c5b5336a0a8615d3295cdc7915953b33e1bda
Joemer Ramos <joemerramos@chromium.org>
2024-01-18 19:11:40 +0000

    [iOS] Create Feature Flag for Startup Improvements
    Bug: 1515588
    Change-Id: I6ab5fb1b02aad2afb2a57bf630ca0d0df4a3d2f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5211987
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248945}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -145,6 +145,10 @@ BASE_FEATURE(kEnableShortenedPasswordAutoFillInstruction,
              "EnableShortenedPasswordAutoFillInstruction",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+BASE_FEATURE(kEnableStartupImprovements,
+             "EnableStartupImprovements",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kEnableExpKitAppleCalendar,
              "EnableExpKitAppleCalendar",
              base::FEATURE_ENABLED_BY_DEFAULT);
