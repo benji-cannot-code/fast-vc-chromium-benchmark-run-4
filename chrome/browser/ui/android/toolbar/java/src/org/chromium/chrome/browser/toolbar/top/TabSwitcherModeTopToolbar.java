3a86173cbd0d209b536206ba2c07d272c62a93f1
Sky Malice <skym@chromium.org>
2024-01-18 23:27:25 +0000

    Adjust ToolbarColorObserverManager's handling of toolbar color.
    * ToolbarAlphaInOverviewObserver is not more clear that it is the
    overview's alpha, not the toolbar alpha, that is shifting and being
    passed as a param.
    * No longer passes any transparent colors into getColorWithOverlay.
    Unfortunately the code is not strictly correct here, but depending on
    feature status that might never matter.
    
    Bug: 1511721
    Change-Id: I1e0aea7c834770414965bc5284cead0db379230a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5166227
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249133}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/TabSwitcherModeTopToolbar.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/TabSwitcherModeTopToolbar.java
@@ -194,7 +194,7 @@ public class TabSwitcherModeTopToolbar extends OptimizedFrameLayout
                         Object alphaValue = animation.getAnimatedValue();
                         if (mToolbarAlphaInOverviewObserver != null
                                 && alphaValue instanceof Float) {
-                            mToolbarAlphaInOverviewObserver.onToolbarAlphaInOverviewChanged(
+                            mToolbarAlphaInOverviewObserver.onOverviewAlphaChanged(
                                     (Float) alphaValue);
                         }
                     });
