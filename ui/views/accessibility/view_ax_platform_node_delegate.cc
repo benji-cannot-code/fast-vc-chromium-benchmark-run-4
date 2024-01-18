d70aa6df29ea68e854efc44ff8203f4668ccf332
Chege Gitau <dagitau@microsoft.com>
2024-01-18 02:04:38 +0000

    [a11y] Make IsInvisibleOrIgnored=true for invisible descendants
    Previously, ViewAXPlatformNodeDelegate::IsInvisibleOrIgnored used
    View::visible_ to determine invisibility [1]. However, View::visible_
    does not cascade down the view hierarchy, and that behavior is correct*.
    
    That said, some a11y clients (e.g., VoiceOver) allow users to interact
    with invisible views if they are in the a11y tree. This CL fixes that by
    having ViewAXPlatformNodeDelegate::IsInvisibleOrIgnored use
    ax::mojom::State::kInvisible instead to determine invisibility. Unlike
    View::visible_, ax::mojom::State::kInvisible cascades down the view
    hierarchy.
    
    * Say I have a view v with two children, c1 and c2, where c1 is visible
    but c2 is not. If I toggle v's visibility, then I should get back to the
    initial state where c1 is visible but c2 is not.
    
    [1]: https://chromium-review.googlesource.com/c/chromium/src/+/2627320
    
    AX-Relnotes: VoiceOver users will no longer navigate to invisible views.
    Change-Id: I13f1e772ef32bbb34066079f718a2469303e2e14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5186313
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Chege Gitau <dagitau@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1248532}

--- ui/views/accessibility/view_ax_platform_node_delegate.cc
+++ ui/views/accessibility/view_ax_platform_node_delegate.cc
@@ -578,7 +578,7 @@ bool ViewAXPlatformNodeDelegate::IsLeaf() const {
 }
 
 bool ViewAXPlatformNodeDelegate::IsInvisibleOrIgnored() const {
-  return IsIgnored() || !view()->GetVisible();
+  return IsIgnored() || GetData().IsInvisible();
 }
 
 bool ViewAXPlatformNodeDelegate::IsAccessibilityEnabled() const {
