22b5c171db8af50600412251c5a565e6dbb11b84
Cheick Cisse <cheickcisse@google.com>
2024-01-18 16:23:48 +0000

    [WhatsNew] Add Default Browser Promo as a Chrome Tip
    This CL:
    - Updates what's new plist to add the assets for default browser promo as chrome tip
    - Plays the animation. This will allow all animated assets to be played and for static assets, this should be a no-op.
    
    Screen recording: https://drive.google.com/file/d/1DJ0n5ZI7trf7ThUbOQpVLAfqXrtJ6FMe/view?usp=sharing
    
    Change-Id: If67950ee63f7707421937eb5476f9e740b84583f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5206232
    Reviewed-by: Gayane Petrosyan <gayane@google.com>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1248811}

--- ios/chrome/browser/ui/whats_new/whats_new_screenshot_view_controller.mm
+++ ios/chrome/browser/ui/whats_new/whats_new_screenshot_view_controller.mm
@@ -113,6 +113,7 @@ NSString* const kDarkModeAnimationSuffix = @"_darkmode";
   LottieAnimationConfiguration* config =
       [[LottieAnimationConfiguration alloc] init];
   config.animationName = animationAssetName;
+  config.loopAnimationCount = 1000;
   return ios::provider::GenerateLottieAnimation(config);
 }
 
@@ -181,6 +182,18 @@ NSString* const kDarkModeAnimationSuffix = @"_darkmode";
 
   self.screenshotViewWrapper.animationView.hidden = darkModeEnabled;
   self.screenshotViewWrapperDarkMode.animationView.hidden = !darkModeEnabled;
+  [self updateAnimationsPlaying];
+}
+
+// Checks if the animations are hidden or unhidden and plays (or stops) them
+// accordingly.
+- (void)updateAnimationsPlaying {
+  self.screenshotViewWrapper.animationView.hidden
+      ? [self.screenshotViewWrapper stop]
+      : [self.screenshotViewWrapper play];
+  self.screenshotViewWrapperDarkMode.animationView.hidden
+      ? [self.screenshotViewWrapperDarkMode stop]
+      : [self.screenshotViewWrapperDarkMode play];
 }
 
 @end
