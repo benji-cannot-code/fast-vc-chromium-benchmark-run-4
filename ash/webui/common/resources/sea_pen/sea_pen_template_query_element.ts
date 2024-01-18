376aae3ebb932318785f58389e5e4779cd2b9fa8
Jason Thai <jasontt@chromium.org>
2024-01-18 01:34:42 +0000

    p13n: inspire button triggers search
    BUG=b:320535158
    TEST=browser_tests "*SeaPenTemplateQuery*"
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: Ia5215294271a52b697a0cff26b115ddaba37528b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5207459
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Reviewed-by: Thuong Phan <thuongphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248521}

--- ash/webui/common/resources/sea_pen/sea_pen_template_query_element.ts
+++ ash/webui/common/resources/sea_pen/sea_pen_template_query_element.ts
@@ -149,6 +149,7 @@ export class SeaPenTemplateQueryElement extends WithSeaPenStore {
     }
     this.templateTokens_ =
         getTemplateTokens(this.seaPenTemplate_, this.selectedOptions_);
+    this.onClickSearchButton_();
   }
 
   private onSeaPenTemplateChanged_(template: SeaPenTemplate) {
