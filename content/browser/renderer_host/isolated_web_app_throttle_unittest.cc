a377389865b7ef22be2162d77d790a8d18951eb8
Robbie McElrath <rmcelrath@chromium.org>
2024-01-18 22:30:04 +0000

    iwa: Don't open URLs in default browser in ChromeOS
    Bug: 1310207
    Change-Id: I737cea6209daf5ee6687baa288aac94d7bc83c24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5206710
    Reviewed-by: Christian Flach <cmfcmf@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Zelin Liu <zelin@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249093}

--- content/browser/renderer_host/isolated_web_app_throttle_unittest.cc
+++ content/browser/renderer_host/isolated_web_app_throttle_unittest.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/frame_tree_node.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/content_browser_client.h"
+#include "content/public/browser/page_navigator.h"
+#include "content/public/browser/site_instance.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_client.h"
 #include "content/public/common/content_features.h"
@@ -71,10 +73,25 @@ class IsolatedWebAppContentBrowserClient : public ContentBrowserClient {
     return external_protocol_call_count_;
   }
 
+  void OpenURL(
+      content::SiteInstance* site_instance,
+      const content::OpenURLParams& params,
+      base::OnceCallback<void(content::WebContents*)> callback) override {
+    open_url_call_count_++;
+    last_page_transition_ = params.transition;
+  }
+
+  unsigned int GetOpenUrlCallCount() const { return open_url_call_count_; }
+
   ui::PageTransition GetLastPageTransition() { return last_page_transition_; }
 
   void ResetExternalProtocolCallCount() {
     external_protocol_call_count_ = 0;
+    last_page_transition_ = ui::PageTransition::PAGE_TRANSITION_QUALIFIER_MASK;
+  }
+
+  void ResetOpenUrlCallCount() {
+    open_url_call_count_ = 0;
     last_page_transition_ = ui::PageTransition::PAGE_TRANSITION_QUALIFIER_MASK;
   }
 
@@ -93,6 +110,7 @@ class IsolatedWebAppContentBrowserClient : public ContentBrowserClient {
 
  private:
   unsigned int external_protocol_call_count_ = 0;
+  unsigned int open_url_call_count_ = 0;
   ui::PageTransition last_page_transition_ =
       ui::PageTransition::PAGE_TRANSITION_QUALIFIER_MASK;
 };
@@ -266,7 +284,11 @@ TEST_F(IsolatedWebAppThrottleTest, CancelCrossOriginNavigation) {
 
   auto start_result = simulator->GetLastThrottleCheckResult();
   EXPECT_EQ(NavigationThrottle::CANCEL, start_result.action());
+#if BUILDFLAG(IS_CHROMEOS)
+  EXPECT_EQ(1u, GetBrowserClient().GetOpenUrlCallCount());
+#else
   EXPECT_EQ(1u, GetBrowserClient().GetExternalProtocolCallCount());
+#endif
   EXPECT_TRUE(ui::PageTransitionTypeIncludingQualifiersIs(
       GetBrowserClient().GetLastPageTransition(),
       ui::PageTransition::PAGE_TRANSITION_LINK));
@@ -274,7 +296,11 @@ TEST_F(IsolatedWebAppThrottleTest, CancelCrossOriginNavigation) {
   simulator = StartRendererInitiatedNavigation(main_frame_id(), kNonAppUrl2);
   start_result = simulator->GetLastThrottleCheckResult();
   EXPECT_EQ(NavigationThrottle::CANCEL, start_result.action());
+#if BUILDFLAG(IS_CHROMEOS)
+  EXPECT_EQ(2u, GetBrowserClient().GetOpenUrlCallCount());
+#else
   EXPECT_EQ(2u, GetBrowserClient().GetExternalProtocolCallCount());
+#endif
   EXPECT_TRUE(ui::PageTransitionTypeIncludingQualifiersIs(
       GetBrowserClient().GetLastPageTransition(),
       ui::PageTransition::PAGE_TRANSITION_LINK));
@@ -294,7 +320,11 @@ TEST_F(IsolatedWebAppThrottleTest, BlockRedirectOutOfIsolatedWebApp) {
 
   auto redirect_result = simulator->GetLastThrottleCheckResult();
   EXPECT_EQ(NavigationThrottle::CANCEL, redirect_result.action());
+#if BUILDFLAG(IS_CHROMEOS)
+  EXPECT_EQ(1u, GetBrowserClient().GetOpenUrlCallCount());
+#else
   EXPECT_EQ(1u, GetBrowserClient().GetExternalProtocolCallCount());
+#endif
   EXPECT_TRUE(ui::PageTransitionTypeIncludingQualifiersIs(
       GetBrowserClient().GetLastPageTransition(),
       ui::PageTransition::PAGE_TRANSITION_SERVER_REDIRECT));
