79754d745890a451fd5a7ca2b7c4e98219fec846
Igor <igorcov@chromium.org>
2024-01-18 12:58:14 +0000

    Create InstallAttributesErrorScreen
    The new screen is used to be shown when a critical error occurred and
    the install attributes cannot be recovered on the enrolled device.
    This change notifies the user about the error and provides the option
    to powerwash the device.
    
    BUG=b:314268843
    TEST=Manual test on device.
    
    Change-Id: Ia620352611dd72e9d2879521b393b85b35e18014
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5134975
    Reviewed-by: Brahim Chikhaoui <bchikhaoui@google.com>
    Commit-Queue: Igor <igorcov@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248714}

--- chrome/browser/ash/login/ui/login_display_host_webui.cc
+++ chrome/browser/ash/login/ui/login_display_host_webui.cc
@@ -69,11 +69,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/login/core_oobe_handler.h"
 #include "chrome/browser/ui/webui/ash/login/device_disabled_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/gaia_screen_handler.h"
+#include "chrome/browser/ui/webui/ash/login/install_attributes_error_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/lacros_data_backward_migration_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/lacros_data_migration_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/oobe_ui.h"
 #include "chrome/browser/ui/webui/ash/login/os_install_screen_handler.h"
-#include "chrome/browser/ui/webui/ash/login/tpm_error_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/welcome_screen_handler.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_switches.h"
@@ -211,8 +211,8 @@ void MaybeShowInstallAttributesCorruptedScreen() {
       !InstallAttributes::Get()->IsDeviceLocked()) {
     LOG(ERROR) << "Corrupted install attributes, showing the TPM error";
     base::UmaHistogramBoolean(kValidInstallAttributesHistogram, false);
-    // TODO(b/314268843): Create a separate screen for this error.
-    LoginDisplayHost::default_host()->StartWizard(TpmErrorView::kScreenId);
+    LoginDisplayHost::default_host()->StartWizard(
+        InstallAttributesErrorView::kScreenId);
   } else {
     base::UmaHistogramBoolean(kValidInstallAttributesHistogram, true);
   }
