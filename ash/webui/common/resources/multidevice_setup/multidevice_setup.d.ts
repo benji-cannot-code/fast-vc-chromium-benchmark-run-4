c4e425451680baaf83901f76608079888d88852e
Danila Kuzmin <dkuzmin@google.com>
2024-01-18 14:48:02 +0000

    [oobe] Allow imports from //resources/ash/common
    Bug: b:320233418, b:320242398
    Change-Id: Iff98134940ab64859677f45c37eb039081e8837e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5198094
    Reviewed-by: Bohdan Tyshchenko <bohdanty@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1248737}

---
+++ ash/webui/common/resources/multidevice_setup/multidevice_setup.d.ts
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Minimal TypeScript definitions to satisfy cases where
+ * multidevice_setup.js is used from TypeScript files.
+ */
+
+import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+export class MultiDeviceSetupElement extends PolymerElement {}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'multidevice-setup': MultiDeviceSetupElement;
+  }
+}
--- chrome/browser/resources/chromeos/login/screens/osauth/pin_setup.ts
+++ chrome/browser/resources/chromeos/login/screens/osauth/pin_setup.ts
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '//resources/ash/common/quick_unlock/setup_pin_keyboard.js';
 import '//resources/cr_elements/cr_input/cr_input.js';
 import '//resources/cr_elements/cr_shared_style.css.js';
 import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
@@ -15,6 +14,8 @@ import '../../components/buttons/oobe_back_button.js';
 import '../../components/buttons/oobe_next_button.js';
 import '../../components/buttons/oobe_text_button.js';
 
+import {SetupPinKeyboardElement} from '//resources/ash/common/quick_unlock/setup_pin_keyboard.js';
+import {assert} from '//resources/js/assert.js';
 import {PolymerElementProperties} from '//resources/polymer/v3_0/polymer/interfaces.js';
 import {mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
@@ -131,12 +132,14 @@ class PinSetup extends PinSetupBase {
     return PinSetupState.START;
   }
 
-  onBeforeShow(data: OobeTypes.PinSetupScreenParameters): void {
+  private getPinKeyboard(): SetupPinKeyboardElement {
     const pinKeyboard = this.shadowRoot?.querySelector('#pinKeyboard');
-    if (pinKeyboard) {
-      // TODO(b/320242398): Fix type once SetupPinKeyboardElement can be added.
-      (pinKeyboard as any).resetState();
-    }
+    assert(pinKeyboard instanceof SetupPinKeyboardElement);
+    return pinKeyboard;
+  }
+
+  onBeforeShow(data: OobeTypes.PinSetupScreenParameters): void {
+    this.getPinKeyboard().resetState();
     this.authToken = data.auth_token;
     this.isChildAccount = data.is_child_account;
   }
@@ -156,11 +159,7 @@ class PinSetup extends PinSetupBase {
   }
 
   private onPinSubmit(): void {
-    const pinKeyboard = this.shadowRoot?.querySelector('#pinKeyboard');
-    if (pinKeyboard) {
-      // TODO(b/320242398): Fix type once SetupPinKeyboardElement can be added.
-      (pinKeyboard as any).doSubmit();
-    }
+    this.getPinKeyboard().doSubmit();
   }
 
   private onSetPinDone(): void {
@@ -169,11 +168,7 @@ class PinSetup extends PinSetupBase {
 
   private onSkipButton(): void {
     this.authToken = '';
-    const pinKeyboard = this.shadowRoot?.querySelector('#pinKeyboard');
-    if (pinKeyboard) {
-      // TODO(b/320242398): Fix type once SetupPinKeyboardElement can be added.
-      (pinKeyboard as any).resetState();
-    }
+    this.getPinKeyboard().resetState();
     if (this.uiStep === PinSetupState.CONFIRM) {
       this.userActed('skip-button-in-flow');
     } else {
@@ -182,11 +177,7 @@ class PinSetup extends PinSetupBase {
   }
 
   private onBackButton(): void {
-    const pinKeyboard = this.shadowRoot?.querySelector('#pinKeyboard');
-    if (pinKeyboard) {
-      // TODO(b/320242398): Fix type once SetupPinKeyboardElement can be added.
-      (pinKeyboard as any).resetState();
-    }
+    this.getPinKeyboard().resetState();
     this.setUIStep(PinSetupState.START);
   }
 
@@ -196,11 +187,7 @@ class PinSetup extends PinSetupBase {
 
   private onDoneButton(): void {
     this.authToken = '';
-    const pinKeyboard = this.shadowRoot?.querySelector('#pinKeyboard');
-    if (pinKeyboard) {
-      // TODO(b/320242398): Fix type once SetupPinKeyboardElement can be added.
-      (pinKeyboard as any).resetState();
-    }
+    this.getPinKeyboard().resetState();
     this.userActed('done-button');
   }
 }
