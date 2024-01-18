191a202808065b3fc51ac7880093ac23f2e38690
Luc Nguyen <lucnguyen@google.com>
2024-01-18 19:27:54 +0000

    Add MetricsLogTrimming feature
    This is a no-op. This simply makes so that the trimming parameters can
    all be controlled from the serverside. Some of them were already are,
    but they were for some reason tied to Structured Metrics. This CL
    creates a separate feature. I also double checked to see if there are
    any Structured Metrics serverside studies that change those params
    currently (there are none).
    
    Bug: 1519156
    Change-Id: Ia3e6578c53cdd09154fe3f64d54817cfa0d002ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5206231
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Commit-Queue: Luc Nguyen <lucnguyen@google.com>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248959}

--- components/metrics/metrics_features.cc
+++ components/metrics/metrics_features.cc
@@ -27,4 +27,8 @@ BASE_FEATURE(kReportingServiceAlwaysFlush,
              "ReportingServiceAlwaysFlush",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kMetricsLogTrimming,
+             "MetricsLogTrimming",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 }  // namespace metrics::features
