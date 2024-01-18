b87670b8a54e4bea7e368bb588672fad778785b0
Matt Menke <mmenke@chromium.org>
2024-01-18 16:27:45 +0000

    [Protected Audiences] Add wasmURL/updateURLgenerateBid() WPT tests.
    Make sure generateBid() gets biddingWasmHelperURL and updateURL. Since
    the WASM tests require a valid WASM file to run generateBid(), also add
    such a file, and add some no-winner WASM tests as well, where the WASM
    file fails to load, for various reasons.
    
    Also make sure that tests that use cross-origin iframes use UTF-8.
    For some reason, the cross-origin iframe used by some sets were
    being rendered with Windows-1252, which affects how unpair surrogates
    are handled.
    
    We still need tests that the WASM file is actually successfully parsed
    as WASM and can be executed, but that's an issue for another day. The
    WASM script the tests currently use is incrementer.wasm, which is
    used by a bunch of other WPT tests. No tests currently actually call
    into the WASM code, they just load it.
    
    Bug: 1425952
    Change-Id: I51e6037664491ac8bf6faceed0f6a53e13d8745c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5189267
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248813}

--- third_party/blink/web_tests/external/wpt/fledge/tentative/no-winner.https.window.js
+++ third_party/blink/web_tests/external/wpt/fledge/tentative/no-winner.https.window.js
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: variant=?21-25
 // META: variant=?26-30
 // META: variant=?31-35
-// META: variant=?36-last
+// META: variant=?36-40
+// META: variant=?41-45
+// META: variant=?46-last
 
 "use strict;"
 
@@ -19,10 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // origin, one frame) which have no winning bid, either due to errors or due to
 // there being no bids, except where tests fit better with another set of tests.
 
-// Errors common to bidding and decision logic scripts. These atrings will be
-// appended to script URLs to make the python scripts that generate bidding
-// logic and decision logic scripts with errors.
-const COMMON_SCRIPT_ERRORS = [
+// Errors common to Protected Audiences network requests. These strings will be
+// appended to URLs to make the Python scripts that generate responses respond
+// with errors.
+const COMMON_NETWORK_ERRORS = [
   'error=close-connection',
   'error=http-error',
   'error=no-content-type',
@@ -34,7 +36,7 @@ const COMMON_SCRIPT_ERRORS = [
 ];
 
 const BIDDING_LOGIC_SCRIPT_ERRORS = [
-  ...COMMON_SCRIPT_ERRORS,
+  ...COMMON_NETWORK_ERRORS,
   'error=no-generateBid',
   'generateBid=throw 1;',
   'generateBid=This does not compile',
@@ -49,11 +51,11 @@ const BIDDING_LOGIC_SCRIPT_ERRORS = [
   'generateBid=return {render: interestGroup.ads[0].renderURL};',
   // These are not bidding rather than errors.
   'generateBid=return {bid:0, render: interestGroup.ads[0].renderURL};',
-  'generateBid=return {bid:-1, render: interestGroup.ads[0].renderURL};',
+  'generateBid=return {bid:-1, render: interestGroup.ads[0].renderURL};'
 ];
 
 const DECISION_LOGIC_SCRIPT_ERRORS = [
-  ...COMMON_SCRIPT_ERRORS,
+  ...COMMON_NETWORK_ERRORS,
   'error=no-scoreAd',
   'scoreAd=throw 1;',
   'scoreAd=This does not compile',
@@ -66,7 +68,12 @@ const DECISION_LOGIC_SCRIPT_ERRORS = [
   'scoreAd=return 0;',
   'scoreAd=return -1;',
   'scoreAd=return {desirability: 0};',
-  'scoreAd=return {desirability: -1};',
+  'scoreAd=return {desirability: -1};'
+];
+
+const BIDDING_WASM_HELPER_ERRORS = [
+  ...COMMON_NETWORK_ERRORS,
+  'error=not-wasm'
 ];
 
 for (error of BIDDING_LOGIC_SCRIPT_ERRORS) {
@@ -87,4 +94,14 @@ for (error of DECISION_LOGIC_SCRIPT_ERRORS) {
       test, { auctionConfigOverrides: { decisionLogicURL: decisionLogicURL } }
     );
   }).bind(undefined, error), `Decision logic script: ${error}`);
+}
+
+for (error of BIDDING_WASM_HELPER_ERRORS) {
+  subsetTest(promise_test, (async (error, test) => {
+    let biddingWasmHelperURL =
+        `${BASE_URL}resources/wasm-helper.py?${error}`;
+    await joinGroupAndRunBasicFledgeTestExpectingNoWinner(
+      test, { interestGroupOverrides: { biddingWasmHelperURL: biddingWasmHelperURL } }
+    );
+  }).bind(undefined, error), `Bidding WASM helper: ${error}`);
 }
