7a0228f6d59555ad93aef2a1ee5c9045171d2049
Roger Johannesson <roger.johannesson@xperi.com>
2024-01-18 16:19:21 +0000

    Don't request renderpass textures bigger than max texture size
    Some Mali GPUs has GL_MAX_TEXTURE_SIZE set to 16383 which is not a
    multiple of 64. When calculating the renderpass texture size both
    width and height are currently rounded up to a multiple of 64. So
    when a renderpass is 16383 on these devices it gets rounded up to
    16384 which is larger than the max texture size. Such a large
    renderpass was requested when performing a view-transition on a
    very large iframe in https://crbug.com/1516874
    
    This patch clamps the rounded renderpass texture to the max texture
    size.
    
    Bug: 1516874
    Change-Id: I0ff76e3431129be4add162be5ce37f0f4dab43e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5183026
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Roger Johannesson <roger.johannesson@xperi.com>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248810}

--- components/viz/service/display/direct_renderer.cc
+++ components/viz/service/display/direct_renderer.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits.h>
 #include <stddef.h>
 
+#include <algorithm>
 #include <utility>
 #include <vector>
 
@@ -997,7 +998,20 @@ gfx::Size DirectRenderer::CalculateTextureSizeForRenderPass(
     constexpr int multiple = 64;
     width = cc::MathUtil::CheckedRoundUp(width, multiple);
     height = cc::MathUtil::CheckedRoundUp(height, multiple);
+
+    // There are no guarantees that max texture size is a multiple of 64 so
+    // clamp the rounded up dimensions to avoid ending up with dimensions
+    // larger than max texture size. Note: Software surfaces and some
+    // test only surfaces does not have max_texture_size set so assume that
+    // we don't have to clamp the dimensions when max texture size is zero.
+    const int max_texture_size =
+        output_surface_->capabilities().max_texture_size;
+    if (max_texture_size > 0) {
+      width = std::min(max_texture_size, width);
+      height = std::min(max_texture_size, height);
+    }
   }
+
   return gfx::Size(width, height);
 }
 
