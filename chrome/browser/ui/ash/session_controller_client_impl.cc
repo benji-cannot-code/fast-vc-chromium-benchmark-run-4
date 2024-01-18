2c4e8d7d6878e71b8df1809b1c9986cc2c37d65d
zhumatthew <zhumatthew@google.com>
2024-01-18 13:28:39 +0000

    fws: Add fws capture logic to lock screen
    Add the capture and upload feature for fws prior to locking the
    screen.
    
    BUG=b:319247699
    TEST=FloatingWorkspaceServiceTest,SessionControllerImpl*,
    SessionControllerClientImpl*
    
    Change-Id: I91e014c0430dd415b88c42ff326b136a459094bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5181776
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Matthew Zhu <zhumatthew@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1248725}

--- chrome/browser/ui/ash/session_controller_client_impl.cc
+++ chrome/browser/ui/ash/session_controller_client_impl.cc
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/app_mode/app_mode_utils.h"
 #include "chrome/browser/ash/crosapi/browser_manager.h"
 #include "chrome/browser/ash/crosapi/browser_util.h"
+#include "chrome/browser/ash/floating_workspace/floating_workspace_service.h"
+#include "chrome/browser/ash/floating_workspace/floating_workspace_util.h"
 #include "chrome/browser/ash/login/demo_mode/demo_session.h"
 #include "chrome/browser/ash/login/lock/screen_locker.h"
 #include "chrome/browser/ash/login/ui/user_adding_screen.h"
@@ -200,6 +202,17 @@ SessionControllerClientImpl* SessionControllerClientImpl::Get() {
 }
 
 void SessionControllerClientImpl::PrepareForLock(base::OnceClosure callback) {
+  if (ash::floating_workspace_util::IsFloatingWorkspaceV2Enabled()) {
+    User* active_user = UserManager::Get()->GetActiveUser();
+    Profile* profile = ash::ProfileHelper::Get()->GetProfileByUser(active_user);
+    if (profile) {
+      auto* floating_workspace_service =
+          ash::FloatingWorkspaceService::GetForProfile(profile);
+      if (floating_workspace_service) {
+        floating_workspace_service->CaptureAndUploadActiveDesk();
+      }
+    }
+  }
   session_controller_->PrepareForLock(std::move(callback));
 }
 
