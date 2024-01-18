b3f3651b7e65712a3bf5fd789ad696f37d202b66
Paul Semel <paulsemel@chromium.org>
2024-01-18 10:46:48 +0000

    fuzzing: introduce a test-only fuzzing IDL
    The point of this IDL is to help fuzz the code base from a renderer POV
    (understand, as if the renderer would be compromised).
    
    To declare a new renderer fuzzer, one just need to inherit from
    RendererFuzzerBase and declare the new class with
    REGISTER_RENDERER_FUZZER.
    
    Registering the renderer fuzzer allows to dynamically discover fuzzers
    without the need to depend on all of them at compile time (since
    `third_party/blink/renderer/modules` can be limited regarding its deps).
    See crrev.com/c/5134508 for a thorough example on how this works.
    
    Change-Id: Ic7b67cab9be845a240316b0e841b9caf926e87fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5123051
    Reviewed-by: Chris Bookholt <bookholt@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Commit-Queue: Paul Semel <paulsemel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248676}

---
+++ third_party/blink/renderer/platform/testing/renderer_fuzzing_support.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "renderer_fuzzing_support.h"
+
+#include "testing/libfuzzer/renderer_fuzzing/renderer_fuzzing.h"
+
+namespace blink {
+
+// static
+void RendererFuzzingSupport::Run(
+    const blink::BrowserInterfaceBrokerProxy* context_interface_broker_proxy,
+    blink::ThreadSafeBrowserInterfaceBrokerProxy*
+        process_interface_broker_proxy,
+    const std::string& fuzzer_id,
+    std::vector<uint8_t>&& input,
+    base::OnceClosure done_closure) {
+  RendererFuzzing::Run(context_interface_broker_proxy,
+                       process_interface_broker_proxy, fuzzer_id,
+                       std::move(input), std::move(done_closure));
+}
+
+}  // namespace blink
