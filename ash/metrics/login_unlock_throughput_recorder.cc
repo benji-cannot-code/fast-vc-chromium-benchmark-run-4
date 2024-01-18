6083e99e8a1516f07d44a1f01033f6bd1db3a201
Alexander Alekseev <alemate@chromium.org>
2024-01-18 21:34:49 +0000

    [ash] Stop limiting BootTimes.Login3 metric
    The existing code limiting BootTimes.Login3 metric to 20 seconds seems to
    conflict with the lab setup when testing and monitoring code adds too much
    overhead. This CL removes the limit so that we could simplify the test setup.
    
    Bug: b/316036494, b/315417366
    Change-Id: I14189b076e40f4ff30528773b94d1d542536c7c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5208360
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Auto-Submit: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249051}

--- ash/metrics/login_unlock_throughput_recorder.cc
+++ ash/metrics/login_unlock_throughput_recorder.cc
@@ -695,7 +695,6 @@ void LoginUnlockThroughputRecorder::OnLoginAnimationFinishedTimerFired() {
   TRACE_EVENT0(
       "startup",
       "LoginUnlockThroughputRecorder::OnLoginAnimationFinishedTimerFired");
-  login_finished_reported_ = true;
   post_login_deferred_task_runner_->Start();
 }
 
