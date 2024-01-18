766d516fae8cf81e525cc0f23d2214b00ebbfd97
Peter Birk Pakkenberg <pbirk@chromium.org>
2024-01-18 16:56:42 +0000

    Control 3P cookies in WebView Shell
    This CL adds an option to turn third party cookies off in the System
    WebView Shell app for easier testing.
    
    Change-Id: Id6aaf3faaf839c9e2c4ef03fb2fbaf17044328a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5210878
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Commit-Queue: Peter Pakkenberg <pbirk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248830}

--- android_webview/tools/system_webview_shell/apk/src/org/chromium/webview_shell/WebViewBrowserActivity.java
+++ android_webview/tools/system_webview_shell/apk/src/org/chromium/webview_shell/WebViewBrowserActivity.java
@@ -167,6 +167,12 @@ public class WebViewBrowserActivity extends AppCompatActivity {
                             WebSettingsCompat.isAlgorithmicDarkeningAllowed(
                                     mWebView.getSettings()));
         }
+
+        menu.findItem(R.id.menu_enable_third_party_cookies).setEnabled(mWebView != null);
+        if (mWebView != null) {
+            menu.findItem(R.id.menu_enable_third_party_cookies)
+                    .setChecked(CookieManager.getInstance().acceptThirdPartyCookies(mWebView));
+        }
         return true;
     }
 
@@ -240,6 +246,13 @@ public class WebViewBrowserActivity extends AppCompatActivity {
                         !WebSettingsCompat.isAlgorithmicDarkeningAllowed(mWebView.getSettings()));
             }
             return true;
+        } else if (itemId == R.id.menu_enable_third_party_cookies) {
+            if (mWebView != null) {
+                boolean enable = !item.isChecked();
+                CookieManager.getInstance().setAcceptThirdPartyCookies(mWebView, enable);
+                item.setChecked(enable);
+                mWebView.reload(); // Reload to apply the settings.
+            }
         } else if (itemId == R.id.menu_multi_profile) {
             startActivity(new Intent(this, WebViewMultiProfileBrowserActivity.class));
             return true;
