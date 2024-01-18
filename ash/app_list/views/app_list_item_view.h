b82348918e1db06888a2a14a6b183981dc1a1ea6
Owen Zhang <owenzhang@google.com>
2024-01-18 19:43:52 +0000

    [App Shortcuts] Create New Image Operation For Shortcuts.
    This CL adds a new method to draw shortcut icon effects that include the icon halos and the badge image in one operation, and refactors AppListItemView to use this new method. In a follow on CL, shelf and applist search will be refactored too.
    
    Change-Id: I240dacc4af50aee5eb48b7e63dc6e89fdb61579c
    bug: b/316049065
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5138055
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Owen Zhang <owenzhang@google.com>
    Reviewed-by: Ana Salazar <anasalazar@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1248970}

--- ash/app_list/views/app_list_item_view.h
+++ ash/app_list/views/app_list_item_view.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/layer_tree_owner.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
+#include "ui/gfx/image/image_skia.h"
 #include "ui/views/context_menu_controller.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/image_view.h"
@@ -170,8 +171,9 @@ class ASH_EXPORT AppListItemView : public views::Button,
   // is true.
   void UpdateIconView(bool update_item_icon);
 
-  // Sets the icon of this image.
-  void SetIcon(const gfx::ImageSkia& icon);
+  // Sets the icon and host badge icon of this image.
+  void SetIconAndMaybeHostBadgeIcon(const gfx::ImageSkia& icon,
+                                    const gfx::ImageSkia& host_badge_icon);
 
   // Returns the main app icon size for the associated item. This is the actual
   // size of the main app icon that is painted in the grid.
@@ -344,11 +346,12 @@ class ASH_EXPORT AppListItemView : public views::Button,
   bool is_promise_app() const { return is_promise_app_; }
   std::optional<size_t> item_counter_count_for_test() const;
   ProgressIndicator* GetProgressIndicatorForTest() const;
-  views::ImageView* GetHostBadgeIconViewForTest() const;
+  bool has_host_badge_for_test() const { return has_host_badge_; }
 
  private:
   class FolderIconView;
 
+  friend class AppListFolderViewTest;
   friend class AppListItemViewTest;
   friend class AppListMainViewTest;
   friend class test::AppsGridViewTest;
@@ -531,9 +534,6 @@ class ASH_EXPORT AppListItemView : public views::Button,
   // The folder icon view used for refreshed folders.
   raw_ptr<FolderIconView> folder_icon_ = nullptr;
 
-  // The host badge icon view used for app shortcuts.
-  raw_ptr<views::ImageView> host_badge_icon_view_ = nullptr;
-
   raw_ptr<views::Label> title_ = nullptr;
 
   // The background layer added under the `icon_` layer to paint the background
@@ -584,6 +584,11 @@ class ASH_EXPORT AppListItemView : public views::Button,
 
   // The bitmap image for this app list item's host badge icon.
   gfx::ImageSkia host_badge_icon_image_;
+
+  // The bitmap image for this app list item's main icon. This is separate from
+  // icon_->GetImage(), since the latter might contain the badge image in its
+  // imageSkia for shortcuts.
+  gfx::ImageSkia icon_image_;
 
   // If set, the icon that will be used for the AppListItemView until the actual
   // app icon loads. Used when animating an installed app into a place of a
--- ash/app_list/views/app_list_item_view_unittest.cc
+++ ash/app_list/views/app_list_item_view_unittest.cc
@@ -557,10 +557,9 @@ TEST_P(AppListItemViewTest, ShortcutIconEffectsShowOnShorcutItemWithHostBadge) {
 
   auto* apps_grid_view = helper->GetScrollableAppsGridView();
   AppListItemView* view = apps_grid_view->GetItemViewAt(0);
-  views::ImageView* badge_icon_view = view->GetHostBadgeIconViewForTest();
 
   EXPECT_FALSE(item->GetHostBadgeIcon().isNull());
-  EXPECT_TRUE(badge_icon_view);
+  EXPECT_TRUE(view->has_host_badge_for_test());
 }
 
 TEST_P(AppListItemViewTest, NoShortcutIconEffectOntItemWithoutHostBadge) {
@@ -571,10 +570,9 @@ TEST_P(AppListItemViewTest, NoShortcutIconEffectOntItemWithoutHostBadge) {
 
   auto* apps_grid_view = helper->GetScrollableAppsGridView();
   AppListItemView* view = apps_grid_view->GetItemViewAt(0);
-  views::ImageView* badge_icon_view = view->GetHostBadgeIconViewForTest();
 
   EXPECT_TRUE(item->GetHostBadgeIcon().isNull());
-  EXPECT_FALSE(badge_icon_view);
+  EXPECT_FALSE(view->has_host_badge_for_test());
 }
 
 TEST_P(AppListItemViewTestWithDragDropController,
@@ -586,7 +584,6 @@ TEST_P(AppListItemViewTestWithDragDropController,
 
   auto* apps_grid_view = helper->GetScrollableAppsGridView();
   AppListItemView* view = apps_grid_view->GetItemViewAt(0);
-  views::ImageView* badge_icon_view = view->GetHostBadgeIconViewForTest();
   auto* generator = GetEventGenerator();
   ASSERT_EQ(GetDragState(view), AppListItemView::DragState::kNone);
 
@@ -619,7 +616,7 @@ TEST_P(AppListItemViewTestWithDragDropController,
   EXPECT_FALSE(IsIconScaled(view));
 
   EXPECT_FALSE(item->GetHostBadgeIcon().isNull());
-  EXPECT_TRUE(badge_icon_view);
+  EXPECT_TRUE(view->has_host_badge_for_test());
 
   MaybeCheckDragStartedOnControllerCount(1);
 }
