47e8f636fe201c1d72d7f07ded469e07760e11f7
Tiago Vignatti <tvignatti@igalia.com>
2024-01-19 21:34:42 +0000

    Rename //net/base/mac directory to //net/base/apple (1/n)
    This renames the remaining directory in //net/base used both by macOS
    and iOS, following the naming convention and changing it something more
    suggestive.
    
    Like suggested by mark@, this renaming process will be carried on in
    three steps:
    
    1. Rename net/base/mac to net/base/apple, creating new temporary
    forwarding headers in net/base/mac that #include net/base/apple. The
    changes here are performing this step.
    
    2. Convert all of the use sites from net/base/mac to net/base/apple.
    This can be done across multiple ch anges if desirable, which could make
    things more manageable for reviewers.
    
    3. Remove the temporary forwarding headers in net/base/mac, so that
    we’re only left with net/base/apple.
    
    Bug: 1519486
    Change-Id: I94cfc6c178286cc35b830a35b06488c80b68fb6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5211389
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Tiago Vignatti <tvignatti@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1249532}

--- net/base/apple/url_conversions.mm
+++ net/base/apple/url_conversions.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "net/base/mac/url_conversions.h"
+#import "net/base/apple/url_conversions.h"
 
 #import <Foundation/Foundation.h>
 
--- net/base/apple/url_conversions_unittest.mm
+++ net/base/apple/url_conversions_unittest.mm
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/Foundation.h>
 
 #include "base/strings/sys_string_conversions.h"
-#import "net/base/mac/url_conversions.h"
+#import "net/base/apple/url_conversions.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/gtest_mac.h"
 #include "url/gurl.h"
