beba909795afafdedb60b03d46efa98bb99d36df
Anupam Snigdha <snianu@microsoft.com>
2024-01-19 21:50:55 +0000

    Remove IMM32 from Windows.
    Remove IMM32 support on Windows as Win7 support has ended and TSF
    is on by-default for many years now.
    
    Bug: 1519933
    
    Change-Id: I46afe643429452ad9ae6721dab7b1ebcb1d9a360
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5217731
    Reviewed-by: Yohei Yukawa <yukawa@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Anupam Snigdha <snianu@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1249554}

--- ui/base/ime/win/tsf_bridge.cc
+++ ui/base/ime/win/tsf_bridge.cc
@@ -717,10 +717,6 @@ HRESULT TSFBridge::Initialize() {
     return S_OK;
   }
 
-  // If we aren't supporting TSF early out.
-  if (!base::FeatureList::IsEnabled(features::kTSFImeSupport))
-    return E_FAIL;
-
   auto delegate = std::make_unique<TSFBridgeImpl>();
   HRESULT hr = delegate->Initialize();
   if (SUCCEEDED(hr)) {
@@ -734,8 +730,6 @@ void TSFBridge::InitializeForTesting() {
   if (!base::CurrentUIThread::IsSet()) {
     return;
   }
-  if (!base::FeatureList::IsEnabled(features::kTSFImeSupport))
-    return;
   ReplaceThreadLocalTSFBridge(std::make_unique<MockTSFBridge>());
 }
 
