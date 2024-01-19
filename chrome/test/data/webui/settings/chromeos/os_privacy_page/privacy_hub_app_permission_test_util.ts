7dba979441a8415226bb8bf8d2b06ba2bfae14ef
Jan Lanik <janlanik@google.com>
2024-01-19 22:55:28 +0000

    privacy-hub: Add system apps to location subpage
    This CL adds the system apps to the the System services sections of the
    'Location Access' subpage.
    Screenshot: https://screenshot.googleplex.com/4jf4CD62iUWFp32.png
    
    Bug: b/314122712
    Change-Id: I4784c49708feae8d3ff4e055978a98cd4db8f29e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5198056
    Commit-Queue: Jan Láník <janlanik@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249624}

--- chrome/test/data/webui/settings/chromeos/os_privacy_page/privacy_hub_app_permission_test_util.ts
+++ chrome/test/data/webui/settings/chromeos/os_privacy_page/privacy_hub_app_permission_test_util.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {SettingsPrivacyHubCameraSubpage, SettingsPrivacyHubMicrophoneSubpage} from 'chrome://os-settings/lazy_load.js';
+import {SettingsPrivacyHubCameraSubpage, SettingsPrivacyHubGeolocationSubpage, SettingsPrivacyHubMicrophoneSubpage} from 'chrome://os-settings/lazy_load.js';
 import {appPermissionHandlerMojom, SettingsPrivacyHubSystemServiceRow} from 'chrome://os-settings/os_settings.js';
 import {AppType, PermissionType, TriState} from 'chrome://resources/cr_components/app_management/app_management.mojom-webui.js';
 import {createTriStatePermission} from 'chrome://resources/cr_components/app_management/permission_util.js';
@@ -31,7 +31,7 @@ export function createFakeMetricsPrivate(): FakeMetricsPrivate {
 
 export function getSystemServicesFromSubpage(
     subpage: SettingsPrivacyHubCameraSubpage|
-    SettingsPrivacyHubMicrophoneSubpage):
+    SettingsPrivacyHubMicrophoneSubpage|SettingsPrivacyHubGeolocationSubpage):
     NodeListOf<SettingsPrivacyHubSystemServiceRow> {
   return subpage.shadowRoot!.querySelectorAll(
       'settings-privacy-hub-system-service-row');
@@ -41,4 +41,10 @@ export function getSystemServicePermissionText(
     systemService: SettingsPrivacyHubSystemServiceRow): string {
   return systemService.shadowRoot!
       .querySelector<HTMLDivElement>('#permissionState')!.innerText.trim();
+}
+
+export function getSystemServiceName(
+    systemService: SettingsPrivacyHubSystemServiceRow): string {
+  return systemService.shadowRoot!.querySelector<HTMLElement>(
+                                      '#serviceName')!.innerText.trim();
 }
