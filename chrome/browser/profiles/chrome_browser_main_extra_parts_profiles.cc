a4b258ee5314ed89b6c257353248f5c294305cee
Scott Haseley <shaseley@google.com>
2024-01-19 23:06:06 +0000

    Revert "SmartCard: Permission Context"
    This reverts commit 96d25ca77ab5a0b5b201079c41dece0c392a27c2.
    
    Reason for revert: A test added in the CL is failing on Msan, e.g.
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/40786/test-results?sortby=&groupby=
    
    Original change's description:
    > SmartCard: Permission Context
    >
    > go/web-smart-card-api-permissions
    >
    > Bug: 1464851
    > Low-Coverage-Reason: TESTS_IN_SEPARATE_CL Testing of SmartCardReaderTracker will come alongside its implementation in another CL.
    > Change-Id: I00e6e288176fce17de5ed601d514dc38151f1b2c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5053734
    > Reviewed-by: Florian Jacky <fjacky@chromium.org>
    > Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    > Reviewed-by: Balazs Engedy <engedy@chromium.org>
    > Reviewed-by: Caroline Rising <corising@chromium.org>
    > Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    > Commit-Queue: Daniel d'Andrada <dandrader@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1249280}
    
    Bug: 1464851, 1520045
    Change-Id: Ia5bd2419e04b95dc594534a514cd296587627ae0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5218200
    Commit-Queue: Scott Haseley <shaseley@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Scott Haseley <shaseley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1249635}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -310,7 +310,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/dlp/dlp_rules_manager_factory.h"
 #include "chrome/browser/policy/networking/policy_cert_service_factory.h"
 #include "chrome/browser/policy/networking/user_network_configuration_updater_factory.h"
-#include "chrome/browser/smart_card/smart_card_permission_context_factory.h"
 #include "chromeos/constants/chromeos_features.h"
 #endif
 
@@ -1171,9 +1170,6 @@ void ChromeBrowserMainExtraPartsProfiles::
   if (site_engagement::SiteEngagementService::IsEnabled()) {
     site_engagement::SiteEngagementServiceFactory::GetInstance();
   }
-#if BUILDFLAG(IS_CHROMEOS)
-  SmartCardPermissionContextFactory::GetInstance();
-#endif
 #if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_CHROMEOS_LACROS)
   SpeechRecognitionClientBrowserInterfaceFactory::EnsureFactoryBuilt();
 #endif
