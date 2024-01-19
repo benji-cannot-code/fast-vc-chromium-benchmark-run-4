fe4f639f09e2c906520f0ad682ea3ca5acb335d1
François Doray <fdoray@chromium.org>
2024-01-19 23:21:31 +0000

    Remove redundant SetTaskRunner in RecentlyAudibleHelperTest.
    base::CurrentThread::Get()->SetTaskRunner() is redundant with
    instantiating a base::TestMockTimeTaskRunner::ScopedContext (both
    set the value returned by
    SingleThreadTaskRunner|SequencedTaskRunner::GetCurrentDefault) and
    disallowed in an upcoming cl (crrev.com/c/5137138).
    
    Bug: 1503967
    Change-Id: I7d8897590c598df76d71c7598b2dba4c1f27d975
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5192686
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249653}

--- chrome/browser/ui/recently_audible_helper_unittest.cc
+++ chrome/browser/ui/recently_audible_helper_unittest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
 #include "base/run_loop.h"
-#include "base/task/current_thread.h"
 #include "base/test/test_mock_time_task_runner.h"
 #include "chrome/test/base/testing_profile.h"
 #include "content/public/test/browser_task_environment.h"
@@ -38,7 +37,6 @@ class RecentlyAudibleHelperTest : public testing::Test {
     scoped_context_ =
         std::make_unique<base::TestMockTimeTaskRunner::ScopedContext>(
             task_runner_);
-    base::CurrentThread::Get()->SetTaskRunner(task_runner_);
 
     RecentlyAudibleHelper::CreateForWebContents(contents_);
     helper_ = RecentlyAudibleHelper::FromWebContents(contents_);
