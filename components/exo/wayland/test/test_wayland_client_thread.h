5cc49c3197a9d996b5029f6c819eab9ef73c5c1d
Jianhui Dai <jianhui.j.dai@intel.com>
2024-01-19 01:51:22 +0000

    Exo testing: Stop watching the descriptor on task runner
    This CL moves `StopWatchingFileDescriptor()` to the task runner,
    addressing thread-check enforcement by the underlying
    `MessagePumpEpoll` [1]. A `stopped_` flag is introduced for state
    management, but the overall functionality remains unchanged.
    
    [1] message_pump_epoll.h - Chromium Code Search
    https://source.chromium.org/chromium/chromium/src/+/main:base/message_loop/message_pump_epoll.h;l=159;bpv=1;bpt=0
    
    Bug: 1243354
    Change-Id: I58eccaac97991f043f104dd40f700f0e68f6d9d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5214657
    Reviewed-by: Eliot Courtney <edcourtney@chromium.org>
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Commit-Queue: Jianhui J Dai <jianhui.j.dai@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1249187}

--- components/exo/wayland/test/test_wayland_client_thread.h
+++ components/exo/wayland/test/test_wayland_client_thread.h
@@ -53,6 +53,8 @@ class TestWaylandClientThread : public base::Thread,
 
   base::MessagePumpLibevent::FdWatchController controller_;
   std::unique_ptr<TestClient> client_;
+
+  bool stopped_ = false;
 };
 
 }  // namespace exo::wayland::test
