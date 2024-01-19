8e669c6a314c1e496106a642acd151b6fb60719b
Chris Fredrickson <cfredric@chromium.org>
2024-01-19 14:06:40 +0000

    Remove default arguments from virtual method overrides
    The style guide bans default arguments for virtual functions, and
    there's a clang-tidy warning for it.
    
    https://google.github.io/styleguide/cppguide.html#Default_Arguments
    https://clang.llvm.org/extra/clang-tidy/checks/google/default-arguments.html
    
    Change-Id: I750bb49353e195c8d79656eb63054ee222e4d3ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5207032
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249327}

--- services/network/cookie_settings.h
+++ services/network/cookie_settings.h
@@ -155,8 +155,9 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) CookieSettings
   // given top level site. PST for specific origins can be disabled through
   // content settings.
   bool ArePrivateStateTokensAllowed(const GURL& primary_url) const {
-    ContentSetting setting = GetContentSetting(primary_url, primary_url,
-                                               ContentSettingsType::COOKIES);
+    ContentSetting setting =
+        GetContentSetting(primary_url, primary_url,
+                          ContentSettingsType::COOKIES, /*info=*/nullptr);
     return (setting == CONTENT_SETTING_ALLOW);
   }
 
@@ -168,7 +169,7 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) CookieSettings
       const GURL& primary_url,
       const GURL& secondary_url,
       ContentSettingsType content_type,
-      content_settings::SettingInfo* info = nullptr) const override;
+      content_settings::SettingInfo* info) const override;
   bool IsThirdPartyCookiesAllowedScheme(
       const std::string& scheme) const override;
   bool ShouldBlockThirdPartyCookies() const override;
