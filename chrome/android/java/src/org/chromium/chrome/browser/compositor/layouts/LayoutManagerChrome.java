f583887266251a63a216e7e83aaf2061d418bf73
Calder Kitagawa <ckitagawa@chromium.org>
2024-01-19 00:06:19 +0000

    [Hub] Fix LayoutManager init for Hub
    LayoutManager testonly code forced tab switcher init could repeatedly
    create the HubLayout. Add a check to prevent this and return the Hub
    as the TabSwitcherLayout if the TabSwitcherLayout doesn't exist.
    
    Also disable the Hub feature for the TabSwitcher related tests in
    LayoutManagerTest. For now I don't plan to create parallel Hub tests
    for these since they are better covered via integration tests with
    Public Transit.
    
    Hub uses a cached flag these issues were found by forcing the flag on
    in a testonly CL in preparation for landing a fieldtrial config update.
    https://chromium-review.googlesource.com/c/chromium/src/+/5211686
    
    Bug: b/321005166
    Change-Id: I095978c3c24786bfd611725d6ae5f513745d616f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5215005
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249152}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/LayoutManagerChrome.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/LayoutManagerChrome.java
@@ -502,7 +502,10 @@ public class LayoutManagerChrome extends LayoutManagerImpl
         }
 
         if (isRefactorEnabled && HubFieldTrial.isHubEnabled()) {
-            createHubLayout(mHubLayoutDependencyHolder);
+            if (mHubLayout == null) {
+                createHubLayout(mHubLayoutDependencyHolder);
+            }
+
             return;
         }
 
@@ -667,7 +670,8 @@ public class LayoutManagerChrome extends LayoutManagerImpl
      * non-null when the Start surface refactor is enabled if init has finished.
      */
     public Layout getTabSwitcherLayoutForTesting() {
-        return mTabSwitcherLayout;
+        if (mTabSwitcherLayout != null) return mTabSwitcherLayout;
+        return mHubLayout;
     }
 
     /** Returns the {@link StripLayoutHelperManager} managed by this class. */
