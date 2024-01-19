24146dc83a8cbda49084c8f79d4c9e11cce3753d
William Liu <liuwilliam@chromium.org>
2024-01-19 19:46:32 +0000

    [Default Nav Transition] Notify the observer of OnDetachCompositor before resetting the compositor
    If the observer has some UIResources, the observer won't be able to
    release them if the compositor_ is set to null before the notification.
    
    This CL changes the order to give the observers a last chance to react
    before the compositor is detached
    
    Bug: 1499920
    Change-Id: I1a9b9e4960084b42ab6bb1b2dba21df5c69d72d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5217495
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249445}

--- ui/android/window_android.cc
+++ ui/android/window_android.cc
@@ -127,10 +127,10 @@ void WindowAndroid::AttachCompositor(WindowAndroidCompositor* compositor) {
 }
 
 void WindowAndroid::DetachCompositor() {
-  compositor_ = nullptr;
   for (WindowAndroidObserver& observer : observer_list_)
     observer.OnDetachCompositor();
   observer_list_.Clear();
+  compositor_ = nullptr;
 }
 
 float WindowAndroid::GetRefreshRate() {
