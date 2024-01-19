70171de9a8abfc074cfa90477353622b1f7ce736
Calder Kitagawa <ckitagawa@chromium.org>
2024-01-19 23:13:37 +0000

    [Tab Switcher] Fix crash if there is no (or empty) tab geometry
    Fix a crash if there is empty tab geometry for the current tab when
    hiding or showing the Hub.
    
    Bug: b/321299142
    Change-Id: I824c38f27ce8636369dab85a036831a25e3c0c56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5218712
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249644}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherPaneBase.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherPaneBase.java
@@ -21,6 +21,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
+import org.chromium.base.Log;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.supplier.ObservableSupplierImpl;
@@ -54,6 +55,8 @@ import java.util.List;
  * incognito modes.
  */
 public abstract class TabSwitcherPaneBase implements Pane, TabSwitcherResetHandler {
+    private static final String TAG = "TabSwitcherPaneBase";
+
     protected final ObservableSupplierImpl<DisplayButtonData> mReferenceButtonDataSupplier =
             new ObservableSupplierImpl<>();
     protected final ObservableSupplierImpl<FullButtonData> mNewTabButtonDataSupplier =
@@ -253,6 +256,11 @@ public abstract class TabSwitcherPaneBase implements Pane, TabSwitcherResetHandl
                         finalRect = coordinator.getRecyclerViewRect();
                         leftOffset = finalRect.left;
                     }
+                    boolean useFallbackAnimation = false;
+                    if (initialRect.isEmpty() || finalRect.isEmpty()) {
+                        Log.d(TAG, "Geometry not ready using fallback animation.");
+                        useFallbackAnimation = true;
+                    }
                     // Ignore left offset and just ensure the width is correct. See crbug/1502437.
                     initialRect.offset(-leftOffset, -hubRect.top);
                     finalRect.offset(-leftOffset, -hubRect.top);
@@ -261,7 +269,7 @@ public abstract class TabSwitcherPaneBase implements Pane, TabSwitcherResetHandl
                                     initialRect,
                                     finalRect,
                                     coordinator.getThumbnailSize(),
-                                    /* useFallbackAnimation= */ false));
+                                    useFallbackAnimation));
                 };
         coordinator.waitForLayoutWithTab(tabId, provideAnimationData);
         return animationDataSupplier;
