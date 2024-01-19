29641161009cd049cb0de96238d5838710fa4d6b
kylechar <kylechar@chromium.org>
2024-01-19 21:31:46 +0000

    Use libyuv to copy pixel data
    Replace memcpy() with libyuv::CopyPlane() to copy pixels. libyuv has
    more efficient intrinsic implementations depending on what is
    supported.
    
    Bug: none
    Change-Id: I48b14a72a2a0eae008dd2996c7477e4fef636a76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5214555
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249527}

--- gpu/command_buffer/service/shared_image/gl_repack_utils.cc
+++ gpu/command_buffer/service/shared_image/gl_repack_utils.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bits.h"
 #include "base/check_op.h"
+#include "third_party/libyuv/include/libyuv/planar_functions.h"
 #include "third_party/skia/include/core/SkPixmap.h"
 
 namespace gpu {
@@ -57,9 +58,9 @@ std::vector<uint8_t> RepackPixelDataWithStride(const gfx::Size& size,
   DCHECK_LT(dst_stride, src_stride);
 
   std::vector<uint8_t> dst_data(dst_stride * size.height());
-  for (int y = 0; y < size.height(); ++y) {
-    memcpy(&dst_data[y * dst_stride], &src_data[y * src_stride], dst_stride);
-  }
+  libyuv::CopyPlane(src_data, src_stride, dst_data.data(), dst_stride,
+                    src_pixmap.info().minRowBytes(), size.height());
+
   return dst_data;
 }
 
@@ -72,9 +73,8 @@ void UnpackPixelDataWithStride(const gfx::Size& size,
 
   DCHECK_GT(dst_stride, src_stride);
 
-  for (int y = 0; y < size.height(); ++y) {
-    memcpy(&dst_data[y * dst_stride], &src_data[y * src_stride], src_stride);
-  }
+  libyuv::CopyPlane(src_data.data(), src_stride, dst_data, dst_stride,
+                    dst_pixmap.info().minRowBytes(), size.height());
 }
 
 void SwizzleRedAndBlue(const SkPixmap& pixmap) {
