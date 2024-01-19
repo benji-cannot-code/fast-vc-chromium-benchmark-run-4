30dc83cb03198dc13b251c8d37fbac6c17f9e334
Sky Malice <skym@chromium.org>
2024-01-19 23:04:35 +0000

    Hub action button supports being disabled.
    Bug: b/320527439
    Change-Id: I7fc39bcb9ba22890d1372dd30648562c02786161
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5217733
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1249633}

--- chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubPaneHostView.java
+++ chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubPaneHostView.java
@@ -11,7 +11,6 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.FrameLayout;
 
-import androidx.annotation.ColorInt;
 import androidx.annotation.Nullable;
 import androidx.annotation.StyleRes;
 import androidx.core.widget.TextViewCompat;
@@ -49,12 +48,12 @@ public class HubPaneHostView extends FrameLayout {
     void setColorScheme(@HubColorScheme int colorScheme) {
         Context context = getContext();
 
-        @ColorInt int iconColor = HubColors.getIconColor(context, colorScheme);
-        TextViewCompat.setCompoundDrawableTintList(
-                mActionButton, ColorStateList.valueOf(iconColor));
+        ColorStateList iconColor = HubColors.getIconColor(context, colorScheme);
+        TextViewCompat.setCompoundDrawableTintList(mActionButton, iconColor);
 
-        @ColorInt int backgroundColor = HubColors.getSecondaryContainerColor(context, colorScheme);
-        mActionButton.setButtonColor(ColorStateList.valueOf(backgroundColor));
+        ColorStateList backgroundColor =
+                HubColors.getSecondaryContainerColorStateList(context, colorScheme);
+        mActionButton.setButtonColor(backgroundColor);
 
         @StyleRes int textAppearance = HubColors.getTextAppearanceMedium(colorScheme);
         mActionButton.setTextAppearance(textAppearance);
