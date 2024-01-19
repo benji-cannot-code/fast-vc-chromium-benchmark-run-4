889ea5575541a2256d29f0cd9eb702cb6ccbdae3
Drew Davenport <ddavenport@chromium.org>
2024-01-19 02:43:31 +0000

    ui/display: Check for NULL mode
    mode could be NULL for a display in the configuration request. In such a
    case, it doesn't make sense to search for modes that could be set
    seamlessly, so skip the check.
    
    This case could arise if there is a race resulting in an
    UpdateDisplayConfigurationTask which simultaneously attempts to enable
    refresh rate throttling and turn off the internal display, such as
    docked configuration.
    
    Add a test case that exercises this code path.
    
    BUG=chromium:1497502
    TEST=DisplayConfiguratorTest.RefreshRateThrottle_RaceWithDockMode
         crashes without the null check
    
    Change-Id: I541192e409fb09fdd3a3ab45b130171d37750492
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5149147
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Commit-Queue: Drew Davenport <ddavenport@chromium.org>
    Reviewed-by: Gil Dekel <gildekel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249202}

--- ui/display/manager/display_configurator.h
+++ ui/display/manager/display_configurator.h
@@ -29,6 +29,7 @@ class Size;
 namespace display {
 
 class ContentProtectionManager;
+class DisplayLayoutManager;
 class DisplayMode;
 class DisplaySnapshot;
 class GammaCurve;
@@ -129,6 +130,8 @@ class DISPLAY_MANAGER_EXPORT DisplayConfigurator
     // Gets the current delay of the |configure_timer_| if it's running, or zero
     // time delta otherwise.
     base::TimeDelta GetConfigureDelay() const;
+
+    DisplayLayoutManager* GetDisplayLayoutManager() const;
 
    private:
     raw_ptr<DisplayConfigurator, DanglingUntriaged> configurator_;  // not owned
