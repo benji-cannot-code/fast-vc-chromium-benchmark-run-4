dff8d2e2704af9a9bb782882560115c52d9c6d4d
Hubert Chao <hchao@chromium.org>
2024-01-19 22:58:53 +0000

    Cert Management Enterprise Policy: add new policy
    Implement CAPlatformIntegrationEnabled as a future_on
    policy. This policy is part of a suite of policies
    designed to help enterprises control CA root certs
    used by Chromium for TLS server auth.
    
    Bug: 1477317
    Change-Id: I3e92db1874760726307276bebf9d65fedb32cb9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5169905
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249626}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -2077,6 +2077,9 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
   { key::kCAHintCertificates,
     prefs::kCAHintCertificates,
     base::Value::Type::LIST },
+  { key::kCAPlatformIntegrationEnabled,
+    prefs::kCAPlatformIntegrationEnabled,
+    base::Value::Type::BOOLEAN },
 #endif // BUILDFLAG(CHROME_CERTIFICATE_POLICIES_SUPPORTED)
 };
 // clang-format on
