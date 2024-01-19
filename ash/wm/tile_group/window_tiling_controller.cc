67ca35da5732a744c80e5656e48595d7facdcbcf
Addison Luh <aluh@chromium.org>
2024-01-19 00:34:23 +0000

    [tiling-resize] Create skeleton for WindowTilingController
    This change creates the skeleton for WindowTilingController and hooks it
    into the shell. The tiling window resize functionality is guarded behind
    the TilingWindowResize ash feature.
    
    Bug: b:316962035
    Change-Id: Id9048747be702447e60d6b1d1a21c495802e0fde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5207517
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Addison Luh <aluh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249161}

---
+++ ash/wm/tile_group/window_tiling_controller.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/wm/tile_group/window_tiling_controller.h"
+
+#include "ui/aura/window.h"
+
+namespace ash {
+
+bool WindowTilingController::CanTilingResize(aura::Window* window) const {
+  return true;
+}
+
+void WindowTilingController::OnTilingResizeLeft(aura::Window* window) {}
+
+void WindowTilingController::OnTilingResizeUp(aura::Window* window) {}
+
+void WindowTilingController::OnTilingResizeRight(aura::Window* window) {}
+
+void WindowTilingController::OnTilingResizeDown(aura::Window* window) {}
+
+}  // namespace ash
