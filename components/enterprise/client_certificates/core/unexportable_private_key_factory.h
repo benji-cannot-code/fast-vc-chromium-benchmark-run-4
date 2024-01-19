7a0f7f2f3b61db220a4c50b1232187e6138c8449
Sebastien Lalancette <seblalancette@chromium.org>
2024-01-19 22:17:38 +0000

    Implement UnexportablePrivateKey and its Factory
    This factory will make use of //crypto APIs to create and use
    TPM-backed keys on Windows.
    
    Bug: b:319255604
    Change-Id: I730c28d6f42c6766b692e88590c9fa9c5b7c5b0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5190110
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Hamda Mare <hmare@google.com>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249578}

---
+++ components/enterprise/client_certificates/core/unexportable_private_key_factory.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ENTERPRISE_CLIENT_CERTIFICATES_CORE_UNEXPORTABLE_PRIVATE_KEY_FACTORY_H_
+#define COMPONENTS_ENTERPRISE_CLIENT_CERTIFICATES_CORE_UNEXPORTABLE_PRIVATE_KEY_FACTORY_H_
+
+#include <memory>
+
+#include "base/memory/weak_ptr.h"
+#include "components/enterprise/client_certificates/core/private_key_factory.h"
+
+namespace client_certificates {
+
+class UnexportablePrivateKeyFactory : public PrivateKeyFactory {
+ public:
+  // Will return a factory instance only if the creation of
+  // crypto::UnexportableSigningKeys is supported on the current device (e.g. a
+  // TPM is present on Windows). Otherwise, will return nullptr.
+  static std::unique_ptr<UnexportablePrivateKeyFactory> TryCreate();
+
+  ~UnexportablePrivateKeyFactory() override;
+
+  // PrivateKeyFactory:
+  void CreatePrivateKey(PrivateKeyCallback callback) override;
+
+ private:
+  UnexportablePrivateKeyFactory();
+
+  base::WeakPtrFactory<UnexportablePrivateKeyFactory> weak_factory_{this};
+};
+
+}  // namespace client_certificates
+
+#endif  // COMPONENTS_ENTERPRISE_CLIENT_CERTIFICATES_CORE_UNEXPORTABLE_PRIVATE_KEY_FACTORY_H_
