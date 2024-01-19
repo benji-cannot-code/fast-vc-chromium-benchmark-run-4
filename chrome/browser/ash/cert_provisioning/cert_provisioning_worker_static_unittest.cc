ecefa221e07739314c61a77f51e25d2bf69e34a0
Pavol Marko <pmarko@google.com>
2024-01-19 23:13:20 +0000

    cert_prov: Perform action on successful invalidation subscription
    When the invalidation client signals that it has successfully registered
    for an invalidation, retry the current action if in waiting mode.
    
    This is necessary because the client could miss invalidations published
    before it has successfully subscribed.
    
    Bug: b:307340577
    
    Change-Id: Ia681709cce658167fe1977d9e04b33b0f3201acc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5206590
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Michael Ershov <miersh@google.com>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249643}

--- chrome/browser/ash/cert_provisioning/cert_provisioning_worker_static_unittest.cc
+++ chrome/browser/ash/cert_provisioning/cert_provisioning_worker_static_unittest.cc
@@ -1239,7 +1239,8 @@ TEST_F(CertProvisioningWorkerStaticTest, ServiceActivationPendingResponse) {
 }
 
 // Checks that when the server returns try_again_later field, the worker will
-// retry when the invalidation is triggered.
+// retry when successfully subscribed for the invalidation or when the
+// invalidation is triggered.
 TEST_F(CertProvisioningWorkerStaticTest, InvalidationRespected) {
   CertProfile cert_profile(kCertProfileId, kCertProfileName,
                            kCertProfileVersion,
@@ -1334,6 +1335,20 @@ TEST_F(CertProvisioningWorkerStaticTest, InvalidationRespected) {
     FastForwardBy(finish_csr_delay + small_delay);
     EXPECT_EQ(worker.GetState(),
               CertProvisioningWorkerState::kFinishCsrResponseReceived);
+  }
+
+  {
+    EXPECT_EQ(worker.GetState(),
+              CertProvisioningWorkerState::kFinishCsrResponseReceived);
+
+    testing::InSequence seq;
+
+    EXPECT_DOWNLOAD_CERT_TRY_LATER(
+        DownloadCert(Eq(std::ref(provisioning_process)), /*callback=*/_),
+        download_cert_server_delay.InMilliseconds());
+
+    on_invalidation_event_callback.Run(
+        InvalidationEvent::kSuccessfullySubscribed);
   }
 
   {
