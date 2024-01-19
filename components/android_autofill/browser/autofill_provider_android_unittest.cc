6e17163c1e7c5241cb8167098438bb3c9438a0f9
Jan Keitel <jkeitel@google.com>
2024-01-19 14:40:33 +0000

    Use PWM predictions in the form sent in the cache request.
    Prior to this CL, the PasswordManager::FormDataParser predictions were
    used to decide whether a form is a login form, but were not actually
    applied to the form sent in the prefill request.
    
    This CL changes that. Since this CL has an impact on the data sent
    for prefill requests, it is guarded behind a feature flag. For that,
    we use the feature flag for comparison by form signature id. We will
    rename the feature to something along the lines of
    "UsePasswordManagerParserPredictionsForPrefillRequests" in a follow-up
    CL.
    
    Bug: 1513847
    Change-Id: Ie00a6244cee4dec25d3b0807ff583c31ad4183fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5216905
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1249339}

--- components/android_autofill/browser/autofill_provider_android_unittest.cc
+++ components/android_autofill/browser/autofill_provider_android_unittest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/android_autofill/browser/autofill_provider_android_bridge.h"
 #include "components/android_autofill/browser/autofill_provider_android_test_api.h"
 #include "components/android_autofill/browser/form_data_android.h"
+#include "components/android_autofill/browser/form_data_android_test_api.h"
 #include "components/android_autofill/browser/mock_form_field_data_android_bridge.h"
 #include "components/autofill/content/browser/test_autofill_client_injector.h"
 #include "components/autofill/content/browser/test_autofill_driver_injector.h"
@@ -44,6 +45,7 @@ namespace {
 using ::testing::_;
 using ::testing::AllOf;
 using ::testing::AtLeast;
+using ::testing::ElementsAre;
 using ::testing::Eq;
 using ::testing::Field;
 using ::testing::InSequence;
@@ -75,6 +77,17 @@ auto EqualsFormData(const FormData& expected) {
         return FormData::DeepEqual(expected, actual.form());
       },
       true);
+}
+
+auto EqualsFormDataWithFields(const FormData& form, auto fields_matcher) {
+  return AllOf(
+      EqualsFormData(form),
+      ResultOf(
+          [](FormDataAndroid& form_android)
+              -> const std::vector<std::unique_ptr<FormFieldDataAndroid>>& {
+            return test_api(form_android).fields();
+          },
+          fields_matcher));
 }
 
 // Creates a matcher that compares the results of a `FormDataAndroid`'s `form()`
@@ -705,6 +718,39 @@ TEST_F(AutofillProviderAndroidTest, SendPrefillRequest) {
 
   // Upon receiving server predictions a prefill request should be sent.
   EXPECT_CALL(provider_bridge(), SendPrefillRequest(EqualsFormData(form)));
+  android_autofill_manager().SimulatePropagateAutofillPredictions(
+      form.global_id());
+}
+
+// Tests the predictions from `password_manager::FormDataParser` are used to
+// overwrite all type predictions of the respective `FormDataAndroidField`s.
+TEST_F(AutofillProviderAndroidTest,
+       UsePasswordManagerOverridesInPrefillRequest) {
+  if (base::android::BuildInfo::GetInstance()->sdk_int() <
+      base::android::SdkVersion::SDK_VERSION_U) {
+    GTEST_SKIP();
+  }
+
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitWithFeatures(
+      /*enabled_features=*/
+      {features::kAndroidAutofillPrefillRequestsForLoginForms,
+       features::kAndroidAutofillSignatureForPrefillRequestSimilarityCheck},
+      /*disabled_features=*/{});
+
+  FormData form =
+      CreateFormDataForFrame(CreateTestLoginForm(), main_frame_token());
+  android_autofill_manager().OnFormsSeen({form}, /*removed_forms=*/{});
+  ASSERT_TRUE(android_autofill_manager().FindCachedFormById(form.global_id()));
+
+  auto has_field_type = [](FieldType field_type) {
+    return Pointee(Property(&FormFieldDataAndroid::field_types,
+                            Eq(AutofillType(field_type))));
+  };
+  EXPECT_CALL(provider_bridge(),
+              SendPrefillRequest(EqualsFormDataWithFields(
+                  form, ElementsAre(has_field_type(FieldType::USERNAME),
+                                    has_field_type(FieldType::PASSWORD)))));
   android_autofill_manager().SimulatePropagateAutofillPredictions(
       form.global_id());
 }
--- components/android_autofill/browser/form_data_android.h
+++ components/android_autofill/browser/form_data_android.h
@@ -111,6 +111,8 @@ class FormDataAndroid {
   SessionId session_id() const { return session_id_; }
 
  private:
+  friend class FormDataAndroidTestApi;
+
   // Returns whether the fields of `this` are similar to the fields of `form`.
   // Returns `false` if the number of fields differs.
   bool SimilarFieldsAs(const FormData& form) const;
