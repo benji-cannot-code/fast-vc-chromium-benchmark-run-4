7a80084ebcd4eda7e8735738435ad510eeb3ea37
Yoav Weiss <yoavweiss@chromium.org>
2024-01-19 21:58:32 +0000

    Add an affiliation override when calculating commit stats
    Change-Id: I45c94cb2a078f6f22130e7599a2d3068aed0ae5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5216465
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Commit-Queue: Yoav Weiss (@Shopify) <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249563}

--- third_party/blink/tools/commit_stats/commit_per_year.sh
+++ third_party/blink/tools/commit_stats/commit_per_year.sh
@@ -28,10 +28,10 @@ botpatterns="chrome-metrics-team+robot@google.com|chrome-release-bot@chromium.or
 
 cd $topdir
 
-declare -A pattern_to_org
+declare -A domain_to_org
 for o in `cat $scriptdir/org-list.txt`; do
   p=(${o//,/ })
-  pattern_to_org["${p[1]}"]=${p[0]}
+  domain_to_org["${p[1]}"]=${p[0]}
 done
 
 function distribution {
@@ -46,12 +46,20 @@ function distribution {
     then
       cd $d
 
-      for pattern in $( \
+      for log_output in $( \
         git log --after="$1-${date}" --before="$2-${date}" \
-          --pretty=format:"%ae" | \
-          grep -Ev ${botpatterns} | awk -F@ '{print $2}'); do
-        if [ -n "${pattern_to_org["$pattern"]}" ]; then
-          echo ${pattern_to_org["$pattern"]}
+          --pretty=format:"%ae@%as" | \
+          grep -Ev ${botpatterns} ); do
+        user=`echo $log_output | awk -F@ '{print $1}'`
+        domain=`echo $log_output | awk -F@ '{print $2}'`
+        date=`echo $log_output | awk -F@ '{print $3}'`
+        email=`echo $user@$domain`
+        affiliation=`$scriptdir/affiliation.py $email $date`
+        if [ $affiliation != "None" ]; then
+          domain=$affiliation
+        fi
+        if [ -n "${domain_to_org["$domain"]}" ]; then
+          echo ${domain_to_org["$domain"]}
         else
           echo Others
         fi
@@ -73,3 +81,5 @@ distribution 2019 2020
 distribution 2020 2021
 distribution 2021 2022
 distribution 2022 2023
+distribution 2023 2024
+distribution 2024 2025
---
+++ third_party/blink/tools/commit_stats/test.sh
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+for email in $( \
+  git log \
+    --pretty=format:"%ae" | head -n 100); do
+  pattern=`echo $email | awk -F@ '{print $2}'`
+  # TODO - just try out these lines separately from the rest of the script!!
+  echo $pattern
+  echo $email
+done
+
