1b946bfa09e0cd0b3fc3ed55db86808a44927947
Christian Biesinger <cbiesinger@chromium.org>
2024-01-19 15:13:29 +0000

    [FedCM] Allow setting login status from a same-site request
    This relaxes the same-origin requirement to same-site for processing
    the login status header. This is useful for IDPs who process IDP
    logins on a different host than the FedCM endpoint.
    
    This is behind a default-enabled flag so we can easily disable this
    in case of issues.
    
    The virtual test suite explicitly enables the flag so that the test
    will keep working in case the flag has to be turned off.
    
    Approved by Security & Privacy in
    https://docs.google.com/document/d/1vZiEh4Q45gleW0lmtfgxMflqarMkcTk0Itwoo6724Vo/edit?resourcekey=0-WjqaM1SrhAoFbhmGNyyyJA&tab=t.0
    
    Bug: 1516640
    Change-Id: I8825e66b43d7d3a6fae7db2447281502fb8015c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5207174
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249349}

--- third_party/blink/web_tests/external/wpt/credential-management/support/fedcm-helper.sub.js
+++ third_party/blink/web_tests/external/wpt/credential-management/support/fedcm-helper.sub.js
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 export const manifest_origin = "https://{{host}}:{{ports[https][0]}}";
 export const alt_manifest_origin = 'https://{{hosts[alt][]}}:{{ports[https][0]}}';
+export const same_site_manifest_origin = 'https://{{hosts[][www1]}}:{{ports[https][0]}}';
 
 export function open_and_wait_for_popup(origin, path) {
   return new Promise(resolve => {
@@ -44,11 +45,11 @@ export function mark_signed_out(origin = manifest_origin) {
 
 // Returns FedCM CredentialRequestOptions for which navigator.credentials.get()
 // succeeds.
-export function request_options_with_mediation_required(manifest_filename) {
+export function request_options_with_mediation_required(manifest_filename, origin = manifest_origin) {
   if (manifest_filename === undefined) {
     manifest_filename = "manifest.py";
   }
-  const manifest_path = `${manifest_origin}/\
+  const manifest_path = `${origin}/\
 credential-management/support/fedcm/${manifest_filename}`;
   return {
     identity: {
@@ -65,21 +66,7 @@ credential-management/support/fedcm/${manifest_filename}`;
 // Returns alternate FedCM CredentialRequestOptions for which navigator.credentials.get()
 // succeeds.
 export function alt_request_options_with_mediation_required(manifest_filename) {
-  if (manifest_filename === undefined) {
-    manifest_filename = "manifest.py";
-  }
-  const manifest_path = `${alt_manifest_origin}/\
-credential-management/support/fedcm/${manifest_filename}`;
-  return {
-    identity: {
-      providers: [{
-        configURL: manifest_path,
-        clientId: '1',
-        nonce: '2'
-      }]
-    },
-    mediation: 'required'
-  };
+  return request_options_with_mediation_required(manifest_filename, alt_manifest_origin);
 }
 
 // Returns FedCM CredentialRequestOptions with auto re-authentication.
