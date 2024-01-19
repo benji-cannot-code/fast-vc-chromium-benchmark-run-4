50734dba34ded7555872afa02782cbe28beeb521
Sylvain Defresne <sdefresne@chromium.org>
2024-01-19 13:33:35 +0000

    [ios] Use TestSessionRestorationService in GridMediatorTestClass
    Some tests based on GridMediatorTestClass uses fake WebStates which
    causes them to fail when using the SessionRestorationService version
    based on the new optimised session storage (as the fake WebStates do
    not implement correctly SerializeToProto).
    
    Since those tests don't depend on serialisation, install a test
    implementation of SessionRestorationService to make the independent
    of the service.
    
    Bug: 1383087
    Change-Id: Iebfa7a98b1a842e07114f35b7ad35b10d4d18c19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5216609
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1249321}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_mediator_test.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_mediator_test.mm
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/main/model/browser_web_state_list_delegate.h"
 #import "ios/chrome/browser/sessions/fake_tab_restore_service.h"
 #import "ios/chrome/browser/sessions/ios_chrome_tab_restore_service_factory.h"
-#import "ios/chrome/browser/sessions/session_restoration_service.h"
 #import "ios/chrome/browser/sessions/session_restoration_service_factory.h"
+#import "ios/chrome/browser/sessions/test_session_restoration_service.h"
 #import "ios/chrome/browser/shared/coordinator/scene/scene_state.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/browser/browser_list.h"
@@ -79,6 +79,8 @@ void GridMediatorTestClass::SetUp() {
                             AuthenticationServiceFactory::GetDefaultFactory());
   builder.AddTestingFactory(ios::HistoryServiceFactory::GetInstance(),
                             ios::HistoryServiceFactory::GetDefaultFactory());
+  builder.AddTestingFactory(SessionRestorationServiceFactory::GetInstance(),
+                            TestSessionRestorationService::GetTestingFactory());
   browser_state_ = builder.Build();
   AuthenticationServiceFactory::CreateAndInitializeForBrowserState(
       browser_state_.get(),
