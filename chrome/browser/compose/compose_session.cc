19bb13a1aa6558026f52949b60883a3a330b7222
Jeffrey Cohen <jeffreycohen@google.com>
2024-01-19 01:17:52 +0000

    [Compose] Add explicit logging uploads on error reset
    Bug: b:320545606
    Change-Id: If4209cde9403e45bc8392a9a02f49e854b9e3d34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5214775
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249172}

--- chrome/browser/compose/compose_session.cc
+++ chrome/browser/compose/compose_session.cc
@@ -457,6 +457,13 @@ void ComposeSession::ModelExecutionComplete(
     token->set_high(session_id_.high());
     token->set_low(session_id_.low());
     most_recent_ok_state_->SetModelingLogEntry(std::move(log_entry));
+    // In the event that we are holding onto an error log upload it before it
+    // gets overwritten
+    if (most_recent_error_log_ && model_quality_logs_uploader_) {
+      model_quality_logs_uploader_->UploadModelQualityLogs(
+          std::move(most_recent_error_log_));
+    }
+
     // if we have a valid most recent state we no longer need an error state.
     most_recent_error_log_.reset();
   }
@@ -802,6 +809,12 @@ void ComposeSession::SetQualityLogEntryUponError(
 
     log_entry->quality_data<optimization_guide::ComposeFeatureTypeMap>()
         ->set_was_generated_via_edit(was_input_edited);
+    // In the event that we are holding onto an error log upload it before it
+    // gets overwritten
+    if (most_recent_error_log_ && model_quality_logs_uploader_) {
+      model_quality_logs_uploader_->UploadModelQualityLogs(
+          std::move(most_recent_error_log_));
+    }
 
     most_recent_error_log_ = std::move(log_entry);
   }
