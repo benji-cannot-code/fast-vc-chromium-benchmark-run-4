6aad0fe57a8cb38d25cd7f293b80c23b76122b43
Callistus Tan <callistus@google.com>
2024-01-19 18:30:40 +0000

    VC: Override WebUI default bounds to be smaller.
    Following the default bounds set for p13n app.
    
    Bug: b/321160522
    Change-Id: Ib5f53ffc315208eec77317094b4d77e5a5ac38e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5215987
    Commit-Queue: Jeffrey Young <cowmoo@google.com>
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1249410}

--- chrome/browser/ash/system_web_apps/apps/vc_background_ui/vc_background_ui_system_app_delegate.cc
+++ chrome/browser/ash/system_web_apps/apps/vc_background_ui/vc_background_ui_system_app_delegate.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_install_info.h"
 #include "components/manta/features.h"
 #include "third_party/blink/public/mojom/manifest/display_mode.mojom-shared.h"
+#include "ui/display/screen.h"
 #include "url/gurl.h"
 
 namespace ash::vc_background_ui {
@@ -44,6 +45,14 @@ VcBackgroundUISystemAppDelegate::GetWebAppInfo() const {
 
 gfx::Size VcBackgroundUISystemAppDelegate::GetMinimumWindowSize() const {
   return {600, 420};
+}
+
+gfx::Rect VcBackgroundUISystemAppDelegate::GetDefaultBounds(
+    Browser* browser) const {
+  gfx::Rect bounds =
+      display::Screen::GetScreen()->GetDisplayForNewWindows().work_area();
+  bounds.ClampToCenteredSize({826, 608});
+  return bounds;
 }
 
 bool VcBackgroundUISystemAppDelegate::IsAppEnabled() const {
