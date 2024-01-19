1f2864dad5419651f98e97257ab018d1c621e52b
Christoph Schwering <schwering@google.com>
2024-01-19 19:48:41 +0000

    [Autofill] Move TTF hiding from driver factory to controller
    This CL move TTF hiding from ContentAutofillDriverFactory to
    TouchToFillCreditCardController.
    
    Change-Id: I1d44b2f1b5c57743499509226f5dd86c77555ea3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5216846
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1249448}

--- chrome/browser/touch_to_fill/autofill/android/touch_to_fill_credit_card_controller.h
+++ chrome/browser/touch_to_fill/autofill/android/touch_to_fill_credit_card_controller.h
@@ -28,7 +28,8 @@ class CreditCard;
 // `java_object_`.
 class TouchToFillCreditCardController
     : public TouchToFillCreditCardViewController,
-      public ContentAutofillDriverFactory::Observer {
+      public ContentAutofillDriverFactory::Observer,
+      public content::WebContentsObserver {
  public:
   explicit TouchToFillCreditCardController(
       ContentAutofillClient* autofill_client);
@@ -37,6 +38,10 @@ class TouchToFillCreditCardController
   TouchToFillCreditCardController& operator=(
       const TouchToFillCreditCardController&) = delete;
   ~TouchToFillCreditCardController() override;
+
+  // content::WebContentsObserver:
+  void DidFinishNavigation(
+      content::NavigationHandle* navigation_handle) override;
 
   // ContentAutofillDriverFactory::Observer:
   void OnContentAutofillDriverFactoryDestroyed(
