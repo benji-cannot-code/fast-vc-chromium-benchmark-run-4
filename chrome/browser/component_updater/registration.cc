a54a97d77951e72d33c747df9db3318b35387266
Noah Rose Ledesma <noahrose@google.com>
2024-01-19 22:04:46 +0000

    Remove payload test component
    The experiment has run its course and can be cleaned up.
    
    Bug: 1490685
    Change-Id: Ic904d225442d664574a40080158ac46e04da2dcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5217413
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249567}

--- chrome/browser/component_updater/registration.cc
+++ chrome/browser/component_updater/registration.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/branding_buildflags.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
-#include "chrome/browser/browser_features.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/buildflags.h"
 #include "chrome/browser/component_updater/app_provisioning_component_installer.h"
@@ -32,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/component_updater/masked_domain_list_component_installer.h"
 #include "chrome/browser/component_updater/mei_preload_component_installer.h"
 #include "chrome/browser/component_updater/network_quality_observer.h"
-#include "chrome/browser/component_updater/payload_test_component_installer.h"
 #include "chrome/browser/component_updater/pki_metadata_component_installer.h"
 #include "chrome/browser/component_updater/pnacl_component_installer.h"
 #include "chrome/browser/component_updater/privacy_sandbox_attestations_component_installer.h"
@@ -230,12 +228,6 @@ void RegisterComponentsForUpdate() {
   RegisterCommerceHeuristicsComponent(cus);
 
   RegisterTpcdMetadataComponent(cus);
-
-  // TODO(crbug.com/1490685): Remove this test component once the
-  // experiment has concluded.
-  if (base::FeatureList::IsEnabled(features::kPayloadTestComponent)) {
-    RegisterPayloadTestComponent(cus);
-  }
 
   // TODO(crbug.com/1499359): Remove once the experiment has concluded.
   EnsureNetworkQualityObserver();
