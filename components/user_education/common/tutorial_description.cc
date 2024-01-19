b2875bd6ffe2ee312cf7f7a2135d587002e97cb3
Eshwar Stalin <estalin@chromium.org>
2024-01-19 21:34:06 +0000

    Support for adding temporary state during tutorial execution
    Providing the ability for tutorials to add temporary state e.g. like
    showing bookmarks bar during the execution of the saved tab groups
    tutorial. The state should be reset when the tutorial execution is
    complete.
    
    Change-Id: Icf0ace8baa255a7c2aea7ea221321a6d68bd2cc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5214974
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249531}

--- components/user_education/common/tutorial_description.cc
+++ components/user_education/common/tutorial_description.cc
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace user_education {
 
+ScopedTutorialState::ScopedTutorialState(ui::ElementContext context)
+    : context_(context) {}
+ScopedTutorialState::~ScopedTutorialState() = default;
+
 TutorialDescription::TutorialDescription() = default;
 TutorialDescription::TutorialDescription(TutorialDescription&&) noexcept =
     default;
