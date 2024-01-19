564c90fb244d23e7d4f6c6e10c69bdfd514314b0
Kush Sinha <sinhak@chromium.org>
2024-01-19 14:58:27 +0000

    Ash: Don't throttle XHR requests for non-Google properties
    https://crrev.com/c/1434283 migrated a utility that throttled XHR
    requests to Google web properties until ChromeOS (Ash) has minted
    cookies for the user. This is useful for avoiding error messages that
    tell the user that they are logged out - since users are supposed to
    have an SSO experience in ChromeOS.
    
    During this migration however, the implementation was slightly changed.
    The original implementation -
    https://chromium-review.googlesource.com/c/chromium/src/+/1434283/5/chrome/browser/chromeos/login/signin/merge_session_resource_throttle.cc#b59
    used to check if the request being throttled is a request to a Google
    sub-domain. The new implementation didn't check for this -
    https://chromium-review.googlesource.com/c/chromium/src/+/1434283/5/chrome/renderer/chromeos_merge_session_loader_throttle.cc#45
    
    Fix this by filtering requests to Google sub-domains when throttling.
    
    Bug: b/315072145
    Test: unit_tests --gtest_filter="*AshMergeSessionLoaderThrottleTest*"
    Test: browser_tests --gtest_filter="*MergeSessionTimeoutTest*"
    Change-Id: I6fe61a6830853eee5c817a5100efd243ff31612d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5201765
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249346}

--- chrome/renderer/ash_merge_session_loader_throttle.h
+++ chrome/renderer/ash_merge_session_loader_throttle.h
@@ -32,11 +32,6 @@ class AshMergeSessionLoaderThrottle final : public blink::URLLoaderThrottle {
 
   ~AshMergeSessionLoaderThrottle() override;
 
- private:
-  bool MaybeDeferForMergeSession(
-      const GURL& url,
-      DelayedCallbackGroup::Callback resume_callback);
-
   // blink::URLLoaderThrottle:
   void WillStartRequest(network::ResourceRequest* request,
                         bool* defer) override;
@@ -47,6 +42,11 @@ class AshMergeSessionLoaderThrottle final : public blink::URLLoaderThrottle {
       std::vector<std::string>* to_be_removed_headers,
       net::HttpRequestHeaders* modified_headers,
       net::HttpRequestHeaders* modified_cors_exempt_headers) override;
+
+ private:
+  bool MaybeDeferForMergeSession(
+      const GURL& url,
+      DelayedCallbackGroup::Callback resume_callback);
   void DetachFromCurrentSequence() override;
   void ResumeLoader(DelayedCallbackGroup::RunReason run_reason);
 
