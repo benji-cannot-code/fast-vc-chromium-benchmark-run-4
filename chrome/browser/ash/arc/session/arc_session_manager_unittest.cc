9c6a0e7ee3963c09aeefa2e1b55d7a5ac767f862
Yury Khmel <khmel@chromium.org>
2024-01-19 22:31:17 +0000

    arc: Fix RegularBoot.vm on ARC-T
    Data migration flow changes the meaning of return type of
    RequestEnableImpl of session manager. It supposed to return true in case
    terms needs to be skipped. When data migration is active (ARC-T ARM?) it
    reported false for all cases. That leads to failure of some tests.
    
    BUG=b:319195788
    TEST=tast run ${TROGDOR_ARC_T} arc.RegularBoot.vm
    
    Change-Id: I79db34c00236c6cecd9549e996617dc1b956ec2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5213789
    Auto-Submit: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Satoshi Niwa <niwa@chromium.org>
    Commit-Queue: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249590}

--- chrome/browser/ash/arc/session/arc_session_manager_unittest.cc
+++ chrome/browser/ash/arc/session/arc_session_manager_unittest.cc
@@ -1052,6 +1052,7 @@ TEST_F(ArcSessionManagerTest, SetArcSignedIn) {
   EXPECT_TRUE(prefs->GetBoolean(prefs::kArcSignedIn));
   EXPECT_TRUE(
       arc_session_manager()->GetArcSessionRunnerForTesting()->arc_signed_in());
+  EXPECT_TRUE(arc_session_manager()->skipped_terms_of_service_negotiation());
 
   // Correctly stop service.
   arc_session_manager()->Shutdown();
@@ -1074,6 +1075,7 @@ TEST_F(ArcSessionManagerTest, ClearArcSignedIn) {
   EXPECT_FALSE(prefs->GetBoolean(prefs::kArcSignedIn));
   EXPECT_FALSE(
       arc_session_manager()->GetArcSessionRunnerForTesting()->arc_signed_in());
+  EXPECT_FALSE(arc_session_manager()->skipped_terms_of_service_negotiation());
 
   // Correctly stop service.
   arc_session_manager()->Shutdown();
@@ -1305,6 +1307,7 @@ TEST_F(ArcSessionManagerTest, DataCleanUpOnNextStart) {
             arc_session_manager()->state());
   base::RunLoop().RunUntilIdle();
   EXPECT_EQ(ArcSessionManager::State::ACTIVE, arc_session_manager()->state());
+  EXPECT_TRUE(arc_session_manager()->skipped_terms_of_service_negotiation());
 
   arc_session_manager()->Shutdown();
 }
