6dca4623b5d466fa215ca8a3a67dd0fc48bbdd08
Dana Fried <dfried@chromium.org>
2024-01-19 19:25:24 +0000

    [User Education] Move legal notice check down into user_education
    This moves the check for outstanding non-IPH legal notices from
    BrowserFeaturePromoController down into FeaturePromoControllerCommon,
    and tests that notices actually prevent IPH launch.
    
    It is one of two steps that will allow IPH legal notices to coexist
    with non-IPH notices. The follow-up CL will allow legal notice IPH
    queued with MaybeShowStartupPromo to wait until after non-IPH legal
    notices have shown.
    
    This behavior will extend to all async legal notice promos once async
    promos are available in User Ed V2.1
    
    Bug: 1479773
    Change-Id: I7b5433c958e2024298453085f1f791fb1d2b1e1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5208320
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249433}

--- chrome/browser/ui/views/user_education/browser_feature_promo_controller_unittest.cc
+++ chrome/browser/ui/views/user_education/browser_feature_promo_controller_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
+#include "base/functional/callback_forward.h"
 #include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
 #include "base/strings/stringprintf.h"
@@ -43,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_education/common/feature_promo_storage_service.h"
 #include "components/user_education/common/help_bubble_factory_registry.h"
 #include "components/user_education/common/help_bubble_params.h"
+#include "components/user_education/common/product_messaging_controller.h"
 #include "components/user_education/common/tutorial.h"
 #include "components/user_education/common/tutorial_description.h"
 #include "components/user_education/common/tutorial_service.h"
@@ -752,6 +754,35 @@ TEST_F(BrowserFeaturePromoControllerTest,
       controller_->DismissNonCriticalBubbleInRegion(overlapping_region);
   EXPECT_FALSE(result);
   EXPECT_TRUE(bubble->is_open());
+}
+
+TEST_F(BrowserFeaturePromoControllerTest, RequiredNoticeBlocksPromo) {
+  EXPECT_CALL(*mock_tracker_, ShouldTriggerHelpUI(Ref(kTutorialIPHFeature)))
+      .Times(0);
+
+  auto& product_messaging_controller =
+      UserEducationServiceFactory::GetForBrowserContext(profile())
+          ->product_messaging_controller();
+
+  DEFINE_LOCAL_REQUIRED_NOTICE_IDENTIFIER(kRequiredNotice);
+
+  // Because the queued notice is posted, the callback does not actually get
+  // called until a message loop runs.
+  base::RunLoop run_loop;
+  product_messaging_controller.QueueRequiredNotice(
+      kRequiredNotice,
+      base::BindOnce(
+          [](base::OnceClosure closure,
+             user_education::RequiredNoticePriorityHandle handle) {
+            std::move(closure).Run();
+          },
+          run_loop.QuitClosure()));
+
+  EXPECT_EQ(user_education::FeaturePromoResult::kBlockedByPromo,
+            controller_->MaybeShowPromo(kTutorialIPHFeature));
+
+  // Run the loop, clearing the notice.
+  run_loop.Run();
 }
 
 TEST_F(BrowserFeaturePromoControllerTest, SnoozeServiceBlocksPromo) {
