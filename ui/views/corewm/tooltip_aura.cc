58daea028134e14a047caded22d1245d955ffbd9
Hongyu Long <hongyulong@chromium.org>
2024-01-20 01:56:06 +0000

    focus-mode: Update the style of tooltip for new spec
    This CL will update the style according to the new spec: the max width
    of the tooltip view will be 296px instead of 800px. Also, if there are
    multiple lines, there are at most 3 lines showing up with an ellipsis at
    the end of the 3rd line.
    
    Bug: b/313484224
    Test: Manual
    
    Change-Id: I8fb182a22fb244215ea757f772dedb32be4d5160
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5180981
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Hongyu Long <hongyulong@chromium.org>
    Reviewed-by: Richard Chui <richui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249755}

--- ui/views/corewm/tooltip_aura.cc
+++ ui/views/corewm/tooltip_aura.cc
@@ -74,6 +74,10 @@ void TooltipAura::RemoveObserver(wm::TooltipObserver* observer) {
   observers_.RemoveObserver(observer);
 }
 
+void TooltipAura::SetMaxWidth(int width) {
+  max_width_ = width;
+}
+
 // static
 void TooltipAura::AdjustToCursor(gfx::Rect* anchor_point) {
   // TODO(crbug.com/1410707): Should adjust with actual cursor size.
@@ -200,7 +204,7 @@ void TooltipAura::DestroyWidget() {
 int TooltipAura::GetMaxWidth(const gfx::Point& location) const {
   display::Screen* screen = display::Screen::GetScreen();
   gfx::Rect display_bounds(screen->GetDisplayNearestPoint(location).bounds());
-  return std::min(kTooltipMaxWidth, (display_bounds.width() + 1) / 2);
+  return std::min(max_width_, (display_bounds.width() + 1) / 2);
 }
 
 void TooltipAura::Update(aura::Window* window,
