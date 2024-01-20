f59e8937b0bf0f17381630e32c7509c742f8be85
Nidhi Jaju <nidhijaju@chromium.org>
2024-01-20 00:28:49 +0000

    Add UMAs for loading fonts breakdown
    This CL adds two UMAs to match existing trace logs for the
    FontServiceThread to give a better understanding of how long these steps
    in loading fonts take in the field.
    
    Bug: 1509719
    Change-Id: I1c8e21ee9077531f88bd9afb2580bb137ecfcbd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5215729
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249707}

--- components/services/font/public/cpp/font_service_thread.cc
+++ components/services/font/public/cpp/font_service_thread.cc
@@ -10,8 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/task/thread_pool.h"
+#include "base/timer/elapsed_timer.h"
 #include "components/services/font/public/cpp/mapped_font_file.h"
 #include "pdf/buildflags.h"
 
@@ -43,6 +45,9 @@ bool FontServiceThread::MatchFamilyName(
     SkFontStyle* out_style) {
   DCHECK(!task_runner_->RunsTasksInCurrentSequence());
   bool out_valid = false;
+
+  base::ElapsedTimer timer;
+
   // This proxies to the other thread, which proxies to mojo. Only on the reply
   // from mojo do we return from this.
   base::WaitableEvent done_event;
@@ -52,6 +57,9 @@ bool FontServiceThread::MatchFamilyName(
                      family_name, requested_style, &out_valid,
                      out_font_identity, out_family_name, out_style));
   done_event.Wait();
+
+  base::UmaHistogramMicrosecondsTimes(
+      "Blink.Fonts.FontServiceThread.MatchFamilyNameTime", timer.Elapsed());
 
   return out_valid;
 }
@@ -135,6 +143,8 @@ scoped_refptr<MappedFontFile> FontServiceThread::OpenStream(
     const SkFontConfigInterface::FontIdentity& identity) {
   DCHECK(!task_runner_->RunsTasksInCurrentSequence());
 
+  base::ElapsedTimer timer;
+
   base::File stream_file;
   // This proxies to the other thread, which proxies to mojo. Only on the
   // reply from mojo do we return from this.
@@ -143,6 +153,9 @@ scoped_refptr<MappedFontFile> FontServiceThread::OpenStream(
       FROM_HERE, base::BindOnce(&FontServiceThread::OpenStreamImpl, this,
                                 &done_event, &stream_file, identity.fID));
   done_event.Wait();
+
+  base::UmaHistogramMicrosecondsTimes(
+      "Blink.Fonts.FontServiceThread.OpenStreamTime", timer.Elapsed());
 
   if (!stream_file.IsValid()) {
     // The font-service may have been killed.
