c3ede2b13686d1271189288dd76e58b88ea1f74d
Gayane Petrosyan <gayane@google.com>
2024-01-22 16:21:41 +0000

    [iOS] Add FET configs for tailored promos
    - Add FET configs for each tailored promo.
    - Add FET group config that will ensure that all default browser promos
    are displayed at least 14 days apart as well as will contain all the
    common eligibility conditions.
    
    Bug: b/315308024
    Change-Id: I07593216724c061c23ca97335ac410925474053b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5189268
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1250213}

--- components/feature_engagement/public/group_list.cc
+++ components/feature_engagement/public/group_list.cc
@@ -15,6 +15,7 @@ const base::Feature* const kAllGroups[] = {
     &kIPHDummyGroup,  // Ensures non-empty array for all platforms.
 #if BUILDFLAG(IS_IOS)
     &kiOSFullscreenPromosGroup,
+    &kiOSDefaultBrowserPromosGroup,
 #endif  // BUILDFLAG(IS_IOS)
 };
 }  // namespace
