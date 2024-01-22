b3e0122c1781010e08b1b65190c398210777aed8
Mingyu Lei <leimy@chromium.org>
2024-01-22 02:07:04 +0000

    Bundz: fix error throwing that may cause crash
    Previously when the error case is triggered, it will always crash with
    the "Cannot create a handle without a HandleScope" error. This CL
    attempts to fix that by changing the exception to DOMException.
    
    Bug: 1513585
    Change-Id: If080ce38db757f939b1e668f3756ec77b83f4664
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5210530
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249959}

--- third_party/blink/renderer/modules/model_execution/model_generic_session.cc
+++ third_party/blink/renderer/modules/model_execution/model_generic_session.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/model_execution/model_session.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/idl_types.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
+#include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/streams/readable_stream_default_controller_with_script_scope.h"
 #include "third_party/blink/renderer/core/streams/underlying_source_base.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
@@ -102,9 +103,10 @@ class ModelGenericSession::StreamingResponder final
         break;
       }
       case mojom::blink::ModelStreamingResponseStatus::kError: {
-        // TODO(leimy): better error information design after the prototype.
-        Controller()->Error(V8ThrowException::CreateError(
-            script_state_->GetIsolate(), "Model execution error"));
+        // TODO(leimy): raise the proper exception based on the spec after the
+        // prototype phase.
+        Controller()->Error(MakeGarbageCollected<DOMException>(
+            DOMExceptionCode::kNotReadableError, "Model execution error"));
       }
     }
   }
