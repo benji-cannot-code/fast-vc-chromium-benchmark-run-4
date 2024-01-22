9ab21bd8403f88210415961707e59d445a54e4be
Kurumi Muto <mkurumi@google.com>
2024-01-22 07:24:06 +0000

    [bfcache] Update fieldtrial config for AllowBFCacheWhenClosedMediaStreamTrack
    As we are going to move the study from Canary to Canary Dev, we have to update the affected test.
    
    Bug: 1502395
    Change-Id: I05279c5a95fccd436a61f28a690276a4158b4772
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5209731
    Reviewed-by: Yuzu Saijo <yuzus@chromium.org>
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Commit-Queue: Kurumi Muto <mkurumi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1250016}

--- third_party/blink/web_tests/external/wpt/webrtc/back-forward-cache-with-open-webrtc-connection.https.window.js
+++ third_party/blink/web_tests/external/wpt/webrtc/back-forward-cache-with-open-webrtc-connection.https.window.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// META: title=Testing BFCache support for page with open WebRTC connection.
+// META: title=Testing BFCache support for page with open WebRTC connection and live MediaStreamTrack.
 // META: script=/common/dispatcher/dispatcher.js
 // META: script=/common/utils.js
 // META: script=/html/browsers/browsing-the-web/back-forward-cache/resources/rc-helper.js
@@ -15,7 +15,7 @@ promise_test(async t => {
   const rc1 = await rcHelper.addWindow(
       /*config=*/ null, /*options=*/ { features: 'noopener' });
   await openWebRTC(rc1);
-  // The page should not be eligible for BFCache because of open WebRTC connection.
+  // The page should not be eligible for BFCache because of open WebRTC connection and live MediaStreamTrack.
   await assertBFCacheEligibility(rc1, /*shouldRestoreFromBFCache=*/ false);
-  await assertNotRestoredFromBFCache(rc1, ['WebRTC']);
+  await assertNotRestoredFromBFCache(rc1, ['WebRTC', 'LiveMediaStreamTrack']);
 });
