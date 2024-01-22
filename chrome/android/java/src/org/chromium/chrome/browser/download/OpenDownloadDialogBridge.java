1937ba78ecb2dec0f278cdbcfaccbc15445794fe
Min Qin <qinmin@chromium.org>
2024-01-22 18:25:27 +0000

    Adding new string pdf open dialog if there is only one pdf viewer
    If there is only 1 pdf viewer, this CL will show the app name in dialog
    title and summary for the settings dialog.
    
    Low-Coverage-Reason: HARD_TO_TEST
    Change-Id: Ibb36917a3165aaf7286f98ee9401cb96c45bcc67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5216586
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250292}

--- chrome/android/java/src/org/chromium/chrome/browser/download/OpenDownloadDialogBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/OpenDownloadDialogBridge.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.download;
 
 import android.app.Activity;
+import android.content.pm.ResolveInfo;
 
 import org.jni_zero.CalledByNative;
 import org.jni_zero.NativeMethods;
@@ -18,6 +19,8 @@ import org.chromium.chrome.browser.preferences.Pref;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.components.user_prefs.UserPrefs;
 import org.chromium.ui.modaldialog.ModalDialogManagerHolder;
+
+import java.util.List;
 
 /** Glues open download dialogs UI code and handles the communication to download native backend. */
 public class OpenDownloadDialogBridge {
@@ -45,6 +48,11 @@ public class OpenDownloadDialogBridge {
      */
     @CalledByNative
     public void showDialog(Profile profile, String guid) {
+        List<ResolveInfo> result = MimeUtils.getPdfIntentHandlers();
+        if (result.size() == 0) {
+            onCancel(guid);
+            return;
+        }
         DownloadActivityLauncher.getInstance()
                 .getActivityForOpenDialog(
                         (activity) -> {
@@ -52,7 +60,11 @@ public class OpenDownloadDialogBridge {
                                 onCancel(guid);
                                 return;
                             }
-                            showOpenDownloadDialog(profile, guid, activity);
+                            showOpenDownloadDialog(
+                                    profile,
+                                    guid,
+                                    activity,
+                                    result.size() > 1 ? null : MimeUtils.getDefaultPdfViewerName());
                         });
     }
 
@@ -62,13 +74,16 @@ public class OpenDownloadDialogBridge {
      * @param profile Profile of the user.
      * @param guid GUID of the download.
      * @param activity Activity that displays the dialog.
+     * @param appName Name of the app to open the file, null if there are multiple apps.
      */
-    private void showOpenDownloadDialog(Profile profile, String guid, Activity activity) {
+    private void showOpenDownloadDialog(
+            Profile profile, String guid, Activity activity, String appName) {
         new OpenDownloadDialog()
                 .show(
                         activity,
                         ((ModalDialogManagerHolder) activity).getModalDialogManager(),
                         UserPrefs.get(profile).getBoolean(Pref.AUTO_OPEN_PDF_ENABLED),
+                        appName,
                         (result) -> {
                             if (result
                                     == OpenDownloadDialogEvent.OPEN_DOWNLOAD_DIALOG_ALWAYS_OPEN) {
