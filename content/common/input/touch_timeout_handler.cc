de02257cc746d2b3997de4efb562a689d93c9541
Stephen Nusko <nuskos@chromium.org>
2024-01-22 12:53:23 +0000

    Add some simple trace events to better trace early out of touchs
    We've seen some traces where TouchEvents during a scroll are not
    converted to GestureScrollUpdates, this will rule out some of the early
    early-outs and also determine which code path we're in.
    
    Then we can focus on adding trace events for whichever path we're in.
    
    Bug: b/315762684
    Change-Id: I5911406d5b207da39f7d0d25e30913ec5ae5f632
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5210594
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250126}

--- content/common/input/touch_timeout_handler.cc
+++ content/common/input/touch_timeout_handler.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/trace_event/trace_event.h"
+#include "base/trace_event/typed_macros.h"
 #include "content/common/input/passthrough_touch_event_queue.h"
 #include "ui/events/base_event_utils.h"
 #include "ui/gfx/geometry/point_f.h"
@@ -98,15 +99,18 @@ bool TouchTimeoutHandler::ConfirmTouchEvent(
       return false;
     case PENDING_ACK_ORIGINAL_EVENT:
       if (AckedTimeoutEventRequiresCancel(ack_result)) {
+        TRACE_EVENT_INSTANT("input", "PendingAckOriginalEvent-RequiresCancel");
         SetPendingAckState(PENDING_ACK_CANCEL_EVENT);
         touch_queue_->SendTouchCancelEventForTouchEvent(timeout_event_);
       } else {
+        TRACE_EVENT_INSTANT("input", "PendingAckOriginalEvent");
         SetPendingAckState(PENDING_ACK_NONE);
         touch_queue_->UpdateTouchConsumerStates(timeout_event_.event,
                                                 ack_result);
       }
       return true;
     case PENDING_ACK_CANCEL_EVENT:
+      TRACE_EVENT_INSTANT("input", "PendingAckCancelEvent");
       SetPendingAckState(PENDING_ACK_NONE);
       return true;
   }
