4de31bb83c07ab5a164c807ebc28c0089de4d603
Luke Fisk-Lennon <lukefl@google.com>
2024-01-22 02:26:39 +0000

    Add flag check for emoji picker global preferences store
    Bug: b:321154313
    Change-Id: I88f47adce4c83cf425ae9ddc0a1228a56e8a5396
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5214830
    Reviewed-by: Chuong Ho <hdchuong@chromium.org>
    Commit-Queue: Luke Fisk-Lennon <lukefl@google.com>
    Reviewed-by: Zac Partridge <zacpartridge@google.com>
    Cr-Commit-Position: refs/heads/main@{#1249964}

--- chrome/browser/resources/chromeos/emoji_picker/app.ts
+++ chrome/browser/resources/chromeos/emoji_picker/app.ts
@@ -468,6 +468,8 @@ export class EmojiPickerApp extends PolymerElement {
     this.sealSupport = featureList.includes(Feature.EMOJI_PICKER_SEAL_SUPPORT);
     this.variantGroupingSupport =
         featureList.includes(Feature.EMOJI_PICKER_VARIANT_GROUPING_SUPPORT);
+
+    this.updateEmojiPreferencesStore();
   }
 
   private fetchOrderingData(url: string): Promise<EmojiGroupData> {
@@ -1150,9 +1152,7 @@ export class EmojiPickerApp extends PolymerElement {
    */
   updateIncognitoState(incognito: boolean) {
     this.incognito = incognito;
-    this.emojiPreferences = incognito ? null : new EmojiPreferencesStore();
-    this.globalTone = this.emojiPreferences?.getTone() ?? null;
-    this.globalGender = this.emojiPreferences?.getGender() ?? null;
+    this.updateEmojiPreferencesStore();
 
     // Load the history item for each category.
     for (const category of Object.values(CategoryEnum)) {
@@ -1160,6 +1160,17 @@ export class EmojiPickerApp extends PolymerElement {
           incognito ? null : new RecentlyUsedStore(`${category}-recently-used`);
       this.categoryHistoryUpdated(category);
     }
+  }
+
+  /**
+   * Updates the emoji preferences store, global tone, and global gender.
+   */
+  updateEmojiPreferencesStore() {
+    this.emojiPreferences = this.incognito || !this.variantGroupingSupport ?
+        null :
+        new EmojiPreferencesStore();
+    this.globalTone = this.emojiPreferences?.getTone() ?? null;
+    this.globalGender = this.emojiPreferences?.getGender() ?? null;
   }
 
   /**
