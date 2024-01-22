5deda785d52fe6c282d948d1df6384119b9b92a2
Daniel d'Andrada <dandrader@google.com>
2024-01-22 17:07:58 +0000

    Reland "SmartCard: Permission Context"
    This is a reland of 96d25ca77ab5a0b5b201079c41dece0c392a27c2
    
    Fix: explicitly initialized boolean and int variables of
    SmartCardReaderTracker::ReaderInfo.
    
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL Testing of SmartCardReaderTracker will come alongside its implementation in another CL.
    
    Original change's description:
    > SmartCard: Permission Context
    >
    > go/web-smart-card-api-permissions
    >
    > Bug: 1464851
    > Low-Coverage-Reason: TESTS_IN_SEPARATE_CL Testing of SmartCardReaderTracker will come alongside its implementation in another CL.
    > Change-Id: I00e6e288176fce17de5ed601d514dc38151f1b2c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5053734
    > Reviewed-by: Florian Jacky <fjacky@chromium.org>
    > Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    > Reviewed-by: Balazs Engedy <engedy@chromium.org>
    > Reviewed-by: Caroline Rising <corising@chromium.org>
    > Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    > Commit-Queue: Daniel d'Andrada <dandrader@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1249280}
    
    Change-Id: Ia0e6df071f62ec65801328aa44c17feb4956387f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5222601
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Daniel d'Andrada <dandrader@google.com>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250241}

--- chrome/browser/smart_card/chromeos_smart_card_delegate.h
+++ chrome/browser/smart_card/chromeos_smart_card_delegate.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_SMART_CARD_CHROMEOS_SMART_CARD_DELEGATE_H_
 
 #include "base/memory/weak_ptr.h"
-#include "chrome/browser/smart_card/smart_card_permission_request.h"
 #include "content/public/browser/smart_card_delegate.h"
 
 class ChromeOsSmartCardDelegate : public content::SmartCardDelegate {
@@ -24,14 +23,6 @@ class ChromeOsSmartCardDelegate : public content::SmartCardDelegate {
       content::RenderFrameHost& render_frame_host,
       const std::string& reader_name,
       RequestReaderPermissionCallback callback) override;
-
- private:
-  void OnPermissionRequestDecided(const url::Origin& origin,
-                                  const std::string& reader_name,
-                                  RequestReaderPermissionCallback callback,
-                                  SmartCardPermissionRequest::Result result);
-
-  base::WeakPtrFactory<ChromeOsSmartCardDelegate> weak_factory_{this};
 };
 
 #endif  // CHROME_BROWSER_SMART_CARD_CHROMEOS_SMART_CARD_DELEGATE_H_
