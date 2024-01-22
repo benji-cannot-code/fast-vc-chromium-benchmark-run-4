b180f5d2d25e540d14d3dee19b94569c3b375e31
Xinghui Lu <xinghuilu@chromium.org>
2024-01-22 19:09:52 +0000

    [AsyncSB] Track committed navigation ids to determine if navigation is pending.
    It turns out DidStartNavigation is not always called (e.g. when open
    a link in a new tab) which makes IsNavigationPending return false
    while the navigation is still pending. Instead of tracking pending
    navigation ids, we will track committed navigation ids to inform if
    a navigation has committed.
    
    To track how large the ids can grow, add the following histograms:
    SafeBrowsing.AsyncCheck.PendingCheckersSize
    SafeBrowsing.AsyncCheck.CommittedNavigationIdsSize
    
    Bug: 1501194
    Change-Id: I90d9d01343bd2f55e830b01ee1518ae666bd1b06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5218532
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250329}

--- components/safe_browsing/content/browser/async_check_tracker_unittest.cc
+++ components/safe_browsing/content/browser/async_check_tracker_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/content/browser/async_check_tracker.h"
 
 #include "base/functional/callback_helpers.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/mock_callback.h"
 #include "base/test/scoped_feature_list.h"
 #include "components/safe_browsing/content/browser/base_ui_manager.h"
@@ -209,6 +210,7 @@ TEST_P(AsyncCheckTrackerTest, DisplayBlockingPageCalled) {
 }
 
 TEST_P(AsyncCheckTrackerTest, IsMainPageLoadPending) {
+  base::HistogramTester histograms;
   content::MockNavigationHandle handle(web_contents());
   UnsafeResource resource;
   resource.threat_type = SB_THREAT_TYPE_URL_PHISHING;
@@ -217,11 +219,23 @@ TEST_P(AsyncCheckTrackerTest, IsMainPageLoadPending) {
 
   AsyncCheckTracker* tracker =
       AsyncCheckTracker::FromWebContents(web_contents());
-  tracker->DidStartNavigation(&handle);
   EXPECT_TRUE(AsyncCheckTracker::IsMainPageLoadPending(resource));
 
   tracker->DidFinishNavigation(&handle);
+  // The navigation is not committed.
+  EXPECT_TRUE(AsyncCheckTracker::IsMainPageLoadPending(resource));
+  histograms.ExpectUniqueSample(
+      "SafeBrowsing.AsyncCheck.CommittedNavigationIdsSize",
+      /*sample=*/0,
+      /*expected_count=*/1);
+
+  handle.set_has_committed(true);
+  tracker->DidFinishNavigation(&handle);
   EXPECT_FALSE(AsyncCheckTracker::IsMainPageLoadPending(resource));
+  histograms.ExpectBucketCount(
+      "SafeBrowsing.AsyncCheck.CommittedNavigationIdsSize",
+      /*sample=*/1,
+      /*expected_count=*/1);
 }
 
 TEST_P(AsyncCheckTrackerTest, IsMainPageLoadPending_NoNavigationId) {
@@ -256,12 +270,22 @@ TEST_P(AsyncCheckTrackerTest,
 
 TEST_P(AsyncCheckTrackerTest,
        PendingCheckersManagement_DeleteOldCheckersAfterDidFinishNavigation) {
+  base::HistogramTester histograms;
   content::MockNavigationHandle handle_1(url_, main_rfh());
   content::MockNavigationHandle handle_2(url_, main_rfh());
   content::MockNavigationHandle handle_3(url_, main_rfh());
   CallTransferUrlChecker(handle_1.GetNavigationId());
+  histograms.ExpectUniqueSample("SafeBrowsing.AsyncCheck.PendingCheckersSize",
+                                /*sample=*/1,
+                                /*expected_count=*/1);
   CallTransferUrlChecker(handle_2.GetNavigationId());
+  histograms.ExpectBucketCount("SafeBrowsing.AsyncCheck.PendingCheckersSize",
+                               /*sample=*/2,
+                               /*expected_count=*/1);
   CallTransferUrlChecker(handle_3.GetNavigationId());
+  histograms.ExpectBucketCount("SafeBrowsing.AsyncCheck.PendingCheckersSize",
+                               /*sample=*/3,
+                               /*expected_count=*/1);
   EXPECT_EQ(tracker_->PendingCheckersSizeForTesting(), 3u);
 
   // Only the third navigation is committed successfully.
