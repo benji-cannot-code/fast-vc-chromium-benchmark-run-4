47dadb78270df25a77ec8fb975cab5e067d33d71
Mohamed Heikal <mheikal@chromium.org>
2024-01-22 17:21:18 +0000

    Revert "🧇 Add plumbing for SearchEngineCountryDelegate"
    This reverts commit baea0c6f055bc5d5de1c927ed9ce9e1d370aa476.
    
    Reason for revert: Cause of multiple clank CQ breakages see crbug.com/1520510
    
    Original change's description:
    > 🧇 Add plumbing for SearchEngineCountryDelegate
    >
    > Changes how SearchEngineChoiceService::GetCountryId works:
    > 1. Now there's an in-memory cache that guarantees that the value
    >    doesn't change in runtime.
    > 2. On Android, the value for GetCountryId is obtained from
    >    SearchEngineCountryDelegate.
    >
    > Bug: b/311170225
    > Change-Id: Ic3e017dc703c02c13ea87d3dac568ccdb9bca79c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5206536
    > Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    > Reviewed-by: David Roger <droger@chromium.org>
    > Reviewed-by: Orin Jaworski <orinj@chromium.org>
    > Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1250104}
    
    Bug: b/311170225, 1520510,1520498
    Change-Id: Ic0a331bef9caf5ddc920e1991fb14c718f60fced
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5225362
    Owners-Override: Alex Ilin <alexilin@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250247}

--- chrome/browser/ui/webui/settings/search_engines_handler_unittest.cc
+++ chrome/browser/ui/webui/settings/search_engines_handler_unittest.cc
@@ -126,13 +126,13 @@ class SearchEnginesHandlerParametrizedTest
     SearchEnginesHandlerTestBase::SetUp();
 
     if (WithSearchEnginesChoiceEnabled()) {
+      PrefService* pref_service = profile()->GetPrefs();
       // The search engine choice feature is only enabled for countries in the
       // EEA region.
       const int kBelgiumCountryId =
           country_codes::CountryCharsToCountryID('B', 'E');
-      base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
-          switches::kSearchEngineChoiceCountry,
-          country_codes::CountryIDToCountryString(kBelgiumCountryId));
+      pref_service->SetInteger(country_codes::kCountryIDAtInstall,
+                               kBelgiumCountryId);
     }
   }
 
@@ -206,13 +206,13 @@ class SearchEnginesHandlerTestWithSearchEngineChoiceEnabled
   void SetUp() override {
     SearchEnginesHandlerTestBase::SetUp();
 
+    PrefService* pref_service = profile()->GetPrefs();
     // The search engine choice feature is only enabled for countries in the
     // EEA region.
     const int kBelgiumCountryId =
         country_codes::CountryCharsToCountryID('B', 'E');
-    base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
-        switches::kSearchEngineChoiceCountry,
-        country_codes::CountryIDToCountryString(kBelgiumCountryId));
+    pref_service->SetInteger(country_codes::kCountryIDAtInstall,
+                             kBelgiumCountryId);
   }
 };
 
@@ -223,9 +223,8 @@ TEST_F(SearchEnginesHandlerTestWithSearchEngineChoiceEnabled,
   // region.
   const int kBelgiumCountryId =
       country_codes::CountryCharsToCountryID('B', 'E');
-  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
-      switches::kSearchEngineChoiceCountry,
-      country_codes::CountryIDToCountryString(kBelgiumCountryId));
+  pref_service->SetInteger(country_codes::kCountryIDAtInstall,
+                           kBelgiumCountryId);
 
   EXPECT_FALSE(pref_service->HasPrefPath(
       prefs::kDefaultSearchProviderChoiceScreenCompletionTimestamp));
@@ -250,15 +249,15 @@ TEST_F(SearchEnginesHandlerTestWithSearchEngineChoiceEnabled,
 
 TEST_F(SearchEnginesHandlerTestWithSearchEngineChoiceEnabled,
        RecordingSearchEngineShouldBeDoneAfterSettingDefault) {
+  PrefService* pref_service = profile()->GetPrefs();
   TemplateURLService* template_url_service =
       TemplateURLServiceFactory::GetForProfile(profile());
   // The search engine choice feature is only enabled for countries in the EEA
   // region.
   const int kBelgiumCountryId =
       country_codes::CountryCharsToCountryID('B', 'E');
-  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
-      switches::kSearchEngineChoiceCountry,
-      country_codes::CountryIDToCountryString(kBelgiumCountryId));
+  pref_service->SetInteger(country_codes::kCountryIDAtInstall,
+                           kBelgiumCountryId);
 
   const TemplateURL* default_search_engine =
       template_url_service->GetDefaultSearchProvider();
--- components/search_engines/template_url_prepopulate_data_unittest.cc
+++ components/search_engines/template_url_prepopulate_data_unittest.cc
@@ -166,19 +166,7 @@ class TemplateURLPrepopulateDataTest : public testing::Test {
     // Pick any EEA country
     const int kFranceCountryId =
         country_codes::CountryCharsToCountryID('F', 'R');
-    OverrideCountryId(kFranceCountryId);
-  }
-
-  void OverrideCountryId(int country_id) {
-    if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-            switches::kSearchEngineChoiceCountry)) {
-      base::CommandLine::ForCurrentProcess()->RemoveSwitch(
-          switches::kSearchEngineChoiceCountry);
-    }
-
-    base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
-        switches::kSearchEngineChoiceCountry,
-        country_codes::CountryIDToCountryString(country_id));
+    prefs_.SetInteger(country_codes::kCountryIDAtInstall, kFranceCountryId);
   }
 
  protected:
@@ -191,7 +179,7 @@ class TemplateURLPrepopulateDataTest : public testing::Test {
 // ids.
 TEST_F(TemplateURLPrepopulateDataTest, UniqueIDs) {
   for (int country_id : kAllCountryIds) {
-    OverrideCountryId(country_id);
+    prefs_.SetInteger(country_codes::kCountryIDAtInstall, country_id);
     std::vector<std::unique_ptr<TemplateURLData>> urls =
         TemplateURLPrepopulateData::GetPrepopulatedEngines(
             &prefs_, &search_engine_choice_service_, nullptr);
@@ -215,7 +203,7 @@ TEST_F(TemplateURLPrepopulateDataTest, NumberOfEntriesPerCountryConsistency) {
   const size_t kMaxRow = 5;
 
   for (int country_id : kAllCountryIds) {
-    OverrideCountryId(country_id);
+    prefs_.SetInteger(country_codes::kCountryIDAtInstall, country_id);
 
     const size_t kNumberOfSearchEngines =
         TemplateURLPrepopulateData::GetPrepopulatedEngines(
@@ -383,7 +371,6 @@ TEST_F(TemplateURLPrepopulateDataTest, ProvidersFromPrefs) {
 }
 
 TEST_F(TemplateURLPrepopulateDataTest, ClearProvidersFromPrefs) {
-  OverrideCountryId(country_codes::kCountryIDUnknown);
   prefs_.SetUserPref(prefs::kSearchProviderOverridesVersion,
                      std::make_unique<base::Value>(1));
 
@@ -434,7 +421,7 @@ TEST_F(TemplateURLPrepopulateDataTest, ClearProvidersFromPrefs) {
 // Verifies that built-in search providers are processed correctly.
 TEST_F(TemplateURLPrepopulateDataTest, ProvidersFromPrepopulated) {
   // Use United States.
-  OverrideCountryId(country_codes::CountryCharsToCountryID('U', 'S'));
+  prefs_.SetInteger(country_codes::kCountryIDAtInstall, 'U' << 8 | 'S');
   size_t default_index;
   std::vector<std::unique_ptr<TemplateURLData>> t_urls =
       TemplateURLPrepopulateData::GetPrepopulatedEngines(
@@ -474,7 +461,7 @@ TEST_F(TemplateURLPrepopulateDataTest, ProvidersFromPrepopulated) {
 // use https urls.
 TEST_F(TemplateURLPrepopulateDataTest, PrepopulatedAreHttps) {
   for (int country_id : kAllCountryIds) {
-    OverrideCountryId(country_id);
+    prefs_.SetInteger(country_codes::kCountryIDAtInstall, country_id);
 
     std::vector<std::unique_ptr<TemplateURLData>> t_urls =
         TemplateURLPrepopulateData::GetPrepopulatedEngines(
@@ -630,7 +617,7 @@ TEST_F(TemplateURLPrepopulateDataTest, FindGoogleIndex) {
   std::vector<std::unique_ptr<TemplateURLData>> urls;
 
   // Google is first in US, so confirm index 0.
-  OverrideCountryId(country_codes::CountryCharsToCountryID('U', 'S'));
+  prefs_.SetInteger(country_codes::kCountryIDAtInstall, 'U' << 8 | 'S');
   urls = TemplateURLPrepopulateData::GetPrepopulatedEngines(
       &prefs_, &search_engine_choice_service_, &index);
   EXPECT_EQ(index, size_t{0});
@@ -640,7 +627,7 @@ TEST_F(TemplateURLPrepopulateDataTest, FindGoogleIndex) {
   // If Google ever does reach top in China, this test will need to be adjusted:
   // check template_url_prepopulate_data.cc reference orders (engines_CN, etc.)
   // to find a suitable country and index.
-  OverrideCountryId(country_codes::CountryCharsToCountryID('C', 'N'));
+  prefs_.SetInteger(country_codes::kCountryIDAtInstall, 'C' << 8 | 'N');
   urls = TemplateURLPrepopulateData::GetPrepopulatedEngines(
       &prefs_, &search_engine_choice_service_, &index);
   EXPECT_GT(index, size_t{0});
@@ -652,7 +639,8 @@ TEST_F(TemplateURLPrepopulateDataTest, FindGoogleIndex) {
 TEST_F(TemplateURLPrepopulateDataTest, GetPrepopulatedEngineFromFullList) {
   // Ensure that we use the default set of search engines, which is google,
   // bing, yahoo.
-  OverrideCountryId(country_codes::kCountryIDUnknown);
+  prefs_.SetInteger(country_codes::kCountryIDAtInstall,
+                    country_codes::kCountryIDUnknown);
   ASSERT_EQ(TemplateURLPrepopulateData::GetPrepopulatedEngines(
                 &prefs_, &search_engine_choice_service_, nullptr)
                 .size(),
@@ -678,8 +666,8 @@ TEST_F(TemplateURLPrepopulateDataTest, GetPrepopulatedEngineFromFullList) {
 #if BUILDFLAG(IS_ANDROID)
 TEST_F(TemplateURLPrepopulateDataTest, GetLocalPrepopulatedEngines) {
   constexpr char sample_country[] = "US";
-  OverrideCountryId(country_codes::CountryCharsToCountryID(sample_country[0],
-                                                           sample_country[1]));
+  prefs_.SetInteger(country_codes::kCountryIDAtInstall,
+                    sample_country[0] << 8 | sample_country[1]);
 
   // For a given country, the output from `GetLocalPrepopulatedEngines`
   // should match the template URLs obtained from `GetPrepopulatedEngines`.
