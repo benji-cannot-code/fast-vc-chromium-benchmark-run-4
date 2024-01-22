3f14608a8b11d086baf3837a9361c4512cd077a6
Fei Shao <fshao@chromium.org>
2024-01-22 10:27:16 +0000

    Revert "Drm blobs: Always specify values"
    This reverts commit 130f33760d39964bae83eaf90ab8c9b3f0023e46.
    
    Reason for revert: break screen on ChromeOS devices: b/321609753
    
    Original change's description:
    > Drm blobs: Always specify values
    >
    > Prior to this change, if the specified CTM, GAMMA, or DEGAMMA values
    > are empty, then we would set the relevant property to 0 (that is,
    > leave them unspecified).
    >
    > This is a difficulty property to maintain. Rather than maintain it,
    > change it so that:
    > - an empty CTM causes the CTM to be set to the identity
    > - an empty [DE]GAMMA causes [DE]GAMMA to be set to the identity
    >
    > Bug: 1505062
    > Change-Id: Iad1a8f66ef3dc552685cc172e9cc15659e156a14
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5201196
    > Reviewed-by: Sasha McIntosh <sashamcintosh@chromium.org>
    > Commit-Queue: ccameron chromium <ccameron@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1248162}
    
    Bug: 1505062
    Bug: b:321609753, b:321593582
    Change-Id: If146b5f4b69d67d8e4801a99b2efdfb0f5131263
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5223101
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Auto-Submit: Fei Shao <fshao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250074}

--- ui/ozone/platform/drm/gpu/drm_gpu_util.h
+++ ui/ozone/platform/drm/gpu/drm_gpu_util.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/drm/common/drm_wrapper.h"
 #include "ui/ozone/platform/drm/common/scoped_drm_types.h"
 
-struct skcms_Matrix3x3;
-
 namespace ui {
 
 // Helper function that finds the property with the specified name.
@@ -35,7 +33,6 @@ ScopedDrmColorLutPtr CreateLutBlob(const display::GammaCurve& source,
 // significant bit is the sign.
 // |color_matrix| represents a 3x3 matrix in vector form.
 ScopedDrmColorCtmPtr CreateCTMBlob(const std::vector<float>& color_matrix);
-ScopedDrmColorCtmPtr CreateCTMBlob(const skcms_Matrix3x3& color_matrix);
 
 // Creates a FB Damage Clip Blob
 ScopedDrmModeRectPtr CreateDCBlob(const gfx::Rect& rect);
