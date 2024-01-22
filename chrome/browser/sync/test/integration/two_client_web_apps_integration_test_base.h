bf102ecc1a433bfbbf269cf75ae073e55ffdf390
Maggie Cai <mxcai@google.com>
2024-01-22 08:51:55 +0000

    [Shortstand] Disable CrosShortstand for TwoClientWebAppsIntegrationTest.
    This CL disables the kCrosShortstand flag for
    TwoClientWebAppsIntegrationTest even after the flag is enabled by
    default. This is because Shortstand changes the behaviour of shortcuts,
    which would need significant changes to the test, and our long term goal
    is to remove the shortcut support entirely from web app system.
    
    BUG=b/320205452
    
    Change-Id: I58369e6c4d03780601bebea566110bfa6179d332
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5197063
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250041}

--- chrome/browser/sync/test/integration/two_client_web_apps_integration_test_base.h
+++ chrome/browser/sync/test/integration/two_client_web_apps_integration_test_base.h
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/test/integration/web_apps_sync_test_base.h"
 #include "chrome/browser/ui/views/web_apps/web_app_integration_test_driver.h"
 
+#if BUILDFLAG(IS_CHROMEOS)
+#include "base/test/scoped_feature_list.h"
+#endif
+
 namespace base {
 class CommandLine;
 }
@@ -43,6 +47,11 @@ class TwoClientWebAppsIntegrationTestBase
   void SetUpCommandLine(base::CommandLine* command_line) override;
 
   WebAppIntegrationTestDriver helper_;
+
+#if BUILDFLAG(IS_CHROMEOS)
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+#endif
 };
 
 }  // namespace web_app::integration_tests
