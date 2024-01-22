de4ea2693ff2e4566d252f1ec2ff55c09e633933
Łukasz Anforowicz <lukasza@chromium.org>
2024-01-22 21:56:10 +0000

    Revert "c/qr_code_generator: enable PRESUBMIT cpplint and fix errors"
    This reverts commit 6b1e8484a2954273011e2e5a28b4cbf6e144d4ea.
    
    Reason for revert: suspected of causing https://crbug.com/1520419
    
    Original change's description:
    > c/qr_code_generator: enable PRESUBMIT cpplint and fix errors
    >
    >
    > - [runtime/arrays] Do not use variable-length arrays.
    > - [build/include_order] Found C system header after C++ system header.
    > - [build/include_what_you_use] Add #include <string> for string
    > - [build/include_what_you_use] Add #include <map> for map<>
    > - [build/include_what_you_use] #include <utility> for move
    >
    > Bug: 1327428
    > Change-Id: If203a09eab4c09acf4c40c68482697df2acbfa12
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5216466
    > Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    > Commit-Queue: Helmut Januschka <helmut@januschka.com>
    > Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1249588}
    
    Bug: 1327428
    Change-Id: I34677a78b01f15b0fc2b74934ea1140884c220dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5225847
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1250456}

--- components/qr_code_generator/qr_code_generator_fuzzer.cc
+++ components/qr_code_generator/qr_code_generator_fuzzer.cc
@@ -3,12 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/qr_code_generator/qr_code_generator.h"
-
-#include <fuzzer/FuzzedDataProvider.h>
 #include <stdint.h>
 
 #include <tuple>
+
+#include <fuzzer/FuzzedDataProvider.h>
+
+#include "components/qr_code_generator/qr_code_generator.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   FuzzedDataProvider provider(data, size);
