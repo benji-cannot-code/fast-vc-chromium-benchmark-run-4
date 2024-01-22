aabd480f0ba52fcb4babec4b8640d913ad2a3eab
Alison Gale <agale@chromium.org>
2024-01-22 17:27:58 +0000

    Add domain as subtitle for discarded tab page action dialog
    Bug: 1518993
    Change-Id: I87049f846812aa555bf5051858df8344ed2dd84a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5212411
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250254}

--- chrome/browser/ui/views/performance_controls/memory_saver_bubble_view.cc
+++ chrome/browser/ui/views/performance_controls/memory_saver_bubble_view.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/performance_controls/memory_saver_bubble_view.h"
 
 #include "base/functional/bind.h"
+#include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/profiles/profile.h"
@@ -99,6 +100,8 @@ views::BubbleDialogModelHost* MemorySaverBubbleView::ShowBubble(
 
   const bool show_memory_savings_chart = base::FeatureList::IsEnabled(
       performance_manager::features::kMemorySavingsReportingImprovements);
+  content::WebContents* web_contents =
+      browser->tab_strip_model()->GetActiveWebContents();
 
   dialog_model_builder
       .SetTitle(
@@ -113,8 +116,6 @@ views::BubbleDialogModelHost* MemorySaverBubbleView::ShowBubble(
                        .SetLabel(l10n_util::GetStringUTF16(IDS_OK))
                        .SetId(kMemorySaverDialogOkButton));
 
-  content::WebContents* web_contents =
-      browser->tab_strip_model()->GetActiveWebContents();
   const uint64_t memory_savings =
       memory_saver::GetDiscardedMemorySavingsInBytes(web_contents);
 
@@ -169,6 +170,8 @@ views::BubbleDialogModelHost* MemorySaverBubbleView::ShowBubble(
   if (base::FeatureList::IsEnabled(
           performance_manager::features::kDiscardExceptionsImprovements) &&
       !is_guest && !profile->IsIncognitoProfile()) {
+    dialog_model_builder.SetSubtitle(
+        base::UTF8ToUTF16(web_contents->GetURL().host()));
     const bool is_site_excluded = performance_manager::user_tuning::prefs::
         IsSiteInTabDiscardExceptionsList(profile->GetPrefs(),
                                          web_contents->GetURL().host());
