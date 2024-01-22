78ae90a18f959cd656b3e9aef2f696ab675d51c0
Jack Yammine <jyammine@google.com>
2024-01-22 21:02:39 +0000

    [waffle]Record a user action when the chevron is expanded
    Record a user action every time the user expands the marketing snippet
    chevron.
    
    The user action can be recorded multiple times per search engine but
    only when the chevron is expanded.
    
    Bug: b:320417729
    Change-Id: I1539f1e1278f121c036208374fe4a4027fcb22d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5206591
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Auto-Submit: Jack Yammine <jyammine@google.com>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250413}

--- chrome/browser/resources/search_engine_choice/app.ts
+++ chrome/browser/resources/search_engine_choice/app.ts
@@ -182,6 +182,12 @@ export class SearchEngineChoiceAppElement extends
     }
   }
 
+  private onChevronClicked_(chevronExpanded: boolean) {
+    if (chevronExpanded) {
+      chrome.metricsPrivate.recordUserAction('ExpandSearchEngineDescription');
+    }
+  }
+
   private onInfoDialogButtonClicked_() {
     this.$.infoDialog.close();
   }
