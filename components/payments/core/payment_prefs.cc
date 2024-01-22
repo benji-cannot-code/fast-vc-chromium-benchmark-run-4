5da8e25e5facd00368dd9cfbc1709d16d06f2944
Marc Treib <treib@chromium.org>
2024-01-22 08:57:50 +0000

    Payments: Make all pref names `constexpr`
    This will allow using them in `consteval` contexts, specifically
    `MakeFixedFlatMap()` - see linked bug for more context.
    (It's also arguably preferable anyway - they're now all in one place,
    the header, rather than split across .h and .cc.)
    
    Bug: 1512537
    Change-Id: Ib6b2b1af49e7e78273385a4c3e49d84724f88b3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5214375
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250043}

--- components/payments/core/payment_prefs.cc
+++ components/payments/core/payment_prefs.cc
@@ -9,11 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace payments {
 
-const char kPaymentsFirstTransactionCompleted[] =
-    "payments.first_transaction_completed";
-
-const char kCanMakePaymentEnabled[] = "payments.can_make_payment_enabled";
-
 void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterBooleanPref(kPaymentsFirstTransactionCompleted, false);
   registry->RegisterBooleanPref(
