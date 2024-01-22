5e6b182e5867879ffb81c5cb9a81b1a36664f6c7
Justin Lulejian <jlulejian@chromium.org>
2024-01-22 22:37:57 +0000

    [Extensions] Create a testing method for number of worker starts.
    Before this change there wasn't a way to measure how many times a worker
    was started during a test. The main benefit of the extensions_features::kExtensionsServiceWorkerOptimizedEventDispatch
    feature was that we would reduce the number of redundant starts of the
    worker so we need a way to measure this.
    
    Low-Coverage-Reason: TRIVIAL_CHANGE This change contains only minor
    interface definitions on a test observer.
    
    Bug: 1467015
    Change-Id: I7e8844b2ebaa174dc9f7f67910142cc08f533253
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5167747
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250492}

--- extensions/browser/service_worker_task_queue.cc
+++ extensions/browser/service_worker_task_queue.cc
@@ -577,6 +577,9 @@ void ServiceWorkerTaskQueue::RunTasksAfterStartWorker(
       base::BindOnce(&ServiceWorkerTaskQueue::DidStartWorkerFail,
                      weak_factory_.GetWeakPtr(), context_id,
                      base::Time::Now()));
+  if (g_test_observer) {
+    g_test_observer->RequestedWorkerStart(context_id.extension_id);
+  }
 }
 
 void ServiceWorkerTaskQueue::DidRegisterServiceWorker(
