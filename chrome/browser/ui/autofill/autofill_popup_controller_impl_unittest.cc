06ddefdd9b3a5aef3e4e03759b35e10f7938ce91
Christoph Schwering <schwering@google.com>
2024-01-22 12:14:38 +0000

    [Autofill] Move popup hiding from client to popup controller
    Bug: 1519872
    Change-Id: I91f59e850e95a9226bcfb9f3f55d4da9a811d052
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5214311
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1250111}

--- chrome/browser/ui/autofill/autofill_popup_controller_impl_unittest.cc
+++ chrome/browser/ui/autofill/autofill_popup_controller_impl_unittest.cc
@@ -400,6 +400,15 @@ class AutofillPopupControllerImplTest : public ChromeRenderViewHostTestHarness {
 #endif  // BUILDFLAG(IS_ANDROID)
   }
 
+  void TearDown() override {
+    // Wait for the pending deletion of the controllers. Otherwise, the
+    // controllers are destroyed after the WebContents, and each of them
+    // receives a final Hide() call for which we'd need to add explicit
+    // expectations.
+    task_environment()->RunUntilIdle();
+    ChromeRenderViewHostTestHarness::TearDown();
+  }
+
   content::RenderFrameHost* main_frame() {
     return web_contents()->GetPrimaryMainFrame();
   }
@@ -410,8 +419,7 @@ class AutofillPopupControllerImplTest : public ChromeRenderViewHostTestHarness {
 
   NiceMock<MockAutofillDriver>& driver(
       content::RenderFrameHost* rfh = nullptr) {
-    return *autofill_driver_injector_
-        [rfh ? rfh : web_contents()->GetPrimaryMainFrame()];
+    return *autofill_driver_injector_[rfh ? rfh : main_frame()];
   }
 
   BrowserAutofillManagerWithMockDelegate& manager(
@@ -1512,6 +1520,18 @@ TEST_F(AutofillPopupControllerImplTestHidingLogic,
   Mock::VerifyAndClearExpectations(&client().popup_controller(manager()));
 }
 
+// Tests that if the popup is shown, destruction of the WebContents hides the
+// popup.
+TEST_F(AutofillPopupControllerImplTestHidingLogic, HideOnWebContentsDestroyed) {
+  ShowSuggestions(manager(), {PopupItemId::kAddressEntry});
+  test::GenerateTestAutofillPopup(&manager().external_delegate());
+  EXPECT_CALL(client().popup_controller(manager()),
+              Hide(PopupHidingReason::kRendererEvent));
+  EXPECT_CALL(client().popup_controller(manager()),
+              Hide(PopupHidingReason::kTabGone));
+  DeleteContents();
+}
+
 // Tests that if the popup is shown in the *main frame*, destruction of the
 // *main frame* hides the popup.
 TEST_F(AutofillPopupControllerImplTestHidingLogic,
@@ -1520,6 +1540,11 @@ TEST_F(AutofillPopupControllerImplTestHidingLogic,
   test::GenerateTestAutofillPopup(&manager().external_delegate());
   EXPECT_CALL(client().popup_controller(manager()),
               Hide(PopupHidingReason::kRendererEvent));
+  // There seems to be no way to destroy only the main frame in a test. We
+  // therefore let the test fixture's TearDown() destroy the main frame. As a
+  // side-effect, the WebContents will also be destroyed and call Hide().
+  EXPECT_CALL(client().popup_controller(manager()),
+              Hide(PopupHidingReason::kTabGone));
 }
 
 // Tests that if the popup is shown in the *sub frame*, destruction of the
@@ -1530,6 +1555,9 @@ TEST_F(AutofillPopupControllerImplTestHidingLogic,
   test::GenerateTestAutofillPopup(&sub_manager().external_delegate());
   EXPECT_CALL(client().popup_controller(sub_manager()),
               Hide(PopupHidingReason::kRendererEvent));
+  content::RenderFrameHostTester::For(sub_frame())->Detach();
+  // Verify and clear before TearDown() closes the popup.
+  Mock::VerifyAndClearExpectations(&client().popup_controller(sub_manager()));
 }
 
 // Tests that if the popup is shown in the *main frame*, a navigation in the
@@ -1541,6 +1569,8 @@ TEST_F(AutofillPopupControllerImplTestHidingLogic,
   EXPECT_CALL(client().popup_controller(manager()),
               Hide(PopupHidingReason::kNavigation));
   NavigateAndCommitFrame(main_frame(), GURL("https://bar.com/"));
+  // Verify and clear before TearDown() closes the popup.
+  Mock::VerifyAndClearExpectations(&client().popup_controller(manager()));
 }
 
 // Tests that if the popup is shown in the *sub frame*, a navigation in the
@@ -1558,10 +1588,17 @@ TEST_F(AutofillPopupControllerImplTestHidingLogic,
                 Hide(PopupHidingReason::kRendererEvent));
   }
   NavigateAndCommitFrame(sub_frame(), GURL("https://bar.com/"));
+  // Verify and clear before TearDown() closes the popup.
+  Mock::VerifyAndClearExpectations(&client().popup_controller(sub_manager()));
 }
 
 // Tests that if the popup is shown in the *sub frame*, a navigation in the
 // *main frame* hides the popup.
+//
+// TODO(crbug.com/1519872): This test only makes little sense: with BFcache, the
+// navigation doesn't destroy the `sub_frame()` and thus we wouldn't hide the
+// popup. What hides the popup in reality is
+// AutofillExternalDelegate::DidEndTextFieldEditing().
 TEST_F(AutofillPopupControllerImplTestHidingLogic,
        HideInSubFrameOnMainFrameNavigation) {
   ShowSuggestions(sub_manager(), {PopupItemId::kAddressEntry});
@@ -1569,6 +1606,9 @@ TEST_F(AutofillPopupControllerImplTestHidingLogic,
   EXPECT_CALL(client().popup_controller(sub_manager()),
               Hide(PopupHidingReason::kRendererEvent));
   NavigateAndCommitFrame(main_frame(), GURL("https://bar.com/"));
+  // The WebContents will also be destroyed and call Hide().
+  EXPECT_CALL(client().popup_controller(sub_manager()),
+              Hide(PopupHidingReason::kTabGone));
 }
 
 }  // namespace autofill
--- chrome/browser/ui/autofill/chrome_autofill_client.cc
+++ chrome/browser/ui/autofill/chrome_autofill_client.cc
@@ -1320,20 +1320,7 @@ ChromeAutofillClient::GetDeviceAuthenticator() {
 #endif
 }
 
-void ChromeAutofillClient::PrimaryMainFrameWasResized(bool width_changed) {
-#if BUILDFLAG(IS_ANDROID)
-  // Ignore virtual keyboard showing and hiding a strip of suggestions.
-  if (!width_changed)
-    return;
-#endif
-
-  HideAutofillPopup(PopupHidingReason::kWidgetChanged);
-  // Do not need to hide the Touch To Fill surface, since it is an overlay UI
-  // which doesn't depend on frame size.
-}
-
 void ChromeAutofillClient::WebContentsDestroyed() {
-  HideAutofillPopup(PopupHidingReason::kTabGone);
   if (IsTouchToFillCreditCardSupported())
     HideTouchToFillCreditCard();
 }
@@ -1341,7 +1328,6 @@ void ChromeAutofillClient::WebContentsDestroyed() {
 void ChromeAutofillClient::OnWebContentsLostFocus(
     content::RenderWidgetHost* render_widget_host) {
   has_focus_ = false;
-  HideAutofillPopup(PopupHidingReason::kFocusChanged);
   // Should not hide the Touch To Fill surface, since it is an overlay UI
   // which takes the focus.
 }
