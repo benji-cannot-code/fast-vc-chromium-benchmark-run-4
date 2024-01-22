b5c4cc84eddb7e1124ea447141b7e536da560192
punithnayak <npunith125@gmail.com>
2024-01-22 18:44:23 +0000

    Remove expired histograms UserInterfaceStyle.CurrentlyUsed
    OBSOLETE_HISTOGRAM[UserInterfaceStyle.CurrentlyUsed] = removed because it expired
    
    Bug: 1508102
    Change-Id: Iaee79789d1872250ff3ed5c94579a4c13ed32f29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5112279
    Commit-Queue: Punith Nayak <npunith125@gmail.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250309}

--- ios/chrome/app/chrome_overlay_window.mm
+++ ios/chrome/app/chrome_overlay_window.mm
@@ -59,16 +59,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
   [super traitCollectionDidChange:previousTraitCollection];
-  if (previousTraitCollection.horizontalSizeClass !=
-      self.traitCollection.horizontalSizeClass) {
-    [self updateCrashKeys];
-  }
-  if ([self.traitCollection
-          hasDifferentColorAppearanceComparedToTraitCollection:
-              previousTraitCollection]) {
-    [self.userInterfaceStyleRecorder
-        userInterfaceStyleDidChange:self.traitCollection.userInterfaceStyle];
-  }
   [self updateCrashKeys];
 }
 
--- ios/chrome/browser/metrics/model/user_interface_style_recorder.h
+++ ios/chrome/browser/metrics/model/user_interface_style_recorder.h
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (instancetype)init NS_UNAVAILABLE;
 
-// Call this method to report the new user interface to UMA.
-- (void)userInterfaceStyleDidChange:(UIUserInterfaceStyle)newUserInterfaceStyle;
 
 @end
 
