c1a152b8c604f9498d2cbf45d8a867e1e72f43f8
Eriko Kurimoto <elkurin@chromium.org>
2024-01-22 12:21:39 +0000

    Revert "[A11y / FaceGaze]: Don't respond to synthesized events"
    This reverts commit 46a323f09644c68bc974fa5ae1ce6e20f6cef9b2.
    
    Reason for revert: Chained cl breaking FaceGazeTest
    
    Original change's description:
    > [A11y / FaceGaze]: Don't respond to synthesized events
    >
    > Upon making the mouse updates faster in crrev.com/c/5207489,
    > found that the mouse position callback to Javascript was
    > occurring later than the previous mouse position was sent in.
    > This is fixed by not sending synthetic mouse events to the
    > extensions.
    >
    > This change also adds a timeout after the last non-synthetic mouse
    > move or drag during which FaceGaze doesn't appear to update the
    > cursor position; this allows a user to use a mouse or trackpad to
    > update their cursor location without it jumping around for half
    > a second (500 ms).
    >
    > Note: Tested that the magnifier still appears to move as expected
    > when moving the mouse around after modifying
    > accessibility_event_rewriter_delegate_impl, and ChromeVox
    > "speak text under the mouse" works with FaceGaze.
    >
    > Bug: b/317235785
    > AX-Relnotes: N/A
    > Test: New, existing, manual
    > Change-Id: I0086890089e3f66d581c8861364fc259a0a7f3c4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5212825
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    > Commit-Queue: Katie Dektar <katie@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1249586}
    
    Bug: b/317235785
    Change-Id: I3b65d7b6e42d688caf69f913418372e2216b473d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5223680
    Auto-Submit: Eriko Kurimoto <elkurin@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1250115}

--- ui/events/test/event_generator.cc
+++ ui/events/test/event_generator.cc
@@ -135,7 +135,6 @@ void EventGenerator::PressButton(int flag) {
     gfx::Point location = GetLocationInCurrentRoot();
     ui::MouseEvent mouseev(ui::ET_MOUSE_PRESSED, location, location,
                            ui::EventTimeForNow(), flags_, flag);
-    mouseev.set_source_device_id(mouse_source_device_id_);
     Dispatch(&mouseev);
   }
 }
@@ -145,7 +144,6 @@ void EventGenerator::ReleaseButton(int flag) {
     gfx::Point location = GetLocationInCurrentRoot();
     ui::MouseEvent mouseev(ui::ET_MOUSE_RELEASED, location, location,
                            ui::EventTimeForNow(), flags_, flag);
-    mouseev.set_source_device_id(mouse_source_device_id_);
     Dispatch(&mouseev);
     flags_ ^= flag;
   }
@@ -190,7 +188,6 @@ void EventGenerator::MoveMouseWheel(int delta_x, int delta_y) {
   gfx::Point location = GetLocationInCurrentRoot();
   ui::MouseWheelEvent wheelev(gfx::Vector2d(delta_x, delta_y), location,
                               location, ui::EventTimeForNow(), flags_, 0);
-  wheelev.set_source_device_id(mouse_source_device_id_);
   Dispatch(&wheelev);
 }
 
@@ -199,7 +196,6 @@ void EventGenerator::SendMouseEnter() {
   delegate()->ConvertPointToTarget(current_target_, &enter_location);
   ui::MouseEvent mouseev(ui::ET_MOUSE_ENTERED, enter_location, enter_location,
                          ui::EventTimeForNow(), flags_, 0);
-  mouseev.set_source_device_id(mouse_source_device_id_);
   Dispatch(&mouseev);
 }
 
@@ -208,7 +204,6 @@ void EventGenerator::SendMouseExit() {
   delegate()->ConvertPointToTarget(current_target_, &exit_location);
   ui::MouseEvent mouseev(ui::ET_MOUSE_EXITED, exit_location, exit_location,
                          ui::EventTimeForNow(), flags_, 0);
-  mouseev.set_source_device_id(mouse_source_device_id_);
   Dispatch(&mouseev);
 }
 
@@ -223,7 +218,6 @@ void EventGenerator::MoveMouseToWithNative(const gfx::Point& point_in_host,
       new ui::MouseEvent(ui::ET_MOUSE_MOVED, point_in_host, point_in_host,
                          ui::EventTimeForNow(), flags_, 0));
   ui::MouseEvent mouseev(native_event.get());
-  mouseev.set_source_device_id(mouse_source_device_id_);
   native_event->set_location(point_for_native);
   Dispatch(&mouseev);
 
@@ -237,7 +231,6 @@ void EventGenerator::MoveMouseToInHost(const gfx::Point& point_in_host) {
       ui::ET_MOUSE_DRAGGED : ui::ET_MOUSE_MOVED;
   ui::MouseEvent mouseev(event_type, point_in_host, point_in_host,
                          ui::EventTimeForNow(), flags_, 0);
-  mouseev.set_source_device_id(mouse_source_device_id_);
   Dispatch(&mouseev);
 
   SetCurrentScreenLocation(point_in_host);
@@ -261,7 +254,6 @@ void EventGenerator::MoveMouseTo(const gfx::Point& point_in_screen,
     delegate()->ConvertPointToTarget(current_target_, &move_point);
     ui::MouseEvent mouseev(event_type, move_point, move_point,
                            ui::EventTimeForNow(), flags_, 0);
-    mouseev.set_source_device_id(mouse_source_device_id_);
     Dispatch(&mouseev);
   }
   SetCurrentScreenLocation(point_in_screen);
