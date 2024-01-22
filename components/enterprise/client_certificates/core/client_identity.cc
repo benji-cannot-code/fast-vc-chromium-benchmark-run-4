6384fba585b9ce5c0eb4c3bc6743a2fb88782f92
Sebastien Lalancette <seblalancette@chromium.org>
2024-01-22 23:51:49 +0000

    Create a CertificateStore backed by LevelDB
    This CertificateStore will be used as storage layer for provisioning
    client certificates for managed Chrome Profiles. Private keys will be
    stored alongside their corresponding certificates, and a consumer
    of that Store will eventually expose those identities in mTLS
    connections or other attestation mechanisms.
    
    Bug: b:319256086
    Change-Id: I9017844e1b30706a242fda3849711cc384235116
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5213410
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Hamda Mare <hmare@google.com>
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250539}

---
+++ components/enterprise/client_certificates/core/client_identity.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/enterprise/client_certificates/core/client_identity.h"
+
+#include <utility>
+
+#include "components/enterprise/client_certificates/core/private_key.h"
+#include "net/cert/x509_certificate.h"
+
+namespace client_certificates {
+
+ClientIdentity::ClientIdentity(
+    scoped_refptr<PrivateKey> private_key,
+    scoped_refptr<net::X509Certificate> client_certificate)
+    : private_key(std::move(private_key)),
+      client_certificate(std::move(client_certificate)) {}
+
+ClientIdentity::~ClientIdentity() = default;
+
+}  // namespace client_certificates
