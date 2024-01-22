37680bde0d94a8be6ab5a9b8490136b78f8e0eb6
adamta <adamta@google.com>
2024-01-22 16:28:42 +0000

    [iOS] Ignore NTP scroll events for replaced scrollViews
    The NTP may receive queued scroll events for a scrollView that has
    been replaced. These events should be ignored since the scrollView is
    no longer valid.
    
    Bug: b/319432995
    Change-Id: I5b18a5de4f671bb3f5de0200268aa3578efabc43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5225020
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1250217}

--- ios/chrome/browser/ui/ntp/new_tab_page_view_controller.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_view_controller.mm
@@ -788,6 +788,11 @@ const CGFloat kModuleMinMargin = 16;
   if (!self.feedWrapperViewController) {
     return;
   }
+  // Scroll events might still be queued for a previous scroll view which was
+  // now replaced. In these cases, ignore the scroll event.
+  if (scrollView != self.collectionView) {
+    return;
+  }
   [self.overscrollActionsController scrollViewDidScroll:scrollView];
   [self updateFakeOmniboxForScrollPosition];
 
@@ -804,6 +809,11 @@ const CGFloat kModuleMinMargin = 16;
 }
 
 - (void)scrollViewWillBeginDragging:(UIScrollView*)scrollView {
+  // Scroll events might still be queued for a previous scroll view which was
+  // now replaced. In these cases, ignore the scroll event.
+  if (scrollView != self.collectionView) {
+    return;
+  }
   // User has interacted with the surface, so it is safe to assume that a saved
   // scroll position can now be overriden.
   self.hasSavedOffsetFromPreviousScrollState = NO;
@@ -814,6 +824,11 @@ const CGFloat kModuleMinMargin = 16;
 - (void)scrollViewWillEndDragging:(UIScrollView*)scrollView
                      withVelocity:(CGPoint)velocity
               targetContentOffset:(inout CGPoint*)targetContentOffset {
+  // Scroll events might still be queued for a previous scroll view which was
+  // now replaced. In these cases, ignore the scroll event.
+  if (scrollView != self.collectionView) {
+    return;
+  }
   [self.overscrollActionsController
       scrollViewWillEndDragging:scrollView
                    withVelocity:velocity
@@ -822,6 +837,11 @@ const CGFloat kModuleMinMargin = 16;
 
 - (void)scrollViewDidEndDragging:(UIScrollView*)scrollView
                   willDecelerate:(BOOL)decelerate {
+  // Scroll events might still be queued for a previous scroll view which was
+  // now replaced. In these cases, ignore the scroll event.
+  if (scrollView != self.collectionView) {
+    return;
+  }
   [self.overscrollActionsController scrollViewDidEndDragging:scrollView
                                               willDecelerate:decelerate];
   if (self.isFeedVisible) {
@@ -847,6 +867,11 @@ const CGFloat kModuleMinMargin = 16;
 }
 
 - (BOOL)scrollViewShouldScrollToTop:(UIScrollView*)scrollView {
+  // Scroll events might still be queued for a previous scroll view which was
+  // now replaced. In these cases, ignore the scroll event.
+  if (scrollView != self.collectionView) {
+    return YES;
+  }
   // User has tapped the status bar to scroll to the top.
   // Prevent scrolling back to pre-focus state, making sure we don't have
   // two scrolling animations running at the same time.
