8c7d685df7cc1c3f1473166741f2e91e02bb60cc
Miyoung Shin <myid.shin@igalia.com>
2024-01-22 06:29:27 +0000

    Fix the use of incorrect namespaces in //net
    This CL fixes the use of incorrect namespaces detected by
    `bugprone-forward-declaration-namespace` clang-tidy check in //net.
    
    Bug: 1519029
    Change-Id: If4569593dea62677cd127d66fb52ca7d36cb64ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5219992
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250006}

--- net/test/embedded_test_server/request_handler_util.h
+++ net/test/embedded_test_server/request_handler_util.h
@@ -16,9 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "net/test/embedded_test_server/http_response.h"
 
-namespace url {
 class GURL;
-}
 
 namespace net::test_server {
 struct HttpRequest;
