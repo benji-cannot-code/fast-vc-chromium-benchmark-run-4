746af3d054420f50ee135acc6d74e031bfff70f6
Daseul Lee <dslee@chromium.org>
2024-01-22 19:16:45 +0000

    [FSA] Enable File System Access Persistent Permission feature.
    Bug: 1011533, 1520490
    Change-Id: I1f2dfa146af8b7dd5585312d2d42203e57653e6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5213227
    Commit-Queue: Daseul Lee <dslee@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Christine Hollingsworth <christinesm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250335}

--- chrome/browser/file_system_access/file_system_access_features.h
+++ chrome/browser/file_system_access/file_system_access_features.h
@@ -16,8 +16,6 @@ namespace features {
 BASE_DECLARE_FEATURE(kFileSystemAccessLocalUNCPathBlock);
 #endif
 
-// TODO(crbug.com/1467574): Remove `kFileSystemAccessPersistentPermissions`
-// flag after FSA Persistent Permissions feature launch.
 BASE_DECLARE_FEATURE(kFileSystemAccessPersistentPermissions);
 
 BASE_DECLARE_FEATURE(kFileSystemAccessSymbolicLinkCheck);
