53f898cd1472bb8fbaa3d94fb00abe7925f00891
Ritika Gupta <ritikagup@google.com>
2024-01-22 22:18:46 +0000

    [Android Home Magic Stack] Integrating segmentation with Android Home Magic Stack.
    In this CL, integrating Segmentation GetClassification API call for
    ranking modules of Magic Stack in Android Home Surface.
    
    Change-Id: I9e372caa069d1c3ddd4fe79c24a5f2734ecb3d08
    Bug: b/319530611
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5208316
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Cr-Commit-Position: refs/heads/main@{#1250475}

--- chrome/browser/magic_stack/android/java/src/org/chromium/chrome/browser/magic_stack/HomeModulesCoordinator.java
+++ chrome/browser/magic_stack/android/java/src/org/chromium/chrome/browser/magic_stack/HomeModulesCoordinator.java
@@ -17,16 +17,23 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.SnapHelper;
 
 import org.chromium.base.Callback;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.magic_stack.ModuleRegistry.OnViewCreatedCallback;
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.segmentation_platform.SegmentationPlatformServiceFactory;
 import org.chromium.components.browser_ui.styles.SemanticColorUtils;
 import org.chromium.components.browser_ui.widget.displaystyle.DisplayStyleObserver;
 import org.chromium.components.browser_ui.widget.displaystyle.UiConfig;
+import org.chromium.components.segmentation_platform.PredictionOptions;
+import org.chromium.components.segmentation_platform.SegmentationPlatformService;
+import org.chromium.components.segmentation_platform.prediction_status.PredictionStatus;
 import org.chromium.ui.base.DeviceFormFactor;
 import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.SimpleRecyclerViewAdapter;
 import org.chromium.url.GURL;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /** Root coordinator which is responsible for showing modules on home surfaces. */
@@ -37,6 +44,7 @@ public class HomeModulesCoordinator implements ModuleDelegate, OnViewCreatedCall
     private final RecyclerView mRecyclerView;
     private final ModelList mModel;
     private final HomeModulesContextMenuManager mHomeModulesContextMenuManager;
+    private final SegmentationPlatformService mSegmentationPlatformService;
 
     private CirclePagerIndicatorDecoration mPageIndicatorDecoration;
     private SnapHelper mSnapHelper;
@@ -54,7 +62,8 @@ public class HomeModulesCoordinator implements ModuleDelegate, OnViewCreatedCall
     public HomeModulesCoordinator(
             @NonNull Activity activity,
             @NonNull ModuleDelegateHost moduleDelegateHost,
-            @NonNull ViewGroup parentView) {
+            @NonNull ViewGroup parentView,
+            Profile profile) {
         mModuleDelegateHost = moduleDelegateHost;
         mHomeModulesContextMenuManager =
                 new HomeModulesContextMenuManager(
@@ -74,6 +83,10 @@ public class HomeModulesCoordinator implements ModuleDelegate, OnViewCreatedCall
         setupRecyclerView(activity);
 
         mMediator = new HomeModulesMediator(mModel, ModuleRegistry.getInstance());
+        mSegmentationPlatformService =
+                (profile != null)
+                        ? SegmentationPlatformServiceFactory.getForProfile(profile)
+                        : null;
     }
 
     private void setupRecyclerView(Activity activity) {
@@ -159,18 +172,13 @@ public class HomeModulesCoordinator implements ModuleDelegate, OnViewCreatedCall
      * @param onHomeModulesShownCallback The callback called when the magic stack is shown.
      */
     public void show(Callback<Boolean> onHomeModulesShownCallback) {
-        List<Integer> moduleList = getModuleList();
-        if (moduleList == null) {
-            onHomeModulesShownCallback.onResult(false);
+        if (mSegmentationPlatformService == null
+                || !ChromeFeatureList.isEnabled(
+                        ChromeFeatureList.SEGMENTATION_PLATFORM_ANDROID_HOME_MODULE_RANKER)) {
+            onGotRankedModules(getFixedModuleList(), onHomeModulesShownCallback);
             return;
         }
-
-        mMediator.buildModulesAndShow(
-                moduleList,
-                this,
-                (isVisible) -> {
-                    onHomeModulesShownCallback.onResult(isVisible);
-                });
+        getSegmentationRanking(onHomeModulesShownCallback);
     }
 
     /** Hides the modules and cleans up. */
@@ -271,9 +279,59 @@ public class HomeModulesCoordinator implements ModuleDelegate, OnViewCreatedCall
         return mIsSnapHelperAttached;
     }
 
-    private List<Integer> getModuleList() {
-        // TODO(https://crbug.com/1512962): Gets the modules ranking list using segmentation service
-        // API.
+    private List<Integer> getFixedModuleList() {
         return List.of(ModuleType.PRICE_CHANGE, ModuleType.SINGLE_TAB);
+    }
+
+    private void onGotRankedModules(
+            List<Integer> moduleList, Callback<Boolean> onHomeModulesShownCallback) {
+        if (moduleList == null) {
+            onHomeModulesShownCallback.onResult(false);
+            return;
+        }
+
+        mMediator.buildModulesAndShow(
+                moduleList,
+                this,
+                (isVisible) -> {
+                    onHomeModulesShownCallback.onResult(isVisible);
+                });
+    }
+
+    private void getSegmentationRanking(Callback<Boolean> onHomeModulesShownCallback) {
+        PredictionOptions options = new PredictionOptions(false);
+        mSegmentationPlatformService.getClassificationResult(
+                "android_home_module_ranker",
+                options,
+                null,
+                result -> {
+                    // If segmentation service fails, fallback to return fixed module list.
+                    List<Integer> moduleList;
+                    if (result.status != PredictionStatus.SUCCEEDED
+                            || result.orderedLabels.isEmpty()) {
+                        moduleList = getFixedModuleList();
+                    } else {
+                        moduleList = convertLabelsToModuleList(result.orderedLabels);
+                    }
+                    onGotRankedModules(moduleList, onHomeModulesShownCallback);
+                });
+    }
+
+    private List<Integer> convertLabelsToModuleList(List<String> orderedLabels) {
+        List<Integer> moduleList = new ArrayList<Integer>();
+        for (String label : orderedLabels) {
+            moduleList.add(convertLabelToModuleType(label));
+        }
+        return moduleList;
+    }
+
+    private Integer convertLabelToModuleType(String label) {
+        switch (label) {
+            case "SingleTab":
+                return ModuleType.SINGLE_TAB;
+            case "PriceChange":
+                return ModuleType.PRICE_CHANGE;
+        }
+        return ModuleType.NUM_ENTRIES;
     }
 }
--- chrome/browser/magic_stack/android/junit/src/org/chromium/chrome/browser/magic_stack/HomeModulesCoordinatorUnitTest.java
+++ chrome/browser/magic_stack/android/junit/src/org/chromium/chrome/browser/magic_stack/HomeModulesCoordinatorUnitTest.java
@@ -100,7 +100,7 @@ public class HomeModulesCoordinatorUnitTest {
     @SmallTest
     public void testCreate_phones() {
         assertFalse(DeviceFormFactor.isNonMultiDisplayContextOnTablet(mActivity));
-        mCoordinator = new HomeModulesCoordinator(mActivity, mModuleDelegateHost, mView);
+        mCoordinator = new HomeModulesCoordinator(mActivity, mModuleDelegateHost, mView, null);
 
         // Verifies that there isn't an observer of UiConfig registered.
         assertTrue(mCoordinator.getIsSnapHelperAttachedForTesting());
@@ -117,7 +117,7 @@ public class HomeModulesCoordinatorUnitTest {
                 new DisplayStyle(HorizontalDisplayStyle.WIDE, VerticalDisplayStyle.REGULAR);
         when(mUiConfig.getCurrentDisplayStyle()).thenReturn(displayStyle);
 
-        mCoordinator = new HomeModulesCoordinator(mActivity, mModuleDelegateHost, mView);
+        mCoordinator = new HomeModulesCoordinator(mActivity, mModuleDelegateHost, mView, null);
         // Verifies that an observer is registered to the mUiConfig on tablets.
         verify(mUiConfig).addObserver(mDisplayStyleObserver.capture());
 
@@ -146,7 +146,7 @@ public class HomeModulesCoordinatorUnitTest {
                 new DisplayStyle(HorizontalDisplayStyle.REGULAR, VerticalDisplayStyle.REGULAR);
         when(mUiConfig.getCurrentDisplayStyle()).thenReturn(displayStyle);
 
-        mCoordinator = new HomeModulesCoordinator(mActivity, mModuleDelegateHost, mView);
+        mCoordinator = new HomeModulesCoordinator(mActivity, mModuleDelegateHost, mView, null);
         // Verifies that an observer is registered to the mUiConfig on tablets.
         verify(mUiConfig).addObserver(mDisplayStyleObserver.capture());
 
