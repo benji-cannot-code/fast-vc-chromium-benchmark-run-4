68f526ed3668d9a94c2d0717dfd103d1e4878dd6
Alexis Hetu <sugoi@chromium.org>
2024-01-22 16:46:05 +0000

    [iOS] Disable exiting the manual fill menu using a side swipe
    When the `kIOSKeyboardAccessoryUpgrade` feature is enabled, the
    mechanism used to exit the manual fill menu by swiping the form
    suggestion's scroll view towards the right should no longer be
    available.
    
    Bug: 1501851
    Change-Id: I732c8088f63391ffd8466f47fe72b9a91d6a812b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5214231
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250229}

--- ios/chrome/browser/ui/autofill/form_input_accessory/form_input_accessory_view_controller.mm
+++ ios/chrome/browser/ui/autofill/form_input_accessory/form_input_accessory_view_controller.mm
@@ -372,6 +372,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)formSuggestionViewShouldResetFromPull:
     (FormSuggestionView*)formSuggestionView {
+  DCHECK(!IsKeyboardAccessoryUpgradeEnabled());
+
   base::RecordAction(base::UserMetricsAction("ManualFallback_ClosePull"));
   // The pull gesture has the same effect as when the keyboard button is
   // pressed.
--- ios/chrome/browser/ui/autofill/form_input_accessory/form_suggestion_view.mm
+++ ios/chrome/browser/ui/autofill/form_input_accessory/form_suggestion_view.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/autofill/ios/browser/form_suggestion.h"
 #import "ios/chrome/browser/autofill/model/form_suggestion_client.h"
 #import "ios/chrome/browser/autofill/model/form_suggestion_constants.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/shared/ui/util/layout_guide_names.h"
 #import "ios/chrome/browser/shared/ui/util/rtl_geometry.h"
 #import "ios/chrome/browser/shared/ui/util/util_swift.h"
@@ -84,7 +85,9 @@ const CGFloat kSuggestionHorizontalMargin = 6;
         self.contentInset = lockedContentInsets;
       }
       completion:^(BOOL finished) {
-        self.delegate = self;
+        if (!IsKeyboardAccessoryUpgradeEnabled()) {
+          self.delegate = self;
+        }
       }];
 }
 
@@ -176,7 +179,11 @@ const CGFloat kSuggestionHorizontalMargin = 6;
   }
 }
 
+#pragma mark - UIScrollViewDelegate
+
 - (void)scrollViewDidScroll:(UIScrollView*)scrollView {
+  DCHECK(!IsKeyboardAccessoryUpgradeEnabled());
+
   CGFloat offset = self.contentOffset.x;
   CGFloat inset = self.contentInset.left;  // Inset is negative when locked.
   CGFloat diff = offset + inset;
