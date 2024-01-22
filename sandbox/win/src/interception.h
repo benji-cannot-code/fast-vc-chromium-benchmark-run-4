a1e999075424290bd72c8f6319cff5e682476b8a
Ben Bamesberger <benb@microsoft.com>
2024-01-22 21:10:39 +0000

    Windows sandbox: Split TransferVariable address param into local and remote parts
    This change enables concurrent use of TransferVariable by splitting the
    address param into local and target parts. This allows the broker to
    copy data from a local variable instead of from the global variable.
    This works because the broker only needs the address of a global
    variable in its own process for determining the corresponding address
    in the target, and so it is not required for any data to be set in the
    broker-side global before copying.
    
    
    Bug: 1499551
    Change-Id: Ib6322ebd0df14178038e511652be91660610cef2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5202112
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: ben b <benb@microsoft.com>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250420}

--- sandbox/win/src/interception.h
+++ sandbox/win/src/interception.h
@@ -18,16 +18,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr_exclusion.h"
 #include "base/memory/raw_ref.h"
+#include "base/types/expected.h"
+#include "sandbox/win/src/interception_internal.h"
 #include "sandbox/win/src/interceptors.h"
 #include "sandbox/win/src/sandbox_types.h"
 
 namespace sandbox {
 
 class TargetProcess;
-
-// Internal structures used for communication between the broker and the target.
-struct DllPatchInfo;
-struct DllInterceptionData;
 
 // The InterceptionManager executes on the parent application, and it is in
 // charge of setting up the desired interceptions, and placing the Interception
@@ -201,12 +199,11 @@ class InterceptionManager {
   ResultCode PatchNtdll(bool hot_patch_needed);
 
   // Peforms the actual interceptions on ntdll.
-  // thunks is the memory to store all the thunks for this dll (on the child),
-  // and dll_data is a local buffer to hold global dll interception info.
-  // Returns SBOX_ALL_OK on success.
-  ResultCode PatchClientFunctions(DllInterceptionData* thunks,
-                                  size_t thunk_bytes,
-                                  DllInterceptionData* dll_data);
+  // thunks is the memory to store all the thunks for this dll (on the child).
+  // On success, returns info about the intercepted functions.
+  base::expected<PatchClientResultData, ResultCode> PatchClientFunctions(
+      DllInterceptionData* thunks,
+      size_t thunk_bytes);
 
   // The process to intercept.
   const raw_ref<TargetProcess> child_;
--- sandbox/win/src/interceptors.h
+++ sandbox/win/src/interceptors.h
@@ -50,7 +50,9 @@ enum InterceptorId {
   INTERCEPTOR_MAX_ID
 };
 
-typedef void* OriginalFunctions[INTERCEPTOR_MAX_ID];
+struct OriginalFunctions {
+  void* functions[INTERCEPTOR_MAX_ID];
+};
 
 }  // namespace sandbox
 
