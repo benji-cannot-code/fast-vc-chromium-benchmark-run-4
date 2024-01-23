21a794da34604503af587bd0ee8b128fa99f175b
Gina Domergue <gdomergue@google.com>
2024-01-23 19:55:22 +0000

    gd: Add Welcome Dialog
    Adds support to display a welcome dialog for 4 seconds on every game
    window opened.
    
    Bug: b:304817405
    Test: Added unit tests
    Demo: b/304817405#comment9
    Change-Id: I1b9dfb3278f9f5dad1a4e7af0423edbc09b73523
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5106463
    Commit-Queue: Gina Domergue <gdomergue@google.com>
    Reviewed-by: Prameet Shah <phshah@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250993}

---
+++ ash/game_dashboard/game_dashboard_constants.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_GAME_DASHBOARD_GAME_DASHBOARD_CONSTANTS_H_
+#define ASH_GAME_DASHBOARD_GAME_DASHBOARD_CONSTANTS_H_
+
+namespace ash::game_dashboard {
+
+// Toolbar padding from the border of the game window.
+inline constexpr int kToolbarEdgePadding = 10;
+
+// Interior margin padding around the game window for the
+// `GameDashboardWelcomeDialog`.
+inline constexpr int kWelcomeDialogEdgePadding = 8;
+
+// Welcome dialog fixed width.
+inline constexpr int kWelcomeDialogFixedWidth = 360;
+
+}  // namespace ash::game_dashboard
+
+#endif  // ASH_GAME_DASHBOARD_GAME_DASHBOARD_CONSTANTS_H_
