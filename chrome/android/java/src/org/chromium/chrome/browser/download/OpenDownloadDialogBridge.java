745a612efd7a9f3d083e4932db7f5b5c1ce70ff1
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2024-01-23 02:28:41 +0000

    Revert "Adding new string pdf open dialog if there is only one pdf viewer"
    This reverts commit cd111a63073aacf80c5472c2bcf64142a9bcb4fa.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5694913708556288
    
    Sample build with failed test: https://ci.chromium.org/b/8758174391987304161
    Affected test(s):
    [ninja://chrome/android:chrome_public_test_apk/org.chromium.chrome.browser.download.DownloadSettingsTest#testLocationPromptDisabledManagedByPolicy](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Fandroid:chrome_public_test_apk%2Forg.chromium.chrome.browser.download.DownloadSettingsTest%23testLocationPromptDisabledManagedByPolicy?q=VHash%3A1f05a35c44be6f93)
    [ninja://chrome/android:chrome_public_test_apk/org.chromium.chrome.browser.download.DownloadSettingsTest#testLocationPromptEnabledManagedByPolicy](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Fandroid:chrome_public_test_apk%2Forg.chromium.chrome.browser.download.DownloadSettingsTest%23testLocationPromptEnabledManagedByPolicy?q=VHash%3A1f05a35c44be6f93)
    [ninja://chrome/android:chrome_public_test_apk/org.chromium.chrome.browser.download.DownloadSettingsTest#testWithoutDownloadLater](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Fandroid:chrome_public_test_apk%2Forg.chromium.chrome.browser.download.DownloadSettingsTest%23testWithoutDownloadLater?q=VHash%3A1f05a35c44be6f93)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5694913708556288&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F5216586&type=BUG
    
    Original change's description:
    > Adding new string pdf open dialog if there is only one pdf viewer
    >
    > If there is only 1 pdf viewer, this CL will show the app name in dialog
    > title and summary for the settings dialog.
    >
    > Low-Coverage-Reason: HARD_TO_TEST
    > Change-Id: Ibb36917a3165aaf7286f98ee9401cb96c45bcc67
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5216586
    > Commit-Queue: Min Qin <qinmin@chromium.org>
    > Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1250292}
    >
    
    Change-Id: Ib078865759cee2de8a4cd30f21704d0d3c85484f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5226447
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Owners-Override: Jeremie Boulic <jboulic@chromium.org>
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250609}

--- chrome/android/java/src/org/chromium/chrome/browser/download/OpenDownloadDialogBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/OpenDownloadDialogBridge.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.download;
 
 import android.app.Activity;
-import android.content.pm.ResolveInfo;
 
 import org.jni_zero.CalledByNative;
 import org.jni_zero.NativeMethods;
@@ -19,8 +18,6 @@ import org.chromium.chrome.browser.preferences.Pref;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.components.user_prefs.UserPrefs;
 import org.chromium.ui.modaldialog.ModalDialogManagerHolder;
-
-import java.util.List;
 
 /** Glues open download dialogs UI code and handles the communication to download native backend. */
 public class OpenDownloadDialogBridge {
@@ -48,11 +45,6 @@ public class OpenDownloadDialogBridge {
      */
     @CalledByNative
     public void showDialog(Profile profile, String guid) {
-        List<ResolveInfo> result = MimeUtils.getPdfIntentHandlers();
-        if (result.size() == 0) {
-            onCancel(guid);
-            return;
-        }
         DownloadActivityLauncher.getInstance()
                 .getActivityForOpenDialog(
                         (activity) -> {
@@ -60,11 +52,7 @@ public class OpenDownloadDialogBridge {
                                 onCancel(guid);
                                 return;
                             }
-                            showOpenDownloadDialog(
-                                    profile,
-                                    guid,
-                                    activity,
-                                    result.size() > 1 ? null : MimeUtils.getDefaultPdfViewerName());
+                            showOpenDownloadDialog(profile, guid, activity);
                         });
     }
 
@@ -74,16 +62,13 @@ public class OpenDownloadDialogBridge {
      * @param profile Profile of the user.
      * @param guid GUID of the download.
      * @param activity Activity that displays the dialog.
-     * @param appName Name of the app to open the file, null if there are multiple apps.
      */
-    private void showOpenDownloadDialog(
-            Profile profile, String guid, Activity activity, String appName) {
+    private void showOpenDownloadDialog(Profile profile, String guid, Activity activity) {
         new OpenDownloadDialog()
                 .show(
                         activity,
                         ((ModalDialogManagerHolder) activity).getModalDialogManager(),
                         UserPrefs.get(profile).getBoolean(Pref.AUTO_OPEN_PDF_ENABLED),
-                        appName,
                         (result) -> {
                             if (result
                                     == OpenDownloadDialogEvent.OPEN_DOWNLOAD_DIALOG_ALWAYS_OPEN) {
