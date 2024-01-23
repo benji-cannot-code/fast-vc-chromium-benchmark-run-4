ba61e11624377cc7f73675f8b6d2ecfbc7ed4887
Marijn Kruisselbrink <mek@chromium.org>
2024-01-23 23:26:39 +0000

    Add new URL features to EarlyAccess allow list in app shims.
    Will clean up old URL features from the allow list in a separate CLs,
    tests for a code path that would hit these crashes will also be in a
    separate CL.
    
    Bug: 1520386
    Change-Id: I77cb45f8c1938087ff4c0696649f278923f17a08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5230921
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251126}

--- chrome/app_shim/app_shim_controller.mm
+++ chrome/app_shim/app_shim_controller.mm
@@ -273,7 +273,8 @@ void AppShimController::PreInitFeatureState(
       {"AppShimLaunchChromeSilently", "AppShimNotificationAttribution",
        "DcheckIsFatal", "MojoBindingsInlineSLS", "MojoInlineMessagePayloads",
        "MojoIpcz", "MojoTaskPerMessage", "StandardCompliantHostCharacters",
-       "UseAdHocSigningForWebAppShims"});
+       "StandardCompliantNonSpecialSchemeURLParsing",
+       "UseAdHocSigningForWebAppShims", "UseIDNA2008NonTransitional"});
 }
 
 // static
