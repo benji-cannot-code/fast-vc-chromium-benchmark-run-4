f4a7d5e8ed086f4d77cd586334aa7b49c328e2cd
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-01-23 23:52:15 +0000

    Import wpt@097898330ea0759bf785b4e01965e7790bec9bf5
    Using wpt-import in Chromium be128e16c9711e38a7e1cab78056d0ad78709b5f.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I7d0500e4af947ab9681ca24448ae69496c385d40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5229981
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1251134}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/classic/new_window/new_window.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/classic/new_window/new_window.py
@@ -19,7 +19,13 @@ def test_payload(session):
     assert len(handles) == len(original_handles) + 1
     assert value["handle"] in handles
     assert value["handle"] not in original_handles
-    assert value["type"] == "window"
+
+    # On Android applications have a single window only and a new tab will
+    # be opened instead.
+    if session.capabilities["platformName"] == "android":
+        assert value["type"] == "tab"
+    else:
+        assert value["type"] == "window"
 
 
 def test_keeps_current_window_handle(session):
@@ -27,7 +33,6 @@ def test_keeps_current_window_handle(session):
 
     response = new_window(session, type_hint="window")
     value = assert_success(response)
-    assert value["type"] == "window"
 
     assert session.window_handle == original_handle
 
@@ -38,7 +43,6 @@ def test_opens_about_blank_in_new_window(session, inline):
 
     response = new_window(session, type_hint="window")
     value = assert_success(response)
-    assert value["type"] == "window"
 
     assert session.url == url
 
@@ -49,7 +53,6 @@ def test_opens_about_blank_in_new_window(session, inline):
 def test_sets_no_window_name(session):
     response = new_window(session, type_hint="window")
     value = assert_success(response)
-    assert value["type"] == "window"
 
     session.window_handle = value["handle"]
     assert window_name(session) == ""
@@ -58,7 +61,6 @@ def test_sets_no_window_name(session):
 def test_sets_no_opener(session):
     response = new_window(session, type_hint="window")
     value = assert_success(response)
-    assert value["type"] == "window"
 
     session.window_handle = value["handle"]
     assert opener(session) is None
@@ -67,7 +69,6 @@ def test_sets_no_opener(session):
 def test_focus_content(session, inline):
     response = new_window(session, type_hint="window")
     value = assert_success(response)
-    assert value["type"] == "window"
 
     session.window_handle = value["handle"]
 
