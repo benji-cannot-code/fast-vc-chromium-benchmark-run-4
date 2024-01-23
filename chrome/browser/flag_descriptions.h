cf8e0b30cf660510e233a612d1a4433d6b5e612f
Zaina Al-Mashni <zalmashni@google.com>
2024-01-23 18:38:11 +0000

    [Quick Delete] Add feature flag for animation on Android
    Bug: 1412087, b/321954563
    Change-Id: I1144b384e86b0cfd4c8205bec949b865a2b6122b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5224733
    Auto-Submit: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1250948}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1735,6 +1735,9 @@ extern const char kQuickIntensiveWakeUpThrottlingAfterLoadingDescription[];
 extern const char kQuickDeleteForAndroidName[];
 extern const char kQuickDeleteForAndroidDescription[];
 
+extern const char kQuickDeleteAndroidAnimationName[];
+extern const char kQuickDeleteAndroidAnimationDescription[];
+
 extern const char kSettingsAppNotificationSettingsName[];
 extern const char kSettingsAppNotificationSettingsDescription[];
 
--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -236,6 +236,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kOmniboxNoopEditUrlSuggestionClicks,
     &kPartnerCustomizationsUma,
     &kQuickDeleteForAndroid,
+    &kQuickDeleteAndroidAnimation,
     &kReadAloud,
     &kReadAloudInOverflowMenuInCCT,
     &kReadAloudPlayback,
@@ -712,6 +713,10 @@ BASE_FEATURE(kPartnerCustomizationsUma,
 
 BASE_FEATURE(kQuickDeleteForAndroid,
              "QuickDeleteForAndroid",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kQuickDeleteAndroidAnimation,
+             "QuickDeleteAndroidAnimation",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kReadAloud, "ReadAloud", base::FEATURE_DISABLED_BY_DEFAULT);
