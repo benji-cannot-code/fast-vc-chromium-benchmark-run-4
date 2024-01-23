ba094b49caebb5ed3e03a1ba3ce3508796d992d7
David Maunder <davidjm@google.com>
2024-01-23 16:31:57 +0000

    Make Cluster ID unsigned int in mojom files in commerce
    The cluster id is an unsigned int. It is being treated as a signed
    int in the mojom files which is leading to overflow.
    
    Testing by hard coding cluster ID of 16919727309146534557
    Before: https://screenshot.googleplex.com/AJArMuPCoL99QmL
    After: https://screenshot.googleplex.com/89FDEs9tMwHgdYj
    
    Bug: b:321290714
    Change-Id: I6c3480ffe3e7c0051dc0a68193dbfe874f9a671f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5216951
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250863}

--- components/commerce/core/webui/shopping_list_handler_unittest.cc
+++ components/commerce/core/webui/shopping_list_handler_unittest.cc
@@ -214,7 +214,7 @@ TEST_F(ShoppingListHandlerTest, ConvertToMojoTypes_PriceIncrease) {
 }
 
 TEST_F(ShoppingListHandlerTest, TestTrackProductSuccess) {
-  uint64_t cluster_id = 123L;
+  uint64_t cluster_id = 123u;
   const bookmarks::BookmarkNode* product = AddProductBookmark(
       bookmark_model_.get(), u"product 1", GURL("http://example.com/1"),
       cluster_id, false, 1230000, "usd");
@@ -237,7 +237,7 @@ TEST_F(ShoppingListHandlerTest, TestTrackProductSuccess) {
 }
 
 TEST_F(ShoppingListHandlerTest, TestUntrackProductSuccess) {
-  uint64_t cluster_id = 123L;
+  uint64_t cluster_id = 123u;
   const bookmarks::BookmarkNode* product = AddProductBookmark(
       bookmark_model_.get(), u"product 1", GURL("http://example.com/1"),
       cluster_id, true, 1230000, "usd");
@@ -260,7 +260,7 @@ TEST_F(ShoppingListHandlerTest, TestUntrackProductSuccess) {
 }
 
 TEST_F(ShoppingListHandlerTest, TestTrackProductFailure) {
-  uint64_t cluster_id = 123L;
+  uint64_t cluster_id = 123u;
   const bookmarks::BookmarkNode* product = AddProductBookmark(
       bookmark_model_.get(), u"product 1", GURL("http://example.com/1"),
       cluster_id, false, 1230000, "usd");
@@ -287,7 +287,7 @@ TEST_F(ShoppingListHandlerTest, TestTrackProductFailure) {
 }
 
 TEST_F(ShoppingListHandlerTest, TestUntrackProductFailure) {
-  uint64_t cluster_id = 123L;
+  uint64_t cluster_id = 123u;
   const bookmarks::BookmarkNode* product = AddProductBookmark(
       bookmark_model_.get(), u"product 1", GURL("http://example.com/1"),
       cluster_id, true, 1230000, "usd");
@@ -328,7 +328,7 @@ TEST_F(ShoppingListHandlerTest, PageUpdateForPriceTrackChange) {
 }
 
 TEST_F(ShoppingListHandlerTest, TestUnsubscribeCausedByBookmarkDeletion) {
-  int64_t cluster_id = 123L;
+  uint64_t cluster_id = 123u;
   EXPECT_CALL(page_, PriceUntrackedForBookmark(
                          MojoBookmarkInfoWithClusterId(cluster_id)))
       .Times(1);
@@ -402,7 +402,7 @@ TEST_F(ShoppingListHandlerTest,
   info.emplace();
   info->title = "example_title";
   info->product_cluster_title = "example_cluster_title";
-  info->product_cluster_id = absl::optional<uint64_t>(123L);
+  info->product_cluster_id = absl::optional<uint64_t>(123u);
   shopping_service_->SetResponseForGetProductInfoForUrl(info);
 
   handler_->GetProductInfoForCurrentUrl(base::BindOnce(
@@ -410,7 +410,7 @@ TEST_F(ShoppingListHandlerTest,
          shopping_list::mojom::ProductInfoPtr product_info) {
         ASSERT_EQ("example_title", product_info->title);
         ASSERT_EQ("example_cluster_title", product_info->cluster_title);
-        ASSERT_EQ(123L, product_info->cluster_id);
+        ASSERT_EQ(123u, product_info->cluster_id);
         run_loop->Quit();
       },
       &run_loop));
@@ -445,7 +445,7 @@ TEST_F(ShoppingListHandlerTest, TestGetPriceInsightsInfoForCurrentUrl) {
 
   absl::optional<commerce::PriceInsightsInfo> info;
   info.emplace();
-  info->product_cluster_id = 123;
+  info->product_cluster_id = 123u;
   info->currency_code = "usd";
   info->typical_low_price_micros = 1230000;
   info->typical_high_price_micros = 2340000;
@@ -462,7 +462,7 @@ TEST_F(ShoppingListHandlerTest, TestGetPriceInsightsInfoForCurrentUrl) {
   handler_->GetPriceInsightsInfoForCurrentUrl(base::BindOnce(
       [](base::RunLoop* run_loop,
          shopping_list::mojom::PriceInsightsInfoPtr info) {
-        ASSERT_EQ(123, info->cluster_id);
+        ASSERT_EQ(123u, info->cluster_id);
         ASSERT_EQ("$1.23", info->typical_low_price);
         ASSERT_EQ("$2.34", info->typical_high_price);
         ASSERT_EQ("Unlocked, 4GB", info->catalog_attributes);
@@ -526,7 +526,7 @@ TEST_F(ShoppingListHandlerTest,
   delegate_->SetCurrentTabUrl(current_url);
 
   ProductInfo info;
-  info.product_cluster_id = 123L;
+  info.product_cluster_id = 123u;
   info.title = "product";
   AddProductBookmark(bookmark_model_.get(), u"product", current_url,
                      info.product_cluster_id.value(), true, 1230000, "usd");
@@ -582,7 +582,7 @@ TEST_F(ShoppingListHandlerTest, TestTrackPriceForCurrentUrl) {
 
 TEST_F(ShoppingListHandlerTest, TestUntrackPriceForCurrentUrl) {
   ProductInfo info;
-  info.product_cluster_id = 123L;
+  info.product_cluster_id = 123u;
   info.title = "product";
   AddProductBookmark(bookmark_model_.get(), u"product",
                      GURL("http://example.com/1"),
@@ -619,7 +619,7 @@ TEST_F(ShoppingListHandlerTest, TestShowBookmarkEditorForCurrentUrl) {
 }
 
 TEST_F(ShoppingListHandlerTest, TestBookmarkNodeMoved) {
-  uint64_t cluster_id = 12345L;
+  uint64_t cluster_id = 12345u;
 
   const bookmarks::BookmarkNode* node_with_product = AddProductBookmark(
       bookmark_model_.get(), u"title", GURL("https://example.com"), cluster_id);
