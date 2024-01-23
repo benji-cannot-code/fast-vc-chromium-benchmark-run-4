782121550c3799deb7e5e03a56757e6ae3ef3e93
Dmitry Vykochko <vykochko@google.com>
2024-01-23 11:53:13 +0000

    Instantiate AccountManagedStatusFinder in PDM on sync state changes.
    The finder provides the account status, which is required for
    calculating the Autofill sync toggle visibility. This is
    a temporary soution until the toggle becomes available on
    the Sync page.
    Note the solution is not perfect as the status retrieval is
    an async operation, while in PDM the toggle visibility calculation
    is synchronous. The status retrieval is initiated when the sync
    service changes and it is assumed that there will be enough time
    to get the response by the time it is required on the Autofill page.
    
    Bug: 1503058
    Change-Id: I6eb8444a39751b33928348800a7873d7635f4a53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5197851
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1250748}

--- components/autofill/core/browser/personal_data_manager_unittest.cc
+++ components/autofill/core/browser/personal_data_manager_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <list>
 #include <map>
 #include <memory>
+#include <optional>
 #include <string>
 #include <utility>
 #include <vector>
@@ -52,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/autofill_switches.h"
 #include "components/autofill/core/common/form_data.h"
 #include "components/signin/public/base/signin_switches.h"
+#include "components/signin/public/identity_manager/account_managed_status_finder.h"
 #include "components/signin/public/identity_manager/identity_test_environment.h"
 #include "components/sync/base/model_type.h"
 #include "components/sync/base/user_selectable_type.h"
@@ -4116,6 +4118,21 @@ TEST_F(PersonalDataManagerTest, ClearAllCvcs) {
   PersonalDataProfileTaskWaiter(*personal_data_).Wait();
   EXPECT_TRUE(personal_data_->GetServerCreditCards()[0]->cvc().empty());
   EXPECT_TRUE(personal_data_->GetLocalCreditCards()[0]->cvc().empty());
+}
+
+TEST_F(PersonalDataManagerTest, AccountStatusSyncRetrieval) {
+  EXPECT_NE(personal_data_->GetAccountStatusForTesting(), std::nullopt);
+
+  // Login with a non-enterprise account (the status is expected to be available
+  // immediately, with no async calls).
+  AccountInfo account = identity_test_env_.MakeAccountAvailable("ab@gmail.com");
+  sync_service_.SetAccountInfo(account);
+  sync_service_.FireStateChanged();
+  EXPECT_EQ(personal_data_->GetAccountStatusForTesting(),
+            signin::AccountManagedStatusFinder::Outcome::kNonEnterprise);
+
+  personal_data_->SetSyncServiceForTest(nullptr);
+  EXPECT_EQ(personal_data_->GetAccountStatusForTesting(), std::nullopt);
 }
 
 }  // namespace autofill
