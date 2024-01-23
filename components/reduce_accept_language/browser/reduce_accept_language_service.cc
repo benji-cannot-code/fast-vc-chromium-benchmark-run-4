c1dd75d53d1c86a30ca670f0c6fb9aaca8e62252
Victor Tan <victortan@google.com>
2024-01-23 22:35:11 +0000

    Speculative Crash Fix: Explicitly clear container and reset pointer
    Instead of depending on the default destructor, explicitly clear all
    storage and reset the raw pointer.
    
    Change-Id: I6bf4724da6ac16c1975280e31b204bc7d72fab09
    Bug: 1491906
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5228601
    Reviewed-by: Andrew Williams <awillia@chromium.org>
    Commit-Queue: Victor Tan <victortan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251103}

--- components/reduce_accept_language/browser/reduce_accept_language_service.cc
+++ components/reduce_accept_language/browser/reduce_accept_language_service.cc
@@ -59,6 +59,7 @@ ReduceAcceptLanguageService::ReduceAcceptLanguageService(
 ReduceAcceptLanguageService::~ReduceAcceptLanguageService() = default;
 
 void ReduceAcceptLanguageService::Shutdown() {
+  user_accept_languages_.clear();
   pref_accept_language_.Destroy();
 }
 
