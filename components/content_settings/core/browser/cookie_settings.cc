5823df0f12680ba562cd1461499363af6a92db05
Mark Rowe <markrowe@chromium.org>
2024-01-23 01:31:27 +0000

    CookieSettings: Fix dangling reference to PrefService
    Ensure that CookieSettings cannot access PrefService after shutdown.
    
    Bug: 1512677
    Change-Id: I5de54828a0b3678cd95a25d4585aa8f235498f67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5155171
    Auto-Submit: Mark Rowe <markrowe@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Mark Rowe <markrowe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250590}

--- components/content_settings/core/browser/cookie_settings.cc
+++ components/content_settings/core/browser/cookie_settings.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/content_settings/core/browser/cookie_settings.h"
 
+#include <memory>
+
 #include "base/check.h"
 #include "base/containers/contains.h"
 #include "base/feature_list.h"
@@ -28,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/pref_names.h"
 #include "components/permissions/features.h"
 #include "components/pref_registry/pref_registry_syncable.h"
+#include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_service.h"
 #include "components/privacy_sandbox/tracking_protection_prefs.h"
 #include "components/privacy_sandbox/tracking_protection_settings.h"
@@ -61,8 +64,9 @@ CookieSettings::CookieSettings(
     tracking_protection_enabled_for_3pcd_ =
         tracking_protection_settings_->IsTrackingProtection3pcdEnabled();
   }
-  pref_change_registrar_.Init(prefs);
-  pref_change_registrar_.Add(
+  pref_change_registrar_ = std::make_unique<PrefChangeRegistrar>();
+  pref_change_registrar_->Init(prefs);
+  pref_change_registrar_->Add(
       prefs::kCookieControlsMode,
       base::BindRepeating(&CookieSettings::OnCookiePreferencesChanged,
                           base::Unretained(this)));
@@ -305,7 +309,7 @@ void CookieSettings::ShutdownOnUIThread() {
   DCHECK(thread_checker_.CalledOnValidThread());
   tracking_protection_settings_ = nullptr;
   tracking_protection_settings_observation_.Reset();
-  pref_change_registrar_.RemoveAll();
+  pref_change_registrar_.reset();
 }
 
 // Returns whether third-party cookie blocking should be bypassed (i.e. always
@@ -379,6 +383,7 @@ bool CookieSettings::ShouldBlockThirdPartyCookiesInternal() {
 #else
 bool CookieSettings::ShouldBlockThirdPartyCookiesInternal() {
   DCHECK(thread_checker_.CalledOnValidThread());
+  DCHECK(pref_change_registrar_);
 
   if (net::cookie_util::IsForceThirdPartyCookieBlockingEnabled()) {
     return true;
@@ -391,7 +396,7 @@ bool CookieSettings::ShouldBlockThirdPartyCookiesInternal() {
   }
 
   CookieControlsMode mode = static_cast<CookieControlsMode>(
-      pref_change_registrar_.prefs()->GetInteger(prefs::kCookieControlsMode));
+      pref_change_registrar_->prefs()->GetInteger(prefs::kCookieControlsMode));
 
   switch (mode) {
     case CookieControlsMode::kBlockThirdParty:
@@ -446,6 +451,8 @@ void CookieSettings::OnBlockAllThirdPartyCookiesChanged() {
 }
 
 void CookieSettings::OnTrackingProtection3pcdChanged() {
+  DCHECK(pref_change_registrar_);
+
   bool new_tracking_protection_enabled_for_3pcd =
       tracking_protection_settings_->IsTrackingProtection3pcdEnabled();
   {
@@ -464,9 +471,9 @@ void CookieSettings::OnTrackingProtection3pcdChanged() {
   // If the user opted to block all 3PC while in the experiment, preserve that
   // preference if they are offboarded.
   if (!new_tracking_protection_enabled_for_3pcd &&
-      pref_change_registrar_.prefs()->GetBoolean(
+      pref_change_registrar_->prefs()->GetBoolean(
           prefs::kBlockAll3pcToggleEnabled)) {
-    pref_change_registrar_.prefs()->SetInteger(
+    pref_change_registrar_->prefs()->SetInteger(
         prefs::kCookieControlsMode,
         static_cast<int>(CookieControlsMode::kBlockThirdParty));
   }
