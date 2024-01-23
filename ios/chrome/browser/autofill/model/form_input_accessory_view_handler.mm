e19aa14434edf99fc8dfe9992484dd5045de5dde
Alexis Hetu <sugoi@chromium.org>
2024-01-23 12:16:05 +0000

    [iOS] Early exit field detection when using the KeyboardAccessoryUpgrade feature
    When the KeyboardAccessoryUpgrade feature is enabled, the previous
    and next buttons are hidden, so we can early exit and return that
    both buttons are disabled when checking if they should be enabled.
    
    Bug: 1501851
    Change-Id: I0e5f04e4a13606870ee58c71182c9dc1f8f1b0f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5225307
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250760}

--- ios/chrome/browser/autofill/model/form_input_accessory_view_handler.mm
+++ ios/chrome/browser/autofill/model/form_input_accessory_view_handler.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/notreached.h"
 #import "base/strings/sys_string_conversions.h"
 #import "components/autofill/ios/browser/suggestion_controller_java_script_feature.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/web/public/js_messaging/web_frames_manager.h"
 #import "ios/web/public/web_state.h"
@@ -226,7 +227,7 @@ NSArray* FindDescendantToolbarItemsForActionName(
     (void (^)(bool, bool))completionHandler {
   DCHECK(completionHandler);
 
-  if (!_webState) {
+  if (!_webState || IsKeyboardAccessoryUpgradeEnabled()) {
     completionHandler(false, false);
     return;
   }
