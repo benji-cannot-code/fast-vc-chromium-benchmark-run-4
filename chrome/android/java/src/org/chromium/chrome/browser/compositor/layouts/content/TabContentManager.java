7de1d87d2c34cce5c25bc2c48b2a8be2468827bf
Calder Kitagawa <ckitagawa@chromium.org>
2024-01-23 23:42:17 +0000

    [Hub][Tab Switcher] Fix thumbnail requests
    Treat Hub the same as enabling GridTabSwitcherAndroidAnimations and
    don't try to capture new thumbnails while on the Tab Switcher. Just the
    ones that exist or wait for any that are still not written to disk.
    
    Update the TabSwitcherPaneCoordinator to use quickMode = false so that
    the select tab actually tries to request a new thumbnail.
    
    Bug: 1520949
    Change-Id: I9db3b307679689d84a0090425e75efdccb3793fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5229938
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251129}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/content/TabContentManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/content/TabContentManager.java
@@ -37,6 +37,7 @@ import org.chromium.chrome.R;
 import org.chromium.chrome.browser.browser_controls.BrowserControlsStateProvider;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
+import org.chromium.chrome.browser.hub.HubFieldTrial;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tasks.ReturnToChromeUtil;
 import org.chromium.chrome.browser.ui.native_page.FrozenNativePage;
@@ -336,8 +337,11 @@ public class TabContentManager {
         // launch:
         // * GridTabSwitcherAndroidAnimations
         // * Start Surface Refactor
-        if (ChromeFeatureList.sGridTabSwitcherAndroidAnimations.isEnabled()
-                && ReturnToChromeUtil.isStartSurfaceRefactorEnabled(mContext)) {
+        // OR
+        // * Hub
+        if (HubFieldTrial.isHubEnabled()
+                || (ChromeFeatureList.sGridTabSwitcherAndroidAnimations.isEnabled()
+                        && ReturnToChromeUtil.isStartSurfaceRefactorEnabled(mContext))) {
             getTabThumbnailFromDisk(tabId, thumbnailSize, callback);
             return;
         }
