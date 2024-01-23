36039c681eaba6a6b9909189305c74ddd2402982
Kenichi Ishibashi <bashi@chromium.org>
2024-01-23 23:17:27 +0000

    Allow content shell to enable the built-in DNS resolver
    It's useful for local testing and writing browser_tests in //content.
    
    Move the feature flag from //chrome to //net so that
    ShellContentBrowserClient can use it to determine using the built-in
    DNS resolver.
    
    Bug: chromium:1520720
    
    Change-Id: I1f5f5cc1e6010d5f2a982630fb6f76f97f3e92e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5224044
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251122}

--- chrome/browser/net/stub_resolver_config_reader_browsertest.cc
+++ chrome/browser/net/stub_resolver_config_reader_browsertest.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/policy_constants.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/test/browser_test.h"
+#include "net/base/features.h"
 #include "net/dns/public/secure_dns_mode.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -46,7 +47,8 @@ class StubResolverConfigReaderBrowsertest
       public testing::WithParamInterface<bool> {
  public:
   StubResolverConfigReaderBrowsertest() {
-    scoped_feature_list_.InitWithFeatureState(features::kAsyncDns, GetParam());
+    scoped_feature_list_.InitWithFeatureState(net::features::kAsyncDns,
+                                              GetParam());
   }
   ~StubResolverConfigReaderBrowsertest() override = default;
 
