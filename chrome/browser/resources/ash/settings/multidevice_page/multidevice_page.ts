3b200eafe774de5376ec2511a56f5aaa264bacd0
Brando <brandosocarras@google.com>
2024-01-23 00:52:47 +0000

    [Nearby Share] Update NS Settings string
    Updates the settings strings to shown in the Connected devices
    settings page when NS is off or disabled. The new strings provide
    a link for users to learn more about NS in both cases. When
    NS is off, instead of saying 'Off', now the page gives a description
    of NS
    
    The new strings to be used are those used regardless of whether
    os-settings-revamp-wayfinding
    was enabled: before, there were different strings used if enabled.
    
    For this reason, the code conditioning the strings shown on
    the flag state are removed.
    
    Before
    --------------------------------------------
    Revamp Wayfinding: Disabled
    ---------------------------
    NS: Disabled
    https://screenshot.googleplex.com/9h3h4asJdppdQyN.png
    
    NS: On
    https://screenshot.googleplex.com/A7hPyFKheYEXJGC.png
    
    NS: Off
    https://screenshot.googleplex.com/7H5j9DMHtF4ukKR.png
    
    Revamp Wayfinding: Enabled
    --------------------------
    NS: Disabled
    https://screenshot.googleplex.com/AxvUQjCn3gRGbvx.png
    
    NS: On
    https://screenshot.googleplex.com/7RJ839YD9qHYEUB.png
    
    NS: Off
    https://screenshot.googleplex.com/8hFo8wfnLYBo2QP.png
    
    
    After
    --------------------------------------------
    Revamp Wayfinding: Disabled
    ---------------------------
    NS: Disabled
    https://screenshot.googleplex.com/7sBPs2sYXG6cAgJ.png
    
    NS: On
    https://screenshot.googleplex.com/AVMHa5wB5SQQWJS.png
    
    NS: Off
    https://screenshot.googleplex.com/8UhLxpgxP8nbYwg.png
    
    Revamp Wayfinding: Enabled
    --------------------------
    NS: Disabled
    https://screenshot.googleplex.com/GcYddhTztfiUKFT.png
    
    NS: On
    https://screenshot.googleplex.com/BYfthDJBHaJ6Zru.png
    
    NS: Off
    https://screenshot.googleplex.com/32km8jNyMfkAiP2.png
    
            Visibility strings for all visibilities when NS is enabled
            appear as expected; because none were changed, screenshots
            above of NS enabled and on show a device set to 'All contacts'
            visibility.
    
    Tested: all unit tests in browser_tests pass.
    Fixed: b/316389476
    Change-Id: Iaa05067078e1e85d7ff5102a80949d1d6b9f1279
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5208313
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Brando Socarras <brandosocarras@google.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1250570}

--- chrome/browser/resources/ash/settings/multidevice_page/multidevice_page.ts
+++ chrome/browser/resources/ash/settings/multidevice_page/multidevice_page.ts
@@ -568,12 +568,8 @@ export class SettingsMultidevicePageElement extends
     return this.pageContentData.isNearbyShareDisallowedByPolicy;
   }
 
-  private getNearbyShareDescription_(
-      state: boolean, visibility: Visibility|undefined): string|undefined {
-    if (!state) {
-      return this.i18n('nearbyShareDescriptionOff');
-    }
-
+  private getNearbyShareDescription_(visibility: Visibility|undefined): string
+      |undefined {
     if (visibility === undefined) {
       return this.i18n('nearbyShareDescriptionHidden');
     }
@@ -591,11 +587,6 @@ export class SettingsMultidevicePageElement extends
       default:
         assertNotReached();
     }
-  }
-
-  private getOnOffString_(state: boolean, onstr: string, offstr: string):
-      string {
-    return state ? onstr : offstr;
   }
 
   private showNearbyShareToggle_(isOnboardingComplete: boolean): boolean {
