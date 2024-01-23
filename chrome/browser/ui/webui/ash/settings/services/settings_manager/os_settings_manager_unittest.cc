5b084d2c5ceadcb92afd3c56ca7c5db6f1cdbf56
Alexei Svitkine <asvitkine@chromium.org>
2024-01-23 16:14:14 +0000

    Split enums.xml for some CrOS metrics dirs.
    They're created/updated for the following directories:
    chromeos_settings
    cros
    cros_ml
    crostini
    
    Done using the ./tools/metrics/histograms/split_enums.py script.
    
    Bug: 1466920
    Change-Id: I3ba8b172dc77e91ea94167cd839b62df8c985934
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5225686
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Commit-Queue: Jeffrey Young <cowmoo@google.com>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Auto-Submit: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250848}

--- chrome/browser/ui/webui/ash/settings/services/settings_manager/os_settings_manager_unittest.cc
+++ chrome/browser/ui/webui/ash/settings/services/settings_manager/os_settings_manager_unittest.cc
@@ -117,7 +117,7 @@ class OsSettingsManagerTest : public testing::Test {
 
 TEST_F(OsSettingsManagerTest, Initialization) {
   std::optional<base::HistogramEnumEntryMap> sections_enum_entry_map =
-      base::ReadEnumFromEnumsXml("OsSettingsSection");
+      base::ReadEnumFromEnumsXml("OsSettingsSection", "chromeos_settings");
   ASSERT_TRUE(sections_enum_entry_map);
   for (const auto& section : AllSections()) {
     // For each mojom::Section value, there should be an associated
@@ -134,7 +134,7 @@ TEST_F(OsSettingsManagerTest, Initialization) {
   }
 
   std::optional<base::HistogramEnumEntryMap> subpages_enum_entry_map =
-      base::ReadEnumFromEnumsXml("OsSettingsSubpage");
+      base::ReadEnumFromEnumsXml("OsSettingsSubpage", "chromeos_settings");
   ASSERT_TRUE(subpages_enum_entry_map);
   for (const auto& subpage : AllSubpages()) {
     // Each mojom::Subpage should be registered in the hierarchy. Note that
@@ -148,7 +148,7 @@ TEST_F(OsSettingsManagerTest, Initialization) {
   }
 
   std::optional<base::HistogramEnumEntryMap> settings_enum_entry_map =
-      base::ReadEnumFromEnumsXml("OsSetting");
+      base::ReadEnumFromEnumsXml("OsSetting", "chromeos_settings");
   ASSERT_TRUE(settings_enum_entry_map);
   for (const auto& setting : AllSettings()) {
     // Each mojom::Setting should be registered in the hierarchy. Note that
