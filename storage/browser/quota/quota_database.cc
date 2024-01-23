c44e13b9846dc1e9bf5a47952b06fe93757502c0
Evan Stade <estade@chromium.org>
2024-01-23 00:43:48 +0000

    Quota database: handle repeated db failure case
    Fixed: 1507001
    Change-Id: I72bc994f88a55901d3684a9ff62bde1da954c592
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5226822
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250568}

--- storage/browser/quota/quota_database.cc
+++ storage/browser/quota/quota_database.cc
@@ -814,7 +814,7 @@ bool QuotaDatabase::RecoverOrRaze(int error_code) {
 
   db_.reset();
   EnsureOpened();
-  return db_->is_open();
+  return db_ && db_->is_open();
 }
 
 QuotaError QuotaDatabase::CorruptForTesting(
