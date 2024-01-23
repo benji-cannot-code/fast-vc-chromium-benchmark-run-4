68769ffe77395670da7d4a97b4a89e712ab6efa3
Jan Keitel <jkeitel@google.com>
2024-01-23 10:35:14 +0000

    Clean up kAndroidAutofillFormSubmissionCheckById.
    The feature was launched and default enabled on M121.
    
    Bug: b/297228856
    Change-Id: I4e5d39dadd4a3c94e5a5e76d9057dad52dc9b626
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5224021
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1250723}

--- components/android_autofill/browser/android_autofill_features.h
+++ components/android_autofill/browser/android_autofill_features.h
@@ -12,8 +12,6 @@ namespace autofill::features {
 
 BASE_DECLARE_FEATURE(kAndroidAutofillBottomSheetWorkaround);
 
-BASE_DECLARE_FEATURE(kAndroidAutofillFormSubmissionCheckById);
-
 BASE_DECLARE_FEATURE(kAndroidAutofillPrefillRequestsForLoginForms);
 
 BASE_DECLARE_FEATURE(kAndroidAutofillUsePwmPredictionsForOverrides);
