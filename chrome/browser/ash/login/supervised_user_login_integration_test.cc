400fbd9cc1c9ef1b97d83ffaf22152a84934d4bb
Courtney Wong <courtneywong@chromium.org>
2024-01-23 02:11:32 +0000

    crosier: Re-enable Supervised user login tests
    http://crrev/c/5221194 extended the timeout of the crosier tests.
    
    Bug: b:318616623
    Change-Id: I672e4d4eec6d21404415c3a896b3bb64ade74a06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5227040
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Reviewed-by: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250601}

--- chrome/browser/ash/login/supervised_user_login_integration_test.cc
+++ chrome/browser/ash/login/supervised_user_login_integration_test.cc
@@ -86,9 +86,7 @@ class SupervisedUserLoginIntegrationTest : public AshIntegrationTest {
   SupervisedUserLoginDelegate delegate_;
 };
 
-// TODO(b/318616623): Re-enable when flaky timeout is resolved.
-IN_PROC_BROWSER_TEST_F(SupervisedUserLoginIntegrationTest,
-                       DISABLED_TestUnicornLogin) {
+IN_PROC_BROWSER_TEST_F(SupervisedUserLoginIntegrationTest, TestUnicornLogin) {
   SetupContextWidget();
 
   login_mixin().Login();
@@ -98,9 +96,7 @@ IN_PROC_BROWSER_TEST_F(SupervisedUserLoginIntegrationTest,
   VerifyPolicies();
 }
 
-// TODO(b/318616623): Re-enable when flaky timeout is resolved.
-IN_PROC_BROWSER_TEST_F(SupervisedUserLoginIntegrationTest,
-                       DISABLED_TestGellerLogin) {
+IN_PROC_BROWSER_TEST_F(SupervisedUserLoginIntegrationTest, TestGellerLogin) {
   SetupContextWidget();
 
   delegate_.set_user_type(
@@ -112,9 +108,7 @@ IN_PROC_BROWSER_TEST_F(SupervisedUserLoginIntegrationTest,
   VerifyPolicies();
 }
 
-// TODO(b/318616623): Re-enable when flaky timeout is resolved.
-IN_PROC_BROWSER_TEST_F(SupervisedUserLoginIntegrationTest,
-                       DISABLED_TestGriffinLogin) {
+IN_PROC_BROWSER_TEST_F(SupervisedUserLoginIntegrationTest, TestGriffinLogin) {
   SetupContextWidget();
 
   delegate_.set_user_type(
