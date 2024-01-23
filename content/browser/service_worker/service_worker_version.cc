803f8e96ef837e5f0fa661044f6e3f95d3640464
Minoru Chikamune <chikamune@chromium.org>
2024-01-23 08:11:21 +0000

    Warm-up service worker on idle timeout
    Before this CL, whenever the service worker stops, the service worker
    automatically warms up unconditionally. This CL changes this
    behavior. After this CL, the service worker will warm up only when the
    worker stops with idle timeout.
    
    Bug: 1431792
    Change-Id: I94420766d3f9748775c942bb20694375b7e18785
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5225060
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250682}

--- content/browser/service_worker/service_worker_version.cc
+++ content/browser/service_worker/service_worker_version.cc
@@ -663,6 +663,13 @@ bool ServiceWorkerVersion::OnRequestTermination() {
     }
   }
 
+  will_warm_up_on_stopped_ =
+      will_be_terminated &&
+      base::FeatureList::IsEnabled(
+          blink::features::kSpeculativeServiceWorkerWarmUp) &&
+      blink::features::kSpeculativeServiceWorkerWarmUpOnIdleTimeout.Get() &&
+      scope_.SchemeIsHTTPOrHTTPS();
+
   if (will_be_terminated) {
     embedded_worker_->Stop();
   } else {
@@ -2036,8 +2043,7 @@ void ServiceWorkerVersion::OpenWindow(
 }
 
 bool ServiceWorkerVersion::HasWorkInBrowser() const {
-  return !inflight_requests_.IsEmpty() || !start_callbacks_.empty() ||
-         !warm_up_callbacks_.empty();
+  return !inflight_requests_.IsEmpty() || !start_callbacks_.empty();
 }
 
 void ServiceWorkerVersion::OnSimpleEventFinished(
@@ -2814,6 +2820,11 @@ void ServiceWorkerVersion::OnStoppedInternal(
   } else if (!HasWorkInBrowser()) {
     OnNoWorkInBrowser();
   }
+
+  if (!should_restart && will_warm_up_on_stopped_ && context_) {
+    context_->wrapper()->WarmUpServiceWorker(scope_, key_, base::DoNothing());
+  }
+  will_warm_up_on_stopped_ = false;
 }
 
 void ServiceWorkerVersion::FinishStartWorker(
@@ -2824,6 +2835,12 @@ void ServiceWorkerVersion::FinishStartWorker(
   for (auto& callback : callbacks)
     std::move(callback).Run(status);
   is_running_start_callbacks_ = false;
+
+  std::vector<StatusCallback> warm_up_callbacks;
+  warm_up_callbacks.swap(warm_up_callbacks_);
+  for (auto& callback : warm_up_callbacks) {
+    std::move(callback).Run(status);
+  }
 }
 
 void ServiceWorkerVersion::CleanUpExternalRequest(
