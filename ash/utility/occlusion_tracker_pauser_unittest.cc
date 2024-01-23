7660d6d64c585f42b2297c18a6efc480b02d6e6c
Mitsuru Oshima <oshima@chromium.org>
2024-01-23 00:33:07 +0000

    Reland "Do not update the surface if the layer already have the same surface id"
    This is a reland of crrev.com/c//5043088.
    Please see the comment for the fix.
    
    This CL also contain following fix/changes.
    * Exo was updating the opacity before resource is updated.
      See UpdateHostLayerOpacity change
    * Factored BeginMainFrameWaiter into separate utility class.
    
    Bug: 1503543
    Test: ShellWithClientTest.BufferCommitNoNeedsCommit
    Change-Id: Ib0f9df69b1ae7aa893621d74a452761085ba4b4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5203136
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250560}

--- ash/utility/occlusion_tracker_pauser_unittest.cc
+++ ash/utility/occlusion_tracker_pauser_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window_occlusion_tracker.h"
 #include "ui/aura/window_tree_host.h"
 #include "ui/compositor/compositor.h"
+#include "ui/compositor/test/begin_main_frame_waiter.h"
 
 namespace ash {
 namespace {
@@ -27,41 +28,6 @@ class TestObserver final : public ui::CompositorAnimationObserver {
   // ui::CompositorAnimationObserver:
   void OnAnimationStep(base::TimeTicks timestamp) override {}
   void OnCompositingShuttingDown(ui::Compositor* compositor) override {}
-};
-
-class BeginMainFrameWaiter : public ui::CompositorObserver {
- public:
-  explicit BeginMainFrameWaiter(ui::Compositor* compositor)
-      : compositor_(compositor) {
-    compositor->AddObserver(this);
-  }
-
-  ~BeginMainFrameWaiter() override { compositor_->RemoveObserver(this); }
-
-  // ui::CompositorObserver
-  void OnDidBeginMainFrame(ui::Compositor* compositor) override {
-    DCHECK_EQ(compositor_, compositor);
-    done_ = true;
-    if (run_loop_) {
-      run_loop_->Quit();
-    }
-  }
-
-  void Wait() {
-    if (done_) {
-      return;
-    }
-
-    run_loop_ = std::make_unique<base::RunLoop>(
-        base::RunLoop::Type::kNestableTasksAllowed);
-    run_loop_->Run();
-    run_loop_.reset();
-  }
-
- private:
-  raw_ptr<ui::Compositor> compositor_;
-  bool done_ = false;
-  std::unique_ptr<base::RunLoop> run_loop_;
 };
 
 }  // namespace
@@ -92,7 +58,7 @@ TEST_F(OcclusionTrackerPauserTest, Basic) {
 
   // Wait for BeginFrame since compositor animation notifications happen
   // on BeginFrame.
-  BeginMainFrameWaiter(compositor).Wait();
+  ui::BeginMainFrameWaiter(compositor).Wait();
   EXPECT_FALSE(tracker->IsPaused());
 
   compositor->AddAnimationObserver(&observer1);
@@ -108,7 +74,7 @@ TEST_F(OcclusionTrackerPauserTest, Basic) {
 
   // Wait for BeginFrame since compositor animation notifications happen
   // on BeginFrame.
-  BeginMainFrameWaiter(compositor).Wait();
+  ui::BeginMainFrameWaiter(compositor).Wait();
   EXPECT_FALSE(tracker->IsPaused());
 }
 
@@ -132,7 +98,7 @@ TEST_F(OcclusionTrackerPauserTest, MultiDisplay) {
 
   // Wait for BeginFrame since compositor animation notifications happen
   // on BeginFrame.
-  BeginMainFrameWaiter(compositor1).Wait();
+  ui::BeginMainFrameWaiter(compositor1).Wait();
 
   // Tracker should still be paused.
   EXPECT_TRUE(tracker->IsPaused());
@@ -141,7 +107,7 @@ TEST_F(OcclusionTrackerPauserTest, MultiDisplay) {
 
   // Wait for BeginFrame since compositor animation notifications happen
   // on BeginFrame.
-  BeginMainFrameWaiter(compositor2).Wait();
+  ui::BeginMainFrameWaiter(compositor2).Wait();
   EXPECT_FALSE(tracker->IsPaused());
 
   // Disconnect display.
@@ -154,7 +120,7 @@ TEST_F(OcclusionTrackerPauserTest, MultiDisplay) {
 
   // Wait for BeginFrame since compositor animation notifications happen
   // on BeginFrame.
-  BeginMainFrameWaiter(compositor1).Wait();
+  ui::BeginMainFrameWaiter(compositor1).Wait();
   // Tracker should still be paused.
   EXPECT_TRUE(tracker->IsPaused());
 
@@ -163,7 +129,7 @@ TEST_F(OcclusionTrackerPauserTest, MultiDisplay) {
 
   // Wait for BeginFrame since compositor animation notifications happen
   // on BeginFrame.
-  BeginMainFrameWaiter(compositor1).Wait();
+  ui::BeginMainFrameWaiter(compositor1).Wait();
   EXPECT_TRUE(tracker->IsPaused());
 
   UpdateDisplay("800x1000");
@@ -171,7 +137,7 @@ TEST_F(OcclusionTrackerPauserTest, MultiDisplay) {
 
   // Wait for BeginFrame since compositor animation notifications happen
   // on BeginFrame.
-  BeginMainFrameWaiter(compositor1).Wait();
+  ui::BeginMainFrameWaiter(compositor1).Wait();
   EXPECT_FALSE(tracker->IsPaused());
 }
 
@@ -191,7 +157,7 @@ TEST_F(OcclusionTrackerPauserTest, MultiDisplaySingleAnimatingCompositor) {
 
   // Wait for BeginFrame since compositor animation notifications happen
   // on BeginFrame.
-  BeginMainFrameWaiter(compositor1).Wait();
+  ui::BeginMainFrameWaiter(compositor1).Wait();
   EXPECT_TRUE(tracker->IsPaused());
 
   compositor1->RemoveAnimationObserver(&observer1);
@@ -199,7 +165,7 @@ TEST_F(OcclusionTrackerPauserTest, MultiDisplaySingleAnimatingCompositor) {
 
   // Wait for BeginFrame since compositor animation notifications happen
   // on BeginFrame.
-  BeginMainFrameWaiter(compositor1).Wait();
+  ui::BeginMainFrameWaiter(compositor1).Wait();
 
   // There are no more animations, so expect the tracker to be unpaused.
   EXPECT_FALSE(tracker->IsPaused());
@@ -225,8 +191,8 @@ TEST_F(OcclusionTrackerPauserTest, Timeout) {
 
   // Wait for BeginFrame since compositor animation notifications happen
   // on BeginFrame.
-  BeginMainFrameWaiter waiter1(compositor1);
-  BeginMainFrameWaiter waiter2(compositor2);
+  ui::BeginMainFrameWaiter waiter1(compositor1);
+  ui::BeginMainFrameWaiter waiter2(compositor2);
   waiter1.Wait();
   waiter2.Wait();
 
