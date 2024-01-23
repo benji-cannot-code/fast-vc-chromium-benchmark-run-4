6515fd6cdefd011e14716bda43b6ebd44ee7eaaa
Daniel Cheng <dcheng@chromium.org>
2024-01-23 23:03:23 +0000

    Fix some random IWYU errors in tests.
    This makes it easier to git bisect googletest.
    
    Bug: 1409870
    Change-Id: Ia99b727693b0fd4201c95a54f491641d3f0ccdfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5216058
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251113}

--- components/breadcrumbs/core/application_breadcrumbs_not_user_action_unittest.cc
+++ components/breadcrumbs/core/application_breadcrumbs_not_user_action_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/breadcrumbs/core/application_breadcrumbs_not_user_action.inc"
 
+#include <algorithm>
 #include <string>
 #include <vector>
 
--- ui/base/clipboard/file_info_unittest.cc
+++ ui/base/clipboard/file_info_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 #define FPL(x) FILE_PATH_LITERAL(x)
 
