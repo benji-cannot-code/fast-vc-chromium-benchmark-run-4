7385bff96263e6646107a07628c82d922b5bb1c9
Lijin Shen <lazzzis@google.com>
2024-01-23 00:29:48 +0000

    [E2E] Draw content under notch only when fullscreen is enabled.
    Observe webcontents and update layout every time it enters/exits
    fullscreen.
    
    Demo: https://bugs.chromium.org/p/chromium/issues/detail?id=1514480#c9
    
    Bug: 1519062, 1511782, 1514480
    Change-Id: I22aba5d3c7ffbb769e13e478ac2fc36e88c75f46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5214847
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250559}

--- chrome/android/junit/src/org/chromium/chrome/browser/display_cutout/DisplayCutoutControllerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/display_cutout/DisplayCutoutControllerTest.java
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.display_cutout;
 
+import static org.mockito.Mockito.description;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -38,6 +40,7 @@ import org.chromium.components.browser_ui.display_cutout.DisplayCutoutController
 import org.chromium.components.browser_ui.widget.InsetObserver;
 import org.chromium.components.browser_ui.widget.InsetObserverSupplier;
 import org.chromium.content_public.browser.WebContents;
+import org.chromium.content_public.browser.WebContentsObserver;
 import org.chromium.ui.base.WindowAndroid;
 
 import java.lang.ref.WeakReference;
@@ -96,6 +99,27 @@ public class DisplayCutoutControllerTest {
 
         mDisplayCutoutTabHelper.setViewportFit(ViewportFit.COVER);
         verify(mController).maybeUpdateLayout();
+    }
+
+    @Test
+    @SmallTest
+    public void testViewportFitUpdateOnFullscreen() {
+        // Re-adding observers; otherwise, the internal observers are bound to un-mocked
+        // mController.
+        mController.destroy();
+        mController.maybeAddObservers();
+
+        ArgumentCaptor<WebContentsObserver> captor =
+                ArgumentCaptor.forClass(WebContentsObserver.class);
+        verify(mWebContents, times(2)).addObserver(captor.capture());
+        WebContentsObserver webContentsObserver = captor.getValue();
+        webContentsObserver.didToggleFullscreenModeForTab(true, false);
+        verify(mController, description("Should update layout when entering fullscreen"))
+                .maybeUpdateLayout();
+
+        webContentsObserver.didToggleFullscreenModeForTab(false, false);
+        verify(mController, times(2).description("Should update layout when exiting fullscreen"))
+                .maybeUpdateLayout();
     }
 
     @Test
