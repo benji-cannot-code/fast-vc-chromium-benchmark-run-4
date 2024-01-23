88afd3a11a13984155e4b43f235a9e7caaeb2d7c
Austin Sullivan <asully@chromium.org>
2024-01-23 18:45:00 +0000

    FSA: Add WPTs testing FileSystemObserver creation
    Bug: 1019297
    Change-Id: Ie735d376b760aa78859b1e8c5a1fed1dee90f959
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5200909
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Daseul Lee <dslee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250956}

---
+++ third_party/blink/web_tests/external/wpt/fs/FileSystemObserver.https.tentative.window.js
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/service-workers/service-worker/resources/test-helpers.sub.js
+// META: script=resources/messaging-helpers.js
+// META: script=script-tests/FileSystemObserver.js
