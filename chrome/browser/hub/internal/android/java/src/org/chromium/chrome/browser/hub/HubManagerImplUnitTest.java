d2aec472877ce5ad6325ae55a87f746bebc22b29
Sky Malice <skym@chromium.org>
2024-01-23 17:36:31 +0000

    Adjust hub SnackManager to be above FAB.
    Use the root hub container instead of the root pane view. This allows
    the snackbar/undo message banner to always drop on top of hub controlled
    content such as the floating action button.
    
    Bug: b/321867253
    Change-Id: I82bd5148fffa95acd352059bce996d533d5656fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5227620
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250906}

--- chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubManagerImplUnitTest.java
+++ chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubManagerImplUnitTest.java
@@ -16,7 +16,6 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import android.app.Activity;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
@@ -170,12 +169,12 @@ public class HubManagerImplUnitTest {
         assertNotNull(coordinator);
         verify(mBackPressManager).addHandler(eq(coordinator), eq(BackPressHandler.Type.HUB));
         verify(mTabSwitcherPane).setPaneHubController(coordinator);
-        verify(mSnackbarManager).setParentView(mTabSwitcherPaneView);
         verify(mMenuOrKeyboardActionController)
                 .registerMenuOrKeyboardActionHandler(mTabSwitcherMenuOrKeyboardActionHandler);
 
-        View containerView = hubController.getContainerView();
+        FrameLayout containerView = hubController.getContainerView();
         assertNotNull(containerView);
+        verify(mSnackbarManager).setParentView(containerView);
 
         // Attach the container to the parent view.
         mRootView.addView(containerView);
@@ -187,7 +186,7 @@ public class HubManagerImplUnitTest {
         verify(mMenuOrKeyboardActionController)
                 .unregisterMenuOrKeyboardActionHandler(mTabSwitcherMenuOrKeyboardActionHandler);
         verify(mIncognitoTabSwitcherPane).setPaneHubController(coordinator);
-        verify(mSnackbarManager).setParentView(mIncognitoTabSwitcherPaneView);
+        verify(mSnackbarManager, times(2)).setParentView(containerView);
         verify(mMenuOrKeyboardActionController)
                 .registerMenuOrKeyboardActionHandler(
                         mIncognitoTabSwitcherMenuOrKeyboardActionHandler);
