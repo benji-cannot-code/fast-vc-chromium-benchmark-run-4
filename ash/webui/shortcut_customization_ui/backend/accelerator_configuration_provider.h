15aa3ad988eff62c00a69e1528a76c4c365f216a
Longbo Wei <longbowei@google.com>
2024-01-23 17:34:15 +0000

    shortcuts: Allow re-adding default accelerator
    User should be allowed to re-add default accelerator even if it is
    not "modifier + key" or a "function key".
    
    video: http://go/scrcast/NTIwMDY5NjY3OTg1ODE3Nnw5YjI4M2U4MS0wOQ
    
    Test: ash_webui_unittests
    Bug: b/216049298
    Change-Id: If515172965926aadf3ef3ddb2f0f4b3e53b54160
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5194926
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Longbo Wei <longbowei@google.com>
    Cr-Commit-Position: refs/heads/main@{#1250905}

--- ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.h
+++ ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.h
@@ -181,6 +181,9 @@ class AcceleratorConfigurationProvider
     return non_configurable_actions_mapping_;
   }
 
+  std::vector<ui::Accelerator> GetDefaultAcceleratorsForId(
+      uint32_t action_id) const;
+
   mojom::AcceleratorInfoPtr CreateTextAcceleratorInfo(
       const NonConfigurableAcceleratorDetails& details) const;
 
