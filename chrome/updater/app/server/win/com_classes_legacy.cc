28fc196809f3c049b6c1d5cd09e853f3a8f8ff3b
S. Ganesh <ganesh@chromium.org>
2024-01-24 22:17:22 +0000

    updater: Fix legacy on-demand `ap` and `brand` parameters to createApp
    Legacy on-demand now correctly saves the `ap` and brand code parameters
    to `IAppBundleWeb::createApp`.
    
    Fixed: 1521449
    Change-Id: I525fcca3bf761953bbe28f17101f1400309af7af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5235257
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251740}

--- chrome/updater/app/server/win/com_classes_legacy.cc
+++ chrome/updater/app/server/win/com_classes_legacy.cc
@@ -349,7 +349,8 @@ class AppWebImpl : public IDispatchImpl<IAppWeb> {
   HRESULT RuntimeClassInitialize(
       const bool is_install,
       const std::wstring& app_id,
-      const std::wstring& language,
+      const std::wstring& brand_code,
+      const std::wstring& ap,
       UpdateService::PolicySameVersionUpdate policy_same_version_update) {
     if (is_install && FAILED(IsCOMCallerAllowed())) {
       VLOG(1) << __func__ << ": admin rights required for installs";
@@ -357,7 +358,6 @@ class AppWebImpl : public IDispatchImpl<IAppWeb> {
     }
 
     app_id_ = base::WideToASCII(app_id);
-    language_ = language;
     policy_same_version_update_ = policy_same_version_update;
 
     // Holds the result of the IPC to register an app.
@@ -373,7 +373,8 @@ class AppWebImpl : public IDispatchImpl<IAppWeb> {
 
     auto result = base::MakeRefCounted<RegisterAppResult>();
     AppServerWin::PostRpcTask(base::BindOnce(
-        [](AppWebImplPtr obj, scoped_refptr<RegisterAppResult> result) {
+        [](AppWebImplPtr obj, const std::wstring& brand_code,
+           const std::wstring& ap, scoped_refptr<RegisterAppResult> result) {
           const base::ScopedClosureRunner signal_event(base::BindOnce(
               [](scoped_refptr<RegisterAppResult> result) {
                 result->completion_event.Signal();
@@ -392,9 +393,11 @@ class AppWebImpl : public IDispatchImpl<IAppWeb> {
           RegistrationRequest request;
           request.app_id = obj->app_id_;
           request.version = base::Version(kNullVersion);
+          request.brand_code = base::WideToASCII(brand_code);
+          request.ap = base::WideToASCII(ap);
           persisted_data->RegisterApp(request);
         },
-        AppWebImplPtr(this), result));
+        AppWebImplPtr(this), brand_code, ap, result));
 
     if (!result->completion_event.TimedWait(base::Seconds(60))) {
       return E_FAIL;
@@ -724,7 +727,6 @@ class AppWebImpl : public IDispatchImpl<IAppWeb> {
 
   bool new_install_ = false;
   std::string app_id_;
-  std::wstring language_;
   UpdateService::PolicySameVersionUpdate policy_same_version_update_ =
       UpdateService::PolicySameVersionUpdate::kNotAllowed;
 
@@ -749,7 +751,7 @@ class AppBundleWebImpl : public IDispatchImpl<IAppBundleWeb> {
   // Overrides for IAppBundleWeb.
   IFACEMETHODIMP createApp(BSTR app_id,
                            BSTR brand_code,
-                           BSTR language,
+                           BSTR /* language */,
                            BSTR ap) override {
     base::AutoLock lock{lock_};
 
@@ -758,7 +760,7 @@ class AppBundleWebImpl : public IDispatchImpl<IAppBundleWeb> {
     }
 
     return MakeAndInitializeComObject<AppWebImpl>(
-        app_web_, /*is_install=*/true, app_id, language,
+        app_web_, /*is_install=*/true, app_id, brand_code, ap,
         UpdateService::PolicySameVersionUpdate::kAllowed);
   }
 
@@ -770,7 +772,7 @@ class AppBundleWebImpl : public IDispatchImpl<IAppBundleWeb> {
     }
 
     return MakeAndInitializeComObject<AppWebImpl>(
-        app_web_, /*is_install=*/false, app_id, GetPreferredLanguage(),
+        app_web_, /*is_install=*/false, app_id, L"", L"",
         UpdateService::PolicySameVersionUpdate::kNotAllowed);
   }
 
