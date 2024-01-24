7ac8d6bd953e29f603d2661f519604ba474bd086
Robbie McElrath <rmcelrath@chromium.org>
2024-01-24 19:54:02 +0000

    iwa: Don't strip query parameter in proxy URL
    Some dev servers use query parameters, and break when they're stripped.
    
    Bug: b/321695826
    Change-Id: I20b70e10898bb2e1704a50502fcd9a5aca8e889d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5225850
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Zelin Liu <zelin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251614}

--- chrome/browser/web_applications/isolated_web_apps/isolated_web_app_url_loader_factory_unittest.cc
+++ chrome/browser/web_applications/isolated_web_apps/isolated_web_app_url_loader_factory_unittest.cc
@@ -489,7 +489,7 @@ TEST_F(IsolatedWebAppURLLoaderFactoryTest,
   EXPECT_THAT(status, IsNetError(net::OK));
 }
 
-TEST_F(IsolatedWebAppURLLoaderFactoryTest, ProxyUrlDoesNotHaveUrlQuery) {
+TEST_F(IsolatedWebAppURLLoaderFactoryTest, ProxyUrlInheritsQuery) {
   RegisterWebApp(CreateIsolatedWebApp(
       kDevAppStartUrl,
       WebApp::IsolationData{DevModeProxy{.proxy_url = url::Origin::Create(
@@ -499,11 +499,12 @@ TEST_F(IsolatedWebAppURLLoaderFactoryTest, ProxyUrlDoesNotHaveUrlQuery) {
   CreateFactory();
 
   auto request = std::make_unique<network::ResourceRequest>();
-  request->url = GURL("isolated-app://" + kDevWebBundleId +
-                      "?testingQueryToRemove=testValue");
+  request->url =
+      GURL("isolated-app://" + kDevWebBundleId + "?testingQuery=testValue");
   CreateLoaderAndRun(std::move(request));
 
-  EXPECT_THAT(url_handler().intercepted_url(), Eq(GURL("http://example.com/")));
+  EXPECT_THAT(url_handler().intercepted_url(),
+              Eq(GURL("http://example.com/?testingQuery=testValue")));
 }
 
 TEST_F(IsolatedWebAppURLLoaderFactoryTest, ProxyUrlDoesNotHaveUrlFragment) {
