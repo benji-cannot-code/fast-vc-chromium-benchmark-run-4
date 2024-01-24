3319e597d830f7723eb09dbe1b0bb533a1889498
Joemer Ramos <joemerramos@chromium.org>
2024-01-24 15:50:28 +0000

    [iOS] Cleanup iOS 15 Guard for DownloadNativeTask Related Code
    Bug: 1418068
    Change-Id: Ic274a38670b7208e5c3b038ae0af2b40b7a5bbdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5230241
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251417}

--- ios/web/test/fakes/fake_native_task_bridge.mm
+++ ios/web/test/fakes/fake_native_task_bridge.mm
@@ -25,8 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                             (id<DownloadNativeTaskBridgeDelegate>)delegate {
   if (self = [super initWithDownload:download delegate:delegate]) {
     _calledStartDownloadBlock = NO;
-    if (@available(iOS 15, *))
-      [self downloadInitialized];
+    [self downloadInitialized];
   }
   return self;
 }
