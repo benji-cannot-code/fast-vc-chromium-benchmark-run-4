17a631dd4edc16b21704de1ff10329f385dedf8c
Nihar Majmudar <niharm@google.com>
2024-01-24 17:56:55 +0000

    [omnibox][webui] Fix focus indicator.
    Updates focus indicator state so it disappears when other elements are
    focused in a match.
    
    Not fixed:
    1) Focus still doesn't work for headers.
    
    Before:
    https://screencast.googleplex.com/cast/NTExNjUyNjg2MzU4MTE4NHxkNTYyZDAzYS1mZQ
    After:
    https://screencast.googleplex.com/cast/NTkyNjc0ODczNzc2NTM3NnxiNzI3YjhhNy1hOA
    
    Bug: 1520641
    Change-Id: I50c8adcfcbc11fd7cf6270f4c17845ae9945de13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5225029
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251510}

--- ui/webui/resources/cr_components/omnibox/realbox_match.ts
+++ ui/webui/resources/cr_components/omnibox/realbox_match.ts
@@ -503,6 +503,9 @@ export class RealboxMatchElement extends PolymerElement {
   }
 
   updateSelection(selection: OmniboxPopupSelection) {
+    this.$['focus-indicator'].classList.toggle(
+        'selected-within', selection.state !== SelectionLineState.kNormal);
+
     this.$.remove.classList.toggle(
         'selected',
         selection.state === SelectionLineState.kFocusedButtonRemoveSuggestion);
