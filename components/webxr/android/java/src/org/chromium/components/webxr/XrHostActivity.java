c88818b6415c66323d582734d7780ac7dd6326b8
Alexander Cooper <alcooper@chromium.org>
2024-01-24 21:56:23 +0000

    Fix OpenXR for Official Builds
    It seems as though Java asserts are stripped out on official builds.
    This change thus moves a call that was happening inside of an assert
    (which had the side effect of continuing the requestSession call) to
    outside of it, while still asserting on the result returned from it.
    This unblocks launching an OpenXR session on official builds on
    Android.
    
    Fixed: 1521427
    Change-Id: Ic53170810e3ac81990101654704646f19c4e7c9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5235006
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251725}

--- components/webxr/android/java/src/org/chromium/components/webxr/XrHostActivity.java
+++ components/webxr/android/java/src/org/chromium/components/webxr/XrHostActivity.java
@@ -51,7 +51,8 @@ public class XrHostActivity extends Activity {
     public void onAttachedToWindow() {
         super.onAttachedToWindow();
 
-        assert (XrSessionCoordinator.onXrHostActivityReady(this));
+        boolean result = XrSessionCoordinator.onXrHostActivityReady(this);
+        assert (result);
     }
 
     @Override
