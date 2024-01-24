07cb909e7a4c2dcbc666d899c7ade5e2b84f44c1
Jose Dapena Paz <jdapena@igalia.com>
2024-01-24 06:46:33 +0000

    IWYU: missing include for std::optional usage in race_network_request_write_buffer_manager.h
    Bug: 957519
    Change-Id: Id3a6c3cc3c1273208bde43b70a2bd298695f7cc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5223947
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1251264}

--- content/common/service_worker/race_network_request_write_buffer_manager.h
+++ content/common/service_worker/race_network_request_write_buffer_manager.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_COMMON_SERVICE_WORKER_RACE_NETWORK_REQUEST_WRITE_BUFFER_MANAGER_H_
 #define CONTENT_COMMON_SERVICE_WORKER_RACE_NETWORK_REQUEST_WRITE_BUFFER_MANAGER_H_
 
+#include <optional>
+
 #include "base/containers/span.h"
 #include "content/common/content_export.h"
 #include "mojo/public/cpp/system/data_pipe.h"
