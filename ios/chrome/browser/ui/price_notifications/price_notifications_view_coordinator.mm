0eddec95b19e19d10aae54db82de0438fe974e28
Joemer Ramos <joemerramos@chromium.org>
2024-01-24 15:46:54 +0000

    [iOS] Cleanup iOS 15 Guard in PriceNotificationsViewController
    Bug: 1418068
    Change-Id: If4fd79325a6fdd72b2b415199315753ab364838a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5230178
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251414}

--- ios/chrome/browser/ui/price_notifications/price_notifications_view_coordinator.mm
+++ ios/chrome/browser/ui/price_notifications/price_notifications_view_coordinator.mm
@@ -122,22 +122,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   self.navigationController.navigationBar.prefersLargeTitles = NO;
 
-  if (@available(iOS 15, *)) {
-    UISheetPresentationController* sheetPresentationController =
-        self.navigationController.sheetPresentationController;
-    if (sheetPresentationController) {
-      sheetPresentationController.prefersEdgeAttachedInCompactHeight = YES;
-      sheetPresentationController
-          .widthFollowsPreferredContentSizeWhenEdgeAttached = YES;
+  UISheetPresentationController* sheetPresentationController =
+      self.navigationController.sheetPresentationController;
+  if (sheetPresentationController) {
+    sheetPresentationController.prefersEdgeAttachedInCompactHeight = YES;
+    sheetPresentationController
+        .widthFollowsPreferredContentSizeWhenEdgeAttached = YES;
 
-      sheetPresentationController.detents =
-          ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_TABLET
-              ? @[ [UISheetPresentationControllerDetent largeDetent] ]
-              : @[
-                  [UISheetPresentationControllerDetent mediumDetent],
-                  [UISheetPresentationControllerDetent largeDetent]
-                ];
-    }
+    sheetPresentationController.detents =
+        ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_TABLET
+            ? @[ [UISheetPresentationControllerDetent largeDetent] ]
+            : @[
+                [UISheetPresentationControllerDetent mediumDetent],
+                [UISheetPresentationControllerDetent largeDetent]
+              ];
   }
 
   [self.baseViewController presentViewController:self.navigationController
