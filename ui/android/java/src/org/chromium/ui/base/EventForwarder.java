4032b6fd914d11d5c820d2394d48595a295c193b
Kartar Singh <kartarsingh@google.com>
2024-01-24 17:32:12 +0000

    Use correct time stamp for latest event in velocity tracker.
    We track the latency of the oldest timestamp, however we accidentally
    used this oldest timestamp rather then the real EventTime which caused
    timestamps to be out of order. This caused the VelocityTracker to
    compute a fling going in the reverse direction sometimes. This change
    fixes the bug to use correct timestamp for latest event.
    
    Bug: b/316055085, 1520879
    Change-Id: I83512170c69b68129ccc7db54092ede3d0c02809
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5225709
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Kartar Singh <kartarsingh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1251488}

--- ui/android/java/src/org/chromium/ui/base/EventForwarder.java
+++ ui/android/java/src/org/chromium/ui/base/EventForwarder.java
@@ -213,12 +213,14 @@ public class EventForwarder {
 
         TraceEvent.begin("sendTouchEvent");
         try {
+            final int historySize = event.getHistorySize();
             // Android may batch multiple events together for efficiency. We
             // want to use the oldest event time as hardware time stamp.
+            final long latestEventTime = MotionEventUtils.getEventTimeNanos(event);
             final long oldestEventTime =
-                    event.getHistorySize() > 0
-                            ? MotionEventUtils.getHistoricalEventTimeNanos(event, 0)
-                            : MotionEventUtils.getEventTimeNanos(event);
+                    historySize == 0
+                            ? latestEventTime
+                            : MotionEventUtils.getHistoricalEventTimeNanos(event, 0);
 
             int eventAction = event.getActionMasked();
 
@@ -265,9 +267,10 @@ public class EventForwarder {
                                     EventForwarder.this,
                                     event,
                                     oldestEventTime,
+                                    latestEventTime,
                                     eventAction,
                                     pointerCount,
-                                    event.getHistorySize(),
+                                    historySize,
                                     event.getActionIndex(),
                                     event.getX(),
                                     event.getY(),
@@ -727,7 +730,8 @@ public class EventForwarder {
                 long nativeEventForwarder,
                 EventForwarder caller,
                 MotionEvent event,
-                long timeNs,
+                long oldestEventTimeNs,
+                long latestEventTimeNs,
                 int action,
                 int pointerCount,
                 int historySize,
