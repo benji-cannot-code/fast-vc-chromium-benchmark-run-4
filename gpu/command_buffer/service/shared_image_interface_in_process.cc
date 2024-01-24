fae696c1bd3f5c18b820f2c0cf50b266aee193e0
Maggie Chen <magchen@chromium.org>
2024-01-24 18:26:05 +0000

    Return shared memory region mapping in CreatSharedImage for bitmap
    Since the mapping is returned in CreatSharedimage() with
    SHARED_IMAGE_USAGE_CPU_WRITE, CreatSharedImage() no longer needs
    ClientSharedImage->Map() and it will not create GpuMemoryBuffer.
    
    Bug: 1434885, b:320547063
    Change-Id: I92bdb8ddcb364af41d30573aa46bf257eebb1a38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5227443
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251534}

--- gpu/command_buffer/service/shared_image_interface_in_process.cc
+++ gpu/command_buffer/service/shared_image_interface_in_process.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
+#include "base/process/memory.h"
 #include "base/synchronization/waitable_event.h"
 #include "build/build_config.h"
 #include "components/viz/common/resources/shared_image_format_utils.h"
@@ -508,7 +509,7 @@ SharedImageInterfaceInProcess::CreateSharedImage(
 
   return base::MakeRefCounted<ClientSharedImage>(mailbox);
 }
-scoped_refptr<ClientSharedImage>
+SharedImageInterface::SharedImageMapping
 SharedImageInterfaceInProcess::CreateSharedImage(
     viz::SharedImageFormat format,
     const gfx::Size& size,
@@ -524,13 +525,25 @@ SharedImageInterfaceInProcess::CreateSharedImage(
   CHECK(!format.PrefersExternalSampler());
 #endif
 
+  SharedImageInterface::SharedImageMapping shared_image_mapping;
   gfx::BufferFormat buffer_format =
       viz::SinglePlaneSharedImageFormatToBufferFormat(format);
   const size_t buffer_size =
       gfx::BufferSizeForBufferFormat(size, buffer_format);
   auto shared_memory_region =
       base::UnsafeSharedMemoryRegion::Create(buffer_size);
-  CHECK(shared_memory_region.IsValid());
+  if (!shared_memory_region.IsValid()) {
+    DLOG(ERROR) << "base::UnsafeSharedMemoryRegion::Create() for SharedImage "
+                   "with SHARED_IMAGE_USAGE_CPU_WRITE fails!";
+    base::TerminateBecauseOutOfMemory(buffer_size);
+  }
+
+  shared_image_mapping.mapping = shared_memory_region.Map();
+  if (!shared_image_mapping.mapping.IsValid()) {
+    DLOG(ERROR)
+        << "shared_memory_region.Map() for SHARED_IMAGE_USAGE_CPU_WRITE fails!";
+    base::TerminateBecauseOutOfMemory(buffer_size);
+  }
 
   gfx::GpuMemoryBufferHandle handle;
   handle.type = gfx::SHARED_MEMORY_BUFFER;
@@ -538,9 +551,6 @@ SharedImageInterfaceInProcess::CreateSharedImage(
   handle.stride = static_cast<int32_t>(
       gfx::RowSizeForBufferFormat(size.width(), buffer_format, 0));
   handle.region = std::move(shared_memory_region);
-
-  GpuMemoryBufferHandleInfo handle_info = GpuMemoryBufferHandleInfo(
-      handle.Clone(), format, size, gfx::BufferUsage::SCANOUT_CPU_READ_WRITE);
 
   auto mailbox = Mailbox::GenerateForSharedImage();
   {
@@ -558,9 +568,10 @@ SharedImageInterfaceInProcess::CreateSharedImage(
                        std::move(handle), std::string(debug_label), sync_token),
         {});
   }
+  shared_image_mapping.shared_image =
+      base::MakeRefCounted<ClientSharedImage>(mailbox);
 
-  return base::MakeRefCounted<ClientSharedImage>(mailbox,
-                                                 std::move(handle_info));
+  return shared_image_mapping;
 }
 
 void SharedImageInterfaceInProcess::CreateSharedImageWithBufferOnGpuThread(
