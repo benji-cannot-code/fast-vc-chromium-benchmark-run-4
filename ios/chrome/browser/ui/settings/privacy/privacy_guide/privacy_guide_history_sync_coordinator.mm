fdc1d86e19d707cd43c4c9e5af1deab6e6f022e8
Rubin Deliallisi <rubindl@chromium.org>
2024-01-24 14:16:29 +0000

    [PG][iOS] History Sync VC skeleton
    - Add History Sync VC and connect it to the coordinator.
    - Add the banner image resources.
    
    Bug: 1520481
    Change-Id: Ib1f2a428e50c3e6b01200f21c30fbecab890c519
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5224184
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251392}

--- ios/chrome/browser/ui/settings/privacy/privacy_guide/privacy_guide_history_sync_coordinator.mm
+++ ios/chrome/browser/ui/settings/privacy/privacy_guide/privacy_guide_history_sync_coordinator.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/check.h"
 #import "ios/chrome/browser/ui/settings/privacy/privacy_guide/privacy_guide_constants.h"
+#import "ios/chrome/browser/ui/settings/privacy/privacy_guide/privacy_guide_history_sync_view_controller.h"
 #import "ios/chrome/common/ui/promo_style/promo_style_view_controller_delegate.h"
 
 @interface PrivacyGuideHistorySyncCoordinator () <
@@ -16,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @end
 
 @implementation PrivacyGuideHistorySyncCoordinator {
-  UIViewController* _viewController;
+  PrivacyGuideHistorySyncViewController* _viewController;
 }
 
 @synthesize baseNavigationController = _baseNavigationController;
@@ -37,8 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)start {
   // TODO(crbug.com/1520481): Implement History Sync view controller.
-  _viewController = [[UIViewController alloc] init];
-  _viewController.view.accessibilityIdentifier = kPrivacyGuideHistorySyncViewID;
+  _viewController = [[PrivacyGuideHistorySyncViewController alloc] init];
 
   CHECK(self.baseNavigationController);
   [self.baseNavigationController pushViewController:_viewController
