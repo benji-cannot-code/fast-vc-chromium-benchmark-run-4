e8c16f123924488d3aca208f6b432ec43bb48795
Colin Blundell <blundell@chromium.org>
2024-01-24 08:19:09 +0000

    [//content] Add comment on WebRTCVideoCaptureService test SI usage
    Make explicit that the SharedImages created by this test are both read
    and written via the raster interface.
    
    Bug: 1519074
    Change-Id: I276c193f45d3e1554ede44f7ea51cd970620dbed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5226582
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251282}

--- content/browser/webrtc/webrtc_video_capture_service_browsertest.cc
+++ content/browser/webrtc/webrtc_video_capture_service_browsertest.cc
@@ -202,6 +202,9 @@ class TextureDeviceExerciser : public VirtualDeviceExerciser {
         continue;
       }
 
+      // This SharedImage is populated via the raster interface below and may
+      // be read via the raster interface in normal VideoFrame usage exercised
+      // by the tests.
       gpu::Mailbox mailbox =
           sii->CreateSharedImage(viz::SinglePlaneFormat::kRGBA_8888,
                                  kDummyFrameCodedSize,
