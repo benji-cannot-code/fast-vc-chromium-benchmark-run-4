7ff5b97cfc2875ef7508b4958c0e8f6f70136468
Sergii Bykov <sbykov@google.com>
2024-01-24 13:23:23 +0000

    Fix networking.onc.getDeviceStates crash in lacros
    Recent refactoring replaced unique_ptrs to vectors with optionals.
    One of them became an empty optional (not init with vector inside),
    which causes a crash when values are emplaced there.
    
    Bug: b:321892541
    Change-Id: Ic885a923e38a56421e7124f2b990cac62e934d07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5229877
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Sergii Bykov <sbykov@google.com>
    Reviewed-by: Irina Fedorova <irfedorova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1251368}

--- extensions/browser/api/networking_private/networking_private_lacros.cc
+++ extensions/browser/api/networking_private/networking_private_lacros.cc
@@ -208,8 +208,8 @@ void DeviceStateListCallbackAdapter(
     std::move(callback).Run(std::nullopt);
     return;
   }
-  auto list =
-      std::optional<extensions::NetworkingPrivateDelegate::DeviceStateList>();
+  auto list = std::make_optional<
+      extensions::NetworkingPrivateDelegate::DeviceStateList>();
   for (auto& item : *result) {
     if (item) {
       list->emplace_back(
