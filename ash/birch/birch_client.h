a459159e303514d2660a1f3f6cca9d6b0c1f5ebe
Matthew Mourgos <mmourgos@chromium.org>
2024-01-24 19:26:56 +0000

    [Birch] Add birch client
    Add a basic birch client which will be used by the BirchModel to request that the BirchKeyedService fetch data.
    
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL Tests will be added once the birch client is used by the model.
    Bug: b/305094143
    Change-Id: Ic90bf7773d4b27c1f48367e71b0ea51e97e23902
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5200860
    Commit-Queue: Matthew Mourgos <mmourgos@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251585}

---
+++ ash/birch/birch_client.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_BIRCH_BIRCH_CLIENT_H_
+#define ASH_BIRCH_BIRCH_CLIENT_H_
+
+#include "ash/ash_export.h"
+
+namespace ash {
+
+// Interface to communicate with the birch keyed service.
+class ASH_EXPORT BirchClient {
+ public:
+  // Request that the birch keyed service start fetching data.
+  virtual void RequestBirchDataFetch() = 0;
+
+  virtual ~BirchClient() = default;
+};
+
+}  // namespace ash
+
+#endif  // ASH_BIRCH_BIRCH_CLIENT_H_
--- chrome/browser/ui/ash/birch/birch_keyed_service.cc
+++ chrome/browser/ui/ash/birch/birch_keyed_service.cc
@@ -11,14 +11,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/birch/birch_model.h"
 #include "ash/shell.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/ash/birch/birch_client_impl.h"
 #include "chrome/browser/ui/ash/birch/birch_file_suggest_provider.h"
 
 namespace ash {
 
 BirchKeyedService::BirchKeyedService(Profile* profile)
     : file_suggest_provider_(
-          std::make_unique<BirchFileSuggestProvider>(profile)) {}
+          std::make_unique<BirchFileSuggestProvider>(profile)) {
+  birch_client_impl_ = std::make_unique<BirchClientImpl>(profile);
+  Shell::Get()->birch_model()->SetClient(birch_client_impl_.get());
+}
 
-BirchKeyedService::~BirchKeyedService() = default;
+BirchKeyedService::~BirchKeyedService() {
+  Shell::Get()->birch_model()->SetClient(nullptr);
+}
+
+void BirchKeyedService::RequestBirchDataFetch() {
+  // TODO(b/305093932): Begin data fetching requests.
+}
 
 }  // namespace ash
