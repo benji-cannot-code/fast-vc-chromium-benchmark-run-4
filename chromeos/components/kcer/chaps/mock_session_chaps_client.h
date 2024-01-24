76fe1c5606b70efc90bb8abdc822e4344bfa1eaf
Michael Ershov <miersh@google.com>
2024-01-24 16:42:23 +0000

    Kcer-without-NSS: Implement GetKeyInfo
    Implement the KcerTokenImpl::GetKeyInfo method.
    
    Bug: b:244409232
    Test: *Chaps*, *Kcer* chromeos_components_unittests
    Change-Id: Iddc7c51b8786eb34e86c55e2518976889ad25592
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5185396
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1251447}

--- chromeos/components/kcer/chaps/mock_session_chaps_client.h
+++ chromeos/components/kcer/chaps/mock_session_chaps_client.h
@@ -18,6 +18,10 @@ class MockSessionChapsClient : public SessionChapsClient {
   ~MockSessionChapsClient() override;
 
   MOCK_METHOD(void,
+              GetMechanismList,
+              (SlotId slot_id, GetMechanismListCallback callback),
+              (override));
+  MOCK_METHOD(void,
               CreateObject,
               (SlotId slot_id,
                const std::vector<uint8_t>& attributes,
