edc49a993deb5a3810262d8c158516b5579c21c3
Justin Cohen <justincohen@google.com>
2024-01-24 19:40:03 +0000

    ios: Use raw_ptr for ios/ directory for iOS builds.
    This CL was uploaded by git cl split.
    
    R=victorvianna@google.com
    
    Change-Id: I3ce2c16027c86b40061d24db9bc159aadc611be6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5232823
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251600}

--- ios/chrome/browser/settings/model/sync/utils/sync_error_infobar_delegate.h
+++ ios/chrome/browser/settings/model/sync/utils/sync_error_infobar_delegate.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#import "base/memory/raw_ptr.h"
 #include "components/infobars/core/confirm_infobar_delegate.h"
 #include "components/sync/service/sync_service.h"
 #include "components/sync/service/sync_service_observer.h"
@@ -51,7 +52,7 @@ class SyncErrorInfoBarDelegate : public ConfirmInfoBarDelegate,
   void OnStateChanged(syncer::SyncService* sync) override;
 
  private:
-  ChromeBrowserState* browser_state_;
+  raw_ptr<ChromeBrowserState> browser_state_;
   syncer::SyncService::UserActionableError error_state_;
   std::u16string title_;
   std::u16string message_;
