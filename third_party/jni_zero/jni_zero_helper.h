3cd3a56be379973291459728eb024cc8bb43fb87
Sam Maier <smaier@chromium.org>
2024-01-24 20:52:32 +0000

    jni_zero: removing now-unused //base includes
    Bug: 1377351
    Change-Id: Id73025abf5eda0247da1290f29265750a0b6a4f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5231992
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251676}

--- third_party/jni_zero/jni_zero_helper.h
+++ third_party/jni_zero/jni_zero_helper.h
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <jni.h>
 
 #include "base/android/jni_android.h"
-#include "base/android/scoped_java_ref.h"
-#include "base/compiler_specific.h"
 #if defined(USE_CHROMIUM_BASE)
 // Used for ARCH_CPU_X86 - embedder must define this correctly if they want
 // 16-byte stack alignment on x86.
