b367c0b4a22df6f3ed3eff6833324395c9cbac36
Slobodan Pejic <slobodan@chromium.org>
2024-01-24 16:07:03 +0000

    Extract the credit card data implementation to a shared location. 1/X
    Move the implementation of PaymentsSuggestionBottomSheetData out of the
    PaymentsSuggetionBottomSheetMediator, so that the future virtual card
    enrollment bottom sheet can reuse the same constructor for transforming
    from an autofill::CreditCard to a CreditCardData (formerly
    PaymentsSuggestionBottomSheetData).
    
    Bug: 1485376, b:304547857
    Change-Id: I974b6b517a9ae5eec0beb7bb08bea3e816629e69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5124471
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251425}

--- ios/chrome/browser/ui/autofill/bottom_sheet/payments_suggestion_bottom_sheet_consumer.h
+++ ios/chrome/browser/ui/autofill/bottom_sheet/payments_suggestion_bottom_sheet_consumer.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
-@protocol PaymentsSuggestionBottomSheetData;
+@class CreditCardData;
 
 // Suggestions consumer for the payments bottom sheet.
 @protocol PaymentsSuggestionBottomSheetConsumer
@@ -16,8 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Sends the list of credit cards to be presented to the user on the bottom
 // sheet and a BOOL to determine if the user sees the GPay logo as title of the
 // bottom sheet.
-- (void)setCreditCardData:
-            (NSArray<id<PaymentsSuggestionBottomSheetData>>*)creditCardData
+- (void)setCreditCardData:(NSArray<CreditCardData*>*)creditCardData
         showGooglePayLogo:(BOOL)showGooglePayLogo;
 
 // Request to dismiss the bottom sheet.
--- ios/chrome/browser/ui/autofill/bottom_sheet/payments_suggestion_bottom_sheet_view_controller.mm
+++ ios/chrome/browser/ui/autofill/bottom_sheet/payments_suggestion_bottom_sheet_view_controller.mm
@@ -11,10 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/autofill/core/browser/data_model/credit_card.h"
 #import "components/grit/components_scaled_resources.h"
 #import "components/url_formatter/elide_url.h"
+#import "ios/chrome/browser/autofill/model/credit_card/credit_card_data.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_detail_icon_item.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
-#import "ios/chrome/browser/ui/autofill/bottom_sheet/payments_suggestion_bottom_sheet_data.h"
 #import "ios/chrome/browser/ui/autofill/bottom_sheet/payments_suggestion_bottom_sheet_delegate.h"
 #import "ios/chrome/browser/ui/autofill/bottom_sheet/payments_suggestion_bottom_sheet_handler.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
@@ -66,7 +66,7 @@ NSString* const kCustomDetentIdentifier = @"customDetent";
   NSLayoutConstraint* _heightConstraint;
 
   // List of credit cards and icon for the bottom sheet.
-  NSArray<id<PaymentsSuggestionBottomSheetData>>* _creditCardData;
+  NSArray<CreditCardData*>* _creditCardData;
 
   // URL of the current page the bottom sheet is being displayed on.
   GURL _URL;
@@ -194,8 +194,7 @@ NSString* const kCustomDetentIdentifier = @"customDetent";
 
 #pragma mark - PaymentsSuggestionBottomSheetConsumer
 
-- (void)setCreditCardData:
-            (NSArray<id<PaymentsSuggestionBottomSheetData>>*)creditCardData
+- (void)setCreditCardData:(NSArray<CreditCardData*>*)creditCardData
         showGooglePayLogo:(BOOL)showGooglePayLogo {
   BOOL requiresUpdate = (_creditCardData != nil);
   _creditCardData = creditCardData;
