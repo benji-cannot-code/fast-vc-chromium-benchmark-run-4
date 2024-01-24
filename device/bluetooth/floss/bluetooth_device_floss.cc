b0511daeb75a6b3e113dee5f04a5adce4335e3f1
Archie <apusaka@google.com>
2024-01-24 08:37:54 +0000

    [floss] Wire the bluetooth address type API
    Address type is to be used to determine whether a dual device is
    actually using LE or classic transport.
    
    BUG=b:312859619
    TEST=Using dbus-monitor, verify GetRemoteAddressType is called by UI
    
    Change-Id: I7e5418efa63f4f6343ed44815cafe8a4d224ec0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5190867
    Reviewed-by: Hsin-chen Chuang <chharry@google.com>
    Reviewed-by: Katherine Lai <laikatherine@chromium.org>
    Commit-Queue: Archie Pusaka <apusaka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251291}

--- device/bluetooth/floss/bluetooth_device_floss.cc
+++ device/bluetooth/floss/bluetooth_device_floss.cc
@@ -86,9 +86,7 @@ std::string BluetoothDeviceFloss::GetAddress() const {
 }
 
 AddressType BluetoothDeviceFloss::GetAddressType() const {
-  NOTIMPLEMENTED();
-
-  return AddressType::ADDR_TYPE_UNKNOWN;
+  return address_type_;
 }
 
 VendorIDSource BluetoothDeviceFloss::GetVendorIDSource() const {
@@ -759,6 +757,30 @@ void BluetoothDeviceFloss::OnGetRemoteVendorProductInfo(
   std::move(callback).Run();
 }
 
+void BluetoothDeviceFloss::OnGetRemoteAddressType(
+    base::OnceClosure callback,
+    DBusResult<FlossAdapterClient::BtAddressType> ret) {
+  if (ret.has_value()) {
+    switch (*ret) {
+      case FlossAdapterClient::BtAddressType::kPublic:
+      case FlossAdapterClient::BtAddressType::kPublicId:
+        address_type_ = AddressType::ADDR_TYPE_PUBLIC;
+        break;
+      case FlossAdapterClient::BtAddressType::kRandom:
+      case FlossAdapterClient::BtAddressType::kRandomId:
+        address_type_ = AddressType::ADDR_TYPE_RANDOM;
+        break;
+      default:
+        address_type_ = AddressType::ADDR_TYPE_UNKNOWN;
+        break;
+    }
+  } else {
+    BLUETOOTH_LOG(ERROR) << "GetRemoteAddressType() failed: " << ret.error();
+  }
+
+  std::move(callback).Run();
+}
+
 void BluetoothDeviceFloss::OnConnectAllEnabledProfiles(DBusResult<Void> ret) {
   if (!ret.has_value()) {
     BLUETOOTH_LOG(ERROR) << "Failed to connect all enabled profiles: "
@@ -893,6 +915,13 @@ void BluetoothDeviceFloss::FetchRemoteVendorProductInfo(
       AsFlossDeviceId());
 }
 
+void BluetoothDeviceFloss::FetchRemoteAddressType(base::OnceClosure callback) {
+  FlossDBusManager::Get()->GetAdapterClient()->GetRemoteAddressType(
+      base::BindOnce(&BluetoothDeviceFloss::OnGetRemoteAddressType,
+                     weak_ptr_factory_.GetWeakPtr(), std::move(callback)),
+      AsFlossDeviceId());
+}
+
 void BluetoothDeviceFloss::InitializeDeviceProperties(
     PropertiesState state,
     base::OnceClosure callback) {
@@ -906,7 +935,7 @@ void BluetoothDeviceFloss::InitializeDeviceProperties(
   // This must be incremented when adding more properties below
   // and followed up with a TriggerInitDevicePropertiesCallback()
   // in the callback.
-  num_pending_properties_ += 5;
+  num_pending_properties_ += 6;
   FetchRemoteType(
       base::BindOnce(&BluetoothDeviceFloss::TriggerInitDevicePropertiesCallback,
                      weak_ptr_factory_.GetWeakPtr()));
@@ -920,6 +949,9 @@ void BluetoothDeviceFloss::InitializeDeviceProperties(
       base::BindOnce(&BluetoothDeviceFloss::TriggerInitDevicePropertiesCallback,
                      weak_ptr_factory_.GetWeakPtr()));
   FetchRemoteVendorProductInfo(
+      base::BindOnce(&BluetoothDeviceFloss::TriggerInitDevicePropertiesCallback,
+                     weak_ptr_factory_.GetWeakPtr()));
+  FetchRemoteAddressType(
       base::BindOnce(&BluetoothDeviceFloss::TriggerInitDevicePropertiesCallback,
                      weak_ptr_factory_.GetWeakPtr()));
 }
