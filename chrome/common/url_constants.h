72315a5c6f9dcf78df396271b4a63d38b4297c14
Muhammad Hasan Khan <mhasank@google.com>
2024-01-24 20:34:51 +0000

    arc: Migrate to new Play terms of service url
    Play terms of service have migrated from play.google.com to play.google.
    In this change we're updating hard-coded url in all places and updating
    tests as well.
    
    BUG=b:321824863
    TEST=tast run arc.Optin.x
    TEST=tast run arc.OobeArc.vm
    TEST=unit_tests
    
    Change-Id: Iac9d83bf385dcded70f10f0cf5d29e379f49a363
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5231937
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Commit-Queue: Muhammad Hasan Khan <mhasank@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Long Cheng <lgcheng@google.com>
    Auto-Submit: Muhammad Hasan Khan <mhasank@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251646}

--- chrome/common/url_constants.h
+++ chrome/common/url_constants.h
@@ -708,7 +708,7 @@ inline constexpr char kCrosEulaOnlineURLPath[] =
 
 // The URL path to online ARC++ terms of service.
 inline constexpr char kArcTosOnlineURLPath[] =
-    "https://play.google.com/about/play-terms/embedded/";
+    "https://play.google/play-terms/embedded/";
 
 // The URL path to online privacy policy.
 inline constexpr char kPrivacyPolicyOnlineURLPath[] =
