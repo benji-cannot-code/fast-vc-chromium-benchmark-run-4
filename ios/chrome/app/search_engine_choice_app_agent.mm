af37055a42326a1099a61fa06d58bbe574e78423
Arthur Milchior <arthurmilchior@chromium.org>
2024-01-24 18:24:45 +0000

    [iOS] explain the use of main Browser Provider.
    Bug: b/322145399
    Change-Id: I96fb2822951cb6a22a0480f5b7176c84b76c8dd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5233628
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Samar Chehade-Lepleux <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1251532}

--- ios/chrome/app/search_engine_choice_app_agent.mm
+++ ios/chrome/app/search_engine_choice_app_agent.mm
@@ -115,6 +115,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (_searchEngineChoiceCoordinator) {
     return;
   }
+  // Using main browser so that, even in incognito mode, the user is not
+  // re-asked which search engine to use.
   if (ShouldDisplaySearchEngineChoiceScreen(
           *sceneState.browserProviderInterface.mainBrowserProvider.browser
                ->GetBrowserState(),
