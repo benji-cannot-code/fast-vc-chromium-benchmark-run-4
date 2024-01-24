33bd02fe7b4f45b8e1870a29d1828e4c97b2ac1d
Kent Tamura <tkent@chromium.org>
2024-01-24 04:32:57 +0000

    Remove SvgTextFixHittestAfterScale runtime flag
    It has been enabled by default since M120, and we have seen no issues.
    This CL has no behavior changes.
    
    Change-Id: Iece8a00b2a66333b275e6d862641c82e2b5768dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5219396
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251231}

--- third_party/blink/renderer/core/layout/layout_view.cc
+++ third_party/blink/renderer/core/layout/layout_view.cc
@@ -68,7 +68,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/instrumentation/histogram.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/traced_value.h"
-#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "ui/display/screen_info.h"
 #include "ui/gfx/geometry/quad_f.h"
 #include "ui/gfx/geometry/size_conversions.h"
@@ -149,8 +148,7 @@ void LayoutView::Trace(Visitor* visitor) const {
 bool LayoutView::HitTest(const HitTestLocation& location,
                          HitTestResult& result) {
   NOT_DESTROYED();
-  if (RuntimeEnabledFeatures::SvgTextFixHittestAfterScaleEnabled() &&
-      has_svg_text_descendants_) {
+  if (has_svg_text_descendants_) {
     // This is necessary because SVG <text> might have obsolete geometry after
     // scale-only changes.  See crbug.com/1296089#c16
     auto it = svg_text_descendants_->find(this);
