e0e02970751656e46b0ed3c2a6876baf376413cf
mikt <mikt@google.com>
2024-01-24 17:32:19 +0000

    [PA/Lightweight Quarantine] Use std::vector<QuarantineSlot> for metadata
    The parent CL made it possible to use `std::vector` for PA's feature
    so using it here.
    There's no strong demand to cap *number* of quarantine entries, so
    dropping the `quarantine_capacity_count` for simplicity.
    
    Bug: 1462223, 1518071
    Change-Id: I6e8e32a168e8fb1cba550219fa9937c43cc26008
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5208323
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Cr-Commit-Position: refs/heads/main@{#1251489}

--- base/allocator/partition_allocator/src/partition_alloc/partition_root.h
+++ base/allocator/partition_allocator/src/partition_alloc/partition_root.h
@@ -174,7 +174,6 @@ struct PartitionOptions {
   AllowToggle use_configurable_pool = kDisallowed;
 
   EnableToggle scheduler_loop_quarantine = kDisabled;
-  size_t scheduler_loop_quarantine_capacity_count = 0;
   size_t scheduler_loop_quarantine_capacity_in_bytes = 0;
 
   EnableToggle zapping_by_free_flags = kDisabled;
@@ -376,7 +375,6 @@ struct PA_ALIGNAS(64) PA_COMPONENT_EXPORT(PARTITION_ALLOC) PartitionRoot {
 
   bool quarantine_always_for_testing = false;
 
-  size_t scheduler_loop_quarantine_capacity_count = 0;
   size_t scheduler_loop_quarantine_capacity_in_bytes = 0;
   internal::LightweightQuarantineRoot scheduler_loop_quarantine_root;
   // NoDestructor because we don't need to dequarantine objects as the root
--- base/allocator/partition_allocator/src/partition_alloc/shim/allocator_shim_default_dispatch_to_partition_alloc.cc
+++ base/allocator/partition_allocator/src/partition_alloc/shim/allocator_shim_default_dispatch_to_partition_alloc.cc
@@ -494,7 +494,6 @@ void ConfigurePartitions(
     BucketDistribution distribution,
     SchedulerLoopQuarantine scheduler_loop_quarantine,
     size_t scheduler_loop_quarantine_capacity_in_bytes,
-    size_t scheduler_loop_quarantine_capacity_count,
     ZappingByFreeFlags zapping_by_free_flags) {
   // Calling Get() is actually important, even if the return value isn't
   // used, because it has a side effect of initializing the variable, if it
@@ -529,8 +528,6 @@ void ConfigurePartitions(
                 : partition_alloc::PartitionOptions::kDisabled;
         opts.scheduler_loop_quarantine_capacity_in_bytes =
             scheduler_loop_quarantine_capacity_in_bytes;
-        opts.scheduler_loop_quarantine_capacity_count =
-            scheduler_loop_quarantine_capacity_count;
         opts.memory_tagging = {
             .enabled = enable_memory_tagging
                            ? partition_alloc::PartitionOptions::kEnabled
