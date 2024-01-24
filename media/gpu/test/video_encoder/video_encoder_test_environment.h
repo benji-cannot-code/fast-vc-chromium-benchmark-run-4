81fc4fda36869b7fd3be19764ce57511a0fd01ab
Hirokazu Honda <hiroh@chromium.org>
2024-01-24 06:43:55 +0000

    media/gpu/vaapi: Set is_screen in rate controllers
    This wires VideoEncodeAccelerator::Config::content_type to is_screen
    of each rate controller.
    
    Bug: b:280361196
    Test: screen sharing in Google Meet
    Test: video_encode_accelerator_tests --content_type=Display
    Change-Id: I82162baeb6d1456944b5d6874236f6d44461d65b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5197230
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Justin Green <greenjustin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1251262}

--- media/gpu/test/video_encoder/video_encoder_test_environment.h
+++ media/gpu/test/video_encoder/video_encoder_test_environment.h
@@ -53,6 +53,7 @@ class VideoEncoderTestEnvironment : public VideoTestEnvironment {
       const base::FilePath& output_folder,
       const std::string& codec,
       const std::string& svc_mode,
+      VideoEncodeAccelerator::Config::ContentType content_type,
       bool save_output_bitstream,
       absl::optional<uint32_t> output_bitrate,
       Bitrate::Mode bitrate_mode,
@@ -77,6 +78,7 @@ class VideoEncoderTestEnvironment : public VideoTestEnvironment {
   const std::vector<VideoEncodeAccelerator::Config::SpatialLayer>&
   SpatialLayers() const;
   SVCInterLayerPredMode InterLayerPredMode() const;
+  VideoEncodeAccelerator::Config::ContentType ContentType() const;
   // Get the target bitrate (bits/second).
   const VideoBitrateAllocation& BitrateAllocation() const;
   // Whether the encoded bitstream is saved to disk.
@@ -111,6 +113,7 @@ class VideoEncoderTestEnvironment : public VideoTestEnvironment {
       SVCInterLayerPredMode inter_layer_pred_mode,
       size_t num_temporal_layers,
       size_t num_spatial_layers,
+      VideoEncodeAccelerator::Config::ContentType content_type,
       const media::VideoBitrateAllocation& bitrate,
       bool save_output_bitstream,
       bool reverse,
@@ -136,6 +139,8 @@ class VideoEncoderTestEnvironment : public VideoTestEnvironment {
   // The spatial layers of the stream which aligned with |num_spatial_layers_|
   // and |num_temporal_layers_|. This is only for vp9 stream.
   std::vector<VideoEncodeAccelerator::Config::SpatialLayer> spatial_layers_;
+  // A type of content to be encoded.
+  VideoEncodeAccelerator::Config::ContentType content_type_;
   // Whether the bitstream produced by VideoEncoder is saved to disk.
   const bool save_output_bitstream_;
   // True if the video should play backwards at reaching the end of video.
