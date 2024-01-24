0a05743b1a8ee3a916ce294c911f23a3d45a0175
Pete Williamson <petewil@chromium.org>
2024-01-24 01:05:57 +0000

    Minor UMA fixes - mark kDisabledMsbb as obsolete, split generic error
    the kGenericBlocked is split out into two cases, kFeatureFlagDisabled
    and kGenericBlocked
    
    Bug: b:319303808
    Change-Id: Ia9de956bee55b080f1ea537536df46ca02d28054
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5231025
    Commit-Queue: Peter Williamson <petewil@chromium.org>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251159}

--- chrome/browser/compose/compose_enabling_unittest.cc
+++ chrome/browser/compose/compose_enabling_unittest.cc
@@ -245,7 +245,7 @@ TEST_F(ComposeEnablingTest, FeatureNotEnabledTest) {
   SignIn(signin::ConsentLevel::kSync);
 
   CheckIsEnabledError(compose_enabling_.get(),
-                      compose::ComposeShowStatus::kGenericBlocked);
+                      compose::ComposeShowStatus::kFeatureFlagDisabled);
 }
 
 TEST_F(ComposeEnablingTest, NotSignedInTest) {
