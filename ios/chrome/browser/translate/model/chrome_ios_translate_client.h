79e50fb2de94527476ac320829c5917b95b2d377
Justin Cohen <justincohen@google.com>
2024-01-24 19:53:32 +0000

    ios: Use raw_ptr for ios/ directory for iOS builds.
    This CL was uploaded by git cl split.
    
    R=perrier@chromium.org
    
    Change-Id: Ia639c9c17ed0198c66a71201151fbea8a5d56f2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5232736
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251613}

--- ios/chrome/browser/translate/model/chrome_ios_translate_client.h
+++ ios/chrome/browser/translate/model/chrome_ios_translate_client.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/gtest_prod_util.h"
+#import "base/memory/raw_ptr.h"
 #include "components/translate/core/browser/translate_client.h"
 #include "components/translate/core/browser/translate_step.h"
 #include "components/translate/core/common/translate_errors.h"
@@ -95,7 +96,7 @@ class ChromeIOSTranslateClient
 
   // The WebState this instance is observing. Will be null after
   // WebStateDestroyed has been called.
-  web::WebState* web_state_ = nullptr;
+  raw_ptr<web::WebState> web_state_ = nullptr;
 
   translate::IOSTranslateDriver translate_driver_;
   std::unique_ptr<translate::TranslateManager> translate_manager_;
