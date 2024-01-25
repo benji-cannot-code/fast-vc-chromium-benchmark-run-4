600956bc02e05b4af3b9ffe25cb6a357d2b58e27
Vidhan <vidhanj@google.com>
2024-01-25 18:25:07 +0000

    Reenable SaveProfileEGTest egtests
    Bug: 1517656
    Change-Id: Ie446c1a0ff8b2d2ee39fe1f7baa282af39bd7145
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5233766
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1252197}

--- ios/chrome/browser/ui/autofill/save_profile_egtest.mm
+++ ios/chrome/browser/ui/autofill/save_profile_egtest.mm
@@ -113,8 +113,7 @@ BOOL WaitForKeyboardToAppear() {
 #pragma mark - Tests
 
 // Ensures that the profile is saved to Chrome after submitting the form.
-// TODO(crbug.com/1517656): Test is failing.
-- (void)DISABLED_testUserData_LocalSave {
+- (void)testUserData_LocalSave {
   GREYAssertTrue(self.testServer->Start(), @"Server did not start.");
   [ChromeEarlGrey loadURL:self.testServer->GetURL(kProfileForm)];
 
@@ -141,8 +140,7 @@ BOOL WaitForKeyboardToAppear() {
 
 // Ensures that the profile is saved to Chrome after submitting and editing the
 // form.
-// TODO(crbug.com/1517656): Test is failing.
-- (void)DISABLED_testUserData_LocalEdit {
+- (void)testUserData_LocalEdit {
   GREYAssertTrue(self.testServer->Start(), @"Server did not start.");
   [ChromeEarlGrey loadURL:self.testServer->GetURL(kProfileForm)];
 
@@ -177,8 +175,7 @@ BOOL WaitForKeyboardToAppear() {
 }
 
 // Ensures that the profile is saved to Account after submitting the form.
-// TODO(crbug.com/1517656): Test is failing.
-- (void)DISABLED_testUserData_AccountSave {
+- (void)testUserData_AccountSave {
   [SigninEarlGreyUI signinWithFakeIdentity:[FakeSystemIdentity fakeIdentity1]
                                 enableSync:YES];
 
@@ -216,8 +213,7 @@ BOOL WaitForKeyboardToAppear() {
 
 // Ensures that the profile is saved to Account after submitting and editing the
 // form.
-// TODO(crbug.com/1517656): Test is failing.
-- (void)DISABLED_testUserData_AccountEdit {
+- (void)testUserData_AccountEdit {
   [SigninEarlGreyUI signinWithFakeIdentity:[FakeSystemIdentity fakeIdentity1]
                                 enableSync:YES];
 
