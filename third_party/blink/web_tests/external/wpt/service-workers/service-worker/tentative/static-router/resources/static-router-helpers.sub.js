83f7a7d50bf09321a8d12b0ed1ac65479828fb72
sisidovski <sisidovski@chromium.org>
2024-01-25 15:42:15 +0000

    Move main resource test functions into helper js
    This CL moves some of functions in the main resource test file to the
    helper js file in order to make them reusable from other tests.
    
    Bug: 1519727, 1511459
    Change-Id: Idaede86258009d109bfcb72af734b2611357485c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5232708
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252104}

--- third_party/blink/web_tests/external/wpt/service-workers/service-worker/tentative/static-router/resources/static-router-helpers.sub.js
+++ third_party/blink/web_tests/external/wpt/service-workers/service-worker/tentative/static-router/resources/static-router-helpers.sub.js
@@ -9,7 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // - errors.
 //
 // See: static-router-sw.js for details.
-const get_info_from_worker = async worker => {
+const get_info_from_worker =
+    async worker => {
   const promise = new Promise(function(resolve) {
       var channel = new MessageChannel();
       channel.port1.onmessage = function(msg) { resolve(msg); };
@@ -21,7 +22,8 @@ const get_info_from_worker = async worker => {
 }
 
 // Reset information stored inside ServiceWorker.
-const reset_info_in_worker = async worker => {
+const reset_info_in_worker =
+    async worker => {
   const promise = new Promise(function(resolve) {
       var channel = new MessageChannel();
       channel.port1.onmessage = function(msg) { resolve(msg); };
@@ -32,8 +34,11 @@ const reset_info_in_worker = async worker => {
 
 // Register the ServiceWorker and wait until activated.
 // {ruleKey} represents the key of routerRules defined in router-rules.js.
-const registerAndActivate = async (test, ruleKey) => {
-  const swScript = 'resources/static-router-sw.js';
+// {swScript} represents the service worker source URL.
+const registerAndActivate = async (test, ruleKey, swScript) => {
+  if (!swScript) {
+    swScript = 'resources/static-router-sw.js'
+  }
   const swURL = `${swScript}?key=${ruleKey}`;
   const swScope = 'resources/';
   const reg = await service_worker_unregister_and_register(
@@ -43,4 +48,22 @@ const registerAndActivate = async (test, ruleKey) => {
   await wait_for_state(test, worker, 'activated');
 
   return worker;
+};
+
+// Create iframe with the given url. This automatically removes the iframe in a
+// cleanup.
+const createIframe = async (t, url) => {
+  const iframe = await with_iframe(url);
+  t.add_cleanup(() => iframe.remove());
+
+  return iframe;
+};
+
+// Register a service worker, then create an iframe at url.
+function iframeTest(url, ruleKey, callback, name) {
+  return promise_test(async t => {
+    const worker = await registerAndActivate(t, ruleKey);
+    const iframe = await createIframe(t, url);
+    await callback(t, iframe.contentWindow, worker);
+  }, name);
 };
