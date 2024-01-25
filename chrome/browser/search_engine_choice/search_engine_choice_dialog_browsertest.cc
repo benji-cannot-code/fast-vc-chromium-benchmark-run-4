1c9a12c866a41d20f99f085d03e3381e48d650b6
Nicolas Dossou-Gbete <dgn@chromium.org>
2024-01-25 18:36:39 +0000

    🧇 Fix Choice prefs cleanup for Guest mode
    The OTR Guest profile was being cleaned up, while the prefs to remove
    were staying in the parent profile. This ensures we also clean up
    the parent guest for choice prefs.
    
    Bug: b:321187721
    Bug: 1521820
    Change-Id: I489fdb2ec4b0b0a681589e29f80dd2460842654a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5237670
    Reviewed-by: David Roger <droger@chromium.org>
    Auto-Submit: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252218}

--- chrome/browser/search_engine_choice/search_engine_choice_dialog_browsertest.cc
+++ chrome/browser/search_engine_choice/search_engine_choice_dialog_browsertest.cc
@@ -50,7 +50,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keep_alive_registry/scoped_keep_alive.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "components/search_engines/choice_made_location.h"
 #include "components/search_engines/default_search_manager.h"
+#include "components/search_engines/prepopulated_engines.h"
 #include "components/search_engines/search_engine_choice_utils.h"
 #include "components/search_engines/search_engine_utils.h"
 #include "components/search_engines/search_engines_pref_names.h"
@@ -770,6 +772,13 @@ IN_PROC_BROWSER_TEST_F(SearchEngineChoiceDialogBrowserTest,
           first_guest_session->profile()));
 
   EXPECT_TRUE(first_service->IsShowingDialog(first_guest_session));
+
+  // Complete the choice for the first guest profile.
+  first_service->NotifyChoiceMade(
+      TemplateURLPrepopulateData::bing.id,
+      SearchEngineChoiceDialogService::EntryPoint::kDialog);
+  EXPECT_FALSE(first_service->IsShowingDialog(first_guest_session));
+
   CloseBrowserSynchronously(first_guest_session);
   EXPECT_EQ(BrowserList::GetInstance()->size(), 1u);
 
@@ -779,6 +788,7 @@ IN_PROC_BROWSER_TEST_F(SearchEngineChoiceDialogBrowserTest,
           second_guest_session->profile()));
   EXPECT_EQ(BrowserList::GetInstance()->size(), 2u);
 
+  // The second guest profile still needs to choose again
   EXPECT_TRUE(second_service->IsShowingDialog(second_guest_session));
 }
 #endif
