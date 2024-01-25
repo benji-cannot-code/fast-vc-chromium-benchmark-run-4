09c87089948863713454b3726d48be129b761c3f
Yotam Hacohen <yotha@chromium.org>
2024-01-25 18:48:02 +0000

    add DecoratedBoundingBox function to layout_svg_filter_primitive
    Bug: 1513094
    Change-Id: I320c0c2362f572c5e9738e2a38ec58afd21cf7b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5230740
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Yotam Hacohen <yotha@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252238}

--- third_party/blink/renderer/core/layout/svg/layout_svg_filter_primitive.h
+++ third_party/blink/renderer/core/layout/svg/layout_svg_filter_primitive.h
@@ -76,6 +76,10 @@ class LayoutSVGFilterPrimitive final : public LayoutObject {
     NOT_DESTROYED();
     return gfx::RectF();
   }
+  gfx::RectF DecoratedBoundingBox() const override {
+    NOT_DESTROYED();
+    return gfx::RectF();
+  }
 };
 
 }  // namespace blink
