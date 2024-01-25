773170d354b0609cf80864a5b5b38b89e9682592
Benedikt Meurer <bmeurer@chromium.org>
2024-01-25 10:34:43 +0000

    [DevTools] Replace flaky IndexedDB layout test.
    Refine the fix from https://crrev.com/c/1403675 to use an
    inspector-protocol test, since the fix was purely about the
    inspector backend, and thereby avoid the flakiness related
    to involving the front-end layers into the test. Also limit
    the test specifically to what it's supposed to test: That
    the version is not truncated to int32.
    
    Fixed: chromium:1521441
    Bug: chromium:916286
    Change-Id: I89e335050a666162aca9401dd8e8f41b52f95e05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5233674
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251996}

---
+++ third_party/blink/web_tests/inspector-protocol/indexeddb/database-version-number.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+  var {page, session, dp} = await testRunner.startURL(
+      '../resources/indexeddb.html',
+      'Tests that IndexedDB database version is reported correctly');
+
+  await dp.IndexedDB.enable();
+  await session.evaluateAsync('window.dbPromise');
+
+  const {result: {databaseWithObjectStores}} =
+      await dp.IndexedDB.requestDatabase({
+        databaseName: 'testDatabase1',
+        securityOrigin: 'file://',
+      });
+
+  testRunner.log(databaseWithObjectStores);
+  testRunner.completeTest();
+});
