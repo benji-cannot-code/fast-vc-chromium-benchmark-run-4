9d6837cc150190302588e01e9b17091909ce4347
Justin Cohen <justincohen@google.com>
2024-01-25 15:21:26 +0000

    ios: Use raw_ptr for ios/ directory for iOS builds.
    This CL was uploaded by git cl split.
    
    R=eic@google.com
    
    Change-Id: Ic812856f3ce9fa610fd69b07ebf9da710538ae77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5233030
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252089}

--- ios/chrome/browser/ui/settings/password/passwords_mediator_unittest.mm
+++ ios/chrome/browser/ui/settings/password/passwords_mediator_unittest.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/password/passwords_mediator+Testing.h"
 
 #import "base/apple/foundation_util.h"
+#import "base/memory/raw_ptr.h"
 #import "base/strings/string_piece.h"
 #import "base/strings/string_util.h"
 #import "base/strings/utf_string_conversions.h"
@@ -187,7 +188,7 @@ class PasswordsMediatorTest : public BlockCleanupTest {
   scoped_refptr<IOSChromePasswordCheckManager> password_check_;
   FakePasswordsConsumer* consumer_;
   PasswordsMediator* mediator_;
-  feature_engagement::test::MockTracker* mock_tracker_;
+  raw_ptr<feature_engagement::test::MockTracker> mock_tracker_;
 };
 
 // Consumer should be notified when passwords are changed.
