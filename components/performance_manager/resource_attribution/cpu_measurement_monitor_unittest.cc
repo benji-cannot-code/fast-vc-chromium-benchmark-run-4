b0ebf11bed69b9c5a28ff1fa6b55cfda2bea4654
Joe Mason <joenotcharles@google.com>
2024-01-25 15:27:44 +0000

    Disallow invalid metadata in QueryResults
    This gives ResultMetadata a constructor to ensure both measurement_time
    and algorithm are always set. All "empty" QueryResult objects are now
    nullopt. This prevents the possibility of an object with invalid
    metadata accidentally being returned from a query, and better fits the
    design principle of avoiding "null" or "invalid" values within a type
    and using optional<Type> to document when it can be null.
    
    R=anthonyvd
    
    Bug: 1471683
    Change-Id: I9f0232bdc7ff26c5581ce5c0342b285de22751d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5231455
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252094}

--- components/performance_manager/resource_attribution/cpu_measurement_monitor_unittest.cc
+++ components/performance_manager/resource_attribution/cpu_measurement_monitor_unittest.cc
@@ -1392,7 +1392,8 @@ TEST_F(ResourceAttrCPUMonitorTest, CPUProportionTracker) {
                              base::TimeTicks measurement_time) {
     results[frame8->GetResourceContext()] = QueryResults{
         .cpu_time_result = CPUTimeResult{
-            .metadata = {.measurement_time = measurement_time},
+            .metadata = ResultMetadata(
+                measurement_time, MeasurementAlgorithm::kDirectMeasurement),
             .start_time = half_first_interval,
             .cumulative_cpu = (measurement_time - half_first_interval) * 0.4,
         }};
