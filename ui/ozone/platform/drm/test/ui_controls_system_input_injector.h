a787c78b14e74ba32b06667c61ba45bca05568cd
Vincent Chiang <vincentchiang@google.com>
2024-01-25 17:36:33 +0000

    Crosier: Add ui_controls for evdev
    This adds an implementation of UIControlsAura that injects event at the
    evdev level. This changes includes mouse and keyboard implementation
    and will be primarily used for ChromeOS integration tests (Crosier).
    The UIControls implementation uses the ozone level SystemInputInjector
    for evdev. Native event handling is also enabled for crosier.
    
    Bug: b:306009282, b:304625912
    Change-Id: I68489a2832f65b419f37621d0a46d3af04a0d04d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5121144
    Commit-Queue: Vincent Chiang <vincentchiang@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252184}

---
+++ ui/ozone/platform/drm/test/ui_controls_system_input_injector.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_OZONE_PLATFORM_DRM_TEST_UI_CONTROLS_SYSTEM_INPUT_INJECTOR_H_
+#define UI_OZONE_PLATFORM_DRM_TEST_UI_CONTROLS_SYSTEM_INPUT_INJECTOR_H_
+
+namespace ui::test {
+
+void EnableUIControlsSystemInputInjector();
+
+}  // namespace ui::test
+
+#endif  // UI_OZONE_PLATFORM_DRM_TEST_UI_CONTROLS_SYSTEM_INPUT_INJECTOR_H_
