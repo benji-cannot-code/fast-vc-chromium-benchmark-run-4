e963b98c6345a5faf56f5f9a3c746feeafe5f5a6
Justin Cohen <justincohen@google.com>
2024-01-25 22:04:34 +0000

    ios: Use raw_ptr for ios/ directory for iOS builds.
    This CL was uploaded by git cl split.
    
    R=wanderview@chromium.org
    
    Change-Id: Ia6da88a28eec514964d839f790951b3cb4b67570
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5234698
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Matt Reichhoff <mreichhoff@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252353}

--- ios/chrome/browser/plus_addresses/coordinator/plus_address_bottom_sheet_mediator.mm
+++ ios/chrome/browser/plus_addresses/coordinator/plus_address_bottom_sheet_mediator.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/plus_addresses/coordinator/plus_address_bottom_sheet_mediator.h"
 
 #import "base/functional/bind.h"
+#import "base/memory/raw_ptr.h"
 #import "base/strings/sys_string_conversions.h"
 #import "components/plus_addresses/plus_address_metrics.h"
 #import "components/plus_addresses/plus_address_service.h"
@@ -17,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation PlusAddressBottomSheetMediator {
   // The service implementation that owns the data.
-  plus_addresses::PlusAddressService* _plusAddressService;
+  raw_ptr<plus_addresses::PlusAddressService> _plusAddressService;
   // The origin to which all operations should be scoped.
   url::Origin _mainFrameOrigin;
   // The autofill callback to be run if the process completes via confirmation
