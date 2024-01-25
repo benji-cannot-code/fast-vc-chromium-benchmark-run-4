01c5151822774824d67eee51de6e9985f55f8b3e
Eriko Kurimoto <elkurin@chromium.org>
2024-01-25 03:20:12 +0000

    Revert "Have TrustedVaultEncryptionKeysTabHelper provide keys to the EnclaveManager."
    This reverts commit 9c2a1f618458a9f12962a5e0af76dd7dfd6170ee.
    
    Reason for revert: Compile failure on lacros-amd64
    https://ci.chromium.org/ui/p/chromium/builders/ci/lacros-amd64-generic-rel/57523/overview
    
    Original change's description:
    > Have TrustedVaultEncryptionKeysTabHelper provide keys to the EnclaveManager.
    >
    > When recovering the passkeys security domain on desktop, the keys should
    > be provided to the EnclaveManager because it'll need to wrap them with
    > the enclave. This change adds a diversion to
    > TrustedVaultEncryptionKeysTabHelper to achieve this, which seems cleaner
    > than implementing a TrustedVaultClient just to get the `StoreKeys` call.
    >
    > Bug: 1459620
    > Change-Id: I9876970d85f6e2d4bc5b03d6c6d35c6edfe1573f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5216787
    > Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    > Commit-Queue: Adam Langley <agl@chromium.org>
    > Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1251832}
    
    Bug: 1459620
    Change-Id: Ibfd4bf0281e96169e4ee9eb18322189cf26f343d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5232771
    Auto-Submit: Eriko Kurimoto <elkurin@chromium.org>
    Owners-Override: Eriko Kurimoto <elkurin@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251879}

--- chrome/browser/trusted_vault/trusted_vault_encryption_keys_tab_helper.h
+++ chrome/browser/trusted_vault/trusted_vault_encryption_keys_tab_helper.h
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 
-class EnclaveManager;
-
 namespace content {
 class RenderFrameHost;
 class WebContents;
@@ -58,17 +56,14 @@ class TrustedVaultEncryptionKeysTabHelper
       TrustedVaultEncryptionKeysTabHelper>;
 
   // Null `trusted_vault_service_` is interpreted as incognito (when it comes to
-  // metrics). Null `enclave_manager_` means that no passkeys enclave service is
-  // active.
+  // metrics).
   TrustedVaultEncryptionKeysTabHelper(
       content::WebContents* web_contents,
-      trusted_vault::TrustedVaultService* trusted_vault_service,
-      EnclaveManager* enclave_manager);
+      trusted_vault::TrustedVaultService* trusted_vault_service);
 
   // Null `trusted_vault_service_` is interpreted as incognito (when it comes to
   // metrics).
   const raw_ptr<trusted_vault::TrustedVaultService> trusted_vault_service_;
-  const raw_ptr<EnclaveManager> enclave_manager_;
 
   WEB_CONTENTS_USER_DATA_KEY_DECL();
 };
