ad08de064a27aaefd567908becf0f4562b70bf25
Elly <ellyjones@chromium.org>
2024-01-25 00:44:17 +0000

    tabs: log version of saved tab state at deserialization
    This will allow us to validate whether the old version 0 and version 1
    deserialization logic is indeed dead code.
    
    Bug: 1520963
    Change-Id: I7b18de6f5f0894087299a8373d02a58136e0529b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5230738
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251826}

--- chrome/browser/tab/web_contents_state.cc
+++ chrome/browser/tab/web_contents_state.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/pickle.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -268,6 +269,12 @@ bool ExtractNavigationEntries(
                << ").";
     return false;
   }
+
+  // TODO(https://crbug.com/1520963): Remove this once we have enough data to
+  // conclude whether V0 and V1 are still used.
+  constexpr size_t kHighestVersion = 3;
+  UMA_HISTOGRAM_EXACT_LINEAR("Android.WebContentsState.SavedStateVersion",
+                             saved_state_version, kHighestVersion);
 
   if (!saved_state_version) {
     // When |saved_state_version| is 0, it predates our notion of each tab
