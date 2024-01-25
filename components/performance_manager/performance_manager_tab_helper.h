e8bb9035ccbcb1a51667f70bf75ad0cd1c48c5a9
Joe Mason <joenotcharles@google.com>
2024-01-25 20:33:36 +0000

    [PM] Make WebContentsProxy a simple WeakPtr<WebContents> wrapper
    This removes some complexity from PerformanceManagerTabHelper that's
    no longer needed.
    
    * Remove ResourceCoordinatorSignalObserver::GetContentsForDispatch,
      which has no callers.
    * Remove WebContentsProxy::LastNavigationId and
      WebContentsProxy::LastNewDocNavigationId, which have no callers
      except GetContentsForDispatch.
    * Since PerformanceManagerTabHelper no longer needs to implement
      LastNavigationId and LastNewDocNavigationId, it no longer needs to
      be a WebContentsProxyImpl and pass WebContentsProxy a WeakPtr to
      itself. Instead it can pass WebContentsProxy a WeakPtr to the
      WebContents.
    
    Bug: 1134162
    Change-Id: I7210687d7ef9a114dd24bdf86468388b563f084b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5232523
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252304}

--- components/performance_manager/performance_manager_tab_helper.h
+++ components/performance_manager/performance_manager_tab_helper.h
@@ -13,11 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_set.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
-#include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "components/performance_manager/graph/page_node_impl.h"
 #include "components/performance_manager/public/mojom/coordination_unit.mojom-forward.h"
-#include "components/performance_manager/web_contents_proxy_impl.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
@@ -34,8 +32,7 @@ class FrameNodeImpl;
 // host to the frame graph entity.
 class PerformanceManagerTabHelper
     : public content::WebContentsObserver,
-      public content::WebContentsUserData<PerformanceManagerTabHelper>,
-      public WebContentsProxyImpl {
+      public content::WebContentsUserData<PerformanceManagerTabHelper> {
  public:
   // Observer interface to be notified when a PerformanceManagerTabHelper is
   // being teared down.
@@ -103,13 +100,6 @@ class PerformanceManagerTabHelper
       content::RenderWidgetHost* render_widget_host) override;
   void AboutToBeDiscarded(content::WebContents* new_contents) override;
 
-  // WebContentsProxyImpl overrides. Note that `LastNavigationId()` and
-  // `LastNewDocNavigationId()` refer to navigations associated with the
-  // primary page.
-  content::WebContents* GetWebContents() const override;
-  int64_t LastNavigationId() const override;
-  int64_t LastNewDocNavigationId() const override;
-
   void BindDocumentCoordinationUnit(
       content::RenderFrameHost* render_frame_host,
       mojo::PendingReceiver<mojom::DocumentCoordinationUnit> receiver);
@@ -133,7 +123,6 @@ class PerformanceManagerTabHelper
  private:
   friend class content::WebContentsUserData<PerformanceManagerTabHelper>;
   friend class PerformanceManagerRegistryImpl;
-  friend class WebContentsProxyImpl;
   FRIEND_TEST_ALL_PREFIXES(PerformanceManagerFencedFrameBrowserTest,
                            FencedFrameDoesNotHaveParentFrameNode);
 
@@ -143,7 +132,7 @@ class PerformanceManagerTabHelper
   // PerformanceManagerRegistry.
   using WebContentsUserData<PerformanceManagerTabHelper>::CreateForWebContents;
 
-  void OnMainFrameNavigation(int64_t navigation_id, bool same_doc);
+  void OnMainFrameNavigation(int64_t navigation_id);
 
   // Returns the FrameNodeImpl* associated with `render_frame_host`. This
   // CHECKs that it exists.
@@ -167,14 +156,6 @@ class PerformanceManagerTabHelper
     // is always supposed to happen.
     bool first_time_favicon_set = false;
     bool first_time_title_set = false;
-
-    // The last navigation ID that was committed to a main frame in this web
-    // contents.
-    int64_t last_navigation_id = 0;
-    // Similar to the above, but for the last non same-document navigation
-    // associated with this WebContents. This is always for a navigation that is
-    // older or equal to |last_navigation_id_|.
-    int64_t last_new_doc_navigation_id = 0;
   };
 
   // A transparent comparator for PageData. These are keyed by
@@ -226,8 +207,6 @@ class PerformanceManagerTabHelper
   base::ObserverList<Observer, true, false> observers_;
 
   WEB_CONTENTS_USER_DATA_KEY_DECL();
-
-  base::WeakPtrFactory<PerformanceManagerTabHelper> weak_factory_{this};
 };
 
 }  // namespace performance_manager
