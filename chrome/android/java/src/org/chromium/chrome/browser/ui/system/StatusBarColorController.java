32e703119af952fb0ba3dc0ee9e7509cb1e5cb84
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2024-01-25 09:43:53 +0000

    Revert "Add support for light/dark status bar icons in Android S+"
    This reverts commit e3a4a2c0f5f489311275f673e18a70518fd9ce19.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/6024618986962944
    
    Sample build with failed test: https://ci.chromium.org/b/8757955948160978913
    Affected test(s):
    [ninja://chrome/android:chrome_public_test_apk/org.chromium.chrome.browser.webapps.WebApkIntegrationTest#testDeepLink](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Fandroid:chrome_public_test_apk%2Forg.chromium.chrome.browser.webapps.WebApkIntegrationTest%23testDeepLink?q=VHash%3A359dcabdb2bd6ffc)
    [ninja://chrome/android:chrome_public_test_apk/org.chromium.chrome.browser.webapps.WebApkIntegrationTest#testWebApkTrampoline](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Fandroid:chrome_public_test_apk%2Forg.chromium.chrome.browser.webapps.WebApkIntegrationTest%23testWebApkTrampoline?q=VHash%3A359dcabdb2bd6ffc)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F6024618986962944&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F5230724&type=BUG
    
    Original change's description:
    > Add support for light/dark status bar icons in Android S+
    >
    > View#setSystemUiVisibility is deprecated for S+. This cl migrates to
    > WindowInsetsControllerCompat, which maps directly to
    > View#setSystemUiVisibility for R- https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:core/core/src/main/java/androidx/core/view/WindowInsetsControllerCompat.java;l=648;drc=488d18612e2bbb401ec1762209febc5200743857
    >
    > Fixed: 1521104
    > Change-Id: Ibb313712216e57e8420db05d8e0d4aa8c473e76e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5230724
    > Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    > Commit-Queue: Geoff Huang <geoffhuang@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1251894}
    >
    
    Change-Id: Ia6292ad4f50328d27f585deb3488898d598bfccb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5231636
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Owners-Override: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251972}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/system/StatusBarColorController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/system/StatusBarColorController.java
@@ -520,8 +520,9 @@ public class StatusBarColorController
     public static void setStatusBarColor(Window window, @ColorInt int color) {
         if (UiUtils.isSystemUiThemingDisabled()) return;
 
+        final View root = window.getDecorView().getRootView();
         boolean needsDarkStatusBarIcons = !ColorUtils.shouldUseLightForegroundOnBackground(color);
-        UiUtils.setStatusBarIconColor(window, needsDarkStatusBarIcons);
+        UiUtils.setStatusBarIconColor(root, needsDarkStatusBarIcons);
         UiUtils.setStatusBarColor(window, color);
     }
 
