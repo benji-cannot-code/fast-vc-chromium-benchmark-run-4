a567e7abffab9943ec0d68045c21918591da779c
Joemer Ramos <joemerramos@chromium.org>
2024-01-25 10:56:03 +0000

    [iOS] Cleanup iOS 15 Guard in UMACoordinator
    Bug: 1418068
    Change-Id: I4e96c41b83495acacfdeb624a3ec868d8929eddd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5229838
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252001}

--- ios/chrome/browser/ui/first_run/uma/uma_coordinator.mm
+++ ios/chrome/browser/ui/first_run/uma/uma_coordinator.mm
@@ -43,15 +43,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       initWithRootViewController:self.viewController];
   navigationController.modalPresentationStyle = UIModalPresentationFormSheet;
   navigationController.presentationController.delegate = self;
-  if (@available(iOS 15, *)) {
-    UISheetPresentationController* presentationController =
-        navigationController.sheetPresentationController;
-    presentationController.prefersEdgeAttachedInCompactHeight = YES;
-    presentationController.detents = @[
-      UISheetPresentationControllerDetent.mediumDetent,
-      UISheetPresentationControllerDetent.largeDetent
-    ];
-  }
+  UISheetPresentationController* presentationController =
+      navigationController.sheetPresentationController;
+  presentationController.prefersEdgeAttachedInCompactHeight = YES;
+  presentationController.detents = @[
+    UISheetPresentationControllerDetent.mediumDetent,
+    UISheetPresentationControllerDetent.largeDetent
+  ];
   [self.baseViewController presentViewController:navigationController
                                         animated:YES
                                       completion:nil];
