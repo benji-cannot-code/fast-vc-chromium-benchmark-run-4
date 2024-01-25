fec91258580062d05a611af94ac4462f7a5754fe
sisidovski <sisidovski@chromium.org>
2024-01-25 15:08:27 +0000

    Always evaluate SW routers when non-fetch sources exist
    This CL resolves the confusing behavior of Chrome that the registered
    SW routers are never evaluated when the service worker does not have
    any fetch handlers or only has empty fetch handlers.
    However, note that even after this CL, Chrome will still skip router
    evaluation when the routers only have 'fetch-event' source and the SW
    only has empty fetch handlers.
    
    Bug: 1511459
    Change-Id: I104b75441c0841e9f1c7668a7d16c8530c4e407f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5119144
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252082}

--- content/browser/service_worker/service_worker_version.cc
+++ content/browser/service_worker/service_worker_version.cc
@@ -3069,6 +3069,13 @@ bool ServiceWorkerVersion::IsStaticRouterEnabled() {
   return false;
 }
 
+bool ServiceWorkerVersion::HasRouterWithNonFetchEventSource() const {
+  if (!router_evaluator_) {
+    return false;
+  }
+  return router_evaluator_->has_non_fetch_event_source();
+}
+
 void ServiceWorkerVersion::GetAssociatedInterface(
     const std::string& name,
     mojo::PendingAssociatedReceiver<blink::mojom::AssociatedInterface>
--- content/common/service_worker/service_worker_router_evaluator.h
+++ content/common/service_worker/service_worker_router_evaluator.h
@@ -45,6 +45,9 @@ class CONTENT_EXPORT ServiceWorkerRouterEvaluator {
 
   const blink::ServiceWorkerRouterRules& rules() const { return rules_; }
   bool need_running_status() const { return need_running_status_; }
+  bool has_non_fetch_event_source() const {
+    return has_non_fetch_event_source_;
+  }
 
   base::Value ToValue() const;
   std::string ToString() const;
@@ -60,6 +63,7 @@ class CONTENT_EXPORT ServiceWorkerRouterEvaluator {
   std::vector<std::unique_ptr<RouterRule>> compiled_rules_;
   bool is_valid_ = false;
   bool need_running_status_ = false;
+  bool has_non_fetch_event_source_ = false;
 };
 
 }  // namespace content
