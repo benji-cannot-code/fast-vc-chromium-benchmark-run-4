8f867238c8238aa194f6227a480c1874b6c00901
Ying Hsu <yinghsu@chromium.org>
2024-01-25 01:10:31 +0000

    [floss] Add IsExtendedAdvertisementAvailable API
    This patch introduces a new API IsExtendedAdvertisementAvailable and
    integrates it with the Floss D-Bus interface.
    It allows applications to determine whether the underlying Bluetooth
    controller supports extended advertising features.
    
    BUG=b:310269227
    TEST=manual test on brya
    TEST=autoninja -C out/Default/ device_unittest
    
    Change-Id: If629f1253c4549f25a9907bb3290caaaa76ab66d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5214832
    Commit-Queue: Ying Hsu <yinghsu@chromium.org>
    Reviewed-by: Katherine Lai <laikatherine@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251838}

--- device/bluetooth/test/fake_central.cc
+++ device/bluetooth/test/fake_central.cc
@@ -595,6 +595,13 @@ void FakeCentral::RegisterAdvertisement(
   NOTREACHED();
 }
 
+#if BUILDFLAG(IS_CHROMEOS)
+bool FakeCentral::IsExtendedAdvertisementsAvailable() const {
+  NOTREACHED();
+  return false;
+}
+#endif
+
 #if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
 void FakeCentral::SetAdvertisingInterval(
     const base::TimeDelta& min,
