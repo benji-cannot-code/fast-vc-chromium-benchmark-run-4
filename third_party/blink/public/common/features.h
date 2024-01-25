c4daedd0f0ed0f9ed0273202a53a674b31aae2c8
Minoru Chikamune <chikamune@chromium.org>
2024-01-25 00:03:06 +0000

    Revert "Warm-up service worker on idle timeout"
    This reverts commit f31708b80fff7cba9ab779a1d9b4d771c5b5a552.
    
    Reason for revert: https://crrev.com/c/5232453
    
    Original change's description:
    > Warm-up service worker on idle timeout
    >
    > Before this CL, whenever the service worker stops, the service worker
    > automatically warms up unconditionally. This CL changes this
    > behavior. After this CL, the service worker will warm up only when the
    > worker stops with idle timeout.
    >
    > Bug: 1431792
    > Change-Id: I94420766d3f9748775c942bb20694375b7e18785
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5225060
    > Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    > Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    > Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    > Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1250682}
    
    Bug: 1431792
    Change-Id: I8f0dfc3c9000bebd317f73e43d1756a1872aaff5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5233864
    Owners-Override: Srinivas Sista <srinivassista@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Srinivas Sista <srinivassista@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251804}

--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -1315,7 +1315,7 @@ BLINK_COMMON_EXPORT extern const base::FeatureParam<bool>
 BLINK_COMMON_EXPORT extern const base::FeatureParam<bool>
     kSpeculativeServiceWorkerWarmUpFromLoadingPredictor;
 BLINK_COMMON_EXPORT extern const base::FeatureParam<bool>
-    kSpeculativeServiceWorkerWarmUpOnIdleTimeout;
+    kSpeculativeServiceWorkerWarmUpOnStopped;
 
 // Make the browser decide when to turn on the capture indicator (red button)
 BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE(
