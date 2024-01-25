1b02af00a55d5cb51ecd58b5edd7869c300a57b9
Rubin Deliallisi <rubindl@chromium.org>
2024-01-25 16:11:20 +0000

    [PG][iOS] History sync VC content
    - Add new strings and symbols.
    - Create the content of the History sync VC.
    
    Screenshot: https://crbug.com/1520481#c4
    
    Bug: 1520481
    Change-Id: I60d0e18585a0a3dcf3a458299943b7cc3be63343
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5233668
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252124}

--- ios/chrome/browser/ui/settings/privacy/privacy_guide/privacy_guide_constants.h
+++ ios/chrome/browser/ui/settings/privacy/privacy_guide/privacy_guide_constants.h
@@ -15,6 +15,9 @@ enum PrivacyGuideStepType : NSInteger {
   kPrivacyGuideHistorySyncStep,
 };
 
+// The accessibility identifier for the Privacy Guide History Sync switch.
+extern NSString* const kPrivacyGuideHistorySyncSwitchID;
+
 // The accessibility identifier for the Privacy Guide History Sync step view.
 extern NSString* const kPrivacyGuideHistorySyncViewID;
 
--- ios/chrome/browser/ui/settings/privacy/privacy_guide/privacy_guide_constants.mm
+++ ios/chrome/browser/ui/settings/privacy/privacy_guide/privacy_guide_constants.mm
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+NSString* const kPrivacyGuideHistorySyncSwitchID =
+    @"kPrivacyGuideHistorySyncSwitchID";
 NSString* const kPrivacyGuideHistorySyncViewID =
     @"kPrivacyGuideHistorySyncViewID";
 NSString* const kPrivacyGuideNavigationBarViewID =
