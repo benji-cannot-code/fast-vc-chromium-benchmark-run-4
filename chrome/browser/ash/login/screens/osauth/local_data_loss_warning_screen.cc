9c589889cdcb260bc3e5939398be1ef327cc6465
Istvan Nagy <iscsi@google.com>
2024-01-25 14:20:21 +0000

    Fix local data loss warning on user action condition.
    Bug: b:301834237
    Change-Id: I61c11b20016b8c75fb30890016a351cf32d63133
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5234143
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1252060}

--- chrome/browser/ash/login/screens/osauth/local_data_loss_warning_screen.cc
+++ chrome/browser/ash/login/screens/osauth/local_data_loss_warning_screen.cc
@@ -80,7 +80,7 @@ void LocalDataLossWarningScreen::OnUserAction(const base::Value::List& args) {
                        weak_factory_.GetWeakPtr()));
     return;
   } else if (action_id == kUserActionPowerwash) {
-    if (isOwner(context()->user_context->GetAccountId())) {
+    if (!isOwner(context()->user_context->GetAccountId())) {
       LOG(ERROR) << "Non owner user requesting powerwash, ignoring";
       return;
     }
