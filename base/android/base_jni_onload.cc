991e87855e1e2f22d0d15d2829a111c8073c45a6
Sam Maier <smaier@chromium.org>
2024-01-25 17:10:46 +0000

    jni_zero: Moving GetClass and related functions out of base
    Bug: 1377351
    Change-Id: I5b3965b8f7d4f62269d3c16a57b81551a7310e75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5234469
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252162}

--- base/android/base_jni_onload.cc
+++ base/android/base_jni_onload.cc
@@ -15,8 +15,6 @@ namespace android {
 
 bool OnJNIOnLoadInit() {
   InitAtExitManager();
-  JNIEnv* env = base::android::AttachCurrentThread();
-  base::android::InitGlobalClassLoader(env);
   return true;
 }
 
