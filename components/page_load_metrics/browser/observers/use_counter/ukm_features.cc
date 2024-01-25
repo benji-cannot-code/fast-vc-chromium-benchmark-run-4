bcf57549e20516eafe74a37e802f8b08a4c63917
Kara Erickson <karakara@google.com>
2024-01-25 19:46:39 +0000

    Add feature detection markers for Next.js third-party wrappers
    This change adds feature usage markers for a few Next.js features so
    we can track prformance outcomes.
    (context: third_party/blink/renderer/core/timing/README.md)
    
    add feature detection markers for Next.js third party wrappers
    
    Change-Id: Iaf959c05029fd7613e929e940c29679c77a26eff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5231898
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Commit-Queue: Kara Erickson <karakara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1252279}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -380,6 +380,10 @@ UseCounterMetricsRecorder::GetAllowedUkmFeatures() {
               kThirdPartyCookieDeprecation_AllowByEnterprisePolicyCookieAllowedForUrls,
           WebFeature::kUserFeatureNgAfterRender,
           WebFeature::kUserFeatureNgHydration,
+          WebFeature::kUserFeatureNextThirdPartiesGA,
+          WebFeature::kUserFeatureNextThirdPartiesGTM,
+          WebFeature::kUserFeatureNextThirdPartiesYouTubeEmbed,
+          WebFeature::kUserFeatureNextThirdPartiesGoogleMapsEmbed,
       }));
   return *opt_in_features;
 }
