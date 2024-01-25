2aa32a6da69d202f271b21479992a116764afee7
Xianzhu Wang <wangxianzhu@chromium.org>
2024-01-25 19:03:36 +0000

    [IntersectionOptimization] Add debug information in CHECK failure
    Bug: 1519303
    Change-Id: Ie08809671bb36222411f7e93de686b8002404bbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5235596
    Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252249}

--- third_party/blink/renderer/core/intersection_observer/intersection_observation.cc
+++ third_party/blink/renderer/core/intersection_observer/intersection_observation.cc
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_view.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
 
-#define CHECK_SKIPPED_UPDATE_ON_SCROLL DCHECK_IS_ON()
-
 namespace blink {
 
 namespace {
@@ -59,13 +57,13 @@ int64_t IntersectionObservation::ComputeIntersection(
   if (MaybeDelayAndReschedule(compute_flags, timestamp))
     return 0;
 
-#if CHECK_SKIPPED_UPDATE_ON_SCROLL
+#if CHECK_SKIPPED_UPDATE_ON_SCROLL()
   std::optional<IntersectionGeometry::CachedRects> cached_rects_backup;
 #endif
   if (RuntimeEnabledFeatures::IntersectionOptimizationEnabled() &&
       cached_rects_.valid && cached_rects_.min_scroll_delta_to_update.x() > 0 &&
       cached_rects_.min_scroll_delta_to_update.y() > 0) {
-#if CHECK_SKIPPED_UPDATE_ON_SCROLL
+#if CHECK_SKIPPED_UPDATE_ON_SCROLL()
     cached_rects_backup.emplace(cached_rects_);
 #else
     return 0;
@@ -78,12 +76,14 @@ int64_t IntersectionObservation::ComputeIntersection(
       observer_->thresholds(), observer_->TargetMargin(),
       observer_->ScrollMargin(), geometry_flags, root_geometry, &cached_rects_);
 
-#if CHECK_SKIPPED_UPDATE_ON_SCROLL
+#if CHECK_SKIPPED_UPDATE_ON_SCROLL()
   if (cached_rects_backup) {
     // A skipped update on scroll should generate the same result.
-    cached_rects_ = cached_rects_backup.value();
-    CHECK_EQ(last_threshold_index_, geometry.ThresholdIndex());
+    CHECK_EQ(last_threshold_index_, geometry.ThresholdIndex())
+        << "Previous: " << cached_rects_backup->ToString()
+        << "\nNew: " << cached_rects_.ToString();
     CHECK_EQ(last_is_visible_, geometry.IsVisible());
+    cached_rects_ = cached_rects_backup.value();
     return 0;
   }
 #endif
