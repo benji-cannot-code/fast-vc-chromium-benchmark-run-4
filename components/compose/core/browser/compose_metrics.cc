b684e02e89ffd857639beb27ccc6e112c7c8cf80
Jeffrey Cohen <jeffreycohen@google.com>
2024-01-25 22:06:59 +0000

    [Compose] add close enum for cancel during wait
    Bug: 1521871
    Change-Id: Ica0db3dbb5d21193a99d53ad29542e2e88493a0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5239066
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252355}

--- components/compose/core/browser/compose_metrics.cc
+++ components/compose/core/browser/compose_metrics.cc
@@ -162,6 +162,7 @@ void LogComposeSessionCloseMetrics(ComposeSessionCloseReason reason,
     case ComposeSessionCloseReason::kCloseButtonPressed:
     case ComposeSessionCloseReason::kEndedImplicitly:
     case ComposeSessionCloseReason::kNewSessionWithSelectedText:
+    case ComposeSessionCloseReason::kCanceledBeforeResponseReceived:
       status = ".Ignored";
   }
   base::UmaHistogramCounts1000(kComposeSessionComposeCount + status,
