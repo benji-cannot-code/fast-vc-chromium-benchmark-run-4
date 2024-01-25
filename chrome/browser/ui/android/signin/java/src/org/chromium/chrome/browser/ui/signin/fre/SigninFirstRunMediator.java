5877bd3c9da73bcaec13e2cb5c5a42a19cadfab0
Tanmoy Mollik <triploblastic@chromium.org>
2024-01-25 22:49:38 +0000

    [Android] Fix crash in SigninFirstRunMediator
    The crash is due to race condition during add account flow.
    SigninFirstRunMediator may get the added account email before
    AccountManagerFacade is updated with the new accounts list.
    
    This cl makes the sign-in button a no-op if selected account
    is not available in AccountManagerFacade yet.
    
    Bug: 1519150
    Change-Id: Ifb740c8c0745f19ebc90738c6c22b8cb91c9bbcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5237013
    Reviewed-by: Samar Chehade-Lepleux <samarchehade@google.com>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1252380}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/fre/SigninFirstRunMediator.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/fre/SigninFirstRunMediator.java
@@ -342,7 +342,6 @@ public class SigninFirstRunMediator
             mDelegate.advanceToNextPage();
             return;
         }
-        mModel.set(SigninFirstRunProperties.SHOW_SIGNIN_PROGRESS_SPINNER_WITH_TEXT, true);
         final SigninManager signinManager =
                 IdentityServicesProvider.get()
                         .getSigninManager(
@@ -373,8 +372,10 @@ public class SigninFirstRunMediator
                 AccountUtils.findCoreAccountInfoByEmail(
                         mAccountManagerFacade.getCoreAccountInfos().getResult(),
                         mSelectedAccountEmail);
-        assert selectedAccount != null;
-        signinManager.signin(selectedAccount, SigninAccessPoint.START_PAGE, signInCallback);
+        if (selectedAccount != null) {
+            mModel.set(SigninFirstRunProperties.SHOW_SIGNIN_PROGRESS_SPINNER_WITH_TEXT, true);
+            signinManager.signin(selectedAccount, SigninAccessPoint.START_PAGE, signInCallback);
+        }
     }
 
     /** Callback for the PropertyKey {@link SigninFirstRunProperties#ON_DISMISS_CLICKED}. */
