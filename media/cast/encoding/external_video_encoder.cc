9d3ac3751109a9d03acfaab55c1992c1e22d4b74
Hirokazu Honda <hiroh@chromium.org>
2024-01-26 07:06:51 +0000

    media/cast: Allow video encoders to drop a frame
    By dropping a frame, in the case of bitrate overshoot, a frame quality
    will be improved but also the transmission delay will be mitigated.
    It doesn't matter to drop a frame occasionally in cast mirroring.
    However, the user expectation about drop frame rate is higher in cast
    mirroring than WebRTC. I set the weaker drop frame threshold (=10),
    than WebRTC (=30).
    The drop frame feature is behind CastVideoEncoderFrameDrop and
    disabled by default.
    
    Design doc: go/frame-drop-VEA
    
    Bug: b:280363228
    Test: Cast mirroring on atlas
    
    Change-Id: I585367104d1dfbcf868b9f05a90994b9c9f33275
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5216388
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252493}

--- media/cast/encoding/external_video_encoder.cc
+++ media/cast/encoding/external_video_encoder.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/cast/common/openscreen_conversion_helpers.h"
 #include "media/cast/common/rtp_time.h"
 #include "media/cast/common/sender_encoded_frame.h"
+#include "media/cast/encoding/encoding_util.h"
 #include "media/cast/encoding/vpx_quantizer_parser.h"
 #include "media/cast/logging/logging_defines.h"
 #include "media/video/h264_parser.h"
@@ -154,6 +155,7 @@ class ExternalVideoEncoder::VEAClientImpl final
         media::PIXEL_FORMAT_I420, frame_size, codec_profile, bitrate);
     config.content_type =
         media::VideoEncodeAccelerator::Config::ContentType::kDisplay;
+    config.drop_frame_thresh_percentage = GetEncoderDropFrameThreshold();
     encoder_active_ = video_encode_accelerator_->Initialize(
         config, this, std::make_unique<media::NullMediaLog>());
     next_frame_id_ = first_frame_id;
@@ -338,9 +340,30 @@ class ExternalVideoEncoder::VEAClientImpl final
                              base::NumberToString(bitstream_buffer_id)});
       return;
     }
-    const char* output_buffer_memory = output_buffers_[bitstream_buffer_id]
-                                           .second.GetMemoryAsSpan<char>()
-                                           .data();
+
+    if (metadata.payload_size_bytes == 0) {
+      CHECK(key_frame_encountered_);
+      // The encoder drops a frame.
+      InProgressExternalVideoFrameEncode& request =
+          in_progress_frame_encodes_.front();
+      cast_environment_->PostTask(
+          CastEnvironment::MAIN, FROM_HERE,
+          base::BindOnce(std::move(request.frame_encoded_callback), nullptr));
+      in_progress_frame_encodes_.pop_front();
+      if (encoder_active_) {
+        video_encode_accelerator_->UseOutputBitstreamBuffer(
+            media::BitstreamBuffer(
+                bitstream_buffer_id,
+                output_buffers_[bitstream_buffer_id].first.Duplicate(),
+                output_buffers_[bitstream_buffer_id].first.GetSize()));
+      }
+      return;
+    }
+
+    const char* output_buffer_memory =
+        output_buffers_[bitstream_buffer_id]
+            .second.GetMemoryAsSpan<char>(metadata.payload_size_bytes)
+            .data();
     if (metadata.payload_size_bytes >
         output_buffers_[bitstream_buffer_id].second.size()) {
       NotifyErrorStatus(
@@ -388,7 +411,6 @@ class ExternalVideoEncoder::VEAClientImpl final
       }
       encoded_frame->data.append(output_buffer_memory,
                                  metadata.payload_size_bytes);
-      DCHECK(!encoded_frame->data.empty()) << "BUG: Encoder must provide data.";
 
       // If FRAME_DURATION metadata was provided in the source VideoFrame,
       // compute the utilization metrics.
--- third_party/blink/renderer/platform/peerconnection/rtc_video_encoder_test.cc
+++ third_party/blink/renderer/platform/peerconnection/rtc_video_encoder_test.cc
@@ -754,7 +754,7 @@ class RTCVideoEncoderEncodeTest
       disabled_features.push_back(
           features::kWebRtcInitializeEncoderOnFirstFrame);
     }
-    enabled_features.push_back(media::kVideoEncoderFrameDrop);
+    enabled_features.push_back(media::kWebRTCHardwareVideoEncoderFrameDrop);
     feature_list_.InitWithFeatures(enabled_features, disabled_features);
   }
 
