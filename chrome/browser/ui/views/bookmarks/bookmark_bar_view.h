7253c31bc75cadd3bc00d5d7516c31703d15d59e
Robert Lin <robertlin@chromium.org>
2024-01-26 07:11:08 +0000

    Move BookmarkButton into its own file.
    This CL is created as a follow-up for
    https://chromium-review.googlesource.com/c/chromium/src/+/5224896/comments/1d29032e_b68d7f26
    
    Change-Id: I30beabc85f4d4dbf4c7979e32be0194431f88f1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5233386
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252500}

--- chrome/browser/ui/views/bookmarks/bookmark_bar_view.h
+++ chrome/browser/ui/views/bookmarks/bookmark_bar_view.h
@@ -84,6 +84,8 @@ class BookmarkBarView : public views::AccessiblePaneView,
   BookmarkBarView& operator=(const BookmarkBarView&) = delete;
   ~BookmarkBarView() override;
 
+  static bool GetAnimationsEnabled();
+
   static void DisableAnimationsForTesting(bool disabled);
 
   // Returns the current browser.
