f903903827a7d24392adf3e887edec92de1268f5
Dale Curtis <dalecurtis@chromium.org>
2024-01-26 06:58:44 +0000

    Cleanup, optimize, and improve SoftwareVideoEncoderTest.
    This improves memory pressure and switches the test to use a real
    four colors frame instead of single color. Creating the four colors
    frame is more expensive, so tests are optimized to improve frame
    pool reuse. The new version is faster than the old by a hair (~2%).
    
    As part of this the code for generating a four colors test frame
    is moved to our media test helpers pass w/ a new feature: optional
    color mask so variants of the four color frame can be produced.
    
    This is all in prep of adding 4:4:4 encoding tests for vp9, av1.
    
    R=eugene
    
    Bug: 1378115
    Change-Id: Ic0004160a606602e95454df693e412906f4d5600
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5239212
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252478}

--- media/base/test_helpers.h
+++ media/base/test_helpers.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/sample_format.h"
 #include "media/base/status.h"
 #include "media/base/video_decoder_config.h"
+#include "media/base/video_frame.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "ui/gfx/geometry/size.h"
 
@@ -250,6 +251,17 @@ bool VerifyFakeVideoBufferForTest(const DecoderBuffer& buffer,
 // Create a MockDemuxerStream for testing purposes.
 std::unique_ptr<::testing::StrictMock<MockDemuxerStream>>
 CreateMockDemuxerStream(DemuxerStream::Type type, bool encrypted);
+
+// Fills `dest_frame` with a four color frame which looks like:
+//
+//   YYYYRRRR
+//   BBBBGGGG
+//
+// Supports 8-bit xRGB, BGRx, NV12x, and I4xxx formats. If `xor_mask` is
+// provided the standard four colors will be XOR'd with the provided 00RRGGBB
+// value (alpha value must be zero).
+void FillFourColors(VideoFrame& dest_frame,
+                    absl::optional<uint32_t> xor_mask = absl::nullopt);
 
 // Compares two media::Status by StatusCode only.  Also allows the ok helper to
 // match kOk.  It's a special case because we don't know the TypedStatus traits
