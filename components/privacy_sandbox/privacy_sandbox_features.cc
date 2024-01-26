ca7b8a575e1bbc16c4751d66d5cf96557569028a
Fiona Macintosh <fmacintosh@google.com>
2024-01-26 17:22:00 +0000

    Add flag + feature for cookie settings UI alignment
    Bug: b:320711396
    Change-Id: I5862d214be4bd9f8b322e9db308baeab23d38656
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5206252
    Reviewed-by: Kevin Graney <kmg@google.com>
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Kevin Graney <kmg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1252708}

--- components/privacy_sandbox/privacy_sandbox_features.cc
+++ components/privacy_sandbox/privacy_sandbox_features.cc
@@ -159,6 +159,10 @@ BASE_FEATURE(kIpProtectionV1,
              "IpProtectionV1",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kCookieSettingsUiAlignment,
+             "CookieSettingsUiAlignment",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 #if BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kTrackingProtectionNoticeRequestTracking,
              "TrackingProtectionNoticeRequestTracking",
