f4252bbb3faea7d9194b5a3bea2bb112ee1a5ba9
Minoru Chikamune <chikamune@chromium.org>
2024-01-26 10:04:37 +0000

    Reland "Bugfix: Stop the warmed-up service worker correctly"
    This is a reland of commit 73272464b7e9ca8c25fd751e4309cce8eab6b3d3
    
    Before this CL, when the warmed-up service worker is stopping, we run
    all start_callbacks_ to tell that we don't need to restart the service
    worker. But it caused crashes because
    scoped_refptr<ServiceWorkerRegistration> were dereferenced from
    start_callbacks_. I'm still not sure why start_callbacks_ is the only
    reference of the registration.
    
    This CL is a speculative fix of this crash by delaying running
    start_callbacks_ until FinishStartWorker() is called in
    ServiceWorkerVersion::OnStoppedInternal().
    
    Original change's description:
    > Bugfix: Stop the warmed-up service worker correctly
    >
    > Before this CL, when the warmed-up service worker is requested to stop,
    > the service worker surprisingly starts instead of stopping. This CL
    > fixes this bug.
    >
    > The bug was located in the ServiceWorkerVersion. Before this CL,
    > start_callbacks_ contained remaining callbacks while the service worker
    > was warming up or warmed up.  These remaining callbacks are checked in
    > ServiceWorkerVersion::OnStoppedInternal() like the following.
    >
    >  bool should_restart = !start_callbacks_.empty();
    >
    > The should_restart flag becomes true, and the service worker is
    > restarted. This CL fixes this problem by flushing callbacks beforehand.
    >
    > Bug: 1431792
    > Change-Id: Id9a6f143ab7c93178f7183e6ed0509c486dda9c2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5224881
    > Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    > Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    > Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1250648}
    
    Bug: 1521360
    Bug: 1431792
    Change-Id: I33ff2af85e4e1f1e6937c3c9cdedadf4dacb4b5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5238125
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252513}

--- content/browser/service_worker/service_worker_test_utils.h
+++ content/browser/service_worker/service_worker_test_utils.h
@@ -37,6 +37,7 @@ class StorageKey;
 namespace content {
 
 class EmbeddedWorkerTestHelper;
+class ServiceWorkerContext;
 class ServiceWorkerContextCore;
 class ServiceWorkerHost;
 class ServiceWorkerRegistry;
@@ -48,6 +49,9 @@ ReceiveServiceWorkerStatus(std::optional<blink::ServiceWorkerStatusCode>* out,
 
 blink::ServiceWorkerStatusCode WarmUpServiceWorker(
     ServiceWorkerVersion* version);
+
+bool WarmUpServiceWorker(ServiceWorkerContext& service_worker_context,
+                         const GURL& url);
 
 blink::ServiceWorkerStatusCode StartServiceWorker(
     ServiceWorkerVersion* version);
