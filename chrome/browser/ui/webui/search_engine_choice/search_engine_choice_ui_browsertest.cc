c435924267dd1b26a30249bc93e0b2197d920f1d
Jack Yammine <jyammine@google.com>
2024-01-26 11:17:20 +0000

    [waffle]Center radio button disc inside initial cell content
    We want to center the radio button disc of each search engine in the
    cell content while excluding the space taken by the marketing
    snippet.
    
    We also remove the flaky "ShowSearchEngineOmnibox" pixel test.
    There were 2 issues causing the test to be flaky:
    1- The fake omnibox animation that happens when we click on a
    radio button is causing some flakiness. This issue can be solved
    by setting the animation duration to 0s in the test.
    2- Clicking on a radio button causes some type of animation to
    show the user that the button is clicked. The animation is that a
    small layer of the color blue covers the radio button for a small
    period of time. We can't disable this in the pixel test.
    
    Fixed: 1521321, b:320627854
    Change-Id: If25eb76d938c457d215f6f0d36e7cfb264b2f0d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5233490
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252554}

--- chrome/browser/ui/webui/search_engine_choice/search_engine_choice_ui_browsertest.cc
+++ chrome/browser/ui/webui/search_engine_choice/search_engine_choice_ui_browsertest.cc
@@ -94,7 +94,6 @@ struct TestParam {
   std::string test_suffix;
   bool use_dark_theme = false;
   bool use_right_to_left_language = false;
-  bool show_search_engine_omnibox = false;
   gfx::Size dialog_dimensions = gfx::Size(988, 900);
 };
 
@@ -111,8 +110,6 @@ const TestParam kTestParams[] = {
     {.test_suffix = "Default"},
     {.test_suffix = "DarkTheme", .use_dark_theme = true},
     {.test_suffix = "RightToLeft", .use_right_to_left_language = true},
-    {.test_suffix = "ShowSearchEngineOmnibox",
-     .show_search_engine_omnibox = true},
     {.test_suffix = "MediumSize", .dialog_dimensions = gfx::Size(800, 700)},
     {.test_suffix = "NarrowSize", .dialog_dimensions = gfx::Size(300, 900)},
 #endif
@@ -121,33 +118,6 @@ const TestParam kTestParams[] = {
     // The test will crash if we exceed that height.
     {.test_suffix = "ShortSize", .dialog_dimensions = gfx::Size(988, 376)},
 };
-
-class SearchEngineChoiceNavigationObserver
-    : public content::TestNavigationObserver {
- public:
-  explicit SearchEngineChoiceNavigationObserver(GURL url)
-      : content::TestNavigationObserver(url) {}
-
-  void NavigationOfInterestDidFinish(
-      content::NavigationHandle* navigation_handle) override {
-    web_contents_ = navigation_handle->GetWebContents();
-  }
-
-  content::WebContents* web_contents() const { return web_contents_; }
-
- private:
-  raw_ptr<content::WebContents> web_contents_;
-};
-
-// Click on a search engine to display the search engine omnibox.
-const char kShowSearchEngineOmniboxJsString[] =
-    "(() => {"
-    "  const app = document.querySelector('search-engine-choice-app');"
-    "  const searchEngineList = app.shadowRoot.querySelectorAll("
-    "      'cr-radio-button');"
-    "  searchEngineList[0].click();"
-    "  return true;"
-    "})();";
 }  // namespace
 
 class SearchEngineChoiceUIPixelTest
@@ -188,7 +158,7 @@ class SearchEngineChoiceUIPixelTest
         /*dialog_disabled=*/false);
 
     GURL url = GURL(chrome::kChromeUISearchEngineChoiceURL);
-    SearchEngineChoiceNavigationObserver observer(url);
+    content::TestNavigationObserver observer(url);
     observer.StartWatchingNewWebContents();
 
     views::NamedWidgetShownWaiter widget_waiter(
@@ -214,14 +184,6 @@ class SearchEngineChoiceUIPixelTest
     ShowSearchEngineChoiceDialog(
         *browser(), gfx::Size(dialog_width, dialog_height), zoom_factor);
     widget_waiter.WaitIfNeededAndGet();
-    content::WebContents* web_contents = observer.web_contents();
-    CHECK(web_contents);
-
-    if (GetParam().show_search_engine_omnibox) {
-      EXPECT_EQ(true, content::EvalJs(web_contents,
-                                      kShowSearchEngineOmniboxJsString));
-    }
-
     observer.Wait();
   }
 
