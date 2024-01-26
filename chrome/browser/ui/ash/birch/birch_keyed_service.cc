0cd4a752b09e1645232bf73d34ff7af4d66ef919
Matthew Mourgos <mmourgos@chromium.org>
2024-01-26 18:46:57 +0000

    [Birch] Add birch tab provider
    Bug: b/305093579
    
    Low-Coverage-Reason: OTHER BirchTabItem::ToString() is meant for local debugging.
    Change-Id: I2dbfaa4e995ef83e40291dc8c5785b75e0bb46ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5215665
    Commit-Queue: Matthew Mourgos <mmourgos@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252766}

--- chrome/browser/ui/ash/birch/birch_keyed_service.cc
+++ chrome/browser/ui/ash/birch/birch_keyed_service.cc
@@ -13,12 +13,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/ash/birch/birch_client_impl.h"
 #include "chrome/browser/ui/ash/birch/birch_file_suggest_provider.h"
+#include "chrome/browser/ui/ash/birch/birch_recent_tabs_provider.h"
 
 namespace ash {
 
 BirchKeyedService::BirchKeyedService(Profile* profile)
     : file_suggest_provider_(
-          std::make_unique<BirchFileSuggestProvider>(profile)) {
+          std::make_unique<BirchFileSuggestProvider>(profile)),
+      recent_tabs_provider_(
+          std::make_unique<BirchRecentTabsProvider>(profile)) {
   birch_client_impl_ = std::make_unique<BirchClientImpl>(profile);
   Shell::Get()->birch_model()->SetClient(birch_client_impl_.get());
 }
@@ -29,6 +32,7 @@ BirchKeyedService::~BirchKeyedService() {
 
 void BirchKeyedService::RequestBirchDataFetch() {
   // TODO(b/305093932): Begin data fetching requests.
+  recent_tabs_provider_->GetRecentTabs();
 }
 
 }  // namespace ash
