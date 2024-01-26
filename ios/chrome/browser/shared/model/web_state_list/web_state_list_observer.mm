da4a9aa3fb0cddafd6622c7db7fa59e910b2b057
Louis Romero <lpromero@google.com>
2024-01-26 15:52:18 +0000

    Remove "selected" prefix in WebStateListChangeStatusOnly
    That prefix didn’t help, as selected doesn't have a meaning here
    (evenmoreso, selected WebState does have a meaning in tab switchers for
    example).
    
    Fixed: 1522138
    Change-Id: Iae7745c4fb1bc31a9ad3580287b77fa88da9cbce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5236999
    Commit-Queue: Louis Romero <lpromero@google.com>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252652}

--- ios/chrome/browser/shared/model/web_state_list/web_state_list_observer.mm
+++ ios/chrome/browser/shared/model/web_state_list/web_state_list_observer.mm
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/check.h"
 
 WebStateListChangeStatusOnly::WebStateListChangeStatusOnly(
-    raw_ptr<web::WebState> selected_web_state)
-    : selected_web_state_(selected_web_state) {}
+    raw_ptr<web::WebState> web_state)
+    : web_state_(web_state) {}
 
 WebStateListChange::Type WebStateListChangeStatusOnly::type() const {
   return kType;
