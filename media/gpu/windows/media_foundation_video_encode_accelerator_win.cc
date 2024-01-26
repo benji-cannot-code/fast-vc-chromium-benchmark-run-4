2e46c3e89e1a9f3b1f9985ecea69b00c5453bb37
Eugene Zemtsov <eugene@chromium.org>
2024-01-26 01:59:12 +0000

    webcodecs: Disable accelerated video encoding on arm64 win
    It looks like OS reports all codecs as supported even though they're not.
    
    Bug: 1509117
    Change-Id: I59e7ae86f7d627796aa570e944052bb534ae725b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5113442
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252437}

--- media/gpu/windows/media_foundation_video_encode_accelerator_win.cc
+++ media/gpu/windows/media_foundation_video_encode_accelerator_win.cc
@@ -304,7 +304,11 @@ uint32_t EnumerateHardwareEncoders(VideoCodec codec, IMFActivate*** activates) {
   if (!InitializeMediaFoundation()) {
     return 0;
   }
-
+#if defined(ARCH_CPU_ARM64)
+  // TODO (crbug.com/1509117): Temporarily disable video encoding on arm64
+  // until we figure out what OS reports all codecs as supported.
+  return 0;
+#else
   uint32_t flags = MFT_ENUM_FLAG_HARDWARE | MFT_ENUM_FLAG_SORTANDFILTER;
   MFT_REGISTER_TYPE_INFO input_info;
   input_info.guidMajorType = MFMediaType_Video;
@@ -333,6 +337,7 @@ uint32_t EnumerateHardwareEncoders(VideoCodec codec, IMFActivate*** activates) {
   }
 
   return count - excluded_encoders;
+#endif
 }
 
 bool IsCodecSupportedForEncoding(VideoCodec codec, int* num_temporal_layers) {
