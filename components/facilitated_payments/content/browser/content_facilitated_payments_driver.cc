8db5ad15d1e92559d9b318c89d5805798ea2dfde
Vishwas Uppoor <vishwasuppoor@google.com>
2024-01-26 16:16:34 +0000

    [PIX] Browser architecture
    Add skeleton code for manager and driver classes to enable PIX code
    detection.
    
    Architecture 1-pager: go/pix-component-one-pager
    Design doc: go/pix-chrome-clank-dd
    
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL This CL introduces skeleton classes which are loosely coupled
    Fixed: b:316281391
    Change-Id: I87f78eb04949de26617f0af8af37e64e700e2a1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5073197
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252667}

---
+++ components/facilitated_payments/content/browser/content_facilitated_payments_driver.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/facilitated_payments/content/browser/content_facilitated_payments_driver.h"
+
+#include <memory>
+
+#include "base/functional/callback.h"
+
+namespace payments::facilitated {
+
+class FaciliatedPaymentsManager;
+
+ContentFacilitatedPaymentsDriver::ContentFacilitatedPaymentsDriver()
+    : FacilitatedPaymentsDriver(
+          std::make_unique<FacilitatedPaymentsManager>(this)) {}
+
+ContentFacilitatedPaymentsDriver::~ContentFacilitatedPaymentsDriver() = default;
+
+void ContentFacilitatedPaymentsDriver::TriggerPixCodeDetection(
+    base::OnceCallback<void(bool)> callback) const {}
+
+}  // namespace payments::facilitated
---
+++ components/facilitated_payments/core/browser/facilitated_payments_driver.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/facilitated_payments/core/browser/facilitated_payments_driver.h"
+
+#include <utility>
+
+namespace payments::facilitated {
+
+FacilitatedPaymentsDriver::FacilitatedPaymentsDriver(
+    std::unique_ptr<FacilitatedPaymentsManager> manager)
+    : manager_(std::move(manager)) {}
+
+FacilitatedPaymentsDriver::~FacilitatedPaymentsDriver() = default;
+
+void FacilitatedPaymentsDriver::DidFinishLoad(const GURL& url) const {
+  manager_->DidFinishLoad(url);
+}
+
+}  // namespace payments::facilitated
