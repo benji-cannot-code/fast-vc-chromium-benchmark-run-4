913fe8b6c7ecdfd4ac98e6eb4eda7a3c2782e141
Mohamad Ahmadi <mahmadi@chromium.org>
2024-01-26 19:42:03 +0000

    [omnibox][grouping] Fixes DCHECK when verifying SideType of Groups in Sections.
    A Section verifies at construction time whether the `Group`s in it have
    the same SideType (Primary/Secondary) as well as the `GroupId`s in a
    given `Group` have the same RenderType (Horizontal/Vertical).
    
    It is possible that Chrome may allow a Secondary SideType `GroupId` that
    the server does not always provide in its response. This can result in
    a DCHECK because the default instance of omnibox::GroupConfig returned
    from looking up omnibox::GroupConfigMap will have the default Primary
    SideType. This is fixed by skipping `GroupId`s that are not present in
    omnibox::GroupConfigMap when performing that check.
    
    Perhaps these checks can be improved in the future by making sure a
    `Section` only permits matches of the expected SideType and a `Group`
    only permits matches of the expected RenderType.
    
    This CL also makes some cosmetic changes to make the tests more readable.
    
    Bug: 1343512
    Change-Id: I8d13ab004fb63c01555b21826eccac883762c7e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5239312
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Jennifer Serrano <jennserrano@google.com>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252805}

--- components/omnibox/browser/autocomplete_grouper_sections.cc
+++ components/omnibox/browser/autocomplete_grouper_sections.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <memory>
 
+#include "base/containers/contains.h"
 #include "base/dcheck_is_on.h"
 #include "base/ranges/algorithm.h"
 #include "components/omnibox/browser/autocomplete_grouper_groups.h"
@@ -31,6 +32,10 @@ Section::Section(size_t limit,
   for (const auto& group : groups_) {
     absl::optional<omnibox::GroupConfig_RenderType> last_render_type;
     for (const auto& [group_id, _] : group.group_id_limits_and_counts()) {
+      // Ignore the `GroupId` if it is not present in the group configs.
+      if (!base::Contains(group_configs, group_id)) {
+        continue;
+      }
       const auto& group_config = group_configs[group_id];
 
       DCHECK(last_side_type.value_or(group_config.side_type()) ==
@@ -173,43 +178,57 @@ void AndroidWebZpsSection::InitFromMatches(ACMatches& matches) {
 DesktopNTPZpsSection::DesktopNTPZpsSection(
     omnibox::GroupConfigMap& group_configs)
     : ZpsSection(8,
-                 {{8, omnibox::GROUP_PERSONALIZED_ZERO_SUGGEST},
-                  {8, omnibox::GROUP_TRENDS}},
+                 {
+                     {8, omnibox::GROUP_PERSONALIZED_ZERO_SUGGEST},
+                     {8, omnibox::GROUP_TRENDS},
+                 },
                  group_configs) {}
 
 DesktopSecondaryNTPZpsSection::DesktopSecondaryNTPZpsSection(
     omnibox::GroupConfigMap& group_configs)
     : ZpsSection(3,
-                 {{3, omnibox::GROUP_PREVIOUS_SEARCH_RELATED_ENTITY_CHIPS}},
+                 {
+                     {3, omnibox::GROUP_PREVIOUS_SEARCH_RELATED_ENTITY_CHIPS},
+                 },
                  group_configs) {}
 
 DesktopSRPZpsSection::DesktopSRPZpsSection(
     omnibox::GroupConfigMap& group_configs)
     : ZpsSection(8,
-                 {{8, omnibox::GROUP_PREVIOUS_SEARCH_RELATED},
-                  {8, omnibox::GROUP_PERSONALIZED_ZERO_SUGGEST}},
+                 {
+                     {8, omnibox::GROUP_PREVIOUS_SEARCH_RELATED},
+                     {8, omnibox::GROUP_PERSONALIZED_ZERO_SUGGEST},
+                 },
                  group_configs) {}
 
 DesktopWebZpsSection::DesktopWebZpsSection(
     omnibox::GroupConfigMap& group_configs)
     : ZpsSection(8,
-                 {{8, omnibox::GROUP_VISITED_DOC_RELATED},
-                  {8, omnibox::GROUP_PERSONALIZED_ZERO_SUGGEST}},
+                 {
+                     {8, omnibox::GROUP_VISITED_DOC_RELATED},
+                     {8, omnibox::GROUP_PERSONALIZED_ZERO_SUGGEST},
+                 },
                  group_configs) {}
 
 DesktopNonZpsSection::DesktopNonZpsSection(
     omnibox::GroupConfigMap& group_configs)
     : Section(10,
-              {{1,
-                {{omnibox::GROUP_STARTER_PACK, {1}},
-                 {omnibox::GROUP_SEARCH, {1}},
-                 {omnibox::GROUP_OTHER_NAVS, {1}}},
-                /*is_default=*/true},
-               {9, omnibox::GROUP_STARTER_PACK},
-               {9,
-                {{omnibox::GROUP_SEARCH, {9}},
-                 {omnibox::GROUP_HISTORY_CLUSTER, {1}}}},
-               {7, omnibox::GROUP_OTHER_NAVS}},
+              {
+                  {1,
+                   {
+                       {omnibox::GROUP_STARTER_PACK, {1}},
+                       {omnibox::GROUP_SEARCH, {1}},
+                       {omnibox::GROUP_OTHER_NAVS, {1}},
+                   },
+                   /*is_default=*/true},
+                  {9, omnibox::GROUP_STARTER_PACK},
+                  {9,
+                   {
+                       {omnibox::GROUP_SEARCH, {9}},
+                       {omnibox::GROUP_HISTORY_CLUSTER, {1}},
+                   }},
+                  {7, omnibox::GROUP_OTHER_NAVS},
+              },
               group_configs) {}
 
 void DesktopNonZpsSection::InitFromMatches(ACMatches& matches) {
@@ -249,9 +268,11 @@ IOSNTPZpsSection::IOSNTPZpsSection(size_t max_trending_queries,
                                    omnibox::GroupConfigMap& group_configs)
     : ZpsSection(
           max_trending_queries + max_psuggest_queries + 1,
-          {{1, omnibox::GROUP_MOBILE_CLIPBOARD},
-           {max_psuggest_queries, omnibox::GROUP_PERSONALIZED_ZERO_SUGGEST},
-           {max_trending_queries, omnibox::GROUP_TRENDS}},
+          {
+              {1, omnibox::GROUP_MOBILE_CLIPBOARD},
+              {max_psuggest_queries, omnibox::GROUP_PERSONALIZED_ZERO_SUGGEST},
+              {max_trending_queries, omnibox::GROUP_TRENDS},
+          },
           group_configs) {}
 
 IOSSRPZpsSection::IOSSRPZpsSection(omnibox::GroupConfigMap& group_configs)
