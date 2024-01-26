a4523ed241871e59da49024c5b61e8626f53f7eb
Calder Kitagawa <ckitagawa@chromium.org>
2024-01-26 00:04:30 +0000

    [Hub] Add metrics to HubLayout
    Add metrics to HubLayout to match TabSwitcherLayout. Mainly TraceEvent
    and also tracking user actions when entering and exiting the Hub.
    
    The enter and exit actions are no longer split with TabSwitcherMediator
    which is a positive although is noteworthy in that the recording time
    is slightly different. It is unfortunate the enter and exit metrics
    continue to use the legacy stack names...
    
    Bug: 1521885
    Change-Id: Icac0fdf3de459f60a4523135bb533241105051f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5237989
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252411}

--- chrome/android/java/src/org/chromium/chrome/browser/hub/HubLayoutUnitTest.java
+++ chrome/android/java/src/org/chromium/chrome/browser/hub/HubLayoutUnitTest.java
@@ -62,6 +62,7 @@ import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.supplier.SyncOneshotSupplierImpl;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.JniMocker;
+import org.chromium.base.test.util.UserActionTester;
 import org.chromium.chrome.browser.browser_controls.BrowserControlsStateProvider;
 import org.chromium.chrome.browser.compositor.layouts.Layout.ViewportMode;
 import org.chromium.chrome.browser.compositor.layouts.LayoutRenderHost;
@@ -131,6 +132,8 @@ public class HubLayoutUnitTest {
     @Mock private TabModelSelector mTabModelSelector;
     @Mock private Tab mTab;
 
+    private UserActionTester mActionTester;
+
     private Activity mActivity;
     private FrameLayout mFrameLayout;
 
@@ -145,6 +148,10 @@ public class HubLayoutUnitTest {
         mJniMocker.mock(SceneLayerJni.TEST_HOOKS, mSceneLayerJni);
         mJniMocker.mock(StaticTabSceneLayerJni.TEST_HOOKS, mStaticTabSceneLayerJni);
         mJniMocker.mock(SolidColorSceneLayerJni.TEST_HOOKS, mSolidColorSceneLayerJni);
+
+        mActionTester = new UserActionTester();
+        ShadowLooper.runUiThreadTasks();
+
         when(mTabSwitcherPane.getPaneId()).thenReturn(PaneId.TAB_SWITCHER);
         when(mTabSwitcherPane.getColorScheme()).thenReturn(HubColorScheme.DEFAULT);
         when(mTabSwitcherPane.createShowHubLayoutAnimatorProvider(any()))
@@ -283,6 +290,7 @@ public class HubLayoutUnitTest {
     @After
     public void tearDown() {
         mHubLayout.destroy();
+        mActionTester.tearDown();
     }
 
     @Test
@@ -663,6 +671,7 @@ public class HubLayoutUnitTest {
         assertFalse(mHubLayout.isRunningAnimations());
         assertFalse(mHubLayout.onUpdateAnimation(FAKE_TIME, false));
         verify(mHubLayout).doneShowing();
+        assertEquals(1, mActionTester.getActionCount("MobileToolbarShowStackView"));
         verify(mTab).hide(eq(TabHidingType.TAB_SWITCHER_SHOWN));
         if (fromLayout == LayoutType.START_SURFACE) {
             verify(mScrimController).forceAnimationToFinish();
@@ -698,6 +707,7 @@ public class HubLayoutUnitTest {
         verify(mHubController, times(1)).onHubLayoutDoneHiding();
         assertEquals(0, mFrameLayout.getChildCount());
         verify(mHubLayout).doneHiding();
+        assertEquals(1, mActionTester.getActionCount("MobileExitStackView"));
 
         if (nextLayout == LayoutType.START_SURFACE) {
             verify(mScrimController).forceAnimationToFinish();
