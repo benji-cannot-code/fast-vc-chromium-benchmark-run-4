49aa540cd8671ca4c0e5757b7a4216937c8ae7e5
Adam Langley <agl@chromium.org>
2024-01-26 17:24:17 +0000

    Reland "webauthn: load enclave state for each request."
    This reverts commit c2f506919268d18edbc576caf51fc90c7b40ce06.
    
    The original was reverted because a change that it depended on was reverted because Rust FFI doesn't work with MSAN (crbug.com/1369167). That change has now stuck overnight and the MSAN bots are still green.
    
    Bug: 1459620
    Change-Id: If2c2041aa0cada840df27b4a82257132f1af48b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5240898
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252712}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -202,6 +202,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/usb/usb_chooser_context_factory.h"
 #include "chrome/browser/web_applications/isolated_web_apps/isolated_web_app_reader_registry_factory.h"
 #include "chrome/browser/web_data_service_factory.h"
+#include "chrome/browser/webauthn/enclave_manager_factory.h"
 #include "chrome/browser/webid/federated_identity_api_permission_context_factory.h"
 #include "chrome/browser/webid/federated_identity_auto_reauthn_permission_context_factory.h"
 #include "chrome/browser/webid/federated_identity_permission_context_factory.h"
@@ -759,6 +760,7 @@ void ChromeBrowserMainExtraPartsProfiles::
   DownloadCoreServiceFactory::GetInstance();
 #if !BUILDFLAG(IS_ANDROID)
   DriveServiceFactory::GetInstance();
+  EnclaveManagerFactory::GetInstance();
 #endif
   enterprise::ProfileIdServiceFactory::GetInstance();
 #if !BUILDFLAG(IS_CHROMEOS_ASH)
