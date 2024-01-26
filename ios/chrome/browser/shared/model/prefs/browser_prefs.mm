5fc1c18c3e6e391c83a3a46bc639d3831a7f81fb
Olivier ROBIN <olivierrobin@google.com>
2024-01-26 17:15:36 +0000

    Add Web annotations policy for iOS
    Create an enterprise policy to enable/disable the detection of
    different entity types.
    Each type can be enabled/disabled/put in long press only individually
    with a "default" type setting the policy for unset types.
    
    
    
    Bug: 1501811
    Change-Id: Ib10472a776869cefcbb0811f41fcd37b0dc90163
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5177002
    Reviewed-by: David Jean <djean@chromium.org>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252705}

--- ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -104,6 +104,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/ntp/metrics/feed_metrics_constants.h"
 #import "ios/chrome/browser/upgrade/model/upgrade_constants.h"
 #import "ios/chrome/browser/voice/model/voice_search_prefs_registration.h"
+#import "ios/chrome/browser/web/model/annotations/annotations_util.h"
 #import "ios/chrome/browser/web/model/font_size/font_size_tab_helper.h"
 #import "ios/components/cookie_util/cookie_constants.h"
 #import "ios/web/common/features.h"
@@ -739,6 +740,8 @@ void RegisterBrowserStatePrefs(user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterBooleanPref(prefs::kInsecureFormWarningsEnabled, true);
 
   registry->RegisterTimePref(kLastCookieDeletionDate, base::Time());
+
+  registry->RegisterDictionaryPref(prefs::kWebAnnotationsPolicy);
 }
 
 // This method should be periodically pruned of year+ old migrations.
