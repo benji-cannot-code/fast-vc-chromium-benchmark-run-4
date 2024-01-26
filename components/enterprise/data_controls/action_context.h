8a14c854940db27abf79013ebb5d3d4aeb36bd2f
Dominique Fauteux-Chapleau <domfc@chromium.org>
2024-01-26 13:32:03 +0000

    Hook "and" and "or" logic in Data Controls rules
    Follow-up to crrev.com/c/5225202
    
    This CL adds support for recursive "and"/"or" in DC rules. These new
    attributes should also work with "not" as added in crrev.com/c/5217416.
    
    Bug: b/302340176
    Change-Id: Id52a5985f9926cc36b34fff01490e17fa6709bf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5238487
    Reviewed-by: Nasser Al-shawwa <alshawwa@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252595}

--- components/enterprise/data_controls/action_context.h
+++ components/enterprise/data_controls/action_context.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_ENTERPRISE_DATA_CONTROLS_ACTION_CONTEXT_H_
 
 #include "build/chromeos_buildflags.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "url/gurl.h"
 
 #if BUILDFLAG(IS_CHROMEOS)
