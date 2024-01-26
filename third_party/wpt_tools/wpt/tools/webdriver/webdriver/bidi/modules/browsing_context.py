525f4510b5665111cb473ec4333fcb09137eba0b
Jonathan Lee <jonathanjlee@google.com>
2024-01-26 20:45:36 +0000

    Roll wpt tooling
    This rolls wpt to latest commit at
    https://github.com/web-platform-tests/wpt.
    REMOTE-WPT-HEAD: 014486aedc1ca357b9409ca9ef7a0dfd32e30258
    
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I9c2d6f0de3c89923f9f8e71af14e1541ec6e7dcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5241279
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1252844}

--- third_party/wpt_tools/wpt/tools/webdriver/webdriver/bidi/modules/browsing_context.py
+++ third_party/wpt_tools/wpt/tools/webdriver/webdriver/bidi/modules/browsing_context.py
@@ -82,7 +82,8 @@ class BrowsingContext(BidiModule):
     def create(self,
                type_hint: str,
                reference_context: Optional[str] = None,
-               background: Optional[bool] = None) -> Mapping[str, Any]:
+               background: Optional[bool] = None,
+               user_context: Optional[str] = None) -> Mapping[str, Any]:
         params: MutableMapping[str, Any] = {"type": type_hint}
 
         if reference_context is not None:
@@ -90,6 +91,9 @@ class BrowsingContext(BidiModule):
 
         if background is not None:
             params["background"] = background
+
+        if user_context is not None:
+            params["userContext"] = user_context
 
         return params
 
