8fc15b66307f2feb26d96d4f71a6b360705b261c
Ken Rockot <rockot@google.com>
2024-01-26 21:51:15 +0000

    Don't block all model loads when overriding others
    This ensures that when --optimization-guide-model-override is used to
    override some models, it doesn't prevent other models from loading
    normally.
    
    Fixed: b:322524160
    Change-Id: Ie361cb0bafb26d1882685be46433363b1d7ef2dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5241539
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1252879}

--- components/optimization_guide/core/prediction_model_override.cc
+++ components/optimization_guide/core/prediction_model_override.cc
@@ -112,7 +112,7 @@ void OnModelOverrideVerified(proto::OptimizationTarget optimization_target,
 
 }  // namespace
 
-void BuildPredictionModelFromCommandLineForOptimizationTarget(
+bool BuildPredictionModelFromCommandLineForOptimizationTarget(
     proto::OptimizationTarget optimization_target,
     const base::FilePath& base_model_dir,
     OnPredictionModelBuiltCallback callback) {
@@ -121,7 +121,7 @@ void BuildPredictionModelFromCommandLineForOptimizationTarget(
           GetModelOverrideForOptimizationTarget(optimization_target);
   if (!model_file_path_and_metadata) {
     std::move(callback).Run(nullptr);
-    return;
+    return false;
   }
 
   if (base::EndsWith(model_file_path_and_metadata->first, ".crx3")) {
@@ -143,7 +143,7 @@ void BuildPredictionModelFromCommandLineForOptimizationTarget(
         base::BindOnce(&OnModelOverrideVerified, optimization_target,
                        *StringToFilePath(model_file_path_and_metadata->first),
                        base_model_dir, std::move(callback)));
-    return;
+    return true;
   }
 
   std::unique_ptr<proto::PredictionModel> prediction_model =
@@ -158,6 +158,7 @@ void BuildPredictionModelFromCommandLineForOptimizationTarget(
   prediction_model->mutable_model()->set_download_url(
       model_file_path_and_metadata->first);
   std::move(callback).Run(std::move(prediction_model));
+  return true;
 }
 
 }  // namespace optimization_guide
