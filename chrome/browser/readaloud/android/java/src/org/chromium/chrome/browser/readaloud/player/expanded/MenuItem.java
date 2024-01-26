b9870e54a61ab3341d8d65a0fbd12961da7d8b71
Andrea Gomez <andreaxg@google.com>
2024-01-26 18:40:35 +0000

    Use accessibility delegate on menu items for better TalkBack focus
    Adds the content description to the toggle switch and radio button so
    the menu items are properly described by TalkBack and include the
    current state of the switches and buttons.
    
    Removes setting the content description for menu items since it is
    handled by the accessibility delegate.
    
    
    Bug: 321786797
    Change-Id: Ifea4426ebfa2dd9ccac34e708b3d4eea87488d65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5230720
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Andrea Gomez <andreaxg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1252764}

--- chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/player/expanded/MenuItem.java
+++ chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/player/expanded/MenuItem.java
@@ -8,6 +8,9 @@ package org.chromium.chrome.browser.readaloud.player.expanded;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.AccessibilityDelegate;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -16,11 +19,15 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 
 import androidx.annotation.IntDef;
+import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.widget.SwitchCompat;
 
 import com.google.android.material.materialswitch.MaterialSwitch;
 
 import org.chromium.base.Callback;
+import org.chromium.base.supplier.ObservableSupplier;
+import org.chromium.base.supplier.ObservableSupplierImpl;
+import org.chromium.base.supplier.OneShotCallback;
 import org.chromium.chrome.browser.readaloud.player.R;
 
 import java.lang.annotation.Retention;
@@ -51,6 +58,7 @@ public class MenuItem extends FrameLayout {
     private final @Action int mActionType;
     private final Menu mMenu;
     private final LinearLayout mLayout;
+    private final ObservableSupplier<LinearLayout> mLayoutSupplier;
     private final ImageView mPlayButton;
     private final ProgressBar mPlayButtonSpinner;
     private Callback<Boolean> mToggleHandler;
@@ -70,8 +78,7 @@ public class MenuItem extends FrameLayout {
             int itemId,
             int iconId,
             String label,
-            @Action int action,
-            String contentDescription) {
+            @Action int action) {
         super(context);
         mMenu = parentMenu;
         mId = itemId;
@@ -84,6 +91,8 @@ public class MenuItem extends FrameLayout {
                     onClick();
                 });
         mLayout = layout;
+        mLayoutSupplier = new ObservableSupplierImpl(mLayout);
+        new OneShotCallback<LinearLayout>(mLayoutSupplier, this::onLayoutInflated);
         if (iconId != 0) {
             ImageView icon = layout.findViewById(R.id.icon);
             icon.setImageResource(iconId);
@@ -92,7 +101,6 @@ public class MenuItem extends FrameLayout {
         }
 
         ((TextView) layout.findViewById(R.id.item_label)).setText(label);
-        layout.setContentDescription(contentDescription);
 
         switch (mActionType) {
             case Action.EXPAND:
@@ -136,6 +144,42 @@ public class MenuItem extends FrameLayout {
         mPlayButtonSpinner = (ProgressBar) findViewById(R.id.spinner);
     }
 
+    private void onLayoutInflated(LinearLayout layout) {
+        // accessibility delegate is only being set for radio and toggle switch item types
+        if (mActionType != Action.RADIO && mActionType != Action.TOGGLE) {
+            return;
+        }
+        // To improve the explore-by-touch experience, the button are hidden from accessibility
+        // and instead, "checked" or "not checked" is read along with the button's label
+        layout.setAccessibilityDelegate(
+                new AccessibilityDelegate() {
+                    @Override
+                    public void onInitializeAccessibilityEvent(
+                            View host, AccessibilityEvent event) {
+                        super.onInitializeAccessibilityEvent(host, event);
+                        if (mActionType == Action.RADIO) {
+                            event.setChecked(getRadioButton().isChecked());
+                        } else if (mActionType == Action.TOGGLE) {
+                            event.setChecked(getToggleSwitch().isChecked());
+                        }
+                    }
+
+                    @Override
+                    public void onInitializeAccessibilityNodeInfo(
+                            View host, AccessibilityNodeInfo info) {
+                        super.onInitializeAccessibilityNodeInfo(host, info);
+                        info.setCheckable(true);
+                        if (mActionType == Action.RADIO) {
+                            info.setChecked(getRadioButton().isChecked());
+                            info.setEnabled(getRadioButton().isEnabled());
+                        } else if (mActionType == Action.TOGGLE) {
+                            info.setChecked(getToggleSwitch().isChecked());
+                            info.setEnabled(getToggleSwitch().isEnabled());
+                        }
+                    }
+                });
+    }
+
     void setToggleHandler(Callback<Boolean> handler) {
         mToggleHandler = handler;
     }
@@ -149,8 +193,6 @@ public class MenuItem extends FrameLayout {
     }
 
     void setItemEnabled(boolean enabled) {
-        mLayout.setClickable(enabled);
-        mLayout.setFocusable(enabled);
         if (mActionType == Action.TOGGLE) {
             getToggleSwitch().setEnabled(enabled);
         }
@@ -224,5 +266,10 @@ public class MenuItem extends FrameLayout {
 
     private RadioButton getRadioButton() {
         return (RadioButton) findViewById(R.id.readaloud_radio_button);
+    }
+
+    @VisibleForTesting(otherwise = VisibleForTesting.PACKAGE_PRIVATE)
+    public ObservableSupplierImpl<LinearLayout> getLayoutSupplier() {
+        return (ObservableSupplierImpl) mLayoutSupplier;
     }
 }
