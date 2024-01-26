fd3feba83efbcbec652f983819870e154c1cbb6e
Kelvin Jiang <kelvinjiang@chromium.org>
2024-01-26 06:45:11 +0000

    [Extensions] Fix crash: extension unload when script validation finishes
    An extension may be unloaded while file validation for content scripts
    that are being registered is taking place. Fix: guard against an
    unloaded extension in OnUserScriptFilesValidated by returning early.
    
    Tests may be added in a follow up as they involve specific timing of
    events to reproduce.
    
    Bug: 1518040
    Change-Id: I766915b530948037b2ea1fff6b481f77dd2977f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5225796
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252454}

--- chrome/browser/extensions/api/scripting/scripting_api.cc
+++ chrome/browser/extensions/api/scripting/scripting_api.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/api/scripting/scripting_utils.h"
 #include "extensions/browser/extension_api_frame_id_map.h"
 #include "extensions/browser/extension_file_task_runner.h"
+#include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/browser/extension_user_script_loader.h"
 #include "extensions/browser/extension_util.h"
@@ -1009,6 +1010,17 @@ void ScriptingRegisterContentScriptsFunction::OnContentScriptFilesValidated(
     return;
   }
 
+  // We cannot proceed if the extension is uninstalled or unloaded in the middle
+  // of validating its script files.
+  ExtensionRegistry* registry = ExtensionRegistry::Get(browser_context());
+  if (!extension() ||
+      !registry->enabled_extensions().Contains(extension_id())) {
+    // Note: a Respond() is not needed if the system is shutting down or if the
+    // extension is no longer enabled.
+    Release();  // Matches the `AddRef()` in `Run()`.
+    return;
+  }
+
   auto error = std::move(result.second);
   auto scripts = std::move(result.first);
   ExtensionUserScriptLoader* loader =
@@ -1268,6 +1280,17 @@ void ScriptingUpdateContentScriptsFunction::OnContentScriptFilesValidated(
   // We cannot proceed if the `browser_context` is not valid as the
   // `ExtensionSystem` will not exist.
   if (!browser_context()) {
+    Release();  // Matches the `AddRef()` in `Run()`.
+    return;
+  }
+
+  // We cannot proceed if the extension is uninstalled or unloaded in the middle
+  // of validating its script files.
+  ExtensionRegistry* registry = ExtensionRegistry::Get(browser_context());
+  if (!extension() ||
+      !registry->enabled_extensions().Contains(extension_id())) {
+    // Note: a Respond() is not needed if the system is shutting down or if the
+    // extension is no longer enabled.
     Release();  // Matches the `AddRef()` in `Run()`.
     return;
   }
