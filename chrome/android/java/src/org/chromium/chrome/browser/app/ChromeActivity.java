be7b5d1efbf979cf34cafe303d860098d9aee6bb
Finnur Thorarinsson <finnur@chromium.org>
2024-01-26 20:42:52 +0000

    [UniversalInstall]: Flesh out a bit more of the dialog.
    This adds...
    
    - A section devoted to a) install and b) shortcut creation.
    - A background for each, with rounded corners.
    - A round (placeholder) app icon.
    - An arrow (possibly placeholder) for both sections.
    
    Bug: 1520511
    Change-Id: I800ffcac199ab572aa97aebeaae604c6d932f2ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5240012
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252842}

--- chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
@@ -3119,7 +3119,8 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
         }
 
         PwaUniversalInstallBottomSheetCoordinator pwaUniversalInstallBottomSheetCoordinator =
-                new PwaUniversalInstallBottomSheetCoordinator(this, controller);
+                new PwaUniversalInstallBottomSheetCoordinator(
+                        this, controller, R.drawable.ic_forward_arrow_black_24dp);
         if (!pwaUniversalInstallBottomSheetCoordinator.show()) {
             // Fall back to install method for the PWA.
             return doAddToHomescreenOrInstallWebApp(
