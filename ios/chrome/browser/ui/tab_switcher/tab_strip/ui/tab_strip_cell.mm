ca771389b25313544a541aa934e2baa6c96180d0
Ewann Pelle <ewannpv@chromium.org>
2024-01-26 10:45:52 +0000

    [iOS][TS] Fix cell traitCollection update
    This CL fixes cell colors when the trait collection did change.
    It also correctly updates colors of `_titleGradientView` in incognito.
    
    Bug: 1508814
    Change-Id: I76d79684342f985ed85fa047ef7be8f36c13db2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5237018
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1252545}

--- ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_cell.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_cell.mm
@@ -225,16 +225,7 @@ UIImage* DefaultFavicon() {
     _trailingSeparatorGradientView.hidden = YES;
   }
 
-  UIColor* backgroundColor = selected
-                                 ? [UIColor colorNamed:kPrimaryBackgroundColor]
-                                 : [UIColor colorNamed:kGrey200Color];
-
-  // Update colors.
-  self.contentView.backgroundColor = backgroundColor;
-  _faviconView.tintColor = selected ? [UIColor colorNamed:kCloseButtonColor]
-                                    : [UIColor colorNamed:kGrey500Color];
-  [_titleGradientView setStartColor:[backgroundColor colorWithAlphaComponent:0]
-                           endColor:backgroundColor];
+  [self updateColors];
 
   // Make the selected cell on top of other cells.
   self.layer.zPosition = selected ? kSelectedZIndex : 0;
@@ -278,6 +269,13 @@ UIImage* DefaultFavicon() {
   _titleLabel.text = nil;
   self.selected = NO;
   [self setFaviconImage:nil];
+}
+
+#pragma mark - UITraitEnvironment
+
+- (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
+  [super traitCollectionDidChange:previousTraitCollection];
+  [self updateColors];
 }
 
 #pragma mark - Private
@@ -330,6 +328,23 @@ UIImage* DefaultFavicon() {
   }
 
   _decorationLayersUpdated = YES;
+}
+
+// Updates view colors.
+- (void)updateColors {
+  UIColor* backgroundColor = self.selected
+                                 ? [UIColor colorNamed:kPrimaryBackgroundColor]
+                                 : [UIColor colorNamed:kGrey200Color];
+  // Needed to correctly update the `_titleGradientView` colors in incognito.
+  backgroundColor =
+      [backgroundColor resolvedColorWithTraitCollection:self.traitCollection];
+
+  self.contentView.backgroundColor = backgroundColor;
+  _faviconView.tintColor = self.selected
+                               ? [UIColor colorNamed:kCloseButtonColor]
+                               : [UIColor colorNamed:kGrey500Color];
+  [_titleGradientView setStartColor:[backgroundColor colorWithAlphaComponent:0]
+                           endColor:backgroundColor];
 }
 
 // Hides the close button view if the cell is collapsed.
