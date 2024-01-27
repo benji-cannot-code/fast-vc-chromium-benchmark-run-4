7b834ebf6ec2a92796c0f3954f8a249c52eb13b6
Mingyu Lei <leimy@chromium.org>
2024-01-27 16:03:32 +0000

    Bundz: pass error from model session to JS
    Instead of raising an exception with general information, the model
    execution API now also provides the detailed error message for each
    of the failure case.
    
    Bug: 1520700
    Change-Id: I0ce7a0b07e344c71554ac7be3a6b1eb503a048f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5232629
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253048}

--- chrome/browser/model_execution/model_execution_session.cc
+++ chrome/browser/model_execution/model_execution_session.cc
@@ -8,11 +8,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "base/functional/bind.h"
+#include "components/optimization_guide/core/model_execution/optimization_guide_model_execution_error.h"
+#include "components/optimization_guide/core/optimization_guide_enums.h"
 #include "components/optimization_guide/core/optimization_guide_model_executor.h"
 #include "components/optimization_guide/core/optimization_guide_util.h"
 #include "components/optimization_guide/proto/common_types.pb.h"
 #include "components/optimization_guide/proto/string_value.pb.h"
 #include "mojo/public/cpp/bindings/remote_set.h"
+#include "third_party/blink/public/mojom/model_execution/model_session.mojom-shared.h"
+
+using ModelExecutionError = optimization_guide::
+    OptimizationGuideModelExecutionError::ModelExecutionError;
 
 ModelExecutionSession::ModelExecutionSession(
     std::unique_ptr<optimization_guide::OptimizationGuideModelExecutor::Session>
@@ -26,6 +32,36 @@ void ModelExecutionSession::BindReceiver(
   receiver_.Bind(std::move(receiver));
 }
 
+blink::mojom::ModelStreamingResponseStatus ConvertModelExecutionError(
+    ModelExecutionError error) {
+  switch (error) {
+    case ModelExecutionError::kUnknown:
+      return blink::mojom::ModelStreamingResponseStatus::kErrorUnknown;
+    case ModelExecutionError::kInvalidRequest:
+      return blink::mojom::ModelStreamingResponseStatus::kErrorInvalidRequest;
+    case ModelExecutionError::kRequestThrottled:
+      return blink::mojom::ModelStreamingResponseStatus::kErrorRequestThrottled;
+    case ModelExecutionError::kPermissionDenied:
+      return blink::mojom::ModelStreamingResponseStatus::kErrorPermissionDenied;
+    case ModelExecutionError::kGenericFailure:
+      return blink::mojom::ModelStreamingResponseStatus::kErrorGenericFailure;
+    case ModelExecutionError::kRetryableError:
+      return blink::mojom::ModelStreamingResponseStatus::kErrorRetryableError;
+    case ModelExecutionError::kNonRetryableError:
+      return blink::mojom::ModelStreamingResponseStatus::
+          kErrorNonRetryableError;
+    case ModelExecutionError::kUnsupportedLanguage:
+      return blink::mojom::ModelStreamingResponseStatus::
+          kErrorUnsupportedLanguage;
+    case ModelExecutionError::kFiltered:
+      return blink::mojom::ModelStreamingResponseStatus::kErrorFiltered;
+    case ModelExecutionError::kDisabled:
+      return blink::mojom::ModelStreamingResponseStatus::kErrorDisabled;
+    case ModelExecutionError::kCancelled:
+      return blink::mojom::ModelStreamingResponseStatus::kErrorCancelled;
+  }
+}
+
 void ModelExecutionSession::ModelExecutionCallback(
     mojo::RemoteSetElementId responder_id,
     optimization_guide::OptimizationGuideModelStreamingExecutionResult result) {
@@ -36,8 +72,9 @@ void ModelExecutionSession::ModelExecutionCallback(
   }
 
   if (!result.response.has_value()) {
-    responder->OnResponse(blink::mojom::ModelStreamingResponseStatus::kError,
-                          std::nullopt);
+    responder->OnResponse(
+        ConvertModelExecutionError(result.response.error().error()),
+        std::nullopt);
     return;
   }
 
