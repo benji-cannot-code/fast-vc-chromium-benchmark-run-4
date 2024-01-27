0f2bb9da6844986cd4797d25ca60be51c174c9a9
Carlos Knippschild <carlosk@chromium.org>
2024-01-27 00:45:46 +0000

    Add a ComposeManagerImpl unit test.
    Also update the reporting of the popup menu item CTR histogram to better
    track the click action.
    
    Bug: b:320499488, 1521851, b:322541701, b:318841248
    Change-Id: I7668828627ff44a43e71b2ae7e96f7e538a93900
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5241323
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Commit-Queue: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252952}

--- components/compose/core/browser/compose_metrics.cc
+++ components/compose/core/browser/compose_metrics.cc
@@ -47,6 +47,7 @@ const char kComposeFirstRunSessionDialogShownCount[] =
     "Compose.Session.FRE.Disclaimer.DialogShownCount";
 const char kInnerTextNodeOffsetFound[] =
     "Compose.Dialog.InnerTextNodeOffsetFound";
+const char kComposeContextMenuCtr[] = "Compose.ContextMenu.CTR";
 const char kOpenComposeDialogResult[] =
     "Compose.ContextMenu.OpenComposeDialogResult";
 
@@ -115,7 +116,7 @@ void PageUkmTracker::MaybeLogUkm() {
 ComposeSessionEvents::ComposeSessionEvents() {}
 
 void LogComposeContextMenuCtr(ComposeContextMenuCtrEvent event) {
-  UMA_HISTOGRAM_ENUMERATION("Compose.ContextMenu.CTR", event);
+  UMA_HISTOGRAM_ENUMERATION(kComposeContextMenuCtr, event);
 }
 
 void LogComposeContextMenuShowStatus(ComposeShowStatus status) {
