fea6c0effd9fe6fb5f08a452286f455205314b2a
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2024-01-27 01:08:11 +0000

    [UIA] Expose kLabelText as a UIA_Group instead of text
    Before this change, the UIA Control tree had the following structure:
    --text ""
    ----text "my label"
    
    For a <label>my label</label>
    
    This was making Narrator announce an empty text element ("blank") before
    announcing "my label". This change modifies the UIA control type of our
    internal kLabelText from UIA_TextControlType to UIA_GroupControlType.
    
    I favored this role because the <label> element can contain 1+ text
    nodes (e.g., <label><span>part 1</span><span>part 2</span></label>),
    and therefore act as a group.
    
    Fixed: 1521433
    Change-Id: Ia1c5f5a4347c2ae9225759a284bacc2cdf40c0a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5232179
    Reviewed-by: Jacques Newman <janewman@microsoft.com>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1252962}

--- ui/accessibility/platform/ax_platform_node_win.cc
+++ ui/accessibility/platform/ax_platform_node_win.cc
@@ -1090,6 +1090,9 @@ AXPlatformNodeWin::UIARoleProperties AXPlatformNodeWin::GetUIARoleProperties() {
               L"textbox"};
 
     case ax::mojom::Role::kLabelText:
+          return {UIALocalizationStrategy::kSupply, UIA_GroupControlTypeId,
+              L"description"};
+
     case ax::mojom::Role::kLegend:
       return {UIALocalizationStrategy::kSupply, UIA_TextControlTypeId,
               L"description"};
@@ -7530,7 +7533,6 @@ bool AXPlatformNodeWin::IsUIAControl() const {
       case ax::mojom::Role::kFooterAsNonLandmark:
       case ax::mojom::Role::kHeader:
       case ax::mojom::Role::kHeaderAsNonLandmark:
-      case ax::mojom::Role::kLabelText:
       case ax::mojom::Role::kListBoxOption:
       case ax::mojom::Role::kListItem:
       // Treat the root of a MathML tree as content/control so that it is seen
