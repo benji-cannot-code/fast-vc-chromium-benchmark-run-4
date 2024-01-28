096ed7fc8f21f5a3598b2c64175e906d186b0f24
Hiroshige Hayashizaki <hiroshige@chromium.org>
2024-01-28 09:32:57 +0000

    Remove MockRenderProcessHost::CreateURLLoaderFactory
    To make `RenderProcessHost::CreateURLLoaderFactory()`
    always creates a URLLoaderFactory from
    its storage partition's network context.
    
    The tests that were previously overriding URLLoaderFactory
    creation via `MockRenderProcessHost` is migrated to
    `StoragePartitionImpl::SetNetworkContextForTesting()`.
    
    Bug: 1506871
    Change-Id: I8c2e5b5f415c179f587dfad207c9b92a0385bc6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5148759
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253090}

--- content/public/test/mock_render_process_host.h
+++ content/public/test/mock_render_process_host.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 #include <stdint.h>
-
 #include <map>
 #include <memory>
 #include <set>
@@ -32,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/media_buildflags.h"
 #include "mojo/public/cpp/bindings/associated_remote.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
-#include "mojo/public/cpp/bindings/pending_remote.h"
 #include "net/base/network_isolation_key.h"
 
 #if BUILDFLAG(IS_ANDROID)
@@ -362,7 +360,6 @@ class MockRenderProcessHost : public RenderProcessHost {
   std::map<std::string, InterfaceBinder> binder_overrides_;
   bool is_renderer_locked_to_site_ = false;
   std::set<GlobalRenderFrameHostId> render_frame_host_id_set_;
-  std::unique_ptr<network::mojom::URLLoaderFactory> url_loader_factory_;
   mojo::PendingReceiver<blink::mojom::CacheStorage> cache_storage_receiver_;
   mojo::PendingReceiver<blink::mojom::IDBFactory> idb_factory_receiver_;
   base::WeakPtrFactory<MockRenderProcessHost> weak_ptr_factory_{this};
@@ -396,10 +393,6 @@ class MockRenderProcessHostFactory : public RenderProcessHostFactory {
   // for deleting all MockRenderProcessHosts that have not deleted by a test in
   // the destructor and prevent them from being leaked.
   mutable std::vector<std::unique_ptr<MockRenderProcessHost>> processes_;
-
-  // A mock URLLoaderFactory which just fails to create a loader.
-  std::unique_ptr<network::mojom::URLLoaderFactory>
-      default_mock_url_loader_factory_;
 };
 
 }  // namespace content
