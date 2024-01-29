e7ab7d0c5e8f3c8d4c47de06de0b1353f6af410d
Sky Malice <skym@chromium.org>
2024-01-29 21:07:25 +0000

    Hide FAB button when incog reauth is shown.
    The disabled button could overlap with the incog reauth buttons. This
    approach keeps the knowledge of this isolated in the incog pane and
    looks the best.
    
    Bug: b/322108400
    Change-Id: I8bff213d53ded4fcda685be56bbca9eed6de7c05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5231906
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253506}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/IncognitoTabSwitcherPane.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/IncognitoTabSwitcherPane.java
@@ -17,6 +17,7 @@ import org.chromium.base.supplier.Supplier;
 import org.chromium.chrome.browser.hub.DelegateButtonData;
 import org.chromium.chrome.browser.hub.FullButtonData;
 import org.chromium.chrome.browser.hub.HubColorScheme;
+import org.chromium.chrome.browser.hub.HubFieldTrial;
 import org.chromium.chrome.browser.hub.Pane;
 import org.chromium.chrome.browser.hub.PaneHubController;
 import org.chromium.chrome.browser.hub.PaneId;
@@ -72,7 +73,7 @@ public class IncognitoTabSwitcherPane extends TabSwitcherPaneBase {
                     coordinator.setInitialScrollIndexOffset();
                     coordinator.requestAccessibilityFocusOnCurrentTab();
 
-                    mNewTabButtonDataSupplier.set(mEnabledNewTabButtonData);
+                    setNewTabButtonEnabledState(/* enabled= */ true);
                 }
 
                 @Override
@@ -138,9 +139,9 @@ public class IncognitoTabSwitcherPane extends TabSwitcherPaneBase {
                                 incognitoReauthController.addIncognitoReauthCallback(
                                         mIncognitoReauthCallback);
                             }));
-            mNewTabButtonDataSupplier.set(mDisabledNewTabButtonData);
+            setNewTabButtonEnabledState(/* enabled= */ false);
         } else {
-            mNewTabButtonDataSupplier.set(mEnabledNewTabButtonData);
+            setNewTabButtonEnabledState(/* enabled= */ true);
         }
     }
 
@@ -208,8 +209,7 @@ public class IncognitoTabSwitcherPane extends TabSwitcherPaneBase {
             coordinator.resetWithTabList(tabList);
         }
 
-        mNewTabButtonDataSupplier.set(
-                incognitoReauthShowing ? mDisabledNewTabButtonData : mEnabledNewTabButtonData);
+        setNewTabButtonEnabledState(/* enabled= */ !incognitoReauthShowing);
         return true;
     }
 
@@ -229,5 +229,15 @@ public class IncognitoTabSwitcherPane extends TabSwitcherPaneBase {
         TabModelFilter incognitoTabModelFilter = mIncognitoTabModelFilterSupplier.get();
         assert incognitoTabModelFilter != null;
         return (IncognitoTabModel) incognitoTabModelFilter.getTabModel();
+    }
+
+    private void setNewTabButtonEnabledState(boolean enabled) {
+        if (enabled) {
+            mNewTabButtonDataSupplier.set(mEnabledNewTabButtonData);
+        } else {
+            // The FAB may overlap the reauth buttons. So just remove it by nulling instead.
+            mNewTabButtonDataSupplier.set(
+                    HubFieldTrial.usesFloatActionButton() ? null : mDisabledNewTabButtonData);
+        }
     }
 }
