b3784961da08a9659b99782bb6d830667c7e648b
Zoraiz Naeem <zoraiznaeem@chromium.org>
2024-01-29 21:39:51 +0000

    [Rounded Windows] Round dialogs that use non_custom_frames
    Currently, dialogs that use non_custom_frames use
    NonClientFrameViewAsh. Since these dialogs look like top-level window
    in ChromeOS, all four corners of these dialogs should be rounded.
    
    The top corners are rounded by NonClientFrameViewAsh and bottom
    corners should be taken care by the client view. (i.e
    DialogClientView)
    
    This CL rounds the DialogClientView by rounding the background and
    adds an implementation for
    `ClientView::UpdateWindowRoundedCorners()`.
    NonClientFrameViewAsh calls UpdateWindowRoudnedCorners when ever
    the window, (window look alike dialog) corners change.
    
    Bug: b/319518323
    Test: Launch chrome task manager
    
    Change-Id: I2fdc264530045ac7c592ba64b5393e3fcd2f821e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5219152
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253525}

--- ui/views/window/dialog_client_view.cc
+++ ui/views/window/dialog_client_view.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/check.h"
 #include "base/memory/raw_ptr.h"
 #include "base/ranges/algorithm.h"
 #include "build/build_config.h"
@@ -181,6 +182,22 @@ void DialogClientView::VisibilityChanged(View* starting_from, bool is_visible) {
   input_protector_->VisibilityChanged(is_visible);
 }
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+
+void DialogClientView::UpdateWindowRoundedCorners(int corner_radius) {
+  DCHECK(GetWidget());
+
+  const gfx::RoundedCornersF radii(0, 0, corner_radius, corner_radius);
+
+  // Chromeos has rounded windows. A dialog can use native frame i.e look like
+  // a top-level window. For ChromeOS, dialogs use `NonClientFrameViewAsh`
+  // as native frame. The top corners will be rounded by the frame_view and
+  // client-view is responsible for rounding the bottom corners.
+  SetBackgroundRadii(radii);
+}
+
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+
 void DialogClientView::Layout() {
   button_row_container_->SetSize(
       gfx::Size(width(), button_row_container_->GetHeightForWidth(width())));
@@ -243,14 +260,8 @@ void DialogClientView::ViewHierarchyChanged(
 
 void DialogClientView::OnThemeChanged() {
   ClientView::OnThemeChanged();
-  // The old dialog style needs an explicit background color, while the new
-  // dialog style simply inherits the bubble's frame view color.
-  const DialogDelegate* dialog = GetDialogDelegate();
 
-  if (dialog && !dialog->use_custom_frame()) {
-    SetBackground(views::CreateSolidBackground(
-        GetColorProvider()->GetColor(ui::kColorDialogBackground)));
-  }
+  UpdateBackground();
 }
 
 void DialogClientView::UpdateInputProtectorTimeStamp() {
@@ -267,6 +278,27 @@ bool DialogClientView::IsPossiblyUnintendedInteraction(const ui::Event& event) {
 
 DialogDelegate* DialogClientView::GetDialogDelegate() const {
   return GetWidget()->widget_delegate()->AsDialogDelegate();
+}
+
+void DialogClientView::SetBackgroundRadii(const gfx::RoundedCornersF& radii) {
+  if (background_radii_ == radii) {
+    return;
+  }
+
+  background_radii_ = radii;
+  UpdateBackground();
+}
+
+void DialogClientView::UpdateBackground() {
+  // The old dialog style needs an explicit background color, while the new
+  // dialog style simply inherits the bubble's frame view color.
+  const DialogDelegate* dialog = GetDialogDelegate();
+
+  if (dialog && !dialog->use_custom_frame()) {
+    SetBackground(views::CreateRoundedRectBackground(
+        GetColorProvider()->GetColor(ui::kColorDialogBackground),
+        background_radii_));
+  }
 }
 
 void DialogClientView::OnButtonVisibilityChanged(View* child) {
