bd819fc7042bf4536079c80b69b68c27670ed64c
Mark PMNKHME <markyacoub@google.com>
2024-01-29 20:28:49 +0000

    display: Add feature flag for Display Performance Mode
    Adds a feature flag to allow development on a feature to toggle
    different display features based on user setting and power state.
    
    BUG=b:320527329
    TEST=flag appears in chrome://flags
    
    Change-Id: I74116042a87bdcfb11e73d33140f61d580392f59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5213816
    Reviewed-by: Drew Davenport <ddavenport@chromium.org>
    Commit-Queue: Mark PMNKHME <markyacoub@google.com>
    Cr-Commit-Position: refs/heads/main@{#1253474}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3515,6 +3515,9 @@ extern const char kEnableAssistantRoutinesDescription[];
 extern const char kEnableBackgroundBlurName[];
 extern const char kEnableBackgroundBlurDescription[];
 
+extern const char kEnableDisplayPerformanceModeName[];
+extern const char kEnableDisplayPerformanceModeDescription[];
+
 extern const char kDisableDnsProxyName[];
 extern const char kDisableDnsProxyDescription[];
 
