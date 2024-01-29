a7d8f9af28e7c92c1e2255eb4ef22573f5a58cbb
Peter Kasting <pkasting@chromium.org>
2024-01-29 22:36:50 +0000

    Add LayoutSuperclass<>() and DeprecatedLayoutImmediately().
    LayoutSuperclass<>() invokes Layout() on behalf of a subclass. It will
    be used to replace calls to base class Layout() implementations from
    inside Layout() overrides.
    
    DeprecatedLayoutImmediately() is a public accessor to
    LayoutImmediately(). It will be used for all other Layout() calls, to
    make it clear these will eventually be removed.
    
    Bug: 1521108
    Change-Id: I2d61890c6d3e2a594a6eb63b3e35efb9422b2b30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5242027
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253567}

--- ui/views/view.cc
+++ ui/views/view.cc
@@ -861,6 +861,10 @@ int View::GetMirroredXWithWidthInView(int x, int w) const {
 
 // Layout ----------------------------------------------------------------------
 
+void View::DeprecatedLayoutImmediately() {
+  LayoutImmediately();
+}
+
 void View::Layout() {
   needs_layout_ = false;
 
--- ui/views/view.h
+++ ui/views/view.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include <concepts>
 #include <memory>
 #include <set>
 #include <string>
@@ -797,15 +798,20 @@ class VIEWS_EXPORT View : public ui::LayerDelegate,
   // To customize layout behavior, use LayoutManagers; see
   // https://chromium.googlesource.com/chromium/src/+/main/docs/ui/learn/bestpractices/layout.md?pli=1#Use-LayoutManagers.
   // For now, classes may override Layout() to customize this manually, but this
-  // will eventually be removed; see https://crbug.com/1005568.
+  // will eventually be removed; see https://crbug.com/1005568. Subclasses which
+  // need to invoke a superclass' Layout() method during their own
+  // implementation of Layout() can do so via LayoutSuperclass<SuperT>(this);
+  // calling this in any other way or context is forbidden (and will likely
+  // break at compile or run time).
   //
   // To cause a view to be laid out, use InvalidateLayout(), which will
   // perform layout asynchronously; see
   // https://chromium.googlesource.com/chromium/src/+/main/docs/ui/learn/bestpractices/layout.md?pli=1#don_t-invoke-layout_directly.
-  // For now, classes may also call Layout() to synchronously lay out a view,
-  // but this will eventually be removed; see https://crbug.com/1521108. Neither
-  // of these methods should be called from Layout(); see
-  // https://crbug.com/1121681.
+  // For now, classes may also call DeprecatedLayoutImmediately() to
+  // synchronously lay out a view, but this will eventually be removed; see
+  // https://crbug.com/1521108. Neither of these methods should be called from
+  // Layout(); see https://crbug.com/1121681.
+  void DeprecatedLayoutImmediately();
   virtual void Layout();
 
   bool needs_layout() const { return needs_layout_; }
@@ -1832,6 +1838,19 @@ class VIEWS_EXPORT View : public ui::LayerDelegate,
   // the child by adding its own layer.
   virtual void OnChildLayerChanged(View* child);
 
+  // Layout --------------------------------------------------------------------
+
+  // Invokes Layout() on a superclass on behalf of the subclass. This is to be
+  // used only inside a Layout() override, where a subclass needs to do the
+  // superclass portion of layout. Invoke like `LayoutSuperclass<SuperT>(this)`,
+  // where SuperT is the relevant superclass type.
+  template <typename Super, typename This>
+    requires std::derived_from<Super, View> && std::derived_from<This, Super> &&
+             (!std::same_as<Super, This>)
+  void LayoutSuperclass(This* ptr) {
+    static_cast<Super*>(ptr)->Super::Layout();
+  }
+
   // Input ---------------------------------------------------------------------
 
   virtual DragInfo* GetDragInfo();
@@ -2147,7 +2166,9 @@ class VIEWS_EXPORT View : public ui::LayerDelegate,
   // default fill layout or the assigned layout manager.
   bool HasLayoutManager() const;
 
-  // Implementation of synchronous layout.
+  // Implementation of synchronous layout. DeprecatedLayoutImmediately() is a
+  // temporary public accessor to this; this is the access point for the few
+  // blessed uses.
   void LayoutImmediately();
 
   // Input ---------------------------------------------------------------------
