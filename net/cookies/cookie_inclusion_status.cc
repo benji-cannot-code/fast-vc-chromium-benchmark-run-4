69640bb87b8e997dd792a484bfa3df0c3338bcc6
Shuran Huang <shuuran@chromium.org>
2024-01-29 16:01:57 +0000

    Associate unblocked 3pc with unblocking reason
    3PCs that should have been blocked by 3PCD can be exempted by User
    Bypass, Deprecation trial, etc.. We should make it explicit regarding
    why a 3PC is exempted.
    
    Bug: 1518872
    Change-Id: I933abeba56d8efc47e291e6af9937aee7d37ed9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5213806
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Shuran Huang <shuuran@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253303}

--- net/cookies/cookie_inclusion_status.cc
+++ net/cookies/cookie_inclusion_status.cc
@@ -80,6 +80,8 @@ void CookieInclusionStatus::AddExclusionReason(ExclusionReason reason) {
   // If the cookie would be excluded for reasons unrelated to 3pcd, don't bother
   // warning about 3pcd.
   MaybeClearThirdPartyPhaseoutReason();
+  // If the cookie would have been excluded, clear the exemption reason.
+  exemption_reason_ = ExemptionReason::kNone;
 }
 
 void CookieInclusionStatus::RemoveExclusionReason(ExclusionReason reason) {
@@ -89,6 +91,12 @@ void CookieInclusionStatus::RemoveExclusionReason(ExclusionReason reason) {
 void CookieInclusionStatus::RemoveExclusionReasons(
     const std::vector<ExclusionReason>& reasons) {
   exclusion_reasons_ = ExclusionReasonsWithout(reasons);
+}
+
+void CookieInclusionStatus::MaybeSetExemptionReason(ExemptionReason reason) {
+  if (IsInclude() && exemption_reason_ == ExemptionReason::kNone) {
+    exemption_reason_ = reason;
+  }
 }
 
 CookieInclusionStatus::ExclusionReasonBitset
