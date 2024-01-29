25403148d62122ef659bf06eb54f4bc2c3846c31
Tom Sepez <tsepez@chromium.org>
2024-01-29 18:11:51 +0000

    Tidy some usage of crypto::RandBytes()
    -- Use span forms where possible.
    -- Use RandBytesAsVector() where possible.
    
    This is a minor sub-task of the linked bug
    
    Bug: 1500765
    Change-Id: I6ac21e7f07c02f2d37df856dd68e96b3cf39c3ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5237064
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253370}

--- components/password_manager/core/browser/password_hash_data.cc
+++ components/password_manager/core/browser/password_hash_data.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/password_manager/core/browser/password_hash_data.h"
 
+#include <iterator>
+
 #include "base/strings/string_piece.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
@@ -20,12 +22,11 @@ namespace {
 std::string CreateRandomSalt() {
   constexpr size_t kSyncPasswordSaltLength = 16;
 
-  char buffer[kSyncPasswordSaltLength];
-  crypto::RandBytes(buffer, kSyncPasswordSaltLength);
+  uint8_t buffer[kSyncPasswordSaltLength];
+  crypto::RandBytes(buffer);
   // Explicit std::string constructor with a string length must be used in order
   // to avoid treating '\0' symbols as a string ends.
-  std::string result(buffer, kSyncPasswordSaltLength);
-  return result;
+  return std::string(std::begin(buffer), std::end(buffer));
 }
 
 }  // namespace
