4426bb52f6f8c031ca04b270d3a72ec631a2632a
Shengsong Tan <sstan@chromium.org>
2024-01-29 15:52:49 +0000

    arc: Use tablet size as default launch bound type for google apps
    This CL change the launch bounds policy when the ARC size app
    pre-defined bound have not been synced to chrome. The reason is
    google app already optimized on tablet, and almost all chromeos devices
    are landscape style.
    
    BUG=b:320684570
    TEST=manuall test on ToT, print log.
    
    Change-Id: Ibcb6f6a3e73cdc225ddce4e57e49c875ab6f8364
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5239894
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Shengsong Tan <sstan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253296}

--- chrome/browser/ash/arc/window_predictor/window_predictor_utils.cc
+++ chrome/browser/ash/arc/window_predictor/window_predictor_utils.cc
@@ -5,10 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/arc/window_predictor/window_predictor_utils.h"
 
+#include "base/strings/string_util.h"
 #include "chromeos/ui/base/window_state_type.h"
 #include "components/app_restore/app_restore_data.h"
 
 namespace arc {
+
+namespace {
+const char kPlayStorePrefix[] = "com.android.vending";
+const char kGoogleOfficialPrefix1[] = "com.android.google";
+const char kGoogleOfficialPrefix2[] = "com.google.android";
+}  // namespace
 
 bool CanLaunchGhostWindowByRestoreData(
     const app_restore::AppRestoreData& restore_data) {
@@ -17,6 +24,12 @@ bool CanLaunchGhostWindowByRestoreData(
       restore_data.window_state_type == chromeos::WindowStateType::kFullscreen;
   return not_need_bounds || restore_data.bounds_in_root.has_value() ||
          restore_data.current_bounds.has_value();
+}
+
+bool IsGoogleSeriesPackage(const std::string& package_name) {
+  return base::StartsWith(package_name, kPlayStorePrefix) ||
+         base::StartsWith(package_name, kGoogleOfficialPrefix1) ||
+         base::StartsWith(package_name, kGoogleOfficialPrefix2);
 }
 
 }  // namespace arc
