04255408ad9fd1f8d4622f6e396a6cddd7d1816f
Alex N. Jose <alexnj@chromium.org>
2024-01-29 17:15:38 +0000

    [LCPP] Auto-preconnect to LCP origins experiment: write path.
    This CL implements the write path of automatic preconnect to LCP
    origins experiment.
    
    Automatic Preconnect to LCP Origins experiments attempts to learn LCP
    image origins using LCPP framework, and use that information to
    preconnect to LCP origins using the ResourcePrefetchPredictor
    infrastructure. The experiment aims at executing a preconnect to
    the LCP origin prior to navigation, and help improve LCP of sites
    with a cross-origin image asset as the LCP element.
    
    Bug: 1518996
    Change-Id: Ib2a995bb65d61341dcc0b85fa819b03d38f37fb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5226614
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253336}

--- third_party/blink/web_tests/http/tests/lcp-script-observer/prioritize_lcp_image_scripts.php
+++ third_party/blink/web_tests/http/tests/lcp-script-observer/prioritize_lcp_image_scripts.php
@@ -25,6 +25,7 @@ if (window.location.search != "?start") {
   // All fields are non-nullable.
   hint.lcpElementLocators = [];
   hint.fetchedFonts = [];
+  hint.preconnectOrigins = [];
 
   const web_test_control_host_remote = new NonAssociatedWebTestControlHostRemote();
   web_test_control_host_remote.$.bindNewPipeAndPassReceiver().bindInBrowser('process');
--- third_party/blink/web_tests/http/tests/lcp_critical_path_predictor/resources/common.js
+++ third_party/blink/web_tests/http/tests/lcp_critical_path_predictor/resources/common.js
@@ -11,6 +11,7 @@ export async function setupLCPTest(lcp_locator_protos) {
     hint.lcpElementLocators = [];
     hint.lcpInfluencerScripts = [];
     hint.fetchedFonts = [];
+    hint.preconnectOrigins = [];
 
     let getLCPBytes = async function(proto_file) {
       const resp = await fetch(
