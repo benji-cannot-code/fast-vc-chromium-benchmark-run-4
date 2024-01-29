410d72bc4b161cf28b0ea0a1551feb1bb730a81d
Philip Pfaffe <pfaffe@chromium.org>
2024-01-29 09:11:29 +0000

    [devtools] Look up registered properties for pseudo element nodes
    Since devtools represents some pseudo elements like ::before as nodes in the DOM tree, we also need to report and validate registered properties
    in the element's own style and not just the inherited styles.
    
    Fixed: chromium:1515814
    Change-Id: I49e2132f80e568480d9a9df6c16df38fc07a82b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5240837
    Auto-Submit: Philip Pfaffe <pfaffe@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253192}

--- third_party/blink/web_tests/inspector-protocol/css/css-property-rule-var.js
+++ third_party/blink/web_tests/inspector-protocol/css/css-property-rule-var.js
@@ -50,17 +50,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await dp.CSS.enable();
 
   const {result: {root}} = await dp.DOM.getDocument();
-  const {result: {nodeId}} =
-      await dp.DOM.querySelector({nodeId: root.nodeId, selector: 'div'});
+
+  const nodes = new Map();
+  function collectNodes(node) {
+    nodes.set(node.localName, node.nodeId);
+    node.children?.forEach(collectNodes);
+    node.pseudoElements?.forEach(collectNodes);
+  }
+  collectNodes(root);
 
   const {result: {computedStyle}} =
-      await dp.CSS.getComputedStyleForNode({nodeId});
+      await dp.CSS.getComputedStyleForNode({nodeId: nodes.get('div')});
   testRunner.log('Computed value:');
   testRunner.log(computedStyle.find(style => style.name === '--len'));
   testRunner.log(computedStyle.find(style => style.name === '--color'));
 
   const {result: {matchedCSSRules, cssKeyframesRules, pseudoElements}} =
-      await dp.CSS.getMatchedStylesForNode({nodeId});
+      await dp.CSS.getMatchedStylesForNode({nodeId: nodes.get('div')});
 
   const rules =
       matchedCSSRules.filter(({rule}) => rule.selectorList.text === 'div');
@@ -85,7 +91,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   {
     const edits = [{styleSheetId, range, text: '--v: 5px; --len: var(--v);'}];
     const {result: {styles: [{cssProperties}]}} = await dp.CSS.setStyleTexts(
-        {edits, nodeForPropertySyntaxValidation: nodeId});
+        {edits, nodeForPropertySyntaxValidation: nodes.get('div')});
     testRunner.log(cssProperties);
   }
 
@@ -98,17 +104,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       styleSheetId,
       location,
       ruleText: 'div { --v: 5px; --len: var(--v); }',
-      nodeForPropertySyntaxValidation: nodeId,
+      nodeForPropertySyntaxValidation: nodes.get('div'),
     });
     testRunner.log(cssProperties);
   }
 
   testRunner.log('Pseudo Elements:');
   {
-    const {result: {nodeId}} =
-        await dp.DOM.querySelector({nodeId: root.nodeId, selector: 'body'});
     const {result: {pseudoElements}} =
-        await dp.CSS.getMatchedStylesForNode({nodeId});
+        await dp.CSS.getMatchedStylesForNode({nodeId: nodes.get('body')});
     testRunner.log(
         pseudoElements
             .map(
@@ -116,8 +120,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                     matches.map(({rule}) => rule.style.cssProperties).flat())
             .flat()
             .filter(({name}) => name.startsWith('--')));
+    const {result: {matchedCSSRules, cssPropertyRules}} =
+        await dp.CSS.getMatchedStylesForNode({nodeId: nodes.get('::before')});
+    testRunner.log(
+        matchedCSSRules
+            .filter(({rule}) => rule.selectorList.text === 'body::before')
+            .map(({rule}) => rule.style.cssProperties)
+            .flat()
+            .filter(({name}) => name.startsWith('--'))
+            .flat());
+    testRunner.log('Pseudo element registered properties:');
+    testRunner.log(cssPropertyRules.map(({propertyName}) => propertyName));
   }
-
 
   testRunner.completeTest();
 });
