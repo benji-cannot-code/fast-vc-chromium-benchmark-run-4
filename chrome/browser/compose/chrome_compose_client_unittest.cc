60cc10a782cd6186a15c7cfad4f2f332fd9154dc
Dan Harrington <harringtond@chromium.org>
2024-01-29 19:07:37 +0000

    Add server/on-device variants for Compose.Session.EventCounts
    If all requests in a session are serviced through
    one of server or on-device, we will record to one of these
    new histograms.
    
    Bug: b:320765855
    Change-Id: I323b3d313db48602ecda5f53c9b613af96ba8a8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5238012
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253416}

--- chrome/browser/compose/chrome_compose_client_unittest.cc
+++ chrome/browser/compose/chrome_compose_client_unittest.cc
@@ -461,6 +461,9 @@ TEST_F(ChromeComposeClientTest, TestCompose) {
       compose::kComposeSessionEventCounts,
       compose::ComposeSessionEventTypes::kDialogShown, 1);
   histograms().ExpectBucketCount(
+      "Compose.Server.Session.EventCounts",
+      compose::ComposeSessionEventTypes::kDialogShown, 1);
+  histograms().ExpectBucketCount(
       compose::kComposeSessionEventCounts,
       compose::ComposeSessionEventTypes::kCreateClicked, 1);
   histograms().ExpectBucketCount(
--- components/compose/core/browser/compose_metrics.h
+++ components/compose/core/browser/compose_metrics.h
@@ -145,6 +145,13 @@ enum class ComposeShowStatus {
   kMaxValue = kFeatureFlagDisabled,
 };
 
+enum class EvalLocation : int {
+  // Response was evaluated on the server.
+  kServer,
+  // Response was evaluated on the device.
+  kOnDevice,
+};
+
 // Struct containing event and logging information for an individual
 // |ComposeSession|.
 struct ComposeSessionEvents {
@@ -200,13 +207,6 @@ struct ComposeSessionEvents {
   unsigned int on_device_responses = 0;
   // Number of server responses received.
   unsigned int server_responses = 0;
-};
-
-enum class EvalLocation {
-  // Response was evaluated on the server.
-  kServer,
-  // Response was evaluated on the device.
-  kOnDevice,
 };
 
 // Enum with the possible reasons for it being impossible to open the Compose
