888e09ddc9487ecca79c8ae8ead2bf6d5962f2b0
François Doray <fdoray@chromium.org>
2024-01-29 15:29:08 +0000

    [base] Integrate (Sequenced|SingleThread)TaskRunner::GetCurrentDefault with RunOrPostTask.
    Within a RunOrPostTask callback running synchronously:
    
    - There is no "current default SingleThreadTaskRunner".
    
      A RunOrPostTask() callback cannot make assumptions about the thread
      on which it runs, so it shouldn't access the current default
      SingleThreadTaskRunner.
    
    - The "current default SequencedTaskRunner" is the SequenceManager's
      default task runner.
    
      A RunOrPostTask() callback runs in sequence with tasks from the
      target SequenceManager. It's appropriate for it to access the
      "current default" SequencedTaskRunner.
    
    Also, with this CL, when
    SingleThreadTaskRunner::BelongsToCurrentThread() is invoked on a
    task runner that runs tasks posted to it "in sequence" with the
    current RunOrPostTask callback running synchronously, but on
    a different thread, it returns `false`. In the same context,
    SingleThreadTaskRunner::RunsTasksInCurrentSequence() returns true.
    
    Bug: 1503967
    Change-Id: I017442f10eddda9070a29fb6cf0fa38dcd14a88c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5138135
    Reviewed-by: Ken Rockot <rockot@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253287}

--- base/task/sequence_manager/task_queue_impl.cc
+++ base/task/sequence_manager/task_queue_impl.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequence_manager/task_order.h"
 #include "base/task/sequence_manager/wake_up_queue.h"
 #include "base/task/sequence_manager/work_queue.h"
+#include "base/task/single_thread_task_runner.h"
 #include "base/task/task_features.h"
 #include "base/task/task_observer.h"
 #include "base/threading/thread_restrictions.h"
@@ -45,6 +46,23 @@ namespace sequence_manager {
 
 namespace internal {
 
+// This class outside the anonymous namespace exists to allow being a friend of
+// `SingleThreadTaskRunner::CurrentDefaultHandle` in order to access
+// `SingleThreadTaskRunner::CurrentDefaultHandle::MayAlreadyExist`.
+class CurrentDefaultHandleOverrideForRunOrPostTask {
+ public:
+  explicit CurrentDefaultHandleOverrideForRunOrPostTask(
+      scoped_refptr<SequencedTaskRunner> task_runner)
+      : sttr_override_(
+            nullptr,
+            SingleThreadTaskRunner::CurrentDefaultHandle::MayAlreadyExist{}),
+        str_override_(std::move(task_runner)) {}
+
+ private:
+  SingleThreadTaskRunner::CurrentDefaultHandle sttr_override_;
+  SequencedTaskRunner::CurrentDefaultHandle str_override_;
+};
+
 namespace {
 
 // An atomic is used here because the value is queried from other threads when
@@ -57,10 +75,13 @@ std::atomic_bool g_explicit_high_resolution_timer_win{true};
 #endif  // BUILDFLAG(IS_WIN)
 
 void RunTaskSynchronously(const AssociatedThreadId* associated_thread,
+                          scoped_refptr<SingleThreadTaskRunner> task_runner,
                           OnceClosure closure) {
   base::internal::TaskScope sequence_scope(
       associated_thread->GetBoundSequenceToken(),
       /* is_thread_bound=*/false);
+  CurrentDefaultHandleOverrideForRunOrPostTask task_runner_override(
+      std::move(task_runner));
   std::move(closure).Run();
 }
 
@@ -115,6 +136,7 @@ bool TaskQueueImpl::GuardedTaskPoster::RunOrPostTask(PostedTask task) {
   // `IsQueueEnabledFromAnyThread()`. That won't prevent the task from running.
   if (sync_work_auth.IsValid() && outer_->IsQueueEnabledFromAnyThread()) {
     RunTaskSynchronously(outer_->associated_thread_.get(),
+                         outer_->sequence_manager_->GetTaskRunner(),
                          std::move(task.callback));
     return true;
   }
@@ -189,8 +211,26 @@ bool TaskQueueImpl::TaskRunner::RunOrPostTask(subtle::RunOrPostTaskPassKey,
                  Nestable::kNestable, task_type_));
 }
 
-bool TaskQueueImpl::TaskRunner::RunsTasksInCurrentSequence() const {
+bool TaskQueueImpl::TaskRunner::BelongsToCurrentThread() const {
   return associated_thread_->IsBoundToCurrentThread();
+}
+
+bool TaskQueueImpl::TaskRunner::RunsTasksInCurrentSequence() const {
+  // Return true on the bound thread. This works even after `thread_local`
+  // destruction.
+  if (BelongsToCurrentThread()) {
+    return true;
+  }
+
+  // Return true in a `RunOrPostTask` callback running synchronously on a
+  // different thread.
+  if (associated_thread_->IsBound() &&
+      associated_thread_->GetBoundSequenceToken() ==
+          base::internal::SequenceToken::GetForCurrentThread()) {
+    return true;
+  }
+
+  return false;
 }
 
 // static
