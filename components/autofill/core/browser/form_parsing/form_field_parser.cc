4c6fed5b57749f6463fbd3774935caea90656d6a
Florian Leimgruber <fleimgruber@google.com>
2024-01-29 11:29:52 +0000

    Remove birthdate field parser
    It was decided to remove the unlaunched, incomplete birthdate support.
    This CL removes the heuristic classification logic, which is behind a
    dead feature flag (there is little overlap with credit card expiry date
    fields - see discussion in the bug).
    
    Bug: b/322467223
    Change-Id: I550c0f81d8d7339878850334552bae7d6041e4f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5237182
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1253227}

--- components/autofill/core/browser/form_parsing/form_field_parser.cc
+++ components/autofill/core/browser/form_parsing/form_field_parser.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/form_parsing/address_field_parser.h"
 #include "components/autofill/core/browser/form_parsing/autofill_parsing_utils.h"
 #include "components/autofill/core/browser/form_parsing/autofill_scanner.h"
-#include "components/autofill/core/browser/form_parsing/birthdate_field_parser.h"
 #include "components/autofill/core/browser/form_parsing/credit_card_field_parser.h"
 #include "components/autofill/core/browser/form_parsing/email_field_parser.h"
 #include "components/autofill/core/browser/form_parsing/form_field_parser.h"
@@ -152,12 +151,6 @@ void FormFieldParser::ParseFormFields(
   // Address pass.
   ParseFormFieldsPass(AddressFieldParser::Parse, context, processed_fields,
                       field_candidates);
-
-  // Birthdate pass.
-  if (base::FeatureList::IsEnabled(features::kAutofillEnableBirthdateParsing)) {
-    ParseFormFieldsPass(BirthdateFieldParser::Parse, context, processed_fields,
-                        field_candidates);
-  }
 
   // Numeric quantity pass.
   ParseFormFieldsPass(NumericQuantityFieldParser::Parse, context,
