289d561e1747728984661ccf1fbb859239ce1c44
Justin Lulejian <jlulejian@chromium.org>
2024-01-30 01:16:57 +0000

    Fix dangling pointer in PermissionAuditingService.
    Prior to this change the `db_` member var (PermissionAuditingDatabase)
    in PermissionAuditingService was a raw_ptr that was created  with `new`
    when calling PermissionAuditingService::Init(). This appeared to cause a
    across task dangling because the `DeleteSoon()` task is what `delete`s
    the memory rather than PermissionAuditingService. But this appears
    intentional.
    
    After this change `db_` is now a std::unique_ptr to better reflect
    ownership.
    
    Fixed: 1512674
    Change-Id: I9f87591080d2976a9621c9229d3b8a78851d9ef3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5156757
    Reviewed-by: Thomas Nguyen <tungnh@chromium.org>
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253673}

--- components/permissions/permission_auditing_service.cc
+++ components/permissions/permission_auditing_service.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/permissions/permission_auditing_service.h"
 
+#include "utility"
+
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
@@ -30,18 +32,17 @@ PermissionAuditingService::PermissionAuditingService(
 
 PermissionAuditingService::~PermissionAuditingService() {
   if (db_) {
-    backend_task_runner_->DeleteSoon(FROM_HERE, db_.get());
-    db_ = nullptr;
+    backend_task_runner_->DeleteSoon(FROM_HERE, std::move(db_));
   }
 }
 
 void PermissionAuditingService::Init(const base::FilePath& database_path) {
-  DCHECK(!db_);
-  db_ = new PermissionAuditingDatabase();
+  CHECK(!db_);
+  db_ = std::make_unique<PermissionAuditingDatabase>();
   backend_task_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(base::IgnoreResult(&PermissionAuditingDatabase::Init),
-                     base::Unretained(db_), database_path));
+                     base::Unretained(db_.get()), database_path));
 }
 
 void PermissionAuditingService::StartPeriodicCullingOfExpiredSessions() {
@@ -53,12 +54,12 @@ void PermissionAuditingService::StartPeriodicCullingOfExpiredSessions() {
 
 void PermissionAuditingService::StorePermissionUsage(
     const PermissionUsageSession& session) {
-  DCHECK(db_);
+  CHECK(db_);
   backend_task_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(
           base::IgnoreResult(&PermissionAuditingDatabase::StorePermissionUsage),
-          base::Unretained(db_), session));
+          base::Unretained(db_.get()), session));
 }
 
 void PermissionAuditingService::GetPermissionUsageHistory(
@@ -66,11 +67,11 @@ void PermissionAuditingService::GetPermissionUsageHistory(
     const url::Origin& origin,
     base::Time start_time,
     PermissionUsageHistoryCallback result_callback) {
-  DCHECK(db_);
+  CHECK(db_);
   backend_task_runner_->PostTaskAndReplyWithResult(
       FROM_HERE,
       base::BindOnce(&PermissionAuditingDatabase::GetPermissionUsageHistory,
-                     base::Unretained(db_), type, origin, start_time),
+                     base::Unretained(db_.get()), type, origin, start_time),
       std::move(result_callback));
 }
 
@@ -78,11 +79,11 @@ void PermissionAuditingService::GetLastPermissionUsageTime(
     ContentSettingsType type,
     const url::Origin& origin,
     LastPermissionUsageTimeCallback result_callback) {
-  DCHECK(db_);
+  CHECK(db_);
   backend_task_runner_->PostTaskAndReplyWithResult(
       FROM_HERE,
       base::BindOnce(&PermissionAuditingDatabase::GetLastPermissionUsageTime,
-                     base::Unretained(db_), type, origin),
+                     base::Unretained(db_.get()), type, origin),
       std::move(result_callback));
 }
 
@@ -90,31 +91,31 @@ void PermissionAuditingService::UpdateEndTime(ContentSettingsType type,
                                               const url::Origin& origin,
                                               base::Time start_time,
                                               base::Time new_end_time) {
-  DCHECK(db_);
+  CHECK(db_);
   backend_task_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(
           base::IgnoreResult(&PermissionAuditingDatabase::UpdateEndTime),
-          base::Unretained(db_), type, origin, start_time, new_end_time));
+          base::Unretained(db_.get()), type, origin, start_time, new_end_time));
 }
 
 void PermissionAuditingService::DeleteSessionsBetween(base::Time start,
                                                       base::Time end) {
-  DCHECK(db_);
+  CHECK(db_);
   backend_task_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(base::IgnoreResult(
                          &PermissionAuditingDatabase::DeleteSessionsBetween),
-                     base::Unretained(db_), start, end));
+                     base::Unretained(db_.get()), start, end));
 }
 
 void PermissionAuditingService::ExpireOldSessions() {
-  DCHECK(db_);
+  CHECK(db_);
   backend_task_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(base::IgnoreResult(
                          &PermissionAuditingDatabase::DeleteSessionsBetween),
-                     base::Unretained(db_), base::Time(),
+                     base::Unretained(db_.get()), base::Time(),
                      base::Time::Now() - kUsageSessionMaxAge));
 }
 
