c1f282fa8570e636cedeb374e5ab537678f287ad
Andrew Paseltiner <apaseltiner@chromium.org>
2024-01-30 19:31:51 +0000

    Improve formatting and sorting of Attribution Internals columns
    - Numeric columns are right-aligned and use tabular digits for ease of
      comparison (screenshots in https://crbug.com/1523252#c2)
    - Numeric columns are sorted numerically instead of textually
    - Event-level epsilon is formatted to 3 decimal digits for ease of
      scanning
    - Anchor tags are used for URLs for ease of copy-paste and navigation
    - List columns (destinations, dedup keys) are sorted
    - List-flattening is replaced by removal of list-item markers, likewise
      allowing them to be formatted numerically (screenshots in
      https://crbug.com/1523252#c3)
    
    Bug: 1523252, 1523013
    Change-Id: I1b66433ee8aaaf885e86d3b24140bb7b3558f6cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5250687
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254103}

--- content/browser/resources/attribution_reporting/table_model.ts
+++ content/browser/resources/attribution_reporting/table_model.ts
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 export interface Column<T> {
-  readonly compare?: (a: T, b: T) => number;
+  compare?(a: T, b: T): number;
 
   render(td: HTMLElement, row: T): void;
 
