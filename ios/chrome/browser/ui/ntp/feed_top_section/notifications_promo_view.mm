c68659f55d4c4e6b6dbf750bc5e371eb9c28c98f
Joemer Ramos <joemerramos@chromium.org>
2024-01-30 21:34:28 +0000

    [iOS] Remove Unused Import Header
    Call sites have been refactored to no
    longer used methods in this header file
    so we can remove the imported headers. We
    will delete the files in the next CL.
    
    This CL was uploaded by git cl split.
    
    R=guiperez@google.com
    
    Bug: 1418068
    Change-Id: I539f5becaae7327e62ff3ffa679c2aa04bf3c2df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5245179
    Reviewed-by: Guillem Perez <guiperez@google.com>
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254186}

--- ios/chrome/browser/ui/ntp/feed_top_section/notifications_promo_view.mm
+++ ios/chrome/browser/ui/ntp/feed_top_section/notifications_promo_view.mm
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/ui/ntp/feed_top_section/feed_top_section_mutator.h"
 #import "ios/chrome/browser/ui/ntp/feed_top_section/notifications_promo_view_constants.h"
-#import "ios/chrome/common/button_configuration_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 #import "ios/chrome/common/ui/util/pointer_interaction_util.h"
