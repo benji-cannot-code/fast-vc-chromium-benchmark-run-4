e6d87ea6017fc23ac3f8ab8801ac9adc53f58d68
Kent Tamura <tkent@chromium.org>
2024-01-30 07:24:29 +0000

    [RemoveLegacy] Remove unused code in blink::DeprecatedLayoutRect
    This CL has no behavior changes.
    
    Bug: 1229581
    Change-Id: I9138a98f7c204e6d47f3a9449ed7b8e5b3ae81e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5246946
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253767}

--- third_party/blink/renderer/core/layout/geometry/logical_rect.h
+++ third_party/blink/renderer/core/layout/geometry/logical_rect.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/geometry/logical_offset.h"
 #include "third_party/blink/renderer/core/layout/geometry/logical_size.h"
 #include "third_party/blink/renderer/platform/geometry/layout_rect.h"
+#include "ui/gfx/geometry/rect_f.h"
 
 namespace blink {
 
--- third_party/blink/renderer/platform/geometry/layout_rect.h
+++ third_party/blink/renderer/platform/geometry/layout_rect.h
@@ -38,9 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/geometry/layout_point.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
-#include "third_party/blink/renderer/platform/wtf/vector.h"
 #include "ui/gfx/geometry/rect.h"
-#include "ui/gfx/geometry/rect_f.h"
 
 namespace blink {
 
@@ -59,12 +57,6 @@ class PLATFORM_EXPORT DeprecatedLayoutRect {
                                  LayoutUnit height)
       : location_(LayoutPoint(x, y)),
         size_(DeprecatedLayoutSize(width, height)) {}
-  constexpr DeprecatedLayoutRect(int x, int y, int width, int height)
-      : location_(LayoutPoint(x, y)),
-        size_(DeprecatedLayoutSize(width, height)) {}
-  constexpr explicit operator gfx::RectF() const {
-    return gfx::RectF(X(), Y(), Width(), Height());
-  }
 
   // This is deleted to avoid unwanted lossy conversion from float or double to
   // LayoutUnit or int. Use explicit LayoutUnit constructor for each parameter
@@ -75,14 +67,8 @@ class PLATFORM_EXPORT DeprecatedLayoutRect {
 
   ALWAYS_INLINE constexpr LayoutUnit X() const { return location_.X(); }
   ALWAYS_INLINE constexpr LayoutUnit Y() const { return location_.Y(); }
-  ALWAYS_INLINE LayoutUnit MaxX() const { return X() + Width(); }
-  ALWAYS_INLINE LayoutUnit MaxY() const { return Y() + Height(); }
   constexpr LayoutUnit Width() const { return size_.Width(); }
   constexpr LayoutUnit Height() const { return size_.Height(); }
-
-  void MoveBy(const LayoutPoint& offset) {
-    location_.Move(offset.X(), offset.Y());
-  }
 
   String ToString() const;
 
