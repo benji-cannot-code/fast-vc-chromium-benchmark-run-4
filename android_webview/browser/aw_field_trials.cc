c37e28f8a12585158c63138c23d93511161072c1
Chris Fredrickson <cfredric@chromium.org>
2024-01-30 19:08:32 +0000

    Use RAII to enforce proper API usage
    This uses RAII to avoid footguns:
    * Calling EnableFeature or DisableFeature after RegisterOverrides has
      already been called;
    * Calling RegisterOverrides more than once;
    * Forgetting to call RegisterOverrides at all.
    
    Change-Id: I23b113a39d8bc6a123e107a5a2bd815441212e7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5245370
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254077}

--- android_webview/browser/aw_field_trials.cc
+++ android_webview/browser/aw_field_trials.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_paths_android.h"
 #include "base/feature_list.h"
+#include "base/memory/raw_ref.h"
 #include "base/metrics/persistent_histogram_allocator.h"
 #include "base/path_service.h"
 #include "components/history/core/browser/features.h"
@@ -28,33 +29,33 @@ namespace {
 
 class AwFeatureOverrides {
  public:
-  AwFeatureOverrides() = default;
+  explicit AwFeatureOverrides(base::FeatureList& feature_list)
+      : feature_list_(feature_list) {}
 
   AwFeatureOverrides(const AwFeatureOverrides& other) = delete;
   AwFeatureOverrides& operator=(const AwFeatureOverrides& other) = delete;
 
-  ~AwFeatureOverrides() = default;
+  ~AwFeatureOverrides() {
+    feature_list_->RegisterExtraFeatureOverrides(std::move(overrides_));
+  }
 
   // Enable a feature with WebView-specific override.
   void EnableFeature(const base::Feature& feature) {
-    overrides.emplace_back(
+    overrides_.emplace_back(
         std::cref(feature),
         base::FeatureList::OverrideState::OVERRIDE_ENABLE_FEATURE);
   }
 
   // Disable a feature with WebView-specific override.
   void DisableFeature(const base::Feature& feature) {
-    overrides.emplace_back(
+    overrides_.emplace_back(
         std::cref(feature),
         base::FeatureList::OverrideState::OVERRIDE_DISABLE_FEATURE);
   }
 
-  void RegisterOverrides(base::FeatureList* feature_list) {
-    feature_list->RegisterExtraFeatureOverrides(std::move(overrides));
-  }
-
  private:
-  std::vector<base::FeatureList::FeatureOverrideInfo> overrides;
+  base::raw_ref<base::FeatureList> feature_list_;
+  std::vector<base::FeatureList::FeatureOverrideInfo> overrides_;
 };
 
 }  // namespace
@@ -72,7 +73,10 @@ void AwFieldTrials::OnVariationsSetupComplete() {
 // TODO(crbug.com/1453407): Consider to migrate all WebView feature overrides
 // from the AwMainDelegate to the new mechanism here.
 void AwFieldTrials::RegisterFeatureOverrides(base::FeatureList* feature_list) {
-  AwFeatureOverrides aw_feature_overrides;
+  if (!feature_list) {
+    return;
+  }
+  AwFeatureOverrides aw_feature_overrides(*feature_list);
 
   // Disable third-party storage partitioning on WebView.
   aw_feature_overrides.DisableFeature(
@@ -174,8 +178,4 @@ void AwFieldTrials::RegisterFeatureOverrides(base::FeatureList* feature_list) {
   // Storage Access permission prompts are not supported on WebView.
   aw_feature_overrides.DisableFeature(
       permissions::features::kPermissionStorageAccessAPI);
-
-  // `RegisterOverrides` must be called *after* all of the overrides have been
-  // set in `aw_feature_overrides`.
-  aw_feature_overrides.RegisterOverrides(feature_list);
 }
