ada4dd116b2cf65d20bd8002b71ea2e4cb55ac8d
Filipa Senra <fsenra@google.com>
2024-01-30 08:51:07 +0000

    [iOS][PageInfo] Incorporate ATP client duties into the service
    When AboutThisPage service/client archicteture was design, the
    optimization guide service was not platform agnostic, namely for iOS.
    However, that as since changed, and we no longer need different ATP
    clients for different platforms. As such, this CL incorporates the
    client's duties into the ATP service.
    
    Bug: 1512580, b:321267381
    Change-Id: Id540af75d2117fba8bb5528c9f66de186ec25a78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5237183
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1253788}

--- chrome/browser/page_info/about_this_site_service_factory.cc
+++ chrome/browser/page_info/about_this_site_service_factory.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
-#include "chrome/browser/page_info/chrome_about_this_site_service_client.h"
 #include "chrome/browser/page_info/page_info_features.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
@@ -56,9 +55,8 @@ AboutThisSiteServiceFactory::BuildServiceInstanceForBrowserContext(
   Profile* profile = Profile::FromBrowserContext(browser_context);
 
   return std::make_unique<page_info::AboutThisSiteService>(
-      std::make_unique<ChromeAboutThisSiteServiceClient>(
-          OptimizationGuideKeyedServiceFactory::GetForProfile(profile),
-          profile->IsOffTheRecord(), profile->GetPrefs()),
+      OptimizationGuideKeyedServiceFactory::GetForProfile(profile),
+      profile->IsOffTheRecord(), profile->GetPrefs(),
       TemplateURLServiceFactory::GetForProfile(profile));
 }
 
