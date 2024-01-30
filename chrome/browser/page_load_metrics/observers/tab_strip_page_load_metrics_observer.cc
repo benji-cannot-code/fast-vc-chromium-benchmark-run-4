ca14808ebd721451066a630ee575ab48b5014c9b
Emily Shack <emshack@chromium.org>
2024-01-30 17:09:50 +0000

    Increase max time for Tabs.PageLoad metrics
    Increases max from 2 weeks to 3 weeks.
    
    OBSOLETE_HISTOGRAM[Tabs.PageLoad.TimeSinceActive]=Replaced by Tabs.PageLoad.TimeSinceActive2
    OBSOLETE_HISTOGRAM[Tabs.PageLoad.TimeSinceCreated]=Replaced by Tabs.PageLoad.TimeSinceCreated2
    
    Change-Id: Ie621bafc79bfad98195d83297a9da169cf9a52a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5246135
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253973}

--- chrome/browser/page_load_metrics/observers/tab_strip_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/tab_strip_page_load_metrics_observer.cc
@@ -24,8 +24,8 @@ namespace internal {
 
 const char kTabsActiveAbsolutePosition[] = "Tabs.Active.AbsolutePosition";
 const char kTabsActiveRelativePosition[] = "Tabs.Active.RelativePosition";
-const char kTabsPageLoadTimeSinceActive[] = "Tabs.PageLoad.TimeSinceActive";
-const char kTabsPageLoadTimeSinceCreated[] = "Tabs.PageLoad.TimeSinceCreated";
+const char kTabsPageLoadTimeSinceActive[] = "Tabs.PageLoad.TimeSinceActive2";
+const char kTabsPageLoadTimeSinceCreated[] = "Tabs.PageLoad.TimeSinceCreated2";
 
 }  // namespace internal
 
@@ -55,7 +55,7 @@ std::vector<std::vector<content::WebContents*>> GetAllWebContents() {
 void RecordTimeDeltaHistogram(const char histogram_name[],
                               base::TimeDelta value) {
   base::UmaHistogramCustomTimes(histogram_name, value, base::TimeDelta(),
-                                base::Days(14), 50);
+                                base::Days(21), 50);
 }
 
 }  // namespace
