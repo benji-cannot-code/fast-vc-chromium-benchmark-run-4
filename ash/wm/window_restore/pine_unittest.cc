0be12728ccb2844203f08892196d20dac8d21a93
Elijah Hewer <hewer@chromium.org>
2024-01-30 02:11:32 +0000

    Pine: Add 4-window overflow view
    Add an overflow view for the non-screenshot scenario when there are
    more than 4 windows to restore. This CL handles 4-window overflow,
    2-, 3-, and 5+ overflow will be handled in future CLs.
    
    Bug: b/319475023
    Test: manual
    Change-Id: I947314b4437f98fbeca727346cda4cab0b583ca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5234749
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Elijah Hewer <hewer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253692}

--- ash/wm/window_restore/pine_unittest.cc
+++ ash/wm/window_restore/pine_unittest.cc
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
+// TODO(b/322358447): Add unit tests for overflow view.
+
 class PineTest : public AshTestBase {
  public:
   PineTest() { switches::SetIgnoreForestSecretKeyForTest(true); }
