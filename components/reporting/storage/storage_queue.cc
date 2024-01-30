7f70ccd30c13b926a8353a7df5bf47126d7eb358
Josh Hilke <jrhilke@google.com>
2024-01-30 19:48:47 +0000

    Remove StorageQueueGenerationIdReset histogram.
    It's expiring and we've seen no UMA logs. Histogram is also being removed from CrOS in crrev.com/c/5246826.
    
    Bug: b:41485440
    Bug: b:41485441
    Bug: chromium:1512864
    Bug: chromium:1512863
    Change-Id: Iab4cd54c9d1cb37ccb47e53d4ad3d6514c381bb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5246628
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Miriam Zimmerman <mutexlox@chromium.org>
    Commit-Queue: Josh Hilke <jrhilke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1254115}

--- components/reporting/storage/storage_queue.cc
+++ components/reporting/storage/storage_queue.cc
@@ -75,10 +75,6 @@ BASE_FEATURE(kReportingStorageDegradationFeature,
 
 namespace {
 
-// Storage queue generation id reset UMA metric name.
-constexpr char kStorageQueueGenerationIdResetUma[] =
-    "Browser.ERP.StorageQueueGenerationIdReset";
-
 // Metadata file name prefix.
 const base::FilePath::CharType METADATA_NAME[] = FILE_PATH_LITERAL("META");
 
@@ -281,7 +277,6 @@ Status StorageQueue::Init() {
       // earlier.
       if (generation_id_ <= 0) {
         LOG(ERROR) << "Unable to retrieve generation id, performing full reset";
-        base::UmaHistogramBoolean(kStorageQueueGenerationIdResetUma, true);
         next_sequencing_id_ = 0;
         first_sequencing_id_ = 0;
         first_unconfirmed_sequencing_id_ = std::nullopt;
