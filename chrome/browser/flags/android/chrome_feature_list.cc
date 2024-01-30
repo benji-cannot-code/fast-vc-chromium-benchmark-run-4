6e5d9aa12eb6ad496ef1d971e5e0fd53b0680a4b
Peilin Wang <peilinwang@google.com>
2024-01-30 21:39:11 +0000

    [AndroidVisibleUrlTruncation] Cleanup and switch to V2
    Bug: 1519026
    Change-Id: I4f1c801430b79c961f4028be6d116b379fabc15e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5234369
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Peilin Wang <peilinwang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1254193}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -163,7 +163,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kAndroidHatsRefactor,
     &kAndroidHub,
     &kAndroidImprovedBookmarks,
-    &kAndroidVisibleUrlTruncation,
     &kAndroidVisibleUrlTruncationV2,
     &kAnimatedImageDragShadow,
     &kAppSpecificHistory,
@@ -414,10 +413,6 @@ BASE_FEATURE(kAndroidHub, "AndroidHub", base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAndroidImprovedBookmarks,
              "AndroidImprovedBookmarks",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
-BASE_FEATURE(kAndroidVisibleUrlTruncation,
-             "AndroidVisibleUrlTruncation",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAndroidVisibleUrlTruncationV2,
--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlBarUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlBarUnitTest.java
@@ -69,6 +69,10 @@ public class UrlBarUnitTest {
     private static final int URL_BAR_WIDTH = 100;
     private static final int URL_BAR_HEIGHT = 10;
 
+    // Separately declare a constant same as UrlBar.MIN_LENGTH_FOR_TRUNCATION_V2 so that one of
+    // these tests will fail if it's accidentally changed.
+    private static final int MIN_LENGTH_FOR_TRUNCATION = 100;
+
     private UrlBar mUrlBar;
     public @Rule TestRule mFeaturesProcessorRule = new JUnitProcessor();
     public @Rule MockitoRule mockitoRule = MockitoJUnit.rule();
@@ -77,12 +81,11 @@ public class UrlBarUnitTest {
 
     private final String mShortPath = "/aaaa";
     private final String mLongPath =
-            "/" + TextUtils.join("", Collections.nCopies(UrlBar.MIN_LENGTH_FOR_TRUNCATION_V2, "a"));
+            "/" + TextUtils.join("", Collections.nCopies(MIN_LENGTH_FOR_TRUNCATION, "a"));
     private final String mShortDomain = "www.a.com";
     private final String mLongDomain =
             "www."
-                    + TextUtils.join(
-                            "", Collections.nCopies(UrlBar.MIN_LENGTH_FOR_TRUNCATION_V2, "a"))
+                    + TextUtils.join("", Collections.nCopies(MIN_LENGTH_FOR_TRUNCATION, "a"))
                     + ".com";
 
     // Screen width is set to 100px, with a default density of 1px per dp, and we estimate 5dp per
@@ -240,7 +243,10 @@ public class UrlBarUnitTest {
     @Test
     @EnableFeatures(ChromeFeatureList.ANDROID_VISIBLE_URL_TRUNCATION_V2)
     public void testTruncation_ShortUrl() {
-        String url = mShortDomain + mShortPath;
+        // Test with a url one character shorter than the minimum length for truncation so that this
+        // test fails when the UrlBar.MIN_LENGTH_FOR_TRUCATION_V2 is changed to something smaller.
+        String url = mShortDomain + mLongPath;
+        url = url.substring(0, 99);
         mUrlBar.setTextWithTruncation(url, UrlBar.ScrollType.SCROLL_TO_TLD, mShortDomain.length());
         String text = mUrlBar.getText().toString();
         assertEquals(url, text);
@@ -315,7 +321,7 @@ public class UrlBarUnitTest {
 
     @Test
     @EnableFeatures(ChromeFeatureList.ANDROID_VISIBLE_URL_TRUNCATION_V2)
-    public void testSetLengtHistogram_withTruncation() {
+    public void testSetLengthHistogram_withTruncation() {
         measureAndLayoutUrlBar();
         String url = mShortDomain + mLongPath;
 
