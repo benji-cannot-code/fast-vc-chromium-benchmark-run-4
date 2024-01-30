2326d183e6fd0556745ee2e2c5a60c4565c447c0
Joemer Ramos <joemerramos@chromium.org>
2024-01-30 21:13:36 +0000

    [iOS] Remove Unused Import Header
    Call sites have been refactored to no
    longer used methods in this header file
    so we can remove the imported headers. We
    will delete the files in the next CL.
    
    This CL was uploaded by git cl split.
    
    R=djean@chromium.org
    
    Bug: 1418068
    Change-Id: Ie48dee2bebdc07ddc671e25257794fc839130b92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5245253
    Reviewed-by: David Jean <djean@chromium.org>
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254162}

--- ios/chrome/browser/ui/autofill/manual_fill/manual_fill_accessory_view_controller.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/manual_fill_accessory_view_controller.mm
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/ui/autofill/manual_fill/manual_fill_accessory_view_controller_delegate.h"
-#import "ios/chrome/common/button_configuration_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 #import "ios/chrome/grit/ios_strings.h"
