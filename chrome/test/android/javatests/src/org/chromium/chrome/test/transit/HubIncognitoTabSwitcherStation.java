58e4efb2da4237b517001f766b3cb4ba8d663cba
Calder Kitagawa <ckitagawa@chromium.org>
2024-01-30 22:14:42 +0000

    [Hub] Improve public transit infrastructure
    Add tests for start surface as NTP and switching panes.
    
    Bug: 1521398
    Change-Id: I812c622ea1b54ae7386e343f60fce8e31363b1a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5234695
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254216}

---
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/HubIncognitoTabSwitcherStation.java
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.test.transit;
+
+import org.chromium.chrome.browser.hub.PaneId;
+import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
+
+/** Incognito tab switcher pane station. */
+public class HubIncognitoTabSwitcherStation extends HubTabSwitcherBaseStation {
+    /**
+     * @param chromeTabbedActivityTestRule The activity rule under test.
+     */
+    public HubIncognitoTabSwitcherStation(
+            ChromeTabbedActivityTestRule chromeTabbedActivityTestRule) {
+        super(chromeTabbedActivityTestRule);
+    }
+
+    @Override
+    public @PaneId int getPaneId() {
+        return PaneId.INCOGNITO_TAB_SWITCHER;
+    }
+}
---
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/HubTabSwitcherStation.java
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.test.transit;
+
+import org.chromium.chrome.browser.hub.PaneId;
+import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
+
+/** Regular tab switcher pane station. */
+public class HubTabSwitcherStation extends HubTabSwitcherBaseStation {
+    /**
+     * @param chromeTabbedActivityTestRule The activity rule under test.
+     */
+    public HubTabSwitcherStation(ChromeTabbedActivityTestRule chromeTabbedActivityTestRule) {
+        super(chromeTabbedActivityTestRule);
+    }
+
+    @Override
+    public @PaneId int getPaneId() {
+        return PaneId.TAB_SWITCHER;
+    }
+}
