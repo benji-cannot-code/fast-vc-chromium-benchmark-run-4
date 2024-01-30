45222ad41d213bbb9127022edb64f572055fc3aa
Calder Kitagawa <ckitagawa@chromium.org>
2024-01-30 17:19:23 +0000

    [Hub] Fix crash when destroying metrics
    If the HubTabSwitcherMetricsRecorder is null we shouldn't try to destroy
    it. Also prevent creating the Hub during destroy.
    
    Bug: 1521885
    Fixed: 1523199
    Change-Id: I9939b57804522fe1201f81e2b6122de5a7000629
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5248991
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253976}

--- chrome/android/java/src/org/chromium/chrome/browser/hub/HubProviderUnitTest.java
+++ chrome/android/java/src/org/chromium/chrome/browser/hub/HubProviderUnitTest.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
@@ -20,7 +21,6 @@ import android.app.Activity;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.SmallTest;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -30,6 +30,7 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.robolectric.shadows.ShadowLooper;
 
+import org.chromium.base.Callback;
 import org.chromium.base.supplier.LazyOneshotSupplier;
 import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.test.BaseRobolectricTestRunner;
@@ -58,6 +59,7 @@ public class HubProviderUnitTest {
     private final ObservableSupplierImpl<TabModel> mTabModelSupplier =
             new ObservableSupplierImpl<>();
 
+    @Mock private Callback<HubManager> mHubManagerCallback;
     @Mock private TabModel mTabModel;
     @Mock private TabModelSelector mTabModelSelector;
     @Mock private Pane mMockTabSwitcherPane;
@@ -102,11 +104,6 @@ public class HubProviderUnitTest {
                         () -> mMenuButtonCoordinator);
     }
 
-    @After
-    public void tearDown() {
-        mHubProvider.destroy();
-    }
-
     @Test
     @SmallTest
     public void testHubProvider() {
@@ -115,6 +112,7 @@ public class HubProviderUnitTest {
         var hubManagerSupplier = mHubProvider.getHubManagerSupplier();
         assertNotNull(hubManagerSupplier);
         assertFalse(hubManagerSupplier.hasValue());
+        hubManagerSupplier.onAvailable(mHubManagerCallback);
 
         builder.registerPane(
                 PaneId.TAB_SWITCHER, LazyOneshotSupplier.fromValue(mMockTabSwitcherPane));
@@ -160,5 +158,67 @@ public class HubProviderUnitTest {
         assertEquals(mMockTabSwitcherPane, paneManager.getFocusedPaneSupplier().get());
         verify(mTabModelSelector, times(2)).commitAllTabClosures();
         verify(mTabModelSelector).selectModel(false);
+
+        verify(mHubManagerCallback).bind(any());
+
+        mHubProvider.destroy();
+    }
+
+    @Test
+    @SmallTest
+    public void testHubProviderDestroyBeforeOnAvailable() {
+        PaneListBuilder builder = mHubProvider.getPaneListBuilder();
+
+        var hubManagerSupplier = mHubProvider.getHubManagerSupplier();
+        assertNotNull(hubManagerSupplier);
+        assertFalse(hubManagerSupplier.hasValue());
+
+        builder.registerPane(
+                PaneId.TAB_SWITCHER, LazyOneshotSupplier.fromValue(mMockTabSwitcherPane));
+        builder.registerPane(
+                PaneId.INCOGNITO_TAB_SWITCHER,
+                LazyOneshotSupplier.fromValue(mMockIncognitoTabSwitcherPane));
+        builder.registerPane(PaneId.BOOKMARKS, LazyOneshotSupplier.fromValue(mMockBookmarksPane));
+        assertFalse(builder.isBuilt());
+
+        HubManager hubManager = hubManagerSupplier.get();
+        assertNotNull(hubManager);
+        assertTrue(hubManagerSupplier.hasValue());
+        assertTrue(builder.isBuilt());
+
+        PaneManager paneManager = hubManager.getPaneManager();
+        assertNotNull(paneManager);
+
+        // This shouldn't crash.
+        mHubProvider.destroy();
+
+        ShadowLooper.runUiThreadTasks();
+    }
+
+    @Test
+    @SmallTest
+    public void testHubProviderDestroyBeforeCreation() {
+        PaneListBuilder builder = mHubProvider.getPaneListBuilder();
+
+        var hubManagerSupplier = mHubProvider.getHubManagerSupplier();
+        assertNotNull(hubManagerSupplier);
+        assertFalse(hubManagerSupplier.hasValue());
+        hubManagerSupplier.onAvailable(mHubManagerCallback);
+
+        builder.registerPane(
+                PaneId.TAB_SWITCHER, LazyOneshotSupplier.fromValue(mMockTabSwitcherPane));
+        builder.registerPane(
+                PaneId.INCOGNITO_TAB_SWITCHER,
+                LazyOneshotSupplier.fromValue(mMockIncognitoTabSwitcherPane));
+        builder.registerPane(PaneId.BOOKMARKS, LazyOneshotSupplier.fromValue(mMockBookmarksPane));
+        assertFalse(builder.isBuilt());
+
+        assertFalse(hubManagerSupplier.hasValue());
+        mHubProvider.destroy();
+        ShadowLooper.runUiThreadTasks();
+
+        assertFalse(hubManagerSupplier.hasValue());
+        verify(mHubManagerCallback, never()).onResult(any());
+        verify(mHubManagerCallback, never()).bind(any());
     }
 }
