7f504e2007c6818708787bd82ba9ca39c68beec6
Marco Georgaklis <mgeorgaklis@google.com>
2024-01-30 19:26:20 +0000

    Add opt guide on-device validation override flags.
    This includes kOnDeviceValidationRequestOverride for overriding
    on-device request protos, and kOnDeviceValidationWriteToFile for
    writing the response to a file path.
    
    Bug: b:313716842
    Change-Id: Ifaa4f5ff0988ff011e0a93868f4a635ae8a7f1e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5124202
    Commit-Queue: Marco Georgaklis <mgeorgaklis@google.com>
    Reviewed-by: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254098}

--- chrome/browser/optimization_guide/optimization_guide_keyed_service.h
+++ chrome/browser/optimization_guide/optimization_guide_keyed_service.h
@@ -47,6 +47,7 @@ class ModelExecutionManager;
 class ModelInfo;
 class ModelQualityLogEntry;
 class ModelQualityLogsUploaderService;
+class ModelValidatorKeyedService;
 class OnDeviceModelComponentStateManager;
 class OptimizationGuideStore;
 class PredictionManager;
@@ -191,6 +192,7 @@ class OptimizationGuideKeyedService
   friend class OptimizationGuideWebContentsObserver;
   friend class optimization_guide::ModelExecutionEnterprisePolicyBrowserTest;
   friend class optimization_guide::ModelExecutionLiveTest;
+  friend class optimization_guide::ModelValidatorKeyedService;
   friend class optimization_guide::PredictionManagerBrowserTestBase;
   friend class optimization_guide::PredictionModelDownloadClient;
   friend class optimization_guide::PredictionModelStoreBrowserTestBase;
