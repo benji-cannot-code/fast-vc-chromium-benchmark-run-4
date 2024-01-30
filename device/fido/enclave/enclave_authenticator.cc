7b6f0584275470b16918f255ca08415f27e1244c
Adam Langley <agl@chromium.org>
2024-01-30 19:35:36 +0000

    webauthn: it's a wrapped secret, not a wrapped key.
    It's a "security domain secret" but sometimes the code said
    `wrapped_key` and in other places `wrapped_secret`. Unify on the latter.
    
    Bug: 1459620
    Change-Id: I30f2a89a3abc73819e3bb43324992568db0ec3ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5247171
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254108}

--- device/fido/enclave/enclave_authenticator.cc
+++ device/fido/enclave/enclave_authenticator.cc
@@ -89,8 +89,8 @@ void EnclaveAuthenticator::MakeCredential(CtapMakeCredentialRequest request,
                                           MakeCredentialOptions options,
                                           MakeCredentialCallback callback) {
   CHECK(!pending_get_assertion_request_ && !pending_make_credential_request_);
-  CHECK_EQ(ui_request_->wrapped_keys.size(), 1u);
-  CHECK(ui_request_->wrapped_key_version.has_value());
+  CHECK_EQ(ui_request_->wrapped_secrets.size(), 1u);
+  CHECK(ui_request_->wrapped_secret_version.has_value());
 
   pending_make_credential_request_ =
       std::make_unique<PendingMakeCredentialRequest>(
@@ -100,7 +100,7 @@ void EnclaveAuthenticator::MakeCredential(CtapMakeCredentialRequest request,
            std::move(ui_request_->access_token),
            BuildMakeCredentialCommand(
                std::move(pending_make_credential_request_->options.json),
-               std::move(ui_request_->wrapped_keys.back())),
+               std::move(ui_request_->wrapped_secrets.back())),
            std::move(ui_request_->signing_callback),
            base::BindOnce(&EnclaveAuthenticator::ProcessMakeCredentialResponse,
                           weak_factory_.GetWeakPtr()));
@@ -121,7 +121,7 @@ void EnclaveAuthenticator::GetAssertion(CtapGetAssertionRequest request,
                *ui_request_->entity,
                std::move(pending_get_assertion_request_->options.json),
                pending_get_assertion_request_->request.client_data_json,
-               std::move(ui_request_->wrapped_keys)),
+               std::move(ui_request_->wrapped_secrets)),
            std::move(ui_request_->signing_callback),
            base::BindOnce(&EnclaveAuthenticator::ProcessGetAssertionResponse,
                           weak_factory_.GetWeakPtr()));
@@ -139,7 +139,7 @@ void EnclaveAuthenticator::ProcessMakeCredentialResponse(
   std::tie(opt_response, opt_entity, error_description) =
       ParseMakeCredentialResponse(std::move(*response),
                                   pending_make_credential_request_->request,
-                                  *ui_request_->wrapped_key_version);
+                                  *ui_request_->wrapped_secret_version);
   if (!opt_response || !opt_entity) {
     FIDO_LOG(ERROR) << "Error in registration response from server: "
                     << error_description;
