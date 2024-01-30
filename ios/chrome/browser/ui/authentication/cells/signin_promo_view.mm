9a8c1d21d9335fa3f991d83256daaefca00f9c2c
Joemer Ramos <joemerramos@chromium.org>
2024-01-30 21:21:54 +0000

    [iOS] Remove Unused Import Header
    Call sites have been refactored to no
    longer used methods in this header file
    so we can remove the imported headers. We
    will delete the files in the next CL.
    
    This CL was uploaded by git cl split.
    
    R=myuu@google.com
    
    Bug: 1418068
    Change-Id: I7d7cf60d3c4e41dbe9deb990339f2456620dbaa1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5245372
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Menghan Yang <myuu@google.com>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254171}

--- ios/chrome/browser/ui/authentication/cells/signin_promo_view.mm
+++ ios/chrome/browser/ui/authentication/cells/signin_promo_view.mm
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/ui/authentication/cells/signin_promo_view_constants.h"
 #import "ios/chrome/browser/ui/authentication/cells/signin_promo_view_delegate.h"
-#import "ios/chrome/common/button_configuration_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 #import "ios/chrome/common/ui/util/pointer_interaction_util.h"
