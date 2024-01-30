6f76622641a295dcb451c06895e981082bd90f28
Hiroshige Hayashizaki <hiroshige@chromium.org>
2024-01-30 13:35:21 +0000

    Remove TestPrefetchURLLoaderInterceptor
    `PrefetchService::GetPrefetchToServe` was totally skipped
    in `PrefetchURLLoaderInterceptorTest` because of
    `TestPrefetchURLLoaderInterceptor::GetPrefetch`, but this
    made the invariants different between tests and non-test code,
    for example as discussed in [1].
    
    To make the test coverage more realistic,
    this CL removes `TestPrefetchURLLoaderInterceptor` and
    use the non-test `PrefetchURLLoaderInterceptor` and
    `PrefetchService` code instead.
    
    By doing so, this CL re-enables
    `DoNotInterceptNavigationCookiesChanged` test that was
    removed due to this invariant inconsistency.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/5232813/comment/459b89b8_b127a828/
    
    Bug: 1491889
    Change-Id: Iafbfa88cb41367936c545f795c3c6d471fb20c7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5239156
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253865}

--- content/browser/preloading/prefetch/prefetch_service.cc
+++ content/browser/preloading/prefetch/prefetch_service.cc
@@ -338,7 +338,7 @@ PrefetchOriginProber* PrefetchService::GetPrefetchOriginProber() const {
   return origin_prober_.get();
 }
 
-void PrefetchService::AddPrefetchContainer(
+void PrefetchService::AddPrefetchContainerWithoutStartingPrefetch(
     std::unique_ptr<PrefetchContainer> owned_prefetch_container) {
   base::WeakPtr<PrefetchContainer> prefetch_container =
       owned_prefetch_container->GetWeakPtr();
@@ -363,7 +363,14 @@ void PrefetchService::AddPrefetchContainer(
 
   owned_prefetches_[prefetch_container_key] =
       std::move(owned_prefetch_container);
+}
 
+void PrefetchService::AddPrefetchContainer(
+    std::unique_ptr<PrefetchContainer> owned_prefetch_container) {
+  base::WeakPtr<PrefetchContainer> prefetch_container =
+      owned_prefetch_container->GetWeakPtr();
+  AddPrefetchContainerWithoutStartingPrefetch(
+      std::move(owned_prefetch_container));
   PrefetchUrl(std::move(prefetch_container));
 }
 
