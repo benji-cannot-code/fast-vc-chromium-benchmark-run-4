33fd0c110a9fd4f9ad0d2c2e8c3eaf7e175f22ca
Kyungjun Lee <kyungjunlee@google.com>
2024-01-30 02:43:29 +0000

    PDF OCR: enable E2E tests for macOS
    AX-Relnotes: n/a.
    Bug: b/40267312
    Change-Id: Ica8595d25d75d808bd155e7cf98372f80971c29b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5217891
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Kyungjun Lee <kyungjunlee@google.com>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253701}

--- chrome/browser/pdf/pdf_extension_accessibility_test.cc
+++ chrome/browser/pdf/pdf_extension_accessibility_test.cc
@@ -67,13 +67,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/renderer_context_menu/pdf_ocr_menu_observer.h"
 #endif  // BUILDFLAG(ENABLE_SCREEN_AI_SERVICE)
 
-#if BUILDFLAG(IS_LINUX)
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC)
 #include "base/scoped_observation.h"
 #include "chrome/browser/screen_ai/screen_ai_install_state.h"
 #include "chrome/common/pref_names.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
-#endif  // BUILDFLAG(IS_LINUX)
+#endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC)
 
 namespace {
 
@@ -181,7 +181,8 @@ constexpr char kExpectedPDFAXTree[] =
     "      staticText '3'\n"
     "        inlineTextBox '3'\n";
 
-#if BUILDFLAG(IS_LINUX) && !defined(MEMORY_SANITIZER)
+#if (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC)) && !defined(MEMORY_SANITIZER)
+
 constexpr char kExpectedHelloWorldPDFAXTreeWithOcrResults[] =
     "pdfRoot 'PDF document containing 1 page'\n"
     "  banner\n"
@@ -218,7 +219,9 @@ constexpr char kExpectedBlankPDFAXTreeWithPdfOcr[] =
     "  region 'Page 1'\n"
     "    paragraph\n"
     "      image 'Unlabeled image'\n";
-#endif  // BUILDFLAG(IS_LINUX) && !defined(MEMORY_SANITIZER)
+
+#endif  // (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC)) &&
+        // !defined(MEMORY_SANITIZER)
 
 }  // namespace
 
@@ -1250,7 +1253,7 @@ INSTANTIATE_FEATURE_OVERRIDE_TEST_SUITE(PDFExtensionAccessibilityPdfOcrTest);
 
 // TODO(crbug.com/1516559): Add a dummy library that is built with Chrome for
 // memory sanitizer tests.
-#if BUILDFLAG(IS_LINUX) && !defined(MEMORY_SANITIZER)
+#if (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC)) && !defined(MEMORY_SANITIZER)
 
 class ScreenAIInstallStateObserver
     : public screen_ai::ScreenAIInstallState::Observer {
@@ -1447,4 +1450,5 @@ IN_PROC_BROWSER_TEST_F(PDFOCRIntegrationTest, NoOcrResultOnBlankImagePdf) {
   ASSERT_MULTILINE_STREQ(kExpectedBlankPDFAXTreeWithPdfOcr, ax_tree_dump);
 }
 
-#endif  // BUILDFLAG(IS_LINUX) && !defined(MEMORY_SANITIZER)
+#endif  // (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC)) &&
+        // !defined(MEMORY_SANITIZER)
