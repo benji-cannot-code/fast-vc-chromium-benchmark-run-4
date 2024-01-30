3eed84b2e53cc68b3aeb5a0c67116151a138f0c1
Sean Maher <spvw@chromium.org>
2024-01-30 21:43:23 +0000

    base ThreadPool: Implement ThreadGroupSemaphore
    It is implemented behind a base::Feature.
    
    Note. It is hard to figure out the diff between ThreadGroupImpl and
    ThreadGroupSemaphore, so I've made it here:
    https://fighet-parnet.github.io/2024/01/24/diffnew.html
    
    Also note, thread_group_semaphore_unittest is a copy-pasted
    thread_group_impl_unittest with s/TGI/TGS/g (but also one slight
    change on an overspecified-test (VerifyCleanup). Here is diff:
    https://paste.googleplex.com/5817060767039488
    
    Last patchset this was made for: 34
    
    I will try to update this diff with the most recent version, but this
    is how it was generated:
    
    diff -U 1000000 --color=always base/task/thread_pool/thread_group_impl.cc base/task/thread_pool/thread_group_semaphore.cc | pastebin -r -l term-ansi
    
    Bug: 1481215
    Change-Id: I9981c766d850696967399ee0822a39a24363b92c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5186974
    Commit-Queue: Sean Maher <spvw@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254200}

--- base/task/thread_pool/thread_group_worker_delegate.h
+++ base/task/thread_pool/thread_group_worker_delegate.h
@@ -22,7 +22,8 @@ namespace base::internal {
 // ThreadGroup.
 class ThreadGroup::ThreadGroupWorkerDelegate : public BlockingObserver {
  public:
-  explicit ThreadGroupWorkerDelegate(TrackedRef<ThreadGroup> outer);
+  explicit ThreadGroupWorkerDelegate(TrackedRef<ThreadGroup> outer,
+                                     bool is_excess);
   ThreadGroupWorkerDelegate(const ThreadGroupWorkerDelegate&) = delete;
   ThreadGroupWorkerDelegate& operator=(const ThreadGroupWorkerDelegate&) =
       delete;
@@ -54,9 +55,6 @@ class ThreadGroup::ThreadGroupWorkerDelegate : public BlockingObserver {
     return *read_any().current_task_priority;
   }
 
-  // True if the calling worker is be eligible for reclaim.
-  virtual bool IsExcess() const = 0;
-
   // Exposed for AnnotateAcquiredLockAlias.
   const CheckedLock& lock() const LOCK_RETURNED(outer_->lock_) {
     return outer_->lock_;
@@ -74,6 +72,10 @@ class ThreadGroup::ThreadGroupWorkerDelegate : public BlockingObserver {
                                       WorkerThread* worker)
       EXCLUSIVE_LOCKS_REQUIRED(outer_->lock_) = 0;
 
+  RegisteredTaskSource GetWorkLockRequired(BaseScopedCommandsExecutor* executor,
+                                           WorkerThread* worker)
+      EXCLUSIVE_LOCKS_REQUIRED(outer_->lock_);
+
   // Calls cleanup on |worker| and removes it from the thread group. Called from
   // GetWork() when no work is available and CanCleanupLockRequired() returns
   // true.
@@ -85,6 +87,10 @@ class ThreadGroup::ThreadGroupWorkerDelegate : public BlockingObserver {
   virtual void OnWorkerBecomesIdleLockRequired(
       BaseScopedCommandsExecutor* executor,
       WorkerThread* worker) EXCLUSIVE_LOCKS_REQUIRED(outer_->lock_) = 0;
+
+  // See worker_thread.h for documentation.
+  void OnMainEntryImpl(WorkerThread* worker);
+  void RecordUnnecessaryWakeupImpl();
 
   // Only used in DCHECKs.
   template <typename Worker>
@@ -147,6 +153,11 @@ class ThreadGroup::ThreadGroupWorkerDelegate : public BlockingObserver {
   }
 
   const TrackedRef<ThreadGroup> outer_;
+
+  // Whether the worker is in excess. This must be decided at worker creation
+  // time to prevent unnecessarily discarding TLS state, as well as any behavior
+  // the OS has learned about a given thread.
+  const bool is_excess_;
 
   // Whether |outer_->max_tasks_|/|outer_->max_best_effort_tasks_| were
   // incremented due to a ScopedBlockingCall on the thread.
