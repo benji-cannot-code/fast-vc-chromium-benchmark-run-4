3d1d163e7076fbf262cb7e1260b6d00bae8d72ba
Thomas Lukaszewicz <tluk@chromium.org>
2024-01-30 21:02:34 +0000

    [exo] Trigger output activation notifications from wayland::Server
    Currently each instance of WaylandDisplayHandler (client-specific
    output handle in exo) maintains its own aura shell observation
    so that it can notify clients when the output it corresponds to
    has been activated.
    
    This CL moves the observation into exo::wayland::Server code and
    delegates to the appropriate WaylandDisplayOutput global wrapper
    where appropriate.
    
    This CL concludes the centralization of display-related
    observations into the Server class. The result is cleaner and easier
    to understand code.
    
    Follow up CLs will extract display-output related logic out of
    Server and into an independent class responsible for handling
    wayland output objects in exo, making new output extensions easier
    to implement and support.
    
    Note: Since output activation is now propagated from Server, the
    ActiveDisplay unittest has been moved into the aura shell server
    tests. This allows correct widget activation => client output event
    testing post refactor.
    
    Bug: 1518335
    Change-Id: I3d75d22eb043ac78c094880cb86bbd64b8a9858b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5219895
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254151}

--- components/exo/wayland/wayland_display_observer.h
+++ components/exo/wayland/wayland_display_observer.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
-#include "ash/shell_observer.h"
 #include "base/memory/raw_ptr.h"
 #include "base/observer_list.h"
 #include "ui/display/display.h"
@@ -43,8 +42,7 @@ class WaylandDisplayObserver : public base::CheckedObserver {
   ~WaylandDisplayObserver() override;
 };
 
-class WaylandDisplayHandler : public WaylandDisplayObserver,
-                              public ash::ShellObserver {
+class WaylandDisplayHandler : public WaylandDisplayObserver {
  public:
   WaylandDisplayHandler(WaylandDisplayOutput* output,
                         wl_resource* output_resource);
@@ -63,6 +61,10 @@ class WaylandDisplayHandler : public WaylandDisplayObserver,
   // metrics events were dispatched to the client.
   void SendDisplayMetricsChanges(const display::Display& display,
                                  uint32_t changed_metrics);
+
+  // Called when the output associated with this handler is activated and sends
+  // the appropriate output events to the client.
+  void SendDisplayActivated();
 
   // Called when an xdg_output object is created through get_xdg_output()
   // request by the wayland client.
@@ -91,9 +93,6 @@ class WaylandDisplayHandler : public WaylandDisplayObserver,
   // required, |false| otherwise.
   bool SendXdgOutputMetrics(const display::Display& display,
                             uint32_t changed_metrics);
-
-  // ShellObserver:
-  void OnDisplayForNewWindowsChanged() override;
 
   // Gets the AuraOutputManager instance associated with this handler, may
   // return null.
