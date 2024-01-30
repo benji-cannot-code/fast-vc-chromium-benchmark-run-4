e5ecccd45c0cb447f1541800718528cd446b1695
Patrick To <patrto@microsoft.com>
2024-01-30 05:15:30 +0000

    WebNN: Define DirectML nodes for l2Pool2d
    Defines the DirectML nodes for the l2Pool2d operator.
    The Pool2d interface is extended to support l2Pool2d.
    
    Updated pooling unit tests to include l2Pool2d.
    
    Bug: 1273291
    Change-Id: I5dde4ee623de78f51286b6c00a69b57fa25eaa19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5233181
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Commit-Queue: Patrick To <patrto@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1253736}

--- services/webnn/webnn_graph_impl_unittest.cc
+++ services/webnn/webnn_graph_impl_unittest.cc
@@ -3570,6 +3570,7 @@ struct Pool2dTester {
 
   void Test() {
     Test(mojom::Pool2d::Kind::kAveragePool2d);
+    Test(mojom::Pool2d::Kind::kL2Pool2d);
     Test(mojom::Pool2d::Kind::kMaxPool2d);
   }
 
