b9e580e7a45640a698f0ac1276220377a8952cb7
Stanley Hon <stahon@microsoft.com>
2024-01-30 20:44:20 +0000

    Fix browser system media controls not going away.
    With #enable-web-app-system-media-controls-win, system media controls
    for the browser was not going away when the last tab playing audio was
    closed.
    
    Furthermore, with #enable-web-app-system-media-controls-win enabled, when one tab playing audio is closed - the
    browser smc was not able to fall back to other tabs playing audio.
    
    This change reworks instanced system media controls to utilize an active
    media session controller that follows the active browser sessions while
    ignoring web app sessions. This allows the browser to continue to use
    an AMSC that follows the active session, thus restoring fallback and
    automatic dismissal when browser sessions are closed.
    
    **For security/mojo review**:
    
    These changes are part of this design doc: https://docs.google.com/document/d/12dVKreUCjtZZaUqeh3VxGMsGPMC4POhK/edit
    
    We originally did not anticipate any mojo changes for this feature, but in order to fix the bug outlined in this CL - we needed to make this small change.
    
    The mojo changes do not intend to change any existing structure of information passing - only that services/media_session/audio_focus_manager needs to take different action based on a flag set from content/browser/media/session/media_session_impl.
    
    The purpose of this flag is essentially to mark the media session as for web apps - thus causing it not to be eligible to become the 'active session'. The reason this is desirable is because web app sessions are tracked separately by an "ActiveMediaSessionController" that specifically follows one session and does not follow around an active session.
    
    The user of an "ActiveMediaSessionController" that follows the active session is only the browser - where all tabbed browser media sessions bubble up to one singular concept of "the media the browser is currently playing". Thus needs to change based on what the user interacted with most recently. In other words, we do not want a web apps' media session to show up in the browser's singleton instance when the web app is activated, used or updated.
    
    Bug: 1517758
    Change-Id: Ic4839bbca89d4d743ca406d4439d396ae69de9e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5235430
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Stanley Hon <stahon@microsoft.com>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254143}

--- content/browser/media/web_app_system_media_controls_browsertest.cc
+++ content/browser/media/web_app_system_media_controls_browsertest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/unguessable_token.h"
 #include "content/browser/browser_main_loop.h"
 #include "content/browser/media/media_keys_listener_manager_impl.h"
+#include "content/browser/media/session/media_session_impl.h"
 #include "content/browser/media/web_app_system_media_controls.h"
 #include "content/browser/media/web_app_system_media_controls_manager.h"
 #include "content/public/common/content_features.h"
@@ -138,14 +139,9 @@ class WebAppSystemMediaControlsBrowserTest
         true;
   }
 
-  // This mechanism allows us to wait for the browser to be added to
-  // WebAppSystemMediaControls bookkeeping.
-  void OnBrowserAdded() override { browser_added_run_loop_->Quit(); }
-
-  void WaitForBrowserAdded() {
-    browser_added_run_loop_->Run();
-    // Reset the runloop for the next use.
-    browser_added_run_loop_.emplace();
+  void SetAlwaysIgnoreMediaSessionForTesting(WebContents* web_contents) {
+    MediaSessionImpl* media_session = MediaSessionImpl::Get(web_contents);
+    media_session->always_ignore_for_active_session_for_testing_ = true;
   }
 
   // This mechanism allows us to wait for a web app to be added to the
@@ -219,19 +215,9 @@ IN_PROC_BROWSER_TEST_F(WebAppSystemMediaControlsBrowserTest,
   // Check video is playing.
   EXPECT_TRUE(IsPlaying(shell(), "long-video-loop"));
 
-  // Wait till the WebAppSystemMediaControlsManager adds the browser.
-  WaitForBrowserAdded();
-
   // Hit pause via simulating SMTC pause.
   MediaKeysListenerManagerImpl* media_keys_listener_manager_impl =
       BrowserMainLoop::GetInstance()->media_keys_listener_manager();
-
-  // Unfortunately, even though we wait for the browser to be added the
-  // MediaKeysListenerManager can still not have the browser registered
-  // properly. We have to wait for MediaKeysListenerManager to also add it to
-  // it's bookkeeping.
-  bool is_for_pwa = WaitForStartWatchingMediaKey();
-  EXPECT_FALSE(is_for_pwa);
 
   // Check video is still playing.
   EXPECT_TRUE(IsPlaying(shell(), "long-video-loop"));
@@ -264,10 +250,7 @@ IN_PROC_BROWSER_TEST_F(WebAppSystemMediaControlsBrowserTest, ThreeBrowserTest) {
   // Now we have 3 things playing at the same time. Browser 3 should have
   // control and be shown in SMTC.
 
-  // Wait till the WebAppSystemMediaControlsManager adds the browser.
-  WaitForBrowserAdded();
-
-  // Also wait until MediaKeysListenerManagerImpl starts listening for keys.
+  // Wait until MediaKeysListenerManagerImpl starts listening for keys.
   bool is_for_pwa = WaitForStartWatchingMediaKey();
   EXPECT_FALSE(is_for_pwa);
 
@@ -311,6 +294,7 @@ IN_PROC_BROWSER_TEST_F(WebAppSystemMediaControlsBrowserTest,
   }
 
   SetAlwaysAssumeWebAppForTesting();
+  SetAlwaysIgnoreMediaSessionForTesting(shell()->web_contents());
 
   StartPlaybackAndWait(web_app, "long-video-loop");
   base::UnguessableToken request_id = WaitForWebAppAdded();
@@ -360,6 +344,9 @@ IN_PROC_BROWSER_TEST_F(WebAppSystemMediaControlsBrowserTest, ThreeWebAppTest) {
 
   // Start all the playbacks.
   SetAlwaysAssumeWebAppForTesting();
+  SetAlwaysIgnoreMediaSessionForTesting(web_app1->web_contents());
+  SetAlwaysIgnoreMediaSessionForTesting(web_app2->web_contents());
+  SetAlwaysIgnoreMediaSessionForTesting(web_app3->web_contents());
 
   base::UnguessableToken web_app1_request_id;
   base::UnguessableToken web_app2_request_id;
@@ -437,6 +424,8 @@ IN_PROC_BROWSER_TEST_F(WebAppSystemMediaControlsBrowserTest, TelemetryTest) {
 
   // Start playback.
   SetAlwaysAssumeWebAppForTesting();
+  SetAlwaysIgnoreMediaSessionForTesting(web_app1->web_contents());
+  SetAlwaysIgnoreMediaSessionForTesting(web_app2->web_contents());
 
   // Load a video from web_app1.
   StartPlaybackAndWait(web_app1, "long-video-loop");
@@ -512,6 +501,63 @@ IN_PROC_BROWSER_TEST_F(WebAppSystemMediaControlsBrowserTest, TelemetryTest) {
         "WebApp.Media.SystemMediaControls",
         WebAppSystemMediaControlsEvent::kPwaSmcSeekTo, 1);
   }
+}
+
+IN_PROC_BROWSER_TEST_F(WebAppSystemMediaControlsBrowserTest, TwoBrowserTest) {
+  // Navigate two shells to the page.
+  GURL http_url(https_server()->GetURL("/media/session/media-session.html"));
+  EXPECT_TRUE(NavigateToURL(shell(), http_url));
+
+  Shell* browser2 = CreateBrowser();
+  EXPECT_TRUE(NavigateToURL(browser2, http_url));
+
+  EXPECT_TRUE(NavigateToURL(shell(), http_url));
+  EXPECT_TRUE(NavigateToURL(browser2, http_url));
+
+  // Start two playbacks, both from the browser.
+  {
+    StartPlaybackAndWait(shell(), "long-video-loop");
+    EXPECT_TRUE(IsPlaying(shell(), "long-video-loop"));
+
+    // Wait for the playing audio to register in the media keys listener
+    // manager.
+    WaitForStartWatchingMediaKey();
+
+    StartPlaybackAndWait(browser2, "long-video-loop");
+    EXPECT_TRUE(IsPlaying(browser2, "long-video-loop"));
+
+    WaitForStartWatchingMediaKey();
+  }
+
+  // Now retrieve the SMC used for the browser and make a call to pause the
+  // video.
+  system_media_controls::SystemMediaControls* system_media_controls =
+      GetBrowserSystemMediaControls();
+
+  MediaKeysListenerManagerImpl* media_keys_listener_manager_impl =
+      BrowserMainLoop::GetInstance()->media_keys_listener_manager();
+
+  media_keys_listener_manager_impl->OnPause(system_media_controls);
+
+  // The browser2 should be paused.
+  WaitForStop(browser2);
+
+  // The shell is still playing.
+  EXPECT_TRUE(IsPlaying(shell(), "long-video-loop"));
+
+  // Close browser2, this will cause the controlled browser to fall back to
+  // shell.
+  browser2->Close();
+
+  // Wait a little for the fallback to occur.
+  WaitForStartWatchingMediaKey();
+
+  // Now hit pause again, it should pause shell().
+  media_keys_listener_manager_impl->OnPause(system_media_controls);
+  WaitForStop(shell());
+
+  // The browser is still playing.
+  EXPECT_FALSE(IsPlaying(shell(), "long-video-loop"));
 }
 
 }  // namespace content
