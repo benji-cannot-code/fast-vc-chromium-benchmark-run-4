83b8b3b1cff3bd088a4f7e1225261cb65fff9095
Joemer Ramos <joemerramos@chromium.org>
2024-01-30 21:47:15 +0000

    [iOS] Remove Unused Import Header
    Call sites have been refactored to no
    longer used methods in this header file
    so we can remove the imported headers. We
    will delete the files in the next CL.
    
    This CL was uploaded by git cl split.
    
    R=jlebel@chromium.org
    
    Bug: 1418068
    Change-Id: If3a62e262a7886fbf2e42b93f1f46c4eb254a943
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5246279
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254203}

--- ios/chrome/browser/ui/account_picker/account_picker_confirmation/account_picker_confirmation_screen_view_controller.mm
+++ ios/chrome/browser/ui/account_picker/account_picker_confirmation/account_picker_confirmation_screen_view_controller.mm
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/account_picker/account_picker_layout_delegate.h"
 #import "ios/chrome/browser/ui/authentication/views/identity_button_control.h"
 #import "ios/chrome/browser/ui/authentication/views/identity_view.h"
-#import "ios/chrome/common/button_configuration_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/button_util.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
