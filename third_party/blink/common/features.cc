f34c194a178ac9e9c75217fb4aa742b29b413a4f
Ian Clelland <iclelland@chromium.org>
2024-01-30 16:17:17 +0000

    Remove two LCP flags
    This removes the feature flags kLCPAnimatedImagesReporting and
    kLCPVideoFirstFrame, which have both been shipped for several
    milestones.
    
    Bug: 1260953, 1289664
    Change-Id: I06fc970d271020a19185cab37be0d00d14093b21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5228639
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253940}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1158,12 +1158,6 @@ BASE_FEATURE(kAttributionReportingInBrowserMigration,
              "kAttributionReportingInBrowserMigration",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-// Enables reporting as LCP of the time the first frame of an animated image was
-// painted.
-BASE_FEATURE(kLCPAnimatedImagesReporting,
-             "LCPAnimatedImagesReporting",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 BASE_FEATURE(kLCPCriticalPathPredictor,
              "LCPCriticalPathPredictor",
              base::FEATURE_DISABLED_BY_DEFAULT);
@@ -1288,11 +1282,6 @@ const base::FeatureParam<bool>
     kHttpDiskCachePrewarmingTriggerOnPointerDownOrHover{
         &kHttpDiskCachePrewarming,
         "http_disk_cache_prewarming_trigger_on_pointer_down_or_hover", true};
-
-// Enables reporting as LCP of the time the first frame of a video was painted.
-BASE_FEATURE(kLCPVideoFirstFrame,
-             "LCPVideoFirstFrame",
-             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kLegacyParsingOfXContentTypeOptions,
              "LegacyParsingOfXContentTypeOptions",
