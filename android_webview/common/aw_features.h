c2081c5ec5206bec7778a6bebf383ddf107292aa
Aashna <aashnajena@google.com>
2024-01-30 12:03:56 +0000

    [sbv5] Clean up aw flag WebViewConnectionlessSafeBrowsing
    As pointed out in go/clank-23q4-fixit-flags, this flag can be cleaned up
    because WebView does not use AwConnectionlessSafetyNetHandler.
    
    Bug: 1522834
    Change-Id: I262abb3fce532079899331afb230f69356524dda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5244612
    Commit-Queue: Aashna Jena <aashnajena@google.com>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253841}

--- android_webview/common/aw_features.h
+++ android_webview/common/aw_features.h
@@ -19,7 +19,6 @@ namespace features {
 // Alphabetical:
 BASE_DECLARE_FEATURE(kWebViewBackForwardCache);
 BASE_DECLARE_FEATURE(kWebViewBrotliSupport);
-BASE_DECLARE_FEATURE(kWebViewConnectionlessSafeBrowsing);
 BASE_DECLARE_FEATURE(kWebViewCheckPakFileDescriptors);
 BASE_DECLARE_FEATURE(kWebViewClearFunctorInBackground);
 BASE_DECLARE_FEATURE(kWebViewDisplayCutout);
