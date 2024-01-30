5673a2082d2d6258b39305feef3bac06ea4b042e
Sylvain Defresne <sdefresne@chromium.org>
2024-01-30 16:44:45 +0000

    [ios] Fix BringAndroidTabsToIOSServiceTest.* tests
    Those tests depends on `IsFirstRunRecent(...)` returning `YES`, so
    clear the "first run" sentinel to ensure this is the case even if
    another test suite leave an old sentinel file around.
    
    Bug: none
    Change-Id: I4822e2c5a3033a84a88e16a72bc2f88ab1fcdff2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5249075
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1253954}

--- ios/chrome/browser/bring_android_tabs/model/bring_android_tabs_to_ios_service_unittest.mm
+++ ios/chrome/browser/bring_android_tabs/model/bring_android_tabs_to_ios_service_unittest.mm
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/sync_sessions/session_sync_test_helper.h"
 #import "components/sync_sessions/synced_session.h"
 #import "ios/chrome/browser/bring_android_tabs/model/metrics.h"
+#import "ios/chrome/browser/first_run/model/first_run.h"
 #import "ios/chrome/browser/segmentation_platform/model/segmentation_platform_config.h"
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
@@ -246,6 +247,9 @@ class MockOpenTabsUIDelegate : public sync_sessions::OpenTabsUIDelegate {
 class BringAndroidTabsToIOSServiceTest : public PlatformTest {
  protected:
   BringAndroidTabsToIOSServiceTest() : PlatformTest() {
+    FirstRun::RemoveSentinel();
+    FirstRun::ClearStateForTesting();
+
     browser_state_ = TestChromeBrowserState::Builder().Build();
     browser_ = std::make_unique<TestBrowser>(browser_state_.get());
     device_info_tracker_ = std::make_unique<syncer::FakeDeviceInfoTracker>();
