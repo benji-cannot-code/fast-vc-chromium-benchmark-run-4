32976a030f596a18ebd3bd3c3b64be4a13890bf7
Edman Anjos <edmanp@google.com>
2024-01-30 15:49:45 +0000

    kiosk: Add a parent window to the diagnose dialog
    This is the diagnose dialog reachable from the network dialog during
    Kiosk launch. The dialog is currently broken, when you click "diagnose"
    nothing happens at the time. The dialog is actually created behind the
    splash screen, and sometimes is visible above the Kiosk app after
    launch.
    
    When in-session the dialog can be leveraged among other bugs to escape
    Kiosk.
    
    This change adds a parent window to the dialog, so it is displayed in
    the login screen correctly and gets closed when the login screen is gone
    after Kiosk launches.
    
      "diagnose", see the dialog appears, connect to internet, see the kiosk
      launches and the dialog disappears.
    
    Bug: b:322775376
    Test: manual - launch kiosk while offline, see the network dialog, click
    Change-Id: I802055a19b56b4fbe8bfd2e04dcf937eeac73e8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5249069
    Reviewed-by: Ben Franz <bfranz@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253921}

--- chrome/browser/ui/webui/ash/connectivity_diagnostics_dialog.cc
+++ chrome/browser/ui/webui/ash/connectivity_diagnostics_dialog.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/connectivity_diagnostics/url_constants.h"
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
+#include "ui/gfx/native_widget_types.h"
 
 namespace {
 
@@ -22,9 +23,9 @@ const float kConnectivityDiagnosticsDialogScale = .8;
 namespace ash {
 
 // static
-void ConnectivityDiagnosticsDialog::ShowDialog() {
+void ConnectivityDiagnosticsDialog::ShowDialog(gfx::NativeWindow parent) {
   ConnectivityDiagnosticsDialog* dialog = new ConnectivityDiagnosticsDialog();
-  dialog->ShowSystemDialog();
+  dialog->ShowSystemDialog(parent);
 }
 
 ConnectivityDiagnosticsDialog::ConnectivityDiagnosticsDialog()
--- chrome/browser/ui/webui/ash/connectivity_diagnostics_dialog.h
+++ chrome/browser/ui/webui/ash/connectivity_diagnostics_dialog.h
@@ -7,12 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_WEBUI_ASH_CONNECTIVITY_DIAGNOSTICS_DIALOG_H_
 
 #include "chrome/browser/ui/webui/ash/system_web_dialog_delegate.h"
+#include "ui/gfx/native_widget_types.h"
 
 namespace ash {
 
 class ConnectivityDiagnosticsDialog : public SystemWebDialogDelegate {
  public:
-  static void ShowDialog();
+  static void ShowDialog(gfx::NativeWindow parent);
 
  protected:
   ConnectivityDiagnosticsDialog();
