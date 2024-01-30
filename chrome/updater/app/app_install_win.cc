c21655c4bde5f96384a9e5967aaeb3cf53c0677f
S. Ganesh <ganesh@chromium.org>
2024-01-30 23:18:32 +0000

    updater: Add download time remaining and install time remaining
    This allows showing the download time remaining as well as the install
    time remaining in the UI, as well as providing the time remaining to the
    COM API.
    
    Fixed: 1521972
    Change-Id: Ia45731c1afe66f0e7b6933195692aa829129c25e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5247724
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254242}

--- chrome/updater/app/app_install_win.cc
+++ chrome/updater/app/app_install_win.cc
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/updater/update_service_internal.h"
 #include "chrome/updater/updater_branding.h"
 #include "chrome/updater/updater_scope.h"
+#include "chrome/updater/util/progress_sampler.h"
 #include "chrome/updater/util/util.h"
 #include "chrome/updater/util/win_util.h"
 #include "chrome/updater/win/installer/exit_code.h"
@@ -462,6 +463,9 @@ class AppInstallControllerImpl : public AppInstallController,
   base::OnceCallback<void(int)> callback_;
 
   const bool is_silent_install_ = false;
+
+  ProgressSampler download_progress_sampler_;
+  ProgressSampler install_progress_sampler_;
 };
 
 AppInstallControllerImpl::AppInstallControllerImpl(
@@ -473,7 +477,9 @@ AppInstallControllerImpl::AppInstallControllerImpl(
            base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN},
           base::SingleThreadTaskRunnerThreadMode::DEDICATED)),
       update_service_(update_service),
-      is_silent_install_(is_silent_install) {}
+      is_silent_install_(is_silent_install),
+      download_progress_sampler_(base::Seconds(5), base::Seconds(1)),
+      install_progress_sampler_(base::Seconds(5), base::Seconds(1)) {}
 AppInstallControllerImpl::~AppInstallControllerImpl() = default;
 
 void AppInstallControllerImpl::InstallApp(
@@ -733,18 +739,37 @@ void AppInstallControllerImpl::StateChange(
     case UpdateService::UpdateState::State::kDownloading: {
       const auto pos = GetDownloadProgress(update_state.downloaded_bytes,
                                            update_state.total_bytes);
-      install_progress_observer_ipc_->OnDownloading(app_id_, app_name_, -1,
-                                                    pos != -1 ? pos : 0);
-    } break;
+      if (pos >= 0) {
+        download_progress_sampler_.AddSample(update_state.downloaded_bytes);
+      }
+      const std::optional<base::TimeDelta> remaining_download_time =
+          download_progress_sampler_.GetRemainingTime(update_state.total_bytes);
+      install_progress_observer_ipc_->OnDownloading(
+          app_id_, app_name_,
+          remaining_download_time ? remaining_download_time->InMilliseconds()
+                                  : -1,
+          pos >= 0 ? pos : 0);
+      break;
+    }
 
     case UpdateService::UpdateState::State::kInstalling: {
       // TODO(crbug.com/1290331): handle the install cancellation.
       bool can_start_install = false;
       install_progress_observer_ipc_->OnWaitingToInstall(app_id_, app_name_,
                                                          &can_start_install);
+
+      // Install progress goes from 0 to 100.
       const int pos = update_state.install_progress;
-      install_progress_observer_ipc_->OnInstalling(app_id_, app_name_, 0,
-                                                   pos != -1 ? pos : 0);
+      if (pos >= 0) {
+        install_progress_sampler_.AddSample(pos);
+      }
+      const std::optional<base::TimeDelta> remaining_install_time =
+          install_progress_sampler_.GetRemainingTime(100);
+      install_progress_observer_ipc_->OnInstalling(
+          app_id_, app_name_,
+          remaining_install_time ? remaining_install_time->InMilliseconds()
+                                 : -1,
+          pos >= 0 ? pos : 0);
       break;
     }
 
