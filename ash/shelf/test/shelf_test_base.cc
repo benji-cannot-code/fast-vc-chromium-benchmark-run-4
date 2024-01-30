c5b7648fe630d733eefb30fe2066e27e84d049e2
Owen Zhang <owenzhang@google.com>
2024-01-30 21:34:46 +0000

    [App Shortcuts] Create Pixel Tests for Shortcut Icon Effects in Shelf.
    Bug: b/316049065
    Change-Id: Ibbc6b8671479f0a1de89c33c651f786a8933296d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5229321
    Auto-Submit: Owen Zhang <owenzhang@google.com>
    Commit-Queue: Owen Zhang <owenzhang@google.com>
    Reviewed-by: Ana Salazar <anasalazar@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254187}

--- ash/shelf/test/shelf_test_base.cc
+++ ash/shelf/test/shelf_test_base.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shelf/shelf_view_test_api.h"
 #include "ash/shelf/shelf_widget.h"
 #include "ash/test/ash_test_util.h"
+#include "ui/gfx/image/image_unittest_util.h"
 
 namespace ash {
 
@@ -58,6 +59,16 @@ void ShelfTestBase::AddAppShortcutsUntilOverflow(bool use_alternative_color) {
                              ? icon_color_generator_.GetAlternativeColor()
                              : icon_color_generator_.default_color());
   }
+}
+
+ShelfItem ShelfTestBase::AddWebAppShortcut() {
+  const int icon_dimension = 28;
+  const int badge_icon_dimension = 18;
+  ShelfItem item = ShelfTestUtil::AddWebAppShortcut(
+      "shortcut_id", true,
+      gfx::test::CreateImageSkia(icon_dimension, SK_ColorRED),
+      gfx::test::CreateImageSkia(badge_icon_dimension, SK_ColorCYAN));
+  return item;
 }
 
 ShelfID ShelfTestBase::AddAppShortcutWithIconColor(ShelfItemType item_type,
