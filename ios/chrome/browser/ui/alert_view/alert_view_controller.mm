b8ebc1799602a5c9bee533eecce45fb105a9ad57
Joemer Ramos <joemerramos@chromium.org>
2024-01-30 15:48:42 +0000

    [iOS] Remove Unused Import Header
    Call sites have been refactored to no
    longer used methods in this header file
    so we can remove the imported headers. We
    will delete the files in the next CL.
    
    This CL was uploaded by git cl split.
    
    R=gambard@chromium.org
    
    Bug: 1418068
    Change-Id: Ic4d37afb5b5534d99313deefd763bb67f6cba26f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5245295
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253920}

--- ios/chrome/browser/ui/alert_view/alert_view_controller.mm
+++ ios/chrome/browser/ui/alert_view/alert_view_controller.mm
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/ui/elements/text_field_configuration.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/ui/alert_view/alert_action.h"
-#import "ios/chrome/common/button_configuration_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 
