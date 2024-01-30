b510a192e5f1969ce29b8452705052cc0e2418b0
Olga Gerchikov <gerchiko@microsoft.com>
2024-01-30 19:14:44 +0000

    Removed unused member have_scrolled_since_page_load_
    ScrollAnimator::have_scrolled_since_page_load_ is not used anymore.
    Removing it.
    
    Change-Id: I56f9c1bb05f6f66cc100d78f2565a5c1629e88c4
    Bug: 1183387
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5246380
    Commit-Queue: Olga Gerchikov <gerchiko@microsoft.com>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254084}

--- third_party/blink/renderer/core/scroll/scroll_animator.cc
+++ third_party/blink/renderer/core/scroll/scroll_animator.cc
@@ -110,10 +110,6 @@ ScrollResult ScrollAnimator::UserScroll(
   // scroll, the callback is invoked immediately without being stored.
   DCHECK(HasRunningAnimation() || on_finish_.is_null());
 
-#if BUILDFLAG(IS_MAC)
-  have_scrolled_since_page_load_ = true;
-#endif
-
   ScrollableArea::ScrollCallback run_on_return(BindOnce(
       [](ScrollableArea::ScrollCallback callback,
          ScrollableArea::ScrollCompletionMode mode) {
@@ -409,9 +405,6 @@ void ScrollAnimator::CancelAnimation() {
   ScrollAnimatorCompositorCoordinator::CancelAnimation();
   if (on_finish_)
     std::move(on_finish_).Run(ScrollableArea::ScrollCompletionMode::kFinished);
-#if BUILDFLAG(IS_MAC)
-  have_scrolled_since_page_load_ = false;
-#endif
 }
 
 void ScrollAnimator::TakeOverCompositorAnimation() {
