cf0626d30856d854d4fa36397e95f8eeb5c7eac4
François Doray <fdoray@chromium.org>
2024-01-30 14:51:05 +0000

    Reland "[base] Integrate (Sequenced|SingleThread)TaskRunner::GetCurrentDefault with RunOrPostTask."
    This is a reland of commit aa9fbff84283de17fe0ee59c28c9edd50b7e6d3a
    
    Gardener said that since
    ToolbarControllerUiTest.DeactivatedActionItemsOverflow started
    failing before this CL landed and kept failing after it was reverted,
    they no longer think it was causing the issue.
    
    Original change's description:
    > [base] Integrate (Sequenced|SingleThread)TaskRunner::GetCurrentDefault with RunOrPostTask.
    >
    > Within a RunOrPostTask callback running synchronously:
    >
    > - There is no "current default SingleThreadTaskRunner".
    >
    >   A RunOrPostTask() callback cannot make assumptions about the thread
    >   on which it runs, so it shouldn't access the current default
    >   SingleThreadTaskRunner.
    >
    > - The "current default SequencedTaskRunner" is the SequenceManager's
    >   default task runner.
    >
    >   A RunOrPostTask() callback runs in sequence with tasks from the
    >   target SequenceManager. It's appropriate for it to access the
    >   "current default" SequencedTaskRunner.
    >
    > Also, with this CL, when
    > SingleThreadTaskRunner::BelongsToCurrentThread() is invoked on a
    > task runner that runs tasks posted to it "in sequence" with the
    > current RunOrPostTask callback running synchronously, but on
    > a different thread, it returns `false`. In the same context,
    > SingleThreadTaskRunner::RunsTasksInCurrentSequence() returns true.
    >
    > Bug: 1503967
    > Change-Id: I017442f10eddda9070a29fb6cf0fa38dcd14a88c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5138135
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    > Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1253287}
    
    Bug: 1503967
    Change-Id: Ia1ec7142e8a255e1aea4015487aae22f16fdadbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5245868
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253895}

--- base/task/single_thread_task_runner.cc
+++ base/task/single_thread_task_runner.cc
@@ -38,6 +38,10 @@ SingleThreadTaskRunner::CurrentDefaultHandle* GetCurrentDefaultHandle() {
 
 }  // namespace
 
+bool SingleThreadTaskRunner::BelongsToCurrentThread() const {
+  return RunsTasksInCurrentSequence();
+}
+
 // static
 const scoped_refptr<SingleThreadTaskRunner>&
 SingleThreadTaskRunner::GetCurrentDefault() {
--- base/task/single_thread_task_runner.h
+++ base/task/single_thread_task_runner.h
@@ -18,6 +18,10 @@ namespace blink::scheduler {
 class MainThreadSchedulerImpl;
 }  // namespace blink::scheduler
 
+namespace base::sequence_manager::internal {
+class CurrentDefaultHandleOverrideForRunOrPostTask;
+}
+
 namespace base {
 
 class ScopedDisallowRunningRunLoop;
@@ -36,8 +40,13 @@ class ScopedDisallowRunningRunLoop;
 //     running other kinds of message loop, e.g. Jingle threads.
 class BASE_EXPORT SingleThreadTaskRunner : public SequencedTaskRunner {
  public:
-  // A more explicit alias to RunsTasksInCurrentSequence().
-  bool BelongsToCurrentThread() const { return RunsTasksInCurrentSequence(); }
+  // Returns true if the `SingleThreadTaskRunner` runs tasks posted to it on the
+  // current thread.
+  //
+  // Identical to `RunsTaskInCurrentSequence()`, except from a `RunOrPostTask()`
+  // callback running synchronously (in that case, `BelongsToCurrentThread()`
+  // returns false and `RunsTaskInCurrentSequence()` returns true).
+  virtual bool BelongsToCurrentThread() const;
 
   // Returns the default SingleThreadTaskRunner for the current thread.
   // On threads that service multiple task queues, the default task queue is
@@ -87,6 +96,8 @@ class BASE_EXPORT SingleThreadTaskRunner : public SequencedTaskRunner {
     // avoid the friend requirement.
     friend class blink::scheduler::MainThreadSchedulerImpl;
     friend class CurrentHandleOverrideForTesting;
+    friend class sequence_manager::internal::
+        CurrentDefaultHandleOverrideForRunOrPostTask;
     FRIEND_TEST_ALL_PREFIXES(SingleThreadTaskRunnerCurrentDefaultHandleTest,
                              NestedRunLoopAllowedUnderHandleOverride);
     FRIEND_TEST_ALL_PREFIXES(SingleThreadTaskRunnerCurrentDefaultHandleTest,
