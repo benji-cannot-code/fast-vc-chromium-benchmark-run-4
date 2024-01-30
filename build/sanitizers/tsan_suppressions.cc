d4adfa4f63561ffa8794755c364c45558022a249
Peter Boström <pbos@chromium.org>
2024-01-30 08:42:42 +0000

    Add swrast_dri.so to tsan_suppressions
    swrast_dri.so isn't instrumented, so we're unaware of internal
    synchronization, which is hopefully present.
    
    Bug: 1473420, 1519998
    Change-Id: Iea8f2ca8938514e72a70caa96a23c14a1260eec0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5247169
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253783}

--- build/sanitizers/tsan_suppressions.cc
+++ build/sanitizers/tsan_suppressions.cc
@@ -17,7 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // for the instructions on writing suppressions.
 char kTSanDefaultSuppressions[] =
     // False positives in libdbus.so, libdconfsettings.so, libflashplayer.so,
-    // libgio.so, libglib.so, libgobject.so, and libfontconfig.so.1.
+    // libgio.so, libglib.so, libgobject.so, libfontconfig.so.1 and
+    // swrast_dri.so.
     // Since we don't instrument them, we cannot reason about the
     // synchronization in them.
     "race:libdbus*.so\n"
@@ -27,6 +28,7 @@ char kTSanDefaultSuppressions[] =
     "race:libglib*.so\n"
     "race:libgobject*.so\n"
     "race:libfontconfig.so.1\n"
+    "race:swrast_dri.so\n"
 
     // Intentional race in ToolsSanityTest.DataRace in base_unittests.
     "race:base/tools_sanity_unittest.cc\n"
