f533da428b25054ee47e9ea2338109409a57c65b
Ishika Patel <ishikapatel@microsoft.com>
2024-01-30 17:12:34 +0000

    Migrate SystemMessageWindowWin to SingletonHwnd
    The current implementation of SystemMessageWindowWin creates message-
    only windows to listen for events from Windows. This impacts
    performance at startup where we have to pay the cost to creating the window to listen. Instead use Singleton HWND in the implementation
    of this component to allow sharing a single HWND. We typically see 1-10ms costs for creating an HWND in startup, so sharing the Singleton HWND will lead to modest but measurable startup improvements.
    
    Bug: 1493032
    Change-Id: I402ee5de74979a3cec195d5f80e8ae376a5ca181
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5202413
    Commit-Queue: Ishika Patel <ishikapatel@microsoft.com>
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253975}

--- media/device_monitors/system_message_window_win.h
+++ media/device_monitors/system_message_window_win.h
@@ -12,6 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/base/media_export.h"
 
+namespace gfx {
+class SingletonHwndObserver;
+}
 namespace media {
 
 class MEDIA_EXPORT SystemMessageWindowWin {
@@ -26,28 +29,11 @@ class MEDIA_EXPORT SystemMessageWindowWin {
   virtual LRESULT OnDeviceChange(UINT event_type, LPARAM data);
 
  private:
-  void Init();
+  void WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam);
 
-  LRESULT CALLBACK WndProc(HWND hwnd,
-                           UINT message,
-                           WPARAM wparam,
-                           LPARAM lparam);
-
-  static LRESULT CALLBACK WndProcThunk(HWND hwnd,
-                                       UINT message,
-                                       WPARAM wparam,
-                                       LPARAM lparam) {
-    SystemMessageWindowWin* msg_wnd = reinterpret_cast<SystemMessageWindowWin*>(
-        GetWindowLongPtr(hwnd, GWLP_USERDATA));
-    if (msg_wnd)
-      return msg_wnd->WndProc(hwnd, message, wparam, lparam);
-    return ::DefWindowProc(hwnd, message, wparam, lparam);
-  }
-
-  HMODULE instance_;
-  HWND window_;
   class DeviceNotifications;
   std::unique_ptr<DeviceNotifications> device_notifications_;
+  std::unique_ptr<gfx::SingletonHwndObserver> singleton_hwnd_observer_;
 };
 
 }  // namespace media
