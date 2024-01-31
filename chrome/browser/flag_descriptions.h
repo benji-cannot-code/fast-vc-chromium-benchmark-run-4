293591d48c9f6ec066d78208c76306662a402926
vikas soni <vikassoni@chromium.org>
2024-01-31 19:26:50 +0000

    [Dr-Dc] Remove features::kForceGpuMainThreadToNormalPriorityDrDc.
    Remove feature features::kForceGpuMainThreadToNormalPriorityDrDc which
    is disabled and is not in finch. We did not have any interesting
    improvements from this experiment and no longer need it. Hence removing
    dead code.
    
    Bug: 1318994
    Change-Id: Icd5068f2c904f0230aee720d982320a8bb987145
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5246476
    Auto-Submit: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254672}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -215,9 +215,6 @@ extern const char kEnableBluetoothSerialPortProfileInSerialApiDescription[];
 extern const char kEnableDrDcName[];
 extern const char kEnableDrDcDescription[];
 
-extern const char kForceGpuMainThreadToNormalPriorityDrDcName[];
-extern const char kForceGpuMainThreadToNormalPriorityDrDcDescription[];
-
 extern const char kUseClientGmbInterfaceName[];
 extern const char kUseClientGmbInterfaceDescription[];
 
