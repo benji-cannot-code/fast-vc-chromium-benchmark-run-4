0c67da7fb2fb9fae868b5a47a0ff21b0741af88f
Mikel Astiz <mastiz@chromium.org>
2024-01-31 18:54:45 +0000

    [bookmarks] Remove unnecessary BookmarkStorage::BookmarkModelDeleted()
    No behavioral changes.
    
    BookmarkStorage already schedules a save upon destruction, which can
    be exercised from BookmarkModel's destructor, instead of having a
    dedicated function in BookmarkStorage that leaves the object in a
    special, unusable state.
    
    Change-Id: Idba6341ba71cbc187a040e4672291a1e312fde83
    Bug: 1520418
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5253207
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254641}

--- components/bookmarks/browser/bookmark_storage.cc
+++ components/bookmarks/browser/bookmark_storage.cc
@@ -52,8 +52,7 @@ BookmarkStorage::BookmarkStorage(BookmarkModel* model,
       last_scheduled_save_(base::TimeTicks::Now()) {}
 
 BookmarkStorage::~BookmarkStorage() {
-  if (writer_.HasPendingWrite())
-    writer_.DoScheduledWrite();
+  SaveNowIfScheduled();
 }
 
 void BookmarkStorage::ScheduleSave() {
@@ -71,17 +70,6 @@ void BookmarkStorage::ScheduleSave() {
       base::TimeTicks::Now() - last_scheduled_save_;
   metrics::RecordTimeSinceLastScheduledSave(schedule_delta);
   last_scheduled_save_ = base::TimeTicks::Now();
-}
-
-void BookmarkStorage::BookmarkModelDeleted() {
-  // We need to save now as otherwise by the time SerializeData() is invoked
-  // the model is gone.
-  if (writer_.HasPendingWrite()) {
-    writer_.DoScheduledWrite();
-    DCHECK(!writer_.HasPendingWrite());
-  }
-
-  model_ = nullptr;
 }
 
 base::ImportantFileWriter::BackgroundDataProducerCallback
@@ -108,6 +96,10 @@ bool BookmarkStorage::HasScheduledSaveForTesting() const {
 }
 
 void BookmarkStorage::SaveNowIfScheduledForTesting() {
+  SaveNowIfScheduled();
+}
+
+void BookmarkStorage::SaveNowIfScheduled() {
   if (writer_.HasPendingWrite()) {
     writer_.DoScheduledWrite();
   }
