6872a92fc6af2496200f34f6fe2cc38791937c4b
Andre Le <leandre@chromium.org>
2024-01-31 05:54:26 +0000

    Mahi: Add feedback buttons with metrics collection
    Add the feedback buttons to Mahi panel, along with metrics collected
    when the button is clicked.
    
    Screenshot: https://screenshot.googleplex.com/BLYJbehYVRGoAMZ
    
    Bug: b:319732110
    Change-Id: I5fa41368b1a83ff33f86cecf07b9eebd8861083c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5251331
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254364}

---
+++ ash/system/mahi/mahi_constants.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SYSTEM_MAHI_MAHI_CONSTANTS_H_
+#define ASH_SYSTEM_MAHI_MAHI_CONSTANTS_H_
+
+namespace ash::mahi_constants {
+
+inline constexpr char kMahiFeedbackHistogramName[] = "Ash.Mahi.Feedback";
+
+}  // namespace ash::mahi_constants
+
+#endif  // ASH_SYSTEM_MAHI_MAHI_CONSTANTS_H_
