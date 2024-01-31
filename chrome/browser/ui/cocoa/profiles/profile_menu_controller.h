db53411a8f6a9a0d5970ea045eb6a33a08a0e84b
Avi Drissman <avi@chromium.org>
2024-01-31 15:44:52 +0000

    Fix ProfileMenuControllerTest.SetActiveAndRemove
    The lifetime of ProfileMenuController was vague, and was not
    actually dealloced when nilled out. Therefore, its
    BrowserListObserver remained active, and when browsers were changed,
    stale instances of ProfileMenuController called methods on dead
    objects.
    
    This is fixed by introducing a -deinitialize method, so that even if
    the ProfileMenuController finds its way into an autorelease pool and
    lives longer than expected, it will be inert.
    
    There were other lifetime issues: TestingProfileManager re-created the
    TestingProfileManager each test, but the ProfileMenuController was
    relying on the underlying ProfileAttributesStorage to stay alive. To
    fix this, ProfileMenuControllerTest re-creates the
    ProfileMenuController each time as well.
    
    This also does some Objective-C modernization.
    
    Fixed: 1500327
    Cq-Include-Trybots: luci.chromium.try:mac_chromium_asan_rel_ng,mac-asan-rel
    Change-Id: I835099cafba4608f70dcfaefafcdeeab177fb194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5250594
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254528}

--- chrome/browser/ui/cocoa/profiles/profile_menu_controller.h
+++ chrome/browser/ui/cocoa/profiles/profile_menu_controller.h
@@ -9,13 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Cocoa/Cocoa.h>
 
 class Browser;
+class ProfileAttributesStorage;
 
 // This controller manages the title and submenu of the Profiles item in the
 // system menu bar. It updates the contents of the menu and the menu's title
 // whenever the active browser changes.
 @interface ProfileMenuController<NSMenuItemValidation> : NSObject
 
-// Designated initializer.
+// Designated initializer. This may be called before the message loop is
+// started; it will do the remainder of the work asynchronously.
 - (instancetype)initWithMainMenuItem:(NSMenuItem*)item;
 
 // Actions for the menu items.
@@ -34,10 +36,22 @@ class Browser;
 @end
 
 @interface ProfileMenuController (PrivateExposedForTesting)
-- (NSMenu*)menu;
-- (void)rebuildMenu;
+
+@property(readonly) NSMenu* menu;
+
+- (instancetype)initSynchronouslyForTestingWithMainMenuItem:(NSMenuItem*)item
+                                   profileAttributesStorage:
+                                       (ProfileAttributesStorage*)storage;
+
+// Clears various internal observers. Not needed for non-test code, where there
+// is effectively a singleton ProfileMenuController to run the profile menu, but
+// needed for test code.
+- (void)deinitialize;
+
 - (NSMenuItem*)createItemWithTitle:(NSString*)title action:(SEL)sel;
+
 - (void)activeBrowserChangedTo:(Browser*)browser;
+
 @end
 
 #endif  // CHROME_BROWSER_UI_COCOA_PROFILES_PROFILE_MENU_CONTROLLER_H_
