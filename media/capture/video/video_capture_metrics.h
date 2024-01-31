56f3cfcc03703641ea2a0838d08c6985eb03350d
mark a. foltz <mfoltz@chromium.org>
2024-01-31 18:47:52 +0000

    [Camera] Add histograms to track effects support on Win/Mac.
    This CL adds three histograms to track support for OS level camera effects on Win/Mac, namely:
    - Background Blur
    - Face framing
    - Eye gaze correction
    
    Bug: b/322047922
    Change-Id: I75c8854f5f0fc57a212eb26c0b59ef244a0d92f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5230634
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254633}

--- media/capture/video/video_capture_metrics.h
+++ media/capture/video/video_capture_metrics.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/span.h"
 #include "media/base/video_types.h"
+#include "media/capture/mojom/image_capture.mojom.h"
 #include "media/capture/video/video_capture_device_info.h"
 
 namespace media {
@@ -18,6 +19,9 @@ void LogCaptureDeviceMetrics(
 
 void LogCaptureDeviceHashedModelId(
     const media::VideoCaptureDeviceDescriptor& descriptor);
+
+CAPTURE_EXPORT
+void LogCaptureDeviceEffects(mojom::PhotoStatePtr photo_state);
 
 CAPTURE_EXPORT
 void LogCaptureCurrentDeviceResolution(int width, int height);
