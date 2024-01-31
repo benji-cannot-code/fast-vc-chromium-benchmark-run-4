61a7c67a257ef3f04f0b66ad27b2ec8941fcfe8f
Dale Curtis <dalecurtis@chromium.org>
2024-01-31 19:42:53 +0000

    Improve web exposed error messages for software decoders.
    There's no need to sanitize messages coming from our software
    decoders since they're all compile time generated (and don't
    include information like pointer addresses, etc).
    
    R=sandersd
    
    Fixed: 1522959
    Change-Id: I8e6ad5679853e8a7ffbe7c4093dc5c297937a8d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5250878
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254685}

--- third_party/blink/renderer/modules/webcodecs/decoder_template.cc
+++ third_party/blink/renderer/modules/webcodecs/decoder_template.cc
@@ -359,7 +359,7 @@ void DecoderTemplate<Traits>::ContinueConfigureWithGpuFactories(
     decoder_ = Traits::CreateDecoder(*ExecutionContext::From(script_state_),
                                      gpu_factories_.value(), logger_->log());
     if (!decoder()) {
-      Shutdown(logger_->MakeOperationError(
+      Shutdown(MakeOperationError(
           "Internal error: Could not create decoder.",
           media::DecoderStatus::Codes::kFailedToCreateDecoder));
       return;
@@ -396,9 +396,8 @@ bool DecoderTemplate<Traits>::ProcessDecodeRequest(Request* request) {
   DCHECK_GT(num_pending_decodes_, 0u);
 
   if (!decoder()) {
-    Shutdown(logger_->MakeEncodingError(
-        "Decoding error: no decoder found.",
-        media::DecoderStatus::Codes::kNotInitialized));
+    Shutdown(MakeEncodingError("Decoding error: no decoder found.",
+                               media::DecoderStatus::Codes::kNotInitialized));
     return false;
   }
 
@@ -411,11 +410,10 @@ bool DecoderTemplate<Traits>::ProcessDecodeRequest(Request* request) {
   // The request may be invalid, if so report that now.
   if (!request->decoder_buffer || request->decoder_buffer->data_size() == 0) {
     if (request->status.is_ok()) {
-      Shutdown(
-          logger_->MakeEncodingError("Null or empty decoder buffer.",
-                                     media::DecoderStatus::Codes::kFailed));
+      Shutdown(MakeEncodingError("Null or empty decoder buffer.",
+                                 media::DecoderStatus::Codes::kFailed));
     } else {
-      Shutdown(logger_->MakeEncodingError("Decoder error.", request->status));
+      Shutdown(MakeEncodingError("Decoder error.", request->status));
     }
 
     return false;
@@ -610,7 +608,7 @@ void DecoderTemplate<Traits>::OnFlushDone(media::DecoderStatus status) {
          pending_request_->type == Request::Type::kFlush);
 
   if (!status.is_ok()) {
-    Shutdown(logger_->MakeEncodingError("Error during flush.", status));
+    Shutdown(MakeEncodingError("Error during flush.", status));
     return;
   }
 
@@ -660,7 +658,7 @@ void DecoderTemplate<Traits>::OnInitializeDone(media::DecoderStatus status) {
     } else {
       error_message = "Decoder initialization error.";
     }
-    Shutdown(logger_->MakeOperationError(error_message, status));
+    Shutdown(MakeOperationError(error_message, status));
     return;
   }
 
@@ -704,7 +702,7 @@ void DecoderTemplate<Traits>::OnDecodeDone(uint32_t id,
 
   if (!status.is_ok() &&
       status.code() != media::DecoderStatus::Codes::kAborted) {
-    Shutdown(logger_->MakeEncodingError("Decoding error.", std::move(status)));
+    Shutdown(MakeEncodingError("Decoding error.", std::move(status)));
     return;
   }
 
@@ -745,9 +743,8 @@ void DecoderTemplate<Traits>::OnOutput(uint32_t reset_generation,
   auto output_or_error = MakeOutput(std::move(output), context);
 
   if (!output_or_error.has_value()) {
-    Shutdown(
-        logger_->MakeEncodingError("Error creating output from decoded data",
-                                   std::move(output_or_error).error()));
+    Shutdown(MakeEncodingError("Error creating output from decoded data",
+                               std::move(output_or_error).error()));
     return;
   }
 
@@ -909,6 +906,28 @@ void DecoderTemplate<Traits>::Request::EndTracing(bool shutting_down) {
 #endif
   TRACE_EVENT_NESTABLE_ASYNC_END1(kCategory, TraceNameFromType(), this,
                                   "completed", !shutting_down);
+}
+
+template <typename Traits>
+DOMException* DecoderTemplate<Traits>::MakeOperationError(
+    std::string error_msg,
+    media::DecoderStatus status) {
+  if (!decoder_ || decoder_->IsPlatformDecoder()) {
+    return logger_->MakeOperationError(std::move(error_msg), std::move(status));
+  }
+  return logger_->MakeSoftwareCodecOperationError(std::move(error_msg),
+                                                  std::move(status));
+}
+
+template <typename Traits>
+DOMException* DecoderTemplate<Traits>::MakeEncodingError(
+    std::string error_msg,
+    media::DecoderStatus status) {
+  if (!decoder_ || decoder_->IsPlatformDecoder()) {
+    return logger_->MakeEncodingError(std::move(error_msg), std::move(status));
+  }
+  return logger_->MakeSoftwareCodecEncodingError(std::move(error_msg),
+                                                 std::move(status));
 }
 
 template class DecoderTemplate<AudioDecoderTraits>;
