6211badcfdff9262295bc819c772e5d937b1a937
Scott Violet <sky@chromium.org>
2024-01-31 20:05:17 +0000

    blink: adds CORE_EXPORT macro to CSSConditionRule
    I get compile errors when building blink_unit_tests
    without it.
    
    Bug: None
    Change-Id: I5722e48e0a1dcdb9f9f746692024411d3e8e200e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5252106
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254704}

--- third_party/blink/renderer/core/css/css_condition_rule.h
+++ third_party/blink/renderer/core/css/css_condition_rule.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class CSSConditionRule : public CSSGroupingRule {
+class CORE_EXPORT CSSConditionRule : public CSSGroupingRule {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
