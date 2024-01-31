1ff0c95108fe21efc0df6c92736f0c7309db9876
rbpotter <rbpotter@chromium.org>
2024-01-31 19:47:55 +0000

    Managed Footnote: Fork for Ash Settings from Browser
    This component relies on cr_elements/ so forking it for Ash Settings
    (the only Ash WebUI user) to unblock Ash Settings from migrating to the
    Ash cr_elements fork.
    
    Bug: 1512231
    Change-Id: Ic1fce9579f26cf9c05f519aeb81035f855723994
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5252089
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254690}

--- chrome/browser/resources/ash/settings/os_settings_main/os_settings_main.ts
+++ chrome/browser/resources/ash/settings/os_settings_main/os_settings_main.ts
@@ -7,12 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview
  * 'os-settings-main' displays the selected settings page.
  */
-import 'chrome://resources/cr_components/managed_footnote/managed_footnote.js';
 import 'chrome://resources/cr_components/settings_prefs/prefs.js';
 import 'chrome://resources/cr_elements/cr_hidden_style.css.js';
 import 'chrome://resources/cr_elements/icons.html.js';
 import 'chrome://resources/js/search_highlight_utils.js';
 import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
+import './managed_footnote.js';
 import '../main_page_container/main_page_container.js';
 import '../settings_shared.css.js';
 import '../settings_vars.css.js';
