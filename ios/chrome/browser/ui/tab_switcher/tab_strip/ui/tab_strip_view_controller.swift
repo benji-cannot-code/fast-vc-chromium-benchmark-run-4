7f76a8eb4e912830b6b061c0fe8eb2f19c2cad0b
Ewann Pelle <ewannpv@chromium.org>
2024-01-31 13:11:14 +0000

    [iOS][TS] Add static separator
    Adds static decoration views that encapsulate the collection view.
    A decoration view contains two decoration separators.
    
    A decoration view is shown if:
    - The selected cell is on an edge.
    - A cell below the selected cell that is reaching the same edge.
    - The cell is not animated (inserted / deleted).
    
    Demo: https://drive.google.com/file/d/1WUlpQIAm-c7bJCN1OUA-vowqlBbF3OyB
    
    Bug: 1508814
    Change-Id: I00dc9e22483299fe8825d0839a9cbae4d3c161bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5244745
    Reviewed-by: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1254462}

--- ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_view_controller.swift
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_view_controller.swift
@@ -24,7 +24,13 @@ class TabStripViewController: UIViewController, TabStripCellDelegate,
   private var tabCellRegistration: UICollectionView.CellRegistration<TabStripCell, TabSwitcherItem>?
 
   // The New tab button.
-  private let newTabButton: TabStripNewTabButton = TabStripNewTabButton(frame: .zero)
+  private let newTabButton: TabStripNewTabButton = TabStripNewTabButton()
+
+  // Static decoration views that border the collection view. They are
+  // visible when the selected cell reaches an edge of the collection view and
+  // if the collection view can be scrolled.
+  private let leftStaticSeparator: TabStripDecorationView = TabStripDecorationView()
+  private let rightStaticSeparator: TabStripDecorationView = TabStripDecorationView()
 
   // Lastest dragged item. This property is set when the item
   // is long pressed which does not always result in a drag action.
@@ -58,6 +64,8 @@ class TabStripViewController: UIViewController, TabStripCellDelegate,
     }
 
     layout.dataSource = diffableDataSource
+    layout.leftStaticSeparator = leftStaticSeparator
+    layout.rightStaticSeparator = rightStaticSeparator
   }
 
   required init?(coder: NSCoder) {
@@ -74,6 +82,11 @@ class TabStripViewController: UIViewController, TabStripCellDelegate,
 
     collectionView.backgroundColor = .clear
     view.addSubview(collectionView)
+
+    // Mirror the layer.
+    rightStaticSeparator.transform = CGAffineTransformMakeScale(-1, 1)
+    view.addSubview(leftStaticSeparator)
+    view.addSubview(rightStaticSeparator)
 
     newTabButton.delegate = self
     view.addSubview(newTabButton)
@@ -93,6 +106,16 @@ class TabStripViewController: UIViewController, TabStripCellDelegate,
       newTabButton.bottomAnchor.constraint(equalTo: view.bottomAnchor),
       newTabButton.topAnchor.constraint(equalTo: view.topAnchor),
       newTabButton.widthAnchor.constraint(equalToConstant: TabStripConstants.NewTabButton.width),
+
+      /// `leftStaticSeparator` constraints.
+      leftStaticSeparator.leftAnchor.constraint(equalTo: collectionView.leftAnchor),
+      leftStaticSeparator.bottomAnchor.constraint(
+        equalTo: collectionView.bottomAnchor),
+
+      /// `rightStaticSeparator` constraints.
+      rightStaticSeparator.rightAnchor.constraint(equalTo: collectionView.rightAnchor),
+      rightStaticSeparator.bottomAnchor.constraint(
+        equalTo: collectionView.bottomAnchor),
     ])
   }
 
