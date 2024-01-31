3815b7fee102dc507ccb6228302deab18a757479
Ian Kilpatrick <ikilpatrick@chromium.org>
2024-01-31 14:43:35 +0000

    [gc] Pull HarfBuzzRunGlyphData and friends into own file.
    This is so we can eventually trace RunInfo/RunInfoPart and fix some
    related TODOs in ShapeResult.
    
    There should be no behaviour change.
    
    Bug: 1517025
    Change-Id: I6885c38be63babac72a9f21c0ca799e5ad3343ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5241677
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254497}

--- third_party/blink/renderer/platform/fonts/shaping/shape_result.h
+++ third_party/blink/renderer/platform/fonts/shaping/shape_result.h
@@ -406,7 +406,6 @@ class PLATFORM_EXPORT ShapeResult : public GarbageCollected<ShapeResult> {
   String ToString() const;
   void ToString(StringBuilder*) const;
 
-  using GlyphOffset = gfx::Vector2dF;
   struct RunInfo;
   RunInfo* InsertRunForTesting(unsigned start_index,
                                unsigned num_characters,
