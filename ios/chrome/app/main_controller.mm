55225d64e35ca48de7072c0d76f3dcf004a2afe1
Jérôme Lebel <jlebel@chromium.org>
2024-01-31 10:34:59 +0000

    [iOS][Waffle] Search engine list should not be updated on startup
    This patch removes iOS code to update the search engine list on
    startup according on the new country code, at the same time.
    
    On startup, iOS code needs to only update the country code pref.
    This is done after SearchEngineChoiceService save the country code
    pref in cache.
    The search engine list will be updated by SearchEngineChoiceService,
    on the following startup.
    
    This patch will be cherry-picked on M-122
    
    Fixed: b/322334466
    Change-Id: I34f571010d21365753b929c8cca3c2888a66c8de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5241067
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254428}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -696,9 +696,8 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
 
 - (void)initializeBrowserState:(ChromeBrowserState*)browserState {
   DCHECK(!browserState->IsOffTheRecord());
-  search_engines::UpdateSearchEnginesIfNeeded(
-      browserState->GetPrefs(),
-      ios::TemplateURLServiceFactory::GetForBrowserState(browserState));
+  search_engines::UpdateSearchEngineCountryCodeIfNeeded(
+      browserState->GetPrefs());
 }
 
 #pragma mark - AppStateObserver
