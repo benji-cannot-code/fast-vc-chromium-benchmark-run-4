8455237693871d507a276466160f6f674f65b4a4
Lijin Shen <lazzzis@google.com>
2024-01-31 01:05:11 +0000

    Record swipe edge related histograms only on navigation gesture mode
    Bug: 1523293
    Change-Id: I9a73989d16a3615f8f5ea514c6172a03cec36afc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5250949
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1254295}

--- chrome/browser/back_press/android/java/src/org/chromium/chrome/browser/back_press/BackPressManagerTest.java
+++ chrome/browser/back_press/android/java/src/org/chromium/chrome/browser/back_press/BackPressManagerTest.java
@@ -9,6 +9,7 @@ import androidx.activity.BackEventCompat;
 import androidx.test.filters.SmallTest;
 
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -228,6 +229,7 @@ public class BackPressManagerTest {
     @SmallTest
     public void testRecordSwipeEdge() {
         BackPressManager manager = new BackPressManager();
+        manager.setIsGestureNavEnabledSupplier(() -> true);
 
         EmptyBackPressHandler h1 =
                 TestThreadUtils.runOnUiThreadBlockingNoException(EmptyBackPressHandler::new);
@@ -251,6 +253,7 @@ public class BackPressManagerTest {
                     manager.addHandler(h2, BackPressHandler.Type.XR_DELEGATE);
                     h1.getHandleBackPressChangedSupplier().set(false);
                     h2.getHandleBackPressChangedSupplier().set(true);
+                    Assert.assertTrue(manager.getCallback().isEnabled());
 
                     var backEvent = new BackEventCompat(0, 0, 0, BackEventCompat.EDGE_LEFT);
                     manager.getCallback().handleOnBackStarted(backEvent);
@@ -293,6 +296,7 @@ public class BackPressManagerTest {
     @SmallTest
     public void testRecordSwipeEdgeOfTabHistoryNavigation() {
         BackPressManager manager = new BackPressManager();
+        manager.setIsGestureNavEnabledSupplier(() -> true);
 
         EmptyBackPressHandler h1 =
                 TestThreadUtils.runOnUiThreadBlockingNoException(EmptyBackPressHandler::new);
