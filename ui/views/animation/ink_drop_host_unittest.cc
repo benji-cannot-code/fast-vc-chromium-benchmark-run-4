241f9de34a5413be25cdc5989da2ef25d26aead6
Sam Fortiner <samfort@microsoft.com>
2024-01-31 22:24:53 +0000

    Fix dangling pointer in InkDropHostTest
    InkDropHostTest embeds TestViewWithInkDrop which caches a pointer to an
    InkDrop created as part of a test's call to test_api_.GetInkDrop. Later
    calls to test_api_.SetInkDropMode can delete that InkDrop and leave the
    pointer on TestViewWithInkDrop dangling. This pattern happens in one of
    the InkDropHostTest tests due to a for loop that iterates through the
    different InkDropMode.
    
    This change fixes the issue for this test by explicitly clearing the
    cached pointer each time the test loop is executed.
    
    Change-Id: Idaa724e3d05326a3bcaa1017390245db18a18ca5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5254542
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Sam Fortiner <samfort@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1254786}

--- ui/views/animation/ink_drop_host_unittest.cc
+++ ui/views/animation/ink_drop_host_unittest.cc
@@ -58,9 +58,10 @@ class TestViewWithInkDrop : public View {
   ui::EventHandler* GetTargetHandler() { return target_handler(); }
 
   TestInkDrop* last_created_inkdrop() const { return last_created_inkdrop_; }
+  void ClearLastCreatedInkDrop() { last_created_inkdrop_ = nullptr; }
 
  private:
-  raw_ptr<TestInkDrop, DanglingUntriaged> last_created_inkdrop_ = nullptr;
+  raw_ptr<TestInkDrop> last_created_inkdrop_ = nullptr;
 };
 
 class InkDropHostTest : public testing::Test {
@@ -199,6 +200,10 @@ TEST_F(InkDropHostTest,
 
     EXPECT_EQ(test_api_.GetInkDrop()->GetTargetInkDropState(),
               InkDropState::HIDDEN);
+    // Subsequent times through the loop will call SetInkDropMode which may
+    // delete the TestInkDrop pointed to by TestViewWithInkDrop and cause a
+    // dangling pointer.
+    host_view_.ClearLastCreatedInkDrop();
   }
 }
 
