512455155ba3680d7633de2df3ed627e5d0d34e4
Ameur Hosni <ameurhosni@google.com>
2024-01-31 11:14:09 +0000

    [IOS][Omnibox] Clip leading and trailing mv tiles by the edge of popup
    Fixed: 1516917
    Change-Id: Ie8d2bd20d88061a44c6f014db5e845e4af84e2fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5233509
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1254440}

--- ios/chrome/browser/ui/omnibox/popup/row/omnibox_popup_row_util.mm
+++ ios/chrome/browser/ui/omnibox/popup/row/omnibox_popup_row_util.mm
@@ -27,3 +27,8 @@ BOOL ShouldApplyOmniboxLayoutGuide(UITraitCollection* traitCollection) {
     return !base::FeatureList::IsEnabled(kOmniboxSuggestionsRTLImprovements);
   }
 }
+
+BOOL ShouldApplyOmniboxPopoutLayout(UITraitCollection* traitCollection) {
+  return IsIpadPopoutOmniboxEnabled() &&
+         IsRegularXRegularSizeClass(traitCollection);
+}
