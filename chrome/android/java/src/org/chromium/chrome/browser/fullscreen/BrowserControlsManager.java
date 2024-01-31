34cd89c8e1dc40dc9c545be99de6ac55871a5d6e
Yaron Friedman <yfriedman@chromium.org>
2024-01-31 19:52:45 +0000

    [Toolbar] Minor cleanups.
    1. Remove dead code relating to translating top views. There are no
      instances of FullscreenManager:child.setTranslationY in our traces.
    2. Add a new trace event for suspected dead code.
    
    Change-Id: Ib59cd9d6eeb5f4f6f498a2b241290d97e107bad2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5252669
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Yaron Friedman <yfriedman@google.com>
    Cr-Commit-Position: refs/heads/main@{#1254694}

--- chrome/android/java/src/org/chromium/chrome/browser/fullscreen/BrowserControlsManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/fullscreen/BrowserControlsManager.java
@@ -539,6 +539,7 @@ public class BrowserControlsManager implements ActivityStateListener, BrowserCon
         mHidingTokenHolder.releaseToken(token);
     }
 
+    @SuppressWarnings("NoDynamicStringsInTraceEventCheck")
     private boolean shouldShowAndroidControls() {
         if (mControlContainer == null) return false;
         if (mHidingTokenHolder.hasTokens()) {
@@ -557,6 +558,7 @@ public class BrowserControlsManager implements ActivityStateListener, BrowserCon
             FrameLayout.LayoutParams layoutParams =
                     (FrameLayout.LayoutParams) child.getLayoutParams();
             if (Gravity.TOP == (layoutParams.gravity & Gravity.FILL_VERTICAL)) {
+                TraceEvent.instant("BCM::showAndroidControls" + child.getId());
                 showControls = true;
                 break;
             }
