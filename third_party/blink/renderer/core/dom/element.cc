2b98808c85218d47ccd0e89f7c4889d5aec7b230
Scott Violet <sky@chromium.org>
2024-01-31 00:57:28 +0000

    Revert "blink: optimizes Element::ClassChanged"
    This reverts commit a004972fae3b5061ca775d7cad6ef6abac813585.
    
    Reason for revert: Turns out this caused some perf regressions.
    
    Original change's description:
    > blink: optimizes Element::ClassChanged
    >
    > This adds a couple of tweaks to ClassChanged:
    > . Don't do anything. This can happen if parsing and ElementData is already
    >   up to date (and not in the active document). In this case there is no
    >   additional processing needed (because ElementData is already up to date,
    >   and ClassChangedForElement() early outs if not in the active document).
    > . If we've already cached the class name, then no need to parse it.
    >
    > Bug: none
    > Change-Id: I81b00e3dd0c065c4b4c26ce784370028962add52
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5238637
    > Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    > Commit-Queue: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1253583}
    
    Bug: 1523101
    Change-Id: I304916f806e52d75c2659b415875d4beacbe3345
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5251405
    Commit-Queue: Scott Violet <sky@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254290}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -234,7 +234,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-enum class ClassStringContent { kWhiteSpaceOnly, kHasClasses };
+enum class ClassStringContent { kEmpty, kWhiteSpaceOnly, kHasClasses };
 
 namespace {
 
@@ -2616,7 +2616,7 @@ void Element::AttributeChanged(const AttributeModificationParams& params) {
         params.reason != AttributeModificationReason::kByMoveToNewDocument) {
       return;
     }
-    ClassAttributeChanged(params.new_value, params.reason);
+    ClassAttributeChanged(params.new_value);
     UpdateClassList(params.old_value, params.new_value);
   } else if (name == html_names::kNameAttr) {
     SetHasName(!params.new_value.IsNull());
@@ -2702,68 +2702,55 @@ static inline ClassStringContent ClassStringHasClassName(
     const CharacterType* characters,
     unsigned length) {
   DCHECK_GT(length, 0u);
-  for (unsigned i = 0; i < length; ++i) {
+
+  unsigned i = 0;
+  do {
     if (IsNotHTMLSpace<CharacterType>(characters[i])) {
-      return ClassStringContent::kHasClasses;
+      break;
     }
+    ++i;
+  } while (i < length);
+
+  if (i == length && length >= 1) {
+    return ClassStringContent::kWhiteSpaceOnly;
   }
-  return ClassStringContent::kWhiteSpaceOnly;
+
+  return ClassStringContent::kHasClasses;
 }
 
 static inline ClassStringContent ClassStringHasClassName(
     const AtomicString& new_class_string) {
   unsigned length = new_class_string.length();
-  DCHECK(length);
+
+  if (!length) {
+    return ClassStringContent::kEmpty;
+  }
+
   if (new_class_string.Is8Bit()) {
     return ClassStringHasClassName(new_class_string.Characters8(), length);
   }
   return ClassStringHasClassName(new_class_string.Characters16(), length);
 }
 
-void Element::ClassAttributeChanged(const AtomicString& new_class_string,
-                                    AttributeModificationReason reason) {
+void Element::ClassAttributeChanged(const AtomicString& new_class_string) {
   DCHECK(HasElementData());
+  ClassStringContent class_string_content_type =
+      ClassStringHasClassName(new_class_string);
   const bool should_fold_case = GetDocument().InQuirksMode();
-
-  if (reason == AttributeModificationReason::kByParser &&
-      GetElementData()->ClassNames().MatchesSingleString(
-          should_fold_case ? new_class_string.LowerASCII()
-                           : new_class_string) &&
-      !InActiveDocument()) {
-    // The parser changed the value and ElementData is already up to date,
-    // in this case there is nothing to do. When parsing, the same ElementData
-    // may be shared between Elements, which is why it may already be up to
-    // date.
-    return;
-  }
-  const SpaceSplitString old_classes = GetElementData()->ClassNames();
-  if (UNLIKELY(new_class_string.empty())) {
-    GetDocument().GetStyleEngine().ClassChangedForElement(old_classes, *this);
-    GetElementData()->ClearClass();
-    return;
-  }
-  // See if the value is cached. If it is, we don't need to determine the
-  // ClassStringContent.
-  if (GetElementData()->SetClassFromCachedString(
-          should_fold_case ? new_class_string.LowerASCII()
-                           : new_class_string)) {
+  if (class_string_content_type == ClassStringContent::kHasClasses) {
+    const SpaceSplitString old_classes = GetElementData()->ClassNames();
+    GetElementData()->SetClass(new_class_string, should_fold_case);
     const SpaceSplitString& new_classes = GetElementData()->ClassNames();
     GetDocument().GetStyleEngine().ClassChangedForElement(old_classes,
                                                           new_classes, *this);
-    return;
-  }
-  switch (ClassStringHasClassName(new_class_string)) {
-    case ClassStringContent::kHasClasses: {
+  } else {
+    const SpaceSplitString& old_classes = GetElementData()->ClassNames();
+    GetDocument().GetStyleEngine().ClassChangedForElement(old_classes, *this);
+    if (class_string_content_type == ClassStringContent::kWhiteSpaceOnly) {
       GetElementData()->SetClass(new_class_string, should_fold_case);
-      const SpaceSplitString& new_classes = GetElementData()->ClassNames();
-      GetDocument().GetStyleEngine().ClassChangedForElement(old_classes,
-                                                            new_classes, *this);
-      break;
+    } else {
+      GetElementData()->ClearClass();
     }
-    case ClassStringContent::kWhiteSpaceOnly:
-      GetDocument().GetStyleEngine().ClassChangedForElement(old_classes, *this);
-      GetElementData()->SetClass(new_class_string, should_fold_case);
-      break;
   }
 }
 
