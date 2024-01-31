c9ae59eb623f483c57264f374c85e7c1f29b1b00
Peter Conn <peconn@chromium.org>
2024-01-31 11:51:45 +0000

    👀 Disable supervised user functionality in SDK sandbox.
    Bug: 1523530
    Change-Id: I598108efa458bf5d97982da8f610085e8b65a7f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5250570
    Commit-Queue: Rupert Wiser <bewise@chromium.org>
    Reviewed-by: Rupert Wiser <bewise@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254448}

--- android_webview/java/src/org/chromium/android_webview/supervised_user/AwSupervisedUserUrlClassifier.java
+++ android_webview/java/src/org/chromium/android_webview/supervised_user/AwSupervisedUserUrlClassifier.java
@@ -15,6 +15,7 @@ import org.chromium.android_webview.AwFeatureMap;
 import org.chromium.android_webview.common.AwFeatures;
 import org.chromium.android_webview.common.AwSupervisedUserUrlClassifierDelegate;
 import org.chromium.android_webview.common.PlatformServiceBridge;
+import org.chromium.base.ContextUtils;
 import org.chromium.url.GURL;
 
 /**
@@ -45,6 +46,10 @@ public class AwSupervisedUserUrlClassifier {
     }
 
     public static AwSupervisedUserUrlClassifier getInstance() {
+        // Supervised user filters currently do not function in the SDK sandbox.
+        // See https://crbug.com/1523530.
+        if (ContextUtils.isSdkSandboxProcess()) return null;
+
         synchronized (sInstanceLock) {
             if (!sInitialized) {
                 if (AwFeatureMap.isEnabled(AwFeatures.WEBVIEW_SUPERVISED_USER_SITE_DETECTION)
