8b9edda12982b7a776758246ad3dfd3203947631
Daniel Andersson <dandersson@chromium.org>
2024-01-31 20:13:24 +0000

    desk_profiles: Fix avatar icon scale in the desk button
    This small CL resizes the avatar (which may be big) before handing it
    off to the ImageView. This results in a better quality image. See the
    bug for an example of the problem and the fix.
    
    BUG=b:323204750
    
    Change-Id: I273f527403cd24b348f8ff1a6aba3369025dd2b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5254026
    Commit-Queue: Daniel Andersson <dandersson@chromium.org>
    Reviewed-by: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254710}

--- ash/wm/desks/desk_button/desk_button.cc
+++ ash/wm/desks/desk_button/desk_button.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rounded_corners_f.h"
 #include "ui/gfx/geometry/size.h"
+#include "ui/gfx/image/image_skia_operations.h"
 #include "ui/gfx/text_constants.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/background.h"
@@ -297,7 +298,10 @@ void DeskButton::UpdateAvatar(const Desk* active_desk) {
       if (auto* summary =
               desk_profiles_delegate->GetProfilesSnapshotByProfileId(
                   active_desk->lacros_profile_id())) {
-        desk_avatar_image_ = summary->icon;
+        desk_avatar_image_ = gfx::ImageSkiaOperations::CreateResizedImage(
+            summary->icon, skia::ImageOperations::RESIZE_BEST,
+            kDeskButtonAvatarSize);
+
         desk_avatar_view_->SetImage(desk_avatar_image_);
         desk_avatar_view_->SetImageSize(kDeskButtonAvatarSize);
         desk_avatar_view_->SetVisible(true);
