902af006bfb53d3362ec1d75fe60b9a1293adb03
Noémie St-Onge <noemies@google.com>
2024-01-31 21:19:16 +0000

    [iOS] Fix RTL alignment of Password Manager widget promo's text label
    The promo's text was aligned to the left even in RTL locales. This CL
    fixes the issue by using the NSTextAlignmentNatural alignment.
    
    Fixed: 318801359
    Change-Id: I850ddb92ec11c895d334682cd695567d55beb71e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5249305
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254745}

--- ios/chrome/browser/ui/settings/cells/inline_promo_cell.mm
+++ ios/chrome/browser/ui/settings/cells/inline_promo_cell.mm
@@ -429,7 +429,7 @@ constexpr CGFloat kPromoTextLabelMinHeightWideLayout = 60;
   CGFloat moreInfoButtonLeftEdgeInset = 0;
 
   if (self.shouldHaveWideLayout) {
-    promoTextLabel.textAlignment = NSTextAlignmentLeft;
+    promoTextLabel.textAlignment = NSTextAlignmentNatural;
     moreInfoButton.contentHorizontalAlignment =
         UIControlContentHorizontalAlignmentLeft;
     verticalStackView.alignment = UIStackViewAlignmentFill;
