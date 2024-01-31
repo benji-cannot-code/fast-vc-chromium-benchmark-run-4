f099cad59fcae6b84fed44923698dbd7984b85ee
Timofey Chudakov <tchudakov@google.com>
2024-01-31 19:22:17 +0000

    [mullet m3] Add AutofillForUnclassifiedFieldsAvailable to Chrome flags.
    Bug: 1493361
    Change-Id: Ic1434f488399f22377d05e315cee32c7a5fd575d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5253134
    Reviewed-by: Dmitry Vykochko <vykochko@google.com>
    Cr-Commit-Position: refs/heads/main@{#1254668}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -297,6 +297,9 @@ extern const char kAutofillEnableCardProductNameDescription[];
 extern const char kAutofillGranularFillingAvailableName[];
 extern const char kAutofillGranularFillingAvailableDescription[];
 
+extern const char kAutofillForUnclassifiedFieldsAvailableName[];
+extern const char kAutofillForUnclassifiedFieldsAvailableDescription[];
+
 extern const char kAutofillEnableIbanAndroidBottomSheetName[];
 extern const char kAutofillEnableIbanAndroidBottomSheetDescription[];
 
