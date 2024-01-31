ff07ff540a162b307c35582e5abf2fb26e0fc452
Peter Kotwicz <pkotwicz@chromium.org>
2024-01-31 21:16:41 +0000

    [RWI] Create use counter for identity digital credential feature
    BUG=1523737
    
    Change-Id: I20bb8cb3acb1d5e934e92150c7e1ac0ef24b78e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5245734
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254742}

--- third_party/blink/renderer/modules/credentialmanagement/credentials_container_test.cc
+++ third_party/blink/renderer/modules/credentialmanagement/credentials_container_test.cc
@@ -19,6 +19,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/v8_union_arraybuffer_arraybufferview.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_credential_creation_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_credential_request_options.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_digital_credential_provider.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_identity_credential_request_options.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_identity_provider_request_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_public_key_credential_creation_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_public_key_credential_parameters.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_public_key_credential_rp_entity.h"
@@ -36,6 +39,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/bindings/wrapper_type_info.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
+#include "third_party/blink/renderer/platform/testing/runtime_enabled_features_test_helpers.h"
 #include "third_party/blink/renderer/platform/testing/task_environment.h"
 #include "third_party/blink/renderer/platform/testing/unit_test_helpers.h"
 #include "third_party/blink/renderer/platform/weborigin/security_origin.h"
@@ -201,6 +206,37 @@ TEST(CredentialsContainerTest, RejectPublicKeyCredentialStoreOperation) {
                   MakeGarbageCollected<MockPublicKeyCredential>());
 
   EXPECT_EQ(v8::Promise::kRejected, promise.V8Promise()->State());
+}
+
+// Test that navigator.credentials.get() increments the feature use counter when
+// one of the identity providers is a digital identity credential.
+TEST(CredentialsContainerTest, IdentityDigitalCredentialUseCounter) {
+  test::TaskEnvironment task_environment;
+  MockCredentialManager mock_credential_manager;
+  CredentialManagerTestingContext context(&mock_credential_manager);
+
+  ScopedWebIdentityDigitalCredentialsForTest scoped_digital_credentials(
+      /*enabled=*/true);
+
+  IdentityProviderRequestOptions* identity_provider_request =
+      IdentityProviderRequestOptions::Create();
+  identity_provider_request->setHolder(DigitalCredentialProvider::Create());
+  HeapVector<Member<IdentityProviderRequestOptions>> identity_providers;
+  identity_providers.push_back(identity_provider_request);
+
+  IdentityCredentialRequestOptions* identity_credential_request =
+      IdentityCredentialRequestOptions::Create();
+  identity_credential_request->setProviders(identity_providers);
+  CredentialRequestOptions* options = CredentialRequestOptions::Create();
+  options->setIdentity(identity_credential_request);
+
+  auto promise =
+      CredentialsContainer::credentials(*context.DomWindow().navigator())
+          ->get(context.GetScriptState(), options,
+                IGNORE_EXCEPTION_FOR_TESTING);
+
+  EXPECT_TRUE(context.DomWindow().document()->IsUseCounted(
+      blink::mojom::WebFeature::kIdentityDigitalCredentials));
 }
 
 }  // namespace blink
