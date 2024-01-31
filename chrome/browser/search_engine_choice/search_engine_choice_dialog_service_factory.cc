dbd6e8bc1ead13a2d28c7e28c4480bfb5ffdf242
Jack Yammine <jyammine@google.com>
2024-01-31 18:01:40 +0000

    [waffle]Propagate choice state between parent profile and incognito
    The search engine choice dialog should either show up in incognito or
    in its parent profile. The choice and dialog state should propagate
    between incognito and its parent.
    
    Screen recording: http://b/310149975#comment8
    
    Fixed: b:310149975
    Change-Id: I4fa5fbb8418e11f77b3a88bd12c5fc9eea50812d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5252935
    Auto-Submit: Jack Yammine <jyammine@google.com>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254602}

--- chrome/browser/search_engine_choice/search_engine_choice_dialog_service_factory.cc
+++ chrome/browser/search_engine_choice/search_engine_choice_dialog_service_factory.cc
@@ -90,7 +90,7 @@ SearchEngineChoiceDialogServiceFactory::SearchEngineChoiceDialogServiceFactory()
     : ProfileKeyedServiceFactory(
           "SearchEngineChoiceDialogServiceFactory",
           ProfileSelections::Builder()
-              .WithRegular(ProfileSelection::kOriginalOnly)
+              .WithRegular(ProfileSelection::kRedirectedToOriginal)
               .WithAshInternals(ProfileSelection::kNone)
               .WithGuest(ProfileSelection::kOffTheRecordOnly)
               .Build()) {
