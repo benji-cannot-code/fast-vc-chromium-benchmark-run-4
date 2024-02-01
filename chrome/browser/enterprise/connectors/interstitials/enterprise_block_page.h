e58f56d0180212e2560e0f9246b278623abbc668
Haihan Chen <haihan@google.com>
2024-02-01 20:20:34 +0000

    [URLF custom message] Implement custom message for block page interstitials
    Added custom message functionality for block page interstitials. Similar implementation as warn changes here: https://crrev.com/c/5237701
    Screenshot: https://screenshot.googleplex.com/BdL9oT8cjmQHytV
    Some small changes in warn as well and passing empty resources list to demo page.
    
    Bug: b/315985998
    Change-Id: I7feae0223d22126baea85d64d270f2bba24c12b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5250844
    Commit-Queue: Haihan Chen <haihan@google.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255244}

--- chrome/browser/enterprise/connectors/interstitials/enterprise_block_page.h
+++ chrome/browser/enterprise/connectors/interstitials/enterprise_block_page.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "chrome/browser/enterprise/connectors/common.h"
+#include "components/safe_browsing/content/browser/safe_browsing_blocking_page.h"
 #include "components/security_interstitials/content/security_interstitial_page.h"
 
 class GURL;
@@ -28,6 +30,8 @@ class EnterpriseBlockPage
   EnterpriseBlockPage(
       content::WebContents* web_contents,
       const GURL& request_url,
+      const safe_browsing::SafeBrowsingBlockingPage::UnsafeResourceList&
+          unsafe_resources,
       std::unique_ptr<
           security_interstitials::SecurityInterstitialControllerClient>
           controller);
@@ -41,6 +45,8 @@ class EnterpriseBlockPage
   security_interstitials::SecurityInterstitialPage::TypeID GetTypeForTesting()
       override;
 
+  std::string GetCustomMessageForTesting();
+
  protected:
   void CommandReceived(const std::string& command) override;
   void PopulateInterstitialStrings(base::Value::Dict& load_time_data) override;
@@ -49,6 +55,8 @@ class EnterpriseBlockPage
 
  private:
   void PopulateStringsForSharedHTML(base::Value::Dict& load_time_data);
+  const safe_browsing::SafeBrowsingBlockingPage::UnsafeResourceList
+      unsafe_resources_;
 };
 
 #endif  // CHROME_BROWSER_ENTERPRISE_CONNECTORS_INTERSTITIALS_ENTERPRISE_BLOCK_PAGE_H_
