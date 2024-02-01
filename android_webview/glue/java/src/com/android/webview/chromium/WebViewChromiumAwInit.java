27a861a346e5a8c08af82fa1a75ec93ff8a037d9
Oksana Zhuravlova <oksamyt@chromium.org>
2024-02-01 20:23:52 +0000

    [aw][tracing] Add an 'android_webview.timeline' event for StartChromiumLocked
    This change adds an WebView.Startup.CreationTime.StartChromiumLocked
    chromium trace event with the lightweight 'android_webview.timeline'
    category to cover startups not triggered by instance creation
    (e.g. static method calls).
    
    Bug: 1452205, b:283286049
    Change-Id: I9b3cbb80e2e505dfbc718fa7d9d0c06ae987f535
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5249538
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255245}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumAwInit.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumAwInit.java
@@ -271,6 +271,8 @@ public class WebViewChromiumAwInit {
         RecordHistogram.recordTimesHistogram(
                 "Android.WebView.Startup.CreationTime.StartChromiumLocked",
                 SystemClock.uptimeMillis() - startTime);
+        TraceEvent.webViewStartupStartChromiumLocked(
+                startTime, SystemClock.uptimeMillis() - startTime);
     }
 
     /**
