3c2329c2b30bcecdb7512c6f2c89ba1595325388
Jose Dapena Paz <jdapena@igalia.com>
2024-02-01 16:58:35 +0000

    IWYU: missing include for std::map and std::variant usage in semantic_tree.cc
    Bug: 957519
    Change-Id: I5fbe97053609c52a3a2577a69eddd9e6a863697b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5259277
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255102}

--- components/browsing_topics/common/semantic_tree.cc
+++ components/browsing_topics/common/semantic_tree.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/browsing_topics/common/semantic_tree.h"
 
+#include <map>
 #include <set>
+#include <variant>
 
 #include "base/check.h"
 #include "base/check_op.h"
