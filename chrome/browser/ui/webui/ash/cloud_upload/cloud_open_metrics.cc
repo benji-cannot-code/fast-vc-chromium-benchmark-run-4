f61c4b54b58d4be1881ea9d3aeb63794cdea36f1
Jérémie Boulic <jboulic@chromium.org>
2024-02-01 02:22:09 +0000

    [Office] More fine-grained UMAs for invalid alternate URLs
    Split OfficeDriveOpenErrors::kInvalidAlternateUrl into
    - kWaitingForUpload (empty URL because the file is not uploaded yet)
    - kEmptyAlternateUrl (empty URL resulting from another error)
    - kInvalidAlternateUrl
    
    Checks in `OpenDriveUrl` were redundant for the "upload to drive and
    open" flow, since the alternate URL is already checked by the
    DriveUploadHandler. Checks are removed from this method.
    
    Bug: b:314878268
    Change-Id: I51b4ac11e8bc13737db679743920e89f890c8d9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5250988
    Reviewed-by: Nigel Tao <nigeltao@chromium.org>
    Auto-Submit: Jeremie Boulic <jboulic@chromium.org>
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254878}

--- chrome/browser/ui/webui/ash/cloud_upload/cloud_open_metrics.cc
+++ chrome/browser/ui/webui/ash/cloud_upload/cloud_open_metrics.cc
@@ -152,6 +152,8 @@ void CloudOpenMetrics::CheckForInconsistencies(
               case OfficeDriveOpenErrors::kInvalidAlternateUrl:
               case OfficeDriveOpenErrors::kDriveAlternateUrl:
               case OfficeDriveOpenErrors::kUnexpectedAlternateUrl:
+              case OfficeDriveOpenErrors::kEmptyAlternateUrl:
+              case OfficeDriveOpenErrors::kWaitingForUpload:
               case OfficeDriveOpenErrors::kSuccess:
                 SetWrongValueLogged(drive_open_error);
                 break;
@@ -255,6 +257,8 @@ void CloudOpenMetrics::CheckForInconsistencies(
               case OfficeDriveOpenErrors::kNoDriveService:
               case OfficeDriveOpenErrors::kDriveAuthenticationNotReady:
               case OfficeDriveOpenErrors::kMeteredConnection:
+              case OfficeDriveOpenErrors::kEmptyAlternateUrl:
+              case OfficeDriveOpenErrors::kWaitingForUpload:
                 break;
               case OfficeDriveOpenErrors::kSuccess:
                 SetWrongValueLogged(drive_open_error);
@@ -307,6 +311,8 @@ void CloudOpenMetrics::CheckForInconsistencies(
               case OfficeDriveOpenErrors::kNoDriveService:
               case OfficeDriveOpenErrors::kDriveAuthenticationNotReady:
               case OfficeDriveOpenErrors::kMeteredConnection:
+              case OfficeDriveOpenErrors::kEmptyAlternateUrl:
+              case OfficeDriveOpenErrors::kWaitingForUpload:
                 SetWrongValueLogged(drive_open_error);
                 break;
             }
