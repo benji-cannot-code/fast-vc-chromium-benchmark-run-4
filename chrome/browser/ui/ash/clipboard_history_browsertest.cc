c086f117059de532c34485bd418407de028283af
Colin Kincaid <ckincaid@chromium.org>
2024-02-01 22:48:52 +0000

    Remove flag guarding clipboard history footer feature
    The feature has been enabled by default since M117.
    
    Bug: b/323389593
    Change-Id: Icdc6941032801495150600b7ba7b8d8e34b23271
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5260162
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1255326}

--- chrome/browser/ui/ash/clipboard_history_browsertest.cc
+++ chrome/browser/ui/ash/clipboard_history_browsertest.cc
@@ -1931,10 +1931,8 @@ class ClipboardHistoryLongpressEnabledBrowserTest
     : public ClipboardHistoryTextfieldBrowserTestBase {
  public:
   ClipboardHistoryLongpressEnabledBrowserTest() {
-    scoped_feature_list_.InitWithFeatures(
-        {ash::features::kClipboardHistoryFooter,
-         ash::features::kClipboardHistoryLongpress},
-        /*disabled_features=*/{});
+    scoped_feature_list_.InitAndEnableFeature(
+        ash::features::kClipboardHistoryLongpress);
   }
 
  private:
@@ -2004,8 +2002,7 @@ class ClipboardHistoryRefreshEnabledBrowserTest
  public:
   ClipboardHistoryRefreshEnabledBrowserTest() {
     scoped_feature_list_.InitWithFeatures(
-        {ash::features::kClipboardHistoryFooter,
-         chromeos::features::kClipboardHistoryRefresh,
+        {chromeos::features::kClipboardHistoryRefresh,
          chromeos::features::kJelly},
         /*disabled_features=*/{});
   }
