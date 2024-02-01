3837de4f894383d3b0946c46e25bc97355304b69
Li Lin <llin@chromium.org>
2024-02-01 21:12:35 +0000

    Add Campaigns Manager session.
    In this CL, the Campaigns Manager session load campaigns component at
    session active and try to trigger proactive campaign when campaigns
    component is loaded.
    
    Follow up CLs will:
    1. Evaludate the proactive campaign and trigger the reusable components
       when there is a match.
    2. Observer other trigger conditions (prefs changes, etc) as other
       triggering points.
    
    Bug: b/318761862
    Change-Id: I0531948f7f0b1b160aae1aeaf663f98141eef04c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5183315
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiqi Ruan <xiqiruan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255278}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1342,10 +1342,15 @@ BASE_FEATURE(kGrowthFramework,
              "GrowthFramework",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-// Enables Demo mode customizations with growth campaigns.
+// Enables Demo Mode customizations with growth campaigns.
 BASE_FEATURE(kGrowthCampaignsInDemoMode,
              "GrowthCampaignsInDemoMode",
              base::FEATURE_ENABLED_BY_DEFAULT);
+
+// Enables consumer session customizations with growth campaigns.
+BASE_FEATURE(kGrowthCampaignsInConsumerSession,
+             "GrowthCampaignsInConsumerSession",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Enables new on-device recognition for legacy handwriting input.
 // This flag should be OVERRIDDEN for devices which do not have on-device
@@ -3535,6 +3540,11 @@ bool IsGrowthFrameworkEnabled() {
 bool IsGrowthCampaignsInDemoModeEnabled() {
   return IsGrowthFrameworkEnabled() &&
          base::FeatureList::IsEnabled(kGrowthCampaignsInDemoMode);
+}
+
+bool IsGrowthCampaignsInConsumerSessionEnabled() {
+  return IsGrowthFrameworkEnabled() &&
+         base::FeatureList::IsEnabled(kGrowthCampaignsInConsumerSession);
 }
 
 bool AreGlanceablesV2Enabled() {
