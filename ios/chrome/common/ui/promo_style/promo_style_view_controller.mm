7afd2197713142a926a38a3e8af002daa039a112
Hira Mahmood <hiramahmood@google.com>
2024-02-01 15:19:10 +0000

    [iOS] Add to PromoStyleViewController interface
    This CL modifies the PromoStyleViewController interface to allow
    subclasses to:
    * adjust the space above the title view
    * set the UIButtonConfiguration for the primary action button
    
    Example of usage:
    crrev.com/c/5249157
    
    Bug: 1519599
    Change-Id: Ic5374e01f8ec8a3bae4306b9329306bf7d8e5052
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5249054
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255050}

--- ios/chrome/common/ui/promo_style/promo_style_view_controller.mm
+++ ios/chrome/common/ui/promo_style/promo_style_view_controller.mm
@@ -71,6 +71,7 @@ const CGFloat kHeaderImageShadowShadowInset = 20;
 // This view contains only the header image.
 @property(nonatomic, strong) UIImageView* headerImageView;
 @property(nonatomic, strong) UITextView* disclaimerView;
+// Primary action button for the view controller.
 @property(nonatomic, strong) HighlightButton* primaryActionButton;
 
 // Read/Write override.
@@ -144,6 +145,7 @@ const CGFloat kHeaderImageShadowShadowInset = 20;
     _headerImageBottomMargin = kDefaultMargin;
     _noBackgroundHeaderImageTopMarginPercentage =
         kNoBackgroundHeaderImageTopMarginPercentage;
+    _primaryButtonEnabled = YES;
   }
 
   return self;
@@ -454,7 +456,8 @@ const CGFloat kHeaderImageShadowShadowInset = 20;
   } else {
     [NSLayoutConstraint activateConstraints:@[
       [titleLabel.topAnchor
-          constraintEqualToAnchor:self.bannerImageView.bottomAnchor],
+          constraintEqualToAnchor:self.bannerImageView.bottomAnchor
+                         constant:_titleTopMarginWhenNoHeaderImage],
     ]];
   }
 
@@ -750,6 +753,8 @@ const CGFloat kHeaderImageShadowShadowInset = 20;
     [_primaryActionButton addTarget:self
                              action:@selector(didTapPrimaryActionButton)
                    forControlEvents:UIControlEventTouchUpInside];
+    _primaryActionButton.configurationUpdateHandler = self.updateHandler;
+    _primaryActionButton.enabled = _primaryButtonEnabled;
   }
   return _primaryActionButton;
 }
@@ -808,6 +813,13 @@ const CGFloat kHeaderImageShadowShadowInset = 20;
                forControlEvents:UIControlEventTouchUpInside];
   }
   return _learnMoreButton;
+}
+
+- (void)setPrimaryButtonEnabled:(BOOL)primaryButtonEnabled {
+  _primaryButtonEnabled = primaryButtonEnabled;
+  if (_primaryActionButton) {
+    _primaryActionButton.enabled = primaryButtonEnabled;
+  }
 }
 
 #pragma mark - Private
