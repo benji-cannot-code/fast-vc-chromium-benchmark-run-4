8aadac04b9ccacd510c3d5c94dfce10569ce78ec
Charles Meng <charlesmeng@chromium.org>
2024-02-01 00:18:35 +0000

    Remove overscroll history navigation setting about flag
    Remove about flag for the feature kOverscrollHistoryNavigationSetting so
    that the usages of the feature may be removed.
    
    Bug: 1523779
    Change-Id: Ic2444b7aebeaa27a4cbfeb29068919173d1b9441
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5254553
    Auto-Submit: Charles Meng <charlesmeng@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254837}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1568,8 +1568,6 @@ extern const char kOverlayStrategiesOccludedAndUnoccluded[];
 
 extern const char kOverscrollHistoryNavigationName[];
 extern const char kOverscrollHistoryNavigationDescription[];
-extern const char kOverscrollHistoryNavigationSettingName[];
-extern const char kOverscrollHistoryNavigationSettingDescription[];
 
 extern const char kPageContentAnnotationsName[];
 extern const char kPageContentAnnotationsDescription[];
