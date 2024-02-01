7767accb2a7beda8b7c30e413a2dceee43ee5b93
Joe Mason <joenotcharles@google.com>
2024-02-01 23:40:36 +0000

    Move ChromeResponsivenessCalculatorDelegate to subdir
    The OWNERS of .../metrics/usage_scenario are better reviewers for this
    file than general metrics owners.
    
    Bug: 1523311
    Change-Id: I22c1ba2a5ecb2e7db0f2382a787c39676dc59c8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5259498
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255349}

--- chrome/browser/metrics/usage_scenario/chrome_responsiveness_calculator_delegate_unittest.cc
+++ chrome/browser/metrics/usage_scenario/chrome_responsiveness_calculator_delegate_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/metrics/chrome_responsiveness_calculator_delegate.h"
+#include "chrome/browser/metrics/usage_scenario/chrome_responsiveness_calculator_delegate.h"
 
 #include "base/test/metrics/histogram_tester.h"
 #include "chrome/browser/metrics/usage_scenario/usage_scenario_data_store.h"
