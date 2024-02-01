2b77611b13eb7a31200bdf0591b3809c3ab7d4cd
Ahmed Mehfooz <amehfooz@chromium.org>
2024-02-01 19:12:10 +0000

    Add timestamps for Conversation notifications
    Created a separate TimestampView class that
    automatically updates itself.
    Added a title row to ConversationNotificationView
    which displays the title label, divider and
    timestamp.
    
    screenshot: https://screenshot.googleplex.com/4LSpNyqPhywo462
    
    Bug: b:284512022
    
    Change-Id: Iae92fb15df50464b497d70909a93158198a4ff7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5254286
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255187}

--- ash/system/notification_center/views/ash_notification_view.h
+++ ash/system/notification_center/views/ash_notification_view.h
@@ -37,6 +37,7 @@ namespace ash {
 class RoundedImageView;
 class AshNotificationExpandButton;
 class IconButton;
+class TimestampView;
 
 // Customized NotificationView for notification on ChromeOS. This view is used
 // to displays all current types of notification on ChromeOS (web, basic, image,
@@ -194,8 +195,7 @@ class ASH_EXPORT AshNotificationView
     // Update title view's text.
     void UpdateTitle(const std::u16string& title);
 
-    // Update the text for `timestamp_in_collapsed_view_`. Also used the timer
-    // to re-update this timestamp view when the next update is needed.
+    // Update the text for `timestamp_in_collapsed_view_`.
     void UpdateTimestamp(base::Time timestamp);
 
     // Update children's visibility based on the state of expand/collapse.
@@ -220,14 +220,10 @@ class ASH_EXPORT AshNotificationView
 
     // Timestamp view shown alongside the title in collapsed state.
     const raw_ptr<views::Label> title_row_divider_;
-    const raw_ptr<views::Label> timestamp_in_collapsed_view_;
+    const raw_ptr<TimestampView> timestamp_in_collapsed_view_;
 
     // The maximum width available to the title row.
     int max_available_width_ = 0;
-
-    // Timer that updates the timestamp over time.
-    base::OneShotTimer timestamp_update_timer_;
-    std::optional<base::Time> timestamp_;
   };
 
   // message_center::MessageCenterObserver:
--- ash/system/notification_center/views/conversation_notification_view.h
+++ ash/system/notification_center/views/conversation_notification_view.h
@@ -24,6 +24,9 @@ class FlexLayoutView;
 }
 
 namespace ash {
+
+class TimestampView;
+
 class ASH_EXPORT ConversationNotificationView
     : public message_center::MessageView {
  public:
@@ -67,6 +70,8 @@ class ASH_EXPORT ConversationNotificationView
   std::unique_ptr<views::FlexLayoutView> CreateRightControlsContainer();
   std::unique_ptr<views::FlexLayoutView> CreateTextContainer(
       const message_center::Notification& notification);
+  std::unique_ptr<views::FlexLayoutView> CreateTitleRow(
+      const message_center::Notification& notification);
 
   // Whether this notification is expanded or not.
   bool expanded_ = true;
@@ -77,6 +82,7 @@ class ASH_EXPORT ConversationNotificationView
       control_buttons_view_ = nullptr;
   raw_ptr<views::View> inline_settings_view_ = nullptr;
   raw_ptr<views::View> right_controls_container_ = nullptr;
+  raw_ptr<TimestampView> timestamp_ = nullptr;
 };
 }  // namespace ash
 
