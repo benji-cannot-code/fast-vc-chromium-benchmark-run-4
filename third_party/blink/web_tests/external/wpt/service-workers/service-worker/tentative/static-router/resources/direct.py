3f33d359197a844527a1bcdaa969203a02c3a660
sisidovski <sisidovski@chromium.org>
2024-02-01 15:13:25 +0000

    Add WPTs for race-network-and-fetch-handler option
    Adds tests for `race-network-and-fetch-handler`, which is the service
    worker static routing API's router source option.
    
    This tests check the case when the network request wins and the fetch
    handler wins the race respectively, both for the main resource and
    subresource.
    
    Bug: 1519727
    Change-Id: I19fe304c294f64d99d08c76be113a41d726b8d52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5256919
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255046}

--- third_party/blink/web_tests/external/wpt/service-workers/service-worker/tentative/static-router/resources/direct.py
+++ third_party/blink/web_tests/external/wpt/service-workers/service-worker/tentative/static-router/resources/direct.py
@@ -1,5 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import os
+import time
 
 def main(request, response):
+    if 'server_slow' in request.url_parts.query:
+        time.sleep(0.2)
     return 200, [(b'Content-Type', b'text/plain')], u'Network with %s request' % request.method
