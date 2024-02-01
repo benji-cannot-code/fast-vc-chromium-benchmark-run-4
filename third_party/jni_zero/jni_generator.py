4adec64d752d4a202db30ed934c1192ecdc91c8d
Mirko Bonadei <mbonadei@chromium.org>
2024-02-01 16:39:32 +0000

    Revert "jni_zero: Removing now obsolete extra-include option"
    This reverts commit dfef43a66381623998ac6bd19b86d85555d391b9.
    
    Reason for revert: WebRTC needs a bit of time in order to cleanup the tricks in the helper header. After that we can reland.
    
    Original change's description:
    > jni_zero: Removing now obsolete extra-include option
    >
    > WebRTC was the only user of this. Now that we've entirely broken our
    > deps onto base, they should be OK to use our headers.
    >
    > Bug: 1377351
    > Change-Id: I7b5f84babc63c2a7f425080ac252db178d044c60
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5246591
    > Auto-Submit: Sam Maier <smaier@chromium.org>
    > Commit-Queue: Sam Maier <smaier@chromium.org>
    > Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    > Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1253922}
    
    Bug: 1377351
    Change-Id: Id64deef885805d91d5a010d40996ae28b6eb9531
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5257438
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1255076}

--- third_party/jni_zero/jni_generator.py
+++ third_party/jni_zero/jni_generator.py
@@ -517,7 +517,7 @@ class InlHeaderFileGenerator(object):
 #include <jni.h>
 
 #include "third_party/jni_zero/jni_export.h"
-#include "third_party/jni_zero/jni_zero_helper.h"
+${INCLUDES}
 
 // Step 1: Forward declarations.
 $CLASS_PATH_DEFINITIONS
@@ -538,6 +538,7 @@ $METHOD_STUBS
         'CONSTANT_FIELDS': self.GetConstantFieldsString(),
         'METHOD_STUBS': self.GetMethodStubsString(),
         'HEADER_GUARD': self.header_guard,
+        'INCLUDES': self.GetIncludesString(),
     }
     open_namespace = self.GetOpenNamespaceString()
     if open_namespace:
@@ -579,6 +580,12 @@ $METHOD_STUBS
         self.GetLazyCalledByNativeMethodStub(called_by_native)
         for called_by_native in self.called_by_natives
     ]
+
+  def GetIncludesString(self):
+    if not self.options.extra_includes:
+      return ''
+    includes = self.options.extra_includes
+    return '\n'.join('#include "%s"' % x for x in includes) + '\n'
 
   def GetOpenNamespaceString(self):
     if self.namespace:
