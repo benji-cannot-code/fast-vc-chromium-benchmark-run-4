8044f0cd5f153be9b0435853857a22a4d7f253d4
Kirubel Aklilu <kaklilu@chromium.org>
2024-02-01 20:06:46 +0000

    Disable PlusAddresses when the first poll requests fail with 403.
    The existing behavior continues to offer the feature even if each
    request fails with a 403. The users that experience this are not
    enrolled to use the feature, so we can disable the feature
    entirely for them.
    
    Before this CL:
    Repeated 403s do not cause changes in PlusAddresses.
    
    After this CL:
    If the first poll fails with a 403, we retry it once.
    If it never succeeds, we disable the PlusAddressService which
    prevents any PlusAddress offerings or further network requests.
    
    This CL adds this behavior behind a new feature flag parameter.
    
    Bug: 1467623, b/318357301
    Change-Id: I3c7cf750ef8d900bad9cdc0aa5817824019901b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5192985
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Kirubel Aklilu <kaklilu@chromium.org>
    Reviewed-by: Matt Reichhoff <mreichhoff@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255236}

--- components/plus_addresses/plus_address_service.cc
+++ components/plus_addresses/plus_address_service.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "google_apis/gaia/google_service_auth_error.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
+#include "net/http/http_status_code.h"
 
 namespace plus_addresses {
 
@@ -202,6 +203,10 @@ std::optional<std::string> PlusAddressService::GetPrimaryEmail() {
 }
 
 bool PlusAddressService::is_enabled() const {
+  if (kDisableForForbiddenUsers.Get() && account_is_forbidden_.has_value() &&
+      account_is_forbidden_.value()) {
+    return false;
+  }
   return base::FeatureList::IsEnabled(plus_addresses::kFeature) &&
          (kEnterprisePlusAddressServerUrl.Get() != "") &&
          identity_manager_ != nullptr &&
@@ -233,7 +238,17 @@ void PlusAddressService::SyncPlusAddressMapping() {
     return;
   }
   plus_address_client_.GetAllPlusAddresses(base::BindOnce(
-      &PlusAddressService::UpdatePlusAddressMap,
+      [](PlusAddressService* service,
+         const PlusAddressMapOrError& maybe_mapping) {
+        if (maybe_mapping.has_value()) {
+          service->UpdatePlusAddressMap(maybe_mapping.value());
+          if (!service->account_is_forbidden_.has_value()) {
+            service->account_is_forbidden_.emplace(false);
+          }
+        } else {
+          service->HandlePollingError(maybe_mapping.error());
+        }
+      },
       // base::Unretained is safe here since PlusAddressService owns
       // the PlusAddressClient and they have the same lifetime.
       base::Unretained(this)));
@@ -250,6 +265,22 @@ void PlusAddressService::UpdatePlusAddressMap(const PlusAddressMap& map) {
   }
 }
 
+void PlusAddressService::HandlePollingError(PlusAddressRequestError error) {
+  if (!kDisableForForbiddenUsers.Get() ||
+      error.type() != PlusAddressRequestErrorType::kNetworkError) {
+    return;
+  }
+  if (!account_is_forbidden_.has_value() &&
+      error.http_response_code() == net::HTTP_FORBIDDEN) {
+    // Only retry failed 403s up to the limit.
+    if (initial_poll_retry_attempt_ < MAX_INITIAL_POLL_RETRY_ATTEMPTS) {
+      initial_poll_retry_attempt_++;
+      SyncPlusAddressMapping();
+    } else {
+      account_is_forbidden_.emplace(true);
+    }
+  }
+}
 void PlusAddressService::OnPrimaryAccountChanged(
     const signin::PrimaryAccountChangeEvent& event) {
   signin::PrimaryAccountChangeEvent::Type type =
