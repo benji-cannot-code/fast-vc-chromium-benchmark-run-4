f95bec22c42ff65ad8324f960ee36ad495fc769e
Riley Tatum <rtatum@google.com>
2024-02-01 01:07:44 +0000

    make the whole inspiration heading the toggle button
    http://screencast/cast/NDU2MDcyMTYzODU4ODQxNnw1OTEyZjYzNi0xYg
    
    Bug: b/322386419
    Change-Id: I210dea30fcf57d0bb412b0a2252502df30700a88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5251334
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1254854}

--- chrome/test/data/webui/side_panel/customize_chrome/wallpaper_search/wallpaper_search_test.ts
+++ chrome/test/data/webui/side_panel/customize_chrome/wallpaper_search/wallpaper_search_test.ts
@@ -2166,9 +2166,9 @@ suite('WallpaperSearchTest', () => {
           $$<IronCollapseElement>(wallpaperSearchElement, 'iron-collapse')!;
       assertFalse(ironCollapse.opened);
       assertEquals(
-          'expand-carets',
+          'cr-icon expand-carets',
           wallpaperSearchElement.shadowRoot!
-              .querySelector('#inspirationToggle')!.className);
+              .querySelector('#inspirationToggle div')!.className);
       assertEquals(
           'false',
           $$<IronCollapseElement>(
@@ -2179,22 +2179,22 @@ suite('WallpaperSearchTest', () => {
 
       assertTrue(ironCollapse.opened);
       assertEquals(
-          'collapse-carets',
+          'cr-icon collapse-carets',
           wallpaperSearchElement.shadowRoot!
-              .querySelector('#inspirationToggle')!.className);
+              .querySelector('#inspirationToggle div')!.className);
       assertEquals(
           'true',
           $$<IronCollapseElement>(
               wallpaperSearchElement, '#inspirationToggle')!.ariaExpanded);
 
-      $$<CrIconButtonElement>(
-          wallpaperSearchElement, '#inspirationToggle')!.click();
+      $$<CrIconButtonElement>(wallpaperSearchElement, '#inspirationToggle')!
+          .dispatchEvent(new KeyboardEvent('keydown', {key: ' '}));
 
       assertFalse(ironCollapse.opened);
       assertEquals(
-          'expand-carets',
+          'cr-icon expand-carets',
           wallpaperSearchElement.shadowRoot!
-              .querySelector('#inspirationToggle')!.className);
+              .querySelector('#inspirationToggle div')!.className);
       assertEquals(
           'false',
           $$<IronCollapseElement>(
