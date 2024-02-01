9c19764cbbdfb51956b2b0297ab09266bc93811a
Maggie Chen <magchen@chromium.org>
2024-02-01 16:41:57 +0000

    Use scoped_refptr for shared_image_interface() in LayerTreeFrameSink
    Now that ClientSharedImageInterface is ref counted, switch to
    scoped_refptr for shared_image_interface().
    
    Bug: 1434885
    Change-Id: Ic4eebc905f3cc6179721434a37c20614b7665354
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5255057
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255080}

--- cc/layers/heads_up_display_layer_impl.cc
+++ cc/layers/heads_up_display_layer_impl.cc
@@ -180,7 +180,7 @@ class HudSoftwareBacking : public ResourcePool::SoftwareBacking {
  public:
   ~HudSoftwareBacking() override {
     if (shared_image) {
-      auto* sii = layer_tree_frame_sink->shared_image_interface();
+      auto sii = layer_tree_frame_sink->shared_image_interface();
       if (sii) {
         sii->DestroySharedImage(mailbox_sync_token, std::move(shared_image));
       }
@@ -332,7 +332,7 @@ void HeadsUpDisplayLayerImpl::UpdateHudTexture(
   } else {
     DCHECK_EQ(draw_mode, DRAW_MODE_SOFTWARE);
 
-    auto* sii = layer_tree_frame_sink->shared_image_interface();
+    auto sii = layer_tree_frame_sink->shared_image_interface();
     if (sii) {
       pool_resource = pool_->AcquireResource(internal_content_bounds_,
                                              viz::SinglePlaneFormat::kBGRA_8888,
@@ -454,10 +454,9 @@ void HeadsUpDisplayLayerImpl::UpdateHudTexture(
     SkiaPaintCanvas canvas(surface->getCanvas());
     DrawHudContents(&canvas);
 
-    if (backing->shared_image) {
-      backing->mailbox_sync_token =
-          layer_tree_frame_sink->shared_image_interface()
-              ->GenVerifiedSyncToken();
+    auto sii = layer_tree_frame_sink->shared_image_interface();
+    if (backing->shared_image && sii) {
+      backing->mailbox_sync_token = sii->GenVerifiedSyncToken();
     }
   }
 
