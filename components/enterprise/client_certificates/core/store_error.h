06285b1bb0179506ac8afedb1b10681f80881321
Sebastien Lalancette <seblalancette@chromium.org>
2024-02-01 20:25:40 +0000

    Create StoreError Enum and Update CertificateStore APIs
    Fixed: b:319256086
    Change-Id: Icf846b8e1b0224e326c8b78fe699122a975cd95d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5253880
    Reviewed-by: Hamda Mare <hmare@google.com>
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255248}

---
+++ components/enterprise/client_certificates/core/store_error.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ENTERPRISE_CLIENT_CERTIFICATES_CORE_STORE_ERROR_H_
+#define COMPONENTS_ENTERPRISE_CLIENT_CERTIFICATES_CORE_STORE_ERROR_H_
+
+#include "base/types/expected.h"
+
+namespace client_certificates {
+
+enum class StoreError {
+  kUnknown = 0,
+  kInvalidIdentityName = 1,
+  kInvalidDatabaseState = 2,
+  kGetDatabaseEntryFailed = 3,
+  kConflictingIdentity = 4,
+  kCreateKeyFailed = 5,
+  kSaveKeyFailed = 6,
+  kInvalidCertificateInput = 7,
+  kCertificateCommitFailed = 8,
+  kLoadKeyFailed = 9,
+  kMaxValue = kLoadKeyFailed
+};
+
+template <class T>
+using StoreErrorOr = base::expected<T, StoreError>;
+
+}  // namespace client_certificates
+
+#endif  // COMPONENTS_ENTERPRISE_CLIENT_CERTIFICATES_CORE_STORE_ERROR_H_
