3a689ddb3824671a1fc5942a7cdb09e683757fdf
Moteva <moteva@google.com>
2024-02-01 01:03:47 +0000

    CrOS Settings: js2gtest migration for os_settings_browsertest (21/N)
    Design Doc: go/cros-settings-js2gtest-migration
    
    BUG=b/315704162
    TEST=
    browser_test:
    AboutPage
    ApnSubpage
    CellularNetworksList
    CellularRoamingToggleButton
    EsimRemoveProfileDialog
    InternetPage
    OSSettingsResetTestSanitize
    
    Change-Id: Ie4cec7432fc84f658b02aac2fcbf8ce46190f5ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5245376
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Nikki Moteva <moteva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1254853}

--- chrome/test/data/webui/settings/chromeos/apn_subpage_test.ts
+++ chrome/test/data/webui/settings/chromeos/apn_subpage_test.ts
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://os-settings/lazy_load.js';
 
-import {Router, routes} from 'chrome://os-settings/os_settings.js';
 import {ApnSubpageElement} from 'chrome://os-settings/lazy_load.js';
+import {Router, routes} from 'chrome://os-settings/os_settings.js';
 import {ApnList} from 'chrome://resources/ash/common/network/apn_list.js';
 import {MojoInterfaceProviderImpl} from 'chrome://resources/ash/common/network/mojo_interface_provider.js';
 import {OncMojo} from 'chrome://resources/ash/common/network/onc_mojo.js';
--- chrome/test/data/webui/settings/chromeos/os_settings_browsertest.js
+++ chrome/test/data/webui/settings/chromeos/os_settings_browsertest.js
@@ -8,9 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 GEN_INCLUDE(['//chrome/test/data/webui/chromeos/polymer_browser_test_base.js']);
 
 GEN('#include "ash/constants/ash_features.h"');
-GEN('#include "build/branding_buildflags.h"');
 GEN('#include "chromeos/ash/components/standalone_browser/standalone_browser_features.h"');
-GEN('#include "chrome/common/buildflags.h"');
 GEN('#include "content/public/test/browser_test.h"');
 
 /** Test fixture for shared Polymer 3 elements. */
@@ -31,51 +29,16 @@ var OSSettingsBrowserTest = class extends PolymerTest {
   }
 };
 
-[['AboutPage', 'os_about_page_tests.js'],
- [
-   'ApnSubpage', 'apn_subpage_test.js', {enabled: ['ash::features::kApnRevamp']}
- ],
- ['CellularNetworksList', 'cellular_networks_list_test.js'],
- ['CellularRoamingToggleButton', 'cellular_roaming_toggle_button_test.js'],
- ['EsimRemoveProfileDialog', 'esim_remove_profile_dialog_test.js'],
- [
-   'InternetPage', 'internet_page_tests.js', {
-     enabled: [
-       'ash::features::kApnRevamp',
-       'ash::features::kHotspot',
-       'ash::features::kPasspointSettings',
-       'ash::features::kPasspointARCSupport',
-     ]
-   }
- ],
- ['OsResetPage', 'os_reset_page/os_reset_page_test.js'],
- [
-   'OsResetPageResetSettingsCardWithSanitize',
-   'os_reset_page/reset_settings_card_test.js',
-   {
-     disabled: ['ash::features::kOsSettingsRevampWayfinding'],
-     enabled: ['ash::features::kSanitize']
-   },
- ],
- [
-   'OsResetPageResetSettingsCardWithoutSanitize',
-   'os_reset_page/reset_settings_card_test.js',
-   {
-     disabled: [
-       'ash::features::kOsSettingsRevampWayfinding', 'ash::features::kSanitize'
-     ]
-   },
- ],
- [
-   'OsPeoplePageAccountManagerSubpageWithArcAccountRestrictionsEnabled',
-   'os_people_page/account_manager_subpage_test.js',
-   {
-     enabled: [
-       'ash::standalone_browser::features::kLacrosOnly',
-       'ash::standalone_browser::features::kLacrosProfileMigrationForceOff'
-     ]
-   },
- ],
+[[
+  'OsPeoplePageAccountManagerSubpageWithArcAccountRestrictionsEnabled',
+  'os_people_page/account_manager_subpage_test.js',
+  {
+    enabled: [
+      'ash::standalone_browser::features::kLacrosOnly',
+      'ash::standalone_browser::features::kLacrosProfileMigrationForceOff'
+    ]
+  },
+],
 ].forEach(test => registerTest(...test));
 
 function registerTest(testName, module, featureList) {
@@ -100,19 +63,5 @@ function registerTest(testName, module, featureList) {
     });
   }
 
-  // AboutPage has a test suite that can only succeed on official builds where
-  // the is_chrome_branded build flag is enabled.
-  if (testName === 'AboutPage') {
-    TEST_F(className, 'AllBuilds' || 'All', () => {
-      mocha.grep('/^(?!AboutPageTest_OfficialBuild).*$/').run();
-    });
-
-    GEN('#if BUILDFLAG(GOOGLE_CHROME_BRANDING)');
-    TEST_F(className, 'OfficialBuild' || 'All', () => {
-      mocha.grep('AboutPageTest_OfficialBuild').run();
-    });
-    GEN('#endif');
-  } else {
-    TEST_F(className, 'All', () => mocha.run());
-  }
+  TEST_F(className, 'All', () => mocha.run());
 }
