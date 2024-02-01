b2900894734eb4f0fa70a76c9c135fa1ec0b663e
Andrew Paseltiner <apaseltiner@chromium.org>
2024-02-01 20:26:25 +0000

    Make better use of Attribution Internals screen real estate
    - Remove redundant heading elements
    - Remove redundant "Source" prefix from "Registration Time" column
    - Reduce unnecessary margins/padding
    - Flatten unnecessarily complex HTML / CSS
    
    Bug: 1524079
    Change-Id: I57b101b92458c8d2cd653f8baf8063065cb7997d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5259182
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255249}

--- content/browser/resources/attribution_reporting/attribution_internals.ts
+++ content/browser/resources/attribution_reporting/attribution_internals.ts
@@ -362,7 +362,7 @@ class SourceTableModel extends ArrayTableModel<Source> {
           urlColumn('Source Origin', (e) => e.sourceOrigin),
           new ListColumn('Destinations', (e) => e.destinations, renderUrl),
           urlColumn('Reporting Origin', (e) => e.reportingOrigin),
-          dateColumn('Source Registration Time', (e) => e.sourceTime),
+          dateColumn('Registration Time', (e) => e.sourceTime),
           dateColumn('Expiry Time', (e) => e.expiryTime),
           new CodeColumn<Source>('Trigger Specs', (e) => e.triggerSpecs),
           dateColumn(
@@ -391,7 +391,7 @@ class SourceTableModel extends ArrayTableModel<Source> {
               'Aggregatable Dedup Keys', (e) => e.aggregatableDedupKeys,
               setInnerText, numberClass),
         ],
-        5,  // Sort by source registration time by default.
+        5,  // Sort by registration time by default.
         'No sources.',
     );
   }
@@ -764,7 +764,7 @@ class OsRegistrationTableModel extends ArrayTableModel<OsRegistration> {
           stringOrBoolColumn('Result', (e) => e.result),
         ],
         0,
-        'No OS Registrations',
+        'No OS registrations.',
     );
   }
 }
@@ -1093,11 +1093,10 @@ class AttributionInternals implements ObserverInterface {
 
   refresh(): void {
     this.handler.isAttributionReportingEnabled().then((response) => {
-      const featureStatusContent =
-          document.querySelector<HTMLElement>('#feature-status-content')!;
-      featureStatusContent.innerText =
-          response.enabled ? 'enabled' : 'disabled';
-      featureStatusContent.classList.toggle('disabled', !response.enabled);
+      const featureStatus =
+          document.querySelector<HTMLElement>('#feature-status')!;
+      featureStatus.innerText = response.enabled ? 'enabled' : 'disabled';
+      featureStatus.classList.toggle('disabled', !response.enabled);
 
       const reportDelaysContent =
           document.querySelector<HTMLElement>('#report-delays')!;
