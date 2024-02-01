0fc881d98643ad580033f833d0ea7217228f6718
Mohamed Heikal <mheikal@chromium.org>
2024-02-01 22:15:10 +0000

    Unused resources tool should ignore synthetic resource types
    Synthetic resource types, like public, overlayable or macro, do not
    actually appear in the resource table or as a file in the apk. They are
    just dummy resources that do not survive appt2 link step.
    UnusedResources.java should not output such resources as unused since
    aapt2 complains.
    
    Change-Id: I43156a93f4b8e00f3c9d38222e12955f18301a34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5259241
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Auto-Submit: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255317}

--- build/android/unused_resources/UnusedResources.java
+++ build/android/unused_resources/UnusedResources.java
@@ -24,6 +24,7 @@ package build.android.unused_resources;
 
 import static com.android.ide.common.symbols.SymbolIo.readFromAapt;
 import static com.android.utils.SdkUtils.endsWithIgnoreCase;
+
 import static com.google.common.base.Charsets.UTF_8;
 
 import com.android.ide.common.resources.usage.ResourceUsageModel;
@@ -40,6 +41,7 @@ import com.android.tools.r8.ResourceShrinker.Command;
 import com.android.tools.r8.ResourceShrinker.ReferenceChecker;
 import com.android.tools.r8.origin.PathOrigin;
 import com.android.utils.XmlUtils;
+
 import com.google.common.base.Charsets;
 import com.google.common.collect.Maps;
 import com.google.common.io.ByteStreams;
@@ -195,6 +197,11 @@ public class UnusedResources {
         StringBuilder sb = new StringBuilder();
         Collections.sort(mUnused);
         for (Resource resource : mUnused) {
+            if (resource.type.isSynthetic()) {
+                // Ignore synthetic resources like overlayable or macro that are
+                // not actually listed in the ResourceTable.
+                continue;
+            }
             sb.append(resource.type + "/" + resource.name + "#remove\n");
         }
         Files.asCharSink(destinationFile, UTF_8).write(sb.toString());
@@ -203,6 +210,12 @@ public class UnusedResources {
     private void dumpReferences() {
         if (mDebugPrinter != null) {
             mDebugPrinter.print(mModel.dumpReferences());
+        }
+    }
+
+    private void dumpModel() {
+        if (mDebugPrinter != null) {
+            mDebugPrinter.print(mModel.dumpResourceModel());
         }
     }
 
@@ -301,7 +314,8 @@ public class UnusedResources {
                         || line.startsWith("android.support.v7.widget.ResourcesWrapper ")
                         || (mResourcesWrapper == null // Recently wrapper moved
                                 && line.startsWith(
-                                        "android.support.v7.widget.TintContextWrapper$TintResources "))) {
+                                        "android.support.v7.widget.TintContextWrapper$TintResources"
+                                                + " "))) {
                     mResourcesWrapper =
                             line.substring(line.indexOf(arrowString) + arrowString.length(),
                                         line.indexOf(':') != -1 ? line.indexOf(':') : line.length())
