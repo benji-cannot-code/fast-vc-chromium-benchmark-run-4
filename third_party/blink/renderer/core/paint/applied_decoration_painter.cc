ec8ac9afd06ebeb1439ab2bd844af413b49f019e
Fredrik Söderquist <fs@opera.com>
2024-02-01 10:15:19 +0000

    Rework text-shadow painting for text decorations
    The current painting code-path for text-decorations with text-shadows is
    complicated: line-throughs are painted using a draw-looper while the
    rest of the decorations are painted for each shadow "layer" using a
    DropShadowPaintFilter.
    
    Unify these two by introducing a ScopedTextShadowPainter object that
    constructs a filter - either a single DropShadowPaintFilter or a merge
    filter combining one DropShadowPaintFilter for each layer. This makes
    the two cases consistent, and for non-line-through decorations there's
    no longer a need to run the paint code for each shadow "layer".
    
    Bug: 1521809
    Change-Id: I496d28672719e5efeee660dc780a96e433578456
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5250107
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1254970}

--- third_party/blink/renderer/core/paint/applied_decoration_painter.cc
+++ third_party/blink/renderer/core/paint/applied_decoration_painter.cc
@@ -12,11 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-void AppliedDecorationPainter::Paint(const cc::PaintFlags* flags) {
+void AppliedDecorationPainter::Paint(const Color& color,
+                                     const cc::PaintFlags* flags) {
   ETextDecorationStyle decoration_style = decoration_info_.DecorationStyle();
 
   context_.SetStrokeStyle(decoration_info_.StrokeStyle());
-  context_.SetStrokeColor(decoration_info_.LineColor());
+  context_.SetStrokeColor(color);
 
   AutoDarkMode auto_dark_mode(
       PaintAutoDarkMode(decoration_info_.TargetStyle(),
