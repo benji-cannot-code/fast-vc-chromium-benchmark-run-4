1856bb16118734db311495197d6558cb371cac63
David Yeung <dayeung@chromium.org>
2024-02-01 22:30:06 +0000

    Add trace event in TabSearch startup.
    Add a trace event right when TabSearch starts recording time.
    This is done to help with investigations in perfetto.
    
    Bug: 1519190
    Change-Id: Ic3d6bdc5e8d94f145e7314855761dcfef323e1b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5260337
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255321}

--- chrome/browser/ui/views/tab_search_bubble_host.cc
+++ chrome/browser/ui/views/tab_search_bubble_host.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/i18n/rtl.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/trace_event/trace_event.h"
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/feature_engagement/tracker_factory.h"
 #include "chrome/browser/ui/browser.h"
@@ -150,6 +151,7 @@ void TabSearchBubbleHost::OnUserInvokedFeature(const Browser* browser) {
 bool TabSearchBubbleHost::ShowTabSearchBubble(
     bool triggered_by_keyboard_shortcut,
     int tab_index) {
+  TRACE_EVENT0("ui", "TabSearchBubbleHost::ShowTabSearchBubble");
   if (tab_index >= 0) {
     profile_->GetPrefs()->SetInteger(tab_search_prefs::kTabSearchTabIndex,
                                      tab_index);
