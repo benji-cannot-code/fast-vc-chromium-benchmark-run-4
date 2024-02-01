2bd87329144099b3654c63264718a519e55aee73
Palak Agarwal <agpalak@chromium.org>
2024-02-01 18:11:34 +0000

    Detach ScriptPromiseResolver when SPRwT throws exception/error
    We encounter a check failure error about the resolver not being
    correctly detached when it hits one of the conditions where an
    exception/type error is thrown. This CL fixes it by manually doing it
    in these cases.
    
    Bug: 1504701
    Change-Id: I6f7d7936735dc049814453daa004440fe029a116
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5154251
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Palak Agarwal <agpalak@chromium.org>
    Reviewed-by: Tony Herre <toprice@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255143}

--- third_party/blink/renderer/modules/mediastream/media_devices.cc
+++ third_party/blink/renderer/modules/mediastream/media_devices.cc
@@ -442,6 +442,7 @@ ScriptPromise MediaDevices::getUserMedia(
   auto* resolver = MakeGarbageCollected<
       ScriptPromiseResolverWithTracker<UserMediaRequestResult>>(
       script_state, "Media.MediaDevices.GetUserMedia", base::Seconds(8));
+  const ScriptPromise promise = resolver->Promise();
 
   DCHECK(options);  // Guaranteed by the default value in the IDL.
   DCHECK(!exception_state.HadException());
@@ -450,9 +451,8 @@ ScriptPromise MediaDevices::getUserMedia(
       ToMediaStreamConstraints(options, exception_state);
   if (!constraints) {
     DCHECK(exception_state.HadException());
-    resolver->RecordResultAndLatency(
-        UserMediaRequestResult::kInvalidConstraints);
-    return ScriptPromise();
+    resolver->RecordAndDetach(UserMediaRequestResult::kInvalidConstraints);
+    return promise;
   }
 
   return SendUserMediaRequest(UserMediaRequestType::kUserMedia, resolver,
@@ -467,6 +467,7 @@ ScriptPromise MediaDevices::SendUserMediaRequest(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(!exception_state.HadException());
 
+  ScriptPromise promise = resolver->Promise();
   ScriptState* script_state = resolver->GetScriptState();
   if (!script_state->ContextIsValid()) {
     resolver->RecordAndThrowDOMException(
@@ -474,7 +475,7 @@ ScriptPromise MediaDevices::SendUserMediaRequest(
         "No media device client available; "
         "is this a detached window?",
         UserMediaRequestResult::kContextDestroyed);
-    return ScriptPromise();
+    return promise;
   }
 
   base::OnceCallback<void(const String&, CaptureController*)>
@@ -496,9 +497,8 @@ ScriptPromise MediaDevices::SendUserMediaRequest(
   }
 
   if (exception_state.HadException()) {
-    resolver->RecordResultAndLatency(
-        UserMediaRequestResult::kInvalidConstraints);
-    return ScriptPromise();
+    resolver->RecordAndDetach(UserMediaRequestResult::kInvalidConstraints);
+    return promise;
   }
 #endif
 
@@ -514,18 +514,16 @@ ScriptPromise MediaDevices::SendUserMediaRequest(
     surface = IdentifiableSurface::FromTypeAndToken(
         surface_type, TokenFromConstraints(options));
   }
-  ScriptPromise promise = resolver->Promise();
   UserMediaRequest* request =
       UserMediaRequest::Create(window, user_media_client, media_type, options,
                                callbacks, exception_state, surface);
   if (!request) {
     DCHECK(exception_state.HadException());
-    resolver->RecordResultAndLatency(
-        UserMediaRequestResult::kInvalidConstraints);
+    resolver->RecordAndDetach(UserMediaRequestResult::kInvalidConstraints);
     RecordIdentifiabilityMetric(
         surface, GetExecutionContext(),
         IdentifiabilityBenignStringToken(exception_state.Message()));
-    return ScriptPromise();
+    return promise;
   }
 
   String error_message;
@@ -533,7 +531,7 @@ ScriptPromise MediaDevices::SendUserMediaRequest(
     resolver->RecordAndThrowDOMException(
         exception_state, DOMExceptionCode::kNotSupportedError, error_message,
         UserMediaRequestResult::kInsecureContext);
-    return ScriptPromise();
+    return promise;
   }
 
 #if !BUILDFLAG(IS_ANDROID)
@@ -556,6 +554,7 @@ ScriptPromise MediaDevices::getAllScreensMedia(
   auto* resolver = MakeGarbageCollected<
       ScriptPromiseResolverWithTracker<UserMediaRequestResult>>(
       script_state, "Media.MediaDevices.GetAllScreensMedia", base::Seconds(6));
+  ScriptPromise promise = resolver->Promise();
 
   ExecutionContext* const context = GetExecutionContext();
   if (!context) {
@@ -563,7 +562,7 @@ ScriptPromise MediaDevices::getAllScreensMedia(
         exception_state, DOMExceptionCode::kInvalidStateError,
         "No media device client available; is this a detached window?",
         UserMediaRequestResult::kContextDestroyed);
-    return ScriptPromise();
+    return promise;
   }
 
   MediaStreamConstraints* constraints = MediaStreamConstraints::Create();
@@ -585,13 +584,14 @@ ScriptPromise MediaDevices::getDisplayMedia(
   auto* resolver = MakeGarbageCollected<
       ScriptPromiseResolverWithTracker<UserMediaRequestResult>>(
       script_state, "Media.MediaDevices.GetDisplayMedia", base::Seconds(6));
+  ScriptPromise promise = resolver->Promise();
 
   if (!window) {
     resolver->RecordAndThrowDOMException(
         exception_state, DOMExceptionCode::kInvalidStateError,
         "No local DOM window; is this a detached window?",
         UserMediaRequestResult::kContextDestroyed);
-    return ScriptPromise();
+    return promise;
   }
 
   const bool capture_allowed_by_permissions_policy = window->IsFeatureEnabled(
@@ -608,14 +608,15 @@ ScriptPromise MediaDevices::getDisplayMedia(
     resolver->RecordAndThrowDOMException(
         exception_state, DOMExceptionCode::kNotAllowedError,
         kFeaturePolicyBlocked, UserMediaRequestResult::kNotAllowedError);
-    return ScriptPromise();
+    return promise;
   }
 
   if (!TransientActivationRequirementSatisfied(window)) {
-    exception_state.ThrowDOMException(
-        DOMExceptionCode::kInvalidStateError,
-        "getDisplayMedia() requires transient activation (user gesture).");
-    return ScriptPromise();
+    resolver->RecordAndThrowDOMException(
+        exception_state, DOMExceptionCode::kInvalidStateError,
+        "getDisplayMedia() requires transient activation (user gesture).",
+        UserMediaRequestResult::kInvalidStateError);
+    return promise;
   }
 
   if (options->hasAutoSelectAllScreens() && options->autoSelectAllScreens()) {
@@ -624,7 +625,7 @@ ScriptPromise MediaDevices::getDisplayMedia(
         "The autoSelectAllScreens property is not allowed for usage with "
         "getDisplayMedia.",
         UserMediaRequestResult::kInvalidConstraints);
-    return ScriptPromise();
+    return promise;
   }
 
   if (CaptureController* const capture_controller =
@@ -635,8 +636,7 @@ ScriptPromise MediaDevices::getDisplayMedia(
           "A CaptureController object may only be used with a single "
           "getDisplayMedia() invocation.",
           UserMediaRequestResult::kInvalidStateError);
-
-      return ScriptPromise();
+      return promise;
     }
     capture_controller->SetIsBound(true);
   }
