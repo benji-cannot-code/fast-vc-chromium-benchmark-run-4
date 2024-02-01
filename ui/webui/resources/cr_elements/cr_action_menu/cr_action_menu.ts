d7314fda221ccd4d83d3bd58dd2370c3921e3c8a
dpapad <dpapad@chromium.org>
2024-02-01 20:40:43 +0000

    WebUI: Add a public fire() helper method in CrLitElement.
    There are currently too many such one-off methods in various
    PolymerElement subclasses. Offering such a method in CrLitElement is an
    opportunity to eliminate this duplication as more elements get migrated
    to Lit.
    
    Bug: 1503223
    Change-Id: I6038bcd6be272698501348738de2359c70d59494
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5255738
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255260}

--- ui/webui/resources/cr_elements/cr_action_menu/cr_action_menu.ts
+++ ui/webui/resources/cr_elements/cr_action_menu/cr_action_menu.ts
@@ -174,11 +174,6 @@ export class CrActionMenuElement extends CrLitElement {
     this.removeListeners_();
   }
 
-  private fire_(eventName: string, detail?: any) {
-    this.dispatchEvent(
-        new CustomEvent(eventName, {bubbles: true, composed: true, detail}));
-  }
-
   /**
    * Exposing internal <dialog> elements for tests.
    */
@@ -204,7 +199,7 @@ export class CrActionMenuElement extends CrLitElement {
 
     // Catch and re-fire the 'close' event such that it bubbles across Shadow
     // DOM v1.
-    this.fire_('close');
+    this.fire('close');
   }
 
   private onClick_(e: Event) {
@@ -220,7 +215,7 @@ export class CrActionMenuElement extends CrLitElement {
     if (e.key === 'Tab' || e.key === 'Escape') {
       this.close();
       if (e.key === 'Tab') {
-        this.fire_('tabkeyclose', {shiftKey: e.shiftKey});
+        this.fire('tabkeyclose', {shiftKey: e.shiftKey});
       }
       e.preventDefault();
       return;
@@ -305,7 +300,7 @@ export class CrActionMenuElement extends CrLitElement {
       this.lastConfig_ = null;
     }
 
-    this.fire_('open-changed', {value: this.open});
+    this.fire('open-changed', {value: this.open});
   }
 
   /**
@@ -410,7 +405,7 @@ export class CrActionMenuElement extends CrLitElement {
       }
     }
 
-    this.fire_('open-changed', {value: this.open});
+    this.fire('open-changed', {value: this.open});
   }
 
   private resetStyle_() {
@@ -478,7 +473,7 @@ export class CrActionMenuElement extends CrLitElement {
       this.resizeObserver_ = new ResizeObserver(() => {
         if (this.lastConfig_) {
           this.positionDialog_(this.lastConfig_);
-          this.fire_('cr-action-menu-repositioned');  // For easier testing.
+          this.fire('cr-action-menu-repositioned');  // For easier testing.
         }
       });
 
