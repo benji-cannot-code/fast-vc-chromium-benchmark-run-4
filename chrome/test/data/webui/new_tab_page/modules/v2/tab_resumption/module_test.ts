8095d064f36de05c6a5bbb55ff9ac348409b4ebc
Marlon Facey <mfacey@chromium.org>
2024-02-02 17:36:57 +0000

    [NTP XDTR] Add click metrics
    Add metrics for which tab is clicked and the time since that tab was
    last visited. Also adds the tab resumption module to the enum so other
    histograms will be updated accordingly.
    
    Bug: 1512173
    Change-Id: I83cd43f39f4ad40067b13cf654891dc5647a5728
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5238678
    Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255633}

--- chrome/test/data/webui/new_tab_page/modules/v2/tab_resumption/module_test.ts
+++ chrome/test/data/webui/new_tab_page/modules/v2/tab_resumption/module_test.ts
@@ -27,7 +27,8 @@ function createSampleTab(
         sessionName: 'Test Device',
         url: {url: 'https://www.foo.com'},
         title: 'Test Tab Title',
-        relativeTime: '0 seconds ago',
+        relativeTime: 0,
+        relativeTimeText: '0 seconds ago',
       },
       overrides);
 
