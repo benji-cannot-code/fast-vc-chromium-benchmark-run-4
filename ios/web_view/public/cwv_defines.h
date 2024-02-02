ba5e344e6feff036d3035f487817992eaaf71861
Anuj Kumar <anujbh@google.com>
2024-02-02 19:46:32 +0000

    defines USE_OPTIMIZED_SESSION_STORAGE for using from iGA
    Change-Id: Ifb9a89dd3f8e68a1d184859bb778ce20ca829a56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5254201
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Anuj Kumar <anujbh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1255709}

--- ios/web_view/public/cwv_defines.h
+++ ios/web_view/public/cwv_defines.h
@@ -98,4 +98,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Supports +[CWVWebView webInspectorEnabled].
 #define IOS_WEB_VIEW_SUPPORTS_WEB_INSPECTOR_API 1
 
+// Supports +[CWVWebView useOptimizedSessionStorage].
+#define IOS_WEB_VIEW_SUPPORTS_OPTIMIZED_STORAGE 1
+
 #endif  // IOS_WEB_VIEW_PUBLIC_CWV_DEFINES_H_
