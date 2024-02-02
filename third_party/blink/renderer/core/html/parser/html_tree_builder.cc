80e0e3deb5430be968bd5d3c3db8de6bbaca1ae2
Joey Arhar <jarhar@chromium.org>
2024-02-02 21:28:28 +0000

    Fix some crashes in StylableSelect HTML parser
    This patch forbids nested <button>s and <datalist>s to avoid setting
    kInSelectMode while we are technically still in a <button> or
    <datalist>.
    
    This patch also replaces a call to SetInsertionMode() with
    ResetInsertionModeAppropriately() and moves it after processing the end
    tag in order to avoid ending up in kInSelectMode when there is not
    actually an open <select> tag.
    
    The clusterfuzz crash didn't have a minimal repro but I manually
    verified that the crash does not repro with this patch.
    
    Bug: 1511354
    Fixed: 1519396
    Change-Id: I4358973b36925ea0862f4050f27f804d2676288c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5254124
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255770}

--- third_party/blink/renderer/core/html/parser/html_tree_builder.cc
+++ third_party/blink/renderer/core/html/parser/html_tree_builder.cc
@@ -1477,6 +1477,13 @@ void HTMLTreeBuilder::ProcessStartTag(AtomicHTMLToken* token) {
           ProcessEndTag(&end_select);
           break;
         }
+        case HTMLTag::kButton:
+        case HTMLTag::kDatalist:
+          // Don't allow nesting of buttons or datalists in this mode because
+          // when we end these tags we go back to kInSelectMode.
+          // TODO(crbug.com/1511354): Try removing this when kButton and
+          // kDatalist are handled in ResetInsertionModeAppropriately
+          break;
         default:
           ProcessStartTagForInBody(token);
           break;
@@ -1837,6 +1844,9 @@ void HTMLTreeBuilder::ResetInsertionModeAppropriately() {
             }
           }
           return SetInsertionMode(kInSelectMode);
+        // TODO(crbug.com/1511354): Consider adding cases for kButton and
+        // kDatalist here to reset the insertion mode to kInButtonInSelectMode
+        // and kInDatalistInSelectMode
         case HTMLTag::kTd:
         case HTMLTag::kTh:
           return SetInsertionMode(kInCellMode);
@@ -2420,11 +2430,7 @@ void HTMLTreeBuilder::ProcessEndTag(AtomicHTMLToken* token) {
     case kInButtonInSelectMode:
       CHECK(RuntimeEnabledFeatures::StylableSelectEnabled());
       switch (tag) {
-        case HTMLTag::kButton:
-          SetInsertionMode(kInSelectMode);
-          break;
         case HTMLTag::kSelect:
-          SetInsertionMode(kInSelectMode);
           tree_.OpenElements()->PopUntilPopped(HTMLTag::kSelect);
           ResetInsertionModeAppropriately();
           return;
@@ -2434,12 +2440,12 @@ void HTMLTreeBuilder::ProcessEndTag(AtomicHTMLToken* token) {
       // TODO(crbug.com/1511354): We need to review which end tags cause which
       // elements to close once there's a reviewed HTML PR for styleable select.
       ProcessEndTagForInBody(token);
+      if (tag == HTMLTag::kButton) {
+        ResetInsertionModeAppropriately();
+      }
       return;
     case kInDatalistInSelectMode:
       switch (tag) {
-        case HTMLTag::kDatalist:
-          SetInsertionMode(kInSelectMode);
-          break;
         case HTMLTag::kSelect:
           SetInsertionMode(kInSelectMode);
           tree_.OpenElements()->PopUntilPopped(HTMLTag::kSelect);
@@ -2449,6 +2455,9 @@ void HTMLTreeBuilder::ProcessEndTag(AtomicHTMLToken* token) {
           break;
       }
       ProcessEndTagForInBody(token);
+      if (tag == HTMLTag::kDatalist) {
+        ResetInsertionModeAppropriately();
+      }
       return;
     case kInSelectInTableMode:
       switch (tag) {
