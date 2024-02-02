d7b31668d2c3a1372ccb9f65e5a9a77bd1b0e721
Jack Yammine <jyammine@google.com>
2024-02-02 15:48:32 +0000

    [CodeHealth]Remove expired histograms Tabs.StateTransfer.NumberOfOtherTabsActivatedBeforeMadeActive
    OBSOLETE_HISTOGRAM[Tabs.StateTransfer.NumberOfOtherTabsActivatedBeforeMadeActive]=
    The histogram is expired as of M77.
    
    Fixed: 1499500
    Change-Id: I8562fae30e5f878f190f409b13fbb06c8ecf4b91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5261884
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Jack Yammine <jyammine@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255574}

--- chrome/browser/ui/tabs/tab_strip_model_stats_recorder_unittest.cc
+++ chrome/browser/ui/tabs/tab_strip_model_stats_recorder_unittest.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
-#include "base/test/metrics/histogram_tester.h"
 #include "chrome/browser/ui/tabs/tab_enums.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/tabs/test_tab_strip_model_delegate.h"
@@ -19,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-using base::HistogramTester;
 using content::WebContents;
 
 class TabStripModelStatsRecorderTest : public ChromeRenderViewHostTestHarness {
@@ -32,8 +30,6 @@ TEST_F(TabStripModelStatsRecorderTest,
 
   TabStripModelStatsRecorder recorder;
   tabstrip.AddObserver(&recorder);
-
-  HistogramTester tester;
 
   // Create first tab
   std::unique_ptr<WebContents> contents0 = CreateTestWebContents();
@@ -52,9 +48,6 @@ TEST_F(TabStripModelStatsRecorderTest,
                          TabStripUserGestureDetails(
                              TabStripUserGestureDetails::GestureType::kOther));
 
-  tester.ExpectUniqueSample(
-      "Tabs.StateTransfer.NumberOfOtherTabsActivatedBeforeMadeActive", 9, 1);
-
   tabstrip.RemoveObserver(&recorder);
   tabstrip.CloseAllTabs();
 }
@@ -66,8 +59,6 @@ TEST_F(TabStripModelStatsRecorderTest,
 
   TabStripModelStatsRecorder recorder;
   tabstrip.AddObserver(&recorder);
-
-  HistogramTester tester;
 
   // Create tab 0, 1, 2
   std::unique_ptr<WebContents> contents0 = CreateTestWebContents();
@@ -97,12 +88,6 @@ TEST_F(TabStripModelStatsRecorderTest,
   tabstrip.ActivateTabAt(tabstrip.GetIndexOfWebContents(raw_contents2),
                          TabStripUserGestureDetails(
                              TabStripUserGestureDetails::GestureType::kOther));
-
-  EXPECT_THAT(
-      tester.GetAllSamples(
-          "Tabs.StateTransfer.NumberOfOtherTabsActivatedBeforeMadeActive"),
-      testing::ElementsAre(base::Bucket(1, 8), base::Bucket(2, 2),
-                           base::Bucket(10, 1)));
 
   tabstrip.RemoveObserver(&recorder);
   tabstrip.CloseAllTabs();
