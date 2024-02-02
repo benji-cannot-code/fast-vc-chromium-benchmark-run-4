1a7a30e55ea15bec7fb4b583cb3ffeb322fac65a
Federica Germinario <fedegermi@google.com>
2024-02-02 15:20:36 +0000

    [ios] Give circular shape to voice search button
    This change modifies the hover effect of the voice search icon.
    Screenshots of this change: crbug.com/1522115.
    There is no visible change on iPad (this was tested both in Light
    and Dark mode).
    
    Bug: 1522115
    Change-Id: I8e335b11ff38f209b1b79afb55f3f3413dd03b89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5238433
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255566}

--- ios/chrome/browser/ui/location_bar/location_bar_view_controller.mm
+++ ios/chrome/browser/ui/location_bar/location_bar_view_controller.mm
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/orchestrator/location_bar_offset_provider.h"
 #import "ios/chrome/browser/ui/toolbar/public/toolbar_type.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
+#import "ios/chrome/common/ui/util/pointer_interaction_util.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/public/provider/chrome/browser/lens/lens_api.h"
 #import "ui/base/l10n/l10n_util.h"
@@ -497,6 +498,15 @@ const NSString* kScribbleOmniboxElementId = @"omnibox";
           l10n_util::GetNSString(IDS_IOS_TOOLS_MENU_VOICE_SEARCH);
       self.locationBarSteadyView.trailingButton.accessibilityIdentifier =
           kOmniboxVoiceSearchButtonIdentifier;
+
+      UIButtonConfiguration* buttonConfig =
+          [UIButtonConfiguration plainButtonConfiguration];
+      buttonConfig.contentInsets = NSDirectionalEdgeInsetsMake(0, 0, 0, 0);
+      self.locationBarSteadyView.trailingButton.configuration = buttonConfig;
+      // Make the pointer shape fit the location bar's semi-circle end shape.
+      self.locationBarSteadyView.trailingButton.pointerStyleProvider =
+          CreateLiftEffectCirclePointerStyleProvider();
+
       [self.locationBarSteadyView enableTrailingButton:YES];
     }
   }
