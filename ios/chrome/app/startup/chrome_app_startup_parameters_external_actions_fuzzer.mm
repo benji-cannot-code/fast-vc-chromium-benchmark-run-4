29643f4eb4363bdf2a221d24c54f01575f46b8ea
Nicolas MacBeth <nicolasmacbeth@google.com>
2024-02-02 16:55:32 +0000

    [iOS fuzzer] Fixed crashes in chrome_app_startup_parameters fuzzer
    Fixed two issues with crashing in the three fuzzers for
    chrome_app_startup_parameters. An AtExitManager was added into the
    fuzzer's environment, which is needed as a callback for CHECKs that
    naturally fail in the fuzzer's limited environment. Also, we
    instantiate the i18n library since it is needed by GURL inside the
    fuzzed function with i18n::InitializeICU().
    
    Bug: 1523661
    Change-Id: I8a4e6bf2b3410c65b119fad006c3ac24fb34145e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5253738
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1255612}

--- ios/chrome/app/startup/chrome_app_startup_parameters_external_actions_fuzzer.mm
+++ ios/chrome/app/startup/chrome_app_startup_parameters_external_actions_fuzzer.mm
@@ -5,7 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/app/startup/chrome_app_startup_parameters.h"
 
+#import "base/at_exit.h"
+#import "base/i18n/icu_util.h"
+
+struct Environment {
+  Environment() { CHECK(base::i18n::InitializeICU()); }
+  base::AtExitManager at_exit;
+};
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  static Environment env;
+
   std::string str = std::string(reinterpret_cast<const char*>(data), size);
 
   NSString* formatted_string = [NSString
