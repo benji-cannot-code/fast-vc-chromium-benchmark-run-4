78f86eedaf828211dbc15d312e7267ced5f31bea
Monica Basta <msalama@chromium.org>
2024-02-02 14:42:12 +0000

    [UNO] Show chrome sign in bubble if chrome isn't signed in
    This CL changes the Chrome sign in bubble to always show it if:
    - the user isn't signed to chrome
    - and sign in is happening through the web
    
    This changes from the previous behavior that the bubble will also be
    shown on reauth and for secondary accounts.
    
    This CL also changes the priority of recording
    `ShouldShowChromeSigninBubbleWithReason`. It will be easier to
    understand the data if we first exclude sign in that aren't happening
    through the web. This will help exclude users that sign in through
    chrome from the bucket `kShouldNotShowAlreadySignedIn`.
    
    Bug: b/320426816
    Change-Id: I83d93bf9369f1ed627d90b0b48e65ad0ad0daf92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5224249
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255554}

--- chrome/browser/signin/dice_web_signin_interceptor_browsertest.cc
+++ chrome/browser/signin/dice_web_signin_interceptor_browsertest.cc
@@ -444,9 +444,12 @@ IN_PROC_BROWSER_TEST_F(DiceWebSigninInterceptorBrowserTest, SwitchAlreadyOpen) {
 // in the new profile (regression test for https://crbug.com/1153321).
 IN_PROC_BROWSER_TEST_F(DiceWebSigninInterceptorBrowserTest, CloseSourceTab) {
   // Setup profile for interception.
-  identity_test_env()->MakeAccountAvailable("alice@example.com");
-  AccountInfo account_info =
-      MakeAccountInfoAvailableAndUpdate("bob@example.com");
+  AccountInfo primary_account_info =
+      identity_test_env()->MakePrimaryAccountAvailable(
+          "alice@gmail.com", signin::ConsentLevel::kSignin);
+
+  AccountInfo account_info = MakeAccountInfoAvailableAndUpdate(
+      "bob@example.com", kNoHostedDomainFound);
 
   // Add a tab.
   GURL intercepted_url = embedded_test_server()->GetURL("/defaultresponse");
@@ -1646,7 +1649,7 @@ IN_PROC_BROWSER_TEST_F(
 
   CheckHistograms(
       histogram_tester,
-      SigninInterceptionHeuristicOutcome::kAbortAccountInfoNotCompatible);
+      SigninInterceptionHeuristicOutcome::kAbortAccountNotNew);
 }
 
 // Tests the complete profile switch flow when the profile is not loaded.
