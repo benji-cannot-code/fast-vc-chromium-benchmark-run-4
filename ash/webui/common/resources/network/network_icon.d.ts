d0b332b0920fbe44e1c4c1527a20d9d46739bc0d
Chad Duffin <chadduffin@chromium.org>
2024-02-02 19:58:50 +0000

    webui: Migrate common network Polymer elements to class syntax
    This change migrates a few common network Polymer elements to use the
    class syntax in preparation for migrating the network_ui directory,
    which depends on these elements, to TypeScript.
    
    Bug: b/314832958
    Test: Manually verified on device.
    Change-Id: I09c665a4222492e6ea37bcf5ca1cc1993f3c216b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5226201
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255713}

--- ash/webui/common/resources/network/network_icon.d.ts
+++ ash/webui/common/resources/network/network_icon.d.ts
@@ -3,4 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-export {};
+import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {OncMojo} from 'chrome://resources/ash/common/network/onc_mojo.js';
+
+export class NetworkIconElement extends PolymerElement {
+  networkState: OncMojo.NetworkStateProperties|undefined;
+  isListItem: boolean;
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'network-icon': NetworkIconElement;
+  }
+}
