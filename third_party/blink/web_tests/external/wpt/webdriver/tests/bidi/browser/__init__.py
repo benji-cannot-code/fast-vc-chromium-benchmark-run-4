64ce648ef835a9a03ded2a1c209d53e1e36d8b89
Jonathan Lee <jonathanjlee@google.com>
2024-02-02 21:57:32 +0000

    Import wpt@14d6dc2e20b6f40eb058e8f550b65e477a81ba0b
    Using wpt-import in Chromium 128bf6579fa6bba1de37e7e9357b2f23ee2e3212.
    With Chromium commits locally applied on WPT:
    8017da1289 "Add WPTs for race-network-and-fetch-handler option"
    
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    mstensho@chromium.org:
      external/wpt/css/printing
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: If33f6bd918f29323404da60db5ae8a863a5443d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5262099
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1255784}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/browser/__init__.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/browser/__init__.py
@@ -1,7 +1,36 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+from webdriver.bidi.modules.script import ContextTarget
+
 async def get_user_context_ids(bidi_session):
     """
     Returns the list of string ids of the current user contexts.
     """
     user_contexts = await bidi_session.browser.get_user_contexts()
     return [user_context_info["userContext"] for user_context_info in user_contexts]
+
+
+async def set_local_storage(bidi_session, context: str, key: str, value: str):
+    """
+    Sets the value for the key in the context's localStorage.
+    """
+    await bidi_session.script.call_function(
+        function_declaration="""(key, value) => localStorage.setItem(key, value)""",
+        arguments=[{"type": "string", "value": key}, {"type": "string", "value": value}],
+        await_promise=False,
+        target=ContextTarget(context["context"]),
+    )
+
+
+async def get_local_storage(bidi_session, context: str, key: str):
+    """
+    Returns the value identified by the key from the context's localStorage.
+    """
+    result = await bidi_session.script.call_function(
+        function_declaration="""(key) => localStorage.getItem(key)""",
+        arguments=[{"type": "string", "value": key}],
+        await_promise=False,
+        target=ContextTarget(context["context"]),
+    )
+    if not "value" in result:
+        return None
+    return result["value"]
