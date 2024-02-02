eb5759d88563350ef3511d0c44cac8da5e4a8d2a
YuhanYang <yyhyyh@google.com>
2024-02-02 20:26:19 +0000

    flag: Remove shortcut-customization-jelly flag
    Bug: b/323429208
    Test: try jobs
    Change-Id: Ibc3cff8930bb4581f99eb114a562ecb04c9646bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5260987
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255731}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2560,12 +2560,6 @@ BASE_FEATURE(kShimlessRMA3pDiagnosticsDevMode,
              "ShimlessRMA3pDiagnosticsDevMode",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-// If enabled, the jelly colors will be used in the shortcut customization app.
-// Requires jelly-colors flag to also be enabled.
-BASE_FEATURE(kShortcutCustomizationJelly,
-             "ShortcutCustomizationJelly",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 // If enabled, system shortcuts will utilize state machiens instead of
 // keeping track of entire history of keys pressed.
 BASE_FEATURE(kShortcutStateMachines,
@@ -3748,8 +3742,7 @@ bool IsJellyEnabledForScanningApp() {
 }
 
 bool IsJellyEnabledForShortcutCustomization() {
-  return chromeos::features::IsJellyEnabled() &&
-         base::FeatureList::IsEnabled(kShortcutCustomizationJelly);
+  return chromeos::features::IsJellyEnabled();
 }
 
 bool IsKerberosRememberPasswordByDefaultEnabled() {
