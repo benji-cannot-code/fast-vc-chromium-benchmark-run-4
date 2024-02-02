957e961b2bdf11b6e320a4b3716c06185ed5f04e
Mason Freed <masonf@chromium.org>
2024-02-02 23:16:05 +0000

    Add unicode-bidi UA stylesheet rules
    Per spec:
    
      https://html.spec.whatwg.org/multipage/rendering.html#bidi-rendering
    
    this should be in the UA stylesheet:
    
    address, blockquote, center, div, figure, figcaption, footer,
    form, header, hr, legend, listing, main, p, plaintext, pre,
    summary, xmp, article, aside, h1, h2, h3, h4, h5, h6, hgroup,
    nav, section, search, table, caption, colgroup, col, thead,
    tbody, tfoot, tr, td, th, dir, dd, dl, dt, menu, ol, ul, li,
    bdi, output, [dir=ltr i], [dir=rtl i], [dir=auto i] {
      unicode-bidi: isolate;
    }
    
    To avoid breakage, I just added the elements, and not the [dir]
    related rules, nor did I change the <bdo> rules.
    
    Firefox and WebKit are both making the same change:
    https://bugzilla.mozilla.org/show_bug.cgi?id=1874033
    https://bugs.webkit.org/show_bug.cgi?id=267887
    
    The stylesheet change interacts with the code at [1], and that code
    also handles some of the cases such as [dir=auto] and bdi/bdo.
    I will clean up some of the code at [1] in a followup CL.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/html/html_element.cc;l=344-363;drc=20fecf94cc2226674dfefd698e96d8d44485b19c
    
    Fixed: 1520671
    Change-Id: Icaa32061efc59d963942b0629327bb596ac224ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5240082
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255819}

--- third_party/blink/renderer/core/html/forms/date_time_edit_element.cc
+++ third_party/blink/renderer/core/html/forms/date_time_edit_element.cc
@@ -491,6 +491,8 @@ void DateTimeEditBuilder::VisitLiteral(const String& text) {
   auto* element =
       MakeGarbageCollected<HTMLDivElement>(EditElement().GetDocument());
   element->SetShadowPseudoId(text_pseudo_id);
+  element->SetInlineStyleProperty(CSSPropertyID::kUnicodeBidi,
+                                  CSSValueID::kNormal);
   if (parameters_.locale.IsRTL() && text.length()) {
     WTF::unicode::CharDirection dir = WTF::unicode::Direction(text[0]);
     if (dir == WTF::unicode::kSegmentSeparator ||
@@ -546,6 +548,7 @@ DateTimeEditElement::DateTimeEditElement(Document& document,
   SetHasCustomStyleCallbacks();
   SetShadowPseudoId(AtomicString("-webkit-datetime-edit"));
   setAttribute(html_names::kIdAttr, shadow_element_names::kIdDateTimeEdit);
+  SetInlineStyleProperty(CSSPropertyID::kUnicodeBidi, CSSValueID::kNormal);
 }
 
 DateTimeEditElement::~DateTimeEditElement() = default;
@@ -769,6 +772,8 @@ void DateTimeEditElement::GetLayout(const LayoutParameters& layout_parameters,
   if (!HasChildren()) {
     auto* element = MakeGarbageCollected<HTMLDivElement>(GetDocument());
     element->SetShadowPseudoId(fields_wrapper_pseudo_id);
+    element->SetInlineStyleProperty(CSSPropertyID::kUnicodeBidi,
+                                    CSSValueID::kNormal);
     AppendChild(element);
   }
   Element* fields_wrapper = FieldsWrapperElement();
--- third_party/blink/renderer/core/html/forms/text_field_input_type.cc
+++ third_party/blink/renderer/core/html/forms/text_field_input_type.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/html/forms/text_field_input_type.h"
 
+#include "third_party/blink/renderer/core/css_value_keywords.h"
 #include "third_party/blink/renderer/core/dom/events/event_dispatch_forbidden_scope.h"
 #include "third_party/blink/renderer/core/dom/focus_params.h"
 #include "third_party/blink/renderer/core/dom/node_computed_style.h"
@@ -337,6 +338,8 @@ void TextFieldInputType::CreateShadowSubtree() {
 
   Document& document = GetElement().GetDocument();
   auto* container = MakeGarbageCollected<HTMLDivElement>(document);
+  container->SetInlineStyleProperty(CSSPropertyID::kUnicodeBidi,
+                                    CSSValueID::kNormal);
   container->SetIdAttribute(shadow_element_names::kIdTextFieldContainer);
   container->SetShadowPseudoId(
       shadow_element_names::kPseudoTextFieldDecorationContainer);
