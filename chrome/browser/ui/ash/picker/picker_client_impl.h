46357c2ab859212ca2b95fd4799055280167ad31
mlcui <mlcui@google.com>
2024-02-02 00:27:22 +0000

    picker: Store and maintain a `Profile` pointer in `PickerClientImpl`
    We will need to know when the current `Profile` is updated when we add a
    `app_list::SearchEngine` to `PickerClientImpl` in the future.
    
    Bug: b:316936687
    Change-Id: I4c8978440c33c7682f5db4091d5426c0a6ee5d1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5247607
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255372}

--- chrome/browser/ui/ash/picker/picker_client_impl.h
+++ chrome/browser/ui/ash/picker/picker_client_impl.h
@@ -7,13 +7,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_ASH_PICKER_PICKER_CLIENT_IMPL_H_
 
 #include "ash/public/cpp/picker/picker_client.h"
+#include "base/memory/weak_ptr.h"
+#include "chrome/browser/ash/login/session/user_session_manager.h"
+
+class Profile;
 
 namespace ash {
 class PickerController;
 }
 
+namespace user_manager {
+class User;
+}
+
 // Implements the PickerClient used by Ash.
-class PickerClientImpl : public ash::PickerClient {
+class PickerClientImpl
+    : public ash::PickerClient,
+      public user_manager::UserManager::UserSessionStateObserver {
  public:
   // Sets this instance as the client of `controller`.
   // Automatically unsets the client when this instance is destroyed.
@@ -28,8 +38,17 @@ class PickerClientImpl : public ash::PickerClient {
   void DownloadGifToString(const GURL& url,
                            DownloadGifToStringCallback callback) override;
 
+  // user_manager::UserManager::UserSessionStateObserver:
+  void ActiveUserChanged(user_manager::User* active_user) override;
+
  private:
+  void SetProfileByUser(const user_manager::User* user);
+  void SetProfile(Profile* profile);
+
   raw_ptr<ash::PickerController> controller_ = nullptr;
+  raw_ptr<Profile> profile_ = nullptr;
+
+  base::WeakPtrFactory<PickerClientImpl> weak_factory_{this};
 };
 
 #endif  // CHROME_BROWSER_UI_ASH_PICKER_PICKER_CLIENT_IMPL_H_
