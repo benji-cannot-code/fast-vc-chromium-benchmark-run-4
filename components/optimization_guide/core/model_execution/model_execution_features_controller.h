84a1fb5a99720fb2bd02ccdf0248a00869182fed
rajendrant <rajendrant@chromium.org>
2024-02-02 04:49:54 +0000

    Enable uninitialized features when main toggle is enabled at startup
    This allows any new model execution features to be automatically enabled, when user has main toggle is on.
    
    Bug: b/322896785
    Change-Id: I788def7e6294c4da9f21dcf671026ee527a2504b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5251767
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255434}

--- components/optimization_guide/core/model_execution/model_execution_features_controller.h
+++ components/optimization_guide/core/model_execution/model_execution_features_controller.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list_types.h"
 #include "base/scoped_observation.h"
 #include "base/threading/thread_checker.h"
+#include "components/optimization_guide/core/model_execution/model_execution_prefs.h"
 #include "components/optimization_guide/core/model_execution/settings_enabled_observer.h"
 #include "components/optimization_guide/core/optimization_guide_prefs.h"
 #include "components/optimization_guide/proto/model_execution.pb.h"
@@ -114,8 +115,9 @@ class ModelExecutionFeaturesController
   UserValidityResult GetCurrentUserValidityResult(
       proto::ModelExecutionFeature feature) const;
 
-  // Returns whether the `feature` is allowed by enterprise policy.
-  bool IsAllowedByEnterprisePolicy(proto::ModelExecutionFeature feature) const;
+  // Returns the enterprise policy value for the `feature`.
+  model_execution::prefs::ModelExecutionEnterprisePolicyValue
+  GetEnterprisePolicyValue(proto::ModelExecutionFeature feature) const;
 
   // Initializes the state of the different features at startup.
   void InitializeFeatureSettings();
@@ -124,7 +126,8 @@ class ModelExecutionFeaturesController
   // callbacks.
   void InitializePrefListener();
 
-  // Resets the prefs for features that were invalid.
+  // Resets the prefs for features that were enabled back to invalid state, when
+  // the conditions disallow the features.
   void ResetInvalidFeaturePrefs();
 
   // Computed at the time `this` is constructed. Stores the set of features
