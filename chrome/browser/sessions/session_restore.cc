0ec62a5a11e74b3f9702d64abcf455b6a8886f3c
Yongshun Liu <yongshun@chromium.org>
2024-02-02 22:00:57 +0000

    desk_profiles: Do not restore to the previous desk for new browser
    With Desk Profiles feature, chromeos can now associate a lacros profile
    with a virtual desk. When launching a new browser within the desk, a
    browser for that profile will be launched. With session restore, it
    restores the new browser window to its previous desk.
    
    This CL proposes that when lacros and Desk Profiles are both enabled,
    the new browser window should *not* be restored to its previous desk.
    
    Quick demo can be found at - b/319730718.
    
    Bug: b/319730718
    Test: manual
    Change-Id: I20360313714ac152d8c681f3d1d47e4b88e3f383
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5249158
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255786}

--- chrome/browser/sessions/session_restore.cc
+++ chrome/browser/sessions/session_restore.cc
@@ -108,6 +108,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/layer.h"
 #endif
 
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "chromeos/constants/chromeos_features.h"
+#endif
+
 using content::NavigationController;
 using content::RenderWidgetHost;
 using content::WebContents;
@@ -969,7 +973,17 @@ class SessionRestoreImpl : public BrowserListObserver {
 #endif
 
     params.initial_show_state = show_state;
+
+    // Do not restore workspace if lacros and Desk Profiles are enabled, i.e. it
+    // uses the profile from the current desk, and should always stay within
+    // that desk.
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+    params.initial_workspace =
+        chromeos::features::IsDeskProfilesEnabled() ? std::string() : workspace;
+#else
     params.initial_workspace = workspace;
+#endif
+
     params.initial_visible_on_all_workspaces_state = visible_on_all_workspaces;
     params.creation_source = Browser::CreationSource::kSessionRestore;
     Browser* browser = Browser::Create(params);
