696033a32d8e055617fd652fb90686f246e9fd25
Keith Cirkel <chromium@keithcirkel.co.uk>
2024-02-02 16:16:35 +0000

    Don't retarget nullptr in InvokeEvent::invoker
    This fixes a bug where dispatching an InvokeEvent with a null invoker
    will cause a nullptr exception as it tries to retarget the Invoker based
    on the currentTarget.
    
    The fix returns early nullptr if invoker is a nullptr, which also skips
    the CHECK_EQ on eventPhase(), as just guarding the Retarget call would
    skip to this check which would cause the check to fail if invoker was
    retrieved during the bubbling phase.
    
    Bug: 1511341
    Change-Id: I6e0e250a47466a502daeb8ab32335ff1521edc48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5260418
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Auto-Submit: Keith Cirkel <chromium@keithcirkel.co.uk>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255591}

--- third_party/blink/renderer/core/events/invoke_event.cc
+++ third_party/blink/renderer/core/events/invoke_event.cc
@@ -38,6 +38,10 @@ InvokeEvent::InvokeEvent(const AtomicString& type,
 Element* InvokeEvent::invoker() const {
   auto* current = currentTarget();
   Element* invoker = invoker_.Get();
+  if (!invoker) {
+    return nullptr;
+  }
+
   if (current) {
     return &current->ToNode()->GetTreeScope().Retarget(*invoker);
   }
