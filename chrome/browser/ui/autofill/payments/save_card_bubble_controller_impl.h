11b01285b1355266ba95985ab6dee3365244405d
Yishui Liu <yishuil@google.com>
2024-02-02 15:34:34 +0000

    Show card art image for card save if available
    Fix the issue that card network image was shown when metadata is
    available.
    
    Issue:
    https://drive.google.com/file/d/1bZnHylVrqgD5yciTdnUnpxxWTEQjuEO8/view?resourcekey=0-0viuX9TcXsv6Yv7wa2vW-g
    
    With this change:
    Card without metadata:
    https://screenshot.googleplex.com/6Nh2zq4SYWkE2rH.png
    
    Card with metadata:
    https://screenshot.googleplex.com/55NxfjsakgiQjs9.png
    
    
    Bug: 1522943
    Change-Id: Ifa852c0a33d11ec408dc20c810a763223b308bd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5246491
    Commit-Queue: Yishui Liu <yishuil@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255570}

--- chrome/browser/ui/autofill/payments/save_card_bubble_controller_impl.h
+++ chrome/browser/ui/autofill/payments/save_card_bubble_controller_impl.h
@@ -103,6 +103,7 @@ class SaveCardBubbleControllerImpl
   GetOnBubbleClosedCallback() override;
   bool ShouldRequestNameFromUser() const override;
   bool ShouldRequestExpirationDateFromUser() const override;
+  ui::ImageModel GetCreditCardImage() const override;
 
   void OnSaveButton(const AutofillClient::UserProvidedCardDetails&
                         user_provided_card_details) override;
