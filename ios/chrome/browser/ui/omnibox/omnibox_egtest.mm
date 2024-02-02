5dd4f713f19e81926ebc92db92e7babe1bda2f84
Ali Juma <ajuma@chromium.org>
2024-02-02 18:53:25 +0000

    Revert "[ios] Fix EGTest set up and checks for bottom/top omnibox positions."
    This reverts commit 34304be62b42604313775e535c19dcd42095afc5.
    
    Reason for revert: broke OmniboxTestCase/testCopyInOmniboxTriggersShareButtonIPHWithTopOmnibox
    on iphone-device.
    
    e.g., see https://ci.chromium.org/ui/p/chrome/builders/ci/iphone-device/42078/test-results?sortby=&groupby= 
    
    Original change's description:
    > [ios] Fix EGTest set up and checks for bottom/top omnibox positions.
    >
    > Before the bottom omnibox wasn't set up correctly.
    >
    > Refactor the code so that the top/bottom matchers can be re-used.
    >
    > Change-Id: Id55183264473295ca73786eb227a4f0ae9105330
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018540
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Reviewed-by: Christian Xu <christianxu@chromium.org>
    > Commit-Queue: Huiting Yu <huitingyu@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1255551}
    
    Change-Id: Id253e4b032907425716ac6d96d18029bf854c21f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5262738
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Owners-Override: Ali Juma <ajuma@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1255681}

--- ios/chrome/browser/ui/omnibox/omnibox_egtest.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_egtest.mm
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "build/build_config.h"
 #import "components/feature_engagement/public/feature_constants.h"
 #import "components/strings/grit/components_strings.h"
-#import "ios/chrome/browser/iph_for_new_chrome_user/model/features.h"
-#import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/ui/content_suggestions/ntp_home_constant.h"
 #import "ios/chrome/browser/ui/omnibox/omnibox_app_interface.h"
@@ -561,9 +559,6 @@ void FocusFakebox() {
   if ([ChromeEarlGrey isIPadIdiom]) {
     EARL_GREY_TEST_SKIPPED(@"Skipped for iPad (no bottom omnibox in tablet)");
   }
-
-  [ChromeEarlGrey setBoolValue:YES forUserPref:prefs::kBottomOmnibox];
-
   // Enable the IPH Demo Mode feature to ensure the IPH triggers
   AppLaunchConfiguration config = [self appConfigurationForTestCase];
   config.additional_args.push_back(
@@ -635,10 +630,6 @@ void FocusFakebox() {
   [ChromeEarlGrey verifyStringCopied:base::SysUTF8ToNSString(_URL1.spec())];
 
   DefocusOmnibox();
-
-  // Verify the omnibox is at the top.
-  [ChromeEarlGrey
-      waitForUIElementToAppearWithMatcher:chrome_test_util::OmniboxOnTop()];
 
   // Verify the share button IPH is shown.
   [ChromeEarlGrey
@@ -776,26 +767,22 @@ void FocusFakebox() {
   if ([ChromeEarlGrey isIPadIdiom]) {
     EARL_GREY_TEST_SKIPPED(@"Skipped for iPad (no bottom omnibox in tablet)");
   }
-
-  [ChromeEarlGrey setBoolValue:YES forUserPref:prefs::kBottomOmnibox];
-
   // Enable the IPH Demo Mode feature to ensure the IPH triggers
   AppLaunchConfiguration config = [self appConfigurationForTestCase];
   config.additional_args.push_back(
       base::StringPrintf("--enable-features=%s:chosen_feature/"
-                         "IPH_iOSShareToolbarItemFeature",
-                         feature_engagement::kIPHDemoMode.name) +
-      "," + std::string(kIPHForSafariSwitcher.name));
-
+                         "IPH_iOSShareToolbarItemFeature,IPHForSafariSwitcher",
+                         feature_engagement::kIPHDemoMode.name));
   // Force the conditions that allow the iph to show.
   config.additional_args.push_back("-ForceExperienceForDeviceSwitcher");
   config.additional_args.push_back("SyncedAndFirstDevice");
 
   [[AppLaunchManager sharedManager] ensureAppLaunchedWithConfiguration:config];
 
+  GREYAssertTrue([ChromeEarlGrey isBottomOmniboxSteadyStateEnabled],
+                 @"Test case should have ran in bottom omnibox environment");
+
   [self openPage1];
-  [ChromeEarlGrey
-      waitForUIElementToAppearWithMatcher:chrome_test_util::OmniboxAtBottom()];
 
   // Long pressing should allow copying.
   [[EarlGrey selectElementWithMatcher:chrome_test_util::DefocusedLocationView()]
@@ -848,10 +835,6 @@ void FocusFakebox() {
   [[EarlGrey selectElementWithMatcher:CopyContextMenuButton()]
       performAction:grey_tap()];
   [ChromeEarlGrey verifyStringCopied:base::SysUTF8ToNSString(_URL1.spec())];
-
-  // Verify the omnibox is at the top.
-  [ChromeEarlGrey
-      waitForUIElementToAppearWithMatcher:chrome_test_util::OmniboxOnTop()];
 
   // Verify the share button IPH is shown.
   GREYAssert([ChromeEarlGrey testUIElementAppearanceWithMatcher:
@@ -1061,7 +1044,6 @@ void FocusFakebox() {
 }
 
 - (void)tearDown {
-  [ChromeEarlGrey setBoolValue:NO forUserPref:prefs::kBottomOmnibox];
   // Clear the pasteboard after every test.
   [ChromeEarlGrey clearPasteboard];
   [super tearDown];
--- ios/chrome/browser/ui/settings/address_bar_preference/address_bar_preference_egtest.mm
+++ ios/chrome/browser/ui/settings/address_bar_preference/address_bar_preference_egtest.mm
@@ -49,6 +49,19 @@ id<GREYMatcher> BottomAddressBarOptionSelected() {
   return grey_allOf(grey_selected(), BottomAddressBarOption(), nil);
 }
 
+id<GREYMatcher> BottomOmnibox() {
+  return grey_allOf(
+      chrome_test_util::DefocusedLocationView(),
+      grey_ancestor(grey_kindOfClassName(@"SecondaryToolbarView")),
+      grey_sufficientlyVisible(), nil);
+}
+
+id<GREYMatcher> TopOmnibox() {
+  return grey_allOf(chrome_test_util::DefocusedLocationView(),
+                    grey_ancestor(grey_kindOfClassName(@"PrimaryToolbarView")),
+                    grey_sufficientlyVisible(), nil);
+}
+
 }  // namespace
 
 @interface AddressBarPreferenceTestCase : ChromeTestCase
@@ -78,7 +91,7 @@ id<GREYMatcher> BottomAddressBarOptionSelected() {
 
   [ChromeEarlGrey loadURL:GURL("about:blank")];
   // The address bar should be on top.
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::OmniboxOnTop()]
+  [[EarlGrey selectElementWithMatcher:TopOmnibox()]
       assertWithMatcher:grey_notNil()];
 
   [self openAddressBarPreferenceSettingPage];
@@ -99,9 +112,9 @@ id<GREYMatcher> BottomAddressBarOptionSelected() {
       performAction:grey_tap()];
 
   // The address bar should be now on bottom.
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::OmniboxAtBottom()]
+  [[EarlGrey selectElementWithMatcher:BottomOmnibox()]
       assertWithMatcher:grey_notNil()];
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::OmniboxOnTop()]
+  [[EarlGrey selectElementWithMatcher:TopOmnibox()]
       assertWithMatcher:grey_nil()];
 }
 
