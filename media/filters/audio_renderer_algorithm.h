83b861eff5ace868a9a00995de6c660de6238962
Thomas Guilbert <tguilbert@chromium.org>
2024-02-02 00:44:24 +0000

    Minimize audio pops when resampling
    This CL removes audio pops when going back and forth between a playback
    rate of ~1.0 and other playback rates, when preservesPitch=false.
    
    When we repeatedly start and stop resampling, a small amount of residual
    data is lost. This causes a discontinuity in the output signal, and thus
    a small pop.
    
    To fix this, always resample when preservesPitch=false. This comes at a
    small performance/memory cost, and might be revisited if it causes any
    measurable regressions.
    
    Note: when `preservesPitch=true` (the default case), we use a WSOLA
    algorithm instead, and there are known audio pops when transitioning to
    and from playback rates of ~1.0. Smoothing those pops is significantly
    more complex, and has not been attempted yet.
    
    Bug: 1492404
    Change-Id: Ibcd189d4c16d4d17cd4fb4f02506f00abf9ae215
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5251995
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255378}

--- media/filters/audio_renderer_algorithm.h
+++ media/filters/audio_renderer_algorithm.h
@@ -43,6 +43,12 @@ class AudioBus;
 
 class MEDIA_EXPORT AudioRendererAlgorithm {
  public:
+  enum class FillBufferMode {
+    kPassthrough,
+    kResampler,
+    kWSOLA,
+  };
+
   AudioRendererAlgorithm(MediaLog* media_log);
   AudioRendererAlgorithm(MediaLog* media_log,
                          AudioRendererAlgorithmParameters params);
@@ -140,12 +146,10 @@ class MEDIA_EXPORT AudioRendererAlgorithm {
 
   std::vector<bool> channel_mask_for_testing() { return channel_mask_; }
 
+  FillBufferMode last_mode_for_testing() { return last_mode_; }
+
  private:
-  enum class FillBufferMode {
-    kPassthrough,
-    kResampler,
-    kWSOLA,
-  };
+  FillBufferMode ChooseBufferMode(double playback_rate);
 
   // Remove buffered data that will be outdated if we switch fill mode.
   void SetFillBufferMode(FillBufferMode mode);
@@ -198,6 +202,12 @@ class MEDIA_EXPORT AudioRendererAlgorithm {
   // Uses |resampler_| to speed up or slowdown audio, by using a resampling
   // ratio of |playback_rate|.
   int ResampleAndFill(AudioBus* dest,
+                      int dest_offset,
+                      int requested_frames,
+                      double playback_rate);
+
+  // Uses the WSOLA algorithm to speed up or slowdown audio.
+  int RunWsolaAndFill(AudioBus* dest,
                       int dest_offset,
                       int requested_frames,
                       double playback_rate);
