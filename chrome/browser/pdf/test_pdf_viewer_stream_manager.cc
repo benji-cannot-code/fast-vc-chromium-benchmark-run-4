43ba87c281fd75fdf9c23d3a9b2dd57b3ef37d85
Andy Phan <andyphan@chromium.org>
2024-02-03 01:32:46 +0000

    [OOPIF PDF] Replace test PDF manager's DeprecatedWaitUntilPdfLoaded()
    Remove DeprecatedWaitUntilPdfLoaded(). Replace it with
    WaitUntilPdfLoaded() which takes an embedder host. The new version is
    still TODO, but it will wait for the PDF to fully load content in all
    PDF frames, rather than just waiting for the PDF frames to navigate.
    This is needed for more sufficient test support.
    
    WaitUntilPdfLoaded() skips waiting if the PDF has already navigated.
    
    Also modify SitePerProcessInteractivePDFTest::WaitUntilPdfLoaded() to
    take an embedder host, which simplifies the current GuestView codepath
    and the new OOPIF codepath.
    
    Bug: 1445746
    Change-Id: I6f0482c96ac333b49bc5a970865935b858e7cfed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5226161
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255867}

--- chrome/browser/pdf/test_pdf_viewer_stream_manager.cc
+++ chrome/browser/pdf/test_pdf_viewer_stream_manager.cc
@@ -7,11 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/check.h"
 #include "base/run_loop.h"
 #include "chrome/browser/pdf/pdf_viewer_stream_manager.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_user_data.h"
+#include "content/public/test/browser_test_utils.h"
 
 namespace pdf {
 
@@ -35,6 +37,10 @@ void TestPdfViewerStreamManager::DidFinishNavigation(
     content::NavigationHandle* navigation_handle) {
   PdfViewerStreamManager::DidFinishNavigation(navigation_handle);
 
+  if (!on_pdf_loaded_) {
+    return;
+  }
+
   // Check if the PDF has finished loading after the final PDF navigation. A
   // complete PDF navigation should have a claimed `StreamInfo`.
   auto* claimed_stream_info =
@@ -46,10 +52,26 @@ void TestPdfViewerStreamManager::DidFinishNavigation(
   std::move(on_pdf_loaded_).Run();
 }
 
-void TestPdfViewerStreamManager::DeprecatedWaitUntilPdfLoaded() {
-  base::RunLoop run_loop;
-  on_pdf_loaded_ = run_loop.QuitClosure();
-  run_loop.Run();
+void TestPdfViewerStreamManager::WaitUntilPdfLoaded(
+    content::RenderFrameHost* embedder_host) {
+  // If all of the PDF frames haven't navigated, wait.
+  auto* claimed_stream_info = GetClaimedStreamInfo(embedder_host);
+  if (!claimed_stream_info || !claimed_stream_info->DidPdfContentNavigate()) {
+    base::RunLoop run_loop;
+    on_pdf_loaded_ = run_loop.QuitClosure();
+    run_loop.Run();
+  }
+
+  // TODO(crbug.com/1445746): Currently, this only waits until all of the PDF
+  // frames finished navigating. Wait until the PDF extension and content has
+  // finished loading, too.
+}
+
+void TestPdfViewerStreamManager::WaitUntilPdfLoadedInFirstChild() {
+  content::RenderFrameHost* embedder_host =
+      ChildFrameAt(web_contents()->GetPrimaryMainFrame(), 0);
+  CHECK(embedder_host);
+  WaitUntilPdfLoaded(embedder_host);
 }
 
 }  // namespace pdf
--- chrome/browser/pdf/test_pdf_viewer_stream_manager.h
+++ chrome/browser/pdf/test_pdf_viewer_stream_manager.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 class NavigationHandle;
+class RenderFrameHost;
 class WebContents;
 }  // namespace content
 
@@ -34,9 +35,14 @@ class TestPdfViewerStreamManager : public PdfViewerStreamManager {
   void DidFinishNavigation(
       content::NavigationHandle* navigation_handle) override;
 
-  // Deprecated. Do not add any additional uses. Wait until the PDF has finished
-  // loading.
-  void DeprecatedWaitUntilPdfLoaded();
+  // Wait until the PDF has finished loading. `embedder_host` must be a PDF
+  // embedder host, otherwise this will hang the test.
+  void WaitUntilPdfLoaded(content::RenderFrameHost* embedder_host);
+
+  // Same as `WaitUntilPdfLoaded()`, but the first child of the primary main
+  // frame should be the embedder. This is a common case where an HTML page only
+  // embeds a single PDF.
+  void WaitUntilPdfLoadedInFirstChild();
 
  private:
   base::OnceClosure on_pdf_loaded_;
