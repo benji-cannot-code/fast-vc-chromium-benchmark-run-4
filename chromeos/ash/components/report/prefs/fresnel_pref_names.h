bb6869b78da210b18d0cf63df1355cbda41dcea3
Hirthanan Subenderan <hirthanan@google.com>
2024-02-04 19:45:44 +0000

    Persist local state if new device signals in prev observation pings.
    Introduce a new local state field to indicate whether
    |first_active_week| and |last_powerwash_week| was attached in any
    observation ping for this device before.
    
    This field is persisted over safe powerwash in the preserved files,
    and retrieved on browser startup.
    
    BUG=chromium:1515011
    
    Change-Id: If8abce9a2fc9b2decf19d015fb7ab7145814c202
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5252843
    Commit-Queue: Hirthanan Subenderan <hirthanan@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256004}

--- chromeos/ash/components/report/prefs/fresnel_pref_names.h
+++ chromeos/ash/components/report/prefs/fresnel_pref_names.h
@@ -23,6 +23,8 @@ extern const char kDeviceActiveChurnCohortMonthlyPingTimestamp[];
 COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_REPORT)
 extern const char kDeviceActiveChurnObservationMonthlyPingTimestamp[];
 COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_REPORT)
+extern const char kDeviceActiveChurnObservationFirstObservedNewChurnMetadata[];
+COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_REPORT)
 extern const char kDeviceActiveLastKnownChurnActiveStatus[];
 COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_REPORT)
 extern const char kDeviceActiveLastKnownIsActiveCurrentPeriodMinus0[];
