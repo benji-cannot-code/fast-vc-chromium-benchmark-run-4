2b6d8d5f14dce323cde2b574762a1ca28748febe
Christina Bannon <chbannon@google.com>
2024-02-05 19:44:26 +0000

    [VCN] (1/2) VCN Start Auth Flow: Implement ChromeAutofillClientIOS::GetRiskBasedAuthenticator()
    Added this method for iOS to use risk based authentication for virtual cards. Follow up CL: Starting the auth flow and showing the progress dialog.
    
    Change-Id: I7bfba3bfc83196ea866e5d59b1ab27e5f0401315
    Bug: b/323506332
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5261859
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Christina Bannon <chbannon@google.com>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256338}

--- ios/chrome/browser/ui/autofill/chrome_autofill_client_ios.mm
+++ ios/chrome/browser/ui/autofill/chrome_autofill_client_ios.mm
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/autofill/core/browser/payments/autofill_save_card_ui_info.h"
 #import "components/autofill/core/browser/payments/credit_card_cvc_authenticator.h"
 #import "components/autofill/core/browser/payments/credit_card_otp_authenticator.h"
+#import "components/autofill/core/browser/payments/credit_card_risk_based_authenticator.h"
 #import "components/autofill/core/browser/payments/payments_network_interface.h"
 #import "components/autofill/core/browser/payments/virtual_card_enroll_metrics_logger.h"
 #import "components/autofill/core/browser/payments/virtual_card_enrollment_manager.h"
@@ -175,6 +176,15 @@ CreditCardOtpAuthenticator* ChromeAutofillClientIOS::GetOtpAuthenticator() {
   if (!otp_authenticator_)
     otp_authenticator_ = std::make_unique<CreditCardOtpAuthenticator>(this);
   return otp_authenticator_.get();
+}
+
+CreditCardRiskBasedAuthenticator*
+ChromeAutofillClientIOS::GetRiskBasedAuthenticator() {
+  if (!risk_based_authenticator_) {
+    risk_based_authenticator_ =
+        std::make_unique<CreditCardRiskBasedAuthenticator>(this);
+  }
+  return risk_based_authenticator_.get();
 }
 
 PrefService* ChromeAutofillClientIOS::GetPrefs() {
