e7e49069d04e4e4c98398a73d8dab1241a84a1ce
Michelle <michellegc@google.com>
2024-02-05 00:04:51 +0000

    Add icons for Picker emoji, symbol, emoticon and gif categories.
    Add vector icons for the current list of Picker categories. (More
    planned to be added later.) These are shown next to the category name
    in the Picker zero state view.
    
    Bug: b:322905189
    Change-Id: Ifde4124e0b30e2eda7f19f3a7721cfa9268fb489
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5252569
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256021}

---
+++ ash/picker/views/picker_icons.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_PICKER_VIEWS_PICKER_ICONS_H_
+#define ASH_PICKER_VIEWS_PICKER_ICONS_H_
+
+#include "ash/ash_export.h"
+#include "ash/picker/model/picker_category.h"
+#include "ash/resources/vector_icons/vector_icons.h"
+
+namespace ui {
+class ImageModel;
+}
+
+namespace ash {
+
+ui::ImageModel GetIconForPickerCategory(PickerCategory category);
+
+}  // namespace ash
+
+#endif  // ASH_PICKER_VIEWS_PICKER_ICONS_H_
--- ash/picker/views/picker_zero_state_view.cc
+++ ash/picker/views/picker_zero_state_view.cc
@@ -12,13 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/picker/model/picker_category.h"
 #include "ash/picker/model/picker_model.h"
+#include "ash/picker/views/picker_icons.h"
 #include "ash/picker/views/picker_item_view.h"
 #include "ash/picker/views/picker_section_view.h"
-#include "ash/resources/vector_icons/vector_icons.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
-#include "ui/base/models/image_model.h"
 #include "ui/chromeos/styles/cros_tokens_color_mappings.h"
 #include "ui/views/layout/flex_layout.h"
 #include "ui/views/layout/layout_manager.h"
@@ -30,9 +29,6 @@ namespace {
 
 const std::u16string kPlaceholderCategorySectionTitle =
     u"Placeholder Categories";
-
-// TODO: b/322905189 - Get a relevant icon for each category.
-const gfx::VectorIcon& kPlaceholderIcon = kImeMenuEmoticonIcon;
 
 }  // namespace
 
@@ -48,8 +44,7 @@ PickerZeroStateView::PickerZeroStateView(
     auto item_view = std::make_unique<PickerItemView>(
         base::BindRepeating(select_category_callback, category));
     item_view->SetPrimaryText(GetStringForPickerCategory(category));
-    item_view->SetLeadingIcon(ui::ImageModel::FromVectorIcon(
-        kPlaceholderIcon, cros_tokens::kCrosSysOnSurface));
+    item_view->SetLeadingIcon(GetIconForPickerCategory(category));
     section_view->AddListItem(std::move(item_view));
   }
   section_views_.push_back(section_view);
