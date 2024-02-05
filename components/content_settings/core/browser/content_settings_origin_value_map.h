82a18480c53a23f2e489884d652146cd1ff31779
Nick Yamane <nickdiego@igalia.com>
2024-02-05 11:56:22 +0000

    IWYU: add missing include to use std::variant
    Without it, build fails in gcc/libstdc++ configs.
    
    Bug: 819294
    Change-Id: Iadfa110f9e2881aa109202123945a9bbf6854274
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5263709
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Auto-Submit: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256130}

--- components/content_settings/core/browser/content_settings_origin_value_map.h
+++ components/content_settings/core/browser/content_settings_origin_value_map.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <memory>
+#include <variant>
 
 #include "base/synchronization/lock.h"
 #include "base/thread_annotations.h"
