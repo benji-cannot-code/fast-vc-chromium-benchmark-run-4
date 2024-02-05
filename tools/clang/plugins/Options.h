ceb3c50870f7ead0cdf424986314a4997b59a10c
danakj <danakj@chromium.org>
2024-02-05 21:33:59 +0000

    Allow `auto` to refer to a type alias even if there's a pointer inside
    stdlib iterators are type aliases which may be a pointer, or may be a
    record type. This can change based on the build configuration and is an
    implementation detail of the library.
    
    Any code that works with string_view iterators right now must write
    `auto*` which is not portable and prevents us from turning on bounded
    iterators.
    
    This loosens the `auto*` rule to allow writing `auto` to refer to a
    deduced type which is an alias, even if the alias itself is of a
    pointer type.
    
    R=dcheng@chromium.org
    
    Bug: 1519908
    Change-Id: I8d34ac54bfb3aee31dbd8084ec3bf6764d5608b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5255277
    Reviewed-by: Arthur Eubanks <aeubanks@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Arthur Eubanks <aeubanks@google.com>
    Auto-Submit: danakj <danakj@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256418}

--- tools/clang/plugins/Options.h
+++ tools/clang/plugins/Options.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chrome_checker {
 
 struct Options {
+  bool allow_auto_typedefs = false;
   bool check_base_classes = false;
   bool check_blink_data_member_type = false;
   bool check_ipc = false;
