d70f7515607367259d2e4b19e655718f11d3c11c
Christopher Cameron <ccameron@chromium.org>
2024-02-05 13:14:27 +0000

    Add CSS gamut mapping to chrome://flags
    Bug: 1508329
    Change-Id: I5f3f477e42c2b6032b28b2b7a2801b35477b09ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5255901
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256163}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -143,6 +143,9 @@ extern const char kCrOSLegacyMediaFormatsName[];
 extern const char kCrOSLegacyMediaFormatsDescription[];
 #endif
 
+extern const char kCssGamutMappingName[];
+extern const char kCssGamutMappingDescription[];
+
 extern const char kCustomizeChromeColorExtractionName[];
 extern const char kCustomizeChromeColorExtractionDescription[];
 
