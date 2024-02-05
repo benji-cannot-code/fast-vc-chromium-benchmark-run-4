6093202bde15f6f4d2e172a1fddeac5d7de9f940
Wei-Luan Wang <weiluanwang@google.com>
2024-02-05 06:26:01 +0000

    cros_healthd: Deprecate SetAudioOutputMute method
    The method is no longer needed.
    
    This revert the changes in CL:4286021.
    
    Bug: b:320845888
    Test: unit_tests -ozone-platform=headless
    Change-Id: I60c3ba7a7ec657301efeb2c03a60c7cee4a62889
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5256566
    Reviewed-by: Kerker Yang <kerker@chromium.org>
    Commit-Queue: Wei-Luan Wang <weiluanwang@google.com>
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256066}

--- chromeos/ash/services/cros_healthd/private/cpp/data_collector.cc
+++ chromeos/ash/services/cros_healthd/private/cpp/data_collector.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/posix/eintr_wrapper.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/thread_pool.h"
-#include "chromeos/ash/components/audio/cras_audio_handler.h"
 #include "chromeos/ash/components/mojo_service_manager/connection.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
@@ -43,8 +42,6 @@ class DataCollectorDelegateImpl : public DataCollector::Delegate {
   bool IsPrivacyScreenSupported() override;
   bool IsPrivacyScreenManaged() override;
   void SetPrivacyScreenState(bool state) override;
-  bool IsOutputForceMuted() override;
-  void SetOutputMute(bool mute_on) override;
 };
 
 DataCollectorDelegateImpl::DataCollectorDelegateImpl() = default;
@@ -97,14 +94,6 @@ bool DataCollectorDelegateImpl::IsPrivacyScreenManaged() {
 
 void DataCollectorDelegateImpl::SetPrivacyScreenState(bool state) {
   Shell::Get()->privacy_screen_controller()->SetEnabled(state);
-}
-
-bool DataCollectorDelegateImpl::IsOutputForceMuted() {
-  return CrasAudioHandler::Get()->IsOutputForceMuted();
-}
-
-void DataCollectorDelegateImpl::SetOutputMute(bool mute_on) {
-  CrasAudioHandler::Get()->SetOutputMute(mute_on);
 }
 
 DataCollectorDelegateImpl* GetDataCollectorDelegate() {
@@ -204,15 +193,10 @@ void DataCollector::SetPrivacyScreenState(
   std::move(callback).Run(true);
 }
 
-void DataCollector::SetAudioOutputMute(bool mute_on,
-                                       SetAudioOutputMuteCallback callback) {
-  if (!mute_on && delegate_->IsOutputForceMuted()) {
-    std::move(callback).Run(false);
-    return;
-  }
-
-  delegate_->SetOutputMute(mute_on);
-  std::move(callback).Run(true);
+void DataCollector::DEPRECATED_SetAudioOutputMute(
+    bool mute_on,
+    DEPRECATED_SetAudioOutputMuteCallback callback) {
+  std::move(callback).Run(/*success*/ false);
 }
 
 void DataCollector::Request(
