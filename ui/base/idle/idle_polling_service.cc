83a64edea8c3749dd03e0f22217924ead04341f9
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2024-02-05 18:29:13 +0000

    Increase IdlePollingService interval to 15s
    Polling every 1s is consuming more battery than necessary, and we don't
    need that level of granularity. 15s should be unnoticeable for most
    users, and reduces power consumption.
    
    Bug: b/323204044
    Change-Id: I2fe9661493a8f6fdb5e774cbe7c9702e0a5aea71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5258718
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256286}

--- ui/base/idle/idle_polling_service.cc
+++ ui/base/idle/idle_polling_service.cc
@@ -15,8 +15,6 @@ namespace ui {
 
 namespace {
 
-constexpr base::TimeDelta kPollInterval = base::Seconds(1);
-
 // Default provider implementation. Everything is delegated to
 // ui::CalculateIdleTime and ui::CheckIdleStateIsLocked.
 class DefaultIdleProvider : public IdleTimeProvider {
@@ -52,7 +50,9 @@ void IdlePollingService::AddObserver(Observer* observer) {
   if (observers_.empty()) {
     DCHECK(!timer_.IsRunning());
     PollIdleState();
-    timer_.Reset();
+    timer_.Start(FROM_HERE, poll_interval_,
+                 base::BindRepeating(&IdlePollingService::PollIdleState,
+                                     base::Unretained(this)));
   }
 
   observers_.AddObserver(observer);
@@ -76,6 +76,11 @@ void IdlePollingService::SetProviderForTest(
   }
 }
 
+void IdlePollingService::SetPollIntervalForTest(base::TimeDelta poll_interval) {
+  DCHECK(!timer_.IsRunning());
+  poll_interval_ = poll_interval;
+}
+
 bool IdlePollingService::IsPollingForTest() {
   return timer_.IsRunning();
 }
@@ -86,10 +91,7 @@ void IdlePollingService::SetTaskRunnerForTest(
 }
 
 IdlePollingService::IdlePollingService()
-    : timer_(FROM_HERE,
-             kPollInterval,
-             base::BindRepeating(&IdlePollingService::PollIdleState,
-                                 base::Unretained(this))),
+    : poll_interval_(kPollInterval),
       provider_(std::make_unique<DefaultIdleProvider>()) {
   DCHECK(!timer_.IsRunning());
 }
