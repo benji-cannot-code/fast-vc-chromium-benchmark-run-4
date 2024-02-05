06be7f6b74ea5a7c6b2e0b0d5cde011ccfd81994
Steinar H. Gunderson <sesse@chromium.org>
2024-02-05 14:01:39 +0000

    Disable PDFOCRIntegration tests on UBSan.
    This was already disabled on ASan (for different reasons?),
    but times out flakily on UBSan bots, so disable them.
    
    Bug: 323792320
    Change-Id: If64be004a58e3221057f7650c24ead4de5ad3fb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5268642
    Auto-Submit: Steinar H Gunderson <sesse@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Owners-Override: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256177}

--- chrome/browser/pdf/pdf_extension_accessibility_test.cc
+++ chrome/browser/pdf/pdf_extension_accessibility_test.cc
@@ -70,7 +70,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // TODO(crbug.com/1516559): Add a dummy library that is built with Chrome for
 // memory sanitizer tests.
-#if (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC)) && !defined(MEMORY_SANITIZER)
+// TODO(crbug.com/323792320): Times out flakily on UBSan bots.
+#if (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC)) && \
+    !defined(MEMORY_SANITIZER) && !defined(UNDEFINED_SANITIZER)
 #define PDF_OCR_INTEGRATION_TEST_ENABLED
 #endif
 
