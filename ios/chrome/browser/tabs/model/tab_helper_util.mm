52035c0b6bd07a5aff837c469329e1ccf0e2e01f
Robbie Gibson <rkgibson@google.com>
2024-02-05 20:36:20 +0000

    [iOS] Add framework for ContextualPanelTabHelper
    This is the skeleton for this class, having it observe the WebState
    it's attached to.
    
    Bug: 1523781
    Change-Id: Ice7dca2093fcbe7b4cf877a860a36fb0a6903b7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5254898
    Auto-Submit: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1256384}

--- ios/chrome/browser/tabs/model/tab_helper_util.mm
+++ ios/chrome/browser/tabs/model/tab_helper_util.mm
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/commerce/model/shopping_persisted_data_tab_helper.h"
 #import "ios/chrome/browser/commerce/model/shopping_service_factory.h"
 #import "ios/chrome/browser/complex_tasks/model/ios_task_tab_helper.h"
+#import "ios/chrome/browser/contextual_panel/model/contextual_panel_tab_helper.h"
 #import "ios/chrome/browser/crash_report/model/breadcrumbs/breadcrumb_manager_tab_helper.h"
 #import "ios/chrome/browser/download/model/ar_quick_look_tab_helper.h"
 #import "ios/chrome/browser/download/model/document_download_tab_helper.h"
@@ -311,5 +312,9 @@ void AttachTabHelpers(web::WebState* web_state, bool for_prerender) {
 
   if (!is_off_the_record) {
     PriceNotificationsTabHelper::CreateForWebState(web_state);
+  }
+
+  if (IsContextualPanelEnabled()) {
+    ContextualPanelTabHelper::CreateForWebState(web_state);
   }
 }
