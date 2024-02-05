5fdbf33d76de1e7e6d6586ec4cfb27979597e247
Henrique Nakashima <hnakashima@chromium.org>
2024-02-05 19:53:22 +0000

    [Android] Avoid String concatenation on CachedFlag isEnabled() check
    Bug: 40267731
    Change-Id: I302b092b63be170247ca5f77a9f6717fe328d639
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5269923
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256354}

--- base/android/java/src/org/chromium/base/cached_flags/CachedFlag.java
+++ base/android/java/src/org/chromium/base/cached_flags/CachedFlag.java
@@ -41,6 +41,7 @@ import java.util.Map;
  */
 public class CachedFlag extends Flag {
     private final boolean mDefaultValue;
+    private String mPreferenceKey;
 
     public CachedFlag(FeatureMap featureMap, String featureName, boolean defaultValue) {
         super(featureMap, featureName);
@@ -126,7 +127,11 @@ public class CachedFlag extends Flag {
     }
 
     String getSharedPreferenceKey() {
-        return CachedFlagsSharedPreferences.FLAGS_CACHED.createKey(mFeatureName);
+        // Create the key only once to avoid String concatenation every flag check.
+        if (mPreferenceKey == null) {
+            mPreferenceKey = CachedFlagsSharedPreferences.FLAGS_CACHED.createKey(mFeatureName);
+        }
+        return mPreferenceKey;
     }
 
     /**
