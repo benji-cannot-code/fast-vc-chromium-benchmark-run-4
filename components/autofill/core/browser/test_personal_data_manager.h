5884dc7efe17552ca9dddf72ad99247335824f24
Florian Leimgruber <fleimgruber@google.com>
2024-02-05 15:55:32 +0000

    Remove PDM::CountryCodeForCurrentTimezone()
    This function only calls some function in base. It is overridden in the
    TestPDM, so its return value can be changed. But this is only used by a
    single test, that doesn't even depend on it.
    
    Bug: b/322170538, 1007974
    Change-Id: Ifb08a30b4c070392e50a376e91bf999b9115007a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5268922
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1256233}

--- components/autofill/core/browser/test_personal_data_manager.h
+++ components/autofill/core/browser/test_personal_data_manager.h
@@ -67,7 +67,6 @@ class TestPersonalDataManager : public PersonalDataManager {
   bool IsAutofillPaymentMethodsEnabled() const override;
   bool IsAutofillWalletImportEnabled() const override;
   bool ShouldSuggestServerPaymentMethods() const override;
-  std::string CountryCodeForCurrentTimezone() const override;
   void ClearAllLocalData() override;
   bool IsDataLoaded() const override;
   bool IsSyncFeatureEnabledForPaymentsServerMetrics() const override;
@@ -124,9 +123,6 @@ class TestPersonalDataManager : public PersonalDataManager {
   void SetNicknameForCardWithGUID(std::string_view guid,
                                   std::string_view nickname);
 
-  void set_timezone_country_code(const std::string& timezone_country_code) {
-    timezone_country_code_ = timezone_country_code;
-  }
   void set_default_country_code(const std::string& default_country_code) {
     default_country_code_ = default_country_code;
   }
@@ -175,7 +171,6 @@ class TestPersonalDataManager : public PersonalDataManager {
   // and not trigger `NotifyPersonalDataObserver()`.
   void RemoveByGuidWithoutNotifications(const std::string& guid);
 
-  std::string timezone_country_code_;
   std::string default_country_code_;
   int num_times_save_imported_credit_card_called_ = 0;
   std::optional<bool> autofill_profile_enabled_;
