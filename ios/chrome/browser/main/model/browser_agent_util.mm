a8850706fa2e7cbbd81a240e103964fb70b6ee27
Nicolas MacBeth <nicolasmacbeth@google.com>
2024-02-05 23:01:55 +0000

    [iOS CP] Added contextual panel browser_agent boilerplate
    The browser agent observes the WebStateList, and upon changes updates
    entrypoint accordingly. It will also keep track of some browser-wide
    state such as which tabs have the panel open on iPad.
    
    Bug: b:316370495
    Change-Id: If4ee49318244677db9fe49c88c4d0e82dde50a19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5258883
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1256467}

--- ios/chrome/browser/main/model/browser_agent_util.mm
+++ ios/chrome/browser/main/model/browser_agent_util.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/feature_list.h"
 #import "components/breadcrumbs/core/breadcrumbs_status.h"
 #import "ios/chrome/browser/app_launcher/model/app_launcher_browser_agent.h"
+#import "ios/chrome/browser/contextual_panel/model/contextual_panel_browser_agent.h"
 #import "ios/chrome/browser/crash_report/model/breadcrumbs/breadcrumb_manager_browser_agent.h"
 #import "ios/chrome/browser/device_sharing/model/device_sharing_browser_agent.h"
 #import "ios/chrome/browser/favicon/model/favicon_browser_agent.h"
@@ -136,6 +137,10 @@ void AttachBrowserAgents(Browser* browser) {
 
   if (!browser_is_inactive) {
     TabBasedIPHBrowserAgent::CreateForBrowser(browser);
+  }
+
+  if (IsContextualPanelEnabled()) {
+    ContextualPanelBrowserAgent::CreateForBrowser(browser);
   }
 
   // This needs to be called last in case any downstream browser agents need to
