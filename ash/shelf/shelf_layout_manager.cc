821c3e326d406267f50fb08fa1b980ff2a8323b5
Yongshun Liu <yongshun@chromium.org>
2024-02-05 21:40:28 +0000

    bento_button: Make desk button animate consistently for gestures
    This makes the desk button animate consistently for gestures, e.g. when
    drag to hide the shelf.
    
    Bug: b/312723946
    Test: Manual
    Change-Id: I7de6730d9d30b46367344fdb63ac7cee4c08675e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5263586
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256424}

--- ash/shelf/shelf_layout_manager.cc
+++ ash/shelf/shelf_layout_manager.cc
@@ -2064,6 +2064,10 @@ void ShelfLayoutManager::UpdateTargetBoundsForGesture(
         adjusted_shelf_position);
     shelf_->hotseat_widget()->UpdateTargetBoundsForGesture(
         adjusted_shelf_position);
+    if (features::IsDeskButtonEnabled()) {
+      shelf_->desk_button_widget()->UpdateTargetBoundsForGesture(
+          adjusted_shelf_position);
+    }
     shelf_->navigation_widget()->UpdateTargetBoundsForGesture(
         adjusted_shelf_position);
     shelf_->status_area_widget()->UpdateTargetBoundsForGesture(
