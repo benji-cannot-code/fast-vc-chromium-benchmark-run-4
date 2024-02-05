c7311e8b0b7d655cd66ab4e524924234667675e4
Wenyu Fu <wenyufu@google.com>
2024-02-05 23:11:55 +0000

    [IncognitoLFF] Add long press menu for tab switcher
    Keep the tab switcher menu on par with phone's tab switcher button.
    Demo: https://hsv.googleplex.com/6222322189991936
    
    Bug: b/323925524
    Change-Id: I373d4b794f4fc1883bd3180a9148b77182011a0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5270029
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256470}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -263,6 +263,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kTabStripGroupIndicatorsAndroid,
     &kTabEngagementReportingAndroid,
     &kTabGroupParityAndroid,
+    &kTabletTabSwitcherLongPressMenu,
     &kTabletToolbarIncognitoStatus,
     &kTabletToolbarReordering,
     &kTabResumptionModuleAndroid,
@@ -808,6 +809,10 @@ BASE_FEATURE(kTabStripGroupIndicatorsAndroid,
 BASE_FEATURE(kTabStateFlatBuffer,
              "TabStateFlatBuffer",
              base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kTabletTabSwitcherLongPressMenu,
+             "TabletTabSwitcherLongPressMenu",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kTabletToolbarIncognitoStatus,
              "TabletToolbarIncognitoStatus",
