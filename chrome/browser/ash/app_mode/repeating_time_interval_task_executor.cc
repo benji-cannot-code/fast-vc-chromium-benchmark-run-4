02d88491dda68a820e2d0076fd0abe7bc942b486
Fahad Mansoor <fahadmansoor@google.com>
2024-02-05 13:00:40 +0000

    autosleep: Add empty RepeatingTimeIntervalTaskExecutor class
    This class will be used to run the provided callbacks whenever the
    current time falls in the interval.
    
    Bug: b:319081765
    Change-Id: Id4cf2341ce27fe1d7e865b3fec13b8712223d602
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5205710
    Reviewed-by: Irina Fedorova <irfedorova@google.com>
    Commit-Queue: Fahad Mansoor <fahadmansoor@google.com>
    Reviewed-by: Emmanuel Arias Soto <eariassoto@google.com>
    Cr-Commit-Position: refs/heads/main@{#1256160}

---
+++ chrome/browser/ash/app_mode/repeating_time_interval_task_executor.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/app_mode/repeating_time_interval_task_executor.h"
+
+namespace ash {
+
+RepeatingTimeIntervalTaskExecutor::RepeatingTimeIntervalTaskExecutor() =
+    default;
+
+RepeatingTimeIntervalTaskExecutor::~RepeatingTimeIntervalTaskExecutor() =
+    default;
+
+}  // namespace ash
