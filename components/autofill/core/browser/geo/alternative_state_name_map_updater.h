2a88a9bf3ee5631ba58eb07c213892bd10cea433
Florian Leimgruber <fleimgruber@google.com>
2024-02-05 12:30:38 +0000

    Use ScopedObservation in AlternativeStateNameMapUpdator
    The AlternativeStateNameMapUpdator is owned by the PDM, who registers
    it as an observer right after creation. Making the AlternativeStateName-
    MapUpdater a PDM observer should be the class's own responsibility.
    
    Bug: b/322170538, 1007974
    Change-Id: I19b44dd68a0b1b23c0595fb1159eee2045e433e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5268581
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Auto-Submit: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1256150}

--- components/autofill/core/browser/geo/alternative_state_name_map_updater.h
+++ components/autofill/core/browser/geo/alternative_state_name_map_updater.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/scoped_observation.h"
 #include "base/sequence_checker.h"
 #include "base/task/sequenced_task_runner.h"
 #include "components/autofill/core/browser/geo/alternative_state_name_map.h"
@@ -158,6 +159,9 @@ class AlternativeStateNameMapUpdater : public PersonalDataManagerObserver {
       parsed_state_values_;
 
   SEQUENCE_CHECKER(sequence_checker_);
+
+  base::ScopedObservation<PersonalDataManager, PersonalDataManagerObserver>
+      pdm_observer_{this};
 
   // base::WeakPtr ensures that the callback bound to the object is canceled
   // when that object is destroyed.
