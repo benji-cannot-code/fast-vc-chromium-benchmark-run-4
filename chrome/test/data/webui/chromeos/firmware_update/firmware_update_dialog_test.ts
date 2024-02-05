ed1340a5a8ab3186dceabb36b6f96a2596b9bbae
Cam Bickel <cambickel@google.com>
2024-02-05 18:36:57 +0000

    firmware_update: Add aria-live to user request body to improve a11y
    This CL adds an aria-live = "assertive" attribute to the dialog body
    element when the app is waiting for user request. This way, ChromeVox
    will notify the user when action is required.
    
    I've also removed unnecessary "tabindexes" (no need to tab through
    uninteractive elements -- ChromeVox still works without them).
    
    I've also removed the dynamic "aria-hidden" logic, and just removed
    the aria-hidden tag altogether from the dialog body. In my testing,
    this had no impact on the ChromeVox output either way, and having
    aria-hidden prevented ChromeVox from reading the aria-live content.
    
    Demo: https://drive.google.com/file/d/1WpbmmQw7LSl6z_90wZ2zkN1260invZwn/view?usp=drive_link&resourcekey=0-qVzHZ_5Q-80FLB7DVzK1SQ
    
    Test: browser_tests -f=*FirmwareUpdateAppBrowserTest*
    Bug: b/308669841
    Change-Id: If7b09f4cab50ed81146037ba90e589d9241d74eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5253065
    Commit-Queue: Camden Bickel <cambickel@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256300}

--- chrome/test/data/webui/chromeos/firmware_update/firmware_update_dialog_test.ts
+++ chrome/test/data/webui/chromeos/firmware_update/firmware_update_dialog_test.ts
@@ -123,6 +123,12 @@ suite('FirmwareUpdateDialogTest', () => {
     assertEquals(
         getTextContent('#updateDialogBody'),
         loadTimeData.getString('restartingBodyText'));
+    // Body text should not have an aria-live value for non-requests.
+    assertEquals(
+        strictQuery(
+            '#updateDialogBody', updateDialogElement.shadowRoot, HTMLDivElement)
+            .ariaLive,
+        '');
     assertEquals(
         getTextContent('#progress'),
         loadTimeData.getString('restartingFooterText'));
@@ -222,6 +228,12 @@ suite('FirmwareUpdateDialogTest', () => {
     assertEquals(
         getTextContent('#updateDialogBody'),
         loadTimeData.getString('restartingBodyText'));
+    // Body text should not have an aria-live value for non-requests.
+    assertEquals(
+        strictQuery(
+            '#updateDialogBody', updateDialogElement.shadowRoot, HTMLDivElement)
+            .ariaLive,
+        '');
     assertEquals(
         getTextContent('#progress'),
         loadTimeData.getString('restartingFooterText'));
@@ -376,6 +388,15 @@ suite('FirmwareUpdateDialogTest', () => {
       assertEquals(
           getTextContent('#updateDialogBody'),
           loadTimeData.getString(expectedString));
+      assert(updateDialogElement?.shadowRoot);
+      // For user requests, the dialog body should be an assertive aria-live
+      // region.
+      assertEquals(
+          strictQuery(
+              '#updateDialogBody', updateDialogElement.shadowRoot,
+              HTMLDivElement)
+              .ariaLive,
+          'assertive');
       assertEquals(
           getTextContent('#progress'),
           loadTimeData.getStringF('waitingFooterText', 70));
