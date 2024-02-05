4ab2732888d2518d5d4fad94ebf3fbf0c07a8cac
Hidehiko Abe <hidehiko@chromium.org>
2024-02-05 09:37:25 +0000

    Fix dangling pointer issue.
    Bug: 1174760
    TEST: Ran ChromeRLZTrackerWebContentsObserverTest*
    Change-Id: I91c23d817540b948dc6698e32715eb26febb7bb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5266517
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256095}

--- chrome/browser/rlz/chrome_rlz_tracker_web_contents_observer_unittest.cc
+++ chrome/browser/rlz/chrome_rlz_tracker_web_contents_observer_unittest.cc
@@ -30,8 +30,8 @@ class ChromeRLZTrackerWebContentsObserverTest
 
   void TearDown() override {
     rlz::RLZTracker::SetRlzChromeHomePageSearchRecordedForTesting(false);
-    rlz::RLZTracker::ClearRlzDelegateForTesting();
     delegate_ = nullptr;
+    rlz::RLZTracker::ClearRlzDelegateForTesting();
     ChromeRenderViewHostTestHarness::TearDown();
   }
 
