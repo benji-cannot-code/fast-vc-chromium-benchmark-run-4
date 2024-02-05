b7c0a146988f0814544cbdb1c5ca1fb335ad2b1e
Boris Sazonov <bsazonov@chromium.org>
2024-02-05 11:22:26 +0000

    [Sync] Make SetSelectedType available on Android
    Exposes SyncUserSettings::SetSelectedType via JNI to Java callers.
    
    Bug: 1521068
    Change-Id: I526d2fd8ea64f633365441207812d14695e5e131
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5257300
    Reviewed-by: Jood Hajeer <jood@google.com>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256122}

--- chrome/browser/sync/test/android/java/src/org/chromium/chrome/browser/sync/FakeSyncServiceImpl.java
+++ chrome/browser/sync/test/android/java/src/org/chromium/chrome/browser/sync/FakeSyncServiceImpl.java
@@ -17,6 +17,7 @@ import org.chromium.components.signin.base.CoreAccountInfo;
 import org.chromium.components.signin.base.GoogleServiceAuthError;
 import org.chromium.components.sync.SyncService;
 import org.chromium.components.sync.SyncServiceImpl;
+import org.chromium.components.sync.UserSelectableType;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 
 import java.util.Date;
@@ -256,6 +257,11 @@ public class FakeSyncServiceImpl implements SyncService {
     @Override
     public void setSelectedTypes(boolean syncEverything, Set<Integer> enabledTypes) {
         mDelegate.setSelectedTypes(syncEverything, enabledTypes);
+    }
+
+    @Override
+    public void setSelectedType(@UserSelectableType int type, boolean isTypeOn) {
+        mDelegate.setSelectedType(type, isTypeOn);
     }
 
     @Override
--- components/sync/android/java/src/org/chromium/components/sync/SyncServiceImpl.java
+++ components/sync/android/java/src/org/chromium/components/sync/SyncServiceImpl.java
@@ -221,6 +221,13 @@ public class SyncServiceImpl implements SyncService, AccountsChangeObserver {
     }
 
     @Override
+    public void setSelectedType(@UserSelectableType int type, boolean isTypeOn) {
+        ThreadUtils.assertOnUiThread();
+        assert mSyncServiceAndroidBridge != 0;
+        SyncServiceImplJni.get().setSelectedType(mSyncServiceAndroidBridge, type, isTypeOn);
+    }
+
+    @Override
     public void setInitialSyncFeatureSetupComplete(int syncFirstSetupCompleteSource) {
         ThreadUtils.assertOnUiThread();
         assert mSyncServiceAndroidBridge != 0;
@@ -543,6 +550,11 @@ public class SyncServiceImpl implements SyncService, AccountsChangeObserver {
                 long nativeSyncServiceAndroidBridge,
                 boolean syncEverything,
                 int[] userSelectableTypeArray);
+
+        void setSelectedType(
+                long nativeSyncServiceAndroidBridge,
+                @UserSelectableType int type,
+                boolean isTypeOn);
 
         boolean isCustomPassphraseAllowed(long nativeSyncServiceAndroidBridge);
 
