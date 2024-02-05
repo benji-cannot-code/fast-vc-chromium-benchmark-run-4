f32c774a0303c3ded4b6f5d76a4998b2646e560a
Geoff Huang <geoffhuang@google.com>
2024-02-05 23:09:46 +0000

    Disable tests running into JaCoCo failures temporarily
    These tests are running into JaCoCo failures on the x86 trybot whenever
    a cl touches UiUtils.java. These failures are not reproducible locally.
    See crrev.com/c/1522985 for more info.
    
    Bug: 1524084
    Change-Id: I35c8a377e7faf8878ce9a7782dc45f0423a668e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5258686
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Commit-Queue: Geoff Huang <geoffhuang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1256469}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherPaneCoordinatorUnitTest.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherPaneCoordinatorUnitTest.java
@@ -43,6 +43,7 @@ import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.supplier.OneshotSupplierImpl;
 import org.chromium.base.supplier.Supplier;
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.HistogramWatcher;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.chrome.browser.browser_controls.BrowserControlsStateProvider;
@@ -197,6 +198,7 @@ public class TabSwitcherPaneCoordinatorUnitTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1524084")
     public void testShowTabListEditor() {
         ObservableSupplier<Boolean> handlesBackPressSupplier =
                 mCoordinator.getHandleBackPressChangedSupplier();
@@ -245,6 +247,7 @@ public class TabSwitcherPaneCoordinatorUnitTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1524084")
     public void testTabGridDialogVisibilitySupplier() {
         Supplier<Boolean> tabGridDialogVisibilitySupplier =
                 mCoordinator.getTabGridDialogVisibilitySupplier();
