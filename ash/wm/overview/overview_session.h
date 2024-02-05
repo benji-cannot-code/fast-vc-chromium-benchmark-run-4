69177cefd3e666cb977cde2cb053a6b729e4f33d
Michele Fan <michelefan@chromium.org>
2024-02-05 23:56:43 +0000

    snap-group: Exit SVOVS when activing snapped window
    This cl fixed the issue that when clicking on the snapped window
    caption bar SVOVS won't exit the session.
    
    This cl also added exit point on touch event.
    
    Fixed: b/323593633
    Demo: http://b/323593633#comment2
    Test: Added new + modify existing + manual
    Change-Id: I6bd34b11025e0b2ed84b0840fc8a3ef0c16aea50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5266117
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256501}

--- ash/wm/overview/overview_session.h
+++ ash/wm/overview/overview_session.h
@@ -345,6 +345,7 @@ class ASH_EXPORT OverviewSession : public display::DisplayObserver,
 
   // ui::EventHandler:
   void OnMouseEvent(ui::MouseEvent* event) override;
+  void OnTouchEvent(ui::TouchEvent* event) override;
   void OnKeyEvent(ui::KeyEvent* event) override;
 
   // ShellObserver:
@@ -449,6 +450,9 @@ class ASH_EXPORT OverviewSession : public display::DisplayObserver,
   void OnItemAdded(aura::Window* window);
 
   size_t GetNumWindows() const;
+
+  // Let `SplitViewOverviewSession` handle the `event` if it is alive.
+  void MaybeDelegateEventToSplitViewOverviewSession(ui::LocatedEvent* event);
 
   // Weak pointer to the overview delegate which will be called when a selection
   // is made.
--- ash/wm/splitview/split_view_overview_session.h
+++ ash/wm/splitview/split_view_overview_session.h
@@ -15,7 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "ui/aura/window_observer.h"
 #include "ui/compositor/presentation_time_recorder.h"
-#include "ui/events/event_handler.h"
+
+namespace ui {
+class LocatedEvent;
+}  // namespace ui
 
 namespace ash {
 
@@ -80,7 +83,7 @@ class ASH_EXPORT SplitViewOverviewSession : public aura::WindowObserver,
   // Called by `OverviewSession` on a key or mouse event that isn't processed by
   // overview session.
   void OnKeyEvent();
-  void OnMouseEvent(const ui::MouseEvent& event);
+  void HandleClickOrTap(const ui::LocatedEvent& event);
 
   // aura::WindowObserver:
   void OnResizeLoopStarted(aura::Window* window) override;
