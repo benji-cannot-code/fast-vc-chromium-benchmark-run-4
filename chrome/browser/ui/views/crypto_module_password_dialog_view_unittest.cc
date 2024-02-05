b8a61e46c6a450abbf8f411bd68d775db8d3e7a0
Elly <ellyjones@chromium.org>
2024-02-05 20:19:35 +0000

    views: run callback on Esc in CryptoModulePasswordDialog
    Since this dialog has a close button, the default DialogClientView
    behavior is to not run its closure callback, which is required to
    unblock the crypto module flow. Oops!
    
    See also
    https://chromium-review.googlesource.com/c/chromium/src/+/5255058 which
    fixes a misleading comment in Views about this behavior.
    
    Fixed: 1523355
    Change-Id: Iab8100d59ee4958203a5f58bd28e255701e57cde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5260349
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256370}

--- chrome/browser/ui/views/crypto_module_password_dialog_view_unittest.cc
+++ chrome/browser/ui/views/crypto_module_password_dialog_view_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/scoped_observation.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/bind.h"
 #include "chrome/browser/ui/crypto_module_password_dialog.h"
@@ -14,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/test/views_test_base.h"
+#include "ui/views/widget/widget_observer.h"
+#include "ui/views/window/dialog_client_view.h"
 
 using CryptoModulePasswordDialogViewTest = ChromeViewsTestBase;
 
@@ -51,4 +54,45 @@ TEST_F(CryptoModulePasswordDialogViewTest, CancelDoesntUsePassword) {
   EXPECT_TRUE(dialog->Cancel());
   EXPECT_TRUE(callback_run);
   EXPECT_EQ("", password);
+}
+
+class WidgetCloseWaiter : public views::WidgetObserver {
+ public:
+  explicit WidgetCloseWaiter(views::Widget* widget) {
+    observation_.Observe(widget);
+  }
+  ~WidgetCloseWaiter() override {}
+
+  void Wait() { loop_.Run(); }
+
+  void OnWidgetDestroying(views::Widget* widget) override {
+    CHECK_EQ(widget, observation_.GetSource());
+    observation_.Reset();
+    loop_.Quit();
+  }
+
+ private:
+  base::ScopedObservation<views::Widget, views::WidgetObserver> observation_{
+      this};
+  base::RunLoop loop_;
+};
+
+TEST_F(CryptoModulePasswordDialogViewTest, EscapeRunsCallback) {
+  bool callback_run = false;
+  auto dialog = CreateCryptoDialog(base::BindLambdaForTesting(
+      [&](const std::string&) { callback_run = true; }));
+  auto* weak_dialog = dialog.get();
+
+  views::Widget* dialog_widget =
+      dialog->CreateDialogWidget(std::move(dialog), GetContext(), nullptr);
+
+  views::DialogClientView* dcv = weak_dialog->GetDialogClientView();
+  ASSERT_TRUE(dcv);
+
+  WidgetCloseWaiter waiter(dialog_widget);
+
+  dcv->AcceleratorPressed(ui::Accelerator(ui::VKEY_ESCAPE, 0));
+
+  waiter.Wait();
+  EXPECT_TRUE(callback_run);
 }
