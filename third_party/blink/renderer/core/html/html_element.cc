2e63e01bbe0ae474e8b2d623f71510fb64d4010a
Mason Freed <masonf@chromium.org>
2024-02-05 15:28:39 +0000

    Clean up bdi/output unicode-bidi handling code
    See the predecessor CL [1] which fixed up other elements. This
    CL removes some extra code from html_element.cc that was taken care
    of in the UA stylesheet from [1].
    
    This CL also updates the default-bidi-css-rules.html test to match
    the spec. That leads to more failures, which I'm hoping to fix up
    in a subsequent CL. But I believe this makes the tested behavior
    correct.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/5240082
    
    Bug: 1520671
    Change-Id: Ie46966fbc1f5cba625660b9ed53fc9380e7acf75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5260610
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256220}

--- third_party/blink/renderer/core/html/html_element.cc
+++ third_party/blink/renderer/core/html/html_element.cc
@@ -358,9 +358,7 @@ void HTMLElement::CollectStyleForPresentationAttribute(
         AddPropertyToPresentationAttributeStyle(
             style, CSSPropertyID::kDirection, "ltr");
       }
-      if (!HasTagName(html_names::kBdiTag) &&
-          !HasTagName(html_names::kBdoTag) &&
-          !HasTagName(html_names::kOutputTag)) {
+      if (!HasTagName(html_names::kBdoTag)) {
         AddPropertyToPresentationAttributeStyle(
             style, CSSPropertyID::kUnicodeBidi, CSSValueID::kIsolate);
       }
