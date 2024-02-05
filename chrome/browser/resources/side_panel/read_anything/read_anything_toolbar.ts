fe448a049a738d498034cf1aae9dfaba22bdf0b3
Eitan Goldberger <eitang@google.com>
2024-02-05 15:23:33 +0000

    Add a field to track whether speech pause is triggered from the play/pause button toggle.
    This is because we want to differentiate between pauses that come from
    the play/pause button from pauses that come from a menu selection. For
    the play/pause button, we want to call synth.pause() then
    synth.resume(), because this continues reading from the word that was
    paused. For menu changes, we want to call synth.cancel() then
    synth.play(), because this allows the menu changes to take effect and
    shows the user that the app is responsive to their changes.
    
    However, synth.cancel() and synth.play() starts speaking from the
    beginning of the utterance. This is why we don't want to use this for
    the play/pause button. This will change when we handle word level
    tracking, but for now we need this temporary code change.
    
    Bug: 1474951
    Change-Id: I9a3c3223d978d46b220da23c90d933d336bd3e3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5262559
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Eitan Goldberger <eitang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1256215}

--- chrome/browser/resources/side_panel/read_anything/read_anything_toolbar.ts
+++ chrome/browser/resources/side_panel/read_anything/read_anything_toolbar.ts
@@ -701,7 +701,7 @@ export class ReadAnythingToolbarElement extends ReadAnythingToolbarElementBase {
     if (this.paused) {
       this.contentPage.playSpeech();
     } else {
-      this.contentPage.stopSpeech();
+      this.contentPage.stopSpeech(/* pausedFromPlayClickButton = */ true);
     }
   }
 
