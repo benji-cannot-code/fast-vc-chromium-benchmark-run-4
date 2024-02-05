1220770c9ca92fc464d628ebb615d3cadf754715
Andy Phan <andyphan@chromium.org>
2024-02-05 22:43:04 +0000

    Remove PdfScriptingApi usage in PDF layout tests
    PdfScriptingApi is used to wait for the PDF load to complete before
    running the layout tests. Remove this usage and just run the tests
    immediately.
    
    PdfScriptingApi is unneeded to wait for the load, because
    PDFExtensionJSTest, which runs the tests, already ensures the PDF has
    fully loaded during test setup. This occurs in
    pdf_extension_test_util::EnsurePDFHasLoaded().
    
    Change-Id: Ibe97c84e45e38351b31b963de2f7248031cf2d56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5269770
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256456}

--- chrome/test/data/pdf/layout_test.ts
+++ chrome/test/data/pdf/layout_test.ts
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {PdfScriptingApi} from 'chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai/pdf_scripting_api.js';
 import {PdfViewerElement} from 'chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai/pdf_viewer_wrapper.js';
 
 // Tests common to all PDFs.
@@ -50,11 +49,8 @@ const perLayoutTests: {[name: string]: Array<() => void>} = {
   ],
 };
 
-const scriptingAPI = new PdfScriptingApi(window, window);
-scriptingAPI.setLoadCompleteCallback((success) => {
-  if (success && document.title in perLayoutTests) {
-    chrome.test.runTests(tests.concat(perLayoutTests[document.title]!));
-  } else {
-    chrome.test.fail(document.title);
-  }
-});
+if (document.title in perLayoutTests) {
+  chrome.test.runTests(tests.concat(perLayoutTests[document.title]!));
+} else {
+  chrome.test.fail(document.title);
+}
