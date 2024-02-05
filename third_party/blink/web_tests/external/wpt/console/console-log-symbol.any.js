836461a202d6aec1856c05607a44a67003313364
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-02-05 19:51:16 +0000

    Import wpt@83ca3f916e893dbdc3cfaa498aa10fc5674ed006
    Using wpt-import in Chromium 3a1c62d03386e1ff7bba3ef332013759213181e8.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    kozyatinskiy@chromium.org:
      external/wpt/console
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I42856a1e1033e64a3cbf60aa2366a30dd71ccfd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5269005
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1256351}

---
+++ third_party/blink/web_tests/external/wpt/console/console-log-symbol.any.js
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,dedicatedworker,shadowrealm
+"use strict";
+// https://console.spec.whatwg.org/
+
+test(() => {
+    console.log(Symbol());
+    console.log(Symbol("abc"));
+    console.log(Symbol.for("def"));
+    console.log(Symbol.isConcatSpreadable);
+}, "Logging a symbol doesn't throw");
