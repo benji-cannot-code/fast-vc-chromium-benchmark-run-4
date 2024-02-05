5912522d881eb4c98387e4d5fa6b2fd31a016a14
Ryan Sultanem <rsult@google.com>
2024-02-05 10:42:49 +0000

    Reland "[InterceptBubble] Adapt identity pill effect for each intercept type with a bubble"
    This is a reland of commit 6347f969ef6314cd3f5a6326670b7348b2c8d096
    
    Revert because of UAF in the test; `widget` was used after being
    deleted.
    Fix: Move the check before deleting the widget (similar to other
    tests in the suite).
    
    Original change's description:
    > [InterceptBubble] Adapt identity pill effect for each intercept type with a bubble
    >
    > The changes were already existing for the ChromeSigninBubble, the goal
    > of this fix is to extend them to the Profile Separation bubble and the
    > Profile Switch bubble.
    >
    > - While the bubbles are shown, do not allow the profile menu to be
    > displayed on top of it. The button action will be disabled.
    > - The identity pill extends and show a text related to the bubble
    > type being displayed, while the bubble is disaplyed and the button
    > action is disabled.
    >
    > Bug: b:319410070
    > Change-Id: I6a57a37280eddde82cfebdda185114c470f9667c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5250584
    > Reviewed-by: David Roger <droger@chromium.org>
    > Commit-Queue: Ryan Sultanem <rsult@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1255491}
    
    Bug: b:319410070
    Change-Id: Id70a71d1a38712f4dcaf5bddfb8065b2c5a25bb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5260100
    Auto-Submit: Ryan Sultanem <rsult@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256111}

--- chrome/browser/ui/views/profiles/avatar_toolbar_button.h
+++ chrome/browser/ui/views/profiles/avatar_toolbar_button.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/time/time.h"
 #include "build/chromeos_buildflags.h"
+#include "chrome/browser/signin/web_signin_interceptor.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_button.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/events/event.h"
@@ -28,7 +29,7 @@ class AvatarToolbarButton : public ToolbarButton {
   enum class State {
     kIncognitoProfile,
     kGuestSession,
-    kSignInTextShowing,
+    kInterceptTextShowing,
     kAnimatedUserIdentity,
     kSyncPaused,
     // An error in sync-the-feature or sync-the-transport.
@@ -56,11 +57,12 @@ class AvatarToolbarButton : public ToolbarButton {
 
   void ShowAvatarHighlightAnimation();
 
-#if !BUILDFLAG(IS_CHROMEOS_LACROS) || BUILDFLAG(IS_CHROMEOS_ASH)
-  // Expands the pill to show the signin text.
-  void ShowSignInText();
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
+  // Expands the pill to show the intercept text.
+  void ShowInterceptText(
+      WebSigninInterceptor::SigninInterceptionType interception_type);
   // Contracts the pill so that no text is shown.
-  void HideSignInText();
+  void HideText();
 #endif
 
   // Control whether the button action is active or not.
--- chrome/browser/ui/views/profiles/avatar_toolbar_button_delegate.h
+++ chrome/browser/ui/views/profiles/avatar_toolbar_button_delegate.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/profiles/profile_attributes_storage.h"
+#include "chrome/browser/signin/web_signin_interceptor.h"
 #include "chrome/browser/sync/sync_ui_util.h"
 #include "chrome/browser/ui/browser_list_observer.h"
 #include "chrome/browser/ui/views/profiles/avatar_toolbar_button.h"
@@ -62,9 +63,12 @@ class AvatarToolbarButtonDelegate : public BrowserListObserver,
   void ShowHighlightAnimation();
   bool IsHighlightAnimationVisible() const;
 
-#if !BUILDFLAG(IS_CHROMEOS_LACROS) || BUILDFLAG(IS_CHROMEOS_ASH)
-  void ShowSignInText();
-  void HideSignInText();
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
+  void ShowInterceptText(
+      WebSigninInterceptor::SigninInterceptionType interception_type);
+  void HideText();
+
+  std::u16string GetInterceptText();
 #endif
 
   // Should be called when the icon is updated. This may trigger the identity
@@ -85,7 +89,7 @@ class AvatarToolbarButtonDelegate : public BrowserListObserver,
     kNotShowing,
     kWaitingForImage,
     kShowingName,
-    kShowingSigninText
+    kShowingInterceptText
   };
 
   // BrowserListObserver:
@@ -127,6 +131,8 @@ class AvatarToolbarButtonDelegate : public BrowserListObserver,
   // this extends the duration of the current animation.
   void ShowIdentityAnimation();
 
+  void Reset();
+
   base::ScopedObservation<ProfileAttributesStorage,
                           ProfileAttributesStorage::Observer>
       profile_observation_{this};
@@ -155,9 +161,12 @@ class AvatarToolbarButtonDelegate : public BrowserListObserver,
   // sync paused/error state will be disabled.
   bool highlight_animation_visible_ = false;
 
-  // Caches the value of the last error so the class can detect when it changes
-  // and notify |avatar_toolbar_button_|.
+  // Caches the value of the last error so the class can detect when it
+  // changes and notify |avatar_toolbar_button_|.
   std::optional<AvatarSyncErrorType> last_avatar_error_;
+
+  std::optional<WebSigninInterceptor::SigninInterceptionType>
+      current_interception_type_;
 
   base::WeakPtrFactory<AvatarToolbarButtonDelegate> weak_ptr_factory_{this};
 };
