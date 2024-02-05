3816a75af457f643f270a83e29c97edf73ea9388
Chris Blume <cblume@chromium.org>
2024-02-05 16:27:27 +0000

    Add PNG eXIf chunk support
    The PNG eXIf chunk has been in the extension part of the PNG spec for a long
    time now. libpng already supports eXIf (outside of push mode).
    
    http://www.libpng.org/pub/png/spec/register/pngext-1.4.0-pdg.html
    
    In PNG Third Edition, eXIf is becoming an official part of the main spec and
    not just in the extensions. As such, Chromium should officially support it.
    
    There is a WPT which tests if the eXIf chunk is recognized by setting an
    orientation:
    https://wpt.fyi/results/png/exif-chunk.html?label=experimental&label=master&aligned
    
    This commit adds support for the eXIf PNG chunk (updating orientation) to Chromium.
    
    Note, libpng supports eXIf in non-push mode. Chromium uses push mode.
    It seems like when eXIf support was added to libpng they simply forgot push mode.
    This commit also adds support for eXIf to libpng's push mode.
    
    Change-Id: Ibda3b9a4f59ad5787fb441d6db015e564c1798ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5249880
    Reviewed-by: Leon Scroggins <scroggo@google.com>
    Commit-Queue: Chris Blume <cblume@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256248}

--- third_party/blink/renderer/platform/image-decoders/png/png_image_decoder.cc
+++ third_party/blink/renderer/platform/image-decoders/png/png_image_decoder.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/adapters.h"
 #include "base/numerics/checked_math.h"
 #include "media/base/video_color_space.h"
+#include "third_party/blink/renderer/platform/image-decoders/exif_reader.h"
 #include "third_party/skia/include/core/SkColorSpace.h"
 #include "third_party/skia/modules/skcms/skcms.h"
 
@@ -482,6 +483,19 @@ void PNGImageDecoder::HeaderAvailable() {
       png_set_gamma(png, kDefaultGamma, gamma);
     } else {
       png_set_gamma(png, kDefaultGamma, kInverseGamma);
+    }
+  }
+
+  // process eXIf chunk
+  png_uint_32 exif_size = 0;
+  png_bytep exif_buffer = nullptr;
+  if (png_get_eXIf_1(png, info, &exif_size, &exif_buffer) != 0) {
+    // exif data exists
+    if (exif_size != 0 && exif_buffer) {
+      DecodedImageMetaData metadata;
+      base::span<const uint8_t> exif_span(exif_buffer, exif_size);
+      ReadExif(exif_span, metadata);
+      ApplyMetadata(metadata, gfx::Size(width, height));
     }
   }
 
