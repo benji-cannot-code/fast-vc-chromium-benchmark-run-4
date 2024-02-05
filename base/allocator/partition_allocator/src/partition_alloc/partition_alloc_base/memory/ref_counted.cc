95684753c872b3aa216b0d16c5166a85a3c6c0e7
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-02-05 22:05:35 +0000

    PA: Fix transitive include of //build/build_config.h
    I am investigating why the CQ is blocking:
    https://chromium-review.googlesource.com/c/chromium/src/+/5205331?checksRunsSelected=linux-rel&tab=checks
    
    Apparently, some bots on Linux become much slower, causing global
    timeout. The patch is a no-op. So I am supposing this is caused by
    some missing //build/build_config.h that used to be transitively
    included, but aren't after reordering some headers.o
    
    Note: In practise this wasn't the issue. It is still worth fixing.
    
    Script:
    -------
    
    ```
    tokens=$(cat build/build_config.h | sed -n "s/#define \([^\ ()]*\) /\1 /p" | cut -f1 -d\ )
    
    for file in $(find base/allocator/partition_allocator/src/partition_alloc -name "*.h" -o -name "*.cc")
    do
      include_line=$(grep -n "build/build_config.h" "$file" | head -1 |cut -f1 -d:)
      for token in $tokens
      do
        token_line=$(grep -n "$token" "$file" | head -1 | cut -f1 -d:)
    
        if [[ "$token_line" == "" ]]
        then
          continue
        fi
    
        if [[ "$include_line" == "" ]]
        then
          echo "$file" -- "$token" -- " Missing include"
          continue
        fi
    
        if [ "$include_line" -gt "$token_line" ]
        then
          echo "$file" -- "$token" -- " Token used before include: $include_line vs $token_line"
        fi
      done
    done | sort | uniq -c | sort -rn
    ```
    
    Output:
    -------
    ```
    2 base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/memory/ref_counted.cc -- ARCH_CPU_X86_FAMILY --  Missing include
    1 base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/memory/ref_counted.cc -- ARCH_CPU_X86 --  Missing include
    1 base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/ios/ios_util.h -- OS_IOS --  Missing include
    1 base/allocator/partition_allocator/src/partition_alloc/page_allocator_internals_posix.cc -- ARCH_CPU_ARM64 --  Missing include
    ```
    
    Bug: 1508847
    Change-Id: I0b409fcb3baf08eedc54c86adad2eeb5f2c2dba5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5269103
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256439}

--- base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/memory/ref_counted.cc
+++ base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/memory/ref_counted.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <ostream>
 #include <type_traits>
 
+#include "build/build_config.h"
 #include "partition_alloc/partition_alloc_base/debug/debugging_buildflags.h"
 
 namespace partition_alloc::internal::base::subtle {
