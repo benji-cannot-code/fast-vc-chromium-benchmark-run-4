ebaa7204cee74e832c30c5f047016d2d5b0f981d
Ashley Prasad <ashleydp@google.com>
2024-02-06 22:04:08 +0000

    cros-preview: Add flag for CrOS preview app
    Adds flag for CrOS print preview app. Enabling does not replace the
    existing Chrome print preview UI, doing so will require a Chrome flag
    and pref configured to facilitate. See b/323421684 for more information.
    
    Bug: b:323421684
    Tests: unit_tests --gtest_filter=*AboutFlags*
    Change-Id: I97c22a8baee282ec652918a836b3bc9b69015018
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5270590
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1257026}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4033,6 +4033,9 @@ extern const char kPrintManagementJellyDescription[];
 extern const char kPrintManagementSetupAssistanceName[];
 extern const char kPrintManagementSetupAssistanceDescription[];
 
+extern const char kPrintPreviewCrosAppName[];
+extern const char kPrintPreviewCrosAppDescription[];
+
 extern const char kProductivityLauncherName[];
 extern const char kProductivityLauncherDescription[];
 
