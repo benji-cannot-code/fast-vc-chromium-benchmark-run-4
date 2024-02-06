de4b5c1f71a257514ce7940b00761a31a032e3ad
Dave Tapuska <dtapuska@chromium.org>
2024-02-06 14:49:34 +0000

    Fix dangling ptr in HUD display
    UpdateHudTexture might not get called if the frame is not drawn leaving
    a dangling ptr. Implement LayerImpl::DidDraw in HeadsUpDisplayLayerImpl
    so we always clear the placeholder_quad even if drawing didn't happen.
    
    Change-Id: I674175876289d859d2a7a569d5af8399ee6c19e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5268333
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256732}

--- cc/layers/heads_up_display_layer_impl.h
+++ cc/layers/heads_up_display_layer_impl.h
@@ -55,6 +55,7 @@ class CC_EXPORT HeadsUpDisplayLayerImpl : public LayerImpl {
 
   bool WillDraw(DrawMode draw_mode,
                 viz::ClientResourceProvider* resource_provider) override;
+  void DidDraw(viz::ClientResourceProvider* resource_provider) override;
   void AppendQuads(viz::CompositorRenderPass* render_pass,
                    AppendQuadsData* append_quads_data) override;
   void UpdateHudTexture(DrawMode draw_mode,
