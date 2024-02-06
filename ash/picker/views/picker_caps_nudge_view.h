131014aba54f450e0a1b8bb7375622e1f56bbab9
John Palmer <jopalmer@chromium.org>
2024-02-06 03:09:25 +0000

    Add caps notifier to picker.
    Since this UI has no actual functionality, there is no testing.
    
    See http://screen/8gLWDwTD3T8uJaM
    
    Bug: b:321577202
    Change-Id: I7ce2246e42d1697968776f42cd4735838d70f6a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5256477
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256582}

---
+++ ash/picker/views/picker_caps_nudge_view.h
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_PICKER_VIEWS_PICKER_CAPS_NUDGE_VIEW_H_
+#define ASH_PICKER_VIEWS_PICKER_CAPS_NUDGE_VIEW_H_
+
+#include "ash/ash_export.h"
+#include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/views/view.h"
+
+namespace ash {
+
+// View for the caps notifier in the picker zero state
+class ASH_EXPORT PickerCapsNudgeView : public views::View {
+  METADATA_HEADER(PickerCapsNudgeView, views::View)
+
+ public:
+  PickerCapsNudgeView();
+  PickerCapsNudgeView(const PickerCapsNudgeView&) = delete;
+  PickerCapsNudgeView& operator=(const PickerCapsNudgeView&) = delete;
+  ~PickerCapsNudgeView() override;
+};
+
+}  // namespace ash
+
+#endif  // ASH_PICKER_VIEWS_PICKER_CAPS_NUDGE_VIEW_H_
