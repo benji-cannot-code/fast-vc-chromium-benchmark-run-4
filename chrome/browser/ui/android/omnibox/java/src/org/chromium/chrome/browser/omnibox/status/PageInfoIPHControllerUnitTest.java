02de0c84d7ef84ea2644ed62b16fa02c1e79af18
Ted Choc <tedchoc@chromium.org>
2024-02-06 23:55:43 +0000

    Remove getLastUsedRegularProfile from PageInfoIPHController.
    Bug: 40254448
    Change-Id: I6d117d42f0909f7a6f29ef788609fd443022974e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5273640
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257081}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/status/PageInfoIPHControllerUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/status/PageInfoIPHControllerUnitTest.java
@@ -50,7 +50,6 @@ public class PageInfoIPHControllerUnitTest {
 
     @Before
     public void setUp() {
-        Profile.setLastUsedProfileForTesting(mProfile);
         TrackerFactory.setTrackerForTests(mTracker);
 
         mView = new View(ContextUtils.getApplicationContext());
@@ -59,13 +58,12 @@ public class PageInfoIPHControllerUnitTest {
 
     @After
     public void tearDown() {
-        Profile.setLastUsedProfileForTesting(null);
         TrackerFactory.setTrackerForTests(null);
     }
 
     @Test
     public void onPermissionDialogShown() {
-        mController.onPermissionDialogShown(TIMEOUT);
+        mController.onPermissionDialogShown(mProfile, TIMEOUT);
         verify(mHelper).requestShowIPH(mIPHCmdCaptor.capture());
         var cmd = mIPHCmdCaptor.getValue();
         cmd.fetchFromResources();
