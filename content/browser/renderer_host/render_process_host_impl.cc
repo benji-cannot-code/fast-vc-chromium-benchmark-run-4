5bcaa42a54a5ff293ce9550753846508bb9fd645
Keren Zhu <kerenzhu@chromium.org>
2024-02-06 17:41:36 +0000

    Add WebUI warmup levels and under each level measure Tab Search startup time
    Add an enum WebUIBubbleWarmUpLevel that describes the readiness levels
    of the browser prior to showing a new WebUI bubble.
    
    * kNoRenderer:
      No render process is available. No pre-existing process, including
      spare renderers, can be used or reused for this WebUI.
    * kSpareRenderer:
      Uses a spare render process for this WebUI.
    * kDedicatedRenderer:
      Uses a render process that already hosts other WebUIs prior to this
      WebUI.
    * kRedirectedWebContents:
      Uses a WebContents that is initially on a different domain and
      requires redirection to this WebUI.
      This is not currently used anywhere, but worth further investigation.
    * kNavigatedWebContents:
      Uses a WebContents that has already navigated to this WebUI.
    
    Add histograms Tabs.TabSearch.TimeToShow.{WarmUpLevel} to record the
    startup time of Tab Search under each level. These metrics are similar
    to previous metrics but have finer granularity. Specifically,
    
    * The existing WindowTimeToShowCachedWebView2 == kNavigatedWebContents
    * The existing WindowTimeToShowUncachedWebView2 is equivalent to an
      aggregation of kNoRenderer, kSpareRenderer and kDedicatedRenderer.
    
    These histogram will help us find the warm-up level that strikes the
    right balance between latency and memory overhead in an effort to
    improve WebUI startup time.
    
    Bug: 1519190
    Change-Id: Ie1889aaf55bd5065d72e50f0524e957e05391022
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5254568
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256848}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -3121,9 +3121,14 @@ void RenderProcessHostImpl::NotifySpareManagerAboutRecentlyUsedSiteInstance(
 }
 
 // static
-RenderProcessHost* RenderProcessHost::GetSpareRenderProcessHostForTesting() {
+RenderProcessHost* RenderProcessHost::GetSpareRenderProcessHost() {
   return SpareRenderProcessHostManager::GetInstance()
       .spare_render_process_host();
+}
+
+// static
+RenderProcessHost* RenderProcessHost::GetSpareRenderProcessHostForTesting() {
+  return GetSpareRenderProcessHost();
 }
 
 // static
