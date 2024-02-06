b72a5653816bb994fa83bf3e14114e4af6f8d69c
Jinsuk Kim <jinsukkim@chromium.org>
2024-02-06 17:34:03 +0000

    [TabModel] Emit the selector model size when closing tab fails
    Added a log to identify situations where tab model selector
    could be closed prior to tab closure, thus causing the assert
    failure
    
    Bug: 40067160
    Change-Id: Ib6c3dba5ece233e4e8db4c3a905aaccf82c6c31a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5268943
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256838}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelSelectorBase.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelSelectorBase.java
@@ -34,7 +34,7 @@ public abstract class TabModelSelectorBase
 
     private static TabModelSelectorObserver sObserverForTesting;
 
-    private List<TabModel> mTabModels = new ArrayList<>();
+    private final List<TabModel> mTabModels = new ArrayList<>();
     private IncognitoTabModel mIncognitoTabModel;
 
     /**
@@ -271,7 +271,9 @@ public abstract class TabModelSelectorBase
                         + " Is destroyed "
                         + tab.isDestroyed()
                         + " Is detached "
-                        + tab.isDetached();
+                        + tab.isDetached()
+                        + " Model-size: "
+                        + getModels().size();
         return false;
     }
 
