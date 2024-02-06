de250bdaf7aaa998d0a441c4c3dc531760e6e8e6
Samar <samarchehade@google.com>
2024-02-06 09:52:01 +0000

    [Gardener][Android] Disable failing tests
    Bug: b/41491395
    Change-Id: I8fd179a63f0bd5dfb4a2fb42b6d7b37873c2474f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5268672
    Auto-Submit: Samar Chehade-Lepleux <samarchehade@google.com>
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Owners-Override: Samar Chehade-Lepleux <samarchehade@google.com>
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1256670}

--- chrome/android/javatests/src/org/chromium/chrome/browser/settings/MainSettingsFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/settings/MainSettingsFragmentTest.java
@@ -61,6 +61,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.ApplicationTestUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.DisableFeatures;
@@ -194,6 +195,7 @@ public class MainSettingsFragmentTest {
     @Test
     @LargeTest
     @Feature({"RenderTest"})
+    @DisabledTest(message = "http://b/issues/41491395")
     public void testRenderDifferentSignedInStates() throws IOException {
         launchSettingsActivity();
         waitForOptionsMenu();
@@ -489,6 +491,7 @@ public class MainSettingsFragmentTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "http://b/issues/41491395")
     public void testAccountSignIn() throws InterruptedException {
         launchSettingsActivity();
 
