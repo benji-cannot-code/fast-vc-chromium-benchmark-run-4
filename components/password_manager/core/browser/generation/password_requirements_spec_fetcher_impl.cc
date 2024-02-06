4e628bccf5282766e25cc29eccb2dd36362fb066
Lukasz Anforowicz <lukasza@chromium.org>
2024-02-06 09:26:22 +0000

    Use `span`-based `base::MD5Sum`: //components/password_manager.
    This CL was uploaded by git cl split.
    
    R=vasilii@chromium.org
    
    Bug: 1490484
    Change-Id: I784c0bc514aba7763ee97953540d9a39a1cbeb97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5270080
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256665}

--- components/password_manager/core/browser/generation/password_requirements_spec_fetcher_impl.cc
+++ components/password_manager/core/browser/generation/password_requirements_spec_fetcher_impl.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/password_manager/core/browser/generation/password_requirements_spec_fetcher_impl.h"
 
+#include "base/containers/span.h"
 #include "base/functional/bind.h"
 #include "base/hash/md5.h"
 #include "base/logging.h"
@@ -65,7 +66,7 @@ std::string GetHashPrefix(const GURL& origin, size_t prefix_length) {
           origin, net::registry_controlled_domains::INCLUDE_PRIVATE_REGISTRIES);
 
   base::MD5Digest digest;
-  base::MD5Sum(domain_and_registry.data(), domain_and_registry.size(), &digest);
+  base::MD5Sum(base::as_byte_span(domain_and_registry), &digest);
 
   for (auto& byte : digest.a) {
     if (prefix_length >= 8) {
