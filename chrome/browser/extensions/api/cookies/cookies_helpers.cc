3b95f50835d9b96c6aa331e4351537446cd8b090
Dylan Cutler <dylancutler@google.com>
2024-02-06 17:14:52 +0000

    Remove relative include
    Bug: None
    Change-Id: I4023b11090c11fc339f1004939a729e0f3872ad7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5273543
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Dylan Cutler <dylancutler@google.com>
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256824}

--- chrome/browser/extensions/api/cookies/cookies_helpers.cc
+++ chrome/browser/extensions/api/cookies/cookies_helpers.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/api/cookies.h"
 #include "chrome/common/url_constants.h"
 #include "content/public/browser/web_contents.h"
-#include "cookies_helpers.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/permissions/permissions_data.h"
 #include "net/cookies/canonical_cookie.h"
