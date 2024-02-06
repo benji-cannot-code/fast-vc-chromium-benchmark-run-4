56d29be20ba700370925cede3065d2c8b6b4f6c4
Daniel Rubery <drubery@chromium.org>
2024-02-06 02:21:37 +0000

    Revert "Add prefs for users interaction with deep scanning"
    This reverts commit 882c85a148470eab87285d965048c601ba80206e. These
    prefs are not needed after all. Patchset 1 is the pure revert, with
    patchset 2 adding pref cleanup code.
    
    Bug: 1520125
    Change-Id: Ie7ed2a6b0b397dcd4acb1951f782e0ad624d1929
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5246595
    Reviewed-by: Javier Castro <jacastro@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256557}

--- chrome/browser/safe_browsing/safe_browsing_service.h
+++ chrome/browser/safe_browsing/safe_browsing_service.h
@@ -80,7 +80,6 @@ class SafeBrowsingServiceFactory;
 class SafeBrowsingUIManager;
 class TriggerManager;
 class HashRealTimeService;
-class SafeBrowsingServiceTest;
 
 // Construction needs to happen on the main thread.
 // The SafeBrowsingService owns both the UI and Database managers which do
@@ -238,7 +237,7 @@ class SafeBrowsingService : public SafeBrowsingServiceInterface,
   friend class SafeBrowsingBlockingPageTestBase;
   friend class SafeBrowsingBlockingQuietPageTest;
   friend class extensions::SafeBrowsingPrivateApiUnitTest;
-  friend class SafeBrowsingServiceTest;
+  friend class SafeBrowsingServerTest;
   friend class SafeBrowsingUIManagerTest;
   friend class TestSafeBrowsingService;
   friend class TestSafeBrowsingServiceFactory;
