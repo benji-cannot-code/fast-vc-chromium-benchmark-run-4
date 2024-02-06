763fc5b0127a2c84bf49c9063d2534e4cc54c971
Takuto Ikuta <tikuta@chromium.org>
2024-02-06 02:06:25 +0000

    content: add an wrapper for ad_auction_service_impl
    This is to remove one indirect include of render_frame_host_impl.h
    from browser_interface_binders.cc to reduce compile time in critical
    path of build and utilize build parallelism more for faster build.
    
    This is extracted from https://crrev.com/c/5252622.
    
    Bug: b/322102823
    Change-Id: I38d0fb98ff9a160c6cb74734d29a9d8516b63a9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5256383
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256553}

---
+++ content/browser/binder_wrappers/ad_auction_service_impl_wrapper.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/binder_wrappers/ad_auction_service_impl_wrapper.h"
+
+#include "content/browser/interest_group/ad_auction_service_impl.h"
+#include "third_party/blink/public/mojom/interest_group/ad_auction_service.mojom.h"
+
+namespace content {
+
+namespace internal {
+
+void AdAuctionServiceImplCreateMojoService(
+    mojo::BinderMapWithContext<RenderFrameHost*>& map) {
+  map.Add<blink::mojom::AdAuctionService>(
+      base::BindRepeating(&AdAuctionServiceImpl::CreateMojoService));
+}
+
+}  // namespace internal
+
+}  // namespace content
