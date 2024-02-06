355e062bf2eebcf45fd3ce9af9291cd48e9e864b
Takuto Ikuta <tikuta@chromium.org>
2024-02-06 06:15:11 +0000

    content: add an wrapper for keyboard_lock_service_impl
    This is to remove one indirect include of render_frame_host_impl.h
    from browser_interface_binders.cc to reduce compile time in critical
    path of build and utilize build parallelism more for faster build.
    
    This is extracted from https://crrev.com/c/5252622.
    
    Bug: b/322102823
    Change-Id: I4cfd55855ef267b747ab85c141533a3bf524ae35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5256384
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256626}

---
+++ content/browser/binder_wrappers/keyboard_lock_service_impl_wrapper.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_BINDER_WRAPPERS_KEYBOARD_LOCK_SERVICE_IMPL_WRAPPER_H_
+#define CONTENT_BROWSER_BINDER_WRAPPERS_KEYBOARD_LOCK_SERVICE_IMPL_WRAPPER_H_
+
+#include "mojo/public/cpp/bindings/binder_map.h"
+
+namespace content {
+
+class RenderFrameHost;
+
+// These are only used from functions in internal namespace of
+// browser_interface_binders.cc, so we use internal namespace here as well.
+namespace internal {
+
+void KeyboardLockServiceImplCreateMojoService(
+    mojo::BinderMapWithContext<RenderFrameHost*>& map);
+
+}  // namespace internal
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_BINDER_WRAPPERS_KEYBOARD_LOCK_SERVICE_IMPL_WRAPPER_H_
