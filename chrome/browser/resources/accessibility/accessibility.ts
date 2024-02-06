b77a0b634479ea1e57ac3ff6c2324e83505d31b5
David Tseng <dtseng@google.com>
2024-02-06 01:14:03 +0000

    [pdf-a11y]: clarify AXMode::kPdf
    https://crrev.com/c/2220253
    added this mode who's purpose is for PDF printing a11y.
    
    Unfortunately, its generic mean may be (and was) interpreted as a general PDF a11y mode.
    
    Rename it and remove the improper usage in PdfAccessibilityTree.
    
    R=aleventhal@chromium.org, thestig@chromium.org
    
    Bug: none
    Test: cq
    Change-Id: I6f60c2f06dd8f583a5f3b4abc51782211394036c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5270301
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256528}

--- chrome/browser/resources/accessibility/accessibility.ts
+++ chrome/browser/resources/accessibility/accessibility.ts
@@ -20,7 +20,7 @@ enum AxMode {
   HTML = 1 << 4,
   HTML_METADATA = 1 << 5,
   LABEL_IMAGES = 1 << 6,
-  PDF = 1 << 7,
+  PDF_PRINTING = 1 << 7,
   PDF_OCR = 1 << 8,
 }
 
@@ -47,7 +47,7 @@ type PageData = Data&{
   // chrome/browser/accessibility/accessibility_ui.cc.
   metadata: boolean,
   native: boolean,
-  pdf: boolean,
+  pdfPrinting: boolean,
   screenreader: boolean,
   web: boolean,
 
@@ -72,7 +72,7 @@ interface InitData {
   html: EnabledStatus;
   internal: EnabledStatus;
   native: EnabledStatus;
-  pdf: EnabledStatus;
+  pdfPrinting: EnabledStatus;
   screenreader: EnabledStatus;
   text: EnabledStatus;
   web: EnabledStatus;
@@ -80,7 +80,7 @@ interface InitData {
 
 type RequestType = 'showOrRefreshTree';
 
-type GlobalStateName = 'native'|'web'|'metadata'|'pdf'|'screenreader';
+type GlobalStateName = 'native'|'web'|'metadata'|'pdfPrinting'|'screenreader';
 
 class BrowserProxy {
   toggleAccessibility(
@@ -365,7 +365,8 @@ function formatRow(
     row.appendChild(createModeElement(AxMode.HTML, pageData, 'web'));
     row.appendChild(
         createModeElement(AxMode.HTML_METADATA, pageData, 'metadata'));
-    row.appendChild(createModeElement(AxMode.PDF, pageData, 'pdf'));
+    row.appendChild(
+        createModeElement(AxMode.PDF_PRINTING, pageData, 'pdfPrinting'));
     row.appendChild(createModeElement(
         AxMode.LABEL_IMAGES, pageData, 'screenreader',
         /*readonly=*/ true));
@@ -468,8 +469,8 @@ function getNameForAccessibilityMode(mode: AxMode): string {
       return 'HTML Metadata';
     case AxMode.LABEL_IMAGES:
       return 'Label images';
-    case AxMode.PDF:
-      return 'PDF';
+    case AxMode.PDF_PRINTING:
+      return 'PDF printing';
     case AxMode.PDF_OCR:
       return 'PDF OCR';
     default:
