0687c478142a0368338be6563e206f5ecd522f8a
Colin Blundell <blundell@chromium.org>
2024-02-06 14:49:25 +0000

    [GPU] Add and use HARDWARE_GRAPHITE GPU mode
    The logic for GPU fallback currently doesn't take into account whether
    Graphite is enabled. The logic works "accidentally" for platforms that
    support fallback to software, because on repeated Graphite crashes the
    mode will devolve from HARDWARE_{GL, VULKAN} to the software modes.
    
    This CL makes this support explicit. It adds a new HARDWARE_GRAPHITE
    mode and uses it as following:
    
    * When Graphite is enabled, the initial mode is set as HARDWARE_GRAPHITE
    * In that case, we do not fall back to Ganesh - instead, we fall back
      directly to software
    * When the GPU mode is not Graphite, we explicitly compute the
      `grContextType` with Graphite disabled to ensure that the service
      side does not see Graphite as enabled if Graphite is enabled but we
      have fallen back from it
    
    The last item is not strictly necessary as of this CL as we only fall
    back from Graphite to software. However, it is conceptually correct
    and sets the foundation for a followup CL that will introduce fallback
    from Graphite to Ganesh-GL for platforms that don't support fallback to
    software.
    
    In this CL there is no functional change.
    
    Followup CLs will do the following:
    * Rework metrics to both take Graphite into account and give more
      visibility on fallbacks overall
    * Add Graphite->Ganesh-GL fallback on Android and ChromeOS
    * Extend the GpuCrash_InfoForHardwareGpu test to verify fallback from
      Graphite
    
    Bug: 1507801
    Change-Id: I098bfaebfaf5b7ad40dc2f6785047b8329dba7e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5257657
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256731}

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -152,6 +152,7 @@ const char* GetProcessLifetimeUmaName(gpu::GpuMode gpu_mode) {
       NOTREACHED();
       return nullptr;
     case gpu::GpuMode::HARDWARE_GL:
+    case gpu::GpuMode::HARDWARE_GRAPHITE:
     case gpu::GpuMode::HARDWARE_VULKAN:
       return kProcessLifetimeEventsHardwareAccelerated;
     case gpu::GpuMode::SWIFTSHADER:
