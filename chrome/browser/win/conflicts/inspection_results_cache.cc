8da12c0989feeb568bec5ec04efc3c10078ab908
Lukasz Anforowicz <lukasza@chromium.org>
2024-02-06 17:26:27 +0000

    Use `span`-based `base::MD5Sum`: //chrome/browser.
    This CL was uploaded by git cl split.
    
    R=dmurph@chromium.org, gujen@google.com, pmonette@chromium.org, rdevlin.cronin@chromium.org
    
    Bug: 1490484
    Change-Id: I2d2d84db7b88660bbee3c94ed19a4c0794929bf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5270836
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256832}

--- chrome/browser/win/conflicts/inspection_results_cache.cc
+++ chrome/browser/win/conflicts/inspection_results_cache.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
+#include "base/containers/span.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/files/important_file_writer.h"
@@ -120,7 +121,7 @@ base::Pickle SerializeInspectionResultsCache(
 
   // Append the md5 digest of the data to detect serializations errors.
   base::MD5Digest md5_digest;
-  base::MD5Sum(pickle.payload(), pickle.payload_size(), &md5_digest);
+  base::MD5Sum(pickle.payload_bytes(), &md5_digest);
   pickle.WriteBytes(&md5_digest, sizeof(md5_digest));
 
   return pickle;
@@ -168,8 +169,8 @@ ReadCacheResult DeserializeInspectionResultsCache(
 
   // Check if the md5 checksum matches.
   base::MD5Digest md5_digest;
-  base::MD5Sum(pickle.payload(), pickle.payload_size() - sizeof(md5_digest),
-               &md5_digest);
+  base::span<const uint8_t> payload = pickle.payload_bytes();
+  base::MD5Sum(payload.first(payload.size() - sizeof(md5_digest)), &md5_digest);
   if (!base::ranges::equal(read_md5_digest->a, md5_digest.a))
     return ReadCacheResult::kFailInvalidMD5;
 
