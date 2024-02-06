c59b441a731987f20a45dcff488a10cba5f7edea
Emily Shack <emshack@chromium.org>
2024-02-06 17:32:55 +0000

    Tab Organization: Observe settings from bubble host
    Makes the tab search bubble host observe pref changes. Fixes a bug where
    the bubble wasn't showing up if the feature was originally disabled, and
    was then enabled without restart.
    
    Also includes some cleanup to convert the tab search page handler to the
    same observer vs. observing prefs directly
    
    Bug: b/323827944
    Change-Id: Ie8526777874d5bd2ae40748a2742eb7309397986
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5269278
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256836}

--- chrome/browser/ui/views/tab_search_bubble_host.cc
+++ chrome/browser/ui/views/tab_search_bubble_host.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/feature_engagement/tracker_factory.h"
+#include "chrome/browser/optimization_guide/optimization_guide_keyed_service.h"
+#include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/browser_finder.h"
@@ -60,7 +62,10 @@ TabSearchOpenAction GetActionForEvent(const ui::Event& event) {
 
 TabSearchBubbleHost::TabSearchBubbleHost(views::Button* button,
                                          Profile* profile)
-    : button_(button),
+    : optimization_guide::SettingsEnabledObserver(
+          optimization_guide::proto::ModelExecutionFeature::
+              MODEL_EXECUTION_FEATURE_TAB_ORGANIZATION),
+      button_(button),
       profile_(profile),
       webui_bubble_manager_(button,
                             profile,
@@ -76,6 +81,12 @@ TabSearchBubbleHost::TabSearchBubbleHost(views::Button* button,
     if (tab_organization_service) {
       tab_organization_service->AddObserver(this);
     }
+  }
+  OptimizationGuideKeyedService* optimization_guide_keyed_service =
+      OptimizationGuideKeyedServiceFactory::GetForProfile(profile);
+  if (optimization_guide_keyed_service) {
+    optimization_guide_keyed_service->AddModelExecutionSettingsEnabledObserver(
+        this);
   }
   auto menu_button_controller = std::make_unique<views::MenuButtonController>(
       button,
@@ -93,6 +104,12 @@ TabSearchBubbleHost::~TabSearchBubbleHost() {
     if (tab_organization_service) {
       tab_organization_service->RemoveObserver(this);
     }
+  }
+  OptimizationGuideKeyedService* optimization_guide_keyed_service =
+      OptimizationGuideKeyedServiceFactory::GetForProfile(profile_);
+  if (optimization_guide_keyed_service) {
+    optimization_guide_keyed_service
+        ->RemoveModelExecutionSettingsEnabledObserver(this);
   }
 }
 
@@ -145,6 +162,21 @@ void TabSearchBubbleHost::OnUserInvokedFeature(const Browser* browser) {
   if (browser == GetBrowser()) {
     const int tab_organization_tab_index = 1;
     ShowTabSearchBubble(false, tab_organization_tab_index);
+  }
+}
+
+void TabSearchBubbleHost::OnChangeInFeatureCurrentlyEnabledState(
+    bool is_now_enabled) {
+  auto* const tab_organization_service =
+      TabOrganizationServiceFactory::GetForProfile(profile_);
+  if (!tab_organization_service) {
+    return;
+  }
+
+  if (is_now_enabled) {
+    tab_organization_service->AddObserver(this);
+  } else {
+    tab_organization_service->RemoveObserver(this);
   }
 }
 
