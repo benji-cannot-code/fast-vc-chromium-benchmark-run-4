eb027867f2a983b263bd4b53210869b667e7f355
Michael Lippautz <mlippautz@chromium.org>
2024-02-06 12:37:39 +0000

    css: Cache tracing flag in static local
    Bug: chromium:41497266
    Change-Id: Ic547cc01eb1a5ea4d44c292c0a1d35f11f25ab74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5268549
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256702}

--- third_party/blink/renderer/core/css/invalidation/pending_invalidations.cc
+++ third_party/blink/renderer/core/css/invalidation/pending_invalidations.cc
@@ -19,10 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-PendingInvalidations::PendingInvalidations() {
-  InvalidationSet::CacheTracingFlag();
-}
-
 void PendingInvalidations::ScheduleInvalidationSetsForNode(
     const InvalidationLists& invalidation_lists,
     ContainerNode& node) {
