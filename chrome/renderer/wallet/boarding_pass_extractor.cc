d242ea7f0a5576358240b4c987dc8fb004ede417
Hanfeng Zhu <hanfeng@google.com>
2024-02-06 00:27:52 +0000

    Record boarding pass detection results to UMA
    Bug: 1523867
    Change-Id: If61c2c22dfe3683002d3b2b87376608a138e6f02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5256201
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Hanfeng Zhu <hanfeng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1256509}

--- chrome/renderer/wallet/boarding_pass_extractor.cc
+++ chrome/renderer/wallet/boarding_pass_extractor.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/renderer/wallet/boarding_pass_extractor.h"
 
+#include "base/metrics/histogram_macros.h"
 #include "chrome/common/chrome_isolated_world_ids.h"
 #include "content/public/renderer/render_frame.h"
 #include "content/public/renderer/render_thread.h"
@@ -170,6 +171,9 @@ void BoardingPassExtractor::OnBoardingPassExtracted(
       render_frame()->GetWebFrame()->GetDocument().GetUkmSourceId())
       .SetDetected(!boarding_passes.empty())
       .Record(ukm_recorder_.get());
+
+  UMA_HISTOGRAM_BOOLEAN("Android.Wallet.BoardingPass.Detected",
+                        !boarding_passes.empty());
 
   std::move(callback).Run(std::move(boarding_passes));
 }
