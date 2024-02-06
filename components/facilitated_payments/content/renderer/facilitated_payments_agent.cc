c342cbf3d1a1fb22159db2c37ba6aefeffdbe19f
Rouslan Solomakhin <rouslan@chromium.org>
2024-02-06 17:54:32 +0000

    [PIX] Code validation
    Before this patch, a page was considered to contain a PIX code if it
    contained a PIX indicator string "0014br.gov.bcb.pix".
    
    This patch adds a PIX code format validator for the renderer to run
    before returning a boolean result for "is PIX code found" to the
    browser.
    
    After this patch, a page is considered to contain a PIX code only if the
    found PIX code passes the rudimentary validity checks.
    
    Change-Id: I02209ae592a7ceab73e576027b6d386b6a624713
    Bug: 1475426
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5228180
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Auto-Submit: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Siddharth Shah <siashah@chromium.org>
    Reviewed-by: Ali Hijazi <ahijazi@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256856}

--- components/facilitated_payments/content/renderer/facilitated_payments_agent.cc
+++ components/facilitated_payments/content/renderer/facilitated_payments_agent.cc
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/location.h"
 #include "base/strings/string_util.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/task/single_thread_task_runner.h"
+#include "components/facilitated_payments/core/util/pix_code_validator.h"
 #include "content/public/renderer/render_frame.h"
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_registry.h"
 #include "third_party/blink/public/platform/web_string.h"
@@ -40,10 +42,10 @@ void FacilitatedPaymentsAgent::TriggerPixCodeDetection(
   blink::WebString result =
       render_frame()->GetWebFrame()->GetDocument().FindTextInElementWith(
           blink::WebString(kPixCodeIdentifier));
-  std::u16string trimmed = std::u16string(
+  std::string trimmed = base::UTF16ToUTF8(
       base::TrimWhitespace(result.Utf16(), base::TrimPositions::TRIM_ALL));
 
-  std::move(callback).Run(/*found_pix_code=*/!trimmed.empty());
+  std::move(callback).Run(/*found_pix_code=*/IsValidPixCode(trimmed));
 }
 
 void FacilitatedPaymentsAgent::OnDestruct() {
--- components/facilitated_payments/content/renderer/facilitated_payments_agent_browsertest.cc
+++ components/facilitated_payments/content/renderer/facilitated_payments_agent_browsertest.cc
@@ -51,7 +51,7 @@ TEST_F(FacilitatedPaymentsAgentTest, TriggerPixCodeDetection_Found) {
   EXPECT_TRUE(IsPixCodeFound(CreateAgentFor(R"(
    <body>
     <div>
-      ABC0014br.gov.bcb.pixXYZ
+      00020126180014br.gov.bcb.pix63041D3D
     </div>
   </form>
   )").get()));
@@ -62,7 +62,7 @@ TEST_F(FacilitatedPaymentsAgentTest,
   std::unique_ptr<FacilitatedPaymentsAgent> agent = CreateAgentFor(R"(
    <body>
     <div>
-      ABC0014br.gov.bcb.pixXYZ
+      00020126180014br.gov.bcb.pix63041D3D
     </div>
   </form>
   )");
