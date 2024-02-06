ccb6371a83cfa3df74d624d5bd00ed4e5e6c346c
Daniel Rubery <drubery@chromium.org>
2024-02-06 18:01:59 +0000

    Create info class for download bubble security view
    The CL moves functionality from BubbleUIInfo into a dedicated class
    for providing information to the DownloadBubbleSecurityView. The new
    structure with multiple classes will make it easier to split the
    download bubble into multiple views in the future.
    
    Bug: 1482901
    Change-Id: Iedb716925caaab3ee43fc605ce82f7dc2d0c85fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5235088
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256861}

--- chrome/browser/ui/download/download_bubble_contents_view_info.cc
+++ chrome/browser/ui/download/download_bubble_contents_view_info.cc
@@ -5,8 +5,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/download/download_bubble_contents_view_info.h"
 
+#include "chrome/browser/ui/download/download_bubble_row_view_info.h"
+
 DownloadBubbleContentsViewInfo::DownloadBubbleContentsViewInfo(
     std::vector<DownloadUIModel::DownloadUIModelPtr> models)
-    : row_list_view_info_(std::move(models)) {}
+    : row_list_view_info_(std::move(models)), security_view_info_() {}
 
 DownloadBubbleContentsViewInfo::~DownloadBubbleContentsViewInfo() = default;
+
+DownloadUIModel* DownloadBubbleContentsViewInfo::GetDownloadModel(
+    const ContentId& id) const {
+  if (const DownloadBubbleRowViewInfo* row_info =
+          row_list_view_info_.GetRowInfo(id);
+      row_info) {
+    return row_info->model();
+  }
+
+  return nullptr;
+}
+
+void DownloadBubbleContentsViewInfo::InitializeSecurityView(
+    const ContentId& id) {
+  CHECK(id != ContentId());
+  if (security_view_info_.content_id() == id) {
+    return;
+  }
+  DownloadUIModel* model = GetDownloadModel(id);
+  CHECK(model);
+  security_view_info_.InitializeForDownload(*model);
+}
+
+void DownloadBubbleContentsViewInfo::ResetSecurityView() {
+  security_view_info_.Reset();
+}
--- chrome/browser/ui/download/download_bubble_contents_view_info.h
+++ chrome/browser/ui/download/download_bubble_contents_view_info.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/download/download_bubble_info.h"
 #include "chrome/browser/ui/download/download_bubble_row_list_view_info.h"
+#include "chrome/browser/ui/download/download_bubble_security_view_info.h"
 
 // Info class for DownloadBubbleContentsView
 class DownloadBubbleContentsViewInfo
@@ -21,8 +22,20 @@ class DownloadBubbleContentsViewInfo
     return row_list_view_info_;
   }
 
+  const DownloadBubbleSecurityViewInfo& security_view_info() const {
+    return security_view_info_;
+  }
+
+  // Get the `DownloadUIModel` for any `id` among those listed within
+  // the row list view.
+  DownloadUIModel* GetDownloadModel(const ContentId& id) const;
+
+  void InitializeSecurityView(const ContentId& id);
+  void ResetSecurityView();
+
  private:
   DownloadBubbleRowListViewInfo row_list_view_info_;
+  DownloadBubbleSecurityViewInfo security_view_info_;
 };
 
 #endif  // CHROME_BROWSER_UI_DOWNLOAD_DOWNLOAD_BUBBLE_CONTENTS_VIEW_INFO_H_
