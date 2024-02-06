73c30b7d7d43fd87e05df93bb9516c27349a5a9e
Yuwei Huang <yuweih@chromium.org>
2024-02-06 22:23:24 +0000

    Reland "[remoting host] Create SessionAuthz playground binary"
    This is a reland of commit a803632d1b1bc96d7d2445a4f9a9d3b350438716
    
    Change since original CL:
    Disable executable from unsupported platforms (e.g. Wayland &
    Lacros).
    
    Original change's description:
    > [remoting host] Create SessionAuthz playground binary
    >
    > This CL creates a SessionAuthz playground binary. It allows for testing
    > the SessionAuthz API with the host's robot account authority.
    >
    > Bug: b/323068262
    > Change-Id: Idf4d6c4b7b82d1af55ee1aa15478ed35b0bb7394
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5263241
    > Reviewed-by: Joe Downing <joedow@chromium.org>
    > Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1256878}
    
    Bug: b/323068262
    Change-Id: I0dcf12e9e5202f310268bb400980f37622877f98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5274561
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Auto-Submit: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257035}

---
+++ remoting/test/session_authz_playground_main.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/at_exit.h"
+#include "base/command_line.h"
+#include "base/task/single_thread_task_executor.h"
+#include "base/task/thread_pool/thread_pool_instance.h"
+#include "mojo/core/embedder/embedder.h"
+#include "remoting/test/session_authz_playground.h"
+
+int main(int argc, char const* argv[]) {
+  base::AtExitManager exitManager;
+  base::CommandLine::Init(argc, argv);
+  base::SingleThreadTaskExecutor io_task_executor(base::MessagePumpType::IO);
+  base::ThreadPoolInstance::CreateAndStartWithDefaultParams(
+      "SessionAuthzPlayground");
+  mojo::core::Init();
+
+  remoting::SessionAuthzPlayground().Start();
+  return 0;
+}
