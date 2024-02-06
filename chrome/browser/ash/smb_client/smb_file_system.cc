a57568eb3b2745afb4ba9a1d2e6c741e0bef481a
Ben Reich <benreich@chromium.org>
2024-02-06 06:03:46 +0000

    [Files app] Fix modernize-use-equals-default in c/b/a/smb_client
    Mechanical changes using:
    
      find chrome/browser/ash/smb_client \
          -type f -name "*.cc" -o -name "*.h" | \
          xargs third_party/llvm-build/Release+Asserts/bin/clang-tidy \
              -checks='-*,modernize-use-equals-default' -fix
    
    Bug: None
    Change-Id: I7ab1aa96988b8471d68868b82cfec083497f0a8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5269065
    Reviewed-by: François Degros <fdegros@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256622}

--- chrome/browser/ash/smb_client/smb_file_system.cc
+++ chrome/browser/ash/smb_client/smb_file_system.cc
@@ -20,7 +20,7 @@ SmbFileSystem::SmbFileSystem(
     const file_system_provider::ProvidedFileSystemInfo& file_system_info)
     : file_system_info_(file_system_info) {}
 
-SmbFileSystem::~SmbFileSystem() {}
+SmbFileSystem::~SmbFileSystem() = default;
 
 AbortCallback SmbFileSystem::RequestUnmount(
     storage::AsyncFileUtil::StatusCallback callback) {
