8e0271bb459fef1e71412d9f8c2f2b401b88ad92
Jakub Flis <jaflis@google.com>
2024-02-07 09:23:21 +0000

    [OOBE] Migrate web_view_loader.js to TS
    Bug: b:314762562
    Change-Id: Ic351b94cc3a875153a887e2394514451e1282cc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5242740
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Jakub Flis <jaflis@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257246}

--- tools/typescript/definitions/webview_tag.d.ts
+++ tools/typescript/definitions/webview_tag.d.ts
@@ -269,6 +269,8 @@ declare global {
         removeContentScripts(scriptNameList?: string[]): void;
         executeScript(
             details: InjectDetails, callback?: (results: any[]) => void): void;
+        insertCSS(details: InjectDetails,
+            callback?: (results: any[]) => void): void;
         terminate(): void;
       }
 
