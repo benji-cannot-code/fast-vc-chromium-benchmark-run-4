ac2ccc0e4e4e2d8501f93625db1d47d9d11a2e4b
Wei Lee <wtlee@google.com>
2024-02-07 04:41:13 +0000

    network: Allow showing password if it is prefilled
    This CL allows each element to specify an extra handling logic when
    prefilled value is given. And moving the behavior to set the element
    "disabled" to be the extra handling for NetworkConfigSelect element
    instead of a default behavior.
    
    It is for making the "Show password" button still work in the
    NetworkPasswordInput element when a prefilled value is given to address
    a suggestion from the privacy review.
    
    Bug: b:271589428
    Test: Browser tests
    Change-Id: I0ce5699d22c3debd35f45b2aca2237bfd283cbdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5256797
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Reviewed-by: Dorah Kim <dorahkim@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257188}

--- chrome/test/data/webui/cr_components/chromeos/network/network_config_element_behavior_test.js
+++ chrome/test/data/webui/cr_components/chromeos/network/network_config_element_behavior_test.js
@@ -110,11 +110,9 @@ suite('CrComponentsNetworkConfigElementBehaviorTest', function() {
       config.prefilledValue = prefilledValue;
       if (shouldBeValid) {
         assertTrue(config.readonly);
-        assertTrue(config.disabled);
         assertEquals(config.value, config.prefilledValue);
       } else {
         assertFalse(config.readonly);
-        assertFalse(config.disabled);
         assertNotEquals(config.value, config.prefilledValue);
       }
     }
--- chrome/test/data/webui/cr_components/chromeos/network/network_config_select_test.js
+++ chrome/test/data/webui/cr_components/chromeos/network/network_config_select_test.js
@@ -105,8 +105,10 @@ suite('NetworkConfigSelectTest', function() {
       configSelect.prefilledValue = prefilledValue;
       if (shouldBeValid) {
         assertEquals(configSelect.value, configSelect.prefilledValue);
+        assertTrue(configSelect.disabled);
       } else {
         assertNotEquals(configSelect.value, configSelect.prefilledValue);
+        assertFalse(configSelect.disabled);
       }
     }
   });
