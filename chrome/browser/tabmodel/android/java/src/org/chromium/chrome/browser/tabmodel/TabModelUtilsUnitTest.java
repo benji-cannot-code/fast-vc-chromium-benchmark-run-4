9dca21de5308dde3a308fcc36faf64750ac1bc45
Brandon Fong <bjfong@google.com>
2024-02-07 19:16:43 +0000

    [Tab Group Parity] Add observer helpers for tab state initialization
    Add a helper class to handle attaching observers based on initialization states, and removing observers.
    
    Bug: 1517346
    Change-Id: I466e2673e8811d99be559e0e8d2b301a80bb13a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5263560
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1257487}

--- chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelUtilsUnitTest.java
+++ chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelUtilsUnitTest.java
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.tabmodel;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import androidx.test.filters.SmallTest;
@@ -18,6 +21,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import org.chromium.base.Callback;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tab.Tab;
@@ -40,6 +44,7 @@ public class TabModelUtilsUnitTest {
     @Mock private Profile mIncognitoProfile;
     @Mock private Tab mTab;
     @Mock private Tab mIncognitoTab;
+    @Mock private Callback<TabModelSelector> mTabModelSelectorCallback;
 
     private MockTabModelSelector mTabModelSelector;
     private MockTabModel mTabModel;
@@ -101,5 +106,22 @@ public class TabModelUtilsUnitTest {
         TabModelUtils.selectTabById(
                 mTabModelSelector, UNUSED_TAB_ID, TabSelectionType.FROM_USER, false);
         assertEquals(TabList.INVALID_TAB_INDEX, mTabModel.index());
+    }
+
+    @Test
+    @SmallTest
+    public void testRunOnTabStateInitializedCallback() {
+        mTabModelSelector.markTabStateInitialized();
+        TabModelUtils.runOnTabStateInitialized(mTabModelSelector, mTabModelSelectorCallback);
+        verify(mTabModelSelectorCallback, times(1)).onResult(mTabModelSelector);
+    }
+
+    @Test
+    @SmallTest
+    public void testRunOnTabStateInitializedObserver() {
+        TabModelUtils.runOnTabStateInitialized(mTabModelSelector, mTabModelSelectorCallback);
+        verify(mTabModelSelectorCallback, never()).onResult(mTabModelSelector);
+        mTabModelSelector.markTabStateInitialized();
+        verify(mTabModelSelectorCallback, times(1)).onResult(mTabModelSelector);
     }
 }
