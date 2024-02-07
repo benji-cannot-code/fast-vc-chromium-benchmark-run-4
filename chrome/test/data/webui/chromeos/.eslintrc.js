5d19f7c7bb59a081a2ea1f353509a6a5fa4d2e66
rbpotter <rbpotter@chromium.org>
2024-02-07 23:21:20 +0000

    WebUI: Turn off type imports in chromeos folder
    Bug: 1494527
    Change-Id: Iec97c95a3f8c31b04aff76c00cbda83694008a1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5277827
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257645}

--- chrome/test/data/webui/chromeos/.eslintrc.js
+++ chrome/test/data/webui/chromeos/.eslintrc.js
@@ -15,6 +15,10 @@ module.exports = {
       // Turn off until all violations under this folder are fixed. This was
       // done for other parts of the codebase in http://crbug.com/1521107
       'no-restricted-syntax': 'off',
+
+      // Turn off until all violations under this folder are fixed. This was
+      // done for other parts of the codebase in http://crbug.com/1494527
+      '@typescript-eslint/consistent-type-imports' : 'off',
     },
   }],
   // clang-format on
