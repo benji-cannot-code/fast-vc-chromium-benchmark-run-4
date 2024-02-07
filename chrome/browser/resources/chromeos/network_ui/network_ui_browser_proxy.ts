28122958460c719c6e114a166da0659fc1d7a186
Chad Duffin <chadduffin@chromium.org>
2024-02-07 22:59:19 +0000

    webui: Fix misc. issues with network_ui
    This change fixes miscellaneous issues with network_ui:
    * Removes the event listener on destruction
    * Replaces .then() usage with async/await
    
    Bug: b/314832958
    Test: Manually verified on device.
    Change-Id: I7e420c0b553d543c885721becf7c59cf5ff65a41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5250103
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257635}

--- chrome/browser/resources/chromeos/network_ui/network_ui_browser_proxy.ts
+++ chrome/browser/resources/chromeos/network_ui/network_ui_browser_proxy.ts
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {sendWithPromise} from 'chrome://resources/js/cr.js';
 
 export interface TetheringStatus {
-  [key: string]: string;
+  state?: string;
 }
 
 export interface NetworkUiBrowserProxy {
