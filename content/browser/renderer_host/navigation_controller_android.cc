9ed912474aaf2cf27854fa1e5520479aefa9ed0a
Min Qin <qinmin@chromium.org>
2024-02-07 21:23:50 +0000

    Add isPdf to LoadUrlParams.java
    isPdf exists in native LoadUrlParams class, but it is missing in the
    java side. This CL adds the java side field so it can be used later.
    
    Bug: chromium:324264020
    Change-Id: Ia19503a9b5212e615014ed9fc6a936fa4b6883a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5274130
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257576}

--- content/browser/renderer_host/navigation_controller_android.cc
+++ content/browser/renderer_host/navigation_controller_android.cc
@@ -258,7 +258,8 @@ base::android::ScopedJavaLocalRef<jobject> NavigationControllerAndroid::LoadUrl(
     jboolean should_clear_history_list,
     const base::android::JavaParamRef<jobject>& j_additional_navigation_params,
     jlong input_start,
-    jlong navigation_ui_data_ptr) {
+    jlong navigation_ui_data_ptr,
+    jboolean is_pdf) {
   DCHECK(url);
   NavigationController::LoadURLParams params(
       GURL(ConvertJavaStringToUTF8(env, url)));
@@ -276,6 +277,7 @@ base::android::ScopedJavaLocalRef<jobject> NavigationControllerAndroid::LoadUrl(
   params.should_replace_current_entry = should_replace_current_entry;
   params.has_user_gesture = has_user_gesture;
   params.should_clear_history_list = should_clear_history_list;
+  params.is_pdf = is_pdf;
 
   if (j_additional_navigation_params) {
     params.initiator_frame_token =
