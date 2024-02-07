b5f9724352762dacb868d109c2874f55e298e3fa
sisidovski <sisidovski@chromium.org>
2024-02-07 03:22:04 +0000

    Add ServiceWorker.RouterEvaluator.EvaluationTime
    This CL adds the histogram to record the time spent for the router
    evaluation. This UMA is useful as it gives us the insight for the router
    performance.
    
    Bug: 40241479
    Change-Id: I3409417c492c05d9a5baf99dab6b2839286498d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5267799
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257167}

--- content/common/service_worker/service_worker_router_evaluator.cc
+++ content/common/service_worker/service_worker_router_evaluator.cc
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/safe_conversions.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
+#include "base/time/time.h"
+#include "base/timer/elapsed_timer.h"
 #include "services/network/public/cpp/request_destination.h"
 #include "services/network/public/cpp/request_mode.h"
 #include "third_party/blink/public/common/service_worker/service_worker_router_rule.h"
@@ -55,6 +57,11 @@ void RecordMatchedSourceType(
     const std::vector<blink::ServiceWorkerRouterSource>& sources) {
   base::UmaHistogramEnumeration(
       "ServiceWorker.RouterEvaluator.MatchedFirstSourceType", sources[0].type);
+}
+
+void RecordEvaluationTime(base::TimeDelta duration) {
+  base::UmaHistogramMicrosecondsTimes(
+      "ServiceWorker.RouterEvaluator.EvaluationTime", duration);
 }
 
 // TODO(crbug.com/1371756): consolidate code with blink::url_pattern.
@@ -625,9 +632,11 @@ ServiceWorkerRouterEvaluator::EvaluateInternal(
     const network::ResourceRequest& request,
     std::optional<blink::EmbeddedWorkerStatus> running_status) const {
   CHECK(is_valid_);
+  base::ElapsedTimer timer;
   for (const auto& rule : compiled_rules_) {
     if (rule->Match(request, running_status)) {
       VLOG(3) << "matched request url=" << request.url;
+      RecordEvaluationTime(timer.Elapsed());
       RecordMatchedSourceType(rule->sources());
       ServiceWorkerRouterEvaluator::Result result;
       result.id = rule->id();
@@ -636,6 +645,7 @@ ServiceWorkerRouterEvaluator::EvaluateInternal(
     }
   }
   VLOG(3) << "not matched request url=" << request.url;
+  RecordEvaluationTime(timer.Elapsed());
   return std::nullopt;
 }
 
