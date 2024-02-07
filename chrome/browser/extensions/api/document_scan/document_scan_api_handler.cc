abf17174785207475530851724698e528ebde5a9
Benjamin Gordon <bmgordon@chromium.org>
2024-02-07 00:34:57 +0000

    documentScan: Reduce StartScan confirmation prompts
    The first time an extension calls StartScan for a particular scanner
    handle, Chrome verifies user approval through a confirmation dialog.
    Subsequent StartScan requests on the same handle are automatically
    approved so that the user doesn't have to approve every page of a
    multi-page scan.
    
    This reasoning can be extended further by automatically approving
    subsequent user-initiated scan requests on the same scanner (same ID,
    not by handle).  The extension could already hold open the handle after
    the first scan indefinitely, so this doesn't create any new access.
    However, it reduces the number of times the user has to approve the same
    scanner in cases where a new handle is needed (e.g. after refreshing the
    list of scanners).
    
    Approval is not saved permanently, so the user will have to approve the
    scanner again after a logout or reboot.
    
    Multiple scanner IDs may point to the same physical scanner in the case
    of multiple protocols.  These other scanner IDs are not automatically
    approved so that the extension can't silently switch to a less secure
    protocol without the user knowing.
    
    Bug: b/312740272
    Test: Scan with test extensions
    Change-Id: I206219269cde58fab4227db03490c2ccb61cd73d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5264221
    Reviewed-by: Piotr Pawliczek <pawliczek@chromium.org>
    Commit-Queue: Benjamin Gordon <bmgordon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257101}

--- chrome/browser/extensions/api/document_scan/document_scan_api_handler.cc
+++ chrome/browser/extensions/api/document_scan/document_scan_api_handler.cc
@@ -235,6 +235,7 @@ void DocumentScanAPIHandler::OnScannerListReceived(
   state.active_scanner_ids.clear();
   state.scanner_handles.clear();
   state.active_job_handles.clear();
+  state.approved_scanner_handles.clear();
   for (auto& scanner : api_response.scanners) {
     state.active_scanner_ids[scanner.scanner_id] = {.name = scanner.name};
   }
@@ -295,6 +296,7 @@ void DocumentScanAPIHandler::OnOpenScannerResponse(
       std::erase_if(state.active_job_handles, [&it](const auto& item) {
         return item.second == it->first;
       });
+      state.approved_scanner_handles.erase(it->first);
       it = state.scanner_handles.erase(it);
     } else {
       ++it;
@@ -372,6 +374,7 @@ void DocumentScanAPIHandler::OnCloseScannerResponse(
     return item.second == scanner_handle;
   });
   state.scanner_handles.erase(scanner_handle);
+  state.approved_scanner_handles.erase(scanner_handle);
 
   std::move(callback).Run(
       response.To<api::document_scan::CloseScannerResponse>());
@@ -493,6 +496,7 @@ void DocumentScanAPIHandler::OnSetOptionsResponse(
 void DocumentScanAPIHandler::StartScan(
     gfx::NativeWindow native_window,
     scoped_refptr<const Extension> extension,
+    bool user_gesture,
     const std::string& scanner_handle,
     api::document_scan::StartScanOptions options,
     StartScanCallback callback) {
@@ -512,11 +516,13 @@ void DocumentScanAPIHandler::StartScan(
   auto start_runner = std::make_unique<StartScanRunner>(
       native_window, browser_context_, std::move(extension), document_scan_);
 
+  bool approved = state.approved_scanner_handles.contains(scanner_handle) ||
+                  (user_gesture && state.approved_scanner_ids.contains(
+                                       state.scanner_handles[scanner_handle]));
   StartScanRunner* raw_runner = start_runner.get();
   raw_runner->Start(
-      state.approved_scanners.contains(scanner_handle),
-      state.active_scanner_ids[handle_it->second].name, scanner_handle,
-      crosapi::mojom::StartScanOptions::From(options),
+      approved, state.active_scanner_ids[handle_it->second].name,
+      scanner_handle, crosapi::mojom::StartScanOptions::From(options),
       base::BindOnce(&DocumentScanAPIHandler::OnStartScanResponse,
                      weak_ptr_factory_.GetWeakPtr(), std::move(start_runner),
                      std::move(callback)));
@@ -535,7 +541,9 @@ void DocumentScanAPIHandler::OnStartScanResponse(
     // If this scanner was approved by the user, keep track so it is not
     // prompted for again.
     if (runner->approved()) {
-      state.approved_scanners.insert(api_response.scanner_handle);
+      const std::string& handle = api_response.scanner_handle;
+      state.approved_scanner_handles.insert(handle);
+      state.approved_scanner_ids.insert(state.scanner_handles[handle]);
     }
 
     // Keep track of active job handles for this extension.
