88ca8160d858558fbd997ce8015e7c0c5a26e035
Camillia Smith Barnes <cammie@chromium.org>
2024-02-07 04:25:53 +0000

    Shared Storage: Enable WAL mode for database
    We add enable WAL mode for the sql::Database in SharedStorageDatabase,
    but gate this behind a new feature flag.
    
    Bug: 1516507,1218540
    Change-Id: I70af8c61da0798608e65974cda558e76b74f301a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5272465
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257184}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1999,6 +1999,10 @@ BASE_FEATURE(kSharedStorageAPIM123,
              "SharedStorageAPIM123",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+BASE_FEATURE(kSharedStorageAPIEnableWALForDatabase,
+             "SharedStorageAPIEnableWALForDatabase",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kSimulateClickOnAXFocus,
              "SimulateClickOnAXFocus",
              base::FEATURE_DISABLED_BY_DEFAULT);
