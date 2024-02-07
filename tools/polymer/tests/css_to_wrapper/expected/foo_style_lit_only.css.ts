5c524dbf000549a14a686b29b6a4c2e567a4a929
dpapad <dpapad@chromium.org>
2024-02-07 22:31:20 +0000

    WebUI: Auto-generate Lit shared styles from equivalent Polymer styles.
    Update css_to_wrapper() to handle any files ending with the "_lit.css"
    suffix in a special way, by extracting the CSS content from the
    equivalent Polymer file instead of the "*_lit.css" file itself.
    
    This way the Polymer file acts as a canonical source of truth, while
    still allowing both Lit and Polymer elements to use the same styles at
    runtime.
    
    This is done to avoid duplicating those styles during the Lit migration,
    and eliminate the chances for such styles to diverge. For example the
    ongoing CR2023 cleanup will not need to worry about updating duplicate
    styles.
    
    Bug: 40943652
    Change-Id: Ia5f698bd8a03475c77181cf00620e465a21b775e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5274028
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257614}

--- tools/polymer/tests/css_to_wrapper/expected/foo_style_lit_only.css.ts
+++ tools/polymer/tests/css_to_wrapper/expected/foo_style_lit_only.css.ts
@@ -1,12 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {css} from 'chrome://resources/lit/v3_0/lit.rollup.js';
-
+import './other_vars.css.js';
 
 export function getCss() {
   return css`div {
-  font-size: 2rem;
-  --foo-bar: calc(var(--foo-bar1)
-      - var(--foo-bar2)
-      - 3 * var(--foo-bar3));
+  color: blue;
 }`;
 }
