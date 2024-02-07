673b910188b7fb80c1a81bcc10668f54acdbbd80
Lauren Winston <lwinston@google.com>
2024-02-07 17:54:48 +0000

    [Read Aloud] Bug fix for previous highlight not clearing on previous.
    When we navigate backwards, we should remove the current element
    from the list of previously highlighted elements. Otherwise,
    it will continue to have a previously highlighted format, even
    when it's after the currently read piece of text.
    
    This is based on UX guidance.
    
    After & Before: https://screenshot.googleplex.com/5gpbNAxGaTd33Wb.png
    
    Bug: 40927698
    Change-Id: Ic2cfbc10a2ac40186f4d430a5a9ef45fab3f23ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5275185
    Reviewed-by: Eitan Goldberger <eitang@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1257430}

--- chrome/browser/resources/side_panel/read_anything/app.ts
+++ chrome/browser/resources/side_panel/read_anything/app.ts
@@ -636,7 +636,7 @@ export class ReadAnythingElement extends ReadAnythingElementBase {
   //  previous granularity.
   playPreviousGranularity() {
     this.synth.cancel();
-    this.resetPreviousHighlight();
+    this.resetPreviousHighlightAndRemoveCurrentHighlight();
     chrome.readingMode.movePositionToPreviousGranularity();
 
     if (!this.highlightAndPlayMessage()) {
@@ -1019,6 +1019,15 @@ export class ReadAnythingElement extends ReadAnythingElementBase {
       default:
         return defaultSelectionColor;
     }
+  }
+
+  private resetPreviousHighlightAndRemoveCurrentHighlight() {
+    const lastElement = this.previousHighlight_.pop();
+    if (lastElement) {
+      lastElement.className = '';
+    }
+
+    this.resetPreviousHighlight();
   }
 
   private resetPreviousHighlight() {
