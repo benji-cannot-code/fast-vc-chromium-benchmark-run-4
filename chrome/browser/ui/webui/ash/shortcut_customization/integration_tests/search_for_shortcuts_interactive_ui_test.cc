b3f5a914259cc032d720e778c93a3cb5e34d8ea0
Longbo Wei <longbowei@google.com>
2024-02-07 00:48:53 +0000

    shortcuts: Update scrollbar position
    Before: http://go/scrcast/NTI4NTA1Mjk3NDg5MTAwOHwxNjI3NDE1OS1hMw
    After:  http://go/scrcast/NTE4MzIwMzMyODM5MzIxNnw4NGY2ZjZlZS0yMg
    
    Bug: b/321312655 b/216049298
    Test: DUT
    Change-Id: Ia06df28dbf590deb6a7f589695606b9d0f0adcc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5254203
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Longbo Wei <longbowei@google.com>
    Cr-Commit-Position: refs/heads/main@{#1257105}

--- chrome/browser/ui/webui/ash/shortcut_customization/integration_tests/search_for_shortcuts_interactive_ui_test.cc
+++ chrome/browser/ui/webui/ash/shortcut_customization/integration_tests/search_for_shortcuts_interactive_ui_test.cc
@@ -80,7 +80,7 @@ class SearchForShortcutsInteractiveUiTest
       "navigation-view-panel#navigationPanel",
       "#category-3",
       // Text editing subsection
-      "#container > accelerator-subsection:nth-child(2)",
+      "#contentWrapper > accelerator-subsection:nth-child(2)",
       "#rowList > accelerator-row:nth-child(10)",
   };
 
