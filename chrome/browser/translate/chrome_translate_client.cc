1799c72f8f36357f211ff588bed482bdde9178da
Trevor Perrier <perrier@google.com>
2024-02-07 20:46:37 +0000

    [TranslateMessages] Enable by default
    Bug: 40213244
    Change-Id: I5d8de044f2c6be6c6457848d8fd5f18606bb3ac3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5273432
    Reviewed-by: Megan Jablonski <megjablon@chromium.org>
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257558}

--- chrome/browser/translate/chrome_translate_client.cc
+++ chrome/browser/translate/chrome_translate_client.cc
@@ -96,7 +96,7 @@ TranslateEventProto::EventType BubbleResultToTranslateEvent(
 bool IsMessageUISnackbarEnabled() {
   constexpr base::FeatureParam<bool> kIsSnackbarEnabled(
       &translate::kTranslateMessageUI,
-      translate::kTranslateMessageUISnackbarParam, false);
+      translate::kTranslateMessageUISnackbarParam, true);
   return kIsSnackbarEnabled.Get();
 }
 
--- chrome/browser/translate/translate_manager_render_view_host_android_unittest.cc
+++ chrome/browser/translate/translate_manager_render_view_host_android_unittest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/infobars/content/content_infobar_manager.h"
 #include "components/infobars/core/infobar.h"
 #include "components/infobars/core/infobar_manager.h"
+#include "components/translate/content/android/translate_message.h"
 #include "components/translate/content/browser/content_translate_driver.h"
 #include "components/translate/content/common/translate.mojom.h"
 #include "components/translate/core/browser/translate_infobar_delegate.h"
@@ -136,6 +137,12 @@ TEST_F(TranslateManagerRenderViewHostAndroidTest,
   if (TranslateService::IsTranslateBubbleEnabled())
     return;
 
+  // TODO: https://crbug.com/40213244 - update this test to work with
+  // Translate Message UI when removing this feature flag.
+  if (base::FeatureList::IsEnabled(translate::kTranslateMessageUI)) {
+    return;
+  }
+
   GURL url("http://www.google.com");
   // We should not have a translate infobar.
   ASSERT_TRUE(GetTranslateInfoBar() == NULL);
@@ -155,6 +162,12 @@ TEST_F(TranslateManagerRenderViewHostAndroidTest,
   // languages moved out of the Infobar into the TranslateManager.
   if (TranslateService::IsTranslateBubbleEnabled())
     return;
+
+  // TODO: https://crbug.com/40213244 - update this test to work with
+  // Translate Message UI when removing this feature flag.
+  if (base::FeatureList::IsEnabled(translate::kTranslateMessageUI)) {
+    return;
+  }
 
   GURL url("http://www.google.com");
   // We should not have a translate infobar.
