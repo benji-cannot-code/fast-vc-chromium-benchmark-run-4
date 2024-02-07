3a4ef13644e14932d04aeecad7a73c23a370f371
Nigel Tao <nigeltao@chromium.org>
2024-02-07 05:26:11 +0000

    file_system_provider: add DiversionBackendDelegate FSURL is_valid checks
    Also add "DiversionBackendDelegate times out" test coverage.
    
    Bug: b:255703917
    Test: CQ
    Change-Id: Ie8bf505b799a006188ea3660c7e0e1fc1b55abd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5260440
    Reviewed-by: François Degros <fdegros@chromium.org>
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Auto-Submit: Nigel Tao <nigeltao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257195}

--- chrome/browser/ash/fileapi/diversion_backend_delegate.cc
+++ chrome/browser/ash/fileapi/diversion_backend_delegate.cc
@@ -123,7 +123,11 @@ void DiversionBackendDelegate::EnsureFileExists(
   DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
   storage::AsyncFileUtil* af_util = wrappee_->GetAsyncFileUtil(url.type());
 
-  if (!ShouldDivert(url)) {
+  if (!url.is_valid()) {
+    std::move(callback).Run(base::File::FILE_ERROR_INVALID_URL,
+                            /*created=*/false);
+    return;
+  } else if (!ShouldDivert(url)) {
     af_util->EnsureFileExists(std::move(context), url, std::move(callback));
     return;
   } else if (diversion_file_manager_->IsDiverting(url)) {
@@ -249,7 +253,10 @@ void DiversionBackendDelegate::CopyFileLocal(
     StatusCallback callback) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
 
-  if (src_url == dest_url) {
+  if (!src_url.is_valid() || !dest_url.is_valid()) {
+    std::move(callback).Run(base::File::FILE_ERROR_INVALID_URL);
+    return;
+  } else if (src_url == dest_url) {
     std::move(callback).Run(base::File::FILE_OK);
     return;
   }
@@ -284,7 +291,10 @@ void DiversionBackendDelegate::MoveFileLocal(
     StatusCallback callback) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
 
-  if (src_url == dest_url) {
+  if (!src_url.is_valid() || !dest_url.is_valid()) {
+    std::move(callback).Run(base::File::FILE_ERROR_INVALID_URL);
+    return;
+  } else if (src_url == dest_url) {
     std::move(callback).Run(base::File::FILE_OK);
     return;
   }
@@ -386,6 +396,11 @@ bool DiversionBackendDelegate::ShouldDivertForTesting(
 }
 
 // static
+base::TimeDelta DiversionBackendDelegate::IdleTimeoutForTesting() {
+  return kDiversionFileIdleTimeout;
+}
+
+// static
 void DiversionBackendDelegate::OnDiversionFinished(
     base::WeakPtr<DiversionBackendDelegate> weak_ptr,
     OnDiversionFinishedCallSite call_site,
@@ -398,6 +413,8 @@ void DiversionBackendDelegate::OnDiversionFinished(
     int64_t file_size,
     base::File::Error error) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
+  CHECK(dest_url.is_valid());
+  CHECK(src_url.is_valid());
 
   if (error != base::File::FILE_OK) {
     if (callback) {
