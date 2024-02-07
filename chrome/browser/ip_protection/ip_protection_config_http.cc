f6400b55017a4b98db650439c09e7318a6ac4354
Andrew Williams <awillia@chromium.org>
2024-02-07 19:30:54 +0000

    [IP Protection] Use GetNonStableAPIKey() when needed
    For the GetProxyList call where we send an API key, we
    should use GetNonStableAPIKey for non-stable releases
    (per
    https://groups.google.com/a/google.com/g/chrome-signin/c/aTrg5Ann_AI/m/ALvUDShKAQAJ)
    
    Bug: 1502566
    Change-Id: I7cf51486e854b7549ddf8db98d6c07adbfe9dc45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5271366
    Commit-Queue: Andrew Williams <awillia@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257505}

--- chrome/browser/ip_protection/ip_protection_config_http.cc
+++ chrome/browser/ip_protection/ip_protection_config_http.cc
@@ -12,8 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
+#include "base/version_info/channel.h"
 #include "build/branding_buildflags.h"
 #include "chrome/browser/ip_protection/get_proxy_config.pb.h"
+#include "chrome/common/channel_info.h"
 #include "google_apis/google_api_keys.h"
 #include "net/base/features.h"
 #include "services/network/public/cpp/resource_request.h"
@@ -94,6 +96,15 @@ const char kChromeIpBlinding[] = "chromeipblinding";
 // should be much smaller than this).
 const int kIpProtectionRequestMaxBodySize = 256 * 1024;
 const char kProtobufContentType[] = "application/x-protobuf";
+
+// TODO(https://crbug.com/1299886): Once `google_apis::GetAPIKey()` handles this
+// logic we can remove this helper.
+std::string GetAPIKey() {
+  return chrome::GetChannel() == version_info::Channel::STABLE
+             ? google_apis::GetAPIKey()
+             : google_apis::GetNonStableAPIKey();
+}
+
 }  // namespace
 
 IpProtectionConfigHttp::IpProtectionConfigHttp(
@@ -223,8 +234,7 @@ void IpProtectionConfigHttp::GetProxyConfig(
         net::HttpRequestHeaders::kAuthorization,
         base::StrCat({"Bearer ", oauth_token.value()}));
   } else {
-    resource_request->headers.SetHeader(kGoogApiKeyHeader,
-                                        google_apis::GetAPIKey());
+    resource_request->headers.SetHeader(kGoogApiKeyHeader, GetAPIKey());
   }
 
   std::unique_ptr<network::SimpleURLLoader> url_loader =
