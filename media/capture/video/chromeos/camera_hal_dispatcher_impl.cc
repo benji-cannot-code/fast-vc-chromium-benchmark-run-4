1de56804a0442c3c7598aedafc1ee3f43660d597
seannli <seannli@google.com>
2024-02-07 13:15:26 +0000

    camera: Add CameraEffectChange to CrosCameraServiceObserver
    Under the current architecture, only a setter can know the camera effect
    is on changed. This CL lets CrosCameraServiceObserver can also observe
    the camera effect events.
    
    Bug: b:315966244
    Test: Build Chrome successfully
    Change-Id: I015c2923702cdbfb7a431cf2e54f8ddd42337d54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5122591
    Reviewed-by: Kam Kwankajornkiet <kamchonlathorn@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Sean Li <seannli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1257306}

--- media/capture/video/chromeos/camera_hal_dispatcher_impl.cc
+++ media/capture/video/chromeos/camera_hal_dispatcher_impl.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2017 The Chromium Authors
+// Copyright 2024 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -533,6 +533,9 @@ void CameraHalDispatcherImpl::CameraSWPrivacySwitchStateChange(
   CAMERA_LOG(EVENT) << "Camera software privacy switch state changed: "
                     << state;
 }
+
+void CameraHalDispatcherImpl::CameraEffectChange(
+    cros::mojom::EffectsConfigPtr config) {}
 
 base::UnguessableToken CameraHalDispatcherImpl::GetTokenForTrustedClient(
     cros::mojom::CameraClientType type) {
--- media/capture/video/chromeos/camera_hal_dispatcher_impl.h
+++ media/capture/video/chromeos/camera_hal_dispatcher_impl.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2017 The Chromium Authors
+// Copyright 2024 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -217,7 +217,7 @@ class CAPTURE_EXPORT CameraHalDispatcherImpl final
       const base::UnguessableToken& auth_token,
       RegisterClientWithTokenCallback callback) final;
 
-  // CameraHalServerCallbacks implementations.
+  // CrosCameraServiceObserver implementations.
   void CameraDeviceActivityChange(int32_t camera_id,
                                   bool opened,
                                   cros::mojom::CameraClientType type) final;
@@ -226,6 +226,8 @@ class CAPTURE_EXPORT CameraHalDispatcherImpl final
       int32_t camera_id) final;
   void CameraSWPrivacySwitchStateChange(
       cros::mojom::CameraPrivacySwitchState state) final;
+
+  void CameraEffectChange(cros::mojom::EffectsConfigPtr config) final;
 
   base::UnguessableToken GetTokenForTrustedClient(
       cros::mojom::CameraClientType type);
