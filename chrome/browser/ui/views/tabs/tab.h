84767323036b4acd81cdb8f5f6e91d20ca5de3bb
Guoxing Zhao <charleszhao@chromium.org>
2024-02-07 04:51:43 +0000

    [AlertIndicatorButton] always appear for every capturing.
    This is to verify that AlertIndicatorButton appears for any capturing,
    camera or microphone.
    
    Bug: 1464249
    
    TEST="autoninja -C  out/Test chrome/test:browser_tests && out/Test/browser_tests --gtest_filter=AlertIndicatorButtonBrowserTest.*"
    
    Change-Id: I2fd3453f0c8ceb7d4e6a2cf8ba21fdd82afa2b8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4714348
    Commit-Queue: Charles Zhao <charleszhao@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257189}

--- chrome/browser/ui/views/tabs/tab.h
+++ chrome/browser/ui/views/tabs/tab.h
@@ -188,6 +188,10 @@ class Tab : public gfx::AnimationDelegate,
 
   TabIcon* GetTabIconForTesting() const { return icon_; }
 
+  AlertIndicatorButton* alert_indicator_button_for_testing() {
+    return alert_indicator_button_;
+  }
+
  private:
   class TabCloseButtonObserver;
   friend class AlertIndicatorButtonTest;
