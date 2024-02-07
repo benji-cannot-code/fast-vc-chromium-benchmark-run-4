abb0b51a5b3c2a544bf85b42b73b1f83243129dc
David Tseng <dtseng@google.com>
2024-02-07 01:06:48 +0000

    [linux-a11y]: fix UAF in AXEventRecorderAuralinux
    R=aleventhal@chromium.org
    
    Fixed: 41483350
    Change-Id: I871a0333050d2e490cc767fe9adbd64da0c4472a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5273251
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257109}

--- ui/accessibility/platform/ax_platform_tree_manager.cc
+++ ui/accessibility/platform/ax_platform_tree_manager.cc
@@ -7,6 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
+AXPlatformTreeManager::AXPlatformTreeManager(std::unique_ptr<AXTree> tree)
+    : AXTreeManager(std::move(tree)) {}
+
+AXPlatformTreeManager::~AXPlatformTreeManager() {}
+
 bool AXPlatformTreeManager::IsPlatformTreeManager() const {
   return true;
 }
