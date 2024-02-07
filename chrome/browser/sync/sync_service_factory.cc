9429a85ce26d6530d6d717c467ae845df220aa61
Glenn Hartmann <hartmanng@chromium.org>
2024-02-07 22:02:48 +0000

    Add WebApkSyncService dependency to SyncServiceFactory.
    Bug: 41491879
    Change-Id: I0d687f4b5e3b4ff95d8fd6c4248e83fd0d8bde3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5274408
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Auto-Submit: Glenn Hartmann <hartmanng@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1257599}

--- chrome/browser/sync/sync_service_factory.cc
+++ chrome/browser/sync/sync_service_factory.cc
@@ -98,6 +98,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/android/jni_headers/SyncServiceFactory_jni.h"
 #endif  // BUILDFLAG(IS_ANDROID)
 
+#if BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/android/webapk/webapk_sync_service_factory.h"
+#endif  // BUILDFLAG(IS_ANDROID)
+
 namespace {
 
 std::unique_ptr<KeyedService> BuildSyncService(
@@ -279,6 +283,11 @@ SyncServiceFactory::SyncServiceFactory()
   DependsOn(ThemeServiceFactory::GetInstance());
 #endif  // !BUILDFLAG(IS_ANDROID)
   DependsOn(TrustedVaultServiceFactory::GetInstance());
+#if BUILDFLAG(IS_ANDROID)
+  if (base::FeatureList::IsEnabled(syncer::kWebApkBackupAndRestoreBackend)) {
+    DependsOn(webapk::WebApkSyncServiceFactory::GetInstance());
+  }
+#endif  // BUILDFLAG(IS_ANDROID)
   DependsOn(WebDataServiceFactory::GetInstance());
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
