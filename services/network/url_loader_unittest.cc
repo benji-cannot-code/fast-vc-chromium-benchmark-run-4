1a10151a705ff9aab3199daf058eb98d5d96aa75
Rupert Ben Wiser <bewise@chromium.org>
2024-02-07 18:23:00 +0000

    Fix CHIPS for WebView when 3PCs are not enabled
    If an Android developer disables 3PCs in their app, paritioned cookies
    will not be returned. The root cause of this is that WebView uses its
    own settings for cookies from the rest of Chromium.
    
    The behaviour of Chromium is that partitioned cookies are still returned
    when 3PCs are disabled so we are matching that.
    
    Because permissions apply at the level of BrowserContext for the rest of
    Chromium, we have to do an awkward work around here where we
    optimistically retrieve all cookies, and then filter out the non
    partitioned cookies. This is because we have no way of telling the lower
    layers that this is the behaviour we want for _this_ WebView. There may
    be a slight performance impact here but hopefully not a large one since
    apps that have 3PCs disabled wouldn't be setting them in the first
    place. The real issue is more around apps changing this setting at
    runtime.
    
    Bug: 1523962
    Change-Id: Ic46a58651d16b85ff9287c52c4f4a89c39274107
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5250507
    Commit-Queue: Rupert Wiser <bewise@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257451}

--- services/network/url_loader_unittest.cc
+++ services/network/url_loader_unittest.cc
@@ -60,6 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cookies/cookie_access_result.h"
 #include "net/cookies/cookie_change_dispatcher.h"
 #include "net/cookies/cookie_inclusion_status.h"
+#include "net/cookies/cookie_partition_key.h"
 #include "net/cookies/cookie_setting_override.h"
 #include "net/cookies/cookie_util.h"
 #include "net/dns/mock_host_resolver.h"
@@ -4844,6 +4845,11 @@ TEST_F(URLLoaderTest, BlockAllCookies) {
       net::SiteForCookies::FromUrl(first_party_url);
   GURL third_party_url("http://www.some.other.origin.test/");
 
+  GURL cookie_url = test_server()->GetURL("/");
+  auto cc = net::CanonicalCookie::Create(
+      cookie_url, "a=b", base::Time::Now(), absl::nullopt /* server_time */,
+      net::CookiePartitionKey::FromURLForTesting(
+          GURL("https://toplevelsite.com")));
   ResourceRequest request = CreateResourceRequest("GET", first_party_url);
   base::RunLoop delete_run_loop;
   mojo::PendingRemote<mojom::URLLoader> loader;
@@ -4856,8 +4862,9 @@ TEST_F(URLLoaderTest, BlockAllCookies) {
       loader.InitWithNewPipeAndPassReceiver(), request,
       client()->CreateRemote());
 
-  EXPECT_FALSE(url_loader->AllowCookies(first_party_url, site_for_cookies));
-  EXPECT_FALSE(url_loader->AllowCookies(third_party_url, site_for_cookies));
+  EXPECT_FALSE(url_loader->AllowCookie(*cc, first_party_url, site_for_cookies));
+  EXPECT_FALSE(url_loader->AllowFullCookies(first_party_url, site_for_cookies));
+  EXPECT_FALSE(url_loader->AllowFullCookies(third_party_url, site_for_cookies));
 }
 
 TEST_F(URLLoaderTest, BlockOnlyThirdPartyCookies) {
@@ -4866,6 +4873,11 @@ TEST_F(URLLoaderTest, BlockOnlyThirdPartyCookies) {
       net::SiteForCookies::FromUrl(first_party_url);
   GURL third_party_url("http://www.some.other.origin.test/");
 
+  GURL cookie_url = test_server()->GetURL("/");
+  auto cc = net::CanonicalCookie::Create(
+      cookie_url, "a=b", base::Time::Now(), absl::nullopt /* server_time */,
+      net::CookiePartitionKey::FromURLForTesting(
+          GURL("https://toplevelsite.com")));
   ResourceRequest request = CreateResourceRequest("GET", first_party_url);
   base::RunLoop delete_run_loop;
   mojo::PendingRemote<mojom::URLLoader> loader;
@@ -4878,8 +4890,9 @@ TEST_F(URLLoaderTest, BlockOnlyThirdPartyCookies) {
       loader.InitWithNewPipeAndPassReceiver(), request,
       client()->CreateRemote());
 
-  EXPECT_TRUE(url_loader->AllowCookies(first_party_url, site_for_cookies));
-  EXPECT_FALSE(url_loader->AllowCookies(third_party_url, site_for_cookies));
+  EXPECT_TRUE(url_loader->AllowCookie(*cc, first_party_url, site_for_cookies));
+  EXPECT_TRUE(url_loader->AllowFullCookies(first_party_url, site_for_cookies));
+  EXPECT_FALSE(url_loader->AllowFullCookies(third_party_url, site_for_cookies));
 }
 
 TEST_F(URLLoaderTest, AllowAllCookies) {
@@ -4888,6 +4901,11 @@ TEST_F(URLLoaderTest, AllowAllCookies) {
       net::SiteForCookies::FromUrl(first_party_url);
   GURL third_party_url("http://www.some.other.origin.test/");
 
+  GURL cookie_url = test_server()->GetURL("/");
+  auto cc = net::CanonicalCookie::Create(
+      cookie_url, "a=b", base::Time::Now(), absl::nullopt /* server_time */,
+      net::CookiePartitionKey::FromURLForTesting(
+          GURL("https://toplevelsite.com")));
   ResourceRequest request = CreateResourceRequest("GET", first_party_url);
   base::RunLoop delete_run_loop;
   mojo::PendingRemote<mojom::URLLoader> loader;
@@ -4898,8 +4916,9 @@ TEST_F(URLLoaderTest, AllowAllCookies) {
       loader.InitWithNewPipeAndPassReceiver(), request,
       client()->CreateRemote());
 
-  EXPECT_TRUE(url_loader->AllowCookies(first_party_url, site_for_cookies));
-  EXPECT_TRUE(url_loader->AllowCookies(third_party_url, site_for_cookies));
+  EXPECT_TRUE(url_loader->AllowCookie(*cc, first_party_url, site_for_cookies));
+  EXPECT_TRUE(url_loader->AllowFullCookies(first_party_url, site_for_cookies));
+  EXPECT_TRUE(url_loader->AllowFullCookies(third_party_url, site_for_cookies));
 }
 
 class URLLoaderCookieSettingOverridesTest
