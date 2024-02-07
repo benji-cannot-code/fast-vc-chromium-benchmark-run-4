81ddebc8d784c865494449653db4752e6226994d
Brando <brandosocarras@google.com>
2024-02-07 19:23:24 +0000

    [Fast Pair][Software Scanning] Create BatterySaverActiveProvider
    BatterySaverActiveProvider indicates whether Battery Saver mode is
    active.
    
    Tested: unit tests in this.
    Fixed: b/322230505
    Change-Id: Ic281ca0bcab8e55b58f160d611cf2a0cb5e57e04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5254545
    Reviewed-by: Jack Shira <jackshira@google.com>
    Commit-Queue: Brando Socarras <brandosocarras@google.com>
    Cr-Commit-Position: refs/heads/main@{#1257495}

---
+++ ash/quick_pair/feature_status_tracker/battery_saver_active_provider.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/quick_pair/feature_status_tracker/battery_saver_active_provider.h"
+
+namespace ash {
+namespace quick_pair {
+
+BatterySaverActiveProvider::BatterySaverActiveProvider() {
+  if (PowerStatus::IsInitialized()) {
+    power_status_ = PowerStatus::Get();
+    power_status_->AddObserver(this);
+    SetEnabledAndInvokeCallback(
+        /*new_value=*/power_status_->IsBatterySaverActive());
+  }
+}
+
+BatterySaverActiveProvider::~BatterySaverActiveProvider() {
+  if (PowerStatus::IsInitialized()) {
+    power_status_->RemoveObserver(this);
+  }
+}
+
+void BatterySaverActiveProvider::OnPowerStatusChanged() {
+  SetEnabledAndInvokeCallback(
+      /*new_value=*/power_status_ && power_status_->IsBatterySaverActive());
+}
+
+}  // namespace quick_pair
+}  // namespace ash
