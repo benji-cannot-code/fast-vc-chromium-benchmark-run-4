843aadabdf709f26962406575191f7eca7da1130
Dominique Fauteux-Chapleau <domfc@chromium.org>
2024-02-08 16:52:44 +0000

    Add "other_profile" to Data Controls conditions
    This CL only adds the JSON parsing and `AttributesCondition` evaluation
    of this attribute, the code to populate the `other_profile` fields of
    `ActionSource` and `ActionDestination` is left to a future CL.
    
    Bug: b/302340176
    Change-Id: I52c4f96b09f6419dbdadf4a08be05a91ad7b40f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5274655
    Reviewed-by: Nasser Al-shawwa <alshawwa@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257980}

--- components/enterprise/data_controls/attributes_condition.cc
+++ components/enterprise/data_controls/attributes_condition.cc
@@ -35,6 +35,7 @@ AttributesCondition::AttributesCondition(const base::Value::Dict& value) {
 
   incognito_ = value.FindBool(kKeyIncognito);
   os_clipboard_ = value.FindBool(kKeyOsClipboard);
+  other_profile_ = value.FindBool(kKeyOtherProfile);
 
 #if BUILDFLAG(IS_CHROMEOS)
   const base::Value::List* components_value = value.FindList(kKeyComponents);
@@ -59,7 +60,8 @@ AttributesCondition::AttributesCondition(AttributesCondition&& other) = default;
 
 bool AttributesCondition::IsValid() const {
   bool valid = (url_matcher_ && !url_matcher_->IsEmpty()) ||
-               incognito_.has_value() || os_clipboard_.has_value();
+               incognito_.has_value() || os_clipboard_.has_value() ||
+               other_profile_.has_value();
 #if BUILDFLAG(IS_CHROMEOS)
   valid |= !components_.empty();
 #endif  // BUILDFLAG(IS_CHROMEOS)
@@ -108,6 +110,14 @@ bool AttributesCondition::OsClipboardMatches(bool os_clipboard) const {
   return os_clipboard == os_clipboard_.value();
 }
 
+bool AttributesCondition::OtherProfileMatches(bool other_profile) const {
+  if (!other_profile_.has_value()) {
+    return true;
+  }
+
+  return other_profile == other_profile_.value();
+}
+
 bool AttributesCondition::is_os_clipboard_condition() const {
   return os_clipboard_.has_value();
 }
@@ -141,10 +151,9 @@ bool SourceAttributesCondition::IsTriggered(
     return OsClipboardMatches(action_context.source.os_clipboard);
   }
 
-  if (!IncognitoMatches(action_context.source.incognito)) {
-    return false;
-  }
-  return URLMatches(action_context.source.url);
+  return IncognitoMatches(action_context.source.incognito) &&
+         OtherProfileMatches(action_context.source.other_profile) &&
+         URLMatches(action_context.source.url);
 }
 
 SourceAttributesCondition::SourceAttributesCondition(
@@ -185,15 +194,12 @@ bool DestinationAttributesCondition::IsTriggered(
     return OsClipboardMatches(action_context.destination.os_clipboard);
   }
 
-  if (!IncognitoMatches(action_context.destination.incognito)) {
-    return false;
-  }
+  return IncognitoMatches(action_context.destination.incognito) &&
+         OtherProfileMatches(action_context.destination.other_profile) &&
 #if BUILDFLAG(IS_CHROMEOS)
-  if (!ComponentMatches(action_context.destination.component)) {
-    return false;
-  }
+         ComponentMatches(action_context.destination.component) &&
 #endif
-  return URLMatches(action_context.destination.url);
+         URLMatches(action_context.destination.url);
 }
 
 DestinationAttributesCondition::DestinationAttributesCondition(
--- components/enterprise/data_controls/attributes_condition.h
+++ components/enterprise/data_controls/attributes_condition.h
@@ -39,6 +39,7 @@ class AttributesCondition {
   static constexpr char kKeyUrls[] = "urls";
   static constexpr char kKeyIncognito[] = "incognito";
   static constexpr char kKeyOsClipboard[] = "os_clipboard";
+  static constexpr char kKeyOtherProfile[] = "other_profile";
 #if BUILDFLAG(IS_CHROMEOS)
   static constexpr char kKeyComponents[] = "components";
 #endif  // BUILDFLAG(IS_CHROMEOS)
@@ -66,6 +67,7 @@ class AttributesCondition {
   // corresponding conditions.
   bool IncognitoMatches(bool incognito) const;
   bool OsClipboardMatches(bool os_clipboard) const;
+  bool OtherProfileMatches(bool other_profile) const;
 
   // Helpers to help check which attributes are meaningful to the condition.
   bool is_os_clipboard_condition() const;
@@ -84,6 +86,10 @@ class AttributesCondition {
   // the OS clipboard. It is always null for non-clipboard conditions.
   std::optional<bool> os_clipboard_;
 
+  // This attribute indicates the destination/source condition must/mustn't be
+  // a separate Chrome profile. It is always null for non-clipboard conditions.
+  std::optional<bool> other_profile_;
+
 #if BUILDFLAG(IS_CHROMEOS)
   // A destination/source must be in this set to pass the condition, unless the
   // set is empty.
@@ -98,6 +104,7 @@ class SourceAttributesCondition : public AttributesCondition, public Condition {
   static std::unique_ptr<Condition> Create(const base::Value& value);
   static std::unique_ptr<Condition> Create(const base::Value::Dict& value);
 
+  // data_controls::Condition:
   bool IsTriggered(const ActionContext& action_context) const override;
 
  private:
@@ -114,6 +121,7 @@ class DestinationAttributesCondition : public AttributesCondition,
   static std::unique_ptr<Condition> Create(const base::Value& value);
   static std::unique_ptr<Condition> Create(const base::Value::Dict& value);
 
+  // data_controls::Condition:
   bool IsTriggered(const ActionContext& action_context) const override;
 
  private:
