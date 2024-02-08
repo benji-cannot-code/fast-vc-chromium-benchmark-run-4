616a7b20515a0ff01d67207e4e766fa150d83a9e
David Maunder <davidjm@google.com>
2024-02-08 15:44:13 +0000

    Batch FeaturesAnnotationsTest
    Bug: b/324325748
    Change-Id: I17352925ba377a3d2fc8e9f22d03d5bd0ffa0d93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5274703
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257930}

--- chrome/android/javatests/src/org/chromium/chrome/browser/FeaturesAnnotationsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/FeaturesAnnotationsTest.java
@@ -11,11 +11,13 @@ import static org.hamcrest.core.IsCollectionContaining.hasItems;
 
 import androidx.test.filters.SmallTest;
 
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.CommandLine;
+import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Features;
 import org.chromium.base.test.util.Features.DisableFeatures;
@@ -23,6 +25,7 @@ import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
+import org.chromium.chrome.test.batch.BlankCTATabInitialStateRule;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -31,9 +34,15 @@ import java.util.List;
 /** Tests for {@link Features}. */
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add(ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE)
+@Batch(Batch.PER_CLASS)
 public class FeaturesAnnotationsTest {
+    @ClassRule
+    public static ChromeTabbedActivityTestRule sActivityTestRule =
+            new ChromeTabbedActivityTestRule();
+
     @Rule
-    public ChromeTabbedActivityTestRule mActivityTestRule = new ChromeTabbedActivityTestRule();
+    public BlankCTATabInitialStateRule mInitialStateRule =
+            new BlankCTATabInitialStateRule(sActivityTestRule, false);
 
     /**
      * Tests that {@link EnableFeatures} and {@link DisableFeatures} can alter the flags registered
@@ -43,8 +52,7 @@ public class FeaturesAnnotationsTest {
     @SmallTest
     @EnableFeatures("One")
     @DisableFeatures("Two")
-    public void testFeaturesSetExistingFlags() throws InterruptedException {
-        mActivityTestRule.startMainActivityOnBlankPage();
+    public void testFeaturesSetExistingFlags() {
         List<String> finalEnabledList = getFeatureList(true);
 
         assertThat(finalEnabledList, hasItems("One"));
@@ -66,8 +74,7 @@ public class FeaturesAnnotationsTest {
     @SmallTest
     @CommandLineFlags.Add("enable-features=One,Two,Three")
     @EnableFeatures("Two")
-    public void testFeaturesDoNotRemoveExistingFlags() throws InterruptedException {
-        mActivityTestRule.startMainActivityOnBlankPage();
+    public void testFeaturesDoNotRemoveExistingFlags() {
         List<String> finalEnabledList = getFeatureList(true);
 
         assertThat(finalEnabledList, hasItems("One", "Two", "Three"));
@@ -85,8 +92,7 @@ public class FeaturesAnnotationsTest {
     @SmallTest
     @CommandLineFlags.Add("enable-features=One,Two,Three")
     @EnableFeatures({"Three", "Four"})
-    public void testFeaturesAddToExistingFlags() throws InterruptedException {
-        mActivityTestRule.startMainActivityOnBlankPage();
+    public void testFeaturesAddToExistingFlags() {
         List<String> finalEnabledList = getFeatureList(true);
 
         assertThat(finalEnabledList, hasItems("Four"));
