5d64fdadb04610b97d84f085cb41e581fa668a86
Charles Meng <charlesmeng@chromium.org>
2024-02-08 17:22:46 +0000

    Move high memory usage check to tab helper
    Check if memory usage is high from the tab helper so that other places
    such as tab hover card footers may read from that source.
    
    Specifically, when memory usage is set in set_memory_usage_in_bytes, a
    bool is_high_memory_usage_ is calculated. Since this is doing something
    more than a simple setter, rename to camel case and move definition to
    the .cc file.
    
    Fixed: 324145069
    Change-Id: I23e2c5d7f9bebc94968e86cf27f3e4be08360e9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5274331
    Commit-Queue: Charles Meng <charlesmeng@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257999}

--- chrome/browser/ui/views/tabs/fade_footer_view.cc
+++ chrome/browser/ui/views/tabs/fade_footer_view.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/layout_constants.h"
 #include "chrome/browser/ui/views/tabs/alert_indicator_button.h"
 #include "chrome/grit/generated_resources.h"
-#include "components/performance_manager/public/features.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/models/image_model.h"
@@ -165,12 +164,8 @@ void FadePerformanceFooterRow::SetData(const PerformanceRowData& data) {
     const std::u16string formatted_memory_usage =
         ui::FormatBytes(data.memory_usage_in_bytes);
     const std::u16string row_text = l10n_util::GetStringFUTF16(
-        data.memory_usage_in_bytes >
-                static_cast<uint64_t>(
-                    performance_manager::features::
-                        kMemoryUsageInHovercardsHighThresholdBytes.Get())
-            ? IDS_HOVERCARD_TAB_HIGH_MEMORY_USAGE
-            : IDS_HOVERCARD_TAB_MEMORY_USAGE,
+        data.is_high_memory_usage ? IDS_HOVERCARD_TAB_HIGH_MEMORY_USAGE
+                                  : IDS_HOVERCARD_TAB_MEMORY_USAGE,
         formatted_memory_usage);
 
     const ui::ImageModel icon_image_model = ui::ImageModel::FromVectorIcon(
