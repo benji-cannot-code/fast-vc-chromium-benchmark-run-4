bedc1f2c2b34ad03c9a3f13d637e630804a606fb
Olivier Robin <olivierrobin@google.com>
2024-02-08 12:52:25 +0000

    Set openInSourceView in DownloadManagerViewController
    This is needed for iPad popup display.
    
    Bug: b/324260897
    Change-Id: Ief95a54dc3bf48fcab11c3a30ed643308f45dc89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5272352
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257874}

--- ios/chrome/browser/ui/download/download_manager_view_controller.mm
+++ ios/chrome/browser/ui/download/download_manager_view_controller.mm
@@ -371,7 +371,7 @@ UIImageView* CreateProgressIcon(NSString* symbol_name) {
 #pragma mark - DownloadManagerViewControllerProtocol
 
 - (UIView*)openInSourceView {
-  return nil;
+  return self.openInButton;
 }
 
 - (void)setFullscreenController:(FullscreenController*)fullscreenController {
