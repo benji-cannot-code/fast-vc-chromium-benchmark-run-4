ccbe79144e34aa26604a5c7eda765e86aad1b7b8
rbpotter <rbpotter@chromium.org>
2024-02-08 22:26:37 +0000

    WebUI: Enforce "import type" ESLint checks in default config
    Moving "import type" presubmit to the top level .eslintrc.js file and
    removing from lower level files.
    
    Adding overrides for ash/webui and ui/file_manager, to avoid applying
    this rule in those folders, where there are a large number of
    violations.
    
    Bug: 1494527
    Change-Id: I729a097b856bc4f6a1a727cbcb56e9dc5545bed7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5278066
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258197}

--- chrome/test/data/webui/.eslintrc.js
+++ chrome/test/data/webui/.eslintrc.js
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 module.exports = {
   // clang-format off
   'env': {'browser': true, 'es6': true},
-  'extends': '../../../../ui/webui/resources/tools/eslint_import_type.config.js',
   'rules': {
     'no-restricted-properties': [
       'error', {
--- components/.eslintrc.js
+++ components/.eslintrc.js
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 module.exports = {
-  'extends': '../ui/webui/resources/tools/eslint_import_type.config.js',
   'rules' : {
     'no-restricted-properties' : 'off',
   },
