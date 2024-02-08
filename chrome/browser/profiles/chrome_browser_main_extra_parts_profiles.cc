0c8e6755e6ed068f378ae38211c3c3683147efac
mlcui <mlcui@google.com>
2024-02-08 02:34:56 +0000

    language_packs: Add barebones LanguagePackFontService KeyedService
    This KeyedService currently does nothing - functionality will be added
    in future CLs (https://crrev.com/c/5201484 and
    https://crrev.com/c/5202932).
    
    Bug: b:320549210
    Change-Id: Ib0a45324c272748835a30b63113b3b4107585619
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5202871
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1257743}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -295,6 +295,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/crosapi/browser_util.h"
 #include "chrome/browser/ash/file_manager/cloud_upload_prefs_watcher.h"
 #include "chrome/browser/ash/input_method/editor_mediator_factory.h"
+#include "chrome/browser/ash/language_packs/language_pack_font_service_factory.h"
 #include "chrome/browser/ash/policy/dlp/files_policy_notification_manager_factory.h"
 #include "chrome/browser/nearby_sharing/nearby_sharing_service_factory.h"
 #include "chrome/browser/push_notification/push_notification_service_factory.h"
@@ -602,6 +603,9 @@ void ChromeBrowserMainExtraPartsProfiles::
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   if (chromeos::features::IsOrcaEnabled()) {
     ash::input_method::EditorMediatorFactory::GetInstance();
+  }
+  if (base::FeatureList::IsEnabled(ash::features::kLanguagePacksFonts)) {
+    ash::language_packs::LanguagePackFontServiceFactory::GetInstance();
   }
 #endif
   AutocompleteClassifierFactory::GetInstance();
