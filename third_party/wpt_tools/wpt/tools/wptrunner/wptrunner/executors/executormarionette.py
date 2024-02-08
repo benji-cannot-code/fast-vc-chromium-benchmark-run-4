88e5e8817e31d207ec835bbb6bbdbe0f4de9b8ed
Jonathan Lee <jonathanjlee@google.com>
2024-02-08 22:28:55 +0000

    Roll wpt tooling
    This rolls wpt to latest commit at
    https://github.com/web-platform-tests/wpt.
    REMOTE-WPT-HEAD: effd91d0c3ea64f8c75f7fe0714d98f96a78dc2a
    
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I99cb6955f375a7812514497e19748a9c7b2396e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5278783
    Auto-Submit: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1258201}

--- third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/executors/executormarionette.py
+++ third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/executors/executormarionette.py
@@ -980,7 +980,7 @@ class MarionetteTestharnessExecutor(TestharnessExecutor):
         if success:
             return self.convert_result(test, data, extra=extra)
 
-        return (test.result_cls(extra=extra, *data), [])
+        return (test.make_result(extra=extra, *data), [])
 
     def do_testharness(self, protocol, url, timeout):
         parent_window = protocol.testharness.close_old_windows(self.last_environment["protocol"])
@@ -1278,7 +1278,7 @@ class MarionetteCrashtestExecutor(CrashtestExecutor):
         if success:
             return self.convert_result(test, data)
 
-        return (test.result_cls(extra=extra, *data), [])
+        return (test.make_result(extra=extra, *data), [])
 
     def do_crashtest(self, protocol, url, timeout):
         if self.protocol.coverage.is_enabled:
