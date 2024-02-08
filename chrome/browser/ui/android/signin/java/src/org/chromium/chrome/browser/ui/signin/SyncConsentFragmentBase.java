45cf8e3985df682095dc50fc71b99df1dca32892
Samar <samarchehade@google.com>
2024-02-08 14:26:46 +0000

    [Signin][Android] Remove nullness checks for SigninView
    These should have been cleaned up as part of https://crrev.com/c/5246008
    but were forgotten.
    
    Change-Id: I3875ffd0bc0acd7e972a0c96971414dcf6a2fef8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5277139
    Auto-Submit: Samar Chehade-Lepleux <samarchehade@google.com>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1257901}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/SyncConsentFragmentBase.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/SyncConsentFragmentBase.java
@@ -501,8 +501,6 @@ public abstract class SyncConsentFragmentBase extends Fragment
      * "Add account" button in this case.
      */
     private void setHasAccounts(boolean hasAccounts) {
-        assert mSigninView != null;
-
         if (hasAccounts) {
             final boolean hideAccountPicker =
                     mIsSignedInWithoutSync
@@ -806,7 +804,7 @@ public abstract class SyncConsentFragmentBase extends Fragment
                 AccountUtils.getCoreAccountInfosIfFulfilledOrEmpty(
                         mAccountManagerFacade.getCoreAccountInfos()));
 
-        if (mSigninView != null) mSigninView.startAnimations();
+        mSigninView.startAnimations();
         if (mDeviceLockReady) {
             mDeviceLockPageCallback.run();
         }
@@ -817,7 +815,7 @@ public abstract class SyncConsentFragmentBase extends Fragment
         super.onPause();
         mAccountManagerFacade.removeObserver(this);
 
-        if (mSigninView != null) mSigninView.stopAnimations();
+        mSigninView.stopAnimations();
     }
 
     private void selectAccount(String accountEmail) {
