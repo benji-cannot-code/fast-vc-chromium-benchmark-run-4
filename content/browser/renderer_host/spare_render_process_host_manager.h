726dcbc84fcce4610fdef1f31c8d32306a96c776
Jose Dapena Paz <jdapena@igalia.com>
2024-02-08 18:28:11 +0000

    IWYU: usage of std::optional requires include in spare_render_process_host_manager.h
    Bug: 957519
    Change-Id: Ib492925af1f57ec10e42b567fead6c6a058efa00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5259417
    Commit-Queue: Charlie Reis <creis@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258033}

--- content/browser/renderer_host/spare_render_process_host_manager.h
+++ content/browser/renderer_host/spare_render_process_host_manager.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_RENDERER_HOST_SPARE_RENDER_PROCESS_HOST_MANAGER_H_
 #define CONTENT_BROWSER_RENDERER_HOST_SPARE_RENDER_PROCESS_HOST_MANAGER_H_
 
+#include <optional>
+
 #include "base/callback_list.h"
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
