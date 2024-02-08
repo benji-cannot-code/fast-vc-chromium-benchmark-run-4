75b2824e72be6fd486f7f4d3703e7e7230ca7b02
Andrew Williams <awillia@chromium.org>
2024-02-08 20:13:50 +0000

    [IP Protection] Initial support for disabling IP Protection at runtime
    This CL implements disabling the IP Protection feature at runtime
    based on the value of a user preference (that will eventually be
    controlled by a user setting). This change is implemented behind
    a disabled-by-default flag that will allow Chrome to continue to
    experiment with enabling IP Protection before the user setting is
    available.
    
    What's implemented in this CL prevents new connections from being
    tunneled via IP Protection and prevents new requests for tokens or
    config information from being made to Phosphor, but doesn't tear
    down existing proxied connections. This will be addressed in a
    follow-up CL.
    
    Bug: 41494110
    Change-Id: Icf3d825396a721ea7206cdad73cc3c373fb75f96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5258623
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    Reviewed-by: Dustin Mitchell <djmitche@chromium.org>
    Commit-Queue: Andrew Williams <awillia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258113}

--- chrome/browser/ip_protection/ip_protection_config_provider_factory.cc
+++ chrome/browser/ip_protection/ip_protection_config_provider_factory.cc
@@ -5,9 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ip_protection/ip_protection_config_provider_factory.h"
 
-#include "base/command_line.h"
 #include "chrome/browser/ip_protection/ip_protection_config_provider.h"
-#include "chrome/browser/ip_protection/ip_protection_switches.h"
 #include "chrome/browser/privacy_sandbox/tracking_protection_settings_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_selections.h"
@@ -31,9 +29,7 @@ IpProtectionConfigProviderFactory::GetInstance() {
 
 // static
 ProfileSelections IpProtectionConfigProviderFactory::CreateProfileSelections() {
-  if (!base::FeatureList::IsEnabled(net::features::kEnableIpProtectionProxy) ||
-      base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kDisableIpProtectionProxy)) {
+  if (!IpProtectionConfigProvider::CanIpProtectionBeEnabled()) {
     return ProfileSelections::BuildNoProfilesSelected();
   }
   // IP Protection usage requires that a Gaia account is available when
@@ -71,7 +67,8 @@ IpProtectionConfigProviderFactory::BuildServiceInstanceForBrowserContext(
   Profile* profile = Profile::FromBrowserContext(context);
   return std::make_unique<IpProtectionConfigProvider>(
       IdentityManagerFactory::GetForProfile(profile),
-      TrackingProtectionSettingsFactory::GetForProfile(profile), profile);
+      TrackingProtectionSettingsFactory::GetForProfile(profile),
+      profile->GetPrefs(), profile);
 }
 
 bool IpProtectionConfigProviderFactory::ServiceIsCreatedWithBrowserContext()
