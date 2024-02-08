55fd0ff1193581d52c92ba5fac47e0efd93ad371
Luke Warlow <lwarlow@igalia.com>
2024-02-08 22:44:10 +0000

    Retire SuggestionPickerDarkModeSupport flag
    Removes the SuggestionPickerDarkModeSupport flag, and merges the dark
    mode stylesheet into the main one.
    
    Bug: 1483710
    Change-Id: I434b3be8a16cc11760a2e34e51cb135325a014d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5200856
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Luke <lwarlow@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1258208}

--- third_party/blink/renderer/core/html/forms/chooser_resource_loader.cc
+++ third_party/blink/renderer/core/html/forms/chooser_resource_loader.cc
@@ -22,16 +22,6 @@ Vector<char> ChooserResourceLoader::GetSuggestionPickerStyleSheet() {
 #endif
 }
 
-Vector<char> ChooserResourceLoader::GetSuggestionPickerDarkModeStyleSheet() {
-  CHECK(RuntimeEnabledFeatures::SuggestionPickerDarkModeSupportEnabled());
-#if !BUILDFLAG(IS_ANDROID)
-  return UncompressResourceAsBinary(IDR_SUGGESTION_PICKER_DARK_CSS);
-#else
-  NOTREACHED();
-  return Vector<char>();
-#endif
-}
-
 Vector<char> ChooserResourceLoader::GetSuggestionPickerJS() {
 #if !BUILDFLAG(IS_ANDROID)
   return UncompressResourceAsBinary(IDR_SUGGESTION_PICKER_JS);
