33e4d51f029bdf338dddf37e99fd0dfcb9ce5a98
Avi Drissman <avi@chromium.org>
2024-02-08 01:52:05 +0000

    Do light Objective-C cleanup
    This uses more direct helper functions, as well as uses some
    Objective-C syntax sugar for clarity.
    
    Bug: none
    Change-Id: I47d6164e6ccf989c38430540f30b6912a510a8b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5277460
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257731}

--- services/webnn/coreml/context_impl.mm
+++ services/webnn/coreml/context_impl.mm
@@ -3,11 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <CoreML/CoreML.h>
-
 #include "services/webnn/coreml/context_impl.h"
 
-#import "base/strings/sys_string_conversions.h"
+#import <CoreML/CoreML.h>
+
 #include "services/webnn/coreml/graph_impl.h"
 #include "services/webnn/webnn_context_provider_impl.h"
 
--- services/webnn/coreml/graph_impl.mm
+++ services/webnn/coreml/graph_impl.mm
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation WebNNMLFeatureProvider
 - (MLFeatureValue*)featureValueForName:(NSString*)featureName {
-  return [_featureValues objectForKey:featureName];
+  return _featureValues[featureName];
 }
 - (instancetype)initWithFeatures:(NSSet<NSString*>*)feature_names
                    featureValues:(NSDictionary*)feature_values {
@@ -139,8 +139,7 @@ void GraphImpl::CreateAndBuildOnBackgroundThread(
 
   // TODO(https://crbug.com/1522278): Add metrics to measure compilation time.
   [MLModel
-      compileModelAtURL:[NSURL URLWithString:base::apple::FilePathToNSString(
-                                                 model_file_path)]
+      compileModelAtURL:base::apple::FilePathToNSURL(model_file_path)
       completionHandler:^(NSURL* compiled_model_url, NSError* error) {
         // compiled_model_url refers to a directory placed directly inside
         // NSTemporaryDirectory(), it is not inside model_file_dir.
@@ -263,11 +262,11 @@ absl::optional<GraphImpl::CoreMLFeatureInfo> GraphImpl::GetCoreMLFeatureInfo(
   }
   uint32_t current_stride = expected_size.ValueOrDie();
   for (uint32_t dimension : operand_info->dimensions) {
-    [shape addObject:[NSNumber numberWithUnsignedInt:dimension]];
+    [shape addObject:@(dimension)];
     // since expected_size was computed by multiplying all dimensions together
     // current_stride has to be perfectly divisible by dimension.
     current_stride = current_stride / dimension;
-    [stride addObject:[NSNumber numberWithUnsignedInt:current_stride]];
+    [stride addObject:@(current_stride)];
   }
   return GraphImpl::CoreMLFeatureInfo(data_type, shape, stride);
 }
@@ -301,7 +300,7 @@ void GraphImpl::ComputeImpl(
           mojom::Error::Code::kUnknownError, "Input initialization error")));
       return;
     }
-    [feature_values setObject:feature_value forKey:feature_name];
+    feature_values[feature_name] = feature_value;
   }
 
   // Run the MLModel
