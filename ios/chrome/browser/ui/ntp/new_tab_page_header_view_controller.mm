defc6715a1755326ecc7fa27be1dfe9866584153
Federica Germinario <fedegermi@google.com>
2024-02-08 10:20:41 +0000

    [ios] Fix hover effect for identity button
    This CL fixes the hover effect of the identity button to match the
    shape of the identity image.
    There is no visible change on iPad.
    Note that the clickable area is now smaller (with old implementation
    all the square area around the button was clickable).
    
    Old implementation:
     - Size of the image
     - Margin (16pt)
    
    New implementation:
    - Size of the image
    - Margin (8pt)
    - Padding (8pt)
    
    As a result the clickable area will be 8pt*2 = 16pt smaller
    (from 64pt to 48pt).
    A screenshot of the new behavior can be found in the bug page.
    
    Bug: 324051579
    Change-Id: I3492447f779031bb6dfa0f388ddc8fa8930236cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5268822
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257846}

--- ios/chrome/browser/ui/ntp/new_tab_page_header_view_controller.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_header_view_controller.mm
@@ -447,6 +447,9 @@ const CGFloat kFakeLocationBarHeightMargin = 2;
   self.identityDiscButton.imageView.layer.cornerRadius =
       self.identityDiscImage.size.width / 2;
   self.identityDiscButton.imageView.layer.masksToBounds = YES;
+  self.identityDiscButton.layer.cornerRadius =
+      self.identityDiscImage.size.width;
+  self.identityDiscButton.clipsToBounds = YES;
 }
 
 - (void)openLens {
