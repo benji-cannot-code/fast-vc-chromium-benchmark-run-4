ebec17b13d4fe6fbb7fe505e402575c7379a5e33
Colin Blundell <blundell@chromium.org>
2024-02-08 08:03:58 +0000

    [//gpu] Fold PassthroughAbstractTextureImpl into AbstractTexture
    PassthroughAbstractTextureImpl is now the only subclass of
    AbstractTexture (this abstraction is used only by //media on desktop,
    where the passthrough decoder has fully shipped). This CL folds the
    implementation into the interface.
    
    Followup CLs will remove methods that have no-op implementations, as
    these are clearly no longer needed.
    
    Bug: 40135856
    Change-Id: I8ab16969a7fda11839d053ea25519f4267780453
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5272906
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257820}

--- gpu/command_buffer/service/gles2_cmd_decoder_passthrough.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder_passthrough.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ranges/algorithm.h"
 #include "base/strings/string_split.h"
 #include "build/build_config.h"
+#include "gpu/command_buffer/service/abstract_texture.h"
 #include "gpu/command_buffer/service/command_buffer_service.h"
 #include "gpu/command_buffer/service/decoder_client.h"
 #include "gpu/command_buffer/service/feature_info.h"
@@ -1105,7 +1106,7 @@ void GLES2DecoderPassthroughImpl::Destroy(bool have_context) {
 
 #if !BUILDFLAG(IS_ANDROID)
   if (resources_) {  // Initialize may not have been called yet.
-    for (PassthroughAbstractTextureImpl* iter : abstract_textures_) {
+    for (AbstractTexture* iter : abstract_textures_) {
       resources_->textures_pending_destruction.insert(
           iter->OnDecoderWillDestroy());
     }
@@ -1649,15 +1650,15 @@ GLES2DecoderPassthroughImpl::CreateAbstractTexture(GLenum target,
       new TexturePassthrough(service_id, target));
 
   // Unretained is safe, because of the destruction cb.
-  std::unique_ptr<PassthroughAbstractTextureImpl> abstract_texture =
-      std::make_unique<PassthroughAbstractTextureImpl>(texture, this);
+  std::unique_ptr<AbstractTexture> abstract_texture =
+      std::make_unique<AbstractTexture>(texture, this);
 
   abstract_textures_.insert(abstract_texture.get());
   return abstract_texture;
 }
 
 void GLES2DecoderPassthroughImpl::OnAbstractTextureDestroyed(
-    PassthroughAbstractTextureImpl* abstract_texture,
+    AbstractTexture* abstract_texture,
     scoped_refptr<TexturePassthrough> texture) {
   DCHECK(texture);
   abstract_textures_.erase(abstract_texture);
--- gpu/command_buffer/service/gles2_cmd_decoder_passthrough.h
+++ gpu/command_buffer/service/gles2_cmd_decoder_passthrough.h
@@ -39,10 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_surface.h"
 #include "ui/gl/gpu_switching_observer.h"
 
-#if !BUILDFLAG(IS_ANDROID)
-#include "gpu/command_buffer/service/passthrough_abstract_texture_impl.h"
-#endif
-
 namespace gl {
 class GLFence;
 class ProgressReporter;
@@ -55,7 +51,7 @@ namespace gles2 {
 
 class ContextGroup;
 class GPUTracer;
-class PassthroughAbstractTextureImpl;
+class AbstractTexture;
 class MultiDrawManager;
 class GLES2DecoderPassthroughImpl;
 class GLES2ExternalFramebuffer;
@@ -405,7 +401,7 @@ class GPU_GLES2_EXPORT GLES2DecoderPassthroughImpl
       CopyTexImageResourceManager* copy_tex_image_blit) override;
 
 #if !BUILDFLAG(IS_ANDROID)
-  void OnAbstractTextureDestroyed(PassthroughAbstractTextureImpl*,
+  void OnAbstractTextureDestroyed(AbstractTexture*,
                                   scoped_refptr<TexturePassthrough>);
 #endif
 
@@ -525,9 +521,9 @@ class GPU_GLES2_EXPORT GLES2DecoderPassthroughImpl
   bool OnlyHasPendingProgramCompletionQueries();
 
 #if !BUILDFLAG(IS_ANDROID)
-  // A set of raw pointers to currently living PassthroughAbstractTextures
+  // A set of raw pointers to currently living AbstractTextures
   // which allow us to properly signal to them when we are destroyed.
-  base::flat_set<PassthroughAbstractTextureImpl*> abstract_textures_;
+  base::flat_set<AbstractTexture*> abstract_textures_;
 #endif
 
   int commands_to_process_;
