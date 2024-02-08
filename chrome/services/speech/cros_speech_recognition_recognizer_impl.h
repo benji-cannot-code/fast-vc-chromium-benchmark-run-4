aec13399628d7cfefe1c36304603ac2e0098c8eb
Rob Schonberger <robsc@chromium.org>
2024-02-08 02:15:37 +0000

    Soda: Add test for CrosSpeechRecognitionRecognizerImpl .
    This adds a small unit test to the implementation by refactoring to pull out
    a simple static method.
    
    Intended as a starting point for further testing.
    
    
    Bug: b/257166700
    Test: Unit test passes.
    Change-Id: Id984f9ea754a4b589b6676697a6cdde55054e861
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5269203
    Auto-Submit: Rob Schonberger <robsc@chromium.org>
    Commit-Queue: Rob Schonberger <robsc@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1257739}

--- chrome/services/speech/cros_speech_recognition_recognizer_impl.h
+++ chrome/services/speech/cros_speech_recognition_recognizer_impl.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/services/speech/speech_recognition_recognizer_impl.h"
+#include "chromeos/services/machine_learning/public/mojom/soda.mojom.h"
 #include "media/mojo/mojom/speech_recognition_service.mojom.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
@@ -54,6 +55,12 @@ class CrosSpeechRecognitionRecognizerImpl
       media::mojom::AudioDataS16Ptr buffer) override;
 
   void MarkDone() override;
+
+  static chromeos::machine_learning::mojom::SodaMultilangConfigPtr
+  AddLiveCaptionLanguagesToConfig(
+      const std::string& primary_language_name,
+      const base::flat_map<std::string, base::FilePath>& config_paths,
+      const std::vector<std::string>& live_caption_languages);
 
  private:
   std::unique_ptr<soda::CrosSodaClient> cros_soda_client_;
