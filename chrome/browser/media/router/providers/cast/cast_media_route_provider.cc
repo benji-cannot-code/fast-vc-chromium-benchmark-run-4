cdaabf07491ff21479b36972148282e1da9da767
Muyao Xu <muyaoxu@google.com>
2024-02-08 18:46:08 +0000

    [Remote Playback] Disable Background Monitoring for short videos
    Media Remoting from Zenith is only available for videos longer than 15
    seconds because short videos are less likely to be cast.
    
    This CL applies this rule for Remote Playback so that the Cast buttons
    won't show up for short videos.
    
    Bug: 41495778
    Change-Id: I8dd74266b22f57b2752a341a0c654c6f38c181e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5274194
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258054}

--- chrome/browser/media/router/providers/cast/cast_media_route_provider.cc
+++ chrome/browser/media/router/providers/cast/cast_media_route_provider.cc
@@ -363,7 +363,6 @@ void CastMediaRouteProvider::OnSinkQueryUpdated(
       mojom::MediaRouteProviderId::CAST, source_id,
       GetRemotePlaybackMediaSourceCompatibleSinks(media_source, sinks),
       GetOrigins(source_id));
-  LOG(ERROR) << "sent sink updates";
 }
 
 }  // namespace media_router
--- third_party/blink/public/platform/modules/remoteplayback/remote_playback_source.h
+++ third_party/blink/public/platform/modules/remoteplayback/remote_playback_source.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_PUBLIC_PLATFORM_MODULES_REMOTEPLAYBACK_REMOTE_PLAYBACK_SOURCE_H_
 
 namespace blink {
+
 inline constexpr char kRemotePlaybackPresentationUrlScheme[] =
     "remote-playback";
 // The path for RemotePlayback Urls on both Android and Desktop.
@@ -18,6 +19,11 @@ inline constexpr char kRemotePlaybackPresentationUrlPath[] =
 // The format for RemotePlayback Urls on desktop.
 inline constexpr char kRemotePlaybackDesktopUrlFormat[] =
     "remote-playback:media-session?video_codec=%s&audio_codec=%s&tab_id=%d";
+
+// The minimum media element duration that is allowed for media remoting.
+// Needs to be in sync with media/remoting/remoting_constants.h
+inline constexpr double kMinRemotingMediaDurationInSec = 15;
+
 }  // namespace blink
 
 #endif  // THIRD_PARTY_BLINK_PUBLIC_PLATFORM_MODULES_REMOTEPLAYBACK_REMOTE_PLAYBACK_SOURCE_H_
