0233274267b97ae8ec7713819a5fa0c1ac5588bd
Andrew Paseltiner <apaseltiner@chromium.org>
2024-02-08 14:22:37 +0000

    Improve accessibility of Attribution Internals
    - Use <button> for sortable table columns
    - Change background color of those buttons on hover
    - Add titles to selection checkboxes
    - Use <time> for time elements
    
    Screenshot of button change in
    https://issues.chromium.org/u/1/issues/324280582#comment2.
    
    Bug: 324280582
    Change-Id: Id49d3da0009f35783f37a7a5db65b1d771dc1542
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5274658
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257898}

--- content/browser/attribution_reporting/attribution_internals_browsertest.cc
+++ content/browser/attribution_reporting/attribution_internals_browsertest.cc
@@ -753,7 +753,7 @@ IN_PROC_BROWSER_TEST_F(AttributionInternalsWebUiBrowserTest,
     // Sort by priority ascending.
     ASSERT_TRUE(ExecJsInWebUI(R"(
       document.querySelector('#reportTable')
-        .shadowRoot.querySelectorAll('th')[5].click();
+        .shadowRoot.querySelector('th:nth-child(6) button').click();
     )"));
     ASSERT_EQ(kCompleteTitle2, title_watcher.WaitAndGetTitle());
   }
@@ -792,7 +792,7 @@ IN_PROC_BROWSER_TEST_F(AttributionInternalsWebUiBrowserTest,
     // Sort by priority descending.
     ASSERT_TRUE(ExecJsInWebUI(R"(
       document.querySelector('#reportTable')
-        .shadowRoot.querySelectorAll('th')[5].click();
+        .shadowRoot.querySelector('th:nth-child(6) button').click();
     )"));
     ASSERT_EQ(kCompleteTitle3, title_watcher.WaitAndGetTitle());
   }
--- content/browser/resources/attribution_reporting/attribution_internals.ts
+++ content/browser/resources/attribution_reporting/attribution_internals.ts
@@ -107,9 +107,12 @@ class ComparableColumn<T, V> extends ValueColumn<T, V> {
 }
 
 function dateColumn<T>(header: string, getValue: (row: T) => Date): Column<T> {
-  return new ComparableColumn(
-      header, getValue, compareDefault,
-      (td, v) => td.innerText = v.toLocaleString());
+  return new ComparableColumn(header, getValue, compareDefault, (td, v) => {
+    const time = td.ownerDocument.createElement('time');
+    time.dateTime = v.toISOString();
+    td.innerText = v.toLocaleString();
+    td.append(time);
+  });
 }
 
 const numberClass: string = 'number';
@@ -249,6 +252,7 @@ class Selectable {
   constructor() {
     this.input = document.createElement('input');
     this.input.type = 'checkbox';
+    this.input.title = 'Select';
   }
 }
 
@@ -260,6 +264,7 @@ class SelectionColumn<T extends Selectable> implements Column<T> {
   constructor(private readonly model: TableModel<T>) {
     this.selectAll = document.createElement('input');
     this.selectAll.type = 'checkbox';
+    this.selectAll.title = 'Select All';
     this.selectAll.addEventListener('input', () => {
       const checked = this.selectAll.checked;
       this.model.getRows().forEach((row) => {
