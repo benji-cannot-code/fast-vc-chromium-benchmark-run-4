8a00b0e2e8f361a9b774209391ca85c41aa2a6b3
Andrey Kosyakov <caseq@chromium.org>
2024-02-08 02:48:29 +0000

    Headless: also keep profile alive while processing headless commands
    This is a follow-up to https://crrev.com/c/5254301, please see
    https://crrev.com/c/5254301/comment/5acda774_5fc2791f/ for context.
    
    Change-Id: I2388f5551b7ab009b1a73394341d2e12099680a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5270782
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Mark Pearson <mpearson@chromium.org>
    Auto-Submit: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257748}

--- chrome/browser/ui/startup/startup_browser_creator_impl.cc
+++ chrome/browser/ui/startup/startup_browser_creator_impl.cc
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/prefs/session_startup_pref.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_service.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_service_factory.h"
+#include "chrome/browser/profiles/keep_alive/profile_keep_alive_types.h"
+#include "chrome/browser/profiles/keep_alive/scoped_profile_keep_alive.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_io_data.h"
 #include "chrome/browser/sessions/app_session_service.h"
@@ -244,6 +246,7 @@ Browser* StartupBrowserCreatorImpl::OpenTabsInBrowser(
     profile_ = browser->profile();
 
   if (!browser || !browser->is_type_normal()) {
+    CHECK(profile_);
     // In some conditions a new browser object cannot be created. The most
     // common reason for not being able to create browser is having this call
     // when the browser process is shutting down. This can also fail if the
@@ -270,12 +273,12 @@ Browser* StartupBrowserCreatorImpl::OpenTabsInBrowser(
 
     browser = Browser::Create(params);
   }
+  CHECK(profile_);
 
   bool first_tab = true;
   bool process_headless_commands = headless::ShouldProcessHeadlessCommands();
   custom_handlers::ProtocolHandlerRegistry* registry =
-      profile_ ? ProtocolHandlerRegistryFactory::GetForBrowserContext(profile_)
-               : nullptr;
+      ProtocolHandlerRegistryFactory::GetForBrowserContext(profile_);
   for (auto& tab : tabs) {
     // We skip URLs that we'd have to launch an external protocol handler for.
     // This avoids us getting into an infinite loop asking ourselves to open
@@ -301,10 +304,13 @@ Browser* StartupBrowserCreatorImpl::OpenTabsInBrowser(
     // Headless mode is restricted to only one url in the command line, so
     // just grab the first one assuming it's the target.
     if (first_tab && process_headless_commands) {
+      auto profile_keepalive = std::make_unique<ScopedProfileKeepAlive>(
+          profile_, ProfileKeepAliveOrigin::kHeadlessCommand);
       headless::ProcessHeadlessCommands(
           profile_, tab.url,
           base::BindOnce(
               [](base::WeakPtr<Browser> browser,
+                 std::unique_ptr<ScopedProfileKeepAlive> profile_keepalive,
                  headless::HeadlessCommandHandler::Result result) {
                 if (browser && browser->window()) {
 #if BUILDFLAG(IS_MAC)
@@ -317,7 +323,7 @@ Browser* StartupBrowserCreatorImpl::OpenTabsInBrowser(
                   browser->window()->Close();
                 }
               },
-              browser->AsWeakPtr()));
+              browser->AsWeakPtr(), std::move(profile_keepalive)));
       continue;
     }
 
