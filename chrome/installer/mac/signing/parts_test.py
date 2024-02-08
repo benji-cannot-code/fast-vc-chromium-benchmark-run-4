7d560ca2e7e5a454169f90d8e44e3d29c3c22cf7
Mark Rowe <markrowe@chromium.org>
2024-02-08 21:22:20 +0000

    Add a helper tool to copy web app shims to their final destination
    When using ad-hoc signing for web app shims, the final app shim must be
    written to disk by this helper tool. This separate helper tool exists so
    that binary authorization tools, such as Santa, can transitively trust
    app shims that it creates without trusting all files written by Chrome.
    This allows app shims to be trusted by the binary authorization tool
    despite having only ad-hoc code signatures.
    
    Care is taken to ensure that the helper tool is only invoked by a
    program signed with the same code signing identity as the Chromium
    framework to ensure that the helper tool cannot be used to arbitrarily
    bypass binary authorization tools.
    
    Bug: 1465647
    Change-Id: I6fc993ca3cf74add2f68ba6e837007ec1ef0e7ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5080017
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Mark Rowe <markrowe@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1258161}

--- chrome/installer/mac/signing/parts_test.py
+++ chrome/installer/mac/signing/parts_test.py
@@ -109,6 +109,12 @@ class TestGetParts(unittest.TestCase):
             | model.CodeSignOptions.LIBRARY_VALIDATION
             | model.CodeSignOptions.KILL
             | model.CodeSignOptions.HARDENED_RUNTIME,
+            all_parts['web-app-shortcut-copier'].options)
+        self.assertEqual(
+            model.CodeSignOptions.RESTRICT
+            | model.CodeSignOptions.LIBRARY_VALIDATION
+            | model.CodeSignOptions.KILL
+            | model.CodeSignOptions.HARDENED_RUNTIME,
             all_parts['privileged-helper'].options)
 
 
