26f3d69b562cb6c8d547a6df99d0ea3ba4b13c05
Michele Fan <michelefan@chromium.org>
2024-02-08 22:05:10 +0000

    snap-group: Trigger a11y announcement when entering SVOVS
    Fixed: b/324267533
    Demo: http://b/324267533#comment2
    Test: Added new test + manual
    Change-Id: I31bc323c2a3a03162b66a83e459a4c68283d3e15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5277011
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258184}

--- ash/wm/snap_group/snap_group_unittest.cc
+++ ash/wm/snap_group/snap_group_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "ash/accessibility/magnifier/docked_magnifier_controller.h"
+#include "ash/accessibility/test_accessibility_controller_client.h"
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_pref_names.h"
 #include "ash/display/window_tree_host_manager.h"
@@ -1381,6 +1382,19 @@ TEST_F(FasterSplitScreenTest, OverviewStartActionHistogramTest) {
       kOverviewStartActionHistogram,
       OverviewStartAction::kFasterSplitScreenSetup,
       /*expected_count=*/1);
+}
+
+// Tests that a11y alert will be announced upon entering the faster split screen
+// setup session.
+TEST_F(FasterSplitScreenTest, A11yAlertOnEnteringFaterSplitScreenSetup) {
+  TestAccessibilityControllerClient client;
+  std::unique_ptr<aura::Window> window(CreateAppWindow());
+  EXPECT_NE(AccessibilityAlert::FASTER_SPLIT_SCREEN_SETUP,
+            client.last_a11y_alert());
+  SnapOneTestWindow(window.get(), chromeos::WindowStateType::kPrimarySnapped,
+                    chromeos::kDefaultSnapRatio);
+  EXPECT_EQ(AccessibilityAlert::FASTER_SPLIT_SCREEN_SETUP,
+            client.last_a11y_alert());
 }
 
 // -----------------------------------------------------------------------------
--- ash/wm/splitview/split_view_overview_session.cc
+++ ash/wm/splitview/split_view_overview_session.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 
+#include "ash/accessibility/accessibility_controller.h"
+#include "ash/public/cpp/accessibility_controller_enums.h"
 #include "ash/root_window_controller.h"
 #include "ash/shell.h"
 #include "ash/wm/overview/overview_controller.h"
@@ -71,6 +73,9 @@ void SplitViewOverviewSession::Init(std::optional<OverviewStartAction> action,
   OverviewController::Get()->StartOverview(
       action.value_or(OverviewStartAction::kFasterSplitScreenSetup),
       type.value_or(OverviewEnterExitType::kNormal));
+
+  Shell::Get()->accessibility_controller()->TriggerAccessibilityAlert(
+      AccessibilityAlert::FASTER_SPLIT_SCREEN_SETUP);
 }
 
 void SplitViewOverviewSession::RecordSplitViewOverviewSessionExitPointMetrics(
