5d2b07502ac9c59648e34ac442904ded73c0f927
Tim Sergeant <tsergeant@chromium.org>
2024-02-08 04:32:31 +0000

    AppManagement: Move PageHandler method impls to platform subclasses
    This moves the implementation of most platform-specific PageHandler
    methods to their platform-specific subclasses. Some methods which share
    an implementation remain in the Base class.
    
    Bug: b/317152362
    Change-Id: Ifa41565aa45dc617eb198f19fe9491764c904de0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5274682
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257778}

--- chrome/browser/ui/webui/app_management/app_management_page_handler_chromeos.h
+++ chrome/browser/ui/webui/app_management/app_management_page_handler_chromeos.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_APP_MANAGEMENT_APP_MANAGEMENT_PAGE_HANDLER_CHROMEOS_H_
 #define CHROME_BROWSER_UI_WEBUI_APP_MANAGEMENT_APP_MANAGEMENT_PAGE_HANDLER_CHROMEOS_H_
 
+#include <string>
+
 #include "chrome/browser/ui/webui/app_management/app_management_page_handler_base.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -28,6 +30,23 @@ class AppManagementPageHandlerChromeOs : public AppManagementPageHandlerBase {
       const AppManagementPageHandlerChromeOs&) = delete;
 
   ~AppManagementPageHandlerChromeOs() override;
+
+  // app_management::mojom::PageHandler:
+  void SetResizeLocked(const std::string& app_id, bool locked) override;
+  void SetPreferredApp(const std::string& app_id,
+                       bool is_preferred_app) override;
+  void GetOverlappingPreferredApps(
+      const std::string& app_id,
+      GetOverlappingPreferredAppsCallback callback) override;
+  void SetWindowMode(const std::string& app_id,
+                     apps::WindowMode window_mode) override;
+  void SetRunOnOsLoginMode(
+      const std::string& app_id,
+      apps::RunOnOsLoginMode run_on_os_login_mode) override;
+  void ShowDefaultAppAssociationsUi() override;
+  void OpenStorePage(const std::string& app_id) override;
+  void SetAppLocale(const std::string& app_id,
+                    const std::string& locale_tag) override;
 };
 
 #endif  // CHROME_BROWSER_UI_WEBUI_APP_MANAGEMENT_APP_MANAGEMENT_PAGE_HANDLER_CHROMEOS_H_
