55bc3026e7586319b514b7c1adc686e668f27d4d
Brahim Chikhaoui <bchikhaoui@google.com>
2024-02-08 13:17:13 +0000

    [OOBE] Fix the  'Use as personal device' button on EnrollmentScreen
    Fix the shelf button 'Use as personal device' in the enrollment page to
    takes the user to the userCreationScreen with the default step creation
    with pre-select 'for personal use'. nuke clearing the user choices
    on beforeShow method.
    
    Bug: b:308603398
    Change-Id: Iccfe22189bf2fdd66444126978c81f264eae8dd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5272396
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1257880}

--- chrome/browser/ui/webui/ash/login/user_creation_screen_handler.cc
+++ chrome/browser/ui/webui/ash/login/user_creation_screen_handler.cc
@@ -103,6 +103,10 @@ void UserCreationScreenHandler::Show() {
   ShowInWebUI();
 }
 
+void UserCreationScreenHandler::SetDefaultStep() {
+  CallExternalAPI("setDefaultStep");
+}
+
 void UserCreationScreenHandler::SetTriageStep() {
   CallExternalAPI("setTriageStep");
 }
