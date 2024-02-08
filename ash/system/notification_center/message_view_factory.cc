c3d2c03b9e681fe4818a862e368cf366f88c903a
Ahmed Mehfooz <amehfooz@chromium.org>
2024-02-08 18:34:44 +0000

    Add new conversation notification type
    This new type will be used by Arc
    to create a new view which is designed
    specifically to display conversations
    
    Bug: b:284512022
    Change-Id: I13488491506584788f29de4d25b1e799c4aae91b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5258447
    Reviewed-by: Andre Le <leandre@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258040}

--- ash/system/notification_center/message_view_factory.cc
+++ ash/system/notification_center/message_view_factory.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/ash_features.h"
 #include "ash/system/notification_center/views/ash_notification_view.h"
+#include "ash/system/notification_center/views/conversation_notification_view.h"
 #include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "build/chromeos_buildflags.h"
@@ -50,6 +51,8 @@ std::unique_ptr<message_center::MessageView> MessageViewFactory::Create(
       break;
     case message_center::NOTIFICATION_TYPE_CUSTOM:
       return GetCustomNotificationView(notification, shown_in_popup);
+    case message_center::NOTIFICATION_TYPE_CONVERSATION:
+      return std::make_unique<ConversationNotificationView>(notification);
     default:
       // If the caller asks for an unrecognized kind of view (entirely possible
       // if an application is running on an older version of this code that
--- ui/message_center/public/cpp/notification_types.h
+++ ui/message_center/public/cpp/notification_types.h
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace message_center {
 
-// Notification types. Note that the values in this enumeration are being
-// recoded in a histogram, updates should not change the entries' values.
+// Notification types. Used to determine the view that will represent a
+// notification.
 enum NotificationType {
   NOTIFICATION_TYPE_SIMPLE = 0,
   DEPRECATED_NOTIFICATION_TYPE_BASE_FORMAT =
@@ -18,9 +18,10 @@ enum NotificationType {
   NOTIFICATION_TYPE_MULTIPLE = 3,
   NOTIFICATION_TYPE_PROGRESS = 4,  // Notification with progress bar.
   NOTIFICATION_TYPE_CUSTOM = 5,
+  NOTIFICATION_TYPE_CONVERSATION = 6,
 
   // Add new values before this line.
-  NOTIFICATION_TYPE_LAST = NOTIFICATION_TYPE_CUSTOM
+  NOTIFICATION_TYPE_LAST = NOTIFICATION_TYPE_CONVERSATION
 };
 
 enum NotificationPriority {
