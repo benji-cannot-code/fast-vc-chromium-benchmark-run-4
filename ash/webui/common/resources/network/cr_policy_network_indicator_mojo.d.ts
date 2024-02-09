b3c65a0eba5e83feec2b6a03b15f3238345c6257
Moteva <moteva@google.com>
2024-02-09 19:08:53 +0000

    CrOS Settings: Covert cellular_roaming_toggle_button_test to ts
    Bug: b:270728282, b:322862072
    Test: browser_test --gtest_filter="*CellularRoamingToggleButton*"
    Change-Id: If31cde2f7ada4ca206a19246b948bdb685eec7c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5273809
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Nikki Moteva <moteva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1258658}

--- ash/webui/common/resources/network/cr_policy_network_indicator_mojo.d.ts
+++ ash/webui/common/resources/network/cr_policy_network_indicator_mojo.d.ts
@@ -3,4 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-export {};
+import {LegacyElementMixin} from '//resources/polymer/v3_0/polymer/lib/legacy/legacy-element-mixin.js';
+
+export interface CrPolicyNetworkIndicatorMojoElement extends
+    HTMLElement, LegacyElementMixin {}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'cr-policy-network-indicator-mojo': CrPolicyNetworkIndicatorMojoElement;
+  }
+}
