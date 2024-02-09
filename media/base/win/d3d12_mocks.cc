800a523a967b2fc4a2ab48a9c29aa19234ebc30d
Zhibo Wang <zhibo1.wang@intel.com>
2024-02-09 07:07:56 +0000

    Reland "Reland "Create D3D12 video decoder""
    This is a reland of commit 9d0030bc5288197aed6dc889135811f908cf2abe
    
    Fix another compile error in tests.
    
    Original change's description:
    > Reland "Create D3D12 video decoder"
    >
    > This is a reland of commit 0e26dbaaeb2ea8e6afc757939caba6a25c408de5
    >
    > Add an if-macro to prevent compile error when HEVC is not enabled.
    >
    > Original change's description:
    > > Create D3D12 video decoder
    > >
    > > Since the rendering side don't accept a D3D12Resource now, the d3d12
    > > video decoder will use D3D11Texture2D as picture buffer, open it in
    > > D3D12 by shared handle, do D3D12 video decoding and close the handle,
    > > then let the D3D11Texture2D follow the current rendering pipeline.
    > > In this case, the D3D12 video decoder backend reuses most of
    > > media::D3D11VideoDecoder's methods, only replacing the wrapper with a
    > > D3D12 one.
    > >
    > > Bug: 1348104, 1434145
    > > Change-Id: I8d9b5274197195242aed320e9fb33a9746e8cf55
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4410406
    > > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > > Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    > > Commit-Queue: Jianlin Qiu <jianlin.qiu@intel.com>
    > > Cr-Commit-Position: refs/heads/main@{#1237903}
    >
    > Bug: 1348104, 1434145
    > Change-Id: I473a2bb3c5e8e4b3eb9f761674177cafbd65fed2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5125691
    > Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    > Commit-Queue: Zhibo Wang <zhibo1.wang@intel.com>
    > Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1241131}
    
    Bug: 1348104, 1434145
    Change-Id: Ia3f242601882699f3ae468cfbff12c7c6f51380e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5154106
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Commit-Queue: Zhibo Wang <zhibo1.wang@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1258380}

---
+++ media/base/win/d3d12_mocks.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/base/win/d3d12_mocks.h"
+
+namespace media {
+
+D3D12DeviceMock::D3D12DeviceMock() = default;
+D3D12DeviceMock::~D3D12DeviceMock() = default;
+
+D3D12ResourceMock::D3D12ResourceMock() = default;
+D3D12ResourceMock::~D3D12ResourceMock() = default;
+
+}  // namespace media
