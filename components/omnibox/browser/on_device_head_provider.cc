eab7b8103d122889e05412657866e0045e17abb3
Ce Chen <cch@chromium.org>
2024-02-09 16:17:37 +0000

    [omnibox] add a flag to toggle on device tail model for single word prefix.
    Enabled (current):
    http://screenshot/Bv9ixEV2djzjrVr (Desktop)
    http://screenshot/9pANmDtr62F4ZCs (Android)
    Disabled (after):
    http://screenshot/mPV7vt8ZZgpB9ny
    
    This should be disabled by default for now (maybe except CJK in future):
    1. UI is not working properly: "..." is being added before the entire suggestion on Android, and is missing on Desktop.
    2. Qualities of the single word prefixes not covered by head model are usually low, and their corresponding tail suggestions are also bad. The issue might be mitigated by adjusting the probability threshold, but we are not sure how this will affect overall coverage of the tail model.
    
    Bug: 5274434
    Change-Id: I7a08a41cafe82d383ca5a73e10485a0b2c599c7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5273000
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: C Ch <cch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258536}

--- components/omnibox/browser/on_device_head_provider.cc
+++ components/omnibox/browser/on_device_head_provider.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <limits>
 
+#include "base/containers/contains.h"
 #include "base/files/file_enumerator.h"
 #include "base/files/file_util.h"
 #include "base/i18n/case_conversion.h"
@@ -257,11 +258,7 @@ void OnDeviceHeadProvider::HeadModelSearchDone(
     return;
   }
 
-  bool should_fetch_tail_suggestions =
-      (base::GetFieldTrialParamByFeatureAsBool(
-           omnibox::kOnDeviceTailModel, "MixHeadAndTailSuggestions", false) ||
-       params->suggestions.empty());
-  if (!should_fetch_tail_suggestions) {
+  if (!ShouldFetchTailSuggestions(*params)) {
     AllSearchDone(std::move(params));
     return;
   }
@@ -374,4 +371,30 @@ std::string OnDeviceHeadProvider::GetOnDeviceHeadModelFilename() const {
   return model_update_listener != nullptr
              ? model_update_listener->head_model_filename()
              : "";
+}
+
+// static
+bool OnDeviceHeadProvider::ShouldFetchTailSuggestions(
+    const OnDeviceHeadProviderParams& params) {
+  if (!base::GetFieldTrialParamByFeatureAsBool(
+          omnibox::kOnDeviceTailModel, "EnableForSingleWordPrefix", false)) {
+    std::string sanitized_input = SanitizeInput(params.input.text());
+    // Determines if the prefix contains multiple words by checking if it has
+    // whitespaces; Note this does not work when the prefix is not using
+    // whitespace as delimiter, e.g. CJK languages.
+    bool is_single_word_prefix = !base::Contains(sanitized_input, " ");
+    if (is_single_word_prefix) {
+      return false;
+    }
+  }
+
+  // Always triggers tail model when head suggestion does not present.
+  if (params.suggestions.empty()) {
+    return true;
+  }
+
+  // Now allows triggering tail model even if head suggestions are available, if
+  // the flag is set.
+  return base::GetFieldTrialParamByFeatureAsBool(
+      omnibox::kOnDeviceTailModel, "MixHeadAndTailSuggestions", false);
 }
