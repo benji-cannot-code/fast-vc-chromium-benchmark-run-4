ed5ad03e9099c71ebae85dfcfb2183730aeef8a6
Sinan Sahin <sinansahin@google.com>
2024-02-09 23:01:58 +0000

    [MCT] Clean up Android O API level checks
    Bug: 40941439
    Change-Id: I81e033edd1c29801d13c7c41221559a046917583
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5282584
    Reviewed-by: Kevin Grosu <kgrosu@google.com>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1258785}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/minimizedcustomtab/CustomTabMinimizationManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/minimizedcustomtab/CustomTabMinimizationManager.java
@@ -21,7 +21,6 @@ import android.os.SystemClock;
 import android.util.Rational;
 
 import androidx.annotation.IntDef;
-import androidx.annotation.RequiresApi;
 import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.PictureInPictureModeChangedInfo;
@@ -57,7 +56,6 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /** Class that manages minimizing a Custom Tab into picture-in-picture. */
-@RequiresApi(VERSION_CODES.O)
 public class CustomTabMinimizationManager
         implements CustomTabMinimizeDelegate,
                 Consumer<PictureInPictureModeChangedInfo>,
--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/minimizedcustomtab/MinimizedFeatureUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/minimizedcustomtab/MinimizedFeatureUtils.java
@@ -16,7 +16,6 @@ import android.text.TextUtils;
 import androidx.annotation.DrawableRes;
 import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.ResettersForTesting;
@@ -54,7 +53,6 @@ public class MinimizedFeatureUtils {
     // numeric values should never be reused.
     @IntDef({
         MinimizedFeatureAvailability.AVAILABLE,
-        MinimizedFeatureAvailability.UNAVAILABLE_API_LEVEL,
         MinimizedFeatureAvailability.UNAVAILABLE_LOW_END_DEVICE,
         MinimizedFeatureAvailability.UNAVAILABLE_SYSTEM_FEATURE,
         MinimizedFeatureAvailability.UNAVAILABLE_PIP_PERMISSION,
@@ -64,7 +62,7 @@ public class MinimizedFeatureUtils {
     @VisibleForTesting
     @interface MinimizedFeatureAvailability {
         int AVAILABLE = 0;
-        int UNAVAILABLE_API_LEVEL = 1;
+        // (Obsolete) int UNAVAILABLE_API_LEVEL = 1;
         int UNAVAILABLE_LOW_END_DEVICE = 2;
         int UNAVAILABLE_SYSTEM_FEATURE = 3;
         int UNAVAILABLE_PIP_PERMISSION = 4;
@@ -115,10 +113,7 @@ public class MinimizedFeatureUtils {
 
         @MinimizedFeatureAvailability int availability = MinimizedFeatureAvailability.AVAILABLE;
         sIsDeviceEligibleForMinimizedCustomTab = true;
-        if (VERSION.SDK_INT < VERSION_CODES.O) {
-            availability = MinimizedFeatureAvailability.UNAVAILABLE_API_LEVEL;
-            sIsDeviceEligibleForMinimizedCustomTab = false;
-        } else if (SysUtils.isLowEndDevice()) {
+        if (SysUtils.isLowEndDevice()) {
             availability = MinimizedFeatureAvailability.UNAVAILABLE_LOW_END_DEVICE;
             sIsDeviceEligibleForMinimizedCustomTab = false;
         } else if (!context.getPackageManager()
@@ -141,7 +136,6 @@ public class MinimizedFeatureUtils {
         return sIsDeviceEligibleForMinimizedCustomTab;
     }
 
-    @RequiresApi(api = VERSION_CODES.O)
     private static boolean isPipAllowed(Context context) {
         AppOpsManager appOpsManager =
                 (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
