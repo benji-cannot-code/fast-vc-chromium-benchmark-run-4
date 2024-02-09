e56fd384844d01c646b394bfc9ba8a4dddbeb58d
Darren Shen <shend@chromium.org>
2024-02-09 00:49:25 +0000

    Add accessible name to PickerGifView.
    This exposes the view to the accessibility tree.
    
    Bug: b/316936743
    Change-Id: I8cacde36e344eaa6e6ca242c7445578ab5021021
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5280996
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1258282}

--- ash/picker/views/picker_gif_view.h
+++ ash/picker/views/picker_gif_view.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/views/controls/image_view.h"
+#include "ui/views/metadata/view_factory.h"
 #include "ui/views/view.h"
 
 namespace ash {
@@ -32,7 +33,8 @@ class ASH_EXPORT PickerGifView : public views::ImageView {
   using FramesFetcher = base::OnceCallback<void(FramesFetchedCallback)>;
 
   PickerGifView(FramesFetcher frames_fetcher,
-                const gfx::Size& original_dimensions);
+                const gfx::Size& original_dimensions,
+                std::u16string accessible_name);
   PickerGifView(const PickerGifView&) = delete;
   PickerGifView& operator=(const PickerGifView&) = delete;
   ~PickerGifView() override;
@@ -61,6 +63,11 @@ class ASH_EXPORT PickerGifView : public views::ImageView {
   base::WeakPtrFactory<PickerGifView> weak_factory_{this};
 };
 
+BEGIN_VIEW_BUILDER(ASH_EXPORT, PickerGifView, views::ImageView)
+END_VIEW_BUILDER
+
 }  // namespace ash
+
+DEFINE_VIEW_BUILDER(ASH_EXPORT, ash::PickerGifView)
 
 #endif  // ASH_PICKER_VIEWS_PICKER_GIF_VIEW_H_
