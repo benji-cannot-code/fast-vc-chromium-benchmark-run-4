41d3364eb4bee3f9aa778d148aafaf23f4736b70
Stephen McGruer <smcgruer@chromium.org>
2024-02-09 00:48:17 +0000

    [Autofill] Convert all full server cards to masked cards
    Full server cards (that is, server cards whose full number has
    been stored locally in the AutofillTable) were deprecated in early
    2020. At that time, it stopped being possible to *add* a new full
    server card, but existing cards were kept.
    
    This change removes full server cards entirely. This is done by
    dropping the unmasked_credit_cards table. Doing so causes those
    cards to become 'normal' masked server cards instead.
    
    This will be a ONE WAY street. Once landed, user data will be deleted
    on start-up and they won't be able to re-cache the cards locally.
    
    Bug: 1497734
    Change-Id: I09ee81bcabedab750521e3ec57eb0ba0646656c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5251343
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258280}

--- components/webdata/common/web_database.cc
+++ components/webdata/common/web_database.cc
@@ -17,8 +17,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // corresponding changes must happen in the unit tests, and new migration test
 // added.  See `WebDatabaseMigrationTest::kCurrentTestedVersionNumber`.
 // static
-const int WebDatabase::kCurrentVersionNumber = 124;
+const int WebDatabase::kCurrentVersionNumber = 125;
 
+// TODO(crbug.com/324468207): Decide and document a process for updating this
+// number, i.e. what our deprecation strategy is for older versions of the
+// database.
 const int WebDatabase::kDeprecatedVersionNumber = 82;
 
 const base::FilePath::CharType WebDatabase::kInMemoryPath[] =
@@ -46,10 +49,9 @@ void LogInitResult(WebDatabaseInitResult result) {
   base::UmaHistogramEnumeration("WebDatabase.InitResult", result);
 }
 
-// Version 124 deletes 'payment_instruments', 'payment_instruments_meetadata',
-// 'payment_instrument_supported_rails', 'bank_accounts' table and thus is no
-// longer compatible with version 123.
-const int kCompatibleVersionNumber = 124;
+// Version 125 deletes the 'unmasked_credit_cards' table, and thus is no longer
+// compatible with version 124.
+const int kCompatibleVersionNumber = 125;
 
 // Change the version number and possibly the compatibility version of
 // |meta_table_|.
