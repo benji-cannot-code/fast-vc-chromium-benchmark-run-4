5f4b47dbb68fea7d4ebc220007e2135df2a3d237
Sergey Poromov <poromov@chromium.org>
2024-02-09 09:50:58 +0000

    Move ScreenCapturePolicyHandler to chrome/browser/ash.
    Moving the handler to more ash-specific place from
    which it'll be able to set ash prefs.
    
    Bug: b/323146997
    Test: Passes trybots.
    Change-Id: Ic3207a55822ecda4750cc72b23763de76be01123
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5273511
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Auto-Submit: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258418}

--- chrome/browser/ash/policy/handlers/screen_capture_location_policy_handler.h
+++ chrome/browser/ash/policy/handlers/screen_capture_location_policy_handler.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_POLICY_SCREEN_CAPTURE_LOCATION_POLICY_HANDLER_H_
-#define CHROME_BROWSER_POLICY_SCREEN_CAPTURE_LOCATION_POLICY_HANDLER_H_
+#ifndef CHROME_BROWSER_ASH_POLICY_HANDLERS_SCREEN_CAPTURE_LOCATION_POLICY_HANDLER_H_
+#define CHROME_BROWSER_ASH_POLICY_HANDLERS_SCREEN_CAPTURE_LOCATION_POLICY_HANDLER_H_
 
 #include "components/policy/core/browser/configuration_policy_handler.h"
 
@@ -33,4 +33,4 @@ class ScreenCaptureLocationPolicyHandler : public TypeCheckingPolicyHandler {
 
 }  // namespace policy
 
-#endif  // CHROME_BROWSER_POLICY_SCREEN_CAPTURE_LOCATION_POLICY_HANDLER_H_
+#endif  // CHROME_BROWSER_ASH_POLICY_HANDLERS_SCREEN_CAPTURE_LOCATION_POLICY_HANDLER_H_
