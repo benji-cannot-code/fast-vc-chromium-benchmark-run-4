b6c6b21095acbc482762d5fb60722624eaaf2671
Takuto Ikuta <tikuta@chromium.org>
2024-02-09 09:01:41 +0000

    Revert "content: add an wrapper for keyboard_lock_service_impl"
    This reverts commit c03ba0c3fa1d34742a19363a617ac35c68513a22.
    
    Reason for revert:
    https://groups.google.com/a/chromium.org/g/content-owners/c/Derds-ksWGM#:~:text=in%20render_frame_impl.h.-,In%20the%20meantime%20can%20we%20revert%20the%20other%20changes%3F,-For%20future%20problems
    
    Original change's description:
    > content: add an wrapper for keyboard_lock_service_impl
    >
    > This is to remove one indirect include of render_frame_host_impl.h
    > from browser_interface_binders.cc to reduce compile time in critical
    > path of build and utilize build parallelism more for faster build.
    >
    > This is extracted from https://crrev.com/c/5252622.
    >
    > Bug: b/322102823
    > Change-Id: I4cfd55855ef267b747ab85c141533a3bf524ae35
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5256384
    > Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    > Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    > Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1256626}
    
    Bug: b/322102823
    Change-Id: Idff5df4c290b37dd41ab53e637e1248e3e04a1d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5273391
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258405}

--- content/browser/browser_interface_binders.cc
+++ content/browser/browser_interface_binders.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/background_fetch/background_fetch_service_impl.h"
 #include "content/browser/bad_message.h"
 #include "content/browser/binder_wrappers/ad_auction_service_impl_wrapper.h"
-#include "content/browser/binder_wrappers/keyboard_lock_service_impl_wrapper.h"
 #include "content/browser/bluetooth/web_bluetooth_service_impl.h"
 #include "content/browser/browser_context_impl.h"
 #include "content/browser/browser_main_loop.h"
@@ -36,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/image_capture/image_capture_impl.h"
 #include "content/browser/indexed_db/indexed_db_internals.mojom.h"
 #include "content/browser/indexed_db/indexed_db_internals_ui.h"
+#include "content/browser/keyboard_lock/keyboard_lock_service_impl.h"
 #include "content/browser/loader/content_security_notifier.h"
 #include "content/browser/media/media_web_contents_observer.h"
 #include "content/browser/media/midi_host.h"
@@ -1171,9 +1171,8 @@ void PopulateBinderMapWithContext(
       base::BindRepeating(&CookieStoreManager::BindReceiverForFrame));
   map->Add<blink::mojom::ContentIndexService>(
       base::BindRepeating(&ContentIndexServiceImpl::CreateForFrame));
-
-  KeyboardLockServiceImplCreateMojoService(*map);
-
+  map->Add<blink::mojom::KeyboardLockService>(
+      base::BindRepeating(&KeyboardLockServiceImpl::CreateMojoService));
   if (base::FeatureList::IsEnabled(blink::features::kInterestGroupStorage)) {
     AdAuctionServiceImplCreateMojoService(*map);
   }
