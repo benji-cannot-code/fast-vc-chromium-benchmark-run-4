e88cd2679845f2c12e052e41a938833bf84fff51
Brandon Fong <bjfong@google.com>
2024-02-09 15:25:36 +0000

    [Tab Group Parity] Add a tab switcher method to refresh the tab list
    Add a method to the tab switcher interface that will refresh the tab switcher's tab list when an update to the UI is required.
    
    Bug: 41490324
    Change-Id: I813c5b66b48bca92d075097a0f57ec9531e652a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5274440
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Brandon Fong <bjfong@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258500}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherCoordinator.java
@@ -445,6 +445,11 @@ public class TabSwitcherCoordinator
     }
 
     @Override
+    public void refreshTabList() {
+        mMediator.refreshTabList();
+    }
+
+    @Override
     public int getTabListTopOffset() {
         return mTabListCoordinator.getTabListTopOffset();
     }
