f946ef57a1682418ace737b88e575e4120257e94
Alison Gale <agale@chromium.org>
2024-02-09 18:51:55 +0000

    Re-enable background tab loading feature to reduce flakiness
    When turning off this abandoned feature, flakiness for this test
    increased. I will turn it back on temporarily while the owners of the
    tab loader can investigate.
    
    Bug: 323780452
    Change-Id: I8e21d07aea0c7cd731a46c185e81433fcb8106c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5281987
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258645}

--- chrome/test/media_router/access_code_cast/access_code_cast_integration_browsertest.cc
+++ chrome/test/media_router/access_code_cast/access_code_cast_integration_browsertest.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/media_router/browser/media_router_factory.h"
 #include "components/media_router/common/test/test_helper.h"
+#include "components/performance_manager/public/features.h"
 #include "components/sessions/content/session_tab_helper.h"
 #include "components/signin/public/identity_manager/identity_test_utils.h"
 #include "content/public/browser/browser_task_traits.h"
@@ -108,7 +109,12 @@ AccessCodeCastIntegrationBrowserTest::AccessCodeCastIntegrationBrowserTest()
     : url_to_intercept_(std::string(kDefaultDiscoveryEndpoint) +
                         kDiscoveryServicePath),
       mock_cast_socket_service_(nullptr, base::OnTaskRunnerDeleter(nullptr)) {
-  feature_list_.InitAndEnableFeature(features::kAccessCodeCastUI);
+  // TODO(crbug.com/323780452): Remove performance manager feature after deflake
+  feature_list_.InitWithFeatures(
+      {features::kAccessCodeCastUI,
+       performance_manager::features::
+           kBackgroundTabLoadingFromPerformanceManager},
+      {});
   task_runner_ = base::MakeRefCounted<base::TestMockTimeTaskRunner>();
 }
 
