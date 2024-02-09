d89943f04ff1f46848e17f5c261ee0e36ea36c22
Peter Kasting <pkasting@chromium.org>
2024-02-09 03:13:12 +0000

    Pull the IsComplete concept into its own header.
    This will be useful in another CL I'm writing.
    
    Bug: 1409914
    Change-Id: I2e091ef752103385ef6fe91d2d5a739a0203f920
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5280946
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258338}

---
+++ base/types/is_complete.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_TYPES_IS_COMPLETE_H_
+#define BASE_TYPES_IS_COMPLETE_H_
+
+#include <type_traits>
+
+namespace base {
+
+// True if `T` is completely defined.
+template <typename T>
+concept IsComplete = requires { sizeof(T); } ||
+                     // Function types must be included explicitly since you
+                     // cannot apply `sizeof()` to a function type.
+                     std::is_function_v<std::remove_cvref_t<T>>;
+
+}  // namespace base
+
+#endif  // BASE_TYPES_IS_COMPLETE_H_
