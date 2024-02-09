ae5b27b93703fae7e465011135513953fbc1cdc4
L. David Baron <dbaron@chromium.org>
2024-02-09 00:53:10 +0000

    Make calc-size() influence Length::IsContentOrIntrinsic().
    Bug: 313072
    Change-Id: I994c53fa615794606766a4bb15c351541f43fd76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5226442
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258286}

--- third_party/blink/renderer/platform/geometry/calculation_value.cc
+++ third_party/blink/renderer/platform/geometry/calculation_value.cc
@@ -149,4 +149,8 @@ bool CalculationValue::HasAutoAnchorPositioning() const {
   return IsExpression() && data_.expression->HasAutoAnchorPositioning();
 }
 
+bool CalculationValue::HasContentOrIntrinsicSize() const {
+  return IsExpression() && data_.expression->HasContentOrIntrinsicSize();
+}
+
 }  // namespace blink
--- third_party/blink/renderer/platform/geometry/calculation_value.h
+++ third_party/blink/renderer/platform/geometry/calculation_value.h
@@ -70,6 +70,7 @@ class PLATFORM_EXPORT CalculationValue : public RefCounted<CalculationValue> {
   }
   bool HasAnchorQueries() const;
   bool HasAutoAnchorPositioning() const;
+  bool HasContentOrIntrinsicSize() const;
 
   float Pixels() const {
     DCHECK(!IsExpression());
