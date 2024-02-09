6382715723539a44337bdc5438af5c00756a3a7d
ssid <ssid@chromium.org>
2024-02-09 00:53:40 +0000

    Add a new bottom sheet for page info share action.
    Adds MVC for the new bottom sheet, without any of the logic.
    
    Bug: b/323920042
    Change-Id: If14f52835b989a39fe192f3ee015685eb9392763
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5274701
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1258287}

---
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/page_info_sheet/PageInfoBottomSheetCoordinator.java
@@ -0,0 +1,41 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.share.page_info_sheet;
+
+import android.content.Context;
+
+import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
+
+/**
+ * Coordinator of the page info bottom sheet.
+ *
+ * <p>This component shows a bottom sheet to let the add page info to shared links.
+ */
+public class PageInfoBottomSheetCoordinator {
+    private final Context mContext;
+    private final PageInfoBottomSheetMediator mMediator;
+
+    /**
+     * Creates the coordinator.
+     *
+     * @param context The context for this component.
+     * @param bottomSheetController The bottom sheet controller where this bottom sheet will be
+     *     shown.
+     */
+    public PageInfoBottomSheetCoordinator(
+            Context context, BottomSheetController bottomSheetController) {
+        mContext = context;
+        mMediator =
+                new PageInfoBottomSheetMediator(
+                        new PageInfoBottomSheet(context), bottomSheetController);
+    }
+
+    /** Request to show the bottom sheet. */
+    public void requestShowContent() {}
+
+    /** Destroys this component hiding the bottom sheet if needed. */
+    public void destroy() {}
+}
---
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/page_info_sheet/PageInfoBottomSheetProperties.java
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.share.page_info_sheet;
+
+import org.chromium.ui.modelutil.PropertyModel;
+
+/** Data properties for the page info bottom sheet contents. */
+final class PageInfoBottomSheetProperties {
+    private PageInfoBottomSheetProperties() {}
+
+    static PropertyModel.Builder defaultModelBuilder() {
+        return new PropertyModel.Builder();
+    }
+}
