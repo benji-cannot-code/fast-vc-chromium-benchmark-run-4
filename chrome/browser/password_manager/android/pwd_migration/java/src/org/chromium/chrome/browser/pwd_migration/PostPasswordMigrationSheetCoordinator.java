63c07dd60f2d137cbe674c6ab3e2945621ee422a
Ivana Žužić <izuzic@google.com>
2024-02-09 18:27:10 +0000

    [UPMLocalPwd] Add the post-migration sheet coordinator
    The coordinator initializes the mediator.
    The bridge will use the coordinator for showing the post-migration sheet
    from cpp code.
    
    Bug: b/323821929
    Change-Id: Ieb1bc95fd84e71a3d105088c0a042575f7829ad8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5272428
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258624}

---
+++ chrome/browser/password_manager/android/pwd_migration/java/src/org/chromium/chrome/browser/pwd_migration/PostPasswordMigrationSheetCoordinator.java
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.pwd_migration;
+
+import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
+import org.chromium.ui.modelutil.PropertyModel;
+
+/** The coordinator of the post password migration sheet. */
+public class PostPasswordMigrationSheetCoordinator {
+    private final PostPasswordMigrationSheetMediator mMediator =
+            new PostPasswordMigrationSheetMediator();
+
+    public PostPasswordMigrationSheetCoordinator(BottomSheetController sheetController) {
+        mMediator.initialize(
+                PostPasswordMigrationSheetProperties.createDefaultModel(mMediator::onDismissed));
+    }
+
+    public void showSheet() {
+        mMediator.showSheet();
+    }
+
+    PropertyModel getModelForTesting() {
+        return mMediator.getModel();
+    }
+}
