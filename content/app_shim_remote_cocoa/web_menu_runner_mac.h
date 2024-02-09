a0b4bf5e2fac786960afa9b4d18b917aae82d098
Marijn Kruisselbrink <mek@chromium.org>
2024-02-09 01:41:32 +0000

    Add remote-cocoa support to <select> popup menus.
    Rather than creating a WebMenuRunner/NSMenu directly in the
    browser process, this has the browser process forward popup menu
    requests from the renderer to the correct remote cocoa process,
    proxying between the two.
    
    Bug: 40073873
    Change-Id: Idff0f6c56e3ef8cc1562c3e79940d0f6552bce46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5270062
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258312}

--- content/app_shim_remote_cocoa/web_menu_runner_mac.h
+++ content/app_shim_remote_cocoa/web_menu_runner_mac.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_BROWSER_RENDERER_HOST_WEB_MENU_RUNNER_MAC_H_
-#define CONTENT_BROWSER_RENDERER_HOST_WEB_MENU_RUNNER_MAC_H_
+#ifndef CONTENT_APP_SHIM_REMOTE_COCOA_WEB_MENU_RUNNER_MAC_H_
+#define CONTENT_APP_SHIM_REMOTE_COCOA_WEB_MENU_RUNNER_MAC_H_
 
 #import <Cocoa/Cocoa.h>
 
@@ -48,4 +48,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @end  // @interface WebMenuRunner
 
-#endif  // CONTENT_BROWSER_RENDERER_HOST_WEB_MENU_RUNNER_MAC_H_
+#endif  // CONTENT_APP_SHIM_REMOTE_COCOA_WEB_MENU_RUNNER_MAC_H_
--- content/browser/renderer_host/popup_menu_helper_mac.h
+++ content/browser/renderer_host/popup_menu_helper_mac.h
@@ -10,20 +10,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/memory/raw_ptr.h"
-#include "base/memory/raw_ptr_exclusion.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "content/common/content_export.h"
+#include "content/common/render_widget_host_ns_view.mojom.h"
 #include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/render_widget_host_observer.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/blink/public/mojom/choosers/popup_menu.mojom.h"
 #include "ui/gfx/geometry/rect.h"
-
-namespace base {
-class ScopedPumpMessagesInPrivateModes;
-}
 
 namespace content {
 
@@ -71,6 +67,8 @@ class PopupMenuHelper : public RenderWidgetHostObserver {
                                          bool became_visible) override;
   void RenderWidgetHostDestroyed(RenderWidgetHost* widget_host) override;
 
+  void PopupMenuClosed(std::optional<uint32_t> selected_item);
+
   RenderWidgetHostViewMac* GetRenderWidgetHostView() const;
 
   raw_ptr<Delegate> delegate_;  // Weak. Owns |this|.
@@ -82,11 +80,7 @@ class PopupMenuHelper : public RenderWidgetHostObserver {
 
   bool popup_was_hidden_ = false;
 
-  // Controls whether messages can be pumped during the menu fade.
-  std::unique_ptr<base::ScopedPumpMessagesInPrivateModes> pump_in_fade_;
-
-  struct ObjCStorage;
-  std::unique_ptr<ObjCStorage> objc_storage_;
+  mojo::Remote<remote_cocoa::mojom::PopupMenuRunner> remote_runner_;
 
   base::WeakPtrFactory<PopupMenuHelper> weak_ptr_factory_{this};
 };
