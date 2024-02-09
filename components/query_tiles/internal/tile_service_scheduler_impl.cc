68f74fdb8b3eaa43050fcaf1be98d581000ddc56
Tomasz Wiszkowski <ender@google.com>
2024-02-09 17:22:09 +0000

    Link Query Tiles Instant Fetch to QueryTilesInZPSOnNTP feature.
    This change proposes that the QueryTile fetches are not delayed when
    the Omnibox feature QueryTilesInZPSOnNTP is enabled.
    
    The retrieval of QueryTiles is delayed by up to 12h, during which we
    would not be showing any tiles in the Omnibox. If the user leaves and
    kills the application before the fetch is complete, the scheduling is
    restarted the next time Chrome is opened.
    
    If Chrome is never kept alive for long enough, the users may never see
    QueryTiles.
    
    Omnibox caches internally QT data for (a configurable duration of) 8h.
    Once QueryTiles data is retrieved, the Omnibox will not request the
    update until that timeout expires (or until Chrome is killed).
    
    Change-Id: I602f31e47cd9cf190debf594cdb8e1f675483999
    Fixed: b:324142195
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5279811
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1258576}

--- components/query_tiles/internal/tile_service_scheduler_impl.cc
+++ components/query_tiles/internal/tile_service_scheduler_impl.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/default_tick_clock.h"
 #include "base/time/time.h"
 #include "components/background_task_scheduler/background_task_scheduler.h"
+#include "components/omnibox/common/omnibox_features.h"
 #include "components/prefs/pref_service.h"
 #include "components/query_tiles/internal/stats.h"
 #include "components/query_tiles/internal/tile_config.h"
@@ -27,8 +28,9 @@ const int kInstantScheduleWindowStartMs = 10 * 1000;  // 10 seconds
 const int kInstantScheduleWindowEndMs = 20 * 1000;    // 20 seconds
 
 bool IsInstantFetchMode() {
-  return base::CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kQueryTilesInstantBackgroundTask);
+  return base::FeatureList::IsEnabled(omnibox::kQueryTilesInZPSOnNTP) ||
+         base::CommandLine::ForCurrentProcess()->HasSwitch(
+             switches::kQueryTilesInstantBackgroundTask);
 }
 
 }  // namespace
