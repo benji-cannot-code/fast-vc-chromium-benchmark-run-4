726462214aa06aa3fed5cd3b7a1c9e10e42e16e9
Jonathan Lee <jonathanjlee@google.com>
2024-02-09 22:05:30 +0000

    [wptrunner] Don't swallow browser logs with `-vvv`
    `chromedriver` writes to stderr by default. The intention of
    `chromedriver --log-path=-` was to write to stdout for wptrunner to
    capture, but it actually writes a file named `-` in the CWD.
    
    As it turns out, wptrunner creates browser processes with stderr
    redirected to stdout [0], so there's no need for this switch in the
    first place.
    
    [0]: https://searchfox.org/mozilla-central/rev/41edcdf7fe44678c5913a603a286b1fc3979d540/testing/mozbase/mozprocess/mozprocess/processhandler.py#782
    
    Bug: None
    Test: run_wpt_tests.py -vvv external/wpt/badging
    Change-Id: Idc690c626a8881a024b8e7761b1aa67b144b7382
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5282721
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1258753}

--- third_party/blink/tools/blinkpy/wpt_tests/wpt_adapter.py
+++ third_party/blink/tools/blinkpy/wpt_tests/wpt_adapter.py
@@ -274,7 +274,6 @@ class WPTAdapter:
         if verbose_level >= 3:
             runner_options.webdriver_args.extend([
                 '--verbose',
-                '--log-path=-',
             ])
 
         if self.using_upstream_wpt:
