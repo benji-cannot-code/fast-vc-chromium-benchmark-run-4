6943a318df6417f1f9dad1cde85a814aa0e763d5
sisidovski <sisidovski@chromium.org>
2024-02-09 07:39:46 +0000

    [SWAutoPreload] Use MOJO_WRITE_DATA_FLAG_ALL_OR_NONE flag to write data
    This is a fix for the crash happening in the write operation.
    https://crash.corp.google.com/browse?q=expanded_custom_data.ChromeCrashProto.magic_signature_1.name+LIKE+%27%25ServiceWorkerRaceNetworkRequestURLLoaderClient%3A%3AReadAndWrite%25%27+AND+EXISTS+%28SELECT+1+FROM+UNNEST%28expanded_custom_data.ChromeCrashProto.experiments.ids%29+expanded_custom_data_ChromeCrashProto_experiments_ids+WHERE+expanded_custom_data_ChromeCrashProto_experiments_ids%3D%27e85106e5-4f4ac5b2%27%29#samplereports
    
    Before this CL, we expected the same amount of data is written equally
    into two data pipes. However, this assumption was wrong. Mojo
    `WriteData()` can write data partially if the available buffer size is
    not enough. If that happens, hits CHECK error.
    
    After this CL, `WriteData()` will be called with the
    |MOJO_WRITE_DATA_FLAG_ALL_OR_NONE| flag, which only write data if the
    whole data can be written to the buffer. Along with this flag, this CL
    limits input data not to exceed the write buffer limit. Otherwise it
    causes the write operation would never be finished.
    
    Bug: 40943349
    Change-Id: I1055e479fe5e5b901bf5d4d8f8c4009fc8539ca6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5276561
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258386}

--- content/common/service_worker/race_network_request_url_loader_client.cc
+++ content/common/service_worker/race_network_request_url_loader_client.cc
@@ -528,11 +528,14 @@ void ServiceWorkerRaceNetworkRequestURLLoaderClient::ReadAndWrite(
     return;
   }
 
+  size_t num_bytes_to_consume = read_buffer->size();
   if (write_buffer_manager_for_race_network_request_.IsWatching() &&
       write_buffer_manager_for_fetch_handler_.IsWatching()) {
     // If both data pipes are watched, write data to both pipes.
-    result = write_buffer_manager_for_race_network_request_.WriteData(
-        read_buffer.value());
+    std::pair<size_t, size_t> written_bytes;
+    std::tie(result, written_bytes.first) =
+        write_buffer_manager_for_race_network_request_.WriteData(
+            read_buffer.value());
     RecordMojoResultForWrite(result);
     switch (result) {
       case MOJO_RESULT_OK:
@@ -544,13 +547,14 @@ void ServiceWorkerRaceNetworkRequestURLLoaderClient::ReadAndWrite(
         Abort();
         return;
       case MOJO_RESULT_SHOULD_WAIT:
+      case MOJO_RESULT_OUT_OF_RANGE:
         // The data pipe is not writable yet. We don't consume data from |body_|
         // and write any data in this case. And retry it later.
         body_->EndReadData(0);
         write_buffer_manager_for_race_network_request_.ArmOrNotify();
         return;
     }
-    result =
+    std::tie(result, written_bytes.second) =
         write_buffer_manager_for_fetch_handler_.WriteData(read_buffer.value());
     RecordMojoResultForWrite(result);
     switch (result) {
@@ -562,6 +566,7 @@ void ServiceWorkerRaceNetworkRequestURLLoaderClient::ReadAndWrite(
         Abort();
         return;
       case MOJO_RESULT_SHOULD_WAIT:
+      case MOJO_RESULT_OUT_OF_RANGE:
         // When the data pipe returns MOJO_RESULT_SHOULD_WAIT, the data pipe is
         // not consumed yet but the buffer is full. Stop processing the data
         // pipe for the fetch handler side, not to make the data transfer
@@ -571,13 +576,16 @@ void ServiceWorkerRaceNetworkRequestURLLoaderClient::ReadAndWrite(
         write_buffer_manager_for_race_network_request_.ArmOrNotify();
         return;
     }
+    CHECK_EQ(written_bytes.first, written_bytes.second);
+    num_bytes_to_consume = written_bytes.first;
     CHECK_EQ(write_buffer_manager_for_race_network_request_.num_bytes_written(),
              write_buffer_manager_for_fetch_handler_.num_bytes_written());
   } else if (write_buffer_manager_for_race_network_request_.IsWatching()) {
     // If the data pipe for RaceNetworkRequest is the only watcher, don't write
     // data to the data pipe for the fetch handler.
-    result = write_buffer_manager_for_race_network_request_.WriteData(
-        read_buffer.value());
+    std::tie(result, num_bytes_to_consume) =
+        write_buffer_manager_for_race_network_request_.WriteData(
+            read_buffer.value());
     RecordMojoResultForWrite(result);
     switch (result) {
       case MOJO_RESULT_OK:
@@ -588,6 +596,7 @@ void ServiceWorkerRaceNetworkRequestURLLoaderClient::ReadAndWrite(
         Abort();
         return;
       case MOJO_RESULT_SHOULD_WAIT:
+      case MOJO_RESULT_OUT_OF_RANGE:
         body_->EndReadData(0);
         write_buffer_manager_for_race_network_request_.ArmOrNotify();
         return;
@@ -595,7 +604,7 @@ void ServiceWorkerRaceNetworkRequestURLLoaderClient::ReadAndWrite(
   } else if (write_buffer_manager_for_fetch_handler_.IsWatching()) {
     // If the data pipe for the fetch handler is the only watcher, don't write
     // data to the data pipe for RaceNetworkRequest.
-    result =
+    std::tie(result, num_bytes_to_consume) =
         write_buffer_manager_for_fetch_handler_.WriteData(read_buffer.value());
     RecordMojoResultForWrite(result);
     switch (result) {
@@ -607,12 +616,13 @@ void ServiceWorkerRaceNetworkRequestURLLoaderClient::ReadAndWrite(
         Abort();
         return;
       case MOJO_RESULT_SHOULD_WAIT:
+      case MOJO_RESULT_OUT_OF_RANGE:
         body_->EndReadData(0);
         write_buffer_manager_for_fetch_handler_.ArmOrNotify();
         return;
     }
   }
-  CompleteReadData(read_buffer.value().size());
+  CompleteReadData(num_bytes_to_consume);
 }
 
 std::optional<base::span<const char>>
