4617300819a163209608ef88ae4190280e8ac76c
L. David Baron <dbaron@chromium.org>
2024-02-09 00:52:12 +0000

    Implement evaluation of size keyword inside the CSS calc-size() function.
    Bug: 313072
    Change-Id: I65856fe024e34a7ff268926134e0b4ffc67b5be4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5214899
    Commit-Queue: David Baron <dbaron@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258285}

--- third_party/blink/renderer/platform/geometry/length.h
+++ third_party/blink/renderer/platform/geometry/length.h
@@ -436,6 +436,7 @@ class PLATFORM_EXPORT Length {
 
    public:
     const Length::AnchorEvaluator* anchor_evaluator = nullptr;
+    std::optional<float> size_keyword_basis = std::nullopt;
   };
 
   float NonNanCalculatedValue(float max_value, const EvaluationInput&) const;
