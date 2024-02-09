b632a7dabeac04141444453c5a33092b4d988f08
Dan Harrington <harringtond@chromium.org>
2024-02-09 18:33:21 +0000

    Use default kComposeTextOutputAnimation state for tests
    kComposeTextOutputAnimation is off by default, so make
    it off by default in tests.
    
    Change-Id: I00b614f8997d8fad1d7d1181f754160a079320f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5281828
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258632}

--- chrome/browser/compose/chrome_compose_client_unittest.cc
+++ chrome/browser/compose/chrome_compose_client_unittest.cc
@@ -154,8 +154,7 @@ class ChromeComposeClientTest : public BrowserWithTestWindowTest {
 
     scoped_feature_list_.InitWithFeatures(
         {compose::features::kEnableCompose,
-         optimization_guide::features::kOptimizationGuideModelExecution,
-         compose::features::kComposeTextOutputAnimation},
+         optimization_guide::features::kOptimizationGuideModelExecution},
         {});
     // Needed for feature params to reset.
     compose::ResetConfigForTesting();
@@ -660,9 +659,13 @@ TEST_F(ChromeComposeClientTest, TestComposeShowContextMenuAndDialog) {
               ukm::builders::Compose_PageEvents::kComposeTextInsertedName, 0)));
 }
 
-TEST_F(ChromeComposeClientTest, TestComposeWithIncompleteResponses) {
-  base::test::ScopedFeatureList scoped_feature_list(
-      optimization_guide::features::kOptimizationGuideOnDeviceModel);
+TEST_F(ChromeComposeClientTest, TestComposeWithIncompleteResponsesAnimated) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitWithFeatures(
+      {optimization_guide::features::kOptimizationGuideOnDeviceModel,
+       compose::features::kComposeTextOutputAnimation},
+      {});
+
   base::HistogramTester histogram_tester;
 
   const std::string input = "a user typed this";
@@ -731,8 +734,7 @@ TEST_F(ChromeComposeClientTest, TestComposeWithIncompleteResponses) {
 TEST_F(ChromeComposeClientTest, TestComposeNoResultAnimation) {
   base::test::ScopedFeatureList scoped_feature_list;
   scoped_feature_list.InitWithFeatures(
-      {optimization_guide::features::kOptimizationGuideOnDeviceModel},
-      {compose::features::kComposeTextOutputAnimation});
+      {optimization_guide::features::kOptimizationGuideOnDeviceModel}, {});
   base::HistogramTester histogram_tester;
 
   const std::string input = "a user typed this";
@@ -768,8 +770,11 @@ TEST_F(ChromeComposeClientTest, TestComposeNoResultAnimation) {
 }
 
 TEST_F(ChromeComposeClientTest, TestComposeSessionIgnoresPreviousResponse) {
-  base::test::ScopedFeatureList scoped_feature_list(
-      optimization_guide::features::kOptimizationGuideOnDeviceModel);
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitWithFeatures(
+      {optimization_guide::features::kOptimizationGuideOnDeviceModel,
+       compose::features::kComposeTextOutputAnimation},
+      {});
   base::HistogramTester histogram_tester;
 
   const std::string input = "a user typed this";
