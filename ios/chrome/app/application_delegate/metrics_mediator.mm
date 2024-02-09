0fa68305b730e1cd7f7a8150b39599dcb326ade5
Gayane Petrosyan <gayane@google.com>
2024-02-09 20:31:30 +0000

    [iOS] Refactor Credential Extension use signal.
    Refactor Credential Provider Extension use  into a centralized file for
    all default browser interest signals
    
    Bug: b/322358517
    Change-Id: I765a67d963fc0e03a7f3fb6841f348913871f335
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5278323
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1258705}

--- ios/chrome/app/application_delegate/metrics_mediator.mm
+++ ios/chrome/app/application_delegate/metrics_mediator.mm
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/app/application_delegate/startup_information.h"
 #import "ios/chrome/app/startup/ios_enable_sandbox_dump_buildflags.h"
 #import "ios/chrome/browser/crash_report/model/crash_helper.h"
+#import "ios/chrome/browser/default_browser/model/default_browser_interest_signals.h"
 #import "ios/chrome/browser/default_browser/model/utils.h"
 #import "ios/chrome/browser/metrics/model/first_user_action_recorder.h"
 #import "ios/chrome/browser/ntp/model/new_tab_page_util.h"
@@ -281,8 +282,7 @@ void RecordWidgetUsage(base::span<const HistogramNameCountPair> histograms) {
                                    kCredentialExtensionPasswordUseCount] ||
           [key isEqualToString:app_group::
                                    kCredentialExtensionQuickPasswordUseCount]) {
-        LogLikelyInterestedDefaultBrowserUserActivity(
-            DefaultPromoTypeMadeForIOS);
+        default_browser::NotifyCredentialExtensionUsed();
       }
     }
   }
--- ios/chrome/browser/default_browser/model/default_browser_interest_signals.mm
+++ ios/chrome/browser/default_browser/model/default_browser_interest_signals.mm
@@ -20,4 +20,8 @@ void NotifyStartWithURL() {
   LogLikelyInterestedDefaultBrowserUserActivity(DefaultPromoTypeGeneral);
 }
 
+void NotifyCredentialExtensionUsed() {
+  // TODO(b/322358517): Migrate to FET.
+  LogLikelyInterestedDefaultBrowserUserActivity(DefaultPromoTypeMadeForIOS);
+}
 }  // namespace default_browser
