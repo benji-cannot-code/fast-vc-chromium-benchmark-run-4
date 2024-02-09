cce8d161f53cb8680f28fa6c23e2c0e02f3a1010
mlcui <mlcui@google.com>
2024-02-09 00:54:29 +0000

    language_pack_font_service: Implement installing fonts on pref change
    Note that fonts will not be installed on initialisation yet - that will
    be implemented in the next CL which checks on initialisation.
    
    Bug: b:320549210
    Change-Id: Ib9d88f2438c43c256c02f4cf7126f8d8f44faedf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5201484
    Reviewed-by: jhtin <jhtin@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1258288}

--- chrome/browser/ash/language_packs/language_pack_font_service.cc
+++ chrome/browser/ash/language_packs/language_pack_font_service.cc
@@ -5,11 +5,49 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/language_packs/language_pack_font_service.h"
 
-class PrefService;
+#include <string>
+#include <string_view>
+
+#include "base/check_deref.h"
+#include "base/containers/flat_set.h"
+#include "base/functional/bind.h"
+#include "base/functional/callback_helpers.h"
+#include "base/memory/weak_ptr.h"
+#include "base/strings/string_split.h"
+#include "chromeos/ash/components/language_packs/language_pack_manager.h"
+#include "components/language/core/browser/pref_names.h"
+#include "components/language/core/common/locale_util.h"
 
 namespace ash::language_packs {
 
 LanguagePackFontService::LanguagePackFontService(PrefService* prefs)
-    : prefs_(*prefs) {}
+    : prefs_(CHECK_DEREF(prefs)) {
+  pref_accept_language_.Init(
+      language::prefs::kPreferredLanguages, &*prefs_,
+      base::BindRepeating(&LanguagePackFontService::InstallFontDlcs,
+                          weak_factory_.GetWeakPtr()));
+}
+
+LanguagePackFontService::~LanguagePackFontService() = default;
+
+base::flat_set<std::string>
+LanguagePackFontService::GetLanguagePacksForAcceptLanguage() {
+  for (std::string& locale :
+       base::SplitString(pref_accept_language_.GetValue(), ",",
+                         base::TRIM_WHITESPACE, base::SPLIT_WANT_NONEMPTY)) {
+    if (language::ExtractBaseLanguage(locale) == "ja") {
+      return {"ja"};
+    }
+  }
+
+  return {};
+}
+
+void LanguagePackFontService::InstallFontDlcs() {
+  for (std::string& language_pack : GetLanguagePacksForAcceptLanguage()) {
+    LanguagePackManager::InstallPack(kFontsFeatureId, language_pack,
+                                     base::DoNothing());
+  }
+}
 
 }  // namespace ash::language_packs
