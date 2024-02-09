2d905aae57ca472557293ad921155f94296607cb
Tommy C. Li <tommycli@chromium.org>
2024-02-09 19:55:35 +0000

    Enable cross-batch deduplication of searched-for ungrouped visits
    Also enables the feature by default.
    
    Bug: b/41492963, b/32077465
    Change-Id: Ib19a0305e19121bd22d157535806b4766efb9e0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5280936
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258685}

--- components/history_clusters/core/query_clusters_state.h
+++ components/history_clusters/core/query_clusters_state.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 #include <string>
+#include <unordered_set>
 #include <vector>
 
 #include "base/functional/callback_forward.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/elapsed_timer.h"
 #include "components/history/core/browser/history_types.h"
 #include "components/history_clusters/core/history_clusters_types.h"
+#include "components/history_clusters/core/similar_visit.h"
 
 namespace history {
 class HistoryService;
@@ -79,6 +81,8 @@ class QueryClustersState {
  private:
   friend class QueryClustersStateTest;
   FRIEND_TEST_ALL_PREFIXES(QueryClustersStateTest, GetUngroupedVisits);
+  FRIEND_TEST_ALL_PREFIXES(QueryClustersStateTest,
+                           GetUngroupedVisitsDoesCrossBatchDeduplication);
 
   // Private class containing state that's only accessed on
   // `post_processing_task_runner`.
@@ -144,6 +148,11 @@ class QueryClustersState {
   // The number of clusters that have already been sent to the page. This is
   // updated AFTER the callback for each batch.
   size_t number_clusters_sent_to_page_ = 0;
+
+  // Tracks the visits that we've seen so far. This is only used for when we
+  // are also aggregating ungrouped visits, i.e. when `query_` is non-empty.
+  std::unordered_set<SimilarVisit, SimilarVisit::Hash, SimilarVisit::Equals>
+      seen_visits_for_deduping_ungrouped_visits_;
 
   // Used only to fast-cancel tasks in case we are destroyed.
   std::unique_ptr<HistoryClustersServiceTask> query_clusters_task_;
