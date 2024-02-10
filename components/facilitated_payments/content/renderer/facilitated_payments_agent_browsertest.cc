9d1bf42a7c599c5dd16c9a4dd991d2489f583cd7
siashah <siashah@chromium.org>
2024-02-10 13:43:50 +0000

    Update Pix validation logic to check for dynamic Pix codes
    Bug: 40279646
    Change-Id: I8b8f6b12352423679a5304334838d368296d758f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5276754
    Reviewed-by: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258888}

--- components/facilitated_payments/content/renderer/facilitated_payments_agent_browsertest.cc
+++ components/facilitated_payments/content/renderer/facilitated_payments_agent_browsertest.cc
@@ -51,7 +51,7 @@ TEST_F(FacilitatedPaymentsAgentTest, TriggerPixCodeDetection_Found) {
   EXPECT_TRUE(IsPixCodeFound(CreateAgentFor(R"(
    <body>
     <div>
-      00020126180014br.gov.bcb.pix63041D3D
+      00020126370014br.gov.bcb.pix2515www.example.com6304EA3F
     </div>
   </form>
   )").get()));
@@ -62,7 +62,7 @@ TEST_F(FacilitatedPaymentsAgentTest,
   std::unique_ptr<FacilitatedPaymentsAgent> agent = CreateAgentFor(R"(
    <body>
     <div>
-      00020126180014br.gov.bcb.pix63041D3D
+      00020126370014br.gov.bcb.pix2515www.example.com6304EA3F
     </div>
   </form>
   )");
