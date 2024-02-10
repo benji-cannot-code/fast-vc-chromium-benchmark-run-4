e06f160f1963ef9f413e83681bf02377cec2fbb8
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2024-02-10 23:05:29 +0000

    [CrOS Bluetooth] Add fake HidPreservingBluetoothStateController
    Adds fake classes for HidPreservingBluetoothStateController which would
    be used in a future CL when writing test.
    
    Fixed: b/324301195
    Test: Ran CQ
    Change-Id: Ic0f50d1abf004436f9fd8199b69516d4f6a7a1ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5274277
    Commit-Queue: Theo Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258916}

---
+++ ash/system/bluetooth/hid_preserving_controller/hid_preserving_bluetooth_state_controller_test_helper.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/bluetooth/hid_preserving_controller/hid_preserving_bluetooth_state_controller_test_helper.h"
+
+#include "ash/constants/ash_features.h"
+#include "ash/system/bluetooth/hid_preserving_controller/in_process_instance.h"
+#include "base/check.h"
+
+namespace ash {
+
+HidPreservingBluetoothStateControllerTestHelper::
+    HidPreservingBluetoothStateControllerTestHelper() {
+  CHECK(features::IsBluetoothDisconnectWarningEnabled());
+  bluetooth::OverrideInProcessInstanceForTesting(&hid_preserving_bluetooth_);
+}
+
+HidPreservingBluetoothStateControllerTestHelper::
+    ~HidPreservingBluetoothStateControllerTestHelper() {
+  bluetooth::OverrideInProcessInstanceForTesting(nullptr);
+}
+
+}  // namespace ash
