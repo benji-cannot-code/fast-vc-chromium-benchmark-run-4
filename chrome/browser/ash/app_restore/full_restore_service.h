070d4e3111675d6cedc892b8258973e0beae0184
Sammie Quon <sammiequon@chromium.org>
2024-02-10 01:37:29 +0000

    pine: Pipe RestoreData from full restore service to ash
    The RestoreData contains the data needed to show app titles, icons and
    tab favicons. Create a new class that captures this and an image for
    the screenshot. Last session exit type and callbacks will be added in
    future CLs.
    
    This is the first CL to move closer towards using real data instead of
    faked data. There are a bunch of TODOs that will be resolved and/or
    replaced with bugs in future CLs.
    
    Bug: b/323556309
    Test: manual
    Test: existing tests
    Change-Id: Ib3f9088dc13b7c4404ab8e783386f8c652b0c8c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5273623
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258837}

--- chrome/browser/ash/app_restore/full_restore_service.h
+++ chrome/browser/ash/app_restore/full_restore_service.h
@@ -22,6 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class Profile;
 
+namespace app_restore {
+class RestoreData;
+}  // namespace app_restore
+
 namespace message_center {
 class Notification;
 }  // namespace message_center
@@ -74,7 +78,8 @@ class FullRestoreService : public KeyedService,
    public:
     virtual ~Delegate() = default;
     // Starts overview with the pine dialog unless overview is already active.
-    virtual void MaybeStartPineOverviewSession() = 0;
+    virtual void MaybeStartPineOverviewSession(
+        std::unique_ptr<::app_restore::RestoreData> restore_data) = 0;
   };
 
   static FullRestoreService* GetForProfile(Profile* profile);
