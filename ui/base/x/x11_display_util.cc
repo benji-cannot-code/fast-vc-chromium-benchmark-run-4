18d2e410aa9bbf5cbd0e6f108fd2fb6a2045b71f
punithnayak <npunith125@gmail.com>
2024-02-11 10:47:35 +0000

    Use `std::midpoint` in UI
    Bug: 1414539
    Change-Id: Ic654f011bb5d6032ffe7a47492676d3e5e6567b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5139278
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Punith Nayak <npunith125@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1258942}

--- ui/base/x/x11_display_util.cc
+++ ui/base/x/x11_display_util.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <bit>
 #include <bitset>
+#include <numeric>
 #include <queue>
 #include <unordered_set>
 
@@ -241,7 +242,7 @@ gfx::PointF DisplayOriginPxToDip(const display::Display& parent,
                             int child_r_px, float parent_l_dip) {
     const base::ClampedNumeric<int> l = std::max(parent_l_px, child_l_px);
     const base::ClampedNumeric<int> r = std::min(parent_r_px, child_r_px);
-    const float mid_px = (l + r) / 2.0f;
+    const float mid_px = std::midpoint<float>(float(l), float(r));
     const float mid_dip = (mid_px - parent_l_px) / parent_scale + parent_l_dip;
     return (child_l_px - mid_px) / child_scale + mid_dip;
   };
--- ui/gfx/color_conversions.cc
+++ ui/gfx/color_conversions.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/color_conversions.h"
 
 #include <cmath>
+#include <numeric>
 #include <tuple>
 
 #include "skia/ext/skcolorspace_primaries.h"
@@ -594,15 +595,14 @@ std::tuple<float, float, float> HSLToSRGB(float h, float s, float l) {
 
 std::tuple<float, float, float> SRGBToHSL(float r, float g, float b) {
   // See https://www.w3.org/TR/css-color-4/#rgb-to-hsl
-  float max = std::max({r, g, b});
-  float min = std::min({r, g, b});
-  float hue = 0.0f, saturation = 0.0f, ligth = (max + min) / 2.0f;
+  auto [min, max] = std::minmax({r, g, b});
+  float hue = 0.0f, saturation = 0.0f, lightness = std::midpoint(min, max);
   float d = max - min;
 
   if (d != 0.0f) {
-    saturation = (ligth == 0.0f || ligth == 1.0f)
+    saturation = (lightness == 0.0f || lightness == 1.0f)
                      ? 0.0f
-                     : (max - ligth) / std::min(ligth, 1 - ligth);
+                     : (max - lightness) / std::min(lightness, 1 - lightness);
     if (max == r) {
       hue = (g - b) / d + (g < b ? 6.0f : 0.0f);
     } else if (max == g) {
@@ -613,7 +613,7 @@ std::tuple<float, float, float> SRGBToHSL(float r, float g, float b) {
     hue *= 60.0f;
   }
 
-  return std::make_tuple(hue, saturation, ligth);
+  return std::make_tuple(hue, saturation, lightness);
 }
 
 std::tuple<float, float, float> HWBToSRGB(float h, float w, float b) {
