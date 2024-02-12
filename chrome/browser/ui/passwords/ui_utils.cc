602fbbde6c4c8e5e10292829d4888c6e09b84a65
Karol Sygiet <sygiet@google.com>
2024-02-12 10:27:12 +0000

    Add triggering for the account storage toggle iph
    This CL uses `ShowPromoInPage` to prompt the IPH when clicking the
    "Settings" button in DefaultStoreChanged bubble.
    
    Implementation:
    https://screenshot.googleplex.com/74vyP4cLdkUUKm7.png
    Mock:
    https://screenshot.googleplex.com/5y8eCkkRByM2G6d.png
    
    Bug: 1503146
    Change-Id: Id5acc4b3c936fdec1d30f8ed4bedc40feef96985
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5255540
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1259083}

--- chrome/browser/ui/passwords/ui_utils.cc
+++ chrome/browser/ui/passwords/ui_utils.cc
@@ -50,6 +50,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if !BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/user_education/show_promo_in_page.h"
+#include "chrome/browser/ui/webui/password_manager/password_manager_ui.h"
 #endif
 
 namespace {
@@ -257,12 +259,14 @@ void NavigateToManagePasswordsPage(Browser* browser,
 }
 
 void NavigateToManagePasswordsSettingsAccountStoreToggle(Browser* browser) {
-  GURL url(chrome::kChromeUIPasswordManagerSettingsURL);
-  url = net::AppendQueryParameter(url, "accountStoreIPH", "true");
+  ShowPromoInPage::Params params;
+  params.target_url = GURL(chrome::kChromeUIPasswordManagerSettingsURL);
+  params.bubble_anchor_id = PasswordManagerUI::kAccountStoreToggleElementId;
+  params.bubble_arrow = user_education::HelpBubbleArrow::kTopRight;
+  params.bubble_text = l10n_util::GetStringUTF16(
+      IDS_PASSWORD_MANAGER_IPH_ACCOUNT_STORAGE_TOGGLE);
 
-  NavigateParams params(browser, url, ui::PAGE_TRANSITION_LINK);
-  params.disposition = WindowOpenDisposition::NEW_FOREGROUND_TAB;
-  Navigate(&params);
+  ShowPromoInPage::Start(browser, std::move(params));
 }
 
 void NavigateToPasswordCheckupPage(Profile* profile) {
