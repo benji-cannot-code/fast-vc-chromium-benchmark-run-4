8bc470ce535c8bc85f2a462b929f95007a19841a
Sam Maier <smaier@chromium.org>
2024-02-12 20:12:51 +0000

    jni_zero: making the sample app actually runnable
    Change-Id: I31c764b16f388fd2176de0ee3cdb38b359b55b67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5282861
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259397}

---
+++ third_party/jni_zero/samples/java/src/org/jni_zero/samples/SampleActivity.java
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+package org.jni_zero.samples;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SampleActivity extends Activity {
+    static {
+        try {
+            System.loadLibrary("sample_lib");
+        } catch (UnsatisfiedLinkError e) {
+            // This is a hacky workaround for the Chromium build system - in some configurations,
+            // we use "component build", and instead of properly supporting this, we can just
+            // try/catch for this sample app.
+            System.loadLibrary("sample_lib.cr");
+        }
+    }
+
+    public void onCreate(Bundle b) {
+        super.onCreate(b);
+        SampleForAnnotationProcessor.test();
+    }
+}
