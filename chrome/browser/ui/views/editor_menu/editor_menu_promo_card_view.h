a8ee7de23fee03937607f2eccae475d40f2d1219
Grey Wang <greywang@google.com>
2024-02-12 23:58:33 +0000

    Reland "Improve window positioning strategy of GetEditorMenuBounds()."
    This is a reland of commit 4bc8d769116eb35d7ad558cfc33ea1d0adebc4ab
    
    Original change's description:
    > Improve window positioning strategy of GetEditorMenuBounds().
    >
    > Bug: b/323962058
    > Change-Id: I22e02aa1ea34d9d3fd1329722198fe1d7a70e49e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5269783
    > Reviewed-by: Tao Wu <wutao@chromium.org>
    > Commit-Queue: Grey Wang <greywang@google.com>
    > Reviewed-by: Andre Le <leandre@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1258229}
    
    Bug: b/323962058
    Change-Id: I9dc9bb992cd203d87f1e09210bb9febaef2c5c0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5276768
    Reviewed-by: Andre Le <leandre@chromium.org>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: Grey Wang <greywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1259550}

--- chrome/browser/ui/views/editor_menu/editor_menu_promo_card_view.h
+++ chrome/browser/ui/views/editor_menu/editor_menu_promo_card_view.h
@@ -46,6 +46,7 @@ class EditorMenuPromoCardView : public views::View,
   void AddedToWidget() override;
   void RequestFocus() override;
   void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
+  int GetHeightForWidth(int width) const override;
 
   // views::WidgetObserver:
   void OnWidgetDestroying(views::Widget* widget) override;
