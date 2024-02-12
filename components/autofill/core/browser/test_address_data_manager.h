559a8898bebec396203b2ffe278d226dac9d7b28
Florian Leimgruber <fleimgruber@google.com>
2024-02-12 13:22:34 +0000

    Split TestADM from TestPDM
    For all the functions that already moved to the ADM, moves the
    corresponding TestPDM overrides to a new TestADM. The TestPDM can just
    initialize its address_data_manager_ with a TestADM.
    
    This removes the need for the friend TestPDM declaration in ADM and
    makes some functions in the PDM non-virtual.
    
    Some of the code in ADM becomes protected (instead of private), so it
    can be accessed in the TestADM.
    
    Bug: b/322170538
    Change-Id: Ic8dd7de64a491f0577436af3fdc2e75c9d6ec0a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5272409
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1259120}

---
+++ components/autofill/core/browser/test_address_data_manager.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_TEST_ADDRESS_DATA_MANAGER_H_
+#define COMPONENTS_AUTOFILL_CORE_BROWSER_TEST_ADDRESS_DATA_MANAGER_H_
+
+#include "components/autofill/core/browser/address_data_manager.h"
+
+namespace autofill {
+
+// An AddressDataManager that doesn't communicate with a database and is thus
+// fully synchronous.
+class TestAddressDataManager : public AddressDataManager {
+ public:
+  explicit TestAddressDataManager(base::RepeatingClosure notify_pdm_observers);
+  ~TestAddressDataManager() override;
+
+  // AddressDataManager overrides:
+  void AddProfile(const AutofillProfile& profile) override;
+  void UpdateProfile(const AutofillProfile& profile) override;
+  void RemoveProfile(const std::string& guid) override;
+  void LoadProfiles() override;
+
+  void ClearProfiles();
+};
+
+}  // namespace autofill
+
+#endif  // COMPONENTS_AUTOFILL_CORE_BROWSER_TEST_ADDRESS_DATA_MANAGER_H_
