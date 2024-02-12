cc0b9a22590e1e771dd3e3f5be029d17120438bd
Dave Tapuska <dtapuska@chromium.org>
2024-02-12 21:54:49 +0000

    Deal with non-compositing LocalFrames being paused
    https://crrev.com/d0ec5763488dfa1783c2822a2320211bec84daaa added
    a unconditional access of the LayerTreeHost. Most accesses are behind
    does_composite() calls but GetLayerTreeDebugState was not.
    
    Bug: 324597312
    Change-Id: Id5af969931effdb977d5a5da2323492d1f4ca900
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5287735
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259464}

--- third_party/blink/renderer/core/inspector/inspector_overlay_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_overlay_agent.cc
@@ -423,7 +423,7 @@ InspectorOverlayAgent::InspectorOverlayAgent(
   if (GetFrame()->GetWidgetForLocalRoot()) {
     original_layer_tree_debug_state_ =
         std::make_unique<cc::LayerTreeDebugState>(
-            GetFrame()->GetWidgetForLocalRoot()->GetLayerTreeDebugState());
+            *GetFrame()->GetWidgetForLocalRoot()->GetLayerTreeDebugState());
   }
 }
 
@@ -554,7 +554,7 @@ protocol::Response InspectorOverlayAgent::setShowDebugBorders(bool show) {
   }
   if (FrameWidgetInitialized()) {
     FrameWidget* widget = GetFrame()->GetWidgetForLocalRoot();
-    cc::LayerTreeDebugState debug_state = widget->GetLayerTreeDebugState();
+    cc::LayerTreeDebugState debug_state = *widget->GetLayerTreeDebugState();
     if (show) {
       debug_state.show_debug_borders.set();
     } else {
@@ -575,7 +575,7 @@ protocol::Response InspectorOverlayAgent::setShowFPSCounter(bool show) {
   }
   if (FrameWidgetInitialized()) {
     FrameWidget* widget = GetFrame()->GetWidgetForLocalRoot();
-    cc::LayerTreeDebugState debug_state = widget->GetLayerTreeDebugState();
+    cc::LayerTreeDebugState debug_state = *widget->GetLayerTreeDebugState();
     debug_state.show_fps_counter = show;
     widget->SetLayerTreeDebugState(debug_state);
   }
@@ -592,7 +592,7 @@ protocol::Response InspectorOverlayAgent::setShowPaintRects(bool show) {
   }
   if (FrameWidgetInitialized()) {
     FrameWidget* widget = GetFrame()->GetWidgetForLocalRoot();
-    cc::LayerTreeDebugState debug_state = widget->GetLayerTreeDebugState();
+    cc::LayerTreeDebugState debug_state = *widget->GetLayerTreeDebugState();
     debug_state.show_paint_rects = show;
     widget->SetLayerTreeDebugState(debug_state);
   }
@@ -609,7 +609,7 @@ protocol::Response InspectorOverlayAgent::setShowLayoutShiftRegions(bool show) {
   }
   if (FrameWidgetInitialized()) {
     FrameWidget* widget = GetFrame()->GetWidgetForLocalRoot();
-    cc::LayerTreeDebugState debug_state = widget->GetLayerTreeDebugState();
+    cc::LayerTreeDebugState debug_state = *widget->GetLayerTreeDebugState();
     debug_state.show_layout_shift_regions = show;
     widget->SetLayerTreeDebugState(debug_state);
   }
@@ -627,7 +627,7 @@ protocol::Response InspectorOverlayAgent::setShowScrollBottleneckRects(
   }
   if (FrameWidgetInitialized()) {
     FrameWidget* widget = GetFrame()->GetWidgetForLocalRoot();
-    cc::LayerTreeDebugState debug_state = widget->GetLayerTreeDebugState();
+    cc::LayerTreeDebugState debug_state = *widget->GetLayerTreeDebugState();
     debug_state.show_touch_event_handler_rects = show;
     debug_state.show_wheel_event_handler_rects = show;
     debug_state.show_non_fast_scrollable_rects = show;
@@ -658,7 +658,7 @@ protocol::Response InspectorOverlayAgent::setShowWebVitals(bool show) {
   }
   if (FrameWidgetInitialized()) {
     FrameWidget* widget = GetFrame()->GetWidgetForLocalRoot();
-    cc::LayerTreeDebugState debug_state = widget->GetLayerTreeDebugState();
+    cc::LayerTreeDebugState debug_state = *widget->GetLayerTreeDebugState();
     debug_state.show_web_vital_metrics = show;
     widget->SetLayerTreeDebugState(debug_state);
   }
@@ -1293,7 +1293,7 @@ void InspectorOverlayAgent::DidInitializeFrameWidget() {
   }
 
   original_layer_tree_debug_state_ = std::make_unique<cc::LayerTreeDebugState>(
-      GetFrame()->GetWidgetForLocalRoot()->GetLayerTreeDebugState());
+      *GetFrame()->GetWidgetForLocalRoot()->GetLayerTreeDebugState());
   Restore();
 }
 
