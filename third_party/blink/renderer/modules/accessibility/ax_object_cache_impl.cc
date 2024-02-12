c1a3c1a3db92b4b31fa517ac83d105373636f65b
Aaron Leventhal <aleventhal@chromium.org>
2024-02-12 17:00:46 +0000

    [A11y] Mark dirty methods should be tolerant of null arg
    Other mark dirty methods in AXObjectCacheImpl already returned early
    if passed a null argument. Make this consistent.
    
    Bug: b/324774789
    Change-Id: I8a29e66d8c25dcb189550eb2c53750440638196d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5286254
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259229}

--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
@@ -4777,6 +4777,9 @@ void AXObjectCacheImpl::MarkAXObjectDirtyWithCleanLayoutHelper(
 }
 
 void AXObjectCacheImpl::MarkAXObjectDirtyWithCleanLayout(AXObject* obj) {
+  if (!obj) {
+    return;
+  }
   MarkAXObjectDirtyWithCleanLayoutHelper(obj, active_event_from_,
                                          active_event_from_action_);
   for (auto agent : agents_) {
@@ -4807,6 +4810,9 @@ void AXObjectCacheImpl::NotifySubtreeDirty(AXObject* obj) {
 }
 
 void AXObjectCacheImpl::MarkAXSubtreeDirtyWithCleanLayout(AXObject* obj) {
+  if (!obj) {
+    return;
+  }
   MarkAXObjectDirtyWithCleanLayoutHelper(obj, active_event_from_,
                                          active_event_from_action_);
   NotifySubtreeDirty(obj);
@@ -4822,7 +4828,7 @@ void AXObjectCacheImpl::MarkAXSubtreeDirty(AXObject* obj) {
 void AXObjectCacheImpl::MarkSubtreeDirty(Node* node) {
   if (AXObject* obj = Get(node)) {
     MarkAXSubtreeDirty(obj);
-  } else {
+  } else if (node) {
     // There is no AXObject, so there is no subtree to mark dirty.
     MarkElementDirty(node);
   }
