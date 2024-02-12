6f59e18250a650cdd192c6886f441b396596490a
Maks Orlovich <morlovich@chromium.org>
2024-02-12 15:59:37 +0000

    Reland "FLEDGE: Turn BidRole into a mojo enum."
    This reverts commit 4b3a6e988232283adad497db748909542304845f.
    
    Reason for revert: A different CL (by me) was causing the test failure, so re-landing this.
    
    Original change's description:
    > Revert "FLEDGE: Turn BidRole into a mojo enum."
    >
    > This reverts commit 20de27b3c975e93a02a2dfe922d723c968a953ff.
    >
    > Reason for revert: Caused multiple tests fail
    >
    > Original change's description:
    > > FLEDGE: Turn BidRole into a mojo enum.
    > >
    > > ...It will need to be returned from the worklet over mojo once it
    > > supports multi-bid (as annotation of each bid in an array, replacing the
    > > awkward BidderWorkletKAnonEnforcedBid stuff). This CL merely moves it
    > > over to a mojo type in prep without any functional changes, as an easy
    > > way to make the eventual functional CL a bit easier to review.
    > >
    > > Bug: 323856489
    > > Change-Id: I94060a85a005afe60c9f14605da3de0fcea1be44
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5280541
    > > Reviewed-by: mmenke <mmenke@chromium.org>
    > > Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    > > Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1258762}
    >
    > Bug: 323856489
    > Change-Id: I0d8d534e347ad24187891e674bc25070634257dc
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5283854
    > Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    > Reviewed-by: mmenke <mmenke@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: mmenke <mmenke@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1258925}
    
    Bug: 323856489
    Change-Id: I629cebdb1b00aaa36636310045ca77690b8fb63d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5285734
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259195}

--- content/browser/interest_group/interest_group_auction.h
+++ content/browser/interest_group/interest_group_auction.h
@@ -356,11 +356,7 @@ class CONTENT_EXPORT InterestGroupAuction
   // duplicates auction_worklet::mojom::BidderWorkletBid, with additional
   // information about the bidder.
   struct CONTENT_EXPORT Bid {
-    // Which auctions the bid is appropriate for, based on whether the auction
-    // enforces k-anonymity or not.
-    enum class BidRole { kUnenforcedKAnon, kEnforcedKAnon, kBothKAnonModes };
-
-    Bid(BidRole bid_role,
+    Bid(auction_worklet::mojom::BidRole bid_role,
         std::string ad_metadata,
         double bid,
         std::optional<blink::AdCurrency> bid_currency,
@@ -380,7 +376,7 @@ class CONTENT_EXPORT InterestGroupAuction
 
     // This considers the bid_role to pick proper trace id.
     uint64_t TraceId() {
-      return (bid_role == BidRole::kEnforcedKAnon)
+      return (bid_role == auction_worklet::mojom::BidRole::kEnforcedKAnon)
                  ? *bid_state->trace_id_for_kanon_scoring
                  : *bid_state->trace_id;
     }
@@ -391,7 +387,7 @@ class CONTENT_EXPORT InterestGroupAuction
     std::vector<GURL> GetAdComponentUrls() const;
 
     // Which auctions the bid participates in.
-    BidRole bid_role;
+    auction_worklet::mojom::BidRole bid_role;
 
     // These are taken directly from the
     // auction_worklet::mojom::BidderWorkletBid.
@@ -973,7 +969,7 @@ class CONTENT_EXPORT InterestGroupAuction
 
   static std::unique_ptr<Bid> CreateBidFromComponentAuctionWinner(
       const ScoredBid* scored_bid,
-      Bid::BidRole bid_role);
+      auction_worklet::mojom::BidRole bid_role);
 
   // Called when a potential source of bids or other scoring dependency has
   // finished. This could be a component auction completing (with or without
