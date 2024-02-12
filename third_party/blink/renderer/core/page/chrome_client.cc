ef8662056912608da8ba60d30f1d5e320561d9d9
Aoyuan Zuo <zuoaoyuan@chromium.org>
2024-02-12 19:39:31 +0000

    Event Timing - Handle modal dialog interference
    * Previous undesired outcome: event timing could measure the arbitrarily
      long user input waiting time on synchronous javascript modal dialogs
      and report it as part of the presentation delay.
    * Root cause: synchronous modal dialogs paused the event-damaged frame
      to be presented until after user close the dialog.
    * This CL address it by: treating modal dialogs showing as another way
      of page responding and report the dialog showing timestamp as an
      alternative end time for duration measurement to any events that was
      interfered by the modal dialog in event timing.
    
    Bug: 1435448
    Change-Id: I8f48c0292e45b01ea51ac5134096a08f584fdcfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5258894
    Commit-Queue: Aoyuan Zuo <zuoaoyuan@chromium.org>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259367}

--- third_party/blink/renderer/core/page/chrome_client.cc
+++ third_party/blink/renderer/core/page/chrome_client.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/core/page/scoped_page_pauser.h"
 #include "third_party/blink/renderer/core/probe/core_probes.h"
+#include "third_party/blink/renderer/core/timing/dom_window_performance.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "ui/display/screen_info.h"
@@ -95,6 +96,7 @@ template <typename Delegate>
 static bool OpenJavaScriptDialog(LocalFrame* frame,
                                  const String& message,
                                  const Delegate& delegate) {
+  DOMWindowPerformance::performance(*frame->DomWindow())->WillShowModalDialog();
   // Suspend pages in case the client method runs a new event loop that would
   // otherwise cause the load to continue while we're in the middle of
   // executing JavaScript.
@@ -269,6 +271,8 @@ bool ChromeClient::Print(LocalFrame* frame) {
         "Ignored call to 'print()' during prerendering."));
     return false;
   }
+
+  DOMWindowPerformance::performance(*frame->DomWindow())->WillShowModalDialog();
 
   // Suspend pages in case the client method runs a new event loop that would
   // otherwise cause the load to continue while we're in the middle of
