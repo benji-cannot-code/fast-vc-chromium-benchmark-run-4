d5b630e176cf51aeb49f64d3bba4fb0e8b72f830
Joseph Pang <pjlee@google.com>
2024-02-12 18:24:45 +0000

    arc/gio: Change TableLayout uses to BoxLayout
    This CL changes instances of TableLayout to use BoxLayout instead, with
    variable flex.
    
    Bug: b/279117180
    Test: Unit tests passed
    Test: Manually check layout
    Change-Id: I9380168c39da10525765bb4ccc4542546c574b27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5241860
    Reviewed-by: Cici Ruan <cuicuiruan@google.com>
    Commit-Queue: Joseph Pang <pjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1259289}

--- chrome/browser/ash/arc/input_overlay/ui/button_options_menu.cc
+++ chrome/browser/ash/arc/input_overlay/ui/button_options_menu.cc
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/flex_layout.h"
 #include "ui/views/layout/layout_types.h"
-#include "ui/views/layout/table_layout.h"
 #include "ui/views/style/typography.h"
 
 namespace arc::input_overlay {
@@ -198,34 +197,24 @@ void ButtonOptionsMenu::AddHeader() {
   // ||"Button options"|          |icon||
   // ------------------------------------
   auto* container = AddChildView(std::make_unique<views::View>());
-  container->SetLayoutManager(std::make_unique<views::TableLayout>())
-      ->AddColumn(views::LayoutAlignment::kStart,
-                  views::LayoutAlignment::kCenter,
-                  /*horizontal_resize=*/1.0f,
-                  views::TableLayout::ColumnSize::kUsePreferred,
-                  /*fixed_width=*/0, /*min_width=*/0)
-      .AddColumn(views::LayoutAlignment::kEnd, views::LayoutAlignment::kCenter,
-                 /*horizontal_resize=*/1.0f,
-                 views::TableLayout::ColumnSize::kUsePreferred,
-                 /*fixed_width=*/0, /*min_width=*/0)
-      .AddRows(1, views::TableLayout::kFixedSize, 0);
+  auto* layout = container->SetLayoutManager(std::make_unique<views::BoxLayout>(
+      views::BoxLayout::Orientation::kHorizontal));
   container->SetProperty(views::kMarginsKey,
                          gfx::Insets::TLBR(0, kHeaderLeftMarginSpacing, 12, 0));
 
   action_name_label_ = container->AddChildView(ash::bubble_utils::CreateLabel(
       // TODO(b/274690042): Replace placeholder text with localized strings.
       ash::TypographyToken::kCrosTitle1, u"", cros_tokens::kCrosSysOnSurface));
+  action_name_label_->SetHorizontalAlignment(gfx::ALIGN_LEFT);
+  action_name_label_->SetMultiLine(true);
+  // Flex `action_name_label_` to fill empty space.
+  layout->SetFlexForView(action_name_label_, /*flex=*/1);
 
   trash_button_ = container->AddChildView(std::make_unique<ash::IconButton>(
       base::BindRepeating(&ButtonOptionsMenu::OnTrashButtonPressed,
                           base::Unretained(this)),
       ash::IconButton::Type::kMedium, &kGameControlsDeleteIcon,
       IDS_INPUT_OVERLAY_BUTTON_OPTIONS_DELETE_TOOLTIP_TEXT));
-
-  action_name_label_->SetMultiLine(true);
-  action_name_label_->SetMaximumWidth(
-      kButtonOptionsMenuWidth - 2 * kArrowContainerHorizontalBorderInset -
-      kHeaderLeftMarginSpacing - trash_button_->GetPreferredSize().width());
 }
 
 void ButtonOptionsMenu::AddEditTitle() {
