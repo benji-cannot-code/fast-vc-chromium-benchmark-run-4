9f55f25443ba2e6d999d3e6fc78dfcba0315deab
Noam Rosenthal <nrosenthal@chromium.org>
2024-02-12 18:37:22 +0000

    Document render-blocking: use classic scripts to generate parser delay
    This would be more interoperable as janking the main thread would not
    necessarily force rendering.
    
    Bug: 324452579
    Change-Id: I2e82535a45ca12063c680278781395d6068b4e01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5272558
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259300}

--- third_party/blink/web_tests/external/wpt/html/dom/render-blocking/support/utils.js
+++ third_party/blink/web_tests/external/wpt/html/dom/render-blocking/support/utils.js
@@ -1,11 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-function jank(ms) {
-  let start = performance.now();
-  while (performance.now() < start + ms);
-}
-
-function jankMany(ms, times) {
-  for (let i = 0; i < times; i++) {
-    jank(ms);
-  }
+function generateParserDelay(seconds = 1) {
+  document.write(`
+    <script src="/loading/resources/dummy.js?pipe=trickle(d${seconds})"></script>
+  `);
 }
