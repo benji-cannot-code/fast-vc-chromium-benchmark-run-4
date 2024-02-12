068a73eda4ceab48bd13a521e05585053d59cdf5
Joey Arhar <jarhar@chromium.org>
2024-02-12 21:57:52 +0000

    Add UseCounter for tags dropped by <select> parser
    At the last WHATWG triage meeting, I was given an action item to get
    data on how many pages would break if we didn't have an opt in for
    changing the <select> parser to allow additional content without an
    explicit opt-in. This patch adds a use counter to measure this.
    
    Bug: 1511354
    Change-Id: I179574e8b73e893b58a090c8e59c47e417d5698c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5280597
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259465}

--- third_party/blink/renderer/core/html/parser/html_tree_builder.cc
+++ third_party/blink/renderer/core/html/parser/html_tree_builder.cc
@@ -1599,6 +1599,8 @@ void HTMLTreeBuilder::ProcessStartTag(AtomicHTMLToken* token) {
           }
           break;
         default:
+          UseCounter::Count(tree_.CurrentNode()->GetDocument(),
+                            WebFeature::kSelectParserDroppedTag);
           break;
       }
       break;
