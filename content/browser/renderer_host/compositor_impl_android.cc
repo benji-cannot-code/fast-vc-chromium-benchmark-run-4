54e7feb3d97b1dc68505ae9f7fd585716a23661f
Colin Blundell <blundell@chromium.org>
2024-02-12 14:02:15 +0000

    [//content] Remove RasterDecoder killswitch in CompositorImplAndroid
    This change rolled out with a killswitch in 121 [1]. 121 has been on
    stable for several weeks with no problems. This CL rips out the
    killswitch.
    
    [1] https://chromiumdash.appspot.com/commits?commit=e8d2949d3cae75650397f9a645c1f16fb7afa0c7&platform=Windows
    
    Bug: 40946049
    Change-Id: I806858cb3ea13e71b5f4ac276622118dd3e6b7d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5284996
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259133}

--- content/browser/renderer_host/compositor_impl_android.cc
+++ content/browser/renderer_host/compositor_impl_android.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/auto_reset.h"
 #include "base/command_line.h"
 #include "base/containers/contains.h"
-#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/lazy_instance.h"
 #include "base/logging.h"
@@ -94,12 +93,6 @@ namespace content {
 
 namespace {
 
-// Controls if browser compositor context can be backed by raster decoder.
-// TODO(crbug.com/1505425): Remove kill switch once rolled out to stable.
-BASE_FEATURE(kUseRasterDecoderForAndroidBrowserContext,
-             "UseRasterDecoderForAndroidBrowserContext",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 // NOINLINE to make sure crashes use this for magic signature.
 NOINLINE void FatalSurfaceFailure() {
   LOG(FATAL) << "Fatal surface initialization failure";
@@ -121,9 +114,8 @@ gpu::ContextCreationAttribs GetCompositorContextAttributes(
   attributes.need_alpha = requires_alpha_channel;
 
   attributes.enable_raster_interface = true;
-  attributes.enable_gles2_interface =
-      !base::FeatureList::IsEnabled(kUseRasterDecoderForAndroidBrowserContext);
-  attributes.enable_grcontext = attributes.enable_gles2_interface;
+  attributes.enable_gles2_interface = false;
+  attributes.enable_grcontext = false;
 
   return attributes;
 }
