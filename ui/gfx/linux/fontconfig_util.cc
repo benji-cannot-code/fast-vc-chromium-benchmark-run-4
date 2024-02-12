7fb5132a6f05cefbee02e0f62d01ba6323173725
Tom Anderson <thomasanderson@chromium.org>
2024-02-12 22:31:52 +0000

    Add UMA histogram and tracing for FontConfig initialization
    Change-Id: I7edf33bb2d682841123d2016f0ff0bca0bc9b1ca
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5280935
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1259487}

--- ui/gfx/linux/fontconfig_util.cc
+++ ui/gfx/linux/fontconfig_util.cc
@@ -9,9 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_op.h"
 #include "base/memory/raw_ptr.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
 #include "base/task/thread_pool.h"
 #include "base/task/thread_pool/thread_pool_instance.h"
+#include "base/trace_event/trace_event.h"
 #include "build/chromeos_buildflags.h"
 #include "ui/gfx/font_render_params.h"
 
@@ -45,6 +47,9 @@ constexpr base::FilePath::CharType kImageloaderMountBase[] =
 class GFX_EXPORT GlobalFontConfig {
  public:
   GlobalFontConfig() {
+    TRACE_EVENT0("ui", "GlobalFontConfig::GlobalFontConfig");
+    SCOPED_UMA_HISTOGRAM_TIMER("Startup.InitializeFontConfigDuration");
+
     // Without this call, the FontConfig library gets implicitly initialized
     // on the first call to FontConfig. Since it's not safe to initialize it
     // concurrently from multiple threads, we explicitly initialize it here
