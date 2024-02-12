ebfa7059b71482547bde6ad31252cf5b7a37fab9
Calder Kitagawa <ckitagawa@chromium.org>
2024-02-12 18:31:17 +0000

    [Base] Make Android Token JNI Natives visible for testing
    In unit tests it is useful to mock the return of stable known values for
    computeRandom. Make this possible by adding a VisibleForTesting
    annotation.
    
    See https://chromium-review.googlesource.com/c/chromium/src/+/5285077
    for the first usage.
    
    Bug: None
    Change-Id: I8581af899f917a72b89d8b756cdeb16df2b09ff4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5286256
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259295}

--- base/android/java/src/org/chromium/base/Token.java
+++ base/android/java/src/org/chromium/base/Token.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.base;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 
 import com.google.errorprone.annotations.DoNotMock;
 
@@ -76,7 +77,8 @@ public final class Token {
     }
 
     @NativeMethods
-    interface Natives {
+    @VisibleForTesting
+    public interface Natives {
         Token createRandom();
     }
 }
