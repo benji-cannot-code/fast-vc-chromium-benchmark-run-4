9cba28b8d6d59ddda4c12ba5ea4b69508dc2e43e
Wes Okuhara <wesokuhara@google.com>
2024-02-12 22:00:06 +0000

    CrOS Settings: Fix SearchHandlerTest for revamp
    When the `OsSettingsRevampWayfinding` feature is enabled, the kPrinting
    section should be replaced with the new kSystemPreferences section.
    
    Bug: b:324913638
    Test: unit_tests --gtest_filter="SearchHandlerTest.*"
    Change-Id: Icfb1b5c22a3af038df00c211a780b66f6741c8fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5287935
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1259466}

--- chrome/browser/ui/webui/ash/settings/search/search_handler_unittest.cc
+++ chrome/browser/ui/webui/ash/settings/search/search_handler_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/ash/settings/search/search_handler.h"
 
+#include "ash/constants/ash_features.h"
 #include "ash/webui/settings/public/constants/routes.mojom.h"
 #include "base/no_destructor.h"
 #include "base/strings/utf_string_conversions.h"
@@ -30,6 +31,9 @@ using ::chromeos::settings::mojom::Subpage;
 }  // namespace mojom
 
 namespace {
+
+const bool kIsRevampEnabled =
+    ash::features::IsOsSettingsRevampWayfindingEnabled();
 
 class FakeObserver : public mojom::SearchResultsObserver {
  public:
@@ -82,6 +86,10 @@ const std::vector<SearchConcept>& GetPrintingSearchConcepts() {
 
 // Creates a result with some default values.
 mojom::SearchResultPtr CreateDummyResult() {
+  const mojom::Section kSection = kIsRevampEnabled
+                                      ? mojom::Section::kSystemPreferences
+                                      : mojom::Section::kPrinting;
+
   return mojom::SearchResult::New(
       /*text=*/std::u16string(),
       /*canonical_text=*/std::u16string(), /*url=*/"",
@@ -90,7 +98,7 @@ mojom::SearchResultPtr CreateDummyResult() {
       mojom::SearchResultDefaultRank::kMedium,
       /*was_generated_from_text_match=*/false,
       mojom::SearchResultType::kSection,
-      mojom::SearchResultIdentifier::NewSection(mojom::Section::kPrinting));
+      mojom::SearchResultIdentifier::NewSection(kSection));
 }
 
 }  // namespace
@@ -110,14 +118,17 @@ class SearchHandlerTest : public testing::Test {
   void SetUp() override {
     handler_.BindInterface(handler_remote_.BindNewPipeAndPassReceiver());
 
+    const mojom::Section kSection = kIsRevampEnabled
+                                        ? mojom::Section::kSystemPreferences
+                                        : mojom::Section::kPrinting;
+
     fake_hierarchy_.AddSubpageMetadata(
-        IDS_SETTINGS_PRINTING_CUPS_PRINTERS, mojom::Section::kPrinting,
+        IDS_SETTINGS_PRINTING_CUPS_PRINTERS, kSection,
         mojom::Subpage::kPrintingDetails, mojom::SearchResultIcon::kPrinter,
         mojom::SearchResultDefaultRank::kMedium,
         mojom::kPrintingDetailsSubpagePath);
-    fake_hierarchy_.AddSettingMetadata(mojom::Section::kPrinting,
-                                       mojom::Setting::kAddPrinter);
-    fake_hierarchy_.AddSettingMetadata(mojom::Section::kPrinting,
+    fake_hierarchy_.AddSettingMetadata(kSection, mojom::Setting::kAddPrinter);
+    fake_hierarchy_.AddSettingMetadata(kSection,
                                        mojom::Setting::kSavedPrinters);
 
     handler_remote_->Observe(observer_.GenerateRemote());
@@ -213,7 +224,11 @@ TEST_F(SearchHandlerTest, UrlModification) {
 
   // The URL should have bee modified according to the FakeOsSettingSection
   // scheme.
-  EXPECT_EQ(std::string("kPrinting::") + mojom::kPrintingDetailsSubpagePath,
+  const std::string kPrefix = kIsRevampEnabled
+                                  ? std::string("kSystemPreferences::")
+                                  : std::string("kPrinting::");
+
+  EXPECT_EQ(kPrefix + mojom::kPrintingDetailsSubpagePath,
             search_results[0]->url_path_with_parameters);
 }
 
