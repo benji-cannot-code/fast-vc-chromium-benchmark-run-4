92425dd314b98592331d4c63cfbe5cdbd57b760b
rbpotter <rbpotter@chromium.org>
2024-02-12 19:01:40 +0000

    Reland "WebUI: Enforce "import type" ESLint checks in default config"
    This reverts commit a5a52f0d59270fc94893397a994ee8126c68f5d4.
    
    Reason for revert: Fixing by adding another exception for
    tools/typescript/definitions/ folder, which contains
    autogenerated files that do not conform to the style guide.
    
    Original change's description:
    > Revert "WebUI: Enforce "import type" ESLint checks in default config"
    >
    > This reverts commit 08578eed0fc4e20efe7d17add164e8dde4185424.
    >
    > Reason for revert: Suspected to break presubmits on https://ci.chromium.org/ui/p/chromium/builders/ci/linux-presubmit/11874/
    >
    > Original change's description:
    > > WebUI: Enforce "import type" ESLint checks in default config
    > >
    > > Moving "import type" presubmit to the top level .eslintrc.js file and
    > > removing from lower level files.
    > >
    > > Adding overrides for ash/webui and ui/file_manager, to avoid applying
    > > this rule in those folders, where there are a large number of
    > > violations.
    > >
    > > Bug: 1494527
    > > Change-Id: I729a097b856bc4f6a1a727cbcb56e9dc5545bed7
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5278066
    > > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > > Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    > > Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1258197}
    >
    > Bug: 1494527
    > Change-Id: I8e1aeaed8b6d20564a3e40f0f5e9a8ba4610134f
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5280738
    > Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Christian Dullweber <dullweber@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1258411}
    
    Bug: 1494527
    Change-Id: I3999d2a245184b4752e3b64638dfe88b70a0b1c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5282074
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259326}

--- chrome/browser/resources/.eslintrc.js
+++ chrome/browser/resources/.eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
     'browser' : true,
     'es6' : true,
   },
-  'extends': '../../../ui/webui/resources/tools/eslint_import_type.config.js',
   'rules' : {
     'eqeqeq' : ['error', 'always', {'null' : 'ignore'}],
   },
