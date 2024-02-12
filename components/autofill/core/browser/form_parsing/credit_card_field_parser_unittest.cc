d7fd57d3a74d2f8cf59a1089571984d001b032a1
Dominic Battre <battre@chromium.org>
2024-02-12 08:38:23 +0000

    Improve members of FormFieldParserTestBase
    This CL renames FormFieldParserTestBase::list_ to
    FormFieldParserTestBase::fields_ to better describe the contents of the
    member and documents the purpose of the member variables of
    FormFieldParserTestBase.
    
    Bug: 1007974
    Change-Id: I5b70a1060ad12a28f728ce4dd73d5a9e6560ab70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5284994
    Commit-Queue: Dominic Battre <battre@chromium.org>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1259066}

--- components/autofill/core/browser/form_parsing/credit_card_field_parser_unittest.cc
+++ components/autofill/core/browser/form_parsing/credit_card_field_parser_unittest.cc
@@ -151,7 +151,7 @@ TEST_P(CreditCardFieldParserTest, ParseMiniumCreditCardWith2DigitYearHint) {
   AddTextFormFieldData("card_number", "Card Number", CREDIT_CARD_NUMBER);
   AddTextFormFieldData("ccmonth", "Exp Month", CREDIT_CARD_EXP_MONTH);
   AddTextFormFieldData("ccyear", "Exp Year", CREDIT_CARD_EXP_2_DIGIT_YEAR);
-  list_.back()->placeholder = u"YY";
+  fields_.back()->placeholder = u"YY";
   ClassifyAndVerify(ParseResult::kParsed);
 }
 
@@ -164,8 +164,8 @@ TEST_P(CreditCardFieldParserTest, ParseMiniumCreditCardWithMaxLength) {
   AddTextFormFieldData("ccyear", "Exp Year", CREDIT_CARD_EXP_2_DIGIT_YEAR);
   // Even though the placehodler indicates YYYY, the max-length only enables
   // a YY expiration format.
-  list_.back()->max_length = 2u;
-  list_.back()->placeholder = u"YYYY";
+  fields_.back()->max_length = 2u;
+  fields_.back()->placeholder = u"YYYY";
   ClassifyAndVerify(ParseResult::kParsed);
 }
 
@@ -215,7 +215,7 @@ TEST_P(CreditCardFieldYearTest, ParseMinimumCreditCardWithExpiryDateOptions) {
                             expected_type());
 
   if (ShouldSwapMonthAndYear())
-    std::swap(list_[1], list_[2]);
+    std::swap(fields_[1], fields_[2]);
 
   ClassifyAndVerify(ParseResult::kParsed);
 }
