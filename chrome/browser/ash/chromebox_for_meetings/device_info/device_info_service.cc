c87935d16c949282fddbb801c1f17068a8540969
Helmut Januschka <helmut@januschka.com>
2024-02-12 18:21:25 +0000

    chromebox_for_meetings: base::StringPiece -> std::string_view
    Bug: 691162
    Change-Id: I19dda541e1dbdb95d07729079c907d3d98b3bc5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5279455
    Reviewed-by: Kyle Williams <kdgwill@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Helmut Januschka <helmut@januschka.com>
    Cr-Commit-Position: refs/heads/main@{#1259283}

--- chrome/browser/ash/chromebox_for_meetings/device_info/device_info_service.cc
+++ chrome/browser/ash/chromebox_for_meetings/device_info/device_info_service.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cstdint>
 #include <optional>
+#include <string_view>
 
 #include "base/files/file_path.h"
 #include "base/functional/bind.h"
@@ -247,7 +248,7 @@ void DeviceInfoService::GetMachineStatisticsInfo(
 
   auto stat_info = mojom::MachineStatisticsInfo::New();
 
-  if (const std::optional<base::StringPiece> hwid =
+  if (const std::optional<std::string_view> hwid =
           system::StatisticsProvider::GetInstance()->GetMachineStatistic(
               system::kHardwareClassKey)) {
     stat_info->hwid = std::string(hwid.value());
