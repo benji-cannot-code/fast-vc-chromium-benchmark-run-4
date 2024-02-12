d518f83a05b63aedf7e9980f3bf4ac5f87dfa2ce
Ioana Pandele <ioanap@chromium.org>
2024-02-12 17:57:05 +0000

    [UPMLocalPwd] Use a different min GMS version on auto
    The default value is set to INT_MAX, since there isn't a good
    default min version yet.
    
    Bug: b/322304412
    Change-Id: Ie2f72124b2480259af223c013fa70294a7a2a302
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5277391
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259271}

--- components/password_manager/core/browser/features/password_features.h
+++ components/password_manager/core/browser/features/password_features.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // This file defines all password manager features used in the browser process.
 // Prefer adding new features here instead of "core/common/".
+#include <limits>
+
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
 #include "build/build_config.h"
@@ -94,7 +96,7 @@ BASE_DECLARE_FEATURE(kPasswordManagerLogToTerminal);
 #if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
 // Enables "Needs access to keychain, restart chrome" bubble and banner.
 BASE_DECLARE_FEATURE(kRestartToGainAccessToKeychain);
-#endif  // BUILDFLAG(IS_MAC)
+#endif  // BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
 
 // Enables the notification UI that is displayed to the user when visiting a
 // website for which a stored password has been shared by another user.
@@ -124,6 +126,13 @@ BASE_DECLARE_FEATURE(kUnifiedPasswordManagerLocalPasswordsAndroidWithMigration);
 inline constexpr char kLocalUpmMinGmsVersionParam[] = "min_gms_version";
 // Default value of kLocalUpmMinGmsVersionParam.
 inline constexpr int kDefaultLocalUpmMinGmsVersion = 240212000;
+
+// Same as above, but for automotive.
+inline constexpr char kLocalUpmMinGmsVersionParamForAuto[] =
+    "min_gms_version_for_auto";
+inline constexpr int kDefaultLocalUpmMinGmsVersionForAuto =
+    std::numeric_limits<int>::max();
+
 #endif  // !BUILDFLAG(IS_ANDROID)
 
 // Improves PSL matching capabilities by utilizing PSL-extension list from
