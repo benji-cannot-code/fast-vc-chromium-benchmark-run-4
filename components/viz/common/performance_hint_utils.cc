1af79f1c3c3e090e375050b04a5f921509c1de3b
Igor Kraskevich <kraskevich@google.com>
2024-02-12 14:40:54 +0000

    Cleanup launched EnableADPFAsyncThreadsVerification feature
    Bug: b/302096134
    Change-Id: Iff095efe558edff84e42ace75fd57be02459b008
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5280746
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Igor Kraskevich <kraskevich@google.com>
    Cr-Commit-Position: refs/heads/main@{#1259155}

--- components/viz/common/performance_hint_utils.cc
+++ components/viz/common/performance_hint_utils.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/viz/common/performance_hint_utils.h"
 
+#include <vector>
+
 #include "base/containers/contains.h"
 #include "base/logging.h"
 #include "build/build_config.h"
@@ -14,30 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace viz {
-
-bool CheckThreadIdsDoNotBelongToProcessIds(
-    const std::vector<base::ProcessId>& privileged_process_ids,
-    const base::flat_set<base::PlatformThreadId>&
-        thread_ids_from_sandboxed_process) {
-#if BUILDFLAG(IS_ANDROID)
-  // This is inherently racy as threads or processes can die and the IDs can be
-  // reused at any time. But this is the best we can do.
-  for (auto& process_id : privileged_process_ids) {
-    std::vector<pid_t> privileged_thread_ids;
-    if (!base::GetThreadsForProcess(process_id, &privileged_thread_ids)) {
-      return false;
-    }
-    for (auto& tid : thread_ids_from_sandboxed_process) {
-      if (base::Contains(privileged_thread_ids, tid)) {
-        return false;
-      }
-    }
-  }
-  return true;
-#else
-  return false;
-#endif
-}
 
 bool CheckThreadIdsDoNotBelongToCurrentProcess(
     const base::flat_set<base::PlatformThreadId>&
