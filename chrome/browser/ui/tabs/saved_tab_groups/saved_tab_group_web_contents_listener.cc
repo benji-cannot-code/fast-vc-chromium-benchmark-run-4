bbc48f6816edc7fe37200deefa88e21640315189
dljames <dljames@google.com>
2024-02-13 00:00:14 +0000

    [ SavedTabGroup ] URL fragments no longer reload tabs across devices
    This CL prevents tabs on other devices from reloading / reacting to
    fragment changes received from the device being used. In some instances
    it would reset the state of the web page which was the source of the
    frustration.
    
    For example, in google docs, users with the group open on multiple
    devices would see their cursor jump around the webpage, and even go back
    to the top of the screen as they were typing / moving over headings
    which updated the url fragment.
    
    Change-Id: I543fb4b5910a6b01c4f9fb49ee5cd8598d5fecf2
    Bug: 40928836
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5287944
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259552}

--- chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_web_contents_listener.cc
+++ chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_web_contents_listener.cc
@@ -31,6 +31,10 @@ bool IsSaveableNavigation(content::NavigationHandle* navigation_handle) {
     return false;
   }
 
+  if (navigation_handle->IsSameDocument()) {
+    return false;
+  }
+
   return SavedTabGroupUtils::IsURLValidForSavedTabGroups(
       navigation_handle->GetURL());
 }
@@ -48,6 +52,11 @@ SavedTabGroupWebContentsListener::SavedTabGroupWebContentsListener(
 SavedTabGroupWebContentsListener::~SavedTabGroupWebContentsListener() = default;
 
 void SavedTabGroupWebContentsListener::NavigateToUrl(const GURL& url) {
+  if (web_contents_->GetURL().GetWithoutRef().spec() ==
+      url.GetWithoutRef().spec()) {
+    return;
+  }
+
   // Dont navigate to the new URL if its not valid for sync.
   if (!SavedTabGroupUtils::IsURLValidForSavedTabGroups(url)) {
     return;
