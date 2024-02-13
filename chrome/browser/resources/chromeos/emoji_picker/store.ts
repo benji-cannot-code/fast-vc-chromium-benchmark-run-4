03b6fa78cb71eea5825d5bf7a77029cc8223f7b2
Luke Fisk-Lennon <lukefl@google.com>
2024-02-13 05:32:16 +0000

    [Emoji picker] Increase recent emojis limit from 10 to 18
    A limit of 10 or 18 uses the same amount of space (2 rows) because there
    are 9 columns in the emoji picker, but now the user will be able to
    access more recents.
    
    Fixed: b:324795730
    Change-Id: Ia3574839718bac8992f1a96a920d7c7b3d7b3120
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5276769
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Commit-Queue: Luke Fisk-Lennon <lukefl@google.com>
    Reviewed-by: Zac Partridge <zacpartridge@google.com>
    Cr-Commit-Position: refs/heads/main@{#1259665}

--- chrome/browser/resources/chromeos/emoji_picker/store.ts
+++ chrome/browser/resources/chromeos/emoji_picker/store.ts
@@ -5,9 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {EmojiPickerApiProxy} from 'emoji_picker_api_proxy.js';
 
+import {EMOJI_PER_ROW} from './constants.js';
 import {CategoryEnum, Emoji, EmojiVariants, Gender, PreferenceMapping, Tone, VisualContent} from './types.js';
 
-const MAX_RECENTS = 10;
+const MAX_RECENTS = EMOJI_PER_ROW * 2;
 
 class Store<T> {
   data: T;
