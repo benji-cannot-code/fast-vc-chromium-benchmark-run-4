97dd795534ec713e730e6c6cb74ee99971938621
Etienne Dechamps <edechamps@google.com>
2024-02-13 13:41:30 +0000

    Move telemetry hashing to a separate class
    This will be reused for logging http flags names and values.
    
    Bug: b:313418339
    Change-Id: Ife9f2971f885c4855faec8cb29e367af4e3d6502
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5279545
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Commit-Queue: Etienne Dechamps <edechamps@google.com>
    Cr-Commit-Position: refs/heads/main@{#1259798}

--- components/cronet/android/java/src/org/chromium/net/telemetry/CronetLoggerImpl.java
+++ components/cronet/android/java/src/org/chromium/net/telemetry/CronetLoggerImpl.java
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.net.telemetry;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
-
 import android.os.Build;
 import android.util.Log;
 
@@ -15,9 +13,6 @@ import androidx.annotation.VisibleForTesting;
 
 import org.chromium.net.impl.CronetLogger;
 
-import java.nio.ByteBuffer;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -25,19 +20,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 @RequiresApi(Build.VERSION_CODES.R)
 public class CronetLoggerImpl extends CronetLogger {
     private static final String TAG = CronetLoggerImpl.class.getSimpleName();
-
-    private static final MessageDigest MD5_MESSAGE_DIGEST;
-
-    static {
-        MessageDigest messageDigest;
-        try {
-            messageDigest = MessageDigest.getInstance("MD5");
-        } catch (NoSuchAlgorithmException e) {
-            Log.d(TAG, "Error while instantiating messageDigest", e);
-            messageDigest = null;
-        }
-        MD5_MESSAGE_DIGEST = messageDigest;
-    }
 
     private final AtomicInteger mSamplesRateLimited = new AtomicInteger();
     private final RateLimiter mRateLimiter;
@@ -197,7 +179,7 @@ public class CronetLoggerImpl extends CronetLogger {
                     SizeBuckets.calcResponseBodySizeBucket(
                             trafficInfo.getResponseBodySizeInBytes()),
                     trafficInfo.getResponseStatusCode(),
-                    hashNegotiatedProtocol(trafficInfo.getNegotiatedProtocol()),
+                    Hash.hash(trafficInfo.getNegotiatedProtocol()),
                     (int) trafficInfo.getHeadersLatency().toMillis(),
                     (int) trafficInfo.getTotalLatency().toMillis(),
                     trafficInfo.wasConnectionMigrationAttempted(),
@@ -264,14 +246,5 @@ public class CronetLoggerImpl extends CronetLogger {
             default:
                 throw new IllegalArgumentException("Expected httpCacheMode to range from 0 to 3");
         }
-    }
-
-    private static long hashNegotiatedProtocol(String protocol) {
-        if (MD5_MESSAGE_DIGEST == null || protocol == null || protocol.isEmpty()) {
-            return 0L;
-        }
-
-        byte[] md = MD5_MESSAGE_DIGEST.digest(protocol.getBytes(UTF_8));
-        return ByteBuffer.wrap(md).getLong();
     }
 }
