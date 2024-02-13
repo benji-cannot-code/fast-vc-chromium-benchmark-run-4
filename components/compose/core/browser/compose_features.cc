c04460850f979bb52758cf81be474a58afb836b9
Cathy Li <chili@chromium.org>
2024-02-13 21:29:39 +0000

    Add flag for text selection heuristics.
    Bug: b/323061838
    Change-Id: Ic050e366b8450e83ae44ea658810e455be5a5f99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5277563
    Commit-Queue: Cathy Li <chili@chromium.org>
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260066}

--- components/compose/core/browser/compose_features.cc
+++ components/compose/core/browser/compose_features.cc
@@ -54,4 +54,7 @@ BASE_FEATURE(kComposeTextOutputAnimation,
              "ComposeTextOutputAnimation",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kComposeTextSelection,
+             "ComposeTextSelection",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 }  // namespace compose::features
