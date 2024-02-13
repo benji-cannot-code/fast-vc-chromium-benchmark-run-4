c7807b5ab01dbefe446be5dd1015225f0a0fa53d
Shuming Hao <shuminghao@google.com>
2024-02-13 07:00:32 +0000

    Override common functions for ArcNotificationView
    [Chrome rendered ARC notifications] This CL overrides a few common functions in ArcNotificationView, so that grouped ARC custom notifications can show collapsed summary view when required. Collapsed summary view is implemented as a child of arc notification view. When the group parent expands, the original arc notification content view is shown with attached surface. When group parent collapses, the collapsed summary view is shown to stay in consistent with AshNotificationView, the arc notification content view is hidden, and the attached notification surface is detached.
    
    Bug: b:306472474
    Test: manual with Notify1.apk
    Recall: http://recall/clips/a922f6a9-f9f2-40aa-abf5-2c85d5a8f36b
    Change-Id: Iff68330209922d6ebc60e377a01caf103a1b83d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5122714
    Reviewed-by: James Cook <jamescook@chromium.org>
    Auto-Submit: Shuming Hao <shuminghao@google.com>
    Reviewed-by: Luzanne Batoon <batoon@google.com>
    Reviewed-by: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Commit-Queue: Shuming Hao <shuminghao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1259683}

--- ash/public/cpp/external_arc/message_center/arc_notification_content_view.cc
+++ ash/public/cpp/external_arc/message_center/arc_notification_content_view.cc
@@ -426,10 +426,10 @@ void ArcNotificationContentView::MaybeCreateFloatingControlButtons() {
   // of the hosting widget's focus chain. It could only be created when both
   // are present. Further, if we are being destroyed (|item_| is null), don't
   // create the control buttons.
-  if (!surface_ || !GetWidget() || !item_)
+  if (!surface_ || !GetWidget() || !item_ || control_buttons_view_.parent()) {
     return;
+  }
 
-  DCHECK(!control_buttons_view_.parent());
   DCHECK(!floating_control_buttons_widget_);
 
   views::Widget::InitParams params(views::Widget::InitParams::TYPE_CONTROL);
@@ -553,6 +553,23 @@ void ArcNotificationContentView::AttachSurface() {
   MaybeCreateFloatingControlButtons();
 
   UpdateMask(false /* force_update */);
+}
+
+void ArcNotificationContentView::EnsureSurfaceAttached() {
+  if (!surface_ || surface_->IsAttached()) {
+    return;
+  }
+  AttachSurface();
+}
+
+void ArcNotificationContentView::EnsureSurfaceDetached() {
+  if (!GetWidget()) {
+    return;
+  }
+
+  if (surface_ && surface_->IsAttached()) {
+    surface_->Detach();
+  }
 }
 
 void ArcNotificationContentView::ShowCopiedSurface() {
