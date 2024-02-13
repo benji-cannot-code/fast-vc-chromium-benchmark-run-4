4148ee554f30e56a71e2765561b6215a7de26f68
Ritika Gupta <ritikagup@google.com>
2024-02-13 21:12:16 +0000

    [garderner] Disabling testAllowRuntimeCamera.
    Change-Id: I1ed4782b0ff4551a0f6796c5ab493df590b9cb8a
    Bug: b/325085976
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5293514
    Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
    Owners-Override: Ritika Gupta <ritikagup@google.com>
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Cr-Commit-Position: refs/heads/main@{#1260053}

--- chrome/android/javatests/src/org/chromium/chrome/browser/permissions/RuntimePermissionTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/permissions/RuntimePermissionTest.java
@@ -18,6 +18,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features;
 import org.chromium.base.test.util.MaxAndroidSdkLevel;
@@ -86,6 +87,7 @@ public class RuntimePermissionTest {
     @MediumTest
     @Feature({"RuntimePermissions", "MediaPermissions"})
     @CommandLineFlags.Add(ContentSwitches.USE_FAKE_DEVICE_FOR_MEDIA_STREAM)
+    @DisabledTest(message = "b/325085976")
     public void testAllowRuntimeCamera() throws Exception {
         String[] requestablePermission = new String[] {Manifest.permission.CAMERA};
         mTestAndroidPermissionDelegate =
