4db67ee43ea02f42e332607dc2ce9d6c329205f2
François Degros <fdegros@chromium.org>
2024-02-13 04:07:32 +0000

    Files app: Convert zip_files.js to TypeScript
    BUG=b:319189127
    
    Change-Id: Ida8bc270812be22168fb9b75710ef14713c37a60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5289694
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Auto-Submit: François Degros <fdegros@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259645}

--- ui/file_manager/integration_tests/testcase.ts
+++ ui/file_manager/integration_tests/testcase.ts
@@ -52,7 +52,7 @@ import * as shareTests from './file_manager/share.js';
 // import * as transferTests from './file_manager/transfer.js';
 import * as trashTests from './file_manager/trash.js';
 import * as traverseTests from './file_manager/traverse.js';
-// import * as zipFilesTests from './file_manager/zip_files.js';
+import * as zipFilesTests from './file_manager/zip_files.js';
 // clang-format on
 
 export type TestFunctionName = string;
@@ -110,5 +110,5 @@ export const testcase: Record<TestFunctionName, TestFunction> = {
   // ...transferTests,
   ...trashTests,
   ...traverseTests,
-  // ...zipFilesTests,
+  ...zipFilesTests,
 };
