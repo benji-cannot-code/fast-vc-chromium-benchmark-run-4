c5f2820984e8fa454c1fe976be65ec16dc514ef1
danakj <danakj@chromium.org>
2024-02-13 23:42:56 +0000

    Write unsafe review for BufferIterator and use safer span methods
    Replaces all memcpy() calls with span::copy_from(). Adds
    UNSAFE_BUFFERS() around any unsafe pointer arithmetic, which is an
    intrinsic requirement of this type, so we can't convert them all to
    spans. Adds SAFETY comments on any UNSAFE_BUFFERS(). Mark the
    constructor from pointer/size as UNSAFE_BUFFER_USAGE and deprecated.
    
    We move the unittests for BufferIterator over to the world of
    unsafe-buffer-usage warnings. To do so, we must silence warnings
    through the raw_ptr/raw_ref headers (and, sadly, their transitive
    includes!!!) so we add UNSAFE_BUFERS_INCLUDE_BEGIN/END macros to
    assist with that for now, with TODOs. Also the same for the
    contiguous iterator include.
    
    Bug: 40284755
    Change-Id: Iad38db2ea8c695cdf1c1aab06140315654a00f81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5288475
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Auto-Submit: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260145}

--- base/containers/checked_iterators.h
+++ base/containers/checked_iterators.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <type_traits>
 
 #include "base/check_op.h"
+#include "base/compiler_specific.h"
 #include "base/containers/util.h"
 #include "base/memory/raw_ptr_exclusion.h"
 #include "build/build_config.h"
@@ -39,10 +40,13 @@ class CheckedContiguousIterator {
 
   constexpr CheckedContiguousIterator() = default;
 
-  constexpr CheckedContiguousIterator(T* start, const T* end)
+  UNSAFE_BUFFER_USAGE constexpr CheckedContiguousIterator(T* start,
+                                                          const T* end)
       : CheckedContiguousIterator(start, start, end) {}
 
-  constexpr CheckedContiguousIterator(const T* start, T* current, const T* end)
+  UNSAFE_BUFFER_USAGE constexpr CheckedContiguousIterator(const T* start,
+                                                          T* current,
+                                                          const T* end)
       : start_(start), current_(current), end_(end) {
     CHECK_LE(start, current);
     CHECK_LE(current, end);
