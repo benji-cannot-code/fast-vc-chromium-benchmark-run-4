c27af1d4cd782be8f7ea99bd70defb9acf0dfb29
Sam Maier <smaier@chromium.org>
2024-02-13 18:57:17 +0000

    jni_zero: rename jni_zero_helper.h to jni_zero.h
    The followup here will be to merge all the headers together.
    
    Bug: 325057352
    Change-Id: Ic663016205b52f44b10ac3a0eaca132c4b0ac3e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5292374
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Owners-Override: Sam Maier <smaier@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259956}

--- chrome/browser/vr/jni_onload.cc
+++ chrome/browser/vr/jni_onload.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "third_party/jni_zero/jni_zero_helper.h"
+#include "third_party/jni_zero/jni_zero.h"
 
 // This method is required by the module loading backend. And it is supposed to
 // register VR's native JNI methods. However, since VR's Android-specific native
