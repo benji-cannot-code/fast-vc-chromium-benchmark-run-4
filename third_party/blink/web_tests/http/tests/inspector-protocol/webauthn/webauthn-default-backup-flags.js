484835d90c3e001681d878bf9298832522ad3ca2
Nina Satragno <nsatragno@chromium.org>
2024-02-13 20:28:01 +0000

    [devtools] Add WebAuthn BE & BS credential flags
    Add backup eligibility and backup state attributes to the WebAuthn's
    Credential type. These attributes can be used to set the corresponding
    flags per-credential using the `addCredential` method, and also reflect
    the state when a credential is returned by `getCredentials`.
    
    See https://w3c.github.io/webauthn#credential-parameters
    
    Bug: 40945430
    Change-Id: I017d867799259f7a6ebeb7331bdad02f1489a149
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5292554
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260020}

--- third_party/blink/web_tests/http/tests/inspector-protocol/webauthn/webauthn-default-backup-flags.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/webauthn/webauthn-default-backup-flags.js
@@ -1,17 +1,21 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 (async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
   const {page, session, dp} =
       await testRunner.startURL(
           "https://devtools.test:8443/inspector-protocol/webauthn/resources/webauthn-test.https.html",
-          "Check that the WebAuthn backup flags are reflected when asserting credentials");
+          "Check that the WebAuthn default backup flags are reflected");
 
   await dp.WebAuthn.enable();
 
   const tests = [
-    { option: "defaultBackupEligibility", flag: "be", value: true },
-    { option: "defaultBackupEligibility", flag: "be", value: false },
-    { option: "defaultBackupState", flag: "bs", value: true },
-    { option: "defaultBackupState", flag: "bs", value: false },
+    { option: "defaultBackupEligibility", flag: "be", prop: "backupEligibility", value: true },
+    { option: "defaultBackupEligibility", flag: "be", prop: "backupEligibility", value: false },
+    { option: "defaultBackupState", flag: "bs", prop: "backupState", value: true },
+    { option: "defaultBackupState", flag: "bs", prop: "backupState", value: false },
   ];
 
   for (const test of tests) {
@@ -46,9 +50,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       type: "public-key",
       id: base64ToArrayBuffer("${credentialId}"),
     })`);
-    testRunner.log(`Get credential status: ${assertion.status}`);
+    testRunner.log(`Get assertion status: ${assertion.status}`);
     testRunner.log(
       `${test.flag}: ${assertion.flags[test.flag]} (expected ${test.value})`);
+
+    // Verify that getting the credential through devtools reflects the value.
+    let credential =
+        (await dp.WebAuthn.getCredential({authenticatorId, credentialId})).result.credential;
+    testRunner.log("Get credential through devtools");
+    testRunner.log(
+      `${test.prop}: ${credential[test.prop]} (expected ${test.value})`);
 
     // Verify that the default also applies to credentials created through the
     // devtools protocol.
