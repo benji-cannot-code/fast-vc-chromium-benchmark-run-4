193cc48189d033cd4e31e857400953caf8a0680d
Shuran Huang <shuuran@chromium.org>
2024-02-13 21:36:05 +0000

    Add mojo traits for net::CookieInclusionStatus::ExemptionReason
    ... and debug string.
    
    Bug: 1518872
    Change-Id: Ie150eaf26a6676a39746b976b7087886fcd46628
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5279619
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Shuran Huang <shuuran@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260071}

--- services/network/public/cpp/cookie_manager_shared_mojom_traits.h
+++ services/network/public/cpp/cookie_manager_shared_mojom_traits.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_NETWORK_PUBLIC_CPP_COOKIE_MANAGER_SHARED_MOJOM_TRAITS_H_
 #define SERVICES_NETWORK_PUBLIC_CPP_COOKIE_MANAGER_SHARED_MOJOM_TRAITS_H_
 
+#include "mojo/public/cpp/bindings/enum_traits.h"
 #include "mojo/public/cpp/bindings/struct_traits.h"
 #include "net/base/schemeful_site.h"
 #include "net/cookies/cookie_inclusion_status.h"
@@ -33,6 +34,17 @@ struct COMPONENT_EXPORT(NETWORK_CPP_COOKIES)
 
 template <>
 struct COMPONENT_EXPORT(NETWORK_CPP_COOKIES)
+    EnumTraits<network::mojom::CookieExemptionReason,
+               net::CookieInclusionStatus::ExemptionReason> {
+  static network::mojom::CookieExemptionReason ToMojom(
+      net::CookieInclusionStatus::ExemptionReason input);
+
+  static bool FromMojom(network::mojom::CookieExemptionReason input,
+                        net::CookieInclusionStatus::ExemptionReason* output);
+};
+
+template <>
+struct COMPONENT_EXPORT(NETWORK_CPP_COOKIES)
     StructTraits<network::mojom::CookieInclusionStatusDataView,
                  net::CookieInclusionStatus> {
   static uint32_t exclusion_reasons(const net::CookieInclusionStatus& s) {
@@ -40,6 +52,10 @@ struct COMPONENT_EXPORT(NETWORK_CPP_COOKIES)
   }
   static uint32_t warning_reasons(const net::CookieInclusionStatus& s) {
     return static_cast<uint32_t>(s.warning_reasons().to_ulong());
+  }
+  static net::CookieInclusionStatus::ExemptionReason exemption_reason(
+      const net::CookieInclusionStatus& s) {
+    return s.exemption_reason();
   }
   static bool Read(network::mojom::CookieInclusionStatusDataView status,
                    net::CookieInclusionStatus* out);
