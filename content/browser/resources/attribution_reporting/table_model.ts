2d0bd38e39e748f2fcf4238747d5de14f65f3a74
Andrew Paseltiner <apaseltiner@chromium.org>
2024-02-13 19:57:42 +0000

    Remove unnecessary classes from Attribution Internals
    Change-Id: I704fd0b79cdce39b453223d6df78fce137c46f5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5287195
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259992}

--- content/browser/resources/attribution_reporting/table_model.ts
+++ content/browser/resources/attribution_reporting/table_model.ts
@@ -16,8 +16,6 @@ export abstract class TableModel<T> {
 
   constructor(public readonly cols: Array<Column<T>>, public sortIdx: number) {}
 
-  styleRow(_tr: Element, _data: T): void {}
-
   abstract getRows(): T[];
 
   rowCount(): number {
