d67e26097bce203e7fa64d563a8f1bc907549a43
Ben Reich <benreich@chromium.org>
2024-02-13 03:57:27 +0000

    [FilesTS] Update converter to rewrite testcase namespace
    The testcase.* namespace is no longer used, update the converter script
    to rewrite to export a function and remove the trailing semicolon from
    the closing brace.
    
    Bug: b/319189127
    Change-Id: I4cc586eddb7eb6ea04832f09b42fe8ccb1fccb21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5289675
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259642}

--- ui/file_manager/base/js/convert_to_ts.py
+++ ui/file_manager/base/js/convert_to_ts.py
@@ -27,6 +27,10 @@ COMMENT_JSDOC = re.compile(
     r'@private|@protected|@override'
 )
 
+TESTCASE_NAMESPACE = re.compile(r"testcase\.(.*) =( async)? \(\) => {")
+IMPORT_TESTCASE = re.compile(r"import {testcase} from '../testcase.js';")
+ARROW_FUNCTION_CLOSE_BRACE = re.compile(r"^};$")
+
 # Matching:` this.bla;``  or `private this.bla;`
 NON_INITIALIZED_PROP = re.compile(r'\s+this\.[\w_]+;')
 # Matching: this.bla = 'anything';
@@ -78,6 +82,10 @@ def process_js_file(js_fname):
                 lines.append(line)
                 continue
 
+            # Don't include testcase import.
+            if IMPORT_TESTCASE.match(line):
+                continue
+
             # Fully commented out line, ignores inline comment.
             if is_comment(line):
                 if ts_ignoring:
@@ -97,6 +105,7 @@ def process_js_file(js_fname):
             lines.append(line)
 
     processing_comment = False
+    processing_arrow_function = False
     idx = -1
     while (idx < len(lines) - 1):
         idx += 1
@@ -123,6 +132,19 @@ def process_js_file(js_fname):
 
         if not is_jsdoc_star(line):
             processing_comment = False
+
+        # Arrow functions end with a ';' after their closing brace, remove it.
+        if processing_arrow_function and ARROW_FUNCTION_CLOSE_BRACE.match(
+                line):
+            lines[idx] = "}\n"
+            processing_arrow_function = False
+            continue
+
+        # Rewrite testcase imports to exports instead.
+        match = TESTCASE_NAMESPACE.search(line)
+        if match:
+            processing_arrow_function = True
+            lines[idx] = f"export async function {match.group(1)}() " "{\n"
 
     # Remove the lines marked for deletion.
     remove_lines(lines)
@@ -637,8 +659,8 @@ def process_js_files(files):
             # Process as `file_names.gni` to remove the .js file and add it to
             # the `ts_files = ` section.
             if b == _INTEGRATION_TESTS_ROOT.joinpath('BUILD.gn'):
-              new_build_file = process_file_names_gni(js_path.absolute(), b)
-              replace_file(b, new_build_file)
+                new_build_file = process_file_names_gni(js_path.absolute(), b)
+                replace_file(b, new_build_file)
 
         # Only process file_names.gni for Files app and Image Loader.
         if js_path_abs_str.startswith((str(_FILES_APP_ROOT),
