fadcc87ac1768f925ab4d61198a07b176d98b0a6
Norge Vizcay <vizcay@google.com>
2024-02-13 14:46:55 +0000

    Update FindInaccessibleProfileValues to cover reassigned field types.
    The current logic for FindInaccessibleProfileValues is only limited to
    field types that have an explicit field in settings according to
    libaddressinput.
    
    This CL adds coverage for types which have not an explicit field in
    settings, but are covered by one of its ancestors in the address model
    tree.
    
    Bug: b/41489895
    Change-Id: I6fa1c7114cc66e5e9f62caff85b5f8140e442094
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5287155
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1259820}

--- components/autofill/core/browser/data_model/autofill_profile.cc
+++ components/autofill/core/browser/data_model/autofill_profile.cc
@@ -1291,7 +1291,6 @@ FieldTypeSet AutofillProfile::FindInaccessibleProfileValues() const {
   FieldTypeSet inaccessible_fields;
   const std::string stored_country =
       base::UTF16ToUTF8(GetRawInfo(ADDRESS_HOME_COUNTRY));
-  AutofillCountry country(stored_country.empty() ? "US" : stored_country);
   // Consider only AddressFields which are invisible in the settings for some
   // countries.
   for (const AddressField& adress_field :
@@ -1299,8 +1298,9 @@ FieldTypeSet AutofillProfile::FindInaccessibleProfileValues() const {
         AddressField::DEPENDENT_LOCALITY, AddressField::POSTAL_CODE,
         AddressField::SORTING_CODE}) {
     FieldType field_type = i18n::TypeForField(adress_field);
+    CHECK_EQ(GroupTypeOfFieldType(field_type), FieldTypeGroup::kAddress);
     if (HasRawInfo(field_type) &&
-        !country.IsAddressFieldSettingAccessible(field_type)) {
+        !GetAddress().IsAddressFieldSettingAccessible(field_type)) {
       inaccessible_fields.insert(field_type);
     }
   }
