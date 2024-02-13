626302fa372e7402947304aec25aeb0504ec65eb
Istvan Nagy <iscsi@google.com>
2024-02-13 21:54:54 +0000

    Owner settings: Consider pending changes during list modification.
    The CL fixes the login.RemoveUsersExceptOwner tast test.
    
    Bug: b:313324931
    Change-Id: Id12e84886c8d06fe406da87e5684bc994bcca5d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5120693
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1260080}

--- chrome/browser/ash/ownership/owner_settings_service_ash.cc
+++ chrome/browser/ash/ownership/owner_settings_service_ash.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_switches.h"
 #include "base/command_line.h"
 #include "base/containers/contains.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/functional/callback_helpers.h"
@@ -344,18 +345,42 @@ bool OwnerSettingsServiceAsh::Set(const std::string& setting,
   return true;
 }
 
+// Returns the latest list for setting:
+// 1: retrieve the list from pending changes
+// 2: retrieve the list with CrosSettings, be careful
+// - the CrosSettings is on observer of this object
+// - or the list is already written to the disk
+base::Value::List OwnerSettingsServiceAsh::GetListForSetting(
+    const std::string& setting) const {
+  auto iter = pending_changes_.find(setting);
+  if (iter != pending_changes_.end()) {
+    const std::unique_ptr<base::Value>& pending_val = iter->second;
+    if (!pending_val->is_list()) {
+      LOG(ERROR) << "The " << setting << " setting is not a list!";
+      base::debug::DumpWithoutCrashing();
+      return base::Value::List();
+    }
+    return pending_val->GetList().Clone();
+  }
+  const base::Value* old_value = CrosSettings::Get()->GetPref(setting);
+
+  if (old_value == nullptr) {
+    return base::Value::List();
+  }
+
+  if (!old_value->is_list()) {
+    LOG(ERROR) << "The " << setting << " setting is not a list!";
+    base::debug::DumpWithoutCrashing();
+    return base::Value::List();
+  }
+
+  return old_value->GetList().Clone();
+}
+
 bool OwnerSettingsServiceAsh::AppendToList(const std::string& setting,
                                            const base::Value& value) {
   DCHECK(thread_checker_.CalledOnValidThread());
-  const base::Value* old_value = CrosSettings::Get()->GetPref(setting);
-  if (old_value && !old_value->is_list()) {
-    LOG(ERROR) << "The " << setting << " setting is not a list, append fail.";
-    return false;
-  }
-
-  base::Value::List new_value =
-      old_value ? old_value->GetList().Clone() : base::Value::List();
-
+  base::Value::List new_value = GetListForSetting(setting);
   new_value.Append(value.Clone());
   return Set(setting, base::Value(std::move(new_value)));
 }
@@ -363,12 +388,7 @@ bool OwnerSettingsServiceAsh::AppendToList(const std::string& setting,
 bool OwnerSettingsServiceAsh::RemoveFromList(const std::string& setting,
                                              const base::Value& value) {
   DCHECK(thread_checker_.CalledOnValidThread());
-  const base::Value* old_value = CrosSettings::Get()->GetPref(setting);
-  if (old_value && !old_value->is_list())
-    return false;
-  base::Value::List new_value;
-  if (old_value)
-    new_value = old_value->GetList().Clone();
+  base::Value::List new_value = GetListForSetting(setting);
   new_value.EraseValue(value);
   return Set(setting, base::Value(std::move(new_value)));
 }
