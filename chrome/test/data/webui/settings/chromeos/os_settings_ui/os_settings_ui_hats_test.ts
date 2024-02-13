233746d30689c89b6517b9715148b7940dbc8282
Xiaohui Chen <xiaohuic@google.com>
2024-02-13 20:35:57 +0000

    os settings: remove os in names (1/N)
    rename os_toolbar.* to just toolbar.*
    
    Bug: b/317791210
    Test: manually check the toolbar still works and relying on the CQ
    Change-Id: Ie5d0a353e1e94baa98d066c23f028da2afbf59cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5281628
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260025}

--- chrome/test/data/webui/settings/chromeos/os_settings_ui/os_settings_ui_hats_test.ts
+++ chrome/test/data/webui/settings/chromeos/os_settings_ui/os_settings_ui_hats_test.ts
@@ -78,7 +78,7 @@ suite('<os-settings-ui> HaTS', () => {
     }
 
     function retrieveSearchBox(): void {
-      const toolbar = ui.shadowRoot!.querySelector('os-toolbar');
+      const toolbar = ui.shadowRoot!.querySelector('settings-toolbar');
       assert(toolbar);
       searchBox = toolbar.shadowRoot!.querySelector('os-settings-search-box');
       assert(searchBox);
--- chrome/test/data/webui/settings/chromeos/os_settings_ui/os_settings_ui_toolbar_test.ts
+++ chrome/test/data/webui/settings/chromeos/os_settings_ui/os_settings_ui_toolbar_test.ts
@@ -72,7 +72,7 @@ suite('<os-settings-ui> toolbar', () => {
     ui = await createElement();
     flush();
 
-    const toolbar = ui.shadowRoot!.querySelector('os-toolbar');
+    const toolbar = ui.shadowRoot!.querySelector('settings-toolbar');
     assertTrue(!!toolbar, 'Toolbar should exist');
 
     ui.isNarrow = true;
@@ -91,7 +91,7 @@ suite('<os-settings-ui> toolbar', () => {
 
     test('Toolbar is hidden in kiosk mode', async () => {
       ui = await createElement();
-      const toolbar = ui.shadowRoot!.querySelector('os-toolbar');
+      const toolbar = ui.shadowRoot!.querySelector('settings-toolbar');
       assertNull(toolbar);
     });
   });
