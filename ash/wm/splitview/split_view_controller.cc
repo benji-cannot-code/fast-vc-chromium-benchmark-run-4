fa6367d3b825b3b58b50b7edd8d4dfda5776ae6d
Michele Fan <michelefan@chromium.org>
2024-02-13 02:03:08 +0000

    snap-group: Don't start SVOVS if the to-be-snapped window is the only one
    This cl addresses the P1 request to not show SVOVS if the
    to-be-snapped window is the only window on the active desk and display
    by adding another pre-check condition before starting SVOVS.
    
    This cl fixed a bunch of unit tests that are based on the previous
    assumptions.
    
    This cl also adds logic to end SVOVS on tablet mode started, which
    is used to work due to the previous assumption protection. But
    with the hiding of virtual desk bar and saved desks button, we need
    to reflect the correct UI element for the corresponding SOVOS setup
    types.
    
    Fixed: b/324479915
    Demo: http://b/324479915#comment2
    Test: Added new + fixed existing + manual
    Change-Id: I6ae6344bdddfbffd8df0d236079f291917b26db9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5283248
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259614}

--- ash/wm/splitview/split_view_controller.cc
+++ ash/wm/splitview/split_view_controller.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/overview_controller.h"
 #include "ash/wm/overview/overview_grid.h"
 #include "ash/wm/overview/overview_item.h"
+#include "ash/wm/overview/overview_metrics.h"
 #include "ash/wm/overview/overview_types.h"
 #include "ash/wm/overview/overview_utils.h"
 #include "ash/wm/snap_group/snap_group.h"
@@ -2572,11 +2573,21 @@ void SplitViewController::OnTabletModeStarted() {
     const int divider_position =
         GetClosestFixedDividerPosition(ash::GetEquivalentDividerPosition(
             primary_window_ ? primary_window_ : secondary_window_,
-            /*should_consider_divider=*/false));
+            /*account_for_divider_width=*/false));
     split_view_divider_.ShowFor(divider_position);
 
     UpdateSnappedWindowsAndDividerBounds();
     NotifyDividerPositionChanged();
+
+    // Ends `SplitViewOverviewSession` if it is currently alive, as
+    // `SplitViewOverviewSession` is for clamshell only.
+    RootWindowController* root_window_controller =
+        RootWindowController::ForWindow(root_window_);
+    if (SplitViewOverviewSession* split_view_overview_session =
+            root_window_controller->split_view_overview_session()) {
+      root_window_controller->EndSplitViewOverviewSession(
+          SplitViewOverviewSessionExitPoint::kTabletConversion);
+    }
   }
 }
 
