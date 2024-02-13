68f2a5c8d8d086ee257b1beb459ea8523c71391a
Daniel White <danieltwhite@google.com>
2024-02-13 18:17:17 +0000

    Revert "[iOS] Added ESB Blue-dot Promo FeatureEngagement Tracker"
    This reverts commit 28dde1fa386943f7b8e96cbcacffe49bfcc1faa8.
    
    Reason for revert: Project is not moving forward
    
    Original change's description:
    > [iOS] Added ESB Blue-dot Promo FeatureEngagement Tracker
    >
    > In this CL, the promo tracking infrastructure has been setup for the
    > Enhanced Safe Browsing blue-dot promo. The promos have
    > been configured to display themselves if the FeatureEngagement object
    > finds that the user has triggered the event within the last seven days
    > and has not been show this promo more than three times.
    >
    > In a follow-up CL, the blue-dot promo will be added to the settings
    > UI and will be connected to this new FeatureEngagement tracker.
    >
    > Low-Coverage-Reason: HARD_TO_TEST since it's a config object
    > Fixed: 1503682
    > Change-Id: I737e87f2f47dd864970be6610b898677d8638dd0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5049373
    > Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    > Reviewed-by: Robbie Gibson <rkgibson@google.com>
    > Commit-Queue: Daniel White <danieltwhite@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1232737}
    
    Change-Id: I0d061d2a20810f42df083200949adb3d14e18aea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5288074
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1259921}

--- components/feature_engagement/public/feature_list.cc
+++ components/feature_engagement/public/feature_list.cc
@@ -146,7 +146,6 @@ const base::Feature* const kAllFeatures[] = {
     &kIPHiOSParcelTrackingFeature,
     &kIPHiOSPullToRefreshFeature,
     &kIPHiOSReplaceSyncPromosWithSignInPromos,
-    &kIPHiOSBlueDotPromoEnhancedSafeBrowsingFeature,
     &kIPHiOSTabGridSwipeLeftForIncognito,
     &kIPHiOSDockingPromoFeature,
     &kIPHiOSDockingPromoRemindMeLaterFeature,
