27a56de00200c6e3e4dc05d5aad5ae7685a0a00b
Louis Romero <lpromero@google.com>
2024-02-13 16:40:33 +0000

    Mark tests as flaky on Official instead of disabled
    The tests only failed flakily on Official bots. Marked them as such,
    so we still get useful coverage on other bots.
    
    Bug: 324867042
    Change-Id: I94b562dc7920a46e947c35c259122ae61003bed3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5291415
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259876}

--- ios/chrome/browser/ui/start_surface/start_surface_egtest.mm
+++ ios/chrome/browser/ui/start_surface/start_surface_egtest.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/test/ios/wait_util.h"
 #import "base/time/time.h"
+#import "build/branding_buildflags.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_constants.h"
 #import "ios/chrome/browser/ui/content_suggestions/tab_resumption/tab_resumption_constants.h"
@@ -83,7 +84,12 @@ void WaitUntilTabResumptionTileVisibleOrTimeout(bool should_show) {
 // Tests that navigating to a page and restarting upon cold start, an NTP page
 // is opened with the Return to Recent Tab tile.
 // TODO(b/324867042): This test fails on Official bots.
-- (void)DISABLED_testColdStartOpenStartSurface {
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#define MAYBE_testColdStartOpenStartSurface FLAKY_testColdStartOpenStartSurface
+#else
+#define MAYBE_testColdStartOpenStartSurface testColdStartOpenStartSurface
+#endif
+- (void)MAYBE_testColdStartOpenStartSurface {
   GREYAssertTrue(self.testServer->Start(), @"Test server failed to start.");
   const GURL destinationUrl = self.testServer->GetURL("/pony.html");
   [ChromeEarlGrey loadURL:destinationUrl];
@@ -102,7 +108,12 @@ void WaitUntilTabResumptionTileVisibleOrTimeout(bool should_show) {
 // Tests that navigating to a page and then backgrounding and foregrounding, an
 // NTP page is opened.
 // TODO(b/324867042): This test fails on Official bots.
-- (void)DISABLED_testWarmStartOpenStartSurface {
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#define MAYBE_testWarmStartOpenStartSurface FLAKY_testWarmStartOpenStartSurface
+#else
+#define MAYBE_testWarmStartOpenStartSurface testWarmStartOpenStartSurface
+#endif
+- (void)MAYBE_testWarmStartOpenStartSurface {
   GREYAssertTrue(self.testServer->Start(), @"Test server failed to start.");
   const GURL destinationUrl = self.testServer->GetURL("/pony.html");
   [ChromeEarlGrey loadURL:destinationUrl];
@@ -123,7 +134,14 @@ void WaitUntilTabResumptionTileVisibleOrTimeout(bool should_show) {
 // is opened with the Return to Recent Tab tile. Then, removing that last tab
 // also removes the tile while that NTP is still being shown.
 // TODO(b/324867042): This test fails on Official bots.
-- (void)DISABLED_testRemoveRecentTabRemovesReturnToRecentTabTile {
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#define MAYBE_testRemoveRecentTabRemovesReturnToRecentTabTile \
+    FLAKY_testRemoveRecentTabRemovesReturnToRecentTabTile
+#else
+#define MAYBE_testRemoveRecentTabRemovesReturnToRecentTabTile \
+    testRemoveRecentTabRemovesReturnToRecentTabTile
+#endif
+- (void)MAYBE_testRemoveRecentTabRemovesReturnToRecentTabTile {
   GREYAssertTrue(self.testServer->Start(), @"Test server failed to start.");
   const GURL destinationUrl = self.testServer->GetURL("/pony.html");
   [ChromeEarlGrey loadURL:destinationUrl];
