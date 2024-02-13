3d9358c6a133fcefc193285e9172c246394ff019
Lucas Radaelli <lucasradaelli@google.com>
2024-02-13 20:03:23 +0000

    [a11y] Implements getAccessibilityFocus in automation ATP.
    Test: AutomationJSApiTest.*
    ax-relnotes: N/A.
    Bug: b:262638176
    Change-Id: I89a8b187d18bf6ceaf8696a631b106bbc66edee2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5282703
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Lucas Radaelli <lucasradaelli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1260000}

--- services/accessibility/features/atp_js_api_test.cc
+++ services/accessibility/features/atp_js_api_test.cc
@@ -1261,8 +1261,9 @@ TEST_F(AutomationJSApiTest, GetDesktop) {
   WaitForJSTestComplete();
 }
 
-// Ensures chrome.automation.getFocus exists and gets the correct node.
-TEST_F(AutomationJSApiTest, GetFocus) {
+// Ensures chrome.automation.getFocus|getAccessibilityFocus exist and gets the
+// correct node.
+TEST_F(AutomationJSApiTest, GetFocuses) {
   std::vector<ui::AXTreeUpdate> updates;
   updates.emplace_back();
   auto& tree_update = updates.back();
@@ -1294,6 +1295,14 @@ TEST_F(AutomationJSApiTest, GetFocus) {
             desktop.firstChild !== desktop.lastChild) {
           remote.testComplete(/*success=*/false);
         }
+
+        // No accessibility focus at the time.
+        chrome.automation.getAccessibilityFocus(focus => {
+          if (focus) {
+            remote.testComplete(/*success=*/false);
+          }
+        });
+
         const button = desktop.firstChild;
         if (button.role !== 'button') {
           remote.testComplete(/*success=*/false);
@@ -1303,12 +1312,21 @@ TEST_F(AutomationJSApiTest, GetFocus) {
             button.indexInParent !== 0 || button.children.length !== 0) {
           remote.testComplete(/*success=*/false);
         }
-        chrome.automation.getFocus(focus => {
+        button.setAccessibilityFocus();
+        chrome.automation.getAccessibilityFocus(focus => {
           if (!focus) {
             remote.testComplete(/*success=*/false);
           }
-         remote.testComplete(/*success=*/focus === button);
+          if (focus !== button) {
+            remote.testComplete(/*success=*/false);
+          }
+          chrome.automation.getFocus(focus => {
+            if (!focus) {
+              remote.testComplete(/*success=*/false);
+            }
+           remote.testComplete(/*success=*/focus === button);
         });
+      });
     });
   )JS");
   WaitForJSTestComplete();
