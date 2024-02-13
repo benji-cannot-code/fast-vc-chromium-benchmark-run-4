86aebe58974e4c4226037614bf492a7785a05539
Ian Kilpatrick <ikilpatrick@chromium.org>
2024-02-13 17:33:14 +0000

    [gc] Remove base::RepeatingCallback(s) from FontFaceList
    Use base::FunctionRef instead.
    
    This is to avoid wrapping GC'd classes in persistents, etc to invoke
    the callbacks correctly.
    
    There should be no behaviour change.
    
    Bug: 41490008
    Change-Id: I19f4e6071e904cc6edb3c600ab33a78817bb289a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5263166
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259891}

--- third_party/blink/renderer/core/css/css_segmented_font_face.h
+++ third_party/blink/renderer/core/css/css_segmented_font_face.h
@@ -27,8 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_CSS_CSS_SEGMENTED_FONT_FACE_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_CSS_SEGMENTED_FONT_FACE_H_
 
+#include "base/containers/adapters.h"
 #include "base/containers/lru_cache.h"
-#include "base/functional/callback.h"
+#include "base/functional/function_ref.h"
 #include "third_party/blink/renderer/platform/fonts/font_cache_key.h"
 #include "third_party/blink/renderer/platform/fonts/font_selection_types.h"
 #include "third_party/blink/renderer/platform/fonts/segmented_font_data.h"
@@ -63,22 +64,15 @@ class FontFaceList : public GarbageCollected<FontFaceList> {
   // Iterate over CSS-connected FontFaces first, and then non-CSS-connected
   // ones.
   // Modifying the collection is not allowed during iteration.
-  bool ForEachUntilTrue(
-      const base::RepeatingCallback<bool(Member<FontFace>)>& callback) const;
-  bool ForEachUntilFalse(
-      const base::RepeatingCallback<bool(Member<FontFace>)>& callback) const;
-  void ForEach(
-      const base::RepeatingCallback<void(Member<FontFace>)>& callback) const;
+  bool ForEachUntilTrue(base::FunctionRef<bool(const Member<FontFace>&)>) const;
+  void ForEach(base::FunctionRef<void(const Member<FontFace>&)>) const;
 
   // Iterate (in reverse order) over non-CSS-connected FontFaces first, and
   // then CSS-connected ones.
   // Modifying the collection is not allowed during iteration.
-  bool ForEachReverseUntilTrue(
-      const base::RepeatingCallback<bool(Member<FontFace>)>& callback) const;
-  bool ForEachReverseUntilFalse(
-      const base::RepeatingCallback<bool(Member<FontFace>)>& callback) const;
-  void ForEachReverse(
-      const base::RepeatingCallback<void(Member<FontFace>)>& callback) const;
+  void ForEachReverseUntilTrue(
+      base::FunctionRef<bool(const Member<FontFace>&)>) const;
+  void ForEachReverse(base::FunctionRef<void(const Member<FontFace>&)>) const;
 
   void Trace(Visitor* visitor) const;
 
