777f9ea53725e39247c5ddb97ea6339238fd6abf
ginnnnnnny <ginnyhuang@chromium.org>
2024-02-13 23:19:43 +0000

    [iOS] Disable tests in OmniboxPopupTestCase and OmniboxTestCase
    Some omnibox tests are disabled in this CL. These tests all involve
    tapping on the fakebox on NTP, and for unknown reasons it could NOT be
    tapped on the iPad device in tests. Disable for now.
    
    Bug: 325112257
    Change-Id: I5ed798ea8f1967ef52e4352e481bcdbd456db7fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5293119
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Yue She <yueshe@google.com>
    Owners-Override: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260132}

--- ios/chrome/browser/ui/omnibox/popup/omnibox_popup_egtest.mm
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_popup_egtest.mm
@@ -326,7 +326,13 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
       assertWithMatcher:grey_nil()];
 }
 
-- (void)testCloseNTPWhenSwitching {
+// TODO(b/325112257): Test fails on device.
+#if !TARGET_IPHONE_SIMULATOR
+#define MAYBE_testCloseNTPWhenSwitching DISABLED_testCloseNTPWhenSwitching
+#else
+#define MAYBE_testCloseNTPWhenSwitching testCloseNTPWhenSwitching
+#endif
+- (void)MAYBE_testCloseNTPWhenSwitching {
   // Open the first page.
   [ChromeEarlGrey loadURL:_URL1];
   [ChromeEarlGrey waitForWebStateContainingText:kPage1];
@@ -350,7 +356,15 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
   [ChromeEarlGrey waitForMainTabCount:1];
 }
 
-- (void)testDontCloseNTPWhenSwitchingWithForwardHistory {
+// TODO(b/325112257): Test fails on device.
+#if !TARGET_IPHONE_SIMULATOR
+#define MAYBE_testDontCloseNTPWhenSwitchingWithForwardHistory \
+  DISABLED_testDontCloseNTPWhenSwitchingWithForwardHistory
+#else
+#define MAYBE_testDontCloseNTPWhenSwitchingWithForwardHistory \
+  testDontCloseNTPWhenSwitchingWithForwardHistory
+#endif
+- (void)MAYBE_testDontCloseNTPWhenSwitchingWithForwardHistory {
   // Open the first page.
   [ChromeEarlGrey loadURL:_URL1];
   [ChromeEarlGrey waitForWebStateContainingText:kPage1];
@@ -417,7 +431,13 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
 
 // Tests that having multiple suggestions with corresponding opened tabs display
 // multiple buttons.
-- (void)testMultiplePageOpened {
+// TODO(b/325112257): Test fails on device.
+#if !TARGET_IPHONE_SIMULATOR
+#define MAYBE_testMultiplePageOpened DISABLED_testMultiplePageOpened
+#else
+#define MAYBE_testMultiplePageOpened testMultiplePageOpened
+#endif
+- (void)MAYBE_testMultiplePageOpened {
   // Open the first page.
   [ChromeEarlGrey loadURL:_URL1];
   [ChromeEarlGrey waitForWebStateContainingText:kPage1];
@@ -453,7 +473,15 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
 // Test that on iPhones, when the popup is scrolled, the keyboard is dismissed
 // but the omnibox is still expanded and the suggestions are visible.
 // Test with flag kEnableSuggestionsScrollingOnIPad disabled.
-- (void)testScrollingDismissesKeyboardOnPhones {
+// TODO(b/325112257): Test fails on device.
+#if !TARGET_IPHONE_SIMULATOR
+#define MAYBE_testScrollingDismissesKeyboardOnPhones \
+  DISABLED_testScrollingDismissesKeyboardOnPhones
+#else
+#define MAYBE_testScrollingDismissesKeyboardOnPhones \
+  testScrollingDismissesKeyboardOnPhones
+#endif
+- (void)MAYBE_testScrollingDismissesKeyboardOnPhones {
   [[AppLaunchManager sharedManager]
       ensureAppLaunchedWithFeaturesEnabled:{}
                                   disabled:{kEnableSuggestionsScrollingOnIPad}
@@ -501,7 +529,14 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
 // Test when the popup is scrolled, the keyboard is dismissed
 // but the omnibox is still expanded and the suggestions are visible.
 // Test with flag kEnableSuggestionsScrollingOnIPad enabled.
-- (void)testScrollingDismissesKeyboard {
+// TODO(b/325112257): Test fails on device.
+#if !TARGET_IPHONE_SIMULATOR
+#define MAYBE_testScrollingDismissesKeyboard \
+  DISABLED_testScrollingDismissesKeyboard
+#else
+#define MAYBE_testScrollingDismissesKeyboard testScrollingDismissesKeyboard
+#endif
+- (void)MAYBE_testScrollingDismissesKeyboard {
   [[AppLaunchManager sharedManager]
       ensureAppLaunchedWithFeaturesEnabled:{kEnableSuggestionsScrollingOnIPad}
                                   disabled:{}
