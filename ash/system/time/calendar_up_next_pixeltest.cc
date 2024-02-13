947456a4d71e766d98562e45b4c5d08cd5e71dc5
Jiaming Cheng <jiamingc@google.com>
2024-02-13 20:19:47 +0000

    Fix a GlanceablesV2CalendarView background color bug
    Both the expand area and the 'up next' bottom drawer use the same
    color token: 'system-on-base-opaque'. The color 'system-on-base' was
    set by mistake in a change.
    
    Bug: b/321789405
    Change-Id: Iedec9d591bcf7906dd8785fe58fdd744889ada55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5270118
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Jiaming Cheng <jiamingc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260011}

--- ash/system/time/calendar_up_next_pixeltest.cc
+++ ash/system/time/calendar_up_next_pixeltest.cc
@@ -144,7 +144,7 @@ TEST_P(CalendarUpNextViewPixelTest,
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "calendar_up_next_single_upcoming_event",
-      /*revision_number=*/6, Widget()));
+      /*revision_number=*/7, Widget()));
 }
 
 TEST_P(CalendarUpNextViewPixelTest,
@@ -168,7 +168,7 @@ TEST_P(CalendarUpNextViewPixelTest,
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "calendar_up_next_multiple_upcoming_events",
-      /*revision_number=*/6, Widget()));
+      /*revision_number=*/7, Widget()));
 }
 
 TEST_P(
@@ -196,7 +196,7 @@ TEST_P(
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "calendar_up_next_multiple_upcoming_events_press_scroll_right_button",
-      /*revision_number=*/5, Widget()));
+      /*revision_number=*/6, Widget()));
 }
 
 TEST_P(CalendarUpNextViewPixelTest, ShouldShowJoinMeetingButton) {
@@ -218,7 +218,7 @@ TEST_P(CalendarUpNextViewPixelTest, ShouldShowJoinMeetingButton) {
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "calendar_up_next_join_button",
-      /*revision_number=*/5, Widget()));
+      /*revision_number=*/6, Widget()));
 }
 
 }  // namespace ash
