5156cc9746f684c12988dc7040049bb3768de28a
Quentin Pubert <qpubert@google.com>
2024-02-13 12:56:05 +0000

    [iOS] Implement -[TabStripMediator closeAllItemsExcept]
    This CL implements -[TabStripMediator closeAllItemsExcept] using
    WebStateList::CloseWebStatesAtIndices(...). It also updates
    `populate(items: [TabSwitcherItem]?, selectedItem: TabSwitcherItem?)`
    in TabStripViewController to ensure the selected item is selected before
    applying the snapshot in case it is already in the data source. This
    makes the animation look better in that case.
    
    Demo before change to TabStripViewController:
    https://drive.google.com/file/d/1H2FkAHHP4cOYc4iNEzLKJk-NuQn-Axbn
    Demo after change to TabStripViewController:
    https://drive.google.com/file/d/1ZzGSuEnWkPqpKxzztmGK9JGuoWqkHt_f
    
    Bug: b/324039258
    Change-Id: Id79076192ba07217abf8614c8d1bd90a7a06f119
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5285218
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1259780}

--- ios/chrome/browser/ui/tab_switcher/tab_utils.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_utils.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/browser/browser_list_factory.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/shared/model/url/url_util.h"
+#import "ios/chrome/browser/shared/model/web_state_list/removing_indexes.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/tabs/model/features.h"
 #import "ios/chrome/browser/tabs/model/tab_title_util.h"
@@ -135,4 +136,21 @@ bool HasDuplicateIdentifiers(NSArray<TabSwitcherItem*>* items) {
     identifiers.insert(item.identifier);
   }
   return identifiers.size() != items.count;
+}
+
+void CloseOtherWebStates(WebStateList* web_state_list,
+                         int index_to_keep,
+                         int close_flags) {
+  const int count = web_state_list->count();
+  const int pinned_count = web_state_list->pinned_tabs_count();
+  std::vector<int> indexes_to_close;
+  indexes_to_close.reserve(count - pinned_count);
+  for (int index = pinned_count; index < count; ++index) {
+    if (index == index_to_keep) {
+      continue;
+    }
+    indexes_to_close.push_back(index);
+  }
+  web_state_list->CloseWebStatesAtIndices(
+      close_flags, RemovingIndexes(std::move(indexes_to_close)));
 }
