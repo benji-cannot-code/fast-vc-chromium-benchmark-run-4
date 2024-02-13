1de7ddc6b0731887de61e726e4552bc68169b363
Monica Basta <msalama@chromium.org>
2024-02-13 14:19:20 +0000

    Revert "Enterprise policy for DevTools AI features"
    This reverts commit 5f39b239e9e0e2c916cfab22c1584bc809a75b90.
    
    Reason for revert: 
    DevToolsConsoleInsightsTest.CanBeDisabledByEnterprisePolicy consistently failing on "chromium/ci/Linux Tests (dbg)(1)"
    
    List of failed builders:
    
    Builder: Linux Tests (dbg)(1)
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20Tests%20%28dbg%29%281%29
    
    Original change's description:
    > Enterprise policy for DevTools AI features
    >
    > Adds an enterprise policy to disable/allow DevTools AI features
    > (only Console Insights for now). Using an int instead of a boolean,
    > because we might add a third option (allow, but don't allow data to
    > be used for model training) later.
    >
    > Bug: b/299423255
    > Change-Id: I9fa054a12bc524ff86967207e04c3254dfd7a756
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5244671
    > Reviewed-by: Anqing Zhao <anqing@chromium.org>
    > Reviewed-by: Cris Guerrero Romero <crisguerrero@chromium.org>
    > Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1259712}
    
    Bug: b/299423255, b/325028020
    Change-Id: I7036d6ee20b40bfbc87fb242867aecca1450cfa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5290835
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Monica Basta <msalama@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259811}

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -2317,8 +2317,6 @@ inline constexpr char kDevToolsSyncedPreferencesSyncEnabled[] =
 inline constexpr char kDevToolsSyncedPreferencesSyncDisabled[] =
     "devtools.synced_preferences_sync_disabled";
 
-inline constexpr char kDevToolsGenAiSettings[] = "devtools.gen_ai_settings";
-
 #if !BUILDFLAG(IS_ANDROID)
 // Tracks the number of times the dice signin promo has been shown in the user
 // menu.
