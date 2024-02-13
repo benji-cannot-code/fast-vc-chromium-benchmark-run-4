3352706e2632491693c50ed18634ac1dd8ab1194
Aida Zolic <aidazolic@google.com>
2024-02-13 09:10:45 +0000

    [skyvault] Add LocalUserFilesAllowed utils
    Adds a utils function to easily check if local user files are allowed.
    
    R=poromov@chromium.org
    
    Fixed: b/322482695
    Change-Id: Ic94712348941c061a46924ebe3e280e3d9723e4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5273151
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259719}

---
+++ chrome/browser/ash/policy/local_user_files/policy_utils.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/policy/local_user_files/policy_utils.h"
+
+#include "base/feature_list.h"
+#include "chrome/browser/browser_process.h"
+#include "chrome/common/chrome_features.h"
+#include "chrome/common/pref_names.h"
+#include "components/prefs/pref_service.h"
+
+namespace policy::local_user_files {
+
+bool LocalUserFilesAllowed() {
+  // If the flag is disabled, ignore the policy value and allow local storage.
+  if (!base::FeatureList::IsEnabled(features::kSkyVault)) {
+    return true;
+  }
+  return g_browser_process->local_state()->GetBoolean(
+      prefs::kLocalUserFilesAllowed);
+}
+
+}  // namespace policy::local_user_files
