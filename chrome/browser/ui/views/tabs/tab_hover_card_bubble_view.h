71a5308e7bd6db0e22b114ce696d2892272468ef
stluong <stluong@chromium.org>
2024-02-14 21:43:59 +0000

    Reland "Fix hover card footer test flakiness"
    This is a reland of commit 121036603cf21ff7954e935dae94fa91719b1bab
    
    FooterHidesOnTabWithoutMemoryUsage was flaking on win-asan builds and
    is re-written in this CL to fix the flakiness.
    
    Original change's description:
    > Fix hover card footer test flakiness
    >
    > The hover card tests were flaky because occasionally the hover card
    > will not appear when a tab is hovered or  memory refreshing earlier than
    > expected which causes a footer to unexpectedly appear. This CL re-writes
    > the test to fix the test flakiness issue.
    >
    > Bug: 1483255
    > Change-Id: I34eb2029ef8e46ca45f130a766f5eef800f1e95d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5262989
    > Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    > Commit-Queue: Steven Luong <stluong@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1258553}
    
    Bug: 1483255
    Change-Id: Ic581f44861970409b37af3dfe74cdc42563662b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5285816
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260714}

--- chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.h
+++ chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.h
@@ -68,6 +68,7 @@ class TabHoverCardBubbleView : public views::BubbleDialogDelegateView {
   std::u16string GetTitleTextForTesting() const;
   std::u16string GetDomainTextForTesting() const;
   views::View* GetThumbnailViewForTesting();
+  FooterView* GetFooterViewForTesting();
 
   // Returns the percentage complete during transition animations when a
   // pre-emptive crossfade to a placeholder should start if a new image is not
@@ -75,14 +76,6 @@ class TabHoverCardBubbleView : public views::BubbleDialogDelegateView {
   static std::optional<double> GetPreviewImageCrossfadeStart();
 
  private:
-  FRIEND_TEST_ALL_PREFIXES(TabHoverCardInteractiveUiTest,
-                           HoverCardDoesNotHaveFooterView);
-  FRIEND_TEST_ALL_PREFIXES(TabHoverCardFadeFooterInteractiveUiTest,
-                           HoverCardFooterUpdatesTabAlertStatus);
-  FRIEND_TEST_ALL_PREFIXES(TabHoverCardFadeFooterInteractiveUiTest,
-                           HoverCardFooterShowsDiscardStatus);
-  FRIEND_TEST_ALL_PREFIXES(TabHoverCardFadeFooterInteractiveUiTest,
-                           HoverCardFooterShowsMemoryUsage);
   FRIEND_TEST_ALL_PREFIXES(TabHoverCardFadeFooterInteractiveUiTest,
                            BackgroundTabHoverCardContentsHaveCorrectDimensions);
   class ThumbnailView;
