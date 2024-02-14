07079bde89fbf791c9799756d84457102bfc7f57
Ben Reich <benreich@chromium.org>
2024-02-14 01:26:42 +0000

    [FilesTS] Convert create_new_folder.js to typescript
    Bug: b/319189127
    Test: browser_tests --gtest_filter=CreateNewFolder/FilesAppBrowserTest*
    Change-Id: I327f2affc94d578714866ba813e9dca9a0a1a027
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5289877
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260200}

--- ui/file_manager/integration_tests/file_manager/create_new_folder.ts
+++ ui/file_manager/integration_tests/file_manager/create_new_folder.ts
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {RootPath, TestEntryInfo} from '../test_util.js';
-import {testcase} from '../testcase.js';
 
 import {remoteCall, setupAndWaitUntilReady} from './background.js';
 import {DirectoryTreePageObject} from './page_objects/directory_tree.js';
@@ -15,16 +14,16 @@ import {BASIC_DRIVE_ENTRY_SET, BASIC_LOCAL_ENTRY_SET} from './test_data.js';
  * When we are not in guest mode, we fill Google Drive with the basic entry set
  * which causes an extra tree-item to be added.
  */
-export const TREEITEM_DRIVE = 'My Drive';
-export const TREEITEM_DOWNLOADS = 'Downloads';
+const TREEITEM_DRIVE = 'My Drive';
+const TREEITEM_DOWNLOADS = 'Downloads';
 
 /**
  * Selects the first item in the file list.
  *
- * @param {string} appId The Files app windowId.
- * @return {Promise<void>} Promise to be fulfilled on success.
+ * @param appId The Files app windowId.
+ * @return Promise to be fulfilled on success.
  */
-async function selectFirstFileListItem(appId) {
+async function selectFirstFileListItem(appId: string): Promise<void> {
   // Ensure no file list items are selected.
   await remoteCall.waitForElementLost(appId, '#file-list [selected]');
 
@@ -37,39 +36,39 @@ async function selectFirstFileListItem(appId) {
   await remoteCall.waitForElement(appId, '.table-row[selected]');
 
   // Retrieve all selected items in the file list.
-  const elements = await remoteCall.callRemoteTestUtil(
-      'queryAllElements', appId, ['#file-list [selected]']);
+  const elements =
+      await remoteCall.queryElements(appId, ['#file-list [selected]']);
 
   // Check: the first list item only should be selected.
   chrome.test.assertEq(1, elements.length);
-  chrome.test.assertEq('listitem-1', elements[0].attributes['id']);
+  chrome.test.assertEq('listitem-1', elements[0]!.attributes['id']);
 }
 
 /*
  * Searches for the file being renamed and gets current value for the renaming
  * field. Throws test assertion error if fails to find one.
  *
- * @param {string} appId The Files app windowId.
- * @return {string} Current value of the name.
+ * @param appId The Files app windowId.
+ * @return Current value of the name.
  */
-// @ts-ignore: error TS7006: Parameter 'appId' implicitly has an 'any' type.
-async function getFileRenamingValue(appId) {
-  const renamingInput = await remoteCall.callRemoteTestUtil(
-      'queryAllElements', appId,
-      ['#file-list .table-row[renaming] input.rename']);
+async function getFileRenamingValue(appId: string): Promise<string> {
+  const renamingInput = await remoteCall.queryElements(
+      appId, ['#file-list .table-row[renaming] input.rename']);
   chrome.test.assertEq(1, renamingInput.length);
-  return renamingInput[0].value;
+  return renamingInput[0]!.value!;
 }
 
 /**
  * Creates a new folder in the file list.
  *
- * @param {string} appId The Files app windowId.
- * @param {!Array<!TestEntryInfo>} initialEntrySet Initial set of entries.
- * @param {string} label Downloads or Drive directory tree item label.
- * @return {Promise<void>} Promise to be fulfilled on success.
+ * @param appId The Files app windowId.
+ * @param initialEntrySet Initial set of entries.
+ * @param label Downloads or Drive directory tree item label.
+ * @return Promise to be fulfilled on success.
  */
-async function createNewFolder(appId, initialEntrySet, label) {
+async function createNewFolder(
+    appId: string, initialEntrySet: TestEntryInfo[],
+    label: string): Promise<void> {
   const textInput = '#file-list .table-row[renaming] input.rename';
 
   // Focus the file-list.
@@ -85,10 +84,6 @@ async function createNewFolder(appId, initialEntrySet, label) {
   // Check: a new folder should be shown in the file list.
   let files = [[newFolderName, '--', 'Folder', '']].concat(
       TestEntryInfo.getExpectedRows(initialEntrySet));
-  // @ts-ignore: error TS2345: Argument of type '{ ignoreLastModifiedTime: true;
-  // }' is not assignable to parameter of type '{ orderCheck: boolean | null |
-  // undefined; ignoreFileSize: boolean | null | undefined;
-  // ignoreLastModifiedTime: boolean | null | undefined; }'.
   await remoteCall.waitForFiles(appId, files, {ignoreLastModifiedTime: true});
 
   // Check: the text input should be shown in the file list.
@@ -98,23 +93,21 @@ async function createNewFolder(appId, initialEntrySet, label) {
 
   // Get all file list rows that have attribute 'renaming'.
   const renamingFileListRows = ['#file-list .table-row[renaming]'];
-  let elements = await remoteCall.callRemoteTestUtil(
-      'queryAllElements', appId, renamingFileListRows);
+  let elements = await remoteCall.queryElements(appId, renamingFileListRows);
 
   // Check: the new folder only should be 'renaming'.
   chrome.test.assertEq(1, elements.length);
-  chrome.test.assertTrue('selected' in elements[0].attributes);
+  chrome.test.assertTrue('selected' in elements[0]!.attributes);
 
   chrome.test.assertEq(newFolderName, await getFileRenamingValue(appId));
 
   // Get all file list rows that have attribute 'selected'.
   const selectedFileListRows = ['#file-list .table-row[selected]'];
-  elements = await remoteCall.callRemoteTestUtil(
-      'queryAllElements', appId, selectedFileListRows);
+  elements = await remoteCall.queryElements(appId, selectedFileListRows);
 
   // Check: the new folder only should be 'selected'.
   chrome.test.assertEq(1, elements.length);
-  chrome.test.assertTrue('renaming' in elements[0].attributes);
+  chrome.test.assertTrue('renaming' in elements[0]!.attributes);
 
   // Type the test folder name.
   newFolderName = 'Test Folder Name';
@@ -132,10 +125,6 @@ async function createNewFolder(appId, initialEntrySet, label) {
   // Check: the test folder should be shown in the file list.
   files = [[newFolderName, '--', 'Folder', '']].concat(
       TestEntryInfo.getExpectedRows(initialEntrySet));
-  // @ts-ignore: error TS2345: Argument of type '{ ignoreLastModifiedTime: true;
-  // }' is not assignable to parameter of type '{ orderCheck: boolean | null |
-  // undefined; ignoreFileSize: boolean | null | undefined;
-  // ignoreLastModifiedTime: boolean | null | undefined; }'.
   await remoteCall.waitForFiles(appId, files, {ignoreLastModifiedTime: true});
 
   // Wait for the new folder to become selected in the file list.
@@ -148,46 +137,36 @@ async function createNewFolder(appId, initialEntrySet, label) {
   await directoryTree.waitForChildItemByLabel(label, newFolderName);
 }
 
-// @ts-ignore: error TS4111: Property 'selectCreateFolderDownloads' comes from
-// an index signature, so it must be accessed with
-// ['selectCreateFolderDownloads'].
-testcase.selectCreateFolderDownloads = async () => {
+export async function selectCreateFolderDownloads() {
   const appId = await setupAndWaitUntilReady(
       RootPath.DOWNLOADS, BASIC_LOCAL_ENTRY_SET, []);
   const directoryTree = await DirectoryTreePageObject.create(appId, remoteCall);
   await directoryTree.recursiveExpand('/My files/Downloads');
   await selectFirstFileListItem(appId);
   await createNewFolder(appId, BASIC_LOCAL_ENTRY_SET, TREEITEM_DOWNLOADS);
-};
+}
 
-// @ts-ignore: error TS4111: Property 'createFolderDownloads' comes from an
-// index signature, so it must be accessed with ['createFolderDownloads'].
-testcase.createFolderDownloads = async () => {
+export async function createFolderDownloads() {
   const appId = await setupAndWaitUntilReady(
       RootPath.DOWNLOADS, BASIC_LOCAL_ENTRY_SET, []);
   const directoryTree = await DirectoryTreePageObject.create(appId, remoteCall);
   await directoryTree.recursiveExpand('/My files/Downloads');
   await createNewFolder(appId, BASIC_LOCAL_ENTRY_SET, TREEITEM_DOWNLOADS);
-};
+}
 
-// @ts-ignore: error TS4111: Property 'createFolderNestedDownloads' comes from
-// an index signature, so it must be accessed with
-// ['createFolderNestedDownloads'].
-testcase.createFolderNestedDownloads = async () => {
+export async function createFolderNestedDownloads() {
   const appId = await setupAndWaitUntilReady(
       RootPath.DOWNLOADS, BASIC_LOCAL_ENTRY_SET, []);
   const directoryTree = await DirectoryTreePageObject.create(appId, remoteCall);
   await directoryTree.recursiveExpand('/My files/Downloads');
   await directoryTree.navigateToPath('/My files/Downloads/photos');
   await createNewFolder(appId, [], 'photos');
-};
+}
 
-// @ts-ignore: error TS4111: Property 'createFolderDrive' comes from an index
-// signature, so it must be accessed with ['createFolderDrive'].
-testcase.createFolderDrive = async () => {
+export async function createFolderDrive() {
   const appId =
       await setupAndWaitUntilReady(RootPath.DRIVE, [], BASIC_DRIVE_ENTRY_SET);
   const directoryTree = await DirectoryTreePageObject.create(appId, remoteCall);
   await directoryTree.recursiveExpand('/Google Drive/My Drive');
   await createNewFolder(appId, BASIC_DRIVE_ENTRY_SET, TREEITEM_DRIVE);
-};
+}
