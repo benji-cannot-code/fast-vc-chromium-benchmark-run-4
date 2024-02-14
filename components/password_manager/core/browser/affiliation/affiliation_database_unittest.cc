e9aa60639633123af5a8dca30df49bacce18938e
Evan Stade <estade@chromium.org>
2024-02-14 18:16:18 +0000

    SQL: remove dependencies on third_party/sqlite
    Only //sql should depend on it.
    
    This CL was uploaded by git cl split.
    
    Bug: 40219064
    Change-Id: Ic2b9511bc75ef02727606b46776ebd5d51931bd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5296995
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Auto-Submit: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260561}

--- components/password_manager/core/browser/affiliation/affiliation_database_unittest.cc
+++ components/password_manager/core/browser/affiliation/affiliation_database_unittest.cc
@@ -15,11 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "components/password_manager/core/browser/affiliation/affiliation_utils.h"
+#include "sql/sqlite_result_code_values.h"
 #include "sql/test/scoped_error_expecter.h"
 #include "sql/test/test_helpers.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/sqlite/sqlite3.h"
 #include "url/gurl.h"
 
 namespace password_manager {
@@ -276,7 +276,8 @@ TEST_F(AffiliationDatabaseTest, CorruptDBGetsPoisoned) {
   EXPECT_EQ(0u, affiliations.size());
 
   histogram_tester.ExpectUniqueSample(
-      "PasswordManager.AffiliationDatabase.Error", SQLITE_INTERRUPT, 1);
+      "PasswordManager.AffiliationDatabase.Error",
+      sql::SqliteResultCode::kInterrupt, 1);
   histogram_tester.ExpectTotalCount(
       "PasswordManager.AffiliationDatabase.StoreResult", 1);
 }
