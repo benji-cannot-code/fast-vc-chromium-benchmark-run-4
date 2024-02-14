3fda6d5eb0040e88ecc196fdd62ddf8ddfa7989f
Ahmed Moussa <ahmedmoussa@google.com>
2024-02-14 21:24:33 +0000

    [live-preview] Fix GMB issue with Windows
    When GpuRasterization is not available or if there is no support for
    D3dSharedImage, rendering GMB frames fails.
    
    The video preview is implemented by painting the video frame into a
    tile via PaintCanvasVideoRenderer in the browser compositor. When the
    incoming video resource is a GpuMemoryBufferHandle it goes to
    GpuMemoryBufferHandleHolder which wraps it in a shared image. That
    should work fine if the tile is GPU rasterized, since the video frame
    texture to tile texture copy happens on the GPU, but it won't work in
    case of software raster since rasterization happens in the browser
    process and GPU texture data is unavailable. This CL provides a fix
    for that by requesting pre-mapped frames and wraps the
    SharedMemoryRegion.
    
    Bug: b:319273222
    Change-Id: I165199ce8f41f1838785c8bd9b132010de106c2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5259809
    Reviewed-by: Ilya Nikolaevskiy <ilnik@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Ahmed Moussa <ahmedmoussa@google.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260702}

--- components/capture_mode/camera_video_frame_handler.h
+++ components/capture_mode/camera_video_frame_handler.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/flat_map.h"
+#include "base/functional/callback_forward.h"
+#include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
@@ -44,9 +46,12 @@ class BufferHandleHolder {
   // Creates and returns a concrete implementation of this interface that
   // matches the buffer type of the given `buffer_handle`.
   // We only support the `kGpuMemoryBuffer` and `kSharedMemory` buffer types.
+  // `require_mapped_frame_callback` is a callback that is passed to the buffer
+  // handle only on Windows. It is used to request pre-mapped frames.
   static std::unique_ptr<BufferHandleHolder> Create(
       media::mojom::VideoBufferHandlePtr buffer_handle,
-      ui::ContextFactory* context_factory);
+      ui::ContextFactory* context_factory,
+      base::RepeatingClosure require_mapped_frame_callback);
 
   // Extracts and returns the ready video frame in the given `buffer`.
   virtual scoped_refptr<media::VideoFrame> OnFrameReadyInBuffer(
@@ -150,6 +155,10 @@ class CAPTURE_MODE_EXPORT CameraVideoFrameHandler
   // `VideoSource` gets disconnected.
   void OnFatalErrorOrDisconnection();
 
+  // Sends a ProcessFeedback() call to `camera_video_stream_subsciption_remote_`
+  // requesting mapped frames. Only used on Windows.
+  void RequireMappedFrame();
+
   raw_ptr<Delegate> delegate_ = nullptr;
 
   const raw_ptr<ui::ContextFactory> context_factory_;
@@ -185,6 +194,8 @@ class CAPTURE_MODE_EXPORT CameraVideoFrameHandler
   // called.
   base::flat_map</*buffer_id=*/int, std::unique_ptr<BufferHandleHolder>>
       buffer_map_;
+
+  SEQUENCE_CHECKER(sequence_checker_);
 
   base::WeakPtrFactory<CameraVideoFrameHandler> weak_ptr_factory_{this};
 };
