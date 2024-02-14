4fc0a3ff98b61d1c7bb3c3a6200e8986c467f3f4
Shengsong Tan <sstan@chromium.org>
2024-02-14 01:08:51 +0000

    arc: Change idle manager enable delay
    This commit change the default delay of arc idle manager enable
    from 0 second to 20 seconds.
    
    BUG=None
    TEST=testing/xvfb.py ./out/Default/unit_tests --gtest_filter="ArcIdleManagerTest.*"
    
    Change-Id: I8d4194b33e27cc76693a87d6c682045e0ab06c9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5273758
    Commit-Queue: Shengsong Tan <sstan@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260190}

--- chrome/browser/ash/arc/idle_manager/arc_idle_manager_unittest.cc
+++ chrome/browser/ash/arc/idle_manager/arc_idle_manager_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "ash/components/arc/arc_features.h"
 #include "ash/components/arc/mojom/power.mojom.h"
 #include "ash/components/arc/power/arc_power_bridge.h"
 #include "ash/components/arc/session/arc_bridge_service.h"
@@ -204,6 +205,9 @@ TEST_F(ArcIdleManagerTest, TestThrottleInstance) {
   background_service_observer()->SetActive(false);
   arc_window_observer()->SetActive(false);
 
+  task_environment()->FastForwardBy(
+      base::Milliseconds(kEnableArcIdleManagerDelayMs.Get()));
+
   EXPECT_EQ(0U, interactive_enabled_counter());
   EXPECT_EQ(2U, interactive_disabled_counter());
 
@@ -214,6 +218,8 @@ TEST_F(ArcIdleManagerTest, TestThrottleInstance) {
 
   // Reset.
   on_battery_observer()->SetActive(false);
+  task_environment()->FastForwardBy(
+      base::Milliseconds(kEnableArcIdleManagerDelayMs.Get()));
   EXPECT_EQ(1U, interactive_enabled_counter());
   EXPECT_EQ(3U, interactive_disabled_counter());
 
@@ -224,6 +230,8 @@ TEST_F(ArcIdleManagerTest, TestThrottleInstance) {
 
   // Reset.
   display_power_observer()->SetActive(false);
+  task_environment()->FastForwardBy(
+      base::Milliseconds(kEnableArcIdleManagerDelayMs.Get()));
   EXPECT_EQ(2U, interactive_enabled_counter());
   EXPECT_EQ(4U, interactive_disabled_counter());
 
@@ -234,6 +242,8 @@ TEST_F(ArcIdleManagerTest, TestThrottleInstance) {
 
   // Reset.
   cpu_throttle_observer()->SetActive(false);
+  task_environment()->FastForwardBy(
+      base::Milliseconds(kEnableArcIdleManagerDelayMs.Get()));
   EXPECT_EQ(3U, interactive_enabled_counter());
   EXPECT_EQ(5U, interactive_disabled_counter());
 
@@ -244,6 +254,8 @@ TEST_F(ArcIdleManagerTest, TestThrottleInstance) {
 
   // Reset.
   background_service_observer()->SetActive(false);
+  task_environment()->FastForwardBy(
+      base::Milliseconds(kEnableArcIdleManagerDelayMs.Get()));
   EXPECT_EQ(4U, interactive_enabled_counter());
   EXPECT_EQ(6U, interactive_disabled_counter());
 
@@ -259,6 +271,8 @@ TEST_F(ArcIdleManagerTest, TestThrottleInstance) {
 
   // Reset.
   arc_window_observer()->SetActive(false);
+  task_environment()->FastForwardBy(
+      base::Milliseconds(kEnableArcIdleManagerDelayMs.Get()));
   EXPECT_EQ(6U, interactive_enabled_counter());
   EXPECT_EQ(7U, interactive_disabled_counter());
 
@@ -281,6 +295,9 @@ TEST_F(ArcIdleManagerTest, TestScreenOffTimerMetrics) {
   // Count time from here.
   base::ScopedMockElapsedTimersForTest mock_elapsed_timers;
   base::HistogramTester histogram_tester;
+
+  task_environment()->FastForwardBy(
+      base::Milliseconds(kEnableArcIdleManagerDelayMs.Get()));
 
   histogram_tester.ExpectUniqueTimeSample(
       "Arc.IdleManager.ScreenOffTime",
