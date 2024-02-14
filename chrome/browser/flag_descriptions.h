472f78fe7b7721f8989c60e98294d4c64698531d
Timothy Loh <timloh@chromium.org>
2024-02-14 06:21:40 +0000

    Add flag for updated file notifications
    Bug: b/319023176
    Change-Id: Icfa835851ab52dbeb72721f9fe6179e82ffc8367
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5293855
    Reviewed-by: Patti Lor <patricialor@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Auto-Submit: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260302}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3757,6 +3757,9 @@ extern const char kExposeOutOfProcessVideoDecodingToLacrosDescription[];
 extern const char kFederatedServiceName[];
 extern const char kFederatedServiceDescription[];
 
+extern const char kFileNotificationRevampName[];
+extern const char kFileNotificationRevampDescription[];
+
 extern const char kFileTransferEnterpriseConnectorName[];
 extern const char kFileTransferEnterpriseConnectorDescription[];
 
