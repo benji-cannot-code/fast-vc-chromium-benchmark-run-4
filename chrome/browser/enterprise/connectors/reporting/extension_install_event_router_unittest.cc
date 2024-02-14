315e87f3cc4daf590696524ddafd266ffd057103
Haihan Chen <haihan@google.com>
2024-02-14 20:45:17 +0000

    [ExtensionEvents v2] Add new extension source field to extension event reporting
    Replacing from_webstore to extension_source as we're changing the field from a bool to an enum in the backend. Proto changes: cl/605017844
    
    Screenshot with functionality: https://screenshot.googleplex.com/AKAtjd5MSFiGuTD
    
    Bug: b/315307820
    Change-Id: Ifea7f14982335a08007cdb03f82e5584a999fb28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5292968
    Reviewed-by: Shanthanu Bhardwaj <xanth@google.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Haihan Chen <haihan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1260682}

--- chrome/browser/enterprise/connectors/reporting/extension_install_event_router_unittest.cc
+++ chrome/browser/enterprise/connectors/reporting/extension_install_event_router_unittest.cc
@@ -43,7 +43,7 @@ constexpr char kFakeInstallAction[] = "INSTALL";
 constexpr char kFakeUpdateAction[] = "UPDATE";
 constexpr char kFakeUninstallAction[] = "UNINSTALL";
 constexpr char kFakeExtensionVersion[] = "1";
-constexpr bool kFakeFromWebstore = false;
+constexpr char kFakeExtensionSource[] = "EXTERNAL";
 
 }  // namespace
 
@@ -141,7 +141,7 @@ TEST_F(ExtensionInstallEventRouterTest, CheckInstallEventReported) {
   expectedEvent.Set("description", kFakeExtensionDescription);
   expectedEvent.Set("extension_action_type", kFakeInstallAction);
   expectedEvent.Set("extension_version", kFakeExtensionVersion);
-  expectedEvent.Set("from_webstore", kFakeFromWebstore);
+  expectedEvent.Set("extension_source", kFakeExtensionSource);
 
   EXPECT_CALL(
       *mockRealtimeReportingClient_,
@@ -160,7 +160,7 @@ TEST_F(ExtensionInstallEventRouterTest, CheckUpdateEventReported) {
   expectedEvent.Set("description", kFakeExtensionDescription);
   expectedEvent.Set("extension_action_type", kFakeUpdateAction);
   expectedEvent.Set("extension_version", kFakeExtensionVersion);
-  expectedEvent.Set("from_webstore", kFakeFromWebstore);
+  expectedEvent.Set("extension_source", kFakeExtensionSource);
 
   EXPECT_CALL(
       *mockRealtimeReportingClient_,
@@ -179,7 +179,7 @@ TEST_F(ExtensionInstallEventRouterTest, CheckUninstallEventReported) {
   expectedEvent.Set("description", kFakeExtensionDescription);
   expectedEvent.Set("extension_action_type", kFakeUninstallAction);
   expectedEvent.Set("extension_version", kFakeExtensionVersion);
-  expectedEvent.Set("from_webstore", kFakeFromWebstore);
+  expectedEvent.Set("extension_source", kFakeExtensionSource);
 
   EXPECT_CALL(
       *mockRealtimeReportingClient_,
