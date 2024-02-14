b0b18ae9011d36a24bbb7b26aa11a83339f4212d
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-02-14 19:39:47 +0000

    Fix missing <string> dependency.
    It was previously provided by absl's optional, but I am going to remove
    it. This is blocking the rename in //base
    https://chromium-review.googlesource.com/c/chromium/src/+/5296147?tab=checks
    
    Bug: chromium:1500249
    Change-Id: I89df83c35a6773dfa4125ac24fa0f25ce1d585ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5296563
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260643}

--- base/win/wmi.h
+++ base/win/wmi.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wbemidl.h>
 #include <wrl/client.h>
 
+#include <string>
 #include <string_view>
 
 #include "base/base_export.h"
