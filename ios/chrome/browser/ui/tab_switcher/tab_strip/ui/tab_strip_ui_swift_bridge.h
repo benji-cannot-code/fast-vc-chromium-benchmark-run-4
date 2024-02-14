98856c7f42f6b42b24e2793093376e1675a3937f
Ewann Pelle <ewannpv@chromium.org>
2024-02-14 14:01:12 +0000

    [iOS][TS] Add accessibility label
    Adds an accessibility label to the tab strip new tab button.
    
    Bug: 41481434
    Change-Id: I7c8d802da9e281f027203ccd56f11f0b7f370f6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5295579
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260428}

--- ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_ui_swift_bridge.h
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_ui_swift_bridge.h
@@ -14,5 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/tab_switcher/tab_switcher_item.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/elements/gradient_view.h"
+#import "ios/chrome/grit/ios_strings.h"
+#import "ui/base/l10n/l10n_util_mac_bridge.h"
 
 #endif  // IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TAB_STRIP_UI_TAB_STRIP_UI_SWIFT_BRIDGE_H_
