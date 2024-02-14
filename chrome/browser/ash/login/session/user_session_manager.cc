4bea853869aa31ebdf072af7c74b55ed43e62d7c
Callistus Tan <callistus@google.com>
2024-02-14 07:32:21 +0000

    Help app: Provide launch source information to untrusted frame.
    For now, we are only interested in knowing if it is the first run.
    Also added a browser test for the first run experience.
    
    Bug: b/322267790
    Change-Id: Ia23a1446806f7ecf440fce432da5a9f47e4259df
    Cq-Include-Trybots: luci.chromium.try:linux-chromeos-rel;luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5294375
    Reviewed-by: Zufeng Wang <zufeng@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Callistus Tan <callistus@google.com>
    Cr-Commit-Position: refs/heads/main@{#1260316}

--- chrome/browser/ash/login/session/user_session_manager.cc
+++ chrome/browser/ash/login/session/user_session_manager.cc
@@ -1247,7 +1247,7 @@ void UserSessionManager::ChildAccountStatusReceivedCallback(Profile* profile) {
 void UserSessionManager::StopChildStatusObserving(Profile* profile) {
   if (waiting_for_child_account_status_ &&
       !SessionStartupPref::TypeIsManaged(profile->GetPrefs())) {
-    MaybeLaunchHelpApp(profile);
+    MaybeLaunchHelpAppForFirstRun(profile);
   }
   waiting_for_child_account_status_ = false;
 }
@@ -1923,7 +1923,7 @@ void UserSessionManager::InitializeBrowser(Profile* profile) {
   }
 }
 
-void UserSessionManager::MaybeLaunchHelpApp(Profile* profile) const {
+void UserSessionManager::MaybeLaunchHelpAppForFirstRun(Profile* profile) const {
   if (first_run::ShouldLaunchHelpApp(profile)) {
     // Don't open default Chrome window if we're going to launch the first-run
     // app. Because we don't want the first-run app to be hidden in the
@@ -1952,7 +1952,7 @@ bool UserSessionManager::MaybeStartNewUserOnboarding(Profile* profile) {
   // start URLs via policy.
   if (!SessionStartupPref::TypeIsManaged(prefs)) {
     if (child_service->IsChildAccountStatusKnown())
-      MaybeLaunchHelpApp(profile);
+      MaybeLaunchHelpAppForFirstRun(profile);
     else
       waiting_for_child_account_status_ = true;
   }
