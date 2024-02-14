a063e533e738582f017c20153b7c1fd2e6bab944
Rodney Ding <rodneyding@google.com>
2024-02-14 14:39:00 +0000

    [SSM] Tag samples for Interaction to Next Paint when event is queued
    - Event queueing time on compositor is plumbed through to
      ResponsivenessMetrics
    - When an interaction is ready to be tagged we use the
      queueing time as the start of samples
    - A new namehash is used for the tagging
      Blink.Responsiveness.UserInteraction.MaxEventDurationFromQueued
    
    Design doc: go/ssm-inp-tagging-queued
    
    Change-Id: Id7d29beeb4fdc200c495f1acf3d70fd042263144
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5259857
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Deep Roy <dproy@chromium.org>
    Commit-Queue: Rodney Ding <rodneyding@google.com>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260441}

--- third_party/blink/renderer/core/timing/window_performance.cc
+++ third_party/blink/renderer/core/timing/window_performance.cc
@@ -167,6 +167,8 @@ AtomicString SameOriginAttribution(Frame* observer_frame,
   return SameOriginKeyword();
 }
 
+// Eligible event types should be kept in sync with IsWebInteractionEvent
+// (widget_event_handler.cc)
 bool IsEventTypeForInteractionId(const AtomicString& type) {
   return type == event_type_names::kPointercancel ||
          type == event_type_names::kContextmenu ||
@@ -432,6 +434,8 @@ void WindowPerformance::RegisterEventTiming(const Event& event,
       event_target ? event_target->ToNode() : nullptr,
       DomWindow());  // TODO(haoliuk): Add WPT for Event Timing.
                      // See crbug.com/1320878.
+  entry->SetUnsafeQueuedTimestamp(
+      responsiveness_metrics_->CurrentInteractionEventQueuedTimestamp());
   std::optional<PointerId> pointer_id;
   if (pointer_event) {
     pointer_id = pointer_event->pointerId();
@@ -509,6 +513,7 @@ void WindowPerformance::ReportEvent(InteractiveDetector* interactive_detector,
                                     base::TimeTicks presentation_timestamp) {
   PerformanceEventTiming* entry = event_data->GetEventTiming();
   base::TimeTicks event_timestamp = event_data->GetEventTimestamp();
+  const base::TimeTicks event_queued_timestamp = entry->unsafeQueuedTimestamp();
   std::optional<int> key_code = event_data->GetKeyCode();
   std::optional<PointerId> pointer_id = event_data->GetPointerId();
 
@@ -550,7 +555,7 @@ void WindowPerformance::ReportEvent(InteractiveDetector* interactive_detector,
 
   // Event Timing
   ResponsivenessMetrics::EventTimestamps event_timestamps = {
-      event_timestamp, entry_end_timetick};
+      event_timestamp, entry_end_timetick, event_queued_timestamp};
   if (SetInteractionIdAndRecordLatency(entry, key_code, pointer_id,
                                        event_timestamps)) {
     NotifyAndAddEventTimingBuffer(entry);
--- third_party/blink/renderer/platform/widget/input/main_thread_event_queue.cc
+++ third_party/blink/renderer/platform/widget/input/main_thread_event_queue.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/time/time.h"
 #include "cc/base/features.h"
 #include "cc/metrics/event_metrics.h"
 #include "third_party/blink/public/common/features.h"
@@ -216,6 +217,10 @@ class QueuedWebInputEvent : public MainThreadEventQueueTask {
   const WebInputEvent& Event() const { return event_->Event(); }
 
   WebCoalescedInputEvent* mutable_coalesced_event() { return event_.get(); }
+
+  void SetQueuedTimeStamp(base::TimeTicks queued_time) {
+    event_->EventPointer()->SetQueuedTimeStamp(queued_time);
+  }
 
  private:
   FilterResult HandleTouchScrollStartQueued() {
@@ -645,10 +650,10 @@ void MainThreadEventQueue::QueueEvent(
   WebInputEvent::Type input_event_type = WebInputEvent::Type::kUndefined;
   WebInputEventAttribution attribution;
   if (is_input_event) {
-    auto* queued_input_event =
-        static_cast<const QueuedWebInputEvent*>(event.get());
+    auto* queued_input_event = static_cast<QueuedWebInputEvent*>(event.get());
     input_event_type = queued_input_event->Event().GetType();
     attribution = queued_input_event->attribution();
+    queued_input_event->SetQueuedTimeStamp(base::TimeTicks::Now());
   }
 
   {
