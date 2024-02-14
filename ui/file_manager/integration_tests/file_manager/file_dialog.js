f93a6bc3c0ef8f47ad10bf16202ecf8256a7acf4
Luciano Pacheco <lucmult@chromium.org>
2024-02-14 22:41:33 +0000

    Files TS: Convert the test_util.js to TS
    Bug: b:319189127
    Change-Id: Ic226c0f8d6673eaaa2eb40154c13e614085d4533
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5294614
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260753}

--- ui/file_manager/integration_tests/file_manager/file_dialog.js
+++ ui/file_manager/integration_tests/file_manager/file_dialog.js
@@ -1143,7 +1143,7 @@ testcase.saveFileDialogExtensionNotAddedWhenProvided = async () => {
 // ['openFileDialogFileListShowContextMenu'].
 testcase.openFileDialogFileListShowContextMenu = async () => {
   // Make sure the file picker will open to Downloads.
-  sendBrowserTestCommand({name: 'setLastDownloadDir'}, () => {});
+  sendBrowserTestCommand({name: 'setLastDownloadDir'});
 
   // Add entries to Downloads.
   await addEntries(['local'], BASIC_LOCAL_ENTRY_SET);
@@ -1234,7 +1234,7 @@ testcase.openFileDialogSelectAllDisabled = async () => {
 // ['openMultiFileDialogSelectAllEnabled'].
 testcase.openMultiFileDialogSelectAllEnabled = async () => {
   // Make sure the file picker will open to Downloads.
-  sendBrowserTestCommand({name: 'setLastDownloadDir'}, () => {});
+  sendBrowserTestCommand({name: 'setLastDownloadDir'});
 
   // Open file picker dialog with support for selecting multiple files.
   await openEntryChoosingWindow({type: 'openFile', acceptsMultiple: true});
