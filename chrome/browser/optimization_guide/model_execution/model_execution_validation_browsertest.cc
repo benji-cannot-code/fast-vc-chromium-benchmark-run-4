0bd15ac45eefe22fd1f33faaf4b0f70a661f97d7
rajendrant <rajendrant@chromium.org>
2024-02-14 19:10:58 +0000

    Deflake model validation browser test
    Add wait based on runloop before waiting for the UMA
    
    Change-Id: I6fffc2469834923e7b2dc3b9d8548d7b73f91dc3
    Bug: b/41493340
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5294954
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260618}

--- chrome/browser/optimization_guide/model_execution/model_execution_validation_browsertest.cc
+++ chrome/browser/optimization_guide/model_execution/model_execution_validation_browsertest.cc
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <memory>
+
+#include "base/run_loop.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "build/chromeos_buildflags.h"
@@ -112,6 +115,7 @@ class ModelExecutionValidationBrowserTestBase : public InProcessBrowserTest {
 
     if (should_server_fail_model_execution_) {
       response->set_code(net::HTTP_NOT_FOUND);
+      model_execution_request_run_loop_->Quit();
       return std::move(response);
     }
 
@@ -127,6 +131,7 @@ class ModelExecutionValidationBrowserTestBase : public InProcessBrowserTest {
     execute_response.SerializeToString(&serialized_response);
     response->set_code(net::HTTP_OK);
     response->set_content(serialized_response);
+    model_execution_request_run_loop_->Quit();
     return std::move(response);
   }
 
@@ -145,6 +150,9 @@ class ModelExecutionValidationBrowserTestBase : public InProcessBrowserTest {
   std::unique_ptr<IdentityTestEnvironmentProfileAdaptor>
       identity_test_env_adaptor_;
   base::CallbackListSubscription create_services_subscription_;
+
+  // RunLoop to wait for model execution request to be received.
+  std::unique_ptr<base::RunLoop> model_execution_request_run_loop_;
 };
 
 class ModelExecutionValidationBrowserTest
@@ -165,7 +173,10 @@ class ModelExecutionValidationBrowserTest
 #endif
 IN_PROC_BROWSER_TEST_F(ModelExecutionValidationBrowserTest,
                        MAYBE_ModelExecutionSuccess) {
+  model_execution_request_run_loop_ = std::make_unique<base::RunLoop>();
   EnableSignin();
+
+  model_execution_request_run_loop_->Run();
   RetryForHistogramUntilCountReached(
       &histogram_tester_, "OptimizationGuide.ModelExecution.Result.Test", 1);
 
@@ -185,8 +196,11 @@ IN_PROC_BROWSER_TEST_F(ModelExecutionValidationBrowserTest,
 #endif
 IN_PROC_BROWSER_TEST_F(ModelExecutionValidationBrowserTest,
                        MAYBE_ModelExecutionFailsServerFailure) {
+  model_execution_request_run_loop_ = std::make_unique<base::RunLoop>();
   EnableServerModelExecutionFailure();
   EnableSignin();
+
+  model_execution_request_run_loop_->Run();
   RetryForHistogramUntilCountReached(
       &histogram_tester_, "OptimizationGuide.ModelExecution.Result.Test", 1);
 
