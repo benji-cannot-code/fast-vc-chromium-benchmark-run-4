bc98bc0a216ff14d0ba1edc915bfe28b3cd646e7
Ameur Hosni <ameurhosni@google.com>
2024-02-14 17:13:51 +0000

    [IOS][Omnibox] Defocus the omnibox when performing accessibility escape
    Bug: 325215431
    Change-Id: I271004b6287516198a50f45df44bed0cd7159930
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5293422
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1260529}

--- ios/chrome/browser/ui/browser_view/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view/browser_view_controller.mm
@@ -1681,6 +1681,7 @@ enum HeaderBehaviour {
 
   [self.popupMenuCommandsHandler dismissPopupMenuAnimated:NO];
   [self.helpHandler hideAllHelpBubbles];
+  [self.omniboxCommandsHandler cancelOmniboxEdit];
 }
 
 // Returns the appropriate frame for the NTP.
