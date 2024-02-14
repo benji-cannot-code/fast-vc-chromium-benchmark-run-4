342409907fe494b9ccbcbdcacd972da1d584cddd
Ewann Pelle <ewannpv@chromium.org>
2024-02-14 10:15:31 +0000

    [iOS][TS] Fix item insertion animation on iOS 16
    Demo:
    https://drive.google.com/file/d/1QqidmoYM9tjRvns0AFl75lEiPKAt258J/
    
    Bug: 41481434
    Change-Id: Ia96d274fdbb099c572556b052366b4db32a53014
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5291294
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260362}

--- ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_view_controller.swift
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_view_controller.swift
@@ -47,6 +47,11 @@ class TabStripViewController: UIViewController, TabStripCellDelegate,
   // Handles drag and drop interactions.
   public weak var dragDropHandler: TabCollectionDragDropHandler?
 
+  /// Targeted scroll offset, used on iOS 16 only.
+  /// On iOS 16, the scroll animation after opening a new tab is delayed.
+  /// This variable ensures that the most recent scroll event is processed.
+  private var targetedScrollOffsetiOS16: CGFloat = 0
+
   init() {
     layout = TabStripLayout()
     collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
@@ -147,6 +152,10 @@ class TabStripViewController: UIViewController, TabStripCellDelegate,
     snapshot.appendSections([.tabs])
     snapshot.appendItems(items, toSection: .tabs)
 
+    if #unavailable(iOS 17.0) {
+      layout.cellAnimatediOS16 = true
+    }
+
     if let selectedItem = selectedItem,
       let diffableDataSource = diffableDataSource,
       diffableDataSource.indexPath(for: selectedItem) != nil
@@ -179,12 +188,15 @@ class TabStripViewController: UIViewController, TabStripCellDelegate,
           // occur simultaneously, the resulting animation lacks of
           // smoothness.
           weak var weakSelf = self
+          targetedScrollOffsetiOS16 = offset
           DispatchQueue.main.asyncAfter(
             deadline: .now() + TabStripConstants.CollectionView.scrollDelayAfterInsert
           ) {
             weakSelf?.scrollToContentOffset(offset)
           }
         }
+      } else {
+        layout.cellAnimatediOS16 = false
       }
     }
   }
@@ -205,7 +217,7 @@ class TabStripViewController: UIViewController, TabStripCellDelegate,
     collectionView.selectItem(at: indexPath, animated: false, scrollPosition: [])
 
     /// Invalidate the layout to correctly recalculate the frame of the `selected` cell.
-    collectionView.collectionViewLayout.invalidateLayout()
+    layout.invalidateLayout()
   }
 
   func reloadItem(_ item: TabSwitcherItem?) {
@@ -244,6 +256,15 @@ class TabStripViewController: UIViewController, TabStripCellDelegate,
     }
     let item = diffableDataSource.itemIdentifier(for: indexPath)
     mutator?.close(item)
+  }
+
+  // MARK: - UIScrollViewDelegate
+
+  func scrollViewDidEndDragging(
+    _ scrollView: UIScrollView,
+    willDecelerate decelerate: Bool
+  ) {
+    layout.cellAnimatediOS16 = false
   }
 
   // MARK: - Private
@@ -356,6 +377,9 @@ class TabStripViewController: UIViewController, TabStripCellDelegate,
 
   /// Scrolls the collection view to the given horizontal `offset`.
   func scrollToContentOffset(_ offset: CGFloat) {
+    if #unavailable(iOS 17.0) {
+      if offset != targetedScrollOffsetiOS16 { return }
+    }
     self.collectionView.setContentOffset(
       CGPoint(x: offset, y: 0),
       animated: true)
