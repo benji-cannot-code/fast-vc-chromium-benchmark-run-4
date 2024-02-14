e37b3d5e339525a4ea2541ecbc951f8d2e1701b5
rajendrant <rajendrant@chromium.org>
2024-02-14 18:13:41 +0000

    Remove unneeded vars in prediction manager
    Change-Id: I15a65a69963f5e048dcaf3e9fc2399a3cc93e07f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5297454
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260560}

--- components/optimization_guide/core/prediction_manager.h
+++ components/optimization_guide/core/prediction_manager.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "base/sequence_checker.h"
-#include "base/time/clock.h"
 #include "base/timer/timer.h"
 #include "base/types/optional_ref.h"
 #include "components/optimization_guide/core/model_enums.h"
@@ -118,9 +117,6 @@ class PredictionManager : public PredictionModelDownloadObserver {
   // Return the optimization targets that are registered.
   base::flat_set<proto::OptimizationTarget> GetRegisteredOptimizationTargets()
       const;
-
-  // Override |clock_| for testing.
-  void SetClockForTesting(const base::Clock* clock);
 
   // Override the model file returned to observers for |optimization_target|.
   // Use |TestModelInfoBuilder| to construct the model files. For
@@ -349,13 +345,6 @@ class PredictionManager : public PredictionModelDownloadObserver {
 
   PredictionModelFetchTimer prediction_model_fetch_timer_
       GUARDED_BY_CONTEXT(sequence_checker_);
-
-  // The clock used to schedule fetching from the remote Optimization Guide
-  // Service.
-  raw_ptr<const base::Clock> clock_;
-
-  // Whether the |model_and_features_store_| is initialized and ready for use.
-  bool store_is_ready_ = false;
 
   // Whether the profile for this PredictionManager is off the record.
   bool off_the_record_ = false;
