95f45079e9064882181b92dccf13d76f08acca5b
Adem Derinel <derinel@google.com>
2024-02-14 09:05:54 +0000

    [WebAuthn] Clean up WebAuthenticationAndroidIncognitoConfirmation flag
    Bug: b/40275628, b/40811886
    Change-Id: I9abe501ded575a0bd6c35d5075593d86daa2bbb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5285754
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260336}

--- services/device/public/cpp/device_feature_map.cc
+++ services/device/public/cpp/device_feature_map.cc
@@ -22,7 +22,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &device::kWebAuthnAndroidCredMan,
     &device::kWebAuthnAndroidCredManForHybrid,
     &device::kWebAuthnAndroidFidoJson,
-    &device::kWebAuthnAndroidIncognitoConfirmation,
     &device::kWebAuthnHybridLinkWithoutNotifications,
     &kGenericSensorExtraClasses,
 };
