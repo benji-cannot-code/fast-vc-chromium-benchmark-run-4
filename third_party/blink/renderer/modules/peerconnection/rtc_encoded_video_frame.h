93b5b5806f4a1e6a3562c37804f4dfa7cc31cb7a
Palak Agarwal <agpalak@chromium.org>
2024-02-14 15:05:01 +0000

    Rm unused clone function
    Bug: 14709
    Change-Id: I7a81177ee4994a3fe1f293bcd8195ec97d729aa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5295876
    Commit-Queue: Palak Agarwal <agpalak@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260449}

--- third_party/blink/renderer/modules/peerconnection/rtc_encoded_video_frame.h
+++ third_party/blink/renderer/modules/peerconnection/rtc_encoded_video_frame.h
@@ -51,7 +51,6 @@ class MODULES_EXPORT RTCEncodedVideoFrame final : public ScriptWrappable {
                    ExceptionState& exception_state);
   void setData(DOMArrayBuffer*);
   String toString() const;
-  RTCEncodedVideoFrame* clone(ExceptionState& exception_state) const;
 
   scoped_refptr<RTCEncodedVideoFrameDelegate> Delegate() const;
   void SyncDelegate() const;
