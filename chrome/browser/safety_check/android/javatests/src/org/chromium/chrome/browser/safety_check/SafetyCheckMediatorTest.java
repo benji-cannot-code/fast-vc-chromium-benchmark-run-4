6a9c122cd2e1516d9e600654fe1c82b27c9af9df
Anna Tsvirchkova <atsvirchkova@google.com>
2024-02-14 08:54:52 +0000

    [UPM] Remove setting signed out error in the safety check initial load
    The signed out check like it's implemented in the SafetyCheckMediator is
    now only valid for no-UPM password check. And so it needs to be removed
    from the safety check mediator. Instead it was moved to the
    ChromeNativePasswordCheckController and it is now returned as a result
    of calling getBreachedCredentialsCount.
    
    Bug: b/321686503
    Change-Id: Ieef3d2f01359f007d3cbf795ee71241cdfff9f9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5291098
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260333}

--- chrome/browser/safety_check/android/javatests/src/org/chromium/chrome/browser/safety_check/SafetyCheckMediatorTest.java
+++ chrome/browser/safety_check/android/javatests/src/org/chromium/chrome/browser/safety_check/SafetyCheckMediatorTest.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.safety_check;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
@@ -715,16 +714,13 @@ public class SafetyCheckMediatorTest {
     @Test
     public void testPasswordsInitialLoadUserSignedOut() {
         // Order: initial state is user signed out -> should display signed out error.
-        doReturn(false).when(mSafetyCheckBridge).userSignedIn(any(BrowserContextHandle.class));
         mMediator.setInitialState();
+        setUpPasswordCheckToReturnError(
+                PasswordStorageType.ACCOUNT_STORAGE,
+                new PasswordCheckNativeException(
+                        "Test signed out error", PasswordCheckUIStatus.ERROR_SIGNED_OUT));
 
         assertEquals(PasswordsState.SIGNED_OUT, mPasswordCheckModel.get(PASSWORDS_STATE));
-        // Check that there was no password check results fetch operation started.
-        assertNull(
-                mPasswordCheckControllerFactory
-                        .getLastCreatedController()
-                        .getFuturePasswordCheckResultForStorageType(
-                                PasswordStorageType.ACCOUNT_STORAGE));
         // The results of the previous check should be ignored.
         assertEquals(
                 1,
