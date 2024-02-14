dc59c4d58e730ec0430461f479fffc4aeef4883f
Lei Zhang <thestig@chromium.org>
2024-02-14 21:15:36 +0000

    Remove expired MediaDeviceNotification.UdevRequestSuccess histogram
    This decade old histogram should have been removed a long time ago, but
    it was checked into a different repo and thus avoided expiration audits
    for a long time. After it was moved into the main Chromium repo, now it
    finally expired.
    
    The success rate is over 99.9% for this Linux-only metric. Since Storage
    Monitor and Media Galleries are not in active development, there is no
    planned action to get the success rate higher. Thus stop recording this
    metric and remove the associated code.
    
    Bug: 324896799
    Change-Id: Id57d0bc020b3c1ea8aef8b2b4f6df58aa950074e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5293334
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1260700}

--- components/storage_monitor/storage_monitor_linux.cc
+++ components/storage_monitor/storage_monitor_linux.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/process/kill.h"
 #include "base/process/launch.h"
 #include "base/process/process.h"
@@ -76,30 +75,6 @@ std::string MakeDeviceUniqueId(struct udev_device* device) {
   return kVendorModelSerialPrefix + vendor + ":" + model + ":" + serial_short;
 }
 
-// Records GetDeviceInfo result on destruction, to see how often we fail to get
-// device details.
-class ScopedGetDeviceInfoResultRecorder {
- public:
-  ScopedGetDeviceInfoResultRecorder() = default;
-
-  ScopedGetDeviceInfoResultRecorder(const ScopedGetDeviceInfoResultRecorder&) =
-      delete;
-  ScopedGetDeviceInfoResultRecorder& operator=(
-      const ScopedGetDeviceInfoResultRecorder&) = delete;
-
-  ~ScopedGetDeviceInfoResultRecorder() {
-    UMA_HISTOGRAM_BOOLEAN("MediaDeviceNotification.UdevRequestSuccess",
-                          result_);
-  }
-
-  void set_result(bool result) {
-    result_ = result;
-  }
-
- private:
-  bool result_ = false;
-};
-
 // Returns the storage partition size of the device specified by |device_path|.
 // If the requested information is unavailable, returns 0.
 uint64_t GetDeviceStorageSize(const base::FilePath& device_path,
@@ -124,8 +99,6 @@ std::unique_ptr<StorageInfo> GetDeviceInfo(const base::FilePath& device_path,
   DCHECK(!device_path.empty());
 
   std::unique_ptr<StorageInfo> storage_info;
-
-  ScopedGetDeviceInfoResultRecorder results_recorder;
 
   device::ScopedUdevPtr udev_obj(device::udev_new());
   if (!udev_obj.get())
@@ -178,8 +151,6 @@ std::unique_ptr<StorageInfo> GetDeviceInfo(const base::FilePath& device_path,
                ? StorageInfo::REMOVABLE_MASS_STORAGE_WITH_DCIM
                : StorageInfo::REMOVABLE_MASS_STORAGE_NO_DCIM;
   }
-
-  results_recorder.set_result(true);
 
   storage_info = std::make_unique<StorageInfo>(
       StorageInfo::MakeDeviceId(type, unique_id), mount_point.value(),
