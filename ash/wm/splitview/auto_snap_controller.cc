ed3d695fc2ac8573a720ffa8dbb56161f648a06e
Michele Fan <michelefan@chromium.org>
2024-02-14 22:54:51 +0000

    snap-group: Avoid window overlapping on window layout complete
    This cl fixed the issue when two windows can be overlapped
    on window layout setup complete. The actual snap ratio might
    differ to accommodate for the window minimum size limitations,
    which didn't get updated. The auto-snap ratio therefore will
    still depend on the initial snap ratio of the first window
    snapped. We should update snap ratio before committing the snap
    event.
    
    Fixed: b/324631432
    Demo: http://b/324631432#comment5
    Test: Added + manual
    Change-Id: If83ba89a3da20470c480b86012845c0d7bdf202e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5284489
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260762}

--- ash/wm/splitview/auto_snap_controller.cc
+++ ash/wm/splitview/auto_snap_controller.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "ash/wm/splitview/auto_snap_controller.h"
+
 #include <optional>
 
 #include "ash/root_window_controller.h"
