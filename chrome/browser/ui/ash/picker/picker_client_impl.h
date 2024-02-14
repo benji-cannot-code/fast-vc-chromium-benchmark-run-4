cd658b0ea6de7e969183350d072cd86000e28b70
mlcui <mlcui@google.com>
2024-02-14 01:02:57 +0000

    picker: Add `PickerClient::StopCrosQuery`; stop query before searches
    This is required for resetting states between searches, as `StopQuery`
    will sometimes synchronously call the search results callback with empty
    results.
    
    Bug: b:316936418
    Change-Id: I3c356db54e6769096947082cc2cf1f8f8320889e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5276398
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1260188}

--- chrome/browser/ui/ash/picker/picker_client_impl.h
+++ chrome/browser/ui/ash/picker/picker_client_impl.h
@@ -61,6 +61,7 @@ class PickerClientImpl
                            DownloadGifToStringCallback callback) override;
   void StartCrosSearch(const std::u16string& query,
                        CrosSearchResultsCallback callback) override;
+  void StopCrosQuery() override;
 
   // user_manager::UserManager::UserSessionStateObserver:
   void ActiveUserChanged(user_manager::User* active_user) override;
