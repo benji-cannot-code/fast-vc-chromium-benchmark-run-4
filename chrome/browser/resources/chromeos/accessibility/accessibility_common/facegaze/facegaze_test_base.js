d128882caccd897455803705729dc596ea77f550
Katie Dektar <katie@chromium.org>
2024-02-14 18:52:33 +0000

    [FaceGaze] Add minimum repeat rate for a gesture
    This creates smoother gesture detection by debouncing gesture
    recognition.
    
    Test: Manual, new
    Bug: b:322511275
    Change-Id: I6e413905d15796e8e4fd995823b8494612c1ac7f
    AX-Relnotes: N/A
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5288535
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260601}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/facegaze_test_base.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/facegaze_test_base.js
@@ -107,7 +107,7 @@ class MockFaceLandmarkerResult {
   }
 
   /**
-   * @param {string} name
+   * @param {MediapipeFacialGesture} name
    * @param {number} confidence
    * @return {!MockFaceLandmarkerResult}
    */
@@ -153,7 +153,11 @@ FaceGazeTestBase = class extends E2ETestBase {
     await importModule(
         'FaceGaze', '/accessibility_common/facegaze/facegaze.js');
     await importModule(
-        ['FacialGesture'],
+        [
+          'FacialGesture',
+          'MediapipeFacialGesture',
+          'FacialGesturesToMediapipeGestures',
+        ],
         '/accessibility_common/facegaze/gesture_detector.js');
     await importModule(
         ['MouseController'],
@@ -163,6 +167,8 @@ FaceGazeTestBase = class extends E2ETestBase {
         assertNotNullNorUndefined(accessibilityCommon);
     assertNotNullNorUndefined(FaceGaze);
     assertNotNullNorUndefined(FacialGesture);
+    assertNotNullNorUndefined(MediapipeFacialGesture);
+    assertNotNullNorUndefined(FacialGesturesToMediapipeGestures);
     assertNotNullNorUndefined(MouseController);
     assertNotNullNorUndefined(MacroName);
     await new Promise(resolve => {
@@ -209,11 +215,13 @@ FaceGazeTestBase = class extends E2ETestBase {
     }
 
     if (config.gestureToMacroName) {
-      faceGaze.gestureToMacroName_ = new Map(config.gestureToMacroName);
+      faceGaze.gestureHandler_.gestureToMacroName_ =
+          new Map(config.gestureToMacroName);
     }
 
     if (config.gestureToConfidence) {
-      faceGaze.gestureToConfidence_ = new Map(config.gestureToConfidence);
+      faceGaze.gestureHandler_.gestureToConfidence_ =
+          new Map(config.gestureToConfidence);
     }
 
     if (config.bufferSize !== -1) {
