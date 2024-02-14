b62361676b843bb97a9116a99730ade736a2a3de
Rakina Zata Amni <rakina@chromium.org>
2024-02-14 05:37:28 +0000

    Revert "Reland "Fix flakiness of ComposeInteractiveUiTest""
    This reverts commit b87a329f8a510cdbf9830bd81ea06412c3e7e3d5.
    
    Reason for revert: Still flaky: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Tests/45931/overview
    
    Original change's description:
    > Reland "Fix flakiness of ComposeInteractiveUiTest"
    >
    > Bug: 41490408
    > Change-Id: I166bc5ca2c6167f01ae6f073b3100214573e9656
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5280625
    > Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    > Reviewed-by: Peter Williamson <petewil@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1260154}
    
    Bug: 41490408
    Change-Id: Ide6b343487f1086386378164d1e39a1cad95210e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5294976
    Auto-Submit: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260294}

--- chrome/browser/ui/views/compose/compose_interactive_uitest.cc
+++ chrome/browser/ui/views/compose/compose_interactive_uitest.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/compose/compose_dialog_view.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/test/base/in_process_browser_test.h"
-#include "chrome/test/base/interactive_test_utils.h"
 #include "chrome/test/interaction/interaction_test_util_browser.h"
 #include "chrome/test/interaction/interactive_browser_test.h"
 #include "components/autofill/core/common/autofill_features.h"
@@ -50,7 +49,7 @@ namespace {
 
 DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kContentPageTabId);
 DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kComposeWebContents);
-DEFINE_LOCAL_CUSTOM_ELEMENT_EVENT_TYPE(kElementRendered);
+DEFINE_LOCAL_CUSTOM_ELEMENT_EVENT_TYPE(kElementReadyEvent);
 DEFINE_LOCAL_CUSTOM_ELEMENT_EVENT_TYPE(kElementChangedEvent);
 
 const char kTestPageDomain[] = "a.test";
@@ -125,26 +124,21 @@ class MAYBE_ComposeInteractiveUiTest : public InteractiveBrowserTest {
 
   /////////////////////////////////////////
   // Compose interactive UI test step helpers.
-  InteractiveTestApi::MultiStep PressJsButton(
+  InteractiveTestApi::StepBuilder PressJsButton(
       const ui::ElementIdentifier web_contents_id,
       const DeepQuery& button_query) {
     // This can close/navigate the current page, so don't wait for success.
-    return Steps(
-        EnsurePresent(web_contents_id, button_query),
-        ExecuteJsAt(web_contents_id, button_query, "(btn) => btn.click()",
-                    ExecuteJsMode::kFireAndForget));
+    return ExecuteJsAt(web_contents_id, button_query, "(btn) => btn.click()",
+                       ExecuteJsMode::kFireAndForget);
   }
 
-  InteractiveTestApi::MultiStep WaitForElementToRender(
-      const DeepQuery& element) {
-    StateChange element_renders;
-    element_renders.event = kElementRendered;
-    element_renders.where = element;
-    element_renders.test_function =
-        "(el) => { if (el !== null) { let rect = el.getBoundingClientRect(); "
-        "return rect.width > 0 && rect.height > 0; } return false; }";
-
-    return WaitForStateChange(kContentPageTabId, element_renders);
+  InteractiveTestApi::MultiStep WaitForElementToLoad(
+      const DeepQuery& element_query) {
+    StateChange element_loaded;
+    element_loaded.event = kElementReadyEvent;
+    element_loaded.type = StateChange::Type::kExists;
+    element_loaded.where = element_query;
+    return WaitForStateChange(kContentPageTabId, std::move(element_loaded));
   }
 
   InteractiveTestApi::MultiStep WaitForElementValueToIncludeCucumbers(
@@ -160,7 +154,7 @@ class MAYBE_ComposeInteractiveUiTest : public InteractiveBrowserTest {
 
   InteractiveTestApi::MultiStep OpenCompose() {
     return Steps(
-        WaitForElementToRender(kTextarea),
+        WaitForElementToLoad(kTextarea),
         MoveMouseTo(kContentPageTabId, kTextarea),
         ClickMouse(ui_controls::RIGHT),
         WaitForShow(RenderViewContextMenu::kComposeMenuItem),
@@ -212,9 +206,6 @@ class MAYBE_ComposeInteractiveUiTest : public InteractiveBrowserTest {
 
     SetUpOptimizationGuide();
     SetUpAccount();
-    ASSERT_TRUE(ui_test_utils::BringBrowserWindowToFront(browser()));
-    ASSERT_TRUE(ui_test_utils::ShowAndFocusNativeWindow(
-        browser()->window()->GetNativeWindow()));
   }
 
   void TearDownOnMainThread() override {
@@ -345,7 +336,8 @@ class MAYBE_ComposeInteractiveUiTest : public InteractiveBrowserTest {
 };
 
 // Flaky on all platforms: https://crbug.com/1517430
-IN_PROC_BROWSER_TEST_F(MAYBE_ComposeInteractiveUiTest, OpenAndCloseCompose) {
+IN_PROC_BROWSER_TEST_F(MAYBE_ComposeInteractiveUiTest,
+                       DISABLED_OpenAndCloseCompose) {
   RunTestSequence(
       MakePrimaryAccountAvailable(), InstrumentTab(kContentPageTabId),
       NavigateWebContents(
@@ -354,7 +346,6 @@ IN_PROC_BROWSER_TEST_F(MAYBE_ComposeInteractiveUiTest, OpenAndCloseCompose) {
       WaitForWebContentsReady(
           kContentPageTabId,
           https_server()->GetURL(kTestPageDomain, kTestPageUrl)),
-      ActivateSurface(kBrowserViewElementId), FlushEvents(), OpenCompose(),
-      AcceptFRE(), RequestCompose(), AcceptComposeResult(),
+      OpenCompose(), AcceptFRE(), RequestCompose(), AcceptComposeResult(),
       WaitForElementValueToIncludeCucumbers(kTextarea));
 }
