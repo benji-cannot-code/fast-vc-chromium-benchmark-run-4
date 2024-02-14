3bb9381348bc8202ff26db2b0a301ecb41cc743a
Keren Zhu <kerenzhu@chromium.org>
2024-02-14 20:42:03 +0000

    views: fix focus ring invisible issue in LayoutManagerBase
    FocusRing was not laid out by LayoutManagerBase because it reports a zero preferred size. This patch marks FocusRing as a "decorative view",
    such that:
    
    * it has the View property kIsDecorativeViewKey set to true.
    * its visibility is respcted by LayoutManagerBase. i.e. the layout
      manager's proposed visibility is disregarded.
    * its Layout() is always invoked during layout.
    
    Bug: b/325029896
    Change-Id: I02daf708405cb3f9db777293107f8cd516929b0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5293541
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260680}

--- ui/views/focus/focus_manager_unittest.cc
+++ ui/views/focus/focus_manager_unittest.cc
@@ -27,12 +27,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/accessible_pane_view.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
 #include "ui/views/buildflags.h"
+#include "ui/views/controls/focus_ring.h"
 #include "ui/views/focus/focus_manager_delegate.h"
 #include "ui/views/focus/focus_manager_factory.h"
 #include "ui/views/focus/widget_focus_manager.h"
+#include "ui/views/layout/flex_layout.h"
 #include "ui/views/test/focus_manager_test.h"
 #include "ui/views/test/native_widget_factory.h"
 #include "ui/views/test/test_platform_native_widget.h"
+#include "ui/views/test/test_views.h"
+#include "ui/views/test/views_test_utils.h"
 #include "ui/views/test/widget_test.h"
 #include "ui/views/view_class_properties.h"
 #include "ui/views/widget/unique_widget_ptr.h"
@@ -1105,6 +1109,33 @@ TEST_F(FocusManagerTest, AnchoredDialogInPane) {
   EXPECT_TRUE(anchor->HasFocus());
   GetWidget()->GetFocusManager()->AdvanceFocus(false);
   EXPECT_TRUE(bubble_child->HasFocus());
+}
+
+// Test that a focused view has a visible focus ring.
+// This test uses FlexLayout intentionally because it had issues showing focus
+// rings.
+TEST_F(FocusManagerTest, FocusRing) {
+  GetContentsView()->SetLayoutManager(std::make_unique<FlexLayout>());
+  View* view = GetContentsView()->AddChildView(
+      std::make_unique<StaticSizedView>(gfx::Size(10, 10)));
+  GetContentsView()->SetFocusBehavior(View::FocusBehavior::ALWAYS);
+  view->SetFocusBehavior(View::FocusBehavior::ALWAYS);
+  FocusRing::Install(GetContentsView());
+  FocusRing::Install(view);
+
+  GetContentsView()->RequestFocus();
+  test::RunScheduledLayout(GetWidget());
+  EXPECT_TRUE(GetContentsView()->HasFocus());
+  EXPECT_TRUE(FocusRing::Get(GetContentsView())->GetVisible());
+  EXPECT_FALSE(view->HasFocus());
+  EXPECT_FALSE(FocusRing::Get(view)->GetVisible());
+
+  view->RequestFocus();
+  test::RunScheduledLayout(GetWidget());
+  EXPECT_FALSE(GetContentsView()->HasFocus());
+  EXPECT_FALSE(FocusRing::Get(GetContentsView())->GetVisible());
+  EXPECT_TRUE(view->HasFocus());
+  EXPECT_TRUE(FocusRing::Get(view)->GetVisible());
 }
 
 #if BUILDFLAG(ENABLE_DESKTOP_AURA)
