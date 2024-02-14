8efd4897330090e1c1828bcd89d68f0c7a95609b
Justin Lulejian <jlulejian@chromium.org>
2024-02-14 01:10:13 +0000

    [Extensions] Fix corrupt extensions metric enum.
    Extensions.CorruptExtensionDisabledReason enum values do not match the
    code enum ContentVerifyJob::FailureReason. This makes them match.
    
    Also convert UMA macro to function per histogram guidelines.
    
    Bug: N/A
    Change-Id: I5c600c58e189a235949f2bb7bd143104cfa03d17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5293465
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260191}

--- chrome/browser/extensions/chrome_content_verifier_delegate.cc
+++ chrome/browser/extensions/chrome_content_verifier_delegate.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/containers/contains.h"
 #include "base/metrics/field_trial.h"
-#include "base/metrics/histogram_macros.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/escape.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/string_util.h"
@@ -274,8 +274,8 @@ void ChromeContentVerifierDelegate::VerifyFailed(
   DCHECK(should_disable);
   service->DisableExtension(extension_id, disable_reason::DISABLE_CORRUPTED);
   ExtensionPrefs::Get(context_)->IncrementPref(kCorruptedDisableCount);
-  UMA_HISTOGRAM_ENUMERATION("Extensions.CorruptExtensionDisabledReason", reason,
-                            ContentVerifyJob::FAILURE_REASON_MAX);
+  base::UmaHistogramEnumeration("Extensions.CorruptExtensionDisabledReason",
+                                reason, ContentVerifyJob::FAILURE_REASON_MAX);
 }
 
 void ChromeContentVerifierDelegate::Shutdown() {}
