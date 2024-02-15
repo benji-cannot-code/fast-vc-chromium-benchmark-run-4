6ee2506fd0ba499d6d2cfa9a555fc1b31185a297
cassycc <cassycc@google.com>
2024-02-15 00:18:30 +0000

    [FspCache] Add a permanent watcher on root
    The CloudFileSystem should add a recursive watcher on root whenever
    there's a ContentCache. This means that the Cloud file system provider
    will send notifications whenever there have been changes on the Cloud.
    All notifications go through CloudFileSystem::Notify().
    
    This is important so that the CloudFileSystem can know whether its
    ContentCache is out of date.
    
    Bug: b/317137739
    Change-Id: I46614671eeae4d6d229ee7b8b7c2df0e6a73915d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5276949
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1260802}

--- chrome/browser/ash/file_system_provider/cloud_file_system.cc
+++ chrome/browser/ash/file_system_provider/cloud_file_system.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/files/file.h"
+#include "base/files/file_path.h"
 #include "base/functional/bind.h"
 #include "base/logging.h"
 #include "base/notreached.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/file_manager/fileapi_util.h"
 #include "chrome/browser/ash/file_system_provider/provided_file_system_interface.h"
 #include "chrome/browser/ash/file_system_provider/queue.h"
+#include "url/origin.h"
 
 namespace ash::file_system_provider {
 
@@ -26,6 +28,14 @@ namespace {
 // The frequency that the FSP syncs with the cloud when the File Manager is a
 // watcher.
 constexpr base::TimeDelta kFileManagerWatcherInterval = base::Seconds(15);
+
+const GURL GetContentCacheURL() {
+  return GURL("chrome://content-cache/");
+}
+
+const base::FilePath RootFilePath() {
+  return base::FilePath("/");
+}
 
 std::ostream& operator<<(std::ostream& out,
                          const std::vector<base::FilePath>& entry_paths) {
@@ -84,9 +94,28 @@ CloudFileSystem::CloudFileSystem(
 CloudFileSystem::CloudFileSystem(
     std::unique_ptr<ProvidedFileSystemInterface> file_system,
     ContentCache* content_cache)
-    : file_system_(std::move(file_system)), content_cache_(content_cache) {}
+    : file_system_(std::move(file_system)), content_cache_(content_cache) {
+  if (content_cache_) {
+    // Add watcher to keep content cache up to date. Notifications are received
+    // though Notify() so no notification_callback is needed.
+    AddWatcher(GetContentCacheURL(), RootFilePath(),
+               /*recursive=*/true, /*persistent=*/false,
+               base::BindOnce([](base::File::Error result) {
+                 VLOG(1) << "Added file watcher on root: " << result;
+               }),
+               base::DoNothing());
+  }
+}
 
-CloudFileSystem::~CloudFileSystem() = default;
+CloudFileSystem::~CloudFileSystem() {
+  if (content_cache_) {
+    RemoveWatcher(GetContentCacheURL(), RootFilePath(),
+                  /*recursive=*/true,
+                  base::BindOnce([](base::File::Error result) {
+                    VLOG(1) << "Removed file watcher on root: " << result;
+                  }));
+  }
+}
 
 AbortCallback CloudFileSystem::RequestUnmount(
     storage::AsyncFileUtil::StatusCallback callback) {
