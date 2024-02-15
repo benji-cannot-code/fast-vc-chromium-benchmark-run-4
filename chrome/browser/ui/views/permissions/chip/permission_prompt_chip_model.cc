335861f6d3390618512ab1050b0253f5bc2672cc
Ravjit <ravjit@chromium.org>
2024-02-15 22:34:23 +0000

    Update the quiet chip text for quiet chips triggered by CPSS
    Update the text from "Notifications Blocked" to "Get Notifications?"
    
    Bug: 325444869
    Change-Id: If6b4ec82cca79ee97317d8cb2589e0e59ebdf217
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5301517
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Ravjit Uppal <ravjit@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261352}

--- chrome/browser/ui/views/permissions/chip/permission_prompt_chip_model.cc
+++ chrome/browser/ui/views/permissions/chip/permission_prompt_chip_model.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/permissions/chip/permission_prompt_chip_model.h"
 
 #include "base/check.h"
+#include "base/feature_list.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/permissions/permission_actions_history.h"
 #include "components/permissions/permission_request_manager.h"
@@ -47,8 +48,20 @@ const gfx::VectorIcon& GetPermissionIconId(
 std::u16string GetQuietPermissionMessage(
     permissions::PermissionPrompt::Delegate* delegate) {
   DCHECK(delegate);
-  auto quiet_request_text = delegate->Requests()[0]->GetRequestChipText(
-      permissions::PermissionRequest::QUIET_REQUEST);
+  DCHECK(delegate->ReasonForUsingQuietUi());
+  auto chip_text_type = permissions::PermissionRequest::QUIET_REQUEST;
+  const auto quiet_ui_reason = delegate->ReasonForUsingQuietUi();
+  if (quiet_ui_reason == permissions::PermissionRequestManager::QuietUiReason::
+                             kServicePredictedVeryUnlikelyGrant ||
+      quiet_ui_reason == permissions::PermissionRequestManager::QuietUiReason::
+                             kOnDevicePredictedVeryUnlikelyGrant) {
+    chip_text_type = base::FeatureList::IsEnabled(
+                         permissions::features::kCpssQuietChipTextUpdate)
+                         ? permissions::PermissionRequest::LOUD_REQUEST
+                         : permissions::PermissionRequest::QUIET_REQUEST;
+  }
+  auto quiet_request_text =
+      delegate->Requests()[0]->GetRequestChipText(chip_text_type);
   return quiet_request_text.value_or(u"");
 }
 
@@ -89,9 +102,21 @@ PermissionPromptChipModel::PermissionPromptChipModel(
   DCHECK(delegate_);
 
   if (delegate_->ShouldCurrentRequestUseQuietUI()) {
+    DCHECK(delegate_->ReasonForUsingQuietUi());
     prompt_style_ = PermissionPromptStyle::kQuietChip;
     should_bubble_start_open_ = false;
-    should_display_blocked_icon_ = true;
+    const auto quiet_ui_reason = delegate_->ReasonForUsingQuietUi();
+    if (quiet_ui_reason ==
+            permissions::PermissionRequestManager::QuietUiReason::
+                kOnDevicePredictedVeryUnlikelyGrant ||
+        quiet_ui_reason ==
+            permissions::PermissionRequestManager::QuietUiReason::
+                kServicePredictedVeryUnlikelyGrant) {
+      should_display_blocked_icon_ = !base::FeatureList::IsEnabled(
+          permissions::features::kCpssQuietChipTextUpdate);
+    } else {
+      should_display_blocked_icon_ = true;
+    }
     should_expand_ =
         ShouldPermissionBubbleExpand(delegate_.get(), prompt_style_) &&
         (should_bubble_start_open_ ||
