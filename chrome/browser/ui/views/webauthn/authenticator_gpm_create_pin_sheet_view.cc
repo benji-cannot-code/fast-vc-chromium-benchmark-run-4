85fae0ebe9c560e07d84e4a75b734c2902731f89
rgod <rgod@google.com>
2024-02-15 11:46:14 +0000

    [Webauthn] Add custom textfield implementation for entering a pin
    For now the textfield supports typing digits (0-9) and removing them
    with a backspace. The code it not wired anywhere, can be triggered by:
    
    browser_tests --gtest_filter=BrowserUiTest.Invoke \
      --test-launcher-interactive \
      --ui=GPMPasskeysAuthenticatorDialogTest.InvokeUi_gpm_create_pin
    
    Bug: b/40274370
    Change-Id: I822c0b10734cfc0818cbc446b8cd0a7524e6fd80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5296535
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260983}

--- chrome/browser/ui/views/webauthn/authenticator_gpm_create_pin_sheet_view.cc
+++ chrome/browser/ui/views/webauthn/authenticator_gpm_create_pin_sheet_view.cc
@@ -5,7 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/webauthn/authenticator_gpm_create_pin_sheet_view.h"
 
+#include "base/strings/string_util.h"
+#include "chrome/browser/ui/views/webauthn/pin_textfield.h"
+#include "ui/events/keycodes/keyboard_codes.h"
 #include "ui/views/controls/textfield/textfield.h"
+
+namespace {
+// Number of digits for the GPM Pin.
+constexpr int kPinDigitCount = 6;
+}  // namespace
 
 AuthenticatorGpmCreatePinSheetView::AuthenticatorGpmCreatePinSheetView(
     std::unique_ptr<AuthenticatorGPMCreatePinSheetModel> sheet_model)
@@ -14,11 +22,32 @@ AuthenticatorGpmCreatePinSheetView::AuthenticatorGpmCreatePinSheetView(
 AuthenticatorGpmCreatePinSheetView::~AuthenticatorGpmCreatePinSheetView() =
     default;
 
+bool AuthenticatorGpmCreatePinSheetView::HandleKeyEvent(
+    views::Textfield* textfield,
+    const ui::KeyEvent& event) {
+  if (event.type() != ui::ET_KEY_PRESSED) {
+    return false;
+  }
+
+  if (event.key_code() == ui::VKEY_BACK) {
+    pin_textfield_->RemoveDigit();
+    return true;
+  }
+
+  char16_t c = event.GetCharacter();
+  if (base::IsAsciiDigit(c)) {
+    pin_textfield_->AppendDigit(std::u16string(1, c));
+  }
+
+  return true;
+}
+
 std::pair<std::unique_ptr<views::View>,
           AuthenticatorGpmCreatePinSheetView::AutoFocus>
 AuthenticatorGpmCreatePinSheetView::BuildStepSpecificContent() {
-  // TODO(rgod): Add proper implementation once its designed.
-  auto pin_textfield = std::make_unique<views::Textfield>();
+  auto pin_textfield = std::make_unique<PinTextfield>(kPinDigitCount);
+  pin_textfield->SetController(this);
   pin_textfield->SetAccessibleName(u"Pin field (UNTRANSLATED)");
+  pin_textfield_ = pin_textfield.get();
   return std::make_pair(std::move(pin_textfield), AutoFocus::kYes);
 }
