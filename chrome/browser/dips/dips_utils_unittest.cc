4bb224ffc25d7c3d5f519500c86cadff34c44687
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2024-02-15 04:24:41 +0000

    Revert "Add AdTagged metric to popup/redirect heuristic cookie access UKMs."
    This reverts commit 1b9fe284d8c4d8b59f303790bae83ed243552c42.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5652972228837376
    
    Sample build with failed test: https://ci.chromium.org/b/8756077425862210721
    Affected test(s):
    [ninja://chrome/test:browser_tests/OpenerHeuristicBrowserTest.PopupPastInteractionIsFollowedByPostPopupCookieAccess](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:browser_tests%2FOpenerHeuristicBrowserTest.PopupPastInteractionIsFollowedByPostPopupCookieAccess?q=VHash%3A6d0a43807ac672d9)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5652972228837376&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F5272190&type=BUG
    
    Original change's description:
    > Add AdTagged metric to popup/redirect heuristic cookie access UKMs.
    >
    > This will help estimate what % of cookies allowed by heuristics are then blocked by ad-tagging. We are collecting other metrics for ad-tagging on page load as well, but this is the only place we can derive the 3P site from the metrics, since we emit a linked UKM event on the 3P top level.
    >
    > UKM reviews:
    > Opener/Popup: https://docs.google.com/document/d/1GJBMYuhl77eTSjLMMEtq1Y2Kd3Vdv1ozfdFi1iCueZI
    > Redirect:
    > https://docs.google.com/document/d/1dogmwtbUU0MEwL5RKICv47Ly_RnxBJhRuJHvMwsKrqs
    >
    > Bug: 324117304
    > Change-Id: I88d10fe09e6bde0fd73b467a1ea3d738b48e305c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5272190
    > Reviewed-by: Ryan Tarpine <rtarpine@chromium.org>
    > Commit-Queue: Anton Maliev <amaliev@chromium.org>
    > Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1260715}
    >
    
    Bug: 324117304
    Change-Id: I30a3d7804828b49da384f46a0e18a48dcb01e8b6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5299036
    Owners-Override: Robert Lin <robertlin@google.com>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Reviewed-by: Huanpo Lin <robertlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260888}

--- chrome/browser/dips/dips_utils_unittest.cc
+++ chrome/browser/dips/dips_utils_unittest.cc
@@ -5,10 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/dips/dips_utils.h"
 
-#include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
-#include "content/public/browser/cookie_access_details.h"
-#include "services/network/public/cpp/features.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -149,44 +146,4 @@ TEST(UpdateTimestampTest, ReplaceAfterIntervalPasses) {
   ASSERT_THAT(time, testing::Optional(old_value));
   EXPECT_TRUE(UpdateTimestamp(time, new_value));
   EXPECT_THAT(time, testing::Optional(new_value));
-}
-
-TEST(IsAdTaggedCookieForHeuristics, ReturnsCorrectlyInExperiment) {
-  base::test::ScopedFeatureList features;
-  features.InitAndEnableFeatureWithParameters(
-      network::features::kSkipTpcdMitigationsForAds,
-      {{"SkipTpcdMitigationsForAdsHeuristics", "true"}});
-
-  content::CookieAccessDetails details;
-  EXPECT_EQ(IsAdTaggedCookieForHeuristics(details), OptionalBool::kFalse);
-
-  details.cookie_setting_overrides.Put(
-      net::CookieSettingOverride::kSkipTPCDHeuristicsGrant);
-  EXPECT_EQ(IsAdTaggedCookieForHeuristics(details), OptionalBool::kTrue);
-}
-
-TEST(IsAdTaggedCookieForHeuristics, ReturnsCorrectlyWithoutExperimentFeature) {
-  base::test::ScopedFeatureList features;
-  features.InitAndDisableFeature(network::features::kSkipTpcdMitigationsForAds);
-
-  content::CookieAccessDetails details;
-  EXPECT_EQ(IsAdTaggedCookieForHeuristics(details), OptionalBool::kUnknown);
-
-  details.cookie_setting_overrides.Put(
-      net::CookieSettingOverride::kSkipTPCDHeuristicsGrant);
-  EXPECT_EQ(IsAdTaggedCookieForHeuristics(details), OptionalBool::kUnknown);
-}
-
-TEST(IsAdTaggedCookieForHeuristics, ReturnsCorrectlyWithoutExperimentParam) {
-  base::test::ScopedFeatureList features;
-  features.InitAndEnableFeatureWithParameters(
-      network::features::kSkipTpcdMitigationsForAds,
-      {{"SkipTpcdMitigationsForAdsHeuristics", "false"}});
-
-  content::CookieAccessDetails details;
-  EXPECT_EQ(IsAdTaggedCookieForHeuristics(details), OptionalBool::kUnknown);
-
-  details.cookie_setting_overrides.Put(
-      net::CookieSettingOverride::kSkipTPCDHeuristicsGrant);
-  EXPECT_EQ(IsAdTaggedCookieForHeuristics(details), OptionalBool::kUnknown);
 }
