4ae2e3fd16e6cbf4548252a90858acbd442ea243
David Roger <droger@chromium.org>
2024-02-15 13:43:01 +0000

    [Uno-d] Enable uno in ExtensionBrowsingDataTest
    Bug: b/325198098
    Change-Id: I0b65b229f2447436395e94713ebcd9a3b845295f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5294235
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1261013}

--- chrome/browser/extensions/api/browsing_data/browsing_data_test.cc
+++ chrome/browser/extensions/api/browsing_data/browsing_data_test.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/storage/public/mojom/local_storage_control.mojom.h"
 #include "components/services/storage/public/mojom/storage_usage_info.mojom.h"
 #include "components/signin/public/base/signin_buildflags.h"
-#include "components/signin/public/base/signin_switches.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/identity_test_utils.h"
 #include "components/sync/service/sync_service.h"
@@ -43,16 +42,7 @@ using extensions::api_test_utils::RunFunctionAndReturnSingleResult;
 
 namespace {
 
-class ExtensionBrowsingDataTest : public InProcessBrowserTest {
- public:
-  ExtensionBrowsingDataTest() {
-    // TODO(b/314968275): Add tests for when UNO Desktop is enabled.
-    scoped_feature_list_.InitAndDisableFeature(switches::kUnoDesktop);
-  }
-
- private:
-  base::test::ScopedFeatureList scoped_feature_list_;
-};
+class ExtensionBrowsingDataTest : public InProcessBrowserTest {};
 
 class ExtensionBrowsingDataTestWithStoragePartitioning
     : public ExtensionBrowsingDataTest {
