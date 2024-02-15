2fec28de58b269bb5d68b73930753ea2065ac68e
David Roger <droger@chromium.org>
2024-02-15 13:48:56 +0000

    [Uno-d] Enable uno in SaveCardBubbleViewsSyncTransportFullFormBrowserTest
    Fixed: b/320651082
    Change-Id: I226219c7f41a3c7a77b3d245b9ff2c99ab0fc207
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5295614
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261014}

--- chrome/browser/ui/views/autofill/payments/save_card_bubble_views_browsertest.cc
+++ chrome/browser/ui/views/autofill/payments/save_card_bubble_views_browsertest.cc
@@ -66,7 +66,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/network_session_configurator/common/network_switches.h"
 #include "components/signin/public/base/signin_buildflags.h"
-#include "components/signin/public/base/signin_switches.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/identity_test_utils.h"
 #include "components/sync/base/features.h"
@@ -943,7 +942,6 @@ class SaveCardBubbleViewsSyncTransportFullFormBrowserTest
     enabled_features.push_back(syncer::kSyncDecoupleAddressPaymentSettings);
     enabled_features.push_back(
         syncer::kSyncEnableContactInfoDataTypeInTransportMode);
-    disabled_features.push_back(switches::kUnoDesktop);
     feature_list_.InitWithFeatures(enabled_features, disabled_features);
   }
 
