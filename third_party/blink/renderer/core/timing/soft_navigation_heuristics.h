5698d5e5bd0212436964259373e8ac4be63333c2
Scott Haseley <shaseley@chromium.org>
2024-02-15 03:45:44 +0000

    [soft navigations] Refactor nested EventScopes
    Previously, we tracked both a deque of EventParameters for nested event
    scopes, the top EventParameters, and the current event EventParameters.
    This can be simplified by putting the initial and nested EventParameters
    in the deque, and then defining the current parameters as the deque's
    back(). This CL does that as well as moves the EventScope complexity to
    the SoftNavigationHeuristics class. Finally, this makes a few methods
    used during scope construction/destruction private, since they are not
    part of the public API.
    
    Note: this CL swaps the order of calling SetCurrentTimeAsStartTime()
    and popping the nested EventParameters since the former relies on
    the latter. This appears to be a bug, although previously we didn't
    clear the last entry (so the behavior didn't change for the last call/
    outermost scope).
    
    Bug: 324111878
    Change-Id: Id68121450136bde1d7d56dd3c584797dbb002958
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5242021
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260878}

--- third_party/blink/renderer/core/timing/soft_navigation_heuristics.h
+++ third_party/blink/renderer/core/timing/soft_navigation_heuristics.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_TIMING_SOFT_NAVIGATION_HEURISTICS_H_
 
 #include "base/containers/enum_set.h"
+#include "base/gtest_prod_util.h"
 #include "base/memory/stack_allocated.h"
 #include "third_party/blink/public/common/scheduler/task_attribution_id.h"
 #include "third_party/blink/renderer/core/core_export.h"
@@ -52,6 +53,9 @@ class CORE_EXPORT SoftNavigationHeuristics
       public Supplement<LocalDOMWindow>,
       public scheduler::TaskAttributionTracker::Observer {
  public:
+  FRIEND_TEST_ALL_PREFIXES(SoftNavigationHeuristicsTest,
+                           EarlyReturnOnInvalidPendingInteractionTimestamp);
+
   // This class defines a scope that would cover click or navigation related
   // events, in order for the SoftNavigationHeuristics class to be able to keep
   // track of them and their descendant tasks.
@@ -69,9 +73,10 @@ class CORE_EXPORT SoftNavigationHeuristics
    private:
     friend class SoftNavigationHeuristics;
 
-    EventScope(SoftNavigationHeuristics*, Type type, bool is_new_interaction);
+    explicit EventScope(SoftNavigationHeuristics*);
 
     SoftNavigationHeuristics* heuristics_;
+    bool nested_ = false;
   };
 
   // Supplement boilerplate.
@@ -84,10 +89,6 @@ class CORE_EXPORT SoftNavigationHeuristics
   void Trace(Visitor*) const override;
 
   // The class's API.
-  void InteractionCallbackCalled(const scheduler::TaskAttributionInfo& task,
-                                 EventScope::Type,
-                                 bool is_new_interaction);
-  void UserInitiatedInteraction();
   void SameDocumentNavigationStarted();
   void SameDocumentNavigationCommitted(const String& url);
   bool ModifiedDOM();
@@ -101,16 +102,7 @@ class CORE_EXPORT SoftNavigationHeuristics
                    uint64_t painted_area,
                    bool is_modified_by_soft_navigation);
 
-  void SetEventParametersAndQueueNestedOnes(EventScope::Type type,
-                                            bool is_new_interaction,
-                                            bool is_nested);
-
   EventScope CreateEventScope(EventScope::Type type, bool is_new_interaction);
-
-  // If there are nested EventParameters, pop one, restore it to the
-  // current_event_parameters_ and return true. Otherwise, return false.
-  bool PopNestedEventParametersIfNeeded();
-  void SetCurrentTimeAsStartTime();
 
   // This method is called during the weakness processing stage of garbage
   // collection, and it's used to detect `potential_soft_navigation_tasks_`
@@ -130,6 +122,7 @@ class CORE_EXPORT SoftNavigationHeuristics
     kURLChange,
     kMainModification,
   };
+
   using FlagTypeSet = base::EnumSet<FlagType, kURLChange, kMainModification>;
   struct PerInteractionData : public GarbageCollected<PerInteractionData> {
     // The timestamp just before the event responding to the user's interaction
@@ -141,6 +134,15 @@ class CORE_EXPORT SoftNavigationHeuristics
     FlagTypeSet flag_set;
     String url;
     void Trace(Visitor*) const {}
+  };
+
+  struct EventParameters {
+    explicit EventParameters() = default;
+    EventParameters(bool is_new_interaction, EventScope::Type type)
+        : is_new_interaction(is_new_interaction), type(type) {}
+
+    bool is_new_interaction = false;
+    EventScope::Type type = EventScope::Type::kClick;
   };
 
   void ReportSoftNavigationToMetrics(LocalFrame* frame) const;
@@ -156,8 +158,19 @@ class CORE_EXPORT SoftNavigationHeuristics
   void CommitPreviousPaints(LocalFrame*);
   void EmitSoftNavigationEntryIfAllConditionsMet(LocalFrame*);
   LocalFrame* GetLocalFrameIfNotDetached() const;
+  void InteractionCallbackCalled(const scheduler::TaskAttributionInfo& task,
+                                 EventScope::Type,
+                                 bool is_new_interaction);
+  void UserInitiatedInteraction();
+  void SetCurrentTimeAsStartTime();
+  void OnSoftNavigationEventScopeDestroyed();
 
   PerInteractionData* GetCurrentInteractionData(scheduler::TaskAttributionId);
+
+  // This must only be called when `all_event_parameters_` is non-empty.
+  const EventParameters& CurrentEventParameters() {
+    return all_event_parameters_.back();
+  }
 
   HeapHashSet<WeakMember<const scheduler::TaskAttributionInfo>>
       potential_soft_navigation_tasks_;
@@ -183,21 +196,14 @@ class CORE_EXPORT SoftNavigationHeuristics
   bool soft_navigation_conditions_met_ = false;
   bool paint_conditions_met_ = false;
   bool initial_interaction_encountered_ = false;
-  struct EventParameters {
-    explicit EventParameters() = default;
-    EventParameters(bool is_new_interaction, EventScope::Type type)
-        : is_new_interaction(is_new_interaction), type(type) {}
-
-    bool is_new_interaction = false;
-    EventScope::Type type = EventScope::Type::kClick;
-  };
-  EventParameters top_event_parameters_;
-  WTF::Deque<EventParameters> nested_event_parameters_;
-  EventParameters* current_event_parameters_ = nullptr;
+  // `SoftNavigationEventScope`s can be nested in case a click/keyboard event
+  // synchronously initiates a navigation. `all_event_parameters_` stores one
+  // `EventParameters` per scope, with the most recent one in the back.
+  WTF::Deque<EventParameters> all_event_parameters_;
   // Used to synchronize resetting the heuristic when
   // `potential_soft_navigation_tasks_` becomes empty during GC.
   bool has_potential_soft_navigation_task_ = false;
-  bool seen_first_observer = false;
+  bool seen_first_observer_ = false;
 };
 
 }  // namespace blink
