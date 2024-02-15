6b23f1a8fe2c4912ce284c0f4de05e8c3d922ed6
Andrey Kosyakov <caseq@chromium.org>
2024-02-15 17:24:35 +0000

    Do not use profile keepalives with OTR profiles
    ... due to a CHECK(!profile->IsOffTheRecord()) in ProfileManager::AddKeepAlive().
    
    Change-Id: If4ec0ab2859b91fff05809b571511d6afc0e6e06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5297252
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261150}

--- chrome/browser/headless/headless_mode_browsertest.h
+++ chrome/browser/headless/headless_mode_browsertest.h
@@ -34,6 +34,8 @@ class HeadlessModeBrowserTest : public InProcessBrowserTest {
   void SetUpOnMainThread() override;
 
  protected:
+  virtual bool IsIncognito();
+
   bool headful_mode() const { return headful_mode_; }
 
   void AppendHeadlessCommandLineSwitches(base::CommandLine* command_line);
