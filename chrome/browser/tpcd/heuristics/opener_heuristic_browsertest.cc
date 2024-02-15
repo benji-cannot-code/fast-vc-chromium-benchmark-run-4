b33ed288eaec9eb62450644bc751eea6a3cb25cd
Anton Maliev <amaliev@chromium.org>
2024-02-15 20:53:44 +0000

    Reland "Add AdTagged metric to popup/redirect heuristic cookie access UKMs."
    This reverts commit d150c0f9ec840c988f3fb0a6a914bf32bdf776ae.
    
    Reason for revert: Fixing broken `browser_tests_no_field_trial` CQ by initializing default features in tests.
    
    Original change's description:
    > Revert "Add AdTagged metric to popup/redirect heuristic cookie access UKMs."
    >
    > This reverts commit 1b9fe284d8c4d8b59f303790bae83ed243552c42.
    >
    > Reason for revert:
    > LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5652972228837376
    >
    > Sample build with failed test: https://ci.chromium.org/b/8756077425862210721
    > Affected test(s):
    > [ninja://chrome/test:browser_tests/OpenerHeuristicBrowserTest.PopupPastInteractionIsFollowedByPostPopupCookieAccess](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:browser_tests%2FOpenerHeuristicBrowserTest.PopupPastInteractionIsFollowedByPostPopupCookieAccess?q=VHash%3A6d0a43807ac672d9)
    >
    > If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5652972228837376&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F5272190&type=BUG
    >
    > Original change's description:
    > > Add AdTagged metric to popup/redirect heuristic cookie access UKMs.
    > >
    > > This will help estimate what % of cookies allowed by heuristics are then blocked by ad-tagging. We are collecting other metrics for ad-tagging on page load as well, but this is the only place we can derive the 3P site from the metrics, since we emit a linked UKM event on the 3P top level.
    > >
    > > UKM reviews:
    > > Opener/Popup: https://docs.google.com/document/d/1GJBMYuhl77eTSjLMMEtq1Y2Kd3Vdv1ozfdFi1iCueZI
    > > Redirect:
    > > https://docs.google.com/document/d/1dogmwtbUU0MEwL5RKICv47Ly_RnxBJhRuJHvMwsKrqs
    > >
    > > Bug: 324117304
    > > Change-Id: I88d10fe09e6bde0fd73b467a1ea3d738b48e305c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5272190
    > > Reviewed-by: Ryan Tarpine <rtarpine@chromium.org>
    > > Commit-Queue: Anton Maliev <amaliev@chromium.org>
    > > Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1260715}
    > >
    >
    > Bug: 324117304
    > Change-Id: I30a3d7804828b49da384f46a0e18a48dcb01e8b6
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5299036
    > Owners-Override: Robert Lin <robertlin@google.com>
    > Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    > Reviewed-by: Huanpo Lin <robertlin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1260888}
    
    Bug: 324117304
    Change-Id: I7b26e3f41188c3b32629d4568f2557150e01f321
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5301095
    Reviewed-by: Ryan Tarpine <rtarpine@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261285}

--- chrome/browser/tpcd/heuristics/opener_heuristic_browsertest.cc
+++ chrome/browser/tpcd/heuristics/opener_heuristic_browsertest.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/mock_host_resolver.h"
 #include "services/metrics/public/cpp/ukm_source.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
+#include "services/network/public/cpp/features.h"
 #include "third_party/blink/public/common/switches.h"
 #include "ui/base/window_open_disposition.h"
 
@@ -138,7 +139,9 @@ class OpenerHeuristicBrowserTest
 
     feature_list_.InitWithFeaturesAndParameters(
         {{content_settings::features::kTpcdHeuristicsGrants,
-          tpcd_heuristics_grants_params_}},
+          tpcd_heuristics_grants_params_},
+         {network::features::kSkipTpcdMitigationsForAds,
+          {{"SkipTpcdMitigationsForAdsHeuristics", "true"}}}},
         // Disable tracking protection by default to test third-party cookie
         // behavior for PostPopupCookieAccess events.
         {content_settings::features::kTrackingProtection3pcd});
@@ -156,9 +159,10 @@ class OpenerHeuristicBrowserTest
     SubresourceFilterBrowserTest::SetUpOnMainThread();
 
     // These rules apply an ad-tagging param to scripts in ad_script.js,
-    // including `windowOpenFromAdScript`.
+    // and to cookies marked with the `isad=1` param value.
     SetRulesetWithRules(
-        {subresource_filter::testing::CreateSuffixRule("ad_script.js")});
+        {subresource_filter::testing::CreateSuffixRule("ad_script.js"),
+         subresource_filter::testing::CreateSuffixRule("isad=1")});
 
     DIPSService::Get(GetActiveWebContents()->GetBrowserContext())
         ->SetStorageClockForTesting(&clock_);
@@ -750,23 +754,26 @@ IN_PROC_BROWSER_TEST_F(OpenerHeuristicBrowserTest,
   // Add a cookie access by popup_url on opener_url.
   ASSERT_TRUE(NavigateToSetCookie(GetActiveWebContents(), &https_server,
                                   "sub.b.test",
-                                  /*is_secure_cookie_set=*/true));
+                                  /*is_secure_cookie_set=*/true,
+                                  /*is_ad_tagged=*/true));
   ASSERT_TRUE(content::NavigateToURL(GetActiveWebContents(), opener_url));
   CreateImageAndWaitForCookieAccess(
       GetActiveWebContents(),
-      https_server.GetURL("sub.b.test", "/favicon/icon.png"));
+      https_server.GetURL("sub.b.test", "/favicon/icon.png?isad=1"));
   GetDipsService()->storage()->FlushPostedTasksForTesting();
 
   // Assert that the UKM event for the PostPopupCookieAccess was recorded.
   auto access_entries = ukm_recorder.GetEntries(
       "OpenerHeuristic.PostPopupCookieAccess",
-      {"AccessId", "AccessSucceeded", "HoursSincePopupOpened"});
+      {"AccessId", "AccessSucceeded", "IsAdTagged", "HoursSincePopupOpened"});
   ASSERT_EQ(access_entries.size(), 1u);
   EXPECT_EQ(
       ukm_recorder.GetSourceForSourceId(access_entries[0].source_id)->url(),
       opener_url);
   EXPECT_EQ(access_entries[0].metrics["AccessId"], access_id);
   EXPECT_EQ(access_entries[0].metrics["AccessSucceeded"], true);
+  EXPECT_EQ(access_entries[0].metrics["IsAdTagged"],
+            static_cast<int32_t>(OptionalBool::kTrue));
   EXPECT_EQ(access_entries[0].metrics["HoursSincePopupOpened"], 0);
 }
 
@@ -1012,7 +1019,8 @@ IN_PROC_BROWSER_TEST_F(
   // Add a cookie access by popup_url on opener_url.
   ASSERT_TRUE(NavigateToSetCookie(GetActiveWebContents(), &https_server,
                                   "sub.b.test",
-                                  /*is_secure_cookie_set=*/true));
+                                  /*is_secure_cookie_set=*/true,
+                                  /*is_ad_tagged=*/false));
   ASSERT_TRUE(content::NavigateToURL(GetActiveWebContents(), opener_url));
   CreateImageAndWaitForCookieAccess(
       GetActiveWebContents(),
@@ -1022,13 +1030,15 @@ IN_PROC_BROWSER_TEST_F(
   // Assert that the UKM event for the PostPopupCookieAccess was recorded.
   auto access_entries = ukm_recorder.GetEntries(
       "OpenerHeuristic.PostPopupCookieAccess",
-      {"AccessId", "AccessSucceeded", "HoursSincePopupOpened"});
+      {"AccessId", "AccessSucceeded", "IsAdTagged", "HoursSincePopupOpened"});
   ASSERT_EQ(access_entries.size(), 1u);
   EXPECT_EQ(
       ukm_recorder.GetSourceForSourceId(access_entries[0].source_id)->url(),
       opener_url);
   EXPECT_EQ(access_entries[0].metrics["AccessId"], access_id);
   EXPECT_EQ(access_entries[0].metrics["AccessSucceeded"], true);
+  EXPECT_EQ(access_entries[0].metrics["IsAdTagged"],
+            static_cast<int32_t>(OptionalBool::kFalse));
   EXPECT_EQ(access_entries[0].metrics["HoursSincePopupOpened"], 0);
 }
 
