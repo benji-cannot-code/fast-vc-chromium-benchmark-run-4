7a3021cff900ef0623c3d0f059dc2fc379ddabae
Adem Derinel <derinel@google.com>
2024-02-15 08:04:18 +0000

    [WebAuthn] Clean up WebAuthenticationAndroidCredManForHybrid flag
    Bug: b/40275628
    Change-Id: I4b12c3c184b8bf4f1302af7a501dc9b3fd27e36f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5285935
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1260928}

--- components/webauthn/android/junit/src/org/chromium/components/webauthn/Fido2CredentialRequestRobolectricTest.java
+++ components/webauthn/android/junit/src/org/chromium/components/webauthn/Fido2CredentialRequestRobolectricTest.java
@@ -220,8 +220,6 @@ public class Fido2CredentialRequestRobolectricTest {
 
         FeatureList.TestValues testValues = new FeatureList.TestValues();
         testValues.addFeatureFlagOverride(DeviceFeatureList.WEBAUTHN_ANDROID_CRED_MAN, false);
-        testValues.addFeatureFlagOverride(
-                DeviceFeatureList.WEBAUTHN_ANDROID_CRED_MAN_FOR_HYBRID, true);
         FeatureList.setTestValues(testValues);
 
         final byte[] clientDataHash = new byte[] {1, 2, 3};
@@ -436,8 +434,6 @@ public class Fido2CredentialRequestRobolectricTest {
 
         FeatureList.TestValues testValues = new FeatureList.TestValues();
         testValues.addFeatureFlagOverride(DeviceFeatureList.WEBAUTHN_ANDROID_CRED_MAN, false);
-        testValues.addFeatureFlagOverride(
-                DeviceFeatureList.WEBAUTHN_ANDROID_CRED_MAN_FOR_HYBRID, true);
         FeatureList.setTestValues(testValues);
 
         mRequest.setIsHybridRequest(true);
