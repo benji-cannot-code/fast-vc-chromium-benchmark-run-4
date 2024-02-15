b0a9d6b50c496b62b9feff51afa7e123ffbcb0ab
Mitsuru Oshima <oshima@chromium.org>
2024-02-15 17:55:53 +0000

    Do not reconfigure if the unified desktop enabled flag hasn't changed
    Bug: 207087494
    Change-Id: Ieeb6291480d927152837d34b1693d80071c73d1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5278685
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Auto-Submit: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261168}

--- chrome/browser/ash/display/display_prefs_browsertest.cc
+++ chrome/browser/ash/display/display_prefs_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "ash/constants/ash_pref_names.h"
+#include "ash/constants/ash_switches.h"
 #include "ash/shell.h"
 #include "base/memory/raw_ptr.h"
 #include "base/run_loop.h"
@@ -24,6 +25,15 @@ class DisplayPrefsBrowserTest : public InProcessBrowserTest {
 
   ~DisplayPrefsBrowserTest() override = default;
 
+  // InProcessBrowserTest:
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    std::string test_name =
+        ::testing::UnitTest::GetInstance()->current_test_info()->name();
+    // Make sure that display prefs are created in PRE_ test.
+    if (test_name.find("PRE_") != std::string::npos) {
+      command_line->AppendSwitch(ash::switches::kFirstExecAfterBoot);
+    }
+  }
   void SetUpOnMainThread() override {
     local_state_ = g_browser_process->local_state();
   }
