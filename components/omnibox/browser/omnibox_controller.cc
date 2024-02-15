2043ebd30d9f867e679048f038845da5034cb2b9
Angela Yoeurng <yoangela@chromium.org>
2024-02-15 05:33:08 +0000

    [omnibox] Update UI for starter pack expansion.
    This CL includes some UI polish for the new "Ask Google" starter pack
    engine.
    
    1. The text on lhs of the omnibox in keyword mode now only shoes the
    name ("Search Ask Google" -> "Ask Google")
    
    2. The mirror query is no longer shown when in the "Ask Google" mode.
    
    Screenshots:
    Before: https://screenshot.googleplex.com/5kUi55iPhTDnY7T.png
    After: https://screenshot.googleplex.com/84Y9daeY8PUTbBU.png
    
    Bug: 1521556
    Change-Id: I474994a0960c0b6bb014b5d3f5859fd044649c42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5296788
    Commit-Queue: Angela Yoeurng <yoangela@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260902}

--- components/omnibox/browser/omnibox_controller.cc
+++ components/omnibox/browser/omnibox_controller.cc
@@ -11,13 +11,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/autocomplete_classifier.h"
 #include "components/omnibox/browser/autocomplete_controller_emitter.h"
 #include "components/omnibox/browser/autocomplete_match.h"
+#include "components/omnibox/browser/autocomplete_match_type.h"
 #include "components/omnibox/browser/location_bar_model.h"
 #include "components/omnibox/browser/omnibox_client.h"
 #include "components/omnibox/browser/omnibox_edit_model.h"
+#include "components/omnibox/browser/omnibox_field_trial.h"
 #include "components/omnibox/browser/omnibox_popup_selection.h"
 #include "components/omnibox/browser/omnibox_popup_view.h"
 #include "components/omnibox/browser/omnibox_prefs.h"
 #include "components/omnibox/browser/page_classification_functions.h"
+#include "components/search_engines/template_url_starter_pack_data.h"
 #include "ui/gfx/geometry/rect.h"
 
 OmniboxController::OmniboxController(OmniboxView* view,
@@ -166,6 +169,20 @@ std::u16string OmniboxController::GetHeaderForSuggestionGroup(
     omnibox::GroupId suggestion_group_id) const {
   return autocomplete_controller_->result().GetHeaderForSuggestionGroup(
       suggestion_group_id);
+}
+
+bool OmniboxController::IsSuggestionHidden(
+    const AutocompleteMatch& match) const {
+  if (OmniboxFieldTrial::IsStarterPackExpansionEnabled() &&
+      match.from_keyword) {
+    TemplateURL* turl =
+        match.GetTemplateURL(client_->GetTemplateURLService(), false);
+    if (turl &&
+        turl->starter_pack_id() == TemplateURLStarterPackData::kAskGoogle) {
+      return true;
+    }
+  }
+  return false;
 }
 
 bool OmniboxController::IsSuggestionGroupHidden(
