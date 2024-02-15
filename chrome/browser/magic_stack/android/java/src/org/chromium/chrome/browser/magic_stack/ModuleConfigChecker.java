451d18a8383d0de4fe7485a2bd490107dc024611
Xi Han <hanxi@google.com>
2024-02-15 20:44:44 +0000

    [MagicStack] Add ModuleConfigChecker.
    Introduce a new interface to split the eligibility and configurable
    check APIs from ModuleProviderBuilder interface.
    
    Bug: b/324312039
    Change-Id: Ifddde4de15e8eb9a6c5fbc08cb88bfe1a875c737
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5300836
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261277}

---
+++ chrome/browser/magic_stack/android/java/src/org/chromium/chrome/browser/magic_stack/ModuleConfigChecker.java
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.magic_stack;
+
+/** An interface to check module's configuration. */
+public interface ModuleConfigChecker {
+    /**
+     * Returns whether the module can be built due to special restrictions, like location. This
+     * method should be called after profile is initialized.
+     */
+    boolean isEligible();
+
+    /** Returns whether the module can be turned on or off in settings. */
+    default boolean isConfigurable() {
+        return true;
+    }
+}
