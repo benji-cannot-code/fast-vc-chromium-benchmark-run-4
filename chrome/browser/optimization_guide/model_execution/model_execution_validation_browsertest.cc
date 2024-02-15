fb99a6e98b4d080511ecf8f9acaf8715d7adee47
Huanpo Lin <robertlin@chromium.org>
2024-02-15 00:49:25 +0000

    Revert "Deflake model validation browser test"
    This reverts commit 796a33326e92a9da9b867bffc5ec117700037afe.
    
    Reason for revert:
    It seems that 
    ModelExecutionValidationBrowserTest.ModelExecutionFailsServerFailure and 
    ModelExecutionValidationBrowserTest.ModelExecutionSuccess are failing after this CL.
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac13%20Tests/9157/overview
    
    Original change's description:
    > Deflake model validation browser test
    >
    > Add wait based on runloop before waiting for the UMA
    >
    > Change-Id: I6fffc2469834923e7b2dc3b9d8548d7b73f91dc3
    > Bug: b/41493340
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5294954
    > Commit-Queue: Raj T <rajendrant@chromium.org>
    > Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1260618}
    
    Bug: b/41493340
    Change-Id: Ie90d477bbab680d4445b745fa1bc7d353c2b458d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5297355
    Owners-Override: Robert Lin <robertlin@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260819}

--- chrome/browser/optimization_guide/model_execution/model_execution_validation_browsertest.cc
+++ chrome/browser/optimization_guide/model_execution/model_execution_validation_browsertest.cc
@@ -3,9 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <memory>
-
-#include "base/run_loop.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "build/chromeos_buildflags.h"
@@ -115,7 +112,6 @@ class ModelExecutionValidationBrowserTestBase : public InProcessBrowserTest {
 
     if (should_server_fail_model_execution_) {
       response->set_code(net::HTTP_NOT_FOUND);
-      model_execution_request_run_loop_->Quit();
       return std::move(response);
     }
 
@@ -131,7 +127,6 @@ class ModelExecutionValidationBrowserTestBase : public InProcessBrowserTest {
     execute_response.SerializeToString(&serialized_response);
     response->set_code(net::HTTP_OK);
     response->set_content(serialized_response);
-    model_execution_request_run_loop_->Quit();
     return std::move(response);
   }
 
@@ -150,9 +145,6 @@ class ModelExecutionValidationBrowserTestBase : public InProcessBrowserTest {
   std::unique_ptr<IdentityTestEnvironmentProfileAdaptor>
       identity_test_env_adaptor_;
   base::CallbackListSubscription create_services_subscription_;
-
-  // RunLoop to wait for model execution request to be received.
-  std::unique_ptr<base::RunLoop> model_execution_request_run_loop_;
 };
 
 class ModelExecutionValidationBrowserTest
@@ -173,10 +165,7 @@ class ModelExecutionValidationBrowserTest
 #endif
 IN_PROC_BROWSER_TEST_F(ModelExecutionValidationBrowserTest,
                        MAYBE_ModelExecutionSuccess) {
-  model_execution_request_run_loop_ = std::make_unique<base::RunLoop>();
   EnableSignin();
-
-  model_execution_request_run_loop_->Run();
   RetryForHistogramUntilCountReached(
       &histogram_tester_, "OptimizationGuide.ModelExecution.Result.Test", 1);
 
@@ -196,11 +185,8 @@ IN_PROC_BROWSER_TEST_F(ModelExecutionValidationBrowserTest,
 #endif
 IN_PROC_BROWSER_TEST_F(ModelExecutionValidationBrowserTest,
                        MAYBE_ModelExecutionFailsServerFailure) {
-  model_execution_request_run_loop_ = std::make_unique<base::RunLoop>();
   EnableServerModelExecutionFailure();
   EnableSignin();
-
-  model_execution_request_run_loop_->Run();
   RetryForHistogramUntilCountReached(
       &histogram_tester_, "OptimizationGuide.ModelExecution.Result.Test", 1);
 
