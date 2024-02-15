f4110333c89c1610c485489f1bbff17c7f5b462e
Jan Keitel <jkeitel@google.com>
2024-02-15 09:35:36 +0000

    Add Document::GetTopLevelForms.
    GetTopLevelForms returns all (shadow-tree-including) HTMLFormElements
    that do not have (shadow-tree-including) ancestors that are also
    form elements.
    
    This method will be used by Autofill to better support form filling
    for Shadow DOM elements.
    
    While the eventual use of the method will be
    feature-gated, the code additions in this CL are not. The only point
    at which the feature touches code that is currently used in production
    is in HTMLFormElement::InsertedInto and HTMLFormElement::RemovedFrom:
    These two methods now mark the top level form cache as dirty. Since
    there are no calls to GetTopLevelForms, these calls are essentially
    no-ops.
    
    Bug: b/40204601
    Change-Id: I2ef735f38c791d69fc9938eb217af2f9185150a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5279686
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260952}

--- third_party/blink/renderer/core/dom/document.h
+++ third_party/blink/renderer/core/dom/document.h
@@ -570,6 +570,13 @@ class CORE_EXPORT Document : public ContainerNode,
   const ListedElement::List& UnassociatedListedElements() const;
   void MarkUnassociatedListedElementsDirty();
 
+  // Returns all `HTMLFormElement`s that have no shadow-including
+  // `HTMLFormElement` ancestor. Note that the form elements are returned in BFS
+  // order.
+  const HeapVector<Member<HTMLFormElement>>& GetTopLevelForms();
+  // Invalidates the cache for top level form elements.
+  void MarkTopLevelFormsDirty();
+
   // "defaultView" attribute defined in HTML spec.
   DOMWindow* defaultView() const;
 
@@ -2114,6 +2121,20 @@ class CORE_EXPORT Document : public ContainerNode,
     bool dirty_ = false;
   };
 
+  // Helper class to cache the top level <form> elements of a document.
+  class TopLevelFormsList {
+    DISALLOW_NEW();
+
+   public:
+    void MarkDirty();
+    const HeapVector<Member<HTMLFormElement>>& Get(Document& owner);
+    void Trace(Visitor*) const;
+
+   private:
+    HeapVector<Member<HTMLFormElement>> list_;
+    bool dirty_ = false;
+  };
+
   friend class AXContext;
   void AddAXContext(AXContext*);
   void RemoveAXContext(AXContext*);
@@ -2650,6 +2671,8 @@ class CORE_EXPORT Document : public ContainerNode,
   Member<PropertyRegistry> property_registry_;
 
   UnassociatedListedElementsList unassociated_listed_elements_;
+
+  TopLevelFormsList top_level_forms_;
 
   // |ukm_recorder_| and |source_id_| will allow objects that are part of
   // the document to record UKM.
--- third_party/blink/renderer/core/html/forms/html_form_element.cc
+++ third_party/blink/renderer/core/html/forms/html_form_element.cc
@@ -139,6 +139,7 @@ Node::InsertionNotificationRequest HTMLFormElement::InsertedInto(
   LogAddElementIfIsolatedWorldAndInDocument("form", html_names::kMethodAttr,
                                             html_names::kActionAttr);
   if (insertion_point.isConnected()) {
+    GetDocument().MarkTopLevelFormsDirty();
     GetDocument().DidChangeFormRelatedElementDynamically(
         this, WebFormRelatedChangeType::kAdd);
   }
@@ -184,6 +185,7 @@ void HTMLFormElement::RemovedFrom(ContainerNode& insertion_point) {
   HTMLElement::RemovedFrom(insertion_point);
 
   if (insertion_point.isConnected()) {
+    GetDocument().MarkTopLevelFormsDirty();
     GetDocument().DidChangeFormRelatedElementDynamically(
         this, WebFormRelatedChangeType::kRemove);
   }
