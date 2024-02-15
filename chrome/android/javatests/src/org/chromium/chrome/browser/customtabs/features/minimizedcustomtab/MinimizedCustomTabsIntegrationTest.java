09ab20db4197f44d3f45f84d443e19610d44d385
Tommy Nyquist <nyquist@chromium.org>
2024-02-15 22:22:43 +0000

    Disable newly added MinimizedCustomTabsIntegrationTest
    The test was recently added here:
    https://chromium-review.googlesource.com/c/chromium/src/+/5147036
    
    Bug: b/325487558
    Change-Id: I8f9175d1f4c638ff329392f5eb3c9348ea51e9c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5299861
    Reviewed-by: Kristian Monsen <kristianm@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261342}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/features/minimizedcustomtab/MinimizedCustomTabsIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/features/minimizedcustomtab/MinimizedCustomTabsIntegrationTest.java
@@ -30,6 +30,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.Restriction;
@@ -72,6 +73,7 @@ public class MinimizedCustomTabsIntegrationTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "b/325487558")
     public void testMinimize() {
         var intent = createIntent();
         mCustomTabActivityTestRule.startCustomTabActivityWithIntent(intent);
