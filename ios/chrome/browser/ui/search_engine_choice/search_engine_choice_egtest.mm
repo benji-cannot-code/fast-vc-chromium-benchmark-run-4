74b77dd4161158932cf17156fc2223e19f9d1d3a
Federica Germinario <fedegermi@google.com>
2024-02-15 15:36:38 +0000

    [ios] Disable tests failing on device in SearchEngineSettingsTestCase
    This CL disables the following egtests on device:
     - SearchEngineTestCase/testOpenSecondWindow
     - SearchEngineTestCase/testSearchEngineChoiceScreenScrollThenSelect
     - SearchEngineTestCase/testSearchEngineChoiceScreenSelectThenScroll
    Tests are consistently failing.
    
    Bug: 325441139
    Change-Id: I7522c895e28afb47602ede9d217fb28593636ee4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5300417
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1261061}

--- ios/chrome/browser/ui/search_engine_choice/search_engine_choice_egtest.mm
+++ ios/chrome/browser/ui/search_engine_choice/search_engine_choice_egtest.mm
@@ -65,9 +65,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [SearchEngineChoiceEarlGreyUI verifySearchEngineChoiceScreenIsDisplayed];
 }
 
+// TODO(b/325441139): Test fails on device.
+#if !TARGET_IPHONE_SIMULATOR
+#define MAYBE_testOpenSecondWindow DISABLED_testOpenSecondWindow
+#else
+#define MAYBE_testOpenSecondWindow testOpenSecondWindow
+#endif
 // Tests that search engine choice dialog is moved to the other active scene
 // when the current scene is removed.
-- (void)testOpenSecondWindow {
+- (void)MAYBE_testOpenSecondWindow {
   if (![ChromeEarlGrey areMultipleWindowsSupported]) {
     EARL_GREY_TEST_DISABLED(@"Multiple windows can't be opened.");
   }
@@ -80,10 +86,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [SearchEngineChoiceEarlGreyUI verifySearchEngineChoiceScreenIsDisplayed];
 }
 
+// TODO(b/325441139): Test fails on device.
+#if !TARGET_IPHONE_SIMULATOR
+#define MAYBE_testSearchEngineChoiceScreenSelectThenScroll \
+  DISABLED_testSearchEngineChoiceScreenSelectThenScroll
+#else
+#define MAYBE_testSearchEngineChoiceScreenSelectThenScroll \
+  testSearchEngineChoiceScreenSelectThenScroll
+#endif
 // Tests that the Search Engine Choice screen is displayed, that the primary
 // button is correctly updated when the user selects a search engine then
 // scrolls down and that it correctly sets the default search engine.
-- (void)testSearchEngineChoiceScreenSelectThenScroll {
+- (void)MAYBE_testSearchEngineChoiceScreenSelectThenScroll {
   // Checks that the choice screen is shown
   [SearchEngineChoiceEarlGreyUI verifySearchEngineChoiceScreenIsDisplayed];
   // Checks that the fake omnibox illustration is displayed and is initially
@@ -116,10 +130,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       verifyDefaultSearchEngineSetting:searchEngineToSelect];
 }
 
+// TODO(b/325441139): Test fails on device.
+#if !TARGET_IPHONE_SIMULATOR
+#define MAYBE_testSearchEngineChoiceScreenScrollThenSelect \
+  DISABLED_testSearchEngineChoiceScreenScrollThenSelect
+#else
+#define MAYBE_testSearchEngineChoiceScreenScrollThenSelect \
+  testSearchEngineChoiceScreenScrollThenSelect
+#endif
 // Tests that the Search Engine Choice screen is displayed, that the
 // primary button is correctly updated when the user scrolls down then selects a
 // search engine and that it correctly sets the default search engine.
-- (void)testSearchEngineChoiceScreenScrollThenSelect {
+- (void)MAYBE_testSearchEngineChoiceScreenScrollThenSelect {
   // Checks that the choice screen is shown
   [SearchEngineChoiceEarlGreyUI verifySearchEngineChoiceScreenIsDisplayed];
   // Checks that the fake omnibox illustration is displayed and is initially
