e95829d451884ce603310788e6f5b10785a7c0d8
Arthur Milchior <arthurmilchior@chromium.org>
2024-02-15 16:26:22 +0000

    [iOS] Sign-In without using UI
    This slightly speed-up tests.
    Note that `[SigninEarlGrey signinWithFakeIdentity:identity]` only
    sign-in. Hence it as the same behavior as
    `[SigninEarlGreyUI signinWithFakeIdentity:identity enableSync:NO]`.
    
    Bug: 325235662
    Change-Id: If66748a4e986544d1fb98a5e35839f32c95e2e3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5296355
    Reviewed-by: Tomek Jurkiewicz <tju@google.com>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261097}

--- ios/chrome/browser/supervised_user/model/supervised_user_incognito_mode_egtest.mm
+++ ios/chrome/browser/supervised_user/model/supervised_user_incognito_mode_egtest.mm
@@ -48,7 +48,7 @@ NSString* const kTestLearnMoreLabel = @"Learn more";
   [SigninEarlGrey addFakeIdentity:fakeIdentity];
   [SigninEarlGrey setIsSubjectToParentalControls:YES forIdentity:fakeIdentity];
 
-  [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity enableSync:NO];
+  [SigninEarlGrey signinWithFakeIdentity:fakeIdentity];
   [SigninEarlGrey verifySignedInWithFakeIdentity:fakeIdentity];
 }
 
