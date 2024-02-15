65d41856f6814e048f872769c363324538739cd2
Kevin McNee <mcnee@chromium.org>
2024-02-15 18:00:45 +0000

    Improve performance of CreateAnchorElementMetrics
    We have several cases of unnecessary string copies. When determining
    whether an anchor has text siblings, we compute `wholeText` only to
    check if it's empty. We now inline the traversal done by `wholeText`
    without actually building the string. `IsSameHost` compares the
    KURL::Host of two URLs. We can use a StringView here. To do so, we add
    an equivalent StringView method for the host to KURL.
    
    Some computations are duplicated between CreateAnchorElementMetrics
    and its caller. We move the logic for skipping metrics for a given
    anchor into CreateAnchorElementMetrics and have a null return value
    indicate that it should be skipped. In the main frame case, we
    compute two bounding boxes--with and without overflow, the former for
    size metrics computation and the latter for skipping empty anchors.
    These bounding box computations are rather expensive, so we use the
    overflow version for the empty anchor check as well. For the subframe
    case, the overflow version isn't otherwise needed, so we calculate
    the bounding box without overflow for the empty check as this version
    is faster to compute.
    
    From local testing on my workstation, together these changes take
    the computation of metrics on the youtube home page from 9.7ms to
    5.5ms.
    
    Bug: 40258405
    Change-Id: Ia3e4f71779dce7c9a271d7a5bd1fe4e0da8c6c8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5275282
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261173}

--- third_party/blink/renderer/platform/weborigin/kurl.h
+++ third_party/blink/renderer/platform/weborigin/kurl.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <iosfwd>
 #include <memory>
+#include "third_party/abseil-cpp/absl/base/attributes.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/cross_thread_copier.h"
@@ -139,6 +140,7 @@ class PLATFORM_EXPORT KURL {
 
   String Protocol() const;
   String Host() const;
+  StringView HostView() const ABSL_ATTRIBUTE_LIFETIME_BOUND;
 
   // Returns 0 when there is no port or the default port was specified, or the
   // URL is invalid.
