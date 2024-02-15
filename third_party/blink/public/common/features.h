1238315136c5333bd2223f83ee894cae7ed584e3
Jan Keitel <jkeitel@google.com>
2024-02-15 09:43:51 +0000

    Add feature to include all listed element descendants of a form.
    Currently, listed element extraction has a parameter to include
    elements in shadow DOM. If that parameter is set, listed elements that
    are descendants of the queried form are extracted regardless of whether
    they are in shadow DOM are not. However, they are extracted iff
    the queried form is the closest shadow-dom-including ancestor form.
    
    This CL adds a feature that modifies this last condition slightly: If
    the feature is enabled, listed element descendants are extracted
    iff the queried form is a shadow-dom-including ancestor form - it
    no longer needs to be the nearest ancestor.
    
    The motivation behind this change is that Autofill already ignores
    nested forms: Nesting can only (legitimately) happen if the nested form
    elements are inside Shadow DOM and Shadow DOM form elements are
    currently ignored by Autofill.
    In the future, Autofill will extract all forms without form ancestors
    and will expect that their associated form control elements contain
    all shadow-dom-including descendants. This feature does that.
    
    Bug: b/40204601
    Change-Id: Ifaa803471c3be12cfb0e1981ed66396a266eecf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5279689
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1260954}

--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -115,6 +115,9 @@ BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE(kAndroidExtendedKeyboardShortcuts);
 BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE(kAudioWorkletThreadRealtimePriority);
 
 BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE(
+    kAutofillIncludeFormElementsInShadowDom);
+
+BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE(
     kAutofillIncludeShadowDomInUnassociatedListedElements);
 
 BLINK_COMMON_EXPORT
