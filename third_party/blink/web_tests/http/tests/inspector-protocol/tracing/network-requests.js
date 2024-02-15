fce7ac2cea79cc071c8243405cf2a98decae194a
Adam Raine <asraine@chromium.org>
2024-02-15 02:42:49 +0000

    [trace-events] Add fetch priority hint to network requests
    Bug: 324059246
    Change-Id: Iebb2354272228bd4e740cba171b29d4a38c34ad9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5297615
    Commit-Queue: Adam Raine <asraine@chromium.org>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260868}

--- third_party/blink/web_tests/http/tests/inspector-protocol/tracing/network-requests.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/tracing/network-requests.js
@@ -49,7 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   for (const key of orderedKeys) {
     const events = eventsByRequestId.get(key);
     for (const event of events) {
-      tracingHelper.logEventShape(event, [], ['name', 'resourceType', 'isLinkPreload'])
+      tracingHelper.logEventShape(event, [], ['name', 'resourceType', 'isLinkPreload', 'fetchPriorityHint'])
     }
   }
 
