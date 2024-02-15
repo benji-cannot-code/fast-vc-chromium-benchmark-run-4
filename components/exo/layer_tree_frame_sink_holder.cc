3f9d52a6e8b4537a9360cd847eb7bfe06be2a3ce
Yuzhu Shen <yzshen@chromium.org>
2024-02-15 21:42:00 +0000

    Exo: add two UMA histograms for frame submission latency.
    BUG=b:41497105
    
    Change-Id: I06d1567cb65ff253b2f19f3429ec66dbf39b3da6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5298100
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261312}

--- components/exo/layer_tree_frame_sink_holder.cc
+++ components/exo/layer_tree_frame_sink_holder.cc
@@ -119,9 +119,13 @@ void LayerTreeFrameSinkHolder::SubmitCompositorFrame(viz::CompositorFrame frame,
     return;
   }
 
-  frame_timing_history_->MayRecordDidNotProduceToFrameArrvial(/*valid=*/true);
-
   DiscardCachedFrame(&frame);
+
+  // Needs to be after DiscardCachedFrame(), because discarding a frame will
+  // reset the frame arrival information in `frame_timing_history_`.
+  frame_timing_history_->FrameArrived();
+
+  frame_timing_history_->MayRecordDidNotProduceToFrameArrvial(/*valid=*/true);
 
   ObserveBeginFrameSource(true);
 
@@ -299,6 +303,7 @@ bool LayerTreeFrameSinkHolder::OnBeginFrameDerivedImpl(
     const viz::BeginFrameArgs& args) {
   DCHECK(reactive_frame_submission_);
 
+  frame_timing_history_->BeginFrameArrived(args.frame_id);
   frame_timing_history_->MayRecordDidNotProduceToFrameArrvial(/*valid=*/false);
 
   pending_begin_frames_.emplace();
@@ -333,8 +338,8 @@ void LayerTreeFrameSinkHolder::SubmitCompositorFrameToRemote(
   DCHECK(!is_lost_);
 
   if (frame_timing_history_) {
-    frame_timing_history_->FrameSubmitted(frame->metadata.frame_token,
-                                          base::TimeTicks::Now());
+    frame_timing_history_->FrameSubmitted(
+        frame->metadata.begin_frame_ack.frame_id, frame->metadata.frame_token);
   }
 
   last_frame_resources_.clear();
@@ -442,9 +447,10 @@ void LayerTreeFrameSinkHolder::OnSendDeadlineExpired(bool update_timer) {
     frame_sink_->DidNotProduceFrame(pending_begin_frame.begin_frame_ack,
                                     cc::FrameSkippedReason::kNoDamage);
 
-    pending_begin_frames_.pop();
+    frame_timing_history_->FrameDidNotProduce(
+        pending_begin_frame.begin_frame_ack.frame_id);
 
-    frame_timing_history_->FrameDidNotProduce();
+    pending_begin_frames_.pop();
 
     bool should_pause_begin_frame =
         frame_sink_->auto_needs_begin_frame() &&
