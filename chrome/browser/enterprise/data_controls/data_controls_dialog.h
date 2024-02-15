92a172760e418c9624a94648069a0c9b4c9e33c4
Dominique Fauteux-Chapleau <domfc@chromium.org>
2024-02-15 21:37:10 +0000

    Add logic to not show repetitive Data Controls dialogs on the same tab
    This CL fixes b/324574631 by keeping track of the currently showing Data
    Controls dialog on each tab before showing a new identical one, and
    simply forwarding the callback passed in `Show` to the existing dialog
    if one is present.
    
    Bug: b/324574631
    Change-Id: I26a462bde55ef190d3744d0c46e590b9161f7041
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5282463
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261308}

--- chrome/browser/enterprise/data_controls/data_controls_dialog.h
+++ chrome/browser/enterprise/data_controls/data_controls_dialog.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ENTERPRISE_DATA_CONTROLS_DATA_CONTROLS_DIALOG_H_
 #define CHROME_BROWSER_ENTERPRISE_DATA_CONTROLS_DATA_CONTROLS_DIALOG_H_
 
+#include <vector>
+
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "components/enterprise/data_controls/rule.h"
@@ -94,6 +96,7 @@ class DataControlsDialog : public views::DialogDelegate {
 
  private:
   DataControlsDialog(Type type,
+                     content::WebContents* web_contents,
                      base::OnceCallback<void(bool bypassed)> callback);
 
   // Helpers to create sub-views of the dialog.
@@ -101,11 +104,12 @@ class DataControlsDialog : public views::DialogDelegate {
   std::unique_ptr<views::Label> CreateMessage() const;
 
   Type type_;
+  raw_ptr<content::WebContents> web_contents_ = nullptr;
   raw_ptr<views::BoxLayoutView> contents_view_ = nullptr;
 
   // Called when the dialog closes, with `true` in the case of a bypassed
   // warning.
-  base::OnceCallback<void(bool bypassed)> callback_;
+  std::vector<base::OnceCallback<void(bool bypassed)>> callbacks_;
 };
 
 }  // namespace data_controls
