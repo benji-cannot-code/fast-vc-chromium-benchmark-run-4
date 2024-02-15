2cc2fcc527df2fbf4dc3bdac4947ac6ab1d6006e
Arthur Milchior <arthurmilchior@chromium.org>
2024-02-15 16:36:18 +0000

    [iOS] Sign-In without using UI
    This slightly speed-up tests.
    
    Bug: 325235662
    Change-Id: I70ca55ca4f1bdaa80715e1ad13601aa951432b34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5296216
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261109}

--- ios/chrome/browser/ui/content_suggestions/ntp_home_egtest.mm
+++ ios/chrome/browser/ui/content_suggestions/ntp_home_egtest.mm
@@ -1289,7 +1289,7 @@ id<GREYMatcher> mostlyNotVisible() {
 
   FakeSystemIdentity* identity = [FakeSystemIdentity fakeIdentity1];
   [SigninEarlGrey addFakeIdentity:identity];
-  [SigninEarlGreyUI signinWithFakeIdentity:identity];
+  [SigninEarlGrey signinWithFakeIdentity:identity];
   GREYWaitForAppToIdle(@"App failed to idle");
 
   // Verify Identity Disc is visible since it is the top-most element and should
@@ -1558,7 +1558,7 @@ id<GREYMatcher> mostlyNotVisible() {
   [SigninEarlGrey addFakeIdentity:identity];
   [SigninEarlGrey setIsSubjectToParentalControls:YES forIdentity:identity];
 
-  [SigninEarlGreyUI signinWithFakeIdentity:identity];
+  [SigninEarlGrey signinWithFakeIdentity:identity];
 
   // Check feed label and if NTP is scrollable.
   [[EarlGrey selectElementWithMatcher:chrome_test_util::DiscoverHeaderLabel()]
