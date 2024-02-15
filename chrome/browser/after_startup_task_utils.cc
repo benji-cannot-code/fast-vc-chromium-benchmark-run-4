90d34fff2c45683b46fb5233c2c29fa8da3c58c0
Chris Davis (EDGE) <chrdavis@microsoft.com>
2024-02-15 18:46:01 +0000

    Change startup complete trace event to instant event
    Perfetto legacy etw events are not logging TRACE_EVENT1 events but they
    are logging instant events.  Changing this event to an instant event to
    unblock benchmarks that depend on this event in etl traces.
    
    Bug: 40229447
    Change-Id: Idfca51c44a74d1fca00b7ac3f7e2f6687bb25dff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5282916
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Chris Davis <chrdavis@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1261206}

--- chrome/browser/after_startup_task_utils.cc
+++ chrome/browser/after_startup_task_utils.cc
@@ -6,15 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/after_startup_task_utils.h"
 
 #include "base/containers/circular_deque.h"
-#include "base/lazy_instance.h"
 #include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/no_destructor.h"
 #include "base/process/process.h"
 #include "base/synchronization/atomic_flag.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
+#include "chrome/browser/ui/browser_finder.h"
 #include "components/performance_manager/performance_manager_impl.h"
 #include "components/performance_manager/public/graph/graph.h"
 #include "components/performance_manager/public/graph/page_node.h"
@@ -46,18 +47,20 @@ struct AfterStartupTask {
 };
 
 // The flag may be read on any thread, but must only be set on the UI thread.
-base::LazyInstance<base::AtomicFlag>::Leaky g_startup_complete_flag;
+base::AtomicFlag& GetStartupCompleteFlag() {
+  static base::NoDestructor<base::AtomicFlag> startup_complete_flag;
+  return *startup_complete_flag;
+}
 
 // The queue may only be accessed on the UI thread.
-base::LazyInstance<base::circular_deque<AfterStartupTask*>>::Leaky
-    g_after_startup_tasks;
+base::circular_deque<AfterStartupTask*>& GetAfterStartupTasks() {
+  static base::NoDestructor<base::circular_deque<AfterStartupTask*>>
+      after_startup_tasks;
+  return *after_startup_tasks;
+}
 
 bool IsBrowserStartupComplete() {
-  // Be sure to initialize the LazyInstance on the main thread since the flag
-  // may only be set on it's initializing thread.
-  if (!g_startup_complete_flag.IsCreated())
-    return false;
-  return g_startup_complete_flag.Get().IsSet();
+  return GetStartupCompleteFlag().IsSet();
 }
 
 void RunTask(std::unique_ptr<AfterStartupTask> queued_task) {
@@ -96,7 +99,7 @@ void QueueTask(std::unique_ptr<AfterStartupTask> queued_task) {
     ScheduleTask(std::move(queued_task));
     return;
   }
-  g_after_startup_tasks.Get().push_back(queued_task.release());
+  GetAfterStartupTasks().push_back(queued_task.release());
 }
 
 void SetBrowserStartupIsComplete() {
@@ -105,8 +108,13 @@ void SetBrowserStartupIsComplete() {
   if (IsBrowserStartupComplete())
     return;
 
-  TRACE_EVENT0("startup", "SetBrowserStartupIsComplete");
-  g_startup_complete_flag.Get().Set();
+  size_t browser_count = 0;
+#if !BUILDFLAG(IS_ANDROID)
+  browser_count = chrome::GetTotalBrowserCount();
+#endif  // !BUILDFLAG(IS_ANDROID)
+  TRACE_EVENT_INSTANT1("startup", "Startup.StartupComplete",
+                       TRACE_EVENT_SCOPE_GLOBAL, "BrowserCount", browser_count);
+  GetStartupCompleteFlag().Set();
 #if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX) || \
     BUILDFLAG(IS_CHROMEOS)
   // Process::Current().CreationTime() is not available on all platforms.
@@ -119,11 +127,12 @@ void SetBrowserStartupIsComplete() {
 #endif  // BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX) ||
         // BUILDFLAG(IS_CHROMEOS)
   UMA_HISTOGRAM_COUNTS_10000("Startup.AfterStartupTaskCount",
-                             g_after_startup_tasks.Get().size());
-  for (AfterStartupTask* queued_task : g_after_startup_tasks.Get())
+                             GetAfterStartupTasks().size());
+  for (AfterStartupTask* queued_task : GetAfterStartupTasks()) {
     ScheduleTask(base::WrapUnique(queued_task));
-  g_after_startup_tasks.Get().clear();
-  g_after_startup_tasks.Get().shrink_to_fit();
+  }
+  GetAfterStartupTasks().clear();
+  GetAfterStartupTasks().shrink_to_fit();
 }
 
 // Observes the first visible page load and sets the startup complete
@@ -277,9 +286,9 @@ bool AfterStartupTaskUtils::IsBrowserStartupComplete() {
 }
 
 void AfterStartupTaskUtils::UnsafeResetForTesting() {
-  DCHECK(g_after_startup_tasks.Get().empty());
+  DCHECK(GetAfterStartupTasks().empty());
   if (!IsBrowserStartupComplete())
     return;
-  g_startup_complete_flag.Get().UnsafeResetForTesting();
+  GetStartupCompleteFlag().UnsafeResetForTesting();  // IN-TEST
   DCHECK(!IsBrowserStartupComplete());
 }
