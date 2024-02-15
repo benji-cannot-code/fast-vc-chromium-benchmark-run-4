1304a18ae2b841035ed1ba901c022512472d806e
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2024-02-15 16:54:00 +0000

    [views_ax] Replace ViewAccessibility::OverrideBounds with SetBounds
    This is a short refactor. We're removing the OverrideBounds function
    from ViewAccessibility and introducing the SetBounds instead. This is
    part of a larger project, ViewsAX, described in the bug attached. The
    main difference is that the bounds are now set on the final AXNodeData
    member (`data_`) and not on the custom one, `override_data_`.
    
    We updated the only caller to this function, as well.
    
    Bug: 325137417
    Change-Id: I73d4b5a39dbf516b06ac0e9883222d5be217e7c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5298098
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Javier Contreras <javiercon@microsoft.com>
    Auto-Submit: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1261127}

--- ui/views/accessibility/view_accessibility.cc
+++ ui/views/accessibility/view_accessibility.cc
@@ -410,6 +410,10 @@ ax::mojom::Role ViewAccessibility::GetViewAccessibilityRole() const {
   return data_.role;
 }
 
+void ViewAccessibility::SetBounds(const gfx::RectF& bounds) {
+  data_.relative_bounds.bounds = bounds;
+}
+
 void ViewAccessibility::OverrideRole(const ax::mojom::Role role) {
   DCHECK(IsValidRoleForViews(role)) << "Invalid role for Views.";
   override_data_.role = role;
@@ -554,10 +558,6 @@ bool ViewAccessibility::IsAccessibilityEnabled() const {
   if (is_enabled_)
     return *is_enabled_;
   return view_->GetEnabled();
-}
-
-void ViewAccessibility::OverrideBounds(const gfx::RectF& bounds) {
-  override_data_.relative_bounds.bounds = bounds;
 }
 
 void ViewAccessibility::OverrideHasPopup(const ax::mojom::HasPopup has_popup) {
