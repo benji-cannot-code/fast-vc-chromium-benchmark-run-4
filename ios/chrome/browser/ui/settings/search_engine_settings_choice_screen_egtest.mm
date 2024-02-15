237f026f850517ae8cb5a093e5d22ddf778e0894
Federica Germinario <fedegermi@google.com>
2024-02-15 11:27:43 +0000

    [ios] Disable tests failing in SearchEngineSettingsTestCase
    This CL disables
    SearchEngineSettingsTestCase/testDeleteCustomSearchEngineEdit
    SearchEngineSettingsTestCase/testDeleteCustomSearchEngineSwipe
    SearchEngineSettingsTestCase/testDeleteCustomSearchEngineSwipeAndTap
    
    Tests in SearchEngineSettingsChoiceScreenTestCase were not failing,
    this CL re-enables them.
    
    Bug: 325379827
    Change-Id: Iecb96447234c468ba265d17ac03b9d53a8c82ade
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5300235
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1260979}

--- ios/chrome/browser/ui/settings/search_engine_settings_choice_screen_egtest.mm
+++ ios/chrome/browser/ui/settings/search_engine_settings_choice_screen_egtest.mm
@@ -177,9 +177,8 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
   [ChromeEarlGrey waitForWebStateContainingText:kYahooURL];
 }
 
-// TODO(crbug.com/325379827): Re-enable this test.
 // Deletes a custom search engine by swiping and tapping on the "Delete" button.
-- (void)DISABLED_testDeleteCustomSearchEngineSwipeAndTap {
+- (void)testDeleteCustomSearchEngineSwipeAndTap {
   [self enterSettingsWithCustomSearchEngine];
 
   [[SearchEngineChoiceEarlGreyUI
@@ -198,9 +197,8 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
       assertWithMatcher:grey_nil()];
 }
 
-// TODO(crbug.com/325379827): Re-enable this test.
 // Deletes a custom engine by swiping it.
-- (void)DISABLED_testDeleteCustomSearchEngineSwipe {
+- (void)testDeleteCustomSearchEngineSwipe {
   [self enterSettingsWithCustomSearchEngine];
   [[SearchEngineChoiceEarlGreyUI
       interactionForSettingsCustomSearchEngineWithName:kCustomSearchEngineName]
@@ -235,9 +233,8 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
       assertWithMatcher:grey_notNil()];
 }
 
-// TODO(crbug.com/325379827): Re-enable this test.
 // Deletes a custom search engine by entering edit mode.
-- (void)DISABLED_testDeleteCustomSearchEngineEdit {
+- (void)testDeleteCustomSearchEngineEdit {
   [self enterSettingsWithCustomSearchEngine];
 
   id<GREYMatcher> editButton = grey_allOf(
