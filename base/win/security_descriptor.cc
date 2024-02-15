1ddc454384f30a112c1e62aebfa1015ec01c2d45
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-02-15 18:24:34 +0000

    base: Guard against wrong windows.h clang-format reordering
    Unfortunately, the include order for <windows.h> matters. This patch
    prevents clang-format to reorder them and cause compile failure.
    
    Context: I am about to rename {absl => std}::optional in base:
    https://chromium-review.googlesource.com/c/chromium/src/+/5296147
    and those #includes have to be reordered.
    See build failure on windows:
    https://ci.chromium.org/ui/p/chromium/builders/try/win_chromium_compile_dbg_ng/1838184/overview
    
    Bug: chromium:1500249
    Change-Id: I7aa7ca564f79bb94a41189213a5290c2d667ca5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5300422
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261184}

--- base/win/security_descriptor.cc
+++ base/win/security_descriptor.cc
@@ -5,9 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/win/security_descriptor.h"
 
+// clang-format off
+#include <windows.h>  // Must be in front of other Windows header files.
+// clang-format on
+
 #include <aclapi.h>
 #include <sddl.h>
-#include <windows.h>
 
 #include <utility>
 #include <vector>
--- base/win/sid_unittest.cc
+++ base/win/sid_unittest.cc
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/win/sid.h"
 
-#include <windows.h>
+// clang-format off
+#include <windows.h>  // Must be in front of other Windows header files.
+// clang-format on
 
 #include <sddl.h>
 
