44aa750aebd4aea9374e1b6de81239218878ee09
Wes Okuhara <wesokuhara@google.com>
2024-02-15 22:02:19 +0000

    CrOS Settings: Record changed setting ID for internet pages
    Uses of recordSettingChange() should always include the setting ID and,
    if appropriate, a corresponding value. Uses of recordSettingChange()
    that do not have an associated setting ID are removed and a TODO
    comment is added.
    
    Bug: b:324480501, b:282233232
    Test: browser_tests --gtest_filter="OSSettings*"
    Change-Id: I6475dd8d2e4cddf4a59dc3711542338ad01240f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5299632
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1261330}

--- chrome/browser/resources/ash/settings/internet_page/internet_subpage.ts
+++ chrome/browser/resources/ash/settings/internet_page/internet_subpage.ts
@@ -39,7 +39,6 @@ import {afterNextRender, DomRepeatEvent, mixinBehaviors, PolymerElement} from 'c
 import {castExists} from '../assert_extras.js';
 import {DeepLinkingMixin, DeepLinkingMixinInterface} from '../common/deep_linking_mixin.js';
 import {RouteOriginMixin, RouteOriginMixinInterface} from '../common/route_origin_mixin.js';
-import {recordSettingChange} from '../metrics_recorder.js';
 import {Setting} from '../mojom-webui/setting.mojom-webui.js';
 import {Route, Router, routes} from '../router.js';
 
@@ -710,7 +709,7 @@ export class SettingsInternetSubpageElement extends
   private onAddThirdPartyVpnClick_(event: DomRepeatEvent<VpnProvider>): void {
     const provider = event.model.item;
     this.browserProxy_.addThirdPartyVpn(provider.appId);
-    recordSettingChange();
+    // TODO(b/282233232) recordSettingChange() for adding third party VPN.
   }
 
   private knownNetworksIsVisible_(deviceState: OncMojo.DeviceStateProperties|
@@ -777,7 +776,7 @@ export class SettingsInternetSubpageElement extends
         detail: {networkState},
       });
       this.dispatchEvent(networkConnectEvent);
-      recordSettingChange();
+      // TODO(b/282233232) recordSettingChange() for connecting to network.
       return;
     }
 
