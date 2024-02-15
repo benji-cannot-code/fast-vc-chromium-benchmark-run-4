629bc47dd17d9fb82cdae8546950fba862f0c134
David Padlipsky <dpad@google.com>
2024-02-15 20:48:17 +0000

    customization: Enable notification flag by default
    Bug: b/241965717
    Change-Id: I24cca688c559a2a6435275141f8037fb8e282416
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5191527
    Auto-Submit: David Padlipsky <dpad@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1261279}

--- ash/system/input_device_settings/input_device_settings_notification_controller.cc
+++ ash/system/input_device_settings/input_device_settings_notification_controller.cc
@@ -84,6 +84,10 @@ constexpr auto kSixPackKeyToPrefName =
          {prefs::kSixPackKeyInsertNotificationsRemaining}},
     });
 
+// Device key of the virutal mouse often used by integration tests, avoid
+// showing notification in this case.
+const char kVirtualMouseDeviceKey[] = "0000:0000";
+
 const char kNotifierId[] = "input_device_settings_controller";
 const char kAltRightClickRewriteNotificationId[] =
     "alt_right_click_rewrite_blocked_by_setting";
@@ -415,6 +419,11 @@ void InputDeviceSettingsNotificationController::
 void InputDeviceSettingsNotificationController::NotifyMouseFirstTimeConnected(
     const mojom::Mouse& mouse) {
   if (!IsActiveUserSession()) {
+    return;
+  }
+
+  // Avoid showing notification for the virtual mouse device.
+  if (mouse.device_key == kVirtualMouseDeviceKey) {
     return;
   }
 
