ea2476ece6f55bb31bff77b2001620fd9e9d09f8
Elias Klim <elklm@google.com>
2024-02-15 13:19:50 +0000

    [LHS indicator] Fix Confirmation chip incorrect width
    This CL fixes a bug when Confirmation chip width becomes too long.
    The bug is reproducible only with LHS indicators flag.
    
    Bug: 1515083
    Change-Id: I5a430264c91d41bc9199a824627c3a082dcdabe5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5250028
    Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
    Commit-Queue: Elias Klim <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261003}

--- chrome/browser/ui/views/permissions/chip/permission_chip_view.cc
+++ chrome/browser/ui/views/permissions/chip/permission_chip_view.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/location_bar/location_bar_util.h"
 #include "chrome/browser/ui/views/permissions/chip/multi_image_container.h"
 #include "chrome/browser/ui/views/permissions/permission_prompt_style.h"
+#include "components/content_settings/core/common/features.h"
 #include "components/permissions/permission_uma_util.h"
 #include "components/vector_icons/vector_icons.h"
 #include "third_party/skia/include/core/SkColor.h"
@@ -78,7 +79,12 @@ void PermissionChipView::AnimateExpand(base::TimeDelta duration) {
 
 void PermissionChipView::AnimateToFit(base::TimeDelta duration) {
   animation_->SetSlideDuration(duration);
-  base_width_ = label()->width();
+  if (base::FeatureList::IsEnabled(
+          content_settings::features::kLeftHandSideActivityIndicators)) {
+    base_width_ = label()->GetPreferredSize().width();
+  } else {
+    base_width_ = label()->width();
+  }
 
   if (label()->GetPreferredSize().width() < width()) {
     // As we're collapsing, we need to make sure that the padding is not
