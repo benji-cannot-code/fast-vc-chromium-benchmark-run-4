9f19489c4ced24c1a5a289eae30f990ee56fc15a
Rushan Suleymanov <rushans@google.com>
2024-02-15 19:51:58 +0000

    [Sync] Clean up histogram for old invalidations
    OBSOLETE_HISTOGRAMS=Topic-based invalidations were removed a couple of milestones ago and the histogram is not used anymore.
    
    Bug: 40252065, 41489684
    Change-Id: Ide70e0b76c3970a4ff8073f180ccd7e0d66b8ddf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5291114
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Auto-Submit: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261251}

--- google_apis/gcm/engine/instance_id_get_token_request_handler.cc
+++ google_apis/gcm/engine/instance_id_get_token_request_handler.cc
@@ -39,7 +39,7 @@ InstanceIDGetTokenRequestHandler::InstanceIDGetTokenRequestHandler(
   DCHECK(!scope.empty());
 }
 
-InstanceIDGetTokenRequestHandler::~InstanceIDGetTokenRequestHandler() {}
+InstanceIDGetTokenRequestHandler::~InstanceIDGetTokenRequestHandler() = default;
 
 void InstanceIDGetTokenRequestHandler::BuildRequestBody(std::string* body) {
   BuildFormEncoding(kScopeKey, scope_, body);
@@ -62,10 +62,7 @@ void InstanceIDGetTokenRequestHandler::ReportStatusToUMA(
   // sense for large users (who might want to look at the status of their
   // requests specifically), or for deep dives into unexplained changes to the
   // top-level "InstanceID.GetToken.RequestStatus" histogram.
-  if (subtype == "com.google.chrome.fcm.invalidations") {
-    base::UmaHistogramEnumeration(
-        "InstanceID.GetToken.RequestStatus.FcmInvalidations", status);
-  } else if (subtype == "com.google.chrome.sync.invalidations") {
+  if (subtype == "com.google.chrome.sync.invalidations") {
     base::UmaHistogramEnumeration(
         "InstanceID.GetToken.RequestStatus.SyncInvalidations", status);
   }
