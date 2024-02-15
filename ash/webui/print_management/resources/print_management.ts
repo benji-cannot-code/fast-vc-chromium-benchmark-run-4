ac47332ab9c3de268b22efdbd6c8cee085601ee9
Gavin Williams <gavinwill@chromium.org>
2024-02-15 23:37:16 +0000

    Print management: Add delay before showing setup assistance
    This a UI polish that fixes the printer setup assistance UI flashing for
    a split second before print jobs can load. Now the UI waits for the
    first print job load before displaying.
    
    Fixed: b:295206404
    Change-Id: Ic98d264aafddedf12368cc670a2479616fc6b13b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5298835
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1261394}

--- ash/webui/print_management/resources/print_management.ts
+++ ash/webui/print_management/resources/print_management.ts
@@ -18,9 +18,9 @@ import './printer_setup_info.js';
 import './strings.m.js';
 
 import {IronIconElement} from '//resources/polymer/v3_0/iron-icon/iron-icon.js';
+import {I18nMixin} from 'chrome://resources/ash/common/cr_elements/i18n_mixin.js';
 import {loadTimeData} from 'chrome://resources/ash/common/load_time_data.m.js';
 import {ColorChangeUpdater} from 'chrome://resources/cr_components/color_change_listener/colors_css_updater.js';
-import {I18nMixin} from 'chrome://resources/ash/common/cr_elements/i18n_mixin.js';
 import {assert} from 'chrome://resources/js/assert.js';
 import {PolymerElementProperties} from 'chrome://resources/polymer/v3_0/polymer/interfaces.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
@@ -141,6 +141,8 @@ export class PrintManagementElement extends PrintManagementElementBase
        * events.
        */
       printJobsObserverReceiver: {type: Object},
+
+      printJobsLoaded: Boolean,
     };
   }
 
@@ -178,6 +180,7 @@ export class PrintManagementElement extends PrintManagementElementBase
   private deletePrintJobHistoryAllowedByPolicy: boolean;
   private shouldDisableClearAllButton: boolean;
   private printJobsObserverReceiver: PrintJobsObserverReceiver;
+  private printJobsLoaded: boolean = false;
 
   override connectedCallback(): void {
     super.connectedCallback();
@@ -248,6 +251,11 @@ export class PrintManagementElement extends PrintManagementElementBase
   }
 
   private onPrintJobsReceived(jobs: {printJobs: PrintJobInfo[]}): void {
+    // Set on the first print jobs response.
+    if (!this.printJobsLoaded) {
+      this.printJobsLoaded = true;
+    }
+
     // TODO(crbug/1073690): Update this when BigInt is supported for
     // updateList().
     const ongoingList = [];
@@ -309,6 +317,11 @@ export class PrintManagementElement extends PrintManagementElementBase
   }
 
   private removePrintJob(e: RemovePrintJobEvent): void {
+    // Reset this variable to prevent the printer setup assistance UI from
+    // showing during the brief time this print job transfers from
+    // `ongoingPrintJobs` to `printJobs`.
+    this.printJobsLoaded = false;
+
     const idx = this.getIndexOfOngoingPrintJob(e.detail);
     if (idx !== -1) {
       this.splice('ongoingPrintJobs', idx, 1);
@@ -342,13 +355,15 @@ export class PrintManagementElement extends PrintManagementElementBase
 
   /** Determine if printer setup UI should be shown. */
   private shouldShowSetupAssistance(): boolean {
-    return this.showSetupAssistance && this.ongoingPrintJobs.length === 0 &&
-        this.printJobs.length === 0;
+    return this.showSetupAssistance && this.printJobsLoaded &&
+        this.ongoingPrintJobs.length === 0 && this.printJobs.length === 0;
   }
 
   /** Determine if ongoing jobs empty messaging should be shown. */
   private shouldShowOngoingEmptyState(): boolean {
-    return !this.shouldShowSetupAssistance() &&
+    // The ongoing empty state should only be shown when there aren't ongoing
+    // print jobs and the completed prints jobs list is showing.
+    return (!this.showSetupAssistance || this.printJobs.length > 0) &&
         this.ongoingPrintJobs.length === 0;
   }
 
