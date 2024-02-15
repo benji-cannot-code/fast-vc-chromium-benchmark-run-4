37b5327250043c6415fc1b465476b12d8c526b51
Jose Dapena Paz <jdapena@igalia.com>
2024-02-15 09:14:33 +0000

    IWYU: LogMessage raw_ptr requires include
    Include LogMessage declaration in raw_ref_unittest.cc as check.h
    declaration will require traits that are not defined. To avoid
    adding full logging.h include in check.h (which is expected to be
    lightweight), just include it in the test.
    
    Bug: 957519
    Change-Id: I513126a71edd669218ace8be7e56441c5a63be9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5225136
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260947}

--- base/allocator/partition_allocator/src/partition_alloc/pointers/raw_ref_unittest.cc
+++ base/allocator/partition_allocator/src/partition_alloc/pointers/raw_ref_unittest.cc
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <functional>
 #include <type_traits>
 
+// TODO(crbug.com/957519): including logging.h is required because of the
+// raw_ptr_traits definition that actually requires all types pointed with
+// raw_ptr to be defined.
+#include "base/logging.h"
 #include "base/memory/raw_ptr.h"
 #include "base/test/gtest_util.h"
 #include "partition_alloc/partition_alloc_base/debug/debugging_buildflags.h"
