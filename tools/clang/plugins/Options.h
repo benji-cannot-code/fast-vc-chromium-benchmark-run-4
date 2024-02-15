dfba4e64256485e2283ac29c2acc9c24fc06076b
danakj <danakj@chromium.org>
2024-02-15 17:11:28 +0000

    Remove old unused paths for checking auto-pointer deduction
    R=aeubanks@google.com
    
    Bug: 41495876
    Change-Id: I116cc21400281b9cf088cc761aa03de51bc48de1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5296957
    Reviewed-by: Arthur Eubanks <aeubanks@google.com>
    Auto-Submit: danakj <danakj@chromium.org>
    Commit-Queue: Arthur Eubanks <aeubanks@google.com>
    Cr-Commit-Position: refs/heads/main@{#1261144}

--- tools/clang/plugins/Options.h
+++ tools/clang/plugins/Options.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chrome_checker {
 
 struct Options {
-  bool allow_auto_typedefs_better = false;
-  bool allow_auto_typedefs_better_nested = false;
   bool check_base_classes = false;
   bool check_blink_data_member_type = false;
   bool check_ipc = false;
