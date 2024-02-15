92393548265bc5f3d955cd9d96d8721e324aba1a
Keren Zhu <kerenzhu@chromium.org>
2024-02-15 18:11:53 +0000

    views: add crash key to window_reorderer.cc
    Bug: 323892437
    Change-Id: If0bcb65fe5cd65b35267a08b4847afcec22f96da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5299414
    Auto-Submit: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261178}

--- ui/views/widget/window_reorderer.cc
+++ ui/views/widget/window_reorderer.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/adapters.h"
+#include "base/debug/crash_logging.h"
 #include "base/memory/raw_ptr.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_occlusion_tracker.h"
@@ -48,6 +49,9 @@ void GetOrderOfViewsWithLayers(
     const std::map<views::View*, aura::Window*>& hosts,
     std::vector<views::View*>* order) {
   DCHECK(view);
+  SCOPED_CRASH_KEY_STRING64("GetOrderOfViewsWithLayers", "view_name",
+                            view->GetObjectName());
+
   DCHECK(parent_layer);
   DCHECK(order);
   if (view->layer() && view->layer()->parent() == parent_layer) {
