9eff0d1f3d2dc6d20a0718cdbdce07613c895a04
Andrew Paseltiner <apaseltiner@chromium.org>
2024-02-16 16:26:01 +0000

    Tidy Attribution Internals TypeScript
    - Remove unnecessary type assertions
    - Separate TableModel from columns
    - Replace manual default-sort-index specification with human-readable
      field
    - Make internal methods private
    - Simplify types and names
    
    This contains no user-visible changes.
    
    Change-Id: I6839950f41a7dbdc6a619dc37c8d6cb4341a237b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5301557
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261685}

--- content/browser/resources/attribution_reporting/table_model.ts
+++ content/browser/resources/attribution_reporting/table_model.ts
@@ -3,18 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-export interface Column<T> {
-  compare?(a: T, b: T): number;
-
-  render(td: HTMLElement, row: T): void;
-
-  renderHeader(th: HTMLElement): void;
-}
-
 export abstract class TableModel<T> {
   readonly rowsChangedListeners: Set<() => void> = new Set();
-
-  constructor(public readonly cols: Array<Column<T>>, public sortIdx: number) {}
 
   abstract getRows(): T[];
 
@@ -22,7 +12,7 @@ export abstract class TableModel<T> {
     return this.getRows().length;
   }
 
-  notifyRowsChanged(): void {
+  protected notifyRowsChanged(): void {
     this.rowsChangedListeners.forEach(f => f());
   }
 }
@@ -30,11 +20,8 @@ export abstract class TableModel<T> {
 export class ArrayTableModel<T> extends TableModel<T> {
   private rows_: T[] = [];
 
-  constructor(
-      cols: Array<Column<T>>,
-      sortIdx: number,
-  ) {
-    super(cols, sortIdx);
+  constructor() {
+    super();
   }
 
   override getRows(): T[] {
