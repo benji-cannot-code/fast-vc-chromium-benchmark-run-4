bc75871141dadad3ef56e8bc5c1c46839835ee30
Xi Han <hanxi@google.com>
2024-02-16 14:20:25 +0000

    [MagicStack] Support freshness count for modules.
    Add APIs to reset/increase/get freshness count for a given module type.
    
    Bug: b/324312039
    Change-Id: Iee190207616015326bd8c1499292b58579c8861c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5301239
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261618}

--- chrome/browser/magic_stack/android/junit/src/org/chromium/chrome/browser/magic_stack/HomeModulesConfigSettingsUnitTest.java
+++ chrome/browser/magic_stack/android/junit/src/org/chromium/chrome/browser/magic_stack/HomeModulesConfigSettingsUnitTest.java
@@ -70,7 +70,7 @@ public class HomeModulesConfigSettingsUnitTest {
                 ChromePreferenceKeys.HOME_MODULES_MODULE_TYPE.createKey(String.valueOf(0));
         String priceChangePreferenceKey =
                 ChromePreferenceKeys.HOME_MODULES_MODULE_TYPE.createKey(String.valueOf(1));
-        when(mHomeModulesConfigManager.getPreferenceKey(ModuleType.PRICE_CHANGE))
+        when(mHomeModulesConfigManager.getSettingsPreferenceKey(ModuleType.PRICE_CHANGE))
                 .thenReturn(priceChangePreferenceKey);
         HomeModulesConfigManager.setInstanceForTesting(mHomeModulesConfigManager);
 
@@ -104,7 +104,7 @@ public class HomeModulesConfigSettingsUnitTest {
                 .thenReturn(moduleTypeShownInSettingsForTest);
         String priceChangePreferenceKey =
                 ChromePreferenceKeys.HOME_MODULES_MODULE_TYPE.createKey(String.valueOf(1));
-        when(mHomeModulesConfigManager.getPreferenceKey(ModuleType.PRICE_CHANGE))
+        when(mHomeModulesConfigManager.getSettingsPreferenceKey(ModuleType.PRICE_CHANGE))
                 .thenReturn(priceChangePreferenceKey);
         HomeModulesConfigManager.setInstanceForTesting(mHomeModulesConfigManager);
 
