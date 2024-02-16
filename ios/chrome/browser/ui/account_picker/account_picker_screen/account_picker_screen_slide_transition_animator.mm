8731ac244162ca7c9e985287a360a9360d8d7bbb
Quentin Pubert <qpubert@google.com>
2024-02-16 16:27:16 +0000

    [iOS] Flip animation, right arrow icon for RTL in consistency promo
    This CL makes the right arrow icon in IdentityButtonControl flip for the
    right-to-left layout direction. It also flips the direction of the
    transition animation in both the account picker code and the consistency
    promo code.
    
    Screenshot before change:
    https://drive.google.com/file/d/1wyDU767JOWsciE8AMlfIrA7qEvvvdDZT
    Screenshot after change:
    https://drive.google.com/file/d/12N_LL7hQjPRQeGuzQ23hBcZpnjvsftuZ
    Video after change:
    https://drive.google.com/file/d/1pQpVo9wHTusEn4f75cMCdZ76OVSz6bO2
    
    Fixed: b/325453089
    Change-Id: Icc97d49e4907a6f60826266b5791bfca219f8ee0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5300601
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1261686}

--- ios/chrome/browser/ui/account_picker/account_picker_screen/account_picker_screen_slide_transition_animator.mm
+++ ios/chrome/browser/ui/account_picker/account_picker_screen/account_picker_screen_slide_transition_animator.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/account_picker/account_picker_screen/account_picker_screen_slide_transition_animator.h"
 
+#import "base/i18n/rtl.h"
 #import "ios/chrome/browser/ui/account_picker/account_picker_screen/account_picker_screen_navigation_controller.h"
 
 namespace {
@@ -72,15 +73,17 @@ const CGFloat kAnimationDuration = 0.25;
       CGRectMake(0, 0, toViewSize.width, toViewSize.height);
   CGRect navigationFrameAfterAnimation = viewControllerFrame;
 
-  switch (self.animation) {
-    case kAccountPickerScreenSlideAnimationPopping:
-      toViewFrameBeforeAnimation.origin.x = -toView.frame.size.width;
-      fromViewFrameAfterAnimation.origin.x = fromView.frame.size.width;
-      break;
-    case kAccountPickerScreenSlideAnimationPushing:
-      toViewFrameBeforeAnimation.origin.x = toView.frame.size.width;
-      fromViewFrameAfterAnimation.origin.x = -fromView.frame.size.width;
-      break;
+  BOOL fromViewShouldMoveTowardsTheRight =
+      (self.animation == kAccountPickerScreenSlideAnimationPopping &&
+       !base::i18n::IsRTL()) ||
+      (self.animation == kAccountPickerScreenSlideAnimationPushing &&
+       base::i18n::IsRTL());
+  if (fromViewShouldMoveTowardsTheRight) {
+    toViewFrameBeforeAnimation.origin.x = -toView.frame.size.width;
+    fromViewFrameAfterAnimation.origin.x = fromView.frame.size.width;
+  } else {
+    toViewFrameBeforeAnimation.origin.x = toView.frame.size.width;
+    fromViewFrameAfterAnimation.origin.x = -fromView.frame.size.width;
   }
   toView.frame = toViewFrameBeforeAnimation;
   switch (self.navigationController.displayStyle) {
--- ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_sheet/consistency_sheet_slide_transition_animator.mm
+++ ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_sheet/consistency_sheet_slide_transition_animator.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_sheet/consistency_sheet_slide_transition_animator.h"
 
+#import "base/i18n/rtl.h"
 #import "ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_sheet/consistency_sheet_navigation_controller.h"
 
 namespace {
@@ -71,15 +72,17 @@ const CGFloat kAnimationDuration = 0.25;
       CGRectMake(0, 0, toViewSize.width, toViewSize.height);
   CGRect navigationFrameAfterAnimation = viewControllerFrame;
 
-  switch (self.animation) {
-    case ConsistencySheetSlideAnimationPopping:
-      toViewFrameBeforeAnimation.origin.x = -toView.frame.size.width;
-      fromViewFrameAfterAnimation.origin.x = fromView.frame.size.width;
-      break;
-    case ConsistencySheetSlideAnimationPushing:
-      toViewFrameBeforeAnimation.origin.x = toView.frame.size.width;
-      fromViewFrameAfterAnimation.origin.x = -fromView.frame.size.width;
-      break;
+  BOOL fromViewShouldMoveTowardsTheRight =
+      (self.animation == ConsistencySheetSlideAnimationPopping &&
+       !base::i18n::IsRTL()) ||
+      (self.animation == ConsistencySheetSlideAnimationPushing &&
+       base::i18n::IsRTL());
+  if (fromViewShouldMoveTowardsTheRight) {
+    toViewFrameBeforeAnimation.origin.x = -toView.frame.size.width;
+    fromViewFrameAfterAnimation.origin.x = fromView.frame.size.width;
+  } else {
+    toViewFrameBeforeAnimation.origin.x = toView.frame.size.width;
+    fromViewFrameAfterAnimation.origin.x = -fromView.frame.size.width;
   }
   toView.frame = toViewFrameBeforeAnimation;
   switch (self.navigationController.displayStyle) {
