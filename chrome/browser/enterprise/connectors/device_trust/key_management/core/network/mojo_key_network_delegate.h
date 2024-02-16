bd600da27a96883dc02f4dfff3afd53c7f50b547
Sebastien Lalancette <seblalancette@chromium.org>
2024-02-16 14:54:15 +0000

    Implement DMServerClient Interface in MojoKeyNetworkDelegate
    This will allow creating MojoKeyNetworkDelegate instances as
    implementation of that interface.
    
    Also update the delegate to support parallel requests. I had to expose
    the new client certs code to Linux as part of this, as the network
    delegate is built on that platform, and we will eventually support it
    anyway. We'll gate the release of the feature on that platform via
    reading the policy.
    
    Change-Id: I1ff090d52eed4cc454c4056b148a609fe0850b45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5301128
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261628}

--- chrome/browser/enterprise/connectors/device_trust/key_management/core/network/mojo_key_network_delegate.h
+++ chrome/browser/enterprise/connectors/device_trust/key_management/core/network/mojo_key_network_delegate.h
@@ -6,13 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_KEY_MANAGEMENT_CORE_NETWORK_MOJO_KEY_NETWORK_DELEGATE_H_
 #define CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_KEY_MANAGEMENT_CORE_NETWORK_MOJO_KEY_NETWORK_DELEGATE_H_
 
-#include "chrome/browser/enterprise/connectors/device_trust/key_management/core/network/key_network_delegate.h"
-
 #include <memory>
 #include <string>
 
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/enterprise/connectors/device_trust/key_management/core/network/key_network_delegate.h"
+#include "components/enterprise/client_certificates/core/dm_server_client.h"
 #include "net/http/http_response_headers.h"
 #include "url/gurl.h"
 
@@ -25,7 +25,8 @@ namespace enterprise_connectors {
 
 // Implementation of the MojoKeyNetworkDelegate interface using mojo.
 // This class does not support support parallel requests.
-class MojoKeyNetworkDelegate : public KeyNetworkDelegate {
+class MojoKeyNetworkDelegate : public KeyNetworkDelegate,
+                               public client_certificates::DMServerClient {
  public:
   explicit MojoKeyNetworkDelegate(
       scoped_refptr<network::SharedURLLoaderFactory> shared_url_loader_factory);
@@ -39,16 +40,27 @@ class MojoKeyNetworkDelegate : public KeyNetworkDelegate {
       const std::string& body,
       UploadKeyCompletedCallback upload_key_completed_callback) override;
 
+  // client_certificates::DMServerClient:
+  void SendRequest(
+      const GURL& url,
+      std::string_view dm_token,
+      const enterprise_management::DeviceManagementRequest& request_body,
+      SendRequestCallback callback) override;
+
  private:
   // Invoked when the network url loader has completed. `headers` is
   // the HTTP response headers received from the server.
   // `upload_key_completed_callback` is used to return the HTTP status
   // code.
   void OnURLLoaderComplete(
+      std::unique_ptr<network::SimpleURLLoader> url_loader,
       UploadKeyCompletedCallback upload_key_completed_callback,
       scoped_refptr<net::HttpResponseHeaders> headers);
 
-  std::unique_ptr<network::SimpleURLLoader> url_loader_;
+  void OnDownloadStringComplete(
+      std::unique_ptr<network::SimpleURLLoader> url_loader,
+      SendRequestCallback callback,
+      std::optional<std::string> response_body);
 
   scoped_refptr<network::SharedURLLoaderFactory> shared_url_loader_factory_;
 
