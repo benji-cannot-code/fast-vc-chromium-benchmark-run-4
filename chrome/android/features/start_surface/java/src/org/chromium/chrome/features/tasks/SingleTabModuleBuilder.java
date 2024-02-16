8e0265a847d468b0c0c257fe151f2bf79d60e4cc
Xi Han <hanxi@google.com>
2024-02-16 23:14:56 +0000

    [MagicStack] Set Single Tab be not configurable.
    This CL fixes a crash when opening "new tab page cards" from settings.
    
    Bug: 41485537
    Change-Id: I58040d19e43bea22fc123bba4777614e71f55251
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5301741
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261952}

--- chrome/android/features/start_surface/java/src/org/chromium/chrome/features/tasks/SingleTabModuleBuilder.java
+++ chrome/android/features/start_surface/java/src/org/chromium/chrome/features/tasks/SingleTabModuleBuilder.java
@@ -109,4 +109,9 @@ public class SingleTabModuleBuilder implements ModuleProviderBuilder, ModuleConf
     public boolean isEligible() {
         return true;
     }
+
+    @Override
+    public boolean isConfigurable() {
+        return false;
+    }
 }
