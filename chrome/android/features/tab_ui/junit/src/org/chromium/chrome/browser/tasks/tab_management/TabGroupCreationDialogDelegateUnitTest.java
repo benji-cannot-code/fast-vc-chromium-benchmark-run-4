7b96f1a8150a85b96847a132264ec7c6e728a7f1
Calder Kitagawa <ckitagawa@chromium.org>
2024-02-16 19:58:24 +0000

    [Tab Groups] Group creation dialog support group size 1
    Modify the TabGroupCreationDialogDelegate to properly trigger or not
    when tab groups of size 1 are supported.
    
    Bug: b/41496693
    Change-Id: Ib8f0734d844f2f9d7846b373f86ebe13fe25c759
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5301063
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1261838}

--- chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupCreationDialogDelegateUnitTest.java
+++ chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupCreationDialogDelegateUnitTest.java
@@ -6,8 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.tasks.tab_management;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -29,6 +33,7 @@ import org.robolectric.annotation.Config;
 
 import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.TabModelFilterProvider;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tasks.tab_groups.TabGroupModelFilter;
@@ -51,6 +56,7 @@ public class TabGroupCreationDialogDelegateUnitTest {
     @Mock private TabGroupModelFilter mRegularTabGroupModelFilter;
     @Mock private TabGroupModelFilter mIncognitoTabGroupModelFilter;
     @Captor private ArgumentCaptor<PropertyModel> mModelCaptor;
+    @Captor private ArgumentCaptor<TabGroupModelFilterObserver> mObserverCaptor;
 
     private Activity mActivity;
     private TabGroupCreationDialogDelegate mTabGroupCreationDialogDelegate;
@@ -63,8 +69,14 @@ public class TabGroupCreationDialogDelegateUnitTest {
         mActivity = Robolectric.buildActivity(Activity.class).setup().get();
         mTabModelSelectorSupplier.set(mTabModelSelector);
         mTabGroupCreationDialogDelegate =
-                new TabGroupCreationDialogDelegate(
-                        mActivity, mModalDialogManager, mTabModelSelectorSupplier);
+                spy(
+                        new TabGroupCreationDialogDelegate(
+                                mActivity, mModalDialogManager, mTabModelSelectorSupplier));
+        when(mTabModelSelector.getTabModelFilterProvider()).thenReturn(mTabModelFilterProvider);
+        when(mTabModelFilterProvider.getTabModelFilter(false))
+                .thenReturn(mRegularTabGroupModelFilter);
+        when(mTabModelFilterProvider.getTabModelFilter(true))
+                .thenReturn(mIncognitoTabGroupModelFilter);
     }
 
     @After
@@ -74,17 +86,62 @@ public class TabGroupCreationDialogDelegateUnitTest {
 
     @Test
     public void testCreationDialogDelegate_addObservers() {
-        when(mTabModelSelector.getTabModelFilterProvider()).thenReturn(mTabModelFilterProvider);
-        when(mTabModelFilterProvider.getTabModelFilter(false))
-                .thenReturn(mRegularTabGroupModelFilter);
-        when(mTabModelFilterProvider.getTabModelFilter(true))
-                .thenReturn(mIncognitoTabGroupModelFilter);
         mTabGroupCreationDialogDelegate.addObservers();
 
-        verify(mRegularTabGroupModelFilter, times(1))
+        verify(mRegularTabGroupModelFilter)
                 .addTabGroupObserver(any(TabGroupModelFilterObserver.class));
-        verify(mIncognitoTabGroupModelFilter, times(1))
+        verify(mIncognitoTabGroupModelFilter)
                 .addTabGroupObserver(any(TabGroupModelFilterObserver.class));
+    }
+
+    @Test
+    public void testShowOnDidCreateGroup() {
+        mTabGroupCreationDialogDelegate.addObservers();
+        verify(mRegularTabGroupModelFilter).addTabGroupObserver(mObserverCaptor.capture());
+        TabGroupModelFilterObserver observer = mObserverCaptor.getValue();
+
+        int rootId = 1;
+        int tabCount = 5;
+        when(mRegularTabGroupModelFilter.getRelatedTabCountForRootId(rootId)).thenReturn(tabCount);
+        observer.didCreateNewGroup(rootId);
+
+        verify(mTabGroupCreationDialogDelegate).showDialog(tabCount, false);
+    }
+
+    @Test
+    public void testShowOnWillMergeTabToGroup() {
+        mTabGroupCreationDialogDelegate.addObservers();
+        verify(mRegularTabGroupModelFilter).addTabGroupObserver(mObserverCaptor.capture());
+        TabGroupModelFilterObserver observer = mObserverCaptor.getValue();
+
+        int newRootId = 2;
+        int newRootIdTabCount = 10;
+        when(mRegularTabGroupModelFilter.getRelatedTabCountForRootId(newRootId))
+                .thenReturn(newRootIdTabCount);
+
+        Tab tab1 = mock(Tab.class);
+        Tab tab2 = mock(Tab.class);
+        when(mRegularTabGroupModelFilter.getGroupLastShownTab(newRootId)).thenReturn(tab2);
+
+        when(mRegularTabGroupModelFilter.isTabInTabGroup(tab1)).thenReturn(true);
+        when(mRegularTabGroupModelFilter.isTabInTabGroup(tab2)).thenReturn(true);
+        observer.willMergeTabToGroup(tab1, newRootId);
+        verify(mTabGroupCreationDialogDelegate, never()).showDialog(anyInt(), anyBoolean());
+
+        when(mRegularTabGroupModelFilter.isTabInTabGroup(tab1)).thenReturn(false);
+        when(mRegularTabGroupModelFilter.isTabInTabGroup(tab2)).thenReturn(true);
+        observer.willMergeTabToGroup(tab1, newRootId);
+        verify(mTabGroupCreationDialogDelegate, never()).showDialog(anyInt(), anyBoolean());
+
+        when(mRegularTabGroupModelFilter.isTabInTabGroup(tab1)).thenReturn(true);
+        when(mRegularTabGroupModelFilter.isTabInTabGroup(tab2)).thenReturn(false);
+        observer.willMergeTabToGroup(tab1, newRootId);
+        verify(mTabGroupCreationDialogDelegate, never()).showDialog(anyInt(), anyBoolean());
+
+        when(mRegularTabGroupModelFilter.isTabInTabGroup(tab1)).thenReturn(false);
+        when(mRegularTabGroupModelFilter.isTabInTabGroup(tab2)).thenReturn(false);
+        observer.willMergeTabToGroup(tab1, newRootId);
+        verify(mTabGroupCreationDialogDelegate).showDialog(newRootIdTabCount + 1, false);
     }
 
     @Test
