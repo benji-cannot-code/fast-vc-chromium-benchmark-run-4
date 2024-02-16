f33880340b8e76e51c58d04cb403d156ef6b1325
Fredrik Söderquist <fs@opera.com>
2024-02-16 23:09:08 +0000

    Move SVG geometry hit-test code into HitTestLocation
    Adds the following methods:
    
      HitTestLocation::IntersectsStroke
      HitTestLocation::IntersectsEllipse
      HitTestLocation::IntersectsCircleStroke
      HitTestLocation::Intersects(const Path&, WindRule)
      HitTestLocation::Intersects(const Path&)
    
    based on the code from LayoutSVGShape/Rect/Ellipse.
    
    Also extend TransformedHitTestLocation with a "forward" mode for use in
    LayoutSVGShape.
    
    This is in preparation for adding proper rect-based hit-testing for
    these codepaths, and use that to address the referenced bug.
    
    Bug: 40364016
    Change-Id: I5067443520f915a7dc28dbd5cb6e493949a7bed3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5294058
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261951}

--- third_party/blink/renderer/core/layout/hit_test_location.h
+++ third_party/blink/renderer/core/layout/hit_test_location.h
@@ -23,9 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_HIT_TEST_LOCATION_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_HIT_TEST_LOCATION_H_
 
-#include "base/memory/scoped_refptr.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/layout/geometry/physical_rect.h"
+#include "third_party/blink/renderer/platform/graphics/graphics_types.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
 #include "ui/gfx/geometry/quad_f.h"
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class FloatRoundedRect;
+class Path;
 
 class CORE_EXPORT HitTestLocation {
   DISALLOW_NEW();
@@ -94,6 +95,17 @@ class CORE_EXPORT HitTestLocation {
   bool Intersects(const FloatRoundedRect&) const;
   bool Intersects(const gfx::QuadF&) const;
   bool ContainsPoint(const gfx::PointF&) const;
+
+  bool Intersects(const Path&) const;
+  bool Intersects(const Path&, WindRule) const;
+
+  bool IntersectsStroke(const gfx::RectF& rect, float stroke_width) const;
+
+  bool IntersectsEllipse(const gfx::PointF& center,
+                         const gfx::SizeF& radii) const;
+  bool IntersectsCircleStroke(const gfx::PointF& center,
+                              float radius,
+                              float stroke_width) const;
 
   const gfx::PointF& TransformedPoint() const { return transformed_point_; }
   const gfx::QuadF& TransformedRect() const { return transformed_rect_; }
