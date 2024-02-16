398884fc61406ec81b7591da1dcb61f9ba9e99d4
siashah <siashah@chromium.org>
2024-02-16 17:46:48 +0000

    Write chrome sync data to bank accounts table
    Bug: 1472125
    Change-Id: I4b9989e635224ae53bd9c036752b71773b536f41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5256137
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Alexander Tekle <alexandertekle@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Siddharth Shah <siashah@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261737}

--- components/autofill/core/browser/webdata/payments/payments_sync_bridge_test_util.h
+++ components/autofill/core/browser/webdata/payments/payments_sync_bridge_test_util.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/core/browser/data_model/credit_card.h"
 #include "components/autofill/core/browser/data_model/iban.h"
+#include "components/sync/protocol/autofill_specifics.pb.h"
 
 namespace sync_pb {
 class AutofillWalletSpecifics;
@@ -36,6 +37,14 @@ CreateAutofillWalletSpecificsForCreditCardCloudTokenData(
 
 sync_pb::AutofillWalletSpecifics CreateAutofillWalletSpecificsForIban(
     const std::string& client_tag);
+
+sync_pb::AutofillWalletSpecifics CreateAutofillWalletSpecificsForBankAccount(
+    const std::string_view client_tag,
+    std::string nickname,
+    const GURL& display_icon_url,
+    std::string bank_name,
+    std::string account_number_suffix,
+    sync_pb::BankAccountDetails::AccountType account_type);
 
 }  // namespace autofill
 
