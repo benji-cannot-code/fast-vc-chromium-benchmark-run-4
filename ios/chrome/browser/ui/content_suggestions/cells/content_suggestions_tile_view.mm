f53bc89fedd1c5775c487b6daa91f7c76abcb021
Federica Germinario <fedegermi@google.com>
2024-02-16 16:53:07 +0000

    [ios] Fix hover effect of tile views in NTP
    This CL fixes hover effect for:
     - ContentSuggestionsMostVisitedTileView
     - ContentSuggestionsShortcutTileView
    
    Bug: 324375467, 324375475
    Change-Id: Ic88c54dc96fedfd61c0123b493478f280fadcc60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5300243
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1261699}

--- ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_tile_view.mm
+++ ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_tile_view.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 #import "ios/chrome/common/ui/util/dynamic_type_util.h"
+#import "ios/public/provider/chrome/browser/raccoon/raccoon_api.h"
 
 namespace {
 
@@ -20,6 +21,8 @@ const CGFloat kIconSize = 56;
 const CGFloat kMagicStackIconSize = 52;
 // Standard width of tiles.
 const CGFloat kPreferredMaxWidth = 74;
+// Image container corner radius.
+const CGFloat kCornerRadius = 8.0;
 
 }  // namespace
 
@@ -49,6 +52,12 @@ const CGFloat kPreferredMaxWidth = 74;
 
     _imageContainerView = [[UIView alloc] init];
     _imageContainerView.translatesAutoresizingMaskIntoConstraints = NO;
+    if (ios::provider::IsRaccoonEnabled()) {
+      if (@available(iOS 17.0, *)) {
+        _imageContainerView.hoverStyle = [UIHoverStyle
+            styleWithShape:[UIShape rectShapeWithCornerRadius:kCornerRadius]];
+      }
+    }
 
     // Use original rounded-square background image for Shorcuts regardless of
     // if it is in the Magic Stack.
@@ -138,7 +147,7 @@ const CGFloat kPreferredMaxWidth = 74;
       [UIPointerHighlightEffect effectWithPreview:preview];
   UIPointerShape* shape =
       [UIPointerShape shapeWithRoundedRect:_imageContainerView.frame
-                              cornerRadius:8.0];
+                              cornerRadius:kCornerRadius];
   return [UIPointerStyle styleWithEffect:effect shape:shape];
 }
 
