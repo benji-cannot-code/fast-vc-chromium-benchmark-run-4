97ec79e1e4841af9527fda5c2626343e0ec84b83
Nicolas Dossou-Gbete <dgn@chromium.org>
2024-02-16 15:46:51 +0000

    🧇 Mark for prefs::kSearchEnginesChoiceProfile for deprecation
    This pref was not read or written since Nov 2023 (usage removed in
    https://crrev.com/c/5018001) but the cleanup had not been done. This
    CL now marks it deprecated, cleans up old values, and removes
    associated useless codepaths
    
    Bug: 309936758
    Change-Id: I342f7dd48ee4a1c87ddc7c88405ecf957a32e8d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5300589
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261658}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -994,6 +994,9 @@ constexpr std::array<const char*, 6u>
 };
 #endif
 
+// Deprecated 02/2024
+constexpr char kSearchEnginesChoiceProfile[] = "search_engines.choice_profile";
+
 // Register local state used only for migration (clearing or moving to a new
 // key).
 void RegisterLocalStatePrefsForMigration(PrefRegistrySimple* registry) {
@@ -1107,6 +1110,9 @@ void RegisterLocalStatePrefsForMigration(PrefRegistrySimple* registry) {
 #if BUILDFLAG(IS_MAC)
   registry->RegisterBooleanPref(kScreenTimeEnabled, true);
 #endif
+
+  // Deprecated 02/2024.
+  registry->RegisterFilePathPref(kSearchEnginesChoiceProfile, base::FilePath());
 }
 
 // Register prefs used only for migration (clearing or moving to a new key).
@@ -1521,7 +1527,6 @@ void RegisterLocalState(PrefRegistrySimple* registry) {
   metrics::TabStatsTracker::RegisterPrefs(registry);
   performance_manager::user_tuning::prefs::RegisterLocalStatePrefs(registry);
   RegisterBrowserPrefs(registry);
-  SearchEngineChoiceDialogService::RegisterLocalStatePrefs(registry);
   SearchEngineChoiceClientSideTrial::RegisterLocalStatePrefs(registry);
   speech::SodaInstaller::RegisterLocalStatePrefs(registry);
   StartupBrowserCreator::RegisterLocalStatePrefs(registry);
@@ -2264,6 +2269,10 @@ void MigrateObsoleteLocalStatePrefs(PrefService* local_state) {
 #if BUILDFLAG(IS_MAC)
   local_state->ClearPref(kScreenTimeEnabled);
 #endif
+
+  // Added 02/2024
+  local_state->ClearPref(kSearchEnginesChoiceProfile);
+
   // Please don't delete the following line. It is used by PRESUBMIT.py.
   // END_MIGRATE_OBSOLETE_LOCAL_STATE_PREFS
 
