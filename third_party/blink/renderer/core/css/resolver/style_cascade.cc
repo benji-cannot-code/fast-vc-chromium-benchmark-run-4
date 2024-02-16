4aa1e3caa6e286b25c0b9774f8ad057075647a23
Steinar H. Gunderson <sesse@chromium.org>
2024-02-16 15:14:38 +0000

    [css-functions] Add implicit calc() as needed.
    When resolving a function argument or return value to a given type,
    add calc() around it if the type is amenable to it (i.e., is a
    numeric type). This makes them much more ergonomic to write, e.g.
    instead of writing
    
      @return calc(--foo(calc(arg(--x) * 2)) + 3);
    
    you can write
    
      @return --foo(arg(--x) * 2) + 3;
    
    which is much easier to read.
    
    Since this would leave a stray calc() (e.g. calc(10)) on the return
    value if we added it to a non-registered or universal-syntax custom
    property, we now also resolve the resulting value against the element
    as if it were against a registered custom property of the given type.
    This will then also resolve units when possible, further simplifying
    the expression.
    
    Bug: 325504770
    Change-Id: Ic64984c0db9c5bf33a0d66194a873b8dd0429a5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5280382
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261639}

--- third_party/blink/renderer/core/css/resolver/style_cascade.cc
+++ third_party/blink/renderer/core/css/resolver/style_cascade.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/resolver/cascade_resolver.h"
 #include "third_party/blink/renderer/core/css/resolver/scoped_style_resolver.h"
 #include "third_party/blink/renderer/core/css/resolver/style_builder.h"
+#include "third_party/blink/renderer/core/css/resolver/style_builder_converter.h"
 #include "third_party/blink/renderer/core/css/resolver/style_resolver_state.h"
 #include "third_party/blink/renderer/core/css/style_engine.h"
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
@@ -1388,11 +1389,20 @@ bool StyleCascade::ResolveFunctionInto(StringView function_name,
 // through a large tree of function calls.
 const CSSValue* StyleCascade::ResolveFunctionExpression(
     StringView expr,
-    const CSSSyntaxDefinition& type,
+    const StyleRuleFunction::Type& type,
     CascadeResolver& resolver,
     const CSSParserContext& context,
     const FunctionContext& function_context) {
   TokenSequence resolved_expr;
+
+  // See documentation on should_add_implicit_calc.
+  if (type.should_add_implicit_calc) {
+    static const char kCalcToken[] = "calc";
+    static const char kCalcStart[] = "calc(";
+    resolved_expr.Append(
+        CSSParserToken(kFunctionToken, kCalcToken, CSSParserToken::kBlockStart),
+        kCalcStart);
+  }
 
   CSSTokenizer tokenizer(expr);
   CSSParserTokenStream argument_stream(tokenizer);
@@ -1401,9 +1411,25 @@ const CSSValue* StyleCascade::ResolveFunctionExpression(
     return nullptr;
   }
 
-  return type.Parse(CSSTokenizedValue{resolved_expr.TokenRange(),
-                                      resolved_expr.OriginalText()},
-                    context, /*is_animation_tainted=*/false);
+  if (type.should_add_implicit_calc) {
+    static const char kCalcEnd[] = ")";
+    resolved_expr.Append(
+        CSSParserToken(kRightParenthesisToken, CSSParserToken::kBlockEnd),
+        kCalcEnd);
+  }
+
+  const CSSValue* value =
+      type.syntax.Parse(CSSTokenizedValue{resolved_expr.TokenRange(),
+                                          resolved_expr.OriginalText()},
+                        context, /*is_animation_tainted=*/false);
+  if (!value) {
+    return nullptr;
+  }
+
+  // Resolve the value as if it were a registered property, to get rid of
+  // extraneous calc(), resolve lengths and so on.
+  return &StyleBuilderConverter::ConvertRegisteredPropertyValue(state_, *value,
+                                                                &context);
 }
 
 bool StyleCascade::ResolveEnvInto(CSSParserTokenStream& stream,
