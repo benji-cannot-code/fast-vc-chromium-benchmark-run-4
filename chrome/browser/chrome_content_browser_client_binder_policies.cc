84eb507fa468c7201413c7e0860c2caa328b1e65
Robert Lin <robertlin@chromium.org>
2024-02-16 11:39:36 +0000

    preloading: Grants extensions::mojom::RendererHost
    This CL grants extensions::mojom::RendererHost to allow activity log
    when prerendering instead of deferring until prerender activation.
    
    Bug: 1259007
    Change-Id: Ifb3df407a37720f71c17dad1a719c546e560d752
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5101989
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261573}

--- chrome/browser/chrome_content_browser_client_binder_policies.cc
+++ chrome/browser/chrome_content_browser_client_binder_policies.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 #include "extensions/common/mojom/event_router.mojom.h"
 #include "extensions/common/mojom/frame.mojom.h"
+#include "extensions/common/mojom/renderer_host.mojom.h"
 #endif
 
 namespace {
@@ -54,6 +55,12 @@ void RegisterPoliciesForChannelAssociatedInterfaces(
   // Grants Prerendering to use EventRouter, and sensitive behaviors are
   // prohibited by permission request boundary.
   policy_map.SetAssociatedPolicy<extensions::mojom::EventRouter>(
+      content::MojoBinderAssociatedPolicy::kGrant);
+
+  // Grants Prerendering to use RendererHost. This API is used for activity log,
+  // and it is safe to grant this API instead of default API behavior (deferring
+  // until prerender activation).
+  policy_map.SetAssociatedPolicy<extensions::mojom::RendererHost>(
       content::MojoBinderAssociatedPolicy::kGrant);
 #endif
 }
