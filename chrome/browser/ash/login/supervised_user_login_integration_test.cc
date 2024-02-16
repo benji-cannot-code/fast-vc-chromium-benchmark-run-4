4b7f769ef255240a29a91593317af67b6a4a8a18
Joel Hockey <joelhockey@chromium.org>
2024-02-16 00:15:39 +0000

    Disable flaky chromeos_integration_tests
    Bug: 318616623
    Bug: 325519819
    Change-Id: I9bede6332996e55ab6d8b61a481b34e78638d5d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5300616
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Courtney Wong <courtneywong@chromium.org>
    Owners-Override: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261416}

--- chrome/browser/ash/login/supervised_user_login_integration_test.cc
+++ chrome/browser/ash/login/supervised_user_login_integration_test.cc
@@ -74,7 +74,9 @@ class SupervisedUserLoginIntegrationTest
   }
 };
 
-IN_PROC_BROWSER_TEST_F(SupervisedUserLoginIntegrationTest, TestUnicornLogin) {
+// Flaky tests disabled crbug.com/318616623.
+IN_PROC_BROWSER_TEST_F(SupervisedUserLoginIntegrationTest,
+                       DISABLED_TestUnicornLogin) {
   SetupContextWidget();
 
   login_mixin().Login();
@@ -84,7 +86,9 @@ IN_PROC_BROWSER_TEST_F(SupervisedUserLoginIntegrationTest, TestUnicornLogin) {
   VerifyPolicies();
 }
 
-IN_PROC_BROWSER_TEST_F(SupervisedUserLoginIntegrationTest, TestGellerLogin) {
+// Flaky tests disabled crbug.com/318616623.
+IN_PROC_BROWSER_TEST_F(SupervisedUserLoginIntegrationTest,
+                       DISABLED_TestGellerLogin) {
   SetupContextWidget();
 
   delegate_.set_user_type(
@@ -96,7 +100,9 @@ IN_PROC_BROWSER_TEST_F(SupervisedUserLoginIntegrationTest, TestGellerLogin) {
   VerifyPolicies();
 }
 
-IN_PROC_BROWSER_TEST_F(SupervisedUserLoginIntegrationTest, TestGriffinLogin) {
+// Flaky tests disabled crbug.com/318616623.
+IN_PROC_BROWSER_TEST_F(SupervisedUserLoginIntegrationTest,
+                       DISABLED_TestGriffinLogin) {
   SetupContextWidget();
 
   delegate_.set_user_type(
