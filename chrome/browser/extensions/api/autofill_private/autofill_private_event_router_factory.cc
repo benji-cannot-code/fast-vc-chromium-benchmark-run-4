26f1bff9bdbe429b0cd84b74550cb3924c7f52f4
Florian Leimgruber <fleimgruber@google.com>
2024-02-16 12:49:30 +0000

    Remove PDMObserver::OnPersonalDataSyncStateChanged()
    Only the AutofillPrivateEventRouter is listening for this event. In an
    effort to simplify the PDMObserver, as part of go/split-pdm, this CL
    removes the event from PDMObserver and instead observes the sync service
    from AutofillPrivateEventRouter directly.
    
    As a drive-by clean-up, the existing PDMObserver in
    AutofillPrivateEventRouter is refactored to a base::ScopedObservation<>.
    
    Bug: b/322170538
    Change-Id: I0ebd362fa78ee227a517ff8b6300e6d29ba66da0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5234272
    Reviewed-by: Dmitry Vykochko <vykochko@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1261590}

--- chrome/browser/extensions/api/autofill_private/autofill_private_event_router_factory.cc
+++ chrome/browser/extensions/api/autofill_private/autofill_private_event_router_factory.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/autofill/personal_data_manager_factory.h"
 #include "chrome/browser/extensions/api/autofill_private/autofill_private_event_router.h"
+#include "chrome/browser/sync/sync_service_factory.h"
 #include "content/public/browser/browser_context.h"
 #include "extensions/browser/extension_system_provider.h"
 #include "extensions/browser/extensions_browser_client.h"
@@ -39,6 +40,7 @@ AutofillPrivateEventRouterFactory::AutofillPrivateEventRouterFactory()
               .Build()) {
   DependsOn(ExtensionsBrowserClient::Get()->GetExtensionSystemFactory());
   DependsOn(autofill::PersonalDataManagerFactory::GetInstance());
+  DependsOn(SyncServiceFactory::GetInstance());
 }
 
 std::unique_ptr<KeyedService>
