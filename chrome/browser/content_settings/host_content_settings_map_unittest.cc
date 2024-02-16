9d82791933b7ae6716c9906e8e7f056bfe39cc16
Christian Dullweber <dullweber@chromium.org>
2024-02-16 18:00:37 +0000

    ContentSettings: Ensure precedence of off_the_record settings
    Incognito settings are queried before regular mode settings and we need
    to preserve that order when querying cookie settings through an index
    in the network service.
    
    Bug: 316530672
    Change-Id: If99a0ed3eb4511636b8d8946d929c01bc0c5a4b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5303438
    Reviewed-by: Patricia Alfonso <trishalfonso@google.com>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261755}

--- chrome/browser/content_settings/host_content_settings_map_unittest.cc
+++ chrome/browser/content_settings/host_content_settings_map_unittest.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/content_settings_types.h"
 #include "components/content_settings/core/common/content_settings_utils.h"
 #include "components/content_settings/core/common/features.h"
+#include "components/content_settings/core/common/host_indexed_content_settings.h"
 #include "components/content_settings/core/common/pref_names.h"
 #include "components/content_settings/core/test/content_settings_test_utils.h"
 #include "components/permissions/features.h"
@@ -1041,6 +1042,45 @@ TEST_P(IndexedHostContentSettingsMapTest, IncognitoPartialInheritDefault) {
   EXPECT_EQ(
       CONTENT_SETTING_ASK,
       otr_map->GetContentSetting(host, host, ContentSettingsType::GEOLOCATION));
+}
+
+TEST_P(IndexedHostContentSettingsMapTest, IncognitoInheritCookies) {
+  TestingProfile profile;
+  Profile* otr_profile =
+      profile.GetPrimaryOTRProfile(/*create_if_needed=*/true);
+  HostContentSettingsMap* map =
+      HostContentSettingsMapFactory::GetForProfile(&profile);
+  HostContentSettingsMap* otr_map =
+      HostContentSettingsMapFactory::GetForProfile(otr_profile);
+
+  GURL url("http://example.com/");
+
+  map->SetContentSettingCustomScope(
+      ContentSettingsPattern::FromURLNoWildcard(url),
+      ContentSettingsPattern::FromURLNoWildcard(url),
+      ContentSettingsType::COOKIES, CONTENT_SETTING_SESSION_ONLY);
+
+  otr_map->SetContentSettingCustomScope(ContentSettingsPattern::FromURL(url),
+                                        ContentSettingsPattern::FromURL(url),
+                                        ContentSettingsType::COOKIES,
+                                        CONTENT_SETTING_BLOCK);
+
+  EXPECT_EQ(CONTENT_SETTING_SESSION_ONLY,
+            map->GetContentSetting(url, url, ContentSettingsType::COOKIES));
+  EXPECT_EQ(CONTENT_SETTING_BLOCK,
+            otr_map->GetContentSetting(url, url, ContentSettingsType::COOKIES));
+
+  auto indices = content_settings::HostIndexedContentSettings::Create(
+      otr_map->GetSettingsForOneType(ContentSettingsType::COOKIES));
+  ContentSetting result = CONTENT_SETTING_ALLOW;
+  for (const auto& index : indices) {
+    auto* found = index.Find(url, url);
+    if (found) {
+      result = content_settings::ValueToContentSetting(found->second.value);
+      break;
+    }
+  }
+  EXPECT_EQ(CONTENT_SETTING_BLOCK, result);
 }
 
 TEST_P(IndexedHostContentSettingsMapTest, IncognitoDontInheritSetting) {
