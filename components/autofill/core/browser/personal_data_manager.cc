5a7f7a22ac2d14a2e696b9606886ae07d8407a1a
Florian Leimgruber <fleimgruber@google.com>
2024-02-16 15:59:39 +0000

    Change semantics of PDMObserver::OnPersonalDataChanged()
    Two of the PDMObserver's events are OnPersonalDataChanged() and
    OnPersonalDataFinishedProfileTasks(). The goal of these changes is to
    remove OnPersonalDataFinishedProfileTasks() and replace its uses with
    OnPersonalDataChanged().
    
    Currently, they work as follows:
    - OnPersonalDataFinishedProfileTasks() is called when all pending read
      and write operations have finished. Contrary to what the name suggest,
      this includes non-address related changes; it is blocked on and
      triggered by payment related changes.
    - OnPersonalDataChanged() is called whenever all pending read operations
      have finished, and after every address write operation that finishes.
      Moreover, OnPersonalDataChanged() is also called when the user
      disables payments sync via settings, since this remasks full server
      cards (which are deprecated anyways... see b/40939195).
    
    Some examples:
    - pdm.AddProfile(profile1);
      pdm.AddProfile(profile2);
      Will trigger two OnPersonalDataChanged() events, one after the first
      AddProfile() has finished and second one, followed by one
      OnPersonalDataFinishedProfileTasks() event*.
    - pdm.AddCreditCard(card1);
      pdm.AddCreditCard(card2);
      Will trigger one OnPersonalDataChanged() and one
      OnPersonalDataFinishedProfileTasks() event, once both AddCreditCard()
      operations have finished.
    - pdm.Refresh()
      Same events as in the previous example.
    
    This is confusing and makes go/split-pdm more difficult.
    
    This CL changes the semantics of OnPersonalDataChanged() to
    OnPersonalDataFinishedProfileTasks()'s "called when all pending read
    and write operations have finished". With this change,
    OnPersonalDataFinishedProfileTasks() becomes obsolete and is removed in
    crrev.com/c/5291015.
    
    * There are some more special cases when the passed in profile is empty.
      In this case, OnPersonalDataChanged() followed by
      OnPersonalDataFinishedProfileTasks() is triggered after every add
      immediately, since no database is involved/the function early returns.
      Similar special cases exist when removing a profile that doesn't
      exist, etc.
    
    Bug: b/322170538
    Change-Id: I1fcff512fa980acf9485dbd39d91a827f1e9bbb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5290994
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1261665}

--- components/autofill/core/browser/personal_data_manager.cc
+++ components/autofill/core/browser/personal_data_manager.cc
@@ -649,7 +649,9 @@ void PersonalDataManager::OnSyncPaymentsIntegrationEnabledChanged(
     // Re-mask all server cards when the user turns off wallet card
     // integration.
     ResetFullServerCards();
-    NotifyPersonalDataObserver();
+    for (PersonalDataManagerObserver& observer : observers_) {
+      observer.OnPersonalDataChanged();
+    }
   }
 }
 
@@ -2385,17 +2387,16 @@ void PersonalDataManager::OnUserAcceptedUpstreamOffer() {
 }
 
 void PersonalDataManager::NotifyPersonalDataObserver() {
-  bool pending_changes =
-      IsAwaitingPendingAddressChanges() || HasPendingPaymentQueries();
+  if (IsAwaitingPendingAddressChanges() || HasPendingPaymentQueries()) {
+    return;
+  }
   for (PersonalDataManagerObserver& observer : observers_) {
     observer.OnPersonalDataChanged();
   }
-  if (!pending_changes) {
-    // Call `OnPersonalDataFinishedProfileTasks()` in a separate loop as
-    // the observers might have removed themselves in OnPersonalDataChanged
-    for (PersonalDataManagerObserver& observer : observers_) {
-      observer.OnPersonalDataFinishedProfileTasks();
-    }
+  // Call `OnPersonalDataFinishedProfileTasks()` in a separate loop as
+  // the observers might have removed themselves in OnPersonalDataChanged
+  for (PersonalDataManagerObserver& observer : observers_) {
+    observer.OnPersonalDataFinishedProfileTasks();
   }
 }
 
