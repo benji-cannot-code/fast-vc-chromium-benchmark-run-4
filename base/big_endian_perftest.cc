c4dcf5ddbbe86597cd82d406a4b3c03fb4474dc2
Egor Pasko <pasko@chromium.org>
2024-02-16 17:07:51 +0000

    base_perftests: Register google_benchmark tests as gtests
    Currently BENCHMARK(BM_ReturnIfError_Ok) expands as:
    
    static ::benchmark::internal::Benchmark*
        benchmark_uniq_2_benchmark_ [[maybe_unused]] =
            (::benchmark::internal::RegisterBenchmarkInternal(
                new ::benchmark::internal::FunctionBenchmark(
                    "BM_ReturnIfError_Ok",
                    BM_ReturnIfError_Ok)));
    
    This registers the benchmark function in a global instance during static
    init.
    
    The main() in run_all_perftests.cc runs all tests. It allows matching
    tests via a commandline flag parsed by google_benchmark, but the
    google_benchmark flags ignore gtest flags.
    
    To run some specific test from base_perftests on Android we do:
    
    ```
    autoninja -C out/AndroidReleaseOfficial base_perftests
    out/AndroidReleaseOfficial/bin/run_base_perftests \
        '--gtest_filter=MyBenchmark*' -v
    ```
    
    This asks the binary to --gtest_list_tests before attempting to run any
    existing gtest benchmark. As we learned above, google_benchmark parts
    ignore this gtest flag and proceed with running all registered
    benchmarks. This hits a timeout of 30 seconds on a modern device because
    we hardcoded warmup to take at least 24 seconds in
    big_endian_perftest.cc.
    
    It would be nice to keep the legacy --gtest_filter interface in a
    working state during transition.
    
    As a quick fix I suggest to remove the 31 second delay by registering
    the google_benchmark functions inside gtest, like so:
    ```
    TEST(ExpectedMacrosPerfTest, BM_ReturnIfError_Ok) {
      BENCHMARK(BM_ReturnIfError_Ok);
    }
    ```
    
    This is a change to do so for base_perftests.
    
    Bug: b/40251982
    Change-Id: Ibee0912fa1b6dfc0593bc55a54309002f9a0e950
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5300560
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261706}

--- base/big_endian_perftest.cc
+++ base/big_endian_perftest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/containers/span.h"
+#include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/google_benchmark/src/include/benchmark/benchmark.h"
 
 namespace base {
@@ -100,13 +101,18 @@ void BM_ReadBigEndianMisaligned(::benchmark::State& state) {
   BENCHMARK(function<int32_t>)->MinWarmUpTime(1.0);  \
   BENCHMARK(function<uint32_t>)->MinWarmUpTime(1.0); \
   BENCHMARK(function<int64_t>)->MinWarmUpTime(1.0);  \
-  BENCHMARK(function<uint64_t>)->MinWarmUpTime(1.0); \
-  typedef int force_semicolon
+  BENCHMARK(function<uint64_t>)->MinWarmUpTime(1.0);
 
-BENCHMARK_FOR_INT_TYPES(BM_WriteBigEndianAligned);
-BENCHMARK_FOR_INT_TYPES(BM_WriteBigEndianMisaligned);
-BENCHMARK_FOR_INT_TYPES(BM_ReadBigEndianAligned);
-BENCHMARK_FOR_INT_TYPES(BM_ReadBigEndianMisaligned);
+// Register the benchmarks as a GTest test. This allows using legacy
+// --gtest_filter and --gtest_list_tests.
+// TODO(https://crbug.com/40251982): Clean this up after transitioning to
+// --benchmark_filter and --benchmark_list_tests.
+TEST(BigEndianPerfTest, All) {
+  BENCHMARK_FOR_INT_TYPES(BM_WriteBigEndianAligned);
+  BENCHMARK_FOR_INT_TYPES(BM_WriteBigEndianMisaligned);
+  BENCHMARK_FOR_INT_TYPES(BM_ReadBigEndianAligned);
+  BENCHMARK_FOR_INT_TYPES(BM_ReadBigEndianMisaligned);
+}
 
 #undef BENCHMARK_FOR_INT_TYPES
 
