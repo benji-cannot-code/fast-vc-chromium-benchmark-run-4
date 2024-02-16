04e8ce269089a2bf0ce490286c310df38e1667df
François Doray <fdoray@chromium.org>
2024-02-16 16:55:43 +0000

    [throttling] Track agent cluster id in FrameScheduler.
    In preparation for a feature that will prevent throttling of
    frames that are same-agent with a visible frame, this CL tracks
    agent cluster id in frame scheduler.
    
    Note: Prior to this CL, a frame scheduler could already access the
    agent cluster id via its delegate, but couldn't be notified of agent
    cluster id changes, which is required to adjust throttling policy.
    
    Bug: 323790692
    Change-Id: Icf66d4abc1b560477ff88bdab550ff6984623552
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5278944
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261700}

--- third_party/blink/renderer/platform/scheduler/main_thread/frame_scheduler_impl.h
+++ third_party/blink/renderer/platform/scheduler/main_thread/frame_scheduler_impl.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequence_manager/task_queue.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/time.h"
+#include "base/unguessable_token.h"
 #include "net/base/request_priority.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
 #include "third_party/blink/public/platform/task_type.h"
@@ -98,6 +99,9 @@ class PLATFORM_EXPORT FrameSchedulerImpl : public FrameScheduler,
 
   void SetCrossOriginToNearestMainFrame(bool cross_origin) override;
   bool IsCrossOriginToNearestMainFrame() const override;
+
+  void SetAgentClusterId(
+      const base::UnguessableToken& agent_cluster_id) override;
 
   void SetIsAdFrame(bool is_ad_frame) override;
   bool IsAdFrame() const override;
@@ -314,6 +318,8 @@ class PLATFORM_EXPORT FrameSchedulerImpl : public FrameScheduler,
   // Whether this scheduler is created for a frame that is contained
   // inside an embedded frame tree. See /docs/frame_trees.md.
   const bool is_in_embedded_frame_tree_;
+
+  base::UnguessableToken agent_cluster_id_ = base::UnguessableToken::Null();
 
   bool is_ad_frame_ = false;
 
