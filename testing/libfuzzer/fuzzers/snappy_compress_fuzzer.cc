0dace6f2980860417d59ed9bcaba4640b3deac65
Hans Wennborg <hans@chromium.org>
2024-02-16 18:20:12 +0000

    Add missing includes uncovered by libc++ change
    These files were previously relying on a transitive include in libc++,
    which is now gone.
    
    Bug: 325103513
    Change-Id: I06fe8136178e7a9df128741280479bdf2adb4447
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5301349
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261767}

--- testing/libfuzzer/fuzzers/snappy_compress_fuzzer.cc
+++ testing/libfuzzer/fuzzers/snappy_compress_fuzzer.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/snappy/src/snappy.h"
 
+#include <cstdlib>
+
 #define FUZZING_ASSERT(condition)                                      \
   if (!(condition)) {                                                  \
     fprintf(stderr, "%s\n", "Fuzzing Assertion Failure: " #condition); \
