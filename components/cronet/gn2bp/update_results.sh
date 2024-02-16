5a8bb608c8d4b9590922371f214c2cd099ffcf32
Mohannad Farrag <aymanm@google.com>
2024-02-16 18:07:16 +0000

    Add gn2bp checker to CI
    * Implemented a GN2BP validation script that would generate the desc_*.json
    files on the fly during execution then run `gn2bp` script and make sure that it
    executes with no error. This will allow the Cronet team to catch any changes to
    the script as early as possible which decreases the time to import significantly.
    
    * After this change, the gn2bp source of truth will be in Chromium and it should be
    only used from Chromium.
    
    * Add the new target to the CI(temporarily) until we have calculated how often
    does it fail and if it's reasonable to switch it over to CQ.
    
    Bug: 1523214
    Cq-Include-Trybots: luci.chromium.try:android-cronet-arm-dbg,android-cronet-arm64-dbg,android-cronet-arm64-rel,android-cronet-x64-dbg,android-cronet-x64-rel,android-cronet-x86-dbg,android-cronet-x86-rel
    Change-Id: Ib705d06ddc073f78579636971f01415bce71bc5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5250569
    Auto-Submit: Mohannad Farrag <aymanm@google.com>
    Commit-Queue: Haiyang Pan <hypan@google.com>
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1261759}

---
+++ components/cronet/gn2bp/update_results.sh
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+
+# This script is expected to run after gen_android_bp is modified.
+#
+#   ./update_result.sh
+#
+# TARGETS contains targets which are supported by gen_android_bp and
+# this script generates Android.bp.swp from TARGETS.
+# This makes it easy to realize unintended impact/degression on
+# previously supported targets.
+
+set -eux
+
+BASEDIR=$(dirname "$0")
+$BASEDIR/gen_android_bp --desc $BASEDIR/desc_x64.json --desc $BASEDIR/desc_x86.json \
+--desc $BASEDIR/desc_arm.json --desc $BASEDIR/desc_arm64.json --out $BASEDIR/Android.bp
