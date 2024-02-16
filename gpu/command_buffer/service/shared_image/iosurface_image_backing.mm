f72c0a305fd30bbc39a49b7cb3d0754125887164
Colin Blundell <blundell@chromium.org>
2024-02-16 08:45:16 +0000

    [//gpu] Convert checks for SI webgpu usage to WEBGPU_READ | WEBGPU_WRITE
    This CL converts service-side checks for WebGPU usage in a given
    SharedImage to check WEBGPU_READ | WEBGPU_WRITE rather than WEBGPU.
    There are no functional changes, as WEBGPU is a constant that expands
    to WEBGPU_READ | WEBGPU_WRITE. This will allow us to subsequently (1)
    eliminate WEBGPU and (2) refine all WEBGPU_READ | WEBGPU_WRITE usage
    based on where raster reads and writes are actually necessary.
    
    Bug: 324930395
    Change-Id: Ide792512f8c84a05c6181397703c294d0482aa74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5299734
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261532}

--- gpu/command_buffer/service/shared_image/iosurface_image_backing.mm
+++ gpu/command_buffer/service/shared_image/iosurface_image_backing.mm
@@ -1643,7 +1643,8 @@ bool IOSurfaceImageBacking::IOSurfaceBackingEGLStateBeginAccess(
     return true;
   }
 
-  if (usage() & SHARED_IMAGE_USAGE_WEBGPU &&
+  if (usage() &
+          (SHARED_IMAGE_USAGE_WEBGPU_READ | SHARED_IMAGE_USAGE_WEBGPU_WRITE) &&
       gl::GetANGLEImplementation() == gl::ANGLEImplementation::kMetal) {
     // If this image could potentially be shared with WebGPU's Metal
     // device, it's necessary to synchronize between the two devices.
@@ -1731,7 +1732,8 @@ void IOSurfaceImageBacking::IOSurfaceBackingEGLStateEndAccess(
 
   // If this image could potentially be shared with Metal via WebGPU, then flush
   // the GL context to ensure Metal will see it.
-  if (usage() & SHARED_IMAGE_USAGE_WEBGPU) {
+  if (usage() &
+      (SHARED_IMAGE_USAGE_WEBGPU_READ | SHARED_IMAGE_USAGE_WEBGPU_WRITE)) {
     gl::GLApi* api = gl::g_current_gl_context;
     api->glFlushFn();
   }
