41fb811c59dffb1c1921ee0b62ce846b68dd7933
mlcui <mlcui@google.com>
2024-02-16 01:43:19 +0000

    picker: Limit GIFs loaded from GIF search to 4
    This matches the UX mocks.
    
    Bug: b:324984457
    Change-Id: I6ca0e03ad40800e271106ad7159f1e40868020d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5299196
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: John Palmer <jopalmer@google.com>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261446}

--- chrome/browser/ui/webui/ash/emoji/gif_tenor_api_fetcher.h
+++ chrome/browser/ui/webui/ash/emoji/gif_tenor_api_fetcher.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_WEBUI_ASH_EMOJI_GIF_TENOR_API_FETCHER_H_
 
 #include <memory>
+#include <optional>
 
 #include "chrome/browser/ui/webui/ash/emoji/emoji_picker.mojom.h"
 #include "components/endpoint_fetcher/endpoint_fetcher.h"
@@ -51,7 +52,8 @@ class GifTenorApiFetcher {
       TenorGifsApiCallback callback,
       const scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
       const std::string& query,
-      const std::optional<std::string>& pos);
+      const std::optional<std::string>& pos,
+      std::optional<int> limit = std::nullopt);
 
   // Fetch tenor API Posts endpoint
   void FetchGifsByIds(
