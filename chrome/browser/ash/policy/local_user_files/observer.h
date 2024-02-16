b5421df571c1b73e25a1e6911f5eb9a45615b6a9
Aida Zolic <aidazolic@google.com>
2024-02-16 13:15:29 +0000

    [skyvault] Add observer for LocalUserFilesAllowed
    Adds an Observer interface to get notified of LocalUserFilesAllowed
    pref changes. This will be used by e.g. by file manager EventRouter and
    VolumeManager to hide/show local folders when the policy is set.
    
    R=ayaelattar@chromium.org, petermarshall@chromium.org
    
    Fixed: b/325459470
    Change-Id: I1d82b55f581824656952f76711ba2c84c12f6344
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5300424
    Auto-Submit: Aida Zolic <aidazolic@chromium.org>
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261593}

---
+++ chrome/browser/ash/policy/local_user_files/observer.h
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_POLICY_LOCAL_USER_FILES_OBSERVER_H_
+#define CHROME_BROWSER_ASH_POLICY_LOCAL_USER_FILES_OBSERVER_H_
+
+#include "components/prefs/pref_change_registrar.h"
+
+namespace policy::local_user_files {
+
+// Observer interface for LocalUserFilesEnabled policy changes.
+class Observer {
+ public:
+  Observer();
+  virtual ~Observer();
+
+  // Called when the value of the LocalUserFilesEnabled policy changes.
+  virtual void OnLocalUserFilesPolicyChanged() {}
+
+ private:
+  std::unique_ptr<PrefChangeRegistrar> pref_change_registrar_;
+};
+
+}  // namespace policy::local_user_files
+
+#endif  // CHROME_BROWSER_ASH_POLICY_LOCAL_USER_FILES_OBSERVER_H_
