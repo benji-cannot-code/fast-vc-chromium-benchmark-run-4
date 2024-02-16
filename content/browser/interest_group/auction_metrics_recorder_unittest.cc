97b37b296bbd8bc95cd5fe6765977ec144c02990
Dan McArdle <dmcardle@chromium.org>
2024-02-16 14:04:11 +0000

    Add Protected Audience NumOwnersWithoutInterestGroups to UKM
    This CL corrects an omission in the original Private Aggregation UKMs,
    approved in go/fledge-auction-latency-ukm-collection-review and tracked
    by <https://crbug.com/40260377>. It adds NumOwnersWithoutInterestGroups,
    which is closely related to the existing NumOwnersWithInterestGroups.
    
    This follows up on a prior CL that added the equivalent UMA histogram:
    <https://crrev.com/c/5250476>.
    
    Bug: 41496623
    Change-Id: Ic7fe8ea7ea126baadf7e9db567ae5bc22e8c43ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5296883
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Sun Yueru <yrsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261608}

--- content/browser/interest_group/auction_metrics_recorder_unittest.cc
+++ content/browser/interest_group/auction_metrics_recorder_unittest.cc
@@ -142,7 +142,7 @@ TEST_F(AuctionMetricsRecorderTest, NumInterestGroups) {
       /*sample=*/42, /*expected_bucket_count=*/1);
 }
 
-TEST_F(AuctionMetricsRecorderTest, NumOwnersWithInterestGroupsName) {
+TEST_F(AuctionMetricsRecorderTest, NumOwnersWithInterestGroups) {
   recorder().SetNumOwnersWithInterestGroups(62);
   recorder().OnAuctionEnd(AuctionResult::kSuccess);
 
@@ -153,10 +153,12 @@ TEST_F(AuctionMetricsRecorderTest, NumOwnersWithInterestGroupsName) {
       /*sample=*/62, /*expected_bucket_count=*/1);
 }
 
-TEST_F(AuctionMetricsRecorderTest, NumOwnersExcluded) {
+TEST_F(AuctionMetricsRecorderTest, NumOwnersWithoutInterestGroups) {
   recorder().SetNumOwnersWithoutInterestGroups(62);
   recorder().OnAuctionEnd(AuctionResult::kSuccess);
 
+  // 62 becomes 58 because of bucketing
+  EXPECT_EQ(GetMetricValue(UkmEntry::kNumOwnersWithoutInterestGroupsName), 58);
   histogram_tester().ExpectUniqueSample(
       /*name=*/"Ads.InterestGroup.Auction.NumOwnersWithoutInterestGroups",
       /*sample=*/62, /*expected_bucket_count=*/1);
