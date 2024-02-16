adc657ac79100cdc541da01f38d6842264cd9dd3
Vishwas Uppoor <vishwasuppoor@google.com>
2024-02-16 20:23:15 +0000

    [PIX] UKM - PIX code detection latency
    Add UKM logs to measure the latency in scanning the document to find PIX
    codes.
    New UKM collection review doc: https://docs.google.com/document/d/1t1uwDIl56d27c9YyS0wigx-HVDfUgIA5xtp15hChfH0/edit?resourcekey=0-24Mv_EHTwTgwz-rTpkeBRw
    
    Bug: b/325476935
    Change-Id: Ia04f8e2a3dd8c0ba51200a8e1e213de82fde7130
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5301763
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Siddharth Shah <siashah@chromium.org>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261858}

--- components/facilitated_payments/core/browser/facilitated_payments_manager.h
+++ components/facilitated_payments/core/browser/facilitated_payments_manager.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ref.h"
 #include "base/memory/weak_ptr.h"
+#include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "components/facilitated_payments/core/browser/facilitated_payments_driver.h"
 #include "components/facilitated_payments/core/mojom/facilitated_payments_agent.mojom.h"
@@ -58,8 +59,9 @@ class FacilitatedPaymentsManager {
   friend class FacilitatedPaymentsManagerTest;
   FRIEND_TEST_ALL_PREFIXES(FacilitatedPaymentsManagerTest,
                            TestRegisterPixAllowlist);
-  FRIEND_TEST_ALL_PREFIXES(FacilitatedPaymentsManagerMetricsTest,
-                           TestProcessPixCodeDetectionResult);
+  FRIEND_TEST_ALL_PREFIXES(
+      FacilitatedPaymentsManagerMetricsTest,
+      TestProcessPixCodeDetectionResult_VerifyResultAndLatencyUkmLogged);
 
   // Register optimization guide deciders for PIX. It is an allowlist of URLs
   // where we attempt PIX code detection.
@@ -79,6 +81,10 @@ class FacilitatedPaymentsManager {
   void ProcessPixCodeDetectionResult(
       mojom::PixCodeDetectionResult result) const;
 
+  void StartPixCodeDetectionLatencyTimer();
+
+  int64_t GetPixCodeDetectionLatencyInMillis() const;
+
   raw_ref<FacilitatedPaymentsDriver> driver_;
 
   // The optimization guide decider to help determine whether the current main
@@ -89,6 +95,9 @@ class FacilitatedPaymentsManager {
   const ukm::SourceId ukm_source_id_;
 
   base::OneShotTimer pix_code_detection_triggering_timer_;
+
+  // Measures the time taken to scan the document for the PIX code.
+  base::TimeTicks pix_code_detection_latency_measuring_timestamp_;
 
   base::WeakPtrFactory<FacilitatedPaymentsManager> weak_ptr_factory_{this};
 };
