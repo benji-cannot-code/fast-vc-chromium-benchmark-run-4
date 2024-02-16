707decd094abf0a6323cf2d3a90defd57f9cebea
Michal Mazur <michamazur@google.com>
2024-02-16 21:08:19 +0000

    carrier_lock: Use real model name if possible + fixes
    Fixes:
    1. The InvalidTimestamp error need to be ignored because the modem
    accepts only newer configurations and we have to apply the same
    config on every boot.
    2. Provisioning service sometimes responds with older config after it
    sends notification through FCM. Added delay to workaround this race.
    3. Fix possible crashes when FCM notification comes at the wrong time.
    4. Prevent manager from missing a configuration update when FCM
    notification is received before the notification handler is initialized.
    
    Bug: b:324322719
    Test: run CarrierLock* unit tests
    
    Change-Id: Iee4d42dd9d9abdc673f8275119c8dc8c15d58f35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5285417
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Commit-Queue: Michał Mazur <michamazur@google.com>
    Cr-Commit-Position: refs/heads/main@{#1261888}

--- chromeos/ash/components/carrier_lock/fcm_topic_subscriber_unittest.cc
+++ chromeos/ash/components/carrier_lock/fcm_topic_subscriber_unittest.cc
@@ -52,6 +52,8 @@ class FcmTopicSubscriberTest : public testing::Test {
     fcm_->android_id_ = kTestAndroidId;
     fcm_->android_secret_ = kTestAndroidSecret;
     fcm_->set_is_testing(true);
+    fcm_->Initialize(base::BindRepeating(
+        &FcmTopicSubscriberTest::NotificationCallback, base::Unretained(this)));
   }
 
   void TearDown() override { fcm_.reset(); }
@@ -69,8 +71,6 @@ TEST_F(FcmTopicSubscriberTest, CarrierLockSubscribeTopicSuccess) {
   // Request token and subscribe with valid topic
   fcm_->SubscribeTopic(
       kFcmTopic,
-      base::BindRepeating(&FcmTopicSubscriberTest::NotificationCallback,
-                          base::Unretained(this)),
       future.GetCallback());
   EXPECT_TRUE(test_url_loader_factory_.SimulateResponseForPendingRequest(
       GURL(kFcmUrl), network::URLLoaderCompletionStatus(net::OK),
@@ -86,8 +86,8 @@ TEST_F(FcmTopicSubscriberTest, CarrierLockTestNotifications) {
   base::test::RepeatingTestFuture<bool> notifications;
 
   // Request token and subscribe with valid topic
-  fcm_->SubscribeTopic(kFcmTopic, notifications.GetCallback(),
-                       future.GetCallback());
+  fcm_->Initialize(notifications.GetCallback());
+  fcm_->SubscribeTopic(kFcmTopic, future.GetCallback());
   EXPECT_TRUE(test_url_loader_factory_.SimulateResponseForPendingRequest(
       GURL(kFcmUrl), network::URLLoaderCompletionStatus(net::OK),
       network::CreateURLResponseHead(net::HTTP_OK), std::string("{}")));
@@ -119,13 +119,9 @@ TEST_F(FcmTopicSubscriberTest, CarrierLockSubscribeTopicTwice) {
   // Request token and subscribe with valid topic
   fcm_->SubscribeTopic(
       kFcmTopic,
-      base::BindRepeating(&FcmTopicSubscriberTest::NotificationCallback,
-                          base::Unretained(this)),
       future.GetCallback());
   fcm_->SubscribeTopic(
       kFcmTopic,
-      base::BindRepeating(&FcmTopicSubscriberTest::NotificationCallback,
-                          base::Unretained(this)),
       future.GetCallback());
 
   // Wait for callback
@@ -139,8 +135,6 @@ TEST_F(FcmTopicSubscriberTest, CarrierLockSubscribeTopicFail) {
   // Request token and subscribe with empty topic
   fcm_->SubscribeTopic(
       std::string(),
-      base::BindRepeating(&FcmTopicSubscriberTest::NotificationCallback,
-                          base::Unretained(this)),
       future.GetCallback());
 
   // Wait for callback
@@ -153,8 +147,6 @@ TEST_F(FcmTopicSubscriberTest, CarrierLockGetTokenAndSubscribe) {
 
   // Only request token
   fcm_->RequestToken(
-      base::BindRepeating(&FcmTopicSubscriberTest::NotificationCallback,
-                          base::Unretained(this)),
       future.GetCallback());
 
   // Wait for callback
@@ -164,8 +156,6 @@ TEST_F(FcmTopicSubscriberTest, CarrierLockGetTokenAndSubscribe) {
   // Subscribe to valid topic
   fcm_->SubscribeTopic(
       kFcmTopic,
-      base::BindRepeating(&FcmTopicSubscriberTest::NotificationCallback,
-                          base::Unretained(this)),
       future.GetCallback());
   EXPECT_TRUE(test_url_loader_factory_.SimulateResponseForPendingRequest(
       GURL(kFcmUrl), network::URLLoaderCompletionStatus(net::OK),
