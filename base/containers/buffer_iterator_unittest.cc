7db2ecaa70220477fa51239fbd59d9c9bdc771d5
Daniel Cheng <dcheng@chromium.org>
2024-02-16 13:33:47 +0000

    Fix missing include for absl::optional in BufferIterator test.
    Discovered by building without GTEST_HAS_ABSL.
    
    Bug: 1409870
    Change-Id: I1990bb8892e81ec12cefa5666fc39223b02ce1fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5302313
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261600}

--- base/containers/buffer_iterator_unittest.cc
+++ base/containers/buffer_iterator_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/span.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace base {
