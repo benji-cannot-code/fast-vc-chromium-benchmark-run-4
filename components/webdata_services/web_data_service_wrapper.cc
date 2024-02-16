602789c6f5cc7e8a21838f0277718b333f40b6c1
Florian Leimgruber <fleimgruber@google.com>
2024-02-16 17:33:02 +0000

    Add PlusAddressTable
    Adds a new WebDatabaseTable "PlusAddressTable", owned by the
    WebDataServiceWrapper managing the "Web Data" database. This CL just
    adds all the boilerplate, but doesn't define any new SQL tables yet.
    
    PlusAddressTable will be used to persist (facet, email) pairs, arriving
    through Sync. See go/plus-address-storage.
    
    Bug: b/322147254
    Change-Id: I87a8562e5bf73bab6caf7671450cfe04361e1a51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5301163
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1261725}

--- components/webdata_services/web_data_service_wrapper.cc
+++ components/webdata_services/web_data_service_wrapper.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/webdata/payments/payments_autofill_table.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/autofill_payments_features.h"
+#include "components/plus_addresses/webdata/plus_address_table.h"
 #include "components/search_engines/keyword_table.h"
 #include "components/search_engines/keyword_web_data_service.h"
 #include "components/signin/public/webdata/token_service_table.h"
@@ -135,6 +136,8 @@ WebDataServiceWrapper::WebDataServiceWrapper(
   profile_database_->AddTable(
       std::make_unique<payments::WebAppManifestSectionTable>());
 #endif
+  profile_database_->AddTable(
+      std::make_unique<plus_addresses::PlusAddressTable>());
   profile_database_->LoadDatabase();
 
   profile_autofill_web_data_ =
