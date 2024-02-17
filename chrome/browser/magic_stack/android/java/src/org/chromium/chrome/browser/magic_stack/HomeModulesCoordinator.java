95479d6b5c350deb1185e6dd9ef20e2835da5637
Samuel Huang <huangs@chromium.org>
2024-02-17 01:01:19 +0000

    [MagicStack] Have destroy() call hide() in Home Modules.
    Magic Stack modules implementing ModuleProvider performs cleanup in
    hideModule(). Recently it was found that when the NTP is closed,
    HomeModulesCoordinator's destroy() gets called, but not hide() (which
    calls Mediator's hide(), leading to hideModule()). This can cause
    memory and handler leaks, which also affect stability.
    
    This CL fixes the issue by doing the following:
    * Having destroy() call hide() in Coordinator.
    * In Mediator, early-exit hide() on if Magic Stack is not shown.
    
    Fix: 325641882
    Change-Id: I62bf3fad38d12063c180a478fee7710cc67585e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5304099
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Auto-Submit: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261993}

--- chrome/browser/magic_stack/android/java/src/org/chromium/chrome/browser/magic_stack/HomeModulesCoordinator.java
+++ chrome/browser/magic_stack/android/java/src/org/chromium/chrome/browser/magic_stack/HomeModulesCoordinator.java
@@ -345,6 +345,7 @@ public class HomeModulesCoordinator implements ModuleDelegate, OnViewCreatedCall
     }
 
     public void destroy() {
+        hide();
         if (mUiConfig != null) {
             mUiConfig.removeObserver(mDisplayStyleObserver);
             mUiConfig = null;
