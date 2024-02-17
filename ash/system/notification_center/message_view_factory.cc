f8c2d998c59feaff1c2e01641d99d0a736ba00af
Kevin Radtke <kradtke@chromium.org>
2024-02-17 00:40:08 +0000

    OP: Use new PinnedNotificationView when feature is enabled
    Have all notifications that have their `pinned` status set to true use
    `PinnedNotificationView` when the feature is enabled.
    
    screenshot: https://screenshot.googleplex.com/APbNPgVs6NfsyGW
    
    go/ongoing-processes-dd
    go/ongoing-processes-variations
    
    Bug: b:293482928
    Change-Id: Ia629e5998aa4090b32f1b3b8e09d3621162aac26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5288119
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261985}

--- ash/system/notification_center/message_view_factory.cc
+++ ash/system/notification_center/message_view_factory.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_features.h"
 #include "ash/system/notification_center/views/ash_notification_view.h"
 #include "ash/system/notification_center/views/conversation_notification_view.h"
+#include "ash/system/notification_center/views/pinned_notification_view.h"
 #include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "build/chromeos_buildflags.h"
@@ -64,6 +65,11 @@ std::unique_ptr<message_center::MessageView> MessageViewFactory::Create(
                    << ". Falling back to simple notification type.";
       break;
   }
+
+  if (features::AreOngoingProcessesEnabled() && notification.pinned()) {
+    return std::make_unique<PinnedNotificationView>(notification);
+  }
+
   return std::make_unique<AshNotificationView>(notification, shown_in_popup);
 }
 
