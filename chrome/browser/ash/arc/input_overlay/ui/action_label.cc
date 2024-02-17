1fc84f47ca105ba4df744a91bdf123f111493bee
David Jacobo <djacobo@google.com>
2024-02-17 01:39:21 +0000

    gio: string.h removal for input_overlay
    This CL removes potential wrong library, header already includes string.
    Removing duplicated raw_ptr include too.
    
    Bug: none
    Test: build
    Change-Id: Idb36bb28cd2c01ba610c00fb82ea8bb755f37090
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5304325
    Reviewed-by: Cici Ruan <cuicuiruan@google.com>
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Auto-Submit: David Jacobo <djacobo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262007}

--- chrome/browser/ash/arc/input_overlay/ui/action_label.cc
+++ chrome/browser/ash/arc/input_overlay/ui/action_label.cc
@@ -5,11 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/arc/input_overlay/ui/action_label.h"
 
-#include <string.h>
 #include <set>
 
 #include "ash/style/style_util.h"
-#include "base/memory/raw_ptr.h"
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/ash/arc/input_overlay/actions/action.h"
 #include "chrome/browser/ash/arc/input_overlay/constants.h"
