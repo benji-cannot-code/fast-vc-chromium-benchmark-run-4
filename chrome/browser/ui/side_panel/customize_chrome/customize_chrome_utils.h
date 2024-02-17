fd1dced2526e1c19d38f86bdd1ea49fc8c169737
Paul Adedeji <pauladedeji@google.com>
2024-02-17 03:55:25 +0000

    Expand logic for NTP Wallpaper Search button enablement
    - Hides/shows NTP Wallpaper Search button depending on Wallpaper Search flags + settings.
    - Refactors the enablement logic used by all Wallpaper Search features, to avoid inconsistencies.
    
    Change-Id: If7d026857468e866e4c4f61ebc9def9b1e49aa21
    Bug: 324631907,325620228
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5304243
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262048}

--- chrome/browser/ui/side_panel/customize_chrome/customize_chrome_utils.h
+++ chrome/browser/ui/side_panel/customize_chrome/customize_chrome_utils.h
@@ -8,9 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/side_panel/customize_chrome/customize_chrome_tab_helper.h"
 
+class Profile;
+
 namespace customize_chrome {
 
 bool IsSidePanelEnabled();
+bool IsWallpaperSearchEnabledForProfile(Profile* profile);
 
 }  // namespace customize_chrome
 
