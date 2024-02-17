6ab5b4886e2080c0103bdd05e21dffbd290c0d85
Cam Bickel <cambickel@google.com>
2024-02-17 04:18:21 +0000

    Add metric to emit the specific top row keys on custom keyboard layouts
    Test: Manually tested that histogram is emitted on one of my DUTs
    Test: ash_unittests
    Bug: b/325650996
    Change-Id: I1c18580ec43e51fe2dc2caafd4bc3464eff2c06c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5304229
    Commit-Queue: Camden Bickel <cambickel@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262051}

--- ui/events/ash/keyboard_capability.h
+++ ui/events/ash/keyboard_capability.h
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 
 // TODO(dpad): Handle display mirror top row keys.
+// This enum should mirror the enum `KeyboardTopRowLayout` in
+// tools/metrics/histograms/enums.xml and values should not be changed.
 enum class TopRowActionKey {
   kNone = 0,
   kMinValue = kNone,
