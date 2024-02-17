8c4777a5e3802abe86e70974a30f2da668b78513
Andre Le <leandre@chromium.org>
2024-02-17 00:34:54 +0000

    Mahi: Add focus handling for mahi menu view
    Similar to EditorMenuView, when pressing up/down in the context menu,
    the focus will then be taken to the settings button of the Mahi menu.
    
    Change-Id: I0a3fee1fb4d35a7e625309dab6367e3d90082813
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5296603
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261978}

--- chrome/browser/ui/views/mahi/mahi_menu_view.h
+++ chrome/browser/ui/views/mahi/mahi_menu_view.h
@@ -6,11 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_MAHI_MAHI_MENU_VIEW_H_
 #define CHROME_BROWSER_UI_VIEWS_MAHI_MAHI_MENU_VIEW_H_
 
+#include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/views/editor_menu/utils/pre_target_handler_view.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 
 namespace views {
+class ImageButton;
 class UniqueWidgetPtr;
 }  // namespace views
 
@@ -28,6 +30,9 @@ class MahiMenuView : public chromeos::editor_menu::PreTargetHandlerView {
 
   ~MahiMenuView() override;
 
+  // chromeos::editor_menu::PreTargetHandlerView:
+  void RequestFocus() override;
+
   // Creates a menu widget that contains a `MahiMenuView`, configured with the
   // given `anchor_view_bounds`.
   static views::UniqueWidgetPtr CreateWidget(
@@ -39,6 +44,8 @@ class MahiMenuView : public chromeos::editor_menu::PreTargetHandlerView {
  private:
   // Button callback.
   void OnSummaryButtonPressed();
+
+  raw_ptr<views::ImageButton> settings_button_ = nullptr;
 
   base::WeakPtrFactory<MahiMenuView> weak_ptr_factory_{this};
 };
