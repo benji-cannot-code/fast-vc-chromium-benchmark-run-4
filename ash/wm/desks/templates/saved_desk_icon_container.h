71ea4d45ff24e2ac7140fbb083e0b8b4e980af7d
Daniel Andersson <dandersson@chromium.org>
2024-02-18 18:33:02 +0000

    desk_profiles: Fix saved desk favicons for secondary lacros profiles
    This CL completes the ash side of fixing favicons in the saved desk
    library. For saved desks that contain lacros profile IDs, we will now
    pass this ID (via mojom) over to lacros when retrieving the favicon for
    a tab.
    
    The code was previously passing around icon identifiers as a plain
    string. These identifiers can represent either a regular URL, an app ID
    or a special identifier. The big change in this CL is to turn this into
    a small struct where the string is paired with the lacros profile
    ID. The rest is just plumbing.
    
    BUG=b:322847219
    
    Change-Id: Ic8a27142421cebee9f1db946a11a43c1cd5a2028
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5304540
    Reviewed-by: Yongshun Liu <yongshun@chromium.org>
    Commit-Queue: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262185}

--- ash/wm/desks/templates/saved_desk_icon_container.h
+++ ash/wm/desks/templates/saved_desk_icon_container.h
@@ -53,7 +53,8 @@ class SavedDeskIconContainer : public views::BoxLayoutView {
     int count;
   };
 
-  using IconIdentifierAndIconInfo = std::pair<std::string, IconInfo>;
+  using IconIdentifierAndIconInfo =
+      std::pair<SavedDeskIconIdentifier, IconInfo>;
 
   SavedDeskIconContainer();
   SavedDeskIconContainer(const SavedDeskIconContainer&) = delete;
