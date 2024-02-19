c8d9f380870aeafc7a6cb23d148ddc1c6ff8635b
François Degros <fdegros@chromium.org>
2024-02-19 02:27:59 +0000

    drive_internals: Remove "FILE_ERROR_" prefix when displaying FileError strings
    BUG=b:216228528
    
    Change-Id: Ib6a1e98f8d0757a47d756e90f076818e40274b0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5302104
    Commit-Queue: François Degros <fdegros@chromium.org>
    Auto-Submit: François Degros <fdegros@chromium.org>
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262223}

--- chrome/browser/ui/webui/ash/drive_internals_ui.cc
+++ chrome/browser/ui/webui/ash/drive_internals_ui.cc
@@ -103,6 +103,14 @@ std::string ToString(const T x) {
   return (std::ostringstream() << drivefs::pinning::NiceNum << x).str();
 }
 
+std::string ToString(const drive::FileError error) {
+  std::string s = drive::FileErrorToString(error);
+  if (const std::string_view prefix = "FILE_ERROR_"; s.starts_with(prefix)) {
+    s.erase(0, prefix.size());
+  }
+  return s;
+}
+
 template <typename T>
 std::string ToPercent(const T num, const T total) {
   if (num >= 0 && total > 0) {
@@ -543,9 +551,8 @@ class DriveInternalsWebUIHandler : public content::WebUIMessageHandler,
       return;
     }
     for (const FilePath& sync_path : paths) {
-      MaybeCallJavascript(
-          "onAddSyncPath", Value(sync_path.value()),
-          Value(drive::FileErrorToString(drive::FILE_ERROR_OK)));
+      MaybeCallJavascript("onAddSyncPath", Value(sync_path.value()),
+                          Value(ToString(drive::FILE_ERROR_OK)));
     }
   }
 
@@ -573,12 +580,12 @@ class DriveInternalsWebUIHandler : public content::WebUIMessageHandler,
 
   void OnAddSyncPath(const FilePath& sync_path, drive::FileError status) {
     MaybeCallJavascript("onAddSyncPath", Value(sync_path.value()),
-                        Value(drive::FileErrorToString(status)));
+                        Value(ToString(status)));
   }
 
   void OnRemoveSyncPath(const FilePath& sync_path, drive::FileError status) {
     MaybeCallJavascript("onRemoveSyncPath", Value(sync_path.value()),
-                        Value(drive::FileErrorToString(status)));
+                        Value(ToString(status)));
   }
 
   void UpdateBulkPinningDeveloperSection() {
