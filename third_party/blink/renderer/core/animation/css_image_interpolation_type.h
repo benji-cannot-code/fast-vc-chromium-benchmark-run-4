862669330872f854c69aa43e731e7fb4d1ed9345
Daniil Sakhapov <sakhapov@chromium.org>
2024-02-19 11:58:50 +0000

    Update call to new Value() for CSSImageInterpolationType
    Note: no other changes are needed, because the InterpolableNumbers
    used are the same regardless of the input.
    
    Bug: 41494232
    Change-Id: I0396339167478127b612c51e4f4cbff466c25a80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5291094
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262335}

--- third_party/blink/renderer/core/animation/css_image_interpolation_type.h
+++ third_party/blink/renderer/core/animation/css_image_interpolation_type.h
@@ -64,7 +64,8 @@ class CSSImageInterpolationType : public CSSInterpolationType {
   }
 
   static const CSSValue* StaticCreateCSSValue(const InterpolableValue&,
-                                              const NonInterpolableValue*);
+                                              const NonInterpolableValue*,
+                                              const CSSLengthResolver&);
   const CSSValue* CreateCSSValue(const InterpolableValue&,
                                  const NonInterpolableValue*,
                                  const StyleResolverState&) const final;
