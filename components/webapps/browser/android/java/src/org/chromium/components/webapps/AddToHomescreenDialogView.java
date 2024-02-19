a6374da3f723683fe50e9d5ce5f185562aa7b28b
Finnur Thorarinsson <finnur@chromium.org>
2024-02-19 14:51:18 +0000

    [UniversalInstall]: Fix dialog label when adding shortcut.
    This CL ensures that the dialog label and positive button
    text reflect the type set for the dialog.
    
    Bug: 1520511
    Change-Id: Ib956e3d3cf26af3f7dc91b767fe6887aaf8c5c66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5303804
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Auto-Submit: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262384}

--- components/webapps/browser/android/java/src/org/chromium/components/webapps/AddToHomescreenDialogView.java
+++ components/webapps/browser/android/java/src/org/chromium/components/webapps/AddToHomescreenDialogView.java
@@ -66,6 +66,11 @@ public class AddToHomescreenDialogView
     private @AppType int mAppType;
     private boolean mCanSubmit;
 
+    private final String mInstallTitleText;
+    private final String mInstallButtonText;
+    private final String mAddTitleText;
+    private final String mAddButtonText;
+
     @VisibleForTesting
     public AddToHomescreenDialogView(
             Context context,
@@ -138,6 +143,11 @@ public class AddToHomescreenDialogView
                 });
 
         Resources resources = context.getResources();
+        mInstallTitleText = resources.getString(R.string.menu_install_webapp);
+        mInstallButtonText = resources.getString(R.string.app_banner_install);
+        mAddTitleText = resources.getString(R.string.menu_add_to_homescreen);
+        mAddButtonText = resources.getString(R.string.add);
+
         mDialogModel =
                 new PropertyModel.Builder(ModalDialogProperties.ALL_KEYS)
                         .with(ModalDialogProperties.CONTROLLER, this)
@@ -209,6 +219,14 @@ public class AddToHomescreenDialogView
                 mAppLayout.setVisibility(View.VISIBLE);
                 mAppOriginView.setVisibility(View.VISIBLE);
                 break;
+        }
+
+        if (mAppType == AppType.SHORTCUT) {
+            mDialogModel.set(ModalDialogProperties.TITLE, mAddTitleText);
+            mDialogModel.set(ModalDialogProperties.POSITIVE_BUTTON_TEXT, mAddButtonText);
+        } else {
+            mDialogModel.set(ModalDialogProperties.TITLE, mInstallTitleText);
+            mDialogModel.set(ModalDialogProperties.POSITIVE_BUTTON_TEXT, mInstallButtonText);
         }
     }
 
