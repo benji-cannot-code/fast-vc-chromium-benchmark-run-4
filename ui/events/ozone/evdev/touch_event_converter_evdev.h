4987e123a7f2c2b11b2e73a8c4fad014076984bc
Jing Wang <jiwan@chromium.org>
2024-02-19 04:13:11 +0000

    Initialize heatmap palm detector in evdev converter
    Bug: b:289735201
    Change-Id: I0642e63164b567dc04196af77d7eb87bfd72ba48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5273950
    Reviewed-by: Rob Schonberger <robsc@chromium.org>
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262237}

--- ui/events/ozone/evdev/touch_event_converter_evdev.h
+++ ui/events/ozone/evdev/touch_event_converter_evdev.h
@@ -32,6 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "ui/events/ozone/evdev/event_converter_evdev.h"
 #include "ui/events/ozone/evdev/event_device_info.h"
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "ui/events/ozone/evdev/heatmap_palm_detector.h"
+#endif
 #include "ui/events/ozone/evdev/touch_evdev_debug_buffer.h"
 #include "ui/events/ozone/evdev/touch_filter/palm_detection_filter.h"
 #include "ui/events/types/event_type.h"
@@ -316,6 +319,17 @@ class COMPONENT_EXPORT(EVDEV) TouchEventConverterEvdev
 
   // Not owned!
   const raw_ptr<SharedPalmDetectionFilterState> shared_palm_state_;
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  // The corresponding model id if the device supports hidraw spi, otherwise
+  // it will be `kNotSupported`.
+  HeatmapPalmDetector::ModelId hidraw_model_id_ =
+      HeatmapPalmDetector::ModelId::kNotSupported;
+
+  // The hidraw device path if the device supports hidraw spi, otherwise it will
+  // be empty string.
+  std::string hidraw_device_path_;
+#endif
 
   base::WeakPtrFactory<TouchEventConverterEvdev> weak_factory_{this};
 };
