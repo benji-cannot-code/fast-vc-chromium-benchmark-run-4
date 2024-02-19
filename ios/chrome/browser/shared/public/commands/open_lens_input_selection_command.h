8cd35f07a27347f93e3be381185fa23b327d7223
Junji Watanabe <jwata@google.com>
2024-02-19 03:06:12 +0000

    [ios] fix capitalization of UIkit.h in #import to work on case-sensitive filesystem
    Remote execution include preprocessor can't find UIKit.h file with
    "#import <UIKit/UIkit.h>". This ends up with remote compile errors
    due to lack of UIKit.h.
    This CL fixes them to UI'K'it.h so that the file is sent to remote.
    
    Bug: b/325538963
    Change-Id: I540acc7a3f8a3525a0d6d94b7f735fa39e73fa77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5302100
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Junji Watanabe <jwata@google.com>
    Cr-Commit-Position: refs/heads/main@{#1262228}

--- ios/chrome/browser/shared/public/commands/open_lens_input_selection_command.h
+++ ios/chrome/browser/shared/public/commands/open_lens_input_selection_command.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_SHARED_PUBLIC_COMMANDS_OPEN_LENS_INPUT_SELECTION_COMMAND_H_
 #define IOS_CHROME_BROWSER_SHARED_PUBLIC_COMMANDS_OPEN_LENS_INPUT_SELECTION_COMMAND_H_
 
-#import <UIKit/UIkit.h>
+#import <UIKit/UIKit.h>
 #import "base/ios/block_types.h"
 
 enum class LensEntrypoint;
--- ios/chrome/browser/ui/download/download_manager_view_controller_protocol.h
+++ ios/chrome/browser/ui/download/download_manager_view_controller_protocol.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_UI_DOWNLOAD_DOWNLOAD_MANAGER_VIEW_CONTROLLER_PROTOCOL_H_
 #define IOS_CHROME_BROWSER_UI_DOWNLOAD_DOWNLOAD_MANAGER_VIEW_CONTROLLER_PROTOCOL_H_
 
-#import <UIKit/UIkit.h>
+#import <UIKit/UIKit.h>
 
 @protocol DownloadManagerViewControllerDelegate;
 class FullscreenController;
