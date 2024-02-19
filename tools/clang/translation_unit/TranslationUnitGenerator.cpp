a0e0bc318707c69dcb3d54460c193cb4ac0363e1
Arthur Eubanks <aeubanks@google.com>
2024-02-19 15:30:23 +0000

    Roll clang+rust llvmorg-18-init-17730-gf670112a-5 : llvmorg-19-init-2319-g7c4c2746-1 / cd6d8f2a04528f827ad3d399581c0f3502b15a72-1 : 340bb19fea20fd5f9357bbfac542fad84fc7ea2b-3
    https://chromium.googlesource.com/external/github.com/llvm/llvm-project/+log/f670112a..7c4c2746
    
    https://chromium.googlesource.com/external/github.com/rust-lang/rust/+log/cd6d8f2a0452..340bb19fea20
    
    Ran: ./tools/clang/scripts/upload_revision.py 7c4c274643cedcf9671e1db65361231a7d9bee70
    
    $ tools/rust/gnrt_stdlib.py
    to regenerate rust stdlib rules
    
    $ third_party/abseil-cpp/generate_def_files.py
    to regenerate absl def files
    
    Pass -Wl,-no_fixup_chains in various places to work around https://crbug.com/325133695 and https://crbug.com/325410295
    
    Override CARGO_CFG_TARGET_OS to be "android" when targeting android to work around https://crbug.com/325543500 (it was "androideabi" in some cases)
    
    Bug: 324269865, 325543500, 325410295, 325133695, 324440103, 41496712, 41496735
    Change-Id: I2b9789bfd5508c4deea2b5d4ceb33dd0d6dbcbd0
    Disable-Rts: True
    Cq-Include-Trybots: chromium/try:chromeos-amd64-generic-cfi-thin-lto-rel
    Cq-Include-Trybots: chromium/try:dawn-win10-x86-deps-rel
    Cq-Include-Trybots: chromium/try:lacros-arm64-generic-rel
    Cq-Include-Trybots: chromium/try:linux-chromeos-dbg
    Cq-Include-Trybots: chromium/try:linux_chromium_cfi_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_chromeos_msan_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_msan_rel_ng
    Cq-Include-Trybots: chromium/try:mac11-arm64-rel,mac_chromium_asan_rel_ng
    Cq-Include-Trybots: chromium/try:ios-catalyst,win-asan,android-official
    Cq-Include-Trybots: chromium/try:fuchsia-arm64-cast-receiver-rel
    Cq-Include-Trybots: chromium/try:mac-official,linux-official
    Cq-Include-Trybots: chromium/try:win-official,win32-official
    Cq-Include-Trybots: chromium/try:linux-swangle-try-x64,win-swangle-try-x86
    Cq-Include-Trybots: chromium/try:android-cronet-mainline-clang-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-cronet-mainline-clang-arm64-rel
    Cq-Include-Trybots: chromium/try:android-cronet-mainline-clang-x86-dbg
    Cq-Include-Trybots: chromium/try:android-cronet-mainline-clang-x86-rel
    Cq-Include-Trybots: chromium/try:android-cronet-riscv64-dbg
    Cq-Include-Trybots: chromium/try:android-cronet-riscv64-rel
    Cq-Include-Trybots: chrome/try:iphone-device,ipad-device
    Cq-Include-Trybots: chrome/try:linux-chromeos-chrome
    Cq-Include-Trybots: chrome/try:win-chrome,win64-chrome,linux-chrome,mac-chrome
    Cq-Include-Trybots: chrome/try:linux-pgo,mac-pgo,win32-pgo,win64-pgo
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:mac-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5296403
    Owners-Override: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262393}

--- tools/clang/translation_unit/TranslationUnitGenerator.cpp
+++ tools/clang/translation_unit/TranslationUnitGenerator.cpp
@@ -65,12 +65,8 @@ class IncludeFinderPPCallbacks : public clang::PPCallbacks {
                           clang::OptionalFileEntryRef file,
                           llvm::StringRef search_path,
                           llvm::StringRef relative_path,
-#if defined(LLVM_FORCE_HEAD_REVISION)
                           const clang::Module* SuggestedModule,
                           bool ModuleImported,
-#else
-                          const clang::Module* imported,
-#endif
                           clang::SrcMgr::CharacteristicKind /*file_type*/
                           ) override;
   void EndOfMainFile() override;
@@ -168,12 +164,8 @@ void IncludeFinderPPCallbacks::InclusionDirective(
     clang::OptionalFileEntryRef file,
     llvm::StringRef search_path,
     llvm::StringRef relative_path,
-#if defined(LLVM_FORCE_HEAD_REVISION)
     const clang::Module* SuggestedModule,
     bool ModuleImported,
-#else
-    const clang::Module* imported,
-#endif
     clang::SrcMgr::CharacteristicKind /*file_type*/
 ) {
   if (!file)
