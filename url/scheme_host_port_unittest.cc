5bf17dd38d7db006bc94d3f125532cd48ef6d1c4
Hayato Ito <hayato@chromium.org>
2024-02-19 09:01:18 +0000

    Add non-special URL test cases to scheme_host_port_unittest
    Add several test cases to increase test coverage.
    
    Bug: 40063064
    Change-Id: Ifefb968a4fc9c7cfee7808ec90e2e5cfd47c8acf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5302098
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262279}

--- url/scheme_host_port_unittest.cc
+++ url/scheme_host_port_unittest.cc
@@ -64,6 +64,9 @@ TEST_F(SchemeHostPortTest, Invalid) {
       "about:srcdoc#ref", "about:srcdoc?query=123", "data:text/html,Hello!",
       "javascript:alert(1)",
 
+      // Non-special URLs which don't have an opaque path.
+      "git:/", "git://", "git:///", "git://host/", "git://host/path",
+
       // GURLs where GURL::is_valid returns false translate into an invalid
       // SchemeHostPort.
       "file://example.com:443/etc/passwd", "#!^%!$!&*",
@@ -132,6 +135,8 @@ TEST_F(SchemeHostPortTest, InvalidConstruction) {
                {"filesystem", "", 0},
                {"http", "", 80},
                {"data", "example.com", 80},
+               {"git", "", 0},
+               {"git", "example.com", 80},
                {"http", "☃.net", 80},
                {"http\nmore", "example.com", 80},
                {"http\rmore", "example.com", 80},
