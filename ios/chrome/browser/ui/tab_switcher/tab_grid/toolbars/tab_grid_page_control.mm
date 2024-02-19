bccfe56ce6d23d885d35832bdf073652828884fe
Federica Germinario <fedegermi@google.com>
2024-02-19 19:46:44 +0000

    [ios] Fix hover effect of incognito, regular and remote view
    This CL updates the hover effect of:
     - incognitoHoverView
     - regularHoverView
     - remoteHoverView
     - sliderView
    
    All changes are behind a flag, so there is no visible change on iPad.
    To see the new behavior check the bug page.
    
    Bug: 324448366
    Change-Id: Ie656dad33df6f3d6547ca112fb0cc614691b73c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5303656
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1262449}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/toolbars/tab_grid_page_control.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/toolbars/tab_grid_page_control.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_constants.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/grit/ios_strings.h"
+#import "ios/public/provider/chrome/browser/raccoon/raccoon_api.h"
 #import "ui/base/l10n/l10n_util.h"
 
 UIControlEvents TabGridPageChangeByTapEvent = 1 << 24;
@@ -602,7 +603,13 @@ UIImageView* ImageViewForSymbol(NSString* symbolName, bool selected) {
   slider.layer.cornerRadius = kSliderCornerRadius;
   slider.layer.masksToBounds = YES;
   slider.backgroundColor = UIColor.whiteColor;
-
+  if (ios::provider::IsRaccoonEnabled()) {
+    if (@available(iOS 17.0, *)) {
+      slider.hoverStyle = [UIHoverStyle
+          styleWithShape:
+              [UIShape rectShapeWithCornerRadius:kBackgroundCornerRadius]];
+    }
+  }
   [self addSubview:slider];
   self.sliderView = slider;
 
@@ -625,23 +632,10 @@ UIImageView* ImageViewForSymbol(NSString* symbolName, bool selected) {
   [self.selectedImageView addSubview:regularSelectedLabel];
   self.regularSelectedLabel = regularSelectedLabel;
 
-  CGRect segmentRect = CGRectMake(0, 0, kSegmentWidth, kSegmentHeight);
-  UIView* incognitoHoverView = [[UIView alloc] initWithFrame:segmentRect];
-  UIView* regularHoverView = [[UIView alloc] initWithFrame:segmentRect];
-  UIView* remoteHoverView = [[UIView alloc] initWithFrame:segmentRect];
-  [self insertSubview:incognitoHoverView belowSubview:self.sliderView];
-  [self insertSubview:regularHoverView belowSubview:self.sliderView];
-  [self insertSubview:remoteHoverView belowSubview:self.sliderView];
-  self.incognitoHoverView = incognitoHoverView;
-  self.regularHoverView = regularHoverView;
-  self.remoteHoverView = remoteHoverView;
+  self.incognitoHoverView = [self configureHoverView];
+  self.regularHoverView = [self configureHoverView];
+  self.remoteHoverView = [self configureHoverView];
 
-  [self.incognitoHoverView
-      addInteraction:[[UIPointerInteraction alloc] initWithDelegate:self]];
-  [self.regularHoverView
-      addInteraction:[[UIPointerInteraction alloc] initWithDelegate:self]];
-  [self.remoteHoverView
-      addInteraction:[[UIPointerInteraction alloc] initWithDelegate:self]];
   [self.sliderView
       addInteraction:[[UIPointerInteraction alloc] initWithDelegate:self]];
 
@@ -736,6 +730,24 @@ UIImageView* ImageViewForSymbol(NSString* symbolName, bool selected) {
 // Callback for the notification that the user changed the bold status.
 - (void)accessibilityBoldTextStatusDidChange {
   [self updateRegularLabels];
+}
+
+#pragma mark - Private's helpers
+
+- (UIView*)configureHoverView {
+  CGRect segmentRect = CGRectMake(0, 0, kSegmentWidth, kSegmentHeight);
+  UIView* hoverView = [[UIView alloc] initWithFrame:segmentRect];
+  if (ios::provider::IsRaccoonEnabled()) {
+    if (@available(iOS 17.0, *)) {
+      hoverView.hoverStyle = [UIHoverStyle
+          styleWithShape:
+              [UIShape rectShapeWithCornerRadius:kBackgroundCornerRadius]];
+    }
+  }
+  [self insertSubview:hoverView belowSubview:self.sliderView];
+  [hoverView
+      addInteraction:[[UIPointerInteraction alloc] initWithDelegate:self]];
+  return hoverView;
 }
 
 #pragma mark UIPointerInteractionDelegate
