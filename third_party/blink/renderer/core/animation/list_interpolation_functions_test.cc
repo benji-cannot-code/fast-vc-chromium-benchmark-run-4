7a7ab9b7fa03224b8b2b7ea99b7b641c27eef567
Daniil Sakhapov <sakhapov@chromium.org>
2024-02-20 10:02:31 +0000

    Update call to new Value() for interpolable list tests
    Bug: 41494232
    Change-Id: I4598dbdc43eef4c5ece08c8e3ad9b1025a0de0d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5306999
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262606}

--- third_party/blink/renderer/core/animation/list_interpolation_functions_test.cc
+++ third_party/blink/renderer/core/animation/list_interpolation_functions_test.cc
@@ -228,10 +228,11 @@ TEST(ListInterpolationFunctionsTest, EqualCompositeSameLengths) {
 
   const auto& result = To<InterpolableList>(*owner.Value().interpolable_value);
 
+  CSSToLengthConversionData length_resolver;
   ASSERT_EQ(result.length(), 3u);
-  EXPECT_EQ(To<InterpolableNumber>(result.Get(0))->Value(), 2.0);
-  EXPECT_EQ(To<InterpolableNumber>(result.Get(1))->Value(), 4.0);
-  EXPECT_EQ(To<InterpolableNumber>(result.Get(2))->Value(), 6.0);
+  EXPECT_EQ(To<InterpolableNumber>(result.Get(0))->Value(length_resolver), 2.0);
+  EXPECT_EQ(To<InterpolableNumber>(result.Get(1))->Value(length_resolver), 4.0);
+  EXPECT_EQ(To<InterpolableNumber>(result.Get(2))->Value(length_resolver), 6.0);
 }
 
 // Two lists of different lengths are not interpolable, so we expect the
@@ -256,9 +257,10 @@ TEST(ListInterpolationFunctionsTest, EqualCompositeDifferentLengths) {
 
   const auto& result = To<InterpolableList>(*owner.Value().interpolable_value);
 
+  CSSToLengthConversionData length_resolver;
   ASSERT_EQ(result.length(), 2u);
-  EXPECT_EQ(To<InterpolableNumber>(result.Get(0))->Value(), 4.0);
-  EXPECT_EQ(To<InterpolableNumber>(result.Get(1))->Value(), 5.0);
+  EXPECT_EQ(To<InterpolableNumber>(result.Get(0))->Value(length_resolver), 4.0);
+  EXPECT_EQ(To<InterpolableNumber>(result.Get(1))->Value(length_resolver), 5.0);
 }
 
 // If one (or more) of the element pairs are incompatible, the list as a whole
@@ -286,10 +288,11 @@ TEST(ListInterpolationFunctionsTest,
 
   const auto& result = To<InterpolableList>(*owner.Value().interpolable_value);
 
+  CSSToLengthConversionData length_resolver;
   ASSERT_EQ(result.length(), 3u);
-  EXPECT_EQ(To<InterpolableNumber>(result.Get(0))->Value(), 4.0);
-  EXPECT_EQ(To<InterpolableNumber>(result.Get(1))->Value(), 5.0);
-  EXPECT_EQ(To<InterpolableNumber>(result.Get(2))->Value(), 6.0);
+  EXPECT_EQ(To<InterpolableNumber>(result.Get(0))->Value(length_resolver), 4.0);
+  EXPECT_EQ(To<InterpolableNumber>(result.Get(1))->Value(length_resolver), 5.0);
+  EXPECT_EQ(To<InterpolableNumber>(result.Get(2))->Value(length_resolver), 6.0);
 }
 
 // If one (or more) of the element pairs are incompatible, the list as a whole
@@ -314,10 +317,11 @@ TEST(ListInterpolationFunctionsTest,
 
   const auto& result = To<InterpolableList>(*owner.Value().interpolable_value);
 
+  CSSToLengthConversionData length_resolver;
   ASSERT_EQ(result.length(), 3u);
-  EXPECT_EQ(To<InterpolableNumber>(result.Get(0))->Value(), 4.0);
-  EXPECT_EQ(To<InterpolableNumber>(result.Get(1))->Value(), 5.0);
-  EXPECT_EQ(To<InterpolableNumber>(result.Get(2))->Value(), 6.0);
+  EXPECT_EQ(To<InterpolableNumber>(result.Get(0))->Value(length_resolver), 4.0);
+  EXPECT_EQ(To<InterpolableNumber>(result.Get(1))->Value(length_resolver), 5.0);
+  EXPECT_EQ(To<InterpolableNumber>(result.Get(2))->Value(length_resolver), 6.0);
 }
 
 TEST(ListInterpolationFunctionsTest, BuilderNoModify) {
