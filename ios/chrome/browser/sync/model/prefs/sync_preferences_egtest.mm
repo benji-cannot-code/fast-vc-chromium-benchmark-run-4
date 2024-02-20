91133dda4a539ac79035d77642bbb189ca85e925
Arthur Milchior <arthurmilchior@chromium.org>
2024-02-20 01:21:44 +0000

    [iOS] Sign-In without using UI
    This slightly speed-up tests.
    
    Bug: 325235662
    Change-Id: I7b3d8538e15b153e9fd8ef105a966985c17be302
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5296474
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262495}

--- ios/chrome/browser/sync/model/prefs/sync_preferences_egtest.mm
+++ ios/chrome/browser/sync/model/prefs/sync_preferences_egtest.mm
@@ -103,7 +103,7 @@ void WaitForPreferenceValue(int pref_value) {
 
   // Sign in and sign out.
   FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeIdentity1];
-  [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity];
+  [SigninEarlGrey signinWithFakeIdentity:fakeIdentity];
   // No entity is committed to the server.
   WaitForEntitiesOnFakeServer(0);
   [SigninEarlGrey signOut];
@@ -116,7 +116,7 @@ void WaitForPreferenceValue(int pref_value) {
       clearUserPrefWithName:base::SysUTF8ToNSString(kTestSyncablePref)];
 
   // Sign in again to validate the value is not set from the server.
-  [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity];
+  [SigninEarlGrey signinWithFakeIdentity:fakeIdentity];
   GREYAssertNotEqual([ChromeEarlGrey userIntegerPref:kTestSyncablePref],
                      kTestPrefValue1, @"Incorrect account pref value.");
 }
@@ -126,7 +126,7 @@ void WaitForPreferenceValue(int pref_value) {
 - (void)testAccountPrefValueCleanedUpOnSignout {
   // Set a pref value of `kTestPrefValue2` in account.
   FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeIdentity1];
-  [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity];
+  [SigninEarlGrey signinWithFakeIdentity:fakeIdentity];
   [ChromeEarlGrey setIntegerValue:kTestPrefValue2
                       forUserPref:kTestSyncablePref];
   WaitForEntitiesOnFakeServer(1);
@@ -140,7 +140,7 @@ void WaitForPreferenceValue(int pref_value) {
   // `kTestPrefValue2`.
 
   // Sign in and sync.
-  [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity];
+  [SigninEarlGrey signinWithFakeIdentity:fakeIdentity];
   WaitForPreferenceValue(kTestPrefValue2);
 
   // Sign out and validate that the active pref value is the local value.
