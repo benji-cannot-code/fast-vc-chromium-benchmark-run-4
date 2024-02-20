ae9a18de4aeef4d290d3c3c52116ed3e1304e664
Elijah Hewer <hewer@chromium.org>
2024-02-20 20:06:33 +0000

    Overview: Hide feedback behind feature
    Update the post-login feedback button to only show in overview when
    the Forest feature is enabled.
    
    Bug: b/324293780
    Test: manual
    Change-Id: I50c329a483a5cd7e80a25f4c19afbcbcf4550159
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5310043
    Commit-Queue: Elijah Hewer <hewer@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262859}

--- ash/wm/overview/overview_grid.cc
+++ ash/wm/overview/overview_grid.cc
@@ -719,7 +719,9 @@ void OverviewGrid::PositionWindows(
 
   // Create a feedback button that shows even when no items are present (e.g.,
   // for Pine).
-  UpdateFeedbackButton();
+  if (features::IsForestFeatureEnabled()) {
+    UpdateFeedbackButton();
+  }
 
   if (item_list_.empty()) {
     return;
@@ -3025,6 +3027,8 @@ void OverviewGrid::UpdateFasterSplitViewWidget() {
 }
 
 void OverviewGrid::UpdateFeedbackButton() {
+  CHECK(features::IsForestFeatureEnabled());
+
   if (SplitViewController::Get(root_window_)->InSplitViewMode()) {
     feedback_widget_.reset();
     return;
