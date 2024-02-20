57e45c2649b716a180afaf8709a97c9c095b3713
Elie Maamari <emaamari@google.com>
2024-02-20 18:14:55 +0000

    Disallow going back to LocalDataWarningScreen from SignFatalScreen
    Bug: b/320173738
    Change-Id: I57ab3f056201a1403a3a2dad799b3e4c61edf2b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5309921
    Reviewed-by: Leon Masopust <lmasopust@google.com>
    Commit-Queue: Elie Maamari <emaamari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1262784}

--- chrome/browser/ash/login/wizard_controller.cc
+++ chrome/browser/ash/login/wizard_controller.cc
@@ -363,7 +363,8 @@ bool IsGaiaPageDefaultsToSAML() {
 bool IsContextNeededForScreen(OobeScreenId screen_id) {
   return screen_id == SamlConfirmPasswordView::kScreenId ||
          screen_id == CryptohomeRecoveryScreenView::kScreenId ||
-         screen_id == GaiaPasswordChangedView::kScreenId;
+         screen_id == GaiaPasswordChangedView::kScreenId ||
+         screen_id == LocalDataLossWarningScreenView::kScreenId;
 }
 
 }  // namespace
