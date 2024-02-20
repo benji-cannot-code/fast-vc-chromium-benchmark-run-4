40402d011570012b2538535892f941722639ced8
Sophie Wen <sophiewen@chromium.org>
2024-02-20 22:13:42 +0000

    float: Disable tablet multitask menu on non-touch events
    This modifies TabletModeMultitaskMenuController to ignore targets
    with `touch-action:none` and event pointer types that are not touch.
    
    Bug: b/321784724
    Change-Id: I2489a6035485d3382630a3035360c3917021dbd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5301396
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262942}

--- ash/wm/tablet_mode/tablet_mode_multitask_menu_controller.cc
+++ ash/wm/tablet_mode/tablet_mode_multitask_menu_controller.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/accelerators/debug_commands.h"
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
+#include "ash/shell_delegate.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "ash/wm/tablet_mode/tablet_mode_multitask_cue_controller.h"
 #include "ash/wm/tablet_mode/tablet_mode_multitask_menu.h"
@@ -106,7 +107,16 @@ void TabletModeMultitaskMenuController::OnGestureEvent(
     ui::GestureEvent* event) {
   aura::Window* target = static_cast<aura::Window*>(event->target());
   aura::Window* window = GetTargetWindow(target);
-  if (!window) {
+  if (!window ||
+      !Shell::Get()->shell_delegate()->AllowDefaultTouchActions(window)) {
+    return;
+  }
+
+  const ui::GestureEventDetails details = event->details();
+  // Do not handle PEN and ERASER events. PEN events can come from stylus
+  // device.
+  if (details.primary_pointer_type() == ui::EventPointerType::kPen ||
+      details.primary_pointer_type() == ui::EventPointerType::kEraser) {
     return;
   }
 
@@ -116,8 +126,6 @@ void TabletModeMultitaskMenuController::OnGestureEvent(
   // Save the window coordinates to pass to the menu.
   gfx::PointF window_location = event->location_f();
   aura::Window::ConvertPointToTarget(target, window, &window_location);
-
-  const ui::GestureEventDetails details = event->details();
   switch (event->type()) {
     case ui::ET_GESTURE_SCROLL_BEGIN:
       if (std::fabs(details.scroll_y_hint()) <
