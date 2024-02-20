c69b6d0ceb76765fa4268f034666445130474f1a
Hiroshige Hayashizaki <hiroshige@chromium.org>
2024-02-20 22:24:02 +0000

    Remove URLLoaderThrottle::Delegate::PauseReadingBodyFromNet
    This CL removes PauseReadingBodyFromNet and ResumeReadingBodyFromNet
    methods from URLLoaderThrottle::Delegate. They were introduced by
    https://crrev.com/c/679612, and not used anymore after
    https://crrev.com/c/5121947.
    
    Bug: 325314722
    Change-Id: Iba37af9ef59793c4409a63b694afdb67d7a08541
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5299622
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262952}

--- chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_request.cc
+++ chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_request.cc
@@ -76,8 +76,6 @@ class CheckForCancelledOrPausedDelegate
 
   void Resume() override {}
 
-  void PauseReadingBodyFromNet() override { cancelled_or_paused_ = true; }
-
   void RestartWithFlags(int additional_load_flags) override {
     cancelled_or_paused_ = true;
   }
