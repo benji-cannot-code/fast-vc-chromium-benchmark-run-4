f91205d660d4e2a67f85f3b30dfe8ad30fbfd379
Gauthier Ambard <gambard@chromium.org>
2024-02-20 15:33:46 +0000

    [iOS] Set InterfaceStyle to Dark in the TabGrid
    This CL changes the interface style of the whole app when entering
    the TabGrid, forcing it to Dark. This ensures that the elements of
    the app are displayed in dark mode to ensure visual continuity.
    
    The context menu displayed when long pressing on a cell in a
    collection view are not respecting the trait collection of the
    cell nor the collection view. This is the only way I found to
    force it.
    
    This is different from the context menu attached to a button
    that are respecting the button trait collection.
    
    Before:
    https://screenshot.googleplex.com/6DBbb3DAh3jwz5d
    https://screenshot.googleplex.com/AXhNFQ5S6D6Lye2
    https://screenshot.googleplex.com/5XiAmmrj74A9x4T
    https://screenshot.googleplex.com/3XCVVKVssZLd5jK
    https://screenshot.googleplex.com/SHqqosin7qYWZrh
    
    After:
    https://screenshot.googleplex.com/7tDsb2c7548uarJ
    https://screenshot.googleplex.com/9HeVtPR75tuwhf5
    https://screenshot.googleplex.com/9LJ9XtWLD8us5BP
    https://screenshot.googleplex.com/7Tb74coXuoavacp
    https://screenshot.googleplex.com/9J3S8QubY3fj4p2
    
    Bug: 1524313
    Change-Id: Iff22a33e13e6aa265ebe1129f21e96afaf259542
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5307226
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262697}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_coordinator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_coordinator.mm
@@ -421,6 +421,9 @@ bool FindNavigatorShouldBePresentedInBrowser(Browser* browser) {
     if (shouldDisplayBringAndroidTabsPrompt) {
       [strongSelf displayBringAndroidTabsPrompt];
     }
+
+    strongSelf.baseViewController.view.window.overrideUserInterfaceStyle =
+        UIUserInterfaceStyleDark;
   };
 
   ProceduralBlock transitionBlock = ^{
@@ -482,6 +485,9 @@ bool FindNavigatorShouldBePresentedInBrowser(Browser* browser) {
     base::UmaHistogramLongTimes("IOS.TabSwitcher.TimeSpent", duration);
     self.tabGridEnterTime = base::TimeTicks();
   }
+
+  self.baseViewController.view.window.overrideUserInterfaceStyle =
+      UIUserInterfaceStyleUnspecified;
 
   // If another BVC is already being presented, swap this one into the
   // container.
