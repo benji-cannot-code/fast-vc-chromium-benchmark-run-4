28134164aad2ce0eb3c7fc9f3935f58b6b65d846
Aya ElAttar <ayaelattar@chromium.org>
2024-02-20 16:35:40 +0000

    [skyvault] DownloadDirectory policy
    - Added Microsoft OneDrive to possible DownloadPolicy values
    - Added the needed download path sanitisation on Ash. The needed
    changes on Lacros will be in another CL.
    - Extracted getting OneDrive Fusebox path to a helper function.
    
    Bug: b/325896432
    Change-Id: I2641d93ba3f760c23085d9a391ac5517e5b1e577
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5300206
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262726}

--- chrome/browser/download/download_prefs.cc
+++ chrome/browser/download/download_prefs.cc
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/drive/drive_integration_service.h"
 #include "chrome/browser/ash/drive/file_system_util.h"
 #include "chrome/browser/ash/file_manager/path_util.h"
+#include "chrome/browser/ui/webui/ash/cloud_upload/cloud_upload_util.h"
 #include "chromeos/ash/components/dbus/cros_disks/cros_disks_client.h"
 #elif BUILDFLAG(IS_CHROMEOS_LACROS)
 #include "chrome/common/chrome_paths_lacros.h"
@@ -566,6 +567,12 @@ base::FilePath DownloadPrefs::SanitizeDownloadTargetPath(
     return SanitizeDownloadTargetPath(migrated_drive_path);
   }
 
+  base::FilePath onedrive_path;
+  if (download_dir_util::ExpandOneDrivePolicyVariable(profile_, path,
+                                                      &onedrive_path)) {
+    return SanitizeDownloadTargetPath(onedrive_path);
+  }
+
   const base::FilePath default_downloads_path =
       GetDefaultDownloadDirectoryForProfile();
   // Relative paths might be unsafe, so use the default path.
@@ -589,6 +596,9 @@ base::FilePath DownloadPrefs::SanitizeDownloadTargetPath(
   bool drivefs_mounted = chrome::GetDriveFsMountPointPath(&drivefs);
   if (drivefs_mounted && drivefs.IsParent(path))
     return path;
+
+  // TODO(b/325897784): Allow paths under OneDrive mount point if the feature
+  // flag is enabled.
 
   // Allow paths for removable media devices.
   base::FilePath removable_media_path;
@@ -631,6 +641,12 @@ base::FilePath DownloadPrefs::SanitizeDownloadTargetPath(
     return SanitizeDownloadTargetPath(migrated_drive_path);
   }
 
+  base::FilePath onedrive_path;
+  if (download_dir_util::ExpandOneDrivePolicyVariable(profile_, path,
+                                                      &onedrive_path)) {
+    return SanitizeDownloadTargetPath(onedrive_path);
+  }
+
   // If |path| isn't absolute, fall back to the default directory.
   base::FilePath profile_myfiles_path =
       file_manager::util::GetMyFilesFolderForProfile(profile_);
@@ -647,6 +663,14 @@ base::FilePath DownloadPrefs::SanitizeDownloadTargetPath(
       drive::DriveIntegrationServiceFactory::FindForProfile(profile_);
   if (integration_service && integration_service->is_enabled() &&
       integration_service->GetMountPointPath().IsParent(path)) {
+    return path;
+  }
+
+  // Allow paths under one drive mount point if the feature flag is enabled.
+  auto odfs_path = ash::cloud_upload::GetODFSFuseboxMount(profile_);
+  if (base::FeatureList::IsEnabled(features::kSkyVault) &&
+      ash::cloud_upload::IsODFSMounted(profile_) &&
+      ((odfs_path == path) || odfs_path.IsParent(path))) {
     return path;
   }
 
