6cd9e19da4a035eb0fb2b0adabfcbfa66ef258f5
Elias Klim <elklm@google.com>
2024-02-20 20:26:44 +0000

    Fix blocked indicators displayed for an incorrect/new document
    Bug: 325935584
    Change-Id: I067ea02f296a8d98645474f0eccc8a2348354cb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5307803
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Elias Klim <elklm@chromium.org>
    Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262872}

--- components/permissions/permission_request_manager.h
+++ components/permissions/permission_request_manager.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
+#include "components/content_settings/browser/page_specific_content_settings.h"
 #include "components/permissions/permission_prompt.h"
 #include "components/permissions/permission_request_queue.h"
 #include "components/permissions/permission_ui_selector.h"
