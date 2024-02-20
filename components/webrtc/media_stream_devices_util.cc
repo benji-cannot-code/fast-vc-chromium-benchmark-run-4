e11bdd9155f700aa05f0b27c7a2748d6d935e1c4
Bryant Chandler <bryantchandler@chromium.org>
2024-02-20 21:35:08 +0000

    Reland "[gUM] Use preferred device if more than one is eligible"
    This is a reland of commit 803ffdda92d89f00a119454f7aaba402ec6fe17e
    
    The original CL assumed that eligible devices were always passed
    but this isn't true in the case of MEDIA_ACCESS_REQUEST. That
    type of request is only used by speech recognition, and it doesn't
    pass eligible devices. Instead it expects to just use the most
    preferred device. I've added a mechanism for requesting the
    preferred device without filtering.
    
    The original CL also accidentally removed the mechanism from MEDIA_OPEN_DEVICE_PEPPER_ONLY, which allowed it to return a default
    device if no devices were requested.
    
    Original change's description:
    > [gUM] Use preferred device if more than one is eligible
    >
    > This will be a noop for now, because there's never more
    > than one requested id, but it will have an effect once more
    > are passed.
    >
    > Bug: b:310972736
    > Change-Id: Ib94c5406efd833f1426adec49fbda405b84f6637
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5239247
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1259313}
    
    Bug: b:310972736
    Cq-Include-Trybots: luci.chrome.try:chromeos-betty-pi-arc-chrome
    Change-Id: I9e83a50c6cb1bd633d6f953dc890dc49925ae122
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5293937
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262914}

---
+++ components/webrtc/media_stream_devices_util.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/webrtc/media_stream_devices_util.h"
+
+namespace webrtc {
+
+blink::MediaStreamDevices FilterMediaDevices(
+    blink::MediaStreamDevices devices,
+    const std::vector<std::string>& eligible_device_ids) {
+  base::flat_set<std::string> eligible_device_id_set{eligible_device_ids};
+  std::erase_if(devices, [&eligible_device_id_set](
+                             const blink::MediaStreamDevice& device) {
+    return !eligible_device_id_set.contains(device.id);
+  });
+  return devices;
+}
+
+}  // namespace webrtc
