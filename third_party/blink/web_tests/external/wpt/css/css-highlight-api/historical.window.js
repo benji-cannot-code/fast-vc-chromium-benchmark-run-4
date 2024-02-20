1097dcbd5f0180bca6c5f6c83015aa5f3f511eae
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-02-20 03:19:39 +0000

    Import wpt@b31783e518cd5c95f3143027fd31ab5b11f3bb36
    Using wpt-import in Chromium dd0e61c73a51edf999775cdec25acd4c58431704.
    With Chromium commits locally applied on WPT:
    075c2ea3ac "Content-shell support for the SetRPHRegistrationMode webdriver command"
    12977d15ed "Reland "webnn: Remove sync methods""
    6649fe20bc "Add support for math expressions for border-image-slice interpolation"
    eddb8aac0d "Use only sign() function and don't mark tests as tentative"
    ebb27070e3 "webrtc wpt: add test for direction-based filtering of codecs"
    
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jsbell@chromium.org:
      external/wpt/IndexedDB
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I5d94abdf65054ac480c72bf5e3dc69be93700957
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5308435
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1262519}

---
+++ third_party/blink/web_tests/external/wpt/css/css-highlight-api/historical.window.js
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// https://drafts.csswg.org/css-highlight-api-1/
+
+"use strict";
+
+test(() => {
+  assert_equals(self.HighlightRegister, undefined);
+}, "HighlightRegister was renamed");
