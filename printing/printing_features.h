3d88b2cbe1082bbc83b1bdc1780ccc307a6a5094
Paul Moy <pmoy@chromium.org>
2024-02-21 19:02:26 +0000

    printing: switch add printer calls to printscanmgr
    Use printscanmgr instead of debugd for CupsAddAutoConfiguredPrinter and
    CupsAddManuallyConfiguredPrinter D-Bus calls.
    
    Bug: b:257070388
    Change-Id: I4d94bb00dec3c5acf4204258c28f0e69cf63a4c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5141232
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Benjamin Gordon <bmgordon@chromium.org>
    Commit-Queue: Paul Moy <pmoy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263484}

--- printing/printing_features.h
+++ printing/printing_features.h
@@ -19,6 +19,8 @@ namespace features {
 // documented with descriptions of their behaviors in the .cc file.
 
 #if BUILDFLAG(IS_CHROMEOS)
+COMPONENT_EXPORT(PRINTING_BASE)
+BASE_DECLARE_FEATURE(kAddPrinterViaPrintscanmgr);
 COMPONENT_EXPORT(PRINTING_BASE) BASE_DECLARE_FEATURE(kEnableBorderlessPrinting);
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
