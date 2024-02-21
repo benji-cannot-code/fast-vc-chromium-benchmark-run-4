f1e59712a02a5029ce10bff88bb8c30cd60a73ad
Sebastien Lalancette <seblalancette@chromium.org>
2024-02-21 13:47:12 +0000

    Create the CertificateProvisioningServiceFactory
    This factory ties the whole managed identities structure together by
    constructing all of the main components and injecting the right
    dependencies. When created, the CertificateProvisioningService will
    start watching for policy changes (via observing pref values) and only
    take action when certain policies are enabled. That behavior, coupled
    with the overwriting of ServiceIsCreatedWithBrowserContext, will allow
    the provisioning process to bootstrap itself along with the
    instantiation of a Profile. The whole process is not yet supported on
    OTR Profiles.
    
    Fixed: b:319627471
    Change-Id: If69199303f9a603af8354219ccdd7f114ac72d0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5309405
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Hamda Mare <hmare@google.com>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1263302}

---
+++ components/enterprise/client_certificates/core/features.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/enterprise/client_certificates/core/features.h"
+
+namespace client_certificates::features {
+
+BASE_FEATURE(kManagedClientCertificateForUserEnabled,
+             "ManagedClientCertificateForUserEnabled",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+bool IsManagedClientCertificateForUserEnabled() {
+  return base::FeatureList::IsEnabled(kManagedClientCertificateForUserEnabled);
+}
+
+}  // namespace client_certificates::features
---
+++ components/enterprise/client_certificates/core/features.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ENTERPRISE_CLIENT_CERTIFICATES_CORE_FEATURES_H_
+#define COMPONENTS_ENTERPRISE_CLIENT_CERTIFICATES_CORE_FEATURES_H_
+
+#include "base/feature_list.h"
+
+namespace client_certificates::features {
+
+// Controls whether the management of a client certificate for the current user
+// is enabled or not (still requires the policy to be enabled).
+BASE_DECLARE_FEATURE(kManagedClientCertificateForUserEnabled);
+
+// Return true if the user's managed client cert feature is enabled.
+bool IsManagedClientCertificateForUserEnabled();
+
+}  // namespace client_certificates::features
+
+#endif  // COMPONENTS_ENTERPRISE_CLIENT_CERTIFICATES_CORE_FEATURES_H_
