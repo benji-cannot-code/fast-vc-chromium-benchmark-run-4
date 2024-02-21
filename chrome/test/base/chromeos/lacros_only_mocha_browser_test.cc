06b493609a53bab9a082434ce2cc073bc74bc401
Kush Sinha <sinhak@chromium.org>
2024-02-21 08:42:48 +0000

    Extract LacrosOnlyMochaBrowserTest into its own header file
    Extract `LacrosOnlyMochaBrowserTest` class (currently defined in
    inline_login_browsertest.cc) into its own header file.
    
    This utility test base class is needed in Ash OSSettings tests.
    
    Bug: 40273581
    Test: browser_tests --gtest_filter="*InlineLogin*"
    Change-Id: I48df013e51c878ac394dac677de293bc60987755
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5290159
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263208}

---
+++ chrome/test/base/chromeos/lacros_only_mocha_browser_test.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/test/base/chromeos/lacros_only_mocha_browser_test.h"
+#include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/ui/browser_commands.h"
+
+LacrosOnlyMochaBrowserTest::LacrosOnlyMochaBrowserTest() = default;
+
+LacrosOnlyMochaBrowserTest::~LacrosOnlyMochaBrowserTest() = default;
+
+void LacrosOnlyMochaBrowserTest::SetUpInProcessBrowserTestFixture() {
+  if (!ash_starter_.HasLacrosArgument()) {
+    return;
+  }
+
+  ASSERT_TRUE(ash_starter_.PrepareEnvironmentForLacros());
+  WebUIMochaBrowserTest::SetUpInProcessBrowserTestFixture();
+}
+
+void LacrosOnlyMochaBrowserTest::SetUpOnMainThread() {
+  if (!ash_starter_.HasLacrosArgument()) {
+    GTEST_SKIP() << "This test needs to run together with Lacros but the "
+                    "--lacros-chrome-path switch is missing.";
+  }
+
+  ash_starter_.StartLacros(this);
+  WebUIMochaBrowserTest::SetUpOnMainThread();
+}
