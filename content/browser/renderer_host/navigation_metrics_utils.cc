dc919621de991f2ceb7bd1c22ba6f865f0b6fbf4
Yao Xiao <yaoxia@chromium.org>
2024-02-21 23:12:00 +0000

    Record UMA for ad click triggered main frame navigation
    This makes it easier to analyze it in finch (compared to use counters)
    The use counter is still kept, as it's recorded in UKM which could
    still be useful.
    
    
    Bug: 325293264
    Change-Id: If1f3381f1a4da156a2f9955ebbfa38378b17ec2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5296439
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263649}

--- content/browser/renderer_host/navigation_metrics_utils.cc
+++ content/browser/renderer_host/navigation_metrics_utils.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-void MaybeRecordAdClickMainFrameNavigationUseCounter(
+void MaybeRecordAdClickMainFrameNavigationMetrics(
     RenderFrameHostImpl* initiator_frame,
     blink::mojom::NavigationInitiatorActivationAndAdStatus
         initiator_activation_and_ad_status) {
@@ -25,6 +25,8 @@ void MaybeRecordAdClickMainFrameNavigationUseCounter(
           kStartedWithTransientActivationFromAd) {
     GetContentClient()->browser()->LogWebFeatureForCurrentPage(
         initiator_frame, blink::mojom::WebFeature::kAdClickMainFrameNavigation);
+
+    UMA_HISTOGRAM_BOOLEAN("Navigation.MainFrame.FromAdClick", true);
   }
 }
 
--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -8117,7 +8117,7 @@ void RenderFrameHostImpl::OpenURL(blink::mojom::OpenURLParamsPtr params) {
     // TODO(crbug.com/1237552): Resolve the discussion of download policy.
     blink::NavigationDownloadPolicy download_policy;
 
-    MaybeRecordAdClickMainFrameNavigationUseCounter(
+    MaybeRecordAdClickMainFrameNavigationMetrics(
         /*initiator_frame=*/this, params->initiator_activation_and_ad_status);
 
     target_frame->frame_tree_node()->navigator().NavigateFromFrameProxy(
@@ -8149,7 +8149,7 @@ void RenderFrameHostImpl::OpenURL(blink::mojom::OpenURLParamsPtr params) {
     // targeting this page's main frame, or targeting a new tab.
     if (params->disposition != WindowOpenDisposition::CURRENT_TAB ||
         IsOutermostMainFrame()) {
-      MaybeRecordAdClickMainFrameNavigationUseCounter(
+      MaybeRecordAdClickMainFrameNavigationMetrics(
           initiator_frame, params->initiator_activation_and_ad_status);
     }
   }
@@ -8422,7 +8422,7 @@ void RenderFrameHostImpl::CreateNewWindow(
 
   transient_allow_popup_.Deactivate();
 
-  MaybeRecordAdClickMainFrameNavigationUseCounter(
+  MaybeRecordAdClickMainFrameNavigationMetrics(
       /*initiator_frame=*/this, params->initiator_activation_and_ad_status);
 
   if (is_new_browsing_instance || !new_frame_tree) {
@@ -9364,7 +9364,7 @@ void RenderFrameHostImpl::BeginNavigation(
     RenderFrameHostImpl* initiator_frame = RenderFrameHostImpl::FromFrameToken(
         GetProcess()->GetID(), begin_params->initiator_frame_token.value());
     if (IsOutermostMainFrame()) {
-      MaybeRecordAdClickMainFrameNavigationUseCounter(
+      MaybeRecordAdClickMainFrameNavigationMetrics(
           initiator_frame, begin_params->initiator_activation_and_ad_status);
     }
   }
