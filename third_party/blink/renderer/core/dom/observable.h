824c5277c91931a84ad79595df126a2d5ecc49d8
Dominic Farolino <dom@chromium.org>
2024-02-21 22:59:32 +0000

    DOM: Implement the `filter()` Observable operator
    This CL implements the semantics specified in
    https://wicg.github.io/observable/#dom-observable-filter.
    
    See https://github.com/WICG/observable/pull/112.
    
    For WPTs:
    Co-authored-by: ben@benlesh.com
    
    R=masonf@chromium.org
    
    Bug: 40282760
    Change-Id: I507cd7bd4117e81e718bb31e496cc4b763d9570a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5280443
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263643}

--- third_party/blink/renderer/core/dom/observable.h
+++ third_party/blink/renderer/core/dom/observable.h
@@ -19,6 +19,7 @@ class ScriptState;
 class Subscriber;
 class SubscribeOptions;
 class V8Mapper;
+class V8Predicate;
 class V8SubscribeCallback;
 class V8UnionObserverOrObserverCallback;
 class V8Visitor;
@@ -58,6 +59,7 @@ class CORE_EXPORT Observable final : public ScriptWrappable,
   // https://wicg.github.io/observable/#observable-returning-operators.
   Observable* takeUntil(ScriptState*, Observable*);
   Observable* map(ScriptState*, V8Mapper*);
+  Observable* filter(ScriptState*, V8Predicate*);
 
   // Promise-returning operators. See
   // https://wicg.github.io/observable/#promise-returning-operators.
