440ab52a6f7d8da83b6391413e1f7afdc8850d48
Daniel Cheng <dcheng@chromium.org>
2024-02-21 21:14:41 +0000

    Remove unnecessary subclassing of scoped_refptr<PolicyContainerHost>.
    This is not needed.
    
    Change-Id: I490f3db454d38d19ea9b361b86b505526bd806b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5310504
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263571}

--- content/browser/renderer_host/policy_container_host.cc
+++ content/browser/renderer_host/policy_container_host.cc
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/renderer_host/policy_container_host.h"
 
+#include <utility>
+
 #include "base/lazy_instance.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/ranges/algorithm.h"
 #include "content/browser/renderer_host/frame_navigation_entry.h"
 #include "content/browser/renderer_host/frame_tree_node.h"
@@ -22,15 +25,14 @@ namespace {
 // KeepAliveHandle is simply a class referencing a PolicyContainerHost through a
 // scoped_refptr, hence maintaining it alive.
 class KeepAliveHandle
-    : public scoped_refptr<PolicyContainerHost>,
-      public blink::mojom::PolicyContainerHostKeepAliveHandle {
+    : public blink::mojom::PolicyContainerHostKeepAliveHandle {
  public:
-  explicit KeepAliveHandle(PolicyContainerHost* policy_container_host) {
-    wrapped_pointer = policy_container_host;
-  }
+  explicit KeepAliveHandle(
+      scoped_refptr<PolicyContainerHost> policy_container_host)
+      : host_(std::move(policy_container_host)) {}
 
  private:
-  scoped_refptr<PolicyContainerHost> wrapped_pointer;
+  const scoped_refptr<PolicyContainerHost> host_;
 };
 
 using TokenPolicyContainerMap =
@@ -303,8 +305,9 @@ void PolicyContainerHost::Bind(
 void PolicyContainerHost::IssueKeepAliveHandle(
     mojo::PendingReceiver<blink::mojom::PolicyContainerHostKeepAliveHandle>
         receiver) {
-  keep_alive_handles_receiver_set_.Add(std::make_unique<KeepAliveHandle>(this),
-                                       std::move(receiver));
+  keep_alive_handles_receiver_set_.Add(
+      std::make_unique<KeepAliveHandle>(base::WrapRefCounted(this)),
+      std::move(receiver));
 }
 
 }  // namespace content
