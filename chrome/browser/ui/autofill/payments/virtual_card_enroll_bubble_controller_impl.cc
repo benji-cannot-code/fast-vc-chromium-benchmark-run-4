b79ed5774177e4724134e4a7194d8bfbcdb34c39
Darwin Yang <darwinyang@google.com>
2024-02-21 16:44:20 +0000

    [autofill][desktop][metric] Add VCN loading & confirmation metrics
    Add new metrics that records the VCN loading and confirmation bubble
    view results when they are closed. The confirmation view also includes
    whether the VCN enrollment succeeded or failed.
    
    Bug: 1499264,b/325102256
    Change-Id: I074e18a429462099656ef93b18b97f5bc4c160ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5296692
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Darwin Yang <darwinyang@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263390}

--- chrome/browser/ui/autofill/payments/virtual_card_enroll_bubble_controller_impl.cc
+++ chrome/browser/ui/autofill/payments/virtual_card_enroll_bubble_controller_impl.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/autofill/autofill_bubble_base.h"
 #include "chrome/browser/ui/autofill/autofill_bubble_handler.h"
 #include "chrome/browser/ui/page_action/page_action_icon_type.h"
+#include "components/autofill/core/browser/metrics/payments/virtual_card_enrollment_metrics.h"
 #include "components/autofill/core/browser/payments/virtual_card_enroll_metrics_logger.h"
 #include "components/autofill/core/browser/payments/virtual_card_enrollment_flow.h"
 #include "components/autofill/core/common/autofill_payments_features.h"
@@ -137,6 +138,7 @@ void VirtualCardEnrollBubbleControllerImpl::OnAcceptButton(
     // When user clicks "Accept", the bubble closing is delayed since we wait
     // for the enrollment to finish on the server.
     enrollment_status_ = EnrollmentStatus::kPaymentsServerRequestInFlight;
+    LogVirtualCardEnrollmentLoadingViewShown(/*is_shown=*/true);
 
     // Log metrics here instead of when the bubble is closed. When
     // "did_switch_to_loading_state == true" we don't immediately close the
@@ -158,6 +160,7 @@ void VirtualCardEnrollBubbleControllerImpl::OnAcceptButton(
     }
   } else {
     bubble_state_ = BubbleState::kHidden;
+    LogVirtualCardEnrollmentLoadingViewShown(/*is_shown=*/false);
   }
 #endif
 }
@@ -233,10 +236,12 @@ void VirtualCardEnrollBubbleControllerImpl::OnBubbleClosed(
     return false;
 #else
     switch (enrollment_status_) {
-        // TODO(crbug.com/40287758) Record metrics for closing the loading and
-        // confirmation view.
       case EnrollmentStatus::kPaymentsServerRequestInFlight:
+        LogVirtualCardEnrollmentLoadingViewResult(get_metric(closed_reason));
+        return true;
       case EnrollmentStatus::kCompleted:
+        LogVirtualCardEnrollmentConfirmationViewResult(
+            get_metric(closed_reason), confirmation_ui_params_->is_success);
         return true;
       case EnrollmentStatus::kNone:
         return false;
@@ -333,13 +338,19 @@ void VirtualCardEnrollBubbleControllerImpl::DoShowBubble() {
 
   Browser* browser = chrome::FindBrowserWithTab(web_contents());
 
-  if (enrollment_status_ == EnrollmentStatus::kCompleted &&
-      base::FeatureList::IsEnabled(
-          features::kAutofillEnableVcnEnrollLoadingAndConfirmation)) {
-    set_bubble_view(
-        browser->window()
-            ->GetAutofillBubbleHandler()
-            ->ShowVirtualCardEnrollConfirmationBubble(web_contents(), this));
+  if (enrollment_status_ == EnrollmentStatus::kCompleted) {
+    if (base::FeatureList::IsEnabled(
+            features::kAutofillEnableVcnEnrollLoadingAndConfirmation)) {
+      set_bubble_view(
+          browser->window()
+              ->GetAutofillBubbleHandler()
+              ->ShowVirtualCardEnrollConfirmationBubble(web_contents(), this));
+      LogVirtualCardEnrollmentConfirmationViewShown(
+          /*is_shown=*/true, confirmation_ui_params_->is_success);
+    } else {
+      LogVirtualCardEnrollmentConfirmationViewShown(
+          /*is_shown=*/false, confirmation_ui_params_->is_success);
+    }
   } else {
     // For reprompts after link clicks, `is_user_gesture` is set to false.
     bool user_gesture_reprompt = reprompt_required_ ? false : is_user_gesture_;
