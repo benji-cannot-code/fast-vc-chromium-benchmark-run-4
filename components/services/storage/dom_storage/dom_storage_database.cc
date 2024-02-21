6a880f4c5b765546a4b512778f55b0b6d0f0fb4b
Evan Stade <estade@chromium.org>
2024-02-21 19:39:18 +0000

    IDB/LevelDB: add thread safety documentation and delete unused var
    Bug: none
    Change-Id: Ie3b8a400ef60d06cb5d151b2455a2b3b5a5ee730
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5310640
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263504}

--- components/services/storage/dom_storage/dom_storage_database.cc
+++ components/services/storage/dom_storage/dom_storage_database.cc
@@ -31,8 +31,7 @@ const char kInvalidDatabaseMessage[] = "DomStorageDatabase no longer valid.";
 
 class DomStorageDatabaseEnv : public leveldb_env::ChromiumEnv {
  public:
-  DomStorageDatabaseEnv()
-      : ChromiumEnv("ChromiumEnv.StorageService", CreateFilesystemProxy()) {}
+  DomStorageDatabaseEnv() : ChromiumEnv(CreateFilesystemProxy()) {}
 
   DomStorageDatabaseEnv(const DomStorageDatabaseEnv&) = delete;
   DomStorageDatabaseEnv& operator=(const DomStorageDatabaseEnv&) = delete;
