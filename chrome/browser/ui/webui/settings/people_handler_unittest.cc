7ee543de418044a800749696f76d802e8bcb832b
Monica Basta <msalama@chromium.org>
2024-02-21 14:25:43 +0000

    [UNO] Support Sign in paused - PeopleHandler
    With Uno the primary account should be set/cleared with explicit user
    action.
    
    The people handler on signout:
    - Shows Gaia logout tab
    - If a primary account with sync still exists, it invokes sync.
    
    This relies on:
    - In case of signin primary account,
      `DiceResponseHandler::ProcessDiceSignoutHeader` will revoke  all
      tokens on signout.
    - On revoke sync consent, the `SigninManager` will clear all accounts if
      the accounts in the Gaia cookie is empty (/Logout).
    
    These assumptions will be broken on follow-up CLs with:
    - `SigninManager` no longer created in the Full Uno experiment
    - Signout from the web only invalidated the primary account.
    
    This CL ensures on Signout from Chrome Setting, the primary account is
    removed. On next reconciliation cycle, the `AccountReconcilor` will
    match the refresh tokens to the accounts in the Gaia cookie. As the
    \Logout + \ListAccounts are network requests, it is expected the account
    reconcilor will run after the primary account is removed.
    
    Bug: b/319402216
    Change-Id: If8edfdf0d21d6e50ac102d6776144d74bd90a2f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5307136
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263325}

--- chrome/browser/ui/webui/settings/people_handler_unittest.cc
+++ chrome/browser/ui/webui/settings/people_handler_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/mock_callback.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/values.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
@@ -45,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/signin/public/base/account_consistency_method.h"
 #include "components/signin/public/base/consent_level.h"
+#include "components/signin/public/base/signin_switches.h"
 #include "components/signin/public/identity_manager/accounts_mutator.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/identity_test_utils.h"
@@ -1386,6 +1388,12 @@ class PeopleHandlerSignoutTest : public BrowserWithTestWindowTest {
     handler_ = std::make_unique<TestingPeopleHandler>(&web_ui_, profile());
   }
 
+  void SimulateSignout(const base::Value::List& args) {
+    handler()->HandleSignout(args);
+  }
+
+  content::WebUI* web_ui() { return handler()->web_ui(); }
+
   content::WebContents* web_contents() {
     return browser()->tab_strip_model()->GetActiveWebContents();
   }
@@ -1442,7 +1450,7 @@ TEST_F(PeopleHandlerSignoutTest, RevokeSyncNotAllowed) {
   CreatePeopleHandler();
   base::Value::List args;
   args.Append(/*delete_profile=*/false);
-  EXPECT_DEATH(handler()->HandleSignout(args), ".*");
+  EXPECT_DEATH(SimulateSignout(args), ".*");
 }
 
 TEST_F(PeopleHandlerSignoutTest, SignoutNotAllowedSyncOff) {
@@ -1461,7 +1469,7 @@ TEST_F(PeopleHandlerSignoutTest, SignoutNotAllowedSyncOff) {
 
   base::Value::List args;
   args.Append(/*delete_profile=*/false);
-  EXPECT_DEATH(handler()->HandleSignout(args), ".*");
+  EXPECT_DEATH(SimulateSignout(args), ".*");
 }
 #endif  // DCHECK_IS_ON()
 
@@ -1485,7 +1493,7 @@ TEST_F(PeopleHandlerSignoutTest, SignoutNotAllowedSyncOn) {
 
   base::Value::List args;
   args.Append(/*delete_profile=*/false);
-  handler()->HandleSignout(args);
+  SimulateSignout(args);
 
   EXPECT_FALSE(identity_manager()->HasPrimaryAccount(ConsentLevel::kSync));
   EXPECT_TRUE(identity_manager()->HasPrimaryAccount(ConsentLevel::kSignin));
@@ -1513,7 +1521,7 @@ TEST_F(PeopleHandlerSignoutTest, SignoutWithSyncOff) {
 
   base::Value::List args;
   args.Append(/*delete_profile=*/false);
-  handler()->HandleSignout(args);
+  SimulateSignout(args);
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
   EXPECT_EQ(web_contents()->GetVisibleURL(),
             GaiaUrls::GetInstance()->service_logout_url());
@@ -1537,15 +1545,14 @@ TEST_F(PeopleHandlerSignoutTest, SignoutWithSyncOn) {
 
   CreatePeopleHandler();
 
-  EXPECT_NE(handler()->web_ui(), nullptr);
-  EXPECT_NE(nullptr, handler()->web_ui()->GetWebContents());
+  EXPECT_NE(web_ui(), nullptr);
+  EXPECT_NE(nullptr, web_ui()->GetWebContents());
 
-  EXPECT_TRUE(
-      chrome::FindBrowserWithTab(handler()->web_ui()->GetWebContents()));
+  EXPECT_TRUE(chrome::FindBrowserWithTab(web_ui()->GetWebContents()));
 
   base::Value::List args;
   args.Append(/*delete_profile=*/false);
-  handler()->HandleSignout(args);
+  SimulateSignout(args);
 
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
   EXPECT_EQ(web_contents()->GetVisibleURL(),
@@ -1558,4 +1565,29 @@ TEST_F(PeopleHandlerSignoutTest, SignoutWithSyncOn) {
 }
 #endif  // BUILDFLAG(ENABLE_DICE_SUPPORT) || BUILDFLAG(IS_CHROMEOS_LACROS)
 
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
+class ExplicitBrowserSigninPeopleHandlerSignoutTest
+    : public PeopleHandlerSignoutTest {
+ private:
+  base::test::ScopedFeatureList features_{
+      switches::kExplicitBrowserSigninUIOnDesktop};
+};
+
+TEST_F(ExplicitBrowserSigninPeopleHandlerSignoutTest, Signout) {
+  auto account_1 = identity_test_env()->MakePrimaryAccountAvailable(
+      "a@gmail.com", ConsentLevel::kSignin);
+  auto account_2 = identity_test_env()->MakeAccountAvailable("b@gmail.com");
+  EXPECT_TRUE(identity_manager()->HasPrimaryAccount(ConsentLevel::kSignin));
+  EXPECT_EQ(2U, identity_manager()->GetAccountsWithRefreshTokens().size());
+
+  CreatePeopleHandler();
+
+  base::Value::List args;
+  args.Append(/*delete_profile=*/false);
+  SimulateSignout(args);
+  EXPECT_EQ(web_contents()->GetVisibleURL(),
+            GaiaUrls::GetInstance()->LogOutURLWithContinueURL(GURL()));
+  EXPECT_FALSE(identity_manager()->HasPrimaryAccount(ConsentLevel::kSignin));
+}
+#endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
 }  // namespace settings
