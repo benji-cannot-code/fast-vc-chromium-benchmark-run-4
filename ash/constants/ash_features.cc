994fd87a294a034a9f7390f0823b3ec0ad7de5c9
Colin Blundell <blundell@chromium.org>
2024-02-21 07:49:09 +0000

    [//ash] Remove EnableMappableSIForFastInkHost killswitch
    The new codepath was enabled by default in [1], which landed in 121 [2].
    There have been no reports of problems. This CL rips out the old code.
    
    NOTE: This CL just does the minimal ripping out to actually get rid of
    the killswitch. Followup CL(s) will perform further cleanup and
    simplification.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/4971177
    [2] https://chromiumdash.appspot.com/commits?commit=bbc184a&platform=Windows
    
    Change-Id: I3231a69e1f07fd9e30363026f6a945913332fa01
    Bug: 40263449
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5305316
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263192}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -3051,12 +3051,6 @@ BASE_FEATURE(kEnableFastInkForSoftwareCursor,
              "EnableFastInkForSoftwareCursor",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-// NOTE: Do not check the value of this feature directly; instead, use
-// ShouldUseMappableSharedImage() below.
-BASE_FEATURE(kEnableMappableSIForFastInkHost,
-             "EnableMappableSIForFastInkHost",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 ////////////////////////////////////////////////////////////////////////////////
 
 bool AreDesksTemplatesEnabled() {
@@ -4517,10 +4511,6 @@ bool ShouldShowPlayStoreInDemoMode() {
 bool IsFeatureAwareDeviceDemoModeEnabled() {
   return base::FeatureList::IsEnabled(
       kFeatureManagementFeatureAwareDeviceDemoMode);
-}
-
-bool ShouldUseMappableSharedImage() {
-  return base::FeatureList::IsEnabled(kEnableMappableSIForFastInkHost);
 }
 
 bool ShouldUseV1DeviceSync() {
