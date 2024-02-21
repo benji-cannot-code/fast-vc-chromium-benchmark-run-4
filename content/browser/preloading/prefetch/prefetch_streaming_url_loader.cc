8a49b7e06437135eea7bc4c94e62b3177c837208
Hiroshige Hayashizaki <hiroshige@chromium.org>
2024-02-21 18:49:27 +0000

    Use NavigationURLLoader::GetURLLoaderOptions() for prefetch
    To unify the same options used and  clarify the same logic
    `GetURLLoaderOptions()` is used for prefetch and navigation.
    
    Bug: 1403746
    Change-Id: I1ae3dfe06d3136b1edbc791fbe3009781a294f3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5298466
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263477}

--- content/browser/preloading/prefetch/prefetch_streaming_url_loader.cc
+++ content/browser/preloading/prefetch/prefetch_streaming_url_loader.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/single_thread_task_runner.h"
+#include "content/browser/loader/navigation_url_loader.h"
 #include "content/browser/preloading/prefetch/prefetch_response_reader.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/mojom/early_hints.mojom.h"
@@ -38,11 +39,13 @@ void PrefetchStreamingURLLoader::Start(
   //
   // This is a violation of const correctness which lead to a confusing bug
   // here. If that goes away, then this copy might not be necessary.
+  //
+  // `is_outermost_main_frame` is true here because the prefetched result is
+  // served only for outermost main frames.
   url_loader_factory->CreateLoaderAndStart(
       prefetch_url_loader_.BindNewPipeAndPassReceiver(), /*request_id=*/0,
-      network::mojom::kURLLoadOptionSendSSLInfoWithResponse |
-          network::mojom::kURLLoadOptionSniffMimeType |
-          network::mojom::kURLLoadOptionSendSSLInfoForCertificateError,
+      NavigationURLLoader::GetURLLoaderOptions(
+          /*is_outermost_main_frame=*/true),
       network::ResourceRequest(request),
       prefetch_url_loader_client_receiver_.BindNewPipeAndPassRemote(
           base::SingleThreadTaskRunner::GetCurrentDefault()),
