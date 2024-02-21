65537d06a2197cf5b477b7943916e0d8fa967a74
Nathan Hebert <nhebert@chromium.org>
2024-02-21 03:35:52 +0000

    media/gpu/chromeos: Change MailboxVideoConverter interface
    This changes the MailboxVideoConverter interface to accept
    FrameResources. This is done in an effort to split up a large CL that
    changed all usage of VideoFrame to FrameResource across the decoders,
    frame pools, and MailboxVideoConverter.
    
    MailboxVideoFrameConverter is entangled with PlatformVideoFramePool
    through the GetOriginalFrameCB. That callback cannot be changed without
    also changing the frame pool. As a result, GetOriginalFrameCB still
    returns a pointer to a VideoFrame, and much of the
    MailboxVideoFrameConverter is still VideoFrame-based. The intention is
    to change GetOriginalFrameCB to return a pointer to a FrameResource when
    the PlatformVideoFramePool gets migrated to use FrameResource natively.
    
    Bug: b:277581596
    Test: video.Play.*hw* tests pass on Volteer.
    Test: media_unittests pass on Volteer.
    
    Change-Id: Ib00462501991481b8d541565956f2fbea6ab95b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5281153
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Commit-Queue: Nathan Hebert <nhebert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263122}

--- media/gpu/chromeos/video_decoder_pipeline.cc
+++ media/gpu/chromeos/video_decoder_pipeline.cc
@@ -804,7 +804,8 @@ void VideoDecoderPipeline::OnFrameDecoded(scoped_refptr<VideoFrame> frame) {
     return;
   }
   if (frame_converter_) {
-    frame_converter_->ConvertFrame(std::move(frame));
+    frame_converter_->ConvertFrame(
+        VideoFrameResource::Create(std::move(frame)));
   } else {
     OnFrameConverted(VideoFrameResource::Create(std::move(frame)));
   }
@@ -816,7 +817,8 @@ void VideoDecoderPipeline::OnFrameProcessed(scoped_refptr<VideoFrame> frame) {
   TRACE_EVENT1("media,gpu", "VideoDecoderPipeline::OnFrameProcessed",
                "timestamp", (frame ? frame->timestamp().InMicroseconds() : 0));
   if (frame_converter_) {
-    frame_converter_->ConvertFrame(std::move(frame));
+    frame_converter_->ConvertFrame(
+        VideoFrameResource::Create(std::move(frame)));
   } else {
     OnFrameConverted(VideoFrameResource::Create(std::move(frame)));
   }
