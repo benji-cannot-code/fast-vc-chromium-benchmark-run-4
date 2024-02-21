cd4282c39838b3813443dd7b5c33a514acfd0154
Ewann Pelle <ewannpv@chromium.org>
2024-02-21 10:15:06 +0000

    [iOS] Update BrowsingTestCase
    This CL updates BrowsingTestCase in order to add the field trial config
    for the ModernTabStrip feature.
    
    Bug: 325935573
    Change-Id: I64630b211cbbc97d6c1ba93efdf717a2d4b9c2b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5310015
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263231}

--- ios/chrome/browser/web/model/browsing_egtest.mm
+++ ios/chrome/browser/web/model/browsing_egtest.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/strings/stringprintf.h"
 #import "base/strings/sys_string_conversions.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
@@ -74,13 +75,18 @@ class ReloadResponseProvider : public web::DataResponseProvider {
 
 @implementation BrowsingTestCase
 
+- (AppLaunchConfiguration)appConfigurationForTestCase {
+  AppLaunchConfiguration config;
+  config.features_enabled.push_back(kModernTabStrip);
+  return config;
+}
+
 // Matcher for the title of the current tab (on tablet only), which is
 // sufficiently visible.
 id<GREYMatcher> TabWithTitle(const std::string& tab_title) {
-  id<GREYMatcher> notPartOfOmnibox =
-      grey_not(grey_ancestor(chrome_test_util::Omnibox()));
   return grey_allOf(grey_accessibilityLabel(base::SysUTF8ToNSString(tab_title)),
-                    notPartOfOmnibox, grey_sufficientlyVisible(), nil);
+                    grey_kindOfClassName(@"TabStripCell"),
+                    grey_sufficientlyVisible(), nil);
 }
 
 // Tests that page successfully reloads.
