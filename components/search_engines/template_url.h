6e17a627cd1bf792c73beba4c5ea204cb578dcfb
Angela Yoeurng <yoangela@chromium.org>
2024-02-21 13:36:35 +0000

    [omnibox] Add staging url option to starter pack expansion flag.
    Bug: 41494524
    Change-Id: I68394317312b79582404a83f9d79f594b7e32d49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5311159
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263295}

--- components/search_engines/template_url.h
+++ components/search_engines/template_url.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class TemplateURL;
 
-
 // TemplateURLRef -------------------------------------------------------------
 
 // A TemplateURLRef represents a single URL within the larger TemplateURL class
@@ -564,10 +563,9 @@ class TemplateURLRef {
   // Replaces all replacements in |parsed_url_| with their actual values and
   // returns the result.  This is the main functionality of
   // ReplaceSearchTerms().
-  std::string HandleReplacements(
-      const SearchTermsArgs& search_terms_args,
-      const SearchTermsData& search_terms_data,
-      PostContent* post_content) const;
+  std::string HandleReplacements(const SearchTermsArgs& search_terms_args,
+                                 const SearchTermsData& search_terms_data,
+                                 PostContent* post_content) const;
 
   // The TemplateURL that contains us.  This should outlive us.
   raw_ptr<const TemplateURL> owner_;
@@ -616,7 +614,6 @@ class TemplateURLRef {
   // Whether the contained URL is a pre-populated URL.
   bool prepopulated_ = false;
 };
-
 
 // TemplateURL ----------------------------------------------------------------
 
