e68bcc929a211aaa4da4001300c96181fd5c67d0
Yiren Wang <yrw@chromium.org>
2024-02-21 21:41:23 +0000

    Extend MediaProgressView to use straight foreground progress line
    Add a bool in MediaProgressView's init params to use straight foreground
    progress line for non-CrOS media view
    
    Bug: 325377772
    Change-Id: Idf9d6ccedfe85e2c328efaf06c32a6f5acbd3af2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5312079
    Commit-Queue: Yiren Wang <yrw@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263592}

--- components/global_media_controls/public/views/media_progress_view.h
+++ components/global_media_controls/public/views/media_progress_view.h
@@ -27,6 +27,7 @@ class COMPONENT_EXPORT(GLOBAL_MEDIA_CONTROLS) MediaProgressView
 
  public:
   explicit MediaProgressView(
+      bool use_squiggly_line,
       ui::ColorId playing_foreground_color_id,
       ui::ColorId playing_background_color_id,
       ui::ColorId paused_foreground_color_id,
@@ -83,6 +84,7 @@ class COMPONENT_EXPORT(GLOBAL_MEDIA_CONTROLS) MediaProgressView
   bool IsValidSeekPosition(int x, int y);
 
   // Init parameters.
+  bool use_squiggly_line_;
   ui::ColorId playing_foreground_color_id_;
   ui::ColorId playing_background_color_id_;
   ui::ColorId paused_foreground_color_id_;
