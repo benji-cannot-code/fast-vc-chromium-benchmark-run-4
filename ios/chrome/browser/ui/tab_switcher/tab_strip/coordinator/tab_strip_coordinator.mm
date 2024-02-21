7f8ed8944fe5dfbd03010a44ed5128946cc3c1ce
Ewann Pelle <ewannpv@chromium.org>
2024-02-21 10:18:20 +0000

    [iOS][TS] Update new tab button accessibility label
    Bug: 41481434, 325935573
    Change-Id: I0d71ee768b7efd5d1e6aea4f7e16bb64f8faeec4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5307089
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263232}

--- ios/chrome/browser/ui/tab_switcher/tab_strip/coordinator/tab_strip_coordinator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/coordinator/tab_strip_coordinator.mm
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.tabStripViewController.overrideUserInterfaceStyle =
       browserState->IsOffTheRecord() ? UIUserInterfaceStyleDark
                                      : UIUserInterfaceStyleUnspecified;
+  self.tabStripViewController.isIncognito = browserState->IsOffTheRecord();
 
   self.mediator =
       [[TabStripMediator alloc] initWithConsumer:self.tabStripViewController];
--- ios/chrome/browser/ui/tab_switcher/tab_strip/test/tab_strip_egtest.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/test/tab_strip_egtest.mm
@@ -14,17 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/testing/earl_grey/app_launch_manager.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
 
-namespace {
-
-// Matcher for the new tab button.
-id<GREYMatcher> NewTabButton() {
-  return grey_allOf(
-      grey_accessibilityID(@"TabStripNewTabButtonAccessibilityIdentifier"),
-      grey_kindOfClassName(@"UIButton"), grey_sufficientlyVisible(), nil);
-}
-
-}  // namespace
-
 // Tests for the tab strip shown on iPad.
 @interface TabStripTestCase : ChromeTestCase
 @end
@@ -46,12 +35,14 @@ id<GREYMatcher> NewTabButton() {
                  @"Wrong number of opened tabs");
 
   // Open a second tab.
-  [[EarlGrey selectElementWithMatcher:NewTabButton()] performAction:grey_tap()];
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::NewTabButton()]
+      performAction:grey_tap()];
   GREYAssertTrue([ChromeEarlGrey mainTabCount] == 2,
                  @"Wrong number of opened tabs");
 
   // Open a third tab.
-  [[EarlGrey selectElementWithMatcher:NewTabButton()] performAction:grey_tap()];
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::NewTabButton()]
+      performAction:grey_tap()];
   GREYAssertTrue([ChromeEarlGrey mainTabCount] == 3,
                  @"Wrong number of opened tabs");
 }
