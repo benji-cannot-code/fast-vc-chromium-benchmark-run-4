7cda07680deb4dc29418fcff7759782eff6d612b
Elaine Chien <elainechien@chromium.org>
2024-02-21 02:15:02 +0000

    [Layout Metrics] Add new proto for layout trace events
    Collect the class name as a parameter for the trace arguments. The
    class name is set by the view class itself and contains no data that
    can be modified by a user.
    
    Bug: 1519243
    Change-Id: I245c1a05c07afed03dc7cf070c23e52576481338
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5262990
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263096}

--- ui/views/widget/widget.cc
+++ ui/views/widget/widget.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/trace_event/base_tracing.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "ui/base/cursor/cursor.h"
@@ -1987,6 +1988,7 @@ bool Widget::ShouldDescendIntoChildForEventHandling(
 
 void Widget::LayoutRootViewIfNecessary() {
   if (root_view_ && root_view_->needs_layout()) {
+    // Widget name is only collected in local traces.
     TRACE_EVENT1("ui", "Widget::LayoutRootViewIfNecessary", "widget name",
                  GetName());
     root_view_->LayoutImmediately();
