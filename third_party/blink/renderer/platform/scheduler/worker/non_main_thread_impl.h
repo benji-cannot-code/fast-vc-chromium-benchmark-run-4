28d36a2e8c75e89dbe37af59ce55d55dbec16671
Michael Lippautz <mlippautz@chromium.org>
2024-02-21 17:26:11 +0000

    heap: Remove Blink's GCTaskRunner
    Note: This change does not affect production semantics of garbage
    collection.
    
    Safepoint support was necessary in the initial implementation of
    Oilpan (anno 2015) to run stack-less GCs in a timely fashion. The
    safepoint mechanism uses task observers and as such hooks into _every_
    task that was executed on the main thread. This completely bypassed
    the scheduler and any heuristics it may have to meet deadlines.
    
    These days the GCs are all triggered through unified heap in V8.
    Asynchronous GCs (== stack-less GC) are implemented via scheduling
    non-nestable tasks through `PostNonNestableTask()` for GC
    finalizations.
    
    The safepoint hook was still used to perform stack-less GCs after JS
    `gc()` intrinsic calls in testing scenarios. These days these are
    better written using `asyncGC()` (in web tests) and using the advanced
    options for the `gc()` intrinsic that allow for awaitable async GC
    otherwise (using `await gc({type: 'major', execution: 'async'})`).
    
    A small side effect of this is that we also make prod faster by
    removing a task observer that was otherwise always present.
    
    Change-Id: I45bf0e826b07423c301fd242f38ed7a9e400667c
    Bug: b/325123755, b/326091483, b/326192741
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5310195
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263406}

--- third_party/blink/renderer/platform/scheduler/worker/non_main_thread_impl.h
+++ third_party/blink/renderer/platform/scheduler/worker/non_main_thread_impl.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "base/threading/simple_thread.h"
 #include "third_party/blink/public/platform/web_private_ptr.h"
-#include "third_party/blink/renderer/platform/heap/gc_task_runner.h"
 #include "third_party/blink/renderer/platform/scheduler/public/non_main_thread.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
@@ -134,7 +133,6 @@ class PLATFORM_EXPORT NonMainThreadImpl : public NonMainThread {
     ~GCSupport();
 
    private:
-    std::unique_ptr<GCTaskRunner> gc_task_runner_;
     std::unique_ptr<BlinkGCMemoryDumpProvider> blink_gc_memory_dump_provider_;
   };
 
