c68d54f51890fbb48b65b6404d5e69cae1dbea88
Callistus Tan <callistus@google.com>
2024-02-21 02:02:46 +0000

    Reland "Help app: Add Welcome Tour as a launch source for the Help app."
    This is a reland of commit bc82df3f42f9176ed19465e8875893bd7d8bf436
    
    Added import for base::HistogramTester in
    chrome_user_education_delegate_browsertest.cc.
    
    Original change's description:
    > Help app: Add Welcome tour as a launch source for the Help app.
    >
    > The welcome tour currently launches Help app with ChromeInternal as the
    > launch source. However, we want to track launches via the Welcome tour
    > as its own launch source.
    >
    > Bug: b/322267508
    > Change-Id: If5ecfb15c918a6b3acd1d92391b261f57b4de16d
    > Cq-Include-Trybots: luci.chromium.try:linux-chromeos-rel;luci.chrome.try:linux-chromeos-chrome
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5298477
    > Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    > Reviewed-by: David Black <dmblack@google.com>
    > Commit-Queue: Callistus Tan <callistus@google.com>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1262238}
    
    Bug: b/322267508
    Change-Id: Iacf1678cccbc3f292d79bacbe91b84f8b3dc11a1
    Cq-Include-Trybots: luci.chromium.try:linux-chromeos-rel;luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5302109
    Reviewed-by: David Black <dmblack@google.com>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Callistus Tan <callistus@google.com>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263089}

--- ash/user_education/mock_user_education_delegate.h
+++ ash/user_education/mock_user_education_delegate.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/user_education/user_education_delegate.h"
 #include "components/account_id/account_id.h"
+#include "components/services/app_service/public/cpp/app_launch_util.h"
 #include "components/user_education/common/help_bubble.h"
 #include "components/user_education/common/tutorial_description.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -76,6 +77,7 @@ class ASH_EXPORT MockUserEducationDelegate : public UserEducationDelegate {
               LaunchSystemWebAppAsync,
               (const AccountId& account_id,
                SystemWebAppType system_web_app_type,
+               apps::LaunchSource launch_source,
                int64_t display_id),
               (override));
   MOCK_METHOD(bool,
--- ash/user_education/welcome_tour/welcome_tour_controller_unittest.cc
+++ ash/user_education/welcome_tour/welcome_tour_controller_unittest.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
 #include "components/account_id/account_id.h"
+#include "components/services/app_service/public/cpp/app_launch_util.h"
 #include "components/user_education/common/tutorial_description.h"
 #include "components/user_manager/user_type.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -448,6 +449,7 @@ TEST_F(WelcomeTourControllerTest, StartsTourAndPropagatesEvents) {
     EXPECT_CALL(*user_education_delegate,
                 LaunchSystemWebAppAsync(
                     Eq(primary_account_id), Eq(ash::SystemWebAppType::HELP),
+                    Eq(apps::LaunchSource::kFromWelcomeTour),
                     Eq(display::Screen::GetScreen()->GetPrimaryDisplay().id())))
         .Times(display::Screen::GetScreen()->InTabletMode() ? 0u : 1u);
     std::move(ended_callback).Run();
@@ -504,6 +506,7 @@ TEST_F(WelcomeTourControllerTest, AbortsTourAndPropagatesEvents) {
   EXPECT_CALL(*user_education_delegate(),
               LaunchSystemWebAppAsync(
                   Eq(primary_account_id), Eq(ash::SystemWebAppType::HELP),
+                  Eq(apps::LaunchSource::kFromWelcomeTour),
                   Eq(display::Screen::GetScreen()->GetPrimaryDisplay().id())));
 
   // Click the `cancel_button` and verify the Welcome Tour is ended.
@@ -539,6 +542,7 @@ TEST_F(WelcomeTourControllerTest, AbortTourIfChromeVoxEnabledDuringTour) {
   EXPECT_CALL(*user_education_delegate(),
               LaunchSystemWebAppAsync(
                   Eq(primary_account_id), Eq(ash::SystemWebAppType::HELP),
+                  Eq(apps::LaunchSource::kFromWelcomeTour),
                   Eq(display::Screen::GetScreen()->GetPrimaryDisplay().id())));
 
   auto* const a11y_controller = Shell::Get()->accessibility_controller();
@@ -571,6 +575,7 @@ TEST_F(WelcomeTourControllerTest, PreventTourFromStartingIfChromeVoxEnabled) {
   EXPECT_CALL(*user_education_delegate(),
               LaunchSystemWebAppAsync(
                   Eq(primary_account_id), Eq(ash::SystemWebAppType::HELP),
+                  Eq(apps::LaunchSource::kFromWelcomeTour),
                   Eq(display::Screen::GetScreen()->GetPrimaryDisplay().id())));
   session->SetSessionState(SessionState::ACTIVE);
   Mock::VerifyAndClearExpectations(user_education_delegate());
@@ -634,6 +639,7 @@ TEST_P(WelcomeTourControllerCounterfactualTest,
   EXPECT_CALL(*user_education_delegate(),
               LaunchSystemWebAppAsync(
                   Eq(primary_account_id), Eq(ash::SystemWebAppType::HELP),
+                  Eq(apps::LaunchSource::kFromWelcomeTour),
                   Eq(display::Screen::GetScreen()->GetPrimaryDisplay().id())))
       .Times(IsCounterfactual().value_or(false) ? 1u : 0u);
 
@@ -768,6 +774,7 @@ TEST_P(WelcomeTourControllerUserEligibilityTest, EnforcesUserEligibility) {
   EXPECT_CALL(*user_education_delegate(),
               LaunchSystemWebAppAsync(
                   Eq(primary_account_id()), Eq(ash::SystemWebAppType::HELP),
+                  Eq(apps::LaunchSource::kFromWelcomeTour),
                   Eq(display::Screen::GetScreen()->GetPrimaryDisplay().id())))
       .Times(0);
 
@@ -864,6 +871,7 @@ class WelcomeTourControllerRunTest : public WelcomeTourControllerTest {
         *user_education_delegate(),
         LaunchSystemWebAppAsync(
             Eq(primary_account_id), Eq(ash::SystemWebAppType::HELP),
+            Eq(apps::LaunchSource::kFromWelcomeTour),
             Eq(display::Screen::GetScreen()->GetPrimaryDisplay().id())));
 
     // Click `accept_button` to close the Welcome Tour dialog.
@@ -1334,6 +1342,7 @@ TEST_F(WelcomeTourControllerTabletTest, DoesNotStart) {
   EXPECT_CALL(*user_education_delegate(),
               LaunchSystemWebAppAsync(
                   Eq(primary_account_id), Eq(ash::SystemWebAppType::HELP),
+                  Eq(apps::LaunchSource::kFromWelcomeTour),
                   Eq(display::Screen::GetScreen()->GetPrimaryDisplay().id())))
       .Times(0);
   SimulateNewUserFirstLogin(primary_account_id.GetUserEmail());
@@ -1376,6 +1385,7 @@ TEST_F(WelcomeTourControllerTabletTest, TriggersAbort) {
   EXPECT_CALL(*user_education_delegate(),
               LaunchSystemWebAppAsync(
                   Eq(primary_account_id), Eq(ash::SystemWebAppType::HELP),
+                  Eq(apps::LaunchSource::kFromWelcomeTour),
                   Eq(display::Screen::GetScreen()->GetPrimaryDisplay().id())))
       .Times(0);
   EXPECT_CALL(*observer(), OnWelcomeTourEnded);
