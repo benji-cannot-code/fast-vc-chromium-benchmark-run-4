6c22330034276a066f53786ba7c1d5457c13d8e3
Javier Contreras Tenorio <javiercon@microsoft.com>
2024-02-21 18:03:59 +0000

    [views_ax] Migrating to new SetIsEnabled pipeline
    This CL updates existing Views to use the new AXNodeData pipeline
    for the IsEnabled attribute instead of the Override and
    GetAccessibleNodeData pipelines.
    
    This new pipeline updates the querying of accessible node
    data on Views from a "pull" system to a "push" system.
    
    This CL gets rid of OverrideIsEnabled since it will no longer
    be needed.
    
    Bug:325137417
    
    Change-Id: Ie64031a2bb9a6ed0d9f51ca652e39c9f1f346b2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5286855
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Colin Kincaid <ckincaid@chromium.org>
    Commit-Queue: Javier Contreras <javiercon@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1263434}

--- ui/views/controls/button/button.cc
+++ ui/views/controls/button/button.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/animation/throb_animation.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/native_theme/native_theme.h"
+#include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/animation/ink_drop.h"
 #include "ui/views/animation/ink_drop_highlight.h"
 #include "ui/views/animation/ink_drop_impl.h"
@@ -258,6 +259,9 @@ void Button::SetState(ButtonState state) {
 
   ButtonState old_state = state_;
   state_ = state;
+
+  GetViewAccessibility().SetIsEnabled(state_ != STATE_DISABLED);
+
   StateChanged(old_state);
   OnPropertyChanged(&state_, kPropertyEffectsPaint);
 }
@@ -611,8 +615,6 @@ void Button::OnPaint(gfx::Canvas* canvas) {
 
 void Button::GetAccessibleNodeData(ui::AXNodeData* node_data) {
   View::GetAccessibleNodeData(node_data);
-  if (!GetEnabled())
-    node_data->SetRestriction(ax::mojom::Restriction::kDisabled);
 
   switch (state_) {
     case STATE_HOVERED:
@@ -622,15 +624,14 @@ void Button::GetAccessibleNodeData(ui::AXNodeData* node_data) {
       node_data->SetCheckedState(ax::mojom::CheckedState::kTrue);
       break;
     case STATE_DISABLED:
-      node_data->SetRestriction(ax::mojom::Restriction::kDisabled);
-      break;
     case STATE_NORMAL:
     case STATE_COUNT:
       // No additional accessibility node_data set for this button node_data.
       break;
   }
-  if (GetEnabled())
+  if (GetEnabled()) {
     node_data->SetDefaultActionVerb(ax::mojom::DefaultActionVerb::kPress);
+  }
 
   button_controller_->UpdateAccessibleNodeData(node_data);
 }
