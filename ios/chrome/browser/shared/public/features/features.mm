8d183b4557c2454a5cae41ca1e2f0e86ddc6110e
Nicolas MacBeth <nicolasmacbeth@google.com>
2024-02-21 20:53:33 +0000

    [iOS external actions] Enabled by default
    Setting iOS external actions to enabled by default, and removed from
    field testing config.
    
    Bug: b:326242324
    Change-Id: I0e41ae491da91a704f50c96b4ee0192d0d76f13d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5314322
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1263556}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -389,7 +389,7 @@ BASE_FEATURE(kTabGroupsInGrid,
 
 BASE_FEATURE(kIOSExternalActionURLs,
              "IOSExternalActionURLs",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDisableLensCamera,
              "DisableLensCamera",
