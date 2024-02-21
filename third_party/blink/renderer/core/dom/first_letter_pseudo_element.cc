9f3036621659ea1f59da854c2adbb5f41b2c6535
Kent Tamura <tkent@chromium.org>
2024-02-21 05:22:04 +0000

    rt elements should not support :first-letter and :first-line
    Counters for this behavior changes:
    https://chromestatus.com/metrics/feature/timeline/popularity/4748
    https://chromestatus.com/metrics/feature/timeline/popularity/4749
    
    The new behavior matches to Firefox and Safari TP.
    
    Bug: 40942025
    Change-Id: Id0ecea977eb678d44b960027b273c796101d21c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5311677
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263151}

--- third_party/blink/renderer/core/dom/first_letter_pseudo_element.cc
+++ third_party/blink/renderer/core/dom/first_letter_pseudo_element.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/style_request.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/node_computed_style.h"
-#include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/core/layout/generated_children.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/layout/layout_object_inlines.h"
@@ -263,14 +262,6 @@ LayoutText* FirstLetterTextLayoutObjectLegacy(const Element& element) {
       IsInvalidFirstLetterLayoutObject(first_letter_text_layout_object))
     return nullptr;
 
-  // TODO(crbug.com/1501719): See LayoutObject::BehavesLikeBlockContainer().
-  if (parent_layout_object->IsRubyText()) {
-    const Node* parent_node = parent_layout_object->GetNode();
-    if (parent_node && parent_node->HasTagName(html_names::kRtTag)) {
-      UseCounter::Count(element.GetDocument(),
-                        WebFeature::kPseudoFirstLetterOnRt);
-    }
-  }
   return To<LayoutText>(first_letter_text_layout_object);
 }
 
@@ -474,15 +465,6 @@ LayoutText* FirstLetterPseudoElement::FirstLetterTextLayoutObject(
               punctuation_text = layout_text;
             }
           } else {
-            // TODO(crbug.com/1501719): See
-            // LayoutObject::BehavesLikeBlockContainer().
-            if (parent_layout_object->IsRubyText()) {
-              const Node* parent_node = parent_layout_object->GetNode();
-              if (parent_node && parent_node->HasTagName(html_names::kRtTag)) {
-                UseCounter::Count(element.GetDocument(),
-                                  WebFeature::kPseudoFirstLetterOnRt);
-              }
-            }
             // We have found valid ::first-letter text. When the ::first-letter
             // text spans multiple elements, the UA is free to style only one of
             // the elements, all of the elements, or none of the elements. Here
