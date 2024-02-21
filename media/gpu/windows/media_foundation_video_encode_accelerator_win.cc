3b0896877309f2bd8638a497c46446f2de07a7e3
Ilya Nikolaevskiy <ilnik@chromium.org>
2024-02-21 12:07:05 +0000

    Process return value of AcquireSync correctly
    Bug: 325449825
    Change-Id: I8aa8c517cdd5abd5a37fc94e2eea0d96b83927c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5301346
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Ilya Nikolaevskiy <ilnik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263265}

--- media/gpu/windows/media_foundation_video_encode_accelerator_win.cc
+++ media/gpu/windows/media_foundation_video_encode_accelerator_win.cc
@@ -2032,7 +2032,12 @@ HRESULT MediaFoundationVideoEncodeAccelerator::PopulateInputSampleBufferGpu(
     if (SUCCEEDED(hr)) {
       constexpr int kMaxSyncTimeMs = 100;
       hr = keyed_mutex->AcquireSync(0, kMaxSyncTimeMs);
-      RETURN_ON_HR_FAILURE(hr, "Failed to acquire keyed mutex", hr);
+      // Can't check for FAILED(hr) because AcquireSync may return e.g.
+      // WAIT_ABANDONED.
+      if (hr != S_OK && hr != WAIT_TIMEOUT) {
+        LOG(ERROR) << "Failed to acquire mutex: " << PrintHr(hr);
+        return E_FAIL;
+      }
       release_keyed_mutex.emplace(std::move(keyed_mutex), 0);
     }
     sample_texture = input_texture;
@@ -2493,7 +2498,12 @@ HRESULT MediaFoundationVideoEncodeAccelerator::PerformD3DScaling(
       // level then it still may block here temporarily.
       constexpr int kMaxSyncTimeMs = 100;
       hr = keyed_mutex->AcquireSync(0, kMaxSyncTimeMs);
-      RETURN_ON_HR_FAILURE(hr, "Failed to acquire keyed mutex", hr);
+      // Can't check for FAILED(hr) because AcquireSync may return e.g.
+      // WAIT_ABANDONED.
+      if (hr != S_OK && hr != WAIT_TIMEOUT) {
+        LOG(ERROR) << "Failed to acquire mutex: " << PrintHr(hr);
+        return E_FAIL;
+      }
       release_keyed_mutex.emplace(std::move(keyed_mutex), 0);
     }
 
@@ -2599,7 +2609,12 @@ HRESULT MediaFoundationVideoEncodeAccelerator::PerformD3DCopy(
     if (SUCCEEDED(hr)) {
       constexpr int kMaxSyncTimeMs = 100;
       hr = keyed_mutex->AcquireSync(0, kMaxSyncTimeMs);
-      RETURN_ON_HR_FAILURE(hr, "Failed to acquire keyed mutex", hr);
+      // Can't check for FAILED(hr) because AcquireSync may return e.g.
+      // WAIT_ABANDONED.
+      if (hr != S_OK && hr != WAIT_TIMEOUT) {
+        LOG(ERROR) << "Failed to acquire mutex: " << PrintHr(hr);
+        return E_FAIL;
+      }
       release_keyed_mutex.emplace(std::move(keyed_mutex), 0);
     }
 
