b106731db53d9aa75d3f65ad96e8a440906e6d65
Matt Reichhoff <mreichhoff@chromium.org>
2024-02-21 14:27:00 +0000

    [Reland][iOS] Plus addresses settings integration
    This is an initial, rough implementation in advance of the full
    set of requirements. This is intended to support an initial small
    experiment, nothing more. It accordingly omits icons, etc. in favor
    of a minimal implementation.
    
    This is a reland of crrev.com/c/5097387; the test in question has now
    been updated, and a run of the ios-simulator-noncq bot passed.
    
    Bug: 1467623, b/308603630
    Change-Id: I8717a3b008292afc1ef9eeecd9bee56fde35dbbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5302022
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263327}

--- ios/chrome/browser/ui/settings/settings_table_view_controller_constants.mm
+++ ios/chrome/browser/ui/settings/settings_table_view_controller_constants.mm
@@ -74,3 +74,4 @@ NSString* const kSettingsTabPickupCellId = @"kSettingsTabPickupCellId";
 NSString* const kSettingsPrivacyGuideCellId = @"kSettingsPrivacyGuideCellId";
 NSString* const kSettingsDetectUnitsCellId = @"kSettingsDetectUnitsCellId";
 NSString* const kSettingsSafetyCheckCellId = @"SettingsSafetyCheckCellId";
+NSString* const kSettingsPlusAddressesId = @"kSettingsPlusAddressesId";
