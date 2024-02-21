ee5fda86dc364dc15e99ec9ab436b818abb3fd28
Michelle <michellegc@google.com>
2024-02-21 05:47:45 +0000

    Create separate classes for Picker section item containers.
    Move item container layout logic out of picker_section_view.cc into
    separate classes PickerListItemContainerView, PickerSmallItemGridView
    and PickerImageItemGridView.
    
    This is to make it easier to add keyboard navigation logic for each item
    container, which we'll do in a followup.
    
    Bug: b:322900302
    Change-Id: I71138117a6d2fc5a1cc59eee2969672d9ba65040
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5311838
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263163}

---
+++ ash/picker/views/picker_list_item_container_view.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/picker/views/picker_list_item_container_view.h"
+
+#include <memory>
+#include <utility>
+
+#include "ash/picker/views/picker_list_item_view.h"
+#include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/views/layout/flex_layout.h"
+#include "ui/views/layout/layout_types.h"
+
+namespace ash {
+
+PickerListItemContainerView::PickerListItemContainerView() {
+  SetLayoutManager(std::make_unique<views::FlexLayout>())
+      ->SetOrientation(views::LayoutOrientation::kVertical)
+      .SetCrossAxisAlignment(views::LayoutAlignment::kStretch);
+}
+
+PickerListItemContainerView::~PickerListItemContainerView() = default;
+
+PickerListItemView* PickerListItemContainerView::AddListItem(
+    std::unique_ptr<PickerListItemView> list_item) {
+  return AddChildView(std::move(list_item));
+}
+
+BEGIN_METADATA(PickerListItemContainerView)
+END_METADATA
+
+}  // namespace ash
