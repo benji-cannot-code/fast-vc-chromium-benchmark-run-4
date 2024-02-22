16398dfef0f7f7cd708283b1181bd07d99b22eeb
Darwin Yang <darwinyang@google.com>
2024-02-22 19:55:59 +0000

    [autofill][desktop][metric] Add metric for saving cards in settings
    Log a new metric
    "Autofill.PaymentMethods.SettingsPage.StoredCreditCardCountBeforeCardAdded"
    that records the number of existing credit cards a user has when saving
    a new credit card through the settings page.
    
    See https://crrev.com/c/5304154 for the clank implementation.
    
    Bug: 41496219,b/323198432
    Change-Id: Ie27f06820423c6e46609e534c363b194f847e4bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5313952
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Darwin Yang <darwinyang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264142}

--- chrome/browser/extensions/api/autofill_private/autofill_private_api.cc
+++ chrome/browser/extensions/api/autofill_private/autofill_private_api.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/functional/bind.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/user_metrics.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/uuid.h"
@@ -426,8 +427,15 @@ ExtensionFunction::ResponseAction AutofillPrivateSaveCreditCardFunction::Run() {
     personal_data->UpdateCreditCard(credit_card);
     base::RecordAction(base::UserMetricsAction("AutofillCreditCardsEdited"));
   } else {
+    int current_card_count = personal_data->GetCreditCards().size();
     personal_data->AddCreditCard(credit_card);
+
     base::RecordAction(base::UserMetricsAction("AutofillCreditCardsAdded"));
+    base::UmaHistogramCounts100(
+        "Autofill.PaymentMethods.SettingsPage."
+        "StoredCreditCardCountBeforeCardAdded",
+        current_card_count);
+
     if (credit_card.HasNonEmptyValidNickname()) {
       base::RecordAction(
           base::UserMetricsAction("AutofillCreditCardsAddedWithNickname"));
