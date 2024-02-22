da6959a6fe4eb77bb571589ae24b13b6220396f9
Javier Contreras Tenorio (from Dev Box) <javiercon@microsoft.com>
2024-02-22 00:18:09 +0000

    [views_ax] Introducing setters for SetIsIgnored
    This CL introduces the ViewAccessibility setters and getters for
    the IsIgnored state.
    
    For the time being we don't remove the OverrideIsIgnored pipeline,
    that is done in the follow up CLs. Those CLs will also update any
    existing Views to use the new pipeline.
    
    Bug: 325137417
    Change-Id: Id7e4384c871a15bea4280fc90bcc8b217937e8c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5314293
    Commit-Queue: Javier Contreras <javiercon@microsoft.com>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1263684}

--- ui/views/accessibility/view_accessibility.h
+++ ui/views/accessibility/view_accessibility.h
@@ -120,6 +120,10 @@ class VIEWS_EXPORT ViewAccessibility {
 
   void SetIsSelected(bool selected);
 
+  // Hides this view from the accessibility APIs.
+  void SetIsIgnored(bool is_ignored);
+  bool GetIsIgnored() const;
+
   // Note that `pos_in_set` starts from 1 not 0.
   void SetPosInSet(int pos_in_set);
   void SetSetSize(int set_size);
@@ -227,6 +231,8 @@ class VIEWS_EXPORT ViewAccessibility {
 
   // Hides this View from the accessibility tree that is exposed to platform
   // APIs.
+  // Deprecated. Use ViewAccessibility::SetIsIgnored instead.
+  // See https://crbug.com/324485311.
   void OverrideIsIgnored(bool value);
   virtual bool IsIgnored() const;
 
