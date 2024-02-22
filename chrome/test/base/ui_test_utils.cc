4f1ea97fca8f4f46072bf59f53de45c54b0d2f81
Sophie Chang <sophiechang@chromium.org>
2024-02-22 21:22:09 +0000

    Add trace import to ui_test_utils
    Bug: b:40285541
    Change-Id: I8931a548171381903869210a191a86ab2400c186
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5318580
    Auto-Submit: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264205}

--- chrome/test/base/ui_test_utils.cc
+++ chrome/test/base/ui_test_utils.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_timeouts.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/time/time.h"
+#include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/history/history_service_factory.h"
