70c65c038a2f2125659ff768848209b520ee1a11
Emily Shack <emshack@chromium.org>
2024-02-22 19:45:19 +0000

    Tab Organization: Create compact TabSearchItem
    Adds a compact version of the TabSearchItem component for use in multi
    tab organization
    
    Bug: b/324942438
    Change-Id: I38e595cd0d4bbfa45d10462956af18a0ef591f62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5315983
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264134}

--- chrome/browser/resources/tab_search/tab_search_item.ts
+++ chrome/browser/resources/tab_search/tab_search_item.ts
@@ -61,6 +61,12 @@ export class TabSearchItem extends TabSearchItemBase {
         type: Boolean,
         value: false,
       },
+
+      compact: {
+        type: Boolean,
+        value: false,
+        reflectToAttribute: true,
+      },
     };
   }
 
@@ -68,6 +74,7 @@ export class TabSearchItem extends TabSearchItemBase {
   private buttonRipples_: boolean;
   index: number;
   inSuggestedGroup: boolean;
+  compact: boolean;
 
   /**
    * @return Whether a close action can be performed on the item.
