feee246bf7124c77a94273b4450af6c6bfb1408a
Christian Xu <christianxu@google.com>
2024-02-22 14:52:32 +0000

    [iOS][Omnibox] Rich inline piping
    When rich inline autocompletion is enabled, additional text can be
    appended to the textfield.
    
    Add the necessary piping to get the SetAdditionelText call in the text
    field.
    
    Bug: 325035406
    Change-Id: I987ed33d87ce2ca9296110a2dab50cf714809b34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5233145
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263952}

--- ios/chrome/browser/ui/omnibox/omnibox_ui_features.cc
+++ ios/chrome/browser/ui/omnibox/omnibox_ui_features.cc
@@ -5,7 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/omnibox/omnibox_ui_features.h"
 
-#include "base/metrics/field_trial_params.h"
+#import "base/metrics/field_trial_params.h"
+#import "components/omnibox/common/omnibox_features.h"
 #import "ui/base/device_form_factor.h"
 
 BASE_FEATURE(kEnableSuggestionsScrollingOnIPad,
@@ -35,4 +36,8 @@ BASE_FEATURE(kOmniboxPopupRowContentConfiguration,
 bool IsIpadPopoutOmniboxEnabled() {
   return base::FeatureList::IsEnabled(kEnablePopoutOmniboxIpad) &&
          ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_TABLET;
+}
+
+bool IsRichAutocompletionEnabled() {
+  return base::FeatureList::IsEnabled(omnibox::kRichAutocompletion);
 }
--- ios/chrome/browser/ui/omnibox/omnibox_ui_features.h
+++ ios/chrome/browser/ui/omnibox/omnibox_ui_features.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_UI_OMNIBOX_OMNIBOX_UI_FEATURES_H_
 #define IOS_CHROME_BROWSER_UI_OMNIBOX_OMNIBOX_UI_FEATURES_H_
 
-#include "base/feature_list.h"
+#import "base/feature_list.h"
 
 // Feature flag to enable omnibox suggestions scrolling on iPad. This will also
 // disable suggestions hiding on keyboard dismissal.
@@ -31,5 +31,8 @@ BASE_DECLARE_FEATURE(kOmniboxLockIconEnabled);
 
 // Feature flag to enable the use of content configuration for the popup rows.
 BASE_DECLARE_FEATURE(kOmniboxPopupRowContentConfiguration);
+
+// Returns whether kRichAutocompletion feature is enabled.
+bool IsRichAutocompletionEnabled();
 
 #endif  // IOS_CHROME_BROWSER_UI_OMNIBOX_OMNIBOX_UI_FEATURES_H_
