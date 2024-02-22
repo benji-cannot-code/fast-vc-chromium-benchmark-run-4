30f13d7f30e87d40228bad41e6d2afd0d2ed3208
Guillem Perez <guiperez@google.com>
2024-02-22 22:41:26 +0000

    [iOS] New eg test for the content notifications settings menu.
    Added a new eg test for the content notifications settings menu.
    
    Fixed: b/324443743
    Change-Id: Ica55e39b46f3a680478c3bf11efdaa1b84a27568
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5319024
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Guillem Perez <guiperez@google.com>
    Reviewed-by: Marke Hallowell <wry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264282}

--- ios/chrome/test/earl_grey/chrome_matchers.mm
+++ ios/chrome/test/earl_grey/chrome_matchers.mm
@@ -418,6 +418,10 @@ id<GREYMatcher> SettingsTrackingPriceTableView() {
   return [ChromeMatchersAppInterface settingsTrackingPriceTableView];
 }
 
+id<GREYMatcher> SettingsContentNotificationsTableView() {
+  return [ChromeMatchersAppInterface settingsContentNotificationsTableView];
+}
+
 id<GREYMatcher> ContentSettingsButton() {
   return [ChromeMatchersAppInterface contentSettingsButton];
 }
