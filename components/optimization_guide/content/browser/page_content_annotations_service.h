81d90430ce31c67f9bd0cd2f87660402c8edb1c9
Sophie Chang <sophiechang@chromium.org>
2024-02-22 22:37:15 +0000

    Remove entity metadata provider
    Bug: b:312212465
    Change-Id: If9f24900bd77505fa3d1212f416872feaf1049eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5319379
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264279}

--- components/optimization_guide/content/browser/page_content_annotations_service.h
+++ components/optimization_guide/content/browser/page_content_annotations_service.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/zero_suggest_cache_service.h"
 #include "components/optimization_guide/content/browser/in_memory_text_embedding_manager.h"
 #include "components/optimization_guide/content/browser/page_content_annotator.h"
-#include "components/optimization_guide/core/entity_metadata_provider.h"
 #include "components/optimization_guide/core/model_info.h"
 #include "components/optimization_guide/core/optimization_guide_decision.h"
 #include "components/optimization_guide/core/page_content_annotations_common.h"
@@ -114,7 +113,6 @@ enum class PageContentAnnotationsType {
 
 // A KeyedService that annotates page content.
 class PageContentAnnotationsService : public KeyedService,
-                                      public EntityMetadataProvider,
                                       public history::HistoryServiceObserver,
                                       public ZeroSuggestCacheService::Observer {
  public:
@@ -165,11 +163,6 @@ class PageContentAnnotationsService : public KeyedService,
   // Returns the model info for the given annotation type, if the model file is
   // available.
   std::optional<ModelInfo> GetModelInfoForType(AnnotationType type) const;
-
-  // EntityMetadataProvider:
-  void GetMetadataForEntityId(
-      const std::string& entity_id,
-      EntityMetadataRetrievedCallback callback) override;
 
   // history::HistoryServiceObserver:
   void OnURLsModified(history::HistoryService* history_service,
@@ -314,14 +307,6 @@ class PageContentAnnotationsService : public KeyedService,
   virtual void PersistSalientImageMetadata(
       const HistoryVisit& visit,
       const proto::SalientImageMetadata& salient_image_metadata);
-
-  // Called when entity metadata for |entity_id| that had weight |weight| on
-  // page with |url| has been retrieved.
-  void OnEntityMetadataRetrieved(
-      const GURL& url,
-      const std::string& entity_id,
-      int weight,
-      const std::optional<EntityMetadata>& entity_metadata);
 
   using PersistAnnotationsCallback = base::OnceCallback<void(history::VisitID)>;
   // Queries |history_service| for all the visits to the visited URL of |visit|.
