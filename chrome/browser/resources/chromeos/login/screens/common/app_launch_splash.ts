b9dd870c65e4c80669362d43db56486cc1555c81
Danila Kuzmin <dkuzmin@google.com>
2024-02-22 15:21:31 +0000

    [oobe] Address undefined data in AppLaunchSplash
    AppLaunchSplash screen might be reshown without data after the
    ErrorScreen was shown.
    
    Bug: b:278614209
    Change-Id: Iec720bc4c916fcd2aecdafb7be09ef6b99ecb4b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5311118
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1263965}

--- chrome/browser/resources/chromeos/login/screens/common/app_launch_splash.ts
+++ chrome/browser/resources/chromeos/login/screens/common/app_launch_splash.ts
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import '//resources/js/action_link.js';
 import '../../components/throbber_notice.js';
 
+import {assert} from '//resources/js/assert.js';
 import {ensureTransitionEndEvent} from '//resources/js/util.js';
 import {PolymerElementProperties} from '//resources/polymer/v3_0/polymer/interfaces.js';
 import {mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
@@ -109,13 +110,18 @@ class AppLaunchSplash extends AppLaunchSplashBase {
    * Event handler that is invoked just before the frame is shown.
    * @param data Screen init payload.
    */
-  onBeforeShow(data: AppLaunchSplashScreenData): void {
-    this.shadowRoot!.getElementById('configNetwork')!.hidden = true;
+  onBeforeShow(data?: AppLaunchSplashScreenData): void {
+    assert(this.shadowRoot);
+    this.shadowRoot.getElementById('configNetwork')!.hidden = true;
     this.toggleNetworkConfig(false);
-    this.updateApp(data['appInfo']);
-
-    this.shadowRoot!.getElementById('shortcutInfo')!.hidden =
-        !data['shortcutEnabled'];
+    // If the screen is reshown from the ErrorScreen using the default callback
+    // data might be undefined.
+    if (data) {
+      this.updateApp(data['appInfo']);
+      const shortcutInfo = this.shadowRoot.getElementById('shortcutInfo');
+      assert(shortcutInfo instanceof HTMLElement);
+      shortcutInfo.hidden = !data['shortcutEnabled'];
+    }
   }
 
   /**
