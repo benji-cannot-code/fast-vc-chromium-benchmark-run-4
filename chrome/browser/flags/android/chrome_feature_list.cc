03ea1cb2b622ddb37087474a433aefca253e1b7f
Neil Coronado <nemco@google.com>
2024-02-22 21:58:29 +0000

    [TabStripPerformance] Disable temporary tab strip removal delay
    Disable temporary tab strip removal delay.
    -The placeholder tab strip on startup is now enabled on ToT. Since this
     placeholder strip removes the scroll/flicker jank, this temporary tab
     strip removal is now obsolete.
    
    Bug: 326462263
    Change-Id: I3d6628c11f8ccdc9886a89dc9231e951b436d401
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5319335
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Neil Coronado <nemco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1264247}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -596,7 +596,7 @@ BASE_FEATURE(kDeferTabSwitcherLayoutCreation,
 
 BASE_FEATURE(kDelayTempStripRemoval,
              "DelayTempStripRemoval",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDownloadAutoResumptionThrottling,
              "DownloadAutoResumptionThrottling",
