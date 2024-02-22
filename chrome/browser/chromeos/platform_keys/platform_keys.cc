6131cc1530d5f28de528d41fa99c3d4ff62cbc72
Felipe Andrade <fsandrade@chromium.org>
2024-02-22 14:18:30 +0000

    Clean up and fix typos in platform_keys extension files
    Clean up unused function from extension_platform_keys_service.h
    (generatedKey). Also rename function from platform_keys.h to fix a typo.
    
    Bug: b:288880151
    Change-Id: I8fc0ce7ff0b8313a8059967b03e0de41e5db5c91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5315256
    Reviewed-by: Michael Ershov <miersh@google.com>
    Reviewed-by: Michael Slutskii <slutskii@google.com>
    Commit-Queue: Felipe Andrade <fsandrade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263928}

--- chrome/browser/chromeos/platform_keys/platform_keys.cc
+++ chrome/browser/chromeos/platform_keys/platform_keys.cc
@@ -432,7 +432,7 @@ GetPublicKeyAndAlgorithmOutput GetPublicKeyAndAlgorithm(
   }
 
   std::optional<base::Value::Dict> algorithm =
-      BuildWebCrypAlgorithmDictionary(key_info);
+      BuildWebCryptoAlgorithmDictionary(key_info);
   DCHECK(algorithm.has_value());
   output.algorithm = std::move(algorithm.value());
 
@@ -479,7 +479,7 @@ net::X509Certificate::PublicKeyType GetKeyTypeForAlgorithm(
   return net::X509Certificate::kPublicKeyTypeUnknown;
 }
 
-std::optional<base::Value::Dict> BuildWebCrypAlgorithmDictionary(
+std::optional<base::Value::Dict> BuildWebCryptoAlgorithmDictionary(
     const PublicKeyInfo& key_info) {
   switch (key_info.key_type) {
     case net::X509Certificate::kPublicKeyTypeRSA: {
