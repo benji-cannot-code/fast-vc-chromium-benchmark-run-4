ec6aae64041e3fc7e7babbe4539d9ea735e8b2d5
Hiroshige Hayashizaki <hiroshige@chromium.org>
2024-02-22 01:38:02 +0000

    Always use GetURLLoaderOptions() in NavigationURLLoaderImpl
    To unify the logic, and particularly to use the same options
    for initial SW-intercepted requests and its fallback requests.
    
    This is preparation for
    https://chromium-review.googlesource.com/c/chromium/src/+/5274112
    that will plumb the opitions from the initial SW-intercepted requests
    and fallback requests, instead of asking or duplicating the
    `NavigationURLLoaderImpl` logic.
    
    This CL shouldn't change the behavior, because this CL uses
    `GetURLLoaderOptions()` for WebUI, blob and intercepted requests,
    but the option flags set in `GetURLLoaderOptions()` are not checked
    in the URLLoaderFactories etc. for these requests, according to
    code search.
    
    Bug: 1403746
    Change-Id: Ida0ad7760a6626c31cfa0fc7250c380df2c5ea6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5297141
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263728}

--- content/browser/loader/navigation_url_loader_impl.h
+++ content/browser/loader/navigation_url_loader_impl.h
@@ -180,8 +180,7 @@ class CONTENT_EXPORT NavigationURLLoaderImpl
   void CreateThrottlingLoaderAndStart(
       scoped_refptr<network::SharedURLLoaderFactory> factory,
       std::vector<std::unique_ptr<blink::URLLoaderThrottle>>
-          additional_throttles,
-      uint32_t options = network::mojom::kURLLoadOptionNone);
+          additional_throttles);
 
 #if BUILDFLAG(ENABLE_PLUGINS)
   void CheckPluginAndContinueOnReceiveResponse(
