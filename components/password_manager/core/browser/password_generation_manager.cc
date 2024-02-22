7950db0555c54920010417dcf54c9fd9f2b6cb56
Jood Hajeer <jood@google.com>
2024-02-22 14:32:43 +0000

    [iOS] Update local password infobar
    For local passwords, the update infobar subtitle should indicate the
    update to the local store, where the password is saved even if there is
    a signed in account that is using the account store.
    This is not the case before this CL, where the infobar subtitle depends
    on finding a signed-in account.
    
    This CL introduces IsUpdateAffectingPasswordsStoredInTheGoogleAccount
    which checks if the updated password is in the account store, to decide
    the correct subtitle of the infobar.
    
    Note: the issue and the fix are related to choosing the string only, and
    the update action works as expected.
    
    Fixed: 41496171
    Change-Id: I999932a5ac1078fd6cac509b46d5ba98483b9adc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5300265
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1263935}

--- components/password_manager/core/browser/password_generation_manager.cc
+++ components/password_manager/core/browser/password_generation_manager.cc
@@ -63,6 +63,7 @@ class PasswordDataForUI : public PasswordFormManagerForUI {
   bool IsMovableToAccountStore() const override;
   void Save() override;
   void Update(const PasswordForm& credentials_to_update) override;
+  bool IsUpdateAffectingPasswordsStoredInTheGoogleAccount() const override;
   void OnUpdateUsernameFromPrompt(const std::u16string& new_username) override;
   void OnUpdatePasswordFromPrompt(const std::u16string& new_password) override;
   void OnNopeUpdateClicked() override;
@@ -158,6 +159,12 @@ void PasswordDataForUI::Save() {
 void PasswordDataForUI::Update(const PasswordForm&) {
   // The method is obsolete.
   NOTREACHED();
+}
+
+bool PasswordDataForUI::IsUpdateAffectingPasswordsStoredInTheGoogleAccount()
+    const {
+  // Generated passwords are always in the Google Account.
+  return true;
 }
 
 void PasswordDataForUI::OnUpdateUsernameFromPrompt(
