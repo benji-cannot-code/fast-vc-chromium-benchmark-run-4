f85ba543b33d6644204292e19addcc9734a3efd5
Guillem Perez <guiperez@google.com>
2024-02-22 17:32:39 +0000

    [iOS] Creates the Content subfolder for notification settings.
    This CL creates the content subfolder and adds boilerplate code to add a
    new view controller accessible from the Notifications Content settings
    TableViewCell.
    
    Bug: b/324443743
    Change-Id: Ida282bd6ff215aceee50c4e4eeeb56e6495bd123
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5309586
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Marke Hallowell <wry@chromium.org>
    Commit-Queue: Guillem Perez <guiperez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1264037}

---
+++ ios/chrome/browser/ui/settings/notifications/content_notifications/content_notifications_constants.mm
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/settings/notifications/content_notifications/content_notifications_constants.h"
+
+NSString* const kContentNotificationsTableViewId =
+    @"ContentNotificationsTableViewId";
+NSString* const kContentNotificationsCellId = @"ContentNotificationsCellId";
