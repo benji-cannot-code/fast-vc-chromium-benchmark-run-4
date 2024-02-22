cee64681f125d0ce44b7e1c13056c3bbd3f52234
Rainhard Findling <rainhard@chromium.org>
2024-02-22 14:52:57 +0000

    Revert "[Nearby Share] Enable Quick Share branding on ToT"
    This reverts commit 7f727f6e2ce58208cc3fdf74eaf6d4cda752a328.
    
    Reason for revert: crbug.com/326314488
    
    Original change's description:
    > [Nearby Share] Enable Quick Share branding on ToT
    >
    > Enables Quick Share branding on ToT.
    >
    > Bug: b/311731010
    > Change-Id: I4ded015e3d352ca37fd1fdb4c4577b5187cc1169
    > Tested: Unit tests.
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5314884
    > Reviewed-by: Jack Shira <jackshira@google.com>
    > Commit-Queue: Daniel Classon <dclasson@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1263486}
    
    Bug: 311731010,326314488
    Change-Id: Id5623106c8d8d4195a0a340d41514c3de636b391
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5317671
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Owners-Override: Rainhard Findling <rainhard@chromium.org>
    Owners-Override: David Baron <dbaron@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263953}

--- chrome/browser/nearby_sharing/common/nearby_share_features.cc
+++ chrome/browser/nearby_sharing/common/nearby_share_features.cc
@@ -8,8 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace features {
 
-// Enables Quick Share branding.
-BASE_FEATURE(kIsNameEnabled, "IsNameEnabled", base::FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(kIsNameEnabled,
+             "IsNameEnabled",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Enables Nearby Sharing functionality.
 BASE_FEATURE(kNearbySharing, "NearbySharing", base::FEATURE_ENABLED_BY_DEFAULT);
