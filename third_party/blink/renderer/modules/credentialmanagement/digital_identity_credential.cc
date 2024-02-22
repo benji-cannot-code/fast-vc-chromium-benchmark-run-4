e1acb789e41fb78d396c408aa856a4e33e70f57e
Peter Kotwicz <pkotwicz@chromium.org>
2024-02-22 21:22:33 +0000

    [RWI] Add CredentialRequestOptions::digital
    This CL adds CredentialRequestOptions::digital and modifies
    DigitalCredentialRequestOptions to process the new member.
    
    The member is defined here https://wicg.github.io/digital-identities/#extensions-to-credentialrequestoptions-dictionary
    
    BUG=326289862
    
    Change-Id: Ic1335269f68906046ba2baacc023920923f51050
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5314684
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264206}

--- third_party/blink/renderer/modules/credentialmanagement/digital_identity_credential.cc
+++ third_party/blink/renderer/modules/credentialmanagement/digital_identity_credential.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_credential_request_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_digital_credential_provider.h"
-#include "third_party/blink/renderer/bindings/modules/v8/v8_federated_credential_request_options.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_digital_credential_request_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_identity_credential_request_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_identity_provider_request_options.h"
 #include "third_party/blink/renderer/core/dom/abort_signal.h"
@@ -93,9 +93,12 @@ void OnCompleteRequest(ScriptPromiseResolver* resolver,
 }  // anonymous namespace
 
 bool IsDigitalIdentityCredentialType(const CredentialRequestOptions& options) {
-  return options.hasIdentity() && options.identity()->hasProviders() &&
-         base::ranges::any_of(options.identity()->providers(),
-                              &IdentityProviderConfig::hasHolder);
+  if (options.hasIdentity()) {
+    return options.identity()->hasProviders() &&
+           base::ranges::any_of(options.identity()->providers(),
+                                &IdentityProviderConfig::hasHolder);
+  }
+  return options.hasDigital() && options.digital()->hasProviders();
 }
 
 ScriptPromise DiscoverDigitalIdentityCredentialFromExternalSource(
@@ -112,9 +115,13 @@ ScriptPromise DiscoverDigitalIdentityCredentialFromExternalSource(
     return resolver->Promise();
   }
 
+  size_t num_providers = options.hasIdentity()
+                             ? options.identity()->providers().size()
+                             : options.digital()->providers().size();
+
   // TODO(https://crbug.com/1416939): make sure the Digital Credentials
   // API works well with the Multiple IdP API.
-  if (options.identity()->providers().size() > 1u) {
+  if (num_providers > 1u) {
     exception_state.ThrowTypeError(
         "Digital identity API currently does not support multiple "
         "providers.");
@@ -151,9 +158,11 @@ ScriptPromise DiscoverDigitalIdentityCredentialFromExternalSource(
     scoped_abort_state = std::make_unique<ScopedAbortState>(signal, handle);
   }
 
+  const DigitalCredentialProvider& digital_provider =
+      options.hasIdentity() ? *options.identity()->providers()[0]->holder()
+                            : *options.digital()->providers()[0];
   auto digital_credential_provider =
-      blink::mojom::blink::DigitalCredentialProvider::From(
-          *options.identity()->providers()[0]->holder());
+      blink::mojom::blink::DigitalCredentialProvider::From(digital_provider);
 
   auto* request =
       CredentialManagerProxy::From(script_state)->DigitalIdentityRequest();
