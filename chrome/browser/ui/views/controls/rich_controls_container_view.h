eba7cd320f558acc75a5fde1c1f89e67a0bed2b7
Marijn Kruisselbrink <mek@chromium.org>
2024-02-22 21:26:45 +0000

    Add link to system notification settings to page info for PWAs on Mac.
    When notification attribution is enabled, and the status of the
    system level notification permission for a PWA is such that
    notifications are likely not working, include a link to the
    notifications page in System Settings for the specific app in the
    page info bubble.
    
    To implement this, this CL adds support for StyledLabel secondary
    labels to RichControlsContainerView.
    
    Note that because of crbug.com/40241406 this link is currently
    unclickable.
    
    Additionally, because of crbug.com/326168466 the link uses the wrong
    font size.
    
    Bug: 40616749
    Change-Id: Ia363c74650bcc6ef9356f4ca1459f4f9ba8974d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5314245
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264213}

--- chrome/browser/ui/views/controls/rich_controls_container_view.h
+++ chrome/browser/ui/views/controls/rich_controls_container_view.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace views {
 class ImageView;
 class Label;
+class StyledLabel;
 }  // namespace views
 
 // A view that contains basic layout for a container with controls.
@@ -36,6 +37,7 @@ class RichControlsContainerView : public views::FlexLayoutView {
   void SetIcon(const ui::ImageModel image);
   void SetTitle(std::u16string title);
   views::Label* AddSecondaryLabel(std::u16string text);
+  views::StyledLabel* AddSecondaryStyledLabel(std::u16string text);
   template <typename T>
   T* AddControl(std::unique_ptr<T> control_view) {
     control_view->SetProperty(views::kInternalPaddingKey,
