197a1f1de6b20f7197ed610e34e4c757fab5e68b
libangyan <libangyan@microsoft.com>
2024-02-22 02:46:34 +0000

    enable speak content under the pointer on PWA and WV2
    Bug: 323771594
    
    Implements accessibility hit testing for RemoteCocoa so that Speak Content under the pointer can read the accessible objects under the user's pointer.
    
    This comes from a commit:https://chromium-review.googlesource.com/c/chromium/src/+/3861171 in 2022/9/9 and was reverted on 2023/2/8. The reason why it was reverted is that it causes some macOS tools hangs and OOMs.
    
    Now to resolve A11y related issues(such as hover text or Speak Content under the pointer), I want to re-land it with a feature flag in accessibility_features.h named kAccessibilityRemoteUIApp.
    
    Change-Id: I1924a9e84852f7df2c8ba1259b87e9096ab9a93f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5267921
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: yan libang <libangyan@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1263760}

--- content/app_shim_remote_cocoa/render_widget_host_view_cocoa.mm
+++ content/app_shim_remote_cocoa/render_widget_host_view_cocoa.mm
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/mojom/input/input_handler.mojom.h"
 #include "third_party/blink/public/platform/web_text_input_type.h"
+#include "ui/accessibility/accessibility_features.h"
 #import "ui/base/clipboard/clipboard_util_mac.h"
 #import "ui/base/cocoa/appkit_utils.h"
 #import "ui/base/cocoa/nsmenu_additions.h"
@@ -88,6 +89,7 @@ class DummyHostHelper : public RenderWidgetHostNSViewHostHelper {
 
  private:
   // RenderWidgetHostNSViewHostHelper implementation.
+  id GetAccessibilityElement() override { return nil; }
   id GetRootBrowserAccessibilityElement() override { return nil; }
   id GetFocusedBrowserAccessibilityElement() override { return nil; }
   void SetAccessibilityWindow(NSWindow* window) override {}
@@ -1891,6 +1893,12 @@ void ExtractUnderlines(NSAttributedString* string,
 - (id)accessibilityHitTest:(NSPoint)point {
   id rootElement = _hostHelper->GetRootBrowserAccessibilityElement();
   if (!rootElement) {
+    if (features::IsAccessibilityRemoteUIAppEnabled()) {
+      id rwhvElement = _hostHelper->GetAccessibilityElement();
+      if (rwhvElement && rwhvElement != self) {
+        return [rwhvElement accessibilityHitTest:point];
+      }
+    }
     return self;
   }
 
--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -1685,6 +1685,10 @@ void RenderWidgetHostViewMac::ShowSharePicker(
 // RenderWidgetHostNSViewHostHelper and mojom::RenderWidgetHostNSViewHost
 // implementation:
 
+id RenderWidgetHostViewMac::GetAccessibilityElement() {
+  return GetNativeViewAccessible();
+}
+
 id RenderWidgetHostViewMac::GetRootBrowserAccessibilityElement() {
   if (auto* manager = host()->GetRootBrowserAccessibilityManager())
     return manager->GetBrowserAccessibilityRoot()->GetNativeViewAccessible();
@@ -2201,6 +2205,15 @@ void RenderWidgetHostViewMac::StartSpeaking() {
 
 void RenderWidgetHostViewMac::StopSpeaking() {
   ui::TextServicesContextMenu::StopSpeaking();
+}
+
+void RenderWidgetHostViewMac::GetRenderWidgetAccessibilityToken(
+    GetRenderWidgetAccessibilityTokenCallback callback) {
+  base::ProcessId pid = getpid();
+  id element_id = GetNativeViewAccessible();
+  std::vector<uint8_t> token =
+      ui::RemoteAccessibility::GetTokenForLocalElement(element_id);
+  std::move(callback).Run(pid, token);
 }
 
 void RenderWidgetHostViewMac::SetRemoteAccessibilityWindowToken(
