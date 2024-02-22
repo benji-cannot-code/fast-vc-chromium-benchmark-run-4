3c05b0b2b71ab267da96ae023115e419ae6859e7
Alexis Hetu <sugoi@chromium.org>
2024-02-22 21:48:41 +0000

    [iOSKAUpgrade] Adjust minimum button target area size
    This CL makes sure the new keyboard accessory's buttons
    have a minimum target area of 44x44.
    
    Bug: 1501851
    Change-Id: I1b3dae30209f2446a5177dfbd125835e6942afaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5301512
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264238}

--- ios/chrome/common/ui/elements/form_input_accessory_view.mm
+++ ios/chrome/common/ui/elements/form_input_accessory_view.mm
@@ -17,10 +17,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Default Height for the accessory.
-const CGFloat kDefaultAccessoryHeight = 44;
+constexpr CGFloat kDefaultAccessoryHeight = 44;
 
 // Large Height for the accessory.
-const CGFloat kLargeAccessoryHeight = 59;
+constexpr CGFloat kLargeAccessoryHeight = 59;
+
+// Button target area for the large keyboard accessory.
+constexpr CGFloat kLargeButtonTargetArea = 44;
 
 // The width for the white gradient UIView.
 constexpr CGFloat ManualFillGradientWidth = 44;
@@ -318,6 +321,14 @@ NSString* const kFormInputAccessoryViewOmniboxTypingShieldAccessibilityID =
             accessibilityLabel:(NSString*)accessibilityLabel {
   UIButton* imageButton = [UIButton buttonWithType:UIButtonTypeSystem];
   [imageButton setImage:image forState:UIControlStateNormal];
+  if (_largeAccessoryViewEnabled) {
+    [imageButton.widthAnchor
+        constraintGreaterThanOrEqualToConstant:kLargeButtonTargetArea]
+        .active = YES;
+    [imageButton.heightAnchor
+        constraintGreaterThanOrEqualToConstant:kLargeButtonTargetArea]
+        .active = YES;
+  }
   [imageButton addTarget:self
                   action:action
         forControlEvents:UIControlEventTouchUpInside];
