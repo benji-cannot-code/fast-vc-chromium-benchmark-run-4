3bf728c90d6c7bf9ed41125cfb1461e4e21b50cd
David Tseng <dtseng@google.com>
2024-02-22 21:24:51 +0000

    [blink-a11y]: simplify BlinkAXTreeSource::SerializeNode
    Finishes pushing serialization logic into AXObject, removes
    obsolete TODO, and consolidates image data node state.
    
    R=aleventhal@chromium.org
    
    Bug: none
    Test: cq
    Change-Id: I0d1f50059b560233cd565a95a314eaadb780bcb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5318404
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264210}

--- third_party/blink/renderer/modules/accessibility/ax_object.h
+++ third_party/blink/renderer/modules/accessibility/ax_object.h
@@ -1510,6 +1510,10 @@ class MODULES_EXPORT AXObject : public GarbageCollected<AXObject> {
   // Serialization implemented in specific subclasses.
   virtual void SerializeMarkerAttributes(ui::AXNodeData* node_data) const;
 
+  void SerializeImageDataAttributes(ui::AXNodeData* node_data) const;
+  void SerializeTextInsertionDeletionOffsetAttributes(
+      ui::AXNodeData* node_data) const;
+
  private:
   bool ComputeCanSetFocusAttribute() const;
   String KeyboardShortcut() const;
