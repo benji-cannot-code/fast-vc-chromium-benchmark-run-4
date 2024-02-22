0c4faede5833257a6a8739b2e9e20185135368c0
Christian Biesinger <cbiesinger@chromium.org>
2024-02-22 21:57:32 +0000

    Reapply "[FedCM] Protect from `this` getting deleted"
    This reverts commit e91874a35e3e23319382588843e6293cc7fb55e2.
    
    Now adds initialization of popup_window_state_ to fix MSAN
    errors.
    
    R=npm@chromium.org
    
    Bug: 325936438, 40279117
    Change-Id: Ia99b3c81343bf01a8f37470b46a8d42db303cbaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5318014
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264246}

--- chrome/browser/ui/views/webid/fedcm_account_selection_view_desktop_browsertest.cc
+++ chrome/browser/ui/views/webid/fedcm_account_selection_view_desktop_browsertest.cc
@@ -28,7 +28,9 @@ class FedCmAccountSelectionViewBrowserTest : public DialogBrowserTest {
         std::make_unique<FedCmAccountSelectionView>(delegate());
   }
 
-  void ShowUi(const std::string& name) override {
+  void ShowUi(const std::string& name) override { ShowAccounts(); }
+
+  void ShowAccounts(Account::SignInMode mode = Account::SignInMode::kExplicit) {
     std::vector<content::IdentityRequestAccount> accounts = {
         {"id", "email", "name", "given_name", GURL(),
          /*login_hints=*/std::vector<std::string>(),
@@ -40,7 +42,7 @@ class FedCmAccountSelectionViewBrowserTest : public DialogBrowserTest {
           content::ClientMetadata(GURL(), GURL()),
           blink::mojom::RpContext::kSignIn, /*request_permission=*/true,
           /*has_login_status_mismatch=*/false}},
-        Account::SignInMode::kExplicit, blink::mojom::RpMode::kWidget,
+        mode, blink::mojom::RpMode::kWidget,
         /*new_account_idp*/ std::nullopt);
   }
 
@@ -59,7 +61,9 @@ class FedCmAccountSelectionViewBrowserTest : public DialogBrowserTest {
     return account_selection_view_.get();
   }
 
- private:
+  void ResetAccountSelectionView() { account_selection_view_ = nullptr; }
+
+ protected:
   base::test::ScopedFeatureList feature_list_;
   std::unique_ptr<FakeDelegate> delegate_;
   std::unique_ptr<FedCmAccountSelectionView> account_selection_view_;
@@ -119,6 +123,22 @@ IN_PROC_BROWSER_TEST_F(FedCmAccountSelectionViewBrowserTest, ShowWhileHidden) {
   browser()->tab_strip_model()->GetActiveWebContents()->WasShown();
   ASSERT_TRUE(GetDialog());
   EXPECT_TRUE(GetDialog()->IsVisible());
+}
+
+IN_PROC_BROWSER_TEST_F(FedCmAccountSelectionViewBrowserTest,
+                       ModalDialogThenShowThenCloseModalDialog) {
+  PreShow();
+  delegate_->SetAccountSelectedCallback(base::BindOnce(
+      &FedCmAccountSelectionViewBrowserTest::ResetAccountSelectionView,
+      base::Unretained(this)));
+  account_selection_view_->ShowModalDialog(GURL("https://example.test/"));
+  // Because a modal dialog is up, this should save the accounts for later.
+  ShowAccounts(Account::SignInMode::kAuto);
+  // This should trigger auto re-authn without crashing.
+  account_selection_view_->CloseModalDialog();
+  // The account selected callback should have been called, thus the view
+  // should be null now.
+  EXPECT_EQ(nullptr, account_selection_view_);
 }
 
 IN_PROC_BROWSER_TEST_F(FedCmAccountSelectionViewBrowserTest, DetachAndDelete) {
