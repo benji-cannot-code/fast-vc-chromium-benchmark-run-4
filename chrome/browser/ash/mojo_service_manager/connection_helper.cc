d14c2b2cbe4f02534fc2428cd8dd5bcf99b6da62
Joel Hockey <joelhockey@chromium.org>
2024-02-22 21:21:22 +0000

    Fix compile for simplechrome unit_tests
    Duplicate fakes in //chromeos/ash/components/mojo_service_manager
    into a :test_support target where they can be used by tests,
    but the fakes are still compiled into the linux-chromeos build.
    
    Move fakes //chromeos/ash/services/cros_healthd/public/cpp into :fakes and :test_support targets to be used by
    //chrome/browser/ash/mojo_service_manager/connection_helper.cc and tests.
    
    Change-Id: I1bb13a41c19ad09c10b0da696c97662b7b294e32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5314180
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Chung-sheng Wu <chungsheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1264204}

--- chrome/browser/ash/mojo_service_manager/connection_helper.cc
+++ chrome/browser/ash/mojo_service_manager/connection_helper.cc
@@ -14,8 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if !BUILDFLAG(USE_REAL_CHROMEOS_SERVICES)
 #include "base/system/sys_info.h"
-#include "chromeos/ash/components/mojo_service_manager/fake_mojo_service_manager.h"
-#include "chromeos/ash/services/cros_healthd/public/cpp/fake_cros_healthd.h"
+#include "chromeos/ash/components/mojo_service_manager/fake_mojo_service_manager.h"  // nogncheck
+#include "chromeos/ash/services/cros_healthd/public/cpp/fake_cros_healthd.h"  // nogncheck
 #endif
 
 namespace {
