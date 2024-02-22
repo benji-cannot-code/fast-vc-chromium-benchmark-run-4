da54c651c4cfc3646e0ec90df38b5c3b6315287b
Lan Wei <lanwei@chromium.org>
2024-02-22 22:00:47 +0000

    Use partly enable flag for Autofill UKM recording
    In this CL, we implement a way of sampling on the session to record
    Autofill UKM events on the client-side instead of keeping this feature
    in the finch trail for a long time. We can also overwrite the feature
    parameter, kAutofillLogUKMEventsWithSamplingOnSessionRate from the finch
    config file.
    
    For details, please check this doc:
    go/partly_enabled_flag_autofill_ukm_recording
    
    
    Bug: 1325851
    Change-Id: Ic2e804a60ff35e779556692c68187e3ff4d98d84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5296838
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264251}

--- components/autofill/core/browser/metrics/autofill_metrics_unittest.cc
+++ components/autofill/core/browser/metrics/autofill_metrics_unittest.cc
@@ -8049,9 +8049,14 @@ class AutofillMetricsSeamlessnessTest
 
  protected:
   AutofillMetricsSeamlessnessTest() {
-    scoped_features_.InitWithFeatures(
-        /*enabled_features=*/{features::kAutofillLogUKMEventsWithSampleRate,
-                              features::kAutofillParsingPatternProvider},
+    base::FieldTrialParams feature_parameters{
+        {features::kAutofillLogUKMEventsWithSamplingOnSessionRate.name, "100"},
+    };
+    scoped_features_.InitWithFeaturesAndParameters(
+        /*enabled_features=*/{{features::
+                                   kAutofillLogUKMEventsWithSamplingOnSession,
+                               feature_parameters},
+                              {features::kAutofillParsingPatternProvider, {}}},
         /*disabled_features=*/{});
   }
 
@@ -8381,9 +8386,14 @@ TEST_F(AutofillMetricsSeamlessnessTest, CreditCardFormRecordOnIFrames) {
 class AutofillMetricsFromLogEventsTest : public AutofillMetricsTest {
  protected:
   AutofillMetricsFromLogEventsTest() {
-    scoped_features_.InitWithFeatures(
-        /*enabled_features=*/{features::kAutofillLogUKMEventsWithSampleRate,
-                              features::kAutofillParsingPatternProvider},
+    base::FieldTrialParams feature_parameters{
+        {features::kAutofillLogUKMEventsWithSamplingOnSessionRate.name, "100"},
+    };
+    scoped_features_.InitWithFeaturesAndParameters(
+        /*enabled_features=*/{{features::
+                                   kAutofillLogUKMEventsWithSamplingOnSession,
+                               feature_parameters},
+                              {features::kAutofillParsingPatternProvider, {}}},
         /*disabled_features=*/{});
   }
 
