e377f5baea1cdfc2091695d53c70ebfb05449d03
Calder Kitagawa <ckitagawa@chromium.org>
2024-02-22 20:16:09 +0000

    [Tab Group] Rename GroupId to RootId in HistoricalTab*
    We are introducing TabGroupId as a base::Token to Android. To help
    disambiguate, migrate to the term "RootId" in HistoricalTab* related
    code.
    
    Note that RecentTabs code refers to GroupId using the GroupId in the
    form of a Token that is proxied into the SessionRestore. I'm going to
    try and unify that so we don't create proxy group IDs if
    AndroidTabGroupStableIds is enabled.
    
    Bug: b/41496693
    Change-Id: I817f25b558a1f67b8c114a5b2e12317a6a8ab38d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5310259
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Reviewed-by: Brandon Fong <bjfong@google.com>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264170}

--- chrome/android/junit/src/org/chromium/chrome/browser/tab/tab_restore/HistoricalTabSaverImplUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/tab/tab_restore/HistoricalTabSaverImplUnitTest.java
@@ -292,9 +292,9 @@ public class HistoricalTabSaverImplUnitTest {
         entries.add(new HistoricalEntry(3, "Group 3", Arrays.asList(new Tab[] {tab10, tab11})));
         mHistoricalTabSaver.createHistoricalBulkClosure(entries);
 
-        int[] groupIds = new int[] {1, 3};
+        int[] rootIds = new int[] {1, 3};
         String[] groupTitles = new String[] {"Group 1", "Group 3"};
-        int[] perTabGroupIds =
+        int[] perTabRootIds =
                 new int[] {Tab.INVALID_TAB_ID, 1, 1, Tab.INVALID_TAB_ID, Tab.INVALID_TAB_ID, 3, 3};
         Tab[] tabs = new Tab[] {tab0, tab4, tab6, tab7, tab8, tab10, tab11};
 
@@ -305,9 +305,9 @@ public class HistoricalTabSaverImplUnitTest {
         verify(mHistoricalTabSaverJni, times(1))
                 .createHistoricalBulkClosure(
                         eq(mTabModel),
-                        eq(groupIds),
+                        eq(rootIds),
                         eq(groupTitles),
-                        eq(perTabGroupIds),
+                        eq(perTabRootIds),
                         eq(tabs),
                         eq(buffers),
                         eq(versions));
