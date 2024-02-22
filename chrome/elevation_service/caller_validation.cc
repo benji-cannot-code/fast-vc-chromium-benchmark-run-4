d6abf636f8ef3d46d88821084295600459e53a82
Will Harris <wfh@chromium.org>
2024-02-22 22:35:52 +0000

    Add fuzzy path match for app-bound path validation
    This CL adds a 'fuzzy' match to the path validation. Upon capture
    of the path, the final filename is removed to obtain the path,
    then 'Temp' or 'Application' is removed, if present.
    
    This should result in a higher degree of matches to valid chrome
    instances in production, since it is possible that the installer
    might be midway through an update causing the exe name to be
    different (e.g. new_chrome.exe instead of chrome.exe) or in a
    temp path.
    
    Both of these situations should now result in validation passing.
    
    This code is intentionally designed to be backwards compatible with
    existing validation data.
    
    BUG=41493095
    
    Change-Id: I94404e1e23652545e787c7af1d19a5eef6c310d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5319332
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264277}

--- chrome/elevation_service/caller_validation.cc
+++ chrome/elevation_service/caller_validation.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/process/process.h"
+#include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "chrome/elevation_service/elevation_service_idl.h"
 
@@ -23,6 +24,44 @@ namespace {
 
 constexpr char kPathValidationPrefix[] = "PATH";
 constexpr char kNoneValidationPrefix[] = "NONE";
+
+// Paths look like this: "\Device\HarddiskVolume6\Program Files\Blah\app.exe".
+// This function will remove the final EXE, then it will remove paths that match
+// 'Temp' or 'Application' if they are the final directory.
+//
+// Examples:
+// "\Device\HarddiskVolume6\Program Files\Blah\app.exe" ->
+// "\Device\HarddiskVolume6\Program Files\Blah\"
+//
+// "\Device\HarddiskVolume6\Program Files\Blah\app2.exe" ->
+// "\Device\HarddiskVolume6\Program Files\Blah\"
+//
+// "\Device\HarddiskVolume6\Program Files\Blah\Temp\app.exe" ->
+// "\Device\HarddiskVolume6\Program Files\Blah\"
+//
+// "\Device\HarddiskVolume6\Program Files\Blah\Application\app.exe" ->
+// "\Device\HarddiskVolume6\Program Files\Blah\"
+//
+// Note: base::FilePath is not used here because NT paths are not real paths.
+std::string MaybeTrimProcessPath(const std::string& full_path) {
+  auto tokens = base::SplitString(full_path, "\\", base::KEEP_WHITESPACE,
+                                  base::SPLIT_WANT_ALL);
+  std::string output;
+  size_t token = 0;
+  for (auto it = tokens.rbegin(); it != tokens.rend(); ++it) {
+    token++;
+    if (token == 1 &&
+        base::EndsWith(*it, ".exe", base::CompareCase::INSENSITIVE_ASCII)) {
+      continue;
+    }
+    if (token == 2 && (base::EqualsCaseInsensitiveASCII(*it, "Temp") ||
+                       base::EqualsCaseInsensitiveASCII(*it, "Application"))) {
+      continue;
+    }
+    output = *it + "\\" + output;
+  }
+  return output;
+}
 
 std::string GetProcessExecutablePath(const base::Process& process) {
   std::string image_path(MAX_PATH, L'\0');
@@ -53,7 +92,8 @@ std::string GeneratePathValidationData(const base::Process& process) {
 }
 
 bool ValidatePath(const base::Process& process, const std::string& data) {
-  return data == GetProcessExecutablePath(process);
+  return MaybeTrimProcessPath(data) ==
+         MaybeTrimProcessPath(GetProcessExecutablePath(process));
 }
 
 }  // namespace
