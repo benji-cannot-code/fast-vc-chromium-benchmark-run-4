50eebb92afb47998b7b3826ae353ac63ce2fb5d8
Evan Stade <estade@chromium.org>
2024-02-22 18:48:41 +0000

    IDB: remove kIDBCloseImmediatelySwitch
    This switch was added in 5a30eb49336338ff7fc5c73be68ab23664c08cbc and
    appears to have no current purpose.
    
    Bug: none
    Change-Id: I52cc022fbe37668f5bc091e728bd07e95973f1dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5315546
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Auto-Submit: Evan Stade <estade@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264108}

--- content/browser/indexed_db/indexed_db_bucket_context.cc
+++ content/browser/indexed_db/indexed_db_bucket_context.cc
@@ -1106,9 +1106,7 @@ void IndexedDBBucketContext::StartClosing() {
   DCHECK(CanClose());
   DCHECK(!IsClosing());
 
-  if (skip_closing_sequence_ ||
-      base::CommandLine::ForCurrentProcess()->HasSwitch(
-          kIDBCloseImmediatelySwitch)) {
+  if (skip_closing_sequence_) {
     CloseNow();
     return;
   }
