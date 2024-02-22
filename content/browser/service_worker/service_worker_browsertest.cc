5138693d606135c52aa80b3130eb088cb9880669
Ayu Ishii <ayui@chromium.org>
2024-02-22 19:03:52 +0000

    CacheStorage: Cleanup methods from CacheStorageHelper
    Bug: 40826521
    Change-Id: I70d5d83b048d40d4bb15f704e9ee3ace1bf8142c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5310505
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264124}

--- content/browser/service_worker/service_worker_browsertest.cc
+++ content/browser/service_worker/service_worker_browsertest.cc
@@ -2766,14 +2766,6 @@ class CacheStorageControlForBadOrigin
     // is bad.
     NOTREACHED();
   }
-  void DeleteForStorageKey(const blink::StorageKey& storage_key) override {
-    NOTREACHED();
-  }
-  void GetAllStorageKeysInfo(
-      storage::mojom::CacheStorageControl::GetAllStorageKeysInfoCallback
-          callback) override {
-    NOTREACHED();
-  }
   void AddObserver(mojo::PendingRemote<storage::mojom::CacheStorageObserver>
                        observer) override {
     NOTREACHED();
