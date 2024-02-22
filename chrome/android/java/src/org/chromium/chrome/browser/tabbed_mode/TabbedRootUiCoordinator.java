c35275e5f7a320a74af8f42693a9e28533c4ae9b
Andrea Gomez <andreaxg@google.com>
2024-02-22 23:17:09 +0000

    Reland "IPH and highlight for the app menu button"
    This reverts commit 7d323341ee6ac9c9a6f1fb757c06166d6c13cfe8.
    
    Reason for revert: CL causing failure has been reverted.
    
    Original change's description:
    > Revert "IPH and highlight for the app menu button"
    >
    > This reverts commit c93862b17bf1036e16ff0717a6e41260fd319cb2.
    >
    > Reason for revert:
    > Appears to break the Android arm64 Builder All Targets (dbg) build
    >
    > Original change's description:
    > > IPH and highlight for the app menu button
    > >
    > > Add a ReadAloud IPH controller class that will decide whether to
    > > request an IPH for the 'Listen to this page' button in the app menu
    > > depending on the tab's readability. Triggered onPageLoadFinished.
    > >
    > > Bug: 323224119
    > > Change-Id: I4572eb257fb9fee2de7bccc4dbaa972ecc13652d
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5255258
    > > Commit-Queue: Andrea Gomez <andreaxg@google.com>
    > > Reviewed-by: Dana Fried <dfried@chromium.org>
    > > Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    > > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > > Reviewed-by: Basia Zimirska <basiaz@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1263647}
    >
    > Bug: 323224119
    > Change-Id: I187083e2a80fdfeba0138f0704a28afb2c523214
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5313958
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    > Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    > Owners-Override: Emilia Paz <emiliapaz@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1263696}
    
    Bug: 323224119
    Change-Id: I0beac883f426dfad5be99f200acdfb83d310fa3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5318435
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Andrea Gomez <andreaxg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1264301}

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
@@ -92,6 +92,7 @@ import org.chromium.chrome.browser.privacy_sandbox.PrivacySandboxDialogControlle
 import org.chromium.chrome.browser.privacy_sandbox.TrackingProtectionNoticeController;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.read_later.ReadLaterIPHController;
+import org.chromium.chrome.browser.readaloud.ReadAloudIPHController;
 import org.chromium.chrome.browser.settings.SettingsLauncherImpl;
 import org.chromium.chrome.browser.share.ShareDelegate;
 import org.chromium.chrome.browser.share.link_to_text.LinkToTextIPHController;
@@ -155,6 +156,7 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
     private StatusIndicatorCoordinator.StatusIndicatorObserver mStatusIndicatorObserver;
     private OfflineIndicatorControllerV2 mOfflineIndicatorController;
     private OfflineIndicatorInProductHelpController mOfflineIndicatorInProductHelpController;
+    private ReadAloudIPHController mReadAloudIPHController;
     private ReadLaterIPHController mReadLaterIPHController;
     private DesktopSiteSettingsIPHController mDesktopSiteSettingsIPHController;
     private WebFeedFollowIntroController mWebFeedFollowIntroController;
@@ -432,6 +434,10 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
 
         if (mToolbarButtonInProductHelpController != null) {
             mToolbarButtonInProductHelpController.destroy();
+        }
+
+        if (mReadAloudIPHController != null) {
+            mReadAloudIPHController.destroy();
         }
 
         if (mWebFeedFollowIntroController != null) {
@@ -826,6 +832,13 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
                         mIsInOverviewModeSupplier,
                         mToolbarManager.getMenuButtonView(),
                         mToolbarManager.getSecurityIconView());
+        mReadAloudIPHController =
+                new ReadAloudIPHController(
+                        mActivity,
+                        getToolbarManager().getMenuButtonView(),
+                        mAppMenuCoordinator.getAppMenuHandler(),
+                        mActivityTabProvider,
+                        mReadAloudControllerSupplier);
         mReadLaterIPHController =
                 new ReadLaterIPHController(
                         mActivity,
--- components/feature_engagement/public/feature_list.cc
+++ components/feature_engagement/public/feature_list.cc
@@ -76,6 +76,7 @@ const base::Feature* const kAllFeatures[] = {
     &kIPHPriceDropNTPFeature,
     &kIPHPwaInstallAvailableFeature,
     &kIPHQuietNotificationPromptsFeature,
+    &kIPHReadAloudAppMenuFeature,
     &kIPHReadLaterContextMenuFeature,
     &kIPHReadLaterAppMenuBookmarkThisPageFeature,
     &kIPHReadLaterAppMenuBookmarksFeature,
