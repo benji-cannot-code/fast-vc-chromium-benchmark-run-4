ee668a1990ababc950cd57ee55c65c223c013d37
Brandon Wylie <wylieb@chromium.org>
2024-02-22 20:48:15 +0000

    [bookmarks] Add margins to the section header
    Bug: 325663661
    Change-Id: I7337c47317df51a283fe0fb028ad6b3fe656a364
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5311874
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1264182}

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkFolderPickerRenderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkFolderPickerRenderTest.java
@@ -90,7 +90,7 @@ public class BookmarkFolderPickerRenderTest {
     @Rule
     public final ChromeRenderTestRule mRenderTestRule =
             ChromeRenderTestRule.Builder.withPublicCorpus()
-                    .setRevision(5)
+                    .setRevision(6)
                     .setBugComponent(ChromeRenderTestRule.Component.UI_BROWSER_BOOKMARKS)
                     .build();
 
