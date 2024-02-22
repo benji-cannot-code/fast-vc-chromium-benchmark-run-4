817f72da291b84e814e6ff895d83acbbe795fccb
Salvador Guerrero <salg@google.com>
2024-02-22 01:37:13 +0000

    Added Java interface for model execution
    This CL adds an empty interface to expose model execution to Java code.
    
    Bug: b/324912283
    Change-Id: I8f89b41196d15ae97c5fec0abbca79ad82abc533
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5293024
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263727}

---
+++ chrome/browser/model_execution/android/java/src/org/chromium/chrome/browser/model_execution/ModelExecutionSessionImpl.java
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.model_execution;
+
+import org.chromium.base.Callback;
+import org.chromium.chrome.browser.model_execution.ExecutionResult.ExecutionError;
+
+/**
+ * Instantiable version of {@link ModelExecutionSession}. don't add anything to this class.
+ * Downstream targets may provide a different implementation. In GN, we specify that {@link
+ * ModelExecutionSession} is compiled separately from its implementation; other projects may specify
+ * a different DrivingRestrictionsDelegate via GN.
+ */
+class ModelExecutionSessionImpl extends ModelExecutionSession {
+
+    @Override
+    protected void executeModel(String request, Callback<ExecutionResult> streamingResultCallback) {
+        streamingResultCallback.onResult(new ExecutionResult(ExecutionError.NOT_AVAILABLE));
+    }
+}
