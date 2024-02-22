e2240c7b6a3aa8046283d16fb133b777a88d2d14
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-02-22 10:21:41 +0000

    [gardener] Disable some flaky password manager tests on Mac
    Bug: 326170284
    Change-Id: I0735450439a2eec2f995b6122ef286967302c62e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5317149
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Owners-Override: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263867}

--- chrome/browser/password_manager/credential_manager_browsertest.cc
+++ chrome/browser/password_manager/credential_manager_browsertest.cc
@@ -268,9 +268,16 @@ class CredentialManagerBrowserTest : public PasswordManagerBrowserTestBase {
 };
 
 // Tests.
-
+// TODO(crbug.com/326170284) Flaky on Mac, fix and re-enable.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_AccountChooserWithOldCredentialAndNavigation \
+  DISABLED_AccountChooserWithOldCredentialAndNavigation
+#else
+#define MAYBE_AccountChooserWithOldCredentialAndNavigation \
+  AccountChooserWithOldCredentialAndNavigation
+#endif
 IN_PROC_BROWSER_TEST_F(CredentialManagerBrowserTest,
-                       AccountChooserWithOldCredentialAndNavigation) {
+                       MAYBE_AccountChooserWithOldCredentialAndNavigation) {
   // Save credentials with 'skip_zero_click'.
   scoped_refptr<password_manager::TestPasswordStore> password_store =
       static_cast<password_manager::TestPasswordStore*>(
@@ -412,8 +419,16 @@ IN_PROC_BROWSER_TEST_F(CredentialManagerBrowserTest,
   }
 }
 
+// TODO(crbug.com/326170284) Flaky on Mac, fix and re-enable.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_StoreUpdatesPasswordOfExistingCredential \
+  DISABLED_StoreUpdatesPasswordOfExistingCredential
+#else
+#define MAYBE_StoreUpdatesPasswordOfExistingCredential \
+  StoreUpdatesPasswordOfExistingCredential
+#endif
 IN_PROC_BROWSER_TEST_F(CredentialManagerBrowserTest,
-                       StoreUpdatesPasswordOfExistingCredential) {
+                       MAYBE_StoreUpdatesPasswordOfExistingCredential) {
   scoped_refptr<password_manager::TestPasswordStore> password_store =
       static_cast<password_manager::TestPasswordStore*>(
           ProfilePasswordStoreFactory::GetForProfile(
@@ -713,8 +728,16 @@ IN_PROC_BROWSER_TEST_F(CredentialManagerBrowserTest,
   EXPECT_EQ(u"P4SSW0RD", passwords[www_url.spec()].front().password_value);
 }
 
+// TODO(crbug.com/326170284) Flaky on Mac, fix and re-enable.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_ObsoleteHttpCredentialMovedOnMigrationToHstsSite \
+  DISABLED_ObsoleteHttpCredentialMovedOnMigrationToHstsSite
+#else
+#define MAYBE_ObsoleteHttpCredentialMovedOnMigrationToHstsSite \
+  ObsoleteHttpCredentialMovedOnMigrationToHstsSite
+#endif
 IN_PROC_BROWSER_TEST_F(CredentialManagerBrowserTest,
-                       ObsoleteHttpCredentialMovedOnMigrationToHstsSite) {
+                       MAYBE_ObsoleteHttpCredentialMovedOnMigrationToHstsSite) {
   // Add an http credential to the password store.
   GURL https_origin = https_test_server().base_url();
   ASSERT_TRUE(https_origin.SchemeIs(url::kHttpsScheme));
