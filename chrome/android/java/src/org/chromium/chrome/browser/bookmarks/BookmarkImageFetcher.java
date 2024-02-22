669254bb5931e57d066da27e5f71f88d4ebb52a6
Brandon Wylie <wylieb@chromium.org>
2024-02-22 21:30:13 +0000

    [bookmarks] Use shopping meta for image when available
    When transitioning to ImprovedBookmarkRow, the logic to fetch shopping
    specific images was dropped accidentally. Restoring this logic to the
    currently used codepaths.
    
    Bug: 326309337
    Change-Id: I569ae687ff4f84b908ed09b8dde27c561532723b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5315747
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264219}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkImageFetcher.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkImageFetcher.java
@@ -19,6 +19,7 @@ import org.chromium.components.bookmarks.BookmarkItem;
 import org.chromium.components.browser_ui.widget.RoundedIconGenerator;
 import org.chromium.components.favicon.LargeIconBridge;
 import org.chromium.components.image_fetcher.ImageFetcher;
+import org.chromium.components.power_bookmarks.PowerBookmarkMeta;
 import org.chromium.url.GURL;
 
 import java.util.Iterator;
@@ -176,9 +177,7 @@ public class BookmarkImageFetcher {
                             }
                         });
 
-        // This call may invoke the callback immediately if the url is cached.
-        mPageImageServiceQueue.getSalientImageUrl(
-                item.getUrl(),
+        Callback<GURL> imageUrlCallback =
                 mCallbackController.makeCancelable(
                         (imageUrl) -> {
                             if (imageUrl == null) {
@@ -193,7 +192,18 @@ public class BookmarkImageFetcher {
                                             mImageSize,
                                             mImageSize),
                                     bookmarkImageCallback);
-                        }));
+                        });
+
+        // Price-tracable bookmarks already have image URLs in their metadata. Prioritize that meta
+        // when it's available because the coverage is much higher.
+        PowerBookmarkMeta meta = mBookmarkModel.getPowerBookmarkMeta(item.getId());
+        if (meta != null && meta.hasShoppingSpecifics() && meta.hasLeadImage()) {
+            imageUrlCallback.onResult(new GURL(meta.getLeadImage().getUrl()));
+            return;
+        }
+
+        // This call may invoke the callback immediately if the url is cached.
+        mPageImageServiceQueue.getSalientImageUrl(item.getUrl(), imageUrlCallback);
     }
 
     private void fetchImageUrl(GURL url, Callback<Drawable> callback) {
