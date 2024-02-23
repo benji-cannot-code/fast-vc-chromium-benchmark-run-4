82e11f06b5b4c32c84b99acbd15f5a53f12fa16b
Gauthier Ambard <gambard@chromium.org>
2024-02-23 12:09:48 +0000

    Prevent conflict on SessionIDGenerator after restoring a session
    After restoring a session, the highest restored ID is sometimes higher
    than the next SessionID, even taking into account the cautionary
    padding.
    This CL adds a way for embedders to provide information about their
    highest restored session ID. The Generator can then increment the pref
    to avoid conflicts.
    
    Bug: 325534303, 41495439
    Change-Id: Ia6507c32991253c6fa8e83d806080200cb5bf783
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5307820
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264494}

--- ios/chrome/browser/sessions/web_state_list_serialization.mm
+++ ios/chrome/browser/sessions/web_state_list_serialization.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/metrics/histogram_functions.h"
 #import "base/strings/sys_string_conversions.h"
 #import "components/sessions/core/session_id.h"
+#import "components/sessions/core/session_id_generator.h"
 #import "ios/chrome/browser/sessions/features.h"
 #import "ios/chrome/browser/sessions/proto/storage.pb.h"
 #import "ios/chrome/browser/sessions/session_constants.h"
@@ -311,7 +312,7 @@ std::vector<web::WebState*> DeserializeWebStateListInternal(
   // the WebState as active during the insertion, if restored.
   const int active_index = deserializer.GetActiveIndex();
 
-  int32_t max_identifier = -1;
+  SessionID max_identifier = SessionID::InvalidValue();
 
   // Restore all items directly at their correct position in the WebStateList.
   // The opener-opened relationship is not restored yet, as some WebState may
@@ -324,7 +325,10 @@ std::vector<web::WebState*> DeserializeWebStateListInternal(
             session::features::kSessionRestorationSessionIDCheck)) {
       web::WebStateID web_state_id = web_state->GetUniqueIdentifier();
       CHECK(web_state_id.valid(), base::NotFatalUntil::M125);
-      max_identifier = std::max(max_identifier, web_state_id.identifier());
+      if (!max_identifier.is_valid() ||
+          max_identifier.id() < web_state_id.identifier()) {
+        max_identifier = web_state_id.ToSessionID();
+      }
     }
 
     const int inserted_index = web_state_list->InsertWebState(
@@ -337,12 +341,8 @@ std::vector<web::WebState*> DeserializeWebStateListInternal(
 
   if (base::FeatureList::IsEnabled(
           session::features::kSessionRestorationSessionIDCheck)) {
-    int32_t next_id = SessionID::NewUnique().id();
-    if (next_id > max_identifier - 10000000) {
-      // In case the ID went over the max int value, make sure that we only
-      // compare them if they are not too far.
-      CHECK_LT(max_identifier, next_id, base::NotFatalUntil::M125);
-    }
+    sessions::SessionIdGenerator::GetInstance()->SetHighestRestoredID(
+        max_identifier);
   }
 
   // Check that all WebStates have been restored.
