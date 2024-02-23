564e8fcac34d50394ff45bac40254d9c5ee1ffb6
sbingler <bingler@chromium.org>
2024-02-23 21:27:25 +0000

    [OBC] Update garbage collection to prefer domain cookies
    This CL implements origin-bound cookies' domain garbage collection
    behavior. Primarily that domain cookies should be evicted before
    host only cookies for a given {priority, secureness} tuple. E.x.:
    A {medium, non-secure} domain cookies should be evicted before a
    {medium, non-secure} host cookie.
    
    This new eviction behavior is only active when at least one portion
    of OBC is active: scheme binding or port binding. Otherwise the existing
    eviction algorithm is used. I.e.: When OBC is off we keep the
    pre-existing behavior.
    
    When scheme binding is active, the eviction algorithm will now consider a secure cookie to be one that:
    * Has the secure attribute.
    * Was set by a secure scheme.
    
    To show that priority based eviction behavior hasn't changed this CL
    updates the existing tests to run for all possible OBC states which
    requires some minor test adjustments in order to properly support OBC.
    
    It is intentional that the new eviction algorithm still considers both
    secure and non-secure cookies for the same key. This is because
    splitting the cookie store by secureness is out of scope of the initial
    OBC design.
    
    This CL does not update global evictions, that will be handled in a
    later change.
    
    Bug: 1170548
    Change-Id: I39725fb8f35c1ce4906b6a50ba136b329d305047
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5301781
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Reviewed-by: Dylan Cutler <dylancutler@google.com>
    Cr-Commit-Position: refs/heads/main@{#1264750}

--- net/cookies/cookie_util.cc
+++ net/cookies/cookie_util.cc
@@ -875,6 +875,10 @@ bool IsSchemeBoundCookiesEnabled() {
   return base::FeatureList::IsEnabled(features::kEnableSchemeBoundCookies);
 }
 
+bool IsOriginBoundCookiesPartiallyEnabled() {
+  return IsPortBoundCookiesEnabled() || IsSchemeBoundCookiesEnabled();
+}
+
 bool IsTimeLimitedInsecureCookiesEnabled() {
   return IsSchemeBoundCookiesEnabled() &&
          base::FeatureList::IsEnabled(features::kTimeLimitedInsecureCookies);
