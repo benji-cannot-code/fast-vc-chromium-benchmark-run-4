f1dfe25901b826dc99b45a26ee9ac5d901f65bcf
Peter Marshall <petermarshall@chromium.org>
2024-02-23 03:45:49 +0000

    Revert "[FedCM] Implement modal verifying sheet"
    This reverts commit 39f5b1d4da7a7ddb682a30cd55383c3d2554c3d1.
    
    Reason for revert: Failing tests on linux-ubsan-vptr
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-ubsan-vptr/28612/overview
    
    Original change's description:
    > [FedCM] Implement modal verifying sheet
    >
    > When ShowVerifyingSheet is called in a modal FedCM dialog, a progress
    > bar is added at the very top of the dialog and the buttons are disabled.
    >
    > Screenshot
    > https://issues.chromium.org/action/issues/41491333/attachments/53846790?download=false
    >
    > Bug: 41491333
    > Change-Id: I10f106019350d16c3a97cec3d87af687fae600f0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5314683
    > Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    > Auto-Submit: Zachary Tan <tanzachary@chromium.org>
    > Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1264086}
    
    Bug: 41491333
    Change-Id: I2a93770ae810aa9b4db78154aa5bdcb7c35ad39e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5320210
    Owners-Override: Peter Marshall <petermarshall@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264407}

--- chrome/browser/ui/views/webid/account_selection_modal_view.h
+++ chrome/browser/ui/views/webid/account_selection_modal_view.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/webid/federated_auth_request.mojom.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/controls/button/button.h"
-#include "ui/views/controls/progress_bar.h"
 #include "ui/views/window/dialog_delegate.h"
 
 class AccountSelectionModalView : public views::DialogDelegateView,
@@ -89,12 +88,10 @@ class AccountSelectionModalView : public views::DialogDelegateView,
   // Returns a View for single account chooser. It contains a row of account
   // information. `The size of the `idp_display_data.accounts` vector must be 1.
   // `should_hover` determines whether the row is clickable.
-  // `show_disclosure_label` determines whether disclosure text is shown.
   std::unique_ptr<views::View> CreateSingleAccountChooser(
       const IdentityProviderDisplayData& idp_display_data,
       const content::IdentityRequestAccount& account,
-      bool should_hover,
-      bool show_disclosure_label);
+      bool should_hover);
 
   // Returns a View for multiple account chooser. It contains the info for each
   // account in a button, so the user can pick an account.
@@ -114,15 +111,6 @@ class AccountSelectionModalView : public views::DialogDelegateView,
 
   // Resizes the modal dialog to the size of its contents.
   void UpdateModalPositionAndTitle();
-
-  // View containing the modal dialog header.
-  raw_ptr<views::View> header_view_ = nullptr;
-
-  // View containing the modal dialog button row.
-  raw_ptr<views::View> button_row_ = nullptr;
-
-  // View containing the modal dialog account chooser.
-  raw_ptr<views::View> account_chooser_ = nullptr;
 
   // View containing the modal dialog title.
   raw_ptr<views::Label> title_label_ = nullptr;
