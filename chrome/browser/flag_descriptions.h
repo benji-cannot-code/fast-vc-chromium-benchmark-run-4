f6fb0d106e88f8cf938d41ebf25909c716ae7e30
Paul Adedeji <pauladedeji@google.com>
2024-02-23 15:51:54 +0000

    Create flag for Appearance Wallpaper Search button
    Creates feature flag for Wallpaper Search entry point on Customize Chrome's Appearance page.
    
    
    Change-Id: I96b01e2d6150b538c318f2fe2d88efd39d54b9d7
    Bug: 326415816
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5318400
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264561}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -159,6 +159,9 @@ extern const char kCustomizeChromeSidePanelExtensionsCardDescription[];
 extern const char kCustomizeChromeWallpaperSearchName[];
 extern const char kCustomizeChromeWallpaperSearchDescription[];
 
+extern const char kCustomizeChromeWallpaperSearchButtonName[];
+extern const char kCustomizeChromeWallpaperSearchButtonDescription[];
+
 extern const char kCustomizeChromeWallpaperSearchInspirationCardName[];
 extern const char kCustomizeChromeWallpaperSearchInspirationCardDescription[];
 
