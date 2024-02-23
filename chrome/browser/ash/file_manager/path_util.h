18abc3b0feb710ef9e5723d435bffa92443bb81b
François Degros <fdegros@chromium.org>
2024-02-23 00:32:32 +0000

    Shorten code in path_util*
    No functional change.
    
    BUG=b/253122024, b/326123463
    TEST=unit_tests --gtest_filter=FileManagerPathUtilTest.*
    
    Change-Id: Ib5162fe162ae444bd4e6c5d7747f59fbc4e4c411
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5320289
    Auto-Submit: François Degros <fdegros@chromium.org>
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264347}

--- chrome/browser/ash/file_manager/path_util.h
+++ chrome/browser/ash/file_manager/path_util.h
@@ -27,8 +27,7 @@ class DataTransferEndpoint;
 struct FileInfo;
 }  // namespace ui
 
-namespace file_manager {
-namespace util {
+namespace file_manager::util {
 
 // Absolute path for FuseBox media mount point (sans a trailing slash).
 extern const base::FilePath::CharType kFuseBoxMediaPath[];
@@ -254,7 +253,6 @@ std::vector<ui::FileInfo> ParseFileSystemSources(
     const ui::DataTransferEndpoint* source,
     const base::Pickle& pickle);
 
-}  // namespace util
-}  // namespace file_manager
+}  // namespace file_manager::util
 
 #endif  // CHROME_BROWSER_ASH_FILE_MANAGER_PATH_UTIL_H_
