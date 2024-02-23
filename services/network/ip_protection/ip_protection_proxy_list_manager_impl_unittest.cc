951283fd22f9470a7b565276e0ad6065355bcc9f
Ashley Newson <ashleynewson@chromium.org>
2024-02-23 16:10:42 +0000

    [IP Protection] Add GetProxyListResult and ProxyListRefreshTime histograms
    Adds a NetworkService.IpProtection.GetProxyListResult histogram to
    measure whether proxy list retrieval is successful. The histogram
    distinguishes between three cases:
    
    - Failure to get any list
    - Got a list, but it had no (acceptable) proxy chains
    - Got a list with (at least some) acceptable proxy chains
    
    Also adds a NetworkService.IpProtection.ProxyListRefreshTime histogram
    to measure the time taken to refresh a proxy list.
    
    Bug: 41496228
    Change-Id: I1405c80f6ecf289c9189fb6095b7cc39fc564162
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5272492
    Reviewed-by: Abhijith Nair <abhijithnair@chromium.org>
    Reviewed-by: Dustin Mitchell <djmitche@chromium.org>
    Reviewed-by: Matthew Wang <matthewmwang@chromium.org>
    Commit-Queue: Ashley Newson <ashleynewson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264570}

--- services/network/ip_protection/ip_protection_proxy_list_manager_impl_unittest.cc
+++ services/network/ip_protection/ip_protection_proxy_list_manager_impl_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/strings/stringprintf.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/task_environment.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "net/base/proxy_chain.h"
@@ -19,6 +20,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace network {
+
+namespace {
+
+constexpr char kGetProxyListResultHistogram[] =
+    "NetworkService.IpProtection.GetProxyListResult";
+constexpr char kProxyListRefreshTimeHistogram[] =
+    "NetworkService.IpProtection.ProxyListRefreshTime";
+
+}  // namespace
 
 class MockIpProtectionConfigGetter
     : public network::mojom::IpProtectionConfigGetter {
@@ -119,6 +129,8 @@ class IpProtectionProxyListManagerImplTest : public testing::Test {
 
   // The IpProtectionProxyListImpl being tested.
   std::unique_ptr<IpProtectionProxyListManagerImpl> ipp_proxy_list_;
+
+  base::HistogramTester histogram_tester_;
 };
 
 // The manager gets the proxy list on startup and once again on schedule.
@@ -185,6 +197,39 @@ TEST_F(IpProtectionProxyListManagerImplTest, ProxyListKeptAfterFailure) {
   ASSERT_TRUE(mock_.GotAllExpectedMockCalls());
   EXPECT_TRUE(ipp_proxy_list_->IsProxyListAvailable());
   EXPECT_EQ(ipp_proxy_list_->ProxyList(), exp_proxy_list);
+}
+
+TEST_F(IpProtectionProxyListManagerImplTest, GetProxyListFailureRecorded) {
+  mock_.ExpectGetProxyListCallFailure();
+  ipp_proxy_list_->RequestRefreshProxyList();
+  WaitForProxyListRefresh();
+  ASSERT_TRUE(mock_.GotAllExpectedMockCalls());
+  histogram_tester_.ExpectUniqueSample(
+      kGetProxyListResultHistogram,
+      IpProtectionProxyListManagerImpl::ProxyListResult::kFailed, 1);
+  histogram_tester_.ExpectTotalCount(kProxyListRefreshTimeHistogram, 0);
+}
+
+TEST_F(IpProtectionProxyListManagerImplTest, GotEmptyProxyListRecorded) {
+  mock_.ExpectGetProxyListCall({});
+  ipp_proxy_list_->RequestRefreshProxyList();
+  WaitForProxyListRefresh();
+  ASSERT_TRUE(mock_.GotAllExpectedMockCalls());
+  histogram_tester_.ExpectUniqueSample(
+      kGetProxyListResultHistogram,
+      IpProtectionProxyListManagerImpl::ProxyListResult::kEmptyList, 1);
+  histogram_tester_.ExpectTotalCount(kProxyListRefreshTimeHistogram, 1);
+}
+
+TEST_F(IpProtectionProxyListManagerImplTest, GotPopulatedProxyListRecorded) {
+  mock_.ExpectGetProxyListCall({MakeChain({"a-proxy", "b-proxy"})});
+  ipp_proxy_list_->RequestRefreshProxyList();
+  WaitForProxyListRefresh();
+  ASSERT_TRUE(mock_.GotAllExpectedMockCalls());
+  histogram_tester_.ExpectUniqueSample(
+      kGetProxyListResultHistogram,
+      IpProtectionProxyListManagerImpl::ProxyListResult::kPopulatedList, 1);
+  histogram_tester_.ExpectTotalCount(kProxyListRefreshTimeHistogram, 1);
 }
 
 }  // namespace network
