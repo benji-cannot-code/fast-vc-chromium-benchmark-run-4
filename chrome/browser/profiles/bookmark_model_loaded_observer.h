b8cadf38e24e4ee447a32f5957a6d3ff93936de9
Mikel Astiz <mastiz@chromium.org>
2024-02-23 16:51:35 +0000

    Adopt ScopedObservation in BookmarkModelLoadedObserver
    No behavioral changes: ScopedObservation is generally less error-prone
    and, in this particular case, allows follow-up patches to refactor some
    BookmarkModelObserver signatures.
    
    Change-Id: If9d9bd7fa202bb44730c6d40087feb609570a839
    Bug: 326185948
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5321461
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264601}

--- chrome/browser/profiles/bookmark_model_loaded_observer.h
+++ chrome/browser/profiles/bookmark_model_loaded_observer.h
@@ -7,16 +7,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_PROFILES_BOOKMARK_MODEL_LOADED_OBSERVER_H_
 
 #include "base/memory/raw_ptr.h"
+#include "base/scoped_observation.h"
 #include "components/bookmarks/browser/base_bookmark_model_observer.h"
 
 class Profile;
 
+namespace bookmarks {
+class BookmarkModel;
+}  // namespace bookmarks
+
 class BookmarkModelLoadedObserver
     : public bookmarks::BaseBookmarkModelObserver {
  public:
-  explicit BookmarkModelLoadedObserver(Profile* profile);
-
+  BookmarkModelLoadedObserver(Profile* profile,
+                              bookmarks::BookmarkModel* model);
   BookmarkModelLoadedObserver(const BookmarkModelLoadedObserver&) = delete;
+  ~BookmarkModelLoadedObserver() override;
+
   BookmarkModelLoadedObserver& operator=(const BookmarkModelLoadedObserver&) =
       delete;
 
@@ -26,7 +33,10 @@ class BookmarkModelLoadedObserver
                            bool ids_reassigned) override;
   void BookmarkModelBeingDeleted(bookmarks::BookmarkModel* model) override;
 
-  raw_ptr<Profile> profile_;
+  const raw_ptr<Profile> profile_;
+  base::ScopedObservation<bookmarks::BookmarkModel,
+                          bookmarks::BaseBookmarkModelObserver>
+      observation_{this};
 };
 
 #endif  // CHROME_BROWSER_PROFILES_BOOKMARK_MODEL_LOADED_OBSERVER_H_
