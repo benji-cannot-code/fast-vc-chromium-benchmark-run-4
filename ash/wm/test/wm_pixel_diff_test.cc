ed220b667945616de566c99d5be78e5e16c7e27d
Sean Kau <skau@chromium.org>
2024-02-23 23:37:30 +0000

    Update pixel tests for ChromeRefresh2023 ash/wm
    Small color change in the text for "current desk" in the window cycler.
    For overview, there's one pixel with a different color.
    
    Bug: b:326288692
    Change-Id: Id2762bc1d5c12fce974531cbabc39344b515850e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5315980
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264806}

--- ash/wm/test/wm_pixel_diff_test.cc
+++ ash/wm/test/wm_pixel_diff_test.cc
@@ -20,8 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/window_cycle/window_cycle_list.h"
 #include "ash/wm/window_cycle/window_cycle_view.h"
 #include "ash/wm/window_state.h"
+#include "base/test/scoped_feature_list.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/aura/window.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/views/widget/widget.h"
 
@@ -33,11 +35,21 @@ namespace ash {
 // dark/light mode, tablet mode, etc.
 class WmPixelDiffTest : public AshTestBase {
  public:
+  WmPixelDiffTest() {
+    scoped_features_.InitWithFeatures({::features::kChromeRefresh2023,
+                                       ::features::kChromeRefreshSecondary2023,
+                                       ::features::kChromeRefresh2023NTB},
+                                      {});
+  }
+
   // AshTestBase:
   std::optional<pixel_test::InitParams> CreatePixelTestInitParams()
       const override {
     return pixel_test::InitParams();
   }
+
+ private:
+  base::test::ScopedFeatureList scoped_features_;
 };
 
 // A basic overview pixel test that shows three overview windows and the virtual
@@ -75,7 +87,7 @@ TEST_F(WmPixelDiffTest, OverviewAndDesksBarBasic) {
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "overview_and_desks_bar_basic",
-      /*revision_number=*/10, desk_widget, overview_widget1, overview_widget2,
+      /*revision_number=*/11, desk_widget, overview_widget1, overview_widget2,
       overview_widget3));
 }
 
@@ -154,7 +166,7 @@ TEST_F(WmPixelDiffTest, WindowCycleBasic) {
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "window_cycle_basic",
-      /*revision_number=*/19, widget));
+      /*revision_number=*/20, widget));
 }
 
 }  // namespace ash
