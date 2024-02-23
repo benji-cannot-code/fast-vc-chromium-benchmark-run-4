72ceacbf2c9409b2c8465816a55a4b2ca319f64e
Tommy C. Li <tommycli@chromium.org>
2024-02-23 01:50:40 +0000

    Add a storage sequenced task runner to the HistoryEmbeddingsService
    Also adds a stub of a SqlDatabase class that lives and is destroyed on
    that sequence.
    
    Bug: b/325524013
    Change-Id: I5424ce3dc37a98256e6159bfbac76d5f3abe11eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5319340
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264382}

--- components/history_embeddings/history_embeddings_service.h
+++ components/history_embeddings/history_embeddings_service.h
@@ -6,19 +6,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_HISTORY_EMBEDDINGS_HISTORY_EMBEDDINGS_SERVICE_H_
 #define COMPONENTS_HISTORY_EMBEDDINGS_HISTORY_EMBEDDINGS_SERVICE_H_
 
+#include <memory>
+
+#include "base/files/file_path.h"
+#include "base/task/sequenced_task_runner.h"
+#include "components/history_embeddings/sql_database.h"
 #include "components/keyed_service/core/keyed_service.h"
 
 namespace history_embeddings {
 
 class HistoryEmbeddingsService : public KeyedService {
  public:
-  HistoryEmbeddingsService();
+  // `storage_dir` will generally be the Profile directory.
+  explicit HistoryEmbeddingsService(const base::FilePath& storage_dir);
   HistoryEmbeddingsService(const HistoryEmbeddingsService&) = delete;
   HistoryEmbeddingsService& operator=(const HistoryEmbeddingsService&) = delete;
   ~HistoryEmbeddingsService() override;
 
   // KeyedService:
   void Shutdown() override;
+
+ private:
+  // All storage tasks are posted to this to avoid blocking the main thread.
+  scoped_refptr<base::SequencedTaskRunner> storage_task_runner_;
+
+  // Created on this main thread, but afterwards lives and is destroyed on the
+  // `storage_task_runner_` sequence.
+  std::unique_ptr<SqlDatabase> database_;
 };
 
 }  // namespace history_embeddings
--- components/history_embeddings/history_embeddings_service_unittest.cc
+++ components/history_embeddings/history_embeddings_service_unittest.cc
@@ -7,13 +7,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "testing/gmock/include/gmock/gmock.h"
+#include "base/files/file_path.h"
+#include "base/files/scoped_temp_dir.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace history_embeddings {
 
-TEST(HistoryEmbeddingsTest, Constructs) {
-  std::make_unique<HistoryEmbeddingsService>();
+class HistoryEmbeddingsTest : public testing::Test {
+ public:
+  void SetUp() override { CHECK(history_dir_.CreateUniqueTempDir()); }
+
+  void TearDown() override {}
+
+ protected:
+  base::ScopedTempDir history_dir_;
+};
+
+TEST_F(HistoryEmbeddingsTest, Constructs) {
+  std::make_unique<HistoryEmbeddingsService>(history_dir_.GetPath());
 }
 
 }  // namespace history_embeddings
