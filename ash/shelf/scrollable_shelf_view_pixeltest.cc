bc435b58a4c5711728c85ab72f079930fa2b4aa0
Sean Kau <skau@chromium.org>
2024-02-23 03:11:20 +0000

    Update pixel tests for ChromeRefresh2023 ash/shelf
    While here, remove enabled features Jelly and DeskButton.
    
    Change resulting from flag flip is a small change in the color of
    text due to a small difference in how browser (vs Ash) defines this
    color.
    
    Bug: b:326288692
    Change-Id: I46cb5127d292c1fa7d04728257b84cfb9765b5ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5315978
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264399}

--- ash/shelf/scrollable_shelf_view_pixeltest.cc
+++ ash/shelf/scrollable_shelf_view_pixeltest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test/pixel/ash_pixel_test_init_params.h"
 #include "base/test/scoped_feature_list.h"
 #include "chromeos/constants/chromeos_features.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/views/controls/menu/menu_item_view.h"
 #include "ui/views/controls/menu/submenu_view.h"
 
@@ -20,7 +21,10 @@ class ScrollableShelfViewPixelRTLTestBase : public ShelfTestBase {
  public:
   // ScrollableShelfTestBase:
   void SetUp() override {
-    scoped_features_.InitAndEnableFeature(chromeos::features::kJelly);
+    scoped_features_.InitWithFeatures({::features::kChromeRefresh2023,
+                                       ::features::kChromeRefreshSecondary2023,
+                                       ::features::kChromeRefresh2023NTB},
+                                      {});
     ShelfTestBase::SetUp();
     AddAppShortcutsUntilOverflow(/*use_alternative_color=*/true);
   }
@@ -85,10 +89,11 @@ class ScrollableShelfViewWithGuestModePixelTest
   }
 
   void SetUp() override {
-    scoped_features_.InitWithFeatures(
-        /*enabled_features=*/{chromeos::features::kJelly,
-                              features::kDeskButton},
-        /*disabled_features=*/{});
+    scoped_features_.InitWithFeatures({::features::kChromeRefresh2023,
+                                       ::features::kChromeRefreshSecondary2023,
+                                       ::features::kChromeRefresh2023NTB},
+                                      {});
+
     set_start_session(false);
 
     ShelfTestBase::SetUp();
@@ -118,7 +123,7 @@ TEST_P(ScrollableShelfViewWithGuestModePixelTest, VerifyShelfContextMenu) {
   // Verify the shelf context menu and the shelf.
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "shelf_context_menu",
-      /*revision_number=*/17,
+      /*revision_number=*/18,
       GetPrimaryShelf()
           ->shelf_widget()
           ->shelf_view_for_testing()
