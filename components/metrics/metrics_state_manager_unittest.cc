acb81607798e3c6bf17b014f570b881ff4de2652
Luc Nguyen <lucnguyen@google.com>
2024-02-23 19:16:02 +0000

    Address comments in LaCrOS initial client ID CL
    See additional comments in
    https://chromium-review.googlesource.com/c/chromium/src/+/5310595
    
    Bug: b/322530179
    Change-Id: I3350cf39d1cb58117df1209ae28c853f7735b37b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5317844
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264672}

--- components/metrics/metrics_state_manager_unittest.cc
+++ components/metrics/metrics_state_manager_unittest.cc
@@ -194,9 +194,8 @@ TEST_F(MetricsStateManagerTest, EntropySourceUsed_Low) {
   prefs_.SetInt64(prefs::kInstallDate, base::Time::Now().ToTimeT());
 
   std::unique_ptr<MetricsStateManager> state_manager(CreateStateManager());
+  // |enable_limited_entropy_mode| is irrelevant but is set for test coverage.
   state_manager->CreateEntropyProviders(
-      // |enable_limited_entropy_mode| is irrelevant but is set to true for test
-      // coverage.
       /*enable_limited_entropy_mode=*/true);
   EXPECT_EQ(state_manager->entropy_source_returned(),
             MetricsStateManager::ENTROPY_SOURCE_LOW);
@@ -206,8 +205,7 @@ TEST_F(MetricsStateManagerTest, EntropySourceUsed_Low) {
 TEST_F(MetricsStateManagerTest, EntropySourceUsed_High) {
   EnableMetricsReporting();
   std::unique_ptr<MetricsStateManager> state_manager(CreateStateManager());
-  // |enable_limited_entropy_mode| is irrelevant but is set to true for test
-  // coverage.
+  // |enable_limited_entropy_mode| is irrelevant but is set for test coverage.
   state_manager->CreateEntropyProviders(
       /*enable_limited_entropy_mode=*/true);
   EXPECT_EQ(state_manager->entropy_source_returned(),
@@ -221,8 +219,7 @@ TEST_F(MetricsStateManagerTest, EntropySourceUsed_High_ExternalClientId) {
   const std::string kExternalClientId = "abc";
   std::unique_ptr<MetricsStateManager> state_manager(
       CreateStateManager(kExternalClientId));
-  // |enable_limited_entropy_mode| is irrelevant but is set to true for test
-  // coverage.
+  // |enable_limited_entropy_mode| is irrelevant but is set for test coverage.
   state_manager->CreateEntropyProviders(
       /*enable_limited_entropy_mode=*/true);
   EXPECT_EQ(state_manager->entropy_source_returned(),
@@ -236,8 +233,7 @@ TEST_F(MetricsStateManagerTest,
   const std::string kExternalClientId = "abc";
   std::unique_ptr<MetricsStateManager> state_manager(
       CreateStateManager(kExternalClientId));
-  // |enable_limited_entropy_mode| is irrelevant but is set to true for test
-  // coverage.
+  // |enable_limited_entropy_mode| is irrelevant but is set for test coverage.
   state_manager->CreateEntropyProviders(
       /*enable_limited_entropy_mode=*/true);
   EXPECT_TRUE(state_manager->client_id().empty());
