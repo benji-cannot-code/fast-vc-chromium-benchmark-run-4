5825249a4f026e2880f4544bf5c0b05dd21aa895
Tanmoy Mollik <triploblastic@chromium.org>
2024-02-23 09:17:26 +0000

    Reland "[Android] Add tests for SigninFirstRunFragment"
    This is a reland of commit 7d8de3abf823010ad01d85defa9c286c9b8a09b0
    
    The original change was reverted due to test failures in
    android-arm64-tests. I couldn't reproduce the failure. But after
    inspecting the logs I suspect that it's probably due to seeding
    issues. This cl enables SEED_ACCOUNTS_REVAMP for the failing test.
    
    Original change's description:
    > [Android] Add tests for SigninFirstRunFragment
    >
    > This cl adds tests in SigninFirstRunFragment to prevent the crash linked
    > in the bug.
    >
    > Bug: 1519150
    > Change-Id: I976764919243b373fdacb5159e9da9baf0006e9c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5237037
    > Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    > Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1262592}
    
    Bug: 326052687
    Change-Id: I768104794d473eb6eb00e3affed7798ff8abb113
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5310578
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264459}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninFirstRunFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninFirstRunFragmentTest.java
@@ -39,6 +39,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.widget.ProgressBar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.test.espresso.ViewAction;
 import androidx.test.filters.MediumTest;
@@ -64,6 +65,7 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.DoNotBatch;
+import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.HistogramWatcher;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.base.test.util.ScalableTimeout;
@@ -94,6 +96,7 @@ import org.chromium.chrome.test.util.browser.signin.SigninTestRule;
 import org.chromium.chrome.test.util.browser.signin.SigninTestUtil;
 import org.chromium.components.browser_ui.styles.SemanticColorUtils;
 import org.chromium.components.externalauth.ExternalAuthUtils;
+import org.chromium.components.signin.SigninFeatures;
 import org.chromium.components.signin.base.CoreAccountInfo;
 import org.chromium.components.signin.identitymanager.ConsentLevel;
 import org.chromium.components.signin.identitymanager.IdentityManager;
@@ -845,17 +848,7 @@ public class SigninFirstRunFragmentTest {
         clickContinueButton(continueAsText);
 
         verify(mFirstRunPageDelegateMock).acceptTermsOfService(true);
-        onView(withId(R.id.fre_signin_progress_spinner)).check(matches(isDisplayed()));
-        onView(withText(R.string.fre_signing_in)).check(matches(isDisplayed()));
-        onView(withText(R.string.fre_welcome)).check(matches(isDisplayed()));
-        onView(withText(R.string.signin_fre_subtitle)).check(matches(isDisplayed()));
-        onView(withText(TEST_EMAIL1)).check(matches(not(isDisplayed())));
-        onView(withText(FULL_NAME1)).check(matches(not(isDisplayed())));
-        onView(withId(R.id.signin_fre_selected_account_expand_icon))
-                .check(matches(not(isDisplayed())));
-        onView(withText(continueAsText)).check(matches(not(isDisplayed())));
-        onView(withText(R.string.signin_fre_dismiss_button)).check(matches(not(isDisplayed())));
-        onView(withId(R.id.signin_fre_footer)).check(matches(not(isDisplayed())));
+        checkFragmentWithSignInSpinner(TEST_EMAIL1, FULL_NAME1, continueAsText);
     }
 
     @Test
@@ -970,7 +963,8 @@ public class SigninFirstRunFragmentTest {
         clickContinueButton(continueAsText);
 
         verify(mFirstRunPageDelegateMock).acceptTermsOfService(false);
-        verify(mFirstRunPageDelegateMock, timeout(2000)).advanceToNextPage();
+        verify(mFirstRunPageDelegateMock, timeout(CriteriaHelper.DEFAULT_MAX_TIME_TO_POLL))
+                .advanceToNextPage();
     }
 
     @Test
@@ -1000,6 +994,36 @@ public class SigninFirstRunFragmentTest {
         checkFragmentWithSelectedAccount(TEST_EMAIL1, /* fullName= */ null, /* givenName= */ null);
         verify(mFirstRunPageDelegateMock)
                 .recordFreProgressHistogram(MobileFreProgress.WELCOME_ADD_ACCOUNT);
+    }
+
+    @Test
+    @MediumTest
+    @EnableFeatures(SigninFeatures.SEED_ACCOUNTS_REVAMP)
+    public void testFragmentSigninWhenAddedAccountIsNotYetAvailable() {
+        // This will freeze AccountManagerFacade with the currently available list of accounts.
+        // The added account from add account flow later on will not be available.
+        mSigninTestRule.blockGetCoreAccountInfosUpdate(/* populateCache= */ true);
+        mSigninTestRule.setResultForNextAddAccountFlow(Activity.RESULT_OK, TEST_EMAIL1);
+        launchActivityWithFragment();
+        onView(withText(R.string.signin_add_account_to_device)).perform(click());
+        checkFragmentWithSelectedAccount(TEST_EMAIL1, /* fullName= */ null, /* givenName= */ null);
+
+        final String continueAsText =
+                mActivityTestRule
+                        .getActivity()
+                        .getString(R.string.sync_promo_continue_as, TEST_EMAIL1);
+        clickContinueButton(continueAsText);
+
+        // The click on continue button should be a no-op.
+        verify(mFirstRunPageDelegateMock, never()).advanceToNextPage();
+        checkFragmentWithSelectedAccount(TEST_EMAIL1, /* fullName= */ null, /* givenName= */ null);
+
+        // Allow account list update and the continue button starts sign-in.
+        mSigninTestRule.unblockGetCoreAccountInfos();
+        clickContinueButton(continueAsText);
+        verify(mFirstRunPageDelegateMock, timeout(CriteriaHelper.DEFAULT_MAX_TIME_TO_POLL))
+                .advanceToNextPage();
+        checkFragmentWithSignInSpinner(TEST_EMAIL1, /* fullName= */ null, continueAsText);
     }
 
     @Test
@@ -1322,6 +1346,23 @@ public class SigninFirstRunFragmentTest {
         verify(mSigninManagerMock, never()).signin(any(CoreAccountInfo.class), anyInt(), any());
         verify(mSigninManagerMock, never())
                 .signinAndEnableSync(any(CoreAccountInfo.class), anyInt(), any());
+    }
+
+    private void checkFragmentWithSignInSpinner(
+            String email, @Nullable String fullName, String continueAsText) {
+        onView(withId(R.id.fre_signin_progress_spinner)).check(matches(isDisplayed()));
+        onView(withText(R.string.fre_signing_in)).check(matches(isDisplayed()));
+        onView(withText(R.string.fre_welcome)).check(matches(isDisplayed()));
+        onView(withText(R.string.signin_fre_subtitle)).check(matches(isDisplayed()));
+        onView(withText(email)).check(matches(not(isDisplayed())));
+        if (fullName != null) {
+            onView(withText(fullName)).check(matches(not(isDisplayed())));
+        }
+        onView(withId(R.id.signin_fre_selected_account_expand_icon))
+                .check(matches(not(isDisplayed())));
+        onView(withText(continueAsText)).check(matches(not(isDisplayed())));
+        onView(withText(R.string.signin_fre_dismiss_button)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.signin_fre_footer)).check(matches(not(isDisplayed())));
     }
 
     private void checkFragmentWhenSigninIsDisabledByPolicy() {
