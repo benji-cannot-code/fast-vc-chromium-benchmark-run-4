3195513c11753883881b58460e51d82cdc8cb3c7
Aliona DANGLA <alionadangla@google.com>
2024-02-23 17:39:19 +0000

    [iOS] Add tab group view menu
    Add 3 buttons to three dot menu in tab group view.
    See video:
    https://drive.google.com/file/d/10432U4mEBqb-gSdn5oHCjSylXF81NfSw/view?usp=drive_link
    
    Bug: 1501837
    Change-Id: Ia14ed89ad19930c435baada5786bfa07d349cc4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5307644
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264619}

--- ios/chrome/browser/ui/menu/menu_histograms.h
+++ ios/chrome/browser/ui/menu/menu_histograms.h
@@ -32,6 +32,7 @@ enum MenuScenarioHistogram {
   kMenuScenarioHistogramTabStripEntry = 18,
   kMenuScenarioHistogramInactiveTabsEntry = 19,
   kMenuScenarioHistogramTabGroupGridEntry = 20,
+  kMenuScenarioHistogramTabGroupViewEntry = 21,
   kMenuScenarioHistogramCount,
 };
 // LINT.ThenChange(/tools/metrics/histograms/metadata/mobile/enums.xml)
--- ios/chrome/browser/ui/menu/menu_histograms.mm
+++ ios/chrome/browser/ui/menu/menu_histograms.mm
@@ -50,6 +50,8 @@ const char kTabStripEntryActionsHistogram[] =
     "Mobile.ContextMenu.TabStrip.Actions";
 const char kInactiveTabsEntryActionsHistogram[] =
     "Mobile.ContextMenu.InactiveTabsEntry.Actions";
+const char kTabGroupEntryActionsHistogram[] =
+    "Mobile.ContextMenu.TabGroupEntry.Actions";
 }  // namespace
 
 void RecordMenuShown(MenuScenarioHistogram scenario) {
@@ -99,6 +101,8 @@ const char* GetActionsHistogramName(MenuScenarioHistogram scenario) {
       return kTabStripEntryActionsHistogram;
     case kMenuScenarioHistogramInactiveTabsEntry:
       return kInactiveTabsEntryActionsHistogram;
+    case kMenuScenarioHistogramTabGroupViewEntry:
+      return kTabGroupEntryActionsHistogram;
     case kMenuScenarioHistogramCount:
       NOTREACHED_NORETURN();
   }
