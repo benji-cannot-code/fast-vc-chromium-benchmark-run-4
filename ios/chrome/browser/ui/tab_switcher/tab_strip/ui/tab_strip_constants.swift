aed2d5a3d666388c2b7cf6d3e4b1c23c4ede92e5
Ewann Pelle <ewannpv@chromium.org>
2024-02-23 16:50:35 +0000

    [iOS][TS] Add dynamic new tab button
    Add an experiment that makes the new tab button position dynamic.
    
    Demo:
    https://drive.google.com/file/d/1qd7RrHpsabmQFKOCcfnoIJVWEL16Pq6x/
    
    Experiment flag:
    https://drive.google.com/file/d/1cl8KMDyAAOUd8C20-RQKrex4VGyx1vZ_/
    
    Bug: 41481434, 326244014
    Change-Id: I4e5461bbc3261c018796fed31787204745572302
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5314699
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264600}

--- ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_constants.swift
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_constants.swift
@@ -39,6 +39,7 @@ struct TabStripConstants {
     static let trailingInset: CGFloat = 10
     static let cornerRadius: CGFloat = 16
     static let symbolPointSize: CGFloat = 16
+    static let constraintUpdateAnimationDuration: CGFloat = 0.3
   }
 
   /// Animated separator constants.
---
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_features_utils.mm
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_features_utils.h"
+
+#import "base/feature_list.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
+#import "ios/chrome/browser/shared/public/features/features_utils.h"
+
+@implementation TabStripFeaturesUtils
+
++ (BOOL)isModernTabStripNewTabButtonDynamic {
+  CHECK(IsModernTabStripOrRaccoonEnabled());
+  std::string feature_param = base::GetFieldTrialParamValueByFeature(
+      kModernTabStrip, kModernTabStripParameterName);
+  return feature_param != kModernTabStripNTBStaticParam;
+}
+
+@end
