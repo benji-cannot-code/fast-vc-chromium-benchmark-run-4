bfa71679c864e80c70400cd49502e9a749373c99
Riley Tatum <rtatum@google.com>
2024-02-24 01:39:13 +0000

    Add metric for wallpaper search session setting theme
    This adds an enum metric that stores the type of theme set by wallpaper
    search, if it is still the theme when the customize chrome side panel
    is closed. Inspiration isn't saved in history so it can't take
    advantage of the result from SaveCurrentBackgroundToHistory() to know if
    it is still the current on session end. Therefore, if an inspiration
    image was set in the session, it will check if it is the set one and
    save it to the histogram.
    
    Bug: 324648372
    Change-Id: Ifa868ebee0439f3aaa7d1c0db049ccb3db16c35e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5318581
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264835}

--- chrome/browser/ui/webui/side_panel/customize_chrome/wallpaper_search/wallpaper_search_handler.cc
+++ chrome/browser/ui/webui/side_panel/customize_chrome/wallpaper_search/wallpaper_search_handler.cc
@@ -144,6 +144,25 @@ WallpaperSearchHandler::~WallpaperSearchHandler() {
             *history_entry_);
   }
 
+  bool is_result = false;
+  if (background_id) {
+    if (base::Contains(wallpaper_search_results_, *background_id)) {
+      base::UmaHistogramEnumeration(
+          "NewTabPage.WallpaperSearch.SessionSetTheme",
+          NtpWallpaperSearchThemeType::kResult);
+      is_result = true;
+    } else {
+      base::UmaHistogramEnumeration(
+          "NewTabPage.WallpaperSearch.SessionSetTheme",
+          NtpWallpaperSearchThemeType::kHistory);
+    }
+  } else if (inspiration_token_ &&
+             wallpaper_search_background_manager_->IsCurrentBackground(
+                 *inspiration_token_)) {
+    base::UmaHistogramEnumeration("NewTabPage.WallpaperSearch.SessionSetTheme",
+                                  NtpWallpaperSearchThemeType::kInspiration);
+  }
+
   if (!log_entries_.empty()) {
     auto& [log_entry, render_time] = log_entries_.back();
     auto* quality =
@@ -154,8 +173,7 @@ WallpaperSearchHandler::~WallpaperSearchHandler() {
       quality->set_complete_latency_ms(
           (base::Time::Now() - *render_time).InMilliseconds());
     }
-    if (background_id.has_value() &&
-        base::Contains(wallpaper_search_results_, *background_id)) {
+    if (is_result) {
       auto* image_quality =
           std::get<0>(wallpaper_search_results_[*background_id]);
       if (image_quality) {
@@ -747,6 +765,7 @@ void WallpaperSearchHandler::OnInspirationImageDecoded(
     const base::Token& id,
     base::ElapsedTimer timer,
     const gfx::Image& image) {
+  inspiration_token_ = id;
   wallpaper_search_background_manager_->SelectLocalBackgroundImage(
       id, image.AsBitmap(), /*is_inspiration_image=*/true, std::move(timer));
 }
