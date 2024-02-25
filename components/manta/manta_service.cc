78112b643e1ab70144bbe4c936b4288de7608740
Xinglong Luan <alanlxl@google.com>
2024-02-25 23:02:56 +0000

    Add mahi provider to manta
    Bug: b:318566801
    Test: autoninja -C out_hatch/Release chrome
    Change-Id: I9d92776463b5e69426e78d58a2062b6111f88196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5216225
    Commit-Queue: Xinglong Luan <alanlxl@chromium.org>
    Auto-Submit: Xinglong Luan <alanlxl@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264996}

--- components/manta/manta_service.cc
+++ components/manta/manta_service.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/scoped_refptr.h"
+#include "components/manta/mahi_provider.h"
 #include "components/manta/orca_provider.h"
 #include "components/manta/snapper_provider.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
@@ -37,6 +38,14 @@ std::unique_ptr<SnapperProvider> MantaService::CreateSnapperProvider() {
   }
   return std::make_unique<SnapperProvider>(shared_url_loader_factory_,
                                            identity_manager_);
+}
+
+std::unique_ptr<MahiProvider> MantaService::CreateMahiProvider() {
+  if (!identity_manager_) {
+    return nullptr;
+  }
+  return std::make_unique<MahiProvider>(shared_url_loader_factory_,
+                                        identity_manager_);
 }
 
 void MantaService::Shutdown() {
