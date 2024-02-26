d7e3e66a884890bf0f4f042c9286b0b712cac2a4
Daniil Sakhapov <sakhapov@chromium.org>
2024-02-26 21:01:09 +0000

    Update call to new Value() for effect stack test
    Bug: 41494232
    Change-Id: Icdd6e754d26854bbf663979c5b3315442c7f427f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5321249
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265430}

--- third_party/blink/renderer/core/animation/effect_stack_test.cc
+++ third_party/blink/renderer/core/animation/effect_stack_test.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/animation/keyframe_effect_model.h"
 #include "third_party/blink/renderer/core/animation/pending_animations.h"
 #include "third_party/blink/renderer/core/animation/string_keyframe.h"
+#include "third_party/blink/renderer/core/css/css_to_length_conversion_data.h"
 #include "third_party/blink/renderer/core/css/properties/longhands.h"
 #include "third_party/blink/renderer/core/execution_context/security_context.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
@@ -115,7 +116,7 @@ class AnimationEffectStackTest : public PageTestBase {
     // z-index is stored as a straight number value.
     EXPECT_TRUE(typed_value->GetInterpolableValue().IsNumber());
     return To<InterpolableNumber>(&typed_value->GetInterpolableValue())
-        ->Value();
+        ->Value(CSSToLengthConversionData());
   }
 
   Persistent<DocumentTimeline> timeline;
