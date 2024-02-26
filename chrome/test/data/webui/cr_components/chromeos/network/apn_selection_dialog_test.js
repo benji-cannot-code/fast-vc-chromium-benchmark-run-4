52f4a71d11b1fb9e3029a3c09cc2a97192988db3
Gordon Seto <gordonseto@google.com>
2024-02-26 19:08:17 +0000

    [CrOS Cellular] Move ApnSelectionDialogTest to cr_components directory
    Move apn_selection_dialog_test into correct cr_components directory. It
    was previously incorrectly in the os_settings directory. Also update
    init to be in setup(), not suiteSetup() as this was causing issues with
    test (to be added in follow-up CL).
    
    Bug: b:325487350
    Test: *ApnSelectionDialog*
    Change-Id: I3f8a7aa794f0189c4d5a9d7bd92867cc5820d9ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5319645
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Auto-Submit: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265341}

--- chrome/test/data/webui/cr_components/chromeos/network/apn_selection_dialog_test.js
+++ chrome/test/data/webui/cr_components/chromeos/network/apn_selection_dialog_test.js
@@ -3,16 +3,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'chrome://os-settings/os_settings.js';
+import 'chrome://os-settings/strings.m.js';
+import 'chrome://resources/ash/common/network/apn_selection_dialog.js';
 
-import {ApnSelectionDialog} from 'chrome://os-settings/os_settings.js';
+import {ApnSelectionDialog} from 'chrome://resources/ash/common/network/apn_selection_dialog.js';
 import {assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {waitAfterNextRender} from 'chrome://webui-test/polymer_test_util.js';
 
-suite('<apn-selection-dialog>', () => {
-  let apnSelectionDialog: ApnSelectionDialog;
+suite('ApnSelectionDialog', () => {
+  /** @type {ApnSelectionDialog} */
+  let apnSelectionDialog = null;
 
-  suiteSetup(() => {
+  setup(function() {
     apnSelectionDialog = document.createElement('apn-selection-dialog');
     document.body.appendChild(apnSelectionDialog);
     return waitAfterNextRender(apnSelectionDialog);
@@ -23,7 +25,7 @@ suite('<apn-selection-dialog>', () => {
   });
 
   test('Element contains dialog', async () => {
-    const dialog = apnSelectionDialog.shadowRoot!.querySelector('cr-dialog');
+    const dialog = apnSelectionDialog.shadowRoot.querySelector('cr-dialog');
     assertTrue(!!dialog);
     assertTrue(dialog.open);
   });
