9a7e0456a34d6854bb14cf71b4b616a38dbd629b
Michael Lippautz <mlippautz@chromium.org>
2024-02-26 09:36:42 +0000

    bindings: Rely on GC for DOMWrapperWorld
    Only clear data in `Dispose()`. Worlds are only reclaimed and removed
    from the world map using GC. This also means that
    `EnsureIsolatedWorld()` with a world id from a disposed world will
    yield in the same world now.
    
    Bug: 41493598
    Change-Id: I2def8daf6ded77924af36a109d814398b1d5be01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5318098
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265089}

--- third_party/blink/renderer/bindings/core/v8/dom_wrapper_world_test.cc
+++ third_party/blink/renderer/bindings/core/v8/dom_wrapper_world_test.cc
@@ -42,6 +42,14 @@ CollectInitialWorlds(v8::Isolate* isolate) {
   return {initial_worlds, used_isolated_world_id};
 }
 
+auto NumberOfWorlds(v8::Isolate* isolate) {
+  HeapVector<Member<DOMWrapperWorld>> worlds;
+  DOMWrapperWorld::AllWorldsInIsolate(isolate, worlds);
+  const auto num_worlds = worlds.size();
+  worlds.clear();
+  return num_worlds;
+}
+
 }  // namespace
 
 TEST(DOMWrapperWorldTest, MainWorld) {
@@ -70,15 +78,11 @@ TEST(DOMWrapperWorldTest, IsolatedWorlds) {
   EXPECT_TRUE(isolated_world2->IsIsolatedWorld());
   EXPECT_TRUE(DOMWrapperWorld::NonMainWorldsExistInMainThread());
 
-  HeapVector<Member<DOMWrapperWorld>> worlds;
-  DOMWrapperWorld::AllWorldsInIsolate(isolate, worlds);
-  EXPECT_EQ(worlds.size(), initial_worlds->size() + 2);
-  worlds.clear();
+  EXPECT_EQ(NumberOfWorlds(isolate), initial_worlds->size() + 2);
   // Remove temporary worlds via stackless GC.
   ThreadState::Current()->CollectAllGarbageForTesting(
       ThreadState::StackState::kNoHeapPointers);
-  DOMWrapperWorld::AllWorldsInIsolate(isolate, worlds);
-  EXPECT_EQ(worlds.size(), initial_worlds->size());
+  EXPECT_EQ(NumberOfWorlds(isolate), initial_worlds->size());
 }
 
 TEST(DOMWrapperWorldTest, ExplicitDispose) {
@@ -108,16 +112,16 @@ TEST(DOMWrapperWorldTest, ExplicitDispose) {
       worker_world_ids.insert(worker_world3->GetWorldId()).is_new_entry);
   EXPECT_TRUE(DOMWrapperWorld::NonMainWorldsExistInMainThread());
 
-  HeapVector<Member<DOMWrapperWorld>> worlds;
-  DOMWrapperWorld::AllWorldsInIsolate(isolate, worlds);
-  EXPECT_EQ(worlds.size(), initial_worlds->size() + 3);
-  worlds.clear();
-  // Explicitly disposing worlds should also remove them.
+  EXPECT_EQ(NumberOfWorlds(isolate), initial_worlds->size() + 3);
+  // Explicitly disposing worlds will clear internal state but not remove them.
   worker_world1->Dispose();
   worker_world2->Dispose();
   worker_world3->Dispose();
-  DOMWrapperWorld::AllWorldsInIsolate(isolate, worlds);
-  EXPECT_EQ(worlds.size(), initial_worlds->size());
+  EXPECT_EQ(NumberOfWorlds(isolate), initial_worlds->size() + 3);
+  // GC will remove the worlds.
+  ThreadState::Current()->CollectAllGarbageForTesting(
+      ThreadState::StackState::kNoHeapPointers);
+  EXPECT_EQ(NumberOfWorlds(isolate), initial_worlds->size());
 }
 
 namespace {
@@ -140,10 +144,7 @@ void WorkerThreadFunc(
       isolate, DOMWrapperWorld::WorldType::kWorkerOrWorklet);
   auto* worker_world2 = DOMWrapperWorld::Create(
       isolate, DOMWrapperWorld::WorldType::kWorkerOrWorklet);
-  HeapVector<Member<DOMWrapperWorld>> worlds;
-  DOMWrapperWorld::AllWorldsInIsolate(isolate, worlds);
-  EXPECT_EQ(worlds.size(), initial_worlds.size() + 2);
-  worlds.clear();
+  EXPECT_EQ(NumberOfWorlds(isolate), initial_worlds.size() + 2);
 
   // Dispose of remaining worlds.
   worker_world1->Dispose();
@@ -182,10 +183,7 @@ TEST(DOMWrapperWorldTest, NonMainThreadWorlds) {
   loop.Run();
 
   // Worlds on the worker thread should not be visible from the main thread.
-  HeapVector<Member<DOMWrapperWorld>> worlds;
-  DOMWrapperWorld::AllWorldsInIsolate(isolate, worlds);
-  EXPECT_EQ(worlds.size(), initial_worlds->size());
-  worlds.clear();
+  EXPECT_EQ(NumberOfWorlds(isolate), initial_worlds->size());
 }
 
 }  // namespace blink
