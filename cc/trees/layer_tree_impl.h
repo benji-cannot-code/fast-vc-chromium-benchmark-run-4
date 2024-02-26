151ea012922c640b7aabc549840b84f1e135dc45
Kalvin Lee <kdlee@chromium.org>
2024-02-26 06:15:33 +0000

    MiraclePtr: Mark performance-sensitive containers
    Both `cc::LayerImplList` and `cc::RenderSurfaceList` are visible in
    stack samples when we enable Renderer BRP (and when these types are
    vectors of `raw_ptr`s).
    
    This CL keeps them unrewritten (i.e. keeps them as vectors of raw
    pointers) but directly attaches `RAW_PTR_EXCLUSION` to the usage sites,
    as that's the only real way to make the rewriter ignore them.
    
    Bug: 41496832
    Change-Id: Icc09747dc564ea8a0b69e8c937f0f64cd36be3b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5316419
    Reviewed-by: Ali Hijazi <ahijazi@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Owners-Override: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265059}

--- cc/trees/layer_tree_impl.h
+++ cc/trees/layer_tree_impl.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_set.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/raw_ptr_exclusion.h"
 #include "base/time/time.h"
 #include "cc/base/synced_property.h"
 #include "cc/input/browser_controls_offset_manager.h"
@@ -900,7 +901,9 @@ class CC_EXPORT LayerTreeImpl {
   base::flat_set<viz::SurfaceRange> surface_layer_ranges_;
 
   // List of render surfaces for the most recently prepared frame.
-  RenderSurfaceList render_surface_list_;
+  //
+  // RAW_PTR_EXCLUSION: visible in stack samples when Renderer BRP is enabled.
+  RAW_PTR_EXCLUSION RenderSurfaceList render_surface_list_;
   // After drawing the |render_surface_list_| the areas in this region
   // would not be fully covered by opaque content.
   Region unoccluded_screen_space_region_;
--- components/viz/service/display/bsp_tree_perftest.cc
+++ components/viz/service/display/bsp_tree_perftest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/circular_deque.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
+#include "base/memory/raw_ptr_exclusion.h"
 #include "base/path_service.h"
 #include "base/strings/string_piece.h"
 #include "base/threading/thread.h"
@@ -130,7 +131,8 @@ class BspTreePerfTest : public cc::LayerTreeTest {
   base::LapTimer timer_;
   std::string story_;
   std::string json_;
-  cc::LayerImplList base_list_;
+  // RAW_PTR_EXCLUSION: visible in stack samples when Renderer BRP is enabled.
+  RAW_PTR_EXCLUSION cc::LayerImplList base_list_;
   int num_duplicates_ = 1;
 };
 
