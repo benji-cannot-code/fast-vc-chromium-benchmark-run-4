71757d0e6b3654142b0b99fe8c636f05a023a0d7
Jesse Doherty <jwd@google.com>
2024-02-26 18:19:15 +0000

    Populate log_written_by_app_version when partially purging UKM logs.
    Bug: 326113215
    Change-Id: Ieb98f0f0bb977e7dae3532bb1687f6d601d112c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5321881
    Reviewed-by: Sun Yueru <yrsun@chromium.org>
    Commit-Queue: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265301}

--- components/ukm/ukm_service.h
+++ components/ukm/ukm_service.h
@@ -158,6 +158,9 @@ class UkmService : public UkmRecorderImpl {
                            TestRegisterUkmProvidersWhenUKMFeatureEnabled);
   FRIEND_TEST_ALL_PREFIXES(UkmServiceTest,
                            PurgeExtensionDataFromUnsentLogStore);
+  FRIEND_TEST_ALL_PREFIXES(
+      UkmServiceTest,
+      PurgeExtensionDataFromUnsentLogStoreWithVersionChange);
   FRIEND_TEST_ALL_PREFIXES(UkmServiceTest, PurgeAppDataFromUnsentLogStore);
   FRIEND_TEST_ALL_PREFIXES(UkmServiceTest, PurgeMsbbDataFromUnsentLogStore);
   FRIEND_TEST_ALL_PREFIXES(UkmServiceTest, PurgeAppDataLogMetadataUpdate);
