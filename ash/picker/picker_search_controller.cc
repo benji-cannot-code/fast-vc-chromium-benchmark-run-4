775ae539a7c42e992f9b3e4b76e7792f2b608bba
mlcui <mlcui@google.com>
2024-02-26 03:55:24 +0000

    Return weights from emoji search
    Bug: b:326002205, b:309343774
    Change-Id: If5a31a177b8651baf04aaacfe49da4ff09de79c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5311674
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1265038}

--- ash/picker/picker_search_controller.cc
+++ ash/picker/picker_search_controller.cc
@@ -48,8 +48,8 @@ constexpr int kMaxEmojiResults = 3;
 constexpr int kMaxSymbolResults = 2;
 constexpr int kMaxEmoticonResults = 2;
 
-base::span<const std::string> FirstNOrLessElements(
-    base::span<const std::string> container,
+base::span<const emoji::EmojiSearchEntry> FirstNOrLessElements(
+    base::span<const emoji::EmojiSearchEntry> container,
     size_t n) {
   return container.subspan(0, std::min(container.size(), n));
 }
@@ -312,20 +312,20 @@ void PickerSearchController::HandleEmojiSearchResults(
   emoji_results_.reserve(kMaxEmojiResults + kMaxSymbolResults +
                          kMaxEmoticonResults);
 
-  for (const std::string& result :
+  for (const emoji::EmojiSearchEntry& result :
        FirstNOrLessElements(results.emojis, kMaxEmojiResults)) {
     emoji_results_.push_back(
-        PickerSearchResult::Emoji(base::UTF8ToUTF16(result)));
+        PickerSearchResult::Emoji(base::UTF8ToUTF16(result.emoji_string)));
   }
-  for (const std::string& result :
+  for (const emoji::EmojiSearchEntry& result :
        FirstNOrLessElements(results.symbols, kMaxSymbolResults)) {
     emoji_results_.push_back(
-        PickerSearchResult::Symbol(base::UTF8ToUTF16(result)));
+        PickerSearchResult::Symbol(base::UTF8ToUTF16(result.emoji_string)));
   }
-  for (const std::string& result :
+  for (const emoji::EmojiSearchEntry& result :
        FirstNOrLessElements(results.emoticons, kMaxEmoticonResults)) {
     emoji_results_.push_back(
-        PickerSearchResult::Emoticon(base::UTF8ToUTF16(result)));
+        PickerSearchResult::Emoticon(base::UTF8ToUTF16(result.emoji_string)));
   }
 }
 
