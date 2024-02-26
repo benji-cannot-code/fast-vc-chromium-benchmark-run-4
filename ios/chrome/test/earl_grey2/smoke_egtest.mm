b83ce66177da1f0d7bd1acab738f6f711da95e86
Federica Germinario <fedegermi@google.com>
2024-02-26 15:03:52 +0000

    [ios] Move tab pickup pref keys under the correct PrefService
    This CL moves the following keys:
     - kTabPickupLastDisplayedTime
     - kTabPickupLastDisplayedURL
    under the correct prefService. These keys are profile specific,
    so they should use the PrefService associated to the browserState.
    
    Fixed: 324411856, 324413039
    Change-Id: Iafe0c1fe956dfd828164d9b0dde491c519eaba71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5320893
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1265201}

--- ios/chrome/test/earl_grey2/smoke_egtest.mm
+++ ios/chrome/test/earl_grey2/smoke_egtest.mm
@@ -326,7 +326,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // The actual pref names and values below are irrelevant, but the calls
   // themselves should return data without crashing or asserting.
   [ChromeEarlGrey localStateIntegerPref:prefs::kBrowserStatesNumCreated];
-  [ChromeEarlGrey localStateStringPref:prefs::kTabPickupLastDisplayedURL];
+  [ChromeEarlGrey localStateBooleanPref:prefs::kAppStoreRatingPolicyEnabled];
 
   [ChromeEarlGrey userBooleanPref:prefs::kIosBookmarkPromoAlreadySeen];
   [ChromeEarlGrey userIntegerPref:prefs::kIosBookmarkCachedTopMostRow];
