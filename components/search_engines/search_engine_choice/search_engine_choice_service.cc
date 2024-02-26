e5b6b918852982aafc9203ee84e6d86fdea48505
Nicolas Dossou-Gbete <dgn@chromium.org>
2024-02-26 14:03:53 +0000

    🧇 Add SkipFor3p option for Choice screen triggers
    This CL adds a feature flag that controls whether the default
    search engine choice dialog should be shown when the default search
    engine is non-Google.
    
    When this feature flag is enabled, the default search engine choice
    dialog is only shown when the default search engine is Google.
    When the feature flag is disabled, we return to the pre-existing
    checks filtering out certain types of search engines (manual-custom,
    distribution-custom, etc).
    
    Bug: 326579197
    Change-Id: Ibb9c37623cdbbeb4c7790c1e2e63476a37272122
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5314418
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265178}

--- components/search_engines/search_engine_choice/search_engine_choice_service.cc
+++ components/search_engines/search_engine_choice/search_engine_choice_service.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/search_engines/eea_countries_ids.h"
 #include "components/search_engines/search_engine_choice_utils.h"
+#include "components/search_engines/search_engine_type.h"
 #include "components/search_engines/search_engines_pref_names.h"
 #include "components/search_engines/search_engines_switches.h"
 #include "components/search_engines/template_url_prepopulate_data.h"
@@ -248,6 +249,13 @@ SearchEngineChoiceService::GetDynamicChoiceScreenConditions(
     return SearchEngineChoiceScreenConditions::kControlledByPolicy;
   }
   CHECK(default_search_engine);
+
+  if (switches::kSearchEngineChoiceTriggerSkipFor3p.Get()) {
+    if (default_search_engine->GetEngineType(
+            template_url_service.search_terms_data()) != SEARCH_ENGINE_GOOGLE) {
+      return SearchEngineChoiceScreenConditions::kHasNonGoogleSearchEngine;
+    }
+  }
 
   if (!template_url_service.IsPrepopulatedOrDefaultProviderByPolicy(
           default_search_engine)) {
