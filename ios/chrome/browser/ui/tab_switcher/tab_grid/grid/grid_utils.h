df4a6518839a38bc94466ef7290f967cf7bc9641
Elmehdi Rahmaoui <erahmaoui@google.com>
2024-02-26 13:52:12 +0000

    [iOS] Update TabCollectionConsumer IV
    Clean header files, remove unnecessary imports and forward declarations.
    
    Bug: 1501837
    Change-Id: Ia75440c01bbc8cfda46e0344aac346baf019de1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5316724
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265175}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_utils.h
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_utils.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
-@class TabSwitcherItem;
 @class GridItemIdentifier;
 class WebStateList;
 
