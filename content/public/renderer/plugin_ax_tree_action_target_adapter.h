1154ab0ade6386f51b67d3c6ce153c4800309ee4
David Tseng <dtseng@google.com>
2024-02-26 23:01:22 +0000

    [pdf-a11y]: further simplify PluginAXTreeActionTargetAdapter
    Following comments from
    https://crrev.com/c/5302041
    - simplify the PluginAXTreeActionTargetAdapter interface by combining
      the two methods. GetFromId is not strictly needed as the only
      production callsite used it immediately before calling the other
      method, CreateActionTarget.
    - addresses one nit
    
    Bug: 326131114
    Test: cq
    Change-Id: I4bbf1cf5365c4857422bb46b0d6cf56ba28fbd57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5322047
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265511}

--- content/public/renderer/plugin_ax_tree_action_target_adapter.h
+++ content/public/renderer/plugin_ax_tree_action_target_adapter.h
@@ -6,15 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_PUBLIC_RENDERER_PLUGIN_AX_TREE_ACTION_TARGET_ADAPTER_H_
 #define CONTENT_PUBLIC_RENDERER_PLUGIN_AX_TREE_ACTION_TARGET_ADAPTER_H_
 
-#include "third_party/blink/public/platform/web_common.h"
 #include "ui/accessibility/ax_action_target.h"
-#include "ui/accessibility/ax_node_data.h"
-#include "ui/accessibility/ax_tree_data.h"
-#include "ui/accessibility/ax_tree_source.h"
-
-namespace ui {
-class AXNode;
-}
+#include "ui/accessibility/ax_node_id_forward.h"
 
 namespace content {
 
@@ -23,8 +16,7 @@ namespace content {
 class PluginAXTreeActionTargetAdapter {
  public:
   virtual std::unique_ptr<ui::AXActionTarget> CreateActionTarget(
-      const ui::AXNode& target_node) = 0;
-  virtual const ui::AXNode* GetFromId(ui::AXNodeID id) const = 0;
+      ui::AXNodeID id) = 0;
 };
 
 }  // namespace content
