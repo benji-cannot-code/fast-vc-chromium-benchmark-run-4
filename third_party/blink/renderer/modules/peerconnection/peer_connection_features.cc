2785ad8440de3b76ba8b92689916a00858be605d
Tony Herre <toprice@chromium.org>
2024-02-26 11:08:52 +0000

    Enable WebRtcEncodedTransformsPerStreamCreation by default
    Bug: 40943169
    Change-Id: I366bcb961440b4c771d4b6a39b880a0b37d4b9ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5323500
    Auto-Submit: Tony Herre <toprice@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265120}

--- third_party/blink/renderer/modules/peerconnection/peer_connection_features.cc
+++ third_party/blink/renderer/modules/peerconnection/peer_connection_features.cc
@@ -13,6 +13,6 @@ namespace blink {
 // PeerConnections created without the encodedInsertableStreams parameter.
 BASE_FEATURE(kWebRtcEncodedTransformsPerStreamCreation,
              "WebRtcEncodedTransformsPerStreamCreation",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace blink
