25f924752a36503d7ee0e9317929b341def0b03a
Guillem Perez <guiperez@google.com>
2024-02-26 22:49:23 +0000

    [iOS][CodeHealth] Update Content notif alert presenter in setuplists.
    This CL updates the notification alert presenter and confirmation
    presenter to the new OptInAlertCoordinator for consistency with other
    content notification / notification features.
    
    Fixed: b/321787924
    Change-Id: I2029dd758bd02a8866cb52a07712e9e309a13f45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5324849
    Auto-Submit: Guillem Perez <guiperez@google.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1265502}

--- ios/chrome/browser/ui/content_suggestions/set_up_list/set_up_list_content_notification_promo_coordinator.h
+++ ios/chrome/browser/ui/content_suggestions/set_up_list/set_up_list_content_notification_promo_coordinator.h
@@ -48,7 +48,8 @@ enum class ContentNotificationSetUpListPromoEvent {
 enum class ContentNotificationPromptAction {
   kGoToSettingsTapped = 0,
   kNoThanksTapped = 1,
-  kMaxValue = kNoThanksTapped,
+  kAccepted = 2,
+  kMaxValue = kAccepted,
 };
 // LINT.ThenChange(/tools/metrics/histograms/metadata/content/enums.xml)
 
