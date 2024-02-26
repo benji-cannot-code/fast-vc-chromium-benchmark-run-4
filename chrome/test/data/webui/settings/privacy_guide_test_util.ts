17ccf1dd4e1445cd46c921b4124190cac9ee9167
Nidhi Jaju <nidhijaju@chromium.org>
2024-02-26 01:28:52 +0000

    Revert "Reland "Privacy Guide: clean up search suggestions and preload cards""
    This reverts commit 45c5759407501e700405434d464d645850cdfc93.
    
    Reason for revert: Causing consistent failures on "chrome/ci/linux-chromeos-chrome" and "chromium/ci/Win10 Tests x64"
    i.e. https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/39589/test-results?sortby=&groupby=
    
    Original change's description:
    > Reland "Privacy Guide: clean up search suggestions and preload cards"
    >
    > This is a reland of commit 01170d74d06655cd7244026dc0689542702c9bc2:
    >
    > Now the integration test no longer contains the incorrect additional
    > check for a non-optional step before the completion card.
    >
    > Original change's description:
    > > Privacy Guide: clean up search suggestions and preload cards
    > >
    > > The product decision has been made that the "improve search
    > > suggestions" and "preload" cards in privacy guide won't launch. This
    > > CL removes those cards from privacy guide.
    > >
    > > Bug: 40184479
    > > Change-Id: I747c2c693ca624d769290c2351d02d4c0913bdab
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5279392
    > > Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    > > Auto-Submit: Rainhard Findling <rainhard@chromium.org>
    > > Commit-Queue: John Lee <johntlee@chromium.org>
    > > Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    > > Reviewed-by: John Lee <johntlee@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1264176}
    >
    > Bug: 40184479
    > Change-Id: Ib433019183c11d4dabaab0abafe7f736c61708dd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5320889
    > Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    > Reviewed-by: John Lee <johntlee@chromium.org>
    > Auto-Submit: Rainhard Findling <rainhard@chromium.org>
    > Commit-Queue: John Lee <johntlee@chromium.org>
    > Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1264788}
    
    Bug: 40184479
    Change-Id: I4e824d7f1a33f15fdbf006fb872d5e979adb63e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5320308
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Owners-Override: Nidhi Jaju <nidhijaju@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265009}

--- chrome/test/data/webui/settings/privacy_guide_test_util.ts
+++ chrome/test/data/webui/settings/privacy_guide_test_util.ts
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {webUIListenerCallback} from 'chrome://resources/js/cr.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import type {SettingsPrivacyGuidePageElement} from 'chrome://settings/lazy_load.js';
-import {CookiePrimarySetting, PrivacyGuideStep, SafeBrowsingSetting} from 'chrome://settings/lazy_load.js';
+import {CookiePrimarySetting, PrivacyGuideStep, SafeBrowsingSetting, NetworkPredictionOptions} from 'chrome://settings/lazy_load.js';
 import type {SettingsPrefsElement} from 'chrome://settings/settings.js';
 import {Router, routes, StatusAction} from 'chrome://settings/settings.js';
 import {assertEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
@@ -109,6 +109,23 @@ export function shouldShowHistorySyncCard(
     syncBrowserProxy: TestSyncBrowserProxy): boolean {
   return !syncBrowserProxy.testSyncStatus ||
       !!syncBrowserProxy.testSyncStatus.signedIn;
+}
+
+// Set the preload setting for the privacy guide.
+export function setPreloadSetting(
+    page: SettingsPrivacyGuidePageElement,
+    setting: NetworkPredictionOptions): void {
+  page.set('prefs.net.network_prediction_options', {
+    type: chrome.settingsPrivate.PrefType.NUMBER,
+    value: setting,
+  });
+}
+
+export function shouldShowPreloadCard(page: SettingsPrivacyGuidePageElement):
+    boolean {
+  const setting = page.getPref('net.network_prediction_options').value;
+  return setting === NetworkPredictionOptions.DISABLED ||
+      setting === NetworkPredictionOptions.STANDARD;
 }
 
 // Bundles functionality to create the page object for tests.
