6a41ae5a5c58bc99884aa9804d18d02a475cbb18
Mark Rowe <markrowe@chromium.org>
2024-02-26 22:33:33 +0000

    Only consider dynamic validity when validating parent process
    By default `SecCodeCheckValidity` on a running process validates both
    that the process's code signature is valid and that it matches the
    signature of the app bundle on disk. This logic fails for apps such as
    Chrome that can be updated on disk while they're running.
    
    `ProcessIsSignedAndFulfillsRequirement` is updated to take an optional
    flag to request that only the dynamic validity of the process be
    checked. This is used within `web_app_shortcut_copier` since it is
    validating that its parent process is Chrome. For the other callers it
    is still correct to check both dynamic and static validity.
    
    Validating only the dynamic code signature requires an awkward dance of
    retrieving a `SecCodeRef` in order to retrieve the `Info.plist` from the
    running process's code signature. This is then passed to
    `SecCodeCopyGuestWithAttributes` via the
    `kSecGuestAttributeDynamicCodeInfoPlist` /
    `kSecGuestAttributeDynamicCode` attributes in order to retrieve a
    `SecCodeRef` that will validate only the dynamic signature.
    
    Bug: 324978880
    Change-Id: I94adb80a56139779b2dc6326c39134386631890c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5293322
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Auto-Submit: Mark Rowe <markrowe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265496}

--- chrome/browser/apps/app_shim/code_signature_mac.h
+++ chrome/browser/apps/app_shim/code_signature_mac.h
@@ -13,10 +13,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace apps {
 
+enum class SignatureValidationType {
+  // Verify that the running application has a valid code signature and
+  // that it is unchanged from the copy on disk.
+  DynamicAndStatic,
+
+  // Verify that the running application has a valid code signature.
+  // Do not verify that the application matches the copy on disk.
+  DynamicOnly,
+};
+
 // Returns whether `process` has a valid code signature that fulfills
 // `requirement`.
-OSStatus ProcessIsSignedAndFulfillsRequirement(pid_t process,
-                                               SecRequirementRef requirement);
+OSStatus ProcessIsSignedAndFulfillsRequirement(
+    pid_t process,
+    SecRequirementRef requirement,
+    SignatureValidationType validation_type =
+        SignatureValidationType::DynamicAndStatic);
 
 // Reason why no requirement could be returned.
 enum class MissingRequirementReason {
