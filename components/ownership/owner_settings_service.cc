3c8082a3f941b982096a4c194825198fb8ffcc4a
Michael Ershov <miersh@google.com>
2024-02-26 13:10:12 +0000

    Clean up ChromeSideOwnerKeyGeneration experiment
    ChromeSideOwnerKeyGeneration has been enabled by default since
    September 2023. Delete the feature flag and the now unused code
    branches. This CL is not expected to change any behavior.
    
    Bug: b:259362896
    Test: *session*, *login* tast tests
    Change-Id: Iad37fe4626032cde2bf873cc1335642c09e20779
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5318107
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1265165}

--- components/ownership/owner_settings_service.cc
+++ components/ownership/owner_settings_service.cc
@@ -59,10 +59,8 @@ std::unique_ptr<em::PolicyFetchResponse> AssembleAndSignPolicy(
   std::unique_ptr<em::PolicyFetchResponse> policy_response(
       new em::PolicyFetchResponse());
 
-  if (base::FeatureList::IsEnabled(ownership::kChromeSideOwnerKeyGeneration)) {
-    policy_response->set_new_public_key(public_key->data().data(),
-                                        public_key->data().size());
-  }
+  policy_response->set_new_public_key(public_key->data().data(),
+                                      public_key->data().size());
 
   if (!policy->SerializeToString(policy_response->mutable_policy_data())) {
     LOG(ERROR) << "Failed to encode policy payload.";
@@ -95,10 +93,6 @@ std::unique_ptr<em::PolicyFetchResponse> AssembleAndSignPolicy(
 }
 
 }  // namespace
-
-BASE_FEATURE(kChromeSideOwnerKeyGeneration,
-             "ChromeSideOwnerKeyGeneration",
-             base::FeatureState::FEATURE_ENABLED_BY_DEFAULT);
 
 OwnerSettingsService::OwnerSettingsService(
     const scoped_refptr<ownership::OwnerKeyUtil>& owner_key_util)
