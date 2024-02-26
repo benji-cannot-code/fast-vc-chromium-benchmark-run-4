61d293bc163654bbdb166e027dac0aae9d201b9f
Tommy C. Li <tommycli@chromium.org>
2024-02-26 19:14:44 +0000

    History Embeddings: Create SQL database file, meta table, and versioning
    Also migrates the sequence management to use base::SequenceBound.
    
    Bug: b/325524013
    Change-Id: I6830e7f6dfc4ec11de43995dc2f94c64181cbf09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5321865
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265345}

--- components/history_embeddings/sql_database.h
+++ components/history_embeddings/sql_database.h
@@ -6,9 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_HISTORY_EMBEDDINGS_SQL_DATABASE_H_
 #define COMPONENTS_HISTORY_EMBEDDINGS_SQL_DATABASE_H_
 
+#include <optional>
 #include "base/files/file_path.h"
+#include "base/sequence_checker.h"
+#include "base/thread_annotations.h"
+#include "sql/database.h"
+#include "sql/init_status.h"
 
 namespace history_embeddings {
+
+inline constexpr base::FilePath::CharType kHistoryEmbeddingsName[] =
+    FILE_PATH_LITERAL("HistoryEmbeddings");
 
 // Wraps the Sqlite database that provides on-disk storage for History
 // Embeddings component. This class is expected to live and die on a backend
@@ -20,6 +28,21 @@ class SqlDatabase {
   SqlDatabase(const SqlDatabase&) = delete;
   SqlDatabase& operator=(const SqlDatabase&) = delete;
   ~SqlDatabase();
+
+ private:
+  // Initializes the database, if it's not already initialized. Returns true if
+  // the initialization was successful (or already succeeded in the past).
+  bool LazyInit(const base::FilePath& storage_dir);
+  // Helper function for LazyInit(). Should only be called by LazyInit().
+  sql::InitStatus InitInternal(const base::FilePath& storage_dir);
+
+  // The underlying SQL database.
+  sql::Database db_ GUARDED_BY_CONTEXT(sequence_checker_);
+  // The initialization status of the database. It's not set if never attempted.
+  std::optional<sql::InitStatus> db_init_status_ = std::nullopt;
+
+  // Verifies that all operations happen on the same sequence.
+  SEQUENCE_CHECKER(sequence_checker_);
 };
 
 }  // namespace history_embeddings
