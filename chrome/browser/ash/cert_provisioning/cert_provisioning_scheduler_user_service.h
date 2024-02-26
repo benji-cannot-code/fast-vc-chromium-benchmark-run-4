f569700ee087b69ecf182edbd55cbdbd6810336c
Artem Sumaneev <asumaneev@google.com>
2024-02-26 17:48:16 +0000

    [Cert Provisioning] Destroy cert scheduler on service shutdown
    Previously, cert provisioning keyed service did not have  shutdown and
    was destroying itself with destructor and not Shutdown. The cert service
    depends on invalidation service which is destroyed on Shutdown and
    expects its dependants to do the same.
    
    For example, invalidation service expects InvalidationHandlers
    to unregister before the service is shutdown, thus handlers must
    unregister on Shutdown. Before this change, cert invalidators were
    unregistering on destruction, which happens after shutdown and were
    breaking order and causing crashes.
    
    Now, the whole cert provisioning stack is destroyed on service shutdown
    before service destruction.
    
    CertProvisioningAsh uses the service and outlives it, as it was before,
    without any breaks.
    
    Bug: chromium:40070281
    Test: manual + existing unit tests
    Change-Id: I6a5030e17104d4bffb5f6c09a3fd67083049687e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5324305
    Commit-Queue: Artem Sumaneev <asumaneev@google.com>
    Reviewed-by: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1265283}

--- chrome/browser/ash/cert_provisioning/cert_provisioning_scheduler_user_service.h
+++ chrome/browser/ash/cert_provisioning/cert_provisioning_scheduler_user_service.h
@@ -20,6 +20,8 @@ class CertProvisioningSchedulerUserService : public KeyedService {
   explicit CertProvisioningSchedulerUserService(Profile* profile);
   ~CertProvisioningSchedulerUserService() override;
 
+  void Shutdown() override;
+
   CertProvisioningScheduler* scheduler() { return scheduler_.get(); }
 
  private:
