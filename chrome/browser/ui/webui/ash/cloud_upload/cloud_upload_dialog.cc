c482d3c196c1226d414371c8d75e486325f8d547
cassycc <cassycc@google.com>
2024-02-26 06:56:25 +0000

    [Office upload] All uploads must be successful to record latency
    Previously, in a multi-file upload, only the last upload had to be
    successful to record upload latency. However, latency should only be
    recorded when all uploads were successful. Set the has_upload_errors_
    properly to ensure this.
    
    Bug: b/325989334
    Change-Id: I82d68c3fb288f4d220390a8a6a4877fffc24a100
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5323661
    Reviewed-by: Alexander Bolodurin <alexbn@chromium.org>
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1265069}

--- chrome/browser/ui/webui/ash/cloud_upload/cloud_upload_dialog.cc
+++ chrome/browser/ui/webui/ash/cloud_upload/cloud_upload_dialog.cc
@@ -788,7 +788,8 @@ void CloudOpenTask::FinishedDriveUpload(OfficeTaskResult task_result,
     OpenUploadedDriveUrl(url.value(), task_result);
   } else {
     cloud_open_metrics_->LogTaskResult(task_result);
-    has_upload_errors_ = task_result == OfficeTaskResult::kFailedToUpload;
+    has_upload_errors_ = has_upload_errors_ ||
+                         (task_result == OfficeTaskResult::kFailedToUpload);
   }
   file_urls_idx_++;
   if (file_urls_idx_ < file_urls_.size()) {
@@ -820,7 +821,8 @@ void CloudOpenTask::FinishedOneDriveUpload(
                                task_result));
   } else {
     cloud_open_metrics_->LogTaskResult(task_result);
-    has_upload_errors_ = task_result == OfficeTaskResult::kFailedToUpload;
+    has_upload_errors_ = has_upload_errors_ ||
+                         (task_result == OfficeTaskResult::kFailedToUpload);
   }
   file_urls_idx_++;
   if (file_urls_idx_ < file_urls_.size()) {
