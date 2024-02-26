ea16847fe7bdcd61f4780901178ce2a3d24df950
Ben Pastene <bpastene@chromium.org>
2024-02-26 21:40:54 +0000

    utr: Add basic recipes.py invocation for running the UTR recipe
    This hooks up the UTR script to an actual `recipes.py run` invocation.
    For now, it invokes recipes.py a single time, and plumbs the recipe
    stdout to the UTR's stdout. This means the user will get spammed
    with a bunch of recipe junk output. Cleaning and filtering that will
    be in a follow-up.
    
    Bug: 41492688
    Change-Id: I41d016d881f986ceff8274395a2cba3cb0e24481
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5322459
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Reviewed-by: Struan Shrimpton <sshrimp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1265462}

---
+++ tools/utr/recipe_test.py
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python3
+# Copyright 2024 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Tests for recipe.py"""
+
+import os
+import pathlib
+import shutil
+import tempfile
+import unittest
+from unittest import mock
+
+import recipe
+
+
+class LegacyRunnerTests(unittest.TestCase):
+
+  def setUp(self):
+    self.tmp_dir = pathlib.Path(tempfile.mkdtemp())
+    self.tmp_dir.joinpath('recipes').touch()
+    self.subp_mock = mock.Mock()
+
+  def tearDown(self):
+    shutil.rmtree(self.tmp_dir)
+
+  def testProps(self):
+    runner = recipe.LegacyRunner(self.tmp_dir, {}, 'some-bucket',
+                                 'some-builder', [], False, False)
+    self.assertEqual(
+        runner._input_props['$recipe_engine/buildbucket']['build']['builder']
+        ['builder'], 'some-builder')
+
+  def testRun(self):
+    runner = recipe.LegacyRunner(self.tmp_dir, {}, 'some-bucket',
+                                 'some-builder', [], False, False)
+    self.subp_mock.returncode = 123
+    with mock.patch('subprocess.Popen', return_value=self.subp_mock):
+      self.assertEqual(runner.run_recipe(), 123)
+
+
+if __name__ == '__main__':
+  unittest.main()
