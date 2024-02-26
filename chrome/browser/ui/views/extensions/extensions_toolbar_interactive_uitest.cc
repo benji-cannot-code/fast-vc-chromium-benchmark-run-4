5c8cf2268411c6b5cb4b3680a92d5a507d0a48cc
EmiliaPaz <emiliapaz@chromium.org>
2024-02-26 21:07:46 +0000

    [Extensions] Use provided name when installing enterprise extension
    Use the provided name parameter when installing an enterprise extension.
    Additionally, set two fields that are important for the install.
    
    This doesn't change any functionality.
    
    Bug: n/a
    Change-Id: Id0962bccc4eaba95481ed014533c8ec145b5b241
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5320526
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265434}

--- chrome/browser/ui/views/extensions/extensions_toolbar_interactive_uitest.cc
+++ chrome/browser/ui/views/extensions/extensions_toolbar_interactive_uitest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "components/crx_file/id_util.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/test_navigation_observer.h"
 #include "extensions/browser/extension_system.h"
@@ -56,8 +57,10 @@ ExtensionsToolbarUITest::LoadTestExtension(const std::string& path,
 scoped_refptr<const extensions::Extension>
 ExtensionsToolbarUITest::ForceInstallExtension(const std::string& name) {
   scoped_refptr<const extensions::Extension> extension =
-      extensions::ExtensionBuilder("extension")
+      extensions::ExtensionBuilder(name)
+          .SetManifestVersion(3)
           .SetLocation(extensions::mojom::ManifestLocation::kExternalPolicy)
+          .SetID(crx_file::id_util::GenerateId(name))
           .Build();
   extensions::ExtensionSystem::Get(browser()->profile())
       ->extension_service()
