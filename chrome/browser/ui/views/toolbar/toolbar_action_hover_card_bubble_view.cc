1745f1a439c91d69eb30194750dc8470fcfc8bff
EmiliaPaz <emiliapaz@chromium.org>
2024-02-27 22:34:32 +0000

    [Extensions c2s] Update action hover card to GM3
    Update the tab hover card style to the new chrome refresh style.
    Increase the label max lines because important content not shown.
    
    Screenshot:
    https://drive.google.com/file/d/13Wk8cN6uEPYnuD8idkULTTc93_JRbuif/view?usp=sharing
    
    Screencast:
    https://drive.google.com/file/d/1e7lSWSZWNlBB_3aXck3Fo9TipGmNV_dv/view?usp=sharing
    
    Bug: 40857356
    Change-Id: Ic5cf9e09847957978c539d40ba254bb2ce85f982
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5328810
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266122}

--- chrome/browser/ui/views/toolbar/toolbar_action_hover_card_bubble_view.cc
+++ chrome/browser/ui/views/toolbar/toolbar_action_hover_card_bubble_view.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/memory/raw_ptr.h"
 #include "base/notreached.h"
+#include "chrome/browser/ui/color/chrome_color_id.h"
 #include "chrome/browser/ui/toolbar/toolbar_action_view_controller.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/chrome_typography.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/insets.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/layout/flex_layout.h"
+#include "ui/views/style/typography.h"
 
 namespace {
 
@@ -34,11 +36,11 @@ constexpr int kHoverCardWidth = 240;
 
 // Hover card margins.
 // TODO(crbug.com/1351778): Move to a base hover card class.
-constexpr int kHorizontalMargin = 18;
-constexpr int kVerticalMargin = 10;
+constexpr int kHorizontalMargin = 12;
+constexpr int kVerticalMargin = 12;
 
 // Maximum number of lines that a label occupies.
-constexpr int kHoverCardLavelMaxLines = 2;
+constexpr int kHoverCardLabelMaxLines = 3;
 
 std::u16string GetSiteAccessTitle(
     ToolbarActionViewController::HoverCardState::SiteAccess state) {
@@ -151,9 +153,14 @@ ToolbarActionHoverCardBubbleView::ToolbarActionHoverCardBubbleView(
   layout->SetCollapseMargins(true);
 
   // Set up content.
-  auto create_label = [](int context, gfx::Insets insets) {
-    auto label =
-        std::make_unique<FadeLabelView>(kHoverCardLavelMaxLines, context);
+  auto create_label = [](int context, int text_style,
+                         std::optional<ui::ColorId> color_id,
+                         gfx::Insets insets) {
+    auto label = std::make_unique<FadeLabelView>(kHoverCardLabelMaxLines,
+                                                 context, text_style);
+    if (color_id) {
+      label->SetEnabledColorId(color_id.value());
+    }
     label->SetProperty(views::kMarginsKey, insets);
     label->SetProperty(
         views::kFlexBehaviorKey,
@@ -169,24 +176,28 @@ ToolbarActionHoverCardBubbleView::ToolbarActionHoverCardBubbleView(
     return separator;
   };
 
-  title_label_ = AddChildView(
-      create_label(CONTEXT_TAB_HOVER_CARD_TITLE,
-                   gfx::Insets::VH(kVerticalMargin, kHorizontalMargin)));
+  title_label_ = AddChildView(create_label(
+      CONTEXT_TAB_HOVER_CARD_TITLE, views::style::STYLE_BODY_3_EMPHASIS,
+      /*color_id=*/std::nullopt,
+      gfx::Insets::VH(kVerticalMargin, kHorizontalMargin)));
 
   site_access_separator_ = AddChildView(create_separator());
-  site_access_title_label_ = AddChildView(
-      create_label(CONTEXT_TAB_HOVER_CARD_TITLE,
-                   gfx::Insets::TLBR(kVerticalMargin, kHorizontalMargin, 0,
-                                     kHorizontalMargin)));
-  site_access_description_label_ = AddChildView(
-      create_label(views::style::CONTEXT_DIALOG_BODY_TEXT,
-                   gfx::Insets::TLBR(0, kHorizontalMargin, kVerticalMargin,
-                                     kHorizontalMargin)));
+  site_access_title_label_ = AddChildView(create_label(
+      CONTEXT_TAB_HOVER_CARD_TITLE, views::style::STYLE_BODY_3_EMPHASIS,
+      /*color_id=*/std::nullopt,
+      gfx::Insets::TLBR(kVerticalMargin, kHorizontalMargin, 0,
+                        kHorizontalMargin)));
+  site_access_description_label_ = AddChildView(create_label(
+      views::style::CONTEXT_DIALOG_BODY_TEXT, views::style::STYLE_BODY_4,
+      /*color_id=*/kColorTabHoverCardSecondaryText,
+      gfx::Insets::TLBR(0, kHorizontalMargin, kVerticalMargin,
+                        kHorizontalMargin)));
 
   policy_separator_ = AddChildView(create_separator());
-  policy_label_ = AddChildView(
-      create_label(views::style::CONTEXT_DIALOG_BODY_TEXT,
-                   gfx::Insets::VH(kVerticalMargin, kHorizontalMargin)));
+  policy_label_ = AddChildView(create_label(
+      views::style::CONTEXT_DIALOG_BODY_TEXT, views::style::STYLE_BODY_4,
+      /*color_id=*/kColorTabHoverCardSecondaryText,
+      gfx::Insets::VH(kVerticalMargin, kHorizontalMargin)));
 
   // Set up widget.
   views::BubbleDialogDelegateView::CreateBubble(this);
