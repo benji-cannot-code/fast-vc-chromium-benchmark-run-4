9581898ff827f11dfb3f2efc1947cdd41a1e2ebb
Oksana Zhuravlova <oksamyt@chromium.org>
2024-02-27 20:56:42 +0000

    [CodeHealth] std::unique_ptr<T[]> -> HeapArray in input_method_win_base.cc
    Bug: 326459261, 40285824
    Change-Id: I96b6b4a79ee4e2ab77daa739d79c99e58cbc60bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5329007
    Reviewed-by: Yohei Yukawa <yukawa@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266047}

--- ui/base/ime/win/input_method_win_base.cc
+++ ui/base/ime/win/input_method_win_base.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/auto_reset.h"
 #include "base/command_line.h"
+#include "base/containers/heap_array.h"
 #include "base/functional/bind.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/string_util.h"
@@ -84,8 +85,8 @@ bool IsRTLKeyboardLayoutInstalled() {
 
   // Retrieve the keyboard layouts in an array and check if there is an RTL
   // layout in it.
-  std::unique_ptr<HKL[]> layouts(new HKL[size]);
-  ::GetKeyboardLayoutList(size, layouts.get());
+  auto layouts = base::HeapArray<HKL>::Uninit(size);
+  ::GetKeyboardLayoutList(size, layouts.data());
   for (int i = 0; i < size; ++i) {
     if (IsRTLPrimaryLangID(
             PRIMARYLANGID(reinterpret_cast<uintptr_t>(layouts[i])))) {
