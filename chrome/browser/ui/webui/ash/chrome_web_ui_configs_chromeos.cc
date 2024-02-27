c5943a4c9c63468ff5de67040002f8ba3d0f8c88
Addison Luh <aluh@chromium.org>
2024-02-27 08:01:29 +0000

    [extended-updates] Add WebUI controller and config for Extended Updates
    Add and hook up initial WebUI handlers for Extended Updates opt-in
    dialog page. The page is reachable via chrome://extended-updates-dialog
    
    Also add a missing source inclusion for the generated pak resource file.
    
    Bug: b:322418004
    Change-Id: Ie00ac0af722e155f77a140f711850ec55af7a390
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5318638
    Commit-Queue: Addison Luh <aluh@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265672}

--- chrome/browser/ui/webui/ash/chrome_web_ui_configs_chromeos.cc
+++ chrome/browser/ui/webui/ash/chrome_web_ui_configs_chromeos.cc
@@ -72,6 +72,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/drive_internals_ui.h"
 #include "chrome/browser/ui/webui/ash/emoji/emoji_ui.h"
 #include "chrome/browser/ui/webui/ash/enterprise_reporting/enterprise_reporting_ui.h"
+#include "chrome/browser/ui/webui/ash/extended_updates/extended_updates_ui.h"
 #include "chrome/browser/ui/webui/ash/healthd_internals/healthd_internals_ui.h"
 #include "chrome/browser/ui/webui/ash/human_presence_internals_ui.h"
 #include "chrome/browser/ui/webui/ash/in_session_password_change/password_change_ui.h"
@@ -231,6 +232,8 @@ void RegisterAshChromeWebUIConfigs() {
   map.AddWebUIConfig(MakeEcheAppUIConfig());
   map.AddWebUIConfig(std::make_unique<SensorInfoUIConfig>());
   map.AddWebUIConfig(std::make_unique<EmojiUIConfig>());
+  map.AddWebUIConfig(
+      std::make_unique<extended_updates::ExtendedUpdatesUIConfig>());
   map.AddWebUIConfig(
       MakeComponentConfigWithDelegate<FilesInternalsUIConfig, FilesInternalsUI,
                                       ChromeFilesInternalsUIDelegate>());
