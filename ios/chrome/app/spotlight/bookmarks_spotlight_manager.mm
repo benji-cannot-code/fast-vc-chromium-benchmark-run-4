a7751ff7e36896aeb0e4f039f4f1aa1ca30f217e
Stepan Khapugin <stkhapugin@chromium.org>
2024-02-27 15:34:31 +0000

    [iOS][Spotlight] Fix errors in bookmarks manager.
    Fixes a couple of errors that may happen due to the background pausing
    added in a recent CL:
    1. Actually pauses the long initial indexing
    2. Resets the needsClearAndReindex flag at reindexing
    3. Drops the queue and scheduled next batch operation before reindexing
    
    Bug: 326494272
    Change-Id: Id5f13168135071eb11262c0d93690088eb3d3ce9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5323516
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265806}

--- ios/chrome/app/spotlight/bookmarks_spotlight_manager.mm
+++ ios/chrome/app/spotlight/bookmarks_spotlight_manager.mm
@@ -345,6 +345,11 @@ class SpotlightBookmarkModelBridge;
     return;
   }
 
+  if (self.isAppInBackground) {
+    // The next batch will auto resume on foreground.
+    return;
+  }
+
   for (int i = 0; i < kBatchSize; i++) {
     if (_indexingStack.empty() || _nodesIndexed > kMaxInitialIndexSize) {
       self.modelUpdatesShouldCauseFullReindex = NO;
@@ -393,7 +398,12 @@ class SpotlightBookmarkModelBridge;
 
 - (void)appWillEnterForeground {
   [super appWillEnterForeground];
-  [self clearAndReindexModelIfNeeded];
+
+  if (self.needsClearAndReindex) {
+    [self clearAndReindexModelIfNeeded];
+  } else {
+    [self indexNextBatchInStack];
+  }
 }
 
 - (void)clearAndReindexModel {
@@ -410,6 +420,9 @@ class SpotlightBookmarkModelBridge;
   if (self.isAppInBackground || !self.needsClearAndReindex) {
     return;
   }
+
+  [self stopIndexing];
+  self.needsClearAndReindex = NO;
 
   __weak BookmarksSpotlightManager* weakSelf = self;
   [self.spotlightInterface
@@ -429,6 +442,7 @@ class SpotlightBookmarkModelBridge;
   if (self.isShuttingDown) {
     return;
   }
+
   // If the app is in background at this point, avoid accessing the spotlight
   // index and schedule a full reindex on foreground.
   if (self.isAppInBackground) {
@@ -443,7 +457,6 @@ class SpotlightBookmarkModelBridge;
   // operations.
   DCHECK(_indexingStack.empty());
   DCHECK(!self.nextBatchOperation);
-  DCHECK(self.modelUpdatesShouldCauseFullReindex);
 
   // If this method is called before bookmark model loaded, or after it
   // unloaded, reindexing won't be possible. The latter should happen at
