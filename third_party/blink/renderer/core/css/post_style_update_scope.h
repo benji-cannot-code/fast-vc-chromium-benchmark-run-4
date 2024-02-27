2eaa705f054e381fee515744031f7c3c84e37aa1
Anders Hartvoll Ruud <andruud@chromium.org>
2024-02-27 20:29:27 +0000

    [anchor] Use real AnchorEvaluator in UpdateStyleForOutOfFlow
    This CL sends the actual AnchorEvaluator(Impl) from OOF layout
    into the interleaved style update, which allows anchor()
    and anchor-size() queries to compute to their proper values.
    
    In additional to using a real evaluator, we need to integrate with
    some of the concepts of our Container Queries implementation:
    
     - Any element with anchor functions are now "interleaving roots"
       (ComputedStyle::IsInterleavingRoot), which means "layout upgrades"
       automatically do what they need to (e.g. for getComputedStyle and
       similar cases).
     - The "old style" is stored on PostStyleUpdateScope for every element
       with anchor queries, which ensures that we transition from the right
       state. See crbug.com/40943044 however, which is not addressed
       by this CL.
    
    Also, some of our infamous optimizations are disabled for elements
    with anchor queries:
    
     - The matched properties cache can't be used due to implicit anchors.
     - The base computed style optimization could probably be used,
       but support is deferred to a future CL.
    
    This feature is still behind a separate flag with status:<nothing>.
    Before we can set status:experimental, we need to fix a blocking
    crash bug (crbug.com/324930388), and also implement "persistent"
    anchor query results, i.e. store the last seen results on Element
    and use that for regular style recalcs.
    
    Bug: 41483417
    Change-Id: I93d2a627262c7a39db1996d24a6041853a12c6d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5317888
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266027}

--- third_party/blink/renderer/core/css/post_style_update_scope.h
+++ third_party/blink/renderer/core/css/post_style_update_scope.h
@@ -64,6 +64,7 @@ class CORE_EXPORT PostStyleUpdateScope {
    private:
     friend class PostStyleUpdateScope;
     friend class ContainerQueryTest;
+    friend class StyleResolverTest;
 
     HeapHashSet<Member<Element>> elements_with_pending_updates_;
     HeapHashMap<Member<const Element>, Member<const ComputedStyle>> old_styles_;
--- third_party/blink/renderer/core/style/computed_style.cc
+++ third_party/blink/renderer/core/style/computed_style.cc
@@ -2687,8 +2687,9 @@ bool ComputedStyle::CanMatchSizeContainerQueries(const Element& element) const {
 
 bool ComputedStyle::IsInterleavingRoot(const ComputedStyle* style) {
   const ComputedStyle* unensured = ComputedStyle::NullifyEnsured(style);
-  return unensured && (unensured->IsContainerForSizeContainerQueries() ||
-                       unensured->PositionFallback());
+  return unensured &&
+         (unensured->IsContainerForSizeContainerQueries() ||
+          unensured->PositionFallback() || unensured->HasAnchorFunctions());
 }
 
 bool ComputedStyle::CalculateIsStackingContextWithoutContainment() const {
