cc510e9f6453507eb057e685ce401c35d03789cd
Peter Marshall <petermarshall@chromium.org>
2024-02-27 03:14:32 +0000

    FSP: Notify watcher removed after removing watcher
    Fixed: b/324988851
    Change-Id: I5399178c7ea3d601bd09f6554a111bd4b700469f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5320805
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Reviewed-by: Cassy Chun-Crogan <cassycc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1265606}

--- chrome/browser/ash/file_system_provider/provided_file_system.h
+++ chrome/browser/ash/file_system_provider/provided_file_system.h
@@ -215,7 +215,7 @@ class ProvidedFileSystem : public ProvidedFileSystemInterface {
   // Notifies about a notifier even within |watcher_queue_|.
   AbortCallback NotifyInQueue(std::unique_ptr<NotifyInQueueArgs> args);
 
-  // Called when adding a watcher is completed with either success or en error.
+  // Called when adding a watcher is completed with either success or an error.
   void OnAddWatcherInQueueCompleted(
       size_t token,
       const base::FilePath& entry_path,
@@ -224,7 +224,7 @@ class ProvidedFileSystem : public ProvidedFileSystemInterface {
       storage::AsyncFileUtil::StatusCallback callback,
       base::File::Error result);
 
-  // Called when adding a watcher is completed with either a success or an
+  // Called when removing a watcher is completed with either a success or an
   // error.
   void OnRemoveWatcherInQueueCompleted(
       size_t token,
