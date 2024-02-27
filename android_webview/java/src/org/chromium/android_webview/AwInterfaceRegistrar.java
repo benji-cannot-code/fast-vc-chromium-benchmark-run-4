54deb950994e729628bc92dadcd718ed8dd6446d
Adem Derinel <derinel@google.com>
2024-02-27 08:38:00 +0000

    [WebView][WebAuthn] Register Authenticator interface
    Binary size change is unfortunate with 66 kB and most come from
    - android_mojo_bindings: WebAuthn related classes such as
     PublicKeyCredentialRequestOptions (18kiB)
    - components/webauthn/android: The component which controls the
     flows (36.27kiB)
    
    The code already uses Binder calls instead of the androidx library to
    reduce the binary size. Considering that we're adding a whole already
    existing component in WebView, this should be an acceptable size
    increase.
    
    Super Size Tiger View diff link:
    https://chrome-supersize.firebaseapp.com/viewer.html?load_url=https%3A%2F%2Fstorage.googleapis.com%2Fchromium-binary-size-trybot-results%2Fandroid-binary-size%2F2024%2F02%2F09%2F1778149%2Fsupersize_diff.sizediff#focus=7
    
    Bug: b/316886235, b/41483780
    Binary-Size: Size increase is unavoidable (see above).
    Change-Id: I83a6a70c95eb5c040c0316959dcbcb392e9ab4a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5278181
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265684}

---
+++ android_webview/java/src/org/chromium/android_webview/AwInterfaceRegistrar.java
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.android_webview;
+
+import org.jni_zero.CalledByNative;
+
+import org.chromium.blink.mojom.Authenticator;
+import org.chromium.components.webauthn.AuthenticatorFactory;
+import org.chromium.content_public.browser.InterfaceRegistrar;
+import org.chromium.content_public.browser.RenderFrameHost;
+import org.chromium.services.service_manager.InterfaceRegistry;
+
+/** Registers mojo interface implementations exposed to C++ code at the Android Webview layer. */
+class AwInterfaceRegistrar {
+    @CalledByNative
+    private static void registerMojoInterfaces() {
+        InterfaceRegistrar.Registry.addRenderFrameHostRegistrar(
+                new AndroidWebviewRenderFrameHostInterfaceRegistrar());
+    }
+
+    private static class AndroidWebviewRenderFrameHostInterfaceRegistrar
+            implements InterfaceRegistrar<RenderFrameHost> {
+        @Override
+        public void registerInterfaces(
+                InterfaceRegistry registry, final RenderFrameHost renderFrameHost) {
+            registry.addInterface(
+                    Authenticator.MANAGER,
+                    new AuthenticatorFactory(renderFrameHost, /* confirmationFactory= */ null));
+        }
+    }
+}
