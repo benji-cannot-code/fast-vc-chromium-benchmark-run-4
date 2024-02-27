de136358be6960a178d44b3e1e101eccaf80303c
Andrew Bregger <andrewbregger@google.com>
2024-02-27 22:35:54 +0000

    [Structured Metrics] Support platform2 in debug page.
    NO_BUG
    
    Change-Id: I63f1a80ce4d5368714e198e774ba7542c027b5b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5321715
    Reviewed-by: Jong Ahn <jongahn@chromium.org>
    Commit-Queue: Andrew Bregger <andrewbregger@google.com>
    Cr-Commit-Position: refs/heads/main@{#1266125}

--- chrome/browser/metrics/structured/ash_structured_metrics_recorder.cc
+++ chrome/browser/metrics/structured/ash_structured_metrics_recorder.cc
@@ -130,6 +130,10 @@ void AshStructuredMetricsRecorder::OnExternalMetricsCollected(
 
   event_storage_->AddBatchEvents(events.non_uma_events());
 
+  for (const auto& event : events.non_uma_events()) {
+    NotifyEventRecorded(event);
+  }
+
   // Only increment if new events were add.
   if (events.non_uma_events_size()) {
     external_metrics_scans_ += 1;
