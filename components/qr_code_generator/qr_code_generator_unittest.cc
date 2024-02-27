9a3da58113bac8d175baafcf07bf4b2c67d2feec
Lukasz Anforowicz <lukasza@chromium.org>
2024-02-27 22:44:12 +0000

    QR code: Paint quiet zone underneath `//components/qr_code_generator`.
    This CL moves painting of the "quiet zone" from
    `//chrome/browser/ui/views/qrcode_generator/qrcode_generator_bubble.cc`
    into `//components/qr_code_generator/bitmap_generator.cc`.  This helps
    to ensure that all clients of `qr_code_generator` make an explicit
    decision about who is responsible for generating the quiet zone.
    
    The CL tweaks the code underneath `//components/qr_code_generator`
    instead of reusing/copying the code from `qrcode_generator_bubble.cc`.
    This results in a somewhat simpler code, and avoids creating additional
    images (and additional memory copies).  The end-to-end behavior of
    `qrcode_generator_bubble.cc` shouldn't change - it should include the
    exact same quiet zone before and after this CL (except for the colors -
    see the next paragraph).
    
    The old code would use
    `GetColorProvider()->GetColor(kColorQrCodeBackground)` as the color of
    the "quiet zone".  The new code always uses white.  The new behavior is
    desirable because the quiet zone should have the same color as the
    light-colored QR pixels (and currently `bitmap_generator.cc` always
    paints black pixels on white background).  In the future we may want to
    make the exact colors configurable (e.g. theme-based).
    
    Other than the color change described above, the CL should preserve the
    old behavior (using `kWillBeAddedByClient` everywhere except the pixel
    tests and `qrcode_generator_bubble.cc`).  In particular, after this CL
    most QR code clients continue to not include the quiet zone within the
    image of the QR code (although surrounding UI elements may accidentally
    provide such quiet zone).
    
    Manual tests:
    * Manually navigated to https://example.com and
      used Chrome menu => Save and share => Create QR Code and
      verified that the generated code works with Google Lens on an Android
      device
    * Manually tested whether the new pixel test goldens decode successfully
      with Google Lens on an Android device (see
      https://crbug.com/325664342#comment6 and #comment8 and #comment9):
      - `DinoWithRoundQrPixelsAndLocators`,
        `PassKeyWithSquareQrPixelsAndLocators`, and `BigUrl` tests worked
        fine after this CL (an improvement, because their goldens didn't
        decode before this CL)
      - `HugeUrl` test didn't decode before and after this CL
    
    Bug: 325664342
    Change-Id: I7fa32710cb82959f215d5b10a0e78e3862ec1918
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5304091
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266129}

--- components/qr_code_generator/qr_code_generator_unittest.cc
+++ components/qr_code_generator/qr_code_generator_unittest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "base/containers/span.h"
-#include "base/test/scoped_feature_list.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace qr_code_generator {
