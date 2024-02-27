3052ad39df6f68b3388cc50ebda9dc1059df52ab
Vinny Persky <vinnypersky@google.com>
2024-02-27 20:30:35 +0000

    Show error dialog during the VCN 3DS flow
    This CL adds the logic to show the appropriate error dialog during VCN
    3DS flows, as discussed with UX.
    
    Bug: 1517762
    Change-Id: I99745b06c93fadb5e4c8b34474d66592a5ab0897
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5322045
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266029}

--- chrome/browser/ui/autofill/payments/desktop_payments_window_manager_interactive_uitest.cc
+++ chrome/browser/ui/autofill/payments/desktop_payments_window_manager_interactive_uitest.cc
@@ -235,6 +235,7 @@ IN_PROC_BROWSER_TEST_F(DesktopPaymentsWindowManagerInteractiveUiTest,
   EXPECT_EQ(response->card->expiration_year(), expiration_year);
   EXPECT_EQ(response->card->record_type(),
             CreditCard::RecordType::kVirtualCard);
+  EXPECT_FALSE(client()->autofill_error_dialog_shown());
 }
 
 // Test that the VCN 3DS pop-up is shown correctly, and on close an
@@ -265,6 +266,7 @@ IN_PROC_BROWSER_TEST_F(DesktopPaymentsWindowManagerInteractiveUiTest,
       authentication_response();
   ASSERT_TRUE(response.has_value());
   EXPECT_FALSE(response->card.has_value());
+  EXPECT_TRUE(client()->autofill_error_dialog_shown());
 }
 
 // Test that the VCN 3DS pop-up is shown correctly, and on close an
@@ -286,6 +288,7 @@ IN_PROC_BROWSER_TEST_F(DesktopPaymentsWindowManagerInteractiveUiTest,
       authentication_response();
   ASSERT_TRUE(response.has_value());
   EXPECT_FALSE(response->card.has_value());
+  EXPECT_FALSE(client()->autofill_error_dialog_shown());
 }
 
 // Test that the VCN 3DS pop-up is shown correctly, and on close an
@@ -315,6 +318,7 @@ IN_PROC_BROWSER_TEST_F(DesktopPaymentsWindowManagerInteractiveUiTest,
       authentication_response();
   ASSERT_TRUE(response.has_value());
   EXPECT_FALSE(response->card.has_value());
+  EXPECT_TRUE(client()->autofill_error_dialog_shown());
 }
 
 // Test that the VCN 3DS pop-up is shown correctly, and when the user cancels
