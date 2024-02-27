16f52db66a384a719b3f31881d9ed830ca9f04b3
Stephen McGruer <smcgruer@chromium.org>
2024-02-27 17:42:47 +0000

    [Autofill] Delete 'Remove copy' button from Clank settings
    This button was only used for full server cards, which have now
    been removed from the codebase (as of crrev.com/fdef8037). As of
    that CL, the button would no longer show up for any user.
    
    Bug: 1497734
    Change-Id: I251a3d3e59a408c2517bb5e6c9417286d910c873
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5301054
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265886}

--- chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/PersonalDataManager.java
+++ chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/PersonalDataManager.java
@@ -822,11 +822,6 @@ public class PersonalDataManager {
         PersonalDataManagerJni.get().deleteAllLocalCreditCards(mPersonalDataManagerAndroid);
     }
 
-    public void clearUnmaskedCache(String guid) {
-        PersonalDataManagerJni.get()
-                .clearUnmaskedCache(mPersonalDataManagerAndroid, PersonalDataManager.this, guid);
-    }
-
     public String getShippingAddressLabelWithCountryForPaymentRequest(AutofillProfile profile) {
         return PersonalDataManagerJni.get()
                 .getShippingAddressLabelForPaymentRequest(
@@ -1296,9 +1291,6 @@ public class PersonalDataManager {
 
         void clearServerDataForTesting(
                 long nativePersonalDataManagerAndroid, PersonalDataManager caller);
-
-        void clearUnmaskedCache(
-                long nativePersonalDataManagerAndroid, PersonalDataManager caller, String guid);
 
         boolean hasProfiles(long nativePersonalDataManagerAndroid);
 
