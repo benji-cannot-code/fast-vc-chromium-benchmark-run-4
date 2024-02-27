94f79ca77b23b317f0cf455ad4785da933bd761e
Jing Wang <jiwan@chromium.org>
2024-02-27 01:55:32 +0000

    Apply heatmap palm detection results on touch events.
    We match the touch events with heatmap palm detection results
    according to their timestamp. If the device supports heatmap and the
    heatmap service is available, we will rely only on heatmap results,
    otherwise we fallback to firmware results.
    
    Bug: b:289735201
    Change-Id: I30d021ee5a5242bda56c52543b3276e3f588087a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5311375
    Reviewed-by: Rob Schonberger <robsc@chromium.org>
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265586}

--- chromeos/ash/components/heatmap/heatmap_palm_detector_impl.h
+++ chromeos/ash/components/heatmap/heatmap_palm_detector_impl.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMEOS_ASH_COMPONENTS_HEATMAP_HEATMAP_PALM_DETECTOR_IMPL_H_
 #define CHROMEOS_ASH_COMPONENTS_HEATMAP_HEATMAP_PALM_DETECTOR_IMPL_H_
 
+#include <queue>
+#include <unordered_set>
+
 #include "chromeos/services/machine_learning/public/mojom/heatmap_palm_rejection.mojom.h"
 #include "chromeos/services/machine_learning/public/mojom/machine_learning_service.mojom.h"
 #include "mojo/public/cpp/bindings/receiver.h"
@@ -25,8 +28,11 @@ class HeatmapPalmDetectorImpl
 
   // ui::HeatmapPalmDetector:
   void Start(ModelId model_id, std::string_view hidraw_path) override;
-  DetectionResult GetDetectionResult() const override;
+  bool IsPalm(int tracking_id) const override;
   bool IsReady() const override;
+  void AddTouchRecord(base::Time timestamp,
+                      const std::vector<int>& tracking_ids) override;
+  void RemoveTouch(int tracking_id) override;
 
   // chromeos::machine_learning::mojom::HeatmapPalmRejectionClient
   void OnHeatmapProcessedEvent(
@@ -39,8 +45,10 @@ class HeatmapPalmDetectorImpl
 
   void OnConnectionError();
 
-  bool is_palm_ = false;
   bool is_ready_ = false;
+
+  std::queue<TouchRecord> touch_records_;
+  std::unordered_set<int> palm_tracking_ids_;
 
   mojo::Remote<chromeos::machine_learning::mojom::MachineLearningService>
       ml_service_;
--- ui/events/ozone/evdev/heatmap_palm_detector.h
+++ ui/events/ozone/evdev/heatmap_palm_detector.h
@@ -7,25 +7,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_EVENTS_OZONE_EVDEV_HEATMAP_PALM_DETECTOR_H_
 
 #include <memory>
-
 #include <string_view>
+#include <vector>
 
 #include "base/component_export.h"
 #include "base/functional/callback_forward.h"
+#include "base/time/time.h"
 
 namespace ui {
 
 // Interface for touch screen heatmap palm detector.
 class COMPONENT_EXPORT(EVDEV) HeatmapPalmDetector {
  public:
-  enum class DetectionResult { kNoPalm = 0, kPalm = 1 };
   enum class ModelId {
     kNotSupported = 0,
     kRex = 1,
     kGeralt = 2,
   };
+  struct TouchRecord {
+    TouchRecord(base::Time timestamp, const std::vector<int>& tracking_ids);
+    TouchRecord(const TouchRecord& t);
+    ~TouchRecord();
 
-  using DetectionDoneCallback = base::OnceCallback<void(DetectionResult)>;
+    base::Time timestamp;
+    std::vector<int> tracking_ids;
+  };
 
   static void SetInstance(std::unique_ptr<HeatmapPalmDetector> detector);
 
@@ -39,11 +45,19 @@ class COMPONENT_EXPORT(EVDEV) HeatmapPalmDetector {
   // Starts the palm detection service based on the model id and hidraw path.
   virtual void Start(ModelId model_id, std::string_view hidraw_path) = 0;
 
-  // Gets the palm detection results of the latest heatmap data.
-  virtual DetectionResult GetDetectionResult() const = 0;
+  // Returns if a touch, specified with `tracking_id`, is detected as palm.
+  virtual bool IsPalm(int tracking_id) const = 0;
 
   // Returns if the palm detection results is ready.
   virtual bool IsReady() const = 0;
+
+  // Adds a touch record containing timestamp and traicking ids, we will use the
+  // timestamp to match touch record with heatmap palm detection results.
+  virtual void AddTouchRecord(base::Time timestamp,
+                              const std::vector<int>& tracking_ids) = 0;
+
+  // Removes the tracking id which is no longer on the screen.
+  virtual void RemoveTouch(int tracking_id) = 0;
 };
 
 }  // namespace ui
