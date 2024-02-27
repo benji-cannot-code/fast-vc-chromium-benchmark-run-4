b3839d512960ebb3c46645e3387ff29728349264
Kenichi Ishibashi <bashi@chromium.org>
2024-02-27 11:29:53 +0000

    Remove unused include from host_resolver_manager.cc
    Sorting IP addresses has moved into HostResolverDnsTask.
    
    Change-Id: Ica11215b06c28d32d707d4ff066013002441a000
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5321907
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265722}

--- net/dns/host_resolver_manager.cc
+++ net/dns/host_resolver_manager.cc
@@ -77,7 +77,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/trace_constants.h"
 #include "net/base/tracing.h"
 #include "net/base/url_util.h"
-#include "net/dns/address_sorter.h"
 #include "net/dns/dns_alias_utility.h"
 #include "net/dns/dns_client.h"
 #include "net/dns/dns_names_util.h"
