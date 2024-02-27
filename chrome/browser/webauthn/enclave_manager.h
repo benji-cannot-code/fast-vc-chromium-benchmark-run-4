2bd66fdb8736ed13b8271de1d545d1276a36ac38
Ken Buchanan <kenrb@chromium.org>
2024-02-27 06:03:33 +0000

    [WebAuthn] Cache HW and UV keys once they have been loaded
    Currently the HW and UV keys are loaded when they are needed to
    provide signatures and then destroyed. Loading them can be quite slow,
    so it is better to cache them when they have been loaded or created.
    
    Bug: 40274370
    Change-Id: I47cd09087eb1aff2a404ce48753a79eaa03e29cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5318555
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265647}

--- chrome/browser/webauthn/enclave_manager.h
+++ chrome/browser/webauthn/enclave_manager.h
@@ -21,6 +21,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/fido/enclave/types.h"
 #include "services/network/public/mojom/network_context.mojom-forward.h"
 
+namespace crypto {
+class UnexportableSigningKey;
+class UserVerifyingSigningKey;
+}  // namespace crypto
+
 namespace network {
 class SharedURLLoaderFactory;
 }  // namespace network
@@ -164,6 +169,18 @@ class EnclaveManager : public KeyedService {
 
   webauthn_pb::EnclaveLocalState_User* current_user_state() const;
 
+  // Accessors for the HW and UV keys, invoking the supplied callbacks with
+  // the result. These can complete synchronously if the respective key is
+  // cached, or will attempt to load them asynchronously otherwise.
+  // If the key fails to load, the callback will be invoked with nullptr and
+  // the device's enclave registration will be reset.
+  void GetHardwareKeyForSignature(
+      base::OnceCallback<void(crypto::UnexportableSigningKey*)>
+          signing_callback);
+  void GetUserVerifyingKeyForSignature(
+      base::OnceCallback<void(crypto::UserVerifyingSigningKey*)>
+          signing_callback);
+
   const base::FilePath file_path_;
   const raw_ptr<signin::IdentityManager> identity_manager_;
   const raw_ptr<network::mojom::NetworkContext> network_context_;
@@ -183,6 +200,12 @@ class EnclaveManager : public KeyedService {
 
   std::unique_ptr<StateMachine> state_machine_;
   std::unique_ptr<PendingActions> pending_actions_;
+
+  // Allow keys to persist across sequences because loading them is slow.
+  // TODO(enclave): Make these scoped_refptrs instead, because they are
+  // used asynchronously by system APIs.
+  std::unique_ptr<crypto::UserVerifyingSigningKey> user_verifying_key_;
+  std::unique_ptr<crypto::UnexportableSigningKey> hardware_key_;
 
   unsigned store_keys_count_ = 0;
 
