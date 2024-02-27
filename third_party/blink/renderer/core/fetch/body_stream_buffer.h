794e42837ade89cebfbc42e131901ce3da5c2f25
Nidhi Jaju <nidhijaju@chromium.org>
2024-02-27 05:44:55 +0000

    Make BodyStreamBuffer inherit from UnderlyingByteSourceBase directly
    This CL further cleans up the logic for fetch to use byte streams.
    
    Bug: 40195373
    Change-Id: Ic505c8d9f35d82a255ecd29a4a18ed383a70dfb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5325482
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265643}

--- third_party/blink/renderer/core/fetch/body_stream_buffer.h
+++ third_party/blink/renderer/core/fetch/body_stream_buffer.h
@@ -19,13 +19,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
 #include "third_party/blink/renderer/core/fetch/bytes_uploader.h"
 #include "third_party/blink/renderer/core/fetch/fetch_data_loader.h"
+#include "third_party/blink/renderer/core/streams/underlying_byte_source_base.h"
 #include "third_party/blink/renderer/platform/bindings/trace_wrapper_v8_reference.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/loader/fetch/bytes_consumer.h"
 
 namespace blink {
 
-class BodyStreamBufferUnderlyingByteSource;
 class EncodedFormData;
 class ExceptionState;
 class ReadableStream;
@@ -33,7 +33,7 @@ class ScriptState;
 class ScriptCachedMetadataHandler;
 
 class CORE_EXPORT BodyStreamBuffer final
-    : public GarbageCollected<BodyStreamBuffer>,
+    : public UnderlyingByteSourceBase,
       public ExecutionContextLifecycleObserver,
       public BytesConsumer::Client {
  public:
@@ -86,7 +86,12 @@ class CORE_EXPORT BodyStreamBuffer final
                     ExceptionState&);
   void Tee(BodyStreamBuffer**, BodyStreamBuffer**, ExceptionState&);
 
-  ScriptPromise Cancel(ScriptState*, ScriptValue reason, ExceptionState&);
+  // UnderlyingByteSourceBase
+  ScriptPromise Pull(ReadableByteStreamController* controller,
+                     ExceptionState&) override;
+  ScriptPromise Cancel(ExceptionState&) override;
+  ScriptPromise Cancel(v8::Local<v8::Value> reason, ExceptionState&) override;
+  ScriptState* GetScriptState() override;
 
   // ExecutionContextLifecycleObserver
   void ContextDestroyed() override;
@@ -151,7 +156,6 @@ class CORE_EXPORT BodyStreamBuffer final
 
   Member<ScriptState> script_state_;
   Member<ReadableStream> stream_;
-  Member<BodyStreamBufferUnderlyingByteSource> underlying_byte_source_;
   Member<BytesUploader> stream_uploader_;
   Member<BytesConsumer> consumer_;
   // We need this member to keep it alive while loading.
