ad0c0365dd682b6332751858984fb13786dd36ed
S. Ganesh <ganesh@chromium.org>
2024-02-27 20:13:54 +0000

    updater: add a method to get the logged on user token
    This CL adds a `GetLoggedOnUserToken` method that retrieves the logged
    on user token for the active explorer process if one exists.
    
    Bug: 326449222
    Change-Id: Id458a82002d925b76a8bd22c6ed6ba491c104e3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5326012
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266016}

--- chrome/updater/util/win_util_unittest.cc
+++ chrome/updater/util/win_util_unittest.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/updater/updater_version.h"
 #include "chrome/updater/util/unit_test_util.h"
 #include "chrome/updater/util/unit_test_util_win.h"
+#include "chrome/updater/win/scoped_impersonation.h"
 #include "chrome/updater/win/test/test_executables.h"
 #include "chrome/updater/win/test/test_strings.h"
 #include "chrome/updater/win/win_constants.h"
@@ -575,6 +576,20 @@ TEST(WinUtil, GetTextForSystemError) {
   EXPECT_EQ(
       GetTextForSystemError(MAKE_HRESULT(SEVERITY_ERROR, FACILITY_ITF, 0x200)),
       L"0x80040200");
+}
+
+TEST(WinUtil, GetLoggedOnUserToken) {
+  if (!::IsUserAnAdmin() || !IsUACOn()) {
+    return;
+  }
+
+  ASSERT_TRUE(::IsUserAnAdmin());
+  HResultOr<ScopedKernelHANDLE> token = GetLoggedOnUserToken();
+  ASSERT_TRUE(token.has_value());
+
+  ScopedImpersonation impersonate;
+  ASSERT_TRUE(SUCCEEDED(impersonate.Impersonate(token.value().get())));
+  ASSERT_FALSE(::IsUserAnAdmin());
 }
 
 }  // namespace updater
