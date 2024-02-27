5459d689ef5161a7e35514e791ce6ee0a74aebf3
Peter Kasting <pkasting@chromium.org>
2024-02-27 20:53:31 +0000

    Revert "Adding/removing ignored children should not invalidate layout managers."
    This reverts commit 2b2453dd57e82e79b04715c5e30d7fed0f6b0118.
    
    Reason for revert: Likely causes UaFs in AnimatingLayoutManager; see crbug.com/323695104.
    
    Original change's description:
    > Adding/removing ignored children should not invalidate layout managers.
    >
    > This simply leads to unnecessary extra work.
    >
    > Bug: 1524189
    > Change-Id: Iab94f15262d56d7b4bc239d8a69e5da563f9a2e4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5259929
    > Auto-Submit: Peter Kasting <pkasting@chromium.org>
    > Reviewed-by: David Yeung <dayeung@chromium.org>
    > Commit-Queue: David Yeung <dayeung@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1255628}
    
    Bug: 1524189
    Change-Id: I8c28c95c56db7e5d9a866380609be85537f6da61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5327321
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266044}

--- ui/views/layout/layout_manager_base.cc
+++ ui/views/layout/layout_manager_base.cc
@@ -157,11 +157,8 @@ bool LayoutManagerBase::IsChildIncludedInLayout(const View* child,
   // During callbacks when a child is removed we can get in a state where a view
   // in the child list of the host view is not in |child_infos_|. In that case,
   // the view is being removed and is not part of the layout.
-  // TODO(crbug.com/1524187): This is a symptom of bad removal handling. Fix,
-  // then convert this to a CHECK.
-  if (it == child_infos_.end()) {
+  if (it == child_infos_.end())
     return false;
-  }
 
   return !child->GetProperty(kViewIgnoredByLayoutKey) &&
          !child->GetProperty(kIsDecorativeViewKey) &&
@@ -174,11 +171,8 @@ bool LayoutManagerBase::CanBeVisible(const View* child) const {
   // During callbacks when a child is removed we can get in a state where a view
   // in the child list of the host view is not in |child_infos_|. In that case,
   // the view is being removed and is not part of the layout.
-  // TODO(crbug.com/1524187): This is a symptom of bad removal handling. Fix,
-  // then convert this to a CHECK.
-  if (it == child_infos_.end()) {
+  if (it == child_infos_.end())
     return false;
-  }
 
   return it->second.can_be_visible;
 }
@@ -254,16 +248,12 @@ bool LayoutManagerBase::OnChildViewIgnoredByLayout(View* child_view,
 }
 
 bool LayoutManagerBase::OnViewAdded(View* host, View* view) {
-  if (IsChildIncludedInLayout(view)) {
-    OnLayoutChanged();
-  }
+  OnLayoutChanged();
   return false;
 }
 
 bool LayoutManagerBase::OnViewRemoved(View* host, View* view) {
-  if (IsChildIncludedInLayout(view)) {
-    OnLayoutChanged();
-  }
+  OnLayoutChanged();
   return false;
 }
 
@@ -419,15 +409,14 @@ bool LayoutManagerBase::PropagateViewAdded(View* host, View* view) {
 }
 
 bool LayoutManagerBase::PropagateViewRemoved(View* host, View* view) {
+  child_infos_.erase(view);
+
   bool result = false;
 
   for (auto& owned_layout : owned_layouts_)
     result |= owned_layout->PropagateViewRemoved(host, view);
 
   result |= OnViewRemoved(host, view);
-
-  child_infos_.erase(view);
-
   return result;
 }
 
