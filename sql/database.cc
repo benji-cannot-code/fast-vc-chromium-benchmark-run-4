442b96d4262b36652d5b09d18774d2c9ab1e0bd1
Dan McArdle <dmcardle@chromium.org>
2024-02-27 17:18:54 +0000

    SQL: Drop explicit rollback in Database::Poison()
    With this CL, Database::Poison() relies on the implicit rollback of
    transactions that occurs when closing the database connection, rather
    than explicitly issuing a "ROLLBACK" statement.
    
    This addresses a post-merge comment on a prior CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/5321958/comment/4c088549_9a0e5e08/
    
    Fixed: 326498384
    Change-Id: Ia422e230ff44794e7407156178078766f838867b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5323382
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265867}

--- sql/database.cc
+++ sql/database.cc
@@ -412,6 +412,12 @@ void Database::CloseInternal(bool forced) {
     DCHECK_EQ(sqlite_result_code, SqliteResultCode::kOk)
         << "sqlite3_close() failed in an unexpected way: "
         << sqlite3_errmsg(raw_db);
+
+    // Closing a SQLite database connection implicitly rolls back transactions.
+    // (See https://www.sqlite.org/c3ref/close.html for details.) Callers need
+    // not call `RollbackAllTransactions()`, but we still must account for the
+    // implicit rollback in our internal bookkeeping.
+    transaction_nesting_ = 0;
   }
 }
 
@@ -430,13 +436,6 @@ void Database::Close() {
   }
 
   CloseInternal(false);
-
-  // Closing a SQLite database connection implicitly rolls back transactions.
-  // (See https://www.sqlite.org/c3ref/close.html for details.) Thus, we can
-  // omit the call to `RollbackAllTransactions()`, but we still must account for
-  // the implicit rollback in our internal bookkeeping.
-  CHECK_GE(transaction_nesting_, 0);
-  transaction_nesting_ = 0;
 }
 
 void Database::Preload() {
@@ -1135,7 +1134,6 @@ void Database::Poison() {
     return;
   }
 
-  RollbackAllTransactions();
   CloseInternal(true);
 
   // Mark the database so that future API calls fail appropriately,
