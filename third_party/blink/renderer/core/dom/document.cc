05463e67966cccd66f15e82ba332104980712e71
Yoichi Osato <yoichio@chromium.org>
2024-02-27 00:36:33 +0000

    [LCPP] Call LCP predictor callback on window.onload if no learned DB
    This CL improves the LCPP coverage by call the fallback on
    window.onload if no learned DB for the page.
    This CL makes sure the LCPP callback is called definitely for
    unknown pages or web tests.
    
    This CL also makes it called only once on load
    (Document::CheckCompletedInternal() is called many times.)
    
    Bug: 40285771
    Change-Id: Ibc2aee708c9c848872f0195c9ad731e6709c4b5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5312215
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265559}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -4121,8 +4121,10 @@ bool Document::CheckCompletedInternal() {
 
   // OK, completed. Fire load completion events as needed.
   SetReadyState(kComplete);
-  if (LoadEventStillNeeded())
+  const bool load_event_needed = LoadEventStillNeeded();
+  if (load_event_needed) {
     ImplicitClose();
+  }
 
   DCHECK(fetcher_);
   fetcher_->ScheduleWarnUnusedPreloads();
@@ -4180,8 +4182,10 @@ bool Document::CheckCompletedInternal() {
     }
   }
 
-  if (LCPCriticalPathPredictor* lcpp = GetFrame()->GetLCPP()) {
-    lcpp->OnOutermostMainFrameDocumentLoad();
+  if (load_event_needed) {
+    if (LCPCriticalPathPredictor* lcpp = GetFrame()->GetLCPP()) {
+      lcpp->OnOutermostMainFrameDocumentLoad();
+    }
   }
 
   return true;
