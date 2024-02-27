5d45f6bd8051fa22062b4caa18827e102b228c79
Timofey Chudakov <tchudakov@google.com>
2024-02-27 11:54:35 +0000

    [Mullhet] Don't change field selection range during preview.
    Before the CL:
    http://screencast/cast/NjQxOTE1NTc5OTUwNjk0NHw0NzQzYmFmOS0zZA
    
    After the CL:
    http://screencast/cast/NDc2NjQ0NDg4NDU5MDU5MnxhZjM5YTRlYS1iMw
    
    Bug: b/326879023
    Change-Id: Ida24845c8a4c8e20b1fafd20696c66e7c86a73d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5326177
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1265724}

--- components/autofill/content/renderer/password_autofill_agent.cc
+++ components/autofill/content/renderer/password_autofill_agent.cc
@@ -922,10 +922,6 @@ void PasswordAutofillAgent::DoPreviewField(WebInputElement& input,
       .is_password = is_password,
   });
   input.SetSuggestedValue(WebString::FromUTF16(credential));
-  if (!is_password) {
-    form_util::PreviewSuggestion(input.SuggestedValue().Utf16(),
-                                 input.Value().Utf16(), input);
-  }
 }
 
 void PasswordAutofillAgent::FillField(WebInputElement* input,
@@ -995,11 +991,6 @@ void PasswordAutofillAgent::ClearPreviewedForm() {
     }
     element.SetSuggestedValue(WebString());
     element.SetAutofillState(preview_info.autofill_state);
-    if (!preview_info.is_password) {
-      element.SetSelectionRange(
-          base::checked_cast<unsigned>(element.Value().length()),
-          base::checked_cast<unsigned>(element.Value().length()));
-    }
   }
   previewed_elements_.clear();
 }
