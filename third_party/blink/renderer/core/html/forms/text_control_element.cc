59f49db85e29b86753c3ed2414a0c08ee0e12924
Mikel Astiz <mastiz@chromium.org>
2024-02-27 10:31:35 +0000

    [gardener] Revert "skip posting selectionchange tasks when the length selection range"
    This reverts commit 797738dd51c2bcb157ea4c66b5e55de32ec9e5ea.
    
    Reason for revert: suspect of causing blink_web_tests failures, e.g.
    https://ci.chromium.org/ui/p/chromium/builders/ci/WebKit%20Win10/129960/overview
    
    Original change's description:
    > skip posting selectionchange tasks when the length selection range
    > is not changed
    >
    > Setting the value of the input element triggers selectionchange event,
    > which posts async tasks to handle it. In some cases, the value is changed
    > but the length is not actually changed. This CL skips posting selectionchange
    > event tasks triggered by setting value when there the length of selection
    > range is not changed according to the spec[1].
    >
    > Based on pinpoint results of Speedometer2, VueJS-TodoMVC and
    > Preact-TodoMVC both get improved with a ~1.5% progression.
    >
    > [1] https://www.w3.org/TR/selection-api/#selectionchange-event
    >
    > Bug: 1517800
    >
    > Change-Id: I7b9cbb6dc2a4958f9c0afa3ec09c95fd8fd9e2a3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5153443
    > Reviewed-by: Koji Ishii <kojii@chromium.org>
    > Commit-Queue: Kent Tamura <tkent@chromium.org>
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Commit-Queue: Shuangshuang Zhou <shuangshuang.zhou@intel.com>
    > Cr-Commit-Position: refs/heads/main@{#1265626}
    
    Bug: 1517800
    Change-Id: I3f5ac28e937fae4df2537d095df83013030b96d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5317967
    Owners-Override: Mikel Astiz <mastiz@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265705}

--- third_party/blink/renderer/core/html/forms/text_control_element.cc
+++ third_party/blink/renderer/core/html/forms/text_control_element.cc
@@ -542,7 +542,6 @@ bool TextControlElement::SetSelectionRange(
           .SetShouldCloseTyping(true)
           .SetShouldClearTypingStyle(true)
           .SetDoNotSetFocus(true)
-          .SetShouldSkipSelectionChangeEvent(!did_change)
           .SetIsDirectional(direction != kSelectionHasNoDirection)
           .Build());
   return did_change;
