d9e9dec76b25898d7022211f65ca5bfc4c1fbb1a
Thomas Guilbert <tguilbert@chromium.org>
2024-02-27 01:55:18 +0000

    Revert "Refactor selection decoration in TextPaintStyle"
    This reverts commit 515a71f561fbb3cd20e91f4d4fb0611b30c92e45.
    
    Reason for revert: Breaking MSAN build:
    https://ci.chromium.org/ui/p/chromium/builders/ci/WebKit%20Linux%20MSAN/25516
    
    Original change's description:
    > Refactor selection decoration in TextPaintStyle
    >
    > Rather than storing the entire decoration, just keep the
    > lines and color. The former is needed to know if the decoration
    > should be applied, and the latter is the color that should override
    > the non-selection decorations' colors.
    >
    > This is in preparation for another CL where the color may remain
    > unresolved until use time when painting highlight layers.
    >
    > Bug: 41497157
    > Change-Id: I7d1b1900e62955cb1f76de6303af8a75aef0cdf4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5317922
    > Reviewed-by: Fredrik Söderquist <fs@opera.com>
    > Commit-Queue: Stephen Chenney <schenney@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1265442}
    
    Bug: 41497157
    Change-Id: Idbb565967b8976210a463db6e3dd75d381ee7875
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5326080
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Owners-Override: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265585}

--- third_party/blink/renderer/core/paint/text_paint_style.h
+++ third_party/blink/renderer/core/paint/text_paint_style.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/mojom/frame/color_scheme.mojom-blink-forward.h"
 #include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/core/style/applied_text_decoration.h"
 #include "third_party/blink/renderer/core/style/computed_style_constants.h"
 #include "third_party/blink/renderer/platform/graphics/color.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
@@ -29,8 +30,7 @@ struct CORE_EXPORT TextPaintStyle {
   float stroke_width;
   mojom::blink::ColorScheme color_scheme;
   scoped_refptr<const ShadowList> shadow;
-  TextDecorationLine selection_decoration_lines;
-  Color selection_decoration_color;
+  std::optional<AppliedTextDecoration> selection_text_decoration;
   EPaintOrder paint_order;
 
   bool operator==(const TextPaintStyle& other) const {
@@ -40,8 +40,7 @@ struct CORE_EXPORT TextPaintStyle {
            emphasis_mark_color == other.emphasis_mark_color &&
            stroke_width == other.stroke_width &&
            color_scheme == other.color_scheme && shadow == other.shadow &&
-           selection_decoration_lines == other.selection_decoration_lines &&
-           selection_decoration_color == other.selection_decoration_color &&
+           selection_text_decoration == other.selection_text_decoration &&
            paint_order == other.paint_order;
   }
   bool operator!=(const TextPaintStyle& other) const {
