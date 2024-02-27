9d3dfa0c27d3c1699cc598836a10246c86f6b7c9
Jenny Zhang <jennyz@chromium.org>
2024-02-27 04:33:57 +0000

    Revert "Fix the flaky DnD test related to racy last active browser update."
    This reverts commit 04bf9fc1388c94c457751b98779b1ee7244b49ff.
    
    Reason for revert: The test is quite flaky with the tentative fix.
    Original change's description:
    > Fix the flaky DnD test related to racy last active browser update.
    >
    > This is a tentative fix for DetachToOwnWindowFromMaximizedWindow
    > DnD test, which is caused by race of async last active browser
    > update (b/325634285). Until we fix the root cause of b/325634285
    > by making last active browser update completely asynchronous, there
    > will always be a chance of such race causing the test flaky.
    >
    > However, by adding the wait for the window being created in exo,
    > and last active browser being the expected browser, this may
    > help to mitigate the race issue for now.
    >
    > Bug: 40267511
    > Change-Id: Icff73ba7a3316acb60cb27c0afc9e4d7cf253fd3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5324335
    > Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1265477}
    
    Bug: 40267511
    Change-Id: I0449abf932f53db34191642f48573dec46d7595d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5318813
    Owners-Override: Matthew Mourgos <mmourgos@google.com>
    Reviewed-by: Matthew Mourgos <mmourgos@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Matthew Mourgos <mmourgos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1265625}

--- chrome/browser/ui/views/tabs/tab_drag_controller_interactive_uitest.cc
+++ chrome/browser/ui/views/tabs/tab_drag_controller_interactive_uitest.cc
@@ -118,7 +118,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
 #include "base/threading/platform_thread.h"
 #include "base/time/time.h"
-#include "chrome/browser/lacros/browser_test_util.h"
 #include "chrome/browser/ui/views/frame/desktop_browser_frame_lacros.h"
 #include "ui/aura/window_tree_host_platform.h"
 #include "ui/platform_window/extensions/wayland_extension.h"
@@ -2086,17 +2085,10 @@ IN_PROC_BROWSER_TEST_P(DetachToBrowserTabDragControllerTest,
 
   // Move to the first tab and drag it enough so that it detaches.
   int tab_0_width = tab_strip->tab_at(0)->width();
-  ui_test_utils::BrowserChangeObserver new_browser_observer(
-      nullptr, ui_test_utils::BrowserChangeObserver::ChangeType::kAdded);
   DragTabAndNotify(tab_strip,
                    base::BindOnce(&DetachToBrowserTabDragControllerTest::
                                       ReleaseInputAfterWindowDetached,
                                   base::Unretained(this), tab_0_width));
-  Browser* new_browser = new_browser_observer.Wait();
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-  ASSERT_TRUE(browser_test_util::WaitForWindowCreation(new_browser));
-#endif
-  ui_test_utils::WaitForBrowserSetLastActive(new_browser);
 
   // Should no longer be dragging.
   ASSERT_FALSE(tab_strip->GetDragContext()->IsDragSessionActive());
@@ -2104,6 +2096,7 @@ IN_PROC_BROWSER_TEST_P(DetachToBrowserTabDragControllerTest,
 
   // There should now be another browser.
   ASSERT_EQ(2u, browser_list->size());
+  Browser* new_browser = browser_list->get(1);
   ASSERT_TRUE(new_browser->window()->IsActive());
   TabStrip* tab_strip2 = GetTabStripForBrowser(new_browser);
   ASSERT_FALSE(tab_strip2->GetDragContext()->IsDragSessionActive());
