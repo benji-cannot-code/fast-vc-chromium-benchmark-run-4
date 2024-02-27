c9a5f2013d38097198618506186c0fbc2c0de749
CJ Huang <chenjih@google.com>
2024-02-27 23:45:29 +0000

    launcher: restrict shortcut results without keyword.
    Disallow shortcut results to display if the query does not have the
    keyword "shortcut". It's a temporary filtering before the overtriggering
    is fully resolved and the filtering applies to English only.
    
    Bug: b/326514738
    Test: Verified on DUT.
    Change-Id: I063ad0d3c8b00e33c1365b79f709c48fe7e763cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5326096
    Reviewed-by: Dmitry Grebenyuk <dgrebenyuk@google.com>
    Commit-Queue: CJ Huang <chenjih@google.com>
    Cr-Commit-Position: refs/heads/main@{#1266158}

--- chrome/browser/ash/app_list/search/keyboard_shortcut_provider.h
+++ chrome/browser/ash/app_list/search/keyboard_shortcut_provider.h
@@ -60,6 +60,12 @@ class KeyboardShortcutProvider : public SearchProvider {
   }
   std::vector<KeyboardShortcutData> shortcut_data() { return shortcut_data_; }
 
+  // Overrides `should_apply_query_filtering_` for testing purposes.
+  void set_should_apply_query_filtering_for_test(
+      bool should_apply_query_filtering) {
+    should_apply_query_filtering_ = should_apply_query_filtering;
+  }
+
  private:
   using ShortcutDataAndScores =
       std::vector<std::pair<KeyboardShortcutData, double>>;
@@ -81,6 +87,10 @@ class KeyboardShortcutProvider : public SearchProvider {
   // A check for whether the |embedding_| field of KeyboardShortcutData has been
   // set.
   bool is_embeddings_set_ = false;
+
+  // A check for whether we should apply query filtering for keyboard shortcut
+  // results.
+  bool should_apply_query_filtering_ = false;
 
   // A full collection of keyboard shortcuts, against which a query is compared
   // during a search.
--- chrome/browser/ash/app_list/search/keyboard_shortcut_provider_unittest.cc
+++ chrome/browser/ash/app_list/search/keyboard_shortcut_provider_unittest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/app_list/search/search_features.h"
 #include "chrome/browser/ash/app_list/search/test/test_manatee_cache.h"
 #include "chrome/browser/ash/app_list/search/test/test_search_controller.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/test/base/chrome_ash_test_base.h"
 #include "chrome/test/base/testing_profile.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -121,6 +122,9 @@ class KeyboardShortcutProviderFuzzyMatchTest
         profile_.get(), std::move(test_manatee_cache_));
     provider_ = provider.get();
     search_controller_->AddProvider(std::move(provider));
+
+    // TODO(b/326514738): bypassed the filtering in the unit test.
+    provider_->set_should_apply_query_filtering_for_test(false);
   }
 
   void Wait() { task_environment()->RunUntilIdle(); }
@@ -290,6 +294,8 @@ class KeyboardShortcutProviderManateeTest
                              IDS_KSV_DESCRIPTION_HIGHLIGHT_NEXT_ITEM_ON_SHELF,
                              IDS_KSV_SHORTCUT_HIGHLIGHT_NEXT_ITEM_ON_SHELF)};
     provider_->set_shortcut_data_for_test(test_shortcut_data_);
+    // TODO(b/326514738): bypassed the filtering in the unit test.
+    provider_->set_should_apply_query_filtering_for_test(false);
   }
 
   void Wait() { task_environment()->RunUntilIdle(); }
@@ -506,6 +512,9 @@ class CustomizableKeyboardShortcutProviderTest : public ChromeAshTestBase {
     // Initialize search_controller_;
     search_controller_ = std::make_unique<TestSearchController>();
     search_controller_->AddProvider(std::move(provider));
+
+    // TODO(b/326514738): bypassed the filtering in the unit test.
+    provider_->set_should_apply_query_filtering_for_test(false);
   }
 
   void Wait() { task_environment()->RunUntilIdle(); }
