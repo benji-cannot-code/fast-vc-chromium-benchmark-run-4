4b57c839539bdd753df55e9f54cad362cbafa67d
Elie Maamari <emaamari@google.com>
2024-02-27 07:03:38 +0000

    Add feature flag to control AuthPanel use in ChromeOS settings
    Bug: b:271248452
    Change-Id: I4a38b097035e586fb9efa4eb25f56eb92f6a82ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5306915
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Elie Maamari <emaamari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1265657}

--- chrome/browser/ui/webui/ash/settings/os_settings_ui.cc
+++ chrome/browser/ui/webui/ash/settings/os_settings_ui.cc
@@ -50,6 +50,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/services/auth_factor_config/in_process_instances.h"
 #include "chromeos/ash/services/cellular_setup/cellular_setup_impl.h"
 #include "chromeos/ash/services/cellular_setup/public/mojom/esim_manager.mojom.h"
+#include "chromeos/components/in_session_auth/in_process_instances.h"
+#include "chromeos/components/in_session_auth/mojom/in_session_auth.mojom.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/web_contents.h"
@@ -360,6 +362,11 @@ void OSSettingsUI::BindInterface(
   auth::BindToPasswordFactorEditor(
       std::move(receiver), quick_unlock::QuickUnlockFactory::GetDelegate(),
       g_browser_process->local_state());
+}
+
+void OSSettingsUI::BindInterface(
+    mojo::PendingReceiver<chromeos::auth::mojom::InSessionAuth> receiver) {
+  chromeos::auth::BindToInSessionAuthService(std::move(receiver));
 }
 
 void OSSettingsUI::BindInterface(
--- chrome/browser/ui/webui/ash/settings/os_settings_ui.h
+++ chrome/browser/ui/webui/ash/settings/os_settings_ui.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/services/connectivity/public/mojom/passpoint.mojom-forward.h"
 #include "chromeos/ash/services/hotspot_config/public/mojom/cros_hotspot_config.mojom-forward.h"
 #include "chromeos/ash/services/nearby/public/mojom/nearby_share_settings.mojom.h"
+#include "chromeos/components/in_session_auth/mojom/in_session_auth.mojom.h"
 #include "chromeos/services/network_config/public/mojom/cros_network_config.mojom-forward.h"
 #include "content/public/browser/webui_config.h"
 #include "content/public/common/url_constants.h"
@@ -185,6 +186,11 @@ class OSSettingsUI : public ui::MojoWebUIController {
       mojo::PendingReceiver<auth::mojom::PinFactorEditor> receiver);
   void BindInterface(
       mojo::PendingReceiver<auth::mojom::PasswordFactorEditor> receiver);
+
+  // Binds to the in session auth service, for authenticating sensitive
+  // operations.
+  void BindInterface(
+      mojo::PendingReceiver<chromeos::auth::mojom::InSessionAuth> receiver);
 
   // Binds to the Jelly dynamic color Mojo
   void BindInterface(
