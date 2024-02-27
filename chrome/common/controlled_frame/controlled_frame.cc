1de9078a2c7773a61edebe7f7ab54d7d4e0b3460
Chase Phillips <cmp@chromium.org>
2024-02-27 19:08:36 +0000

    controlled frame: Restrict feature to non-stable channel on CrOS
    Bug: b/40191772, b/326532409
    Change-Id: I59c0ed17f223b2d65c90e75bc44994460ecc9557
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5320623
    Commit-Queue: Chase Phillips <cmp@chromium.org>
    Reviewed-by: Ovidio Ruiz-Henríquez <odejesush@chromium.org>
    Auto-Submit: Chase Phillips <cmp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265960}

--- chrome/common/controlled_frame/controlled_frame.cc
+++ chrome/common/controlled_frame/controlled_frame.cc
@@ -10,8 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/initialize_extensions_client.h"
+#include "components/version_info/version_info.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/features/feature.h"
+#include "extensions/common/features/feature_channel.h"
 #include "extensions/common/mojom/context_type.mojom.h"
 
 #if BUILDFLAG(IS_CHROMEOS)
@@ -70,7 +72,8 @@ bool AvailabilityCheck(const std::string& api_full_name,
   // Also allow API exposure in ChromeOS Kiosk mode for web apps.
   if (base::FeatureList::IsEnabled(features::kWebKioskEnableIwaApis) &&
       IsRunningInKioskMode() && url.SchemeIs(url::kHttpsScheme)) {
-    is_allowed_for_scheme = true;
+    is_allowed_for_scheme =
+        extensions::GetCurrentChannel() != version_info::Channel::STABLE;
   }
 #endif
 
