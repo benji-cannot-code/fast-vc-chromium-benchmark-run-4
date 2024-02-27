8aa41d8010b463016a723f32ae2c50108b5a8f2f
Christopher Cameron <ccameron@chromium.org>
2024-02-27 13:53:23 +0000

    Revert "ui::HardwareDisplayPlaneManager: Remove old color management path"
    This reverts commit b409113aef0588e922ae551c7a2856d6d2ba0cda.
    
    Reason for revert: b/324755797
    
    Was also blamed for b/321593582, but that appears unrelated
    
    Original change's description:
    > ui::HardwareDisplayPlaneManager: Remove old color management path
    >
    > The path that computes the CTM, DEGAMMA, and GAMMA in the browser
    > is now unused. Remove its plumbing.
    >
    > Bug: 1505062
    > Change-Id: I9ff422242edac631bf859e50585475960f2d2b87
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5233882
    > Reviewed-by: Sasha McIntosh <sashamcintosh@chromium.org>
    > Commit-Queue: ccameron chromium <ccameron@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1257483}
    
    Bug: 1505062
    Change-Id: I9abe1b56569d4e889194a6a2e6223525176a547d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5310836
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265755}

--- ui/ozone/platform/drm/gpu/drm_gpu_display_manager.h
+++ ui/ozone/platform/drm/gpu/drm_gpu_display_manager.h
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using drmModeModeInfo = struct _drmModeModeInfo;
 
 namespace display {
+class GammaCurve;
+struct ColorCalibration;
 struct ColorTemperatureAdjustment;
 struct GammaAdjustment;
 }  // namespace display
@@ -69,9 +71,16 @@ class DrmGpuDisplayManager {
   void SetColorTemperatureAdjustment(
       int64_t display_id,
       const display::ColorTemperatureAdjustment& cta);
+  void SetColorCalibration(int64_t display_id,
+                           const display::ColorCalibration& calibration);
   void SetGammaAdjustment(int64_t display_id,
                           const display::GammaAdjustment& adjustment);
+  void SetColorMatrix(int64_t display_id,
+                      const std::vector<float>& color_matrix);
   void SetBackgroundColor(int64_t display_id, const uint64_t background_color);
+  void SetGammaCorrection(int64_t display_id,
+                          const display::GammaCurve& degamma,
+                          const display::GammaCurve& gamma);
   bool SetPrivacyScreen(int64_t display_id, bool enabled);
 
  private:
