3dcc1c4ae3cde4754e6c61526c161b6d8e4252ec
zhumatthew <zhumatthew@google.com>
2024-02-27 00:26:05 +0000

    Saved Desk: Remove import
    Remove accidental include of the header class in the impl file
    
    BUG=None
    TEST=None
    
    Change-Id: Iea0c08822eb5245c9665cb957bc97c756a551955
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5324110
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Commit-Queue: Matthew Zhu <zhumatthew@google.com>
    Cr-Commit-Position: refs/heads/main@{#1265554}

--- components/desks_storage/core/local_desk_data_manager.cc
+++ components/desks_storage/core/local_desk_data_manager.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/app_service/public/cpp/app_registry_cache.h"
 #include "components/services/app_service/public/cpp/app_registry_cache_wrapper.h"
 #include "components/sync/protocol/workspace_desk_specifics.pb.h"
-#include "local_desk_data_manager.h"
 
 namespace desks_storage {
 
