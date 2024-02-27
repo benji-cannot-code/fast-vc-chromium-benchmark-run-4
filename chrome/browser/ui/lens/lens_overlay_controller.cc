aa9657d80f98d3b73203315906da48a056e54615
Erik Chen <erikchen@chromium.org>
2024-02-27 23:24:42 +0000

    Add more scaffolding for lens overlay.
    Change-Id: I2ae6eb1e4da9e088609a2ca3871a2a2bad140a1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5326299
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266148}

--- chrome/browser/ui/lens/lens_overlay_controller.cc
+++ chrome/browser/ui/lens/lens_overlay_controller.cc
@@ -5,7 +5,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/lens/lens_overlay_controller.h"
 
+#include "chrome/browser/ui/tabs/tab_model.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
+
 LensOverlayController::LensOverlayController(tabs::TabModel* tab_model)
-    : tab_model_(tab_model) {}
+    : tab_model_(tab_model) {
+  tab_model_->owning_model()->AddObserver(this);
+}
 
 LensOverlayController::~LensOverlayController() = default;
+
+void LensOverlayController::OnTabStripModelChanged(
+    TabStripModel* tab_strip_model,
+    const TabStripModelChange& change,
+    const TabStripSelectionChange& selection) {
+  if (!selection.active_tab_changed()) {
+    return;
+  }
+
+  if (selection.new_contents == tab_model_->contents()) {
+    TabForegrounded();
+    return;
+  }
+
+  if (selection.old_contents == tab_model_->contents()) {
+    TabBackgrounded();
+  }
+}
+
+void LensOverlayController::TabForegrounded() {}
+
+void LensOverlayController::TabBackgrounded() {}
