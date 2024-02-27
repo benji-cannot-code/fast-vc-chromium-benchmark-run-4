9f6b1ea84c28ec6deb233994d1d487478dbafcd7
Ben Reich <benreich@chromium.org>
2024-02-27 02:48:41 +0000

    [FspCache] IWYU std::set
    Forgot this in my previous CL.
    
    Bug: b/317137739
    Change-Id: I20666df1248a47af8d3e7d062925b03f749a1185
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5319818
    Auto-Submit: Ben Reich <benreich@chromium.org>
    Reviewed-by: Cassy Chun-Crogan <cassycc@google.com>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265600}

--- chrome/browser/ash/file_system_provider/content_cache/cache_manager.h
+++ chrome/browser/ash/file_system_provider/content_cache/cache_manager.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_FILE_SYSTEM_PROVIDER_CONTENT_CACHE_CACHE_MANAGER_H_
 #define CHROME_BROWSER_ASH_FILE_SYSTEM_PROVIDER_CONTENT_CACHE_CACHE_MANAGER_H_
 
+#include <set>
+
 #include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/functional/callback_forward.h"
