327dd7c7a921a7deaed3e16aedada0eaa57ebd07
Geoff Lang <geofflang@chromium.org>
2024-02-27 21:40:46 +0000

    Mark DefaultANGLEMetal as launched.
    Bug: 1523778, 1522187, 326278125
    Change-Id: I31b699dc59714a5837f0455454fb5d8cfec2f4e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5310540
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Geoff Lang <geofflang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266075}

--- gpu/command_buffer/tests/gl_angle_shader_pixel_local_storage_unittest.cc
+++ gpu/command_buffer/tests/gl_angle_shader_pixel_local_storage_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/tests/gl_manager.h"
 #include "gpu/command_buffer/tests/gl_test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/gl/gl_implementation.h"
 
 namespace gpu {
 class ANGLEShaderPixelLocalStorageTest : public testing::Test {
@@ -172,6 +173,13 @@ TEST_F(ANGLEShaderPixelLocalStorageTest, LoadStoreTokens) {
     return;
   }
 
+// Test skipped on Intel-based Macs when running Metal. crbug.com/326278125
+#if BUILDFLAG(IS_MAC) && defined(ARCH_CPU_X86_64)
+  if (gl::GetANGLEImplementation() == gl::ANGLEImplementation::kMetal) {
+    return;
+  }
+#endif
+
   GLuint texs[4];
   glGenTextures(4, texs);
   for (GLuint tex : texs) {
@@ -213,6 +221,13 @@ TEST_F(ANGLEShaderPixelLocalStorageTest, DrawAPI) {
       !GLTestHelper::HasExtension("GL_ANGLE_shader_pixel_local_storage")) {
     return;
   }
+
+// Test skipped on Intel-based Macs when running Metal. crbug.com/326278125
+#if BUILDFLAG(IS_MAC) && defined(ARCH_CPU_X86_64)
+  if (gl::GetANGLEImplementation() == gl::ANGLEImplementation::kMetal) {
+    return;
+  }
+#endif
 
   GLuint tex;
   glGenTextures(1, &tex);
