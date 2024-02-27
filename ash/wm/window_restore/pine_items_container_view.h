7e1d529fd8e41d31aefd7d9e2517bd45bbe9bb68
Elijah Hewer <hewer@chromium.org>
2024-02-27 22:25:38 +0000

    Pine: Separate view classes
    Separate PineItemsContainerView and PineItemsOverflowView into their
    own files so they can more easily be used with unit tests in the
    future.
    
    Test: manual + auto
    Bug: b/322358447
    Change-Id: I31da6eda261578074e19a6d20fdc69a9329a8dac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5322621
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Elijah Hewer <hewer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266112}

---
+++ ash/wm/window_restore/pine_items_container_view.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_WM_WINDOW_RESTORE_PINE_ITEMS_CONTAINER_VIEW_H_
+#define ASH_WM_WINDOW_RESTORE_PINE_ITEMS_CONTAINER_VIEW_H_
+
+#include "ash/wm/window_restore/pine_contents_data.h"
+#include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/views/layout/box_layout_view.h"
+
+namespace ash {
+
+// The right side contents (in LTR) of the `PineContentsView`. It is a
+// vertical list of `PineItemView`, with each view representing an app. Shows
+// a maximum of `kMaxItems` items.
+class PineItemsContainerView : public views::BoxLayoutView {
+  METADATA_HEADER(PineItemsContainerView, views::BoxLayoutView)
+
+ public:
+  explicit PineItemsContainerView(
+      const PineContentsData::AppsInfos& apps_infos);
+  PineItemsContainerView(const PineItemsContainerView&) = delete;
+  PineItemsContainerView& operator=(const PineItemsContainerView&) = delete;
+  ~PineItemsContainerView() override = default;
+};
+
+}  // namespace ash
+
+#endif  // ASH_WM_WINDOW_RESTORE_PINE_ITEMS_CONTAINER_VIEW_H_
