f44e2860b204508caeb73b302359488332a166a3
Yulun Wu <yulunwu@chromium.org>
2024-02-27 22:19:28 +0000

    Add backup accessible name for untitled windows in window cycler.
    Additional are being filed for chrome-owned pages that are untitled.
    
    Bug: b/322881931
    Change-Id: I2edd8985ac0417524db83ec19db9b52601a29269
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5318959
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266109}

--- ash/wm/window_mini_view.cc
+++ ash/wm/window_mini_view.cc
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/shell.h"
+#include "ash/strings/grit/ash_strings.h"
+#include "ash/wm/snap_group/snap_group.h"
+#include "ash/wm/snap_group/snap_group_controller.h"
 #include "ash/wm/window_mini_view_header_view.h"
 #include "ash/wm/window_preview_view.h"
 #include "ash/wm/window_util.h"
@@ -16,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ui/base/window_properties.h"
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/aura/client/aura_constants.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/chromeos/styles/cros_tokens_color_mappings.h"
 #include "ui/compositor/layer.h"
@@ -295,7 +299,15 @@ void WindowMiniView::GetAccessibleNodeData(ui::AXNodeData* node_data) {
   }
 
   node_data->role = ax::mojom::Role::kWindow;
-  node_data->SetName(wm::GetTransientRoot(source_window_)->GetTitle());
+  const std::u16string& accessible_name =
+      wm::GetTransientRoot(source_window_)->GetTitle();
+
+  if (accessible_name.empty()) {
+    node_data->SetName(
+        l10n_util::GetStringUTF8(IDS_WM_WINDOW_CYCLER_UNTITLED_WINDOW));
+  } else {
+    node_data->SetName(accessible_name);
+  }
 }
 
 void WindowMiniView::OnWindowPropertyChanged(aura::Window* window,
