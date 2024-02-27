eb49c447f990ca16394dc342a37183f09bb5c2b8
Mikel Astiz <mastiz@chromium.org>
2024-02-27 15:46:45 +0000

    Revert "crosapi.mojom.NetworkChange: Add RequestPortalDetection call"
    This reverts commit a342350515919a932640ee9b37910a408677b1b9.
    
    Reason for revert: suspect of causing DCHECK failures, e.g.
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-lacros-asan-lsan-rel/8512/overview
    
    Original change's description:
    > crosapi.mojom.NetworkChange: Add RequestPortalDetection call
    >
    > On Lacros, when a navigation event occurs in the captive portal signin
    > window, a crosapi call is required to inform Ash that captive portal
    > detection should be started.
    >
    > This introduces the API call and adds LACROS code to trigger it.
    >
    > BUG=b:254336429
    > TEST=lacros_chrome_browsertests --gtest_filter=NetworkPortalSigninWindowLacrosBrowserTest*
    >
    > Change-Id: I61bdf3d9ccd061a6e06823b51b163007934c148a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5245589
    > Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1265403}
    
    No-Presubmit: true
    Bug: b:254336429, 327072851
    Change-Id: Id820248eab4d30854b1c3390a14b9d7402b939ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5324574
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Owners-Override: Mikel Astiz <mastiz@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1265812}

--- chrome/browser/chromeos/network/network_portal_signin_window.cc
+++ chrome/browser/chromeos/network/network_portal_signin_window.cc
@@ -24,8 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "chromeos/ash/components/network/portal_detector/network_portal_detector.h"
 #elif BUILDFLAG(IS_CHROMEOS_LACROS)
-#include "chromeos/crosapi/mojom/network_change.mojom.h"
-#include "chromeos/lacros/lacros_service.h"
+// TODO(b/261727502): Add crosapi call for portal detection.
 #endif
 
 namespace chromeos {
@@ -123,9 +122,7 @@ class NetworkPortalSigninWindow::WindowObserver
     ash::network_portal_detector::GetInstance()
         ->RequestCaptivePortalDetection();
 #elif BUILDFLAG(IS_CHROMEOS_LACROS)
-    chromeos::LacrosService::Get()
-        ->GetRemote<crosapi::mojom::NetworkChange>()
-        ->RequestPortalDetection();
+    // TODO(b/261727502): Add crosapi call for portal detection.
 #endif
   }
 
--- chrome/browser/chromeos/network/network_portal_signin_window.h
+++ chrome/browser/chromeos/network/network_portal_signin_window.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 class Browser;
-class NetworkPortalSigninWindowLacrosBrowserTest;
 
 namespace chromeos {
 
@@ -36,7 +35,6 @@ class NetworkPortalSigninWindow {
 
  protected:
   friend class base::NoDestructor<NetworkPortalSigninWindow>;
-  friend class NetworkPortalSigninWindowLacrosBrowserTest;
   NetworkPortalSigninWindow();
 
  private:
