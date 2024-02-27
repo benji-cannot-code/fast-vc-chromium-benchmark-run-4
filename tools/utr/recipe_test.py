45d737ca5f1be39595e53ffa5949e672c10a0042
Ben Pastene <bpastene@chromium.org>
2024-02-27 23:16:03 +0000

    utr: Fetch and print the summary_markdown of the recipe invocation
    This adds the "--output-result-json" arg to all recipes.py invocations,
    which can be used to fetch the summary_markdown of the recipe run.
    (But only for failed builds?)
    
    This markdown will then be printed at the end of the utr invocation,
    to hopefully help clarify what fails when it fails.
    
    Bug: 41492688
    Change-Id: Ib1c75eaa9cc9ff7d2b890a651f6ec5ac9dda749c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5325051
    Reviewed-by: Struan Shrimpton <sshrimp@google.com>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266141}

--- tools/utr/recipe_test.py
+++ tools/utr/recipe_test.py
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 """Tests for recipe.py"""
 
+import json
 import os
 import pathlib
 import shutil
@@ -37,7 +38,29 @@ class LegacyRunnerTests(unittest.TestCase):
                                  'some-builder', [], False, False)
     self.subp_mock.returncode = 123
     with mock.patch('subprocess.Popen', return_value=self.subp_mock):
-      self.assertEqual(runner.run_recipe(), 123)
+      exit_code, _ = runner.run_recipe()
+      self.assertEqual(exit_code, 123)
+
+  def testJson(self):
+    runner = recipe.LegacyRunner(self.tmp_dir, {}, 'some-bucket',
+                                 'some-builder', [], False, False)
+    with mock.patch('tempfile.TemporaryDirectory', return_value=self.tmp_dir):
+      with mock.patch('subprocess.Popen', return_value=self.subp_mock):
+        # Missing json file
+        _, error_msg = runner.run_recipe()
+        self.assertIsNone(error_msg)
+
+        # Broken json
+        with open(self.tmp_dir.joinpath('out.json'), 'w') as f:
+          f.write('this-is-not-json')
+        _, error_msg = runner.run_recipe()
+        self.assertIsNone(error_msg)
+
+        # Actual json
+        with open(self.tmp_dir.joinpath('out.json'), 'w') as f:
+          json.dump({'failure': {'humanReason': 'it exploded'}}, f)
+        _, error_msg = runner.run_recipe()
+        self.assertEqual(error_msg, 'it exploded')
 
 
 if __name__ == '__main__':
