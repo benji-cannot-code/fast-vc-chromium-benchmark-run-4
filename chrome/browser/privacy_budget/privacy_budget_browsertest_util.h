3e9f90c1028032ec789273f983a76eb0a2372356
Antonio Sartori <antoniosartori@chromium.org>
2024-02-27 13:45:51 +0000

    [IdentifiabilityStudy] Add browsertest for meta experiment
    This CL adds a browser test for checking end2end that the meta
    experiment of the Identifiability Study works correctly (reporting
    encountered surfaces and reserved metrics) independently from the rest
    of the Identifiability Study.
    
    Bug: 324271182
    Change-Id: Ia8e18e032d735b632cb59edb44ec2abd55fc8473
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5279580
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265750}

--- chrome/browser/privacy_budget/privacy_budget_browsertest_util.h
+++ chrome/browser/privacy_budget/privacy_budget_browsertest_util.h
@@ -66,6 +66,8 @@ class PrivacyBudgetBrowserTestBaseWithUkmRecording : public SyncTest {
 
   static PrefService* local_state();
 
+  content::WebContents* web_contents();
+
   bool EnableUkmRecording();
 
   bool DisableUkmRecording();
