9b1e4eb695b372297b746aac268a40847b036c4c
David Jean <djean@chromium.org>
2024-02-27 13:35:03 +0000

    [ios] Fix timing crash in CBD vc
    Can't reproduce the issue, even when forcing in code, but this
    CL puts in place a safeguard.
    
    Bug: 326708091
    Change-Id: I977ebd95d80cdceef6f85a3979108d2a3fcf780b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5322959
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265746}

--- ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_table_view_controller.mm
@@ -573,6 +573,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (BOOL)hasDataTypeItemsSelected {
   // Returns YES iff at least 1 data type cell is selected.
+  // Check if table model has the data types section, because sometimes this
+  // is called before it does.
+  if (![self.tableViewModel
+          hasSectionForSectionIdentifier:SectionIdentifierDataTypes]) {
+    return NO;
+  }
   NSArray* dataTypeItems = [self.tableViewModel
       itemsInSectionWithIdentifier:SectionIdentifierDataTypes];
   for (TableViewClearBrowsingDataItem* dataTypeItem in dataTypeItems) {
