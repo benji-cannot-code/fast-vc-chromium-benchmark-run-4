5b372a4e0edfd7b58c9ca02e184f5c35c8354a21
Dale Curtis <dalecurtis@chromium.org>
2024-02-27 21:20:54 +0000

    Fix memory leak with VideoFrameConverter.
    VFC uses FrameBufferPool to provide thread safe scratch space for
    conversions, but FBP has weird usage. In addition to the concept
    of being held by a VideoFrame, buffers also have the concept of
    being held by a library. This is needed by libvpx and ffmpeg, but
    is a foot gun in other circumstances.
    
    The fix is to call ReleaseFrameBuffer() immediately in our case
    since there is no library also trying to hold on to our frame
    buffer. A test is added to ensure the pool doesn't grow.
    
    R=sandersd
    
    Fixed: 326904528
    Change-Id: I9124262bb9c38d7e3a00bb568fd057d3321f7148
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5328768
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266064}

--- media/base/video_frame_converter.cc
+++ media/base/video_frame_converter.cc
@@ -105,7 +105,7 @@ scoped_refptr<VideoFrame> VideoFrameConverter::CreateTempFrame(
   if (tmp_frame) {
     tmp_frame->AddDestructionObserver(frame_pool_->CreateFrameCallback(fb_id));
   }
-
+  frame_pool_->ReleaseFrameBuffer(fb_id);
   return tmp_frame;
 }
 
@@ -156,7 +156,7 @@ scoped_refptr<VideoFrame> VideoFrameConverter::WrapNV12xFrameInI420xFrame(
     wrapped_frame->AddDestructionObserver(
         frame_pool_->CreateFrameCallback(fb_id));
   }
-
+  frame_pool_->ReleaseFrameBuffer(fb_id);
   return wrapped_frame;
 }
 
