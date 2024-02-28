9ad68a437b7a2fb7d6dee0cda9a46f356c3ce6a4
Ryan Sultanem <rsult@google.com>
2024-02-28 17:57:56 +0000

    [AvatarButton] Migrate SyncError/Paused state to the State Manager
    Merge the two states into one, differentiating on the text that is
    shown and some UI colors.
    
    Bug: b/324018028
    Change-Id: I8a9e5c30b1d0998e407a2a00415ef231820e9ff1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5307527
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1266513}

--- chrome/browser/ui/views/profiles/avatar_toolbar_button_delegate.h
+++ chrome/browser/ui/views/profiles/avatar_toolbar_button_delegate.h
@@ -47,8 +47,7 @@ enum class ButtonState;
 // - Explicit modifications override: such as displaying specific text when
 //   intercept bubbles are displayed.
 // - Sync paused/error state.
-class AvatarToolbarButtonDelegate : public ProfileAttributesStorage::Observer,
-                                    public syncer::SyncServiceObserver {
+class AvatarToolbarButtonDelegate : public ProfileAttributesStorage::Observer {
  public:
   AvatarToolbarButtonDelegate(AvatarToolbarButton* button, Browser* browser);
 
@@ -99,10 +98,6 @@ class AvatarToolbarButtonDelegate : public ProfileAttributesStorage::Observer,
   void OnProfileUserManagementAcceptanceChanged(
       const base::FilePath& profile_path) override;
 
-  // SyncServiceObserver:
-  void OnStateChanged(syncer::SyncService*) override;
-  void OnSyncShutdown(syncer::SyncService*) override;
-
   TextState GetDefaultTextState() const;
 
   std::u16string GetProfileName() const;
@@ -112,7 +107,6 @@ class AvatarToolbarButtonDelegate : public ProfileAttributesStorage::Observer,
   gfx::Image GetProfileAvatarImage(int preferred_size) const;
   // Returns the count of incognito or guest windows attached to the profile.
   int GetWindowCount() const;
-  std::optional<AvatarSyncErrorType> GetAvatarSyncErrorType() const;
   gfx::Image GetGaiaAccountImage() const;
 
   // Callback used to remove the explicit text shown and reset to the default.
@@ -123,8 +117,6 @@ class AvatarToolbarButtonDelegate : public ProfileAttributesStorage::Observer,
   base::ScopedObservation<ProfileAttributesStorage,
                           ProfileAttributesStorage::Observer>
       profile_observation_{this};
-  base::ScopedObservation<syncer::SyncService, syncer::SyncServiceObserver>
-      sync_service_observation_{this};
 
   const raw_ptr<AvatarToolbarButton> avatar_toolbar_button_;
   const raw_ptr<Browser> browser_;
@@ -132,10 +124,6 @@ class AvatarToolbarButtonDelegate : public ProfileAttributesStorage::Observer,
   TextState button_text_state_ = TextState::kNotShowing;
 
   bool enterprise_text_hide_scheduled_ = false;
-
-  // Caches the value of the last error so the class can detect when it
-  // changes and notify |avatar_toolbar_button_|.
-  std::optional<AvatarSyncErrorType> last_avatar_error_;
 
   // Text to be displayed while the state is
   // `ButtonState::kExplicitTextShowing`.
