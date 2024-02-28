13831558e2d459308981b75b69ca0eb190e2c575
Nico Weber <thakis@chromium.org>
2024-02-28 15:02:22 +0000

    Remove a few more vector<const T> in tests
    See comments on bug for details and motivation, e.g. comment 16.
    
    I made a few of the vectors const instead. In some cases, that
    wasn't possible. The const element didn't add a ton in those cases,
    so I just removed it.
    
    Bug: 323708866
    Change-Id: Idffc79ea1fe0b88413c2c4c64e79f85367595a30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5328267
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Owners-Override: Nico Weber <thakis@chromium.org>
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266406}

--- chrome/browser/ui/web_applications/sub_apps_admin_policy_browsertest.cc
+++ chrome/browser/ui/web_applications/sub_apps_admin_policy_browsertest.cc
@@ -104,7 +104,7 @@ class SubAppsAdminPolicyTest : public IsolatedWebAppBrowserTestHarness {
   }
 
   void SetAllowlistedOrigins(
-      const std::vector<const std::string>& allowlisted_origins) {
+      const std::vector<std::string>& allowlisted_origins) {
     policy::PolicyMap policy_map;
     base::Value::List allowed_origins;
 
