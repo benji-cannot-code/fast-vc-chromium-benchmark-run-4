c7165f55c077a5c648a7899681c04d2acb268af2
Louis Romero <lpromero@google.com>
2024-02-28 15:33:09 +0000

    Clean up WebStateList Tab Groups tests
    This adds comments, adds tests for ActivateWebStateAt and
    ReplaceWebStateAt, and checks that visual data are correctly used when
    creating groups.
    
    Bug: 327152038
    Change-Id: Ife2db494bb64d5b3232cfd4ed0fa6e0f04750043
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5324664
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1266424}

--- ios/chrome/browser/shared/model/web_state_list/web_state_list_unittest.mm
+++ ios/chrome/browser/shared/model/web_state_list/web_state_list_unittest.mm
@@ -1482,6 +1482,7 @@ TEST_F(WebStateListTest, WebStateListAsWeakPtr) {
   EXPECT_FALSE(weak_web_state_list);
 }
 
+// Tests that GetGroupOfWebStateAt returns the correct group(s).
 TEST_F(WebStateListTest, GetGroupOfWebStateAt) {
   WebStateListBuilderFromDescription builder;
   ASSERT_TRUE(builder.BuildWebStateListFromDescription(web_state_list_,
@@ -1495,6 +1496,7 @@ TEST_F(WebStateListTest, GetGroupOfWebStateAt) {
   EXPECT_EQ(nullptr, web_state_list_.GetGroupOfWebStateAt(4));
 }
 
+// Tests that GetWebStates returns the correct ranges.
 TEST_F(WebStateListTest, GetWebStates) {
   WebStateListBuilderFromDescription builder;
   ASSERT_TRUE(builder.BuildWebStateListFromDescription(
@@ -1508,6 +1510,7 @@ TEST_F(WebStateListTest, GetWebStates) {
   EXPECT_EQ(WebStateList::Range(8, 1), web_state_list_.GetWebStates(group_2));
 }
 
+// Tests that inserting when there are no groups doesn't create any group.
 TEST_F(WebStateListTest, InsertWebState_NoGroup) {
   WebStateListBuilderFromDescription builder;
   ASSERT_TRUE(
@@ -1518,6 +1521,7 @@ TEST_F(WebStateListTest, InsertWebState_NoGroup) {
   EXPECT_EQ("| a* _", builder.GetWebStateListDescription(web_state_list_));
 }
 
+// Tests that moving when there are no groups doesn't create any group.
 TEST_F(WebStateListTest, MoveWebStateAt_NoGroup) {
   WebStateListBuilderFromDescription builder;
   ASSERT_TRUE(
@@ -1528,6 +1532,7 @@ TEST_F(WebStateListTest, MoveWebStateAt_NoGroup) {
   EXPECT_EQ("| a c d b*", builder.GetWebStateListDescription(web_state_list_));
 }
 
+// Tests that replacing when there are no groups doesn't create any group.
 TEST_F(WebStateListTest, ReplaceWebStateAt_NoGroup) {
   WebStateListBuilderFromDescription builder;
   ASSERT_TRUE(
@@ -1540,6 +1545,41 @@ TEST_F(WebStateListTest, ReplaceWebStateAt_NoGroup) {
   EXPECT_EQ("| b*", builder.GetWebStateListDescription(web_state_list_));
 }
 
+// Tests that replacing when there is a group keeps the group.
+TEST_F(WebStateListTest, ReplaceWebStateAt_Grouped) {
+  WebStateListBuilderFromDescription builder;
+  ASSERT_TRUE(
+      builder.BuildWebStateListFromDescription(web_state_list_, "| [ 0 a* ]"));
+  auto replacement_web_state = CreateWebState(kURL1);
+  builder.SetWebStateIdentifier(replacement_web_state.get(), 'b');
+
+  web_state_list_.ReplaceWebStateAt(0, std::move(replacement_web_state));
+
+  EXPECT_EQ("| [ 0 b* ]", builder.GetWebStateListDescription(web_state_list_));
+}
+
+// Tests that activating a non-grouped WebState doesn't create any group.
+TEST_F(WebStateListTest, ActivateWebStateAt_NoGroup) {
+  WebStateListBuilderFromDescription builder;
+  ASSERT_TRUE(builder.BuildWebStateListFromDescription(web_state_list_, "| a"));
+
+  web_state_list_.ActivateWebStateAt(0);
+
+  EXPECT_EQ("| a*", builder.GetWebStateListDescription(web_state_list_));
+}
+
+// Tests that activating a grouped WebState keeps the group.
+TEST_F(WebStateListTest, ActivateWebStateAt_Grouped) {
+  WebStateListBuilderFromDescription builder;
+  ASSERT_TRUE(
+      builder.BuildWebStateListFromDescription(web_state_list_, "| [ 0 a ]"));
+
+  web_state_list_.ActivateWebStateAt(0);
+
+  EXPECT_EQ("| [ 0 a* ]", builder.GetWebStateListDescription(web_state_list_));
+}
+
+// Tests creating a group with one tab.
 TEST_F(WebStateListTest, CreateGroup_OneTab) {
   WebStateListBuilderFromDescription builder;
   ASSERT_TRUE(
@@ -1552,8 +1592,10 @@ TEST_F(WebStateListTest, CreateGroup_OneTab) {
 
   EXPECT_EQ("| [ 0 a* ]", builder.GetWebStateListDescription(web_state_list_));
   EXPECT_EQ(WebStateList::Range(0, 1), web_state_list_.GetWebStates(group));
+  EXPECT_EQ(visual_data, group->visual_data());
 }
 
+// Tests creating a group with several tabs.
 TEST_F(WebStateListTest, CreateGroup_SeveralTabs) {
   WebStateListBuilderFromDescription builder;
   ASSERT_TRUE(builder.BuildWebStateListFromDescription(web_state_list_,
@@ -1567,8 +1609,10 @@ TEST_F(WebStateListTest, CreateGroup_SeveralTabs) {
   EXPECT_EQ("| [ 0 a c e ] b* d",
             builder.GetWebStateListDescription(web_state_list_));
   EXPECT_EQ(WebStateList::Range(0, 3), web_state_list_.GetWebStates(group));
+  EXPECT_EQ(visual_data, group->visual_data());
 }
 
+// Tests creating a group with several tabs, some being pinned.
 TEST_F(WebStateListTest, CreateGroup_SeveralTabs_SomePinned) {
   WebStateListBuilderFromDescription builder;
   ASSERT_TRUE(builder.BuildWebStateListFromDescription(web_state_list_,
@@ -1582,8 +1626,10 @@ TEST_F(WebStateListTest, CreateGroup_SeveralTabs_SomePinned) {
   EXPECT_EQ("a c | [ 0 b* d ] e",
             builder.GetWebStateListDescription(web_state_list_));
   EXPECT_EQ(WebStateList::Range(2, 2), web_state_list_.GetWebStates(group));
+  EXPECT_EQ(visual_data, group->visual_data());
 }
 
+// Tests creating a group with several tabs, some being already grouped.
 TEST_F(WebStateListTest, CreateGroup_SeveralTabs_SomeGrouped) {
   WebStateListBuilderFromDescription builder;
   ASSERT_TRUE(builder.BuildWebStateListFromDescription(web_state_list_,
@@ -1600,4 +1646,5 @@ TEST_F(WebStateListTest, CreateGroup_SeveralTabs_SomeGrouped) {
 
   EXPECT_EQ(WebStateList::Range(0, 2), web_state_list_.GetWebStates(group_0));
   EXPECT_EQ(WebStateList::Range(2, 2), web_state_list_.GetWebStates(group_1));
+  EXPECT_EQ(visual_data_1, group_1->visual_data());
 }
