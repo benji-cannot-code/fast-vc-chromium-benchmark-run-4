bcb75224de15427eb36d2759f5dea5c8be7f42c4
Gauthier Ambard <gambard@chromium.org>
2024-02-28 15:55:23 +0000

    [iOS] Remove Dynamic Color flags
    Those flags are no longer used.
    
    Fixed: 326355036, 326360724
    Change-Id: I81e06c4c8b690f38c45f14c871fbdc2454675b8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5329868
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266436}

--- ios/chrome/browser/ui/toolbar/adaptive_toolbar_mediator.mm
+++ ios/chrome/browser/ui/toolbar/adaptive_toolbar_mediator.mm
@@ -324,9 +324,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         setLoadingProgressFraction:self.webState->GetLoadingProgress()];
   }
   [self updateShareMenuForWebState:self.webState];
-  if (base::FeatureList::IsEnabled(kThemeColorInTopToolbar) ||
-      base::FeatureList::IsEnabled(kDynamicThemeColor) ||
-      base::FeatureList::IsEnabled(kDynamicBackgroundColor)) {
+  if (base::FeatureList::IsEnabled(kThemeColorInTopToolbar)) {
     [self.consumer setPageThemeColor:self.webState->GetThemeColor()];
     [self.consumer
         setUnderPageBackgroundColor:self.webState
