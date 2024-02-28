accf6ae7da25440fafd8062089888427efba9eca
Abigail Klein <abigailbklein@google.com>
2024-02-28 21:31:34 +0000

    Remove reader mode icon.
    This is part of a larger effort to remove the desktop kReaderMode flag
    from the codebase.
    
    Bug: 40258128
    Change-Id: Ic742c0765b6c2b7b04e455240895a4bc59cf6d94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5318799
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1266640}

--- chrome/browser/dom_distiller/distillable_page_utils_browsertest.cc
+++ chrome/browser/dom_distiller/distillable_page_utils_browsertest.cc
@@ -29,11 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
-#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || \
-    BUILDFLAG(IS_WIN)
-#include "components/ukm/test_ukm_recorder.h"
-#endif
-
 namespace dom_distiller {
 namespace {
 
@@ -288,43 +283,5 @@ IN_PROC_BROWSER_TEST_F(DistillablePageUtilsBrowserTestAllArticles,
       GetLatestResult(web_contents_),
       Optional(AllOf(IsDistillable(), IsLast(), Not(IsMobileFriendly()))));
 }
-
-#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || \
-    BUILDFLAG(IS_WIN)
-IN_PROC_BROWSER_TEST_F(DistillablePageUtilsBrowserTestAllArticles,
-                       RecordPageIsDistillableOnArticleLoad) {
-  ON_CALL(holder_, OnResult(IsLast()))
-      .WillByDefault(InvokeWithoutArgs(this, &TestOption::QuitSoon));
-
-  ukm::TestAutoSetUkmRecorder ukm_recorder;
-  NavigateAndWait(kSimpleArticlePath, base::TimeDelta());
-
-  std::vector<raw_ptr<const ukm::mojom::UkmEntry, VectorExperimental>>
-      distillability_entries =
-          ukm_recorder.GetEntriesByName("ReaderModeReceivedDistillability");
-  ASSERT_THAT(distillability_entries, SizeIs(1));
-  EXPECT_THAT(ukm_recorder.GetEntryMetric(distillability_entries.front(),
-                                          "IsPageDistillable"),
-              Pointee(true));
-}
-
-IN_PROC_BROWSER_TEST_F(DistillablePageUtilsBrowserTestAllArticles,
-                       RecordPageIsNotDistillableOnNonArticleLoad) {
-  ON_CALL(holder_, OnResult(IsLast()))
-      .WillByDefault(InvokeWithoutArgs(this, &TestOption::QuitSoon));
-
-  ukm::TestAutoSetUkmRecorder ukm_recorder;
-  NavigateAndWait(kNonArticlePath, base::TimeDelta());
-
-  std::vector<raw_ptr<const ukm::mojom::UkmEntry, VectorExperimental>>
-      distillability_entries =
-          ukm_recorder.GetEntriesByName("ReaderModeReceivedDistillability");
-  ASSERT_THAT(distillability_entries, SizeIs(1));
-  EXPECT_THAT(ukm_recorder.GetEntryMetric(distillability_entries.front(),
-                                          "IsPageDistillable"),
-              Pointee(false));
-}
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_LINUX) ||
-        // BUILDFLAG(IS_MAC)OS || BUILDFLAG(IS_WIN)
 
 }  // namespace dom_distiller
