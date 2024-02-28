a9f1648ce58840187eb28f35e4e9aba54a3b923e
S. Ganesh <ganesh@chromium.org>
2024-02-28 19:42:45 +0000

    Reland "updater: pick up usagestats for Windows"
    This reverts commit da5ce591937630b558b08c94ba24f95bdad2e425.
    
    Reason for revert: Fix unit tests that broke previously.
    
    Original change's description:
    > Revert "updater: pick up usagestats for Windows"
    >
    > This reverts commit 7024785a0327cdfbce74585b5774d00ba782631f.
    >
    > Reason for revert: UpdateUsageStatsTaskTest.NoApps and .ZeroAppsEnabled are flaky on win64, see https://ci.chromium.org/ui/p/chrome/builders/ci/win64-rel-ready?limit=200 and first failing build https://ci.chromium.org/ui/p/chrome/builders/ci/win64-rel-ready/15861/test-results?sortby=&groupby= - specifically, the newly-added ASSERTs on AreRawUsageStatsEnabled() are failing.
    >
    > Original change's description:
    > > updater: pick up usagestats for Windows
    > >
    > > This CL fixes an issue with usage stats not being picked up before the
    > > Wake task runs, by reading the raw usage stats from the registry.
    > >
    > > This allows for:
    > > * any crashes that happen before the Wake task to get uploaded.
    > > * any appcommands that run before the Wake task to ping.
    > >
    > > Fixed: 325316125
    > > Change-Id: Ie4279f4e423a550ad1e7bb324501ec5f63fc5c17
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5315548
    > > Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    > > Auto-Submit: S Ganesh <ganesh@chromium.org>
    > > Commit-Queue: S Ganesh <ganesh@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1264743}
    >
    > Change-Id: Id34145221f09c4c226728ec28ff2ee95dca22d3e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5323982
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Marc Treib <treib@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1265169}
    
    Change-Id: Ifc06884946de39c9f46213321f9bddaf6b698dd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5328438
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266576}

--- chrome/updater/update_usage_stats_task_win.cc
+++ chrome/updater/update_usage_stats_task_win.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/strings/strcat.h"
-#include "base/strings/sys_string_conversions.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/win/registry.h"
 #include "base/win/windows_types.h"
 #include "chrome/updater/constants.h"
@@ -49,7 +49,7 @@ bool OtherAppUsageStatsAllowed(const std::vector<std::string>& app_ids,
       continue;
     }
 
-    if (AppUsageStatsAllowed(scope, base::SysUTF8ToWide(app_id))) {
+    if (AppUsageStatsAllowed(scope, base::UTF8ToWide(app_id))) {
       VLOG(2) << "usagestats enabled by app " << app_id;
       return true;
     }
@@ -57,6 +57,36 @@ bool OtherAppUsageStatsAllowed(const std::vector<std::string>& app_ids,
 
   VLOG(2) << "No app enables usagestats.";
   return false;
+}
+
+bool AreRawUsageStatsEnabled(
+    UpdaterScope scope,
+    const std::vector<std::string>& include_only_these_app_ids) {
+  return OtherAppUsageStatsAllowed(
+      [&]() {
+        const HKEY root = UpdaterScopeToHKeyRoot(scope);
+        std::vector<std::wstring> subkeys;
+        if (IsSystemInstall(scope)) {
+          subkeys.push_back(CLIENT_STATE_MEDIUM_KEY);
+        }
+        subkeys.push_back(CLIENT_STATE_KEY);
+        std::vector<std::string> app_ids;
+        for (const auto& subkey : subkeys) {
+          for (base::win::RegistryKeyIterator it(root, subkey.c_str(),
+                                                 KEY_WOW64_32KEY);
+               it.Valid(); ++it) {
+            const std::string app_id = base::WideToUTF8(it.Name());
+            if (include_only_these_app_ids.empty() ||
+                base::ranges::find(include_only_these_app_ids, app_id) !=
+                    std::end(include_only_these_app_ids)) {
+              app_ids.push_back(app_id);
+            }
+          }
+        }
+
+        return app_ids;
+      }(),
+      scope);
 }
 
 }  // namespace updater
