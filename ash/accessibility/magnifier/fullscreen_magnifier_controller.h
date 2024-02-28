ba10c1bbbb29140a310b8681766d522a423a004b
Katie Dektar <katie@chromium.org>
2024-02-28 17:08:59 +0000

    [A11y] Fix bug at high magnifications where centered mode could not move left/down
    When in centered mode, the magnifier could not move left or down at high zoom levels because we truncated floating-point DIP cursor position to the floor int, so movements less than a full DIP didn't have an impact.
    This change rounds instead of truncating, which makes the magnifier
    behave better at very high zoom levels, and still feel reactive.
    
    Tests are different for this because EventGenerator works in integer
    DIPs rather than floating-point DIPs or pixels. In real life, the
    mouse position is tracked in floating-point DIPs, so incremental
    movements will add up.
    
    See http://b/326468396#comment9 for videos of the existing behavior,
    the behavior with the bug fix, and also how centered magnifier looks on
    mac.
    
    Bug: b:326468396
    AX-Relnotes: Fixes magnifier bug at high zoom
    Test: New, manual with all magnifier modes to ensure edges reachable & behave correctly, and dragging still works across monitors
    Change-Id: Iaa5002a0faefdcf789abba5d2f4849e9ab65241e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5324756
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1266475}

--- ash/accessibility/magnifier/fullscreen_magnifier_controller.h
+++ ash/accessibility/magnifier/fullscreen_magnifier_controller.h
@@ -151,6 +151,7 @@ class ASH_EXPORT FullscreenMagnifierController
   }
 
  private:
+  friend class FullscreenMagnifierControllerTest;
   class GestureProviderClient;
 
   // ui::ImplicitAnimationObserver overrides:
@@ -208,7 +209,10 @@ class ASH_EXPORT FullscreenMagnifierController
                                   bool animate,
                                   bool ignore_mouse_change);
 
-  void OnMouseMove(const gfx::Point& location);
+  // Takes mouse root `location` in floating-point DIP. Note at higher zoom
+  // levels, the floating point values matter more, because the ratio of px to
+  // DIP increases.
+  void OnMouseMove(const gfx::PointF& location);
 
   // Move the mouse cursot to the given point. Actual move will be done when
   // the animation is completed. This should be called after animation is
