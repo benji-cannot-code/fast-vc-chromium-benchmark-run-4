b34390a502e02a3912b9d747493a98978881ea3b
Benjamin Zielinski <bzielinski@google.com>
2024-02-28 19:23:46 +0000

    Remove projector account switch notification flag
    This change cleans up the flag `kProjectorAccountSwitchNotification`
    which has been default enabled for a while.
    
    Bug: b/288623455
    Change-Id: I2aade3d169e4b23dca49e6cfc49a4dfe7e5de4d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5331273
    Auto-Submit: Benjamin Zielinski <bzielinski@google.com>
    Reviewed-by: Li Lin <llin@chromium.org>
    Commit-Queue: Li Lin <llin@chromium.org>
    Reviewed-by: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1266566}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2392,11 +2392,6 @@ BASE_FEATURE(kProjectorTranscriptChapterTitle,
              "ProjectorTranscriptChapterTitle",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-// Controls whether to show toast notification when account switches.
-BASE_FEATURE(kProjectorAccountSwitchNotification,
-             "ProjectorAccountSwitchNotification",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 // Controls whether to use USM for serverside speech recognition for projector.
 BASE_FEATURE(kProjectorUseUSMForS3,
              "ProjectorUseUSMForS3",
@@ -4185,10 +4180,6 @@ bool IsProjectorShowShortPseudoTranscript() {
 
 bool IsProjectorUpdateIndexableTextEnabled() {
   return base::FeatureList::IsEnabled(kProjectorUpdateIndexableText);
-}
-
-bool IsProjectorAccountSwitchNotificationEnabled() {
-  return base::FeatureList::IsEnabled(kProjectorAccountSwitchNotification);
 }
 
 bool IsProjectorServerSideRecognitionFallbackImplEnabled() {
