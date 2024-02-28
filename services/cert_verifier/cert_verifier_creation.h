6f76b449d6171b2c2120192033bd6f2bbe9538fd
Hubert Chao <hchao@chromium.org>
2024-02-28 21:40:53 +0000

    CertVeriferService: refactor CertVerifyProc::InstanceParam creation code to be shared in both the creation and update code paths.
    Bug: 40928765
    Change-Id: I0849287934cb54e0ed27593cea63b3b971c640a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5317912
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Bob Beck <bbe@chromium.org>
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266646}

--- services/cert_verifier/cert_verifier_creation.h
+++ services/cert_verifier/cert_verifier_creation.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/cert_verifier.h"
 #include "net/cert/cert_verify_proc.h"
 #include "services/cert_verifier/public/mojom/cert_verifier_service_factory.mojom.h"
+#include "services/network/public/mojom/cert_verifier_service_updater.mojom.h"
 
 // Set of utility functions to help with creation of CertVerifiers for
 // CertVerifyServiceFactory.
@@ -32,6 +33,12 @@ std::unique_ptr<net::CertVerifierWithUpdatableProc> CreateCertVerifier(
     scoped_refptr<net::CertNetFetcher> cert_net_fetcher,
     const net::CertVerifyProc::ImplParams& impl_params,
     const net::CertVerifyProc::InstanceParams& instance_params);
+
+// Update the |instance_params| for the verifier based on the contents of
+// |additional_certificates|.
+void UpdateCertVerifierInstanceParams(
+    const mojom::AdditionalCertificatesPtr& additional_certificates,
+    net::CertVerifyProc::InstanceParams* instance_params);
 
 }  // namespace cert_verifier
 
