578034c31f25e5a81f78b7c1333698ecb42a4a3e
Alexis Hetu <sugoi@chromium.org>
2024-02-28 16:54:02 +0000

    [iOSKAUpgrade] Add max pill width rules for suggestions
    This CL adds the maximum pill width rules, by suggestion type:
    - Password suggesions have no maximum width
    - Address suggestions have a max width of half screen width
    - Credit Card suggestions have a max width allowing the 2nd
      credit card's icon to be half visible
    
    Bug: 1501851
    Change-Id: I9c9eac9740e46508f33531cd8748a58214656277
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5302019
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266462}

--- ios/chrome/browser/ui/autofill/form_input_accessory/form_suggestion_label.h
+++ ios/chrome/browser/ui/autofill/form_input_accessory/form_suggestion_label.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (instancetype)initWithSuggestion:(FormSuggestion*)suggestion
                              index:(NSUInteger)index
                     numSuggestions:(NSUInteger)numSuggestions
+             accessoryTrailingView:(UIView*)accessoryTrailingView
                           delegate:(id<FormSuggestionLabelDelegate>)delegate
     NS_DESIGNATED_INITIALIZER;
 
--- ios/chrome/browser/ui/autofill/form_input_accessory/form_suggestion_view.h
+++ ios/chrome/browser/ui/autofill/form_input_accessory/form_suggestion_view.h
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Updates with `suggestions`.
 - (void)updateSuggestions:(NSArray<FormSuggestion*>*)suggestions
            showScrollHint:(BOOL)showScrollHint
+    accessoryTrailingView:(UIView*)trailingView
                completion:(void (^)(BOOL finished))completion;
 
 // Reset content insets back to zero and sets the delegate to nil. Used to stop
