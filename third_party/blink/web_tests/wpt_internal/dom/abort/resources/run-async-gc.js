e47397213f9a242d1ef3f32099c87252a51431d3
Scott Haseley <shaseley@chromium.org>
2024-02-28 17:25:52 +0000

    AbortSignal: Use native async gc() for GC tests
    Use native async gc() instead of postTask() + sync gc() in an attempt
    to deflake abort signal memory tests, which might help since it's
    stack-less.
    
    Bug: 326449175
    Change-Id: Ic95cc657aae84c0d5b0c2918f2154411cf378694
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5327503
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266481}

--- third_party/blink/web_tests/wpt_internal/dom/abort/resources/run-async-gc.js
+++ third_party/blink/web_tests/wpt_internal/dom/abort/resources/run-async-gc.js
@@ -1,12 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-async function runAsyncGC(args = {}) {
+async function runAsyncGC() {
   // Run gc in a loop to ensure anything needing more than one cycle can be
   // collected, e.g. due to dependencies. Note this is similar to
   // ThreadState::CollectAllGarbageForTesting, but async and with 2 less
   // iterations.
   for (let i = 0; i < 3; i++) {
-    // crbug.com/1474629: invoking gc({execution: 'async'}) trips leak
-    // detection, so use postTask and run sync gc() to do async GC.
-    await scheduler.postTask(() => { gc(); }, args);
+    await gc({type: 'major', execution: 'async', flavor: 'last-resort'});
   }
 }
