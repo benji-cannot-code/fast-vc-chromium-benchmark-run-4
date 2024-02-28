b4647a026633876ad39f689a5a7acb7b8921a951
Georg Neis <neis@chromium.org>
2024-02-28 23:46:05 +0000

    Fix a crash in GetUserAffiliationStatus
    Calls to BrowserDMTokenStorage such as
      policy::BrowserDMTokenStorage::Get()->RetrieveDMToken()
    are no longer valid on Lacros since the removal of CBCM
    (see referenced bug for additional context).
    
    Avoid such a call in GetUserAffiliationStatus by using
    policy::GetDMToken instead, which does the right thing on Lacros.
    This fixes a crash in chrome://policy for secondary profiles.
    Also add a regression test for that.
    
    I could not find any other problematic calls.
    
    Bug: b/326630783
    Change-Id: I3f8155ede69b5ae31f2db5211a6c6fdf50d398e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5330386
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Igor <igorcov@chromium.org>
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266725}

--- chrome/browser/policy/status_provider/status_provider_util.cc
+++ chrome/browser/policy/status_provider/status_provider_util.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_manager/user_manager.h"
 #else
 #include "chrome/browser/enterprise/util/affiliation.h"
-#include "components/enterprise/browser/controller/browser_dm_token_storage.h"
+#include "chrome/browser/policy/dm_token_utils.h"
 #endif
 
 #if BUILDFLAG(IS_CHROMEOS)
@@ -58,8 +58,9 @@ void GetUserAffiliationStatus(base::Value::Dict* dict, Profile* profile) {
   if (!profile->IsMainProfile())
 #endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
   {
-    if (!policy::BrowserDMTokenStorage::Get()->RetrieveDMToken().is_valid())
+    if (!policy::GetDMToken(profile).is_valid()) {
       return;
+    }
   }
   dict->Set("isAffiliated",
             chrome::enterprise_util::IsProfileAffiliated(profile));
