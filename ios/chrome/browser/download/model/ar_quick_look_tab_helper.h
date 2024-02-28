cb53450121fcb0d089d889d230c6f6ffad443032
Joemer Ramos <joemerramos@chromium.org>
2024-02-28 19:54:15 +0000

    [iOS] Make ARQuickLookTabHelper Inherit LazyWebStateUserData
    This CL refactors ARQuickLookTabHelper to use LazyWebStateUserData
    instead of WebStateUserData. This also makes use of the new method
    GetOrCreateForWebState which is used as a replacement to FromWebState
    or CreateWebState. The main difference is that when
    kEnableStartupImprovements is enabled, ARQuickLookTabHelper isn't
    attached in AttachTabHelpers and is instead attached when a WebState
    needs to add it as a dependency.
    
    Bug: 327166055
    
    Change-Id: I944d9b8a9af6d21c0d78e4efab5e3aa0c8eeffc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5332654
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266581}

--- ios/chrome/browser/download/model/ar_quick_look_tab_helper.h
+++ ios/chrome/browser/download/model/ar_quick_look_tab_helper.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "ios/web/public/download/download_task_observer.h"
-#import "ios/web/public/web_state_user_data.h"
+#import "ios/web/public/lazy_web_state_user_data.h"
 
 @protocol ARQuickLookTabHelperDelegate;
 
@@ -51,7 +51,7 @@ enum class IOSDownloadARModelState {
 class ARQuickLookTabHelper
     : public base::SupportsWeakPtr<ARQuickLookTabHelper>,
       public web::DownloadTaskObserver,
-      public web::WebStateUserData<ARQuickLookTabHelper> {
+      public web::LazyWebStateUserData<ARQuickLookTabHelper> {
  public:
   ARQuickLookTabHelper(const ARQuickLookTabHelper&) = delete;
   ARQuickLookTabHelper& operator=(const ARQuickLookTabHelper&) = delete;
@@ -73,7 +73,7 @@ class ARQuickLookTabHelper
   explicit ARQuickLookTabHelper(web::WebState* web_state);
 
  private:
-  friend class web::WebStateUserData<ARQuickLookTabHelper>;
+  friend class web::LazyWebStateUserData<ARQuickLookTabHelper>;
 
   // Previews the downloaded file given by current download task.
   void DidFinishDownload();
--- ios/chrome/browser/download/model/browser_download_service_unittest.mm
+++ ios/chrome/browser/download/model/browser_download_service_unittest.mm
@@ -116,7 +116,7 @@ class BrowserDownloadServiceTest : public PlatformTest {
 
   StubTabHelper<ARQuickLookTabHelper>* ar_quick_look_tab_helper() {
     return static_cast<StubTabHelper<ARQuickLookTabHelper>*>(
-        ARQuickLookTabHelper::FromWebState(&web_state_));
+        ARQuickLookTabHelper::GetOrCreateForWebState(&web_state_));
   }
 
   StubTabHelper<VcardTabHelper>* vcard_tab_helper() {
