75946fcc3f4f90ab36b7e0e347978cf20e09fa96
Chad Duffin <chadduffin@chromium.org>
2024-02-28 23:50:22 +0000

    cellular: Refresh installed profiles
    This change updates the "add eSIM" dialog to refresh the installed
    profiles immediately when opened. This preserves the behavior of the
    dialog when the SM-DS Support feature flag is disabled; this discrepancy
    arose after we made the dialog require manual consent to perform an
    SM-DS scan, and the previous SM-DS scan internally refreshed the
    installed profile list.
    
    Manual test steps:
    + Open "add eSIM" dialog
    + Observe we immediately start refreshing profile list
    + Skip to manual entry
    + Enter eSIM profile information
    + Begin installation before refreshing finishes
    + Observe that we continue refreshing profile list, and afterwards begin
      installing the eSIM profile
    
    Bug: b/326163082
    Test: Manually on device and unit tests.
    Change-Id: I99ce51ea434d6de74a806279e210844d6764c684
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5325139
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Chris Bookholt <bookholt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266728}

--- chrome/test/data/webui/cr_components/chromeos/cellular_setup/fake_esim_manager_remote.ts
+++ chrome/test/data/webui/cr_components/chromeos/cellular_setup/fake_esim_manager_remote.ts
@@ -148,6 +148,8 @@ export class FakeEuicc implements EuiccInterface {
   properties: EuiccProperties;
   private fakeESimManager_: FakeESimManagerRemote;
   private profiles_: FakeProfile[];
+  private refreshInstalledProfilesResult_: ESimOperationResult;
+  private refreshInstalledProfilesCount_: number = 0;
   private requestPendingProfilesResult_: ESimOperationResult;
   private eidQRCode_: QRCode|null = null;
   private profileInstallResult_: ProfileInstallResult|null = null;
@@ -159,6 +161,7 @@ export class FakeEuicc implements EuiccInterface {
     for (let i = 0; i < numProfiles; i++) {
       this.addProfile();
     }
+    this.refreshInstalledProfilesResult_ = ESimOperationResult.kSuccess;
     this.requestPendingProfilesResult_ = ESimOperationResult.kSuccess;
   }
 
@@ -167,6 +170,8 @@ export class FakeEuicc implements EuiccInterface {
   }
 
   requestPendingProfiles(): Promise<{result:ESimOperationResult}> {
+    // Requesting pending profiles refreshes the installed profile list.
+    this.refreshInstalledProfilesCount_++;
     return Promise.resolve({
       result: this.requestPendingProfilesResult_,
     });
@@ -179,6 +184,13 @@ export class FakeEuicc implements EuiccInterface {
       profiles: this.profiles_.map(profile => {
         return profile.properties;
       }),
+    });
+  }
+
+  refreshInstalledProfiles(): Promise<{result: ESimOperationResult}> {
+    this.refreshInstalledProfilesCount_++;
+    return Promise.resolve({
+      result: this.refreshInstalledProfilesResult_,
     });
   }
 
@@ -195,6 +207,10 @@ export class FakeEuicc implements EuiccInterface {
     } else {
       return Promise.resolve({qrCode: null});
     }
+  }
+
+  getRefreshInstalledProfilesCount(): number {
+    return this.refreshInstalledProfilesCount_;
   }
 
   installProfileFromActivationCode(
