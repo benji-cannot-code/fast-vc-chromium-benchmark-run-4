c41259ff67b17ae12f8c987f6d9777f249b25124
Ahmed Mehfooz <amehfooz@chromium.org>
2024-02-28 23:24:26 +0000

    Add support to expand/collapse NotificationActionsView
    Bug: b:325555641
    Change-Id: I33a038a1d8c6ed5b2f93d8b4123aa8fba0a50e36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5333340
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266713}

--- ash/system/notification_center/views/conversation_notification_view.cc
+++ ash/system/notification_center/views/conversation_notification_view.cc
@@ -78,6 +78,9 @@ void ConversationNotificationView::ToggleExpand() {
   expanded_ = !expanded_;
   conversations_container_->SetVisible(expanded_);
   collapsed_preview_container_->SetVisible(!expanded_);
+  expand_button_->SetExpanded(expanded_);
+  actions_view_->SetExpanded(expanded_);
+
   PreferredSizeChanged();
 }
 
@@ -99,6 +102,7 @@ void ConversationNotificationView::UpdateWithNotification(
   UpdateControlButtonsVisibilityWithNotification(notification);
 
   actions_view_->UpdateWithNotification(notification);
+  actions_view_->SetExpanded(expanded_);
 
   if (notification.items().empty()) {
     return;
@@ -196,7 +200,8 @@ ConversationNotificationView::CreateRightControlsContainer() {
   expand_button->SetCallback(base::BindRepeating(
       &ConversationNotificationView::ToggleExpand, base::Unretained(this)));
 
-  expand_button_container->AddChildView(std::move(expand_button));
+  expand_button_ =
+      expand_button_container->AddChildView(std::move(expand_button));
 
   auto view =
       std::make_unique<message_center::NotificationControlButtonsView>(this);
--- ash/system/notification_center/views/conversation_notification_view_unittest.cc
+++ ash/system/notification_center/views/conversation_notification_view_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/notification_center/views/conversation_notification_view.h"
 
+#include "ash/system/notification_center/views/notification_actions_view.h"
 #include "ash/test/ash_test_base.h"
 #include "ui/events/test/test_event.h"
 #include "ui/message_center/message_center.h"
@@ -47,7 +48,11 @@ class ConversationNotificationViewTest : public AshTestBase {
     rich_data.items = items;
     rich_data.settings_button_handler =
         message_center::SettingsButtonHandler::INLINE;
-
+    auto reply_button = message_center::ButtonInfo(u"Reply");
+    reply_button.placeholder = std::make_optional(u"Placeholder");
+    std::vector<message_center::ButtonInfo> buttons;
+    buttons.push_back(reply_button);
+    rich_data.buttons = buttons;
     return std::make_unique<message_center::Notification>(
         message_center::NOTIFICATION_TYPE_SIMPLE, "id", u"title",
         u"test message", ui::ImageModel(), /*display_source=*/std::u16string(),
@@ -57,6 +62,8 @@ class ConversationNotificationViewTest : public AshTestBase {
   ConversationNotificationView* notification_view() {
     return notification_view_.get();
   }
+
+  views::View* actions_view() { return notification_view_->actions_view_; }
 
   views::View* collapsed_preview_container() {
     return notification_view_->collapsed_preview_container_;
@@ -135,4 +142,17 @@ TEST_F(ConversationNotificationViewTest, ToggleInlineSettings) {
   EXPECT_TRUE(right_controls_container()->GetVisible());
 }
 
+TEST_F(ConversationNotificationViewTest, ActionsViewToggleExpandVisibility) {
+  ASSERT_EQ(actions_view()->GetVisible(), notification_view()->IsExpanded());
+
+  notification_view()->ToggleExpand();
+
+  EXPECT_FALSE(notification_view()->IsExpanded());
+  EXPECT_FALSE(actions_view()->GetVisible());
+
+  notification_view()->ToggleExpand();
+
+  EXPECT_TRUE(notification_view()->IsExpanded());
+  EXPECT_TRUE(actions_view()->GetVisible());
+}
 }  // namespace ash
