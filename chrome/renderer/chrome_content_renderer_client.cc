d1560ba4e42381238f9fb103a1170a21198f478b
Seth Brenith <seth.brenith@microsoft.com>
2024-02-28 16:48:42 +0000

    Start Isolate in foreground mode for mimehandler processes
    In Edge, we've seen a regression in the time to load a PDF document when
    the feature V8EfficiencyModeTiering is enabled. I'm not sure how much
    code is shared between Chrome and Edge regarding PDF rendering, but I
    believe at least the overall architecture matches: there is an extension
    renderer process (the "mimehandler") which needs to run some script.
    This renderer process is initializing its V8 Isolate in background mode,
    which causes a slower overall load. As described at [1], extension
    processes start their Isolates in background mode because they might not
    have any way to detect visibility. However, the PDF mimehandler process
    does in fact get notified when its visibility changes, so it can safely
    start up in foreground mode.
    
    In this CL, I propose adding another command-line switch so that the
    browser process can indicate that the renderer should start its Isolate
    in foreground mode.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/bindings/core/v8/v8_initializer.cc;drc=7477a270035df963ba48d3f7ea4d8b384bce770c;l=951
    
    Change-Id: Ibedb3fd8e80168e515394804805a5b7fcf44ba49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5304236
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Seth Brenith <seth.brenith@microsoft.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266461}

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -1406,7 +1406,9 @@ void ChromeContentRendererClient::PostCompositorThreadCreated(
 }
 
 bool ChromeContentRendererClient::RunIdleHandlerWhenWidgetsHidden() {
-  return !IsStandaloneContentExtensionProcess();
+  return base::CommandLine::ForCurrentProcess()->HasSwitch(
+             switches::kInitIsolateAsForeground) ||
+         !IsStandaloneContentExtensionProcess();
 }
 
 bool ChromeContentRendererClient::AllowPopup() {
