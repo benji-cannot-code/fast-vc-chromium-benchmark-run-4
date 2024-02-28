fc2dbf6f2f82667665c443f9dd4c8b9d2cf086a3
Alexis Hetu <sugoi@chromium.org>
2024-02-28 15:09:30 +0000

    [iOS] New suggestions format for credit cards
    This CL proposes slight modifications to the suggestions
    format to add a bit of extra information when the screen
    real estate allows it.
    - Credit cards always show whether the card is virtual or
      adds the expiration date if it isn't already shown.
    - When showing credit card expiry data (month and/or year),
      always show the full date instead.
    
    Design doc:
    http://go/ios-keyboard-accessory
    
    Link to the desired format changes to suggestions:
    http://go/ios-keyboard-accessory#heading=h.p68giwzh5cl0
    
    Bug: 1501851
    Change-Id: Icf41640ec1b8899c0bd4d6666ec266f3992a165b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5272976
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266411}

--- ios/chrome/browser/ui/autofill/chrome_autofill_client_ios.mm
+++ ios/chrome/browser/ui/autofill/chrome_autofill_client_ios.mm
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/plus_addresses/model/plus_address_service_factory.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/public/commands/autofill_commands.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/signin/model/authentication_service.h"
 #import "ios/chrome/browser/signin/model/authentication_service_factory.h"
 #import "ios/chrome/browser/signin/model/identity_manager_factory.h"
@@ -570,6 +571,10 @@ LogManager* ChromeAutofillClientIOS::GetLogManager() const {
 
 bool ChromeAutofillClientIOS::IsLastQueriedField(FieldGlobalId field_id) {
   return [bridge_ isLastQueriedField:field_id];
+}
+
+bool ChromeAutofillClientIOS::ShouldFormatForLargeKeyboardAccessory() const {
+  return IsKeyboardAccessoryUpgradeEnabled();
 }
 
 std::unique_ptr<device_reauth::DeviceAuthenticator>
