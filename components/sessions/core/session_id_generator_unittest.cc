4f7b70342aa2cdd2e882498493e0e7ab244f547d
Gauthier Ambard <gambard@chromium.org>
2024-02-28 17:49:58 +0000

    Add histogram when updating the pref backing the SessionID
    When SetHighestRestoredID() is called, record by how much the
    local pref was incremented. Record 0 if it wasn't updated.
    
    Bug: 327200695
    Change-Id: I423cbdc7d551cf185de2f93ccff3d1ebb25975ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5328287
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266505}

--- components/sessions/core/session_id_generator_unittest.cc
+++ components/sessions/core/session_id_generator_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <limits>
 
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/mock_callback.h"
 #include "components/prefs/testing_pref_service.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -155,6 +156,9 @@ TEST_F(SessionIdGeneratorTest, ShouldHandleOverflowDuringPadding) {
 }
 
 TEST_F(SessionIdGeneratorTest, HighestRestoredID) {
+  std::string histogram("Session.ID.RestoredDifference");
+  base::HistogramTester tester;
+
   base::MockCallback<SessionIdGenerator::RandomGenerator> random_generator;
   SessionIdGenerator* generator = SessionIdGenerator::GetInstance();
   generator->SetRandomGeneratorForTest(random_generator.Get());
@@ -167,6 +171,8 @@ TEST_F(SessionIdGeneratorTest, HighestRestoredID) {
   generator->SetHighestRestoredID(highest);
 
   EXPECT_EQ(123 + 1 + kExpectedIdPadding, generator->NewUnique().id());
+  tester.ExpectBucketCount(histogram, 0, 1);
+  tester.ExpectTotalCount(histogram, 1);
 
   // Highest restored ID is higher than the next value.
   generator->Shutdown();
@@ -178,6 +184,8 @@ TEST_F(SessionIdGeneratorTest, HighestRestoredID) {
   generator->SetHighestRestoredID(highest);
 
   EXPECT_EQ(201, generator->NewUnique().id());
+  tester.ExpectBucketCount(histogram, 27, 1);
+  tester.ExpectTotalCount(histogram, 2);
 
   // Highest restored ID is higher than the next value because it overflown.
   generator->Shutdown();
@@ -192,6 +200,8 @@ TEST_F(SessionIdGeneratorTest, HighestRestoredID) {
   generator->SetHighestRestoredID(highest);
 
   EXPECT_EQ(123 + 1 + kExpectedIdPadding, generator->NewUnique().id());
+  tester.ExpectBucketCount(histogram, 0, 2);
+  tester.ExpectTotalCount(histogram, 3);
 }
 
 // Verifies correctness of the test-only codepath.
