db2ba4996e8763dfc9e4bbff9646a6541e7292c1
Gauthier Ambard <gambard@chromium.org>
2024-02-28 14:24:17 +0000

    [iOS] Update colors in TabGroup
    This CL makes sure that the TabGroup screen is using the right
    color now that Dark mode is forced.
    
    Bug: 40942154
    Change-Id: I6cb7a65fa1a4f7ce24ed5e1f749577e2eb0dcbd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5331772
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266390}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_cell.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_cell.mm
@@ -198,6 +198,19 @@ void PositionView(UIView* view, CGPoint point) {
   }
 }
 
+- (void)didMoveToWindow {
+  if (self.window) {
+    if (self.theme == GridThemeLight) {
+      UIUserInterfaceStyle previousStyle =
+          self.window.overrideUserInterfaceStyle;
+      self.window.overrideUserInterfaceStyle = UIUserInterfaceStyleUnspecified;
+      self.overrideUserInterfaceStyle =
+          self.window.traitCollection.userInterfaceStyle;
+      self.window.overrideUserInterfaceStyle = previousStyle;
+    }
+  }
+}
+
 #pragma mark - UICollectionViewCell
 
 - (void)setHighlighted:(BOOL)highlighted {
@@ -266,19 +279,6 @@ void PositionView(UIView* view, CGPoint point) {
   }
 
   _theme = theme;
-}
-
-- (void)didMoveToWindow {
-  if (self.window) {
-    if (self.theme == GridThemeLight) {
-      UIUserInterfaceStyle previousStyle =
-          self.window.overrideUserInterfaceStyle;
-      self.window.overrideUserInterfaceStyle = UIUserInterfaceStyleUnspecified;
-      self.overrideUserInterfaceStyle =
-          self.window.traitCollection.userInterfaceStyle;
-      self.window.overrideUserInterfaceStyle = previousStyle;
-    }
-  }
 }
 
 - (void)setIcon:(UIImage*)icon {
