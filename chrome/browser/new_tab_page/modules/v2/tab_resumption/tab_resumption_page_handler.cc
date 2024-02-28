47499f6a5bb34b150e8fd96d99b4f974fe8230fa
Marlon Facey <mfacey@chromium.org>
2024-02-28 20:46:35 +0000

    [NTP XDTR] Update seconds text to be recently opened and computer icon.
    "X secs ago" is rarely accurate. Switch to "Recently opened" instead.
    Also switch computer icon.
    
    Screenshot: http://screenshot/BNzV7MqTF97E8k9
    
    Bug: 326292806, 327232795
    Change-Id: I644273a9b5d5f2df46cdf0564fb9fe6d0143a542
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5323349
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266609}

--- chrome/browser/new_tab_page/modules/v2/tab_resumption/tab_resumption_page_handler.cc
+++ chrome/browser/new_tab_page/modules/v2/tab_resumption/tab_resumption_page_handler.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ntp/new_tab_ui.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
+#include "chrome/grit/generated_resources.h"
 #include "components/history/core/browser/history_types.h"
 #include "components/history/core/browser/mojom/history_types.mojom.h"
 #include "components/prefs/scoped_user_pref_update.h"
@@ -86,9 +87,15 @@ history::mojom::TabPtr SessionTabToMojom(
   tab_mojom->url = GURL(*dictionary.FindString("url"));
   tab_mojom->title = *dictionary.FindString("title");
 
-  tab_mojom->relative_time_text =
-      base::UTF16ToUTF8(FormatRelativeTime(tab.timestamp));
-  tab_mojom->relative_time = base::Time::Now() - tab.timestamp;
+  auto relative_time = base::Time::Now() - tab.timestamp;
+  tab_mojom->relative_time = relative_time;
+  if (relative_time.InSeconds() < 60) {
+    tab_mojom->relative_time_text = l10n_util::GetStringUTF8(
+        IDS_NTP_MODULES_TAB_RESUMPTION_RECENTLY_OPENED);
+  } else {
+    tab_mojom->relative_time_text =
+        base::UTF16ToUTF8(FormatRelativeTime(tab.timestamp));
+  }
 
   return tab_mojom;
 }
