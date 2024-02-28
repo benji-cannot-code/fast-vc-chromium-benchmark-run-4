767359daf860a9350da0d6afd1c2b53b32df7b88
Jiaming Cheng <jiamingc@google.com>
2024-02-28 23:53:44 +0000

    Move the kBackgroundListening flag to media/base layer
    , so it can be used under both ash/ and component/.
    
    (Most of the media player ui is under component/)
    
    Bug: b/316230031
    Change-Id: I7d4334b778413d8b4be0b764cfed68278f09dc62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5329930
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Jiaming Cheng <jiamingc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266731}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -249,11 +249,6 @@ BASE_FEATURE(kAutozoomNudgeSessionReset,
              "AutozoomNudgeSessionReset",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-// Enables the new media player features.
-BASE_FEATURE(kBackgroundListening,
-             "BackgroundListening",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 // Make Battery Saver available.
 BASE_FEATURE(kBatterySaver,
              "CrosBatterySaver",
@@ -3179,10 +3174,6 @@ bool IsBackgroundBlurEnabled() {
 #else
   return enabled_by_feature_flag;
 #endif
-}
-
-bool IsBackgroundListeningEnabled() {
-  return base::FeatureList::IsEnabled(kBackgroundListening);
 }
 
 bool IsBatterySaverAvailable() {
