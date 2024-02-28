098aed89632100e8fac8b282daba8538117fe6e6
Kush Sinha <sinhak@chromium.org>
2024-02-28 17:56:46 +0000

    QuickStart: Extract common test function in SDAuthBroker
    Extract a common function in SecondDeviceAuthBroker unit tests to
    produce rejection responses from Gaia.
    
    This will be used in follow up CLs to add better error responses for
    Unicorn accounts.
    
    Bug: b/291086297
    Test: unit_tests --gtest_filter="*SecondDeviceAuthBrokerTest*"
    Change-Id: I9ab431a313554a652986a899459c4d92bb17f834
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5330969
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1266511}

--- chrome/browser/ash/login/oobe_quick_start/second_device_auth_broker_unittest.cc
+++ chrome/browser/ash/login/oobe_quick_start/second_device_auth_broker_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/json/json_reader.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/strings/stringprintf.h"
 #include "base/test/bind.h"
 #include "base/test/gmock_expected_support.h"
 #include "base/test/gtest_util.h"
@@ -272,6 +273,30 @@ class SecondDeviceAuthBrokerTest : public ::testing::Test {
                        const std::string& response,
                        net::HttpStatusCode status = net::HTTP_OK) {
     test_factory_.AddResponse(url, response, status);
+  }
+
+  void AddFakeRejectionResponse(const std::string& email,
+                                const std::string& rejection_reason) {
+    // clang-format off
+    AddFakeResponse(kStartSessionUrl, base::StringPrintf(R"(
+        {
+          "sessionStatus": "REJECTED",
+          "email": "%s",
+          "rejectionReason": "%s"
+        }
+      )", email.c_str(), rejection_reason.c_str()));
+    // clang-format on
+  }
+
+  void AddFakeRejectionResponse(const std::string& rejection_reason) {
+    // clang-format off
+    AddFakeResponse(kStartSessionUrl, base::StringPrintf(R"(
+        {
+          "sessionStatus": "REJECTED",
+          "rejectionReason": "%s"
+        }
+      )", rejection_reason.c_str()));
+    // clang-format on
   }
 
   // Sets an `interceptor`. Overwrites any other interceptor that may have been
@@ -770,15 +795,10 @@ TEST_F(SecondDeviceAuthBrokerTest,
 // they will consider it to be a "less_secure_device" and reject the request.
 TEST_F(SecondDeviceAuthBrokerTest,
        FetchAuthCodeReturnsRejectionResponseForLessSecureDevices) {
-  AddFakeResponse(kStartSessionUrl, std::string(R"(
-      {
-        "sessionStatus": "REJECTED",
-        "email": "fake-user@example.com",
-        "rejectionReason": "LESS_SECURE_DEVICE"
-      }
-    )"));
+  const std::string email = "fake-user@example.com";
+  AddFakeRejectionResponse(email, /*rejection_reason=*/"LESS_SECURE_DEVICE");
   AuthCodeRejectionResponse expected_response;
-  expected_response.email = "fake-user@example.com";
+  expected_response.email = email;
   expected_response.reason =
       AuthCodeRejectionResponse::Reason::kLessSecureDevice;
   SecondDeviceAuthBroker::AuthCodeResponse response =
@@ -809,12 +829,7 @@ TEST_F(
 TEST_F(
     SecondDeviceAuthBrokerTest,
     FetchAuthCodeReturnsRejectionResponseWithUnknownReasonForMalformedRejectionReason) {
-  AddFakeResponse(kStartSessionUrl, std::string(R"(
-      {
-        "sessionStatus": "REJECTED",
-        "rejectionReason": "Malformed rejection reason"
-      }
-    )"));
+  AddFakeRejectionResponse(/*rejection_reason=*/"Malformed rejection reason");
   AuthCodeRejectionResponse expected_response{
       .reason = AuthCodeRejectionResponse::Reason::kUnknownReason};
   SecondDeviceAuthBroker::AuthCodeResponse response =
