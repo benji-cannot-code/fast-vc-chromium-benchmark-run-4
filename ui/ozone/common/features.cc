922deb6cfec53ff603273d61f0af48c3065a0bb4
Jiahe Zhang <jiahe.zhang@intel.com>
2024-02-28 03:46:24 +0000

    Use dynamic cursor size on Intel GPUs
    There's a separate cursor plane on Intel GPUs, which supports various
    buffer sizes with some fixed widths. According to Intel GPU spec and
    i915 driver code, Intel GPUs support cursor buffer with width of 64,
    128 or 256 for the cursor plane [1].
    
    Currently Chromium uses the default max buffer size supported
    (256x256), while most of the cursor bitmap are smaller than 64x64 (e.g.
    32x32 for the default cursor style).
    
    Experiments show that using smaller cursor size can reduce power
    consumption. On an ADL chromebook, using 64x64 cursor instead of the
    default shows ~13% package power gain for video playback and ~0.8% for
    video conferencing (Meet 1:1).
    
    This CL introduces a mechanism that can dynamically change the buffer
    size for cursor plane based on the input cursor bitmap.
    
    [1] https://github.com/torvalds/linux/blob/v6.8-rc1/drivers/gpu/drm/i915/display/intel_cursor.c#L367
    
    Bug: 326156501
    
    Change-Id: Idf55fb159909c721807abc8313824d8ab6e2b26a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5271574
    Commit-Queue: Jiahe Zhang <jiahe.zhang@intel.com>
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266237}

--- ui/ozone/common/features.cc
+++ ui/ozone/common/features.cc
@@ -44,6 +44,12 @@ BASE_FEATURE(kPrettyPrintDrmModesetConfigLogs,
              "PrettyPrintDrmModesetConfigLogs",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// If enabled, Chromium will try to use the smallest valid size for the cursor
+// plane that fits the cursor bitmap.
+BASE_FEATURE(kUseDynamicCursorSize,
+             "UseDynamicCursorSize",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 bool IsWaylandSurfaceSubmissionInPixelCoordinatesEnabled() {
   return base::FeatureList::IsEnabled(
       kWaylandSurfaceSubmissionInPixelCoordinates);
@@ -59,6 +65,10 @@ bool IsWaylandFractionalScaleV1Enabled() {
 
 bool IsPrettyPrintDrmModesetConfigLogsEnabled() {
   return base::FeatureList::IsEnabled(kPrettyPrintDrmModesetConfigLogs);
+}
+
+bool IsUseDynamicCursorSizeEnabled() {
+  return base::FeatureList::IsEnabled(kUseDynamicCursorSize);
 }
 
 }  // namespace ui
