ec693f9958b0c3f7283a7a2c079b1ac290028f40
Fred Shih <ffred@chromium.org>
2024-02-29 21:46:40 +0000

    Fix incorrect boolean array size calculation
    The '+' operator has higher precedent than the '>>' operator:
    
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_precedence#table
    
    This means that the computation:
    
    headerSize + (value.length + 7) >> 3
    
    will end up dividing headerSize by 8 as well. This results in an error:
    
    "RangeError: Offset is outside the bounds of the DataView"
    
    Change-Id: Id9b176d6495581851cab80f863da974bab3a4e4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5332847
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Fred Shih <ffred@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267225}

--- content/web_test/browser/mojo_echo.cc
+++ content/web_test/browser/mojo_echo.cc
@@ -182,4 +182,9 @@ void MojoEcho::EchoStringAsOptionalNestedUnion(
       mojom::TestUnion::NewStringValue(value)));
 }
 
+void MojoEcho::EchoBoolArray(const std::vector<bool>& values,
+                             EchoBoolArrayCallback callback) {
+  std::move(callback).Run(values);
+}
+
 }  // namespace content
--- content/web_test/browser/mojo_echo.h
+++ content/web_test/browser/mojo_echo.h
@@ -91,6 +91,9 @@ class MojoEcho : public mojom::MojoEcho {
   void EchoStringAsOptionalNestedUnion(
       const std::string& value,
       EchoStringAsOptionalNestedUnionCallback callback) override;
+
+  void EchoBoolArray(const std::vector<bool>& values,
+                     EchoBoolArrayCallback callback) override;
 };
 
 }  // namespace content
