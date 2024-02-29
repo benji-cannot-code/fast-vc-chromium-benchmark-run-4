657eaea1e035d2d2f0c2fa9e41cc3580cd49fcee
Elmehdi Rahmaoui <erahmaoui@google.com>
2024-02-29 15:16:38 +0000

    [iOS] Create group items from web state list
    The aim of this CL is to add groups handling when creating items from a
    webStateList.
    
    Bug: 1501837
    Change-Id: Ia30bf13ab0fb45bc11def625a3f52048304ae70b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5332472
    Reviewed-by: Louis Romero <lpromero@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1266993}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_utils_unittest.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_utils_unittest.mm
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/memory/raw_ptr.h"
 #import "base/numerics/safe_conversions.h"
+#import "components/tab_groups/tab_group_color.h"
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
 #import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/browser/shared/model/web_state_list/tab_group.h"
 #import "ios/chrome/browser/shared/model/web_state_list/test/fake_web_state_list_delegate.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_opener.h"
@@ -18,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/test/web_task_environment.h"
 #import "testing/platform_test.h"
 #import "ui/base/device_form_factor.h"
+
+using tab_groups::TabGroupVisualData;
 
 class GridUtilsTest : public PlatformTest {
  public:
@@ -90,4 +94,32 @@ TEST_F(GridUtilsTest, CreateValidItemsListWithoutPinnedTabs) {
     EXPECT_EQ(web_state->GetUniqueIdentifier(),
               itemsList[i].tabSwitcherItem.identifier);
   }
+}
+
+// Test that `-CreateItems` handles the creation of different item types (groups
+// and tabs) when the `web_state_list_` contains groups.
+TEST_F(GridUtilsTest, CreateItemsListWithGroup) {
+  for (int i = 0; i < 10; i++) {
+    AddWebState();
+  }
+  TabGroupVisualData visual_data_a =
+      TabGroupVisualData(u"Group A", tab_groups::TabGroupColorId::kGrey);
+  TabGroupVisualData visual_data_b =
+      TabGroupVisualData(u"Group B", tab_groups::TabGroupColorId::kRed);
+
+  web_state_list_->CreateGroup({0, 1, 2}, visual_data_a);
+  web_state_list_->CreateGroup({5, 6}, visual_data_b);
+
+  NSArray<GridItemIdentifier*>* itemsList = CreateItems(web_state_list_);
+
+  // The number of items should be equal to 7, 2 groups ({0, 1, 2} and {5, 6})
+  // and 5 web states({3,4,7,8,9}).
+  ASSERT_EQ(7, (int)[itemsList count]);
+  EXPECT_EQ(GridItemType::Group, itemsList[0].type);
+  EXPECT_EQ(GridItemType::Tab, itemsList[1].type);
+  EXPECT_EQ(GridItemType::Tab, itemsList[2].type);
+  EXPECT_EQ(GridItemType::Group, itemsList[3].type);
+  EXPECT_EQ(GridItemType::Tab, itemsList[4].type);
+  EXPECT_EQ(GridItemType::Tab, itemsList[5].type);
+  EXPECT_EQ(GridItemType::Tab, itemsList[6].type);
 }
