8529743eb19697d09acf3b2aa8815be8715f7c62
Zauri Meshveliani <zauri@google.com>
2024-02-29 17:38:56 +0000

    privacy-hub: Tie Play Store reinstall location ToS to PH pref
    Privacy Hub project introduces the system location toggle for ChroemOS,
    which deprecates the ARC-specific location toggle. ARC toggle reflects
    the state of the ChromeOS location toggle.
    
    When user reinstalls the Play Store they need to configure the default
    location state for it. As there's no such thing as ARC-specific
    location, users will now configure the ChromeOS location state instead.
    
    Update ToS text to include ChromeOS and tie the location checkbox to
    `kUserGeolocationAccessLevel`.
    
    Change-Id: I2b0055831dbfa1ec9cdd18dfd4a853b492dbe44a
    Bug: b:326401829
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5327266
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Auto-Submit: Zauri Meshveliani <zauri@chromium.org>
    Commit-Queue: Zauri Meshveliani <zauri@chromium.org>
    Commit-Queue: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267076}

--- chrome/browser/ash/arc/arc_support_host.cc
+++ chrome/browser/ash/arc/arc_support_host.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "ash/constants/ash_features.h"
 #include "base/functional/bind.h"
 #include "base/hash/sha1.h"
 #include "base/i18n/timezone.h"
@@ -567,10 +568,17 @@ bool ArcSupportHost::Initialize() {
   loadtime_data.Set(
       "textGoogleServiceConfirmation",
       l10n_util::GetStringUTF16(IDS_ARC_OPT_IN_GOOGLE_SERVICE_CONFIRMATION));
-  loadtime_data.Set(
-      "textLocationService",
-      l10n_util::GetStringUTF16(is_child ? IDS_ARC_OPT_IN_LOCATION_SETTING_CHILD
-                                         : IDS_ARC_OPT_IN_LOCATION_SETTING));
+  if (ash::features::IsCrosPrivacyHubLocationEnabled()) {
+    loadtime_data.Set("textLocationService",
+                      l10n_util::GetStringUTF16(
+                          is_child ? IDS_CROS_OPT_IN_LOCATION_SETTING_CHILD
+                                   : IDS_CROS_OPT_IN_LOCATION_SETTING));
+  } else {
+    loadtime_data.Set("textLocationService",
+                      l10n_util::GetStringUTF16(
+                          is_child ? IDS_ARC_OPT_IN_LOCATION_SETTING_CHILD
+                                   : IDS_ARC_OPT_IN_LOCATION_SETTING));
+  }
   loadtime_data.Set("serverError", l10n_util::GetStringUTF16(
                                        IDS_ARC_SERVER_COMMUNICATION_ERROR));
   loadtime_data.Set("controlledByPolicy",
@@ -593,11 +601,19 @@ bool ArcSupportHost::Initialize() {
   loadtime_data.Set("learnMoreLocationServicesTitle",
                     l10n_util::GetStringUTF16(
                         IDS_ARC_OPT_IN_LEARN_MORE_LOCATION_SERVICES_TITLE));
-  loadtime_data.Set(
-      "learnMoreLocationServices",
-      l10n_util::GetStringUTF16(
-          is_child ? IDS_ARC_OPT_IN_LEARN_MORE_LOCATION_SERVICES_CHILD
-                   : IDS_ARC_OPT_IN_LEARN_MORE_LOCATION_SERVICES));
+  if (ash::features::IsCrosPrivacyHubLocationEnabled()) {
+    loadtime_data.Set(
+        "learnMoreLocationServices",
+        l10n_util::GetStringUTF16(
+            is_child ? IDS_CROS_OPT_IN_LEARN_MORE_LOCATION_SERVICES_CHILD
+                     : IDS_CROS_OPT_IN_LEARN_MORE_LOCATION_SERVICES));
+  } else {
+    loadtime_data.Set(
+        "learnMoreLocationServices",
+        l10n_util::GetStringUTF16(
+            is_child ? IDS_ARC_OPT_IN_LEARN_MORE_LOCATION_SERVICES_CHILD
+                     : IDS_ARC_OPT_IN_LEARN_MORE_LOCATION_SERVICES));
+  }
   loadtime_data.Set(
       "learnMorePaiServiceTitle",
       l10n_util::GetStringUTF16(IDS_ARC_OPT_IN_LEARN_MORE_PAI_SERVICE_TITLE));
@@ -746,20 +762,23 @@ void ArcSupportHost::OnMessage(const base::Value::Dict& message) {
     // If the user - not policy - controls Location Services setting, record
     // whether consent was given.
     if (!is_location_service_managed.value()) {
-      UserConsentTypes::ArcGoogleLocationServiceConsent
-          location_service_consent;
-      location_service_consent.set_confirmation_grd_id(
-          IDS_ARC_OPT_IN_DIALOG_BUTTON_AGREE);
-      location_service_consent.add_description_grd_ids(
-          is_child ? IDS_ARC_OPT_IN_LOCATION_SETTING_CHILD
-                   : IDS_ARC_OPT_IN_LOCATION_SETTING);
-      location_service_consent.set_status(is_location_service_enabled.value()
-                                              ? UserConsentTypes::GIVEN
-                                              : UserConsentTypes::NOT_GIVEN);
+      // TODO(b/327350824): Stop sending ARC controls to consent auditor.
+      if (!ash::features::IsCrosPrivacyHubLocationEnabled()) {
+        UserConsentTypes::ArcGoogleLocationServiceConsent
+            location_service_consent;
+        location_service_consent.set_confirmation_grd_id(
+            IDS_ARC_OPT_IN_DIALOG_BUTTON_AGREE);
+        location_service_consent.add_description_grd_ids(
+            is_child ? IDS_ARC_OPT_IN_LOCATION_SETTING_CHILD
+                     : IDS_ARC_OPT_IN_LOCATION_SETTING);
+        location_service_consent.set_status(is_location_service_enabled.value()
+                                                ? UserConsentTypes::GIVEN
+                                                : UserConsentTypes::NOT_GIVEN);
 
-      ConsentAuditorFactory::GetForProfile(profile_)
-          ->RecordArcGoogleLocationServiceConsent(account_id,
-                                                  location_service_consent);
+        ConsentAuditorFactory::GetForProfile(profile_)
+            ->RecordArcGoogleLocationServiceConsent(account_id,
+                                                    location_service_consent);
+      }
     }
 
     if (accepted) {
