7f6227ce9d244a05919414b8bd0d3a5407732e2c
Jonathan Hao <phao@chromium.org>
2024-02-29 10:17:14 +0000

    [Private Network Access] Store redirect info when redirect mode is manual
    When a redirected navigation request needs a preflight because of
    Private Network Access, the redirect_info is not properly stored in
    CorsURLLoader.  In FollowRedirect, the request URL and methods, etc will
    be overwritten with the stored redirect_info which is currently empty,
    causing the ERR_INVALID_URL in the associated bug.
    
    This CL stores the redirect_info properly for this case.
    
    The "missing cors headers" test cases are expected to fail because
    kPrivateNetworkAccessForNavigationsWarningOnly is by default enabled so
    requests actually succeeds
    .
    When kPrivateNetworkAccessForNavigations is disabled
    (virtual/pna-navigations-disabled), the preflight aren't sent so we
    expect failures too.
    
    Bug: 327017904,327022946
    Change-Id: I5631a2ba865c9a39dde7095888d7df24539194f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5331430
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Jonathan Hao <phao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266894}

--- third_party/blink/web_tests/external/wpt/fetch/private-network-access/resources/support.sub.js
+++ third_party/blink/web_tests/external/wpt/fetch/private-network-access/resources/support.sub.js
@@ -481,6 +481,13 @@ const NavigationTestResult = {
 };
 
 async function windowOpenTest(t, { source, target, expected }) {
+  if (target.behavior && target.behavior.redirect) {
+    target.behavior.redirect.searchParams.set('file', 'openee.html');
+    target.behavior.redirect.searchParams.set(
+        'file-if-no-preflight-received',
+        'no-preflight-received.html',
+    );
+  }
   const targetUrl = preflightUrl(target);
   targetUrl.searchParams.set("file", "openee.html");
   targetUrl.searchParams.set(
@@ -508,6 +515,13 @@ async function windowOpenTest(t, { source, target, expected }) {
 }
 
 async function windowOpenExistingTest(t, { source, target, expected }) {
+  if (target.behavior && target.behavior.redirect) {
+    target.behavior.redirect.searchParams.set('file', 'openee.html');
+    target.behavior.redirect.searchParams.set(
+        'file-if-no-preflight-received',
+        'no-preflight-received.html',
+    );
+  }
   const targetUrl = preflightUrl(target);
   targetUrl.searchParams.set("file", "openee.html");
   targetUrl.searchParams.set(
@@ -536,6 +550,13 @@ async function windowOpenExistingTest(t, { source, target, expected }) {
 }
 
 async function anchorTest(t, { source, target, expected }) {
+  if (target.behavior && target.behavior.redirect) {
+    target.behavior.redirect.searchParams.set('file', 'openee.html');
+    target.behavior.redirect.searchParams.set(
+        'file-if-no-preflight-received',
+        'no-preflight-received.html',
+    );
+  }
   const targetUrl = preflightUrl(target);
   targetUrl.searchParams.set("file", "openee.html");
   targetUrl.searchParams.set(
