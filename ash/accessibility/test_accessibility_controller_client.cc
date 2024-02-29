85cc0350a67632378a4bf525a3f9cae2b0d37336
David Tseng <dtseng@google.com>
2024-02-29 04:35:31 +0000

    [cfm-a11y]: ash side toggle for ChromeVox
    Currently, ChromeVox on CFM devices has a special toggle path:
    -  TouchAccessibilityEnabler::OnTimer
    -> AccessibilityManager::OnTwoFingersTouch[Start|Stop]
    -> extensions::EventRouter::BroadcastEvent(accessibility_private_on_two_fingers_start_or_stop_event)
    -> Meet app js listening to
    chrome.accessibilityPrivate.onTwoFingersTouch[Start|Stop]
    -> Meet app js calls
    chrome.accessibilityFeatures.spokenFeedback.set
    -> in Ash, toggles ChromeVox
    
    This change simplifies this path just to:
    -  TouchAccessibilityEnabler::OnTimer
    -> in Ash, toggles ChromeVox
    
    Bug: 327492307
    Test: manually by CFM team
    Change-Id: I45bba61e96f046e9896114d2ed3d406cbb3d6ff8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5328596
    Reviewed-by: Kyle Williams <kdgwill@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266818}

--- ash/accessibility/test_accessibility_controller_client.cc
+++ ash/accessibility/test_accessibility_controller_client.cc
@@ -55,10 +55,6 @@ bool TestAccessibilityControllerClient::ToggleDictation() {
 
 void TestAccessibilityControllerClient::SilenceSpokenFeedback() {}
 
-void TestAccessibilityControllerClient::OnTwoFingerTouchStart() {}
-
-void TestAccessibilityControllerClient::OnTwoFingerTouchStop() {}
-
 bool TestAccessibilityControllerClient::ShouldToggleSpokenFeedbackViaTouch()
     const {
   return true;
