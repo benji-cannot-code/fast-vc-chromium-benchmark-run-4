c89c21ba18e194370687c633c863117e5cbc8338
Timofey Chudakov <tchudakov@google.com>
2024-02-29 16:58:59 +0000

    [Mullhet] Implement "Fill password" suggestion.
    If configured, biometric authentication is triggered before
    actually filling the password into the field.
    
    Demo: http://screencast/cast/NTIwNzUzNjY2NjkzNTI5Nnw4NjUyNjlhMy03ZQ
    
    Fixed: b/326794605, b/326572565
    Change-Id: I32a500a6a730dc6b1c54da53d82953f47e72cabe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5317885
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267041}

--- components/password_manager/core/browser/password_manual_fallback_flow.cc
+++ components/password_manager/core/browser/password_manual_fallback_flow.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/manage_passwords_referrer.h"
 #include "components/password_manager/core/browser/password_manager_client.h"
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
+#include "components/password_manager/core/browser/password_manager_util.h"
+#include "components/password_manager/core/browser/password_ui_utils.h"
 
 namespace password_manager {
 
@@ -27,7 +29,9 @@ PasswordManualFallbackFlow::PasswordManualFallbackFlow(
   passwords_presenter_->Init();
 }
 
-PasswordManualFallbackFlow::~PasswordManualFallbackFlow() = default;
+PasswordManualFallbackFlow::~PasswordManualFallbackFlow() {
+  CancelBiometricReauthIfOngoing();
+}
 
 // static
 bool PasswordManualFallbackFlow::SupportsSuggestionType(
@@ -117,8 +121,8 @@ void PasswordManualFallbackFlow::DidAcceptSuggestion(
       // TODO(b/321678448): Fill username.
       break;
     case autofill::PopupItemId::kFillPassword:
-      // TODO(b/324241248): Conditionally trigger concent dialog and fill
-      // password.
+      FillPasswordSuggestion(
+          suggestion.GetPayload<Suggestion::ValueToFill>().value());
       break;
     case autofill::PopupItemId::kViewPasswordDetails:
       // TODO(b/324242001): Trigger password details dialog.
@@ -179,6 +183,58 @@ void PasswordManualFallbackFlow::RunFlowImpl(
       autofill::AutofillSuggestionTriggerSource::kManualFallbackPasswords);
   autofill_client_->ShowAutofillPopup(open_args,
                                       weak_ptr_factory_.GetWeakPtr());
+}
+
+void PasswordManualFallbackFlow::FillPasswordSuggestion(
+    const std::u16string& password) {
+  // TODO(b/324241248): Conditionally trigger consent dialog and fill
+  // password.
+  CancelBiometricReauthIfOngoing();
+  std::unique_ptr<device_reauth::DeviceAuthenticator> authenticator =
+      password_client_->GetDeviceAuthenticator();
+  // Note: this is currently only implemented on Android, Mac and Windows.
+  // For other platforms, the `authenticator` will be null.
+  if (!password_client_->CanUseBiometricAuthForFilling(authenticator.get())) {
+    password_manager_driver_->FillField(saved_field_id_, password);
+  } else {
+    authenticator_ = std::move(authenticator);
+
+    std::u16string message;
+    auto on_reath_complete =
+        base::BindOnce(&PasswordManualFallbackFlow::OnBiometricReauthCompleted,
+                       weak_ptr_factory_.GetWeakPtr(), password);
+
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
+    const std::u16string origin = base::UTF8ToUTF16(GetShownOrigin(
+        url::Origin::Create(password_manager_driver_->GetLastCommittedURL())));
+    message =
+        l10n_util::GetStringFUTF16(IDS_PASSWORD_MANAGER_FILLING_REAUTH, origin);
+#endif
+    authenticator_->AuthenticateWithMessage(
+        message, metrics_util::TimeCallback(
+                     std::move(on_reath_complete),
+                     "PasswordManager.PasswordFilling.AuthenticationTime"));
+  }
+}
+
+void PasswordManualFallbackFlow::OnBiometricReauthCompleted(
+    const std::u16string& password,
+    bool auth_succeeded) {
+  authenticator_.reset();
+  base::UmaHistogramBoolean(
+      "PasswordManager.PasswordFilling.AuthenticationResult", auth_succeeded);
+  if (!auth_succeeded) {
+    return;
+  }
+  password_manager_driver_->FillField(saved_field_id_, password);
+}
+
+void PasswordManualFallbackFlow::CancelBiometricReauthIfOngoing() {
+  if (!authenticator_) {
+    return;
+  }
+  authenticator_->Cancel();
+  authenticator_.reset();
 }
 
 }  // namespace password_manager
