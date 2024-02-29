d7c9eced29aa3e84111eccd79b3c6b62921d0f51
Gaston Rodriguez Lopez <gastonr@microsoft.com>
2024-02-29 17:31:59 +0000

    Fix non-composited overlay Fluent minimal mode thumb.
    The current calculation for the shrunk thumb rect truncates the results
    of the multiplications, resulting in a smaller minimal mode thumb for
    non-composited scrollbars. This CL changes the calculation to be like
    the composited scrollbar's size calculation, producing the correct
    dimensions and positioning of the thumb (see
    ScrollbarLayerImplBase::ComputeThumbQuadRectWithThumbThicknessScale())
    
    Created crbug.com/327457586 to follow up and write unit tests to
    ensure correct sizing.
    
    Bug: 327458810
    Change-Id: I7171a959e050592afc78292948d2df2647ed7ca6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5333602
    Commit-Queue: Gaston Rodriguez <gastonr@microsoft.com>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Olga Gerchikov <gerchiko@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1267069}

--- third_party/blink/renderer/core/scroll/scrollbar_theme_fluent.cc
+++ third_party/blink/renderer/core/scroll/scrollbar_theme_fluent.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/theme/web_theme_engine_helper.h"
 #include "third_party/blink/renderer/platform/web_test_support.h"
 #include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/geometry/rect_conversions.h"
 
 namespace blink {
 
@@ -225,20 +226,21 @@ int ScrollbarThemeFluent::ScrollbarTrackInsetPx(float scale) const {
 }
 
 gfx::Rect ScrollbarThemeFluent::ShrinkMainThreadedMinimalModeThumbRect(
-    Scrollbar& scrollbar,
-    gfx::Rect& rect) const {
+    const Scrollbar& scrollbar,
+    const gfx::Rect& rect) const {
   CHECK(UsesOverlayScrollbars());
   const float idle_thickness_scale = style_.idle_thickness_scale;
+  gfx::RectF thumb_rect(rect);
   if (scrollbar.Orientation() == kHorizontalScrollbar) {
-    rect.set_y(rect.y() + rect.height() * (1 - idle_thickness_scale));
-    rect.set_height(rect.height() * idle_thickness_scale);
+    thumb_rect.set_y(rect.y() + rect.height() * (1 - idle_thickness_scale));
+    thumb_rect.set_height(rect.height() * idle_thickness_scale);
   } else {
     if (!scrollbar.IsLeftSideVerticalScrollbar()) {
-      rect.set_x(rect.x() + rect.width() * (1 - idle_thickness_scale));
+      thumb_rect.set_x(rect.x() + rect.width() * (1 - idle_thickness_scale));
     }
-    rect.set_width(rect.width() * idle_thickness_scale);
+    thumb_rect.set_width(rect.width() * idle_thickness_scale);
   }
-  return rect;
+  return gfx::ToEnclosingRect(thumb_rect);
 }
 
 }  // namespace blink
