f6940e8f5a000a8038cefac9af18b8d2532edc17
Andrew Bregger <andrewbregger@google.com>
2024-02-29 23:16:59 +0000

    [Structured Metrics] Fix python formatting for G3 sync
    NO_BUG
    
    Change-Id: I6c505475ae9a15c245197a69e9670b768deddecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5336160
    Reviewed-by: Jong Ahn <jongahn@chromium.org>
    Commit-Queue: Andrew Bregger <andrewbregger@google.com>
    Cr-Commit-Position: refs/heads/main@{#1267267}

--- tools/metrics/structured/sync/model.py
+++ tools/metrics/structured/sync/model.py
@@ -108,7 +108,8 @@ class Model:
 </structured-metrics>"""
 
 
-def MergeModels(primary: Model, other: Model) -> Model:
+def merge_models(primary: Model, other: Model) -> Model:
+  """Merges two models into one."""
   primary.projects += [
       p for p in other.projects if not re.match('Test', p.name)
   ]
