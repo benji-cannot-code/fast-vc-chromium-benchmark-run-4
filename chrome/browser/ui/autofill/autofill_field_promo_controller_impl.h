fed9537afc81718a5faf4f15ce695177053337fd
Christoph Schwering <schwering@google.com>
2024-02-29 02:59:59 +0000

    [Autofill] Wrap AutofillPopupHideHelper in std::optional, not unique_ptr
    Bug: 320632147
    Change-Id: I315390eb576e8d685299cae6892ee92dd787647e
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5319670
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1266798}

--- chrome/browser/ui/autofill/autofill_field_promo_controller_impl.h
+++ chrome/browser/ui/autofill/autofill_field_promo_controller_impl.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_AUTOFILL_AUTOFILL_FIELD_PROMO_CONTROLLER_IMPL_H_
 #define CHROME_BROWSER_UI_AUTOFILL_AUTOFILL_FIELD_PROMO_CONTROLLER_IMPL_H_
 
+#include <optional>
+
 #include "base/memory/raw_ref.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/autofill/autofill_field_promo_controller.h"
@@ -54,7 +56,7 @@ class AutofillFieldPromoControllerImpl : public AutofillFieldPromoController {
   const ui::ElementIdentifier promo_element_identifier_;
   base::WeakPtr<AutofillFieldPromoView> promo_view_;
   // This is a helper which detects events that should hide the promo.
-  std::unique_ptr<AutofillPopupHideHelper> promo_hide_helper_;
+  std::optional<AutofillPopupHideHelper> promo_hide_helper_;
 };
 
 }  // namespace autofill
