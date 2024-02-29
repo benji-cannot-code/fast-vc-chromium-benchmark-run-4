65b2a6fc548562be87786343ac9abaf0ae59b770
Ben Pastene <bpastene@chromium.org>
2024-02-29 01:13:16 +0000

    utr: Set swarming env-var in recipe invocations
    Some parts of the recipe need the swarming server env var set to
    control what it does. (eg: what cas instance to upload test inputs to)
    
    So this just hardcodes the chromium-swarm server for now. When
    support for internal builders is added, it can do something a
    little smarter to use the internal server.
    
    Bug: 41492688
    Change-Id: I9ce215fce712c5453e8176a960933c54fcf8e2a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5327502
    Reviewed-by: Struan Shrimpton <sshrimp@google.com>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266761}

--- tools/utr/recipe.py
+++ tools/utr/recipe.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import json
 import logging
+import os
 import pathlib
 import subprocess
 import tempfile
@@ -29,6 +30,7 @@ class LegacyRunner:
                builder_props,
                bucket,
                builder,
+               swarming_server,
                tests,
                skip_compile,
                skip_test,
@@ -40,6 +42,7 @@ class LegacyRunner:
       builder_props: Dict containing the props for the builder to run as.
       bucket: Bucket name of the builder to run as.
       builder: Builder name of the builder to run as.
+      swarming_server: Swarming server the builder runs on.
       tests: List of tests to run.
       skip_compile: If True, the UTR will only run the tests.
       skip_test: If True, the UTR will only compile.
@@ -47,6 +50,7 @@ class LegacyRunner:
           default otherwise if needed.
     """
     self._recipes_py = bundle_root_path.joinpath('recipes')
+    self._swarming_server = swarming_server
     assert self._recipes_py.exists()
 
     # Add UTR recipe props. Its schema is located at:
@@ -97,7 +101,11 @@ class LegacyRunner:
           '-',  # '-' means read from stdin
           self.UTR_RECIPE_NAME,
       ]
-      p = subprocess.Popen(cmd, stdin=subprocess.PIPE, text=True)
+      env = os.environ.copy()
+      # This env var is read by both the cas and swarming recipe modules to
+      # determine where to upload/run things.
+      env['SWARMING_SERVER'] = f'https://{self._swarming_server}.appspot.com'
+      p = subprocess.Popen(cmd, stdin=subprocess.PIPE, env=env, text=True)
       p.communicate(input=json.dumps(self._input_props))
 
       # Try to pull out the summary markdown from the recipe run.
