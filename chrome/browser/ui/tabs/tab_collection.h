2c75112e85a007bf4f5def29ed9f0f6ef111823c
Shibalik Mohapatra <shibalik@chromium.org>
2024-02-29 06:57:16 +0000

    tab collection storage implementation.
    This CL has -
    
    1. the initial implementation of TabCollectionStorage. Some of the collection related methods will be implemented later along with the implementation of specific tab collection classes.
    
    2. A basic pinned tab collection with mostly non implemented methods. This is to help with the unittest for tab collection storage instead of using a fake class/ mock.
    
    Change-Id: Idcdc38b2dc2fa3e3658cdf39b5479c883e7ce4a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5320080
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266852}

--- chrome/browser/ui/tabs/tab_collection.h
+++ chrome/browser/ui/tabs/tab_collection.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstddef>
 #include <memory>
 #include <optional>
+
 #include "base/memory/raw_ptr.h"
 #include "base/types/pass_key.h"
 
@@ -18,7 +19,7 @@ class TabModel;
 
 class TabCollection {
  public:
-  TabCollection();
+  TabCollection() = default;
   virtual ~TabCollection() = default;
   TabCollection(const TabCollection&) = delete;
   TabCollection& operator=(const TabCollection&) = delete;
