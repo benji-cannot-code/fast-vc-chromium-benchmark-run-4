b53eb4433e3de7b9e46bdc2c399a63f89c2db64e
Paul Adedeji <pauladedeji@google.com>
2024-02-29 23:04:11 +0000

    Animate NTP Wallpaper Search Button
    Implements feature `NtpWallpaperSearchButtonAnimation`
    
    *video demo in bug comments*
    
    Change-Id: I4696f8495336521d28a3125472e815419919b487
    Bug: 327437135
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5332588
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1267260}

--- chrome/browser/resources/new_tab_page/app.ts
+++ chrome/browser/resources/new_tab_page/app.ts
@@ -336,6 +336,13 @@ export class AppElement extends AppElementBase {
         value: document.documentElement.scrollTop <= 0,
       },
 
+      wallpaperSearchButtonAnimationEnabled_: {
+        type: Boolean,
+        value: () =>
+            loadTimeData.getBoolean('wallpaperSearchButtonAnimationEnabled'),
+        reflectToAttribute: true,
+      },
+
       wallpaperSearchButtonEnabled_: {
         type: Boolean,
         value: () => loadTimeData.getBoolean('wallpaperSearchButtonEnabled'),
@@ -383,6 +390,7 @@ export class AppElement extends AppElementBase {
   private promoAndModulesLoaded_: boolean;
   private lazyRender_: boolean;
   private scrolledToTop_: boolean;
+  private wallpaperSearchButtonAnimationEnabled_: boolean;
   private wallpaperSearchButtonEnabled_: boolean;
 
   private callbackRouter_: PageCallbackRouter;
