2eabebb766cfca328e8a546d5afb532cf7e3a466
Nan Lin <linnan@chromium.org>
2024-02-29 20:46:46 +0000

    Remove unused MockDataHost
    Change-Id: Iccf2c02f574dbc3ec56f97809e903d825561469f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5333380
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267188}

--- content/browser/attribution_reporting/attribution_src_browsertest.cc
+++ content/browser/attribution_reporting/attribution_src_browsertest.cc
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/attribution_reporting/test/mock_attribution_host.h"
 #include "content/browser/attribution_reporting/test/mock_attribution_manager.h"
 #include "content/browser/attribution_reporting/test/mock_content_browser_client.h"
-#include "content/browser/attribution_reporting/test/mock_data_host.h"
 #include "content/browser/attribution_reporting/test/source_observer.h"
 #include "content/browser/renderer_host/render_frame_host_impl.h"
 #include "content/browser/storage_partition_impl.h"
