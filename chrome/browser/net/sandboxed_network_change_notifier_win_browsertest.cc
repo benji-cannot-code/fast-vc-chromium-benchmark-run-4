3c8c595bf259ce335b5797a2616457464c2fb3a7
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-02-29 14:26:59 +0000

    Cleanup: Guard against clang-format header reordering.
    The new clang format "IncludeBlocks: Regroup" is going to reorder
    header. Unfortunately, the order around the <windows.h> header matters.
    
    This patch disables clang format.
    This is blocking: https://chromium-review.googlesource.com/c/chromium/src/+/5328112
    
    Bug: 40288126
    Change-Id: I80fdfac18e21ef0c162b7b22879a8eba3f789223
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5328571
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266964}

--- chrome/browser/net/sandboxed_network_change_notifier_win_browsertest.cc
+++ chrome/browser/net/sandboxed_network_change_notifier_win_browsertest.cc
@@ -5,13 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/network/public/mojom/network_service.mojom.h"
 
+// clang-format off
 #include <windows.h>  // Must be in front of other Windows header files.
-
 #include <initguid.h>  // Must be in front of devpkey.h.
-
 // Must be in front of Windows includes because they define LogSeverity and this
 // breaks gmock.
 #include "testing/gmock/include/gmock/gmock.h"
+// clang-format on
 
 #include <cfgmgr32.h>
 #include <devpkey.h>
--- remoting/host/win/unprivileged_process_delegate.cc
+++ remoting/host/win/unprivileged_process_delegate.cc
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "remoting/host/win/unprivileged_process_delegate.h"
 
+// clang-format off
 #include <windows.h>  // Must be in front of other Windows header files.
+// clang-format on
 
 #include <sddl.h>
 
