22de8fd3c09429e6a825ce4b75285ffbdd7ea271
Jan Keitel <jkeitel@google.com>
2024-02-29 17:56:52 +0000

    Include contenteditables in AutofillAgent::TriggerSuggestions.
    This CL includes support for explicitly triggering suggestions in
    contenteditables. This is needed for programmatically triggering
    a Compose saved state nudge on focus loss of the Compose widget.
    
    Bug: 303500278
    Change-Id: Iaf89346a059484bf5d74a1e24d3bb922d9ef6050
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5335136
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1267090}

--- components/autofill/content/renderer/autofill_agent_browsertest.cc
+++ components/autofill/content/renderer/autofill_agent_browsertest.cc
@@ -21,12 +21,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/content/common/mojom/autofill_driver.mojom.h"
 #include "components/autofill/content/renderer/autofill_agent_test_api.h"
 #include "components/autofill/content/renderer/autofill_renderer_test.h"
+#include "components/autofill/content/renderer/form_autofill_util.h"
 #include "components/autofill/content/renderer/form_tracker.h"
 #include "components/autofill/content/renderer/test_utils.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/field_data_manager.h"
 #include "components/autofill/core/common/form_data.h"
 #include "components/autofill/core/common/mojom/autofill_types.mojom-shared.h"
+#include "components/autofill/core/common/unique_ids.h"
 #include "content/public/renderer/render_frame.h"
 #include "content/public/test/navigation_simulator.h"
 #include "content/public/test/test_utils.h"
@@ -137,6 +139,12 @@ class AutofillAgentTest : public test::AutofillRendererTest {
   blink::WebElement GetWebElementById(const std::string& id) {
     return GetMainFrame()->GetDocument().GetElementById(
         blink::WebString::FromUTF8(id));
+  }
+
+  FormRendererId GetFormRendererIdById(std::string_view id) {
+    return form_util::GetFormRendererId(
+        GetMainFrame()->GetDocument().GetElementById(
+            blink::WebString::FromUTF8(id)));
   }
 
   void SimulateUserEditField(const blink::WebFormElement& form,
@@ -492,12 +500,6 @@ class AutofillAgentTestExtractForms : public AutofillAgentTestWithFeatures {
     AutofillAgentTestWithFeatures::LoadHTML(html);
     WaitForFormsSeen();
   }
-
-  FormRendererId GetFormRendererIdById(std::string_view id) {
-    return form_util::GetFormRendererId(
-        GetMainFrame()->GetDocument().GetElementById(
-            blink::WebString::FromUTF8(id)));
-  }
 };
 
 TEST_F(AutofillAgentTestExtractForms, CallbackIsCalledIfFormIsNotFound) {
@@ -564,6 +566,17 @@ TEST_F(AutofillAgentTestWithFeatures, TriggerSuggestions) {
   autofill_agent().TriggerSuggestions(
       FieldRendererId(2),
       AutofillSuggestionTriggerSource::kFormControlElementClicked);
+}
+
+// Tests that `AutofillDriver::TriggerSuggestions()` works for contenteditables.
+TEST_F(AutofillAgentTestWithFeatures, TriggerSuggestionsForContenteditable) {
+  LoadHTML("<body><div id=ce contenteditable></div></body>");
+
+  FormRendererId form_id = GetFormRendererIdById("ce");
+  EXPECT_CALL(autofill_driver(), AskForValuesToFill);
+  autofill_agent().TriggerSuggestions(
+      FieldRendererId(form_id.value()),
+      AutofillSuggestionTriggerSource::kComposeDialogLostFocus);
 }
 
 TEST_F(AutofillAgentTest, UndoAutofillSetsLastQueriedElement) {
