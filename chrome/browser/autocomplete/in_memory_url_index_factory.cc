45011358ee63b46db31ad2a4f8db74096c7b2548
Mikel Astiz <mastiz@chromium.org>
2024-02-29 12:18:48 +0000

    [ios] Integrate account bookmarks into omnibox's InMemoryURLIndex
    This is a follow-up after https://crrev.com/c/5310163 overlooked this
    codepath. It addresses a TODO to make sure all bookmarks are visible
    to InMemoryURLIndex for the purpose of influencing scoring.
    
    Bug: 326185948
    Fixed: 40260907
    Change-Id: Id15b084fad2fadef523646ca95bf46677f3d0d23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5331028
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Angela Yoeurng <yoangela@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266925}

--- chrome/browser/autocomplete/in_memory_url_index_factory.cc
+++ chrome/browser/autocomplete/in_memory_url_index_factory.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
+#include "components/bookmarks/browser/bookmark_model.h"
 #include "components/keyed_service/core/service_access_type.h"
 #include "components/omnibox/browser/in_memory_url_index.h"
 #include "content/public/common/url_constants.h"
--- components/omnibox/browser/url_index_private_data.cc
+++ components/omnibox/browser/url_index_private_data.cc
@@ -30,8 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
 #include "base/trace_event/memory_usage_estimator.h"
-#include "components/bookmarks/browser/bookmark_model.h"
 #include "components/bookmarks/browser/bookmark_utils.h"
+#include "components/bookmarks/browser/core_bookmark_model.h"
 #include "components/history/core/browser/history_database.h"
 #include "components/history/core/browser/history_db_task.h"
 #include "components/history/core/browser/history_service.h"
@@ -130,7 +130,7 @@ ScoredHistoryMatches URLIndexPrivateData::HistoryItemsForTerms(
     size_t cursor_position,
     const std::string& host_filter,
     size_t max_matches,
-    bookmarks::BookmarkModel* bookmark_model,
+    bookmarks::CoreBookmarkModel* bookmark_model,
     TemplateURLService* template_url_service,
     OmniboxTriggeredFeatureService* triggered_feature_service) {
   // This list will contain the original search string and any other string
@@ -648,7 +648,7 @@ void URLIndexPrivateData::HistoryIdsToScoredMatches(
     const std::u16string& lower_raw_string,
     const std::string& host_filter,
     const TemplateURLService* template_url_service,
-    bookmarks::BookmarkModel* bookmark_model,
+    bookmarks::CoreBookmarkModel* bookmark_model,
     ScoredHistoryMatches* scored_items,
     OmniboxTriggeredFeatureService* triggered_feature_service) const {
   if (history_ids.empty())
