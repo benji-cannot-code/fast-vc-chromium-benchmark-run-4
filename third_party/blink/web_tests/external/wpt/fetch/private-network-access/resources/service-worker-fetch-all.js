d33f2edb60be672b0ef70a05c444e9c2618771b8
Yifan Luo <lyf@chromium.org>
2024-02-29 13:53:26 +0000

    [Private Network Access] Add test for service worker fetching document
    Bug: b/323583084
    Change-Id: I031d4ebd81d105e01a66eea10bf2bfefa781cd19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5307002
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Yifan Luo <lyf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266949}

---
+++ third_party/blink/web_tests/external/wpt/fetch/private-network-access/resources/service-worker-fetch-all.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+self.addEventListener("install", () => {
+    // Skip waiting before replacing the previously-active service worker, if any.
+    // This allows the bridge script to notice the controller change and query
+    // the install time via fetch.
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    // Claim all clients so that the bridge script notices the activation.
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url).searchParams.get("proxied-url");
+  if (url) {
+    event.respondWith(fetch(url));
+  } else {
+    event.respondWith(fetch(event.request));
+  }
+});
