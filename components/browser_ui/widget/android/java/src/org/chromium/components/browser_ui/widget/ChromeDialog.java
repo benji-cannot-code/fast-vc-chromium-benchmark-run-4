bf96cc9b993d6ee5673a606649fdf8104c5d921d
Geoff Huang <geoffhuang@google.com>
2024-02-29 00:32:04 +0000

    Fix automotive toolbar bug causing layouts to be smaller than intended
    Example fix of PrivacySandboxDialogConsentEEA:
    Before: https://hsv.googleplex.com/4866946005729280
    After: https://hsv.googleplex.com/5436375259152384
    
    Bug: b/327500118, b/327274697
    Change-Id: Ie7cbd4a1a573922baa8f6a5db8d8138e8cb8a4f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5331987
    Commit-Queue: Geoff Huang <geoffhuang@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266744}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/ChromeDialog.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/ChromeDialog.java
@@ -11,6 +11,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.view.ViewStub;
 import android.widget.LinearLayout;
 
@@ -64,7 +65,7 @@ public class ChromeDialog extends ComponentDialog {
                     AutomotiveUtils.getAutomotiveLayoutWithBackButtonToolbar(mContext));
             setAutomotiveToolbarBackButtonAction();
             LinearLayout linearLayout = findViewById(R.id.automotive_base_linear_layout);
-            linearLayout.addView(view);
+            linearLayout.addView(view, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
         } else {
             super.setContentView(view);
         }
@@ -77,8 +78,7 @@ public class ChromeDialog extends ComponentDialog {
                     AutomotiveUtils.getAutomotiveLayoutWithBackButtonToolbar(mContext));
             setAutomotiveToolbarBackButtonAction();
             LinearLayout linearLayout = findViewById(R.id.automotive_base_linear_layout);
-            linearLayout.setLayoutParams(params);
-            linearLayout.addView(view);
+            linearLayout.addView(view, params);
         } else {
             super.setContentView(view, params);
         }
