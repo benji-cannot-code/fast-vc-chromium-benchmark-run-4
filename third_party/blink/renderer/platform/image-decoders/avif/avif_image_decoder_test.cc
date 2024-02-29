f7f737f82d0b735a5e6ad5858e0094d76c303c70
Vignesh Venkat <vigneshv@google.com>
2024-02-29 17:38:09 +0000

    avif: Remove unnecessary libavif deps from unit tests
    The unit tests only depend on the ":image_decoders" target and
    they don't need to directly depend on libavif. Remove the unused
    header file and the direct libavif dependencies from the unit
    test targets.
    
    Change-Id: I5ae40f9b6fad9dad77d227549d2b52cb411b2833
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5319509
    Reviewed-by: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Vignesh Venkat <vigneshv@google.com>
    Cr-Commit-Position: refs/heads/main@{#1267075}

--- third_party/blink/renderer/platform/image-decoders/avif/avif_image_decoder_test.cc
+++ third_party/blink/renderer/platform/image-decoders/avif/avif_image_decoder_test.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/renderer/platform/image-decoders/image_decoder_test_helpers.h"
 #include "third_party/blink/renderer/platform/wtf/shared_buffer.h"
-#include "third_party/libavif/src/include/avif/avif.h"
 #include "ui/gfx/color_space.h"
 #include "ui/gfx/color_transform.h"
 
