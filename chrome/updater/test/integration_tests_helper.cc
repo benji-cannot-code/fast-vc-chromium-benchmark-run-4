56c15d1fca092f9a6190397286fc08eae5bb84a4
Sorin Jianu <sorin@chromium.org>
2024-02-29 01:34:23 +0000

    updater: rename --version cmd line argument in integration tests.
    `--version` conflicts with the same argument defined by `absl` in
    its usage flags.
    
    Bug: 327502550
    Change-Id: I714ae467edf2843c2bbc5e730d59e730ec68d065
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5332201
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266777}

--- chrome/updater/test/integration_tests_helper.cc
+++ chrome/updater/test/integration_tests_helper.cc
@@ -311,7 +311,7 @@ void AppTestHelper::FirstTaskRun() {
                                         WithSystemScope(Wrap(&ExpectAppTag))))},
           {"expect_app_version",
            WithSwitch(
-               "version",
+               "app_version",
                WithSwitch("app_id", WithSystemScope(Wrap(&ExpectAppVersion))))},
           {"expect_candidate_uninstalled",
            WithSystemScope(Wrap(&ExpectCandidateUninstalled))},
@@ -355,9 +355,10 @@ void AppTestHelper::FirstTaskRun() {
            WithSwitch("app_id", WithSystemScope(Wrap(&RunHandoff)))},
 #endif  // BUILDFLAG(IS_WIN)
           {"expect_version_active",
-           WithSwitch("version", WithSystemScope(Wrap(&ExpectVersionActive)))},
+           WithSwitch("updater_version",
+                      WithSystemScope(Wrap(&ExpectVersionActive)))},
           {"expect_version_not_active",
-           WithSwitch("version",
+           WithSwitch("updater_version",
                       WithSystemScope(Wrap(&ExpectVersionNotActive)))},
           {"install", WithSystemScope(Wrap(&Install))},
           {"install_updater_and_app",
@@ -396,8 +397,8 @@ void AppTestHelper::FirstTaskRun() {
           {"delete_file",
            (WithSwitch("path", WithSystemScope(Wrap(&DeleteFile))))},
           {"install_app",
-           WithSwitch("version", WithSwitch("app_id", WithSystemScope(
-                                                          Wrap(&InstallApp))))},
+           WithSwitch("app_version", WithSwitch("app_id", WithSystemScope(Wrap(
+                                                              &InstallApp))))},
           {"install_app_via_service",
            WithSwitch("expected_final_values",
                       WithSwitch("app_id", WithSystemScope(
@@ -442,7 +443,7 @@ void AppTestHelper::FirstTaskRun() {
           {"expect_legacy_updater_migrated",
            WithSystemScope(Wrap(&ExpectLegacyUpdaterMigrated))},
           {"run_recovery_component",
-           WithSwitch("version",
+           WithSwitch("browser_version",
                       WithSwitch("app_id", WithSystemScope(
                                                Wrap(&RunRecoveryComponent))))},
           {"set_last_checked",
