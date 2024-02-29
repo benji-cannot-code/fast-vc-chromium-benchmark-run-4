ce3dccc97498a3b3fcf902abedd483eb5c049b5c
Joshua Hood <jdh@chromium.org>
2024-02-29 19:42:29 +0000

    [3PCD] Extend tpcd trial validity service to handle the top-level trial
    This CL updates tpcd::trial::ValidityService to verify the TopLevelTpcd
    deprecation trial is enabled for the first-party url associated with any
    3PC access that is allowed as a result of a `TOP_LEVEL_TPCD_TRIAL`
    content setting.
    
    Bug: 1503718
    Change-Id: I7815ac6df78b4248fa2b8db36ea17f1b27e32d20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5318371
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Joshua Hood <jdh@chromium.org>
    Reviewed-by: Anton Maliev <amaliev@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267145}

--- chrome/browser/subresource_filter/ad_heuristic_tpcd_browsertest.cc
+++ chrome/browser/subresource_filter/ad_heuristic_tpcd_browsertest.cc
@@ -571,6 +571,10 @@ class AdHeuristicTPCDBrowserTestTopLevelTrialGrant
            {"SkipTpcdMitigationsForAdsMetadata", "true"},
            {"SkipTpcdMitigationsForAdsHeuristics", "true"}}}},
         {});
+
+    // Disable the validity service so it doesn't remove manually created
+    // trial settings.
+    tpcd::trial::ValidityService::DisableForTesting();
   }
 
  private:
@@ -608,6 +612,10 @@ class AdHeuristicTPCDBrowserTestSkipTopLevelTrialGrant
            {"SkipTpcdMitigationsForAdsMetadata", "false"},
            {"SkipTpcdMitigationsForAdsHeuristics", "false"}}}},
         {});
+
+    // Disable the validity service so it doesn't remove manually created
+    // trial settings.
+    tpcd::trial::ValidityService::DisableForTesting();
   }
 
  private:
