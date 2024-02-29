28b4b3fc2ba2618a44418820c07588cdf1a5ebd9
James Cook <jamescook@chromium.org>
2024-02-29 21:25:11 +0000

    birch: Fix typo in birch_keyed_service_unittest.cc
    There was one line of commented out code that was accidentally left in.
    
    Bug: none
    Change-Id: I5e0bee51765f800866fe579f24c429d06e9d5268
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5335048
    Auto-Submit: James Cook <jamescook@chromium.org>
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Commit-Queue: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267208}

--- chrome/browser/ui/ash/birch/birch_keyed_service_unittest.cc
+++ chrome/browser/ui/ash/birch/birch_keyed_service_unittest.cc
@@ -182,7 +182,6 @@ std::unique_ptr<KeyedService> BuildMockSessionSyncService(
 using ash::holding_space::ScopedTestMountPoint;
 
 class BirchKeyedServiceTest : public BrowserWithTestWindowTest {
-  // public testing::Test {
  public:
   BirchKeyedServiceTest()
       : BrowserWithTestWindowTest(
