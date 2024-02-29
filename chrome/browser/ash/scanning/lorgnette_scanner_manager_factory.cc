a64f77fa985e43367827da42822978984655543c
Rishabh Agrawal <rishabhagr@chromium.org>
2024-02-29 17:49:26 +0000

    scanning: Add notification controller
    Add a scanning notifications controller and an obsever to Dlcservice.
    Send notification once download has started, in-progress and
    completed successfully/unsuccessfully.
    
    TEST=build; deploy; Used dlcservice_util to manually install and uninstall and verify the notification
    
    Bug: b/268700936
    Change-Id: Ic2120abfb50f036652de4e093efbbdaf6e280caf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5100621
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Benjamin Gordon <bmgordon@chromium.org>
    Commit-Queue: Rishabh Agrawal <rishabhagr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267084}

--- chrome/browser/ash/scanning/lorgnette_scanner_manager_factory.cc
+++ chrome/browser/ash/scanning/lorgnette_scanner_manager_factory.cc
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/scanning/lorgnette_scanner_manager_factory.h"
 
 #include "base/no_destructor.h"
+#include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/ash/scanning/lorgnette_scanner_manager.h"
 #include "chrome/browser/ash/scanning/zeroconf_scanner_detector.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_selections.h"
 #include "content/public/browser/browser_context.h"
 
@@ -42,7 +44,9 @@ LorgnetteScannerManagerFactory::~LorgnetteScannerManagerFactory() = default;
 std::unique_ptr<KeyedService>
 LorgnetteScannerManagerFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
-  return LorgnetteScannerManager::Create(ZeroconfScannerDetector::Create());
+  auto* profile = Profile::FromBrowserContext(context);
+  return LorgnetteScannerManager::Create(ZeroconfScannerDetector::Create(),
+                                         profile);
 }
 
 bool LorgnetteScannerManagerFactory::ServiceIsCreatedWithBrowserContext()
