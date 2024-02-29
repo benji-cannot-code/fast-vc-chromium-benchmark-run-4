fc3d289c78e97d1141366bd144e4bc0eeab24993
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2024-02-29 07:21:12 +0000

    Add crash key for calling CountFeature
    To understand what feature to be recorded caused this, let me record the
    feature ID in the crash key.
    
    Bug: 40918057
    Change-Id: I7a97f474e6d18cc7f929b2a0c591b746c7f33ef4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5332953
    Auto-Submit: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266862}

--- content/browser/service_worker/service_worker_container_host.cc
+++ content/browser/service_worker/service_worker_container_host.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/adapters.h"
 #include "base/containers/contains.h"
+#include "base/debug/crash_logging.h"
 #include "base/functional/callback_helpers.h"
 #include "base/functional/overloaded.h"
 #include "base/strings/stringprintf.h"
@@ -620,6 +621,7 @@ void ServiceWorkerContainerHost::PostMessageToClient(
 void ServiceWorkerContainerHost::CountFeature(
     blink::mojom::WebFeature feature) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  SCOPED_CRASH_KEY_NUMBER("SWCH_CF", "feature", static_cast<int32_t>(feature));
 
   // CountFeature is a message about the client's controller. It should be sent
   // only for clients.
