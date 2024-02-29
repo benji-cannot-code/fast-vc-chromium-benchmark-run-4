ffb82f7b7c93590b5e8a6d20346f52666ead58f8
Richard Chen <richche@chromium.org>
2024-02-29 19:49:27 +0000

    Update blocklist pref methods to use const ExtensionPrefs*
    By updating from "ExtensionPref*" to "const ExtensionPrefs*",
    callers with only const ExtensionPrefs* can also use these
    blocklist methods.
    
    Bug: 327251907
    Change-Id: Iffd0baaba8f548ba14cb3a3ac1ad29accabcfdda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5328991
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Richard Chen <richche@google.com>
    Cr-Commit-Position: refs/heads/main@{#1267148}

--- extensions/browser/blocklist_extension_prefs.cc
+++ extensions/browser/blocklist_extension_prefs.cc
@@ -87,7 +87,7 @@ BitMapBlocklistState BlocklistStateToBitMapBlocklistState(
 
 BitMapBlocklistState GetExtensionBlocklistState(
     const ExtensionId& extension_id,
-    ExtensionPrefs* extension_prefs) {
+    const ExtensionPrefs* extension_prefs) {
   BitMapBlocklistState sb_state =
       GetSafeBrowsingExtensionBlocklistState(extension_id, extension_prefs);
   BitMapBlocklistState extension_telemetry_service_state =
@@ -136,7 +136,7 @@ void RemoveOmahaBlocklistState(const ExtensionId& extension_id,
 
 bool HasOmahaBlocklistState(const ExtensionId& extension_id,
                             BitMapBlocklistState state,
-                            ExtensionPrefs* extension_prefs) {
+                            const ExtensionPrefs* extension_prefs) {
   int current_states = extension_prefs->GetBitMapPrefBits(
       extension_id, kPrefOmahaBlocklistState,
       static_cast<int>(kDefaultBitMapBlocklistState));
@@ -220,7 +220,7 @@ void SetSafeBrowsingExtensionBlocklistState(
 
 BitMapBlocklistState GetSafeBrowsingExtensionBlocklistState(
     const ExtensionId& extension_id,
-    ExtensionPrefs* extension_prefs) {
+    const ExtensionPrefs* extension_prefs) {
   int int_value = -1;
   if (extension_prefs->ReadPrefAsInteger(extension_id, kPrefBlocklistState,
                                          &int_value) &&
@@ -251,7 +251,7 @@ void SetExtensionTelemetryServiceBlocklistState(
 
 BitMapBlocklistState GetExtensionTelemetryServiceBlocklistState(
     const ExtensionId& extension_id,
-    ExtensionPrefs* extension_prefs) {
+    const ExtensionPrefs* extension_prefs) {
   int int_value = -1;
   if (extension_prefs->ReadPrefAsInteger(
           extension_id, kPrefExtensionTelemetryServiceBlocklistState,
