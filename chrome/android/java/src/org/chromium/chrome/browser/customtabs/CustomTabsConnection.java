a27309752d43d42d2c67ef99106c0010e7da5348
Tomasz Wiszkowski <ender@google.com>
2024-02-29 19:58:52 +0000

    Introduce an AppHook validator controlling Search in CCT feature.
    This is a stub method that ensures the feature is - by default - not
    allowed to be enabled.
    
    Bug: b:327023785
    Change-Id: I8cd330ac8e66abc749b8a9ebfa0c3a6d329e1f59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5335721
    Reviewed-by: Kevin Grosu <kgrosu@google.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1267158}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsConnection.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsConnection.java
@@ -1963,6 +1963,11 @@ public class CustomTabsConnection {
         return mClientManager.getEngagementSignalsCallbackForSession(session) != null;
     }
 
+    /** Whether a CustomTabs instance should include interactive Omnibox. */
+    public boolean shouldEnableOmniboxForIntent(BrowserServicesIntentDataProvider intentData) {
+        return false;
+    }
+
     /**
      * Whether PageInsight Hub is enabled by the launching Intent. False by default.
      * @param intentData {@link BrowserServicesIntentDataProvider} built from the Intent that
