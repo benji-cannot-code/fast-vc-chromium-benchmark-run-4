b45bd8e0c588d98e1e23112e28c17699398771c5
Luciano Pacheco <lucmult@chromium.org>
2024-02-29 02:23:57 +0000

    Files app: Add new UMA FileBrowser.ViewingVolumeType
    This metric records the volume type of a file when users chooses:
    1. Open a file in an app; or
    2. Selects the file in the FilePicker.
    
    Bug: b:326848226
    Change-Id: I34d9a766b0faf891fc6b298da1ca0aaad6874dd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5320862
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Nigel Tao <nigeltao@chromium.org>
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266786}

--- ui/file_manager/file_manager/foreground/js/dialog_action_controller.ts
+++ ui/file_manager/file_manager/foreground/js/dialog_action_controller.ts
@@ -10,9 +10,11 @@ import {isFolderDialogType} from '../../common/js/dialog_type.js';
 import type {FilesAppDirEntry, FilesAppEntry} from '../../common/js/files_app_entry_types.js';
 import {recordEnum} from '../../common/js/metrics.js';
 import {str} from '../../common/js/translations.js';
+import {recordViewingVolumeTypeUma} from '../../common/js/uma.js';
 import {testSendMessage, UserCanceledError} from '../../common/js/util.js';
 import {AllowedPaths, RootTypesForUMA} from '../../common/js/volume_manager_types.js';
 import {DialogType} from '../../state/state.js';
+import {getStore} from '../../state/store.js';
 
 import type {FileFilter} from './directory_contents.js';
 import type {DirectoryModel} from './directory_model.js';
@@ -50,8 +52,7 @@ export class DialogActionController {
    * @param fileFilter File filter model.
    * @param namingController Naming controller.
    * @param fileSelectionHandler Initial file selection.
-   * @param launchParam Whether the dialog should return local
-   *     path or not.
+   * @param launchParam Whether the dialog should return local path or not.
    */
   constructor(
       private dialogType_: DialogType, private dialogFooter_: DialogFooter,
@@ -254,6 +255,12 @@ export class DialogActionController {
         this.dialogType_ === DialogType.SELECT_OPEN_MULTI_FILE) {
       recordEnum('OpenFiles.RootType', currentRootType, RootTypesForUMA);
     }
+
+    const state = getStore().getState();
+    for (const url of selection.urls) {
+      recordViewingVolumeTypeUma(state, url);
+    }
+
     if (selection.multiple) {
       chrome.fileManagerPrivate.selectFiles(
           selection.urls, this.allowedPaths_ === AllowedPaths.NATIVE_PATH,
