6e0b963b2e37c2ec85a7477d853a1cdde302bf24
Joel Hockey <joelhockey@chromium.org>
2024-02-29 10:18:55 +0000

    Use ChromeSecurityDelegate::GetVmName() for translating paths
    Use vm_name directly from the SecurityDelegate rather than mapping
    from the ui::EndpointType.  This adds support for bru and any other
    future Guest OS.
    
    Bug: b/323465931
    Change-Id: I1ea76ba9c2a2dd789fde47ea0b27f7706ead8bb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5303315
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/main@{#1266895}

--- chrome/browser/ash/exo/chrome_security_delegate.h
+++ chrome/browser/ash/exo/chrome_security_delegate.h
@@ -14,14 +14,14 @@ namespace ash {
 // Translate paths from |source| VM to valid paths in the host. Invalid paths
 // are ignored.
 std::vector<base::FilePath> TranslateVMPathsToHost(
-    ui::EndpointType source,
+    const std::string& vm_name,
     const std::vector<ui::FileInfo>& vm_paths);
 
 // Share |files| with |target| VM, and translate |files| to be "file://" URLs
 // which can be used inside the vm. |callback| is invoked with translated
 // "file://" URLs.
 void ShareWithVMAndTranslateToFileUrls(
-    ui::EndpointType target,
+    const std::string& vm_name,
     const std::vector<base::FilePath>& files,
     base::OnceCallback<void(std::vector<std::string>)> callback);
 
@@ -46,7 +46,7 @@ class ChromeSecurityDelegate : public exo::SecurityDelegate {
                   const base::Pickle& pickle,
                   SendDataCallback callback) override;
 
-  virtual std::string GetVmName() const;
+  virtual std::string GetVmName(ui::EndpointType target) const;
 };
 
 }  // namespace ash
