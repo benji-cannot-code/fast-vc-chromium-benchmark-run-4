6948f33b16797ad29ac6e6856afcc0ff83b5bc18
Fumitoshi Ukai <ukai@google.com>
2024-02-29 16:47:20 +0000

    fix InputScope.h case
    inputscope.h can not be found on case sensitive filesystem.
    Use InputScope.h instead
    
    Change-Id: Ia2db7a0d7e5674594da62bc0f0c944ea1716d4cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5334417
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Cr-Commit-Position: refs/heads/main@{#1267040}

--- ui/base/ime/win/tsf_input_scope.h
+++ ui/base/ime/win/tsf_input_scope.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_BASE_IME_WIN_TSF_INPUT_SCOPE_H_
 #define UI_BASE_IME_WIN_TSF_INPUT_SCOPE_H_
 
-#include <inputscope.h>
+#include <InputScope.h>
 #include <vector>
 
 #include "base/component_export.h"
