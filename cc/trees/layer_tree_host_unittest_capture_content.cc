2ac867128e3f95749e57635661e16121ff1d129e
Nico Weber <thakis@chromium.org>
2024-02-29 17:19:17 +0000

    Remove a few more vector<const T> in tests
    See bug for context, e.g. comment 16.
    
    In some cases, I made the vector const. In others, I just
    removed it.
    
    Some of the confusion here seems to be that people think a
    span<const T> can only be created from a vector<const T>.
    That's not the case: A span<const T> makes sense – it's a
    readonly view into a series of elements. A vector<const T>
    doesn't make much sense (you can just assign to the vector
    a new vector with an element removed or replaced), and a
    vector<T> converts to a span<const T> just fine.
    
    No behavior change.
    
    Bug: 323708866
    Change-Id: I939f3bdaf4ea91272f6f96a4f5d24778a656f969
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5333161
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Owners-Override: Nico Weber <thakis@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267059}

--- cc/trees/layer_tree_host_unittest_capture_content.cc
+++ cc/trees/layer_tree_host_unittest_capture_content.cc
@@ -38,7 +38,7 @@ class FakeCaptureContentLayerClient : public FakeContentLayerClient {
 
   scoped_refptr<DisplayItemList> PaintContentsToDisplayList() override {
     auto display_list = base::MakeRefCounted<DisplayItemList>();
-    for (auto& holder : holders_) {
+    for (const auto& holder : holders_) {
       display_list->StartPaint();
       SkFont font = skia::DefaultFont();
       display_list->push<DrawTextBlobOp>(
@@ -53,7 +53,7 @@ class FakeCaptureContentLayerClient : public FakeContentLayerClient {
   }
 
  private:
-  std::vector<const FakeTextHolder> holders_;
+  std::vector<FakeTextHolder> holders_;
 };
 
 // These tests are for LayerTreeHost::CaptureContent().
--- chrome/browser/web_applications/chromeos_web_app_experiments.h
+++ chrome/browser/web_applications/chromeos_web_app_experiments.h
@@ -49,7 +49,7 @@ class ChromeOsWebAppExperiments {
 
   static void SetAlwaysEnabledForTesting();
   static void SetScopeExtensionsForTesting(
-      std::vector<const char* const> scope_extensions_override);
+      std::vector<const char*> scope_extensions_override);
   static void ClearOverridesForTesting();
 
   ChromeOsWebAppExperiments() = delete;
