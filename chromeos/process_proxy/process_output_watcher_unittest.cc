b28eb1ab24f4d11702f16afad69cb28bc75db68e
Helmut Januschka <helmut@januschka.com>
2024-03-01 05:02:31 +0000

    process_proxy: base::StringPiece -> std::string_view
    Bug: 691162
    Change-Id: I4c377ae4e6f9d07a7db411599208a70ffbc6311a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5321262
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267370}

--- chromeos/process_proxy/process_output_watcher_unittest.cc
+++ chromeos/process_proxy/process_output_watcher_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <queue>
 #include <string>
+#include <string_view>
 #include <vector>
 
 #include "base/files/file_util.h"
@@ -175,7 +176,7 @@ class ProcessOutputWatcherTest : public testing::Test {
       if (test_cases[i].should_send_terminating_null)
         test_size += sizeof(*test_str.c_str());
       EXPECT_TRUE(base::WriteFileDescriptor(
-          pt_pipe[1], base::StringPiece(test_str.c_str(), test_size)));
+          pt_pipe[1], std::string_view(test_str.c_str(), test_size)));
 
       run_loop.Run();
       EXPECT_TRUE(expectations_.IsDone());
