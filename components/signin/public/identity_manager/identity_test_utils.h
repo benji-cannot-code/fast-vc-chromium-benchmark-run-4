153028dac97e2f813bea0d9d8a814a0753198262
David Roger <droger@chromium.org>
2024-03-01 13:08:10 +0000

    [signin][tests] Identity test utils don't use unknown access point by default
    The main change is in identity_test_utils.h
    
    ACCESS_POINT_UNKNOWN is not something that is used in production, and
    triggers edge cases and errors. In particular it does not count as an
    explicit signin for Uno.
    
    This CL uses a more conventional access point, to match what is
    happening in production.
    
    As a result, UNO gets enabled in many more tests.
    Some of them are testing pre-UNO behavior, and for those tests, the
    UNO feature is explicitly disabled.
    
    Change-Id: Idf379dfe307af289e7099b0850c017981c6eab42
    Cq-Do-Not-Cancel-Tryjobs: true
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5300960
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267411}

--- components/signin/public/identity_manager/identity_test_utils.h
+++ components/signin/public/identity_manager/identity_test_utils.h
@@ -160,7 +160,7 @@ struct AccountAvailabilityOptions {
       nullptr;
 
   const signin_metrics::AccessPoint access_point =
-      signin_metrics::AccessPoint::ACCESS_POINT_UNKNOWN;
+      signin_metrics::AccessPoint::ACCESS_POINT_SETTINGS;
 
   explicit AccountAvailabilityOptions(base::StringPiece email);
   ~AccountAvailabilityOptions();
@@ -231,7 +231,7 @@ class AccountAvailabilityOptionsBuilder {
   std::optional<std::string> refresh_token_ = std::string();
   bool with_cookie_ = false;
   signin_metrics::AccessPoint access_point_ =
-      signin_metrics::AccessPoint::ACCESS_POINT_UNKNOWN;
+      signin_metrics::AccessPoint::ACCESS_POINT_SETTINGS;
 };
 
 // Sets up an account identified by `email` according to options provided. See
