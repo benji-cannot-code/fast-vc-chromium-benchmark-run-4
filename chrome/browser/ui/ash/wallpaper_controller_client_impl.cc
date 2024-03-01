e3f00801219693fb6884873778baada6a3d89b3b
Hidehiko Abe <hidehiko@chromium.org>
2024-03-01 04:18:12 +0000

    Move WallpaperControllerClientImpl out from ChromeUserManagerImpl.
    Now OnUserLoggedIn is called for every type of User,
    move to WallpaperControllerClientImpl.
    This CL reverses the dependency between WallpaperControllerClientImpl
    and ChromeUserManagerImpl.
    
    BUG=b/278643115
    TEST=Tryjob
    
    Change-Id: Ib32892b225b3b3974822fdf7df96cbbdc71154ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5333256
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267363}

--- chrome/browser/ui/ash/wallpaper_controller_client_impl.cc
+++ chrome/browser/ui/ash/wallpaper_controller_client_impl.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/personalization_app/personalization_app_url_constants.h"
 #include "ash/webui/personalization_app/proto/backdrop_wallpaper.pb.h"
 #include "ash/webui/system_apps/public/system_web_app_type.h"
+#include "base/check_is_test.h"
 #include "base/command_line.h"
 #include "base/functional/bind.h"
 #include "base/hash/hash.h"
@@ -184,6 +185,12 @@ WallpaperControllerClientImpl::WallpaperControllerClientImpl(
   // SessionManager might not exist in unit tests.
   if (session_manager)
     session_observation_.Observe(session_manager);
+
+  if (user_manager::UserManager::IsInitialized()) {
+    user_manager_observation_.Observe(user_manager::UserManager::Get());
+  } else {
+    CHECK_IS_TEST();
+  }
 }
 
 WallpaperControllerClientImpl::~WallpaperControllerClientImpl() {
@@ -359,6 +366,16 @@ void WallpaperControllerClientImpl::OnUserProfileLoaded(
     const AccountId& account_id) {
   wallpaper_controller_->SyncLocalAndRemotePrefs(account_id);
   ObserveVolumeManagerForAccountId(account_id);
+}
+
+void WallpaperControllerClientImpl::OnUserLoggedIn(
+    const user_manager::User& user) {
+  // For public account, it's possible that the user-policy controlled wallpaper
+  // was fetched/cleared at the login screen (while for a regular user it was
+  // always fetched/cleared inside a user session), in the case the user-policy
+  // controlled wallpaper was fetched/cleared but not updated in the login
+  // screen, we need to update the wallpaper after the public user logged in.
+  ShowUserWallpaper(user.GetAccountId());
 }
 
 void WallpaperControllerClientImpl::DeviceWallpaperImageFilePathChanged() {
