18ba0e6839da9a6010d577009b202910bfa337fd
Lia Hiscock <liahiscock@microsoft.com>
2024-03-01 01:14:54 +0000

    [dPWA][SystemMediaControls] Fix flaky ASAN browser test
    WebAppSystemMediaControlsBrowserTest.SimpleOneBrowserTest was flaky on Windows asan builds.
    
    Add a call to the waiter to ensure the MediaKeysListenerManagerImpl is ready to receive media key events.
    
    Bug: 325016212
    Change-Id: I8068a2d0ea701c47c6f7aac872823c614c0aafa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5325594
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Lia Hiscock <liahiscock@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1267321}

--- content/browser/media/web_app_system_media_controls_browsertest.cc
+++ content/browser/media/web_app_system_media_controls_browsertest.cc
@@ -208,13 +208,8 @@ class WebAppSystemMediaControlsBrowserTest
   base::test::ScopedFeatureList feature_list_;
 };
 
-#if BUILDFLAG(IS_WIN) && defined(ADDRESS_SANITIZER)
-#define MAYBE_SimpleOneBrowserTest DISABLED_SimpleOneBrowserTest
-#else
-#define MAYBE_SimpleOneBrowserTest SimpleOneBrowserTest
-#endif
 IN_PROC_BROWSER_TEST_F(WebAppSystemMediaControlsBrowserTest,
-                       MAYBE_SimpleOneBrowserTest) {
+                       SimpleOneBrowserTest) {
   GURL http_url(https_server()->GetURL("/media/session/media-session.html"));
   EXPECT_TRUE(NavigateToURL(shell(), http_url));
 
@@ -222,6 +217,9 @@ IN_PROC_BROWSER_TEST_F(WebAppSystemMediaControlsBrowserTest,
   StartPlaybackAndWait(shell(), "long-video-loop");
   // Check video is playing.
   EXPECT_TRUE(IsPlaying(shell(), "long-video-loop"));
+
+  bool is_for_pwa = WaitForStartWatchingMediaKey();
+  EXPECT_FALSE(is_for_pwa);
 
   // Hit pause via simulating SMTC pause.
   MediaKeysListenerManagerImpl* media_keys_listener_manager_impl =
