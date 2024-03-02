b07ff79e6d7c43a7b1f06251ea38309c56af1aa6
Robert Ogden <robertogden@chromium.org>
2024-03-02 04:29:36 +0000

    Allow the url fragment to be matched with optimization filters
    Leaves the username and password fields cleared.
    
    Change-Id: I2bd6424e6257b361fa1fa0336da612c04b2223af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5337539
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267502}

--- components/optimization_guide/core/optimization_filter.cc
+++ components/optimization_guide/core/optimization_filter.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "crypto/sha2.h"
+#include "url/gurl.h"
 
 namespace optimization_guide {
 
@@ -26,7 +27,12 @@ bool MatchesRegexp(const GURL& url, const RegexpList& regexps) {
   if (!url.is_valid())
     return false;
 
-  std::string clean_url = base::ToLowerASCII(url.GetAsReferrer().spec());
+  GURL::Replacements replace_url_auth;
+  replace_url_auth.ClearUsername();
+  replace_url_auth.ClearPassword();
+  std::string clean_url =
+      base::ToLowerASCII(url.ReplaceComponents(replace_url_auth).spec());
+
   for (auto& regexp : regexps) {
     if (!regexp->ok()) {
       continue;
