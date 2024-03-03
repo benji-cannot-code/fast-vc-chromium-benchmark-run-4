ef15eab14fce6314a7fe7dd02c134914006780fa
Jan Keitel <jkeitel@google.com>
2024-03-03 22:16:50 +0000

    Fix crash in MaybeLogAutocompleteSuppressionByPlusAddresses.
    Bug: 327866993
    Change-Id: Icb01abb11091b02fdd743ddc5bb79984d88d9441
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5331800
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1267642}

--- components/autofill/core/browser/browser_autofill_manager.cc
+++ components/autofill/core/browser/browser_autofill_manager.cc
@@ -274,7 +274,7 @@ bool IsSingleFieldFormFillerFillingProduct(FillingProduct filling_product) {
 void MaybeLogAutocompleteSuppressionByPlusAddresses(
     AutofillClient& client,
     base::span<const Suggestion> suggestions,
-    AutofillField* focused_field) {
+    FieldTypeGroup focused_field_type_group) {
   if (client.IsOffTheRecord()) {
     return;
   }
@@ -296,8 +296,7 @@ void MaybeLogAutocompleteSuppressionByPlusAddresses(
   // If the focused field is not classified as an email address, plus addresses
   // would never be shown.
   using enum AutocompleteSuppressionByPlusAddress;
-  if (!focused_field ||
-      focused_field->Type().group() != FieldTypeGroup::kEmail) {
+  if (focused_field_type_group != FieldTypeGroup::kEmail) {
     base::UmaHistogramEnumeration(kAutocompleteSuppressionByPlusAddressUma,
                                   kNotSuppressed);
     return;
@@ -1234,13 +1233,14 @@ void BrowserAutofillManager::OnAskForValuesToFillImpl(
               base::BindRepeating(
                   [](base::WeakPtr<BrowserAutofillManager> self,
                      base::TimeTicks request_start_time,
-                     AutofillField* focused_field, FieldGlobalId field_id,
+                     FieldTypeGroup focused_field_type_group,
+                     FieldGlobalId field_id,
                      const std::vector<Suggestion>& suggestions) {
                     if (!self) {
                       return;
                     }
                     MaybeLogAutocompleteSuppressionByPlusAddresses(
-                        self->client(), suggestions, focused_field);
+                        self->client(), suggestions, focused_field_type_group);
                     LogTimeDelayForSingleFieldFormFill(
                         suggestions,
                         base::TimeTicks::Now() - request_start_time);
@@ -1248,7 +1248,8 @@ void BrowserAutofillManager::OnAskForValuesToFillImpl(
                         field_id, suggestions);
                   },
                   weak_ptr_factory_.GetWeakPtr(), base::TimeTicks::Now(),
-                  context.focused_field),
+                  context.focused_field ? context.focused_field->Type().group()
+                                        : FieldTypeGroup::kNoGroup),
               context);
       if (handled_by_single_field_form_filler) {
         return false;
