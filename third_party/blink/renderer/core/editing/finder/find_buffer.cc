179d6be5b910f1a471d0c15b78c15ef01bfc8aae
zhoupeng <zhoupeng.1996@bytedance.com>
2024-03-03 21:12:24 +0000

    Make sure that the inert element and its subtree cannot be searchable
    As per the HTML spec:
    Content in an inert element and its subtree will not be perceivable
    by all users, or interactive.
    
    Consider this test case:
    <body>
      <div inert>Do not find me please</div>
    </body>
    The return value of `window.find('me')` is true, but `div` is
    non-interactive due to the inert attribute being assigned to it.
    `ShouldIgnoreContents` should assess the inert attribute of the element.
    The reason why some cases in `inert/inert-and-find.html` cannot be
    passed is also attributed to this factor.
    
    Bug: 327017912
    Change-Id: I5b4b15859c9e9c31f57b1d5cfc44509dc37105aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5319168
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Peng Zhou <zhoupeng.1996@bytedance.com>
    Cr-Commit-Position: refs/heads/main@{#1267634}

--- third_party/blink/renderer/core/editing/finder/find_buffer.cc
+++ third_party/blink/renderer/core/editing/finder/find_buffer.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_block_flow.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/text/unicode_utilities.h"
 #include "third_party/blink/renderer/platform/wtf/text/character_names.h"
 #include "third_party/blink/renderer/platform/wtf/text/unicode.h"
@@ -39,7 +40,9 @@ bool ShouldIgnoreContents(const Node& node) {
   const auto* element = DynamicTo<HTMLElement>(node);
   if (!element)
     return false;
-  return (!element->ShouldSerializeEndTag() &&
+  return (RuntimeEnabledFeatures::InertElementNonSearchableEnabled() &&
+          element->IsInertRoot()) ||
+         (!element->ShouldSerializeEndTag() &&
           !IsA<HTMLInputElement>(*element)) ||
          (IsA<TextControlElement>(*element) &&
           !To<TextControlElement>(*element).SuggestedValue().empty()) ||
