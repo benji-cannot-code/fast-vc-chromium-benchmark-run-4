f3803f8f4098d87f92c75f129ff615414c26f5b2
Kartik Hegde <khegde@google.com>
2024-03-04 19:51:21 +0000

    traffic_counters: Remove all auto reset enabled code
    This code was originally added to support the toggle functionality for traffic counters. Since the toggle functionality is no longer supported,  remove it from the UX and model. See bug for additional details.
    
    BUG=b:324745923
    TEST=existing unit tests
    
    Change-Id: Ica2e50b93db3814ccfd5eae5a3cb043f8a1f8aa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5284327
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Commit-Queue: Kartik Hegde <khegde@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268015}

--- ash/webui/common/resources/traffic_counters/traffic_counters_adapter.ts
+++ ash/webui/common/resources/traffic_counters/traffic_counters_adapter.ts
@@ -26,7 +26,6 @@ export interface Network {
   counters: TrafficCounter[];
   lastResetTime: Time|null;
   friendlyDate: string|null;
-  autoReset: boolean;
   userSpecifiedResetDay: number;
 }
 
@@ -41,7 +40,6 @@ function createNetwork(
     counters: TrafficCounter[],
     lastResetTime: Time|null,
     friendlyDate: string|null,
-    autoReset: boolean,
     userSpecifiedResetDay: number): Network {
   return {
     guid: guid,
@@ -50,7 +48,6 @@ function createNetwork(
     counters: counters,
     lastResetTime: lastResetTime,
     friendlyDate: friendlyDate,
-    autoReset: autoReset,
     userSpecifiedResetDay: userSpecifiedResetDay,
   };
 }
@@ -85,14 +82,11 @@ export class TrafficCountersAdapter {
           await this.requestLastResetTimeForNetwork(networkState.guid);
       const friendlyDate =
           await this.requestFriendlyDateForNetwork(networkState.guid);
-      const autoReset =
-          await this.requestEnableAutoResetBooleanForNetwork(networkState.guid);
       const userSpecifiedResetDay =
           await this.requestUserSpecifiedResetDayForNetwork(networkState.guid);
       networks.push(createNetwork(
           networkState.guid, networkState.name, networkState.type,
-          trafficCounters, lastResetTime, friendlyDate, autoReset,
-          userSpecifiedResetDay));
+          trafficCounters, lastResetTime, friendlyDate, userSpecifiedResetDay));
     }
     return networks;
   }
@@ -144,19 +138,6 @@ export class TrafficCountersAdapter {
   }
 
   /**
-   * Requests enable traffic counters auto reset boolean for the given network.
-   */
-  async requestEnableAutoResetBooleanForNetwork(
-    guid: string): Promise<boolean> {
-    const managedPropertiesPromise =
-        await this.networkConfig_.getManagedProperties(guid);
-    if (!managedPropertiesPromise || !managedPropertiesPromise.result) {
-      return false;
-    }
-    return managedPropertiesPromise.result.trafficCounterProperties.autoReset;
-  }
-
-  /**
    * Requests user specified reset day for the given network.
    */
   async requestUserSpecifiedResetDayForNetwork(guid: string): Promise<number> {
@@ -172,11 +153,8 @@ export class TrafficCountersAdapter {
   /**
    * Sets values for auto reset.
    */
-  async setTrafficCountersAutoResetForNetwork(
-    guid: string,
-    autoReset: boolean,
-    resetDay: UInt32Value|null): Promise<void> {
-    await this.networkConfig_.setTrafficCountersAutoReset(
-        guid, autoReset, resetDay);
+  async setTrafficCountersResetDayForNetwork(
+      guid: string, resetDay: UInt32Value|null): Promise<void> {
+    await this.networkConfig_.setTrafficCountersResetDay(guid, resetDay);
   }
 }
