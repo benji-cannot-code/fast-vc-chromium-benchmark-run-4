c3385f681c51f11edc84e9e4a4a3a8a80207a108
Tommy C. Li <tommycli@chromium.org>
2024-03-04 23:22:03 +0000

    History Embeddings: Flesh out the SqlDatabase for passages
    Bug: 326659533
    Change-Id: I562e17657db65e7bad34c84b752acde9bea35e39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5335194
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268142}

--- components/history_embeddings/passages_util.h
+++ components/history_embeddings/passages_util.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_HISTORY_EMBEDDINGS_PASSAGES_UTIL_H_
 #define COMPONENTS_HISTORY_EMBEDDINGS_PASSAGES_UTIL_H_
 
+#include <optional>
+
 #include "base/containers/span.h"
 #include "components/history_embeddings/proto/history_embeddings.pb.h"
 
