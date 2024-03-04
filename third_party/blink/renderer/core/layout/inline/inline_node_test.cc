9dbeed13c69131e9bc756664e2a244c91e555795
Kent Tamura <tkent@chromium.org>
2024-03-04 07:43:17 +0000

    Layout: Reuse script segmentation result
    We can reuse the previous segmentation result if text_content is
    identical.
    
    This CL is a performance optimization, and this improves the score of
    Speedometer3 Editor-TipTap by approximately 6%.
    
    Bug: 41485014
    Change-Id: I40df560c7026b6f45405cd171a4dfcd7f747fe46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5334377
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267718}

--- third_party/blink/renderer/core/layout/inline/inline_node_test.cc
+++ third_party/blink/renderer/core/layout/inline/inline_node_test.cc
@@ -83,7 +83,7 @@ class InlineNodeForTest : public InlineNode {
   void SegmentText() {
     InlineNodeData* data = MutableData();
     data->is_bidi_enabled_ = true;
-    InlineNode::SegmentText(data);
+    InlineNode::SegmentText(data, nullptr);
   }
 
   void CollectInlines() { InlineNode::CollectInlines(MutableData()); }
