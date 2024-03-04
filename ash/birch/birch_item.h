95906aea5732f22b9d58d11834d4aee98eb9ce70
James Cook <jamescook@chromium.org>
2024-03-04 21:52:01 +0000

    birch: Add support for BirchItem::LoadIcon()
    This invokes a callback with an icon for the birch item. It may run the
    callback immediately (if an image is already available) or there may be
    a delay for a network fetch.
    
    Remove BirchCalendarItem::icon_url because the Calendar API does not
    provide an icon url.
    
    Bug: b:305093221
    Test: added to ash_unittests
    Change-Id: Ibd5d31b369553e2611bb7de2e53fc21916df2413
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5335903
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268062}

--- ash/birch/birch_item.h
+++ ash/birch/birch_item.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "base/files/file_path.h"
+#include "base/functional/callback_forward.h"
 #include "base/time/time.h"
 #include "ui/base/models/image_model.h"
 #include "url/gurl.h"
@@ -18,7 +19,7 @@ namespace ash {
 
 // The base item which is stored by the birch model.
 struct ASH_EXPORT BirchItem {
-  BirchItem(const std::u16string& title, const ui::ImageModel icon);
+  explicit BirchItem(const std::u16string& title);
   BirchItem(BirchItem&&);
   BirchItem& operator=(BirchItem&&);
   BirchItem(const BirchItem&);
@@ -27,7 +28,6 @@ struct ASH_EXPORT BirchItem {
   bool operator==(const BirchItem& rhs) const;
 
   std::u16string title;
-  ui::ImageModel icon;
   float ranking;  // Lower is better.
 
   virtual const char* GetItemType() const = 0;
@@ -37,6 +37,11 @@ struct ASH_EXPORT BirchItem {
 
   // Perform the action associated with this item (e.g. open a document).
   virtual void PerformAction() = 0;
+
+  // Loads the icon for this image. This may invoke the callback immediately
+  // (e.g. with a local icon) or there may be a delay for a network fetch.
+  using LoadIconCallback = base::OnceCallback<void(const ui::ImageModel&)>;
+  virtual void LoadIcon(LoadIconCallback callback) = 0;
 };
 
 // A birch item which contains calendar event information.
@@ -53,8 +58,8 @@ struct ASH_EXPORT BirchCalendarItem : public BirchItem {
   const char* GetItemType() const override;
   std::string ToString() const override;
   void PerformAction() override;
+  void LoadIcon(LoadIconCallback callback) override;
 
-  GURL icon_url;
   base::Time start_time;
   base::Time end_time;
   // Link to the event in the Google Calendar UI.
@@ -80,6 +85,7 @@ struct ASH_EXPORT BirchAttachmentItem : public BirchItem {
   const char* GetItemType() const override;
   std::string ToString() const override;
   void PerformAction() override;
+  void LoadIcon(LoadIconCallback callback) override;
 
   GURL file_url;          // Link to the file.
   GURL icon_url;          // Link to the file's icon's art asset.
@@ -105,6 +111,7 @@ struct ASH_EXPORT BirchFileItem : public BirchItem {
   const char* GetItemType() const override;
   std::string ToString() const override;
   void PerformAction() override;
+  void LoadIcon(LoadIconCallback callback) override;
 };
 
 // A birch item which contains tab and session information.
@@ -131,6 +138,7 @@ struct ASH_EXPORT BirchTabItem : public BirchItem {
   const char* GetItemType() const override;
   std::string ToString() const override;
   void PerformAction() override;
+  void LoadIcon(LoadIconCallback callback) override;
 };
 
 struct ASH_EXPORT BirchWeatherItem : public BirchItem {
@@ -144,6 +152,7 @@ struct ASH_EXPORT BirchWeatherItem : public BirchItem {
   ~BirchWeatherItem() override;
 
   std::u16string temperature;
+  ui::ImageModel icon;
 
   static constexpr char kItemType[] = "WeatherItem";
 
@@ -151,6 +160,7 @@ struct ASH_EXPORT BirchWeatherItem : public BirchItem {
   const char* GetItemType() const override;
   std::string ToString() const override;
   void PerformAction() override;
+  void LoadIcon(LoadIconCallback callback) override;
 };
 
 }  // namespace ash
