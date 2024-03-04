b5f666de875447981ef69bd161d2d5d695acc288
Ben Reich <benreich@chromium.org>
2024-03-04 23:04:41 +0000

    [FspCache] Introduce ContentLRUCache to retain a list of items
    An in-memory LRU cache will be used to retain the recency of files as
    they are written to the disk as well as retrieved from the disk. This
    will enable eviction of the files when space needs to be reclaimed.
    
    The underlying data structure will be a std::unordered_map with the hash
    key being the std::string representation of a base::FilePath. This
    enables O(1) lookup so the actual data pertaining to the file will be
    stored here as well.
    
    For now there is not much additional complexity added against the LRU
    cache except allowing for it to be initialized from state. This is to
    enable the state of cached files to be initialized in their LRU order
    without starting from scratch again.
    
    Bug: b/317137739
    Test: unit_tests --gtest_filter=FileSystemProviderContentLRUCacheTest*
    Change-Id: I64a51380707a3ff35f642d6aab0da5148a8f7946
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5337462
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Cassy Chun-Crogan <cassycc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1268135}

---
+++ chrome/browser/ash/file_system_provider/content_cache/content_lru_cache.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/file_system_provider/content_cache/content_lru_cache.h"
+
+#include "base/containers/adapters.h"
+
+namespace ash::file_system_provider {
+
+ContentLRUCache::ContentLRUCache()
+    : base::HashingLRUCache<base::FilePath, CacheFileContext>(NO_AUTO_EVICT) {}
+ContentLRUCache::~ContentLRUCache() = default;
+
+void ContentLRUCache::Init(std::list<PathContextPair> initial_order) {
+  for (const auto& [key, value] : base::Reversed(initial_order)) {
+    Put(key, value);
+  }
+}
+
+}  // namespace ash::file_system_provider
