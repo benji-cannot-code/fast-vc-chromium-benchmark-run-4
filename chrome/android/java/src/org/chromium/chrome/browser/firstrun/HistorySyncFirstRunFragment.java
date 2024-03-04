f129955a0366c68318e29c44ab67d18f81974296
Ioana Pandele <ioanap@chromium.org>
2024-03-04 12:04:44 +0000

    Revert "[UNOp2][Android] Change button layout for the history sync screen"
    This reverts commit d4bcb701b126dde643275c412e1ceddef1161590.
    
    Reason for revert: Potential culprit for failing tests on
    https://ci.chromium.org/ui/p/chromium/builders/ci/android-12l-x64-dbg-tests/15818/
    
    The failing tests are:
    - FirstRunActivitySigninAndSyncTest#acceptingHistorySyncEndsFreAndEnablesHistorySync
    - FirstRunActivitySigninAndSyncTest#refusingHistorySyncEndsFreAndDoesNotEnableHistorySync
    
    They seems to not be able to find the positive/negative buttons in
    the layout.
    
    Original change's description:
    > [UNOp2][Android] Change button layout for the history sync screen
    >
    > UX-approved screenshots: http://b/41493766#comment15
    >
    > Bug: b/41493766
    > Change-Id: I32ed5e53fe11aaef5d20d02fb15f81b180579eb4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5328486
    > Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    > Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1267746}
    
    Bug: b/41493766
    Change-Id: I271ffe222b02c2a11ff1a94a3b2faaffe07d58c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5339750
    Owners-Override: Ioana Pandele <ioanap@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267779}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/HistorySyncFirstRunFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/HistorySyncFirstRunFragment.java
@@ -76,8 +76,8 @@ public class HistorySyncFirstRunFragment extends Fragment
     }
 
     @Override
-    public boolean isLargeScreen() {
-        return !getPageDelegate().canUseLandscapeLayout();
+    public boolean canUseLandscapeLayout() {
+        return getPageDelegate().canUseLandscapeLayout();
     }
 
     private void createCoordinatorAndAddToFragment() {
--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/history_sync/HistorySyncTest.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/history_sync/HistorySyncTest.java
@@ -14,7 +14,6 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.allOf;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
-import static org.mockito.Mockito.when;
 
 import android.view.LayoutInflater;
 
@@ -77,7 +76,6 @@ public class HistorySyncTest {
         mActivityTestRule.launchActivity(null);
         mSigninTestRule.addTestAccountThenSignin();
         SyncServiceFactory.setInstanceForTesting(mSyncServiceMock);
-        when(mHistorySyncDelegateMock.isLargeScreen()).thenReturn(false);
     }
 
     @After
