e4030cb95adbb77e6eef5baf933cd05990c45e76
Rune Lillesveen <futhark@chromium.org>
2024-03-04 18:28:36 +0000

    [css-anchor-position] Add @position-try rules to RuleSet
    And collect them into the ScopedStyleResolver.
    
    Bug: 40279608
    Change-Id: I240ecc6daf0cf2ec9929151d54464d7bae6e3325
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5331734
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267955}

--- third_party/blink/renderer/core/css/resolver/scoped_style_resolver.h
+++ third_party/blink/renderer/core/css/resolver/scoped_style_resolver.h
@@ -70,6 +70,7 @@ class CORE_EXPORT ScopedStyleResolver final
 
   StyleRulePositionFallback* PositionFallbackForName(
       const AtomicString& fallback_name);
+  StyleRulePositionTry* PositionTryForName(const AtomicString& try_name);
 
   StyleRuleFunction* FunctionForName(StringView name);
 
@@ -120,6 +121,7 @@ class CORE_EXPORT ScopedStyleResolver final
       const StyleRuleKeyframes* new_rule,
       const StyleRuleKeyframes* existing_rule) const;
   void AddPositionFallbackRules(const RuleSet&);
+  void AddPositionTryRules(const RuleSet&);
   void AddFunctionRules(const RuleSet&);
 
   CounterStyleMap& EnsureCounterStyleMap();
@@ -142,6 +144,10 @@ class CORE_EXPORT ScopedStyleResolver final
   using PositionFallbackRuleMap =
       HeapHashMap<AtomicString, Member<StyleRulePositionFallback>>;
   PositionFallbackRuleMap position_fallback_rule_map_;
+
+  using PositionTryRuleMap =
+      HeapHashMap<AtomicString, Member<StyleRulePositionTry>>;
+  PositionTryRuleMap position_try_rule_map_;
 
   using FunctionRuleMap = HeapHashMap<String, Member<StyleRuleFunction>>;
   FunctionRuleMap function_rule_map_;
--- third_party/blink/renderer/core/css/rule_set.h
+++ third_party/blink/renderer/core/css/rule_set.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/cascade_layer.h"
 #include "third_party/blink/renderer/core/css/css_keyframes_rule.h"
 #include "third_party/blink/renderer/core/css/css_position_fallback_rule.h"
+#include "third_party/blink/renderer/core/css/css_position_try_rule.h"
 #include "third_party/blink/renderer/core/css/media_query_evaluator.h"
 #include "third_party/blink/renderer/core/css/resolver/media_query_result.h"
 #include "third_party/blink/renderer/core/css/robin_hood_map.h"
@@ -471,6 +472,9 @@ class CORE_EXPORT RuleSet final : public GarbageCollected<RuleSet> {
       const {
     return position_fallback_rules_;
   }
+  const HeapVector<Member<StyleRulePositionTry>>& PositionTryRules() const {
+    return position_try_rules_;
+  }
   const HeapVector<Member<StyleRuleFunction>>& FunctionRules() const {
     return function_rules_;
   }
@@ -593,6 +597,7 @@ class CORE_EXPORT RuleSet final : public GarbageCollected<RuleSet> {
   void AddFontPaletteValuesRule(StyleRuleFontPaletteValues*);
   void AddFontFeatureValuesRule(StyleRuleFontFeatureValues*);
   void AddPositionFallbackRule(StyleRulePositionFallback*);
+  void AddPositionTryRule(StyleRulePositionTry*);
   void AddFunctionRule(StyleRuleFunction*);
   void AddViewTransitionRule(StyleRuleViewTransition*);
 
@@ -707,6 +712,7 @@ class CORE_EXPORT RuleSet final : public GarbageCollected<RuleSet> {
   HeapVector<Member<StyleRuleProperty>> property_rules_;
   HeapVector<Member<StyleRuleCounterStyle>> counter_style_rules_;
   HeapVector<Member<StyleRulePositionFallback>> position_fallback_rules_;
+  HeapVector<Member<StyleRulePositionTry>> position_try_rules_;
   HeapVector<MediaQuerySetResult> media_query_set_results_;
   HeapVector<Member<StyleRuleFunction>> function_rules_;
 
