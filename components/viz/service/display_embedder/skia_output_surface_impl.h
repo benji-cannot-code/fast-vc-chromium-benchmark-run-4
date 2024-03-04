4c74897bdaaf889c008608548285926d5fbbda2e
Sunny Sachanandani <sunnyps@chromium.org>
2024-03-04 13:49:55 +0000

    graphite: Do not use non-volatile promise images with swiftshader
    Instead of unconditionally using non-volatile promise images on Mac,
    check if the Dawn device is using Metal. If it's using swiftshader the
    DawnFallbackImageRepresentation that's used doesn't cache textures
    between accesses so using non-volatile promise images is incorrect.
    
    Bug: 41495039
    Change-Id: I79e20839d07f40970622a316a8a996bcaf564e16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5336460
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267805}

--- components/viz/service/display_embedder/skia_output_surface_impl.h
+++ components/viz/service/display_embedder/skia_output_surface_impl.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/buildflags.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "third_party/skia/include/core/SkOverdrawCanvas.h"
+#include "third_party/skia/include/gpu/graphite/GraphiteTypes.h"
 #include "third_party/skia/include/private/chromium/GrDeferredDisplayListRecorder.h"
 #include "third_party/skia/include/private/chromium/GrSurfaceCharacterization.h"
 #include "ui/gfx/presentation_feedback.h"
@@ -435,6 +436,8 @@ class VIZ_SERVICE_EXPORT SkiaOutputSurfaceImpl : public SkiaOutputSurface {
   raw_ptr<skgpu::graphite::Recorder> graphite_recorder_ = nullptr;
   scoped_refptr<gpu::raster::GraphiteCacheController>
       graphite_cache_controller_;
+  skgpu::graphite::Volatile graphite_use_volatile_promise_images_ =
+      skgpu::graphite::Volatile::kYes;
 
   bool has_set_draw_rectangle_for_frame_ = false;
   std::optional<gfx::Rect> draw_rectangle_;
