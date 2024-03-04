9df028abc6f54a2fe5a41b121f1172a6292f5960
siashah <siashah@chromium.org>
2024-03-04 20:04:34 +0000

    Cache Pix bank accounts in PersonalDataManager
    This change adds a GetBankAccounts method in PersonalDataManager which
    queries the database layer and returns a vector of BankAccounts.
    
    Bug: 1472125
    Change-Id: Ic3bbba236c897d89abb32bb1024c0905a9744b60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5302343
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Commit-Queue: Siddharth Shah <siashah@chromium.org>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268027}

--- components/autofill/core/browser/webdata/autofill_webdata_backend_impl.cc
+++ components/autofill/core/browser/webdata/autofill_webdata_backend_impl.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/data_model/autofill_metadata.h"
 #include "components/autofill/core/browser/data_model/autofill_offer_data.h"
 #include "components/autofill/core/browser/data_model/autofill_wallet_usage_data.h"
+#include "components/autofill/core/browser/data_model/bank_account.h"
 #include "components/autofill/core/browser/data_model/credit_card.h"
 #include "components/autofill/core/browser/data_model/credit_card_benefit.h"
 #include "components/autofill/core/browser/data_model/credit_card_cloud_token_data.h"
@@ -866,6 +867,16 @@ AutofillWebDataBackendImpl::GetCreditCardBenefits(WebDatabase* db) {
       &credit_card_benefits);
   return std::make_unique<WDResult<std::vector<CreditCardBenefit>>>(
       CREDIT_CARD_BENEFIT_RESULT, std::move(credit_card_benefits));
+}
+
+std::unique_ptr<WDTypedResult>
+AutofillWebDataBackendImpl::GetMaskedBankAccounts(WebDatabase* db) {
+  CHECK(owning_task_runner()->RunsTasksInCurrentSequence());
+  std::vector<std::unique_ptr<BankAccount>> masked_bank_accounts;
+  PaymentsAutofillTable::FromWebDatabase(db)->GetMaskedBankAccounts(
+      masked_bank_accounts);
+  return std::make_unique<WDResult<std::vector<std::unique_ptr<BankAccount>>>>(
+      MASKED_BANK_ACCOUNTS_RESULT, std::move(masked_bank_accounts));
 }
 
 WebDatabase::State AutofillWebDataBackendImpl::ClearAllServerData(
