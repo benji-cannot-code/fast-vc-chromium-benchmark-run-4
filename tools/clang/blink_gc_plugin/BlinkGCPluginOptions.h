905f1f002c53453c7d64974128d28ef2aa35ac9e
Omer Katz <omerkatz@chromium.org>
2024-03-04 10:27:44 +0000

    [blink_gc_plugin] Enable optional of traceables check
    Bug: 41492939
    Change-Id: I3aec1021d6644dfce571e2af2448473437319e0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5326897
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267752}

--- tools/clang/blink_gc_plugin/BlinkGCPluginOptions.h
+++ tools/clang/blink_gc_plugin/BlinkGCPluginOptions.h
@@ -47,7 +47,7 @@ struct BlinkGCPluginOptions {
 
   // Enables checks for std::optional and absl::optional of non-GCed traceable
   // type.
-  bool enable_optional_of_traceable_check = false;
+  bool enable_optional_of_traceable_check = true;
 
   std::set<std::string> ignored_classes;
   std::set<std::string> checked_namespaces;
