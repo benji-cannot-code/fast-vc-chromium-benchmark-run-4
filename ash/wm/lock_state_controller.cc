9ef2b70a2d3d3e979ba5d44f7cfb7479a609e651
minch <minch@google.com>
2024-03-04 19:43:22 +0000

    pine: No pine image if all the windows are minimized
    Do not take the pine image if all the windows inside the active desk
    are minimized.
    
    Test: Added LockStateControllerPineTest.* + manually
    Bug: b/327498267
    Change-Id: I0444df75363183a2025af205c4b65bd0e51908f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5335899
    Commit-Queue: Min Chen <minch@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268010}

--- ash/wm/lock_state_controller.cc
+++ ash/wm/lock_state_controller.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wallpaper/views/wallpaper_widget_controller.h"
 #include "ash/wallpaper/wallpaper_controller_impl.h"
 #include "ash/wm/desks/desks_util.h"
+#include "ash/wm/mru_window_tracker.h"
 #include "ash/wm/overview/overview_controller.h"
 #include "ash/wm/session_state_animator_impl.h"
 #include "ash/wm/window_restore/window_restore_util.h"
@@ -142,16 +143,25 @@ void MaybeAppendTestCallback(Callback& callback,
 // Returns true if the pine screenshot should be taken on shutdown.
 bool ShouldTakePineScreeshot(aura::Window* active_desk) {
   auto* shell = Shell::Get();
-  // Do not take the pine screenshot if it is in overview mode, lock screen or
-  // in the home launcher page.
+  // Do not take the pine screenshot if it is in overview mode, lock screen,
+  // home launcher or there is no windows inside the active desk.
   if (shell->overview_controller()->InOverviewSession() ||
       shell->session_controller()->IsScreenLocked() ||
-      shell->app_list_controller()->IsHomeScreenVisible()) {
+      shell->app_list_controller()->IsHomeScreenVisible() ||
+      active_desk->children().empty()) {
     return false;
   }
 
-  // Do not take the pine screenshot if the active desk has no windows.
-  return !active_desk->children().empty();
+  for (aura::Window* window :
+       shell->mru_window_tracker()->BuildMruWindowList(kActiveDesk)) {
+    if (!WindowState::Get(window)->IsMinimized()) {
+      return true;
+    }
+  }
+
+  // Do not take the pine screenshot if all the windows inside the active desk
+  // are minimized.
+  return false;
 }
 
 }  // namespace
--- ash/wm/lock_state_controller_unittest.cc
+++ ash/wm/lock_state_controller_unittest.cc
@@ -1062,6 +1062,14 @@ class LockStateControllerPineTest : public LockStateControllerTest {
     LockStateControllerTest::TearDown();
   }
 
+  void RequestShutdown() {
+    base::RunLoop run_loop;
+    lock_state_test_api_->set_pine_image_callback(run_loop.QuitClosure());
+    lock_state_controller_->RequestShutdown(
+        ShutdownReason::TRAY_SHUT_DOWN_BUTTON);
+    run_loop.Run();
+  }
+
   const base::FilePath& file_path() const { return file_path_; }
 
  private:
@@ -1075,12 +1083,7 @@ class LockStateControllerPineTest : public LockStateControllerTest {
 TEST_F(LockStateControllerPineTest, ShutdownWithWindows) {
   std::unique_ptr<aura::Window> window = CreateTestWindow();
 
-  base::RunLoop run_loop;
-  lock_state_test_api_->set_pine_image_callback(run_loop.QuitClosure());
-  lock_state_controller_->RequestShutdown(
-      ShutdownReason::TRAY_SHUT_DOWN_BUTTON);
-  run_loop.Run();
-
+  RequestShutdown();
   // The pine image was taken and not empty.
   EXPECT_TRUE(base::PathExists(file_path()));
   int64_t file_size = 0;
@@ -1103,12 +1106,7 @@ TEST_F(LockStateControllerPineTest, ShutdownWithoutWindows) {
   // Create an empty file to simulate an old pine image.
   ASSERT_TRUE(base::WriteFile(file_path(), ""));
 
-  base::RunLoop run_loop;
-  lock_state_test_api_->set_pine_image_callback(run_loop.QuitClosure());
-  lock_state_controller_->RequestShutdown(
-      ShutdownReason::TRAY_SHUT_DOWN_BUTTON);
-  run_loop.Run();
-
+  RequestShutdown();
   // Existing pine image was deleted.
   EXPECT_FALSE(base::PathExists(file_path()));
 }
@@ -1121,12 +1119,7 @@ TEST_F(LockStateControllerPineTest, ShutdownInOverview) {
   CreateTestWindow();
   EnterOverview();
 
-  base::RunLoop run_loop;
-  lock_state_test_api_->set_pine_image_callback(run_loop.QuitClosure());
-  lock_state_controller_->RequestShutdown(
-      ShutdownReason::TRAY_SHUT_DOWN_BUTTON);
-  run_loop.Run();
-
+  RequestShutdown();
   // The pine image should not be taken if it is in overview when shutting down.
   // The existing pine image should be deleted as well.
   EXPECT_FALSE(base::PathExists(file_path()));
@@ -1141,12 +1134,7 @@ TEST_F(LockStateControllerPineTest, ShutdownInLockScreen) {
   GetSessionControllerClient()->LockScreen();
   EXPECT_TRUE(Shell::Get()->session_controller()->IsScreenLocked());
 
-  base::RunLoop run_loop;
-  lock_state_test_api_->set_pine_image_callback(run_loop.QuitClosure());
-  lock_state_controller_->RequestShutdown(
-      ShutdownReason::TRAY_SHUT_DOWN_BUTTON);
-  run_loop.Run();
-
+  RequestShutdown();
   // The pine image should not be taken if it is in the lock screen. The
   // existing pine image should be deleted as well.
   EXPECT_FALSE(base::PathExists(file_path()));
@@ -1165,14 +1153,24 @@ TEST_F(LockStateControllerPineTest, ShutdownInHomeLauncher) {
   ASSERT_TRUE(app_list_controller->IsHomeScreenVisible());
   EXPECT_TRUE(WindowState::Get(window.get())->IsMinimized());
 
-  base::RunLoop run_loop;
-  lock_state_test_api_->set_pine_image_callback(run_loop.QuitClosure());
-  lock_state_controller_->RequestShutdown(
-      ShutdownReason::TRAY_SHUT_DOWN_BUTTON);
-  run_loop.Run();
-
+  RequestShutdown();
   // The pine image should not be taken if it is in the home launcher page when
   // shutting down. The existing image should be deleted as well.
+  EXPECT_FALSE(base::PathExists(file_path()));
+}
+
+TEST_F(LockStateControllerPineTest, AllWindowsMinimized) {
+  // Create an empty file to simulate an old pine image.
+  ASSERT_TRUE(base::WriteFile(file_path(), ""));
+
+  std::unique_ptr<aura::Window> window1(CreateTestWindow());
+  std::unique_ptr<aura::Window> window2(CreateTestWindow());
+  WindowState::Get(window1.get())->Minimize();
+  WindowState::Get(window2.get())->Minimize();
+
+  RequestShutdown();
+  // The pine image should not be taken if all the windows inside the active
+  // desk are minimized. The existing image should be deleted as well.
   EXPECT_FALSE(base::PathExists(file_path()));
 }
 
