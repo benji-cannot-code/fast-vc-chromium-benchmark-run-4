97b8320a70fb0a104f26cc6fba8b5dc3d504b6df
Hans Wennborg <hans@chromium.org>
2024-03-04 13:09:09 +0000

    [PA][MTE] Add missing include
    The M_BIONIC_SET_HEAP_TAGGING_LEVEL etc. macros are defined in malloc.h.
    The code was relying on libc++ including that implicitly, which it no
    longer does.
    
    Bug: 327996421
    Change-Id: I183a1bbd7b00a698b8a9dfe0d25f29164f2ee29b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5335112
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267794}

--- base/allocator/partition_allocator/src/partition_alloc/tagging.cc
+++ base/allocator/partition_allocator/src/partition_alloc/tagging.cc
@@ -46,6 +46,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_ANDROID)
 #include "partition_alloc/partition_alloc_base/files/file_path.h"
 #include "partition_alloc/partition_alloc_base/native_library.h"
+#if BUILDFLAG(HAS_MEMORY_TAGGING)
+#include <malloc.h>
+#endif  // BUILDFLAGS(HAS_MEMORY_TAGGING)
 #endif  // BUILDFLAG(IS_ANDROID)
 
 namespace partition_alloc {
