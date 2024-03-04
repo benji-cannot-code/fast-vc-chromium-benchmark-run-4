b49519c9ec8c4ba643d991c9d06394f22cafcbe5
Elly <ellyjones@chromium.org>
2024-03-04 23:58:54 +0000

    tracing mojom: use ByteString for raw configs
    These are usually encoded protobufs.
    
    Bug: 326376204
    Change-Id: I1a467ca98301267b2b3ebb401319aa728cddc443
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5334241
    Auto-Submit: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268160}

--- services/tracing/public/mojom/data_source_config_mojom_traits.cc
+++ services/tracing/public/mojom/data_source_config_mojom_traits.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "mojo/public/cpp/base/byte_string_mojom_traits.h"
 #include "services/tracing/public/mojom/chrome_config_mojom_traits.h"
 #include "services/tracing/public/mojom/interceptor_config_mojom_traits.h"
 
