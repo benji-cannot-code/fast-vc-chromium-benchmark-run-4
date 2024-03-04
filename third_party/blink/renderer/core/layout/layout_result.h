00da00b668d007fe2578bce38cc84ea2ef379810
Rune Lillesveen <futhark@chromium.org>
2024-03-04 12:49:55 +0000

    [css-anchor-position] Remove unused fallback_index
    Fallback index was stored on the layout result but never used. Remove
    it.
    
    Bug: 40059176
    Change-Id: Iba1c6f2fc248e8f680ff31b933bdf935cac78ff1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5339850
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267789}

--- third_party/blink/renderer/core/layout/layout_result.h
+++ third_party/blink/renderer/core/layout/layout_result.h
@@ -179,9 +179,6 @@ class CORE_EXPORT LayoutResult final : public GarbageCollected<LayoutResult> {
     return bitfields_.can_use_out_of_flow_positioned_first_tier_cache;
   }
 
-  std::optional<wtf_size_t> PositionFallbackIndex() const {
-    return rare_data_ ? rare_data_->PositionFallbackIndex() : std::nullopt;
-  }
   const Vector<NonOverflowingScrollRange>*
   PositionFallbackNonOverflowingRanges() const {
     return rare_data_ ? rare_data_->PositionFallbackNonOverflowingRanges()
@@ -537,10 +534,9 @@ class CORE_EXPORT LayoutResult final : public GarbageCollected<LayoutResult> {
     }
 
     void SetPositionFallbackResult(
-        std::optional<wtf_size_t> fallback_index,
         const Vector<NonOverflowingScrollRange>& non_overflowing_ranges) {
       layout_result_->EnsureRareData()->SetPositionFallbackResult(
-          fallback_index, non_overflowing_ranges);
+          non_overflowing_ranges);
     }
 
    private:
@@ -797,7 +793,6 @@ class CORE_EXPORT LayoutResult final : public GarbageCollected<LayoutResult> {
           block_end_annotation_space(rare_data.block_end_annotation_space),
           lines_until_clamp(rare_data.lines_until_clamp),
           line_box_bfc_block_offset(rare_data.line_box_bfc_block_offset),
-          position_fallback_index(rare_data.position_fallback_index),
           position_fallback_non_overflowing_ranges(
               rare_data.position_fallback_non_overflowing_ranges),
           oof_positioned_offset(rare_data.oof_positioned_offset),
@@ -866,14 +861,9 @@ class CORE_EXPORT LayoutResult final : public GarbageCollected<LayoutResult> {
     }
 
     void SetPositionFallbackResult(
-        std::optional<wtf_size_t> fallback_index,
         const Vector<NonOverflowingScrollRange>& non_overflowing_ranges) {
-      position_fallback_index = fallback_index;
       position_fallback_non_overflowing_ranges = non_overflowing_ranges;
       set_position_fallback_result_is_set(true);
-    }
-    std::optional<wtf_size_t> PositionFallbackIndex() const {
-      return position_fallback_index;
     }
     const Vector<NonOverflowingScrollRange>*
     PositionFallbackNonOverflowingRanges() const {
@@ -920,8 +910,6 @@ class CORE_EXPORT LayoutResult final : public GarbageCollected<LayoutResult> {
    private:
     // Only valid if line_box_bfc_block_offset_is_set
     LayoutUnit line_box_bfc_block_offset;
-
-    std::optional<wtf_size_t> position_fallback_index;
 
     // Only valid if position_fallback_result_is_set
     Vector<NonOverflowingScrollRange> position_fallback_non_overflowing_ranges;
