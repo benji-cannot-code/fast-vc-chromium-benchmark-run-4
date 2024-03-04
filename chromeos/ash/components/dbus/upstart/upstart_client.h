52cb0a47795b18b9e152e30ffcda4799944a3347
BaiiYuan <byronlee@google.com>
2024-03-04 07:51:59 +0000

    upstart: Remove wilco DTC service function
    Since caller is removed in CL:5239052, we can remove the unused
    functions.
    
    BUG: b/319045572
    TEST: CQ
    TEST: autoninja -C out/Default chrome chromeos_unittests unit_tests
    
    Change-Id: Ie89180c34b5b7b80af41b476143ed70ff20801e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5333171
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Byron Lee <byronlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267722}

--- chromeos/ash/components/dbus/upstart/upstart_client.h
+++ chromeos/ash/components/dbus/upstart/upstart_client.h
@@ -99,14 +99,6 @@ class COMPONENT_EXPORT(UPSTART_CLIENT) UpstartClient {
   virtual void StopMediaAnalytics(
       chromeos::VoidDBusMethodCallback callback) = 0;
 
-  // Start wilco DTC services.
-  virtual void StartWilcoDtcService(
-      chromeos::VoidDBusMethodCallback callback) = 0;
-
-  // Stops wilco DTC services.
-  virtual void StopWilcoDtcService(
-      chromeos::VoidDBusMethodCallback callback) = 0;
-
  protected:
   // Initialize() should be used instead.
   UpstartClient();
