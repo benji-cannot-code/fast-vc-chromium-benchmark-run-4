7dade1fe57c62c565ef3c85757950fbfe1f46690
Rainhard Findling <rainhard@chromium.org>
2024-03-05 08:20:43 +0000

    Reland #2 "Privacy Guide: remove search suggestions and preload cards"
    This is attempt #2 to reland this cleanup. Earlier attempts:
    commit 1faae6bd62538e3f685c7143d003a05dd5fbfffa and
    commit 01170d74d06655cd7244026dc0689542702c9bc2.
    This is created as a revert-of-a-revert as Gerrit currently doesn't
    allow to create a reland with merge conflicts (crbug.com/41395970).
    
    The position of the cookie step that is shown in the PG flow in the
    3PCD-disabled codepath has been updated in the integration test (see
    delta of patchset 1>2) which should (hopefully) make it pass also on
    waterfall bots.
    
    The reason why this wasn't caught in the last 2 CLs seems to stem
    from how 3PCD is tested in privacy_guide_integration_test.ts:
    privacy_guide_integration_test.ts does not force the 3PCD state to
    test both possible codepaths, but instead relies on the
    fieldtrial_testing_configs to run both possible 3PCD code paths [1].
    Contrary to our earlier understanding it seems that on local builds
    and on CQ bots, the fieldtrial_testing_configs does not actually run
    both possible 3PCD code paths, but only the 3PCD-enabled code path.
    As the cookie step is shown in PG only in the 3PCD-disabled code path,
    as a result of that code path seemingly not being tested in local builds
    and in CQ bots, the incorrect position of the cookie step in the
    integration test was not caught in the last 2 CLs.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/5014485/comment/6a847c98_64081fc3/
    
    Original change's description:
    > Privacy Guide: clean up search suggestions and preload cards
    >
    > The product decision has been made that the "improve search
    > suggestions" and "preload" cards in privacy guide won't launch. This
    > CL removes those cards from privacy guide.
    >
    > Bug: 40184479
    > Change-Id: I747c2c693ca624d769290c2351d02d4c0913bdab
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5279392
    > Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    > Commit-Queue: John Lee <johntlee@chromium.org>
    > Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    > Reviewed-by: John Lee <johntlee@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1264176}
    
    Bug: 40184479
    Change-Id: Ib01bdaf980c560be3f1fd2fc0cd37dc3fbbc3f9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5331791
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268303}

--- chrome/test/data/webui/settings/privacy_guide_integration_test.ts
+++ chrome/test/data/webui/settings/privacy_guide_integration_test.ts
@@ -134,10 +134,10 @@ suite('PrivacyGuideEligibleReachedMetrics', function() {
   test('recordStepsAreEligibleReached', async function() {
     const optionalSteps: PrivacyGuideStep[] = [];
     optionalSteps.push(PrivacyGuideStep.HISTORY_SYNC);
+    optionalSteps.push(PrivacyGuideStep.SAFE_BROWSING);
     if (!loadTimeData.getBoolean('is3pcdCookieSettingsRedesignEnabled')) {
       optionalSteps.push(PrivacyGuideStep.COOKIES);
     }
-    optionalSteps.push(PrivacyGuideStep.SAFE_BROWSING);
 
     const masks: number[] = [];
     for (let i = 0; i < optionalSteps.length; i++) {
@@ -165,8 +165,6 @@ suite('PrivacyGuideEligibleReachedMetrics', function() {
         }
       });
 
-      expectedArguments.add(
-          PrivacyGuideStepsEligibleAndReached.SEARCH_SUGGESTIONS_ELIGIBLE);
       expectedArguments.add(
           PrivacyGuideStepsEligibleAndReached.COMPLETION_ELIGIBLE);
 
@@ -203,20 +201,6 @@ suite('PrivacyGuideEligibleReachedMetrics', function() {
         assertTrue(!!nextButtonElementOnStep);
         nextButtonElementOnStep.click();
       }
-
-      expectedArguments.add(
-          PrivacyGuideStepsEligibleAndReached.SEARCH_SUGGESTIONS_REACHED);
-
-      assertTrue(
-          isSetEqual(
-              expectedArguments,
-              await getPromiseArguments(testMetricsBrowserProxy)),
-          'Sets differ for the step: SEARCH_SUGGESTIONS_REACHED');
-
-      const nextButtonElementOnSearchSuggestionsStep =
-          page.shadowRoot!.querySelector<HTMLElement>('#nextButton');
-      assertTrue(!!nextButtonElementOnSearchSuggestionsStep);
-      nextButtonElementOnSearchSuggestionsStep.click();
 
       expectedArguments.add(
           PrivacyGuideStepsEligibleAndReached.COMPLETION_REACHED);
