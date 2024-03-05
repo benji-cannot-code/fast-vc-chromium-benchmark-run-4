216bc03666738e63722d466471f40e60a1ee0bbf
Daniil Sakhapov <sakhapov@chromium.org>
2024-03-05 16:39:41 +0000

    Update call to new Value() for SVGPointListInterpolationType
    we are guaranteed to only have doubles there.
    
    Note: Per SVG spec https://drafts.csswg.org/css-transforms/#svg-syntax
    Bug: 41494232
    Change-Id: Ia410f576a52fd7e3966a61f45d8bae91c3ae641f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5339882
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268500}

--- third_party/blink/renderer/core/animation/svg_point_list_interpolation_type.cc
+++ third_party/blink/renderer/core/animation/svg_point_list_interpolation_type.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/animation/interpolation_environment.h"
 #include "third_party/blink/renderer/core/animation/string_keyframe.h"
 #include "third_party/blink/renderer/core/animation/underlying_length_checker.h"
+#include "third_party/blink/renderer/core/css/css_to_length_conversion_data.h"
 #include "third_party/blink/renderer/core/svg/svg_point_list.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 
@@ -87,9 +88,14 @@ SVGPropertyBase* SVGPointListInterpolationType::AppliedSVGValue(
 
   const auto& list = To<InterpolableList>(interpolable_value);
   DCHECK_EQ(list.length() % 2, 0U);
+  // Note: using default CSSToLengthConversionData here as it's
+  // guaranteed to be a double.
+  // TODO(crbug.com/325821290): Avoid InterpolableNumber here.
+  CSSToLengthConversionData length_resolver;
   for (wtf_size_t i = 0; i < list.length(); i += 2) {
-    gfx::PointF point(To<InterpolableNumber>(list.Get(i))->Value(),
-                      To<InterpolableNumber>(list.Get(i + 1))->Value());
+    gfx::PointF point(
+        To<InterpolableNumber>(list.Get(i))->Value(length_resolver),
+        To<InterpolableNumber>(list.Get(i + 1))->Value(length_resolver));
     result->Append(MakeGarbageCollected<SVGPoint>(point));
   }
 
