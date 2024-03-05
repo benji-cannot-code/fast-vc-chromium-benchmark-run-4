0ee846680e582732a315f5cc1ef08cc2148b2945
John L Chen <zuan@chromium.org>
2024-03-05 15:08:12 +0000

    Cryptohome errors: Log ErrorWrapper instead of CryptohomeErrorCode
    This migrate some logging locations that uses CryptohomeErrorCode to use
    ErrorWrapper instead.
    
    BUG: b/320478829
    Change-Id: I4e0d5468bd00e48619758f8136e8a2d848a24a82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5201336
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: John L Chen <zuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268449}

--- chrome/browser/ash/login/users/chrome_user_manager_impl.cc
+++ chrome/browser/ash/login/users/chrome_user_manager_impl.cc
@@ -150,7 +150,7 @@ void OnRemoveUserComplete(const AccountId& account_id,
                           std::optional<AuthenticationError> error) {
   if (error.has_value()) {
     LOG(ERROR) << "Removal of cryptohome for " << account_id.Serialize()
-               << " failed, return code: " << error->get_cryptohome_code();
+               << " failed, return code: " << error->get_cryptohome_error();
   }
 }
 
