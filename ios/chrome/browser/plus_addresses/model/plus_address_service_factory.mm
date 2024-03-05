29eb14800b5b6188dd4caa53a483cec5c9ec8988
Florian Leimgruber <fleimgruber@google.com>
2024-03-05 16:11:54 +0000

    Use PlusAddressWebDataService in PlusAddressService
    Currently, whenever plus addresses are polled from the backend, they are
    simply persisted in PlusAddressService's members in memory. This is
    implemented in PlusAddressService::UpdatePlusAddressMap().
    This CL adds a round trip to the database to persist them there. For
    now, this is completely unnecessary, since plus addresses are regularly
    pulled from the backend anyways, including on start-up. However, once
    the sync integration is done, PlusAddressService will solely rely on the
    data from the database. In that sense, this CL adds the necessary
    infrastructure and santiy checks that the database does work.
    
    More specifically, this CL:
    - Injects PlusAddressWebDataService into PlusAddressService via the
      keyed service factory.
    - Makes PlusAddressService a WebDataServiceConsumer, so it can receive
      results from the database.
    - Updates PlusAddressService::UpdatePlusAddressMap() to clear all
      existing data from the database, insert all the new data received and
      issue a read request for the data that was just inserted.
    - Implements PlusAddressService::OnWebDataServiceRequestDone(), which is
      called when the database read finishes. Updating PlusAddressService's
      state is moved there.
    - Issues a database read right after construction of PlusAddressService.
    - Clear all data in the database on sign-out.
    
    Bug: b/322147254
    Change-Id: I2c892b9b823a1faa65577886381c7f843c1dd567
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5314384
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268477}

--- ios/chrome/browser/plus_addresses/model/plus_address_service_factory.mm
+++ ios/chrome/browser/plus_addresses/model/plus_address_service_factory.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <memory>
 
 #import "base/no_destructor.h"
+#import "components/keyed_service/core/service_access_type.h"
 #import "components/keyed_service/ios/browser_state_dependency_manager.h"
 #import "components/plus_addresses/features.h"
 #import "components/plus_addresses/plus_address_http_client_impl.h"
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/browser_state/browser_state_otr_helper.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/signin/model/identity_manager_factory.h"
+#import "ios/chrome/browser/webdata_services/model/web_data_service_factory.h"
 #import "services/network/public/cpp/shared_url_loader_factory.h"
 
 // static
@@ -36,6 +38,7 @@ PlusAddressServiceFactory::PlusAddressServiceFactory()
           "PlusAddressService",
           BrowserStateDependencyManager::GetInstance()) {
   DependsOn(IdentityManagerFactory::GetInstance());
+  DependsOn(ios::WebDataServiceFactory::GetInstance());
 }
 
 PlusAddressServiceFactory::~PlusAddressServiceFactory() {}
@@ -55,7 +58,9 @@ PlusAddressServiceFactory::BuildServiceInstanceFor(
   return std::make_unique<plus_addresses::PlusAddressService>(
       identity_manager, browser_state->GetPrefs(),
       std::make_unique<plus_addresses::PlusAddressHttpClientImpl>(
-          identity_manager, browser_state->GetSharedURLLoaderFactory()));
+          identity_manager, browser_state->GetSharedURLLoaderFactory()),
+      ios::WebDataServiceFactory::GetPlusAddressWebDataForBrowserState(
+          browser_state, ServiceAccessType::EXPLICIT_ACCESS));
 }
 
 bool PlusAddressServiceFactory::ServiceIsCreatedWithBrowserState() const {
