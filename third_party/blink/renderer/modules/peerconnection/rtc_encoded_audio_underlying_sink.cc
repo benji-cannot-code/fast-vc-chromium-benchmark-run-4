228b7b9dc051e8b73cb7ab5a7a2684043f7dc8ed
Tony Herre <toprice@chromium.org>
2024-03-05 09:01:20 +0000

    Remove size checks on encoded audio frames
    Now that webrtc doesn't drop large packets, we can remove this check.
    See https://crbug.com/40277804#comment14.
    
    Bug: 40790593,40277804,41490235
    Change-Id: I5ebc519ce262e277fc13f16a1ea75f73a89a9329
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5339958
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Auto-Submit: Tony Herre <toprice@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268321}

--- third_party/blink/renderer/modules/peerconnection/rtc_encoded_audio_underlying_sink.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_encoded_audio_underlying_sink.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/peerconnection/rtc_encoded_audio_underlying_sink.h"
 
-#include "base/feature_list.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_rtc_encoded_audio_frame.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/typed_arrays/dom_array_buffer.h"
@@ -15,11 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/webrtc/api/frame_transformer_interface.h"
 
 namespace blink {
-
-// Killswitch base feature
-BASE_FEATURE(kRTCEncodedAudioFrameLimitSize,
-             "RTCEncodedAudioFrameLimitSize",
-             base::FEATURE_ENABLED_BY_DEFAULT);
 
 RTCEncodedAudioUnderlyingSink::RTCEncodedAudioUnderlyingSink(
     ScriptState* script_state,
@@ -53,13 +47,6 @@ ScriptPromise RTCEncodedAudioUnderlyingSink::write(
   if (!transformer_broker_) {
     exception_state.ThrowDOMException(DOMExceptionCode::kInvalidStateError,
                                       "Stream closed");
-    return ScriptPromise();
-  }
-
-  if (base::FeatureList::IsEnabled(kRTCEncodedAudioFrameLimitSize) &&
-      encoded_frame->IsDataTooLarge()) {
-    exception_state.ThrowDOMException(DOMExceptionCode::kOperationError,
-                                      "Frame too large");
     return ScriptPromise();
   }
 
