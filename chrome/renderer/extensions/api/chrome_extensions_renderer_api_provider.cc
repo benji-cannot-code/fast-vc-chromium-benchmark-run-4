eac4d1ac3f14685494cd56249b6e0e72735869d3
Devlin Cronin <rdevlin.cronin@chromium.org>
2024-03-05 20:14:17 +0000

    [Extensions] Change when webview elements are exposed
    There are a number of different tags that are exposed for webview
    elements, including chromeWebViewElement (<webview>) and
    controlledFrame (<controlledframe>). Because controlledFrame is built
    atop the Chrome Web View implementation, contexts that may construct
    controlledframes also necessarily have access to the
    chromeWebViewInternal API.
    
    This created an issue when exposing the elements themselves, since
    contexts that are able to create controlledframes should *not* be
    able to create webviews. The way this is handled today is by allowing
    an ExtensionRendererAPIProvider to provide a webview module and, if
    any provider provides one, not calling into the DispatcherDelegate to
    provide one (which is where the chrome webview module is loaded).
    
    This works, but is a bit clunky and subtle, and also introduces a few
    limitations:
    - It means there must be a "default" implementation, which makes it
      difficult to move out logic from the
      ChromeExtensionsDispatcherDelegate into the
      ChromeExtensionsRendererAPIProvider.
    - It means there is an implicit requirement that only one webview-
      style element is exposed. Today, that's true, but it doesn't need
      to be (in theory, we could have a world where extensions or webui
      could also create controlledframes).
    
    This CL refactors this by introducing a new feature into
    _api_features.json, "chromeWebViewTag". This feature explicitly
    controls the availability of the webview tag exposure, and is only
    available to extensions/apps and webui (not to controlledframe
    embedders). controlledframe embedders still have access to the
    underlying chromeWebViewInternal API, as does everything for which
    chromeWebViewTag is available.
    
    This allows the registration of chromeWebViewElement and
    controlledFrame to be fully independent of one another.
    
    Bug: 326903574
    Change-Id: If893a5e1a6084fb13b99c0804bc464d8793a4729
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5334058
    Reviewed-by: Ovidio Ruiz-Henríquez <odejesush@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268643}

--- chrome/renderer/extensions/api/chrome_extensions_renderer_api_provider.cc
+++ chrome/renderer/extensions/api/chrome_extensions_renderer_api_provider.cc
@@ -218,9 +218,7 @@ void ChromeExtensionsRendererAPIProvider::PopulateSourceMap(
 
 void ChromeExtensionsRendererAPIProvider::EnableCustomElementAllowlist() {}
 
-bool ChromeExtensionsRendererAPIProvider::RequireWebViewModules(
-    ScriptContext* context) {
-  return false;
-}
+void ChromeExtensionsRendererAPIProvider::RequireWebViewModules(
+    ScriptContext* context) {}
 
 }  // namespace extensions
