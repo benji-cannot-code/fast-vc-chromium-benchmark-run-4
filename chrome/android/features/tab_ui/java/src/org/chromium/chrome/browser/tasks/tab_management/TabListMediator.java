bd92d40564a3971555f218ec2c80f8cbe99274a2
Calder Kitagawa <ckitagawa@chromium.org>
2024-03-05 14:07:57 +0000

    [Tab Group] Fix favicon appearing on GTS for groups with 1 tab
    Don't set a default favicon for a tab group since it shouldn't have a
    favicon in the first place.
    
    Bug: 328113684
    Change-Id: I9e279b52d89121fb673df90020153527eb822e2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5340973
    Reviewed-by: Brandon Fong <bjfong@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268422}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediator.java
@@ -473,7 +473,11 @@ class TabListMediator {
                             || tab.getUrl().equals(navigationHandle.getUrl())) {
                         return;
                     }
-                    if (mModel.indexFromId(tab.getId()) == TabModel.INVALID_TAB_INDEX) return;
+                    if (mModel.indexFromId(tab.getId()) == TabModel.INVALID_TAB_INDEX
+                            || (mActionsOnAllRelatedTabs
+                                    && mCurrentTabModelFilterSupplier.get().isTabInTabGroup(tab))) {
+                        return;
+                    }
 
                     mModel.get(mModel.indexFromId(tab.getId()))
                             .model
@@ -1749,9 +1753,11 @@ class TabListMediator {
                                 ClosableTabGridView.QuickDeleteAnimationStatus.TAB_RESTORE)
                         .build();
 
-        tabInfo.set(
-                TabProperties.FAVICON_FETCHER,
-                mTabListFaviconProvider.getDefaultFaviconFetcher(pseudoTab.isIncognito()));
+        if (!mActionsOnAllRelatedTabs || !isPseudoTabInTabGroup(pseudoTab)) {
+            tabInfo.set(
+                    TabProperties.FAVICON_FETCHER,
+                    mTabListFaviconProvider.getDefaultFaviconFetcher(pseudoTab.isIncognito()));
+        }
 
         if (mUiType == UiType.SELECTABLE) {
             // Incognito in both light/dark theme is the same as non-incognito mode in dark theme.
