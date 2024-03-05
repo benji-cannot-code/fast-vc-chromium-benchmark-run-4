47c6e64e1627e0a3f1ee1ca2c88c7ecc09b2c3e7
Christoph Schwering <schwering@google.com>
2024-03-05 11:31:51 +0000

    [Autofill] Put FormFieldData::is_visible behind feature
    Determining FormFieldData::is_visible is expensive. This CL makes it
    an alias of FormFieldData::is_focusable, unless a Finch feature is
    enabled.
    
    FormFieldData::is_visible was intended as a more accurate alternative
    to FormFieldData::is_focusable. It is currently only used for
    RationalizePhoneNumberFields() and debugging code and one metric,
    so the impact of this CL should be limited.
    
    There will be a Finch experiment that combines delaying form
    extraction on DOMContentLoaded together with the feature for
    FormFieldData::is_visible.
    
    Another followup will change the condition for extracting <iframe>
    elements: currently also *visible* ad frames are extracted; we'll
    experiment with only extracting non-ad frames.
    
    Bug: 324199622
    Change-Id: I340456c6dc7ca16b5aa1bcfb2976cdf6e63f3219
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5331803
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1268369}

--- components/autofill/content/renderer/form_autofill_util.cc
+++ components/autofill/content/renderer/form_autofill_util.cc
@@ -1813,8 +1813,10 @@ bool IsWebElementVisible(const WebElement& element) {
     return size.width() >= kMinPixelSize && size.height() >= kMinPixelSize;
   };
   return !element.IsNull() && IsWebElementFocusableForAutofill(element) &&
-         (IsCheckableElement(element) || HasMinSize(element.GetClientSize()) ||
-          HasMinSize(element.GetScrollSize()));
+         (!base::FeatureList::IsEnabled(
+              features::kAutofillDetectFieldVisibility) ||
+          (IsCheckableElement(element) || HasMinSize(element.GetClientSize()) ||
+           HasMinSize(element.GetScrollSize())));
 }
 
 uint64_t GetMaxLength(const WebFormControlElement& element) {
