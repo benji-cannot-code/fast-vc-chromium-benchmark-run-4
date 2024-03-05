b97718ff729d4030c762fcf7702a142166381c0e
Side Yilmaz <sideyilmaz@google.com>
2024-03-05 12:52:50 +0000

    Revert "Replace string with url.mojom.Url for url field"
    This reverts commit bc993a9b426a8309d0e37cc3fbaca90c48616e4e.
    
    Reason for revert: Cause to fail BackForwardCacheBrowserTestWithNotRestoredReasons.Reload
    https://ci.chromium.org/ui/p/chrome/builders/ci/android-arm-tests
    
    Bug:b/328205561
    
    Original change's description:
    > Replace string with url.mojom.Url for url field
    >
    > With this patch, the use of string for url is replaced with
    > url.mojom.Url in back_forward_cache_not_restored_reasons.mojom.
    >
    > Bug: 324031239
    > Change-Id: I2ecb81bd63cee177c582dbef49cb3414d42054dc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5320652
    > Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    > Reviewed-by: Joe Mason <joenotcharles@google.com>
    > Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1268274}
    
    Bug: 324031239
    Change-Id: I979b4f56f548bcac7bfe19870cbd8f4522e17d20
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5344232
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Side Yilmaz <sideyilmaz@google.com>
    Commit-Queue: Side Yilmaz <sideyilmaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1268391}

--- content/browser/back_forward_cache_no_store_browsertest.cc
+++ content/browser/back_forward_cache_no_store_browsertest.cc
@@ -963,7 +963,7 @@ IN_PROC_BROWSER_TEST_P(
       {MatchesDetailedReason("cache-control-no-store",
                              /*source=*/std::nullopt)},
       MatchesSameOriginDetails(
-          /*url=*/url_a_no_store,
+          /*url=*/url_a_no_store.spec(),
           /*children=*/{}));
   EXPECT_THAT(
       current_frame_host()->NotRestoredReasonsForTesting(),
@@ -973,7 +973,7 @@ IN_PROC_BROWSER_TEST_P(
           {MatchesDetailedReason("cache-control-no-store",
                                  /*source=*/std::nullopt)},
           MatchesSameOriginDetails(
-              /*url=*/url_a_no_store,
+              /*url=*/url_a_no_store.spec(),
               /*children=*/
               {subframe_result})));
 }
