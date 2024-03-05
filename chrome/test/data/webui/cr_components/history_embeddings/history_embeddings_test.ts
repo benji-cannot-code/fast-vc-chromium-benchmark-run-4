f6074a14c6dd1bea3fa9128fd2676379e3c1358f
John Lee <johntlee@chromium.org>
2024-03-05 19:07:14 +0000

    Add some skeleton tests for history embeddings components
    Bug: 325108686
    Change-Id: I5f32d3bc838a1b281dde046130e9fb1ec633ca3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5341493
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268603}

---
+++ chrome/test/data/webui/cr_components/history_embeddings/history_embeddings_test.ts
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://resources/cr_components/history_embeddings/history_embeddings.js';
+
+import {HistoryEmbeddingsBrowserProxyImpl} from 'chrome://resources/cr_components/history_embeddings/browser_proxy.js';
+import type {HistoryEmbeddingsElement} from 'chrome://resources/cr_components/history_embeddings/history_embeddings.js';
+import {PageHandlerRemote} from 'chrome://resources/cr_components/history_embeddings/history_embeddings.mojom-webui.js';
+import {assertEquals} from 'chrome://webui-test/chai_assert.js';
+import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
+import {TestMock} from 'chrome://webui-test/test_mock.js';
+
+suite('cr-history-embeddings', () => {
+  let element: HistoryEmbeddingsElement;
+  let handler: TestMock<PageHandlerRemote>&PageHandlerRemote;
+
+  setup(() => {
+    document.body.innerHTML = window.trustedTypes!.emptyHTML;
+
+    handler = TestMock.fromClass(PageHandlerRemote);
+    HistoryEmbeddingsBrowserProxyImpl.setInstance(
+        new HistoryEmbeddingsBrowserProxyImpl(handler));
+    handler.setResultFor('doSomething', Promise.resolve(true));
+
+    element = document.createElement('cr-history-embeddings');
+    document.body.appendChild(element);
+    return flushTasks();
+  });
+
+  test('CallsProxy', async () => {
+    await handler.whenCalled('doSomething');
+    assertEquals(1, handler.getCallCount('doSomething'));
+  });
+});
