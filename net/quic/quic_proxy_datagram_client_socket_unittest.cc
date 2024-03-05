3a1b81b77ee4b16cc0e9e9f2a9a0959740c4ea85
Ciara McMullin <ciaramcmullin@google.com>
2024-03-05 22:44:30 +0000

    Call WriteConnectUdpPayload for packet transmission in datagram client socket
    Bug: 41497362
    Change-Id: I7284157652a3de6fe89e203090396608d15daf1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5340989
    Reviewed-by: David Schinazi <dschinazi@google.com>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Commit-Queue: Ciara McMullin <ciaramcmullin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1268746}

--- net/quic/quic_proxy_datagram_client_socket_unittest.cc
+++ net/quic/quic_proxy_datagram_client_socket_unittest.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/test/test_data_directory.h"
 #include "net/test/test_with_task_environment.h"
 #include "net/third_party/quiche/src/quiche/quic/test_tools/qpack/qpack_test_utils.h"
+#include "net/third_party/quiche/src/quiche/quic/test_tools/quic_spdy_session_peer.h"
 #include "net/third_party/quiche/src/quiche/quic/test_tools/quic_test_utils.h"
 #include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -48,6 +49,13 @@ namespace {
 constexpr char kTestHeaderName[] = "Foo";
 
 }  // anonymous namespace
+
+class EstablishedCryptoStream : public quic::test::MockQuicCryptoStream {
+ public:
+  using quic::test::MockQuicCryptoStream::MockQuicCryptoStream;
+
+  bool encryption_established() const override { return true; }
+};
 
 class QuicProxyDatagramClientSocketTest : public QuicProxyClientSocketTestBase {
  public:
@@ -110,11 +118,19 @@ class QuicProxyDatagramClientSocketTest : public QuicProxyClientSocketTestBase {
   }
 
   void AssertWriteReturns(const char* data, int len, int rv) override {
-    CHECK(false);
+    auto buf = base::MakeRefCounted<IOBufferWithSize>(len);
+    memcpy(buf->data(), data, len);
+    EXPECT_EQ(rv,
+              sock_->Write(buf.get(), buf->size(), write_callback_.callback(),
+                           TRAFFIC_ANNOTATION_FOR_TESTS));
   }
 
   void AssertSyncWriteSucceeds(const char* data, int len) override {
-    CHECK(false);
+    auto buf = base::MakeRefCounted<IOBufferWithSize>(len);
+    memcpy(buf->data(), data, len);
+    EXPECT_EQ(len,
+              sock_->Write(buf.get(), buf->size(), CompletionOnceCallback(),
+                           TRAFFIC_ANNOTATION_FOR_TESTS));
   }
 
   void AssertSyncReadEquals(const char* data, int len) override {
@@ -245,6 +261,65 @@ TEST_P(QuicProxyDatagramClientSocketTest, ConnectFails) {
   AssertConnectFails(ERR_QUIC_PROTOCOL_ERROR);
 
   ASSERT_FALSE(sock_->IsConnected());
+}
+
+TEST_P(QuicProxyDatagramClientSocketTest, WriteSendsData) {
+  int packet_number = 1;
+  mock_quic_data_.AddWrite(SYNCHRONOUS,
+                           ConstructSettingsPacket(packet_number++));
+  mock_quic_data_.AddWrite(SYNCHRONOUS,
+                           ConstructConnectRequestPacket(packet_number++));
+  mock_quic_data_.AddRead(ASYNC, ConstructServerConnectReplyPacket(1, !kFin));
+  mock_quic_data_.AddReadPauseForever();
+
+  std::string quarter_stream_id(1, '\0');
+  std::string context_id(1, '\0');
+
+  mock_quic_data_.AddWrite(SYNCHRONOUS, ConstructAckAndDatagramPacket(
+                                            packet_number++, 1, 1,
+                                            {quarter_stream_id + context_id +
+                                             std::string(kMsg1, kLen1)}));
+  mock_quic_data_.AddWrite(
+      SYNCHRONOUS,
+      ConstructDatagramPacket(packet_number++, {quarter_stream_id + context_id +
+                                                std::string(kMsg2, kLen2)}));
+  mock_quic_data_.AddWrite(
+      SYNCHRONOUS,
+      ConstructRstPacket(packet_number++, quic::QUIC_STREAM_CANCELLED));
+
+  InitializeSession();
+
+  quic::test::QuicSpdySessionPeer::SetHttpDatagramSupport(
+      session_.get(), quic::HttpDatagramSupport::kRfc);
+
+  InitializeClientSocket();
+
+  AssertConnectSucceeds();
+
+  AssertSyncWriteSucceeds(kMsg1, kLen1);
+  AssertSyncWriteSucceeds(kMsg2, kLen2);
+}
+
+TEST_P(QuicProxyDatagramClientSocketTest, WriteOnClosedSocket) {
+  int packet_number = 1;
+  mock_quic_data_.AddWrite(SYNCHRONOUS,
+                           ConstructSettingsPacket(packet_number++));
+  mock_quic_data_.AddWrite(SYNCHRONOUS,
+                           ConstructConnectRequestPacket(packet_number++));
+  mock_quic_data_.AddRead(ASYNC, ConstructServerConnectReplyPacket(1, !kFin));
+  mock_quic_data_.AddReadPauseForever();
+  mock_quic_data_.AddWrite(
+      SYNCHRONOUS, ConstructAckAndRstPacket(packet_number++,
+                                            quic::QUIC_STREAM_CANCELLED, 1, 1));
+
+  InitializeSession();
+  InitializeClientSocket();
+
+  AssertConnectSucceeds();
+
+  sock_->Close();
+
+  AssertWriteReturns(kMsg1, kLen1, ERR_SOCKET_NOT_CONNECTED);
 }
 
 INSTANTIATE_TEST_SUITE_P(VersionIncludeStreamDependencySequence,
--- net/quic/quic_test_packet_maker.cc
+++ net/quic/quic_test_packet_maker.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/quic/quic_chromium_client_session.h"
 #include "net/quic/quic_http_utils.h"
 #include "net/spdy/spdy_http_utils.h"
+#include "net/third_party/quiche/src/quiche/common/quiche_buffer_allocator.h"
+#include "net/third_party/quiche/src/quiche/common/simple_buffer_allocator.h"
 #include "net/third_party/quiche/src/quiche/quic/core/http/http_constants.h"
 #include "net/third_party/quiche/src/quiche/quic/core/qpack/qpack_instruction_encoder.h"
 #include "net/third_party/quiche/src/quiche/quic/core/quic_framer.h"
@@ -70,9 +72,11 @@ quic::QuicFrames CloneFrames(const quic::QuicFrames& frames) {
                 *frame.retire_connection_id_frame);
         break;
       case quic::MESSAGE_FRAME:
-        DCHECK(false) << "Message frame not supported";
-        // frame.message_frame = new
-        // quic::QuicMessageFrame(*frame.message_frame);
+        frame.message_frame = new quic::QuicMessageFrame(
+            frame.message_frame->message_id,
+            quiche::QuicheMemSlice(quiche::QuicheBuffer::Copy(
+                quiche::SimpleBufferAllocator::Get(),
+                frame.message_frame->message_data.data()->AsStringView())));
         break;
       case quic::CRYPTO_FRAME:
         frame.crypto_frame = new quic::QuicCryptoFrame(*frame.crypto_frame);
@@ -689,6 +693,14 @@ std::unique_ptr<quic::QuicReceivedPacket> QuicTestPacketMaker::MakeDataPacket(
 }
 
 std::unique_ptr<quic::QuicReceivedPacket>
+QuicTestPacketMaker::MakeDatagramPacket(uint64_t packet_number,
+                                        std::string_view data) {
+  InitializeHeader(packet_number);
+  AddQuicMessageFrame(data);
+  return BuildPacket();
+}
+
+std::unique_ptr<quic::QuicReceivedPacket>
 QuicTestPacketMaker::MakeAckAndDataPacket(uint64_t packet_number,
                                           quic::QuicStreamId stream_id,
                                           uint64_t largest_received,
@@ -699,7 +711,18 @@ QuicTestPacketMaker::MakeAckAndDataPacket(uint64_t packet_number,
 
   AddQuicAckFrame(largest_received, smallest_received);
   AddQuicStreamFrame(stream_id, fin, data);
+  return BuildPacket();
+}
 
+std::unique_ptr<quic::QuicReceivedPacket>
+QuicTestPacketMaker::MakeAckAndDatagramPacket(uint64_t packet_number,
+                                              uint64_t largest_received,
+                                              uint64_t smallest_received,
+                                              std::string_view data) {
+  InitializeHeader(packet_number);
+
+  AddQuicAckFrame(largest_received, smallest_received);
+  AddQuicMessageFrame(data);
   return BuildPacket();
 }
 
@@ -1116,6 +1139,14 @@ void QuicTestPacketMaker::AddQuicStreamsBlockedFrame(
   quic::QuicStreamsBlockedFrame streams_blocked_frame(
       control_frame_id, stream_count, unidirectional);
   frames_.push_back(quic::QuicFrame(streams_blocked_frame));
+  DVLOG(1) << "Adding frame: " << frames_.back();
+}
+
+void QuicTestPacketMaker::AddQuicMessageFrame(std::string_view data) {
+  auto* message_frame = new quic::QuicMessageFrame(
+      /*message_id=*/0, quiche::QuicheMemSlice(quiche::QuicheBuffer::Copy(
+                            quiche::SimpleBufferAllocator::Get(), data)));
+  frames_.push_back(quic::QuicFrame(message_frame));
   DVLOG(1) << "Adding frame: " << frames_.back();
 }
 
