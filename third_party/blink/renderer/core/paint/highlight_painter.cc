05da2f5e52203a43314326292d495ba411e5a4a6
Fredrik Söderquist <fs@opera.com>
2024-03-05 09:38:13 +0000

    Rename DocumentMarkerPainter to StyleableMarkerPainter
    This is in essence what it is now. Reorder the argument list to have the
    marker instance first. Drop an unused Document argument. Fix the enum
    type used to use the 'blink' variant.
    
    Change-Id: I6eab8ae1ab13d9a95053f2c9b0095575ff6c1363
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5339959
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1268335}

--- third_party/blink/renderer/core/paint/highlight_painter.cc
+++ third_party/blink/renderer/core/paint/highlight_painter.cc
@@ -22,12 +22,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/layout/svg/layout_svg_inline_text.h"
 #include "third_party/blink/renderer/core/layout/text_decoration_offset.h"
-#include "third_party/blink/renderer/core/paint/document_marker_painter.h"
 #include "third_party/blink/renderer/core/paint/highlight_overlay.h"
 #include "third_party/blink/renderer/core/paint/line_relative_rect.h"
 #include "third_party/blink/renderer/core/paint/marker_range_mapping_context.h"
 #include "third_party/blink/renderer/core/paint/paint_auto_dark_mode.h"
 #include "third_party/blink/renderer/core/paint/paint_info.h"
+#include "third_party/blink/renderer/core/paint/styleable_marker_painter.h"
 #include "third_party/blink/renderer/core/paint/text_decoration_painter.h"
 #include "third_party/blink/renderer/core/paint/text_painter.h"
 #include "third_party/blink/renderer/platform/graphics/graphics_context_state_saver.h"
@@ -527,13 +527,12 @@ void HighlightPainter::Paint(Phase phase) {
               styleable_marker.BackgroundColor(), background_auto_dark_mode_);
           break;
         }
-        if (DocumentMarkerPainter::ShouldPaintMarkerUnderline(
-                styleable_marker)) {
+        if (StyleableMarkerPainter::ShouldPaintUnderline(styleable_marker)) {
           const SimpleFontData* font_data =
               originating_style_.GetFont().PrimaryFont();
-          DocumentMarkerPainter::PaintStyleableMarkerUnderline(
-              paint_info_.context, box_origin_, styleable_marker,
-              originating_style_, node_->GetDocument(),
+          StyleableMarkerPainter::PaintUnderline(
+              styleable_marker, paint_info_.context, box_origin_,
+              originating_style_,
               LineRelativeLocalRect(fragment_item_, text, paint_start_offset,
                                     paint_end_offset),
               LayoutUnit(font_data->GetFontMetrics().Height()),
--- third_party/blink/renderer/core/paint/text_fragment_painter.cc
+++ third_party/blink/renderer/core/paint/text_fragment_painter.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/text_decoration_offset.h"
 #include "third_party/blink/renderer/core/mobile_metrics/mobile_friendliness_checker.h"
 #include "third_party/blink/renderer/core/paint/box_model_object_painter.h"
-#include "third_party/blink/renderer/core/paint/document_marker_painter.h"
 #include "third_party/blink/renderer/core/paint/line_relative_rect.h"
 #include "third_party/blink/renderer/core/paint/highlight_painter.h"
 #include "third_party/blink/renderer/core/paint/inline_paint_context.h"
