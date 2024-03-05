cded5bcb96ecfe892dbd0a6463756bab4b8f86c4
Joe Antonetti <joeantonetti@google.com>
2024-03-05 19:52:14 +0000

    [Instant Hotspot] Add "Set Up Your Device" button
    This is one of two changes needed to implement the "Set Up Your Device"
    button in Network Quick Settings
    
    UI Deck: https://docs.google.com/presentation/d/1EDKDiBCu-KLgGGHL0Laz90S2BsEzU0HLuanbDHhpzdI/edit?resourcekey=0-HLWeff66N6-mD6ZIZFnWSg#slide=id.g21d9e5a24ff_0_0
    
    This button is shown in the "Your Devices" section if the user has one eligible device for hotspot which is not currently enabled.
    
    This change adds the button, if the user has a device to set up
    
    Example: https://screenshot.googleplex.com/9xebZ3MhdNWzLap
    
    Bug=b/321940291
    Test: Added Unit Tests
    
    Change-Id: Ic0a0759979dd10e419f8ba7f9f1928f89944c9ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5253708
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Joe Antonetti <joeantonetti@google.com>
    Reviewed-by: Nikhil Nayunigari <nikhilcn@google.com>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268627}

--- chrome/browser/ui/ash/system_tray_client_impl.cc
+++ chrome/browser/ui/ash/system_tray_client_impl.cc
@@ -718,9 +718,6 @@ void SystemTrayClientImpl::ShowNetworkSettingsHelper(
 
 void SystemTrayClientImpl::ShowMultiDeviceSetup() {
   ash::multidevice_setup::MultiDeviceSetupDialog::Show();
-  ash::phonehub::util::LogMultiDeviceSetupDialogEntryPoint(
-      ash::phonehub::util::MultiDeviceSetupDialogEntrypoint::
-          kSetupNotification);
 }
 
 void SystemTrayClientImpl::ShowFirmwareUpdate() {
