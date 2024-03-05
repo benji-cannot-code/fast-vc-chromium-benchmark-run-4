2d37948496b272d3611b52957dc3a0158b8b679c
Viplav Kadam <viplavkadam@google.com>
2024-03-05 20:08:28 +0000

    [Benefits] ClearAllCreditCardBenefits API to clear all the card benefits
    This CL includes a ClearAllCreditCardBenefits function within the
    Personal Data Manager. This function initiates the clearing of credit
    card benefits by calling a corresponding function in the
    AutofillWebDataService. The AutofillWebDataService function then
    forwards the clearing request to the backend implementation, where a
    function (also named ClearAllCreditCardBenefits) handles the process
    by making the necessary call to the ClearAllCreditCardBenefits
    AutofillTable API.
    
    This CL includes the following additions to support
    ClearAllCreditCardBenefits API unit tests:
    autofill_test_util methods:
      SetCreditCardBenefits for database population.
      GetAllCreditCardBenefits for data retrieval.
    personal_data_manager_test_api: Provides functionality to provide
    credit card benefits count within the PDM.
    
    Design: go/pay-autofill-chrome-benefits-design
    
    Bug: 326095647
    
    Change-Id: I9223d7703f8d624542d1a9b323e3dfb0f5de95d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5311615
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Commit-Queue: Viplav Kadam <viplavkadam@google.com>
    Reviewed-by: Alexander Tekle <alexandertekle@google.com>
    Cr-Commit-Position: refs/heads/main@{#1268638}

--- components/autofill/core/browser/payments_data_manager.h
+++ components/autofill/core/browser/payments_data_manager.h
@@ -205,6 +205,9 @@ class PaymentsDataManager : public AutofillWebDataServiceObserverOnUISequence,
   void RemoveServerCvc(int64_t instrument_id);
   virtual void ClearServerCvcs();
 
+  // Clears all the credit card benefits from the webdata database.
+  void ClearAllCreditCardBenefits();
+
   // Method to clear all local CVCs from the local web database.
   virtual void ClearLocalCvcs();
 
@@ -246,6 +249,7 @@ class PaymentsDataManager : public AutofillWebDataServiceObserverOnUISequence,
  protected:
   // TODO(b/322170538): Remove dependency.
   friend class PersonalDataManager;
+  friend class PersonalDataManagerTestApi;
   friend class TestPersonalDataManager;
 
   FRIEND_TEST_ALL_PREFIXES(PersonalDataManagerTest,
