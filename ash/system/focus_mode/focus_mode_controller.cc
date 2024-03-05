947a396a851a19e8ecc308aafd7818142428ac38
Richard Chui <richui@chromium.org>
2024-03-05 23:13:22 +0000

    focus-mode: show nudge after tray bounce-in animation
    Instead of showing the nudge immediately, this CL will move the nudge
    showing logic to after the ending moment is triggered and the tray has
    bounced in. Will not show the tray if the bubble is already showing.
    
    Bug: b/325346740
    Test: manual, covered by existing tests
    Change-Id: If0720b3d3f092bb865a8df33c9a8fcc09f582874
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5325560
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Richard Chui <richui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268759}

--- ash/system/focus_mode/focus_mode_controller.cc
+++ ash/system/focus_mode/focus_mode_controller.cc
@@ -31,9 +31,6 @@ namespace ash {
 
 namespace {
 
-constexpr char kFocusModeEndingMomentNudgeId[] =
-    "focus_mode_ending_moment_nudge";
-
 FocusModeController* g_instance = nullptr;
 
 // The default Focus Mode session duration.
@@ -86,7 +83,7 @@ void ShowEndingMomentNudge() {
   // NOTE: we anchor to `tray->image_view()` in order to center the nudge
   // properly because there is extra spacing on the actual `FocusModeTray` view.
   AnchoredNudgeData nudge_data(
-      kFocusModeEndingMomentNudgeId,
+      focus_mode_util::kFocusModeEndingMomentNudgeId,
       NudgeCatalogName::kFocusModeEndingMomentNudge,
       l10n_util::GetStringUTF16(
           IDS_ASH_STATUS_TRAY_FOCUS_MODE_ENDING_MOMENT_NUDGE),
@@ -101,7 +98,7 @@ void ShowEndingMomentNudge() {
 
 void HideEndingMomentNudge() {
   if (AnchoredNudgeManager* nudge_manager = AnchoredNudgeManager::Get()) {
-    nudge_manager->Cancel(kFocusModeEndingMomentNudgeId);
+    nudge_manager->Cancel(focus_mode_util::kFocusModeEndingMomentNudgeId);
   }
 }
 
@@ -342,6 +339,22 @@ void FocusModeController::CompleteTask() {
   SetSelectedTask(nullptr);
 }
 
+void FocusModeController::MaybeShowEndingMomentNudge() {
+  // Do not show the nudge if there is a persistent tray bubble open during the
+  // ending moment.
+  if (!in_ending_moment() || current_session_->persistent_ending()) {
+    return;
+  }
+
+  if (auto* anchored_nudge_manager = AnchoredNudgeManager::Get();
+      anchored_nudge_manager->IsNudgeShown(
+          focus_mode_util::kFocusModeEndingMomentNudgeId)) {
+    return;
+  }
+
+  ShowEndingMomentNudge();
+}
+
 void FocusModeController::TriggerEndingMomentImmediately() {
   if (!in_focus_session()) {
     return;
@@ -416,8 +429,6 @@ void FocusModeController::OnTimerTick() {
                      &FocusModeController::ResetFocusSession,
                      base::TimeTicks::Now());
         MaybeUpdateDndNotification();
-        // TODO(b/325346740): Show nudge after tray bounce-in animation.
-        ShowEndingMomentNudge();
       } else {
         current_session_->set_persistent_ending();
       }
