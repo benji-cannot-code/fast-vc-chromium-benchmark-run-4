61d23b7ed0d916edf75608317b964c60b241a858
Juan Mojica <juanmojica@google.com>
2024-03-05 20:09:01 +0000

    Create an overlay view for lens overlay.
    Depends on https://crrev.com/c/5337204
    
    Bug: b:327270456
    Change-Id: Ib6be79632587619bceb0e27caa49463028bc6b61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5337083
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1268639}

--- chrome/browser/ui/lens/lens_overlay_controller.h
+++ chrome/browser/ui/lens/lens_overlay_controller.h
@@ -19,6 +19,11 @@ namespace tabs {
 class TabModel;
 }  // namespace tabs
 
+namespace views {
+class View;
+class WebView;
+}  // namespace views
+
 // Manages all state associated with the lens overlay.
 // This class is not thread safe. It should only be used from the browser
 // thread.
@@ -48,6 +53,9 @@ class LensOverlayController : public TabStripModelObserver {
   };
   State state() { return state_; }
 
+  // Testing helper method for checking web view state.
+  raw_ptr<views::WebView> GetOverlayWebViewForTesting();
+
  private:
   // Called once a screenshot has been captured. This should trigger transition
   // to kOverlay. As this process is asynchronous, there are edge cases that can
@@ -68,6 +76,9 @@ class LensOverlayController : public TabStripModelObserver {
   // Called when the associated tab enters the background.
   void TabBackgrounded();
 
+  // Called when the UI needs to create the view to show in the overlay.
+  std::unique_ptr<views::View> CreateViewForOverlay();
+
   // Owns this class.
   raw_ptr<tabs::TabModel> tab_model_;
 
@@ -77,6 +88,12 @@ class LensOverlayController : public TabStripModelObserver {
 
   // Tracks the internal state machine.
   State state_ = State::kOff;
+
+  // Pointer to the overlay host view.
+  std::unique_ptr<views::View> overlay_host_view_ = nullptr;
+
+  // A raw ptr containing the web view within the overlay.
+  raw_ptr<views::WebView> overlay_web_view_ = nullptr;
 
   // Must be the last member.
   base::WeakPtrFactory<LensOverlayController> weak_factory_{this};
