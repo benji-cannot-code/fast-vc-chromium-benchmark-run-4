4e483a7f1a393809b2f5ff9b3f3bb42a991eda07
Xi Han <hanxi@google.com>
2024-03-05 15:24:54 +0000

    [StartRefactor] Remove TabSwitcherAndStartSurfaceLayout.
    In this CL:
    1. Remove TabSwitcherAndStartSurfaceLayout.java;
    2. Rename TabSwitcherAndStartSurfaceLayout*Test to TabSwitcher*Test.
    3. Clean up flag isStartSurfaceRefactorEnabled related check. More
       clean up of StartSurfaceCoordinator|Mediator will be done in a
       follow up CL.
    
    Bug: 40221888
    Change-Id: If6b5aa3025bcdf3513ed620461915039fa593be6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5340575
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268459}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListRecyclerView.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListRecyclerView.java
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tasks.tab_management;
 
-import static org.chromium.chrome.features.start_surface.TabSwitcherAndStartSurfaceLayout.ZOOMING_DURATION;
+import static org.chromium.chrome.browser.tasks.tab_management.TabSwitcherLayout.ZOOMING_DURATION;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
