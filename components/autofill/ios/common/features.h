8b7d2d51a0eabccc598a2ba2636c9478abbedab7
Vidhan Jain <vidhanj@google.com>
2024-03-05 16:45:03 +0000

    [iOS] Add feature flag AutofillDynamicallyLoadsFieldsForAddressInput
    Bug: 1482269
    Change-Id: I6c4abb95ced4ece6da23a8065a519f5293087445
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5344971
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1268502}

--- components/autofill/ios/common/features.h
+++ components/autofill/ios/common/features.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/feature_list.h"
 
+BASE_DECLARE_FEATURE(kAutofillDynamicallyLoadsFieldsForAddressInput);
 BASE_DECLARE_FEATURE(kAutofillIsolatedWorldForJavascriptIos);
 
 #endif  // COMPONENTS_AUTOFILL_IOS_COMMON_FEATURES_H_
--- components/autofill/ios/common/features.mm
+++ components/autofill/ios/common/features.mm
@@ -7,6 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Features that are exlusive to iOS go here in alphabetical order.
 
+// Controls whether to dynamically load the address input fields in the save
+// flow and settings based on the country value.
+// TODO(crbug.com/1482269): Remove once launched.
+BASE_FEATURE(kAutofillDynamicallyLoadsFieldsForAddressInput,
+             "AutofillDynamicallyLoadsFieldsForAddressInput",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Controls whether to use the isolated content world instead of the page
 // content world for the Autofill JS feature scripts.
 // TODO(crbug.com/1175793) Remove once the isolated content world is launched
