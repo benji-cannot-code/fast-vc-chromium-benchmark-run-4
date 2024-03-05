3ab468c9cc4fe4e401b77ce887b01be41c896ee4
rgod <rgod@google.com>
2024-03-05 14:29:58 +0000

    [WebAuthn] Remove inaccurate gpm pin comment
    Bug: 40274370
    Change-Id: Ifdfea19f599d5151942e22a7aacb8b462834d37a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5344929
    Auto-Submit: Rafał Godlewski <rgod@google.com>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268433}

--- chrome/browser/ui/views/webauthn/authenticator_gpm_pin_sheet_view.h
+++ chrome/browser/ui/views/webauthn/authenticator_gpm_pin_sheet_view.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webauthn/sheet_models.h"
 
 // Represents a sheet in the Web Authentication request dialog that allows the
-// user to enter GPM pin code used as a recovery factor in passkeys flow.
+// user to enter GPM pin code used in passkeys flow.
 class AuthenticatorGpmPinSheetView : public AuthenticatorRequestSheetView,
                                      public AuthenticatorGPMPinView::Delegate {
  public:
