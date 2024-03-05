614b9efd182d714fbd72095eda9ddda7972c3559
Kristi Saney <kristislee@google.com>
2024-03-05 18:52:54 +0000

    Move Reading List test out of the Read Anything test suite
    These are different products
    
    Bug: none
    Change-Id: Ie562d3d895ca3cd41376738d9c4d0e67daf378f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5341939
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268595}

--- chrome/test/data/webui/side_panel/read_anything/read_anything_browsertest.cc
+++ chrome/test/data/webui/side_panel/read_anything/read_anything_browsertest.cc
@@ -14,12 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test_utils.h"
 #include "ui/accessibility/accessibility_features.h"
 
-using SidePanelReadingListTest = WebUIMochaBrowserTest;
-IN_PROC_BROWSER_TEST_F(SidePanelReadingListTest, App) {
-  set_test_loader_host(chrome::kChromeUIReadLaterHost);
-  RunTest("side_panel/reading_list/reading_list_app_test.js", "mocha.run()");
-}
-
 class ReadAnythingMochaBrowserTest : public WebUIMochaBrowserTest {
  protected:
   ReadAnythingMochaBrowserTest() {
---
+++ chrome/test/data/webui/side_panel/reading_list/reading_list_browsertest.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/common/webui_url_constants.h"
+#include "chrome/test/base/web_ui_mocha_browser_test.h"
+#include "content/public/test/browser_test.h"
+
+using SidePanelReadingListTest = WebUIMochaBrowserTest;
+IN_PROC_BROWSER_TEST_F(SidePanelReadingListTest, App) {
+  set_test_loader_host(chrome::kChromeUIReadLaterHost);
+  RunTest("side_panel/reading_list/reading_list_app_test.js", "mocha.run()");
+}
