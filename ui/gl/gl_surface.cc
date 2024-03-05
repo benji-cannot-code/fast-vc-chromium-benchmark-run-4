03d77c7a128242f77939686ebc105168373abaf4
Vasiliy Telezhnikov <vasilyt@chromium.org>
2024-03-05 21:42:41 +0000

    Remove SwapBuffersWithBounds
    Code is dead for a few years.
    
    Change-Id: I47d2fcc900ca0303c1b93d91d6ff93fe2ad53763
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5315118
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268709}

--- ui/gl/gl_surface.cc
+++ ui/gl/gl_surface.cc
@@ -66,10 +66,6 @@ bool GLSurface::DeferDraws() {
   return false;
 }
 
-bool GLSurface::SupportsSwapBuffersWithBounds() {
-  return false;
-}
-
 bool GLSurface::SupportsPostSubBuffer() {
   return false;
 }
@@ -86,13 +82,6 @@ void GLSurface::SwapBuffersAsync(SwapCompletionCallback completion_callback,
                                  PresentationCallback presentation_callback,
                                  gfx::FrameData data) {
   NOTREACHED();
-}
-
-gfx::SwapResult GLSurface::SwapBuffersWithBounds(
-    const std::vector<gfx::Rect>& rects,
-    PresentationCallback callback,
-    gfx::FrameData data) {
-  return gfx::SwapResult::SWAP_FAILED;
 }
 
 gfx::SwapResult GLSurface::PostSubBuffer(int x,
--- ui/ozone/platform/cast/gl_surface_cast.h
+++ ui/ozone/platform/cast/gl_surface_cast.h
@@ -6,9 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_OZONE_PLATFORM_CAST_GL_SURFACE_CAST_H_
 #define UI_OZONE_PLATFORM_CAST_GL_SURFACE_CAST_H_
 
-#include <memory>
-#include <vector>
-
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/native_widget_types.h"
 #include "ui/gfx/overlay_transform.h"
@@ -28,11 +25,6 @@ class GLSurfaceCast : public gl::NativeViewGLSurfaceEGL {
   GLSurfaceCast(const GLSurfaceCast&) = delete;
   GLSurfaceCast& operator=(const GLSurfaceCast&) = delete;
 
-  // gl::GLSurface:
-  bool SupportsSwapBuffersWithBounds() override;
-  gfx::SwapResult SwapBuffersWithBounds(const std::vector<gfx::Rect>& rects,
-                                        PresentationCallback callback,
-                                        gfx::FrameData data) override;
   bool Resize(const gfx::Size& size,
               float scale_factor,
               const gfx::ColorSpace& color_space,
@@ -45,7 +37,6 @@ class GLSurfaceCast : public gl::NativeViewGLSurfaceEGL {
 
   gfx::AcceleratedWidget widget_;
   GLOzoneEglCast* parent_;
-  bool supports_swap_buffer_with_bounds_;
   bool uses_triple_buffering_;
 };
 
