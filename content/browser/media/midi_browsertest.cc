0e559e1bd4f3a41a1f264dfb54acc6f9272f5f52
Michael Wilson <mjwilson@chromium.org>
2024-03-05 23:04:04 +0000

    Update comments about Fuchsia MidiManager from TODO to informational
    There is no plan to implement this on Fuchsia.
    
    Also remove redundant #define statements and group tests by type.
    
    Bug: 40217214
    Change-Id: If6550ee95f16a01df3982e21291a937a6efe162d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5345146
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Sina Firoozabadi <sinafirooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268753}

--- content/browser/media/midi_browsertest.cc
+++ content/browser/media/midi_browsertest.cc
@@ -118,7 +118,7 @@ class MidiBrowserTestBlockMidiByDefault : public ContentBrowserTest {
   base::test::ScopedFeatureList feature_list_;
 };
 
-// TODO(https://crbug.com/1302995): MidiManager has no Fuchsia implementation.
+// MidiManager has no Fuchsia implementation (see https://crbug.com/1302995).
 #if BUILDFLAG(IS_FUCHSIA)
 #define MAYBE_RequestMIDIAccess DISABLED_RequestMIDIAccess
 #else
@@ -127,7 +127,12 @@ class MidiBrowserTestBlockMidiByDefault : public ContentBrowserTest {
 IN_PROC_BROWSER_TEST_F(MidiBrowserTest, MAYBE_RequestMIDIAccess) {
   NavigateAndCheckResult("/midi/request_midi_access.html");
 }
+IN_PROC_BROWSER_TEST_F(MidiBrowserTestBlockMidiByDefault,
+                       MAYBE_RequestMIDIAccess) {
+  NavigateAndCheckResult("/midi/request_midi_access.html");
+}
 
+// MidiManager has no Fuchsia implementation (see https://crbug.com/1302995).
 #if BUILDFLAG(IS_FUCHSIA)
 #define MAYBE_SubscribeAll DISABLED_SubscribeAll
 #else
@@ -136,23 +141,6 @@ IN_PROC_BROWSER_TEST_F(MidiBrowserTest, MAYBE_RequestMIDIAccess) {
 IN_PROC_BROWSER_TEST_F(MidiBrowserTest, MAYBE_SubscribeAll) {
   NavigateAndCheckResult("/midi/subscribe_all.html");
 }
-
-// TODO(https://crbug.com/1302995): MidiManager has no Fuchsia implementation.
-#if BUILDFLAG(IS_FUCHSIA)
-#define MAYBE_RequestMIDIAccess DISABLED_RequestMIDIAccess
-#else
-#define MAYBE_RequestMIDIAccess RequestMIDIAccess
-#endif
-IN_PROC_BROWSER_TEST_F(MidiBrowserTestBlockMidiByDefault,
-                       MAYBE_RequestMIDIAccess) {
-  NavigateAndCheckResult("/midi/request_midi_access.html");
-}
-
-#if BUILDFLAG(IS_FUCHSIA)
-#define MAYBE_SubscribeAll DISABLED_SubscribeAll
-#else
-#define MAYBE_SubscribeAll SubscribeAll
-#endif
 IN_PROC_BROWSER_TEST_F(MidiBrowserTestBlockMidiByDefault, MAYBE_SubscribeAll) {
   NavigateAndCheckResult("/midi/subscribe_all.html");
 }
