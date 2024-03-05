b6529c5252ee2c15febe1060f76e9eae9fb25494
Friedrich Horschig <fhorschig@chromium.org>
2024-03-05 08:10:51 +0000

    [Android][3PPWM] Remove autofill flag from ChromeFeatureList
    Autofill has its own Feature collection. Therefore, remove
    the ViewStructure feature from the generic list.
    
    Bug: b/326231439
    Change-Id: I02ffd2411894ab82c92b5c3034de8d3bc67cc111
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5317757
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1268300}

--- chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/options/AutofillOptionsCoordinator.java
+++ chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/options/AutofillOptionsCoordinator.java
@@ -17,6 +17,7 @@ import androidx.lifecycle.Observer;
 
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.preferences.Pref;
+import org.chromium.components.autofill.AutofillFeatures;
 import org.chromium.components.user_prefs.UserPrefs;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
@@ -56,7 +57,7 @@ public class AutofillOptionsCoordinator {
     @VisibleForTesting
     AutofillOptionsCoordinator(AutofillOptionsFragment fragment) {
         assert ChromeFeatureList.isEnabled(
-                ChromeFeatureList.AUTOFILL_VIRTUAL_VIEW_STRUCTURE_ANDROID);
+                AutofillFeatures.AUTOFILL_VIRTUAL_VIEW_STRUCTURE_ANDROID);
         mFragment = fragment;
         mMediator = new AutofillOptionsMediator(mFragment.getProfile());
     }
