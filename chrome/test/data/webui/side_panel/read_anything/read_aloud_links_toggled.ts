2ea7e36dfda4bd7880b1d63c1bade258eba29158
Kelvin Jiang <kelvinjiang@chromium.org>
2024-03-05 01:35:08 +0000

    Revert "Convert max speech length and highlight toggle tests to Mocha+TS"
    This reverts commit a766dd553b0242e11f1b823007da87a301b7962e.
    
    Reason for revert: Tree (compile) failure, "ReadAloudMochaTest" was not defined (see comment on reverted CL)
    
    Original change's description:
    > Convert max speech length and highlight toggle tests to Mocha+TS
    >
    > This also includes removing excess code from the links test which I realized weren't needed after doing the max speech length test.
    >
    > Bug: 323960128
    > Change-Id: If7590a37bdc9e4eda1729001c67e1f870e16d037
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5332645
    > Reviewed-by: Lauren Winston <lwinston@google.com>
    > Commit-Queue: Kristi Saney <kristislee@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1268187}
    
    Bug: 323960128
    Change-Id: I031dca082ece84873bcb7994fffdbd2de3dae3a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5342486
    Owners-Override: Kelvin Jiang <kelvinjiang@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Matt Jones <mdjones@google.com>
    Cr-Commit-Position: refs/heads/main@{#1268192}

--- chrome/test/data/webui/side_panel/read_anything/read_aloud_links_toggled.ts
+++ chrome/test/data/webui/side_panel/read_anything/read_aloud_links_toggled.ts
@@ -4,13 +4,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 import 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything_toolbar.js';
 
+import {BrowserProxy} from '//resources/cr_components/color_change_listener/browser_proxy.js';
 import type {CrIconButtonElement} from '//resources/cr_elements/cr_icon_button/cr_icon_button.js';
 import type {ReadAnythingElement} from 'chrome-untrusted://read-anything-side-panel.top-chrome/app.js';
 import {LINK_TOGGLE_BUTTON_ID, LINKS_DISABLED_ICON, LINKS_ENABLED_ICON} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything_toolbar.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome-untrusted://webui-test/chai_assert.js';
 
+import {TestColorUpdaterBrowserProxy} from './test_color_updater_browser_proxy.js';
+
 suite('ReadAloudLinksToggled', () => {
   let app: ReadAnythingElement;
+  let testBrowserProxy: TestColorUpdaterBrowserProxy;
   let linksToggleButton: CrIconButtonElement;
   let playPauseButton: CrIconButtonElement;
 
@@ -83,6 +87,8 @@ suite('ReadAloudLinksToggled', () => {
 
   setup(() => {
     suppressInnocuousErrors();
+    testBrowserProxy = new TestColorUpdaterBrowserProxy();
+    BrowserProxy.setInstance(testBrowserProxy);
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
     // Do not call the real `onConnected()`. As defined in
     // ReadAnythingAppController, onConnected creates mojo pipes to connect to
