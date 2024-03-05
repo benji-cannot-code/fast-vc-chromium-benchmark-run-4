404a8f94a3173ed8e2ea498b6b64a070267b9705
Christoph Schwering <schwering@google.com>
2024-03-05 22:55:43 +0000

    [Autofill] Move EnableHeavyFormDataScraping config to the renderer
    Bug: 41489599
    Change-Id: I6b1ffaafcb87130c87b0bfa1e5a45512b3079cff
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5339900
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268748}

--- chrome/browser/autofill/mock_autofill_agent.h
+++ chrome/browser/autofill/mock_autofill_agent.h
@@ -80,7 +80,6 @@ class MockAutofillAgent : public mojom::AutofillAgent {
               PreviewPasswordGenerationSuggestion,
               (const ::std::u16string& password),
               (override));
-  MOCK_METHOD(void, EnableHeavyFormDataScraping, (), (override));
   MOCK_METHOD(void,
               GetPotentialLastFourCombinationsForStandaloneCvc,
               (base::OnceCallback<void(const std::vector<std::string>&)>),
