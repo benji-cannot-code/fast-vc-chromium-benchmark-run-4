59b27556b39b0c90f097be335cc848f222682d72
Adem Derinel <derinel@google.com>
2024-03-05 08:11:41 +0000

    [Android][PasswordEditDialog] Disable android autofill save prompt
    Bug: b/325269974
    Change-Id: I00945673d7346613188288cca916d29e5a702b34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5331889
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268301}

---
+++ chrome/browser/password_edit_dialog/android/java/src/org/chromium/chrome/browser/password_edit_dialog/TextInputEditTextNoAutofill.java
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.password_edit_dialog;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewStructure;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+/**
+ * A wrapper class around {@link TextInputEditText} to stop Android Autofill from showing the save
+ * password prompt. This is achieved by overriding the {@code onProvideAutofillStructure} method.
+ */
+public class TextInputEditTextNoAutofill extends TextInputEditText {
+
+    public TextInputEditTextNoAutofill(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void onProvideAutofillStructure(ViewStructure structure, int flags) {}
+}
