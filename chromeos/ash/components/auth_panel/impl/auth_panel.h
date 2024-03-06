1183396b7c500f18c3b21434b68da7adccc3c721
Elie Maamari <emaamari@google.com>
2024-03-06 09:04:43 +0000

    Show AuthPanel in settings when feature flag is enabled
    Bug: b:271248452
    Change-Id: Ib172f696d3f79112a4eca2e64f589dbf670fde14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5307817
    Commit-Queue: Elie Maamari <emaamari@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268933}

--- chromeos/ash/components/auth_panel/impl/auth_panel.h
+++ chromeos/ash/components/auth_panel/impl/auth_panel.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
+class AuthHubConnector;
 class AuthPanelEventDispatcher;
 class AuthPanelEventDispatcherFactory;
 class AuthFactorStore;
@@ -37,7 +38,8 @@ class AuthPanel : public views::View, public AuthFactorStatusConsumer {
       std::unique_ptr<FactorAuthViewFactory> view_factory,
       std::unique_ptr<AuthFactorStoreFactory> store_factory,
       std::unique_ptr<AuthPanelEventDispatcherFactory> event_dispatcher_factory,
-      auth_panel::AuthCompletionCallback on_auth_complete);
+      auth_panel::AuthCompletionCallback on_auth_complete,
+      AuthHubConnector* connector);
   AuthPanel(const AuthPanel&) = delete;
   AuthPanel(AuthPanel&&) = delete;
   AuthPanel& operator=(const AuthPanel&) = delete;
@@ -66,6 +68,8 @@ class AuthPanel : public views::View, public AuthFactorStatusConsumer {
   base::flat_map<AshAuthFactor, views::View*> views_;
 
   auth_panel::AuthCompletionCallback on_auth_complete_;
+
+  raw_ptr<AuthHubConnector> auth_hub_connector_;
 };
 
 }  // namespace ash
