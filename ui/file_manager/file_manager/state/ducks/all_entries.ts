22883e5ee3f72e0fa6945b36520c05877ac6ccf1
Luciano Pacheco <lucmult@chromium.org>
2024-03-06 03:38:03 +0000

    Files app: Change from Entry to FileData in the new DirectoryTree
    This is the first CL of a series to update the Directory Tree to not
    depend on the Entry type, instead it should use the FileData type.
    
    This is a preparation to onboard a new type called Materialized View
    that isn't directly linked to Entry and to start moving away from the
    File System API.
    
    Bug: b:327534506
    Change-Id: Ic5c845361b77d54dda3df5fb2b4a54295843230b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5333949
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268875}

--- ui/file_manager/file_manager/state/ducks/all_entries.ts
+++ ui/file_manager/file_manager/state/ducks/all_entries.ts
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import type {EntryLocation} from '../../background/js/entry_location_impl.js';
+import type {VolumeInfo} from '../../background/js/volume_info.js';
 import {getParentEntry} from '../../common/js/api.js';
 import {isDriveRootEntryList, isEntryInsideDrive, isEntryScannable, isEntrySupportUiChildren, isFakeEntryInDrives, isGrandRootEntryInDrives, isVolumeEntry, readEntries, shouldSupportDriveSpecificIcons, sortEntries} from '../../common/js/entry_utils.js';
 import {getIcon} from '../../common/js/file_type.js';
@@ -227,20 +228,26 @@ function getEntryIcon(
 }
 
 /**
- * Given an entry, check if its loading children should be delayed.
+ * Given fileData, check if its loading children should be delayed.
  * We are doing this for SMB to avoid potentially hanging whilst scanning a
  * large SMB file share and causing performance issues.
  */
-export function shouldDelayLoadingChildren(entry: Entry|
-                                           FilesAppEntry): boolean {
-  const {volumeManager} = window.fileManager;
+export function shouldDelayLoadingChildren(
+    fileData: FileData, state: State): boolean {
   // When this function is triggered when mounting new volumes, volumeInfo is
   // not available in the VolumeManager yet, we need to get volumeInfo from the
   // entry itself.
-  const volumeInfo = isVolumeEntry(entry) ? entry.volumeInfo :
-                                            volumeManager.getVolumeInfo(entry);
-  return volumeInfo?.source === Source.NETWORK &&
-      volumeInfo.volumeType === VolumeType.SMB;
+  const volume: Volume|VolumeInfo|undefined =
+      fileData.type === EntryType.VOLUME_ROOT ?
+      // TODO: Confirm how to remove the usage of entry here.
+      (fileData.entry as VolumeEntry).volumeInfo :
+      state!.volumes[fileData.volumeId!];
+  return isVolumeSlowToScan(volume);
+}
+
+function isVolumeSlowToScan(volume?: Volume|VolumeInfo|null): boolean {
+  return volume?.source === Source.NETWORK &&
+      volume.volumeType === VolumeType.SMB;
 }
 
 /**
@@ -275,7 +282,7 @@ export function convertEntryToFileData(entry: Entry|FilesAppEntry): FileData {
       metadataModel.getCache([entry as FileEntry], prefetchPropertyNames)[0]! :
       {} as MetadataItem;
 
-  return {
+  const fileData: FileData = {
     key: entry.toURL(),
     fullPath: entry.fullPath,
     entry,
@@ -289,12 +296,16 @@ export function convertEntryToFileData(entry: Entry|FilesAppEntry): FileData {
     expanded: false,
     disabled: 'disabled' in entry ? entry.disabled as boolean : false,
     isRootEntry: !!locationInfo?.isRootEntry,
+    canExpand: false,
     // `isEjectable` is determined by its corresponding volume, will be updated
     // when volume is added.
     isEjectable: false,
-    canExpand: shouldDelayLoadingChildren(entry),
     children: [],
   };
+  // For slow volumes, we always mark the root and directories as canExpand, to
+  // avoid scanning to determine if it has sub-directories.
+  fileData.canExpand = isVolumeSlowToScan(volumeInfo);
+  return fileData;
 }
 
 /**
