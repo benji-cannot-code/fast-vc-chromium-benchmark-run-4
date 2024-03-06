e458780ca3249b9839ada0202b98a30f6150d97b
Jan Keitel <jkeitel@google.com>
2024-03-06 19:43:41 +0000

    Remove feature parameters in favor of grit strings.
    There is no need for feature parameters to set UI strings, as those can
    be set directly in the finch config.
    
    Bug: 327716672
    Change-Id: If4ec85301c97da882d212b4fb6e013aafd94a3b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5335343
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1269162}

--- components/plus_addresses/features.cc
+++ components/plus_addresses/features.cc
@@ -12,10 +12,6 @@ namespace plus_addresses::features {
 
 namespace {
 
-constexpr char kEnterprisePlusAddressSuggestionLabelOverrideName[] =
-    "suggestion-label";
-constexpr char kEnterprisePlusAddressSettingsLabelOverrideName[] =
-    "settings-label";
 constexpr char kEnterprisePlusAddressOAuthScopeName[] = "oauth-scope";
 constexpr char kEnterprisePlusAddressServerUrlName[] = "server-url";
 constexpr char kSyncWithEnterprisePlusAddressServerName[] = "sync-with-server";
@@ -32,14 +28,6 @@ BASE_FEATURE(kFeature,
              "PlusAddressesEnabled",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-const base::FeatureParam<std::string>
-    kEnterprisePlusAddressSuggestionLabelOverride{
-        &kFeature, kEnterprisePlusAddressSuggestionLabelOverrideName,
-        "Lorem Ipsum"};
-const base::FeatureParam<std::string>
-    kEnterprisePlusAddressSettingsLabelOverride{
-        &kFeature, kEnterprisePlusAddressSettingsLabelOverrideName,
-        "Lorem Ipsum"};
 const base::FeatureParam<std::string> kEnterprisePlusAddressOAuthScope{
     &kFeature, kEnterprisePlusAddressOAuthScopeName, ""};
 const base::FeatureParam<std::string> kEnterprisePlusAddressServerUrl{
