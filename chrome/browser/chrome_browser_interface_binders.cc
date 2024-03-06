560d4e298d970ce05bb28923f1f42c6dceb825ad
Jason Hu <hujasonx@google.com>
2024-03-06 00:23:54 +0000

    Add mojom scaffolding for Lens overlay
    Bug: 327506247
    Change-Id: Id046b26a54c578494a5f86dad2efed9eaafa1203
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5336117
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1268803}

--- chrome/browser/chrome_browser_interface_binders.cc
+++ chrome/browser/chrome_browser_interface_binders.cc
@@ -73,6 +73,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history_clusters/core/history_clusters_service.h"
 #include "components/history_clusters/history_clusters_internals/webui/history_clusters_internals_ui.h"
 #include "components/history_embeddings/history_embeddings_features.h"
+#include "components/lens/lens_features.h"
 #include "components/live_caption/caption_util.h"
 #include "components/live_caption/pref_names.h"
 #include "components/no_state_prefetch/browser/no_state_prefetch_contents.h"
@@ -184,6 +185,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/hats/hats_ui.h"
 #include "chrome/browser/ui/webui/history/history_ui.h"
 #include "chrome/browser/ui/webui/internals/user_education/user_education_internals.mojom.h"
+#include "chrome/browser/ui/webui/lens/lens_untrusted_ui.h"
 #include "chrome/browser/ui/webui/new_tab_page/new_tab_page.mojom.h"
 #include "chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.h"
 #include "chrome/browser/ui/webui/new_tab_page_third_party/new_tab_page_third_party_ui.h"
@@ -1858,6 +1860,10 @@ void PopulateChromeWebUIFrameInterfaceBrokers(
       .Add<compose::mojom::ComposeSessionUntrustedPageHandlerFactory>();
 #endif  // BUILDFLAG(ENABLE_COMPOSE)
 #if !BUILDFLAG(IS_ANDROID)
+  if (lens::features::IsLensOverlayEnabled()) {
+    registry.ForWebUI<lens::LensUntrustedUI>()
+        .Add<lens::mojom::LensPageHandlerFactory>();
+  }
   if (companion::IsCompanionFeatureEnabled()) {
     registry.ForWebUI<CompanionSidePanelUntrustedUI>()
         .Add<side_panel::mojom::CompanionPageHandlerFactory>();
