281c52adaace72c0d669e87402908f70b3831540
Ben Joyce <bjoyce@google.com>
2024-03-06 08:07:15 +0000

    Fix AppModalPresenterTest flakiness with espresso 3.2
    These tests are flaky when ran with espresso 3.2 after API 29. Changing the showDialog with 'waitForShow' = false  does not fix all tests.
    
    Bug: 40821218
    Change-Id: I31a4141f856aa55eab5f6f7fd43894dde18ad89f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5346203
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268925}

--- ui/android/javatests/src/org/chromium/ui/test/util/ViewUtils.java
+++ ui/android/javatests/src/org/chromium/ui/test/util/ViewUtils.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.ui.test.util;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 
 import static org.hamcrest.Matchers.greaterThan;
@@ -142,6 +143,23 @@ public class ViewUtils {
     public static void waitForView(
             ViewGroup root, Matcher<View> viewMatcher, @ExpectedViewState int viewState) {
         CriteriaHelper.pollUiThread(new ExpectedViewCriteria(viewMatcher, viewState, root));
+    }
+
+    /**
+     * Waits until a view in a dialog root view that matches the given matcher and any of the given
+     * {@link ExpectedViewState}s. waitForViewCheckingState is flaky with dialogs after api 29.
+     * Fails if the matcher applies to multiple views. Times out after {@link
+     * CriteriaHelper#DEFAULT_MAX_TIME_TO_POLL} milliseconds.
+     *
+     * @param viewMatcher The matcher matching the view that should be waited for.
+     * @param viewState State that the matching view should be in. If multiple states are passed,
+     *     the waiting will stop if at least one applies.
+     */
+    public static void waitForDialogViewCheckingState(
+            Matcher<View> viewMatcher, @ExpectedViewState int viewState) {
+        onView(isRoot())
+                .inRoot(isDialog())
+                .check(withEventualExpectedViewState(viewMatcher, viewState));
     }
 
     /**
