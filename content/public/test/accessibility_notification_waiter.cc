be862e542aac43eee17e84118ab42260cbbcf6b3
Greg Thompson <grt@chromium.org>
2024-03-06 18:56:56 +0000

    [accessibility] Deflake accessibility tests that use guest views
    PDFOCRIntegrationTests use an AccessibilityNotificationWaiter on an
    outer WebContents and wait for changes to the accessibility tree
    generated by renderers for an inner WebContents. In some cases, the
    outer WC's tree is rooted at about:blank.
    AccessibilityNotificationWaiter has a special carve-out to ignore events
    generated while about:blank is loading, which is a problem in this case.
    
    In this CL, we gently change the waiter's OnAccessibilityEvent so that
    it doesn't ignore events to an inner WC (e.g., one hosting a PDF) when
    the outer is about:blank.
    
    Bug: 325530075
    Change-Id: Ie0c5392d7bf0503a54176ba749f9439e4cb7e740
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5349147
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269119}

--- content/public/test/accessibility_notification_waiter.cc
+++ content/public/test/accessibility_notification_waiter.cc
@@ -144,12 +144,8 @@ bool AccessibilityNotificationWaiter::WaitForNotificationWithTimeout(
 }
 
 const ui::AXTree& AccessibilityNotificationWaiter::GetAXTree() const {
-  static base::NoDestructor<ui::AXTree> empty_tree;
-  WebContentsImpl* web_contents_impl =
-      static_cast<WebContentsImpl*>(web_contents());
-  BrowserAccessibilityManager* manager =
-      web_contents_impl->GetRootBrowserAccessibilityManager();
-  return manager && manager->ax_tree() ? *manager->ax_tree() : *empty_tree;
+  return GetAXTreeForFrame(
+      static_cast<WebContentsImpl*>(web_contents())->GetPrimaryMainFrame());
 }
 
 void AccessibilityNotificationWaiter::BindOnAccessibilityEvent(
@@ -163,8 +159,9 @@ void AccessibilityNotificationWaiter::OnAccessibilityEvent(
     RenderFrameHostImpl* rfhi,
     ax::mojom::Event event_type,
     int event_target_id) {
-  if (IsAboutBlank())
+  if (IsAboutBlank(rfhi)) {
     return;
+  }
 
   VLOG(1) << "OnAccessibilityEvent " << event_type;
 
@@ -210,8 +207,9 @@ void AccessibilityNotificationWaiter::OnGeneratedEvent(
   DCHECK_NE(event_target_id, ui::kInvalidAXNodeID);
   VLOG(1) << "OnGeneratedEvent " << event;
 
-  if (IsAboutBlank())
+  if (IsAboutBlank(/*render_frame=*/nullptr)) {
     return;
+  }
 
   if (generated_event_to_wait_for_ == event) {
     event_target_id_ = event_target_id;
@@ -225,8 +223,9 @@ void AccessibilityNotificationWaiter::OnGeneratedEvent(
 }
 
 void AccessibilityNotificationWaiter::OnLocationsChanged() {
-  if (IsAboutBlank())
+  if (IsAboutBlank(/*render_frame=*/nullptr)) {
     return;
+  }
 
   notification_received_ = true;
   loop_runner_quit_closure_.Run();
@@ -245,11 +244,24 @@ void AccessibilityNotificationWaiter::OnFocusChanged() {
   }
 }
 
-bool AccessibilityNotificationWaiter::IsAboutBlank() {
+const ui::AXTree& AccessibilityNotificationWaiter::GetAXTreeForFrame(
+    RenderFrameHostImpl* render_frame) const {
+  static base::NoDestructor<ui::AXTree> empty_tree;
+  WebContentsImpl* web_contents_impl =
+      WebContentsImpl::FromRenderFrameHostImpl(render_frame);
+  BrowserAccessibilityManager* manager =
+      web_contents_impl->GetRootBrowserAccessibilityManager();
+  return manager && manager->ax_tree() ? *manager->ax_tree() : *empty_tree;
+}
+
+bool AccessibilityNotificationWaiter::IsAboutBlank(
+    RenderFrameHostImpl* render_frame) {
   // Skip any accessibility notifications related to "about:blank",
   // to avoid a possible race condition between the test beginning
   // listening for accessibility events and "about:blank" loading.
-  return GetAXTree().data().url == url::kAboutBlankURL;
+  const auto& ax_tree =
+      render_frame ? GetAXTreeForFrame(render_frame) : GetAXTree();
+  return ax_tree.data().url == url::kAboutBlankURL;
 }
 
 // WebContentsObserver override:
