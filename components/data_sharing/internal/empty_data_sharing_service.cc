68a53507ee2e4275699058e5181066d67af38bd9
Maksim Moskvitin <mmoskvitin@google.com>
2024-03-06 17:47:49 +0000

    [DataSharing] Add platform-agnostic group-related API skeleton
    This CL adds new methods (not yet implemented) to DataSharingService,
    that provides API to access/edit group data, needed to implement the UI
    and maybe few other things. Java counterpart is to be added in a follow
    up.
    
    DataSharingService diverges from SDK:
    1. Most of the methods work with singular, e.g. there is no batch group
    editing operations. This likely suits all our needs and slightly simpler
    to use.
    2. API uses dedicated data structures, decoupling them early from DB
    protos (coming soon) and SDK protos should give us more flexibility in
    the future in case we prefer to diverge from them more.
    3. Instead of using single UpdateGroup(Action), there are dedicated
    methods, such as AddMember()/RemoveMember(). The reasoning is similar
    to 1., on top of that this makes API more constrained -> perhaps harder
    to misuse / better compile-time checks.
    
    Bug: 301389859
    Change-Id: Ia0100d857677ce914c3356cf59eb39a032bd510b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5307401
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1269038}

--- components/data_sharing/internal/empty_data_sharing_service.cc
+++ components/data_sharing/internal/empty_data_sharing_service.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/data_sharing/internal/empty_data_sharing_service.h"
 
+#include "base/functional/callback.h"
+
 namespace data_sharing {
 
 EmptyDataSharingService::EmptyDataSharingService() = default;
@@ -19,5 +21,30 @@ DataSharingNetworkLoader*
 EmptyDataSharingService::GetDataSharingNetworkLoader() {
   return nullptr;
 }
+
+void EmptyDataSharingService::ReadAllGroups(
+    base::OnceCallback<void(const GroupsDataSetOrFailureOutcome&)> callback) {}
+
+void EmptyDataSharingService::ReadGroup(
+    const std::string& group_id,
+    base::OnceCallback<void(const GroupDataOrFailureOutcome&)> callback) {}
+
+void EmptyDataSharingService::CreateGroup(
+    const std::string& group_name,
+    base::OnceCallback<void(const GroupDataOrFailureOutcome&)> callback) {}
+
+void EmptyDataSharingService::DeleteGroup(
+    const std::string& group_id,
+    base::OnceCallback<void(PeopleGroupActionOutcome)> callback) {}
+
+void EmptyDataSharingService::InviteMember(
+    const std::string& group_id,
+    const std::string& invitee_gaia_id,
+    base::OnceCallback<void(PeopleGroupActionOutcome)> callback) {}
+
+void EmptyDataSharingService::RemoveMember(
+    const std::string& group_id,
+    const std::string& member_gaia_id,
+    base::OnceCallback<void(PeopleGroupActionOutcome)> callback) {}
 
 }  // namespace data_sharing
