84e9b642df8317f634a1807a7e0e615532e36166
Luke Warlow <lwarlow@igalia.com>
2024-03-06 22:06:20 +0000

    Add interestaction attribute
    This adds support for the experimental `interestaction` attribute,
    as specified in the open-ui "Interest Invokers" explainer.
    
    (https://open-ui.org/components/interest-invokers.explainer/)
    
    The `interestaction` attribute maps to the IDL `interestAction` similar
    to `invokeAction`.
    
    This attribute currently has no behaviour changes associated with it,
    that will be handled in follow-ups.
    
    Bug: 326681249
    Change-Id: I18c806476378ef7213482d339032ecd4b0a50b78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5340853
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Luke <lwarlow@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1269261}

--- third_party/blink/renderer/core/html/html_anchor_element.h
+++ third_party/blink/renderer/core/html/html_anchor_element.h
@@ -108,6 +108,8 @@ class CORE_EXPORT HTMLAnchorElement : public HTMLElement, public DOMURLUtils {
 
   Element* interestTargetElement();
 
+  AtomicString interestAction() const;
+
   void Trace(Visitor*) const override;
 
  protected:
