18bb93f73ca7cfb41bd53e90a84d6eaf9d9fd03a
Mikihito Matsuura <mikt@google.com>
2024-03-06 16:49:57 +0000

    Revert "Reland "Convert max speech length and highlight toggle tests to Mocha+TS""
    This reverts commit 1dd894caaf89b167965c7da648f16be6937e9324.
    
    Reason for revert: 
    `ReadAnythingReadAloudMochaTest.SpeechUsesMaxTextLength` fails.
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests%20(dbg)(1)/119068/overview
    
    
    Original change's description:
    > Reland "Convert max speech length and highlight toggle tests to Mocha+TS"
    >
    > This is a reland of commit a766dd553b0242e11f1b823007da87a301b7962e
    >
    > Original change's description:
    > > Convert max speech length and highlight toggle tests to Mocha+TS
    > >
    > > This also includes removing excess code from the links test which I realized weren't needed after doing the max speech length test.
    > >
    > > Bug: 323960128
    > > Change-Id: If7590a37bdc9e4eda1729001c67e1f870e16d037
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5332645
    > > Reviewed-by: Lauren Winston <lwinston@google.com>
    > > Commit-Queue: Kristi Saney <kristislee@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1268187}
    >
    > Bug: 323960128
    > Change-Id: I94ad00d5607fcd9c55cd3a1023e4dbc1d2324cab
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5344839
    > Commit-Queue: Kristi Saney <kristislee@google.com>
    > Reviewed-by: Lauren Winston <lwinston@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1268557}
    
    Bug: 323960128
    Change-Id: I9b8cfafc67d4924a3efd17d927a4d2c7af30ea32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5348556
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269020}

--- chrome/test/data/webui/side_panel/read_anything/fake_reading_mode.ts
+++ chrome/test/data/webui/side_panel/read_anything/fake_reading_mode.ts
@@ -25,7 +25,7 @@ export class FakeReadingMode {
 
   // Current audio settings values.
   speechRate: number = 1;
-  highlightGranularity: number = 1;
+  highlightGranularity: number = 0;
 
   // Enum values for various visual theme changes.
   standardLineSpacing: number = 0;
@@ -177,12 +177,8 @@ export class FakeReadingMode {
   onVoiceChange(_voice: string, _lang: string) {}
 
   // Called when the highlight granularity is changed via the webui toolbar.
-  turnedHighlightOn() {
-    this.highlightGranularity = 1;
-  }
-  turnedHighlightOff() {
-    this.highlightGranularity = 0;
-  }
+  turnedHighlightOn() {}
+  turnedHighlightOff() {}
 
   // Returns the actual spacing value to use based on the given lineSpacing
   // category.
--- chrome/test/data/webui/side_panel/read_anything/read_anything_browsertest.cc
+++ chrome/test/data/webui/side_panel/read_anything/read_anything_browsertest.cc
@@ -73,11 +73,6 @@ IN_PROC_BROWSER_TEST_F(ReadAnythingMochaTest, PlayPause) {
                    "mocha.run()");
 }
 
-IN_PROC_BROWSER_TEST_F(ReadAnythingMochaTest, HighlightToggle) {
-  RunSidePanelTest("side_panel/read_anything/highlight_toggle_test.js",
-                   "mocha.run()");
-}
-
 IN_PROC_BROWSER_TEST_F(ReadAnythingMochaTest, RateSelection) {
   RunSidePanelTest("side_panel/read_anything/rate_selection_test.js",
                    "mocha.run()");
@@ -102,10 +97,5 @@ class ReadAnythingReadAloudMochaTest : public ReadAnythingMochaBrowserTest {
 
 IN_PROC_BROWSER_TEST_F(ReadAnythingReadAloudMochaTest, ReadAloudLinksToggled) {
   RunSidePanelTest("side_panel/read_anything/read_aloud_links_toggled.js",
-                   "mocha.run()");
-}
-
-IN_PROC_BROWSER_TEST_F(ReadAnythingReadAloudMochaTest, SpeechUsesMaxTextLength) {
-  RunSidePanelTest("side_panel/read_anything/speech_uses_max_text_length.js",
                    "mocha.run()");
 }
