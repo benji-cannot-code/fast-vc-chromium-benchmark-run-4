ef2dea2677ab1fb91e89b2117cc6182a0d9ccc73
Courtney Wong <courtneywong@chromium.org>
2024-03-06 20:06:14 +0000

    Families TS migration: Convert PostMessageApi tests to TS
    Bug: b:306190951
    Change-Id: I0bed3d806d1d3a4f809673fd1e1184720e144a09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5346835
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269185}

--- chrome/test/data/webui/chromeos/ash_common/post_message_api/post_message_api_browsertest.js
+++ chrome/test/data/webui/chromeos/ash_common/post_message_api/post_message_api_browsertest.js
@@ -8,9 +8,9 @@ GEN_INCLUDE(['//chrome/test/data/webui/chromeos/polymer_browser_test_base.js']);
 
 GEN('#include "content/public/test/browser_test.h"');
 
-function PostMessageAPIModuleTest() {}
+function PostMessageApiModuleTest() {}
 
-PostMessageAPIModuleTest.prototype = {
+PostMessageApiModuleTest.prototype = {
 
   __proto__: testing.Test.prototype,
 
@@ -23,6 +23,6 @@ PostMessageAPIModuleTest.prototype = {
 
 // Test the postmessage api server and client by defining server methods, and
 // passing a series of postmessages.
-TEST_F('PostMessageAPIModuleTest', 'PostMessageCommTest', function() {
-  runMochaTest('PostMessageAPIModuleTest', 'PostMessageCommTest');
+TEST_F('PostMessageApiModuleTest', 'PostMessageCommTest', function() {
+  runMochaTest('PostMessageApiModuleTest', 'PostMessageCommTest');
 });
