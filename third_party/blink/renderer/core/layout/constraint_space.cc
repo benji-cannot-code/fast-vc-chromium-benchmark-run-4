26b35f8a8361900472c88f429347439764398478
Koji Ishii <kojii@chromium.org>
2024-03-06 21:51:34 +0000

    [text-box-trim] Move `ConstraintSpace` bits to `RareData`
    Saves the space added in crrev.com/c/5334688.
    
    Bug: 40254880
    Change-Id: I35dc5e357b86457d7e7eb06063542551dd1debe6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5348678
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269244}

--- third_party/blink/renderer/core/layout/constraint_space.cc
+++ third_party/blink/renderer/core/layout/constraint_space.cc
@@ -27,7 +27,7 @@ struct SameSizeAsConstraintSpace {
     void* rare_data;
   };
   ExclusionSpace exclusion_space;
-  unsigned bitfields[2];
+  unsigned bitfields[1];
 };
 
 ASSERT_SIZE(ConstraintSpace, SameSizeAsConstraintSpace);
