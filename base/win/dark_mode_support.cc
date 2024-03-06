7a5c2fe981efdf7448c0b4122fce3af19b4e8579
Stefan Smolen <ssmole@microsoft.com>
2024-03-06 19:53:32 +0000

    Don't load user32.dll and gdi32.dll in sandboxed browser test processes
    Browser tests on Windows run the entire browser stack, including the
    sandbox. One sandbox mitigation is 'win32k lockdown', which prevents
    loading gdi32.dll or anything that depends on it in the process (like
    user32.dll or shell32.dll). Today they load anyway due to sandbox
    interceptions on these OS dlls.
    
    In preparation for the removal of these interceptions, modify code that
    attempts to load these dlls in every sandboxed browser test process:
    1) Add a delayload config where these dlls are statically linked
    2) Add IsUser32AndGdi32Available checks for win32k lockdown for code
       which calls in to these DLLs.
    
    These changes have the tests run under a similar environment as retail
    chrome, so the tested behavior more closely matches shipping code.
    
    Bug: 326277735
    Change-Id: I8c22f4a4c40ffe6b763f11a1caf00cf975b8226c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5325283
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Stefan Smolen <ssmole@microsoft.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269176}

--- base/win/dark_mode_support.cc
+++ base/win/dark_mode_support.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/native_library.h"
+#include "base/win/win_util.h"
 #include "base/win/windows_version.h"
 
 namespace {
@@ -58,8 +59,10 @@ const DarkModeSupport& GetDarkModeSupport() {
       [] {
         DarkModeSupport dark_mode_support;
         auto* os_info = base::win::OSInfo::GetInstance();
-        // Dark mode only works on WIN10_RS5 and up.
-        if (os_info->version() >= base::win::Version::WIN10_RS5) {
+        // Dark mode only works on WIN10_RS5 and up. uxtheme.dll depends on
+        // GDI32.dll which is not available under win32k lockdown sandbox.
+        if (os_info->version() >= base::win::Version::WIN10_RS5 &&
+            base::win::IsUser32AndGdi32Available()) {
           base::NativeLibraryLoadError error;
           HMODULE ux_theme_lib = base::PinSystemLibrary(L"uxtheme.dll", &error);
           DCHECK(!error.code);
