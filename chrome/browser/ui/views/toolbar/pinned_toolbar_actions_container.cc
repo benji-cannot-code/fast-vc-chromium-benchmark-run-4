22f0d2be4eb1c84d13245f57f4758833f034b9b0
Caroline Rising <corising@chromium.org>
2024-03-06 19:54:02 +0000

    Allow for ephemeral non-active button in the PinnedToolbarActionContainer
    This in needed for things like the downloads button after a recent
    download if it is not pinned.
    
    Bug: 323982992
    Change-Id: I0deefe69bfe70617682786e7238cf500a413dd8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5344671
    Reviewed-by: Joseph Park <josephjoopark@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269177}

--- chrome/browser/ui/views/toolbar/pinned_toolbar_actions_container.cc
+++ chrome/browser/ui/views/toolbar/pinned_toolbar_actions_container.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/vector_icon_types.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/actions/action_view_controller.h"
+#include "ui/views/cascading_property.h"
 #include "ui/views/controls/button/button_controller.h"
 #include "ui/views/layout/animating_layout_manager.h"
 #include "ui/views/layout/flex_layout.h"
@@ -238,12 +239,31 @@ void PinnedToolbarActionsContainer::UpdateActionState(actions::ActionId id,
     button->ResetHighlight();
   }
 
-  if (!pinned && !is_active) {
-    RemovePoppedOutButtonFor(id);
+  if (!is_active) {
+    MaybeRemovePoppedOutButtonFor(id);
   }
 
   UpdateDividerFlexSpecification();
   InvalidateLayout();
+}
+
+void PinnedToolbarActionsContainer::ShowActionEphemerallyInToolbar(
+    actions::ActionId id,
+    bool show) {
+  auto* button = GetButtonFor(id);
+  // If the button doesn't exist and shouldn't be shown, do nothing.
+  if (!button && !show) {
+    return;
+  }
+  // Create the button if it doesn't exist.
+  if (!button) {
+    button = AddPopOutButtonFor(id);
+  }
+  button->SetShouldShowEphemerallyInToolbar(show);
+
+  if (!show) {
+    MaybeRemovePoppedOutButtonFor(id);
+  }
 }
 
 void PinnedToolbarActionsContainer::UpdateDividerFlexSpecification() {
@@ -475,12 +495,13 @@ PinnedActionToolbarButton* PinnedToolbarActionsContainer::AddPopOutButtonFor(
   return button;
 }
 
-void PinnedToolbarActionsContainer::RemovePoppedOutButtonFor(
+void PinnedToolbarActionsContainer::MaybeRemovePoppedOutButtonFor(
     const actions::ActionId& id) {
   const auto iter = base::ranges::find(
       popped_out_buttons_, id,
       [](PinnedActionToolbarButton* button) { return button->GetActionId(); });
-  if (iter == popped_out_buttons_.end()) {
+  if (iter == popped_out_buttons_.end() ||
+      ShouldRemainPoppedOutInToolbar(*iter)) {
     return;
   }
   GetAnimatingLayoutManager()->FadeOut(*iter);
@@ -529,7 +550,7 @@ void PinnedToolbarActionsContainer::RemovePinnedActionButtonFor(
   if (iter == pinned_buttons_.end()) {
     return;
   }
-  if (!(*iter)->IsActive()) {
+  if (!ShouldRemainPoppedOutInToolbar(*iter)) {
     GetAnimatingLayoutManager()->FadeOut(*iter);
     GetAnimatingLayoutManager()->PostOrQueueAction(
         base::BindOnce(&PinnedToolbarActionsContainer::RemoveButton,
@@ -559,6 +580,19 @@ PinnedActionToolbarButton* PinnedToolbarActionsContainer::GetPoppedOutButtonFor(
       popped_out_buttons_, id,
       [](PinnedActionToolbarButton* button) { return button->GetActionId(); });
   return iter == popped_out_buttons_.end() ? nullptr : *iter;
+}
+
+PinnedActionToolbarButton* PinnedToolbarActionsContainer::GetButtonFor(
+    const actions::ActionId& id) {
+  if (auto* button = GetPinnedButtonFor(id)) {
+    return button;
+  }
+  return GetPoppedOutButtonFor(id);
+}
+
+bool PinnedToolbarActionsContainer::ShouldRemainPoppedOutInToolbar(
+    PinnedActionToolbarButton* button) {
+  return button->IsActive() || button->ShouldShowEphemerallyInToolbar();
 }
 
 void PinnedToolbarActionsContainer::RemoveButton(
