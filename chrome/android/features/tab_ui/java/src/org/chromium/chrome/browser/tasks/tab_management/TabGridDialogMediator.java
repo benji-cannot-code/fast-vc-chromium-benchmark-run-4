c07b521226817762fc6d600665ae595b9a4bca7e
Hailey Wang <haileywang@google.com>
2024-03-06 00:38:33 +0000

    [Data Sharing] Add onClick listeners to the share bottom toolbar
    Screenshot: https://screenshot.googleplex.com/9hmhSmVpMqyYa46.png
    
    Clicking on the bottom toolbar icon tiles or the bottom toolbar add button will add 1 to the count. This behavior is for prototyping purposes and will be changed later.
    
    Bug: b/324909919
    Change-Id: I52efe5e27d80e7effe35bb8c5854c795ea32d077
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5328437
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268811}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogMediator.java
@@ -348,9 +348,16 @@ public class TabGridDialogMediator
         setupToolbarEditText();
 
         mModel.set(TabGridDialogProperties.MENU_CLICK_LISTENER, getMenuButtonClickListener());
+
+        // TODO(b/325082444): Only a subset should be visible at a time. Only set the listeners that
+        // can be seen and used.
+        mModel.set(TabGridDialogProperties.SHARE_INVITE_CLICK_LISTENER, getShareBarClickListener());
         mModel.set(
-                TabGridDialogProperties.SHARE_INVITE_CLICK_LISTENER,
-                getShareInviteButtonClickListener());
+                TabGridDialogProperties.SHARE_IMAGE_TILES_CLICK_LISTENER,
+                getShareBarClickListener());
+        mModel.set(
+                TabGridDialogProperties.SHARE_MANAGE_ADD_CLICK_LISTENER,
+                getShareBarClickListener());
     }
 
     void hideDialog(boolean showAnimation) {
@@ -669,10 +676,13 @@ public class TabGridDialogMediator
                 mToolbarMenuCallback);
     }
 
-    private View.OnClickListener getShareInviteButtonClickListener() {
+    private View.OnClickListener getShareBarClickListener() {
         return view -> {
             // TODO(b/325082444): Ask data sharing service about if the tab group is shared.
             mModel.set(TabGridDialogProperties.IS_TAB_GROUP_SHARED, true);
+            if (mSharedImageTilesCoordinator != null) {
+                mSharedImageTilesCoordinator.updateTilesCount(0);
+            }
         };
     }
 
