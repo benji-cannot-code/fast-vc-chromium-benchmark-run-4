a4c5d2ef8cfa5f803ba36bdb455c819ea8bd2e8a
Noémie St-Onge <noemies@google.com>
2024-03-06 20:11:16 +0000

    [iOSKAUpgrade] Add empty state header to manual fill address table view
    This CL displays the "No addresses found" message in the address
    manual fill view when the Keyboard Accessory Upgrade feature is enabled
    and there a no address suggestions to show.
    
    UI with the feature on:
    https://screenshot.googleplex.com/6DENJsDbRvVWhMM.png
    
    Mocks: go/ios-keyboard-accessory-upgrade-mocks
    
    Fixed: 328089736
    Change-Id: If7449e95d8d5cab98891745050ffbe5be6b2723c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5321217
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1269189}

--- ios/chrome/browser/ui/autofill/manual_fill/address_view_controller.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/address_view_controller.mm
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/ios/ios_util.h"
 #import "base/metrics/histogram_macros.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
+#import "ios/chrome/browser/shared/ui/table_view/cells/table_view_text_header_footer_item.h"
 #import "ios/chrome/browser/shared/ui/table_view/legacy_chrome_table_view_styler.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/ui/autofill/manual_fill/manual_fill_action_cell.h"
@@ -17,6 +19,10 @@ namespace manual_fill {
 
 NSString* const AddressTableViewAccessibilityIdentifier =
     @"kManualFillAddressTableViewAccessibilityIdentifier";
+
+enum ManualFallbackItemType : NSInteger {
+  kNoAddressesMessage = kItemTypeEnumZero,
+};
 
 }  // namespace manual_fill
 
@@ -36,6 +42,17 @@ NSString* const AddressTableViewAccessibilityIdentifier =
 - (void)presentAddresses:(NSArray<ManualFillAddressItem*>*)addresses {
   UMA_HISTOGRAM_COUNTS_100("ManualFallback.PresentedOptions.Profiles",
                            addresses.count);
+
+  if (!addresses.count && IsKeyboardAccessoryUpgradeEnabled()) {
+    TableViewTextHeaderFooterItem* textHeaderFooterItem =
+        [[TableViewTextHeaderFooterItem alloc]
+            initWithType:manual_fill::ManualFallbackItemType::
+                             kNoAddressesMessage];
+    textHeaderFooterItem.text =
+        l10n_util::GetNSString(IDS_IOS_MANUAL_FALLBACK_NO_ADDRESSES);
+    self.noDataItemsToShowHeaderItem = textHeaderFooterItem;
+  }
+
   [self presentDataItems:(NSArray<TableViewItem*>*)addresses];
 }
 
