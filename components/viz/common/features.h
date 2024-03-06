5128c46d4a66338a5d35f0fcc71af9bbf5ef1dee
Robert Flack <flackr@chromium.org>
2024-03-06 18:38:34 +0000

    Use 'reflect' edge mode for back drop filters
    SkTileMode::kMirror is equivalent to the "reflect" edge mode that was
    recently approved as the new edge mode for CSS backdrop filters in:
    https://github.com/w3c/fxtf-drafts/issues/374.
    
    Bug: 40040614
    Change-Id: I7b4861fa85ba2f1fd2dabb003bec635f3f2da57a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5274043
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269094}

--- components/viz/common/features.h
+++ components/viz/common/features.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace features {
 
+VIZ_COMMON_EXPORT BASE_DECLARE_FEATURE(kBackdropFilterMirrorEdgeMode);
 VIZ_COMMON_EXPORT BASE_DECLARE_FEATURE(kDelegatedCompositing);
 VIZ_COMMON_EXPORT BASE_DECLARE_FEATURE(kDelegateTransforms);
 VIZ_COMMON_EXPORT BASE_DECLARE_FEATURE(kRecordSkPicture);
