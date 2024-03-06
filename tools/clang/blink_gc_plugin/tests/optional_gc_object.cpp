b524bef923b78b13d9975bcee2f5fc48f2c32ccb
Omer Katz <omerkatz@chromium.org>
2024-03-06 21:52:46 +0000

    [blink_gc_plugin] Implement check for raw_ptr of GCed or Traceable
    Change-Id: Iff9cb160f8862320399f39e991f608d71513c4d0
    Cq-Include-Trybots: luci.chromium.try:linux_upload_clang,mac_upload_clang,mac_upload_clang_arm,win_upload_clang
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5349439
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269246}

--- tools/clang/blink_gc_plugin/tests/optional_gc_object.cpp
+++ tools/clang/blink_gc_plugin/tests/optional_gc_object.cpp
@@ -17,6 +17,8 @@ class WithOpt : public GarbageCollected<WithOpt> {
   absl::optional<Traceable>
       optional_field3_;  // Optional fields are disallowed.
   std::optional<Traceable> optional_field4_;
+  base::raw_ptr<Base> raw_ptr_field_;
+  base::raw_ptr<Traceable> raw_ptr_field2_;
 };
 
 void DisallowedUseOfOptional() {
@@ -53,6 +55,23 @@ void DisallowedUseOfOptional() {
     new std::optional<Traceable>;  // New expression with traceable optionals
                                    // are not allowed.
   }
+
+  {
+    base::raw_ptr<Base> raw_ptr_base;  // Must be okay.
+    (void)raw_ptr_base;
+
+    base::raw_ptr<Derived> raw_ptr_derived;  // Must also be okay.
+    (void)raw_ptr_derived;
+
+    base::raw_ptr<Traceable> raw_ptr_traceable;  // Must also be okay.
+    (void)raw_ptr_traceable;
+
+    new base::raw_ptr<Base>;  // New expression with gced raw_ptrs are not
+                              // allowed.
+
+    new base::raw_ptr<Traceable>;  // New expression with traceable raw_ptrs
+                                   // are not allowed.
+  }
 }
 
 class OnStack {
@@ -64,6 +83,8 @@ class OnStack {
     (void)optional_field2_;
     (void)optional_field3_;
     (void)optional_field4_;
+    (void)raw_ptr_field_;
+    (void)raw_ptr_field2_;
   }
 
  private:
@@ -72,6 +93,8 @@ class OnStack {
   std::optional<Base> optional_field2_;
   absl::optional<Traceable> optional_field3_;
   std::optional<Traceable> optional_field4_;
+  base::raw_ptr<Base> raw_ptr_field_;
+  base::raw_ptr<Traceable> raw_ptr_field2_;
 };
 
 }  // namespace blink
