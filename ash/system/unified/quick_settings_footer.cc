429778704f9acf510971f91295770bf76bbf22a1
Paul Semel <paulsemel@chromium.org>
2024-03-06 18:43:35 +0000

    DanglingPtr: triage unretained dangling pointers
    This triages new unretained dangling pointers found in the code base.
    Those are mostly triggered by tests.
    
    This is a non functional change.
    
    Bug: 40061562
    Change-Id: I76f116c7b45f7959175247425b42d4b43b7abe72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5349119
    Commit-Queue: Paul Semel <paulsemel@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Owners-Override: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269103}

--- ash/system/unified/quick_settings_footer.cc
+++ ash/system/unified/quick_settings_footer.cc
@@ -130,7 +130,9 @@ QsBatteryInfoViewBase::QsBatteryInfoViewBase(
                            QsButtonCatalogName::kBatteryButton);
                        controller->HandleOpenPowerSettingsAction();
                      },
-                     controller),
+                     // TODO(https://crbug.com/1380714): Remove
+                     // `UnsafeDanglingUntriaged`
+                     base::UnsafeDanglingUntriaged(controller)),
                  PowerStatus::Get()->GetInlinedStatusString(),
                  type,
                  icon,
@@ -286,7 +288,8 @@ QuickSettingsFooter::QuickSettingsFooter(
                   QsButtonCatalogName::kAvatarButton);
               controller->ShowUserChooserView();
             },
-            base::Unretained(controller))));
+            // TODO(https://crbug.com/1380714): Remove `UnsafeDanglingUntriaged`
+            base::UnsafeDanglingUntriaged(controller))));
     user_avatar_button->SetEnabled(
         UserChooserDetailedViewController::IsUserChooserEnabled());
     user_avatar_button->SetID(VIEW_ID_QS_USER_AVATAR_BUTTON);
@@ -301,7 +304,9 @@ QuickSettingsFooter::QuickSettingsFooter(
                       QsButtonCatalogName::kSignOutButton);
                   controller->HandleSignOutAction();
                 },
-                base::Unretained(controller)),
+                // TODO(https://crbug.com/1380714): Remove
+                // `UnsafeDanglingUntriaged`
+                base::UnsafeDanglingUntriaged(controller)),
             user::GetLocalizedSignOutStringForStatus(
                 Shell::Get()->session_controller()->login_status(),
                 /*multiline=*/false),
