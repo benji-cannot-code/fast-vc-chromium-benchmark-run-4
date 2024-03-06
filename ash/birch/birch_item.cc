19bbd0dc8e5472670b2a7f3acb06cfc683677018
Owen Zhang <owenzhang@google.com>
2024-03-06 00:39:30 +0000

    [Birch] Add LoadIcon for File Items.
    Bug: b/305093221
    Test: ash_unittests --gtest_filter=**BirchItemIconTest**
    Change-Id: I0890aff0055cb687c7b78fda5a70ff09d7ebb3b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5346342
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Auto-Submit: Owen Zhang <owenzhang@google.com>
    Commit-Queue: Owen Zhang <owenzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1268812}

--- ash/birch/birch_item.cc
+++ ash/birch/birch_item.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/time_formatting.h"
 #include "base/logging.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chromeos/ui/base/file_icon_util.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "ui/base/models/image_model.h"
 
@@ -201,9 +202,8 @@ void BirchFileItem::PerformAction() {
 }
 
 void BirchFileItem::LoadIcon(LoadIconCallback callback) {
-  // TODO(jamescook): Figure out how to show the file icon based on its type.
-  // See chromeos/ui/base/file_icon_util.h for ideas.
-  std::move(callback).Run(ui::ImageModel());
+  std::move(callback).Run(
+      ui::ImageModel::FromVectorIcon(chromeos::GetIconForPath(file_path)));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
