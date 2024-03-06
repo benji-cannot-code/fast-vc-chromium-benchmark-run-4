3350721f8aa1ac635e8d8372cbe9e8277bc521d2
Sky Malice <skym@chromium.org>
2024-03-06 18:17:29 +0000

    Remove CallbackController for TabLoader to fix crash.
    The CallbackController wasn't even used anymore, so this should not
    affect normal usage at all.
    
    Bug: b:323834771
    Change-Id: I7f4346200bc3d4793a2827c190eef7fdc7257695
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5349634
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269074}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabPersistentStore.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabPersistentStore.java
@@ -19,7 +19,6 @@ import androidx.annotation.VisibleForTesting;
 import androidx.core.util.AtomicFile;
 
 import org.chromium.base.Callback;
-import org.chromium.base.CallbackController;
 import org.chromium.base.Log;
 import org.chromium.base.ObserverList;
 import org.chromium.base.StreamUtil;
@@ -1521,7 +1520,6 @@ public class TabPersistentStore {
     private class TabLoader {
         public final TabRestoreDetails mTabToRestore;
         private LoadTabTask mLoadTabTask;
-        private CallbackController mCallbackController = new CallbackController();
 
         /**
          * @param tabToRestore details of {@link Tab} which will be read from storage
@@ -1540,7 +1538,6 @@ public class TabPersistentStore {
             if (mLoadTabTask != null) {
                 mLoadTabTask.cancel(mayInterruptIfRunning);
             }
-            mCallbackController.destroy();
         }
     }
 
