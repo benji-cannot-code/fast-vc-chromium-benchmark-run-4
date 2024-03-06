ea9687ced46402fa3060d72641ca7ea6af621a38
Kristi Saney <kristislee@google.com>
2024-03-06 23:04:42 +0000

    Fix tests
    Reland "Reland "Convert max speech length and highlight toggle tests to Mocha+TS""
    
    This is a reland of commit 1dd894caaf89b167965c7da648f16be6937e9324
    
    Original change's description:
    > Reland "Convert max speech length and highlight toggle tests to Mocha+TS"
    >
    > This is a reland of commit a766dd553b0242e11f1b823007da87a301b7962e
    >
    > Original change's description:
    > > Convert max speech length and highlight toggle tests to Mocha+TS
    > >
    > > This also includes removing excess code from the links test which I realized weren't needed after doing the max speech length test.
    > >
    > > Bug: 323960128
    > > Change-Id: If7590a37bdc9e4eda1729001c67e1f870e16d037
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5332645
    > > Reviewed-by: Lauren Winston <lwinston@google.com>
    > > Commit-Queue: Kristi Saney <kristislee@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1268187}
    >
    > Bug: 323960128
    > Change-Id: I94ad00d5607fcd9c55cd3a1023e4dbc1d2324cab
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5344839
    > Commit-Queue: Kristi Saney <kristislee@google.com>
    > Reviewed-by: Lauren Winston <lwinston@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1268557}
    
    Bug: 323960128
    Change-Id: Icc595116e51e553d69ee015b1a3ba2949ce35df5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5348888
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1269289}

--- chrome/test/data/webui/side_panel/read_anything/fake_reading_mode.ts
+++ chrome/test/data/webui/side_panel/read_anything/fake_reading_mode.ts
@@ -25,7 +25,7 @@ export class FakeReadingMode {
 
   // Current audio settings values.
   speechRate: number = 1;
-  highlightGranularity: number = 0;
+  highlightGranularity: number = 1;
 
   // Enum values for various visual theme changes.
   standardLineSpacing: number = 0;
@@ -177,8 +177,12 @@ export class FakeReadingMode {
   onVoiceChange(_voice: string, _lang: string) {}
 
   // Called when the highlight granularity is changed via the webui toolbar.
-  turnedHighlightOn() {}
-  turnedHighlightOff() {}
+  turnedHighlightOn() {
+    this.highlightGranularity = 1;
+  }
+  turnedHighlightOff() {
+    this.highlightGranularity = 0;
+  }
 
   // Returns the actual spacing value to use based on the given lineSpacing
   // category.
