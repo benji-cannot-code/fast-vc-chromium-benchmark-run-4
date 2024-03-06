a4574949996ee402fd289d45a5f78c8ccb352dde
Keishi Hattori <keishi@chromium.org>
2024-03-06 21:49:16 +0000

    [MTE] Change ChangeMemoryTaggingModeForAllThreadsPerProcess to return true on success
    ChangeMemoryTaggingModeForAllThreadsPerProcess() had a CHECK to see if the change was
    successful. This CL changes it so it returns true on success instead.
    
    This way we can customize the action after failure to change the MTE mode.
    
    Change-Id: I708728173d5546ac2ac1042225a4da81e61df982
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5316250
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269240}

--- base/allocator/partition_allocator/src/partition_alloc/page_allocator_unittest.cc
+++ base/allocator/partition_allocator/src/partition_alloc/page_allocator_unittest.cc
@@ -312,8 +312,9 @@ TEST(PartitionAllocPageAllocatorTest,
       {
   // Switch to synchronous mode.
 #if BUILDFLAG(IS_ANDROID)
-        ChangeMemoryTaggingModeForAllThreadsPerProcess(
+        bool success = ChangeMemoryTaggingModeForAllThreadsPerProcess(
             TagViolationReportingMode::kSynchronous);
+        EXPECT_TRUE(success);
 #else
         ChangeMemoryTaggingModeForCurrentThread(
             TagViolationReportingMode::kSynchronous);
@@ -364,8 +365,9 @@ TEST(PartitionAllocPageAllocatorTest,
       {
   // Switch to asynchronous mode.
 #if BUILDFLAG(IS_ANDROID)
-        ChangeMemoryTaggingModeForAllThreadsPerProcess(
+        bool success = ChangeMemoryTaggingModeForAllThreadsPerProcess(
             TagViolationReportingMode::kAsynchronous);
+        EXPECT_TRUE(success);
 #else
         ChangeMemoryTaggingModeForCurrentThread(
             TagViolationReportingMode::kAsynchronous);
