adb28ecef27e7516eff67426b9395b25efdbc613
Junichi Uekawa <uekawa@google.com>
2024-03-06 04:50:23 +0000

    concierge: Call StartVm2 instead of StartVm.
    Send array of fds instead of sending variable number of fds via dbus.
    
    BUG=b:324960184
    
    Change-Id: I4185d61346fa94c9ad7803a49287587b1f82cc0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5320045
    Reviewed-by: David Munro <davidmunro@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Junichi Uekawa <uekawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268893}

--- chromeos/ash/components/dbus/concierge/concierge_client.cc
+++ chromeos/ash/components/dbus/concierge/concierge_client.cc
@@ -140,7 +140,8 @@ class ConciergeClientImpl : public ConciergeClient {
   void StartVm(const concierge::StartVmRequest& request,
                chromeos::DBusMethodCallback<concierge::StartVmResponse>
                    callback) override {
-    CallMethod(concierge::kStartVmMethod, request, std::move(callback));
+    CallMethodWithFds(concierge::kStartVm2Method, request, {},
+                      std::move(callback));
   }
 
   void StartVmWithFd(
@@ -148,8 +149,10 @@ class ConciergeClientImpl : public ConciergeClient {
       const vm_tools::concierge::StartVmRequest& request,
       chromeos::DBusMethodCallback<vm_tools::concierge::StartVmResponse>
           callback) override {
-    CallMethodWithFd(concierge::kStartVmMethod, request, std::move(fd),
-                     std::move(callback));
+    std::vector<base::ScopedFD> fds;
+    fds.emplace_back(std::move(fd));
+    CallMethodWithFds(concierge::kStartVm2Method, request, std::move(fds),
+                      std::move(callback));
   }
 
   void StartVmWithFds(
@@ -157,7 +160,7 @@ class ConciergeClientImpl : public ConciergeClient {
       const vm_tools::concierge::StartVmRequest& request,
       chromeos::DBusMethodCallback<vm_tools::concierge::StartVmResponse>
           callback) override {
-    CallMethodWithFds(concierge::kStartVmMethod, request, std::move(fds),
+    CallMethodWithFds(concierge::kStartVm2Method, request, std::move(fds),
                       std::move(callback));
   }
 
@@ -363,9 +366,14 @@ class ConciergeClientImpl : public ConciergeClient {
       return;
     }
 
-    for (auto& fd : fds) {
-      if (fd.is_valid())
-        writer.AppendFileDescriptor(fd.get());
+    {
+      dbus::MessageWriter array_writer(nullptr);
+
+      writer.OpenArray("h", &array_writer);
+      for (const auto& fd : fds) {
+        array_writer.AppendFileDescriptor(fd.get());
+      }
+      writer.CloseContainer(&array_writer);
     }
 
     concierge_proxy_->CallMethod(
@@ -379,10 +387,26 @@ class ConciergeClientImpl : public ConciergeClient {
                         const RequestProto& request,
                         base::ScopedFD fd,
                         chromeos::DBusMethodCallback<ResponseProto> callback) {
-    std::vector<base::ScopedFD> fds;
-    fds.push_back(std::move(fd));
-    CallMethodWithFds(method_name, request, std::move(fds),
-                      std::move(callback));
+    dbus::MethodCall method_call(concierge::kVmConciergeInterface, method_name);
+    dbus::MessageWriter writer(&method_call);
+
+    if (!writer.AppendProtoAsArrayOfBytes(request)) {
+      LOG(ERROR) << "Failed to encode protobuf for " << method_name;
+      base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
+          FROM_HERE, base::BindOnce(std::move(callback), std::nullopt));
+      return;
+    }
+
+    // TODO(b/324960184): Keep this check for compatibility until we update
+    // CreateDiskImage and ExportDiskImage
+    if (fd.is_valid()) {
+      writer.AppendFileDescriptor(fd.get());
+    }
+
+    concierge_proxy_->CallMethod(
+        &method_call, kConciergeDBusTimeoutMs,
+        base::BindOnce(&ConciergeClientImpl::OnDBusProtoResponse<ResponseProto>,
+                       weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
   }
 
   template <typename RequestProto, typename ResponseProto>
@@ -435,8 +459,9 @@ class ConciergeClientImpl : public ConciergeClient {
       return;
     }
 
-    for (auto& observer : vm_observer_list_)
+    for (auto& observer : vm_observer_list_) {
       observer.OnVmStarted(vm_started_signal);
+    }
   }
 
   void OnVmStoppedSignal(dbus::Signal* signal) {
@@ -450,8 +475,9 @@ class ConciergeClientImpl : public ConciergeClient {
       return;
     }
 
-    for (auto& observer : vm_observer_list_)
+    for (auto& observer : vm_observer_list_) {
       observer.OnVmStopped(vm_stopped_signal);
+    }
   }
 
   void OnVmStoppingSignal(dbus::Signal* signal) {
@@ -506,8 +532,9 @@ class ConciergeClientImpl : public ConciergeClient {
                          const std::string& signal_name,
                          bool is_connected) {
     DCHECK_EQ(interface_name, concierge::kVmConciergeInterface);
-    if (!is_connected)
+    if (!is_connected) {
       LOG(ERROR) << "Failed to connect to signal: " << signal_name;
+    }
 
     if (signal_name == concierge::kVmStartedSignal) {
       is_vm_started_signal_connected_ = is_connected;
@@ -568,8 +595,9 @@ void ConciergeClient::InitializeFake() {
 void ConciergeClient::InitializeFake(FakeCiceroneClient* fake_cicerone_client) {
   // Do not create a new fake if it was initialized early in a browser test to
   // allow the test to set its own client.
-  if (!FakeConciergeClient::Get())
+  if (!FakeConciergeClient::Get()) {
     new FakeConciergeClient(fake_cicerone_client);
+  }
 }
 
 // static
