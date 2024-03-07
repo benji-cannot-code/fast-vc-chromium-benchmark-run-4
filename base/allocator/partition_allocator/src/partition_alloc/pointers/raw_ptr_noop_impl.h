28ddd1d91d7732ad93f11a74744abb73293c7fec
Kevin Lin <kevinptt@chromium.org>
2024-03-07 10:13:23 +0000

    [BRP] Comment unused parameters
    Comment unused parameters in BRP to avoid "unused parameter" warnings.
    
    Bug: b:327075114
    Change-Id: I586f6651b875feabcedfdb135c132d9a2fa2e4fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5340693
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Kevin Lin <kevinptt@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269484}

--- base/allocator/partition_allocator/src/partition_alloc/pointers/raw_ptr_noop_impl.h
+++ base/allocator/partition_allocator/src/partition_alloc/pointers/raw_ptr_noop_impl.h
@@ -70,7 +70,7 @@ struct RawPtrNoOpImpl {
       typename =
           std::enable_if_t<partition_alloc::internal::is_offset_type<Z>, void>>
   PA_ALWAYS_INLINE static constexpr T*
-  Advance(T* wrapped_ptr, Z delta_elems, bool is_in_pointer_modification) {
+  Advance(T* wrapped_ptr, Z delta_elems, bool /*is_in_pointer_modification*/) {
     return wrapped_ptr + delta_elems;
   }
 
@@ -81,7 +81,7 @@ struct RawPtrNoOpImpl {
       typename =
           std::enable_if_t<partition_alloc::internal::is_offset_type<Z>, void>>
   PA_ALWAYS_INLINE static constexpr T*
-  Retreat(T* wrapped_ptr, Z delta_elems, bool is_in_pointer_modification) {
+  Retreat(T* wrapped_ptr, Z delta_elems, bool /*is_in_pointer_modification*/) {
     return wrapped_ptr - delta_elems;
   }
 
