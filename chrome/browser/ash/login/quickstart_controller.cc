29ee08e3bddae33baf967cae185f1f7cfdd39fe6
Renato Silva <rrsilva@google.com>
2024-03-07 18:05:17 +0000

    [QuickStart] Fix hotkey activation
    Properly enable bluetooth observation when using the hotkey and
    also fix an issue that prevented the entry point button from
    being shown on the Network and Gaia screens when using the hotkey.
    
    Test: Manual on Pixelbook
    Bug: b/327251904
    Change-Id: I9bc42758e91967cfb131586b2fe595f5548517d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5340134
    Auto-Submit: Renato Silva <rrsilva@google.com>
    Reviewed-by: Brittany Hartmire <bhartmire@google.com>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1269683}

--- chrome/browser/ash/login/quickstart_controller.cc
+++ chrome/browser/ash/login/quickstart_controller.cc
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/constants/ash_features.h"
+#include "ash/constants/ash_switches.h"
 #include "ash/public/cpp/bluetooth_config_service.h"
 #include "base/check.h"
 #include "base/logging.h"
+#include "chrome/browser/ash/login/demo_mode/demo_setup_controller.h"
 #include "chrome/browser/ash/login/oobe_quick_start/connectivity/target_device_connection_broker.h"
 #include "chrome/browser/ash/login/oobe_quick_start/oobe_quick_start_pref_names.h"
 #include "chrome/browser/ash/login/oobe_quick_start/target_device_bootstrap_controller.h"
@@ -167,6 +169,8 @@ void QuickStartController::ForceEnableQuickStart() {
   }
 
   InitTargetDeviceBootstrapController();
+  StartObservingBluetoothState();
+
   QS_LOG(INFO) << "Force enabling LocalPasswordsForConsumers!";
   ash::features::ForceEnableLocalPasswordsForConsumers();
 }
@@ -176,6 +180,13 @@ void QuickStartController::DetermineEntryPointVisibility(
   // Bootstrap controller is only instantiated when the feature is enabled (also
   // via the keyboard shortcut. See |ForceEnableQuickStart|.)
   if (!bootstrap_controller_) {
+    std::move(callback).Run(/*visible=*/false);
+    return;
+  }
+
+  // QuickStart should not be enabled for Demo mode or OS Install flows
+  if (DemoSetupController::IsOobeDemoSetupFlowInProgress() ||
+      ash::switches::IsOsInstallAllowed()) {
     std::move(callback).Run(/*visible=*/false);
     return;
   }
