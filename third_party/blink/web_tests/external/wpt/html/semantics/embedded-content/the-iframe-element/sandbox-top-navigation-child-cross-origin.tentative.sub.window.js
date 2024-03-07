98708e91d0f9d219ea66d5c08d2331fd519541d2
Liam Brady <lbrady@google.com>
2024-03-07 22:11:26 +0000

    Clear user activation on cross-site navigations.
    When full site isolation is disabled, renderer processes and
    RenderFrameHosts are re-used when performing cross-site navigations.
    This includes user activation state, and, more specifically, the sticky
    `has_been_active_` bit in `UserActivationState`.
    
    Currently, the `UserActivationState` on the renderer-side is reset only
    if the navigation's associated frame is a main frame. That means that if
    an iframe navigates to a cross-site page, its sticky user activation
    state will be the leftover state from the previous page. So, if a user
    interacted with the previous page in any capacity, the newly loaded page
    will think it has received a user gesture, essentially using an
    unintentional cache of the user activation state.
    
    This becomes an issue when dealing with our framebusting interventions.
    We only allow an iframe to do a top-level navigation if it received a
    user gesture. However, if an iframe's previous document received a user
    activation, or worse, if the iframe was not navigated to anything and
    got a user activation because its embedder was interacted with, this
    allows the current document to circumvent our framebusting
    interventions. The latter happens because of same-origin descendant
    activation behavior. See:
    https://source.chromium.org/chromium/chromium/src/+/main:content/browser/renderer_host/frame_tree_node.cc;l=766-778;drc=30753b1135fa271a3b45bbdbfef6567e46733a7f;bpv=1;bpt=1
    
    Note that this problem does not exist if site isolation is enabled
    (which is by default on desktop platforms), since a cross-site
    navigation will create a whole new process with a fresh
    `UserActivationState`.
    
    To fix this, this CL clears the user activation state on cross-site
    subframe navigations in the renderer (user activation is already cleared
    for main frames). To ensure that same-site navigations persist user
    state even if a cross-origin or same-origin navigation results in a new
    process or RenderFrameHost being created, this CL also explicitly
    transfers sticky user activation state for all same-site
    cross-RenderFrameHost navigations. This takes place in the browser, and
    the resulting bit to determine if a frame should have sticky user
    activation is passed to the renderer.
    
    The ultimate end goal is to unconditionally clear the user activation
    state for all cross-document navigations. That unfortunately is not
    possible today as there are entrenched use cases that rely on sticky
    user activation state being cached for same-site navigations. See:
    https://crbug.com/40228985.
    
    This CL also fixes the aforementioned regression when enabling the
    RenderDocument feature, since this CL will now preserve the sticky user
    activation state regardless of what
    process/RenderFrameHost/RenderDocument state the navigation results in.
    
    This CL adds some tests to the no-auto-wpt-origin-isolation test suite, which requires some additional description:
    
    * These tests are running on all platforms because site isolation behavior may differ per platform
    * All of the tests in the-iframe-element are being added because it would be useful to understand their behavior in all expected process
    configurations
    * The total time taken for this test suite on linux-rel showed a total time percentage of <0.3%
    
    Bug: 41493458
    Change-Id: Ibec11437fcd03470571e04a4e0dfaadffddf6c03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5269464
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Andrew Verge <averge@chromium.org>
    Commit-Queue: Liam Brady <lbrady@google.com>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269856}

---
+++ third_party/blink/web_tests/external/wpt/html/semantics/embedded-content/the-iframe-element/sandbox-top-navigation-child-cross-origin.tentative.sub.window.js
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title=Top-level navigation tests with cross origin & user activated child frames
+// META: script=/common/dispatcher/dispatcher.js
+// META: script=/common/get-host-info.sub.js
+// META: script=/common/utils.js
+// META: script=/resources/testdriver.js
+// META: script=/resources/testdriver-vendor.js
+// META: script=/html/browsers/browsing-the-web/remote-context-helper/resources/remote-context-helper.js
+// META: script=./resources/sandbox-top-navigation-helper.js
+
+'use strict';
+
+// /* ---------------------- CROSS ORIGIN (A -> B) TESTS ---------------------- */
+
+promise_test(async t => {
+  const main = await setupTest();
+  const iframe_1 = await createNestedIframe(main,
+      "HTTP_REMOTE_ORIGIN", "allow-top-navigation", "");
+
+  await attemptTopNavigation(iframe_1, true);
+}, "A cross-origin frame with frame sandbox flags can navigate top");
+
+promise_test(async t => {
+  const main = await setupTest();
+  const iframe_1 = await createNestedIframe(main,
+      "HTTP_REMOTE_ORIGIN", "", "allow-top-navigation");
+
+  await attemptTopNavigation(iframe_1, false);
+}, "A cross-origin frame with delivered sandbox flags can not navigate top");
--- third_party/blink/web_tests/external/wpt/html/semantics/embedded-content/the-iframe-element/sandbox-top-navigation-user-activation.tentative.sub.window.js
+++ third_party/blink/web_tests/external/wpt/html/semantics/embedded-content/the-iframe-element/sandbox-top-navigation-user-activation.tentative.sub.window.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=/common/get-host-info.sub.js
 // META: script=/common/utils.js
 // META: script=/resources/testdriver.js
+// META: script=/resources/testdriver-actions.js
 // META: script=/resources/testdriver-vendor.js
 // META: script=/html/browsers/browsing-the-web/remote-context-helper/resources/remote-context-helper.js
 // META: script=./resources/sandbox-top-navigation-helper.js
@@ -28,21 +29,3 @@ promise_test(async t => {
 
   await attemptTopNavigation(iframe_1, false);
 }, "allow-top-navigation-by-user-activation set but no sticky activation");
-
-/* ---------------------- CROSS ORIGIN (A -> B) TESTS ---------------------- */
-
-promise_test(async t => {
-  const main = await setupTest();
-  const iframe_1 = await createNestedIframe(main,
-      "HTTP_REMOTE_ORIGIN", "allow-top-navigation", "");
-
-  await attemptTopNavigation(iframe_1, true);
-}, "A cross-origin frame with frame sandbox flags can navigate top");
-
-promise_test(async t => {
-  const main = await setupTest();
-  const iframe_1 = await createNestedIframe(main,
-      "HTTP_REMOTE_ORIGIN", "", "allow-top-navigation");
-
-  await attemptTopNavigation(iframe_1, false);
-}, "A cross-origin frame with delivered sandbox flags can not navigate top");
