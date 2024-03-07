a66d4f274ce4a4bd56f0df321d412a54762db2fe
ahmedmoussa <ahmedmoussa@google.com>
2024-03-07 16:59:01 +0000

    Add device counter into page info string for camera and mic
    This change would only affects Win/Mac/Linux when `#camera-mic-preview` flag is enabled
    
    Screenshot: https://screenshot.googleplex.com/5xG8762TPmraYdG.png
    
    Bug: b:325547637
    Change-Id: Iac95e6867aee8cd8038869fcf817881d080a7ef8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5302957
    Commit-Queue: Ahmed Moussa <ahmedmoussa@google.com>
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1269634}

--- chrome/browser/ui/views/page_info/page_info_permission_content_view.cc
+++ chrome/browser/ui/views/page_info/page_info_permission_content_view.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/page_info/page_info_permission_content_view.h"
 
 #include "base/feature_list.h"
-#include "base/ranges/algorithm.h"
+#include "base/strings/string_number_conversions.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/layout_constants.h"
 #include "chrome/browser/ui/page_info/chrome_page_info_ui_delegate.h"
@@ -201,6 +201,29 @@ void PageInfoPermissionContentView::PermissionChanged() {
                                       permission_.is_one_time);
 }
 
+#if !BUILDFLAG(IS_CHROMEOS) && !BUILDFLAG(IS_FUCHSIA)
+void PageInfoPermissionContentView::OnAudioDevicesChanged(
+    const std::optional<std::vector<media::AudioDeviceDescription>>&
+        device_infos) {
+  if (type_ == ContentSettingsType::MEDIASTREAM_MIC && device_infos) {
+    title_->SetText(l10n_util::GetStringFUTF16(
+        IDS_SITE_SETTINGS_TYPE_MIC_WITH_COUNT,
+        base::NumberToString16(
+            media_effects::GetRealAudioDeviceCount(device_infos.value()))));
+  }
+}
+
+void PageInfoPermissionContentView::OnVideoDevicesChanged(
+    const std::optional<std::vector<media::VideoCaptureDeviceInfo>>&
+        device_infos) {
+  if (type_ == ContentSettingsType::MEDIASTREAM_CAMERA && device_infos) {
+    title_->SetText(l10n_util::GetStringFUTF16(
+        IDS_SITE_SETTINGS_TYPE_CAMERA_WITH_COUNT,
+        base::NumberToString16(device_infos->size())));
+  }
+}
+#endif
+
 void PageInfoPermissionContentView::MaybeAddMediaPreview(
     content::WebContents* web_contents,
     views::View& preceding_separator) {
@@ -212,6 +235,16 @@ void PageInfoPermissionContentView::MaybeAddMediaPreview(
   if (type_ != ContentSettingsType::MEDIASTREAM_CAMERA &&
       type_ != ContentSettingsType::MEDIASTREAM_MIC) {
     return;
+  }
+
+  auto* cached_device_info = media_effects::MediaDeviceInfo::GetInstance();
+  devices_observer_.Observe(cached_device_info);
+  if (type_ == ContentSettingsType::MEDIASTREAM_CAMERA) {
+    // Initialize `title_` with the current number of cached video devices.
+    OnVideoDevicesChanged(cached_device_info->GetVideoDeviceInfos());
+  } else {
+    // Initialize `title_` with the current number of cached audio devices.
+    OnAudioDevicesChanged(cached_device_info->GetAudioDeviceInfos());
   }
 
   preceding_separator.GetProperty(views::kMarginsKey)->set_bottom(0);
