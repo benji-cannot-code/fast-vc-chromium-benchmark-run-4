fae576bb627b03a749407dd14644ee7cc528dea1
Mark Schillaci <mschillaci@google.com>
2024-03-07 19:21:57 +0000

    [AssistData] Construct BrowserAccessibilityManagerAndroid for snapshot
    This CL continues the work of unifying the snapshot code paths. We add
    part of the method to process a completed AXTreeUpdate, reusing some of
    the methods that exist for paint preview flow.
    
    AX-Relnotes: N/A
    Bug: 325665399
    Change-Id: I3c4b2bf83a39f0256dcdade37d0e241616c445e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5331172
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1269742}

--- content/browser/accessibility/web_contents_accessibility_android.cc
+++ content/browser/accessibility/web_contents_accessibility_android.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unordered_map>
 #include <vector>
 
+#include "base/android/callback_android.h"
 #include "base/android/jni_android.h"
 #include "base/android/jni_array.h"
 #include "base/android/jni_string.h"
@@ -234,11 +235,6 @@ WebContentsAccessibilityAndroid::WebContentsAccessibilityAndroid(
       web_contents_(static_cast<WebContentsImpl*>(web_contents)) {
   // A Connector is not required for a simple snapshot.
   connector_ = nullptr;
-
-  // Stubbed method.
-  if (!jassist_data_builder.is_null()) {
-    Java_AssistDataBuilder_stubbed(env, jassist_data_builder);
-  }
 }
 
 WebContentsAccessibilityAndroid::~WebContentsAccessibilityAndroid() {
@@ -1726,7 +1722,7 @@ void WebContentsAccessibilityAndroid::RequestAccessibilityTreeSnapshot(
   auto combiner = base::MakeRefCounted<AccessibilityTreeSnapshotCombiner>(
       base::BindOnce(&WebContentsAccessibilityAndroid::
                          ProcessCompletedAccessibilityTreeSnapshot,
-                     GetWeakPtr()),
+                     GetWeakPtr(), env),
       std::move(params));
   web_contents_->GetPrimaryMainFrame()->ForEachRenderFrameHost(
       [&combiner](RenderFrameHostImpl* rfhi) {
@@ -1735,8 +1731,33 @@ void WebContentsAccessibilityAndroid::RequestAccessibilityTreeSnapshot(
 }
 
 void WebContentsAccessibilityAndroid::ProcessCompletedAccessibilityTreeSnapshot(
+    JNIEnv* env,
     const ui::AXTreeUpdate& result) {
+  // If we don't have a connection back to the Java-side objects, then fail.
+  ScopedJavaLocalRef<jobject> obj = java_adb_ref_.get(env);
+  CHECK(obj);
+
+  // Construct a root manager without a delegate if one does not already exist.
+  // In some situations (e.g. unit tests) the full accessibility engine may
+  // already be running and we do not need to create a new manager.
+  if (!GetRootBrowserAccessibilityManager()) {
+    snapshot_root_manager_ =
+        std::make_unique<BrowserAccessibilityManagerAndroid>(
+            result, GetWeakPtr(), /* delegate= */ nullptr);
+  }
+
+  // Construct the Java-side tree, use the JNI builder `java_adb_ref_` to
+  // recursively construct each node of the tree starting with the provided
+  // root. Stubbed.
+  Java_AssistDataBuilder_stubbed(env, obj);
+
+  // Add tree-level data to Java-side tree.
   // Stubbed.
+
+  // We have fulfilled the request for an accessibility tree snapshot, so we can
+  // now call the provided Java-side callback to inform original client that the
+  // async construction is complete.
+  base::android::RunRunnableAndroid(on_done_callback_);
 }
 
 base::WeakPtr<WebContentsAccessibilityAndroid>
