a25e6614d75e6d5fc89333dd9745b85c4c0cffd2
Michael Ershov <miersh@google.com>
2024-03-07 13:48:13 +0000

    Revert "Convert highlight tests to Mocha+TS"
    This reverts commit b0dadd8de07c9ba16f2353d2457697241d601b52.
    
    Reason for revert: 
    PersistentBackground/ExtensionPageCaptureApiTest.SaveAsMHTMLWithFileAccess/0
    PersistentBackground/ExtensionPageCaptureApiTest.SaveAsMHTMLWithoutFileAccess/0
    tests started failing after this CL.
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Tests
    
    Original change's description:
    > Convert highlight tests to Mocha+TS
    >
    > Bug: 323960128
    > Change-Id: Ie51080fdb9037f98df24278b16bfd4de5f325928
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5350879
    > Commit-Queue: Kristi Saney <kristislee@google.com>
    > Reviewed-by: Lauren Winston <lwinston@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1269333}
    
    Bug: 323960128
    Change-Id: I7d0f1f6a7955b64641737aa0a7ab0bee7bd8bf1b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5352166
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Michael Ershov <miersh@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1269543}

--- chrome/test/data/webui/side_panel/read_anything/read_anything_browsertest.cc
+++ chrome/test/data/webui/side_panel/read_anything/read_anything_browsertest.cc
@@ -78,11 +78,6 @@ IN_PROC_BROWSER_TEST_F(ReadAnythingMochaTest, HighlightToggle) {
                    "mocha.run()");
 }
 
-IN_PROC_BROWSER_TEST_F(ReadAnythingMochaTest, NextPrevious) {
-  RunSidePanelTest("side_panel/read_anything/next_previous_granularity_test.js",
-                   "mocha.run()");
-}
-
 IN_PROC_BROWSER_TEST_F(ReadAnythingMochaTest, RateSelection) {
   RunSidePanelTest("side_panel/read_anything/rate_selection_test.js",
                    "mocha.run()");
@@ -121,9 +116,4 @@ IN_PROC_BROWSER_TEST_F(ReadAnythingReadAloudMochaTest,
   RunSidePanelTest(
       "side_panel/read_anything/read_aloud_update_content_selection.js",
       "mocha.run()");
-}
-
-IN_PROC_BROWSER_TEST_F(ReadAnythingReadAloudMochaTest, ReadAloudHighlight) {
-  RunSidePanelTest("side_panel/read_anything/read_aloud_highlighting_test.js",
-                   "mocha.run()");
 }
