9da1def91718a5fe0d638cd55963dcfb86a97a31
Evan Stade <estade@chromium.org>
2024-03-07 00:09:44 +0000

    IDB: shard backing stores across threads, part 3
    In this part, each bucket context/backing store is given its own task
    runner from the ThreadPool when the flag is enabled.
    
    The approach that attempts to keep the IndexedDBBucketContext owned
    by a unique_ptr is abandoned as it became too difficult to construct,
    operate, and delete this object on one task runner, while holding
    ownership on a different task runner (the main IDB task runner). Thus
    this CL creates a second container for holding the bucket contexts
    which holds them as `SequenceBound`, and which container is used
    depends on the state of the feature flag. This leads to some
    unfortunate code duplication but that should go away when the
    functionality is enabled by default. Also this approach will make
    removal of the old code simpler as it only involves deleting dead
    paths and not rewriting live paths.
    
    On testing: browser tests run in an environment that closely matches
    production, including operating the backing stores on a separate task
    runner when the flag is enabled. Unit tests that are easy to update
    also run against multiple task runners as appropriate. However, some
    unit tests disable the background task runners because the tests
    themselves reach into bucket-sequence state from the main IDB sequence.
    Updating them to work in a multi-threaded environment would be very
    painful due to:
    
    * the quantity of tests
    * the timing assumptions they make (e.g. state that would change if
      checked asynchronously)
    
    Bug: 40279485
    
    Change-Id: Ia62f7cbe63ccc3635ec0683154977d79f51c1824
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5341944
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269308}

--- content/browser/indexed_db/indexed_db_bucket_context.cc
+++ content/browser/indexed_db/indexed_db_bucket_context.cc
@@ -1033,6 +1033,11 @@ void IndexedDBBucketContext::FillInMetadata(
   std::move(result).Run(std::move(info));
 }
 
+IndexedDBBucketContext* IndexedDBBucketContext::GetReferenceForTesting() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  return this;
+}
+
 void IndexedDBBucketContext::CompactBackingStoreForTesting() {
   // Compact the first db's backing store since all the db's are in the same
   // backing store.
@@ -1048,14 +1053,12 @@ void IndexedDBBucketContext::CompactBackingStoreForTesting() {
 
 void IndexedDBBucketContext::WriteToIndexedDBForTesting(
     const std::string& key,
-    const std::string& value,
-    base::OnceClosure callback) {
+    const std::string& value) {
   TransactionalLevelDBDatabase* db = backing_store_->db();
   std::string value_copy = value;
   leveldb::Status s = db->Put(key, &value_copy);
   CHECK(s.ok()) << s.ToString();
   ForceClose(true);
-  std::move(callback).Run();
 }
 
 void IndexedDBBucketContext::BindMockFailureSingletonForTesting(
