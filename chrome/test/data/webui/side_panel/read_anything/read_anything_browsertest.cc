871531a89b3b4e8241cafb038edc6701482ae96d
Kristi Saney <kristislee@google.com>
2024-03-07 00:42:51 +0000

    Convert highlight tests to Mocha+TS
    Bug: 323960128
    Change-Id: Ie51080fdb9037f98df24278b16bfd4de5f325928
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5350879
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1269333}

--- chrome/test/data/webui/side_panel/read_anything/read_anything_browsertest.cc
+++ chrome/test/data/webui/side_panel/read_anything/read_anything_browsertest.cc
@@ -78,6 +78,11 @@ IN_PROC_BROWSER_TEST_F(ReadAnythingMochaTest, HighlightToggle) {
                    "mocha.run()");
 }
 
+IN_PROC_BROWSER_TEST_F(ReadAnythingMochaTest, NextPrevious) {
+  RunSidePanelTest("side_panel/read_anything/next_previous_granularity_test.js",
+                   "mocha.run()");
+}
+
 IN_PROC_BROWSER_TEST_F(ReadAnythingMochaTest, RateSelection) {
   RunSidePanelTest("side_panel/read_anything/rate_selection_test.js",
                    "mocha.run()");
@@ -116,4 +121,9 @@ IN_PROC_BROWSER_TEST_F(ReadAnythingReadAloudMochaTest,
   RunSidePanelTest(
       "side_panel/read_anything/read_aloud_update_content_selection.js",
       "mocha.run()");
+}
+
+IN_PROC_BROWSER_TEST_F(ReadAnythingReadAloudMochaTest, ReadAloudHighlight) {
+  RunSidePanelTest("side_panel/read_anything/read_aloud_highlighting_test.js",
+                   "mocha.run()");
 }
