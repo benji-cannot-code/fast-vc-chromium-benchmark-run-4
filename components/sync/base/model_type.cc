430c4d7e559752d959098c5cbf1f7d3653818dae
Florian Leimgruber <fleimgruber@google.com>
2024-03-07 09:55:41 +0000

    Add PLUS_ADDRESS model type
    The model type is _not_ tied to a UserSelectableType yet, since PMs are
    still discussing how opt-out should be handled. Currently, they are
    leaning towards hiding PLUS_ADDRESS in
    chrome://settings/syncSetup/advanced and having a separate, global opt-
    out option in PLUS_ADDRESS settings.
    
    Bug: b/322147254
    Change-Id: Ib58452ba615a571d70304785515c4b327b52fc0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5340177
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1269476}

--- components/sync/base/model_type.cc
+++ components/sync/base/model_type.cc
@@ -208,6 +208,9 @@ const ModelTypeInfo kModelTypeInfoMap[] = {
      "Collaboration Group",
      sync_pb::EntitySpecifics::kCollaborationGroupFieldNumber,
      ModelTypeForHistograms::kCollaborationGroup},
+    {PLUS_ADDRESS, "PLUS_ADDRESS", "plus_address", "Plus Address",
+     sync_pb::EntitySpecifics::kPlusAddressFieldNumber,
+     ModelTypeForHistograms::kPlusAddresses},
     // ---- Control Types ----
     {NIGORI, "NIGORI", "nigori", "Encryption Keys",
      sync_pb::EntitySpecifics::kNigoriFieldNumber,
@@ -217,7 +220,7 @@ const ModelTypeInfo kModelTypeInfoMap[] = {
 static_assert(std::size(kModelTypeInfoMap) == GetNumModelTypes(),
               "kModelTypeInfoMap should have GetNumModelTypes() elements");
 
-static_assert(50 == syncer::GetNumModelTypes(),
+static_assert(51 == syncer::GetNumModelTypes(),
               "When adding a new type, update enum SyncModelTypes in enums.xml "
               "and suffix SyncModelType in histograms.xml.");
 
@@ -299,6 +302,7 @@ constexpr kSpecificsFieldNumberToModelTypeMap
          SHARED_TAB_GROUP_DATA},
         {sync_pb::EntitySpecifics::kCollaborationGroupFieldNumber,
          COLLABORATION_GROUP},
+        {sync_pb::EntitySpecifics::kPlusAddressFieldNumber, PLUS_ADDRESS},
         // ---- Control Types ----
         {sync_pb::EntitySpecifics::kNigoriFieldNumber, NIGORI},
     });
@@ -458,6 +462,9 @@ void AddDefaultFieldValue(ModelType type, sync_pb::EntitySpecifics* specifics) {
     case COLLABORATION_GROUP:
       specifics->mutable_collaboration_group();
       break;
+    case PLUS_ADDRESS:
+      specifics->mutable_plus_address();
+      break;
   }
 }
 
@@ -486,7 +493,7 @@ void internal::GetModelTypeSetFromSpecificsFieldNumberListHelper(
 }
 
 ModelType GetModelTypeFromSpecifics(const sync_pb::EntitySpecifics& specifics) {
-  static_assert(50 == syncer::GetNumModelTypes(),
+  static_assert(51 == syncer::GetNumModelTypes(),
                 "When adding new protocol types, the following type lookup "
                 "logic must be updated.");
   if (specifics.has_bookmark())
@@ -594,6 +601,9 @@ ModelType GetModelTypeFromSpecifics(const sync_pb::EntitySpecifics& specifics) {
   if (specifics.has_collaboration_group()) {
     return COLLABORATION_GROUP;
   }
+  if (specifics.has_plus_address()) {
+    return PLUS_ADDRESS;
+  }
 
   // This client version doesn't understand |specifics|.
   DVLOG(1) << "Unknown datatype in sync proto.";
@@ -601,7 +611,7 @@ ModelType GetModelTypeFromSpecifics(const sync_pb::EntitySpecifics& specifics) {
 }
 
 ModelTypeSet EncryptableUserTypes() {
-  static_assert(50 == syncer::GetNumModelTypes(),
+  static_assert(51 == syncer::GetNumModelTypes(),
                 "If adding an unencryptable type, remove from "
                 "encryptable_user_types below.");
   ModelTypeSet encryptable_user_types = UserTypes();
@@ -630,6 +640,9 @@ ModelTypeSet EncryptableUserTypes() {
   encryptable_user_types.Remove(OUTGOING_PASSWORD_SHARING_INVITATION);
   // Never encrypted because consumed server-side.
   encryptable_user_types.Remove(SHARED_TAB_GROUP_DATA);
+  // Plus addresses are never encrypted because the originate from outside
+  // Chrome.
+  encryptable_user_types.Remove(PLUS_ADDRESS);
 
   return encryptable_user_types;
 }
--- components/sync/engine/cycle/data_type_tracker.cc
+++ components/sync/engine/cycle/data_type_tracker.cc
@@ -112,6 +112,7 @@ base::TimeDelta GetDefaultLocalChangeNudgeDelay(ModelType model_type) {
     case NIGORI:
     case POWER_BOOKMARK:
     case WEBAUTHN_CREDENTIAL:
+    case PLUS_ADDRESS:
       return kMediumLocalChangeNudgeDelay;
     case UNSPECIFIED:
       NOTREACHED();
@@ -177,6 +178,7 @@ bool CanGetCommitsFromExtensions(ModelType model_type) {
     case OUTGOING_PASSWORD_SHARING_INVITATION:
     case SHARED_TAB_GROUP_DATA:
     case COLLABORATION_GROUP:
+    case PLUS_ADDRESS:
       return false;
     case UNSPECIFIED:
       NOTREACHED();
