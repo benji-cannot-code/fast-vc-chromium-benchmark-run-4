8de65cc7d7ccbcaa24f538a42160248a7ea89c8b
Quentin Pubert <qpubert@google.com>
2024-03-07 09:21:19 +0000

    [iOS] Rename TabStripCell to TabStripTabCell
    This CL renames TabStripCell to TabStripTabCell since it represents a
    tab. TabStripGroupCell will represent a group header and TabStripCell
    will become a parent class for both of these.
    
    Bug: 328395603
    Change-Id: Ic2df6a81134a889be0c974ea113eea7554457360
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5348784
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269462}

--- ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_layout.swift
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_layout.swift
@@ -195,7 +195,7 @@ class TabStripLayout: UICollectionViewFlowLayout {
     }
 
     guard
-      let cell = collectionView.cellForItem(at: indexPath) as? TabStripCell
+      let cell = collectionView.cellForItem(at: indexPath) as? TabStripTabCell
     else { return layoutAttributes }
 
     let contentOffset = collectionView.contentOffset
@@ -369,7 +369,7 @@ class TabStripLayout: UICollectionViewFlowLayout {
 
     /// `cellAnimatediOS16` is always `false` above iOS 16.
     var cellAnimated = cellAnimatediOS16
-    let cell = collectionView.cellForItem(at: indexPath) as? TabStripCell
+    let cell = collectionView.cellForItem(at: indexPath) as? TabStripTabCell
     if let animationKeys = cell?.layer.animationKeys() {
       cellAnimated = !animationKeys.isEmpty || cellAnimatediOS16
     }
--- ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_view_controller.swift
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_view_controller.swift
@@ -8,7 +8,7 @@ import ios_chrome_browser_shared_ui_util_util_swift
 
 /// View Controller displaying the TabStrip.
 @objcMembers
-class TabStripViewController: UIViewController, TabStripCellDelegate,
+class TabStripViewController: UIViewController, TabStripTabCellDelegate,
   TabStripConsumer, TabStripCommands, TabStripNewTabButtonDelegate
 {
 
@@ -22,7 +22,8 @@ class TabStripViewController: UIViewController, TabStripCellDelegate,
   private let collectionView: UICollectionView
   // The DataSource for this collection view.
   private var diffableDataSource: UICollectionViewDiffableDataSource<Section, TabSwitcherItem>?
-  private var tabCellRegistration: UICollectionView.CellRegistration<TabStripCell, TabSwitcherItem>?
+  private var tabCellRegistration:
+    UICollectionView.CellRegistration<TabStripTabCell, TabSwitcherItem>?
 
   // The New tab button.
   private let newTabButton: TabStripNewTabButton = TabStripNewTabButton()
@@ -281,9 +282,9 @@ class TabStripViewController: UIViewController, TabStripCellDelegate,
     newTabButton.IPHHighlighted = IPHHighlighted
   }
 
-  // MARK: - TabStripCellDelegate
+  // MARK: - TabStripTabCellDelegate
 
-  func closeButtonTapped(for cell: TabStripCell?) {
+  func closeButtonTapped(for cell: TabStripTabCell?) {
     guard let cell = cell, let diffableDataSource = diffableDataSource else {
       return
     }
@@ -322,7 +323,7 @@ class TabStripViewController: UIViewController, TabStripCellDelegate,
 
   /// Creates the registrations of the different cells used in the collection view.
   private func createRegistrations() {
-    tabCellRegistration = UICollectionView.CellRegistration<TabStripCell, TabSwitcherItem> {
+    tabCellRegistration = UICollectionView.CellRegistration<TabStripTabCell, TabSwitcherItem> {
       (cell, indexPath, item) in
       cell.setTitle(item.title)
       cell.loading = item.showsActivity
@@ -345,10 +346,10 @@ class TabStripViewController: UIViewController, TabStripCellDelegate,
       }
     }
 
-    // UICollectionViewDropPlaceholder uses a TabStripCell and needs the class to be
+    // UICollectionViewDropPlaceholder uses a TabStripTabCell and needs the class to be
     // registered.
     collectionView.register(
-      TabStripCell.self,
+      TabStripTabCell.self,
       forCellWithReuseIdentifier: TabStripConstants.CollectionView.tabStripCellReuseIdentifier)
   }
 
@@ -406,7 +407,7 @@ class TabStripViewController: UIViewController, TabStripCellDelegate,
     }
   }
 
-  // Returns the accessibility identifier to set on a TabStripCell when
+  // Returns the accessibility identifier to set on a TabStripTabCell when
   // positioned at the given index.
   func tabTripCellAccessibilityIdentifier(index: Int) -> String {
     return String(
@@ -494,7 +495,7 @@ extension TabStripViewController: UICollectionViewDelegateFlowLayout {
     contextMenuConfiguration configuration: UIContextMenuConfiguration,
     highlightPreviewForItemAt indexPath: IndexPath
   ) -> UITargetedPreview? {
-    guard let cell = collectionView.cellForItem(at: indexPath) as? TabStripCell else {
+    guard let cell = collectionView.cellForItem(at: indexPath) as? TabStripTabCell else {
       return nil
     }
     return UITargetedPreview(view: cell, parameters: cell.dragPreviewParameters)
@@ -583,7 +584,7 @@ extension TabStripViewController: UICollectionViewDragDelegate, UICollectionView
     _ collectionView: UICollectionView,
     dragPreviewParametersForItemAt indexPath: IndexPath
   ) -> UIDragPreviewParameters? {
-    guard let cell = collectionView.cellForItem(at: indexPath) as? TabStripCell else {
+    guard let cell = collectionView.cellForItem(at: indexPath) as? TabStripTabCell else {
       return nil
     }
     return cell.dragPreviewParameters
@@ -667,7 +668,7 @@ extension TabStripViewController: UICollectionViewDragDelegate, UICollectionView
           reuseIdentifier: TabStripConstants.CollectionView.tabStripCellReuseIdentifier)
         placeholder.previewParametersProvider = {
           (placeholderCell: UICollectionViewCell) -> UIDragPreviewParameters? in
-          guard let tabStripCell = placeholderCell as? TabStripCell else {
+          guard let tabStripCell = placeholderCell as? TabStripTabCell else {
             return nil
           }
           return tabStripCell.dragPreviewParameters
