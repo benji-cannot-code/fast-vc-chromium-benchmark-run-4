f4e56ceef8c6673c87cc96441e92fb18752362c1
Nicolas Dossou-Gbete <dgn@chromium.org>
2024-03-07 16:40:42 +0000

    🧇 Enable manual triggering of the choice screen on non-branded builds
    The existing `--force-search-engine-choice-screen` can now also be used
    on non-branded builds, allowing them to trigger the choice screen there.
    
    Does not guarantee that all assets on the choice screen will reflect
    what is available in branded builds. These should still be used to more
    accurately test rendering and triggering.
    
    Suppressing the screenshots because there are no new strings, we
    already have them for Chrome branded builds.
    
    Fixed: 317049138
    Skip-Translation-Screenshots-Check: True
    Change-Id: Ia44e84d3ef609eaf2ec3b3155d286e374fac945d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5345990
    Reviewed-by: Jack Yammine <jyammine@google.com>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269623}

--- chrome/browser/search_engine_choice/search_engine_choice_dialog_service_factory.cc
+++ chrome/browser/search_engine_choice/search_engine_choice_dialog_service_factory.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_deref.h"
 #include "base/check_is_test.h"
+#include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "build/branding_buildflags.h"
 #include "chrome/browser/browser_process.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_engines/search_engine_choice/search_engine_choice_service.h"
 #include "components/search_engines/search_engine_choice_utils.h"
 #include "components/search_engines/search_engines_pref_names.h"
+#include "components/search_engines/search_engines_switches.h"
 #include "components/search_engines/template_url_service.h"
 
 #if BUILDFLAG(IS_CHROMEOS)
@@ -131,7 +133,8 @@ SearchEngineChoiceDialogServiceFactory::BuildServiceInstanceForBrowserContext(
     BUILDFLAG(CHROME_FOR_TESTING)
   return nullptr;
 #else
-  if (!g_is_chrome_build) {
+  if (!g_is_chrome_build && !base::CommandLine::ForCurrentProcess()->HasSwitch(
+                                switches::kForceSearchEngineChoiceScreen)) {
     return nullptr;
   }
 
