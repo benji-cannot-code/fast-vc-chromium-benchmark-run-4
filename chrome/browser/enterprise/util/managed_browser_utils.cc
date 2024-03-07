3121d7d919cd43e9f372d18791210686b2f63e3f
Yann Dago <ydago@chromium.org>
2024-03-07 15:50:59 +0000

    Add possibility to know if an account is a edu account
    Bug: b:323037330, b:322796016
    Change-Id: I9830f8d15fa22edd723cdb0feb1654545bec4d2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5329267
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269594}

--- chrome/browser/enterprise/util/managed_browser_utils.cc
+++ chrome/browser/enterprise/util/managed_browser_utils.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/pref_names.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
+#include "components/signin/public/identity_manager/account_info.h"
 #include "google_apis/gaia/gaia_auth_util.h"
 #include "net/base/host_port_pair.h"
 #include "net/cert/x509_certificate.h"
@@ -198,8 +199,8 @@ ManagementEnvironment GetManagementEnvironment(
   if (!UserAcceptedAccountManagement(profile)) {
     return ManagementEnvironment::kNone;
   }
-  // TODO (b/322796016): Add check for school using account_info
-  return ManagementEnvironment::kWork;
+  return account_info.IsEduAccount() ? ManagementEnvironment::kSchool
+                                     : ManagementEnvironment::kWork;
 }
 
 bool IsKnownConsumerDomain(const std::string& email_domain) {
