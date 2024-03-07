fe2adc7adc93e7eff4524e770b86850d3ea874a8
Guido Urdaneta <guidou@chromium.org>
2024-03-07 17:55:38 +0000

    Revert "Implement constructor in RTCEncodedVideoFrame"
    This reverts commit edd81f4f02dd5ebb365f0e7eceb42780a881920f.
    
    Reason for revert: see crbug.com/328441133
    
    Original change's description:
    > Implement constructor in RTCEncodedVideoFrame
    >
    > This is inline with the algorithm described in https://github.com/w3c/webrtc-encoded-transform/compare/main...palak8669:webrtc-encoded-transform:constructor_frame
    >
    > This is a reland of https://chromium-review.googlesource.com/c/chromium/src/+/5295578 which got reverted due
    > to wrong include rules.
    >
    > Bug: 14709
    > Change-Id: I5da20d10295908b2592c3c7202aa56fd206bf908
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5307134
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Commit-Queue: Palak Agarwal <agpalak@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1268408}
    
    Bug: 14709
    Change-Id: Id44ba189fd1f9c1af38d85bcfd63245b1809d1de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5352949
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269678}

--- third_party/blink/renderer/modules/peerconnection/rtc_encoded_video_frame_delegate.h
+++ third_party/blink/renderer/modules/peerconnection/rtc_encoded_video_frame_delegate.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class DOMArrayBuffer;
+class ExceptionState;
 
 // This class wraps a WebRTC video frame and allows making shallow
 // copies. Its purpose is to support making RTCEncodedVideoFrames
@@ -33,15 +34,14 @@ class RTCEncodedVideoFrameDelegate
 
   String Type() const;
   uint32_t RtpTimestamp() const;
-  bool SetRtpTimestamp(uint32_t timestamp, String& error_message);
+  void SetRtpTimestamp(uint32_t timestamp, ExceptionState& exception_state);
   std::optional<webrtc::Timestamp> PresentationTimestamp() const;
   DOMArrayBuffer* CreateDataBuffer() const;
   void SetData(const DOMArrayBuffer* data);
   std::optional<uint8_t> PayloadType() const;
   std::optional<std::string> MimeType() const;
   std::optional<webrtc::VideoFrameMetadata> GetMetadata() const;
-  bool SetMetadata(const webrtc::VideoFrameMetadata& metadata,
-                   String& error_message);
+  void SetMetadata(const webrtc::VideoFrameMetadata& metadata);
   std::unique_ptr<webrtc::TransformableVideoFrameInterface> PassWebRtcFrame();
   std::unique_ptr<webrtc::TransformableVideoFrameInterface> CloneWebRtcFrame();
 
