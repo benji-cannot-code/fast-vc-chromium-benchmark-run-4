b5824081d1db71ef036f80eb5ccb9f371a444e28
Luciano Pacheco <lucmult@chromium.org>
2024-03-07 01:10:16 +0000

    Files app: Change from Entry to FileData in the new DirectoryTree #3
    Change the ActionsProducer
    readSubDirectoriesToCheckDirectoryChildrenInternal() to be based on
    the FileData instead of Entry.
    
    Bug: b:327534506
    Change-Id: Iaca4866a64cd7e439e19b30cec863d244da30a65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5347524
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269344}

--- ui/file_manager/file_manager/containers/directory_tree_container.ts
+++ ui/file_manager/file_manager/containers/directory_tree_container.ts
@@ -616,8 +616,8 @@ export class DirectoryTreeContainer {
       shouldCheckDirectoryChildren = !!(element.parentItem?.expanded);
     }
     if (shouldCheckDirectoryChildren) {
-      const entry = fileData.entry;
-      this.store_.dispatch(readSubDirectoriesToCheckDirectoryChildren(entry));
+      this.store_.dispatch(
+          readSubDirectoriesToCheckDirectoryChildren(fileData.key));
     }
   }
 
@@ -967,7 +967,7 @@ export class DirectoryTreeContainer {
     if (shouldDelayLoadingChildren(fileData, this.store_.getState()) &&
         fileData.children.length === 0) {
       this.store_.dispatch(
-          readSubDirectoriesToCheckDirectoryChildren(fileData.entry));
+          readSubDirectoriesToCheckDirectoryChildren(fileData.key));
     }
 
     this.store_.dispatch(changeDirectory({toKey: fileKey}));
