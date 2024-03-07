5651e6161ed9899a73ba6ea9064b998f9e5a9618
Greg Thompson <grt@chromium.org>
2024-03-07 15:05:40 +0000

    [accessibility] Update comments on some disabled tests
    The previous bout of flakiness in the
    PDFExtensionAccessibilityTestWithOopifOverride tests was resolved in
    https://crrev.com/1268318, but a new regression snuck in with
    https://crrev.com/1264786. Update the comments accordingly.
    
    Bug: 327458821
    Change-Id: Ib9ef6a0bad5cd48467e73bbd8d4667a848ad57d8
    Fixed: 40928791
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5351827
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269567}

--- chrome/browser/pdf/pdf_extension_accessibility_test.cc
+++ chrome/browser/pdf/pdf_extension_accessibility_test.cc
@@ -325,7 +325,7 @@ IN_PROC_BROWSER_TEST_P(PDFExtensionAccessibilityTestWithOopifOverride,
   ASSERT_MULTILINE_STREQ(kExpectedPDFAXTree, ax_tree_dump);
 }
 
-// Flaky, see crbug.com/1477361
+// Fails; see https://crbug.com/327458821.
 IN_PROC_BROWSER_TEST_P(PDFExtensionAccessibilityTestWithOopifOverride,
                        DISABLED_PdfAccessibilityEnableLater) {
   // In this test, load the PDF file first, with accessibility off.
@@ -418,7 +418,7 @@ IN_PROC_BROWSER_TEST_P(PDFExtensionAccessibilityTestWithOopifOverride,
   ASSERT_TRUE(found);
 }
 
-// Flaky, see crbug.com/1477361
+// Fails; see https://crbug.com/327458821.
 IN_PROC_BROWSER_TEST_P(PDFExtensionAccessibilityTestWithOopifOverride,
                        DISABLED_PdfAccessibilitySelection) {
   // TODO(crbug.com/324636880): Remove this once the test passes for OOPIF PDF.
@@ -471,7 +471,7 @@ IN_PROC_BROWSER_TEST_P(PDFExtensionAccessibilityTestWithOopifOverride,
   EXPECT_EQ(ax::mojom::Role::kRegion, region->GetRole());
 }
 
-// Flaky, see crbug.com/1477361
+// Fails; see https://crbug.com/327458821.
 IN_PROC_BROWSER_TEST_P(PDFExtensionAccessibilityTestWithOopifOverride,
                        DISABLED_PdfAccessibilityContextMenuAction) {
   // TODO(crbug.com/324636880): Remove this once the test passes for OOPIF PDF.
