e1bc496ecd91621f03f94dbe0b745cb17c7404b3
Daniel Rubery <drubery@chromium.org>
2024-03-07 18:25:13 +0000

    Remove LiveScorersAtCreation and LocalModelDetectsPhishing
    Neither of these has shown an alarmingly high number of instances.
    
    OBSOLETE_HISTOGRAMS=Added to investigate https://crbug.com/1515348, but did not show any unusual results.
    
    Fixed: 325945894, 1515348
    Change-Id: Ie501acdbaf66bede40d342ee83ee51a65435f00c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5347374
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Seung Jae Lim <andysjlim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269695}

--- components/safe_browsing/content/browser/client_side_phishing_model.cc
+++ components/safe_browsing/content/browser/client_side_phishing_model.cc
@@ -154,11 +154,6 @@ void CloseModelFile(base::File model_file) {
   model_file.Close();
 }
 
-int* GetLiveClientPhishingModelCount() {
-  static int count = 0;
-  return &count;
-}
-
 }  // namespace
 
 // --- ClientSidePhishingModel methods ---
@@ -172,10 +167,6 @@ ClientSidePhishingModel::ClientSidePhishingModel(
   opt_guide_->AddObserverForOptimizationTargetModel(
       optimization_guide::proto::OPTIMIZATION_TARGET_CLIENT_SIDE_PHISHING,
       /*model_metadata=*/std::nullopt, this);
-  *GetLiveClientPhishingModelCount() += 1;
-  base::UmaHistogramCounts1000(
-      "SBClientPhishing.LiveClientPhishingModelCountAtCreation",
-      *GetLiveClientPhishingModelCount());
 }
 
 void ClientSidePhishingModel::OnModelUpdated(
@@ -443,8 +434,6 @@ ClientSidePhishingModel::~ClientSidePhishingModel() {
   }
 
   opt_guide_ = nullptr;
-
-  *GetLiveClientPhishingModelCount() -= 1;
 }
 
 base::CallbackListSubscription ClientSidePhishingModel::RegisterCallback(
