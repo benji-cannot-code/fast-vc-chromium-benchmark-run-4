90a250370c961e616ed86de81369136fb84b359b
Norge Vizcay <vizcay@google.com>
2024-03-07 10:22:29 +0000

    Introduce affiliation source class and password store adapter.
    The CL is the first step towards generalizing the affiliation prefetch
    logic into a generic fetcher and multiple affiliation sources. It
    introduces the AffiliationSource interface and the first few bits of
    the password store source adapter.
    
    Bug: b/328037758
    Change-Id: I2341fde6256033c8c92abdcc7c8ef9a09c9ea60f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5340141
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1269490}

---
+++ components/affiliations/core/browser/mock_affiliation_source.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/affiliations/core/browser/mock_affiliation_source.h"
+
+namespace affiliations {
+
+MockAffiliationSource::MockAffiliationSource() = default;
+
+MockAffiliationSource::~MockAffiliationSource() = default;
+
+MockAffiliationSourceObserver::MockAffiliationSourceObserver() = default;
+
+MockAffiliationSourceObserver::~MockAffiliationSourceObserver() = default;
+
+}  // namespace affiliations
