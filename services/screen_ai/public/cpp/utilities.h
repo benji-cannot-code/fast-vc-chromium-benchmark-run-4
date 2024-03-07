61ebb3a59f90d50c1f36d542be12eb51fae9bb5f
Ramin Halavati <rhalavati@chromium.org>
2024-03-07 16:12:33 +0000

    Add UMA for ChromeScreenAI missing binary after install.
    ChromeScreenAI's binary can be unavailable after install, due to being
    locked by malware protection or similar issues. An UMA is added to
    measure that and DumpWithoutCrashing is limited to cases that the
    installed path is unexpected.
    
    AX-Relnotes: n/a
    Bug: 328047700
    Change-Id: Ib53765bd5ba43d982c331e2b9cf3c00667c257b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5340218
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269607}

--- services/screen_ai/public/cpp/utilities.h
+++ services/screen_ai/public/cpp/utilities.h
@@ -10,7 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace screen_ai {
 
-// Get the absolute path of the ScreenAI component.
+// Returns the absolute path of the highest-versioned component directory.
+base::FilePath GetLatestComponentPath();
+
+// Get the absolute path of the ScreenAI component. This function verifies that
+// the binary exists on disk and can be opened. It may return empty if some
+// other piece of software has opened the binary file with restrictive
+// permissions (e.g., "security" software or malware protection).
 base::FilePath GetLatestComponentBinaryPath();
 
 // Returns the install directory relative to components folder.
