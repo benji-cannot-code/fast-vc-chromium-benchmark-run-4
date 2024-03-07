f953741a832ca5ec787114d6cd47f15865fa461c
Arthur Eubanks <aeubanks@google.com>
2024-03-07 23:05:36 +0000

    Don't assume STL iterators are pointers
    This is batch 2 of a Chromium-wide cleanup preparing for a libc++ change that changes various iterators to not be raw pointers.
    
    MetadataWriter::AddOutputConfigForMultiClassClassifier() was changed to use base::span.
    
    The two parameter (D)CHECK_* variants try to stringify their parameters on failure which doesn't work with iterators.
    
    Bug: 328308661
    Change-Id: Icfdf41839991f335675627ae054d40ceb371299e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5353370
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Owners-Override: Nico Weber <thakis@chromium.org>
    Commit-Queue: Arthur Eubanks <aeubanks@google.com>
    Cr-Commit-Position: refs/heads/main@{#1269887}

--- components/segmentation_platform/internal/post_processor/post_processor_unittest.cc
+++ components/segmentation_platform/internal/post_processor/post_processor_unittest.cc
@@ -70,8 +70,8 @@ proto::OutputConfig GetTestOutputConfigForMultiClassClassifier(
       {kVoiceUser, kVoiceUserTTL},
       {kShoppingUser, kShoppingUserTTL},
   };
-  writer.AddOutputConfigForMultiClassClassifier(labels.begin(), labels.size(),
-                                                top_k_outputs, threshold);
+  writer.AddOutputConfigForMultiClassClassifier(labels, top_k_outputs,
+                                                threshold);
   writer.AddPredictedResultTTLInOutputConfig(ttl_for_labels, kDefaultTTL,
                                              proto::TimeUnit::DAY);
   return model_metadata.output_config();
@@ -79,8 +79,7 @@ proto::OutputConfig GetTestOutputConfigForMultiClassClassifier(
 
 proto::OutputConfig GetTestOutputConfigForMultiClassClassifier(
     int top_k_outputs,
-    const float* per_class_thresholds,
-    size_t per_class_thresholds_length) {
+    const base::span<float> per_class_thresholds) {
   proto::SegmentationModelMetadata model_metadata;
   MetadataWriter writer(&model_metadata);
 
@@ -92,9 +91,8 @@ proto::OutputConfig GetTestOutputConfigForMultiClassClassifier(
       {kVoiceUser, kVoiceUserTTL},
       {kShoppingUser, kShoppingUserTTL},
   };
-  writer.AddOutputConfigForMultiClassClassifier(
-      labels.begin(), labels.size(), top_k_outputs, per_class_thresholds,
-      per_class_thresholds_length);
+  writer.AddOutputConfigForMultiClassClassifier(labels, top_k_outputs,
+                                                per_class_thresholds);
   writer.AddPredictedResultTTLInOutputConfig(ttl_for_labels, kDefaultTTL,
                                              proto::TimeUnit::DAY);
   return model_metadata.output_config();
@@ -238,8 +236,7 @@ TEST(PostProcessorTest, MultiClassClassifierWithPerClassThresholds) {
           /*model_scores=*/{0.5, 0.2, 0.4, 0.7},
           GetTestOutputConfigForMultiClassClassifier(
               /*top_k-outputs=*/4,
-              /*per_class_thresholds = */ per_class_thresholds.begin(),
-              per_class_thresholds.size()),
+              /*per_class_thresholds=*/per_class_thresholds),
           /*timestamp=*/base::Time::Now(), /*model_version=*/1));
   // Return labels greater or equal than its threshold sorted by score.
   EXPECT_THAT(top_k_labels, testing::ElementsAre(kShareUser, kNewTabUser));
