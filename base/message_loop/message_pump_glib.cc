3628b987f31554a129ffc401a308500c3f9861a7
mikt <mikt@google.com>
2024-03-07 07:01:54 +0000

    [BRP] Triage dangling pointers in base/message_loop/message_pump_glib.cc
    `std::unique_ptr<FdWatchController> DeleteWatcher::owned_controller_`
    frees itself without clearing out `FdWatchSource::controller`.
    This CL clears it explicitly on `FdWatchController::~FdWatchController`.
    
    Bug: 40212619
    Change-Id: I0b9dda6c410519dbb58d35112e6f6cb61bbbce9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5347187
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Cr-Commit-Position: refs/heads/main@{#1269430}

--- base/message_loop/message_pump_glib.cc
+++ base/message_loop/message_pump_glib.cc
@@ -316,7 +316,7 @@ GSourceFuncs g_observer_funcs = {ObserverPrepare, ObserverCheck, nullptr,
 
 struct FdWatchSource : public GSource {
   raw_ptr<MessagePumpGlib> pump;
-  raw_ptr<MessagePumpGlib::FdWatchController, DanglingUntriaged> controller;
+  raw_ptr<MessagePumpGlib::FdWatchController> controller;
 };
 
 gboolean FdWatchSourcePrepare(GSource* source, gint* timeout_ms) {
@@ -435,6 +435,9 @@ MessagePumpGlib::FdWatchController::FdWatchController(const Location& location)
 
 MessagePumpGlib::FdWatchController::~FdWatchController() {
   if (IsInitialized()) {
+    auto* source = static_cast<FdWatchSource*>(source_);
+    source->controller = nullptr;
+
     CHECK(StopWatchingFileDescriptor());
   }
   if (was_destroyed_) {
