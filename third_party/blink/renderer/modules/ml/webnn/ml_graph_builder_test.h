365589d54844c27856b1b91e8e8be00b6a78cf22
Miao Bin <bin.miao@intel.com>
2024-03-07 11:34:21 +0000

    WebNN: Define mojo and add IDL definition for gru operator
    This CL defines mojo and adds the IDL definition for gru. It also
    implements `MLGraphBuilder::gru()` and adds the operator validation
    steps.
    
    This CL also adds unit test `MLGraphBuilderTest.GruTest`.
    
    Bug: 40206287
    Change-Id: I5fb7a27986e68dc17dfe74864063017e45bc69c7
    Cq-Include-Trybots: luci.chromium.try:win11-blink-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5252307
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Bin Miao <bin.miao@intel.com>
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1269510}

--- third_party/blink/renderer/modules/ml/webnn/ml_graph_builder_test.h
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_builder_test.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_elu_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_gather_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_gemm_options.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_ml_gru_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_hard_sigmoid_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_instance_normalization_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_layer_normalization_options.h"
--- third_party/blink/renderer/modules/ml/webnn/ml_graph_mojo_test.cc
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_mojo_test.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_context_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_triangular_options.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
+#include "third_party/blink/renderer/modules/ml/webnn/ml_activation.h"
 #include "third_party/blink/renderer/modules/ml/webnn/ml_graph_builder_test.h"
 #include "third_party/blink/renderer/modules/ml/webnn/ml_graph_test_base.h"
 #include "third_party/blink/renderer/modules/ml/webnn/ml_graph_type_converter.h"
