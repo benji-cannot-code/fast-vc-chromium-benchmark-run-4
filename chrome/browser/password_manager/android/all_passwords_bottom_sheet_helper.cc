056c5700729910baacb411dbf80471c5232466d0
Ivana Žužić <izuzic@google.com>
2024-03-07 09:46:34 +0000

    [UPMLocalPwd] AllPasswordsBottomSheetHelper checks stores for passwords
    The all passwords sheet was suggesting only the other passwords from the
    profile store. Now it checks for passwords in the account store too.
    
    This is planned to be merged back to M123.
    
    Bug: b/328373594
    Change-Id: I770a0299e9163f1528bcbfae02d603a8bfa78446
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5349122
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1269470}

--- chrome/browser/password_manager/android/all_passwords_bottom_sheet_helper.cc
+++ chrome/browser/password_manager/android/all_passwords_bottom_sheet_helper.cc
@@ -12,10 +12,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_store/password_store_interface.h"
 
 AllPasswordsBottomSheetHelper::AllPasswordsBottomSheetHelper(
-    password_manager::PasswordStoreInterface* store) {
-  DCHECK(store);
-  store->GetAllLoginsWithAffiliationAndBrandingInformation(
+    password_manager::PasswordStoreInterface* profile_store,
+    password_manager::PasswordStoreInterface* account_store) {
+  DCHECK(profile_store);
+  profile_store->GetAllLoginsWithAffiliationAndBrandingInformation(
       weak_ptr_factory_.GetWeakPtr());
+  if (account_store) {
+    account_store->GetAllLoginsWithAffiliationAndBrandingInformation(
+        weak_ptr_factory_.GetWeakPtr());
+  }
 }
 
 AllPasswordsBottomSheetHelper::~AllPasswordsBottomSheetHelper() = default;
@@ -37,8 +42,9 @@ void AllPasswordsBottomSheetHelper::ClearUpdateCallback() {
 
 void AllPasswordsBottomSheetHelper::OnGetPasswordStoreResults(
     std::vector<std::unique_ptr<password_manager::PasswordForm>> results) {
-  available_credentials_ = base::ranges::count_if(
+  int results_count = base::ranges::count_if(
       results, std::not_fn(&password_manager::PasswordForm::blocked_by_user));
+  available_credentials_ = available_credentials_.value_or(0) + results_count;
   if (available_credentials_.value() == 0)
     return;  // Don't update if sheet still wouldn't be available.
   if (update_callback_.is_null())
