465ec3cac18dfc84e1fcf1bde9b81803d3a45f29
Fredrik Söderquist <fs@opera.com>
2024-03-07 18:27:44 +0000

    Revalidate the interval when SMIL timing parameters change
    When one of the 'dur', 'repeatDur', 'repeatCount', 'min' or 'max'
    attributes changes the interval will not be revalidated. Since the state
    from these are "lazy" we will instead get strange effects when using
    timing state for other things.
    
    When any of the attributes above change, then tell the time container to
    reschedule the timed element and set a flag so that the interval will be
    revalidated when that happens.
    
    Fixed: 328451421
    Change-Id: Ia80becaf813e3c734c85ac85bfa483ce325f0786
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5352368
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269700}

--- third_party/blink/renderer/core/svg/animation/svg_smil_element.h
+++ third_party/blink/renderer/core/svg/animation/svg_smil_element.h
@@ -191,6 +191,7 @@ class CORE_EXPORT SVGSMILElement : public SVGElement, public SVGTests {
                        SMILTime time,
                        SMILTimeOrigin origin);
   void InstanceListChanged();
+  void IntervalStateChanged();
 
   // This represents conditions on elements begin or end list that need to be
   // resolved on runtime, for example
@@ -306,6 +307,7 @@ class CORE_EXPORT SVGSMILElement : public SVGElement, public SVGTests {
 
   bool interval_has_changed_;
   bool instance_lists_have_changed_;
+  bool interval_needs_revalidation_;
   bool is_notifying_dependents_;
 
   friend class ConditionEventListener;
