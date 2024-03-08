37428fe8bfbee310de6d05d1ed5824118bb4411f
Ashley Prasad <ashleydp@google.com>
2024-03-08 22:25:26 +0000

    cros-prevew: Add destination-select skeleton
    - Add skeleton element and controller for destination select.
    - Add TODO for localizing and display of loading UI.
    - Test element renders in UI and has controller.
    - Add TODO for temporarily placing destination-select directly in
      print-prevew-cros-app while building out destination behavior.
    
    Bug: b/323421684
    Test: browser_tests --gtest_filter=*PrintPreviewCros*
    Change-Id: Ib022038dca644567a84b2eae1e03c369d57d5e1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5354618
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270416}

---
+++ chrome/test/data/webui/chromeos/print_preview_cros/destination_select_controller_test.ts
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://os-print/js/destination_select_controller.js';
+
+import {DestinationSelectController} from 'chrome://os-print/js/destination_select_controller.js';
+import {assertTrue} from 'chrome://webui-test/chromeos/chai_assert.js';
+
+suite('DestinationSelectController', () => {
+  let controller: DestinationSelectController;
+
+  setup(() => {
+    controller = new DestinationSelectController();
+    assertTrue(!!controller);
+  });
+
+  // Verify controller is event target.
+  test('is event target', () => {
+    assertTrue(controller instanceof EventTarget, 'Is event target');
+  });
+});
