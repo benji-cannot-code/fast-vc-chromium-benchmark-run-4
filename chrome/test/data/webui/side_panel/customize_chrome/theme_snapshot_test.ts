421bb1c96c5cf4c5361d71938dd808f2e237d039
Riley Tatum <rtatum@google.com>
2024-03-08 19:02:35 +0000

    make panorama gm3 styles default
    https://screencast.googleplex.com/cast/NDg5NDczMTU4MTUyMTkyMHxlOTg0YjE5NC01ZA
    
    Bug: 328633944
    Change-Id: I73efe89e7f9408102403866ab5deb5ef9f10f183
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5353694
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1270316}

--- chrome/test/data/webui/side_panel/customize_chrome/theme_snapshot_test.ts
+++ chrome/test/data/webui/side_panel/customize_chrome/theme_snapshot_test.ts
@@ -13,7 +13,7 @@ import {CustomizeThemeType} from 'chrome://customize-chrome-side-panel.top-chrom
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import type {TestMock} from 'chrome://webui-test/test_mock.js';
 
-import {$$, assertStyle, createBackgroundImage, createTheme, installMock} from './test_support.js';
+import {$$, createBackgroundImage, createTheme, installMock} from './test_support.js';
 
 suite('ThemeSnapshotTest', () => {
   let themeSnapshotElement: ThemeSnapshotElement;
@@ -76,7 +76,7 @@ suite('ThemeSnapshotTest', () => {
             themeSnapshotElement, '.snapshot-container img')!.src);
   });
 
-  test('not setting a theme updates preview background color', async () => {
+  test('default chrome updates theme snapshot', async () => {
     // Arrange.
     createThemeSnapshotElement();
     const theme = createTheme();
@@ -110,10 +110,6 @@ suite('ThemeSnapshotTest', () => {
         $$(themeSnapshotElement,
            '.snapshot-container #classicChromeThemeTitle')!.textContent!
             .trim());
-    assertStyle(
-        $$(themeSnapshotElement,
-           '.snapshot-container #classicChromeBackground')!,
-        'background-color', 'rgb(20, 83, 154)');
   });
 
   test('uploading a background updates theme snapshot', async () => {
