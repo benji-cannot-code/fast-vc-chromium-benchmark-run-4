82ae899c2c643a5bd1c6dce10ce7c33e08cf1016
David Tseng <dtseng@google.com>
2024-03-08 04:10:35 +0000

    [a11y]: provide [un]register event methods for AXEventGenerator
    This change introduces the idea of event registration into
    AXEventGenerator.
    
    For a given event, AXEventGenerator needs to compute the diff of old and
    new node data, and potentially fire events on one or more nodes.
    
    In the case of related nodes changed, the number of events fired may be
    substantial.
    
    With event registration:
    - events targeted on a node with no node/ancestor registrants for that
      event will skip
    - if no nodes are registered for a given event, it can be skipped
    
    We currently start off with related nodes changed, but this can be
    expanded to other events.
    
    Bug: none
    Test: cq
    Change-Id: Id486b64a6de5350afcacb6e5aa678d0831351c37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5354226
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270002}

--- ui/accessibility/ax_event_generator_unittest.cc
+++ ui/accessibility/ax_event_generator_unittest.cc
@@ -674,6 +674,8 @@ TEST(AXEventGeneratorTest, ActiveDescendantChanged) {
   update.nodes[0].int_attributes.clear();
   update.nodes[0].AddIntAttribute(ax::mojom::IntAttribute::kActivedescendantId,
                                   3);
+  event_generator.RegisterEventOnNode(
+      AXEventGenerator::Event::RELATED_NODE_CHANGED, update.nodes[0].id);
   ASSERT_TRUE(tree.Unserialize(update));
   EXPECT_THAT(
       event_generator,
@@ -1270,6 +1272,8 @@ TEST(AXEventGeneratorTest, OtherAttributeChanged) {
   std::vector<int> ids = {2};
   update.nodes[5].AddIntListAttribute(ax::mojom::IntListAttribute::kControlsIds,
                                       ids);
+  event_generator.RegisterEventOnNode(
+      AXEventGenerator::Event::RELATED_NODE_CHANGED, update.nodes[0].id);
   ASSERT_TRUE(tree.Unserialize(update));
   EXPECT_THAT(
       event_generator,
@@ -1357,6 +1361,8 @@ TEST(AXEventGeneratorTest, MenuItemSelected) {
   update.nodes[0].int_attributes.clear();
   update.nodes[0].AddIntAttribute(ax::mojom::IntAttribute::kActivedescendantId,
                                   3);
+  event_generator.RegisterEventOnNode(
+      AXEventGenerator::Event::RELATED_NODE_CHANGED, update.nodes[0].id);
   ASSERT_TRUE(tree.Unserialize(update));
   EXPECT_THAT(
       event_generator,
@@ -1890,6 +1896,8 @@ TEST(AXEventGeneratorTest, ActiveDescendantChangeOnDescendant) {
   // create all of the node's children. Since node 3 already exists and remains
   // in the tree, that (re)created child is reporting a new parent.
   update.node_id_to_clear = 2;
+  event_generator.RegisterEventOnNode(
+      AXEventGenerator::Event::RELATED_NODE_CHANGED, update.nodes[0].id);
   ASSERT_TRUE(tree.Unserialize(update));
   EXPECT_THAT(
       event_generator,
@@ -2061,10 +2069,10 @@ TEST(AXEventGeneratorTest, IntListPropertyChanges) {
           HasEventAtNode(AXEventGenerator::Event::FLOW_FROM_CHANGED, 1),
           HasEventAtNode(AXEventGenerator::Event::FLOW_FROM_CHANGED, 2),
           HasEventAtNode(AXEventGenerator::Event::FLOW_TO_CHANGED, 3),
-          HasEventAtNode(AXEventGenerator::Event::LABELED_BY_CHANGED, 4),
-          HasEventAtNode(AXEventGenerator::Event::RELATED_NODE_CHANGED, 2),
-          HasEventAtNode(AXEventGenerator::Event::RELATED_NODE_CHANGED, 3),
-          HasEventAtNode(AXEventGenerator::Event::RELATED_NODE_CHANGED, 4)));
+          HasEventAtNode(AXEventGenerator::Event::LABELED_BY_CHANGED, 4)
+          // Related node changed not fired because it requires explicit
+          // registration.
+          ));
 }
 
 TEST(AXEventGeneratorTest, AriaBusyChanged) {
@@ -2170,6 +2178,8 @@ TEST(AXEventGeneratorTest, FlowToChanged) {
   AXTreeUpdate update = initial_state;
   update.nodes[1].AddIntListAttribute(ax::mojom::IntListAttribute::kFlowtoIds,
                                       {4, 5, 6});
+  event_generator.RegisterEventOnNode(
+      AXEventGenerator::Event::RELATED_NODE_CHANGED, update.nodes[0].id);
 
   EXPECT_TRUE(tree.Unserialize(update));
   EXPECT_THAT(
@@ -2198,6 +2208,8 @@ TEST(AXEventGeneratorTest, ControlsChanged) {
   std::vector<int> ids = {2};
   update.nodes[0].AddIntListAttribute(ax::mojom::IntListAttribute::kControlsIds,
                                       ids);
+  event_generator.RegisterEventOnNode(
+      AXEventGenerator::Event::RELATED_NODE_CHANGED, update.nodes[0].id);
   EXPECT_TRUE(tree.Unserialize(update));
   EXPECT_THAT(
       event_generator,
