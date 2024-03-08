fafe60d6c0530dded3f99166a4aff5a48066922e
Sophie Wen <sophiewen@chromium.org>
2024-03-08 23:51:22 +0000

    splitview: Remove SplitViewController from SplitViewDividerView
    This removes the `SplitViewController` dependencies from
    `SplitViewDividerView`. It replaces it with `LayoutDividerController`.
    
    Also fixes an existing dangling raw_ptr issue when
    `SplitViewController` is destroyed from the root window closing,
    during which we should clear `controller_` in `SplitViewDividerView`.
    
    Test: existing
    Bug: b/314018158
    Change-Id: I74086beb7540f14144961bb2849fa44861738a2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5331181
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270486}

--- ash/wm/splitview/split_view_divider.cc
+++ ash/wm/splitview/split_view_divider.cc
@@ -110,8 +110,11 @@ gfx::Rect SplitViewDivider::GetDividerBoundsInScreen(
   }
 }
 
-aura::Window* SplitViewDivider::GetRootWindow() const {
-  return divider_widget_->GetNativeWindow()->GetRootWindow();
+void SplitViewDivider::ShutDown() {
+  if (divider_view_) {
+    divider_view_->OnShuttingDown();
+  }
+  CloseDividerWidget();
 }
 
 bool SplitViewDivider::HasDividerWidget() const {
@@ -445,6 +448,10 @@ void SplitViewDivider::CreateDividerWidget(int divider_position) {
   wm::TransientWindowManager::GetOrCreate(divider_widget_native_window)
       ->set_parent_controls_lifetime(false);
   divider_widget_->Show();
+}
+
+aura::Window* SplitViewDivider::GetRootWindow() const {
+  return divider_widget_->GetNativeWindow()->GetRootWindow();
 }
 
 void SplitViewDivider::RefreshStackingOrder() {
--- ash/wm/splitview/split_view_divider_view.h
+++ ash/wm/splitview/split_view_divider_view.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 class IconButton;
-class SplitViewController;
+class LayoutDividerController;
 class SplitViewDivider;
 class SplitViewDividerHandlerView;
 
@@ -24,7 +24,7 @@ class SplitViewDividerView : public views::View,
   METADATA_HEADER(SplitViewDividerView, views::View)
 
  public:
-  explicit SplitViewDividerView(SplitViewController* controller,
+  explicit SplitViewDividerView(LayoutDividerController* controller,
                                 SplitViewDivider* divider);
   SplitViewDividerView(const SplitViewDividerView&) = delete;
   SplitViewDividerView& operator=(const SplitViewDividerView&) = delete;
@@ -32,6 +32,10 @@ class SplitViewDividerView : public views::View,
 
   void DoSpawningAnimation(int spawn_position);
   void SetDividerBarVisible(bool visible);
+
+  // Called explicitly by SplitViewDivider when LayoutDividerController is
+  // shutting down.
+  void OnShuttingDown();
 
   // views::View:
   void Layout(PassKey) override;
@@ -71,8 +75,7 @@ class SplitViewDividerView : public views::View,
   // a resize.
   bool mouse_move_started_ = false;
 
-  // TODO(b/314018158): Replace with `LayoutDividerController`.
-  raw_ptr<SplitViewController, DanglingUntriaged> split_view_controller_;
+  raw_ptr<LayoutDividerController> controller_;
   raw_ptr<SplitViewDividerHandlerView> divider_handler_view_ = nullptr;
   raw_ptr<SplitViewDivider, DanglingUntriaged> divider_;
 
