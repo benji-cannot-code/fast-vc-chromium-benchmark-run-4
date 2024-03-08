cc66ad1e5249b0b9701cd2a26498842c8306e5aa
Ahmed Mehfooz <amehfooz@chromium.org>
2024-03-08 01:03:42 +0000

    Update mahi menu input container styling
    Bug: b:324290682
    
    Change-Id: I3cb039a07f5fb6857866c6b5dbea6ae6a73691d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5346395
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269946}

--- chrome/browser/ui/views/mahi/mahi_menu_view.h
+++ chrome/browser/ui/views/mahi/mahi_menu_view.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_header_macros.h"
 
 namespace views {
+class FlexLayoutView;
 class ImageButton;
 class LabelButton;
 class UniqueWidgetPtr;
@@ -47,6 +48,8 @@ class MahiMenuView : public chromeos::editor_menu::PreTargetHandlerView {
  private:
   // Button callback.
   void OnSummaryButtonPressed();
+
+  std::unique_ptr<views::FlexLayoutView> CreateInputContainer();
 
   raw_ptr<views::ImageButton> settings_button_ = nullptr;
   raw_ptr<views::LabelButton> summary_button_ = nullptr;
