7dbf2b0455519b67a629612c1a4b571777c04d0d
Maksim Sisov <msisov@igalia.com>
2024-03-08 16:11:03 +0000

    ozone/wayland: add some benchmark tracing
    Adds STEP_BACKEND_SEND_BUFFER_SWAP,
    STEP_BACKEND_SEND_BUFFER_POST_SUBMIT,
    and STEP_BACKEND_FINISH_BUFFER_SWAP steps that backends can use.
    
    Ozone/Wayland uses those steps when storing a frame in the
    frame manager, playing it back and then notifying viz about
    swap submission.
    
    Please see an example trace with Ozone/Wayland on Linux that has STEP_BACKEND* here - https://drive.google.com/file/d/1Ql4ViEOCTGkcMZZ0tvxhPWYJ_SplDCrC/view?usp=sharing
    
    Bug: b:40264030
    Change-Id: I5c33a59122082e60946480e9b175a6fb34a372dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5310027
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1270220}

--- ui/ozone/platform/wayland/gpu/gbm_surfaceless_wayland.cc
+++ ui/ozone/platform/wayland/gpu/gbm_surfaceless_wayland.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ranges/algorithm.h"
 #include "base/task/thread_pool.h"
 #include "base/trace_event/trace_event.h"
+#include "base/trace_event/typed_macros.h"
 #include "ui/gfx/gpu_fence.h"
 #include "ui/gfx/gpu_fence_handle.h"
 #include "ui/gl/gl_bindings.h"
@@ -129,7 +130,11 @@ GbmSurfacelessWayland::GbmSurfacelessWayland(
 
 void GbmSurfacelessWayland::QueueWaylandOverlayConfig(
     wl::WaylandOverlayConfig config) {
-  unsubmitted_frames_.back()->configs.emplace_back(std::move(config));
+  auto* frame = unsubmitted_frames_.back().get();
+  DCHECK(frame);
+  TRACE_EVENT("wayland", "GbmSurfacelessWayland::QueueWaylandOverlayConfig",
+              "frame_id", frame->frame_id, "buffer_id", config.buffer_id);
+  frame->configs.emplace_back(std::move(config));
 }
 
 bool GbmSurfacelessWayland::ScheduleOverlayPlane(
@@ -314,6 +319,10 @@ void GbmSurfacelessWayland::OnSubmission(uint32_t frame_id,
   }
 
   auto submitted_frame = std::move(submitted_frames_.front());
+
+  TRACE_EVENT("wayland", "GbmSurfacelessWayland::OnSubmission", "frame_id",
+              submitted_frame->frame_id);
+
   submitted_frames_.erase(submitted_frames_.begin());
   for (auto& buf : submitted_frame->in_flight_color_buffers) {
     // Let the holder mark this buffer as free to reuse.
