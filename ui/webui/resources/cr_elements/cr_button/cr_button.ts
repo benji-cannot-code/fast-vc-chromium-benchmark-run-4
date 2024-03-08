6e3b9db4b401c1a77463edd1be34c3aa447333f9
dpapad <dpapad@chromium.org>
2024-03-08 19:01:14 +0000

    WebUI: Remove unused circleRipple parameter from cr-button.
    This is only used by CrOS quick_unlock UI and is no longer needed in
    Desktop's cr-button.
    
    Effectively this CL partially reverts http://crrev.com/c/1967806, and is
    in preparation of migrating cr-button to Lit.
    
    Bug: 40943652
    Change-Id: If2248800f1ef90d9b7d97e75ddc202d077689f84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5350684
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270314}

--- ui/webui/resources/cr_elements/cr_button/cr_button.ts
+++ ui/webui/resources/cr_elements/cr_button/cr_button.ts
@@ -54,15 +54,6 @@ export class CrButtonElement extends CrButtonElementBase {
         observer: 'applyTabIndex_',
       },
 
-      /**
-       * Flag used for formatting ripples on circle shaped cr-buttons.
-       * @private
-       */
-      circleRipple: {
-        type: Boolean,
-        value: false,
-      },
-
       hasPrefixIcon_: {
         type: Boolean,
         reflectToAttribute: true,
@@ -79,7 +70,6 @@ export class CrButtonElement extends CrButtonElementBase {
 
   disabled: boolean;
   customTabIndex: number;
-  circleRipple: boolean;
   private hasPrefixIcon_: boolean;
   private hasSuffixIcon_: boolean;
 
@@ -222,22 +212,6 @@ export class CrButtonElement extends CrButtonElementBase {
 
   private onPointerDown_() {
     this.ensureRipple();
-  }
-
-  /**
-   * Customize the element's ripple. Overriding the '_createRipple' function
-   * from PaperRippleMixin.
-   */
-  /* eslint-disable-next-line @typescript-eslint/naming-convention */
-  override _createRipple() {
-    const ripple = super._createRipple();
-
-    if (this.circleRipple) {
-      ripple.setAttribute('center', '');
-      ripple.classList.add('circle');
-    }
-
-    return ripple;
   }
 }
 
