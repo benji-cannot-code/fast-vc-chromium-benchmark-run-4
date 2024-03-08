fdf536f8a7abe1d9382bd09e89bcefb5af92ee71
Thuong Phan <thuongphan@chromium.org>
2024-03-08 00:23:12 +0000

    p13n: update sea pen routing
    When a user selects a template from Sea Pen root, navigates to the
    result page and shows zero state svg.
    When the Sea Pen root page breadcrumb is selected, show the root page
    with wallpaper selected at the top again.
    
    Demo:
    http://go/scrcast/NTMzNTExNTU4MzQ1NTIzMnxmYjBiN2U2ZC02Yw
    http://go/scrcast/NTc1OTgwMjg4OTIwNzgwOHxiYWIxMzIyZS05Yg
    
    Bug: b/324247199, b/324247261
    Test: browser_tests --gtest_filter="*SeaPen*"
    Test: browser_tests --gtest_filter="*VcBackgroundUi*"
    Change-Id: Ie4dd18af2f0d8275b844e3f7a79c3575a5eafdf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5349385
    Commit-Queue: Thuong Phan <thuongphan@chromium.org>
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Reviewed-by: Jerry Liu <pzliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1269926}

--- chrome/test/data/webui/chromeos/personalization_app/sea_pen_images_element_test.ts
+++ chrome/test/data/webui/chromeos/personalization_app/sea_pen_images_element_test.ts
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://personalization/strings.m.js';
 import 'chrome://webui-test/chromeos/mojo_webui_test_support.js';
 
-import {SeaPenImageLoadingElement, SeaPenImagesElement, setSelectedRecentSeaPenImageAction, SparklePlaceholderElement, WallpaperGridItemElement} from 'chrome://personalization/js/personalization_app.js';
+import {SeaPenImageLoadingElement, SeaPenImagesElement, SeaPenZeroStateSvgElement, setSelectedRecentSeaPenImageAction, SparklePlaceholderElement, WallpaperGridItemElement} from 'chrome://personalization/js/personalization_app.js';
 import {CrIconButtonElement} from 'chrome://resources/ash/common/cr_elements/cr_icon_button/cr_icon_button.js';
 import {PromiseResolver} from 'chrome://resources/ash/common/promise_resolver.js';
 import {MantaStatusCode, SeaPenThumbnail} from 'chrome://resources/ash/common/sea_pen/sea_pen.mojom-webui.js';
@@ -48,7 +48,7 @@ suite('SeaPenImagesElementTest', function() {
     seaPenImagesElement = null;
   });
 
-  test('displays thumbnail placeholders', async () => {
+  test('displays zero state SVG', async () => {
     // Initialize |seaPenImagesElement|.
     seaPenImagesElement = initElement(SeaPenImagesElement);
     await waitAfterNextRender(seaPenImagesElement);
@@ -59,12 +59,10 @@ suite('SeaPenImagesElementTest', function() {
         !!loadingThumbnailPlaceholder,
         'thumbnails should not be in loading state');
 
-    const thumbnailPlaceholders =
-        seaPenImagesElement.shadowRoot!.querySelectorAll(
-            'div:not([hidden]) .thumbnail-placeholder');
-    assertEquals(
-        4, thumbnailPlaceholders!.length,
-        'should be 4 placeholders available.');
+    assertTrue(
+        !!seaPenImagesElement.shadowRoot!.querySelector(
+            SeaPenZeroStateSvgElement.is),
+        'sea-pen-zero-state-svg is shown initially');
   });
 
   test('displays loading thumbnail placeholders', async () => {
