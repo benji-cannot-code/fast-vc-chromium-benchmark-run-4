18ddaf9e4f136c424dbb16d332f7aee04d373090
Adam Walls <avvall@google.com>
2024-03-08 13:43:43 +0000

    [WebView] Post LauncherThread init to a BG task
    This avoids initialization of LauncherThread's private static
    members on the UIThread. Instead, WebView will now post init to a BG
    thread.
    This also removes the requirement for the ChildProcessLauncherHelperImpl#warmUp method to run on the UIThread, since it should be thread-safe.
    
    Bug: b:325495402
    Change-Id: I53929cb7d9f74206ffea940bd465f1fd6a5d10ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5304023
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Adam Walls <avvall@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270149}

--- content/public/android/java/src/org/chromium/content/browser/ChildProcessLauncherHelperImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/ChildProcessLauncherHelperImpl.java
@@ -417,8 +417,7 @@ public final class ChildProcessLauncherHelperImpl {
     /**
      * @see {@link ChildProcessLauncherHelper#warmUp(Context)}.
      */
-    public static void warmUp(final Context context, boolean sandboxed) {
-        assert ThreadUtils.runningOnUiThread();
+    public static void warmUpOnAnyThread(final Context context, boolean sandboxed) {
         LauncherThread.post(
                 new Runnable() {
                     @Override
--- content/public/android/javatests/src/org/chromium/content/browser/ChildProcessLauncherHelperTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ChildProcessLauncherHelperTest.java
@@ -238,7 +238,7 @@ public class ChildProcessLauncherHelperTest {
     private static void warmUpOnUiThreadBlocking(final Context context, boolean sandboxed) {
         TestThreadUtils.runOnUiThreadBlocking(
                 () -> {
-                    ChildProcessLauncherHelperImpl.warmUp(context, sandboxed);
+                    ChildProcessLauncherHelperImpl.warmUpOnAnyThread(context, sandboxed);
                 });
         ChildProcessConnection connection = getWarmUpConnection(sandboxed);
         Assert.assertNotNull(connection);
