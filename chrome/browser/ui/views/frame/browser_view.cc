4499267850feff3d6ffe19c796e37e064b18396b
Yuta Hijikata <ythjkt@google.com>
2024-03-08 18:04:32 +0000

    Call SetLastActive with BrowserView::Activate.
    On platforms other than Windows or Ash, calling
    `BrowserWindow::Activate()` does not call
    `BrowserList::SetLastActive()` synchronously but waits for
    `OnWidgetActivationChanged()` to call it (which gets called
    async).
    
    However if its called from `BrowserView::Show()`,
    `BrowserList::SetLastActive()` gets called synchronously as commented
    in the function. Because of this by the time `ProcessSessionWindows()`
    finished running, `BrowserList::SetLastActive()` is called in order
    the browser appears in the order of the `windows` passed to
    `ProcessSessionWindows()` but not get called for `browser_to_activate`
    which is supposed to ensure that the correct browser is restored
    as the active browser.
    
    To fix this issue, call SetLastActive from `BrowserView::Activate`
    the same way `BrowserView::Show()` does.
    
    Otherwise when profile is activated by
    `ProfileLaunchObserver::ActivateProfile`, incorrect browser gets
    activated.
    
    Some information can be found on
    https://docs.google.com/document/d/13nOG8hftYBebJJ422g_Rl-S8WF59jMaKiIdYj4_3MXo/edit?resourcekey=0-Ylakdx356ti4k9AmG8RbzA&tab=t.0
    
    Bug: b/40073136
    Change-Id: Ic856467cd20283f9d816fc09995425cefcba14e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5302975
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Yuta Hijikata <ythjkt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270285}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -1408,6 +1408,11 @@ void BrowserView::Close() {
 }
 
 void BrowserView::Activate() {
+#if !BUILDFLAG(IS_WIN) && !BUILDFLAG(IS_CHROMEOS_ASH)
+  // Update the list managed by `BrowserList` synchronously the same way
+  // `BrowserView::Show()` does.
+  BrowserList::SetLastActive(browser());
+#endif
   frame_->Activate();
 }
 
