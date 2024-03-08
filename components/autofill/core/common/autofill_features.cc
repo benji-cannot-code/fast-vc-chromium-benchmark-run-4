c4ae8fae04c967c6cff768624390644e7a280f18
Jihad Hanna <jihadghanna@google.com>
2024-03-08 17:02:49 +0000

    Enable feature AutofillUseAddressRewriterInProfileSubsetComparison
    Bug: 40257475
    Change-Id: I6cc839f7512c0adcd31e8dd6bb082c27c511fdbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5352546
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1270245}

--- components/autofill/core/common/autofill_features.cc
+++ components/autofill/core/common/autofill_features.cc
@@ -324,7 +324,7 @@ BASE_FEATURE(kAutofillReplaceCachedWebElementsByRendererIds,
 // normalization.
 BASE_FEATURE(kAutofillUseAddressRewriterInProfileSubsetComparison,
              "AutofillUseAddressRewriterInProfileSubsetComparison",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Enables using the newer i18n address model, overriding the legacy one.
 // This includes:
