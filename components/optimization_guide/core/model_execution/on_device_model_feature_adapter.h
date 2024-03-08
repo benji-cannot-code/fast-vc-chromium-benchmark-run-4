0bf90cc725431386b0442144aa29c39f50d9df9a
Steven Holte <holte@google.com>
2024-03-08 02:45:00 +0000

    Simplify interpreter/adapter interface.
    Combine GetStringToCheckForRedacting and redactor()->Redact + timing
    histogram into a single "Redact" call.
    
    Change-Id: Id26e5d7f62070f6d5c55733cfc296d162d5ec957
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5347177
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269981}

--- components/optimization_guide/core/model_execution/on_device_model_feature_adapter.h
+++ components/optimization_guide/core/model_execution/on_device_model_feature_adapter.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
 #include "base/task/sequenced_task_runner.h"
+#include "components/optimization_guide/core/model_execution/redactor.h"
 #include "components/optimization_guide/core/model_execution/substitution.h"
 #include "components/optimization_guide/proto/model_execution.pb.h"
 
@@ -40,15 +41,15 @@ class OnDeviceModelFeatureAdapter final {
   std::optional<proto::Any> ConstructOutputMetadata(
       const std::string& output) const;
 
+  // Redacts the content of current response, given the last executed message.
+  RedactResult Redact(const google::protobuf::MessageLite& last_message,
+                      std::string& current_response) const;
+
+ private:
   // Returns the string that is used for checking redaction against.
   std::string GetStringToCheckForRedacting(
       const google::protobuf::MessageLite& message) const;
 
-  // Returns the Redactor. Return value is owned by
-  // this and may be null.
-  const Redactor* redactor() const { return redactor_.get(); }
-
- private:
   proto::OnDeviceModelExecutionFeatureConfig config_;
   std::unique_ptr<Redactor> redactor_;
 };
