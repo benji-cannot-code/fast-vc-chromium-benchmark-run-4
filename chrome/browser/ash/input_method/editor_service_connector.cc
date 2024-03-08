f03e1f2bf9c4b5fbede9714ce4bddb70896068ac
Curtis McMullan <curtismcmullan@chromium.org>
2024-03-08 08:46:36 +0000

    editor: send allowed presets to backend
    This CL adds the logic required to map preset feature flag values to a
    list of allowed presets. This list is then sent to the backend so that
    it can restrict the presets given to clients based on the list.
    
    BUG=b:328330399
    
    Change-Id: Ie239fa9b2cda715e1743eb0ac9da4633c87fe907
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5355187
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270077}

--- chrome/browser/ash/input_method/editor_service_connector.cc
+++ chrome/browser/ash/input_method/editor_service_connector.cc
@@ -5,9 +5,38 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/input_method/editor_service_connector.h"
 
+#include <vector>
+
+#include "ash/constants/ash_features.h"
+#include "base/feature_list.h"
+#include "chromeos/ash/services/orca/public/mojom/orca_service.mojom.h"
 #include "content/public/browser/service_process_host.h"
 
 namespace ash::input_method {
+namespace {
+
+orca::mojom::EditorConfigPtr GenerateConfig() {
+  std::vector<orca::mojom::PresetTextQueryType> allowed;
+  if (base::FeatureList::IsEnabled(features::kOrcaElaborate)) {
+    allowed.push_back(orca::mojom::PresetTextQueryType::kElaborate);
+  }
+  if (base::FeatureList::IsEnabled(features::kOrcaEmojify)) {
+    allowed.push_back(orca::mojom::PresetTextQueryType::kEmojify);
+  }
+  if (base::FeatureList::IsEnabled(features::kOrcaFormalize)) {
+    allowed.push_back(orca::mojom::PresetTextQueryType::kFormalize);
+  }
+  if (base::FeatureList::IsEnabled(features::kOrcaRephrase)) {
+    allowed.push_back(orca::mojom::PresetTextQueryType::kRephrase);
+  }
+  if (base::FeatureList::IsEnabled(features::kOrcaShorten)) {
+    allowed.push_back(orca::mojom::PresetTextQueryType::kShorten);
+  }
+  return orca::mojom::EditorConfig::New(
+      /*allowed_types=*/std::move(allowed));
+}
+
+}  // namespace
 
 EditorServiceConnector::EditorServiceConnector() {}
 
@@ -40,7 +69,8 @@ void EditorServiceConnector::BindEditor(
         text_query_provider) {
   remote_orca_service_connector_->BindEditor(
       std::move(system_actuator), std::move(text_query_provider),
-      std::move(editor_client_connector), std::move(editor_event_sink));
+      std::move(editor_client_connector), std::move(editor_event_sink),
+      GenerateConfig());
 }
 
 bool EditorServiceConnector::IsBound() {
--- chrome/browser/ash/input_method/editor_service_connector.h
+++ chrome/browser/ash/input_method/editor_service_connector.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/remote.h"
 
 namespace ash::input_method {
+
+// TODO(b/328330399): Modify this class to make testing easier and add tests.
 class EditorServiceConnector {
  public:
   EditorServiceConnector();
