9acaf0c89fa93cc09804b7c06a09069662eaeb95
Khalid Peer <khalidpeer@chromium.org>
2024-03-08 18:08:23 +0000

    [omnibox][ml] Use microsecond resolution for ElapsedTime scoring metric.
    Prior to this CL, the Omnibox.URLScoringModelExecuted.ElapsedTime metric
    was being logged with millisecond resolution. While this is fine in
    general, the issue is that the ML scoring model would often take a
    fraction of a millisecond (e.g. 0.3 ms) for a single suggestion scoring
    pass. As such, most of the logged times would get clustered into a
    single bucket when viewing the data via chrome://histograms.
    
    In order to remedy this issue, this CL updates the histogram definition
    and logging code to use a more fine-grained microsecond resolution for
    emitted samples. As a result, the data in chrome://histograms associated
    with this metric is much more readable (i.e. not as tightly clustered),
    which should help simplify analysis when looking at these numbers during
    future experimentation.
    
    BEFORE: http://screen/4GATHgj6PZqGXaf
    AFTER: http://screen/9WyN8epUy2tMhSE
    
    Bug: 324519307
    Change-Id: Ia585d11c49dd01869c3e0e3f78899160d160c591
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5352162
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Khalid Peer <khalidpeer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270288}

--- components/omnibox/browser/autocomplete_controller.cc
+++ components/omnibox/browser/autocomplete_controller.cc
@@ -1892,8 +1892,9 @@ void AutocompleteController::RunBatchUrlScoringModel(OldResult& old_result) {
                                  relevance_heap.size());
 
     // Record how long it took to execute the model for all eligible matches.
-    base::UmaHistogramTimes("Omnibox.URLScoringModelExecuted.ElapsedTime",
-                            elapsed_timer.Elapsed());
+    UMA_HISTOGRAM_CUSTOM_MICROSECONDS_TIMES(
+        "Omnibox.URLScoringModelExecuted.ElapsedTime", elapsed_timer.Elapsed(),
+        base::Microseconds(1), base::Milliseconds(3), 100);
   }
 
   while (!relevance_heap.empty()) {
@@ -1972,8 +1973,9 @@ void AutocompleteController::RunBatchUrlScoringModelWithStableSearches(
                                results.size());
 
   // Record how long it took to execute the model for all eligible matches.
-  base::UmaHistogramTimes("Omnibox.URLScoringModelExecuted.ElapsedTime",
-                          elapsed_timer.Elapsed());
+  UMA_HISTOGRAM_CUSTOM_MICROSECONDS_TIMES(
+      "Omnibox.URLScoringModelExecuted.ElapsedTime", elapsed_timer.Elapsed(),
+      base::Microseconds(1), base::Milliseconds(3), 100);
 
   // Record whether the model was executed for at least one eligible match.
   provider_client_->GetOmniboxTriggeredFeatureService()->FeatureTriggered(
@@ -2072,8 +2074,9 @@ void AutocompleteController::RunBatchUrlScoringModelMappedSearchBlending(
                                results.size());
 
   // Record how long it took to execute the model for all eligible matches.
-  base::UmaHistogramTimes("Omnibox.URLScoringModelExecuted.ElapsedTime",
-                          elapsed_timer.Elapsed());
+  UMA_HISTOGRAM_CUSTOM_MICROSECONDS_TIMES(
+      "Omnibox.URLScoringModelExecuted.ElapsedTime", elapsed_timer.Elapsed(),
+      base::Microseconds(1), base::Milliseconds(3), 100);
 
   // Record whether the model was executed for at least one eligible match.
   provider_client_->GetOmniboxTriggeredFeatureService()->FeatureTriggered(
