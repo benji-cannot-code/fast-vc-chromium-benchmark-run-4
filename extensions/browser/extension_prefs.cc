e3249aca402f3b5f61d6e1fdf91fedbc7cd423fe
Darryl James <dljames@chromium.org>
2024-03-08 23:02:05 +0000

    Revert "Perform extension parent approval migration on Desktop"
    This reverts commit 8baaf5ef045a0742ff377b5673bda909ab3ae267.
    
    Reason for revert: Causing a Linux MSAN bot to fail: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/41702/overview
    
    It appears to be a UAF caused at some point in SupervisedUserExtensionsManager::RefreshApprovedExtensionsFromPrefs()
    
    Original change's description:
    > Perform extension parent approval migration on Desktop
    >
    > When the feature 'EnableSupervisedUserSkipParentApprovalToInstallExtensions'
    > become enabled, the existing extensions on Desktop (Win/Mac/Linux) platforms are marked as locally-parent approved on that device, in a local preference, with a single-run migration.
    > The migration does not apply to ChromeOS platforms
    > where parental controls on extensions are already in place.
    >
    > Bug: b/321240030
    > Change-Id: I929fcb10b8f855602d6852b8195cedea477ed8b7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5321729
    > Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    > Commit-Queue: Anthi Orfanou <anthie@google.com>
    > Reviewed-by: Courtney Wong <courtneywong@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1270253}
    
    Bug: b/321240030
    Change-Id: Iaae5a62a1e6c8ec03e4247ff1970afc9b2a0fca5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5356979
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Darryl James <dljames@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Owners-Override: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270458}

--- extensions/browser/extension_prefs.cc
+++ extensions/browser/extension_prefs.cc
@@ -2264,8 +2264,6 @@ void ExtensionPrefs::RegisterProfilePrefs(
   registry->RegisterDictionaryPref(
       prefs::kSupervisedUserApprovedExtensions,
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
-  registry->RegisterDictionaryPref(
-      prefs::kSupervisedUserLocallyParentApprovedExtensions);
 #endif  // #if BUILDFLAG(ENABLE_SUPERVISED_USERS)
 
 #if !BUILDFLAG(IS_MAC)
