d9934ca66c897d93509a503d3620733bc1b23942
David Pennington <dpenning@chromium.org>
2024-03-08 23:42:24 +0000

    [TabHandles Migration] Update more files to new detach/attach methods.
    Bug: b/324274594
    Change-Id: I804dc5da7e2838c3adfd6abde760d27b4ec87a10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5288042
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Tomas Gunnarsson <tommi@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270481}

--- chrome/browser/media/webrtc/tab_desktop_media_list_unittest.cc
+++ chrome/browser/media/webrtc/tab_desktop_media_list_unittest.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/apps/chrome_app_delegate.h"
 #include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/tabs/tab_model.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/fake_profile_manager.h"
@@ -389,9 +390,9 @@ TEST_P(TabDesktopMediaListTest, RemoveTab) {
   base::RunLoop loop;
   TabStripModel* tab_strip_model = browser_->tab_strip_model();
   ASSERT_TRUE(tab_strip_model);
-  std::unique_ptr<WebContents> released_web_contents =
-      tab_strip_model->DetachWebContentsAtForInsertion(kDefaultSourceCount - 1);
-  std::erase(manually_added_web_contents_, released_web_contents.get());
+  std::unique_ptr<tabs::TabModel> detached_tab =
+      tab_strip_model->DetachTabAtForInsertion(kDefaultSourceCount - 1);
+  std::erase(manually_added_web_contents_, detached_tab.get()->contents());
 
   EXPECT_CALL(observer_, OnSourceRemoved(0))
       .WillOnce(
