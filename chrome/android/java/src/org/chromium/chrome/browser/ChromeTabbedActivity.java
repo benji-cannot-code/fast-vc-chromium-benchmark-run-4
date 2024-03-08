286983246fe4abe92e43f3866c022b5bf59d64b3
Xi Han <hanxi@google.com>
2024-03-08 18:49:17 +0000

    [DSE] Don't use DseNewTabUrlManager in TabCreator if not swapping out NTP.
    In CL https://crrev.com/c/5353550, the DseNewTabUrlManager is created
    as long as the feature flag is enabled. This object is used by
    ChromeTabCreator upon creation. Without swapping out NTP is enabled,
    the ChromeTabCreator doesn't need to check the DseNewTabUrlManager at
    all. In this CL, we pass null to ChromeTabCreator to prevent
    unnecessary checks in ChromeTabCreator. The swap out feature was added
    in CL (https://crrev.com/c/4844471).
    
    Bug: 328761907
    Change-Id: Ib8c19cae52a706bc9fe1833c76fcf2a321d11746
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5356235
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270304}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -2648,7 +2648,9 @@ public class ChromeTabbedActivity extends ChromeActivity<ChromeActivityComponent
                         AsyncTabParamsManagerSingleton.getInstance(),
                         getTabModelSelectorSupplier(),
                         getCompositorViewHolderSupplier(),
-                        mDseNewTabUrlManager),
+                        DseNewTabUrlManager.isSwapOutNtpFlagEnabled()
+                                ? mDseNewTabUrlManager
+                                : null),
                 new ChromeTabCreator(
                         this,
                         getWindowAndroid(),
