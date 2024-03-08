7c5c60d72f179ea4753134bdd05ac27457f4981e
Kazuki Takise <takise@google.com>
2024-03-08 05:22:53 +0000

    Remove ignore_bounds_change_request_
    This was introduced many years ago on crrev.com/c/1578719 so
    we can skip UpdatePipBounds() between window state change into
    PiP and initial bounds change for PiP.
    
    This approach was taken at that time because there was no way to
    tell whether initial bounds is already set when UpdatePipBounds()
    is called, but now we set PiP snap fraction once the initial
    bounds is set in exo, so we can handle this in ash.
    
    BUG=b:328344695
    TEST=Both chrome and ARC PiPs are placed at the proper position.
    
    Change-Id: I2f4a3ee62a4cab0bc737ac704a6690dfb23cfab6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5354172
    Reviewed-by: Eliot Courtney <edcourtney@chromium.org>
    Commit-Queue: Kazuki Takise <takise@chromium.org>
    Auto-Submit: Kazuki Takise <takise@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270024}

--- ash/wm/pip/pip_controller.cc
+++ ash/wm/pip/pip_controller.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wm/pip/pip_controller.h"
 
+#include "ash/public/cpp/app_types_util.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/screen_util.h"
@@ -145,6 +146,17 @@ void PipController::UpdatePipBounds() {
     return;
   }
   WindowState* window_state = WindowState::Get(pip_window_);
+  if (!ash::PipPositioner::HasSnapFraction(window_state) &&
+      IsArcWindow(pip_window_)) {
+    // Prevent PiP bounds from being updated between window state change into
+    // PiP and initial bounds change for PiP. This is only needed for ARC
+    // because chrome PiP becomes visible only after both window state and
+    // initial bounds are set properly while in the case of ARC a normal visible
+    // window can trainsition to PiP. Also, in fact, this check is only valid
+    // for ARC PiP as the first timing snap fraction is set is different between
+    // chrome PiP and ARC PiP.
+    return;
+  }
   gfx::Rect new_bounds =
       PipPositioner::GetPositionAfterMovementAreaChange(window_state);
   wm::ConvertRectFromScreen(pip_window_->GetRootWindow(), &new_bounds);
