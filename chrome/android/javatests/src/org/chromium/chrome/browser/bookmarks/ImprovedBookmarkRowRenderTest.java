f188461a015400edf7472902b509b57bd68e743b
Brandon Wylie <wylieb@chromium.org>
2024-03-08 23:22:56 +0000

    [bookmarks] Reduce size of local icon to reduce margin
    Using min_touch_target_size isn't necessary since this button is not
    clickable. Removing this and adding a 4dp margin to match the margin
    between the left image/title.
    
    Bug: 328621464
    Change-Id: I948be020ecf5335ee794762596c3d3d802843661
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5352462
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270470}

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/ImprovedBookmarkRowRenderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/ImprovedBookmarkRowRenderTest.java
@@ -89,7 +89,7 @@ public class ImprovedBookmarkRowRenderTest {
     @Rule
     public ChromeRenderTestRule mRenderTestRule =
             ChromeRenderTestRule.Builder.withPublicCorpus()
-                    .setRevision(5)
+                    .setRevision(6)
                     .setBugComponent(ChromeRenderTestRule.Component.UI_BROWSER_BOOKMARKS)
                     .build();
 
