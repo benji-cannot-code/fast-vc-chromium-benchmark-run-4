d2db9bd506c3eb3aa7f5f2aca410c4e056fc0c69
anthie@google.com <anthie@google.com>
2024-03-08 17:45:46 +0000

    Perform extension parent approval migration on Desktop
    When the feature 'EnableSupervisedUserSkipParentApprovalToInstallExtensions'
    become enabled, the existing extensions on Desktop (Win/Mac/Linux) platforms are marked as locally-parent approved on that device, in a local preference, with a single-run migration.
    The migration does not apply to ChromeOS platforms
    where parental controls on extensions are already in place.
    
    Bug: b/321240030
    Change-Id: I929fcb10b8f855602d6852b8195cedea477ed8b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5321729
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Reviewed-by: Courtney Wong <courtneywong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270253}

--- extensions/browser/extension_prefs.cc
+++ extensions/browser/extension_prefs.cc
@@ -2264,6 +2264,8 @@ void ExtensionPrefs::RegisterProfilePrefs(
   registry->RegisterDictionaryPref(
       prefs::kSupervisedUserApprovedExtensions,
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
+  registry->RegisterDictionaryPref(
+      prefs::kSupervisedUserLocallyParentApprovedExtensions);
 #endif  // #if BUILDFLAG(ENABLE_SUPERVISED_USERS)
 
 #if !BUILDFLAG(IS_MAC)
