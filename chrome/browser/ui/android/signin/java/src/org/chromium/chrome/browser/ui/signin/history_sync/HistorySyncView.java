616bf704c7375ac6fa43ee0b0bad008ae48b99a4
Samar <samarchehade@google.com>
2024-03-08 13:22:47 +0000

    [UNOp2][Android] Setup scrollviews for history sync
    We should follow what is being done for the fullscreen signin promo, no
    "more" button is needed.
    
    Bug: 41493766
    Change-Id: I8659450b66a614c91038c7cde2b2e2110a22d862
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5350124
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1270141}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/history_sync/HistorySyncView.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/history_sync/HistorySyncView.java
@@ -21,7 +21,6 @@ import org.chromium.components.browser_ui.widget.DualControlLayout;
 class HistorySyncView extends LinearLayout {
     private ImageView mAccountImage;
     private Button mDeclineButton;
-    private Button mMoreButton;
     private Button mAcceptButton;
     private DualControlLayout mButtonBar;
 
@@ -45,7 +44,6 @@ class HistorySyncView extends LinearLayout {
             createButtonBar();
         }
 
-        mMoreButton = findViewById(R.id.more_button);
         TextView detailsDescription = findViewById(R.id.sync_consent_details_description);
 
         // TODO(crbug.com/1520791): Confirm that these are the correct title and subtitle strings.
@@ -53,7 +51,6 @@ class HistorySyncView extends LinearLayout {
         title.setText(R.string.history_sync_consent_title_c);
         subtitle.setText(R.string.history_sync_consent_subtitle_c);
         mDeclineButton.setText(R.string.no_thanks);
-        mMoreButton.setText(R.string.more);
         mAcceptButton.setText(R.string.signin_accept_button);
         detailsDescription.setText(R.string.sync_consent_details_description);
     }
@@ -64,10 +61,6 @@ class HistorySyncView extends LinearLayout {
 
     Button getDeclineButton() {
         return mDeclineButton;
-    }
-
-    Button getMoreButton() {
-        return mMoreButton;
     }
 
     Button getAcceptButton() {
