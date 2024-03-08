40f200b2a8d024353772d4838a0b8bade0ce28db
Darwin Yang <darwinyang@google.com>
2024-03-08 17:44:34 +0000

    [autofill][flag]  Add AutofillEnableSaveCardLocalSaveFallback to flags
    Add the AutofillEnableSaveCardLocalSaveFallback feature flag to
    chrome://flags. When enabled, if the server card fails to be uploaded,
    the card will be saved locally as a fallback. If a card with the same
    card number and expiration date already exists in the local database,
    it will result in a no-op and the existing card will not be updated.
    
    The original CL adding the feature flag: https://crrev.com/c/5013164
    
    
    Bug: 1499264,b/308968607
    Change-Id: Ia8dca124fab67cb39a1c410e244d4088ce6a17a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5353532
    Commit-Queue: Darwin Yang <darwinyang@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270251}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -11032,6 +11032,13 @@ const FeatureEntry kFeatureEntries[] = {
          password_manager::features::kUnifiedPasswordManagerSyncOnlyInGMSCore)},
 #endif  // BUILDFLAG(IS_ANDROID)
 
+    {"autofill-enable-save-card-local-save-fallback",
+     flag_descriptions::kAutofillEnableSaveCardLocalSaveFallbackName,
+     flag_descriptions::kAutofillEnableSaveCardLocalSaveFallbackDescription,
+     kOsAll,
+     FEATURE_VALUE_TYPE(
+         autofill::features::kAutofillEnableSaveCardLocalSaveFallback)},
+
     // NOTE: Adding a new flag requires adding a corresponding entry to enum
     // "LoginCustomFlags" in tools/metrics/histograms/enums.xml. See "Flag
     // Histograms" in tools/metrics/histograms/README.md (run the
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -334,6 +334,9 @@ extern const char kAutofillEnableRemadeDownstreamMetricsDescription[];
 extern const char kAutofillEnableSaveCardLoadingAndConfirmationName[];
 extern const char kAutofillEnableSaveCardLoadingAndConfirmationDescription[];
 
+extern const char kAutofillEnableSaveCardLocalSaveFallbackName[];
+extern const char kAutofillEnableSaveCardLocalSaveFallbackDescription[];
+
 extern const char kAutofillEnableServerIbanName[];
 extern const char kAutofillEnableServerIbanDescription[];
 
