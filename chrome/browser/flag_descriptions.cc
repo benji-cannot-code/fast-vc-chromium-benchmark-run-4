f62ab7927e0d16d472f9ef99e2cb97e7662e34e5
Zainab Rizvi <rizvis@google.com>
2024-03-08 22:41:16 +0000

    Add a chrome://flags entry for Fingerprinting Protection.
    Uses the existing `kEnableFingerprintingProtectionBlocklist` feature:
     https://source.chromium.org/chromium/chromium/src/+/main:chrome/common/chrome_features.h;drc=1d5869c89615a31639ded2f190681f517f53de95;l=252
    
    Bug: 328675527
    Change-Id: I1ae546b1aafbf4798f62bfb823ca30ae4e8e06d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5355228
    Reviewed-by: Matt Reichhoff <mreichhoff@chromium.org>
    Reviewed-by: Patricia Alfonso <trishalfonso@google.com>
    Commit-Queue: Zainab Rizvi <rizvis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1270433}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -1265,6 +1265,12 @@ const char kEnableControlledFrameDescription[] =
     "https://github.com/WICG/controlled-frame/blob/main/EXPLAINER.md "
     "for more information.";
 
+const char kEnableFingerprintingProtectionBlocklistName[] =
+    "Enable Fingerprinting Protection Blocklist";
+const char kEnableFingerprintingProtectionBlocklistDescription[] =
+    "Enable Fingerprinting Protection which may block fingerprinting "
+    "resources from loading in a 3p context.";
+
 const char kEnableShortcutCustomizationName[] =
     "Enable customization in new shortcuts app";
 const char kEnableShortcutCustomizationDescription[] =
