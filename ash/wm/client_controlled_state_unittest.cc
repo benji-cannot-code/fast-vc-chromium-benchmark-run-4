299858750a23b82c8e6518d24e207c508720d88f
Sophie Wen <sophiewen@chromium.org>
2024-03-08 22:22:19 +0000

    snap-groups: Start resize on mouse/gesture dragged
    This modifies `SplitViewDividerView` to start resizing on a
    mouse/gesture *move*, not the first press/tap down. This will prevent
    starting a drag and creating DragDetails when a user double taps the
    divider, which causes conflicts with SnapGroups.
    
    Test: modified
    Bug: b/326296071
    Change-Id: I2189bd67db977c01d4e8f8f5831577a031a008b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5315922
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270415}

--- ash/wm/client_controlled_state_unittest.cc
+++ ash/wm/client_controlled_state_unittest.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/window_state_delegate.h"
 #include "ash/wm/window_util.h"
 #include "ash/wm/wm_event.h"
+#include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
@@ -59,6 +60,9 @@ namespace ash {
 namespace {
 
 using ::chromeos::WindowStateType;
+
+using BoundsRequestCallback =
+    base::RepeatingCallback<void(const gfx::Rect& bounds)>;
 
 constexpr gfx::Rect kInitialBounds(0, 0, 100, 100);
 
@@ -99,6 +103,9 @@ class TestClientControlledStateDelegate
       new_state_ = requested_state;
     }
     display_id_ = display_id;
+    if (bounds_request_callback_) {
+      bounds_request_callback_.Run(bounds);
+    }
   }
 
   WindowStateType old_state() const { return old_state_; }
@@ -106,6 +113,10 @@ class TestClientControlledStateDelegate
   WindowStateType new_state() const { return new_state_; }
 
   const gfx::Rect& requested_bounds() const { return requested_bounds_; }
+
+  void set_bounds_request_callback(BoundsRequestCallback callback) {
+    bounds_request_callback_ = std::move(callback);
+  }
 
   int64_t display_id() const { return display_id_; }
 
@@ -125,6 +136,7 @@ class TestClientControlledStateDelegate
   int64_t display_id_ = display::kInvalidDisplayId;
   gfx::Rect requested_bounds_;
   bool deleted_ = false;
+  BoundsRequestCallback bounds_request_callback_;
 };
 
 class TestWidgetDelegate : public views::WidgetDelegateView {
@@ -1434,6 +1446,7 @@ TEST_P(ClientControlledStateTestClamshellAndTablet, ResizeSnappedWindow) {
   state()->EnterNextState(window_state(), delegate()->new_state());
   ApplyPendingRequestedBounds();
   EXPECT_TRUE(window_state()->IsSnapped());
+  const gfx::Rect bounds_before_resizing(delegate()->requested_bounds());
 
   // Start drag-resizing from the center point of the work area.
   auto* const event_generator = GetEventGenerator();
@@ -1443,13 +1456,21 @@ TEST_P(ClientControlledStateTestClamshellAndTablet, ResizeSnappedWindow) {
                                      .CenterPoint();
   event_generator->set_current_screen_location(next_cursor_point);
   event_generator->PressLeftButton();
-  // The following drag info is used by client to determine how to handle the
-  // bounds change.
-  EXPECT_TRUE(window_state_delegate()->drag_in_progress());
-  EXPECT_TRUE(window_state()->drag_details()->bounds_change &
-              WindowResizer::kBoundsChange_Resizes);
+  // Test the requested bounds do not change.
+  EXPECT_EQ(bounds_before_resizing, delegate()->requested_bounds());
 
   // Keep dragging...
+  delegate()->set_bounds_request_callback(
+      base::BindLambdaForTesting([&](const gfx::Rect& bounds) {
+        if (bounds == bounds_before_resizing) {
+          return;
+        }
+        // When any new bounds is requested, `OnDragStarted()` should be called
+        // already.
+        EXPECT_TRUE(window_state_delegate()->drag_in_progress());
+        EXPECT_TRUE(window_state()->drag_details()->bounds_change &
+                    WindowResizer::kBoundsChange_Resizes);
+      }));
   next_cursor_point.Offset(-50, 0);
   event_generator->MoveMouseTo(next_cursor_point);
   // The following drag info is used by client to determine how to handle the
@@ -1458,6 +1479,7 @@ TEST_P(ClientControlledStateTestClamshellAndTablet, ResizeSnappedWindow) {
   EXPECT_TRUE(window_state()->drag_details()->bounds_change &
               WindowResizer::kBoundsChange_Resizes);
   ApplyPendingRequestedBounds();
+  delegate()->set_bounds_request_callback(base::NullCallback());
 
   // Drag to 1/3 (i.e. make the width 400).
   const float target_width = 400;
--- ash/wm/splitview/split_view_divider_view.cc
+++ ash/wm/splitview/split_view_divider_view.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/layer.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/display/screen.h"
+#include "ui/events/types/event_type.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/views/background.h"
 #include "ui/views/highlight_border.h"
@@ -149,11 +150,20 @@ void SplitViewDividerView::OnMouseExited(const ui::MouseEvent& event) {
 bool SplitViewDividerView::OnMousePressed(const ui::MouseEvent& event) {
   gfx::Point location(event.location());
   views::View::ConvertPointToScreen(this, &location);
-  StartResizing(location);
+  initial_mouse_event_location_ = location;
   return true;
 }
 
 bool SplitViewDividerView::OnMouseDragged(const ui::MouseEvent& event) {
+  if (!mouse_move_started_) {
+    // If this is the first mouse drag event, start the resize and reset
+    // `mouse_move_started_`.
+    DCHECK_NE(initial_mouse_event_location_, gfx::Point());
+    mouse_move_started_ = true;
+    StartResizing(initial_mouse_event_location_);
+    return true;
+  }
+  // Else continue with the resize.
   gfx::Point location(event.location());
   views::View::ConvertPointToScreen(this, &location);
   divider_->ResizeWithDivider(location);
@@ -163,6 +173,8 @@ bool SplitViewDividerView::OnMouseDragged(const ui::MouseEvent& event) {
 void SplitViewDividerView::OnMouseReleased(const ui::MouseEvent& event) {
   gfx::Point location(event.location());
   views::View::ConvertPointToScreen(this, &location);
+  initial_mouse_event_location_ = gfx::Point();
+  mouse_move_started_ = false;
   EndResizing(location, /*swap_windows=*/event.GetClickCount() == 2);
 }
 
@@ -182,6 +194,7 @@ void SplitViewDividerView::OnGestureEvent(ui::GestureEvent* event) {
       }
       break;
     case ui::ET_GESTURE_TAP_DOWN:
+      break;
     case ui::ET_GESTURE_SCROLL_BEGIN:
       StartResizing(location);
       break;
