6f4bcf395b7a2c03dd3405e05b604b4a97efddc2
Sammie Quon <sammiequon@chromium.org>
2024-03-08 18:55:44 +0000

    pine: Add a basic fade in/out animation
    This is the default fade in/out animation used for overview. We use
    this instead of the default widget fade in/out animation so the
    overview on starting/ending animation complete signals can be fired
    correctly.
    
    Test: manual
    Bug: b/328773255
    Change-Id: Iedaed16cbf1f8af6164a03113b86bc6c2486a4e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5351259
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270309}

--- ash/constants/ash_switches.cc
+++ ash/constants/ash_switches.cc
@@ -14,8 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/time/time.h"
 
-namespace ash {
-namespace switches {
+namespace ash::switches {
 
 namespace {
 
@@ -1371,22 +1370,21 @@ bool IsForestSecretKeyMatched() {
 
   // Commandline looks like:
   //  out/Default/chrome --user-data-dir=/tmp/tmp123
-  //  --birch-feature-key="INSERT KEY HERE" --enable-features=ForestFeature
+  //  --forest-feature-key="INSERT KEY HERE" --enable-features=ForestFeature
   const std::string provided_key_hash = base::SHA1HashString(
       base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
           kForestFeatureKey));
 
-  bool birch_key_matched = (provided_key_hash == kForestHashKey);
-  if (!birch_key_matched) {
+  bool forest_key_matched = (provided_key_hash == kForestHashKey);
+  if (!forest_key_matched) {
     LOG(ERROR) << "Provided secret key does not match with the expected one.";
   }
 
-  return birch_key_matched;
+  return forest_key_matched;
 }
 
 void SetIgnoreForestSecretKeyForTest(bool ignore) {
   g_ignore_forest_secret_key = ignore;
 }
 
-}  // namespace switches
-}  // namespace ash
+}  // namespace ash::switches
