7f290b4cb7942677e15465ade89b94d08e2c482e
Leonid Baraz <lbaraz@chromium.org>
2024-03-08 22:40:17 +0000

    Factor out upload response parser.
    In preparation of the events caching.
    Parser is prepared and unit-tested.
    RecordHandleImpl is modified to use it.
    
    Bug: b:314195198
    Change-Id: Idc92a08bd6d2c51516198298fda669fb4d3ac8be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5353528
    Reviewed-by: Josh Hilke <jrhilke@google.com>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270432}

--- chrome/browser/policy/messaging_layer/upload/record_handler_impl.h
+++ chrome/browser/policy/messaging_layer/upload/record_handler_impl.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/task_runner.h"
-#include "base/values.h"
 #include "chrome/browser/policy/messaging_layer/upload/file_upload_job.h"
 #include "chrome/browser/policy/messaging_layer/upload/server_uploader.h"
 #include "components/reporting/proto/synced/record.pb.h"
@@ -43,16 +42,6 @@ class RecordHandlerImpl : public ServerUploader::RecordHandler {
       ScopedReservation scoped_reservation,
       CompletionCallback upload_complete,
       EncryptionKeyAttachedCallback encryption_key_attached_cb) override;
-
- protected:
-  // Uses `SequenceInformationValueToProto` for testing.
-  friend class FakeUploadClient;
-
-  // Helper function for converting a base::Value representation of
-  // SequenceInformation into a proto. Will return an INVALID_ARGUMENT error
-  // if the base::Value is not convertible.
-  static StatusOr<SequenceInformation> SequenceInformationValueToProto(
-      const base::Value::Dict& value);
 
  private:
   // Helper `ReportUploader` class handles events being uploaded.
