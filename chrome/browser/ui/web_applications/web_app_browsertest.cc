7d2bf5ed5cdbf00d00e202a8d6ad640d0d2f45f4
Wei Lee <wtlee@google.com>
2024-03-08 03:20:56 +0000

    Move TestStructuredMetricsRecorder to components/
    This CL moves TestStructuredMetricsRecorder from chrome/ to components/
    so that it can be used by other tests outside chrome/.
    
    Bug: b:325853230
    Test: browser tests
    Change-Id: Ia6cfc46372875a2b85019a1c7f410786bfd8f187
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5351448
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Jong Ahn <jongahn@chromium.org>
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269991}

--- chrome/browser/ui/web_applications/web_app_browsertest.cc
+++ chrome/browser/ui/web_applications/web_app_browsertest.cc
@@ -132,7 +132,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_CHROMEOS)
 #include "chrome/browser/metrics/structured/event_logging_features.h"
-#include "chrome/browser/metrics/structured/test/test_structured_metrics_recorder.h"
+#include "components/metrics/structured/test/test_structured_metrics_recorder.h"
 #include "ui/views/test/dialog_test.h"
 #include "ui/views/widget/any_widget_observer.h"
 #include "ui/views/widget/widget.h"
--- components/metrics/structured/test/test_structured_metrics_recorder.cc
+++ components/metrics/structured/test/test_structured_metrics_recorder.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/metrics/structured/test/test_structured_metrics_recorder.h"
+#include "components/metrics/structured/test/test_structured_metrics_recorder.h"
 
 namespace metrics::structured {
 
