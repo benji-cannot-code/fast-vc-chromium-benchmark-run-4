f8388852f9431fd7c0affff13e10ddee8055d884
Jan Keitel <jkeitel@google.com>
2024-03-08 10:35:01 +0000

    Perform small clean-ups in plus_address_types.h
    Bug: 322279583
    Change-Id: Id14031296798ab5faea6d24f7f1b20a058d8a97a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5352171
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1270108}

--- components/plus_addresses/plus_address_types.h
+++ components/plus_addresses/plus_address_types.h
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PLUS_ADDRESSES_PLUS_ADDRESS_TYPES_H_
 #define COMPONENTS_PLUS_ADDRESSES_PLUS_ADDRESS_TYPES_H_
 
+#include <map>
 #include <optional>
 #include <string>
-#include <unordered_map>
 
 #include "base/functional/callback_forward.h"
 #include "base/types/expected.h"
@@ -68,20 +68,23 @@ class PlusAddressRequestError {
 
 // Only used by Autofill.
 using autofill::PlusAddressCallback;
-using PlusAddressMap = std::unordered_map<std::string, std::string>;
+
+using PlusAddressMap = std::map<std::string, std::string>;
 using PlusAddressMapCallback = base::OnceCallback<void(const PlusAddressMap&)>;
+
 // Holds either a PlusProfile or an error that prevented us from getting it.
 using PlusProfileOrError = base::expected<PlusProfile, PlusAddressRequestError>;
 using PlusAddressRequestCallback =
     base::OnceCallback<void(const PlusProfileOrError&)>;
-typedef base::expected<PlusProfile, PlusAddressRequestError> PlusProfileOrError;
-typedef base::OnceCallback<void(const PlusProfileOrError&)>
-    PlusAddressRequestCallback;
+using PlusProfileOrError = base::expected<PlusProfile, PlusAddressRequestError>;
+using PlusAddressRequestCallback =
+    base::OnceCallback<void(const PlusProfileOrError&)>;
+
 // Holds either a PlusAddressMap or an error that prevented us from getting it.
-typedef base::expected<PlusAddressMap, PlusAddressRequestError>
-    PlusAddressMapOrError;
-typedef base::OnceCallback<void(const PlusAddressMapOrError&)>
-    PlusAddressMapRequestCallback;
+using PlusAddressMapOrError =
+    base::expected<PlusAddressMap, PlusAddressRequestError>;
+using PlusAddressMapRequestCallback =
+    base::OnceCallback<void(const PlusAddressMapOrError&)>;
 
 // Defined for use in metrics and to share code for certain network-requests.
 enum class PlusAddressNetworkRequestType {
