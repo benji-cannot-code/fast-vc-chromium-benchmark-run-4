ecbb2c2f7d89e61d3c788429bc546a5f3ae10a98
Friedrich Horschig <fhorschig@chromium.org>
2024-03-08 13:20:11 +0000

    [Android][3PPWM] Factory to create correct client
    Noop since this factory is only used in crrev.com/c/5314306.
    
    See go/3ppwm-dd#heading=h.s0qbhft4nxzx:
    For each tab, Chrome creates a new autofill client. Using the client
    that support virtual view structures, Chrome requires
    - the autofill.using_virtual_view_structure pref, and
    - the AutofillVirtualViewStructureAndroid feature
    
    If either is not enabled on startup, Chrome should default to the
    client using the built-in autofill.
    
    Bug: 308420431, 308430023
    Change-Id: I7f5893f7f5132633394b1bad12286400770483be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5343822
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1270140}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -188,6 +188,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tpcd/support/tpcd_support_service_factory.h"
 #include "chrome/browser/translate/translate_model_service_factory.h"
 #include "chrome/browser/translate/translate_ranker_factory.h"
+#include "chrome/browser/ui/autofill/autofill_client_provider_factory.h"
 #include "chrome/browser/ui/cookie_controls/cookie_controls_service_factory.h"
 #include "chrome/browser/ui/find_bar/find_bar_state_factory.h"
 #include "chrome/browser/ui/hats/hats_service_factory.h"
@@ -618,6 +619,7 @@ void ChromeBrowserMainExtraPartsProfiles::
   AutocompleteScoringModelServiceFactory::GetInstance();
 #endif
   autofill::AutocompleteHistoryManagerFactory::GetInstance();
+  autofill::AutofillClientProviderFactory::GetInstance();
   autofill::AutofillImageFetcherFactory::GetInstance();
   autofill::AutofillLogRouterFactory::GetInstance();
 #if BUILDFLAG(BUILD_WITH_TFLITE_LIB)
