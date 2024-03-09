b404b9c5fb7da0d9db6cda28731eb629bcc492c1
Yuta Hijikata <ythjkt@google.com>
2024-03-09 05:57:36 +0000

    Do not activate window if minimized.
    Bug: b/40073136
    Change-Id: Ibd04ce40257f12669985642743b5fd47c7f2f0c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5296815
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Yuta Hijikata <ythjkt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270556}

--- ui/aura/window_tree_host_platform.cc
+++ ui/aura/window_tree_host_platform.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/check_is_test.h"
 #include "base/functional/bind.h"
 #include "base/observer_list.h"
 #include "base/run_loop.h"
@@ -44,6 +45,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace aura {
 
+namespace {
+WindowTreeHostPlatform::PlatformWindowFactoryDelegateForTesting*
+    g_platform_window_factory_delegate_for_testing = nullptr;
+}
+
 // static
 std::unique_ptr<WindowTreeHost> WindowTreeHost::Create(
     ui::PlatformWindowInitProperties properties) {
@@ -73,14 +79,7 @@ void WindowTreeHostPlatform::CreateAndSetPlatformWindow(
   // end up propagating unneeded bounds change event when it is first notified
   // through OnBoundsChanged, which may lead to unneeded re-layouts, etc.
   size_in_pixels_ = properties.bounds.size();
-#if BUILDFLAG(IS_OZONE)
-  platform_window_ = ui::OzonePlatform::GetInstance()->CreatePlatformWindow(
-      this, std::move(properties));
-#elif BUILDFLAG(IS_WIN)
-  platform_window_ = std::make_unique<ui::WinWindow>(this, properties.bounds);
-#else
-  NOTIMPLEMENTED();
-#endif
+  platform_window_ = CreatePlatformWindow(std::move(properties));
 }
 
 void WindowTreeHostPlatform::SetPlatformWindow(
@@ -200,6 +199,30 @@ void WindowTreeHostPlatform::OnCursorVisibilityChangedNative(bool show) {
 void WindowTreeHostPlatform::LockMouse(Window* window) {
   window->SetCapture();
   WindowTreeHost::LockMouse(window);
+}
+
+std::unique_ptr<ui::PlatformWindow>
+WindowTreeHostPlatform::CreatePlatformWindow(
+    ui::PlatformWindowInitProperties properties) {
+  if (g_platform_window_factory_delegate_for_testing) {
+    return g_platform_window_factory_delegate_for_testing->Create(this);
+  }
+#if BUILDFLAG(IS_OZONE)
+  return ui::OzonePlatform::GetInstance()->CreatePlatformWindow(
+      this, std::move(properties));
+#elif BUILDFLAG(IS_WIN)
+  return std::make_unique<ui::WinWindow>(this, properties.bounds);
+#else
+  NOTIMPLEMENTED();
+  return nullptr;
+#endif
+}
+
+// static
+void WindowTreeHostPlatform::SetPlatformWindowFactoryDelegateForTesting(
+    PlatformWindowFactoryDelegateForTesting* delegate) {
+  CHECK_IS_TEST();
+  g_platform_window_factory_delegate_for_testing = delegate;
 }
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
