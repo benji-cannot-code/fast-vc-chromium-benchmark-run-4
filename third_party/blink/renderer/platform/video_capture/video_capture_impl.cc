a811e0c08eaf20a1386b45b1821bd66b466f9e93
Sunny Sachanandani <sunnyps@chromium.org>
2024-03-09 00:35:14 +0000

    webgpu: Enable zero-copy video capture import on Windows
    Since Dawn now supports keyed mutex synchronization with concurrent read
    access and Chromium uses it after https://crrev.com/c/5325939, we can
    use zero-copy import for DXGI camera capture frames on Windows.
    
    Bug: 40270305, 40145992
    Change-Id: I36fc4c7964c392bc8f384ee4c0165d071096991c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5333805
    Reviewed-by: Ilya Nikolaevskiy <ilnik@chromium.org>
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270505}

--- third_party/blink/renderer/platform/video_capture/video_capture_impl.cc
+++ third_party/blink/renderer/platform/video_capture/video_capture_impl.cc
@@ -275,9 +275,7 @@ struct VideoCaptureImpl::BufferContext
 struct VideoCaptureImpl::VideoFrameInitData {
   media::mojom::blink::ReadyBufferPtr ready_buffer;
   scoped_refptr<BufferContext> buffer_context;
-#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_MAC)
   bool is_webgpu_compatible = false;
-#endif
   absl::variant<scoped_refptr<media::VideoFrame>,
                 std::unique_ptr<gfx::GpuMemoryBuffer>>
       frame_or_buffer;
@@ -472,11 +470,12 @@ VideoCaptureImpl::CreateVideoFrameInitData(
 #if BUILDFLAG(IS_CHROMEOS)
       video_frame_init_data.is_webgpu_compatible =
           buffer_handle.native_pixmap_handle.supports_zero_copy_webgpu_import;
-#endif
-
-#if BUILDFLAG(IS_MAC)
+#elif BUILDFLAG(IS_MAC)
       video_frame_init_data.is_webgpu_compatible =
           media::IOSurfaceIsWebGPUCompatible(buffer_handle.io_surface.get());
+#elif BUILDFLAG(IS_WIN)
+      video_frame_init_data.is_webgpu_compatible =
+          buffer_handle.type == gfx::GpuMemoryBufferType::DXGI_SHARED_HANDLE;
 #endif
       // No need to propagate shared memory region further as it's already
       // exposed by |buffer_context->data()|.
@@ -735,10 +734,8 @@ bool VideoCaptureImpl::BindVideoFrameOnMediaTaskRunner(
 
   frame->metadata().allow_overlay = true;
   frame->metadata().read_lock_fences_enabled = true;
-#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_MAC)
   frame->metadata().is_webgpu_compatible =
       video_frame_init_data.is_webgpu_compatible;
-#endif
   video_frame_init_data.frame_or_buffer = frame;
   return true;
 }
