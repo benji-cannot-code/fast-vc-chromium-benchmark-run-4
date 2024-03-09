0243ff86f4bf00d2c91ebe457ba4f2b49d1ac43d
Colin Kincaid <ckincaid@chromium.org>
2024-03-09 03:04:23 +0000

    Mahi: Adjust scroll bar to stay within the content section
    This CL restricts the scroll view's scroll bar from overlapping with the
    content section's rounded corners or extending into the bottom-right
    corner's cutout region. This CL also changes the scroll bar from the
    scroll view default to a thinner one with rounded corners, matching the
    spec.
    
    Recording: http://shortn/_o1TgW9BBgk
    
    Fixed: b/328793911
    Change-Id: I0c8a08777e2af79400c3a1a3a7cf468cebc89727
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5356860
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270535}

--- ash/system/mahi/mahi_panel_view.cc
+++ ash/system/mahi/mahi_panel_view.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "ash/controls/rounded_scroll_bar.h"
 #include "ash/public/cpp/new_window_delegate.h"
 #include "ash/public/cpp/style/color_provider.h"
 #include "ash/resources/vector_icons/vector_icons.h"
@@ -159,6 +160,15 @@ class ContentScrollView : public views::ScrollView,
                     views::MaximumFlexSizeRule::kUnbounded)));
     ClipHeightTo(/*min_height=*/0, /*max_height=*/INT_MAX);
     SetDrawOverflowIndicator(false);
+    auto scroll_bar = std::make_unique<RoundedScrollBar>(
+        RoundedScrollBar::Orientation::kVertical);
+    // Prevent the scroll bar from overlapping with any rounded corners or
+    // extending into the cutout region.
+    scroll_bar->SetInsets(gfx::Insets::TLBR(kContentScrollViewCornerRadius, 0,
+                                            kCutoutHeight + kCutoutConvexRadius,
+                                            0));
+    scroll_bar->SetSnapBackOnDragOutside(false);
+    SetVerticalScrollBar(std::move(scroll_bar));
     SetContents(views::Builder<views::View>()
                     .SetUseDefaultFillLayout(true)
                     .AddChild(views::Builder<SummaryOutlinesSection>())
