400c6d3fbec24cea0be8967788407413b6e5233b
Steve Becker <stevebe@microsoft.com>
2024-03-09 03:17:12 +0000

    IDB: Rename databbase_impl.h/.cc to indexed_db_connection.h/.cc
    This is part two of a change that merged IndexedDBConnection and
    DatabaseImpl into a single class.
    
    See part one here:
    https://chromium-review.googlesource.com/c/chromium/src/+/5345854
    
    Bug: 41483624
    Change-Id: Ib3fa6844898543decf937c69635ff7083435f577
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5355814
    Reviewed-by: Daseul Lee <dslee@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Steve Becker <stevebe@microsoft.com>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270539}

--- content/browser/indexed_db/indexed_db_connection.h
+++ content/browser/indexed_db/indexed_db_connection.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_BROWSER_INDEXED_DB_DATABASE_IMPL_H_
-#define CONTENT_BROWSER_INDEXED_DB_DATABASE_IMPL_H_
+#ifndef CONTENT_BROWSER_INDEXED_DB_INDEXED_DB_CONNECTION_H_
+#define CONTENT_BROWSER_INDEXED_DB_INDEXED_DB_CONNECTION_H_
 
 #include <map>
 #include <memory>
@@ -246,4 +246,4 @@ class CONTENT_EXPORT IndexedDBConnection : public blink::mojom::IDBDatabase {
 
 }  // namespace content
 
-#endif  // CONTENT_BROWSER_INDEXED_DB_DATABASE_IMPL_H_
+#endif  // CONTENT_BROWSER_INDEXED_DB_INDEXED_DB_CONNECTION_H_
