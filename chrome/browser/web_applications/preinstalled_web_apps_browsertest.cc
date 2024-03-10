03c46c79a8de5210f0b02b5c29e21ee1a37c91a7
Tim Sergeant <tsergeant@chromium.org>
2024-03-10 23:45:18 +0000

    [Mall] Remove offline install information
    We will rely on an online preload to allow Manifest attributes to more
    easily be changed server-side.
    
    Bug: b/327080071
    Change-Id: I5fabce9a0560d6facf50141d360de81631de95b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5348264
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270748}

--- chrome/browser/web_applications/preinstalled_web_apps_browsertest.cc
+++ chrome/browser/web_applications/preinstalled_web_apps_browsertest.cc
@@ -101,11 +101,6 @@ IN_PROC_BROWSER_TEST_P(PreinstalledWebAppsBrowserTest, CheckInstalledFields) {
           "installwebapp?usp=chrome_default",
           "https://calendar.google.com/calendar/r?usp=installed_webapp",
       },
-      {
-          kMallAppId,
-          "https://discover.apps.chrome/install/",
-          "https://discover.apps.chrome/",
-      },
 #endif  // BUILDFLAG(IS_CHROMEOS)
       {
           kGoogleDocsAppId,
@@ -157,6 +152,9 @@ IN_PROC_BROWSER_TEST_P(PreinstalledWebAppsBrowserTest, CheckInstalledFields) {
       },
       {
           "https://calculator.apps.chrome/install",
+      },
+      {
+          "https://discover.apps.chrome/install/",
       },
 #endif  // BUILDFLAG(IS_CHROMEOS)
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
