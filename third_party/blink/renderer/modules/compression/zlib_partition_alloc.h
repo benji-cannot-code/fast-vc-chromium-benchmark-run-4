f10b5ebc8d0fa3df329de54f6567d1dbe6f5d777
Kalvin Lee <kdlee@chromium.org>
2024-03-10 17:57:49 +0000

    PA: Prefer just "PartitionAlloc"
    Common parlance has favored "PartitionAlloc" for quite a while now. This
    best-effort CL was constructed from a naive `git grep` to replace other
    spellings (e.g. "partition alloc," "Partition Alloc," "partition
    allocator," etc.) with the most common one.
    
    This change was created with `git cl split` and is scoped to
    /third_party/blink/renderer/modules/compression.
    
    This CL was uploaded by git cl split.
    
    R=ricea@chromium.org
    
    Change-Id: I1564d2835a0b6bf9bc584f78a662ad1a93ea00c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5353571
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270727}

--- third_party/blink/renderer/modules/compression/zlib_partition_alloc.h
+++ third_party/blink/renderer/modules/compression/zlib_partition_alloc.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-// Use partition alloc for zlib allocations.
+// Use PartitionAlloc for zlib allocations.
 class ZlibPartitionAlloc {
   STATIC_ONLY(ZlibPartitionAlloc);
 
