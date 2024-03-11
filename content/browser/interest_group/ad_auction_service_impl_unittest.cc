a1e55516aabcde3652261d5cc084d00670211e87
Rakina Zata Amni <rakina@chromium.org>
2024-03-11 20:41:35 +0000

    Update chrome/ and content/ tests to expect same-site RenderFrameHost changes
    These tests expected the RenderFrameHost to stay the same after
    navigation, leading to failed expectations or dangling ptrs.
    
    The CL updates the expectations, how we keep track of the ptrs,
    or even skips the tests that no longer make sense with RenderDocument
    enabled.
    
    Bug: 936696
    Change-Id: Ie7897fce62638215074d52d94a2eece372485c86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5358539
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271154}

--- content/browser/interest_group/ad_auction_service_impl_unittest.cc
+++ content/browser/interest_group/ad_auction_service_impl_unittest.cc
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/private_aggregation/private_aggregation_test_utils.h"
 #include "content/browser/renderer_host/render_frame_host_impl.h"
 #include "content/browser/storage_partition_impl.h"
+#include "content/common/content_navigation_policy.h"
 #include "content/common/features.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/privacy_sandbox_invoking_api.h"
@@ -8779,6 +8780,11 @@ TEST_F(AdAuctionServiceImplTest, PageImplChangedBeforeAuction) {
 // TODO(crbug.com/936696): Once RenderDocument is launched, remove this test.
 TEST_F(AdAuctionServiceImplTest,
        ResetAuctionInitiatorPageOnCrossDocumentNavigation) {
+  if (ShouldCreateNewRenderFrameHostOnSameSiteNavigation(
+          /*is_main_frame=*/false,
+          /*is_local_root=*/AreAllSitesIsolatedForTesting())) {
+    GTEST_SKIP() << "RenderDocument is enabled.";
+  }
   content::RenderFrameHostTester* rfh_tester =
       content::RenderFrameHostTester::For(main_rfh());
   content::RenderFrameHost* subframe = rfh_tester->AppendChild("subframe");
