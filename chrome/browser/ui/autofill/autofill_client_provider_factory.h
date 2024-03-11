842d894a0c471ac1e78c25c60b8edc6f6005e8b3
Friedrich Horschig <fhorschig@chromium.org>
2024-03-11 13:19:00 +0000

    [Android][3PPWM] Make provider available in guest/ash
    The client is needed in guest/ash profiles, too. E.g. for
    autocomplete.
    The provider shouldn't be nullable but available for every profile that
    needs it. Therefore, make it a reference.
    
    Bug: 326231439
    Change-Id: Ifd9736cb195f66a6f5dbedcdf61bb1c0a60186bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5360916
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270912}

--- chrome/browser/ui/autofill/autofill_client_provider_factory.h
+++ chrome/browser/ui/autofill/autofill_client_provider_factory.h
@@ -19,7 +19,7 @@ class AutofillClientProvider;
 class AutofillClientProviderFactory : public ProfileKeyedServiceFactory {
  public:
   static AutofillClientProviderFactory* GetInstance();
-  static AutofillClientProvider* GetForProfile(Profile* profile);
+  static AutofillClientProvider& GetForProfile(Profile* profile);
 
   AutofillClientProviderFactory(const AutofillClientProviderFactory&) = delete;
   AutofillClientProviderFactory& operator=(
