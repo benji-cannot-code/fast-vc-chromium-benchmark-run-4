6e829131c2ca28a2d424f56a6343360f4c7d2812
Liam Brady <lbrady@google.com>
2024-03-11 16:07:39 +0000

    Fix sandbox top navigation tests using external URLs.
    The sandbox top navigation WPTs currently attempt a top-level navigation
    to 'https://google.com'. Not only is using external URLs bad practice,
    Firefox's test harness code refuses to make requests to remote URLs.
    This causes these tests to crash in their trybots.
    
    This CL updates the tests to point to the WPT-supported URL
    'http://{{hosts[alt][www2]}}:{{ports[http][0]}}'. This is chosen so the
    URL will be cross-site to the URLs the main page and the iframes will
    be navigated to in the various tests.
    
    See this comment on the WPT GitHub repo:
    https://github.com/web-platform-tests/wpt/pull/35697#issuecomment-1970026245
    
    Change-Id: I8b893d5c61c91d76acf15e47c4fe42c28a09cca5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5357673
    Reviewed-by: Garrett Tanzer <gtanzer@chromium.org>
    Commit-Queue: Liam Brady <lbrady@google.com>
    Cr-Commit-Position: refs/heads/main@{#1270985}

--- third_party/blink/web_tests/external/wpt/html/semantics/embedded-content/the-iframe-element/resources/sandbox-top-navigation-helper.sub.js
+++ third_party/blink/web_tests/external/wpt/html/semantics/embedded-content/the-iframe-element/resources/sandbox-top-navigation-helper.sub.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // // META: script=/resources/testdriver.js
 // // META: script=/resources/testdriver-vendor.js
 // // META: script=/html/browsers/browsing-the-web/remote-context-helper/resources/remote-context-helper.js
-// // META: script=./resources/sandbox-top-navigation-helper.js
+// // META: script=./resources/sandbox-top-navigation-helper.sub.js
 
 // Helper file that provides various functions to test top-level navigation
 // with various frame and sandbox flag configurations.
@@ -28,8 +28,7 @@ async function createNestedIframe(parent, origin, frame_sandbox, header_sandbox)
     origin: origin,
     scripts: [
       '/resources/testdriver.js',
-      '/resources/testdriver-driver.js',
-      '/resources/testdriver-vendor.js'
+      '/resources/testdriver-vendor.js',
     ],
     headers: headers,
   }, iframe_attributes);
@@ -40,8 +39,7 @@ async function navigateFrameTo(frame, origin) {
     origin: origin,
     scripts: [
       '/resources/testdriver.js',
-      '/resources/testdriver-driver.js',
-      '/resources/testdriver-vendor.js'
+      '/resources/testdriver-vendor.js',
     ],
   });
 }
@@ -50,7 +48,8 @@ async function attemptTopNavigation(iframe, should_succeed) {
   let did_succeed;
   try {
     await iframe.executeScript(() => {
-      window.top.location.href = "https://google.com";
+      window.top.location.href =
+          'http://{{hosts[alt][www2]}}:{{ports[http][0]}}';
     });
     did_succeed = true;
   } catch (e) {
--- third_party/blink/web_tests/external/wpt/html/semantics/embedded-content/the-iframe-element/sandbox-top-navigation-child.tentative.sub.window.js
+++ third_party/blink/web_tests/external/wpt/html/semantics/embedded-content/the-iframe-element/sandbox-top-navigation-child.tentative.sub.window.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=/resources/testdriver.js
 // META: script=/resources/testdriver-vendor.js
 // META: script=/html/browsers/browsing-the-web/remote-context-helper/resources/remote-context-helper.js
-// META: script=./resources/sandbox-top-navigation-helper.js
+// META: script=./resources/sandbox-top-navigation-helper.sub.js
 
 'use strict';
 
