0f8fd47953e5de7d7847f4fbdfd78436a2b5c589
Abigail Klein <abigailbklein@google.com>
2024-03-11 23:36:06 +0000

    Add an AXMode for AXMainNodeAnnotator.
    Bug: 327248475
    Change-Id: Iea0920d5f7c5d5f352b7b44e9c2f6af62c55c86d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5353675
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1271251}

--- content/renderer/accessibility/annotations/ax_main_node_annotator.cc
+++ content/renderer/accessibility/annotations/ax_main_node_annotator.cc
@@ -43,8 +43,7 @@ void AXMainNodeAnnotator::CancelAnnotations() {
 }
 
 uint32_t AXMainNodeAnnotator::GetAXModeToEnableAnnotations() {
-  // TODO: Change to the AXMode for main node.
-  return ui::AXMode::kLabelImages;
+  return ui::AXMode::kAnnotateMainNode;
 }
 
 ax::mojom::Action AXMainNodeAnnotator::GetAXActionToEnableAnnotations() {
