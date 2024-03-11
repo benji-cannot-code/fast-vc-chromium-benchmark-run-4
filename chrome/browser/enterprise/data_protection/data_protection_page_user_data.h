883589fbe83f437b6acd0e8fb66b4c09e3044dd0
Dominique Fauteux-Chapleau <domfc@chromium.org>
2024-03-11 20:22:33 +0000

    Add reporting logic for audit-only watermark URL filtering verdicts
    This CL:
    - Adds extra logic to cache verdicts alongside the watermark string in
      `DataProtectionPageUserData` so that it can later be retrieved to
      populate reports
    - Adds logic in DataProtectionNavigationObserver to use that verdict.
    - Tweaks `SafeBrowsingPrivateEventRouter::OnUrlFilteringInterstitial` to
      handle audit-only URL filtering events
    
    Change-Id: I362acac66bfcfad130fb988cafeb3fbec5a305bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5356219
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271144}

--- chrome/browser/enterprise/data_protection/data_protection_page_user_data.h
+++ chrome/browser/enterprise/data_protection/data_protection_page_user_data.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "components/safe_browsing/core/common/proto/realtimeapi.pb.h"
 #include "content/public/browser/page_user_data.h"
 
 namespace enterprise_data_protection {
@@ -27,8 +28,10 @@ class DataProtectionPageUserData
   // main RFH.  During navigations this should only be called after the page is
   // ready to be committed, otherwise the state will be saved to an intermediate
   // Page.
-  static void UpdateDataProtectionState(content::Page& page,
-                                        const std::string& watermark_text);
+  static void UpdateDataProtectionState(
+      content::Page& page,
+      const std::string& watermark_text,
+      std::unique_ptr<safe_browsing::RTLookupResponse> rt_lookup_response);
 
   ~DataProtectionPageUserData() override;
 
@@ -37,13 +40,24 @@ class DataProtectionPageUserData
   }
   const std::string& watermark_text() { return watermark_text_; }
 
+  void set_rt_lookup_response(
+      std::unique_ptr<safe_browsing::RTLookupResponse> rt_lookup_response) {
+    rt_lookup_response_ = std::move(rt_lookup_response);
+  }
+  const safe_browsing::RTLookupResponse* rt_lookup_response() {
+    return rt_lookup_response_.get();
+  }
+
  private:
   friend class content::PageUserData<DataProtectionPageUserData>;
 
-  DataProtectionPageUserData(content::Page& page,
-                             const std::string& watermark_text);
+  DataProtectionPageUserData(
+      content::Page& page,
+      const std::string& watermark_text,
+      std::unique_ptr<safe_browsing::RTLookupResponse> rt_lookup_response);
 
   std::string watermark_text_;
+  std::unique_ptr<safe_browsing::RTLookupResponse> rt_lookup_response_;
 
   PAGE_USER_DATA_KEY_DECL();
 };
