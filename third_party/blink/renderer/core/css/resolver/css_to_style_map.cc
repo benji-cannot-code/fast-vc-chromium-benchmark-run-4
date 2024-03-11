a36fd76f2ae814e2d652259f7661eeaf67652075
Daniil Sakhapov <sakhapov@chromium.org>
2024-03-11 10:38:42 +0000

    Use length_resolver in ComputeDegree for MapAnimationDelay
    Bug: 40946458
    Change-Id: I46bd6af2c149a26544dca07da677a224364a94bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5359994
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270859}

--- third_party/blink/renderer/core/css/resolver/css_to_style_map.cc
+++ third_party/blink/renderer/core/css/resolver/css_to_style_map.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_repeat_style_value.h"
 #include "third_party/blink/renderer/core/css/css_scroll_value.h"
 #include "third_party/blink/renderer/core/css/css_timing_function_value.h"
+#include "third_party/blink/renderer/core/css/css_to_length_conversion_data.h"
 #include "third_party/blink/renderer/core/css/css_value_pair.h"
 #include "third_party/blink/renderer/core/css/css_view_value.h"
 #include "third_party/blink/renderer/core/css/resolver/style_builder_converter.h"
@@ -321,9 +322,11 @@ void CSSToStyleMap::MapFillPositionY(StyleResolverState& state,
 
 namespace {
 
-Timing::Delay MapAnimationTimingDelay(const CSSValue& value) {
+Timing::Delay MapAnimationTimingDelay(const CSSLengthResolver& length_resolver,
+                                      const CSSValue& value) {
   if (const auto* primitive = DynamicTo<CSSPrimitiveValue>(value)) {
-    return Timing::Delay(AnimationTimeDelta(primitive->ComputeSeconds()));
+    return Timing::Delay(
+        AnimationTimeDelta(primitive->ComputeSeconds(length_resolver)));
   }
 
   return Timing::Delay();
@@ -333,16 +336,18 @@ Timing::Delay MapAnimationTimingDelay(const CSSValue& value) {
 
 Timing::Delay CSSToStyleMap::MapAnimationDelayStart(StyleResolverState& state,
                                                     const CSSValue& value) {
-  return MapAnimationTimingDelay(value);
+  return MapAnimationTimingDelay(state.CssToLengthConversionData(), value);
 }
 
 Timing::Delay CSSToStyleMap::MapAnimationDelayEnd(const CSSValue& value) {
-  return MapAnimationTimingDelay(value);
+  // Note: using default length resolver here, as this function is only
+  // called from the serialization code.
+  return MapAnimationTimingDelay(CSSToLengthConversionData(), value);
 }
 
 Timing::Delay CSSToStyleMap::MapAnimationDelayEnd(StyleResolverState& state,
                                                   const CSSValue& value) {
-  return MapAnimationDelayEnd(value);
+  return MapAnimationTimingDelay(state.CssToLengthConversionData(), value);
 }
 
 Timing::PlaybackDirection CSSToStyleMap::MapAnimationDirection(
