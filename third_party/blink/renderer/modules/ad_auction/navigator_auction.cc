1b783a956df717c6a23e323d7058b28650549d4e
Alex Turner <alexmt@chromium.org>
2024-03-11 17:35:10 +0000

    Mark auctionReportBuyers debug mode experimental
    Also fixes feature checking in validation code, given that simply
    marking a field as RuntimeEnabled does not affect certain checks. Also
    adds the newly disabled feature to virtual test suites to keep WPTs
    passing.
    
    Bug: 329032156
    Change-Id: I8bcb20e26df1feb46f542f2a64d7012110be5ac9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5359415
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Qingxin Wu <qingxinwu@google.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Auto-Submit: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271043}

--- third_party/blink/renderer/modules/ad_auction/navigator_auction.cc
+++ third_party/blink/renderer/modules/ad_auction/navigator_auction.cc
@@ -2140,7 +2140,10 @@ bool CopyAuctionReportBuyerDebugModeConfigFromIdlToMojo(
     ExceptionState& exception_state,
     const AuctionAdConfig& input,
     mojom::blink::AuctionAdConfig& output) {
-  if (!input.hasAuctionReportBuyerDebugModeConfig()) {
+  if (!base::FeatureList::IsEnabled(
+          blink::features::
+              kPrivateAggregationAuctionReportBuyerDebugModeConfig) ||
+      !input.hasAuctionReportBuyerDebugModeConfig()) {
     return true;
   }
 
