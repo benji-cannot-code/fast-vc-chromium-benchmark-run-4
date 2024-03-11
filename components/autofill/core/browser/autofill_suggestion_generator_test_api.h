53face3997c641588f56b780aaa8ca438c1829ec
Bruno Braga <brunobraga@google.com>
2024-03-11 12:58:27 +0000

    [mullet m3] Profiles are not filtered when manual fallback is used.
    When Autofill is triggered from the context menu (manual fallback),
    profiles are not filtered out unless the triggering field is of type
    address AND a profile does not have data to fill it. This is done so
    that we do not end up with a suggestion without a main text.
    
    In every other case, all profiles are used to create suggestions.
    
    Bug: 1521250
    Change-Id: Ibf23a2b23d02ca5e2f4e3f7ad0276ebb5c1c4bce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5343820
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1270902}

--- components/autofill/core/browser/autofill_suggestion_generator_test_api.h
+++ components/autofill/core/browser/autofill_suggestion_generator_test_api.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/core/browser/autofill_suggestion_generator.h"
 #include "components/autofill/core/browser/field_types.h"
+#include "components/autofill/core/common/aliases.h"
 #include "components/autofill/core/common/form_field_data.h"
 
 namespace autofill {
@@ -20,12 +21,16 @@ class AutofillSuggestionGeneratorTestApi {
       : suggestion_generator_(suggestion_generator) {}
 
   std::vector<raw_ptr<const AutofillProfile, VectorExperimental>>
-  GetProfilesToSuggest(FieldType trigger_field_type,
-                       const std::u16string& field_contents,
-                       bool field_is_autofilled,
-                       const FieldTypeSet& field_types) {
+  GetProfilesToSuggest(
+      FieldType trigger_field_type,
+      const std::u16string& field_contents,
+      bool field_is_autofilled,
+      const FieldTypeSet& field_types,
+      AutofillSuggestionTriggerSource trigger_source =
+          AutofillSuggestionTriggerSource::kFormControlElementClicked) {
     return suggestion_generator_->GetProfilesToSuggest(
-        trigger_field_type, field_contents, field_is_autofilled, field_types);
+        trigger_field_type, field_contents, field_is_autofilled, field_types,
+        trigger_source);
   }
 
   std::vector<CreditCard> GetOrderedCardsToSuggest(
