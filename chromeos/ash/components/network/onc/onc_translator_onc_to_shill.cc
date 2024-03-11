f09a84c5562c0f0afc5bd3c6158db54277dfd35e
Michael Rygiel <michaelrygiel@google.com>
2024-03-11 22:10:19 +0000

    onc_to_shill: Add translation for CheckCaptivePortal
    This CL adds a translation table for CheckCaptivePortal values.
    The values do not currently exist in dbus and will be changed to
    the constants later.
    
    Bug: b/318370582
    Test: chromeos_components_unittests
    Change-Id: I1fffc409af28a1d2fd302d29af481cbbcb0f56d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5350155
    Reviewed-by: Nikhil Nayunigari <nikhilcn@google.com>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Michael Rygiel <michaelrygiel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1271207}

--- chromeos/ash/components/network/onc/onc_translator_onc_to_shill.cc
+++ chromeos/ash/components/network/onc/onc_translator_onc_to_shill.cc
@@ -491,6 +491,13 @@ void LocalTranslator::TranslateNetworkConfiguration() {
     shill_dictionary_->Set(shill::kProxyConfigProperty, proxy_config_str);
   }
 
+  const std::string* checkCaptivePortal =
+      onc_object_->FindString(::onc::network_config::kCheckCaptivePortal);
+  if (checkCaptivePortal) {
+    TranslateWithTableAndSet(*checkCaptivePortal,
+                             kCheckCaptivePortalTranslationTable,
+                             shill::kCheckPortalProperty);
+  }
   CopyFieldsAccordingToSignature();
 }
 
