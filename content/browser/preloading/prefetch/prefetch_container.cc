bea43faaa9c82952c4b22b48ee3bd62e0a835460
Taiyo Mizuhashi <taiyo@chromium.org>
2024-03-11 17:04:10 +0000

    Introduce IsRendererInitiated to prefetch
    This CL introduces helper function IsRendererInitiated() to
    PrefetchType and PrefetchContainer.
    This will be mainly used in upcoming CL to branch the flow between
    renderer-initiated triggers and browser-initiated triggers.
    
    Bug: 40946257
    Change-Id: I8009ff1162c55e57fa9a0ec5b3d434514ab556d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5307915
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271014}

--- content/browser/preloading/prefetch/prefetch_container.cc
+++ content/browser/preloading/prefetch/prefetch_container.cc
@@ -426,6 +426,8 @@ PrefetchContainer::PrefetchContainer(
       attempt_(std::move(attempt)),
       initiator_devtools_navigation_token_(
           referring_render_frame_host.GetDevToolsNavigationToken()) {
+  CHECK(prefetch_type_.IsRendererInitiated());
+
   auto* web_contents =
       WebContentsImpl::FromRenderFrameHostImpl(&referring_render_frame_host);
   is_javascript_enabled_ =
@@ -1236,6 +1238,10 @@ GURL PrefetchContainer::GetCurrentURL() const {
 
 GURL PrefetchContainer::GetPreviousURL() const {
   return GetPreviousSinglePrefetchToPrefetch().url_;
+}
+
+bool PrefetchContainer::IsRendererInitiated() const {
+  return prefetch_type_.IsRendererInitiated();
 }
 
 bool PrefetchContainer::IsIsolatedNetworkContextRequiredForCurrentPrefetch()
--- content/browser/preloading/prefetch/prefetch_type.cc
+++ content/browser/preloading/prefetch/prefetch_type.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <tuple>
 
 #include "base/check.h"
+#include "content/browser/preloading/prefetch/prefetch_params.h"
 #include "content/browser/preloading/preloading_trigger_type_impl.h"
 #include "content/public/browser/preloading_trigger_type.h"
 #include "third_party/blink/public/mojom/speculation_rules/speculation_rules.mojom.h"
@@ -18,6 +19,7 @@ PrefetchType::PrefetchType(PreloadingTriggerType non_speculation_trigger_type,
                            bool use_prefetch_proxy)
     : trigger_type_(non_speculation_trigger_type),
       use_prefetch_proxy_(use_prefetch_proxy) {
+  CHECK(PrefetchBrowserInitiatedTriggersEnabled());
   CHECK(!IsSpeculationRuleType(non_speculation_trigger_type));
 }
 
@@ -38,6 +40,10 @@ void PrefetchType::SetProxyBypassedForTest() {
 blink::mojom::SpeculationEagerness PrefetchType::GetEagerness() const {
   CHECK(IsSpeculationRuleType(trigger_type_));
   return eagerness_.value();
+}
+
+bool PrefetchType::IsRendererInitiated() const {
+  return IsSpeculationRuleType(trigger_type_);
 }
 
 }  // namespace content
