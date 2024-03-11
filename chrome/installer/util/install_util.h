372fcc714aa34c6025fdefff47f853dba8d8ac4d
Javier Flores Assad <floresa@microsoft.com>
2024-03-11 19:23:09 +0000

    Scope repairing shortcuts to installers running interactively
    When the installer installs the same version as the current one, we
    check if the installer is running in the interactive user context. If it
    is, then it is safe to assume recreating the shortcuts will be fine. If
    it is not (running as system as part of a silent update), then we do not
    re-create the shortcuts.
    
    Bug: 328843997
    Change-Id: Id1e8d3f0a18fa5bab1611441fe2d8de030a979dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5357741
    Commit-Queue: Javier Flores Assad <floresa@microsoft.com>
    Reviewed-by: Mohamed Mansour <mmansour@microsoft.com>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271110}

--- chrome/installer/util/install_util.h
+++ chrome/installer/util/install_util.h
@@ -89,6 +89,10 @@ class InstallUtil {
   // CLSID registered.
   static bool IsStartMenuShortcutWithActivatorGuidInstalled();
 
+  // Returns true if the current process has the interactive user token. False
+  // otherwise.
+  static bool IsRunningAsInteractiveUser();
+
   // Returns the toast activator registry path.
   static std::wstring GetToastActivatorRegistryPath();
 
