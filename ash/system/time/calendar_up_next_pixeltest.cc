74c532bba8343bf8f4b0d4b77e7f60b522bd3dad
Greg Thompson <grt@chromium.org>
2024-03-11 13:28:34 +0000

    Disable CalendarUpNextViewPixelTest due to failures
    Bug: 328975344
    Change-Id: Ie853f82d5b4206f42d4559f42ac3e3ca0430881f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5358395
    Commit-Queue: Hamda Mare <hmare@google.com>
    Reviewed-by: Hamda Mare <hmare@google.com>
    Owners-Override: Hamda Mare <hmare@google.com>
    Owners-Override: Greg Thompson <grt@google.com>
    Cr-Commit-Position: refs/heads/main@{#1270916}

--- ash/system/time/calendar_up_next_pixeltest.cc
+++ ash/system/time/calendar_up_next_pixeltest.cc
@@ -125,8 +125,9 @@ INSTANTIATE_TEST_SUITE_P(GlanceablesV2,
                          CalendarUpNextViewPixelTest,
                          testing::Bool());
 
+// Disabled due to pixel test failures; see https://crbug.com/328975344.
 TEST_P(CalendarUpNextViewPixelTest,
-       ShouldShowSingleEventTakingUpFullWidthOfParentView) {
+       DISABLED_ShouldShowSingleEventTakingUpFullWidthOfParentView) {
   // Set time override.
   base::subtle::ScopedTimeClockOverrides time_override(
       []() { return base::subtle::TimeNowIgnoringOverride().LocalMidnight(); },
@@ -147,8 +148,9 @@ TEST_P(CalendarUpNextViewPixelTest,
       /*revision_number=*/8, Widget()));
 }
 
+// Disabled due to pixel test failures; see https://crbug.com/328975344.
 TEST_P(CalendarUpNextViewPixelTest,
-       ShouldShowMultipleEventsInHorizontalScrollView) {
+       DISABLED_ShouldShowMultipleEventsInHorizontalScrollView) {
   // Set time override.
   base::subtle::ScopedTimeClockOverrides time_override(
       []() { return base::subtle::TimeNowIgnoringOverride().LocalMidnight(); },
@@ -171,9 +173,10 @@ TEST_P(CalendarUpNextViewPixelTest,
       /*revision_number=*/8, Widget()));
 }
 
+// Disabled due to pixel test failures; see https://crbug.com/328975344.
 TEST_P(
     CalendarUpNextViewPixelTest,
-    ShouldMakeSecondEventFullyVisibleAndLeftAligned_WhenScrollRightButtonIsPressed) {
+    DISABLED_ShouldMakeSecondEventFullyVisibleAndLeftAligned_WhenScrollRightButtonIsPressed) {
   // Set time override.
   base::subtle::ScopedTimeClockOverrides time_override(
       []() { return base::subtle::TimeNowIgnoringOverride().LocalMidnight(); },
@@ -199,7 +202,8 @@ TEST_P(
       /*revision_number=*/7, Widget()));
 }
 
-TEST_P(CalendarUpNextViewPixelTest, ShouldShowJoinMeetingButton) {
+// Disabled due to pixel test failures; see https://crbug.com/328975344.
+TEST_P(CalendarUpNextViewPixelTest, DISABLED_ShouldShowJoinMeetingButton) {
   // Set time override.
   base::subtle::ScopedTimeClockOverrides time_override(
       []() { return base::subtle::TimeNowIgnoringOverride().LocalMidnight(); },
