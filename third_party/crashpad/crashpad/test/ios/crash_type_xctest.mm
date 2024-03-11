05ad5d16510451bf9dc7193ecca2e20553e0c0d3
Justin Cohen <justincohen@google.com>
2024-03-11 15:52:06 +0000

    Update Crashpad to c4d4a4d83e864df270c267888176ffa415a599b0
    c4d4a4d83e86 ios: Disable annotations tests on older simulators on macOS
                 14.3
    
    Bug: crbug.com/328282286
    Change-Id: Ib2b6e0623a06401906e0e934e5d268e240cd229a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5354545
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270980}

--- third_party/crashpad/crashpad/test/ios/crash_type_xctest.mm
+++ third_party/crashpad/crashpad/test/ios/crash_type_xctest.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <XCTest/XCTest.h>
 #include <objc/runtime.h>
+#include <sys/sysctl.h>
 
 #include <vector>
 
@@ -24,6 +25,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "test/ios/host/cptest_shared_object.h"
 #include "util/mach/exception_types.h"
 #include "util/mach/mach_extensions.h"
+
+namespace {
+
+#if TARGET_OS_SIMULATOR
+// macOS 14.0 is 23A344, macOS 13.6.5 is 22G621, so if the first two characters
+// in the kern.osversion are > 22, this build will reproduce the simulator bug
+// in crbug.com/328282286
+bool IsMacOSVersion143OrGreaterAndiOS16OrLess() {
+  if (__builtin_available(iOS 17, *)) {
+    return false;
+  }
+
+  size_t buf_len;
+  static constexpr char name[] = "kern.osversion";
+  if (sysctlbyname(name, nullptr, &buf_len, nullptr, 0) != 0) {
+    return false;
+  }
+
+  std::string build(buf_len - 1, '\0');
+  if (sysctlbyname(name, &build[0], &buf_len, nullptr, 0) != 0) {
+    return false;
+  }
+
+  return std::stoi(build.substr(0, 2)) > 22;
+}
+#endif
+
+}  // namespace
 
 @interface CPTestTestCase : XCTestCase {
   XCUIApplication* app_;
@@ -318,6 +347,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 - (void)testCrashWithAnnotations {
+#if TARGET_OS_SIMULATOR
+  // This test will fail on older (<iOS17 simulators) when running on macOS 14.3
+  // or newer due to a bug in Simulator. crbug.com/328282286
+  if (IsMacOSVersion143OrGreaterAndiOS16OrLess()) {
+    return;
+  }
+#endif
+
   [rootObject_ crashWithAnnotations];
   [self verifyCrashReportException:EXC_SOFT_SIGNAL];
   NSNumber* report_exception;
