ff16809b1c95dfdc8a2fedf739b8449824afa686
Charles Yang <cmyang@google.com>
2024-03-11 23:49:47 +0000

    [Lens] Update color Omnibox icons to be monchrome in darkmode.
    Lightmode: https://screenshot.googleplex.com/3dWBZRHKxiS9LWT
    Darkmode: https://screenshot.googleplex.com/9eAMeAvaM55vUjZ
    Bug: b/327257072
    Change-Id: I8697865050b10abf95f722bc2bc46272fdb451a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5357213
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Charles Yang <cmyang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1271261}

--- ios/chrome/browser/ui/ntp/new_tab_page_header_view_controller.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_header_view_controller.mm
@@ -117,6 +117,14 @@ const CGFloat kFakeLocationBarHeightMargin = 2;
           self.traitCollection.preferredContentSizeCategory) {
     [self updateFakeboxDisplay];
   }
+  if (previousTraitCollection.userInterfaceStyle !=
+      self.traitCollection.userInterfaceStyle) {
+    if (base::FeatureList::IsEnabled(kOmniboxColorIcons)) {
+      [self.headerView
+          updateButtonsForUserInterfaceStyle:self.traitCollection
+                                                 .userInterfaceStyle];
+    }
+  }
 }
 
 - (void)willTransitionToTraitCollection:(UITraitCollection*)newCollection
