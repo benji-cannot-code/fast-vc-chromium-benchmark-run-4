590777cf5b93e1d305c14034ce49a4d83a63b06d
Gabriel Brito <gabrielbrito@microsoft.com>
2024-03-11 23:12:28 +0000

    [Gamepad] Add 'effects' array to the JS API
    This change implements the latest updates in the GamepadHapticActuator API introduced in https://github.com/w3c/gamepad/pull/190. Which are:
    - Add the `effects` array;
    - Emit deprecation console messages for `type` and `canPlay()`.
    
    Bug: 40834175
    Change-Id: I4d77441887c3e4d4d98f65668517d98a8a0a66ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5350942
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Gabriel Brito <gabrielbrito@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1271235}

--- third_party/blink/renderer/modules/gamepad/gamepad_haptic_actuator.h
+++ third_party/blink/renderer/modules/gamepad/gamepad_haptic_actuator.h
@@ -14,9 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
-#include "third_party/blink/renderer/platform/wtf/hash_set.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_hash.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
+#include "third_party/blink/renderer/platform/wtf/vector.h"
 
 namespace blink {
 
@@ -36,6 +36,8 @@ class GamepadHapticActuator final : public ScriptWrappable,
                         device::GamepadHapticActuatorType type);
   ~GamepadHapticActuator() override;
 
+  const Vector<String>& effects() const { return supported_effects_; }
+
   const String& type() const { return type_; }
   void SetType(device::GamepadHapticActuatorType);
 
@@ -43,8 +45,6 @@ class GamepadHapticActuator final : public ScriptWrappable,
   playEffect(ScriptState*, const String&, const GamepadEffectParameters*);
 
   ScriptPromiseTyped<V8GamepadHapticsResult> reset(ScriptState*);
-
-  bool canPlay(const String& type);
 
   void Trace(Visitor*) const override;
 
@@ -59,7 +59,7 @@ class GamepadHapticActuator final : public ScriptWrappable,
   int pad_index_;
   String type_;
   bool should_reset_ = false;
-  HashSet<String> supported_effect_types_;
+  Vector<String> supported_effects_;
 
   Member<GamepadDispatcher> gamepad_dispatcher_;
 };
