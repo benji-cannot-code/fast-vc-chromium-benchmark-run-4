0f1866658aa12b23fefb6c1b28c0d863cc65b25d
Matt Jones <mdjones@chromium.org>
2024-03-11 23:38:40 +0000

    [commerce] Allow product info to fall back to on-demand if possible
    This patch allows the GetProductInfoForUrl API in the shopping service
    to fall back to the on-demand API that optimization guide provides.
    This will only be called if the user is known to have visited the URL
    that is being queried. In our case, this currently corresponds to open
    tabs but will also reference sync entities in the future.
    
    Bug: b:328622410
    Change-Id: Icecb8abb971f1860c00477e4a70d05cc26f0aa3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5356564
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271254}

--- components/commerce/core/shopping_service_test_base.h
+++ components/commerce/core/shopping_service_test_base.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "base/values.h"
+#include "components/commerce/core/commerce_info_cache.h"
 #include "components/commerce/core/shopping_service.h"
 #include "components/commerce/core/web_extractor.h"
 #include "components/commerce/core/web_wrapper.h"
@@ -228,6 +229,9 @@ class ShoppingServiceTestBase : public testing::Test {
 
   // Get the item in the product info cache if it exists.
   const ProductInfo* GetFromProductInfoCache(const GURL& url);
+
+  // Gets a handle to the cache.
+  CommerceInfoCache& GetCache();
 
  protected:
   base::test::TaskEnvironment task_environment_{
