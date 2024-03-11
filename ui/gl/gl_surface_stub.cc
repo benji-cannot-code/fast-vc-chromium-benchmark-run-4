db500b71392ad3ecea515a5bc18882cb868f3499
Maksim Sisov <msisov@igalia.com>
2024-03-11 10:41:40 +0000

    Reland "gpu: don't store GLSurface in SharedContextState"
    This is a reland of commit c5024c20a3a5f28aea3601a20e2510bea80de12b
    
    Original change's description:
    > gpu: don't store GLSurface in SharedContextState
    >
    > https://crrev.com/c/4931228 made gl::GLContext hold gl::GLSurface
    > if it's offscreen. Given SCS is supposed to always get an offscreen
    > surface , we can drop a reference to a surface there.
    >
    > Bug: 1479884
    > Change-Id: I58f90c251b4572273b3202eb19c7d112af4860e3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5255898
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Reviewed-by: Peng Huang <penghuang@chromium.org>
    > Commit-Queue: Maksim Sisov <msisov@igalia.com>
    > Cr-Commit-Position: refs/heads/main@{#1257817}
    
    Cq-Include-Trybots: luci.chromium.try:android-webview-pie-arm64-dbg
    Bug: 1479884
    Change-Id: I843c4e15d49886e4465bf77b0b7a52c29bb521d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5299814
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270861}

--- ui/gl/gl_surface_stub.cc
+++ ui/gl/gl_surface_stub.cc
@@ -24,7 +24,7 @@ bool GLSurfaceStub::Resize(const gfx::Size& size,
 }
 
 bool GLSurfaceStub::IsOffscreen() {
-  return false;
+  return true;
 }
 
 gfx::SwapResult GLSurfaceStub::SwapBuffers(PresentationCallback callback,
