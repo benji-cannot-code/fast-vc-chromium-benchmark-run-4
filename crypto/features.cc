7fef0b483833bd7ab755622868a9a50fbfac7d44
Nina Satragno <nsatragno@chromium.org>
2024-03-11 16:41:29 +0000

    [crypto] Enable mac unexportable keys
    Enable the unexportable keys macOS implementation. The only client that
    will immediately use this feature is the metrics collection code. Both
    DBSC and device trust are hardcoded to Windows for now.
    
    Bug: 326451282
    Change-Id: Ib423be508f970d43aaf60b02d5286087d891bee3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5353910
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270999}

--- crypto/features.cc
+++ crypto/features.cc
@@ -11,10 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace crypto {
 
 #if BUILDFLAG(IS_MAC)
-// Not yet enabled by default.
+// Enabled in M124. Remove in or after M127.
 BASE_FEATURE(kEnableMacUnexportableKeys,
              "EnableMacUnexportableKeys",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_MAC)
 
 }  // namespace crypto
