07bf7ad8175b37f0edc5a67048b51daa270a3796
Keren Zhu <kerenzhu@chromium.org>
2024-03-11 18:42:58 +0000

    webui: move warmup level code to top_chrome/
    WebUI warmup level code was under cbui/views/bubble but the concept is
    applicable to any top chrome WebUIs. This patch moves relevant code to
    cbui/webui/top_chrome which is a more appropriate place.
    
    Bug: 326490753, 40168622
    Change-Id: Ibe7b7bb8289b9cd1eee94b8ab04f3a40f84783b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5357682
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271085}

--- chrome/browser/ui/views/tab_search_bubble_host.cc
+++ chrome/browser/ui/views/tab_search_bubble_host.cc
@@ -95,7 +95,7 @@ void TabSearchBubbleHost::OnWidgetVisibilityChanged(views::Widget* widget,
         base::BindOnce(
             [](base::TimeTicks bubble_created_time,
                bool bubble_using_cached_web_contents,
-               WebUIBubbleWarmUpLevel bubble_warmup_level,
+               WebUIContentsWarmupLevel contents_warmup_level,
                const viz::FrameTimingDetails& frame_timing_details) {
               base::TimeTicks presentation_timestamp =
                   frame_timing_details.presentation_feedback.timestamp;
@@ -108,12 +108,12 @@ void TabSearchBubbleHost::OnWidgetVisibilityChanged(views::Widget* widget,
                   time_to_show);
               base::UmaHistogramMediumTimes(
                   base::StrCat({"Tabs.TabSearch.TimeToShow.",
-                                ToString(bubble_warmup_level)}),
+                                ToString(contents_warmup_level)}),
                   time_to_show);
             },
             *bubble_created_time_,
             webui_bubble_manager_.bubble_using_cached_web_contents(),
-            webui_bubble_manager_.bubble_warmup_level()));
+            webui_bubble_manager_.contents_warmup_level()));
     bubble_created_time_.reset();
   }
 }
--- chrome/browser/ui/webui/top_chrome/webui_contents_wrapper.cc
+++ chrome/browser/ui/webui/top_chrome/webui_contents_wrapper.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/input/native_web_keyboard_event.h"
 #include "ui/base/models/menu_model.h"
 #include "ui/gfx/geometry/rounded_corners_f.h"
-#include "ui/views/widget/widget.h"
 
 namespace {
 
