d6bbd82bfb29c703d01088965d00fa0e39ed748c
Maggie Chen <magchen@chromium.org>
2024-03-11 17:38:19 +0000

    Put GetVideoFrameCompositorSharedImageInterface behind a flag.
    The function GetVideoFrameCompositorSharedImageInterface is
    intended for kSharedBitmapToSharedImage and
    kMediaSharedBitmapToSharedImage only.
    
    Bug: 40064122
    Change-Id: I8c40ea499dc4b50b2f54893bfccd097f86f4a87d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5350585
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271047}

--- media/renderers/video_resource_updater.cc
+++ media/renderers/video_resource_updater.cc
@@ -66,10 +66,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 namespace {
 
-BASE_FEATURE(kMediaSharedBitmapToSharedImage,
-             "MediaSharedBitmapToSharedImage_DoNotEnable",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 bool MediaSharedBitmapConversionEnabled() {
   return base::FeatureList::IsEnabled(features::kSharedBitmapToSharedImage) &&
          base::FeatureList::IsEnabled(kMediaSharedBitmapToSharedImage);
