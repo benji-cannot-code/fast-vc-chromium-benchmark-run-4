bce35fedc3f6e7d204285cbff0e3f7c0a1ceb187
Cheick Cisse <cheickcisse@google.com>
2024-03-11 12:52:17 +0000

    [PriceInsightsIos] Create UI classes skeleton
    This CL creates few skeleton classes for PriceInsights UI.
    
    Bug: b/326449454
    Change-Id: Id180f150990286a5b0420fc2f0d6316972c007a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5351805
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1270898}

---
+++ ios/chrome/browser/price_insights/coordinator/price_insights_modulator.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_PRICE_INSIGHTS_COORDINATOR_PRICE_INSIGHTS_MODULATOR_H_
+#define IOS_CHROME_BROWSER_PRICE_INSIGHTS_COORDINATOR_PRICE_INSIGHTS_MODULATOR_H_
+
+#import <UIKit/UIKit.h>
+
+// A coordinator-like object for Price Insights integrated into the contextual
+// panel as an info block.
+@interface PriceInsightsModulator : NSObject
+
+// Starts the modulator.
+- (void)start;
+
+// Stops the modulator.
+- (void)stop;
+
+// Return the Price Insights UICollectionViewCell registration.
+- (UICollectionViewCellRegistration*)cellRegistration;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_PRICE_INSIGHTS_COORDINATOR_PRICE_INSIGHTS_MODULATOR_H_
---
+++ ios/chrome/browser/price_insights/ui/price_insights_cell.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_PRICE_INSIGHTS_UI_PRICE_INSIGHTS_CELL_H_
+#define IOS_CHROME_BROWSER_PRICE_INSIGHTS_UI_PRICE_INSIGHTS_CELL_H_
+
+#import <UIKit/UIKit.h>
+
+#import "ios/chrome/browser/price_insights/ui/price_insights_item.h"
+
+// UICollectionViewCell that contains data for Price Insights.
+@interface PriceInsightsCell : UICollectionViewCell
+
+// Configures the UICollectionViewCell with `PriceInsightsitem`.
+- (void)configureWithItem:(PriceInsightsItem*)item;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_PRICE_INSIGHTS_UI_PRICE_INSIGHTS_CELL_H_
