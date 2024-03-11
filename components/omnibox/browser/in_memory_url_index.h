50522f596202bd7ca57ece18eaf3f8d8c468d67f
Tommy C. Li <tommycli@chromium.org>
2024-03-11 19:19:22 +0000

    [omnibox] Remove two old History.InMemoryURLIndexingTime UMAs
    Removes:
    History.InMemoryURLIndexingTime
    History.InMemoryURLIndexingTime.RoundTripTime
    
    No one is looking at either of these anymore.
    
    Bug: 40940468, 40940469
    Change-Id: Ie053ec5b36184b56497321a48edb8b8fef3a19b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5353380
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271106}

--- components/omnibox/browser/in_memory_url_index.h
+++ components/omnibox/browser/in_memory_url_index.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "base/task/cancelable_task_tracker.h"
 #include "base/threading/thread_checker.h"
-#include "base/time/time.h"
 #include "base/trace_event/memory_dump_provider.h"
 #include "components/history/core/browser/history_db_task.h"
 #include "components/history/core/browser/history_service.h"
@@ -141,9 +140,6 @@ class InMemoryURLIndex : public KeyedService,
     SchemeSet scheme_allowlist_;  // Schemes to be indexed.
     bool succeeded_ = false;      // Indicates if the rebuild was successful.
     scoped_refptr<URLIndexPrivateData> data_;  // The rebuilt private data.
-    // When the task was first requested from the main thread. This is the same
-    // time as when this task object is constructed.
-    const base::TimeTicks task_creation_time_;
   };
 
   // Clears the in-memory cache entirely. Called when History is cleared.
