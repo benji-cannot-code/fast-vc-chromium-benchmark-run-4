5e11f1c4d06bf3415adda8bba969284b9d718cae
conniekxu <conniekxu@chromium.org>
2024-03-12 16:44:37 +0000

    CrOS Settings: Redirect Files route to SystemPreferences route
    When OsSettingsRevampWayfinding is enabled, redirect Files route to
    SystemPreferences route.
    
    Before fix:
    https://screencast.googleplex.com/cast/NTY3MDM2OTE4Nzg1NjM4NHwxYzZjNzI3Mi0xZg
    After fix:
    https://screencast.googleplex.com/cast/NTAwMDc2NTczNDEyNTU2OHwxZDQ0YmUyNC1jOQ
    
    Bug: b/329167794
    Test: browser_tests --gtest_filter="OSSettings*Router*"
    Change-Id: Id5d6c56f0afe8c379472953f8860c0a50027e927
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5364512
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Connie Xu <conniekxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271617}

--- chrome/browser/resources/ash/settings/os_settings_routes.ts
+++ chrome/browser/resources/ash/settings/os_settings_routes.ts
@@ -777,6 +777,10 @@ const PATH_REDIRECT_PAIRS: Array<[string, string]> = [
     routesMojom.DATE_AND_TIME_SECTION_PATH,
     routesMojom.SYSTEM_PREFERENCES_SECTION_PATH,
   ],
+  [
+    routesMojom.FILES_SECTION_PATH,
+    routesMojom.SYSTEM_PREFERENCES_SECTION_PATH,
+  ],
 ];
 
 /**
