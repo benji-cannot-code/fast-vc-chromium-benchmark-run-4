5a7efd5e78691bbb8f37a95d1a2819171cbc2de5
Adam Walls <avvall@google.com>
2024-03-12 22:07:57 +0000

    Posting registerLocaleChangeReceiver to BG thread
    This is in an effort to move as many calls to
    ContextUtils#registerProtectedBroadcastReceiver off of the UIThread.
    This is part of an effort to improve runtime performance by reducing
    the number of IActivityManager binder transactions in the UIThread.
    
    Bug: b:325352920
    Change-Id: I61ea80fbaa26929d96aaf5673ae7d467c0412b56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5366686
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Adam Walls <avvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271847}

--- content/public/android/java/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityImpl.java
@@ -92,6 +92,8 @@ import org.chromium.base.StrictModeContext;
 import org.chromium.base.TraceEvent;
 import org.chromium.base.UserData;
 import org.chromium.base.metrics.RecordHistogram;
+import org.chromium.base.task.PostTask;
+import org.chromium.base.task.TaskTraits;
 import org.chromium.build.BuildConfig;
 import org.chromium.content.browser.WindowEventObserver;
 import org.chromium.content.browser.WindowEventObserverManager;
@@ -483,7 +485,10 @@ public class WebContentsAccessibilityImpl extends AccessibilityNodeProviderCompa
                 };
 
         // Register a broadcast receiver for locale change.
-        if (mView.isAttachedToWindow()) registerLocaleChangeReceiver();
+        if (mView.isAttachedToWindow()) {
+            PostTask.postTask(
+                    TaskTraits.BEST_EFFORT_MAY_BLOCK, () -> registerLocaleChangeReceiver());
+        }
 
         // Define a set of relevant AccessibilityEvents.
         Runnable serviceMaskRunnable =
@@ -691,7 +696,7 @@ public class WebContentsAccessibilityImpl extends AccessibilityNodeProviderCompa
             mCaptioningController.startListening();
         }
 
-        registerLocaleChangeReceiver();
+        PostTask.postTask(TaskTraits.BEST_EFFORT_MAY_BLOCK, () -> registerLocaleChangeReceiver());
         TraceEvent.end("WebContentsAccessibilityImpl.onAttachedToWindow");
     }
 
