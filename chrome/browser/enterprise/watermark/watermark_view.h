df10351d2e65bc4c8d1379790036efdb2b941cdb
Dominique Fauteux-Chapleau <domfc@chromium.org>
2024-03-12 17:11:23 +0000

    Refactor WatermarkView implementation to use gfx::RenderText
    Bug: b/328447915
    Change-Id: Ib74804335d73df41322d510095623049ea970fcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5362193
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271634}

--- chrome/browser/enterprise/watermark/watermark_view.h
+++ chrome/browser/enterprise/watermark/watermark_view.h
@@ -6,16 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ENTERPRISE_WATERMARK_WATERMARK_VIEW_H_
 #define CHROME_BROWSER_ENTERPRISE_WATERMARK_WATERMARK_VIEW_H_
 
-#include "third_party/skia/include/core/SkTextBlob.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/view.h"
 
-namespace cc {
-class PaintCanvas;
-}
-
 namespace gfx {
 class Canvas;
+class RenderText;
 }
 
 namespace enterprise_watermark {
@@ -35,6 +31,7 @@ class WatermarkView : public views::View {
   // view's bounds when performing transformations.
   void SetBackgroundColor(SkColor color);
 
+  // `text` must be UTF-8 encoded.
   void SetString(const std::string& text);
 
   // views::View
@@ -42,8 +39,8 @@ class WatermarkView : public views::View {
 
  private:
   // Helper function to draw a single block of text with the data in
-  // `text_blocks_` at the provided coordinates.
-  void DrawTextBlock(cc::PaintCanvas* canvas, int x, int y);
+  // `text_fill_` and `text_outline_` at the provided coordinates.
+  void DrawTextBlock(gfx::Canvas* canvas, int x, int y);
 
   // The width/height of individual blocks of text, including spacing.
   int block_width_offset() const;
@@ -56,10 +53,26 @@ class WatermarkView : public views::View {
   int min_y(double angle, const gfx::Rect& bounds) const;
   int max_y(double angle, const gfx::Rect& bounds) const;
 
-  std::vector<sk_sp<SkTextBlob>> text_blocks_;
+  // Background color of the whole `WatermarkView`. This is normally
+  // transparent, but can be an arbitrary color for testing with the
+  // "watermark_app" target.
   SkColor background_color_;
-  int block_width_;
-  int block_height_;
+
+  // Height/width required to draw all the lines in `text_fill_`/`text_outline_`
+  // in a single block.
+  int block_width_ = 0;
+  int block_height_ = 0;
+
+  // Height of a single line of `text_fill_` and `text_fill_`.
+  int single_line_height_ = 0;
+
+  // Containers for the fill/outline representations of each line in a single
+  // text block. This is done to avoid calling methods like
+  // `RenderText::SetText` as much as possible as that would invalidate that
+  // object's layout cache, and to avoid running into multiline issues for mixed
+  // character set cases.
+  std::vector<std::unique_ptr<gfx::RenderText>> text_fill_;
+  std::vector<std::unique_ptr<gfx::RenderText>> text_outline_;
 };
 
 }  // namespace enterprise_watermark
