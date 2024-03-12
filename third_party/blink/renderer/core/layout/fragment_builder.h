985a5a5da60c6d25c756a566b8c9e65c30bfdba1
David Awogbemila <awogbemila@chromium.org>
2024-03-12 04:20:36 +0000

    [css-scroll-snap-2] Prioritize tree-order among aligned snap targets
    FragmentBuilder::PropagateSnapAreas receives the snap areas in sorted
    order. By storing them in a vector, this order is preserved in
    SnapContainerData's snap areas, so the snap selection algorithm
    factors in tree order by default as specified[1].
    
    The change to waitForScrollReset addresses the case where the reset
    fails to happen because, given the layout configuration, setting
    scrollTop and scrollLeft individually results in noops.
    
    [1]https://github.com/w3c/csswg-drafts/issues/9622#issue-2006578282
    
    Bug: 326980489
    
    Change-Id: Ib1e2955aad909c14630dc4fc2d67da8f788ce072
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5322868
    Commit-Queue: David Awogbemila <awogbemila@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271364}

--- third_party/blink/renderer/core/layout/fragment_builder.h
+++ third_party/blink/renderer/core/layout/fragment_builder.h
@@ -512,7 +512,7 @@ class CORE_EXPORT FragmentBuilder {
   }
 
   HeapVector<Member<LayoutBoxModelObject>>& EnsureStickyDescendants();
-  HeapHashSet<Member<LayoutBox>>& EnsureSnapAreas();
+  HeapVector<Member<LayoutBox>>& EnsureSnapAreas();
   LogicalAnchorQuery& EnsureAnchorQuery();
   ScrollStartTargetCandidates& EnsureScrollStartTargets();
 
@@ -559,7 +559,7 @@ class CORE_EXPORT FragmentBuilder {
   const BreakToken* break_token_ = nullptr;
 
   HeapVector<Member<LayoutBoxModelObject>>* sticky_descendants_ = nullptr;
-  HeapHashSet<Member<LayoutBox>>* snap_areas_ = nullptr;
+  HeapVector<Member<LayoutBox>>* snap_areas_ = nullptr;
   LogicalAnchorQuery* anchor_query_ = nullptr;
   LayoutUnit bfc_line_offset_;
   std::optional<LayoutUnit> bfc_block_offset_;
