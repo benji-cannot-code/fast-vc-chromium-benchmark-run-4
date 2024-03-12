e9b5b6406a0857f0999d841176ba8382f7eb8e9e
Lucas Berthou <berlu@google.com>
2024-03-12 15:10:02 +0000

    wayland_clients: fix wayland_vulkan_client buffer format
    Vulkan doesn't seem to have an ARGB format, switching shm format to
    reflect the right format to fix vulkan rendering.
    
    Bug: b:324929240
    Test: manual
    Change-Id: Ice1ef4e0396fce018a0cc349bb52d7dd54824b5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5287559
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Commit-Queue: Lucas Berthou <berlu@chromium.org>
    Reviewed-by: Brian Ho <hob@chromium.org>
    Auto-Submit: Lucas Berthou <berlu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271561}

--- components/exo/wayland/clients/vulkan.cc
+++ components/exo/wayland/clients/vulkan.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <drm_fourcc.h>
+
 #include "base/at_exit.h"
 #include "base/command_line.h"
 #include "base/memory/raw_ptr.h"
@@ -182,6 +184,7 @@ int main(int argc, char* argv[]) {
     return 1;
 
   params.use_vulkan = true;
+  params.drm_format = DRM_FORMAT_ABGR8888;
   base::SingleThreadTaskExecutor main_task_executor(base::MessagePumpType::UI);
   exo::wayland::clients::VulkanClient client;
   client.Run(params);
