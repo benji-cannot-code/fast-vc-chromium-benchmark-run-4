a4e85cb418edbde90f8ef0499b7fadd6ea806a34
Hao Liu <haoliuk@chromium.org>
2024-03-12 16:31:55 +0000

    Use resource load timings to replace image load timings
    Currently we use fields like image_load_start to pass the LCP load
    timings as only image LCP has load timings.
    
    Now we want to expand the concept of load timings to text too. Adding
    more fields like text_load_start in parallel to image_load_start would
    be too much. It's better to just use a ResourceLoadTiming struct to wrap
    all load timing fields and the struct can be used for both image LCP
    and text LCP and petentially LCP of other elements.
    
    This CL does this replacement without material changes.
    
    Bug: 40276574
    Change-Id: I351f318dbeead5dbea6f7b0695b55b22c7e31196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5352832
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    Commit-Queue: Hao Liu <haoliuk@chromium.org>
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271613}

--- components/page_load_metrics/browser/observers/core/largest_contentful_paint_handler.cc
+++ components/page_load_metrics/browser/observers/core/largest_contentful_paint_handler.cc
@@ -303,9 +303,9 @@ void LargestContentfulPaintHandler::UpdateSoftNavigationLargestContentfulPaint(
             largest_contentful_paint.type),
         largest_contentful_paint.image_bpp,
         GetImageRequestPriority(largest_contentful_paint),
-        largest_contentful_paint.largest_image_discovery_time,
-        largest_contentful_paint.largest_image_load_start,
-        largest_contentful_paint.largest_image_load_end);
+        largest_contentful_paint.resource_load_timings->discovery_time,
+        largest_contentful_paint.resource_load_timings->load_start,
+        largest_contentful_paint.resource_load_timings->load_end);
   }
 }
 
@@ -338,9 +338,9 @@ void LargestContentfulPaintHandler::RecordMainFrameTiming(
             largest_contentful_paint.type),
         largest_contentful_paint.image_bpp,
         GetImageRequestPriority(largest_contentful_paint),
-        largest_contentful_paint.largest_image_discovery_time,
-        largest_contentful_paint.largest_image_load_start,
-        largest_contentful_paint.largest_image_load_end);
+        largest_contentful_paint.resource_load_timings->discovery_time,
+        largest_contentful_paint.resource_load_timings->load_start,
+        largest_contentful_paint.resource_load_timings->load_end);
   }
 }
 
@@ -418,11 +418,12 @@ void LargestContentfulPaintHandler::UpdateSubFrameTiming(
       /*in_main_frame=*/false,
       static_cast<blink::LargestContentfulPaintType>(
           largest_contentful_paint.type),
-      AdjustedTime(largest_contentful_paint.largest_image_discovery_time,
+      AdjustedTime(
+          largest_contentful_paint.resource_load_timings->discovery_time,
+          navigation_start_offset),
+      AdjustedTime(largest_contentful_paint.resource_load_timings->load_start,
                    navigation_start_offset),
-      AdjustedTime(largest_contentful_paint.largest_image_load_start,
-                   navigation_start_offset),
-      AdjustedTime(largest_contentful_paint.largest_image_load_end,
+      AdjustedTime(largest_contentful_paint.resource_load_timings->load_end,
                    navigation_start_offset));
 
   if (IsValid(new_image_candidate.Time())) {
