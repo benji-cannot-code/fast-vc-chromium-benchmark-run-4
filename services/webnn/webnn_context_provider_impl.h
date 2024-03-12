f18cb0da3063cb492e089a1d4242466383139317
Pooja Patel <patelpooja@microsoft.com>
2024-03-12 16:01:15 +0000

    GPU workaround to pass DML_EXECUTION_FLAG_DISABLE_META_COMMANDS flag
    This implements a GPU workaround
    'dml_execution_disable_meta_commands' that, when enabled, passes DML_EXECUTION_FLAG_DISABLE_META_COMMANDS
    flag to CompileGraph. This workaround can be used for testing to see if
    the root cause of a bug is related to bad metacommands.
    
    
    This workaround can be enabled via command line:
    --dml_execution_disable_meta_commands=1
    
    Bug: 40206287
    Change-Id: I2f7bdc80b00ebff73f81c46177c2059800188c5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5336443
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Rafael Cintron <rafael.cintron@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1271592}

--- services/webnn/webnn_context_provider_impl.h
+++ services/webnn/webnn_context_provider_impl.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/component_export.h"
+#include "gpu/config/gpu_feature_info.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "services/webnn/public/mojom/webnn_context_provider.mojom.h"
@@ -23,7 +24,7 @@ class WebNNContextImpl;
 class COMPONENT_EXPORT(WEBNN_SERVICE) WebNNContextProviderImpl
     : public mojom::WebNNContextProvider {
  public:
-  explicit WebNNContextProviderImpl(bool is_gpu_supported);
+  explicit WebNNContextProviderImpl(gpu::GpuFeatureInfo gpu_feature_info);
 
   WebNNContextProviderImpl(const WebNNContextProviderImpl&) = delete;
   WebNNContextProviderImpl& operator=(const WebNNContextProviderImpl&) = delete;
@@ -32,7 +33,7 @@ class COMPONENT_EXPORT(WEBNN_SERVICE) WebNNContextProviderImpl
 
   static void Create(
       mojo::PendingReceiver<mojom::WebNNContextProvider> receiver,
-      bool is_gpu_supported = true);
+      gpu::GpuFeatureInfo gpu_feature_info = gpu::GpuFeatureInfo());
 
   // Called when a WebNNContextImpl has a connection error. After this call, it
   // is no longer safe to access |impl|.
@@ -58,7 +59,7 @@ class COMPONENT_EXPORT(WEBNN_SERVICE) WebNNContextProviderImpl
 
   std::vector<std::unique_ptr<WebNNContextImpl>> impls_;
 
-  const bool is_gpu_supported_;
+  const gpu::GpuFeatureInfo gpu_feature_info_;
 };
 
 }  // namespace webnn
