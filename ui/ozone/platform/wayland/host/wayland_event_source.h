672b3446cdfa63b3f0fee62d8dde2ef75759398f
Georg Neis <neis@chromium.org>
2024-03-12 04:16:08 +0000

    Fix wayland unit test setup
    A previous test (if any) may have left the Wayland event source in
    "watching" state even though its message pump was already destroyed.
    This prevents the next test from watching Wayland events when
    its WindowTreeHost gets created. Fix this by resetting the event
    source in the AuraTestHelper constructor.
    
    See the detailed discussion in crbug.com/325560488.
    
    Bug: 325560488, 40199989
    Change-Id: I55da866f0bd8a707e61269b85360bb8a0d2eed82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5354632
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271360}

--- ui/ozone/platform/wayland/host/wayland_event_source.h
+++ ui/ozone/platform/wayland/host/wayland_event_source.h
@@ -96,6 +96,8 @@ class WaylandEventSource : public PlatformEventSource,
 
   void DumpState(std::ostream& out) const;
 
+  void ResetStateForTesting() override;
+
  protected:
   // WaylandKeyboard::Delegate
   void OnKeyboardFocusChanged(WaylandWindow* window, bool focused) override;
