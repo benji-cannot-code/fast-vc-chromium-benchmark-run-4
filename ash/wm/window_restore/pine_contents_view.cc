080ad68333b20cfab8b1c52b507d2b0bb579472b
Elijah Hewer <hewer@chromium.org>
2024-03-12 01:18:46 +0000

    Pine: Add highlight border and change subtext color
    Add a highlight border to PineContentsView and change the subtext
    color token to kCrosSysOnSurface.
    
    Bug: b/322360373
    Test: manual
    Change-Id: I79eb70c8973f29369cc6c41032fe3c3b4243a291
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5362897
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Elijah Hewer <hewer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271298}

--- ash/wm/window_restore/pine_contents_view.cc
+++ ash/wm/window_restore/pine_contents_view.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/menu/menu_item_view.h"
 #include "ui/views/controls/menu/menu_model_adapter.h"
 #include "ui/views/controls/menu/menu_types.h"
+#include "ui/views/highlight_border.h"
 #include "ui/views/layout/box_layout_view.h"
 #include "ui/views/view_utils.h"
 #include "ui/wm/core/window_animations.h"
@@ -94,7 +95,7 @@ PineContentsView::PineContentsView() {
                       l10n_util::GetStringUTF16(IDS_ASH_PINE_DIALOG_TITLE)),
               // Description.
               views::Builder<views::Label>()
-                  .SetEnabledColorId(cros_tokens::kCrosSysOnSurfaceVariant)
+                  .SetEnabledColorId(cros_tokens::kCrosSysOnSurface)
                   .SetFontList(gfx::FontList({"Roboto"}, gfx::Font::NORMAL,
                                              kContentsDescriptionFontSize,
                                              gfx::Font::Weight::NORMAL))
@@ -158,6 +159,12 @@ PineContentsView::PineContentsView() {
     preview->SetImage(pine_image);
     preview->SetImageSize(pine_image.size());
   }
+
+  // Add a highlight border to match the Quick Settings menu, i.e.,
+  // `TrayBubbleView`.
+  SetBorder(std::make_unique<views::HighlightBorder>(
+      kContentsRounding,
+      views::HighlightBorder::Type::kHighlightBorderOnShadow));
 }
 
 PineContentsView::~PineContentsView() = default;
