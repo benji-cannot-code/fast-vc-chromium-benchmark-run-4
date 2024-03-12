4d343278117695e6b7f55f3cf4497333dbdcf4bd
Amanda Deacon <amandadeacon@chromium.org>
2024-03-12 00:19:31 +0000

    [Autocorrect FA] Check Chrome metrics consent prior to federated logging.
    Per PWG discussion in b/327068044.
    
    For reference, see also this past CL for a similar case [Apr 2023]:
    crrev.com/c/4393829.
    
    Test: unit tests
    Bug: b/320559902
    Change-Id: I23e60da3bfed766eab323f26310e453841483e57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5348465
    Commit-Queue: Amanda Deacon <amandadeacon@chromium.org>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/main@{#1271276}

--- chrome/browser/ash/input_method/autocorrect_manager.cc
+++ chrome/browser/ash/input_method/autocorrect_manager.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/input_method/field_trial.h"
 #include "chrome/browser/ash/input_method/suggestion_enums.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
+#include "chrome/browser/metrics/chrome_metrics_service_accessor.h"
 #include "chrome/browser/ui/ash/keyboard/chrome_keyboard_controller_client.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/grit/generated_resources.h"
@@ -438,7 +439,8 @@ void AutocorrectManager::ProcessSetAutocorrectRangeDone(
   LogAssistiveAutocorrectAction(AutocorrectActions::kUnderlined);
   RecordAssistiveCoverage(AssistiveType::kAutocorrectUnderlined);
 
-  if (base::FeatureList::IsEnabled(features::kAutocorrectFederatedPhh)) {
+  if (ChromeMetricsServiceAccessor::IsMetricsAndCrashReportingEnabled() &&
+      base::FeatureList::IsEnabled(features::kAutocorrectFederatedPhh)) {
     // Report `original_text` to the Federated Service.
     federated_manager_.ReportSingleString(
         /*table_id*/ chromeos::federated::mojom::FederatedExampleTableId::
