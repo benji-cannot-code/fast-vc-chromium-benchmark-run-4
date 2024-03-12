96d16e0f391c5497d59be58b0028915cbc67b91f
Mihai Sardarescu <msarda@chromium.org>
2024-03-12 10:07:46 +0000

    Saved the default search engine location choice to a pref
    This CL adds pref "default_search_provider_data.choice_location" which
    stores the location of the default search engine choice. This preference
    is stored every time the default search engine preference is updated.
    
    This CL also adds the following logic to read the choice made location:
    a) if the source of the default search engine is FROM_USER, then the
    value of the pref default_search_provider_data.choice_location
    is used.
    b) if the source of the default search engine is anything else, then
    ChoiceMadeLocation::kOther is used.
    
    Fixed: b/326969414
    
    Change-Id: Ia3769cd73bcc494c37e1d8cbebe06c235f80184b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5331692
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271450}

--- components/search_engines/default_search_manager.h
+++ components/search_engines/default_search_manager.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "build/chromeos_buildflags.h"
 #include "components/prefs/pref_change_registrar.h"
+#include "components/search_engines/choice_made_location.h"
 
 namespace search_engines {
 class SearchEngineChoiceService;
@@ -35,6 +36,9 @@ class DefaultSearchManager {
   // default_search_provider.* prefs.
   static constexpr char kDefaultSearchProviderDataPrefName[] =
       "default_search_provider_data.template_url_data";
+
+  static constexpr char kDefaultSearchProviderChoiceLocationPrefName[] =
+      "default_search_provider_data.choice_location";
 
   static const char kID[];
   static const char kShortName[];
@@ -141,11 +145,21 @@ class DefaultSearchManager {
   // Gets the source of the current Default Search Engine value.
   Source GetDefaultSearchEngineSource() const;
 
+  // Returns the choice made location for the case when the user selected the
+  // default search engine:
+  // * Returns choice made location if the source is of the search engine is
+  //   `Source::FROM_USER`
+  // * Returns `ChoiceMadeLocation::kOther` in all other cases.
+  search_engines::ChoiceMadeLocation
+  GetChoiceMadeLocationForUserSelectedDefaultSearchEngine() const;
+
   // Returns a pointer to the fallback engine.
   const TemplateURLData* GetFallbackSearchEngine() const;
 
   // Write default search provider data to |pref_service_|.
-  void SetUserSelectedDefaultSearchEngine(const TemplateURLData& data);
+  void SetUserSelectedDefaultSearchEngine(
+      const TemplateURLData& data,
+      search_engines::ChoiceMadeLocation choice_location);
 
   // Clear the user's default search provider choice from |pref_service_|. Does
   // not explicitly disable Default Search. The new default search
--- components/search_engines/template_url_service.cc
+++ components/search_engines/template_url_service.cc
@@ -1001,7 +1001,8 @@ void TemplateURLService::SetUserSelectedDefaultSearchProvider(
     // We rely on the DefaultSearchManager to call ApplyDefaultSearchChange if,
     // in fact, the effective DSE changes.
     if (url) {
-      default_search_manager_.SetUserSelectedDefaultSearchEngine(url->data());
+      default_search_manager_.SetUserSelectedDefaultSearchEngine(
+          url->data(), choice_made_location);
       selection_added = true;
     } else {
       default_search_manager_.ClearUserSelectedDefaultSearchEngine();
@@ -1971,7 +1972,8 @@ void TemplateURLService::ApplyInitializersForTesting(
 
     // Set the first provided identifier to be the default.
     if (i == 0) {
-      default_search_manager_.SetUserSelectedDefaultSearchEngine(data);
+      default_search_manager_.SetUserSelectedDefaultSearchEngine(
+          data, search_engines::ChoiceMadeLocation::kOther);
     }
   }
 }
@@ -2158,7 +2160,7 @@ void TemplateURLService::MaybeUpdateDSEViaPrefs(TemplateURL* synced_turl) {
   if (prefs_ && (synced_turl->sync_guid() ==
                  GetDefaultSearchProviderPrefValue(*prefs_))) {
     default_search_manager_.SetUserSelectedDefaultSearchEngine(
-        synced_turl->data());
+        synced_turl->data(), search_engines::ChoiceMadeLocation::kOther);
   }
 }
 
@@ -2738,8 +2740,10 @@ void TemplateURLService::OnDefaultSearchProviderGUIDChanged() {
   }
 
   const TemplateURL* turl = GetTemplateURLForGUID(new_guid);
-  if (turl)
-    default_search_manager_.SetUserSelectedDefaultSearchEngine(turl->data());
+  if (turl) {
+    default_search_manager_.SetUserSelectedDefaultSearchEngine(
+        turl->data(), search_engines::ChoiceMadeLocation::kOther);
+  }
 }
 
 void TemplateURLService::MaybeSetIsActiveSearchEngines(
