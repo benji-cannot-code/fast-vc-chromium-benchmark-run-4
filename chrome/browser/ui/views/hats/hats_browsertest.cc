0afd22920e9bc795eec0f4020dd4b9f3ad274cf4
Eriko Kurimoto <elkurin@chromium.org>
2024-03-12 12:37:15 +0000

    HaTS: Resolve dangilng HatsService pointer
    This CL resolved dangling pointer in the browser test by removing
    unnecessary filed.
    
    Change-Id: I3ff2e2364b096ef87aefc2b1e1d10670523e74a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5362100
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271496}

--- chrome/browser/ui/views/hats/hats_browsertest.cc
+++ chrome/browser/ui/views/hats/hats_browsertest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/functional/callback_helpers.h"
 #include "base/json/values_util.h"
-#include "base/memory/raw_ptr.h"
 #include "base/path_service.h"
 #include "base/run_loop.h"
 #include "base/test/bind.h"
@@ -98,9 +97,8 @@ class MockHatsNextWebDialog : public HatsNextWebDialog {
 class HatsNextWebDialogBrowserTest : public InProcessBrowserTest {
  public:
   void SetUpOnMainThread() override {
-    hats_service_ = static_cast<MockHatsService*>(
-        HatsServiceFactory::GetInstance()->SetTestingFactoryAndUse(
-            browser()->profile(), base::BindRepeating(&BuildMockHatsService)));
+    HatsServiceFactory::GetInstance()->SetTestingFactoryAndUse(
+        browser()->profile(), base::BindRepeating(&BuildMockHatsService));
   }
 
   // Open a blank tab in the main browser, inspect it, and return the devtools
@@ -114,7 +112,10 @@ class HatsNextWebDialogBrowserTest : public InProcessBrowserTest {
     return devtools_window->browser_;
   }
 
-  MockHatsService* hats_service() { return hats_service_; }
+  MockHatsService* hats_service() {
+    return static_cast<MockHatsService*>(HatsServiceFactory::GetForProfile(
+        browser()->profile(), /*create_if_necessary=*/false));
+  }
 
   base::OnceClosure GetSuccessClosure() {
     return base::BindLambdaForTesting([&]() { ++success_count; });
@@ -126,9 +127,6 @@ class HatsNextWebDialogBrowserTest : public InProcessBrowserTest {
 
   int success_count = 0;
   int failure_count = 0;
-
- private:
-  raw_ptr<MockHatsService, DanglingUntriaged> hats_service_;
 };
 
 // Test that the web dialog correctly receives change to history state that
