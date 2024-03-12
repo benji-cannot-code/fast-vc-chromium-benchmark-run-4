3035e5154c9e33c36df9daadd94042fd2cbfbb78
Phillis Tang <phillis@chromium.org>
2024-03-12 20:19:04 +0000

    Revert "[test] Suppress generation of stack traces in death tests"
    This reverts commit 247e4de55550191706972861e586831a0d94d524.
    
    Reason for revert: Causing test failures https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests%20(dbg)(1)/119200/overview
    
    Original change's description:
    > [test] Suppress generation of stack traces in death tests
    >
    > In https://crrev.com/1246434, we suppressed symbolized stack traces in
    > death test child procs. While this worked for CHECK and DCHECK messages,
    > it did not catch other code, such as ObserverList, that may do its own
    > symbolization. In this change, we switch to suppressing symbolization in
    > death test child procs in base::StackTrace itself. This avoids the need
    > to chase down all callers and catch new callers.
    >
    > Bug: 1517343
    > Change-Id: I897b13b6326c7627e3fa56b4a34ad0d771762996
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5358577
    > Commit-Queue: Greg Thompson <grt@chromium.org>
    > Auto-Submit: Greg Thompson <grt@chromium.org>
    > Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1271557}
    
    Bug: 1517343
    Change-Id: I0cb12ce563a3b7d02c383f14c284398a178f9703
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5365797
    Auto-Submit: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271775}

--- base/debug/stack_trace.cc
+++ base/debug/stack_trace.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sstream>
 
 #include "base/check_op.h"
-#include "base/gtest_prod_util.h"
 #include "build/build_config.h"
 #include "build/config/compiler/compiler_buildflags.h"
 
@@ -268,12 +267,6 @@ void StackTrace::Print() const {
 }
 
 void StackTrace::OutputToStream(std::ostream* os) const {
-  if (::base::internal::InDeathTestChild()) {
-    // Backtraces are not visible in death test children, so do not waste
-    // resources by generating any.
-    (*os) << "Backtrace suppresed in death test.";
-    return;
-  }
   OutputToStreamWithPrefix(os, nullptr);
 }
 
@@ -283,11 +276,6 @@ std::string StackTrace::ToString() const {
 std::string StackTrace::ToStringWithPrefix(const char* prefix_string) const {
   std::stringstream stream;
 #if !defined(__UCLIBC__) && !defined(_AIX)
-  if (::base::internal::InDeathTestChild()) {
-    // Backtraces are not visible in death test children, so do not waste
-    // resources by generating any.
-    return "Backtrace suppresed in death test.";
-  }
   OutputToStreamWithPrefix(&stream, prefix_string);
 #endif
   return stream.str();
