0dee0b64ee03aaf833234cd5301213e68d300c16
Kelvin Jiang <kelvinjiang@chromium.org>
2024-03-12 20:13:18 +0000

    [DNR] End to end support for rules matching on response headers
    This CL adds the necessary changes in classes such as CompositeMatcher,
    RulesetManager and the extension web request event router to support
    rules that match on response headers. Currently the following action
    types are supported:
     - block
     - allow (scoped within rules matched in onHeadersReceived)
     - redirect
     - upgradeScheme
    
    Bug: b/40727004
    Change-Id: I2af5dbdf886f42842dfd0e2c96c04fe351fe3f2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5311916
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271770}

--- extensions/browser/api/declarative_net_request/composite_matcher.cc
+++ extensions/browser/api/declarative_net_request/composite_matcher.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ranges/algorithm.h"
 #include "base/time/time.h"
 #include "base/timer/elapsed_timer.h"
+#include "extensions/browser/api/declarative_net_request/constants.h"
 #include "extensions/browser/api/declarative_net_request/flat/extension_ruleset_generated.h"
 #include "extensions/browser/api/declarative_net_request/request_action.h"
 #include "extensions/browser/api/declarative_net_request/request_params.h"
@@ -40,18 +41,32 @@ bool AreIDsUnique(const CompositeMatcher::MatcherList& matchers) {
   return true;
 }
 
-// Helper to log the time taken in CompositeMatcher::GetBeforeRequestAction.
+// Helper to log the time taken in CompositeMatcher::GetAction.
 class ScopedGetBeforeRequestActionTimer {
  public:
-  ScopedGetBeforeRequestActionTimer() = default;
+  explicit ScopedGetBeforeRequestActionTimer(RulesetMatchingStage stage)
+      : stage_(stage) {}
   ~ScopedGetBeforeRequestActionTimer() {
-    UMA_HISTOGRAM_CUSTOM_MICROSECONDS_TIMES(
-        "Extensions.DeclarativeNetRequest.EvaluateBeforeRequestTime."
-        "SingleExtension2",
-        timer_.Elapsed(), base::Microseconds(1), base::Milliseconds(50), 50);
+    switch (stage_) {
+      case RulesetMatchingStage::kOnBeforeRequest:
+        UMA_HISTOGRAM_CUSTOM_MICROSECONDS_TIMES(
+            "Extensions.DeclarativeNetRequest.EvaluateBeforeRequestTime."
+            "SingleExtension2",
+            timer_.Elapsed(), base::Microseconds(1), base::Milliseconds(50),
+            50);
+        break;
+      case RulesetMatchingStage::kOnHeadersReceived:
+        UMA_HISTOGRAM_CUSTOM_MICROSECONDS_TIMES(
+            "Extensions.DeclarativeNetRequest.EvaluateHeadersReceivedTime."
+            "SingleExtension2",
+            timer_.Elapsed(), base::Microseconds(1), base::Milliseconds(50),
+            50);
+        break;
+    }
   }
 
  private:
+  RulesetMatchingStage stage_;
   base::ElapsedTimer timer_;
 };
 
@@ -121,10 +136,11 @@ std::set<RulesetID> CompositeMatcher::ComputeStaticRulesetIDs() const {
   return result;
 }
 
-ActionInfo CompositeMatcher::GetBeforeRequestAction(
+ActionInfo CompositeMatcher::GetAction(
     const RequestParams& params,
-    PageAccess page_access) const {
-  ScopedGetBeforeRequestActionTimer timer;
+    RulesetMatchingStage stage,
+    PermissionsData::PageAccess page_access) const {
+  ScopedGetBeforeRequestActionTimer timer(stage);
 
   bool always_require_host_permissions =
       host_permissions_always_required_ == HostPermissionsAlwaysRequired::kTrue;
@@ -142,8 +158,7 @@ ActionInfo CompositeMatcher::GetBeforeRequestAction(
   std::optional<uint64_t> max_allow_rule_priority;
 
   for (const auto& matcher : matchers_) {
-    std::optional<RequestAction> action =
-        matcher->GetAction(params, RulesetMatchingStage::kOnBeforeRequest);
+    std::optional<RequestAction> action = matcher->GetAction(params, stage);
     if (!action)
       continue;
 
@@ -224,6 +239,13 @@ void CompositeMatcher::OnDidFinishNavigation(
     content::NavigationHandle* navigation_handle) {
   for (auto& matcher : matchers_)
     matcher->OnDidFinishNavigation(navigation_handle);
+}
+
+bool CompositeMatcher::HasRulesets(RulesetMatchingStage stage) const {
+  return base::ranges::any_of(
+      matchers_, [stage](const std::unique_ptr<RulesetMatcher>& matcher) {
+        return matcher->GetRulesCount(stage) > 0;
+      });
 }
 
 void CompositeMatcher::OnMatchersModified() {
--- extensions/browser/api/declarative_net_request/composite_matcher_unittest.cc
+++ extensions/browser/api/declarative_net_request/composite_matcher_unittest.cc
@@ -97,8 +97,8 @@ TEST_F(CompositeMatcherTest, SamePrioritySpace) {
   params.url = &google_url;
 
   // The block rule should be higher priority.
-  ActionInfo action_info =
-      composite_matcher->GetBeforeRequestAction(params, PageAccess::kAllowed);
+  ActionInfo action_info = composite_matcher->GetAction(
+      params, RulesetMatchingStage::kOnBeforeRequest, PageAccess::kAllowed);
   ASSERT_TRUE(action_info.action);
   EXPECT_EQ(action_info.action->type, RequestAction::Type::BLOCK);
 
@@ -117,8 +117,8 @@ TEST_F(CompositeMatcherTest, SamePrioritySpace) {
       std::move(matchers), HostPermissionsAlwaysRequired::kFalse);
 
   // The allow rule should now have higher priority.
-  action_info =
-      composite_matcher->GetBeforeRequestAction(params, PageAccess::kAllowed);
+  action_info = composite_matcher->GetAction(
+      params, RulesetMatchingStage::kOnBeforeRequest, PageAccess::kAllowed);
   ASSERT_TRUE(action_info.action);
   EXPECT_EQ(action_info.action->type, RequestAction::Type::ALLOW);
 }
@@ -168,8 +168,9 @@ TEST_F(CompositeMatcherTest, GetModifyHeadersActions) {
 
   // Call GetBeforeRequestAction first to ensure that test and production code
   // paths are consistent.
-  composite_matcher->GetBeforeRequestAction(google_params,
-                                            PageAccess::kAllowed);
+  composite_matcher->GetAction(google_params,
+                               RulesetMatchingStage::kOnBeforeRequest,
+                               PageAccess::kAllowed);
 
   std::vector<RequestAction> actions =
       composite_matcher->GetModifyHeadersActions(google_params);
@@ -218,8 +219,9 @@ TEST_F(CompositeMatcherTest, GetModifyHeadersActions) {
 
   // Call GetBeforeRequestAction first to ensure that test and production code
   // paths are consistent.
-  composite_matcher->GetBeforeRequestAction(google_params,
-                                            PageAccess::kAllowed);
+  composite_matcher->GetAction(google_params,
+                               RulesetMatchingStage::kOnBeforeRequest,
+                               PageAccess::kAllowed);
 
   // Re-create |action_1| and |action_2| with the updated rule
   // priorities. The headers modified by each action should not change.
@@ -328,8 +330,9 @@ TEST_F(CompositeMatcherTest, GetModifyHeadersActions_Priority) {
 
   // Call GetBeforeRequestAction first to ensure that test and production code
   // paths are consistent.
-  composite_matcher->GetBeforeRequestAction(google_params,
-                                            PageAccess::kAllowed);
+  composite_matcher->GetAction(google_params,
+                               RulesetMatchingStage::kOnBeforeRequest,
+                               PageAccess::kAllowed);
 
   std::vector<RequestAction> actions =
       composite_matcher->GetModifyHeadersActions(google_params);
@@ -366,8 +369,9 @@ TEST_F(CompositeMatcherTest, GetModifyHeadersActions_Priority) {
 
   // Call GetBeforeRequestAction first to ensure that test and production code
   // paths are consistent.
-  composite_matcher->GetBeforeRequestAction(google_params,
-                                            PageAccess::kAllowed);
+  composite_matcher->GetAction(google_params,
+                               RulesetMatchingStage::kOnBeforeRequest,
+                               PageAccess::kAllowed);
   actions = composite_matcher->GetModifyHeadersActions(google_params);
 
   RequestAction header_1_action = create_action_for_rule(
@@ -471,8 +475,8 @@ TEST_F(CompositeMatcherTest, NotifyWithholdFromPageAccess) {
     params.element_type = url_pattern_index::flat::ElementType_SUBDOCUMENT;
     params.is_third_party = false;
 
-    ActionInfo redirect_action_info =
-        composite_matcher->GetBeforeRequestAction(params, test_case.access);
+    ActionInfo redirect_action_info = composite_matcher->GetAction(
+        params, RulesetMatchingStage::kOnBeforeRequest, test_case.access);
 
     EXPECT_EQ(test_case.should_notify_withheld,
               redirect_action_info.notify_request_withheld);
@@ -533,8 +537,8 @@ TEST_F(CompositeMatcherTest, HostPermissionsAlwaysRequired) {
     RequestParams params;
     params.url = &url;
 
-    ActionInfo info =
-        composite_matcher->GetBeforeRequestAction(params, cases[i].access);
+    ActionInfo info = composite_matcher->GetAction(
+        params, RulesetMatchingStage::kOnBeforeRequest, cases[i].access);
     EXPECT_EQ(cases[i].expected_notify_withheld, info.notify_request_withheld);
 
     std::optional<int> rule_matched_id;
@@ -616,8 +620,8 @@ TEST_F(CompositeMatcherTest, GetRedirectUrlFromPriority) {
     params.element_type = url_pattern_index::flat::ElementType_SUBDOCUMENT;
     params.is_third_party = false;
 
-    ActionInfo redirect_action_info =
-        composite_matcher->GetBeforeRequestAction(params, PageAccess::kAllowed);
+    ActionInfo redirect_action_info = composite_matcher->GetAction(
+        params, RulesetMatchingStage::kOnBeforeRequest, PageAccess::kAllowed);
 
     if (test_case.expected_final_url) {
       ASSERT_TRUE(redirect_action_info.action);
@@ -653,21 +657,21 @@ TEST_F(CompositeMatcherTest, RulePlacement) {
     RequestParams params;
     params.url = &url;
 
-    ActionInfo info =
-        composite_matcher->GetBeforeRequestAction(params, PageAccess::kAllowed);
+    ActionInfo info = composite_matcher->GetAction(
+        params, RulesetMatchingStage::kOnBeforeRequest, PageAccess::kAllowed);
     ASSERT_TRUE(info.action);
     EXPECT_EQ(kMinValidID + 1u, info.action->rule_id);
     EXPECT_FALSE(info.notify_request_withheld);
 
     // The highest priority matching rule (`redirect_rule`) needs host
     // permissions to match.
-    info = composite_matcher->GetBeforeRequestAction(params,
-                                                     PageAccess::kWithheld);
+    info = composite_matcher->GetAction(
+        params, RulesetMatchingStage::kOnBeforeRequest, PageAccess::kWithheld);
     EXPECT_FALSE(info.action);
     EXPECT_TRUE(info.notify_request_withheld);
 
-    info =
-        composite_matcher->GetBeforeRequestAction(params, PageAccess::kDenied);
+    info = composite_matcher->GetAction(
+        params, RulesetMatchingStage::kOnBeforeRequest, PageAccess::kDenied);
     EXPECT_FALSE(info.action);
     EXPECT_FALSE(info.notify_request_withheld);
   };
