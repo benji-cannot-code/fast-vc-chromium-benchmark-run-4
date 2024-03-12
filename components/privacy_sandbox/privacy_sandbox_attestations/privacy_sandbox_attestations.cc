f81f070c8c72b661a6c0eada7e408a0b674eeb36
Xiaochen Zhou <xiaochenzh@chromium.org>
2024-03-12 20:54:33 +0000

    [Privacy Sandbox] Record first attestation check time.
    To reduce the Privacy Sandbox API loss due to not-yet-available
    attestation list, we would like to know when the first attestation
    check takes place.
    
    This helps use determine when we need to make sure the attestation list
    is ready after Chrome starts.
    
    Bug: 41483922
    Change-Id: I99ea64bcbc8f6f2f2ac78f3682c121b30f11f66c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5354186
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Xiaochen Zhou <xiaochenzh@chromium.org>
    Reviewed-by: Sean Maher <spvw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271807}

--- components/privacy_sandbox/privacy_sandbox_attestations/privacy_sandbox_attestations.cc
+++ components/privacy_sandbox/privacy_sandbox_attestations/privacy_sandbox_attestations.cc
@@ -30,12 +30,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
+#include "base/time/time.h"
 #include "base/timer/elapsed_timer.h"
 #include "base/trace_event/memory_usage_estimator.h"
 #include "base/types/expected.h"
 #include "components/privacy_sandbox/privacy_sandbox_attestations/privacy_sandbox_attestations_histograms.h"
 #include "components/privacy_sandbox/privacy_sandbox_attestations/privacy_sandbox_attestations_parser.h"
 #include "components/privacy_sandbox/privacy_sandbox_features.h"
+#include "components/startup_metric_utils/browser/startup_metric_utils.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/privacy_sandbox_attestations_observer.h"
 #include "net/base/schemeful_site.h"
@@ -279,6 +281,11 @@ PrivacySandboxSettingsImpl::Status PrivacySandboxAttestations::IsSiteAttested(
   PrivacySandboxSettingsImpl::Status status =
       IsSiteAttestedInternal(site, invoking_api);
   base::UmaHistogramEnumeration(kAttestationStatusUMA, status);
+
+  // Record the time when the first Privacy Sandbox API attestation check takes
+  // place.
+  startup_metric_utils::GetBrowser().RecordPrivacySandboxAttestationFirstCheck(
+      base::TimeTicks::Now());
 
   // If the attestations map is absent and feature
   // `kDefaultAllowPrivacySandboxAttestations` is on, default allow.
