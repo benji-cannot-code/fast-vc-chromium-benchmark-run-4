d5d1ed5b66a2ea22398556ff0fdb27ae9e89da4c
Federica Germinario <fedegermi@google.com>
2024-03-12 13:36:28 +0000

    [ios] Disable first_run_egtests checking SearchEngineChoiceScreen
    When running the disabled tests on iPhone device the
    SearchEngineChoiceScreen is not displayed, for this reason tests are
    failing.
    
    Bug: 329210226
    Change-Id: I4b3b847033d857e7a8af7363e6f37d7be7e4ebcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5366181
    Owners-Override: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1271517}

--- ios/chrome/browser/ui/first_run/first_run_egtest.mm
+++ ios/chrome/browser/ui/first_run/first_run_egtest.mm
@@ -1730,7 +1730,15 @@ void DismissDefaultBrowserAndOmniboxPositionSelectionScreens() {
 // Tests that the Search Engine Choice screen is displayed, that the primary
 // button is correctly updated when the user selects a search engine then
 // scrolls down and that it correctly sets the default search engine.
-- (void)testSearchEngineChoiceScreenSelectThenScroll {
+// TODO(crbug.com/329210226): Re-enable the test on device.
+#if !TARGET_OS_SIMULATOR
+#define MAYBE_testSearchEngineChoiceScreenSelectThenScroll \
+  DISABLED_testSearchEngineChoiceScreenSelectThenScroll
+#else
+#define MAYBE_testSearchEngineChoiceScreenSelectThenScroll \
+  testSearchEngineChoiceScreenSelectThenScroll
+#endif
+- (void)MAYBE_testSearchEngineChoiceScreenSelectThenScroll {
   // Skips sign-in.
   [[self elementInteractionWithGreyMatcher:
              chrome_test_util::PromoStyleSecondaryActionButtonMatcher()
@@ -1767,7 +1775,15 @@ void DismissDefaultBrowserAndOmniboxPositionSelectionScreens() {
 // Tests that the Search Engine Choice screen is displayed, that the
 // primary button is correctly updated when the user scrolls down then selects a
 // search engine and that it correctly sets the default search engine.
-- (void)testSearchEngineChoiceScreenScrollThenSelect {
+// TODO(crbug.com/329210226): Re-enable the test on device.
+#if !TARGET_OS_SIMULATOR
+#define MAYBE_testSearchEngineChoiceScreenScrollThenSelect \
+  DISABLED_testSearchEngineChoiceScreenScrollThenSelect
+#else
+#define MAYBE_testSearchEngineChoiceScreenScrollThenSelect \
+  testSearchEngineChoiceScreenScrollThenSelect
+#endif
+- (void)MAYBE_testSearchEngineChoiceScreenScrollThenSelect {
   // Skips sign-in.
   [[self elementInteractionWithGreyMatcher:
              chrome_test_util::PromoStyleSecondaryActionButtonMatcher()
