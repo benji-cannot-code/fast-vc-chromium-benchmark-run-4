b42abd34def5d12302b83aa21227927922e9ee6d
Yann Dago <ydago@chromium.org>
2024-03-12 18:31:14 +0000

    Add policy to control the enterprise badging label
    This policy only controls the label shown next to the toolbar avatar
    but will later also control the default name given to a new profile.
    
    Screenshot: http://screen/4AfgjHjD9oEGkZg
    
    Bug: b:322796016
    Change-Id: Ice2266880d7f3093e3e4fa968bb83efdbf874907
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5340915
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271703}

--- chrome/browser/profiles/profile_impl.cc
+++ chrome/browser/profiles/profile_impl.cc
@@ -434,6 +434,7 @@ void ProfileImpl::RegisterProfilePrefs(
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   registry->RegisterBooleanPref(prefs::kPdfAnnotationsEnabled, true);
 #endif
+  registry->RegisterStringPref(prefs::kCustomProfileLabel, std::string());
 }
 
 ProfileImpl::ProfileImpl(
