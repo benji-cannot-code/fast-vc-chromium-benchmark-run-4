383c9c4a8b2296a8553b2cdbcfd9dd955ce6fd77
Ramin Halavati <rhalavati@chromium.org>
2024-03-12 17:05:52 +0000

    Add Accessibility.ScreenAI.Component.Available metric.
    This metric will tell us that regardless of install retries or success
    or failure at startup, how much the component is available when needed.
    
    AX-Relnotes: n/a
    Bug: 41489907
    Change-Id: I4f58fac6f99bb4a5c84e1bd173147931097e8257
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5365163
    Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1271630}

--- chrome/browser/screen_ai/screen_ai_service_router.cc
+++ chrome/browser/screen_ai/screen_ai_service_router.cc
@@ -109,6 +109,12 @@ std::unique_ptr<ComponentFiles> ComponentFiles::Load(
       files_list_file_name);
 }
 
+// TODO(https://crbug.com/41489907): Remove after the issue is fixed.
+void RecordComponentAvailablity(bool available) {
+  base::UmaHistogramBoolean("Accessibility.ScreenAI.Component.Available",
+                            available);
+}
+
 }  // namespace
 
 namespace screen_ai {
@@ -145,6 +151,7 @@ void ScreenAIServiceRouter::GetServiceStateAsync(
   if (service_state) {
     // Either service is already initialized or disabled.
     std::move(callback).Run(*service_state);
+    RecordComponentAvailablity(true);
     return;
   }
 
@@ -189,6 +196,7 @@ void ScreenAIServiceRouter::StateChanged(ScreenAIInstallState::State state) {
       for (Service service : all_services) {
         CallPendingStatusRequests(service, false);
       }
+      RecordComponentAvailablity(false);
       break;
     }
 
@@ -197,6 +205,7 @@ void ScreenAIServiceRouter::StateChanged(ScreenAIInstallState::State state) {
       for (Service service : all_services) {
         InitializeServiceIfNeeded(service);
       }
+      RecordComponentAvailablity(true);
       break;
     }
   }
