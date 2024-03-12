a09ea4335c9d59016e730b0c2c72ef9e5edcb976
Jihad Hanna <jihadghanna@google.com>
2024-03-12 11:54:49 +0000

    Add a SuggestionStrikeDatabase
    This CL adds the class responsible for managing suggestion strike, and
    also the feature where this functionality will be gated behind.
    
    More info about the general goal of this CL chain can be found on
    go/ac-off-false-positives
    
    Bug: 322910752
    Change-Id: Id0bee7f9e7272cecc43b57b177ed959d404aabb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5355864
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1271473}

---
+++ components/autofill/core/browser/strike_databases/address_suggestion_strike_database.cc
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill/core/browser/strike_databases/address_suggestion_strike_database.h"
+
+#include <string>
+
+#include "base/strings/string_number_conversions.h"
+#include "components/autofill/core/browser/proto/strike_data.pb.h"
+#include "components/autofill/core/browser/strike_databases/autofill_profile_save_strike_database.h"
+#include "components/autofill/core/common/signatures.h"
+
+namespace autofill {
+
+// static
+std::string AddressSuggestionStrikeDatabase::GetId(
+    FormSignature form_signature,
+    FieldSignature field_signature,
+    const GURL& url) {
+  return base::NumberToString(form_signature.value()) +
+         base::NumberToString(form_signature.value()) + "-" + url.host();
+}
+
+int AddressSuggestionStrikeDatabase::GetMaxStrikesLimit() const {
+  return features::kSuggestionStrikeLimit.Get();
+}
+
+}  // namespace autofill
