8c6a3626f5a16bf4011121435e0ea88b16501441
Scott Yoder <scottyoder@google.com>
2024-03-12 19:49:05 +0000

    [ios] Fix icon color when editing MagicStack carousel
    This change uses a color with a separate variant for Dark Mode so that
    it updates when the mode changes.
    
    Light mode screenshot:
    https://screenshot.googleplex.com/9nwtnLajq4EWVit.png
    
    Dark mode screenshot:
    https://screenshot.googleplex.com/8aVXHYUDWSXRaEt.png
    
    Bug: 329281642
    Change-Id: Ia535b1b63721073badac46cc67188ebfe5afc5d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5365768
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271753}

--- ios/chrome/browser/ui/content_suggestions/magic_stack_half_sheet_table_view_controller.mm
+++ ios/chrome/browser/ui/content_suggestions/magic_stack_half_sheet_table_view_controller.mm
@@ -218,7 +218,7 @@ enum ItemType : NSInteger {
       [[TableViewSwitchItem alloc] initWithType:type];
   switchItem.text = title;
   switchItem.iconImage = symbol;
-  switchItem.iconTintColor = UIColor.blackColor;
+  switchItem.iconTintColor = [UIColor colorNamed:kSolidBlackColor];
   switchItem.accessibilityIdentifier = title;
   return switchItem;
 }
