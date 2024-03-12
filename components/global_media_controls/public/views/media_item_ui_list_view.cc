bd3ecf5fec21ea2f3d73d2b730deebc8bca2ade9
Yiren Wang <yrw@chromium.org>
2024-03-12 21:36:15 +0000

    Add MediaItemUIUpdatedView as a new MediaItemUI
    Initial commit to create classes and need more implementation in future
    CLs. This UI will be used for the updated non-cros GMC and combines
    the functions of MediaItemUIView and MediaItemUIDetailedView to remove
    redundant codes.
    
    Add this class to MediaItemUIListView and eventually MediaDialogView
    under the media::kGlobalMediaControlsUpdatedUI flag to make testing the
    UI easier.
    
    Bug: 328694773
    Change-Id: If993c0106fc8d6ef87e73d4e2e1ba5a98b342e9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5354815
    Commit-Queue: Yiren Wang <yrw@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271830}

--- components/global_media_controls/public/views/media_item_ui_list_view.cc
+++ components/global_media_controls/public/views/media_item_ui_list_view.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/global_media_controls/public/views/media_item_ui_list_view.h"
 
 #include "base/containers/contains.h"
+#include "components/global_media_controls/public/views/media_item_ui_updated_view.h"
 #include "components/global_media_controls/public/views/media_item_ui_view.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/color/color_id.h"
@@ -112,6 +113,35 @@ void MediaItemUIListView::HideItem(const std::string& id) {
 
 MediaItemUIView* MediaItemUIListView::GetItem(const std::string& id) {
   return items_[id];
+}
+
+void MediaItemUIListView::ShowUpdatedItem(
+    const std::string& id,
+    std::unique_ptr<MediaItemUIUpdatedView> item) {
+  CHECK(!base::Contains(updated_items_, id));
+  CHECK(item.get());
+
+  updated_items_[id] = contents()->AddChildView(std::move(item));
+
+  contents()->InvalidateLayout();
+  PreferredSizeChanged();
+}
+
+void MediaItemUIListView::HideUpdatedItem(const std::string& id) {
+  if (!base::Contains(updated_items_, id)) {
+    return;
+  }
+
+  contents()->RemoveChildViewT(updated_items_[id]);
+  updated_items_.erase(id);
+
+  contents()->InvalidateLayout();
+  PreferredSizeChanged();
+}
+
+MediaItemUIUpdatedView* MediaItemUIListView::GetUpdatedItem(
+    const std::string& id) {
+  return updated_items_[id];
 }
 
 base::WeakPtr<MediaItemUIListView> MediaItemUIListView::GetWeakPtr() {
