f53a058be757aa1931d8614ad1e2e6644e513bc8
Shiyi Zou <shiyi.zou@intel.com>
2024-03-12 02:16:41 +0000

    webnn: add gather validation tests in WPT
    This CL adds WPT tests for gather and removes the unit tests
    `MLGraphBuilderTest.GatherTest` and `MLGraphTestMojo.GatherTest`.
    
    Bug: 327337525, 328026885
    Change-Id: Ie37f4caa49a87ae0d279c19c5969e23fa6e40d39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5351229
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Auto-Submit: Shiyi Zou <shiyi.zou@intel.com>
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Commit-Queue: Shiyi Zou <shiyi.zou@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1271324}

--- third_party/blink/renderer/modules/ml/webnn/ml_graph_builder_test.h
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_builder_test.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_conv_2d_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_conv_transpose_2d_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_elu_options.h"
-#include "third_party/blink/renderer/bindings/modules/v8/v8_ml_gather_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_gemm_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_gru_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_hard_sigmoid_options.h"
@@ -79,13 +78,6 @@ MLOperand* BuildConvTranspose2d(V8TestingScope& scope,
                                 const MLOperand* filter,
                                 const MLConvTranspose2dOptions* options =
                                     MLConvTranspose2dOptions::Create());
-
-MLOperand* BuildGather(
-    V8TestingScope& scope,
-    MLGraphBuilder* builder,
-    const MLOperand* input,
-    const MLOperand* indices,
-    const MLGatherOptions* options = MLGatherOptions::Create());
 
 MLOperand* BuildLeakyRelu(
     V8TestingScope& scope,
