26237fd7863966f01936cd10b8d5a651a445a14f
Andy Paicu <andypaicu@chromium.org>
2024-03-12 20:46:28 +0000

    [PEPC] Correctly clamp negative margins to 0
    Update the PEPC style for margins to always use the NonNegative length
    range.
    
    Also updated the Length equality operator to take into account the
    range when deciding equality of computed values as this code reuses the
    same expression ref and only updates the length range which was not
    being detected as being a different length.
    
    Bug: 1462930
    Change-Id: I0e65c4ab3cd40474a675daa964cdae3029b288cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5335147
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271802}

--- third_party/blink/renderer/core/html/html_permission_element.cc
+++ third_party/blink/renderer/core/html/html_permission_element.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "base/functional/bind.h"
+#include "base/memory/scoped_refptr.h"
 #include "third_party/blink/public/common/input/web_pointer_properties.h"
 #include "third_party/blink/public/mojom/permissions/permission.mojom-blink.h"
 #include "third_party/blink/public/strings/grit/blink_strings.h"
@@ -31,6 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/platform/fonts/font_description.h"
 #include "third_party/blink/renderer/platform/fonts/font_selection_types.h"
+#include "third_party/blink/renderer/platform/geometry/calculation_expression_node.h"
+#include "third_party/blink/renderer/platform/geometry/calculation_value.h"
+#include "third_party/blink/renderer/platform/geometry/length.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/text/platform_locale.h"
@@ -178,6 +182,19 @@ String PermissionNameToString(PermissionName permission_name) {
     default:
       NOTREACHED_NORETURN() << "Not supported permission " << permission_name;
   }
+}
+
+Length AdjustedMargin(const Length& margin) {
+  if (margin.IsCalculated()) {
+    if (margin.GetCalculationValue().IsNonNegative()) {
+      return margin;
+    }
+
+    return Length(CalculationValue::CreateSimplified(
+        margin.GetCalculationValue().GetOrCreateExpression(),
+        Length::ValueRange::kNonNegative));
+  }
+  return (margin.Value() < 0) ? Length::FixedZero() : margin;
 }
 
 }  // namespace
@@ -332,22 +349,10 @@ void HTMLPermissionElement::AdjustStyle(ComputedStyleBuilder& builder) {
 
   builder.SetOutlineOffset(builder.OutlineOffset().ClampNegativeToZero());
 
-  // TODO(crbug.com/1462930): Handle calculated Lengths.
-  if (!builder.MarginLeft().IsCalculated() &&
-      builder.MarginLeft().Value() < 0) {
-    builder.ResetMarginLeft();
-  }
-  if (!builder.MarginRight().IsCalculated() &&
-      builder.MarginRight().Value() < 0) {
-    builder.ResetMarginRight();
-  }
-  if (!builder.MarginTop().IsCalculated() && builder.MarginTop().Value() < 0) {
-    builder.ResetMarginTop();
-  }
-  if (!builder.MarginBottom().IsCalculated() &&
-      builder.MarginBottom().Value() < 0) {
-    builder.ResetMarginBottom();
-  }
+  builder.SetMarginLeft(AdjustedMargin(builder.MarginLeft()));
+  builder.SetMarginRight(AdjustedMargin(builder.MarginRight()));
+  builder.SetMarginTop(AdjustedMargin(builder.MarginTop()));
+  builder.SetMarginBottom(AdjustedMargin(builder.MarginBottom()));
 
   // Check and modify (if needed) properties related to the font.
   std::optional<FontDescription> new_font_description;
