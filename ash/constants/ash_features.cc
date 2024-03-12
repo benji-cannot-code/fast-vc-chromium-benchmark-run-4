35559efb619fc3bea9ac4bf4ff60be802ba0fcd1
Dorah <dorahkim@google.com>
2024-03-12 03:42:26 +0000

    camera: Enable CameraAppAutoQRDetection by default
    This CL enables feature flag CameraAppAutoQRDetection by default because
    the launch review is approved.
    Also an error handling part when detection service is unavailable was
    added.
    
    BUG=b:298581154
    Test=Manually.
    
    Change-Id: Ifd68061d530887da1a5d5ec0b7092c36b890d95a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5347520
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Dorah Kim <dorahkim@google.com>
    Cr-Commit-Position: refs/heads/main@{#1271351}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -298,7 +298,7 @@ BASE_FEATURE(kBluetoothQualityReport,
 
 BASE_FEATURE(kCameraAppAutoQRDetection,
              "CameraAppAutoQRDetection",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kCameraAppCrosEvents,
              "CameraAppCrosEvents",
