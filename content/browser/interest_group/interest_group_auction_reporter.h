8b1163e4172dd2e54c9eced5d7ed17f75103efa2
Russ Hamilton <behamilton@google.com>
2024-03-12 17:32:22 +0000

    Use consistent rounded bid for buyer and seller reporting
    Bug: 329244579
    Change-Id: I51d7b95a392d7b5f007bae1f6c094a1f91f7055d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5365244
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271651}

--- content/browser/interest_group/interest_group_auction_reporter.h
+++ content/browser/interest_group/interest_group_auction_reporter.h
@@ -147,6 +147,7 @@ class CONTENT_EXPORT InterestGroupAuctionReporter {
     // returned by the component seller. Otherwise, it's the bid from the
     // bidder.
     double bid;
+    double rounded_bid;
 
     // Currency the bid is in.
     std::optional<blink::AdCurrency> bid_currency;
@@ -331,6 +332,8 @@ class CONTENT_EXPORT InterestGroupAuctionReporter {
           PrivateAggregationKey,
           std::vector<auction_worklet::mojom::PrivateAggregationRequestPtr>>
           private_aggregation_requests);
+
+  static double RoundBidStochastically(double bid);
 
   // Returns the result of performing stochastic rounding on `value`. We limit
   // the value to `k` bits of precision in the mantissa (not including sign) and
