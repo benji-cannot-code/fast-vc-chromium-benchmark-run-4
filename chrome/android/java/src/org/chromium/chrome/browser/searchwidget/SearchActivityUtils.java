666ef29e177cf7f25337d5094b0d46de32c983bb
Tomasz Wiszkowski <ender@google.com>
2024-03-12 20:17:56 +0000

    Route SearchActivity navigations back to calling activity.
    This change ensures that upon navigation, SearchActivity invoked
    from CCT will send the navigation URL back to the caller (rather than
    calling up ChromeTabbedActivity).
    
    Bug: b:327023983
    Change-Id: I3ead2a4ce53cd5c87648481015e6b6f1b463d9c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5363496
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1271773}

--- chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivityUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivityUtils.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.searchwidget;
 
 import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -15,11 +16,12 @@ import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.IntentUtils;
+import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ui.searchactivityutils.SearchActivityConstants;
 import org.chromium.url.GURL;
 
 /** Class facilitating interactions with the SearchActivity and the Omnibox. */
-public interface SearchActivityUtils {
+public class SearchActivityUtils {
     @VisibleForTesting
     /* package */ static final int OMNIBOX_REQUEST_CODE = 'O' << 24 | 'M' << 16 | 'N' << 8 | 'I';
 
@@ -44,9 +46,16 @@ public interface SearchActivityUtils {
                 buildTrustedIntent(
                                 activity, SearchActivityConstants.ACTION_START_EXTENDED_TEXT_SEARCH)
                         .putExtra(EXTRA_CURRENT_URL, currentUrl.getSpec())
-                        .addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                        .addFlags(
+                                Intent.FLAG_ACTIVITY_NO_HISTORY
+                                        | Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP);
 
-        activity.startActivityForResult(intent, OMNIBOX_REQUEST_CODE);
+        activity.startActivityForResult(
+                intent,
+                OMNIBOX_REQUEST_CODE,
+                ActivityOptions.makeCustomAnimation(
+                                activity, android.R.anim.fade_in, R.anim.no_anim)
+                        .toBundle());
     }
 
     /**
