5dacd68fe18d56f6cb7d725754af7fb821f7e287
Peter McNeeley <petermcneeley@chromium.org>
2024-03-12 19:05:15 +0000

    Minor optimization for timer in video detection
    The video detection timer is restarted every frame. This costs >10us
    even on high end chromeos devices.
    
    Change-Id: I87d5a33fb216ac6256ccded08803e814c3376ada
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5362734
    Commit-Queue: Peter McNeeley <petermcneeley@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271727}

--- components/viz/service/frame_sinks/video_detector.cc
+++ components/viz/service/frame_sinks/video_detector.cc
@@ -23,7 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace viz {
 
-constexpr base::TimeDelta VideoDetector::kVideoTimeout;
+constexpr base::TimeDelta VideoDetector::kMaxVideoTimeout;
+constexpr base::TimeDelta VideoDetector::kMinVideoTimeout;
 constexpr base::TimeDelta VideoDetector::kMinVideoDuration;
 
 // Stores information about updates to a client and determines whether it's
@@ -193,8 +194,13 @@ void VideoDetector::OnSurfaceWillBeDrawn(Surface* surface) {
   base::TimeTicks now = tick_clock_->NowTicks();
 
   if (it->second->ReportDrawnAndCheckForVideo(surface, now)) {
-    video_inactive_timer_.Start(FROM_HERE, kVideoTimeout, this,
-                                &VideoDetector::OnVideoActivityEnded);
+    // Avoid (re)starting the timer every frame since it has considerable
+    // overhead.
+    if (!video_inactive_timer_.IsRunning() ||
+        (video_inactive_timer_.desired_run_time() - now) < kMinVideoTimeout) {
+      video_inactive_timer_.Start(FROM_HERE, kMaxVideoTimeout, this,
+                                  &VideoDetector::OnVideoActivityEnded);
+    }
     if (!video_is_playing_) {
       video_is_playing_ = true;
       for (auto& observer : observers_) {
