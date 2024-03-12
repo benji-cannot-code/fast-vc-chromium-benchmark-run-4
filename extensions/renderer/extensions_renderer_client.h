b564a2dfebe308d7d920a5fae3d0e35f63fd61eb
Devlin Cronin <rdevlin.cronin@chromium.org>
2024-03-12 01:04:18 +0000

    [Extensions] Improve thread safety of RendererAPIProviders
    ExtensionRendererAPIProvders are used to initialize different script
    contexts as they are created. Since these script contexts can be
    created either for the main thread (as in the case of a page-based
    context) or on worker threads (e.g. for a service worker-based
    extension's background context), the API providers are used on
    multiple threads (potentially concurrently).
    
    This is safe because:
    * The API providers are created when the main thread starts, before any
      worker threads could be created.
    * The API providers are owned by the Dispatcher, which is in turn owned
      by the ExtensionsRendererClient, which is a singleton object that is
      leaked. Thus, the API providers are never destroyed once they are
      created.
    * The API providers owned by the dispatcher are never modified through
      either assignment or methods.
    
    The last point was true, but not enforced. We can enforce this by:
    * Making the collection of API providers on the dispatcher const
      (ensuring no API providers are added or removed once the
      Dispatcher is constructed).
    * Making each provider within that collection const (ensuring no
      method called on the API provider modify state within that API
      provider).
    * Making each method on the API provider const (to be compatible with
      the point above).
    
    These help ensure the thread-safety of accessing these variables on
    multiple threads in the renderer.
    
    Bug: 326903574
    Change-Id: Ib52f88ec0f589ff62f2622fc66f7637b6253386d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5342718
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Ovidio Ruiz-Henríquez <odejesush@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271292}

--- extensions/renderer/extensions_renderer_client.h
+++ extensions/renderer/extensions_renderer_client.h
@@ -65,7 +65,8 @@ class ExtensionsRendererClient {
   static void Set(ExtensionsRendererClient* client);
 
  protected:
-  std::vector<std::unique_ptr<ExtensionsRendererAPIProvider>> api_providers_;
+  std::vector<std::unique_ptr<const ExtensionsRendererAPIProvider>>
+      api_providers_;
 };
 
 }  // namespace extensions
