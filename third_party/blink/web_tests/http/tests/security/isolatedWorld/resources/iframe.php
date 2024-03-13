7a6aaca6614a16b4deadb12ad138a66d4e4780a6
Rakina Zata Amni <rakina@chromium.org>
2024-03-13 01:35:12 +0000

    Update layout tests to account for same-site RFH change
    With RenderDocument, we can change RenderFrames within the same
    process, causing some tests that require specific timing (e.g.
    ordering of console.log and loads) to fail. This CL fixes those
    tests.
    
    Bug: 936696
    Change-Id: I0176145641b05844b2097b12e46a248f1eeac82d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5359597
    Auto-Submit: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271933}

--- third_party/blink/web_tests/http/tests/security/isolatedWorld/resources/iframe.php
+++ third_party/blink/web_tests/http/tests/security/isolatedWorld/resources/iframe.php
@@ -2,6 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <!DOCTYPE html>
 <html>
 <script>
-alert("iframe loaded in test #<? echo $_GET["test"] ?>");
+window.parent.postMessage("iframe loaded in test #<? echo $_GET["test"] ?>");
 </script>
 </html>
