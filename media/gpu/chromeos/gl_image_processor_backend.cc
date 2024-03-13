272f6d1969cfbe436179cdd5a6ed8e06703ad21b
Michelle Chen <michellegc@google.com>
2024-03-13 00:47:13 +0000

    Revert "media/gpu/ip_factory: Update GL Scaling for VDA tests"
    This reverts commit 9d2c91e927711cdb05ba09af237a45db5700512f.
    
    Reason for revert: broke tree: https://ci.chromium.org/ui/p/chromium/builders/ci/linux-lacros-dbg/32159/overview
    
    Original change's description:
    > media/gpu/ip_factory: Update GL Scaling for VDA tests
    >
    > In crrev/c/5327871, the |kUseGLForScaling| flag was set to be enabled
    > by default. This resulted in the video_decode_accelerator_tests being
    > broken due to ozone and threading errors.
    >
    > The test was updated to initialize Ozone and GLSurfaceTestSupport.
    > To handle threading, |ScopedAllowBaseSyncPrimitives| was added to
    > the gl backend and thread restrictions was updated respectively to make
    > the gl backend a friend class.
    >
    > Finally, the image_processor_factory is updated to make sure that the
    > |useGLForScaling| flag only creates a valid GLBackend when the operation
    > is NV12 scaling.
    >
    > Bug: b:328511661
    > Test: Successfully ran v_d_a tests on a Cherry
    > Also ran image_processor_test on a Cherry
    >
    > Successfully ran image_processor_perf_tests on a Cherry
    >
    > Change-Id: Ief0ea84dd1829066703b137112cc1d0dcea18f5b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5353745
    > Commit-Queue: Bijan Choobineh‎ <bchoobineh@chromium.org>
    > Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1271859}
    
    Bug: b:328511661
    Change-Id: Ib676104cea7b4b1c128637a252ced0ab3a743e95
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5366861
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Michelle Chen <michellegc@google.com>
    Auto-Submit: Michelle Chen <michellegc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1271911}

--- media/gpu/chromeos/gl_image_processor_backend.cc
+++ media/gpu/chromeos/gl_image_processor_backend.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/waitable_event.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/thread_pool.h"
-#include "base/threading/thread_restrictions.h"
 #include "base/trace_event/trace_event.h"
 #include "media/base/format_utils.h"
 #include "media/gpu/chromeos/frame_resource.h"
@@ -250,9 +249,6 @@ std::unique_ptr<ImageProcessorBackend> GLImageProcessorBackend::Create(
       base::BindOnce(&GLImageProcessorBackend::InitializeTask,
                      base::Unretained(image_processor.get()),
                      base::Unretained(&done), base::Unretained(&success)));
-  // The thread local nature of GL context forces us to use |PostTask()|,
-  // which in turn forces us to use synchronization primitives.
-  base::ScopedAllowBaseSyncPrimitives allow_wait;
   done.Wait();
   if (!success) {
     return nullptr;
