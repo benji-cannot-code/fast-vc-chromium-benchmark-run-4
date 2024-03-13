237280da0234c773d639fc2cf3a96820d3cce68b
Friedrich Horschig <fhorschig@chromium.org>
2024-03-13 16:58:28 +0000

    [Android][3PPWM] For AwG, fall back to built-in Autofill
    AwG shouldn't replace built-in Autofilll which is in
    a better position to fill exactly the same data.
    
    This CL adds a variation to the flag which allows to
    skip that fallback. It simplifies manual testing and
    temporarily, automated testing, too.
    
    Bug: 323130329
    Change-Id: I8078fac19adb149918de69479e187344e0e062de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5366403
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272224}

--- components/android_autofill/browser/android_autofill_client.cc
+++ components/android_autofill/browser/android_autofill_client.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/locale_utils.h"
 #include "base/android/scoped_java_ref.h"
 #include "base/check_op.h"
+#include "base/feature_list.h"
 #include "base/functional/function_ref.h"
 #include "base/notreached.h"
 #include "base/types/cxx23_to_underlying.h"
@@ -26,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/ui/suggestion.h"
 #include "components/autofill/core/browser/webdata/autofill_webdata_service.h"
 #include "components/autofill/core/common/aliases.h"
+#include "components/autofill/core/common/autofill_features.h"
 #include "components/prefs/pref_service.h"
 #include "components/user_prefs/user_prefs.h"
 #include "content/public/browser/browser_context.h"
@@ -36,6 +38,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/android/view_android.h"
 #include "ui/gfx/geometry/rect_f.h"
 
+using autofill::features::kAutofillVirtualViewStructureAndroid;
+using autofill::features::
+    kAutofillVirtualViewStructureAndroidSkipsCompatibilityCheck;
 using base::android::AttachCurrentThread;
 using base::android::ConvertUTF16ToJavaString;
 using base::android::JavaParamRef;
@@ -54,6 +59,18 @@ void AndroidAutofillClient::CreateForWebContents(
                           base::WrapUnique(new AndroidAutofillClient(
                               contents, std::move(notify_client_created))));
   }
+}
+
+// static
+bool AndroidAutofillClient::AllowedForAutofillService() {
+  if (!base::FeatureList::IsEnabled(kAutofillVirtualViewStructureAndroid)) {
+    return false;
+  }
+  if (kAutofillVirtualViewStructureAndroidSkipsCompatibilityCheck.Get()) {
+    return true;
+  }
+  return Java_AndroidAutofillClient_allowedForAutofillService(
+      AttachCurrentThread());
 }
 
 AndroidAutofillClient::~AndroidAutofillClient() {
--- components/android_autofill/browser/java/src/org/chromium/components/autofill/AndroidAutofillClient.java
+++ components/android_autofill/browser/java/src/org/chromium/components/autofill/AndroidAutofillClient.java
@@ -5,8 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.autofill;
 
+import android.content.ComponentName;
 import android.content.Context;
+import android.os.Build;
 import android.view.View;
+import android.view.autofill.AutofillManager;
 
 import org.jni_zero.CalledByNative;
 import org.jni_zero.JNINamespace;
@@ -21,6 +24,9 @@ import org.chromium.base.ContextUtils;
  */
 @JNINamespace("android_autofill")
 public class AndroidAutofillClient {
+    private static final String AWG_COMPONENT_NAME =
+            "com.google.android.gms/com.google.android.gms.autofill.service.AutofillService";
+
     private final long mNativeAndroidAutofillClient;
     private AutofillPopup mAutofillPopup;
     private Context mContext;
@@ -28,6 +34,24 @@ public class AndroidAutofillClient {
     @CalledByNative
     public static AndroidAutofillClient create(long nativeClient) {
         return new AndroidAutofillClient(nativeClient);
+    }
+
+    @CalledByNative
+    public static boolean allowedForAutofillService() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
+            return false;
+        }
+        AutofillManager manager =
+                ContextUtils.getApplicationContext().getSystemService(AutofillManager.class);
+        if (manager == null || !manager.isEnabled()) {
+            return false;
+        }
+        ComponentName componentName = null;
+        try {
+            componentName = manager.getAutofillServiceComponentName();
+        } catch (Exception e) {
+        }
+        return componentName != null && !AWG_COMPONENT_NAME.equals(componentName.flattenToString());
     }
 
     private AndroidAutofillClient(long nativeAndroidAutofillClient) {
