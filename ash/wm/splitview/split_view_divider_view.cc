9c83582e123fdd8f0886ed8de3e309845bf01af2
Michele Fan <michelefan@chromium.org>
2024-03-13 18:35:00 +0000

    snap-group: Hide feedback button on drag and re-show when drag ends
    Demo: http://b/328789217#comment2
    Fixed: b/328789217
    Change-Id: I5486609ee368e98c2d7606b1ee45b4f6eef8ac58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5368170
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272311}

--- ash/wm/splitview/split_view_divider_view.cc
+++ ash/wm/splitview/split_view_divider_view.cc
@@ -167,6 +167,7 @@ bool SplitViewDividerView::OnMousePressed(const ui::MouseEvent& event) {
 }
 
 bool SplitViewDividerView::OnMouseDragged(const ui::MouseEvent& event) {
+  RefreshFeedbackButton(/*visible=*/false);
   if (!mouse_move_started_) {
     // If this is the first mouse drag event, start the resize and reset
     // `mouse_move_started_`.
@@ -175,6 +176,7 @@ bool SplitViewDividerView::OnMouseDragged(const ui::MouseEvent& event) {
     StartResizing(initial_mouse_event_location_);
     return true;
   }
+
   // Else continue with the resize.
   gfx::Point location(event.location());
   views::View::ConvertPointToScreen(this, &location);
@@ -188,6 +190,8 @@ void SplitViewDividerView::OnMouseReleased(const ui::MouseEvent& event) {
   initial_mouse_event_location_ = gfx::Point();
   mouse_move_started_ = false;
   EndResizing(location, /*swap_windows=*/event.GetClickCount() == 2);
+
+  RefreshFeedbackButton(/*visible=*/true);
 }
 
 void SplitViewDividerView::OnGestureEvent(ui::GestureEvent* event) {
