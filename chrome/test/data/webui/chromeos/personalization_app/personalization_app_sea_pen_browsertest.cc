a01c1700e4ff43d90d76a7d1257d6f850977b3dc
Jason Thai <jasontt@chromium.org>
2024-03-13 23:08:02 +0000

    p13n: sea pen browser tests
    Move the sea pen tests currently under wallpaper subpage into a
    standalone suite. This makes it easier to assign the screen play to this
    feature.
    
    BUG=b:329149811
    TEST=browser_tests --gtest_filter="PersonalizationAppSeaPenBrowserTest*"
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I8105f897ce2843ff9b4d4aa2d8f62903d6eb56a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5368176
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272485}

--- chrome/test/data/webui/chromeos/personalization_app/personalization_app_sea_pen_browsertest.cc
+++ chrome/test/data/webui/chromeos/personalization_app/personalization_app_sea_pen_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "ash/webui/personalization_app/test/personalization_app_mojom_banned_mocha_test_base.h"
+#include "chrome/browser/ash/system_web_apps/apps/personalization_app/personalization_app_mocha_test_base.h"
 #include "content/public/test/browser_test.h"
 
 // TODO(b/312208348) move this test to ash common sea_pen browsertest.
@@ -16,6 +17,26 @@ using SeaPenControllerTest = PersonalizationAppMojomBannedMochaTestBase;
 IN_PROC_BROWSER_TEST_F(SeaPenControllerTest, All) {
   RunTest("chromeos/personalization_app/sea_pen_controller_test.js",
           "mocha.run()");
+}
+
+// Tests the SeaPen UI.
+// TODO(b/329149811) Add screenplay id
+class PersonalizationAppSeaPenBrowserTest
+    : public PersonalizationAppMochaTestBase {
+ public:
+  PersonalizationAppSeaPenBrowserTest() = default;
+  PersonalizationAppSeaPenBrowserTest(
+      const PersonalizationAppSeaPenBrowserTest&) = delete;
+  PersonalizationAppSeaPenBrowserTest& operator=(
+      const PersonalizationAppSeaPenBrowserTest&) = delete;
+
+  ~PersonalizationAppSeaPenBrowserTest() override = default;
+};
+
+IN_PROC_BROWSER_TEST_F(PersonalizationAppSeaPenBrowserTest, SeaPen) {
+  RunTestWithoutTestLoader(
+      "chromeos/personalization_app/personalization_app_test.js",
+      "runMochaSuite('sea pen')");
 }
 
 }  // namespace ash::personalization_app
