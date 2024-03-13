aa69550ba15c71d688aea9bce937be5fa8f19a03
danakj <danakj@chromium.org>
2024-03-13 17:12:12 +0000

    Migrating code from base::ByteSwap and friends to byte conversions
    ByteSwap + memcpy (or similar via iterators) can be spelt more clearly
    with the new base::numerics byte conversions to convert to and from
    byte arrays of big/little endian.
    
    Some code uses BigEndianReader + ByteSwap to read little endian
    buffers! Introduce SpanReader to provide a simple api around reading
    off the front of the span dynamically in place of using many nested
    split_at() calls.
    
    SpanReader overlaps with BufferIterator in some ways, but it provides
    a more ergonomic API for reading off spans (including fixed size
    spans) and dealing with running out of room explicitly, like
    BigEndianReader did. And it does not introduce the complexity of
    worrying about seeking or truncating to users of the API.
    
    Bug: 40284755
    Change-Id: I1a3c44f0016e74f17c6282a51e5eca9dae7fad01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5353355
    Reviewed-by: Tomas Gunnarsson <tommi@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272236}

--- device/fido/cable/noise.cc
+++ device/fido/cable/noise.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string.h>
 
 #include "base/check_op.h"
-#include "base/sys_byteorder.h"
+#include "base/numerics/byte_conversions.h"
 #include "crypto/aead.h"
 #include "crypto/sha2.h"
 #include "device/fido/fido_constants.h"
@@ -108,9 +108,9 @@ void Noise::MixKeyAndHash(base::span<const uint8_t> ikm) {
 
 std::vector<uint8_t> Noise::EncryptAndHash(
     base::span<const uint8_t> plaintext) {
-  uint8_t nonce[12] = {0};
-  const uint32_t counter = base::ByteSwap(symmetric_nonce_);
-  memcpy(nonce, &counter, sizeof(counter));
+  uint8_t nonce[12] = {};
+  base::span(nonce).first<4u>().copy_from(
+      base::numerics::U32ToBigEndian(symmetric_nonce_));
   symmetric_nonce_++;
 
   crypto::Aead aead(crypto::Aead::AES_256_GCM);
@@ -122,9 +122,9 @@ std::vector<uint8_t> Noise::EncryptAndHash(
 
 std::optional<std::vector<uint8_t>> Noise::DecryptAndHash(
     base::span<const uint8_t> ciphertext) {
-  uint8_t nonce[12] = {0};
-  const uint32_t counter = base::ByteSwap(symmetric_nonce_);
-  memcpy(nonce, &counter, sizeof(counter));
+  uint8_t nonce[12] = {};
+  base::span(nonce).first<4u>().copy_from(
+      base::numerics::U32ToBigEndian(symmetric_nonce_));
   symmetric_nonce_++;
 
   crypto::Aead aead(crypto::Aead::AES_256_GCM);
