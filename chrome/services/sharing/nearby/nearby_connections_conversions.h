1114dc46b4fd96e45724194e89b0968803b70b97
Crisrael Lucero <crisrael@google.com>
2024-03-13 19:24:57 +0000

    [Nearby Presence] Use AuthenticationStatus in ConnectionListenerV3
    This change is a follow-up to cl/613278476 and properly uses the
    `AuthenticationStatus` as opposed to the hardcoded success enum.
    
    Test: updated unit tests to properly use AuthenticationStatus
    Change-Id: Icfdc7c3065d5e4e5371c2947d7ece3d73d38d182
    Fixed: b/314197753
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5357702
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Auto-Submit: Crisrael Lucero <crisrael@google.com>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#1272350}

--- chrome/services/sharing/nearby/nearby_connections_conversions.h
+++ chrome/services/sharing/nearby/nearby_connections_conversions.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/services/nearby/public/mojom/nearby_connections_types.mojom-forward.h"
 #include "third_party/nearby/src/connections/params.h"
 #include "third_party/nearby/src/connections/v3/bandwidth_info.h"
+#include "third_party/nearby/src/internal/interop/authentication_status.h"
 
 namespace nearby::connections {
 
@@ -37,6 +38,9 @@ mojom::BandwidthQuality BandwidthQualityToMojom(v3::Quality quality);
 
 BooleanMediumSelector MediumSelectorFromMojom(
     mojom::MediumSelection* allowed_mediums);
+
+mojom::AuthenticationStatus AuthenticationStatusToMojom(
+    AuthenticationStatus status);
 
 }  // namespace nearby::connections
 
