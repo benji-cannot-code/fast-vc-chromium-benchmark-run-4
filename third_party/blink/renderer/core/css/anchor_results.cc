b5238a6a9693dae400114025d7f7715c05366dd1
Anders Hartvoll Ruud <andruud@chromium.org>
2024-03-13 12:43:55 +0000

    [anchor] Generate flips using the try-tactics layer
    Previous CLs (CL:5349527 and preceding) established a way for
    an internal CSSPropertyValueSet (containing CSSFlipRevertValues)
    to be added at the "try tactics layer" in the cascade.
    This made it possible to carry out the various "flips" required
    by CSS Anchor Positioning, even with interleaving anchor()
    implemented (crbug.com/41483417).
    
    This CL uses the value specified in the position-try-options
    property to generate those value-flipping CSSPropertyValueSets,
    and adds them to the cascade.
    
    For now, the flips only apply to insets and sizing properties.
    Additionally, values are not yet rewritten during the flip
    (e.g. anchor(left) to anchor(right), etc). This behavior will
    be added separately.
    
    Bug: 40279608
    Change-Id: Iab85e96e343df1f4527a51e60e10154b4c938ee1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5366066
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272096}

--- third_party/blink/renderer/core/css/anchor_results.cc
+++ third_party/blink/renderer/core/css/anchor_results.cc
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2024 The Chromium Authors
-// Use of this source code is governed by a BSD-style license t_t can be
+// Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "third_party/blink/renderer/core/css/anchor_results.h"
--- third_party/blink/renderer/core/layout/out_of_flow_layout_part.cc
+++ third_party/blink/renderer/core/layout/out_of_flow_layout_part.cc
@@ -190,14 +190,18 @@ class OOFCandidateStyleIterator {
          ++*try_option_index_) {
       const PositionTryOption& option =
           position_try_options_->GetOptions()[*try_option_index_];
-      // TODO(crbug.com/40279608): Handle flip-block/inline/start values.
+      const CSSPropertyValueSet* properties = nullptr;
       if (const ScopedCSSName* name = option.GetPositionTryName()) {
-        if (const StyleRulePositionTry* rule = GetPositionTryRule(*name)) {
-          style_ = UpdateStyle(&rule->Properties());
-          CHECK(style_);
-          return true;
+        const StyleRulePositionTry* rule = GetPositionTryRule(*name);
+        if (!rule) {
+          // @position-try option does not exist.
+          continue;
         }
+        properties = &rule->Properties();
       }
+      style_ = UpdateStyle(properties, option.GetTryTactic());
+      CHECK(style_);
+      return true;
     }
     return false;
   }
@@ -205,7 +209,7 @@ class OOFCandidateStyleIterator {
   void MoveToStyleWithoutOptions() {
     CHECK(element_);
     CHECK(position_try_options_);
-    style_ = UpdateStyle(/* try_set */ nullptr);
+    style_ = UpdateStyle(/* try_set */ nullptr, kNoTryTactics);
   }
 
  private:
@@ -220,7 +224,7 @@ class OOFCandidateStyleIterator {
       // Note that UpdateStyle returns early without any update
       // if the incoming try_set matches the set on OutOfFlowData
       // (including the case where both are nullptr).
-      style_ = UpdateStyle(/* try_set */ nullptr);
+      style_ = UpdateStyle(/* try_set */ nullptr, kNoTryTactics);
     }
   }
 
@@ -231,7 +235,8 @@ class OOFCandidateStyleIterator {
         scoped_name);
   }
 
-  const ComputedStyle* UpdateStyle(const CSSPropertyValueSet* try_set) {
+  const ComputedStyle* UpdateStyle(const CSSPropertyValueSet* try_set,
+                                   const TryTacticList& tactic_list) {
     CHECK(element_);
     if (RuntimeEnabledFeatures::CSSAnchorPositioningCascadeFallbackEnabled()) {
       StyleEngine& style_engine = element_->GetDocument().GetStyleEngine();
@@ -239,7 +244,7 @@ class OOFCandidateStyleIterator {
           RuntimeEnabledFeatures::CSSAnchorPositioningCascadeFallbackEnabled()
               ? anchor_evaluator_
               : nullptr;
-      style_engine.UpdateStyleForOutOfFlow(*element_, try_set,
+      style_engine.UpdateStyleForOutOfFlow(*element_, try_set, tactic_list,
                                            anchor_evaluator);
     }
     CHECK(element_->GetLayoutObject());
@@ -248,7 +253,6 @@ class OOFCandidateStyleIterator {
     // propagation of writing modes.
     return element_->GetLayoutObject()->Style();
   }
-
   Element* element_ = nullptr;
 
   // The current candidate style if no auto anchor fallback is triggered.
