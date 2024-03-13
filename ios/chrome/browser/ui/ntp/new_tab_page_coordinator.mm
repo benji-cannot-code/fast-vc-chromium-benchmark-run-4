75a662763b40e51fdf79e525907ad90d662e12ad
Chris Lu <thegreenfrog@chromium.org>
2024-03-13 09:23:37 +0000

    [ios] Remove kFakebox Home action enum type
    kOmnibox is the replacement enum type and actually indicates a
    navigation action.
    
    Change-Id: I6082aff3cedbec5599ae688321c0bfc70e67a06b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5365793
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272046}

--- ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
@@ -811,7 +811,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)fakeboxTapped {
-  RecordHomeAction(IOSHomeActionType::kFakebox, [self isStartSurface]);
   [self focusFakebox];
 }
 
