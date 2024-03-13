5827ae384e56e7643ac7e798ae5f64958e132f8c
Jan Keitel <jkeitel@google.com>
2024-03-13 16:49:22 +0000

    Remove test-only code from PlusAddressService.
    This CL moves a few more PlusAddressService objects into test fixtures
    and removes the shorthand constructors. It also makes formerly
    protected members and methods of PlusAddressService private.
    
    Bug: 322279583
    Change-Id: I4e1244c37d568567931cc11368f11cb113bf366e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5365161
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1272215}

--- components/plus_addresses/plus_address_service.cc
+++ components/plus_addresses/plus_address_service.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/plus_addresses/plus_address_metrics.h"
 #include "components/plus_addresses/plus_address_prefs.h"
 #include "components/plus_addresses/plus_address_types.h"
+#include "components/plus_addresses/webdata/plus_address_webdata_service.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/public/base/consent_level.h"
 #include "components/signin/public/base/persistent_repeating_timer.h"
@@ -54,26 +55,6 @@ std::string GetEtldPlusOne(const url::Origin origin) {
 }  // namespace
 
 PlusAddressService::PlusAddressService(
-    signin::IdentityManager* identity_manager)
-    : PlusAddressService(identity_manager,
-                         /*pref_service=*/nullptr,
-                         std::make_unique<PlusAddressHttpClientImpl>(
-                             identity_manager,
-                             /*url_loader_factory=*/nullptr),
-                         /*webdata_service=*/nullptr) {}
-
-PlusAddressService::PlusAddressService()
-    : PlusAddressService(
-          /*identity_manager=*/nullptr,
-          /*pref_service=*/nullptr,
-          std::make_unique<PlusAddressHttpClientImpl>(
-              /*identity_manager=*/nullptr,
-              /*url_loader_factory=*/nullptr),
-          /*webdata_service=*/nullptr) {}
-
-PlusAddressService::~PlusAddressService() = default;
-
-PlusAddressService::PlusAddressService(
     signin::IdentityManager* identity_manager,
     PrefService* pref_service,
     std::unique_ptr<PlusAddressHttpClient> plus_address_http_client,
@@ -93,6 +74,8 @@ PlusAddressService::PlusAddressService(
     webdata_service_->GetPlusProfiles(this);
   }
 }
+
+PlusAddressService::~PlusAddressService() = default;
 
 bool PlusAddressService::SupportsPlusAddresses(const url::Origin& origin,
                                                bool is_off_the_record) const {
