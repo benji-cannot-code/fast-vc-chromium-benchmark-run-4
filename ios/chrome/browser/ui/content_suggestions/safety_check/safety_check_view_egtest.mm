a470cffe130a6c28428bb33d66030e40e255c7f9
Louis Romero <lpromero@google.com>
2024-03-13 11:06:17 +0000

    Revert "[iOS] Set safetyCheckModuleContainer's delegate."
    This reverts commit e109ee3ee4bb4294bd51496dcac89c9127671804.
    
    Reason for revert: Makes SafetyCheckViewTestCase/testPasswordCheckupDismissedAfterFailedAuthentication and SafetyCheckViewTestCase/testPasswordCheckupDismissedAfterAllPasswordsGone fail.
    
    Original change's description:
    > [iOS] Set safetyCheckModuleContainer's delegate.
    >
    > Currently, safetyCheckModuleContainer's delegate is not set to
    > ContentSuggestionsViewController.
    >
    > This means the Safety Check (Magic Stack) module cannot be hidden via
    > the '"Hide" Safety Check' action button via the context menu.
    >
    > This change sets the delegate so the Safety Check module can be hidden
    > again.
    >
    > Change-Id: Ifca16a7551d0305a31f0bbfc079134fe2e116e36
    > Bug: 329124001
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5362638
    > Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    > Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1271893}
    
    Bug: 329124001, 329376569
    Change-Id: I6b80240728a720c9f47dc0587c32228e3493279e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5369910
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Owners-Override: Louis Romero <lpromero@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1272067}

--- ios/chrome/browser/ui/content_suggestions/safety_check/safety_check_view_egtest.mm
+++ ios/chrome/browser/ui/content_suggestions/safety_check/safety_check_view_egtest.mm
@@ -65,14 +65,6 @@ void WaitUntilSafetyCheckModuleVisibleOrTimeout(bool should_show) {
                                     ReauthenticationResult::kSuccess];
   [ChromeEarlGrey resetDataForLocalStatePref:
                       safety_check_prefs::kSafetyCheckInMagicStackDisabledPref];
-
-  if (![ChromeEarlGrey isIPadIdiom]) {
-    // Rotate iphone device so Magic Stack can be scrollable.
-    [EarlGrey rotateDeviceToOrientation:UIDeviceOrientationLandscapeLeft
-                                  error:nil];
-    [[EarlGrey selectElementWithMatcher:chrome_test_util::NTPCollectionView()]
-        performAction:grey_scrollInDirection(kGREYDirectionDown, 180)];
-  }
 }
 
 - (void)tearDown {
@@ -95,7 +87,7 @@ void WaitUntilSafetyCheckModuleVisibleOrTimeout(bool should_show) {
 
 // Tests that long pressing the Safety Check view displays a context menu; tests
 // the Safety Check view is properly hidden via the context menu.
-- (void)testLongPressAndHide {
+- (void)DISABLED_testLongPressAndHide {
   [[[EarlGrey
       selectElementWithMatcher:grey_allOf(grey_accessibilityID(
                                               safety_check::kSafetyCheckViewID),
@@ -121,7 +113,7 @@ void WaitUntilSafetyCheckModuleVisibleOrTimeout(bool should_show) {
 
 // Tests that the Password Checkup view is dismissed when there are no saved
 // passwords.
-- (void)testPasswordCheckupDismissedAfterAllPasswordsGone {
+- (void)DISABLED_testPasswordCheckupDismissedAfterAllPasswordsGone {
   password_manager_test_utils::SavePasswordFormToProfileStore();
 
   [[[EarlGrey
@@ -169,7 +161,7 @@ void WaitUntilSafetyCheckModuleVisibleOrTimeout(bool should_show) {
 
 // Tests that the Password Checkup view is dismissed when the user doesn't pass
 // Local Authentication.
-- (void)testPasswordCheckupDismissedAfterFailedAuthentication {
+- (void)DISABLED_testPasswordCheckupDismissedAfterFailedAuthentication {
   password_manager_test_utils::SavePasswordFormToProfileStore();
 
   [[[EarlGrey
