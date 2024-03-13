28c164bf03916b1e5da8abe8fbd2f24217cb335f
Saifuddin Hitawala <hitawala@chromium.org>
2024-03-13 18:06:28 +0000

    [ozone] Remove NativePixmapGLBinding::GetDataType
    NativePixmapGLBinding::GetDataType() is no longer being used after
    removal of validating support from ozone backing gl texture holders.
    
    Bug: 40135856
    Change-Id: I95775fd0118f2953e75ea920274b020fbe8f1e71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5368593
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272290}

--- ui/ozone/common/native_pixmap_egl_binding.h
+++ ui/ozone/common/native_pixmap_egl_binding.h
@@ -40,7 +40,6 @@ class NativePixmapEGLBinding : public NativePixmapGLBinding {
 
   // NativePixmapGLBinding:
   GLuint GetInternalFormat() override;
-  GLenum GetDataType() override;
 
  private:
   // Create an EGLImage from a given NativePixmap and bind |texture_id| to
