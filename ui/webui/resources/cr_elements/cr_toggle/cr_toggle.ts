9f0ca80f0332fdb09acdb33cf09b1a64af31d994
John Lee <johntlee@chromium.org>
2024-03-13 17:56:07 +0000

    WebUI cr-toggle: Remove support for [dark] attribute
    WebUI pages opened in incognito automatically now show in dark mode,
    which means the Incognito NTP WebUI pages can rely on the default
    behavior of CSS matching (prefers-color-scheme: dark) and no longer
    need the [dark] attribute to force <cr-toggle>s to dark mode.
    
    Also enable chrome-refresh-2023 styles on these pages to get the new
    toggles.
    
    incognito_tab.html: http://screen/78yA5NpPKTtQiye
    revamped_incognito_tab.html: http://screen/nVdj6xB9TKZMwma
    
    Bug: 326116364
    Change-Id: Iad79c8b43a2f2db10017580a60be966087842430
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5366695
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272281}

--- ui/webui/resources/cr_elements/cr_toggle/cr_toggle.ts
+++ ui/webui/resources/cr_elements/cr_toggle/cr_toggle.ts
@@ -51,11 +51,6 @@ export class CrToggleElement extends CrToggleElementBase {
         notify: true,
       },
 
-      dark: {
-        type: Boolean,
-        reflect: true,
-      },
-
       disabled: {
         type: Boolean,
         reflect: true,
@@ -64,7 +59,6 @@ export class CrToggleElement extends CrToggleElementBase {
   }
 
   checked: boolean = false;
-  dark: boolean = false;
   disabled: boolean = false;
 
   private boundPointerMove_: ((e: PointerEvent) => void)|null = null;
