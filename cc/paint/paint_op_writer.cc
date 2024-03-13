eeb5666ba6f9658113aaf4e37605d716e0393115
Brian Osman <brianosman@google.com>
2024-03-13 17:13:43 +0000

    Remove image serialization procs from Slug serialization
    These are no longer needed - images applied to slugs are serialized via
    the paint (external to the slug), and re-supplied when we draw the slug.
    
    Bug: b/325463564
    Change-Id: I70abd7d5b25fe5fbe08a722a00e816404c42bf27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5368592
    Reviewed-by: Kevin Lubick <kjlubick@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Cr-Commit-Position: refs/heads/main@{#1272237}

--- cc/paint/paint_op_writer.cc
+++ cc/paint/paint_op_writer.cc
@@ -37,10 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkRegion.h"
 #include "third_party/skia/include/core/SkSamplingOptions.h"
 #include "third_party/skia/include/core/SkScalar.h"
-#include "third_party/skia/include/core/SkSerialProcs.h"
 #include "third_party/skia/include/core/SkSize.h"
 #include "third_party/skia/include/effects/SkHighContrastFilter.h"
-#include "third_party/skia/include/encode/SkPngEncoder.h"
 #include "third_party/skia/include/private/chromium/SkChromeRemoteGlyphCache.h"
 #include "third_party/skia/include/private/chromium/SkImageChromium.h"
 #include "third_party/skia/include/private/chromium/Slug.h"
@@ -516,19 +514,8 @@ void PaintOpWriter::Write(const sk_sp<sktext::gpu::Slug>& slug) {
   if (slug) {
     // TODO(penghuang): should we use a unique id to avoid sending the same
     // slug?
-    SkSerialProcs procs;
-    procs.fImageProc = [](SkImage* img, void*) -> sk_sp<SkData> {
-      if (!img) {
-        return nullptr;
-      }
-      // Slugs never use GPU-backed images because OOP-R does not use
-      // GrDirectContext.
-      CHECK(!img->isTextureBacked());
-      return SkPngEncoder::Encode(nullptr, img, SkPngEncoder::Options{});
-    };
     bytes_written = slug->serialize(
-        memory_, base::bits::AlignDown(remaining_bytes(), kDefaultAlignment),
-        procs);
+        memory_, base::bits::AlignDown(remaining_bytes(), kDefaultAlignment));
     if (bytes_written == 0u) {
       valid_ = false;
       return;
