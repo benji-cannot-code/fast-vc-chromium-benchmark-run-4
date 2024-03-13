0ddb83976d524f6918db6f364c041d4397b0e4af
dpapad <dpapad@chromium.org>
2024-03-13 01:14:09 +0000

    WebUI: Migrate cr-button to LitElement.
    As part of the migration adding an explicit test case to capture
    previously undefined behavior when  cr-button's 'tabindex' is  set by
    the parent element. This behavior is odd since cr-button already
    provides 'customTabIndex' attribute to achieve a similar behavior, but
    at the same time there are parts of the code relying on such behavior.
    
    Also adding missing cr_hidden_style.css.js import in
    chrome/browser/resources/intro/lacros_app.ts revealed by the migration
    (it was previously transitively satisfiyng this dependency via
    cr-button).
    
    Bug: 40943652
    Change-Id: I4571ba2468d1d5a3e2bae3267446bb0f514882a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5361147
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271926}

--- chrome/browser/resources/intro/lacros_app.ts
+++ chrome/browser/resources/intro/lacros_app.ts
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://intro/tangible_sync_style_shared.css.js';
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
+import 'chrome://resources/cr_elements/cr_hidden_style.css.js';
 import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
 import 'chrome://resources/cr_elements/icons.html.js';
 import './strings.m.js';
--- chrome/test/data/webui/settings/startup_urls_page_test.ts
+++ chrome/test/data/webui/settings/startup_urls_page_test.ts
@@ -12,6 +12,7 @@ import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min
 import type {SettingsStartupUrlDialogElement,SettingsStartupUrlEntryElement, SettingsStartupUrlsPageElement, StartupUrlsPageBrowserProxy} from 'chrome://settings/settings.js';
 import {EDIT_STARTUP_URL_EVENT, StartupUrlsPageBrowserProxyImpl} from 'chrome://settings/settings.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {microtasksFinished} from 'chrome://webui-test/test_util.js';
 import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
 
 // clang-format on
@@ -187,10 +188,12 @@ suite('StartupUrlDialog', function() {
     // Input a URL and force validation.
     const inputElement = dialog.$.url;
     inputElement.value = 'foo.com';
-    await inputElement.updateComplete;
+    await microtasksFinished();
     pressSpace(inputElement);
 
     await browserProxy.whenCalled('validateStartupPage');
+    // Wait for the action button to become enabled.
+    await microtasksFinished();
     keyEventOn(inputElement, 'keypress', 13, undefined, 'Enter');
 
     await browserProxy.whenCalled('addStartupPage');
