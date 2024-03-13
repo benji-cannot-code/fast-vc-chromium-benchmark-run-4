67b496ab65e2797760d7748681aff61e7758ee04
jhtin <jhtin@chromium.org>
2024-03-13 07:52:29 +0000

    IME user data service: Add ImeService Bind function to set up service
    Bug: b/329173273
    Change-Id: I0bf2ba963c8324f31584685a0b4317eaf0af1f71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5364254
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: jhtin <jhtin@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272022}

--- chromeos/ash/services/ime/ime_service.cc
+++ chromeos/ash/services/ime/ime_service.cc
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/services/ime/constants.h"
 #include "chromeos/ash/services/ime/decoder/decoder_engine.h"
 #include "chromeos/ash/services/ime/decoder/system_engine.h"
+#include "chromeos/ash/services/ime/ime_shared_library_wrapper.h"
+#include "chromeos/ash/services/ime/input_method_user_data_service_impl.h"
 #include "mojo/public/c/system/thunks.h"
 
 namespace ash {
@@ -75,6 +77,21 @@ void ImeService::BindInputEngineManager(
 void ImeService::ResetAllBackendConnections() {
   proto_mode_shared_lib_engine_.reset();
   mojo_mode_shared_lib_engine_.reset();
+}
+
+void ImeService::BindInputMethodUserDataService(
+    mojo::PendingReceiver<mojom::InputMethodUserDataService> receiver) {
+  if (input_method_user_data_api_ == nullptr) {
+    std::optional<ImeSharedLibraryWrapper::EntryPoints> entry_points =
+        ime_shared_library_->MaybeLoadThenReturnEntryPoints();
+    if (!entry_points.has_value()) {
+      LOG(ERROR) << "shared library entry_points not loaded";
+      return;
+    }
+    input_method_user_data_api_ =
+        std::make_unique<InputMethodUserDataServiceImpl>(this, *entry_points);
+  }
+  input_method_user_data_api_->AddReceiver(std::move(receiver));
 }
 
 void ImeService::ConnectToImeEngine(
