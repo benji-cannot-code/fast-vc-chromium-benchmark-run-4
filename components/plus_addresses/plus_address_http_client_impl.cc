d44f508f8107c8d0031463d16fcc965dc96832e4
Jan Keitel <jkeitel@google.com>
2024-03-13 17:58:03 +0000

    Run plus address client request callbacks on reset.
    Bug: 322279583
    Change-Id: Icf2c1961f0f2d86e1904953581fe7c1b957f7a7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5366203
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1272283}

--- components/plus_addresses/plus_address_http_client_impl.cc
+++ components/plus_addresses/plus_address_http_client_impl.cc
@@ -5,7 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/plus_addresses/plus_address_http_client_impl.h"
 
+#include <concepts>
+#include <memory>
 #include <optional>
+#include <string>
+#include <utility>
 
 #include "base/functional/bind.h"
 #include "base/json/json_writer.h"
@@ -36,6 +40,8 @@ namespace plus_addresses {
 namespace {
 
 constexpr base::TimeDelta kRequestTimeout = base::Seconds(5);
+constexpr auto kSignoutError =
+    PlusAddressRequestError(PlusAddressRequestErrorType::kUserSignedOut);
 
 // See docs/network_traffic_annotations.md for reference.
 // TODO(b/295556954): Update the description and trigger fields when possible.
@@ -144,6 +150,55 @@ std::optional<int> GetResponseCode(network::SimpleURLLoader* loader) {
              : std::optional<int>();
 }
 
+// Helper wrapper around a `base::OnceCallback` that runs the `callback` with
+// argument `arg_on_destroy` if the callback is destroyed and has not been run
+// at that point in time.
+template <typename T>
+class RunOnDestroyHelper final {
+ public:
+  using Callback = base::OnceCallback<void(const T&)>;
+
+  RunOnDestroyHelper(Callback callback, T arg_on_destroy)
+      : callback_(std::move(callback)),
+        arg_on_destroy_(std::move(arg_on_destroy)) {}
+
+  RunOnDestroyHelper(const RunOnDestroyHelper&) = delete;
+  RunOnDestroyHelper(RunOnDestroyHelper&&) = default;
+  RunOnDestroyHelper& operator=(const RunOnDestroyHelper&) = delete;
+  RunOnDestroyHelper& operator=(RunOnDestroyHelper&&) = default;
+
+  ~RunOnDestroyHelper() {
+    if (callback_) {
+      std::move(callback_).Run(arg_on_destroy_);
+    }
+  }
+
+  void Run(const T& arg) && {
+    std::move(callback_).Run(arg);
+    callback_.Reset();
+  }
+
+ private:
+  Callback callback_;
+  T arg_on_destroy_;
+};
+
+// Given a `base::OnceCallback<void(const T&)>` `callback`, it returns another
+// `base::OnceCallback` of the same signature with the property that the
+// returned callback is run with argument `arg_on_destroy` on its destruction if
+// it has not been run before.
+template <typename T, typename V>
+  requires(std::constructible_from<T, V>)
+base::OnceCallback<void(const T&)> WrapAsAutorunCallback(
+    base::OnceCallback<void(const T&)> callback,
+    V arg_on_destroy) {
+  return base::BindOnce(
+      [](RunOnDestroyHelper<T> helper, const T& profile) {
+        std::move(helper).Run(profile);
+      },
+      RunOnDestroyHelper<T>(std::move(callback), std::move(arg_on_destroy)));
+}
+
 }  // namespace
 
 PlusAddressHttpClientImpl::PlusAddressHttpClientImpl(
@@ -162,9 +217,11 @@ void PlusAddressHttpClientImpl::ReservePlusAddress(
   if (!server_url_) {
     return;
   }
-  GetAuthToken(base::BindOnce(&PlusAddressHttpClientImpl::ReservePlusAddressInternal,
-                              base::Unretained(this), origin,
-                              std::move(on_completed)));
+  GetAuthToken(
+      base::BindOnce(&PlusAddressHttpClientImpl::ReservePlusAddressInternal,
+                     base::Unretained(this), origin,
+                     WrapAsAutorunCallback(std::move(on_completed),
+                                           base::unexpected(kSignoutError))));
 }
 
 void PlusAddressHttpClientImpl::ConfirmPlusAddress(
@@ -174,9 +231,11 @@ void PlusAddressHttpClientImpl::ConfirmPlusAddress(
   if (!server_url_) {
     return;
   }
-  GetAuthToken(base::BindOnce(&PlusAddressHttpClientImpl::ConfirmPlusAddressInternal,
-                              base::Unretained(this), origin, plus_address,
-                              std::move(on_completed)));
+  GetAuthToken(
+      base::BindOnce(&PlusAddressHttpClientImpl::ConfirmPlusAddressInternal,
+                     base::Unretained(this), origin, plus_address,
+                     WrapAsAutorunCallback(std::move(on_completed),
+                                           base::unexpected(kSignoutError))));
 }
 
 void PlusAddressHttpClientImpl::GetAllPlusAddresses(
@@ -184,8 +243,11 @@ void PlusAddressHttpClientImpl::GetAllPlusAddresses(
   if (!server_url_) {
     return;
   }
-  GetAuthToken(base::BindOnce(&PlusAddressHttpClientImpl::GetAllPlusAddressesInternal,
-                              base::Unretained(this), std::move(on_completed)));
+  GetAuthToken(
+      base::BindOnce(&PlusAddressHttpClientImpl::GetAllPlusAddressesInternal,
+                     base::Unretained(this),
+                     WrapAsAutorunCallback(std::move(on_completed),
+                                           base::unexpected(kSignoutError))));
 }
 
 void PlusAddressHttpClientImpl::Reset() {
