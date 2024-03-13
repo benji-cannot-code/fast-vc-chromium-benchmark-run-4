fdd78698eebdf331335d33ef9adb49c230595fa0
Jan Keitel <jkeitel@google.com>
2024-03-13 09:24:08 +0000

    Do not return early from FakePlusAddressService.
    Bug: 322279583
    Change-Id: I05cae14d82372f549377690cd75bf2a48a5e8350
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5366049
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1272047}

--- components/plus_addresses/fake_plus_address_service.cc
+++ components/plus_addresses/fake_plus_address_service.cc
@@ -55,7 +55,6 @@ void FakePlusAddressService::ConfirmPlusAddress(
   if (on_confirmed_) {
     std::move(on_confirmed_).Run(profile);
     on_confirmed_.Reset();
-    return;
   }
   std::move(on_completed).Run(profile);
 }
