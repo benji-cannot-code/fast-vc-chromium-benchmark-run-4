e07d4496dbfcf038acd540499f13727f71886fb1
Svend Larsen <svend@chromium.org>
2024-03-13 17:34:25 +0000

    [DIPS] Replace InteractionOrWaa in DIPS names with ProtectiveEvent
    OBSOLETE_HISTOGRAM[Privacy.DIPS.Database.Operation.FilterSitesWithInteractionOrWaaTime]=Replaced by Privacy.DIPS.Database.Operation.FilterSitesWithProtectiveEventTime
    
    Bug: 329346769
    Change-Id: I9cf406f5a0f44169235b666d471ddb118c2446b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5370071
    Commit-Queue: Svend Larsen <svend@chromium.org>
    Reviewed-by: Jonathan Njeunje <njeunje@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272260}

--- chrome/browser/dips/dips_database.cc
+++ chrome/browser/dips/dips_database.cc
@@ -909,7 +909,7 @@ std::vector<std::string> DIPSDatabase::GetSitesThatUsedStorage(
   return sites;
 }
 
-std::set<std::string> DIPSDatabase::FilterSitesWithInteractionOrWaa(
+std::set<std::string> DIPSDatabase::FilterSitesWithProtectiveEvent(
     const std::set<std::string>& sites) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   if (!CheckDBInit()) {
@@ -917,7 +917,7 @@ std::set<std::string> DIPSDatabase::FilterSitesWithInteractionOrWaa(
   }
 
   SCOPED_UMA_HISTOGRAM_TIMER(
-      "Privacy.DIPS.Database.Operation.FilterSitesWithInteractionOrWaaTime");
+      "Privacy.DIPS.Database.Operation.FilterSitesWithProtectiveEventTime");
 
   ClearExpiredRows();
 
@@ -936,7 +936,7 @@ std::set<std::string> DIPSDatabase::FilterSitesWithInteractionOrWaa(
     i++;
   }
 
-  std::set<std::string> interacted_sites;
+  std::set<std::string> sites_with_protective_event;
   while (statement.Step()) {
     std::optional<base::Time> last_user_interaction =
         ColumnOptionalTime(&statement, 1);
@@ -945,10 +945,10 @@ std::set<std::string> DIPSDatabase::FilterSitesWithInteractionOrWaa(
 
     if (last_user_interaction.has_value() ||
         last_web_authn_assertion_time.has_value()) {
-      interacted_sites.insert(statement.ColumnString(0));
+      sites_with_protective_event.insert(statement.ColumnString(0));
     }
   }
-  return interacted_sites;
+  return sites_with_protective_event;
 }
 
 size_t DIPSDatabase::ClearExpiredRows() {
