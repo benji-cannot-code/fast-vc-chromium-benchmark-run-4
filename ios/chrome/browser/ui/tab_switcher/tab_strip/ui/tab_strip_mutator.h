7391a55285d88266c752b212b121e182de8e98a0
Quentin Pubert <qpubert@google.com>
2024-03-13 17:26:08 +0000

    [iOS][TabStrip] Add context menu action to add a tab to a new group
    This CL adds a context menu action to add a tab to a new group.
    
    Bug: 328786199
    Change-Id: I62c24b350fe07ee5f00318df30d38752a7b8766b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5353078
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1272251}

--- ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_mutator.h
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_mutator.h
@@ -23,6 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /// Tells the receiver to close all items except `item`.
 - (void)closeAllItemsExcept:(TabSwitcherItem*)item;
 
+/// Tells the receiver to create a new group containing `item`.
+- (void)createNewGroupWithItem:(TabSwitcherItem*)item;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TAB_STRIP_UI_TAB_STRIP_MUTATOR_H_
