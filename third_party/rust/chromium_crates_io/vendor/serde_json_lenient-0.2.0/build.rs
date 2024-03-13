3f90e74e849291c982bf8505d7b75495b693bf1c
Dustin J. Mitchell <djmitche@chromium.org>
2024-03-13 14:39:55 +0000

    Roll serde_json_lenient to 0.2.0
    Principally, this is to get the split of `allow_newlines_in_strings` out
    of `allow_control_characters_in_strings`, but also includes some changes
    in 0.1.9 as well as changes merged from `serde_json`.
    
    This does not remove v0.1.8 nor update Chromium to use the new version.
    
    Bug: 40812356
    Change-Id: Ie2f8d028c6d00b96b6889a7cc9365bddccc66c4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5361256
    Commit-Queue: danakj <danakj@chromium.org>
    Auto-Submit: Dustin Mitchell <djmitche@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272142}

---
+++ third_party/rust/chromium_crates_io/vendor/serde_json_lenient-0.2.0/build.rs
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+use std::env;
+
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+
+    // Decide ideal limb width for arithmetic in the float parser. Refer to
+    // src/lexical/math.rs for where this has an effect.
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    match target_arch.as_str() {
+        "aarch64" | "mips64" | "powerpc64" | "x86_64" | "loongarch64" => {
+            println!("cargo:rustc-cfg=limb_width_64");
+        }
+        _ => {
+            println!("cargo:rustc-cfg=limb_width_32");
+        }
+    }
+}
---
+++ third_party/rust/chromium_crates_io/vendor/serde_json_lenient-0.2.0/src/lexical/large_powers.rs
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Adapted from https://github.com/Alexhuszagh/rust-lexical.
+
+//! Precalculated large powers for limbs.
+
+#[cfg(limb_width_32)]
+pub(crate) use super::large_powers32::*;
+
+#[cfg(limb_width_64)]
+pub(crate) use super::large_powers64::*;
