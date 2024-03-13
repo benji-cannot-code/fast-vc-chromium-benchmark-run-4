c75d6f2a91d2c8be1fa17baa93e07513b70bb9dd
ahmedmoussa <ahmedmoussa@google.com>
2024-03-13 19:17:02 +0000

    Allow combobox to handle empty model without crashing
    The combobox API allow the option for empty models, but it fails to handle it correctly. It would always cause crashes. This CL fixes that issue by always making sure that `selected_index_.value()` is only called if `selected_index_.has_value()`.
    
    Bug: b:328262459
    Change-Id: Ic988a37c37c615a98ac913fcac118c927d0cb169
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5345511
    Commit-Queue: Ahmed Moussa <ahmedmoussa@google.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272343}

--- ui/views/controls/combobox/combobox_unittest.cc
+++ ui/views/controls/combobox/combobox_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/combobox/combobox.h"
 
 #include <memory>
+#include <optional>
 #include <set>
 #include <string>
 #include <utility>
@@ -24,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/models/combobox_model.h"
 #include "ui/base/models/combobox_model_observer.h"
 #include "ui/base/models/menu_model.h"
+#include "ui/base/models/simple_combobox_model.h"
 #include "ui/events/event.h"
 #include "ui/events/event_constants.h"
 #include "ui/events/event_utils.h"
@@ -912,6 +914,13 @@ TEST_F(ComboboxTest, NoCrashWhenComboboxOutlivesModel) {
 
 namespace {
 
+std::string GetComboboxA11yValue(Combobox* combobox) {
+  const std::optional<size_t>& selected_index = combobox->GetSelectedIndex();
+  return selected_index ? base::UTF16ToUTF8(combobox->GetModel()->GetItemAt(
+                              selected_index.value()))
+                        : std::string();
+}
+
 using ComboboxDefaultTest = ViewsTestBase;
 
 class ConfigurableComboboxModel final : public ui::ComboboxModel {
@@ -1012,6 +1021,40 @@ TEST_F(ComboboxDefaultTest, SetOwnedModelOverwriteOwned) {
     ASSERT_FALSE(destroyed_second);
   }
   EXPECT_TRUE(destroyed_second);
+}
+
+TEST_F(ComboboxDefaultTest, InteractionWithEmptyModel) {
+  ui::AXNodeData node_data;
+
+  // Empty model.
+  // Verify `GetAccessibleNodeData()` doesn't crash when interacting with empty
+  // model.
+  auto simple_model = std::make_unique<ui::SimpleComboboxModel>(
+      std::vector<ui::SimpleComboboxModel::Item>());
+  auto combobox = std::make_unique<Combobox>(simple_model.get());
+  combobox->GetAccessibleNodeData(&node_data);
+  EXPECT_EQ(0u, combobox->GetModel()->GetItemCount());
+  EXPECT_EQ(std::nullopt, combobox->GetSelectedIndex());
+  EXPECT_EQ(GetComboboxA11yValue(combobox.get()),
+            node_data.GetStringAttribute(ax::mojom::StringAttribute::kValue));
+
+  // Non-empty model.
+  simple_model->UpdateItemList({ui::SimpleComboboxModel::Item(u"item")});
+  combobox->GetAccessibleNodeData(&node_data);
+  EXPECT_EQ(1u, combobox->GetModel()->GetItemCount());
+  EXPECT_EQ(0u, combobox->GetSelectedIndex());
+  EXPECT_EQ(GetComboboxA11yValue(combobox.get()),
+            node_data.GetStringAttribute(ax::mojom::StringAttribute::kValue));
+
+  // Empty model.
+  // Verify `OnComboboxModelChanged()` doesn't crash when interacting with empty
+  // model.
+  simple_model->UpdateItemList({});
+  combobox->GetAccessibleNodeData(&node_data);
+  EXPECT_EQ(0u, combobox->GetModel()->GetItemCount());
+  EXPECT_EQ(std::nullopt, combobox->GetSelectedIndex());
+  EXPECT_EQ(GetComboboxA11yValue(combobox.get()),
+            node_data.GetStringAttribute(ax::mojom::StringAttribute::kValue));
 }
 
 }  // namespace views
