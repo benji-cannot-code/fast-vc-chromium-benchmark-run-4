7ff8f8ef065debefbe40fd293af5aab06842edd1
Nohemi Fernandez <fernandex@chromium.org>
2024-03-13 19:33:10 +0000

    Ensure kids test utils are consistent with test gaia id population.
    Unblocks crrev.com/c/5345255 which checks the primary account id with
    the kids management user id to populate the user's family member role.
    
    Bug: 327602312
    Change-Id: Iddb50a4f24e16e6f796e8b2f16ca738d75a0c714
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5366803
    Reviewed-by: Anthi Orfanou <anthie@google.com>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272354}

--- components/supervised_user/test_support/kids_chrome_management_test_utils.cc
+++ components/supervised_user/test_support/kids_chrome_management_test_utils.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/strcat.h"
 #include "base/strings/string_piece.h"
+#include "components/signin/public/identity_manager/identity_test_utils.h"
 #include "components/supervised_user/core/browser/proto/kidschromemanagement_messages.pb.h"
 
 namespace supervised_user {
@@ -18,14 +19,14 @@ void SetFamilyMemberAttributesForTesting(
     kids_chrome_management::FamilyRole role,
     base::StringPiece username) {
   mutable_member->mutable_profile()->set_display_name(std::string(username));
-  mutable_member->mutable_profile()->set_email(
-      base::StrCat({username, "@gmail.com"}));
+  const std::string email = base::StrCat({username, "@gmail.com"});
+  mutable_member->mutable_profile()->set_email(email);
   mutable_member->mutable_profile()->set_profile_url(
       base::StrCat({"http://profile.url/", username}));
   mutable_member->mutable_profile()->set_profile_image_url(
       base::StrCat({"http://image.url/", username}));
   mutable_member->set_role(role);
-  mutable_member->set_user_id(base::StrCat({"obfuscatedGaiaId", username}));
+  mutable_member->set_user_id(signin::GetTestGaiaIdForEmail(email));
 }
 
 }  // namespace supervised_user
