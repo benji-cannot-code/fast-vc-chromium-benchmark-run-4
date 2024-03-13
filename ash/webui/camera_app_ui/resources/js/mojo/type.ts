3697a00c9463a0f8779f59e45996e591de3c67b1
Wei Lee <wtlee@google.com>
2024-03-13 04:05:37 +0000

    CCA: Send remaining events through CrOS Events
    This CL sends the remaining events via CrOS Events, including:
    1. Open Camera Events
    2. Low Storage Action Events
    3. Barcode Detection Events
    4. Perf Events
    5. End Session Events
    
    METRICS_DOCUMENTATION_UPDATED=true
    
    Bug: b:325853230
    Test: Manually
    Change-Id: I26a018b0693243f1759ac59f7dccca0990d1a6b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5313966
    Reviewed-by: Chu-Hsuan Yang <chuhsuan@chromium.org>
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Reviewed-by: Jong Ahn <jongahn@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271975}

--- ash/webui/camera_app_ui/resources/js/mojo/type.ts
+++ ash/webui/camera_app_ui/resources/js/mojo/type.ts
@@ -26,9 +26,6 @@ export {
   StorageMonitorStatus,
   TabletModeMonitorCallbackRouter,
   ToteMetricFormat,
-  WifiEapMethod,
-  WifiEapPhase2Method,
-  WifiSecurityType,
   WindowStateControllerRemote,
   WindowStateMonitorCallbackRouter,
   WindowStateType,
@@ -39,6 +36,7 @@ export type {
 export {
   AndroidIntentResultType,
   AspectRatioSet,
+  BarcodeContentType,
   DocScanActionType,
   DocScanFixType,
   DocScanResultType,
@@ -46,16 +44,24 @@ export {
   GifResultType,
   GridType,
   LaunchType,
+  LowStorageActionType,
   Mode,
+  PerfEventType,
   RecordType,
   ResolutionLevel,
   ShutterType,
   TimerType,
+  UserBehavior,
 } from '../../mojom/ash/webui/camera_app_ui/events_sender.mojom-webui.js';
 export type {
   CaptureEventParams,
   EventsSenderRemote,
 } from '../../mojom/ash/webui/camera_app_ui/events_sender.mojom-webui.js';
+export {
+  WifiEapMethod,
+  WifiEapPhase2Method,
+  WifiSecurityType,
+} from '../../mojom/ash/webui/camera_app_ui/types.mojom-webui.js';
 export {
   Rotation,
 } from
