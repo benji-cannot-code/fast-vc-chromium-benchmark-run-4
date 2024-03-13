ad048ade6876b05db819f070662915505b39f76f
Shu Yang <shuyng@google.com>
2024-03-13 16:42:12 +0000

    Wait to load pdf until the view is attached to window
    Call #loadPdfFileIfNeeded when the view is attached to window or the
    download is completed.
    But only load the pdf file when both conditions are met.
    
    Bug: 325664951
    Change-Id: Ic24ff40cd9e4a26a06c3f6281db9bcc864a25840
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5368575
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1272208}

--- chrome/browser/ui/android/pdf/java/src/org/chromium/chrome/browser/pdf/PdfPageUnitTest.java
+++ chrome/browser/ui/android/pdf/java/src/org/chromium/chrome/browser/pdf/PdfPageUnitTest.java
@@ -7,8 +7,11 @@ package org.chromium.chrome.browser.pdf;
 
 import static org.mockito.Mockito.doReturn;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Rect;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
@@ -41,7 +44,7 @@ public class PdfPageUnitTest {
     @Mock private DestroyableObservableSupplier<Rect> mMarginSupplier;
     @Mock private Context mContext;
     @Mock private LoadUrlParams mLoadUrlParams;
-    private TestActivity mActivity;
+    private Activity mActivity;
     private AutoCloseable mCloseableMocks;
 
     private static final String CONTENT_URL = "content://media/external/downloads/1000000022";
@@ -78,6 +81,18 @@ public class PdfPageUnitTest {
         Assert.assertEquals(
                 "Pdf page host should match.", UrlConstants.PDF_HOST, pdfPage.getHost());
         Assert.assertEquals("Pdf page url should match.", CONTENT_URL, pdfPage.getUrl());
+        Assert.assertFalse(
+                "Pdf should not be loaded when the view is not attached to window.",
+                pdfPage.mPdfCoordinator.getIsPdfLoadedForTesting());
+
+        // Simulate tab brought from background to foreground
+        View view = pdfPage.mPdfCoordinator.getView();
+        ViewGroup contentView = mActivity.findViewById(android.R.id.content);
+        contentView.addView(view);
+        Assert.assertTrue(
+                "Pdf should be loaded when the view is attached to window.",
+                pdfPage.mPdfCoordinator.getIsPdfLoadedForTesting());
+        contentView.removeView(view);
     }
 
     @Test
@@ -88,17 +103,46 @@ public class PdfPageUnitTest {
         Assert.assertEquals(
                 "Pdf page host should match.", UrlConstants.PDF_HOST, pdfPage.getHost());
         Assert.assertEquals("Pdf page url should match.", FILE_URL, pdfPage.getUrl());
+        Assert.assertFalse(
+                "Pdf should not be loaded when the view is not attached to window.",
+                pdfPage.mPdfCoordinator.getIsPdfLoadedForTesting());
+
+        // Simulate tab brought from background to foreground
+        View view = pdfPage.mPdfCoordinator.getView();
+        ViewGroup contentView = mActivity.findViewById(android.R.id.content);
+        contentView.addView(view);
+        Assert.assertTrue(
+                "Pdf should be loaded when the view is attached to window.",
+                pdfPage.mPdfCoordinator.getIsPdfLoadedForTesting());
+        contentView.removeView(view);
     }
 
     @Test
     public void testCreatePdfPage_WithPdfLink() {
         PdfPage pdfPage = new PdfPage(mMockNativePageHost, mMockProfile, mActivity, PDF_LINK);
         Assert.assertNotNull(pdfPage);
+        Assert.assertFalse(
+                "Pdf should not be loaded when the download is not completed.",
+                pdfPage.mPdfCoordinator.getIsPdfLoadedForTesting());
+
+        // Simulate download complete
         pdfPage.onDownloadComplete(FILE_NAME, FILE_PATH);
         Assert.assertEquals("Pdf page title should match.", FILE_NAME, pdfPage.getTitle());
         Assert.assertEquals(
                 "Pdf page host should match.", UrlConstants.PDF_HOST, pdfPage.getHost());
         Assert.assertEquals("Pdf page url should match.", PDF_LINK, pdfPage.getUrl());
+        Assert.assertFalse(
+                "Pdf should not be loaded when the view is not attached to window.",
+                pdfPage.mPdfCoordinator.getIsPdfLoadedForTesting());
+
+        // Simulate tab brought from background to foreground
+        View view = pdfPage.mPdfCoordinator.getView();
+        ViewGroup contentView = mActivity.findViewById(android.R.id.content);
+        contentView.addView(view);
+        Assert.assertTrue(
+                "Pdf should be loaded when the view is attached to window.",
+                pdfPage.mPdfCoordinator.getIsPdfLoadedForTesting());
+        contentView.removeView(view);
     }
 
     @Test
