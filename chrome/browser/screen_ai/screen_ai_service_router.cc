1a748fa1143389f69d5d0c8703ed55bd180f6134
Ramin Halavati <rhalavati@chromium.org>
2024-03-13 16:22:31 +0000

    Remove unnecessary DumpWithoutCrashing from ScreenAIServiceRouter.
    The root cause of the issue is known and we don't need to collect more
    data.
    
    AX-Relnotes: n/a
    Bug: 41493789
    Change-Id: I7bf88cec0d7fcb4f215448d31748d6819a758025
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5366310
    Auto-Submit: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    Commit-Queue: Kyungjun Lee <kyungjunlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1272195}

--- chrome/browser/screen_ai/screen_ai_service_router.cc
+++ chrome/browser/screen_ai/screen_ai_service_router.cc
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/contains.h"
 #include "base/containers/flat_map.h"
-#include "base/debug/alias.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
@@ -267,10 +265,9 @@ void ScreenAIServiceRouter::LaunchIfNotRunning() {
     return;
   }
 
-  // TODO(crbug.com/1520814): Remove after crash root cause is fixed.
+  // TODO(crbug.com/41493789): Remove when Reading Mode does not enable OCR
+  // without checking component availability.
   if (install_state != screen_ai::ScreenAIInstallState::State::kDownloaded) {
-    base::debug::Alias(&install_state);
-    base::debug::DumpWithoutCrashing();
     return;
   }
 
