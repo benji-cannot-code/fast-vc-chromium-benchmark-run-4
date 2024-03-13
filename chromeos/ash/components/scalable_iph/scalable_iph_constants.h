168fea4f4e71c8a5746a64d8d1cbec15d71f7d94
Yuki Awano <yawano@google.com>
2024-03-13 17:46:21 +0000

    Add a new Chromebook perks action type
    - Add ActionType::kOpenChromebookPerksMinecraftRealms2023. The action
      opens different urls depending on a country code.
    
    Bug: b:323192344
    Fixed: b:323192344
    Test: browser_tests --gtest_filter=*ScalableIph*
    Change-Id: Ib0bc76afe66edc766434315ea64cff125cd7eee6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5346733
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Yuki Awano <yawano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1272272}

--- chromeos/ash/components/scalable_iph/scalable_iph_constants.h
+++ chromeos/ash/components/scalable_iph/scalable_iph_constants.h
@@ -62,7 +62,8 @@ enum class ActionType {
   kOpenChromebookPerksWeb = 12,
   kOpenChromebookPerksGfnPriority2022 = 13,
   kOpenChromebookPerksMinecraft2023 = 14,
-  kLastAction = kOpenChromebookPerksMinecraft2023,
+  kOpenChromebookPerksMinecraftRealms2023 = 15,
+  kLastAction = kOpenChromebookPerksMinecraftRealms2023,
 };
 
 std::ostream& operator<<(std::ostream& out, ActionType action_type);
@@ -87,6 +88,9 @@ inline constexpr char kActionTypeOpenChromebookPerksGfnPriority2022[] =
     "OpenChromebookPerksGfnPriority2022";
 inline constexpr char kActionTypeOpenChromebookPerksMinecraft2023[] =
     "OpenChromebookPerksMinecraft2023";
+// Use shorter string to keep Finch config payload size smaller.
+inline constexpr char kActionTypeOpenChromebookPerksMinecraftRealms2023[] =
+    "PerksMinecraftRealms2023";
 
 // Constants for events.
 // Naming convention: Camel case starting with a capital letter. Note that
