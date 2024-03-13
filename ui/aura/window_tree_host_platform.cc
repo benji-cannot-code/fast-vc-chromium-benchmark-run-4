d197125f462cc0329bcea6368dc24ef22a49437c
Eliot Courtney <edcourtney@google.com>
2024-03-13 15:23:01 +0000

    lacros: Disable native occlusion if server version is old
    This CL disables native occlusion if the server does not support
    synchronized occlusion state via configure. This is required because the
    old occlusion path has some bugs and can lead to broken occlusion that
    does not recover.
    
    Bug: 1278648
    Test: CQ
    Change-Id: I8d41cd6655494776628a9d88bd81f1464295714a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5367126
    Auto-Submit: Eliot Courtney <edcourtney@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272166}

--- ui/aura/window_tree_host_platform.cc
+++ ui/aura/window_tree_host_platform.cc
@@ -389,9 +389,8 @@ void WindowTreeHostPlatform::SetFrameRateThrottleEnabled(bool enabled) {
     HostFrameRateThrottler::GetInstance().RemoveHost(this);
 }
 
-bool WindowTreeHostPlatform::IsNativeWindowOcclusionTrackingAlwaysEnabled() {
-  return NativeWindowOcclusionTracker::
-      IsNativeWindowOcclusionTrackingAlwaysEnabled(this);
+void WindowTreeHostPlatform::DisableNativeWindowOcclusion() {
+  SetNativeWindowOcclusionEnabled(false);
 }
 
 }  // namespace aura
