9520f2286913790ac1c6d4121e5b0db5a2ff477e
Michele Fan <michelefan@chromium.org>
2024-03-13 22:06:30 +0000

    snap-group: Add a separating border in personalization section
    Demo: http://b/329148818#comment2
    Fixed: b/329148818
    Test: Manually
    Change-Id: I27771ee8074db3ca036d917d78bfd786976675bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5367744
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272454}

--- chrome/browser/ui/webui/ash/settings/pages/personalization/personalization_section.cc
+++ chrome/browser/ui/webui/ash/settings/pages/personalization/personalization_section.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/ui/webui/ash/settings/pages/personalization/personalization_section.h"
+
 #include <optional>
 
 #include "ash/constants/ash_features.h"
