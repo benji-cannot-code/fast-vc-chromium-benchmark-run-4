c00f58a727eba1ecd59e0a9d1fb2e6a07809cc07
Teresa Mao <temao@chromium.org>
2024-03-14 18:45:35 +0000

    Feedback UI: Remove assistant_logs_info.html and its dependencies
    Chromeos has implemented a newer version of assistant logs info page
    under ash/webui/os_feedback_ui/ that replaced the one in
    c/b/r/feedback/. This CL removes the code that's no longer being used.
    
    Bug: 41481648
    Change-Id: I4b8cae0b5764a0b5351e58f00cdf2b3e805c5064
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5370696
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272958}

--- chrome/browser/resources/feedback/js/feedback_browser_proxy.ts
+++ chrome/browser/resources/feedback/js/feedback_browser_proxy.ts
@@ -30,10 +30,6 @@ export interface FeedbackBrowserProxy {
   // Send a message to close the WebDialog
   closeDialog(): void;
 
-  // <if expr="chromeos_ash">
-  showAssistantLogsInfo(): void;
-  // </if>
-
   showSystemInfo(): void;
   showMetrics(): void;
   showAutofillMetadataInfo(autofillMetadata: string): void;
@@ -74,12 +70,6 @@ export class FeedbackBrowserProxyImpl implements FeedbackBrowserProxy {
   closeDialog() {
     chrome.send('dialogClose');
   }
-
-  // <if expr="chromeos_ash">
-  showAssistantLogsInfo() {
-    chrome.send('showAssistantLogsInfo');
-  }
-  // </if>
 
   showSystemInfo() {
     chrome.send('showSystemInfo');
--- chrome/test/data/webui/feedback/test_feedback_browser_proxy.ts
+++ chrome/test/data/webui/feedback/test_feedback_browser_proxy.ts
@@ -18,9 +18,6 @@ export class TestFeedbackBrowserProxy extends TestBrowserProxy implements
       'closeDialog', 'getSystemInformation', 'getUserEmail',
       'getDialogArguments', 'getUserMedia', 'sendFeedback',
       'showAutofillMetadataInfo', 'showDialog', 'showMetrics', 'showSystemInfo',
-      // <if expr="chromeos_ash">
-      'showAssistantLogsInfo',
-      // </if>
     ]);
   }
 
@@ -74,12 +71,6 @@ export class TestFeedbackBrowserProxy extends TestBrowserProxy implements
   closeDialog() {
     this.methodCalled('closeDialog');
   }
-
-  // <if expr="chromeos_ash">
-  showAssistantLogsInfo() {
-    this.methodCalled('showAssistantLogsInfo');
-  }
-  // </if>
 
   showSystemInfo() {
     this.methodCalled('showSystemInfo');
