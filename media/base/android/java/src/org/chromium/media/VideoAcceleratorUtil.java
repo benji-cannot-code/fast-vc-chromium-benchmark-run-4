4c11ea87bb26ea9b82bfcda5c9f13d752c9cf2e0
Eugene Zemtsov <eugene@chromium.org>
2024-03-14 21:13:07 +0000

    media: Temporal SVC on Android
    Modern MediaCodecs support temporal scalability via
    KEY_TEMPORAL_LAYERING. This change exposes it in WebCodecs API.
    
    Bug: 1500436
    Change-Id: I3ee3082795d34b336f1407982d5aa4fbfcd9f49b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5357559
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273052}

--- media/base/android/java/src/org/chromium/media/VideoAcceleratorUtil.java
+++ media/base/android/java/src/org/chromium/media/VideoAcceleratorUtil.java
@@ -23,6 +23,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * A collection of SDK based helper functions for retrieving supported profiles
@@ -50,6 +51,10 @@ class VideoAcceleratorUtil {
         MediaCodecUtil.MimeTypes.VIDEO_DV,
     };
 
+    // Encoders known to support temporal layers.
+    private static final Set<String> TEMPORAL_SVC_SUPPORTING_ENCODERS =
+            Set.of("c2.qti.avc.encoder", "c2.exynos.h264.encoder");
+
     private static class SupportedProfileAdapter {
         public int profile;
         public int level;
@@ -65,6 +70,7 @@ class VideoAcceleratorUtil {
         public boolean isSoftwareCodec;
         public boolean supportsSecurePlayback;
         public boolean requiresSecurePlayback;
+        public int maxNumberOfTemporalLayers;
 
         @CalledByNative("SupportedProfileAdapter")
         public int getProfile() {
@@ -135,6 +141,11 @@ class VideoAcceleratorUtil {
         public boolean requiresSecurePlayback() {
             return this.requiresSecurePlayback;
         }
+
+        @CalledByNative("SupportedProfileAdapter")
+        public int getMaxNumberOfTemporalLayers() {
+            return this.maxNumberOfTemporalLayers;
+        }
     }
 
     // Currently our encoder only supports NV12.
@@ -169,6 +180,17 @@ class VideoAcceleratorUtil {
         var lowerName = name.toLowerCase(Locale.ROOT);
         // This is usually a hw decoder provided by the OEM vendors.
         return lowerName.endsWith(".low_latency");
+    }
+
+    private static int getNumberOfTemporalLayers(String name) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+            return 1;
+        }
+
+        if (TEMPORAL_SVC_SUPPORTING_ENCODERS.contains(name)) {
+            return 3;
+        }
+        return 1;
     }
 
     /**
@@ -251,6 +273,8 @@ class VideoAcceleratorUtil {
                     }
                 }
 
+                int maxNumberOfTemporalLayers =
+                        getNumberOfTemporalLayers(info.getName().toLowerCase(Locale.getDefault()));
                 ArrayList<SupportedProfileAdapter> profiles =
                         info.isHardwareAccelerated() ? hardwareProfiles : softwareProfiles;
                 for (int mediaProfile : supportedProfiles) {
@@ -267,6 +291,7 @@ class VideoAcceleratorUtil {
                     profile.supportsVbr = supportsVbr;
                     profile.name = info.getName();
                     profile.isSoftwareCodec = info.isSoftwareOnly();
+                    profile.maxNumberOfTemporalLayers = maxNumberOfTemporalLayers;
                     profiles.add(profile);
 
                     // Invert min/max height/width for a portrait mode entry if needed.
