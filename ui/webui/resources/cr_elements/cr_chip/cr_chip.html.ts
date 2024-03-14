c38c5c41030d09d5ce69e9b514164f6f17e4a8b8
rbpotter <rbpotter@chromium.org>
2024-03-14 21:01:06 +0000

    WebUI: Migrate cr-chip to LitElement
    Also adding a unit test since one was missing.
    
    Bug: 40943652
    Change-Id: I31020d3c07b0e628dc27acf6c76d057a9471f501
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5371400
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273043}

---
+++ ui/webui/resources/cr_elements/cr_chip/cr_chip.html.ts
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {CrChipElement} from './cr_chip.js';
+
+export function getHtml(this: CrChipElement) {
+  return html`
+<button id="button" ?selected="${this.selected}"
+    ?disabled="${this.disabled}" aria-pressed="${this.selected}"
+    role="${this.chipRole}">
+  <div id="hoverLayer"></div>
+  <slot></slot>
+</button>`;
+}
