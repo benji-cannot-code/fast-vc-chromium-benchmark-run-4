c468e842060212f8fc0f7c3e11b878646f9b7a23
Daniel Vogelheim <vogelheim@chromium.org>
2024-03-14 08:46:17 +0000

    [ORB] Move ORB implementation out of public dir.
    This moves the ORB implementation out of services/network/public/cpp/orb,
    into services/network/orb. The only code that remains in public/cpp
    is orb_api.h.
    
    This is a cleanup CL, and should have no observable effects.
    
    Bug: 324418206
    Change-Id: Idcc0645bf582674a80ebaa891cafcd801ed63278
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5360899
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272627}

--- services/network/orb/orb_api.cc
+++ services/network/orb/orb_api.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unordered_set>
 
 #include "net/http/http_response_headers.h"
-#include "services/network/public/cpp/orb/orb_impl.h"
+#include "services/network/orb/orb_impl.h"
 #include "services/network/public/mojom/url_response_head.mojom.h"
 
 namespace network::orb {
--- services/network/orb/orb_impl.h
+++ services/network/orb/orb_impl.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef SERVICES_NETWORK_PUBLIC_CPP_ORB_ORB_IMPL_H_
-#define SERVICES_NETWORK_PUBLIC_CPP_ORB_ORB_IMPL_H_
+#ifndef SERVICES_NETWORK_ORB_ORB_IMPL_H_
+#define SERVICES_NETWORK_ORB_ORB_IMPL_H_
 
 #include <optional>
 #include <string_view>
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace network::orb {
 
-class COMPONENT_EXPORT(NETWORK_CPP) OpaqueResponseBlockingAnalyzer final
+class COMPONENT_EXPORT(NETWORK_SERVICE) OpaqueResponseBlockingAnalyzer final
     : public ResponseAnalyzer {
  public:
   explicit OpaqueResponseBlockingAnalyzer(PerFactoryState& state);
@@ -104,4 +104,4 @@ class COMPONENT_EXPORT(NETWORK_CPP) OpaqueResponseBlockingAnalyzer final
 
 }  // namespace network::orb
 
-#endif  // SERVICES_NETWORK_PUBLIC_CPP_ORB_ORB_IMPL_H_
+#endif  // SERVICES_NETWORK_ORB_ORB_IMPL_H_
