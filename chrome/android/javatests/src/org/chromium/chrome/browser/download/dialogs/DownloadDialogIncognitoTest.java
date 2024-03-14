c76d87d80a0bcb7bb238ef82143156e94b0a9008
Nicola Tommasi <tommasin@chromium.org>
2024-03-14 17:02:06 +0000

    [Gardener] Fix flakiness in DownloadDialogIncognitoTest
    These tests are flaky with espresso 3.2 and above API 30.
    
    Bug: 40234615
    Change-Id: I41cfe81d2235f06013ec08b3491f027a6ae1c046
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5371928
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Owners-Override: Nicola Tommasi <tommasin@chromium.org>
    Auto-Submit: Nicola Tommasi <tommasin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272871}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/dialogs/DownloadDialogIncognitoTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/dialogs/DownloadDialogIncognitoTest.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.download.dialogs;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.ViewMatchers.Visibility.GONE;
 import static androidx.test.espresso.matcher.ViewMatchers.Visibility.VISIBLE;
 import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
@@ -92,7 +93,7 @@ public class DownloadDialogIncognitoTest {
         waitForWarningVisibilityToBe(VISIBLE);
 
         // Dismiss the dialog and verify the callback is called with false.
-        onView(withId(R.id.negative_button)).perform(ViewActions.click());
+        onView(withId(R.id.negative_button)).inRoot(isDialog()).perform(ViewActions.click());
         verify(mResultCallback).onResult(false);
     }
 
@@ -118,7 +119,7 @@ public class DownloadDialogIncognitoTest {
         waitForWarningVisibilityToBe(VISIBLE);
 
         // Accept the dialog and verify the callback is called with true.
-        onView(withId(R.id.positive_button)).perform(ViewActions.click());
+        onView(withId(R.id.positive_button)).inRoot(isDialog()).perform(ViewActions.click());
         verify(mResultCallback).onResult(true);
     }
 
@@ -132,7 +133,7 @@ public class DownloadDialogIncognitoTest {
         waitForWarningVisibilityToBe(GONE);
 
         // Dismiss the dialog and verify the callback is called with false.
-        onView(withId(R.id.negative_button)).perform(ViewActions.click());
+        onView(withId(R.id.negative_button)).inRoot(isDialog()).perform(ViewActions.click());
         verify(mResultCallback).onResult(false);
     }
 
@@ -146,7 +147,7 @@ public class DownloadDialogIncognitoTest {
         waitForWarningVisibilityToBe(GONE);
 
         // Dismiss the dialog and verify the callback is called with false.
-        onView(withId(R.id.negative_button)).perform(ViewActions.click());
+        onView(withId(R.id.negative_button)).inRoot(isDialog()).perform(ViewActions.click());
         verify(mResultCallback).onResult(false);
     }
 
