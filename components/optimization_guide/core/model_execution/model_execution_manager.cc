c2ab3e623a7c55ddba1aed07243b83aedf1d61a7
Sophie Chang <sophiechang@chromium.org>
2024-03-14 05:54:14 +0000

    Add temporary config option to disable server fallback
    Bug: 316606261
    Change-Id: Idcefa299481ab808deb6bd05a62e8a51ac87abf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5371350
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272601}

--- components/optimization_guide/core/model_execution/model_execution_manager.cc
+++ components/optimization_guide/core/model_execution/model_execution_manager.cc
@@ -255,6 +255,10 @@ ModelExecutionManager::StartSession(
     }
   }
 
+  if (config_params && config_params->disable_server_fallback) {
+    return nullptr;
+  }
+
   RecordSessionUsedRemoteExecutionHistogram(feature, /*is_remote=*/true);
   return std::make_unique<SessionImpl>(
       base::DoNothing(), feature, std::nullopt, nullptr, nullptr,
