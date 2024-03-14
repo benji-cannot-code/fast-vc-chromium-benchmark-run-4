4c67802e6ccf7fa48f90a8694e07e40267cb6392
S. Ganesh <ganesh@chromium.org>
2024-03-14 22:51:02 +0000

    updater: Send the AppCommand's Command ID in pings
    The app command id will be reported in the `name` attribute in the ping.
    The `name` attribute can be used along with the `app_id` attribute to
    uniquely identify the app command associated with the ping.
    
    A follow-up change will be made to have `update_client` send the `name`
    attribute in pings.
    
    Bug: 329482488
    Change-Id: I42a9a87d4bcd50da8806fbe214ec70f349139d3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5371345
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273102}

--- chrome/updater/app/server/win/com_classes_legacy.cc
+++ chrome/updater/app/server/win/com_classes_legacy.cc
@@ -1050,6 +1050,7 @@ HRESULT LegacyAppCommandWebImpl::RuntimeClassInitialize(
       AppCommandRunner::LoadAppCommand(scope, app_id, command_id);
   scope_ = scope;
   app_id_ = base::WideToUTF8(app_id);
+  command_id_ = base::WideToUTF8(command_id);
   ping_sender_ = std::move(ping_sender);
   return app_command_runner_.error_or(S_OK);
 }
@@ -1124,7 +1125,7 @@ STDMETHODIMP LegacyAppCommandWebImpl::execute(VARIANT substitution1,
   const HRESULT hr = app_command_runner_->Run(substitutions, process_);
   if (FAILED(hr)) {
     VLOG(2) << __func__ << ": AppCommand failed to launch: " << hr;
-    ping_sender_.Run(scope_, app_id_,
+    ping_sender_.Run(scope_, app_id_, command_id_,
                      {
                          .error_code = hr,
                          .extra_code1 = kErrorAppCommandLaunchFailed,
@@ -1153,16 +1154,18 @@ STDMETHODIMP LegacyAppCommandWebImpl::execute(VARIANT substitution1,
                        };
                      },
                      process_.Duplicate())
-                     .Then(base::BindOnce(ping_sender_, scope_, app_id_)));
+                     .Then(base::BindOnce(ping_sender_, scope_, app_id_,
+                                          command_id_)));
   return hr;
 }
 
 void LegacyAppCommandWebImpl::SendPing(UpdaterScope scope,
                                        const std::string& app_id,
+                                       const std::string& command_id,
                                        ErrorParams error_params) {
   AppServerWin::PostRpcTask(base::BindOnce(
       [](UpdaterScope scope, const std::string& app_id,
-         ErrorParams error_params) {
+         const std::string& command_id, ErrorParams error_params) {
         scoped_refptr<Configurator> config =
             GetAppServerWinInstance()->config();
         scoped_refptr<PersistedData> persisted_data =
@@ -1176,6 +1179,7 @@ void LegacyAppCommandWebImpl::SendPing(UpdaterScope scope,
         app_command_data.ap = persisted_data->GetAP(app_id);
         app_command_data.app_id = app_id;
         app_command_data.brand = persisted_data->GetBrandCode(app_id);
+        app_command_data.name = command_id;
         app_command_data.requires_network_encryption = false;
         app_command_data.version = persisted_data->GetProductVersion(app_id);
 
@@ -1188,7 +1192,7 @@ void LegacyAppCommandWebImpl::SendPing(UpdaterScope scope,
              .extra_code1 = error_params.extra_code1},
             base::DoNothing());
       },
-      scope, app_id, error_params));
+      scope, app_id, command_id, error_params));
 }
 
 PolicyStatusImpl::PolicyStatusImpl()
--- chrome/updater/app/server/win/com_classes_legacy.h
+++ chrome/updater/app/server/win/com_classes_legacy.h
@@ -262,6 +262,7 @@ class LegacyAppCommandWebImpl : public IDispatchImpl<IAppCommandWeb> {
 
   using PingSender = base::RepeatingCallback<void(UpdaterScope scope,
                                                   const std::string& app_id,
+                                                  const std::string& command_id,
                                                   ErrorParams error_params)>;
   LegacyAppCommandWebImpl();
   LegacyAppCommandWebImpl(const LegacyAppCommandWebImpl&) = delete;
@@ -306,6 +307,7 @@ class LegacyAppCommandWebImpl : public IDispatchImpl<IAppCommandWeb> {
 
   static void SendPing(UpdaterScope scope,
                        const std::string& app_id,
+                       const std::string& command_id,
                        ErrorParams error_params);
 
   ~LegacyAppCommandWebImpl() override;
@@ -314,6 +316,7 @@ class LegacyAppCommandWebImpl : public IDispatchImpl<IAppCommandWeb> {
   HResultOr<AppCommandRunner> app_command_runner_;
   UpdaterScope scope_ = UpdaterScope::kSystem;
   std::string app_id_;
+  std::string command_id_;
   PingSender ping_sender_ = base::DoNothing();
 };
 
