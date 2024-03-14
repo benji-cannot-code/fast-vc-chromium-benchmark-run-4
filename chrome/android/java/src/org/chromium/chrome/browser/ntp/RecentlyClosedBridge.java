dc9e0e6fcc5fe07671d140d72691b36082b92573
Brandon Fong <bjfong@google.com>
2024-03-14 06:16:37 +0000

    [Tab Group Parity] Store the tab group color on recent tabs restore
    Store the tab group color retrieved from recent tabs when a tab group is
    restored.
    
    Bug: 41490324
    Change-Id: Ied689670afa8b4f46ad42e2edb1f1ce763446032
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5366292
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272606}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/RecentlyClosedBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/RecentlyClosedBridge.java
@@ -18,6 +18,7 @@ import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tabmodel.TabModel;
 import org.chromium.chrome.browser.tabmodel.TabModelFilter;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
+import org.chromium.chrome.browser.tasks.tab_groups.TabGroupColorUtils;
 import org.chromium.chrome.browser.tasks.tab_groups.TabGroupModelFilter;
 import org.chromium.chrome.browser.tasks.tab_groups.TabGroupTitleUtils;
 import org.chromium.url.GURL;
@@ -106,7 +107,7 @@ public class RecentlyClosedBridge implements RecentlyClosedTabManager {
     }
 
     @CalledByNative
-    private void restoreTabGroup(TabModel tabModel, String title, int[] tabIds) {
+    private void restoreTabGroup(TabModel tabModel, String title, int color, int[] tabIds) {
         if (tabIds.length == 0) return;
 
         assert mTabModelSelector.getModel(tabModel.isIncognito()) == tabModel;
@@ -129,6 +130,10 @@ public class RecentlyClosedBridge implements RecentlyClosedTabManager {
 
                 groupFilter.mergeTabsToGroup(id, rootId);
             }
+        }
+
+        if (ChromeFeatureList.isEnabled(ChromeFeatureList.TAB_GROUP_PARITY_ANDROID)) {
+            TabGroupColorUtils.storeTabGroupColor(rootId, color);
         }
 
         if (title == null || title.isEmpty()) return;
