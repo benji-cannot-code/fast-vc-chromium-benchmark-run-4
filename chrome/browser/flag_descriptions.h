52270462e8c209623c1614361e2701a66fe7f80f
Abe Boujane <boujane@google.com>
2024-03-14 22:19:24 +0000

    Expose PrivacySandboxSettings4 in chrome://flags
    Change-Id: If83a9d279d2509640328f92ee463a5084c666fa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372984
    Reviewed-by: Jacob Stanley <jacobstanley@google.com>
    Auto-Submit: Abe Boujane <boujane@google.com>
    Commit-Queue: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1273076}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1708,6 +1708,9 @@ extern const char kPrivacyGuidePreloadAndroidDescription[];
 extern const char kPrivacySandboxAdsAPIsOverrideName[];
 extern const char kPrivacySandboxAdsAPIsOverrideDescription[];
 
+extern const char kPrivacySandboxSettings4Name[];
+extern const char kPrivacySandboxSettings4Description[];
+
 extern const char kPrivacySandboxAdsNoticeCCTName[];
 extern const char kPrivacySandboxAdsNoticeCCTDescription[];
 
