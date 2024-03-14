e080f9e7eb9ca626022788ce849896f0afdf01c7
Joe Mason <joenotcharles@google.com>
2024-03-14 15:36:29 +0000

    Heap Profiler: Treat the UNKNOWN channel as STABLE
    R=etienneb
    
    Bug: 40840943
    Change-Id: Ib3769040ba2f6282c6f6ab61ab863e49ac644a5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5362643
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1272800}

--- components/heap_profiling/in_process/heap_profiler_controller.cc
+++ components/heap_profiling/in_process/heap_profiler_controller.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/notreached.h"
 #include "base/numerics/clamped_math.h"
 #include "base/profiler/module_cache.h"
 #include "base/rand_util.h"
@@ -100,17 +101,33 @@ std::string ProcessHistogramName(base::StringPiece base_name,
   }
 }
 
+double GetChannelProbability(version_info::Channel channel,
+                             const HeapProfilerParameters& params) {
+  switch (channel) {
+    case version_info::Channel::STABLE:
+    case version_info::Channel::UNKNOWN:
+      // If the channel can't be determined, treat it as `stable` for safety.
+      // Don't disable heap profiling completely so that developers can still
+      // enable it with --enable-feature flags.
+      return params.stable_probability;
+    case version_info::Channel::BETA:
+    case version_info::Channel::DEV:
+    case version_info::Channel::CANARY:
+      return params.nonstable_probability;
+  }
+  NOTREACHED_NORETURN();
+}
+
 ProfilingEnabled DecideIfCollectionIsEnabled(version_info::Channel channel,
                                              ProcessType process_type) {
   HeapProfilerParameters params =
       GetHeapProfilerParametersForProcess(process_type);
-  if (!params.is_supported)
+  if (!params.is_supported) {
     return ProfilingEnabled::kDisabled;
-  const double probability = (channel == version_info::Channel::STABLE)
-                                 ? params.stable_probability
-                                 : params.nonstable_probability;
-  if (base::RandDouble() >= probability)
+  }
+  if (base::RandDouble() >= GetChannelProbability(channel, params)) {
     return ProfilingEnabled::kDisabled;
+  }
   return ProfilingEnabled::kEnabled;
 }
 
