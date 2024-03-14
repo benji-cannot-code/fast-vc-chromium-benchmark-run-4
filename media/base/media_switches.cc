81522a5d5afa5b556d272d8c2b43530499b0e0b2
Saifuddin Hitawala <hitawala@chromium.org>
2024-03-14 17:39:26 +0000

    Multiplanar: Remove legacy codepath from FuchsiaVideoDecoder
    Multiplanar SI is launched on Fuchsia hardware video decoder in M119.
    This change removes the legacy multiplanar shared image codepath there.
    
    Bug: 1366495
    Change-Id: I75c49b67d6a99bb594f3edb6f2186b92004db36a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5105629
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272894}

--- media/base/media_switches.cc
+++ media/base/media_switches.cc
@@ -1859,8 +1859,12 @@ bool IsVideoCaptureAcceleratedJpegDecodingEnabled() {
 }
 
 bool IsMultiPlaneFormatForHardwareVideoEnabled() {
+#if BUILDFLAG(IS_FUCHSIA)
+  return true;
+#else
   return
       base::FeatureList::IsEnabled(kUseMultiPlaneFormatForHardwareVideo);
+#endif
 }
 
 bool IsWritePixelsYUVEnabled() {
