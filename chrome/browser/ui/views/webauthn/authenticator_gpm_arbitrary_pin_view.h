6750b6126d6cfb4cb153c0831b20c73be4e7bca3
rgod <rgod@google.com>
2024-03-14 09:17:02 +0000

    [WebAuthn] Add reveal button to the arbitrary pin entry textfield
    For now the button is next to the textfield, will be positioned
    properly in the follow-up.
    
    Bug: 40274370
    Change-Id: I29bfbf4c32dc8f1fc593b1bd009861eedbe149dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5366036
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1272642}

--- chrome/browser/ui/views/webauthn/authenticator_gpm_arbitrary_pin_view.h
+++ chrome/browser/ui/views/webauthn/authenticator_gpm_arbitrary_pin_view.h
@@ -13,10 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace views {
 class Textfield;
+class ToggleImageButton;
 }  // namespace views
 
 // View showing a textfield for entering an alphanumeric GPM pin.
-// TODO(rgod): Add eye icon for revealing the typed pin.
 class AuthenticatorGPMArbitraryPinView : public views::View,
                                          public views::TextfieldController {
   METADATA_HEADER(AuthenticatorGPMArbitraryPinView, views::View)
@@ -36,6 +36,8 @@ class AuthenticatorGPMArbitraryPinView : public views::View,
   ~AuthenticatorGPMArbitraryPinView() override;
 
  private:
+  void OnRevealButtonClicked();
+
   // views::View:
   void RequestFocus() override;
 
@@ -45,6 +47,8 @@ class AuthenticatorGPMArbitraryPinView : public views::View,
 
   const raw_ptr<Delegate> delegate_;
   raw_ptr<views::Textfield> pin_textfield_ = nullptr;
+  raw_ptr<views::ToggleImageButton> reveal_button_ = nullptr;
+  bool pin_revealed_ = false;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_WEBAUTHN_AUTHENTICATOR_GPM_ARBITRARY_PIN_VIEW_H_
