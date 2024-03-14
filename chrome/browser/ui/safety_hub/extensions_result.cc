f17c5db71b69b4b5cb88f2d0561cfe653bd35daa
Richard Chen <richche@chromium.org>
2024-03-14 22:15:57 +0000

    Refactor Extensions Safety Check string generation logic
    Currently, an extension's safety check strings are generated only if the
    CWS Info service has retrieved metadata for it. This CL removes that
    restriction because some of the strings can be generated without the
    CWS Info service metadata (i.e. by using the extension blocklist prefs
    data).
    
    Bug: 327251907
    Change-Id: I7b2ec6364befd2baf3045dd5957c9358a10887de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5350320
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Commit-Queue: Richard Chen <richche@google.com>
    Cr-Commit-Position: refs/heads/main@{#1273070}

--- chrome/browser/ui/safety_hub/extensions_result.cc
+++ chrome/browser/ui/safety_hub/extensions_result.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/safety_hub/safety_hub_constants.h"
 #include "chrome/browser/ui/safety_hub/safety_hub_service.h"
 #include "chrome/grit/generated_resources.h"
+#include "extensions/browser/blocklist_extension_prefs.h"
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/extension_prefs_factory.h"
 #include "extensions/browser/extension_registry.h"
@@ -67,6 +68,24 @@ bool ShouldExtensionBeReviewed(
         break;
     }
   }
+
+  // If an extension appears on the blocklist, that extension will be
+  // marked for review. Currently, only malware and policy violation blocklist
+  // states are marked for review.
+  extensions::BitMapBlocklistState blocklist_state =
+      extensions::blocklist_prefs::GetExtensionBlocklistState(extension.id(),
+                                                              extension_prefs);
+  switch (blocklist_state) {
+    case extensions::BitMapBlocklistState::BLOCKLISTED_MALWARE:
+    case extensions::BitMapBlocklistState::BLOCKLISTED_CWS_POLICY_VIOLATION:
+      return true;
+    case extensions::BitMapBlocklistState::BLOCKLISTED_POTENTIALLY_UNWANTED:
+    case extensions::BitMapBlocklistState::BLOCKLISTED_SECURITY_VULNERABILITY:
+    case extensions::BitMapBlocklistState::NOT_BLOCKLISTED:
+      // no-op.
+      break;
+  }
+
   return false;
 }
 }  // namespace
--- chrome/browser/ui/webui/settings/safety_check_extensions_handler.cc
+++ chrome/browser/ui/webui/settings/safety_check_extensions_handler.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/cws_info_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/grit/generated_resources.h"
+#include "extensions/browser/blocklist_extension_prefs.h"
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/extension_prefs_factory.h"
 #include "extensions/browser/extension_registry.h"
@@ -61,22 +62,40 @@ bool SafetyCheckExtensionsHandler::CheckExtensionForTrigger(
   if (warning_acked || !is_extension) {
     return false;
   }
-  std::optional<extensions::CWSInfoService::CWSInfo> extension_info =
+  std::optional<extensions::CWSInfoService::CWSInfo> cws_info =
       cws_info_service_->GetCWSInfo(extension);
-  if (extension_info.has_value() && extension_info->is_present) {
-    switch (extension_info->violation_type) {
+  if (cws_info.has_value() && cws_info->is_present) {
+    switch (cws_info->violation_type) {
       case extensions::CWSInfoService::CWSViolationType::kMalware:
       case extensions::CWSInfoService::CWSViolationType::kPolicy:
         return true;
       case extensions::CWSInfoService::CWSViolationType::kNone:
       case extensions::CWSInfoService::CWSViolationType::kMinorPolicy:
       case extensions::CWSInfoService::CWSViolationType::kUnknown:
-        if (extension_info->unpublished_long_ago) {
+        if (cws_info->unpublished_long_ago) {
           return true;
         }
         break;
     }
   }
+
+  // If an extension appears on the blocklist, that extension will be
+  // triggered. Currently, only malware and policy violation blocklist
+  // states are triggered.
+  extensions::BitMapBlocklistState blocklist_state =
+      extensions::blocklist_prefs::GetExtensionBlocklistState(extension.id(),
+                                                              extension_prefs);
+  switch (blocklist_state) {
+    case extensions::BitMapBlocklistState::BLOCKLISTED_MALWARE:
+    case extensions::BitMapBlocklistState::BLOCKLISTED_CWS_POLICY_VIOLATION:
+      return true;
+    case extensions::BitMapBlocklistState::BLOCKLISTED_SECURITY_VULNERABILITY:
+    case extensions::BitMapBlocklistState::BLOCKLISTED_POTENTIALLY_UNWANTED:
+    case extensions::BitMapBlocklistState::NOT_BLOCKLISTED:
+      // no-op.
+      break;
+  }
+
   return false;
 }
 
