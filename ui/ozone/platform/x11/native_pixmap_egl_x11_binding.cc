9421e011e3991591c22c0213b25c5d1f37f65882
Saifuddin Hitawala <hitawala@chromium.org>
2024-03-14 19:12:11 +0000

    [ozone] Remove NativePixmapGLBinding::GetInternalFormat
    NativePixmapGLBinding::GetInternalFormat() is no longer used after
    removal of validating support from ozone backing gl texture holders.
    
    Bug: 40135856
    Change-Id: I8a191bfeb947141b1cf26eccdf7eec90be596005
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5370947
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272983}

--- ui/ozone/platform/x11/native_pixmap_egl_x11_binding.cc
+++ ui/ozone/platform/x11/native_pixmap_egl_x11_binding.cc
@@ -142,7 +142,7 @@ inline EGLDisplay FromXDisplay() {
 namespace ui {
 
 NativePixmapEGLX11Binding::NativePixmapEGLX11Binding(gfx::BufferFormat format)
-    : display_(gl::FromXDisplay()), format_(format) {}
+    : display_(gl::FromXDisplay()) {}
 
 NativePixmapEGLX11Binding::~NativePixmapEGLX11Binding() {
   if (surface_) {
@@ -273,11 +273,6 @@ bool NativePixmapEGLX11Binding::BindTexture(GLenum target, GLuint texture_id) {
   }
 
   return true;
-}
-
-GLuint NativePixmapEGLX11Binding::GetInternalFormat() {
-  return NativePixmapGLBinding::BufferFormatToGLInternalFormatDefaultMapping(
-      format_);
 }
 
 }  // namespace ui
