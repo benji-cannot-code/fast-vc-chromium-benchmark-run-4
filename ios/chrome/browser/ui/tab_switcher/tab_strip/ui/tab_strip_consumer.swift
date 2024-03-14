6a25c07288b87a3cdff29217d54aba9b33a5a3f0
Gauthier Ambard <gambard@chromium.org>
2024-03-14 12:45:01 +0000

    [iOS] Add insert/detach operation in the TabStrip
    This CL adds the insert and detach operation, avoiding reloading the
    whole collection view in those cases.
    
    To make it work, it also changes the populateConsumerItem to also
    work when there is no item, allowing the TabStrip to be reset.
    
    It also allows to fix the new tab animation by removing the bool
    tracking if a new tab was inserted using the button.
    
    However, this will not make the inserted item visible. With the
    current implementation, it is not possible to do. It would require
    moving away from having the attributes of the items having a frame
    of zero when they are on the border.
    
    Bug: 326242006
    Change-Id: I69c2feacf201df614764618e37c3784e46ad66ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5355740
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1272708}

--- ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_consumer.swift
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_consumer.swift
@@ -20,6 +20,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   func moveItem(
     _ item: TabSwitcherItem, afterItem destinationItem: TabSwitcherItem?)
 
+  /// Inserts `items` before `destinationItem`. Pass nil to insert at the end.
+  /// It's an error if any of the `items` is already passed to the consumer (and not yet removed).
+  func insertItems(
+    _ items: [TabStripItemIdentifier], beforeItem destinationItem: TabStripItemIdentifier?)
+
+  /// Removes `items`.
+  func removeItems(_ items: [TabStripItemIdentifier]?)
+
   /// Replaces `oldItem` by `newItem`.
   /// The nullability is here for Objective-C compatibility. If one of them is nil, the consumer will do nothing.
   func replaceItem(_ oldItem: TabSwitcherItem?, withItem newItem: TabSwitcherItem?)
