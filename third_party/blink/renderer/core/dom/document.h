7ffb89ecac5ff2b67a046eb8cd5fbf37162f1ca8
David Awogbemila <awogbemila@chromium.org>
2024-03-14 18:13:43 +0000

    [css-scroll-snap-2] Add SnapEvents interface
    This  will replace the now-obsolete SnapEventsDeprecated. The new
    interface is described in [1].
    
    [1]https://github.com/w3c/csswg-drafts/pull/9515
    
    Bug: 329111147
    Change-Id: I9c01b28ef937bc44c099c01f7190e2677a407314
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5369680
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: David Awogbemila <awogbemila@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272922}

--- third_party/blink/renderer/core/dom/document.h
+++ third_party/blink/renderer/core/dom/document.h
@@ -1505,6 +1505,12 @@ class CORE_EXPORT Document : public ContainerNode,
   void EnqueueSnapChangedEvent(Node* target, HeapVector<Member<Node>>& targets);
   void EnqueueSnapChangingEvent(Node* target,
                                 HeapVector<Member<Node>>& targets);
+  void EnqueueSnapChangedEvent(Node* target,
+                               Member<Node>& block_target,
+                               Member<Node>& inline_target);
+  void EnqueueSnapChangingEvent(Node* target,
+                                Member<Node>& block_target,
+                                Member<Node>& inline_target);
 
   void DispatchEventsForPrinting();
 
