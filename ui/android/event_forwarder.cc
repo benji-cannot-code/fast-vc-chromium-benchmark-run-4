25124c155f8b06504095d2c4263c5e5ba4ef90c5
Kartar Singh <kartarsingh@google.com>
2024-03-14 15:42:32 +0000

    Don't send touch moves to EventForwarder observers.
    This is part of Input Vizard
    project(https://docs.google.com/document/d/1mcydbkgFCO_TT9NuFE962L8PLJWT2XOfXUAPO88VuKE)
    where we are trying to move input to GPU process. With input coming on
    GPU process we would need to send back the inputs back to browser for
    the observers which cannot be moved to GPU itself. We are trying to
    minimize sending inputs back to Browser process to minimize the IPCs we
    make.
    
    With this change we are stopping sending touch moves to observers early
    in project so that we get to know about any breaking changes early i.e.
    if we need to send touch moves back to browser.
    
    This shouldn't break the only existing observer of EventForwarder touch
    events(AttributionInputEventTrackerAndroid) since it only cares about
    events leading to navigations and touch moves shouldn't be causing any
    navigations.
    
    Bug: b/328602561
    Change-Id: I77eb877da5b6c11fc3d9241bc91c3bd842d0f778
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5355053
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Kartar Singh <kartarsingh@google.com>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272807}

--- ui/android/event_forwarder.cc
+++ ui/android/event_forwarder.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/ranges.h"
 #include "base/trace_event/typed_macros.h"
 #include "base/tracing/protos/chrome_track_event.pbzero.h"
+#include "ui/android/ui_android_features.h"
 #include "ui/android/ui_android_jni_headers/EventForwarder_jni.h"
 #include "ui/android/window_android.h"
 #include "ui/base/ui_base_switches_util.h"
@@ -26,7 +27,10 @@ using base::android::JavaParamRef;
 using base::android::ScopedJavaLocalRef;
 }  // namespace
 
-EventForwarder::EventForwarder(ViewAndroid* view) : view_(view) {}
+EventForwarder::EventForwarder(ViewAndroid* view)
+    : view_(view),
+      send_touch_moves_to_observers(base::FeatureList::IsEnabled(
+          kSendTouchMovesToEventForwarderObservers)) {}
 
 EventForwarder::~EventForwarder() {
   if (!java_obj_.is_null()) {
@@ -124,8 +128,18 @@ jboolean EventForwarder::OnTouchEvent(JNIEnv* env,
       raw_pos_x - pos_x_0, raw_pos_y - pos_y_0, for_touch_handle, &pointer0,
       &pointer1);
 
-  for (auto& observer : observers_) {
-    observer.OnTouchEvent(event);
+  if (send_touch_moves_to_observers ||
+      android_action !=
+          MotionEventAndroid::GetAndroidAction(MotionEvent::Action::MOVE)) {
+    // Don't send touch moves to observers. Currently we just have one observer
+    // which shouldn't be affected by this. This is a temporary change until we
+    // have confirmed touch moves are not required by the observer and we can
+    // cleanup the observer API.
+    // TODO(b/328601354): Confirm touch moves are not required, and if they are
+    // not required cleanup the observer API.
+    for (auto& observer : observers_) {
+      observer.OnTouchEvent(event);
+    }
   }
 
   return view_->OnTouchEvent(event);
