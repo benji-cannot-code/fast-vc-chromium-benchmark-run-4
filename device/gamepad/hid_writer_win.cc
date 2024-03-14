17fde376d8cfac0d69ecf5ab8bc052471fe63014
Gabriel Brito <gabrielbrito@microsoft.com>
2024-03-14 00:06:37 +0000

    [Gamepad] Remove std::unique_ptr<T[]> from HidWriterWin
    This CL replaces occurrences of std::unique_ptr<T[]> with std::wstring.
    Using std::unique_ptr<T[]> does not automatically preserve the size of
    the allocation. This forces a need for ad-hoc bounds checks, leading to
    bugs which attackers use to compromise our users.
    
    This CL also contain a small follow-up fix to CL 5368671 in raw_gamepad_device_win.cc
    
    Bug: 326458686, 40285824
    Change-Id: Ie5884836dd018e362a08e89541046f26c313aef9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5370756
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Gabriel Brito <gabrielbrito@microsoft.com>
    Auto-Submit: Gabriel Brito <gabrielbrito@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1272514}

--- device/gamepad/hid_writer_win.cc
+++ device/gamepad/hid_writer_win.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 #include <windows.h>
 
+#include "base/strings/string_util_win.h"
+
 namespace device {
 
 HidWriterWin::HidWriterWin(HANDLE device) {
@@ -17,13 +19,13 @@ HidWriterWin::HidWriterWin(HANDLE device) {
   UINT result =
       ::GetRawInputDeviceInfo(device, RIDI_DEVICENAME, nullptr, &size);
   if (result == 0U) {
-    std::unique_ptr<wchar_t[]> name_buffer(new wchar_t[size]);
-    result = ::GetRawInputDeviceInfo(device, RIDI_DEVICENAME, name_buffer.get(),
-                                     &size);
+    std::wstring name_buffer;
+    result = ::GetRawInputDeviceInfo(
+        device, RIDI_DEVICENAME, base::WriteInto(&name_buffer, size), &size);
     if (result == size) {
       // Open the device handle for asynchronous I/O.
       hid_handle_.Set(
-          ::CreateFile(name_buffer.get(), GENERIC_READ | GENERIC_WRITE,
+          ::CreateFile(name_buffer.c_str(), GENERIC_READ | GENERIC_WRITE,
                        FILE_SHARE_READ | FILE_SHARE_WRITE, nullptr,
                        OPEN_EXISTING, FILE_FLAG_OVERLAPPED, nullptr));
     }
--- device/gamepad/raw_input_gamepad_device_win.cc
+++ device/gamepad/raw_input_gamepad_device_win.cc
@@ -378,7 +378,7 @@ bool RawInputGamepadDeviceWin::QueryDeviceName() {
   }
   DCHECK_EQ(size, result);
 
-  name_ = buffer.data();
+  name_ = std::move(buffer);
 
   return true;
 }
