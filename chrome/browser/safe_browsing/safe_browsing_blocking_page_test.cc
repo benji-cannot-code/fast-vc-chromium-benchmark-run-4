4e264cd69aab402d412e5570013a33c8c880c85b
Sarah Krakowiak Criel <skrakowi@chromium.org>
2024-03-14 18:09:05 +0000

    Implement Safe Browsing Interstitial UKM
    To help us accurately understand how safe browsing interstitial events
    occur in relation to other events (such as users accepting notification
    permissions for a site), we want to add a UKM that logs occurrences of
    the two events: Safe Browsing interstitial shown and user proceeds past
    interstitial. See the UKM collection doc [1] and UKM data usage
    doc[2] for more details.
    
    [1] https://docs.google.com/document/d/1CRC_8zRzbAjMT7Rbfmt6cdqJLHELZzP6O9tfVlohsbs
    [2] https://docs.google.com/document/d/1dht3-0s5BXbGQt75i3GSJrrRjpO14WcGxSz5LX9Bo3c
    
    Bug: 307743408
    Change-Id: Ia49a4fa5bcf545ae18015dd70231f08cc7d728ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5301918
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Sarah Krakowiak <skrakowi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272915}

--- chrome/browser/safe_browsing/safe_browsing_blocking_page_test.cc
+++ chrome/browser/safe_browsing/safe_browsing_blocking_page_test.cc
@@ -102,6 +102,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/security_interstitials/core/urls.h"
 #include "components/security_state/core/security_state.h"
 #include "components/strings/grit/components_strings.h"
+#include "components/ukm/test_ukm_recorder.h"
 #include "components/unified_consent/pref_names.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
@@ -1442,6 +1443,7 @@ IN_PROC_BROWSER_TEST_P(SafeBrowsingBlockingPageBrowserTest, MAYBE_LearnMore) {
 
 IN_PROC_BROWSER_TEST_P(SafeBrowsingBlockingPageBrowserTest,
                        Histograms_DontProceed) {
+  ukm::TestAutoSetUkmRecorder test_ukm_recorder;
   WebContents* web_contents =
       browser()->tab_strip_model()->GetActiveWebContents();
   SafeBrowsingMetricsCollector* metrics_collector =
@@ -1521,10 +1523,18 @@ IN_PROC_BROWSER_TEST_P(SafeBrowsingBlockingPageBrowserTest,
       security_interstitials::SecurityInterstitialTabHelper::
           InterstitialCloseReason::NAVIGATE_AWAY,
       1);
+
+  // Check that we are recording the UKM when interstitial is shown and we do
+  // not record for the interstitial bypassed.
+  auto ukm_entries =
+      test_ukm_recorder.GetEntriesByName("SafeBrowsingInterstitial");
+  EXPECT_EQ(1u, ukm_entries.size());
+  test_ukm_recorder.ExpectEntryMetric(ukm_entries[0], "Shown", true);
 }
 
 IN_PROC_BROWSER_TEST_P(SafeBrowsingBlockingPageBrowserTest,
                        Histograms_Proceed) {
+  ukm::TestAutoSetUkmRecorder test_ukm_recorder;
   base::HistogramTester histograms;
   SBThreatType threat_type = GetThreatType();
   std::string prefix = GetHistogramPrefix(threat_type);
@@ -1589,6 +1599,14 @@ IN_PROC_BROWSER_TEST_P(SafeBrowsingBlockingPageBrowserTest,
       security_interstitials::SecurityInterstitialTabHelper::
           InterstitialCloseReason::NAVIGATE_AWAY,
       1);
+
+  // Check that we are recording the UKM when interstitial is shown and we do
+  // not record for the interstitial bypassed.
+  auto ukm_entries =
+      test_ukm_recorder.GetEntriesByName("SafeBrowsingInterstitial");
+  EXPECT_EQ(2u, ukm_entries.size());
+  test_ukm_recorder.ExpectEntryMetric(ukm_entries[0], "Shown", true);
+  test_ukm_recorder.ExpectEntryMetric(ukm_entries[1], "Bypassed", true);
 }
 
 IN_PROC_BROWSER_TEST_P(SafeBrowsingBlockingPageBrowserTest,
