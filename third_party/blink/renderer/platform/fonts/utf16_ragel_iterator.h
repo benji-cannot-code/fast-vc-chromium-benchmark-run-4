e4203f4d60da9a16dcdb770780ab8aa659d16809
Dominik Röttsches <drott@chromium.org>
2024-03-14 14:24:54 +0000

    Accelerate `UTF16RagelIterator`
    Improve caching: Due to the Ragel scanner using two different
    UTF16RagelIterator objects, this frequently lead to situations where
    we would call the character properties calls redundantly for the same
    cursor position. Improve the caching logic to avoid that, by moving
    the lookup to dereferencing time.
    
    This leads to speedups of 1.77x vs 1.22x speed-up respectively in the
    synthetic SymbolsIterator benchmark [1]:
    
    Without [2], average of 3 runs of [1]:
    21.033s vs. 17.337s = 1.21x speed-up
    
    With [2], avg of 3 runs of [1]:
    5.154s vs 3.064s = 1.68x speed-up
    
    The difference between with [2]/without [2] is explained by the relative
    amount of time that is spent in UTF16RagelIterator, which increases
    after the optimizations in [2].
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/5307975
    [2] https://chromium-review.googlesource.com/c/chromium/src/+/5317569
    
    Bug: 41495057, 325814602
    Change-Id: Id94ce034bd6b601cae65e354e74123bd9b469f7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5317649
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272756}

--- third_party/blink/renderer/platform/fonts/utf16_ragel_iterator.h
+++ third_party/blink/renderer/platform/fonts/utf16_ragel_iterator.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/text/emoji_segmentation_category.h"
+#include "third_party/blink/renderer/platform/text/emoji_segmentation_category_inline_header.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 
 namespace blink {
@@ -26,7 +27,11 @@ class PLATFORM_EXPORT UTF16RagelIterator {
   DISALLOW_NEW();
 
  public:
-  UTF16RagelIterator() : buffer_(nullptr), buffer_size_(0), cursor_(0) {}
+  UTF16RagelIterator()
+      : buffer_(nullptr),
+        buffer_size_(0),
+        cursor_(0),
+        cached_category_(EmojiSegmentationCategory::kInvalidCacheEntry) {}
 
   UTF16RagelIterator(const UChar* buffer,
                      unsigned buffer_size,
@@ -34,9 +39,7 @@ class PLATFORM_EXPORT UTF16RagelIterator {
       : buffer_(buffer),
         buffer_size_(buffer_size),
         cursor_(cursor),
-        cached_category_(EmojiSegmentationCategory::kMaxCategory) {
-    UpdateCachedCategory();
-  }
+        cached_category_(EmojiSegmentationCategory::kInvalidCacheEntry) {}
 
   UTF16RagelIterator end() {
     UTF16RagelIterator ret = *this;
@@ -44,7 +47,15 @@ class PLATFORM_EXPORT UTF16RagelIterator {
     return ret;
   }
 
-  UTF16RagelIterator& SetCursor(unsigned new_cursor);
+  unsigned size() { return buffer_size_; }
+
+  UTF16RagelIterator& SetCursor(unsigned new_cursor) {
+    DCHECK_GE(new_cursor, 0u);
+    DCHECK_LT(new_cursor, buffer_size_);
+    cursor_ = new_cursor;
+    InvalidateCache();
+    return *this;
+  }
 
   unsigned Cursor() { return cursor_; }
 
@@ -54,7 +65,7 @@ class PLATFORM_EXPORT UTF16RagelIterator {
     } else if (v < 0) {
       U16_BACK_N(buffer_, 0, cursor_, -v);
     }
-    UpdateCachedCategory();
+    InvalidateCache();
     return *this;
   }
 
@@ -75,14 +86,14 @@ class PLATFORM_EXPORT UTF16RagelIterator {
   UTF16RagelIterator& operator++() {
     DCHECK_LT(cursor_, buffer_size_);
     U16_FWD_1(buffer_, cursor_, buffer_size_);
-    UpdateCachedCategory();
+    InvalidateCache();
     return *this;
   }
 
   UTF16RagelIterator& operator--() {
     DCHECK_GT(cursor_, 0u);
     U16_BACK_1(buffer_, 0, cursor_);
-    UpdateCachedCategory();
+    InvalidateCache();
     return *this;
   }
 
@@ -108,8 +119,17 @@ class PLATFORM_EXPORT UTF16RagelIterator {
   }
 
   EmojiSegmentationCategory operator*() {
-    CHECK(buffer_size_);
+    DCHECK(buffer_size_);
+    if (cached_category_ == EmojiSegmentationCategory::kInvalidCacheEntry) {
+      UChar32 codepoint;
+      U16_GET(buffer_, 0, cursor_, buffer_size_, codepoint);
+      cached_category_ = GetEmojiSegmentationCategory(codepoint);
+    }
     return cached_category_;
+  }
+
+  inline void InvalidateCache() {
+    cached_category_ = EmojiSegmentationCategory::kInvalidCacheEntry;
   }
 
   bool operator==(const UTF16RagelIterator& other) const {
@@ -122,15 +142,6 @@ class PLATFORM_EXPORT UTF16RagelIterator {
   }
 
  private:
-  UChar32 Codepoint() const {
-    DCHECK_GT(buffer_size_, 0u);
-    UChar32 output;
-    U16_GET(buffer_, 0, cursor_, buffer_size_, output);
-    return output;
-  }
-
-  void UpdateCachedCategory();
-
   const UChar* buffer_;
   unsigned buffer_size_;
   unsigned cursor_;
