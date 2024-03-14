bb5a2573fb85b3a2cacf9213d4b0e3963ee3f42e
Ahmed Mehfooz <amehfooz@chromium.org>
2024-03-14 23:17:40 +0000

    Add option to set TrayBubbleView as activatable
    This CL adds an init_param option to make a `TrayBubbleView` activatable
    on click or tap events.
    This CL does not change any current behavior.
    
    Additional context:
    
    TrayBubbleViews are not activatable by default, the RerouteEventHandler
    sets them as activatable when a keyboard event is detected so the
    bubble can get focus. For some bubbles like the NotificationCenterBubble
    we need them to get focus after clicks/taps like when clicking on a
    reply button on a notification. This is currently handled in an obscure
    way by a `ClickActivator` in `NotificationViewBase`. We should handle
    this in TrayBubbleView logic instead and clean up `ClickActivator`.
    
    Bug: 329712701
    
    Change-Id: I3d858c2dcae661b879c97daa2f1d82c7033442c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372421
    Reviewed-by: Andre Le <leandre@chromium.org>
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273116}

--- ash/system/tray/tray_bubble_view.cc
+++ ash/system/tray/tray_bubble_view.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor_extra/shadow.h"
 #include "ui/display/manager/display_manager.h"
 #include "ui/events/event.h"
+#include "ui/events/types/event_type.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/geometry/insets.h"
@@ -252,6 +253,26 @@ void TrayBubbleView::RerouteEventHandler::OnKeyEvent(ui::KeyEvent* event) {
   }
 }
 
+void TrayBubbleView::RerouteEventHandler::OnMouseEvent(
+    ui::MouseEvent* mouse_event) {
+  if (tray_bubble_view_->set_can_activate_on_click_or_tap_ &&
+      mouse_event->type() == ui::ET_MOUSE_PRESSED) {
+    tray_bubble_view_->SetCanActivate(true);
+  }
+}
+
+void TrayBubbleView::RerouteEventHandler::OnTouchEvent(
+    ui::TouchEvent* mouse_event) {
+  if (!tray_bubble_view_->set_can_activate_on_click_or_tap_) {
+    return;
+  }
+
+  if (tray_bubble_view_->set_can_activate_on_click_or_tap_ &&
+      mouse_event->type() == ui::ET_MOUSE_PRESSED) {
+    tray_bubble_view_->SetCanActivate(true);
+  }
+}
+
 TrayBubbleView::TrayBubbleView(const InitParams& init_params)
     : BubbleDialogDelegateView(init_params.anchor_view,
                                GetArrowAlignment(init_params.shelf_alignment)),
@@ -260,6 +281,8 @@ TrayBubbleView::TrayBubbleView(const InitParams& init_params)
       delegate_(init_params.delegate),
       preferred_width_(init_params.preferred_width),
       is_gesture_dragging_(false),
+      set_can_activate_on_click_or_tap_(
+          init_params.set_can_activate_on_click_or_tap),
       mouse_actively_entered_(false) {
   // We set the dialog role because views::BubbleDialogDelegate defaults this to
   // an alert dialog. This would make screen readers announce the whole of the
