444f4945702fae11f281832f5d6e005ef53029f0
Liviu Tinta <liviutinta@chromium.org>
2024-03-14 14:03:56 +0000

    [nvs-prerender] Cache No-Vary-Search header on PrerenderHost
    Start adding No-Vary-Search support for prerender2.
    
    In this CL, add a feature flag and cache No-Vary-Search header
    on PrerenderHost when the head is received.
    
    Bug: 41494389
    Change-Id: Ieec3faaf991245499ff78f19efc0d49e45dc0a86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5318878
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Liviu Tinta <liviutinta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272738}

--- content/browser/preloading/prerender/prerender_host.cc
+++ content/browser/preloading/prerender/prerender_host.cc
@@ -17,7 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/typed_macros.h"
 #include "content/browser/client_hints/client_hints.h"
 #include "content/browser/devtools/devtools_instrumentation.h"
+#include "content/browser/preloading/prefetch/no_vary_search_helper.h"
 #include "content/browser/preloading/prerender/devtools_prerender_attempt.h"
+#include "content/browser/preloading/prerender/prerender_features.h"
 #include "content/browser/preloading/prerender/prerender_final_status.h"
 #include "content/browser/preloading/prerender/prerender_host_registry.h"
 #include "content/browser/preloading/prerender/prerender_metrics.h"
@@ -444,7 +446,7 @@ void PrerenderHost::DidStartNavigation(NavigationHandle* navigation_handle) {
   CHECK(navigation_request->IsInPrerenderedMainFrame());
 
   // Do nothing for the initial navigation.
-  if (GetInitialNavigationId() == navigation_request->GetNavigationId()) {
+  if (IsInitialNavigation(*navigation_request)) {
     return;
   }
 
@@ -453,6 +455,29 @@ void PrerenderHost::DidStartNavigation(NavigationHandle* navigation_handle) {
   // prerendered page and PrerenderHost::DidFinishNavigation is called multiple
   // times.
   is_ready_for_activation_ = false;
+}
+
+void PrerenderHost::ReadyToCommitNavigation(
+    NavigationHandle* navigation_handle) {
+  CHECK(navigation_handle);
+  // For the initial navigation, set No-Vary-Search if there is a
+  // No-Vary-Search header.
+  auto* navigation_request = NavigationRequest::From(navigation_handle);
+  CHECK(navigation_request->IsInPrerenderedMainFrame());
+  if (base::FeatureList::IsEnabled(features::kPrerender2NoVarySearch) &&
+      IsInitialNavigation(*navigation_request) &&
+      navigation_request->response() &&
+      navigation_request->response()->parsed_headers &&
+      navigation_request->response()
+          ->parsed_headers->no_vary_search_with_parse_error &&
+      navigation_request->response()
+          ->parsed_headers->no_vary_search_with_parse_error
+          ->is_no_vary_search()) {
+    SetNoVarySearch(no_vary_search::ParseHttpNoVarySearchDataFromMojom(
+        navigation_request->response()
+            ->parsed_headers->no_vary_search_with_parse_error
+            ->get_no_vary_search()));
+  }
 }
 
 void PrerenderHost::DidFinishNavigation(NavigationHandle* navigation_handle) {
@@ -1167,6 +1192,16 @@ void PrerenderHost::Cancel(PrerenderFinalStatus status) {
       host->delegate()->GetPrerenderHostRegistry();
   CHECK(registry);
   registry->CancelHost(frame_tree_node_id_, status);
+}
+
+void PrerenderHost::SetNoVarySearch(net::HttpNoVarySearchData no_vary_search) {
+  CHECK(!no_vary_search_);
+  no_vary_search_ = std::move(no_vary_search);
+}
+
+bool PrerenderHost::IsInitialNavigation(
+    const NavigationRequest& navigation_request) const {
+  return GetInitialNavigationId() == navigation_request.GetNavigationId();
 }
 
 }  // namespace content
