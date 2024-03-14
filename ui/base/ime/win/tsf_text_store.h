fe3c6161189375fca8ce50a57944df46c521c222
Anupam Snigdha <snianu@microsoft.com>
2024-03-14 19:27:54 +0000

    [TSF] Cleanup code.
    Make the variable names consistent.
    
    Bug: 40064682
    
    Change-Id: I48a60a9f4ac4f8b3b869714dedb0991b145ad9f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372965
    Commit-Queue: Yohei Yukawa <yukawa@chromium.org>
    Reviewed-by: Yohei Yukawa <yukawa@chromium.org>
    Commit-Queue: Anupam Snigdha <snianu@microsoft.com>
    Auto-Submit: Anupam Snigdha <snianu@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1272989}

--- ui/base/ime/win/tsf_text_store.h
+++ ui/base/ime/win/tsf_text_store.h
@@ -275,7 +275,7 @@ class COMPONENT_EXPORT(UI_BASE_IME_WIN) TSFTextStore
   bool MaybeSendOnUrlChanged();
 
   // Sets the flag to indicate TSF support for empty text stores.
-  void SetUseEmptyTextStore(bool isEnabled);
+  void UseEmptyTextStore(bool is_enabled);
 
  private:
   friend class TSFTextStoreTest;
