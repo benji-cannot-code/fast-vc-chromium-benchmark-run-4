24ba9d3a67fb7ff3e63f469501fa57abefd548d7
Chris Lu <thegreenfrog@chromium.org>
2024-03-14 17:00:28 +0000

    [ios] Disable SetUpList for TabResumptionTestCase
    Change-Id: I66ab8b488d1dc523d933378f926bf500ad8f8b82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372530
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1272868}

--- ios/chrome/browser/ui/content_suggestions/tab_resumption/tab_resumption_egtest.mm
+++ ios/chrome/browser/ui/content_suggestions/tab_resumption/tab_resumption_egtest.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey_ui_test_util.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_constants.h"
+#import "ios/chrome/browser/ui/content_suggestions/new_tab_page_app_interface.h"
 #import "ios/chrome/browser/ui/content_suggestions/tab_resumption/tab_resumption_constants.h"
 #import "ios/chrome/browser/ui/ntp/ntp_app_interface.h"
 #import "ios/chrome/browser/ui/start_surface/start_surface_features.h"
@@ -119,6 +120,7 @@ NSString* HostnameFromGURL(GURL URL) {
   [ChromeEarlGrey clearBrowsingHistory];
   GREYAssertTrue(self.testServer->Start(), @"Test server failed to start.");
   SignInAndSync();
+  [NewTabPageAppInterface disableSetUpList];
   [NTPAppInterface recordModuleFreshnessSignalForType:
                        ContentSuggestionsModuleType::kTabResumption];
   [[self class] closeAllTabs];
