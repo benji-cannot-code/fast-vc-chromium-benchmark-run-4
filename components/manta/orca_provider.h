48f2cf86957e2fd871453068be03d88973304a00
Xinglong Luan <alanlxl@google.com>
2024-03-14 10:32:53 +0000

    Refactor manta service and providers to avoid duplication
    This CL adds base_provider.(cc|h) and base_provider_test_helper.(cc|h)
    to simplify the implementation of particular providers and their unittests.
    
    It also provides a unified place to take care of the network fetcher and
    authorization.
    
    Bug: b:329350049
    Test: autoninja -C out_brya/Release chrome
    Test: components_unittests --gtest_filter="SnapperProviderTest.*:OrcaProviderTest.*"
    Change-Id: I38902f2b864cd26527f2926509fe57a646acf08e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5369510
    Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    Commit-Queue: Xinglong Luan <alanlxl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272670}

--- components/manta/orca_provider.h
+++ components/manta/orca_provider.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "components/endpoint_fetcher/endpoint_fetcher.h"
+#include "components/manta/base_provider.h"
 #include "components/manta/manta_service_callbacks.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
@@ -28,8 +29,7 @@ namespace manta {
 // IMPORTANT: This class depends on `IdentityManager`.
 // `OrcaProvider::Call` will return an empty response after `IdentityManager`
 // destruction.
-class COMPONENT_EXPORT(MANTA) OrcaProvider
-    : public signin::IdentityManager::Observer {
+class COMPONENT_EXPORT(MANTA) OrcaProvider : virtual public BaseProvider {
  public:
   // Returns a `OrcaProvider` instance tied to the profile of the passed
   // arguments.
@@ -50,26 +50,8 @@ class COMPONENT_EXPORT(MANTA) OrcaProvider
   void Call(const std::map<std::string, std::string>& input,
             MantaGenericCallback done_callback);
 
-  // signin::IdentityManager::Observer:
-  void OnIdentityManagerShutdown(
-      signin::IdentityManager* identity_manager) override;
-
  private:
   friend class FakeOrcaProvider;
-
-  // Creates and returns unique pointer to an `EndpointFetcher` initialized with
-  // the provided parameters and defaults relevant to `OrcaProvider`. Virtual
-  // to allow overriding in tests.
-  virtual std::unique_ptr<EndpointFetcher> CreateEndpointFetcher(
-      const GURL& url,
-      const std::vector<std::string>& scopes,
-      const std::string& post_data);
-
-  const scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
-
-  base::ScopedObservation<signin::IdentityManager,
-                          signin::IdentityManager::Observer>
-      identity_manager_observation_{this};
 
   base::WeakPtrFactory<OrcaProvider> weak_ptr_factory_{this};
 };
