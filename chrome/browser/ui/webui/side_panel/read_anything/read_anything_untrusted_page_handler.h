ccd025d5275b4fed45cbf0e373c36a0b55303743
Abigail Klein <abigailbklein@google.com>
2024-03-14 22:44:38 +0000

    [Read Anything] Refactor Reading Mode handling of PDFs.
    Previously, when a user opened a PDF with with reading mode enabled,
    reading mode would do the following:
    1. ReadAnythingUntrustedPageHandler in the browser process sends
       AXTreeID of web contents to ReadAnythingAppController in the
       renderer process.
    2. Controller and ReadAnythingAppModel determine whether the web
       contents is for a pdf by checking the url (this was delicate
       because it required the url ending with ".pdf" which isn't always
       the case).
    3. If the web contents is for a pdf, controller sends a message
       back to page handler that it's for a pdf.
    4. Page handler begins to observe the pdf web contents and sends its
       AXTreeID to the controller.
    
    This CL remove the back and forth between the browser and renderer.
    Page handler figures out whether a web contents is for a PDF by
    checking if it has an inner web contents, and if that inner web
    contents has a pdf extension origin. It then sends the correct
    AXTreeID to controller.
    
    Controller and model still need to know whether the AXTreeID
    corresponds to a PDF because there are certain formatting changes we
    do in that case, such as modifying the html tag.
    
    Fixed: 41496290
    Change-Id: Ibcb50bbb96994bb01125de60024a4d8b1535a9c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5301501
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1273100}

--- chrome/browser/ui/webui/side_panel/read_anything/read_anything_untrusted_page_handler.h
+++ chrome/browser/ui/webui/side_panel/read_anything/read_anything_untrusted_page_handler.h
@@ -126,7 +126,6 @@ class ReadAnythingUntrustedPageHandler
                          ui::AXNodeID focus_node_id,
                          int focus_offset) override;
   void OnCollapseSelection() override;
-  void EnablePDFContentAccessibility(const ui::AXTreeID& ax_tree_id) override;
 
   // ReadAnythingModel::Observer:
   void OnReadAnythingThemeChanged(
@@ -165,9 +164,9 @@ class ReadAnythingUntrustedPageHandler
   // 2. Notifies the model that the AXTreeID has changed.
   void OnActiveWebContentsChanged();
 
-  // force_update_state will tell the UI to update the state even if the active
-  // tree id does not change.
-  void OnActiveAXTreeIDChanged(bool force_update_state = false);
+  void SetUpPdfObserver();
+
+  void OnActiveAXTreeIDChanged();
 
   // Logs the current visual settings values.
   void LogTextStyle();
