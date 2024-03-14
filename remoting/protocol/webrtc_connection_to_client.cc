2ee87aa4512447f285be12cfeebaeb32d0866786
Yuwei Huang <yuweih@chromium.org>
2024-03-14 23:35:42 +0000

    [remoting host] Don't report INCOMPATIBLE_PROTOCOL when channel is closed
    It appears that the host reports INCOMPATIBLE_PROTOCOL error even when
    it's the user clicking the disconnect button on the client. The problem
    seems to be that the control channel is closed, and the existing code
    just believes that it is unexpected and disconnects the session with the
    INCOMPATIBLE_PROTOCOL error.
    
    I don't see why we can't change the error code to be report to OK. There
    is a OnWebrtcTransportError() which should supposedly handle error
    cases. Based on my observation, the control channel is closed only when
    the user clicks disconnect, or closes the tab on the client. If I just
    kill the Chrome process, then the host will seemingly be unaware of the
    peer going offline (which sounds like another bug, filed
    crbug.com/329716908).
    
    Given this, this CL just changes the error code to OK. This might
    potentially introduce false negatives that I'm not aware of, but I think
    the overall accuracy is higher if we report OK.
    
    Bug: 329551346
    Change-Id: I315caa56dbef0537367021e98d941dd90c7e9e53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5371975
    Auto-Submit: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273125}

--- remoting/protocol/webrtc_connection_to_client.cc
+++ remoting/protocol/webrtc_connection_to_client.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "components/webrtc/thread_wrapper.h"
 #include "net/base/io_buffer.h"
+#include "remoting/base/logging.h"
 #include "remoting/codec/video_encoder.h"
 #include "remoting/codec/webrtc_video_encoder_vpx.h"
 #include "remoting/protocol/audio_source.h"
@@ -287,13 +288,17 @@ void WebrtcConnectionToClient::OnChannelClosed(
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
 
   if (channel_dispatcher == &video_stats_dispatcher_) {
-    LOG(WARNING) << "video_stats channel was closed.";
+    HOST_LOG << "video_stats channel was closed.";
     return;
   }
 
-  LOG(ERROR) << "Channel " << channel_dispatcher->channel_name()
-             << " was closed unexpectedly.";
-  Disconnect(ErrorCode::INCOMPATIBLE_PROTOCOL);
+  // The control channel is closed when the user clicks disconnect on the client
+  // or the client page is closed normally. If the client goes offline then the
+  // channel will remain open. Hence it should be safe to report ErrorCode::OK
+  // here.
+  HOST_LOG << "Channel " << channel_dispatcher->channel_name()
+           << " was closed.";
+  Disconnect(ErrorCode::OK);
 }
 
 bool WebrtcConnectionToClient::allChannelsConnected() {
