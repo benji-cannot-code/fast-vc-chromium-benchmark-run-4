b849572f15cbd539f4e8c962b2364b1314b2bcb7
Min Qin <qinmin@chromium.org>
2024-03-14 15:54:08 +0000

    Move more java classes to use BaseNotificationManagerProxy
    This will allow us to switch to the async version of the
    NotificationManagerProxy
    
    Bug: b:242038130, b:242028843
    Change-Id: Iff3ca6a20b2ec004d1c5a2ca28362662e6d91a14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5371060
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272814}

--- chrome/android/java/src/org/chromium/chrome/browser/incognito/IncognitoNotificationManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/incognito/IncognitoNotificationManager.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.incognito;
 
 import android.app.Notification;
-import android.app.NotificationManager;
 import android.content.Context;
 
 import org.chromium.base.ContextUtils;
@@ -15,8 +14,7 @@ import org.chromium.chrome.browser.notifications.NotificationConstants;
 import org.chromium.chrome.browser.notifications.NotificationUmaTracker;
 import org.chromium.chrome.browser.notifications.NotificationWrapperBuilderFactory;
 import org.chromium.chrome.browser.notifications.channels.ChromeChannelDefinitions;
-import org.chromium.components.browser_ui.notifications.NotificationManagerProxy;
-import org.chromium.components.browser_ui.notifications.NotificationManagerProxyImpl;
+import org.chromium.components.browser_ui.notifications.BaseNotificationManagerProxyFactory;
 import org.chromium.components.browser_ui.notifications.NotificationMetadata;
 import org.chromium.components.browser_ui.notifications.NotificationWrapper;
 import org.chromium.components.browser_ui.notifications.NotificationWrapperBuilder;
@@ -56,9 +54,8 @@ public class IncognitoNotificationManager {
                         .setShowWhen(false)
                         .setLocalOnly(true)
                         .setGroup(NotificationConstants.GROUP_INCOGNITO);
-        NotificationManagerProxy nm = new NotificationManagerProxyImpl(context);
         NotificationWrapper notification = builder.buildNotificationWrapper();
-        nm.notify(notification);
+        BaseNotificationManagerProxyFactory.create(context).notify(notification);
         NotificationUmaTracker.getInstance()
                 .onNotificationShown(
                         NotificationUmaTracker.SystemNotificationType.CLOSE_INCOGNITO,
@@ -68,8 +65,7 @@ public class IncognitoNotificationManager {
     /** Dismisses the incognito notification. */
     public static void dismissIncognitoNotification() {
         Context context = ContextUtils.getApplicationContext();
-        NotificationManager nm =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        nm.cancel(INCOGNITO_TABS_OPEN_TAG, INCOGNITO_TABS_OPEN_ID);
+        BaseNotificationManagerProxyFactory.create(context)
+                .cancel(INCOGNITO_TABS_OPEN_TAG, INCOGNITO_TABS_OPEN_ID);
     }
 }
--- chrome/android/java/src/org/chromium/chrome/browser/notifications/scheduler/DisplayAgent.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/scheduler/DisplayAgent.java
@@ -26,7 +26,7 @@ import org.chromium.chrome.browser.notifications.NotificationUmaTracker;
 import org.chromium.chrome.browser.notifications.NotificationUmaTracker.SystemNotificationType;
 import org.chromium.chrome.browser.notifications.NotificationWrapperBuilderFactory;
 import org.chromium.chrome.browser.notifications.channels.ChromeChannelDefinitions.ChannelId;
-import org.chromium.components.browser_ui.notifications.NotificationManagerProxyImpl;
+import org.chromium.components.browser_ui.notifications.BaseNotificationManagerProxyFactory;
 import org.chromium.components.browser_ui.notifications.NotificationMetadata;
 import org.chromium.components.browser_ui.notifications.NotificationWrapper;
 import org.chromium.components.browser_ui.notifications.NotificationWrapperBuilder;
@@ -211,7 +211,7 @@ public class DisplayAgent {
     }
 
     private static void closeNotification(String guid) {
-        new NotificationManagerProxyImpl(ContextUtils.getApplicationContext())
+        BaseNotificationManagerProxyFactory.create(ContextUtils.getApplicationContext())
                 .cancel(DISPLAY_AGENT_TAG, guid.hashCode());
     }
 
@@ -337,7 +337,8 @@ public class DisplayAgent {
         }
 
         NotificationWrapper notification = builder.buildNotificationWrapper();
-        new NotificationManagerProxyImpl(ContextUtils.getApplicationContext()).notify(notification);
+        BaseNotificationManagerProxyFactory.create(ContextUtils.getApplicationContext())
+                .notify(notification);
         NotificationUmaTracker.getInstance()
                 .onNotificationShown(
                         platformData.systemNotificationType, notification.getNotification());
