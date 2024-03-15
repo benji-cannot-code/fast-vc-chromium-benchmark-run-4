2500c2dc85be58d763128d0357a882bf545ab359
Igor <igorcov@chromium.org>
2024-03-15 16:15:02 +0000

    Fix the histogram name
    The histogram name was mismatched in the code compared to what was
    declared, so nothing got logged so far. This fixes the problem.
    
    BUG=None
    TEST=None
    
    Change-Id: Ib0dac629fda2e723e4664d956ec656fec961fac5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372711
    Reviewed-by: Roland Bock <rbock@google.com>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1273449}

--- chrome/browser/ash/policy/enrollment/enrollment_config.cc
+++ chrome/browser/ash/policy/enrollment/enrollment_config.cc
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const char kRecoveryHistogram[] = "Enterprise.EnrollementRecoveryOnBoot";
+const char kRecoveryHistogram[] = "EnterpriseCheck.EnrollementRecoveryOnBoot";
 
 // Do not reorder or delete entries because it is used in UMA.
 enum class EnrollmentRecoveryOnBootUma {
