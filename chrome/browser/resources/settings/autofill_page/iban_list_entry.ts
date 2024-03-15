1ac6e73a1060e6929d2194576a960cc9f4b14c09
rbpotter <rbpotter@chromium.org>
2024-03-15 00:00:54 +0000

    WebUI: Move screen reader only css to only user
    This was pre-emptively placed in ui/webui/resources with assurances
    that it would soon be used elsewhere, but after ~6 months it is still
    only used by the settings autofill page. Move it there.
    
    Bug: None
    Change-Id: Ib3aece540a919224b9f1cc2f317bb5774961a4de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372752
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273141}

--- chrome/browser/resources/settings/autofill_page/iban_list_entry.ts
+++ chrome/browser/resources/settings/autofill_page/iban_list_entry.ts
@@ -9,11 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.js';
-import 'chrome://resources/cr_elements/cr_screen_reader_only.css.js';
 import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
 import '../i18n_setup.js';
 import '../settings_shared.css.js';
 import './passwords_shared.css.js';
+import './screen_reader_only.css.js';
 
 import {I18nMixin} from '//resources/cr_elements/i18n_mixin.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
