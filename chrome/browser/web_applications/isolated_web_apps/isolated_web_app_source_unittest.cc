ece869990a9936390f24a9455a4613082bfbc6c7
Christian Flach <cmfcmf@chromium.org>
2024-03-15 17:09:05 +0000

    iwa: Add `IwaSource*WithMode::WithFileOp`, rework source inheritance
    This CL does two things:
    1. The new `IwaSource(Bundle)WithMode::WithFileOp` method closes the
       missing link between sources with mode and sources with mode and file
       op.
    2. We previously inherited all bundle-based sources from
       `IwaSourceBundle`. This was nice for sharing code and allowing
       implicit conversions, but allowed you to write code like the
       following, which doesn't really make sense:
       ```
       IwaSourceBundleProdModeWithFileOp bundle;
       IwaSourceBundleDevModeWithFileOp new_bundle =
         bundle.WithDevModeFileOp(
           IwaSourceBundleDevFileOp::kReference
         );
       ```
       With the changes in this CL, you'd have to be more explicit if you
       _really_ wanted to achieve this:
       ```
       IwaSourceBundleProdModeWithFileOp bundle;
       IwaSourceBundleDevModeWithFileOp new_bundle(
         bundle.path(),
         IwaSourceBundleDevFileOp::kReference
       );
       ```
    
    Bug: None
    Change-Id: Ibb202bc34f4a56a396270618fca5ba72e53ed123
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372845
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Auto-Submit: Christian Flach <cmfcmf@chromium.org>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273492}

--- chrome/browser/web_applications/isolated_web_apps/isolated_web_app_source_unittest.cc
+++ chrome/browser/web_applications/isolated_web_apps/isolated_web_app_source_unittest.cc
@@ -129,6 +129,25 @@ TEST_F(IwaSourceBundleWithModeTest, FromDevOrProdMode) {
   }
 }
 
+TEST_F(IwaSourceBundleWithModeTest, WithFileOp) {
+  {
+    IwaSourceBundleWithMode bundle{kExamplePath, /*dev_mode=*/false};
+    EXPECT_THAT(
+        bundle.WithFileOp(IwaSourceBundleProdFileOp::kCopy,
+                          IwaSourceBundleDevFileOp::kReference),
+        Eq(IwaSourceBundleWithModeAndFileOp{
+            kExamplePath, IwaSourceBundleModeAndFileOp::kProdModeCopy}));
+  }
+  {
+    IwaSourceBundleWithMode bundle{kExamplePath, /*dev_mode=*/true};
+    EXPECT_THAT(
+        bundle.WithFileOp(IwaSourceBundleProdFileOp::kCopy,
+                          IwaSourceBundleDevFileOp::kReference),
+        Eq(IwaSourceBundleWithModeAndFileOp{
+            kExamplePath, IwaSourceBundleModeAndFileOp::kDevModeReference}));
+  }
+}
+
 using IwaSourceBundleDevModeTest = IwaSourceTestBase;
 
 TEST_F(IwaSourceBundleDevModeTest, Works) {
@@ -353,6 +372,27 @@ TEST_F(IwaSourceWithModeTest, FromStorageLocation) {
         profile_dir, IwaStorageProxy(kExampleOrigin));
     EXPECT_THAT(source.dev_mode(), IsTrue());
     EXPECT_THAT(source, Eq(IwaSourceProxy(kExampleOrigin)));
+  }
+}
+
+TEST_F(IwaSourceWithModeTest, WithFileOp) {
+  {
+    IwaSourceWithMode source{
+        IwaSourceBundleWithMode(kExamplePath, /*dev_mode=*/false)};
+    EXPECT_THAT(
+        source.WithFileOp(IwaSourceBundleProdFileOp::kCopy,
+                          IwaSourceBundleDevFileOp::kReference),
+        Eq(IwaSourceBundleWithModeAndFileOp{
+            kExamplePath, IwaSourceBundleModeAndFileOp::kProdModeCopy}));
+  }
+  {
+    IwaSourceWithMode source{
+        IwaSourceBundleWithMode(kExamplePath, /*dev_mode=*/true)};
+    EXPECT_THAT(
+        source.WithFileOp(IwaSourceBundleProdFileOp::kCopy,
+                          IwaSourceBundleDevFileOp::kReference),
+        Eq(IwaSourceBundleWithModeAndFileOp{
+            kExamplePath, IwaSourceBundleModeAndFileOp::kDevModeReference}));
   }
 }
 
