985eb2ff4c8ae78be3f8e79006d1425428693463
Eriko Kurimoto <elkurin@chromium.org>
2024-03-15 02:41:57 +0000

    Revert "Reorder precedence of Cookie Access Grants"
    This reverts commit 9808ffb946ced743655507f56986b0fe9cea5019.
    
    Reason for revert: Breaking ThirdPartyCookieDeprecationObserverMechanismBrowserTest.ThirdPartyJavaScriptCookieReadAndWrite on linux-chromeos-chrome
    
    Original change's description:
    > Reorder precedence of Cookie Access Grants
    >
    > This CL set precedence to allow first priority consideration of our long term solutions, to enhance visibility on their web ecosystem adoption.
    > Similarly, sites controlled mechanisms should be check before Chrome
    > ones, and user controlled mechanisms should come last as in an ideal
    > scenario users shouldn't need to trigger them.
    >
    > The predefined and defaulted controls will always come first.
    >
    > OBSOLETE_HISTOGRAMS=histogram PageLoad.Clients.TPCD.CookieAccess.ThirdPartyCookieAllowMechanism2 is replaced by PageLoad.Clients.TPCD.CookieAccess.ThirdPartyCookieAllowMechanism3
    >
    > OBSOLETE_HISTOGRAMS=histogram PageLoad.Clients.TPCD.TPCAccess.CookieReadStatus is replaced by PageLoad.Clients.TPCD.TPCAccess.CookieReadStatus2
    >
    > OBSOLETE_HISTOGRAMS=histogram API.StorageAccess.AllowedRequests2 is replaced by API.StorageAccess.AllowedRequests3
    >
    > OBSOLETE_HISTOGRAMS=histogram Tpcd.Mitigations.Dt.FirstParty.Deployment is replaced by Tpcd.Mitigations.Dt.FirstParty.Deployment2
    >
    >
    > Change-Id: Iebe37f089a1f25fc07d78446851ca5a6d2175165
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5331513
    > Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    > Reviewed-by: Jesse Doherty <jwd@chromium.org>
    > Reviewed-by: Ben Kelly <wanderview@chromium.org>
    > Commit-Queue: Jonathan Njeunje <njeunje@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1273030}
    
    Change-Id: Id3f605d33f7fb5e1636308b7d19e1e227ddaa400
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372764
    Owners-Override: Eriko Kurimoto <elkurin@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273200}

--- chrome/browser/dips/dips_bounce_detector_browsertest.cc
+++ chrome/browser/dips/dips_bounce_detector_browsertest.cc
@@ -1645,8 +1645,8 @@ class RedirectHeuristicBrowserTest : public PlatformBrowserTest {
   }
 };
 
-// Tests the conditions for recording RedirectHeuristic_CookieAccess2 and
-// RedirectHeuristic_CookieAccessThirdParty2 UKM events.
+// Tests the conditions for recording RedirectHeuristic_CookieAccess and
+// RedirectHeuristic_CookieAccessThirdParty UKM events.
 IN_PROC_BROWSER_TEST_F(RedirectHeuristicBrowserTest,
                        RecordsRedirectHeuristicCookieAccessEvent) {
   ukm::TestAutoSetUkmRecorder ukm_recorder;
@@ -1727,7 +1727,7 @@ IN_PROC_BROWSER_TEST_F(RedirectHeuristicBrowserTest,
 
   std::vector<ukm::TestUkmRecorder::HumanReadableUkmEntry>
       ukm_first_party_entries =
-          ukm_recorder.GetEntries("RedirectHeuristic.CookieAccess2", {});
+          ukm_recorder.GetEntries("RedirectHeuristic.CookieAccess", {});
 
   // Expect one UKM entry.
 
@@ -1746,7 +1746,7 @@ IN_PROC_BROWSER_TEST_F(RedirectHeuristicBrowserTest,
   // Expect one corresponding UKM entry for CookieAccessThirdParty.
   std::vector<ukm::TestUkmRecorder::HumanReadableUkmEntry>
       ukm_third_party_entries = ukm_recorder.GetEntries(
-          "RedirectHeuristic.CookieAccessThirdParty2", {});
+          "RedirectHeuristic.CookieAccessThirdParty", {});
   ASSERT_EQ(1u, ukm_third_party_entries.size());
   EXPECT_THAT(
       ukm_recorder.GetSourceForSourceId(ukm_third_party_entries[0].source_id)
@@ -1754,7 +1754,7 @@ IN_PROC_BROWSER_TEST_F(RedirectHeuristicBrowserTest,
       Eq(tracker_url_pre_target_redirect));
 }
 
-// Tests setting different metrics for the RedirectHeuristic_CookieAccess2 UKM
+// Tests setting different metrics for the RedirectHeuristic_CookieAccess UKM
 // event.
 // TODO(https://crbug.com/1489241): Flaky on multiple platforms.
 IN_PROC_BROWSER_TEST_F(RedirectHeuristicBrowserTest,
@@ -1849,7 +1849,7 @@ IN_PROC_BROWSER_TEST_F(RedirectHeuristicBrowserTest,
 
   std::vector<ukm::TestUkmRecorder::HumanReadableUkmEntry> ukm_entries =
       ukm_recorder.GetEntries(
-          "RedirectHeuristic.CookieAccess2",
+          "RedirectHeuristic.CookieAccess",
           {"AccessId", "AccessAllowed", "IsAdTagged",
            "HoursSinceLastInteraction", "MillisecondsSinceRedirect",
            "OpenerHasSameSiteIframe", "SitesPassedCount",
@@ -1906,7 +1906,7 @@ IN_PROC_BROWSER_TEST_F(RedirectHeuristicBrowserTest,
   // matching access IDs.
   std::vector<ukm::TestUkmRecorder::HumanReadableUkmEntry>
       ukm_third_party_entries = ukm_recorder.GetEntries(
-          "RedirectHeuristic.CookieAccessThirdParty2", {"AccessId"});
+          "RedirectHeuristic.CookieAccessThirdParty", {"AccessId"});
   ASSERT_EQ(2u, ukm_third_party_entries.size());
 
   EXPECT_THAT(
