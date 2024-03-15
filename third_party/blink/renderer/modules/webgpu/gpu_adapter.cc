1f6ce9d9a80b192538c412e29b6696b7b45c1492
François Beaufort <beaufort.francois@gmail.com>
2024-03-15 15:30:39 +0000

    WebGPU: Add Vulkan driver version to adapter info developer features
    Test: https://webgpureport.org/
    Change-Id: I3c23bad24a386fbe9dd259e0cdf30ad1d667158b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5374310
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1273429}

--- third_party/blink/renderer/modules/webgpu/gpu_adapter.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_adapter.cc
@@ -114,6 +114,14 @@ GPUAdapter::GPUAdapter(
     *propertiesChain = &d3dProperties.chain;
     propertiesChain = &(*propertiesChain)->next;
   }
+  WGPUAdapterPropertiesVk vkProperties = {};
+  vkProperties.chain.sType = WGPUSType_AdapterPropertiesVk;
+  bool supportsPropertiesVk = GetProcs().adapterHasFeature(
+      GetHandle(), WGPUFeatureName_AdapterPropertiesVk);
+  if (supportsPropertiesVk) {
+    *propertiesChain = &vkProperties.chain;
+    propertiesChain = &(*propertiesChain)->next;
+  }
   GetProcs().adapterGetProperties(GetHandle(), &properties);
   is_fallback_adapter_ = properties.adapterType == WGPUAdapterType_CPU;
   adapter_type_ = properties.adapterType;
@@ -135,6 +143,9 @@ GPUAdapter::GPUAdapter(
   }
   if (supportsPropertiesD3D) {
     d3d_shader_model_ = d3dProperties.shaderModel;
+  }
+  if (supportsPropertiesVk) {
+    vk_driver_version_ = vkProperties.driverVersion;
   }
 
   features_ = MakeFeatureNameSet(GetProcs(), GetHandle());
