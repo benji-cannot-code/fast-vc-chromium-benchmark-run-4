566953a974ed95d33c2342ff931fb9fb4b51877f
David Padlipsky <dpad@google.com>
2024-03-15 19:44:22 +0000

    Reland "split-refactor: Add top row layout for split modifier code refactor"
    This is a reland of commit 2d09eb35c872e8164e469d89c524ce0b620b6d53
    
    This can reland since we now set a default value for top_row_layout in
    KeyboardInfo struct which was causing an ASAN error in a unit test.
    
    Original change's description:
    > split-refactor: Add top row layout for split modifier code refactor
    >
    > Bug: b/329475925
    > Change-Id: I9d414839601bcfe5d0358eb8620f24f4ec716f3c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5369713
    > Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    > Reviewed-by: David Padlipsky <dpad@google.com>
    > Commit-Queue: David Padlipsky <dpad@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1272962}
    
    Bug: b/329475925
    Change-Id: Ie568cbd2cd23e10e2e2af38fa023b1936bb38311
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5375048
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1273603}

--- ash/accelerators/accelerator_alias_converter_unittest.cc
+++ ash/accelerators/accelerator_alias_converter_unittest.cc
@@ -170,6 +170,8 @@ class AcceleratorAliasConverterTest : public AshTestBase {
             std::end(ui::kLayoutWilcoDrallionTopRowActionKeys));
         break;
       case ui::KeyboardCapability::KeyboardTopRowLayout::kKbdTopRowLayoutCustom:
+      case ui::KeyboardCapability::KeyboardTopRowLayout::
+          kKbdTopRowLayoutSplitModifiers:
         break;
     }
     Shell::Get()->keyboard_capability()->SetKeyboardInfoForTesting(
