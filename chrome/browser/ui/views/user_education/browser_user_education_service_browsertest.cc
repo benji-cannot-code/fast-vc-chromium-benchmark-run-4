df3ed8669154346b9c8626ebcc8e3b9657d07b6c
Daniel Cheng <dcheng@chromium.org>
2024-03-15 15:07:58 +0000

    Remove FixedFlatSet use in BrowserUserEducationServiceBrowserTest.
    MakeFixedFlatSet is only for building compile time maps. As such, there
    are restrictions on when it can be used: in particular, the set itself
    must be constructed with a constant expression, which means any
    variables used in the initializer must also be constexpr variables or be
    constant-initialized variables. A forward-declared extern variable is
    neither of these.
    
    Rather than trying to make the initializer a constant expression, just
    use base::flat_map: since this is test-only code, there is no
    implication on production code anyway.
    
    Bug: 41486252
    Change-Id: Icca68cb000bf95223d5a8db0260e130ce36eaea8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372939
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273410}

--- chrome/browser/ui/views/user_education/browser_user_education_service_browsertest.cc
+++ chrome/browser/ui/views/user_education/browser_user_education_service_browsertest.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sstream>
 #include <vector>
 
-#include "base/containers/fixed_flat_set.h"
+#include "base/containers/flat_set.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/run_loop.h"
@@ -481,14 +481,19 @@ std::ostream& operator<<(std::ostream& os, const TutorialFailure& failure) {
 
 IN_PROC_BROWSER_TEST_F(BrowserUserEducationServiceBrowserTest,
                        TutorialConsistencyCheck) {
-  const auto kAlwaysPresentElementIds =
-      base::MakeFixedFlatSet<ui::ElementIdentifier>(
-          {kToolbarAppMenuButtonElementId, kToolbarAvatarButtonElementId,
-           kToolbarBackButtonElementId, kBrowserViewElementId,
-           kToolbarForwardButtonElementId, kNewTabButtonElementId,
-           kOmniboxElementId, kToolbarSidePanelButtonElementId,
-           kTabSearchButtonElementId, kTabStripElementId,
-           kTabStripRegionElementId, kTopContainerElementId});
+  const base::flat_set<ui::ElementIdentifier> kAlwaysPresentElementIds = {
+      kToolbarAppMenuButtonElementId,
+      kToolbarAvatarButtonElementId,
+      kToolbarBackButtonElementId,
+      kBrowserViewElementId,
+      kToolbarForwardButtonElementId,
+      kNewTabButtonElementId,
+      kOmniboxElementId,
+      kToolbarSidePanelButtonElementId,
+      kTabSearchButtonElementId,
+      kTabStripElementId,
+      kTabStripRegionElementId,
+      kTopContainerElementId};
 
   std::vector<TutorialFailure> failures;
 
