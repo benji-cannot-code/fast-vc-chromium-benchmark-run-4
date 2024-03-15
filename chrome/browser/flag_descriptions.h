1877608002f6ee7400e4282ad5a197762793c752
Vinny Persky <vinnypersky@google.com>
2024-03-15 19:40:17 +0000

    Clean up AutofillVirtualCardsOnTouchToFillAndroid finch flag
    Fixed: 40256402
    Change-Id: I40a5a73f1bbd8908b95bbce722abd89c08e68587
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372944
    Commit-Queue: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1273601}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -383,9 +383,6 @@ extern const char kAutofillUpdateChromeSettingsLinkToGPayWebDescription[];
 extern const char kAutofillMoreProminentPopupName[];
 extern const char kAutofillMoreProminentPopupDescription[];
 
-extern const char kAutofillVirtualCardsOnTouchToFillAndroidName[];
-extern const char kAutofillVirtualCardsOnTouchToFillAndroidDescription[];
-
 extern const char kAutofillVirtualViewStructureAndroidName[];
 extern const char kAutofillVirtualViewStructureAndroidDescription[];
 
