64b00351765e4a0a04f19256638ca6a3cc63c7bb
Danny Wang <wangdanny@google.com>
2024-03-15 21:59:39 +0000

    Revert "modifier-split: Add right alt to drop down for all keyboards"
    This reverts commit e5139d17ecfa0a5ef5fd8b601d6b6897dc8149bf.
    
    Reason for revert: should be gated with feature flag
    
    Original change's description:
    > modifier-split: Add right alt to drop down for all keyboards
    >
    > screenshot: http://screen/3BgrEAdGUwwTQDz
    >
    > Bug: b/328316040, b/329880993
    > Change-Id: I0014ce794b0fda517e78e183d1dbcb6f1b036788
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5373187
    > Commit-Queue: Danny Wang <wangdanny@google.com>
    > Reviewed-by: David Padlipsky <dpad@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1273627}
    
    Bug: b/328316040, b/329880993
    Change-Id: I641e7e672f219ee27df6f95a8d82b1efcba9dc19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5375059
    Reviewed-by: David Padlipsky <dpad@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Danny Wang <wangdanny@google.com>
    Cr-Commit-Position: refs/heads/main@{#1273681}

--- chrome/browser/resources/ash/settings/device_page/keyboard_remap_modifier_key_row.ts
+++ chrome/browser/resources/ash/settings/device_page/keyboard_remap_modifier_key_row.ts
@@ -207,10 +207,6 @@ export class KeyboardRemapModifierKeyRowElement extends
         name: this.i18n('perDeviceKeyboardKeyAssistant'),
       },
       {
-        value: ModifierKey.kRightAlt,
-        name: this.i18n('perDeviceKeyboardKeyRightAlt'),
-      },
-      {
         value: ModifierKey.kVoid,
         name: this.i18n('perDeviceKeyboardKeyDisabled'),
       },
