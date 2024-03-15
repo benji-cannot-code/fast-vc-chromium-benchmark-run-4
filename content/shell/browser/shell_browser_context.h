56cd79f129f80c07f81e11a1cdd7781f9bf97ab7
Christian Biesinger <cbiesinger@chromium.org>
2024-03-15 15:05:42 +0000

    [FedCM] Ensure that we reset FedCM state between tests
    The web_test runner already resets a lot of state between tests; add FedCM
    to the list.
    
    This fixes fecdm-login-status-unknown.html when not run on its own.
    
    Bug: 329477540
    Change-Id: I13d99ec5f95ecab1b5abd97adf2852cc151bd6f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372545
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273406}

--- content/shell/browser/shell_browser_context.h
+++ content/shell/browser/shell_browser_context.h
@@ -73,6 +73,7 @@ class ShellBrowserContext : public BrowserContext {
   OriginTrialsControllerDelegate* GetOriginTrialsControllerDelegate() override;
 
   ShellFederatedPermissionContext* GetShellFederatedPermissionContext();
+  void ResetFederatedPermissionContext();
 
  protected:
   bool ignore_certificate_errors() const { return ignore_certificate_errors_; }
