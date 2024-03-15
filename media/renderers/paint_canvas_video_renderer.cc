613b84b279a146fe184262e72f7c469f672f7d93
Colin Blundell <blundell@chromium.org>
2024-03-15 07:59:58 +0000

    [//media] Rename variables to match names of base::Features
    Followup to [1]. Has no functional impact, but eliminates chance that
    someone in the future incorrectly renames either of the base::Features
    back to the variable name.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/5349219
    
    Bug: 328540028
    Change-Id: Ib179cd81890ca8b8ccd75ad1805d29ab8cac80fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372043
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273272}

--- media/renderers/paint_canvas_video_renderer.cc
+++ media/renderers/paint_canvas_video_renderer.cc
@@ -87,7 +87,7 @@ namespace media {
 
 namespace {
 
-BASE_FEATURE(kAddSharedImageRasterUsageWithNonOOPR,
+BASE_FEATURE(kPCVRAddSharedImageRasterUsageWithNonOOPR,
              "PCVRAddSharedImageRasterUsageWithNonOOPR",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
@@ -1849,7 +1849,8 @@ bool PaintCanvasVideoRenderer::CopyVideoFrameYUVDataToGLTexture(
       // via raster, but historically this usage was included only for OOP-R.
       // Currently in the process of adding with a killswitch.
       // TODO(crbug.com/1524353): Remove this killswitch post-safe rollout.
-      if (base::FeatureList::IsEnabled(kAddSharedImageRasterUsageWithNonOOPR)) {
+      if (base::FeatureList::IsEnabled(
+              kPCVRAddSharedImageRasterUsageWithNonOOPR)) {
         usage = usage | gpu::SHARED_IMAGE_USAGE_RASTER_WRITE;
       }
     }
