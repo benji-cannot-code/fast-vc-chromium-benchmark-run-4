89ff067c97787e3c0b35710bed7eb2fe8667e601
Sergey Poromov <poromov@chromium.org>
2024-03-15 23:44:44 +0000

    skyvault: Cleanup user files when policy is set.
    Once the LocalUserFilesAllowed policy is set to false,
    we need to delete all existing user files in MyFiles/
    This can be done using existing chromeos::FilesCleanupHandler
    
    This class will need to be moved to some shared location later.
    
    Bug: b/327354327
    Test: Set the policy, check logs. New browsertest added.
    Change-Id: I960516c5249c8c1082212109820ad8848aca90d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5366209
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273724}

--- chrome/browser/ash/policy/core/user_cloud_policy_manager_ash.cc
+++ chrome/browser/ash/policy/core/user_cloud_policy_manager_ash.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/session/user_session_manager.h"
 #include "chrome/browser/ash/login/users/affiliation.h"
 #include "chrome/browser/ash/policy/core/policy_oauth2_token_fetcher.h"
+#include "chrome/browser/ash/policy/local_user_files/local_files_cleanup.h"
 #include "chrome/browser/ash/policy/login/wildcard_login_checker.h"
 #include "chrome/browser/ash/policy/remote_commands/user_commands_factory_ash.h"
 #include "chrome/browser/ash/policy/reporting/arc_app_install_event_log_uploader.h"
@@ -249,6 +250,8 @@ void UserCloudPolicyManagerAsh::ConnectManagementService(
 
   app_install_event_log_uploader_ =
       std::make_unique<ArcAppInstallEventLogUploader>(client(), profile_);
+  local_files_cleanup_ =
+      std::make_unique<local_user_files::LocalFilesCleanup>();
 }
 
 void UserCloudPolicyManagerAsh::OnAccessTokenAvailable(
@@ -322,6 +325,7 @@ UserCloudPolicyManagerAsh::GetAppInstallEventLogUploader() {
 
 void UserCloudPolicyManagerAsh::Shutdown() {
   observed_profile_.Reset();
+  local_files_cleanup_.reset();
   app_install_event_log_uploader_.reset();
   report_scheduler_.reset();
   if (client())
