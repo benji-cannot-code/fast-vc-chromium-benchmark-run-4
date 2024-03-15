0af757cdb2e2ad7ea736fb1882aa1e32071f86d9
Michael Ershov <miersh@google.com>
2024-03-15 10:00:12 +0000

    Kcer: Record UMA stats around PKCS#12 import
    Bug: b/264387231
    Change-Id: I8a0a307965417e0f0454202b8c882739f4de6070
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372265
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273314}

--- chrome/browser/certificate_manager_model.cc
+++ chrome/browser/certificate_manager_model.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chromeos/ash/components/network/policy_certificate_provider.h"
 #include "chromeos/components/kcer/kcer.h"
+#include "chromeos/components/kcer/kcer_histograms.h"
 #include "chromeos/constants/chromeos_features.h"
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
@@ -500,7 +501,15 @@ void RecordImportFromPKCS12KcerResult(
     base::OnceCallback<void(int nss_import_result)> callback,
     base::expected<void, kcer::Error> kcer_import_result) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  // TODO(b/264387231): Record UMA events about PKCS#12 import using Kcer.
+
+  if (kcer_import_result.has_value()) {
+    kcer::RecordPkcs12MigrationUmaEvent(
+        kcer::Pkcs12MigrationUmaEvent::kPkcs12ImportKcerSuccess);
+  } else {
+    kcer::RecordPkcs12MigrationUmaEvent(
+        kcer::Pkcs12MigrationUmaEvent::kPkcs12ImportKcerFailed);
+    kcer::RecordKcerError(kcer_import_result.error());
+  }
 
   // Just return the nss_import_result. Kcer will attempt to import only if NSS
   // succeeds and even if Kcer fails, the cert should be usable.
@@ -691,6 +700,14 @@ void CertificateManagerModel::ImportFromPKCS12(
                                                      is_extractable, nullptr);
 
 #if BUILDFLAG(IS_CHROMEOS)
+  if (nss_import_result == net::OK) {
+    kcer::RecordPkcs12MigrationUmaEvent(
+        kcer::Pkcs12MigrationUmaEvent::kPkcs12ImportNssSuccess);
+  } else {
+    kcer::RecordPkcs12MigrationUmaEvent(
+        kcer::Pkcs12MigrationUmaEvent::kPkcs12ImportNssFailed);
+  }
+
   // `is_extractable` == true indicates that the cert came from the "Import"
   // button. By default it's imported into the software NSS database (aka public
   // slot). With the experiment enabled it should also be imported into Chaps.
---
+++ chromeos/components/kcer/kcer_histograms.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/components/kcer/kcer_histograms.h"
+
+#include "base/metrics/histogram_functions.h"
+
+namespace kcer {
+namespace {
+constexpr char kPkcs12MigrationHistogram[] =
+    "ChromeOS.Kcer.Pkcs12MigrationUmaEvent";
+constexpr char kKcerErrorHistogram[] = "ChromeOS.Kcer.Error";
+}  // namespace
+
+void RecordPkcs12MigrationUmaEvent(Pkcs12MigrationUmaEvent event) {
+  base::UmaHistogramEnumeration(kPkcs12MigrationHistogram, event);
+}
+
+void RecordKcerError(Error error) {
+  base::UmaHistogramEnumeration(kKcerErrorHistogram, error);
+}
+
+}  // namespace kcer
