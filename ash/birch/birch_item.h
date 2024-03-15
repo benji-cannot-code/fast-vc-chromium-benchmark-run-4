7c49acca7a15fef91a6afbdbcf8aacca7f5e6eff
James Cook <jamescook@chromium.org>
2024-03-15 22:30:27 +0000

    birch: Localize BirchAttachmentItem subtitle and calendar join button
    Replace hard-coded strings with strings from ash_strings.grd. We don't
    have final strings from UX yet, but when we get them we'll need to be
    using ash_strings.grd, so this is a step in the right direction.
    
    Bug: b:329358094
    Test: visual, strings appear correctly in UI
    Change-Id: I053bf0e56ef6419aff5a7465572a61a91295c67a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5375288
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273697}

--- ash/birch/birch_item.h
+++ ash/birch/birch_item.h
@@ -104,6 +104,8 @@ class ASH_EXPORT BirchCalendarItem : public BirchItem {
   const GURL& conference_url() const { return conference_url_; }
 
  private:
+  static std::u16string GetSubtitle(base::Time start_time, base::Time end_time);
+
   base::Time start_time_;
   base::Time end_time_;
   // Link to the event in the Google Calendar UI.
@@ -143,6 +145,8 @@ class ASH_EXPORT BirchAttachmentItem : public BirchItem {
   const base::Time& end_time() const { return end_time_; }
 
  private:
+  static std::u16string GetSubtitle();
+
   GURL file_url_;          // Link to the file.
   GURL icon_url_;          // Link to the file's icon's art asset.
   base::Time start_time_;  // Start time of the event (used for ranking).
