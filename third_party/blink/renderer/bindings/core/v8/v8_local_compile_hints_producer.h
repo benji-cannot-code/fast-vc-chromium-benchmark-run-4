e344aea2f4531adfb053a0ecb5f859e7e26dac36
Marja Hölttä <marja@chromium.org>
2024-03-15 07:55:39 +0000

    [local compile hints] Generate local compile hints multiple times
    Previously, we generated local compile hints only when the page turned
    interactive. It's possible that the user navigates away before that, so
    in that case we didn't get any compile hints.
    
    This CL makes us compile hints earlier, and when the page turns
    interactive, we generate them again and replace the data.
    
    We also add a histogram for observing how many navigations now
    create compile hints data vs. how many would have created it in
    the original version.
    
    Bug: 40286622
    Change-Id: Iec1bd1ec26a5fe870c456d1977109912cc560482
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5344969
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Marja Hölttä <marja@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273270}

--- third_party/blink/renderer/bindings/core/v8/v8_local_compile_hints_producer.h
+++ third_party/blink/renderer/bindings/core/v8/v8_local_compile_hints_producer.h
@@ -36,7 +36,7 @@ class CORE_EXPORT V8LocalCompileHintsProducer
   void RecordScript(ExecutionContext* execution_context,
                     const v8::Local<v8::Script> script,
                     ClassicScript* classic_script);
-  void GenerateData();
+  void GenerateData(bool final_data);
 
   void Trace(Visitor* visitor) const;
 
--- third_party/blink/renderer/platform/scheduler/main_thread/frame_scheduler_impl.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/frame_scheduler_impl.cc
@@ -1001,6 +1001,9 @@ void FrameSchedulerImpl::OnFirstMeaningfulPaint(base::TimeTicks timestamp) {
   }
 
   main_thread_scheduler_->OnMainFramePaint();
+  if (delegate_) {
+    return delegate_->MainFrameFirstMeaningfulPaint();
+  }
 }
 
 void FrameSchedulerImpl::OnDispatchLoadEvent() {
