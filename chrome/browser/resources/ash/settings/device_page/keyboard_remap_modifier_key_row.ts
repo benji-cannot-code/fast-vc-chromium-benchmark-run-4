2da58646d90bbadbd3a26f776302f74538d1b5f2
Danny Wang <wangdanny@google.com>
2024-03-15 20:24:35 +0000

    modifier-split: Add right alt to drop down for all keyboards
    screenshot: http://screen/3BgrEAdGUwwTQDz
    
    Bug: b/328316040, b/329880993
    Change-Id: I0014ce794b0fda517e78e183d1dbcb6f1b036788
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5373187
    Commit-Queue: Danny Wang <wangdanny@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1273627}

--- chrome/browser/resources/ash/settings/device_page/keyboard_remap_modifier_key_row.ts
+++ chrome/browser/resources/ash/settings/device_page/keyboard_remap_modifier_key_row.ts
@@ -207,6 +207,10 @@ export class KeyboardRemapModifierKeyRowElement extends
         name: this.i18n('perDeviceKeyboardKeyAssistant'),
       },
       {
+        value: ModifierKey.kRightAlt,
+        name: this.i18n('perDeviceKeyboardKeyRightAlt'),
+      },
+      {
         value: ModifierKey.kVoid,
         name: this.i18n('perDeviceKeyboardKeyDisabled'),
       },
