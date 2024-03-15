2f782aa78b80cd329d9d0819b668e98e5fdae66b
Gayane Petrosyan <gayane@google.com>
2024-03-15 02:57:20 +0000

    [iOS] Add RTL and 3rd button support to DB promo view provider
    Add RTL and 3rd button (remind me later) support to default browser
    promo view provider. Additionally use that view provider for displaying
    default browser video promo.
    
    Bug: b/298046221
    Change-Id: Ia1097d5642cc6494f59e0b7332c54ed994bbe993
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5370511
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Reviewed-by: Cheick Cisse <cheickcisse@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1273204}

--- ios/chrome/browser/ui/default_promo/default_browser_instructions_view.mm
+++ ios/chrome/browser/ui/default_promo/default_browser_instructions_view.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/default_promo/default_browser_instructions_view.h"
 
+#import "base/i18n/rtl.h"
 #import "ios/chrome/browser/shared/ui/elements/instruction_view.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.h"
@@ -18,8 +19,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 // Video animation asset names.
-NSString* const kDarkModeAnimationSuffix = @"_darkmode";
 NSString* const kDefaultBrowserAnimation = @"default_browser_animation";
+NSString* const kDefaultBrowserAnimationRtl = @"default_browser_animation_rtl";
+NSString* const kDefaultBrowserAnimationDarkmode =
+    @"default_browser_animation_darkmode";
+NSString* const kDefaultBrowserAnimationRtlDarkmode =
+    @"default_browser_animation_rtl_darkmode";
 
 // Keys in the lottie assets.
 NSString* const kDefaultBrowserAppKeypath = @"IDS_DEFAULT_BROWSER_APP";
@@ -51,13 +56,16 @@ NSString* const kDefaultBrowserInstructionsViewDarkAnimationViewId =
 
 @implementation DefaultBrowserInstructionsView
 
-- (instancetype)init:(BOOL)hasDissmissButton
-            hasSteps:(BOOL)hasSteps
-       actionHandler:(id<ConfirmationAlertActionHandler>)actionHandler {
+- (instancetype)initWithDismissButton:(BOOL)hasDissmissButton
+                     hasRemindMeLater:(BOOL)hasRemindMeLater
+                             hasSteps:(BOOL)hasSteps
+                        actionHandler:
+                            (id<ConfirmationAlertActionHandler>)actionHandler {
   self = [super init];
   if (self) {
     [self addVideoSection];
     [self addInformationSection:hasDissmissButton
+               hasRemindMeLater:hasRemindMeLater
                        hasSteps:hasSteps
                   actionHandler:actionHandler];
     [self setBackgroundColor:[UIColor colorNamed:kGrey100Color]];
@@ -75,12 +83,24 @@ NSString* const kDefaultBrowserInstructionsViewDarkAnimationViewId =
 
 // Adds the top part of the view which contains the video animation.
 - (void)addVideoSection {
-  self.animationViewWrapper = [self createAnimation:kDefaultBrowserAnimation];
+  NSString* animationAssetName;
+  NSString* animationAssetNameDarkMode;
+
+  // TODO(crbug.com/1508131): Handle the case when the promo is displayed and
+  // the user switches between LTR and RLT.
+  if (base::i18n::IsRTL()) {
+    animationAssetName = kDefaultBrowserAnimationRtl;
+    animationAssetNameDarkMode = kDefaultBrowserAnimationRtlDarkmode;
+  } else {
+    animationAssetName = kDefaultBrowserAnimation;
+    animationAssetNameDarkMode = kDefaultBrowserAnimationDarkmode;
+  }
+
+  self.animationViewWrapper = [self createAnimation:animationAssetName];
   self.animationViewWrapper.animationView.accessibilityIdentifier =
       kDefaultBrowserInstructionsViewAnimationViewId;
-  self.animationViewWrapperDarkMode = [self
-      createAnimation:[kDefaultBrowserAnimation
-                          stringByAppendingString:kDarkModeAnimationSuffix]];
+  self.animationViewWrapperDarkMode =
+      [self createAnimation:animationAssetNameDarkMode];
   self.animationViewWrapperDarkMode.animationView.accessibilityIdentifier =
       kDefaultBrowserInstructionsViewDarkAnimationViewId;
 
@@ -157,6 +177,7 @@ NSString* const kDefaultBrowserInstructionsViewDarkAnimationViewId =
 
 // Adds the bottom section of the view which contains instructions and buttons.
 - (void)addInformationSection:(BOOL)hasDissmissButton
+             hasRemindMeLater:(BOOL)hasRemindMeLater
                      hasSteps:(BOOL)hasSteps
                 actionHandler:
                     (id<ConfirmationAlertActionHandler>)actionHandler {
@@ -206,6 +227,11 @@ NSString* const kDefaultBrowserInstructionsViewDarkAnimationViewId =
         IDS_IOS_DEFAULT_BROWSER_VIDEO_PROMO_SECONDARY_BUTTON_TEXT);
   }
 
+  if (hasRemindMeLater) {
+    alertScreen.tertiaryActionString = l10n_util::GetNSString(
+        IDS_IOS_DEFAULT_BROWSER_VIDEO_PROMO_TERTIARY_BUTTON_TEXT);
+  }
+
   [self addSubview:alertScreen.view];
 
   // Layout the alert view to take up bottom half of the view.
@@ -220,7 +246,7 @@ NSString* const kDefaultBrowserInstructionsViewDarkAnimationViewId =
   self.alertScreen = alertScreen;
 }
 
-// Returns the center offset for video instructions and information sectionto
+// Returns the center offset for video instructions and information section to
 // align with.
 - (CGFloat)centerOffset {
   if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_TABLET) {
