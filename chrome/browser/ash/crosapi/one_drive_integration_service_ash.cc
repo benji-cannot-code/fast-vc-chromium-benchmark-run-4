06fabf52e9fc847b0b9e0c9f05ae872e643dcc9f
Aya ElAttar <ayaelattar@chromium.org>
2024-03-15 06:19:58 +0000

    Notify FSP Service observers on shutdown
    - file_system_provider::Service is a keyed service while
    OneDriveIntegrationServiceAsh isn't.
    On OneDriveIntegrationServiceAsh destruction, the observer
    tries to remove itself from the FSP service which
    is no longer there, so it crashes.
    - Changed file_system_provider::Service to notify its
    observer when the shutting down starts.
    
    Bug: b:329305132
    Change-Id: Ifeee5be1e7591c5c1c32d8a7e2711c1c4d85d6b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5366056
    Auto-Submit: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273247}

--- chrome/browser/ash/crosapi/one_drive_integration_service_ash.cc
+++ chrome/browser/ash/crosapi/one_drive_integration_service_ash.cc
@@ -88,4 +88,11 @@ void OneDriveIntegrationServiceAsh::OnProvidedFileSystemUnmount(
   }
 }
 
+void OneDriveIntegrationServiceAsh::OnShutDown() {
+  if (!file_system_provider_observation_.IsObserving()) {
+    return;
+  }
+  file_system_provider_observation_.Reset();
+}
+
 }  // namespace crosapi
