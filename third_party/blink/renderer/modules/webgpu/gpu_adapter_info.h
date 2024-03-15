688c49b044a8206c6d3bb40c3c4b51a94bd867a1
François Beaufort <beaufort.francois@gmail.com>
2024-03-15 15:07:18 +0000

    WebGPU: Add D3D shader model to adapter info developer features
    Test: https://webgpureport.org/
    
    Change-Id: I2ed2a81a0e0036a9c8e9fc1cd274dd0c4c9b73b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5366599
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1273409}

--- third_party/blink/renderer/modules/webgpu/gpu_adapter_info.h
+++ third_party/blink/renderer/modules/webgpu/gpu_adapter_info.h
@@ -24,7 +24,8 @@ class GPUAdapterInfo : public ScriptWrappable {
                  const String& description = String(),
                  const String& driver = String(),
                  const String& backend = String(),
-                 const String& type = String());
+                 const String& type = String(),
+                 const std::optional<uint32_t> d3d_shader_model = std::nullopt);
 
   GPUAdapterInfo(const GPUAdapterInfo&) = delete;
   GPUAdapterInfo& operator=(const GPUAdapterInfo&) = delete;
@@ -40,6 +41,7 @@ class GPUAdapterInfo : public ScriptWrappable {
   const String& backend() const;
   const String& type() const;
   const HeapVector<Member<GPUMemoryHeapInfo>>& memoryHeaps() const;
+  const std::optional<uint32_t>& d3dShaderModel() const;
 
   void Trace(Visitor*) const override;
 
@@ -52,6 +54,7 @@ class GPUAdapterInfo : public ScriptWrappable {
   String backend_;
   String type_;
   HeapVector<Member<GPUMemoryHeapInfo>> memory_heaps_;
+  std::optional<uint32_t> d3d_shader_model_;
 };
 
 }  // namespace blink
