bc0039031bd0b41b8f5aa525403d4dd0314368c4
Tomasz Wiszkowski <ender@google.com>
2024-03-15 20:17:50 +0000

    Ensure that PendingIntent actions are truly unique across clients
    The change explicitly links SearchWidget actions to its surface and
    request types for proper deduplication at the Launcher level.
    
    Interesting fact: PendingIntents constructed from Intents are
    deduplicated across all origins.
    
    This means, that if a SearchWidget and QuickActionSearchWidget share
    the same "intent" ("text search") but not origin, the last created
    intent will overwrite definitions for the intent created before.
    
    IOW: if the SearchWidget is the last client to create a Text search
    intent, QuickAction will adopt all its extras. If QuickAction is the
    last client to create that intent - it will override whatever
    SearchWidget asked.
    
    Change-Id: Ia289c508ca2c9b0acecc4c9fb885781060e824cf
    Bug: 327023983
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372416
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273620}

--- chrome/android/junit/src/org/chromium/chrome/browser/searchwidget/SearchActivityUtilsUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/searchwidget/SearchActivityUtilsUnitTest.java
@@ -48,9 +48,13 @@ public class SearchActivityUtilsUnitTest {
 
         SearchActivityClient client = new SearchActivityUtils();
         for (int origin : origins) {
+            String action =
+                    String.format(
+                            SearchActivityUtils.ACTION_SEARCH_FORMAT, origin, SearchType.TEXT);
+
             // null URL
             var intent = client.createIntent(mActivity, origin, null, SearchType.TEXT);
-            assertEquals(SearchActivityUtils.ACTION_TEXT_SEARCH, intent.getAction());
+            assertEquals(action, intent.getAction());
             assertNull(intent.getStringExtra(SearchActivityUtils.EXTRA_CURRENT_URL));
             assertEquals(SearchType.TEXT, SearchActivityUtils.getIntentSearchType(intent));
             assertEquals(origin, SearchActivityUtils.getIntentOrigin(intent));
@@ -59,7 +63,7 @@ public class SearchActivityUtilsUnitTest {
             intent =
                     client.createIntent(
                             mActivity, origin, new GURL("http://abc.xyz"), SearchType.TEXT);
-            assertEquals(SearchActivityUtils.ACTION_TEXT_SEARCH, intent.getAction());
+            assertEquals(action, intent.getAction());
             assertEquals(
                     "http://abc.xyz/",
                     intent.getStringExtra(SearchActivityUtils.EXTRA_CURRENT_URL));
@@ -80,9 +84,13 @@ public class SearchActivityUtilsUnitTest {
 
         SearchActivityClient client = new SearchActivityUtils();
         for (int origin : origins) {
+            String action =
+                    String.format(
+                            SearchActivityUtils.ACTION_SEARCH_FORMAT, origin, SearchType.VOICE);
+
             // null URL
             var intent = client.createIntent(mActivity, origin, null, SearchType.VOICE);
-            assertEquals(SearchActivityUtils.ACTION_VOICE_SEARCH, intent.getAction());
+            assertEquals(action, intent.getAction());
             assertNull(intent.getStringExtra(SearchActivityUtils.EXTRA_CURRENT_URL));
             assertEquals(SearchType.VOICE, SearchActivityUtils.getIntentSearchType(intent));
             assertEquals(origin, SearchActivityUtils.getIntentOrigin(intent));
@@ -91,7 +99,7 @@ public class SearchActivityUtilsUnitTest {
             intent =
                     client.createIntent(
                             mActivity, origin, new GURL("http://abc.xyz"), SearchType.VOICE);
-            assertEquals(SearchActivityUtils.ACTION_VOICE_SEARCH, intent.getAction());
+            assertEquals(action, intent.getAction());
             assertEquals(
                     "http://abc.xyz/",
                     intent.getStringExtra(SearchActivityUtils.EXTRA_CURRENT_URL));
@@ -112,9 +120,13 @@ public class SearchActivityUtilsUnitTest {
 
         SearchActivityClient client = new SearchActivityUtils();
         for (int origin : origins) {
+            String action =
+                    String.format(
+                            SearchActivityUtils.ACTION_SEARCH_FORMAT, origin, SearchType.LENS);
+
             // null URL
             var intent = client.createIntent(mActivity, origin, null, SearchType.LENS);
-            assertEquals(SearchActivityUtils.ACTION_LENS_SEARCH, intent.getAction());
+            assertEquals(action, intent.getAction());
             assertNull(intent.getStringExtra(SearchActivityUtils.EXTRA_CURRENT_URL));
             assertEquals(SearchType.LENS, SearchActivityUtils.getIntentSearchType(intent));
             assertEquals(origin, SearchActivityUtils.getIntentOrigin(intent));
@@ -123,7 +135,7 @@ public class SearchActivityUtilsUnitTest {
             intent =
                     client.createIntent(
                             mActivity, origin, new GURL("http://abc.xyz"), SearchType.LENS);
-            assertEquals(SearchActivityUtils.ACTION_LENS_SEARCH, intent.getAction());
+            assertEquals(action, intent.getAction());
             assertEquals(
                     "http://abc.xyz/",
                     intent.getStringExtra(SearchActivityUtils.EXTRA_CURRENT_URL));
