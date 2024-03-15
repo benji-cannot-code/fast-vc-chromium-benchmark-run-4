7332a4a5c654724e4d87d2dbe4ee1aad81c46bd6
James Zern <jzern@chromium.org>
2024-03-15 19:26:37 +0000

    Roll src/third_party/libvpx/source/libvpx/ f51417671..cf1b7a65f (10 commits)
    https://chromium.googlesource.com/webm/libvpx.git/+log/f51417671e06..cf1b7a65ffc0
    
    $ git log f51417671..cf1b7a65f --date=short --no-merges --format='%ad %ae %s'
    2024-03-08 jzern VP9RateControlRtcConfig: relocate some initializations
    2024-03-08 jzern ratectrl_rtc.h: remove use of vp9_zero()
    2024-03-08 jzern ratectrl_rtc.h: move some includes to .cc
    2024-03-08 jzern *ratectrl_rtc.h: remove unneeded 'public:'
    2024-03-08 jzern vp8_ratectrl_rtc.cc: fix include order
    2024-03-09 gerdazsejke.more Add SVE2 implementation of vpx_highbd_convolve8_avg_vert
    2024-03-11 gerdazsejke.more Add SVE implementation of vpx_highbd_convolve8_avg_horiz
    2024-03-07 gerdazsejke.more Add SVE2 implementation of vpx_highbd_convolve8_vert
    2024-03-07 gerdazsejke.more Add Arm SVE2 build flags and run-time CPU feature detection
    2024-03-08 wtc VP8: Always reset the setjmp flag before returning
    
    Created with:
      roll-dep src/third_party/libvpx/source/libvpx
    
    Bug: 308446709
    Change-Id: I69518d316f2a35f8a4d0f740cee038d6829f0cd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5371174
    Commit-Queue: James Zern <jzern@google.com>
    Reviewed-by: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1273591}

--- third_party/libvpx/source/config/win/x64/vpx_config.asm
+++ third_party/libvpx/source/config/win/x64/vpx_config.asm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 %define HAVE_NEON_DOTPROD 0
 %define HAVE_NEON_I8MM 0
 %define HAVE_SVE 0
+%define HAVE_SVE2 0
 %define HAVE_MIPS32 0
 %define HAVE_DSPR2 0
 %define HAVE_MSA 0
