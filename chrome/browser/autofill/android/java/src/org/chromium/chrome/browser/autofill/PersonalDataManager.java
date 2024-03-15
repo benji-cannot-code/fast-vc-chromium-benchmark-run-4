fb4ad307bcc7349dbec3511760dfb427409e2cdb
Ted Choc <tedchoc@chromium.org>
2024-03-15 16:18:42 +0000

    Convert GetBasicCardIssuerNetwork to a static method.
    This avoids requiring a PersonalDataManager object around in
    Java to access a C++ method that has no dependencies on the
    underlying PersonalDataManager C++ object.
    
    Bug: 40689806
    Change-Id: Ib2687264b23efaec58fed2fdc9bbc2ac8e8392a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5371965
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273453}

--- chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/PersonalDataManager.java
+++ chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/PersonalDataManager.java
@@ -806,12 +806,10 @@ public class PersonalDataManager implements Destroyable {
                         mPersonalDataManagerAndroid, PersonalDataManager.this, card);
     }
 
-    public String getBasicCardIssuerNetwork(String cardNumber, boolean emptyIfInvalid) {
+    public static String getBasicCardIssuerNetwork(String cardNumber, boolean emptyIfInvalid) {
         ThreadUtils.assertOnUiThread();
         return PersonalDataManagerJni.get()
                 .getBasicCardIssuerNetwork(
-                        mPersonalDataManagerAndroid,
-                        PersonalDataManager.this,
                         cardNumber,
                         emptyIfInvalid);
     }
@@ -1267,8 +1265,6 @@ public class PersonalDataManager implements Destroyable {
                 long nativePersonalDataManagerAndroid, PersonalDataManager caller, CreditCard card);
 
         String getBasicCardIssuerNetwork(
-                long nativePersonalDataManagerAndroid,
-                PersonalDataManager caller,
                 String cardNumber,
                 boolean emptyIfInvalid);
 
