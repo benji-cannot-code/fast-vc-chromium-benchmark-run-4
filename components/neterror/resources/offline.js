2dee32f4325dcaf75c7fb49f5280bea19d7a4983
Julian Pastarmov <pastarmovj@google.com>
2024-03-15 17:10:22 +0000

    Make adjustments for supporting alt versions
    Bug: 329674305
    Binary-Size: Increase is temporary.
    Change-Id: Iafa4ea91abebf459f629eab5f01353698a8b3dff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372056
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Edward Jung <edwardjung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273494}

--- components/neterror/resources/offline.js
+++ components/neterror/resources/offline.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 
 import {HIDDEN_CLASS} from './constants.js';
-import {CollisionBox, spriteDefinitionByType} from './offline-sprite-definitions.js';
+import {CollisionBox, GAME_TYPE, spriteDefinitionByType} from './offline-sprite-definitions.js';
 
 /**
  * T-Rex runner.
