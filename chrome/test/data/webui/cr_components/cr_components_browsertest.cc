5d7d543fd8663ce8ebf51a5de2bc761416eaf3c8
John Lee <johntlee@chromium.org>
2024-03-15 17:57:19 +0000

    Add suggestion chips to history embeddings
    http://screencast/cast/NTk2MTQ2OTk1ODAyOTMxMnxlM2M2MWFiYy1mYQ
    
    Currently, clicking on chips does not do anything. You can only select
    one chip at a time.
    
    Bug: 328648133
    Change-Id: I6a604e50210d16a17d9c0656b6e08a9e30553ab8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5370472
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273530}

--- chrome/test/data/webui/cr_components/cr_components_browsertest.cc
+++ chrome/test/data/webui/cr_components/cr_components_browsertest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/test/base/web_ui_mocha_browser_test.h"
 #include "components/history_clusters/core/features.h"
+#include "components/history_embeddings/history_embeddings_features.h"
 #include "content/public/test/browser_test.h"
 #include "crypto/crypto_buildflags.h"
 
@@ -57,12 +58,25 @@ IN_PROC_BROWSER_TEST_F(CrComponentsTest, HorizontalCarousel) {
           "mocha.run()");
 }
 
-IN_PROC_BROWSER_TEST_F(CrComponentsTest, HistoryEmbeddings) {
+class CrComponentsHistoryEmbeddingsTest : public WebUIMochaBrowserTest {
+ protected:
+  CrComponentsHistoryEmbeddingsTest() {
+    scoped_feature_list_.InitAndEnableFeature(
+        history_embeddings::kHistoryEmbeddings);
+    set_test_loader_host(chrome::kChromeUIHistoryHost);
+  }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+};
+
+IN_PROC_BROWSER_TEST_F(CrComponentsHistoryEmbeddingsTest, HistoryEmbeddings) {
   RunTest("cr_components/history_embeddings/history_embeddings_test.js",
           "mocha.run()");
 }
 
-IN_PROC_BROWSER_TEST_F(CrComponentsTest, HistoryEmbeddingsFilterChips) {
+IN_PROC_BROWSER_TEST_F(CrComponentsHistoryEmbeddingsTest,
+                       HistoryEmbeddingsFilterChips) {
   RunTest("cr_components/history_embeddings/filter_chips_test.js",
           "mocha.run()");
 }
