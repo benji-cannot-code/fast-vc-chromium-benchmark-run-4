1c57b2ddc14659351ac88c2d16c465c667a3f98c
Benoit Lize <lizeb@chromium.org>
2024-03-15 16:26:26 +0000

    [canvas] Add a feature to release recycled canvas resource
    Due to various pipelined stages in the rendering stack (renderer-side,
    GPU-side, and OS-side), when a 2D canvas is being painted, there are
    multiple buffers in flight (locally, 7 buffers have been observed on a
    platform with delegated compositing).
    
    When a pipeline length changes, or when the canvas becomes static, these
    buffers are eventually returned to the CanvasResourceProvider, where up
    to 4 are kept around for recycling. This is similar to other such
    recycling behaviors in Chromium, e.g. with cc::ResourcePool for
    tiles. However, contrary to ResourcePool, there is no expiration of
    unused resources in CanvasResourceProvider. Locally, on a 14 inch
    macBook Pro, this has been seen to cost >100MiB for a single foreground
    Google Docs tab.
    
    This CL adds expiration and eviction of these resources, behind a
    feature flag. Since reclaiming only happens after 5s of inactivity
    (similar to ResourcePool), this should not impact performance-critical
    workloads, while saving memory. That being said, precise cost and
    benefits will be assessed with a field trial. For more details, see the
    linked bug.
    
    Bug: 326235714
    Change-Id: I3962277f0c7572b3eaa487485e2f4b9a5c0b41a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5323842
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273457}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -6,12 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_CANVAS_RESOURCE_PROVIDER_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_CANVAS_RESOURCE_PROVIDER_H_
 
+#include <algorithm>
 #include <memory>
 #include <optional>
 
 #include "base/feature_list.h"
 #include "base/memory/raw_ptr.h"
 #include "base/notreached.h"
+#include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "cc/paint/skia_paint_canvas.h"
 #include "cc/raster/playback_image_provider.h"
 #include "gpu/command_buffer/common/shared_image_usage.h"
@@ -51,6 +54,7 @@ class RasterInterface;
 namespace blink {
 
 PLATFORM_EXPORT BASE_DECLARE_FEATURE(kCanvas2DAutoFlushParams);
+PLATFORM_EXPORT BASE_DECLARE_FEATURE(kCanvas2DReclaimUnusedResources);
 
 class CanvasResourceDispatcher;
 class MemoryManagedPaintCanvas;
@@ -278,6 +282,24 @@ class PLATFORM_EXPORT CanvasResourceProvider
     return last_recording_;
   }
 
+  struct UnusedResource {
+    UnusedResource(base::TimeTicks last_use,
+                   scoped_refptr<CanvasResource> resource)
+        : last_use(last_use), resource(std::move(resource)) {}
+    base::TimeTicks last_use;
+    scoped_refptr<CanvasResource> resource;
+  };
+
+  // Visible for testing; should be protected.
+  const WTF::Vector<UnusedResource>& CanvasResources() const {
+    return canvas_resources_;
+  }
+  bool unused_resources_reclaim_timer_is_running_for_testing() const {
+    return unused_resources_reclaim_timer_.IsRunning();
+  }
+  constexpr static base::TimeDelta kUnusedResourceExpirationTime =
+      base::Seconds(5);
+
  protected:
   class CanvasImageProvider;
 
@@ -335,10 +357,6 @@ class PLATFORM_EXPORT CanvasResourceProvider
   virtual void OnFlushForImage(cc::PaintImage::ContentId content_id);
   void OnMemoryDump(base::trace_event::ProcessMemoryDump*) override;
 
-  const WTF::Vector<scoped_refptr<CanvasResource>>& canvas_resources() const {
-    return canvas_resources_;
-  }
-
  private:
   friend class FlushForImageListener;
   virtual sk_sp<SkSurface> CreateSkSurface() const = 0;
@@ -363,6 +381,10 @@ class PLATFORM_EXPORT CanvasResourceProvider
   // Called after the recording was cleared from any draw ops it might have had.
   void RecordingCleared() override;
 
+  void RegisterUnusedResource(scoped_refptr<CanvasResource>&& resource);
+  void MaybePostUnusedResourcesReclaimTask();
+  void ClearOldUnusedResources();
+
   base::WeakPtr<WebGraphicsContext3DProviderWrapper> context_provider_wrapper_;
   base::WeakPtr<CanvasResourceDispatcher> resource_dispatcher_;
   // Note that `info_` should be const, but the relevant SkImageInfo
@@ -384,7 +406,8 @@ class PLATFORM_EXPORT CanvasResourceProvider
 
   // When and if |resource_recycling_enabled_| is false, |canvas_resources_|
   // will only hold one CanvasResource at most.
-  WTF::Vector<scoped_refptr<CanvasResource>> canvas_resources_;
+  WTF::Vector<UnusedResource> canvas_resources_;
+  base::OneShotTimer unused_resources_reclaim_timer_;
   bool resource_recycling_enabled_ = true;
   bool is_single_buffered_ = false;
   bool oopr_uses_dmsaa_ = false;
