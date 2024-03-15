893de092c10cbb6690393e53508d4c63cd4daa4e
Yuta Hijikata <ythjkt@google.com>
2024-03-15 09:59:06 +0000

    Fix minimized windows never getting restored with desk restore.
    Detailed explanation on why this is needed =>
    https://b.corp.google.com/issues/314101620#comment9
    
    Bug: b/314101620
    Change-Id: Ic1ac687825ab35a3283c5c8eb5dec09a7b154258
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5369730
    Commit-Queue: Yuta Hijikata <ythjkt@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273312}

---
+++ ui/views/test/widget_show_state_waiter.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_VIEWS_TEST_WIDGET_SHOW_STATE_WAITER_H_
+#define UI_VIEWS_TEST_WIDGET_SHOW_STATE_WAITER_H_
+
+#include "ui/base/ui_base_types.h"
+
+namespace views {
+
+class Widget;
+
+namespace test {
+
+void WaitForWidgetShowState(Widget* widget, ui::WindowShowState show_state);
+
+}  // namespace test
+
+}  // namespace views
+
+#endif  // UI_VIEWS_TEST_WIDGET_SHOW_STATE_WAITER_H_
