37eede10b6d2ce651ce903760dee1ce5038af4f4
Boris Sazonov <bsazonov@chromium.org>
2024-03-15 21:54:10 +0000

    [Signin][Android] Check gaia id when looking for the primary account
    Prior to this CL, SigninManager was comparing complete CoreAccountInfo
    to detect if the primary account was removed from the device. After
    this CL, it only compares the gaia id to correctly handle the flow in
    which the primary account changes the email.
    
    Bug: 328801001, 40284908
    Change-Id: I76749697f8869781ee21c7ca84a77573ecba3783
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5365921
    Reviewed-by: Samar Chehade-Lepleux <samarchehade@google.com>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273677}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManagerImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManagerImpl.java
@@ -39,6 +39,7 @@ import org.chromium.chrome.browser.signin.services.SigninPreferencesManager;
 import org.chromium.components.externalauth.ExternalAuthUtils;
 import org.chromium.components.signin.AccountManagerFacade;
 import org.chromium.components.signin.AccountManagerFacadeProvider;
+import org.chromium.components.signin.AccountUtils;
 import org.chromium.components.signin.AccountsChangeObserver;
 import org.chromium.components.signin.SigninFeatureMap;
 import org.chromium.components.signin.SigninFeatures;
@@ -206,7 +207,10 @@ class SigninManagerImpl implements IdentityManager.Observer, SigninManager, Acco
         @Nullable
         CoreAccountInfo primaryAccountInfo =
                 mIdentityManager.getPrimaryAccountInfo(ConsentLevel.SIGNIN);
-        if (primaryAccountInfo == null || coreAccountInfos.contains(primaryAccountInfo)) {
+        if (primaryAccountInfo == null
+                || AccountUtils.findCoreAccountInfoByGaiaId(
+                                coreAccountInfos, primaryAccountInfo.getGaiaId())
+                        != null) {
             // Reload the coreAccountInfos if the primary account is still on the device or if the
             // user is signed out.
             seedThenReloadAllAccountsFromSystem(CoreAccountInfo.getIdFrom(primaryAccountInfo));
