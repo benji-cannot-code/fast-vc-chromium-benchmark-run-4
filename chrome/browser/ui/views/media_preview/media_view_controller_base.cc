06a230ca95dbd079047ad9b72bf69a8aac34674b
ahmedmoussa <ahmedmoussa@google.com>
2024-03-15 21:44:49 +0000

    [live-preview] Announce device name upon device automatic updates
    Bug: b:328495231
    Change-Id: I9e1f2ccf82d2e1be34b0c0ac8985b463494b0b14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5281151
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Ahmed Moussa <ahmedmoussa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1273666}

--- chrome/browser/ui/views/media_preview/media_view_controller_base.cc
+++ chrome/browser/ui/views/media_preview/media_view_controller_base.cc
@@ -6,13 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/media_preview/media_view_controller_base.h"
 
 #include <memory>
+#include <optional>
 #include <string>
 
 #include "base/check.h"
 #include "base/functional/bind.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/media_preview/media_view.h"
+#include "chrome/grit/generated_resources.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/ui_base_features.h"
+#include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/background.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/combobox/combobox.h"
@@ -93,7 +97,8 @@ MediaViewControllerBase::~MediaViewControllerBase() {
 }
 
 void MediaViewControllerBase::OnDeviceListChanged(size_t device_count) {
-  bool has_devices = device_count > 0;
+  const std::u16string previous_device_name = device_name_label_->GetText();
+  const bool has_devices = device_count > 0;
   if (!has_devices) {
     live_feed_container_->SetVisible(false);
     no_devices_found_label_->SetVisible(true);
@@ -103,6 +108,8 @@ void MediaViewControllerBase::OnDeviceListChanged(size_t device_count) {
     device_name_label_->SetVisible(true);
     device_selector_combobox_->SetVisible(false);
     base_view_->RefreshSize();
+    AnnounceDynamicChangeIfNeeded(previous_device_name,
+                                  no_devices_found_label_->GetText());
     return;
   }
 
@@ -113,6 +120,13 @@ void MediaViewControllerBase::OnDeviceListChanged(size_t device_count) {
   device_selector_combobox_->SetVisible(allow_device_selection_);
   OnComboboxSelection();
   base_view_->RefreshSize();
+  if (allow_device_selection_) {
+    AnnounceDynamicChangeIfNeeded(
+        previous_device_name,
+        l10n_util::GetStringFUTF16(
+            IDS_MEDIA_PREVIEW_ANNOUNCE_SELECTED_DEVICE_CHANGE,
+            device_name_label_->GetText()));
+  }
 }
 
 void MediaViewControllerBase::OnComboboxSelection() {
@@ -125,4 +139,20 @@ void MediaViewControllerBase::UpdateDeviceNameLabel() {
   device_name_label_->SetText(
       device_selector_combobox_->GetModel()->GetItemAt(index.value()));
   device_name_label_->SetEnabledColorId(ui::ColorIds::kColorSysOnSurface);
+}
+
+void MediaViewControllerBase::AnnounceDynamicChangeIfNeeded(
+    std::u16string previous_device_name,
+    std::u16string announcement) {
+  if (!has_device_list_changed_before_) {
+    has_device_list_changed_before_ = true;
+    return;
+  }
+
+  if (previous_device_name == device_name_label_->GetText()) {
+    return;
+  }
+
+  // TODO(b/329888948): Add test coverage for announcement.
+  device_name_label_->GetViewAccessibility().AnnouncePolitely(announcement);
 }
