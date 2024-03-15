3bd413a31ccf4b3d0fd89f2749275fbce0219cda
David Padlipsky <dpad@google.com>
2024-03-15 22:58:30 +0000

    Update combo device classification metric to work with metadata
    Metric tracking combo device numbers was not tied correctly to metadata.
    Previously this did not matter as we had very little metadata, recent
    efforts have increased our usage of metadata substantially, thus the
    metric needs to be updated.
    
    Fixed: b/329899153
    Change-Id: Idcbb9cd4bc897762be9feab5568ebb864c5db59d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5375415
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1273706}

--- ash/system/input_device_settings/input_device_settings_metrics_manager.cc
+++ ash/system/input_device_settings/input_device_settings_metrics_manager.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/input_device_settings/input_device_settings_metrics_manager.h"
 
 #include <cstdint>
+#include <optional>
 #include <string_view>
 
 #include "ash/accelerators/accelerator_encoding.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
 #include "ash/system/input_device_settings/input_device_settings_controller_impl.h"
+#include "ash/system/input_device_settings/input_device_settings_metadata.h"
 #include "ash/system/input_device_settings/input_device_settings_pref_names.h"
 #include "ash/system/input_device_settings/input_device_settings_utils.h"
 #include "ash/system/input_device_settings/settings_updated_metrics_info.h"
@@ -27,9 +29,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/strings/stringprintf.h"
 #include "base/time/time.h"
+#include "base/types/optional_ref.h"
 #include "components/prefs/pref_service.h"
 #include "ui/events/ash/keyboard_capability.h"
 #include "ui/events/ash/mojom/six_pack_shortcut_modifier.mojom-shared.h"
+#include "ui/events/devices/device_data_manager.h"
+#include "ui/events/devices/keyboard_device.h"
 #include "ui/events/keycodes/keyboard_codes_posix.h"
 #include "ui/events/ozone/evdev/keyboard_mouse_combo_device_metrics.h"
 
@@ -418,6 +423,19 @@ void RecordInitialButtonRemappingAction(
   // Add metrics for recording remapping actions.
   RecordButtonRemappingAction(*(button_remapping->remapping_action),
                               peripheral_kind, "Initial");
+}
+
+std::optional<ui::KeyboardDevice> FindKeyboardWithId(int device_id) {
+  const auto& keyboards =
+      ui::DeviceDataManager::GetInstance()->GetKeyboardDevices();
+  auto iter = base::ranges::find(
+      keyboards, device_id,
+      [](const ui::KeyboardDevice& keyboard) { return keyboard.id; });
+  if (iter == keyboards.end()) {
+    return std::nullopt;
+  }
+
+  return *iter;
 }
 
 }  // namespace
@@ -992,22 +1010,18 @@ void InputDeviceSettingsMetricsManager::RecordKeyboardMouseComboDeviceMetric(
     const mojom::Keyboard& keyboard,
     const mojom::Mouse& mouse) {
   static base::NoDestructor<base::flat_set<std::string>> logged_devices;
-  static constexpr auto kKnownKeyboardMouseComboDevices =
-      base::MakeFixedFlatSet<std::string_view>({
-          "046d:4024",  // Logitech K400
-          "046d:404d",  // Logitech K400+
-          "046d:c548",  // Logitech BOLT Receiver
-          "17ef:60e1",  // Lenovo TrackPoint Keyboard II
-          "17ef:60ee",  // Lenovo TrackPoint Keyboard II
-          "17ef:609f",  // Lenovo 100 USB-A Wireless Combo Keyboard and Mouse
-      });
 
   auto [_, inserted] = logged_devices->insert(keyboard.device_key);
   if (!inserted) {
     return;
   }
 
-  if (kKnownKeyboardMouseComboDevices.contains(keyboard.device_key)) {
+  auto keyboard_device = FindKeyboardWithId(keyboard.id);
+  if (!keyboard_device) {
+    return;
+  }
+
+  if (GetDeviceType(*keyboard_device) == DeviceType::kKeyboardMouseCombo) {
     base::UmaHistogramEnumeration(
         "ChromeOS.Inputs.ComboDeviceClassification",
         ui::ComboDeviceClassification::kKnownComboDevice);
