4eddfbdcab655baf6b38748d04251d56cc7fd3d3
Ameur Hosni <ameurhosni@google.com>
2024-03-15 15:05:50 +0000

    [IOS][Omnibox] Fix scrolling with keyboard up/down arrows
    We plan to increase popup suggestions limit. scrolling with up/down
    keyboard arrows is broken when the number of suggestions exceeds 10.
    
    This CL:
    - Fixes The issue by using scrollToRowAtIndexPath method.
    - Updates and fixes the popupVC unittest.
    
    Bug: 329212584
    Change-Id: Ie10e14825ea76c026d50fec57237da80f3f37324
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5363616
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1273407}

--- ios/chrome/browser/ui/omnibox/popup/omnibox_popup_view_controller.mm
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_popup_view_controller.mm
@@ -567,6 +567,10 @@ BOOL ShouldDismissKeyboardOnScroll() {
     path = [NSIndexPath indexPathForRow:path.row - 1 inSection:path.section];
   }
 
+  [self.tableView scrollToRowAtIndexPath:path
+                        atScrollPosition:UITableViewScrollPositionTop
+                                animated:NO];
+
   self.highlightedIndexPath = path;
 }
 
@@ -605,6 +609,10 @@ BOOL ShouldDismissKeyboardOnScroll() {
   } else {
     path = [NSIndexPath indexPathForRow:path.row + 1 inSection:path.section];
   }
+
+  [self.tableView scrollToRowAtIndexPath:path
+                        atScrollPosition:UITableViewScrollPositionBottom
+                                animated:NO];
 
   // There is a row below, move highlight there.
   self.highlightedIndexPath = path;
