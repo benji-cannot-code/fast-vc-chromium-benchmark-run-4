3e0bb9e22d05e4fe3fa7b4b4ae4db72b384b1f79
Rubin Deliallisi <rubindl@chromium.org>
2024-03-16 21:45:09 +0000

    [Cake] Cleanup SystemIdentityManager
    - Remove boilerplate code.
    
    This is part of a multi CLs change:
    -  https://crrev.com/c/5371765
    -  https://crrev.com/i/7071632
    => https://crrev.com/c/5374203
    
    Bug: 324091979
    Change-Id: I032a8e9b90e46de01d6014676257755667749780
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5374203
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273855}

--- ios/chrome/browser/signin/model/system_identity_manager.mm
+++ ios/chrome/browser/signin/model/system_identity_manager.mm
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/functional/bind.h"
 #import "base/functional/callback.h"
-#import "base/functional/callback_helpers.h"
-#import "base/metrics/user_metrics.h"
 #import "components/signin/internal/identity_manager/account_capabilities_constants.h"
 
 namespace {
@@ -86,17 +84,4 @@ void SystemIdentityManager::FireIdentityAccessTokenRefreshFailed(
   for (auto& observer : observers_) {
     observer.OnIdentityAccessTokenRefreshFailed(identity, error);
   }
-}
-
-// TODO(crbug.com/324091979): Remove once it is replaced by the internal
-// implementetion.
-DismissViewCallback
-SystemIdentityManager::PresentLinkedServicesSettingsDetailsController(
-    id<SystemIdentity> identity,
-    UIViewController* view_controller,
-    bool animated) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  base::RecordAction(
-      base::UserMetricsAction("Signin_MyGoogleUI_LinkedServicesControls"));
-  return base::DoNothing();
 }
