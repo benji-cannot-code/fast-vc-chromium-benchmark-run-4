4dada3a0327f0f23069f55c0bcce5c9d9d832716
Zoraiz Naeem <zoraiznaeem@chromium.org>
2024-03-16 00:07:33 +0000

    Handle snap group windows when synchronizing layer trees
    Currently when synchronizing layer trees of WindowMiniView for overview
    and alt-tab cases, we  assume that both bottom corners of the
    WindowMiniView are rounded. However, this is not true when windows are
    part of a snap group.
    
    To address this issue, we have introduced a helper method that
    finds the radii of WindowMiniView, considering whether the window is
    part of a snap group.
    
    This method is used to determine the correct rounded corners against
    which the layer trees are synchronized.
    
    Bug: b/326468096
    Change-Id: Iaea2f6cd86b5228fe413915dfea15ed22633fca6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5375090
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273733}

--- ash/wm/window_util.cc
+++ ash/wm/window_util.cc
@@ -65,6 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/screen.h"
 #include "ui/events/event.h"
 #include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/geometry/rounded_corners_f.h"
 #include "ui/gfx/geometry/transform_util.h"
 #include "ui/views/view.h"
 #include "ui/views/widget/widget.h"
@@ -176,6 +177,40 @@ int GetMiniWindowRoundedCornerRadius() {
   return chromeos::features::IsRoundedWindowsEnabled()
              ? chromeos::features::RoundedWindowsRadius()
              : kWindowMiniViewCornerRadius;
+}
+
+gfx::RoundedCornersF GetMiniWindowRoundedCorners(const aura::Window* window,
+                                                 bool include_header_rounding,
+                                                 std::optional<float> scale) {
+  const int corner_radius = window_util::GetMiniWindowRoundedCornerRadius();
+  const float scaled_corner_radius = corner_radius / scale.value_or(1.0f);
+
+  if (SnapGroupController* snap_group_controller = SnapGroupController::Get()) {
+    if (SnapGroup* snap_group =
+            snap_group_controller->GetSnapGroupForGivenWindow(window)) {
+      return window == snap_group->window1()
+                 ? gfx::RoundedCornersF(
+                       /*upper_left=*/include_header_rounding
+                           ? scaled_corner_radius
+                           : 0,
+                       /*upper_right=*/0, /*lower_right=*/0,
+                       /*lower_left=*/
+                       scaled_corner_radius)
+                 : gfx::RoundedCornersF(
+                       /*upper_left=*/0,
+                       /*upper_right=*/
+                       include_header_rounding ? scaled_corner_radius : 0,
+                       /*lower_right=*/
+                       scaled_corner_radius,
+                       /*lower_left=*/0);
+    }
+  }
+
+  return gfx::RoundedCornersF(
+      /*upper_left=*/include_header_rounding ? scaled_corner_radius : 0,
+      /*upper_right=*/include_header_rounding ? scaled_corner_radius : 0,
+      /*lower_right=*/scaled_corner_radius,
+      /*lower_left=*/scaled_corner_radius);
 }
 
 aura::Window* GetActiveWindow() {
