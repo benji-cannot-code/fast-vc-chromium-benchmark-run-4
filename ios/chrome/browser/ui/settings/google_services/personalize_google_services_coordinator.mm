ee9050a50efcdfd66fb08198b966c5ea0389bb24
Rubin Deliallisi <rubindl@chromium.org>
2024-03-17 00:19:09 +0000

    [Cake] Linkout functionality in Personalize Google services page
    - Linkout to the correct Google Account page from the Personalize Google
    services page link rows.
    
    Bug: 324091979
    Change-Id: I50737a9837731e51ce24ab7b982732b31433381e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5371889
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273864}

--- ios/chrome/browser/ui/settings/google_services/personalize_google_services_coordinator.mm
+++ ios/chrome/browser/ui/settings/google_services/personalize_google_services_coordinator.mm
@@ -6,9 +6,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/google_services/personalize_google_services_coordinator.h"
 
 #import "base/check_op.h"
+#import "base/metrics/user_metrics.h"
+#import "ios/chrome/browser/shared/model/application_context/application_context.h"
+#import "ios/chrome/browser/shared/model/browser/browser.h"
+#import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/shared/ui/table_view/table_view_utils.h"
+#import "ios/chrome/browser/signin/model/authentication_service.h"
+#import "ios/chrome/browser/signin/model/authentication_service_factory.h"
+#import "ios/chrome/browser/signin/model/system_identity_manager.h"
 #import "ios/chrome/browser/ui/settings/google_services/personalize_google_services_command_handler.h"
 #import "ios/chrome/browser/ui/settings/google_services/personalize_google_services_view_controller.h"
+
+using DismissViewCallback = SystemIdentityManager::DismissViewCallback;
 
 @interface PersonalizeGoogleServicesCoordinator () <
     PersonalizeGoogleServicesViewControllerPresentationDelegate,
@@ -17,6 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation PersonalizeGoogleServicesCoordinator {
   PersonalizeGoogleServicesViewController* _viewController;
+  // Dismiss callback for Web and app setting details view.
+  DismissViewCallback _dismissWebAndAppSettingDetailsCallback;
+  // Dismiss callback for Linked Google services settings details view.
+  DismissViewCallback _dismissLinkedGoogleServicesSettingsDetailsCallback;
 }
 
 @synthesize baseNavigationController = _baseNavigationController;
@@ -45,6 +58,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)stop {
   _viewController = nil;
+
+  if (!_dismissLinkedGoogleServicesSettingsDetailsCallback.is_null()) {
+    std::move(_dismissLinkedGoogleServicesSettingsDetailsCallback)
+        .Run(/*animated*/ false);
+  }
+
+  if (!_dismissWebAndAppSettingDetailsCallback.is_null()) {
+    std::move(_dismissWebAndAppSettingDetailsCallback).Run(/*animated*/ false);
+  }
 }
 
 #pragma mark - PersonalizeGoogleServicesViewControllerPresentationDelegate
@@ -58,11 +80,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - PersonalizeGoogleServicesCommandHandler
 
 - (void)openWebAppActivityDialog {
-  // TODO(crbug.com/324091979): Open Web & App Activity page.
+  base::RecordAction(base::UserMetricsAction(
+      "Signin_AccountSettings_GoogleActivityControlsClicked"));
+
+  AuthenticationService* authService =
+      AuthenticationServiceFactory::GetForBrowserState(
+          self.browser->GetBrowserState());
+  id<SystemIdentity> identity =
+      authService->GetPrimaryIdentity(signin::ConsentLevel::kSignin);
+  _dismissWebAndAppSettingDetailsCallback =
+      GetApplicationContext()
+          ->GetSystemIdentityManager()
+          ->PresentWebAndAppSettingDetailsController(identity, _viewController,
+                                                     YES);
 }
 
 - (void)openLinkedGoogleServicesDialog {
-  // TODO(crbug.com/324091979): Open Linked Google services page.
+  base::RecordAction(base::UserMetricsAction(
+      "Signin_AccountSettings_LinkedGoogleServicesClicked"));
+
+  AuthenticationService* authService =
+      AuthenticationServiceFactory::GetForBrowserState(
+          self.browser->GetBrowserState());
+  id<SystemIdentity> identity =
+      authService->GetPrimaryIdentity(signin::ConsentLevel::kSignin);
+  _dismissLinkedGoogleServicesSettingsDetailsCallback =
+      GetApplicationContext()
+          ->GetSystemIdentityManager()
+          ->PresentLinkedServicesSettingsDetailsController(
+              identity, _viewController, YES);
 }
 
 @end
