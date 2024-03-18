30d9edb9af3da2f303c254197a876e04d34a6beb
Ed Chin <edchin@google.com>
2024-03-18 19:34:20 +0000

    Enable Saved State Notification
    Confirmed saved state notification works properly in canary
    Version 124.0.6364.0 (Official Build) canary (arm64)
    including fix for autofill password suggestion collision.
    
    Bug: b/328730215
    Change-Id: I03cd82e208e6033e5725c1d7436db03b63b78115
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5375221
    Commit-Queue: edchin <edchin@google.com>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274369}

--- components/compose/core/browser/compose_features.cc
+++ components/compose/core/browser/compose_features.cc
@@ -44,7 +44,7 @@ BASE_FEATURE(kEnableComposeOnDeviceDogfoodFooter,
 
 BASE_FEATURE(kEnableComposeSavedStateNotification,
              "EnableComposeSavedStateNotification",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kComposeUiParams,
              "ComposeUiParams",
