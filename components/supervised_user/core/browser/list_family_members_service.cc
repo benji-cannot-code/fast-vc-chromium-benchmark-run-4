9dab88e28304d5fd09289461d09967a972015c68
Tomasz Jurkiewicz <tju@google.com>
2024-03-18 10:44:19 +0000

    Add disposable observers to the ListFamilyMembersService.
    They're only consumed once and then disposed of.
    
    Change-Id: If86ee8aaf9010ef7c144815f0f56f9dcc36f7728
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5375241
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Auto-Submit: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1274063}

--- components/supervised_user/core/browser/list_family_members_service.cc
+++ components/supervised_user/core/browser/list_family_members_service.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/supervised_user/core/browser/list_family_members_service.h"
 
 #include <memory>
+#include <utility>
+
 #include "base/callback_list.h"
 #include "base/functional/callback_forward.h"
 #include "base/location.h"
@@ -44,7 +46,13 @@ ListFamilyMembersService::~ListFamilyMembersService() = default;
 base::CallbackListSubscription
 ListFamilyMembersService::SubscribeToSuccessfulFetches(
     base::RepeatingCallback<SuccessfulFetchCallback> callback) {
-  return successful_fetch_consumers_.Add(callback);
+  return successful_fetch_repeating_consumers_.Add(callback);
+}
+
+base::CallbackListSubscription
+ListFamilyMembersService::SubscribeToNextSuccessfulFetch(
+    base::OnceCallback<SuccessfulFetchCallback> callback) {
+  return successful_fetch_disposable_consumers_.Add(std::move(callback));
 }
 
 void ListFamilyMembersService::Start() {
@@ -77,7 +85,8 @@ void ListFamilyMembersService::OnResponse(
     return;
   }
 
-  successful_fetch_consumers_.Notify(*response);
+  successful_fetch_repeating_consumers_.Notify(*response);
+  successful_fetch_disposable_consumers_.Notify(*response);
   ScheduleNextUpdate(NextUpdate(status));
 }
 
