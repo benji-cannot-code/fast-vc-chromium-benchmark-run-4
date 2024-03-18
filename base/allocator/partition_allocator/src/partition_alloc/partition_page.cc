43f6865941b47404797001ee49ae7a4fb8006822
Scott Violet <sky@chromium.org>
2024-03-18 18:25:04 +0000

    pa: always set ring buffer to null
    And add DCHECKs that the buffer is null.
    
    I think this better conveys expectations.
    
    Bug: None
    Change-Id: I3cb70a6d163171401b3d1a902c3173aa5401443c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5370361
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274311}

--- base/allocator/partition_allocator/src/partition_alloc/partition_page.cc
+++ base/allocator/partition_allocator/src/partition_alloc/partition_page.cc
@@ -108,6 +108,10 @@ PA_ALWAYS_INLINE void SlotSpanMetadata::RegisterEmpty() {
     slot_span_to_decommit->DecommitIfPossible(root);
   }
 
+  // There should not be a slot span in the buffer at the position this is
+  // going into.
+  PA_DCHECK(!root->global_empty_slot_span_ring[current_index]);
+
   // We put the empty slot span on our global list of "slot spans that were once
   // empty", thus providing it a bit of breathing room to get re-used before we
   // really free it. This reduces the number of system calls. Otherwise any
@@ -250,8 +254,8 @@ void SlotSpanMetadata::DecommitIfPossible(PartitionRoot* root) {
   in_empty_cache_ = 0;
   if (is_empty()) {
     Decommit(root);
-    root->global_empty_slot_span_ring[empty_cache_index_] = nullptr;
   }
+  root->global_empty_slot_span_ring[empty_cache_index_] = nullptr;
 }
 
 void SlotSpanMetadata::SortFreelist() {
