c5e138baac099842f0c88f14435760500e1ea38b
dpapad <dpapad@chromium.org>
2024-03-18 19:04:38 +0000

    WebUI: Use cr-grid instead of custom cr-profile-avatar-selector-grid.
     - Port ignoreModifiedKeyEvents from cr-profile-avatar-selector-grid to
       cr-grid.
     - Add ability to customize cr-grid's focus() logic, to focus a child
       of the slotted element, instead of the slotted element itself.
     - Add ability to catch keyboard events that originate in a child of
       a slotted element.
     - Add cr-grid tests for each addition.
     - Delete cr-profile-avatar-selector-grid.
    
    This is in preparation of migrating cr-profile-avatar-selector to Lit.
    
    Bug: 40943652
    Change-Id: I18f068ee813b3de5454291844abca4f825b8be55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5373378
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274346}

--- ui/webui/resources/cr_elements/cr_profile_avatar_selector/cr_profile_avatar_selector.ts
+++ ui/webui/resources/cr_elements/cr_profile_avatar_selector/cr_profile_avatar_selector.ts
@@ -9,11 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 import '../cr_button/cr_button.js';
+import '../cr_grid/cr_grid.js';
 import '../cr_shared_vars.css.js';
 import '../cr_shared_style.css.js';
 import '//resources/polymer/v3_0/paper-styles/color.js';
 import '//resources/polymer/v3_0/paper-tooltip/paper-tooltip.js';
-import './cr_profile_avatar_selector_grid.js';
 
 import {assert} from '//resources/js/assert.js';
 import {getImage} from '//resources/js/icon.js';
@@ -71,6 +71,14 @@ export class CrProfileAvatarSelectorElement extends PolymerElement {
       tabFocusableAvatar_: {
         type: Number,
         computed: 'computeTabFocusableAvatar_(avatars, selectedAvatar)',
+      },
+
+      /**
+       * Number of columns in the grid.
+       */
+      columns: {
+        type: Number,
+        value: 6,
       },
     };
   }
