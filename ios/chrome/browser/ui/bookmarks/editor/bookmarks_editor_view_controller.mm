470e578976b3e3657194648cdc3987b04cbd406a
Arthur Milchior <arthurmilchior@chromium.org>
2024-03-18 12:10:26 +0000

    [iOS][Bookmarks] Add cloud slash icon in bookmark editor
    It was added only after the folder was changed. Now it is always, if
    needed, when the view is first opened.
    
    Also, move some repeated code in a function. This change the test by
    checking for the new behaviour.
    
    Fixed: 329606404
    Change-Id: I1562e7cc4f04454c2a13c536a425b410a7cddace
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5368856
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274080}

--- ios/chrome/browser/ui/bookmarks/editor/bookmarks_editor_view_controller.mm
+++ ios/chrome/browser/ui/bookmarks/editor/bookmarks_editor_view_controller.mm
@@ -212,6 +212,8 @@ const CGFloat kEstimatedTableSectionFooterHeight = 40;
   self.folderItem =
       [[BookmarkParentFolderItem alloc] initWithType:ItemTypeFolder];
   self.folderItem.title = _folderName;
+  self.folderItem.shouldDisplayCloudSlashIcon =
+      [self.mutator shouldDisplayCloudSlashSymbolForParentFolder];
   [model addItem:self.folderItem toSectionWithIdentifier:SectionIdentifierInfo];
 
   self.URLItem = [[BookmarkTextFieldItem alloc] initWithType:ItemTypeURL];
