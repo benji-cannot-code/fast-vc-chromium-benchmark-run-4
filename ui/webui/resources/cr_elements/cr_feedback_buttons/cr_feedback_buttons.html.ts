db567095ae6ffa6316cae8a9b6b703f29c07796e
rbpotter <rbpotter@chromium.org>
2024-03-18 18:25:12 +0000

    WebUI: Migrate cr-feedback-buttons to LitElement
    Bug: 40943652
    Change-Id: I934ba505f8b38fbcd6dfe778d8627e9e7e0fd696
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5370768
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274312}

---
+++ ui/webui/resources/cr_elements/cr_feedback_buttons/cr_feedback_buttons.html.ts
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {CrFeedbackButtonsElement} from './cr_feedback_buttons.js';
+
+export function getHtml(this: CrFeedbackButtonsElement) {
+  return html`
+<div class="buttons">
+  <cr-icon-button id="thumbsUp" iron-icon="${this.getThumbsUpIcon_()}"
+      aria-label="${this.thumbsUpLabel_}"
+      title="${this.thumbsUpLabel_}"
+      aria-pressed="${this.getThumbsUpAriaPressed_()}"
+      @click="${this.onThumbsUpClick_}">
+  </cr-icon-button>
+  <cr-icon-button id="thumbsDown"
+      iron-icon="${this.getThumbsDownIcon_()}"
+      aria-label="${this.thumbsDownLabel_}"
+      title="${this.thumbsDownLabel_}"
+      aria-pressed="${this.getThumbsDownAriaPressed_()}"
+      @click="${this.onThumbsDownClick_}">
+  </cr-icon-button>
+</div>`;
+}
