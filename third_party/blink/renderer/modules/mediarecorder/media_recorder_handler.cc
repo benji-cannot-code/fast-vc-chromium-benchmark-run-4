5d9ce0cbfb2e5ddb365316752f9996a0ce7b2c41
Sunggook Chue <sunggch@microsoft.com>
2024-03-18 22:43:18 +0000

    Adjust option of audioBitsPerSecond beyond MediaFoundation accepted list.
    In Windows OS, AAC encoder uses Media Foundation but it supports only
    limited bitrates such as 95k, 128k, 160k, 192k.
    
    The current MediaRecorder implementation for AAC encoding does
    not record audio if the unaccepted audioBitsPerSecond value is given.
    
    The spec said that this value is a hint, not hard requirement. So,
    the change round the input value to the accepted bitrate.
    
    Bug: 327695863
    
    Change-Id: I08ae54d7492307db714b2782d9b96ba34179f199
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5337945
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Markus Handell <handellm@google.com>
    Commit-Queue: Sunggook Chue <sunggch@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1274515}

--- third_party/blink/renderer/modules/mediarecorder/media_recorder_handler.cc
+++ third_party/blink/renderer/modules/mediarecorder/media_recorder_handler.cc
@@ -50,6 +50,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 
+#if BUILDFLAG(IS_WIN)
+#include "media/gpu/windows/mf_audio_encoder.h"
+#endif
+
 using base::TimeTicks;
 
 namespace blink {
@@ -481,6 +485,16 @@ bool MediaRecorderHandler::Start(int timeslice,
         audio_codec, use_video_tracks, use_audio_tracks,
         std::make_unique<media::Mp4MuxerDelegate>(write_callback),
         optional_timeslice);
+
+#if BUILDFLAG(IS_WIN)
+    // Windows OS uses MediaFoundation for MP4 muxing, which requires the
+    // specific audio bit rate for AAC encoding.
+    if (audio_bits_per_second_ != 0u) {
+      audio_bits_per_second_ =
+          media::MFAudioEncoder::ClampAccCodecBitrate(audio_bits_per_second_);
+      recorder_->UpdateAudioBitrate(audio_bits_per_second_);
+    }
+#endif
   } else {
     muxer = std::make_unique<media::WebmMuxer>(
         audio_codec, use_video_tracks, use_audio_tracks,
