ee09bfa56766d7c36db5fbda5d22fecc02dc8b9c
Gastón Rodríguez <gastonr@microsoft.com>
2024-03-18 20:17:46 +0000

    [Fluent Scrollbar] Deferring typeface initialization to first PaintArrow
    On instantiation, NativeThemeFluent queries the DefaultFontManager and
    tries to fetch the font to draw scrollbar arrows with, which will be
    used in NativeThemeFluent::PaintArrow().
    In many tests, when FluentScrollbars are enabled, this causes issues
    because tests can be particular about wanting to instantiate font
    managers or configs in a specific order which is disrupted when the
    NativeTheme instantiates a FontManager before the desired order. This
    has been the cause for some tests needing rewriting in the past [1,2,3]
    
    In many of these tests, the NativeTheme gets instantiated to check
    things like the color scheme, and paint calls are never actually called.
    
    This CL defers the call to fetch the typeface to the first call to
    PaintArrow, to avoid unnecessary instantiations and lazily load the
    fonts.
    
    [1] https://crrev.com/4257851
    [2] https://crrev.com/5111784
    [3] https://ci.chromium.org/ui/p/chromium/builders/try/linux-rel/1736279/test-results?sortby=&groupby=&q=renderthread+
    
    Bug: 329489158
    Change-Id: If66c1f569bdbf7a9b4d964c361d69f54f7f225d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5375528
    Reviewed-by: Yaroslav Shalivskyy <yshalivskyy@microsoft.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Gaston Rodriguez <gastonr@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1274399}

--- ui/native_theme/native_theme_fluent.cc
+++ ui/native_theme/native_theme_fluent.cc
@@ -26,10 +26,6 @@ namespace ui {
 NativeThemeFluent::NativeThemeFluent(bool should_only_use_dark_colors)
     : NativeThemeBase(should_only_use_dark_colors) {
   scrollbar_width_ = kFluentScrollbarThickness;
-
-  const sk_sp<SkFontMgr> font_manager(skia::DefaultFontMgr());
-  typeface_ = sk_sp<SkTypeface>(
-      font_manager->matchFamilyStyle(kFluentScrollbarFont, SkFontStyle()));
 }
 
 NativeThemeFluent::~NativeThemeFluent() = default;
@@ -259,6 +255,11 @@ void NativeThemeFluent::PaintArrow(
   cc::PaintFlags flags;
   flags.setColor(arrow_color);
 
+  if (!typeface_.has_value()) {
+    const sk_sp<SkFontMgr> font_manager(skia::DefaultFontMgr());
+    typeface_ = sk_sp<SkTypeface>(
+        font_manager->matchFamilyStyle(kFluentScrollbarFont, SkFontStyle()));
+  }
   if (!ArrowIconsAvailable()) {
     // Paint regular triangular arrows if the font with arrow icons is not
     // available. GetArrowRect() returns the float rect but it is expected to be
@@ -272,8 +273,8 @@ void NativeThemeFluent::PaintArrow(
   const gfx::RectF bounding_rect = GetArrowRect(rect, part, state);
   // The bounding rect for an arrow is a square, so that we can use the width
   // despite the arrow direction.
-  DCHECK(typeface_);
-  SkFont font(typeface_, bounding_rect.width());
+  CHECK(typeface_.has_value());
+  SkFont font(typeface_.value(), bounding_rect.width());
   font.setEdging(SkFont::Edging::kAntiAlias);
   font.setSubpixel(true);
   flags.setAntiAlias(true);
