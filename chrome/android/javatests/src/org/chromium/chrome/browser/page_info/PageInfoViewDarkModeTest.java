078d7cb779e42b891ec160b17f4822659de61481
Brandon Wylie <wylieb@chromium.org>
2024-03-18 18:28:16 +0000

    [sheriff] Disable flaky PageInfoViewDarkModeTest
    Bug: 330166513
    Change-Id: I20fd96aec605ddb771947b3bb9213afc4ba79ddb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5378463
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Owners-Override: Brandon Wylie <wylieb@google.com>
    Auto-Submit: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1274316}

--- chrome/android/javatests/src/org/chromium/chrome/browser/page_info/PageInfoViewDarkModeTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/page_info/PageInfoViewDarkModeTest.java
@@ -24,6 +24,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.chrome.browser.app.ChromeActivity;
@@ -132,6 +133,7 @@ public class PageInfoViewDarkModeTest {
     @MediumTest
     @Feature({"RenderTest"})
     @DisableFeatures(ChromeFeatureList.TRACKING_PROTECTION_3PCD)
+    @DisabledTest(message = "crbug.com/330166513")
     public void testShowOnSecureWebsiteDark() throws IOException {
         loadUrlAndOpenPageInfo(mTestServerRule.getServer().getURL(sSimpleHtml));
         mRenderTestRule.render(getPageInfoView(), "PageInfo_SecureWebsiteDark");
