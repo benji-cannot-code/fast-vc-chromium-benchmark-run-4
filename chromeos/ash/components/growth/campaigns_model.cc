357d4a94dc4dde9bf35fcbb6afce892edbd02daa
Li Lin <llin@chromium.org>
2024-03-18 20:44:29 +0000

    Add API for performing action.
    This CL also added dismiss action, which will be handled by surface instead.
    We haven't rolled out action so it is still safe to renumbered the action type.
    
    This will be used by Nudge to perform button click action. A follow up
    CL will wire it up with Nudge and add tests.
    
    Bug: b/328815164
    Change-Id: Ibc832310aab4cf0e8b4d6f7eba60270179e01448
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5375284
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Li Lin <llin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274423}

--- chromeos/ash/components/growth/campaigns_model.cc
+++ chromeos/ash/components/growth/campaigns_model.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/growth/campaigns_model.h"
 
 #include <memory>
+#include <optional>
 
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
@@ -58,6 +59,10 @@ inline constexpr char kExperimentTargetings[] = "experimentTags";
 // Payloads
 inline constexpr char kPayloadPathTemplate[] = "payload.%s";
 inline constexpr char kDemoModePayloadPath[] = "demoModeApp";
+
+// Actions
+inline constexpr char kActionTypePath[] = "type";
+inline constexpr char kActionParamsPath[] = "params";
 
 }  // namespace
 
@@ -252,6 +257,22 @@ SessionTargeting::GetSchedulings() const {
 
 const base::Value::List* SessionTargeting::GetExperimentTags() const {
   return GetListCriteria(kExperimentTargetings);
+}
+
+Action::Action(const base::Value::Dict* action_dict)
+    : action_dict_(action_dict) {}
+
+std::optional<growth::ActionType> Action::GetActionType() const {
+  auto action_type_value = action_dict_->FindInt(kActionTypePath);
+  if (!action_type_value) {
+    return std::nullopt;
+  }
+
+  return static_cast<growth::ActionType>(action_type_value.value());
+}
+
+const base::Value::Dict* Action::GetParams() const {
+  return action_dict_->FindDict(kActionParamsPath);
 }
 
 const std::vector<std::unique_ptr<AppTargeting>>
