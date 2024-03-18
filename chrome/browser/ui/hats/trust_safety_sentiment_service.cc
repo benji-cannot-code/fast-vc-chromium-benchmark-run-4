f0e649cacecce2fe334139520a58602b192e4db1
Daniel Rubery <drubery@chromium.org>
2024-03-18 17:02:40 +0000

    Prepare to convert SBThreatType to an enum class in /chrome/browser/ui/hats
    It's somewhat difficult to log an SBThreatType as an UMA histogram since
    we don't have a kMaxValue. We can't simply add a kMaxValue because
    SBThreatType is an enum instead of enum class, and we'd be exposing a
    constant safe_browsing::kMaxValue. This CL prepares to convert
    SBThreatType to an enum class in /chrome/browser/ui/hats.
    
    That requires two changes:
    
    - SBThreatType is no longer implicitly convertible to int, so we will
      need a few static casts
    
    - SBThreatType values are now in the namespace
      safe_browsing::SBThreatType instead of just safe_browsing. In some
      places this is fixed by adding the new namespace, and in some places
      with a "using enum" declaration. We can't just add a "using enum" for
      all usages of SBThreatType because "using enum" would declare a
      constant kMaxValue. So "using enum" is only used when many
      SBThreatType constants are used, and in as narrow a scope as possible.
    
    This CL was uploaded by git cl split.
    
    Bug: 329698341
    Change-Id: I6e9d000b525df873c8852ab970fb339793a8d583
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5376935
    Auto-Submit: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Florian Jacky <fjacky@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274226}

--- chrome/browser/ui/hats/trust_safety_sentiment_service.cc
+++ chrome/browser/ui/hats/trust_safety_sentiment_service.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/privacy_sandbox/privacy_sandbox_prefs.h"
 #include "components/privacy_sandbox/tracking_protection_prefs.h"
+#include "components/safe_browsing/core/browser/db/v4_protocol_manager_util.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #include "components/signin/public/base/signin_pref_names.h"
 #include "components/unified_consent/pref_names.h"
@@ -157,11 +158,11 @@ std::map<std::string, bool> GetPrivacySettingsProductSpecificData(
 // software, or billing threat categories.
 bool IsOtherSBInterstitialCategory(safe_browsing::SBThreatType threat_type) {
   switch (threat_type) {
-    case safe_browsing::SB_THREAT_TYPE_URL_PHISHING:
-    case safe_browsing::SB_THREAT_TYPE_URL_CLIENT_SIDE_PHISHING:
-    case safe_browsing::SB_THREAT_TYPE_URL_MALWARE:
-    case safe_browsing::SB_THREAT_TYPE_URL_UNWANTED:
-    case safe_browsing::SB_THREAT_TYPE_BILLING:
+    case safe_browsing::SBThreatType::SB_THREAT_TYPE_URL_PHISHING:
+    case safe_browsing::SBThreatType::SB_THREAT_TYPE_URL_CLIENT_SIDE_PHISHING:
+    case safe_browsing::SBThreatType::SB_THREAT_TYPE_URL_MALWARE:
+    case safe_browsing::SBThreatType::SB_THREAT_TYPE_URL_UNWANTED:
+    case safe_browsing::SBThreatType::SB_THREAT_TYPE_BILLING:
       return false;
     default:
       return true;
@@ -438,14 +439,15 @@ void TrustSafetySentimentService::InteractedWithSafeBrowsingInterstitial(
   product_specific_data["Enhanced protection enabled"] =
       safe_browsing::IsEnhancedProtectionEnabled(*profile_->GetPrefs());
   product_specific_data["Threat is phishing"] =
-      threat_type == safe_browsing::SB_THREAT_TYPE_URL_PHISHING ||
-      threat_type == safe_browsing::SB_THREAT_TYPE_URL_CLIENT_SIDE_PHISHING;
+      threat_type == safe_browsing::SBThreatType::SB_THREAT_TYPE_URL_PHISHING ||
+      threat_type ==
+          safe_browsing::SBThreatType::SB_THREAT_TYPE_URL_CLIENT_SIDE_PHISHING;
   product_specific_data["Threat is malware"] =
-      threat_type == safe_browsing::SB_THREAT_TYPE_URL_MALWARE;
+      threat_type == safe_browsing::SBThreatType::SB_THREAT_TYPE_URL_MALWARE;
   product_specific_data["Threat is unwanted software"] =
-      threat_type == safe_browsing::SB_THREAT_TYPE_URL_UNWANTED;
+      threat_type == safe_browsing::SBThreatType::SB_THREAT_TYPE_URL_UNWANTED;
   product_specific_data["Threat is billing"] =
-      threat_type == safe_browsing::SB_THREAT_TYPE_BILLING;
+      threat_type == safe_browsing::SBThreatType::SB_THREAT_TYPE_BILLING;
   DCHECK(!IsOtherSBInterstitialCategory(threat_type));
   TriggerOccurred(FeatureArea::kSafeBrowsingInterstitial,
                   product_specific_data);
