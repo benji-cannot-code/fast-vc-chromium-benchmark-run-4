4c202239e30b8982d71236a92a942b672aacd94e
James Cook <jamescook@chromium.org>
2024-03-18 20:37:14 +0000

    birch: Fix file suggest icon colors
    Use chromeos::GetIconColorForPath() so the icon colors match the ones
    used in the File Manager.
    
    Bug: b:330185257
    Test: ash_unittests --gtest_filter=BirchItemIconTest.*
    Change-Id: I305af59fee385fa8efdd899e9466dea038b4df5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5378466
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274415}

--- ash/birch/birch_item_unittest.cc
+++ ash/birch/birch_item_unittest.cc
@@ -15,7 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/models/image_model.h"
+#include "ui/chromeos/styles/cros_tokens_color_mappings.h"
+#include "ui/color/color_provider.h"
+#include "ui/color/color_provider_manager.h"
 #include "ui/gfx/image/image_unittest_util.h"
+#include "ui/native_theme/native_theme.h"
 
 namespace ash {
 namespace {
@@ -236,8 +240,17 @@ TEST_F(BirchItemIconTest, File_LoadIcon) {
   const base::FilePath excel_path("/my/test/mySheet.xlsx");
   BirchFileItem item(excel_path, u"suggested", base::Time());
 
-  item.LoadIcon(base::BindOnce(
-      [](const ui::ImageModel& icon) { EXPECT_FALSE(icon.IsEmpty()); }));
+  item.LoadIcon(base::BindOnce([](const ui::ImageModel& icon) {
+    // Icon was set.
+    EXPECT_FALSE(icon.IsEmpty());
+
+    // Color is the one used for MS Excel documents.
+    auto* native_theme = ui::NativeTheme::GetInstanceForNativeUi();
+    auto* color_provider = ui::ColorProviderManager::Get().GetColorProviderFor(
+        native_theme->GetColorProviderKey(nullptr));
+    EXPECT_EQ(icon.GetVectorIcon().color(),
+              color_provider->GetColor(cros_tokens::kCrosSysFileMsExcel));
+  }));
 }
 
 }  // namespace
