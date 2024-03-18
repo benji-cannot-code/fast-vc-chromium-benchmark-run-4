35d17e2f436504ea0ecb03dce163bb1eb86c575c
David Pennington <dpenning@chromium.org>
2024-03-18 16:50:48 +0000

    Show bookmarks bar when saved tab group exists
    In the case where there are 0 bookmarks, the bookmarks bar wont show on the new tab page. Now that saved tab groups are displayed in the bookmarks UI we need to also check to see if there are any SavedTabGroups to decide whether to show.
    
    Bug: b/41495660
    Change-Id: I4f50b0bdc391ebc11c1c318de7b0973995b9a3b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5371165
    Reviewed-by: pengchao Cai <pengchaocai@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274220}

--- chrome/browser/ui/bookmarks/bookmark_tab_helper.cc
+++ chrome/browser/ui/bookmarks/bookmark_tab_helper.cc
@@ -15,11 +15,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/bookmarks/bookmark_tab_helper_observer.h"
 #include "chrome/browser/ui/bookmarks/bookmark_utils.h"
 #include "chrome/browser/ui/sad_tab.h"
+#include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_keyed_service.h"
+#include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_service_factory.h"
 #include "chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.h"
 #include "chrome/browser/ui/webui/new_tab_page_third_party/new_tab_page_third_party_ui.h"
 #include "chrome/browser/ui/webui/ntp/new_tab_ui.h"
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/bookmarks/common/bookmark_pref_names.h"
+#include "components/saved_tab_groups/saved_tab_group_model.h"
 #include "components/sync_preferences/pref_service_syncable.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/navigation_handle.h"
@@ -71,10 +74,17 @@ bool BookmarkTabHelper::ShouldShowBookmarkBar() const {
       !prefs->GetBoolean(bookmarks::prefs::kShowBookmarkBar))
     return false;
 
+  const bool has_bookmarks = bookmark_model_ && bookmark_model_->HasBookmarks();
+
+  const tab_groups::SavedTabGroupKeyedService* stg_service =
+      tab_groups::SavedTabGroupServiceFactory::GetInstance()->GetForProfile(
+          profile);
+  const bool has_saved_tab_groups =
+      stg_service && (stg_service->model()->Count() > 0);
+
   // The bookmark bar is only shown on the NTP if the user
   // has added something to it.
-  return IsNTP(web_contents()) && bookmark_model_ &&
-         bookmark_model_->HasBookmarks();
+  return IsNTP(web_contents()) && (has_bookmarks || has_saved_tab_groups);
 }
 
 void BookmarkTabHelper::AddObserver(BookmarkTabHelperObserver* observer) {
