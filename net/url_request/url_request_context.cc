dc6aa05d226542bef083aa94f0bbc718c190b76f
Kristian Monsen <kristianm@google.com>
2024-03-18 23:32:00 +0000

    Starting Device Bound Session Credentials (DBSC) integration
    For more information on DBSC see https://github.com/WICG/dbsc.
    
    - Create a feature flag for DBSC, off by default
    - Add a mojom::NetworkContextParamsPtr param for dbsc, only set to true
    in the main network context
    - The DBSC service is attached to the URLRequestContext
    - Add a DBSC service if the feature flag is enabled and param is true,
    otherwise the service returns null
    - If the headers contains a valid DBSC registration, and dbsc is
    enabled, ask the service to create the session
    
    Bug: 41495201
    Change-Id: Ic2737f3d8144893ff39d16af89264c0868c2ec7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5274486
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Kristian Monsen <kristianm@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274553}

--- net/url_request/url_request_context.cc
+++ net/url_request/url_request_context.cc
@@ -48,6 +48,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/reporting/reporting_service.h"
 #endif  // BUILDFLAG(ENABLE_REPORTING)
 
+#if BUILDFLAG(ENABLE_DEVICE_BOUND_SESSIONS)
+#include "net/device_bound_sessions/device_bound_session_service.h"
+#endif  // BUILDFLAG(ENABLE_DEVICE_BOUND_SESSIONS)
+
 namespace net {
 
 URLRequestContext::URLRequestContext(
@@ -253,5 +257,12 @@ void URLRequestContext::set_transport_security_persister(
     std::unique_ptr<TransportSecurityPersister> transport_security_persister) {
   transport_security_persister_ = std::move(transport_security_persister);
 }
+
+#if BUILDFLAG(ENABLE_DEVICE_BOUND_SESSIONS)
+void URLRequestContext::set_device_bound_session_service(
+    std::unique_ptr<DeviceBoundSessionService> device_bound_session_service) {
+  device_bound_session_service_ = std::move(device_bound_session_service);
+}
+#endif  // BUILDFLAG(ENABLE_DEVICE_BOUND_SESSIONS)
 
 }  // namespace net
--- net/url_request/url_request_http_job.cc
+++ net/url_request/url_request_http_job.cc
@@ -104,6 +104,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/android/network_library.h"
 #endif
 
+#if BUILDFLAG(ENABLE_DEVICE_BOUND_SESSIONS)
+#include "net/device_bound_sessions/bound_session_registration_fetcher_param.h"
+#include "net/device_bound_sessions/device_bound_session_service.h"
+#endif  // BUILDFLAG(ENABLE_DEVICE_BOUND_SESSIONS)
+
 namespace {
 
 // These values are persisted to logs. Entries should not be renumbered and
@@ -524,6 +529,9 @@ void URLRequestHttpJob::NotifyHeadersComplete() {
   }
 
   ProcessStrictTransportSecurityHeader();
+#if BUILDFLAG(ENABLE_DEVICE_BOUND_SESSIONS)
+  ProcessDeviceBoundSessionsHeader();
+#endif  // BUILDFLAG(ENABLE_DEVICE_BOUND_SESSIONS)
 
   // Clear |set_cookie_access_result_list_| after any processing in case
   // SaveCookiesAndNotifyHeadersComplete is called again.
@@ -1055,6 +1063,19 @@ void URLRequestHttpJob::OnSetCookieResult(const CookieOptions& options,
   if (num_cookie_lines_left_ == 0)
     NotifyHeadersComplete();
 }
+
+#if BUILDFLAG(ENABLE_DEVICE_BOUND_SESSIONS)
+void URLRequestHttpJob::ProcessDeviceBoundSessionsHeader() {
+  std::vector<BoundSessionRegistrationFetcherParam> params =
+      BoundSessionRegistrationFetcherParam::CreateIfValid(request_->url(),
+                                                          GetResponseHeaders());
+  if (auto* service = request_->context()->device_bound_session_service()) {
+    for (const auto& param : params) {
+      service->RegisterBoundSession(param);
+    }
+  }
+}
+#endif  // BUILDFLAG(ENABLE_DEVICE_BOUND_SESSIONS)
 
 void URLRequestHttpJob::ProcessStrictTransportSecurityHeader() {
   DCHECK(response_info_);
