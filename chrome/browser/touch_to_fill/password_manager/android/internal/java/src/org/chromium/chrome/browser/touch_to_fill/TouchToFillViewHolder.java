e01916822044de9d772a2236cade856f50c94b7a
Timofey Chudakov <tchudakov@google.com>
2024-03-18 16:03:31 +0000

    [Android] Filter touch events for passwords bottom sheet.
    Bug: b/328278717
    Change-Id: I4302cc8c9cad4c7408eee4ef103f69f611c1cf2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5376481
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1274184}

--- chrome/browser/touch_to_fill/password_manager/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/TouchToFillViewHolder.java
+++ chrome/browser/touch_to_fill/password_manager/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/TouchToFillViewHolder.java
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.touch_to_fill;
 
+import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -20,18 +22,24 @@ import org.chromium.ui.modelutil.PropertyModelChangeProcessor.ViewBinder;
 class TouchToFillViewHolder extends RecyclerView.ViewHolder {
     private final ViewBinder<PropertyModel, View, PropertyKey> mViewBinder;
 
+    @SuppressLint("ClickableViewAccessibility")
     TouchToFillViewHolder(
             ViewGroup parent,
             @LayoutRes int layout,
             ViewBinder<PropertyModel, View, PropertyKey> viewBinder) {
         super(LayoutInflater.from(parent.getContext()).inflate(layout, parent, false));
         mViewBinder = viewBinder;
+        itemView.setFilterTouchesWhenObscured(true);
+        itemView.setOnTouchListener(
+                (View v, MotionEvent ev) ->
+                        (ev.getFlags() & MotionEvent.FLAG_WINDOW_IS_PARTIALLY_OBSCURED) != 0);
     }
 
     /**
-     * Called whenever an item is bound to this view holder. Please note that this method
-     * might be called on the same list entry repeatedly, so make sure to always set a default
-     * for unused fields.
+     * Called whenever an item is bound to this view holder. Please note that this method might be
+     * called on the same list entry repeatedly, so make sure to always set a default for unused
+     * fields.
+     *
      * @param model The {@link PropertyModel} whose data needs to be displayed.
      */
     void setupModelChangeProcessor(PropertyModel model) {
