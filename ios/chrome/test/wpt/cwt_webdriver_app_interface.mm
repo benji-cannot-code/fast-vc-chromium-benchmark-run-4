6479360236b31054f3061531274850138e17d700
Federica Germinario <fedegermi@google.com>
2024-03-18 10:31:25 +0000

    [ios] Avoid using mainController.browserProviderInterface
    This CL removes use of mainController.browserProviderInterface from
    cwt_webdriver_app_interface.mm.
    browserProviderInterface is now taken from GetForegroundActiveScene()
    
    Fixed: 327330603
    Change-Id: Ica806f89bbeb5903c1a66bf8d122e7701a4928c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5345250
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1274060}

--- ios/chrome/test/wpt/cwt_webdriver_app_interface.mm
+++ ios/chrome/test/wpt/cwt_webdriver_app_interface.mm
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/strings/utf_string_conversions.h"
 #import "base/test/ios/wait_util.h"
 #import "base/values.h"
-#import "ios/chrome/app/main_controller.h"
+#import "ios/chrome/browser/shared/coordinator/scene/scene_state.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/browser/browser_provider.h"
 #import "ios/chrome/browser/shared/model/browser/browser_provider_interface.h"
@@ -39,10 +39,8 @@ NSString* GetIdForWebState(web::WebState* web_state) {
   return web_state->GetStableIdentifier();
 }
 
-// TODO(crbug.com/327330603): Avoid using
-// mainController.browserProviderInterface.
 WebStateList* GetCurrentWebStateList() {
-  return chrome_test_util::GetMainController()
+  return chrome_test_util::GetForegroundActiveScene()
       .browserProviderInterface.currentBrowserProvider.browser
       ->GetWebStateList();
 }
