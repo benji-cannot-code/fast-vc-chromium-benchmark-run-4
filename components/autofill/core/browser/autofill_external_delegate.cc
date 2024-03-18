a0c33903e31023715def55802c39c14d834ae3b2
Jihad Hanna <jihadghanna@google.com>
2024-03-18 15:19:46 +0000

    Remove PreviouslyHiddenSuggestion logic/metrics
    Those metrics were only needed to evaluate a now-launched Finch feature
    AutofillUseAddressRewriterInProfileSubsetComparison.
    
    Bug: 324029575
    Change-Id: I3f36ec1ea2a6c548e2f8882b0f03962ea96f4ff0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372734
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Auto-Submit: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1274149}

--- components/autofill/core/browser/autofill_external_delegate.cc
+++ components/autofill/core/browser/autofill_external_delegate.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/field_type_utils.h"
 #include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/filling_product.h"
-#include "components/autofill/core/browser/metrics/address_rewriter_in_profile_subset_metrics.h"
 #include "components/autofill/core/browser/metrics/autofill_metrics.h"
 #include "components/autofill/core/browser/metrics/granular_filling_metrics.h"
 #include "components/autofill/core/browser/metrics/log_event.h"
@@ -704,8 +703,6 @@ void AutofillExternalDelegate::DidAcceptSuggestion(
       autofill_metrics::LogFillingMethodUsed(
           FillingMethod::kFullForm, FillingProduct::kAddress,
           /*triggering_field_type_matches_filling_product=*/true);
-      autofill_metrics::LogUserAcceptedPreviouslyHiddenProfileSuggestion(
-          suggestion.hidden_prior_to_address_rewriter_usage);
       FillAutofillFormData(
           suggestion.popup_item_id,
           suggestion.GetPayload<Suggestion::BackendId>(), /*is_preview=*/false,
--- components/autofill/core/browser/data_model/autofill_profile.cc
+++ components/autofill/core/browser/data_model/autofill_profile.cc
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/geo/autofill_country.h"
 #include "components/autofill/core/browser/geo/phone_number_i18n.h"
 #include "components/autofill/core/browser/geo/state_names.h"
-#include "components/autofill/core/browser/metrics/address_rewriter_in_profile_subset_metrics.h"
 #include "components/autofill/core/browser/metrics/autofill_metrics.h"
 #include "components/autofill/core/browser/profile_token_quality.h"
 #include "components/autofill/core/browser/validation.h"
