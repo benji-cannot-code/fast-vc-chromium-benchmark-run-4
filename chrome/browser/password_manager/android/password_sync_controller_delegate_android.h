c2f5bad2c7f521f06fc4e375a62f7026d0c6afa8
Karol Sygiet <sygiet@google.com>
2024-03-18 17:09:18 +0000

    [UPM] Split PasswordSyncControllerDelegate
    This CL extracts logic relevant for the `ModelTypeControllerDelegate`
    to the new class and uses it to get `ProxyModelTypeControllerDelegate`
    in `PasswordStoreAndroidAccountBackend`. There should be no behavioural
    changes and this CL is purely refactoring that's needed for the
    followup.
    
    In the followup CL an existing flag
    `kUnifiedPasswordManagerSyncUsingAndroidBackendOnly` will be removed
    and all backends that used it previously will call
    `CreateSyncControllerDelegate()` on the active backend directly. Then
    the `PasswordStoreBuiltInBackend` will check for the
    `kUnifiedPasswordManagerSyncOnlyInGMSCore` flag and if it is enabled
    the new `PasswordModelTypeControllerDelegate` will be returned. Thus
    sync events will be ignored. If the flag was disabled, the existing
    implementation will be used.
    
    Bug: 321220529
    Change-Id: I2047847c54a20f6a043461939ce727e31e7a7582
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5366300
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1274232}

--- chrome/browser/password_manager/android/password_sync_controller_delegate_android.h
+++ chrome/browser/password_manager/android/password_sync_controller_delegate_android.h
@@ -17,19 +17,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/strong_alias.h"
 #include "chrome/browser/password_manager/android/password_sync_controller_delegate_bridge.h"
 #include "components/password_manager/core/browser/password_store/password_store_backend.h"
-#include "components/sync/model/model_type_controller_delegate.h"
 #include "components/sync/service/sync_service.h"
 #include "components/sync/service/sync_service_observer.h"
 
-namespace syncer {
-class ModelTypeControllerDelegate;
-}  // namespace syncer
-
 namespace password_manager {
 
+// TODO: crbug.com/321220529 - Rename this class to indicate that it will be
+// used only for recording metrics.
 class PasswordSyncControllerDelegateAndroid
-    : public syncer::ModelTypeControllerDelegate,
-      public syncer::SyncServiceObserver,
+    : public syncer::SyncServiceObserver,
       public PasswordSyncControllerDelegateBridge::Consumer {
  public:
   using IsPwdSyncEnabled = base::StrongAlias<struct IsPwdSyncEnabledTag, bool>;
@@ -56,18 +52,6 @@ class PasswordSyncControllerDelegateAndroid
   // Sets a callback to be called when the sync service is being shut down.
   void SetSyncShutdownCallback(base::OnceClosure(on_sync_shutdown));
 
-  // syncer::ModelTypeControllerDelegate implementation
-  void OnSyncStarting(const syncer::DataTypeActivationRequest& request,
-                      StartCallback callback) override;
-  void OnSyncStopping(syncer::SyncStopMetadataFate metadata_fate) override;
-  void GetAllNodesForDebugging(AllNodesCallback callback) override;
-  void GetTypeEntitiesCountForDebugging(
-      base::OnceCallback<void(const syncer::TypeEntitiesCount&)> callback)
-      const override;
-  void RecordMemoryUsageAndCountsHistograms() override;
-  void ClearMetadataIfStopped() override;
-  void ReportBridgeErrorForTest() override;
-
   // syncer::SyncServiceObserver implementation.
   void OnStateChanged(syncer::SyncService* sync) override;
   void OnSyncShutdown(syncer::SyncService* sync) override;
@@ -77,9 +61,6 @@ class PasswordSyncControllerDelegateAndroid
   void OnCredentialManagerError(const AndroidBackendError& error,
                                 int api_error_code) override;
 
-  std::unique_ptr<syncer::ProxyModelTypeControllerDelegate>
-  CreateProxyModelControllerDelegate();
-
   // Updates |is_sync_enabled| to hold the initial sync setting.
   void OnSyncServiceInitialized(syncer::SyncService* sync_service);
 
@@ -87,8 +68,6 @@ class PasswordSyncControllerDelegateAndroid
   // Notify credential manager about current account on startup or if
   // password sync setting has changed.
   void UpdateCredentialManagerSyncStatus(syncer::SyncService* sync_service);
-
-  base::WeakPtr<syncer::ModelTypeControllerDelegate> GetWeakPtrToBaseClass();
 
   const std::unique_ptr<PasswordSyncControllerDelegateBridge> bridge_;
 
