eec501a1cd90226169b746523024e4af88938da1
Maria Kazinova <kazinova@google.com>
2024-03-18 18:17:01 +0000

    [Passwords] Exclude 1-symbol name inputs from single username flows
    Currently, Password Manager does not consider nameless fields for
    UFF voting, because a single nameless text field can be anything, and
    is likely to cause aggregation of many unrelated fields on the server.
    However, 1-symbol long inputs are also not meaningful, and cause the
    same problems.
    
    This CL:
    1) Extends the voting check in the renderer to also ignore 1-symbol
    long field names.
    2) Adds the same check in the form parser to ignore the existing
    predictions for such fields.
    
    Bug: b/329847725
    Change-Id: Ib7b90207e4a661a4c87b85a1bd81ec85a048135b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5377825
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1274298}

--- components/password_manager/core/common/password_manager_util.cc
+++ components/password_manager/core/common/password_manager_util.cc
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace password_manager::util {
 
+// The minimum length of the input name that allows considering it as potential
+// single username field.
+const size_t kMinInputNameLengthForSingleUsername = 2;
+
 bool IsRendererRecognizedCredentialForm(const autofill::FormData& form) {
   // TODO(crbug.com/1465793): Consolidate with the parsing logic in
   // form_autofill_util.cc.
@@ -30,6 +34,14 @@ bool IsRendererRecognizedCredentialForm(const autofill::FormData& form) {
 bool CanBeConsideredAsSingleUsername(const std::u16string& name,
                                      const std::u16string& id,
                                      const std::u16string& label) {
+  // Do not consider fields with very short names/ids to avoid aggregating
+  // multiple unrelated fields on the server. (crbug.com/1209143)
+  if (name.length() < kMinInputNameLengthForSingleUsername &&
+      id.length() < kMinInputNameLengthForSingleUsername) {
+    return false;
+  }
+  // Do not consider fields if their HTML attributes indicate they
+  // are search fields.
   return (name.find(password_manager::constants::kSearch) ==
           std::u16string::npos) &&
          (id.find(password_manager::constants::kSearch) ==
