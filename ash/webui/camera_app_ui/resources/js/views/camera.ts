f59c13f9207e78f0491b5c0fdbb14dcd1ad84291
Dorah <dorahkim@google.com>
2024-03-18 03:18:14 +0000

    camera: Move taking state back after preparation
    This CL catches an error when a video recording is stopped before the
    video recording is started. In some devices(i.e. kukui), stopping
    a video recording is finished earlier than starting a recording when the
    shutter is clicked so fast by pressing a space bar for long. By moving
    the taking state back after the taking preparation, we can prevent the
    race condition of stop operation is finished during taking preparation.
    
    BUG=b:328711730
    Test=By pressing a space bar in kukui/volteer
    
    Change-Id: Iafdf1d0f00975581188217ebc278459d78141569
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5369602
    Commit-Queue: Dorah Kim <dorahkim@google.com>
    Reviewed-by: Chu-Hsuan Yang <chuhsuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273983}

--- ash/webui/camera_app_ui/resources/js/views/camera.ts
+++ ash/webui/camera_app_ui/resources/js/views/camera.ts
@@ -415,7 +415,6 @@ export class Camera extends View implements CameraViewUI {
         return;
       }
 
-      state.set(state.State.TAKING, true);
       this.shutterType = shutterType;
       // Refocus the visible shutter button for ChromeVox.
       this.focusShutterButton();
@@ -435,6 +434,7 @@ export class Camera extends View implements CameraViewUI {
         // what we need to rotate the captured video with.
         this.outputVideoRotation = (360 - cameraFrameRotation) % 360;
         await timertick.start();
+        state.set(state.State.TAKING, true);
         const [captureDone] = await this.cameraManager.startCapture();
         await captureDone;
       } catch (e) {
