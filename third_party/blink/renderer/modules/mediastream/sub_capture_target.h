d98c1133bb470f7989139a19a2051a8475371721
Elad Alon <eladalon@chromium.org>
2024-03-18 10:13:43 +0000

    Migrate Region/Element-Capture to use typed promises/resolvers
    Bug: 329702363, 329705703
    Change-Id: I4b93468fe5c6fd003959aa0fe2bb26282c103349
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5374211
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274053}

--- third_party/blink/renderer/modules/mediastream/sub_capture_target.h
+++ third_party/blink/renderer/modules/mediastream/sub_capture_target.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class MediaDevices;
 class ScriptState;
 
 // Mutual non-Web-exposed parent class for various Web-exposed tokens
@@ -29,10 +30,11 @@ class MODULES_EXPORT SubCaptureTarget : public ScriptWrappable {
   const String& GetId() const { return id_; }
 
  protected:
-  static ScriptPromise fromElement(ScriptState* script_state,
-                                   Element* element,
-                                   ExceptionState& exception_state,
-                                   Type type);
+  // Tries to get the relevant MediaDevices object.
+  // If unsuccessful, throws a relevant exception and returns nullptr.
+  static MediaDevices* GetMediaDevices(ScriptState* script_state,
+                                       Element* element,
+                                       ExceptionState& exception_state);
 
   SubCaptureTarget(Type type, String id);
 
