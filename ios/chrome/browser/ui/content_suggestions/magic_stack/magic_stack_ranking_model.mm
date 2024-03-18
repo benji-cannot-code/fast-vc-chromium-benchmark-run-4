3f1a16c48874460884fdd20ad00b81142dc81917
Chris Lu <thegreenfrog@chromium.org>
2024-03-18 21:47:39 +0000

    [ios, MagicStack] Move consumer validity check to later
    `self.consumer` is not set for the RankingModel when
    IsIOSMagicStackCollectionViewEnabled is YES, so move its check
    to after this feature code has executed.
    
    Change-Id: Ied3bfa08a98ecb28c06e9228227eb5a93e01ff63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5378429
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1274474}

--- ios/chrome/browser/ui/content_suggestions/magic_stack/magic_stack_ranking_model.mm
+++ ios/chrome/browser/ui/content_suggestions/magic_stack/magic_stack_ranking_model.mm
@@ -196,8 +196,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)tabResumptionHelperDidReceiveItem {
   CHECK(IsTabResumptionEnabled());
-  if (!self.consumer ||
-      tab_resumption_prefs::IsTabResumptionDisabled(_localState)) {
+  if (tab_resumption_prefs::IsTabResumptionDisabled(_localState)) {
     return;
   }
 
@@ -205,8 +204,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)tabResumptionHelperDidReplaceItem:(TabResumptionItem*)oldItem {
-  if (!self.consumer ||
-      tab_resumption_prefs::IsTabResumptionDisabled(_localState)) {
+  if (tab_resumption_prefs::IsTabResumptionDisabled(_localState)) {
     return;
   }
 
@@ -577,6 +575,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return;
   }
 
+  if (!self.consumer) {
+    return;
+  }
   _latestMagicStackOrder =
       base::FeatureList::IsEnabled(
           segmentation_platform::features::kSegmentationPlatformIosModuleRanker)
