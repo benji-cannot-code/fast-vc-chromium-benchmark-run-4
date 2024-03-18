64c5de03ab7d4c171dcb8b6d7476a94f52811125
Sammie Quon <sammiequon@chromium.org>
2024-03-18 23:23:06 +0000

    pine: Save and display a temporary title for non browser apps
    App titles are fetched via appIDs from the app service. Sometimes, the
    app service is not ready when pine is created. This CL makes it so we
    save the last session's title into full restore file, and uses that
    temporarily.
    
    In future CLs, we will monitor when the app service is ready and
    replace the temporary title with the one fetched from app service. In
    most cases, it should be the same anyhow.
    
    Bug: b:328830102
    Test: manual
    Change-Id: I7ce9ad14c7f079f7668f72a4e681ee5e35f36a60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5375413
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274544}

--- ash/wm/desks/templates/saved_desk_icon_container.cc
+++ ash/wm/desks/templates/saved_desk_icon_container.cc
@@ -14,11 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/window_properties.h"
 #include "ash/shell.h"
 #include "ash/wm/desks/templates/saved_desk_constants.h"
+#include "ash/wm/window_restore/window_restore_util.h"
 #include "base/check.h"
 #include "base/containers/contains.h"
 #include "base/memory/raw_ptr.h"
 #include "base/ranges/algorithm.h"
-#include "components/app_constants/constants.h"
 #include "components/app_restore/app_restore_utils.h"
 #include "ui/accessibility/ax_enums.mojom-shared.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
@@ -28,11 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 namespace {
-
-bool IsBrowserAppId(const std::string& app_id) {
-  return app_id == app_constants::kChromeAppId ||
-         app_id == app_constants::kLacrosAppId;
-}
 
 // Given a map of unique icon identifiers to icon info, returns a vector of the
 // same key, value pair ordered by icons' activation index.
