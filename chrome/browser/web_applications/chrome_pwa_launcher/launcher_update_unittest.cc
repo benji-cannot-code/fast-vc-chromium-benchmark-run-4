41a2c6e1e86216dfa4e8340104f7e96c1e7daa58
Helmut Januschka <helmut@januschka.com>
2024-03-18 18:06:10 +0000

    chrome_pwa_launcher: base::StringPiece -> std::string_view
    Bug: 691162
    Change-Id: I27f2d2af63dafff8396fab5fb6ff20a0d3f7284c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5374117
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Auto-Submit: Helmut Januschka <helmut@januschka.com>
    Reviewed-by: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1274284}

--- chrome/browser/web_applications/chrome_pwa_launcher/launcher_update_unittest.cc
+++ chrome/browser/web_applications/chrome_pwa_launcher/launcher_update_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/web_applications/chrome_pwa_launcher/launcher_update.h"
 
+#include <string_view>
 #include <vector>
 
 #include "base/files/file_util.h"
@@ -18,7 +19,7 @@ namespace web_app {
 namespace {
 
 void CreateFileAndWriteData(const base::FilePath& file_path,
-                            const base::StringPiece& data) {
+                            std::string_view data) {
   ASSERT_TRUE(base::WriteFile(file_path, data));
 }
 
