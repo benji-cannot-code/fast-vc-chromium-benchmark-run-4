226dc0085d9c400f65d6567c81682704d3d67110
rudranshd <rudranshd@google.com>
2024-03-18 18:48:45 +0000

    [Instant Hotspot] Rename tether to hotspot on multidevice page
    Renames Instant Tethering to Instant hotspot on the multidevice page
    when the instant-hotspot-rebrand feature flag is enabled.
    
    Bug: b/290074777
    Screenshot: https://screenshot.googleplex.com/59mJZQEarpFTo4k.png
    
    Change-Id: Icae6273fd269c3f4d5479113b277b715ba2dab24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5333381
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Commit-Queue: Rudransh Dikshit <rudranshd@google.com>
    Reviewed-by: Joe Antonetti <joeantonetti@google.com>
    Cr-Commit-Position: refs/heads/main@{#1274331}

--- chrome/browser/resources/ash/settings/multidevice_page/multidevice_feature_mixin.ts
+++ chrome/browser/resources/ash/settings/multidevice_page/multidevice_feature_mixin.ts
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * intended to facilitate passing data between elements in the MultiDevice page
  * cleanly and concisely. It includes some constants and utility methods.
  */
-
 import {I18nMixin, I18nMixinInterface} from 'chrome://resources/ash/common/cr_elements/i18n_mixin.js';
+import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {dedupingMixin, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {isRevampWayfindingEnabled} from '../common/load_time_booleans.js';
@@ -83,6 +83,13 @@ export const MultiDeviceFeatureMixin = dedupingMixin(
           return !!this.pageContentData &&
               this.pageContentData.betterTogetherState !==
               MultiDeviceFeatureState.PROHIBITED_BY_POLICY;
+        }
+
+        /**
+         * Whether the instant hotspot rebrand feature flag is enabled
+         */
+        isInstantHotspotRebrandEnabled(): boolean {
+          return loadTimeData.getBoolean('isInstantHotspotRebrandEnabled');
         }
 
         /**
@@ -215,7 +222,9 @@ export const MultiDeviceFeatureMixin = dedupingMixin(
             case MultiDeviceFeature.BETTER_TOGETHER_SUITE:
               return this.i18n('multideviceSetupItemHeading');
             case MultiDeviceFeature.INSTANT_TETHERING:
-              return this.i18n('multideviceInstantTetheringItemTitle');
+              return this.isInstantHotspotRebrandEnabled() ?
+                  this.i18n('multideviceInstantHotspotItemTitle') :
+                  this.i18n('multideviceInstantTetheringItemTitle');
             case MultiDeviceFeature.SMART_LOCK:
               return this.i18n('multideviceSmartLockItemTitle');
             case MultiDeviceFeature.PHONE_HUB:
