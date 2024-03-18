13fd20a621f5ebd2a2199dd2dc73817997b21aa5
Alex Gough <ajgo@chromium.org>
2024-03-18 18:51:53 +0000

    Reland "Allow child processes to inherit logging file handle on Windows"
    This is a reland of commit b80a478f9f8bff1fb9001d249ca8c09105fd3405
    
    Previously win_asan failed ContentBrowserTest.RendererCrashCallStack
    as the stack was not being sent out on stderr. The Sanitizer
    machinery in llvm only allows a single output handle to be provided
    and the change caused a file rather than stderr to be used. This
    is fixed by adding `--enable-logging=stderr` as an option that
    content_shell understands (it does the same thing as chrome, and only
    logs to stderr and any internal debuglog). Passing this argument from
    the test launcher now allows the test to pass.
    
    Original change's description:
    > Allow child processes to inherit logging file handle on Windows
    >
    > Child processes on Windows cannot always open their log file
    > (for instance if they cannot determine the user-data-dir) making
    > configuration of logging complicated. Child processes also need
    > a sandbox exception to open their log files.
    >
    > This CL replaces logging related flags to pass a handle to children
    > in cases where a file would have been passed, allowing a later CL
    > to remove the sandbox rule allowing direct access to the log file.
    >
    > If the browser is run with `--enable-logging --log-file=file`,
    > children will now be run with `--enable-logging=handle --log-file=N`.
    > It is not possible to mix stderr logging with file logging (it will
    > still work for the browser process), but this already does not work
    > in official builds as std handles are not passed into children.
    >
    > This also allows the CHROME_LOG_FILE to be useful again for renderer
    > processes if Chrome is run using:
    >
    >  $env:CHROME_LOG_FILE="d:\temp\env-log.log"
    >  chrome.exe --enable-logging
    >
    > the browser will inspect the env var and provide a handle to that
    > file when starting renderer processes.
    >
    > Running the browser with --enable-logging=handle directly is not
    > supported, these flags should only be provided by the browser when
    > starting child processes.
    >
    > Tests: Manually tested results https://docs.google.com/document/d/1Hq37YReGM91fmcCcqkRbjtjwP62FVH_zpZtfpSPlLeY/edit?usp=sharing
    > Bug: 328285906, 40270808
    > Change-Id: Id79731b2d35ab3ee58f6c1612990bdec1485da68
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5337202
    > Commit-Queue: Alex Gough <ajgo@chromium.org>
    > Reviewed-by: Greg Thompson <grt@chromium.org>
    > Reviewed-by: Peter Boström <pbos@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1272538}
    
    Bug: 328285906, 40270808
    Change-Id: I4a4f994e8afa22da84c81c02e9e786b94eec9aad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5373414
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274336}

--- base/logging.h
+++ base/logging.h
@@ -29,6 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstdio>
 #endif
 
+#if BUILDFLAG(IS_WIN)
+#include "base/win/windows_types.h"
+#endif
+
 //
 // Optional message capabilities
 // -----------------------------
@@ -189,6 +193,7 @@ using LoggingDestination = uint32_t;
 // with bitwise OR.
 // Unless destination is LOG_NONE, all logs with severity ERROR and above will
 // be written to stderr in addition to the specified destination.
+// LOG_TO_FILE includes logging to externally-provided file handles.
 enum : uint32_t {
   LOG_NONE = 0,
   LOG_TO_FILE = 1 << 0,
@@ -248,6 +253,13 @@ struct BASE_EXPORT LoggingSettings {
   raw_ptr<FILE> log_file = nullptr;
   // ChromeOS uses the syslog log format by default.
   LogFormat log_format = LogFormat::LOG_FORMAT_SYSLOG;
+#endif
+#if BUILDFLAG(IS_WIN)
+  // Contains an optional file that logs should be written to. If present,
+  // `log_file_path` will be ignored, and the logging system will take ownership
+  // of the HANDLE. If there's an error writing to this file, no fallback paths
+  // will be opened.
+  HANDLE log_file = nullptr;
 #endif
 };
 
@@ -756,6 +768,9 @@ BASE_EXPORT bool IsLoggingToFileEnabled();
 
 // Returns the default log file path.
 BASE_EXPORT std::wstring GetLogFileFullPath();
+
+// Duplicates the log file handle to send into a child process.
+BASE_EXPORT HANDLE DuplicateLogFileHandle();
 #endif
 
 }  // namespace logging
