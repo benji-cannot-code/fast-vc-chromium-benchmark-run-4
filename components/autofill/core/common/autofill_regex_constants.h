3371c377e4cd6209088b17cda96b751ffc0b7f6e
Dominic Battre <battre@chromium.org>
2024-03-18 16:27:32 +0000

    Recognize xxxx xxxx xxxx xxxx as a credit card number field
    This CL extends the regex for credit card number fields to also
    recognize placeholders like xxxx xxxx xxxx xxxx.
    
    The pattern is wrapped in ^ $, to reduce false positives.
    Otherwise any sequence of 16 'x' would be classified as a CC
    number. Note, however, that also CreditCardFieldParser::Parse
    imposes a bunch of constraints for delivering a CC number.
    
    Bug: 40279279
    Change-Id: I1d4a764400b7129e59917390afe937640c81c6d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372617
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Auto-Submit: Dominic Battre <battre@chromium.org>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1274206}

--- components/autofill/core/common/autofill_regex_constants.h
+++ components/autofill/core/common/autofill_regex_constants.h
@@ -280,7 +280,8 @@ inline constexpr char16_t kCardNumberRe[] =
     // E.g. "número de (?:la )?tarjeta" in es-MX, "número do cartão" in pt-BR
     u"|(numero|número|numéro)(?!.*(document|fono|phone|réservation))"
     u"|0000 ?0000 ?0000 ?0000"
-    u"|1234 ?1234 ?1234 ?1234";
+    u"|1234 ?1234 ?1234 ?1234"
+    u"|^xxxx ?xxxx ?xxxx ?xxxx$";
 
 inline constexpr char16_t kCardCvcRe[] =
     u"verification|card.?identification|security.?code|card.?code"
