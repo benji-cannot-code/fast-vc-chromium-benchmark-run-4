b41c4b39c459d38b4e16e026a2c5760024814226
Darren Shen <shend@chromium.org>
2024-03-18 07:06:51 +0000

    Paste instead of insert for clipboard data.
    Pasting works differently as paste doesn't require a focused input
    field. So to preserve the same behavior, use paste when inserting
    a clipboard data.
    
    Bug: b/329309518
    Change-Id: I28eebbfd8be1ed0e1c85fb7a241dfe46a3ce33f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5366795
    Reviewed-by: Jing Wang <jiwan@chromium.org>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274012}

--- ash/picker/picker_controller.h
+++ ash/picker/picker_controller.h
@@ -31,6 +31,7 @@ namespace ash {
 class PickerAssetFetcher;
 class PickerClient;
 class PickerInsertMediaRequest;
+class PickerPasteRequest;
 class PickerSearchController;
 class PickerSearchResult;
 
@@ -99,6 +100,7 @@ class ASH_EXPORT PickerController
   views::UniqueWidgetPtr widget_;
   std::unique_ptr<PickerAssetFetcher> asset_fetcher_;
   std::unique_ptr<PickerInsertMediaRequest> insert_media_request_;
+  std::unique_ptr<PickerPasteRequest> paste_request_;
   std::unique_ptr<PickerSearchController> search_controller_;
 
   // Periodically records usage metrics based on the Standard Feature Usage
