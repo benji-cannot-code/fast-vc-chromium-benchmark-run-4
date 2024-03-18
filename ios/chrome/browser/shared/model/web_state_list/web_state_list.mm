cc49508532c7018e7ae359131bc4895cfed8d6ca
Louis Romero <lpromero@google.com>
2024-03-18 12:45:35 +0000

    Minor clean up in WebStateList
    Bug: none
    Change-Id: I7151aad263b25a472d8d69a3455cc2dcc34fbfbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372488
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274090}

--- ios/chrome/browser/shared/model/web_state_list/web_state_list.mm
+++ ios/chrome/browser/shared/model/web_state_list/web_state_list.mm
@@ -867,7 +867,7 @@ void WebStateList::RemoveFromGroupsImpl(const std::set<int>& indices) {
     const TabGroup* group = GetGroupOfWebStateAt(index);
     if (group) {
       const int to_index = GetGroupRange(group).range_end() - 1;
-      MoveWebStateWrapperAt(*it, to_index, /*pinned=*/false,
+      MoveWebStateWrapperAt(index, to_index, /*pinned=*/false,
                             /*new_group=*/nullptr);
     }
   }
@@ -876,6 +876,7 @@ void WebStateList::RemoveFromGroupsImpl(const std::set<int>& indices) {
 void WebStateList::DeleteGroupImpl(const TabGroup* group) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(locked_);
+  DCHECK(group);
 
   for (int index : GetGroupRange(group)) {
     MoveWebStateWrapperAt(index, index, /*pinned=*/false,
@@ -966,11 +967,9 @@ void WebStateList::MoveWebStateWrapperAt(int from_index,
   web_state_wrappers_[from_index]->SetGroup(new_group);
 
   // Update the groups ranges.
-  for (auto group_it = begin(groups_); group_it != end(groups_); ++group_it) {
-    const TabGroup* group = group_it->first.get();
-    Range& group_range = group_it->second;
+  for (auto& [group, group_range] : groups_) {
     // Remove the item from the old group.
-    if (group == old_group) {
+    if (group.get() == old_group) {
       group_range.ContractRight();
     }
     // Slide all groups after the removed tab to the left.
@@ -978,7 +977,7 @@ void WebStateList::MoveWebStateWrapperAt(int from_index,
       group_range.MoveLeft();
     }
     // Add the item to the new group.
-    if (group == new_group) {
+    if (group.get() == new_group) {
       group_range.ExpandRight();
     } else if (to_index <= group_range.range_begin()) {
       // Slide all groups at or after the added tab to the right.
@@ -1006,7 +1005,7 @@ void WebStateList::MoveWebStateWrapperAt(int from_index,
   }
 
   // Notify the observers of the change.
-  // The moves didn't change the active WebState.
+  // The move didn't change the active WebState.
   web::WebState* const active_web_state = GetActiveWebState();
   const WebStateListStatus status = {.old_active_web_state = active_web_state,
                                      .new_active_web_state = active_web_state};
