5e46cadfeb3032547642aa3506edc34301dee550
Slobodan Pejic <slobodan@chromium.org>
2024-03-18 22:30:11 +0000

    Implement card unmask authentication selection mediator.
    This change adds the CardUnmaskAuthenticationSelectionConsumer protocol
    and implements the corresponding mediator. The challenge options are
    passed to the consumer, so a new Objective-C class has been added
    corresponding to the similar autofill C++ type.
    
    Bug: 40282545, b/303715687
    Change-Id: Ieaa0aa069272a54e834e643604da3fab6a2413be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5332513
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274504}

--- components/autofill/core/browser/ui/payments/card_unmask_authentication_selection_dialog_controller_impl.cc
+++ components/autofill/core/browser/ui/payments/card_unmask_authentication_selection_dialog_controller_impl.cc
@@ -258,4 +258,9 @@ void CardUnmaskAuthenticationSelectionDialogControllerImpl::
   selected_challenge_option_id_ = selected_challenge_option_id;
 }
 
+base::WeakPtr<CardUnmaskAuthenticationSelectionDialogControllerImpl>
+CardUnmaskAuthenticationSelectionDialogControllerImpl::GetWeakPtr() {
+  return weak_ptr_factory_.GetWeakPtr();
+}
+
 }  // namespace autofill
--- ios/chrome/browser/ui/autofill/authentication/card_unmask_authentication_selection_coordinator.mm
+++ ios/chrome/browser/ui/autofill/authentication/card_unmask_authentication_selection_coordinator.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/autofill/model/bottom_sheet/autofill_bottom_sheet_tab_helper.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
+#import "ios/chrome/browser/ui/autofill/authentication/card_unmask_authentication_selection_mediator.h"
 
 @implementation CardUnmaskAuthenticationSelectionCoordinator {
   // A reference to the base view controller with UINavigationController type.
@@ -24,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   std::unique_ptr<
       autofill::CardUnmaskAuthenticationSelectionDialogControllerImpl>
       _modelController;
+
+  std::unique_ptr<CardUnmaskAuthenticationSelectionMediator> _mediator;
 }
 
 - (instancetype)initWithBaseNavigationController:
@@ -46,6 +49,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // TODO(crbug.com/40282545): Remove placeholder view and implement card
   // unmask authentication.
   UIViewController* selectionViewController = [[UIViewController alloc] init];
+  _mediator = std::make_unique<CardUnmaskAuthenticationSelectionMediator>(
+      _modelController->GetWeakPtr(),
+      /*consumer=*/nil);
   selectionViewController.view.backgroundColor = [UIColor redColor];
   selectionViewController.title = @"TODO";
   _selectionViewController = selectionViewController;
