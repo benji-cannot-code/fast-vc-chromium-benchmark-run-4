a15b3a8279f16546c78c9dc5e2ed648f1846aa9f
Justin Donnelly <jdonnelly@chromium.org>
2024-03-19 02:36:55 +0000

    Rename the cr_components omnibox/ directories to searchbox/.
    More precisely:
    ui/webui/resources/cr_components/omnibox/
    ->
    ui/webui/resources/cr_components/searchbox/
    and
    chrome/test/data/webui/cr_components/omnibox/
    ->
    chrome/test/data/webui/cr_components/searchbox/
    
    Lots of things will have to change to generalize the concept of
    omnibox/realbox to a "searchbox" useful in new scenarios. Which will
    have to happen over time as it doesn't make sense to stop and change the
    naming everywhere first. But this directory stands out to as having a
    particularly confusing name when trying to use a searchbox in a new
    context.
    
    This CL also makes some changes to realbox_test.ts. The tests in this file have been flaky for a while. The failures are almost entirely in the areMatchesShowing() check. A change (crrev.com/c/4070784) to try to force the UI to update synchronously in this method may have helped. But a more recent change (crrev.com/c/5354009) to add async waits seems to me to be trying to solve the same problem in a better way. So I've removed the synchronous update here. Anecdotally, this seems to have produced fewer flakes, though I may just be getting lucky. Additionally, this cleans up some leftover comments.
    
    Bug: b:328293795, 328827188, 328650182
    Change-Id: Iadbd5b6c68d6b6f3810ebeeecbaabe423f81b451
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5371453
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274658}

--- chrome/browser/resources/new_tab_page/new_tab_page.ts
+++ chrome/browser/resources/new_tab_page/new_tab_page.ts
@@ -10,10 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * things tests need.
  */
 
-export {RealboxElement} from 'chrome://resources/cr_components/omnibox/realbox.js';
-export {RealboxBrowserProxy} from 'chrome://resources/cr_components/omnibox/realbox_browser_proxy.js';
-export {RealboxIconElement} from 'chrome://resources/cr_components/omnibox/realbox_icon.js';
-export {RealboxMatchElement} from 'chrome://resources/cr_components/omnibox/realbox_match.js';
+export {RealboxElement} from 'chrome://resources/cr_components/searchbox/realbox.js';
+export {RealboxBrowserProxy} from 'chrome://resources/cr_components/searchbox/realbox_browser_proxy.js';
+export {RealboxIconElement} from 'chrome://resources/cr_components/searchbox/realbox_icon.js';
+export {RealboxMatchElement} from 'chrome://resources/cr_components/searchbox/realbox_match.js';
 export {CrAutoImgElement} from 'chrome://resources/cr_elements/cr_auto_img/cr_auto_img.js';
 export {BrowserCommandProxy} from 'chrome://resources/js/browser_command/browser_command_proxy.js';
 export {BrowserProxyImpl} from 'chrome://resources/js/metrics_reporter/browser_proxy.js';
--- chrome/test/data/webui/cr_components/searchbox/realbox_match_test.ts
+++ chrome/test/data/webui/cr_components/searchbox/realbox_match_test.ts
@@ -4,11 +4,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://new-tab-page/strings.m.js';
-import 'chrome://resources/cr_components/omnibox/realbox_match.js';
+import 'chrome://resources/cr_components/searchbox/realbox_match.js';
 
-import {NavigationPredictor} from 'chrome://resources/cr_components/omnibox/omnibox.mojom-webui.js';
-import {RealboxBrowserProxy} from 'chrome://resources/cr_components/omnibox/realbox_browser_proxy.js';
-import type {RealboxMatchElement} from 'chrome://resources/cr_components/omnibox/realbox_match.js';
+import {NavigationPredictor} from 'chrome://resources/cr_components/searchbox/omnibox.mojom-webui.js';
+import {RealboxBrowserProxy} from 'chrome://resources/cr_components/searchbox/realbox_browser_proxy.js';
+import type {RealboxMatchElement} from 'chrome://resources/cr_components/searchbox/realbox_match.js';
 import {assertDeepEquals, assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
 
