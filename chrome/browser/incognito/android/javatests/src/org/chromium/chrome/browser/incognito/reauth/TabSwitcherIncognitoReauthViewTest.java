02813a4afaa925aab135ff93c3dad0605b072d74
Ben Joyce <bjoyce@google.com>
2024-03-19 17:50:12 +0000

    Fix flakiness in TabSwitcherIncognitoReauthViewTest
    Flakiness is caused by espresso 3.2 running on api 30+. This ensures the
    dialog is in focus.
    
    Bug: 40234615
    Change-Id: Ia8ef620078b71f8b43486b5ca66bc0f36c55d28c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5379082
    Commit-Queue: Benjamin Joyce (Ben) <bjoyce@chromium.org>
    Auto-Submit: Benjamin Joyce (Ben) <bjoyce@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275044}

--- chrome/browser/incognito/android/javatests/src/org/chromium/chrome/browser/incognito/reauth/TabSwitcherIncognitoReauthViewTest.java
+++ chrome/browser/incognito/android/javatests/src/org/chromium/chrome/browser/incognito/reauth/TabSwitcherIncognitoReauthViewTest.java
@@ -205,7 +205,9 @@ public class TabSwitcherIncognitoReauthViewTest {
 
         mActivityTestRule.recreateActivity();
 
-        onViewWaiting(withId(R.id.incognito_reauth_unlock_incognito_button))
+        onViewWaiting(
+                        withId(R.id.incognito_reauth_unlock_incognito_button),
+                        true) // checkRootDialog=true ensures dialog is in focus, avoids flakiness.
                 .check(matches(isDisplayed()));
     }
 }
