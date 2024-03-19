03e6aad89c6b53af73e16d73cbdc981da29e4620
Mohamed Heikal <mheikal@chromium.org>
2024-03-19 18:27:25 +0000

    [Jni Zero] Fix bug with nested classes placeholders
    Also adds a regression test/golden.
    
    Change-Id: I8f1209926cc7a447688fe60682cca8e18f998e5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5371777
    Auto-Submit: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275074}

--- third_party/jni_zero/test/java/src/org/dummy/MyClass.java
+++ third_party/jni_zero/test/java/src/org/dummy/MyClass.java
@@ -5,4 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.dummy;
 
-public class MyClass<T> {}
+public class MyClass<T> {
+    public static interface FirstNestedInterface {}
+
+    public static interface SecondNestedInterface {}
+}
--- third_party/jni_zero/test/java/src/org/jni_zero/SampleForAnnotationProcessor.java
+++ third_party/jni_zero/test/java/src/org/jni_zero/SampleForAnnotationProcessor.java
@@ -8,6 +8,8 @@ import android.content.Context;
 import android.view.View;
 
 import org.dummy.MyClass;
+import org.dummy.MyClass.FirstNestedInterface;
+import org.dummy.MyClass.SecondNestedInterface;
 import org.dummy.MyInterface;
 
 import java.util.ArrayList;
@@ -104,6 +106,8 @@ class SampleForAnnotationProcessor {
         View view = null;
         Context context = null;
         MyClass myclass = null;
+        FirstNestedInterface nested1 = null;
+        SecondNestedInterface nested2 = null;
         MyInterface myinterface = null;
         ArrayList myList = new ArrayList<String>();
 
