da016063ca806f42545c7883419fa305ac537d64
Gauthier Ambard <gambard@chromium.org>
2024-03-19 08:27:26 +0000

    [iOS] Fix TabGroup colors utils
    This CL makes sure that the TabGroup color utils are using the correct
    colours and add a util to get the next colour to be used in the
    WebStateList.
    
    Bug: 326198347, 326198649, 326199041
    Change-Id: I0234b1669764c823f2207403c5439bc78191d731
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372502
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274760}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/create_tab_group_mediator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/create_tab_group_mediator.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/web_state_list/tab_group.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
+#import "ios/chrome/browser/ui/tab_switcher/group_utils.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_group_creation_consumer.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_switcher_item.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_utils.h"
@@ -45,8 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     CHECK(!identifiers.empty()) << "Cannot create an empty tab group.";
     CHECK(webStateList);
     _consumer = consumer;
-    // TODO(crbug.com/1501837): Get the default color from the component.
-    [_consumer setDefaultGroupColor:tab_groups::TabGroupColorId::kPink];
+    [_consumer setDefaultGroupColor:DefaultColorForNewTabGroup(webStateList)];
 
     _identifiers = identifiers;
     _webStateList = webStateList;
