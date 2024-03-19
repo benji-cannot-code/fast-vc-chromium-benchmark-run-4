6a0f9f9f46fbf946e04fc98c50703be04711c146
Darwin Yang <darwinyang@google.com>
2024-03-19 14:30:25 +0000

    [autofill][desktop] Handle Confirmation Bubble on Unfocused Tab
    Update the SaveCardBubbleControllerImpl so that the confirmation bubble
    does not show up if the server upload completes but the user is on
    a different tab (from the tab where they accepted the server card
    upload). Also added a change where when returning to the original tab,
    the confirmation bubble is automatically shown instead of just the icon.
    
    Bug: 1499264,b/320513154
    Change-Id: If98b6dca52940601f141fdfc1945441363893e98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5371970
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Kavita Soni <kavitasoni@chromium.org>
    Commit-Queue: Darwin Yang <darwinyang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274897}

--- chrome/browser/ui/autofill/payments/save_card_bubble_controller_impl.cc
+++ chrome/browser/ui/autofill/payments/save_card_bubble_controller_impl.cc
@@ -696,8 +696,9 @@ void SaveCardBubbleControllerImpl::OnVisibilityChanged(
     content::Visibility visibility) {
   if (base::FeatureList::IsEnabled(
           features::kAutofillEnableSaveCardLoadingAndConfirmation)) {
-    if (visibility == content::Visibility::VISIBLE && !bubble_view() &&
-        was_url_opened_) {
+    if (visibility == content::Visibility::VISIBLE &&
+        (was_url_opened_ ||
+         current_bubble_type_ == BubbleType::UPLOAD_COMPLETED)) {
       ReshowBubble(/*is_user_gesture=*/false);
     } else if (visibility == content::Visibility::HIDDEN) {
       HideBubble();
@@ -713,6 +714,10 @@ PageActionIconType SaveCardBubbleControllerImpl::GetPageActionIconType() {
 }
 
 void SaveCardBubbleControllerImpl::DoShowBubble() {
+  if (!IsWebContentsActive()) {
+    return;
+  }
+
   Browser* browser = chrome::FindBrowserWithTab(web_contents());
   if (current_bubble_type_ == BubbleType::UPLOAD_COMPLETED) {
     set_bubble_view(browser->window()
@@ -760,6 +765,11 @@ void SaveCardBubbleControllerImpl::DoShowBubble() {
     case BubbleType::INACTIVE:
       NOTREACHED();
   }
+}
+
+security_state::SecurityLevel SaveCardBubbleControllerImpl::GetSecurityLevel()
+    const {
+  return security_level_;
 }
 
 void SaveCardBubbleControllerImpl::ShowBubble() {
@@ -829,9 +839,11 @@ void SaveCardBubbleControllerImpl::OpenUrl(const GURL& url) {
       ui::PAGE_TRANSITION_LINK, false));
 }
 
-security_state::SecurityLevel SaveCardBubbleControllerImpl::GetSecurityLevel()
-    const {
-  return security_level_;
+bool SaveCardBubbleControllerImpl::IsWebContentsActive() {
+  Browser* active_browser = chrome::FindBrowserWithActiveWindow();
+  return active_browser &&
+         active_browser->tab_strip_model()->GetActiveWebContents() ==
+             web_contents();
 }
 
 WEB_CONTENTS_USER_DATA_KEY_IMPL(SaveCardBubbleControllerImpl);
--- chrome/browser/ui/autofill/payments/save_card_bubble_controller_impl_browsertest.cc
+++ chrome/browser/ui/autofill/payments/save_card_bubble_controller_impl_browsertest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/test/test_browser_dialog.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/interactive_test_utils.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/browser/payments/legal_message_line.h"
@@ -60,6 +61,9 @@ class SaveCardBubbleControllerImplTest : public DialogBrowserTest {
 
   // DialogBrowserTest:
   void ShowUi(const std::string& name) override {
+    // Ensure that the browser window is active.
+    CHECK(ui_test_utils::BringBrowserWindowToFront(browser()));
+
     content::WebContents* web_contents =
         browser()->tab_strip_model()->GetActiveWebContents();
 
