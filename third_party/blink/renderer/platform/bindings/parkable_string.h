3ef5f3c3f2b52a2ecaeb99a7012ad951a98c01fa
Benoît Lizé <lizeb@chromium.org>
2024-03-19 15:25:41 +0000

    [blink/bindings] Add ZSTD as a compression algorithm for ParkableString
    ParkableString defaults to zlib as its compression algorithm. To improve
    (de)compression latency, this CL adds ZSTD as a compression algorithnm. Based on
    benchmark results at https://github.com/facebook/zstd, this should be both
    smaller, and several times faster than zlib. This is confirmed by local
    benchmarks on x86_64.
    
    This is gated by a feature flag. However, Chromium currently only include the
    decompression side of zstd, and thus this CL adds compression targets as
    well. This has a binary cost, which is ~200kiB on ARM32 Android, and ~400kiB on
    ARM64. Since ARM32 Android (and Fuchsia) are platforms where binary size is at a
    premium, this CL doesn't add the target on these platforms.
    
    Change-Id: I255599c721facabc8a2a8e216c793f55eea9a210
    Bug: 329824817
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5366061
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274936}

--- third_party/blink/renderer/platform/bindings/parkable_string.h
+++ third_party/blink/renderer/platform/bindings/parkable_string.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "base/thread_annotations.h"
 #include "base/time/time.h"
+#include "third_party/blink/renderer/platform/bindings/buildflags.h"
 #include "third_party/blink/renderer/platform/disk_data_metadata.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
@@ -51,6 +52,13 @@ class PLATFORM_EXPORT ParkableStringImpl
     kNonTransientFailure
   };
   enum class Age { kYoung = 0, kOld = 1, kVeryOld = 2 };
+  enum class CompressionAlgorithm {
+    kZlib = 0,
+    kSnappy = 1,
+#if BUILDFLAG(HAS_ZSTD_COMPRESSION)
+    kZstd = 2
+#endif
+  };
 
   constexpr static size_t kDigestSize = 32;  // SHA256.
   using SecureDigest = Vector<uint8_t, kDigestSize>;
@@ -67,6 +75,8 @@ class PLATFORM_EXPORT ParkableStringImpl
   static scoped_refptr<ParkableStringImpl> MakeParkable(
       scoped_refptr<StringImpl>&& impl,
       std::unique_ptr<SecureDigest> digest);
+
+  static CompressionAlgorithm GetCompressionAlgorithm();
 
   ParkableStringImpl(const ParkableStringImpl&) = delete;
   ParkableStringImpl& operator=(const ParkableStringImpl&) = delete;
