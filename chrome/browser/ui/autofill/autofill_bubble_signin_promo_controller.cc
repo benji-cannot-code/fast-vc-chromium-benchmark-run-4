554b498bda35d3dd944caba100135fe7f7a570cf
Amelie Schneider <amelies@google.com>
2024-03-19 16:53:50 +0000

    [UNO] Create Autofill Bubble Signin Promo View & Controller
    Adds the `AutofillBubbleSignInPromoController` and
    `AutofillBubbleSignInPromoView` which will be needed by
    https://chromium-review.googlesource.com/c/chromium/src/+/5231400.
    
    The files do not have a recent license because they are mostly
    resurrected from https://chromium-review.googlesource.com/c/chromium/src/+/2020944.
    
    Mocks: https://www.figma.com/file/FcprHAZ22sMAcdeOedunX1?node-id=11558:290031
    Bug: b:319411476
    
    Change-Id: I0508a72393de654568ebfa73696d19fa36e7bbd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5349098
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Amelie Schneider <amelies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1274997}

---
+++ chrome/browser/ui/autofill/autofill_bubble_signin_promo_controller.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/autofill/autofill_bubble_signin_promo_controller.h"
+
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/passwords/passwords_model_delegate.h"
+#include "components/password_manager/core/common/password_manager_pref_names.h"
+#include "components/prefs/pref_service.h"
+#include "content/public/browser/web_contents.h"
+
+namespace autofill {
+
+AutofillBubbleSignInPromoController::AutofillBubbleSignInPromoController(
+    base::WeakPtr<PasswordsModelDelegate> delegate)
+    : delegate_(std::move(delegate)) {}
+
+AutofillBubbleSignInPromoController::~AutofillBubbleSignInPromoController() =
+    default;
+
+void AutofillBubbleSignInPromoController::OnSignInToChromeClicked(
+    const AccountInfo& account) {
+  // Signing in is triggered by the user interacting with the sign-in promo.
+  if (delegate_) {
+    delegate_->SignIn(account);
+  }
+}
+
+}  // namespace autofill
