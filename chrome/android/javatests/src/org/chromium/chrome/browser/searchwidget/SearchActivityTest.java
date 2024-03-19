fa763210e63132e0df126fb6ce9f36697753010f
Gang Wu <gangwu@chromium.org>
2024-03-19 23:53:00 +0000

    [SearchActivity] Set toolbar height in XML file
    Since ux revamp is launched, toolbar height is not needed to be
    calculated in the java file.
    The test SearchActivityTest::testupdateAnchorViewLayout_ActiveColorOn
    will make sure the height is toolbar_height_no_shadow +
    toolbar_url_focus_height_increase_active_color.
    
    Bug: 330207609
    Change-Id: I81656d6ba1a453bf2864899872f1827e3fd6a2bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5380973
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1275300}

--- chrome/android/javatests/src/org/chromium/chrome/browser/searchwidget/SearchActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/searchwidget/SearchActivityTest.java
@@ -628,7 +628,7 @@ public class SearchActivityTest {
     @Test
     @SmallTest
     @DisableFeatures(ChromeFeatureList.OMNIBOX_MODERNIZE_VISUAL_UPDATE)
-    public void testupdateAnchorViewLayout_NoEffectIfFlagDisabled() {
+    public void testupdateAnchorViewLayout() {
         SearchActivity searchActivity = startSearchActivity();
         View anchorView = searchActivity.getAnchorViewForTesting();
         var layoutParams = anchorView.getLayoutParams();
@@ -636,7 +636,7 @@ public class SearchActivityTest {
         int expectedHeight =
                 searchActivity
                         .getResources()
-                        .getDimensionPixelSize(R.dimen.toolbar_height_no_shadow);
+                        .getDimensionPixelSize(R.dimen.toolbar_height_no_shadow_focused);
         int expectedBottomPadding = 0;
 
         Assert.assertEquals(expectedHeight, layoutParams.height);
