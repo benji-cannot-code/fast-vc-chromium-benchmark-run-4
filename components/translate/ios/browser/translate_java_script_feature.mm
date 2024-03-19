72559c24976c1ec68b936e26433629e979670d1c
John Wu <jzw@chromium.org>
2024-03-19 20:36:54 +0000

    Fix possible flake with ios translate's callback installation
    https://crrev.com/c/5349180 moved the callback installation timing
    and may have resulted in some flakes. This change goes back to the old
    timing where the callbacks are guaranteed to be installed before
    possibly being invoked.
    
    Bug: 327016283
    Change-Id: If1dfb74e66703df3de7659a2a6b9de19bdb6b774
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5379623
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275155}

--- components/translate/ios/browser/translate_java_script_feature.mm
+++ components/translate/ios/browser/translate_java_script_feature.mm
@@ -69,8 +69,6 @@ void TranslateJavaScriptFeature::InjectTranslateScript(
 
   ExecuteJavaScript(frame, base::UTF8ToUTF16(script),
                     base::DoNothingAs<void(const base::Value*, NSError*)>());
-
-  CallJavaScriptFunction(frame, "translate.installCallbacks", {});
 }
 
 std::optional<std::string>
