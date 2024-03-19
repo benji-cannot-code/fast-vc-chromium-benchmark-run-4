af244fa5b8c72caf40b9881f32a6b1c93580f579
Hirokazu Honda <hiroh@chromium.org>
2024-03-19 09:41:11 +0000

    media/VEA: Add drop_frame_thresh_percentage to HumanReadableString
    Bug: None
    Test: See chrome log
    Change-Id: If96aa8352713826b1ba7aabb375ac57c689401b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5373685
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274788}

--- media/video/video_encode_accelerator.cc
+++ media/video/video_encode_accelerator.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback.h"
 #include "base/ranges/algorithm.h"
+#include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
@@ -133,6 +134,9 @@ std::string VideoEncodeAccelerator::Config::AsHumanReadableString() const {
       str += "no-preference";
       break;
   }
+
+  str += base::StringPrintf(", drop_frame_thresh_percentage: %hhu",
+                            drop_frame_thresh_percentage);
 
   if (spatial_layers.empty())
     return str;
