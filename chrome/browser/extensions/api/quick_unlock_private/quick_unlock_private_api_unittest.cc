41fc0a31311d0c89028ea5dadf7749d19a181900
Helmut Januschka <helmut@januschka.com>
2024-03-19 00:08:10 +0000

    api/quick_unlock_private: base::StringPiece -> std::string_view
    Bug: 691162
    Change-Id: Iaa47fe4b870eec71c1d47b774f127c3d95619ada
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5376494
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Auto-Submit: Helmut Januschka <helmut@januschka.com>
    Cr-Commit-Position: refs/heads/main@{#1274575}

--- chrome/browser/extensions/api/quick_unlock_private/quick_unlock_private_api_unittest.cc
+++ chrome/browser/extensions/api/quick_unlock_private/quick_unlock_private_api_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <optional>
+#include <string_view>
 #include <utility>
 
 #include "ash/constants/ash_features.h"
@@ -21,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "base/run_loop.h"
 #include "base/strings/strcat.h"
-#include "base/strings/string_piece.h"
 #include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
@@ -72,7 +72,7 @@ using CredentialList = std::vector<std::string>;
 // The type of the test. Either based on Prefs or Cryptohome
 enum class TestType { kPrefs, kCryptohome };
 
-const base::StringPiece TestTypeStr(TestType type) {
+const std::string_view TestTypeStr(TestType type) {
   switch (type) {
     case TestType::kPrefs:
       return "PrefBased";
