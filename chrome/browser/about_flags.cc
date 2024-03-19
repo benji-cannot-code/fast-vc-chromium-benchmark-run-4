b307222ebe213c2ecadcbd590e6f9ef273c9d6c7
Juliana Chang <julianachang@google.com>
2024-03-19 07:21:26 +0000

    VCD: Add feature flag for super-res control override
    CrOS change: crrev/c/5372784
    
    Bug: b:316252303
    Test: ./out/Default/unit_tests --gtest_filter='AboutFlags*'
    Test: Manually
    Change-Id: I38dba089f950cf7f8d3ada647dff20a6d7bd39bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5369624
    Commit-Queue: Juliana Chang <julianachang@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Reviewed-by: Ren-Pei Zeng <kamesan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274736}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -2873,7 +2873,14 @@ const FeatureEntry::Choice kAutoFramingOverrideChoices[] = {
      media::switches::kAutoFramingForceEnabled},
     {"Force disabled", media::switches::kAutoFramingOverride,
      media::switches::kAutoFramingForceDisabled}};
-#endif
+
+const FeatureEntry::Choice kCameraSuperResOverrideChoices[] = {
+    {"Default", "", ""},
+    {"Enabled", media::switches::kCameraSuperResOverride,
+     media::switches::kCameraSuperResForceEnabled},
+    {"Disabled", media::switches::kCameraSuperResOverride,
+     media::switches::kCameraSuperResForceDisabled}};
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 const FeatureEntry::Choice kCrostiniContainerChoices[] = {
@@ -5768,6 +5775,10 @@ const FeatureEntry kFeatureEntries[] = {
     {"auto-framing-override", flag_descriptions::kAutoFramingOverrideName,
      flag_descriptions::kAutoFramingOverrideDescription, kOsCrOS,
      MULTI_VALUE_TYPE(kAutoFramingOverrideChoices)},
+    {"camera-super-res-override",
+     flag_descriptions::kCameraSuperResOverrideName,
+     flag_descriptions::kCameraSuperResOverrideDescription, kOsCrOS,
+     MULTI_VALUE_TYPE(kCameraSuperResOverrideChoices)},
     {"camera-app-autoqr-detection",
      flag_descriptions::kCameraAppAutoQRDetectionName,
      flag_descriptions::kCameraAppAutoQRDetectionDescription, kOsCrOS,
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3493,6 +3493,9 @@ extern const char kPermissiveUsbPassthroughDescription[];
 extern const char kCameraAngleBackendName[];
 extern const char kCameraAngleBackendDescription[];
 
+extern const char kCameraSuperResOverrideName[];
+extern const char kCameraSuperResOverrideDescription[];
+
 extern const char kCrostiniContainerInstallName[];
 extern const char kCrostiniContainerInstallDescription[];
 
