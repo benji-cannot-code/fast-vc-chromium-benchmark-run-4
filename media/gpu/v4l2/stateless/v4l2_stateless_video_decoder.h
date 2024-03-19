274cde8af91d7175027a99b90347606ecdaaba6e
Miguel Casas <mcasas@chromium.org>
2024-03-19 00:15:18 +0000

    media/gpu/V4L2StatelessVD: Limit max simultaneous instances
    This CL limits the maximum number of simultaneous V4L2StatelessVD
    instances, similar to ToT and the recent https://crrev.com/c/5368833.
    
    Unit test is extended via a typed test [1], which runs for both
    stateful and stateless, skipping the corresponding API when the
    platform doesn't support it (drivers support one or the other but
    never both). Tests were verified on strongbad (stateful) and
    corsola (stateless).
    
    Re the disappearance of FRIEND_TEST_ALL_PREFIXES and use of a static
    GetMax...ForTesting(). FRIEND_TEST_ALL_PREFIXES, and its underlying
    FRIEND_TEST() macro, don't get along with the complicated test name
    generator in gtest-typed-test.h; I tried a couple of approaches,
    including using the same name extension used in the macros in that
    file, and they all looked atrocious. Having a ForTesting() class
    method sounded like the least bad option.
    
    [1] https://github.com/google/googletest/blob/main/googletest/include/gtest/gtest-typed-test.h
    
    Bug: 278935312
    Change-Id: I1727b90610132fe357cfc81a4d5f03eed8312262
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5365946
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Commit-Queue: Miguel Casas-Sanchez <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274581}

--- media/gpu/v4l2/stateless/v4l2_stateless_video_decoder.h
+++ media/gpu/v4l2/stateless/v4l2_stateless_video_decoder.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <queue>
 #include <vector>
 
+#include "base/atomic_ref_count.h"
 #include "base/containers/lru_cache.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
@@ -70,6 +71,10 @@ class MEDIA_GPU_EXPORT V4L2StatelessVideoDecoder
                    const uint8_t* data,
                    size_t size,
                    scoped_refptr<StatelessDecodeSurface> dec_surface) override;
+
+  static int GetMaxNumDecoderInstancesForTesting() {
+    return GetMaxNumDecoderInstances();
+  }
 
  private:
   V4L2StatelessVideoDecoder(
@@ -142,6 +147,13 @@ class MEDIA_GPU_EXPORT V4L2StatelessVideoDecoder
   // multiple compressed frames without waiting for a callback. These frames
   // need to be queued up as there may not be free input buffers available.
   void ServiceDecodeRequestQueue();
+
+  // Pages with multiple decoder instances might run out of memory (e.g.
+  // b/170870476) or crash (e.g. crbug.com/1109312). this class method provides
+  // that number to prevent that erroneous behaviour during Initialize().
+  static int GetMaxNumDecoderInstances();
+  // Tracks the number of decoder instances globally in the process.
+  static base::AtomicRefCount num_decoder_instances_;
 
   SEQUENCE_CHECKER(decoder_sequence_checker_);
 
