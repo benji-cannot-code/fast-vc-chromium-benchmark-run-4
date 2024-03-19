90984fb74700171df6d70fb919f2b8d8fd5a1c4c
Dmitry Vykochko <vykochko@google.com>
2024-03-19 16:29:19 +0000

    Add "Autofill.AddedNewAddress" emission for the Setting address page.
    Bug: 313586436
    Change-Id: Id29616b1cebc463596eb324df78a929842f7bb4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372851
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Cr-Commit-Position: refs/heads/main@{#1274978}

--- chrome/browser/extensions/api/autofill_private/autofill_private_api.cc
+++ chrome/browser/extensions/api/autofill_private/autofill_private_api.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/data_model/credit_card.h"
 #include "components/autofill/core/browser/data_model/iban.h"
 #include "components/autofill/core/browser/form_data_importer.h"
+#include "components/autofill/core/browser/metrics/address_save_metrics.h"
 #include "components/autofill/core/browser/metrics/payments/mandatory_reauth_metrics.h"
 #include "components/autofill/core/browser/payments/credit_card_access_manager.h"
 #include "components/autofill/core/browser/payments/local_card_migration_manager.h"
@@ -229,6 +230,9 @@ ExtensionFunction::ResponseAction AutofillPrivateSaveAddressFunction::Run() {
   } else {
     profile.FinalizeAfterImport();
     personal_data->AddProfile(profile);
+    autofill::autofill_metrics::LogManuallyAddedAddress(
+        autofill::autofill_metrics::AutofillManuallyAddedAddressSurface::
+            kSettings);
   }
 
   return RespondNow(NoArguments());
