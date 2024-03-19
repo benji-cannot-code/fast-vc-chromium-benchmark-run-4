4a98780999d0945e7c76f4f5b43ae887ce020a4f
W. James MacLean <wjmaclean@chromium.org>
2024-03-19 14:00:04 +0000

    Support access to extension resources, but not extension APIs, for OOPSIFs
    This CL modifies the loading of resources so that sandboxed data: and
    srcdoc frames within an extension don't lose access to extension
    resources when they are placed in a different process. It also adds
    test coverage to ensure that the sandboxed data: and srcdoc frames
    don't have access to extension APIs.
    
    This CL also removes temporary code landed in
    https://chromium-review.googlesource.com/c/chromium/src/+/5020358
    to prevent sandboxed frames in extensions from being process-isolated
    while work was proceeding on 1501910.
    
    Bug: 510122, 1501910
    Change-Id: Ia505149d2f5be826df2b61b32e246139d43e6299
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5190385
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: W. James Maclean <wjmaclean@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274879}

--- content/public/common/content_features.h
+++ content/public/common/content_features.h
@@ -35,6 +35,8 @@ CONTENT_EXPORT BASE_DECLARE_FEATURE(kBackForwardCacheEntryTimeout);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kBackForwardCacheMemoryControls);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kBackForwardCacheMediaSessionService);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kBackForwardTransitions);
+CONTENT_EXPORT BASE_DECLARE_FEATURE(
+    kBlockCrossOriginInitiatedAboutSrcdocNavigations);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kBlockInsecurePrivateNetworkRequests);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(
     kBlockInsecurePrivateNetworkRequestsFromPrivate);
