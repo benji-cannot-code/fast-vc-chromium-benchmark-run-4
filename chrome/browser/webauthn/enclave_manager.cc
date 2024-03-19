a53095525468b984b6e783a68164acc01fa1f2b0
Nina Satragno <nsatragno@chromium.org>
2024-03-19 15:32:27 +0000

    [webauthn] Reuse UV key if available
    When registering with the enclave, do not create a new user verifying
    key if one already exists.
    
    Bug: 40274370
    Change-Id: I57f2921b640384a66deeb56645f9b944dbc730b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5374997
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274942}

--- chrome/browser/webauthn/enclave_manager.cc
+++ chrome/browser/webauthn/enclave_manager.cc
@@ -65,10 +65,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/boringssl/src/include/openssl/evp.h"
 #include "third_party/boringssl/src/include/openssl/rand.h"
 
-#if BUILDFLAG(IS_WIN)
-#include "base/strings/strcat.h"
-#endif
-
 namespace enclave = device::enclave;
 using webauthn_pb::EnclaveLocalState;
 
@@ -1113,25 +1109,30 @@ class EnclaveManager::StateMachine {
               if (!state_machine) {
                 return;
               }
-              if (is_uv_key_supported) {
-                if (!state_machine->user_->wrapped_uv_private_key().empty()) {
-                  // TODO(nsatragno): remove the previous key entry.
-                }
-                state_machine->user_verifying_key_provider_ =
-                    crypto::GetUserVerifyingKeyProvider(
-                        MakeUserVerifyingKeyConfig());
-                if (state_machine->user_verifying_key_provider_) {
-                  state_machine->user_verifying_key_provider_
-                      ->GenerateUserVerifyingSigningKey(
-                          kSigningAlgorithms,
-                          base::BindOnce(&StateMachine::GenerateHardwareKey,
-                                         state_machine));
-                  return;
-                }
+              if (!is_uv_key_supported ||
+                  !(state_machine->user_verifying_key_provider_ =
+                        crypto::GetUserVerifyingKeyProvider(
+                            MakeUserVerifyingKeyConfig()))) {
+                // UV keys are not available, so skip to generating a hardware
+                // key.
+                state_machine->GenerateHardwareKey(nullptr);
+                return;
               }
-              // UV keys are not available, so skip to generating a hardware
-              // key.
-              state_machine->GenerateHardwareKey(nullptr);
+              if (state_machine->user_->wrapped_uv_private_key().empty()) {
+                // Create a new UV key.
+                state_machine->user_verifying_key_provider_
+                    ->GenerateUserVerifyingSigningKey(
+                        kSigningAlgorithms,
+                        base::BindOnce(&StateMachine::GenerateHardwareKey,
+                                       state_machine));
+                return;
+              }
+              // Use the existing UV key.
+              state_machine->user_verifying_key_provider_
+                  ->GetUserVerifyingSigningKey(
+                      state_machine->user_->wrapped_uv_private_key(),
+                      base::BindOnce(&StateMachine::GenerateHardwareKey,
+                                     state_machine));
             },
             weak_ptr_factory_.GetWeakPtr()));
   }
@@ -2192,7 +2193,7 @@ EnclaveManager::UvKeyState EnclaveManager::uv_key_state() const {
   if (user_->wrapped_uv_private_key().empty()) {
     return UvKeyState::kNone;
   }
-#if BUILDFLAG(IS_WIN)
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
   return UvKeyState::kUsesSystemUI;
 #else
   return UvKeyState::kNone;
