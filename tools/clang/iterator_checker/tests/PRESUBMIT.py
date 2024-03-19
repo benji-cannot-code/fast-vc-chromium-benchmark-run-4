6649291117f1cece721cf91006b7e41aae9f2b62
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-03-19 10:32:43 +0000

    IteratorChecker: Basic PRESUBMIT.py in tests.
    This will avoid adding some .actual files.
    
    Bug: 328661225
    Change-Id: I136e656004e655aa1b511d16b776e05966d40ca8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5378153
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Paul Semel <paulsemel@chromium.org>
    Commit-Queue: Paul Semel <paulsemel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274808}

---
+++ tools/clang/iterator_checker/tests/PRESUBMIT.py
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2024 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Presubmit script for changes affecting partition_alloc.
+
+See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
+for more details about the presubmit API built into depot_tools.
+"""
+
+PRESUBMIT_VERSION = '2.0.0'
+
+
+# Make sure no `.actual` file is added in tests:
+def CheckAllowedFiles(input_api, output_api):
+  errors = []
+  for f in input_api.AffectedFiles(include_deletes=False):
+    path = f.LocalPath()
+    if not path.endswith(('.py', '.cpp', '.txt')):
+      errors.append(output_api.PresubmitError("Unexpected file added %s" %
+                                              path))
+  return errors
