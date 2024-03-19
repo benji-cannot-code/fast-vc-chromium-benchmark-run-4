7a82ec787295579205170a3b88911834f26e6f79
Ming-Ying Chung <mych@chromium.org>
2024-03-19 02:46:46 +0000

    Hook in-browser attribution reporting with FetchLater API
    Per offline discussion, some potential fetchLater() API users are
    looking to use the API with attribution reporting ability, i.e.
    sending out fetchLater requests with `Attribution-Reporting-Eligible`
    header, where the corresponding responses should also be recognized by
    Chrome privacy sandbox.
    
    This CL enables the Attribution Reporting in-browser handler [1] to
    be used by fetchLater loader to process such requests.
    
    [1]: https://docs.google.com/document/d/1KUQcj_WcOAjxj89WYtmUEaFdiCAQ2H4ODLyqRTbNeFY/edit?tab=t.0
    
    Bug: 1465781,1374121
    Change-Id: Id70d016ff6f9b00924e8c29a5ef02cbb14e832c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5259717
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Reviewed-by: Anthony Garant <anthonygarant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274664}

--- content/browser/attribution_reporting/attribution_data_host_manager_impl.cc
+++ content/browser/attribution_reporting/attribution_data_host_manager_impl.cc
@@ -185,8 +185,9 @@ void RecordBackgroundNavigationOutcome(BackgroundNavigationOutcome outcome) {
 }
 
 bool BackgroundRegistrationsEnabled() {
-  return base::FeatureList::IsEnabled(
-             blink::features::kKeepAliveInBrowserMigration) &&
+  return (base::FeatureList::IsEnabled(
+              blink::features::kKeepAliveInBrowserMigration) ||
+          base::FeatureList::IsEnabled(blink::features::kFetchLaterAPI)) &&
          base::FeatureList::IsEnabled(
              blink::features::kAttributionReportingInBrowserMigration);
 }
