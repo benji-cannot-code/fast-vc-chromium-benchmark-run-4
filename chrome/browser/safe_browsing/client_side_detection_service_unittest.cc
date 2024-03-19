2633e454b1a35d6e230c174fd81584a9dbfbe67c
Seung Jae Lim <andysjlim@chromium.org>
2024-03-19 20:58:38 +0000

    Add CSD-Phishing debugging metadata information to PhishGuard pings
    Adding CSD-Phishing debugging metadata information to PhishGuard pings
    will help understand why for a given URL a PG ping was sent, but
    CSD-Phishing was not.
    
    Bug: 1470906
    Change-Id: Ib7b8a46a2392e42e28e3fc1185de0702aa42f352
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5037136
    Reviewed-by: Nwokedi Idika <nwokedi@chromium.org>
    Commit-Queue: Seung Jae Lim <andysjlim@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275171}

--- chrome/browser/safe_browsing/client_side_detection_service_unittest.cc
+++ chrome/browser/safe_browsing/client_side_detection_service_unittest.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/variations/variations_associated_data.h"
 #include "content/public/test/browser_task_environment.h"
 #include "crypto/sha2.h"
+#include "net/http/http_status_code.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
 #include "services/network/public/mojom/url_response_head.mojom.h"
 #include "services/network/test/test_url_loader_factory.h"
@@ -302,7 +303,8 @@ class ClientSideDetectionServiceTest
  private:
   void SendRequestDone(base::OnceClosure continuation_callback,
                        GURL phishing_url,
-                       bool is_phishing) {
+                       bool is_phishing,
+                       std::optional<net::HttpStatusCode> response_code) {
     ASSERT_EQ(phishing_url, phishing_url_);
     is_phishing_ = is_phishing;
     std::move(continuation_callback).Run();
--- components/safe_browsing/content/browser/client_side_detection_service.h
+++ components/safe_browsing/content/browser/client_side_detection_service.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_process_host_creation_observer.h"
 #include "net/base/ip_address.h"
+#include "net/http/http_status_code.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "url/gurl.h"
 
@@ -66,8 +67,10 @@ class ClientSideDetectionService
     : public KeyedService,
       public content::RenderProcessHostCreationObserver {
  public:
-  // void(GURL phishing_url, bool is_phishing).
-  typedef base::OnceCallback<void(GURL, bool)>
+  // void(GURL phishing_url, bool is_phishing,
+  // absl::optional<net::HttpStatusCode> response_code).
+  typedef base::OnceCallback<
+      void(GURL, bool, std::optional<net::HttpStatusCode>)>
       ClientReportPhishingRequestCallback;
 
   // Delegate which allows to provide embedder specific implementations.
@@ -201,6 +204,10 @@ class ClientSideDetectionService
   base::CallbackListSubscription RegisterCallbackForModelUpdates(
       base::RepeatingClosure callback);
 
+  // Returns the trigger model version to be used in cache for CSD-Phishing
+  // debugging metadata.
+  int GetTriggerModelVersion();
+
  private:
   friend class ClientSideDetectionServiceTest;
   FRIEND_TEST_ALL_PREFIXES(ClientSideDetectionServiceTest,
@@ -249,7 +256,7 @@ class ClientSideDetectionService
   void HandlePhishingVerdict(network::SimpleURLLoader* source,
                              const GURL& url,
                              int net_error,
-                             int response_code,
+                             std::optional<net::HttpStatusCode> response_code,
                              const std::string& data);
 
   // Invalidate cache results which are no longer useful.
@@ -289,6 +296,8 @@ class ClientSideDetectionService
   // the renderer host processes. This is used to determine, when the image
   // embedding model arrives, whether a new scorer should be made with all
   // models or the image embedding model can be attached to the current scorer.
+  // This is also used to add to CSD-Phishing debugging metadata to PhishGuard
+  // pings.
   int trigger_model_version_ = 0;
 
   // Map of client report phishing request to the corresponding callback that
