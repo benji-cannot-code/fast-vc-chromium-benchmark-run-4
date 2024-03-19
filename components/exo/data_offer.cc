30864027e12e7bf74970d2bfd91f8d9d8bb553ab
Avi Drissman <avi@chromium.org>
2024-03-19 22:49:42 +0000

    Convert OSExchangeData pickle access to use std::optional
    Bug: 328710562
    Cq-Include-Trybots: luci.chromium.try:linux-lacros-asan-lsan-rel
    Include-Ci-Only-Tests: true
    Change-Id: Ifb2b395bd2fead8b609000e9053817822486ba9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5352134
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275264}

--- components/exo/data_offer.cc
+++ components/exo/data_offer.cc
@@ -253,11 +253,11 @@ void DataOffer::SetDropData(DataExchangeDelegate* data_exchange_delegate,
   // We accept the filenames pickle from FilesApp, or
   // OSExchangeData::GetFilenames().
   std::vector<ui::FileInfo> filenames;
-  base::Pickle pickle;
-  if (data.GetPickledData(ui::ClipboardFormatType::WebCustomDataType(),
-                          &pickle)) {
+  if (std::optional<base::Pickle> pickle =
+          data.GetPickledData(ui::ClipboardFormatType::WebCustomDataType());
+      pickle.has_value()) {
     filenames = data_exchange_delegate->ParseFileSystemSources(data.GetSource(),
-                                                               pickle);
+                                                               pickle.value());
   }
 
   if (filenames.empty() && data.HasFile()) {
@@ -278,13 +278,15 @@ void DataOffer::SetDropData(DataExchangeDelegate* data_exchange_delegate,
     return;
   }
 
-  if (data.GetPickledData(GetClipboardFormatType(), &pickle) &&
-      data_exchange_delegate->HasUrlsInPickle(pickle)) {
+  if (std::optional<base::Pickle> pickle =
+          data.GetPickledData(GetClipboardFormatType());
+      pickle.has_value() &&
+      data_exchange_delegate->HasUrlsInPickle(pickle.value())) {
     data_callbacks_.emplace(
         uri_list_mime_type,
         base::BindOnce(&SecurityDelegate::SendPickle,
                        base::Unretained(delegate_->GetSecurityDelegate()),
-                       endpoint_type, pickle));
+                       endpoint_type, pickle.value()));
     delegate_->OnOffer(uri_list_mime_type);
     return;
   }
