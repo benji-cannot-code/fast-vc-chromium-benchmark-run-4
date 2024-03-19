3c69808f28cf2382882afe8b457cec9a83b80aa0
Hardik Goyal <hardikgoyal@google.com>
2024-03-19 17:28:46 +0000

    Add SmartCardEngine stub
    The implementation of this class is not complete, but that is okay since
    it is not being used. Follow up CLs will continue to add more
    implementation.
    
    BUG=b:271261365
    
    Change-Id: I3b1e3dfdfdbfe9dcbde4da00a4447c252f3b4d25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372698
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Reviewed-by: Istvan Nagy <iscsi@google.com>
    Auto-Submit: Hardik Goyal <hardikgoyal@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Hardik Goyal <hardikgoyal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275028}

--- chromeos/ash/components/osauth/impl/engines/cryptohome_pin_engine.cc
+++ chromeos/ash/components/osauth/impl/engines/cryptohome_pin_engine.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/ash/components/osauth/impl/engines/cryptohome_pin_engine.h"
 
+#include <memory>
+#include <optional>
 #include <string>
 
 #include "ash/constants/ash_pref_names.h"
