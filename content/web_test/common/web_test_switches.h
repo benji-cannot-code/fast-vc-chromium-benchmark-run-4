593683f4ead77e13bfa5112c655ae1ef36afd081
Alex Rudenko <alexrudenko@chromium.org>
2024-03-19 16:16:58 +0000

    Allow running inspector protocol tests using protocol logs
    This CL implements a flag called `--inspector-protocol-log` that accepts
    a path to a Chrome DevTools Protocol message log. If specified, the test
    runner would replay the log mocking the actual browser. The purpose of
    this flag is to allow reproducing test flakiness locally and it is not
    meant to be used on the bots for now.
    
    Bug: 327140253
    Change-Id: I871442d568878b3a1a0d71a18eb2eb721b457e76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5331573
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274969}

--- content/web_test/common/web_test_switches.h
+++ content/web_test/common/web_test_switches.h
@@ -15,6 +15,7 @@ extern const char kEnableAccelerated2DCanvas[];
 extern const char kEnableFontAntialiasing[];
 extern const char kAlwaysUseComplexText[];
 extern const char kEnableLeakDetection[];
+extern const char kInspectorProtocolLog[];
 extern const char kEncodeBinary[];
 extern const char kStableReleaseMode[];
 extern const char kDisableHeadlessMode[];
