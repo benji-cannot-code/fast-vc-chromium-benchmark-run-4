af25ee1b59244399bd2ed3be47e55a8e4d552293
Boris Sazonov <bsazonov@chromium.org>
2024-03-19 14:34:59 +0000

    [Signin][Android] Update the cached email after account rename
    SigninManagerImpl caches the email of the current sync account in
    a shared pref (see https://crbug.com/40697988 for more details).
    Prior to this CL, that shared pref was set only during the sync opt-in
    flow. This CL adds the code to correctly update the pref in case the
    primary email of the primary account changes.
    
    To correctly test the newly added behavior (as well as the fact that
    IdentityManager updates the info for the primary account upon the
    primary email change),
    testOnCoreAccountInfosChanged_PrimaryAccountRenamed is promoted from
    a Robolectric test to a device unit test and adjusted to cover new
    behavior.
    
    Note: the behavior described above requires SeedAccountsRevamp feature
    to be enabled.
    
    Bug: 327122937
    Change-Id: I4cc694cb113f65e958ba8c90b3eef759a58dd6ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5377733
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Samar Chehade-Lepleux <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1274905}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninManagerIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninManagerIntegrationTest.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.signin;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 
@@ -32,8 +33,11 @@ import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.profiles.ProfileManager;
 import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
 import org.chromium.chrome.browser.signin.services.SigninManager;
+import org.chromium.chrome.browser.signin.services.SigninPreferencesManager;
 import org.chromium.chrome.test.util.browser.signin.SigninTestRule;
 import org.chromium.chrome.test.util.browser.signin.SigninTestUtil;
+import org.chromium.components.signin.SigninFeatures;
+import org.chromium.components.signin.base.AccountInfo;
 import org.chromium.components.signin.base.CoreAccountInfo;
 import org.chromium.components.signin.identitymanager.ConsentLevel;
 import org.chromium.components.signin.identitymanager.IdentityManager;
@@ -98,7 +102,7 @@ public class SigninManagerIntegrationTest {
         TestThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     // Run test.
-                    if (!ChromeFeatureList.isEnabled(ChromeFeatureList.SEED_ACCOUNTS_REVAMP)) {
+                    if (!ChromeFeatureList.isEnabled(SigninFeatures.SEED_ACCOUNTS_REVAMP)) {
                         mSigninManager.reloadAllAccountsFromSystem(null);
                     }
 
@@ -117,7 +121,7 @@ public class SigninManagerIntegrationTest {
         TestThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     // Run test.
-                    if (!ChromeFeatureList.isEnabled(ChromeFeatureList.SEED_ACCOUNTS_REVAMP)) {
+                    if (!ChromeFeatureList.isEnabled(SigninFeatures.SEED_ACCOUNTS_REVAMP)) {
                         mSigninManager.reloadAllAccountsFromSystem(null);
                     }
 
@@ -145,7 +149,7 @@ public class SigninManagerIntegrationTest {
 
     @Test
     @MediumTest
-    @DisableFeatures(ChromeFeatureList.SEED_ACCOUNTS_REVAMP)
+    @DisableFeatures(SigninFeatures.SEED_ACCOUNTS_REVAMP)
     // TODO(crbug.com/1491005): Delete this test
     public void testUpdateAccountListOneAccountsRegisteredSignedInOther() {
         mSigninTestRule.addAccountAndWaitForSeeding(TEST_ACCOUNT1);
@@ -182,7 +186,7 @@ public class SigninManagerIntegrationTest {
         TestThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     // Re-run validation.
-                    if (!ChromeFeatureList.isEnabled(ChromeFeatureList.SEED_ACCOUNTS_REVAMP)) {
+                    if (!ChromeFeatureList.isEnabled(SigninFeatures.SEED_ACCOUNTS_REVAMP)) {
                         mSigninManager.reloadAllAccountsFromSystem(mTestAccount1.getId());
                     }
 
@@ -197,7 +201,7 @@ public class SigninManagerIntegrationTest {
 
     @Test
     @MediumTest
-    @EnableFeatures(ChromeFeatureList.SEED_ACCOUNTS_REVAMP)
+    @EnableFeatures(SigninFeatures.SEED_ACCOUNTS_REVAMP)
     public void testUpdateAccountListTwoAccountsThenRemoveOne() {
         // Add accounts.
         mSigninTestRule.addAccountAndWaitForSeeding(TEST_ACCOUNT1);
@@ -250,7 +254,7 @@ public class SigninManagerIntegrationTest {
         TestThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     // Re-validate and run checks.
-                    if (!ChromeFeatureList.isEnabled(ChromeFeatureList.SEED_ACCOUNTS_REVAMP)) {
+                    if (!ChromeFeatureList.isEnabled(SigninFeatures.SEED_ACCOUNTS_REVAMP)) {
                         mSigninManager.reloadAllAccountsFromSystem(mTestAccount1.getId());
                     }
 
@@ -263,7 +267,7 @@ public class SigninManagerIntegrationTest {
 
     @Test
     @MediumTest
-    @EnableFeatures(ChromeFeatureList.SEED_ACCOUNTS_REVAMP)
+    @EnableFeatures(SigninFeatures.SEED_ACCOUNTS_REVAMP)
     public void testUpdateAccountListTwoAccountsThenRemoveAllSignOut() {
         // Add accounts.
         mSigninTestRule.addAccountAndWaitForSeeding(TEST_ACCOUNT1);
@@ -314,7 +318,7 @@ public class SigninManagerIntegrationTest {
 
     @Test
     @MediumTest
-    @DisableFeatures(ChromeFeatureList.SEED_ACCOUNTS_REVAMP)
+    @DisableFeatures(SigninFeatures.SEED_ACCOUNTS_REVAMP)
     // TODO(crbug.com/1491005): Delete this test
     public void testUpdateAccountListNoAccountsRegisteredButSignedIn() {
         TestThreadUtils.runOnUiThreadBlocking(
@@ -326,6 +330,38 @@ public class SigninManagerIntegrationTest {
                             "No accounts available",
                             new CoreAccountInfo[] {},
                             mIdentityManager.getAccountsWithRefreshTokens());
+                });
+    }
+
+    @Test
+    @MediumTest
+    @EnableFeatures(SigninFeatures.SEED_ACCOUNTS_REVAMP)
+    public void testOnCoreAccountInfosChanged_PrimaryAccountRenamed() {
+        mSigninTestRule.addAccount(TEST_ACCOUNT1);
+        SigninTestUtil.signinAndEnableSync(mTestAccount1, null);
+
+        TestThreadUtils.runOnUiThreadBlocking(
+                () -> {
+                    assertEquals(
+                            SigninPreferencesManager.getInstance().getLegacySyncAccountEmail(),
+                            mTestAccount1.getEmail());
+                });
+
+        mSigninTestRule.blockGetCoreAccountInfosUpdate(true);
+        mSigninTestRule.removeAccount(mTestAccount1.getId());
+        AccountInfo renamedAccount =
+                new AccountInfo.Builder("renamed@gmail.com", mTestAccount1.getGaiaId()).build();
+        mSigninTestRule.addAccount(renamedAccount);
+        mSigninTestRule.unblockGetCoreAccountInfos();
+
+        TestThreadUtils.runOnUiThreadBlocking(
+                () -> {
+                    assertEquals(
+                            mIdentityManager.getPrimaryAccountInfo(ConsentLevel.SYNC).getEmail(),
+                            renamedAccount.getEmail());
+                    assertEquals(
+                            SigninPreferencesManager.getInstance().getLegacySyncAccountEmail(),
+                            renamedAccount.getEmail());
                 });
     }
 
