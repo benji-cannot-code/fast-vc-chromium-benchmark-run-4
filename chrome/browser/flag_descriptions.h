b5fcb52c742f5f7186e7c983ea82768c0ea95ed5
Kristian Monsen <kristianm@google.com>
2024-03-19 06:42:46 +0000

    Adding a UI flag for standards based DBSC
    Bug: 41495201
    Change-Id: I3cf373e2eccc474d6caeabfe7e522f48a79ae11d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5369116
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Kristian Monsen <kristianm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274727}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/vr/buildflags/buildflags.h"
 #include "extensions/buildflags/buildflags.h"
 #include "media/media_buildflags.h"
+#include "net/net_buildflags.h"
 #include "pdf/buildflags.h"
 #include "printing/buildflags/buildflags.h"
 #include "third_party/blink/public/common/buildflags.h"
@@ -4683,6 +4684,11 @@ extern const char
 extern const char
     kEnableBoundSessionCredentialsSoftwareKeysForManualTestingDescription[];
 #endif  // BUILDFLAG(ENABLE_BOUND_SESSION_CREDENTIALS)
+
+#if BUILDFLAG(ENABLE_DEVICE_BOUND_SESSIONS)
+extern const char kEnableStandardBoundSessionCredentialsName[];
+extern const char kEnableStandardBoundSessionCredentialsDescription[];
+#endif  // BUILDFLAG(ENABLE_DEVICE_BOUND_SESSIONS)
 
 // ============================================================================
 // Don't just add flags to the end, put them in the right section in
