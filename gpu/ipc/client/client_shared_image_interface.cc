078bcafe1476f1d4baa31d2bff83ab1dee69702e
Colin Blundell <blundell@chromium.org>
2024-03-19 16:23:27 +0000

    [//gpu] Supply param explicitly in ClientSII CSI creation
    We are preparing to make the `gmb_handle_type` parameter of the
    relevant ClientSharedImage constructor non-optional. This CL supplies
    the appropriate values in the creations in
    client_shared_image_interface.cc that currently implicitly pass the
    default value.
    
    Bug: 41494843
    Change-Id: I67ca2dac56a7651113aea890447bc1cc0ce10d97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5380240
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274976}

--- gpu/ipc/client/client_shared_image_interface.cc
+++ gpu/ipc/client/client_shared_image_interface.cc
@@ -99,7 +99,7 @@ scoped_refptr<ClientSharedImage> ClientSharedImageInterface::CreateSharedImage(
   DCHECK(gpu::IsValidClientUsage(si_info.meta.usage)) << si_info.meta.usage;
   return base::MakeRefCounted<ClientSharedImage>(
       AddMailbox(proxy_->CreateSharedImage(si_info)), si_info.meta,
-      GenUnverifiedSyncToken(), holder_);
+      GenUnverifiedSyncToken(), holder_, gfx::EMPTY_BUFFER);
 }
 
 scoped_refptr<ClientSharedImage> ClientSharedImageInterface::CreateSharedImage(
@@ -121,7 +121,8 @@ scoped_refptr<ClientSharedImage> ClientSharedImageInterface::CreateSharedImage(
   }
 
   return base::MakeRefCounted<ClientSharedImage>(
-      AddMailbox(mailbox), si_info.meta, GenUnverifiedSyncToken(), holder_);
+      AddMailbox(mailbox), si_info.meta, GenUnverifiedSyncToken(), holder_,
+      gfx::EMPTY_BUFFER);
 }
 
 scoped_refptr<ClientSharedImage> ClientSharedImageInterface::CreateSharedImage(
@@ -223,7 +224,8 @@ ClientSharedImageInterface::CreateSharedImage(const SharedImageInfo& si_info) {
 
   shared_image_mapping.shared_image = base::MakeRefCounted<ClientSharedImage>(
       AddMailbox(proxy_->CreateSharedImage(si_info, std::move(handle))),
-      si_info.meta, GenUnverifiedSyncToken(), holder_);
+      si_info.meta, GenUnverifiedSyncToken(), holder_,
+      gfx::SHARED_MEMORY_BUFFER);
   return shared_image_mapping;
 }
 
@@ -280,12 +282,12 @@ ClientSharedImageInterface::CreateSwapChain(viz::SharedImageFormat format,
           mailboxes.front_buffer,
           SharedImageMetadata(format, size, color_space, surface_origin,
                               alpha_type, usage),
-          sync_token, holder_),
+          sync_token, holder_, gfx::EMPTY_BUFFER),
       base::MakeRefCounted<ClientSharedImage>(
           mailboxes.back_buffer,
           SharedImageMetadata(format, size, color_space, surface_origin,
                               alpha_type, usage),
-          sync_token, holder_));
+          sync_token, holder_, gfx::EMPTY_BUFFER));
 }
 
 void ClientSharedImageInterface::DestroySharedImage(const SyncToken& sync_token,
@@ -319,8 +321,13 @@ scoped_refptr<ClientSharedImage> ClientSharedImageInterface::ImportSharedImage(
   AddMailbox(mailbox);
   proxy_->AddReferenceToSharedImage(sync_token, mailbox, metadata.usage);
 
+  // TODO(crbug.com/41494843): This should get the GMB handle type (or
+  // alternatively the computed texture target) from either the
+  // ExportedSharedImage directly or the metadata. When doing that, determine
+  // how to update this constructor to be most clear as to how the flow is
+  // working as well.
   return base::MakeRefCounted<ClientSharedImage>(mailbox, metadata, sync_token,
-                                                 holder_);
+                                                 holder_, gfx::EMPTY_BUFFER);
 }
 
 uint32_t ClientSharedImageInterface::UsageForMailbox(const Mailbox& mailbox) {
@@ -342,7 +349,7 @@ scoped_refptr<ClientSharedImage> ClientSharedImageInterface::NotifyMailboxAdded(
       mailbox,
       SharedImageMetadata(format, size, color_space, surface_origin, alpha_type,
                           usage),
-      GenUnverifiedSyncToken(), holder_);
+      GenUnverifiedSyncToken(), holder_, gfx::EMPTY_BUFFER);
 }
 
 Mailbox ClientSharedImageInterface::AddMailbox(const gpu::Mailbox& mailbox) {
