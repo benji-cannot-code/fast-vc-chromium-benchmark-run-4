ab265a56caa17c162216cf9899719ce441e7c4e7
Helmut Januschka <helmut@januschka.com>
2024-03-19 01:44:10 +0000

    pdf: base::StringPiece -> std::string_view
    Bug: 691162
    Change-Id: Id6349118747b2379cfec738a84eb9733bff47a18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5374373
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274638}

--- chrome/browser/pdf/pdf_extension_accessibility_test.cc
+++ chrome/browser/pdf/pdf_extension_accessibility_test.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <set>
 #include <string>
+#include <string_view>
 #include <tuple>
 #include <variant>
 #include <vector>
@@ -18,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/run_loop.h"
 #include "base/strings/strcat.h"
-#include "base/strings/string_piece.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
@@ -811,7 +811,7 @@ class PDFExtensionAccessibilityTextExtractionTest
 
       std::string name =
           node->GetStringAttribute(ax::mojom::StringAttribute::kName);
-      base::StringPiece trimmed_name =
+      std::string_view trimmed_name =
           base::TrimString(name, "\r\n", base::TRIM_TRAILING);
       int prev_id =
           node->GetIntAttribute(ax::mojom::IntAttribute::kPreviousOnLineId);
--- chrome/browser/pdf/pdf_find_request_manager_browsertest.cc
+++ chrome/browser/pdf/pdf_find_request_manager_browsertest.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <string_view>
+
 #include "base/command_line.h"
 #include "base/files/file_util.h"
 #include "base/memory/raw_ptr.h"
@@ -139,8 +141,8 @@ void SendRangeResponse(net::test_server::ControllableHttpResponse* response,
   {
     auto it = response->http_request()->headers.find("Range");
     ASSERT_NE(response->http_request()->headers.end(), it);
-    base::StringPiece range_header = it->second;
-    base::StringPiece kBytesPrefix = "bytes=";
+    std::string_view range_header = it->second;
+    std::string_view kBytesPrefix = "bytes=";
     ASSERT_TRUE(base::StartsWith(range_header, kBytesPrefix));
     range_header.remove_prefix(kBytesPrefix.size());
     auto dash_pos = range_header.find('-');
