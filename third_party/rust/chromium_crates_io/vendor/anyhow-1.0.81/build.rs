101758d28c05e48d78028b64dd47fe6807328ccd
Adrian Taylor <adetaylor@chromium.org>
2024-03-19 17:41:48 +0000

    Roll anyhow: 1.0.80 => 1.0.81 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * anyhow: 1.0.80 => 1.0.81
    
    The following `cargo vet` audit criteria are required:
    
    * anyhow: does-not-implement-crypto, safe-to-run
    
    Bug: None
    Change-Id: I2039edbb30342f88b04316ae75e555af7fe696bf
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5380326
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275034}

--- third_party/rust/chromium_crates_io/vendor/anyhow-1.0.81/build.rs
+++ third_party/rust/chromium_crates_io/vendor/anyhow-1.0.81/build.rs
@@ -123,6 +123,7 @@ fn compile_probe(rustc_bootstrap: bool) -> bool {
         .arg("--crate-name=anyhow")
         .arg("--crate-type=lib")
         .arg("--emit=dep-info,metadata")
+        .arg("--cap-lints=allow")
         .arg("--out-dir")
         .arg(out_dir)
         .arg(probefile);
--- third_party/rust/chromium_crates_io/vendor/anyhow-1.0.81/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/anyhow-1.0.81/src/lib.rs
@@ -207,7 +207,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //! will require an explicit `.map_err(Error::msg)` when working with a
 //! non-Anyhow error type inside a function that returns Anyhow's error type.
 
-#![doc(html_root_url = "https://docs.rs/anyhow/1.0.80")]
+#![doc(html_root_url = "https://docs.rs/anyhow/1.0.81")]
 #![cfg_attr(error_generic_member_access, feature(error_generic_member_access))]
 #![cfg_attr(doc_cfg, feature(doc_cfg))]
 #![no_std]
