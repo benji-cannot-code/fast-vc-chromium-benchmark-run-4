baa0a46c4e5dc60aaceec03c48a9b33aaaacde2c
Xiaoling Bao <xiaolingbao@chromium.org>
2024-03-20 22:54:57 +0000

    Ignore updates policy for the updater itself.
    Bug: 330548757
    Change-Id: Ibc7bdec3ace359a8132922b8a01374500474af93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5383771
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275898}

--- chrome/updater/test/request_matcher.cc
+++ chrome/updater/test/request_matcher.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/test/bind.h"
 #include "base/values.h"
+#include "chrome/updater/constants.h"
 #include "chrome/updater/test/http_request.h"
 #include "chrome/updater/update_service.h"
 #include "chrome/updater/updater_scope.h"
@@ -161,6 +162,39 @@ Matcher GetAppPriorityMatcher(const std::string& app_id,
                     << GetPrintableContent(request);
     }
     return is_match;
+  });
+}
+
+Matcher GetUpdaterEnableUpdatesMatcher() {
+  return base::BindLambdaForTesting([](const HttpRequest& request) {
+    const bool update_disabled = [&request] {
+      const std::optional<base::Value> doc =
+          base::JSONReader::Read(request.decoded_content);
+      if (!doc || !doc->is_dict()) {
+        return false;
+      }
+      const base::Value::List* app_list =
+          doc->GetDict().FindListByDottedPath("request.app");
+      if (!app_list) {
+        return false;
+      }
+      for (const base::Value& app : *app_list) {
+        if (const auto* dict = app.GetIfDict()) {
+          if (const auto* appid = dict->FindString("appid");
+              *appid == kUpdaterAppId) {
+            if (const auto* update_check = dict->FindDict("updatecheck")) {
+              return update_check->FindBool("updatedisabled").value_or(false);
+            }
+          }
+        }
+      }
+      return false;
+    }();
+    if (update_disabled) {
+      ADD_FAILURE() << R"(Update is wrongfully disabled for updater itself: )"
+                    << GetPrintableContent(request);
+    }
+    return !update_disabled;
   });
 }
 
