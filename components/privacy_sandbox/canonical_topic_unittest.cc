8ee0d255b611793fe8a7c9d5c4c6731bec9571e6
Jacob Stanley <jacobstanley@google.com>
2024-03-20 19:09:45 +0000

    Removing Settings.PrivacySandbox.InvalidTopicIdLocalized histogram
    OBSOLETE_HISTOGRAM[Settings.PrivacySandbox.InvalidTopicIdLocalized]=Expiring soon, and is not needed anymore.
    
    Bug: 325945712
    Change-Id: Iba1383317ac0b0baecb89f7fe80240e86b887ca4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5380428
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1275760}

--- components/privacy_sandbox/canonical_topic_unittest.cc
+++ components/privacy_sandbox/canonical_topic_unittest.cc
@@ -27,9 +27,6 @@ constexpr Topic kNoFirstLevelTopicID = Topic(500);
 constexpr Topic kWithMoreChildrenTopicID = Topic(207);
 constexpr Topic kWithOneChildTopicID = Topic(250);
 
-constexpr char kInvalidTopicLocalizedHistogramName[] =
-    "Settings.PrivacySandbox.InvalidTopicIdLocalized";
-
 }  // namespace
 
 using CanonicalTopicTest = testing::Test;
@@ -47,9 +44,6 @@ TEST_F(CanonicalTopicTest, LocalizedRepresentation) {
   EXPECT_EQ(l10n_util::GetStringUTF16(
                 IDS_PRIVACY_SANDBOX_TOPICS_TAXONOMY_V2_TOPIC_ID_629),
             last_topic.GetLocalizedRepresentation());
-
-  // Successful localizations should not result in any metrics being recorded.
-  histogram_tester.ExpectTotalCount(kInvalidTopicLocalizedHistogramName, 0);
 }
 
 TEST_F(CanonicalTopicTest, InvalidTopicIdLocalized) {
@@ -70,14 +64,6 @@ TEST_F(CanonicalTopicTest, InvalidTopicIdLocalized) {
         l10n_util::GetStringUTF16(IDS_PRIVACY_SANDBOX_TOPICS_INVALID_TOPIC),
         topic.GetLocalizedRepresentation());
   }
-
-  histogram_tester.ExpectTotalCount(kInvalidTopicLocalizedHistogramName, 3);
-  histogram_tester.ExpectBucketCount(kInvalidTopicLocalizedHistogramName,
-                                     too_low_id.topic_id().value(), 1);
-  histogram_tester.ExpectBucketCount(kInvalidTopicLocalizedHistogramName,
-                                     negative_id.topic_id().value(), 1);
-  histogram_tester.ExpectBucketCount(kInvalidTopicLocalizedHistogramName,
-                                     too_high_id.topic_id().value(), 1);
 }
 
 TEST_F(CanonicalTopicTest, ValueConversion) {
