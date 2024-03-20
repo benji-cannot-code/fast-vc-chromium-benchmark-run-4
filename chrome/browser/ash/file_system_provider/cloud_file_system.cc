8d19f6cfaf77a0f908e550c63d9c010c0f8802fb
cassycc <cassycc@google.com>
2024-03-20 00:45:27 +0000

    [FspCache] Introduce OpenedCloudFile accessible by file_handle
    Introduce the struct OpenedCloudFile to keep track of information which
    will be needed to access the file from a cache. This is created upon
    an OpenFile success. The CloudFileSystem will store a map of
    OpenedCloudFile where each is accessed by the associated file_handle.
    
    Upon CloseFile completion, remove the mapping. Introduce
    OnCloseFileCompleted to intercept the CloseFile completion.
    
    Bug: b/330262353
    Change-Id: Ia76e7d51aa542174c8f2b69d164db3008817b656
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5374232
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275325}

--- chrome/browser/ash/file_system_provider/cloud_file_system.cc
+++ chrome/browser/ash/file_system_provider/cloud_file_system.cc
@@ -91,6 +91,11 @@ std::ostream& operator<<(std::ostream& out,
 
 }  // namespace
 
+OpenedCloudFile::OpenedCloudFile(const base::FilePath& file_path)
+    : file_path(file_path) {}
+
+OpenedCloudFile::~OpenedCloudFile() = default;
+
 CloudFileSystem::CloudFileSystem(
     std::unique_ptr<ProvidedFileSystemInterface> file_system)
     : CloudFileSystem(std::move(file_system), nullptr) {}
@@ -197,7 +202,8 @@ AbortCallback CloudFileSystem::OpenFile(const base::FilePath& file_path,
   return file_system_->OpenFile(
       file_path, mode,
       base::BindOnce(&CloudFileSystem::OnOpenFileCompleted,
-                     weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
+                     weak_ptr_factory_.GetWeakPtr(), file_path,
+                     std::move(callback)));
 }
 
 AbortCallback CloudFileSystem::CloseFile(
@@ -205,7 +211,10 @@ AbortCallback CloudFileSystem::CloseFile(
     storage::AsyncFileUtil::StatusCallback callback) {
   VLOG(1) << "CloseFile {fsid = '" << GetFileSystemId() << "', file_handle = '"
           << file_handle << "'}";
-  return file_system_->CloseFile(file_handle, std::move(callback));
+  return file_system_->CloseFile(
+      file_handle, base::BindOnce(&CloudFileSystem::OnCloseFileCompleted,
+                                  weak_ptr_factory_.GetWeakPtr(), file_handle,
+                                  std::move(callback)));
 }
 
 AbortCallback CloudFileSystem::CreateDirectory(
@@ -401,10 +410,24 @@ void CloudFileSystem::OnTimer() {
                 }));
 }
 
-void CloudFileSystem::OnOpenFileCompleted(OpenFileCallback callback,
+void CloudFileSystem::OnOpenFileCompleted(const base::FilePath& file_path,
+                                          OpenFileCallback callback,
                                           int file_handle,
                                           base::File::Error result) {
+  if (result == base::File::FILE_OK) {
+    opened_files_.try_emplace(file_handle, OpenedCloudFile(file_path));
+  }
   std::move(callback).Run(file_handle, result);
+}
+
+void CloudFileSystem::OnCloseFileCompleted(
+    int file_handle,
+    storage::AsyncFileUtil::StatusCallback callback,
+    base::File::Error result) {
+  // Closing is always final. Even if an error happened, we remove it from the
+  // list of opened files.
+  opened_files_.erase(file_handle);
+  std::move(callback).Run(result);
 }
 
 }  // namespace ash::file_system_provider
