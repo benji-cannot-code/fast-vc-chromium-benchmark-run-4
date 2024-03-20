e2d5d4c14dc5cd8540021768cd975a941f0c3df4
seannli <seannli@google.com>
2024-03-20 02:36:28 +0000

    personalization: Use fake VideoCaptureService in unittests
    Video capture service is functional only after mojo service manager is
    set and CameraHalDispatcher is started. Moreover, after video capture
    service is moved to utility process, the mojo channel of mojo service
    manager, GPU, tracking components has to be set in Ash and passed to
    utility process. However, they are not set in an unit-test. Since video
    capture service is not functional, this CL introudes a fake
    VideoCaptureService in PersonalizationAppUserProviderImplTest to avoid
    any flakiness.
    
    Bug: b:303584485
    Test: Pass CQ test
    Change-Id: I02f3df4214f69a9f8f343ebb106648f20a5bae89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5376615
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Commit-Queue: Sean Li <seannli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1275365}

--- chrome/browser/ash/system_web_apps/apps/personalization_app/personalization_app_user_provider_impl_unittest.cc
+++ chrome/browser/ash/system_web_apps/apps/personalization_app/personalization_app_user_provider_impl_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <optional>
 
+#include "ash/capture_mode/fake_video_source_provider.h"
 #include "ash/public/cpp/default_user_image.h"
 #include "ash/webui/personalization_app/mojom/personalization_app.mojom.h"
 #include "base/containers/span.h"
@@ -38,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_manager/scoped_user_manager.h"
 #include "components/user_manager/user_image/user_image.h"
 #include "components/user_manager/user_manager.h"
+#include "content/public/browser/video_capture_service.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_task_environment.h"
 #include "content/public/test/test_web_ui.h"
@@ -46,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/remote.h"
 #include "mojo/public/cpp/test_support/test_utils.h"
 #include "services/data_decoder/public/cpp/test_support/in_process_data_decoder.h"
+#include "services/video_capture/public/mojom/video_capture_service.mojom.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/webui/web_ui_util.h"
@@ -140,6 +143,44 @@ class LocalStateUpdateWaiter : public user_manager::UserManager::Observer {
                           user_manager::UserManager::Observer>
       observation_{this};
   base::RunLoop run_loop_;
+};
+
+class FakeVideoCaptureService
+    : public video_capture::mojom::VideoCaptureService {
+ public:
+  FakeVideoCaptureService() {
+    content::OverrideVideoCaptureServiceForTesting(this);
+  }
+
+  ~FakeVideoCaptureService() override {
+    content::OverrideVideoCaptureServiceForTesting(nullptr);
+  }
+
+  // mojom::VideoCaptureService:
+  void InjectGpuDependencies(
+      mojo::PendingRemote<video_capture::mojom::AcceleratorFactory>
+          accelerator_factory) override {}
+
+  void ConnectToCameraAppDeviceBridge(
+      mojo::PendingReceiver<cros::mojom::CameraAppDeviceBridge> receiver)
+      override {}
+
+  void BindVideoCaptureDeviceFactory(
+      mojo::PendingReceiver<crosapi::mojom::VideoCaptureDeviceFactory> receiver)
+      override {}
+
+  void ConnectToVideoSourceProvider(
+      mojo::PendingReceiver<video_capture::mojom::VideoSourceProvider> receiver)
+      override {
+    fake_provider_.Bind(std::move(receiver));
+  }
+
+  void BindControlsForTesting(
+      mojo::PendingReceiver<video_capture::mojom::TestingControls> receiver)
+      override {}
+
+ private:
+  FakeVideoSourceProvider fake_provider_;
 };
 
 }  // namespace
@@ -273,6 +314,7 @@ class PersonalizationAppUserProviderImplTest : public testing::Test {
  private:
   base::test::ScopedFeatureList scoped_feature_list_;
   content::BrowserTaskEnvironment task_environment_;
+  FakeVideoCaptureService fake_video_capture_service_;
   user_manager::TypedScopedUserManager<ash::FakeChromeUserManager>
       user_manager_{std::make_unique<ash::FakeChromeUserManager>()};
   UserImageManagerRegistry user_image_manager_registry_{user_manager_.Get()};
