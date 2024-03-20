a58d0e55ae91e38d871f02df2d7cadfdb2d72195
Filipa Senra <fsenra@google.com>
2024-03-20 13:11:54 +0000

    [iOS][PageInfo] Add new badge to entry point
    The new badge on the page info entry point will show up a max of three
    times as recommended by Bling guidelines (go/blingusereducation - slide
    24).
    
    Screenshot: http://screen/8fjPzWmpd7fmxnc
    
    Fixed: b:328429116
    Bug: 1512580
    Change-Id: I485264181cb2d6f70c562f8227dcad37a5bdb823
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5349506
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275521}

--- ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_carousel_egtest.mm
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_carousel_egtest.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/signin/model/fake_system_identity.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey_ui_test_util.h"
+#import "ios/chrome/browser/ui/page_info/features.h"
 #import "ios/chrome/browser/ui/popup_menu/overflow_menu/feature_flags.h"
 #import "ios/chrome/browser/ui/popup_menu/popup_menu_constants.h"
 #import "ios/chrome/browser/ui/whats_new/constants.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/test_switches.h"
 #import "ios/testing/earl_grey/app_launch_manager.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
+#import "net/test/embedded_test_server/embedded_test_server.h"
 #import "ui/base/l10n/l10n_util.h"
 
 namespace {
@@ -62,6 +64,12 @@ id<GREYMatcher> GetSettingsDestinationWithErrorBadgeMatcher() {
 id<GREYMatcher> GetWhatsNewDestinationWithNewBadgeMatcher() {
   return grey_accessibilityID(
       GetDestinationIdWithNewBadge(kToolsMenuWhatsNewId));
+}
+
+// Get the matcher for the Page Info destination with a "New" badge.
+id<GREYMatcher> GetPageInfoDestinationWithNewBadgeMatcher() {
+  return grey_accessibilityID(
+      GetDestinationIdWithNewBadge(kToolsMenuSiteInformation));
 }
 
 // Cleans up the data related to the destination badge highlight features, e.g.,
@@ -263,6 +271,25 @@ void ResolvePassphraseErrorFromOverflowMenu() {
   [ChromeEarlGrey
       waitForUIElementToAppearWithMatcher:grey_accessibilityID(
                                               @"BubbleViewLabelIdentifier")];
+}
+
+// Tests that the Page Info new badge IPH shows up on the overflow menu
+// carousel.
+- (void)testPageInfoIPH {
+  AppLaunchConfiguration config;
+  config.iph_feature_enabled =
+      feature_engagement::kIPHiOSPageInfoRevampFeature.name;
+  config.features_enabled.push_back(kRevampPageInfoIos);
+  [[AppLaunchManager sharedManager] ensureAppLaunchedWithConfiguration:config];
+
+  // Open a page so Page Info is available.
+  GREYAssertTrue(self.testServer->Start(), @"Test server failed to start.");
+  [ChromeEarlGrey loadURL:self.testServer->GetURL("/")];
+
+  // Open the tools menu and see the new badge appear on the Page Info chip.
+  [ChromeEarlGreyUI openToolsMenu];
+  [ChromeEarlGrey waitForUIElementToAppearWithMatcher:
+                      GetPageInfoDestinationWithNewBadgeMatcher()];
 }
 
 @end
