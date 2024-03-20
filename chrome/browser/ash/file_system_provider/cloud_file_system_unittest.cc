bdd354db0ee7a3f578a7523d5c79efc4c62c17c4
Ben Reich <benreich@chromium.org>
2024-03-20 23:58:42 +0000

    [FspCache] Base64 encode the providers mount path
    The providers mount path contains an escaped version of the filesystem
    ID. This value is supplied by the FSP, let's base64 encode that value
    for now.
    
    Fixed: b/330596205
    Test: unit_tests --gtest_filter=FileSystemProviderCacheManagerTest*
    Test: unit_tests --gtest_filter=FileSystemProviderCloudFileSystemTest*
    Change-Id: I94391d8c9aadd08b831e00e76c0e3aaaf01b828d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5376955
    Reviewed-by: Cassy Chun-Crogan <cassycc@google.com>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275941}

--- chrome/browser/ash/file_system_provider/cloud_file_system_unittest.cc
+++ chrome/browser/ash/file_system_provider/cloud_file_system_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/file_system_provider/cloud_file_system.h"
 
+#include "base/base64.h"
 #include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/run_loop.h"
@@ -78,8 +79,11 @@ class FileSystemProviderCloudFileSystemTest : public testing::Test,
     // Wait until the CloudFileSystem content cache has been initialised.
     if (with_cache_manager) {
       base::RunLoop run_loop;
-      EXPECT_CALL(observer, OnContentCacheInitializeComplete(
-                                mount_path.BaseName(), base::File::FILE_OK))
+      EXPECT_CALL(
+          observer,
+          OnContentCacheInitializeComplete(
+              base::FilePath(base::Base64Encode(mount_path.BaseName().value())),
+              base::File::FILE_OK))
           .WillOnce(base::test::RunClosure(run_loop.QuitClosure()));
       run_loop.Run();
     }
