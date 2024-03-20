81ec91163c1146ea4e4e1a70d1d6089344cb922b
Adithya Srinivasan <adithyas@chromium.org>
2024-03-20 18:10:30 +0000

    LoadingPredictor: Record simplified subresource URL
    We don't need to record any parts of subresource URLs that aren't used
    for the network request (this is also what's used when creating a
    cache key) in PageRequestSummary. The OptimizationGuide predictions
    also don't include these parts of the URL.
    
    Change-Id: Iccc6f7968b79660b3390022634946a8cbd716c10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5382206
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275718}

--- chrome/browser/predictors/loading_data_collector.cc
+++ chrome/browser/predictors/loading_data_collector.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history/core/browser/history_service.h"
 #include "content/public/browser/browser_thread.h"
 #include "net/base/mime_util.h"
+#include "net/base/url_util.h"
 #include "net/http/http_response_headers.h"
 #include "services/network/public/mojom/fetch_api.mojom-shared.h"
 #include "third_party/blink/public/common/loader/resource_type_util.h"
@@ -121,10 +122,12 @@ void PageRequestSummary::UpdateOrAddResource(
       }
       UpdateOrAddToOrigins(url::Origin::Create(resource_load_info.final_url),
                            resource_load_info.network_info, is_low_priority);
+      const GURL final_url =
+          net::SimplifyUrlForRequest(resource_load_info.final_url);
       if (is_low_priority) {
-        low_priority_subresource_urls.insert(resource_load_info.final_url);
+        low_priority_subresource_urls.insert(final_url);
       } else {
-        subresource_urls.insert(resource_load_info.final_url);
+        subresource_urls.insert(final_url);
       }
       return;
   }
