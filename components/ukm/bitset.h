b5f044312070289daa9b6249ca6225e5eb4f42de
Jose Dapena Paz <jdapena@igalia.com>
2024-03-20 12:38:04 +0000

    IWYU: missing includes in ukm::BitSet
    Usage of uint8_t and std::memcpy require includes.
    
    Bug: 41455655
    Change-Id: Ib96b3c3595660fc05d37d61ba9ec36add18eba57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5370063
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Reviewed-by: Cait Phillips <caitkp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275509}

--- components/ukm/bitset.h
+++ components/ukm/bitset.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_UKM_BITSET_H_
 #define COMPONENTS_UKM_BITSET_H_
 
+#include <cstdint>
 #include <string>
 #include <vector>
 
