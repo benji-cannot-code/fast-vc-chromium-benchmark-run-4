514424fb0fe38dae76917ab9c8f775264890682d
Neil Kronlage <neilkr@meta.com>
2024-03-20 23:47:06 +0000

    Apply offset to Mouse Scroll Wheel events on Android
    Bug: 41485781
    Change-Id: If4d22f1af484086f5435b22298dee1f774605681
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5381849
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Wayne Jackson Jr. <wbjacksonjr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275934}

--- ui/android/java/src/org/chromium/ui/base/EventForwarder.java
+++ ui/android/java/src/org/chromium/ui/base/EventForwarder.java
@@ -629,6 +629,10 @@ public class EventForwarder {
             updateMouseEventState(event);
         }
 
+        if (event.getActionMasked() == MotionEvent.ACTION_SCROLL) {
+            event = createOffsetMotionEventIfNeeded(event);
+        }
+
         return EventForwarderJni.get()
                 .onGenericMotionEvent(
                         mNativeEventForwarder,
