100771fd53f44247e2d0628abd7644757793bb11
Dominik Röttsches <drott@chromium.org>
2024-03-20 13:44:53 +0000

    [gardening] Disable two accessibility tests on Linux and ChromeOS
    No culprit could be identified. First crrev.com/85938e08272ea was
    suspected, but the revert in
    https://chromium-review.googlesource.com/c/chromium/src/+/5382398 did
    not lead to the expected outcome of fixing the test failures.
    
    Bug: 330450836
    AX-Relnotes: n/a.
    Change-Id: I660db88542ddced945fe0bfe8a5115a2863ba124
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5380339
    Owners-Override: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Daniil Sakhapov <sakhapov@chromium.org>
    Auto-Submit: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275537}

--- chrome/browser/ash/accessibility/accessibility_highlights_browsertest.cc
+++ chrome/browser/ash/accessibility/accessibility_highlights_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <memory>
+#include "build/build_config.h"
 
 #include "ash/accessibility/ui/accessibility_cursor_ring_layer.h"
 #include "ash/accessibility/ui/accessibility_focus_ring_controller_impl.h"
@@ -111,8 +112,14 @@ IN_PROC_BROWSER_TEST_F(AccessibilityHighlightsBrowserTest,
   EXPECT_FALSE(controller->cursor_layer_for_testing());
 }
 
+// TODO(crbug.com/330450836): Re-enable this test
+#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
+#define MAYBE_CaretHighlightWebContents DISABLED_CaretHighlightWebContents
+#else
+#define MAYBE_CaretHighlightWebContents CaretHighlightWebContents
+#endif
 IN_PROC_BROWSER_TEST_F(AccessibilityHighlightsBrowserTest,
-                       CaretHighlightWebContents) {
+                       MAYBE_CaretHighlightWebContents) {
   AccessibilityFocusRingControllerImpl* controller =
       Shell::Get()->accessibility_focus_ring_controller();
   EXPECT_FALSE(controller->caret_layer_for_testing());
@@ -218,7 +225,14 @@ IN_PROC_BROWSER_TEST_F(AccessibilityHighlightsBrowserTest,
   prefs->SetBoolean(prefs::kAccessibilityCaretHighlightEnabled, false);
 }
 
-IN_PROC_BROWSER_TEST_F(AccessibilityHighlightsBrowserTest, FocusHighlight) {
+// TODO(crbug.com/330450836): Re-enable this test
+#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
+#define MAYBE_FocusHighlight DISABLED_FocusHighlight
+#else
+#define MAYBE_FocusHighlight FocusHighlight
+#endif
+IN_PROC_BROWSER_TEST_F(AccessibilityHighlightsBrowserTest,
+                       MAYBE_FocusHighlight) {
   AccessibilityFocusRingControllerImpl* controller =
       Shell::Get()->accessibility_focus_ring_controller();
   PrefService* prefs = GetProfile()->GetPrefs();
