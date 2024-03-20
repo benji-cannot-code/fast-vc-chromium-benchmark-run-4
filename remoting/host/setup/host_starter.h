ada1e86ca90468382dd2bcbc88472622828ac8d4
Joe Downing <joedow@google.com>
2024-03-20 18:02:02 +0000

    Rename HostStarter to OAuthHostStarter
    The original HostStarter impl requires the user to visit the /headless
    page to generate an authorization_code which is used to register the
    new host instance on behalf of that user.
    
    Newer registration mechanisms (i.e. Corp and Cloud) do not require an
    authorization_code as we have other means of determining the machine
    and user identity. Thus I'm renaming the original HostStarter class
    and will introduce a HostStartBase class in a follow-up CL which the
    {Cloud|Corp|OAuth}HostStarter class can derive from.
    
    Bug: b/320745119, b/330404222
    Change-Id: Ifff2bd951eb57b28a2454751e46d658fb9adae3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5381892
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Auto-Submit: Joe Downing <joedow@chromium.org>
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275713}

--- remoting/host/setup/host_starter.h
+++ remoting/host/setup/host_starter.h
@@ -6,15 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef REMOTING_HOST_SETUP_HOST_STARTER_H_
 #define REMOTING_HOST_SETUP_HOST_STARTER_H_
 
-#include <memory>
 #include <string>
 
 #include "base/functional/callback.h"
-#include "base/memory/scoped_refptr.h"
-
-namespace network {
-class SharedURLLoaderFactory;
-}
 
 namespace remoting {
 
@@ -69,10 +63,6 @@ class HostStarter {
   };
 
   typedef base::OnceCallback<void(Result)> CompletionCallback;
-
-  // Creates a HostStarter instance.
-  static std::unique_ptr<HostStarter> Create(
-      scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory);
 
   virtual ~HostStarter();
 
--- remoting/host/setup/start_host_main.cc
+++ remoting/host/setup/start_host_main.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/base/url_request_context_getter.h"
 #include "remoting/host/setup/corp_host_starter.h"
 #include "remoting/host/setup/host_starter.h"
+#include "remoting/host/setup/oauth_host_starter.h"
 #include "remoting/host/setup/pin_validator.h"
 #include "remoting/host/usage_stats_consent.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
@@ -344,7 +345,7 @@ int StartHostMain(int argc, char** argv) {
         ProvisionCorpMachine(url_loader_factory_owner.GetURLLoaderFactory());
   } else {
     host_starter =
-        HostStarter::Create(url_loader_factory_owner.GetURLLoaderFactory());
+        CreateOAuthHostStarter(url_loader_factory_owner.GetURLLoaderFactory());
   }
 
   host_starter->StartHost(std::move(params), base::BindOnce(&OnDone));
