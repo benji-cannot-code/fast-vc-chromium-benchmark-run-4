91f9c68c54759681cbe6cca23dfe6c1c005e7fcc
Cam Bickel <cambickel@google.com>
2024-03-20 20:25:32 +0000

    brightness: Add success metric TimeUntilFirstBrightnessChange
    This CL adds a new patterned histogram that tracks the first display
    brightness change event that occurs since reboot. When that event
    happens, a metric is recorded with the number of seconds that have
    elapsed since startup (if on the login screen) or since login (if the
    user is logged-in).
    
    This histogram contains additional dimensions for power source at the
    time of the event, and the specific brightness action that took place.
    
    Note: the changes in shell.cc have been made since
    BrightnessControllerChromeos now depends on the SessionController, and
    needs to be initialized after / destroyed before SessionController.
    
    I tested this change on my DUT and confirmed that the metrics are logged
    as expected.
    
    Test: ash_unittests --gtest_filter=*BrightnessControllerChromeosTest*
    Bug: b/328287584
    Change-Id: I6d17378ecfc8d4008253caf9602f9d39fd1c1aef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5369126
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Camden Bickel <cambickel@google.com>
    Reviewed-by: Wenyu Zhang <zhangwenyu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1275805}

--- ash/shell.cc
+++ ash/shell.cc
@@ -674,9 +674,7 @@ DeskProfilesDelegate* Shell::GetDeskProfilesDelegate() {
 // Shell, private:
 
 Shell::Shell(std::unique_ptr<ShellDelegate> shell_delegate)
-    : brightness_control_delegate_(
-          std::make_unique<system::BrightnessControllerChromeos>()),
-      focus_cycler_(std::make_unique<FocusCycler>()),
+    : focus_cycler_(std::make_unique<FocusCycler>()),
       ime_controller_(std::make_unique<ImeControllerImpl>()),
       immersive_context_(std::make_unique<ImmersiveContextAsh>()),
       webauthn_dialog_controller_(
@@ -1165,6 +1163,7 @@ Shell::~Shell() {
 
   // Observes `SessionController` and must be destroyed before it.
   federated_service_controller_.reset();
+  brightness_control_delegate_.reset();
 
   UsbguardClient::Shutdown();
 
@@ -1225,6 +1224,11 @@ void Shell::Init(
 
   // Initialized early since it is used by some other objects.
   keyboard_capability_ = std::make_unique<ui::KeyboardCapability>();
+
+  // This needs to be initialized after SessionController.
+  brightness_control_delegate_ =
+      std::make_unique<system::BrightnessControllerChromeos>(
+          session_controller_.get());
 
   // These controllers call Shell::Get() in their constructors, so they cannot
   // be in the member initialization list.
