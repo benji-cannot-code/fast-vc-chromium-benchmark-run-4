921468014a07f6f6903919467b20c49199e6bb27
Ana Salazar <anasalazar@chromium.org>
2024-03-20 22:38:42 +0000

    Create Downloaded by you collection
    This collection will be shown on top of other collections.
    The collection contains all apps that do not have a category defined
    (AppCollections::kUnknown category)
    
    Folder items also carry the category kUnknown, so as a fix to avoid showing folders in app collections, skip folder items when fetching the items that belong to a collection.
    
    Bug: None
    Change-Id: I716a8cf9e5c876e8a78d8d13f68fb3606125ed24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5381454
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275893}

--- ash/app_list/views/apps_collection_section_view.cc
+++ ash/app_list/views/apps_collection_section_view.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/app_list/app_collections_constants.h"
 #include "ash/app_list/app_list_view_delegate.h"
+#include "ash/app_list/model/app_list_folder_item.h"
 #include "ash/app_list/model/app_list_item.h"
 #include "ash/app_list/model/app_list_item_list.h"
 #include "ash/app_list/model/app_list_model.h"
@@ -50,8 +51,13 @@ std::vector<AppListItem*> GetAppListItemsForCollection(
   AppListItemList* items = model->top_level_item_list();
 
   for (size_t i = 0; i < items->item_count(); i++) {
-    if (items->item_at(i)->collection_id() == collection_id) {
-      collection.emplace_back(items->item_at(i));
+    AppListItem* app_item = items->item_at(i);
+    if (app_item->is_folder()) {
+      // Ignore app folder items.
+      continue;
+    }
+    if (app_item->collection_id() == collection_id) {
+      collection.emplace_back(app_item);
     }
   }
   return collection;
