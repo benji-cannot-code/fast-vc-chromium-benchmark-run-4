40dc309a2b7c17d9aa609baae8f22c998d578b0b
Anastasia Helfinstein <anastasi@google.com>
2024-03-20 23:05:45 +0000

    Reland "[a11y extensions] Remove importModules from test base"
    This is a reland of commit 468a0c11995d1a1625f449bc8f4def7a3a2c0095
    
    Was reverted because test SwitchAccessTabNodeTest.FindCloseButton was failing after upload on browser_tests_with_lacros.
    
    Original change's description:
    > [a11y extensions] Remove importModules from test base
    >
    > In addition to removing calls to importModules, we need to add a few imports to select_to_speak_main. This is because the tests rely on some infrastructure from common/ that Select-to-Speak does not otherwise use, and with the updated import method modules need to be loaded before they can be imported in tests.
    >
    > AX-Relnotes: n/a.
    > Bug: b/260590502
    > Change-Id: I94f76553c1c9852c683937008f0da67dec3d996e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5321574
    > Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    > Reviewed-by: Katie Dektar <katie@chromium.org>
    > Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1272501}
    
    AX-Relnotes: n/a.
    Cq-Include-Trybots: luci.chromium.try:chromeos-amd64-generic-lacros-dbg,lacros-amd64-generic-rel-gtest,lacros-arm-generic-rel
    Bug: b/260590502
    Change-Id: I85f07e073b073467d04318feff83a4b71437482e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372890
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275903}

--- chrome/browser/resources/chromeos/accessibility/switch_access/switch_access_e2e_test_base.js
+++ chrome/browser/resources/chromeos/accessibility/switch_access/switch_access_e2e_test_base.js
@@ -78,8 +78,7 @@ SwitchAccessE2ETest = class extends E2ETestBase {
    * @param {function()} callback
    */
   waitForPredicate(predicate, callback) {
-    this.listenUntil(
-        predicate, Navigator.byItem.desktopNode, 'childrenChanged', callback);
+    this.listenUntil(predicate, this.desktop_, 'childrenChanged', callback);
   }
 
   /**
