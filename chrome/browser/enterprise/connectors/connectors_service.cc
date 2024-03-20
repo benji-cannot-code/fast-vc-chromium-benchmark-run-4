6911f2fbde92e8b31b6352038a916c921da8c5b3
Dominique Fauteux-Chapleau <domfc@chromium.org>
2024-03-20 17:06:19 +0000

    Fix ConnectorsService::GetProfileDmToken
    This CL tweaks the existing user DM token fetching code in
    `ConnectorsService` to work with the OidcAuthProfileManagement
    experiment.
    
    Change-Id: I58e9cca5ed7a6b178671213ffb9f330eb8e68a3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5381562
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275666}

--- chrome/browser/enterprise/connectors/connectors_service.cc
+++ chrome/browser/enterprise/connectors/connectors_service.cc
@@ -64,6 +64,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_manager/user.h"
 #include "components/user_manager/user_manager.h"
 #include "extensions/common/constants.h"
+#else
+#include "components/policy/core/common/cloud/profile_cloud_policy_manager.h"
 #endif
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
@@ -532,8 +534,12 @@ std::optional<ConnectorsService::DmToken> ConnectorsService::GetProfileDmToken()
     const {
   Profile* profile = Profile::FromBrowserContext(context_);
 
-  policy::UserCloudPolicyManager* policy_manager =
+  policy::CloudPolicyManager* policy_manager =
       profile->GetUserCloudPolicyManager();
+  if (!policy_manager) {
+    policy_manager = profile->GetProfileCloudPolicyManager();
+  }
+
   if (!policy_manager || !policy_manager->IsClientRegistered()) {
     return std::nullopt;
   }
