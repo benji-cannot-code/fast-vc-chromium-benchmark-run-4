587cac748097fce9c94a08f6bf8ab85b078ffa51
Anna Tsvirchkova <atsvirchkova@google.com>
2024-03-20 13:00:15 +0000

    [UPM] Use the check which respects the upstream implementation in IsGmsCoreUpdateRequired
    Bug: b/321223016
    Change-Id: Icd6dbbd9d2651f7cd9637a8c7db88c0c1aabc4ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5377872
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1275517}

--- components/password_manager/core/browser/password_store/split_stores_and_local_upm_unittest.cc
+++ components/password_manager/core/browser/password_store/split_stores_and_local_upm_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/build_info.h"
 #include "base/test/scoped_feature_list.h"
 #include "components/password_manager/core/browser/features/password_features.h"
+#include "components/password_manager/core/browser/password_manager_buildflags.h"
 #include "components/password_manager/core/common/password_manager_pref_names.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/testing_pref_service.h"
@@ -101,6 +102,7 @@ TEST_P(SplitStoresAndLocalUpmTestIsGmsCoreUpdateRequired,
       password_manager::prefs::kCurrentMigrationVersionToGoogleMobileServices,
       p.was_initial_migration_done);
 
+#if !BUILDFLAG(USE_LOGIN_DATABASE_AS_BACKEND)
   bool expected_is_update_required =
       base::android::BuildInfo::GetInstance()->is_automotive()
           ? p.expected_is_update_required_automotive
@@ -108,6 +110,10 @@ TEST_P(SplitStoresAndLocalUpmTestIsGmsCoreUpdateRequired,
   EXPECT_EQ(expected_is_update_required,
             IsGmsCoreUpdateRequired(pref_service(), p.is_pwd_sync_enabled,
                                     p.gms_version));
+#else
+  EXPECT_EQ(false, IsGmsCoreUpdateRequired(
+                       pref_service(), p.is_pwd_sync_enabled, p.gms_version));
+#endif
 }
 
 INSTANTIATE_TEST_SUITE_P(
