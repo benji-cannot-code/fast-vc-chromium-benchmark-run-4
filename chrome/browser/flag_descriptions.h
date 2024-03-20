c08a459137ba5a7c22aad0f97180ba978d9b90bb
Jihad Hanna <jihadghanna@google.com>
2024-03-20 11:36:27 +0000

    Removed undo chrome flag
    Bug: 40266549
    Fixed: 330367110
    Change-Id: Iceacdc0eaf2cca32b5f3f08055f64be4719f73c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5383094
    Auto-Submit: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1275487}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1323,9 +1323,6 @@ extern const char kLocationBarModelOptimizationsDescription[];
 extern const char kLogJsConsoleMessagesName[];
 extern const char kLogJsConsoleMessagesDescription[];
 
-extern const char kUndoAutofillName[];
-extern const char kUndoAutofillDescription[];
-
 extern const char kMediaRouterCastAllowAllIPsName[];
 extern const char kMediaRouterCastAllowAllIPsDescription[];
 
