65c4d80cc8dd64136645e8b05b00466f3629df14
Robert Ogden <robertogden@chromium.org>
2024-03-20 03:51:51 +0000

    Have the XNNPACK build script download the Android NDK
    Bazel wants the NDK to run the aquery command so fetch it and make it
    available from the script beforehand.
    
    Change-Id: Ib13352e86e4ddd0fb6fe95848ea947caf09997db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5381860
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Auto-Submit: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275382}

--- third_party/xnnpack/generate_build_gn.py
+++ third_party/xnnpack/generate_build_gn.py
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import atexit
 import collections
+import io
 import json
 import logging
 import os
@@ -40,6 +41,8 @@ import platform
 import shutil
 import subprocess
 import sys
+import urllib.request
+import zipfile
 
 from dataclasses import dataclass, field
 
@@ -629,6 +632,17 @@ def MakeXNNPACKDepsList(target_sss):
 
   return deps_list
 
+def EnsureAndroidNDK():
+  """
+  Ensures that the Android NDK is available and bazel can find it later.
+  """
+  if 'ANDROID_NDK_HOME' in os.environ:
+    return
+  logging.info('Downloading a copy of the Android NDK for bazel')
+  resp = urllib.request.urlopen('https://dl.google.com/android/repository/android-ndk-r19c-linux-x86_64.zip')
+  logging.info('Unpacking the Android NDK')
+  zipfile.ZipFile(io.BytesIO(resp.read())).extractall(path='/tmp/')
+  os.environ['ANDROID_NDK_HOME'] = '/tmp/android-ndk-r19c'
 
 def MakeXNNPACKSourceSet(ss):
   """
@@ -652,6 +666,8 @@ def main():
     logging.error(f'{_AARCH64_LINUX_GCC} and {_X86_64_LINUX_GCC} are required!')
     logging.error('On x86-64 Debian, install gcc-aarch64-linux-gnu and gcc.')
     sys.exit(1)
+
+  EnsureAndroidNDK()
 
   CreateToolchainFiles()
 
