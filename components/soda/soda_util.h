464fd2d3916aa570978e3615f7852c2f58c56737
Yaowei Zhou <yaowei.zhou@intel.com>
2024-03-20 12:06:09 +0000

    Web `Speech to Text` with SODA backend
    Support Web Speech Recognition with local On-Device Soda.
    
    Bug: 1495388
    Change-Id: Ic72176042d2dc689ec35c64d1ff2e0b059465947
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5252833
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Commit-Queue: Jianlin Qiu <jianlin.qiu@intel.com>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275497}

---
+++ components/soda/soda_util.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_SODA_SODA_UTIL_H_
+#define COMPONENTS_SODA_SODA_UTIL_H_
+
+namespace speech {
+
+// Returns whether the Speech On-Device API is supported in
+// Chrome. This can depend on e.g. Chrome feature flags, platform/OS, supported
+// CPU instructions.
+bool IsOnDeviceSpeechRecognitionSupported();
+
+}  // namespace speech
+
+#endif  // COMPONENTS_SODA_SODA_UTIL_H_
--- extensions/shell/browser/shell_speech_recognition_manager_delegate.cc
+++ extensions/shell/browser/shell_speech_recognition_manager_delegate.cc
@@ -29,6 +29,11 @@ ShellSpeechRecognitionManagerDelegate::
 ~ShellSpeechRecognitionManagerDelegate() {
 }
 
+#if !BUILDFLAG(IS_FUCHSIA) && !BUILDFLAG(IS_ANDROID)
+void ShellSpeechRecognitionManagerDelegate::BindSpeechRecognitionContext(
+    mojo::PendingReceiver<media::mojom::SpeechRecognitionContext> receiver) {}
+#endif  // !BUILDFLAG(IS_FUCHSIA) && !BUILDFLAG(IS_ANDROID)
+
 void ShellSpeechRecognitionManagerDelegate::OnRecognitionStart(int session_id) {
 }
 
