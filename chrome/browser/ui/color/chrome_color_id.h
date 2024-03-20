b41a23149a1a1c9d68335e470dafe8171b3604b6
Riley Tatum <rtatum@google.com>
2024-03-20 19:34:50 +0000

    Add a GM2 color for the wallpaper search button.
    Fixes issue where users might see a red wallpaper search button if they haven't updated to a GM3 theme.
    
    Spec is in bug comments
    
    non-white bg: screenshot/A4SjMne9TdVzf3x
    theme w/ white bg: screenshot/A3z44FPN6EwrrKs
    
    Change-Id: I690c86f33c3d7ae2ab7cce247850f26145333b6c
    Bug: 330008140
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5379349
    Auto-Submit: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275776}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -268,6 +268,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorNewTabPageTextUnthemed) \
   E_CPONLY(kColorNewTabPageTextLight) \
   E_CPONLY(kColorNewTabPageWallpaperSearchButtonBackground) \
+  E_CPONLY(kColorNewTabPageWallpaperSearchButtonBackgroundHovered) \
   E_CPONLY(kColorNewTabPageWallpaperSearchButtonForeground) \
   /* Omnibox colors. */ \
   E_CPONLY(kColorOmniboxAnswerIconBackground) \
