c84457f2e307b490105ec4721ff8a7c6924811ca
James Lee <ljjlee@google.com>
2024-03-20 15:27:24 +0000

    Abandon ClassifyUrl request priority experiment
    The experiment has not shown any meaningful improvement in latency.
    
    Bug: 313600979
    Change-Id: I7e60a721fee11455d0cfc9d1a98ae1343af3757f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372718
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Auto-Submit: James Lee <ljjlee@google.com>
    Commit-Queue: James Lee <ljjlee@google.com>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275593}

--- components/supervised_user/core/browser/kids_chrome_management_url_checker_client.cc
+++ components/supervised_user/core/browser/kids_chrome_management_url_checker_client.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 #include <utility>
 
-#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/functional/callback_helpers.h"
@@ -76,9 +75,6 @@ std::unique_ptr<ProtoFetcher<kidsmanagement::ClassifyUrlResponse>> ClassifyURL(
 }
 
 FetcherConfig GetFetcherConfig() {
-  if (base::FeatureList::IsEnabled(kHighestRequestPriorityForClassifyUrl)) {
-    return kClassifyUrlConfigWithHighestPriority;
-  }
   return kClassifyUrlConfig;
 }
 
