3367620cffbd5238492fee75921745e8ffa93271
Timofey Chudakov <tchudakov@google.com>
2024-03-21 09:21:49 +0000

    [Mullhet] Implement full form preview and filling.
    This CL adds logic to distinguish between cases when the MF
    suggestions are triggered on a password or non-password form.
    This is done by querying the password form cache.
    
    Demo: http://screencast/cast/NjI3MDUyMTA0MDY5OTM5MnwzOWMwNzAyNS1jMA
    
    Bug: b/326033879
    Change-Id: Ie0f102efc79ad4c1e486369db5688a98237d51cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5340133
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1276107}

---
+++ components/password_manager/core/browser/mock_password_form_cache.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/password_manager/core/browser/mock_password_form_cache.h"
+
+namespace password_manager {
+
+MockPasswordFormCache::MockPasswordFormCache() = default;
+
+MockPasswordFormCache::~MockPasswordFormCache() = default;
+
+}  // namespace password_manager
--- components/password_manager/core/browser/password_manager.cc
+++ components/password_manager/core/browser/password_manager.cc
@@ -1297,6 +1297,10 @@ std::optional<PasswordForm> PasswordManager::GetSubmittedCredentials() {
   return std::nullopt;
 }
 
+const PasswordFormCache* PasswordManager::GetPasswordFormCache() const {
+  return &password_form_cache_;
+}
+
 void PasswordManager::ResetSubmittedManager() {
   client_->ResetSubmissionTrackingAfterTouchToFill();
 
