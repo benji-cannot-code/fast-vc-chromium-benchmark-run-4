34bad6422273c5a92714f644f843344d873f0d56
Tsuyoshi Horo <horo@chromium.org>
2024-03-21 00:30:22 +0000

    Introduce UMA of the result of reading system DNS config on windows
    We received a bug report that may be caused by some error in reading the
    system DNS configuration on Windows.
    To investigate this issue, this CL introduce UMA of the result of
    reading system DNS config on Windows.
    
    Bug: 327732682
    Change-Id: I718eec6b5fb388be6053770ad4137db3c0f7fcd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5369615
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275954}

--- net/dns/dns_config_service_win_unittest.cc
+++ net/dns/dns_config_service_win_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/memory/free_deleter.h"
+#include "base/test/gmock_expected_support.h"
 #include "net/base/ip_address.h"
 #include "net/base/ip_endpoint.h"
 #include "net/dns/public/dns_protocol.h"
@@ -179,13 +180,14 @@ TEST(DnsConfigServiceWinTest, ConvertAdapterAddresses) {
       expected_nameservers.push_back(IPEndPoint(ip, port));
     }
 
-    std::optional<DnsConfig> config =
-        internal::ConvertSettingsToDnsConfig(settings);
+    base::expected<DnsConfig, ReadWinSystemDnsSettingsError> config_or_error =
+        internal::ConvertSettingsToDnsConfig(std::move(settings));
     bool expected_success = !expected_nameservers.empty();
-    EXPECT_EQ(expected_success, config.has_value());
-    if (config.has_value()) {
-      EXPECT_EQ(expected_nameservers, config->nameservers);
-      EXPECT_THAT(config->search, testing::ElementsAre(t.expected_suffix));
+    EXPECT_EQ(expected_success, config_or_error.has_value());
+    if (config_or_error.has_value()) {
+      EXPECT_EQ(expected_nameservers, config_or_error->nameservers);
+      EXPECT_THAT(config_or_error->search,
+                  testing::ElementsAre(t.expected_suffix));
     }
   }
 }
@@ -384,10 +386,12 @@ TEST(DnsConfigServiceWinTest, ConvertSuffixSearch) {
     settings.dnscache_devolution = t.input_settings.dnscache_devolution;
     settings.tcpip_devolution = t.input_settings.tcpip_devolution;
 
-    EXPECT_THAT(
-        internal::ConvertSettingsToDnsConfig(settings),
-        testing::Optional(testing::Field(
-            &DnsConfig::search, testing::ElementsAreArray(t.expected_search))));
+    ASSERT_OK_AND_ASSIGN(
+        DnsConfig dns_config,
+        internal::ConvertSettingsToDnsConfig(std::move(settings)));
+    EXPECT_THAT(dns_config,
+                testing::Field(&DnsConfig::search,
+                               testing::ElementsAreArray(t.expected_search)));
   }
 }
 
@@ -410,10 +414,12 @@ TEST(DnsConfigServiceWinTest, AppendToMultiLabelName) {
     WinDnsSystemSettings settings;
     settings.addresses = CreateAdapterAddresses(infos);
     settings.append_to_multi_label_name = t.input;
-    EXPECT_THAT(
-        internal::ConvertSettingsToDnsConfig(settings),
-        testing::Optional(testing::Field(&DnsConfig::append_to_multi_label_name,
-                                         testing::Eq(t.expected_output))));
+    ASSERT_OK_AND_ASSIGN(
+        DnsConfig dns_config,
+        internal::ConvertSettingsToDnsConfig(std::move(settings)));
+    EXPECT_THAT(dns_config,
+                testing::Field(&DnsConfig::append_to_multi_label_name,
+                               testing::Eq(t.expected_output)));
   }
 }
 
@@ -436,11 +442,11 @@ TEST(DnsConfigServiceWinTest, HaveNRPT) {
     WinDnsSystemSettings settings;
     settings.addresses = CreateAdapterAddresses(infos);
     settings.have_name_resolution_policy = t.have_nrpt;
-    std::optional<DnsConfig> config =
-        internal::ConvertSettingsToDnsConfig(settings);
-    ASSERT_TRUE(config.has_value());
-    EXPECT_EQ(t.unhandled_options, config->unhandled_options);
-    EXPECT_EQ(t.have_nrpt, config->use_local_ipv6);
+    ASSERT_OK_AND_ASSIGN(
+        DnsConfig dns_config,
+        internal::ConvertSettingsToDnsConfig(std::move(settings)));
+    EXPECT_EQ(t.unhandled_options, dns_config.unhandled_options);
+    EXPECT_EQ(t.have_nrpt, dns_config.use_local_ipv6);
   }
 }
 
@@ -463,10 +469,11 @@ TEST(DnsConfigServiceWinTest, HaveProxy) {
     WinDnsSystemSettings settings;
     settings.addresses = CreateAdapterAddresses(infos);
     settings.have_proxy = t.have_proxy;
-    EXPECT_THAT(
-        internal::ConvertSettingsToDnsConfig(settings),
-        testing::Optional(testing::Field(&DnsConfig::unhandled_options,
-                                         testing::Eq(t.unhandled_options))));
+    ASSERT_OK_AND_ASSIGN(
+        DnsConfig dns_config,
+        internal::ConvertSettingsToDnsConfig(std::move(settings)));
+    EXPECT_THAT(dns_config, testing::Field(&DnsConfig::unhandled_options,
+                                           testing::Eq(t.unhandled_options)));
   }
 }
 
@@ -480,9 +487,11 @@ TEST(DnsConfigServiceWinTest, UsesVpn) {
 
   WinDnsSystemSettings settings;
   settings.addresses = CreateAdapterAddresses(infos);
-  EXPECT_THAT(internal::ConvertSettingsToDnsConfig(settings),
-              testing::Optional(testing::Field(&DnsConfig::unhandled_options,
-                                               testing::IsTrue())));
+  ASSERT_OK_AND_ASSIGN(
+      DnsConfig dns_config,
+      internal::ConvertSettingsToDnsConfig(std::move(settings)));
+  EXPECT_THAT(dns_config,
+              testing::Field(&DnsConfig::unhandled_options, testing::IsTrue()));
 }
 
 // Setting adapter specific nameservers should set `unhandled_options`.
@@ -501,9 +510,11 @@ TEST(DnsConfigServiceWinTest, AdapterSpecificNameservers) {
 
   WinDnsSystemSettings settings;
   settings.addresses = CreateAdapterAddresses(infos);
-  EXPECT_THAT(internal::ConvertSettingsToDnsConfig(settings),
-              testing::Optional(testing::Field(&DnsConfig::unhandled_options,
-                                               testing::IsTrue())));
+  ASSERT_OK_AND_ASSIGN(
+      DnsConfig dns_config,
+      internal::ConvertSettingsToDnsConfig(std::move(settings)));
+  EXPECT_THAT(dns_config,
+              testing::Field(&DnsConfig::unhandled_options, testing::IsTrue()));
 }
 
 // Setting adapter specific nameservers for non operational adapter should not
@@ -523,9 +534,11 @@ TEST(DnsConfigServiceWinTest, AdapterSpecificNameserversForNo) {
 
   WinDnsSystemSettings settings;
   settings.addresses = CreateAdapterAddresses(infos);
-  EXPECT_THAT(internal::ConvertSettingsToDnsConfig(settings),
-              testing::Optional(testing::Field(&DnsConfig::unhandled_options,
-                                               testing::IsFalse())));
+  ASSERT_OK_AND_ASSIGN(
+      DnsConfig dns_config,
+      internal::ConvertSettingsToDnsConfig(std::move(settings)));
+  EXPECT_THAT(dns_config, testing::Field(&DnsConfig::unhandled_options,
+                                         testing::IsFalse()));
 }
 
 }  // namespace
