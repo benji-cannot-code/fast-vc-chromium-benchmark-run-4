e2fdc7246cf00c0ea95498c33817fe027aa7dee7
Teresa Mao <temao@chromium.org>
2024-03-21 00:45:04 +0000

    Flags UI: Update search field focus behavior.
    - Focus remains on search field after clearing search.
    - Don't blur the searchbox when Enter is pressed.
    
    Bug: 327269048
    Change-Id: I742b6b65d8fbe17596d1ce8a2171f8d6f4bfb351
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5373420
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275962}

--- components/flags_ui/resources/app.ts
+++ components/flags_ui/resources/app.ts
@@ -71,7 +71,6 @@ export class FlagSearch {
           this.searchBox.focus();
           break;
         case 'Escape':
-        case 'Enter':
           this.searchBox.blur();
           break;
       }
@@ -86,6 +85,7 @@ export class FlagSearch {
   clearSearch() {
     this.searchBox.value = '';
     this.doSearch();
+    this.searchBox.focus();
   }
 
   /**
