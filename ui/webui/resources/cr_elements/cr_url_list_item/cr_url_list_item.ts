c8b6e568563cb82e2c20b0d9f47925d6d5f72907
John Lee <johntlee@chromium.org>
2024-03-21 18:37:21 +0000

    WebUI: Expand slotted suffix content in cr-url-list-item
    Rename "suffix-icons" to "suffix" to just make it semantically more
    fitting to include other content beyond just icons. This is to
    support using cr-url-list-item on History.
    
    Bug: 329688583
    Change-Id: I33f03ce7a373f7758030f53504a475072fe3d41a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5383582
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276395}

--- ui/webui/resources/cr_elements/cr_url_list_item/cr_url_list_item.ts
+++ ui/webui/resources/cr_elements/cr_url_list_item/cr_url_list_item.ts
@@ -45,6 +45,10 @@ export class CrUrlListItemElement extends CrUrlListItemElementBase {
 
   static get properties() {
     return {
+      alwaysShowSuffix: {
+        type: Boolean,
+        reflectToAttribute: true,
+      },
       buttonAriaLabel: String,
       buttonAriaDescription: String,
       count: Number,
@@ -116,6 +120,7 @@ export class CrUrlListItemElement extends CrUrlListItemElementBase {
     };
   }
 
+  alwaysShowSuffix: boolean;
   buttonAriaLabel?: string;
   buttonAriaDescription?: string;
   count?: number;
