09e1fc3e3260593e4a4e4a27a88f5ac45de25d1c
Joe Antonetti <joeantonetti@google.com>
2024-03-21 19:13:12 +0000

    [Tether] Wired SecureChannelTetherAvailabilityOperationOrchestrator to HostScanner
    This change replaces a direct call to TetherAvailabilityOperation with
    a call to the orchestrator in HostScannerImpl.
    
    As Secure Channel only supports one device as of now, any calls to the
    host prioritizer have been removed. A later CL in the stack will reintroduce
    this to the orchestrator.
    
    This change also adds a series of test classes to make testing easier.
    
    TEST=Manually verified by attempting to connect to tether on a local device and added unit tests
    
    Change-Id: I86e4f2e921a6459afe99f7f174875b7070be1f21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5318471
    Commit-Queue: Joe Antonetti <joeantonetti@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276421}

--- chromeos/ash/components/tether/message_transfer_operation.cc
+++ chromeos/ash/components/tether/message_transfer_operation.cc
@@ -100,8 +100,9 @@ MessageTransferOperation::MessageTransferOperation(
 
 MessageTransferOperation::~MessageTransferOperation() {
   // If initialization never occurred, devices were never registered.
-  if (!initialized_)
+  if (!initialized_) {
     return;
+  }
 
   shutting_down_ = true;
 
@@ -110,8 +111,9 @@ MessageTransferOperation::~MessageTransferOperation() {
   // down (see crbug.com/761106). Note that a copy of |remote_devices_| is used
   // here because UnregisterDevice() will modify |remote_devices_| internally.
   multidevice::RemoteDeviceRefList remote_devices_copy = remote_devices_;
-  for (const auto& remote_device : remote_devices_copy)
+  for (const auto& remote_device : remote_devices_copy) {
     UnregisterDevice(remote_device);
+  }
 }
 
 void MessageTransferOperation::Initialize() {
@@ -189,8 +191,9 @@ void MessageTransferOperation::UnregisterDevice(
     remote_device_to_client_channel_observer_map_.erase(remote_device);
   }
 
-  if (!shutting_down_ && remote_devices_.empty())
+  if (!shutting_down_ && remote_devices_.empty()) {
     OnOperationFinished();
+  }
 }
 
 int MessageTransferOperation::SendMessageToDevice(
@@ -273,8 +276,9 @@ void MessageTransferOperation::StartTimerForDevice(
 
 void MessageTransferOperation::StopTimerForDeviceIfRunning(
     multidevice::RemoteDeviceRef remote_device) {
-  if (!remote_device_to_timer_map_[remote_device])
+  if (!remote_device_to_timer_map_[remote_device]) {
     return;
+  }
 
   remote_device_to_timer_map_[remote_device]->Stop();
   remote_device_to_timer_map_.erase(remote_device);
@@ -293,8 +297,9 @@ void MessageTransferOperation::OnTimeout(
 std::optional<multidevice::RemoteDeviceRef>
 MessageTransferOperation::GetRemoteDevice(const std::string& device_id) {
   for (auto& remote_device : remote_devices_) {
-    if (remote_device.GetDeviceId() == device_id)
+    if (remote_device.GetDeviceId() == device_id) {
       return remote_device;
+    }
   }
 
   return std::nullopt;
