e16839bf53b79559d481796ca4f6f4460ceddf74
Markus Handell <handellm@google.com>
2024-03-21 07:10:31 +0000

    Add traces for RGBA->NV12 conversion.
    Bug: b/329396382
    Change-Id: If98a17463c6a2685672b5d3c2a425da790abfc74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5383032
    Commit-Queue: Markus Handell <handellm@google.com>
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276079}

--- third_party/blink/renderer/platform/graphics/web_graphics_context_3d_video_frame_pool.cc
+++ third_party/blink/renderer/platform/graphics/web_graphics_context_3d_video_frame_pool.cc
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/web_graphics_context_3d_video_frame_pool.h"
 
 #include "base/feature_list.h"
+#include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
+#include "base/trace_event/trace_event_impl.h"
 #include "components/viz/common/gpu/raster_context_provider.h"
 #include "gpu/GLES2/gl2extchromium.h"
 #include "gpu/command_buffer/client/client_shared_image.h"
@@ -19,9 +21,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/renderers/video_frame_rgba_to_yuva_converter.h"
 #include "media/video/gpu_video_accelerator_factories.h"
 #include "media/video/renderable_gpu_memory_buffer_video_frame_pool.h"
+#include "perfetto/tracing/track_event_args.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/renderer/platform/graphics/web_graphics_context_3d_provider_wrapper.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
+#include "third_party/perfetto/include/perfetto/tracing/track.h"
 
 namespace blink {
 
@@ -150,6 +154,10 @@ bool WebGraphicsContext3DVideoFramePool::CopyRGBATextureToVideoFrame(
     const gpu::MailboxHolder& src_mailbox_holder,
     const gfx::ColorSpace& dst_color_space,
     FrameReadyCallback callback) {
+  TRACE_EVENT("media", "CopyRGBATextureToVideoFrame");
+  int flow_id = trace_flow_seqno_.GetNext();
+  TRACE_EVENT_INSTANT("media", "CopyRGBATextureToVideoFrame",
+                      perfetto::Flow::ProcessScoped(flow_id));
   if (!weak_context_provider_)
     return false;
   auto* context_provider = weak_context_provider_->ContextProvider();
@@ -202,7 +210,7 @@ bool WebGraphicsContext3DVideoFramePool::CopyRGBATextureToVideoFrame(
   auto on_query_done_cb =
       [](scoped_refptr<media::VideoFrame> frame,
          base::WeakPtr<blink::WebGraphicsContext3DProviderWrapper> ctx_wrapper,
-         unsigned query_id, FrameReadyCallback callback) {
+         unsigned query_id, int flow_id, FrameReadyCallback callback) {
         if (ctx_wrapper) {
           if (auto* ctx_provider = ctx_wrapper->ContextProvider()) {
             if (auto* ri_provider = ctx_provider->RasterContextProvider()) {
@@ -211,6 +219,8 @@ bool WebGraphicsContext3DVideoFramePool::CopyRGBATextureToVideoFrame(
             }
           }
         }
+        TRACE_EVENT_INSTANT("media", "CopyRGBATextureToVideoFrame",
+                            perfetto::TerminatingFlow::ProcessScoped(flow_id));
         std::move(callback).Run(std::move(frame));
       };
 
@@ -219,7 +229,7 @@ bool WebGraphicsContext3DVideoFramePool::CopyRGBATextureToVideoFrame(
   context_support->SignalQuery(
       query_id,
       base::BindOnce(on_query_done_cb, dst_frame, weak_context_provider_,
-                     query_id, std::move(callback)));
+                     query_id, flow_id, std::move(callback)));
 
   return true;
 }
