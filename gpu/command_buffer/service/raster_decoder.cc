68ea67b8dd10d5dd7b42e4c2776ff59558b9ddb8
Kevin Haslett <khaslett@chromium.org>
2024-03-21 20:10:44 +0000

    Get Ash Chrome working on Graphite Dawn
    Lands basic support that allows Graphite to be used on Ash when
    enabled. We query the new Graphite API landed in[1] to get supported
    DRM format modifiers when creating images. However I also needed to add
    a blocklist of formats that use multiple *memory* planes as Dawn does
    not currently support multiple memory plane images.
    
    These are needed to enable:
    gn args:
      use_skia_dawn=true
    Launch flag:
      --enable-skia-graphite
    
    Known Issues:
    - Lacros Chrome does not work. It only renders about 3 frames before
    freezing, regardless of whether Graphite is enable for Lacros.
    - Videos don't work.
    
    [1] https://dawn-review.googlesource.com/c/dawn/+/179080
    
    Change-Id: I77e7f643c6c9d99c2ca77893fcb87ef233152823
    Bug: 330382071
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5381040
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Commit-Queue: Kevin Haslett <khaslett@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276455}

--- gpu/command_buffer/service/raster_decoder.cc
+++ gpu/command_buffer/service/raster_decoder.cc
@@ -107,6 +107,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/service/dawn_context_provider.h"
 #endif  // BUILDFLAG(USE_DAWN)
 
+#if BUILDFLAG(SKIA_USE_DAWN) && BUILDFLAG(IS_OZONE)
+#include "gpu/command_buffer/service/drm_modifiers_filter_dawn.h"
+#endif  // BUILDFLAG(SKIA_USE_DAWN) && BUILDFLAG(IS_OZONE)
+
 #if !BUILDFLAG(IS_ANDROID)
 #include "gpu/command_buffer/service/abstract_texture.h"
 #endif  // !BUIDLFLAG(IS_ANDROID)
@@ -1235,6 +1239,15 @@ Capabilities RasterDecoderImpl::GetCapabilities() {
                                           caps.drm_formats_and_modifiers);
   }
 #endif  // BUILDFLAG(ENABLE_VULKAN)
+#if BUILDFLAG(SKIA_USE_DAWN)
+  else if (shared_context_state_->IsGraphiteDawnVulkan()) {
+    auto adapter = shared_context_state_->dawn_context_provider()
+                       ->GetDevice()
+                       .GetAdapter();
+    gpu::PopulateDawnDrmFormatsAndModifiers(adapter,
+                                            caps.drm_formats_and_modifiers);
+  }
+#endif  // BUILDFLAG(SKIA_USE_DAWN)
   else {
     NOTREACHED();
   }
