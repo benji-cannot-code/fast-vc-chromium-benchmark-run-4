706ee390bcf485fd704016aa25956704ebefee0e
Adam Rice <ricea@chromium.org>
2024-03-21 05:46:29 +0000

    Remove unneeded includes from bidirectional_stream_unittest.cc
    Also remove a confusing comment.
    
    BUG=40628504
    
    Change-Id: Ie1b6d4e0e2ea49c1da016fef6213bf4e5eee3757
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5385005
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276059}

--- components/grpc_support/bidirectional_stream_unittest.cc
+++ components/grpc_support/bidirectional_stream_unittest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/memory/raw_ptr.h"
-#include "base/run_loop.h"
 #include "base/strings/strcat.h"
 #include "base/synchronization/waitable_event.h"
 #include "build/build_config.h"
@@ -21,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 #include "net/test/quic_simple_test_server.h"
 #include "net/test/test_data_directory.h"
-#include "net/url_request/url_request_context_getter.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
@@ -64,7 +62,6 @@ class BidirectionalStreamTest : public ::testing::TestWithParam<bool> {
  private:
   std::string quic_server_hello_url_;
 
-  // Optional to permit delayed construction.
   std::unique_ptr<TestStreamEngineGetter> test_stream_engine_getter_;
 };
 
