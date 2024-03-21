a7296d28c145caa665f258b3f38c0f71f7513e2e
Qingxin Wu <qingxinwu@google.com>
2024-03-21 19:46:50 +0000

    Add reportingTimeout field to auction config.
    Both top level and component auction config can specify a
    reportingTimeout field to control the timeout of their reportWin() and
    reportResult().
    
    Using this field will be in a separate CL.
    
    Bug: b/326922003
    
    Change-Id: I2460150e6c77e1f39425887a98c0c32cf5dcc767
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5374595
    Commit-Queue: Qingxin Wu <qingxinwu@google.com>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1276442}

--- third_party/blink/common/interest_group/auction_config_test_util.cc
+++ third_party/blink/common/interest_group/auction_config_test_util.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/common/interest_group/auction_config_test_util.h"
 
 #include "base/containers/flat_map.h"
+#include "base/time/time.h"
 #include "url/gurl.h"
 #include "url/origin.h"
 
@@ -66,16 +67,6 @@ AuctionConfig CreateFullAuctionConfig() {
       AuctionConfig::MaybePromiseBuyerTimeouts::FromValue(
           std::move(buyer_timeouts));
 
-  AuctionConfig::BuyerCurrencies buyer_currencies;
-  buyer_currencies.per_buyer_currencies.emplace();
-  (*buyer_currencies.per_buyer_currencies)[buyer] = AdCurrency::From("CAD");
-  buyer_currencies.all_buyers_currency = AdCurrency::From("USD");
-  non_shared_params.buyer_currencies =
-      AuctionConfig::MaybePromiseBuyerCurrencies::FromValue(
-          std::move(buyer_currencies));
-
-  non_shared_params.seller_currency = AdCurrency::From("EUR");
-
   AuctionConfig::BuyerTimeouts buyer_cumulative_timeouts;
   buyer_cumulative_timeouts.per_buyer_timeouts.emplace();
   (*buyer_cumulative_timeouts.per_buyer_timeouts)[buyer] = base::Seconds(432);
@@ -83,6 +74,17 @@ AuctionConfig CreateFullAuctionConfig() {
   non_shared_params.buyer_cumulative_timeouts =
       AuctionConfig::MaybePromiseBuyerTimeouts::FromValue(
           std::move(buyer_cumulative_timeouts));
+
+  non_shared_params.reporting_timeout = base::Seconds(7);
+  non_shared_params.seller_currency = AdCurrency::From("EUR");
+
+  AuctionConfig::BuyerCurrencies buyer_currencies;
+  buyer_currencies.per_buyer_currencies.emplace();
+  (*buyer_currencies.per_buyer_currencies)[buyer] = AdCurrency::From("CAD");
+  buyer_currencies.all_buyers_currency = AdCurrency::From("USD");
+  non_shared_params.buyer_currencies =
+      AuctionConfig::MaybePromiseBuyerCurrencies::FromValue(
+          std::move(buyer_currencies));
 
   non_shared_params.per_buyer_group_limits[buyer] = 10;
   non_shared_params.all_buyers_group_limit = 11;
