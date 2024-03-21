7459fb0eb798541cbbc770945583842a2e1b18dd
Slobodan Pejic <slobodan@chromium.org>
2024-03-21 15:45:42 +0000

    Remove flag enable-secure-payment-confirmation-extensions.
    This has been in stable since M120:
    https://chromiumdash.appspot.com/commit/add91f57a889342a575e88071f7c4a926ecca6f5
    
    Bug: 41493984, b:322013249
    Change-Id: I72073e8a86fb11a9b9409cbf22329de9530d984f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5321382
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276254}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -10629,11 +10629,6 @@ const FeatureEntry kFeatureEntries[] = {
                             kNewConfirmationBubbleForGeneratedPasswords)},
 #endif  // !BUILDFLAG(IS_ANDROID)
 
-    {"enable-secure-payment-confirmation-extensions",
-     flag_descriptions::kSecurePaymentConfirmationExtensionsName,
-     flag_descriptions::kSecurePaymentConfirmationExtensionsDescription, kOsAll,
-     FEATURE_VALUE_TYPE(blink::features::kSecurePaymentConfirmationExtensions)},
-
 #if BUILDFLAG(IS_CHROMEOS_ASH)
     {"upstream-trusted-reports-firmware",
      flag_descriptions::kUpstreamTrustedReportsFirmwareName,
