95edb946dab6ac834f827e8d5387a871002af403
Monica Basta <msalama@chromium.org>
2024-03-21 13:11:58 +0000

    [Uno-d] Don't show multi user bubble if not signed in to Chrome
    This CL ensure the multi-user bubble is not shown if the user isn't
    signed in to the browser.
    
    Bug: b/319397022
    Change-Id: Ibab12cbb2ec4499d00d84027d3b899e0dc5f864d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5379306
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276163}

--- chrome/browser/signin/dice_web_signin_interceptor.h
+++ chrome/browser/signin/dice_web_signin_interceptor.h
@@ -198,6 +198,8 @@ class DiceWebSigninInterceptor : public KeyedService,
                            ShouldShowEnterpriseBubbleWithoutUPA);
   FRIEND_TEST_ALL_PREFIXES(DiceWebSigninInterceptorTest,
                            ShouldShowMultiUserBubble);
+  FRIEND_TEST_ALL_PREFIXES(DiceWebSigninInterceptorTest,
+                           ShouldShowMultiUserBubbleNoPrimaryAccount);
   FRIEND_TEST_ALL_PREFIXES(DiceWebSigninInterceptorTest, PersistentHash);
   FRIEND_TEST_ALL_PREFIXES(DiceWebSigninInterceptorTest,
                            ShouldEnforceEnterpriseProfileSeparation);
