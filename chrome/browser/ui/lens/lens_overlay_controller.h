d4a59303497e802272c9223789efcbb66a7c0672
Duncan Mercer <mercerd@google.com>
2024-03-21 20:09:24 +0000

    Lens Overlay: Render text in WebUI
    This CL renders the bounding boxes for text send from the C++ mojom
    channel. A followup CL will add logic for actually making text
    selections.
    
    The C++ implementation is not ready yet, and being developed in
    parallel. For now, I did a small demo with mock data with boxes outlined
    to make it more apparent what is happening.
    
    Demo: https://screencast.googleplex.com/cast/NTE1NTE5NzMyMzA1MTAwOHxhYmRkNWE5Ni1hNw
    
    Bugs: b:328294794
    Change-Id: I4746196a1fe5aebcb713b1625ba01b46d60696ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5375511
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276453}

--- chrome/browser/ui/lens/lens_overlay_controller.h
+++ chrome/browser/ui/lens/lens_overlay_controller.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "chrome/browser/lens/core/mojom/lens.mojom.h"
+#include "chrome/browser/lens/core/mojom/text.mojom.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -127,6 +128,9 @@ class LensOverlayController : public TabStripModelObserver,
   // closed when the overlay is still open. This is a no-op if the provided web
   // view is not glued.
   void RemoveGlueForWebView(views::WebView* web_view);
+
+  // Send text data to the WebUI.
+  void SendText(lens::mojom::TextPtr text);
 
  private:
   class UnderlyingWebContentsObserver;
