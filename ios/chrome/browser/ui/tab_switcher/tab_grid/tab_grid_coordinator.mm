f63ebfafb2544b0d6018884738c36b5e5e2c521b
Aliona DANGLA <alionadangla@google.com>
2024-03-21 11:50:28 +0000

    [iOS] Open tab group view when user come back to tab grid
    When the user navigates in a web state that is already in a group, the
    group view should be displayed when they come back to tab grid view.
    
    Bug: 1501837
    Change-Id: I339e1371d9c3352db461d6c484e0afb18810b32a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5380325
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276138}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_coordinator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_coordinator.mm
@@ -89,6 +89,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_mediator_delegate.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/incognito/incognito_grid_coordinator.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/incognito/incognito_grid_mediator.h"
+#import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/incognito/incognito_grid_view_controller.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/regular/regular_grid_coordinator.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/regular/regular_grid_mediator.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/regular/regular_grid_view_controller.h"
@@ -458,6 +459,30 @@ bool FindNavigatorShouldBePresentedInBrowser(Browser* browser) {
     dispatch_async(dispatch_get_main_queue(), transitionBlock);
   } else if (shouldDisplayBringAndroidTabsPrompt) {
     [self displayBringAndroidTabsPrompt];
+  }
+
+  if (currentActivePage == TabGridPageRegularTabs) {
+    WebStateList* webStateList = self.regularBrowser->GetWebStateList();
+    int activeWebStateIndex =
+        webStateList->GetIndexOfWebState(webStateList->GetActiveWebState());
+    if (activeWebStateIndex != WebStateList::kInvalidIndex) {
+      const TabGroup* tabGroup =
+          webStateList->GetGroupOfWebStateAt(activeWebStateIndex);
+      if (tabGroup) {
+        [_regularGridCoordinator showTabGroup:tabGroup];
+      }
+    }
+  } else if (currentActivePage == TabGridPageIncognitoTabs) {
+    WebStateList* webStateList = self.incognitoBrowser->GetWebStateList();
+    int activeWebStateIndex =
+        webStateList->GetIndexOfWebState(webStateList->GetActiveWebState());
+    if (activeWebStateIndex != WebStateList::kInvalidIndex) {
+      const TabGroup* tabGroup =
+          webStateList->GetGroupOfWebStateAt(activeWebStateIndex);
+      if (tabGroup) {
+        [_incognitoGridCoordinator showTabGroup:tabGroup];
+      }
+    }
   }
 
   // Record when the tab switcher is presented.
