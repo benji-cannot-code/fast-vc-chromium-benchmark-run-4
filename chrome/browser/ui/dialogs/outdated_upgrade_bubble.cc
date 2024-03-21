936cba875b1320601a1c1f7de769e3b3249ccdf6
Tom Anderson <thomasanderson@chromium.org>
2024-03-21 17:30:01 +0000

    Use channel-specific download URL for outdated upgrade bubble
    The URL was always for the stable channel, so users of beta/dev/canary
    would incorrectly get sent to the stable download URL.
    
    Change-Id: I5613724e2ce5f0c677768c2637be28417f0b94c5
    Fixed: 325807989
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5376545
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276334}

--- chrome/browser/ui/dialogs/outdated_upgrade_bubble.cc
+++ chrome/browser/ui/dialogs/outdated_upgrade_bubble.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/user_metrics.h"
 #include "base/strings/strcat.h"
 #include "base/task/thread_pool.h"
+#include "base/version_info/channel.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/browser_process.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/upgrade_detector/upgrade_detector.h"
+#include "chrome/common/channel_info.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/branded_strings.h"
@@ -54,7 +56,7 @@ void OnWindowClosing() {
 
 void OnDialogAccepted(content::PageNavigator* navigator,
                       bool auto_update_enabled,
-                      const char* update_browser_redirect_url) {
+                      const std::string& update_browser_redirect_url) {
   if (auto_update_enabled) {
     DCHECK(UpgradeDetector::GetInstance()->is_outdated_install());
     base::RecordAction(
@@ -85,6 +87,22 @@ void OnDialogAccepted(content::PageNavigator* navigator,
   }
 }
 
+#if !BUILDFLAG(IS_CHROMEOS_LACROS)
+const char* GetUpdateUrlChannelSuffix(version_info::Channel channel) {
+  switch (channel) {
+    case version_info::Channel::CANARY:
+      return "/canary";
+    case version_info::Channel::DEV:
+      return "/dev";
+    case version_info::Channel::BETA:
+      return "/beta";
+    case version_info::Channel::UNKNOWN:
+    case version_info::Channel::STABLE:
+      return "";
+  }
+}
+#endif
+
 }  // namespace
 
 void ShowOutdatedUpgradeBubble(Browser* browser, bool auto_update_enabled) {
@@ -93,12 +111,19 @@ void ShowOutdatedUpgradeBubble(Browser* browser, bool auto_update_enabled) {
 
   g_upgrade_bubble_is_showing = true;
 
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  auto update_url = std::string(kUpdateBrowserRedirectUrl);
+#else
+  auto update_url = std::string(kUpdateBrowserRedirectUrl) +
+                    GetUpdateUrlChannelSuffix(chrome::GetChannel());
+#endif
+
   auto dialog_model =
       ui::DialogModel::Builder()
           .SetTitle(l10n_util::GetStringUTF16(IDS_UPGRADE_BUBBLE_TITLE))
           .AddOkButton(
               base::BindOnce(&OnDialogAccepted, browser, auto_update_enabled,
-                             kUpdateBrowserRedirectUrl),
+                             std::move(update_url)),
               ui::DialogModel::Button::Params().SetLabel(
                   l10n_util::GetStringUTF16(auto_update_enabled
                                                 ? IDS_REINSTALL_APP
