2be73c0f6792a1bc36de25511a48ebfb2a82b40d
Ciara McMullin <ciaramcmullin@google.com>
2024-03-21 15:30:00 +0000

    Add metrics for quic datagram queue capacity
    Bug: 41497362
    Change-Id: I28e955e5c064a48c5bb21e07247a17563ecb969c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5375121
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Ciara McMullin <ciaramcmullin@google.com>
    Reviewed-by: Dustin Mitchell <djmitche@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276240}

--- net/quic/quic_proxy_client_socket_test_base.h
+++ net/quic/quic_proxy_client_socket_test_base.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/run_loop.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/time/default_tick_clock.h"
 #include "mock_quic_data.h"
 #include "net/base/test_proxy_delegate.h"
@@ -291,6 +292,8 @@ class QuicProxyClientSocketTestBase
   TestCompletionCallback write_callback_;
 
   quic::test::NoopQpackStreamSenderDelegate noop_qpack_stream_sender_delegate_;
+
+  base::HistogramTester histogram_tester_;
 };
 }  // namespace net
 
