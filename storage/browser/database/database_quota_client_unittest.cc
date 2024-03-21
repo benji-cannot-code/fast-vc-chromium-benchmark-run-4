4affa518d0e44cd0ed37094a905a145a87fb0e8f
Ayu Ishii <ayui@chromium.org>
2024-03-21 20:06:29 +0000

    WebSQL: Remove session data logic
    On Profile destruction, currently there is a race with
    the DatabaseTracker shutdown. DatabaseTracker shutdown
    will post task[1] on the database task runner because
    it needs to do file I/O. However it also has some logic
    to retrieve info for special storage policies from the
    Profile which may already be destroyed by that time.
    
    This change removes logic for special storage policy from
    DatabaseTracker. DatabaseTracker(WebSQL) is removed
    from all platforms as of M119 except for WebView, which does
    not utilize the special storage policy. All policy and
    deprecation support have ended with M123. Therefore cleaning
    up the code to avoid the race.
    
    [1]https://source.chromium.org/chromium/chromium/src/+/main:content/browser/storage_partition_impl.cc;l=1200;drc=be92f4cc2f137460213d52a926c9477275a456c5
    
    Bug: 323898565, 325476286
    Change-Id: Ie2ef898c558308439a8b0d3fdf67f7157440b20a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5288855
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276451}

--- storage/browser/database/database_quota_client_unittest.cc
+++ storage/browser/database/database_quota_client_unittest.cc
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/browser/database/database_util.h"
 #include "storage/browser/quota/quota_manager.h"
 #include "storage/browser/quota/quota_manager_proxy.h"
-#include "storage/browser/quota/special_storage_policy.h"
 #include "storage/browser/test/mock_special_storage_policy.h"
 #include "storage/common/database/database_identifier.h"
 #include "testing/gmock/include/gmock/gmock-matchers.h"
@@ -56,14 +55,11 @@ static const blink::mojom::StorageType kTemp =
 // Mocks DatabaseTracker methods used by DatabaseQuotaClient.
 class MockDatabaseTracker : public DatabaseTracker {
  public:
-  MockDatabaseTracker(
-      const base::FilePath& path,
-      bool is_incognito,
-      scoped_refptr<QuotaManagerProxy> quota_manager_proxy,
-      scoped_refptr<SpecialStoragePolicy> special_storage_policy)
+  MockDatabaseTracker(const base::FilePath& path,
+                      bool is_incognito,
+                      scoped_refptr<QuotaManagerProxy> quota_manager_proxy)
       : DatabaseTracker(path,
                         is_incognito,
-                        std::move(special_storage_policy),
                         std::move(quota_manager_proxy),
                         DatabaseTracker::CreatePassKey()) {}
 
@@ -152,9 +148,8 @@ class DatabaseQuotaClientTest : public testing::TestWithParam<bool> {
       : kStorageKeyA(
             blink::StorageKey::CreateFromStringForTesting("http://host")),
         kStorageKeyB(
-            blink::StorageKey::CreateFromStringForTesting("http://host:8000")),
-        special_storage_policy_(
-            base::MakeRefCounted<MockSpecialStoragePolicy>()) {}
+            blink::StorageKey::CreateFromStringForTesting("http://host:8000")) {
+  }
   ~DatabaseQuotaClientTest() override = default;
 
   DatabaseQuotaClientTest(const DatabaseQuotaClientTest&) = delete;
@@ -168,8 +163,7 @@ class DatabaseQuotaClientTest : public testing::TestWithParam<bool> {
         /*quota_change_callback=*/base::DoNothing(), special_storage_policy_,
         GetQuotaSettingsFunc());
     mock_tracker_ = base::MakeRefCounted<MockDatabaseTracker>(
-        data_dir_.GetPath(), is_incognito(), quota_manager_->proxy(),
-        special_storage_policy_);
+        data_dir_.GetPath(), is_incognito(), quota_manager_->proxy());
   }
 
   void TearDown() override {
