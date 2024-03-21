b0c84b929572b3fbfee0f8a19cf98aef77dda45b
Evan Liu <evliu@google.com>
2024-03-21 17:46:12 +0000

    Reland "Web `Speech to Text` with SODA backend"
    This reverts commit 80783758b1d04a02569870cf8fdb2664a723feee.
    
    Original change's description:
    > Revert "Web `Speech to Text` with SODA backend"
    >
    > This reverts commit 6086de44421440d7bb70c7e8629ed377eab112ae.
    >
    > Reason for revert: Fails on Linux Lacros: https://ci.chromium.org/ui/p/chromium/builders/ci/linux-lacros-tester-rel/64313/test-results?q=ExactID%3Aninja%3A%2F%2Fcontent%2Ftest%3Acontent_browsertests%2FSpeechRecognitionBrowserTest.OneShotRecognition+VHash%3A0e10f2f5e2efd245
    > https://ci.chromium.org/ui/b/8752945049480096177
    >
    > Original change's description:
    > > Web `Speech to Text` with SODA backend
    > >
    > > Support Web Speech Recognition with local On-Device Soda.
    > >
    > > Bug: 1495388
    > > Change-Id: Ic72176042d2dc689ec35c64d1ff2e0b059465947
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5252833
    > > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > > Reviewed-by: Evan Liu <evliu@google.com>
    > > Commit-Queue: Jianlin Qiu <jianlin.qiu@intel.com>
    > > Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    > > Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1275497}
    >
    > Bug: 1495388
    > Change-Id: I7c23aa08187ec9b163ba0ec6b0037612c89968e1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5377859
    > Commit-Queue: Dominik Röttsches <drott@chromium.org>
    > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > Owners-Override: Dominik Röttsches <drott@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Dominik Röttsches <drott@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Dominik Röttsches <drott@chromium.org>
    > Commit-Queue: Colin Blundell <blundell@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1275542}
    
    Bug: 1495388
    Change-Id: Ia2d386082229331747de3199704a51874abe1afc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5382233
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1276349}

--- components/live_caption/caption_util.cc
+++ components/live_caption/caption_util.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include "base/command_line.h"
-#include "base/cpu.h"
 #include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
@@ -22,16 +21,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ui_base_switches.h"
 #include "ui/native_theme/native_theme.h"
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-#include "ash/constants/ash_features.h"
-#endif
-
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-#include "chromeos/startup/browser_params_proxy.h"
-#endif
-
 #if BUILDFLAG(IS_WIN)
 #include "base/win/windows_version.h"
+#endif
+
+#if !BUILDFLAG(IS_FUCHSIA) && !BUILDFLAG(IS_ANDROID)
+#include "components/soda/soda_util.h"
 #endif
 
 namespace {
@@ -134,33 +129,15 @@ std::optional<ui::CaptionStyle> GetCaptionStyleFromUserSettings(
 }
 
 bool IsLiveCaptionFeatureSupported() {
-  if (!base::FeatureList::IsEnabled(media::kLiveCaption))
+  if (!base::FeatureList::IsEnabled(media::kLiveCaption)) {
     return false;
+  }
 
-// Some Chrome OS devices do not support on-device speech.
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-  if (!base::FeatureList::IsEnabled(ash::features::kOnDeviceSpeechRecognition))
-    return false;
-#elif BUILDFLAG(IS_CHROMEOS_LACROS)
-  if (!chromeos::BrowserParamsProxy::Get()->IsOndeviceSpeechSupported())
-    return false;
-#endif
-
-#if BUILDFLAG(IS_LINUX)
-  // Check if the CPU has the required instruction set to run the Speech
-  // On-Device API (SODA) library.
-  static bool has_sse41 = base::CPU().has_sse41();
-  if (!has_sse41)
-    return false;
-#endif
-
-#if BUILDFLAG(IS_WIN) && defined(ARCH_CPU_ARM64)
-  // The Speech On-Device API (SODA) component does not support Windows on
-  // arm64.
-  return false;
+#if !BUILDFLAG(IS_FUCHSIA) && !BUILDFLAG(IS_ANDROID)
+  return speech::IsOnDeviceSpeechRecognitionSupported();
 #else
-  return true;
-#endif
+  return false;
+#endif  // !BUILDFLAG(IS_FUCHSIA) && !BUILDFLAG(IS_ANDROID)
 }
 
 std::string GetCaptionSettingsUrl() {
