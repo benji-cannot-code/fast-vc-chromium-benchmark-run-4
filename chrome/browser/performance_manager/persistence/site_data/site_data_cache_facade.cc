56d6f47e7a63bed6992635ac60819d0d29467df2
Joe Mason <joenotcharles@google.com>
2024-03-21 19:07:39 +0000

    [PM] Log a histogram to detect lost SiteDB writes on shutdown
    R=fdoray
    
    Bug: 40755583
    Change-Id: Id672d3f91d0e718a354d4fe1bdc1752bfa89e715
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5384268
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276415}

--- chrome/browser/performance_manager/persistence/site_data/site_data_cache_facade.cc
+++ chrome/browser/performance_manager/persistence/site_data/site_data_cache_facade.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/location.h"
 #include "base/memory/raw_ref.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
 #include "base/run_loop.h"
 #include "base/threading/sequence_bound.h"
@@ -206,6 +207,10 @@ void SiteDataCacheFacade::OnURLsDeleted(
     const history::DeletionInfo& deletion_info) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   if (deletion_info.IsAllHistory()) {
+    if (!browser_context_->IsOffTheRecord()) {
+      base::UmaHistogramBoolean(
+          "PerformanceManager.SiteDB.WriteScheduled.ClearAllSiteData", true);
+    }
     ClearAllSiteData();
     return;
   }
@@ -227,6 +232,11 @@ void SiteDataCacheFacade::OnURLsDeleted(
     return;
   }
 
+  if (!browser_context_->IsOffTheRecord()) {
+    base::UmaHistogramBoolean(
+        "PerformanceManager.SiteDB.WriteScheduled.ClearSiteDataForOrigins",
+        true);
+  }
   auto clear_site_data_cb = base::BindOnce(
       [](const std::string& browser_context_id,
          const std::vector<url::Origin>& origins_to_remove,
