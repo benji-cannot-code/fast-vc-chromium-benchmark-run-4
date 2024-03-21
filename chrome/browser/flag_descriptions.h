e77a1ffeb696bdebabad4d9d783bdf1f2b49908a
Tommy Steimel <steimel@chromium.org>
2024-03-21 23:03:18 +0000

    [System Media Controls] Re-add hardware media key handling flag
    This CL re-adds the previously-expired and removed "hardware media key
    handling" flag, as it is still widely used.
    
    Bug: 330548733
    Change-Id: Ie94bd94c543dcb8a752fbbc51be2572a2d6ff8ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5386366
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276571}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1128,6 +1128,9 @@ extern const char kHandwritingLegacyRecognitionDescription[];
 extern const char kHandwritingLibraryDlcName[];
 extern const char kHandwritingLibraryDlcDescription[];
 
+extern const char kHardwareMediaKeyHandling[];
+extern const char kHardwareMediaKeyHandlingDescription[];
+
 extern const char kHeavyAdPrivacyMitigationsName[];
 extern const char kHeavyAdPrivacyMitigationsDescription[];
 
