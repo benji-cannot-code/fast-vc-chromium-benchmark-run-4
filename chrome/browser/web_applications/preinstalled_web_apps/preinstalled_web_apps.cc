33e7837f9ec55c01d0ad4af64d7d430f3074947e
David Black <dmblack@google.com>
2024-03-21 23:15:57 +0000

    Register config to preinstall container app.
    This CL registers a config to preinstall the container app. The
    preinstallation is gated on the `kContainerAppPreinstall` feature flag
    and the value returned by `IsContainerAppPreinstallEnabled()`.
    
    This CL can only be merged once DEPS have been updated to include:
    http://shortn/_3GE7dWJZ4y
    
    Bug: b:329706834
    Change-Id: Ia60db8b0ae17d62e0d075dfedc7963760a5da352
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5384552
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1276581}

--- chrome/browser/web_applications/preinstalled_web_apps/preinstalled_web_apps.cc
+++ chrome/browser/web_applications/preinstalled_web_apps/preinstalled_web_apps.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_CHROMEOS)
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/resources/preinstalled_web_apps/internal/container.h"
 #include "chrome/browser/web_applications/preinstalled_web_apps/app_mall.h"
 #include "chrome/browser/web_applications/preinstalled_web_apps/calculator.h"
 #include "chrome/browser/web_applications/preinstalled_web_apps/google_calendar.h"
@@ -82,6 +83,7 @@ std::vector<ExternalInstallOptions> GetChromeBrandedApps(Profile& profile) {
 #if BUILDFLAG(IS_CHROMEOS)
       GetConfigForAppMall(),
       GetConfigForCalculator(),
+      GetConfigForContainer(),
       GetConfigForGoogleCalendar(),
       GetConfigForGoogleChat(),
       GetConfigForGoogleMeet(),
