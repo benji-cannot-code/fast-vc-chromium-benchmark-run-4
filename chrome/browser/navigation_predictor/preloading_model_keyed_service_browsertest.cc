6be49b8b392cbe8e574080132dd3a275354f5297
Kevin McNee <mcnee@chromium.org>
2024-03-21 15:27:20 +0000

    NavigationPredictor: Various small cleanups
    We rename is_same_origin to is_same_host, since the code is actually
    using hosts (which aren't the same thing). We remove some dead code and
    indicate test only code. We make the float conversions to create the
    model input less verbose.
    
    Bug: None
    Change-Id: I6bcc79518e780bd904e8596c7206bd243569f8a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5375318
    Reviewed-by: Iman Saboori <isaboori@google.com>
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Iman Saboori <isaboori@google.com>
    Cr-Commit-Position: refs/heads/main@{#1276237}

--- chrome/browser/navigation_predictor/preloading_model_keyed_service_browsertest.cc
+++ chrome/browser/navigation_predictor/preloading_model_keyed_service_browsertest.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_util.h"
 #include "base/path_service.h"
-#include "base/test/bind.h"
+#include "base/test/test_future.h"
 #include "chrome/browser/navigation_predictor/preloading_model_keyed_service_factory.h"
 #include "chrome/browser/optimization_guide/browser_test_util.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
@@ -117,7 +117,7 @@ IN_PROC_BROWSER_TEST_F(PreloadingModelKeyedServiceTest, Score) {
 
   {
     base::RunLoop run_loop;
-    model_service->AddOnModelUpdatedCallback(run_loop.QuitClosure());
+    model_service->AddOnModelUpdatedCallbackForTesting(run_loop.QuitClosure());
     run_loop.Run();
   }
 
@@ -136,22 +136,16 @@ IN_PROC_BROWSER_TEST_F(PreloadingModelKeyedServiceTest, Score) {
   inputs.path_length = 0;
   inputs.percent_clickable_area = 0.0;
   inputs.percent_vertical_distance = 0.0;
-  inputs.is_same_origin = false;
+  inputs.is_same_host = false;
   inputs.is_in_viewport = false;
   inputs.is_pointer_hovering_over = false;
   inputs.entered_viewport_to_left_viewport = base::TimeDelta();
   inputs.hover_dwell_time = base::TimeDelta();
   inputs.pointer_hovering_over_count = 0;
-  {
-    base::RunLoop run_loop;
-    model_service->Score(&tracker, inputs,
-                         base::BindLambdaForTesting(
-                             [&](PreloadingModelKeyedService::Result result) {
-                               EXPECT_TRUE(result.has_value());
-                               run_loop.Quit();
-                             }));
-    run_loop.Run();
-  }
+
+  base::test::TestFuture<PreloadingModelKeyedService::Result> score_future;
+  model_service->Score(&tracker, inputs, score_future.GetCallback());
+  EXPECT_TRUE(score_future.Get().has_value());
 }
 
 }  // namespace
