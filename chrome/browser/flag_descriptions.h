febc61066274ff940ef62955a91d6e8ff4a4930b
Aliaa Alborhamy <aliaaborhamy@google.com>
2024-03-21 13:49:45 +0000

    [iOS Promo] Feature flag with params for iOS bookmarks promo on desktop
    Feature flag for the upcoming iOS promo on Desktop for the bookmarks. The params allow to change the different activation methods for the promo.There are two types of params, one for contextual case and the other one for the always show case.
    
    Bug: 329975065
    Change-Id: Ib0c5b965c5288dd32340a25ee74f991a6788767a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5373053
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Aliaa Alborhamy <aliaaborhamy@google.com>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1276180}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2930,6 +2930,9 @@ extern const char kSyncWebauthnCredentialsDescription[];
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
 extern const char kSettingsEnableGetTheMostOutOfChromeName[];
 extern const char kSettingsEnableGetTheMostOutOfChromeDescription[];
+
+extern const char kIOSPromoBookmarkBubbleName[];
+extern const char kIOSPromoBookmarkBubbleDescription[];
 #endif
 
 extern const char kWebAppDedupeInstallUrlsName[];
