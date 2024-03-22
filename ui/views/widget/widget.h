996104737ba1d4a723da628944c9ae8bcd0b5c8c
Sean Kau <skau@chromium.org>
2024-03-22 00:09:08 +0000

    Remove ColorProviderKey::ElevationMode
    Colors no longer depend on the elevation of the associated widget (Views
    select the appropriate color instead).
    
    Bug: None
    Change-Id: Ica581a1fc9fbab98b553f4964ffd541baab69875
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5385748
    Auto-Submit: Sean Kau <skau@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276606}

--- ui/views/widget/widget.h
+++ ui/views/widget/widget.h
@@ -317,11 +317,6 @@ class VIEWS_EXPORT Widget : public internal::NativeWidgetDelegate,
     // be ignored on some platforms. No value indicates no preference.
     std::optional<int> shadow_elevation;
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-    ui::ColorProviderKey::ElevationMode background_elevation =
-        ui::ColorProviderKey::ElevationMode::kLow;
-#endif
-
     // The window corner radius. May be ignored on some platforms.
     std::optional<int> corner_radius;
 
@@ -1360,11 +1355,6 @@ class VIEWS_EXPORT Widget : public internal::NativeWidgetDelegate,
 
   // See set_is_secondary_widget().
   bool is_secondary_widget_ = true;
-
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-  ui::ColorProviderKey::ElevationMode background_elevation_ =
-      ui::ColorProviderKey::ElevationMode::kLow;
-#endif
 
   // If set, overrides this value is used instead of the one from NativeTheme
   // when constructing a ColorProvider.
