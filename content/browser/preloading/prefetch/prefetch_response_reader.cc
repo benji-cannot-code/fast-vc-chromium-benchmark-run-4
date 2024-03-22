d16e6b23e0204d270d451c78df7a3a35f9175615
Hiroshige Hayashizaki <hiroshige@chromium.org>
2024-03-22 06:39:40 +0000

    Remove DumpWithoutCrashing in PrefetchResponseReader
    The DumpWithoutCrashing can/should be now removed because:
    
    - It was to investigate cases where CreateRequestHandler() is called
      twice with `PrefetchReusable` disabled, but the issue has been fixed
      (no crashes for the last 2 months).
    - It isn't conpatible with `PrefetchReusable`:
      CreateRequestHandler() is inherently called more than once, and
      the flag was recently enabled on experiments and caused
      DumpWithoutCrashing reports.
    
    Bug: 330353196, 1483599
    Change-Id: Idd02c082d98fc5b8768f8815ad4a92f5fc0fe605
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5386787
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276728}

--- content/browser/preloading/prefetch/prefetch_response_reader.cc
+++ content/browser/preloading/prefetch/prefetch_response_reader.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/preloading/prefetch/prefetch_response_reader.h"
 
-#include "base/debug/dump_without_crashing.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/task/sequenced_task_runner.h"
 #include "content/browser/preloading/prefetch/prefetch_features.h"
@@ -119,15 +118,6 @@ void PrefetchResponseReader::OnServingURLLoaderMojoDisconnect() {
 }
 
 PrefetchRequestHandler PrefetchResponseReader::CreateRequestHandler() {
-  if (create_request_handler_called_) {
-    // Monitor cases where CreateRequestHandler() is called multiple times, for
-    // investigation of crbug.com/1483599. Anyway such cases should be handled
-    // (failing gracefully) below, e.g. by checking `body_`.
-    // TODO(crbug.com/1483599): Remove this.
-    base::debug::DumpWithoutCrashing();
-  }
-  create_request_handler_called_ = true;
-
   mojo::ScopedDataPipeConsumerHandle body;
 
   // Returns a null handler if some checks fail here.
