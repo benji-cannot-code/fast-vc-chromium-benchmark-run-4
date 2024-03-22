5596c7fb2f9f3782d4b8d83c8b73842a23b39155
Peter Boström <pbos@chromium.org>
2024-03-22 23:14:19 +0000

    Mark WeakLinkNode sequence checking expensive
    This makes for-each-node sequence checking expensive (which seems
    redundant). As a compromise we do non-"expensive" sequence checking in
    ObserverList::begin(), which should provide the same level of protection
    unless iterators are passed between sequences, which would be one heck
    of a thing to try to do.
    
    This accounts for about 60% of sequence checking in a profile I did way
    back. I have not profiled to see how much sequence checking remains with
    the sequence checking moved to begin() nor do I know the average
    ObserverList size. Let's try it out.
    
    In the same profile (though I don't remember what I profiled) sequence
    checking accounted for 1.2% of cycles. Hopefully this explains some of
    the performance gap between a DCHECK and regular Canary build.
    
    Bug: 40241607
    Change-Id: Id80d3363771e05e6f38c1432ae66b4c352acf8b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5319909
    Reviewed-by: François Degros <fdegros@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277148}

--- components/variations/variations_ids_provider.h
+++ components/variations/variations_ids_provider.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/metrics/field_trial.h"
-#include "base/observer_list.h"
 #include "base/synchronization/lock.h"
 #include "components/variations/proto/study.pb.h"
 #include "components/variations/synthetic_trials.h"
@@ -314,8 +313,9 @@ class COMPONENT_EXPORT(VARIATIONS) VariationsIdsProvider
 
   // List of observers to notify on variation ids header update.
   // NOTE this should really check observers are unregistered but due to
-  // https://crbug.com/1051937 this isn't currently possible.
-  base::ObserverList<Observer>::Unchecked observer_list_;
+  // https://crbug.com/1051937 this isn't currently possible. Note that
+  // ObserverList is sequence checked so we can't use that here.
+  std::vector<Observer*> observer_list_ GUARDED_BY(lock_);
 
   raw_ptr<const VariationsClient> variations_client_ = nullptr;
 };
