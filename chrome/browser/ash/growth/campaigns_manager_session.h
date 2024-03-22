1c774b774dc4efd056c572936d6a6f230221cf42
ThuHuong <thv@chromium.org>
2024-03-22 22:12:15 +0000

    Show nudge when app opens.
    Load campaigns and show appropriate nudge when an app is open.
    
    Bug: b/330388975
    Change-Id: I0c36c544ee7226d622f3d91e8539c149cd02756a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5386027
    Reviewed-by: Li Lin <llin@chromium.org>
    Commit-Queue: Thu Huong Vu <thv@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277118}

--- chrome/browser/ash/growth/campaigns_manager_session.h
+++ chrome/browser/ash/growth/campaigns_manager_session.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/scoped_observation.h"
+#include "components/services/app_service/public/cpp/instance_registry.h"
 #include "components/session_manager/core/session_manager.h"
 #include "components/session_manager/core/session_manager_observer.h"
 
@@ -16,7 +17,8 @@ class Profile;
 // Campaigns Manager session to store camapigns manager specific state, and to
 // observe related components changes to conditionally trigger proactive growth
 // slots.
-class CampaignsManagerSession : public session_manager::SessionManagerObserver {
+class CampaignsManagerSession : public session_manager::SessionManagerObserver,
+                                public apps::InstanceRegistry::Observer {
  public:
   CampaignsManagerSession();
   CampaignsManagerSession(const CampaignsManagerSession&) = delete;
@@ -26,11 +28,17 @@ class CampaignsManagerSession : public session_manager::SessionManagerObserver {
   // session_manager::SessionManagerObserver:
   void OnSessionStateChanged() override;
 
+  // apps::InstanceRegistry::Observer:
+  void OnInstanceUpdate(const apps::InstanceUpdate& update) override;
+  void OnInstanceRegistryWillBeDestroyed(
+      apps::InstanceRegistry* cache) override;
+
   void SetProfileForTesting(Profile* profile);
 
  private:
   Profile* GetProfile();
   bool IsEligible();
+  void SetupWindowObserver();
   void MaybeTriggerProactiveCampaigns();
 
   base::ScopedObservation<session_manager::SessionManager,
@@ -38,6 +46,10 @@ class CampaignsManagerSession : public session_manager::SessionManagerObserver {
       session_manager_observation_{this};
 
   raw_ptr<Profile, DanglingUntriaged> profile_for_testing_ = nullptr;
+
+  base::ScopedObservation<apps::InstanceRegistry,
+                          apps::InstanceRegistry::Observer>
+      scoped_observation_{this};
 
   base::WeakPtrFactory<CampaignsManagerSession> weak_ptr_factory_{this};
 };
