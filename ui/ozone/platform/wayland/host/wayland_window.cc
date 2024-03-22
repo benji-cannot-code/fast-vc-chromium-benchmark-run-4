458ef980a4dffdb0f68bc0ac75f1a9ffcd6a3d24
Eriko Kurimoto <elkurin@chromium.org>
2024-03-22 08:16:57 +0000

    wayland: Guarantee there is not re-enterancy on RequestState
    This CL introduces re-enterant check inside state request. Re-entrancy
    may break WaylandWindow by for example `in_flight_request_` being
    reallocated and invalidating the pointer.
    
    Bug: 328367130
    Change-Id: I4fceb7a4862e6241d79507a5a0d3953115535ff0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5387477
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276749}

--- ui/ozone/platform/wayland/host/wayland_window.cc
+++ ui/ozone/platform/wayland/host/wayland_window.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <utility>
 
+#include "base/auto_reset.h"
 #include "base/functional/bind.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/notreached.h"
@@ -1263,6 +1264,10 @@ void WaylandWindow::RequestStateFromClient(
 void WaylandWindow::RequestState(PlatformWindowDelegate::State state,
                                  int64_t serial,
                                  bool force) {
+  // State should NOT be requested during the ongoing request handling.
+  CHECK(!requesting_state_) << "Detected re-enterancy of state request.";
+  base::AutoReset<bool> setter(&requesting_state_, true);
+
   LOG_IF(WARNING, in_flight_requests_.size() > 100u)
       << "The queue of configures is longer than 100!";
 
