7a6ebc200f1eaf5582044d1cef8af07e34696d89
Leszek Swirski <leszeks@google.com>
2024-03-22 14:52:51 +0000

    [v8] Remove small script streaming feature
    Small script streaming has been enabled by default for a long time,
    and has benefits for performance and simplicity. Remove the option
    to disable it.
    
    Change-Id: I388f07117b6aea1487424359131932d126a62709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5378719
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Auto-Submit: Leszek Swirski <leszeks@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276870}

--- third_party/blink/renderer/bindings/core/v8/script_streamer.cc
+++ third_party/blink/renderer/bindings/core/v8/script_streamer.cc
@@ -575,16 +575,6 @@ void ResourceScriptStreamer::RunScriptStreamingTask(
       "v8.parseOnBackground2");
 }
 
-bool ResourceScriptStreamer::HasEnoughDataForStreaming(
-    size_t resource_buffer_size) {
-  if (base::FeatureList::IsEnabled(features::kSmallScriptStreaming)) {
-    return resource_buffer_size >= kMaximumLengthOfBOM;
-  } else {
-    // Only stream larger scripts.
-    return resource_buffer_size >= kSmallScriptThreshold;
-  }
-}
-
 // Try to start a task streaming the script from the datapipe, with the task
 // taking ownership of the datapipe and weak ownership of the client. Returns
 // true if streaming succeeded and false otherwise.
@@ -592,7 +582,7 @@ bool ResourceScriptStreamer::HasEnoughDataForStreaming(
 // Streaming may fail to start because:
 //
 //   * The encoding is invalid (not UTF-8 or one-byte data)
-//   * The script is too small (see HasEnoughDataForStreaming)
+//   * The script is too small to check for a byte-order marker
 //   * There is a code cache for this script already
 //   * V8 failed to create a script streamer
 //
@@ -618,7 +608,7 @@ bool ResourceScriptStreamer::TryStartStreamingTask() {
   // wait until the next data chunk comes before deciding whether to start the
   // streaming.
   if (!script_resource_->ResourceBuffer() ||
-      !HasEnoughDataForStreaming(script_resource_->ResourceBuffer()->size())) {
+      script_resource_->ResourceBuffer()->size() < kMaximumLengthOfBOM) {
     CHECK(!IsLoaded());
     return false;
   }
@@ -642,9 +632,6 @@ bool ResourceScriptStreamer::TryStartStreamingTask() {
     // The encoding may change when we see the BOM. Check for BOM now
     // and update the encoding from the decoder when necessary. Suppress
     // streaming if the encoding is unsupported.
-    //
-    // Also note that have at least s_smallScriptThreshold worth of
-    // data, which is more than enough for detecting a BOM.
     if (!ConvertEncoding(decoder->Encoding().GetName(), &encoding_)) {
       SuppressStreaming(NotStreamingReason::kEncodingNotSupported);
       return false;
@@ -819,8 +806,7 @@ void ResourceScriptStreamer::OnDataPipeReadable(
       // must be because we suppressed streaming earlier, or never got enough
       // data to start streaming.
       CHECK(IsStreamingSuppressed() || !script_resource_->ResourceBuffer() ||
-            !HasEnoughDataForStreaming(
-                script_resource_->ResourceBuffer()->size()));
+            script_resource_->ResourceBuffer()->size() < kMaximumLengthOfBOM);
       watcher_.reset();
       // Pass kScriptTooSmall for the !IsStreamingSuppressed() case, it won't
       // override an existing streaming reason.
