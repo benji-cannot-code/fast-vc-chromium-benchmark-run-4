bd2da6fb88843f0e46b03509da0220e426ab66df
Emily Shack <emshack@chromium.org>
2024-03-22 22:21:42 +0000

    Tab Organization: Conditionally show reject button
    There should only be a reject button on a group if there are multiple
    groups presented to the user. Otherwise, the clear button will suffice.
    
    Bug: b/330775575
    Change-Id: If907eb0f89d697405287feb450f0d2ec53cc880b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5386371
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277127}

--- chrome/browser/resources/tab_search/tab_organization_group.ts
+++ chrome/browser/resources/tab_search/tab_organization_group.ts
@@ -59,6 +59,11 @@ export class TabOrganizationGroupElement extends PolymerElement {
         value: -1,
       },
 
+      showReject: {
+        type: Boolean,
+        value: false,
+      },
+
       lastFocusedIndex_: {
         type: Number,
         value: 0,
@@ -81,6 +86,7 @@ export class TabOrganizationGroupElement extends PolymerElement {
   name: string;
   multiTabOrganization: boolean;
   organizationId: number;
+  showReject: boolean;
 
   private lastFocusedIndex_: number;
   private showInput_: boolean;
