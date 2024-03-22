27824c3f848f49a91f5a51690d6f34f8e5dfc4fa
danakj <danakj@chromium.org>
2024-03-22 19:25:36 +0000

    Introduce base::cstring_view
    We currently have vocabulary types to receive a string parameter:
    - std::string for owning + NUL-terminated
    - std::string_view for non-owning and non-NUL-terminated
    - const char* for non-owning and NUL-terminated
    
    However the use of const char* will require unsafe pointer arithmetic
    which is incompatible with -Wunsafe-buffer-usage (every use of the
    pointer will cause compilation errors) so we need an alternative way
    to express NUL-terminated string views that include bounds safety.
    This type will allow conversion from pointer, or pointer+len, to a
    bounds-safe view type that maintains its NUL terminator.
    
    Throwing away the NUL byte is not a good option, as it forces a copy
    into a std::string every time you want the NUL again. We should have
    a non-owning view type that encodes in the type system that there is
    a NUL terminator, and which provides a .c_str() method to get the
    NUL-terminated pointer for C apis like string has.
    
    So far the type has:
    - default ctor
    - ctor from ptr+len [unsafe buffer usage]
    - ctor from string literal
    - c_str
    - data/size
    - size_bytes (like span)
    - begin/end/cbegin/cend
    - operator[]
    - operator==
    - operator<=>
    
    Eventually it will have all of string_view API, plus:
    - have .c_str() representing that it returns a NUL-terminated char
      pointer suitable for passing to C APIs (done).
    - be constructible from string literal (done).
    - have a template parameter to retain compile-time size when possible,
      like span (done).
    - avoid holding a size field when it's known at compile time, like
      span (done).
    
    In order to use them, introduce the following macros in base:
    - LIFETIME_BOUND
    - PURE_FUNCTION
    As proposed in the cxx@ thread:
    https://groups.google.com/a/chromium.org/g/cxx/c/lVQOJTng1RU/m/91r04iLAAwAJ
    
    As well as the __attribute__((enable_if())) extension via the macro
    in order to catch bugs at compile time and ensure cstring_view is
    only constructed from string literals (and not non-NUL-terminated
    char arrays):
    - ENABLE_IF_ATTR
    
    Bug: 329476354, 40285824
    Change-Id: I1047a3b049401ecf56636f9f8e1a69801098da9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5370959
    Commit-Queue: danakj <danakj@chromium.org>
    Auto-Submit: danakj <danakj@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277039}

--- base/containers/span.h
+++ base/containers/span.h
@@ -22,15 +22,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/compiler_specific.h"
 #include "base/containers/checked_iterators.h"
+#include "base/containers/dynamic_extent.h"
 #include "base/numerics/safe_conversions.h"
+#include "base/strings/is_basic_cstring_view.h"
 #include "base/template_util.h"
 #include "base/types/to_address.h"
 #include "third_party/abseil-cpp/absl/base/attributes.h"
 
 namespace base {
-
-// [views.constants]
-constexpr size_t dynamic_extent = std::numeric_limits<size_t>::max();
 
 template <typename T,
           size_t Extent = dynamic_extent,
@@ -76,6 +75,10 @@ struct ExtentImpl<T[N]> : size_constant<N> {};
 
 template <typename T, size_t N>
 struct ExtentImpl<std::array<T, N>> : size_constant<N> {};
+
+template <typename T>
+  requires(internal::IsBasicCStringView<T>::value)
+struct ExtentImpl<T> : size_constant<dynamic_extent> {};
 
 template <typename T, size_t N>
 struct ExtentImpl<base::span<T, N>> : size_constant<N> {};
