c2e60bf52e0e890374535dc3a8fbe2fb428e3a4b
Charles Hager <clhager@google.com>
2024-03-22 20:08:03 +0000

    Pass in duplicated Rect to observers to avoid observers mutating shared Rect
    With the existing logic, a single shared Rect reference is pass to all the observers, with DisplayCutoutObservers modifying that shared Rect. When multiple tabs are drawing EdgeToEdge, each will have their own DisplayCutoutObserver, overlapping DIP scaling modifications to the shared Rect. Thus, with multiple EdgeToEdge tabs, the bottom safe inset will be decreased, leading to the Footer in the test site moving lower and lower as more tabs are created.
    
    Bug: https://screencast.googleplex.com/cast/NTUwNjMzNjA2NTMyMzAwOHwyYWY2YThiMi05NA
    
    Fix: https://screencast.googleplex.com/cast/NTA2ODc1NzI4MTAxMzc2MHw3MDAyYTU2OS01Mg
    
    Bug: 328490726, b/329985397
    Change-Id: I9bf7c1183448bfefdf448912ff9800d32e49f96e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5381459
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Charles Hager <clhager@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1277056}

--- chrome/browser/ui/android/edge_to_edge/java/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeUtils.java
+++ chrome/browser/ui/android/edge_to_edge/java/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeUtils.java
@@ -172,7 +172,7 @@ public class EdgeToEdgeUtils {
     }
 
     /** Whether enable the inset management through InsetObserver. */
-    static boolean isInsetsManagementEnabled() {
+    public static boolean isInsetsManagementEnabled() {
         return ChromeFeatureList.sDrawEdgeToEdgeInsetsManagement.isEnabled();
     }
 }
--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/InsetObserverTest.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/InsetObserverTest.java
@@ -87,7 +87,7 @@ public class InsetObserverTest {
                 .when(mContentView)
                 .onApplyWindowInsets(mModifiedNonCompatInsets);
 
-        mInsetObserver = new InsetObserver(mContentView);
+        mInsetObserver = new InsetObserver(mContentView, true);
         mInsetObserver.addObserver(mObserver);
     }
 
