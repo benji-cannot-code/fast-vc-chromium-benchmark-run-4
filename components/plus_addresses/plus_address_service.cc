a5c92f6df44aa3fc9c299b1001ea407f352335bb
Florian Leimgruber <fleimgruber@google.com>
2024-03-22 13:43:46 +0000

    Add missing PlusAddressService notifications
    crrev.com/c/5378283 added a PlusAddressService::Observer, but didn't
    emit notifications in all places properly. This CL fixes that.
    
    Bug: b/322147254
    Change-Id: Ib8fda2e8e0cfd5b32d0d8aaf93578bf39394db4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5383898
    Auto-Submit: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1276843}

--- components/plus_addresses/plus_address_service.cc
+++ components/plus_addresses/plus_address_service.cc
@@ -139,6 +139,9 @@ void PlusAddressService::SavePlusAddress(url::Origin origin,
   std::string etld_plus_one = GetEtldPlusOne(origin);
   plus_address_by_site_[etld_plus_one] = plus_address;
   plus_addresses_.insert(plus_address);
+  for (Observer& o : observers_) {
+    o.OnPlusAddressesChanged();
+  }
 }
 
 bool PlusAddressService::IsPlusAddress(
@@ -410,6 +413,9 @@ void PlusAddressService::HandleSignout() {
   plus_address_http_client_->Reset();
   if (webdata_service_) {
     webdata_service_->ClearPlusProfiles();
+  }
+  for (Observer& o : observers_) {
+    o.OnPlusAddressesChanged();
   }
 }
 
