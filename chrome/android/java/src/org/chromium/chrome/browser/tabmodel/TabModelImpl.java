01e6cb70e406e50e11f8573aff3774d2aa7236af
Sky Malice <skym@chromium.org>
2024-03-22 22:45:53 +0000

    Use id mapping to perform tab contains check.
    This is a follow up to https://crrev.com/c/5381665, where when our
    flag is enabled, now have a HashMap of tabId to Tab. This change uses
    that mapping to do a more efficient contains check.
    
    The external functionality of the TabModelImpl should be unchanged.
    
    Bug: 330171667
    Change-Id: If611050109c819c336a58885462f249bc873690e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5388965
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277138}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelImpl.java
@@ -17,6 +17,7 @@ import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.flags.ActivityType;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.homepage.HomepageManager;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tab.Tab;
@@ -542,7 +543,7 @@ public class TabModelImpl extends TabModelJniBridge {
             return false;
         }
 
-        if (!mTabs.contains(tabToClose)) {
+        if (!containsTab(tabToClose)) {
             assert false : "Tried to close a tab from another model!";
             return false;
         }
@@ -567,7 +568,7 @@ public class TabModelImpl extends TabModelJniBridge {
     @Override
     public void closeMultipleTabs(List<Tab> tabs, boolean canUndo) {
         for (Tab tab : tabs) {
-            if (!mTabs.contains(tab)) {
+            if (!containsTab(tab)) {
                 assert false : "Tried to close a tab from another model!";
                 continue;
             }
@@ -665,6 +666,14 @@ public class TabModelImpl extends TabModelJniBridge {
             if (!mTabs.get(i).isClosing()) return true;
         }
         return false;
+    }
+
+    private boolean containsTab(Tab tab) {
+        if (ChromeFeatureList.sTabIdMap.isEnabled()) {
+            return mTabIdToTabs.containsKey(tab.getId());
+        } else {
+            return mTabs.contains(tab);
+        }
     }
 
     @Override
