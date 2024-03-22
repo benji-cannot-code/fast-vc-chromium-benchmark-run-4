8a94708346634147bd46b596c8ed57fca36c8e26
Michael Thiessen <mthiesse@chromium.org>
2024-03-22 19:57:38 +0000

    Revert "Fix flaky SyncConsentFragmentTest#testManagedAccount*"
    This reverts commit 64dfed54ccfb56368d286a9bcdad5da2085282b7.
    
    Reason for revert: Tests are still flaky, must be a different cause... I was unable to repro locally.
    
    Original change's description:
    > Fix flaky SyncConsentFragmentTest#testManagedAccount*
    >
    > Flakiness caused by espresso 3.2. Need to look for the view in the
    > dialog's root.
    >
    > Bug: 330570364
    > Change-Id: Ifdff3b7bc8c8f9aac54e15c0e92c7f4129b288d8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5383936
    > Auto-Submit: Michael Thiessen <mthiesse@chromium.org>
    > Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    > Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1276834}
    
    Bug: 330570364
    Change-Id: Id0f087f3d3e48eece4a0074e9b3ecb1fdc3df0e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5386694
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1277052}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/SyncConsentFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/SyncConsentFragmentTest.java
@@ -1505,6 +1505,7 @@ public class SyncConsentFragmentTest {
     @Test
     @LargeTest
     @EnableFeatures(SigninFeatures.ENTERPRISE_POLICY_ON_SIGNIN)
+    @DisabledTest(message = "Broken and/or flake on different bots, see b/40944120.")
     public void testManagedAccount_confirmed() throws Exception {
         mChromeActivityTestRule.startMainActivityOnBlankPage();
         CoreAccountInfo accountInfo = mSigninTestRule.addAccountAndWaitForSeeding(NEW_ACCOUNT_NAME);
@@ -1534,7 +1535,7 @@ public class SyncConsentFragmentTest {
             onView(withId(R.id.more_button)).perform(click());
         }
         onViewWaiting(withText(R.string.signin_accept_button)).perform(click());
-        onViewWaiting(withId(R.id.positive_button), true).perform(click());
+        onViewWaiting(withId(R.id.positive_button)).perform(click());
         // Wait for the sync consent to be set.
         CriteriaHelper.pollUiThread(
                 () -> {
@@ -1551,6 +1552,7 @@ public class SyncConsentFragmentTest {
     @Test
     @LargeTest
     @EnableFeatures(SigninFeatures.ENTERPRISE_POLICY_ON_SIGNIN)
+    @DisabledTest(message = "Broken and/or flake on different bots, see b/40944120.")
     public void testManagedAccount_failedSignin() throws Exception {
         mChromeActivityTestRule.startMainActivityOnBlankPage();
         CoreAccountInfo accountInfo = mSigninTestRule.addAccountAndWaitForSeeding(NEW_ACCOUNT_NAME);
@@ -1588,7 +1590,7 @@ public class SyncConsentFragmentTest {
             onView(withId(R.id.more_button)).perform(click());
         }
         onViewWaiting(withText(R.string.signin_accept_button)).perform(click());
-        onViewWaiting(withId(R.id.positive_button), true).perform(click());
+        onViewWaiting(withId(R.id.positive_button)).perform(click());
         assertFalse(
                 TestThreadUtils.runOnUiThreadBlocking(
                         signinManager::getUserAcceptedAccountManagement));
