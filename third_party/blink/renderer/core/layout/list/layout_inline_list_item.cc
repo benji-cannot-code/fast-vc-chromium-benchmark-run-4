a54a139e30f789787ae31cf3cbfaace2dd2b6df4
Kent Tamura <tkent@chromium.org>
2024-03-22 06:40:47 +0000

    Remove CounterStyleChangeShouleCollectInlines flag
    The flag has been enabled since M122, and it caused no issues.
    This CL has no behavior changes.
    
    Bug: 41484865
    Change-Id: I855e803d3d1cab73065ade30add7b5237b0df8d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5382073
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276730}

--- third_party/blink/renderer/core/layout/list/layout_inline_list_item.cc
+++ third_party/blink/renderer/core/layout/list/layout_inline_list_item.cc
@@ -87,9 +87,7 @@ void LayoutInlineListItem::UpdateCounterStyle() {
     return;
   }
   list_marker->CounterStyleChanged(*marker);
-  if (RuntimeEnabledFeatures::CounterStyleChangeShouleCollectInlinesEnabled()) {
-    SetNeedsCollectInlines();
-  }
+  SetNeedsCollectInlines();
 }
 
 int LayoutInlineListItem::Value() const {
