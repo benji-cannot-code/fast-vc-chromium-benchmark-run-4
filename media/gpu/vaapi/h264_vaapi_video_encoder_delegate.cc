6eec3080b83cd70e1fb994b1a46a81ae44d3f263
Hirokazu Honda <hiroh@chromium.org>
2024-03-22 02:53:28 +0000

    media/gpu/vaapiVED: Remove base GetMetadata() implementation
    VaapiVideoEncoderDelegate has GetMetadata() and its derived class can
    also implement it. This makes what function is called at first glance.
    This CL removes the base implementation of GetMetadata(), so the
    implementation of the derived class is always called.
    
    Bug: b:329760130, b:329745253
    Test: media_unittest
    Test: video_encode_accelerator_tests
    Change-Id: I054c88cd4184909f3b43e64485ef6687195843f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5373686
    Reviewed-by: Justin Green <greenjustin@google.com>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276668}

--- media/gpu/vaapi/h264_vaapi_video_encoder_delegate.cc
+++ media/gpu/vaapi/h264_vaapi_video_encoder_delegate.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/vaapi/vaapi_common.h"
 #include "media/gpu/vaapi/vaapi_wrapper.h"
 #include "media/video/h264_level_limits.h"
+#include "media/video/video_encode_accelerator.h"
 
 namespace media {
 namespace {
@@ -342,9 +343,12 @@ BitstreamBufferMetadata H264VaapiVideoEncoderDelegate::GetMetadata(
     const EncodeJob& encode_job,
     size_t payload_size) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-
-  auto metadata =
-      VaapiVideoEncoderDelegate::GetMetadata(encode_job, payload_size);
+  CHECK(!encode_job.IsFrameDropped());
+  CHECK_NE(payload_size, 0u);
+  BitstreamBufferMetadata metadata(
+      payload_size, encode_job.IsKeyframeRequested(), encode_job.timestamp());
+  // TODO(b/329745253): Remove end_of_picture assignment.
+  metadata.end_of_picture = encode_job.end_of_picture();
   CHECK(metadata.end_of_picture);
   auto picture = GetH264Picture(encode_job);
   DCHECK(picture);
