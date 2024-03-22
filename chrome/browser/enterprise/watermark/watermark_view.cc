1253ecf16192c0a2843b6e86bde8228997c95fbd
Marshall Greenblatt <marshall@chromium.org>
2024-03-22 18:20:10 +0000

    Use base::NoDestructor with static FontList
    BUG=328447915
    
    Change-Id: I40c4e792a2c4b3a233a16aa543c2210e76061281
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5388128
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Nasser Al-shawwa <alshawwa@chromium.org>
    Commit-Queue: Marshall Greenblatt <marshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276999}

--- chrome/browser/enterprise/watermark/watermark_view.cc
+++ chrome/browser/enterprise/watermark/watermark_view.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <string>
 
+#include "base/no_destructor.h"
 #include "cc/paint/paint_canvas.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/compositor/layer.h"
@@ -28,8 +29,8 @@ constexpr double kRotationAngle = 45;
 constexpr SkColor kFillColor = SkColorSetARGB(0x12, 0x00, 0x00, 0x00);
 constexpr SkColor kOutlineColor = SkColorSetARGB(0x27, 0xff, 0xff, 0xff);
 
-const gfx::Font& WatermarkFont() {
-  static gfx::Font font(
+gfx::Font WatermarkFont() {
+  return gfx::Font(
 #if BUILDFLAG(IS_WIN)
       "Segoe UI",
 #elif BUILDFLAG(IS_MAC)
@@ -42,7 +43,6 @@ const gfx::Font& WatermarkFont() {
       "sans-serif",
 #endif
       kTextSize);
-  return font;
 }
 
 gfx::Font::Weight WatermarkFontWeight() {
@@ -54,8 +54,8 @@ gfx::Font::Weight WatermarkFontWeight() {
 }
 
 const gfx::FontList& WatermarkFontList() {
-  static gfx::FontList font_list(WatermarkFont());
-  return font_list;
+  static base::NoDestructor<gfx::FontList> font_list(WatermarkFont());
+  return *font_list;
 }
 
 std::unique_ptr<gfx::RenderText> CreateRenderText(const gfx::Rect& display_rect,
