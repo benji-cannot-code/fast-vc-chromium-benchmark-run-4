ece9e5c2c783c3558fcfed38067f3e208ecf55fa
Nathan Hebert <nhebert@chromium.org>
2024-03-22 13:33:48 +0000

    media/gpu/vaapi_wrapper: Add CreateVASurfaceForFrameResource
    This function can be used to create a VASurface from a FrameResource.
    The function opportunistically avoids duplicating FD's when |frame| is
    a NativePixmapFrameResource. Follow-up CL's migrate to this interface
    where appropriate.
    
    CreateVASurfaceForPixmap() has also been modified to accept a const
    NativePixmap smart pointer, which lets it be reused by
    CreateVASurfaceForFrameResource.
    
    Bug: b:277581596
    Test: Chrome and unit test targets compile
    Test: gn gen --check passes
    
    Change-Id: Id18010a1422ccbba0d97b9666074d86e1555379f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5355811
    Commit-Queue: Nathan Hebert <nhebert@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276839}

--- media/gpu/vaapi/vaapi_wrapper.cc
+++ media/gpu/vaapi/vaapi_wrapper.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/video_codecs.h"
 #include "media/base/video_frame.h"
 #include "media/base/video_types.h"
+#include "media/gpu/chromeos/frame_resource.h"
 #include "media/gpu/macros.h"
 // Auto-generated for dlopen libva libraries
 #include "media/gpu/vaapi/va_stubs.h"
@@ -2405,8 +2406,27 @@ bool VaapiWrapper::CreateContext(const gfx::Size& size) {
   return MaybeAttachProtectedSession_Locked();
 }
 
+scoped_refptr<VASurface> VaapiWrapper::CreateVASurfaceForFrameResource(
+    const FrameResource& frame,
+    bool protected_content) {
+  CHECK(!enforce_sequence_affinity_ ||
+        sequence_checker_.CalledOnValidSequence());
+  scoped_refptr<const gfx::NativePixmap> pixmap;
+  if (frame.HasNativePixmap()) {
+    pixmap = frame.GetNativePixmapDmaBuf();
+  } else {
+    pixmap = frame.CreateNativePixmapDmaBuf();
+  }
+
+  if (!pixmap) {
+    LOG(ERROR) << "Failed to create NativePixmap from FrameResource";
+    return nullptr;
+  }
+  return CreateVASurfaceForPixmap(std::move(pixmap), protected_content);
+}
+
 scoped_refptr<VASurface> VaapiWrapper::CreateVASurfaceForPixmap(
-    scoped_refptr<gfx::NativePixmap> pixmap,
+    scoped_refptr<const gfx::NativePixmap> pixmap,
     bool protected_content) {
   CHECK(!enforce_sequence_affinity_ ||
         sequence_checker_.CalledOnValidSequence());
