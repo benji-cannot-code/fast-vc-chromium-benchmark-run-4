bd65170a4b738e64d6b4c7d45ae929aaa961ea60
Peter Boström <pbos@chromium.org>
2024-03-22 19:40:19 +0000

    Add back fallback wasm-trap handling
    Crashpad was understood to be unconditionally enabled in
    https://crrev.com/c/5237441. Turns out that that wasn't true for
    content_shell.
    
    This change sets up wasm-trap handling for the case where content_shell
    has not enabled crash reporting but moves the responsibility to
    ShellContentRendererClient. The default ContentRendererClient assumes
    that crash reporting is enabled (crashpad enabled by default) and does
    not set up its own handler.
    
    Bug: 327517309
    Change-Id: If3d5f9a313c22e5926d276e6fdfbe9fefe20445d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372409
    Reviewed-by: Andreas Haas <ahaas@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277046}

--- content/public/renderer/content_renderer_client.cc
+++ content/public/renderer/content_renderer_client.cc
@@ -21,8 +21,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/icc_profile.h"
 #include "url/gurl.h"
 #include "url/origin.h"
+#include "v8/include/v8-initialization.h"
 
 namespace content {
+
+void ContentRendererClient::SetUpWebAssemblyTrapHandler() {
+  constexpr bool use_v8_trap_handler =
+#if BUILDFLAG(IS_WIN)
+      // On Windows we use the default trap handler provided by V8.
+      true
+#elif BUILDFLAG(IS_MAC)
+      // On macOS, Crashpad uses exception ports to handle signals in a
+      // different process. As we cannot just pass a callback to this other
+      // process, we ask V8 to install its own signal handler to deal with
+      // WebAssembly traps.
+      true
+#else
+      // The trap handler is set as the first chance handler for Crashpad's
+      // signal handler.
+      false
+#endif
+      ;
+  v8::V8::EnableWebAssemblyTrapHandler(use_v8_trap_handler);
+}
 
 SkBitmap* ContentRendererClient::GetSadPluginBitmap() {
   return nullptr;
