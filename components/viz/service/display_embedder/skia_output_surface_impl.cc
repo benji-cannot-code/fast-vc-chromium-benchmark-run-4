5245aa90f2dbf3f39ee4940171bd5c1a034cb280
Colin Blundell <blundell@chromium.org>
2024-03-22 13:59:45 +0000

    [Viz] Get texture target on Mac via Mac-specific service-side function
    This CL replaces Mac-specific service-side usage of
    GetPlatformSpecificTextureTarget() with
    GetMacOSSpecificTextureTargetForCurrentGLImplementation(), which has
    identical semantics. This will facilitate folding the functionality of
    GetPlatformSpecificTextureTarget() behind ClientSharedImage.
    
    Bug: 41494843
    Change-Id: I15f931d429afb0845376d7433b011f3bfc9cfd23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5383457
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276849}

--- components/viz/service/display_embedder/skia_output_surface_impl.cc
+++ components/viz/service/display_embedder/skia_output_surface_impl.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/common/sync_token.h"
 #include "gpu/command_buffer/service/graphite_cache_controller.h"
 #include "gpu/command_buffer/service/scheduler.h"
+#include "gpu/command_buffer/service/service_utils.h"
 #include "gpu/command_buffer/service/shared_image/shared_image_factory.h"
 #include "gpu/command_buffer/service/shared_image/shared_image_format_service_utils.h"
 #include "gpu/command_buffer/service/shared_image/shared_image_representation.h"
@@ -1219,11 +1220,16 @@ SkiaOutputSurfaceImpl::CreateGrSurfaceCharacterizationRenderPass(
 #if BUILDFLAG(IS_APPLE)
   if (is_overlay) {
     DCHECK_EQ(gr_context_type_, gpu::GrContextType::kGL);
-    // For overlay, IOSurface will be used, and we may need using
-    // GL_TEXTURE_RECTANGLE_ARB as texture target.
+    // For overlay, IOSurface will be used. Hence, we need to ensure that we are
+    // using the correct texture target for IOSurfaces, which depends on the GL
+    // implementation.
     backend_format = GrBackendFormats::MakeGL(
         GrBackendFormats::AsGLFormatEnum(backend_format),
-        gpu::GetPlatformSpecificTextureTarget());
+#if BUILDFLAG(IS_MAC)
+        gpu::GetMacOSSpecificTextureTargetForCurrentGLImplementation());
+#else
+        GL_TEXTURE_2D);
+#endif
   }
 #endif
   auto image_info =
@@ -1266,11 +1272,12 @@ SkiaOutputSurfaceImpl::CreateGrSurfaceCharacterizationCurrentFrame(
       color_type, GrRenderable::kYes);
 #if BUILDFLAG(IS_MAC)
   DCHECK_EQ(gr_context_type_, gpu::GrContextType::kGL);
-  // For root rander pass, IOSurface will be used, and we may need using
-  // GL_TEXTURE_RECTANGLE_ARB as texture target.
-  backend_format =
-      GrBackendFormats::MakeGL(GrBackendFormats::AsGLFormatEnum(backend_format),
-                               gpu::GetPlatformSpecificTextureTarget());
+  // For root render pass, IOSurface will be used. Hence, we need to ensure that
+  // we are using the correct texture target for IOSurfaces, which depends on
+  // the GL implementation.
+  backend_format = GrBackendFormats::MakeGL(
+      GrBackendFormats::AsGLFormatEnum(backend_format),
+      gpu::GetMacOSSpecificTextureTargetForCurrentGLImplementation());
 #endif
   DCHECK(backend_format.isValid())
       << "GrBackendFormat is invalid for color_type: " << color_type;
