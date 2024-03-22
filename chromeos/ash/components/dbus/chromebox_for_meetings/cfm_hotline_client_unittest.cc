376af16598233a5a8fcd155cba6e71fb0ab35841
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-03-22 18:19:40 +0000

    Triage dangling pointers in ChromeOS.
    Those are blocking the patch:
    https://chromium-review.googlesource.com/c/chromium/src/+/5385328/5
    enabling the dangling pointer detector on ChromeOS, no matter the bot.
    
    DanglingUntriaged-notes: Annotating pre-existing dangling pointers.
    Bug: chromium:328104161, chromium:327473315
    Change-Id: Iad1206a74d33393a65547737fcb041e70e4edb64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5386152
    Owners-Override: danakj <danakj@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276998}

--- chromeos/ash/components/dbus/chromebox_for_meetings/cfm_hotline_client_unittest.cc
+++ chromeos/ash/components/dbus/chromebox_for_meetings/cfm_hotline_client_unittest.cc
@@ -74,7 +74,6 @@ class CfmHotlineClientTest : public testing::Test {
         .WillRepeatedly(Invoke(this, &CfmHotlineClientTest::ConnectToSignal));
 
     CfmHotlineClient::Initialize(mock_bus_.get());
-    client_ = CfmHotlineClient::Get();
 
     // The easiest source of fds is opening /dev/null.
     test_file_ = base::File(base::FilePath("/dev/null"),
@@ -131,7 +130,6 @@ class CfmHotlineClientTest : public testing::Test {
   }
 
   base::test::SingleThreadTaskEnvironment task_environment_;
-  raw_ptr<CfmHotlineClient> client_;
   scoped_refptr<dbus::MockBus> mock_bus_;
   scoped_refptr<dbus::MockObjectProxy> mock_proxy_;
   std::deque<std::unique_ptr<dbus::Response>> responses_;
@@ -139,8 +137,8 @@ class CfmHotlineClientTest : public testing::Test {
 
  private:
   std::deque<std::unique_ptr<dbus::Response>> used_responses_;
-  // Maps from biod signal name to the corresponding callback provided by
-  // |client_|.
+  // Maps from biod signal name to the corresponding callback provided by the
+  // CfmHotlineClient.
   std::map<std::string, dbus::ObjectProxy::SignalCallback> signal_callbacks_;
 };
 
@@ -154,7 +152,7 @@ TEST_F(CfmHotlineClientTest, BootstrapMojoSuccessTest) {
       callback;
   EXPECT_CALL(callback, Run(true)).Times(1);
 
-  client_->BootstrapMojoConnection(
+  CfmHotlineClient::Get()->BootstrapMojoConnection(
       base::ScopedFD(test_file_.TakePlatformFile()), callback.Get());
 
   base::RunLoop().RunUntilIdle();
@@ -169,7 +167,7 @@ TEST_F(CfmHotlineClientTest, BootstrapMojoFailureTest) {
   EXPECT_CALL(callback, Run(false)).Times(1);
 
   // Fail with no normal or error response
-  client_->BootstrapMojoConnection(
+  CfmHotlineClient::Get()->BootstrapMojoConnection(
       base::ScopedFD(test_file_.TakePlatformFile()), callback.Get());
 
   base::RunLoop().RunUntilIdle();
