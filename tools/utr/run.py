b1107ca2956ced11776b2bf1dae0dbedc8fd97dd
Struan Shrimpton <sshrimp@google.com>
2024-03-22 00:16:06 +0000

    utr: Add an arg for using recipe checkout
    This arg should allow us to test recipe and cli changes at the same time
    more easily by replacing the bundle with a local checkout.
    
    Also fix a small formatting presubmit error
    
    Bug: 41492688
    Change-Id: I277a30f639756177ed12b487d227abcdf1359d63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5385282
    Commit-Queue: Struan Shrimpton <sshrimp@google.com>
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276609}

--- tools/utr/run.py
+++ tools/utr/run.py
@@ -11,6 +11,7 @@ same way it's done on the bots. See the README.md in //tools/utr/ for more info.
 
 import argparse
 import logging
+import pathlib
 import sys
 
 import builders
@@ -54,6 +55,11 @@ def add_common_args(parser):
       help='Path to the build dir to use for compilation and/or for invoking '
       'test binaries. Will use the output path used by the builder if not '
       'specified (likely //out/Release/).')
+  parser.add_argument(
+      '--recipe-path',
+      '-r',
+      type=pathlib.Path,
+      help='Path to override the recipe bundle with a local checkout.')
 
 
 def add_compile_args(parser):
@@ -106,7 +112,11 @@ def main():
   if not recipe.check_rdb_auth():
     return 1
 
-  bundle_root = cipd.fetch_recipe_bundle(args.verbosity)
+  if not args.recipe_path:
+    recipes_path = cipd.fetch_recipe_bundle(args.verbosity).joinpath('recipes')
+  else:
+    recipes_path = args.recipe_path.joinpath('recipes', 'recipes.py')
+
   builder_props, swarming_server = builders.find_builder_props(
       args.bucket, args.builder)
   if not builder_props:
@@ -115,7 +125,7 @@ def main():
   skip_compile = args.run_mode == 'test'
   skip_test = args.run_mode == 'compile'
   recipe_runner = recipe.LegacyRunner(
-      bundle_root,
+      recipes_path,
       builder_props,
       args.bucket,
       args.builder,
