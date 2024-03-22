e5dc712f608e6787fa897d3300bd29e70c28a40d
Li Lin <llin@chromium.org>
2024-03-22 02:08:34 +0000

    Customize the nudge with campaign payload.
    This CL:
    1. Customize icon
    2. Customize duration
    3. Add buttons and handle button clicks based on payload.
    
    Bug: b/330625533
    Change-Id: I92e79ce48a3b62ec539774ffa34af606df92eaaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5384713
    Commit-Queue: Li Lin <llin@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276654}

--- chrome/browser/ash/growth/show_nudge_action_performer.h
+++ chrome/browser/ash/growth/show_nudge_action_performer.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_GROWTH_SHOW_NUDGE_ACTION_PERFORMER_H_
 #define CHROME_BROWSER_ASH_GROWTH_SHOW_NUDGE_ACTION_PERFORMER_H_
 
+#include "ash/public/cpp/system/anchored_nudge_data.h"
 #include "base/functional/callback.h"
+#include "base/memory/weak_ptr.h"
 #include "base/values.h"
 #include "chromeos/ash/components/growth/action_performer.h"
 
@@ -30,7 +32,15 @@ class ShowNudgeActionPerformer : public growth::ActionPerformer {
   growth::ActionType ActionType() const override;
 
  private:
-  bool ShowNudge(const NudgePayload* nudge_payload);
+  bool ShowNudge(int campaign_id, const NudgePayload* nudge_payload);
+  void MaybeSetButtonData(int campaign_id,
+                          const base::Value::Dict* button_dict,
+                          ash::AnchoredNudgeData& nudge_data,
+                          bool is_primary);
+  void OnNudgeButtonClicked(int campaign_id,
+                            const base::Value::Dict* action_dict);
+
+  base::WeakPtrFactory<ShowNudgeActionPerformer> weak_ptr_factory_{this};
 };
 
 #endif  // CHROME_BROWSER_ASH_GROWTH_SHOW_NUDGE_ACTION_PERFORMER_H_
