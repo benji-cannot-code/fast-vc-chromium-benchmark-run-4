52bbbf70487305b385ef00ddb8d327a157d92460
Nina Satragno <nsatragno@chromium.org>
2024-03-22 21:20:07 +0000

    [crypto] Plumb LAContext to mac signing keys
    Plumb LAContext to UserVerifyingSigningKeyMac and UnexportableKeyMac.
    Clients can pass an authenticated LAContext to avoid prompting the user
    for local authentication on every call to Sign().
    
    For UserVerifyingSigningKeyMac, callers can set the LAContext on the
    provider config, and the provider takes ownership. Then, every operation
    with that provider will use the LAContext.
    
    UnexportableKeyMac uses a more involved approach where callers need to
    pass the LAContext every time a credential is discovered or created from
    an objective C source file. This affords more flexibility, which
    simplifies the UVKey implementation. Clients interested in this
    functionality should look at UVKeys instead anyway.
    
    Bug: 326451282
    Change-Id: I5e986e5a76ce8c821ef3abd2d19a5acc4a14e04f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5381486
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277093}

---
+++ crypto/scoped_lacontext.mm
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "crypto/scoped_lacontext.h"
+
+#include <memory>
+
+#include <LocalAuthentication/LocalAuthentication.h>
+
+#include "base/check.h"
+
+namespace crypto {
+
+struct ScopedLAContext::ObjCStorage {
+  LAContext* __strong context;
+};
+
+ScopedLAContext::ScopedLAContext(LAContext* lacontext)
+    : storage_(std::make_unique<ObjCStorage>()) {
+  storage_->context = lacontext;
+}
+
+ScopedLAContext::ScopedLAContext(ScopedLAContext&&) = default;
+ScopedLAContext& ScopedLAContext::operator=(ScopedLAContext&& other) = default;
+ScopedLAContext::~ScopedLAContext() = default;
+
+LAContext* ScopedLAContext::release() {
+  CHECK(storage_);
+  LAContext* context = storage_->context;
+  storage_.reset();
+  return context;
+}
+
+}  // namespace crypto
--- crypto/user_verifying_key_mac.mm
+++ crypto/user_verifying_key_mac.mm
@@ -21,7 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "base/threading/scoped_thread_priority.h"
 #include "crypto/apple_keychain_v2.h"
+#include "crypto/scoped_lacontext.h"
 #include "crypto/unexportable_key.h"
+#include "crypto/unexportable_key_mac.h"
 #include "crypto/user_verifying_key.h"
 
 namespace crypto {
@@ -103,7 +105,8 @@ class UserVerifyingSigningKeyMac : public UserVerifyingSigningKey {
 class UserVerifyingKeyProviderMac : public UserVerifyingKeyProvider {
  public:
   explicit UserVerifyingKeyProviderMac(UserVerifyingKeyProvider::Config config)
-      : config_(std::move(config)) {}
+      : lacontext_(config.lacontext ? config.lacontext->release() : nil),
+        config_(std::move(config)) {}
   ~UserVerifyingKeyProviderMac() override = default;
 
   void GenerateUserVerifyingSigningKey(
@@ -111,14 +114,15 @@ class UserVerifyingKeyProviderMac : public UserVerifyingKeyProvider {
           acceptable_algorithms,
       base::OnceCallback<void(std::unique_ptr<UserVerifyingSigningKey>)>
           callback) override {
-    std::unique_ptr<UnexportableKeyProvider> key_provider =
-        GetUnexportableKeyProvider(MakeUnexportableKeyConfig());
+    std::unique_ptr<UnexportableKeyProviderMac> key_provider =
+        GetUnexportableKeyProviderMac(MakeUnexportableKeyConfig());
     if (!key_provider) {
       std::move(callback).Run(nullptr);
       return;
     }
     std::unique_ptr<UnexportableSigningKey> key =
-        key_provider->GenerateSigningKeySlowly(acceptable_algorithms);
+        key_provider->GenerateSigningKeySlowly(acceptable_algorithms,
+                                               lacontext_);
     if (!key) {
       std::move(callback).Run(nullptr);
       return;
@@ -132,14 +136,14 @@ class UserVerifyingKeyProviderMac : public UserVerifyingKeyProvider {
       base::OnceCallback<void(std::unique_ptr<UserVerifyingSigningKey>)>
           callback) override {
     std::vector<uint8_t> wrapped_key(key_label.begin(), key_label.end());
-    std::unique_ptr<UnexportableKeyProvider> key_provider =
-        GetUnexportableKeyProvider(MakeUnexportableKeyConfig());
+    std::unique_ptr<UnexportableKeyProviderMac> key_provider =
+        GetUnexportableKeyProviderMac(MakeUnexportableKeyConfig());
     if (!key_provider) {
       std::move(callback).Run(nullptr);
       return;
     }
     std::unique_ptr<UnexportableSigningKey> key =
-        key_provider->FromWrappedSigningKeySlowly(wrapped_key);
+        key_provider->FromWrappedSigningKeySlowly(wrapped_key, lacontext_);
     if (!key) {
       std::move(callback).Run(nullptr);
       return;
@@ -169,6 +173,7 @@ class UserVerifyingKeyProviderMac : public UserVerifyingKeyProvider {
             UnexportableKeyProvider::Config::AccessControl::kUserPresence,
     };
   }
+  LAContext* __strong lacontext_;
   const UserVerifyingKeyProvider::Config config_;
 };
 
