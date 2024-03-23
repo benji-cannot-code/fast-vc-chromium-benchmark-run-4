6cace61a9a53c6237cb0265b13238d2ebb8142a2
Robbie McElrath <rmcelrath@chromium.org>
2024-03-23 02:57:45 +0000

    controlled frame: Refactor ControlledFrameApiTest
    This migrates most ControlledFrameApiTest over to IsolatedWebAppBuilder
    which allows several test apps in chrome/test/data to be removed, and
    simplifies how the test IWAs are configured and installed.
    
    Additionally, InProcessBrowserTest already has an http test server, so
    this updates the tests to use the existing server to serve content for
    <controlledframe> rather than creating a dedicated one.
    
    Bug: b/322386534, b/40191772
    Change-Id: I992968d5cd5995ed196f8d2e1a313944bb186848
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5386747
    Reviewed-by: Chase Phillips <cmp@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277217}

--- chrome/browser/web_applications/isolated_web_apps/isolated_web_app_csp_browsertest.cc
+++ chrome/browser/web_applications/isolated_web_apps/isolated_web_app_csp_browsertest.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 
+#include "base/base_paths.h"
 #include "base/files/file_path.h"
+#include "base/path_service.h"
 #include "base/test/gmock_expected_support.h"
 #include "chrome/browser/ui/web_applications/test/isolated_web_app_test_utils.h"
 #include "chrome/browser/web_applications/isolated_web_apps/isolated_web_app_url_info.h"
@@ -24,17 +26,19 @@ class IsolatedWebAppCspBrowserTest : public IsolatedWebAppBrowserTestHarness {
   void SetUpOnMainThread() override {
     IsolatedWebAppBrowserTestHarness::SetUpOnMainThread();
 
-    embedded_https_test_server().ServeFilesFromSourceDirectory(resource_path());
+    embedded_https_test_server().ServeFilesFromDirectory(resource_path());
     ASSERT_TRUE(embedded_https_test_server().Start());
 
-    embedded_test_server()->ServeFilesFromSourceDirectory(resource_path());
+    embedded_test_server()->ServeFilesFromDirectory(resource_path());
     ASSERT_TRUE(embedded_test_server()->Start());
   }
 
  protected:
   base::FilePath resource_path() {
-    return GetChromeTestDataDir().Append(
-        FILE_PATH_LITERAL("web_apps/isolated_csp"));
+    base::FilePath base_path;
+    CHECK(base::PathService::Get(base::DIR_SRC_TEST_DATA_ROOT, &base_path));
+    return base_path.Append(GetChromeTestDataDir())
+        .AppendASCII("web_apps/isolated_csp");
   }
 };
 
@@ -63,7 +67,7 @@ IN_PROC_BROWSER_TEST_F(IsolatedWebAppCspBrowserTest, Base) {
 IN_PROC_BROWSER_TEST_F(IsolatedWebAppCspBrowserTest, Src) {
   std::unique_ptr<ScopedBundledIsolatedWebApp> app =
       IsolatedWebAppBuilder(ManifestBuilder())
-          .AddFolderFromDisk("/", resource_path().AsUTF8Unsafe())
+          .AddFolderFromDisk("/", resource_path())
           .BuildBundle();
 
   app->TrustSigningKey();
