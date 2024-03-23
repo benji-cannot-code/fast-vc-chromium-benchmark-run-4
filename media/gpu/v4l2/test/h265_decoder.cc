a912ae68a6b9714c3feb9cfdbcf0f599e2e47729
Fritz Koenig <frkoenig@chromium.org>
2024-03-23 00:51:19 +0000

    media/gpu/v4l2/test: Wait on request fd
    The request fd acts as a fence for the completion of
    the OUTPUT, CAPTURE, and MEDIA_REQUEST queues. This avoids
    looping on dequeuing each buffer individually.
    
    This change reduces the independence of the queues and
    that is desirable. There may be a very small performance
    boost by continuing when the OUTPUT queue has returned.
    In reality all of the queues complete at the same time.
    
    Bug: b:330762949
    Change-Id: I282974dc5b8ca26bdd181c6f23c8f9425bfd1ed5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5388080
    Commit-Queue: Fritz Koenig <frkoenig@chromium.org>
    Reviewed-by: Steve Cho <stevecho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277185}

--- media/gpu/v4l2/test/h265_decoder.cc
+++ media/gpu/v4l2/test/h265_decoder.cc
@@ -1140,6 +1140,8 @@ bool H265Decoder::DecodePicture() {
     CreateCAPTUREQueue(kNumberOfBuffersInCaptureQueue);
   }
 
+  v4l2_ioctl_->WaitForRequestCompletion(OUTPUT_queue_);
+
   uint32_t CAPTURE_id;
   v4l2_ioctl_->DQBuf(CAPTURE_queue_, &CAPTURE_id);
 
