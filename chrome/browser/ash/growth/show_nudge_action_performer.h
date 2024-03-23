0319adbee34228f949289871a8bfd26417060bc2
Tao Wu <wutao@chromium.org>
2024-03-23 05:21:19 +0000

    growth: Add UI action performer
    Add a base class for UI action performers, such as show nudge action
    performer.
    
    The base class will handle basic methods to notify observers of the UI
    actions, such as logging the impression, button press, and dismissal.
    
    Bug: b:329671682
    Test: Manually tested and Added new unittests
    Change-Id: I0c38bd47cb6d6d17276f838f6353a0303f26c515
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5388885
    Reviewed-by: Li Lin <llin@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277235}

--- chrome/browser/ash/growth/show_nudge_action_performer.h
+++ chrome/browser/ash/growth/show_nudge_action_performer.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-/// Copyright 2024 The Chromium Authors
+// Copyright 2024 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "base/values.h"
-#include "chromeos/ash/components/growth/action_performer.h"
+#include "chrome/browser/ash/growth/ui_action_performer.h"
 
 // Dictionary of supported nudge payload. For example:
 // {
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using NudgePayload = base::Value::Dict;
 
 // Implements the action to show nudge.
-class ShowNudgeActionPerformer : public growth::ActionPerformer {
+class ShowNudgeActionPerformer : public UiActionPerformer {
  public:
   ShowNudgeActionPerformer();
   ~ShowNudgeActionPerformer() override;
