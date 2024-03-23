56a43de9a0c62f1b154c4c754f1bd092d2b84733
Kent Tamura <tkent@chromium.org>
2024-03-23 12:51:31 +0000

    RubyLB: Consume InlineItemResultRubyColumn::base_line
    * Add PlaceRubyColumn().
    * The `for` loop for InlineItemResults is moved to a new function, and
      PlaceRubyColumn() uses it.
    
    This CL has no behavior changes due to a disabled runtime flag.
    
    Bug: 324111880
    Change-Id: I862d4186a6b23db8cc5e2556668bf0148758e822
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5382183
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277269}

--- third_party/blink/renderer/core/layout/inline/logical_line_builder.h
+++ third_party/blink/renderer/core/layout/inline/logical_line_builder.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_INLINE_LOGICAL_LINE_BUILDER_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_INLINE_LOGICAL_LINE_BUILDER_H_
 
+#include "third_party/blink/renderer/core/layout/inline/inline_item_result.h"
 #include "third_party/blink/renderer/core/layout/inline/inline_node.h"
 
 namespace blink {
@@ -16,7 +17,6 @@ class InlineLayoutStateStack;
 class LineInfo;
 class LogicalLineItems;
 struct InlineBoxState;
-struct InlineItemResult;
 
 // This class is responsible to build a LogicalLineItems from a LineInfo.
 // It's a helper for InlineLayoutAlgorithm.
@@ -52,6 +52,11 @@ class LogicalLineBuilder {
                                 InlineLayoutStateStack*) const;
 
  private:
+  InlineBoxState* HandleItemResults(const LineInfo& line_info,
+                                    InlineItemResults& line_items,
+                                    LogicalLineItems* line_box,
+                                    InlineLayoutAlgorithm* main_line_helper,
+                                    InlineBoxState* box);
   InlineBoxState* HandleCloseTag(const InlineItem&,
                                  const InlineItemResult&,
                                  LogicalLineItems* line_box,
@@ -76,6 +81,10 @@ class LogicalLineBuilder {
                          LogicalLineItems* line_box,
                          InlineBoxState*,
                          LayoutUnit inline_offset = LayoutUnit());
+  InlineBoxState* PlaceRubyColumn(const LineInfo& line_info,
+                                  InlineItemResult& item_result,
+                                  LogicalLineItems& line_box,
+                                  InlineBoxState* box);
   void PlaceListMarker(const InlineItem&, InlineItemResult*);
 
   void BidiReorder(TextDirection base_direction, LogicalLineItems* line_box);
