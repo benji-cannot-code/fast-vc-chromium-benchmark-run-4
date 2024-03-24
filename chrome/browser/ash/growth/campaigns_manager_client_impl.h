6394f18666609cf723864007387b2cf70864f854
Tao Wu <wutao@chromium.org>
2024-03-24 04:44:25 +0000

    Reland "growth: Add UI action performer"
    This is a reland of commit bbf7839eae83a8cd1524d3561e99d5fe8ad3dbe5
    
    Original change's description:
    > growth: Add UI action performer
    >
    > Add a base class for UI action performers, such as show nudge action
    > performer.
    >
    > The base class will handle basic methods to notify observers of the UI
    > actions, such as logging the impression, button press, and dismissal.
    >
    > Bug: b:329671682
    > Test: Manually tested and Added new unittests
    > Change-Id: I0c38bd47cb6d6d17276f838f6353a0303f26c515
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5388885
    > Reviewed-by: Li Lin <llin@chromium.org>
    > Commit-Queue: Tao Wu <wutao@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1277235}
    
    Bug: b:329671682
    Change-Id: I1241bf2f1d933b9c26c8c5690651c4db8f17f267
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5390070
    Reviewed-by: Li Lin <llin@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277345}

--- chrome/browser/ash/growth/campaigns_manager_client_impl.h
+++ chrome/browser/ash/growth/campaigns_manager_client_impl.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/weak_ptr.h"
+#include "base/scoped_observation.h"
+#include "chrome/browser/ash/growth/ui_action_performer.h"
 #include "chrome/browser/component_updater/cros_component_manager.h"
 #include "chromeos/ash/components/growth/campaigns_manager_client.h"
 
@@ -20,7 +22,8 @@ namespace growth {
 class CampaignsManager;
 }  // namespace growth
 
-class CampaignsManagerClientImpl : public growth::CampaignsManagerClient {
+class CampaignsManagerClientImpl : public growth::CampaignsManagerClient,
+                                   public UiActionPerformer::Observer {
  public:
   CampaignsManagerClientImpl();
   CampaignsManagerClientImpl(const CampaignsManagerClientImpl&) = delete;
@@ -36,9 +39,16 @@ class CampaignsManagerClientImpl : public growth::CampaignsManagerClient {
   bool IsFeatureAwareDevice() const override;
   const std::string& GetApplicationLocale() const override;
   const base::Version& GetDemoModeAppVersion() const override;
-  growth::ActionMap GetCampaignsActions() const override;
+  growth::ActionMap GetCampaignsActions() override;
   void RegisterSyntheticFieldTrial(const std::optional<int> study_id,
                                    const int campaign_id) const override;
+
+  // UiActionPerformer::Observer:
+  void OnReadyToLogImpression() override;
+  void OnUiDismissed() override;
+  void OnPrimaryButtonPressed() override;
+  void OnSecondaryButtonPressed() override;
+  void OnCloseButtonPressed() override;
 
  private:
   void OnComponentDownloaded(
@@ -47,6 +57,11 @@ class CampaignsManagerClientImpl : public growth::CampaignsManagerClient {
       const base::FilePath& path);
 
   std::unique_ptr<growth::CampaignsManager> campaigns_manager_;
+
+  // Reset before `campaigns_manager_`, because `this` observes one of the
+  // performers owned by the manager.
+  base::ScopedObservation<UiActionPerformer, UiActionPerformer::Observer>
+      show_nudge_performer_observation_{this};
 
   base::WeakPtrFactory<CampaignsManagerClientImpl> weak_ptr_factory_{this};
 };
