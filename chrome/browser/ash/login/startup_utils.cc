11d4349fb8fdb30f52394eeb62febddbd63911f3
Li Lin <llin@chromium.org>
2024-03-25 19:39:43 +0000

    Persist registered date in local state.
    The new pref name is added to ash_pref_names.h instead of pref_names.h
    to allow access from other part of ash code base.
    
    Bug: b/330745552
    Change-Id: I03c7e734685145c6ed4e0d7c14cf3915c9f62331
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5386373
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Li Lin <llin@chromium.org>
    Reviewed-by: Jakub Flis <jaflis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1277852}

--- chrome/browser/ash/login/startup_utils.cc
+++ chrome/browser/ash/login/startup_utils.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/components/arc/arc_prefs.h"
 #include "ash/constants/ash_features.h"
+#include "ash/constants/ash_pref_names.h"
 #include "ash/constants/ash_switches.h"
 #include "base/command_line.h"
 #include "base/files/file_util.h"
@@ -66,6 +67,13 @@ void SaveStringPreferenceForced(const char* pref_name,
   prefs->CommitPendingWrite();
 }
 
+// Saves time "Local State" preference and forces its persistence to disk.
+void SaveTimePreferenceForced(const char* pref_name, base::Time value) {
+  PrefService* prefs = g_browser_process->local_state();
+  prefs->SetTime(pref_name, value);
+  prefs->CommitPendingWrite();
+}
+
 // Returns the path to flag file indicating that both parts of OOBE were
 // completed.
 // On chrome device, returns /home/chronos/.oobe_completed.
@@ -105,6 +113,7 @@ void StartupUtils::RegisterPrefs(PrefRegistrySimple* registry) {
   registry->RegisterStringPref(prefs::kOobeScreenPending, "");
   registry->RegisterTimePref(prefs::kOobeStartTime, base::Time());
   registry->RegisterIntegerPref(::prefs::kDeviceRegistered, -1);
+  registry->RegisterTimePref(ash::prefs::kDeviceRegisteredTime, base::Time());
   registry->RegisterBooleanPref(::prefs::kEnrollmentRecoveryRequired, false);
   registry->RegisterStringPref(::prefs::kInitialLocale, "en-US");
   registry->RegisterBooleanPref(kDisableHIDDetectionScreenForTests, false);
@@ -247,6 +256,9 @@ void StartupUtils::ClearSpecificOobePrefs() {
 // static
 void StartupUtils::MarkDeviceRegistered(base::OnceClosure done_callback) {
   SaveIntegerPreferenceForced(::prefs::kDeviceRegistered, 1);
+
+  SaveTimePreferenceForced(ash::prefs::kDeviceRegisteredTime,
+                           base::Time::Now());
 
   auto* host = LoginDisplayHost::default_host();
   if (host) {
