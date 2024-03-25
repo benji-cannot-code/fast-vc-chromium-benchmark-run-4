ca94881e7d12fd29d637c5f778f4732a4a256c84
Avi Drissman <avi@chromium.org>
2024-03-25 18:01:11 +0000

    Move components/feed/ to use new base::Pickle functionality
    This moves to using the new base::Pickle creation factories,
    opting for a safer internalized memory.
    
    Bug: 330028190
    Change-Id: Iaf4ee019973a789e9898549e2799e054b5fa1a48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5386007
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277766}

--- components/feed/core/v2/types.cc
+++ components/feed/core/v2/types.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/base64.h"
+#include "base/containers/span.h"
 #include "base/json/values_util.h"
 #include "base/pickle.h"
 #include "base/strings/strcat.h"
@@ -161,7 +162,7 @@ std::optional<DebugStreamData> DeserializeDebugStreamData(
   std::string binary_data;
   if (!base::Base64Decode(base64_encoded, &binary_data))
     return std::nullopt;
-  base::Pickle pickle(binary_data.data(), binary_data.size());
+  base::Pickle pickle = base::Pickle::WithData(base::as_byte_span(binary_data));
   DebugStreamData result;
   if (!UnpickleDebugStreamData(base::PickleIterator(pickle), result))
     return std::nullopt;
