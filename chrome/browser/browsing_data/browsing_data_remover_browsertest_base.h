f6edd60653145a62b84d009b8be605c1ab148dad
Mariam Ali <alimariam@google.com>
2024-03-25 13:42:42 +0000

    [Deprecate CookiesTreeModel] Remove model from tests
    Bug: 40205603, b:277057437
    Change-Id: Ie899645985092dcc194c2ee182ec9a92d6101b13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5385971
    Commit-Queue: Mariam Ali <alimariam@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277606}

--- chrome/browser/browsing_data/browsing_data_remover_browsertest_base.h
+++ chrome/browser/browsing_data/browsing_data_remover_browsertest_base.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
-#include "chrome/browser/browsing_data/cookies_tree_model.h"
 #include "chrome/test/base/chrome_test_utils.h"
 #include "components/browsing_data/content/browsing_data_model.h"
 #include "components/signin/public/base/signin_buildflags.h"
@@ -88,18 +87,8 @@ class BrowsingDataRemoverBrowserTestBase : public PlatformBrowserTest {
       const std::vector<std::string>& ignore_file_patterns,
       bool check_leveldb_content);
 
-  // Returns the cookie tree model for the browser.
-  std::unique_ptr<CookiesTreeModel> GetCookiesTreeModel(Profile* profile);
-
   // Returns the browsing data model for the browser.
   std::unique_ptr<BrowsingDataModel> GetBrowsingDataModel(Profile* profile);
-
-  // Returns the sum of the number of datatypes per host.
-  int GetCookiesTreeModelCount(const CookieTreeNode* root);
-
-  // Returns a string with information about the content of the
-  // cookie tree model.
-  std::string GetCookiesTreeModelInfo(const CookieTreeNode* root);
 
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
   // Sets the APISID Gaia cookie, which is monitored by the AccountReconcilor.
