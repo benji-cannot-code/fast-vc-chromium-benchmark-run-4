328a54d26033f82ec73e16c24eeb3e06de4f75ae
Brahim Chikhaoui <bchikhaoui@google.com>
2024-03-25 13:39:09 +0000

    [OOBE][Mojo] Migration of LocalDataLossWarningScreen to Mojo
    add screen_osauth mojom for osauth screens in oobe and append to
    OobeScreensHandlerFactory to create LocalDataLossWarningScreen pipe
    and migrate the LocalDataLossWarningScreen to mojo.
    
    DD: go/oobe-mojo-migration
    Bug: b:325924806
    Change-Id: Ie915684c0b26383e91fa3d7535390d839158fb43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5366412
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1277605}

--- chrome/browser/ui/webui/ash/login/oobe_screens_handler_factory.h
+++ chrome/browser/ui/webui/ash/login/oobe_screens_handler_factory.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/login/mojom/screens_factory.mojom.h"
 #include "chrome/browser/ui/webui/ash/login/mojom/screens_login.mojom.h"
 #include "chrome/browser/ui/webui/ash/login/mojom/screens_oobe.mojom.h"
+#include "chrome/browser/ui/webui/ash/login/mojom/screens_osauth.mojom.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver.h"
@@ -49,6 +50,11 @@ class OobeScreensHandlerFactory
       mojo::PendingReceiver<
           screens_login::mojom::LacrosDataBackwardMigrationPageHandler>
           receiver) override;
+
+  void CreateLocalDataLossWarningPageHandler(
+      mojo::PendingReceiver<
+          screens_osauth::mojom::LocalDataLossWarningPageHandler> receiver)
+      override;
 
   mojo::Receiver<screens_factory::mojom::ScreensFactory> page_factory_receiver_{
       this};
