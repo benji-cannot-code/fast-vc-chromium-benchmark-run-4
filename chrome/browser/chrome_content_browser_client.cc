a486de4fec35d8d91694c82c3e65c93438ad3456
Sam Davis Omekara <samomekarajr@microsoft.com>
2024-03-25 21:56:42 +0000

    Add block list feature to forced colors state in Blink
    This CL adds the page colors block list feature for the forced colors
    state in Blink. The addition of this feature improves user experience
    by allowing the exclusion of specific sites from forced colors mode,
    particularly where it may render the site unusable.
    
    This CL also adds a new browser test to verify the behavior of forced
    colors mode with and without the block list feature.
    
    Bug: 1231644
    Change-Id: I3dddfd58fe1b6fb43e463a5438d1c3421db057c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5368935
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Commit-Queue: Sam Davis Omekara <samomekarajr@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1277972}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -3594,6 +3594,43 @@ bool ChromeContentBrowserClient::ShouldDenyRequestOnCertificateError(
 
 namespace {
 
+bool IsForcedColorsEnabledForWebContent(content::WebContents* contents,
+                                        const ui::NativeTheme* native_theme) {
+  if (!native_theme->InForcedColorsMode() || !contents) {
+    return false;
+  }
+
+  PrefService* prefs =
+      Profile::FromBrowserContext(contents->GetBrowserContext())->GetPrefs();
+  CHECK(prefs);
+
+  const base::Value::List& forced_colors_blocklist =
+      prefs->GetList(prefs::kPageColorsBlockList);
+
+  if (forced_colors_blocklist.empty()) {
+    return true;
+  }
+
+  GURL url = contents->GetLastCommittedURL();
+
+  // Forced Colors should be disabled for the current URL if it is in the block
+  // list.
+  for (auto const& value : forced_colors_blocklist) {
+    ContentSettingsPattern pattern =
+        ContentSettingsPattern::FromString(value.GetString());
+
+    if (pattern == ContentSettingsPattern::Wildcard() || !pattern.IsValid()) {
+      continue;
+    }
+
+    if (pattern.Matches(url)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 #if !BUILDFLAG(IS_ANDROID)
 blink::mojom::PreferredColorScheme ToBlinkPreferredColorScheme(
     ui::NativeTheme::PreferredColorScheme native_theme_scheme) {
@@ -4218,7 +4255,8 @@ void ChromeContentBrowserClient::OverrideWebkitPrefs(
       break;
   }
 
-  web_prefs->in_forced_colors = GetWebTheme()->InForcedColorsMode();
+  web_prefs->in_forced_colors =
+      IsForcedColorsEnabledForWebContent(web_contents, GetWebTheme());
 
   UpdatePreferredColorScheme(
       web_prefs,
@@ -4321,7 +4359,10 @@ bool ChromeContentBrowserClient::OverrideWebPreferencesAfterNavigation(
         parts->OverrideWebPreferencesAfterNavigation(web_contents, web_prefs);
   }
 
-  prefs_changed |= GetWebTheme()->InForcedColorsMode();
+  const bool in_forced_colors =
+      IsForcedColorsEnabledForWebContent(web_contents, GetWebTheme());
+  prefs_changed |= (web_prefs->in_forced_colors != in_forced_colors);
+  web_prefs->in_forced_colors = in_forced_colors;
 
   prefs_changed |=
       UpdatePreferredColorScheme(web_prefs, web_contents->GetLastCommittedURL(),
