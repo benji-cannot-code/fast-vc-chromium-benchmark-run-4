f46607bba55a71630e4212bc8c43f64a8ed94aa2
Takuto Ikuta <tikuta@chromium.org>
2024-03-25 02:03:03 +0000

    remoting: disable format around atl headers
    This is a preparation CL to enable include ordering with clang-format in
    https://crrev.com/c/5197064.
    
    Bug: 329138753, 330805203
    Change-Id: Icbe3cfd51219acad0601729def32d4d023b7c4fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5383148
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277437}

--- remoting/host/win/rdp_client_window.h
+++ remoting/host/win/rdp_client_window.h
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef REMOTING_HOST_WIN_RDP_CLIENT_WINDOW_H_
 #define REMOTING_HOST_WIN_RDP_CLIENT_WINDOW_H_
 
-// Must be included before <atlapp.h>.
-#include "base/win/atl.h"  // NOLINT(build/include_order)
+// clang-format off
+// This needs to be included before ATL headers.
+#include "base/win/atl.h"
+// clang-format on
 
 #include <atlapp.h>
 #include <atlcrack.h>
