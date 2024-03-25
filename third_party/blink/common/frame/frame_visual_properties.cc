69ccb8aad54ad0acc8a8408a150fd2a97d30df00
Stefan Zager <szager@chromium.org>
2024-03-25 22:35:04 +0000

    Switch to proportion of frame size for input discarding threshold
    Bug: chromium:40866929
    Change-Id: I073d85f4a3c6f48144fa96a6ffecc1901bfcc730
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5386389
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277998}

--- third_party/blink/common/frame/frame_visual_properties.cc
+++ third_party/blink/common/frame/frame_visual_properties.cc
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 namespace {
-std::optional<int> max_child_frame_screen_rect_movement;
+// Note that this is a factor to be multiplied by the frame dimensions, in
+// contrast to s_legacy_max_child_frame_screen_rect_movement, which is in DIPS.
+std::optional<double> max_child_frame_screen_rect_movement;
 std::optional<int> min_screen_rect_stable_time_ms;
 
 // These are the values that were in use prior to adding the feature flag
@@ -30,12 +32,12 @@ FrameVisualProperties::~FrameVisualProperties() = default;
 FrameVisualProperties& FrameVisualProperties::operator=(
     const FrameVisualProperties& other) = default;
 
-int FrameVisualProperties::MaxChildFrameScreenRectMovement() {
+double FrameVisualProperties::MaxChildFrameScreenRectMovement() {
   if (!max_child_frame_screen_rect_movement.has_value()) {
     max_child_frame_screen_rect_movement.emplace(
-        base::GetFieldTrialParamByFeatureAsInt(
-            features::kDiscardInputEventsToRecentlyMovedFrames, "distance",
-            std::numeric_limits<int>::max()));
+        base::GetFieldTrialParamByFeatureAsDouble(
+            features::kDiscardInputEventsToRecentlyMovedFrames,
+            "distance_factor", std::numeric_limits<double>::max()));
   }
   return max_child_frame_screen_rect_movement.value();
 }
