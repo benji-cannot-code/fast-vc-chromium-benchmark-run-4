a4e272f62531c27c1c147beca40a28a3379185bf
Helmut Januschka <helmut@januschka.com>
2024-03-25 16:43:28 +0000

    android: base::StringPiece -> std::string_view
    Bug: 691162
    Change-Id: Ie5f7f6a1b8aa66f65bdde985c6a310b581d09659
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372455
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277716}

--- chrome/browser/android/servicification_background_service_jni.cc
+++ chrome/browser/android/servicification_background_service_jni.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <sstream>
+#include <string_view>
 
 #include "base/android/jni_android.h"
 #include "base/files/file.h"
@@ -60,7 +61,7 @@ JNI_ServicificationBackgroundService_TestPersistentHistogramsOnDiskSystemProfile
   // Map the file and validate it.
   std::unique_ptr<base::FilePersistentMemoryAllocator> memory_allocator =
       std::make_unique<base::FilePersistentMemoryAllocator>(
-          std::move(mapped), 0, 0, base::StringPiece(),
+          std::move(mapped), 0, 0, std::string_view(),
           base::FilePersistentMemoryAllocator::kReadOnly);
   if (memory_allocator->GetMemoryState() ==
       base::PersistentMemoryAllocator::MEMORY_DELETED) {
