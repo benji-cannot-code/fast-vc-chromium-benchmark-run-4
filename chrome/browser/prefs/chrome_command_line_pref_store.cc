eab481e2877de8531dd7bd8f330ef0a497ad856a
Helmut Januschka <helmut@januschka.com>
2024-03-25 20:14:29 +0000

    prefs: base::StringPiece -> std::string_view
    Bug: 691162
    Change-Id: I2756999bb106b2333c744b76c8ef63aeb2657624
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5377725
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Helmut Januschka <helmut@januschka.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277887}

--- chrome/browser/prefs/chrome_command_line_pref_store.cc
+++ chrome/browser/prefs/chrome_command_line_pref_store.cc
@@ -9,13 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <string>
+#include <string_view>
 #include <utility>
 #include <vector>
 
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/strings/string_piece.h"
 #include "base/strings/string_split.h"
 #include "base/values.h"
 #include "build/build_config.h"
