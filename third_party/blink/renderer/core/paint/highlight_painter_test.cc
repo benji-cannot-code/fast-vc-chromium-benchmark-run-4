ca65a1defd5b61d69ab764193bd5c5e4bd40d58e
Philip Rogers <pdr@chromium.org>
2024-03-25 23:09:43 +0000

    Fold TextPainterBase into TextPainter
    This abstraction was used during the development of LayoutNG, but is no
    longer useful, and is making https://crrev.com/c/5367130 difficult.
    TextPainter is now the base class, with only TextCombinePainter
    extending from it. This refactoring enabled cleaning up some deadcode,
    such as RotationDirection. To match the style guide, some protected
    members have become private with protected accessors.
    
    Bug: 40829719
    Change-Id: Ifbbf13097b0f335e69c89af9045c752244fc79c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5378357
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278014}

--- third_party/blink/renderer/core/paint/highlight_painter_test.cc
+++ third_party/blink/renderer/core/paint/highlight_painter_test.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/inline_paint_context.h"
 #include "third_party/blink/renderer/core/paint/line_relative_rect.h"
 #include "third_party/blink/renderer/core/paint/paint_controller_paint_test.h"
+#include "third_party/blink/renderer/core/paint/paint_info.h"
 #include "third_party/blink/renderer/core/paint/text_decoration_painter.h"
 #include "third_party/blink/renderer/core/paint/text_paint_style.h"
 #include "third_party/blink/renderer/core/paint/text_painter.h"
@@ -63,7 +64,7 @@ TEST_P(HighlightPainterTest, FastSpellingGrammarPaintCase) {
     GraphicsContext graphics_context{RootPaintController()};
     PaintInfo paint_info{graphics_context, cull_rect, PaintPhase::kForeground};
     TextPaintStyle text_style =
-        TextPainterBase::TextPaintingStyle(GetDocument(), style, paint_info);
+        TextPainter::TextPaintingStyle(GetDocument(), style, paint_info);
     if (selection) {
       selection->ComputeSelectionStyle(GetDocument(), style,
                                        text_item.GetLayoutObject()->GetNode(),
