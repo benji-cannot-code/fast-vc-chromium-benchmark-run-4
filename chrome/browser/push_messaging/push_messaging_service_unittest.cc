b9541227c5066e78d523f26b194bd02bd0194e90
Helmut Januschka <helmut@januschka.com>
2024-03-25 12:29:14 +0000

    push_messaging: base::StringPiece -> std::string_view
    Bug: 691162
    Change-Id: I7db5607b1c683cdce6c94f47047b1def0302c1e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5377840
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277575}

--- chrome/browser/push_messaging/push_messaging_service_unittest.cc
+++ chrome/browser/push_messaging/push_messaging_service_unittest.cc
@@ -3,12 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/public/browser/push_messaging_service.h"
-
 #include <stdint.h>
 
 #include <optional>
 #include <string>
+#include <string_view>
 #include <utility>
 #include <vector>
 
@@ -39,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history/core/browser/history_service.h"
 #include "components/history/core/test/test_history_database.h"
 #include "components/permissions/permission_manager.h"
+#include "content/public/browser/push_messaging_service.h"
 #include "content/public/common/content_features.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -350,10 +350,10 @@ TEST_F(PushMessagingServiceTest, MAYBE_PayloadEncryptionTest) {
 
   ASSERT_TRUE(gcm::CreateEncryptedPayloadForTesting(
       kTestPayload,
-      base::StringPiece(reinterpret_cast<char*>(subscription.p256dh_.data()),
-                        subscription.p256dh_.size()),
-      base::StringPiece(reinterpret_cast<char*>(subscription.auth_.data()),
-                        subscription.auth_.size()),
+      std::string_view(reinterpret_cast<char*>(subscription.p256dh_.data()),
+                       subscription.p256dh_.size()),
+      std::string_view(reinterpret_cast<char*>(subscription.auth_.data()),
+                       subscription.auth_.size()),
       &message));
 
   ASSERT_GT(message.raw_data.size(), 0u);
