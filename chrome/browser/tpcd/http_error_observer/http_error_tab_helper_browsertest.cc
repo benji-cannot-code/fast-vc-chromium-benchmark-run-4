2c8028abc936472a29d62842be46f10a1b74ed6a
Helmut Januschka <helmut@januschka.com>
2024-03-25 16:10:51 +0000

    tpcd: base::StringPiece -> std::string_view
    Bug: 691162
    Change-Id: I702024565a4512c0a91a82602fed32b27ab27014
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5375252
    Reviewed-by: Joshua Hood <jdh@chromium.org>
    Commit-Queue: Helmut Januschka <helmut@januschka.com>
    Reviewed-by: Jonathan Njeunje <njeunje@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277689}

--- chrome/browser/tpcd/http_error_observer/http_error_tab_helper_browsertest.cc
+++ chrome/browser/tpcd/http_error_observer/http_error_tab_helper_browsertest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/tpcd/http_error_observer/http_error_tab_helper.h"
 
+#include <string_view>
+
 #include "base/feature_list.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
@@ -102,7 +104,7 @@ class HTTPErrProcBrowserTest : public InProcessBrowserTest {
 
   // Navigates to a page with an iframe, then navigates the iframe to the given
   // GURL. Can also set TPC blocking cookie.
-  void NavigateToURLAndIFrame(base::StringPiece host, const GURL iframe_url) {
+  void NavigateToURLAndIFrame(std::string_view host, const GURL iframe_url) {
     ASSERT_TRUE(ui_test_utils::NavigateToURL(
         browser(), https_server()->GetURL(host, "/iframe.html")));
     ASSERT_TRUE(NavigateIframeToURL(
