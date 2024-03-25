09b2dade68d2d57d562c8ff81a3f251b48827e2e
Evan Stade <estade@chromium.org>
2024-03-25 19:02:32 +0000

    SQLite: tweak protobuf query generator to avoid infinite recursion
    This code is used by fuzzers to generate queries. Make sure CTEs won't
    infinitely recurse by setting a hard cap.
    
    See buganizer issue for more details.
    
    Bug: 328281725
    Change-Id: I9da31e9d1535fa01cfbca2a2723620ecb4bd85b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5388621
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277820}

--- third_party/sqlite/fuzz/sql_query_proto_to_string.cc
+++ third_party/sqlite/fuzz/sql_query_proto_to_string.cc
@@ -1200,7 +1200,8 @@ CONV_FN(CommonTableExpr, cte) {
   }
   ret += " AS (";
   ret += SelectToString(cte.select());
-  ret += ") ";
+  // Avert infinite recursion.
+  ret += " LIMIT 1000)";
   return ret;
 }
 
