d1a8ebd0bce6575eb22eff6f6af2e2cfed8a5457
Eric Sum <esum@google.com>
2024-03-25 20:29:47 +0000

    Add NearbyConnectionsManager::ClearIncomingPayloadWithId().
    The data migration project (linked in the bug's description) may
    transfer thousands of bytes and file payloads in one session. On
    top of that there may be bytes payloads sent in parallel with other
    file payloads.
    
    Currently, the only method available is ClearIncomingPayloads()
    which erases every single payload that's been received thus far.
    The data migration project requires a more fine-grained method to
    erase specific payloads as it processes them (or we'll have consumed
    a lot of memory and file descriptors as the session goes on). That's
    the method being added in this CL.
    
    Bug: b:324298544
    Test: NearbyConnectionsManagerImplTest, ClearIncomingPayloadWithId
    Change-Id: I6a279950d333a3da43d084d0b15bc3a6a0ed5ccb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5389530
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Commit-Queue: Eric Sum <esum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1277901}

--- chrome/browser/nearby_sharing/nearby_connections_manager_impl.h
+++ chrome/browser/nearby_sharing/nearby_connections_manager_impl.h
@@ -72,6 +72,7 @@ class NearbyConnectionsManagerImpl
   Payload* GetIncomingPayload(int64_t payload_id) override;
   void Cancel(int64_t payload_id) override;
   void ClearIncomingPayloads() override;
+  void ClearIncomingPayloadWithId(int64_t payload_id) override;
   std::optional<std::string> GetAuthenticationToken(
       const std::string& endpoint_id) override;
   std::optional<std::vector<uint8_t>> GetRawAuthenticationToken(
