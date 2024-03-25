1c6bd3eea93bd15553e58a0d76a14065f6789604
Chuwei He <chuweih@google.com>
2024-03-25 06:08:04 +0000

    support-tool: Fix error when collecting address information
    The code in ShillDataCollector and ShillLogSource cannot correctly
    collect address data because of mistaken key value.
    This patch solved the issue by correcting the key value collected in the
    code.
    
    BUG: b/329524359
    TEST: build
    Change-Id: I1687daaa2cb2ce08c421f133f35673f4b9167acc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5367785
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Commit-Queue: Chuwei He <chuweih@google.com>
    Reviewed-by: Irem Uguz <iremuguz@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277475}

--- chromeos/ash/components/dbus/shill/fake_shill_device_client.cc
+++ chromeos/ash/components/dbus/shill/fake_shill_device_client.cc
@@ -350,7 +350,8 @@ ShillDeviceClient::TestInterface* FakeShillDeviceClient::GetTestInterface() {
 
 void FakeShillDeviceClient::AddDevice(const std::string& device_path,
                                       const std::string& type,
-                                      const std::string& name) {
+                                      const std::string& name,
+                                      const std::string& address) {
   ShillManagerClient::Get()->GetTestInterface()->AddDevice(device_path);
 
   base::Value::Dict* properties = stub_devices_.EnsureDict(device_path);
@@ -359,6 +360,9 @@ void FakeShillDeviceClient::AddDevice(const std::string& device_path,
   properties->Set(shill::kDBusObjectProperty, device_path);
   properties->Set(shill::kDBusServiceProperty,
                   modemmanager::kModemManager1ServiceName);
+  if (!address.empty()) {
+    properties->Set(shill::kAddressProperty, address);
+  }
   if (type == shill::kTypeCellular) {
     properties->Set(shill::kCellularPolicyAllowRoamingProperty, false);
   }
