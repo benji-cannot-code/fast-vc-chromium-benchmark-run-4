3cc958d21f130469172551dc18fc885fb5a23d19
Ian Kilpatrick <ikilpatrick@chromium.org>
2024-03-25 23:00:19 +0000

    [layout] Change how we resolve the flex-basis.
    Previously we'd use IsUsedFlexBasisDefinite which called:
    InlineLengthUnresolvable/BlockLengthUnresolvable to determine if we
    could resolve the flex-basis.
    
    Instead try and resolve the flex-basis directly and check if it
    resolved using kIndefiniteSize.
    
    This changes how we calculate the is_used_flex_basis_indefinite
    bool. Instead of using IsUsedFlexBasisDefinite, check if we needed
    to use any "content" based functions while resolving.
    
    Bug: 328572265
    Change-Id: Icf3930174bd218ecfd462399fa6e6f19b2f7e1ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5386493
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278009}

--- third_party/blink/renderer/core/layout/length_utils.cc
+++ third_party/blink/renderer/core/layout/length_utils.cc
@@ -171,6 +171,7 @@ LayoutUnit ResolveInlineLengthInternal(
       return min_max_sizes.ShrinkToFit(fill_available);
     }
     case Length::kAuto:
+    case Length::kContent:
     case Length::kNone:
       return unresolvable_length_result;
     case Length::kDeviceWidth:
@@ -260,6 +261,7 @@ LayoutUnit ResolveBlockLengthInternal(
       return intrinsic_size;
     }
     case Length::kAuto:
+    case Length::kContent:
     case Length::kNone:
       return unresolvable_block_size_func();
     case Length::kDeviceWidth:
