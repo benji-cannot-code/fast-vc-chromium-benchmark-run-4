56dbbd24666af25b3fc5f8bf4d0b67827114ff8c
Karol Sygiet <sygiet@google.com>
2024-03-25 13:17:40 +0000

    [UPM] Removes kUnifiedPasswordManagerSyncUsingAndroidBackendOnly feature
    This CL removes `kUnifiedPasswordManagerSyncUsingAndroidBackendOnly`
    feature. All android backends will return model type delegate from
    `PasswordStoreBuiltInBackend`. This is ok since now
    `PasswordStoreBuiltInBackend` checks for the requirements of disabling
    sync machinery in chrome and returns the correct model type delegate
    accordingly.
    
    Bug: 321220529
    Change-Id: I0b279069d035be71405b6304474a2625cf4f3fff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5383513
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277591}

--- components/password_manager/core/common/password_manager_features.h
+++ components/password_manager/core/common/password_manager_features.h
@@ -40,7 +40,6 @@ BASE_DECLARE_FEATURE(kNoPasswordSuggestionFiltering);
 BASE_DECLARE_FEATURE(kPasswordGenerationBottomSheet);
 BASE_DECLARE_FEATURE(kPasswordSuggestionBottomSheetV2);
 BASE_DECLARE_FEATURE(kUnifiedPasswordManagerLocalPasswordsMigrationWarning);
-BASE_DECLARE_FEATURE(kUnifiedPasswordManagerSyncUsingAndroidBackendOnly);
 #endif
 
 // All features parameters are in alphabetical order.
