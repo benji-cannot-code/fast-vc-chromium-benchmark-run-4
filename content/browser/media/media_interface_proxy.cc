779ec647ea16298b5f0ff596385537ef78efc756
Andres Calderon Jaramillo <andrescj@chromium.org>
2024-03-25 22:22:08 +0000

    media: Introduce InterfaceFactory.CreateStableVideoDecoder()
    This CL adds a new method to the InterfaceFactory Mojo interface,
    CreateStableVideoDecoder(), to be used with the GTFO OOP-VD path
    (go/oopvd-gtfo-dd).
    
    The intended usage is for a renderer process to ask the browser process
    to broker a media.stable.mojom.StableVideoDecoder connection on its
    behalf. Ultimately, the receiving endpoint will end up living in a
    utility video decoder process.
    
    Bug: b:327268445
    Test: None
    Change-Id: I70b7ab768de192d1f7cfc03d257b705aabfcf333
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5330169
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Reviewed-by: Pilar Molina Lopez <pmolinalopez@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277992}

--- content/browser/media/media_interface_proxy.cc
+++ content/browser/media/media_interface_proxy.cc
@@ -79,6 +79,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(ALLOW_OOP_VIDEO_DECODER)
 #include "content/public/browser/stable_video_decoder_factory.h"
 #include "media/base/media_switches.h"
+#include "mojo/public/cpp/bindings/message.h"
 #endif  // BUILDFLAG(ALLOW_OOP_VIDEO_DECODER)
 
 namespace content {
@@ -320,6 +321,28 @@ void MediaInterfaceProxy::CreateVideoDecoder(
   factory->CreateVideoDecoder(std::move(receiver),
                               std::move(oop_video_decoder));
 }
+
+#if BUILDFLAG(ALLOW_OOP_VIDEO_DECODER)
+void MediaInterfaceProxy::CreateStableVideoDecoder(
+    mojo::PendingReceiver<media::stable::mojom::StableVideoDecoder>
+        video_decoder) {
+  DCHECK(thread_checker_.CalledOnValidThread());
+  switch (media::GetOutOfProcessVideoDecodingMode()) {
+    case media::OOPVDMode::kEnabledWithGpuProcessAsProxy:
+    case media::OOPVDMode::kDisabled:
+      // Well-behaved clients shouldn't call CreateStableVideoDecoder() in this
+      // OOP-VD mode and MediaInterfaceProxy::CreateStableVideoDecoder() should
+      // always be called during a message dispatch.
+      CHECK(mojo::IsInMessageDispatch());
+      mojo::ReportBadMessage("CreateStableVideoDecoder() called unexpectedly");
+      return;
+    case media::OOPVDMode::kEnabledWithoutGpuProcessAsProxy:
+      render_frame_host().GetProcess()->CreateStableVideoDecoder(
+          std::move(video_decoder));
+      break;
+  }
+}
+#endif  // BUILDFLAG(ALLOW_OOP_VIDEO_DECODER)
 
 void MediaInterfaceProxy::CreateAudioEncoder(
     mojo::PendingReceiver<media::mojom::AudioEncoder> receiver) {
