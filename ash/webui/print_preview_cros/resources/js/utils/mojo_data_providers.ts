a41be3280788be89862006fc32b8332c4cfa2512
Ashley Prasad <ashleydp@google.com>
2024-03-25 20:09:40 +0000

    cros-preview: Fake fetch local destinations
    - Setup destination provider in destination manager.
    - Add getter and setter to shared mojo provider file.
    - Test getLocalDestinations called.
    
    Bug: b:323421684
    Test: browser_test --gtest_filter=*PrintPreviewCros*
    Change-Id: Ib1f6784fc4b21dca5e988acaab68d6c5b8459ad1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5356398
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1277881}

--- ash/webui/print_preview_cros/resources/js/utils/mojo_data_providers.ts
+++ ash/webui/print_preview_cros/resources/js/utils/mojo_data_providers.ts
@@ -5,9 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {assert} from 'chrome://resources/js/assert.js';
 
+import {FakeDestinationProvider} from '../fakes/fake_destination_provider.js';
 import {FakePrintPreviewPageHandler} from '../fakes/fake_print_preview_page_handler.js';
 
-import {type PrintPreviewPageHandler} from './print_preview_cros_app_types.js';
+import {DestinationProvider, type PrintPreviewPageHandler} from './print_preview_cros_app_types.js';
 
 /**
  * @fileoverview
@@ -16,6 +17,7 @@ import {type PrintPreviewPageHandler} from './print_preview_cros_app_types.js';
  */
 
 let printPreviewPageHandler: PrintPreviewPageHandler|null = null;
+let destinationProvider: DestinationProvider|null = null;
 
 // Returns shared instance of PrintPreviewPageHandler.
 export function getPrintPreviewPageHandler(): PrintPreviewPageHandler {
@@ -31,4 +33,20 @@ export function getPrintPreviewPageHandler(): PrintPreviewPageHandler {
 export function setPrintPreviewPageHandlerForTesting(
     handler: PrintPreviewPageHandler): void {
   printPreviewPageHandler = handler;
+}
+
+// Returns shared instance of DestinationProvider.
+export function getDestinationProvider(): DestinationProvider {
+  if (destinationProvider == null) {
+    destinationProvider = new FakeDestinationProvider();
+  }
+
+  assert(destinationProvider);
+  return destinationProvider;
+}
+
+// Override shared instance of DestinationProvider for testing.
+export function setDestinationProviderForTesting(provider: DestinationProvider):
+    void {
+  destinationProvider = provider;
 }
