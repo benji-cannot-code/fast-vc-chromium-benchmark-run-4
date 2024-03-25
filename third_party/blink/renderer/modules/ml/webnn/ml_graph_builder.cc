8273b3eaca885aa74029432a4dadc4418ea4f100
Austin Sullivan <asully@chromium.org>
2024-03-25 17:30:33 +0000

    webnn: Validate activations passed as options to MLGraphBuilder methods
    See https://www.w3.org/TR/webnn/#mlgraphbuilder-validate-activation
    
    Follow-up to https://crrev.com/c/5371346
    
    Bug: 325612086
    Change-Id: I245b555d4c7e0605bd8d754f8e0848a8718754cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5388623
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277746}

--- third_party/blink/renderer/modules/ml/webnn/ml_graph_builder.cc
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_builder.cc
@@ -813,6 +813,11 @@ MLOperand* MLGraphBuilder::batchNormalization(
   }
   THROW_AND_RETURN_TYPE_IF_ERROR(ValidateInputs(inputs), nullptr);
 
+  if (options->hasActivation()) {
+    THROW_AND_RETURN_TYPE_IF_ERROR(ValidateActivation(options->activation()),
+                                   nullptr);
+  }
+
   const auto validated_output = webnn::ValidateBatchNormalizationAndInferOutput(
       ConvertToComponentOperand(input), ConvertToComponentOperand(mean),
       ConvertToComponentOperand(variance),
@@ -910,6 +915,11 @@ MLOperand* MLGraphBuilder::conv2d(const MLOperand* input,
   }
   THROW_AND_RETURN_TYPE_IF_ERROR(ValidateInputs(inputs), nullptr);
 
+  if (options->hasActivation()) {
+    THROW_AND_RETURN_TYPE_IF_ERROR(ValidateActivation(options->activation()),
+                                   nullptr);
+  }
+
   auto conv2d_attributes = ConvertToConv2dAttributes(options);
   if (!conv2d_attributes.has_value()) {
     exception_state.ThrowDOMException(DOMExceptionCode::kDataError,
@@ -953,6 +963,11 @@ MLOperand* MLGraphBuilder::convTranspose2d(
     inputs.push_back(options->bias());
   }
   THROW_AND_RETURN_TYPE_IF_ERROR(ValidateInputs(inputs), nullptr);
+
+  if (options->hasActivation()) {
+    THROW_AND_RETURN_TYPE_IF_ERROR(ValidateActivation(options->activation()),
+                                   nullptr);
+  }
 
   auto convTranspose2d_attributes = ConvertToConvTranspose2dAttributes(options);
   if (!convTranspose2d_attributes.has_value()) {
@@ -1239,6 +1254,11 @@ HeapVector<Member<const MLOperand>> MLGraphBuilder::gru(
   THROW_AND_RETURN_TYPE_IF_ERROR(ValidateInputs(inputs),
                                  HeapVector<Member<const MLOperand>>());
 
+  if (options->hasActivations()) {
+    THROW_AND_RETURN_TYPE_IF_ERROR(ValidateActivations(options->activations()),
+                                   HeapVector<Member<const MLOperand>>());
+  }
+
   auto validated_outputs = webnn::ValidateGruAndInferOutput(
       ConvertToComponentOperand(input), ConvertToComponentOperand(weight),
       ConvertToComponentOperand(recurrent_weight), steps, hidden_size,
@@ -1482,6 +1502,11 @@ HeapVector<Member<const MLOperand>> MLGraphBuilder::lstm(
   }
   THROW_AND_RETURN_TYPE_IF_ERROR(ValidateInputs(inputs),
                                  HeapVector<Member<const MLOperand>>());
+
+  if (options->hasActivations()) {
+    THROW_AND_RETURN_TYPE_IF_ERROR(ValidateActivations(options->activations()),
+                                   HeapVector<Member<const MLOperand>>());
+  }
 
   // If the activations are not specified, create a default activation sequence
   // [sigmoid, tanh, tanh] as defined in the spec.
@@ -2149,9 +2174,7 @@ void MLGraphBuilder::SetBackendForTesting(
   g_backend_for_testing = backend_for_testing;
 }
 
-// As specified in https://www.w3.org/TR/webnn/#mloperand-validate-mloperand.
-// Note that checking the dimensions of `input` here would be redundant, since
-// otherwise the `MLOperand` would not have been created.
+// As specified in https://www.w3.org/TR/webnn/#mlgraphbuilder-validate-operand.
 base::expected<void, String> MLGraphBuilder::ValidateInput(
     const MLOperand* input) {
   CHECK(input);
@@ -2165,6 +2188,26 @@ base::expected<void, String> MLGraphBuilder::ValidateInputs(
     const HeapVector<Member<const MLOperand>>& inputs) {
   for (const MLOperand* input_to_validate : inputs) {
     RETURN_IF_ERROR(ValidateInput(input_to_validate));
+  }
+  return base::ok();
+}
+
+// As specified in
+// https://www.w3.org/TR/webnn/#mlgraphbuilder-validate-activation.
+base::expected<void, String> MLGraphBuilder::ValidateActivation(
+    const MLActivation* activation) {
+  CHECK(activation);
+  if (activation->Operator()->Builder() != this) {
+    return base::unexpected(
+        "Invalid activation: Created from another builder.");
+  }
+  return base::ok();
+}
+
+base::expected<void, String> MLGraphBuilder::ValidateActivations(
+    const HeapVector<Member<MLActivation>>& activations) {
+  for (const MLActivation* activation_to_validate : activations) {
+    RETURN_IF_ERROR(ValidateActivation(activation_to_validate));
   }
   return base::ok();
 }
