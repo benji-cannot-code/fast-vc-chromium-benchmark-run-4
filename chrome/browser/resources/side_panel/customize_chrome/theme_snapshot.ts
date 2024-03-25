36d0a941622dd5e6a748619ffa52dab7eb24e6d1
dpapad <dpapad@chromium.org>
2024-03-25 18:40:14 +0000

    WebUI: Replace paper-ripple with cr-ripple in various UIs.
    This is in preparation of removing paper-ripple from Desktop builds.
    
    Bug: 330377196
    Change-Id: I48fde72cb3a9cd633eb5f8bab6457625c73a4989
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5367779
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277800}

--- chrome/browser/resources/side_panel/customize_chrome/theme_snapshot.ts
+++ chrome/browser/resources/side_panel/customize_chrome/theme_snapshot.ts
@@ -4,8 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/cr_auto_img/cr_auto_img.js';
+import 'chrome://resources/cr_elements/cr_ripple/cr_ripple.js';
 import 'chrome://resources/polymer/v3_0/iron-pages/iron-pages.js';
-import 'chrome://resources/polymer/v3_0/paper-ripple/paper-ripple.js';
 
 import {assert} from 'chrome://resources/js/assert.js';
 import {skColorToRgba} from 'chrome://resources/js/color_utils.js';
