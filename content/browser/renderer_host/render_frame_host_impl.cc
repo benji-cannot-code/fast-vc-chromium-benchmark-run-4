d078c6d2587019fc3f55291a03650e2db8477ee9
Aman Verma <amanvr@google.com>
2024-03-26 15:51:49 +0000

    Move TransformRootPointToViewCoordSpace method from RenderWidgetHostView to RenderWidgetHostViewInput.
    To facilitate this move, the method RenderFrameHostImpl::GetView() has
    been updated to return RenderWidgetHostViewBase* from
    RenderWidgetHostView*.
    
    This CL is required to break the dependency of
    RenderWidgetHostInputEventRouter on RWHVB, since we are moving RWHIER to
    common code for InputVizard.
    
    Doc Link:
    https://docs.google.com/document/d/1dgbvnRChsvfMpXceQ24UbTdFwJpmk5VIl7WXHwExAC4/edit?tab=t.0#heading=h.7wb78gd5w8i0
    
    Bug: b:328246876
    Change-Id: I2b43d84f5d478f4f8fb42215bb824cf9b36ad754
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372049
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Aman Verma <amanvr@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278343}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -2729,8 +2729,10 @@ void RenderFrameHostImpl::ExecuteJavaScriptForTests(
 }
 
 void RenderFrameHostImpl::ExecutePluginActionAtLocalLocation(
-    const gfx::Point& local_location,
+    const gfx::Point& location,
     blink::mojom::PluginActionType plugin_action) {
+  gfx::Point local_location = gfx::ToFlooredPoint(
+      GetView()->TransformRootPointToViewCoordSpace(gfx::PointF(location)));
   GetAssociatedLocalFrame()->PluginActionAt(local_location, plugin_action);
 }
 
@@ -5116,7 +5118,7 @@ RenderWidgetHostImpl* RenderFrameHostImpl::GetRenderWidgetHost() {
   return nullptr;
 }
 
-RenderWidgetHostView* RenderFrameHostImpl::GetView() {
+RenderWidgetHostViewBase* RenderFrameHostImpl::GetView() {
   return GetRenderWidgetHost()->GetView();
 }
 
