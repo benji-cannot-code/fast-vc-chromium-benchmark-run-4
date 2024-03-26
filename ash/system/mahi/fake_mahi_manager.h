6e97d67703b642b2dd6e40324b905f0792132801
andrewxu <andrewxu@chromium.org>
2024-03-26 23:49:03 +0000

    [Mahi] refactoring: Create a mock mahi manager class for testing
    This CL creates a `MockMahiManager` for testing only. With this CL,
    `FakeMahiManager` is used in the product code when the corresponding
    flag is enabled.
    
    This CL should not bring any UI difference.
    
    Bug: b/319731862
    Change-Id: I87cc8d130c5908f48425e5e632bc1727d4a9c9b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5392114
    Reviewed-by: Andre Le <leandre@chromium.org>
    Reviewed-by: Kevin Radtke <kradtke@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278690}

--- ash/system/mahi/fake_mahi_manager.h
+++ ash/system/mahi/fake_mahi_manager.h
@@ -6,19 +6,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_SYSTEM_MAHI_FAKE_MAHI_MANAGER_H_
 #define ASH_SYSTEM_MAHI_FAKE_MAHI_MANAGER_H_
 
+#include <optional>
 #include <string>
 
 #include "ash/ash_export.h"
 #include "chromeos/components/mahi/public/cpp/mahi_manager.h"
+#include "chromeos/crosapi/mojom/mahi.mojom.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/views/widget/unique_widget_ptr.h"
 
 namespace ash {
 
-// A fake implementation of `MahiManager`.
+// A fake implementation of `MahiManager` used for development only. Returns
+// predetermined contents asyncly. Created only when
+// `chromeos::switches::kUseFakeMahiManager` is enabled.
 class ASH_EXPORT FakeMahiManager : public chromeos::MahiManager {
  public:
-  explicit FakeMahiManager(bool enable_callback_delays_for_animations = false);
+  FakeMahiManager();
   FakeMahiManager(const FakeMahiManager&) = delete;
   FakeMahiManager& operator=(const FakeMahiManager&) = delete;
   ~FakeMahiManager() override;
@@ -39,45 +43,30 @@ class ASH_EXPORT FakeMahiManager : public chromeos::MahiManager {
       crosapi::mojom::MahiPageInfoPtr info) override {}
   void OnContextMenuClicked(
       crosapi::mojom::MahiContextMenuRequestPtr context_menu_request) override;
-  void OpenFeedbackDialog() override;
+  void OpenFeedbackDialog() override {}
 
-  void set_content_title(const std::u16string& content_title) {
-    content_title_ = content_title;
+  void set_answer_text(const std::u16string& answer_text) {
+    answer_text_ = answer_text;
   }
 
   void set_content_icon(const gfx::ImageSkia& content_icon) {
     content_icon_ = content_icon;
   }
 
+  void set_content_title(const std::u16string& content_title) {
+    content_title_ = content_title;
+  }
+
   void set_summary_text(const std::u16string& summary_text) {
     summary_text_ = summary_text;
   }
 
-  void set_answer_text(const std::u16string& answer_text) {
-    answer_text_ = answer_text;
-  }
-
-  int open_feedback_dialog_called_count() {
-    return open_feedback_dialog_called_count_;
-  }
-
-  void set_enable_fake_delays_for_animations(
-      bool enable_fake_delays_for_animations) {
-    enable_fake_delays_for_animations_ = enable_fake_delays_for_animations;
-  }
-
-  const std::u16string& asked_question() { return asked_question_; }
-
  private:
   std::optional<std::u16string> answer_text_;
-  std::u16string asked_question_;
-  std::u16string content_title_;
+  std::optional<std::u16string> asked_question_;
   gfx::ImageSkia content_icon_;
-  std::u16string summary_text_;
-
-  bool enable_fake_delays_for_animations_ = false;
-
-  int open_feedback_dialog_called_count_ = 0;
+  std::optional<std::u16string> content_title_;
+  std::optional<std::u16string> summary_text_;
 
   // The widget contains the Mahi main panel.
   views::UniqueWidgetPtr mahi_panel_widget_;
