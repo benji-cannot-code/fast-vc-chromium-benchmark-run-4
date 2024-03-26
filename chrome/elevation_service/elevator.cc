28d613b100c4b815968625a477130829ea393941
Will Harris <wfh@chromium.org>
2024-03-26 03:05:19 +0000

    Add calls to additional data processing for elevation_service.
    This adds calls to PreProcessData and PostProcessData when
    performing Encrypt and Decrypt COM IPCs.
    
    This also adds the unit tests to the elevation_service_unittests.
    
    BUG=331237479
    
    Change-Id: Ic7d0b720bfc67e0feda207ad177bf564ad488539
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5394770
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278094}

--- chrome/elevation_service/elevator.cc
+++ chrome/elevation_service/elevator.cc
@@ -22,9 +22,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/version_info/channel.h"
 #include "base/win/scoped_localalloc.h"
 #include "base/win/win_util.h"
+#include "build/branding_buildflags.h"
 #include "chrome/elevation_service/caller_validation.h"
 #include "chrome/elevation_service/elevated_recovery_impl.h"
 #include "chrome/install_static/install_util.h"
+
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#include "chrome/elevation_service/internal/elevation_service_internal.h"
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 
 namespace elevation_service {
 
@@ -76,6 +81,15 @@ HRESULT Elevator::EncryptData(ProtectionLevel protection_level,
   if (!length)
     return E_INVALIDARG;
 
+  std::string plaintext_str(reinterpret_cast<char*>(plaintext), length);
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  auto pre_process_result = PreProcessData(plaintext_str);
+  if (!pre_process_result.has_value()) {
+    return pre_process_result.error();
+  }
+  plaintext_str.swap(*pre_process_result);
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
+
   HRESULT hr = ::CoImpersonateClient();
   if (FAILED(hr))
     return hr;
@@ -99,9 +113,7 @@ HRESULT Elevator::EncryptData(ProtectionLevel protection_level,
 
     std::string data_to_encrypt;
     AppendStringWithLength(data, data_to_encrypt);
-    AppendStringWithLength(
-        std::string(reinterpret_cast<char*>(plaintext), length),
-        data_to_encrypt);
+    AppendStringWithLength(plaintext_str, data_to_encrypt);
 
     DATA_BLOB input = {};
     input.cbData = base::checked_cast<DWORD>(data_to_encrypt.length());
@@ -204,6 +216,13 @@ HRESULT Elevator::DecryptData(const BSTR ciphertext,
     }
     plaintext_str = PopFromStringFront(mutable_plaintext);
   }
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  auto post_process_result = PostProcessData(plaintext_str);
+  if (!post_process_result.has_value()) {
+    return post_process_result.error();
+  }
+  plaintext_str.swap(*post_process_result);
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 
   *plaintext =
       ::SysAllocStringByteLen(plaintext_str.c_str(), plaintext_str.length());
