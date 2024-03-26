3020d241cc3747a6f067803d2f1c5e4e0f9f2419
Noam Rosenthal <nrosenthal@chromium.org>
2024-03-26 19:54:52 +0000

    View transition: make CSSViewTransitionRule.navigation readonly
    See CSSWG resolution:
    https://github.com/w3c/csswg-drafts/issues/10011#issuecomment-2009951924
    
    Bug: 330531066
    Change-Id: Ica350ab517a96a9a9e25af78ef28a00ded30d2c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5395234
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278548}

--- third_party/blink/renderer/core/css/css_view_transition_rule.h
+++ third_party/blink/renderer/core/css/css_view_transition_rule.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class ExecutionContext;
 class StyleRuleViewTransition;
 
 class CSSViewTransitionRule final : public CSSRule {
@@ -28,8 +27,6 @@ class CSSViewTransitionRule final : public CSSRule {
   void Trace(Visitor*) const override;
 
   String navigation() const;
-
-  void setNavigation(const ExecutionContext*, const String&);
 
  private:
   CSSRule::Type GetType() const override { return kViewTransitionRule; }
