29a0ac3543cfebdb17482e759e11e05f62adc9e3
Joemer Ramos <joemerramos@chromium.org>
2024-03-26 18:08:38 +0000

    [iOS] Split Out interstitial.*.decision Metrics By Threat Source
    This CL adds code to account for the extra suffix when recording metrics related to an interstitial decision. Since logic is the same in
    `content/`, we decided to move logic to `core/common`. Doing this
    introduced a deps cycle (which can be seen in patch 5). To resolve this,
    we decided to remove dependencies that weren't needed. We ask the
    reviewers for callsites in `content` to check that there
    aren't any issues with the refactor especially with removing the
    static keyword from the method declaration.
    
    Fixed: 41481528
    Change-Id: Ib778682492eff743ebf7ba62ccd0a9c23ed271cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372287
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278467}

--- components/safe_browsing/core/common/utils.h
+++ components/safe_browsing/core/common/utils.h
@@ -26,6 +26,10 @@ namespace network {
 struct ResourceRequest;
 }  // namespace network
 
+namespace security_interstitials {
+struct UnsafeResource;
+}
+
 class PrefService;
 
 namespace safe_browsing {
@@ -79,6 +83,10 @@ void RecordHttpResponseOrErrorCode(const char* metric_name,
 // returns whether an error is considered retriable and doesn't need to
 // increment backoff.
 bool ErrorIsRetriable(int net_error, int http_error);
+
+// We populate a parallel set of metrics to differentiate some threat sources.
+std::string GetExtraMetricsSuffix(
+    security_interstitials::UnsafeResource unsafe_resource);
 
 }  // namespace safe_browsing
 
