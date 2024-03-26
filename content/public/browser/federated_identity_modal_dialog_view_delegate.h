8cf8a635745d7b970ff7819bf0c09386581333e7
Jose Dapena Paz <jdapena@igalia.com>
2024-03-26 09:31:53 +0000

    IWYU: missing include for usage of std::optional in federated_identity_modal_dialog_view_delegate.h
    Bug: 41455655
    Change-Id: I66d7175a0dee9dee46c54560edea51fa9fc5a41e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5392720
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1278203}

--- content/public/browser/federated_identity_modal_dialog_view_delegate.h
+++ content/public/browser/federated_identity_modal_dialog_view_delegate.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_PUBLIC_BROWSER_FEDERATED_IDENTITY_MODAL_DIALOG_VIEW_DELEGATE_H_
 #define CONTENT_PUBLIC_BROWSER_FEDERATED_IDENTITY_MODAL_DIALOG_VIEW_DELEGATE_H_
 
+#include <optional>
 #include <string>
 
 #include "content/common/content_export.h"
