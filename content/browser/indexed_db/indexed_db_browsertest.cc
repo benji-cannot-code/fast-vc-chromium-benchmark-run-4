8302ee6f4182fcf7dc257aa0ef6cb304f1cb7c73
Evan Stade <estade@chromium.org>
2024-03-26 21:06:00 +0000

    Reapply "IndexedDB: simplify IndexedDBControlWrapper"
    This reverts commit 983fc08f8c01c228cc2260b8e98352cbef1113c9.
    
    The difference to the original version of this patch is that
    `IndexedDBControlWrapper` no longer observes `IndexedDBContextImpl`
    via mojo.
    
    Bug: 40947125,717812
    Change-Id: I40a01b4579830eca4ff89c44bceade2e4244ca68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5393295
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278591}

--- content/browser/indexed_db/indexed_db_browsertest.cc
+++ content/browser/indexed_db/indexed_db_browsertest.cc
@@ -157,10 +157,10 @@ class IndexedDBBrowserTest : public ContentBrowserTest {
   storage::mojom::IndexedDBControl& GetControl(Shell* browser = nullptr) {
     if (!browser)
       browser = shell();
-    StoragePartition* partition = browser->web_contents()
-                                      ->GetBrowserContext()
-                                      ->GetDefaultStoragePartition();
-    return partition->GetIndexedDBControl();
+    return browser->web_contents()
+        ->GetBrowserContext()
+        ->GetDefaultStoragePartition()
+        ->GetIndexedDBControl();
   }
 
   mojo::Remote<storage::mojom::IndexedDBControlTest> GetControlTest(
@@ -176,11 +176,11 @@ class IndexedDBBrowserTest : public ContentBrowserTest {
     if (!browser) {
       browser = shell();
     }
-    StoragePartition* partition = browser->web_contents()
-                                      ->GetBrowserContext()
-                                      ->GetDefaultStoragePartition();
-    auto& control = partition->GetIndexedDBControl();
-    control.BindTestInterface(std::move(receiver));
+    browser->web_contents()
+        ->GetBrowserContext()
+        ->GetDefaultStoragePartition()
+        ->GetIndexedDBControl()
+        .BindTestInterface(std::move(receiver));
   }
 
   void SetQuota(int per_host_quota_kilobytes) {
