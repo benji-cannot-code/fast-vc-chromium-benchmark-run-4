f3fac48dea4c6e9d30e9d7abe8a54d20f6244525
Daniel Rubery <drubery@chromium.org>
2024-03-26 16:18:36 +0000

    Begin removing BLOCKED_UNSUPPORTED_FILETYPE
    This CL removes usages of the download danger type
    BLOCKED_UNSUPPORTED_FILETYPE that are not in switch
    statements with no default.
    
    The remaining usages must be deleted along with the
    enum itself, and will follow in a separate CL.
    
    Bug: 330373911
    Change-Id: I8c1bb018f2b95a3d9aa0c3958818483f79719b5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5392703
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278361}

--- chrome/browser/download/download_item_model_unittest.cc
+++ chrome/browser/download/download_item_model_unittest.cc
@@ -899,8 +899,6 @@ TEST_F(DownloadItemModelTest, ShouldShowDropdown) {
       download::DOWNLOAD_DANGER_TYPE_BLOCKED_TOO_LARGE;
   download::DownloadDangerType blocked_sensitive =
       download::DOWNLOAD_DANGER_TYPE_SENSITIVE_CONTENT_BLOCK;
-  download::DownloadDangerType blocked_filetype =
-      download::DOWNLOAD_DANGER_TYPE_BLOCKED_UNSUPPORTED_FILETYPE;
 
   const struct TestCase {
     DownloadItem::DownloadState state;        // Expectation for GetState()
@@ -918,7 +916,6 @@ TEST_F(DownloadItemModelTest, ShouldShowDropdown) {
       {DownloadItem::COMPLETE,  blocked_encrypted, true,  false},
       {DownloadItem::COMPLETE,  blocked_too_large, true,  false},
       {DownloadItem::COMPLETE,  blocked_sensitive, true,  false},
-      {DownloadItem::COMPLETE,  blocked_filetype,  true,  false},
   };
 
   SetupDownloadItemDefaults();
