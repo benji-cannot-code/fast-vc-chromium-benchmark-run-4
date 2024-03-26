5f09a3396b5411fbc2b5339c4cff5989cac91cad
pemilia <pemilia@google.com>
2024-03-26 12:53:16 +0000

    [gbb-cct] Add support for button list provided by intent extra
    Bug: b/329454646
    Change-Id: I1825158ca358afd44f8fae6aec90b47a5cd762e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5392921
    Commit-Queue: Emilia Palaghita <pemilia@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Edmund Wright <edmundw@google.com>
    Reviewed-by: Edmund Wright <edmundw@google.com>
    Cr-Commit-Position: refs/heads/main@{#1278262}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabRootUiCoordinator.java
@@ -466,7 +466,10 @@ public class BaseCustomTabRootUiCoordinator extends RootUiCoordinator {
             return null;
         }
 
-        return new GoogleBottomBarCoordinator(mActivity);
+        return new GoogleBottomBarCoordinator(
+                mActivity,
+                CustomTabsConnection.getInstance()
+                        .getGoogleBottomBarIntentParams(intentDataProvider));
     }
 
     public @Nullable GoogleBottomBarCoordinator getGoogleBottomBarCoordinator() {
