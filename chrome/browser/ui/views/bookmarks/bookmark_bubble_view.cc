fba0d54114ba9c7c3545d9b41e413d840e1c400d
Yue Zhang <yuezhanggg@chromium.org>
2024-03-26 17:15:14 +0000

    [Shopping UKM] Add UKM for ShoppingAction for price tracking [3]
    This CL adds UKM event to track when user tracks price of the current
    product via bookmarking the current page.
    
    Bug: b/329869761
    Change-Id: Icb531e2a8147d44b7f44e498fb95719ff9b32c07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5388508
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278405}

--- chrome/browser/ui/views/bookmarks/bookmark_bubble_view.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_bubble_view.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/bookmarks/browser/bookmark_utils.h"
 #include "components/bookmarks/common/bookmark_metrics.h"
 #include "components/commerce/core/commerce_feature_list.h"
+#include "components/commerce/core/metrics/metrics_utils.h"
 #include "components/commerce/core/price_tracking_utils.h"
 #include "components/commerce/core/shopping_service.h"
 #include "components/feature_engagement/public/feature_constants.h"
@@ -492,6 +493,16 @@ void BookmarkBubbleView::ShowBubble(
                                                 product_info.value()),
             views::BubbleDialogModelHost::FieldType::kControl),
         kPriceTrackingBookmarkViewElementId);
+
+    // When a user clicks to open bookmark bubble for a product page that is
+    // neither price tracked nor bookmarked, user will track the price of the
+    // product, and we'll record the UKM for this event.
+    // TODO(b/331277578): Move the track-by-default logic here.
+    if (!is_price_tracked && !already_bookmarked) {
+      commerce::metrics::RecordShoppingActionUKM(
+          web_contents->GetPrimaryMainFrame()->GetPageUkmSourceId(),
+          commerce::metrics::ShoppingAction::kPriceTracked);
+    }
   }
 
   // views:: land below, there's no agnostic reference to arrow / anchors /
