a6e0206b6f70402f091a722a02613a7d03497454
Salvador Guerrero <salg@google.com>
2024-03-26 21:51:33 +0000

    Update page info bottom sheet UI to latest spec
    This CL updates the contents of the page info sharing bottom sheet to
    follow the latest UI spec, this includes adding feedback buttons and
    removing the refresh button.
    
    Layout was refactored to use ConstraintLayout instead of nested
    LinearLayouts.
    
    Strings are re-used from other views as our strings are not yet final.
    
    Bug: b/329099913
    Change-Id: I0b103727fcdccc3b3f36148528b650634eda0008
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5375310
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1278623}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/page_info_sheet/PageInfoSharingControllerImpl.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/page_info_sheet/PageInfoSharingControllerImpl.java
@@ -96,6 +96,12 @@ public class PageInfoSharingControllerImpl implements PageInfoSharingController 
         }
 
         @Override
+        public void onPositiveFeedback() {}
+
+        @Override
+        public void onNegativeFeedback() {}
+
+        @Override
         public ObservableSupplier<PageInfoContents> getContentSupplier() {
             return mPageInfoSupplier;
         }
