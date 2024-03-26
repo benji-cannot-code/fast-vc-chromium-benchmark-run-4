67f2d42466da83b921805ec9d7c008235b901cda
dpapad <dpapad@chromium.org>
2024-03-26 23:00:32 +0000

    WebUI: Migrate cr_components/managed_dialog/ to Lit.
    As part of migration also ported I18nMixin to Lit.
    
    Bug: 40943652
    Change-Id: I3b714cfe6c891d2d191460fc305d772f67cdffd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5393542
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278663}

---
+++ ui/webui/resources/cr_components/managed_dialog/managed_dialog.html.ts
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {ManagedDialogElement} from './managed_dialog.js';
+
+export function getHtml(this: ManagedDialogElement) {
+  return html`
+<cr-dialog id="dialog" close-text="${this.i18n('close')}" show-on-attach>
+  <div slot="title">
+    <iron-icon icon="cr:domain" role="img"
+        aria-label="${this.i18n('controlledSettingPolicy')}">
+    </iron-icon>
+    ${this.title}
+  </div>
+  <div slot="body">${this.body}</div>
+  <div slot="button-container">
+    <cr-button class="action-button" @click="${this.onOkClick_}">
+      ${this.i18n('ok')}
+    </cr-button>
+  </div>
+</cr-dialog>`;
+}
