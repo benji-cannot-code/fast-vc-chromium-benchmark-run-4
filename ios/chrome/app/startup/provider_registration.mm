558cc2ef02e60285365300ca6ebc50596639c4cb
Asami Doi <asamidoi@chromium.org>
2024-03-26 10:55:11 +0000

    [ios] Remove CHECK and add override the "platform" value in crush report
    This CL
    - removes CHECK(g_was_hardware_model_name_called) from sys_info_ios.mm
    - adds OverridePlatformValue() to crashpad.h to allow the "platform"
    value to be modified
    
    Bug: 330342304
    Change-Id: Iad8f03e563dcdc4909a1c9cc6fd728ff50ee1396
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5378296
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278224}

--- ios/chrome/app/startup/provider_registration.mm
+++ ios/chrome/app/startup/provider_registration.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/app/startup/provider_registration.h"
 
 #import "base/system/sys_info.h"
+#import "components/crash/core/app/crashpad.h"
 #import "ios/public/provider/chrome/browser/app_utils/app_utils_api.h"
 #import "ios/public/provider/chrome/browser/raccoon/raccoon_api.h"
 
@@ -16,7 +17,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   ios::provider::Initialize();
 
   if (ios::provider::IsRaccoonEnabled()) {
-    base::SysInfo::OverrideHardwareModelName("iPad0,0");
+    const std::string name = "iPad0,0";
+    // Reset the "platform" value in the crash report that was initialized by
+    // StartCrashController().
+    crash_reporter::OverridePlatformValue(name);
+
+    // Override the hardware model name. This will be used in
+    // base::SysInfo::HardwareModelName().
+    base::SysInfo::OverrideHardwareModelName(std::move(name));
   }
 }
 
