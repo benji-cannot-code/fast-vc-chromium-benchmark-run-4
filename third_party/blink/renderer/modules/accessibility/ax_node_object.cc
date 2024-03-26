5edb49c59c5a0e04fa85cf31f17ca1080818eb6e
Joel Hockey <joelhockey@chromium.org>
2024-03-26 04:29:13 +0000

    Revert "Reland "[pdf-a11y]: enable tree consistency checks for plugin tree""
    This reverts commit 7839eba014f941fcc42dec8e073859362c6b3ebb.
    
    Reason for revert: arc.Print.container_p broken again See b/326733014
    
    Feel free to ping @joelhockey for instructions how to run this
    test locally and verify, or run bot chromeos-betty-pi-arc-chrome
    
    Original change's description:
    > Reland "[pdf-a11y]: enable tree consistency checks for plugin tree"
    >
    > This change relands
    > https://crrev.com/c/5323727
    >
    > with additions:
    > - AXNodeObject::AddChildrenImpl which recomputes children if needed, now
    >   checks if it is an embed. If so, it resets the plugin serializer.
    >
    > - AXNodeObject::Detach, similarly, checks if it is an embed. If so, it
    > also resets the plugin serializer.
    >
    > This should capture the lifetime of the embed and its children (creation
    > and deletion).
    >
    > This fixes issues where the embed was clearing its children, resulting
    > in mismatches in the plugin serializer with the blink serializer
    >
    > Bug: 326733014, 327493665, 326131114
    > Test: cq and specific stress tests in the bugs
    > Change-Id: I574abd68e2d0a67174bb1428788853f6bb7c68f5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5378611
    > Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    > Commit-Queue: David Tseng <dtseng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1277761}
    
    Bug: 326733014, 327493665, 326131114
    Change-Id: If458a81a41681bef033236b0a0385c601310106a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5392846
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278111}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -1957,7 +1957,6 @@ void AXNodeObject::Detach() {
   SANITIZER_CHECK(!is_adding_children_)
       << "Cannot detach |this| during AddChildren(): " << GetNode();
 #endif
-
   AXObject::Detach();
   node_ = nullptr;
 }
