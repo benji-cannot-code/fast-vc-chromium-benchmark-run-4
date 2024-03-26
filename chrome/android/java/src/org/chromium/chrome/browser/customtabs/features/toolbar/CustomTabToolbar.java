cdcb664c513cd0a46664beaaa6957a8bd3720944
Sinan Sahin <sinansahin@google.com>
2024-03-26 00:22:55 +0000

    Apply pill-shaped background to Omnibox in CCT
    If an application requests the CCT to feature an interactive Omnibox,
    this change ensures that that LocationBar shows Pill-shaped
    background.
    
    Bug: 330907625
    Change-Id: I1bf2c89ff5343c8e5d4e94aeb03931d2a0473eb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5368831
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Kevin Grosu <kgrosu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1278054}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/toolbar/CustomTabToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/toolbar/CustomTabToolbar.java
@@ -1106,6 +1106,7 @@ public class CustomTabToolbar extends ToolbarLayout implements View.OnLongClickL
                 (v, l, t, r, b, ol, ot, or, ob) -> setButtonsVisibility();
         private boolean mCurrentlyShowingBranding;
         private boolean mBrandingStarted;
+        private Drawable mOmniboxBackground;
         private CallbackController mCallbackController = new CallbackController();
         // Cached the state before branding start so we can reset to the state when its done.
         private @Nullable Integer mPreBandingState;
@@ -1658,6 +1659,7 @@ public class CustomTabToolbar extends ToolbarLayout implements View.OnLongClickL
         }
 
         private void updateColors() {
+            updateOmniboxBackground();
             updateButtonsTint();
 
             if (mUrlCoordinator.setBrandedColorScheme(mBrandedColorScheme)) {
@@ -1668,6 +1670,18 @@ public class CustomTabToolbar extends ToolbarLayout implements View.OnLongClickL
             mTitleBar.setTextColor(
                     OmniboxResourceProvider.getUrlBarPrimaryTextColor(
                             getContext(), mBrandedColorScheme));
+        }
+
+        private void updateOmniboxBackground() {
+            if (mOmniboxBackground == null) return;
+            @ColorInt int background = getBackground().getColor();
+            @ColorInt
+            int bg =
+                    ThemeUtils.getTextBoxColorForToolbarBackgroundInNonNativePage(
+                            getContext(),
+                            background,
+                            mBrandedColorScheme == BrandedColorScheme.INCOGNITO);
+            mOmniboxBackground.setTint(bg);
         }
 
         @Override
@@ -1789,6 +1803,15 @@ public class CustomTabToolbar extends ToolbarLayout implements View.OnLongClickL
         }
 
         void setOmniboxEnabled() {
+            mOmniboxBackground =
+                    AppCompatResources.getDrawable(
+                            getContext(),
+                            R.drawable.modern_toolbar_text_box_background_with_primary_color);
+            mOmniboxBackground.mutate();
+            mOmniboxBackground.setTint(
+                    ChromeColors.getSurfaceColor(getContext(), R.dimen.toolbar_text_box_elevation));
+            mLocationBarFrameLayout.setBackground(mOmniboxBackground);
+
             mTitleUrlContainer.setOnClickListener(
                     v -> {
                         RecordUserAction.record("CustomTabs.OmniboxClicked");
