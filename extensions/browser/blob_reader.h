a08ab7bf667924093980e2acb28edddfb9727532
Lei Zhang <thestig@chromium.org>
2024-03-26 22:05:14 +0000

    Update extensions::BlobReader::BlobReadCallback comment
    On error, BlobReadCallback used to receive -1 for the size, as stated in
    the comments. After https://crrev.com/568063, the implementation changed
    and the callback receives 0 instead. Considering it has been many years
    since the implementation started using 0, just update the comments to
    match the actual behavior. Also update the comments to use backticks.
    
    Change-Id: Ia83b89efc7bdaeb969f3c3caad885a2aa61dc71a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5395517
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278631}

--- extensions/browser/blob_reader.h
+++ extensions/browser/blob_reader.h
@@ -22,9 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class BlobReader : public blink::mojom::BlobReaderClient,
                    public mojo::DataPipeDrainer::Client {
  public:
-  // |blob_data| contains the portion of the Blob requested. |blob_total_size|
-  // is the total size of the Blob, and may be larger than |blob_data->size()|.
-  // |blob_total_size| is -1 if it cannot be determined.
+  // `blob_data` contains the portion of the Blob requested. `blob_total_size`
+  // is the total size of the Blob, and may be larger than `blob_data->size()`.
+  // `blob_total_size` is 0 if it cannot be determined.
   using BlobReadCallback =
       base::OnceCallback<void(std::string blob_data, int64_t blob_total_size)>;
 
