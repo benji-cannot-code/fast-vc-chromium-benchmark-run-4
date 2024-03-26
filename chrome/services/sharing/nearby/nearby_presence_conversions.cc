e058d44a741a8bfd25619a8182ce2efdabe5d95d
Juliet Levesque <julietlevesque@google.com>
2024-03-26 21:16:00 +0000

    [Fix It] Fix type in Nearby Presence - CovertStatusToMojomStatus
    Fixes type from CovertStatusToMojomStatus to
    ConvertStatusToMojoStatus.
    
    Tested: N/A, trivial change
    Fixed: 291931479
    Change-Id: If02c06c5898b3b762bb8ad9fb8fdd3c46599af23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5392263
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#1278601}

--- chrome/services/sharing/nearby/nearby_presence_conversions.cc
+++ chrome/services/sharing/nearby/nearby_presence_conversions.cc
@@ -115,8 +115,7 @@ mojom::IdentityType ConvertIdentityTypeToMojom(
   }
 }
 
-mojo_base::mojom::AbslStatusCode CovertStatusToMojomStatus(
-    absl::Status status) {
+mojo_base::mojom::AbslStatusCode ConvertStatusToMojom(absl::Status status) {
   switch (status.code()) {
     case absl::StatusCode::kOk:
       return mojo_base::mojom::AbslStatusCode::kOk;
