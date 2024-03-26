58ee73a6e36f3e2a6b6c223a0234bdde446b94b0
Jose Dapena Paz <jdapena@igalia.com>
2024-03-26 11:08:45 +0000

    IWYU: missing include for usage of std::optional in inset_area.h
    Bug: 41455655
    Change-Id: Ideef817267b0b1af90b2fa9be17ff07e90bad1d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5392760
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1278226}

--- third_party/blink/renderer/core/style/inset_area.h
+++ third_party/blink/renderer/core/style/inset_area.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_STYLE_INSET_AREA_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_STYLE_INSET_AREA_H_
 
+#include <optional>
+
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/css/anchor_query.h"
 #include "third_party/blink/renderer/core/css/css_anchor_query_enums.h"
