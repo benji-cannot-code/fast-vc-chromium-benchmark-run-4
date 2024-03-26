6ecad41198de35fdced1699fa3f0bb4ed7215efc
Chris Fredrickson <cfredric@chromium.org>
2024-03-26 18:07:31 +0000

    Restrict CookiePartitionKey default ctor to mojom-generated code
    Client code should prefer the various factory methods, instead of a
    default ctor.
    
    Bug: 326278108
    Change-Id: I6ccfb506618e33e7686fc46501cf698f01af2d85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5236517
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278465}

--- net/cookies/cookie_partition_key.cc
+++ net/cookies/cookie_partition_key.cc
@@ -11,7 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/logging.h"
 #include "base/types/optional_util.h"
+#include "net/base/cronet_buildflags.h"
 #include "net/cookies/cookie_constants.h"
+
+#if !BUILDFLAG(CRONET_BUILD)
+#include "mojo/public/cpp/bindings/default_construct_tag.h"
+#endif
 
 namespace net {
 
@@ -34,7 +39,9 @@ CookiePartitionKey::SerializedCookiePartitionKey::TopLevelSite() const {
   return top_level_site_;
 }
 
-CookiePartitionKey::CookiePartitionKey() = default;
+#if !BUILDFLAG(CRONET_BUILD)
+CookiePartitionKey::CookiePartitionKey(mojo::DefaultConstruct::Tag) {}
+#endif
 
 CookiePartitionKey::CookiePartitionKey(
     const SchemefulSite& site,
