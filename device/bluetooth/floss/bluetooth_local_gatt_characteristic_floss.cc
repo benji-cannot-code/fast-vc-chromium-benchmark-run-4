107ff8559dca2e446aa1af810c7b695123b850a8
Katherine Lai <laikatherine@google.com>
2024-03-26 14:36:12 +0000

    [floss] Update local GATT chars/descs to use Floss props/perms
    Floss daemon enums for properties and permissions is different
    from those defined in device/bluetooth. Map local floss GATT
    characteristics and descriptors properties/permissions to be
    Floss defined
    
    BUG=b:328813308
    TEST=chrome-btclient
    
    Change-Id: Ia429a5d70ad363a8eb91949512c3a30ee765a896
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5394169
    Reviewed-by: Ying Hsu <yinghsu@chromium.org>
    Commit-Queue: Ying Hsu <yinghsu@chromium.org>
    Auto-Submit: Katherine Lai <laikatherine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278298}

--- device/bluetooth/floss/bluetooth_local_gatt_characteristic_floss.cc
+++ device/bluetooth/floss/bluetooth_local_gatt_characteristic_floss.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "device/bluetooth/bluetooth_gatt_characteristic.h"
 #include "device/bluetooth/floss/bluetooth_adapter_floss.h"
+#include "device/bluetooth/floss/bluetooth_gatt_characteristic_floss.h"
 #include "device/bluetooth/floss/bluetooth_gatt_service_floss.h"
 #include "device/bluetooth/floss/bluetooth_local_gatt_service_floss.h"
 #include "device/bluetooth/floss/floss_dbus_manager.h"
@@ -24,8 +25,11 @@ BluetoothLocalGattCharacteristicFloss::Create(
     Properties properties,
     Permissions permissions,
     BluetoothLocalGattServiceFloss* service) {
+  const auto& [floss_properties, floss_permissions] =
+      BluetoothGattCharacteristicFloss::ConvertPropsAndPermsToFloss(
+          static_cast<uint8_t>(properties), static_cast<uint16_t>(permissions));
   auto* characteristic = new BluetoothLocalGattCharacteristicFloss(
-      uuid, properties, permissions, service);
+      uuid, floss_properties, floss_permissions, service);
   auto weak_ptr = characteristic->weak_ptr_factory_.GetWeakPtr();
   weak_ptr->index_ =
       service->AddCharacteristic(base::WrapUnique(characteristic));
@@ -60,12 +64,18 @@ device::BluetoothUUID BluetoothLocalGattCharacteristicFloss::GetUUID() const {
 
 device::BluetoothGattCharacteristic::Properties
 BluetoothLocalGattCharacteristicFloss::GetProperties() const {
-  return properties_;
+  const auto& [props, _] =
+      BluetoothGattCharacteristicFloss::ConvertPropsAndPermsFromFloss(
+          properties_, permissions_);
+  return props;
 }
 
 device::BluetoothGattCharacteristic::Permissions
 BluetoothLocalGattCharacteristicFloss::GetPermissions() const {
-  return permissions_;
+  const auto& [_, perms] =
+      BluetoothGattCharacteristicFloss::ConvertPropsAndPermsFromFloss(
+          properties_, permissions_);
+  return perms;
 }
 
 device::BluetoothLocalGattCharacteristic::NotificationStatus
@@ -73,10 +83,12 @@ BluetoothLocalGattCharacteristicFloss::NotifyValueChanged(
     const device::BluetoothDevice* device,
     const std::vector<uint8_t>& new_value,
     bool indicate) {
-  if (indicate && !(properties_ & PROPERTY_INDICATE)) {
+  if (indicate &&
+      !(properties_ & GattCharacteristic::GATT_CHAR_PROP_BIT_INDICATE)) {
     return INDICATE_PROPERTY_NOT_SET;
   }
-  if (!indicate && !(properties_ & PROPERTY_NOTIFY)) {
+  if (!indicate &&
+      !(properties_ & GattCharacteristic::GATT_CHAR_PROP_BIT_NOTIFY)) {
     return NOTIFY_PROPERTY_NOT_SET;
   }
   return service_->GetAdapter()->SendValueChanged(this, new_value)
