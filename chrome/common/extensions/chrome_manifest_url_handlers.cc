22296d6e13c085648acb765f9c0eeeb1802ed5a9
Anton Bershanskyi <bershanskyi@gmail.com>
2024-03-26 17:37:35 +0000

    [Extensions] Forbid module URLs in developer_tools
    Fixed: 41497193
    Change-Id: Ied8c79a7395773c9aa9cdcec330505a3d62e215d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5338468
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Anton Bershanskyi <bershanskyi@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1278428}

--- chrome/common/extensions/chrome_manifest_url_handlers.cc
+++ chrome/common/extensions/chrome_manifest_url_handlers.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/manifest.h"
 #include "extensions/common/manifest_constants.h"
 #include "extensions/common/manifest_handlers/permissions_parser.h"
+#include "extensions/common/manifest_handlers/shared_module_info.h"
 #include "extensions/common/manifest_url_handlers.h"
 #include "extensions/common/permissions/api_permission.h"
 
@@ -66,8 +67,12 @@ bool DevToolsPageHandler::Parse(Extension* extension, std::u16string* error) {
     return false;
   }
   GURL url = extension->GetResourceURL(*devtools_str);
-  const bool is_extension_url =
-      url.SchemeIs(kExtensionScheme) && url.host_piece() == extension->id();
+  // SharedModuleInfo::IsImportedPath() does not require knowledge of data from
+  // extension, so we can call it right here in Parse() and not Validate() and
+  // do not need to specify DevToolsPageHandler::PrerequisiteKeys()
+  const bool is_extension_url = url.SchemeIs(kExtensionScheme) &&
+                                url.host_piece() == extension->id() &&
+                                !SharedModuleInfo::IsImportedPath(url.path());
   if (!is_extension_url) {
     *error = errors::kInvalidDevToolsPage;
     return false;
