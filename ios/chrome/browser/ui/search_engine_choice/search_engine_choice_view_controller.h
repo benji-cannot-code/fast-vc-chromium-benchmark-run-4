f38a388178fcaf98fce667c44a0e39c788c3f72b
Mihai Sardarescu <msarda@chromium.org>
2024-03-26 09:11:01 +0000

    [iOS] Use 54px margins on iPad for DSE choice screen.
    This CL uses 54 px margins for the Default Search Engine choice screen
    on iPad both on FRE and non-FRE cases.
    
    Screenshots (Google internal):
    https://docs.google.com/document/d/12IADv_g7UzkS335ijXqUU9CHr8xRJKAW1cYc-1vmyp4/edit?usp=sharing
    
    Fixed: 331244180
    
    Change-Id: I420cac10e8eae00756f3ec38d8a31d4d11d23be7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5392920
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Auto-Submit: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278194}

--- ios/chrome/browser/ui/search_engine_choice/search_engine_choice_view_controller.h
+++ ios/chrome/browser/ui/search_engine_choice/search_engine_choice_view_controller.h
@@ -31,7 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Delegate to update the selected search engine.
 @property(nonatomic, weak) id<SearchEngineChoiceMutator> mutator;
 
-- (instancetype)initWithFirstRunMode:(BOOL)isForFRE NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithFirstRunMode:(BOOL)isForFRE
+                     wideMarginWidth:(BOOL)wideMarginWidth
+    NS_DESIGNATED_INITIALIZER;
 - (instancetype)initWithNibName:(NSString*)nibNameOrNil
                          bundle:(NSBundle*)nibBundleOrNil NS_UNAVAILABLE;
 - (instancetype)initWithCoder:(NSCoder*)aDecoder NS_UNAVAILABLE;
