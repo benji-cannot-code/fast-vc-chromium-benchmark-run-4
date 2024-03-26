dc2268844a3ab9e000f40f930db10d01e3786507
Alex Mitra <alexmitra@chromium.org>
2024-03-26 13:13:43 +0000

    Set up mojo pipe from blink -> java for IME data
    We currently send limited CursorAnchorInfo data from Blink to the
    browser. The path for data goes through many classes including being
    passed through both Mojo and JNI.
    
    This change replaces that path with a new one only using Mojo. The
    send end of the pipe lives in WebFrameWidgetImpl in Blink and the
    receiving end of the pipe is attached to ImeRenderWidgetHostImpl in
    Java where calls are forwarded to the ImeAdapterImpl.
    
    Using a single Mojo call to handle both the IPC and the language
    switch should decrease the latency between calculating this data and
    sending it to Android.
    
    https://developer.android.com/reference/android/view/inputmethod/CursorAnchorInfo
    
    Bug: 1500633
    Change-Id: I5b4f5b0ac3b1a3eec881691cc589dbf11c9a7524
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5132102
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Alex Mitra <alexmitra@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278267}

--- content/browser/android/ime_adapter_android.cc
+++ content/browser/android/ime_adapter_android.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/common/input/native_web_keyboard_event.h"
 #include "third_party/blink/public/common/input/web_input_event.h"
+#include "third_party/blink/public/mojom/input/ime_host.mojom.h"
 #include "third_party/blink/public/mojom/input/stylus_writing_gesture.mojom.h"
 #include "third_party/blink/public/platform/web_text_input_type.h"
 #include "ui/base/ime/ime_text_span.h"
@@ -448,6 +449,24 @@ void ImeAdapterAndroid::OnStylusWritingGestureActionCompleted(
     Java_ImeAdapterImpl_onStylusWritingGestureActionCompleted(env, obj, id,
                                                               (int)result);
   }
+}
+
+void ImeAdapterAndroid::SetUpImeRenderWidgetHost(JNIEnv* env) {
+  if (!base::FeatureList::IsEnabled(
+          blink::features::kCursorAnchorInfoMojoPipe)) {
+    return;
+  }
+  auto* input_handler = GetFocusedFrameWidgetInputHandler();
+  if (!input_handler) {
+    return;
+  }
+  // Use a pending remote so we can pass it to Blink.
+  mojo::PendingRemote<blink::mojom::ImeRenderWidgetHost> ime_render_widget_host;
+  auto receiver = ime_render_widget_host.InitWithNewPipeAndPassReceiver();
+  ScopedJavaLocalRef<jobject> obj = java_ime_adapter_.get(env);
+  Java_ImeAdapterImpl_bindImeRenderHost(env, obj,
+                                        receiver.PassPipe().release().value());
+  input_handler->PassImeRenderWidgetHost(std::move(ime_render_widget_host));
 }
 
 void ImeAdapterAndroid::AdvanceFocusForIME(JNIEnv* env,
