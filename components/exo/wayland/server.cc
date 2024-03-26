4dd54c3d5855289c05ceadddbdab5f87bba7d096
Chloe Pelling <cpelling@google.com>
2024-03-26 21:43:22 +0000

    Reland "exo: Emit trace events for all Wayland events and requests."
    This reverts commit 6c3f6e63efe176f150a7128eb02b6d0608a0c1a8.
    
    Reason for revert: reland with fix
    
    Original change's description:
    > Revert "exo: Emit trace events for all Wayland events and requests."
    >
    > This reverts commit d9c252b13b876bcc662adb3134e08493b427dbf8.
    >
    > Reason for revert:
    > This CL introduced a memory leak when it created the logger with wl_display_add_protocol_logger but never destroyed it via wl_protocol_logger_destroy
    >
    >
    > Original change's description:
    > > exo: Emit trace events for all Wayland events and requests.
    > >
    > > This lets us visualize interactions between Exo and its clients
    > > using Perfetto's timeline view, in context with other Exo/Ash
    > > TRACE_EVENTs. The formatting is similar to that given by the
    > > WAYLAND_DEBUG environment variable.
    > >
    > > Arguments aren't logged yet, but if the approach looks good it
    > > should be easy enough to add them in a follow-up CL.
    > >
    > > BUG=b:313769741
    > >
    > > Change-Id: Id5a656d486c5b5b13d148fc8a0bab8c281779518
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5367052
    > > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > > Commit-Queue: Chloe Pelling <cpelling@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1276035}
    >
    > Bug: b:313769741
    > Change-Id: I23a12644bdcf7c04bd1366e7321db866cf0a823b
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5383454
    > Owners-Override: Zaina Al-Mashni <zalmashni@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Zaina Al-Mashni <zalmashni@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1276253}
    
    Bug: b:313769741
    Change-Id: Ifc8d4b22a910189ada58b42e450b13969c6d8442
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5386902
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Auto-Submit: Chloe Pelling <cpelling@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278617}

--- components/exo/wayland/server.cc
+++ components/exo/wayland/server.cc
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/wayland/server_util.h"
 #include "components/exo/wayland/surface_augmenter.h"
 #include "components/exo/wayland/wayland_dmabuf_feedback_manager.h"
+#include "components/exo/wayland/wayland_protocol_logger.h"
 #include "components/exo/wayland/wayland_watcher.h"
 #include "components/exo/wayland/wl_compositor.h"
 #include "components/exo/wayland/wl_data_device_manager.h"
@@ -255,6 +256,8 @@ Server::Server(Display* display,
   SetSecurityDelegate(wl_display_.get(), security_delegate_.get());
 
   client_tracker_ = std::make_unique<ClientTracker>(wl_display_.get());
+  wayland_protocol_logger_ =
+      std::make_unique<WaylandProtocolLogger>(wl_display_.get());
 }
 
 void Server::Initialize() {
