c024d53345124f5dee99acf010a0c04a35d76790
Akihiro Ota <akihiroota@chromium.org>
2024-03-26 13:48:13 +0000

    Remove Google TTS with language packs flag
    Since TTS with language packs has been launched and is stable, we can
    safely remove the feature flag.
    
    Note that this change has the potential to impact the Google TTS
    extension code living in google3 [1]. However, there are no longer
    references to this feature flag in that part of the codebase, so we
    can safely remove the flag.
    
    [1] cs/speech/tts/platforms/web/chrome_extension/googletts_engine_chromiumos.js
    
    Fixed: b/40283796
    Change-Id: I2e03d9f694b121f9fc81491ce0cb802fa52b3233
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5391684
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278280}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3731,9 +3731,6 @@ extern const char kExperimentalAccessibilityDictationContextCheckingName[];
 extern const char
     kExperimentalAccessibilityDictationContextCheckingDescription[];
 
-extern const char kExperimentalAccessibilityGoogleTtsLanguagePacksName[];
-extern const char kExperimentalAccessibilityGoogleTtsLanguagePacksDescription[];
-
 extern const char kExperimentalAccessibilityGoogleTtsHighQualityVoicesName[];
 extern const char
     kExperimentalAccessibilityGoogleTtsHighQualityVoicesDescription[];
