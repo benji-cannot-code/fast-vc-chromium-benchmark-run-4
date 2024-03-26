7b2b57c98e54770ad2edf0e34ccd924a676a0573
Longbo Wei <longbowei@google.com>
2024-03-26 20:21:21 +0000

    kbd-backlight: Add slider element
    screenshot: http://screen/3YnNdwnDh4QFsTY
    
    Bug: b/326009607
    Test: browser_tests -f DevicePagePerDeviceKeyboardSubsection
    Change-Id: I4f1bda19c59437f849a70c480dd2aca50708aa53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5389075
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: Longbo Wei <longbowei@google.com>
    Cr-Commit-Position: refs/heads/main@{#1278562}

--- chrome/browser/resources/ash/settings/device_page/per_device_keyboard_subsection.ts
+++ chrome/browser/resources/ash/settings/device_page/per_device_keyboard_subsection.ts
@@ -77,6 +77,17 @@ export class SettingsPerDeviceKeyboardSubsectionElement extends
         },
       },
 
+      keyboardBrightnessPercentPref: {
+        type: Object,
+        value() {
+          return {
+            key: 'fakekeyboardBrightnessPercentPref',
+            type: chrome.settingsPrivate.PrefType.NUMBER,
+            value: 40,
+          };
+        },
+      },
+
       isKeyboardBacklightControlInSettingsEnabled: {
         type: Boolean,
         value() {
@@ -159,6 +170,7 @@ export class SettingsPerDeviceKeyboardSubsectionElement extends
   protected keyboardPolicies: KeyboardPolicies;
   private topRowAreFunctionKeysPref: chrome.settingsPrivate.PrefObject;
   private blockMetaFunctionKeyRewritesPref: chrome.settingsPrivate.PrefObject;
+  private keyboardBrightnessPercentPref: chrome.settingsPrivate.PrefObject;
   private remapKeyboardKeysSublabel: string;
   private isInitialized: boolean = false;
   private inputDeviceSettingsProvider: InputDeviceSettingsProviderInterface =
