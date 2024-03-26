17f3d7b24ce7d969713d4bc42ca30926ab1056ee
Jose Dapena Paz <jdapena@igalia.com>
2024-03-26 10:06:31 +0000

    IWYU: use regular atomic include instead of hardcoding libc++ path
    Bug: 41455655
    Change-Id: I2f2076d533b9732d1f7ff8aea5dc027547f62347
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5373879
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Reviewed-by: Marja Hölttä <marja@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278213}

--- third_party/blink/renderer/bindings/core/v8/script_streamer.cc
+++ third_party/blink/renderer/bindings/core/v8/script_streamer.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/bindings/core/v8/script_streamer.h"
 
+#include <atomic>
 #include <memory>
 #include <utility>
 
@@ -61,7 +62,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/shared_buffer.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 #include "third_party/blink/renderer/platform/wtf/text/text_encoding_registry.h"
-#include "third_party/libc++/src/include/__atomic/atomic.h"
 
 namespace blink {
 namespace {
