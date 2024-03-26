8c95e9b56211b2b0f3ed262d16c99cc72cf852c0
Matt Menke <mmenke@chromium.org>
2024-03-26 12:47:26 +0000

    Remove DanglingUntriaged annotation from HttpNetworkSession.
    The QuicNetworkTransactionTest.QuicProxyAuth test was creating an
    HttpNetworkSession twice from the same objects, but creating a
    new ProxyResolutionService for each session, and only destroying the
    first HttpNetworkSession after overwriting and destroying the first
    ProxyResolutionService.
    
    There was no need for the test to create a ProxyResolutionService
    twice, so just switch to using a single ProxyResolutionService for
    both HttpNetworkSessions.
    
    The test could perhaps be switching to using a single
    HttpNetworkSession as well, flushing the QUIC session pool between
    tests, but keeping that as-is, for now.
    
    Bug: 40212619
    Change-Id: I41886941b2325cb623d24602f4b116fed3c239f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5388922
    Commit-Queue: mmenke <mmenke@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278258}

--- net/http/http_network_session.h
+++ net/http/http_network_session.h
@@ -188,7 +188,7 @@ struct NET_EXPORT HttpNetworkSessionContext {
   raw_ptr<CertVerifier> cert_verifier;
   raw_ptr<TransportSecurityState> transport_security_state;
   raw_ptr<SCTAuditingDelegate> sct_auditing_delegate;
-  raw_ptr<ProxyResolutionService, DanglingUntriaged> proxy_resolution_service;
+  raw_ptr<ProxyResolutionService> proxy_resolution_service;
   raw_ptr<ProxyDelegate> proxy_delegate;
   raw_ptr<const HttpUserAgentSettings> http_user_agent_settings;
   raw_ptr<SSLConfigService> ssl_config_service;
@@ -327,8 +327,7 @@ class NET_EXPORT HttpNetworkSession {
   const raw_ptr<ReportingService> reporting_service_;
   const raw_ptr<NetworkErrorLoggingService> network_error_logging_service_;
 #endif
-  const raw_ptr<ProxyResolutionService, DanglingUntriaged>
-      proxy_resolution_service_;
+  const raw_ptr<ProxyResolutionService> proxy_resolution_service_;
   const raw_ptr<SSLConfigService> ssl_config_service_;
 
   HttpAuthCache http_auth_cache_;
