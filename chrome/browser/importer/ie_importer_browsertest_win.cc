c68062551c5bc2a6e575afb1671e66987f1b64da
Helmut Januschka <helmut@januschka.com>
2024-03-27 10:49:36 +0000

    importer: refactor kDummyFaviconImageData making it constexpr and removing the need of StringPiece
    Bug: 691162
    Change-Id: I9e2627fd0c04ef0642b0bb6e53f0720e3be34faf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5377722
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Helmut Januschka <helmut@januschka.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278859}

--- chrome/browser/importer/ie_importer_browsertest_win.cc
+++ chrome/browser/importer/ie_importer_browsertest_win.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wrl/client.h>
 
 #include <algorithm>
+#include <array>
 #include <string>
 #include <vector>
 
@@ -93,16 +94,17 @@ const char16_t kIECacheItemUrl[] =
 const char16_t kIECacheItemTitle[] = u"Unittest Cache Item GUID";
 
 const wchar_t kFaviconStreamSuffix[] = L"url:favicon:$DATA";
-const char kDummyFaviconImageData[] =
-    "\x42\x4D"          // Magic signature 'BM'
-    "\x1E\x00\x00\x00"  // File size
-    "\x00\x00\x00\x00"  // Reserved
-    "\x1A\x00\x00\x00"  // Offset of the pixel data
-    "\x0C\x00\x00\x00"  // Header Size
-    "\x01\x00\x01\x00"  // Size: 1x1
-    "\x01\x00"          // Reserved
-    "\x18\x00"          // 24-bits
-    "\x00\xFF\x00\x00"; // The pixel
+constexpr std::array<uint8_t, 30> kDummyFaviconImageData = {
+    0x42, 0x4D,              // Magic signature 'BM'
+    0x1E, 0x00, 0x00, 0x00,  // File size
+    0x00, 0x00, 0x00, 0x00,  // Reserved
+    0x1A, 0x00, 0x00, 0x00,  // Offset of the pixel data
+    0x0C, 0x00, 0x00, 0x00,  // Header Size
+    0x01, 0x00, 0x01, 0x00,  // Size: 1x1
+    0x01, 0x00,              // Planes
+    0x18, 0x00,              // 24-bits
+    0x00, 0xFF, 0x00, 0x00   // The pixel
+};
 
 struct FaviconGroup {
   const char16_t* favicon_url;
@@ -208,8 +210,7 @@ bool CreateUrlFileWithFavicon(const base::FilePath& file,
   // Write dummy favicon image data in NTFS alternate data stream.
   return favicon_url.empty() ||
          base::WriteFile(file.ReplaceExtension(kFaviconStreamSuffix),
-                         base::StringPiece(kDummyFaviconImageData,
-                                           sizeof kDummyFaviconImageData));
+                         kDummyFaviconImageData);
 }
 
 bool CreateUrlFile(const base::FilePath& file, const std::wstring& url) {
