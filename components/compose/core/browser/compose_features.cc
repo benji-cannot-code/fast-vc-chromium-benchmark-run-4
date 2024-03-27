ca2f23b12d35dd73cc4f86f9dc8fc2839bbc17b4
Cathy Li <chili@chromium.org>
2024-03-27 17:53:50 +0000

    Add flag for compose UI Refinement bundle
    Bug: b/331486594
    Change-Id: Ic8513df7a4d2eef3e5766af7dc6b35800fbdb802
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5398269
    Reviewed-by: Anthony Cui <cuianthony@chromium.org>
    Commit-Queue: Cathy Li <chili@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279082}

--- components/compose/core/browser/compose_features.cc
+++ components/compose/core/browser/compose_features.cc
@@ -62,4 +62,8 @@ BASE_FEATURE(kComposeRequestLatencyTimeout,
              "ComposeRequestLatencyTimeout",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+BASE_FEATURE(kComposeUiRefinement,
+             "ComposeUiRefinement",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 }  // namespace compose::features
