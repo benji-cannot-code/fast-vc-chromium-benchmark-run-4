1eb83777bcbcb96cb3e062ede4a4995bb007a69f
Steven Holte <holte@google.com>
2024-03-27 07:29:14 +0000

    Refactor SafetyModelInfo.
    Move logic into methods and make fields private.
    
    Change-Id: Ic44cdfce47f76263e782ef4a2e9f4dad270ddcf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5399132
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278824}

--- components/optimization_guide/core/model_execution/on_device_model_service_controller.h
+++ components/optimization_guide/core/model_execution/on_device_model_service_controller.h
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_OPTIMIZATION_GUIDE_CORE_MODEL_EXECUTION_ON_DEVICE_MODEL_SERVICE_CONTROLLER_H_
 #define COMPONENTS_OPTIMIZATION_GUIDE_CORE_MODEL_EXECUTION_ON_DEVICE_MODEL_SERVICE_CONTROLLER_H_
 
+#include <sys/types.h>
 #include <cstdint>
 #include <memory>
 #include <optional>
@@ -131,21 +132,30 @@ class OnDeviceModelServiceController
 
   class SafetyModelInfo {
    public:
+    ~SafetyModelInfo();
+
+    static std::unique_ptr<SafetyModelInfo> Load(
+        base::optional_ref<const ModelInfo> model_info);
+    std::optional<proto::FeatureTextSafetyConfiguration> GetConfig(
+        proto::ModelExecutionFeature feature) const;
+    base::FilePath GetDataPath() const;
+    base::FilePath GetSpModelPath() const;
+    int64_t GetVersion() const;
+    uint32_t num_output_categories() const { return num_output_categories_; }
+
+   private:
     SafetyModelInfo(
         const ModelInfo& model_info,
         uint32_t num_output_categories,
         base::flat_map<proto::ModelExecutionFeature,
                        proto::FeatureTextSafetyConfiguration> feature_configs);
-    ~SafetyModelInfo();
 
-    const ModelInfo model_info;
-    const uint32_t num_output_categories;
+    const ModelInfo model_info_;
+    const uint32_t num_output_categories_;
     base::flat_map<proto::ModelExecutionFeature,
                    proto::FeatureTextSafetyConfiguration>
-        feature_configs;
+        feature_configs_;
   };
-
-  bool InitializeSafetyModelInfo(const ModelInfo& model_info);
 
   // Sets the base model directory and initializes the on-device model
   // controller with the parameters, to be ready to load models and execute.
@@ -177,10 +187,6 @@ class OnDeviceModelServiceController
   // Gets the model versions based on the current model paths set.
   proto::OnDeviceModelVersions GetModelVersions(
       const std::string& component_version) const;
-
-  // Returns the text safety configuration for `feature`.
-  std::optional<proto::FeatureTextSafetyConfiguration>
-  GetFeatureTextSafetyConfigForFeature(proto::ModelExecutionFeature feature);
 
   // This may be null in the destructor, otherwise non-null.
   std::unique_ptr<OnDeviceModelAccessController> access_controller_;
