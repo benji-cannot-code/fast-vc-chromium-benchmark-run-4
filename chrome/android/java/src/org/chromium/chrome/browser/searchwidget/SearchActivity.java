053c5d5935b106537217a89460bf4ccf1c139f69
Tomasz Wiszkowski <ender@google.com>
2024-03-27 21:16:50 +0000

    Update hint showing in empty Omnibox on CCT.
    Doc: http://shortn/_dwJID3fMDj
    Bug: 330907625
    Change-Id: Ia06e400423e7b44d50a13936522d8ec159ae7975
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5399524
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279245}

--- chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivity.java
@@ -624,6 +624,7 @@ public class SearchActivity extends AsyncInitializationActivity
 
     private void beginQuery() {
         mSearchBox.beginQuery(
+                mIntentOrigin,
                 mSearchType,
                 SearchActivityUtils.getIntentQuery(getIntent()),
                 mLocationBarCoordinator.getVoiceRecognitionHandler(),
