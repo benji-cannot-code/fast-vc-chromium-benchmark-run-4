fb01ddb48e13fb6a4167e71e933f0fa36bfad8dc
Sylvain Defresne <sdefresne@chromium.org>
2024-03-27 10:54:21 +0000

    [ios] Add sample application that uses IntentsKit
    Bug: 326059372
    Change-Id: I3a1ebac4e9209297308b6711919a3a38c17beda1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5374288
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278868}

---
+++ ios/intents/src/Bar.swift
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import AppIntents
+
+public struct BarShortcuts: AppShortcutsProvider {
+  public init() {}
+
+  public static var appShortcuts: [AppShortcut] {
+    AppShortcut(
+      intent: FooIntent(),
+      phrases: [
+        "Foo",
+        "Foo \(.applicationName)",
+      ],
+      shortTitle: "Foo Shortcut",
+      systemImageName: ""
+    )
+  }
+}
