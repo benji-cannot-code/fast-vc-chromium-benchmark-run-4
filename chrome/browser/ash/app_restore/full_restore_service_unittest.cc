23329e4a784e167d854840edc33b6b6cb7b20e97
Sammie Quon <sammiequon@chromium.org>
2024-03-27 21:05:19 +0000

    pine: Fix some *FullRestoreService* tests
    Some of the tests were passing when run individually, but not when ran
    in a batch. Forcing first run was masking some issues with the tests
    themselves, and also fixes some logic for non-forest runs.
    
    Test: unit_tests *FullRestoreService*
    Bug: b:330815402
    Change-Id: Ifaf87075b03d824dd02632b7c4aaa2f2f0b1016a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5397521
    Reviewed-by: Sophie Wen <sophiewen@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279232}

--- chrome/browser/ash/app_restore/full_restore_service_unittest.cc
+++ chrome/browser/ash/app_restore/full_restore_service_unittest.cc
@@ -386,8 +386,6 @@ TEST_F(FullRestoreServiceTest, AskEveryTime) {
   EXPECT_EQ(RestoreOption::kAskEveryTime, GetRestoreOption());
 }
 
-// TODO(http://b/326982900): Migrate this test suite to test with forest
-// enabled.
 class FullRestoreServiceTestHavingFullRestoreFile
     : public FullRestoreServiceTest {
  public:
@@ -1019,6 +1017,9 @@ class ForestFullRestoreServiceTest : public FullRestoreServiceTest {
   ForestFullRestoreServiceTest() {
     scoped_feature_list_.Reset();
     scoped_feature_list_.InitAndEnableFeature(features::kForestFeature);
+
+    base::CommandLine::ForCurrentProcess()->AppendSwitch(
+        ::switches::kNoFirstRun);
   }
   ForestFullRestoreServiceTest(const ForestFullRestoreServiceTest&) = delete;
   ForestFullRestoreServiceTest& operator=(const ForestFullRestoreServiceTest&) =
@@ -1036,25 +1037,25 @@ class ForestFullRestoreServiceTest : public FullRestoreServiceTest {
   }
 };
 
-// If the system is crash, and there is no full restore file, don't show the
-// pine dialog.
+// Tests that if the system is crash, even if there is no full restore file, we
+// show the pine dialog.
 TEST_F(ForestFullRestoreServiceTest, Crash) {
   ExitTypeService::GetInstanceForProfile(profile())
       ->SetLastSessionExitTypeForTest(ExitType::kCrashed);
   auto mock_delegate = std::make_unique<MockFullRestoreServiceDelegate>();
   EXPECT_CALL(*mock_delegate, MaybeStartPineOverviewSession(testing::_))
-      .Times(0);
+      .Times(1);
   CreateFullRestoreServiceForTesting(std::move(mock_delegate));
   EXPECT_EQ(RestoreOption::kAskEveryTime, GetRestoreOption());
 }
 
-// If the OS restore setting is 'Ask every time', and there is no full restore
-// file, don't show the pine dialog.
+// Tests that if the OS restore setting is 'Ask every time', even if there is no
+// full restore file, we show the pine dialog.
 TEST_F(ForestFullRestoreServiceTest, AskEveryTime) {
   SetRestoreOption(RestoreOption::kAskEveryTime);
   auto mock_delegate = std::make_unique<MockFullRestoreServiceDelegate>();
   EXPECT_CALL(*mock_delegate, MaybeStartPineOverviewSession(testing::_))
-      .Times(0);
+      .Times(1);
   CreateFullRestoreServiceForTesting(std::move(mock_delegate));
   VerifyRestoreInitSettingHistogram(RestoreOption::kAskEveryTime, 1);
   EXPECT_EQ(RestoreOption::kAskEveryTime, GetRestoreOption());
@@ -1298,6 +1299,9 @@ class ForestFullRestoreServiceMultipleUsersTest
   ForestFullRestoreServiceMultipleUsersTest() {
     scoped_feature_list_.Reset();
     scoped_feature_list_.InitAndEnableFeature(features::kForestFeature);
+
+    base::CommandLine::ForCurrentProcess()->AppendSwitch(
+        ::switches::kNoFirstRun);
   }
   ForestFullRestoreServiceMultipleUsersTest(
       const ForestFullRestoreServiceMultipleUsersTest&) = delete;
