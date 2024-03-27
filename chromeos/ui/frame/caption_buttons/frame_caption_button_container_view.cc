4bd6473b083bc288dfcd8d7d7944b3505502d37d
ThuHuong <thv@chromium.org>
2024-03-27 22:17:29 +0000

    Add Caption Button Container View ID.
    Set Caption Button container view id in ash and browser frame view. This
    ID will be used in the next CL to retrieve caption button container from
    an aura window Root View.
    
    Bug: b/330788197
    Change-Id: I7a75551301cb6393a5fd01e150fed6b8f645679c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402261
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Thu Huong Vu <thv@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279283}

--- chromeos/ui/frame/caption_buttons/frame_caption_button_container_view.cc
+++ chromeos/ui/frame/caption_buttons/frame_caption_button_container_view.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/user_metrics.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "chromeos/strings/grit/chromeos_strings.h"
+#include "chromeos/ui/base/chromeos_ui_constants.h"
 #include "chromeos/ui/base/display_util.h"
 #include "chromeos/ui/base/window_properties.h"
 #include "chromeos/ui/base/window_state_type.h"
@@ -217,6 +218,8 @@ FrameCaptionButtonContainerView::FrameCaptionButtonContainerView(
     bool is_close_button_enabled,
     std::unique_ptr<views::FrameCaptionButton> custom_button)
     : views::AnimationDelegateViews(frame->GetRootView()), frame_(frame) {
+  SetID(ViewID::VIEW_ID_CAPTION_BUTTON_CONTAINER);
+
   auto default_caption_button_model =
       std::make_unique<DefaultCaptionButtonModel>(frame,
                                                   is_close_button_enabled);
