c001761b27b48e59ef8e28eadde875641fb7fe62
Vinny Persky <vinnypersky@google.com>
2024-03-27 14:56:23 +0000

    Set size for VCN 3DS pop-up
    This CL sets the size of the VCN 3DS pop-up based on the Mastercard and
    Visa developer guides.
    
    Bug: 1517762
    Change-Id: Ia90fb558e137551c40cdc05232456d53f551767e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5393432
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1278953}

--- chrome/browser/ui/autofill/payments/desktop_payments_window_manager_test_api.h
+++ chrome/browser/ui/autofill/payments/desktop_payments_window_manager_test_api.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_deref.h"
 #include "chrome/browser/ui/autofill/payments/desktop_payments_window_manager.h"
+#include "ui/gfx/geometry/rect.h"
 
 namespace autofill::payments {
 
@@ -22,7 +23,9 @@ class DesktopPaymentsWindowManagerTestApi {
       const DesktopPaymentsWindowManagerTestApi&) = delete;
   ~DesktopPaymentsWindowManagerTestApi() = default;
 
-  void CreatePopup(const GURL& url) { window_manager_->CreatePopup(url); }
+  void CreatePopup(const GURL& url, gfx::Rect popup_size) {
+    window_manager_->CreatePopup(url, std::move(popup_size));
+  }
 
   void OnVcn3dsAuthenticationResponseReceived(
       AutofillClient::PaymentsRpcResult result,
