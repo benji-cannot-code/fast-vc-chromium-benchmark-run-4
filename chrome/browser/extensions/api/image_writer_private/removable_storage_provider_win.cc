0bae8aa749b69f313933fa1a6a99aaf34a910414
Avi Drissman <avi@chromium.org>
2024-03-27 18:59:00 +0000

    Use base::HeapArray in image_writer
    Fixed: 326458515
    Fixed: 326458575
    Fixed: 326458512
    Fixed: 326458573
    Fixed: 326458647
    Fixed: 326457992
    Fixed: 326458493
    Change-Id: I7470fd577ae42b983bd6ba1c858c645452e4f9d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5398380
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279140}

--- chrome/browser/extensions/api/image_writer_private/removable_storage_provider_win.cc
+++ chrome/browser/extensions/api/image_writer_private/removable_storage_provider_win.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/containers/heap_array.h"
 #include "base/logging.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/strings/string_number_conversions.h"
@@ -111,17 +112,17 @@ bool AddDeviceInfo(HANDLE interface_enumerator,
   query.PropertyId = StorageDeviceProperty;
   query.QueryType = PropertyStandardQuery;
 
-  std::unique_ptr<char[]> output_buf(new char[1024]);
+  auto output_buf = base::HeapArray<char>::WithSize(1024);
   status = DeviceIoControl(
-      device_handle.Get(),            // Device handle.
-      IOCTL_STORAGE_QUERY_PROPERTY,   // Flag to request device properties.
-      &query,                         // Query parameters.
-      sizeof(STORAGE_PROPERTY_QUERY), // query parameters size.
-      output_buf.get(),               // output buffer.
-      1024,                           // Size of buffer.
-      &bytes_returned,                // Number of bytes returned.
-                                      // Must not be null.
-      NULL);                          // Optional unused overlapped perameter.
+      device_handle.Get(),             // Device handle.
+      IOCTL_STORAGE_QUERY_PROPERTY,    // Flag to request device properties.
+      &query,                          // Query parameters.
+      sizeof(STORAGE_PROPERTY_QUERY),  // query parameters size.
+      output_buf.data(),               // output buffer.
+      output_buf.size(),               // Size of buffer.
+      &bytes_returned,                 // Number of bytes returned.
+                                       // Must not be null.
+      nullptr);                        // Optional unused overlapped parameter.
 
   if (status == FALSE) {
     PLOG(ERROR) << "Storage property query failed.";
@@ -129,7 +130,7 @@ bool AddDeviceInfo(HANDLE interface_enumerator,
   }
 
   STORAGE_DEVICE_DESCRIPTOR* device_descriptor =
-      reinterpret_cast<STORAGE_DEVICE_DESCRIPTOR*>(output_buf.get());
+      reinterpret_cast<STORAGE_DEVICE_DESCRIPTOR*>(output_buf.data());
 
   if (!device_descriptor->RemovableMedia &&
       !(device_descriptor->BusType == BusTypeUsb)) {
@@ -165,13 +166,13 @@ bool AddDeviceInfo(HANDLE interface_enumerator,
 
   if (device_descriptor->VendorIdOffset &&
       output_buf[device_descriptor->VendorIdOffset]) {
-    device.vendor.assign(output_buf.get() + device_descriptor->VendorIdOffset);
+    device.vendor.assign(output_buf.data() + device_descriptor->VendorIdOffset);
   }
 
   std::string product_id;
   if (device_descriptor->ProductIdOffset &&
       output_buf[device_descriptor->ProductIdOffset]) {
-    device.model.assign(output_buf.get() + device_descriptor->ProductIdOffset);
+    device.model.assign(output_buf.data() + device_descriptor->ProductIdOffset);
   }
 
   device_list->data.push_back(std::move(device));
