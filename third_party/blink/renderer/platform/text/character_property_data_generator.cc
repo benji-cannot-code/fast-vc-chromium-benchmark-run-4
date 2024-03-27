5edfa44492804fc12a98c5973a72e211f4c56f97
Koji Ishii <kojii@chromium.org>
2024-03-27 16:45:33 +0000

    Fix build when `use_icu_data_file` is `false`
    This patch is a follow up fix for crrev.com/c/5382280 where
    the build fails when the build config `use_icu_data_file` is
    `false`. It's used in the `linux-headless-shell-rel` bot.
    
    Bug: 41485013
    Change-Id: Ice71338dcb8c175cc471b6cf44039deb289f4a04
    Cq-Include-Trybots: luci.chromium.try:linux-headless-shell-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5401294
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279025}

--- third_party/blink/renderer/platform/text/character_property_data_generator.cc
+++ third_party/blink/renderer/platform/text/character_property_data_generator.cc
@@ -35,6 +35,15 @@ namespace {
 #define CHECK_U_ERROR(error, name) \
   CHECK(U_SUCCESS(error)) << name << ": (" << error << ")" << u_errorName(error)
 
+// Check ICU functions that need the data resources are working.
+// https://unicode-org.github.io/icu/userguide/icu/design.html#icu4c-initialization-and-termination
+void CheckIcuDataResources() {
+  UErrorCode error = U_ZERO_ERROR;
+  UVersionInfo version;
+  ulocdata_getCLDRVersion(version, &error);
+  CHECK_U_ERROR(error, "ulocdata_getCLDRVersion");
+}
+
 //
 // Load the ICU data file and set it to the ICU.
 //
@@ -46,7 +55,12 @@ void InitializeIcu(const char* exec_path) {
   path = path.parent_path() / "icudt" U_ICUDATA_TYPE_LETTER ".dat";
 
   std::ifstream data_ifstream(path, std::ios_base::binary);
-  CHECK(data_ifstream.is_open());
+  if (!data_ifstream.is_open()) {
+    // When the build config is `!use_icu_data_file`, the ICU data is built into
+    // the binary.
+    CheckIcuDataResources();
+    return;
+  }
   static std::vector<uint8_t> icu_data;
   CHECK(icu_data.empty());
   std::copy(std::istreambuf_iterator<char>(data_ifstream),
@@ -55,11 +69,7 @@ void InitializeIcu(const char* exec_path) {
   udata_setCommonData(icu_data.data(), &error);
   CHECK_U_ERROR(error, "udata_setCommonData");
 
-  // Check ICU functions that need the data resources are working.
-  // https://unicode-org.github.io/icu/userguide/icu/design.html#icu4c-initialization-and-termination
-  UVersionInfo version;
-  ulocdata_getCLDRVersion(version, &error);
-  CHECK_U_ERROR(error, "ulocdata_getCLDRVersion");
+  CheckIcuDataResources();
 }
 
 class CharacterPropertyValues {
