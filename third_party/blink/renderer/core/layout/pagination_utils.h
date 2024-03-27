a55e57b327399f924ef0355b68afe5012383f260
Morten Stenshorne <mstensho@chromium.org>
2024-03-27 17:05:56 +0000

    Move overflow shrink calculation for printing out of LocalFrameView.
    Layout is a better home for this logic. Move it into a new pagination
    utils file. More functions will be added here shortly, as part of adding
    support for all applicable @page properties.
    
    No behavior changes intended.
    
    Change-Id: I41b461a08085a9bf87474448298e5c08c4c0e9fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5401932
    Auto-Submit: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279048}

---
+++ third_party/blink/renderer/core/layout/pagination_utils.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_PAGINATION_UTILS_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_PAGINATION_UTILS_H_
+
+namespace blink {
+
+class LayoutView;
+
+float CalculateOverflowShrinkForPrinting(const LayoutView&,
+                                         float maximum_shrink_factor);
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_PAGINATION_UTILS_H_
