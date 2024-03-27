494435383e4c54bf22a157b1ffee828cd97234aa
Andrew Paseltiner <apaseltiner@chromium.org>
2024-03-27 19:17:42 +0000

    Make AttributionManagerImpl::NotifyOsRegistration private
    It is only called internally.
    
    Change-Id: Id22fc9eae31f68f5d3bcfc7e4f5ba97c40fe9015
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402197
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279152}

--- content/browser/attribution_reporting/attribution_manager_impl.h
+++ content/browser/attribution_reporting/attribution_manager_impl.h
@@ -161,13 +161,6 @@ class CONTENT_EXPORT AttributionManagerImpl
 
   void HandleOsRegistration(OsRegistration) override;
 
-  void NotifyOsRegistration(base::Time time,
-                            const attribution_reporting::OsRegistrationItem&,
-                            const url::Origin& top_level_origin,
-                            bool is_debug_key_allowed,
-                            attribution_reporting::mojom::RegistrationType,
-                            attribution_reporting::mojom::OsRegistrationResult);
-
  private:
   friend class AttributionManagerImplTest;
 
@@ -239,6 +232,12 @@ class CONTENT_EXPORT AttributionManagerImpl
   void NotifyTotalOsRegistrationFailure(
       const OsRegistration&,
       attribution_reporting::mojom::OsRegistrationResult);
+  void NotifyOsRegistration(base::Time time,
+                            const attribution_reporting::OsRegistrationItem&,
+                            const url::Origin& top_level_origin,
+                            bool is_debug_key_allowed,
+                            attribution_reporting::mojom::RegistrationType,
+                            attribution_reporting::mojom::OsRegistrationResult);
 
   bool IsReportAllowed(const AttributionReport&) const;
 
