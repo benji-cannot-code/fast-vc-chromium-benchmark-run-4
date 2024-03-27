9a96ce8db966035be897d51e789b9a24beabcd40
Colin Blundell <blundell@chromium.org>
2024-03-27 18:51:30 +0000

    [//gpu] Plumb MacOS-specific texture target into Mac SI backing factory
    Now that SharedImageFactory already needs to know this target to give it
    to SharedImageCapabilities, it might as well also give it to
    IOSurfaceImageBackingFactory.
    
    Bug: 41494843
    Change-Id: I2d5254476283e99050404f0a1d592e8627c62463
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402867
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279132}

--- gpu/command_buffer/service/shared_image/iosurface_image_backing_factory.h
+++ gpu/command_buffer/service/shared_image/iosurface_image_backing_factory.h
@@ -40,7 +40,8 @@ class GPU_GLES2_EXPORT IOSurfaceImageBackingFactory
   IOSurfaceImageBackingFactory(GrContextType gr_context_type,
                                int32_t max_texture_size,
                                const gles2::FeatureInfo* feature_info,
-                               gl::ProgressReporter* progress_reporter);
+                               gl::ProgressReporter* progress_reporter,
+                               uint32_t texture_target);
   ~IOSurfaceImageBackingFactory() override;
 
   // SharedImageBackingFactory implementation.
@@ -144,6 +145,8 @@ class GPU_GLES2_EXPORT IOSurfaceImageBackingFactory
   // Used to notify the watchdog before a buffer allocation in case it takes
   // long.
   const raw_ptr<gl::ProgressReporter> progress_reporter_;
+
+  uint32_t texture_target_;
 };
 
 }  // namespace gpu
