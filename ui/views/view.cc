ad409f840f8be8c143444ddc7820f46e8b116d05
jiawei wang <hellojw513@gmail.com>
2024-03-27 18:46:35 +0000

    Widget::DestroyRootView() should not fire an InvalidateLayout()
    InvalidateLayout() triggered by destroying the root view is unnecessary. Blocking it reduces unnecessary calls, including in autosize mode.
    
    Bug: 330830920
    Change-Id: Ib1005c6d1a9e4959fb0d3ae39728f10596bd269b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5394898
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Weidong Liu <weidongliu@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279129}

--- ui/views/view.cc
+++ ui/views/view.cc
@@ -922,6 +922,11 @@ void View::InvalidateLayout() {
     return;
   }
 
+  // There is no need to `InvalidateLayout()` when `Widget::IsClosed()`.
+  if (Widget* widget = GetWidget(); widget && widget->IsClosed()) {
+    return;
+  }
+
   base::AutoReset<bool> invalidating(&invalidating_, true);
   // We should never need to invalidate during a layout call; this tracks
   // how many times that is happening.
@@ -1231,6 +1236,11 @@ void View::SchedulePaint() {
 }
 
 void View::SchedulePaintInRect(const gfx::Rect& rect) {
+  // There is no need to `SchedulePaintInRect()` when `Widget::IsClosed()`.
+  if (Widget* widget = GetWidget(); widget && widget->IsClosed()) {
+    return;
+  }
+
   needs_paint_ = true;
   SchedulePaintInRectImpl(rect);
 }
