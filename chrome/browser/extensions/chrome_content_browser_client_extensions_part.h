962e826abdf29e9bf8c22f803c1fb450bf9501ce
Andrew Rayskiy <greengrape@google.com>
2024-03-27 21:08:53 +0000

    Enable Direct Sockets in Chrome Apps behind a dedicated feature flag
    This flag is off-by-default; it will be flipped in a separate CL once
    all necessary launch approvals are through.
    
    This CL also adds a few tests to validate that bindings are exposed
    correctly depending on the presence of the "sockets" key in the
    manifest.
    
    Bug: b/329445684
    
    Change-Id: I5beec899ea1fb0c41c243e6fee3b25c63c0f8fc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5300256
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279234}

--- chrome/browser/extensions/chrome_content_browser_client_extensions_part.h
+++ chrome/browser/extensions/chrome_content_browser_client_extensions_part.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/auto_reset.h"
+#include "base/feature_list.h"
 #include "base/gtest_prod_util.h"
 #include "chrome/browser/chrome_content_browser_client_parts.h"
 #include "components/download/public/common/quarantine_connection.h"
@@ -43,6 +44,8 @@ class Profile;
 namespace extensions {
 
 BASE_DECLARE_FEATURE(kStopUsingRenderProcessHostPrivilege);
+
+BASE_DECLARE_FEATURE(kDirectSocketsInChromeApps);
 
 // Implements the extensions portion of ChromeContentBrowserClient.
 class ChromeContentBrowserClientExtensionsPart
