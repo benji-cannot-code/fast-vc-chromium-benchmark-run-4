532eed65b66271d1838a9d8edf6a9e1db94118d9
Aaron Leventhal <aleventhal@chromium.org>
2024-03-27 23:14:59 +0000

    [A11y] Move 5 methods out of AXLayoutObject
    Bug: 40699001
    Change-Id: I8fed63f6c0af5bb2afbb7a0d8e283bbec8d119d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402422
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279317}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.h
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.h
@@ -138,6 +138,8 @@ class MODULES_EXPORT AXNodeObject : public AXObject {
   bool IsNativeSlider() const override;
   bool IsNativeSpinButton() const override;
   bool IsEmbeddingElement() const override;
+  bool IsLinked() const override;
+  bool IsVisited() const override;
 
   // Check object state.
   bool IsClickable() const final;
@@ -146,6 +148,7 @@ class MODULES_EXPORT AXNodeObject : public AXObject {
   AccessibilitySelectedState IsSelected() const override;
   bool IsSelectedFromFocusSupported() const override;
   bool IsSelectedFromFocus() const override;
+  bool IsNotUserSelectable() const override;
   bool IsRequired() const final;
   bool IsControl() const override;
   AXRestriction Restriction() const override;
@@ -164,6 +167,7 @@ class MODULES_EXPORT AXNodeObject : public AXObject {
   int HeadingLevel() const final;
   unsigned HierarchicalLevel() const final;
   void SerializeMarkerAttributes(ui::AXNodeData* node_data) const override;
+  ax::mojom::blink::ListStyle GetListStyle() const final;
   AXObject* InPageLinkTarget() const override;
   const AtomicString& EffectiveTarget() const override;
   AccessibilityOrientation Orientation() const override;
