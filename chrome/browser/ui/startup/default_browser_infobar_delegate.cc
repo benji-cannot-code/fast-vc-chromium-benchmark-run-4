07628e4d57514433c7c6c565a12d3fcfa1a9ddbd
Charles Meng <charlesmeng@chromium.org>
2024-03-27 21:21:58 +0000

    [DefaultPrompt] Prevent updated copy from showing when flag is disabled
    Previously, the feature param check did not first check if the feature
    was enabled. The assumption was that it would return false if the
    feature was disabled, but actually it returns the default value, true.
    This CL adds a check to the feature flag before checking the param.
    
    Fixed: 331660130
    Change-Id: I1d48907f632981d4e7ffffd46762222d9cd9d098
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402266
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Auto-Submit: Charles Meng <charlesmeng@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279250}

--- chrome/browser/ui/startup/default_browser_infobar_delegate.cc
+++ chrome/browser/ui/startup/default_browser_infobar_delegate.cc
@@ -90,7 +90,8 @@ void DefaultBrowserInfoBarDelegate::InfoBarDismissed() {
 }
 
 std::u16string DefaultBrowserInfoBarDelegate::GetMessageText() const {
-  if (features::kUpdatedInfoBarCopy.Get()) {
+  if (base::FeatureList::IsEnabled(features::kDefaultBrowserPromptRefresh) &&
+      features::kUpdatedInfoBarCopy.Get()) {
     return l10n_util::GetStringUTF16(IDS_DEFAULT_BROWSER_INFOBAR_REFRESH_TEXT);
   }
   return l10n_util::GetStringUTF16(IDS_DEFAULT_BROWSER_INFOBAR_TEXT);
@@ -103,7 +104,8 @@ int DefaultBrowserInfoBarDelegate::GetButtons() const {
 std::u16string DefaultBrowserInfoBarDelegate::GetButtonLabel(
     InfoBarButton button) const {
   DCHECK_EQ(BUTTON_OK, button);
-  if (features::kUpdatedInfoBarCopy.Get()) {
+  if (base::FeatureList::IsEnabled(features::kDefaultBrowserPromptRefresh) &&
+      features::kUpdatedInfoBarCopy.Get()) {
     return l10n_util::GetStringUTF16(
         IDS_DEFAULT_BROWSER_INFOBAR_REFRESH_OK_BUTTON_LABEL);
   }
