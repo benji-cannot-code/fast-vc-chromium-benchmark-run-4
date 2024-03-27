e41430ce8e59c37b04e6a4dc0f861ac73e269e03
Dana Fried <dfried@chromium.org>
2024-03-27 22:54:13 +0000

    Add more recent sessions infrastructure
    Adds a tracker to the UserEducationService subject to a kill switch.
    Adds the ability to listen to recent sessions data updates.
    Adds thorough testing for these features.
    
    Change-Id: I79559b97e9698446ad88a0973e7b12c0be1729d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5394730
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279304}

--- chrome/browser/user_education/recent_session_tracker.h
+++ chrome/browser/user_education/recent_session_tracker.h
@@ -6,7 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_USER_EDUCATION_RECENT_SESSION_TRACKER_H_
 #define CHROME_BROWSER_USER_EDUCATION_RECENT_SESSION_TRACKER_H_
 
+#include <optional>
+
 #include "base/callback_list.h"
+#include "base/functional/callback_forward.h"
 #include "base/memory/raw_ref.h"
 #include "base/time/time.h"
 #include "chrome/browser/user_education/browser_feature_promo_storage_service.h"
@@ -24,6 +27,10 @@ class RecentSessionTracker {
   static constexpr int kMaxRecentSessionRecords = 12;
   static constexpr base::TimeDelta kMaxRecentSessionRetention = base::Days(60);
 
+  // Callback that provides updated recent session data to observers.
+  using RecentSessionsUpdatedCallback =
+      base::RepeatingCallback<void(const RecentSessionData&)>;
+
   RecentSessionTracker(
       user_education::FeaturePromoSessionManager& session_manager,
       user_education::FeaturePromoStorageService& feature_promo_storage,
@@ -31,6 +38,17 @@ class RecentSessionTracker {
   RecentSessionTracker(const RecentSessionTracker&) = delete;
   void operator=(const RecentSessionTracker&) = delete;
   ~RecentSessionTracker();
+
+  // Add a callback that observes when recent sessions have been updated.
+  // Will be called immediately if there has already been an update since the
+  // current application started.
+  base::CallbackListSubscription AddRecentSessionsUpdatedCallback(
+      RecentSessionsUpdatedCallback callback);
+
+  const std::optional<RecentSessionData>& recent_session_data_for_testing()
+      const {
+    return recent_session_data_;
+  }
 
  private:
   // Called when a new User Education session starts.
@@ -40,6 +58,11 @@ class RecentSessionTracker {
   const raw_ref<user_education::FeaturePromoStorageService>
       feature_promo_storage_;
   const raw_ref<RecentSessionDataStorageService> recent_session_storage_;
+  base::RepeatingCallbackList<RecentSessionsUpdatedCallback::RunType>
+      recent_sessions_updated_callback_;
+
+  // Cache this data so that observers have access to it.
+  std::optional<RecentSessionData> recent_session_data_;
 };
 
 #endif  // CHROME_BROWSER_USER_EDUCATION_RECENT_SESSION_TRACKER_H_
--- chrome/browser/user_education/user_education_service.h
+++ chrome/browser/user_education/user_education_service.h
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/feature_list.h"
+#include "chrome/browser/user_education/browser_feature_promo_storage_service.h"
 #include "chrome/browser/user_education/browser_tutorial_service.h"
+#include "chrome/browser/user_education/recent_session_tracker.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/user_education/common/feature_promo_registry.h"
 #include "components/user_education/common/feature_promo_session_manager.h"
@@ -22,6 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_education/common/tutorial_registry.h"
 #include "content/public/browser/browser_context.h"
 
+// Kill switch for recent session tracking. Enabled by default.
+BASE_DECLARE_FEATURE(kAllowRecentSessionTracking);
+
 extern const char kTabGroupTutorialId[];
 extern const char kSavedTabGroupTutorialId[];
 extern const char kSidePanelCustomizeChromeTutorialId[];
@@ -31,8 +36,7 @@ extern const char kPasswordManagerTutorialId[];
 class UserEducationService : public KeyedService {
  public:
   explicit UserEducationService(
-      std::unique_ptr<user_education::FeaturePromoStorageService>
-          storage_service,
+      std::unique_ptr<BrowserFeaturePromoStorageService> storage_service,
       bool allows_promos);
   ~UserEducationService() override;
 
@@ -66,6 +70,9 @@ class UserEducationService : public KeyedService {
   user_education::NewBadgeController* new_badge_controller() {
     return new_badge_controller_.get();
   }
+  RecentSessionTracker* recent_session_tracker() {
+    return recent_session_tracker_.get();
+  }
 
   // Utility methods for when a browser [window] isn't available; for example,
   // when only a WebContents is available:
@@ -86,13 +93,14 @@ class UserEducationService : public KeyedService {
   user_education::FeaturePromoRegistry feature_promo_registry_;
   BrowserTutorialService tutorial_service_;
   user_education::ProductMessagingController product_messaging_controller_;
-  std::unique_ptr<user_education::FeaturePromoStorageService>
+  std::unique_ptr<BrowserFeaturePromoStorageService>
       feature_promo_storage_service_;
   user_education::FeaturePromoSessionManager feature_promo_session_manager_;
   std::unique_ptr<user_education::FeaturePromoSessionPolicy>
       feature_promo_session_policy_;
   std::unique_ptr<user_education::NewBadgeRegistry> new_badge_registry_;
   std::unique_ptr<user_education::NewBadgeController> new_badge_controller_;
+  std::unique_ptr<RecentSessionTracker> recent_session_tracker_;
 };
 
 #endif  // CHROME_BROWSER_USER_EDUCATION_USER_EDUCATION_SERVICE_H_
