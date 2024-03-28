fa9a71976ff2ba7cc128974dfc4a68578d96c283
Rohit Rao <rohitrao@chromium.org>
2024-03-28 14:03:27 +0000

    Adds OS macros and buildflags for visionOS/watchOS.
    Updates build_config.h to support the visionOS and watchOS
    platforms. With this change, IS_IOS becomes both an individual
    platform as well as a family of platforms (iOS, Mac Catalyst, watchOS,
    visionOS). The child platform macros are named accordingly, for
    example IS_IOS_VISION.
    
    We considered creating a new IS_APPLEMOBILE family instead, but felt
    that updating every existing use of IS_IOS would be too invasive at
    this stage.
    
    Bug: 331226142, 331320406
    Change-Id: I975b48dfca70c2c02358e17c3298fa80fef27f67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5396396
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279614}

--- build/build_config.h
+++ build/build_config.h
@@ -17,13 +17,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 //  Operating System:
 //    IS_AIX / IS_ANDROID / IS_ASMJS / IS_CHROMEOS / IS_FREEBSD / IS_FUCHSIA /
-//    IS_IOS / IS_IOS_MACCATALYST / IS_LINUX / IS_MAC / IS_NACL / IS_NETBSD /
-//    IS_OPENBSD / IS_QNX / IS_SOLARIS / IS_WIN
+//    IS_IOS / IS_IOS_MACCATALYST / IS_IOS_VISION / IS_IOS_WATCH / IS_LINUX /
+//    IS_MAC / IS_NACL / IS_NETBSD / IS_OPENBSD / IS_QNX / IS_SOLARIS / IS_WIN
 //  Operating System family:
-//    IS_APPLE: IOS or MAC or IOS_MACCATALYST
+//    IS_APPLE: MAC or IOS or IOS_MACCATALYST or IOS_VISION or IOS_WATCH
+//    IS_IOS: IOS or IOS_MACCATALYST or IOS_VISION or IOS_WATCH
 //    IS_BSD: FREEBSD or NETBSD or OPENBSD
-//    IS_POSIX: AIX or ANDROID or ASMJS or CHROMEOS or FREEBSD or IOS or LINUX
-//              or MAC or NACL or NETBSD or OPENBSD or QNX or SOLARIS
+//    IS_POSIX: AIX or ANDROID or ASMJS or CHROMEOS or FREEBSD or IOS
+//              or IOS_MACCATALYST or IOS_VISION or IOS_WATCH or LINUX or MAC
+//              or NACL or NETBSD or OPENBSD or QNX or SOLARIS
 
 // This file also adds defines specific to the platform, architecture etc.
 //
@@ -73,6 +75,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(TARGET_OS_MACCATALYST) && TARGET_OS_MACCATALYST
 #define OS_IOS_MACCATALYST
 #endif  // defined(TARGET_OS_MACCATALYST) && TARGET_OS_MACCATALYST
+#if defined(TARGET_OS_VISION) && TARGET_OS_VISION
+#define OS_IOS_VISION 1
+#endif  // defined(TARGET_OS_VISION) && TARGET_OS_VISION
+#if defined(TARGET_OS_WATCH) && TARGET_OS_WATCH
+#define OS_IOS_WATCH 1
+#endif  // defined(TARGET_OS_WATCH) && TARGET_OS_WATCH
 #else
 #define OS_MAC 1
 #endif  // defined(TARGET_OS_IPHONE) && TARGET_OS_IPHONE
@@ -193,6 +201,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BUILDFLAG_INTERNAL_IS_IOS_MACCATALYST() (1)
 #else
 #define BUILDFLAG_INTERNAL_IS_IOS_MACCATALYST() (0)
+#endif
+
+#if defined(OS_IOS_VISION)
+#define BUILDFLAG_INTERNAL_IS_IOS_VISION() (1)
+#else
+#define BUILDFLAG_INTERNAL_IS_IOS_VISION() (0)
+#endif
+
+#if defined(OS_IOS_WATCH)
+#define BUILDFLAG_INTERNAL_IS_IOS_WATCH() (1)
+#else
+#define BUILDFLAG_INTERNAL_IS_IOS_WATCH() (0)
 #endif
 
 #if defined(OS_LINUX)
