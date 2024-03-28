a57411eaaa2d0d41db09131406801d6bc05d06e3
Nate Fischer <ntfschr@chromium.org>
2024-03-28 04:24:44 +0000

    python: switch to logging.warning
    No change to logic. logging.warn() is deprecated. Switch to
    logging.warning().
    
    This CL was uploaded by git cl split.
    
    R=chuhsuan@chromium.org
    
    Bug: 40701320
    Change-Id: I3ca90237da57bb6bfb4e017fc0f8d4ee8fe05449
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402049
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Chu-Hsuan Yang <chuhsuan@chromium.org>
    Reviewed-by: Chu-Hsuan Yang <chuhsuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279463}

--- ash/webui/camera_app_ui/resources/utils/cca/commands/bundle.py
+++ ash/webui/camera_app_ui/resources/utils/cca/commands/bundle.py
@@ -117,7 +117,7 @@ def cmd(board: str) -> int:
                 to_output_files.remove(path)
 
     if to_output_files:
-        logging.warn(
+        logging.warning(
             f"Some files are not covered by route: {to_output_files}.")
 
     return 0
