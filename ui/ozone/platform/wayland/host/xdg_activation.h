7fe64954b2a15a80b3560c17e64930859d1e116c
Orko Garai <orko@igalia.com>
2024-03-28 02:41:46 +0000

    [ozone/wayland] Request activation token from sequenced task
    Ensure token request is initiated from the same thread where
    XdgActivation is created. This avoids race conditions as well as ensures
    a valid sequence exists for the timer.
    
    Bug: 330966550, 40747285
    Change-Id: Ied20983d75f520404d0beb7f6cb3b64cb8cd5f48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5397336
    Reviewed-by: Max Ihlenfeldt <max@igalia.com>
    Commit-Queue: Orko Garai <orko@igalia.com>
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1279424}

--- ui/ozone/platform/wayland/host/xdg_activation.h
+++ ui/ozone/platform/wayland/host/xdg_activation.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/nix/xdg_util.h"
+#include "base/task/sequenced_task_runner.h"
 #include "ui/ozone/platform/wayland/common/wayland_object.h"
 
 namespace ui {
@@ -60,6 +61,11 @@ class XdgActivation : public wl::GlobalObjectRegistrar<XdgActivation> {
   mutable base::queue<std::unique_ptr<TokenRequest>> token_request_queue_;
 
   const raw_ptr<WaylandConnection> connection_;
+
+  scoped_refptr<base::SequencedTaskRunner> task_runner_ =
+      base::SequencedTaskRunner::GetCurrentDefault();
+
+  base::WeakPtrFactory<XdgActivation> weak_ptr_factory_{this};
 };
 
 }  // namespace ui
