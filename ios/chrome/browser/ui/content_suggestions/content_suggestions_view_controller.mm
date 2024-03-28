12135b32c5eb29a22d8ce31cb332676fce13f319
Scott Yoder <scottyoder@google.com>
2024-03-28 23:08:21 +0000

    [ios] Fix NTP flicker when asynchronously loading MVTs
    Change-Id: I5cf9643c3aa1e5ba8d95a353c90760c843fc36e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402470
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Auto-Submit: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279990}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.mm
@@ -559,6 +559,7 @@ const float kMagicStackReplaceModuleFadeAnimationDistance = 50;
         [self.mostVisitedModuleContainer.centerXAnchor
             constraintEqualToAnchor:self.view.centerXAnchor],
       ]];
+      [self.view layoutIfNeeded];
     }
 }
 
