a32b9bd7cb4e9eb13ca83742a21a34abe5ba00c6
Avi Drissman <avi@chromium.org>
2024-03-28 17:05:20 +0000

    Use HeapArray for content/browser/browsing_data
    Fixed: 326458556
    Change-Id: Id8e6473849519742f343bdd703c5b3992ac053e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5400749
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279744}

--- content/browser/browsing_data/browsing_data_browsertest_utils.cc
+++ content/browser/browsing_data/browsing_data_browsertest_utils.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/command_line.h"
+#include "base/containers/heap_array.h"
+#include "base/containers/span.h"
 #include "base/functional/bind.h"
 #include "base/run_loop.h"
 #include "components/network_session_configurator/common/network_switches.h"
@@ -146,11 +148,12 @@ void SetResponseContent(const GURL& url,
     base::FilePath path(GetTestFilePath("browsing_data", value->c_str()));
     base::File file(path, base::File::FLAG_OPEN | base::File::FLAG_READ);
     EXPECT_TRUE(file.IsValid());
+
     int64_t length = file.GetLength();
     EXPECT_GE(length, 0);
-    std::unique_ptr<char[]> buffer(new char[length + 1]);
-    file.Read(0, buffer.get(), length);
-    buffer[length] = '\0';
+
+    auto buffer = base::HeapArray<uint8_t>::WithSize(length);
+    file.Read(0, buffer);
 
     if (path.Extension() == FILE_PATH_LITERAL(".js"))
       response->set_content_type("application/javascript");
@@ -159,7 +162,7 @@ void SetResponseContent(const GURL& url,
     else
       NOTREACHED();
 
-    response->set_content(buffer.get());
+    response->set_content(base::as_string_view(buffer));
   }
 }
 
