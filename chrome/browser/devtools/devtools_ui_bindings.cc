8e16ad8c0a5ec5a236597c05fe15b91699a52458
Wolfgang Beyer <wolfi@chromium.org>
2024-03-28 13:15:28 +0000

    [DevTools] Add base::Feature for whether console insights setting is visible
    This CL introduces a new base::Feature
    `kDevToolsConsoleInsightsSettingVisible`. If disabled, DevTools should
    not show the console insights setting. If enabled, DevTools should
    show the setting. And if the `bocked_reason` is present, DevTools
    should show the setting, but disable it and show the reason why it is
    disabled.
    
    Drive-by: remove geo-blocking from Chrome, this will be handled by
    finch.
    
    Bug: chromium:299423255
    Change-Id: Ib928cb4a17917c34a6f47166e36a4720a3ce2ced
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402140
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279590}

--- chrome/browser/devtools/devtools_ui_bindings.cc
+++ chrome/browser/devtools/devtools_ui_bindings.cc
@@ -381,7 +381,9 @@ std::string SanitizeFrontendQueryParam(
 
   if (base::FeatureList::IsEnabled(::features::kDevToolsConsoleInsights) ||
       base::FeatureList::IsEnabled(
-          ::features::kDevToolsConsoleInsightsDogfood)) {
+          ::features::kDevToolsConsoleInsightsDogfood) ||
+      base::FeatureList::IsEnabled(
+          ::features::kDevToolsConsoleInsightsSettingVisible)) {
     if (key == "enableAida" && value == "true") {
       return value;
     }
@@ -402,6 +404,10 @@ std::string SanitizeFrontendQueryParam(
   }
 
   if (key == "ci_blockedByGeo" && value == "true") {
+    return value;
+  }
+
+  if (key == "ci_blockedByRollout" && value == "true") {
     return value;
   }
 
