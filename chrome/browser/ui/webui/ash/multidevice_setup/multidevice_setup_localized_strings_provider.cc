065d9af993e4a1d7be8fc77dbe2ee28ef0fa4304
Ryan Hansberry <hansberry@chromium.org>
2024-03-28 18:12:19 +0000

    [Multidevice] Adjust onboarding dialog footnote copy.
    Screenshot: https://screenshot.googleplex.com/7HhbUbPSMqTCofj
    
    Fixed: 328602938
    TEST: Manually confirmed that the new copy displays correctly.
    Change-Id: I0628117c9ecbf45d067a493b5c6035d0f5a7320f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402179
    Auto-Submit: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279809}

--- chrome/browser/ui/webui/ash/multidevice_setup/multidevice_setup_localized_strings_provider.cc
+++ chrome/browser/ui/webui/ash/multidevice_setup/multidevice_setup_localized_strings_provider.cc
@@ -50,6 +50,7 @@ constexpr webui::LocalizedString kLocalizedStringsWithoutPlaceholders[] = {
      IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_SINGLE_DEVICE_HEADER},
     {"startSetupPageOfflineDeviceOption",
      IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_OFFLINE_DEVICE_OPTION},
+    {"startSetupPageFootnote", IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_FOOTNOTE},
     {"startSetupPageFeatureMirrorPhoneNotifications",
      IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_MIRROR_PHONE_NOTIFICATIONS},
     {"startSetupPageFeatureWifiSyncTitle",
@@ -112,12 +113,6 @@ GetLocalizedStringsWithPlaceholders() {
                     GetBoardSpecificBetterTogetherSuiteLearnMoreUrl().spec())));
 
         localized_strings.emplace_back(
-            "startSetupPageFootnote",
-            l10n_util::GetStringFUTF16(
-                IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_FOOTNOTE,
-                kFootnoteMarker));
-
-        localized_strings.emplace_back(
             "startSetupPageFeatureListAwm",
             l10n_util::GetStringFUTF16(
                 IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_AWM_DESCRIPTION,
@@ -168,10 +163,6 @@ void AddLocalizedValuesToBuilder(::login::LocalizedValuesBuilder* builder) {
                 ui::GetChromeOSDeviceName(), kFootnoteMarker,
                 base::UTF8ToUTF16(
                     GetBoardSpecificBetterTogetherSuiteLearnMoreUrl().spec()));
-
-  builder->AddF("startSetupPageFootnote",
-                IDS_MULTIDEVICE_SETUP_START_SETUP_PAGE_FOOTNOTE,
-                kFootnoteMarker);
 
   builder->AddF(
       "startSetupPageFeatureListAwm",
