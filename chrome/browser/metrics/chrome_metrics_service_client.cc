33e6c1950ad1deb3871378e03981daa4dbac5a50
Shengsong Tan <sstan@chromium.org>
2024-03-28 03:54:36 +0000

    arc: Add metric for ARCVM vmm swap status
    This CL add a boolean metric for ARCVM, collect the ARCVM swap status
    during each uma report period, in order to create filter on uma
    dashboard in follow steps.
    
    BUG=b:315721196
    TEST=ChromeMetricsServiceClientTest
    
    Change-Id: I0f57dc3fc339cb958e3121f033a3210dd09ce1aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5313081
    Reviewed-by: Alexandre Marciano Gimenez <raging@google.com>
    Commit-Queue: Shengsong Tan <sstan@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279452}

--- chrome/browser/metrics/chrome_metrics_service_client.cc
+++ chrome/browser/metrics/chrome_metrics_service_client.cc
@@ -158,6 +158,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ash/constants/ash_features.h"
 #include "base/feature_list.h"
+#include "chrome/browser/ash/arc/vmm/vmm_metrics_provider.h"
 #include "chrome/browser/ash/login/demo_mode/demo_session.h"
 #include "chrome/browser/ash/printing/printer_metrics_provider.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
@@ -960,6 +961,8 @@ void ChromeMetricsServiceClient::RegisterMetricsServiceProviders() {
       std::make_unique<ash::settings::OsSettingsMetricsProvider>());
   metrics_service_->RegisterMetricsProvider(
       std::make_unique<WallpaperMetricsProvider>());
+  metrics_service_->RegisterMetricsProvider(
+      std::make_unique<arc::VmmMetricsProvider>());
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 #if !BUILDFLAG(IS_CHROMEOS_ASH)
--- chrome/browser/metrics/chrome_metrics_service_client_unittest.cc
+++ chrome/browser/metrics/chrome_metrics_service_client_unittest.cc
@@ -249,8 +249,9 @@ TEST_F(ChromeMetricsServiceClientTest, TestRegisterMetricsServiceProviders) {
   // PersonalizationAppThemeMetricsProvider, PrinterMetricsProvider,
   // FamilyUserMetricsProvider, FamilyLinkUserMetricsProvider,
   // UpdateEngineMetricsProvider, OsSettingsMetricsProvider,
-  // UserTypeByDeviceTypeMetricsProvider, and WallpaperMetricsProvider.
-  expected_providers += 14;
+  // UserTypeByDeviceTypeMetricsProvider, WallpaperMetricsProvider,
+  // and VmmMetricsProvider.
+  expected_providers += 15;
 
   // StructuredMetricsProvider.
   if (!base::FeatureList::IsEnabled(
