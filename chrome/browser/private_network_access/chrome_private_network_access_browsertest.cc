61b67aedf62e5ac787f0bda2eb78b412fadb2d57
Helmut Januschka <helmut@januschka.com>
2024-03-28 12:11:24 +0000

    private_network_access: base::StringPiece -> std::string_view
    Bug: 691162
    Change-Id: I4c1f87c3a167f07762c827982aa4f98663e63e10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372199
    Commit-Queue: Helmut Januschka <helmut@januschka.com>
    Reviewed-by: Jonathan Hao <phao@chromium.org>
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279570}

--- chrome/browser/private_network_access/chrome_private_network_access_browsertest.cc
+++ chrome/browser/private_network_access/chrome_private_network_access_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <string>
+#include <string_view>
 
 #include "base/memory/ref_counted.h"
 #include "base/test/bind.h"
@@ -58,7 +59,7 @@ using ::testing::Return;
 
 using Type = NewAcceptedDeviceType;
 
-constexpr base::StringPiece kUserAcceptedPrivateNetworkDeviceHistogramName =
+constexpr std::string_view kUserAcceptedPrivateNetworkDeviceHistogramName =
     "Security.PrivateNetworkAccess.PermissionNewAcceptedDeviceType";
 
 constexpr char kPrivateHost[] = "a.test";
--- chrome/browser/private_network_access/private_network_device_permission_context_unittest.cc
+++ chrome/browser/private_network_access/private_network_device_permission_context_unittest.cc
@@ -5,7 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/private_network_access/private_network_device_permission_context.h"
 
-#include "base/strings/string_piece.h"
+#include <string_view>
+
 #include "base/test/metrics/histogram_tester.h"
 #include "chrome/browser/private_network_access/private_network_device_permission_context_factory.h"
 #include "chrome/test/base/testing_profile.h"
@@ -23,9 +24,9 @@ namespace {
 using Validity = PrivateNetworkDeviceValidity;
 using Type = NewAcceptedDeviceType;
 
-constexpr base::StringPiece kPrivateNetworkDeviceValidityHistogramName =
+constexpr std::string_view kPrivateNetworkDeviceValidityHistogramName =
     "Security.PrivateNetworkAccess.PermissionDeviceValidity";
-constexpr base::StringPiece kUserAcceptedPrivateNetworkDeviceHistogramName =
+constexpr std::string_view kUserAcceptedPrivateNetworkDeviceHistogramName =
     "Security.PrivateNetworkAccess.PermissionNewAcceptedDeviceType";
 
 class PrivateNetworkDevicePermissionContextTest : public testing::Test {
