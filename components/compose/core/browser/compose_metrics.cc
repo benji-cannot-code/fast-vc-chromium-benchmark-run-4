9f5c5e829c0f9c8b818e2baef9b0e1c32db71e8f
Cathy Li <chili@chromium.org>
2024-03-28 18:09:51 +0000

    Updates selectAllText criteria per live review discussion
    - Adjust threshold to 1 word
    - Remove the extraneous whitespace formatting around content editables
    - - this is done by re-grabbing the form data after select all is done.
    
    Bug: b/328635977
    Change-Id: I3291dd71821808dbdedd2952b79d50cafcf87c98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5352192
    Commit-Queue: Cathy Li <chili@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279806}

--- components/compose/core/browser/compose_metrics.cc
+++ components/compose/core/browser/compose_metrics.cc
@@ -50,6 +50,7 @@ const char kInnerTextNodeOffsetFound[] =
 const char kComposeContextMenuCtr[] = "Compose.ContextMenu.CTR";
 const char kOpenComposeDialogResult[] =
     "Compose.ContextMenu.OpenComposeDialogResult";
+const char kComposeSelectAll[] = "Compose.ContextMenu.SelectedAll";
 
 namespace {
 
@@ -478,6 +479,10 @@ void LogComposeRequestFeedback(EvalLocation eval_location,
       base::StrCat(
           {"Compose.", EvalLocationString(eval_location), ".Request.Feedback"}),
       feedback);
+}
+
+void LogComposeSelectAllStatus(ComposeSelectAllStatus select_all_status) {
+  base::UmaHistogramEnumeration(kComposeSelectAll, select_all_status);
 }
 
 }  // namespace compose
