c6e90e4569eacab77ec4bdec2a8e48b8d77656a4
Avi Drissman <avi@chromium.org>
2024-03-28 15:41:22 +0000

    Use base::HeapArray in content/browser/file_system
    Fixed: 326459006
    Fixed: 326459247
    Change-Id: Ib8d00110c4e92a55f642964d38c27b23808b2b0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402412
    Reviewed-by: Christine Hollingsworth <christinesm@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Christine Hollingsworth <christinesm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279676}

--- content/browser/file_system/file_system_url_loader_factory_browsertest.cc
+++ content/browser/file_system/file_system_url_loader_factory_browsertest.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/command_line.h"
+#include "base/containers/heap_array.h"
+#include "base/containers/span.h"
 #include "base/feature_list.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
@@ -80,14 +82,14 @@ namespace {
 enum class TestMode { kRegular, kIncognito };
 
 // We always use the TEMPORARY FileSystem in these tests.
-const char kFileSystemURLPrefix[] = "filesystem:http://remote/temporary/";
+constexpr char kFileSystemURLPrefix[] = "filesystem:http://remote/temporary/";
 
-const char kValidExternalMountPoint[] = "mnt_name";
+constexpr char kValidExternalMountPoint[] = "mnt_name";
 
-const char kTestFileData[] = "0123456789";
+constexpr char kTestFileData[] = "0123456789";
 
-void FillBuffer(char* buffer, size_t len) {
-  base::RandBytes(buffer, len);
+void FillBuffer(base::span<uint8_t> buffer) {
+  base::RandBytes(buffer);
 }
 
 // An auto mounter that will try to mount anything for `storage_domain` =
@@ -296,8 +298,7 @@ class FileSystemURLLoaderFactoryTest
   }
 
   void WriteFile(const base::StringPiece& file_name,
-                 const char* buf,
-                 int buf_size) {
+                 base::span<const uint8_t> buf) {
     FileSystemURL url;
     url = file_system_context_->CreateCrackedFileSystemURL(
         blink::StorageKey::CreateFromStringForTesting("http://remote"),
@@ -307,11 +308,13 @@ class FileSystemURLLoaderFactoryTest
     base::File::Error result = base::File::FILE_OK;
     base::FilePath local_path;
     base::ScopedTempDir dir;
-    if (!dir.CreateUniqueTempDir())
+    if (!dir.CreateUniqueTempDir()) {
       result = base::File::FILE_ERROR_FAILED;
+    }
     local_path = dir.GetPath().AppendASCII("tmp");
-    if (!base::WriteFile(local_path, base::StringPiece(buf, buf_size)))
+    if (!base::WriteFile(local_path, buf)) {
       result = base::File::FILE_ERROR_FAILED;
+    }
     EXPECT_EQ(base::File::FILE_OK, result);
 
     base::RunLoop loop;
@@ -716,7 +719,9 @@ IN_PROC_BROWSER_TEST_P(FileSystemURLLoaderFactoryTest, AutoMountNoHandler) {
 
 IN_PROC_BROWSER_TEST_P(FileSystemURLLoaderFactoryTest, FileTest) {
   base::ScopedAllowBlockingForTesting allow_blocking;
-  WriteFile("file1.dat", kTestFileData, std::size(kTestFileData) - 1);
+  WriteFile(
+      "file1.dat",
+      base::as_byte_span(kTestFileData).first(std::size(kTestFileData) - 1));
   auto client = TestLoad(CreateFileSystemURL("file1.dat"));
 
   EXPECT_TRUE(client->has_received_response());
@@ -742,7 +747,9 @@ IN_PROC_BROWSER_TEST_P(FileSystemURLLoaderFactoryTest, FileTestDlp) {
   EXPECT_CALL(scoped_file_access_delegate, CreateFileAccessCallback)
       .WillOnce(::testing::Return(fileAccessCallback.Get()));
 
-  WriteFile("file1.dat", kTestFileData, std::size(kTestFileData) - 1);
+  WriteFile(
+      "file1.dat",
+      base::as_byte_span(kTestFileData).first(std::size(kTestFileData) - 1));
   auto client =
       TestLoadWithInitiator(CreateFileSystemURL("file1.dat"),
                             url::Origin::Create(GURL("https://example.com")));
@@ -766,7 +773,9 @@ IN_PROC_BROWSER_TEST_P(FileSystemURLLoaderFactoryTest, CrossOriginFileBlocked) {
   IsolateAllSitesForTesting(base::CommandLine::ForCurrentProcess());
 
   base::ScopedAllowBlockingForTesting allow_blocking;
-  WriteFile("file1.dat", kTestFileData, std::size(kTestFileData) - 1);
+  WriteFile(
+      "file1.dat",
+      base::as_byte_span(kTestFileData).first(std::size(kTestFileData) - 1));
 
   // Navigate main frame to foo.com.
   ASSERT_TRUE(embedded_test_server()->Start());
@@ -790,14 +799,14 @@ IN_PROC_BROWSER_TEST_P(FileSystemURLLoaderFactoryTest,
   // that large files are properly read in chunks and not entirely into
   // memory.
   const size_t buffer_size = 180'000;
-  std::unique_ptr<char[]> buffer(new char[buffer_size]);
-  FillBuffer(buffer.get(), buffer_size);
-  WriteFile("bigfile", buffer.get(), buffer_size);
+  auto buffer = base::HeapArray<uint8_t>::Uninit(buffer_size);
+  FillBuffer(buffer);
+  WriteFile("bigfile", buffer);
 
   const size_t first_byte_position = 500;
   const size_t last_byte_position = buffer_size - first_byte_position;
-  std::string partial_buffer_string(buffer.get() + first_byte_position,
-                                    buffer.get() + last_byte_position + 1);
+  base::span<uint8_t> partial_buffer = buffer.subspan(
+      first_byte_position, last_byte_position - first_byte_position + 1);
 
   net::HttpRequestHeaders headers;
   headers.SetHeader(
@@ -810,20 +819,21 @@ IN_PROC_BROWSER_TEST_P(FileSystemURLLoaderFactoryTest,
   std::string response_text = ReadDataPipe(client->response_body_release());
   client->RunUntilComplete();
   EXPECT_TRUE(client->has_received_completion());
-  EXPECT_TRUE(partial_buffer_string == response_text);
+  // Don't use EXPECT_EQ, it will print out a lot of garbage if check failed.
+  EXPECT_TRUE(partial_buffer == base::as_byte_span(response_text));
 }
 
 IN_PROC_BROWSER_TEST_P(FileSystemURLLoaderFactoryTest,
                        FileTestHalfSpecifiedRange) {
   base::ScopedAllowBlockingForTesting allow_blocking;
   const size_t buffer_size = 4000;
-  std::unique_ptr<char[]> buffer(new char[buffer_size]);
-  FillBuffer(buffer.get(), buffer_size);
-  WriteFile("bigfile", buffer.get(), buffer_size);
+  auto buffer = base::HeapArray<uint8_t>::Uninit(buffer_size);
+  FillBuffer(buffer);
+  WriteFile("bigfile", buffer);
 
   const size_t first_byte_position = 500;
-  std::string partial_buffer_string(buffer.get() + first_byte_position,
-                                    buffer.get() + buffer_size);
+  base::span<uint8_t> partial_buffer =
+      buffer.subspan(first_byte_position, buffer_size - first_byte_position);
 
   net::HttpRequestHeaders headers;
   headers.SetHeader(
@@ -832,15 +842,17 @@ IN_PROC_BROWSER_TEST_P(FileSystemURLLoaderFactoryTest,
   auto client = TestLoadWithHeaders(CreateFileSystemURL("bigfile"), &headers);
   ASSERT_TRUE(client->has_received_response());
   EXPECT_TRUE(client->has_received_completion());
-  // Don't use EXPECT_EQ, it will print out a lot of garbage if check failed.
   std::string response_text = ReadDataPipe(client->response_body_release());
-  EXPECT_TRUE(partial_buffer_string == response_text);
+  // Don't use EXPECT_EQ, it will print out a lot of garbage if check failed.
+  EXPECT_TRUE(partial_buffer == base::as_byte_span(response_text));
 }
 
 IN_PROC_BROWSER_TEST_P(FileSystemURLLoaderFactoryTest,
                        FileTestMultipleRangesNotSupported) {
   base::ScopedAllowBlockingForTesting allow_blocking;
-  WriteFile("file1.dat", kTestFileData, std::size(kTestFileData) - 1);
+  WriteFile(
+      "file1.dat",
+      base::as_byte_span(kTestFileData).first(std::size(kTestFileData) - 1));
   net::HttpRequestHeaders headers;
   headers.SetHeader(net::HttpRequestHeaders::kRange,
                     "bytes=0-5,10-200,200-300");
@@ -853,7 +865,9 @@ IN_PROC_BROWSER_TEST_P(FileSystemURLLoaderFactoryTest,
 
 IN_PROC_BROWSER_TEST_P(FileSystemURLLoaderFactoryTest, FileRangeOutOfBounds) {
   base::ScopedAllowBlockingForTesting allow_blocking;
-  WriteFile("file1.dat", kTestFileData, std::size(kTestFileData) - 1);
+  WriteFile(
+      "file1.dat",
+      base::as_byte_span(kTestFileData).first(std::size(kTestFileData) - 1));
   net::HttpRequestHeaders headers;
   headers.SetHeader(net::HttpRequestHeaders::kRange,
                     net::HttpByteRange::Bounded(500, 1000).GetHeaderValue());
@@ -897,7 +911,9 @@ IN_PROC_BROWSER_TEST_P(FileSystemURLLoaderFactoryTest, NoSuchFile) {
 
 IN_PROC_BROWSER_TEST_P(FileSystemURLLoaderFactoryTest, FileCancel) {
   base::ScopedAllowBlockingForTesting allow_blocking;
-  WriteFile("file1.dat", kTestFileData, std::size(kTestFileData) - 1);
+  WriteFile(
+      "file1.dat",
+      base::as_byte_span(kTestFileData).first(std::size(kTestFileData) - 1));
   auto client = TestLoadNoRun(CreateFileSystemURL("file1.dat"));
 
   // client.reset();
@@ -911,7 +927,7 @@ IN_PROC_BROWSER_TEST_P(FileSystemURLLoaderFactoryTest, FileGetMimeType) {
       "<!DOCTYPE HTML><html><head>test</head>"
       "<body>foo</body></html>";
   const char kFilename[] = "hoge.html";
-  WriteFile(kFilename, file_data.data(), file_data.size());
+  WriteFile(kFilename, base::as_byte_span(file_data));
 
   std::string mime_type_direct;
   base::FilePath::StringType extension =
@@ -931,7 +947,9 @@ IN_PROC_BROWSER_TEST_P(FileSystemURLLoaderFactoryTest, FileGetMimeType) {
 
 IN_PROC_BROWSER_TEST_P(FileSystemURLLoaderFactoryTest, FileIncognito) {
   base::ScopedAllowBlockingForTesting allow_blocking;
-  WriteFile("file", kTestFileData, std::size(kTestFileData) - 1);
+  WriteFile(
+      "file",
+      base::as_byte_span(kTestFileData).first(std::size(kTestFileData) - 1));
 
   // Creates a new filesystem context for incognito mode.
   scoped_refptr<FileSystemContext> file_system_context =
