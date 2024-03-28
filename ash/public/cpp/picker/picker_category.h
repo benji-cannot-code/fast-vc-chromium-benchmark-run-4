0f681ff6e677ef4c263b48e82d926098136e7cf2
Darren Shen <shend@chromium.org>
2024-03-28 02:40:27 +0000

    Use new compact categories
    Bug: b/328682610
    Change-Id: Ie341eb118f1738aced3cc3b749a937e801abed0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5399656
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Patti Lor <patricialor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279421}

--- ash/public/cpp/picker/picker_category.h
+++ ash/public/cpp/picker/picker_category.h
@@ -12,19 +12,14 @@ namespace ash {
 
 // A category specifies a type of data that can be searched for.
 enum class ASH_PUBLIC_EXPORT PickerCategory {
-  kEmojis,
-  kSymbols,
-  kEmoticons,
-  kGifs,
-  kOpenTabs,
-  kBrowsingHistory,
-  kBookmarks,
+  kEditor,
+  kLinks,
+  kExpressions,
+  kClipboard,
   kDriveFiles,
   kLocalFiles,
-  kEditor,
   kDatesTimes,
   kUnitsMaths,
-  kClipboard,
 };
 
 }  // namespace ash
