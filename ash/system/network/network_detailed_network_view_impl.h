5324d21f55f9230eadd56dd5c9b48e00b91d3ef5
Joe Antonetti <joeantonetti@google.com>
2024-03-28 17:15:03 +0000

    [Instant Hotspot] Add Chevron to collapse Tether Section
    This change adds a chevron to collapse the Tether section in Quick Settings.
    
    The chevron starts expanded, and the user can collapse it by clicking
    on the section.
    
    http://screencast/cast/NDg1NTczNzIwNDM0Mjc4NHw5MTJkYWYzYS04Ng
    
    Bug=b/321940291
    Test=Added Unit Tests
    
    Change-Id: I45b0cc3bb640528ed3a9d2d31fbfad2bae554894
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5269963
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Joe Antonetti <joeantonetti@google.com>
    Cr-Commit-Position: refs/heads/main@{#1279758}

--- ash/system/network/network_detailed_network_view_impl.h
+++ ash/system/network/network_detailed_network_view_impl.h
@@ -48,7 +48,9 @@ class ASH_EXPORT NetworkDetailedNetworkViewImpl
   HoverHighlightView* AddConfigureNetworkEntry(NetworkType type) override;
   NetworkListMobileHeaderView* AddMobileSectionHeader() override;
   NetworkListWifiHeaderView* AddWifiSectionHeader() override;
-  NetworkListTetherHostsHeaderView* AddTetherHostsSectionHeader() override;
+  NetworkListTetherHostsHeaderView* AddTetherHostsSectionHeader(
+      NetworkListTetherHostsHeaderView::OnExpandedStateToggle callback)
+      override;
   void UpdateScanningBarVisibility(bool visible) override;
   views::View* GetNetworkList(NetworkType type) override;
   void ReorderFirstListView(size_t index) override;
@@ -56,7 +58,6 @@ class ASH_EXPORT NetworkDetailedNetworkViewImpl
   void ReorderNetworkListView(size_t index) override;
   void ReorderMobileTopContainer(size_t index) override;
   void ReorderMobileListView(size_t index) override;
-  void ReorderTetherHostsTopContainer(size_t index) override;
   void ReorderTetherHostsListView(size_t index) override;
   void MaybeRemoveFirstListView() override;
   void UpdateWifiStatus(bool enabled) override;
@@ -77,7 +78,6 @@ class ASH_EXPORT NetworkDetailedNetworkViewImpl
   raw_ptr<RoundedContainer> mobile_network_list_view_ = nullptr;
   raw_ptr<RoundedContainer> wifi_top_container_ = nullptr;
   raw_ptr<RoundedContainer> wifi_network_list_view_ = nullptr;
-  raw_ptr<RoundedContainer> tether_hosts_top_container_ = nullptr;
   raw_ptr<RoundedContainer> tether_hosts_network_list_view_ = nullptr;
 };
 
