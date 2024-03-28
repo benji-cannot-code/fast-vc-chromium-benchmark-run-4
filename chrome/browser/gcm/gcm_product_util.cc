df7988438426184e6d2e3ac31a062fad655e0d6f
Helmut Januschka <helmut@januschka.com>
2024-03-28 16:00:01 +0000

    gcm: base::StringPiece -> std::string_view
    Bug: 691162
    Change-Id: Ib94d4a68274b49c9996118c45a377973852e420e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372456
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279689}

--- chrome/browser/gcm/gcm_product_util.cc
+++ chrome/browser/gcm/gcm_product_util.cc
@@ -5,7 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/gcm/gcm_product_util.h"
 
-#include "base/strings/string_piece.h"
+#include <string_view>
+
 #include "base/strings/string_util.h"
 #include "chrome/common/chrome_version.h"
 #include "chrome/common/pref_names.h"
@@ -18,7 +19,7 @@ namespace gcm {
 
 namespace {
 
-std::string ToLowerAlphaNum(base::StringPiece in) {
+std::string ToLowerAlphaNum(std::string_view in) {
   std::string out;
   out.reserve(in.size());
   for (char ch : in) {
