1c4ebaf4e1cc4f2adac428248d0cd17de1da6fdd
Adam Langley <agl@chromium.org>
2024-03-28 17:23:35 +0000

    webauthn: add a UI step for account reauth.
    Change-Id: Id2bc2054efe811bde6fede4cdd54f6f6020f9722
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5386676
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279765}

--- chrome/browser/webauthn/authenticator_request_dialog_model.cc
+++ chrome/browser/webauthn/authenticator_request_dialog_model.cc
@@ -402,6 +402,7 @@ StepUIType step_ui_type(AuthenticatorRequestDialogModel::Step step) {
       return StepUIType::NONE;
 
     case AuthenticatorRequestDialogModel::Step::kRecoverSecurityDomain:
+    case AuthenticatorRequestDialogModel::Step::kGPMReauthAccount:
       return StepUIType::WINDOW;
 
     case AuthenticatorRequestDialogModel::Step::kGPMCreatePasskey:
