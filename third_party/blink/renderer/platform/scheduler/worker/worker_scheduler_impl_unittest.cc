3d81d7b5932a68931d32d7506d874811ca93df88
Scott Haseley <shaseley@chromium.org>
2024-03-28 04:20:45 +0000

    [blink scheduler] Re-enable WorkerSchedulerImplTests on LSAN
    These tests don't fail locally or on the linux_chromium_asan_rel_ng bot,
    so re-enable them.
    
    Bug: 40626546
    Change-Id: Ifdb10a27378750d737bd571d6588d7bd2a14ec51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5403279
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Auto-Submit: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279460}

--- third_party/blink/renderer/platform/scheduler/worker/worker_scheduler_impl_unittest.cc
+++ third_party/blink/renderer/platform/scheduler/worker/worker_scheduler_impl_unittest.cc
@@ -31,15 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/scheduler/worker/worker_thread_scheduler.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 
-// TODO(crbug.com/960984): Fix memory leaks in tests and re-enable on LSAN.
-#ifdef LEAK_SANITIZER
-#define MAYBE_PausableTasks DISABLED_PausableTasks
-#define MAYBE_NestedPauseHandlesTasks DISABLED_NestedPauseHandlesTasks
-#else
-#define MAYBE_PausableTasks PausableTasks
-#define MAYBE_NestedPauseHandlesTasks NestedPauseHandlesTasks
-#endif
-
 using testing::ElementsAre;
 using testing::ElementsAreArray;
 
@@ -328,7 +319,7 @@ TEST_F(WorkerSchedulerImplTest,
                                  start_time_ + base::Seconds(40)));
 }
 
-TEST_F(WorkerSchedulerImplTest, MAYBE_PausableTasks) {
+TEST_F(WorkerSchedulerImplTest, PausableTasks) {
   Vector<String> run_order;
   auto pause_handle = worker_scheduler_->Pause();
   // Tests interlacing pausable, throttable and unpausable tasks and
@@ -347,7 +338,7 @@ TEST_F(WorkerSchedulerImplTest, MAYBE_PausableTasks) {
   EXPECT_THAT(run_order, testing::ElementsAre("T3", "T1", "T2"));
 }
 
-TEST_F(WorkerSchedulerImplTest, MAYBE_NestedPauseHandlesTasks) {
+TEST_F(WorkerSchedulerImplTest, NestedPauseHandlesTasks) {
   Vector<String> run_order;
   auto pause_handle = worker_scheduler_->Pause();
   {
