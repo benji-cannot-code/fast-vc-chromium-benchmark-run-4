b57bea840ea936dd3f0804a568bb967b08f5ecc5
Raj T <rajendrant@chromium.org>
2024-03-28 17:09:36 +0000

    [ios] Add PageContentAnnotations service
    Change-Id: Ie7d8d653b8ad7d2f88bfd9c9806e1ae8f1bb2e65
    Bug: b:302607981
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5361149
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1279749}

--- ios/chrome/browser/browser_state/model/browser_state_keyed_service_factories.mm
+++ ios/chrome/browser/browser_state/model/browser_state_keyed_service_factories.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/browser_state/model/browser_state_keyed_service_factories.h"
 
 #import "base/feature_list.h"
+#import "components/page_content_annotations/core/page_content_annotations_features.h"
 #import "ios/chrome/browser/affiliations/model/ios_chrome_affiliation_service_factory.h"
 #import "ios/chrome/browser/autocomplete/model/autocomplete_classifier_factory.h"
 #import "ios/chrome/browser/autocomplete/model/in_memory_url_index_factory.h"
@@ -54,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/metrics/model/google_groups_updater_service_factory.h"
 #import "ios/chrome/browser/metrics/model/ios_profile_session_durations_service_factory.h"
 #import "ios/chrome/browser/optimization_guide/model/optimization_guide_service_factory.h"
+#import "ios/chrome/browser/page_content_annotations/model/page_content_annotations_service_factory.h"
 #import "ios/chrome/browser/page_info/about_this_site_service_factory.h"
 #import "ios/chrome/browser/passwords/model/ios_chrome_account_password_store_factory.h"
 #import "ios/chrome/browser/passwords/model/ios_chrome_password_check_manager_factory.h"
@@ -225,6 +227,11 @@ void EnsureBrowserStateKeyedServiceFactoriesBuilt() {
   // All factories that are only conditionally added go below here.
   if (base::FeatureList::IsEnabled(kIOSSaveToDrive)) {
     drive::DriveServiceFactory::GetInstance();
+  }
+
+  if (page_content_annotations::features::
+          ShouldEnablePageContentAnnotations()) {
+    PageContentAnnotationsServiceFactory::GetInstance();
   }
 
 #if BUILDFLAG(IOS_CREDENTIAL_PROVIDER_ENABLED)
