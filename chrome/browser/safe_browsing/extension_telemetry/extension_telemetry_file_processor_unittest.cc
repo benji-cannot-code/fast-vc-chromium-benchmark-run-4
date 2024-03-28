389dc9aeb11a90dd26e5b304d8f234cdb810d3fb
Helmut Januschka <helmut@januschka.com>
2024-03-28 17:55:13 +0000

    safe_browsing: base::StringPiece -> std::string_view
    Bug: 691162
    Change-Id: I48068a66d9e1677c730f4c810323af0d3a3e9d9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372458
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279792}

--- chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_file_processor_unittest.cc
+++ chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_file_processor_unittest.cc
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 #include "chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_file_processor.h"
 
+#include <string_view>
+
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
@@ -43,7 +45,7 @@ void WriteExtensionFile(const base::FilePath& path,
 
 void WriteEmptyFile(const base::FilePath& path, const std::string& file_name) {
   base::FilePath file_path = path.AppendASCII(file_name);
-  base::WriteFile(file_path, base::StringPiece());
+  base::WriteFile(file_path, std::string_view());
 
   int64_t file_size;
   EXPECT_TRUE(base::GetFileSize(file_path, &file_size));
--- chrome/browser/safe_browsing/safe_browsing_blocking_page_test.cc
+++ chrome/browser/safe_browsing/safe_browsing_blocking_page_test.cc
@@ -7,12 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // threat urls.  It then uses a real browser to go to these urls, and sends
 // "goback" or "proceed" commands and verifies they work.
 
-#include "components/safe_browsing/content/browser/safe_browsing_blocking_page.h"
-
 #include <algorithm>
 #include <map>
 #include <memory>
 #include <optional>
+#include <string_view>
 #include <utility>
 
 #include "base/command_line.h"
@@ -78,6 +77,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/policy_constants.h"
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing/content/browser/async_check_tracker.h"
+#include "components/safe_browsing/content/browser/safe_browsing_blocking_page.h"
 #include "components/safe_browsing/content/browser/safe_browsing_blocking_page_factory.h"
 #include "components/safe_browsing/content/browser/threat_details.h"
 #include "components/safe_browsing/content/browser/ui_manager.h"
@@ -807,8 +807,8 @@ class SafeBrowsingBlockingPageBrowserTest
   // Adds a safebrowsing threat results to the fake safebrowsing service,
   // navigates to a page with a subresource containing the threat site, and
   // returns the url of the parent page.
-  GURL SetupThreatOnSubresourceAndNavigate(base::StringPiece main_frame_url,
-                                           base::StringPiece subresource_url) {
+  GURL SetupThreatOnSubresourceAndNavigate(std::string_view main_frame_url,
+                                           std::string_view subresource_url) {
     GURL url = embedded_test_server()->GetURL(main_frame_url);
     GURL embedded_url = embedded_test_server()->GetURL(subresource_url);
     SetURLThreatType(embedded_url, GetThreatType());
