8ef008896469e8885793fc0307b410d47531f871
David Maunder <davidjm@google.com>
2024-03-28 18:43:16 +0000

    Fix sync SequenceChecker crash for ProductSpecifications
    Controller delegate acquisition followed the power bookmarks example
    whereby the ModelTypeControllerDelegate was acquired on a background
    thread. This unfortunately lead to a SequenceChecker crash.
    Other examples were investigated and the approach of SESSIONS and
    SEND_TAB_TO_SELF was adopted whereby a raw pointer to the delegate for
    full sync mode and transport mode was acquired on the UI thread, then
    wrapped in a unique_ptr for full sync mode and transport mode. Note our
    use case has no local data, so the same delegate is used for full sync
    and transport mode. A smoke test was also was also written so any
    similar crashes in future are caught before code can be checked in.
    
    Bug: b:331400350
    Change-Id: Ib7f60edc31e2b31d2a21f6a7f803b2c7ba8f1410
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5403301
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279828}

--- components/commerce/core/product_specifications/product_specifications_service.cc
+++ components/commerce/core/product_specifications/product_specifications_service.cc
@@ -10,20 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace commerce {
 
 ProductSpecificationsService::ProductSpecificationsService(
-    std::unique_ptr<ProductSpecificationsSyncBridge> bridge,
-    scoped_refptr<base::SequencedTaskRunner> backend_task_runner)
-    : bridge_(std::move(bridge)), backend_task_runner_(backend_task_runner) {}
+    std::unique_ptr<ProductSpecificationsSyncBridge> bridge)
+    : bridge_(std::move(bridge)) {}
 
 ProductSpecificationsService::~ProductSpecificationsService() = default;
-
-std::unique_ptr<syncer::ModelTypeControllerDelegate>
-ProductSpecificationsService::CreateSyncControllerDelegate() {
-  return std::make_unique<syncer::ProxyModelTypeControllerDelegate>(
-      backend_task_runner_,
-      base::BindRepeating(
-          &ProductSpecificationsService::GetSyncControllerDelegate,
-          base::Unretained(this)));
-}
 
 base::WeakPtr<syncer::ModelTypeControllerDelegate>
 ProductSpecificationsService::GetSyncControllerDelegate() {
