33846ae4d228eb8344a56466b2de96b9eb54d79f
Greg Thompson <grt@chromium.org>
2024-03-28 02:35:55 +0000

    [histograms] Fix crash in WriteAsciiBucketGraph
    Properly handle a negative x_count in WriteAsciiBucketGraph.
    
    Fixed: 331095130
    Change-Id: I5f27a26e1ead267e44bb9e8dc04022bbe21c1499
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5398224
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279415}

--- base/metrics/histogram_samples.h
+++ base/metrics/histogram_samples.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/atomicops.h"
 #include "base/base_export.h"
+#include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/metrics/histogram_base.h"
 
@@ -245,9 +246,9 @@ class BASE_EXPORT HistogramSamples {
   }
 
   // Produces an actual graph (set of blank vs non blank char's) for a bucket.
-  void WriteAsciiBucketGraph(double x_count,
-                             int line_length,
-                             std::string* output) const;
+  static void WriteAsciiBucketGraph(double x_count,
+                                    int line_length,
+                                    std::string* output);
 
   // Writes textual description of the bucket contents (relative to histogram).
   // Output is the count in the buckets, as well as the percentage.
@@ -269,6 +270,8 @@ class BASE_EXPORT HistogramSamples {
   Metadata* meta() { return meta_; }
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(HistogramSamplesTest, WriteAsciiBucketGraph);
+
   // Depending on derived class `meta_` can come from:
   // - Local storage: Then `meta_owned_` is set and meta_ points to it.
   // - External storage: Then `meta_owned_` is null, and `meta_` point toward an
