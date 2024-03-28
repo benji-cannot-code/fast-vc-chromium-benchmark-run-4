963efab81ad051bec7793fbe90954664705924c9
Ewann Pelle <ewannpv@chromium.org>
2024-03-28 09:11:39 +0000

    [iOS][TS] Fix tab strip visibility in multi-window
    This CL updates the visibility of the tab strip view after its creation.
    This avoids displaying the tab strip view in a small Chrome window when
    using Chrome in multi-window.
    
    Bug: 331623193
    Change-Id: I0d91cdab990ded7967f49ea68d3265d67c003960
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402866
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279520}

--- ios/chrome/browser/ui/browser_view/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view/browser_view_controller.mm
@@ -1342,6 +1342,8 @@ enum HeaderBehaviour {
   if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_TABLET) {
     if (IsModernTabStripOrRaccoonEnabled()) {
       [self.tabStripCoordinator start];
+      const bool canShowTabStrip = IsRegularXRegularSizeClass(self);
+      [self.tabStripCoordinator hideTabStrip:!canShowTabStrip];
     } else {
       self.legacyTabStripCoordinator.presentationProvider = self;
       [self.legacyTabStripCoordinator start];
