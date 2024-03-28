7246746a1b0a9f76779ab8582cc872c220e52a3c
Shu Yang <shuyng@google.com>
2024-03-28 19:26:31 +0000

    Add JNI to expose Java method to determine pdf viewer availability
    Bug: 331700801
    Change-Id: I56012f94617970a1792fdf494374e867df1705e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5404227
    Commit-Queue: Shu Yang <shuyng@google.com>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Auto-Submit: Shu Yang <shuyng@google.com>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279866}

--- chrome/browser/download/chrome_download_manager_delegate.cc
+++ chrome/browser/download/chrome_download_manager_delegate.cc
@@ -111,6 +111,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/download/android/insecure_download_dialog_bridge.h"
 #include "chrome/browser/download/android/insecure_download_infobar_delegate.h"
 #include "chrome/browser/flags/android/chrome_feature_list.h"
+#include "chrome/browser/ui/android/pdf/pdf_jni_headers/PdfUtils_jni.h"
 #include "chrome/browser/ui/android/tab_model/tab_model.h"
 #include "chrome/browser/ui/android/tab_model/tab_model_list.h"
 #include "components/infobars/content/content_infobar_manager.h"
@@ -1934,9 +1935,8 @@ bool ChromeDownloadManagerDelegate::IsFromExternalApp(
 }
 
 bool ChromeDownloadManagerDelegate::ShouldOpenPdfInline() {
-  return base::FeatureList::IsEnabled(features::kAndroidOpenPdfInline) &&
-         base::android::BuildInfo::GetInstance()->sdk_int() >=
-             base::android::SDK_VERSION_U;
+  JNIEnv* env = base::android::AttachCurrentThread();
+  return Java_PdfUtils_shouldOpenPdfInline(env);
 }
 #endif  // BUILDFLAG(IS_ANDROID)
 
