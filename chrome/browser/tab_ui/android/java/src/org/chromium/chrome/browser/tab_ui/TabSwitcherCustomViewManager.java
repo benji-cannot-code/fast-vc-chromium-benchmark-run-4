6311627872ac4121d8381829333be2c0af9c8ca5
Xi Han <hanxi@google.com>
2024-03-28 15:04:58 +0000

    [Refactor] Move TabSwitcher interface to chrome/browser/tab_ui.
    In this CL:
    1) Move TabSwitcherType from TabManagementDelegate to TabSwitcher;
    2) Move TabSwitcher, RecyclerViewPosition,
       TabSwitcherCustomViewManager and
       TabSwitcherCustomViewManagerUnitTest to chrome/browser/tab_ui.
    
    Bug: 330574966
    Change-Id: I03a8b46e8730fb912e8d66b09ac4ae8d4adec21d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5403026
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279650}

--- chrome/browser/tab_ui/android/java/src/org/chromium/chrome/browser/tab_ui/TabSwitcherCustomViewManager.java
+++ chrome/browser/tab_ui/android/java/src/org/chromium/chrome/browser/tab_ui/TabSwitcherCustomViewManager.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.tasks.tab_management;
+package org.chromium.chrome.browser.tab_ui;
 
 import android.view.View;
 
@@ -16,7 +16,7 @@ public class TabSwitcherCustomViewManager {
      * An interface for tab switcher, via which it can listen for signals concerning
      * addition and removal of custom views.
      */
-    interface Delegate {
+    public interface Delegate {
         /**
          * This is fired when a client has requested a view to be shown.
          *
