eb65228e8b78e0e4b030c7b528803c32f23be72f
Lukasz Anforowicz <lukasza@chromium.org>
2024-03-28 00:15:21 +0000

    Use `base::HeapArray` in `mojo::BigBuffer`.
    Fixed: 331643947
    Change-Id: If9af0f103c3d3213315b7dbf9f338049417b4864
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402504
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279355}

--- mojo/public/cpp/base/big_buffer.h
+++ mojo/public/cpp/base/big_buffer.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/component_export.h"
+#include "base/containers/heap_array.h"
 #include "base/containers/span.h"
 #include "mojo/public/cpp/bindings/struct_traits.h"
 #include "mojo/public/cpp/system/buffer.h"
@@ -122,7 +123,7 @@ class COMPONENT_EXPORT(MOJO_BASE) BigBuffer {
 
   base::span<const uint8_t> byte_span() const {
     DCHECK_EQ(storage_type_, StorageType::kBytes);
-    return base::make_span(bytes_.get(), bytes_size_);
+    return bytes_.as_span();
   }
 
   internal::BigBufferSharedMemoryRegion& shared_memory() {
@@ -134,8 +135,7 @@ class COMPONENT_EXPORT(MOJO_BASE) BigBuffer {
   friend class BigBufferView;
 
   StorageType storage_type_;
-  std::unique_ptr<uint8_t[]> bytes_;
-  size_t bytes_size_;
+  base::HeapArray<uint8_t> bytes_;
   std::optional<internal::BigBufferSharedMemoryRegion> shared_memory_;
 };
 
