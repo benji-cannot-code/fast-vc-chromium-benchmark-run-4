a7a206df137410d068449bb61f37ebf0aa73d2df
Ben Joyce <bjoyce@google.com>
2024-03-28 17:02:02 +0000

    Change uninclusive language
    Avoid getting uninclusive language presubmit check.
    
    Change-Id: Ifa1dd03717a097770f0757f945d3e544c5f41bb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5403589
    Auto-Submit: Benjamin Joyce (Ben) <bjoyce@chromium.org>
    Commit-Queue: Benjamin Joyce (Ben) <bjoyce@chromium.org>
    Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279741}

--- chrome/browser/commerce/merchant_viewer/android/javatests/src/org/chromium/chrome/browser/merchant_viewer/PageInfoStoreInfoViewTest.java
+++ chrome/browser/commerce/merchant_viewer/android/javatests/src/org/chromium/chrome/browser/merchant_viewer/PageInfoStoreInfoViewTest.java
@@ -89,7 +89,7 @@ public class PageInfoStoreInfoViewTest {
     @Mock private ShoppingService mMockShoppingService;
 
     private final MerchantInfo mFakeMerchantTrustSignals =
-            new MerchantInfo(4.5f, 100, new GURL("http://dummy/url"), false, 0f, false, false);
+            new MerchantInfo(4.5f, 100, new GURL("http://fake/url"), false, 0f, false, false);
 
     @Before
     public void setUp() {
@@ -150,7 +150,7 @@ public class PageInfoStoreInfoViewTest {
     @Feature({"RenderTest"})
     public void testStoreInfoRowVisibleWithData_WithoutReviews() throws IOException {
         MerchantInfo fakeMerchantTrustSignals =
-                new MerchantInfo(4.5f, 0, new GURL("http://dummy/url"), false, 0f, false, false);
+                new MerchantInfo(4.5f, 0, new GURL("http://fake/url"), false, 0f, false, false);
         mockShoppingServiceResponse(fakeMerchantTrustSignals);
 
         openPageInfoFromStoreIcon(false, true); // fromStoreIcon, dialogCheck
@@ -163,7 +163,7 @@ public class PageInfoStoreInfoViewTest {
     @Feature({"RenderTest"})
     public void testStoreInfoRowVisibleWithData_WithoutRating() throws IOException {
         MerchantInfo fakeMerchantTrustSignals =
-                new MerchantInfo(0f, 0, new GURL("http://dummy/url"), true, 0f, false, false);
+                new MerchantInfo(0f, 0, new GURL("http://fake/url"), true, 0f, false, false);
         mockShoppingServiceResponse(fakeMerchantTrustSignals);
 
         openPageInfoFromStoreIcon(false, true); // fromStoreIcon, dialogCheck
