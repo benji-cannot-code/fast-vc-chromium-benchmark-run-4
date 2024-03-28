b8fafbcd635fa52614ea2be5defa5616016dcb0f
Neil Coronado <nemco@google.com>
2024-03-28 22:15:38 +0000

    [TabGroupIndicators] Fix NPE when creating indicator bitmap
    Fix NPE when creating indicator bitmap.
    -Determine incognito state with variable instead of inferring from the
     root Tab. The root Tab may no longer exist in the model, which caused
     the NPE.
    
    Bug: 330718987
    Change-Id: Iffc260f98dad46e01414b5a7abc27406b53b29ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5403673
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Neil Coronado <nemco@google.com>
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1279933}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
@@ -211,7 +211,8 @@ public class StripLayoutHelper implements StripLayoutTab.StripLayoutTabDelegate 
 
                     // TODO(crbug.com/331664842): When group title bitmaps are culled, guard this
                     //  with groupTitle.isVisible() check to avoid creating when unneeded.
-                    mLayerTitleCache.getUpdatedGroupTitle(rootId, groupTitle.getTitle());
+                    mLayerTitleCache.getUpdatedGroupTitle(
+                            rootId, groupTitle.getTitle(), mIncognito);
                     mRenderHost.requestRender();
                 }
 
@@ -229,7 +230,8 @@ public class StripLayoutHelper implements StripLayoutTab.StripLayoutTabDelegate 
                     // Title may also need to change color.
                     // TODO(crbug.com/331664842): When group title bitmaps are culled, guard this
                     //  with groupTitle.isVisible() check to avoid creating when unneeded.
-                    mLayerTitleCache.getUpdatedGroupTitle(rootId, groupTitle.getTitle());
+                    mLayerTitleCache.getUpdatedGroupTitle(
+                            rootId, groupTitle.getTitle(), mIncognito);
                     mRenderHost.requestRender();
                 }
             };
@@ -2319,7 +2321,7 @@ public class StripLayoutHelper implements StripLayoutTab.StripLayoutTabDelegate 
         }
 
         StripLayoutGroupTitle groupTitle =
-                new StripLayoutGroupTitle(mContext, rootId, titleString, textWidth, color);
+                new StripLayoutGroupTitle(mIncognito, rootId, titleString, textWidth, color);
         pushPropertiesToGroupTitle(groupTitle);
 
         return groupTitle;
--- chrome/android/java/src/org/chromium/chrome/browser/compositor/scene_layer/TabStripSceneLayer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/scene_layer/TabStripSceneLayer.java
@@ -271,6 +271,7 @@ public class TabStripSceneLayer extends SceneOverlayLayer {
                     .putGroupIndicatorLayer(
                             mNativePtr,
                             TabStripSceneLayer.this,
+                            gt.isIncognito(),
                             gt.getRootId(),
                             gt.getTint(),
                             gt.getDrawX() * mDpToPx,
@@ -417,6 +418,7 @@ public class TabStripSceneLayer extends SceneOverlayLayer {
         void putGroupIndicatorLayer(
                 long nativeTabStripSceneLayer,
                 TabStripSceneLayer caller,
+                boolean incognito,
                 int id,
                 int tint,
                 float x,
