478df876a4745afc5614413926dd0b09d6dd3194
Dorah <dorahkim@google.com>
2024-03-28 09:13:14 +0000

    camera: Collect metrics when Wi-fi protocol is unsupported
    This CL collects metrics when CCA scans a barcode and the Wi-fi protocol
    is unsupported. The metrics will be collected from GA and CrOS events
    
    METRICS_DOCUMENTATION_UPDATED=true
    
    BUG=b:309573972
    Test=$autoninja -C out/Debug/ ash_webui_unittests &&
    ./out/Debug/ash_webui_unittests
    --gtest_filter=CameraAppEventsSenderTest*
    
    Change-Id: Ic91d0e6dcaeda26f20c1ceec9c44b479fd74c4eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5387479
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Jong Ahn <jongahn@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Dorah Kim <dorahkim@google.com>
    Cr-Commit-Position: refs/heads/main@{#1279522}

--- ash/webui/camera_app_ui/resources/js/barcode_chip.ts
+++ ash/webui/camera_app_ui/resources/js/barcode_chip.ts
@@ -8,7 +8,11 @@ import * as dom from './dom.js';
 import {reportError} from './error.js';
 import {Flag} from './flag.js';
 import {I18nString} from './i18n_string.js';
-import {BarcodeContentType, sendBarcodeDetectedEvent} from './metrics.js';
+import {
+  BarcodeContentType,
+  sendBarcodeDetectedEvent,
+  sendUnsupportedProtocolEvent,
+} from './metrics.js';
 import * as loadTimeData from './models/load_time_data.js';
 import {ChromeHelper} from './mojo/chrome_helper.js';
 import {
@@ -91,6 +95,7 @@ function isSafeUrl(s: string): boolean {
       reportError(
           ErrorType.UNSUPPORTED_PROTOCOL, ErrorLevel.WARNING,
           new Error(`Reject url with protocol: ${url.protocol}`));
+      sendUnsupportedProtocolEvent();
       return false;
     }
     return true;
