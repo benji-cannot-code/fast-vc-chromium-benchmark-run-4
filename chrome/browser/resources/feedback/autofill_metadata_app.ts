7c4a42252e25558399e9b4454e290e5d5c022e33
Teresa Mao <temao@chromium.org>
2024-03-28 00:07:59 +0000

    Feedback UI: Use key-value-pair-viewer in autofill-metadata page
    - Replace autofill metadata table with a limited sharing key-value-pair-viewer element.
    - Remove autofill metadata table rendering code.
    
    Bug: 41481648
    Change-Id: Iddb674b1c109610193698006c64f7bd9c5d09872
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5399885
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279350}

--- chrome/browser/resources/feedback/autofill_metadata_app.ts
+++ chrome/browser/resources/feedback/autofill_metadata_app.ts
@@ -3,19 +3,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '../../strings.m.js';
+import '/shared/key_value_pair_viewer/key_value_pair_viewer.js';
 import './css/about_sys.css.js';
-import './css/logs_map_page.css.js';
-import './feedback_shared_styles.css.js';
+import './strings.m.js';
 // <if expr="chromeos_ash">
 import './js/jelly_colors.js';
 
 // </if>
+import type {KeyValuePairEntry} from '/shared/key_value_pair_viewer/key_value_pair_entry.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './autofill_metadata_app.html.js';
 import {FeedbackBrowserProxyImpl} from './js/feedback_browser_proxy.js';
-import {createLogsMapTable} from './js/logs_map_page.js';
 
 export class AutofillMetadataAppElement extends PolymerElement {
   static get is() {
@@ -26,6 +25,20 @@ export class AutofillMetadataAppElement extends PolymerElement {
     return getTemplate();
   }
 
+  static get properties() {
+    return {
+      entries_: Array,
+      loading_: {
+        type: Boolean,
+        value: true,
+        reflectToAttribute: true,
+      },
+    };
+  }
+
+  private entries_: KeyValuePairEntry[];
+  private loading_: boolean;
+
   override connectedCallback() {
     super.connectedCallback();
     const dialogArgs =
@@ -33,8 +46,8 @@ export class AutofillMetadataAppElement extends PolymerElement {
     if (!dialogArgs) {
       return;
     }
-
     this.createAutofillMetadataTable(dialogArgs);
+    this.loading_ = false;
   }
 
   /**
@@ -44,7 +57,7 @@ export class AutofillMetadataAppElement extends PolymerElement {
   private createAutofillMetadataTable(dialogArgs: string) {
     const autofillMetadata = JSON.parse(dialogArgs);
 
-    const items: chrome.feedbackPrivate.LogsMapEntry[] = [];
+    const items: KeyValuePairEntry[] = [];
 
     if (autofillMetadata.triggerFormSignature) {
       items.push({
@@ -74,7 +87,7 @@ export class AutofillMetadataAppElement extends PolymerElement {
       });
     }
 
-    createLogsMapTable(items, this.shadowRoot!);
+    this.entries_ = items;
   }
 }
 
