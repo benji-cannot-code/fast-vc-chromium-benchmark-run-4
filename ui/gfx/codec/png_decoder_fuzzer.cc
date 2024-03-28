d453f6bdfb668c5d2afef92c18cd4d607b5778c3
Daniel Cheng <dcheng@chromium.org>
2024-03-28 00:17:15 +0000

    Add fuzzer for gfx::PNGCodec::Decode().
    Bug: 331556924
    Change-Id: I482a19c867dce57762bedd24951b96ea285702d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5403588
    Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279357}

---
+++ ui/gfx/codec/png_decoder_fuzzer.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <memory>
+
+#include "third_party/skia/include/core/SkBitmap.h"
+#include "ui/gfx/codec/png_codec.h"
+
+namespace gfx {
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  // TODO(crbug.com/323934468): Initialize decoder settings dynamically using
+  // fuzzer input.
+  SkBitmap out;
+  PNGCodec::Decode(data, size, &out);
+  return 0;
+}
+
+}  // namespace gfx
