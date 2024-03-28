4adabd5ac5f2610c35d306605e9a3b6f8a495633
Maks Orlovich <morlovich@chromium.org>
2024-03-28 19:29:26 +0000

    FLEDGE: Seller worklet: lazily construct portions of AuctionConfig
    (And also fix a couple of fields to not be filled in a silly
     inefficient way).
    
    On Youssef's benchmark this changes the argument construction time
    for scoreAd from about 790μs to about 290μs, and overall execution
    time (which includes argument construction) from about 3870μs to about
    3310μs.
    
    Change-Id: Ifeab706f27377910da445a7a26a8ed058da16b51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5383059
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279867}

--- content/services/auction_worklet/context_recycler.cc
+++ content/services/auction_worklet/context_recycler.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/services/auction_worklet/register_ad_beacon_bindings.h"
 #include "content/services/auction_worklet/register_ad_macro_bindings.h"
 #include "content/services/auction_worklet/report_bindings.h"
+#include "content/services/auction_worklet/seller_lazy_filler.h"
 #include "content/services/auction_worklet/set_bid_bindings.h"
 #include "content/services/auction_worklet/set_priority_bindings.h"
 #include "content/services/auction_worklet/set_priority_signals_override_bindings.h"
@@ -106,6 +107,22 @@ void ContextRecycler::AddBiddingBrowserSignalsLazyFiller() {
       std::make_unique<BiddingBrowserSignalsLazyFiller>(v8_helper_);
 }
 
+void ContextRecycler::EnsureAuctionConfigLazyFillers(size_t required) {
+  // We may see different limits in the same worklet if it's used for multiple
+  // auctions.  In that case, we have to be sure to never shrink the vector
+  // since there may be pointers to entries beyond the current need floating
+  // around.
+  size_t cur_size = auction_config_lazy_fillers_.size();
+  if (cur_size >= required) {
+    return;
+  }
+  auction_config_lazy_fillers_.resize(required);
+  for (size_t pos = cur_size; pos < required; ++pos) {
+    auction_config_lazy_fillers_[pos] =
+        std::make_unique<AuctionConfigLazyFiller>(v8_helper_);
+  }
+}
+
 void ContextRecycler::AddSetPrioritySignalsOverrideBindings() {
   DCHECK(!set_priority_signals_override_bindings_);
   set_priority_signals_override_bindings_ =
@@ -137,6 +154,9 @@ void ContextRecycler::ResetForReuse() {
     bidding_browser_signals_lazy_filler_->Reset();
   if (interest_group_lazy_filler_)
     interest_group_lazy_filler_->Reset();
+  for (const auto& auction_config_lazy_filler : auction_config_lazy_fillers_) {
+    auction_config_lazy_filler->Reset();
+  }
 }
 
 ContextRecyclerScope::ContextRecyclerScope(ContextRecycler& context_recycler)
