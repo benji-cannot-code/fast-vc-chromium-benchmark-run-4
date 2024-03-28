181c95b84dbf6d59f2c2c91849c29951eabbfa25
Artem Sumaneev <asumaneev@google.com>
2024-03-28 17:22:59 +0000

    [enrollment] Split EnrollmentScreen unit tests.
    Split EnrollmentScreen unit tests by use cases.
    
    Turn EnrollmentScreenUnitTest class into a base test class.
    Make a separate test class for rollback.
    
    Bug: b:314129939
    Test: unit_tests --gtest_filter="*EnrollmentScreen*"
    Change-Id: If1dba1974ba303642537aad3200d72e047b8fde9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5093935
    Reviewed-by: Roland Bock <rbock@google.com>
    Commit-Queue: Artem Sumaneev <asumaneev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1279764}

--- chrome/browser/ash/login/enrollment/enrollment_screen.h
+++ chrome/browser/ash/login/enrollment/enrollment_screen.h
@@ -140,8 +140,7 @@ class EnrollmentScreen
   ScreenExitCallback* exit_callback() { return &exit_callback_; }
 
  private:
-  friend class EnrollmentScreenUnitTest;
-  friend class AutomaticReenrollmentScreenUnitTest;
+  friend class EnrollmentScreenBaseTest;
   friend class test::EnrollmentHelperMixin;
 
   FRIEND_TEST_ALL_PREFIXES(AttestationAuthEnrollmentScreenTest, TestCancel);
