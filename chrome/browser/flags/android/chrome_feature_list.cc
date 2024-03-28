0a8bbe4854747c8b958e1cafee6e74b44ff2efaf
Jian Li <jianli@chromium.org>
2024-03-28 19:02:41 +0000

    Add a flag for feed containment
    Bug: 331756537
    Change-Id: I0d7902295b033846996144d21e7dc98b9d3fee0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5405872
    Commit-Queue: Jian Li <jianli@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279848}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -124,6 +124,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kWebNfc,
     &features::kIncognitoNtpRevamp,
     &feature_engagement::kIPHTabSwitcherButtonFeature,
+    &feed::kFeedContainment,
     &feed::kFeedDynamicColors,
     &feed::kFeedFollowUiUpdate,
     &feed::kFeedImageMemoryCacheSizePercentage,
