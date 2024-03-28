de20937dbf7ca611a2be615a189532adb8f46910
Avi Drissman <avi@chromium.org>
2024-03-28 15:56:03 +0000

    Use base::HeapArray in safe_browsing
    Fixed: 326458016
    Fixed: 326458683
    Fixed: 326458968
    Fixed: 326458194
    Change-Id: I030637e69399ea61a6b268138f850e187e28834f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402415
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Commit-Queue: Nathan Parker <nparker@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279686}

--- chrome/browser/safe_browsing/incident_reporting/download_metadata_manager.cc
+++ chrome/browser/safe_browsing/incident_reporting/download_metadata_manager.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/check_op.h"
+#include "base/containers/heap_array.h"
 #include "base/debug/dump_without_crashing.h"
 #include "base/files/file.h"
 #include "base/files/file_util.h"
@@ -121,10 +122,10 @@ void ReadMetadataInBackground(const base::FilePath& metadata_path,
   base::File::Info info;
   if (metadata_file.IsValid() && metadata_file.GetInfo(&info) &&
       info.size <= INT_MAX) {
-    const int size = static_cast<int>(info.size);
-    std::unique_ptr<char[]> file_data(new char[info.size]);
-    if (metadata_file.Read(0, file_data.get(), size) &&
-        metadata->ParseFromArray(file_data.get(), size) &&
+    auto file_data = base::HeapArray<uint8_t>::Uninit(info.size);
+    if (metadata_file.Read(0, file_data).value_or(0) &&
+        metadata->ParseFromArray(file_data.data(),
+                                 static_cast<int>(file_data.size())) &&
         MetadataIsValid(*metadata)) {
       return;
     }
