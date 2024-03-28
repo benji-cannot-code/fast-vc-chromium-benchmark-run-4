6546a986cc25d5a7a58b9676f6093708d7b7265c
Fumitoshi Ukai <ukai@google.com>
2024-03-28 02:40:37 +0000

    Fix non-portable include name
    DelayIMP.h is delayimp.h in VC/Tools/MSVC/14.39.33519/include/,
    
    Use the same case as actual filename to make it cross compiling simpler
    (i.e. not requiring case insensitive filesystem)
    
    Bug: b/331302909
    Change-Id: I0be61b1eb68f6dbc90feb8a27e926cb9c23867b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5395149
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>
    Reviewed-by: Philipp Wollermann <philwo@chromium.org>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1279422}

--- base/win/pe_image.h
+++ base/win/pe_image.h
@@ -14,9 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <windows.h>
 
+#include <delayimp.h>
 #include <stdint.h>
-
-#include <DelayIMP.h>
 
 namespace base {
 namespace win {
