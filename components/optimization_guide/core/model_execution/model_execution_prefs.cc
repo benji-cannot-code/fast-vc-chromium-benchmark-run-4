e174257f1c3093310185b85a8f08c4494da0c9aa
Steven Holte <holte@google.com>
2024-03-29 22:23:56 +0000

    Migrate model_execution_prefs.cc
    Bug: 331306557
    
    Change-Id: If9bde80e94027cbceb4c11c1825b607536e11866
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5405669
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1280375}

--- components/optimization_guide/core/model_execution/model_execution_prefs.cc
+++ components/optimization_guide/core/model_execution/model_execution_prefs.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/model_execution/model_execution_prefs.h"
 
 #include "base/notreached.h"
+#include "components/optimization_guide/core/model_execution/feature_keys.h"
 #include "components/prefs/pref_registry_simple.h"
 
 namespace optimization_guide::model_execution::prefs {
@@ -37,18 +38,17 @@ void RegisterProfilePrefs(PrefRegistrySimple* registry) {
 }
 
 const char* GetEnterprisePolicyPrefName(proto::ModelExecutionFeature feature) {
+  return GetEnterprisePolicyPrefName(ToUserVisibleFeatureKey(feature).value());
+}
+
+const char* GetEnterprisePolicyPrefName(UserVisibleFeatureKey feature) {
   switch (feature) {
-    case proto::ModelExecutionFeature::MODEL_EXECUTION_FEATURE_COMPOSE:
+    case UserVisibleFeatureKey::kCompose:
       return kComposeEnterprisePolicyAllowed;
-    case proto::ModelExecutionFeature::MODEL_EXECUTION_FEATURE_TAB_ORGANIZATION:
+    case UserVisibleFeatureKey::kTabOrganization:
       return kTabOrganizationEnterprisePolicyAllowed;
-    case proto::ModelExecutionFeature::MODEL_EXECUTION_FEATURE_WALLPAPER_SEARCH:
+    case UserVisibleFeatureKey::kWallpaperSearch:
       return kWallpaperSearchEnterprisePolicyAllowed;
-    case proto::ModelExecutionFeature::MODEL_EXECUTION_FEATURE_TEXT_SAFETY:
-    case proto::ModelExecutionFeature::MODEL_EXECUTION_FEATURE_TEST:
-    case proto::ModelExecutionFeature::MODEL_EXECUTION_FEATURE_UNSPECIFIED:
-      NOTREACHED();
-      return nullptr;
   }
 }
 
