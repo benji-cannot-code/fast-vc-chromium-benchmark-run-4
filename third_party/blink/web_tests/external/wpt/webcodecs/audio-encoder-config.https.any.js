0af58a1404b7f18ee23f564ef2d7ad7e54f32a3b
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-03-29 19:48:25 +0000

    Import wpt@d281c718c65699764f30269e3a0243f80f2174c4
    Using wpt-import in Chromium 983ac9e5b89c21176c7c5a526a1faf8fd120905a.
    With Chromium commits locally applied on WPT:
    b987e8fd6f "Implement container-progress() function"
    ca785e885e "[bfcache] Report null when iframes' atrributes are null"
    
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    ericwilligers@chromium.org:
      external/wpt/css/motion
    estade@chromium.org, asully@chromium.org, ayui@chromium.org, garykac@chromium.org, jsbell@chromium.org:
      external/wpt/clipboard-apis
    futhark@chromium.org, andruud@chromium.org:
      external/wpt/css/css-cascade
    hongchan@chromium.org, mjwilson@chromium.org, alvinji@chromium.org, sinafirooz@chromium.org:
      external/wpt/webaudio
    vogelheim@chromium.org:
      external/wpt/trusted-types
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I96e62f8a106ca81bc372712da49877cf3f2384fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5403167
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1280301}

--- third_party/blink/web_tests/external/wpt/webcodecs/audio-encoder-config.https.any.js
+++ third_party/blink/web_tests/external/wpt/webcodecs/audio-encoder-config.https.any.js
@@ -112,6 +112,15 @@ const invalidConfigs = [
       },
     },
   },
+  {
+    comment: 'Bitrate is too low for Opus',
+    config: {
+      codec: 'opus',
+      sampleRate: 48000,
+      numberOfChannels: 2,
+      bitrate: 1,
+    },
+  },
 ];
 
 invalidConfigs.forEach(entry => {
@@ -138,15 +147,6 @@ invalidConfigs.forEach(entry => {
 });
 
 const validButUnsupportedConfigs = [
-  {
-    comment: 'Bitrate is too low',
-    config: {
-      codec: 'opus',
-      sampleRate: 48000,
-      numberOfChannels: 2,
-      bitrate: 1,
-    },
-  },
   {
     comment: 'Unrecognized codec',
     config: {
