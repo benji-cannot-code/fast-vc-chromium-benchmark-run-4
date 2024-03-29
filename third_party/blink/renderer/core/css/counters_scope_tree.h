f5fa518456370d6ddba8e8d62adf063fb8e6e2e7
Stefan Zager <szager@chromium.org>
2024-03-29 19:04:55 +0000

    Delete obsolete CSS counter code
    Change-Id: Id7ab439f3ee6abeaeee90b1162ae79f48ddbf6f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5405089
    Auto-Submit: Stefan Zager <szager@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1280285}

--- third_party/blink/renderer/core/css/counters_scope_tree.h
+++ third_party/blink/renderer/core/css/counters_scope_tree.h
@@ -32,7 +32,6 @@ class CORE_EXPORT CountersScopeTree final
   void CreateCountersForLayoutObject(LayoutObject&);
   void CreateCounterForLayoutObject(LayoutObject&, const AtomicString&);
   void CreateCounterForLayoutCounter(LayoutCounter&);
-  void CreateListItemCounterForLayoutObject(LayoutObject&);
 
   void RemoveCounterForLayoutCounter(LayoutCounter&);
   void RemoveCounterFromScope(CounterNode&,
--- third_party/blink/renderer/core/css/style_containment_scope.h
+++ third_party/blink/renderer/core/css/style_containment_scope.h
@@ -38,7 +38,6 @@ class StyleContainmentScope final
   void CreateCounterNodeForLayoutObject(LayoutObject& object,
                                         const AtomicString& identifier);
   CORE_EXPORT void CreateCounterNodeForLayoutCounter(LayoutCounter&);
-  void CreateListItemCounterNodeForLayoutObject(LayoutObject&);
   void RemoveCounterNodeForLayoutCounter(LayoutCounter&);
   void ReparentCountersToStyleScope(StyleContainmentScope&);
   void UpdateCounters() const;
