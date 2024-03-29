c553b0109384f4307b17b5c5e79b5a6f8bdd385b
Dana Fried <dfried@chromium.org>
2024-03-29 22:34:54 +0000

    Record histograms of the number of sessions within certain windows.
    Histograms are recorded when the recent session list is updated.
    Tests are provided.
    This is subject to the "recent sessions" killswitch.
    
    Change-Id: Ib81c86077d3ed0f8b0c055ee57c0fc9a0f1b3b76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5398272
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1280382}

---
+++ chrome/browser/ui/user_education/recent_session_observer_impl.cc
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/user_education/recent_session_observer_impl.h"
+
+#include <utility>
+
+#include "base/functional/bind.h"
+#include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/user_education/recent_session_policy.h"
+
+RecentSessionObserverImpl::RecentSessionObserverImpl(
+    Profile& profile,
+    std::unique_ptr<RecentSessionPolicy> policy)
+    : profile_(profile), policy_(std::move(policy)) {}
+
+RecentSessionObserverImpl::~RecentSessionObserverImpl() = default;
+
+void RecentSessionObserverImpl::Init(RecentSessionTracker& tracker) {
+  CHECK(!subscription_);
+  subscription_ = tracker.AddRecentSessionsUpdatedCallback(
+      base::BindRepeating(&RecentSessionObserverImpl::OnRecentSessionsUpdated,
+                          base::Unretained(this)));
+}
+
+void RecentSessionObserverImpl::OnRecentSessionsUpdated(
+    const RecentSessionData& recent_sessions) {
+  policy_->RecordRecentUsageMetrics(recent_sessions);
+
+  // TODO(dfried): use the return value of this to change User Education
+  // behavior.
+  //
+  // Note: this is called to ensure that unit tests are consistent.
+  policy_->ShouldEnableLowUsagePromoMode(recent_sessions);
+}
+
+// Method that will be imported by `UserEducationServiceFactory` to create the
+// recent session observer for a profile.
+std::unique_ptr<RecentSessionObserver> CreateRecentSessionObserver(
+    Profile& profile) {
+  return std::make_unique<RecentSessionObserverImpl>(
+      profile, std::make_unique<RecentSessionPolicyImpl>());
+}
---
+++ chrome/browser/user_education/recent_session_observer.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_USER_EDUCATION_RECENT_SESSION_OBSERVER_H_
+#define CHROME_BROWSER_USER_EDUCATION_RECENT_SESSION_OBSERVER_H_
+
+#include "chrome/browser/user_education/recent_session_tracker.h"
+
+class RecentSessionObserver {
+ public:
+  RecentSessionObserver() = default;
+  RecentSessionObserver(const RecentSessionObserver&) = delete;
+  void operator=(const RecentSessionObserver&) = delete;
+  virtual ~RecentSessionObserver() = default;
+
+  // Initialization once the tracker is available.
+  virtual void Init(RecentSessionTracker& tracker) = 0;
+};
+
+#endif  // CHROME_BROWSER_USER_EDUCATION_RECENT_SESSION_OBSERVER_H_
