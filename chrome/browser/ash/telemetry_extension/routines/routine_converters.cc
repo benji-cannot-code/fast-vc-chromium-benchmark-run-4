bc887125c9873cf87d7a42168bec9a1f28d31726
Wei-Luan Wang <weiluanwang@google.com>
2024-03-29 06:33:38 +0000

    cros_healthd: Sync mojom for network bandwidth routine
    Sync CL:5333378.
    
    Bug: b:320631510
    Test: CQ
    Change-Id: I342049342926df631f2e77dfee3ae1d01dda21c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5404311
    Commit-Queue: Wei-Luan Wang <weiluanwang@google.com>
    Reviewed-by: Byron Lee <byronlee@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1280092}

--- chrome/browser/ash/telemetry_extension/routines/routine_converters.cc
+++ chrome/browser/ash/telemetry_extension/routines/routine_converters.cc
@@ -77,6 +77,7 @@ crosapi::TelemetryDiagnosticRoutineDetailPtr UncheckedConvertPtr(
     case healthd::RoutineDetail::Tag::kBluetoothScanning:
     case healthd::RoutineDetail::Tag::kBluetoothPairing:
     case healthd::RoutineDetail::Tag::kCameraAvailability:
+    case healthd::RoutineDetail::Tag::kNetworkBandwidth:
       // The actual value of unrecognizedArgument should not be used. Assign an
       // arbitrary value to it.
       return crosapi::TelemetryDiagnosticRoutineDetail::NewUnrecognizedArgument(
