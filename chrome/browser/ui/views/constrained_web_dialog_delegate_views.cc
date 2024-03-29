3a12019872c4687b8c603a4eecec364dabdf696a
Gavin Williams <gavinwill@chromium.org>
2024-03-29 00:10:27 +0000

    Printing: Create launch Printer Settings from Preview test
    This test:
    - Opens print preview in a browser
    - Navigates to an empty destination dialog
    - Clicks Manage Printers to open printer settings
    
    For ease of testing I added a view identifier to represent all
    Constrained Web Dialog. It currently only has 3 consumers
    (print preview, email confirmation dialog, and certificate viewer) so
    this should be satisfactory from a testing perspective.
    
    Bug: b/307385730, b/330714117
    Change-Id: I533341cfcf732c2941b15334315c523338c1c1af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5380000
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1280010}

--- chrome/browser/ui/views/constrained_web_dialog_delegate_views.cc
+++ chrome/browser/ui/views/constrained_web_dialog_delegate_views.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/renderer_preferences_util.h"
 #include "chrome/browser/ui/blocked_content/popunder_preventer.h"
+#include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/webui/chrome_web_contents_handler.h"
@@ -24,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/webview/unhandled_keyboard_event_handler.h"
 #include "ui/views/controls/webview/webview.h"
 #include "ui/views/view.h"
+#include "ui/views/view_class_properties.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/window/dialog_delegate.h"
 #include "ui/web_dialogs/web_dialog_delegate.h"
@@ -366,6 +368,7 @@ ConstrainedDialogWebView::ConstrainedDialogWebView(
     EnableSizingFromWebContents(RestrictToPlatformMinimumSize(min_size),
                                 max_size);
   }
+  SetProperty(views::kElementIdentifierKey, kConstrainedDialogWebViewElementId);
 }
 ConstrainedDialogWebView::~ConstrainedDialogWebView() {}
 
