5c7e0a8380e897ad47a87bf61e76239a52d6c07e
Ryan Sultanem <rsult@google.com>
2024-03-29 09:49:54 +0000

    [Uno-D][ProfileMenu] Remove customize profile from Guest mode
    This will be tested in a followup CL where the
    kExplicitBrowserSigninUIOnDesktop will be enabled by default on all
    browser tests since there should be no effect with and without the
    flag.
    
    Bug: b/319397639
    Change-Id: Id06647e93638888b24a77d485d9825bebc005931
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5401768
    Auto-Submit: Ryan Sultanem <rsult@google.com>
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1280122}

--- chrome/browser/ui/views/profiles/profile_menu_view.cc
+++ chrome/browser/ui/views/profiles/profile_menu_view.cc
@@ -741,8 +741,10 @@ void ProfileMenuView::BuildSyncInfo() {
 }
 
 void ProfileMenuView::BuildFeatureButtons() {
+  Profile* profile = browser()->profile();
   if (switches::IsExplicitBrowserSigninUIOnDesktopEnabled(
-          switches::ExplicitBrowserSigninPhase::kFull)) {
+          switches::ExplicitBrowserSigninPhase::kFull) &&
+      !profile->IsGuestSession()) {
     AddFeatureButton(
         l10n_util::GetStringUTF16(IDS_PROFILE_MENU_CUSTOMIZE_PROFILE_BUTTON),
         base::BindRepeating(&ProfileMenuView::OnEditProfileButtonClicked,
@@ -750,7 +752,6 @@ void ProfileMenuView::BuildFeatureButtons() {
         vector_icons::kEditChromeRefreshIcon);
   }
 
-  Profile* profile = browser()->profile();
   bool has_unconsented_account = HasUnconstentedProfile(profile);
   if (has_unconsented_account && !IsSyncPaused(profile)) {
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
