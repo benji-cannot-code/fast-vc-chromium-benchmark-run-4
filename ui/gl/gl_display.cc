eb33152539404ed259b8153ba0bc587f6a9e9437
Sunny Sachanandani <sunnyps@chromium.org>
2024-03-29 13:24:18 +0000

    graphite: Remove separate ANGLEDebugLayer feature flag
    Currently, SkiaGraphite feature is checked in the browser process after
    making an educated guess about whether it will be possible for the GPU
    process to use Graphite.
    
    On Mac, this involves checking ANGLE Metal feature state and the
    hardware model name against a list to match ANGLE Metal's blocklist.
    However, based on UMA data from the wild, we see cases where ANGLE
    Metal is disabled, but we're still registered in the enabled group for
    Graphite - this is precisely the scenario the early check was designed
    to avoid.
    
    Closer inspection of the experiment config by vasilyt@ points to
    ANGLEDebugLayer feature check as a culprit. This feature is configured
    in SkiaGraphite experiment groups and checked in gl_display.cc code
    independently of SkiaGraphite feature state. Checking a feature can
    enroll the client in experiment groups that specify that feature.
    Therefore, it's possible that a client is falling into the SkiaGraphite
    enabled arm only because the client checked ANGLEDebugLayer.
    
    This CL fixes that by only enabling ANGLE's debug layer if GrContextType
    is GraphiteDawn and Graphite backend validation FeatureParam is enabled,
    getting rid of the separate ANGLEDebugLayer feature flag as a result. It
    also removes the SkiaGraphiteDawnShareDevice FeatureParam that's never
    set to a non-default value via finch and is obsolete at this point.
    
    Bug: 331680185
    Change-Id: I470846e311e89e63f793783b52ee01d46e85e17e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5403504
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1280143}

--- ui/gl/gl_display.cc
+++ ui/gl/gl_display.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
+#include "base/synchronization/atomic_flag.h"
 #include "base/system/sys_info.h"
 #include "build/build_config.h"
 #include "ui/base/ozone_buildflags.h"
@@ -142,6 +143,11 @@ namespace gl {
 
 namespace {
 
+base::AtomicFlag* GetANGLEDebugLayerFlag() {
+  static base::AtomicFlag* const flag = new base::AtomicFlag();
+  return flag;
+}
+
 void AdjustAngleFeaturesFromChromeFeatures(
     std::vector<std::string>& enabled_angle_features,
     std::vector<std::string>& disabled_angle_features) {
@@ -252,9 +258,8 @@ EGLDisplay GetPlatformANGLEDisplay(
   }
 
   display_attribs.push_back(EGL_PLATFORM_ANGLE_DEBUG_LAYERS_ENABLED_ANGLE);
-  display_attribs.push_back(
-      base::FeatureList::IsEnabled(features::kANGLEDebugLayer) ? EGL_TRUE
-                                                               : EGL_FALSE);
+  display_attribs.push_back(GetANGLEDebugLayerFlag()->IsSet() ? EGL_TRUE
+                                                              : EGL_FALSE);
 
   display_attribs.push_back(EGL_NONE);
 
@@ -647,6 +652,11 @@ DisplayType GLDisplayEGL::GetDisplayType() const {
 GLDisplayEGL* GLDisplayEGL::GetDisplayForCurrentContext() {
   GLContext* context = GLContext::GetCurrent();
   return context ? context->GetGLDisplayEGL() : nullptr;
+}
+
+// static
+void GLDisplayEGL::EnableANGLEDebugLayer() {
+  GetANGLEDebugLayerFlag()->Set();
 }
 
 bool GLDisplayEGL::IsEGLSurfacelessContextSupported() {
--- ui/gl/gl_display.h
+++ ui/gl/gl_display.h
@@ -118,6 +118,8 @@ class GL_EXPORT GLDisplayEGL : public GLDisplay {
 
   static GLDisplayEGL* GetDisplayForCurrentContext();
 
+  static void EnableANGLEDebugLayer();
+
   EGLDisplay GetDisplay() const override;
   void Shutdown() override;
   bool IsInitialized() const override;
