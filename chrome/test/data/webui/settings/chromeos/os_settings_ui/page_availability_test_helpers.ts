61fe64167546dfe78561601ef43d7b66c93f56ae
Wes Okuhara <wesokuhara@google.com>
2024-03-29 21:13:51 +0000

    CrOS Settings: Personalization section available in guest mode
    In guest mode, users are able to update wallpaper and light/dark theme from the Personalization hub. Make the personalization section available
    in Settings in guest mode to help route users to these settings.
    Revamp enabled: http://screen/4bB8JvY4FFwtYZG.png
    Revamp disabled: http://screen/536yw9MSMz7yFx9.png
    
    Bug: b:330660839
    Test: browser_tests --gtest_filter="OSSettings*PageAvailablility*"
    Change-Id: I4f7a55c7f627bfdcd21ac58ce5d84c7d11b88f48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5406312
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1280342}

--- chrome/test/data/webui/settings/chromeos/os_settings_ui/page_availability_test_helpers.ts
+++ chrome/test/data/webui/settings/chromeos/os_settings_ui/page_availability_test_helpers.ts
@@ -63,7 +63,7 @@ export const SECTION_EXPECTATIONS: SectionData[] = [
     name: 'kPersonalization',
     availableBeforeRevamp: true,
     availableAfterRevamp: true,
-    availableForGuest: false,
+    availableForGuest: true,
   },
   {
     name: 'kSearchAndAssistant',
