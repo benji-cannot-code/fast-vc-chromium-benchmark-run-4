6958b432c987733440a6bb148cb0b9a1f6217d58
Ed Chin <edchin@google.com>
2024-03-29 20:50:10 +0000

    Fix content editable not autohiding saved state notification
    Make sure the correct trigger_source is passed for content editables instead of always passing AutofillSuggestionTriggerSource::kContentEditableClicked. This allows the AutoFillPopupController to set a close timer for kComposeDialogLostFocus trigger_sources.
    
    Bug: 331964491, b/331497693
    Change-Id: Ie3b984e4eb192f8bb0980325c4ea6cf6421eac77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5406364
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: edchin <edchin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1280326}

--- components/autofill/content/renderer/autofill_agent.cc
+++ components/autofill/content/renderer/autofill_agent.cc
@@ -797,7 +797,7 @@ void AutofillAgent::TriggerSuggestions(
     if (WebElement content_editable =
             form_util::GetContentEditableByRendererId(field_id);
         !content_editable.IsNull()) {
-      ShowSuggestionsForContentEditable(content_editable);
+      ShowSuggestionsForContentEditable(content_editable, trigger_source);
     }
   }
 }
@@ -1059,7 +1059,8 @@ void AutofillAgent::ShowSuggestions(
 }
 
 void AutofillAgent::ShowSuggestionsForContentEditable(
-    const blink::WebElement& element) {
+    const blink::WebElement& element,
+    AutofillSuggestionTriggerSource trigger_source) {
   std::optional<FormData> form = form_util::FindFormForContentEditable(element);
   if (!form) {
     return;
@@ -1067,9 +1068,8 @@ void AutofillAgent::ShowSuggestionsForContentEditable(
   CHECK_EQ(form->fields.size(), 1u);
   if (auto* autofill_driver = unsafe_autofill_driver()) {
     is_popup_possibly_visible_ = true;
-    autofill_driver->AskForValuesToFill(
-        *form, form->fields[0], form->fields[0].bounds,
-        mojom::AutofillSuggestionTriggerSource::kContentEditableClicked);
+    autofill_driver->AskForValuesToFill(*form, form->fields[0],
+                                        form->fields[0].bounds, trigger_source);
   }
 }
 
@@ -1471,7 +1471,9 @@ void AutofillAgent::HandleFocusChangeComplete(
   //   false since at the preceding DidReceiveLeftMouseDownOrGestureTapInNode()
   //   call `node.Focused()` was false.
   if (!focused_element.IsNull()) {
-    ShowSuggestionsForContentEditable(focused_element);
+    ShowSuggestionsForContentEditable(
+        focused_element,
+        AutofillSuggestionTriggerSource::kContentEditableClicked);
   }
 }
 
