44259bc7349f063b4487c8714bdbf9b7afe3f1ea
Tzarial Kuznia <zork@google.com>
2024-03-29 04:15:46 +0000

    [a11y] Remove dead code.
    In MagnificationController, keep_focus_centered_ is never read except in
    a single disabled test.  Removing all dead code associated with this
    variable.
    
    BUG=None
    TEST=Turn on Full-screen magnifier, test with "Zoomed in screen follows
    keyboar focus" on and off.
    
    AX-Relnotes: n/a
    Change-Id: I175eb049f9ac2e9b65f69cbccd61390b9aa04b6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5401372
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Tzarial Kuznia <zork@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1280069}

--- chrome/browser/ash/accessibility/magnification_manager.h
+++ chrome/browser/ash/accessibility/magnification_manager.h
@@ -105,7 +105,6 @@ class MagnificationManager
   void SetProfile(Profile* profile);
 
   void SetMagnifierEnabledInternal(bool enabled);
-  void SetMagnifierKeepFocusCenteredInternal(bool keep_focus_centered);
   void SetMagnifierScaleInternal(double scale);
   void SetMagnifierMouseFollowingModeInternal(
       MagnifierMouseFollowingMode mouse_following_mode);
@@ -120,7 +119,6 @@ class MagnificationManager
   base::TimeTicks last_mouse_event_;
 
   bool fullscreen_magnifier_enabled_ = false;
-  bool keep_focus_centered_ = false;
   double scale_ = 0.0;
 
   base::ScopedObservation<session_manager::SessionManager,
