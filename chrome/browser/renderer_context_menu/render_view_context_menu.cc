34534fe61d897047348a730bb045259b8f40d5c4
Erik Chen <erikchen@chromium.org>
2024-03-29 20:52:29 +0000

    [Reland 2] Add TabFeatures and test harness for mocking out tab-based...
    The first two attempts to land this CL were reverted not due to any
    problem with the CL itself, but because a dependent CL introduced
    flakiness. The flakiness has been diagnosed as a larger issue affecting
    UI tests in general, see https://crbug.com/331859922. This may take time
    to resolve.
    
    This CL decouples itself from the dependent CL, thereby avoiding the
    test flakiness issue.
    
    > This reland has no change (other than a rebase) from the original CL. A
    > precursor CL introduced flakiness to a test, which has since been fixed.
    >
    > > Add TabFeatures and test harness for mocking out tab-based features.
    > >
    > > This CL also hooks up the plumbing for LensOverlayController to
    > > demonstrate how it would be used.
    > >
    > > Change-Id: I24c549da829d6953b4069a64e41dfe948218cefc
    > > Reviewed-on:
    > https://chromium-review.googlesource.com/c/chromium/src/+/5394701
    > > Reviewed-by: David Pennington <dpenning@chromium.org>
    > > Commit-Queue: Erik Chen <erikchen@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1278704}
    >
    > Bug: 331114227
    > Change-Id: I27466e50f3370a130824e34179a7e1b17741f4d8
    > Reviewed-on:
    > https://chromium-review.googlesource.com/c/chromium/src/+/5400458
    > Reviewed-by: David Pennington <dpenning@chromium.org>
    > Commit-Queue: Erik Chen <erikchen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1279332}
    
    Change-Id: Ib898b57d59c29f5a6347de866cc5adbdde222c6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5406084
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1280329}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -106,6 +106,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/side_panel/read_anything/read_anything_side_panel_controller_utils.h"
 #include "chrome/browser/ui/side_search/side_search_utils.h"
 #include "chrome/browser/ui/tab_contents/core_tab_helper.h"
+#include "chrome/browser/ui/tabs/tab_features.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/translate/partial_translate_bubble_model.h"
 #include "chrome/browser/ui/ui_features.h"
@@ -4273,6 +4274,7 @@ void RenderViewContextMenu::ExecRegionSearch(
       lens::features::IsLensOverlayEnabled()) {
     browser->tab_strip_model()
         ->GetActiveTab()
+        ->tab_features()
         ->lens_overlay_controller()
         ->ShowUI();
     return;
--- chrome/browser/ui/browser_tab_strip_model_delegate.h
+++ chrome/browser/ui/browser_tab_strip_model_delegate.h
@@ -66,6 +66,7 @@ class BrowserTabStripModelDelegate : public TabStripModelDelegate {
   void CopyURL(content::WebContents* web_contents) override;
   void GoBack(content::WebContents* web_contents) override;
   bool CanGoBack(content::WebContents* web_contents) override;
+  bool IsNormalWindow() override;
 
   void CloseFrame();
 
