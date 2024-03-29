0f76fbc7fe06522adccec58b4163b98b342efbc1
Alex Ilin <alexilin@chromium.org>
2024-03-29 16:10:53 +0000

    [signin] Make locale optional in AccountInfo
    Do not require `locale` to be filled in `AccountInfo::IsValid()` as this
    field might not be populated by the UserInfo.
    
    Bug: 330384267
    Change-Id: I6acdcaa95d79ff9dd9bfd0fdb1c1ec93925bd0fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5404793
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1280196}

--- components/signin/public/identity_manager/account_info_unittest.cc
+++ components/signin/public/identity_manager/account_info_unittest.cc
@@ -33,8 +33,8 @@ TEST_F(AccountInfoTest, IsEmpty) {
   }
 }
 
-// Tests that IsValid() returns true only when all the fields are non-empty
-// (except is_child_account).
+// Tests that IsValid() returns true only when all mandatory fields are
+// non-empty.
 TEST_F(AccountInfoTest, IsValid) {
   AccountInfo info;
   EXPECT_EQ(signin::Tribool::kUnknown, info.is_child_account);
@@ -46,7 +46,6 @@ TEST_F(AccountInfoTest, IsValid) {
 
   info.full_name = info.given_name = "test_name";
   info.hosted_domain = "test_domain";
-  info.locale = "test_locale";
   info.picture_url = "test_picture_url";
   EXPECT_TRUE(info.IsValid());
 }
@@ -74,6 +73,7 @@ TEST_F(AccountInfoTest, UpdateWithNoModification) {
   info.account_id = CoreAccountId::FromGaiaId("test_id");
   info.is_child_account = signin::Tribool::kTrue;
   info.is_under_advanced_protection = true;
+  info.locale = "en";
   info.access_point = signin_metrics::AccessPoint::ACCESS_POINT_SETTINGS;
 
   AccountInfo other;
@@ -83,10 +83,12 @@ TEST_F(AccountInfoTest, UpdateWithNoModification) {
   EXPECT_EQ(signin_metrics::AccessPoint::ACCESS_POINT_UNKNOWN,
             other.access_point);
   other.is_under_advanced_protection = false;
+  other.locale = "en";
 
   EXPECT_FALSE(info.UpdateWith(other));
   EXPECT_EQ("test_id", info.gaia);
   EXPECT_EQ("test_id", info.email);
+  EXPECT_EQ("en", info.locale);
   EXPECT_EQ(signin_metrics::AccessPoint::ACCESS_POINT_SETTINGS,
             info.access_point);
   EXPECT_EQ(signin::Tribool::kTrue, info.is_child_account);
@@ -102,6 +104,7 @@ TEST_F(AccountInfoTest, UpdateWithSuccessfulUpdate) {
   AccountInfo other;
   other.account_id = CoreAccountId::FromGaiaId("test_id");
   other.full_name = other.given_name = "test_name";
+  other.locale = "fr";
   other.is_child_account = signin::Tribool::kTrue;
   other.access_point = signin_metrics::AccessPoint::ACCESS_POINT_SETTINGS;
   AccountCapabilitiesTestMutator mutator(&other.capabilities);
@@ -113,6 +116,7 @@ TEST_F(AccountInfoTest, UpdateWithSuccessfulUpdate) {
   EXPECT_EQ("test_id", info.email);
   EXPECT_EQ("test_name", info.full_name);
   EXPECT_EQ("test_name", info.given_name);
+  EXPECT_EQ("fr", info.locale);
   EXPECT_EQ(signin_metrics::AccessPoint::ACCESS_POINT_SETTINGS,
             info.access_point);
   EXPECT_EQ(signin::Tribool::kTrue, info.is_child_account);
