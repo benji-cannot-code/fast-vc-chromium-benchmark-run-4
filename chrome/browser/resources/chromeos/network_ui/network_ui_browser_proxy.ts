5e0e3872e4c4ff4e9c536940318faa443e26fbfb
Jason Zhang <jiajunz@google.com>
2024-03-29 18:50:07 +0000

    [CrOS WiFi] Add WiFi Direct client and group info in debug UI
    Add two more sections in chrome://network WiFi Direct tab to show WiFi
    Direct owner and client info.
    
    Screenshot: https://screenshot.googleplex.com/89zRz7VBeiEcHj5
    
    Bug: b/329903911
    Change-Id: I57958fb6542194433b3006ab9968181dba7c13ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5405995
    Reviewed-by: Theo Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Nikhil Nayunigari <nikhilcn@google.com>
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1280274}

--- chrome/browser/resources/chromeos/network_ui/network_ui_browser_proxy.ts
+++ chrome/browser/resources/chromeos/network_ui/network_ui_browser_proxy.ts
@@ -59,6 +59,10 @@ export interface NetworkUiBrowserProxy {
   setTetheringEnabled(enabled: boolean): Promise<string>;
 
   getWifiDirectCapabilities(): Promise<string>;
+
+  getWifiDirectOwnerInfo(): Promise<string>;
+
+  getWifiDirectClientInfo(): Promise<string>;
 }
 
 export class NetworkUiBrowserProxyImpl implements NetworkUiBrowserProxy {
@@ -160,6 +164,14 @@ export class NetworkUiBrowserProxyImpl implements NetworkUiBrowserProxy {
 
   getWifiDirectCapabilities(): Promise<string> {
     return sendWithPromise('getWifiDirectCapabilities');
+  }
+
+  getWifiDirectOwnerInfo(): Promise<string> {
+    return sendWithPromise('getWifiDirectOwnerInfo');
+  }
+
+  getWifiDirectClientInfo(): Promise<string> {
+    return sendWithPromise('getWifiDirectClientInfo');
   }
 
   static getInstance(): NetworkUiBrowserProxy {
