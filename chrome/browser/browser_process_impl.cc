acfddab1c0a4403bb19b0d4ab093087e22efd9a4
Brett Wilson <brettw@chromium.org>
2024-03-29 17:59:12 +0000

    Update SetLocale comments.
    BrowserProcess::SetLocale had a comment that this is only called on
    startup before any threads are created, but this is not true in Ash
    which calls this in response to system locale changes. (On other
    platforms, changing the locale at runtime is either not possible or
    requires a browser restart.) This comment is removed.
    
    Clarify in the header that SetLocale doesn't actually update the UI. The
    Ash code that sets the locale has to do a bunch of other stuff to ensure
    the UI is up-to-date.
    
    Change-Id: I2205723bb54d821ac087fdc4f30d0fec36b37ee4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5407811
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Brett Wilson <brettw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1280245}

--- chrome/browser/browser_process_impl.cc
+++ chrome/browser/browser_process_impl.cc
@@ -972,8 +972,6 @@ const std::string& BrowserProcessImpl::GetApplicationLocale() {
 
 void BrowserProcessImpl::SetApplicationLocale(
     const std::string& actual_locale) {
-  // NOTE: this is called before any threads have been created in non-test
-  // environments.
   locale_ = actual_locale;
   ChromeContentBrowserClient::SetApplicationLocale(actual_locale);
   translate::TranslateDownloadManager::GetInstance()->set_application_locale(
