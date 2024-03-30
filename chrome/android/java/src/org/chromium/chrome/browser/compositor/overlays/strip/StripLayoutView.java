c60a9f8c6e993b160638697fc93edc9a669aef79
Zhe Li <zheliooo@google.com>
2024-03-30 01:21:23 +0000

    [TabGroupIndicators] Account for StripLayoutGroupTitle when reorder tab
    - Reorder StripView when tab drag is interacting with group title.
    - Update drag threshold in #calculateTabGroupThreshold.
    - adjust offset to account for group title.
    
    demo: https://drive.google.com/file/d/1NffEemBZLtCz0WmqSpkn065OjRiP9uX_/view?usp=sharing
    
    Bug: 327506847
    Change-Id: I470e1d36a06e3cccb471004e3dd0d6e01b11ec92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5405135
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Reviewed-by: Neil Coronado <nemco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1280459}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutView.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.compositor.overlays.strip;
 
+import android.util.FloatProperty;
+
 import org.chromium.chrome.browser.layouts.components.VirtualView;
 
 import java.util.List;
@@ -15,6 +17,21 @@ import java.util.List;
  * the GL canvas.
  */
 public abstract class StripLayoutView implements VirtualView {
+
+    /** A property for animations to use for changing the drawX of the view. */
+    public static final FloatProperty<StripLayoutView> DRAW_X =
+            new FloatProperty<>("drawX") {
+                @Override
+                public void setValue(StripLayoutView object, float value) {
+                    object.setDrawX(value);
+                }
+
+                @Override
+                public Float get(StripLayoutView object) {
+                    return object.getDrawX();
+                }
+            };
+
     boolean mVisible = true;
 
     /**
