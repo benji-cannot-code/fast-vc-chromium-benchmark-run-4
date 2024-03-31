b459f9fdc6b8a81203d5767baa5c43945f75ee78
Miao Bin <bin.miao@intel.com>
2024-03-31 03:53:03 +0000

    WebNN: Support gruCell MLOperator
    This CL adds the IDL and mojo definition for gruCell and implements
    `MLGraphBuilder::gruCell()`.
    
    This CL also adds gruCell validation tests in WPT and unit tests in
    `WebNNGraphImplTest`.
    
    Bug: 40206287
    Change-Id: I0873a5c979bdd3e44a0dd30cba7a53410141ef78
    Cq-Include-Trybots: luci.chromium.try:gpu-fyi-try-win11-qualcomm-rel-64, win11-blink-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5376901
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Bin Miao <bin.miao@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1280572}

--- third_party/blink/renderer/modules/ml/webnn/ml_graph_builder.h
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_builder.h
@@ -36,6 +36,7 @@ class MLEluOptions;
 class MLGatherOptions;
 class MLGemmOptions;
 class MLGruOptions;
+class MLGruCellOptions;
 class MLGraph;
 class MLHardSigmoidOptions;
 class MLInstanceNormalizationOptions;
@@ -208,6 +209,14 @@ class MODULES_EXPORT MLGraphBuilder final : public ScriptWrappable {
                                           const uint32_t hidden_size,
                                           MLGruOptions* options,
                                           ExceptionState& exception_state);
+
+  MLOperand* gruCell(const MLOperand* input,
+                     const MLOperand* weight,
+                     const MLOperand* recurrent_weight,
+                     const MLOperand* hidden_state,
+                     const uint32_t hidden_size,
+                     MLGruCellOptions* options,
+                     ExceptionState& exception_state);
 
   MLOperand* hardSigmoid(const MLOperand* input,
                          const MLHardSigmoidOptions* options,
