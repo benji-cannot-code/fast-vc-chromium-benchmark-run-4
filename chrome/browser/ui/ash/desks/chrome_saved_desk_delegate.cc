784e2a24e6a7aedf15d2079ca7a342622eb3e292
Sammie Quon <sammiequon@chromium.org>
2024-04-01 21:42:58 +0000

    desks: Use feedback app id constant
    Test: none
    Bug: none
    Change-Id: I3d79393165f30c7d6df739d78d0d33f05d3bd117
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5407376
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1280888}

--- chrome/browser/ui/ash/desks/chrome_saved_desk_delegate.cc
+++ chrome/browser/ui/ash/desks/chrome_saved_desk_delegate.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/desk_template.h"
 #include "ash/public/cpp/system/toast_data.h"
 #include "ash/public/cpp/system/toast_manager.h"
+#include "ash/public/cpp/window_properties.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "base/check.h"
 #include "base/functional/bind.h"
@@ -30,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/desks/chrome_desks_util.h"
 #include "chrome/browser/ui/ash/desks/desks_client.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/web_applications/web_app_id_constants.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/theme_resources.h"
 #include "components/app_constants/constants.h"
@@ -469,6 +471,12 @@ void ChromeSavedDeskDelegate::LaunchAppsFromSavedDesk(
 bool ChromeSavedDeskDelegate::IsWindowSupportedForSavedDesk(
     aura::Window* window) const {
   if (!window) {
+    return false;
+  }
+
+  const auto* app_id = window->GetProperty(ash::kAppIDKey);
+  // Feedback app is not saved, see http://b/301479278.
+  if (app_id && *app_id == web_app::kOsFeedbackAppId) {
     return false;
   }
 
