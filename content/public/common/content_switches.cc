60a111745e65dfbf6278068309f84c914a510288
Chris Fredrickson <cfredric@chromium.org>
2024-04-01 15:21:39 +0000

    Remove reference to non-existent feature
    Bug: 324456212
    Change-Id: I6454219ddc0157780918406338b0fe3d26bdd24d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5409195
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1280721}

--- content/public/common/content_switches.cc
+++ content/public/common/content_switches.cc
@@ -320,7 +320,6 @@ const char kEnableCaretBrowsing[] = "enable-caret-browsing";
 //   net::features::kSameSiteDefaultChecksMethodRigorously
 //   net::features::kSchemefulSameSite
 //   net::features::kCookieSameSiteConsidersRedirectChain
-//   net::features::kPartitionedCookies
 const char kEnableExperimentalCookieFeatures[] =
     "enable-experimental-cookie-features";
 
