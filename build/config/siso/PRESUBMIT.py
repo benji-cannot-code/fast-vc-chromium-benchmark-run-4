c4c1d02103512940452791964084c44059a7bb5a
Junji Watanabe <jwata@google.com>
2024-04-01 06:20:57 +0000

    [infra] Remove remaining Siso experimental builders
    Siso CQ migration has completed.
    The experimental CQ builders aren't necessary anymore.
    
    Bug: b/277863839
    Change-Id: I0a9b162ff8ca3c55d6354ff4921e99338e09de6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5404095
    Auto-Submit: Junji Watanabe <jwata@google.com>
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1280665}

--- build/config/siso/PRESUBMIT.py
+++ build/config/siso/PRESUBMIT.py
@@ -6,22 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 PRESUBMIT_VERSION = '2.0.0'
 
 
-def CheckTryjobFooters(input_api, output_api):
-  """Check if footers include Cq-Include-Trybots to trigger Siso tryjobs."""
-  footerTryjobs = input_api.change.GitFootersFromDescription().get(
-      'Cq-Include-Trybots', [])
-  if footerTryjobs:
-    return []
-
-  message = (
-      "Missing 'Cq-Include-Trybots:' field required for Siso config changes"
-      "\nPlease add the following fields to run Siso tryjobs.\n\n"
-      "Cq-Include-Trybots: luci.chromium.try:linux_chromium_asan_siso_rel_ng\n"
-      "Cq-Include-Trybots: luci.chromium.try:linux_chromium_tsan_siso_rel_ng\n"
-  )
-  return [output_api.PresubmitPromptWarning(message)]
-
-
 def CheckSisoConfigFormat(input_api, output_api):
   """Check if build/config/siso/*.star files are formatted correctly."""
   repo_root = input_api.change.RepositoryRoot()
