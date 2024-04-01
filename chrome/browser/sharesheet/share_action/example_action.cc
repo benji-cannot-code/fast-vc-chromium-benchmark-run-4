1a65e8ca3d9479406257170160b2fc43d9542e8f
Daniel Classon <dclasson@google.com>
2024-04-01 23:28:27 +0000

    Reland "[Sharesheet] Refactor Sharesheet to map actions by const, not UI string"
    This is a reland of commit 434c4df1bc527dcf13c7050bd083084b1be78f55
    
    Failure reason: MSAN failed for using active_share_action_type_ while
    uninitialized in the *SharesheetBubbleView* tests.
    
    Original change's description:
    > [Sharesheet] Refactor Sharesheet to map actions by const, not UI string
    >
    > Refactors the Sharesheet Cache and related classes to use const enum
    > ShareTypes to distinguish Sharesheet actions, rather than the user
    > visible strings.
    >
    > Clipboard & Quick Share from Files, and Copy to Clipboard, Quick Share,
    > and send to Files from Photos App (Arc++).
    >
    > Bug: b:326633226
    > Test: Unit tests, tested that I could manually perform Copy to
    > Change-Id: If0ddb1410322aad1d8f4439f775ebe1c1b1166b4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5321767
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Commit-Queue: Daniel Classon <dclasson@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1280233}
    
    Bug: b:326633226
    Test: Locally ran MSAN unit_tests and browser_tests.
    Change-Id: I7f160033c9bc1bf02db4be022680d2e8430d5d71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5408136
    Commit-Queue: Daniel Classon <dclasson@google.com>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1280922}

--- chrome/browser/sharesheet/share_action/example_action.cc
+++ chrome/browser/sharesheet/share_action/example_action.cc
@@ -19,6 +19,10 @@ ExampleAction::ExampleAction() {
 
 ExampleAction::~ExampleAction() = default;
 
+ShareActionType ExampleAction::GetActionType() const {
+  return ShareActionType::kExample;
+}
+
 const std::u16string ExampleAction::GetActionName() {
   return base::ASCIIToUTF16(name_);
 }
