4c0ec56bb462f1cdf81a4d85e11f6ca35a73ff7e
Li Lin <llin@chromium.org>
2024-04-01 17:01:08 +0000

    Minor clean ups.
    1. Remove duplicated functions.
    2. Remove error logging for an expected valid case.
    
    Bug: b/331961870
    Change-Id: Ib3614b1bac406b6a668a458c51fa5687140161c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5407992
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Li Lin <llin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1280746}

--- chromeos/ash/components/growth/campaigns_manager_unittest.cc
+++ chromeos/ash/components/growth/campaigns_manager_unittest.cc
@@ -1084,11 +1084,6 @@ TEST_F(CampaignsManagerTest, GetSchedulingCampaignInvalidTargeting) {
   VerifyDemoModePayload(
       campaigns_manager_->GetCampaignBySlot(Slot::kDemoModeApp));
 
-  histogram_tester.ExpectBucketCount(
-      kCampaignsManagerErrorHistogramName,
-      CampaignsManagerError::kInvalidSchedulingTargeting,
-      /*count=*/1);
-
   histogram_tester.ExpectBucketCount(kCampaignsManagerErrorHistogramName,
                                      CampaignsManagerError::kInvalidCampaign,
                                      /*count=*/1);
