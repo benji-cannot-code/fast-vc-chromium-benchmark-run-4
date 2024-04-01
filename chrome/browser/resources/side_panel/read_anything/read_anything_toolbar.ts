350bb7a8969229286a9e711eac2042826c0dd5a8
Lauren Winston <lwinston@google.com>
2024-04-01 20:25:59 +0000

    [Read Aloud] Fix font-select assertion failure.
    This was causing checkmarks to not render properly when the Read Aloud
    flag was disabled because the font-select assertion kept failing.
    
    Note: renaming the property from 'font-select' to 'fontSelect' didn't
    fix the issue.
    
    Bug:332295085
    
    Change-Id: Ib4d924776631198e351720fac231c8dbdf65b053
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5410182
    Reviewed-by: Eitan Goldberger <eitang@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1280849}

--- chrome/browser/resources/side_panel/read_anything/read_anything_toolbar.ts
+++ chrome/browser/resources/side_panel/read_anything/read_anything_toolbar.ts
@@ -41,7 +41,6 @@ export interface ReadAnythingToolbarElement {
     moreOptionsMenu: CrActionMenuElement,
     voiceSelectionMenu: VoiceSelectionMenuElement,
     toolbarContainer: HTMLElement,
-    fontSelect: HTMLSelectElement,
     more: CrIconButtonElement,
   };
 }
@@ -418,7 +417,8 @@ export class ReadAnythingToolbarElement extends ReadAnythingToolbarElementBase {
           this.$.fontMenu.getIfExists(), currentFontIndex);
 
     } else {
-      const select = this.$.fontSelect;
+      const select = this.$.toolbarContainer.querySelector<HTMLSelectElement>(
+          '#font-select');
       assert(select, 'no font select menu');
       select.selectedIndex = currentFontIndex;
     }
@@ -779,7 +779,8 @@ export class ReadAnythingToolbarElement extends ReadAnythingToolbarElementBase {
 
     // Allow focusing the font selection if it's visible.
     if (!this.isReadAloudEnabled_) {
-      const select = this.$.fontSelect;
+      const select = this.$.toolbarContainer.querySelector<HTMLSelectElement>(
+          '#font-select');
       assert(select, 'no font select menu');
       focusableElements.unshift(select);
     }
