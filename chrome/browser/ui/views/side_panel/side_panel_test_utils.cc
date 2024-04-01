8c9a8dd15c92cf05ad6407ed1380b7e83bcd59b2
Caroline Rising <corising@chromium.org>
2024-04-01 23:29:21 +0000

    Animate side panel opening and closing.
    Create an animation that slides the side panel out from the side of the
    browser. This updates the side panel and side panel coordinator to use
    an enum, SidePanelContentState, to convey when a view should be hidden
    and allow us to delay hiding it until the side panel has been closed.
    
    Bug: 328295071
    Change-Id: I40b099a1a3d0469ba815caf152fb0ecc3562af2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5406892
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1280923}

---
+++ chrome/browser/ui/views/side_panel/side_panel_test_utils.cc
@@ -0,0 +1,33 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/side_panel/side_panel_test_utils.h"
+
+#include <memory>
+
+#include "base/run_loop.h"
+#include "chrome/browser/ui/views/side_panel/side_panel_coordinator.h"
+
+SidePanelWaiter::SidePanelWaiter(SidePanelCoordinator* side_panel_coordinator)
+    : side_panel_coordinator_(side_panel_coordinator) {
+  side_panel_coordinator->AddSidePanelViewStateObserver(this);
+}
+
+SidePanelWaiter::~SidePanelWaiter() {
+  side_panel_coordinator_->RemoveSidePanelViewStateObserver(this);
+}
+
+void SidePanelWaiter::WaitForSidePanelClose() {
+  if (side_panel_coordinator_->IsSidePanelShowing()) {
+    side_panel_close_run_loop_ = std::make_unique<base::RunLoop>();
+    side_panel_close_run_loop_->Run();
+  }
+}
+
+void SidePanelWaiter::OnSidePanelDidClose() {
+  if (side_panel_close_run_loop_ && side_panel_close_run_loop_->running()) {
+    side_panel_close_run_loop_->Quit();
+  }
+}
