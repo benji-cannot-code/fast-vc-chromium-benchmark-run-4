965ecd2d7b0c865b5e8965bdb889bb1a3a0df9ca
Cam Bickel <cambickel@google.com>
2024-04-01 17:09:49 +0000

    brightness: Add metric 'BrightnessSliderAdjusted'
    Test: unit_tests --gtest_filter=*DisplaySettingsProviderTest*
    Bug: b/328287584
    Change-Id: I2e60dac5a512ae1a1e0c675c562cda904f94aae7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5401457
    Commit-Queue: Camden Bickel <cambickel@google.com>
    Reviewed-by: Wenyu Zhang <zhangwenyu@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1280749}

--- chrome/browser/ui/webui/ash/settings/pages/device/display_settings/display_settings_provider.cc
+++ chrome/browser/ui/webui/ash/settings/pages/device/display_settings/display_settings_provider.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/brightness_control_delegate.h"
 #include "base/functional/bind.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/strings/strcat.h"
 #include "chrome/browser/ui/webui/ash/settings/pages/device/display_settings/display_settings_provider.mojom.h"
 #include "chromeos/dbus/power_manager/backlight.pb.h"
 #include "ui/display/manager/display_manager.h"
@@ -298,6 +299,11 @@ void DisplaySettingsProvider::SetInternalDisplayScreenBrightness(
   }
 
   brightness_control_delegate_->SetBrightnessPercent(percent, /*gradual=*/true);
+
+  // Record the brightness change event.
+  std::string histogram_name(base::StrCat(
+      {kDisplaySettingsHistogramName, ".Internal.BrightnessSliderAdjusted"}));
+  base::UmaHistogramPercentage(histogram_name, percent);
 }
 
 }  // namespace ash::settings
