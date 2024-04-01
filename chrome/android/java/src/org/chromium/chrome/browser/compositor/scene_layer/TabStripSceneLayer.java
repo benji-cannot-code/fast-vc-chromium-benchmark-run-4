37c97a4c6badfe144902c8568ea145cd32c226fa
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2024-04-01 21:12:37 +0000

    Revert "[TSLO] Move tab scroll layer to increase tab strip height"
    This reverts commit f2cfe977cc21defdf1b7506184c50d96d96364a8.
    
    Reason for revert:
    LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8751826434271318369
    
    Sample failed build: https://ci.chromium.org/b/8751826434271318369
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F5388592&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8751826434271318369&type=BUG
    
    Original change's description:
    > [TSLO] Move tab scroll layer to increase tab strip height
    >
    > This CL changes the tab strip height by shifting the scroll_tab_layer downwards with the size equals to the added |topPadding|, so the size of the StripLayoutHelper remains 40dp while the scene layer can go taller than that.
    >
    > The integration for the changes is in a follow up crrev.com/c/5402928.
    > Preview:
    > https://screenshot.googleplex.com/TcgR3Zzfm38Ph3C
    > https://screenshot.googleplex.com/kdhBTxYF6fCNsev
    >
    > Bug: 325351108
    > Change-Id: Iecefb79abeb8243ce74e9bfbf7402a4f4e02386c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5388592
    > Reviewed-by: Neil Coronado <nemco@google.com>
    > Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1280861}
    >
    
    Bug: 325351108
    Change-Id: I4cfa01c631851b849a92202e72d151cf2a0b97db
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5411092
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1280872}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/scene_layer/TabStripSceneLayer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/scene_layer/TabStripSceneLayer.java
@@ -88,8 +88,7 @@ public class TabStripSceneLayer extends SceneOverlayLayer {
             int scrimColor,
             float scrimOpacity,
             float leftPaddingDp,
-            float rightPaddingDp,
-            float topPaddingDp) {
+            float rightPaddingDp) {
         if (mNativePtr == 0) return;
         final boolean visible = yOffset > -layoutHelper.getHeight();
         // This will hide the tab strips if necessary.
@@ -97,11 +96,9 @@ public class TabStripSceneLayer extends SceneOverlayLayer {
                 .beginBuildingFrame(mNativePtr, TabStripSceneLayer.this, visible);
         // When strip tabs are completely off screen, we don't need to update it.
         if (visible) {
-            // Ceil the padding to avoid off-by-one issues similar to crbug/329722454. This is
-            // required since these values are originated from Android UI.
-            float leftPaddingPx = (float) Math.ceil(leftPaddingDp * mDpToPx);
-            float rightPaddingPx = (float) Math.ceil(rightPaddingDp * mDpToPx);
-            float topPaddingPx = (float) Math.ceil(topPaddingDp * mDpToPx);
+            // Round the padding to avoid off-by-one issues similar to crbug/329722454.
+            float leftPaddingPx = Math.round(leftPaddingDp * mDpToPx);
+            float rightPaddingPx = Math.round(rightPaddingDp * mDpToPx);
             pushButtonsAndBackground(
                     layoutHelper,
                     resourceManager,
@@ -109,8 +106,7 @@ public class TabStripSceneLayer extends SceneOverlayLayer {
                     scrimColor,
                     scrimOpacity,
                     leftPaddingPx,
-                    rightPaddingPx,
-                    topPaddingPx);
+                    rightPaddingPx);
             pushStripTabs(
                     layoutHelper,
                     layerTitleCache,
@@ -130,8 +126,7 @@ public class TabStripSceneLayer extends SceneOverlayLayer {
             @ColorInt int scrimColor,
             float scrimOpacity,
             float leftPaddingPx,
-            float rightPaddingPx,
-            float topPaddingPx) {
+            float rightPaddingPx) {
         final int width = Math.round(layoutHelper.getWidth() * mDpToPx);
         final int height = Math.round(layoutHelper.getHeight() * mDpToPx);
         TabStripSceneLayerJni.get()
@@ -145,8 +140,7 @@ public class TabStripSceneLayer extends SceneOverlayLayer {
                         scrimColor,
                         scrimOpacity,
                         leftPaddingPx,
-                        rightPaddingPx,
-                        topPaddingPx);
+                        rightPaddingPx);
 
         TintedCompositorButton newTabButton = layoutHelper.getNewTabButton();
         CompositorButton modelSelectorButton = layoutHelper.getModelSelectorButton();
@@ -161,7 +155,6 @@ public class TabStripSceneLayer extends SceneOverlayLayer {
                         newTabButton.getShouldApplyHoverBackground(),
                         newTabButton.getDrawX() * mDpToPx,
                         newTabButton.getDrawY() * mDpToPx,
-                        topPaddingPx,
                         layoutHelper.getNewTabBtnVisualOffset() * mDpToPx,
                         newTabButtonVisible,
                         newTabButton.getTint(),
@@ -317,8 +310,7 @@ public class TabStripSceneLayer extends SceneOverlayLayer {
                 @ColorInt int scrimColor,
                 float scrimOpacity,
                 float leftPaddingPx,
-                float rightPaddingPx,
-                float topPaddingPx);
+                float rightPaddingPx);
 
         void updateNewTabButton(
                 long nativeTabStripSceneLayer,
@@ -328,7 +320,6 @@ public class TabStripSceneLayer extends SceneOverlayLayer {
                 boolean isHovered,
                 float x,
                 float y,
-                float topPadding,
                 float touchTargetOffset,
                 boolean visible,
                 int tint,
--- chrome/android/junit/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperManagerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperManagerTest.java
@@ -560,7 +560,6 @@ public class StripLayoutHelperManagerTest {
     }
 
     @Test
-    @EnableFeatures(ChromeFeatureList.TAB_STRIP_LAYOUT_OPTIMIZATION)
     public void testGetUpdatedSceneOverlayTree() {
         // Setup and stub required mocks.
         int hoveredTabId = 1;
@@ -578,9 +577,7 @@ public class StripLayoutHelperManagerTest {
         // Update the paddings.
         float leftPadding = 10f;
         float rightPadding = 20f;
-        int topPaddingPx = 5;
         mStripLayoutHelperManager.updateHorizontalPaddings(leftPadding, rightPadding);
-        mStripLayoutHelperManager.onHeightChanged(TAB_STRIP_HEIGHT_PX + topPaddingPx);
 
         // Invoke the method.
         mStripLayoutHelperManager.getUpdatedSceneOverlayTree(
@@ -600,8 +597,7 @@ public class StripLayoutHelperManagerTest {
                         anyInt(),
                         anyFloat(),
                         eq(leftPadding),
-                        eq(rightPadding),
-                        eq((float) topPaddingPx));
+                        eq(rightPadding));
     }
 
     @Test
@@ -670,7 +666,6 @@ public class StripLayoutHelperManagerTest {
                         eq(mToolbarPrimaryColor),
                         /* scrimOpacity= */ eq(0f),
                         anyFloat(),
-                        anyFloat(),
                         anyFloat());
 
         // With tab strip transition, the yOffset will be forced to be 0.
@@ -695,7 +690,6 @@ public class StripLayoutHelperManagerTest {
                         eq(mToolbarPrimaryColor),
                         /* scrimOpacity= */ eq(expectedOpacity),
                         anyFloat(),
-                        anyFloat(),
                         anyFloat());
 
         // With tab strip transition finished, the yOffset will be forced to be the negative of the
@@ -715,7 +709,6 @@ public class StripLayoutHelperManagerTest {
                         anyInt(),
                         eq(mToolbarPrimaryColor),
                         /* scrimOpacity= */ eq(0f),
-                        anyFloat(),
                         anyFloat(),
                         anyFloat());
 
@@ -855,7 +848,6 @@ public class StripLayoutHelperManagerTest {
                         eq(scrimColor),
                         /* scrimOpacity= */ eq(0f),
                         anyFloat(),
-                        anyFloat(),
                         anyFloat());
 
         // With tab strip transition, the yOffset will be forced to be 0.
@@ -881,7 +873,6 @@ public class StripLayoutHelperManagerTest {
                         eq(scrimColor),
                         /* scrimOpacity= */ eq(expectedOpacity),
                         anyFloat(),
-                        anyFloat(),
                         anyFloat());
 
         // When transition finished while tabs strip showing, yOffset will be forwarded to cc
@@ -902,7 +893,6 @@ public class StripLayoutHelperManagerTest {
                         eq(scrimColor),
                         /* scrimOpacity= */ eq(0f),
                         anyFloat(),
-                        anyFloat(),
                         anyFloat());
 
         // Verify StatusBarColorController method invocations.
@@ -921,7 +911,7 @@ public class StripLayoutHelperManagerTest {
     }
 
     @Test
-    public void testTouchEventsIgnoredOnPaddings() {
+    public void testTouchEventsIgnoredOnMargins() {
         // Update the size and paddings.
         float leftPadding = 10f;
         float rightPadding = 20f;
@@ -951,28 +941,6 @@ public class StripLayoutHelperManagerTest {
         assertTrue(
                 "Event on not on margin should be handled.",
                 motionEvenHandled(SCREEN_WIDTH - rightPadding - 1, yCenterOfStrip));
-    }
-
-    @Test
-    @EnableFeatures(ChromeFeatureList.TAB_STRIP_LAYOUT_OPTIMIZATION)
-    public void testTopPadding() {
-        int topPadding = 10;
-        mStripLayoutHelperManager.onHeightChanged(TAB_STRIP_HEIGHT_PX + topPadding);
-        mStripLayoutHelperManager.onSizeChanged(
-                SCREEN_WIDTH, SCREEN_HEIGHT, VISIBLE_VIEWPORT_Y, ORIENTATION);
-
-        assertFalse(
-                "Event on top padding should not be handled.",
-                motionEvenHandled(SCREEN_WIDTH / 2, 0));
-        assertFalse(
-                "Event on top padding should not be handled.",
-                motionEvenHandled(SCREEN_WIDTH / 2, topPadding - 1));
-        assertTrue(
-                "Event should be handled below top padding.",
-                motionEvenHandled(SCREEN_WIDTH / 2, topPadding));
-        assertTrue(
-                "Ensure top padding increase the entire height",
-                motionEvenHandled(SCREEN_WIDTH / 2, topPadding + TAB_STRIP_HEIGHT_PX - 1));
     }
 
     private boolean motionEvenHandled(float x, float y) {
