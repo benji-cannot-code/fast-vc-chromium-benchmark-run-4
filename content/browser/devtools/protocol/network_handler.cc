18a5b348d643f3fd33c18aea0461c3941cbb365f
Shuran Huang <shuuran@chromium.org>
2024-04-02 09:34:01 +0000

    Include cookieline string in ExemptedSetCookieWithReason
    This is for accurately mapping exempted cookies to parsed response
    cookies in https://source.chromium.org/chromium/_/chromium/devtools/devtools-frontend/+/b4de8af03d5e829476ffaefe793d946bfe93b5dd:front_end/panels/network/RequestCookiesView.ts;l=245;drc=0ce3e2b1dd5f1934dde87cc89236f940bcdb9945;bpv=1;bpt=0
    
    Bug: 41491846
    Change-Id: I704033a8b8cba5677d88fb843d345a59a46d23de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5362013
    Auto-Submit: Shuran Huang <shuuran@chromium.org>
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281074}

--- content/browser/devtools/protocol/network_handler.cc
+++ content/browser/devtools/protocol/network_handler.cc
@@ -1014,6 +1014,7 @@ BuildProtocolExemptedSetCookies(
       protocol_list->push_back(
           Network::ExemptedSetCookieWithReason::Create()
               .SetExemptionReason(std::move(exemption_reason))
+              .SetCookieLine(cookie.cookie_string)
               .SetCookie(BuildCookie(cookie.cookie.value()))
               .Build());
     }
