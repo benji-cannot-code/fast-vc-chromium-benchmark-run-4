9f786cae269ea14bcaeef698690f833df0bc302c
Duncan Mercer <mercerd@google.com>
2024-04-02 19:35:43 +0000

    Add selecting text logic for lens overlay
    This CL adds logic for selecting text. A follow up CL will
    be responsible for doing something with this selection.
    
    Demo: https://screencast.googleplex.com/cast/NDk1NDcwMjAwODU0OTM3NnxiNWUzNGExOS1hNg
    
    Bug: b/328294794
    Change-Id: I293059e5ec5c4e29e0076e6411e3018ee6c09c44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5398378
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281384}

--- chrome/browser/resources/lens/overlay/selection_overlay.ts
+++ chrome/browser/resources/lens/overlay/selection_overlay.ts
@@ -10,9 +10,10 @@ import type {RectF} from '//resources/mojo/ui/gfx/geometry/mojom/geometry.mojom-
 import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {BrowserProxyImpl} from './browser_proxy.js';
-import {type RegionSelectionElement} from './region_selection.js';
+import type {RegionSelectionElement} from './region_selection.js';
 import {getTemplate} from './selection_overlay.html.js';
 import {DRAG_THRESHOLD, DragFeature, emptyGestureEvent, type GestureEvent, GestureState} from './selection_utils.js';
+import type {TextLayerElement} from './text_layer.js';
 
 /**
  * Returns a mojo RectF corresponding to the gesture provided.
@@ -26,7 +27,11 @@ function getRectFromGesture(gesture: GestureEvent): RectF {
 }
 
 export interface SelectionOverlayElement {
-  $: {regionSelectionLayer: RegionSelectionElement};
+  $: {
+    regionSelectionLayer: RegionSelectionElement,
+    selectionOverlay: HTMLElement,
+    textSelectionLayer: TextLayerElement,
+  };
 }
 
 /*
@@ -71,6 +76,11 @@ export class SelectionOverlayElement extends PolymerElement {
       clientX: event.clientX,
       clientY: event.clientY,
     };
+
+    if (this.$.textSelectionLayer.handleDownGesture(this.currentGesture)) {
+      // Text is responding to this sequence of gestures.
+      this.draggingRespondent = DragFeature.TEXT;
+    }
   }
 
   private onPointerUp(event: PointerEvent) {
@@ -87,6 +97,8 @@ export class SelectionOverlayElement extends PolymerElement {
         if (this.draggingRespondent === DragFeature.MANUAL_REGION) {
           BrowserProxyImpl.getInstance().handler.issueLensRequest(
               getRectFromGesture(this.currentGesture));
+        } else if (this.draggingRespondent === DragFeature.TEXT) {
+          this.$.textSelectionLayer.handleUpGesture();
         }
         break;
       case GestureState.STARTING:
@@ -98,6 +110,7 @@ export class SelectionOverlayElement extends PolymerElement {
 
     // After features have responded to the event, reset the current drag state.
     this.currentGesture = emptyGestureEvent();
+    this.draggingRespondent = DragFeature.NONE;
   }
 
   private onPointerMove(event: PointerEvent) {
@@ -111,9 +124,14 @@ export class SelectionOverlayElement extends PolymerElement {
     if (this.isDragging()) {
       this.currentGesture.state = GestureState.DRAGGING;
 
-      // Let the features respond to the current drag.
-      this.draggingRespondent = DragFeature.MANUAL_REGION;
-      this.$.regionSelectionLayer.handleDragGesture(this.currentGesture);
+      if (this.draggingRespondent === DragFeature.TEXT) {
+        this.$.textSelectionLayer.handleDragGesture(this.currentGesture);
+      } else {
+        // Let the features respond to the current drag if no other feature
+        // responded first.
+        this.draggingRespondent = DragFeature.MANUAL_REGION;
+        this.$.regionSelectionLayer.handleDragGesture(this.currentGesture);
+      }
     }
   }
 
