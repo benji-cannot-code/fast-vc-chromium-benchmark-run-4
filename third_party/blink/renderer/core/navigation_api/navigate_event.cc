5e9c9ddf36a7534d312572acd15a8a8f72094f01
Brendon Tiszka <tiszka@chromium.org>
2024-04-02 23:00:35 +0000

    Add missing return statement in NavigationEvent
    Bug: 324909819
    Change-Id: I7974808c2268cc72924959c1d2c7dec753cab5c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5414494
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Brendon Tiszka <tiszka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281521}

--- third_party/blink/renderer/core/navigation_api/navigate_event.cc
+++ third_party/blink/renderer/core/navigation_api/navigate_event.cc
@@ -206,6 +206,7 @@ void NavigateEvent::commit(ExceptionState& exception_state) {
     exception_state.ThrowDOMException(DOMExceptionCode::kInvalidStateError,
                                       "commit() may only be used if { commit: "
                                       "'after-transition' } was specified.");
+    return;
   }
   if (IsBeingDispatched()) {
     exception_state.ThrowDOMException(
