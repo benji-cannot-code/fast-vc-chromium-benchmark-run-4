df803550c57a210a4e5ab2aaed8793cfd5fff40b
John Stiles <johnstiles@google.com>
2024-04-02 22:17:55 +0000

    Implement naive version of `endWebGPUAccess`.
    This implementation copies the WebGPU texture back onto the canvas
    resource using OverwriteImage.
    
    Tests for this feature are disabled on Linux (since WebGPU is not
    finished).
    
    Bug: 41490345
    Change-Id: Ifd7a71314edfa1a216d990553d1923a8c89d560a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5388339
    Commit-Queue: John Stiles <johnstiles@google.com>
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281496}

---
+++ third_party/blink/web_tests/wpt_internal/webgpu/canvas_webgpu_access/endWebGPUAccess-canvas-readback-rgba16f.https.worker.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=worker
+
+// ============================================================================
+
+importScripts("/resources/testharness.js");
+importScripts("./webgpu-helpers.js");
+
+// This test parallels endWebGPUAccess-canvas-readback.https.html.
+promise_test(() => {
+    return with_webgpu((adapter, adapterInfo, device) => {
+      return test_endWebGPUAccess_canvas_readback(
+          adapterInfo,
+          device,
+          new OffscreenCanvas(50, 50),
+          {colorSpace: 'srgb', pixelFormat: 'float16'});
+    });
+  },
+  'endWebGPUAccess() should preserve texture changes on an RGBA16F canvas ' +
+  'when called from a worker.'
+);
+
+done();
