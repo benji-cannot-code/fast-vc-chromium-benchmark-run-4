24755679d21dabf70396b6038dccec2c05e9926e
Andre Le <leandre@chromium.org>
2024-04-02 06:35:41 +0000

    QA+Mahi: Apply ViewShadow to ReadWriteCardsView
    Add shadow support to ReadWriteCardsView so that each view in the widget
    can show shadow. Also remove the shadow in the widget.
    
    Fixed: b:327786910
    Change-Id: If0dcc95c08232b000c740e0296a68ead15192356
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5403541
    Reviewed-by: Andrew Xu <andrewxu@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281035}

--- chrome/browser/ui/chromeos/read_write_cards/read_write_cards_view.cc
+++ chrome/browser/ui/chromeos/read_write_cards/read_write_cards_view.cc
@@ -10,12 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/views/view.h"
+#include "ui/views/view_shadow.h"
 
 namespace chromeos {
 
 ReadWriteCardsView::ReadWriteCardsView(
     chromeos::ReadWriteCardsUiController& read_write_cards_ui_controller)
-    : read_write_cards_ui_controller_(read_write_cards_ui_controller) {}
+    : view_shadow_(std::make_unique<views::ViewShadow>(this, /*elevation=*/2)),
+      read_write_cards_ui_controller_(read_write_cards_ui_controller) {}
 
 ReadWriteCardsView::~ReadWriteCardsView() = default;
 
