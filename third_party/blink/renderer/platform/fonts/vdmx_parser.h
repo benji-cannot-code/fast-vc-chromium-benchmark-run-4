ce3fd9a586c95253ab2c220cfce562b4d1385e4a
danakj <danakj@chromium.org>
2024-04-02 21:52:14 +0000

    Spanify ParseVDMX
    The function now reads big endian values from a span, convert it to use
    BigEndianReader and base::numerics byte conversions.
    
    Add signed integer reading methods to BigEndianReader to avoid
    static_casts in code that wants signed values.
    
    R=jbroman@chromium.org
    
    Bug: 40284755
    Change-Id: I74edd053e20df61f1e9cd7d31261ef08fcd3b1ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5405599
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281472}

--- third_party/blink/renderer/platform/fonts/vdmx_parser.h
+++ third_party/blink/renderer/platform/fonts/vdmx_parser.h
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 bool ParseVDMX(int* ymax,
                int* ymin,
-               const uint8_t* vdmx,
+               const uint8_t* vdmx_ptr,
                size_t vdmx_length,
                unsigned target_pixel_size);
 }  // namespace blink
