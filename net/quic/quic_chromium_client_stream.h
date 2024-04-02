95f714772e25fa079f2e7bfcb454f9410765431b
Ciara McMullin <ciaramcmullin@google.com>
2024-04-02 14:38:19 +0000

    Drop datagrams in WriteConnectUdpPayload if QUIC session does not support HTTP/3 Datagrams yet.
    Bug: 329250105
    Change-Id: Ib1dff34607db9109908effdea6dd1bb7e507924d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5401478
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Reviewed-by: Dustin Mitchell <djmitche@chromium.org>
    Commit-Queue: Ciara McMullin <ciaramcmullin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1281160}

--- net/quic/quic_chromium_client_stream.h
+++ net/quic/quic_chromium_client_stream.h
@@ -302,6 +302,9 @@ class NET_EXPORT_PRIVATE QuicChromiumClientStream
     return can_migrate_to_cellular_network_;
   }
 
+  // True if the underlying QUIC session supports HTTP/3 Datagrams.
+  bool SupportsH3Datagram() const;
+
   // True if this stream is the first data stream created on this session.
   bool IsFirstStream();
 
@@ -311,6 +314,10 @@ class NET_EXPORT_PRIVATE QuicChromiumClientStream
 
   bool DeliverTrailingHeaders(spdy::Http2HeaderBlock* header_block,
                               int* frame_len);
+
+  static constexpr char kHttp3DatagramDroppedHistogram[] =
+      "Net.QuicChromiumClientStream."
+      "Http3DatagramDroppedOnWriteConnectUdpPayload";
 
   using quic::QuicSpdyStream::HasBufferedData;
   using quic::QuicStream::sequencer;
