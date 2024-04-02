2889928c8e84947c945a9a19f1fc7ecd1b0a4df7
Wes Okuhara <wesokuhara@google.com>
2024-04-02 18:41:50 +0000

    CrOS Settings: Convert cellular networks list test to TS
    Bug: b:270728282
    Test: browser_tests --gtest_filter="*OSSettings*CellularNetworksList*"
    Change-Id: Ifa9d0d94ff6d9f171630d58e69a6a677c68972e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5411243
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281335}

--- ash/webui/common/resources/network/network_list.d.ts
+++ ash/webui/common/resources/network/network_list.d.ts
@@ -5,10 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {LegacyElementMixin} from '//resources/polymer/v3_0/polymer/lib/legacy/legacy-element-mixin.js';
 
+import {NetworkList} from './network_list_types.js';
 import {OncMojo} from './onc_mojo.js';
 
 interface NetworkListElement extends LegacyElementMixin, HTMLElement {
   networks: OncMojo.NetworkStateProperties[];
+  customItems: NetworkList.CustomItemState[];
 }
 
 declare global {
