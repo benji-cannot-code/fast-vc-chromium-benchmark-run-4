8208f583d3604c97df53ab21f0ed244929598ba8
Omer Katz <omerkatz@chromium.org>
2024-04-02 08:29:40 +0000

    [blink_gc_plugin] Enable raw_ref check and remove enabled flags
    Bug: 331100928, 41492939
    Change-Id: I70ce8f0d1cf491af2928d90df2893e04720bec4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402584
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281057}

--- tools/clang/blink_gc_plugin/BadPatternFinder.cpp
+++ tools/clang/blink_gc_plugin/BadPatternFinder.cpp
@@ -104,13 +104,9 @@ class OptionalOrRawPtrToGCedMatcher : public MatchFinder::MatchCallback {
  public:
   OptionalOrRawPtrToGCedMatcher(DiagnosticsReporter& diagnostics,
                                 RecordCache& record_cache,
-                                bool check_traceable,
-                                bool check_raw_ptr,
                                 bool check_raw_ref)
       : diagnostics_(diagnostics),
         record_cache_(record_cache),
-        check_traceable_(check_traceable),
-        check_raw_ptr_(check_raw_ptr),
         check_raw_ref_(check_raw_ref) {}
 
   void Register(MatchFinder& match_finder) {
@@ -120,10 +116,8 @@ class OptionalOrRawPtrToGCedMatcher : public MatchFinder::MatchCallback {
         classTemplateSpecializationDecl(
             hasAnyName("::absl::optional", "::std::optional", "::base::raw_ptr",
                        "::base::raw_ref"),
-            hasTemplateArgument(
-                0, refersToType(check_traceable_ ? anyOf(GarbageCollectedType(),
-                                                         TraceableType())
-                                                 : GarbageCollectedType())))
+            hasTemplateArgument(0, refersToType(anyOf(GarbageCollectedType(),
+                                                      TraceableType()))))
             .bind("type"));
     // Only check fields. Optional variables on stack will be found by
     // conservative stack scanning.
@@ -137,9 +131,6 @@ class OptionalOrRawPtrToGCedMatcher : public MatchFinder::MatchCallback {
   void run(const MatchFinder::MatchResult& result) override {
     auto* type = result.Nodes.getNodeAs<clang::CXXRecordDecl>("type");
     bool is_raw_ptr = (type->getName() == "raw_ptr");
-    if (is_raw_ptr && !check_raw_ptr_) {
-      return;
-    }
     bool is_raw_ref = (type->getName() == "raw_ref");
     if (is_raw_ref && !check_raw_ref_) {
       return;
@@ -174,8 +165,6 @@ class OptionalOrRawPtrToGCedMatcher : public MatchFinder::MatchCallback {
  private:
   DiagnosticsReporter& diagnostics_;
   RecordCache& record_cache_;
-  const bool check_traceable_;
-  const bool check_raw_ptr_;
   const bool check_raw_ref_;
 };
 
@@ -549,8 +538,7 @@ void FindBadPatterns(clang::ASTContext& ast_context,
   unique_ptr_gc.Register(match_finder);
 
   OptionalOrRawPtrToGCedMatcher optional_or_rawptr_gc(
-      diagnostics, record_cache, options.enable_optional_of_traceable_check,
-      options.enable_raw_ptr_of_gced_or_traceable_check,
+      diagnostics, record_cache,
       options.enable_raw_ref_of_gced_or_traceable_check);
   optional_or_rawptr_gc.Register(match_finder);
 
