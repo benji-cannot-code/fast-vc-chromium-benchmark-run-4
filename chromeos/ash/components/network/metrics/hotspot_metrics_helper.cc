d01ab1387a35805e45e7e2dfd0faf92d6efe070a
Jason Zhang <jiajunz@google.com>
2024-04-02 22:44:02 +0000

    [CrOS Hotspot] Emit invalid bucket to Disable.OperationResult metric
    Bug: b/332576468
    Change-Id: I00122797772fc1739758bbd431af44dcd1528cab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5415917
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Reviewed-by: Nikhil Nayunigari <nikhilcn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1281512}

--- chromeos/ash/components/network/metrics/hotspot_metrics_helper.cc
+++ chromeos/ash/components/network/metrics/hotspot_metrics_helper.cc
@@ -145,6 +145,8 @@ HotspotMetricsHelper::GetSetEnabledMetricsResult(
       return HotspotMetricsSetEnabledResult::kAlreadyFulfilled;
     case HotspotControlResult::kAborted:
       return HotspotMetricsSetEnabledResult::kAborted;
+    case HotspotControlResult::kInvalid:
+      return HotspotMetricsSetEnabledResult::kInvalid;
     default:
       return HotspotMetricsSetEnabledResult::kUnknownFailure;
   }
