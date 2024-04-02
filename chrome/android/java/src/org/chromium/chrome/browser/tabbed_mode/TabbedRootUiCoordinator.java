f4b38c530b9efe43c9bdc17c0e83411fe78d533a
Brandon Fong <bjfong@google.com>
2024-04-02 17:47:56 +0000

    [Tab Group Parity] Null check TabSwitcherSupplier on show dialog
    Add a null check to the refresh tab list runnable when showing the group
    creation dialog, to prevent any crashes.
    
    Bug: 332233928
    Change-Id: Ief2d702b3649905e2f3e5144aafb30deed57d28b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5413833
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281287}

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
@@ -668,7 +668,9 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
                                                 mModalDialogManagerSupplier.get(),
                                                 tabModelSelector,
                                                 () -> {
-                                                    mTabSwitcherSupplier.get().refreshTabList();
+                                                    if (mTabSwitcherSupplier.get() != null) {
+                                                        mTabSwitcherSupplier.get().refreshTabList();
+                                                    }
                                                 });
                     });
         }
