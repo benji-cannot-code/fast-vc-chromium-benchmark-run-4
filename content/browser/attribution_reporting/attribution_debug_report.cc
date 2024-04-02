1d5b62c8c4278be3fbca107c1b4b6da4682d577f
Nan Lin <linnan@chromium.org>
2024-04-02 16:03:32 +0000

    Report source reporting origins per site limit explicitly
    https://github.com/WICG/attribution-reporting-api/pull/1225
    
    Bug: 332050434
    Change-Id: Iedcdb9eb61431d1558779e9c621282072027379e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5408782
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281218}

--- content/browser/attribution_reporting/attribution_debug_report.cc
+++ content/browser/attribution_reporting/attribution_debug_report.cc
@@ -81,7 +81,8 @@ enum class DebugDataType {
   kTriggerEventReportWindowNotStarted = 26,
   kTriggerEventNoMatchingTriggerData = 27,
   kHeaderParsingError = 28,
-  kMaxValue = kHeaderParsingError,
+  kSourceReportingOriginPerSiteLimit = 29,
+  kMaxValue = kSourceReportingOriginPerSiteLimit,
 };
 
 struct DebugDataTypeAndBody {
@@ -102,16 +103,15 @@ std::optional<DebugDataTypeAndBody> GetReportDataBody(
               StoreSourceResult::ExceedsMaxTriggerStateCardinality>) {
             return std::optional<DebugDataTypeAndBody>();
           },
-          [](absl::variant<
-              StoreSourceResult::Success,
-              // `kSourceSuccess` is sent for a few errors as well to mitigate
-              // the security concerns on reporting these errors. Because these
-              // errors are thrown based on information across reporting
-              // origins, reporting on them would violate the same-origin
-              // policy.
-              StoreSourceResult::ExcessiveReportingOrigins,
-              StoreSourceResult::DestinationGlobalLimitReached,
-              StoreSourceResult::ReportingOriginsPerSiteLimitReached>) {
+          [](absl::variant<StoreSourceResult::Success,
+                           // `kSourceSuccess` is sent for a few errors as well
+                           // to mitigate the security concerns on reporting
+                           // these errors. Because these errors are thrown
+                           // based on information across reporting origins,
+                           // reporting on them would violate the same-origin
+                           // policy.
+                           StoreSourceResult::ExcessiveReportingOrigins,
+                           StoreSourceResult::DestinationGlobalLimitReached>) {
             return std::make_optional<DebugDataTypeAndBody>(
                 DebugDataType::kSourceSuccess);
           },
@@ -136,6 +136,10 @@ std::optional<DebugDataTypeAndBody> GetReportDataBody(
           [](StoreSourceResult::InternalError) {
             return std::make_optional<DebugDataTypeAndBody>(
                 DebugDataType::kSourceUnknownError);
+          },
+          [](StoreSourceResult::ReportingOriginsPerSiteLimitReached v) {
+            return std::make_optional<DebugDataTypeAndBody>(
+                DebugDataType::kSourceReportingOriginPerSiteLimit, v.limit);
           },
       },
       result.result());
@@ -271,6 +275,8 @@ std::string_view SerializeReportDataType(DebugDataType data_type) {
       return "os-trigger-delegated";
     case DebugDataType::kHeaderParsingError:
       return "header-parsing-error";
+    case DebugDataType::kSourceReportingOriginPerSiteLimit:
+      return "source-reporting-origin-per-site-limit";
   }
 }
 
@@ -360,6 +366,7 @@ base::Value::Dict GetReportDataBody(DebugDataType data_type,
     case DebugDataType::kOsSourceDelegated:
     case DebugDataType::kOsTriggerDelegated:
     case DebugDataType::kHeaderParsingError:
+    case DebugDataType::kSourceReportingOriginPerSiteLimit:
       NOTREACHED_NORETURN();
   }
 
@@ -374,7 +381,8 @@ base::Value::Dict GetReportData(DebugDataType type, base::Value::Dict body) {
 }
 
 void RecordVerboseDebugReportType(DebugDataType type) {
-  static_assert(DebugDataType::kMaxValue == DebugDataType::kHeaderParsingError,
+  static_assert(DebugDataType::kMaxValue ==
+                    DebugDataType::kSourceReportingOriginPerSiteLimit,
                 "Update ConversionVerboseDebugReportType enum.");
   base::UmaHistogramEnumeration("Conversions.SentVerboseDebugReportType4",
                                 type);
