2ec117938b22a3ca3223ea230cc9ee77c7bc1030
Christian Flach <cmfcmf@chromium.org>
2024-04-02 09:22:30 +0000

    iwa: Enforce Update Channel IDs to be valid UTF-8
    Bug: 328587741
    Change-Id: I626fd9e1957f26fa057888df0b0ce437df40fbef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402827
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Christian Flach <cmfcmf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281071}

--- chrome/browser/web_applications/isolated_web_apps/update_manifest/update_manifest.cc
+++ chrome/browser/web_applications/isolated_web_apps/update_manifest/update_manifest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/containers/flat_map.h"
 #include "base/ranges/algorithm.h"
+#include "base/strings/string_util.h"
 #include "base/types/expected.h"
 #include "base/types/expected_macros.h"
 #include "base/types/optional_ref.h"
@@ -35,7 +36,7 @@ const UpdateChannelId& UpdateChannelId::default_id() {
 // static
 base::expected<UpdateChannelId, absl::monostate> UpdateChannelId::Create(
     std::string input) {
-  if (input.empty()) {
+  if (input.empty() || !base::IsStringUTF8(input)) {
     return base::unexpected(absl::monostate());
   }
   return UpdateChannelId(std::move(input));
