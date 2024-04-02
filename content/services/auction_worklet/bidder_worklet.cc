fd261f13590f251be42d6eabe16c081d94f41487
Qingxin Wu <qingxinwu@google.com>
2024-04-02 16:12:22 +0000

    Add UMA for reportWin and reportResult time.
    Change-Id: I07245d17f8245c2fb90a49414b826ac360500aee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5405142
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Qingxin Wu <qingxinwu@google.com>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281222}

--- content/services/auction_worklet/bidder_worklet.cc
+++ content/services/auction_worklet/bidder_worklet.cc
@@ -1037,6 +1037,8 @@ void BidderWorklet::V8State::ReportWin(
               args, total_timeout.get(), errors_out)
           .IsEmpty();
   TRACE_EVENT_NESTABLE_ASYNC_END0("fledge", "report_win", trace_id);
+  base::TimeDelta elapsed = elapsed_timer.Elapsed();
+  base::UmaHistogramTimes("Ads.InterestGroup.Auction.ReportWinTime", elapsed);
 
   if (script_failed) {
     // Keep Private Aggregation API requests since `reportWin()` might use it to
@@ -1046,7 +1048,7 @@ void BidderWorklet::V8State::ReportWin(
         /*ad_beacon_map=*/{}, /*ad_macro_map=*/{},
         context_recycler.private_aggregation_bindings()
             ->TakePrivateAggregationRequests(),
-        elapsed_timer.Elapsed(), std::move(errors_out));
+        elapsed, std::move(errors_out));
     return;
   }
 
@@ -1058,7 +1060,7 @@ void BidderWorklet::V8State::ReportWin(
       context_recycler.register_ad_macro_bindings()->TakeAdMacroMap(),
       context_recycler.private_aggregation_bindings()
           ->TakePrivateAggregationRequests(),
-      elapsed_timer.Elapsed(), std::move(errors_out));
+      elapsed, std::move(errors_out));
 }
 
 void BidderWorklet::V8State::GenerateBid(
