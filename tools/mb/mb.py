4700239d00e6e23e46555ee0fdc6b5065e96c8f6
Ben Pastene <bpastene@chromium.org>
2024-04-02 17:45:12 +0000

    Revert "mb: Allow custom build dirs for the isolate-everything run mode"
    This reverts commit 75004f6b767c69f166011a3aec6ca0d3b6ec69f8.
    
    Reason for revert: to fix tree closure:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Deterministic%20Linux/46720/infra
    
    Original change's description:
    > mb: Allow custom build dirs for the isolate-everything run mode
    >
    > "GetConfig()" mostly just wraps "Lookup()", but is smart enough to
    > gracefully re-use a pre-existing build dir. This can be leveraged by
    > the UTR in instances where the UTR hasn't used mb.py to gen.
    > Practically that's:
    > - when the user skips "compile" mode
    > - when the user overrides with some custom GN args
    >
    > So this allows "mb.py isolate-everything" to be used in both of those
    > cases by preventing it from over-writing the local build dir.
    >
    > Bug: 41492686
    > Change-Id: I150672865d5cc333b946603cac61716281f643a5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5411040
    > Reviewed-by: Struan Shrimpton <sshrimp@google.com>
    > Commit-Queue: Ben Pastene <bpastene@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1281261}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-try: true
    Bug: 41492686
    Change-Id: Ib841f66c730f13d7667f9a798eb79f350879a4f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5414142
    Reviewed-by: Struan Shrimpton <sshrimp@google.com>
    Auto-Submit: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: Struan Shrimpton <sshrimp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1281285}

--- tools/mb/mb.py
+++ tools/mb/mb.py
@@ -491,7 +491,7 @@ class MetaBuildWrapper:
     return 0
 
   def CmdIsolateEverything(self):
-    vals = self.GetConfig()
+    vals = self.Lookup()
     return self.RunGNGenAllIsolates(vals)
 
   def CmdHelp(self):
