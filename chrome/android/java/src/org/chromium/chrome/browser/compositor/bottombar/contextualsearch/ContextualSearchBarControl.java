cc2cb920409ac09d7c574521a334b6661a470d63
Charles Hager <clhager@google.com>
2024-04-02 20:16:15 +0000

    Pad contextual search caption in edge-to-edge to avoid showing page preview early and shifting focus
    https://screencast.googleplex.com/cast/NTQ4OTc1OTA2ODYxODc1Mnw0YjdhOTEwNi1mNw
    
    Bug: b/331945732
    Change-Id: I3e4fb7a442c9fb3f4e62ccf125048a0a5cb84a2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5411394
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1281406}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/contextualsearch/ContextualSearchBarControl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/contextualsearch/ContextualSearchBarControl.java
@@ -140,23 +140,35 @@ public class ContextualSearchBarControl {
     /**
      * Constructs a new bottom bar control container by inflating views from XML.
      *
-     * @param panel     The panel.
+     * @param panel The panel.
      * @param container The parent view for the bottom bar views.
-     * @param loader    The resource loader that will handle the snapshot capturing.
+     * @param loader The resource loader that will handle the snapshot capturing.
+     * @param edgeToEdgeBottomPaddingDp Extra bottom padding in dp used when the current page is in
+     *     edge-to-edge mode in order to keep the search bar contents above the bottom nav bar area.
+     *     0 if edge-to-edge is not enabled, or if the current page is not edge-to-edge.
      */
     public ContextualSearchBarControl(
             ContextualSearchPanel panel,
             Context context,
             ViewGroup container,
-            DynamicResourceLoader loader) {
+            DynamicResourceLoader loader,
+            int edgeToEdgeBottomPaddingDp) {
         mContextualSearchPanel = panel;
         mCanPromoteToNewTab = panel.canPromoteToNewTab();
         mImageControl = new ContextualSearchImageControl(panel);
         mContextControl = new ContextualSearchContextControl(panel, context, container, loader);
         mSearchTermControl = new ContextualSearchTermControl(panel, context, container, loader);
+
+        mDpToPx = context.getResources().getDisplayMetrics().density;
         mCaptionControl =
                 new ContextualSearchCaptionControl(
-                        panel, context, container, loader, mCanPromoteToNewTab);
+                        panel,
+                        context,
+                        container,
+                        loader,
+                        mCanPromoteToNewTab,
+                        edgeToEdgeBottomPaddingDp * mDpToPx);
+
         mQuickActionControl = new ContextualSearchQuickActionControl(context, loader);
         mCardIconControl = new ContextualSearchCardIconControl(context, loader);
 
@@ -172,7 +184,6 @@ public class ContextualSearchBarControl {
         mEndButtonWidth =
                 mPaddedIconWidthPx
                         + context.getResources().getDimension(R.dimen.overlay_panel_button_padding);
-        mDpToPx = context.getResources().getDisplayMetrics().density;
     }
 
     /**
@@ -660,5 +671,14 @@ public class ContextualSearchBarControl {
     public void setInBarAnimationTestNotifier(Runnable runnable) {
         assert mInBarAnimationTestNotifier == null;
         mInBarAnimationTestNotifier = runnable;
+    }
+
+    /**
+     * Override the extra bottom padding used when the current page is drawing edge-to-edge.
+     *
+     * @param edgeToEdgeBottomPaddingDp The extra bottom padding in dp.
+     */
+    public void overrideEdgeToEdgePadding(int edgeToEdgeBottomPaddingDp) {
+        mCaptionControl.overrideEdgeToEdgePadding(edgeToEdgeBottomPaddingDp * mDpToPx);
     }
 }
