9699eb57a634cbe0bf77a956d905ccc09062abbb
ThuHuong <thv@chromium.org>
2024-04-02 23:40:15 +0000

    Check campaigns is loaded before reading the campaign.
    This cl checks if the campaigns loading is completed before fetching it. It also prevents subscribing to instant registry more than once.
    
    Bug: b/331051412, b/331045249
    Change-Id: Ib3f10d6c70137a8c9145a9c2fe0d065bbffcc986
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5406314
    Reviewed-by: Li Lin <llin@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Thu Huong Vu <thv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281546}

--- chrome/browser/ash/growth/campaigns_manager_session.h
+++ chrome/browser/ash/growth/campaigns_manager_session.h
@@ -44,7 +44,7 @@ class CampaignsManagerSession : public session_manager::SessionManagerObserver,
   Profile* GetProfile();
   bool IsEligible();
   void SetupWindowObserver();
-  void MaybeTriggerProactiveCampaigns();
+  void OnLoadCampaignsCompleted();
 
   base::ScopedObservation<session_manager::SessionManager,
                           session_manager::SessionManagerObserver>
