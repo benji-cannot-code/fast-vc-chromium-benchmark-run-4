86c2076ba80b7e8e7aa4431324ddd7031cc83c44
Anupam Snigdha <snianu@microsoft.com>
2024-04-02 16:09:26 +0000

    [Clipboard] Fix WPT test failures.
    Add missing source file to the clipboard tests that has the
    definition of `tryGrantReadPermission` and `tryGrantWritePermission`.
    Also, remove the unused parameter in `tryGrantReadPermission`.
    
    Bug: 331836811
    
    Change-Id: Ia3e204a733b176b03c2478dc8445fc1fe22e6cd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5407270
    Reviewed-by: Sanket Joshi <sajos@microsoft.com>
    Reviewed-by: Christine Hollingsworth <christinesm@chromium.org>
    Commit-Queue: Anupam Snigdha <snianu@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1281220}

--- third_party/blink/web_tests/external/wpt/clipboard-apis/resources/user-activation.js
+++ third_party/blink/web_tests/external/wpt/clipboard-apis/resources/user-activation.js
@@ -35,7 +35,7 @@ async function trySetPermission(perm, state) {
   }
 }
 
-async function tryGrantReadPermission(state) {
+async function tryGrantReadPermission() {
   await trySetPermission("clipboard-read", "granted");
 }
 
