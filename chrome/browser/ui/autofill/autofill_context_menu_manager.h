19be623de4f61c1479a8235999910a6553aa1a1d
Dmitry Vykochko <vykochko@google.com>
2024-04-02 16:56:14 +0000

    Add reopening the autofill popup after adding an address.
    This makes the behaviour consistent with address removing.
    
    Bug: 325440757
    Change-Id: I95fccb9197e0ccdc8ba363504fa04c457a5ef8a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5401719
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Cr-Commit-Position: refs/heads/main@{#1281253}

--- chrome/browser/ui/autofill/autofill_context_menu_manager.h
+++ chrome/browser/ui/autofill/autofill_context_menu_manager.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_AUTOFILL_AUTOFILL_CONTEXT_MENU_MANAGER_H_
 
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/types/strong_alias.h"
 #include "components/autofill/content/browser/content_autofill_driver.h"
 #include "components/autofill/core/common/unique_ids.h"
@@ -120,6 +121,8 @@ class AutofillContextMenuManager : public RenderViewContextMenuObserver {
   const raw_ptr<ui::SimpleMenuModel> menu_model_;
   const raw_ptr<RenderViewContextMenuBase> delegate_;
   content::ContextMenuParams params_;
+
+  base::WeakPtrFactory<AutofillContextMenuManager> weak_ptr_factory_{this};
 };
 
 }  // namespace autofill
