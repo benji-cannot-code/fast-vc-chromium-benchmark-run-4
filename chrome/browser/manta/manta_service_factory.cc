435d31542f768bf0b2a787c145a679e80efc4934
Xinglong Luan <alanlxl@google.com>
2024-04-02 05:06:10 +0000

    [manta] Add demo mode support to manta service
    1. Make BaseProvider creat EndpointFetcher that uses API key
    2. Makes OrcaProvider support demo mode
    
    Bug: b:329350049
    Test: autoninja -C out_${SDK_BOARD}/Release chrome
    Change-Id: Ie1e71dea01a829bda8fdfa3fcc81b3eaf2ce8486
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5401635
    Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    Commit-Queue: Xinglong Luan <alanlxl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281015}

--- chrome/browser/manta/manta_service_factory.cc
+++ chrome/browser/manta/manta_service_factory.cc
@@ -7,6 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "build/chromeos_buildflags.h"
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "chrome/browser/ash/login/demo_mode/demo_session.h"
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_selections.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
@@ -39,11 +43,16 @@ MantaServiceFactory::~MantaServiceFactory() = default;
 std::unique_ptr<KeyedService>
 MantaServiceFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* const context) const {
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  bool is_demo_mode = ash::DemoSession::IsDeviceInDemoMode();
+#else
+  bool is_demo_mode = false;
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
   Profile* const profile = Profile::FromBrowserContext(context);
   return std::make_unique<MantaService>(
       profile->GetDefaultStoragePartition()
           ->GetURLLoaderFactoryForBrowserProcess(),
-      IdentityManagerFactory::GetForProfile(profile));
+      IdentityManagerFactory::GetForProfile(profile), is_demo_mode);
 }
 
 }  // namespace manta
