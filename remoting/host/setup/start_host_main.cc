27c6fa165bd4bdf1cb76c63c51abf763e9359f04
Joe Downing <joedow@google.com>
2024-04-02 22:03:11 +0000

    Fix some help and error message handling in start_host.
    This CL cleans up some error message handling by removing some
    redundant log statements and adding newline/whitespaces where
    needed to make the tool output more readable.
    
    An additional change is that the error message we get from the
    service can contain invalid UTF-8 chars so we use a conversion
    function to clean it up, otherwise the protobuf library will
    complain when we try to report it to our backend.
    
    Change-Id: I15e1e5167c9359a60fdf34e1ff08e71ae0ad76c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5415494
    Auto-Submit: Joe Downing <joedow@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281485}

--- remoting/host/setup/start_host_main.cc
+++ remoting/host/setup/start_host_main.cc
@@ -71,9 +71,10 @@ void PrintDefaultHelpMessage(const char* process_name) {
           "instructions on running this tool and help generating the command "
           "line arguments.\n"
           "\n"
-          "Usage: %s [--name=<host display name>] [--pin=<6+ digit PIN>] "
+          "Example usage:\n%s --code=<authCode> --redirect-url=<redirectURL> "
+          "[--name=<host display name>] [--pin=<6+ digit PIN>] "
           "[--host-owner=<host owner email>]"
-          "--redirect-url=<redirectURL> --code=<authCode>\n",
+          " \n",
           process_name);
 }
 
@@ -81,7 +82,7 @@ void PrintCorpUserHelpMessage(const char* process_name) {
   fprintf(stdout,
           "Too many arguments provided.\nSetting up a machine for a corp user "
           "requires the email address of that user and an optional display "
-          "name.\nExample usage: %s --corp-user=<user_email_address> "
+          "name.\nExample usage:\n%s --corp-user=<user_email_address> "
           "[--display-name=corp-machine-name]\n",
           process_name);
 }
@@ -90,7 +91,7 @@ void PrintCloudUserHelpMessage(const char* process_name) {
   fprintf(stdout,
           "Too many arguments provided.\nSetting up a machine for a cloud user "
           "requires the email address of that user and an optional display "
-          "name.\nExample usage: %s --cloud-user=<user_email_address> "
+          "name.\nExample usage:\n%s --cloud-user=<user_email_address> "
           "[--display-name=cloud-instance-name]\n",
           process_name);
 }
