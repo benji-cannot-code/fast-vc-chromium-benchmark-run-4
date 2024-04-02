293f705534e5d4e813e9b414f9423ea68b180b23
Jeffrey Young <cowmoo@google.com>
2024-04-02 18:36:44 +0000

    p13n: reset loading state on SeaPen select failure
    When selecting a SeaPen image fails, the loading state gets
    stuck. It is waiting for wallpaper_observer.ts to trigger
    an update from the wallpaper side, but that never occurs.
    Manually reset the loading state on select recent image
    and select thumbnail failure.
    
    Bug: b:332398708
    Test: browser_tests --gtest_filter=*SeaPenController*
    Change-Id: I68669eb851ee674ffc87af8f3465fc686d6a5207
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5411326
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Commit-Queue: Jeffrey Young <cowmoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1281328}

--- chrome/test/data/webui/chromeos/personalization_app/test_sea_pen_interface_provider.ts
+++ chrome/test/data/webui/chromeos/personalization_app/test_sea_pen_interface_provider.ts
@@ -72,6 +72,10 @@ export class TestSeaPenProvider extends TestBrowserProxy implements
       ReturnType<SeaPenProviderInterface['selectSeaPenThumbnail']> =
           Promise.resolve({success: true});
 
+  selectSeaPenRecentImageResponse:
+      ReturnType<SeaPenProviderInterface['selectRecentSeaPenImage']> =
+          Promise.resolve({success: true});
+
   shouldShowSeaPenTermsOfServiceDialogResponse = true;
 
   constructor() {
@@ -106,7 +110,7 @@ export class TestSeaPenProvider extends TestBrowserProxy implements
     assertTrue(
         isSeaPenImageId(id), `id must be SeaPenImageId but received: ${id}`);
     this.methodCalled('selectRecentSeaPenImage', id);
-    return Promise.resolve({success: true});
+    return this.selectSeaPenRecentImageResponse;
   }
 
   getRecentSeaPenImages() {
