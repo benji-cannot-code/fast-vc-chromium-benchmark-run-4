d2b158612da1906b77959331f091aaedc6c8633f
Daniel White <danieltwhite@google.com>
2024-04-02 19:08:12 +0000

    [iOS] Created Enhanced Safe Browsing Promo Flag
    In this CL, a feature flag was created that toggles the visibility of
    inline and infobar promotions that are being run to promote the Enhanced
    Safe Browsing feature.
    
    Bug: 332537935
    Change-Id: I240319d1c6178517adcaec387f5f4eaff8ae7ad1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5412738
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281364}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -104,6 +104,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/web/model/features.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/components/security_interstitials/https_only_mode/feature.h"
+#import "ios/components/security_interstitials/safe_browsing/features.h"
 #import "ios/public/provider/chrome/browser/app_utils/app_utils_api.h"
 #import "ios/web/common/features.h"
 #import "ios/web/common/user_agent.h"
@@ -1621,6 +1622,11 @@ const flags_ui::FeatureEntry kFeatureEntries[] = {
      flags_ui::kOsIos,
      FEATURE_VALUE_TYPE(page_content_annotations::features::
                             kPageVisibilityPageContentAnnotations)},
+    {"enhanced-safe-browsing-promo",
+     flag_descriptions::kEnhancedSafeBrowsingPromoName,
+     flag_descriptions::kEnhancedSafeBrowsingPromoDescription, flags_ui::kOsIos,
+     FEATURE_VALUE_TYPE(
+         security_interstitials::features::kEnhancedSafeBrowsingPromo)},
 };
 
 bool SkipConditionalFeatureEntry(const flags_ui::FeatureEntry& entry) {
