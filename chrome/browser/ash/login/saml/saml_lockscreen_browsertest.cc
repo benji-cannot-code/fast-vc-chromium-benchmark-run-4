81fe40c225a27e0aa9aa1b54dafe2532c8939cd1
Andrey Davydov <andreydav@google.com>
2024-04-02 11:30:38 +0000

    Get domain from the email during lock screen online reauth
    Simplify the way we determine user's domain during lock screen online
    reauth. Previously we used identity manager, now we just get it from
    user's email which is already available in the code. This should help
    with SamlUnlockTest.* tests which are currently flaky due to a dcheck in
    UpdateAccountInfoForAccount.
    
    Bug: 331470143
    Change-Id: Ie7b4cd6f58b8ff922ed9e4111cc3714d92dd1b74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5401845
    Reviewed-by: Mohammed Abdon <mohammedabdon@chromium.org>
    Commit-Queue: Andrey Davydov <andreydav@google.com>
    Cr-Commit-Position: refs/heads/main@{#1281100}

--- chrome/browser/ash/login/saml/saml_lockscreen_browsertest.cc
+++ chrome/browser/ash/login/saml/saml_lockscreen_browsertest.cc
@@ -30,9 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/policy/affiliation/affiliation_test_helper.h"
 #include "chrome/browser/ash/policy/core/device_policy_cros_browser_test.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
-#include "chrome/browser/enterprise/util/managed_browser_utils.h"
 #include "chrome/browser/profiles/profile_manager.h"
-#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/test/base/mixin_based_in_process_browser_test.h"
 #include "chromeos/ash/components/browser_context_helper/browser_context_helper.h"
 #include "chromeos/ash/components/dbus/session_manager/fake_session_manager_client.h"
@@ -47,8 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/network_session_configurator/common/network_switches.h"
 #include "components/policy/core/common/policy_pref_names.h"
 #include "components/proxy_config/proxy_config_dictionary.h"
-#include "components/signin/public/identity_manager/account_info.h"
-#include "components/signin/public/identity_manager/identity_test_utils.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/storage_partition.h"
@@ -178,21 +174,6 @@ class LockscreenWebUiTest : public MixinBasedInProcessBrowserTest {
     // sign in on the login screen, which we skip in these tests.
     static_cast<ash::ChromeUserManagerImpl*>(user_manager::UserManager::Get())
         ->SetUsingSamlForTesting(GetAccountId(), true);
-
-    // Populate hosted domain for the user - lock screen reauth code might need
-    // it as a parameter for /samlredirect URL.
-    Profile* profile = ProfileManager::GetPrimaryUserProfile();
-    auto* identity_manager = IdentityManagerFactory::GetForProfile(profile);
-    const CoreAccountId primary_account_id =
-        identity_manager->GetPrimaryAccountId(signin::ConsentLevel::kSignin);
-    AccountInfo account_info =
-        identity_manager->FindExtendedAccountInfoByGaiaId(
-            FakeGaiaMixin::kEnterpriseUser1GaiaId);
-    const std::string email_domain =
-        chrome::enterprise_util::GetDomainFromEmail(
-            FakeGaiaMixin::kEnterpriseUser1);
-    account_info.hosted_domain = email_domain;
-    signin::UpdateAccountInfoForAccount(identity_manager, account_info);
   }
 
   AccountId GetAccountId() { return logged_in_user_mixin_.GetAccountId(); }
