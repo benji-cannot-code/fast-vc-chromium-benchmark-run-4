8ef9093bcbdd0f17762f44572d5c131c2e32adf8
Elias Klim <elklm@chromium.org>
2024-04-02 18:45:10 +0000

    [LHS indicator] Remove Browser from LHS indicators.
    LocationBarView sometimes can exist without Browser and WebContent
    instances. All access points to such instances should be verified
    before accessed.
    
    Bug: 41487636,331380168
    Change-Id: I3215d1647f2a7a1e7dfb5b3f55616dc57ebd62ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5401720
    Commit-Queue: Elias Klim <elklm@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281339}

--- chrome/browser/ui/views/permissions/chip/permission_dashboard_controller.h
+++ chrome/browser/ui/views/permissions/chip/permission_dashboard_controller.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/global_routing_id.h"
 #include "ui/views/view_tracker.h"
 
-class Browser;
 class LocationBarView;
 class ChipController;
 class ContentSettingImageModel;
@@ -25,7 +24,6 @@ class ContentSettingImageModel;
 class PermissionDashboardController : public PermissionChipView::Observer {
  public:
   PermissionDashboardController(
-      Browser* browser,
       LocationBarView* location_bar_view,
       PermissionDashboardView* permission_dashboard_view);
 
@@ -75,7 +73,7 @@ class PermissionDashboardController : public PermissionChipView::Observer {
   void OnIndicatorsChipButtonPressed();
   std::u16string GetIndicatorTitle(ContentSettingImageModel* model);
 
-  raw_ptr<Browser> browser_ = nullptr;
+  // `LocationBarView` owns this.
   raw_ptr<LocationBarView> location_bar_view_ = nullptr;
   raw_ptr<PermissionDashboardView> permission_dashboard_view_ = nullptr;
   // Currently only Camera and Mic are supported.
