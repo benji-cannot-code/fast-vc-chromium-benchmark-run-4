57c47500a0b707bac72848cf2dde4ffe29c21fb4
Gabriel Charette <gab@chromium.org>
2024-04-02 21:16:37 +0000

    [base] Always emit a TerminatingFlow from ~WaitableEvent()
    Discovered this bug while reading the documentation of
    perfetto::Flow::FromPointer which mentions protecting against
    reuse of "this" as a flow id in the future which could connect
    unrelated objects.
    
    Change-Id: I50d237d701aa4ddc1d9f9bf3817cc09c90e06898
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5405713
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281446}

--- base/synchronization/waitable_event_win.cc
+++ base/synchronization/waitable_event_win.cc
@@ -49,8 +49,6 @@ WaitableEvent::WaitableEvent(win::ScopedHandle handle)
   CHECK(handle_.is_valid()) << "Tried to create WaitableEvent from NULL handle";
 }
 
-WaitableEvent::~WaitableEvent() = default;
-
 void WaitableEvent::Reset() {
   ResetEvent(handle_.get());
 }
