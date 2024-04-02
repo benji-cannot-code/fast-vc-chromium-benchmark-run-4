a518802e7f95d32a5797e1923ef8c884137a294c
Jacob Francis <francisjp@google.com>
2024-04-02 20:46:01 +0000

    [AX Table Fuzzer] CHECK for negative rows and columns.
    - This CL adds CHECKs to make sure that rows and column attributes are
      positive.
    
    - This fix is predicated on the assumption that negative row and column values are impossible.
    
    Bug: 331243267
    Change-Id: I868b3947a1e7d2d80b346407ded69b28b4376afd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5414334
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Jacob Francis <francisjp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1281422}

--- ui/accessibility/ax_table_info.cc
+++ ui/accessibility/ax_table_info.cc
@@ -8,12 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <iostream>
 #include <string>
 
+#include "base/check.h"
 #include "base/logging.h"
 #include "base/memory/raw_ptr.h"
 #include "base/observer_list.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
-
 #include "ui/accessibility/ax_constants.mojom.h"
 #include "ui/accessibility/ax_enum_util.h"
 #include "ui/accessibility/ax_enums.mojom-shared.h"
@@ -152,11 +152,13 @@ void FindCellsForRowlessTable(
       } else if (IsCellOrTableHeader(child->GetRole())) {
         const int rowIndex =
             child->GetIntAttribute(ax::mojom::IntAttribute::kTableCellRowIndex);
+        CHECK_GE(rowIndex,0);
         if (current_row < rowIndex) {
           cell_nodes_per_row->emplace_back();
           current_row = rowIndex;
           current_index++;
         }
+        CHECK_GE(current_index,0);
         auto& cell_nodes = cell_nodes_per_row->at(current_index);
         cell_nodes.push_back(child);
       }
