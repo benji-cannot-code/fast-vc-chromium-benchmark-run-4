f1c7c5dfd93c1cc1ba25f47796583ede39c1541b
Prameet Shah <phshah@google.com>
2024-04-02 23:47:15 +0000

    gd: Enable Game Dashboard by default.
    In ChromeGameDashboardDelegate, if there's no valid ArcAppListPrefs,
    assume the window is not a game.
    
    In `ChromeDashboardController`'s dtor, close the welcome dialog,
    and make sure to not toggle the toolbar.
    
    Bug: b:331475373
    Test: Ran ash unit test
    Test: Verified on DUT without enabling the flags.
    Change-Id: I8b81ac90dee54e7c01424c34ee8ecba1be28c358
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5408471
    Commit-Queue: Prameet Shah <phshah@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Auto-Submit: Prameet Shah <phshah@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281557}

--- ash/accelerators/accelerator_controller_unittest.cc
+++ ash/accelerators/accelerator_controller_unittest.cc
@@ -3404,7 +3404,6 @@ class AcceleratorControllerGameDashboardTests
   ~AcceleratorControllerGameDashboardTests() override = default;
 
   void SetUp() override {
-    EXPECT_FALSE(features::IsGameDashboardEnabled());
     scoped_feature_list_.InitAndEnableFeature(features::kGameDashboard);
     AcceleratorControllerTest::SetUp();
     EXPECT_TRUE(features::IsGameDashboardEnabled());
