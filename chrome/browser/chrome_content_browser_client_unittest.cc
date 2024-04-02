08dabacb167e7971c01ff7c5cb7915c16d9f22fd
Christian Flach <cmfcmf@chromium.org>
2024-04-02 08:51:10 +0000

    iwa: Fix unit tests that would fail once we enable the IWA feature flag
    These tests assumed that the feature flag is default-off. I tested this
    by default-enabling the feature flag and then fixing most of the tests
    that failed, then set default-disabled the feature flag again.
    
    Bug: 319837357
    Change-Id: Id5ad59be8bcfdb1eea9f3671d4062a545c16de2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5205171
    Auto-Submit: Christian Flach <cmfcmf@chromium.org>
    Commit-Queue: Christian Flach <cmfcmf@chromium.org>
    Reviewed-by: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1281061}

--- chrome/browser/chrome_content_browser_client_unittest.cc
+++ chrome/browser/chrome_content_browser_client_unittest.cc
@@ -1157,6 +1157,9 @@ TEST_F(ChromeContentBrowserClientStoragePartitionTest,
 
 TEST_F(ChromeContentBrowserClientStoragePartitionTest,
        DefaultPartitionIsUsedWhenIsolationDisabled) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndDisableFeature(features::kIsolatedWebApps);
+
   TestChromeContentBrowserClient test_content_browser_client;
   content::StoragePartitionConfig config =
       test_content_browser_client.GetStoragePartitionConfigForSite(
