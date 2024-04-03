c228a2d1596a8240b3c6a4eb59d905c5cb657c36
Ivana Žužić <izuzic@google.com>
2024-04-03 12:45:14 +0000

    [UPMLocalPwd] Reset the settings migration pref if needed
    The settings migration pref is reset for the users who don't use UPM
    with store split. The reset is necessary for the users who got this
    pref wrongly set to true because of b/332506422.
    In the future, if the feature flag gets disabled, this code will also
    be triggered and used to reset the pref.
    
    Bug: b/332515889
    Change-Id: Ife7b30adfc38354a764891a14be2f5467eca3e00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5412554
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281780}

--- chrome/browser/password_manager/android/password_manager_android_util.h
+++ chrome/browser/password_manager/android/password_manager_android_util.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_PASSWORD_MANAGER_ANDROID_PASSWORD_MANAGER_ANDROID_UTIL_H_
 #define CHROME_BROWSER_PASSWORD_MANAGER_ANDROID_PASSWORD_MANAGER_ANDROID_UTIL_H_
 
+#include "components/password_manager/core/common/password_manager_pref_names.h"
+
+using password_manager::prefs::UseUpmLocalAndSeparateStoresState;
+
 class PrefService;
 
 namespace base {
@@ -13,6 +17,11 @@ class FilePath;
 }  // namespace base
 
 namespace password_manager_android_util {
+
+// Used to prevent static casting issues with
+// `PasswordsUseUPMLocalAndSeparateStores` pref.
+UseUpmLocalAndSeparateStoresState GetSplitStoresAndLocalUpmPrefValue(
+    PrefService* pref_service);
 
 // Used to decide whether using UPM as backend is possible. The check is based
 // on whether the GMSCore is installed and the internal wiring is present, and
