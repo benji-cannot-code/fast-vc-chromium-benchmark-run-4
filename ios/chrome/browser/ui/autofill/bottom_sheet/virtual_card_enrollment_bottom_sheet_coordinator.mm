b7ce21ac700a7a19212844d238494e96f0b5eb6a
Slobodan Pejic <slobodan@chromium.org>
2024-04-03 21:07:11 +0000

    Implement links on the virtual card enrollment bottom sheet.
    Before tapping links on the virtual card enrollment bottom sheet did
    nothing. Now, clicking a link opens a view with a title, done button,
    and the webstate showing the contents of the link.
    
    Overview of options/alternatives: https://docs.google.com/document/d/1XsT_PzGDz9PI-ImSpIj1S4xyztTIem6AVUY8HAXicYg/edit?usp=sharing&resourcekey=0-nBm5YJJCYnt6FAaAhk04AQ
    Screenshot: https://screenshot.googleplex.com/A6SVsLjgnocVTP4.png
    Screen recording: https://drive.google.com/file/d/1RSFFwwcxh3DtMYMs7bIPgojMyzvj6San/view?usp=sharing&resourcekey=0-U3gBUwYv8YTWWbgjfWOgbg
    
    Bug: 40282545, b/304549454
    Change-Id: I9aa270c9d18fdcdb746c2c66e9dcafbdf108deba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5414053
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282115}

--- ios/chrome/browser/ui/autofill/bottom_sheet/virtual_card_enrollment_bottom_sheet_coordinator.mm
+++ ios/chrome/browser/ui/autofill/bottom_sheet/virtual_card_enrollment_bottom_sheet_coordinator.mm
@@ -6,12 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/autofill/bottom_sheet/virtual_card_enrollment_bottom_sheet_coordinator.h"
 #import "components/autofill/core/browser/metrics/payments/virtual_card_enrollment_metrics.h"
 #import "components/autofill/core/browser/payments/virtual_card_enroll_metrics_logger.h"
+#import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
+#import "ios/chrome/browser/ui/autofill/bottom_sheet/bottom_sheet_link_coordinator.h"
+#import "ios/chrome/browser/ui/autofill/bottom_sheet/bottom_sheet_link_coordinator_delegate.h"
 #import "ios/chrome/browser/ui/autofill/bottom_sheet/virtual_card_enrollment_bottom_sheet_delegate.h"
 #import "ios/chrome/browser/ui/autofill/bottom_sheet/virtual_card_enrollment_bottom_sheet_mediator.h"
 #import "ios/chrome/browser/ui/autofill/bottom_sheet/virtual_card_enrollment_bottom_sheet_view_controller.h"
 
 @interface VirtualCardEnrollmentBottomSheetCoordinator () <
-    VirtualCardEnrollmentBottomSheetDelegate>
+    VirtualCardEnrollmentBottomSheetDelegate,
+    BottomSheetLinkCoordinatorDelegate>
 
 @property(nonatomic, strong) VirtualCardEnrollmentBottomSheetMediator* mediator;
 @property(nonatomic, strong)
@@ -28,6 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @implementation VirtualCardEnrollmentBottomSheetCoordinator {
   autofill::VirtualCardEnrollUiModel model_;
   std::optional<autofill::VirtualCardEnrollmentCallbacks> callbacks_;
+  Browser* browser_;
+  ChromeBrowserState* browser_state_;
+  BottomSheetLinkCoordinator* bottom_sheet_link_coordinator_;
 }
 
 @synthesize mediator;
@@ -44,6 +51,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     self->callbacks_ =
         AutofillBottomSheetTabHelper::FromWebState(activeWebState)
             ->GetVirtualCardEnrollmentCallbacks();
+    self->browser_ = browser;
+    self->browser_state_ = self.browser->GetBrowserState();
   }
   return self;
 }
@@ -100,12 +109,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self stop];
 }
 
-- (void)didTapLinkURL:(CrURL*)url {
-  // TODO(crbug.com/1485376): Implement opening links from the virtual
-  // card enrollment bottom sheet.
+- (void)didTapLinkURL:(CrURL*)url text:(NSString*)text {
+  bottom_sheet_link_coordinator_ = [[BottomSheetLinkCoordinator alloc]
+      initWithBaseViewController:self.viewController
+                         browser:self.browser
+                             url:url
+                           title:text];
+  bottom_sheet_link_coordinator_.delegate = self;
+  [bottom_sheet_link_coordinator_ start];
 }
 
 - (void)viewDidDisappear:(BOOL)animated {
+}
+
+#pragma mark - BottomSheetLinkCoordinatorDelegate
+
+- (void)dismissBottomSheetLinkCoordinator {
+  [bottom_sheet_link_coordinator_ stop];
 }
 
 #pragma mark - Private
--- ios/chrome/browser/ui/autofill/bottom_sheet/virtual_card_enrollment_bottom_sheet_delegate.h
+++ ios/chrome/browser/ui/autofill/bottom_sheet/virtual_card_enrollment_bottom_sheet_delegate.h
@@ -17,8 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)didCancel;
 
 // Called when the user tapped on a link in the legal message or the learn more
-// link.
-- (void)didTapLinkURL:(CrURL*)url;
+// link. The text of the link is included.
+- (void)didTapLinkURL:(CrURL*)url text:(NSString*)text;
 
 // Called when the view disappeared.
 - (void)viewDidDisappear:(BOOL)animated;
