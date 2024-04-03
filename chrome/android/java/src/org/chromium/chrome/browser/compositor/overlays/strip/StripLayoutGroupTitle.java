fd190316fa7f3675628712d0b493e45a0e25d046
Neil Coronado <nemco@google.com>
2024-04-03 20:04:11 +0000

    [TabGroupIndicators] Add group indicator a11y description
    Add group indicator a11y description.
    
    Screenshots: https://drive.google.com/drive/folders/1yLSjppdPsH3ok11aZW13SaOOSiG_yEoY?usp=sharing
    
    Bug: 326494015
    Change-Id: I3993b938c1d677a66f58dcc18201941da1f9e17c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5417353
    Commit-Queue: Neil Coronado <nemco@google.com>
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1282065}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutGroupTitle.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutGroupTitle.java
@@ -67,6 +67,8 @@ public class StripLayoutGroupTitle extends StripLayoutView {
     private String mTitle;
     @ColorInt private int mColor;
 
+    private String mAccessibilityDescription = "";
+
     // Bottom indicator variables
     private float mBottomIndicatorWidth;
 
@@ -142,8 +144,11 @@ public class StripLayoutGroupTitle extends StripLayoutView {
 
     @Override
     public String getAccessibilityDescription() {
-        // TODO(crbug.com/326494015): Update when official descriptions are finalized.
-        return "Tab group";
+        return mAccessibilityDescription;
+    }
+
+    protected void setAccessibilityDescription(String accessibilityDescription) {
+        mAccessibilityDescription = accessibilityDescription;
     }
 
     @Override
