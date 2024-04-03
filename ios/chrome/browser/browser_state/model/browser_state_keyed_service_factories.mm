43a7816cb6eaf5de59c5b89a794ef29cf9e6e97f
Marke Hallowell <wry@chromium.org>
2024-04-03 16:16:19 +0000

    Add factory for Content Notification Service.
    Bug: b/326254114
    Change-Id: Ie9e941773d93b26fa00483b2641acf3d7adbc8f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402718
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Marke Hallowell <wry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281891}

--- ios/chrome/browser/browser_state/model/browser_state_keyed_service_factories.mm
+++ ios/chrome/browser/browser_state/model/browser_state_keyed_service_factories.mm
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/browsing_data/model/browsing_data_remover_factory.h"
 #import "ios/chrome/browser/commerce/model/shopping_service_factory.h"
 #import "ios/chrome/browser/consent_auditor/model/consent_auditor_factory.h"
+#import "ios/chrome/browser/content_notification/model/content_notification_service_factory.h"
 #import "ios/chrome/browser/content_settings/model/cookie_settings_factory.h"
 #import "ios/chrome/browser/contextual_panel/model/contextual_panel_model_service_factory.h"
 #import "ios/chrome/browser/contextual_panel/model/sample/sample_panel_model_factory.h"
@@ -167,6 +168,7 @@ void EnsureBrowserStateKeyedServiceFactoriesBuilt() {
   ChromeAccountManagerServiceFactory::GetInstance();
   ChromePasswordProtectionServiceFactory::GetInstance();
   ConsentAuditorFactory::GetInstance();
+  ContentNotificationServiceFactory::GetInstance();
   ContextualPanelModelServiceFactory::GetInstance();
   DeviceSharingManagerFactory::GetInstance();
   DiscoverFeedServiceFactory::GetInstance();
