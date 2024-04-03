b998279f99797cc651cff93b682ea2cacce893de
Andreu Botella <abotella@igalia.com>
2024-04-03 16:16:26 +0000

    [line-clamp] Fix crash when explicitly setting `line-clamp: none`
    https://crrev.com/c/5317993 added the `line-clamp` property, whose
    valid values are positive integers and the keyword `none`. The
    resolved type of this property was `int`, where the keyword `none`
    would map to a value of 0.
    
    However, this property definition did not specify an explicit
    converter. This meant that when `line-clamp: none` was explicitly set,
    the conversion to an `int` would trigger an assertion failure, since
    it expects a `CSSPrimitiveValue`, not a `CSSIdentifierValue`.
    
    This patch fixes this by adding the converter
    `StyleBuilderConverter::ConvertIntegerOrNone`, which allows the `none`
    keyword in addition to integers, and which takes a template argument
    for the integer value to use for `none`.
    
    Bug: 40336192
    Change-Id: Ide218b2fec75f21908280f475e85b391b7551f7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5419122
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281892}

--- third_party/blink/renderer/core/css/resolver/style_builder_converter.h
+++ third_party/blink/renderer/core/css/resolver/style_builder_converter.h
@@ -220,6 +220,8 @@ class StyleBuilderConverter {
   static Length ConvertLineHeight(StyleResolverState&, const CSSValue&);
   static float ConvertNumberOrPercentage(StyleResolverState&, const CSSValue&);
   static int ConvertInteger(StyleResolverState&, const CSSValue&);
+  template <int NoneValue = 0>
+  static int ConvertIntegerOrNone(StyleResolverState&, const CSSValue&);
   static ScrollStartData ConvertScrollStart(const StyleResolverState&,
                                             const CSSValue&);
   static float ConvertAlpha(StyleResolverState&,
@@ -495,6 +497,16 @@ AtomicString StyleBuilderConverter::ConvertString(StyleResolverState&,
   }
   DCHECK_EQ(To<CSSIdentifierValue>(value).GetValueID(), IdForNone);
   return g_null_atom;
+}
+
+template <int NoneValue>
+int StyleBuilderConverter::ConvertIntegerOrNone(StyleResolverState& state,
+                                                const CSSValue& value) {
+  if (IsA<CSSPrimitiveValue>(value)) {
+    return ConvertInteger(state, value);
+  }
+  DCHECK_EQ(To<CSSIdentifierValue>(value).GetValueID(), CSSValueID::kNone);
+  return NoneValue;
 }
 
 // Returns the computed <color> value for `value`. Note that it's expected that
