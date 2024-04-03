cbdcc398f444b7dcc2cfb373e0f37d039385c562
Andy Paicu <andypaicu@chromium.org>
2024-04-03 21:19:50 +0000

    [PEPC] Only consider the PEPC to be valid between 'small' and 'xxxlarge'
    Since font-size is used for "em"-type lengths and they have already been
    calculated, changing the font-size here won't have the desired effect.
    
    Instead, we will mark the element as invalid if the font-size is too
    small or too large.
    
    Bug: 1462930
    Change-Id: I34c5b2aa79caaa7c0ce0573acdb8e8d3127282e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5383436
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282125}

--- third_party/blink/renderer/core/html/html_permission_element.cc
+++ third_party/blink/renderer/core/html/html_permission_element.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/permissions/permission.mojom-blink.h"
 #include "third_party/blink/public/strings/grit/blink_strings.h"
 #include "third_party/blink/renderer/core/css/css_selector.h"
+#include "third_party/blink/renderer/core/css/font_size_functions.h"
 #include "third_party/blink/renderer/core/css/properties/css_property_instances.h"
 #include "third_party/blink/renderer/core/css/properties/longhand.h"
 #include "third_party/blink/renderer/core/css/properties/longhands.h"
@@ -421,19 +422,14 @@ void HTMLPermissionElement::AdjustStyle(ComputedStyleBuilder& builder) {
   // that it's not too big.
 
   // TODO(crbug.com/1462930): Set text direction (ltr\rtl) based on language.
-
-  // TODO(crbug.com/1462930): Ensure font-size at least as large as the
-  // equivalent of 'small'.
 }
 
 void HTMLPermissionElement::DidRecalcStyle(const StyleRecalcChange change) {
-  if (!GetComputedStyle() || AreColorsNonOpaque(GetComputedStyle()) ||
-      ContrastBetweenColorAndBackgroundColor(GetComputedStyle()) <
-          kMinimumAllowedContrast) {
-    DisableClickingIndefinitely(DisableReason::kInvalidStyle);
-  } else {
+  if (IsStyleValid()) {
     EnableClickingAfterDelay(DisableReason::kInvalidStyle,
                              kDefaultDisableTimeout);
+  } else {
+    DisableClickingIndefinitely(DisableReason::kInvalidStyle);
   }
 }
 
@@ -643,6 +639,37 @@ void HTMLPermissionElement::OnIntersectionChanged(
     EnableClickingAfterDelay(DisableReason::kIntersectionChanged,
                              kDefaultDisableTimeout);
   }
+}
+
+bool HTMLPermissionElement::IsStyleValid() {
+  // No computed style when using `display: none`.
+  if (!GetComputedStyle()) {
+    return false;
+  }
+
+  if (AreColorsNonOpaque(GetComputedStyle())) {
+    return false;
+  }
+  if (ContrastBetweenColorAndBackgroundColor(GetComputedStyle()) <
+      kMinimumAllowedContrast) {
+    return false;
+  }
+
+  if (GetComputedStyle()->ComputedFontSize() <
+      FontSizeFunctions::FontSizeForKeyword(
+          &GetDocument(), FontSizeFunctions::KeywordSize(CSSValueID::kSmall),
+          GetComputedStyle()->GetFontDescription().IsMonospace())) {
+    return false;
+  }
+
+  if (GetComputedStyle()->ComputedFontSize() >
+      FontSizeFunctions::FontSizeForKeyword(
+          &GetDocument(), FontSizeFunctions::KeywordSize(CSSValueID::kXxxLarge),
+          GetComputedStyle()->GetFontDescription().IsMonospace())) {
+    return false;
+  }
+
+  return true;
 }
 
 }  // namespace blink
