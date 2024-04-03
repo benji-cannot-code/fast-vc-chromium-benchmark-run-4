70425dd49bf867c6f618a6d742167c907f19580a
Jonathan Lee <jonathanjlee@google.com>
2024-04-03 01:17:28 +0000

    Roll wpt tooling
    This rolls wpt to latest commit at
    https://github.com/web-platform-tests/wpt.
    REMOTE-WPT-HEAD: 7b53a7e5c36c83728d6a7dc6ca0c39093f5f3cb3
    
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: Ife2fbdc0e48b3de38fb46e5b820fb23a7c7e1ed0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5415439
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1281591}

--- third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/browsers/servodriver.py
+++ third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/browsers/servodriver.py
@@ -162,6 +162,7 @@ class ServoWebDriverBrowser(Browser):
         if self.output_handler is not None:
             self.output_handler.after_process_stop()
 
+    @property
     def pid(self):
         if self.proc is None:
             return None
