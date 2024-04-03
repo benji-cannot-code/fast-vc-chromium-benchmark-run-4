7c2ee4abd81615c023e0572e3faaf3507f11fdbc
Jackson Tadie <jacksontadie@google.com>
2024-04-03 23:26:42 +0000

    Define helper for Flex-enrollment-related tests
    The same constants/methods were being defined in each test class (since
    they need to set the same conditions to get FakeOobeConfigurationClient
    to return a flex-token-containing OOBE config), so this class
    consolidates them for reuse.
    
    Test: <out-dir>/unit_tests --gtest_filter=*AutoEnrollment*
    Test: <out-dir>/unit_tests --gtest_filter=*FlexEnrollment*
    Bug: b/331285209
    Change-Id: I6f5397b0995571a8a26ae825593cb7819fff12e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5399537
    Reviewed-by: Yves Arrouye <drcrash@chromium.org>
    Commit-Queue: Jackson Tadie <jacksontadie@google.com>
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1282213}

--- chrome/browser/ash/policy/enrollment/auto_enrollment_type_checker_unittest.cc
+++ chrome/browser/ash/policy/enrollment/auto_enrollment_type_checker_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "build/branding_buildflags.h"
 #include "chrome/browser/ash/login/oobe_configuration.h"
+#include "chrome/browser/ash/policy/enrollment/flex_enrollment_test_helper.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chromeos/ash/components/dbus/oobe_config/fake_oobe_configuration_client.h"
@@ -41,9 +42,6 @@ constexpr char kSerialNumberValue[] = "a_value";
 constexpr char kBrandCodeValue[] = "brand_code";
 constexpr char kActivateDateValue[] = "activated";
 constexpr char kMalformedEmbargoDateValue[] = "adventure_time";
-constexpr char kFlexTokenOobeConfig[] = R"({
-  "flexToken": "test_flex_token"
-})";
 
 std::string ToUTCString(const base::Time& time) {
   return base::UnlocalizedTimeFormatWithPattern(time, "yyyy-MM-dd",
@@ -59,10 +57,6 @@ class AutoEnrollmentTypeCheckerTest : public testing::Test {
   AutoEnrollmentTypeCheckerTest() = default;
   ~AutoEnrollmentTypeCheckerTest() override = default;
 
-  void SetUp() override { ash::OobeConfigurationClient::InitializeFake(); }
-
-  void TearDown() override { ash::OobeConfigurationClient::Shutdown(); }
-
  protected:
   void SetUpNonchromeDevice() {
     fake_statistics_provider_.SetMachineStatistic(
@@ -72,8 +66,7 @@ class AutoEnrollmentTypeCheckerTest : public testing::Test {
 
   void SetUpFlexDevice() {
     SetUpNonchromeDevice();
-    command_line_.GetProcessCommandLine()->AppendSwitch(
-        ash::switches::kRevenBranding);
+    flex_test_helper_.SetUpFlexDevice();
   }
 
   void SetUpFlexDeviceWithCommandLineSwitchToAlways() {
@@ -81,13 +74,6 @@ class AutoEnrollmentTypeCheckerTest : public testing::Test {
     command_line_.GetProcessCommandLine()->AppendSwitchASCII(
         ash::switches::kEnterpriseEnableForcedReEnrollmentOnFlex,
         AutoEnrollmentTypeChecker::kForcedReEnrollmentAlways);
-  }
-
-  void SetUpFlexToken(const char config[] = kFlexTokenOobeConfig) {
-    static_cast<ash::FakeOobeConfigurationClient*>(
-        ash::OobeConfigurationClient::Get())
-        ->SetConfiguration(config);
-    ash::OobeConfiguration::Get()->CheckConfiguration();
   }
 
   void SetupFREEnabled() {
@@ -169,8 +155,8 @@ class AutoEnrollmentTypeCheckerTest : public testing::Test {
   static constexpr bool is_google_branded_ = false;
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 
-  ash::OobeConfiguration oobe_configuration_;
   base::test::ScopedCommandLine command_line_;
+  test::FlexEnrollmentTestHelper flex_test_helper_{&command_line_};
   ash::system::ScopedFakeStatisticsProvider fake_statistics_provider_;
 };
 
@@ -462,7 +448,7 @@ TEST_F(AutoEnrollmentTypeCheckerTest,
 TEST_F(AutoEnrollmentTypeCheckerTest,
        DetermineAutoEnrollmentCheckTypeOnFlexWhenTokenPresent) {
   SetUpFlexDevice();
-  SetUpFlexToken();
+  flex_test_helper_.SetUpFlexEnrollmentTokenConfig();
   fake_statistics_provider_.SetMachineStatistic(
       ash::system::kSerialNumberKeyForTest, kSerialNumberValue);
   fake_statistics_provider_.SetMachineStatistic(ash::system::kRlzBrandCodeKey,
@@ -487,7 +473,7 @@ TEST_F(AutoEnrollmentTypeCheckerTest,
 // retrieval request).
 TEST_F(AutoEnrollmentTypeCheckerTest,
        DetermineAutoEnrollmentCheckTypeNotOnFlexWhenTokenPresent) {
-  SetUpFlexToken();
+  flex_test_helper_.SetUpFlexEnrollmentTokenConfig();
   fake_statistics_provider_.SetMachineStatistic(
       ash::system::kSerialNumberKeyForTest, kSerialNumberValue);
   fake_statistics_provider_.SetMachineStatistic(ash::system::kRlzBrandCodeKey,
@@ -529,7 +515,7 @@ TEST_F(AutoEnrollmentTypeCheckerTest,
   constexpr char kEmptyFlexTokenOobeConfig[] = R"({
     "flexToken": ""
   })";
-  SetUpFlexToken(kEmptyFlexTokenOobeConfig);
+  flex_test_helper_.SetUpFlexEnrollmentTokenConfig(kEmptyFlexTokenOobeConfig);
   SetUpFlexDevice();
   fake_statistics_provider_.SetMachineStatistic(
       ash::system::kSerialNumberKeyForTest, kSerialNumberValue);
