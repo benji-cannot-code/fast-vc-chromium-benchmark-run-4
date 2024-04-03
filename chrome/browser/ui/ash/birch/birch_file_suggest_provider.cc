56354448f49d6ce832885a44196c855af23addb3
Matthew Mourgos <mmourgos@chromium.org>
2024-04-03 06:54:44 +0000

    Birch: Avoid setting file suggest items when no results available
    Also, return an empty list of file suggest data in the case where the
    drive service is disabled or when the mount fails. This way file data is
    still propagated and marked fresh and no further waiting on the birch
    file provider will occur.
    
    Bug: b/330385108
    Change-Id: Ia5f2fc2e7d3c09f22c7947bfda5366e286e903d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5404109
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281687}

--- chrome/browser/ui/ash/birch/birch_file_suggest_provider.cc
+++ chrome/browser/ui/ash/birch/birch_file_suggest_provider.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/ash/birch/birch_file_suggest_provider.h"
 
+#include <optional>
 #include <vector>
 
 #include "ash/birch/birch_item.h"
@@ -47,6 +48,13 @@ void BirchFileSuggestProvider::OnSuggestedFileDataUpdated(
   if (!Shell::HasInstance()) {
     return;
   }
+
+  if (!suggest_results.has_value()) {
+    // No suggestion results exist yet, so avoid setting file suggest items and
+    // return early.
+    return;
+  }
+
   if (!suggest_results) {
     Shell::Get()->birch_model()->SetFileSuggestItems({});
     return;
