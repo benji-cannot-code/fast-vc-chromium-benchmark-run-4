75be60df81b4a863b6bc8a8bbbeec350ad414889
Elad Alon <eladalon@chromium.org>
2024-04-03 21:50:53 +0000

    [CSC] Flip CapturedSurfaceControlKillswitch to default-on
    With the Chrome UX for Captured Surface Control complete,
    it is now possible to make the permissions scoped to the
    origin rather than to the capture-session.
    
    Bug: 1466247, 324468211
    Change-Id: I1ee17463e0012b5d09972a42b7a84083462bceae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5411131
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282159}

--- chrome/test/data/webui/settings/site_details_test.ts
+++ chrome/test/data/webui/settings/site_details_test.ts
@@ -170,6 +170,9 @@ suite('SiteDetails', function() {
           createContentSettingTypeToValuePair(
               ContentSettingsTypes.AUTOMATIC_FULLSCREEN,
               [createRawSiteException('https://foo.com:443')]),
+          createContentSettingTypeToValuePair(
+              ContentSettingsTypes.CAPTURED_SURFACE_CONTROL,
+              [createRawSiteException('https://foo.com:443')]),
         ],
         [
           createContentSettingTypeToValuePair(
