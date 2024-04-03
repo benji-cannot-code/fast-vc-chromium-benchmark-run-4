e2f57739e63b743b5e49bcf3c54b35b36315874a
Munira Tursunova <moonira@google.com>
2024-04-03 13:49:35 +0000

    Revert "[UPMLocalPwd] Reset the settings migration pref if needed"
    This reverts commit c6ee1c716d5864b1d86447c4073efd6d13d5c14e.
    
    Reason for revert:
    Broke the lacros-arm-archive-rel build https://ci.chromium.org/ui/p/chromium/builders/ci/lacros-arm-archive-rel/91859/overview
    
    Original change's description:
    > [UPMLocalPwd] Reset the settings migration pref if needed
    >
    > The settings migration pref is reset for the users who don't use UPM
    > with store split. The reset is necessary for the users who got this
    > pref wrongly set to true because of b/332506422.
    > In the future, if the feature flag gets disabled, this code will also
    > be triggered and used to reset the pref.
    >
    > Bug: b/332515889
    > Change-Id: Ife7b30adfc38354a764891a14be2f5467eca3e00
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5412554
    > Commit-Queue: Ivana Žužić <izuzic@google.com>
    > Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1281780}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: b/332515889
    Change-Id: I0c5e4372cfb4632b9f79035cc9dbfb86975c1c2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5420288
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Ivana Žužić <izuzic@google.com>
    Commit-Queue: Munira Tursunova <moonira@google.com>
    Owners-Override: Munira Tursunova <moonira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1281785}

--- chrome/browser/password_manager/android/password_manager_android_util.cc
+++ chrome/browser/password_manager/android/password_manager_android_util.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_manager_buildflags.h"
 #include "components/password_manager/core/browser/password_manager_constants.h"
 #include "components/password_manager/core/browser/password_store/split_stores_and_local_upm.h"
+#include "components/password_manager/core/common/password_manager_pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "components/sync/base/pref_names.h"
 #include "components/version_info/android/channel_getter.h"
@@ -31,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using password_manager::prefs::kCurrentMigrationVersionToGoogleMobileServices;
 using password_manager::prefs::kPasswordsUseUPMLocalAndSeparateStores;
+using password_manager::prefs::UseUpmLocalAndSeparateStoresState;
 using password_manager::prefs::UseUpmLocalAndSeparateStoresState::kOff;
 using password_manager::prefs::UseUpmLocalAndSeparateStoresState::
     kOffAndMigrationPending;
@@ -59,6 +61,19 @@ enum class ActivationError {
   kMigrationWarningUnacknowledged = 6,
   kMaxValue = kMigrationWarningUnacknowledged,
 };
+
+UseUpmLocalAndSeparateStoresState GetSplitStoresAndLocalUpmPrefValue(
+    PrefService* pref_service) {
+  auto value = static_cast<UseUpmLocalAndSeparateStoresState>(
+      pref_service->GetInteger(kPasswordsUseUPMLocalAndSeparateStores));
+  switch (value) {
+    case kOff:
+    case kOffAndMigrationPending:
+    case kOn:
+      return value;
+  }
+  NOTREACHED_NORETURN();
+}
 
 bool IsPasswordSyncEnabled(PrefService* pref_service) {
   // It's not possible to ask the SyncService whether password sync is enabled,
@@ -367,19 +382,6 @@ void MaybeDeactivateSplitStoresAndLocalUpm(
 }
 
 }  // namespace
-
-UseUpmLocalAndSeparateStoresState GetSplitStoresAndLocalUpmPrefValue(
-    PrefService* pref_service) {
-  auto value = static_cast<UseUpmLocalAndSeparateStoresState>(
-      pref_service->GetInteger(kPasswordsUseUPMLocalAndSeparateStores));
-  switch (value) {
-    case kOff:
-    case kOffAndMigrationPending:
-    case kOn:
-      return value;
-  }
-  NOTREACHED_NORETURN();
-}
 
 bool AreMinUpmRequirementsMet() {
   if (!IsInternalBackendPresent()) {
