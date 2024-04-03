48e9cbdb9275c91e07f547e356244e9723283b8d
Struan Shrimpton <sshrimp@google.com>
2024-04-03 22:46:05 +0000

    utr: Use rich for more logging to support multiple platforms
    This applies a RichHandler to the root logger and creates a new logger
    for most recipe output to prevent parsing of output we don't control.
    
    Also makes the "Running: step name" text to be cyan to help visualize
    the flow of the recipe
    
    Bug: 41492688
    Change-Id: I075e89e89790db4fd31ec20ec13d4e120733104b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5421151
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: Struan Shrimpton <sshrimp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1282183}

--- tools/utr/run.py
+++ tools/utr/run.py
@@ -19,6 +19,8 @@ import builders
 import cipd
 import recipe
 
+from rich.logging import RichHandler
+
 _THIS_DIR = pathlib.Path(__file__).resolve().parent
 _SRC_DIR = _THIS_DIR.parents[1]
 
@@ -112,7 +114,13 @@ def parse_args(args=None):
 def main():
   args = parse_args()
   logging.basicConfig(level=logging.DEBUG if args.verbosity else logging.INFO,
-                      format='%(message)s')
+                      format='%(message)s',
+                      handlers=[
+                          RichHandler(show_time=False,
+                                      show_level=False,
+                                      show_path=False,
+                                      markup=True)
+                      ])
 
   cipd_bin_path = _SRC_DIR.joinpath('third_party', 'depot_tools', '.cipd_bin')
   if not cipd_bin_path.exists():
