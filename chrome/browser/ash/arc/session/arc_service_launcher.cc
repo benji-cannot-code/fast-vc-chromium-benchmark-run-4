dfb581a0b8ad87bf66e025b40753950357d2e1db
Hung Nguyen <hungmn@google.com>
2024-04-03 23:31:28 +0000

    Update swap config in ARC enable/disable request
    ARC can be disabled/enabled via other besides play store setting such as
    Chrome test API, or chrome flags to enable ARC via fake logins.
    
    BUG=b:331546394
    TEST=inspect critical memory threshold in a few tast tests and confirm
    the 15% threshold is used for non ARC, and 8% is used for ARC: arc.Boot
    8%, login.Chrome 15%, arc.DisableArc 15%, arc.EnableArc 8%
    
    Change-Id: I2e33e680543b74c52b687936f69bcd0e15b86cb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5408662
    Commit-Queue: Hung Nguyen <hungmn@google.com>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282220}

--- chrome/browser/ash/arc/session/arc_service_launcher.cc
+++ chrome/browser/ash/arc/session/arc_service_launcher.cc
@@ -161,8 +161,6 @@ ArcServiceLauncher::ArcServiceLauncher(
       base::FeatureList::IsEnabled(kEnableArcVmDataMigration)) {
     arc_disk_space_monitor_ = std::make_unique<ArcDiskSpaceMonitor>();
   }
-
-  session_manager_obs_.Observe(arc_session_manager_.get());
 }
 
 ArcServiceLauncher::~ArcServiceLauncher() {
@@ -380,7 +378,6 @@ void ArcServiceLauncher::ResetForTesting() {
   // First destroy the internal states, then re-initialize them.
   // These are for workaround of singletonness DCHECK in their ctors/dtors.
   Shutdown();
-  session_manager_obs_.Reset();
   arc_session_manager_.reset();
 
   // No recreation of arc_service_manager. Pointers to its ArcBridgeService
@@ -389,11 +386,6 @@ void ArcServiceLauncher::ResetForTesting() {
   arc_session_manager_ = CreateArcSessionManager(
       arc_service_manager_->arc_bridge_service(), chrome::GetChannel(),
       scheduler_configuration_manager_);
-  session_manager_obs_.Observe(arc_session_manager_.get());
-}
-
-void ArcServiceLauncher::OnArcPlayStoreEnabledChanged(bool enabled) {
-  ash::ConfigureSwap(/*arc_enabled=*/enabled);
 }
 
 #if BUILDFLAG(USE_ARC_PROTECTED_MEDIA)
