1532d123ddec010cc5525bd1ad567a30f6875763
Roman Arora <romanarora@chromium.org>
2024-04-03 18:03:43 +0000

    [visited-url-ranking] Add component and data type
    Creates the core component, types, and interfaces to be used for the Tab
    Resume 2.0 effort. The new `URLVisit` type and associated component will
    be used for a variety of cross-platform use cases that require fetching
    and merging of URL related data provided by a variety of different
    sources (e.g. history, sessions, shopping, segmentation metrics).
    
    A few sample cases include:
    
    - Resumption of navigated visits that may or may not be tied to
    currently open tabs based on local or foreign session data.
    
    - Cleanup of currently open tabs based.
    
    See design doc: go/tab-resume-fetch-and-rank-services
    
    Low-Coverage-Reason: Tests in crrev.com/c/5386443
    
    Bug: 330740468
    Change-Id: I8c3297972d838f33d020fe4298ed9e7665fd1e2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5386026
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281977}

---
+++ components/visited_url_ranking/README.md
@@ -0,0 +1,5 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Visited URL Ranking Component
+
+Component that facilitates fetching and ranking of aggregated user URL visit
+data managed by different sources (e.g. Tab Model, Session, History).
---
+++ components/visited_url_ranking/public/url_visit.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/visited_url_ranking/public/url_visit.h"
+
+namespace visited_url_ranking {
+
+URLVisit::URLVisit() = default;
+
+URLVisit::~URLVisit() = default;
+
+URLVisit::URLVisit(URLVisit&& other) = default;
+
+URLVisit& URLVisit::operator=(URLVisit&& other) = default;
+
+}  // namespace visited_url_ranking
