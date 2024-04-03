29e130beece32f8a33dd505e87f4eb961dab98c9
Florian Leimgruber <fleimgruber@google.com>
2024-04-03 13:51:42 +0000

    Make PlusAddressSpecifics::profile_id a string
    As discussed offline, it is preferable to make this a string. This CL
    changes the type of the field. There are no older clients using this
    proto yet, so it's safe to change it.
    
    More changes are necessary in the client-side data model, but these are
    deferred to follow-ups (with TODOs), to unblock the server-side work.
    
    Bug: b/322147254
    Change-Id: I72706d09f0b24ce7c1d0bf7f4eb597b424f47ead
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5418935
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1281786}

--- components/plus_addresses/plus_address_types.h
+++ components/plus_addresses/plus_address_types.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace plus_addresses {
 
 struct PlusProfile {
+  // TODO(b/322147254): Make this a string.
   int64_t profile_id;
   std::string facet;
   std::string plus_address;
