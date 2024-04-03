e09fb701b566ab360fa96c0be904f88c579abb84
weidongliu <weidongliu@chromium.org>
2024-04-03 15:02:21 +0000

    use GetPreferredSize(SizeBounds) instead of GetPreferredSize() in //ui
    Bug: 40232718
    Change-Id: I2729b0e81d5fa336dc848fe83354ea5750bd82e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5420508
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281836}

--- ui/message_center/views/notification_view.cc
+++ ui/message_center/views/notification_view.cc
@@ -267,7 +267,7 @@ NotificationView::NotificationView(
   views::InkDrop::Get(this)->SetCreateRippleCallback(base::BindRepeating(
       [](NotificationViewBase* host) -> std::unique_ptr<views::InkDropRipple> {
         return std::make_unique<views::FloodFillInkDropRipple>(
-            views::InkDrop::Get(host), host->GetPreferredSize(),
+            views::InkDrop::Get(host), host->GetPreferredSize({}),
             views::InkDrop::Get(host)->GetInkDropCenterBasedOnLastEvent(),
             views::InkDrop::Get(host)->GetBaseColor(),
             views::InkDrop::Get(host)->GetVisibleOpacity());
@@ -282,7 +282,7 @@ NotificationView::NotificationView(
   const int font_list_height = font_list.GetHeight();
   const gfx::Insets& text_view_padding(CalculateTopPadding(font_list_height));
   header_row->ConfigureLabelsStyle(font_list, text_view_padding, false);
-  header_row->SetPreferredSize(header_row->GetPreferredSize() -
+  header_row->SetPreferredSize(header_row->GetPreferredSize({}) -
                                gfx::Size(GetInsets().width(), 0));
   header_row->SetCallback(base::BindRepeating(
       &NotificationView::HeaderRowPressed, base::Unretained(this)));
@@ -662,7 +662,7 @@ void NotificationView::Layout(PassKey) {
 }
 
 void NotificationView::PreferredSizeChanged() {
-  highlight_path_generator_->set_preferred_size(GetPreferredSize());
+  highlight_path_generator_->set_preferred_size(GetPreferredSize({}));
   MessageView::PreferredSizeChanged();
 }
 
