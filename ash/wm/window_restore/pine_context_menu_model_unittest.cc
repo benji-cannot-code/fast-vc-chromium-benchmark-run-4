cff99ab29b46608f299fe385d591f8c016d61af6
Sammie Quon <sammiequon@chromium.org>
2024-04-03 18:21:35 +0000

    pine: Remove uses of MaybeStartPine...DevAccelerator in tests
    Test: none
    Bug: none
    Change-Id: I0b28192d749dae59151329134f3808cfab1d5a9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5420429
    Reviewed-by: Min Chen <minch@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281990}

--- ash/wm/window_restore/pine_context_menu_model_unittest.cc
+++ ash/wm/window_restore/pine_context_menu_model_unittest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/window_restore/pine_test_base.h"
 #include "ash/wm/window_restore/window_restore_util.h"
 #include "base/test/scoped_feature_list.h"
+#include "components/app_constants/constants.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/testing_pref_service.h"
 #include "components/user_manager/user_type.h"
@@ -68,9 +69,10 @@ TEST_F(PineContextMenuModelTest, LayoutAndCommands) {
 // Tests that pressing the settings button in Pine properly displays the inline
 // context menu.
 TEST_F(PineContextMenuModelTest, ShowContextMenuOnSettingsButtonClicked) {
-  Shell::Get()
-      ->pine_controller()
-      ->MaybeStartPineOverviewSessionDevAccelerator();
+  auto contents_data = std::make_unique<PineContentsData>();
+  contents_data->apps_infos.emplace_back(app_constants::kChromeAppId, "Title");
+  Shell::Get()->pine_controller()->MaybeStartPineOverviewSession(
+      std::move(contents_data));
   WaitForOverviewEntered();
 
   // Get the active Pine widget.
--- ash/wm/window_restore/pine_test_base.cc
+++ ash/wm/window_restore/pine_test_base.cc
@@ -28,7 +28,11 @@ PineTestBase::~PineTestBase() {
   switches::SetIgnoreForestSecretKeyForTest(false);
 }
 
-// AshTestBase:
+PrefService* PineTestBase::GetTestPrefService() {
+  return GetSessionControllerClient()->GetUserPrefService(
+      AccountId::FromUserEmail(kTestUserEmail));
+}
+
 void PineTestBase::SetUp() {
   AshTestBase::SetUp();
 
@@ -58,11 +62,6 @@ void PineTestBase::SetUp() {
   session_controller->SwitchActiveUser(
       AccountId::FromUserEmail(kTestUserEmail));
   session_controller->SetSessionState(session_manager::SessionState::ACTIVE);
-}
-
-PrefService* PineTestBase::GetTestPrefService() {
-  return GetSessionControllerClient()->GetUserPrefService(
-      AccountId::FromUserEmail(kTestUserEmail));
 }
 
 }  // namespace ash
