507a60bc45650a23a9bb55dc83516e42c8fbef93
Ivana Žužić <izuzic@google.com>
2024-04-03 17:19:47 +0000

    Reland "[UPMLocalPwd] Reset the settings migration pref if needed"
    This is a reland of commit c6ee1c716d5864b1d86447c4073efd6d13d5c14e
    
    The problem with the non-Android bot including the Android-specific util
    is resolved by a buildflag guard.
    
    Original change's description:
    > [UPMLocalPwd] Reset the settings migration pref if needed
    >
    > The settings migration pref is reset for the users who don't use UPM
    > with store split. The reset is necessary for the users who got this
    > pref wrongly set to true because of b/332506422.
    > In the future, if the feature flag gets disabled, this code will also
    > be triggered and used to reset the pref.
    >
    > Bug: b/332515889
    > Change-Id: Ife7b30adfc38354a764891a14be2f5467eca3e00
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5412554
    > Commit-Queue: Ivana Žužić <izuzic@google.com>
    > Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1281780}
    
    Bug: b/332515889
    Change-Id: I8f96183011c89afeaf1f9ee35c47db4453198d50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5419298
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1281935}

--- chrome/browser/password_manager/password_store_backend_factory.cc
+++ chrome/browser/password_manager/password_store_backend_factory.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/bind_post_task.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
-#include "chrome/browser/password_manager/android/password_manager_android_util.h"
 #include "chrome/browser/password_manager/android/password_store_proxy_backend.h"
 #include "components/password_manager/core/browser/password_manager_buildflags.h"
 #include "components/password_manager/core/browser/password_store/login_database.h"
@@ -32,6 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/features/password_features.h"
 #include "components/password_manager/core/common/password_manager_pref_names.h"
 #endif  // !BUILDFLAG(USE_LOGIN_DATABASE_AS_BACKEND)
+
+#if BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/password_manager/android/password_manager_android_util.h"
+#endif
 
 namespace {
 
