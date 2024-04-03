97d68ab5252a8e745c73c4f62b8540707bba4ce8
Fergal Daly <fergal@chromium.org>
2024-04-03 06:43:49 +0000

    Revert "Update force-installed off-store extension disable logic"
    This reverts commit c6ab69a33ff8e6d2eaf31036ad1523e6d9d0f327.
    
    Reason for revert: ProfileKeyedServiceBrowserTest.GuestProfileParent_NeededServices started failing after this CL.
    
    Original change's description:
    > Update force-installed off-store extension disable logic
    >
    > Restricting the force-installation of off-store extensions
    > reduces the risk of abuse of the force-install policy to
    > bypass the webstore-hosted restriction in Windows and Mac
    > devices.
    >
    > https://crrev.com/c/5053386 restricted force-installed
    > off-store extensions to only be enabled if the device is
    > managed by a non-local authority (e.g., domain-joined).
    >
    > This CL relaxes that restriction to also allow such
    > extensions to be enabled when the browser is managed by
    > a non-local authority. This change allows the
    > force-installation policy to be configured at the
    > user-level also.
    >
    > Bug: 331061788
    > Change-Id: I04a7b14b9988b7b7f26c91adc2a197ff81a2b591
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5398699
    > Reviewed-by: David Bertoni <dbertoni@chromium.org>
    > Reviewed-by: Anunoy Ghosh <anunoy@chromium.org>
    > Commit-Queue: Richard Chen <richche@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1281630}
    
    Bug: 331061788,332639958
    Change-Id: I76341339089b076b125a31bc2775c2e24d74c7e8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5418497
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Fergal Daly <fergal@google.com>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281684}

--- chrome/browser/extensions/standard_management_policy_provider.h
+++ chrome/browser/extensions/standard_management_policy_provider.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/memory/raw_ptr.h"
-#include "chrome/browser/profiles/profile.h"
 #include "extensions/browser/management_policy.h"
 
 namespace extensions {
@@ -21,8 +20,7 @@ class ExtensionManagement;
 // extension block/allowlists and admin block/allowlists.
 class StandardManagementPolicyProvider : public ManagementPolicy::Provider {
  public:
-  explicit StandardManagementPolicyProvider(ExtensionManagement* settings,
-                                            Profile* profile);
+  explicit StandardManagementPolicyProvider(ExtensionManagement* settings);
 
   ~StandardManagementPolicyProvider() override;
 
@@ -48,7 +46,6 @@ class StandardManagementPolicyProvider : public ManagementPolicy::Provider {
                             std::u16string* error) const override;
 
  private:
-  raw_ptr<Profile> profile_;
   raw_ptr<ExtensionManagement> settings_;
   bool ReturnLoadError(const extensions::Extension* extension,
                        std::u16string* error) const;
